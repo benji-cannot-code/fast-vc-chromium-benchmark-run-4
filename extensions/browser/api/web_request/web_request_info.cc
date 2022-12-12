7c47a5b6d31978c7cbfc2324700bab5359e49b49
David Bertoni <dbertoni@chromium.org>
2022-12-12 23:57:22 +0000

    [Extensions] Remove uses of DictionaryValue from the webRequest API.
    This CL also modernizes by moving from std::unique_ptr to
    absl::optional where possible.
    
    Bug: 1187061
    Change-Id: I62a937b28d5f0ecfa5d417b6d954be0c4f639d1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098305
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082155}

--- extensions/browser/api/web_request/web_request_info.cc
+++ extensions/browser/api/web_request/web_request_info.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/url_loader.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace keys = extension_web_request_api_constants;
 
@@ -112,14 +113,14 @@ bool CreateUploadDataSourcesFromResourceRequest(
   return true;
 }
 
-std::unique_ptr<base::DictionaryValue> CreateRequestBodyData(
+absl::optional<base::Value::Dict> CreateRequestBodyData(
     const std::string& method,
     const net::HttpRequestHeaders& request_headers,
     const std::vector<std::unique_ptr<UploadDataSource>>& data_sources) {
   if (method != "POST" && method != "PUT")
-    return nullptr;
+    return absl::nullopt;
 
-  auto request_body_data = std::make_unique<base::DictionaryValue>();
+  base::Value::Dict request_body_data;
 
   // Get the data presenters, ordered by how specific they are.
   ParsedDataPresenter parsed_data_presenter(request_headers);
@@ -137,8 +138,7 @@ std::unique_ptr<base::DictionaryValue> CreateRequestBodyData(
       for (auto& source : data_sources)
         source->FeedToPresenter(presenters[i]);
       if (presenters[i]->Succeeded()) {
-        request_body_data->GetDict().Set(kKeys[i],
-                                         presenters[i]->TakeResult().value());
+        request_body_data.Set(kKeys[i], presenters[i]->TakeResult().value());
         some_succeeded = true;
         break;
       }
@@ -146,8 +146,7 @@ std::unique_ptr<base::DictionaryValue> CreateRequestBodyData(
   }
 
   if (!some_succeeded) {
-    request_body_data->SetStringKey(keys::kRequestBodyErrorKey,
-                                    "Unknown error.");
+    request_body_data.Set(keys::kRequestBodyErrorKey, "Unknown error.");
   }
 
   return request_body_data;
