b8c076cac5a82bbcfbf0c7d0e84e7583c4077e15
Cole Horvitz <colehorvitz@chromium.org>
2022-12-12 22:59:17 +0000

    CrSettingsSecurityKeysSubpageTest: Split into separate tests
    This change refactors test suites into their own test files.
    
    Bug: 1395417
    Change-Id: Ifedbc33cf5f3c7d458de9d88a0a8f46aca950065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090196
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082104}

---
+++ chrome/test/data/webui/settings/security_keys_test_util.ts
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+export function assertShown(
+    allDivs: string[], dialog: HTMLElement, expectedId: string) {
+  assertTrue(allDivs.includes(expectedId));
+
+  const allShown = allDivs.filter(id => {
+    return dialog.shadowRoot!.querySelector(`#${id}`)!.classList.contains(
+        'iron-selected');
+  });
+  assertEquals(1, allShown.length);
+  assertEquals(expectedId, allShown[0]);
+}
---
+++ chrome/test/data/webui/settings/test_security_keys_browser_proxy.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+/**
+ * A base class for all security key subpage test browser proxies to
+ * inherit from. Provides a |promiseMap_| that proxies can be used to
+ * simulate Promise resolution via |setResponseFor| and |handleMethod|.
+ */
+export class TestSecurityKeysBrowserProxy extends TestBrowserProxy {
+  /**
+   * A map from method names to a promise to return when that method is
+   * called. (If no promise is installed, a never-resolved promise is
+   * returned.)
+   */
+  private promiseMap_ = new Map<string, Promise<any>>();
+
+  setResponseFor(methodName: string, promise: Promise<any>) {
+    this.promiseMap_.set(methodName, promise);
+  }
+
+  protected handleMethod(methodName: string, arg?: any): Promise<any> {
+    this.methodCalled(methodName, arg);
+    const promise = this.promiseMap_.get(methodName);
+    if (promise !== undefined) {
+      this.promiseMap_.delete(methodName);
+      return promise;
+    }
+
+    // Return a Promise that never resolves.
+    return new Promise(() => {});
+  }
+}
