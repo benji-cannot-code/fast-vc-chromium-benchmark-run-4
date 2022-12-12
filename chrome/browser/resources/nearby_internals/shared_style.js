7037ea8f3851fd52990dc5a39d7d323a8238d833
dpapad <dpapad@chromium.org>
2022-12-12 22:06:50 +0000

    Fix TrustedTypes violation in chrome://nearby-internals.
    Bug: 1098690
    Fixed: b/262055311
    Change-Id: Ia9722c6b9c25f6c3332e8c31f41f2f65e794fb02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4095736
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1082046}

--- chrome/browser/resources/nearby_internals/shared_style.js
+++ chrome/browser/resources/nearby_internals/shared_style.js
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 
+import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
 const styleMod = document.createElement('dom-module');
-styleMod.innerHTML = `{__html_template__}`;
+styleMod.appendChild(html`{__html_template__}`.content);
 styleMod.register('shared-style');
