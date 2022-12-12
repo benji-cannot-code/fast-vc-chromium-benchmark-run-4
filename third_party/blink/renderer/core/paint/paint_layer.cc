05f360019230f7856d1625c29785674c55aa6b19
Philip Rogers <pdr@chromium.org>
2022-12-12 22:38:20 +0000

    Add a metric for Blink.Layout.UpdateLayerPositionsAfterLayout
    This will help measure the impact of removing this function. For
    example, it will ensure we do not accidentally improve it before
    running a finch trial to remove it.
    
    Change-Id: Iafa78e179f5f490640bdce5a0228d8844429e631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4092205
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082079}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -101,6 +101,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/filters/filter.h"
 #include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
@@ -293,6 +294,8 @@ const PaintLayer* PaintLayer::ContainingScrollContainerLayer(
 void PaintLayer::UpdateLayerPositionsAfterLayout() {
   DCHECK(IsRootLayer());
 
+  SCOPED_BLINK_UMA_HISTOGRAM_TIMER_HIGHRES(
+      "Blink.Layout.UpdateLayerPositionsAfterLayout");
   TRACE_EVENT0("blink,benchmark",
                "PaintLayer::updateLayerPositionsAfterLayout");
   RUNTIME_CALL_TIMER_SCOPE(
