13dbe673e034b1c92315224482899be297b3de5d
William Liu <liuwilliam@chromium.org>
2022-12-12 20:05:50 +0000

    Set requestStart/responseStart to connectEnd for prefetch.
    For speculation rule based prefetch, the resource-fetching request
    occurs before the navigation, resulting negative requestStart and responseStart. Set both negative values to connectEnd to respect the
    `<=` timeline events ordering, resembling the behaviour HTTP cache.
    
    Relevant doc:
    https://docs.google.com/document/d/1wMelfFY34jnyCDA6AAy0nm3l16vE0ruTo3y3rnEaso4 [internal access].
    
    Bug: 1382255
    Change-Id: I6e25b9304074217c6618a12c2aafbbe32a7cf51e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4009904
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081983}

--- third_party/blink/renderer/core/timing/performance_resource_timing.cc
+++ third_party/blink/renderer/core/timing/performance_resource_timing.cc
@@ -435,7 +435,7 @@ DOMHighResTimeStamp PerformanceResourceTiming::requestStart() const {
   if (!AllowTimingDetails())
     return 0.0;
   ResourceLoadTiming* timing = GetResourceLoadTiming();
-  if (!timing)
+  if (!timing || timing->SendStart().is_null())
     return connectEnd();
 
   return Performance::MonotonicTimeToDOMHighResTimeStamp(
@@ -518,8 +518,8 @@ void PerformanceResourceTiming::BuildJSONValue(V8ObjectBuilder& builder) const {
   builder.AddNumber("domainLookupStart", domainLookupStart());
   builder.AddNumber("domainLookupEnd", domainLookupEnd());
   builder.AddNumber("connectStart", connectStart());
-  builder.AddNumber("connectEnd", connectEnd());
   builder.AddNumber("secureConnectionStart", secureConnectionStart());
+  builder.AddNumber("connectEnd", connectEnd());
   builder.AddNumber("requestStart", requestStart());
   builder.AddNumber("responseStart", responseStart());
   builder.AddNumber("responseEnd", responseEnd());
