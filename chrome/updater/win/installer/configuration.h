e30e08c4b4e4e6e0d8ca291fe8b271b8db1d0a69
S. Ganesh <ganesh@chromium.org>
2022-12-12 21:44:45 +0000

    Fix leaks, use QuoteForCommandLineToArgvW, ScopedLocalAllocTyped
    This CL does the following:
    * uses `QuoteForCommandLineToArgvW` to quote a command line instead of
    hand-rolling the quoting or using base::CommandLine in a roundabout way.
    * changed  code calling `::CommandLineToArgvW` to use
    `base::win::ScopedLocalAllocTyped`.
    * There were instances where the LPWSTR from calling
    `::CommandLineToArgvW` was being leaked that are now fixed.
    
    Bug: 1369674
    Change-Id: I97225d7a45ee523c2050831c72a1b79162d326d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4094462
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082031}

--- chrome/updater/win/installer/configuration.h
+++ chrome/updater/win/installer/configuration.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
-#include "base/memory/raw_ptr.h"
+#include "base/win/scoped_localalloc.h"
 
 namespace updater {
 
@@ -41,7 +41,7 @@ class Configuration {
   void Clear();
   bool ParseCommandLine(const wchar_t* command_line);
 
-  raw_ptr<wchar_t*> args_ = nullptr;
+  base::win::ScopedLocalAllocTyped<wchar_t*> args_;
   const wchar_t* command_line_ = nullptr;
   int argument_count_ = 0;
   Operation operation_ = INSTALL_PRODUCT;
