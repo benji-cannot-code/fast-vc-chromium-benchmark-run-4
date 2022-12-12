7ec47001d8ea665de8b745456dad8b6c5edce143
Steinar H. Gunderson <sesse@chromium.org>
2022-12-12 17:17:33 +0000

    Make SelectorChecker::pseudo_argument a pointer.
    This removes the need for a custom copy constructor and destructor
    on SelectorChecker, which ripples through the code, changes some
    inlining decision and removes more than 2 kB of binary code (x86-64).
    The benchmarks may be exaggerated, though; it's hard to say.
    It's pretty clear that it doesn't hurt, in any case.
    
    As usual, won't affect MotionMark much, since it doesn't do much
    selector checking.
    
    Initial style (µs)     Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce                7039      6996   +0.6%  [ -0.5%,  +1.8%]
    Encyclopedia            68392     68309   +0.1%  [ -0.7%,  +0.9%]
    Extension               87632     87455   +0.2%  [ -0.6%,  +1.2%]
    News                    30061     29625   +1.5%  [ +0.5%,  +3.1%]
    Search                   2010      2013   -0.1%  [ -2.3%,  +1.2%]
    Social1                 17032     16943   +0.5%  [ -0.2%,  +1.8%]
    Social2                   765       758   +0.9%  [ -0.5%,  +2.5%]
    Sports                  24374     24240   +0.6%  [ -0.8%,  +2.0%]
    Video                   29826     29696   +0.4%  [ -1.2%,  +1.1%]
    Geometric mean                            +0.5%  [ +0.1%,  +1.5%]
    
    Recalc style (µs)      Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce                8335      8251   +1.0%  [ -0.0%,  +2.0%]
    Encyclopedia            61417     60997   +0.7%  [ -1.2%,  +2.4%]
    Extension               85278     84951   +0.4%  [ -0.9%,  +1.4%]
    News                    22605     22166   +2.0%  [ +1.0%,  +3.5%]
    Search                    185       185   +0.2%  [ -2.1%,  +2.7%]
    Social1                 12280     12091   +1.6%  [ +0.6%,  +2.9%]
    Social2                   381       377   +0.8%  [ -0.6%,  +2.7%]
    Sports                  19329     18914   +2.2%  [ +0.9%,  +3.3%]
    Video                   18974     18774   +1.1%  [ -1.1%,  +1.9%]
    Geometric mean                            +1.1%  [ +0.7%,  +2.1%]
    
    Change-Id: I320857c3ea1eedbae52b4ea0376c14202cd790ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085035
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081852}

--- third_party/blink/renderer/core/css/selector_checker.cc
+++ third_party/blink/renderer/core/css/selector_checker.cc
@@ -1256,7 +1256,7 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
             ViewTransitionUtils::GetActiveTransition(element.GetDocument());
         DCHECK(transition);
         return transition->MatchForOnlyChild(context.pseudo_id,
-                                             context.pseudo_argument);
+                                             *context.pseudo_argument);
       }
 
       ContainerNode* parent = element.ParentElementOrDocumentFragment();
