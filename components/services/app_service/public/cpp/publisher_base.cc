82da44542eb8dea01564d514c076dd5730e5a925
Nancy Wang <nancylingwang@chromium.org>
2022-12-13 00:25:17 +0000

    Remove the SetWindowMode mojom interface.
    BUG=1253250
    
    Change-Id: I18ff7e56c5b02c700843009ac9c20e94047166b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4096162
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082171}

--- components/services/app_service/public/cpp/publisher_base.cc
+++ components/services/app_service/public/cpp/publisher_base.cc
@@ -93,9 +93,4 @@ void PublisherBase::SetResizeLocked(const std::string& app_id,
   NOTIMPLEMENTED();
 }
 
-void PublisherBase::SetWindowMode(const std::string& app_id,
-                                  apps::mojom::WindowMode window_mode) {
-  NOTIMPLEMENTED();
-}
-
 }  // namespace apps
