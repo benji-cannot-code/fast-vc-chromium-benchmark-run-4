765866f5951d2c697852955a0bf598bf2faffef4
Takashi Toyoshima <toyoshim@chromium.org>
2022-12-13 03:22:40 +0000

    Reland "Prerender2 CSP: Implement script-src 'inline-speculation-rules'"
    This reverts commit 9b861bff816197921cf1013c0429295463067617.
    
    The failure wasn't this CL specific, but a common infra flakiness
    that the test server caused.
    A mitigation was landed as https://crrev.com/c/4092514.
    
    Original change's description:
    > Revert "Prerender2 CSP: Implement script-src 'inline-speculation-rules'"
    >
    > This reverts commit 9a89f759b68a49bb969ec1a7a4d3d966cb5707bc.
    >
    > Reason for revert: Introduces consistent web tests failures on multiple Mac bots.
    > https://ci.chromium.org/ui/p/chromium/builders/ci/mac11-arm64-rel-tests/11486/overview
    > https://ci.chromium.org/ui/p/chromium/builders/ci/mac12-arm64-rel-tests/4418/overview
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac12%20Tests/4679/overview
    >
    > Original change's description:
    > > Prerender2 CSP: Implement script-src 'inline-speculation-rules'
    > >
    > > This patch implements CSP 'inline-speculation-rules' source for
    > > <script type="speculationrules">. This source is available only for
    > > script-src directive, and can permit inline script tags that has
    > > the type attribute with "speculation-rules". This allows developers
    > > to permit inline script only for speculationrules.
    > >
    > > With this patch, the change is applied only for isolated worlds
    > > where we run Chrome Extensions. But it will be extended to the
    > > open web platform once we publish a patch spec and follow the I2S.
    > >
    > > Bug: 1382361
    > > Change-Id: I69b1f3e61e64c3f956964fe59e2e847239654c81
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4056835
    > > Reviewed-by: Dominic Farolino <dom@chromium.org>
    > > Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    > > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > > Reviewed-by: Mike West <mkwst@chromium.org>
    > > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1081341}
    >
    > Bug: 1382361
    > Change-Id: Id9507ac4fe114b3f2b61a43df592f0efaf358e87
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4092170
    > Reviewed-by: Monica Basta <msalama@chromium.org>
    > Commit-Queue: Monica Basta <msalama@chromium.org>
    > Owners-Override: Monica Basta <msalama@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1081474}
    
    Bug: 1382361
    Change-Id: Ieb129940b5591245be772de326b0b5112254220f
    Cq-Include-Trybots: luci.chromium.try:mac11-arm64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4096122
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082231}

--- third_party/blink/renderer/core/frame/csp/content_security_policy.h
+++ third_party/blink/renderer/core/frame/csp/content_security_policy.h
@@ -141,6 +141,7 @@ class CORE_EXPORT ContentSecurityPolicy final
     kNavigation,
     kScript,
     kScriptAttribute,
+    kScriptSpeculationRules,  // TODO(https://crbug.com/1382361): Standardize.
     kStyle,
     kStyleAttribute
   };
