228f091fe00209447348a20a1f8c386d408d2f27
angel <angelsan@google.com>
2022-12-13 22:48:22 +0000

    clipboard: Shift + Launcher + V keyboard shortcut to paste plain text
    Added keyboard shortcut Shift + Launcher + V to paste clipboard history as plain text if clipboard menu is already showing.
    
    Bug: b:260609866
    Change-Id: Ia1745588da3b02136571a91e3ae1200cddf2dc13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087783
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Commit-Queue: Angel Sanchez <angelsan@google.com>
    Auto-Submit: Angel Sanchez <angelsan@google.com>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082776}

--- ash/accelerators/accelerator_controller_impl.cc
+++ ash/accelerators/accelerator_controller_impl.cc
@@ -625,6 +625,8 @@ bool AcceleratorControllerImpl::CanPerformAction(
       return accelerators::CanMoveActiveWindowBetweenDisplays();
     case NEW_INCOGNITO_WINDOW:
       return accelerators::CanCreateNewIncognitoWindow();
+    case PASTE_CLIPBOARD_HISTORY_PLAIN_TEXT:
+      return true;
     case PRIVACY_SCREEN_TOGGLE:
       return accelerators::CanTogglePrivacyScreen();
     case ROTATE_SCREEN:
@@ -1059,6 +1061,9 @@ void AcceleratorControllerImpl::PerformAction(
     case OPEN_GET_HELP:
       accelerators::OpenHelp();
       break;
+    case PASTE_CLIPBOARD_HISTORY_PLAIN_TEXT:
+      accelerators::ToggleClipboardHistory(/*is_plain_text_paste=*/true);
+      break;
     case POWER_PRESSED:
     case POWER_RELEASED:
       if (!base::SysInfo::IsRunningOnChromeOS()) {
@@ -1186,7 +1191,7 @@ void AcceleratorControllerImpl::PerformAction(
       accelerators::ToggleCapsLock();
       break;
     case TOGGLE_CLIPBOARD_HISTORY:
-      accelerators::ToggleClipboardHistory();
+      accelerators::ToggleClipboardHistory(/*is_plain_text_paste=*/false);
       break;
     case TOGGLE_DICTATION:
       base::RecordAction(UserMetricsAction("Accel_Toggle_Dictation"));
