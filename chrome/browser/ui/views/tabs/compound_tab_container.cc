30b2eae51bfa1056d70f2efe289f5119bc8f65c0
Taylor Bergquist <tbergquist@chromium.org>
2022-12-13 19:51:53 +0000

    Target events between CompoundTabContainer's subcontainers.
    Bug: 1346023
    Change-Id: I71f44cadaf9a8116a57f90aec06622baf4a37509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083683
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082660}

--- chrome/browser/ui/views/tabs/compound_tab_container.cc
+++ chrome/browser/ui/views/tabs/compound_tab_container.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/list_selection_model.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/views/layout/layout_types.h"
+#include "ui/views/rect_based_targeting_utils.h"
 #include "ui/views/view.h"
 #include "ui/views/view_utils.h"
 
@@ -230,6 +231,8 @@ CompoundTabContainer::CompoundTabContainer(
       hover_card_controller_(hover_card_controller),
       scroll_contents_view_(scroll_contents_view),
       bounds_animator_(this) {
+  SetEventTargeter(std::make_unique<views::ViewTargeter>(this));
+
   if (!gfx::Animation::ShouldRenderRichAnimation())
     bounds_animator_.SetAnimationDuration(base::TimeDelta());
 }
@@ -480,6 +483,9 @@ void CompoundTabContainer::UpdateHoverCard(
 
 void CompoundTabContainer::HandleLongTap(ui::GestureEvent* const event) {
   TabContainer* const tab_container = GetTabContainerAt(event->location());
+  if (!tab_container)
+    return;
+
   ConvertEventToTarget(tab_container, event);
   tab_container->HandleLongTap(event);
 }
@@ -655,6 +661,14 @@ gfx::Size CompoundTabContainer::CalculatePreferredSize() const {
       unpinned_tab_container_->GetPreferredSize());
 }
 
+views::View* CompoundTabContainer::GetTooltipHandlerForPoint(
+    const gfx::Point& point) {
+  TabContainer* const sub_container = GetTabContainerAt(point);
+  return sub_container ? sub_container->GetTooltipHandlerForPoint(
+                             ConvertPointToTarget(this, sub_container, point))
+                       : this;
+}
+
 void CompoundTabContainer::Layout() {
   // Pinned container gets however much space it wants.
   pinned_tab_container_->SetBoundsRect(
@@ -727,7 +741,11 @@ BrowserRootView::DropIndex CompoundTabContainer::GetDropIndex(
 
 BrowserRootView::DropTarget* CompoundTabContainer::GetDropTarget(
     gfx::Point loc_in_local_coords) {
-  return GetTabContainerAt(loc_in_local_coords);
+  NOTREACHED();  // TODO(1346023): Implement text drag and drop.
+
+  // This might be a starting point for implementation though.
+  TabContainer* const tab_container = GetTabContainerAt(loc_in_local_coords);
+  return tab_container ? tab_container : this;
 }
 
 views::View* CompoundTabContainer::GetViewForDrop() {
@@ -745,6 +763,22 @@ void CompoundTabContainer::HandleDragUpdate(
 void CompoundTabContainer::HandleDragExited() {
   // TODO(1346023): Implement text drag and drop.
   NOTREACHED();
+}
+
+views::View* CompoundTabContainer::TargetForRect(views::View* root,
+                                                 const gfx::Rect& rect) {
+  CHECK_EQ(root, this);
+
+  if (!views::UsePointBasedTargeting(rect))
+    return views::ViewTargeterDelegate::TargetForRect(root, rect);
+
+  const gfx::Point point(rect.CenterPoint());
+  TabContainer* const sub_container = GetTabContainerAt(point);
+  if (sub_container == nullptr)
+    return this;
+
+  return sub_container->GetEventHandlerForRect(ToEnclosingRect(
+      ConvertRectToTarget(this, sub_container, gfx::RectF(rect))));
 }
 
 void CompoundTabContainer::UpdateAnimationTarget(TabSlotView* tab_slot_view,
@@ -859,15 +893,27 @@ raw_ref<TabContainer> CompoundTabContainer::GetTabContainerFor(
 
 TabContainer* CompoundTabContainer::GetTabContainerAt(
     gfx::Point point_in_local_coords) {
-  if (pinned_tab_container_->bounds().Contains(point_in_local_coords))
-    return base::to_address(pinned_tab_container_);
-  if (unpinned_tab_container_->bounds().Contains(point_in_local_coords))
+  const bool in_pinned =
+      pinned_tab_container_->bounds().Contains(point_in_local_coords);
+  const bool in_unpinned =
+      unpinned_tab_container_->bounds().Contains(point_in_local_coords);
+
+  if (in_pinned && in_unpinned) {
+    const int cutoff_x = (pinned_tab_container_->bounds().right() +
+                          unpinned_tab_container_->bounds().x()) /
+                         2;
+    if (point_in_local_coords.x() < cutoff_x)
+      return base::to_address(pinned_tab_container_);
     return base::to_address(unpinned_tab_container_);
-  NOTREACHED() << "point_in_local_coords " << point_in_local_coords.ToString()
-               << " is not in pinned at: "
-               << pinned_tab_container_->bounds().ToString()
-               << " or unpinned at: "
-               << unpinned_tab_container_->bounds().ToString();
+  }
+
+  if (in_pinned)
+    return base::to_address(pinned_tab_container_);
+  if (in_unpinned)
+    return base::to_address(unpinned_tab_container_);
+
+  // `point_in_local_coords` might be in neither sub container if our layout is
+  // (transiently) stale, e.g. during window creation.
   return nullptr;
 }
 
--- chrome/browser/ui/views/tabs/compound_tab_container.h
+++ chrome/browser/ui/views/tabs/compound_tab_container.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab_container_controller.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/gfx/geometry/point.h"
+#include "ui/views/view_targeter_delegate.h"
 
 class TabHoverCardController;
 class TabDragContextBase;
@@ -20,7 +21,8 @@ class TabDragContextBase;
 // and the unpinned tabs in the other.
 // Indices in the public and private API are all in overall indices, unless
 // specifically noted otherwise as being relative to a specific container.
-class CompoundTabContainer : public TabContainer {
+class CompoundTabContainer : public TabContainer,
+                             public views::ViewTargeterDelegate {
  public:
   METADATA_HEADER(CompoundTabContainer);
 
@@ -102,6 +104,7 @@ class CompoundTabContainer : public TabContainer {
   gfx::Size GetMinimumSize() const override;
   views::SizeBounds GetAvailableSize(const View* child) const override;
   gfx::Size CalculatePreferredSize() const override;
+  views::View* GetTooltipHandlerForPoint(const gfx::Point& point) override;
   void Layout() override;
   void PaintChildren(const views::PaintInfo& paint_info) override;
   void ChildPreferredSizeChanged(views::View* child) override;
@@ -115,6 +118,9 @@ class CompoundTabContainer : public TabContainer {
   void HandleDragUpdate(
       const absl::optional<BrowserRootView::DropIndex>& index) override;
   void HandleDragExited() override;
+
+  // views::ViewTargeterDelegate:
+  views::View* TargetForRect(views::View* root, const gfx::Rect& rect) override;
 
   // Notifies this CompoundTabContainer that `tab_slot_view` must be animated to
   // `target_bounds`. `pinned` indicates whether these bounds are relative to
