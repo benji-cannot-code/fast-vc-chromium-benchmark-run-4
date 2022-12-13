85f6ed037e825f0887929b538f9ef3e87a81c8ff
Joe Downing <joedow@google.com>
2022-12-13 03:56:51 +0000

    Init supported_formats with an initializer list
    This is a minor cleanup CL which uses an initializer list to set the
    vector of supported video formats in the WebrtcVideoEncoderFactory
    instead of emplacing each one individually in the c'tor. This lone
    exception is H.264 which requires a function call to determine whether
    it is supported.
    
    Change-Id: Ibb2eca293cf082340576ded93cedc743504e9cdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098650
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082273}

--- remoting/protocol/webrtc_video_encoder_factory.cc
+++ remoting/protocol/webrtc_video_encoder_factory.cc
@@ -10,10 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "remoting/protocol/video_channel_state_observer.h"
 #include "remoting/protocol/webrtc_video_encoder_wrapper.h"
-#include "third_party/webrtc/api/video_codecs/av1_profile.h"
-#include "third_party/webrtc/api/video_codecs/sdp_video_format.h"
 #include "third_party/webrtc/api/video_codecs/video_codec.h"
-#include "third_party/webrtc/api/video_codecs/vp9_profile.h"
 
 #if defined(USE_H264_ENCODER)
 #include "remoting/codec/webrtc_video_encoder_gpu.h"
@@ -23,22 +20,12 @@ namespace remoting::protocol {
 
 WebrtcVideoEncoderFactory::WebrtcVideoEncoderFactory()
     : main_task_runner_(base::SingleThreadTaskRunner::GetCurrentDefault()) {
-  formats_.emplace_back("VP8");
-  formats_.emplace_back("VP9");
-  formats_.emplace_back(webrtc::SdpVideoFormat(
-      "VP9", {{webrtc::kVP9FmtpProfileId,
-               webrtc::VP9ProfileToString(webrtc::VP9Profile::kProfile1)}}));
-  formats_.emplace_back("AV1");
-  formats_.emplace_back(webrtc::SdpVideoFormat(
-      "AV1",
-      {{webrtc::kAV1FmtpProfile,
-        webrtc::AV1ProfileToString(webrtc::AV1Profile::kProfile1).data()}}));
 #if defined(USE_H264_ENCODER)
   // This call will query the underlying media classes to determine whether
   // hardware encoding is supported or not. We use a default resolution and
   // framerate so the call doesn't fail due to invalid params.
   if (WebrtcVideoEncoderGpu::IsSupportedByH264({{1920, 1080}, 30})) {
-    formats_.emplace_back("H264");
+    supported_formats_.emplace_back("H264");
   }
 #endif
 }
@@ -58,7 +45,7 @@ WebrtcVideoEncoderFactory::CreateVideoEncoder(
 
 std::vector<webrtc::SdpVideoFormat>
 WebrtcVideoEncoderFactory::GetSupportedFormats() const {
-  return formats_;
+  return supported_formats_;
 }
 
 void WebrtcVideoEncoderFactory::SetVideoChannelStateObserver(
