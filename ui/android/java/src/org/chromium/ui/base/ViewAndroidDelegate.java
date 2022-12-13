de40289578e159064424797c9a8ae4117b04bdfe
Sinan Sahin <sinansahin@google.com>
2022-12-13 19:49:28 +0000

    [Fixit] Remove Android N references in ViewAndroidDelegate
    Bug: 1385893
    Change-Id: Id717faea5e6d46f3ec9d76d40c78e56404108656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4102620
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082655}

--- ui/android/java/src/org/chromium/ui/base/ViewAndroidDelegate.java
+++ ui/android/java/src/org/chromium/ui/base/ViewAndroidDelegate.java
@@ -7,7 +7,6 @@ package org.chromium.ui.base;
 
 import android.content.ClipData;
 import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.PointerIcon;
@@ -25,7 +24,6 @@ import androidx.core.view.MarginLayoutParamsCompat;
 import org.chromium.base.ObserverList;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
-import org.chromium.base.compat.ApiHelperForN;
 import org.chromium.ui.dragdrop.DragAndDropDelegate;
 import org.chromium.ui.dragdrop.DragAndDropDelegateImpl;
 import org.chromium.ui.dragdrop.DragStateTracker;
@@ -262,18 +260,13 @@ public class ViewAndroidDelegate {
     @VisibleForTesting
     @CalledByNative
     public void onCursorChangedToCustom(Bitmap customCursorBitmap, int hotspotX, int hotspotY) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            PointerIcon icon =
-                    ApiHelperForN.createPointerIcon(customCursorBitmap, hotspotX, hotspotY);
-            ApiHelperForN.setPointerIcon(getContainerViewGroup(), icon);
-        }
+        PointerIcon icon = PointerIcon.create(customCursorBitmap, hotspotX, hotspotY);
+        getContainerViewGroup().setPointerIcon(icon);
     }
 
     @VisibleForTesting
     @CalledByNative
     public void onCursorChanged(int cursorType) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) return;
-
         // Allow stylus writing handler to override the cursor.
         if (mHoverActionStylusWritable && mStylusWritingCursorHandler != null
                 && mStylusWritingCursorHandler.didHandleCursorUpdate(getContainerViewGroup())) {
@@ -403,7 +396,7 @@ public class ViewAndroidDelegate {
         }
         ViewGroup containerView = getContainerViewGroup();
         PointerIcon icon = PointerIcon.getSystemIcon(containerView.getContext(), pointerIconType);
-        ApiHelperForN.setPointerIcon(containerView, icon);
+        containerView.setPointerIcon(icon);
     }
 
     @CalledByNative
