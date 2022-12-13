4cee62122b736f315f9b2ea554fe72443b86f710
Antonio Gomes <tonikitoo@igalia.com>
2022-12-13 18:16:10 +0000

    [ozone/wayland] Use more 'auto' when doing static_cast<XXX>
    BUG=578890
    R=nickdiego@igalia.com
    
    Change-Id: I6f76eab2521c7e0ffa6896ced72c9269816f0bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4100854
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1082558}

--- ui/ozone/platform/wayland/host/wayland_toplevel_window.cc
+++ ui/ozone/platform/wayland/host/wayland_toplevel_window.cc
@@ -640,13 +640,13 @@ void WaylandToplevelWindow::OcclusionChanged(void* data,
                                              uint32_t occlusion_reason) {}
 
 void WaylandToplevelWindow::LockFrame(void* data, zaura_surface* surface) {
-  WaylandToplevelWindow* self = static_cast<WaylandToplevelWindow*>(data);
+  auto* self = static_cast<WaylandToplevelWindow*>(data);
   DCHECK(self);
   self->OnFrameLockingChanged(true);
 }
 
 void WaylandToplevelWindow::UnlockFrame(void* data, zaura_surface* surface) {
-  WaylandToplevelWindow* self = static_cast<WaylandToplevelWindow*>(data);
+  auto* self = static_cast<WaylandToplevelWindow*>(data);
   DCHECK(self);
   self->OnFrameLockingChanged(false);
 }
@@ -654,7 +654,7 @@ void WaylandToplevelWindow::UnlockFrame(void* data, zaura_surface* surface) {
 void WaylandToplevelWindow::OcclusionStateChanged(void* data,
                                                   zaura_surface* surface,
                                                   uint32_t mode) {
-  WaylandToplevelWindow* self = static_cast<WaylandToplevelWindow*>(data);
+  auto* self = static_cast<WaylandToplevelWindow*>(data);
   DCHECK(self);
   auto state = PlatformWindowOcclusionState::kUnknown;
   switch (mode) {
@@ -677,18 +677,18 @@ void WaylandToplevelWindow::OcclusionStateChanged(void* data,
 void WaylandToplevelWindow::DeskChanged(void* data,
                                         zaura_surface* surface,
                                         int state) {
-  WaylandToplevelWindow* self = static_cast<WaylandToplevelWindow*>(data);
+  auto* self = static_cast<WaylandToplevelWindow*>(data);
   DCHECK(self);
   self->OnDeskChanged(state);
 }
 
 void WaylandToplevelWindow::StartThrottle(void* data, zaura_surface* surface) {
-  WaylandToplevelWindow* self = static_cast<WaylandToplevelWindow*>(data);
+  auto* self = static_cast<WaylandToplevelWindow*>(data);
   self->delegate()->SetFrameRateThrottleEnabled(true);
 }
 
 void WaylandToplevelWindow::EndThrottle(void* data, zaura_surface* surface) {
-  WaylandToplevelWindow* self = static_cast<WaylandToplevelWindow*>(data);
+  auto* self = static_cast<WaylandToplevelWindow*>(data);
   self->delegate()->SetFrameRateThrottleEnabled(false);
 }
 
--- ui/ozone/platform/wayland/host/wayland_zwp_linux_dmabuf.cc
+++ ui/ozone/platform/wayland/host/wayland_zwp_linux_dmabuf.cc
@@ -161,8 +161,7 @@ void WaylandZwpLinuxDmabuf::Modifiers(
     uint32_t format,
     uint32_t modifier_hi,
     uint32_t modifier_lo) {
-  WaylandZwpLinuxDmabuf* self = static_cast<WaylandZwpLinuxDmabuf*>(data);
-  if (self) {
+  if (auto* self = static_cast<WaylandZwpLinuxDmabuf*>(data)) {
     uint64_t modifier = static_cast<uint64_t>(modifier_hi) << 32 | modifier_lo;
     self->AddSupportedFourCCFormatAndModifier(format, {modifier});
   }
@@ -172,8 +171,7 @@ void WaylandZwpLinuxDmabuf::Modifiers(
 void WaylandZwpLinuxDmabuf::Format(void* data,
                                    struct zwp_linux_dmabuf_v1* zwp_linux_dmabuf,
                                    uint32_t format) {
-  WaylandZwpLinuxDmabuf* self = static_cast<WaylandZwpLinuxDmabuf*>(data);
-  if (self)
+  if (auto* self = static_cast<WaylandZwpLinuxDmabuf*>(data))
     self->AddSupportedFourCCFormatAndModifier(format, absl::nullopt);
 }
 
@@ -182,8 +180,7 @@ void WaylandZwpLinuxDmabuf::CreateSucceeded(
     void* data,
     struct zwp_linux_buffer_params_v1* params,
     struct wl_buffer* new_buffer) {
-  WaylandZwpLinuxDmabuf* self = static_cast<WaylandZwpLinuxDmabuf*>(data);
-  if (self)
+  if (auto* self = static_cast<WaylandZwpLinuxDmabuf*>(data))
     self->NotifyRequestCreateBufferDone(params, new_buffer);
 }
 
@@ -191,8 +188,7 @@ void WaylandZwpLinuxDmabuf::CreateSucceeded(
 void WaylandZwpLinuxDmabuf::CreateFailed(
     void* data,
     struct zwp_linux_buffer_params_v1* params) {
-  WaylandZwpLinuxDmabuf* self = static_cast<WaylandZwpLinuxDmabuf*>(data);
-  if (self)
+  if (auto* self = static_cast<WaylandZwpLinuxDmabuf*>(data))
     self->NotifyRequestCreateBufferDone(params, nullptr);
 }
 
