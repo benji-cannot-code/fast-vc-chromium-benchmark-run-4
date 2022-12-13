d2121e282a2271ba7229f0995f9ebe22878e1730
Roger Tinkoff <rtinkoff@google.com>
2022-12-13 16:32:51 +0000

    ash: Fake effects and effects-management enhancements
    SimpleToggleEffect changes:
    
    (1) Assigns a default icon and a11y ID. If a fake effect is needed
    with a different icon/ID, make another subclass of SimpleToggleEffect.
    
    (2) Callback is the base class OnEffectControlActivated(), subclass
    callbacks removed and the ability to assign any other callback is
    removed.
    
    Remove FakeVideoConferenceTrayController constructor body from the .cc
    file, "default" it in the header.
    
    Add a function to VideoConferenceTrayEffectsManager for retrieving
    set-value effects.
    
    Add function to VcHostedEffect for retrieving the number of states,
    and make this function and GetState() const.
    
    Bug: b/259838099
    Change-Id: I87c23ab585e58188a7a90ab549a01b6ddd8bc9ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4093453
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082466}

--- ash/system/video_conference/effects/fake_video_conference_effects.h
+++ ash/system/video_conference/effects/fake_video_conference_effects.h
@@ -23,19 +23,27 @@ namespace ash::fake_video_conference {
 class SimpleToggleEffect : public VcEffectsDelegate {
  public:
   // Simplest of all, no `icon` and no `accessible_name_id`, for unit tests.
+  explicit SimpleToggleEffect(const std::u16string& label_text);
+  // Allows setting `icon` and `accessible_name_id` if desired, for unit tests
+  // or the emulator.
   SimpleToggleEffect(const std::u16string& label_text,
-                     views::Button::PressedCallback button_callback);
-  // Allows setting `icon` if desired, and requires `accessible_name_id`, for
-  // unit tests or the emulator.
-  SimpleToggleEffect(const gfx::VectorIcon* icon,
-                     const std::u16string& label_text,
-                     int accessible_name_id,
-                     views::Button::PressedCallback button_callback);
+                     absl::optional<const gfx::VectorIcon*> icon,
+                     absl::optional<int> accessible_name_id);
 
   SimpleToggleEffect(const SimpleToggleEffect&) = delete;
   SimpleToggleEffect& operator=(const SimpleToggleEffect&) = delete;
 
   ~SimpleToggleEffect() override = default;
+
+  // VcEffectsDelegate:
+  int GetEffectState(int effect_id) override;
+  void OnEffectControlActivated(int effect_id, int value) override;
+
+  int num_activations_for_testing() { return num_activations_for_testing_; }
+
+ private:
+  // Number of times the control has been activated, used by unit tests.
+  int num_activations_for_testing_ = 0;
 };
 
 // Delegates that host a series of "fake" effects used in unit tests and the
@@ -49,10 +57,6 @@ class ASH_EXPORT CatEarsEffect : public SimpleToggleEffect {
   CatEarsEffect& operator=(const CatEarsEffect&) = delete;
 
   ~CatEarsEffect() override = default;
-
-  // VcEffectsDelegate:
-  int GetEffectState(int effect_id) override;
-  void OnEffectControlActivated(int effect_id, int value) override;
 };
 
 class ASH_EXPORT DogFurEffect : public SimpleToggleEffect {
@@ -63,10 +67,6 @@ class ASH_EXPORT DogFurEffect : public SimpleToggleEffect {
   DogFurEffect& operator=(const DogFurEffect&) = delete;
 
   ~DogFurEffect() override = default;
-
-  // VcEffectsDelegate:
-  int GetEffectState(int effect_id) override;
-  void OnEffectControlActivated(int effect_id, int value) override;
 };
 
 class ASH_EXPORT SpaceshipEffect : public SimpleToggleEffect {
@@ -77,10 +77,6 @@ class ASH_EXPORT SpaceshipEffect : public SimpleToggleEffect {
   SpaceshipEffect& operator=(const SpaceshipEffect&) = delete;
 
   ~SpaceshipEffect() override = default;
-
-  // VcEffectsDelegate:
-  int GetEffectState(int effect_id) override;
-  void OnEffectControlActivated(int effect_id, int value) override;
 };
 
 class ASH_EXPORT OfficeBunnyEffect : public SimpleToggleEffect {
@@ -91,10 +87,6 @@ class ASH_EXPORT OfficeBunnyEffect : public SimpleToggleEffect {
   OfficeBunnyEffect& operator=(const OfficeBunnyEffect&) = delete;
 
   ~OfficeBunnyEffect() override = default;
-
-  // VcEffectsDelegate:
-  int GetEffectState(int effect_id) override;
-  void OnEffectControlActivated(int effect_id, int value) override;
 };
 
 class ASH_EXPORT CalmForestEffect : public SimpleToggleEffect {
@@ -105,10 +97,6 @@ class ASH_EXPORT CalmForestEffect : public SimpleToggleEffect {
   CalmForestEffect& operator=(const CalmForestEffect&) = delete;
 
   ~CalmForestEffect() override = default;
-
-  // VcEffectsDelegate:
-  int GetEffectState(int effect_id) override;
-  void OnEffectControlActivated(int effect_id, int value) override;
 };
 
 class ASH_EXPORT StylishKitchenEffect : public SimpleToggleEffect {
@@ -119,10 +107,6 @@ class ASH_EXPORT StylishKitchenEffect : public SimpleToggleEffect {
   StylishKitchenEffect& operator=(const StylishKitchenEffect&) = delete;
 
   ~StylishKitchenEffect() override = default;
-
-  // VcEffectsDelegate:
-  int GetEffectState(int effect_id) override;
-  void OnEffectControlActivated(int effect_id, int value) override;
 };
 
 class ASH_EXPORT GreenhouseEffect : public SimpleToggleEffect {
@@ -133,10 +117,6 @@ class ASH_EXPORT GreenhouseEffect : public SimpleToggleEffect {
   GreenhouseEffect& operator=(const GreenhouseEffect&) = delete;
 
   ~GreenhouseEffect() override = default;
-
-  // VcEffectsDelegate:
-  int GetEffectState(int effect_id) override;
-  void OnEffectControlActivated(int effect_id, int value) override;
 };
 
 // Delegate that hosts a set-value effect.
@@ -147,6 +127,7 @@ class ASH_EXPORT ShaggyFurEffect : public VcEffectsDelegate {
     kBald = 0,
     kBuzzcut = 1,
     kThick = 2,
+    kMaxNumValues = 3,
   };
 
   ShaggyFurEffect();
@@ -159,6 +140,15 @@ class ASH_EXPORT ShaggyFurEffect : public VcEffectsDelegate {
   // VcEffectsDelegate:
   int GetEffectState(int effect_id) override;
   void OnEffectControlActivated(int effect_id, int value) override;
+
+  // Returns the number of times the button/state for `value` has been
+  // activated.
+  int GetNumActivationsForTesting(int value);
+
+ private:
+  // Number of times each value has been clicked, one count for each value in
+  // `FurShagginess`.
+  std::vector<int> num_activations_for_testing_;
 };
 
 }  // namespace ash::fake_video_conference
--- ash/system/video_conference/fake_video_conference_tray_controller.cc
+++ ash/system/video_conference/fake_video_conference_tray_controller.cc
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-FakeVideoConferenceTrayController::FakeVideoConferenceTrayController() =
-    default;
-
 void FakeVideoConferenceTrayController::SetCameraMuted(bool muted) {
   camera_muted_ = muted;
   OnCameraSWPrivacySwitchStateChanged(
