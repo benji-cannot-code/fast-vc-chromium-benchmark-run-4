8ff5848c3973a71740d8679dd71c44d1937618ac
Ahmed Fakhry <afakhry@chromium.org>
2022-12-13 04:59:12 +0000

    gif_recording: Implement the recording type menu widget
    This CL adds the widget that contains the recording type selection
    menu, which opens when the drop down button in the label view is
    pressed.
    
    This CL does not hook up the menu options to the capture mode
    controller yet, nor does it handle clicking on those options.
    These will be done in a follow-up CL.
    
    Fixed: b:261450790
    Test: Manually, Added unit tests.
    
    Change-Id: I99a5353063d1961b949bdf81648f5f622f370a20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082373
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082287}

--- ash/capture_mode/capture_mode_session_test_api.h
+++ ash/capture_mode/capture_mode_session_test_api.h
@@ -10,15 +10,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class CaptureModeSession;
+class CaptureLabelView;
 class CaptureModeBarView;
+class CaptureModeSession;
 class CaptureModeSettingsView;
-class UserNudgeController;
 class MagnifierGlass;
+class UserNudgeController;
 
 // Wrapper for CaptureModeSession that exposes internal state to test functions.
 class CaptureModeSessionTestApi {
  public:
+  CaptureModeSessionTestApi();
   explicit CaptureModeSessionTestApi(CaptureModeSession* session);
 
   CaptureModeSessionTestApi(CaptureModeSessionTestApi&) = delete;
@@ -29,9 +31,13 @@ class CaptureModeSessionTestApi {
 
   CaptureModeSettingsView* GetCaptureModeSettingsView();
 
+  CaptureLabelView* GetCaptureLabelView();
+
   views::Widget* GetCaptureModeSettingsWidget();
 
   views::Widget* GetCaptureLabelWidget();
+
+  views::Widget* GetRecordingTypeMenuWidget();
 
   views::Widget* GetDimensionsLabelWidget();
 
