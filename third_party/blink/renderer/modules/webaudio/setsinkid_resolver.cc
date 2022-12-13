4064b1706d48b775e60540abb3d942750013dfec
Hongchan Choi <hongchan@chromium.org>
2022-12-13 20:50:08 +0000

    Add TRACE_EVENT for debugging device selection and configuration
    This CL adds TRACE_EVENT macros to the critical code path for
    AudioContext.setSinkId() or relevant code locations.
    
    Bug: 1395165
    Change-Id: I1fe765dece840de140cf67085430f872741e08ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082326
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082705}

--- third_party/blink/renderer/modules/webaudio/setsinkid_resolver.cc
+++ third_party/blink/renderer/modules/webaudio/setsinkid_resolver.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_context.h"
 #include "third_party/blink/renderer/modules/webaudio/realtime_audio_destination_node.h"
+#include "third_party/blink/renderer/platform/audio/audio_utilities.h"
 
 namespace blink {
 
@@ -41,9 +42,15 @@ SetSinkIdResolver::SetSinkIdResolver(
     sink_descriptor_ =
         WebAudioSinkDescriptor(sink_id.GetAsString(), frame_token);
   }
+
+  TRACE_EVENT1("webaudio", "SetSinkIdResolver::SetSinkIdResolver",
+               "sink_id (after setting sink_descriptor_)",
+               audio_utilities::GetSinkIdForTracing(sink_descriptor_));
 }
 
 void SetSinkIdResolver::Start() {
+  TRACE_EVENT1("webaudio", "SetSinkIdResolver::Start", "sink_id",
+               audio_utilities::GetSinkIdForTracing(sink_descriptor_));
   DCHECK(IsMainThread());
 
   ExecutionContext* context = GetExecutionContext();
@@ -94,6 +101,8 @@ void SetSinkIdResolver::Start() {
 }
 
 void SetSinkIdResolver::OnSetSinkIdComplete(media::OutputDeviceStatus status) {
+  TRACE_EVENT1("webaudio", "SetSinkIdResolver::OnSetSinkIdComplete", "sink_id",
+               audio_utilities::GetSinkIdForTracing(sink_descriptor_));
   DCHECK(IsMainThread());
 
   auto* excecution_context = GetExecutionContext();
