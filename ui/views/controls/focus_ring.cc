66ec75550eb8cd7516b1e328e2cd9f8f76d36c0c
Elaine Chien <elainechien@chromium.org>
2022-12-13 03:26:14 +0000

    [ChromeRefresh2023] Apply placeholder corner radius to focus ring
    Note that because the placeholder value is quite large we run into
    crbug.com/1399636 which still needs to be fixed.
    
    Bug: 1394575
    Change-Id: I527d12ca56fadd99c7e9030f2a88739fbb78941c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090629
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082237}

--- ui/views/controls/focus_ring.cc
+++ ui/views/controls/focus_ring.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/theme_provider.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
 #include "ui/compositor/layer.h"
@@ -59,7 +60,10 @@ SkColor GetPaintColor(FocusRing* focus_ring, bool valid) {
 
 double GetCornerRadius(float halo_thickness) {
   const double thickness = halo_thickness / 2.f;
-  return FocusableBorder::kCornerRadiusDp + thickness;
+  return (features::IsChromeRefresh2023()
+              ? FocusableBorder::kChromeRefresh2023CornerRadiusDp
+              : FocusableBorder::kCornerRadiusDp) +
+         thickness;
 }
 
 SkPath GetHighlightPathInternal(const View* view, float halo_thickness) {
