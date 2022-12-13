5926ab99bf61ed15341e49dba3b822039a422a18
Juliet Levesque <julietlevesque@google.com>
2022-12-13 02:58:16 +0000

    [Fast Pair] Add pairing protocol to WriteDeviceToFootprints
    Adds an optional field to WriteDeviceToFootprints for the device pairing
    protocol, which is used in a follow up to log the final state of the
    retroactive pairing flow. We will eventually need to add logic to
    PendingWrites to also log successes of writing to Footprints, but this
    is a P2 since its a rare scenario and not launch blocking like the
    general retroactive flow is (tracking b/261917790)
    
    TESTED= No unit tests since refactoring, no new added logic. Protocol
    used in unit tests in follow up CL. Testing on DUT in follow up CL.
    
    Change-Id: I0642dc72faf38964445042e133ca165aaa3af0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090008
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1082188}

--- ash/quick_pair/repository/fast_pair_repository_impl.cc
+++ ash/quick_pair/repository/fast_pair_repository_impl.cc
@@ -330,7 +330,8 @@ void FastPairRepositoryImpl::AssociateAccountKey(
       device->metadata_id,
       base::BindOnce(&FastPairRepositoryImpl::WriteDeviceToFootprints,
                      weak_ptr_factory_.GetWeakPtr(), device->metadata_id,
-                     device->classic_address().value(), account_key));
+                     device->classic_address().value(), account_key,
+                     device->protocol));
 }
 
 bool FastPairRepositoryImpl::AssociateAccountKeyLocally(
@@ -354,6 +355,7 @@ void FastPairRepositoryImpl::WriteDeviceToFootprints(
     const std::string& hex_model_id,
     const std::string& mac_address,
     const std::vector<uint8_t>& account_key,
+    absl::optional<Protocol> device_protocol,
     DeviceMetadata* metadata,
     bool has_retryable_error) {
   if (!metadata) {
@@ -368,12 +370,14 @@ void FastPairRepositoryImpl::WriteDeviceToFootprints(
   footprints_fetcher_->AddUserFastPairInfo(
       fast_pair_info,
       base::BindOnce(&FastPairRepositoryImpl::OnWriteDeviceToFootprintsComplete,
-                     weak_ptr_factory_.GetWeakPtr(), mac_address, account_key));
+                     weak_ptr_factory_.GetWeakPtr(), mac_address, account_key,
+                     device_protocol));
 }
 
 void FastPairRepositoryImpl::OnWriteDeviceToFootprintsComplete(
     const std::string& mac_address,
     const std::vector<uint8_t>& account_key,
+    absl::optional<Protocol> device_protocol,
     bool success) {
   if (!success) {
     QP_LOG(WARNING)
@@ -602,7 +606,7 @@ void FastPairRepositoryImpl::RetryPendingWrites() {
         base::BindOnce(
             &FastPairRepositoryImpl::OnWriteDeviceToFootprintsComplete,
             weak_ptr_factory_.GetWeakPtr(), pending_write.mac_address,
-            account_key));
+            account_key, /*device_protocol=*/absl::nullopt));
   }
 }
 
