78c4af6c0020912250c7d37cb04f18e92c42c421
Vishwas Uppoor <vishwasuppoor@google.com>
2022-12-13 19:56:20 +0000

    [Metadata][Clank] Limit Keyboard Accessory chip width for credit cards
    During payments checkout with Keyboard Accessory, cards with long card
    holder name or the card product name take up the entire screen hiding
    other available cards. We want to limit the chip width to show the card
    art of the next card if available.
    
    This change limits the chip width for credit cards at 85% of the screen
    width. It does so by truncating the label (the first of the 2
    TextViews). This has the side effect that the last 4 digits could get
    truncated when the card number field is selected. This will be fixed in
    a follow-up change.
    
    This change does NOT limit the chip width for other Autofillable data
    like addresses or passwords.
    
    This change only focuses on credit card chips. The follow-up change
    (crrev.com/c/4062118) adds custom options to limit the chip width of a
    general Keyboard Accessory chip.
    
    Before: https://screenshot.googleplex.com/6uL722RD4nmnVSd
    After (Portrait): https://screenshot.googleplex.com/5CHGw7ZtCqQMB8o
    After (Landscape): https://screenshot.googleplex.com/72ZcyZRTwSTnCzG
    After (Last 4 digits are truncated):
    https://screenshot.googleplex.com/4pMKmRfqrHUuYPv
    
    Bug: 1376691
    Change-Id: I8f4104cb3559931ecbe4e0c5f1c7e2f44de69a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965178
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1082667}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -98,6 +98,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &autofill::features::kAutofillEnableSupportForHonorificPrefixes,
     &autofill::features::kAutofillEnableUpdateVirtualCardEnrollment,
     &autofill::features::kAutofillEnableVirtualCardMetadata,
+    &autofill::features::kAutofillEnableCardProductName,
     &blink::features::kForceWebContentsDarkMode,
     &blink::features::kOSKResizesVisualViewportByDefault,
     &blink::features::kPrerender2,
