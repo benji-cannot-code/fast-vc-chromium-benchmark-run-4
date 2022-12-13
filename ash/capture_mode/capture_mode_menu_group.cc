3568cc0f22445b1ea43c0a9fcf6f705c30f9be63
Ahmed Fakhry <afakhry@chromium.org>
2022-12-13 18:29:20 +0000

    gif_recording: Implement the recording type menu widget
    Original CL was reverted [https://chromium-review.googlesource.com/c/chromium/src/+/4100287]
    because they needed to revert another CL cleanly.
    This is just a pure reland.
    
    This CL adds the widget that contains the recording type selection
    menu, which opens when the drop down button in the label view is
    pressed.
    
    This CL does not hook up the menu options to the capture mode
    controller yet, nor does it handle clicking on those options.
    These will be done in a follow-up CL.
    
    Fixed: b:261450790
    Test: Manually, Added unit tests.
    Change-Id: I78e32cb84a6ee8e55898aa2b0832f7a9d20bd6df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098820
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082571}

--- ash/capture_mode/capture_mode_menu_group.cc
+++ ash/capture_mode/capture_mode_menu_group.cc
@@ -347,8 +347,12 @@ END_METADATA
 // -----------------------------------------------------------------------------
 // CaptureModeMenuGroup:
 
-CaptureModeMenuGroup::CaptureModeMenuGroup(Delegate* delegate)
-    : CaptureModeMenuGroup(delegate, /*menu_header=*/nullptr) {}
+CaptureModeMenuGroup::CaptureModeMenuGroup(
+    Delegate* delegate,
+    const gfx::Insets& inside_border_insets)
+    : CaptureModeMenuGroup(delegate,
+                           /*menu_header=*/nullptr,
+                           inside_border_insets) {}
 
 CaptureModeMenuGroup::CaptureModeMenuGroup(Delegate* delegate,
                                            const gfx::VectorIcon& header_icon,
@@ -358,7 +362,8 @@ CaptureModeMenuGroup::CaptureModeMenuGroup(Delegate* delegate,
           delegate,
           std::make_unique<CaptureModeMenuHeader>(header_icon,
                                                   std::move(header_label),
-                                                  managed_by_policy)) {}
+                                                  managed_by_policy),
+          kMenuGroupPadding) {}
 
 CaptureModeMenuGroup::~CaptureModeMenuGroup() = default;
 
@@ -469,7 +474,8 @@ std::u16string CaptureModeMenuGroup::GetOptionLabelForTesting(
 
 CaptureModeMenuGroup::CaptureModeMenuGroup(
     Delegate* delegate,
-    std::unique_ptr<CaptureModeMenuHeader> menu_header)
+    std::unique_ptr<CaptureModeMenuHeader> menu_header,
+    const gfx::Insets& inside_border_insets)
     : delegate_(delegate),
       menu_header_(menu_header ? AddChildView(std::move(menu_header))
                                : nullptr),
@@ -478,7 +484,7 @@ CaptureModeMenuGroup::CaptureModeMenuGroup(
   options_container_->SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical));
   SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kVertical, kMenuGroupPadding,
+      views::BoxLayout::Orientation::kVertical, inside_border_insets,
       kSpaceBetweenMenuItem));
 }
 
