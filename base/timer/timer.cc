67f87033dbbd717951703c223e6356c2b6a62faa
François Doray <fdoray@chromium.org>
2022-12-13 00:19:40 +0000

    Enable task cancellation features by default.
    We experiment with these features on 50% of stable for 28+ days with
    satisfactory results.
    
    In addition to enabling the features by default, this CL modifies
    ThreadPool's Delayed Task Manager to make it cancel its internal
    delayed task on the Service Thread. Without this, enabling the
    features by default trips a sequence check in various places.
    
    Bug: 1262205, 1189304
    Change-Id: I90e871f5c7f4bcb3ec2410aab6f523c23cc9e904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4094239
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082168}

--- base/timer/timer.cc
+++ base/timer/timer.cc
@@ -27,7 +27,7 @@ namespace {
 // Cache of the state of the kAlwaysAbandonScheduledTask feature. This avoids
 // the need to constantly query its enabled state through
 // FeatureList::IsEnabled().
-bool g_is_always_abandon_scheduled_task_enabled = false;
+bool g_is_always_abandon_scheduled_task_enabled = true;
 
 }  // namespace
 
