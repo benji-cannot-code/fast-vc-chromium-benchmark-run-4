3d0866626a6ab2d92ddb47cf4f118a3a200d3284
Minoru Chikamune <chikamune@chromium.org>
2022-12-13 02:49:01 +0000

    Code Health Rotation: Migrate base::ListValue to base::Value::List
    This CL is a follow up CL of https://crrev.com/c/4078784.
    - use absl::optional<base::Value::List> instead of
      std::unique_ptr<base::Value::List>.
    - use base::Value::List instead of std::unique_ptr<base::Value::List>.
    
    Bug: 1187062
    Change-Id: Iad15fa2f964f7f55f44f324ae61d3dc97552c98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4096161
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082177}

--- extensions/renderer/bindings/api_binding_js_util_unittest.cc
+++ extensions/renderer/bindings/api_binding_js_util_unittest.cc
@@ -162,7 +162,7 @@ TEST_F(APIBindingJSUtilUnittest, TestSendRequestWithOptions) {
   CallFunctionOnObject(context, v8_util, kSendRequestWithNoOptions);
   ASSERT_TRUE(last_request());
   EXPECT_EQ("alpha.functionWithCallback", last_request()->method_name);
-  EXPECT_EQ("[\"someString\"]", ValueToString(*last_request()->arguments_list));
+  EXPECT_EQ("[\"someString\"]", ValueToString(last_request()->arguments_list));
   reset_last_request();
 
   const char kSendRequestForUIThread[] =
@@ -173,7 +173,7 @@ TEST_F(APIBindingJSUtilUnittest, TestSendRequestWithOptions) {
   ASSERT_TRUE(last_request());
   EXPECT_EQ("alpha.functionWithCallback", last_request()->method_name);
   EXPECT_EQ("[\"someOtherString\"]",
-            ValueToString(*last_request()->arguments_list));
+            ValueToString(last_request()->arguments_list));
   reset_last_request();
 
   const char kSendRequestWithCustomCallback[] =
@@ -189,7 +189,7 @@ TEST_F(APIBindingJSUtilUnittest, TestSendRequestWithOptions) {
   CallFunctionOnObject(context, v8_util, kSendRequestWithCustomCallback);
   ASSERT_TRUE(last_request());
   EXPECT_EQ("alpha.functionWithCallback", last_request()->method_name);
-  EXPECT_EQ("[\"stringy\"]", ValueToString(*last_request()->arguments_list));
+  EXPECT_EQ("[\"stringy\"]", ValueToString(last_request()->arguments_list));
   bindings_system()->CompleteRequest(last_request()->request_id,
                                      base::Value::List(), std::string());
   EXPECT_EQ("true", GetStringPropertyFromObject(context->Global(), context,
@@ -214,7 +214,7 @@ TEST_F(APIBindingJSUtilUnittest, TestSendRequestSerializationFailure) {
   CallFunctionOnObject(context, v8_util, kSendRequest);
   ASSERT_TRUE(last_request());
   EXPECT_EQ("alpha.functionWithCallback", last_request()->method_name);
-  EXPECT_EQ("[null,null]", ValueToString(*last_request()->arguments_list));
+  EXPECT_EQ("[null,null]", ValueToString(last_request()->arguments_list));
   reset_last_request();
 }
 
