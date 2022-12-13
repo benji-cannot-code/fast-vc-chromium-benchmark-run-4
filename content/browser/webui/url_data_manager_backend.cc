cf1ad4555547866b2981785e4e0bc57519b8a131
dpapad <dpapad@chromium.org>
2022-12-13 16:24:16 +0000

    Delete WebUIReportOnlyTrustedTypes feature.
    This feature is no longer useful, as it was meant to be a temporary step
    for enabling TrustedTypes in WebUIs. The latter has happened for most
    Desktop WebUIs already, and a clear path forward exists for remaining
    (Ash-only) WebUIs, without the need for WebUIReportOnlyTrustedTypes.
    
    Bug: 1098690
    Change-Id: I6077316948922d8adc20b4d5699072f78863a816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098456
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082464}

--- content/browser/webui/url_data_manager_backend.cc
+++ content/browser/webui/url_data_manager_backend.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/content_client.h"
-#include "content/public/common/content_features.h"
 #include "content/public/common/url_constants.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
@@ -50,10 +49,6 @@ namespace {
 
 const char kChromeURLContentSecurityPolicyHeaderName[] =
     "Content-Security-Policy";
-const char kChromeURLContentSecurityPolicyReportOnlyHeaderName[] =
-    "Content-Security-Policy-Report-Only";
-const char kChromeURLContentSecurityPolicyReportOnlyHeaderValue[] =
-    "require-trusted-types-for 'script'";
 
 const char kChromeURLCrossOriginOpenerPolicyName[] =
     "Cross-Origin-Opener-Policy";
@@ -202,11 +197,6 @@ scoped_refptr<net::HttpResponseHeaders> URLDataManagerBackend::GetHeaders(
   if (source->ShouldDenyXFrameOptions()) {
     headers->SetHeader(kChromeURLXFrameOptionsHeaderName,
                        kChromeURLXFrameOptionsHeaderValue);
-  }
-
-  if (base::FeatureList::IsEnabled(features::kWebUIReportOnlyTrustedTypes)) {
-    headers->SetHeader(kChromeURLContentSecurityPolicyReportOnlyHeaderName,
-                       kChromeURLContentSecurityPolicyReportOnlyHeaderValue);
   }
 
   if (!source->AllowCaching())
