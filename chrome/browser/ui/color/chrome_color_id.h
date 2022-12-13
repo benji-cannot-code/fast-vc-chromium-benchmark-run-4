397c88094487a0821c07760608bde371aadbda9c
Emilia Paz <emiliapaz@chromium.org>
2022-12-13 14:35:03 +0000

    [Extensions c2s] Add requests access sections skeleton
    Request access section in the extensions menu will list all the extensions requesting access to the current site. This CL introduces
    the sections skeleton, and will populate the extensions in a next CL.
    
    Screenshot: https://drive.google.com/file/d/1mGRTmtY9KH_-gexfhOkbf8mxL4MjjfD5/view?usp=sharing (when forced to visible, currently is never visible)
    
    Bug: 1390952
    Change-Id: I91f9c9295176eb6ef0688e5a6e17cf68c7642c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081333
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082448}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorExtensionMenuIconDisabled) \
   E_CPONLY(kColorExtensionMenuPinButtonIcon) \
   E_CPONLY(kColorExtensionMenuPinButtonIconDisabled) \
+  E_CPONLY(kColorExtensionsMenuHighlightedBackground) \
   E_CPONLY(kColorExtensionsToolbarControlsBackground) \
   /* Eyedropper colors. */ \
   E_CPONLY(kColorEyedropperBoundary) \
--- chrome/browser/ui/color/chrome_color_mixer.cc
+++ chrome/browser/ui/color/chrome_color_mixer.cc
@@ -244,6 +244,8 @@ void AddChromeColorMixer(ui::ColorProvider* provider,
   mixer[kColorExtensionMenuPinButtonIcon] = {ui::kColorAccent};
   mixer[kColorExtensionMenuPinButtonIconDisabled] = ui::SetAlpha(
       kColorExtensionMenuPinButtonIcon, gfx::kDisabledControlAlpha);
+  mixer[kColorExtensionsMenuHighlightedBackground] = {
+      kColorToolbarBackgroundSubtleEmphasis};
   mixer[kColorExtensionsToolbarControlsBackground] = {
       kColorToolbarBackgroundSubtleEmphasis};
   mixer[kColorEyedropperBoundary] = {SK_ColorDKGRAY};
