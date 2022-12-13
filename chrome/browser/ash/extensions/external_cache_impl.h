a813b95410f0adb1d68c5eb709fd56630f486442
Yi Xie <yixie@chromium.org>
2022-12-13 06:56:42 +0000

    Reduce extension download retry backoff while Kiosk is launching
    When there is a communication issue with CWS extension downloader will
    retry with exponentially growing backoff, which blocks Chrome OS Kiosk
    launch flow to maximum about 30 minutes (progressively longer backoff
    periods of up to 10 attempts, with the maximum backoff between attempts
    of 10 minutes). We'd like to set a cap on the backoff duration so that
    the downloader retries more frequently and fails faster while Kiosk
    is launching.
    
    Bug: b:231914992
    Test: unit_tests and manual
    Change-Id: Idf56b6fcf651c3aa2da0fee1aec13cae069ca0dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4039913
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Yi Xie <yixie@chromium.org>
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082326}

--- chrome/browser/ash/extensions/external_cache_impl.h
+++ chrome/browser/ash/extensions/external_cache_impl.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "extensions/browser/updater/extension_downloader_delegate.h"
 #include "extensions/common/extension_id.h"
+#include "net/base/backoff_entry.h"
 
 namespace extensions {
 class ExtensionDownloader;
@@ -84,6 +85,8 @@ class ExternalCacheImpl : public ExternalCache,
                             const base::FilePath& crx_file_path,
                             const std::string& version,
                             PutExternalExtensionCallback callback) override;
+  void SetBackoffPolicy(
+      absl::optional<net::BackoffEntry::Policy> backoff_policy) override;
 
   // Implementation of content::NotificationObserver:
   void Observe(int type,
@@ -170,6 +173,9 @@ class ExternalCacheImpl : public ExternalCache,
 
   // Used to download the extensions and to check for updates.
   std::unique_ptr<extensions::ExtensionDownloader> downloader_;
+
+  // Backoff policy of extension downloader.
+  absl::optional<net::BackoffEntry::Policy> backoff_policy_;
 
   // Observes failures to install CRX files.
   content::NotificationRegistrar notification_registrar_;
--- extensions/browser/updater/request_queue.h
+++ extensions/browser/updater/request_queue.h
@@ -52,7 +52,7 @@ class RequestQueue {
 
   class iterator;
 
-  RequestQueue(const net::BackoffEntry::Policy* backoff_policy,
+  RequestQueue(net::BackoffEntry::Policy backoff_policy,
                const base::RepeatingClosure& start_request_callback);
   ~RequestQueue();
 
@@ -101,7 +101,7 @@ class RequestQueue {
       const base::RepeatingCallback<bool(const T&)> condition);
 
   // Change the backoff policy used by the queue.
-  void set_backoff_policy(const net::BackoffEntry::Policy* backoff_policy);
+  void set_backoff_policy(net::BackoffEntry::Policy backoff_policy);
 
  private:
   // Compares the release time of two pending requests.
@@ -111,7 +111,7 @@ class RequestQueue {
   void PushImpl(Request request);
 
   // The backoff policy used to determine backoff delays.
-  raw_ptr<const net::BackoffEntry::Policy> backoff_policy_;
+  net::BackoffEntry::Policy backoff_policy_;
 
   // Callback to call when a new request has become the active request.
   base::RepeatingClosure start_request_callback_;
