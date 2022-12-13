c3caab8db7d990a04a8c1f6b9446a68f39713b80
Matt Menke <mmenke@chromium.org>
2022-12-13 18:08:32 +0000

    Migrate components/cdm/browser/ to ScopedDictPrefUpdate
    It was using DictionaryPrefUpdate, which is deprecated.
    
    Bug: 1362719
    Change-Id: I29bd00e63a02c7d98cb0e9d9d531cd890c1b1842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098548
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082547}

--- components/cdm/browser/media_drm_storage_impl.cc
+++ components/cdm/browser/media_drm_storage_impl.cc
@@ -332,14 +332,14 @@ void ClearSessionDataForTimePeriod(base::Value::Dict& sessions_dict,
 // 2. Removes the origin data if all of the sessions are removed.
 // 3. Returns a list of origin IDs to unprovision.
 std::vector<base::UnguessableToken> ClearMatchingLicenseData(
-    base::Value* storage_dict,
+    base::Value::Dict& storage_dict,
     base::Time start,
     base::Time end,
     const base::RepeatingCallback<bool(const GURL&)>& filter) {
   std::vector<std::string> origins_to_delete;
   std::vector<base::UnguessableToken> origin_ids_to_unprovision;
 
-  for (const auto key_value : storage_dict->DictItems()) {
+  for (const auto key_value : storage_dict) {
     const std::string& origin_str = key_value.first;
 
     if (filter && !filter.Run(GURL(origin_str)))
@@ -384,7 +384,7 @@ std::vector<base::UnguessableToken> ClearMatchingLicenseData(
 
   // Remove origin data.
   for (const auto& origin_str : origins_to_delete)
-    storage_dict->RemoveKey(origin_str);
+    storage_dict.Remove(origin_str);
 
   return origin_ids_to_unprovision;
 }
@@ -548,8 +548,8 @@ class InitializationSerializer {
 
     // Save the origin ID in the preference as long as it is not null.
     if (origin_id) {
-      DictionaryPrefUpdate update(pref_service, prefs::kMediaDrmStorage);
-      CreateOriginDictAndReturnSessionsDict(update->GetDict(), origin,
+      ScopedDictPrefUpdate update(pref_service, prefs::kMediaDrmStorage);
+      CreateOriginDictAndReturnSessionsDict(update.Get(), origin,
                                             origin_id.value());
     }
 
@@ -663,7 +663,7 @@ void MediaDrmStorageImpl::ClearMatchingLicenses(
     base::OnceClosure complete_cb) {
   DVLOG(1) << __func__ << ": Clear licenses [" << start << ", " << end << "]";
 
-  DictionaryPrefUpdate update(pref_service, prefs::kMediaDrmStorage);
+  ScopedDictPrefUpdate update(pref_service, prefs::kMediaDrmStorage);
 
   std::vector<base::UnguessableToken> no_license_origin_ids =
       ClearMatchingLicenseData(update.Get(), start, end, filter);
@@ -785,13 +785,12 @@ void MediaDrmStorageImpl::OnProvisioned(OnProvisionedCallback callback) {
     return;
   }
 
-  DictionaryPrefUpdate update(pref_service_, prefs::kMediaDrmStorage);
-  base::Value::Dict& storage_dict = update->GetDict();
+  ScopedDictPrefUpdate update(pref_service_, prefs::kMediaDrmStorage);
 
   // Update origin dict once origin provisioning completes. There may be
   // orphaned session info from a previous provisioning. Clear them by
   // recreating the dicts.
-  CreateOriginDictAndReturnSessionsDict(storage_dict, origin(),
+  CreateOriginDictAndReturnSessionsDict(update.Get(), origin(),
                                         origin_id_.value());
   std::move(callback).Run(true);
 }
@@ -816,8 +815,8 @@ void MediaDrmStorageImpl::SavePersistentSession(
     return;
   }
 
-  DictionaryPrefUpdate update(pref_service_, prefs::kMediaDrmStorage);
-  base::Value::Dict& storage_dict = update->GetDict();
+  ScopedDictPrefUpdate update(pref_service_, prefs::kMediaDrmStorage);
+  base::Value::Dict& storage_dict = update.Get();
 
   base::Value::Dict* sessions_dict =
       GetSessionsDictFromStorageDict(storage_dict, origin().Serialize());
@@ -913,10 +912,10 @@ void MediaDrmStorageImpl::RemovePersistentSession(
     return;
   }
 
-  DictionaryPrefUpdate update(pref_service_, prefs::kMediaDrmStorage);
+  ScopedDictPrefUpdate update(pref_service_, prefs::kMediaDrmStorage);
 
   base::Value::Dict* sessions_dict =
-      GetSessionsDictFromStorageDict(update->GetDict(), origin().Serialize());
+      GetSessionsDictFromStorageDict(update.Get(), origin().Serialize());
 
   if (!sessions_dict) {
     std::move(callback).Run(true);
