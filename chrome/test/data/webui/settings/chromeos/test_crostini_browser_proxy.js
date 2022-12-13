382af95cfdeac400f3e9c9f59c22586ae7d6334a
Nicholas Verne <nverne@chromium.org>
2022-12-13 06:37:48 +0000

    crostini: Add per-container control over microphone sharing in Settings.
    The extra containers settings page is now re-ordered by vm. Each
    container has a details section, including a "Share microphone"
    toggle on its own row. The container app badge color selector also has
    its own row now, with a description of what it's for (sadly lacking
    previously). The container IP address row is present in the details only
    when the container is running.
    
    Still to come in a later CL: appropriate shutdown/restart behavior for
    non-default VMs if the global "Linux microphone" toggle is changed.
    
    Screenshots are viewable at
    https://storage.cloud.google.com/chromium-ranslation-screenshots/<sha1>
    where you get the sha1 inside the .sha1 file.
    
    Bug: b:234662029
    Change-Id: Ib2504525c7be7d250dd5987f429aaa9b824ba54c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074569
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082321}

--- chrome/test/data/webui/settings/chromeos/test_crostini_browser_proxy.js
+++ chrome/test/data/webui/settings/chromeos/test_crostini_browser_proxy.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
-
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
 /** @implements {CrostiniBrowserProxy} */
@@ -39,12 +38,18 @@ export class TestCrostiniBrowserProxy extends TestBrowserProxy {
       'setContainerBadgeColor',
       'stopContainer',
       'requestCrostiniExportImportOperationStatus',
+      'openContainerFileSelector',
+      'requestSharedVmDevices',
+      'isVmDeviceShared',
+      'setVmDeviceShared',
     ]);
     this.crostiniMicSharingEnabled = false;
     this.crostiniIsRunning = true;
     this.methodCalls_ = {};
     this.portOperationSuccess = true;
     this.containerInfo = [];
+    this.selectedContainerFileName = '';
+    this.sharedVmDevices = [];
   }
 
   getNewPromiseFor(name) {
@@ -233,5 +238,29 @@ export class TestCrostiniBrowserProxy extends TestBrowserProxy {
   /** @override */
   stopContainer(containerId) {
     this.methodCalled('stopContainer');
+  }
+
+  /** @override */
+  openContainerFileSelector() {
+    this.methodCalled('openContainerFileSelector');
+    return Promise.resolve(this.selectedContainerFileName);
+  }
+
+  /** @override */
+  requestSharedVmDevices() {
+    this.methodCalled('requestSharedVmDevices');
+    webUIListenerCallback('crostini-shared-vmdevices', this.sharedVmDevices);
+  }
+
+  /** @override */
+  isVmDeviceShared(id, device) {
+    this.methodCalled('isVmDeviceShared', id, device);
+    return this.getNewPromiseFor('isVmDeviceShared');
+  }
+
+  /** @override */
+  setVmDeviceShared(id, device, shared) {
+    this.methodCalled('setVmDeviceShared', id, device, shared);
+    return this.getNewPromiseFor('setVmDeviceShared');
   }
 }
