3117d83d8a2fa650c150b580a2d125105463f87d
Adam Langley <agl@chromium.org>
2022-12-13 18:13:37 +0000

    webauthn: enable normal caBLE behaviour for create() with rk != discouraged.
    This change continues the work of replacing caBLEv1 by enabling normal
    behaviour when creating a credential where residentKey is preferred or required.
    
    Change-Id: I5487f47d80cee55f5e675d9b2cde74a5082e7f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4099104
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082553}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
@@ -208,12 +208,14 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, CableConfiguration) {
     const char* origin;
     std::vector<device::CableDiscoveryData> extensions;
     device::CableRequestType request_type;
+    absl::optional<device::ResidentKeyRequirement> resident_key_requirement;
     Result expected_result;
   } kTests[] = {
       {
           "https://example.com",
           {},
           device::CableRequestType::kGetAssertion,
+          absl::nullopt,
           Result::k3rdParty,
       },
       {
@@ -221,6 +223,7 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, CableConfiguration) {
           "https://example.com",
           {v1_extension},
           device::CableRequestType::kGetAssertion,
+          absl::nullopt,
           Result::k3rdParty,
       },
       {
@@ -228,6 +231,7 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, CableConfiguration) {
           "https://example.com",
           {v2_extension},
           device::CableRequestType::kGetAssertion,
+          absl::nullopt,
           Result::k3rdParty,
       },
       {
@@ -236,25 +240,45 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, CableConfiguration) {
           "https://accounts.google.com",
           {},
           device::CableRequestType::kGetAssertion,
+          absl::nullopt,
           Result::k3rdParty,
       },
       {
-          // ... but not for registration.
+          // ... but not for non-discoverable registration.
           "https://accounts.google.com",
           {},
           device::CableRequestType::kMakeCredential,
+          device::ResidentKeyRequirement::kDiscouraged,
           Result::kNone,
+      },
+      {
+          // ... but yes for rk=preferred
+          "https://accounts.google.com",
+          {},
+          device::CableRequestType::kMakeCredential,
+          device::ResidentKeyRequirement::kPreferred,
+          Result::k3rdParty,
+      },
+      {
+          // ... or rk=required.
+          "https://accounts.google.com",
+          {},
+          device::CableRequestType::kDiscoverableMakeCredential,
+          device::ResidentKeyRequirement::kRequired,
+          Result::k3rdParty,
       },
       {
           "https://accounts.google.com",
           {v1_extension},
           device::CableRequestType::kGetAssertion,
+          absl::nullopt,
           NONE_ON_LINUX(Result::kV1),
       },
       {
           "https://accounts.google.com",
           {v2_extension},
           device::CableRequestType::kGetAssertion,
+          absl::nullopt,
           Result::kServerLink,
       },
   };
@@ -269,8 +293,8 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, CableConfiguration) {
     delegate.SetRelyingPartyId(/*rp_id=*/"example.com");
     delegate.SetPassEmptyUsbDeviceManagerForTesting(true);
     delegate.ConfigureCable(url::Origin::Create(GURL(test.origin)),
-                            test.request_type, test.extensions,
-                            &discovery_factory);
+                            test.request_type, test.resident_key_requirement,
+                            test.extensions, &discovery_factory);
 
     switch (test.expected_result) {
       case Result::kNone:
--- content/public/browser/authenticator_request_client_delegate.cc
+++ content/public/browser/authenticator_request_client_delegate.cc
@@ -166,6 +166,7 @@ void AuthenticatorRequestClientDelegate::ShouldReturnAttestation(
 void AuthenticatorRequestClientDelegate::ConfigureCable(
     const url::Origin& origin,
     device::CableRequestType request_type,
+    absl::optional<device::ResidentKeyRequirement> resident_key_requirement,
     base::span<const device::CableDiscoveryData> pairings_from_extension,
     device::FidoDiscoveryFactory* fido_discovery_factory) {}
 
