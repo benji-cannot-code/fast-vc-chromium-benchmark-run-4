af720a5ba7d7ae7cc97d979080108d2b7ceddeed
Avi Drissman <avi@chromium.org>
2022-12-14 22:15:45 +0000

    Switch to modern pasteboard types
    This does a sweep of code, and converts types when they are drop-in
    replaceable.
    
    Bug: 861775
    Change-Id: I7c4f1eaacca689e1c27451a07ef2f4636a4b1630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108431
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083373}

--- ui/base/dragdrop/os_exchange_data_provider_mac.mm
+++ ui/base/dragdrop/os_exchange_data_provider_mac.mm
@@ -365,10 +365,9 @@ NSArray<NSDraggingItem*>* OSExchangeDataProviderMac::GetDraggingItems() const {
 // static
 NSArray* OSExchangeDataProviderMac::SupportedPasteboardTypes() {
   return @[
-    kUTTypeChromiumWebCustomData, kUTTypeWebKitWebURLsWithTitles,
-    NSURLPboardType, NSFilenamesPboardType, kUTTypeChromiumInitiatedDrag,
-    NSStringPboardType, NSHTMLPboardType, NSRTFPboardType,
-    NSFilenamesPboardType, kUTTypeChromiumWebCustomData, NSPasteboardTypeString
+    kUTTypeChromiumInitiatedDrag, kUTTypeChromiumWebCustomData,
+    kUTTypeWebKitWebURLsWithTitles, NSFilenamesPboardType, NSPasteboardTypeHTML,
+    NSPasteboardTypeRTF, NSPasteboardTypeString, NSPasteboardTypeURL
   ];
 }
 
