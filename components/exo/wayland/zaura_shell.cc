923ac47c3d14451e569f34c065db81ac18ed8447
Eriko Kurimoto <elkurin@chromium.org>
2022-12-14 09:05:59 +0000

    nit: Remove tooltip_manager.h include
    No longer needed.
    Should be removed in crrev.com/c/4095959.
    
    Change-Id: Id78deca5f262440f757962826bd6ae01d1671503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104645
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082982}

--- components/exo/wayland/zaura_shell.cc
+++ components/exo/wayland/zaura_shell.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/manager/display_manager_util.h"
 #include "ui/display/screen.h"
 #include "ui/views/corewm/tooltip_controller.h"
-#include "ui/views/widget/tooltip_manager.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/coordinate_conversion.h"
 #include "ui/wm/public/activation_client.h"
