67f58329bef337eebe0f04fedbb19b9d388fc13d
Alex Turner <alexmt@chromium.org>
2022-12-14 09:21:42 +0000

    Remove uses of base::Value::AsDictionaryValue()
    Migrates these to modern base::Value calls
    
    Bug: 1187061
    Change-Id: Idab28090a5dd28b2fc8001608dbc0963fe9bf2dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4099844
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082990}

--- chrome/browser/component_updater/recovery_component_installer.cc
+++ chrome/browser/component_updater/recovery_component_installer.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "base/strings/string_util.h"
 #include "base/task/thread_pool.h"
+#include "base/values.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/common/chrome_switches.h"
@@ -110,7 +111,7 @@ bool SimulatingElevatedRecovery() {
 }
 
 std::vector<std::string> GetRecoveryInstallArguments(
-    const base::DictionaryValue& manifest,
+    const base::Value::Dict& manifest,
     bool is_deferred_run,
     const base::Version& version) {
   std::vector<std::string> arguments;
@@ -121,12 +122,12 @@ std::vector<std::string> GetRecoveryInstallArguments(
     arguments.push_back("/deferredrun");
 
   if (const std::string* recovery_args =
-          manifest.FindStringKey("x-recovery-args")) {
+          manifest.FindString("x-recovery-args")) {
     if (base::IsStringASCII(*recovery_args))
       arguments.push_back(*recovery_args);
   }
   if (const std::string* recovery_add_version =
-          manifest.FindStringKey("x-recovery-add-version")) {
+          manifest.FindString("x-recovery-add-version")) {
     if (*recovery_add_version == "yes") {
       arguments.push_back("/version");
       arguments.push_back(version.GetString());
@@ -138,7 +139,7 @@ std::vector<std::string> GetRecoveryInstallArguments(
 
 base::CommandLine BuildRecoveryInstallCommandLine(
     const base::FilePath& command,
-    const base::DictionaryValue& manifest,
+    const base::Value::Dict& manifest,
     bool is_deferred_run,
     const base::Version& version) {
   base::CommandLine command_line(command);
@@ -151,11 +152,10 @@ base::CommandLine BuildRecoveryInstallCommandLine(
   return command_line;
 }
 
-std::unique_ptr<base::DictionaryValue> ReadManifest(
-    const base::FilePath& manifest) {
+base::Value::Dict ReadManifest(const base::FilePath& manifest) {
   JSONFileValueDeserializer deserializer(manifest);
   std::string error;
-  return base::DictionaryValue::From(deserializer.Deserialize(NULL, &error));
+  return deserializer.Deserialize(NULL, &error)->TakeDict();
 }
 
 void WaitForElevatedInstallToComplete(base::Process process) {
@@ -179,12 +179,12 @@ void DoElevatedInstallRecoveryComponent(const base::FilePath& path) {
   if (!base::PathExists(main_file) || !base::PathExists(manifest_file))
     return;
 
-  std::unique_ptr<base::DictionaryValue> manifest(ReadManifest(manifest_file));
-  const std::string* name = manifest->FindStringKey("name");
+  base::Value::Dict manifest(ReadManifest(manifest_file));
+  const std::string* name = manifest.FindString("name");
   if (!name || *name != kRecoveryManifestName)
     return;
   std::string proposed_version;
-  if (const std::string* ptr = manifest->FindStringKey("version")) {
+  if (const std::string* ptr = manifest.FindString("version")) {
     if (base::IsStringASCII(*ptr))
       proposed_version = *ptr;
   }
@@ -195,7 +195,7 @@ void DoElevatedInstallRecoveryComponent(const base::FilePath& path) {
   const bool is_deferred_run = true;
 #if BUILDFLAG(IS_WIN)
   const auto cmdline = BuildRecoveryInstallCommandLine(
-      main_file, *manifest, is_deferred_run, version);
+      main_file, manifest, is_deferred_run, version);
 
   RecordRecoveryComponentUMAEvent(RCE_RUNNING_ELEVATED);
 
@@ -211,8 +211,8 @@ void DoElevatedInstallRecoveryComponent(const base::FilePath& path) {
     return;
   }
 
-  const auto arguments = GetRecoveryInstallArguments(
-      *manifest, is_deferred_run, version);
+  const auto arguments =
+      GetRecoveryInstallArguments(manifest, is_deferred_run, version);
   // Convert the arguments memory layout to the format required by
   // ExecuteWithPrivilegesAndGetPID(): an array of string pointers
   // that ends with a null pointer.
@@ -470,8 +470,7 @@ bool RecoveryComponentInstaller::DoInstall(
   // Run the recovery component.
   const bool is_deferred_run = false;
   const auto cmdline = BuildRecoveryInstallCommandLine(
-      main_file, base::Value::AsDictionaryValue(manifest), is_deferred_run,
-      current_version_);
+      main_file, manifest.GetDict(), is_deferred_run, current_version_);
 
   if (!RunInstallCommand(cmdline, path)) {
     return false;
