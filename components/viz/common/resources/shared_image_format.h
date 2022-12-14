aa1544ade72f25c34fbf8988b78ac3ae10a2546f
Saifuddin Hitawala <hitawala@chromium.org>
2022-12-14 02:09:07 +0000

    Multiplanar: Clean up some ResourceFormat util usages
    Some of the ResourceFormat util functions usages can be removed as
    they're from within shared image system. This change removes them and
    moves the Dawn util functions to SIFormat utils.
    
    Bug: 1378707
    Change-Id: I5284ac412576e1fc02fba845f0eded7173551e43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4096785
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082882}

--- components/viz/common/resources/shared_image_format.h
+++ components/viz/common/resources/shared_image_format.h
@@ -62,6 +62,8 @@ class SharedImageFormat {
   };
 
   static const SharedImageFormat kRGBA_8888;
+  static const SharedImageFormat kBGRA_8888;
+  static const SharedImageFormat kRGBA_F16;
 
   SharedImageFormat() = default;
   static constexpr SharedImageFormat SinglePlane(
@@ -195,6 +197,10 @@ class SharedImageFormat {
 
 constexpr SharedImageFormat SharedImageFormat::kRGBA_8888 =
     SharedImageFormat::SinglePlane(ResourceFormat::RGBA_8888);
+constexpr SharedImageFormat SharedImageFormat::kBGRA_8888 =
+    SharedImageFormat::SinglePlane(ResourceFormat::BGRA_8888);
+constexpr SharedImageFormat SharedImageFormat::kRGBA_F16 =
+    SharedImageFormat::SinglePlane(ResourceFormat::RGBA_F16);
 
 }  // namespace viz
 
--- gpu/command_buffer/service/shared_image/external_vk_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/external_vk_image_backing_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/gpu/vulkan_context_provider.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "gpu/command_buffer/service/shared_image/external_vk_image_backing.h"
+#include "gpu/command_buffer/service/shared_image/shared_image_format_utils.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_representation.h"
 #include "gpu/vulkan/vulkan_command_buffer.h"
 #include "gpu/vulkan/vulkan_command_pool.h"
@@ -58,10 +59,11 @@ base::flat_map<VkFormat, VkImageUsageFlags> CreateImageUsageCache(
   base::flat_map<VkFormat, VkImageUsageFlags> image_usage_cache;
 
   for (int i = 0; i <= static_cast<int>(viz::RESOURCE_FORMAT_MAX); ++i) {
-    viz::ResourceFormat format = static_cast<viz::ResourceFormat>(i);
-    if (!viz::HasVkFormat(format))
+    viz::SharedImageFormat format = viz::SharedImageFormat::SinglePlane(
+        static_cast<viz::ResourceFormat>(i));
+    if (!HasVkFormat(format))
       continue;
-    VkFormat vk_format = viz::ToVkFormat(format);
+    VkFormat vk_format = ToVkFormat(format);
     DCHECK_NE(vk_format, VK_FORMAT_UNDEFINED);
     VkFormatProperties format_props = {};
     vkGetPhysicalDeviceFormatProperties(vk_physical_device, vk_format,
