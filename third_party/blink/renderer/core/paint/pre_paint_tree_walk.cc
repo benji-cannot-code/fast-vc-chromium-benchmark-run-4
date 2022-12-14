50af3e5e8a734a89a8d1b2426d1ae2481f894883
Philip Rogers <pdr@chromium.org>
2022-12-14 23:02:33 +0000

    Revert "Only invalidate GeometryMapper cache if paint properties change"
    This reverts commit a13c78c6ba6d2065e9a6b80bb3b10aaafb4e9800.
    
    Reason for revert: https://crbug.com/1400638
    
    Original change's description:
    > Only invalidate GeometryMapper cache if paint properties change
    >
    > The PrePaint tree walk would invalidate the GeometryMapper cache on
    > most updates, but we only need to invalidate the cache if transform or
    > clip paint properties change, and this already occurs through
    > {Transform, Clip}PaintPropertyNode::AddChanged.
    >
    > Change-Id: I5a5d07a3cec1d941b983b7014b72dc90632e3fe0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4095563
    > Commit-Queue: Philip Rogers <pdr@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1081902}
    
    Change-Id: I28c71d46b80d6ef53cd475719492b5e1602dabc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109169
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083413}

--- third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
+++ third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/object_paint_invalidator.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_property_tree_printer.h"
+#include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
@@ -56,10 +57,11 @@ void PrePaintTreeWalk::WalkTree(LocalFrameView& root_frame_view) {
 
   PrePaintTreeWalkContext context;
 
-#if DCHECK_IS_ON()
-  bool needed_tree_builder_context_update =
+  // GeometryMapper depends on paint properties.
+  bool needs_tree_builder_context_update =
       NeedsTreeBuilderContextUpdate(root_frame_view, context);
-#endif
+  if (needs_tree_builder_context_update)
+    GeometryMapper::ClearCache();
 
   VisualViewport& visual_viewport =
       root_frame_view.GetPage()->GetVisualViewport();
@@ -73,7 +75,7 @@ void PrePaintTreeWalk::WalkTree(LocalFrameView& root_frame_view) {
   paint_invalidator_.ProcessPendingDelayedPaintInvalidations();
 
 #if DCHECK_IS_ON()
-  if (needed_tree_builder_context_update && VLOG_IS_ON(1))
+  if (needs_tree_builder_context_update && VLOG_IS_ON(1))
     ShowAllPropertyTrees(root_frame_view);
 #endif
 
