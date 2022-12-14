41c60681cdea5117065da5cc7760ac8c1f47ecf8
Florian Leimgruber <fleimgruber@google.com>
2022-12-14 22:02:18 +0000

    Label inference: Support poor man's placeholder.
    See go/poor-mans-placeholders. This fixes the website linked in the
    design doc and the label inference in Apple's checkout flow.
    
    A new LabelSource::kOverlayingLabel is introduced, which will allow us
    to measure the impact using the existing
    Autofill.LabelInference.InferredLabelSource metric.
    
    Guarded by the finch feature kAutofillSupportPoorMansPlaceholder.
    
    Bug: 1396374
    Change-Id: Ia692df53155ca161315c96a6a326f8fa83d2b345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4079457
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083364}

--- components/autofill/content/renderer/form_autofill_util_browsertest.cc
+++ components/autofill/content/renderer/form_autofill_util_browsertest.cc
@@ -60,6 +60,22 @@ struct AutofillFieldUtilCase {
   const char16_t* expected_label;
 };
 
+// An <input> with a label placed on top of it (usually used as a placeholder
+// replacement).
+const char* kPoorMansPlaceholder = R"(
+  <style>
+    .fixed_position_and_size {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100px;
+      height: 20px;
+    }
+  </style>
+  <input id='target' class=fixed_position_and_size>
+  <span class=fixed_position_and_size>label</span>
+)";
+
 void VerifyButtonTitleCache(const WebFormElement& form_target,
                             const ButtonTitleList& expected_button_titles,
                             const ButtonTitlesCache& actual_cache) {
@@ -173,6 +189,9 @@ TEST_F(FormAutofillUtilsTest, FindChildTextSkipElementTest) {
 }
 
 TEST_F(FormAutofillUtilsTest, InferLabelForElementTest) {
+  base::test::ScopedFeatureList features;
+  features.InitAndEnableFeature(features::kAutofillSupportPoorMansPlaceholder);
+
   static const AutofillFieldUtilCase test_cases[] = {
       {"DIV table test 1", R"(
        <div>
@@ -216,6 +235,7 @@ TEST_F(FormAutofillUtilsTest, InferLabelForElementTest) {
        u""},
       {"Infer from next sibling",
        "<input id='target' type='checkbox'>hello <b>world</b>", u"hello world"},
+      {"Poor man's placeholder", kPoorMansPlaceholder, u"label"},
   };
   for (auto test_case : test_cases) {
     SCOPED_TRACE(test_case.description);
@@ -234,6 +254,9 @@ TEST_F(FormAutofillUtilsTest, InferLabelForElementTest) {
 }
 
 TEST_F(FormAutofillUtilsTest, InferLabelSourceTest) {
+  base::test::ScopedFeatureList features;
+  features.InitAndEnableFeature(features::kAutofillSupportPoorMansPlaceholder);
+
   struct AutofillFieldLabelSourceCase {
     const char* html;
     const FormFieldData::LabelSource label_source;
@@ -264,7 +287,7 @@ TEST_F(FormAutofillUtilsTest, InferLabelSourceTest) {
        FormFieldData::LabelSource::kTdTag},
       {"<dl><dt>label</dt><dd><input id='target'></dd></dl>",
        FormFieldData::LabelSource::kDdTag},
-  };
+      {kPoorMansPlaceholder, FormFieldData::LabelSource::kOverlayingLabel}};
 
   for (auto test_case : test_cases) {
     SCOPED_TRACE(testing::Message() << test_case.label_source);
