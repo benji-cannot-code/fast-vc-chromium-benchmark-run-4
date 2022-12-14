033701cf6a78680c279f4ab0e3a6a288a45ba87a
Petro Akzhygitov <pakzhygitov@google.com>
2022-12-14 16:34:34 +0000

    Added Pinned Tabs overflow menu item.
    – Added Pin/Unpin Tab overflow menu item.
    – Added `Pin Tab` and `Unpin Tab` strings.
    – Added Pin/Unpin placeholder icons.
    
    Note:
    The changes are added behind the flag.
    
    Screenshots:
    – https://drive.google.com/file/d/1K8khuTHPlhfFl14v1aP2upnXhRViMfF0/view
    – https://drive.google.com/file/d/1ZDBOlAjpbVeziqk3vk8eMQgbNEwMN_Kg/view
    
    Bug: b/262503205
    Change-Id: I7255b9cbc132e877aa54e5938cfa866da07734c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047296
    Reviewed-by: Petro Akzhygitov <pakzhygitov@google.com>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083137}

--- ios/chrome/browser/ui/icons/symbol_names.h
+++ ios/chrome/browser/ui/icons/symbol_names.h
@@ -90,6 +90,7 @@ extern NSString* const kMicrophoneSymbol;
 extern NSString* const kEllipsisCircleFillSymbol;
 extern NSString* const kPinSymbol;
 extern NSString* const kPinFillSymbol;
+extern NSString* const kPinSlashSymbol;
 extern NSString* const kSettingsSymbol;
 extern NSString* const kSettingsFilledSymbol;
 extern NSString* const kShareSymbol;
--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_swift.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_constants.h"
 #import "ios/chrome/browser/ui/reading_list/reading_list_utils.h"
+#import "ios/chrome/browser/ui/tab_switcher/pinned_tabs/features.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/whats_new/whats_new_util.h"
 #import "ios/chrome/browser/url/chrome_url_constants.h"
@@ -226,6 +227,9 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
 @property(nonatomic, strong) OverflowMenuAction* openTabAction;
 @property(nonatomic, strong) OverflowMenuAction* openIncognitoTabAction;
 @property(nonatomic, strong) OverflowMenuAction* openNewWindowAction;
+
+@property(nonatomic, strong) OverflowMenuAction* pinTabAction;
+@property(nonatomic, strong) OverflowMenuAction* unpinTabAction;
 
 @property(nonatomic, strong) OverflowMenuAction* clearBrowsingDataAction;
 @property(nonatomic, strong) OverflowMenuAction* followAction;
@@ -684,6 +688,18 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
           [weakSelf openNewWindow];
         });
 
+    self.pinTabAction = CreateOverflowMenuAction(
+        IDS_IOS_TOOLS_MENU_PIN_TAB, kPinSymbol,
+        /*systemSymbol=*/YES, /*monochromeSymbol=*/NO, kToolsMenuPinTabId, ^{
+          [weakSelf pinTab];
+        });
+
+    self.unpinTabAction = CreateOverflowMenuAction(
+        IDS_IOS_TOOLS_MENU_UNPIN_TAB, kPinSlashSymbol,
+        /*systemSymbol=*/YES, /*monochromeSymbol=*/NO, kToolsMenuUnpinTabId, ^{
+          [weakSelf unpinTab];
+        });
+
     self.clearBrowsingDataAction = CreateOverflowMenuAction(
         IDS_IOS_TOOLS_MENU_CLEAR_BROWSING_DATA, kTrashSymbol,
         /*systemSymbol=*/YES, /*monochromeSymbol=*/NO,
@@ -808,6 +824,18 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
         IDS_IOS_TOOLS_MENU_NEW_WINDOW, @"overflow_menu_action_new_window",
         kToolsMenuNewWindowId, ^{
           [weakSelf openNewWindow];
+        });
+
+    self.pinTabAction = CreateOverflowMenuAction(
+        IDS_IOS_TOOLS_MENU_PIN_TAB, @"overflow_menu_action_pin_tab",
+        kToolsMenuPinTabId, ^{
+          [weakSelf pinTab];
+        });
+
+    self.unpinTabAction = CreateOverflowMenuAction(
+        IDS_IOS_TOOLS_MENU_UNPIN_TAB, @"overflow_menu_action_unpin_tab",
+        kToolsMenuUnpinTabId, ^{
+          [weakSelf unpinTab];
         });
 
     self.clearBrowsingDataAction =
@@ -1091,6 +1119,11 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
     [appActions addObject:self.openNewWindowAction];
   }
 
+  if (IsPinnedTabsEnabled()) {
+    [appActions addObject:([self isTabPinned] ? self.unpinTabAction
+                                              : self.pinTabAction)];
+  }
+
   self.appActionsGroup.actions = appActions;
 
   BOOL pageIsBookmarked =
@@ -1172,7 +1205,11 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
     self.helpActionsGroup.footer = nil;
   }
 
-  // Enable/disable items based on page state.
+  if (IsPinnedTabsEnabled()) {
+    // Enable/disable items based on page state.
+    self.pinTabAction.enabled = [self isCurrentURLWebURL];
+    self.unpinTabAction.enabled = [self isCurrentURLWebURL];
+  }
 
   // The "Add to Reading List" functionality requires JavaScript execution,
   // which is paused while overlays are displayed over the web content area.
@@ -1342,6 +1379,26 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
                       }];
   action.enabled = NO;
   return action;
+}
+
+- (BOOL)isTabPinned {
+  DCHECK(self.webState);
+  DCHECK(self.webStateList);
+
+  int webStateIndex = self.webStateList->GetIndexOfWebState(self.webState);
+  return self.webStateList->IsWebStatePinnedAt(webStateIndex);
+}
+
+- (void)setTabPinned:(BOOL)pinned {
+  web::WebState* webState = self.webState;
+  WebStateList* webStateList = self.webStateList;
+
+  if (!webState || !webStateList) {
+    return;
+  }
+
+  int webStateIndex = webStateList->GetIndexOfWebState(webState);
+  webStateList->SetWebStatePinnedAt(webStateIndex, pinned);
 }
 
 #pragma mark - CRWWebStateObserver
@@ -1552,6 +1609,18 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
   [self.dispatcher
       openNewWindowWithActivity:ActivityToLoadURL(WindowActivityToolsOrigin,
                                                   GURL(kChromeUINewTabURL))];
+}
+
+// Dismisses the menu and pins the tab.
+- (void)pinTab {
+  [self setTabPinned:YES];
+  [self.popupMenuCommandsHandler dismissPopupMenuAnimated:YES];
+}
+
+// Dismisses the menu and unpins the tab.
+- (void)unpinTab {
+  [self setTabPinned:NO];
+  [self.popupMenuCommandsHandler dismissPopupMenuAnimated:YES];
 }
 
 // Dismisses the menu and opens the Clear Browsing Data screen.
