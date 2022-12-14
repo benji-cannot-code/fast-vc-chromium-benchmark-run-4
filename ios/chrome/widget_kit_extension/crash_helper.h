d35306866aefaa96860bcf7f2d50b1cc1dc94c6e
Justin Cohen <justincohen@google.com>
2022-12-14 14:02:33 +0000

    ios: Add crashpad to widget extension in swift.
    Change-Id: Icd78d6b82ce20811085fae235c949c5f8a632200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2829090
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083063}

---
+++ ios/chrome/widget_kit_extension/crash_helper.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_WIDGET_KIT_EXTENSION_CRASH_HELPER_H_
+#define IOS_CHROME_WIDGET_KIT_EXTENSION_CRASH_HELPER_H_
+
+#import <Foundation/Foundation.h>
+
+@interface CrashHelper : NSObject
++ (void)configure;
+@end
+
+#endif  // IOS_CHROME_WIDGET_KIT_EXTENSION_CRASH_HELPER_H_
--- ios/chrome/widget_kit_extension/main.swift
+++ ios/chrome/widget_kit_extension/main.swift
@@ -37,6 +37,9 @@ struct SimpleEntry: TimelineEntry {
 
 @main
 struct ChromeWidgets: WidgetBundle {
+  init() {
+    CrashHelper.configure()
+  }
   @WidgetBundleBuilder
   var body: some Widget {
     QuickActionsWidget()
