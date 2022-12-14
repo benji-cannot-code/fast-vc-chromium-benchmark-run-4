b7c48a63447e1da771b681d98c53f9e0dcc639e4
Chu-Hsuan Yang <chuhsuan@chromium.org>
2022-12-14 06:48:06 +0000

    CCA: Prevent toasts from overlaying wrong views
    Previously, we always appended custom effect elements to `document.body`
    so the toast of PTZ feature would potentially cover the settings view.
    
    This CL Adds `parent` parameter to customize the parent to append the
    custom effect elements to. This CL also removes the obsolete focus ring
    CSS which should be removed in CL:3666398.
    
    Bug: b:261386379
    Test: Trigger PTZ toast and open settings view.
    Change-Id: Ie82e00aa7c72ff813261913673897cfbc00df92c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4099447
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082947}

--- ash/webui/camera_app_ui/resources/js/main.ts
+++ ash/webui/camera_app_ui/resources/js/main.ts
@@ -226,7 +226,7 @@ export class App {
           if (ready || !state.get(Mode.SCAN) || hasShownDocIndicator) {
             return;
           }
-          customEffect.showDownloadingDocScanIndicator();
+          customEffect.showDownloadingDocScanIndicator(this.cameraView.root);
           hasShownDocIndicator = true;
         },
       });
@@ -237,7 +237,7 @@ export class App {
           if (state.get(state.State.ENABLE_PTZ) &&
               !localStorage.getBool(LocalStorageKey.PTZ_TOAST_SHOWN)) {
             localStorage.set(LocalStorageKey.PTZ_TOAST_SHOWN, true);
-            customEffect.showPtzToast();
+            customEffect.showPtzToast(this.cameraView.root);
           }
         },
       });
@@ -252,11 +252,11 @@ export class App {
       // Only show new feature indicator for doc scan if it is ready when
       // starting the app.
       localStorage.set(LocalStorageKey.DOC_MODE_TOAST_SHOWN, true);
-      customEffect.showDocScanAvailableIndicator();
+      customEffect.showDocScanAvailableIndicator(this.cameraView.root);
     } else if (!localStorage.getBool(LocalStorageKey.PTZ_TOAST_SHOWN)) {
       if (state.get(state.State.ENABLE_PTZ)) {
         localStorage.set(LocalStorageKey.PTZ_TOAST_SHOWN, true);
-        customEffect.showPtzToast();
+        customEffect.showPtzToast(this.cameraView.root);
       } else {
         registerPtzToast();
       }
