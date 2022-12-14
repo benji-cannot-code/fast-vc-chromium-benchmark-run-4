139c5b1890bd29040bd05a23a838f3139bc2bb15
Andrew Paseltiner <apaseltiner@chromium.org>
2022-12-14 17:07:33 +0000

    Clean up Attribution Internals TypeScript
    This CL contains no functional changes, but does:
    
    - Use readonly annotations where applicable
    - Use visibility annotations where applicable
    - Use parameter properties in constructors where applicable
    - Make Column::compare optional instead of nullable
    - Require TableModel properties to be specified in its constructor
    - Make TableModel::getRows abstract
    - Remove unused compare parameter from ValueColumn constructor
    - Share common columns between EventLevelReportTableModel and
      AggregatableAttributionReportTableModel
    - Remove explicit casts from ReportTableModel subclasses by making
      ReportTableModel generic across the actual Report subclass
    
    Change-Id: I0b4f287fd65fc29cadbfaff6f3a9e9d30c77e3a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4106909
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083156}

--- content/browser/resources/attribution_reporting/table_model.ts
+++ content/browser/resources/attribution_reporting/table_model.ts
@@ -4,28 +4,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 export interface Column<T> {
-  compare: ((a: T, b: T) => number)|null;
+  readonly compare?: (a: T, b: T) => number;
 
   render(td: HTMLElement, row: T): void;
 
   renderHeader(th: HTMLElement): void;
 }
 
-export class TableModel<T> {
-  cols: Array<Column<T>> = [];
-  emptyRowText: string = '';
-  sortIdx: number = -1;
-  rowsChangedListeners: Set<() => void>;
+export abstract class TableModel<T> {
+  readonly rowsChangedListeners: Set<() => void> = new Set();
 
-  constructor() {
-    this.rowsChangedListeners = new Set();
-  }
+  constructor(
+      public readonly cols: Array<Column<T>>,
+      public sortIdx: number,
+      public readonly emptyRowText: string,
+  ) {}
 
   styleRow(_tr: Element, _data: T) {}
 
-  getRows(): T[] {
-    return [];
-  }
+  abstract getRows(): T[];
 
   notifyRowsChanged() {
     this.rowsChangedListeners.forEach(f => f());
