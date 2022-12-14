3bb7546ab77c7d001d0e674f00c56f5ed279dcff
Avi Drissman <avi@chromium.org>
2022-12-14 06:21:55 +0000

    Fix missing ASWebAuthenticationSession callback
    When macOS calls into an app to request a cancellation of an
    ASWebAuthenticationSession, the app must acknowledge the cancellation.
    Do so.
    
    This was previously committed in https://crrev.com/c/4103840 and then
    reverted. This CL includes a change to the browsertest to match the
    new behavior.
    
    Fixed: 1400714
    Change-Id: I832dc3a1db7e0945c2ae24d1bf6cef0275baebbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104342
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082934}

--- chrome/browser/mac/auth_session_request_browsertest.mm
+++ chrome/browser/mac/auth_session_request_browsertest.mm
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "base/mac/scoped_nsobject.h"
 #import "chrome/browser/app_controller_mac.h"
 
 #include "base/mac/foundation_util.h"
+#import "base/mac/scoped_nsobject.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_test_util.h"
@@ -163,10 +163,14 @@ IN_PROC_BROWSER_TEST_F(AuthSessionBrowserTest, OSCancellation) {
     ui_test_utils::WaitForBrowserToClose(browser);
     EXPECT_EQ(start_browser_count, browser_list->size());
 
-    // Expect there to not have been any callbacks.
+    // Expect there to have been the user cancellation callback.
 
     EXPECT_EQ(nil, session_request.get().callbackURL);
-    EXPECT_EQ(nil, session_request.get().cancellationError);
+    ASSERT_NE(nil, session_request.get().cancellationError);
+    EXPECT_EQ(ASWebAuthenticationSessionErrorDomain,
+              session_request.get().cancellationError.domain);
+    EXPECT_EQ(ASWebAuthenticationSessionErrorCodeCanceledLogin,
+              session_request.get().cancellationError.code);
   } else {
     GTEST_SKIP() << "ASWebAuthenticationSessionRequest is only available on "
                     "macOS 10.15 and higher.";
