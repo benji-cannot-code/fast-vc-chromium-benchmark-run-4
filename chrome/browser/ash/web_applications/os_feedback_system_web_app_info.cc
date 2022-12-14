85982987e160803a47f09f31cd77f25192120dda
Joel Hockey <joelhockey@chromium.org>
2022-12-14 04:53:52 +0000

    Make OSFeedback app searchable in launcher
    Change-Id: Ica4a3b0d26bdf88b4b2f3d82f207594462f76513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4105243
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1082915}

--- chrome/browser/ash/web_applications/os_feedback_system_web_app_info.cc
+++ chrome/browser/ash/web_applications/os_feedback_system_web_app_info.cc
@@ -103,7 +103,7 @@ bool OSFeedbackAppDelegate::ShouldShowInLauncher() const {
 }
 
 bool OSFeedbackAppDelegate::ShouldShowInSearch() const {
-  return ShouldShowInLauncher();
+  return IsUserFeedbackAllowed(profile());
 }
 
 gfx::Rect OSFeedbackAppDelegate::GetDefaultBounds(Browser* browser) const {
