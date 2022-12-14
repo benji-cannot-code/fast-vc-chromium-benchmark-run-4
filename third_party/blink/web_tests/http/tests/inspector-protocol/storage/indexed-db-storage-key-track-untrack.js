c0856d5d2f1f10e733ba2dff4aa806828a4e5d43
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-12-14 16:03:50 +0000

    Reduce timeout time to 5s
    Bug: 1366717
    Change-Id: I2c5980e01ad5d51c1fdbfc8ba965d1460f181a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4106629
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083117}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   setTimeout(() => {
     testRunner.log(protocolMessages);
     testRunner.die('Timeout', errorForLog);
-  }, 9000);
+  }, 5000);
 
   const frameId = (await dp.Page.getResourceTree()).result.frameTree.frame.id;
   errorForLog = new Error();
