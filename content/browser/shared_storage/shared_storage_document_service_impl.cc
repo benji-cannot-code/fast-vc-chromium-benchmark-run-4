d0fb56b5cc0bf90535f6bd906830bf5a80db17bc
cammie <cammie@chromium.org>
2022-12-14 22:38:41 +0000

    Shared Storage: Fix crash in IsSharedStorageAllowed()
    This fixes a crash that is caused when `SharedStorageWorkletHost`
    enters keep-alive and hence `document_service_` has been destroyed.
    
    Bug: 1399835,1400232
    Change-Id: Ic54a0813c30d75e75fe9e826a0aff0a6fad113e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4103728
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083396}

--- content/browser/shared_storage/shared_storage_document_service_impl.cc
+++ content/browser/shared_storage/shared_storage_document_service_impl.cc
@@ -345,8 +345,8 @@ bool SharedStorageDocumentServiceImpl::IsSharedStorageAllowed() {
     return true;
 
   return GetContentClient()->browser()->IsSharedStorageAllowed(
-      &render_frame_host(), main_frame_origin_,
-      render_frame_host().GetLastCommittedOrigin());
+      render_frame_host().GetBrowserContext(), &render_frame_host(),
+      main_frame_origin_, render_frame_host().GetLastCommittedOrigin());
 }
 
 std::string SharedStorageDocumentServiceImpl::SerializeLastCommittedOrigin()
