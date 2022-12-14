0e4af0a30a60511d20a99fb321ccdf01fda792dd
Tom Sepez <tsepez@chromium.org>
2022-12-14 01:53:13 +0000

    Convert TrapKey::aux from void* to uintptr_t.
    Do the same for TrapResultExprImpl::aux.
    
    Per raw_ptr.md, when a pointer may contain something other than
    a valid address, it should be represented as uintptr_t. Currently,
    this is preventing the application of some more stringent checks
    to raw_ptr<> in an unrelated experimental CL.
    
    -- modernize some code while at it.
    
    Change-Id: I542a8419c69f9b04c2419c1cc517e7b726d018ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104106
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082876}

--- sandbox/linux/bpf_dsl/bpf_dsl.cc
+++ sandbox/linux/bpf_dsl/bpf_dsl.cc
@@ -53,7 +53,7 @@ class ReturnResultExprImpl : public internal::ResultExprImpl {
 class TrapResultExprImpl : public internal::ResultExprImpl {
  public:
   TrapResultExprImpl(TrapRegistry::TrapFnc func, const void* arg, bool safe)
-      : func_(func), arg_(arg), safe_(safe) {
+      : func_(func), arg_(reinterpret_cast<uintptr_t>(arg)), safe_(safe) {
     DCHECK(func_);
   }
 
@@ -63,7 +63,7 @@ class TrapResultExprImpl : public internal::ResultExprImpl {
   ~TrapResultExprImpl() override {}
 
   CodeGen::Node Compile(PolicyCompiler* pc) const override {
-    return pc->Trap(func_, arg_, safe_);
+    return pc->Trap(func_, reinterpret_cast<const void*>(arg_), safe_);
   }
 
   bool HasUnsafeTraps() const override { return safe_ == false; }
@@ -72,7 +72,7 @@ class TrapResultExprImpl : public internal::ResultExprImpl {
 
  private:
   TrapRegistry::TrapFnc func_;
-  raw_ptr<const void> arg_;
+  uintptr_t arg_;  // Usually a pointer, but may be a smuggled int.
   bool safe_;
 };
 
