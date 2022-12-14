081434511ea1e17c42547d779ddda9cbb786f45f
manukh <manukh@chromium.org>
2022-12-14 23:53:10 +0000

    [memories][persist-clusters] Persist on query.
    Currently, persistence is done on a timer. E.g. 1 hour after startup,
    and also periodically every 12 hours.
    
    Currently, keyword cache refreshes are done on query, bounded by a timer
    (10s for the short-cache, 2hr for the all-cache). Query occur when the
    HC suggestion or action providers look for a potential search match;
    this only occurs when:
    a) The respective feature is enabled,
    b) There's a new omnibox input,
    c) The nav intent filter passes, and
    d) In the case of the HC provider, the search provider is done.
    
    Initially, we decided to use the timer approach for persistence because:
    1) We assumed waiting for a query wouldn't matter because queries occur
       much more frequently than the 12 hour timer, and
    2) Using queries to trigger updates seems somewhat arbitrary.
    
    However, assumption (1) may be very wrong. I'm still uncertain, because
    it seems so obviously right, but metrics [1] indicate keyword refreshes
    are occurring 6-60 times less often than persistence updates. This is
    very surprising, so it may be that we're misinterpreting the metrics;
    hopefully this CL will help clarify what's really going on.
    
    This CL does 2 things:
    1) Adds a histogram on the time elapsed between update tasks.
    2) Adds a finch param to switch persistence from the timer approach to
       the query approach keywords use.
    
    The hope is to either decrease update requests on par with keyword
    requests, or confirm assumption (1) and rule out this theory. I've made
    a reasonable effort to investigate this locally, but resorting to
    another round of finch experiments seems necessary.
    
    [1]
    docs.google.com/document/d/1JAeY2PEwn-BnvjDr_LbrAGGPM4ltdCWKDSl_gtF-cX0
    
    Bug: 1184879, 1184875, 1171352
    Change-Id: I736d22196e13bcff668b731b2a535e8d0ffccb01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4102743
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083437}

--- components/history_clusters/core/history_clusters_service.h
+++ components/history_clusters/core/history_clusters_service.h
@@ -211,6 +211,11 @@ class HistoryClustersService : public base::SupportsUserData,
   friend class HistoryClustersServiceTestApi;
 
   // Starts a keyword cache refresh, if necessary.
+  // TODO(manukh): `StartKeywordCacheRefresh()` and
+  //  `PopulateClusterKeywordCache()` should be encapsulated into their own task
+  //  to avoid cluttering `HistoryClusterService` with their callbacks. Similar
+  //  to the `HistoryClustersServiceTaskGetMostRecentClusters` and
+  //  `HistoryClustersServiceTaskUpdateClusters` tasks.
   void StartKeywordCacheRefresh();
 
   // This is a callback used for the `QueryClusters()` call from
@@ -281,6 +286,10 @@ class HistoryClustersService : public base::SupportsUserData,
   // Used to invoke `UpdateClusters()` periodically. See
   // `RepeatedlyUpdateClusters()`'s comment.
   base::RepeatingTimer update_clusters_period_timer_;
+
+  // The time of the last `UpdateClusters()` call. Used for logging and to limit
+  // requests when `persist_on_query` is enabled.
+  base::ElapsedTimer update_clusters_timer_;
 
   // A list of observers for this service.
   base::ObserverList<Observer> observers_;
