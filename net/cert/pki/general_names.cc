879586471023221dbb40a006584d38ea4b981a22
Ivan Murashov <ivan.murashov@lge.com>
2022-12-14 19:31:37 +0000

    IWYU: Add <cstring> for memcmp usage in general_names.cc
    Otherwise build fails when building with use_custom_libcxx=false.
    The error example:
    net/cert/pki/general_names.cc:40:7: error:
    use of undeclared identifier 'memcmp'; did you mean 'wmemcmp'?
    
    Bug: 957519
    Change-Id: I4db3751ac802ffdc5393f59a858a60ae1b39eba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4106310
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083263}

--- net/cert/pki/general_names.cc
+++ net/cert/pki/general_names.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cert/pki/general_names.h"
 
+#include <cstring>
+
 #include "net/cert/pki/cert_error_params.h"
 #include "net/cert/pki/cert_errors.h"
 #include "net/cert/pki/string_util.h"
