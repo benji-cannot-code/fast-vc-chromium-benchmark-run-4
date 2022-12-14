7a14bc59d310a597dffbe11172fb0677960259e2
Henrique Nakashima <hnakashima@chromium.org>
2022-12-14 23:52:39 +0000

    [Android] Remove tests for behavior under Nougat in android_webview
    Nougat is now the minimum supported version.
    
    Bug: 1385893
    Change-Id: Iad1c882408ae5bb3cc9a6e6c43d5e1fd131e15d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4107794
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083436}

--- android_webview/javatests/src/org/chromium/android_webview/test/AcceptLanguageTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AcceptLanguageTest.java
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
-import android.os.Build;
 import android.os.LocaleList;
 import android.support.test.InstrumentationRegistry;
 
-import androidx.annotation.RequiresApi;
 import androidx.test.filters.SmallTest;
 
 import org.junit.After;
@@ -22,7 +20,6 @@ import org.junit.runner.RunWith;
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.JSUtils;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.net.test.EmbeddedTestServer;
 
 import java.util.Locale;
@@ -81,20 +78,12 @@ public class AcceptLanguageTest {
 
     private boolean isSingleLocale(String lang, String country) {
         String languageTag = String.format("%s-%s", lang, country);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            // In N+, multiple locales can be set.
-            return languageTag.equals(LocaleList.getDefault().toLanguageTags());
-        } else {
-            return languageTag.equals(Locale.getDefault().toLanguageTag());
-        }
+        // In N+, multiple locales can be set.
+        return languageTag.equals(LocaleList.getDefault().toLanguageTags());
     }
 
     private void setSingleLocale(String lang, String country) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            LocaleList.setDefault(new LocaleList(new Locale(lang, country)));
-        } else {
-            Locale.setDefault(new Locale(lang, country));
-        }
+        LocaleList.setDefault(new LocaleList(new Locale(lang, country)));
     }
 
     private void setLocaleForTesting(String lang, String country) {
@@ -155,8 +144,6 @@ public class AcceptLanguageTest {
      */
     @Test
     @SmallTest
-    @MinAndroidSdkLevel(Build.VERSION_CODES.N)
-    @RequiresApi(Build.VERSION_CODES.N)
     @Feature({"AndroidWebView"})
     public void testAcceptLanguagesWithenUS() throws Throwable {
         LocaleList.setDefault(new LocaleList(new Locale("ko", "KR")));
