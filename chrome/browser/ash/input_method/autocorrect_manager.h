c1bff318b22c77eaffd4079a14934d164755d562
Zac Partridge <zacpartridge@google.com>
2022-12-14 02:08:44 +0000

    [Autocorrect]: Split metrics for launching by default
    The following metrics will be split out:
    InputMethod.Assistive.AutocorrectV2.Actions.PK
    InputMethod.Assistive.AutocorrectV2.Internal.PkState
    InputMethod.Assistive.AutocorrectV2.Latency.Accept
    InputMethod.Assistive.AutocorrectV2.Latency.ExitField
    InputMethod.Assistive.AutocorrectV2.Latency.Reject
    
    Fixed: b:260891163
    Change-Id: I4b7ccb9493b0a10465ec199445d922ecf0351009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074113
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Zac Partridge <zacpartridge@google.com>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082881}

--- chrome/browser/ash/input_method/autocorrect_manager.h
+++ chrome/browser/ash/input_method/autocorrect_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "chrome/browser/ash/input_method/assistive_window_controller.h"
+#include "chrome/browser/ash/input_method/autocorrect_enums.h"
 #include "chrome/browser/ash/input_method/diacritics_insensitive_string_comparator.h"
 #include "chrome/browser/ash/input_method/input_method_engine.h"
 #include "chrome/browser/ash/input_method/suggestion_handler_interface.h"
@@ -193,6 +194,9 @@ class AutocorrectManager {
   void LogRejectionInteractions(AutocorrectActions action);
   void MeasureAndLogAssistiveAutocorrectQualityBreakdown(
       AutocorrectActions action);
+  void LogAssistiveAutocorrectInternalState(
+      AutocorrectInternalStates internal_state);
+  bool AutoCorrectPrefIsPkEnabledByDefault();
 
   void OnTextFieldContextualInfoChanged(const TextFieldContextualInfo& info);
 
