f04e9e0688490602b70cab80bf4aacf8b533712c
Joe Downing <joedow@google.com>
2022-12-14 18:06:23 +0000

    Removing SetLossless{Color|Encode} methods from VideoStream
    These methods were used for setting the lossless color/encode values
    back when we had the Chrome app. They are no longer used as instead we
    provide the lossless color info via the SDP for the website and the
    legacy mobile apps do not toggle these values.
    
    Change-Id: Iabe8257bf995026c72725ea8fdadd30f1bd02b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104702
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083192}

--- remoting/protocol/fake_connection_to_client.cc
+++ remoting/protocol/fake_connection_to_client.cc
@@ -24,10 +24,6 @@ void FakeVideoStream::SetEventTimestampsSource(
 
 void FakeVideoStream::Pause(bool pause) {}
 
-void FakeVideoStream::SetLosslessEncode(bool want_lossless) {}
-
-void FakeVideoStream::SetLosslessColor(bool want_lossless) {}
-
 void FakeVideoStream::SetObserver(Observer* observer) {
   observer_ = observer;
 }
