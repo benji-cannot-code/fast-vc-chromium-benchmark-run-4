f79a26c3750821583bf6c316162cea8e9253137c
seannli <seannli@google.com>
2022-12-14 06:39:47 +0000

    Reland "CCA: Update webcam list while receiving the lid's event."
    This is a reland of commit 48cde1bbf8a5b5c2648dc1e400360c15e6f12ee5
    
    Original change's description:
    > CCA: Update webcam list while receiving the lid's event.
    >
    > Add an observer named PowerManagerClientProxy in CameraHalDelegate.
    > Once it observes the lid's event, NotifyVideoCaptureDevicesChanged
    > is called to update the camera's order.
    >
    > Bug: b:246881918
    > Test: Manually on DUT. Work well on getUserMedia() and Google Meet.
    > Change-Id: I4311f576d71bfdb06e5b979f3c3ec3f71fed9ca9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054487
    > Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    > Reviewed-by: Wei Lee <wtlee@chromium.org>
    > Commit-Queue: Sean Li <seannli@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1079717}
    
    Bug: b:246881918
    Change-Id: I75485f11d8b5a3b55b83f574a098b4cb84aa1e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4091018
    Commit-Queue: Sean Li <seannli@google.com>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082946}

--- media/capture/video/chromeos/camera_hal_delegate_unittest.cc
+++ media/capture/video/chromeos/camera_hal_delegate_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/run_loop.h"
+#include "base/task/thread_pool.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "media/capture/video/chromeos/mock_camera_module.h"
@@ -47,7 +48,9 @@ class CameraHalDelegateTest : public ::testing::Test {
   void SetUp() override {
     VideoCaptureDeviceFactoryChromeOS::SetGpuBufferManager(
         &mock_gpu_memory_buffer_manager_);
-    camera_hal_delegate_ = std::make_unique<CameraHalDelegate>();
+    camera_hal_delegate_ = std::make_unique<CameraHalDelegate>(
+        base::ThreadPool::CreateSingleThreadTaskRunner(
+            {}, base::SingleThreadTaskRunnerThreadMode::DEDICATED));
     if (!camera_hal_delegate_->Init()) {
       LOG(ERROR) << "Failed to initialize CameraHalDelegate";
       camera_hal_delegate_.reset();
