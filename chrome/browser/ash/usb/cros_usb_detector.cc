cda340a509d70f6d157a709d5c5e4f21b93ae33f
Maria Petrisor <mpetrisor@chromium.org>
2022-12-14 10:30:39 +0000

    Add policy to hide notification when USB device is plugged in
    The policy will enable hiding notifications that a USB devices was
    plugged in on ChromeOS.
    
    The policy will keep the current behavior intact with it's default
    value true (show the notifications) and admins can hide the USB
    notifications as desired when the policy is implemented on the admin
    console.
    
    Bug: b:260569436
    Change-Id: I7a0aa9e32dcf6a7154b0db717f114b96cead42aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061233
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Maria Petrisor <mpetrisor@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083009}

--- chrome/browser/ash/usb/cros_usb_detector.cc
+++ chrome/browser/ash/usb/cros_usb_detector.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/arc_util.h"
 #include "ash/constants/ash_features.h"
+#include "ash/constants/ash_pref_names.h"
 #include "ash/constants/notifier_catalogs.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "base/callback_helpers.h"
@@ -543,6 +544,11 @@ void CrosUsbDetector::ConnectToDeviceManager() {
 }
 
 bool CrosUsbDetector::ShouldShowNotification(const UsbDevice& device) {
+  PrefService* prefs = profile()->GetPrefs();
+  if (!prefs->GetBoolean(ash::prefs::kUsbDetectorNotificationEnabled)) {
+    return false;
+  }
+
   if (!crostini::CrostiniFeatures::Get()->IsEnabled(profile()) &&
       !plugin_vm::PluginVmFeatures::Get()->IsEnabled(profile()) &&
       !IsPlayStoreEnabledWithArcVmForProfile(profile())) {
