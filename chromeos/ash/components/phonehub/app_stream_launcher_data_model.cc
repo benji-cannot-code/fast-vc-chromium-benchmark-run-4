1ad55bd3107fe61cc869ea8de1d042674225f838
Abbas Nayebi <nayebi@google.com>
2022-12-14 19:21:49 +0000

    [eche] Removing the sorting of recent apps based on timestamp.
    As the items that come from the phone does not have the timestamp
    data, sorting the apps based on the timestamp will lead into purging the
    app that are used on the phone when the list is sorted.
    After this change, the list of apps are kept in a LIFO fashion.
    
    Test= Locally built chrome and tested like this: Opened app A and app B on the
    phone. Then connected phone hub. At this time we had app A and B at the head of
    the list. Then streamed app A. Now app B is still in the list (before the change
    local build was not behaving this way and after the new change it was fixed.)
    
    Bug: b/262195624
    Change-Id: I6e25902e687321bc8b95b34e3143c8bb30189245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4099258
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Abbas Nayebi <nayebi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083256}

--- chromeos/ash/components/phonehub/app_stream_launcher_data_model.cc
+++ chromeos/ash/components/phonehub/app_stream_launcher_data_model.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/phonehub/app_stream_launcher_data_model.h"
 
+#include "chromeos/ash/components/multidevice/logging/logging.h"
 #include "chromeos/ash/components/phonehub/notification.h"
 
 namespace ash::phonehub {
@@ -41,6 +42,8 @@ void AppStreamLauncherDataModel::ResetState() {
 
 void AppStreamLauncherDataModel::SetAppList(
     const std::vector<Notification::AppMetadata>& streamable_apps) {
+  PA_LOG(INFO) << "App Streaming Launcher data updated with "
+               << streamable_apps.size() << " apps";
   apps_list_ = streamable_apps;
 
   apps_list_sorted_by_name_ = streamable_apps;
