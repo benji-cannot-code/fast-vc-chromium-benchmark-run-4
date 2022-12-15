8a700900edaf0bf4d34a3786ef6ce3c6d69203e3
Cam Bickel <cambickel@google.com>
2022-12-15 20:26:02 +0000

    shortcuts: Add Space, Tab, Backspace, Escape, and Enter to GetKeyDisplay
    This CL adds Space, Tab, Backspace, Escape, and Enter to the
    GetKeyDisplayMap() function.
    
    These keys would normally be capitalized (e.g. "Tab") if returned
    directly from the KeycodeToKeyString function, and we'd like them to be
    lowercase in the app (e.g. "tab"). Furthermore, since the output of
    GetKeyDisplay is shown directly to users, we want more control over
    their display.
    
    Example of frontend display after this CL:
    https://screenshot.googleplex.com/5WJDtynMy6FeABS
    
    This CL adds grd strings for these five keys, and also updates the
    backend's BUILD rule to see these strings. I've also added tests for
    the key_display change.
    
    Bug: b/216049298
    Test: browser_tests --gtest_filter=ShortcutCustomizationApp*
    Change-Id: I6043d86535e90c1b242002e515fedb74cc4fea41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104500
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083950}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/prefs/pref_service.h"
 #include "mojo/public/cpp/bindings/clone_traits.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -60,6 +61,16 @@ const base::flat_map<ui::KeyboardCode, std::u16string>& GetKeyDisplayMap() {
           {ui::KeyboardCode::VKEY_DICTATE, u"ToggleDictation"},
           {ui::KeyboardCode::VKEY_WLAN, u"ToggleWifi"},
           {ui::KeyboardCode::VKEY_EMOJI_PICKER, u"EmojiPicker"},
+          {ui::KeyboardCode::VKEY_SPACE,
+           l10n_util::GetStringUTF16(IDS_SHORTCUT_CUSTOMIZATION_KEY_SPACE)},
+          {ui::KeyboardCode::VKEY_TAB,
+           l10n_util::GetStringUTF16(IDS_SHORTCUT_CUSTOMIZATION_KEY_TAB)},
+          {ui::KeyboardCode::VKEY_ESCAPE,
+           l10n_util::GetStringUTF16(IDS_SHORTCUT_CUSTOMIZATION_KEY_ESCAPE)},
+          {ui::KeyboardCode::VKEY_RETURN,
+           l10n_util::GetStringUTF16(IDS_SHORTCUT_CUSTOMIZATION_KEY_RETURN)},
+          {ui::KeyboardCode::VKEY_BACK,
+           l10n_util::GetStringUTF16(IDS_SHORTCUT_CUSTOMIZATION_KEY_BACKSPACE)},
       }));
   return *key_display_map;
 }
@@ -67,7 +78,7 @@ const base::flat_map<ui::KeyboardCode, std::u16string>& GetKeyDisplayMap() {
 mojom::DefaultAcceleratorPropertiesPtr CreateDefaultAcceleratorProps(
     const ui::Accelerator& accelerator) {
   return mojom::DefaultAcceleratorProperties::New(
-      accelerator, KeycodeToKeyString(accelerator.key_code()));
+      accelerator, shortcut_ui::GetKeyDisplay(accelerator.key_code()));
 }
 
 std::u16string LookupAcceleratorDescription(mojom::AcceleratorSource source,
@@ -430,20 +441,15 @@ AcceleratorConfigurationProvider::CreateConfigurationMap() {
 }
 
 std::u16string GetKeyDisplay(ui::KeyboardCode key_code) {
-  // TODO(cambickel): All key_displays should be lowercase
-  const std::u16string key_display = KeycodeToKeyString(key_code);
-  if (key_display.empty()) {
-    // If key_display is empty and there's an entry for this key_code in our
-    // map, return that entry's value.
-    auto it = GetKeyDisplayMap().find(key_code);
-    if (it != GetKeyDisplayMap().end()) {
-      return it->second;
-    }
-  } else if (key_display == u" ") {
-    // If key_display is a space character, instead return the string "Space".
-    return l10n_util::GetStringUTF16(IDS_KSV_KEY_SPACE);
+  // If there's an entry for this key_code in our
+  // map, return that entry's value.
+  auto it = GetKeyDisplayMap().find(key_code);
+  if (it != GetKeyDisplayMap().end()) {
+    return it->second;
+  } else {
+    // Otherwise, get the key_display from a util function.
+    return KeycodeToKeyString(key_code);
   }
-  return key_display;
 }
 
 }  // namespace shortcut_ui
