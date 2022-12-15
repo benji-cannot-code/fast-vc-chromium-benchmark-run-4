df0d66e7ed47ccabe2aac29d0c973a66bd5ef647
Alexander Surkov <asurkov@igalia.com>
2022-12-15 12:58:49 +0000

    ax_script: support IA2Table::selectedColumns
    IA2Table::selectedColumns is a good method to implement to
    demonstrate the using of ScopedCoMemArray (see crrev.com/c/4048004)
    to store CoTaskMemAllocat'ed arrays, which are used throughout
    IAccessible2 interfaces.
    
    Bug: 1298144
    Change-Id: I90a5b8fa3605c0149cc4947fa827d476a21bdcb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048066
    Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1083692}

--- ui/accessibility/platform/inspect/ax_target_win.cc
+++ ui/accessibility/platform/inspect/ax_target_win.cc
@@ -47,6 +47,17 @@ std::string AXTargetWin::ToString() const {
   if (Is<int>())
     return base::NumberToString(As<int>());
 
+  if (Is<ScopedCoMemArray<LONG>>()) {
+    std::string str;
+    for (LONG value : As<ScopedCoMemArray<LONG>>()) {
+      if (!str.empty()) {
+        str += ", ";
+      }
+      str += base::NumberToString(value);
+    }
+    return '[' + str + ']';
+  }
+
   return "Unsupported";
 }
 
