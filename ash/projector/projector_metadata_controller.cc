c55f4286206c414332fd93c2306a806f01a71ddb
Yilkal <yilkal@chromium.org>
2022-12-15 18:54:05 +0000

    Projector app expects zh_TW or zh_CN
    Bug: b/262622134
    Change-Id: I090cbc454d11e2481390c5a344d0dbf56af37acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109771
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083878}

--- ash/projector/projector_metadata_controller.cc
+++ ash/projector/projector_metadata_controller.cc
@@ -39,6 +39,14 @@ bool SaveFile(const std::string& content, const base::FilePath& path) {
   return base::WriteFile(path, content);
 }
 
+const std::string GetFormattedLangauge(const icu::Locale& locale) {
+  const std::string language = locale.getLanguage();
+  if (language == "zh") {
+    return "zh_TW";
+  }
+  return language;
+}
+
 }  // namespace
 
 ProjectorMetadataController::ProjectorMetadataController() = default;
@@ -47,8 +55,8 @@ ProjectorMetadataController::~ProjectorMetadataController() = default;
 
 void ProjectorMetadataController::OnRecordingStarted() {
   metadata_ = std::make_unique<ProjectorMetadata>();
-  auto locale = icu::Locale::getDefault();
-  metadata_->SetCaptionLanguage(locale.getLanguage());
+  metadata_->SetCaptionLanguage(
+      GetFormattedLangauge(icu::Locale::getDefault()));
 }
 
 void ProjectorMetadataController::RecordTranscription(
