4d1444cc0d6aa1eeb119ae8580e0e6c9c5e48686
William Liu <liuwilliam@chromium.org>
2022-12-15 18:53:35 +0000

    Reland "Integrate ContentPrefetch with Precog logging."
    The cast from NavigationHandle to NavigationRequest causes failures to
    linux-cfi builder when a MockNavigationHandle is passed to
    PreloadingDataImpl::Did{Start|Finish}Navigation. The cast in fact is
    unnecessary.
    
    Also disable PrefetchServiceTest.PrefetchFailedForRedirect for ChromeOS.
    
    The diff is from PS1 to onward.
    
    Tested with the following on content_unittests.
    ```
    dcheck_always_on = true
    is_cfi = true
    is_component_build = false
    use_goma = true
    is_debug = false
    use_cfi_cast = true
    use_cfi_diag = true
    use_cfi_icall = true
    use_thin_lto = true
    ```
    
    This is a reland of commit 12c553c294f4be2e6e350377bc9aa2d7b63446bd
    
    Original change's description:
    > Integrate ContentPrefetch with Precog logging.
    >
    > Bug: 1299059, 1381519
    > Change-Id: Ic20b21eccfb6a8fe006e652eadb31ab4e3803972
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074485
    > Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    > Reviewed-by: Max Curran <curranmax@chromium.org>
    > Commit-Queue: William Liu <liuwilliam@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1083123}
    
    Bug: 1299059, 1381519
    Change-Id: Ie449129e2ffe420e6b2b4068f449066dbe3cc575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108267
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083877}

--- content/browser/preloading/prefetch/prefetch_document_manager.cc
+++ content/browser/preloading/prefetch/prefetch_document_manager.cc
@@ -228,6 +228,7 @@ bool PrefetchDocumentManager::IsPrefetchAttemptFailedOrDiscarded(
 
   switch (container->GetPrefetchStatus()) {
     case PrefetchStatus::kPrefetchSuccessful:
+    case PrefetchStatus::kPrefetchResponseUsed:
       return false;
     case PrefetchStatus::kPrefetchNotEligibleUserHasCookies:
     case PrefetchStatus::kPrefetchNotEligibleUserHasServiceWorker:
@@ -241,7 +242,6 @@ bool PrefetchDocumentManager::IsPrefetchAttemptFailedOrDiscarded(
     case PrefetchStatus::kPrefetchNotEligibleDataSaverEnabled:
     case PrefetchStatus::kPrefetchNotEligibleExistingProxy:
     case PrefetchStatus::kPrefetchUsedNoProbe:
-    case PrefetchStatus::kPrefetchUsedProbeSuccess:
     case PrefetchStatus::kPrefetchNotUsedProbeFailed:
     case PrefetchStatus::kPrefetchNotStarted:
     case PrefetchStatus::kPrefetchNotFinishedInTime:
@@ -266,6 +266,9 @@ bool PrefetchDocumentManager::IsPrefetchAttemptFailedOrDiscarded(
     case PrefetchStatus::kPrefetchIsStaleNSPNotStarted:
     case PrefetchStatus::kPrefetchNotUsedCookiesChanged:
     case PrefetchStatus::kPrefetchFailedRedirectsDisabled:
+    case PrefetchStatus::kPrefetchNotEligibleBrowserContextOffTheRecord:
+    case PrefetchStatus::kPrefetchHeldback:
+    case PrefetchStatus::kPrefetchAllowed:
       return true;
   }
 }
--- content/browser/preloading/prefetch/prefetch_params.cc
+++ content/browser/preloading/prefetch/prefetch_params.cc
@@ -223,4 +223,8 @@ bool PrefetchUseStreamingURLLoader() {
       features::kPrefetchUseContentRefactor, "use_streaming_url_loader", true);
 }
 
+bool IsContentPrefetchHoldback() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      features::kPrefetchUseContentRefactor, "prefetch_holdback", false);
+}
 }  // namespace content
