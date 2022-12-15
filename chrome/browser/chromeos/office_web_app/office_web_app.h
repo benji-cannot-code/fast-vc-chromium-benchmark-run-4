2d865a7cb32db9d0fe4eb7b164ab2c05076ce25d
Alexander Bolodurin <alexbn@chromium.org>
2022-12-15 10:19:59 +0000

    office: move Microsoft 365 installation code
    Move Microsoft 365 installation to //c/b/chromeos/office_web_app to
    share it between Ash and Lacros.
    
    Bug: b:260641203
    Change-Id: I944d63eda96253893b6e466235dfdb5aeafbfefa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4100247
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083652}

---
+++ chrome/browser/chromeos/office_web_app/office_web_app.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_OFFICE_WEB_APP_OFFICE_WEB_APP_H_
+#define CHROME_BROWSER_CHROMEOS_OFFICE_WEB_APP_OFFICE_WEB_APP_H_
+
+#include "base/functional/callback.h"
+
+class Profile;
+
+namespace webapps {
+enum class InstallResultCode;
+}
+
+namespace chromeos {
+
+void InstallMicrosoft365(
+    Profile* profile,
+    base::OnceCallback<void(webapps::InstallResultCode)> callback);
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_OFFICE_WEB_APP_OFFICE_WEB_APP_H_
