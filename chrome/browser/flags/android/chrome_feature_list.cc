1668dd485e1f05d54b99a2c0bdb3394a3153609e
Matt Jones <mdjones@chromium.org>
2022-12-15 22:06:36 +0000

    Remove long-unused DuetTabStripIntegrationAndroid flag
    Bug: 1385901
    Change-Id: I763a4ed76eba3d5859a7f876659b2fd36cc42c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111463
    Auto-Submit: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084009}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -238,7 +238,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kContextualSearchThinWebViewImplementation,
     &kDeferKeepScreenOnDuringGesture,
     &kDirectActions,
-    &kDuetTabStripIntegrationAndroid,
     &kExperimentsForAgsa,
     &kExploreSites,
     &kFocusOmniboxInIncognitoTabIntents,
@@ -735,10 +734,6 @@ BASE_FEATURE(kDownloadAutoResumptionThrottling,
 BASE_FEATURE(kDownloadHomeForExternalApp,
              "DownloadHomeForExternalApp",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kDuetTabStripIntegrationAndroid,
-             "DuetTabStripIntegrationAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExperimentsForAgsa,
              "ExperimentsForAgsa",
