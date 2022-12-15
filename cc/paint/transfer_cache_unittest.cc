6156185af97b4688c482b742bcc1308cb1a99a98
kylechar <kylechar@chromium.org>
2022-12-15 21:06:16 +0000

    Start unifying test ContextProvider
    viz::TestInProcessContextProvider and ui::InProcessContextProvider
    provide very similar functionality and could be merged. Start to unify
    them by adding some missing functionality for observers and thread
    checking in TestInProcessContextProvider. Also use the same context
    creation attributes in both.
    
    Bug: 1292507
    Change-Id: I798c0556a8e0e9bd5c2c3122c74402f76d09650c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4103645
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083967}

--- cc/paint/transfer_cache_unittest.cc
+++ cc/paint/transfer_cache_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/image_transfer_cache_entry.h"
 #include "cc/paint/raw_memory_transfer_cache_entry.h"
 #include "cc/paint/transfer_cache_entry.h"
+#include "components/viz/test/test_gpu_memory_buffer_manager.h"
 #include "components/viz/test/test_gpu_service_holder.h"
 #include "components/viz/test/test_in_process_context_provider.h"
 #include "gpu/command_buffer/client/client_transfer_cache.h"
