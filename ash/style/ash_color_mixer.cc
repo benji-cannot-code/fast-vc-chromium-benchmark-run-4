ab7a43b06e29faa20b5d0ce1916eca70527ca97f
Sean Kau <skau@chromium.org>
2022-12-15 04:04:16 +0000

    Implement harmonized colors
    In the new color system, some colors have a restricted palette instead
    of being "fully" dynamic. Introduce palettes for red, green, yellow and
    blue.
    
    Bug: b:244498251
    Change-Id: Ied293bc97d70893e16d6d08199d5689edca7b388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085810
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083554}

--- ash/style/ash_color_mixer.cc
+++ ash/style/ash_color_mixer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/style/scoped_light_mode_as_default.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/dark_light_mode_controller_impl.h"
+#include "ash/style/harmonized_colors.h"
 #include "ash/style/style_util.h"
 #include "ash/system/tray/tray_constants.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -388,11 +389,14 @@ void AddCrosStylesColorMixer(ui::ColorProvider* provider,
       features::IsDarkLightModeEnabled()
           ? key.color_mode == ui::ColorProviderManager::ColorMode::kDark
           : DarkLightModeControllerImpl::Get()->IsDarkModeEnabled();
+
   if (ash::features::IsJellyEnabled()) {
     AddRefPalette(mixer, key);
   } else {
     cros_tokens::AddCrosRefColorsToMixer(mixer, dark_mode);
   }
+  // Add after ref colors since it needs to override them.
+  AddHarmonizedColors(mixer, key);
   cros_tokens::AddCrosSysColorsToMixer(mixer, dark_mode);
 
   // TODO(b/234400002): Remove legacy colors once all usages are cleaned up.
