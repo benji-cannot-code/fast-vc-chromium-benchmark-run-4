d6f80b942328ab153dcd775cfbf7aa226c28ab06
Erica Lee <ericamlee@google.com>
2022-12-15 02:48:24 +0000

    [personalization] calculate dynamic color toggle
    Calculate whether the toggle should be enabled or not.
    
    BUG=b:253276972
    TEST=browser_tests PersonalizationApp*DynamicColor*
    TEST=unit_tests *ThemeProvider*
    TEST=https://screencast.googleplex.com/cast/NjM1MzQ1MTg5NTIyNjM2OHxiNDc1NDQxZi0wZg
    
    Change-Id: Ie87f136e8cbd801197ab1f4d5123738c247f3ee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4094454
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083528}

--- ash/webui/personalization_app/resources/js/theme/theme_controller.ts
+++ ash/webui/personalization_app/resources/js/theme/theme_controller.ts
@@ -68,4 +68,5 @@ export function setStaticColorPref(
     store: PersonalizationStore) {
   provider.setStaticColor(staticColor);
   store.dispatch(setStaticColorAction(staticColor));
+  store.dispatch(setColorSchemeAction(ColorScheme.kStatic));
 }
