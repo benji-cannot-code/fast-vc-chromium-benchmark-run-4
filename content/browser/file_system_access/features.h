fe721f9b720304c55a4e546cd4b6a0353b1e74a2
Austin Sullivan <asully@chromium.org>
2022-12-15 00:12:47 +0000

    FSA: Ship FileSystemHandle::remove()
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/tESXuOfqQMQ
    
    Also creates a flag to disable this feature if needed, which would
    reject with a NotSupportedError (as suggested here:
    https://github.com/whatwg/fs/issues/57)
    
    Bug: 1114923
    Change-Id: I6568652a1e3e73f69017d5349a62d65971d61301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4077526
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083453}

--- content/browser/file_system_access/features.h
+++ content/browser/file_system_access/features.h
@@ -16,6 +16,7 @@ namespace content::features {
 
 // Alphabetical:
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kFileSystemAccessDoNotOverwriteOnMove);
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kFileSystemAccessRemove);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kFileSystemAccessRemoveEntryExclusiveLock);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(
     kFileSystemAccessRenameWithoutParentAccessRequiresUserActivation);
