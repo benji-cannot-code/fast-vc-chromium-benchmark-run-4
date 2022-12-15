030099549ec01a281a592cb9ffc6b1d328dc6c11
Abbas Nayebi <nayebi@google.com>
2022-12-15 02:29:11 +0000

    [eche] Updating all apps and recent apps separately.
    We are going to have two sorts of app updates:
    1) all apps including the disabled apps which is only sent once during
    the start of a session. It is large and is sent some time after the
    start of the connection.
    2) only rencent apps: It is small, updated more frequently, and should
    be available before we show the phone hub view.
    
    We will send the first one as part of AppListUpdate.all_apps and the
    latter will be in the Snapshot
    (for now and also in AppListUpdate.recent_apps later).
    
    Test=I made the gms core to send all apps update, then made sure 1) on the chorme side we show "all" apps in the launcher including the disabled ones 2) on the chrome we show the recent apps w/o disabled apps.
    
    Change-Id: I9ce43455f13a15b04226fa0fb337fe405d1d33c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4103900
    Auto-Submit: Abbas Nayebi <nayebi@google.com>
    Commit-Queue: Abbas Nayebi <nayebi@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083520}

--- chromeos/ash/components/phonehub/fake_recent_apps_interaction_handler.cc
+++ chromeos/ash/components/phonehub/fake_recent_apps_interaction_handler.cc
@@ -4,8 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chromeos/ash/components/phonehub/fake_recent_apps_interaction_handler.h"
+#include <utility>
 
 #include "base/containers/contains.h"
+#include "base/time/time.h"
 #include "chromeos/ash/components/phonehub/notification.h"
 
 namespace ash {
@@ -46,7 +48,8 @@ void FakeRecentAppsInteractionHandler::OnFeatureStateChanged(
 void FakeRecentAppsInteractionHandler::NotifyRecentAppAddedOrUpdated(
     const Notification::AppMetadata& app_metadata,
     base::Time last_accessed_timestamp) {
-  recent_apps_metadata_.emplace_back(app_metadata, last_accessed_timestamp);
+  recent_apps_metadata_.emplace(recent_apps_metadata_.begin(), app_metadata,
+                                last_accessed_timestamp);
 }
 
 std::vector<Notification::AppMetadata>
@@ -60,7 +63,10 @@ FakeRecentAppsInteractionHandler::FetchRecentAppMetadataList() {
 
 void FakeRecentAppsInteractionHandler::SetStreamableApps(
     const std::vector<Notification::AppMetadata>& streamable_apps) {
-  // TODO(nayebi): Do we need to implement this?
+  recent_apps_metadata_.clear();
+  for (const auto& app_metadata : streamable_apps) {
+    recent_apps_metadata_.emplace_back(app_metadata, base::Time::UnixEpoch());
+  }
 }
 
 void FakeRecentAppsInteractionHandler::ComputeAndUpdateUiState() {
