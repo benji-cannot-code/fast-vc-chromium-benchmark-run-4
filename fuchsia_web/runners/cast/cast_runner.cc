bd7d83dd7dbf64c306c5e19af0c737446684add0
Greg Thompson <grt@chromium.org>
2022-12-15 15:22:50 +0000

    [fuchsia] Run web_instance as a child of cast_runner
    In so doing, WebInstanceHost may now be constructed to run WebInstance
    via CFv2 apis. In this case, web_instance.cm is run as a dynamic child
    of the hosting component. Conditional capabilities based on
    fuchsia.web/CreateContextParams are delivered via dynamic
    offers. Command line arguments are delivered via a JSON file shared to
    the instance from the hosting component.
    
    Bug: 1280703
    Change-Id: Ib2fde5c99f310cda17644dbf20e663e93d57a647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016364
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083755}

--- fuchsia_web/runners/cast/cast_runner.cc
+++ fuchsia_web/runners/cast/cast_runner.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia_web/runners/cast/cast_streaming.h"
 #include "fuchsia_web/runners/cast/pending_cast_component.h"
 #include "fuchsia_web/runners/common/web_content_runner.h"
+#include "fuchsia_web/webinstance_host/web_instance_host.h"
 #include "url/gurl.h"
 
 namespace {
@@ -250,14 +251,16 @@ class FrameHostComponent final
 
 }  // namespace
 
-CastRunner::CastRunner(WebInstanceHostV1& web_instance_host, Options options)
+CastRunner::CastRunner(WebInstanceHost& web_instance_host, Options options)
     : web_instance_host_(web_instance_host),
       is_headless_(options.headless),
       disable_codegen_(options.disable_codegen),
       main_services_(std::make_unique<base::FilteredServiceDirectory>(
           base::ComponentContextForProcess()->svc())),
       main_context_(std::make_unique<WebContentRunner>(
-          *web_instance_host_,
+          base::BindRepeating(
+              &WebInstanceHost::CreateInstanceForContextWithCopiedArgs,
+              base::Unretained(&web_instance_host_.get())),
           base::BindRepeating(&CastRunner::GetMainWebInstanceConfig,
                               base::Unretained(this)))),
       isolated_services_(std::make_unique<base::FilteredServiceDirectory>(
@@ -552,8 +555,11 @@ CastRunner::GetWebInstanceConfigForAppConfig(
 WebContentRunner* CastRunner::CreateIsolatedRunner(
     WebContentRunner::WebInstanceConfig config) {
   // Create an isolated context which will own the CastComponent.
-  auto context = std::make_unique<WebContentRunner>(*web_instance_host_,
-                                                    std::move(config));
+  auto context = std::make_unique<WebContentRunner>(
+      base::BindRepeating(
+          &WebInstanceHost::CreateInstanceForContextWithCopiedArgs,
+          base::Unretained(&web_instance_host_.get())),
+      std::move(config));
   context->SetOnEmptyCallback(
       base::BindOnce(&CastRunner::OnIsolatedContextEmpty,
                      base::Unretained(this), base::Unretained(context.get())));
