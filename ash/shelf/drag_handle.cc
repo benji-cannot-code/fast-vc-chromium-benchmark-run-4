3416a0311b0dfd74e17c991f3bc438a7930e77d0
Wen-Chien Wang <wcwang@chromium.org>
2022-12-15 01:48:39 +0000

    Notify the state of the drag handle with Chromevox when focusing on it
    Previously with the Chromevox a11y is on, there is no announcement that
    mentions the state of the shelf apps when the drag handle is focus,
    that is, the users won't know if the apps in the hotseat is shown or
    hidden. This cl adds additional states to the drag handle view so that
    it can announce if the shelf apps are showing or not.
    
    Bug: b:261648688
    Change-Id: I8a64f10329d79f639a0ba288411f068dd9a40576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104481
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083499}

--- ash/shelf/drag_handle.cc
+++ ash/shelf/drag_handle.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shelf/drag_handle.h"
 
+#include <string>
+
 #include "ash/accessibility/accessibility_controller_impl.h"
 #include "ash/constants/ash_features.h"
 #include "ash/controls/contextual_tooltip.h"
@@ -273,7 +275,9 @@ gfx::Rect DragHandle::GetAnchorBoundsInScreen() const {
 }
 
 void DragHandle::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  // TODO(b/262424972): Remove unwanted ", window" string from the announcement.
   Button::GetAccessibleNodeData(node_data);
+  GetViewAccessibility().OverrideRole(ax::mojom::Role::kPopUpButton);
 
   std::u16string accessible_name = std::u16string();
   switch (shelf_->shelf_layout_manager()->hotseat_state()) {
@@ -283,7 +287,8 @@ void DragHandle::GetAccessibleNodeData(ui::AXNodeData* node_data) {
       break;
     case HotseatState::kHidden:
       accessible_name = l10n_util::GetStringUTF16(
-          IDS_ASH_DRAG_HANDLE_HOTSEAT_SHOW_ACCESSIBLE_NAME);
+          IDS_ASH_DRAG_HANDLE_HOTSEAT_ACCESSIBLE_NAME);
+      node_data->AddState(ax::mojom::State::kCollapsed);
 
       // When the hotseat is kHidden, the focus traversal should go to the
       // status area as the next focus and the navigation area as the previous
@@ -293,6 +298,8 @@ void DragHandle::GetAccessibleNodeData(ui::AXNodeData* node_data) {
           shelf_->shelf_widget()->navigation_widget());
       break;
     case HotseatState::kExtended:
+      node_data->AddState(ax::mojom::State::kExpanded);
+
       // When the hotseat is kExtended, the focus traversal should go to the
       // hotseat as both the next and previous focus.
       GetViewAccessibility().OverrideNextFocus(shelf_->hotseat_widget());
@@ -300,9 +307,10 @@ void DragHandle::GetAccessibleNodeData(ui::AXNodeData* node_data) {
 
       // The name should be empty when the hotseat is extended but we cannot
       // hide it.
-      if (force_show_hotseat_resetter_)
+      if (force_show_hotseat_resetter_) {
         accessible_name = l10n_util::GetStringUTF16(
-            IDS_ASH_DRAG_HANDLE_HOTSEAT_HIDE_ACCESSIBLE_NAME);
+            IDS_ASH_DRAG_HANDLE_HOTSEAT_ACCESSIBLE_NAME);
+      }
       break;
   }
   node_data->SetName(accessible_name);
