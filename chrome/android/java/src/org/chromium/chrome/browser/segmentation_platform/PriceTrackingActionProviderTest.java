916c7b2180440e9faadd53ea4c77c3dea01c6127
Matt Jones <mdjones@chromium.org>
2022-12-15 22:00:15 +0000

    Use java.util.Optional instead of guava in shopping service
    Change-Id: I4a5a562cd8f470b9f3c9bb032a4a37ef155c870b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062321
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084006}

--- chrome/android/java/src/org/chromium/chrome/browser/segmentation_platform/PriceTrackingActionProviderTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/segmentation_platform/PriceTrackingActionProviderTest.java
@@ -11,8 +11,6 @@ import static org.mockito.Mockito.doReturn;
 
 import android.os.Handler;
 
-import com.google.common.base.Optional;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -38,6 +36,7 @@ import org.chromium.components.commerce.core.ShoppingService.ProductInfoCallback
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Unit tests for {@link PriceTrackingActionProvider}
@@ -84,7 +83,7 @@ public class PriceTrackingActionProviderTest {
 
     private void setPriceTrackingBackendResult(boolean hasProductInfo) {
         ProductInfo testProductInfo =
-                new ProductInfo(null, null, 0, 0, null, 0, null, Optional.absent());
+                new ProductInfo(null, null, 0, 0, null, 0, null, Optional.empty());
         Mockito.doAnswer(invocation -> {
                    ProductInfoCallback callback = invocation.getArgument(1);
                    callback.onResult(
