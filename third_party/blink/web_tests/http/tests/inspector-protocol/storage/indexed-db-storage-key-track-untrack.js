e4f44d99e48322ef857be5d4bb1a5962e0972ca3
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-12-15 15:07:52 +0000

    Add logging of sent CDP commands
    Bug: 1366717
    Change-Id: I5a70250f5f45557f3499be5f756b385a7566bb70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111221
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083747}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
@@ -6,10 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.Page.enable();
   const protocolMessages = [];
   const originalDispatchMessage = DevToolsAPI.dispatchMessage;
+  const originalSendCommand = DevToolsAPI._sendCommand;
   DevToolsAPI.dispatchMessage = (message) => {
     protocolMessages.push(message);
     originalDispatchMessage(message);
   };
+  DevToolsAPI._sendCommand = (sessionId, method, params) => {
+    protocolMessages.push({sessionId, method, params});
+    return originalSendCommand(sessionId, method, params);
+  }
   window.onerror = (msg) => testRunner.log('onerror: ' + msg);
   window.onunhandledrejection = (e) => testRunner.log('onunhandledrejection: ' + e.reason);
   let errorForLog = new Error();
