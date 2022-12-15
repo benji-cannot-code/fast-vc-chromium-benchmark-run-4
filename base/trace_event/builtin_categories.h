f5aeb7fbba299435e98b2ac3b6cc50486220d0b2
Jordan Bayles <jophba@chromium.org>
2022-12-15 04:20:49 +0000

    [Open Screen] enable trace logging
    This patch is the first step in the process of debugging some
    performance related issues in libcast, and implements a trace logging
    Open Screen platform class in Chrome's openscreen_platform component.
    
    This class is instantiated whenever an OpenscreenSessionHost is
    constructed.
    
    Low-Coverage-Reason: api tracing code
    Bug: b/254655002
    Change-Id: I662dae576bbbd70cb6c1a7a6463d73b3d922192b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997361
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083559}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -127,6 +127,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("offline_pages")                                                     \
   X("omnibox")                                                           \
   X("oobe")                                                              \
+  X("openscreen")                                                        \
   X("ozone")                                                             \
   X("partition_alloc")                                                   \
   X("passwords")                                                         \
--- components/mirroring/service/openscreen_session_host.h
+++ components/mirroring/service/openscreen_session_host.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/mirroring/service/openscreen_message_port.h"
 #include "components/mirroring/service/openscreen_rpc_dispatcher.h"
 #include "components/mirroring/service/rtp_stream.h"
+#include "components/openscreen_platform/event_trace_logging_platform.h"
 #include "components/openscreen_platform/task_runner.h"
 #include "gpu/config/gpu_info.h"
 #include "media/capture/video/video_capture_feedback.h"
