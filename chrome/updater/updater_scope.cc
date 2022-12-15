40cba629a6602404097a07f671b7bb7566faf68f
S. Ganesh <ganesh@chromium.org>
2022-12-15 23:57:38 +0000

    Fix Handoff from Omaha to O4 fails
    The updater scope was not taking the legacy command line format into
    account, now fixed by determining the scope using the legacy-compatible
    command line.
    
    Added integration tests.
    
    Bug: 1379746
    Change-Id: I42386daf3276156ab34e6ac17094b0b6439a8898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111693
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084071}

--- chrome/updater/updater_scope.cc
+++ chrome/updater/updater_scope.cc
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "build/build_config.h"
 #include "chrome/updater/constants.h"
+#include "chrome/updater/util/util.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "chrome/updater/tag.h"
-#include "chrome/updater/util/util.h"
 #include "chrome/updater/util/win_util.h"
 #endif
 
@@ -74,7 +74,7 @@ UpdaterScope GetUpdaterScopeForCommandLine(
 }
 
 UpdaterScope GetUpdaterScope() {
-  return GetUpdaterScopeForCommandLine(*base::CommandLine::ForCurrentProcess());
+  return GetUpdaterScopeForCommandLine(GetCommandLineLegacyCompatible());
 }
 
 bool IsSystemInstall() {
