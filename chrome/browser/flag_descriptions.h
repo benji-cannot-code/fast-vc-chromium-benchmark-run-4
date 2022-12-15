32dfbcf34c579f2983052590169ceed87bd1452c
Qihui Zhao <qihuizhao@google.com>
2022-12-15 21:36:39 +0000

    Add flag for Expiration and Type titles removal.
    This CL introduces a flag which controls removing Expiration and Type columns from Chrome Payment Settings page.
    
    Bug: 1399886
    Change-Id: I042b2fe56f4ee772eee4e30877c219655497147d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4092764
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083989}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -266,6 +266,9 @@ extern const char kAutofillParseVcnCardOnFileStandaloneCvcFieldsDescription[];
 extern const char kAutofillPreventOverridingPrefilledValuesName[];
 extern const char kAutofillPreventOverridingPrefilledValuesDescription[];
 
+extern const char kAutofillRemoveCardExpirationAndTypeTitlesName[];
+extern const char kAutofillRemoveCardExpirationAndTypeTitlesDescription[];
+
 extern const char kAutofillSaveAndFillVPAName[];
 extern const char kAutofillSaveAndFillVPADescription[];
 
