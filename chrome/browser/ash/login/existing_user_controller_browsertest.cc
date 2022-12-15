98d1c181ab9579b08f9ad46ec5780553dee55730
Felipe Andrade <fsandrade@chromium.org>
2022-12-15 21:25:11 +0000

    Enable flag in Active Directory OOBE tests
    Update Active Directory OOBE tests to always run with the
    kChromadAvailable feature flag enabled. The tests fail when the flag is
    disabled, as expected, because the device shows the disabled screen and
    blocks any user or guest session from starting.
    
    Bug: b:259180126
    Change-Id: I687f96c6daf738592fc5bf42498f4a0f84ea3c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111231
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083981}

--- chrome/browser/ash/login/existing_user_controller_browsertest.cc
+++ chrome/browser/ash/login/existing_user_controller_browsertest.cc
@@ -879,7 +879,13 @@ IN_PROC_BROWSER_TEST_F(ExistingUserControllerSecondPublicSessionTest,
 class ExistingUserControllerActiveDirectoryTest
     : public ExistingUserControllerTest {
  public:
-  ExistingUserControllerActiveDirectoryTest() = default;
+  ExistingUserControllerActiveDirectoryTest() {
+    // All tests related to Active Directory login don't make sense when the
+    // kChromadAvailable feature is disabled. We also don't need to verify that
+    // the device is disabled in that case, because the Chromad disabling
+    // feature is already tested in `device_disabling_manager_unittest.cc`.
+    scoped_feature_list_.InitAndEnableFeature(features::kChromadAvailable);
+  }
 
   // Overriden from ExistingUserControllerTest:
   void SetUp() override {
@@ -1013,6 +1019,7 @@ class ExistingUserControllerActiveDirectoryTest
 
  private:
   testing::NiceMock<policy::MockConfigurationPolicyProvider> policy_provider_;
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 class ExistingUserControllerActiveDirectoryTestCreateProfileDir
