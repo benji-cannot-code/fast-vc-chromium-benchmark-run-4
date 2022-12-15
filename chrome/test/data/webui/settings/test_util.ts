4b33f1fd950206a915232afd26011fe0154e518b
John Lee <johntlee@chromium.org>
2022-12-15 01:49:24 +0000

    Settings WebUI: Make policy tooltips accessible to screen readers
    Having `display: none` on the tooltip for all policy indicators in this
    component was causing it to be skipped by screen reader. The focusable
    icon in cr-tooltip-icon has an `aria-describedby` set to the tooltip
    text, so the tooltip needs to be visibly hidden but still accessible
    by screen readers.
    
    Fixed: 1347653
    Change-Id: If1716cd0eb174f12433254feb949e8437f64ddc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104244
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083500}

--- chrome/test/data/webui/settings/test_util.ts
+++ chrome/test/data/webui/settings/test_util.ts
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-import {ChooserType, ContentSetting, ContentSettingProvider, ContentSettingsTypes, DefaultContentSetting, OriginInfo, RawChooserException, RawSiteException, SiteGroup, SiteSettingSource} from 'chrome://settings/lazy_load.js';
+import {ChooserType, ContentSetting, ContentSettingProvider, ContentSettingsTypes, DefaultContentSetting, OriginInfo, PaperTooltipElement, RawChooserException, RawSiteException, SiteGroup, SiteSettingSource} from 'chrome://settings/lazy_load.js';
 import {Route, Router} from 'chrome://settings/settings.js';
+import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 // clang-format on
-
 
 /**
  * Helper to create an object containing a ContentSettingsType key to array or
@@ -224,4 +224,16 @@ export function setupPopstateListener() {
             (routerInstance.getRoutes() as {BASIC: Route}).BASIC,
         new URLSearchParams(window.location.search), true);
   });
+}
+
+/**
+ * Helper to assert that a paper-tooltip element is visually hidden but still
+ * accessible by screen readers.
+ */
+export function assertTooltipIsHidden(tooltip: PaperTooltipElement) {
+  const tooltipStyle = window.getComputedStyle(tooltip);
+  assertEquals('rect(0px, 0px, 0px, 0px)', tooltipStyle.clip);
+  assertEquals('1px', tooltipStyle.height);
+  assertEquals('1px', tooltipStyle.width);
+  assertEquals('hidden', tooltipStyle.overflow);
 }
