f75aee9a4bab0a85d4f4b40224dc08a8f0dfabe0
Filipa Senra <fsenra@google.com>
2022-12-15 12:10:21 +0000

    [Android][Privacy Guide] Add a metric for done button of PG
    This CL adds a metric "Settings.PrivacyGuide.NextNavigation"
    to record the click on the done button in the Completion Card of Privacy
    Guide.
    
    Bug: 1238896
    Change-Id: I9da062ab69aae70b0d4b638c18a22a3ff903408b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4097184
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083681}

--- chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/PrivacyGuideMetricsDelegate.java
+++ chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/PrivacyGuideMetricsDelegate.java
@@ -216,6 +216,9 @@ class PrivacyGuideMetricsDelegate {
      */
     static void recordMetricsForDoneButton() {
         RecordUserAction.record("Settings.PrivacyGuide.NextClickCompletion");
+        RecordHistogram.recordEnumeratedHistogram("Settings.PrivacyGuide.NextNavigation",
+                PrivacyGuideInteractions.COMPLETION_NEXT_BUTTON,
+                PrivacyGuideInteractions.MAX_VALUE);
     }
 
     /**
