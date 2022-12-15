c739d571a2907bce142591eb2c86fa8abbb20686
David Bertoni <dbertoni@chromium.org>
2022-12-15 18:08:02 +0000

    [Extensions] Remove the last uses of ListValue from extensions/*
    Bug: 11870612
    Change-Id: I3cb75ae6f711eaa53fd5ab519e8509c2f346e2b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109769
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083841}

--- extensions/browser/script_executor.cc
+++ extensions/browser/script_executor.cc
@@ -34,10 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_macros.h"
 
-namespace base {
-class ListValue;
-}  // namespace base
-
 namespace extensions {
 
 namespace {
