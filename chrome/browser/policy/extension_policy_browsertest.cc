fe659850e0310d083ee09ee73b311ee810fd8587
Camden King <camdenking@google.com>
2022-12-15 18:17:37 +0000

    [dPWA] Migrating uses of deprecated registrar to registrar_unsafe
    This CL is migrating usage of WebAppProvider::registrar to
    WebAppProvider::registrar_unsafe before WebAppProvider::registrar
    is removed.
    
    Design doc linked in crbug.com/1399641
    
    There is no functional change in this CL.
    
    This CL was uploaded by git cl split.
    
    R=nicolaso@chromium.org
    
    Bug: 1399641 b/260863363
    Change-Id: I8f9f7fcaac151798d0f76300a2add4ef17e04e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087982
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Camden King <camdenking@google.com>
    Auto-Submit: Camden King <camdenking@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083850}

--- chrome/browser/policy/extension_policy_browsertest.cc
+++ chrome/browser/policy/extension_policy_browsertest.cc
@@ -2245,7 +2245,8 @@ class WebAppInstallForceListPolicyTest : public ExtensionPolicyTest {
 
 IN_PROC_BROWSER_TEST_F(WebAppInstallForceListPolicyTest, StartUpInstallation) {
   const web_app::WebAppRegistrar& registrar =
-      web_app::WebAppProvider::GetForTest(browser()->profile())->registrar();
+      web_app::WebAppProvider::GetForTest(browser()->profile())
+          ->registrar_unsafe();
   web_app::WebAppTestInstallObserver install_observer(browser()->profile());
   absl::optional<web_app::AppId> app_id =
       registrar.FindAppWithUrlInScope(policy_app_url_);
@@ -2276,7 +2277,8 @@ IN_PROC_BROWSER_TEST_F(
     WebAppInstallForceListPolicyWithAppFallbackNameManifestTest,
     StartUpInstallationPWAFallbackName) {
   const web_app::WebAppRegistrar& registrar =
-      web_app::WebAppProvider::GetForTest(browser()->profile())->registrar();
+      web_app::WebAppProvider::GetForTest(browser()->profile())
+          ->registrar_unsafe();
   web_app::WebAppTestInstallObserver install_observer(browser()->profile());
   absl::optional<web_app::AppId> app_id =
       registrar.FindAppWithUrlInScope(policy_app_url_);
@@ -2307,7 +2309,8 @@ class WebAppInstallForceListPolicySAATest
 IN_PROC_BROWSER_TEST_F(WebAppInstallForceListPolicySAATest,
                        StartUpInstallationSAA) {
   const web_app::WebAppRegistrar& registrar =
-      web_app::WebAppProvider::GetForTest(browser()->profile())->registrar();
+      web_app::WebAppProvider::GetForTest(browser()->profile())
+          ->registrar_unsafe();
   web_app::WebAppTestInstallObserver install_observer(browser()->profile());
   absl::optional<web_app::AppId> app_id =
       registrar.FindAppWithUrlInScope(policy_app_url_);
@@ -2335,7 +2338,8 @@ class WebAppInstallForceListPolicyWithAppFallbackNameSAATest
 IN_PROC_BROWSER_TEST_F(WebAppInstallForceListPolicyWithAppFallbackNameSAATest,
                        StartUpInstallationSAAFallbackName) {
   const web_app::WebAppRegistrar& registrar =
-      web_app::WebAppProvider::GetForTest(browser()->profile())->registrar();
+      web_app::WebAppProvider::GetForTest(browser()->profile())
+          ->registrar_unsafe();
   web_app::WebAppTestInstallObserver install_observer(browser()->profile());
   absl::optional<web_app::AppId> app_id =
       registrar.FindAppWithUrlInScope(policy_app_url_);
@@ -2400,7 +2404,8 @@ IN_PROC_BROWSER_TEST_P(
     WebAppInstallForceListPolicyPlaceholderWithAppFallbackNameTest,
     StartUpInstallationPlaceholderFallbackName) {
   const web_app::WebAppRegistrar& registrar =
-      web_app::WebAppProvider::GetForTest(browser()->profile())->registrar();
+      web_app::WebAppProvider::GetForTest(browser()->profile())
+          ->registrar_unsafe();
   web_app::WebAppTestInstallWithOsHooksObserver install_observer(
       browser()->profile());
   absl::optional<web_app::AppId> app_id =
