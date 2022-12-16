e1d9ef7dfb4ef906ed11b527549cf7a5f493cf96
Antoni Dikov <adikov@google.com>
2022-12-16 11:34:33 +0000

    [CrOS Cellular] APN enable/disable/remove warning prompt
    * Implement the logic for when enable/disable/remove buttons
    should show warning prompt.
    
    https://screenshot.googleplex.com/5DKttZjdirYQbiB.png
    https://screenshot.googleplex.com/8bEwF6D84h2tjgw.png
    
    Test: --gtest_filter=*ApnList*
    Bug: b:162365553
    Change-Id: Iaee33d4fae87dff06b9065971fda5716159e09d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085027
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Antoni Dikov <adikov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084251}

--- ash/webui/common/resources/network/apn_list_item.js
+++ ash/webui/common/resources/network/apn_list_item.js
@@ -53,6 +53,17 @@ class ApnListItem extends ApnListItemBase {
         type: Boolean,
         value: true,
       },
+
+      shouldDisallowDisablingRemoving: {
+        type: Boolean,
+        value: false,
+      },
+
+      shouldDisallowEnabling: {
+        type: Boolean,
+        value: false,
+      },
+
       /** @private */
       isDisabled_: {
         reflectToAttribute: true,
@@ -114,6 +125,15 @@ class ApnListItem extends ApnListItemBase {
       return;
     }
 
+    if (this.shouldDisallowDisablingRemoving) {
+      this.dispatchEvent(new CustomEvent('show-error-toast', {
+        bubbles: true,
+        composed: true,
+        detail: this.i18n('apnWarningPromptForDisableRemove'),
+      }));
+      return;
+    }
+
     const apn =
         /** @type {!ApnProperties} */ (Object.assign({}, this.apn));
     apn.state = ApnState.kDisabled;
@@ -137,6 +157,17 @@ class ApnListItem extends ApnListItemBase {
       return;
     }
 
+    // TODO(b/162365553): Add string to chromeos_string when it is approved by
+    // writers.
+    if (this.shouldDisallowEnabling) {
+      this.dispatchEvent(new CustomEvent('show-error-toast', {
+        bubbles: true,
+        composed: true,
+        detail: `Can't enable this APN. Add a default APN to attach to.`,
+      }));
+      return;
+    }
+
     const apn =
         /** @type {!ApnProperties} */ (Object.assign({}, this.apn));
     apn.state = ApnState.kEnabled;
@@ -152,6 +183,15 @@ class ApnListItem extends ApnListItemBase {
     assert(this.apn);
     if (!this.apn.id) {
       console.error('Only custom APNs can be removed.');
+      return;
+    }
+
+    if (this.shouldDisallowDisablingRemoving) {
+      this.dispatchEvent(new CustomEvent('show-error-toast', {
+        bubbles: true,
+        composed: true,
+        detail: this.i18n('apnWarningPromptForDisableRemove'),
+      }));
       return;
     }
 
