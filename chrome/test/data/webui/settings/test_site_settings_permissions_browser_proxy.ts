62460858041b10ed3330f8a4764d821c87a37605
Eduard Hez <ehez@chromium.org>
2022-12-16 14:31:58 +0000

    [SafetyCheck] Add undo functionality to unused sites module
    Allow users to reverse the 'Allow again' and 'Got it' actions from the toast.
    
    Bug: 1345920
    Change-Id: Ib9c860d5b0519ec1764fa70160689f35015326c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4106390
    Commit-Queue: Eduard Hez <ehez@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084296}

--- chrome/test/data/webui/settings/test_site_settings_permissions_browser_proxy.ts
+++ chrome/test/data/webui/settings/test_site_settings_permissions_browser_proxy.ts
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
 // Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -22,6 +23,8 @@ export class TestSiteSettingsPermissionsBrowserProxy extends TestBrowserProxy
       'acknowledgeRevokedUnusedSitePermissionsList',
       'allowPermissionsAgainForUnusedSite',
       'getRevokedUnusedSitePermissionsList',
+      'undoAcknowledgeRevokedUnusedSitePermissionsList',
+      'undoAllowPermissionsAgainForUnusedSite',
     ]);
   }
 
@@ -45,5 +48,18 @@ export class TestSiteSettingsPermissionsBrowserProxy extends TestBrowserProxy
   getRevokedUnusedSitePermissionsList(): Promise<UnusedSitePermissions[]> {
     this.methodCalled('getRevokedUnusedSitePermissionsList');
     return Promise.resolve(this.unusedSitePermissions_.slice());
+  }
+
+  undoAcknowledgeRevokedUnusedSitePermissionsList(unusedSitePermissionList:
+                                                      UnusedSitePermissions[]) {
+    this.methodCalled(
+        'undoAcknowledgeRevokedUnusedSitePermissionsList',
+        [unusedSitePermissionList]);
+  }
+
+  undoAllowPermissionsAgainForUnusedSite(unusedSitePermissions:
+                                             UnusedSitePermissions) {
+    this.methodCalled(
+        'undoAllowPermissionsAgainForUnusedSite', [unusedSitePermissions]);
   }
 }
