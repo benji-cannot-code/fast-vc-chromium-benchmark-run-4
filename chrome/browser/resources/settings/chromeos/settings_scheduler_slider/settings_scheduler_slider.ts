0f247a4acc0d99e632624276b06b125eb5e55bf6
mlcui <mlcui@google.com>
2022-12-16 02:22:32 +0000

    prefs_mixin: Use more return value generics for `getPref`
    This is part of a following CL which makes
    chrome.settingsPrivate.PrefObject type-safe, but can be committed
    individually to get some better typing before that CL is merged.
    
    Bug: 1373934
    Change-Id: I3c615234df6c7c407a2580f56bb1b47a0ec8959b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4105500
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084135}

--- chrome/browser/resources/settings/chromeos/settings_scheduler_slider/settings_scheduler_slider.ts
+++ chrome/browser/resources/settings/chromeos/settings_scheduler_slider/settings_scheduler_slider.ts
@@ -265,7 +265,7 @@ class SettingsSchedulerSliderElement extends
    */
   private hourFormatChanged_() {
     this.shouldUse24Hours_ =
-        this.getPref('settings.clock.use_24hour_clock').value;
+        this.getPref<boolean>('settings.clock.use_24hour_clock').value;
   }
 
   /**
--- chrome/browser/resources/settings/performance_page/performance_page.ts
+++ chrome/browser/resources/settings/performance_page/performance_page.ts
@@ -59,7 +59,7 @@ export class SettingsPerformancePageElement extends
 
   private onChange_() {
     this.metricsProxy_.recordHighEfficiencyModeChanged(
-        this.getPref(HIGH_EFFICIENCY_MODE_PREF).value);
+        this.getPref<boolean>(HIGH_EFFICIENCY_MODE_PREF).value);
   }
 }
 
