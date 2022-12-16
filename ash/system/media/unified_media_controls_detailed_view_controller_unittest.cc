b53051f8fe7ec787c4d1e7f3342d6408d99f051a
Jazz Xu <jazzhsu@chromium.org>
2022-12-16 01:57:55 +0000

    CrOS MediaControls: Fix media controls may not fill the entire view.
    Media controls list view in quick settings' detailed view may exceed
    desired max height. We should provide an option to let the list view to
    fill its container.
    
    Bug: 1400595
    Change-Id: I53d31d6e461a8247218941f1239ff8deaee70325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4103987
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084125}

--- ash/system/media/unified_media_controls_detailed_view_controller_unittest.cc
+++ ash/system/media/unified_media_controls_detailed_view_controller_unittest.cc
@@ -29,9 +29,9 @@ class MockMediaNotificationProvider : public MediaNotificationProvider {
       : old_provider_(MediaNotificationProvider::Get()) {
     MediaNotificationProvider::Set(this);
 
-    ON_CALL(*this, GetMediaNotificationListView(_)).WillByDefault([](auto) {
-      return std::make_unique<views::View>();
-    });
+    ON_CALL(*this, GetMediaNotificationListView(_, _))
+        .WillByDefault(
+            [](auto, auto) { return std::make_unique<views::View>(); });
   }
 
   ~MockMediaNotificationProvider() override {
@@ -39,7 +39,8 @@ class MockMediaNotificationProvider : public MediaNotificationProvider {
   }
 
   // MediaNotificationProvider implementations.
-  MOCK_METHOD1(GetMediaNotificationListView, std::unique_ptr<views::View>(int));
+  MOCK_METHOD2(GetMediaNotificationListView,
+               std::unique_ptr<views::View>(int, bool));
   MOCK_METHOD0(OnBubbleClosing, void());
   std::unique_ptr<views::View> GetActiveMediaNotificationView() override {
     return std::make_unique<views::View>();
@@ -110,7 +111,8 @@ TEST_F(UnifiedMediaControlsDetailedViewControllerTest,
 
   // We should get a MediaNotificationProvider::GetMediaNotificationListView
   // call when creating the detailed view.
-  EXPECT_CALL(*provider(), GetMediaNotificationListView);
+  EXPECT_CALL(*provider(),
+              GetMediaNotificationListView(_, /*should_clip_height=*/false));
   system_tray_controller()->OnMediaControlsViewClicked();
   EXPECT_NE(system_tray_controller()->detailed_view_controller(), nullptr);
 
