4386558b4b00581d8c04a03dd0a52928a44c602e
Huiting Yu <huitingyu@google.com>
2022-12-16 19:51:19 +0000

    [ios] Add and initiate CPE promo Coordinator class.
    The coordinator's lifecycle is managed in browser_coordinator.
    Make the coordinator the handler for CredentialProviderPromoCommands.
    
    Change-Id: I53dea5c624b00d82bdae6a6f25436b83ec7d9855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060456
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Huiting Yu <huitingyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084477}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/commerce/price_notifications/price_notifications_tab_helper.h"
 #import "ios/chrome/browser/commerce/push_notification/push_notification_feature.h"
+#import "ios/chrome/browser/credential_provider_promo/features.h"
 #import "ios/chrome/browser/download/download_directory_util.h"
 #import "ios/chrome/browser/download/external_app_util.h"
 #import "ios/chrome/browser/download/pass_kit_tab_helper.h"
@@ -89,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/web_content_commands.h"
 #import "ios/chrome/browser/ui/commands/whats_new_commands.h"
 #import "ios/chrome/browser/ui/context_menu/context_menu_configuration_provider.h"
+#import "ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_coordinator.h"
 #import "ios/chrome/browser/ui/default_promo/default_browser_promo_coordinator.h"
 #import "ios/chrome/browser/ui/default_promo/default_browser_promo_non_modal_commands.h"
 #import "ios/chrome/browser/ui/default_promo/default_browser_promo_non_modal_coordinator.h"
@@ -434,6 +436,7 @@ enum class ToolbarKind {
   id<PopupMenuCommands> _popupMenuCommandsHandler;
   id<SnackbarCommands> _snackbarCommandsHandler;
   absl::optional<ToolbarKind> _nextToolbarToPresent;
+  CredentialProviderPromoCoordinator* _credentialProviderPromoCoordinator;
 }
 
 #pragma mark - ChromeCoordinator
@@ -1001,6 +1004,14 @@ enum class ToolbarKind {
                                browser:self.browser];
     [self.priceNotificationsIPHCoordinator start];
   }
+
+  if (IsCredentialProviderExtensionPromoEnabled()) {
+    _credentialProviderPromoCoordinator =
+        [[CredentialProviderPromoCoordinator alloc]
+            initWithBaseViewController:self.viewController
+                               browser:self.browser];
+    [_credentialProviderPromoCoordinator start];
+  }
 }
 
 // Stops child coordinators.
@@ -1113,6 +1124,9 @@ enum class ToolbarKind {
 
   [self.priceNotificationsIPHCoordinator stop];
   self.priceNotificationsIPHCoordinator = nil;
+
+  [_credentialProviderPromoCoordinator stop];
+  _credentialProviderPromoCoordinator = nil;
 }
 
 // Starts mediators owned by this coordinator.
