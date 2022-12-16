54256efdc5a9a6093c41b157431cdbc6cf0cd759
Ender <ender@google.com>
2022-12-16 20:21:34 +0000

    [FixIt] Retire MostVisitedTiles feature flag switch on Android.
    The flag is still in use by iOS hence not removing feature flag
    itself.
    
    Bug: 1385901
    Change-Id: Ie85f844239e663dc1dc5088f1c03e815826504d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114301
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084494}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -346,7 +346,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &offline_pages::kOfflinePagesDescriptivePendingStatusFeature,
     &offline_pages::kOfflinePagesLivePageSharingFeature,
     &offline_pages::kPrefetchingOfflinePagesFeature,
-    &omnibox::kMostVisitedTiles,
     &omnibox::kMostVisitedTilesTitleWrapAround,
     &omnibox::kOmniboxAssistantVoiceSearch,
     &omnibox::kOmniboxMatchToolbarAndStatusBarColor,
