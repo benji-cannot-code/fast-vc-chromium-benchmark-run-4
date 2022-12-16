22338e45ff67849bb4fdc8530252007f057cf92b
Grayson LaFleur <graysonlafleur@google.com>
2022-12-16 18:17:00 +0000

    Enforces invariants based on VAConfigID.
    Created a FakeDriver class that can be used to keep track of state
    in a thread-safe capacity. ObjectTracker was created as a templatized
    class that could manage and store data FakeDriver needed to keep track
    of.
    
    Fake_drv_video.cc ensures that all VAConfigIDs are created and destroyed
    properly as well as ensure that all VAConfigIDs that are used have
    actually been created.
    
    Change-Id: Icf82679db4fe913b9fffb5cb1a4f1e1e809072b4
    Bug: b:259964072
    Test: LIBVA_DRIVERS_PATH=./{buildpath} LIBVA_DRIVER_NAME=libfake ./{buildpath}/fake_libva_driver_unittest
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044162
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Grayson LaFleur <graysonlafleur@google.com>
    Reviewed-by: Pilar Molina Lopez <pmolinalopez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084406}

---
+++ media/gpu/vaapi/test/fake_libva_driver/fake_config.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/gpu/vaapi/test/fake_libva_driver/fake_config.h"
+
+namespace media::internal {
+
+FakeConfig::FakeConfig(FakeConfig::IdType id,
+                       VAProfile profile,
+                       VAEntrypoint entrypoint,
+                       std::vector<VAConfigAttrib> attrib_list)
+    : id_(id),
+      profile_(profile),
+      entrypoint_(entrypoint),
+      attrib_list_(std::move(attrib_list)) {}
+FakeConfig::~FakeConfig() = default;
+
+FakeConfig::IdType FakeConfig::GetID() const {
+  return id_;
+}
+
+VAProfile FakeConfig::GetProfile() const {
+  return profile_;
+}
+
+VAEntrypoint FakeConfig::GetEntrypoint() const {
+  return entrypoint_;
+}
+
+const std::vector<VAConfigAttrib>& FakeConfig::GetConfigAttribs() const {
+  return attrib_list_;
+}
+
+}  // namespace media::internal
