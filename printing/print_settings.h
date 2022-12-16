3a9595b31cd7fc25728cd83946e455a33b422ae5
Sparik Hayrapetyan <ust@google.com>
2022-12-16 16:07:26 +0000

    [ipp-client-info] Add client-info print setting
    Add IppClientInfo to print.mojom. Add a list of IppClientInfo instances
    to PrintSettings. IppClientInfo is added to print.mojom because it will
    be used in local_printer.mojom API in a later CL.
    Allowlist client-info to be sent as a CUPS option without checking
    if it's supported. It is expected that the client-info print setting
    will only be set for printers supporting client-info.
    
    Fixed: b/259272161
    Change-Id: I915c191f73b31c0a07802b5f287b510a8c4e53f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026895
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Sparik Hayrapetyan <ust@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084335}

--- printing/print_settings.h
+++ printing/print_settings.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "base/component_export.h"
 #include "build/build_config.h"
@@ -256,6 +258,13 @@ class COMPONENT_EXPORT(PRINTING) PrintSettings {
 
   void set_pin_value(const std::string& pin_value) { pin_value_ = pin_value; }
   const std::string& pin_value() const { return pin_value_; }
+
+  void set_client_infos(std::vector<mojom::IppClientInfo> client_infos) {
+    client_infos_ = std::move(client_infos);
+  }
+  const std::vector<mojom::IppClientInfo>& client_infos() const {
+    return client_infos_;
+  }
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
   // Cookie generator. It is used to initialize `PrintedDocument` with its
@@ -360,7 +369,11 @@ class COMPONENT_EXPORT(PRINTING) PrintSettings {
 
   // PIN code entered by the user.
   std::string pin_value_;
-#endif
+
+  // Value of the 'client-info' that will be sent to the printer.
+  // Should only be set for printers that support 'client-info'.
+  std::vector<mojom::IppClientInfo> client_infos_;
+#endif  // BUILDFLAG(IS_CHROMEOS)
 };
 
 }  // namespace printing
--- printing/printing_context_chromeos.cc
+++ printing/printing_context_chromeos.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/backend/cups_ipp_helper.h"
 #include "printing/backend/cups_printer.h"
 #include "printing/buildflags/buildflags.h"
+#include "printing/client_info_helpers.h"
 #include "printing/metafile.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/print_job_constants.h"
@@ -44,6 +45,27 @@ const char kDocumentNamePlaceholder[] = "-";
 bool IsUriSecure(base::StringPiece uri) {
   return base::StartsWith(uri, "ipps:") || base::StartsWith(uri, "https:") ||
          base::StartsWith(uri, "usb:") || base::StartsWith(uri, "ippusb:");
+}
+
+// Returns the string representation of `client_infos` in a format suitable to
+// be used as a `cups_option_t` value.
+// `client_infos` represents the 'client-info' IPP attribute. Each item in
+// `client_infos` represents one collection in 'client-info'.
+// Invalid 'client-info' items will be dropped.
+std::string ClientInfoToCupsOptionValue(
+    const std::vector<mojom::IppClientInfo>& client_infos) {
+  // String representation for each client-info item.
+  std::vector<std::string> option_values;
+  option_values.reserve(client_infos.size());
+  for (const mojom::IppClientInfo& client_info : client_infos) {
+    if (auto option_value =
+            ClientInfoCollectionToCupsOptionValue(client_info)) {
+      option_values.emplace_back(option_value.value());
+    } else {
+      LOG(WARNING) << "Invalid client-info item skipped";
+    }
+  }
+  return base::JoinString(option_values, ",");
 }
 
 ScopedCupsOption ConstructOption(std::string name, std::string value) {
@@ -188,6 +210,15 @@ std::vector<ScopedCupsOption> SettingsToCupsOptions(
   if (!settings.oauth_token().empty()) {
     options.push_back(ConstructOption(kSettingChromeOSAccessOAuthToken,
                                       settings.oauth_token()));
+  }
+
+  // IPP client-info attribute.
+  if (!settings.client_infos().empty()) {
+    std::string option_value =
+        ClientInfoToCupsOptionValue(settings.client_infos());
+    if (!option_value.empty()) {
+      options.push_back(ConstructOption(kIppClientInfo, option_value));
+    }
   }
 
   return options;
