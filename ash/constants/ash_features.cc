91dfb758b874287b6470a723bdec1c6b47546782
Angela Xiao <angelaxiao@chromium.org>
2022-12-16 12:39:41 +0000

    Reland "Deprecate Assistant Stylus Features: cleanup metalayer code"
    This is a reland of commit ec14b06503f008cbac073cd86157ef493d1acd76
    
    Original change's description:
    > Deprecate Assistant Stylus Features: cleanup metalayer code
    >
    > This CL removes the metalayer palette tool code and resources as well as
    > associated unit tests - it also cleans up code related to the
    > deprecation launch. The "what's on my screen" feature is now fully
    > disabled (i.e. gone from stylus tools palette and won't trigger on
    > stylus long press actions).
    >
    > Follow-up CLs will clean up the highlighter controller and screen
    > context assistant interactions.
    >
    > Tests: tested on DUT
    > Bug: b/239627711
    > Change-Id: Ic58f6e90c576f2e9492b17c683a41dae56262567
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083183
    > Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    > Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    > Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1084097}
    
    Bug: b/239627711
    Change-Id: I4839cfaa5f1a6b4ef06a50fdc0efa538d27428c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110864
    Auto-Submit: Andreea Costinas <acostinas@google.com>
    Owners-Override: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084267}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -572,13 +572,6 @@ BASE_FEATURE(kDarkLightModeKMeansColor,
              "DarkLightModeKMeansColor",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables or disables Assistant stylus features, including the
-// Assistant option in the stylus palette tool and the Assistant screen
-// selection flow triggered by the stylus long press action.
-BASE_FEATURE(kDeprecateAssistantStylusFeatures,
-             "DeprecateAssistantStylusFeatures",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables or disables Sync for desk templates on ChromeOS.
 BASE_FEATURE(kDeskTemplateSync,
              "DeskTemplateSync",
@@ -2479,10 +2472,6 @@ bool IsDarkLightModeEnabled() {
 bool IsDarkLightModeKMeansColorEnabled() {
   return IsDarkLightModeEnabled() &&
          base::FeatureList::IsEnabled(kDarkLightModeKMeansColor);
-}
-
-bool IsDeprecateAssistantStylusFeaturesEnabled() {
-  return base::FeatureList::IsEnabled(kDeprecateAssistantStylusFeatures);
 }
 
 bool IsDeskTemplateSyncEnabled() {
