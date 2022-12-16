c74b7f16f4b7b9580e306cfc343370d623604df0
Daniel Vogelheim <vogelheim@chromium.org>
2022-12-16 15:07:17 +0000

    Add DetermineOriginAgentClusterEndResult to CommitErrorPage
    Bug: 1399759
    Change-Id: I6f8154b44e0b90bf87d19cb2b0c9accd0402d640
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110961
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084309}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -3225,7 +3225,8 @@ bool NavigationRequest::IsIsolationImplied() {
 }
 
 void NavigationRequest::DetermineOriginAgentClusterEndResult() {
-  DCHECK_EQ(state_, WILL_PROCESS_RESPONSE);
+  DCHECK(state_ == WILL_PROCESS_RESPONSE || state_ == WILL_FAIL_REQUEST ||
+         state_ == CANCELING);
   auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
   url::Origin origin = GetOriginToCommit().value();
   const IsolationContext& isolation_context =
@@ -5033,6 +5034,8 @@ void NavigationRequest::OnCommitDeferringConditionChecksComplete(
 void NavigationRequest::CommitErrorPage(
     const absl::optional<std::string>& error_page_content) {
   DCHECK(!IsSameDocument());
+
+  DetermineOriginAgentClusterEndResult();
 
   UpdateCommitNavigationParamsHistory();
 
