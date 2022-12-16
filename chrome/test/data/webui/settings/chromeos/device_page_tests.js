20303d7dfb67332112a7a7a5c16fea655099b66b
Danny Wang <wangdanny@google.com>
2022-12-16 06:41:58 +0000

    Settings Split: Add per-device touchpad row item to device settings
    Create the per-device-touchpad row UI element in the device settings
    page, and test the UI element with its feature flag's on and off state.
    This is in preparation for splitting up the pointers page into separate
    subpages.
    
    Screenshot: http://screen/36jTdJJV8yyTqnB
    
    Bug: b/241965700
    Test: browser_tests -- gtest_filter=*OSSettingsDevicePage*
    Disable-Rts: True
    Change-Id: Ibe81813256819d915bd83a39cf9d4a1e36bace67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4068925
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084184}

--- chrome/test/data/webui/settings/chromeos/device_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/device_page_tests.js
@@ -487,6 +487,8 @@ suite('SettingsDevicePage', function() {
     assertTrue(
         isVisible(devicePage.shadowRoot.querySelector('#perDeviceMouseRow')));
     assertTrue(isVisible(
+        devicePage.shadowRoot.querySelector('#perDeviceTouchpadRow')));
+    assertTrue(isVisible(
         devicePage.shadowRoot.querySelector('#perDeviceKeyboardRow')));
 
     webUIListenerCallback('has-mouse-changed', false);
@@ -515,6 +517,13 @@ suite('SettingsDevicePage', function() {
     await init();
     assertFalse(
         isVisible(devicePage.shadowRoot.querySelector('#perDeviceMouseRow')));
+  });
+
+  test('per-device-touchpad row visibility', async function() {
+    setDeviceSplitEnabled(false);
+    await init();
+    assertFalse(isVisible(
+        devicePage.shadowRoot.querySelector('#perDeviceTouchpadRow')));
   });
 
   test('per-device-keyboard row visibility', async function() {
