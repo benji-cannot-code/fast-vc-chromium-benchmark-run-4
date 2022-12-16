d65ed4c0c24b49cf4b54ecb61a0f2aee8c182e4e
Joshua Bell <jsbell@chromium.org>
2022-12-16 22:12:32 +0000

    Web Locks: If aborted due to an AbortSignal, use signal's reason
    Updated in the spec a year ago[1], if the AbortSignal has a reason,
    use that as the rejection, rather than a stock AbortError w/ hardcoded
    message.
    
    The WPTs didn't cover verifying the reason for an already-aborted
    signal, so add coverage for those cases too.
    
    [1] https://github.com/w3c/web-locks/pull/86
    
    Bug: 1279877
    Change-Id: I9838805b10dbb3dae0371abe41faf922344a43f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112925
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084556}

--- content/browser/locks/lock_manager_unittest.cc
+++ content/browser/locks/lock_manager_unittest.cc
@@ -59,17 +59,10 @@ class TestLockRequest : public blink::mojom::LockRequest {
     run_loop_.Quit();
   }
 
-  void Abort(const std::string& reason) override {
-    aborted_ = true;
-    run_loop_.Quit();
-    return;
-  }
-
   void WaitForCallback() { run_loop_.Run(); }
 
   bool FailureCalled() const { return failed_; }
   bool GrantedCalled() const { return granted_; }
-  bool AbortCalled() const { return aborted_; }
 
  private:
   raw_ptr<mojo::PendingAssociatedRemote<blink::mojom::LockHandle>> remote_;
@@ -77,7 +70,6 @@ class TestLockRequest : public blink::mojom::LockRequest {
   base::RunLoop run_loop_;
   bool failed_ = false;
   bool granted_ = false;
-  bool aborted_ = false;
 };
 
 TEST_F(LockManagerInvalidBucketTest, RequestLock) {
@@ -93,7 +85,6 @@ TEST_F(LockManagerInvalidBucketTest, RequestLock) {
   request.WaitForCallback();
   EXPECT_TRUE(request.FailureCalled());
   EXPECT_FALSE(request.GrantedCalled());
-  EXPECT_FALSE(request.AbortCalled());
 }
 
 TEST_F(LockManagerInvalidBucketTest, QueryState) {
