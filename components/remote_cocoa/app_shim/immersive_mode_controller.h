4cc165b34ceb3005f41981a13dcd4aa0f46a7a19
Tom Burgin <bur@chromium.org>
2022-12-16 22:12:14 +0000

    immersive fullscreen mac: only lock titlebar when fully visible
    This CL prevents a clear titlebar controller from being added if the
    titlebar is not fully visible. When the titlebar is not fully visible
    the intended effect of locking the titlebar in place does not occur.
    This change prevents needless churn. Additionally on macOS 10.15 and
    older this change prevents a graphical glitch. See the attached bug.
    
    Bug: 1400285
    Change-Id: I7c6b24baabb3da9e96cbb4c721a5cf215071c5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4107795
    Commit-Queue: Tom Burgin <bur@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084555}

--- components/remote_cocoa/app_shim/immersive_mode_controller.h
+++ components/remote_cocoa/app_shim/immersive_mode_controller.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @class ClearTitlebarViewController;
 @class ImmersiveModeMapper;
+@class ImmersiveModeTitlebarObserver;
 @class ImmersiveModeTitlebarViewController;
 @class ImmersiveModeWindowObserver;
 
@@ -62,6 +63,11 @@ class REMOTE_COCOA_APP_SHIM_EXPORT ImmersiveModeController {
 
   NSWindow* browser_window() { return browser_window_; }
   NSWindow* overlay_window() { return overlay_window_; }
+
+  // When true the titlebar is assumed to be fully visible. For testing only.
+  void SetTitlebarFullyVisibleForTesting(bool fully_visible) {
+    titlebar_fully_visible_for_testing_ = fully_visible;
+  }
 
  private:
   // Pin or unpin the titlebar.
@@ -112,6 +118,8 @@ class REMOTE_COCOA_APP_SHIM_EXPORT ImmersiveModeController {
   mojom::ToolbarVisibilityStyle last_used_style_ =
       mojom::ToolbarVisibilityStyle::kAutohide;
 
+  bool titlebar_fully_visible_for_testing_ = false;
+
   base::WeakPtrFactory<ImmersiveModeController> weak_ptr_factory_;
 };
 
@@ -135,6 +143,7 @@ REMOTE_COCOA_APP_SHIM_EXPORT @interface ImmersiveModeTitlebarObserver : NSObject
                     (base::WeakPtr<remote_cocoa::ImmersiveModeController>)
                         controller
                        overlayView:(NSView*)overlay_view;
+
 @end
 
 #endif  // COMPONENTS_REMOTE_COCOA_APP_SHIM_IMMERSIVE_MODE_CONTROLLER_H_
