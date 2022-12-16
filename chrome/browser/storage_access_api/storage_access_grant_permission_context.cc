c4add88331b369e0fee115b189db45a4b324bd0e
Shuran Huang <shuuran@chromium.org>
2022-12-16 15:55:51 +0000

    Add new SessionModel enum that is session-scoped and non-restorable
    This new enum is currently only used for FPS auto-granted SAA
    permission grants, where the settings will be reset when a
    user session ends, and will not be restored upon browser restarts.
    
    User won't notice the difference on startup, because sites cannot
    assume that they have storage access and will need to request it
    again in a fresh user session, which will be auto-granted as long as
    the FPS context is met, thus won’t affect user experience even if the
    session was ended accidentally; if the site calling the API had a FPS
    transition, then the request will be auto-rejected, which is the
    expected outcome.
    
    Bug: 1376250
    Change-Id: Ibea796aa2d92a8c036e90e0f3834d17dc1c6b556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4084181
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084329}

--- chrome/browser/storage_access_api/storage_access_grant_permission_context.cc
+++ chrome/browser/storage_access_api/storage_access_grant_permission_context.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
+#include "components/content_settings/core/common/content_settings_constraints.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/permissions/permission_request_id.h"
@@ -75,6 +76,21 @@ RequestOutcome RequestOutcomeFromPrompt(ContentSetting content_setting,
 
 void RecordOutcomeSample(RequestOutcome outcome) {
   base::UmaHistogramEnumeration("API.StorageAccess.RequestOutcome", outcome);
+}
+
+content_settings::ContentSettingConstraints ComputeConstraints(
+    RequestOutcome outcome,
+    bool implicit_result) {
+  if (!implicit_result) {
+    return {content_settings::GetConstraintExpiration(kExplicitGrantDuration),
+            content_settings::SessionModel::Durable};
+  }
+  if (outcome == RequestOutcome::kGrantedByFirstPartySet) {
+    return {content_settings::GetConstraintExpiration(kImplicitGrantDuration),
+            content_settings::SessionModel::NonRestorableUserSession};
+  }
+  return {content_settings::GetConstraintExpiration(kImplicitGrantDuration),
+          content_settings::SessionModel::UserSession};
 }
 
 }  // namespace
@@ -304,13 +320,6 @@ void StorageAccessGrantPermissionContext::NotifyPermissionSetInternal(
   DCHECK(settings_map);
   DCHECK(persist);
 
-  static const content_settings::ContentSettingConstraints ephemeral_grant = {
-      content_settings::GetConstraintExpiration(kImplicitGrantDuration),
-      content_settings::SessionModel::UserSession};
-  static const content_settings::ContentSettingConstraints durable_grant = {
-      content_settings::GetConstraintExpiration(kExplicitGrantDuration),
-      content_settings::SessionModel::Durable};
-
   // This permission was allowed so store it either ephemerally or more
   // permanently depending on if the allow came from a prompt or automatic
   // grant.
@@ -327,7 +336,7 @@ void StorageAccessGrantPermissionContext::NotifyPermissionSetInternal(
   settings_map->SetContentSettingCustomScope(
       ContentSettingsPattern::FromURLNoWildcard(requesting_origin),
       secondary_site_pattern, ContentSettingsType::STORAGE_ACCESS,
-      content_setting, implicit_result ? ephemeral_grant : durable_grant);
+      content_setting, ComputeConstraints(outcome, implicit_result));
 
   ContentSettingsForOneType grants;
   settings_map->GetSettingsForOneType(ContentSettingsType::STORAGE_ACCESS,
