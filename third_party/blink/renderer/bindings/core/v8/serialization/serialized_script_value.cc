dc7e2e09615afe06eb2e16ae6270e34da95d9eac
Austin Eng <enga@chromium.org>
2022-12-16 09:32:18 +0000

    Fix missing early return in SerializedScriptValue::TransferArrayBufferContents
    Bug: 1344595
    Change-Id: I3221b41ef86c59e37e9fcb0cbe6e2d2bfe0be5a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113283
    Auto-Submit: Austin Eng <enga@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084223}

--- third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.cc
+++ third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.cc
@@ -626,6 +626,7 @@ SerializedScriptValue::TransferArrayBufferContents(
                                           "ArrayBuffer at index " +
                                               String::Number(index) +
                                               " could not be transferred.");
+        return ArrayBufferContentsArray();
       } else if (!array_buffer->Transfer(isolate, contents.at(index),
                                          exception_state)) {
         return ArrayBufferContentsArray();
