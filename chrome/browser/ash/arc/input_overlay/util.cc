c1dd34c83253713d72f9ab397b846bb9add5c59a
Cici Ruan <cuicuiruan@google.com>
2022-12-16 19:52:49 +0000

    arc/gio: Add arrow key support for ActionView and MenuEntryView
    - Add arrow key support for moving action. Once ActionView is focused,
      it can be moved by arrow keys. Each key press event moves the
      distance of 2.
    - Add arrow key support for moving menu entry. Once the menu entry is
      focused, it can be moved by arrow keys. Each key press event moves
      the distance of 2.
    - Don't apply focus effect during dragging as per designer.
    
          focused correctly and to check if the key arrow keys work as
          expected.
    
    Bug: b/260868924
    Test: Manual test to check if the ActionView and MenuEntryView is
    Test: Add unittest in ActionViewTest and MenuEntryViewTest.
    Change-Id: I1e73e89cadd673a09f0f745fec5cadf9ea528538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4093361
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084480}

---
+++ chrome/browser/ash/arc/input_overlay/util.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/arc/input_overlay/util.h"
+
+namespace arc::input_overlay {
+
+bool UpdatePositionByArrowKey(ui::KeyboardCode key, gfx::Point& position) {
+  switch (key) {
+    case ui::VKEY_LEFT:
+      position.set_x(position.x() - kArrowKeyMoveDistance);
+      return true;
+    case ui::VKEY_RIGHT:
+      position.set_x(position.x() + kArrowKeyMoveDistance);
+      return true;
+    case ui::VKEY_UP:
+      position.set_y(position.y() - kArrowKeyMoveDistance);
+      return true;
+    case ui::VKEY_DOWN:
+      position.set_y(position.y() + kArrowKeyMoveDistance);
+      return true;
+    default:
+      return false;
+  }
+}
+
+}  // namespace arc::input_overlay
---
+++ chrome/browser/ash/arc/input_overlay/util.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UTIL_H_
+#define CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UTIL_H_
+
+#include "ui/events/keycodes/keyboard_codes_posix.h"
+#include "ui/gfx/geometry/point.h"
+
+namespace arc::input_overlay {
+
+// Arrow key move distance per key press event.
+constexpr int kArrowKeyMoveDistance = 2;
+
+// Update |position| according to |key| if |key| is arrow key.
+bool UpdatePositionByArrowKey(ui::KeyboardCode key, gfx::Point& position);
+
+}  // namespace arc::input_overlay
+
+#endif  // CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UTIL_H_
