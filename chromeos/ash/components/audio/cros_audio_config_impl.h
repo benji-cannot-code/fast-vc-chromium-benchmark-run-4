814142baad600f73ab89fa9a1eb0a2be809462ea
Yuzu Saijo <yuzus@chromium.org>
2022-12-16 00:19:07 +0000

    Revert "audio-settings: add raw_ptr to CrasAudioHandler"
    This reverts commit ed9b983b823683bf552c94faeb2873109914af00.
    
    Reason for revert: Tests started to fail because use-after-free
    Bug:1401521
    
    Original change's description:
    > audio-settings: add raw_ptr to CrasAudioHandler
    >
    > - Add raw_ptr to cache value of CrasAudioHandler::Get.
    > - Update functions to use cached value.
    >
    > Bug: b:260277007
    > Test: ./out/Default/chromeos_unittest --gtest_filter=*CrosAudioConfig*
    > Change-Id: I7e554c88228c57e5abf7db147010ba74d02b394e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108939
    > Commit-Queue: Ashley Prasad <ashleydp@google.com>
    > Reviewed-by: Michael Checo <michaelcheco@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1084005}
    
    Bug: b:260277007
    Change-Id: I199994800aefcea266b92d8777385f4770ccb76d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113081
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yuzu Saijo <yuzus@chromium.org>
    Owners-Override: Yuzu Saijo <yuzus@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1084080}

--- chromeos/ash/components/audio/cros_audio_config_impl.h
+++ chromeos/ash/components/audio/cros_audio_config_impl.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_COMPONENTS_AUDIO_CROS_AUDIO_CONFIG_IMPL_H_
 
 #include "base/component_export.h"
-#include "base/memory/raw_ptr.h"
 #include "chromeos/ash/components/audio/cras_audio_handler.h"
 #include "chromeos/ash/components/audio/cros_audio_config.h"
 
@@ -32,8 +31,6 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_AUDIO) CrosAudioConfigImpl
   void OnOutputNodeVolumeChanged(uint64_t node_id, int volume) override;
   void OnOutputMuteChanged(bool mute_on) override;
   void OnAudioNodesChanged() override;
-
-  base::raw_ptr<CrasAudioHandler> audio_handler_;
 };
 
 }  // namespace ash::audio_config
