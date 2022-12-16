6b02c55bf6629ccefbd1a627b2f0218c8aaeef69
Peter Boström <pbos@chromium.org>
2022-12-16 19:49:31 +0000

    Always dump-once NOTREACHED temporarily
    Let's turn this on after branch point so that we can find failing
    NOTREACHED()s in Canary on all platforms. This is part of burning down
    invariant failures.
    
    This has non-trivial size overhead, but we can submit this temporarily
    so long as we revert this before branch point.
    
    Bug: 851128
    Binary-Size: Revert before branch. Should not reach stable.
    Fuchsia-Binary-Size: Revert before branch. Should not reach stable.
    Change-Id: I834800908548a74b6c65c80be9f7bb94c3321230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098978
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084474}

--- base/notreached.h
+++ base/notreached.h
@@ -14,13 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace logging {
 
 // Under these conditions NOTREACHED() will effectively either log or DCHECK.
-#if BUILDFLAG(ENABLE_LOG_ERROR_NOT_REACHED) || DCHECK_IS_ON()
 #define NOTREACHED()   \
   CHECK_FUNCTION_IMPL( \
       ::logging::NotReachedError::NotReached(__FILE__, __LINE__), false)
-#else
-#define NOTREACHED() EAT_CHECK_STREAM_PARAMS()
-#endif  // BUILDFLAG(ENABLE_LOG_ERROR_NOT_REACHED) || DCHECK_IS_ON()
 
 // The NOTIMPLEMENTED() macro annotates codepaths which have not been
 // implemented yet. If output spam is a serious concern,
