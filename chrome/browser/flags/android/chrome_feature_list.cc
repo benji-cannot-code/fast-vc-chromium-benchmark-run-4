24f6a38b83a92f808c82fcecc44ce17f901a4f4e
Ender <ender@google.com>
2022-12-16 01:31:41 +0000

    [FixIt] Clean up SearchEnginePromo feature flags.
    Note: this will fail to build until https://crrev.com/i/5224824 lands.
    
    Bug: 1385901
    Change-Id: I7a8f0e1584db164b7c2f08725d24f47945cb2f12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111768
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084117}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -282,10 +282,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kRequestDesktopSiteOptInSynthetic,
     &kRequestDesktopSiteDefaultsDowngrade,
     &kSafeModeForCachedFlags,
-    &kSearchEnginePromoExistingDevice,
-    &kSearchEnginePromoExistingDeviceV2,
-    &kSearchEnginePromoNewDevice,
-    &kSearchEnginePromoNewDeviceV2,
     &kShowScrollableMVTOnNTPAndroid,
     &kFeedPositionAndroid,
     &kSearchResumptionModuleAndroid,
@@ -775,22 +771,6 @@ BASE_FEATURE(kInstantStart, "InstantStart", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIsVoiceSearchEnabledCache,
              "IsVoiceSearchEnabledCache",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSearchEnginePromoExistingDevice,
-             "SearchEnginePromo.ExistingDevice",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSearchEnginePromoExistingDeviceV2,
-             "SearchEnginePromo.ExistingDeviceVer2",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSearchEnginePromoNewDevice,
-             "SearchEnginePromo.NewDevice",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSearchEnginePromoNewDeviceV2,
-             "SearchEnginePromo.NewDeviceVer2",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNewTabPageTilesTitleWrapAround,
