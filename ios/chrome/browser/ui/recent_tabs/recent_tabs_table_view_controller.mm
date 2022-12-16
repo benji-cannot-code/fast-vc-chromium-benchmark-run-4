6dafcf30020191a74c83afbd1066c6fb83730812
Louis Romero <lpromero@google.com>
2022-12-16 16:33:39 +0000

    Add missing newline
    Change-Id: I89b49b24d60998c21cc87d92f22111ae78bb563f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110962
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084348}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
@@ -1793,6 +1793,7 @@ typedef std::pair<SessionID, TableViewURLItem*> RecentlyClosedTableViewItemPair;
   }
   return [super canPerformAction:action withSender:sender];
 }
+
 - (void)keyCommand_close {
   base::RecordAction(base::UserMetricsAction("MobileKeyCommandClose"));
   [self.presentationDelegate showActiveRegularTabFromRecentTabs];
