81c82cce08fa2bb0792ee129dfe102c23233cb24
Andrew Rayskiy <greengrape@google.com>
2022-12-16 11:34:13 +0000

    Remove FakePrintingMetricsAsh.
    After crrev.com/c/4103584 USE_CUPS is a BUILDFLAG(), so the fake class
    is no longer necessary.
    
    Bug: 1347291
    Change-Id: Ie6bf897a2cad1b1d775ebed40127b1d1db73d320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111227
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084250}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -149,8 +149,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(USE_CUPS)
 #include "chrome/browser/ash/crosapi/printing_metrics_ash.h"
-#else
-#include "chrome/browser/ash/crosapi/fake_printing_metrics_ash.h"
 #endif  // BUILDFLAG(USE_CUPS)
 
 namespace crosapi {
