bbbb31b36be7eded5a19ba1b8990966da1d18e51
Sijin Soon <sijins@google.com>
2022-12-18 23:30:16 +0000

    Add GIF section next to search bar
    Bug: b:262288373
    Change-Id: I4e86d56e14395a358562340dce07815d2a5ef15a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4095075
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Sijin Soon <sijins@google.com>
    Cr-Commit-Position: refs/heads/main@{#1084813}

--- chrome/browser/resources/chromeos/emoji_picker/types.ts
+++ chrome/browser/resources/chromeos/emoji_picker/types.ts
@@ -61,4 +61,5 @@ export enum CategoryEnum {
   EMOJI = 'emoji',
   EMOTICON = 'emoticon',
   SYMBOL = 'symbol',
+  GIF = 'GIF',
 }
---
+++ chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_gif_test.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {GIFTestSuite} from './emoji_picker_extension_gif.js';
+
+GIFTestSuite('GIF');
