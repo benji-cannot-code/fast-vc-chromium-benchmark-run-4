7ae322498e07c1a57a84a8372d381b687f02a574
Kelvin Jiang <kelvinjiang@chromium.org>
2022-12-18 07:17:57 +0000

    [Extensions] Do not load scripts that exceed a maximum size
    This CL enforces maximum sizes for individual content scripts and all
    scripts in an extension. The maximum sizes are:
     - 500 MB per script
     - 1 GB for all content scripts (manifest and dynamic) for a single
       extension
    
    Any file in a script entry that would exceed one of these limits will
    not be loaded and will generate an install warning for the extension
    if it's a manifest script.
    
    For dynamic scripts, the registerContentScripts function should return
    an error and be a no-op if a limit is exceeded. This will be tackled in
    a follow up.
    
    Bug: 1379187
    Change-Id: I2de4f123f85783350b10415c5cee022355f47f1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4095461
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084757}

--- extensions/common/manifest_handlers/content_scripts_handler.cc
+++ extensions/common/manifest_handlers/content_scripts_handler.cc
@@ -257,7 +257,7 @@ bool ContentScriptsHandler::Validate(
   // and are UTF-8 encoded.
   return script_parsing::ValidateFileSources(
       ContentScriptsInfo::GetContentScripts(extension),
-      script_parsing::GetSymlinkPolicy(extension), error);
+      script_parsing::GetSymlinkPolicy(extension), error, warnings);
 }
 
 }  // namespace extensions
