925c0095cc5e8b0bbb29e8d7cdcc724a489892a6
Alvin Jia <alvinjia@google.com>
2022-12-19 04:45:03 +0000

    add support for Tast-level retries
    This will enable tast retries on the same DUT.
    
    BUG=b:261835548
    TEST=CQ
    
    Change-Id: I4845c12c349552969fbe455ae746b68e54b4cf9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4105403
    Commit-Queue: Alvin Jia <alvinjia@google.com>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084839}

--- build/chromeos/test_runner.py
+++ build/chromeos/test_runner.py
@@ -247,6 +247,7 @@ class TastTest(RemoteTest):
 
     self._suite_name = args.suite_name
     self._tast_vars = args.tast_vars
+    self._tast_retries = args.tast_retries
     self._tests = args.tests
     # The CQ passes in '--gtest_filter' when specifying tests to skip. Store it
     # here and parse it later to integrate it into Tast executions.
@@ -338,6 +339,9 @@ class TastTest(RemoteTest):
 
     for v in self._tast_vars or []:
       self._test_cmd.extend(['--tast-var', v])
+
+    if self._tast_retries:
+      self._test_cmd.append('--tast-retries=%d' % self._tast_retries)
 
     # Mounting ash-chrome gives it enough disk space to not need stripping,
     # but only for one not instrumented with code coverage.
@@ -918,6 +922,11 @@ def main():
       dest='tast_vars',
       help='Runtime variables for Tast tests, and the format are expected to '
       'be "key=value" pairs.')
+  tast_test_parser.add_argument(
+      '--tast-retries',
+      type=int,
+      dest='tast_retries',
+      help='Number of retries for failed Tast tests on the same DUT.')
   tast_test_parser.add_argument(
       '--test',
       '-t',
