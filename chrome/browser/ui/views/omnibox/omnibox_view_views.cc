74673910768251c8d3938e6ecd458caf1bf6cb44
Michelle <michellegc@google.com>
2022-12-19 00:07:56 +0000

    Move touch text editing flag check into touch selection menu views.
    The Select All option should only appear in the views touch selection
    menu if the touch text editing redesign flag is enabled. Previously,
    crrev/c/3907164 added a flag check into OmniboxViewViews, however,
    this also disables the Select All command for omnibox in general
    (for ChromeOS), rather than just the omnibox touch selection menu.
    
    To fix this, move the flag check into TouchSelectionMenuViews. This
    should also ensure that the Select All command only appears for
    ChromeOS and only when the flag is enabled.
    
    Also remove unnecessary handling of the kSelectAll case from
    OmniboxViewViews, since the default Textfield behaviour should be ok.
    
    Bug: b:244116654
    Change-Id: Id27f1ffa15b4c79a24c16e8a15545cf8f36fb057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113540
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084814}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -89,7 +89,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/models/simple_menu_model.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/compositor/layer.h"
 #include "ui/events/event.h"
 #include "ui/gfx/canvas.h"
@@ -528,9 +527,6 @@ void OmniboxViewViews::ExecuteCommand(int command_id, int event_flags) {
     // These commands do invoke the popup.
     case Textfield::kPaste:
       ExecuteTextEditCommand(ui::TextEditCommand::PASTE);
-      return;
-    case Textfield::kSelectAll:
-      ExecuteTextEditCommand(ui::TextEditCommand::SELECT_ALL);
       return;
     default:
       if (Textfield::IsCommandIdEnabled(command_id)) {
@@ -1450,11 +1446,6 @@ void OmniboxViewViews::OnBlur() {
 }
 
 bool OmniboxViewViews::IsCommandIdEnabled(int command_id) const {
-#if BUILDFLAG(IS_CHROMEOS)
-  if (command_id == Textfield::kSelectAll) {
-    return features::IsTouchTextEditingRedesignEnabled();
-  }
-#endif
   if (command_id == Textfield::kPaste)
     return !GetReadOnly() &&
            !GetClipboardText(/*notify_if_restricted=*/false).empty();
@@ -1469,7 +1460,8 @@ bool OmniboxViewViews::IsCommandIdEnabled(int command_id) const {
     return true;
 
   return Textfield::IsCommandIdEnabled(command_id) ||
-         location_bar_view_->command_updater()->IsCommandEnabled(command_id);
+         (location_bar_view_ &&
+          location_bar_view_->command_updater()->IsCommandEnabled(command_id));
 }
 
 std::u16string OmniboxViewViews::GetSelectionClipboardText() const {
--- ui/views/touchui/touch_selection_menu_views.cc
+++ ui/views/touchui/touch_selection_menu_views.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/chromeos_buildflags.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/pointer/touch_editing_controller.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
 #include "ui/display/display.h"
@@ -37,12 +39,19 @@ namespace {
 struct MenuCommand {
   int command_id;
   int message_id;
-} kMenuCommands[] = {
+};
+
+MenuCommand kMenuCommands[] = {
     {ui::TouchEditable::kCut, IDS_APP_CUT},
     {ui::TouchEditable::kCopy, IDS_APP_COPY},
     {ui::TouchEditable::kPaste, IDS_APP_PASTE},
+};
+
+#if BUILDFLAG(IS_CHROMEOS)
+MenuCommand kMenuSelectCommands[] = {
     {ui::TouchEditable::kSelectAll, IDS_APP_SELECT_ALL},
 };
+#endif
 
 constexpr int kSpacingBetweenButtons = 2;
 
@@ -119,7 +128,12 @@ bool TouchSelectionMenuViews::IsMenuAvailable(
   const auto is_enabled = [client](MenuCommand command) {
     return client->IsCommandIdEnabled(command.command_id);
   };
-  return base::ranges::any_of(kMenuCommands, is_enabled);
+  bool is_available = base::ranges::any_of(kMenuCommands, is_enabled);
+#if BUILDFLAG(IS_CHROMEOS)
+  is_available |= ::features::IsTouchTextEditingRedesignEnabled() &&
+                  base::ranges::any_of(kMenuSelectCommands, is_enabled);
+#endif
+  return is_available;
 }
 
 void TouchSelectionMenuViews::CloseMenu() {
@@ -143,6 +157,18 @@ void TouchSelectionMenuViews::CreateButtons() {
                               base::Unretained(this), command.command_id));
     }
   }
+#if BUILDFLAG(IS_CHROMEOS)
+  if (::features::IsTouchTextEditingRedesignEnabled()) {
+    for (const auto& command : kMenuSelectCommands) {
+      if (client_->IsCommandIdEnabled(command.command_id)) {
+        CreateButton(
+            l10n_util::GetStringUTF16(command.message_id),
+            base::BindRepeating(&TouchSelectionMenuViews::ButtonPressed,
+                                base::Unretained(this), command.command_id));
+      }
+    }
+  }
+#endif
 
   // Finally, add ellipsis button.
   CreateButton(u"...",
