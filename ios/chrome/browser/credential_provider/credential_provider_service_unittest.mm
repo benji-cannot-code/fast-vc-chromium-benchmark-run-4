a04abdf118f488acd69e7293257993a0488c8a2d
Sylvain Defresne <sdefresne@chromium.org>
2022-12-19 17:24:29 +0000

    [ios] Fix CredentialProviderServiceTest
    The test suite CredentialProviderServiceTest uses variations and
    thus needs to setup a ScopedVariationsIdsProvider.
    
    Bug: none
    Change-Id: I1434be36359637362b2a70e96473779c760d6441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111554
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084994}

--- ios/chrome/browser/credential_provider/credential_provider_service_unittest.mm
+++ ios/chrome/browser/credential_provider/credential_provider_service_unittest.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/prefs/testing_pref_service.h"
 #import "components/sync/base/user_selectable_type.h"
 #import "components/sync/test/test_sync_service.h"
+#import "components/variations/scoped_variations_ids_provider.h"
 #import "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/favicon/favicon_service_factory.h"
 #import "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
@@ -136,6 +137,8 @@ class CredentialProviderServiceTest : public PlatformTest {
   base::ScopedTempDir temp_dir_;
   web::WebTaskEnvironment task_environment_{
       web::WebTaskEnvironment::IO_MAINLOOP};
+  variations::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
+      variations::VariationsIdsProvider::Mode::kUseSignedInState};
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   scoped_refptr<PasswordStore> password_store_;
   id<CredentialStore> credential_store_;
