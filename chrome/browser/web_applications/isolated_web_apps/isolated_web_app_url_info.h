0d72612e1215ff2a4e21887843d2b2742d0a30e9
Robbie McElrath <rmcelrath@chromium.org>
2022-12-19 21:28:02 +0000

    iwa: Move GetIsolationInfo to IsolatedWebAppUrlInfo
    This CL moves GetIsolationInfo from
    install_isolated_web_app_from_command_line.h to IsolatedWebAppUrlInfo,
    where it can more easily be reused by other installation paths when
    needed. This also makes install_isolated_web_app_from_command_line.h
    only contain code related to command line installation.
    
    Bug: 1333966
    Change-Id: I48e7467ceb57f90068c9c24dc5722d7026121370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114752
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Dmitrii Kuragin <kuragin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085111}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/functional/callback_forward.h"
 #include "base/types/expected.h"
 #include "chrome/browser/web_applications/web_app_id.h"
 #include "components/web_package/signed_web_bundles/signed_web_bundle_id.h"
@@ -20,6 +21,8 @@ class StoragePartitionConfig;
 }  // namespace content
 
 namespace web_app {
+
+struct IsolationData;
 
 // Wraps an Isolated Web App URL and provides methods to access data derived
 // from the URL.
@@ -36,6 +39,17 @@ class IsolatedWebAppUrlInfo {
   // Creates an IsolatedWebAppUrlInfo instance from a SignedWebBundleId object.
   static IsolatedWebAppUrlInfo CreateFromSignedWebBundleId(
       const web_package::SignedWebBundleId& web_bundle_id);
+
+  // Creates an IsolatedWebAppUrlInfo instance corresponding to the IWA
+  // located at |isolation_data|.
+  //
+  // For proxy-based dev mode IWAs a random hostname will be generated, and
+  // for signed bundles the hostname will be extracted from the bundle's
+  // integrity block.
+  static void CreateFromIsolationData(
+      const IsolationData& isolation_data,
+      base::OnceCallback<
+          void(base::expected<IsolatedWebAppUrlInfo, std::string>)> callback);
 
   // Returns the origin of the IWA that this URL refers to.
   const url::Origin& origin() const;
