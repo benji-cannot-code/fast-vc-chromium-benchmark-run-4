72c2a83f3c988b7306381ebb62c7fea7a5619f27
Nohemi Fernandez <fernandex@chromium.org>
2022-12-19 22:36:33 +0000

    Remove supervised_user_unscaled_resources and its resource ids.
    Bug: b:260402896, b:262703607
    Change-Id: I83ba70d37f03e2a0f55e631a29ee0c5f7fc9a3a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4092324
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085159}

--- chrome/browser/apps/app_service/app_service_proxy_ash.cc
+++ chrome/browser/apps/app_service/app_service_proxy_ash.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
+#include "chrome/grit/browser_resources.h"
 #include "components/account_id/account_id.h"
 #include "components/app_constants/constants.h"
 #include "components/app_restore/full_restore_save_handler.h"
