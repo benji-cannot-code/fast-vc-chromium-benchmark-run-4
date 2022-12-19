29900586d09bc06415d7362051ca0b829281c89e
François Doray <fdoray@chromium.org>
2022-12-19 05:49:48 +0000

    Remove TabMetricsLogger.
    The UKM data generated by TabMetricsLogger is not used.
    
    Bug: 784639
    Change-Id: I121ba04b49affedd55f500713f627c73b152e28c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062908
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084845}

--- chrome/browser/ash/power/ml/user_activity_manager.cc
+++ chrome/browser/ash/power/ml/user_activity_manager.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/resource_coordinator/tab_metrics_logger.h"
 #include "chrome/browser/tab_contents/form_interaction_tab_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -29,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/dbus_thread_manager.h"
 #include "chromeos/constants/devicetype.h"
 #include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
+#include "components/site_engagement/content/site_engagement_service.h"
 #include "ui/aura/client/aura_constants.h"
 
 namespace ash {
@@ -117,6 +117,24 @@ bool ShouldUseLacrosFeatures() {
   }
 
   return false;
+}
+
+int GetRoundedOrInvalidEngagementScore(content::WebContents* contents) {
+  if (!site_engagement::SiteEngagementService::IsEnabled()) {
+    return -1;
+  }
+
+  auto* service = site_engagement::SiteEngagementService::Get(
+      contents->GetBrowserContext());
+  DCHECK(service);
+
+  // Scores range from 0 to 100. Round down to a multiple of 10 to conform to
+  // privacy guidelines.
+  double raw_score = service->GetScore(contents->GetVisibleURL());
+  int rounded_score = static_cast<int>(raw_score / 10) * 10;
+  DCHECK_LE(0, rounded_score);
+  DCHECK_GE(100, rounded_score);
+  return rounded_score;
 }
 
 }  // namespace
@@ -586,8 +604,7 @@ TabProperty UserActivityManager::UpdateOpenTabURL() {
       // Domain could be empty.
       property.domain = contents->GetLastCommittedURL().host();
       // Engagement score could be -1 if engagement service is disabled.
-      property.engagement_score =
-          TabMetricsLogger::GetSiteEngagementScore(contents);
+      property.engagement_score = GetRoundedOrInvalidEngagementScore(contents);
       property.has_form_entry =
           FormInteractionTabHelper::FromWebContents(contents)
               ->had_form_interaction();
