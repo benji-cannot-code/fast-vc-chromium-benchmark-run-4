cf349d2ec68998b30adf0752349fa5bdc8595467
Bijan Choobineh <bchoobineh@chromium.org>
2022-12-19 19:14:35 +0000

    media/gpu/v4l2/test: H264 decoder frame refresh
    Updates H264 Decoder to properly refresh buffer slots in the CAPTURE
    queue. The CAPTURE queue is updated to keep track of queued
    buffer indices. Using this, the CAPTURE queue is updated to
    refresh any index that is not currently queued.
    
    This update has no functional impact on the I-frame only test cases.
    The change is required for future functionality when the decoder
    needs to access reference frames.
    
    BUG=b:234752983
    TEST=v4l2_stateless_decoder --video=BA1_Sony_D.h264 --md5=checksums.log
    TEST=v4l2_stateless_decoder --video=BAMQ1_JVC_C.h264 --md5=checksums.log
    TEST=v4l2_stateless_decoder --video=CABA1_SVA_B.h264 --md5=checksums.log
    
    Change-Id: I5e0f52af88e0dadf9e667692d8017c5e0f7505b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109068
    Reviewed-by: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085036}

--- media/gpu/v4l2/test/h264_decoder.h
+++ media/gpu/v4l2/test/h264_decoder.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_V4L2_TEST_H264_DECODER_H_
 #define MEDIA_GPU_V4L2_TEST_H264_DECODER_H_
 
+#include <set>
+
 #include "base/files/memory_mapped_file.h"
 #include "media/gpu/v4l2/test/v4l2_ioctl_shim.h"
 #include "media/gpu/v4l2/test/video_decoder.h"
@@ -51,6 +53,12 @@ class H264Decoder : public VideoDecoder {
   // device.  Additionally sends Decode Parameters and Decode Mode
   // via IOCTL Ext Ctrls.
   VideoDecoder::Result SubmitSlice(H264SliceHeader curr_slice, int frame_num);
+
+  // Returns all CAPTURE buffer indexes that can be reused for a
+  // VIDIOC_QBUF ioctl call.
+  std::set<uint32_t> GetReusableReferenceSlots(
+      const MmapedBuffer& buffer,
+      std::set<uint32_t> queued_buffer_indexes);
 
   const std::unique_ptr<H264Parser> parser_;
 
