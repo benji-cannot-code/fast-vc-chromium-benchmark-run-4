cda876efbbad18af493735e8f4f99bf539de9eda
Christian Dullweber <dullweber@chromium.org>
2022-12-19 16:11:14 +0000

    Privacy Sandbox Settings: Add topics learn more page
    Add learn more page for Topics settings.
    
    Screenshot: https://crbug.com/1378703#c163
    Bug: 1378703, b/254412699
    Change-Id: Ie8eca19f9ea131de722bfed2d416e8836fabfafa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110798
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084975}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/TopicsFragmentV4.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/TopicsFragmentV4.java
@@ -11,7 +11,6 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
 
 import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.privacy_sandbox.PrivacySandboxBridge;
@@ -26,6 +25,8 @@ import org.chromium.components.browser_ui.settings.SettingsUtils;
 import org.chromium.components.browser_ui.settings.TextMessagePreference;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.user_prefs.UserPrefs;
+import org.chromium.ui.text.NoUnderlineClickableSpan;
+import org.chromium.ui.text.SpanApplier;
 
 import java.util.List;
 
@@ -41,9 +42,9 @@ public class TopicsFragmentV4 extends PrivacySandboxSettingsBaseFragment
     private static final String TOPICS_PAGE_FOOTER_PREFERENCE = "topics_page_footer";
 
     private ChromeSwitchPreference mTopicsTogglePreference;
-    private PreferenceCategory mCurrentTopicsCategory;
-    private PreferenceCategory mEmptyTopicsPreference;
-    private PreferenceCategory mDisabledTopicsPreference;
+    private PreferenceCategoryWithClickableSummary mCurrentTopicsCategory;
+    private PreferenceCategoryWithClickableSummary mEmptyTopicsPreference;
+    private PreferenceCategoryWithClickableSummary mDisabledTopicsPreference;
     private TextMessagePreference mTopicsPageFooterPreference;
 
     static boolean isTopicsPrefEnabled() {
@@ -76,6 +77,15 @@ public class TopicsFragmentV4 extends PrivacySandboxSettingsBaseFragment
         mTopicsTogglePreference.setChecked(isTopicsPrefEnabled());
         mTopicsTogglePreference.setOnPreferenceChangeListener(this);
         mTopicsTogglePreference.setManagedPreferenceDelegate(createManagedPreferenceDelegate());
+
+        mCurrentTopicsCategory.setSummary(SpanApplier.applySpans(
+                getResources().getString(R.string.settings_topics_page_current_topics_description),
+                new SpanApplier.SpanInfo("<link>", "</link>",
+                        new NoUnderlineClickableSpan(getContext(), this::onLearnMoreClicked))));
+    }
+
+    private void onLearnMoreClicked(View view) {
+        launchSettingsActivity(TopicsLearnMoreFragment.class);
     }
 
     @Override
