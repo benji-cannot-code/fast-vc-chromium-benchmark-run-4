f12278f6be39612bfe77a8f4088bca5345ca8021
Tibor Goldschwendt <tiborg@chromium.org>
2022-12-19 15:43:44 +0000

    [cc-side-panel] Replace default color with main color if it exists
    Adds a separate <customize-chrome-color> for the extracted / main color
    of the background image, which is shown in place of the default color if
    a main color for the background image exists.
    
    The main color circles is selected whenever the currently set theme
    matches the extracted color. To that end, this CL refactors the
    selection logic somewhat to better guarantee at most one selected color
    circle.
    
    + Makes the main color optional in the background image data to robustly
      identify the case where there is no main color.
    
    + Sets main color to null if extracted color feature is disabled so that
      we can disable the feature later on if need be.
    
    https://screenshot.googleplex.com/57TvkNaruwuuoTF
    
    Fixed: 1401018
    Change-Id: I9cadd272146081c044e484bb0f024e08d1f23c62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112661
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084962}

--- chrome/browser/search/background/ntp_background_data.h
+++ chrome/browser/search/background/ntp_background_data.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "chrome/browser/search/background/ntp_background.pb.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "url/gurl.h"
 
@@ -136,7 +137,7 @@ struct CustomBackground {
   std::string collection_id;
 
   // Main color of the image.
-  SkColor custom_background_main_color;
+  absl::optional<SkColor> custom_background_main_color;
 };
 
 #endif  // CHROME_BROWSER_SEARCH_BACKGROUND_NTP_BACKGROUND_DATA_H_
