d94dbd3dde0bffc6faeb9a9e33ccddba71cff071
Paul Adedeji <pauladedeji@google.com>
2022-12-19 15:50:57 +0000

    [ntp][side-panel] Make shortcut options collapsible
    Makes it so shortcuts toggle collapses options when toggled. Also, adds final UI changes that were missing from Figma spec.
    
    Demo: http://recall/-/bSSvLvj23dPsL66j2dIfiq/dSFpkc3JuAvxZD8Ft8fa1M
    
    Change-Id: I98a982e80cd2d52966c60e937b1a8899195ae169
    Bug: 1352367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111697
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084964}

--- chrome/browser/resources/side_panel/customize_chrome/shortcuts.ts
+++ chrome/browser/resources/side_panel/customize_chrome/shortcuts.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_toggle/cr_toggle.js';
 import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
 import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
+import 'chrome://resources/polymer/v3_0/iron-collapse/iron-collapse.js';
 
 import {CrRadioButtonElement} from 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
 import {CrRadioGroupElement} from 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
@@ -78,7 +79,7 @@ export class ShortcutsElement extends PolymerElement {
     return this.customLinksEnabled_ ? 'customLinksOption' : 'mostVisitedOption';
   }
 
-  private onShowChange_(e: CustomEvent<boolean>) {
+  private onShowShortcutsToggleChange_(e: CustomEvent<boolean>) {
     this.show_ = e.detail;
     this.setMostVisitedSettings_();
   }
