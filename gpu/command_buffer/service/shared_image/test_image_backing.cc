3562825a0b4d811fa51e770ac8eb4115a870e4a6
Colin Blundell <blundell@chromium.org>
2022-12-19 18:33:01 +0000

    [GPU] Remove dead includes of gl_image.h
    Helps to clarify the work that's remaining in eliminating GLImage.
    
    Bug: 1310018
    Change-Id: Id5e875fe7b967921935a454ab709b5d07c7976ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110789
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085025}

--- gpu/command_buffer/service/shared_image/test_image_backing.cc
+++ gpu/command_buffer/service/shared_image/test_image_backing.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPromiseImageTexture.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/mock/GrMockTypes.h"
-#include "ui/gl/gl_image.h"
 
 namespace gpu {
 namespace {
