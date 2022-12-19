9826bff4dd53eab3e8ee5eb1d5cf2e92ee4fc750
cammie <cammie@chromium.org>
2022-12-19 21:30:00 +0000

    Shared Storage: Add method to reset budget for DevTools.
    We add a DevTools backend method to reset the shared storage entropy
    budget, as well as a test-only DevTools backend method to make budget
    withdrawals so that we can more easily test the former (as web tests
    with fenced frames are complex).
    
    We intend to add a button to `SharedStorageMetadataView` that will make use of the backend method `resetSharedStorageBudget`, as seen at
    https://crrev.com/c/devtools/devtools-frontend/+/4098470.
    
    Bug: 1372007,1218540
    Change-Id: If4559a61076840315c813ca8c066f23d200925c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4093334
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085113}

--- components/services/storage/shared_storage/async_shared_storage_database_impl.h
+++ components/services/storage/shared_storage/async_shared_storage_database_impl.h
@@ -132,6 +132,9 @@ class AsyncSharedStorageDatabaseImpl : public AsyncSharedStorageDatabase {
   void GetEntriesForDevTools(
       url::Origin context_origin,
       base::OnceCallback<void(EntriesResult)> callback) override;
+  void ResetBudgetForDevTools(
+      url::Origin context_origin,
+      base::OnceCallback<void(OperationResult)> callback) override;
 
   // Gets the underlying database for tests.
   base::SequenceBound<SharedStorageDatabase>*
