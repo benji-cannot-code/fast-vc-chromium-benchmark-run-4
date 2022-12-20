8c5cc0b38a02c350d30db13d791117bf48e7c201
Elie Maamari <emaamari@google.com>
2022-12-20 11:35:18 +0000

    Additional logs for fingerprint_handler.cc
    Fixed: b:227621079
    Change-Id: I056137ff676fe53142e0e2cb7ef3aee657247ae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4101961
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085372}

--- chrome/browser/ui/webui/settings/ash/fingerprint_handler.cc
+++ chrome/browser/ui/webui/settings/ash/fingerprint_handler.cc
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/device_service.h"
 #include "ui/base/l10n/l10n_util.h"
 
+// Enable VLOG level 1.
+#undef ENABLED_VLOG_LEVEL
+#define ENABLED_VLOG_LEVEL 1
+
 using session_manager::SessionManager;
 using session_manager::SessionState;
 
