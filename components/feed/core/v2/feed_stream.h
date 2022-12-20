bbbd0b1c4304f0bc3a5b803882b150f1313595b2
Nick Birnie <birnie@google.com>
2022-12-20 19:13:10 +0000

    Client Side SignInStatus Initialization and Plumbing.
    Changes to other protos as result of running generate_protos_for_chrome.sh
    
    DD: go/chrome-android-signin-promo
    
    Bug: 250924526
    Change-Id: I66d72fea0749bcb934ef8b6250441b9b531d8342
    Low-Coverage-Reason: Code in feed_service.cc is all mocked in tests besides integration tests.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060896
    Commit-Queue: Nick Birnie <birnie@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085558}

--- components/feed/core/v2/feed_stream.h
+++ components/feed/core/v2/feed_stream.h
@@ -83,6 +83,9 @@ class FeedStream : public FeedApi,
     virtual TabGroupEnabledState GetTabGroupEnabledState() = 0;
     virtual void ClearAll() = 0;
     virtual AccountInfo GetAccountInfo() = 0;
+    // Returns true if Sync is enabled for the user. If the user is not signed
+    // in it also returns false.
+    virtual bool IsSyncOn() = 0;
     virtual void PrefetchImage(const GURL& url) = 0;
     virtual void RegisterExperiments(const Experiments& experiments) = 0;
     virtual void RegisterFeedUserSettingsFieldTrial(
@@ -243,6 +246,7 @@ class FeedStream : public FeedApi,
   void PrefetchImage(const GURL& url);
 
   bool IsSignedIn() const { return !delegate_->GetAccountInfo().IsEmpty(); }
+  bool IsSyncOn() const { return delegate_->IsSyncOn(); }
   AccountInfo GetAccountInfo() const { return delegate_->GetAccountInfo(); }
 
   // Determines if we should attempt loading the stream or refreshing at all.
@@ -267,6 +271,9 @@ class FeedStream : public FeedApi,
   // user credentials.
   bool ShouldForceSignedOutFeedQueryRequest(
       const StreamType& stream_type) const;
+
+  // Returns the Chrome sign in status
+  feedwire::ChromeSignInStatus::SignInStatus GetSignInStatus() const;
 
   // Unloads one stream model. Surfaces are not updated, and will remain frozen
   // until a model load is requested.
