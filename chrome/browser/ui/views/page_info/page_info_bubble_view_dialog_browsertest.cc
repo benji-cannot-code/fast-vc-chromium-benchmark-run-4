29ce30a2b35dacad0b9a56c5bbcdd1d74e355e7c
Zelin Liu <zelin@chromium.org>
2022-12-20 23:24:26 +0000

    iwa: Use GetSiteNameOrAppNameToDisplay() instead of GetSimpleSiteName()
    For Isolated Web Apps the origin's host name is a non-human-readable
    string of characters, so instead of displaying the origin, the short
    name of the app will be displayed.
    Some surfaces, such as PageInfo title, already use this logic.
    This change targets all remaining the surfaces that uses
    GetSimpleSiteName(). All callsites of GetSimpleSiteName() will be
    replaced with GetSiteNameOrAppNameToDisplay() to promote consistency.
    
    Bug: 1311065
    Change-Id: I52d6b98bd65bd53d7ee5793564904a218a06fcf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4097611
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085693}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_dialog_browsertest.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_dialog_browsertest.cc
@@ -513,7 +513,7 @@ class PageInfoBubbleViewAboutThisSiteDialogBrowserTest
     std::u16string site_name = u"Example site";
     bubble_view->presenter_for_testing()->SetSiteNameForTesting(site_name);
     ASSERT_EQ(
-        bubble_view->presenter_for_testing()->GetSiteOriginOrAppNameToDisplay(),
+        bubble_view->presenter_for_testing()->GetSiteNameOrAppNameToDisplay(),
         site_name);
 
     if (name == "AboutThisSite") {
@@ -604,7 +604,7 @@ class PageInfoBubbleViewPrivacySandboxDialogBrowserTest
     std::u16string site_name = u"Example site";
     bubble_view->presenter_for_testing()->SetSiteNameForTesting(site_name);
     ASSERT_EQ(
-        bubble_view->presenter_for_testing()->GetSiteOriginOrAppNameToDisplay(),
+        bubble_view->presenter_for_testing()->GetSiteNameOrAppNameToDisplay(),
         site_name);
 
     if (name == "PrivacySandboxMain") {
@@ -673,7 +673,7 @@ class PageInfoBubbleViewHistoryDialogBrowserTest : public DialogBrowserTest {
     std::u16string site_name = u"Example site";
     bubble_view->presenter_for_testing()->SetSiteNameForTesting(site_name);
     ASSERT_EQ(
-        bubble_view->presenter_for_testing()->GetSiteOriginOrAppNameToDisplay(),
+        bubble_view->presenter_for_testing()->GetSiteNameOrAppNameToDisplay(),
         site_name);
   }
 
@@ -876,10 +876,9 @@ class PageInfoBubbleViewIsolatedWebAppBrowserTest : public DialogBrowserTest {
     std::u16string app_name = u"Google IWA";
     bubble_view->presenter_for_testing()->SetIsolatedWebAppNameForTesting(
         app_name);
-    bubble_view->presenter_for_testing()->SetSiteNameForTesting(u"google.com");
     // For Isolated Web Apps, normal site name gets overridden by app name.
     ASSERT_EQ(
-        bubble_view->presenter_for_testing()->GetSiteOriginOrAppNameToDisplay(),
+        bubble_view->presenter_for_testing()->GetSiteNameOrAppNameToDisplay(),
         app_name);
   }
 
