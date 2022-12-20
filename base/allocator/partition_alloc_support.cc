ad96172d568a946608301b3a51abbae50f6957ce
landrey <landrey@google.com>
2022-12-20 17:48:02 +0000

    Remove obsolete includes added at https://crrev.com/c/4110828
    The unguarded pcscan.h include breaks downstream WebRTC project: https://logs.chromium.org/logs/webrtc/buildbucket/cr-buildbucket/8794259521579445345/+/u/compile/stdout
    
    Error log:
    ```
    ../../base/allocator/partition_allocator/starscan/pcscan.h:250:24: error: use of undeclared identifier 'StateBitmapFromAddr'
      auto* state_bitmap = StateBitmapFromAddr(slot_start);
    ```
    
    Bug: b/263111771
    Change-Id: I0fffdd4f843049790d466a72c59b23b4ef14476b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110796
    Auto-Submit: Andrey Logvin <landrey@google.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085504}

--- base/allocator/partition_alloc_support.cc
+++ base/allocator/partition_alloc_support.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/allocator/partition_alloc_features.h"
-#include "base/allocator/partition_alloc_support.h"
 #include "base/allocator/partition_allocator/allocation_guard.h"
 #include "base/allocator/partition_allocator/dangling_raw_ptr_checks.h"
 #include "base/allocator/partition_allocator/memory_reclaimer.h"
@@ -24,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_lock.h"
 #include "base/allocator/partition_allocator/shim/allocator_shim.h"
 #include "base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_partition_alloc.h"
-#include "base/allocator/partition_allocator/starscan/pcscan.h"
 #include "base/allocator/partition_allocator/starscan/pcscan_scheduling.h"
 #include "base/allocator/partition_allocator/starscan/stack/stack.h"
 #include "base/allocator/partition_allocator/thread_cache.h"
