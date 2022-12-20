e50ea641ce73ca9aac7e23507651b6fb7bea3ca6
Jonah Ryan-Davis <jonahr@google.com>
2022-12-20 20:16:56 +0000

    Fix bugs in ANGLE backend selection
    This CL does the following:
    1. Fixes the unexpected behavior where in order to omit the --use-angle
    flag, it's necessary to have ANGLEImplementation::kDefault explicitly
    added to the allowed_impls list.
    2. Fixes the case where ANGLE is allowed to select an explicitly
    prohibited backend via omitting the --use-angle flag and choosing
    its default backend.
    3. Removes kDefault as an option from lists of allowed_impls that are
    expected to explicitly prohibit certain ANGLE backends.
    4. Cleans up allowed_impls lists that add kDefault explicitly or
    implicitly via GLImplementationEGLANGLE, and also add unnecessary
    redundant explicit ANGLE backend implementations to the same list.
    5. In cases where the GLImplementation was set programmatically on
    init (mostly tests) and it's not specified in the command-line, use
    whichever ANGLE implementation was already set.
    6. Fix a missing case where OpenGL-EGL and OpenGLES-EGL backends
    were not setting the correct GLImplementation.
    
    The effects should make ANGLE backend selection work as follows:
    - If no ANGLE backend is chosen via explicit request (ie --use-angle),
    then the allowed default backend will be chosen.
    - If an invalid ANGLE backend is chosen via explicit request, then
    we will fall back to either the default backend, or swiftshader,
    depending on the failure mechanism.
    - If a valid ANGLE backend is chosen, it will be used.
    - If either of the two following options are added to the
    allowed_impls list, then all ANGLE backends are considered valid:
      - gl::kGLImplementationEGLANGLE
      - gl::ANGLEImplementation::kDefault
    
    One noted edge case I found was the case where both ANGLE's internal
    default backend was explicitly disallowed, and the swiftshader
    backend was explicitly disallowed, then by specifying an invalid
    backend, ANGLE would fallback to initializing with the ANGLE display
    type DEFAULT, which would initialize the default backend that was
    supposed to be explicitly disallowed. Currently there aren't any
    platforms that use this setup, so this isn't an issue right now. If we
    ever need to add a platform which can only be initialized with a
    secondary backend (like vulkan or metal), we will need to change this
    behavior.
    
    Bug: chromium:1378476
    Bug: chromium:1231934
    Bug: chromium:1400043
    Change-Id: Idca3efc512494e1f7725187c58a7ea2508778df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4102520
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085609}

--- ui/gl/egl_api_unittest.cc
+++ ui/gl/egl_api_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_egl_api_implementation.h"
 #include "ui/gl/gl_surface_egl.h"
 #include "ui/gl/gl_switches.h"
+#include "ui/gl/init/gl_display_initializer.h"
 #include "ui/gl/init/gl_factory.h"
 
 namespace gl {
@@ -58,7 +59,7 @@ class EGLApiTest : public testing::Test {
         GLDisplayManagerEGL::GetInstance()->GetDisplay(GpuPreference::kDefault);
     // Clear the display so InitializeDisplay() will re-initialize it.
     display_->SetDisplay(EGL_NO_DISPLAY);
-    display_->InitializeDisplay(EGLDisplayPlatform(EGL_DEFAULT_DISPLAY));
+    init::InitializeDisplay(display_, EGLDisplayPlatform(EGL_DEFAULT_DISPLAY));
   }
 
   void SetFakeExtensionString(const char* fake_string,
