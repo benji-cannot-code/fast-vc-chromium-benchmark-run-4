495292da0c88169652525b8ea39bfcc50beb5115
Aaron Krajeski <aaronhk@chromium.org>
2022-12-20 16:32:35 +0000

    currentcolor support for color-mix
    color-mix values are generally resolved into single blink::Colors at
    computed value time. "currentcolor" cannot do this as its value is
    unknown until after style is computed.
    
    In the event that a color-mix contains "currentcolor" this CL adds
    a UnresolvedColorMix struct to StyleColor that will get
    resolved at used value time when currentcolor is available. The
    internal color in a StyleColor can now either be a blink::Color
    or a pointer to an UnresolvedColorMix.
    
    If neither color is a "currentcolor" (or a color-mix value with a
    "currentcolor") we continue to resolve color-mix values at computed value time.
    
    It is still unresolved what we'll do when the value of both percentages
    for color-mix are zero as it should get rejected at parse time but
    there are no WPT tests that cover this situation yet.
    
    Changes to third_party/blink/web_tests/external/wpt/css/css-color/parsing/color-computed-color-mix-function-expected.txt are due
    to new tests passing. New failures are all old failures just the
    message has changed.
    
    The approach uses union type for blink::Color and a unique pointer to
    an UnresolvedColorMix together. This means that there is a lot more
    boilerplate code and potentially some difficult-to-maintain custom
    memory management. A version of this CL that uses absl::variant is
    here crrev.com/c/4091814
    
    Running the size command on content_shell for linux for the various
    versions of this CL:
    Main: 146058981
    Here (union and unique ptr): 146077125 (delta: 18,144)
    crrev.com/c/4091814: 146110917 (delta: 51,936)
    
    Bug: 1333988
    Change-Id: I954f2371bc267e7912b62dbaccc363e5bdf901da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4017892
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085475}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_alternate_value.h"
 #include "third_party/blink/renderer/core/css/css_axis_value.h"
 #include "third_party/blink/renderer/core/css/css_color.h"
+#include "third_party/blink/renderer/core/css/css_color_mix_value.h"
 #include "third_party/blink/renderer/core/css/css_content_distribution_value.h"
 #include "third_party/blink/renderer/core/css/css_custom_ident_value.h"
 #include "third_party/blink/renderer/core/css/css_font_family_value.h"
@@ -63,7 +64,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/filter_operation_resolver.h"
 #include "third_party/blink/renderer/core/css/resolver/transform_builder.h"
 #include "third_party/blink/renderer/core/css/scoped_css_value.h"
+#include "third_party/blink/renderer/core/css/style_color.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
+#include "third_party/blink/renderer/core/css_value_keywords.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/style/anchor_scroll_value.h"
@@ -1944,8 +1947,24 @@ StyleColor StyleBuilderConverter::ConvertStyleColor(StyleResolverState& state,
           value_id);
     }
   }
-  // TODO(crbug.com/1362022): We will need to store an unresolved color-mix
-  // value in order to account for currentColor.
+
+  if (auto* color_mix_value = DynamicTo<cssvalue::CSSColorMixValue>(value)) {
+    const StyleColor c1 = StyleBuilderConverter::ConvertStyleColor(
+        state, color_mix_value->Color1(), for_visited_link);
+    const StyleColor c2 = StyleBuilderConverter::ConvertStyleColor(
+        state, color_mix_value->Color2(), for_visited_link);
+
+    // If neither color is "currentcolor" (or a color-mix function containing a
+    // currentcolor) then color-mix functions can be resolved right now like
+    // other colors. Otherwise we need to store an unresolved value on
+    // StyleColor.
+    if (c1.IsCurrentColor() || c1.IsUnresolvedColorMixFunction() ||
+        c2.IsCurrentColor() || c2.IsUnresolvedColorMixFunction()) {
+      return StyleColor(
+          StyleColor::UnresolvedColorMix(color_mix_value, c1, c2));
+    }
+  }
+
   return StyleColor(state.GetDocument().GetTextLinkColors().ColorFromCSSValue(
       value, Color(), state.StyleBuilder().UsedColorScheme(),
       for_visited_link));
