55e8dbcf7757b68caaf269adaf73cb551afcca0d
Alvin Ji <alvinji@chromium.org>
2022-12-20 17:42:58 +0000

    Remove Autofill.SaveCardWithFirstAndLastName histogram
    This is part of code health rotation work - Expired Histograms Clean Up.
    Autofill.SaveCardWithFirstAndLastNameComplete and
    Autofill.SaveCardWithFirstAndLastNameOffered has expired and can not be
    found in any histogram.xml. Hence this CL is used to remove it.
    
    Bug: 1384803
    Change-Id: I85d43d9350468c6ad0028199b0282e5052296df6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112764
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085499}

--- components/autofill/core/browser/data_model/credit_card.cc
+++ components/autofill/core/browser/data_model/credit_card.cc
@@ -1033,11 +1033,6 @@ std::u16string CreditCard::Expiration4DigitYearAsString() const {
   return data_util::Expiration4DigitYearAsString(expiration_year_);
 }
 
-bool CreditCard::HasFirstAndLastName() const {
-  return !temp_card_first_name_.empty() && !temp_card_last_name_.empty() &&
-         !name_on_card_.empty();
-}
-
 bool CreditCard::HasNameOnCard() const {
   return !name_on_card_.empty();
 }
