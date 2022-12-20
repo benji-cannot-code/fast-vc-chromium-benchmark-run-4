e1258862624f6972ef2e546314e0f31e8392eaea
Michele Fan <michelefan@chromium.org>
2022-12-20 03:58:23 +0000

    capture-mode-demo-tools: Set color for the knob and track
    Fix the issue that the color for the demo tools toggle button is
    not set. Demo after the fix: http://b/262933396#comment3
    
    Fixed: b:262933396
    Test: Manually
    Change-Id: I5a24ef0780673cd529d82a931655104769305ef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114636
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085276}

--- ash/capture_mode/capture_mode_menu_toggle_button.h
+++ ash/capture_mode/capture_mode_menu_toggle_button.h
@@ -38,6 +38,9 @@ class CaptureModeMenuToggleButton
       delete;
   ~CaptureModeMenuToggleButton() override;
 
+  // views::View
+  void OnThemeChanged() override;
+
   // CaptureModeSessionFocusCycler::HighlightableView:
   views::View* GetView() override;
 
@@ -46,6 +49,8 @@ class CaptureModeMenuToggleButton
  private:
   views::ImageView* icon_view_;
   views::Label* label_view_;
+
+  // Toggles between enabling and disabling the capture mode demo tools feature.
   views::ToggleButton* toggle_button_;
 };
 
