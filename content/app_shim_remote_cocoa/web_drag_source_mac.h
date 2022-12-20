c400bbe2eff7bef795ac4f27b1c96a601fb3e7d4
Avi Drissman <avi@chromium.org>
2022-12-20 21:25:37 +0000

    Followup to WebDragSource cleanup
    https://crrev.com/c/4114681 did some cleanup on WebDragSource. Follow
    up with additional fixes.
    
    Bug: 867553
    Change-Id: I0e2f6456f741ae4c5c9f7a64dc2c29cfe0fca1ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117039
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085645}

--- content/app_shim_remote_cocoa/web_drag_source_mac.h
+++ content/app_shim_remote_cocoa/web_drag_source_mac.h
@@ -10,11 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
-#include <memory>
-
 #include "base/files/file_path.h"
 #include "base/mac/scoped_nsobject.h"
 #include "content/common/content_export.h"
+#include "content/public/common/drop_data.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -30,12 +29,13 @@ class WebContentsNSViewHost;
 CONTENT_EXPORT
 @interface WebDragSource : NSObject <NSPasteboardWriting> {
  @private
-  // The host through which to communicate with the WebContentsImpl. Owns
-  // |self| and resets |host_| via clearHostAndWebContentsView.
+  // The host through which to communicate with the WebContents. Owns
+  // this object. This pointer gets reset when the WebContents goes away with
+  // `webContentsIsGone`.
   raw_ptr<remote_cocoa::mojom::WebContentsNSViewHost> _host;
 
-  // The drop data. Should only be initialized once.
-  std::unique_ptr<content::DropData> _dropData;
+  // The drop data.
+  content::DropData _dropData;
 
   // The file name to be saved to for a drag-out download.
   base::FilePath _downloadFileName;
@@ -50,9 +50,9 @@ CONTENT_EXPORT
 
 // Initialize a WebDragSource object for a drag.
 - (instancetype)initWithHost:(remote_cocoa::mojom::WebContentsNSViewHost*)host
-                    dropData:(const content::DropData*)dropData;
+                    dropData:(const content::DropData&)dropData;
 
-// Call when the web contents is gone.
+// Call when the WebContents is gone.
 - (void)webContentsIsGone;
 
 @end
