3b7f008fee7b53387849809dbb94898dd2de1816
Eriko Kurimoto <elkurin@chromium.org>
2022-12-20 19:01:18 +0000

    Tooltip: Set show tooltip delay to 500ms
    This CL modifies kDefaultShowTooltipDelay from 50ms to 500ms.
    This was unintentionally changed by crrev.com/c/4019735, so this CL
    fixes it.
    
    Change-Id: Ib0867e82d649af746e0da82e7cdc074900754ab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112739
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085546}

--- ui/views/corewm/tooltip_controller.cc
+++ ui/views/corewm/tooltip_controller.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views::corewm {
 namespace {
 
-constexpr auto kDefaultShowTooltipDelay = base::Milliseconds(50);
+constexpr auto kDefaultShowTooltipDelay = base::Milliseconds(500);
 constexpr auto kDefaultHideTooltipDelay = base::Seconds(10);
 
 // Returns true if |target| is a valid window to get the tooltip from.
