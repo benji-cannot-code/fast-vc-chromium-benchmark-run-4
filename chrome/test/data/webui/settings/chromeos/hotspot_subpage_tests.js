e10b761042994bbce8a539c3ec0356cb27d92c85
Jason Zhang <jiajunz@google.com>
2022-12-20 19:03:45 +0000

    [CrOS Hotspot] Add hotspot configure button, name and connected count
    This CL adds the hotspot name row and configure button as well as the
    connected device count row in hotspot subpage. A following CL will
    implement the on-click functionality of the button.
    
    Screenshot: https://screenshot.googleplex.com/6oZ6nsrbQusLZ65.png
    
    Bug: b/239477916
    Change-Id: Ia5db2a4337ce41a2f40ff0150bf1ab443134002d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109018
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085549}

--- chrome/test/data/webui/settings/chromeos/hotspot_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/hotspot_subpage_tests.js
@@ -136,10 +136,16 @@ suite('HotspotSubpageTest', function() {
         hotspotSubpage.shadowRoot.querySelector('#hotspotToggleText');
     const enableToggle =
         hotspotSubpage.shadowRoot.querySelector('#enableHotspotToggle');
+    const hotspotNameElement =
+        hotspotSubpage.shadowRoot.querySelector('#hotspotSSID');
+    const connectedClientCount =
+        hotspotSubpage.shadowRoot.querySelector('#connectedDeviceCount');
 
     assertEquals(
         hotspotSubpage.i18n('hotspotSummaryStateOff'),
         hotspotOnOffLabel.textContent.trim());
+    assertEquals('test_ssid', hotspotNameElement.textContent.trim());
+    assertEquals('0', connectedClientCount.textContent.trim());
     assertFalse(enableToggle.checked);
 
     // Simulate turning on hotspot.
@@ -181,5 +187,20 @@ suite('HotspotSubpageTest', function() {
         hotspotSubpage.i18n('hotspotSummaryStateOff'),
         hotspotOnOffLabel.textContent.trim());
     assertFalse(enableToggle.checked);
+
+    hotspotConfig.setFakeHotspotActiveClientCount(6);
+    await flushAsync();
+    assertEquals('6', connectedClientCount.textContent.trim());
+
+    hotspotConfig.setFakeHotspotConfig({
+      ssid: 'new_ssid',
+    });
+    await flushAsync();
+    assertEquals('new_ssid', hotspotNameElement.textContent.trim());
+
+    // Verifies UI with null hotspot config
+    hotspotConfig.setFakeHotspotConfig(null);
+    await flushAsync();
+    assertEquals('', hotspotNameElement.textContent.trim());
   });
 });
