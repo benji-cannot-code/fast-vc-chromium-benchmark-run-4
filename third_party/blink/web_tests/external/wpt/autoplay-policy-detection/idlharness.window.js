3baca188e1f1f8cac68290196385014ac6fd9e96
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-12-20 14:34:23 +0000

    Import wpt@0ff577eb30abb0f0d98a1e2bda42e455b645caf3
    Using wpt-import in Chromium 72f76c00228b4168fad20e79f9c65b7f733f5846.
    With Chromium commits locally applied on WPT:
    8ee2fbfcf3 "[WPT] Add WPT for BFCache eviction after version change event"
    184de3f1ef "Add scalabilityMode as outbound rtp stream stat."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yosin@chromium.org, pcupp@microsoft.com:
      external/wpt/editing
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Id5555b6830c391a2d718139abe52c6f7732dccfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116930
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1085413}

---
+++ third_party/blink/web_tests/external/wpt/autoplay-policy-detection/idlharness.window.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://w3c.github.io/autoplay/
+
+'use strict';
+
+idl_test(
+  ['autoplay-detection'],
+  ['html'],
+  idl_array => {}
+);
