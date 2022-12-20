2cfbdf5dcc7802d29a34d45f10d990bb2e19aea9
Aisulu <aisulu@google.com>
2022-12-20 20:16:12 +0000

    [privacy page] Enable PrivacySandbox4 feature for Privacy Page tests
    Eventually the default will be to have this flag on.
    To test the current state where the flag is off
    odd-one-out tests will be added into a special suite.
    
    Bug: 1378703, b/254410689
    Change-Id: I6d66ba46042f7de7e0a4c8812d83350066a87beb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4118473
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085608}

--- chrome/test/data/webui/settings/privacy_page_test.ts
+++ chrome/test/data/webui/settings/privacy_page_test.ts
@@ -176,6 +176,7 @@ suite('PrivacyPage', function() {
     suiteSetup(function() {
       loadTimeData.overrideValues({
         isPrivacySandboxRestricted: false,
+        isPrivacySandboxSettings4: enablePrivacySandbox4,
       });
 
       settingsPrefs = document.createElement('settings-prefs');
