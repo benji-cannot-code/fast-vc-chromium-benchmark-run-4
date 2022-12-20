f8acc4376af5dd407aeec1e8306a607551c1bcd1
conniekxu <conniekxu@chromium.org>
2022-12-20 00:57:20 +0000

    cros_next: Apply visual specs for Desk Preview View
    Apply the  visual specs for Desk Preview View Behind the Feature Flag
    Jellyroll, it includes:
    
    1. Remove the shadow of the Wallpaper Preview view.
    2. Update the corner radius of Desk Preview view and its FocusRing.
    
    Bug: b/263144360
    Change-Id: Ib32a52ac580282c91a0a21d23c4d3e8a0931d360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117450
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085220}

--- ash/wm/desks/desk_mini_view.cc
+++ ash/wm/desks/desk_mini_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "ash/accessibility/accessibility_controller_impl.h"
+#include "ash/constants/ash_features.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/close_button.h"
@@ -51,7 +52,11 @@ constexpr int kCloseButtonMargin = 8;
 
 constexpr int kMinDeskNameViewWidth = 56;
 
-constexpr int kPreviewFocusRingRadius = 6;
+constexpr int kPreviewFocusRingRadiusOld = 6;
+
+// TODO(conniekxu): After CrOS Next is launched, remove
+// `kPreviewFocusRingRadiusOld`.
+constexpr int kPreviewFocusRingRadius = 10;
 
 gfx::Rect ConvertScreenRect(views::View* view, const gfx::Rect& screen_rect) {
   gfx::Point origin = screen_rect.origin();
@@ -121,7 +126,10 @@ DeskMiniView::DeskMiniView(DesksBarView* owner_bar,
 
   views::FocusRing* preview_focus_ring = views::FocusRing::Get(desk_preview_);
   views::InstallRoundRectHighlightPathGenerator(
-      desk_preview_, gfx::Insets(kFocusRingHaloInset), kPreviewFocusRingRadius);
+      desk_preview_, gfx::Insets(kFocusRingHaloInset),
+      features::IsJellyrollEnabled() ? kPreviewFocusRingRadius
+                                     : kPreviewFocusRingRadiusOld);
+
   preview_focus_ring->SetHasFocusPredicate([&](views::View* view) {
     return (owner_bar_->dragged_item_over_bar() &&
             IsPointOnMiniView(
