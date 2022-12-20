e97c7a49ddf17f38bbe8287527bc5d268a98de8f
Kirubel Aklilu <kaklilu@chromium.org>
2022-12-20 19:10:58 +0000

    [DIPS] Migrate the DIPS db to version 2.
    Version 2 of the database will stop tracking `stateless` bounce times
    in favor of tracking the times of any bounce. This version also makes
    the DIPS db support NULL values for timestamps, instead of using
    `base::Time()` as the empty value.
    
    Bug: 1375302
    Change-Id: I852ee630724bf33f932efd79dc5e0d16e77f8c8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111780
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Reviewed-by: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085555}

--- chrome/browser/dips/dips_database.h
+++ chrome/browser/dips/dips_database.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/dips/dips_utils.h"
 #include "sql/database.h"
 #include "sql/init_status.h"
+#include "sql/meta_table.h"
 #include "sql/statement.h"
 
 // TODO(crbug.com/1342228): This is currently in-memory only. Add support for a
@@ -39,12 +40,19 @@ class DIPSDatabase {
   DIPSDatabase(const DIPSDatabase&) = delete;
   DIPSDatabase& operator=(const DIPSDatabase&) = delete;
 
+  // Updates `db_` to use the latest schema.
+  // Returns whether the migration was successful.
+  bool UpdateSchema();
+
+  // Migrates from v1 to v2 of the DIPS database schema.
+  bool MigrateToVersion2();
+
   // DIPS Bounce table functions -----------------------------------------------
   bool Write(const std::string& site,
              const TimestampRange& storage_times,
              const TimestampRange& interaction_times,
              const TimestampRange& stateful_bounce_times,
-             const TimestampRange& stateless_bounce_times);
+             const TimestampRange& bounce_times);
 
   absl::optional<StateValue> Read(const std::string& site);
 
@@ -161,6 +169,7 @@ class DIPSDatabase {
   const base::FilePath db_path_ GUARDED_BY_CONTEXT(sequence_checker_);
   std::unique_ptr<sql::Database> db_ GUARDED_BY_CONTEXT(sequence_checker_);
   raw_ptr<base::Clock> clock_ = base::DefaultClock::GetInstance();
+  sql::MetaTable meta_table_ GUARDED_BY_CONTEXT(sequence_checker_);
   mutable base::Time last_health_metrics_time_
       GUARDED_BY_CONTEXT(sequence_checker_) = base::Time::Min();
   SEQUENCE_CHECKER(sequence_checker_);
