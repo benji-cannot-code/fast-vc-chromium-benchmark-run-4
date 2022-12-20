f6cdb4919972aa003446d8396316205d8b49161f
Lijin Shen <lazzzis@google.com>
2022-12-20 21:35:11 +0000

    Fix hiding animation is cancelled by obsolete showing animation
    Per crash log, before SHOW_FRONT_ONLY is finished, REMOVE_FRONT_ONLY
    is triggered. And then somehow, REMOVE_FRONT is triggered again, thereby
    causing a crash.
    
    This might be because while hiding animation is running, the runnable
    of mContainer.runAfterinitialMessageLayout is finished and the previous
    runnable of SHOW_FRONT_ONLY is invoked such that hiding animation is
    cancelled. Therefore, mCurrentDisplayedMessages is not reset to
    [null, null].
    
    Bug: 1382275
    Change-Id: I29fec83c10e1c4d3841d5cbf02f1ae6c41ec99a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116873
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085651}

--- components/messages/android/internal/java/src/org/chromium/components/messages/MessageAnimationCoordinator.java
+++ components/messages/android/internal/java/src/org/chromium/components/messages/MessageAnimationCoordinator.java
@@ -264,7 +264,7 @@ public class MessageAnimationCoordinator implements SwipeAnimationHandler {
             mMessageQueueDelegate.onStartShowing(() -> {
                 if (candidates.get(0) == mCurrentDisplayedMessages.get(0)
                         && candidates.get(1) == mCurrentDisplayedMessages.get(1)) {
-                    triggerStackingAnimation(candidates, onFinished);
+                    triggerStackingAnimation(candidates, onFinished, mFrontAnimator, mBackAnimator);
                 }
             });
         } else if (nextFront == null) {
@@ -274,20 +274,27 @@ public class MessageAnimationCoordinator implements SwipeAnimationHandler {
                 mCurrentDisplayedMessages = new ArrayList<>(candidates);
                 onFinished.run();
             };
-            triggerStackingAnimation(candidates, runnable);
+            triggerStackingAnimation(candidates, runnable, mFrontAnimator, mBackAnimator);
         } else {
             mCurrentDisplayedMessages = new ArrayList<>(candidates);
-            triggerStackingAnimation(candidates, onFinished);
+            triggerStackingAnimation(candidates, onFinished, mFrontAnimator, mBackAnimator);
         }
     }
 
-    private void triggerStackingAnimation(List<MessageState> candidates, Runnable onFinished) {
+    private void triggerStackingAnimation(List<MessageState> candidates, Runnable onFinished,
+            Animator frontAnimator, Animator backAnimator) {
         Runnable runnable = () -> {
+            // While the runnable is waiting to be triggered, hiding animation might be triggered:
+            // while the hiding animation is running, declare this runnable as obsolete so that
+            // it won't cancel the hiding animation.
+            if (isAnimatorExpired(frontAnimator, backAnimator)) {
+                return;
+            }
             mAnimatorSet.cancel();
             mAnimatorSet.removeAllListeners();
             mAnimatorSet = new AnimatorSet();
-            mAnimatorSet.play(mFrontAnimator);
-            mAnimatorSet.play(mBackAnimator);
+            mAnimatorSet.play(frontAnimator);
+            mAnimatorSet.play(backAnimator);
             mAnimatorSet.addListener(new MessageAnimationListener(() -> {
                 mMessageQueueDelegate.onAnimationEnd();
                 onFinished.run();
@@ -300,6 +307,10 @@ public class MessageAnimationCoordinator implements SwipeAnimationHandler {
         } else {
             mContainer.runAfterInitialMessageLayout(runnable);
         }
+    }
+
+    private boolean isAnimatorExpired(Animator frontAnimator, Animator backAnimator) {
+        return mFrontAnimator != frontAnimator || mBackAnimator != backAnimator;
     }
 
     @Override
