65f685263096528d771a6e9d77a6e6a34e5a7333
Justin DeWitt <dewittj@chromium.org>
2022-12-20 00:41:36 +0000

    Remove obsolete flag FeedReliabilityLogging
    Bug: 1385901
    Change-Id: I7aff8b065a6b986c7031dde1e0c6938fa1eb897f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116070
    Auto-Submit: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085215}

--- components/feed/feed_feature_list.cc
+++ components/feed/feed_feature_list.cc
@@ -64,9 +64,6 @@ BASE_FEATURE(kDiscoFeedEndpoint,
 BASE_FEATURE(kXsurfaceMetricsReporting,
              "XsurfaceMetricsReporting",
              base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kReliabilityLogging,
-             "FeedReliabilityLogging",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kFeedHeaderStickToTop,
              "FeedHeaderStickToTop",
              base::FEATURE_DISABLED_BY_DEFAULT);
