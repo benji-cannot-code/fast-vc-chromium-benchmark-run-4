243bffcb0075b391c6590dbdb987c14a8748b3f6
Nan Lin <linnan@chromium.org>
2022-12-20 21:29:02 +0000

    For stored sources show N/A for verbose debug reporting
    As verbose debug reports will only be sent for sources during source
    registration, "Verbose debug reporting" is irrelevant for stored
    sources. Currently it's always shown as "disabled" and may be confusing.
    
    Bug: 1400645
    Change-Id: I589c113e41ab8cede086416622d33adb46b0c401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116650
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085649}

--- content/browser/attribution_reporting/attribution_internals_handler_impl.cc
+++ content/browser/attribution_reporting/attribution_internals_handler_impl.cc
@@ -54,6 +54,8 @@ namespace {
 
 using Attributability =
     ::attribution_internals::mojom::WebUISource::Attributability;
+using SourceDebugReporting =
+    ::attribution_internals::mojom::WebUISource::DebugReporting;
 
 using Empty = ::attribution_internals::mojom::Empty;
 using ReportStatus = ::attribution_internals::mojom::ReportStatus;
@@ -67,7 +69,7 @@ attribution_internals::mojom::WebUISourcePtr WebUISource(
     const std::vector<uint64_t>& dedup_keys,
     int64_t aggregatable_budget_consumed,
     const std::vector<uint64_t>& aggregatable_dedup_keys,
-    bool debug_reporting_enabled,
+    SourceDebugReporting debug_reporting_enabled,
     absl::optional<uint64_t> cleared_debug_key) {
   DCHECK_GE(aggregatable_budget_consumed, 0);
 
@@ -124,13 +126,10 @@ void ForwardSourcesToWebUI(
       }
     }
 
-    // Note that debug reporting may be enabled when the source was registered
-    // but the value was not persisted in memory. Showing "disabled" in
-    // internals UI as the value is not relevant at this point.
     web_ui_sources.push_back(WebUISource(
         source.common_info(), attributability, source.dedup_keys(),
         source.aggregatable_budget_consumed(), source.aggregatable_dedup_keys(),
-        /*debug_reporting_enabled=*/false,
+        SourceDebugReporting::kNotApplicable,
         /*cleared_debug_key=*/absl::nullopt));
   }
 
@@ -329,7 +328,9 @@ void AttributionInternalsHandlerImpl::OnSourceHandled(
   auto web_ui_source =
       WebUISource(source.common_info(), attributability, /*dedup_keys=*/{},
                   /*aggregatable_budget_consumed=*/0,
-                  /*aggregatable_dedup_keys=*/{}, source.debug_reporting(),
+                  /*aggregatable_dedup_keys=*/{},
+                  source.debug_reporting() ? SourceDebugReporting::kEnabled
+                                           : SourceDebugReporting::kDisabled,
                   cleared_debug_key);
 
   for (auto& observer : observers_) {
