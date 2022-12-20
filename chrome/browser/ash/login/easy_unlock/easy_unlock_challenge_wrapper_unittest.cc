d34c184162ffef93784e068e4fd306367fec6901
Chris Fredrickson <cfredric@chromium.org>
2022-12-20 19:21:45 +0000

    Remove SaveResult helper in favor of TestFuture.
    Change-Id: I5faa4dae522712baf556333b1e4159e6a1f103a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114348
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085566}

--- chrome/browser/ash/login/easy_unlock/easy_unlock_challenge_wrapper_unittest.cc
+++ chrome/browser/ash/login/easy_unlock/easy_unlock_challenge_wrapper_unittest.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/easy_unlock/easy_unlock_challenge_wrapper.h"
 
 #include "base/bind.h"
+#include "base/test/task_environment.h"
+#include "base/test/test_future.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/securemessage/proto/securemessage.pb.h"
 
@@ -20,10 +22,6 @@ const char kChallenge[] = "challenge";
 const char kChannelBindingData[] = "channel binding data";
 const char kUserId[] = "user id";
 const char kSignature[] = "signature";
-
-void SaveResult(std::string* result_out, const std::string& result) {
-  *result_out = result;
-}
 
 class TestableEasyUnlockChallengeWrapper : public EasyUnlockChallengeWrapper {
  public:
@@ -62,12 +60,15 @@ class TestableEasyUnlockChallengeWrapper : public EasyUnlockChallengeWrapper {
 
     std::move(callback).Run(kSignature);
   }
+
+  base::test::SingleThreadTaskEnvironment env_;
 };
 
 TEST(EasyUnlockChallengeWrapperTest, TestWrapChallenge) {
   TestableEasyUnlockChallengeWrapper wrapper;
-  std::string wrapped_challenge;
-  wrapper.WrapChallenge(base::BindOnce(&SaveResult, &wrapped_challenge));
+  base::test::TestFuture<const std::string&> future;
+  wrapper.WrapChallenge(future.GetCallback());
+  std::string wrapped_challenge = future.Take();
 
   securemessage::SecureMessage challenge_secure_message;
   ASSERT_TRUE(challenge_secure_message.ParseFromString(wrapped_challenge));
