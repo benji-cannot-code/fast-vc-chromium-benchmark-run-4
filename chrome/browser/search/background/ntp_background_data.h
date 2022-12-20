b5d70967ed15fef14c7531d9718370f43d1f4710
Paul Adedeji <pauladedeji@google.com>
2022-12-20 19:12:03 +0000

    [ntp][side-panel] Add snapshot URL to NTP background dictionary
    Adds snapshot URL to NTP background dictionary whenever image is set. Before this change, we used the same image URL as the one used to set the background of the NTP. It has a resolution of 3840x2160 so it takes too long to render in the side panel. We cannot use the same url as the preview images as they require a different aspect ratio.
    
    screenshot/7re3wsJ2dTztx7z.png
    
    Change-Id: I1aa6b5d17ea05e69e52bc53fc1e3a1dbdcf81f51
    Bug: 4074588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108085
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085556}

--- chrome/browser/search/background/ntp_background_data.h
+++ chrome/browser/search/background/ntp_background_data.h
@@ -121,6 +121,9 @@ struct CustomBackground {
   // Url of the custom background selected by the user.
   GURL custom_background_url;
 
+  // Url of snapshot for the custom background selected by the user.
+  GURL custom_background_snapshot_url;
+
   // Whether the image is a local resource.
   bool is_uploaded_image;
 
