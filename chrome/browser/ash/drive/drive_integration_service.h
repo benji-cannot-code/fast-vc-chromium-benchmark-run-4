a545bf5aaa066afc1480985b4486da45a8d1d2d2
François Degros <fdegros@chromium.org>
2022-12-20 03:49:14 +0000

    Display notification if DriveFS data dir was removed while user was logged out
    If the DriveFS data directory is not there anymore although DriveFS was
    successfully started in the past, then a system notification is
    displayed. This notification informs the user that their cached DriveFS
    files have been cleared.
    
    BUG=b:262647301, b:262814091
    
    Change-Id: I4ae6cf7f9550543d987fdff91f4b97b33baff6d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113123
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085274}

--- chrome/browser/ash/drive/drive_integration_service.h
+++ chrome/browser/ash/drive/drive_integration_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/feature_list.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/singleton.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -305,13 +306,17 @@ class DriveIntegrationService : public KeyedService,
   // Must be called on UI thread.
   bool IsDriveEnabled();
 
+  enum class DirResult { kError, kExisting, kCreated };
+  static DirResult EnsureDirectoryExists(const base::FilePath& data_dir);
+
   // Registers remote file system for drive mount point. If DriveFS is enabled,
   // but not yet mounted, this will start it mounting and wait for it to
   // complete before adding the mount point.
   void AddDriveMountPoint();
 
   // Mounts Drive if the directory exists.
-  void MaybeMountDrive(bool data_directory_exists);
+  void MaybeMountDrive(const base::FilePath& data_dir,
+                       DirResult data_dir_result);
 
   // Registers remote file system for drive mount point.
   bool AddDriveMountPointAfterMounted();
@@ -417,6 +422,8 @@ class DriveIntegrationService : public KeyedService,
   // Note: This should remain the last member so it'll be destroyed and
   // invalidate its weak pointers before any other members are destroyed.
   base::WeakPtrFactory<DriveIntegrationService> weak_ptr_factory_{this};
+
+  FRIEND_TEST_ALL_PREFIXES(DriveIntegrationServiceTest, EnsureDirectoryExists);
 };
 
 // Singleton that owns all instances of DriveIntegrationService and
