29b3927436bb4e6fd76735a96b229de207a84c64
Alan Zhao <ayzhao@google.com>
2022-12-21 19:45:29 +0000

    [clang] Attempt to fix plugin compilation errors
    After https://reviews.llvm.org/D140332 was submitted, the parameter type
    for the file parameter in clang::PPCallbacks::InclusionDirective(...)
    was changed, so we need to update the override in
    IncludeFinderPPCallbacks
    
    Bug: 1402111
    Change-Id: Iae566b01e4186d5185130016f30ff3c2e595fd18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120913
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Alan Zhao <ayzhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086030}

--- tools/clang/translation_unit/TranslationUnitGenerator.cpp
+++ tools/clang/translation_unit/TranslationUnitGenerator.cpp
@@ -62,7 +62,11 @@ class IncludeFinderPPCallbacks : public clang::PPCallbacks {
                           llvm::StringRef file_name,
                           bool is_angled,
                           clang::CharSourceRange range,
+#ifdef LLVM_FORCE_HEAD_REVISION
+                          clang::OptionalFileEntryRef file,
+#else
                           llvm::Optional<clang::FileEntryRef> file,
+#endif
                           llvm::StringRef search_path,
                           llvm::StringRef relative_path,
                           const clang::Module* imported,
@@ -159,7 +163,11 @@ void IncludeFinderPPCallbacks::InclusionDirective(
     llvm::StringRef file_name,
     bool is_angled,
     clang::CharSourceRange range,
+#ifdef LLVM_FORCE_HEAD_REVISION
+    clang::OptionalFileEntryRef file,
+#else
     llvm::Optional<clang::FileEntryRef> file,
+#endif
     llvm::StringRef search_path,
     llvm::StringRef relative_path,
     const clang::Module* imported,
