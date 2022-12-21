8ad2f214a1ddeb3a7c453447a445a670cdc877ca
Wissem Gamra <wiss@chromium.org>
2022-12-21 21:56:55 +0000

    [Code Health] Cormorant: test CreatorMediator
    We set the follow and following click handlers in the mediator, this CL tests that the setup is correct.
    
    Bug: 1377140
    Change-Id: Ia77d7c2a38c4ce42f1c66fe1fc36916cfc069463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4118816
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Wissem Gamra <wiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086100}

--- chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorCoordinator.java
+++ chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorCoordinator.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.OnScrollListener;
 
@@ -199,6 +200,11 @@ public class CreatorCoordinator implements FeedAutoplaySettingsDelegate,
 
     public ViewGroup getView() {
         return mCreatorViewGroup;
+    }
+
+    @VisibleForTesting
+    public View getProfileView() {
+        return mProfileView;
     }
 
     public PropertyModel getCreatorModel() {
