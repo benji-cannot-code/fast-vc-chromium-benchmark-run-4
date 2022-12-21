da900bdafe52ca55d238ffc01d1593886c037645
Susanne Westphal <swestphal@google.com>
2022-12-21 15:55:05 +0000

    [WebEngine] Add test activity for back navigations
    Bug: 1402447
    Change-Id: I5049fa98b930913c288c65165d6871bc613fa9ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114036
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085928}

--- weblayer/shell/android/webengine_shell_apk/src/org/chromium/webengine/shell/WebEngineShellActivity.java
+++ weblayer/shell/android/webengine_shell_apk/src/org/chromium/webengine/shell/WebEngineShellActivity.java
@@ -230,6 +230,9 @@ public class WebEngineShellActivity extends AppCompatActivity {
                     case 1:
                         intent = new Intent(activity, WebEngineStateTestActivity.class);
                         break;
+                    case 2:
+                        intent = new Intent(activity, WebEngineNavigationTestActivity.class);
+                        break;
                     default:
                         assert false : "Unhandled item: " + String.valueOf(pos);
                         intent = null;
