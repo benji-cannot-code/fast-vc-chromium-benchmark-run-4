3addf620eed512ab562e0b2760cda6f7e0be7a22
Susanne Westphal <swestphal@google.com>
2022-12-21 15:15:14 +0000

    [WebEngine] Add new Test Activity for WebEngine/UI state
    - Add drop down menu to select test Activity
    - Cleanup shell activity from additional buttons
    - Add new Activity for UI/WebEngine state tests
    - Introduce default tab observer class that provides basic logging for test to be used Activities
    
    Bug: 1378606, 1402447
    Change-Id: I332387d0e3b0aa8cbf88a14d8605465b6d6a9544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4092348
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085904}

---
+++ weblayer/shell/android/webengine_shell_apk/src/org/chromium/webengine/shell/DefaultObservers.java
@@ -0,0 +1,87 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.webengine.shell;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.chromium.base.Log;
+import org.chromium.webengine.Navigation;
+import org.chromium.webengine.NavigationObserver;
+import org.chromium.webengine.Tab;
+import org.chromium.webengine.TabListObserver;
+import org.chromium.webengine.TabObserver;
+
+/**
+ * Default observers for Test Acivites.
+ */
+public class DefaultObservers {
+    private static final String TAG = "WEDefaultObservers";
+
+    static class DefaultTabObserver extends TabObserver {
+        @Override
+        public void onVisibleUriChanged(@NonNull String uri) {
+            Log.i(TAG, "received Tab Event: 'onVisibleUriChanged(" + uri + ")'");
+        }
+
+        @Override
+        public void onTitleUpdated(@NonNull String title) {
+            Log.i(TAG, "received Tab Event: 'onTitleUpdated(" + title + ")'");
+        }
+
+        @Override
+        public void onRenderProcessGone() {
+            Log.i(TAG, "received Tab Event: 'onRenderProcessGone()'");
+        }
+    }
+
+    static class DefaultNavigationObserver extends NavigationObserver {
+        @Override
+        public void onNavigationFailed(@NonNull Navigation navigation) {
+            Log.i(TAG, "received NavigationEvent: 'onNavigationFailed()';");
+            Log.i(TAG,
+                    "Navigation: url:" + navigation.getUri()
+                            + ", HTTP-StatusCode: " + navigation.getStatusCode()
+                            + ", samePage: " + navigation.isSameDocument());
+        }
+
+        @Override
+        public void onNavigationCompleted(@NonNull Navigation navigation) {
+            Log.i(TAG, "received NavigationEvent: 'onNavigationCompleted()';");
+            Log.i(TAG,
+                    "Navigation: url:" + navigation.getUri()
+                            + ", HTTP-StatusCode: " + navigation.getStatusCode()
+                            + ", samePage: " + navigation.isSameDocument());
+        }
+    }
+
+    static class DefaultTabListObserver extends TabListObserver {
+        @Override
+        public void onActiveTabChanged(@Nullable Tab activeTab) {
+            Log.i(TAG, "received TabList Event: 'onActiveTabChanged'-event");
+        }
+
+        @Override
+        public void onTabAdded(@NonNull Tab tab) {
+            Log.i(TAG, "received TabList Event: 'onTabAdded'-event");
+
+            // Recursively add tab and navigation observers to any new tab.
+            tab.registerTabObserver(new DefaultTabObserver());
+            tab.getNavigationController().registerNavigationObserver(
+                    new DefaultNavigationObserver());
+        }
+
+        @Override
+        public void onTabRemoved(@NonNull Tab tab) {
+            Log.i(TAG, "received TabList Event: 'onTabRemoved'-event");
+        }
+
+        @Override
+        public void onWillDestroyFragmentAndAllTabs() {
+            Log.i(TAG, "received TabList Event: 'onWillDestroyFragmentAndAllTabs'-event");
+        }
+    }
+}
