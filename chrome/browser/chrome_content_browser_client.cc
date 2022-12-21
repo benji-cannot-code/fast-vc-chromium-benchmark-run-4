a6206fa099d836a0e2f33c4a3359e69e1337c2cc
Chris Thompson <cthomp@chromium.org>
2022-12-21 01:26:05 +0000

    Reland "Initial version of HTTPS-First Mode V2"
    This is a reland of commit f5cf5631e5c6ed4531c07adf6dcdb373edc65e5f
    
    crrev.com/c/4114422 was reverted due to the linux-bfcache-rel bots
    failing on the new InterstitialFallbackMaintainsHistory test. This
    reland disables that test for now, until a more comprehensive fix for
    crbug.com/1272781 is complete.
    
    Original change's description:
    > Initial version of HTTPS-First Mode V2
    >
    > This adds a rearchitected version of HTTPS-First Mode to address some
    > edge cases that occurred in the initial version, behind a new
    > kHttpsFirstModeV2 flag. The new version cancels the upgraded
    > inavigation when it fails (or times out) and initiates a new fallback
    > HTTP navigation. That fallback navigation is then replaced with the
    > HTTPS-First Mode interstitial warning. This addresses a number of
    > issues such as showing the HTTPS URL when the interstitial is showing
    > (crbug.com/1257272), sometimes losing history entries when going
    > back/forward from an HTTPS-First Mode interstitial
    > (crbug.com/crbug.com/1272781), and will allow follow-up work to better
    > handle interactions with network errors (crbug.com/1277211) among
    > others.
    >
    > This rearchitecture will also allow us to more easily add the new
    > HTTPS Upgrades feature
    > (https://chromestatus.com/feature/6056181032812544).
    >
    > Bug: 1394910,1257272,1272781,1277211
    > Change-Id: I787ecff185ab78099c04b458441c302cc254eb06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114422
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Commit-Queue: Chris Thompson <cthomp@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1084642}
    
    Bug: 1394910,1257272,1272781,1277211
    Change-Id: I424d515f095c97c2fd6f7ae2a0aa0ac09f0eaf5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116048
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Auto-Submit: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085756}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -138,6 +138,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ssl/https_defaulted_callbacks.h"
 #include "chrome/browser/ssl/https_only_mode_navigation_throttle.h"
 #include "chrome/browser/ssl/https_only_mode_upgrade_interceptor.h"
+#include "chrome/browser/ssl/https_upgrades_interceptor.h"
+#include "chrome/browser/ssl/https_upgrades_navigation_throttle.h"
 #include "chrome/browser/ssl/sct_reporting_service.h"
 #include "chrome/browser/ssl/ssl_client_auth_metrics.h"
 #include "chrome/browser/ssl/ssl_client_certificate_selector.h"
@@ -5072,11 +5074,19 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
 #endif
 
   if (profile && profile->GetPrefs()) {
-    MaybeAddThrottle(
-        HttpsOnlyModeNavigationThrottle::MaybeCreateThrottleFor(
-            handle, std::make_unique<ChromeSecurityBlockingPageFactory>(),
-            profile->GetPrefs()),
-        &throttles);
+    if (base::FeatureList::IsEnabled(features::kHttpsFirstModeV2)) {
+      MaybeAddThrottle(
+          HttpsUpgradesNavigationThrottle::MaybeCreateThrottleFor(
+              handle, std::make_unique<ChromeSecurityBlockingPageFactory>(),
+              profile->GetPrefs()),
+          &throttles);
+    } else {
+      MaybeAddThrottle(
+          HttpsOnlyModeNavigationThrottle::MaybeCreateThrottleFor(
+              handle, std::make_unique<ChromeSecurityBlockingPageFactory>(),
+              profile->GetPrefs()),
+          &throttles);
+    }
   }
 
   MaybeAddThrottle(MaybeCreateNavigationAblationThrottle(handle), &throttles);
@@ -5903,7 +5913,10 @@ ChromeContentBrowserClient::WillCreateURLLoaderRequestInterceptors(
   interceptors.push_back(
       std::make_unique<SearchPrefetchURLLoaderInterceptor>(frame_tree_node_id));
 
-  if (base::FeatureList::IsEnabled(features::kHttpsOnlyMode)) {
+  if (base::FeatureList::IsEnabled(features::kHttpsFirstModeV2)) {
+    interceptors.push_back(
+        std::make_unique<HttpsUpgradesInterceptor>(frame_tree_node_id));
+  } else {
     interceptors.push_back(
         std::make_unique<HttpsOnlyModeUpgradeInterceptor>(frame_tree_node_id));
   }
