e92432bf3c0b3d7f99d5263169a484cc3ec43662
Sarah Krakowiak <skrakowi@chromium.org>
2022-12-21 19:20:42 +0000

    Create scripts that fetch SB API, generate pb, and push to GCS.
    In this CL, we have created three scripts. The first script calls the Safe Browsing API to fetch the allowlisted URL hash prefixes and store them in the asciipb file. The second script uses BinaryProtoGenerator to create a new protobuf with the version id, scheme id, and url hashes provided in real_time_url_allowlist.asciipb. This script that generates the pb file is automatically run on build. The third script pushes this newly generated protobuf to the GCS bucket defined for this project. The first and third scripts should only be run when there are updates to the safe browsing allowlist and we need a new version of the component that includes the new allowlist updates.
    
    Bug: 1318105
    Change-Id: I5cb284bcd201992edb06a97a954266e76d9fe927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076814
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086010}

--- components/safe_browsing/android/real_time_url_checks_allowlist_resource_file_unittest.cc
+++ components/safe_browsing/android/real_time_url_checks_allowlist_resource_file_unittest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace safe_browsing {
 
 // This test validates the contents of the backup resource file,
-// components/safe_browsing/content/resources/real_time_url_checks_allowlist.pb
+// real_time_url_checks_allowlist.pb
 class RealTimeUrlChecksAllowlistResourceFileTest : public testing::Test {
  protected:
   RealTimeUrlChecksAllowlistResourceFileTest() = default;
