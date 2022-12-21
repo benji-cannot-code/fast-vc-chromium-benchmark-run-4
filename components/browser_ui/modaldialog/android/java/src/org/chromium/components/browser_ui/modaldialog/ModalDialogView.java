a8bdb434a7abbb9a343ba7e9db37b41a4c8b3734
Lijin Shen <lazzzis@google.com>
2022-12-21 18:47:47 +0000

    Make content of AutofillCardUnmaskPrompt scrollable
    Per Android doc of ScrollView, it is discouraged to add a ListView
    inside a ScrollView. So crrev.com/c/4108590 is reverted.
    
    Add a ScrollView inside autofill_card_unmask_prompt to make it
    scrollable.
    
    Demo: https://drive.google.com/open?id=1QfLkZe_HpRQtkdDYFc1UolO94DGPgx3w&authuser=0&resourcekey=0-9qvL_Gbfmr-6Evj-y0iXzA&usp=drive_link
    
    Bug: 1364816, 1402472
    Change-Id: I457e085028e685e92837e78dff938d76f798bcff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117446
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085992}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogView.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogView.java
@@ -15,11 +15,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.IntDef;
-import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
@@ -61,7 +59,7 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
     private ImageView mTitleIcon;
     private TextView mMessageParagraph1;
     private TextView mMessageParagraph2;
-    private ScrollView mCustomViewContainer;
+    private ViewGroup mCustomViewContainer;
     private ViewGroup mCustomButtonBarViewContainer;
     private View mButtonBar;
     private Button mPositiveButton;
@@ -407,10 +405,5 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
         mPositiveButton.setVisibility(positiveButtonVisible ? View.VISIBLE : View.GONE);
         mNegativeButton.setVisibility(negativeButtonVisible ? View.VISIBLE : View.GONE);
         mButtonBar.setVisibility(defaultButtonBarVisible ? View.VISIBLE : View.GONE);
-    }
-
-    @VisibleForTesting
-    public ScrollView getCustomViewContainerForTesting() {
-        return mCustomViewContainer;
     }
 }
