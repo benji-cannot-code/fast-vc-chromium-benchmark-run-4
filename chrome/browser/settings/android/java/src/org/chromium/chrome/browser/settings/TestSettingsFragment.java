f57052c08ce0a19a90d1b40869a4b53b73949f05
Wenyu Fu <wenyufu@google.com>
2022-12-21 06:40:06 +0000

    [Settings] Allow scrolling on sides of Settings
    On large screen, SettingsActivity adds padding to the side of content in order to constrain the width of the recycler view. This however creates the side effect of not able to scrolling in the padding area.
    
    This change adds the padding to the RecyclerView itself, to make the entire content area scrollable. This will however allow the divider to draw on top of the side padding too.
    
    To limit the size of divider, this change abandon the ItemDecoration created in PreferenceFragmentCompat, by setting the divider height to 0 in theme. Then we add PaddedDividerItemDecoration that allows adding paddings on the side of divider.
    
    Bug: 1293429
    Change-Id: I7acea442a03e73d9cf6f78c4173ce0d5a742c28e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114309
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085801}

---
+++ chrome/browser/settings/android/java/src/org/chromium/chrome/browser/settings/TestSettingsFragment.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
+
+/**
+ * A TestSettingsFragment that has several preference inside.
+ */
+public class TestSettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(@Nullable Bundle bundle, @Nullable String s) {
+        addPreferencesFromResource(R.xml.test_settings_fragment);
+    }
+}
