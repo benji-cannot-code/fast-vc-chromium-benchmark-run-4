7eaf6214cfd700ae7fb9666cfab712c2f479147b
Xiaochen Zhou <xiaochenzh@chromium.org>
2022-12-21 15:50:31 +0000

    FLEDGE: Resolve flaky reportEvent test.
    Addressed flaky test RunAdAuctionWithWinnerRegisterAdBeaconBuyer
    by changing URLLoaderMonitor::GetRequestInfo to URLLoaderMonitor::WaitForUrl.
    
    Verified by repeating the test for 1000 times locally. Before this
    change, it fails about 2 or 3 times per 1000 runs.
    
    The flakiness is exposed when I submitted my change which moved
    reportEvent API from the renderer processes to the browser processes:
    crrev.com/c/3991566
    
    The root cause is when URLLoaderMonitor::GetRequestInfo is called,
    the reportEvent beacon might have not been sent yet.
    
    Bug: 1402756
    Change-Id: I0b38f48cf71f815e80de5bb29ce7ed245a333227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117245
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085926}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -10150,7 +10150,7 @@ interestGroupBuyers: [$1],
                                         "/interest_group/decision_logic.js"))));
 
   absl::optional<network::ResourceRequest> request =
-      url_loader_monitor.GetRequestInfo(
+      url_loader_monitor.WaitForUrl(
           https_server_->GetURL("d.test", "/echoall?report_win_beacon"));
   ASSERT_TRUE(request);
   EXPECT_EQ(net::HttpRequestHeaders::kPostMethod, request->method);
