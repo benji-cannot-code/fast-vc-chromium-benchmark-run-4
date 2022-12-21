9fcedf3b392a072498596bfe99ce55155f131fba
Pu Shi <pushi@google.com>
2022-12-21 02:21:59 +0000

    [PhoneHub][Exo] Convert app names to lower case before sorting
    GPay app was incorrectly sorted due to capital cases. Converting app
    names to lower cases before sorting to avoid the issue.
    
    Before: https://screenshot.googleplex.com/ABjfonyNT44pGxi.png
    After: https://screenshot.googleplex.com/UG4SwmJTU4bJL5T.png
    
    Test: manually tested
    Change-Id: Iec88bf74b2b82443261ad2250c531d5176792033
    Fixed: b/262921390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119153
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Abbas Nayebi <nayebi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085773}

--- chromeos/ash/components/phonehub/app_stream_launcher_data_model.cc
+++ chromeos/ash/components/phonehub/app_stream_launcher_data_model.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/phonehub/app_stream_launcher_data_model.h"
 
+#include "base/i18n/case_conversion.h"
 #include "chromeos/ash/components/multidevice/logging/logging.h"
 #include "chromeos/ash/components/phonehub/notification.h"
 
@@ -49,11 +50,14 @@ void AppStreamLauncherDataModel::SetAppList(
   apps_list_sorted_by_name_ = streamable_apps;
 
   // Alphabetically sort the app list.
-  std::sort(apps_list_sorted_by_name_.begin(), apps_list_sorted_by_name_.end(),
-            [](const Notification::AppMetadata& a,
-               const Notification::AppMetadata& b) {
-              return a.visible_app_name < b.visible_app_name;
-            });
+  std::sort(
+      apps_list_sorted_by_name_.begin(), apps_list_sorted_by_name_.end(),
+      [](const Notification::AppMetadata& a,
+         const Notification::AppMetadata& b) {
+        std::u16string a_app_name = base::i18n::ToLower(a.visible_app_name);
+        std::u16string b_app_name = base::i18n::ToLower(b.visible_app_name);
+        return a_app_name < b_app_name;
+      });
   for (auto& observer : observer_list_)
     observer.OnAppListChanged();
 }
