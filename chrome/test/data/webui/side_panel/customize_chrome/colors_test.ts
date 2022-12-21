a37c31d737d479667e99519fa4e478699dc4d966
Tibor Goldschwendt <tiborg@chromium.org>
2022-12-21 23:51:59 +0000

    [cc-side-panel] Move colors focus test to an interactive UI test
    Running tests that require focus in browser tests is flaky. Interactive
    UI tests on the other hand can handle focus properly.
    
    Bug: 1402689
    Change-Id: I903a0af21962e4da15bd8b6ff3cac3a83026bc6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119013
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086154}

--- chrome/test/data/webui/side_panel/customize_chrome/colors_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/colors_test.ts
@@ -158,16 +158,6 @@ suite('ColorsTest', () => {
     assertEquals(3, handler.getArgs('setSeedColor')[0].value);
   });
 
-  test('opens color picker', () => {
-    const focus = capture(colorsElement.$.colorPicker, 'focus');
-    const click = capture(colorsElement.$.colorPicker, 'click');
-
-    colorsElement.$.customColor.click();
-
-    assertTrue(focus.received);
-    assertTrue(click.received);
-  });
-
   test('sets custom color', () => {
     colorsElement.$.colorPicker.value = '#ff0000';
     colorsElement.$.colorPicker.dispatchEvent(new Event('change'));
@@ -363,7 +353,6 @@ suite('ColorsTest', () => {
       callbackRouter.setTheme(theme);
       await callbackRouter.$.flushForTesting();
       await waitAfterNextRender(colorsElement);
-      const focus = capture(colorsElement.$.colorPicker, 'focus');
       const click = capture(colorsElement.$.colorPicker, 'click');
 
       $$<HTMLElement>(colorsElement, selector)!.click();
@@ -375,7 +364,6 @@ suite('ColorsTest', () => {
       assertTrue(managedDialog.$.dialog.open);
       assertEquals(0, handler.getCallCount('setDefaultColor'));
       assertEquals(0, handler.getCallCount('setSeedColor'));
-      assertFalse(focus.received);
       assertFalse(click.received);
     });
   });
