4eac7012460030f0f5f8161a61121f61b8759bbf
Jaroslav Sevcik <jarin@chromium.org>
2022-12-21 09:35:12 +0000

    Reland "[inspector] Introduce a restricted instrumentation pause"
    This is a reland of commit f96df25cdfcaff34c828612347532d9e8d672d9f
    
    Compared to crrev.com/c/3891025, this patch only disables the
    Devtools V8 session when the connection to the IO session is closed.
    This fixes the situation when the IO session was closed ona V8
    interrupt during painting debugger overlays and the devtools
    session object was disposed while still being on stack. With this
    patch, we only disable the v8 session during the v8 interrupt;
    the full devtools session cleanup is performed on a normal message
    loop handler.
    
    Original change's description:
    > [inspector] Introduce a restricted instrumentation pause
    >
    > We introduce a new instrumentation pause state in the renderer.
    > The instrumentation pause is entered on script creation
    > if the instrumentation breakpoints are enabled with
    > Debugger.setInstrumentationBreakpoint. The purpose of the pause
    > is to give the debugger a chance to insert its breakpoints
    > correctly before the script runs.
    >
    > In this patch, we restrict the messages that are handled during
    > the instrumentation pause to the ones that are handled by the
    > devtools IO session (those are mostly breakpoint management
    > messages). Handling of other messages are postponed until the
    > instrumentation pause resumes.
    >
    > Without this patch, the instrumentation pause behaves the same
    > as the normal pause, causing some undesirable interactions
    > with layouting, navigation, etc. (e.g., see crbug.com/1304095).
    >
    > Note that the instrumentation breakpoints/pauses are
    > an experiment in DevTools,  currently off by default.
    >
    > Bug: crbug.com/1354043
    > Change-Id: I44e9e8473fcb10a2868974aa8d20a07e9b00130d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891025
    > Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1079116}
    
    Bug: crbug.com/1354043
    Change-Id: I9584b4699426e9f0392b8dba336a96f37f00d87f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110931
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085832}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/debugger/instrumentation-reload.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Tests reloading during instrumentation pause.`);
+
+  await dp.Target.setDiscoverTargets({discover: true});
+  await dp.Page.enable();
+  await dp.Debugger.enable();
+  await dp.Debugger.setInstrumentationBreakpoint(
+      {instrumentation: 'beforeScriptExecution'});
+
+  const result = session.evaluate('42');
+
+  const expressionPause = await dp.Debugger.oncePaused();
+  testRunner.log(`paused reason: ${expressionPause.params.reason}`);
+
+  const navigated = dp.Page.onceFrameNavigated();
+  const reloadPromise = dp.Page.reload();
+
+  dp.Debugger.resume({terminateOnresume: false});
+  await dp.Debugger.onceResumed();
+  testRunner.log(`resumed`);
+
+  await reloadPromise;
+  testRunner.log(`reloaded`);
+
+  testRunner.log(`frame navigated ${(await navigated).params.frame.url}`);
+
+  testRunner.completeTest();
+})
