8f332ebe924d0ce702750d59ba604e99561bc408
Celia He <celiahe@google.com>
2022-12-21 01:06:21 +0000

    Add a skeleton KeywordRanker
    A skeleton ranker that aims to boost scores that contains certain keyword in the future.
    
    Bug: b/263059094
    Change-Id: I81b31cae2c7e58c90792d213c42b949067e0c291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112867
    Commit-Queue: Celia He <celiahe@google.com>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085750}

---
+++ chrome/browser/ash/app_list/search/ranking/keyword_ranker.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/app_list/search/ranking/keyword_ranker.h"
+
+namespace app_list {
+
+KeywordRanker::KeywordRanker() = default;
+
+KeywordRanker::~KeywordRanker() = default;
+
+void KeywordRanker::Start(const std::u16string& query,
+                          ResultsMap& results,
+                          CategoriesList& categories) {
+  // TODO(b/263059094): when the user start input, this function will
+  // be called.
+}
+
+void KeywordRanker::UpdateResultRanks(ResultsMap& results,
+                                      ProviderType provider) {
+  // TODO(b/263059094): update the result by boost the scores that
+  // match certain keywords, the rest remain unchanged
+}
+
+}  // namespace app_list
