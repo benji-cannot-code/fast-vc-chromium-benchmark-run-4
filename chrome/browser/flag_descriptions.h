c79e0ce7e5ea3250e7df6eaa100e6325c13711b8
Lauren Commeignes <laurencom@chromium.org>
2022-12-21 00:03:47 +0000

    Add two flags to downweight autocomplete search results with fuzzy string match
    Low-Coverage-Reason: low coverage on search features was pre-existing for IsLauncherKeywordExtractionScoringEnabled
    
    Bug: b/260764125
    Change-Id: I7e36595c874ee413a1f5c257d1514a6b36d19f47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4105142
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085723}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3487,6 +3487,9 @@ extern const char kLauncherGameSearchDescription[];
 extern const char kLauncherKeywordExtractionScoring[];
 extern const char kLauncherKeywordExtractionScoringDescription[];
 
+extern const char kLauncherFuzzyMatchForOmniboxName[];
+extern const char kLauncherFuzzyMatchForOmniboxDescription[];
+
 extern const char kMacAddressRandomizationName[];
 extern const char kMacAddressRandomizationDescription[];
 
