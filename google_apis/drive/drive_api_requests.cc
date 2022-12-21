b4aedf2def4097c90279def4e91325fa12bea6d8
Elaine Chien <elainechien@chromium.org>
2022-12-21 14:25:17 +0000

    [Code Health] Remove DictionaryValue usage from google_apis/drive/
    Bug: 1187028
    Change-Id: I2f33b44328f48507b12b59ee754cf7978b1bbfd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114377
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085891}

--- google_apis/drive/drive_api_requests.cc
+++ google_apis/drive/drive_api_requests.cc
@@ -127,8 +127,7 @@ std::string CreateMultipartUploadMetadataJson(
   // Fill parent link.
   if (!parent_resource_id.empty()) {
     base::Value::List parents;
-    parents.Append(base::Value::FromUniquePtrValue(
-        google_apis::util::CreateParentValue(parent_resource_id)));
+    parents.Append(google_apis::util::CreateParentValue(parent_resource_id));
     root.Set("parents", base::Value(std::move(parents)));
   }
 
@@ -732,8 +731,7 @@ bool InitiateUploadNewFileRequest::GetContentData(
 
   // Fill parent link.
   base::Value::List parents;
-  parents.Append(base::Value::FromUniquePtrValue(
-      util::CreateParentValue(parent_resource_id_)));
+  parents.Append(util::CreateParentValue(parent_resource_id_));
   root.Set("parents", base::Value(std::move(parents)));
 
   if (!modified_date_.is_null())
@@ -796,8 +794,7 @@ bool InitiateUploadExistingFileRequest::GetContentData(
   base::Value::Dict root;
   if (!parent_resource_id_.empty()) {
     base::Value::List parents;
-    parents.Append(base::Value::FromUniquePtrValue(
-        util::CreateParentValue(parent_resource_id_)));
+    parents.Append(util::CreateParentValue(parent_resource_id_));
     root.Set("parents", base::Value(std::move(parents)));
   }
 
--- google_apis/drive/request_util.h
+++ google_apis/drive/request_util.h
@@ -8,10 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
-
-namespace base {
-class DictionaryValue;
-}
+#include "base/values.h"
 
 namespace google_apis {
 namespace util {
@@ -25,8 +22,7 @@ extern const char kContentTypeApplicationJson[];
 std::string GenerateIfMatchHeader(const std::string& etag);
 
 // Creates a Parent value which can be used as a part of request body.
-std::unique_ptr<base::DictionaryValue> CreateParentValue(
-    const std::string& file_id);
+base::Value::Dict CreateParentValue(const std::string& file_id);
 
 }  // namespace util
 }  // namespace google_apis
