501ee74fad81a3a3ff17e580cbfa05963c3ba853
Tom Sepez <tsepez@chromium.org>
2022-12-21 19:06:22 +0000

    Improve raw_ptr increment/decrement tests
    Run tests against heap-based objects so that the BRP/PA algorithms
    are more thoroughly exercised.
    
    Change-Id: I92f47ed57fbe5645d872535eb748bf16cc580de3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119546
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086004}

--- base/memory/raw_ptr_unittest.cc
+++ base/memory/raw_ptr_unittest.cc
@@ -889,8 +889,8 @@ TEST_F(RawPtrTest, StdSwap) {
 }
 
 TEST_F(RawPtrTest, PostIncrementOperator) {
-  int foo[] = {42, 43, 44, 45};
-  CountingRawPtr<int> ptr = foo;
+  std::vector<int> foo({42, 43, 44, 45});
+  CountingRawPtr<int> ptr = &foo[0];
   for (int i = 0; i < 4; ++i) {
     ASSERT_EQ(*ptr++, 42 + i);
   }
@@ -903,7 +903,7 @@ TEST_F(RawPtrTest, PostIncrementOperator) {
 }
 
 TEST_F(RawPtrTest, PostDecrementOperator) {
-  int foo[] = {42, 43, 44, 45};
+  std::vector<int> foo({42, 43, 44, 45});
   CountingRawPtr<int> ptr = &foo[3];
   for (int i = 3; i >= 0; --i) {
     ASSERT_EQ(*ptr--, 42 + i);
@@ -917,8 +917,8 @@ TEST_F(RawPtrTest, PostDecrementOperator) {
 }
 
 TEST_F(RawPtrTest, PreIncrementOperator) {
-  int foo[] = {42, 43, 44, 45};
-  CountingRawPtr<int> ptr = foo;
+  std::vector<int> foo({42, 43, 44, 45});
+  CountingRawPtr<int> ptr = &foo[0];
   for (int i = 0; i < 4; ++i, ++ptr) {
     ASSERT_EQ(*ptr, 42 + i);
   }
@@ -931,7 +931,7 @@ TEST_F(RawPtrTest, PreIncrementOperator) {
 }
 
 TEST_F(RawPtrTest, PreDecrementOperator) {
-  int foo[] = {42, 43, 44, 45};
+  std::vector<int> foo({42, 43, 44, 45});
   CountingRawPtr<int> ptr = &foo[3];
   for (int i = 3; i >= 0; --i, --ptr) {
     ASSERT_EQ(*ptr, 42 + i);
@@ -945,8 +945,8 @@ TEST_F(RawPtrTest, PreDecrementOperator) {
 }
 
 TEST_F(RawPtrTest, PlusEqualOperator) {
-  int foo[] = {42, 43, 44, 45};
-  CountingRawPtr<int> ptr = foo;
+  std::vector<int> foo({42, 43, 44, 45});
+  CountingRawPtr<int> ptr = &foo[0];
   for (int i = 0; i < 4; i += 2, ptr += 2) {
     ASSERT_EQ(*ptr, 42 + i);
   }
@@ -959,8 +959,8 @@ TEST_F(RawPtrTest, PlusEqualOperator) {
 }
 
 TEST_F(RawPtrTest, PlusEqualOperatorTypes) {
-  int foo[] = {42, 43, 44, 45};
-  CountingRawPtr<int> ptr = foo;
+  std::vector<int> foo({42, 43, 44, 45});
+  CountingRawPtr<int> ptr = &foo[0];
   ASSERT_EQ(*ptr, 42);
   ptr += 2;  // Positive literal.
   ASSERT_EQ(*ptr, 44);
@@ -973,7 +973,7 @@ TEST_F(RawPtrTest, PlusEqualOperatorTypes) {
 }
 
 TEST_F(RawPtrTest, MinusEqualOperator) {
-  int foo[] = {42, 43, 44, 45};
+  std::vector<int> foo({42, 43, 44, 45});
   CountingRawPtr<int> ptr = &foo[3];
   for (int i = 3; i >= 0; i -= 2, ptr -= 2) {
     ASSERT_EQ(*ptr, 42 + i);
