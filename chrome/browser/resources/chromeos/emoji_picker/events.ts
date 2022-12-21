30f5b313e430cb42e8f738aae3d7de0a31177ac4
Kaiqi Liang <kaiqiliang@google.com>
2022-12-21 00:14:44 +0000

    Change string to CategoryEnum for better typing
    Bug: None
    Change-Id: Ifa02c934011c9735fae0cb31c3dd82b5dc50735b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116324
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Kaiqi Liang <kaiqiliang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085730}

--- chrome/browser/resources/chromeos/emoji_picker/events.ts
+++ chrome/browser/resources/chromeos/emoji_picker/events.ts
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {CategoryEnum} from './types';
 
-export type CategoryButtonClickEvent = CustomEvent<{categoryName: string}>;
+export type CategoryButtonClickEvent =
+    CustomEvent<{categoryName: CategoryEnum}>;
 
 export const CATEGORY_BUTTON_CLICK = 'category-button-click';
 
