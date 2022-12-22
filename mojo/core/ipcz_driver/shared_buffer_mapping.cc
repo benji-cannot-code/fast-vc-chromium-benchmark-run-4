839231cd73ab1563aaaafee6315585b51efb67b5
Tom Sepez <tsepez@chromium.org>
2022-12-22 02:46:14 +0000

    Rework destruction order in SharedBufferMapping::Close().
    Make sure memory_ is null before releasing the actual memory to
    which it points. Failure to do so may trigger spurious dangling
    pointer detection.
    
    Bug: 1401495
    Change-Id: I3e92ce44cce78d2484eba6af893c8783d09906c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109316
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086212}

--- mojo/core/ipcz_driver/shared_buffer_mapping.cc
+++ mojo/core/ipcz_driver/shared_buffer_mapping.cc
@@ -89,8 +89,8 @@ scoped_refptr<SharedBufferMapping> SharedBufferMapping::Create(
 }
 
 void SharedBufferMapping::Close() {
-  mapping_.reset();
   memory_ = nullptr;
+  mapping_.reset();
 }
 
 }  // namespace mojo::core::ipcz_driver
