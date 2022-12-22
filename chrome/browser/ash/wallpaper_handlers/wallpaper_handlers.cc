07a9d65229ba858e7201d8c130155af927dad173
Henrique Ferreiro <hferreiro@igalia.com>
2022-12-22 14:05:30 +0000

    Remove remaining temporary declarations from ash/constants/
    This CL is part of the ChromeOS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: If2bd282dd7c3d8326b5cfacca3a3890364a5bb25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4118736
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086327}

--- chrome/browser/ash/wallpaper_handlers/wallpaper_handlers.cc
+++ chrome/browser/ash/wallpaper_handlers/wallpaper_handlers.cc
@@ -339,8 +339,9 @@ void BackdropCollectionInfoFetcher::Start(OnCollectionsInfoFetched callback) {
   // The language field may include the country code (e.g. "en-US").
   request.set_language(g_browser_process->GetApplicationLocale());
   request.add_filtering_label(kFilteringLabel);
-  if (chromeos::IsGoogleBrandedDevice())
+  if (ash::IsGoogleBrandedDevice()) {
     request.add_filtering_label(kGoogleDeviceFilteringLabel);
+  }
   std::string serialized_proto;
   request.SerializeToString(&serialized_proto);
 
@@ -411,8 +412,9 @@ void BackdropImageInfoFetcher::Start(OnImagesInfoFetched callback) {
   request.set_language(g_browser_process->GetApplicationLocale());
   request.set_collection_id(collection_id_);
   request.add_filtering_label(kFilteringLabel);
-  if (chromeos::IsGoogleBrandedDevice())
+  if (ash::IsGoogleBrandedDevice()) {
     request.add_filtering_label(kGoogleDeviceFilteringLabel);
+  }
   std::string serialized_proto;
   request.SerializeToString(&serialized_proto);
 
@@ -484,8 +486,9 @@ void BackdropSurpriseMeImageFetcher::Start(OnSurpriseMeImageFetched callback) {
   request.set_language(g_browser_process->GetApplicationLocale());
   request.add_collection_ids(collection_id_);
   request.add_filtering_label(kFilteringLabel);
-  if (chromeos::IsGoogleBrandedDevice())
+  if (ash::IsGoogleBrandedDevice()) {
     request.add_filtering_label(kGoogleDeviceFilteringLabel);
+  }
   if (!resume_token_.empty())
     request.set_resume_token(resume_token_);
   std::string serialized_proto;
