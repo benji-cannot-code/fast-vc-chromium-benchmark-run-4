21a80c93d0c02916b222a3d9b48884b99b0e05b3
Matt Mueller <mattm@chromium.org>
2022-12-22 16:58:07 +0000

    enforce_anchor_constraints: if keyUsage present, require keyCertSign
    (enforce_anchor_constraints is not enabled by default, so this change
    is expected to have no behavior change in chrome at this time.)
    
    Bug: 1072083
    Change-Id: Idfaf21bf2e52c50decb8bcbf4b43fb3c431301b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120695
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086394}

---
+++ net/data/verify_certificate_chain_unittest/root-lacks-keycertsign-key-usage/generate-chains.py
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2022 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Certificate chain where the root keyUsage extension is present but does not
+contain keyCertSign."""
+
+import sys
+sys.path += ['../..']
+
+import gencerts
+
+# Self-signed root certificate without keyCertSign.
+root = gencerts.create_self_signed_root_certificate('Root')
+root.get_extensions().set_property('keyUsage',
+                                   'critical,digitalSignature,keyEncipherment')
+
+# Intermediate certificate.
+intermediate = gencerts.create_intermediate_certificate('Intermediate', root)
+
+# Target certificate.
+target = gencerts.create_end_entity_certificate('Target', intermediate)
+
+chain = [target, intermediate, root]
+gencerts.write_chain(__doc__, chain, 'chain.pem')
