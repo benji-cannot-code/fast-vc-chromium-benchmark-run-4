f95a46fabfed963c5ade96196439777b7727b67a
Wenyu Fu <wenyufu@chromium.org>
2022-12-22 17:03:26 +0000

    Reland "[CCTBrand] Add param to pause branding icon animation"
    This reverts commit 4bb5748d9a58a4655ca2d62a85235228053e57d9.
    
    Reason for revert: Reland with fix to avoid adding dependency.
    
    Original change's description:
    > Revert "[CCTBrand] Add param to pause branding icon animation"
    >
    > This reverts commit 829be1ef89ecb38c48ce0b2b467b18d5e5f725e5.
    >
    > Reason for revert: Broke compile, e.g.
    >
    > https://ci.chromium.org/ui/p/chromium/builders/ci/android-arm64-archive-rel/903/overview
    >
    > Original change's description:
    > > [CCTBrand] Add param to pause branding icon animation
    > >
    > > Add a parameter to pause security icon animation. This could be useful to free up main thread resources for the animation to load web contents faster.
    > >
    > > Bug: 1341138, 1403137
    > > Change-Id: Ie3b6ad02c4ad29a6d74ccc54542fd1e88e46d239
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117326
    > > Reviewed-by: Sinan Sahin <sinansahin@google.com>
    > > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1086238}
    >
    > Bug: 1341138, 1403137
    > Change-Id: I05e62e0ce41782182a2ca33bd0149f4d7f8f950f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120499
    > Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1086242}
    
    Bug: 1341138, 1403137
    Change-Id: I7eddebcf6ccb6d92ee439feba154feaa16b49abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119380
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086398}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
@@ -760,6 +760,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
         private final Runnable[] mAfterBrandingRunnables = new Runnable[TOTAL_POST_BRANDING_KEYS];
         private boolean mCurrentlyShowingBranding;
         private boolean mBrandingStarted;
+        private boolean mAnimateIconTransition = true;
         private CallbackController mCallbackController = new CallbackController();
         // Cached the state before branding start so we can reset to the state when its done.
         private @Nullable Integer mPreBandingState;
@@ -845,6 +846,11 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
                             -> mBrowserControlsVisibilityDelegate.releasePersistentShowingToken(
                                     token),
                     MIN_URL_BAR_VISIBLE_TIME_POST_BRANDING_MS);
+        }
+
+        @Override
+        public void setIconTransitionEnabled(boolean enabled) {
+            mAnimateIconTransition = enabled;
         }
 
         private void cacheRegularState() {
@@ -1069,7 +1075,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
             ColorStateList colorStateList = AppCompatResources.getColorStateList(
                     getContext(), mLocationBarDataProvider.getSecurityIconColorStateList());
             ApiCompatibilityUtils.setImageTintList(mSecurityButton, colorStateList);
-            mAnimDelegate.updateSecurityButton(R.drawable.chromelogo16);
+            mAnimDelegate.updateSecurityButton(R.drawable.chromelogo16, mAnimateIconTransition);
 
             mUrlBar.setText(R.string.twa_running_in_chrome);
         }
@@ -1100,7 +1106,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
                         getContext(), mLocationBarDataProvider.getSecurityIconColorStateList());
                 ApiCompatibilityUtils.setImageTintList(mSecurityButton, colorStateList);
             }
-            mAnimDelegate.updateSecurityButton(securityIconResource);
+            mAnimDelegate.updateSecurityButton(securityIconResource, mAnimateIconTransition);
 
             int contentDescriptionId =
                     mLocationBarDataProvider.getSecurityIconContentDescriptionResourceId();
