d471e022840e0f6480875cc9eba14d5ee8ed4721
Muyao Xu <muyaoxu@google.com>
2022-12-22 18:24:19 +0000

    [Code Health Rotation] Replace DictionaryValue with base::Value::Dict
    Bug: 1187061
    Change-Id: I29277944d8a0c9d6a567c1ee199bd35d0439b5fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119361
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086435}

--- extensions/common/manifest_url_handlers.h
+++ extensions/common/manifest_url_handlers.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_handler.h"
 
-namespace base {
-class DictionaryValue;
-}
-
 namespace extensions {
 
 // A structure to hold various URLs like devtools_page, homepage_url, etc
@@ -52,7 +48,6 @@ struct ManifestURL : public Extension::ManifestData {
 
   // Returns true if this extension's update URL is the extension gallery.
   static bool UpdatesFromGallery(const Extension* extension);
-  static bool UpdatesFromGallery(const base::DictionaryValue* manifest);
 
   // Returns the About Page for this extension.
   static const GURL& GetAboutPage(const Extension* extension);
