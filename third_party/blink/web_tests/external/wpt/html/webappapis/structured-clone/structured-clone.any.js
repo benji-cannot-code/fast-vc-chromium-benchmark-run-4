a3305b13c0122ab71719ef79aae84162d13c2c60
Shu-yu Guo <syg@chromium.org>
2022-12-22 22:47:53 +0000

    [rab/gsab] Add WPT tests for resizable ArrayBuffer
    Bug: v8:11111
    Change-Id: I531f10cae15dd09e7934ad698acf9dcae225ca4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082152
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086557}

--- third_party/blink/web_tests/external/wpt/html/webappapis/structured-clone/structured-clone.any.js
+++ third_party/blink/web_tests/external/wpt/html/webappapis/structured-clone/structured-clone.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=structuredClone() tests
+// META: script=/common/sab.js
 // META: script=/html/webappapis/structured-clone/structured-clone-battery-of-tests.js
 // META: script=/html/webappapis/structured-clone/structured-clone-battery-of-tests-with-transferables.js
 // META: script=/html/webappapis/structured-clone/structured-clone-battery-of-tests-harness.js
