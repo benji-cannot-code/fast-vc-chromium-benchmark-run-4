ef281c44c78c17194bc0008f3b33651c52bbf934
Mikel Astiz <mastiz@chromium.org>
2022-12-22 10:43:35 +0000

    [ReadingList] Address or remove minor TODOs
    Two ouf of three TODOs are simply removed, after a closer look suggested
    that the introduced complexity may not be worth it.
    
    The third one, about reusing a single storage batch, was tackled by
    exposing a new getter in ScopedReadingListBatchUpdateImpl.
    
    Change-Id: Ie2f225a887281b2b7d0870350908da1bd475e361
    Fixed: 1386158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116976
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086260}

--- components/reading_list/core/reading_list_model_storage_impl.cc
+++ components/reading_list/core/reading_list_model_storage_impl.cc
@@ -148,7 +148,6 @@ void ReadingListModelStorageImpl::OnReadAllMetadata(
 
 void ReadingListModelStorageImpl::OnDatabaseSave(
     const absl::optional<syncer::ModelError>& error) {
-  // TODO(crbug.com/1386158): Errors should be propagated up.
   return;
 }
 
