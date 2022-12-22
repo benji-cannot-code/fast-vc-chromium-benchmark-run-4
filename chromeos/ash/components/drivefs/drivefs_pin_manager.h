5be01e53f24055ba4a649cb30f4beafc27410659
François Degros <fdegros@chromium.org>
2022-12-22 10:36:33 +0000

    DriveFsPinManager: Log human-readable sizes
    Bug: b:261530520, b:263320579
    Test: out/Default/chromeos_unittests --gtest_filter=DriveFsPinManagerTest.*
    Change-Id: Id34ffd10f7b373969066367e03a353fcb83d38cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4121795
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086248}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.h
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.h
@@ -28,6 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace drivefs::pinning {
 
+// Prints a size in bytes in a human-readable way.
+enum HumanReadableSize : int64_t;
+
+COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DRIVEFS)
+std::ostream& operator<<(std::ostream& out, HumanReadableSize size);
+
 // Constant representing the GCache folder name.
 extern const char kGCacheFolderName[];
 
