25ecc3e43d7243e1dd81e30d7a8327b95887651d
Stephan Hartmann <stha09@googlemail.com>
2022-12-22 16:05:08 +0000

    GCC: fix incomplete type of gl::Presenter
    GCC requires full definition of gl::Presenter for return type
    of gl::GLOzoneEGL::CreateGLContext() with base::scoped_refptr.
    
    Bug: 819294
    Change-Id: I807beb8ef068f7ddb822a2e7f2cb48517ae62768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110925
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086362}

--- ui/ozone/common/gl_ozone_egl.cc
+++ ui/ozone/common/gl_ozone_egl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_share_group.h"
 #include "ui/gl/gl_surface.h"
 #include "ui/gl/gl_utils.h"
+#include "ui/gl/presenter.h"
 
 namespace ui {
 
