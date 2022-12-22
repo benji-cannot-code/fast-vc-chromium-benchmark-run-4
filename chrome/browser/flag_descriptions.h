4a0dea94cce2a69efd9cdca051ff498aedc4c36b
Brittany Hartmire <bhartmire@google.com>
2022-12-22 18:55:44 +0000

    [SmartLock]: Delete expired flag metadata
    Clean up expired flags to silence alerts. Underlying flag logic remains.
    Follow up CLs will clean up the kSmartLockSignInRemoved flag logic.
    
    TEST=existing unittests pass
    
    Bug: b/227674947
    Change-Id: Ib51e3d273c06dbe031aa0a5c98d3a083e8cca52e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119077
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086457}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3356,12 +3356,6 @@ extern const char kShowTouchHudDescription[];
 extern const char kSimLockPolicyName[];
 extern const char kSimLockPolicyDescription[];
 
-extern const char kSmartLockSignInRemovedName[];
-extern const char kSmartLockSignInRemovedDescription[];
-
-extern const char kSmartLockUIRevampName[];
-extern const char kSmartLockUIRevampDescription[];
-
 extern const char kSnoopingProtectionName[];
 extern const char kSnoopingProtectionDescription[];
 
