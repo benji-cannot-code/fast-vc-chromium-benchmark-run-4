3787d8b68b9ce265c6b8275da4cdf2b7831d90ab
Hira Mahmood <hiramahmood@google.com>
2022-12-22 16:35:12 +0000

    [iOS] Add enterprise policy for App Store Rating.
    This change adds an enterprise policy for the App Store Rating promo.
    The policy will allow admins to disable the promo for a user.
    
    This CL only creates the new policy, and the integration of the policy with the AppStoreRatingSceneAgent class will be in a following CL.
    
    Bug: 1359065
    Change-Id: I8aac2e44e318605ea30c01ee7bd8e37ad8667ef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116592
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086383}

--- ios/chrome/browser/prefs/pref_names.h
+++ ios/chrome/browser/prefs/pref_names.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace prefs {
 
 extern const char kApplicationLocale[];
+extern const char kAppStoreRatingPolicyEnabled[];
 extern const char kArticlesForYouEnabled[];
 extern const char kBrowserStateInfoCache[];
 extern const char kBrowserStateLastUsed[];
