23c05e2f9a50783ad341db90bfffe65125bfd7b1
Kelvin Jiang <kelvinjiang@chromium.org>
2022-12-22 19:45:22 +0000

    [Extensions] Return error for scripting API methods if limit exceeded
    This CL returns an error for any scripting API methods that add scripts
    (i.e. registerContentScripts and updateContentScripts) if the script
    files specified will exceed the per-file or per-extension size limit.
    
    E.g. If the per-extension limit is 1GB and one call to
    registerContentScripts specifies files that add up to 1.1GB, the
    call returns and error and is a no-op. This is checked during file
    validation for the API call, before scripts are loaded.
    
    Note: Tracking loaded script size deltas for pending API calls is
    tricky because script loads are queued and obtaining changes in script
    sizes for pending calls can be difficult from an implementation
    standpoint.
    
    However, we can't just look at the current loaded script size either
    because it does not take into account pending API calls that can
    remove scripts (and would create false positives where
    updateContentScript() calls can be wrongly rejected since the removal,
    then addition of scripts in that call happens in a single load).
    
    This implementation should be fine because
    1. Given the proposed per-file limit of 500MB and per-extension limit
       of 1GB, we anticipate the # of extensions having that amount of
       content scripts to be very low.
    2. When it's time to actually load the script files' content, the
       ExtensionUserScriptLoader stops loading once the # bytes read from
       script files reaches the limit, which prevents the OOM scenario
       described in the linked bug.
    
    Bug: 1379187
    Change-Id: I2059d489f993f1e035e780b2b80f819fcbbbaf60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117453
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086467}

--- chrome/browser/extensions/api/scripting/scripting_apitest.cc
+++ chrome/browser/extensions/api/scripting/scripting_apitest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/background_script_executor.h"
 #include "extensions/browser/disable_reason.h"
 #include "extensions/common/features/feature_channel.h"
+#include "extensions/common/utils/content_script_utils.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
 #include "extensions/test/test_extension_dir.h"
@@ -189,6 +190,15 @@ IN_PROC_BROWSER_TEST_F(ScriptingAPITest, RapidLoadUnload) {
   // css_injection extension, will complete.
   OpenURLInCurrentTab(
       embedded_test_server()->GetURL("google.com", "/simple.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(ScriptingAPITest, DynamicContentScriptsSizeLimits) {
+  auto single_scripts_limit_reset =
+      script_parsing::CreateScopedMaxScriptLengthForTesting(700u);
+  auto extension_scripts_limit_reset =
+      script_parsing::CreateScopedMaxScriptsLengthPerExtensionForTesting(1200u);
+  ASSERT_TRUE(RunExtensionTest("scripting/dynamic_scripts_size_limits"))
+      << message_;
 }
 
 // Tests that calling scripting.executeScript works on a newly created tab
---
+++ chrome/test/data/extensions/api_test/scripting/dynamic_scripts_size_limits/inject_dynamic_slightly_big.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var div = document.createElement('div');
+div.id = 'dynamic-slightly-big';
+document.body.appendChild(div);
+/*
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE TO BE OVER 650 BYTES
+PADDING OUT THE FILE
+*/
