61f75b91294ecb4224edcdbce96132881eb98b26
Sean Kau <skau@chromium.org>
2022-12-22 23:25:38 +0000

    Remove scoped light mode from ash/style
    DarkLightMode is launched so the path is removed from ash_color_mixer. Unused include in pill_button.
    
    Bug: b/252360927
    Change-Id: I0e2a052ac9c4ef8dec069895a254cf5547a9f348
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081236
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086569}

--- ash/style/ash_color_mixer.cc
+++ ash/style/ash_color_mixer.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_mixer.h"
 
 #include "ash/constants/ash_features.h"
-#include "ash/public/cpp/style/scoped_light_mode_as_default.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/dark_light_mode_controller_impl.h"
 #include "ash/style/harmonized_colors.h"
@@ -452,19 +451,6 @@ void AddAshColorMixer(ui::ColorProvider* provider,
   mixer[ui::kColorAshLightFocusRing] = {cros_tokens::kColorProminentDark};
 
   mixer[ui::kColorAshOnboardingFocusRing] = {cros_tokens::kColorProminentDark};
-
-  if (!features::IsDarkLightModeEnabled()) {
-    ash::ScopedLightModeAsDefault scoped_light_mode_as_default;
-    mixer[ui::kColorAshSystemUILightBorderColor1] = {
-        ui::kColorHighlightBorderBorder1};
-    mixer[ui::kColorAshSystemUILightBorderColor2] = {
-        ui::kColorHighlightBorderBorder2};
-    mixer[ui::kColorAshSystemUILightHighlightColor1] = {
-        ui::kColorHighlightBorderHighlight1};
-    mixer[ui::kColorAshSystemUILightHighlightColor2] = {
-        ui::kColorHighlightBorderHighlight2};
-    return;
-  }
 
   mixer[ui::kColorAshSystemUIMenuBackground] = {kColorAshShieldAndBase80};
   mixer[ui::kColorAshSystemUIMenuIcon] = {kColorAshIconColorPrimary};
