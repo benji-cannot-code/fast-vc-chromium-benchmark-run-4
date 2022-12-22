740e4511c259bf9d38359cff6a29409da104c5b4
Wes Okuhara <wesokuhara@google.com>
2022-12-22 23:26:54 +0000

    CrOS Settings: Convert internet APN subpage to TS
    Additionally loads HTML templates for custom elements at runtime.
    
    Bug: b:263413628, chromium:1292025
    Test: browser_tests --gtest_filter='OSSettingsApn*'
    Change-Id: Id266dac2f430b406b1e5d57f4d008cc21537dc78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111764
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086570}

--- ash/webui/common/resources/network/apn_list.d.ts
+++ ash/webui/common/resources/network/apn_list.d.ts
@@ -3,4 +3,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export {};
+import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {ManagedCellularProperties} from 'chrome://resources/mojo/chromeos/services/network_config/public/mojom/cros_network_config.mojom-webui.js';
+
+export class ApnList extends PolymerElement {
+  static get is(): string;
+  static get template(): HTMLTemplateElement;
+  static get properties(): {
+    guid: StringConstructor,
+    managedCellularProperties: ManagedCellularProperties,
+    shouldOmitLinks: {
+      type: BooleanConstructor,
+      value: boolean,
+    },
+    shouldShowApnDetailDialog_: {
+      type: BooleanConstructor,
+      value: boolean,
+    },
+    isConnectedApnAutoDetected_: {
+      type: BooleanConstructor,
+      value: boolean,
+    },
+  };
+  openApnDetailDialogInCreateMode(): void;
+  private getApns_;
+  private isConnectedApnAutoDetected_: boolean;
+  private isApnConnected_;
+  private isApnAutoDetected_;
+  private onLearnMoreClicked_;
+  private onShowApnDetailDialog_;
+  private showApnDetailDialog_;
+  private shouldShowApnDetailDialog_: boolean;
+  private onApnDetailDialogClose_;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'apn-list': ApnList;
+  }
+}
