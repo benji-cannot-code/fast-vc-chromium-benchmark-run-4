40c60d3d458f9e0346a7bc1b32dfbbc348d3897e
manukh <manukh@chromium.org>
2022-12-23 02:16:27 +0000

    [omnibox][grouping] Timing histograms `UpdateResult()` & `SortAndCull()`
    Add microsecond scoped timing histograms for the 2 methods. The grouping
    framework is likely to slow these down, and we want to ensure the
    regression is acceptable. Likewise, they'll help verify any
    optimizations we do are effective.
    
    Current testing shows the grouping framework is 2-5x slower than the
    traditional grouping; which isn't surprising because the traidtional
    grouping is essentially merely a `std::partial_sort()` while the
    framework initializes structs. Regardless, it's not worrisome compared
    to the overall omnibox latency (10-50 us compared to 1-100 ms).
    
    Bug: 1343512
    Change-Id: I4a3603c4bb9b866e776369c445039caeb6d280d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4123231
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086623}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/format_macros.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/observer_list.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
@@ -914,6 +915,7 @@ void AutocompleteController::UpdateResult(
     bool regenerate_result,
     bool force_notify_default_match_changed) {
   TRACE_EVENT0("omnibox", "AutocompleteController::UpdateResult");
+  SCOPED_UMA_HISTOGRAM_TIMER_MICROS("Omnibox.AutocompletionTime.UpdateResult");
 
   absl::optional<AutocompleteMatch> last_default_match;
   std::u16string last_default_associated_keyword;
