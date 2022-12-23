cfa89e5f7ee000146ce4db3908a6851c1f4ac392
Wes Okuhara <wesokuhara@google.com>
2022-12-23 04:46:49 +0000

    CrOS Settings: Setup dynamic colors under Jelly experiment
    Adds support for dynamic colors to ChromeOS Settings when the Jelly
    feature is enabled, by appending the stylesheet via <link> element to
    the <head> of the HTML document.
    
    Bug: b:257329722
    Test: Assert dynamic colors show only with Jelly feature enabled
    Change-Id: I7e97db498e9ee25fe4efb3322440cc4a27eaebc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4118564
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086641}

--- chrome/browser/ui/webui/settings/ash/os_settings_ui.h
+++ chrome/browser/ui/webui/settings/ash/os_settings_ui.h
@@ -31,16 +31,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "ui/webui/mojo_web_ui_controller.h"
 #include "ui/webui/resources/cr_components/app_management/app_management.mojom-forward.h"
+#include "ui/webui/resources/cr_components/color_change_listener/color_change_listener.mojom.h"
 
 namespace user_prefs {
 class PrefRegistrySyncable;
 }  // namespace user_prefs
 
+namespace ui {
+class ColorChangeHandler;
+}  // namespace ui
+
 namespace ash::settings {
 
 namespace mojom {
 class SearchHandler;
-}
+}  // namespace mojom
 
 class OSSettingsUI;
 
@@ -146,6 +151,11 @@ class OSSettingsUI : public ui::MojoWebUIController {
   void BindInterface(
       mojo::PendingReceiver<auth::mojom::RecoveryFactorEditor> receiver);
 
+  // Binds to the Jelly dynamic color Mojo
+  void BindInterface(
+      mojo::PendingReceiver<color_change_listener::mojom::PageHandler>
+          receiver);
+
  private:
   base::TimeTicks time_when_opened_;
 
@@ -154,6 +164,9 @@ class OSSettingsUI : public ui::MojoWebUIController {
   std::unique_ptr<mojom::UserActionRecorder> user_action_recorder_;
   std::unique_ptr<AppManagementPageHandlerFactory>
       app_management_page_handler_factory_;
+
+  // This handler notifies the WebUI when the color provider changes.
+  std::unique_ptr<ui::ColorChangeHandler> color_provider_handler_;
 
   WEB_UI_CONTROLLER_TYPE_DECL();
 };
