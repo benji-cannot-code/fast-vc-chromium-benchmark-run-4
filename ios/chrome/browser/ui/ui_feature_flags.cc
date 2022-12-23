0aaab1b5b8257d3defb0ab31b9b3f8d81001ff85
Jérôme Lebel <jlebel@chromium.org>
2022-12-23 12:15:14 +0000

    [iOS] Add kEnableNewBookmarksImplementation flag
    This flag can switch from the legacy implementation and the new
    bookmark implementation.
    
    Design doc: http://go/b4b-coordinator-dd
    
    Bug: 1402758
    Change-Id: I64f1645ee34eb84c7c70b641e86011022e29185f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120502
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086720}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -133,6 +133,10 @@ BASE_FEATURE(kTabGridRecencySort,
              "TabGridRecencySort",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableNewBookmarksImplementation,
+             "EnableNewBookmarksImplementation",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsTabGridSortedByRecency() {
   return base::FeatureList::IsEnabled(kTabGridRecencySort);
 }
