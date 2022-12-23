85d261d9cedc42965ac67e67d07ec2ce9addb948
Danila Kuzmin <dkuzmin@google.com>
2022-12-23 14:51:25 +0000

    [SigninScreenHandler]: Start network observation on GaiaScreen::Show
    Bug: 1270652
    Change-Id: I38de8103dc358c28486f7f7b4b59c9d92c47c662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120287
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086741}

--- chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
@@ -873,6 +873,9 @@ void GaiaScreenHandler::SubmitLoginFormForTest() {
 }
 
 void GaiaScreenHandler::Show() {
+  // Start network observation.
+  signin_screen_handler_->StartNetworkObservation();
+
   base::Value::Dict data;
   if (LoginDisplayHost::default_host())
     data.Set("hasUserPods", LoginDisplayHost::default_host()->HasUserPods());
@@ -881,6 +884,7 @@ void GaiaScreenHandler::Show() {
 }
 
 void GaiaScreenHandler::Hide() {
+  signin_screen_handler_->StopNetworkObservation();
   hidden_ = true;
 }
 
