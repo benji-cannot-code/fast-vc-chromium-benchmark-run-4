e1a0295e0a547b793ecafd48f2581ec740843b4e
Brittany Hartmire <bhartmire@google.com>
2022-12-23 00:09:12 +0000

    [QuickStart]: Record metric for reason multidevice screen is skipped
    Create a new metric that will allow us to determine the severity of
    CryptAuth latency on the multidevice screen in OOBE. See related
    discussion in go/cros-quick-start-unified-setup-screen-enhancements
    
    TEST=create new unittests in MultiDeviceSetupScreenTest for added functionality
    
    Bug: b/234655072
    Change-Id: I6c0715e7f8ef1c846d46f8f080cc0685a31590ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966921
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086587}

--- chromeos/ash/services/device_sync/public/cpp/fake_device_sync_client.h
+++ chromeos/ash/services/device_sync/public/cpp/fake_device_sync_client.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/multidevice/remote_device_ref.h"
 #include "chromeos/ash/components/multidevice/software_feature.h"
 #include "chromeos/ash/services/device_sync/feature_status_change.h"
+#include "chromeos/ash/services/device_sync/group_private_key_and_better_together_metadata_status.h"
 #include "chromeos/ash/services/device_sync/proto/cryptauth_common.pb.h"
 #include "chromeos/ash/services/device_sync/public/cpp/device_sync_client.h"
 #include "chromeos/ash/services/device_sync/public/mojom/device_sync.mojom.h"
@@ -112,6 +113,8 @@ class FakeDeviceSyncClient : public DeviceSyncClient {
 
   int GetForceEnrollmentNowCallbackQueueSize() const;
   int GetForceSyncNowCallbackQueueSize() const;
+  int GetBetterTogetherMetadataStatusCallbackQueueSize() const;
+  int GetGroupPrivateKeyStatusCallbackQueueSize() const;
   int GetSetSoftwareFeatureStateInputsQueueSize() const;
   int GetSetFeatureStatusInputsQueueSize() const;
   int GetFindEligibleDevicesInputsQueueSize() const;
@@ -120,6 +123,10 @@ class FakeDeviceSyncClient : public DeviceSyncClient {
 
   void InvokePendingForceEnrollmentNowCallback(bool success);
   void InvokePendingForceSyncNowCallback(bool success);
+  void InvokePendingGetBetterTogetherMetadataStatusCallback(
+      BetterTogetherMetadataStatus status);
+  void InvokePendingGetGroupPrivateKeyStatusCallback(
+      GroupPrivateKeyStatus status);
   void InvokePendingSetSoftwareFeatureStateCallback(
       mojom::NetworkRequestResult result_code);
   void InvokePendingSetFeatureStatusCallback(
