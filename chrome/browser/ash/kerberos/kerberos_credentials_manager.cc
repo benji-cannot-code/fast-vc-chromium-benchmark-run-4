20cad85220849ddb2af79f58b656d22d680b4701
slutskii <slutskii@google.com>
2022-12-23 18:12:18 +0000

    Add KerberosUseCustomPrefilledConfig policy
    This policy is an utility addition for KerberosCustomPrefilledConfig
    (used to be called KerberosDefaultConfiguration).
    KerberosDefaultConfiguration was a string policy, which had unset
    value different from empty string. While it was working correctly
    for local tests, DPanel treats unset string as empty. Now the
    KerberosCustomPrefilledConfig's unset value is "" and the previous
    default value is introduced via a boolean flag
    (i.e. KerberosUseCustomPrefilledConfig).
    
    Bug: b:260522054
    Change-Id: I364d477b4796ae6fcf38af9b72a80e5757b3adc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4100827
    Commit-Queue: Michael Slutskii <slutskii@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086779}

--- chrome/browser/ash/kerberos/kerberos_credentials_manager.cc
+++ chrome/browser/ash/kerberos/kerberos_credentials_manager.cc
@@ -388,8 +388,9 @@ void KerberosCredentialsManager::RegisterLocalStatePrefs(
   registry->RegisterBooleanPref(prefs::kKerberosAddAccountsAllowed, true);
   registry->RegisterListPref(prefs::kKerberosAccounts);
   registry->RegisterStringPref(prefs::kKerberosDomainAutocomplete, "");
-  registry->RegisterStringPref(prefs::kKerberosDefaultConfiguration,
-                               kDefaultKerberosConfig);
+  registry->RegisterBooleanPref(prefs::kKerberosUseCustomPrefilledConfig,
+                                false);
+  registry->RegisterStringPref(prefs::kKerberosCustomPrefilledConfig, "");
 }
 
 void KerberosCredentialsManager::RegisterProfilePrefs(
@@ -404,6 +405,11 @@ KerberosCredentialsManager::EmptyResultCallback() {
   return base::BindOnce([](kerberos::ErrorType error) {
     // Do nothing.
   });
+}
+
+// static
+const char* KerberosCredentialsManager::GetDefaultKerberosConfig() {
+  return kDefaultKerberosConfig;
 }
 
 bool KerberosCredentialsManager::IsKerberosEnabled() const {
@@ -944,11 +950,6 @@ void KerberosCredentialsManager::SetKerberosFilesHandlerForTesting(
 void KerberosCredentialsManager::SetAddManagedAccountCallbackForTesting(
     base::RepeatingCallback<void(kerberos::ErrorType)> callback) {
   add_managed_account_callback_for_testing_ = std::move(callback);
-}
-
-// static
-const char* KerberosCredentialsManager::GetDefaultKerberosConfigForTesting() {
-  return kDefaultKerberosConfig;
 }
 
 }  // namespace ash
