6d2fd5e757fb20c5df15ba2e43ca27b19d63b100
Scott Yoder <scottyoder@google.com>
2022-12-23 17:28:06 +0000

    [ios] Rename BrowserCoordinator's NTPCoordinator ivar
    This changes the ivar from _ntpCoordinator to _NTPCoordinator
    to follow the ObjC style guide (acronyms should be uppercase).
    
    Change-Id: I501357441e609af07ba8f63c26cf72bafcc6f5ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120684
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086768}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -446,8 +446,6 @@ enum class ToolbarKind {
   CredentialProviderPromoCoordinator* _credentialProviderPromoCoordinator;
 }
 
-@synthesize NTPCoordinator = _ntpCoordinator;
-
 #pragma mark - ChromeCoordinator
 
 - (void)start {
@@ -772,10 +770,10 @@ enum class ToolbarKind {
     }
   }
 
-  _ntpCoordinator =
+  _NTPCoordinator =
       [[NewTabPageCoordinator alloc] initWithBrowser:self.browser];
-  _ntpCoordinator.toolbarDelegate = _toolbarCoordinatorAdaptor;
-  _ntpCoordinator.bubblePresenter = _bubblePresenter;
+  _NTPCoordinator.toolbarDelegate = _toolbarCoordinatorAdaptor;
+  _NTPCoordinator.bubblePresenter = _bubblePresenter;
 
   _lensCoordinator = [[LensCoordinator alloc] initWithBrowser:self.browser];
 
@@ -795,7 +793,7 @@ enum class ToolbarKind {
   _viewControllerDependencies.popupMenuCoordinator = self.popupMenuCoordinator;
   _viewControllerDependencies.downloadManagerCoordinator =
       self.downloadManagerCoordinator;
-  _viewControllerDependencies.ntpCoordinator = _ntpCoordinator;
+  _viewControllerDependencies.ntpCoordinator = _NTPCoordinator;
   _viewControllerDependencies.lensCoordinator = _lensCoordinator;
   _viewControllerDependencies.primaryToolbarCoordinator =
       _primaryToolbarCoordinator;
@@ -839,7 +837,7 @@ enum class ToolbarKind {
   _primaryToolbarCoordinator.popupPresenterDelegate = self.viewController;
   [_primaryToolbarCoordinator start];
 
-  _ntpCoordinator.baseViewController = self.viewController;
+  _NTPCoordinator.baseViewController = self.viewController;
 
   [_dispatcher startDispatchingToTarget:self.viewController
                             forProtocol:@protocol(BrowserCommands)];
@@ -898,8 +896,8 @@ enum class ToolbarKind {
   [self.browserContainerCoordinator stop];
   self.browserContainerCoordinator = nil;
 
-  [_ntpCoordinator stop];
-  _ntpCoordinator = nil;
+  [_NTPCoordinator stop];
+  _NTPCoordinator = nil;
 
   _keyCommandsProvider = nil;
   _dispatcher = nil;
@@ -1169,7 +1167,7 @@ enum class ToolbarKind {
 
   self.tabEventsMediator = [[TabEventsMediator alloc]
       initWithWebStateList:self.browser->GetWebStateList()
-            ntpCoordinator:_ntpCoordinator];
+            ntpCoordinator:_NTPCoordinator];
 
   self.viewController.reauthHandler =
       HandlerForProtocol(self.dispatcher, IncognitoReauthCommands);
@@ -1407,7 +1405,7 @@ enum class ToolbarKind {
 
 - (void)focusFakebox {
   if ([self isNTPActiveForCurrentWebState]) {
-    [_ntpCoordinator focusFakebox];
+    [_NTPCoordinator focusFakebox];
   }
 }
 
@@ -2477,7 +2475,7 @@ enum class ToolbarKind {
   DCHECK(webState);
 
   if (self.isNTPActiveForCurrentWebState) {
-    [_ntpCoordinator willUpdateSnapshot];
+    [_NTPCoordinator willUpdateSnapshot];
   }
   OverscrollActionsTabHelper::FromWebState(webState)->Clear();
 }
@@ -2486,7 +2484,7 @@ enum class ToolbarKind {
          baseViewForWebState:(web::WebState*)webState {
   NewTabPageTabHelper* NTPHelper = NewTabPageTabHelper::FromWebState(webState);
   if (NTPHelper && NTPHelper->IsActive())
-    return _ntpCoordinator.viewController.view;
+    return _NTPCoordinator.viewController.view;
   return webState->GetView();
 }
 
@@ -2510,11 +2508,11 @@ enum class ToolbarKind {
 }
 
 - (void)updateFollowingFeedHasUnseenContent:(BOOL)hasUnseenContent {
-  [_ntpCoordinator updateFollowingFeedHasUnseenContent:hasUnseenContent];
+  [_NTPCoordinator updateFollowingFeedHasUnseenContent:hasUnseenContent];
 }
 
 - (void)handleFeedModelDidEndUpdates:(FeedType)feedType {
-  [_ntpCoordinator handleFeedModelDidEndUpdates:feedType];
+  [_NTPCoordinator handleFeedModelDidEndUpdates:feedType];
 }
 
 - (void)scrollToNTPAfterPresentedStateCleared:(FeedType)feedType {
@@ -2551,7 +2549,7 @@ enum class ToolbarKind {
 }
 
 - (void)reloadNTPForWebState:(web::WebState*)webState {
-  [_ntpCoordinator reload];
+  [_NTPCoordinator reload];
 }
 
 #pragma mark - PageInfoPresentation
