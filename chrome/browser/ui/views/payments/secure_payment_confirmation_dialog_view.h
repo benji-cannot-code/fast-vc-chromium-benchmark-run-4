ed75da0d8cb161fb7cf9c7add7fd023749a62baf
0xd00d00 <0xd00d00b@gmail.com>
2022-12-24 06:20:47 +0000

    Change gfx::CreateVectorIcon to ui::ImageModel::FromVectorIcon
    There is no change in behavior.
    We prefer ui::ImageModel::FromVectorIcon than gfx::CreateVectorIcon.
    
    Bug: 1100034
    Change-Id: Ibe5555ecb068859e2cf4cd87a5281e0251640d1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4078644
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086813}

--- chrome/browser/ui/views/payments/secure_payment_confirmation_dialog_view.h
+++ chrome/browser/ui/views/payments/secure_payment_confirmation_dialog_view.h
@@ -73,9 +73,6 @@ class SecurePaymentConfirmationDialogView
   bool ShouldShowCloseButton() const override;
   bool Accept() override;
 
-  // views::View:
-  void OnThemeChanged() override;
-
   base::WeakPtr<SecurePaymentConfirmationDialogView> GetWeakPtr();
 
  private:
