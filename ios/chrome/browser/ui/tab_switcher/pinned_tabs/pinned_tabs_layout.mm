16f85a0d10106b325c01234fba26c94d567d77db
Ewann Pelle <ewannpv@chromium.org>
2022-12-26 10:35:11 +0000

    [iOS][PT] Update PinnedTabsLayout
    This CL centers pinned items if possible. It also updates their width
    according to their number.
    
    Fixed: 1402262
    Change-Id: Ic9f8ca4eee146eff98a7dc0b5dc26d0068475041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112037
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1086960}

--- ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_tabs_layout.mm
+++ ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_tabs_layout.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)init {
   if (self = [super init]) {
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    self.minimumInteritemSpacing = kPinnedCellInteritemSpacing;
   }
   return self;
 }
@@ -25,11 +26,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)prepareLayout {
   [super prepareLayout];
 
-  self.itemSize = CGSize{kPinnedCelldWidth, kPinnedCelldHeight};
+  NSInteger itemCount = [self.collectionView numberOfItemsInSection:0];
+  // No neeed to update the layout if the collectionView is empty.
+  if (itemCount == 0) {
+    return;
+  }
+
+  CGFloat collectionViewWidth = CGRectGetWidth(self.collectionView.bounds);
+  CGFloat itemSpacingSum = self.minimumInteritemSpacing * (itemCount - 1);
+  CGFloat horizontalPaddingSum = 2 * kPinnedCellHorizontalLayoutInsets;
+
+  CGFloat itemWidth =
+      (collectionViewWidth - itemSpacingSum - horizontalPaddingSum) / itemCount;
+  itemWidth = MAX(itemWidth, kPinnedCellMinWidth);
+  itemWidth = MIN(itemWidth, kPinnedCellMaxWidth);
+  self.itemSize = CGSize{itemWidth, kPinnedCelldHeight};
+
+  // Center items if there is enough available space.
+  CGFloat availableHorizontalLayoutInsets =
+      (collectionViewWidth - (itemWidth * itemCount) - itemSpacingSum) / 2;
+  availableHorizontalLayoutInsets =
+      MAX(availableHorizontalLayoutInsets, kPinnedCellHorizontalLayoutInsets);
 
   self.sectionInset = UIEdgeInsets{
-      kPinnedCellVerticalLayoutInsets, kPinnedCellHorizontalLayoutInsets,
-      kPinnedCellVerticalLayoutInsets, kPinnedCellHorizontalLayoutInsets};
+      kPinnedCellVerticalLayoutInsets, availableHorizontalLayoutInsets,
+      kPinnedCellVerticalLayoutInsets, availableHorizontalLayoutInsets};
 }
 
 @end
