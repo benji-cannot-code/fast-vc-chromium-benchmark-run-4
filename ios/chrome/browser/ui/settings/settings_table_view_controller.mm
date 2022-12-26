f0e6575a0a264ef319d7102437d30067c60a9a8c
Gauthier Ambard <gambard@chromium.org>
2022-12-26 10:49:53 +0000

    [iOS] Fix Sync Off state
    The color should be grey, not red. The symbol was also the wrong one.
    
    Fixed: 1403477
    Change-Id: I70ea37c428690380360d4d3f1e426b0fc63c297a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4124039
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086964}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -1907,8 +1907,9 @@ UIImage* GetBrandedGoogleServicesSymbol() {
     case kSyncConsentOff: {
       googleSyncItem.detailText = l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
       if (UseSymbols()) {
-        googleSyncItem.iconImage = DefaultSettingsRootSymbol(kSyncErrorSymbol);
-        googleSyncItem.iconBackgroundColor = UIColor.redColor;
+        googleSyncItem.iconImage =
+            CustomSettingsRootSymbol(kSyncDisabledSymbol);
+        googleSyncItem.iconBackgroundColor = [UIColor colorNamed:kGrey400Color];
         googleSyncItem.iconTintColor = UIColor.whiteColor;
         googleSyncItem.iconCornerRadius = kColorfulBackgroundSymbolCornerRadius;
 
@@ -1920,7 +1921,6 @@ UIImage* GetBrandedGoogleServicesSymbol() {
     case kSyncOff:
     case kSyncEnabledWithNoSelectedTypes: {
       googleSyncItem.detailText = nil;
-      googleSyncItem.iconImage = [UIImage imageNamed:kSyncOffImageName];
       if (UseSymbols()) {
         googleSyncItem.iconImage =
             CustomSettingsRootSymbol(kSyncDisabledSymbol);
