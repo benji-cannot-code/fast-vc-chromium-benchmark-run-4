a3691b0c64996cd6be712c5c22182949f3961c4d
Cam Bickel <cambickel@google.com>
2022-12-27 19:35:26 +0000

    diagnostics: Fix eslint presubmit error by ignoring JS files
    This CL fixes the issue in
    https://buganizer.corp.google.com/issues/263276134#comment8
    by adding a JS glob to `ignorePatterns`. The presubmit error is
    happening because there are still a few JS files in diagnostics that
    were triggering the error on "explicit-function-return-type". Function
    return types are not possible in JS, so we ignore those files.
    
    This CL also adds a gitignore file to ignore tsconfig.json file, which
    are used to add full IDE support for TypeScript projects.
    See http://go/chromium-cookbook-typescript-setup-environment
    for more information.
    
    Bug: b:263276134
    Change-Id: I12f41a57498cb075f1381b4d29dd0f5d7892e0a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4125493
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087156}

--- ash/webui/diagnostics_ui/resources/.eslintrc.js
+++ ash/webui/diagnostics_ui/resources/.eslintrc.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Disable clang-format because it produces odd formatting.
 // clang-format off
 module.exports = {
+    'ignorePatterns': ['**/*.js'],
     'parserOptions': {
       'project': ['./tsconfig_base.json'],
     },
