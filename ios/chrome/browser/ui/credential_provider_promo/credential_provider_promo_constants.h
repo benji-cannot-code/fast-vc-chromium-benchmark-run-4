442404eb550c1dfeee0b10fa86d90d8d27fa05f2
Hira Mahmood <hiramahmood@google.com>
2022-12-27 09:14:01 +0000

    Create initial skeleton CPE Promo VC.
    This change creates the initial
    CredentialProviderExtensionPromoViewController class along with relevant
    view and child VC classes for the Credential Provider promo.
    
    Bug: 1402963
    Change-Id: I9b1603829d59a64a13d5d344a0e8580edfcb2aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026584
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087058}

---
+++ ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_constants.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_CREDENTIAL_PROVIDER_PROMO_CREDENTIAL_PROVIDER_PROMO_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_UI_CREDENTIAL_PROVIDER_PROMO_CREDENTIAL_PROVIDER_PROMO_CONSTANTS_H_
+
+namespace credential_provider_promo {
+
+enum PromoSource {
+  kPasswordCopied,
+  kPasswordSaved,
+  kRemindLaterSelected,
+  kAutofillUsed,
+};
+
+enum PromoContext {
+  kFirstStep,
+  kLearnMore,
+};
+}  // namespace credential_provider_promo
+
+#endif  // IOS_CHROME_BROWSER_UI_CREDENTIAL_PROVIDER_PROMO_CREDENTIAL_PROVIDER_PROMO_CONSTANTS_H_
---
+++ ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_consumer.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_CREDENTIAL_PROVIDER_PROMO_CREDENTIAL_PROVIDER_PROMO_CONSUMER_H_
+#define IOS_CHROME_BROWSER_UI_CREDENTIAL_PROVIDER_PROMO_CREDENTIAL_PROVIDER_PROMO_CONSUMER_H_
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "base/values.h"
+
+// Consumer for the Credential Provider Extension Promo. All of the setters
+// should be called before the view is created.
+@protocol CredentialProviderPromoConsumer
+
+// Sets the respective state in the consumer.
+- (void)setTitleString:(NSString*)titleString
+           subtitleString:(NSString*)subtitleString
+      primaryActionString:(NSString*)primaryActionString
+    secondaryActionString:(NSString*)secondaryActionString
+     tertiaryActionString:(NSString*)tertiaryActionString
+                    image:(UIImage*)image;
+
+// Passes the animation to the consumer.
+- (void)setAnimation:(std::unique_ptr<base::Value>)animationAsset;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_CREDENTIAL_PROVIDER_PROMO_CREDENTIAL_PROVIDER_PROMO_CONSUMER_H_
