bc92011f1b3936d513fc082736bc1ef43889dfb9
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-12-27 23:55:05 +0000

    [Extensions] Remove some expired metrics
    Remove the following expired metrics:
    - Extensions.Bindings.NativeBindingCreationTime
    - Extensions.ResetPermissionsIncrease
    - Extensions.AttemptedToDowngradeVersionLocation
    - Extensions.ExtensionUninstalled
    
    Along with their logging in the code.
    
    Bug: 1400355, 1375467, 1373121, 1308560
    Change-Id: Id113c9dbcfdfb14bdde3adfc22b5d13681204b8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4123157
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087216}

--- extensions/renderer/native_extension_bindings_system.cc
+++ extensions/renderer/native_extension_bindings_system.cc
@@ -10,10 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
-#include "base/timer/elapsed_timer.h"
 #include "components/crx_file/id_util.h"
 #include "content/public/common/content_switches.h"
 #include "extensions/common/constants.h"
@@ -741,7 +739,6 @@ v8::Local<v8::Object> NativeExtensionBindingsSystem::GetAPIHelper(
   CHECK(
       gin::Converter<std::string>::FromV8(isolate, api_name, &api_name_string));
 
-  base::ElapsedTimer timer;
   v8::Local<v8::Object> root_binding = CreateFullBinding(
       context, script_context, &data->bindings_system->api_system_,
       FeatureProvider::GetAPIFeatures(), api_name_string);
@@ -753,9 +750,6 @@ v8::Local<v8::Object> NativeExtensionBindingsSystem::GetAPIHelper(
   if (!success.IsJust() || !success.FromJust())
     return v8::Local<v8::Object>();
 
-  UMA_HISTOGRAM_CUSTOM_COUNTS("Extensions.Bindings.NativeBindingCreationTime",
-                              timer.Elapsed().InMicroseconds(), 1, 10000000,
-                              50);
   return root_binding;
 }
 
