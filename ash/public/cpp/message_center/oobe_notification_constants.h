71faa7834414208903bb33f48984adfcda9a7eb2
Bohdan Tyshchenko <bohdanty@google.com>
2022-12-27 23:55:43 +0000

    [OOBE][LocaleSwitchNotification] - Add notification to switch locale
    Add notification to delegate locale switch call for the "Add Person"
    flow. A delegation event will occur if there is a mismatch between the
    user's GAIA locale and the current system locale during the "Add Person"
    flow. Notification will appear on the first screen after the Locale
    Switch screen that contains a WebView.
    
    Notification logic handles the following scenarios:
    1) User clicks on the body of a notification - notification will move to
    the status tray and will be available there until the user either clicks
    one of the buttons on it or moves to the next screen in the OOBE flow.
    2) User clicks the "Switch" button - the system will change locale
    immediately, and the notification will disappear.
    3) User clicks the "Don't switch" button - the notification will
    disappear.
    4) User clicks the "Close" button - the notification will disappear.
    5) If the user proceeds with the OOBE flow, the notification will
    disappear from the screen or the status tray, and the locale will remain
    the same.
    
    Bug: b:240535193
    Change-Id: I013a2d655b0cace3822db6eb51a6dc92308ee57f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111841
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087217}

---
+++ ash/public/cpp/message_center/oobe_notification_constants.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PUBLIC_CPP_MESSAGE_CENTER_OOBE_NOTIFICATION_CONSTANTS_H_
+#define ASH_PUBLIC_CPP_MESSAGE_CENTER_OOBE_NOTIFICATION_CONSTANTS_H_
+
+namespace ash {
+
+// ID of the locale switch notification that could be shown during the OOBE.
+inline constexpr static char kOOBELocaleSwitchNotificationId[] =
+    "oobe.locale-switch-notification";
+
+}  // namespace ash
+
+#endif  // ASH_PUBLIC_CPP_MESSAGE_CENTER_OOBE_NOTIFICATION_CONSTANTS_H_
