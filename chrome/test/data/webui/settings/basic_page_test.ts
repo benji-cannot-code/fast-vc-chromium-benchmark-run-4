be91b638d7290bc5122f3a34a7691cbd77796017
Rainhard Findling <rainhard@chromium.org>
2022-12-27 14:16:20 +0000

    Revert "sheriff: disable CrSettingsBasicPageTest.PrivacyGuidePromo on linuxdbg"
    This reverts commit 28b162db53e6024f4e16d2369f3853d2b948d1f1.
    
    Reason for revert: Fixes the test that caused the flakes and was the reason for the test suite to be disabled. See crbug.com/1378010#c7 for details.
    
    Original change's description:
    > sheriff: disable CrSettingsBasicPageTest.PrivacyGuidePromo on linuxdbg
    >
    > Bug: 1378010
    > Change-Id: I9fbb46eef9a22d09f750aa2333b9dc143ee49696
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990122
    > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    > Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    > Owners-Override: Lingqi Chi <lingqi@chromium.org>
    > Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1065403}
    
    Bug: 1378010
    Change-Id: Ic9b9c368fe8dc7852ea13efd0a684004d0cacac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4124305
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Auto-Submit: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087095}

--- chrome/test/data/webui/settings/basic_page_test.ts
+++ chrome/test/data/webui/settings/basic_page_test.ts
@@ -431,7 +431,7 @@ suite('PrivacyGuidePromo', () => {
         page.shadowRoot!.querySelector<HTMLElement>('#privacyGuidePromo')!;
     privacyGuidePromo.shadowRoot!.querySelector<HTMLElement>(
                                      '#startButton')!.click();
-    flush();
+    await flushTasks();
 
     const result = await testMetricsBrowserProxy.whenCalled(
         'recordPrivacyGuideEntryExitHistogram');
