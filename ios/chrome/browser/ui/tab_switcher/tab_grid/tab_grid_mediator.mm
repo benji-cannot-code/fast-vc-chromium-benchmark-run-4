e9bd5b1a962c47967beed01fe48e92336fe3e130
Petro Akzhygitov <pakzhygitov@google.com>
2022-12-27 16:09:29 +0000

    Fixed adding/removal of WebState's observations in mediators.
    Bug: 1403687
    Change-Id: Id87c7f313f42ee6c6c24c4c4622884041bc7e400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4124902
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087123}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator.mm
@@ -307,10 +307,8 @@ void RecordTabGridCloseTabsCount(int count) {
 
   if (_webStateList) {
     _scopedWebStateListObservation->AddObservation(_webStateList);
-    for (int i = 0; i < self.webStateList->count(); i++) {
-      web::WebState* webState = self.webStateList->GetWebStateAt(i);
-      _scopedWebStateObservation->AddObservation(webState);
-    }
+    [self addWebStateObservations];
+
     if (self.webStateList->count() > 0) {
       [self populateConsumerItems];
     }
@@ -432,12 +430,16 @@ void RecordTabGridCloseTabsCount(int count) {
   if (IsPinnedTabsEnabled() && webStateList->IsWebStatePinnedAt(index)) {
     [self.consumer removeItemWithID:webState->GetStableIdentifier()
                      selectedItemID:GetActiveRegularWebStateId(webStateList)];
+
+    _scopedWebStateObservation->RemoveObservation(webState);
   } else {
     ItemListIndex itemListIndex =
         [self itemListIndexFromWebStateListIndex:index];
     [self.consumer insertItem:CreateItem(webState)
                       atIndex:itemListIndex
                selectedItemID:GetActiveRegularWebStateId(webStateList)];
+
+    _scopedWebStateObservation->AddObservation(webState);
   }
 }
 
@@ -448,12 +450,9 @@ void RecordTabGridCloseTabsCount(int count) {
 
 - (void)webStateListBatchOperationEnded:(WebStateList*)webStateList {
   DCHECK_EQ(_webStateList, webStateList);
-  for (int i = 0; i < self.webStateList->count(); i++) {
-    web::WebState* webState = self.webStateList->GetWebStateAt(i);
-    _scopedWebStateObservation->AddObservation(webState);
-  }
-  [self.consumer populateItems:CreateItems(self.webStateList)
-                selectedItemID:GetActiveRegularWebStateId(self.webStateList)];
+
+  [self addWebStateObservations];
+  [self populateConsumerItems];
 }
 
 #pragma mark - CRWWebStateObserver
@@ -1066,6 +1065,16 @@ void RecordTabGridCloseTabsCount(int count) {
 - (void)populateConsumerItems {
   [self.consumer populateItems:CreateItems(self.webStateList)
                 selectedItemID:GetActiveRegularWebStateId(self.webStateList)];
+}
+
+- (void)addWebStateObservations {
+  int firstIndex = IsPinnedTabsEnabled()
+                       ? self.webStateList->GetIndexOfFirstNonPinnedWebState()
+                       : 0;
+  for (int i = firstIndex; i < self.webStateList->count(); i++) {
+    web::WebState* webState = self.webStateList->GetWebStateAt(i);
+    _scopedWebStateObservation->AddObservation(webState);
+  }
 }
 
 // Removes `self.syncedClosedTabsCount` most recent entries from the
