6951f806a22a6e7af617d7363bc0da8ce0ae2562
Marlon Facey <mfacey@chromium.org>
2022-12-27 18:15:18 +0000

    [journeys] Add searchbox context menu to journeys in side panel
    Added context menu to the searchbox including internationalized
    strings and updated handler method for the menu.
    
    Bug: 1394475
    Change-Id: Id0ad3b0609a0d6f32d164f9ffd871980ee8d5d70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117042
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087136}

--- chrome/browser/resources/side_panel/history_clusters/app.ts
+++ chrome/browser/resources/side_panel/history_clusters/app.ts
@@ -4,9 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import '../strings.m.js';
+import 'chrome://resources/cr_components/history_clusters/browser_proxy.js';
 import 'chrome://resources/cr_components/history_clusters/clusters.js';
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 
+import {BrowserProxyImpl} from 'chrome://resources/cr_components/history_clusters/browser_proxy.js';
 import {CrToolbarSearchFieldElement} from 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -44,6 +46,15 @@ export class HistoryClustersAppElement extends PolymerElement {
   //============================================================================
 
   query: string;
+
+  //============================================================================
+  // Event Handlers
+  //============================================================================
+
+  private onContextMenu_(event: MouseEvent) {
+    BrowserProxyImpl.getInstance().handler.showContextMenuForSearchbox(
+        this.query, {x: event.clientX, y: event.clientY});
+  }
 
   //============================================================================
   // Overridden methods
