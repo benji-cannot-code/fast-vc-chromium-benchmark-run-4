33cd539df5e1c93061047e723764254b773aeafc
Xianzhu Wang <wangxianzhu@chromium.org>
2022-12-27 00:43:48 +0000

    Optional Predicate parameter for base::DataEquivalent()
    This allows the caller to use a predicate other than operator==,
    like base::ranges::equal() and std::equal() accepting the optional
    predicate parameter.
    
    Change-Id: Ib264103306eb4739de9f1736b067c48f1ca91648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4122881
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087018}

--- base/memory/values_equivalent.h
+++ base/memory/values_equivalent.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_MEMORY_VALUES_EQUIVALENT_H_
 #define BASE_MEMORY_VALUES_EQUIVALENT_H_
 
+#include <functional>
 #include <memory>
 
 #include "base/memory/scoped_refptr.h"
@@ -15,14 +16,16 @@ namespace base {
 // Compares two pointers for equality, returns the dereferenced value comparison
 // if both are non-null.
 // Behaves like std::optional<T>::operator==(const std::optional<T>&) but for
-// pointers.
-template <typename T>
-bool ValuesEquivalent(const T* a, const T* b) {
+// pointers, with an optional predicate.
+// If `p` is specified, `p(const T& x, const T& y)` should return whether `x`
+// and `y` are equal. It's called with `(*a, *b)` when `a != b && a && b`.
+template <typename T, typename Predicate = std::equal_to<>>
+bool ValuesEquivalent(const T* a, const T* b, Predicate p = {}) {
   if (a == b)
     return true;
   if (!a || !b)
     return false;
-  return *a == *b;
+  return p(*a, *b);
 }
 
 // Specialize for smart pointers like std::unique_ptr and base::scoped_refptr
@@ -35,10 +38,11 @@ bool ValuesEquivalent(const T* a, const T* b) {
 //     }
 //   };
 template <typename T,
+          typename Predicate = std::equal_to<>,
           std::enable_if_t<
               std::is_pointer_v<decltype(std::declval<T>().get())>>* = nullptr>
-bool ValuesEquivalent(const T& x, const T& y) {
-  return ValuesEquivalent(x.get(), y.get());
+bool ValuesEquivalent(const T& x, const T& y, Predicate p = {}) {
+  return ValuesEquivalent(x.get(), y.get(), std::move(p));
 }
 
 // Specialize for smart pointers like blink::Persistent and blink::Member that
@@ -54,10 +58,11 @@ bool ValuesEquivalent(const T& x, const T& y) {
 //   };
 //   }  // namespace blink
 template <typename T,
+          typename Predicate = std::equal_to<>,
           std::enable_if_t<
               std::is_pointer_v<decltype(std::declval<T>().Get())>>* = nullptr>
-bool ValuesEquivalent(const T& x, const T& y) {
-  return ValuesEquivalent(x.Get(), y.Get());
+bool ValuesEquivalent(const T& x, const T& y, Predicate p = {}) {
+  return ValuesEquivalent(x.Get(), y.Get(), std::move(p));
 }
 
 }  // namespace base
