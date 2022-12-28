919ac7e4e298d24747e384cea26b965a31db2f4b
Zachary Tan <tanzachary@chromium.org>
2022-12-28 18:01:38 +0000

    [CodeHealth] Refactor base::Value in chromeos/ash/services/device_sync/cryptauth_device.*
    Bug: 1187001
    Change-Id: I5e1591eedee36430324a8bb0df4b2fedfa8b4021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120779
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087362}

--- chromeos/ash/services/device_sync/cryptauth_device.h
+++ chromeos/ash/services/device_sync/cryptauth_device.h
@@ -26,7 +26,7 @@ class CryptAuthDevice {
  public:
   // Returns null if |dict| cannot be converted into a CryptAuthDevice.
   static absl::optional<CryptAuthDevice> FromDictionary(
-      const base::Value& dict);
+      const base::Value::Dict& dict);
 
   // |instance_id|: The Instance ID, used as a unique device identifier. Cannot
   //                be empty.
@@ -58,11 +58,11 @@ class CryptAuthDevice {
   //         <|better_together_device_metadata_| serialized and base64 encoded>,
   //     "feature_states": <|feature_states_| as dictionary>
   //   }
-  base::Value AsDictionary() const;
+  base::Value::Dict AsDictionary() const;
 
   // Converts the device to a human-readable dictionary.
   // Note: We remove |last_update_time_| which can be misleading in the logs.
-  base::Value AsReadableDictionary() const;
+  base::Value::Dict AsReadableDictionary() const;
 
   bool operator==(const CryptAuthDevice& other) const;
   bool operator!=(const CryptAuthDevice& other) const;
