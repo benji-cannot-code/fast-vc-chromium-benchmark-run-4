c838788f24ba10eb12e8423560b78eb178f2532d
Jongmok Kim <jongmok.kim@navercorp.com>
2022-12-28 03:47:44 +0000

    Search tab icon displays properly after theme changed
    Bug: 1401293
    Change-Id: I840abf6fbc82ef120edc3c6f7e4bc80d3c783d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111720
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: jongmok kim <johny.kimc@gmail.com>
    Commit-Queue: jongmok kim <johny.kimc@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1087245}

--- chrome/browser/ui/views/tabs/new_tab_button.h
+++ chrome/browser/ui/views/tabs/new_tab_button.h
@@ -61,6 +61,7 @@ class NewTabButton : public views::ImageButton,
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
   void AddedToWidget() override;
   void RemovedFromWidget() override;
+  void OnThemeChanged() override;
 
  private:
   class HighlightPathGenerator;
