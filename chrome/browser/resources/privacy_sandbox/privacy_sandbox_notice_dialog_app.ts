830657d550670b60362e129c571df9c4c0c60649
Olesia Marukhno <olesiamarukhno@google.com>
2022-12-28 16:09:28 +0000

    [privacy sandbox] Add "More" button to prompts
    Add "More" button to prompts. It is shown when the contents of the
    dialog is scrollable. After the content was scrolled to the bottom and
    the whole content was shown, hide "More" button and show action buttons.
    "More" button scrolls the dialog to show the next visible portion of
    content.
    
    Bug: 1378703
    Change-Id: I6a6df041a85761ea5d2e37079c274e5509315220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116511
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087343}

--- chrome/browser/resources/privacy_sandbox/privacy_sandbox_notice_dialog_app.ts
+++ chrome/browser/resources/privacy_sandbox/privacy_sandbox_notice_dialog_app.ts
@@ -42,11 +42,12 @@ export class PrivacySandboxNoticeDialogAppElement extends
   override ready() {
     super.ready();
 
-    this.resizeAndShowNativeDialog()
-        .then(() => this.updateScrollableContents())
-        .then(
-            () => this.promptActionOccurred(
-                PrivacySandboxPromptAction.NOTICE_SHOWN));
+    this.resizeAndShowNativeDialog().then(() => {
+      this.updateScrollableContents();
+      this.maybeShowMoreButton().then(
+          () => this.promptActionOccurred(
+              PrivacySandboxPromptAction.NOTICE_SHOWN));
+    });
   }
 }
 
