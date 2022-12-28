4e1e16c5d34adbe0371209dccc4ca6b46b37d7ad
Rainhard Findling <rainhard@chromium.org>
2022-12-28 11:25:46 +0000

    [3PC page] Update layout of description on top of the 3PC page
    Update layout of the description that's on top of the 3PC page, and
    replace all strings in the 3PC controls with placeholders.
    
    Bug: 1378703
    Change-Id: I7dfa050918c2600cef757dbcf5a74924e586655d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4124205
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087299}

--- chrome/test/data/webui/settings/cookies_page_test.ts
+++ chrome/test/data/webui/settings/cookies_page_test.ts
@@ -67,6 +67,7 @@ suite('CrSettingsCookiesPageTest', function() {
   test('ElementVisibility', async function() {
     // TODO(): Remove assertFalse checks after the feature is launched.
     await flushTasks();
+    assertTrue(isChildVisible(page, '#explanationText'));
     assertTrue(isChildVisible(page, '#exceptionHeader'));
     assertTrue(isChildVisible(page, '#allowExceptionsList'));
     assertFalse(isChildVisible(page, '#sessionOnlyExceptionsList'));
@@ -373,6 +374,7 @@ suite('CrSettingsCookiesPageTest_PrivacySandboxSettings4Disabled', function() {
 
   test('ElementVisibility', async function() {
     await flushTasks();
+    assertFalse(isChildVisible(page, '#explanationText'));
     assertTrue(isChildVisible(page, '#exceptionHeader'));
     assertTrue(isChildVisible(page, '#clearOnExit'));
     assertTrue(isChildVisible(page, '#doNotTrack'));
