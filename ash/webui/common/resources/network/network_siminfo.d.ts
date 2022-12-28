9ea815a7ee8ffdb164dd19f1b538cf90452bf7da
Wes Okuhara <wesokuhara@google.com>
2022-12-28 21:19:51 +0000

    CrOS Settings: Cleanup internet detail page and misc network typedefs
    - Adds several entries for network components into the
    HTMLElementTagNameMap
    - Virtual pref objects for detail page have a default value so the
    undefined typing can be removed
    
    Bug: b:263413628
    Test: browser_tests --gtest_filter='OSSettingsInternet*'
    Change-Id: Ic93e710e809720bea8d2467f461600da20029673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4123475
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087417}

--- ash/webui/common/resources/network/network_siminfo.d.ts
+++ ash/webui/common/resources/network/network_siminfo.d.ts
@@ -11,4 +11,11 @@ interface NetworkSiminfoElement extends LegacyElementMixin, HTMLElement {
   getUnlockButton(): CrButtonElement|null;
   getSimLockToggle(): CrToggleElement|null;
 }
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'network-siminfo': NetworkSiminfoElement;
+  }
+}
+
 export {NetworkSiminfoElement};
