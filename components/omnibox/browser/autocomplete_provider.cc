2979ef7e66e3e0742b8124a06c2913748cc9493c
manukh <manukh@chromium.org>
2022-12-28 19:26:15 +0000

    [omnibox] Remove unused functions and includes in ACMatch & ACProvider.
    Files touched:
    - autocomplete_provider.h|cc
    - autocomplete_match.h|cc
    
    Functions removed:
    - ClassifyMatchInString()
    - ClassifyLocationInString()
    - HasMatchStyle()
    - WriteIntoTrace()
    
    Change-Id: I9c429d961aa9c5af213d63d93a649a996d87f730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119309
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087391}

--- components/omnibox/browser/autocomplete_provider.cc
+++ components/omnibox/browser/autocomplete_provider.cc
@@ -6,13 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_provider.h"
 
 #include <algorithm>
-#include <set>
 #include <string>
 
-#include "base/feature_list.h"
-#include "base/i18n/case_conversion.h"
-#include "base/logging.h"
-#include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/memory_usage_estimator.h"
@@ -24,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_provider_listener.h"
 #include "components/omnibox/browser/history_provider.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
-#include "components/omnibox/browser/scored_history_match.h"
-#include "components/omnibox/common/omnibox_features.h"
 #include "components/url_formatter/url_fixer.h"
 #include "url/gurl.h"
 
