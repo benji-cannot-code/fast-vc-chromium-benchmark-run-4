b4d47b703e816b6869fc0582dc9320473bed62c3
Noel Gordon <noel@chromium.org>
2022-12-28 05:05:48 +0000

    [quickview] remove untrusted resouces LEGACY_FILES_APP_ORIGIN
    Files app SWA has shipped, so we do not need LEGACY_FILES_APP_ORIGIN
    anymore in QuickView untrusted resources: remove it.
    
    Bug: b/192894681
    Change-Id: Ibdd399e2c7569ca2da1a6454210cc0b5b3cb0200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127715
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087253}

--- ash/webui/file_manager/untrusted_resources/url_constants.js
+++ ash/webui/file_manager/untrusted_resources/url_constants.js
@@ -6,10 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @const {string}
  */
-const LEGACY_FILES_APP_ORIGIN =
-    'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj';
-
-/**
- * @const {string}
- */
 const FILES_APP_SWA_ORIGIN = 'chrome://file-manager';
