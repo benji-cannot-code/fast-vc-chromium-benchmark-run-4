7ee2fd72d1997b0e61d0490409ba5f6d2f48af5b
Zhe Li <zheliooo@google.com>
2022-12-29 00:41:24 +0000

    [Tab Strip Redesign] Use cached TSR flag from Java
    Bug: 1395387
    Change-Id: I3e7d72b6c256868fa4099b82face9d5a4a77ffe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4126338
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087457}

--- chrome/browser/android/compositor/layer/tab_handle_layer.cc
+++ chrome/browser/android/compositor/layer/tab_handle_layer.cc
@@ -47,7 +47,8 @@ void TabHandleLayer::SetProperties(
     bool is_loading,
     float spinner_rotation,
     float brightness,
-    float opacity) {
+    float opacity,
+    bool is_tab_strip_redesign_enabled) {
   if (brightness != brightness_ || foreground != foreground_ ||
       opacity != opacity_) {
     brightness_ = brightness;
@@ -59,7 +60,7 @@ void TabHandleLayer::SetProperties(
     // rather than adding a brightness filter. We can't swap to simply setting
     // the opacity when TSR is disabled, because then, the tab containers can
     // be seen overlapping. (See https://crbug.com/1373632).
-    if (base::FeatureList::IsEnabled(chrome::android::kTabStripRedesign)) {
+    if (is_tab_strip_redesign_enabled) {
       tab_->SetOpacity(brightness_);
     } else {
       cc::FilterOperations filters;
@@ -68,6 +69,7 @@ void TabHandleLayer::SetProperties(
             cc::FilterOperation::CreateBrightnessFilter(brightness_));
       }
       layer_->SetFilters(filters);
+      tab_outline_->SetIsDrawable(true);
     }
   }
 
@@ -230,10 +232,6 @@ TabHandleLayer::TabHandleLayer(LayerTitleCache* layer_title_cache)
       brightness_(1.0f),
       foreground_(false) {
   decoration_tab_->SetIsDrawable(true);
-  // Show tab outline when TabStripRedesign is NOT enabled
-  if (!base::FeatureList::IsEnabled(chrome::android::kTabStripRedesign)) {
-    tab_outline_->SetIsDrawable(true);
-  }
 
   tab_->AddChild(decoration_tab_);
   tab_->AddChild(tab_outline_);
