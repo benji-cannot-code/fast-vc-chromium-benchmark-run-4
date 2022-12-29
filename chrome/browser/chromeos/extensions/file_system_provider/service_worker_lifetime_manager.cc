b19cc50a7d00a8e7da66a0909855bee823f0bbf4
Henrique Ferreiro <hferreiro@igalia.com>
2022-12-29 22:39:43 +0000

    Reland "Make //chrome/browser/chromeos usable from Lacros"
    This is a reland of commit 6ba29508eb550b75cec4c6aaa97d5f6ea285295b.
    Even though //chrome/browser/chromeos:unit_tests is only included as
    a dependency of //chrome/test:unit_tests in a is_chromeos_ash block,
    some bots (e.g. linux-lacros-dbg) build this target in Lacros.
    
    In this reland, all files and dependencies from
    //chrome/browser/chromeos:* targets are checked to avoid these
    failures.
    
    Original change's description:
    > Make //chrome/browser/chromeos usable from Lacros
    >
    > Revamp all dependencies, particularly separating ash and lacros, so
    > that this target can be used by both. //chrome/browser will now depend
    > on //chrome/browser/chromeos instead of duplicating its source files in
    > the Lacros build.
    >
    > Dependencies of :test_support and :unit_tests are also updated because
    > assertions work at the file level and unguarded dependencies to Ash in
    > these targets fail even if in this file only :chromeos is used in
    > Lacros.
    >
    > Also, restrict DEPS entries as much as possible.
    >
    > This CL is part of the Chrome OS source code directory migration:
    > https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    >
    > Bug: 1164001
    > Change-Id: Ie8f03a5fbbc63a1fea941ff4482093b5a453a264
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047862
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1085624}
    
    Bug: 1164001
    Change-Id: I2eded624cae6c5d717058646f969d456aaa70c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116980
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087673}

--- chrome/browser/chromeos/extensions/file_system_provider/service_worker_lifetime_manager.cc
+++ chrome/browser/chromeos/extensions/file_system_provider/service_worker_lifetime_manager.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/process_manager_factory.h"
-#include "third_party/blink/public/mojom/service_worker/service_worker_database.mojom-forward.h"
+#include "third_party/blink/public/mojom/service_worker/service_worker_database.mojom.h"
 
 namespace extensions::file_system_provider {
 
--- chrome/browser/chromeos/policy/dlp/dlp_file_access_copy_or_move_delegate_factory_unittest.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_file_access_copy_or_move_delegate_factory_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/policy/dlp/dlp_file_access_copy_or_move_delegate_factory.h"
+
 #include "components/file_access/file_access_copy_or_move_delegate_factory.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
