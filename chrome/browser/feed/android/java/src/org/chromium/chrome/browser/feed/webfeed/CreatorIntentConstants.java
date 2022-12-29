94a69ba07853ce3cf9fc1f230bbda47b9266f998
Wissem Gamra <wiss@chromium.org>
2022-12-29 01:44:55 +0000

    [Code Health] Cormorant: create CreatorIntentConstants to provide intent constants
    This allows us to avoid having 2 different definitions for each intent
    
    Bug: 1404010
    Change-Id: Icc1427cd848767d6aa9fb2281ae72415ca8835a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128677
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087468}

---
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/webfeed/CreatorIntentConstants.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.feed.webfeed;
+
+/**
+ * CreatorIntentConstants strings used in the intent of launching CreatorActivity
+ */
+public final class CreatorIntentConstants {
+    public static final String CREATOR_WEB_FEED_ID = "CREATOR_WEB_FEED_ID";
+    public static final String CREATOR_TITLE = "CREATOR_TITLE";
+    public static final String CREATOR_URL = "CREATOR_URL";
+
+    private CreatorIntentConstants() {}
+}
