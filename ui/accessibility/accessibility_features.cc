f7e8bff9239588c1be24b5587d76d7be7aa56d40
Josiah Krutz <josiahk@google.com>
2022-12-29 03:13:08 +0000

    Reland "[Select-to-speak] Enable UI improvements and voice switching"
    This is a reland of commit 91c87206de4d8504c9f9299f5a73851876b6f418
    after revert in crrev.com/c/4117707. Reverted for merge request to
    M110; relanding in M111 for launch.
    
    Original change's description:
    > [Select-to-speak] Enable UI improvements and voice switching
    >
    > Default enable Select-to-speak UI improvements and voice switching for
    > all users. http://go/select-to-speak-ui-improvements
    >
    > Launch: http://launch/4212650
    > Bug: 1269912, 950391, 1269891
    > Change-Id: I4ac920b8f0d7f384b70c2f83e999f0e896815bcd
    > AX-Relnotes: Enables automatic voice-switching on text which is tagged by the developer with a language code. Allows users to start Select-to-Speak on selected text using the right-click context menu in web content. Improves usability of tray button by adding new tooltip text, giving instructions to select text and stop speaking depending on the current Select-to-speak mode.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022575
    > Commit-Queue: Josiah Krutz <josiahk@google.com>
    > Reviewed-by: Katie Dektar <katie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1071256}
    
    Bug: b:263734520
    Change-Id: I5c644cfe4b7012fc05025d305b8dd2a419b5769a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4125492
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087479}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -168,7 +168,7 @@ bool IsExperimentalAccessibilityGoogleTtsLanguagePacksEnabled() {
 
 BASE_FEATURE(kExperimentalAccessibilitySelectToSpeakVoiceSwitching,
              "ExperimentalAccessibilitySelectToSpeakVoiceSwitching",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsExperimentalAccessibilitySelectToSpeakVoiceSwitchingEnabled() {
   return base::FeatureList::IsEnabled(
@@ -204,7 +204,7 @@ bool IsAccessibilitySelectToSpeakPrefsMigrationEnabled() {
 
 BASE_FEATURE(kAccessibilitySelectToSpeakContextMenuOption,
              "AccessibilitySelectToSpeakContextMenuOption",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsAccessibilitySelectToSpeakContextMenuOptionEnabled() {
   return base::FeatureList::IsEnabled(
@@ -213,7 +213,7 @@ bool IsAccessibilitySelectToSpeakContextMenuOptionEnabled() {
 
 BASE_FEATURE(kAccessibilitySelectToSpeakHoverTextImprovements,
              "AccessibilitySelectToSpeakHoverTextImprovements",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsAccessibilitySelectToSpeakHoverTextImprovementsEnabled() {
   return base::FeatureList::IsEnabled(
