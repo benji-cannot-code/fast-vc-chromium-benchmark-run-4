a57dab92bdf04d40fa9330f4a1d1a7161df73bb5
Anastasia Helfinstein <anastasi@google.com>
2022-12-29 15:19:30 +0000

    [ChromeVox] Move TreeWalkerTest to use common base
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:260599332
    Test: cq
    Change-Id: I68fac33c784e30ccfc068ed3a1699f2fd5226de5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127188
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087577}

--- chrome/browser/resources/chromeos/accessibility/common/tree_walker_test.js
+++ chrome/browser/resources/chromeos/accessibility/common/tree_walker_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Include test fixture.
 GEN_INCLUDE([
-  '../select_to_speak/select_to_speak_e2e_test_base.js',
+  'testing/common_e2e_test_base.js',
   'testing/snippets.js',
 ]);
 
@@ -13,7 +13,7 @@ GEN_INCLUDE([
  * Test fixture for tree_walker.js.
  */
 AccessibilityExtensionAutomationTreeWalkerTest =
-    class extends SelectToSpeakE2ETest {
+    class extends CommonE2ETestBase {
   /** @override */
   testGenCppIncludes() {
     super.testGenCppIncludes.call();
