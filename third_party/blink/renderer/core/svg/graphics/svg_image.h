0dbe2dd133f2b9fa62f47458a832fc00b75d08c4
Xianzhu Wang <wangxianzhu@chromium.org>
2022-12-29 12:21:30 +0000

    Remove unused SVGImage::paint_controller_
    Change-Id: I73a16513e92d8a63161fd186925eacdb9ebd0927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128590
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087553}

--- third_party/blink/renderer/core/svg/graphics/svg_image.h
+++ third_party/blink/renderer/core/svg/graphics/svg_image.h
@@ -49,7 +49,6 @@ class LayoutSVGRoot;
 class LocalFrame;
 class Node;
 class Page;
-class PaintController;
 class SVGImageChromeClient;
 class SVGImageForContainer;
 class SVGSVGElement;
@@ -225,7 +224,6 @@ class CORE_EXPORT SVGImage final : public Image {
 
   Persistent<SVGImageChromeClient> chrome_client_;
   Persistent<Page> page_;
-  std::unique_ptr<PaintController> paint_controller_;
   Persistent<AgentGroupScheduler> agent_group_scheduler_;
 
   // When an SVG image has no intrinsic size, the size depends on the default
