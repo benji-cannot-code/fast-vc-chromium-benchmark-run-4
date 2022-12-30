6392a57c2d8861f733a1dc6d89738071ca0340e9
Avi Drissman <avi@chromium.org>
2022-12-30 20:52:58 +0000

    Upgrade AuthorizationRef code
    Make ScopedAuthorizationRef movable and do clean-up across
    all the code for modernization.
    
    Bug: none
    Change-Id: I463cd808574241ba12377f7394fc3080425e8eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127123
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087795}

--- base/mac/scoped_authorizationref.h
+++ base/mac/scoped_authorizationref.h
@@ -8,21 +8,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <Security/Authorization.h>
 
-#include "base/base_export.h"
+#include <utility>
 
-// ScopedAuthorizationRef maintains ownership of an AuthorizationRef.  It is
-// patterned after the unique_ptr interface.
+#include "base/base_export.h"
+#include "base/check.h"
+
+// `ScopedAuthorizationRef` maintains ownership of an `AuthorizationRef`.  It is
+// patterned after the `unique_ptr` interface.
 
 namespace base::mac {
 
 class BASE_EXPORT ScopedAuthorizationRef {
  public:
-  explicit ScopedAuthorizationRef(AuthorizationRef authorization = NULL)
-      : authorization_(authorization) {
-  }
+  explicit ScopedAuthorizationRef(AuthorizationRef authorization = nullptr)
+      : authorization_(authorization) {}
 
   ScopedAuthorizationRef(const ScopedAuthorizationRef&) = delete;
   ScopedAuthorizationRef& operator=(const ScopedAuthorizationRef&) = delete;
+
+  ScopedAuthorizationRef(ScopedAuthorizationRef&& that)
+      : authorization_(std::exchange(that.authorization_, nullptr)) {}
+  ScopedAuthorizationRef& operator=(ScopedAuthorizationRef&& that) {
+    authorization_ = std::exchange(that.authorization_, nullptr);
+    return *this;
+  }
 
   ~ScopedAuthorizationRef() {
     if (authorization_) {
@@ -30,7 +39,7 @@ class BASE_EXPORT ScopedAuthorizationRef {
     }
   }
 
-  void reset(AuthorizationRef authorization = NULL) {
+  void reset(AuthorizationRef authorization = nullptr) {
     if (authorization_ != authorization) {
       if (authorization_) {
         FreeInternal();
@@ -51,16 +60,16 @@ class BASE_EXPORT ScopedAuthorizationRef {
     return authorization_;
   }
 
-  AuthorizationRef* get_pointer() { return &authorization_; }
+  // This is to be used only to take ownership of objects that are created
+  // by pass-by-pointer create functions. To enforce this, require that the
+  // object be reset to NULL before this may be used.
+  [[nodiscard]] AuthorizationRef* InitializeInto() {
+    DCHECK(!authorization_);
+    return &authorization_;
+  }
 
   AuthorizationRef get() const {
     return authorization_;
-  }
-
-  void swap(ScopedAuthorizationRef& that) {
-    AuthorizationRef temp = that.authorization_;
-    that.authorization_ = authorization_;
-    authorization_ = temp;
   }
 
   // ScopedAuthorizationRef::release() is like std::unique_ptr<>::release. It is
@@ -68,7 +77,7 @@ class BASE_EXPORT ScopedAuthorizationRef {
   // object to call AuthorizationFree(), use ScopedAuthorizationRef::reset().
   [[nodiscard]] AuthorizationRef release() {
     AuthorizationRef temp = authorization_;
-    authorization_ = NULL;
+    authorization_ = nullptr;
     return temp;
   }
 
