ffa5fe6b2458ae6b82fa40839b14137d69cfcc3e
Arthur Wang <wuwang@google.com>
2023-01-01 04:24:38 +0000

    Create a Pan Reporting Connector e2e test
    Bug: b:252854264
    Change-Id: I51630c4e8196e047582d2259ca34f084153c22c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119080
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087885}

--- chrome/test/enterprise/e2e/connector/__init__.py
+++ chrome/test/enterprise/e2e/connector/__init__.py
@@ -6,5 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from .realtime_reporting_bce.realtime_reporting_bce_test import *
 from .reporting_connector_chronicle.reporting_connector_chronicle_test import *
 from .reporting_connector_crowdstrike.reporting_connector_crowdstrike_test import *
+from .reporting_connector_pan.reporting_connector_pan_test import *
 from .reporting_connector_pubsub.reporting_connector_pubsub_test import *
 from .reporting_connector_splunk.reporting_connector_splunk_test import *
