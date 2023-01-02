a8210b2f5779ed580a96bee61def52e6447b591b
Mariam Ali <alimariam@google.com>
2023-01-02 09:28:52 +0000

    Privacy Sandbox: Enable all V4 prefs for testing
    This CL creates a method that enables all privacy sandbox APIs prefs for
    testing for V4 and updates all tests which enable privacy sandbox by
    `SetPrivacySandboxEnabled` to use
    `SetAllPrivacySandboxAllowedForTesting`.
    This change only covers tests which don't care about granular access
    control, other tests that call `SetPrivacySandboxEnabled` to disallow
    Privacy Sandbox will be updated to allow/disallow the specific APIs in
    the following CL.
    
    Bug: 1403781, 1378703
    Change-Id: I64bdf1a18c142777d25ecf8ddb25dbba3f8ac486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128296
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087966}

--- chrome/browser/interest_group/interest_group_permissions_browsertest.cc
+++ chrome/browser/interest_group/interest_group_permissions_browsertest.cc
@@ -48,7 +48,7 @@ class InterestGroupPermissionsBrowserTest : public InProcessBrowserTest {
     ASSERT_TRUE(https_server_->Start());
 
     PrivacySandboxSettingsFactory::GetForProfile(browser()->profile())
-        ->SetPrivacySandboxEnabled(true);
+        ->SetAllPrivacySandboxAllowedForTesting();
     // Prime the interest groups if the API is enabled.
     ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), test_url()));
     if (HasInterestGroupApi(web_contents()) &&
