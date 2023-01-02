134d0b9ee819976181a40e32ab1fbe02173efa55
Christian Xu <christianxu@google.com>
2023-01-02 10:59:53 +0000

    [iOS][Omnibox] Cleanup paste suggestion experiment
    Change-Id: I39600af0e947af740d166ecb442b672d394f72d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116793
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087981}

--- ios/chrome/browser/ui/omnibox/popup/pedal_suggestion_wrapper.mm
+++ ios/chrome/browser/ui/omnibox/popup/pedal_suggestion_wrapper.mm
@@ -43,9 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)isTabMatch {
   return NO;
 }
-- (BOOL)isClipboardMatch {
-  return NO;
-}
 - (NSNumber*)suggestionGroupId {
   return nil;
 }
--- ios/showcase/omnibox_popup/fake_autocomplete_suggestion.mm
+++ ios/showcase/omnibox_popup/fake_autocomplete_suggestion.mm
@@ -282,8 +282,6 @@ NSAttributedString* calculatorText() {
 
 @implementation FakeAutocompleteSuggestion
 
-@synthesize isClipboardMatch = _isClipboardMatch;
-
 - (instancetype)init {
   self = [super init];
   if (self) {
@@ -295,7 +293,6 @@ NSAttributedString* calculatorText() {
         [[UIImage imageNamed:@"omnibox_completion_default_favicon"]
             imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
     _icon = [[SimpleOmniboxIcon alloc] init];
-    _isClipboardMatch = NO;
   }
   return self;
 }
