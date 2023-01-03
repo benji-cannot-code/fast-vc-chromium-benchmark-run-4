a1815eb3eeb6c8c6e6cd6a18906e43a6d80b3c84
Xinan Lin <linxinan@google.com>
2023-01-03 19:36:38 +0000

    Add static_assert for enums in Preloading
    Assert that conversions of below enum should respect the destination's reserved slots.
    - PreloadingFailureReason
    - PreloadingPredictor
    
    BUG=1399932
    TEST=content_unitttest
    
    Change-Id: I27fe410daaddbaed59a891e06969c70616b75ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4115698
    Commit-Queue: Xinan Lin <linxinan@chromium.org>
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088327}

--- content/browser/preloading/preloading.cc
+++ content/browser/preloading/preloading.cc
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+static_assert(
+    static_cast<int>(ContentPreloadingPredictor::kMaxValue) <
+    static_cast<int>(PreloadingPredictor::kPreloadingPredictorContentEnd));
+
 PreloadingPredictor ToPreloadingPredictor(
     ContentPreloadingPredictor predictor) {
   return static_cast<PreloadingPredictor>(predictor);
