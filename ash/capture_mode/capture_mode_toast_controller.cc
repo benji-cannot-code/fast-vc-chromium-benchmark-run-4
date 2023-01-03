3e872ba69aa756c774d1c4a0c10ccedea6acbf26
Michele Fan <michelefan@chromium.org>
2023-01-03 16:17:50 +0000

    capture-mode-demo-tools: Add nudge to grab user's attention
    This CL adds a new nudge to grab user's attention of the new demo
    tools feature. Demo of the nudge is available at:
    http://b/263188273#comment2
    
    Fixed: b:263188273
    Test: Manually
    Change-Id: Ibcf617376b4f56b5286cd21b704273e46dc85ce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129782
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088249}

--- ash/capture_mode/capture_mode_toast_controller.cc
+++ ash/capture_mode/capture_mode_toast_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/capture_mode/capture_mode_session.h"
 #include "ash/capture_mode/capture_mode_util.h"
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/style/color_provider.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -42,10 +43,14 @@ constexpr base::TimeDelta kDelayToDismissToast = base::Seconds(6);
 
 std::u16string GetCaptureToastLabelOnToastType(
     CaptureToastType capture_toast_type) {
+  const int nudge_message_id =
+      features::AreCaptureModeDemoToolsEnabled()
+          ? IDS_ASH_SCREEN_CAPTURE_SHOW_DEMO_TOOLS_USER_NUDGE
+          : IDS_ASH_SCREEN_CAPTURE_SHOW_CAMERA_USER_NUDGE;
   const int message_id =
       capture_toast_type == CaptureToastType::kCameraPreview
           ? IDS_ASH_SCREEN_CAPTURE_SURFACE_TOO_SMALL_USER_NUDGE
-          : IDS_ASH_SCREEN_CAPTURE_SHOW_CAMERA_USER_NUDGE;
+          : nudge_message_id;
   return l10n_util::GetStringUTF16(message_id);
 }
 
