1bf9d63e845aa061c4251f081330903006ecfe47
Henrique Nakashima <hnakashima@chromium.org>
2023-01-03 21:35:51 +0000

    [Android] Update references to Android M- in .content
    Marshmallow is not supported anymore.
    
    Bug: 1385893
    Change-Id: If8dacb7fe2558c448f577336f9ec8a7a7effec40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112794
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088360}

--- content/public/android/java/src/org/chromium/content/browser/input/ThreadedInputConnectionFactory.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ThreadedInputConnectionFactory.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.input;
 
-import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.view.View;
@@ -130,8 +129,7 @@ public class ThreadedInputConnectionFactory implements ChromiumBaseInputConnecti
 
         // https://crbug.com/820756
         final String htcMailPackageId = "com.htc.android.mail";
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N
-                || htcMailPackageId.equals(view.getContext().getPackageName())) {
+        if (htcMailPackageId.equals(view.getContext().getPackageName())) {
             // IMM can internally ignore subsequent activation requests, e.g., by checking
             // mServedConnecting.
             if (mCheckInvalidator != null) mCheckInvalidator.invalidate();
