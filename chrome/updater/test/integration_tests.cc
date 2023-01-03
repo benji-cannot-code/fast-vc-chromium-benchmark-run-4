cf7b5956ca5b9d3d5a6d1836884cbe3a940946f4
Sorin Jianu <sorin@chromium.org>
2023-01-03 21:41:29 +0000

    updater: add missing ASSERT_NO_FATAL_FAILURE call in integration tests.
    This is a follow up for
    https://chromium-review.googlesource.com/c/chromium/src/+/4128405
    Use ASSERT_NO_FATAL_FAILURE everywhere in integration tests.
    
    Fixed: 1403998
    Change-Id: Ie61ffc74c8c314c9e27f3b039c2025682269da8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134404
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088363}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -951,7 +951,7 @@ TEST_F(IntegrationTest, InstallDataIndex) {
 }
 
 TEST_F(IntegrationTest, MigrateLegacyUpdater) {
-  SetupFakeLegacyUpdaterData();
+  ASSERT_NO_FATAL_FAILURE(SetupFakeLegacyUpdaterData());
   ASSERT_NO_FATAL_FAILURE(Install());
   ASSERT_NO_FATAL_FAILURE(ExpectInstalled());
   ASSERT_NO_FATAL_FAILURE(ExpectLegacyUpdaterDataMigrated());
