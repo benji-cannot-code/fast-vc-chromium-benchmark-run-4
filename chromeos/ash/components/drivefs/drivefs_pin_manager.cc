4e479edc9f1fedb7a0b428af72dd15c86a93e7d3
François Degros <fdegros@chromium.org>
2023-01-03 00:26:52 +0000

    DriveFsPinManager: Tweak the logging of human-readable sizes
    Bug: b:261530520, b:263320344
    Test: out/Default/chromeos_unittests --gtest_filter=DriveFsPinManagerTest.*
    Change-Id: Id4dc8156b2852cd2b1a454c4b4541011ef723db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128640
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088078}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.cc
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.cc
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/drivefs/drivefs_pin_manager.h"
 
+#include <locale>
 #include <utility>
 #include <vector>
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
 #include "base/logging.h"
+#include "base/no_destructor.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
@@ -55,6 +57,12 @@ class FreeDiskSpaceImpl : public FreeDiskSpaceDelegate {
   }
 };
 
+class NumPunct : public std::numpunct<char> {
+ private:
+  char do_thousands_sep() const override { return ','; }
+  std::string do_grouping() const override { return "\3"; }
+};
+
 }  // namespace
 
 std::ostream& operator<<(std::ostream& out, HumanReadableSize size) {
@@ -68,8 +76,16 @@ std::ostream& operator<<(std::ostream& out, HumanReadableSize size) {
     i = -i;
   }
 
+  {
+    static const base::NoDestructor<std::locale> with_separators(
+        std::locale::classic(), new NumPunct);
+    std::locale old_locale = out.imbue(*with_separators);
+    out << i;
+    out.imbue(std::move(old_locale));
+  }
+
   if (i < 1024) {
-    return out << i << " B";
+    return out;
   }
 
   double d = static_cast<double>(i) / 1024;
@@ -80,7 +96,7 @@ std::ostream& operator<<(std::ostream& out, HumanReadableSize size) {
   }
 
   const int precision = d < 10 ? 2 : d < 100 ? 1 : 0;
-  return out << base::StringPrintf("%.*f %ciB", precision, d, *unit);
+  return out << base::StringPrintf(" (%.*f %c)", precision, d, *unit);
 }
 
 std::ostream& operator<<(std::ostream& out, const SetupError error) {
