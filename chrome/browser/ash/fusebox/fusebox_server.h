08809174df61a8929af32cc5a7d6e29a572ad2c8
Nigel Tao <nigeltao@chromium.org>
2023-01-03 00:27:10 +0000

    fusebox: serialize reads and writes
    Prior to this commit, a FuseFileMapEntry could have at most one read in
    flight, and ditto for writes, but it could have both a read and write up
    at the same time.
    
    After this commit, it has at most one operation in-flight, whether the
    operation is a read or a write.
    
    This simplifies some future work, using file descriptors and not just
    storage::FileStreamReader and storage::FileStreamWriter, as a file
    descriptor has a single seek offset.
    
    Also run the read/write callbacks even if the WeakPtr is invalidated.
    
    BUG=b:255703917
    TEST=manual
    
    Change-Id: Ia6f0b8cf91bf530dc254045e9bee0be76de6de34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110308
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088079}

--- chrome/browser/ash/fusebox/fusebox_server.h
+++ chrome/browser/ash/fusebox/fusebox_server.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/fusebox/fusebox_staging.pb.h"
 #include "storage/browser/file_system/async_file_util.h"
 #include "storage/browser/file_system/file_system_context.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 
 class Profile;
 
@@ -198,6 +199,7 @@ class Server {
 
   using PendingRead2 = std::pair<Read2RequestProto, Read2Callback>;
   using PendingWrite2 = std::pair<Write2RequestProto, Write2Callback>;
+  using PendingOp = absl::variant<PendingRead2, PendingWrite2>;
 
   struct FuseFileMapEntry {
     FuseFileMapEntry(scoped_refptr<storage::FileSystemContext> fs_context_arg,
@@ -209,15 +211,16 @@ class Server {
 
     void DoRead2(const Read2RequestProto& request, Read2Callback callback);
     void DoWrite2(const Write2RequestProto& request, Write2Callback callback);
+    void Do(PendingOp& op,
+            base::WeakPtr<Server> weak_ptr_server,
+            uint64_t fuse_handle);
 
     const scoped_refptr<storage::FileSystemContext> fs_context_;
     const bool readable_;
     const bool writable_;
 
-    bool has_in_flight_read_ = false;
-    bool has_in_flight_write_ = false;
-    base::circular_deque<PendingRead2> pending_reads_;
-    base::circular_deque<PendingWrite2> pending_writes_;
+    bool has_in_flight_op_ = false;
+    base::circular_deque<PendingOp> pending_ops_;
 
     base::SequenceBound<ReadWriter> seqbnd_read_writer_;
   };
