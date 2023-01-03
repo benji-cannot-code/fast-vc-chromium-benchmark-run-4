00cf2ed5c073bbd1ee61e6d8a121f7e8f4efb41d
dpapad <dpapad@chromium.org>
2023-01-03 10:32:50 +0000

    Remove dot indicator in SidePanelToolbarButton.
    It is no longer used after the removal of the
    features::kUnifiedSidePanel feature flag.
    
    Bug: 1401515
    Change-Id: I35944fa60caf18f6f1c85386f44648c92f02fe9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120639
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088155}

--- chrome/browser/ui/views/performance_controls/battery_saver_button_unittest.cc
+++ chrome/browser/ui/views/performance_controls/battery_saver_button_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/features.h"
 #include "components/performance_manager/public/user_tuning/prefs.h"
 #include "components/prefs/testing_pref_service.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/events/event_utils.h"
 #include "ui/views/bubble/bubble_dialog_model_host.h"
 #include "ui/views/interaction/element_tracker_views.h"
--- chrome/browser/ui/views/toolbar/side_panel_toolbar_button_unittest.cc
+++ chrome/browser/ui/views/toolbar/side_panel_toolbar_button_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/test/icu_test_util.h"
 #include "base/test/scoped_feature_list.h"
-#include "chrome/browser/reading_list/reading_list_model_factory.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -16,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/side_panel_toolbar_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/common/pref_names.h"
-#include "components/reading_list/core/reading_list_model.h"
-#include "components/reading_list/core/reading_list_test_utils.h"
 #include "ui/events/event_utils.h"
 #include "ui/gfx/image/image_unittest_util.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -26,58 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class SidePanelToolbarButtonTest : public TestWithBrowserView {
  public:
-  void SetUp() override {
-    TestWithBrowserView::SetUp();
-
-    model_ = ReadingListModelFactory::GetForBrowserContext(profile());
-    ReadingListLoadObserver(model_).Wait();
-  }
-
-  TestingProfile::TestingFactories GetTestingFactories() override {
-    TestingProfile::TestingFactories factories =
-        TestWithBrowserView::GetTestingFactories();
-    factories.emplace_back(
-        ReadingListModelFactory::GetInstance(),
-        ReadingListModelFactory::GetDefaultFactoryForTesting());
-    return factories;
-  }
-
   SidePanelToolbarButton* GetSidePanelToolbarButton() {
     return browser_view()->toolbar()->side_panel_button();
   }
-
-  ReadingListModel* model() { return model_; }
-
- private:
-  raw_ptr<ReadingListModel> model_;
 };
-
-TEST_F(SidePanelToolbarButtonTest, DotIndicatorVisibleWithUnreadItems) {
-  if (browser_view()->side_panel_coordinator()) {
-    GTEST_SKIP() << "The unified side panel doesn't use the dot indicator so "
-                    "this test shouldn't run";
-  }
-  // Verify the dot indicator is seen when there is an unseen entry.
-  model()->AddOrReplaceEntry(GURL("http://foo/1"), "Tab 1",
-                             reading_list::EntrySource::ADDED_VIA_CURRENT_APP,
-                             /*estimated_read_time=*/base::TimeDelta());
-  SidePanelToolbarButton* const side_panel_button = GetSidePanelToolbarButton();
-  ASSERT_TRUE(side_panel_button->GetDotIndicatorVisibilityForTesting());
-
-  // Verify the dot indicator is hidden once the toolbar button is clicked.
-  ui::MouseEvent e(ui::ET_MOUSE_PRESSED, gfx::Point(), gfx::Point(),
-                   ui::EventTimeForNow(), 0, 0);
-  views::test::ButtonTestApi test_api(side_panel_button);
-  test_api.NotifyClick(e);
-  ASSERT_FALSE(side_panel_button->GetDotIndicatorVisibilityForTesting());
-
-  // Verify the dot indicator is hidden when entries are added while the panel
-  // is open.
-  model()->AddOrReplaceEntry(GURL("http://foo/2"), "Tab 2",
-                             reading_list::EntrySource::ADDED_VIA_CURRENT_APP,
-                             /*estimated_read_time=*/base::TimeDelta());
-  ASSERT_FALSE(side_panel_button->GetDotIndicatorVisibilityForTesting());
-}
 
 // Verify correct buttons are shown when side panel alignment is changed.
 TEST_F(SidePanelToolbarButtonTest, SetCorrectIconInLTR) {
