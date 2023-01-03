35de9733d1f66ac6b838e6d1339581e34d1eda95
Wes Okuhara <wesokuhara@google.com>
2023-01-03 23:23:40 +0000

    CrOS Settings: Remove dependency on i18n_setup.js
    Avoid importing i18n_setup.js for ChromeOS Settings-specific code
    to reduce the dependency on Browser Settings by importing strings.m.js
    in the JS entry point and importing loadTimeData directly.
    
    Bug: b:263413628
    Test: browser_tests --gtest_filter='OSSettings*'
    Change-Id: I3275581c3c7f28f7407521a8a5f96c582d6bcacb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4121434
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1088453}

--- chrome/browser/resources/settings/chromeos/router.js
+++ chrome/browser/resources/settings/chromeos/router.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../i18n_setup.js';
-
 import {assert} from 'chrome://resources/ash/common/assert.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 
