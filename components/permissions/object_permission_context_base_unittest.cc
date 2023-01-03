1175a56087edbfcba7d07d5643ca2b25bea982d4
Nicolás Peña Moreno <npm@chromium.org>
2023-01-03 22:20:58 +0000

    [CodeHealth] Remove Type::DICTIONARY from components/permissions
    Bug: 1291670
    Change-Id: I68c6a5b757cfe34928f7ea92fb7a8e748116f467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129034
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088387}

--- components/permissions/object_permission_context_base_unittest.cc
+++ components/permissions/object_permission_context_base_unittest.cc
@@ -33,8 +33,9 @@ class TestObjectPermissionContext : public ObjectPermissionContextBase {
   ~TestObjectPermissionContext() override = default;
 
   bool IsValidObject(const base::Value& object) override {
-    return object.DictSize() == 2 && object.FindKey(kRequiredKey1) &&
-           object.FindKey(kRequiredKey2);
+    const base::Value::Dict& dict = object.GetDict();
+    return dict.size() == 2 && dict.Find(kRequiredKey1) &&
+           dict.Find(kRequiredKey2);
   }
 
   std::u16string GetObjectDisplayName(const base::Value& object) override {
@@ -42,7 +43,7 @@ class TestObjectPermissionContext : public ObjectPermissionContextBase {
   }
 
   std::string GetKeyForObject(const base::Value& object) override {
-    return *object.FindStringKey(kRequiredKey1);
+    return *object.GetDict().FindString(kRequiredKey1);
   }
 };
 
@@ -55,13 +56,13 @@ class ObjectPermissionContextBaseTest : public testing::Test {
         url2_("https://chromium.org"),
         origin1_(url::Origin::Create(url1_)),
         origin2_(url::Origin::Create(url2_)),
-        object1_(base::Value::Type::DICTIONARY),
-        object2_(base::Value::Type::DICTIONARY),
+        object1_(base::Value::Type::DICT),
+        object2_(base::Value::Type::DICT),
         context_(browser_context()) {
-    object1_.SetStringKey(kRequiredKey1, "value1");
-    object1_.SetStringKey(kRequiredKey2, "value2");
-    object2_.SetStringKey(kRequiredKey1, "value3");
-    object2_.SetStringKey(kRequiredKey2, "value4");
+    object1_.GetDict().Set(kRequiredKey1, "value1");
+    object1_.GetDict().Set(kRequiredKey2, "value2");
+    object2_.GetDict().Set(kRequiredKey1, "value3");
+    object2_.GetDict().Set(kRequiredKey2, "value4");
   }
 
   ~ObjectPermissionContextBaseTest() override = default;
