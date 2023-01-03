ea5814e72530cf4fb148ac8fee329e52383cb5d7
Darren Shen <shend@chromium.org>
2023-01-03 01:38:56 +0000

    inputs: Rename SetCursorBounds to SetCursorAndCompositionBounds.
    The longer name describes it better. Also rename composition_head to
    composition_bounds to better match the method name.
    
    BUG=b:245020074
    
    Change-Id: I22355f9b600c285048b7a62133a51c589808227c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127695
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088089}

--- ui/base/ime/ash/mock_ime_candidate_window_handler.cc
+++ ui/base/ime/ash/mock_ime_candidate_window_handler.cc
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 MockIMECandidateWindowHandler::MockIMECandidateWindowHandler()
-    : set_cursor_bounds_call_count_(0),
-      update_lookup_table_call_count_(0) {
-}
+    : set_cursor_and_composition_bounds_call_count_(0),
+      update_lookup_table_call_count_(0) {}
 
 MockIMECandidateWindowHandler::~MockIMECandidateWindowHandler() = default;
 
@@ -31,10 +30,10 @@ void MockIMECandidateWindowHandler::UpdatePreeditText(
     uint32_t cursor_pos,
     bool visible) {}
 
-void MockIMECandidateWindowHandler::SetCursorBounds(
+void MockIMECandidateWindowHandler::SetCursorAndCompositionBounds(
     const gfx::Rect& cursor_bounds,
-    const gfx::Rect& composition_head) {
-  ++set_cursor_bounds_call_count_;
+    const gfx::Rect& composition_bounds) {
+  ++set_cursor_and_composition_bounds_call_count_;
 }
 
 gfx::Rect MockIMECandidateWindowHandler::GetCursorBounds() const {
@@ -42,7 +41,7 @@ gfx::Rect MockIMECandidateWindowHandler::GetCursorBounds() const {
 }
 
 void MockIMECandidateWindowHandler::Reset() {
-  set_cursor_bounds_call_count_ = 0;
+  set_cursor_and_composition_bounds_call_count_ = 0;
   update_lookup_table_call_count_ = 0;
 }
 
