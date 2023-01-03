61a19ede5bb3a843ba232c0ecaf9c3069d2a8306
Anastasia Helfinstein <anastasi@google.com>
2023-01-03 23:26:51 +0000

    [ChromeVox] Format log_types.js
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:260599332
    Test: cq
    Change-Id: Id837691e8c04aef6807a20ea2706f3dbf1f66bf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127834
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1088458}

--- chrome/browser/resources/chromeos/accessibility/chromevox/common/log_types.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/log_types.js
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TreeDumper} from './tree_dumper.js';
 import {QueueMode} from './tts_types.js';
 
+const AutomationEvent = chrome.automation.AutomationEvent;
+const EventType = chrome.automation.EventType;
+
 /**
- * List of all types of logs supported.
+ * Supported log types.
  * Note that filter type checkboxes are shown in this order at the log page.
  * @enum {string}
  */
@@ -37,14 +40,10 @@ export let SerializableLog;
 
 export class BaseLog {
   constructor(logType) {
-    /**
-     * @type {!LogType}
-     */
+    /** @public {!LogType} */
     this.logType = logType;
 
-    /**
-     * @type {!Date}
-     */
+    /** @public {!Date} */
     this.date = new Date();
   }
 
@@ -60,36 +59,21 @@ export class BaseLog {
   }
 }
 
-
 export class EventLog extends BaseLog {
-  /**
-   * @param {!chrome.automation.AutomationEvent} event
-   */
+  /** @param {!AutomationEvent} event */
   constructor(event) {
     super(LogType.EVENT);
 
-    /**
-     * @type {chrome.automation.EventType}
-     * @private
-     */
+    /** @private {EventType} */
     this.type_ = event.type;
 
-    /**
-     * @type {string | undefined}
-     * @private
-     */
+    /** @private {string|undefined} */
     this.targetName_ = event.target.name;
 
-    /**
-     * @type {string | undefined}
-     * @private
-     */
+    /** @private {string|undefined} */
     this.rootName_ = event.target.root.name;
 
-    /**
-     * @type {string | undefined}
-     * @private
-     */
+    /** @private {string|undefined} */
     this.docUrl_ = event.target.docUrl;
   }
 
@@ -100,7 +84,6 @@ export class EventLog extends BaseLog {
   }
 }
 
-
 export class SpeechLog extends BaseLog {
   /**
    * @param {!string} textString
@@ -110,22 +93,13 @@ export class SpeechLog extends BaseLog {
   constructor(textString, queueMode, category) {
     super(LogType.SPEECH);
 
-    /**
-     * @type {string}
-     * @private
-     */
+    /** @private {string} */
     this.textString_ = textString;
 
-    /**
-     * @type {QueueMode}
-     * @private
-     */
+    /** @private {QueueMode} */
     this.queueMode_ = queueMode;
 
-    /**
-     * @type {?string}
-     * @private
-     */
+    /** @private {?string} */
     this.category_ = category;
   }
 
@@ -149,7 +123,6 @@ export class SpeechLog extends BaseLog {
   }
 }
 
-
 export class TextLog extends BaseLog {
   /**
    * @param {string} logStr
@@ -158,10 +131,7 @@ export class TextLog extends BaseLog {
   constructor(logStr, logType) {
     super(logType);
 
-    /**
-     * @type {string}
-     * @private
-     */
+    /** @private {string} */
     this.logStr_ = logStr;
   }
 
@@ -171,23 +141,17 @@ export class TextLog extends BaseLog {
   }
 }
 
-
 export class TreeLog extends BaseLog {
-  /**
-   * @param {!TreeDumper} logTree
-   */
-  constructor(logTree) {
+  /** @param {!TreeDumper} tree */
+  constructor(tree) {
     super(LogType.TREE);
 
-    /**
-     * @type {!TreeDumper}
-     * @private
-     */
-    this.logTree_ = logTree;
+    /** @private {!TreeDumper} */
+    this.tree_ = tree;
   }
 
   /** @override */
   toString() {
-    return this.logTree_.treeToString();
+    return this.tree_.treeToString();
   }
 }
