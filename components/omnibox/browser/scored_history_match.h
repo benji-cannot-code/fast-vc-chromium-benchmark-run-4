852bd4ff335247e4e6b1e87696516747a252c5d2
manukh <manukh@chromium.org>
2023-01-03 21:55:24 +0000

    [omnibox] Add friend class for `ScoredHistoryMatch`.
    To avoid an ever-growing list of `FRIEND_TEST_ALL_PREFIXES`, this CL
    replaces the existing 7 instances of it with a `friend class
    ScoredHistoryMatchPublic`, where `ScoredHistoryMatchPublic` is a
    test-only class that makes all the needed fields public.
    
    Change-Id: I5d31673846f70df75f51be48a4ab58980531b41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129130
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088372}

--- components/omnibox/browser/scored_history_match.h
+++ components/omnibox/browser/scored_history_match.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "omnibox_event.pb.h"
 
-class ScoredHistoryMatchTest;
-
 // An HistoryMatch that has a score as well as metrics defining where in the
 // history item's URL and/or page title matches have occurred.
 struct ScoredHistoryMatch : public history::HistoryMatch {
@@ -116,14 +114,7 @@ struct ScoredHistoryMatch : public history::HistoryMatch {
   metrics::OmniboxEventProto::Suggestion::ScoringSignals scoring_signals;
 
  private:
-  friend class ScoredHistoryMatchTest;
-  FRIEND_TEST_ALL_PREFIXES(ScoredHistoryMatchTest, GetDocumentSpecificityScore);
-  FRIEND_TEST_ALL_PREFIXES(ScoredHistoryMatchTest, GetFinalRelevancyScore);
-  FRIEND_TEST_ALL_PREFIXES(ScoredHistoryMatchTest, GetFrequency);
-  FRIEND_TEST_ALL_PREFIXES(ScoredHistoryMatchTest, GetHQPBucketsFromString);
-  FRIEND_TEST_ALL_PREFIXES(ScoredHistoryMatchTest, ScoringBookmarks);
-  FRIEND_TEST_ALL_PREFIXES(ScoredHistoryMatchTest, ScoringScheme);
-  FRIEND_TEST_ALL_PREFIXES(ScoredHistoryMatchTest, ScoringTLD);
+  friend class ScoredHistoryMatchPublic;
 
   // Initialize ScoredHistoryMatch statics. Must be called before any other
   // method of ScoredHistoryMatch and before creating any instances.
