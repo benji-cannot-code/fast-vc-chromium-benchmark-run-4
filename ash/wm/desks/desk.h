bed2b574c871730354797b343bb953730ca60ece
Yongshun Liu <yongshun@chromium.org>
2023-01-03 22:43:07 +0000

    ash: Cleanup unused header files for desk
    This cleans up unused header files for desk.
    
    Test: Manual
    Bug: N/A
    Change-Id: I33366e3a95ff30e081366c646b40bc55f82aefb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4123089
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088412}

--- ash/wm/desks/desk.h
+++ ash/wm/desks/desk.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/guid.h"
 #include "base/observer_list.h"
-#include "base/time/clock.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "ui/aura/window_observer.h"
