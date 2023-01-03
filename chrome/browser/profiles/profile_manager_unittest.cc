a03566e929be8604eabbe027e8f903fd6c898b3a
Yue Li <updowndota@google.com>
2023-01-03 22:50:26 +0000

    Avatars: Skip downloading default images under test environment
    Bug: b/264278859
    Test: Run existing tests with feature flag default on
    Change-Id: I28078d440cf3210d5261f7f154d0ac5b5df0943a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133504
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088424}

--- chrome/browser/profiles/profile_manager_unittest.cc
+++ chrome/browser/profiles/profile_manager_unittest.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/arc_prefs.h"
 #include "ash/components/arc/session/arc_management_transition.h"
 #include "ash/constants/ash_switches.h"
+#include "chrome/browser/ash/login/users/avatar/user_image_manager_impl.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/ash/login/users/scoped_test_user_manager.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
@@ -160,6 +161,7 @@ class ProfileManagerTest : public testing::Test {
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     base::CommandLine::ForCurrentProcess()->AppendSwitch(switches::kTestType);
+    ash::UserImageManagerImpl::SkipDefaultUserImageDownloadForTesting();
     wallpaper_controller_client_ =
         std::make_unique<WallpaperControllerClientImpl>();
     wallpaper_controller_client_->InitForTesting(&test_wallpaper_controller_);
