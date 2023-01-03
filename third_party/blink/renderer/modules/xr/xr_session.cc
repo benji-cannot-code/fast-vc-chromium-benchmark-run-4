d65951b1020a4343e126845f36de0907684890ee
Alex Cooper <alcooper@chromium.org>
2023-01-03 23:05:14 +0000

    Remove unused WebXR metric
    XR.WebXR.ReferenceSpace.Succeeded is duplicated by an existing UKM
    metric. It's an open question if we still need the UKM or if we could
    instead use UMA metrics for all the features; however, if we did opt for
    UMAs we'd want to design a more coheseive set of UMAs with the other,
    currently unimplemented, WebXR features.
    
    Fixed: 1404554
    Change-Id: I119715c604e38764103152d2f37660940396889d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133725
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088433}

--- third_party/blink/renderer/modules/xr/xr_session.cc
+++ third_party/blink/renderer/modules/xr/xr_session.cc
@@ -625,9 +625,6 @@ ScriptPromise XRSession::requestReferenceSpace(
   ScriptPromise promise = resolver->Promise();
   resolver->Resolve(reference_space);
 
-  UMA_HISTOGRAM_ENUMERATION("XR.WebXR.ReferenceSpace.Succeeded",
-                            requested_type);
-
   return promise;
 }
 
