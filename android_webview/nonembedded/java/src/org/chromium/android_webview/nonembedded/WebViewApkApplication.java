06e5127ebfbe26552eb77651eac02da5dfc47ff7
Henrique Nakashima <hnakashima@chromium.org>
2023-01-03 19:35:26 +0000

    [Android] Remove Marshmallow code in org.chromium.android_webview
    Marshmallow is not supported anymore.
    
    Bug: 1385893
    Change-Id: I13b518b8edef7c10554df2c5a98dc30bfd2435b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108433
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088325}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/WebViewApkApplication.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/WebViewApkApplication.java
@@ -9,7 +9,6 @@ import android.app.Application;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.Build;
 
 import com.android.webview.chromium.WebViewLibraryPreloader;
 
@@ -113,9 +112,7 @@ public class WebViewApkApplication extends Application {
             controller.registerActions(NonembeddedSafeModeActionsList.sList);
         }
 
-        // Limit to N+ since external services were added in N.
-        if (!LibraryLoader.getInstance().isLoadedByZygote()
-                && Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        if (!LibraryLoader.getInstance().isLoadedByZygote()) {
             LibraryLoader.getInstance().setNativeLibraryPreloader(new WebViewLibraryPreloader());
         }
     }
--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/ComponentsProviderService.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/ComponentsProviderService.java
@@ -12,7 +12,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
@@ -26,7 +25,6 @@ import org.chromium.android_webview.common.services.ServiceNames;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.FileUtils;
 import org.chromium.base.Log;
-import org.chromium.base.compat.ApiHelperForN;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
@@ -295,18 +293,10 @@ public class ComponentsProviderService extends Service {
         }
     }
 
-    // Use JobScheduler.getPendingJob() if it's available. Otherwise, fall back to iterating over
-    // all jobs to find the one we want.
     // TODO(crbug.com/1189126): move this to utils class
     @VisibleForTesting
     public static boolean isJobScheduled(JobScheduler scheduler, int jobId) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            for (JobInfo info : scheduler.getAllPendingJobs()) {
-                if (info.getId() == jobId) return true;
-            }
-            return false;
-        }
-        return ApiHelperForN.getPendingJob(scheduler, jobId) != null;
+        return scheduler.getPendingJob(jobId) != null;
     }
 
     @VisibleForTesting
