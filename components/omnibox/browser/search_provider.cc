fdff7e40f9caf179c905723140d44672d25f56e7
manukh <manukh@chromium.org>
2023-01-04 17:33:08 +0000

    [omnibox] Clean up usages of deprecated `field_trial_triggered`.
    We used to use `field_trial_triggered` &
    `field_trial_triggered_in_session`, field trial hashes logged per
    provider per session to indicate which features might have affected the
    omnibox results.
    
    These were deprecated; `feature_triggered_in_session` replaced the
    latter, essentially equivalent except logged per event (rather than per
    provider) and using a feature enum rather than field trial hashes. The
    former, `field_trial_triggered` was deprecated without a replacement.
    (cl/337509845)
    
    This CL cleans up the code logging the 2 deprecated fields.
    
    The clipboard, doc, pedal, search, and zero-search providers had code
    logging to the old fields:
    - The histograms for clipboard and pedals were never added to UMA, so
      those definitely aren't used. With this CL, these are no longer log
      when triggered.
    - Though we have histograms for doc slicing, there are no ongoing
      experiments using them. It too is no longer logged when triggered.
    - The search and zero-search likewise have histograms unused since mid
      February 2022. Despite that, this CL does migrate these; they will be
      logged to the new fields.
      (https://uma.googleplex.com/p/chrome/timeline_v2?sid=b3e50c1e709294ae2240e7f8c7cd66d7)
    
    The pedals provider used to log to the search provider info. This was
    problematic: if we ran any pedal and search experiments simultaneously,
    the sliced histograms would have confounded which event triggered which.
    This concern is moot with this CL.
    
    The search and zero-search providers additionally send whether they
    triggered as part of the URL path param `aqs` (assisted query stats).
    E.g.
    https://www.google.com/search?q=<query>&rlz=1C1GCEU_en&oq=test&aqs=chrome..69i57j69i61l5j69i60j69i61.697j0j4&sourceid=chrome&ie=UTF-8
    This is preserved with this CL. It's used for rasta slicing.
    
    Most features are set triggered locally. E.g. if the doc provider
    returns results, it might log the doc feature triggered. The search and
    zero-search providers instead trigger if their respective remote
    responses include `"google:fieldtrialtriggered":true`. This allowed
    mendel experiments to slice omnibox UMA metrics. Though this is not used
    currently (see UMA link above), this CL does migrate these to the new
    fields using catch-all `kRemoteSearchFeature` &
    `kRemoteZeroSearchFeature` enums. This preserves existing granulity, but
    if we need to distinguish between specific search or zero-search
    features, we can add more enums later.
    
    This also simplifies providers, as they duplicated the code handling the
    old fields, whereas the new fields are handled by a shared service.
    
    Change-Id: I8eb850592dc7322b4d0a6aa115f05c2e02cbf95b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4126664
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088792}

--- components/omnibox/browser/search_provider.cc
+++ components/omnibox/browser/search_provider.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_result.h"
 #include "components/omnibox/browser/keyword_provider.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
+#include "components/omnibox/browser/omnibox_triggered_feature_service.h"
 #include "components/omnibox/browser/suggestion_answer.h"
 #include "components/omnibox/browser/url_prefix.h"
 #include "components/omnibox/common/omnibox_features.h"
@@ -197,11 +198,6 @@ int SearchProvider::CalculateRelevanceForKeywordVerbatim(
              : 1100;
 }
 
-void SearchProvider::ResetSession() {
-  set_field_trial_triggered(false);
-  set_field_trial_triggered_in_session(false);
-}
-
 bool SearchProvider::CanSendCurrentPageURLInRequest(
     const GURL& current_page_url,
     const TemplateURL* template_url,
@@ -252,7 +248,6 @@ void SearchProvider::Start(const AutocompleteInput& input,
   model->Load();
 
   matches_.clear();
-  set_field_trial_triggered(false);
 
   // At this point, we could exit early if the input is on-focus or empty,
   // because offering suggestions in those scenarios is handled by
@@ -460,11 +455,9 @@ void SearchProvider::OnURLLoadComplete(
           *data, GetInput(is_keyword), client()->GetSchemeClassifier(), -1,
           is_keyword, results);
       if (results_updated) {
-        if (!field_trial_triggered()) {
-          set_field_trial_triggered(results->field_trial_triggered);
-        }
-        if (!field_trial_triggered_in_session()) {
-          set_field_trial_triggered_in_session(results->field_trial_triggered);
+        if (results->field_trial_triggered) {
+          client()->GetOmniboxTriggeredFeatureService()->FeatureTriggered(
+              OmniboxTriggeredFeatureService::Feature::kRemoteSearchFeature);
         }
         SortResults(is_keyword, results);
         PrefetchImages(results);
