e3d1bf9f25d8ce4f874f2b60c184a6d9f84a2d9c
Piotr Bialecki <bialpio@chromium.org>
2023-01-04 18:02:24 +0000

    WebXR: migrate depth api to non-deprecated equivalent
    Fixed: 1394735,1340301
    Change-Id: I2d77813fd23973e05a663c4a112d3b0b34e8555c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133949
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088811}

--- device/vr/android/arcore/arcore_shim.cc
+++ device/vr/android/arcore/arcore_shim.cc
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// TODO(https://crbug.com/1394735): Remove this again.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
 // Run DO macro for every function defined in the API.
 #define FOR_EACH_API_FN(DO)                                        \
   DO(ArAnchor_detach)                                              \
@@ -68,7 +64,7 @@ namespace {
   DO(ArConfig_setFocusMode)                                        \
   DO(ArConfig_setLightEstimationMode)                              \
   DO(ArFrame_acquireCamera)                                        \
-  DO(ArFrame_acquireDepthImage)                                    \
+  DO(ArFrame_acquireDepthImage16Bits)                              \
   DO(ArFrame_create)                                               \
   DO(ArFrame_destroy)                                              \
   DO(ArFrame_getLightEstimate)                                     \
@@ -160,9 +156,6 @@ void LoadArCoreApi(void* handle, ArCoreApi* api) {
 }
 
 #undef FOR_EACH_API_FN
-
-// TODO(https://crbug.com/1394735): Remove this again.
-#pragma GCC diagnostic pop
 
 void* g_sdk_handle = nullptr;
 ArCoreApi* g_arcore_api = nullptr;
@@ -517,11 +510,11 @@ void ArFrame_acquireCamera(const ArSession* session,
   return g_arcore_api->impl_ArFrame_acquireCamera(session, frame, out_camera);
 }
 
-ArStatus ArFrame_acquireDepthImage(const ArSession* session,
-                                   const ArFrame* frame,
-                                   ArImage** out_depth_image) {
-  return g_arcore_api->impl_ArFrame_acquireDepthImage(session, frame,
-                                                      out_depth_image);
+ArStatus ArFrame_acquireDepthImage16Bits(const ArSession* session,
+                                         const ArFrame* frame,
+                                         ArImage** out_depth_image) {
+  return g_arcore_api->impl_ArFrame_acquireDepthImage16Bits(session, frame,
+                                                            out_depth_image);
 }
 
 void ArFrame_create(const ArSession* session, ArFrame** out_frame) {
