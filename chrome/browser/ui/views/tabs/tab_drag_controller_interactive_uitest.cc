0d2620295f68ab6e6af6ca97b7fd9649e920f3b8
Darren Shen <shend@chromium.org>
2023-01-04 02:40:42 +0000

    sheriff: Disable flaky TabDragging/DetachToBrowserTabDragControllerTest.OffsetForDraggingTab for Mac.
    Already disabled for Win/Linux, disabling for Mac as well.
    
    Bug: 979013
    Change-Id: Iabf95c2bdc4581e0b6ee338f140b45ff4ad3c08d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135293
    Reviewed-by: Michael Cui <mlcui@google.com>
    Owners-Override: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088539}

--- chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
@@ -3164,8 +3164,9 @@ void DragWindowAndVerifyOffset(DetachToBrowserTabDragControllerTest* test,
 
 }  // namespace
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
 // TODO(mukai): enable this test on Windows and Linux.
+// TODO(crbug.com/979013): flaky on Mac
 #define MAYBE_OffsetForDraggingTab DISABLED_OffsetForDraggingTab
 #else
 #define MAYBE_OffsetForDraggingTab OffsetForDraggingTab
