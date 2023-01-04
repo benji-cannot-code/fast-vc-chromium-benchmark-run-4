db902b6210eed0941c8b04091ce83b4d8d4dd429
Artem Sumaneev <asumaneev@google.com>
2023-01-04 11:01:45 +0000

    [Enrollment] Do not cancel enrollment in progress.
    Before the change, EnrollmentScreen was handling cancellation shortcut
    during registration. Since EnrollmentScreen does not have any buttons to
    cancel the process while enrollment is in progress, it was the only way
    to do so. But it should not be available for users.
    
    Now, a user is not able to cancel ongoing enrollment.
    
    The LP enrollment case is kept as is showing skip confirmation dialog.
    
    Bug: b:262628685
    Test: manual on device.
    Change-Id: I2bbd2c5511a111ab6065240185a401841e373ee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110826
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1088657}

--- chrome/browser/ash/login/enrollment/enterprise_enrollment_helper_impl.cc
+++ chrome/browser/ash/login/enrollment/enterprise_enrollment_helper_impl.cc
@@ -218,6 +218,13 @@ void EnterpriseEnrollmentHelperImpl::DoEnroll(policy::DMAuth auth_data) {
   enrollment_handler_->StartEnrollment();
 }
 
+bool EnterpriseEnrollmentHelperImpl::InProgress() const {
+  // `enrollment_handler_` lives from `DoEnroll` till `OnEnrollmentFinished`
+  // which covers the whole enrollment process whether it ends with success or
+  // failure.
+  return enrollment_handler_ != nullptr;
+}
+
 void EnterpriseEnrollmentHelperImpl::GetDeviceAttributeUpdatePermission() {
   policy::BrowserPolicyConnectorAsh* connector =
       g_browser_process->platform_part()->browser_policy_connector_ash();
