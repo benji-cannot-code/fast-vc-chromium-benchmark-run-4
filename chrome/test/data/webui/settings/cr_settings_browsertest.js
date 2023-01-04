915f01f355c7a517351f97ec2141b94948c94621
Gabriel Oliveira <gabolvr@google.com>
2023-01-04 14:27:13 +0000

    [Privacy Sandbox] Add expand button to show all sites in Fledge page
    Preview:
    https://screencast.googleplex.com/cast/NTk0OTM4NjgzMzAwMjQ5Nnw0NTY0Nzc4My00NA
    
    Preview when "See all sites" button is not shown due to the length of
    the list: https://crbug.com/1378703#c239
    
    Bug: 1378703, b/254411463, b/254411387, b/254412638
    Change-Id: Iefa361d2d408ebaee9cbb1703bf013f8b16ca6fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128314
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1088696}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -643,6 +643,12 @@ TEST_F('CrSettingsPrivacySandboxPageTest', 'FledgeSubpageTests', function() {
 });
 
 TEST_F(
+    'CrSettingsPrivacySandboxPageTest', 'FledgeSubpageSeeAllSitesTests',
+    function() {
+      runMochaSuite('PrivacySandboxFledgeSubpageSeeAllSitesTests');
+    });
+
+TEST_F(
     'CrSettingsPrivacySandboxPageTest', 'AdMeasurementSubpageTests',
     function() {
       runMochaSuite('PrivacySandboxAdMeasurementSubpageTests');
