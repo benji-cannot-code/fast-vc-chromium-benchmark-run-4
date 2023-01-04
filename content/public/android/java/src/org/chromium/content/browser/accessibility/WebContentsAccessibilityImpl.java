e1e61f9e449e56dfb4fcf7ec9ee9cb57e1fd191c
Jack Lynch <jacklynch@google.com>
2023-01-04 18:22:27 +0000

    Rework AccessibilityState.Listener Interface
    This CL updates AccessibilityState's Listener interface to use a new
    State class to pass multiple aspects of the state to its listeners.
    The new interface also passes the old state in addition to the new one.
    
    Originally reviewed as part of crrev.com/c/4029066
    
    Bug: 1362100
    Change-Id: Ic0616c52ded9b1a591a518199173f879901ce182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4130870
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Jack Lynch <jacklynch@google.com>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1088826}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
@@ -819,7 +819,8 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
     // BrowserAccessibilityStateListener
 
     @Override
-    public void onBrowserAccessibilityStateChanged(boolean newScreenReaderEnabledState) {
+    public void onAccessibilityStateChanged(AccessibilityState.State oldAccessibilityState,
+            AccessibilityState.State newAccessibilityState) {
         refreshNativeState();
 
         // TODO(mschillaci,jacklynch): Move into {refreshNativeState} or similar method once
