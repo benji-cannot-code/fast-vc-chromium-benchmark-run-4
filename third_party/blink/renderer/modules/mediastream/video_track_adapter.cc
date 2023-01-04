cd8ee9bd543c0375b95092fd6bc200954d0442fe
henrika <henrika@chromium.org>
2023-01-04 15:35:35 +0000

    Ensures that the VTA only saves the last timestamp when the frame is used.
    It was found while testing a change in https://chromium-review.googlesource.com/c/chromium/src/+/4090609 that the VTA stored
    the last frame timestamp even if the frame was dropped due to a too
    high frame rate. This could trigger a long sequence of dropped frames with reason set to
    kResolutionAdapterTimestampTooCloseToPrevious when we added a
    RequestRefreshFrame for Desktop and Window capturers on Windows.
    
    This fix ensures that |last_time_stamp_| is only updated when the
    frame is kept/valid.
    
    Bug: 1400204
    Change-Id: I9a769e0bdc3cf59fe0d519dfd24154f1ea2f71b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136370
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Markus Handell <handellm@google.com>
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088732}

--- third_party/blink/renderer/modules/mediastream/video_track_adapter.cc
+++ third_party/blink/renderer/modules/mediastream/video_track_adapter.cc
@@ -60,7 +60,8 @@ const float kNormalFrameTimeoutInFrameIntervals = 25.0f;
 
 // Min delta time between two frames allowed without being dropped if a max
 // frame rate is specified.
-constexpr base::TimeDelta kMinTimeInMsBetweenFrames = base::Milliseconds(5);
+constexpr base::TimeDelta kMinTimeInMsBetweenFrames =
+    base::Milliseconds(VideoTrackAdapter::kMinTimeBetweenFramesMs);
 // If the delta between two frames is bigger than this, we will consider it to
 // be invalid and reset the fps calculation.
 constexpr base::TimeDelta kMaxTimeInMsBetweenFrames = base::Milliseconds(1000);
@@ -364,6 +365,9 @@ void VideoTrackAdapter::VideoFrameResolutionAdapter::DeliverFrame(
     return;
   }
 
+  // Update the last timestamp since the frame was approved and not dropped.
+  last_time_stamp_ = video_frame->timestamp();
+
   // If the frame is a texture not backed up by GPU memory we don't apply
   // cropping/scaling and deliver the frame as-is, leaving it up to the
   // destination to rescale it. Otherwise, cropping and scaling is soft-applied
@@ -494,7 +498,6 @@ bool VideoTrackAdapter::VideoFrameResolutionAdapter::MaybeDropFrame(
 
   // Do not drop frames if max frame rate hasn't been specified.
   if (!settings_.max_frame_rate().has_value()) {
-    last_time_stamp_ = frame.timestamp();
     return false;
   }
 
@@ -503,8 +506,7 @@ bool VideoTrackAdapter::VideoFrameResolutionAdapter::MaybeDropFrame(
   // Check if the time since the last frame is completely off.
   if (delta_ms.is_negative() || delta_ms > kMaxTimeInMsBetweenFrames) {
     DVLOG(3) << " reset timestamps";
-    // Reset |last_time_stamp_| and fps calculation.
-    last_time_stamp_ = frame.timestamp();
+    // Reset fps calculation.
     frame_rate_ = settings_.max_frame_rate().value_or(
         MediaStreamVideoSource::kDefaultFrameRate);
     DVLOG(1) << " frame rate filter initialized to " << frame_rate_ << " fps";
@@ -527,7 +529,7 @@ bool VideoTrackAdapter::VideoFrameResolutionAdapter::MaybeDropFrame(
         kResolutionAdapterTimestampTooCloseToPrevious;
     return true;
   }
-  last_time_stamp_ = frame.timestamp();
+
   // Calculate the frame rate using an exponential moving average (EMA) filter.
   // Use a simple filter with 0.1 weight of the current sample.
   frame_rate_ = 100 / delta_ms.InMillisecondsF() + 0.9 * frame_rate_;
