2ab01c9c805bc76fc8afc296b6323635ee6ab0d4
Joey Arhar <jarhar@google.com>
2023-01-04 18:53:06 +0000

    Revert "Privacy Sandbox Settings: Match ClearOnExit against primary_pattern only"
    This reverts commit 9b6c0a64c3c0c813b35092e90b7faf2b5d9ea188.
    
    Reason for revert:
    This is causing a DCHECK to get hit on debug builds: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/110430/overview
    I manually verified that reverting this patch makes the tests start passing again.
    
    Original change's description:
    > Privacy Sandbox Settings: Match ClearOnExit against primary_pattern only
    >
    > When storage and cookies are deleted on exit, we don't know the
    > context in which they were created. https://crrev.com/c/4129525
    > already removed the UI to create and view SESSION_ONLY exceptions that
    > target the top level url. This CL also removes the logic that applied
    > these settings.
    >
    > Bug: 1378703
    > Change-Id: Ic16dde428cea5db62a1f5185da28d8f7b75ca5c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128573
    > Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    > Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1088686}
    
    Bug: 1378703
    Change-Id: Ie14ad5a7939e86a720b47d25b4afb1f708e514ca
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134710
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joey Arhar <jarhar@google.com>
    Auto-Submit: Joey Arhar <jarhar@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1088846}

--- components/content_settings/core/browser/cookie_settings_unittest.cc
+++ components/content_settings/core/browser/cookie_settings_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "extensions/buildflags/buildflags.h"
 #include "net/base/features.h"
+#include "net/base/schemeful_site.h"
 #include "net/cookies/cookie_constants.h"
 #include "net/cookies/cookie_setting_override.h"
 #include "net/cookies/cookie_util.h"
@@ -34,8 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_IOS)
 #include "components/content_settings/core/common/features.h"
 #else
-#include "components/privacy_sandbox/privacy_sandbox_features.h"
-
+#include "third_party/blink/public/common/features.h"
 namespace {
 constexpr char kAllowedRequestsHistogram[] =
     "API.StorageAccess.AllowedRequests2";
@@ -123,10 +123,6 @@ class CookieSettingsTest : public testing::TestWithParam<TestCase> {
   ~CookieSettingsTest() override { settings_map_->ShutdownOnUIThread(); }
 
   void SetUp() override {
-#if !BUILDFLAG(IS_IOS)
-    is_privacy_sandbox_v4_enabled_ =
-        base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4);
-#endif
     ContentSettingsRegistry::GetInstance()->ResetForTest();
     CookieSettings::RegisterProfilePrefs(prefs_.registry());
     HostContentSettingsMap::RegisterProfilePrefs(prefs_.registry());
@@ -220,7 +216,6 @@ class CookieSettingsTest : public testing::TestWithParam<TestCase> {
   const net::SiteForCookies kHttpSiteForCookies;
   const net::SiteForCookies kHttpsSiteForCookies;
   ContentSettingsPattern kAllHttpsSitesPattern;
-  bool is_privacy_sandbox_v4_enabled_ = false;
 
  private:
   base::test::ScopedFeatureList feature_list_;
@@ -427,34 +422,6 @@ TEST_P(CookieSettingsTest, CookiesExplicitSessionOnly) {
                                                     QueryReason::kCookies));
 }
 
-TEST_P(CookieSettingsTest, ThirdPartyExceptionSessionOnly) {
-  cookie_settings_->SetThirdPartyCookieSetting(kBlockedSite,
-                                               CONTENT_SETTING_SESSION_ONLY);
-  EXPECT_EQ(cookie_settings_->IsCookieSessionOnly(kBlockedSite,
-                                                  QueryReason::kCookies),
-            !is_privacy_sandbox_v4_enabled_);
-}
-
-#if !BUILDFLAG(IS_IOS)
-class CookieSettingsTestSandboxV4Enabled : public CookieSettingsTest {
- public:
-  CookieSettingsTestSandboxV4Enabled() {
-    feature_list_.InitAndEnableFeature(
-        privacy_sandbox::kPrivacySandboxSettings4);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-TEST_P(CookieSettingsTestSandboxV4Enabled, ThirdPartyExceptionSessionOnly) {
-  cookie_settings_->SetThirdPartyCookieSetting(kBlockedSite,
-                                               CONTENT_SETTING_SESSION_ONLY);
-  EXPECT_FALSE(cookie_settings_->IsCookieSessionOnly(kBlockedSite,
-                                                     QueryReason::kCookies));
-}
-#endif
-
 TEST_P(CookieSettingsTest, KeepBlocked) {
   // Keep blocked cookies.
   cookie_settings_->SetDefaultCookieSetting(CONTENT_SETTING_ALLOW);
@@ -564,24 +531,6 @@ TEST_P(CookieSettingsTest, DeletionWithSubDomains) {
   EXPECT_TRUE(ShouldDeleteCookieOnExit(kSubDomain, true));
 }
 
-TEST_P(CookieSettingsTest, DeleteCookiesWithThirdPartyException) {
-  cookie_settings_->SetDefaultCookieSetting(CONTENT_SETTING_ALLOW);
-  cookie_settings_->SetThirdPartyCookieSetting(kHttpsSite,
-                                               CONTENT_SETTING_SESSION_ONLY);
-  EXPECT_EQ(ShouldDeleteCookieOnExit(kDomain, true),
-            !is_privacy_sandbox_v4_enabled_);
-}
-
-#if !BUILDFLAG(IS_IOS)
-TEST_P(CookieSettingsTestSandboxV4Enabled,
-       DeleteCookiesWithThirdPartyException) {
-  cookie_settings_->SetDefaultCookieSetting(CONTENT_SETTING_ALLOW);
-  cookie_settings_->SetThirdPartyCookieSetting(kHttpsSite,
-                                               CONTENT_SETTING_SESSION_ONLY);
-  EXPECT_FALSE(ShouldDeleteCookieOnExit(kDomain, true));
-}
-#endif
-
 TEST_P(CookieSettingsTest, CookiesThirdPartyBlockedExplicitAllow) {
   cookie_settings_->SetCookieSetting(kAllowedSite, CONTENT_SETTING_ALLOW);
   prefs_.SetInteger(prefs::kCookieControlsMode,
@@ -627,15 +576,13 @@ TEST_P(CookieSettingsTest, CookiesThirdPartyBlockedAllSitesAllowed) {
   EXPECT_FALSE(cookie_settings_->IsCookieSessionOnly(kAllowedSite,
                                                      QueryReason::kCookies));
 
-  // HTTP sites should be allowed, session_only attributes are ignored for
-  // exceptions with secondary pattern.
+  // HTTP sites should be allowed, but session-only.
   EXPECT_TRUE(cookie_settings_->IsFullCookieAccessAllowed(
       kFirstPartySite, kFirstPartySiteForCookies,
       /*top_frame_origin=*/absl::nullopt, cookie_setting_overrides,
       QueryReason::kCookies));
-  EXPECT_EQ(cookie_settings_->IsCookieSessionOnly(kFirstPartySite,
-                                                  QueryReason::kCookies),
-            !is_privacy_sandbox_v4_enabled_);
+  EXPECT_TRUE(cookie_settings_->IsCookieSessionOnly(kFirstPartySite,
+                                                    QueryReason::kCookies));
 
   // Third-party cookies should be blocked.
   EXPECT_EQ(cookie_setting_overrides.Has(
@@ -1059,20 +1006,6 @@ INSTANTIATE_TEST_SUITE_P(
       return info.param.test_name;
     });
 
-#if !BUILDFLAG(IS_IOS)
-INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
-    CookieSettingsTestSandboxV4Enabled,
-    testing::ValuesIn<TestCase>({
-        {"disable_SAA", false, false},
-        {"enable_SAA", true, false},
-        {"disable_SAA_force_3PCs", false, true},
-        {"enable_SAA_force_3PCs", true, true},
-    }),
-    [](const testing::TestParamInfo<CookieSettingsTest::ParamType>& info) {
-      return info.param.test_name;
-    });
-#endif
 }  // namespace
 
 }  // namespace content_settings
--- components/content_settings/core/common/cookie_settings_base.cc
+++ components/content_settings/core/common/cookie_settings_base.cc
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/cookie_settings_base.h"
 
 #include "base/check.h"
+#include "base/debug/stack_trace.h"
+#include "base/debug/task_trace.h"
 #include "base/feature_list.h"
 #include "base/notreached.h"
 #include "base/types/optional_util.h"
 #include "build/build_config.h"
+#include "components/content_settings/core/common/features.h"
 #include "net/base/features.h"
 #include "net/base/net_errors.h"
 #include "net/cookies/cookie_setting_override.h"
@@ -17,10 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/site_for_cookies.h"
 #include "net/cookies/static_cookie_policy.h"
 #include "url/gurl.h"
-
-#if !BUILDFLAG(IS_IOS)
-#include "components/privacy_sandbox/privacy_sandbox_features.h"
-#endif
 
 namespace content_settings {
 
@@ -30,16 +29,7 @@ CookieSettingsBase::CookieSettingsBase()
       storage_access_api_grants_unpartitioned_storage_(
           net::features::kStorageAccessAPIGrantsUnpartitionedStorage.Get()),
       is_storage_partitioned_(base::FeatureList::IsEnabled(
-          net::features::kThirdPartyStoragePartitioning)),
-      is_privacy_sandbox_v4_enabled_(
-#if BUILDFLAG(IS_IOS)
-          false
-#else
-          base::FeatureList::IsEnabled(
-              privacy_sandbox::kPrivacySandboxSettings4)
-#endif
-      ) {
-}
+          net::features::kThirdPartyStoragePartitioning)) {}
 
 // static
 bool CookieSettingsBase::IsThirdPartyRequest(
@@ -63,11 +53,8 @@ bool CookieSettingsBase::ShouldDeleteCookieOnExit(
     const std::string& domain,
     bool is_https) const {
   GURL origin = net::cookie_util::CookieOriginToURL(domain, is_https);
-  // Pass GURL() as first_party_url since we don't know the context and
-  // don't want to match against (*, exception) pattern.
   ContentSetting setting =
-      GetCookieSetting(origin, is_privacy_sandbox_v4_enabled_ ? GURL() : origin,
-                       nullptr, QueryReason::kCookies);
+      GetCookieSetting(origin, origin, nullptr, QueryReason::kCookies);
   DCHECK(IsValidSetting(setting));
   if (setting == CONTENT_SETTING_ALLOW)
     return false;
@@ -124,11 +111,8 @@ bool CookieSettingsBase::IsFullCookieAccessAllowed(
 
 bool CookieSettingsBase::IsCookieSessionOnly(const GURL& origin,
                                              QueryReason query_reason) const {
-  // Pass GURL() as first_party_url since we don't know the context and
-  // don't want to match against (*, exception) pattern.
   ContentSetting setting =
-      GetCookieSetting(origin, is_privacy_sandbox_v4_enabled_ ? GURL() : origin,
-                       nullptr, query_reason);
+      GetCookieSetting(origin, origin, nullptr, query_reason);
   DCHECK(IsValidSetting(setting));
   return setting == CONTENT_SETTING_SESSION_ONLY;
 }
