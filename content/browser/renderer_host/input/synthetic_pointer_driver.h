69a731875c2ccba67ba1b890042ed956c61032fb
David Bokan <bokan@chromium.org>
2023-01-04 19:59:29 +0000

    Fix UAF in SyntheticPointerAction
    SyntheticPointerAction supports providing an external pointer driver for
    use with DevTools' InputHandler so that state can be kept across
    gestures.
    
    This code currently assumes InputHandler will outlive the
    SyntheticPointerAction so the driver is kept as a raw pointer. However,
    InputHandler can go away without the action being cleared. In this case,
    we'll use a WeakPtr to clear the driver from the action and end the
    gesture.
    
    Bug: 1395354
    Change-Id: I93337ea04df1d426ce4942f11cb23bdf6aad3e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134565
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088894}

--- content/browser/renderer_host/input/synthetic_pointer_driver.h
+++ content/browser/renderer_host/input/synthetic_pointer_driver.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
 #include "content/common/input/synthetic_gesture_params.h"
 #include "content/common/input/synthetic_pointer_action_params.h"
@@ -16,7 +17,8 @@ namespace content {
 
 class SyntheticGestureTarget;
 
-class CONTENT_EXPORT SyntheticPointerDriver {
+class CONTENT_EXPORT SyntheticPointerDriver
+    : public base::SupportsWeakPtr<SyntheticPointerDriver> {
  public:
   SyntheticPointerDriver();
 
