d73115f76e7510ecf5b2b659b42eae4e45f9b610
kylechar <kylechar@chromium.org>
2023-01-04 19:58:19 +0000

    Last cleanup for BUILDFLAG(IS_OZONE)
    Remove a few last references to USE_OZONE and add some comments
    
    Bug: 1377327
    Change-Id: I5ded6ff38b61828c4c076872e0a83110c9dfd6c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4103982
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088892}

--- build/build_config.h
+++ build/build_config.h
@@ -27,6 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file also adds defines specific to the platform, architecture etc.
 //
+//  Platform:
+//    IS_OZONE
+//
 //  Compiler:
 //    COMPILER_MSVC / COMPILER_GCC
 //
