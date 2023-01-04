5acac868e7a7a24801e49319b47bc65dd9af075d
Anastasia Helfinstein <anastasi@google.com>
2023-01-04 01:24:54 +0000

    [ChromeVox] Refactor test-specific code in panel
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:260599332
    Test: cq
    Change-Id: I5888a32897687e1233578763494ef43c29275847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128711
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1088521}

--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/tutorial_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/tutorial_test.js
@@ -86,6 +86,10 @@ ChromeVoxTutorialTest = class extends ChromeVoxPanelTestBase {
     return this.getPanel().instance_.tutorial_;
   }
 
+  disableRestartNudges() {
+    this.getPanel().instance_.tutorial_.restartNudges = null;
+  }
+
   get simpleDoc() {
     return `
       <p>Some web content</p>
@@ -215,10 +219,10 @@ AX_TEST_F(
 // Afterward, general hints will be given about using ChromeVox. Lastly,
 // we will give a hint for exiting the tutorial.
 AX_TEST_F('ChromeVoxTutorialTest', 'GeneralNudgesTest', async function() {
-  this.getPanel().instance_.disableRestartTutorialNudgesForTesting_ = true;
   const mockFeedback = this.createMockFeedback();
   const root = await this.runWithLoadedTree(this.simpleDoc);
   await this.launchAndWaitForTutorial();
+  this.disableRestartNudges();
   const tutorial = this.getTutorial();
   const giveNudge = () => {
     tutorial.giveNudge();
@@ -745,10 +749,10 @@ AX_TEST_F(
     });
 
 AX_TEST_F('ChromeVoxTutorialTest', 'GeneralTouchNudges', async function() {
-  this.getPanel().instance_.disableRestartTutorialNudgesForTesting_ = true;
   const mockFeedback = this.createMockFeedback();
   const root = await this.runWithLoadedTree(this.simpleDoc);
   await this.launchAndWaitForTutorial();
+  this.disableRestartNudges();
   const tutorial = this.getTutorial();
   const giveNudge = () => {
     tutorial.giveNudge();
