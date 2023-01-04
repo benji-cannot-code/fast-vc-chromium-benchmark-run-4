b4f0e9c99b4d5a4ab0b911abf4c927346e3d231a
Khalid Peer <khalidpeer@chromium.org>
2023-01-04 19:50:08 +0000

    [ZPS][Journeys] Add metrics logging for ZPS cache observer in Journeys.
    As a follow-up to previous work to integrate the in-memory ZPS caching
    service with the Journeys feature in Chrome, this CL adds metrics
    logging to the new "related searches" extraction flow in order to
    achieve logging parity with the old "related searches" extraction flow.
    
    Bug: 1378776
    Change-Id: I2bf878cf70dd709e18113402f5231b5c24073ca6
    Low-Coverage-Reason: Minimal changes to implementation logic.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133424
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088882}

--- components/optimization_guide/content/browser/page_content_annotations_service.cc
+++ components/optimization_guide/content/browser/page_content_annotations_service.cc
@@ -81,6 +81,13 @@ void LogPageContentAnnotationsStorageStatus(
       status);
 }
 
+void LogRelatedSearchesExtracted(bool success) {
+  base::UmaHistogramBoolean(
+      "OptimizationGuide.PageContentAnnotationsService."
+      "RelatedSearchesExtracted",
+      success);
+}
+
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
 // Record the visibility score of the provided visit as a RAPPOR-style record to
 // UKM.
@@ -461,6 +468,7 @@ void PageContentAnnotationsService::OnZeroSuggestResponseUpdated(
   }
 
   if (page_url.empty() || !google_util::IsGoogleSearchUrl(GURL(page_url))) {
+    LogRelatedSearchesExtracted(false);
     return;
   }
 
@@ -476,7 +484,10 @@ void PageContentAnnotationsService::
     ExtractRelatedSearchesFromZeroSuggestResponse(
         const ZeroSuggestCacheService::CacheEntry& response,
         history::QueryURLResult url_result) {
-  if (url_result.visits.empty()) {
+  if (!url_result.success || url_result.visits.empty()) {
+    LogPageContentAnnotationsStorageStatus(
+        PageContentAnnotationsStorageStatus::kNoVisitsForUrl,
+        PageContentAnnotationsType::kRelatedSearches);
     return;
   }
 
@@ -498,11 +509,18 @@ void PageContentAnnotationsService::
   }
 
   if (related_searches.empty()) {
+    LogRelatedSearchesExtracted(false);
     return;
   }
 
+  LogRelatedSearchesExtracted(true);
+
   auto visit_id = url_result.visits.front().visit_id;
   history_service_->AddRelatedSearchesForVisit(related_searches, visit_id);
+
+  LogPageContentAnnotationsStorageStatus(
+      PageContentAnnotationsStorageStatus::kSuccess,
+      PageContentAnnotationsType::kRelatedSearches);
 }
 
 void PageContentAnnotationsService::OnRelatedSearchesExtracted(
@@ -511,10 +529,7 @@ void PageContentAnnotationsService::OnRelatedSearchesExtracted(
     continuous_search::mojom::CategoryResultsPtr results) {
   const bool success =
       status == continuous_search::SearchResultExtractorClientStatus::kSuccess;
-  base::UmaHistogramBoolean(
-      "OptimizationGuide.PageContentAnnotationsService."
-      "RelatedSearchesExtracted",
-      success);
+  LogRelatedSearchesExtracted(success);
 
   if (!success) {
     return;
