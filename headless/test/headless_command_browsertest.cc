70192ef44bc50b01a631e63bb029c15c78becd40
Kevin Marshall <kmarshall@chromium.org>
2023-01-04 17:46:17 +0000

    [headless] Use main thread TaskRunner for async IO in headless tests.
    Addresses a flake due to asynchronous IO tasks not completing before
    testing assertions are run.
    
    The main thread's TaskRunner is now used for tests, which allows the
    pending IO tasks to run as part of the main RunLoop.
    
    Bug: 1404799
    Test: no failures with gtest_repeat=500
    Change-Id: I7879d396dfc4222c14d1f470a4079ec294f272f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134112
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088801}

--- headless/test/headless_command_browsertest.cc
+++ headless/test/headless_command_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/threading/thread_restrictions.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "content/public/test/browser_test.h"
 #include "headless/app/headless_command_handler.h"
@@ -72,7 +73,8 @@ class HeadlessCommandBrowserTest : public HeadlessBrowserTest {
         HeadlessWebContentsImpl::From(web_contents)->web_contents(),
         GetTargetUrl(),
         base::BindOnce(&HeadlessCommandBrowserTest::FinishTest,
-                       base::Unretained(this)));
+                       base::Unretained(this)),
+        base::ThreadTaskRunnerHandle::Get());
 
     RunAsynchronousTest();
 
