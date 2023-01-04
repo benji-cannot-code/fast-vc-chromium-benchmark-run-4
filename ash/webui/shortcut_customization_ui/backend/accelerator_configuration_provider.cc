1bf0da64edac43eb6ef30dc5080d4879c33ec642
wenyu zhang <zhangwenyu@google.com>
2023-01-04 21:43:33 +0000

    accelerators: Add keyboard capability delegate
    Add KeyboardCapabilityDelegateImpl in ash/events. Move
    TopRowKeysAreFKeys function from shortcut_customization_ui/backend/
    accelerator_configuration_provider.cc to the new class.
    
    ash/events/keyboard_capability_unittest.cc
    
    Bug: b:216049298
    Test: accelerator_configuration_provider_unittest.cc
    Change-Id: I7a35b9ebad37733b54c34f572ee25b4f3322d1da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108781
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088961}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
@@ -12,10 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accelerators/accelerator_layout_table.h"
 #include "ash/accelerators/ash_accelerator_configuration.h"
-#include "ash/constants/ash_pref_names.h"
 #include "ash/public/cpp/accelerators_util.h"
 #include "ash/public/mojom/accelerator_info.mojom-shared.h"
-#include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom.h"
 #include "base/containers/fixed_flat_map.h"
@@ -23,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
-#include "components/prefs/pref_service.h"
 #include "mojo/public/cpp/bindings/clone_traits.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -173,15 +170,6 @@ mojom::AcceleratorLayoutInfoPtr LayoutInfoToMojom(
   layout_info->action = static_cast<uint32_t>(layout_details.action_id);
 
   return layout_info;
-}
-
-bool TopRowKeysAreFunctionKeys() {
-  const PrefService* pref_service =
-      Shell::Get()->session_controller()->GetLastActiveUserPrefService();
-  if (!pref_service) {
-    return false;
-  }
-  return pref_service->GetBoolean(prefs::kSendFunctionKeys);
 }
 
 }  // namespace
@@ -379,7 +367,8 @@ mojom::AcceleratorInfoPtr
 AcceleratorConfigurationProvider::CreateRemappedTopRowAcceleratorInfo(
     const ui::Accelerator& accelerator) const {
   // Avoid remapping if [Search] is part of original accelerator.
-  if (accelerator.IsCmdDown() || !TopRowKeysAreFunctionKeys() ||
+  if (accelerator.IsCmdDown() ||
+      !Shell::Get()->keyboard_capability()->TopRowKeysAreFKeys() ||
       !ui::kLayout2TopRowKeyToFKeyMap.contains(accelerator.key_code())) {
     // No remapping is done.
     return nullptr;
--- ui/chromeos/events/keyboard_capability.cc
+++ ui/chromeos/events/keyboard_capability.cc
@@ -11,6 +11,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+KeyboardCapability::KeyboardCapability(std::unique_ptr<Delegate> delegate)
+    : delegate_(std::move(delegate)) {}
+
+KeyboardCapability::~KeyboardCapability() = default;
+
+bool KeyboardCapability::TopRowKeysAreFKeys() const {
+  return delegate_->TopRowKeysAreFKeys();
+}
+
+void KeyboardCapability::SetTopRowKeysAsFKeysEnabledForTesting(
+    bool enabled) const {
+  delegate_->SetTopRowKeysAsFKeysEnabledForTesting(enabled);  // IN-TEST
+}
+
 // static
 bool KeyboardCapability::IsSixPackKey(const KeyboardCode& key_code) {
   return base::Contains(kSixPackKeyToSystemKeyMap, key_code);
