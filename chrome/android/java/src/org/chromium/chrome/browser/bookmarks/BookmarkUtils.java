b4dcce15f0da19b301e9b01d953f1754e9767e8d
Henrique Nakashima <hnakashima@chromium.org>
2023-01-04 19:18:27 +0000

    [Android] Remove N+ check from BookmarkUtils
    Nougat is now the minimum supported version.
    
    Bug: 1385893
    Change-Id: I2cdc5261ac1868aaf20466922600d42b759e258d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135779
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088858}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkUtils.java
@@ -11,7 +11,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Handler;
 import android.os.LocaleList;
 import android.provider.Browser;
@@ -818,11 +817,9 @@ public class BookmarkUtils {
     }
 
     private static Locale getLocale(Activity activity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            LocaleList locales = activity.getResources().getConfiguration().getLocales();
-            if (locales.size() > 0) {
-                return locales.get(0);
-            }
+        LocaleList locales = activity.getResources().getConfiguration().getLocales();
+        if (locales.size() > 0) {
+            return locales.get(0);
         }
         @SuppressWarnings("deprecation")
         Locale locale = activity.getResources().getConfiguration().locale;
