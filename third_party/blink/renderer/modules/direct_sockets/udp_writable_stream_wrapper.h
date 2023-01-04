4d825894e8bb26c262a48f6b02e5dd50af52ec99
Andrew Rayskiy <greengrape@google.com>
2023-01-04 13:14:10 +0000

    [DirectSockets] Support BYOB Reading for TCP, Part 1/2
    This CL simplifies the structure of readable and writable stream
    wrappers and introduces a stub for ReadableByteStreamWrapper.
    
    1. [this CL]
    2. crrev.com/c/4118768
    
    Bug: 1166811
    Change-Id: Ie403e5fb7f7597c4c284f2a5ed8f6a9a97dda814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114288
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088681}

--- third_party/blink/renderer/modules/direct_sockets/udp_writable_stream_wrapper.h
+++ third_party/blink/renderer/modules/direct_sockets/udp_writable_stream_wrapper.h
@@ -35,9 +35,6 @@ class MODULES_EXPORT UDPWritableStreamWrapper final
   void ErrorStream(int32_t error_code) override;
   bool HasPendingWrite() const override;
   void Trace(Visitor*) const override;
-
- protected:
-  // WritableStreamWrapper:
   void OnAbortSignal() override;
   ScriptPromise Write(ScriptValue chunk, ExceptionState&) override;
 
