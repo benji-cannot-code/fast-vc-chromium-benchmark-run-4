8f947e415c115f1ee8e901e5818ed1f63ab2adf5
Norge Vizcay <vizcay@google.com>
2023-01-04 10:00:55 +0000

    [AutofillAssistant] Remove invalid references.
    Bug: b/263360694
    Change-Id: I865deaa6ee59c48fa854879112e6cf7cc2550922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4124035
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088637}

--- chrome/test/base/chrome_render_view_test.cc
+++ chrome/test/base/chrome_render_view_test.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using autofill::AutofillAgent;
-using autofill::AutofillAssistantAgent;
 using autofill::PasswordAutofillAgent;
 using autofill::PasswordGenerationAgent;
 using blink::WebFrame;
