fd3de24cedda94ee92e350eff8fdc0b288b761b8
Thiago Perrotta <tperrotta@chromium.org>
2023-01-04 16:01:16 +0000

    Add ability to run ChromeDriver with a custom BiDi Mapper
    Introduce a Capability for a custom BiDi Mapper path.
    
    R=nechaev@chromium.org, sadym@chromium.org
    
    Fixed: chromedriver:4216
    Change-Id: I14c2851f913b9fde07dd834d7d1ede420ffb1f53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114081
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Commit-Queue: Thiago Perrotta <tperrotta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088752}

--- chrome/test/chromedriver/client/chromedriver.py
+++ chrome/test/chromedriver/client/chromedriver.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import platform
 import sys
 import util
 
@@ -748,4 +747,3 @@ class ChromeDriver(object):
 
   def __exit__(self, *args):
     self.Quit()
-
