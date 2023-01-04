0f9bf471d171ea31bbff795399920caaecf031b9
Nick Diego Yamane <nickdiego@igalia.com>
2023-01-04 20:47:00 +0000

    wayland: specify the event source when starting tabdrag dnd sessions
    R=max@igalia.com, sky
    
    Bug: 1246529
    Test: Covered by ozone_unittests
    Change-Id: I9028b9226b0a60953e192f61e5bb7a2225c5f1eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114171
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088924}

--- chrome/browser/ui/views/frame/browser_desktop_window_tree_host_linux.cc
+++ chrome/browser/ui/views/frame/browser_desktop_window_tree_host_linux.cc
@@ -18,8 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/desktop_browser_frame_aura_linux.h"
 #include "chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h"
+#include "chrome/browser/ui/views/tabs/tab_drag_controller.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkRRect.h"
+#include "ui/base/dragdrop/mojom/drag_drop_types.mojom-shared.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/skia_conversions.h"
@@ -43,6 +46,16 @@ bool CreateGlobalMenuBar() {
 std::unordered_set<std::string>& SentStartupIds() {
   static base::NoDestructor<std::unordered_set<std::string>> sent_startup_ids;
   return *sent_startup_ids;
+}
+
+// Returns the event source for the active tab drag session.
+absl::optional<ui::mojom::DragEventSource> GetCurrentTabDragEventSource() {
+  if (auto* source_context = TabDragController::GetSourceContext()) {
+    if (auto* drag_controller = source_context->GetDragController()) {
+      return drag_controller->event_source();
+    }
+  }
+  return absl::nullopt;
 }
 
 }  // namespace
@@ -155,9 +168,12 @@ void BrowserDesktopWindowTreeHostLinux::TabDraggingKindChanged(
 
   if (auto* wayland_extension = ui::GetWaylandExtension(*platform_window())) {
     if (tab_drag_kind != TabDragKind::kNone) {
-      auto allow_system_drag = base::FeatureList::IsEnabled(
-          features::kAllowWindowDragUsingSystemDragDrop);
-      wayland_extension->StartWindowDraggingSessionIfNeeded(allow_system_drag);
+      if (auto event_source = GetCurrentTabDragEventSource()) {
+        const auto allow_system_drag = base::FeatureList::IsEnabled(
+            features::kAllowWindowDragUsingSystemDragDrop);
+        wayland_extension->StartWindowDraggingSessionIfNeeded(
+            *event_source, allow_system_drag);
+      }
     }
   }
 }
--- ui/ozone/platform/wayland/host/wayland_toplevel_window.h
+++ ui/ozone/platform/wayland/host/wayland_toplevel_window.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "build/chromeos_buildflags.h"
+#include "ui/base/dragdrop/mojom/drag_drop_types.mojom-shared.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/ozone/platform/wayland/host/wayland_window.h"
@@ -140,7 +141,9 @@ class WaylandToplevelWindow : public WaylandWindow,
   void EndMoveLoop() override;
 
   // WaylandExtension:
-  void StartWindowDraggingSessionIfNeeded(bool allow_system_drag) override;
+  void StartWindowDraggingSessionIfNeeded(
+      ui::mojom::DragEventSource event_source,
+      bool allow_system_drag) override;
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   void SetImmersiveFullscreenStatus(bool status) override;
 #endif
