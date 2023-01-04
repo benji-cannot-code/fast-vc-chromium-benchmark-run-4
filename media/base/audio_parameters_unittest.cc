088d3ae75fb49b4b964541485c8ade95ca2870c3
Antonio Gomes <tonikitoo@igalia.com>
2023-01-04 18:45:09 +0000

    Use ASSERT_DEATH_IF_SUPPORTED in media_unittests
    Namely, in media/base/reentrancy_checker_unittest.cc and
               media/base/audio_parameters_unittest.cc.
    
    BUG=NONE
    
    Change-Id: Ic8af6533facf505e329c5421efcaffeac18ff519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4130408
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088841}

--- media/base/audio_parameters_unittest.cc
+++ media/base/audio_parameters_unittest.cc
@@ -195,7 +195,7 @@ TEST(AudioParameters, Constructor_CopyChannelLayoutConfig) {
 }
 
 TEST(AudioParameters, ShouldCheckDiscreteWithNoChannels) {
-  ASSERT_DEATH(
+  ASSERT_DEATH_IF_SUPPORTED(
       {
         ChannelLayoutConfig channel_layout_config(CHANNEL_LAYOUT_DISCRETE, 0);
       },
