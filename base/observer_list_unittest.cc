6e0cf294a2d778914665e61155e70d1624bab429
Tom Sepez <tsepez@chromium.org>
2023-01-05 00:10:39 +0000

    Fix lifetime issues in observer_list_unittest.cc
    -- Apply ExtractAsDangling() to raw pointers passed to bare delete.
    -- Re-order stack variables so pointed-to object outlives raw_ptr.
    
    Change-Id: I5bc805715d18c7c184a77679d86e367e902e9360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133960
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089030}

--- base/observer_list_unittest.cc
+++ base/observer_list_unittest.cc
@@ -554,7 +554,7 @@ class ListDestructor : public Foo {
   explicit ListDestructor(ObserverListType* list) : list_(list) {}
   ~ListDestructor() override = default;
 
-  void Observe(int x) override { delete list_; }
+  void Observe(int x) override { delete list_.ExtractAsDangling(); }
 
  private:
   raw_ptr<ObserverListType> list_;
@@ -820,8 +820,8 @@ TYPED_TEST(ObserverListTest, NonCompactList) {
   ObserverListFoo observer_list;
   Adder a(1), b(-1);
 
+  Disrupter disrupter2(&observer_list, true);  // Must outlive `disrupter1`.
   Disrupter disrupter1(&observer_list, true);
-  Disrupter disrupter2(&observer_list, true);
 
   // Disrupt itself and another one.
   disrupter1.SetDoomed(&disrupter2);
@@ -849,8 +849,8 @@ TYPED_TEST(ObserverListTest, BecomesEmptyThanNonEmpty) {
   ObserverListFoo observer_list;
   Adder a(1), b(-1);
 
+  Disrupter disrupter2(&observer_list, true);  // Must outlive `disrupter1`.
   Disrupter disrupter1(&observer_list, true);
-  Disrupter disrupter2(&observer_list, true);
 
   // Disrupt itself and another one.
   disrupter1.SetDoomed(&disrupter2);
