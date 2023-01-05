aaf410767e275dcfbb2c12d8a103d7f45aabaece
Christian Xu <christianxu@google.com>
2023-01-05 15:48:15 +0000

    [iOS][Omnibox] Fix suggestion scrolling
    The table view’s content is inset to make the suggestions scrollable
    above the keyboard on tablet. Disable this behavior on phone, since
    scrolling dismisses the keyboard.
    
    On iPad the selfSizingTableView sizes itself with the content size + the
    content inset. This causes the table view to have a bigger height when
    adding an inset to the suggestions.
    This CL: Remove the content inset and replace it with
    directionalLayoutMargins.
    
    Bug: 1404701
    Change-Id: I506f69241fa143a3359b4a42ad25a0e4efaac4cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4131383
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089271}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
@@ -56,6 +56,13 @@ const CGFloat kMaxTileFaviconSize = 48.0f;
 const CGFloat kHeaderPaddingBottom = 10.0f;
 /// Leading, trailing, and top padding for table view headers.
 const CGFloat kHeaderPadding = 2.0f;
+
+/// Returns whether the keyboard is dismissed when scrolling suggestions.
+BOOL ShouldDismissKeyboardOnScroll() {
+  return ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_TABLET ||
+         base::FeatureList::IsEnabled(kEnableSuggestionsScrollingOnIPad);
+}
+
 }  // namespace
 
 @interface OmniboxPopupViewController () <UITableViewDataSource,
@@ -116,8 +123,8 @@ const CGFloat kHeaderPadding = 2.0f;
 /// updated.
 @property(nonatomic, assign) BOOL shouldHideCarousel;
 
-/// Cached `tableView.contentSize.height` used in `viewDidLayoutSubviews` to
-/// avoid infinite loop and redudant computation when updating table view's
+/// Cached `tableView.visibleContentSize.height` used in `viewDidLayoutSubviews`
+/// to avoid infinite loop and redudant computation when updating table view's
 /// content inset.
 @property(nonatomic, assign) CGFloat cachedContentHeight;
 
@@ -132,7 +139,7 @@ const CGFloat kHeaderPadding = 2.0f;
     _visibleSuggestionCount = 0;
     _cachedContentHeight = 0;
     NSNotificationCenter* defaultCenter = [NSNotificationCenter defaultCenter];
-    if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+    if (!ShouldDismissKeyboardOnScroll()) {
       // The iPad keyboard can cover some of the rows of the scroll view. The
       // scroll view's content inset may need to be updated when the keyboard is
       // displayed.
@@ -246,7 +253,8 @@ const CGFloat kHeaderPadding = 2.0f;
   }
   self.tableView.contentInsetAdjustmentBehavior =
       UIScrollViewContentInsetAdjustmentAutomatic;
-  [self.tableView setContentInset:UIEdgeInsetsMake(0, 0, kBottomPadding, 0)];
+  [self.tableView setDirectionalLayoutMargins:NSDirectionalEdgeInsetsMake(
+                                                  0, 0, kBottomPadding, 0)];
 
   self.tableView.sectionHeaderHeight = 0.1;
   self.tableView.estimatedRowHeight = 0;
@@ -281,8 +289,10 @@ const CGFloat kHeaderPadding = 2.0f;
 }
 
 - (void)viewDidLayoutSubviews {
-  if (self.tableView.contentSize.height != self.cachedContentHeight) {
-    self.cachedContentHeight = self.tableView.contentSize.height;
+  [super viewDidLayoutSubviews];
+  if (!ShouldDismissKeyboardOnScroll() &&
+      self.tableView.visibleSize.height != self.cachedContentHeight) {
+    self.cachedContentHeight = self.tableView.visibleSize.height;
     [self updateContentInsetForKeyboard];
   }
 }
@@ -868,7 +878,7 @@ const CGFloat kHeaderPadding = 2.0f;
 /// below the keyboard.
 - (void)updateContentInsetForKeyboard {
   // Disable content inset update when scrolling dismisses the keyboard.
-  if (base::FeatureList::IsEnabled(kEnableSuggestionsScrollingOnIPad) ||
+  if (ShouldDismissKeyboardOnScroll() ||
       self.tableView.contentSize.height <= 0) {
     return;
   }
@@ -936,6 +946,8 @@ const CGFloat kHeaderPadding = 2.0f;
 
 #pragma mark - Keyboard events
 
+/// Handles `UIKeyboardDidShowNotification`, only active when
+/// `ShouldDismissKeyboardOnScroll` is false.
 - (void)keyboardDidShow:(NSNotification*)notification {
   self.keyboardHeight =
       [KeyboardObserverHelper keyboardHeightInWindow:self.tableView.window];
