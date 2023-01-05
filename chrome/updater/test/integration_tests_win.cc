f788144eec4435a80f36165cdead3fee344b85f4
S. Ganesh <ganesh@chromium.org>
2023-01-05 22:10:20 +0000

    Use common utility to create WaitableEvents for tests in more places
    Bug: 1402743
    Change-Id: Ia73d28f692d9b4ca90bab2259b7844f70ac45ce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4137226
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089454}

--- chrome/updater/test/integration_tests_win.cc
+++ chrome/updater/test/integration_tests_win.cc
@@ -1544,17 +1544,6 @@ void RunOfflineInstall(UpdaterScope scope,
 
   EXPECT_TRUE(DeleteRegKey(root, app_client_state_key));
 
-  // Create a unique name for a shared event to be waited for in this process
-  // and signaled in the offline installer process to confirm the installer
-  // was run.
-  const std::wstring event_name = base::StrCat(
-      {L"OfflineInstallTest-", base::NumberToWString(::GetCurrentProcessId())});
-  NamedObjectAttributes attr =
-      GetNamedObjectAttributes(event_name.c_str(), scope);
-  base::WaitableEvent event(base::win::ScopedHandle(
-      ::CreateEvent(&attr.sa, FALSE, FALSE, attr.name.c_str())));
-  ASSERT_NE(event.handle(), nullptr);
-
   base::ScopedTempDir temp_dir;
   EXPECT_TRUE(temp_dir.CreateUniqueTempDir());
   const base::FilePath& offline_dir = temp_dir.GetPath();
@@ -1562,6 +1551,12 @@ void RunOfflineInstall(UpdaterScope scope,
   // Create a batch file as the installer script, which creates some registry
   // values as the installation artifacts.
   base::FilePath installer_path = offline_dir.AppendASCII("test_installer.bat");
+
+  // Create a unique name for a shared event to be waited for in this process
+  // and signaled in the offline installer process to confirm the installer
+  // was run.
+  test::EventHolder event_holder(test::CreateWaitableEventForTest());
+
   EXPECT_TRUE(base::WriteFile(
       installer_path,
       [](UpdaterScope scope, const std::string& app_client_state_key,
@@ -1591,7 +1586,7 @@ void RunOfflineInstall(UpdaterScope scope,
               reg_item.value_name, reg_item.type, reg_item.value.c_str()));
         }
         return base::JoinString(commands, "\n");
-      }(scope, base::WideToASCII(app_client_state_key), attr.name)));
+      }(scope, base::WideToASCII(app_client_state_key), event_holder.name)));
 
   // The updater only allows `.exe` or `.msi` to run from the offline directory.
   // Setup `cmd.exe` as the wrapper installer.
@@ -1674,7 +1669,8 @@ void RunOfflineInstall(UpdaterScope scope,
     // Silent install does not run post-install command. For other cases the
     // event should have been signaled by the post-install command via the
     // installer result API.
-    EXPECT_TRUE(event.TimedWait(TestTimeouts::action_max_timeout()));
+    EXPECT_TRUE(
+        event_holder.event.TimedWait(TestTimeouts::action_max_timeout()));
   }
 
   EXPECT_TRUE(DeleteRegKey(root, app_client_state_key));
