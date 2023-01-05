b8a90354a2b0115043bc0da72e9b3e44bf369c02
Min Qin <qinmin@chromium.org>
2023-01-05 16:55:33 +0000

    [Download] Add a new histogram to record read errors from mojo inputs
    This could happen during download and need to record the histogram
    to check what is causing this to happen.
    
    BUG=1403837
    
    Change-Id: Id3731782cb0add3975ebcd5737cf6acebb5b2b3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129147
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089311}

--- components/download/public/common/download_stats.h
+++ components/download/public/common/download_stats.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_export.h"
 #include "components/download/public/common/download_interrupt_reasons.h"
 #include "components/download/public/common/download_source.h"
+#include "mojo/public/c/system/types.h"
 #include "net/base/network_change_notifier.h"
 #include "net/http/http_response_info.h"
 #include "url/gurl.h"
@@ -328,6 +329,20 @@ enum class DownloadMetricsCallsite {
   kMixContentDownloadBlocking,
 };
 
+enum class InputStreamReadError {
+  // Reading the input stream cause a mojo input argument error.
+  kInvalidArgument = 0,
+
+  // Reading the input stream cause a mojo out of range error.
+  kOutOfRange = 1,
+
+  // Reading the input stream cause a mojo busy error.
+  kBusy = 2,
+
+  kUnknown = 3,
+  kMaxValue = kUnknown,
+};
+
 COMPONENTS_DOWNLOAD_EXPORT DownloadConnectionSecurity
 CheckDownloadConnectionSecurity(const GURL& download_url,
                                 const std::vector<GURL>& url_chain);
@@ -374,6 +389,10 @@ COMPONENTS_DOWNLOAD_EXPORT void RecordParallelRequestCreationFailure(
 // Record download later events.
 COMPONENTS_DOWNLOAD_EXPORT void RecordDownloadLaterEvent(
     DownloadLaterEvent event);
+
+// Record download later events.
+COMPONENTS_DOWNLOAD_EXPORT void RecordInputStreamReadError(
+    MojoResult mojo_result);
 
 #if BUILDFLAG(IS_ANDROID)
 enum class BackgroudTargetDeterminationResultTypes {
