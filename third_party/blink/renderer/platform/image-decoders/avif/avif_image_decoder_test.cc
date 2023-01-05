891f815921acd0858aef4d8c4edede278c912af7
Wan-Teh Chang <wtc@google.com>
2023-01-05 20:34:57 +0000

    Assume we have a 12-bit AV1 decoder
    This reverts https://crrev.com/c/2657067, which was added when libgav1
    only had 8-bit and 10-bit AV1 decoders. We can revert it now for two
    reasons:
    1. media/filters/gav1_video_decoder.h has been removed in
    https://crrev.com/c/4027350 and https://crrev.com/c/4027351.
    2. The current version of libgav1 has a 12-bit AV1 decoder.
    
    Bug: 1074147
    Change-Id: I03d8748c3bb65b247b713bfc6ad5a556e7affc2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133537
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089420}

--- third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder_test.cc
+++ third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder_test.cc
@@ -18,17 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/task/thread_pool.h"
 #include "base/test/task_environment.h"
-#include "media/media_buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/image-decoders/image_decoder_test_helpers.h"
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
 #include "third_party/libavif/src/include/avif/avif.h"
-
-// If the AV1 decoder library supports the bit depth 12, define
-// HAVE_AVIF_BIT_DEPTH_12_SUPPORT.
-#if BUILDFLAG(ENABLE_DAV1D_DECODER)
-#define HAVE_AVIF_BIT_DEPTH_12_SUPPORT
-#endif
 
 #define FIXME_SUPPORT_ICC_PROFILE_NO_TRANSFORM 0
 #define FIXME_SUPPORT_ICC_PROFILE_TRANSFORM 0
@@ -702,10 +695,6 @@ void TestYUVRed(const char* file_name,
                 const gfx::Size& expected_uv_size,
                 SkColorType color_type = kGray_8_SkColorType,
                 int bit_depth = 8) {
-#if !defined(HAVE_AVIF_BIT_DEPTH_12_SUPPORT)
-  if (bit_depth == 12)
-    return;
-#endif
   SCOPED_TRACE(base::StringPrintf("file_name=%s, color_type=%d", file_name,
                                   int{color_type}));
 
@@ -770,7 +759,6 @@ TEST(AnimatedAVIFTests, ValidImages) {
       &CreateAVIFDecoder,
       "/images/resources/avif/star-animated-10bpc-with-alpha.avif", 5u,
       kAnimationLoopInfinite);
-#if defined(HAVE_AVIF_BIT_DEPTH_12_SUPPORT)
   TestByteByByteDecode(&CreateAVIFDecoder,
                        "/images/resources/avif/star-animated-12bpc.avif", 5u,
                        0);
@@ -778,7 +766,6 @@ TEST(AnimatedAVIFTests, ValidImages) {
       &CreateAVIFDecoder,
       "/images/resources/avif/star-animated-12bpc-with-alpha.avif", 5u,
       kAnimationLoopInfinite);
-#endif
   // TODO(ryoh): Add animated avif files with EditListBox.
 }
 
@@ -862,12 +849,10 @@ TEST(StaticAVIFTests, ValidImages) {
       &CreateAVIFDecoder,
       "/images/resources/avif/red-at-12-oclock-with-color-profile-10bpc.avif",
       1, kAnimationNone);
-#if defined(HAVE_AVIF_BIT_DEPTH_12_SUPPORT)
   TestByteByByteDecode(
       &CreateAVIFDecoder,
       "/images/resources/avif/red-at-12-oclock-with-color-profile-12bpc.avif",
       1, kAnimationNone);
-#endif
   TestByteByByteDecode(&CreateAVIFDecoder,
                        "/images/resources/avif/tiger_3layer_1res.avif", 1,
                        kAnimationNone);
@@ -880,12 +865,10 @@ TEST(StaticAVIFTests, ValidImages) {
   TestByteByByteDecode(&CreateAVIFDecoder,
                        "/images/resources/avif/dice_444_10b_grid4x3.avif", 1,
                        kAnimationNone);
-#if defined(HAVE_AVIF_BIT_DEPTH_12_SUPPORT)
   TestByteByByteDecode(
       &CreateAVIFDecoder,
       "/images/resources/avif/gracehopper_422_12b_grid2x4.avif", 1,
       kAnimationNone);
-#endif
 }
 
 TEST(StaticAVIFTests, YUV) {
@@ -1115,10 +1098,6 @@ INSTANTIATE_TEST_SUITE_P(Parameterized,
 
 TEST_P(StaticAVIFColorTests, InspectImage) {
   const StaticColorCheckParam& param = GetParam();
-#if !defined(HAVE_AVIF_BIT_DEPTH_12_SUPPORT)
-  if (param.bit_depth == 12)
-    return;
-#endif
   // TODO(ryoh): Add tests with ImageDecoder::kHighBitDepthToHalfFloat
   std::unique_ptr<ImageDecoder> decoder = CreateAVIFDecoderWithOptions(
       param.alpha_option, ImageDecoder::kDefaultBitDepth, param.color_behavior,
