114ab1f51098d0373cb5f669f1bea378d3ab05cb
YuhanYang <yyhyyh@google.com>
2023-01-05 22:08:18 +0000

    Device Split: Add fake input device settings provider
    Add fake input device settings provider and input device settings types.
    
    Bug: b/241965700
    Test: browser_tests
    Change-Id: I1e8129360d6f3e9fa24d6867660457c2600b3a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4084361
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1089451}

---
+++ chrome/test/data/webui/settings/chromeos/fake_input_device_settings_provider_test.js
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {FakeInputDeviceSettingsProvider, MetaKey} from 'chrome://os-settings/chromeos/os_settings.js';
+import {assertDeepEquals} from 'chrome://webui-test/chai_assert.js';
+
+suite('FakeInputDeviceSettings', function() {
+  /**
+   * @type {?FakeInputDeviceSettingsProvider}
+   */
+  let provider = null;
+
+  setup(() => {
+    provider = new FakeInputDeviceSettingsProvider();
+  });
+
+  teardown(() => {
+    provider = null;
+  });
+
+  test('setFakeKeyboards', () => {
+    const fakeKeyboards = [{
+      id: 0,
+      name: 'fake-keyboard',
+      isExternal: false,
+      metaKey: MetaKey.COMMAND,
+      modifierKeys: [],
+      settings: {},
+    }];
+
+    provider.setFakeKeyboards(fakeKeyboards);
+    return provider.getFakeKeyboards().then(
+        result => assertDeepEquals(fakeKeyboards, result));
+  });
+});
