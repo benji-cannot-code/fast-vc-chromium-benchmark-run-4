bc806e2ab686b69eaa9a30329d75695593b800c5
Luciano Pacheco <lucmult@chromium.org>
2023-01-05 02:28:42 +0000

    Files SWA: Stop importing background page code in the SWA
    Change the SWA to stop importing the BG code and also some of the test
    functions that are indirectly imported in the SWA.
    
    Bug: b:262833874
    Change-Id: I19e9092db35fc59930b3a77850745abc27aba2ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113128
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089078}

--- ash/webui/file_manager/resources/main.js
+++ ash/webui/file_manager/resources/main.js
@@ -4,11 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './strings.m.js';
-import 'chrome://file-manager/background/js/metrics_start.js';
 import './test_util_swa.js';
 
-import {background} from 'chrome://file-manager/background/js/file_manager_base.js';
-import {VolumeManagerImpl} from 'chrome://file-manager/background/js/volume_manager_impl.js';
 import {GlitchType, reportGlitch} from 'chrome://file-manager/common/js/glitch.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 
--- ui/file_manager/file_manager/background/js/test_util.js
+++ ui/file_manager/file_manager/background/js/test_util.js
@@ -7,8 +7,6 @@ import {FilesAppState} from '../../common/js/files_app_state.js';
 import {ProgressCenterItem} from '../../common/js/progress_center_common.js';
 import {util} from '../../common/js/util.js';
 
-import {background} from './file_manager_base.js';
-import {launcher} from './launcher.js';
 import {test} from './test_util_base.js';
 
 export {test};
@@ -29,7 +27,7 @@ export function sanitizeDate(strDate) {
  *     App ID.
  */
 test.util.async.openMainWindow = (appState, callback) => {
-  launcher.launchFileManager(appState).then(callback);
+  window.background.launchFileManager(appState).then(callback);
 };
 
 /**
@@ -949,6 +947,6 @@ test.util.sync.sendProgressItem =
       item.progressValue = progressValue;
       item.itemCount = count;
 
-      background.progressCenter.updateItem(item);
+      window.background.progressCenter.updateItem(item);
       return true;
     };
