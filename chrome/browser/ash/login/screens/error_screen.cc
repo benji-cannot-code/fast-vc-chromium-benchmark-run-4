ce4ad335b7ff31cad15678fdf70e3cfe11966017
Ashutosh Singhal <macinashutosh@google.com>
2023-01-05 13:41:47 +0000

    [CLEANUP]: Auto-Formatting changes in error_screen.cc
    Change-Id: I8f991030c509d4501373eafd9d0e1dfac8216c0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134848
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1089217}

--- chrome/browser/ash/login/screens/error_screen.cc
+++ chrome/browser/ash/login/screens/error_screen.cc
@@ -101,13 +101,15 @@ ErrorScreen::~ErrorScreen() {
 }
 
 void ErrorScreen::AllowGuestSignin(bool allowed) {
-  if (view_)
+  if (view_) {
     view_->SetGuestSigninAllowed(allowed);
+  }
 }
 
 void ErrorScreen::ShowOfflineLoginOption(bool show) {
-  if (view_)
+  if (view_) {
     view_->SetOfflineSigninAllowed(show);
+  }
 }
 
 void ErrorScreen::OnOfflineLoginClicked() {
@@ -146,15 +148,17 @@ OobeScreenId ErrorScreen::GetParentScreen() const {
 }
 
 void ErrorScreen::HideCaptivePortal() {
-  if (captive_portal_window_proxy_.get())
+  if (captive_portal_window_proxy_.get()) {
     captive_portal_window_proxy_->Close();
+  }
 }
 
 void ErrorScreen::SetUIState(NetworkError::UIState ui_state) {
   LOG(WARNING) << __func__ << " to " << ui_state;
   ui_state_ = ui_state;
-  if (view_)
+  if (view_) {
     view_->SetUIState(ui_state);
+  }
 }
 
 void ErrorScreen::SetErrorState(NetworkError::ErrorState error_state,
@@ -184,13 +188,15 @@ void ErrorScreen::ShowCaptivePortal() {
 }
 
 void ErrorScreen::ShowConnectingIndicator(bool show) {
-  if (view_)
+  if (view_) {
     view_->SetShowConnectingIndicator(show);
+  }
 }
 
 void ErrorScreen::SetIsPersistentError(bool is_persistent) {
-  if (view_)
+  if (view_) {
     view_->SetIsPersistentError(is_persistent);
+  }
 }
 
 base::CallbackListSubscription ErrorScreen::RegisterConnectRequestCallback(
@@ -219,8 +225,9 @@ void ErrorScreen::ShowNetworkErrorMessage(NetworkStateInformer::State state,
   const bool is_loading_timeout =
       (reason == NetworkError::ERROR_REASON_LOADING_TIMEOUT);
 
-  if (!is_behind_captive_portal)
+  if (!is_behind_captive_portal) {
     HideCaptivePortal();
+  }
 
   if (is_proxy_error) {
     SetErrorState(NetworkError::ERROR_STATE_PROXY, std::string());
@@ -254,8 +261,9 @@ void ErrorScreen::ShowImpl() {
     SetHideCallback(base::BindOnce(&ErrorScreen::DefaultHideCallback,
                                    weak_factory_.GetWeakPtr()));
   }
-  if (!view_)
+  if (!view_) {
     return;
+  }
 
   view_->Show();
   LOG(WARNING) << "Network error screen message is shown";
@@ -264,8 +272,9 @@ void ErrorScreen::ShowImpl() {
 }
 
 void ErrorScreen::HideImpl() {
-  if (!view_ || is_hidden())
+  if (!view_ || is_hidden()) {
     return;
+  }
 
   LOG(WARNING) << "Network error screen message is hidden";
   if (on_hide_callback_) {
@@ -345,8 +354,9 @@ void ErrorScreen::PolicyLoadFailed() {
 }
 
 void ErrorScreen::DefaultHideCallback() {
-  if (parent_screen_ != OOBE_SCREEN_UNKNOWN && view_)
+  if (parent_screen_ != OOBE_SCREEN_UNKNOWN && view_) {
     view_->ShowOobeScreen(parent_screen_);
+  }
 
   // TODO(antrim): Due to potential race with GAIA reload and hiding network
   // error UI we can't just reset parent screen to SCREEN_UNKNOWN here.
@@ -422,21 +432,24 @@ void ErrorScreen::StartGuestSessionAfterOwnershipCheck(
       return;
     case CrosSettingsProvider::PERMANENTLY_UNTRUSTED:
       // Only allow guest sessions if there is no owner yet.
-      if (ownership_status == DeviceSettingsService::OWNERSHIP_NONE)
+      if (ownership_status == DeviceSettingsService::OWNERSHIP_NONE) {
         break;
+      }
       return;
     case CrosSettingsProvider::TRUSTED: {
       // Honor kAccountsPrefAllowGuest.
       bool allow_guest = false;
       CrosSettings::Get()->GetBoolean(kAccountsPrefAllowGuest, &allow_guest);
-      if (allow_guest)
+      if (allow_guest) {
         break;
+      }
       return;
     }
   }
 
-  if (guest_login_performer_)
+  if (guest_login_performer_) {
     return;
+  }
 
   guest_login_performer_ =
       std::make_unique<ChromeLoginPerformer>(this, AuthMetricsRecorder::Get());
