f341740a332ae987c9b8438321f4e894259cd4ed
Roger Tinkoff <rtinkoff@google.com>
2023-01-05 22:17:42 +0000

    ash: VcEffectsDelegate::GetEffectState() returns absl::optional<int>
    Make the return value of VcEffectsDelegate::GetEffectState() an
    optional int. This is a cleaner solution for cases where the current
    state of a hosted effect cannot be obtained e.g. the delegate is in a
    bad state or encounters an error.
    
    This fixes a case where the background blur selector was disappearing
    from the VC bubble, because it's state of
    "off" (CameraEffectsController::BackgroundBlurEffectState::kOff) had
    the same value (-1) as VcEffectState::kUnusedId.
    
    Bug: b/264470750
    Change-Id: I3ead33518c5ad79f1b891bed074c9d724163244f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135196
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089466}

--- ash/system/camera/camera_effects_controller.cc
+++ ash/system/camera/camera_effects_controller.cc
@@ -269,7 +269,7 @@ void CameraEffectsController::OnActiveUserPrefServiceChanged(
   InitializeEffectControls();
 }
 
-int CameraEffectsController::GetEffectState(int effect_id) {
+absl::optional<int> CameraEffectsController::GetEffectState(int effect_id) {
   switch (static_cast<cros::mojom::CameraEffect>(effect_id)) {
     case cros::mojom::CameraEffect::kBackgroundBlur: {
       DCHECK(pref_change_registrar_ && pref_change_registrar_->prefs());
@@ -290,11 +290,11 @@ int CameraEffectsController::GetEffectState(int effect_id) {
           prefs::kPortraitRelighting);
     case cros::mojom::CameraEffect::kBackgroundReplace:
     case cros::mojom::CameraEffect::kNone:
-      return VcEffectState::kUnusedId;
+      return absl::nullopt;
   }
 
   NOTREACHED();
-  return VcEffectState::kUnusedId;
+  return absl::nullopt;
 }
 
 void CameraEffectsController::OnEffectControlActivated(int effect_id,
--- ash/system/video_conference/effects/fake_video_conference_effects.cc
+++ ash/system/video_conference/effects/fake_video_conference_effects.cc
@@ -46,7 +46,7 @@ SimpleToggleEffect::SimpleToggleEffect(
   AddEffect(std::move(effect));
 }
 
-int SimpleToggleEffect::GetEffectState(int effect_id) {
+absl::optional<int> SimpleToggleEffect::GetEffectState(int effect_id) {
   return VcHostedEffect::kOff;
 }
 
@@ -111,7 +111,7 @@ ShaggyFurEffect::ShaggyFurEffect() {
 
 ShaggyFurEffect::~ShaggyFurEffect() = default;
 
-int ShaggyFurEffect::GetEffectState(int effect_id) {
+absl::optional<int> ShaggyFurEffect::GetEffectState(int effect_id) {
   return static_cast<int>(FurShagginess::kBuzzcut);
 }
 
@@ -170,9 +170,9 @@ SuperCutnessEffect::SuperCutnessEffect() {
 
 SuperCutnessEffect::~SuperCutnessEffect() = default;
 
-int SuperCutnessEffect::GetEffectState(int effect_id) {
+absl::optional<int> SuperCutnessEffect::GetEffectState(int effect_id) {
   if (has_invalid_effect_state_for_testing_) {
-    return VcEffectState::kUnusedId;
+    return absl::nullopt;
   }
 
   return static_cast<int>(HowCute::kTeddyBear);
