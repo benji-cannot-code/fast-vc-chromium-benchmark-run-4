992c88edb1f449c36e058f1740414e60a7ab8dd2
Scott Violet <sky@chromium.org>
2023-01-05 23:34:44 +0000

    blink: removes CopyLCharsFromUCharSource feature
    This has been enabled for a while with no ill effects.
    
    BUG=1330164
    
    Change-Id: Ia192c6a970918b034a99e7b2b8f6086d3f08f46a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4140165
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089506}

--- third_party/blink/renderer/platform/wtf/text/copy_lchars_from_uchar_source.h
+++ third_party/blink/renderer/platform/wtf/text/copy_lchars_from_uchar_source.h
@@ -39,26 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-#if !BUILDFLAG(IS_MAC) && defined(ARCH_CPU_X86_FAMILY)
-// Controls whether CopyLCharsFromUCharSource() uses intrinsics. This is backed
-// by a feature, but a variable is used as this code is performance sensitive.
-WTF_EXPORT extern bool g_enable_sse_path_for_copy_lchars;
-#endif
-
 inline void CopyLCharsFromUCharSource(LChar* destination,
                                       const UChar* source,
                                       size_t length) {
 #if defined(ARCH_CPU_X86_FAMILY)
-#if !BUILDFLAG(IS_MAC)
-  if (!g_enable_sse_path_for_copy_lchars) {
-    for (size_t i = 0; i < length; ++i) {
-      DCHECK(!(source[i] & 0xff00));
-      destination[i] = static_cast<LChar>(source[i]);
-    }
-    return;
-  }
-#endif
-
   const uintptr_t kMemoryAccessSize =
       16;  // Memory accesses on 16 byte (128 bit) alignment
   const uintptr_t kMemoryAccessMask = kMemoryAccessSize - 1;
