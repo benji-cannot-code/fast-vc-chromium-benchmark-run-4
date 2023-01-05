c5bd8dd277e32f6526e213c93ffd8c9929980f2e
Bastian Kersting <bkersting@google.com>
2023-01-05 11:27:51 +0000

    cros-healthd: Add ways for param checks in tests
    This change adds a way to set expectations around passed parameters
    when testing the diagnostics routines exposed by cros_healthd.
    This originated from a discussion in crrev/c/4085147, where
    distinguishing different parameters is needed.
    
    Bug: None
    Change-Id: Iebdae3f1aa662e85446d8f97573982a8f421e36d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136202
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1089190}

--- chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h
+++ chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/time/time.h"
+#include "base/values.h"
 #include "chromeos/ash/components/mojo_service_manager/mojom/mojo_service_manager.mojom.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_diagnostics.mojom.h"
@@ -132,6 +133,15 @@ class FakeCrosHealthd final : public mojom::CrosHealthdServiceFactory,
   // ProbeMultipleProcessInfo IPCs received.
   void SetProbeMultipleProcessInfoResponseForTesting(
       mojom::MultipleProcessResultPtr& result);
+
+  // Set expectation about the parameter that is passed to a call of
+  // a Diagnostics routine (`Run*Routine`) and `GetRoutineUpdate`.
+  void SetExpectedLastPassedDiagnosticsParametersForTesting(
+      base::Value::Dict expected_parameters);
+
+  // Verifies that the actual passed parameters to Diagnostic
+  // routines match the previously set expectations.
+  bool DidExpectedDiagnosticsParametersMatch();
 
   // Adds a delay before the passed callback is called.
   void SetCallbackDelay(base::TimeDelta delay);
@@ -443,6 +453,11 @@ class FakeCrosHealthd final : public mojom::CrosHealthdServiceFactory,
   // Contains the most recent params passed to `GetRoutineUpdate`, if it has
   // been called.
   absl::optional<RoutineUpdateParams> routine_update_params_;
+
+  // Expectation of the passed parameters.
+  base::Value::Dict expected_passed_parameters_;
+  // Actually passed parameter.
+  base::Value::Dict actual_passed_parameters_;
 
   // Allow |this| to call the methods on the NetworkDiagnosticsRoutines
   // interface.
