e0403c1039e70c64b062a47310c00cf273c3c74f
Sergii Bykov <sbykov@google.com>
2023-01-05 12:10:23 +0000

    Implement clang tidy fixes for kiosk ext apis crosapi files.
    Fixed comments from clang tidy in kiosk related extension APIs crosapi.
    E.g. removed unused includes, set default ctors.
    
    Bug: b:264386397
    Change-Id: I5e0ef8ab0d5adb489b9756b94e116b2c97d10dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136120
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089205}

--- ash/display/cros_display_config_unittest.cc
+++ ash/display/cros_display_config_unittest.cc
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "ash/touch/ash_touch_transform_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller_test_api.h"
-#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
+#include "base/functional/bind.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "chromeos/crosapi/mojom/cros_display_config.mojom.h"
@@ -81,12 +81,12 @@ class TestObserver : public crosapi::mojom::CrosDisplayConfigObserver {
 
 class CrosDisplayConfigTest : public AshTestBase {
  public:
-  CrosDisplayConfigTest() {}
+  CrosDisplayConfigTest() = default;
 
   CrosDisplayConfigTest(const CrosDisplayConfigTest&) = delete;
   CrosDisplayConfigTest& operator=(const CrosDisplayConfigTest&) = delete;
 
-  ~CrosDisplayConfigTest() override {}
+  ~CrosDisplayConfigTest() override = default;
 
   void SetUp() override {
     scoped_feature_list_.InitAndEnableFeature(features::kDisplayAlignAssist);
--- chrome/browser/ash/crosapi/kiosk_session_service_ash.h
+++ chrome/browser/ash/crosapi/kiosk_session_service_ash.h
@@ -6,13 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_CROSAPI_KIOSK_SESSION_SERVICE_ASH_H_
 #define CHROME_BROWSER_ASH_CROSAPI_KIOSK_SESSION_SERVICE_ASH_H_
 
-#include "base/callback.h"
-#include "base/observer_list.h"
-#include "base/observer_list_types.h"
 #include "chromeos/crosapi/mojom/kiosk_session_service.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
-#include "mojo/public/cpp/bindings/remote_set.h"
 
 namespace crosapi {
 
