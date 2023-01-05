041007cde2babd0dd2ec81d8f268110ea65ac9d7
Ioana Pandele <ioanap@chromium.org>
2023-01-05 09:15:24 +0000

    Revert "WebUI: Migrate more UIs to TypeScript Mojo bindings"
    This reverts commit 312cb4a08bc85e9049cacd503d8b30050bfd5ded.
    
    Reason for revert: Compile error on linux-archive-rel https://ci.chromium.org/ui/p/chromium/builders/ci/linux-archive-rel/83261/steps
    
    Original change's description:
    > WebUI: Migrate more UIs to TypeScript Mojo bindings
    >
    > usb-internals (non-services/ pieces; services/ deps are used on Blink,
    > which requires js_data_deps closure dependencies).
    > feed
    > access-code-cast
    >
    > Bug: 1002798
    > Change-Id: Iea6d4dfbf970436d003d75908e7476ed5f7a2d67
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117440
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1089149}
    
    Bug: 1002798
    Change-Id: I01ff835ec7511da6e66233aa403c497f40d7de46
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4138010
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089155}

--- chrome/browser/ui/webui/usb_internals/usb_internals_ui.cc
+++ chrome/browser/ui/webui/usb_internals/usb_internals_ui.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/usb_internals/usb_internals_page_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/grit/browser_resources.h"
 #include "chrome/grit/usb_internals_resources.h"
 #include "chrome/grit/usb_internals_resources_map.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -19,6 +20,14 @@ UsbInternalsUI::UsbInternalsUI(content::WebUI* web_ui)
   // Set up the chrome://usb-internals source.
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       Profile::FromWebUI(web_ui), chrome::kChromeUIUsbInternalsHost);
+
+  static constexpr webui::ResourcePath kPaths[] = {
+      {"usb_enumeration_options.mojom-webui.js",
+       IDR_USB_ENUMERATION_OPTIONS_MOJOM_WEBUI_JS},
+      {"usb_manager_client.mojom-webui.js",
+       IDR_USB_DEVICE_MANAGER_CLIENT_MOJOM_WEBUI_JS},
+  };
+  source->AddResourcePaths(kPaths);
 
   webui::SetupWebUIDataSource(
       source,
