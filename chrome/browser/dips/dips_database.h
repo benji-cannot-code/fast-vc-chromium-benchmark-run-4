b45c6159fb6efd9cedbe16588ca718289f7b9094
Kirubel Aklilu <kaklilu@chromium.org>
2023-01-05 21:02:46 +0000

    [DIPS] Restart DIPS procedure for sites whose interactions expire.
    Previous CLs removed the use of `ClearRowsWithExpiredInteractions` from
    the GetSitesThat* querying methods. This allowed for DIPS enforcement on
    sites previously protected *from* DIPS, if their last interaction was
    at least `dips::kInteractionTtl` ago from the DIPS timer firing.
    
    We instead want to restart the DIPS procedure for a site entirely when its interaction expires by removing its entry from the DIPS database.
    
    This CL also adds ClearRowsWithExpiredInteractions back into
    GetEntryCount, which was removed to ensure that this clearing wouldn't
    happen before the querying methods are used.
    
    Bug: 1375302
    Change-Id: I2efa37975a75633fb2c83c99f53e6b8475681a32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133448
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089433}

--- chrome/browser/dips/dips_database.h
+++ chrome/browser/dips/dips_database.h
@@ -54,11 +54,11 @@ class DIPSDatabase {
              const TimestampRange& stateful_bounce_times,
              const TimestampRange& bounce_times);
 
-  absl::optional<StateValue> Read(const std::string& site) const;
+  absl::optional<StateValue> Read(const std::string& site);
 
   // Note: this doesn't clear expired interactions from the database unlike the
   // other database querying methods.
-  std::vector<std::string> GetAllSitesForTesting() const;
+  std::vector<std::string> GetAllSitesForTesting();
 
   // Returns all sites which bounced the user and aren't protected from DIPS.
   //
@@ -66,10 +66,7 @@ class DIPSDatabase {
   // - it's still in its grace period after the first bounce
   // - it received user interaction before the first bounce
   // - it received user interaction in the grace period after the first bounce
-  //
-  // If a site is protected by an interaction, it won't be returned by this
-  // query until that interaction expires after interaction_ttl.
-  std::vector<std::string> GetSitesThatBounced() const;
+  std::vector<std::string> GetSitesThatBounced();
 
   // Returns all sites which used storage and aren't protected from DIPS.
   //
@@ -77,10 +74,7 @@ class DIPSDatabase {
   // - it's still in its grace period after the first storage
   // - it received user interaction before the first storage
   // - it received user interaction in the grace period after the first storage
-  //
-  // If a site is protected by an interaction, it won't be returned by this
-  // query until that interaction expires after interaction_ttl.
-  std::vector<std::string> GetSitesThatUsedStorage() const;
+  std::vector<std::string> GetSitesThatUsedStorage();
 
   // Returns all sites which statefully bounced the user and aren't protected
   // from DIPS.
@@ -90,10 +84,7 @@ class DIPSDatabase {
   // - it received user interaction before the first stateful bounce
   // - it received user interaction in the grace period after the first stateful
   //   bounce
-  //
-  // If a site is protected by an interaction, it won't be returned by this
-  // query until that interaction expires after interaction_ttl.
-  std::vector<std::string> GetSitesThatBouncedWithState() const;
+  std::vector<std::string> GetSitesThatBouncedWithState();
 
   // Deletes all rows in the database whose interactions have expired out.
   //
@@ -125,7 +116,7 @@ class DIPSDatabase {
                           const DIPSEventRemovalType type);
 
   // Returns the number of entries present in the database.
-  size_t GetEntryCount() const;
+  size_t GetEntryCount();
 
   // If the number of entries in the database is greater than
   // |GetMaxEntries()|, garbage collect. Returns the number of entries deleted
@@ -143,7 +134,7 @@ class DIPSDatabase {
   }
 
   // Checks that the internal SQLite database is initialized.
-  bool CheckDBInit() const;
+  bool CheckDBInit();
 
   size_t GetMaxEntries() const { return max_entries_; }
   size_t GetPurgeEntries() const { return purge_entries_; }
@@ -175,7 +166,7 @@ class DIPSDatabase {
                              const DIPSEventRemovalType type);
   bool RemoveEmptyRows();
 
-  void LogDatabaseMetrics() const;
+  void LogDatabaseMetrics();
 
  private:
   // Callback for database errors.
