adf2482d1cd2a7628e423de12fd7fa78c389a4d6
Ewann Pelle <ewannpv@chromium.org>
2023-01-05 11:32:50 +0000

    [iOS][PT] Add Tab Commands
    This CL adds a new shared protocol to handle tab collection view updates. This new protocol will be completed in other CLs.
    
    Change-Id: I2682018102bdbf855196c1fe6169c264e9ded293
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136237
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089193}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_mediator.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_mediator.h
@@ -8,13 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_commands.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_collection_commands.h"
 
 class Browser;
 @protocol TabCollectionConsumer;
 
 // Mediates between model layer and pinned tabs collection UI layer.
-@interface PinnedTabsMediator : NSObject <PinnedTabsCommands>
+@interface PinnedTabsMediator : NSObject <TabCollectionCommands>
 
 // The source browser.
 @property(nonatomic, assign) Browser* browser;
--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_collection_commands.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_collection_commands.h
@@ -1,15 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_PINNED_TABS_PINNED_TABS_COMMANDS_H_
-#define IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_PINNED_TABS_PINNED_TABS_COMMANDS_H_
+#ifndef IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_TAB_COLLECTION_COMMANDS_H_
+#define IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_TAB_COLLECTION_COMMANDS_H_
 
 #import <UIKit/UIKit.h>
 
-// Protocol used to update the model layer.
-@protocol PinnedTabsCommands
+// Commands for updating tab collection views.
+@protocol TabCollectionCommands
 
 // Tells the receiver to select the item with identifier `itemID`. If there is
 // no item with that identifier, no change in selection should be made.
@@ -17,4 +17,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_PINNED_TABS_PINNED_TABS_COMMANDS_H_
+#endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_TAB_COLLECTION_COMMANDS_H_
