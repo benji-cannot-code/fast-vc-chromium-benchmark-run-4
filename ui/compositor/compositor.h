dda07ca1ea543b0291caee61fae362024c1643f6
magchen <magchen@chromium.org>
2023-01-05 19:44:47 +0000

    Get CGDirectDisplayID from browser to begin frame source in GPU on Mac
    To use VCDisplay Vsync callback for begin frame source in GPU, CGDirectDisplayID is needed for the callback setup. The display ID in GPU will be updated when there is a window display change.
    
    Bug: 1404797
    Change-Id: Ibf54ee891cea142d637bdbea402c87e7e80b18b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134587
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089369}

--- ui/compositor/compositor.h
+++ ui/compositor/compositor.h
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_animator_collection.h"
 #include "ui/compositor/throughput_tracker.h"
 #include "ui/compositor/throughput_tracker_host.h"
+#include "ui/display/types/display_constants.h"
 #include "ui/gfx/display_color_spaces.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/vector2d.h"
@@ -246,6 +247,12 @@ class COMPOSITOR_EXPORT Compositor : public base::PowerSuspendObserver,
   // sets the SDR white level (in nits) used to scale HDR color space primaries.
   void SetDisplayColorSpaces(
       const gfx::DisplayColorSpaces& display_color_spaces);
+
+#if BUILDFLAG(IS_MAC)
+  // Set the current CGDirectDisplayID and update the private client.
+  void SetVSyncDisplayID(const int64_t display_id);
+  int64_t display_id() const;
+#endif
 
   const gfx::DisplayColorSpaces& display_color_spaces() const {
     return display_color_spaces_;
@@ -514,6 +521,11 @@ class COMPOSITOR_EXPORT Compositor : public base::PowerSuspendObserver,
   gfx::AcceleratedWidget widget_ = gfx::kNullAcceleratedWidget;
   // A sequence number of a current compositor frame for use with metrics.
   int activated_frame_count_ = 0;
+
+#if BUILDFLAG(IS_MAC)
+  // Current CGDirectDisplayID for the screen.
+  int64_t display_id_ = display::kInvalidDisplayId;
+#endif
 
   // Current vsync refresh rate per second. Initialized to 60hz as a reasonable
   // value until first begin frame arrives with the real refresh rate.
