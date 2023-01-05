c3faa97dbe299fa2ab3bdf5fe040348a80a2b720
David Padlipsky <dpad@google.com>
2023-01-05 23:09:12 +0000

    Settings Split: Add pref manager interface
    The InputDevicePrefManager will be used by the
    InputDeviceSettingsController to deal with the reading/updating of
    input device settings prefs.
    
    Bug: b/241965700
    Test: ash_unittests
    Change-Id: Ib8d8475217a683a4b3c7125f89da2389285c815b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4094082
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1089480}

---
+++ ash/system/input_device_settings/input_device_pref_manager_impl.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/input_device_settings/input_device_pref_manager_impl.h"
+#include "ash/public/mojom/input_device_settings.mojom.h"
+#include "base/notreached.h"
+
+namespace ash {
+
+InputDevicePrefManagerImpl::InputDevicePrefManagerImpl() = default;
+InputDevicePrefManagerImpl::~InputDevicePrefManagerImpl() = default;
+
+// TODO(dpad): Implement retrieval of settings when keyboard is initially
+// connected.
+void InputDevicePrefManagerImpl::InitializeKeyboardSettings(
+    mojom::Keyboard* keyboard) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace ash
