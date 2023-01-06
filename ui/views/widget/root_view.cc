6069344d794ef96271ab7c49e938c9d39db5dd1d
Keren Zhu <kerenzhu@chromium.org>
2023-01-06 01:32:16 +0000

    views: CHECK dangling mouse_move_handler_ in RootView::OnMouseMoved
    Sometimes mouse_move_handler_ becomes dangling while handling
    OnMouseMoved event. Add CHECK to get stack trace when this happens.
    
    Bug: 1295290
    Change-Id: I4d1d4f714dea6d7d4955697ca847e8133e859389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4138963
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089556}

--- ui/views/widget/root_view.cc
+++ ui/views/widget/root_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/strings/string_piece.h"
 #include "build/build_config.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -56,6 +57,31 @@ class MouseEnterExitEvent : public ui::MouseEvent {
   std::unique_ptr<ui::Event> Clone() const override {
     return std::make_unique<MouseEnterExitEvent>(*this);
   }
+};
+
+// TODO(crbug.com/1295290): This class is for debug purpose only.
+// Remove it after resolving the issue.
+class DanglingMouseMoveHandlerOnViewDestroyingChecker
+    : public views::ViewObserver {
+ public:
+  explicit DanglingMouseMoveHandlerOnViewDestroyingChecker(
+      const raw_ptr<views::View, DanglingUntriaged>& mouse_move_handler)
+      : mouse_move_handler_(mouse_move_handler) {
+    scoped_observation.Observe(mouse_move_handler_);
+  }
+
+  // views::ViewObserver:
+  void OnViewIsDeleting(views::View* view) override {
+    // `mouse_move_handler_` should be nulled before `view` dies. Otherwise
+    // `mouse_move_handler_` will become a dangling pointer.
+    CHECK(!mouse_move_handler_);
+    scoped_observation.Reset();
+  }
+
+ private:
+  base::ScopedObservation<views::View, views::ViewObserver> scoped_observation{
+      this};
+  const raw_ptr<views::View, DanglingUntriaged>& mouse_move_handler_;
 };
 
 }  // namespace
@@ -567,6 +593,10 @@ void RootView::OnMouseMoved(const ui::MouseEvent& event) {
       }
       View* old_handler = mouse_move_handler_;
       mouse_move_handler_ = v;
+      // TODO(crbug.com/1295290): This is for debug purpose only.
+      // Remove it after resolving the issue.
+      DanglingMouseMoveHandlerOnViewDestroyingChecker
+          mouse_move_handler_dangling_checker(mouse_move_handler_);
       if (!mouse_move_handler_->GetNotifyEnterExitOnChild() ||
           !mouse_move_handler_->Contains(old_handler)) {
         MouseEnterExitEvent entered(event, ui::ET_MOUSE_ENTERED);
