6374cc568b44e26f2add70c602f8bca19368990e
Timothy Loh <timloh@chromium.org>
2023-01-06 03:42:32 +0000

    Remove features::kCrostiniForceClose, it's been enabled long enough
    This feature was enabled in crrev.com/c/2235576 (2.5y ago) and there's
    no reason to keep the base::Feature around.
    
    Change-Id: I62812e833a5039c94f4ca3b8693a19e2f0d701ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136519
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089599}

--- chrome/browser/ui/ash/shelf/app_service/app_service_app_window_crostini_tracker.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_app_window_crostini_tracker.cc
@@ -242,8 +242,6 @@ std::string AppServiceAppWindowCrostiniTracker::GetShelfAppId(
 void AppServiceAppWindowCrostiniTracker::RegisterCrostiniWindowForForceClose(
     aura::Window* window,
     const std::string& app_name) {
-  if (!base::FeatureList::IsEnabled(features::kCrostiniForceClose))
-    return;
   exo::ShellSurfaceBase* surface = exo::GetShellSurfaceBaseForWindow(window);
   if (!surface)
     return;
