9293c0611da74ab36a0fbc4e51d3eeab13198b26
Nancy Wang <nancylingwang@chromium.org>
2023-01-06 12:36:14 +0000

    Remove mojom interfaces in Standalone browser extension apps publisher.
    BUG=1253250
    
    Change-Id: I433179b25c56831bf427eff0e9bb4446b2de361b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4141639
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089683}

--- chrome/browser/apps/app_service/publishers/standalone_browser_extension_apps.cc
+++ chrome/browser/apps/app_service/publishers/standalone_browser_extension_apps.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/services/app_service/public/cpp/features.h"
 #include "components/services/app_service/public/cpp/intent.h"
-#include "components/services/app_service/public/mojom/types.mojom.h"
 
 namespace apps {
 
@@ -58,13 +57,6 @@ StandaloneBrowserExtensionApps::StandaloneBrowserExtensionApps(
     AppServiceProxy* proxy,
     AppType app_type)
     : apps::AppPublisher(proxy), app_type_(app_type) {
-  mojo::Remote<apps::mojom::AppService>& app_service = proxy->AppService();
-  if (!base::FeatureList::IsEnabled(kStopMojomAppService) &&
-      !app_service.is_bound()) {
-    return;
-  }
-  PublisherBase::Initialize(app_service,
-                            apps::ConvertAppTypeToMojomAppType(app_type_));
   login_observation_.Observe(ash::LoginState::Get());
   // Check now in case login has already happened.
   LoggedInStateChanged();
@@ -328,24 +320,6 @@ void StandaloneBrowserExtensionApps::SetWindowMode(const std::string& app_id,
   controller_->SetWindowMode(app_id, window_mode);
 }
 
-void StandaloneBrowserExtensionApps::Connect(
-    mojo::PendingRemote<apps::mojom::Subscriber> subscriber_remote,
-    apps::mojom::ConnectOptionsPtr opts) {
-  mojo::Remote<apps::mojom::Subscriber> subscriber(
-      std::move(subscriber_remote));
-
-  mojo::RemoteSetElementId id = subscribers_.Add(std::move(subscriber));
-
-  std::vector<apps::mojom::AppPtr> apps;
-  for (auto& it : app_mojom_cache_) {
-    apps.push_back(it.second.Clone());
-  }
-
-  subscribers_.Get(id)->OnApps(std::move(apps),
-                               apps::ConvertAppTypeToMojomAppType(app_type_),
-                               true /* should_notify_initialized */);
-}
-
 void StandaloneBrowserExtensionApps::StopApp(const std::string& app_id) {
   // It is possible that Lacros is briefly unavailable, for example if it shuts
   // down for an update.
@@ -371,11 +345,6 @@ void StandaloneBrowserExtensionApps::OnApps(std::vector<AppPtr> deltas) {
   }
 
   if (controller_.is_bound()) {
-    for (const AppPtr& delta : deltas) {
-      app_mojom_cache_[delta->app_id] = ConvertAppToMojomApp(delta);
-      PublisherBase::Publish(ConvertAppToMojomApp(delta), subscribers_);
-    }
-
     apps::AppPublisher::Publish(std::move(deltas), app_type_,
                                 should_notify_initialized_);
     should_notify_initialized_ = false;
@@ -407,8 +376,6 @@ void StandaloneBrowserExtensionApps::RegisterAppController(
   } else {
     std::vector<AppPtr> deltas;
     for (auto& it : app_cache_) {
-      app_mojom_cache_[it.first] = ConvertAppToMojomApp(it.second);
-      PublisherBase::Publish(ConvertAppToMojomApp(it.second), subscribers_);
       deltas.push_back(std::move(it.second));
     }
     app_cache_.clear();
