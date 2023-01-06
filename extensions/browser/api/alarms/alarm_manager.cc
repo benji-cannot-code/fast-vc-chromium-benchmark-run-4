2527182e79d95d631d59a37225cb11351495d9db
David Bertoni <dbertoni@chromium.org>
2023-01-06 22:25:06 +0000

    [Extensions] Add a histogram for how many alarms an extension creates.
    Bug: 1400582
    Change-Id: If8e86808c4c0f5d72a0748efbf34178e50725e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134751
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089915}

--- extensions/browser/api/alarms/alarm_manager.cc
+++ extensions/browser/api/alarms/alarm_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/values_util.h"
 #include "base/lazy_instance.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/time/clock.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
@@ -337,6 +338,9 @@ void AlarmManager::ReadFromStorage(const std::string& extension_id,
         AlarmsFromValue(extension_id, is_unpacked, value->GetList());
     for (auto& alarm : alarm_states)
       AddAlarmImpl(extension_id, std::move(alarm));
+
+    base::UmaHistogramCounts1000("Extensions.AlarmManager.AlarmsLoadedCount",
+                                 alarm_states.size());
   }
 
   ReadyQueue& extension_ready_queue = ready_actions_[extension_id];
