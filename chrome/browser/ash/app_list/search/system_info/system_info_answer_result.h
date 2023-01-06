b7ee001ec34e8a4bc468798ff094e4b512a6d060
Lauren Commeignes <laurencom@chromium.org>
2023-01-06 03:09:29 +0000

    Create framework for System Info Answer cards
    Bug: b/263994165
    Change-Id: I7455cdeb9dce6d6c1139c76e18e1249873219c5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133779
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089587}

---
+++ chrome/browser/ash/app_list/search/system_info/system_info_answer_result.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_APP_LIST_SEARCH_SYSTEM_INFO_SYSTEM_INFO_ANSWER_RESULT_H_
+#define CHROME_BROWSER_ASH_APP_LIST_SEARCH_SYSTEM_INFO_SYSTEM_INFO_ANSWER_RESULT_H_
+
+#include "chrome/browser/ash/app_list/search/chrome_search_result.h"
+#include "chrome/browser/profiles/profile.h"
+
+namespace app_list {
+
+class SystemInfoAnswerResult : public ChromeSearchResult {
+ public:
+  SystemInfoAnswerResult(Profile* profile,
+                         const std::u16string& query,
+                         const std::string& url_path);
+  SystemInfoAnswerResult(const SystemInfoAnswerResult&) = delete;
+  SystemInfoAnswerResult& operator=(const SystemInfoAnswerResult&) = delete;
+
+  ~SystemInfoAnswerResult() override;
+
+  void Open(int event_flags) override;
+
+ private:
+  void UpdateIcon();
+
+  Profile* const profile_;
+  const std::string url_path_;
+};
+
+}  // namespace app_list
+
+#endif  // CHROME_BROWSER_ASH_APP_LIST_SEARCH_SYSTEM_INFO_SYSTEM_INFO_ANSWER_RESULT_H_
