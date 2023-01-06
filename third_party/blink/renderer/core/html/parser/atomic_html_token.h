044104a7fe58debca02609f8deda8ed3a5451ed3
Scott Violet <sky@chromium.org>
2023-01-06 23:27:26 +0000

    blink: enable attribute lookup on android
    The fast path attribute lookup was not enabled on android because of
    the binary size hit (~25k on android bot). In speaking with Android
    folks the tradeoff is worth it, so this enables the lookup on android as
    well.
    
    Binary-Size: this is a performance improvement that helps android as well. agrieve agreed with change.
    Bug: 1338583
    Change-Id: I63a394a863b8c0523cb8d6b0de2e3026f23ee822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4137248
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089949}

--- third_party/blink/renderer/core/html/parser/atomic_html_token.h
+++ third_party/blink/renderer/core/html/parser/atomic_html_token.h
@@ -36,16 +36,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/attribute.h"
 #include "third_party/blink/renderer/core/html/parser/html_token.h"
+#include "third_party/blink/renderer/core/html_element_attribute_name_lookup_trie.h"
 #include "third_party/blink/renderer/core/html_element_lookup_trie.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string_hash.h"
-
-// TODO(https://crbug.com/1338583): enable on android.
-#if !BUILDFLAG(IS_ANDROID)
-#include "third_party/blink/renderer/core/html_element_attribute_name_lookup_trie.h"  // nogncheck
-#endif
 
 namespace blink {
 
@@ -334,13 +330,8 @@ void AtomicHTMLToken::InitializeAttributes(
     if (attribute.NameIsEmpty())
       continue;
 
-// TODO(https://crbug.com/1338583): enable on android.
-#if !BUILDFLAG(IS_ANDROID)
     QualifiedName name = LookupHTMLAttributeName(attribute.NameBuffer().data(),
                                                  attribute.NameBuffer().size());
-#else
-    QualifiedName name = g_null_name;
-#endif
     if (name == g_null_name) {
       name = QualifiedName(g_null_atom, attribute.GetName(), g_null_atom);
     }
