4c0a71752b1debabd5159bc853059459fdb359bb
Nancy Wang <nancylingwang@chromium.org>
2023-01-06 00:05:30 +0000

    Add unit tests to verify the ARC app icon loading.
    BUG=1380608
    
    Change-Id: I8990be7fccbffca2b6e0497b09dd7ab8411bbaa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127506
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089524}

--- chrome/browser/apps/app_service/app_icon/app_icon_decoder.h
+++ chrome/browser/apps/app_service/app_icon/app_icon_decoder.h
@@ -70,6 +70,11 @@ class AppIconDecoder {
 
   void OnIconRead(std::map<ui::ResourceScaleFactor, IconValuePtr> icon_datas);
 
+  void DecodeImage(ui::ResourceScaleFactor scale_factor,
+                   const std::vector<uint8_t>& icon_data,
+                   gfx::ImageSkia& image_skia,
+                   std::set<ui::ResourceScaleFactor>& incomplete_scale_factors);
+
   void UpdateImageSkia(
       ui::ResourceScaleFactor scale_factor,
       const SkBitmap& bitmap,
@@ -79,6 +84,12 @@ class AppIconDecoder {
   void DiscardDecodeRequest();
 
   void CompleteWithImageSkia(const gfx::ImageSkia& image_skia);
+
+  void DecodeRequestForTesting(
+      ui::ResourceScaleFactor scale_factor,
+      const std::vector<uint8_t>& icon_data,
+      gfx::ImageSkia& image_skia,
+      std::set<ui::ResourceScaleFactor>& incomplete_scale_factors);
 
   const base::FilePath base_path_;
   const std::string app_id_;
