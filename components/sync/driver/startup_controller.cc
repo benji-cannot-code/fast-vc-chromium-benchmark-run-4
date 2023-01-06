4feebc7281e9e7c8db019d2d587af0069dda2cc2
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-01-06 17:18:01 +0000

    [sync] Add a (disabled) feature flag  to reduce startup deferral
    Bug: 1374718
    Change-Id: I1a686d7ff492b6775b92940fb9148cb2040613a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136251
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089770}

--- components/sync/driver/startup_controller.cc
+++ components/sync/driver/startup_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
@@ -26,6 +27,10 @@ namespace {
 constexpr base::TimeDelta kDefaultDeferredInitDelay = base::Seconds(10);
 
 base::TimeDelta GetDeferredInitDelay() {
+  if (base::FeatureList::IsEnabled(kDeferredSyncStartupCustomDelay)) {
+    return base::Seconds(kDeferredSyncStartupCustomDelayInSeconds.Get());
+  }
+
   const base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
   if (cmdline->HasSwitch(kSyncDeferredStartupTimeoutSeconds)) {
     int timeout = 0;
