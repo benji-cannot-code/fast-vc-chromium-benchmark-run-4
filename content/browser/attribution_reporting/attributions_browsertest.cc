045bd5bd0856cebf17b090b18849b584fa84abb5
Rohan Pavone <rohpavone@chromium.org>
2023-01-06 14:53:06 +0000

    [fuchsia] Disable flaky AttributionsBrowserTest.AttributionSrcNavigationSourceAndTrigger_ReportSent
    This test is 12% flaky on Fuchsia (about the same on all platforms).
    Disabling on Fuchsia for now.
    
    Bug: 1405318
    Change-Id: I99fd15a638e303868896cefab3402a2755dc19eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4139372
    Auto-Submit: Rohan Pavone <rohpavone@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089715}

--- content/browser/attribution_reporting/attributions_browsertest.cc
+++ content/browser/attribution_reporting/attributions_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <memory>
+#include "build/build_config.h"
 
 #include "base/command_line.h"
 #include "base/containers/contains.h"
@@ -1257,8 +1258,17 @@ IN_PROC_BROWSER_TEST_F(AttributionsBrowserTest,
   expected_report.WaitForReport();
 }
 
-IN_PROC_BROWSER_TEST_F(AttributionsBrowserTest,
-                       AttributionSrcNavigationSourceAndTrigger_ReportSent) {
+// TODO(crbug.com/1405318): Re-enable this test
+#if BUILDFLAG(IS_FUCHSIA)
+#define MAYBE_AttributionSrcNavigationSourceAndTrigger_ReportSent \
+  DISABLED_AttributionSrcNavigationSourceAndTrigger_ReportSent
+#else
+#define MAYBE_AttributionSrcNavigationSourceAndTrigger_ReportSent \
+  AttributionSrcNavigationSourceAndTrigger_ReportSent
+#endif
+IN_PROC_BROWSER_TEST_F(
+    AttributionsBrowserTest,
+    MAYBE_AttributionSrcNavigationSourceAndTrigger_ReportSent) {
   // Expected reports must be registered before the server starts.
   ExpectedReportWaiter expected_report(
       GURL("https://a.test/.well-known/attribution-reporting/"
