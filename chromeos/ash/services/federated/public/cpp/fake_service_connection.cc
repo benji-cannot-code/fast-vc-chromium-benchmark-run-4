dddcabe4d3a8fb172778aa536ff073964f1ba730
Xinglong Luan <alanlxl@google.com>
2023-01-06 11:12:27 +0000

    [Federated]: Update mojo interface to support per client switch
    This CL updates federated service mojom interface to support passing a
    client launch stage map to the OS side. Launch stages can be configured
    via Finch experiments.
    
    CL that implements the updated interface in OS side: https://crrev.com/c/4137751
    
    
    Bug: b:263210684
    Test: Tested on DUT against federated-service
    Test: ash_unittests --gtest_filter="*FederatedServiceControllerTestBase.*"
    Test: chromeos_unittests --gtest_filter="*FederatedServiceConnectionTest*"
    Change-Id: I07e7eb6d1bdf74c2087620b0f14b0f587fae54a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133780
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089673}

--- chromeos/ash/services/federated/public/cpp/fake_service_connection.cc
+++ chromeos/ash/services/federated/public/cpp/fake_service_connection.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/services/federated/public/cpp/fake_service_connection.h"
 
+#include "base/containers/flat_map.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
 namespace ash {
 namespace federated {
 
@@ -30,7 +33,9 @@ void FakeServiceConnectionImpl::ReportExample(
   return;
 }
 
-void FakeServiceConnectionImpl::StartScheduling() {
+void FakeServiceConnectionImpl::StartScheduling(
+    const absl::optional<base::flat_map<std::string, std::string>>&
+        client_launch_stage) {
   return;
 }
 
