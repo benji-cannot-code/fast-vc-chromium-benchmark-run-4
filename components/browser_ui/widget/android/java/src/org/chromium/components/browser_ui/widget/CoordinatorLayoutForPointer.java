d911aa7c57e7c9633c87ee64fdeb447e15570ce4
Jinsuk Kim <jinsukkim@chromium.org>
2023-01-06 15:42:50 +0000

    Android: Remove conditionals on older OS versions
    Removes the code running only on older version (J~M) and unnecessary
    check on N.
    
    Bug: 1385893
    Change-Id: Ie8087e442260ab5299ea4c39059a78b473e96d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4130309
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089725}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/CoordinatorLayoutForPointer.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/CoordinatorLayoutForPointer.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.widget;
 
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.PointerIcon;
@@ -35,8 +34,6 @@ public class CoordinatorLayoutForPointer extends CoordinatorLayout {
 
     @Override
     public PointerIcon onResolvePointerIcon(MotionEvent event, int pointerIndex) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) return null;
-
         final int x = (int) event.getX(pointerIndex);
         final int y = (int) event.getY(pointerIndex);
         final int childrenCount = getChildCount();
