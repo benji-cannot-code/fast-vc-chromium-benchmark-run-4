d239b0a04d186a211b2b2171bcf63b9907b40fb6
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-01-06 11:14:33 +0000

    Fix dangling pointers in callback in WindowsSpellChecker
    `metrics` was passed and used after free in a callback.
    It didn't crash, because the method where it was used could be made static, and `this` was not really dereferenced.
    
    This patch removes the argument, and turn the member function into a
    static one. This will prevent future folks doing UAF by using `this`.
    
    Bug: 1381426
    Fixed: 1381426
    Change-Id: I224ee9850cff81759791a17e01d59b21bcef70f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110823
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089674}

--- components/spellcheck/browser/spellcheck_host_metrics.cc
+++ components/spellcheck/browser/spellcheck_host_metrics.cc
@@ -150,6 +150,7 @@ void SpellCheckHostMetrics::RecordSpellingServiceStats(bool enabled) {
 }
 
 #if BUILDFLAG(IS_WIN)
+// static
 void SpellCheckHostMetrics::RecordAcceptLanguageStats(
     const LocalesSupportInfo& locales_info) {
   base::UmaHistogramExactLinear(
@@ -171,6 +172,7 @@ void SpellCheckHostMetrics::RecordAcceptLanguageStats(
       base::saturated_cast<int>(locales_info.unsupported_locales), 20);
 }
 
+// static
 void SpellCheckHostMetrics::RecordSpellcheckLanguageStats(
     const LocalesSupportInfo& locales_info) {
   base::UmaHistogramExactLinear(
