de3df5ac782540edc27b3093630ee93766d556a0
Huiting Yu <huitingyu@google.com>
2023-01-06 17:08:38 +0000

    [ios] Add a display handler for CPE promo in PromosManager.
    The display handler will be told by the PromosManager when it's right
    time to show the Promo, and will send the command to the CPE coordinator
    which configures and presents the promo with more context (like
    previous user actions).
    
    Bug: 1402963
    Change-Id: Ibed0cc7584403bf55e5612b331c8c7010c2774b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4138518
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Huiting Yu <huitingyu@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089762}

--- ios/chrome/browser/ui/promos_manager/promos_manager_coordinator.mm
+++ ios/chrome/browser/ui/promos_manager/promos_manager_coordinator.mm
@@ -14,11 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/notreached.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/application_context/application_context.h"
+#import "ios/chrome/browser/credential_provider_promo/features.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/ui/app_store_rating/app_store_rating_display_handler.h"
 #import "ios/chrome/browser/ui/app_store_rating/features.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
+#import "ios/chrome/browser/ui/commands/credential_provider_promo_commands.h"
 #import "ios/chrome/browser/ui/commands/promos_manager_commands.h"
+#import "ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_display_handler.h"
 #import "ios/chrome/browser/ui/post_restore_signin/features.h"
 #import "ios/chrome/browser/ui/post_restore_signin/post_restore_signin_provider.h"
 #import "ios/chrome/browser/ui/promos_manager/bannered_promo_view_provider.h"
@@ -455,6 +458,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (IsWhatsNewEnabled()) {
     _displayHandlerPromos[promos_manager::Promo::WhatsNew] =
         [[WhatsNewPromoDisplayHandler alloc] init];
+  }
+
+  // CredentialProvider Promo handler
+  if (IsCredentialProviderExtensionPromoEnabled()) {
+    id<CredentialProviderPromoCommands> handler = HandlerForProtocol(
+        self.browser->GetCommandDispatcher(), CredentialProviderPromoCommands);
+    _displayHandlerPromos[promos_manager::Promo::CredentialProviderExtension] =
+        [[CredentialProviderPromoDisplayHandler alloc] initWithHandler:handler];
   }
 }
 
