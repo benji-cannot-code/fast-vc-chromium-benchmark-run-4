371276d0628c9043c7e5f7f38b0437ab204bb26e
Paul Semel <paulsemel@chromium.org>
2023-01-06 23:23:27 +0000

    Fix include error in GC clang plug-in
    Bug: 1405298
    Change-Id: I3c3f46172bdbe07060629df124c063a4ef561b9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4142718
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089948}

--- tools/clang/blink_gc_plugin/tests/forbidden_fields.h
+++ tools/clang/blink_gc_plugin/tests/forbidden_fields.h
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TOOLS_CLANG_BLINK_GC_PLUGIN_TESTS_FORBIDDEN_FIELDS_H_
 #define TOOLS_CLANG_BLINK_GC_PLUGIN_TESTS_FORBIDDEN_FIELDS_H_
 
-#include <map>
-#include <set>
-#include <vector>
-
 #include "heap/stubs.h"
 
 namespace blink {
