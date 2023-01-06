7c64f98d896d0e5ee75b984a51c1b334675a2ea7
John Lee <johntlee@chromium.org>
2023-01-06 22:25:20 +0000

    WebUI: Introduce cr-buttons that contain icons
    This CL adds two slots for prefix and suffix icons to cr-button so
    that the button can be styled specifically when there are icons
    inside of it.
    
    Currently, cr-buttons with icons are styled independently which
    results in some inconsistencies in how they look. This will standardize
    the look for those who choose to use the attribute and is to be used
    specifically in the side panel.
    
    Bug: 1402857
    Change-Id: I9e2f383f7924cb3fbcfa965690444cb357db659a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117017
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089916}

--- ui/webui/resources/cr_elements/cr_button/cr_button.ts
+++ ui/webui/resources/cr_elements/cr_button/cr_button.ts
@@ -20,6 +20,13 @@ import {FocusOutlineManager} from '../../js/focus_outline_manager.js';
 
 import {getTemplate} from './cr_button.html.js';
 
+export interface CrButtonElement {
+  $: {
+    prefixIcon: HTMLSlotElement,
+    suffixIcon: HTMLSlotElement,
+  };
+}
+
 const CrButtonElementBase =
     mixinBehaviors([PaperRippleBehavior], PolymerElement) as {
       new (): PolymerElement & PaperRippleBehavior,
@@ -59,12 +66,19 @@ export class CrButtonElement extends CrButtonElementBase {
         type: Boolean,
         value: false,
       },
+
+      hasIcon_: {
+        type: Boolean,
+        reflectToAttribute: true,
+        value: false,
+      },
     };
   }
 
   disabled: boolean;
   customTabIndex: number;
   circleRipple: boolean;
+  private hasIcon_: boolean;
 
   /**
    * It is possible to activate a tab when the space key is pressed down. When
@@ -151,6 +165,11 @@ export class CrButtonElement extends CrButtonElementBase {
     if (this.disabled) {
       e.stopImmediatePropagation();
     }
+  }
+
+  private onIconSlotChanged_() {
+    this.hasIcon_ = this.$.prefixIcon.assignedElements().length > 0 ||
+        this.$.suffixIcon.assignedElements().length > 0;
   }
 
   private onKeyDown_(e: KeyboardEvent) {
