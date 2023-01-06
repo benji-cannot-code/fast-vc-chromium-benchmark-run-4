5e2fcea6172911fbe42224ac8f26e674b8e2ba69
Jason Rhee <jasonrhee@google.com>
2023-01-06 19:28:51 +0000

    [QuickStart] Add BootstrapConfigurations Decoder
    To parse the raw data incoming from the phone containing
    BootstrapConfigurations after sending BootstrapOptions, we need to
    validate the response via the decoder in a sandboxed process to comply
    with "Rule of 2". This CL validates that BootstrapConfigurations is
    valid response payload as well as extracting CryptauthDeviceId required
    for UnifiedSetup.
    
    go/cros-quick-start-base-client-class
    go/cros-quick-start-messaging-specs
    
    TEST=added unit tests
    
    Bug: b/264473518
    Change-Id: If6ff9bdfe8ebebc9249dc31b8805a74cb699e05c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134593
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Curt Clemens <cclem@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Commit-Queue: Jason Rhee <jasonrhee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1089827}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/quick_start_decoder.cc
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/quick_start_decoder.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/containers/fixed_flat_set.h"
 #include "base/containers/flat_tree.h"
+#include "base/json/json_reader.h"
 #include "base/logging.h"
+#include "base/values.h"
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom.h"
 #include "components/cbor/reader.h"
 #include "components/cbor/values.h"
@@ -23,6 +25,9 @@ using GetAssertionStatus = mojom::GetAssertionResponse::GetAssertionStatus;
 
 constexpr char kCredentialIdKey[] = "id";
 constexpr char kEntitiyIdMapKey[] = "id";
+constexpr char kBootstrapConfigurationsKey[] = "bootstrapConfigurations";
+constexpr char kDeviceDetailsKey[] = "deviceDetails";
+constexpr char kCryptauthDeviceIdKey[] = "cryptauthDeviceId";
 constexpr uint8_t kCtapDeviceResponseSuccess = 0x00;
 constexpr int kCborDecoderNoError = 0;
 constexpr int kCborDecoderUnknownError = 14;
@@ -151,6 +156,48 @@ mojom::GetAssertionResponsePtr QuickStartDecoder::DoDecodeGetAssertionResponse(
                                           ctap_status, decoded_values.first);
   }
   return ParseGetAssertionResponse(std::move(decoded_values.second.value()));
+}
+
+mojom::BootstrapConfigurationsPtr
+QuickStartDecoder::DoDecodeBootstrapConfigurations(
+    const std::vector<uint8_t>& data) {
+  std::string raw_message_payload(data.begin(), data.end());
+  absl::optional<base::Value> message_payload_json =
+      base::JSONReader::Read(raw_message_payload);
+  if (!message_payload_json.has_value() || !message_payload_json->is_dict()) {
+    LOG(ERROR) << "MessagePayload cannot be parsed as a JSON Dictionary.";
+    return nullptr;
+  }
+  base::Value::Dict& message_payload = message_payload_json.value().GetDict();
+  base::Value::Dict* bootstrap_configurations =
+      message_payload.FindDict(kBootstrapConfigurationsKey);
+  if (!bootstrap_configurations) {
+    LOG(ERROR)
+        << "BootstrapConfigurations cannot be found within MessagePayload.";
+    return nullptr;
+  }
+  base::Value::Dict* device_details =
+      bootstrap_configurations->FindDict(kDeviceDetailsKey);
+  if (!device_details) {
+    LOG(ERROR)
+        << "DeviceDetails cannot be found within BootstrapConfigurations.";
+    return nullptr;
+  }
+  std::string* cryptauth_device_id_ptr =
+      device_details->FindString(kCryptauthDeviceIdKey);
+  if (!cryptauth_device_id_ptr) {
+    LOG(WARNING)
+        << "CryptauthDeviceId for the Android Device could not be found.";
+    return mojom::BootstrapConfigurations::New(/*cryptauth_device_id=*/"");
+  }
+  return mojom::BootstrapConfigurations::New(*cryptauth_device_id_ptr);
+}
+
+void QuickStartDecoder::DecodeBootstrapConfigurations(
+    const std::vector<uint8_t>& data,
+    DecodeBootstrapConfigurationsCallback callback) {
+  DCHECK(sandbox::policy::Sandbox::IsProcessSandboxed());
+  std::move(callback).Run(DoDecodeBootstrapConfigurations(data));
 }
 
 void QuickStartDecoder::DecodeGetAssertionResponse(
