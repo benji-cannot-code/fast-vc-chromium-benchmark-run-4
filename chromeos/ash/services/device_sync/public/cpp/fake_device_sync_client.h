653f9cf021f8f0c12c8f8acf9e6133b4ef87f4f5
Brittany Hartmire <bhartmire@google.com>
2023-01-06 00:56:32 +0000

    Revert "[QuickStart]: Record metric for reason multidevice screen is skipped"
    This reverts commit 573337e1011e3c6d8a0f76279ba756f0212d7480.
    
    Reason for revert: Remove caller that causes P0 crash described in crbug.com/1404037
    
    TEST=unittests pass and manually tested to ensure crbug.com/1404037 is
    no longer reproducible.
    Original change's description:
    > [QuickStart]: Record metric for reason multidevice screen is skipped
    >
    > Create a new metric that will allow us to determine the severity of
    > CryptAuth latency on the multidevice screen in OOBE. See related
    > discussion in go/cros-quick-start-unified-setup-screen-enhancements
    >
    > TEST=create new unittests in MultiDeviceSetupScreenTest for added functionality
    >
    > Bug: b/234655072
    > Change-Id: I6c0715e7f8ef1c846d46f8f080cc0685a31590ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966921
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    > Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1086587}
    
    Bug: b/234655072
    Change-Id: I9f5dc3681ac8f2e3f3641aa8a7d6d2cca1ebf9bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4140199
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1089536}

--- chromeos/ash/services/device_sync/public/cpp/fake_device_sync_client.h
+++ chromeos/ash/services/device_sync/public/cpp/fake_device_sync_client.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/multidevice/remote_device_ref.h"
 #include "chromeos/ash/components/multidevice/software_feature.h"
 #include "chromeos/ash/services/device_sync/feature_status_change.h"
-#include "chromeos/ash/services/device_sync/group_private_key_and_better_together_metadata_status.h"
 #include "chromeos/ash/services/device_sync/proto/cryptauth_common.pb.h"
 #include "chromeos/ash/services/device_sync/public/cpp/device_sync_client.h"
 #include "chromeos/ash/services/device_sync/public/mojom/device_sync.mojom.h"
@@ -113,8 +112,6 @@ class FakeDeviceSyncClient : public DeviceSyncClient {
 
   int GetForceEnrollmentNowCallbackQueueSize() const;
   int GetForceSyncNowCallbackQueueSize() const;
-  int GetBetterTogetherMetadataStatusCallbackQueueSize() const;
-  int GetGroupPrivateKeyStatusCallbackQueueSize() const;
   int GetSetSoftwareFeatureStateInputsQueueSize() const;
   int GetSetFeatureStatusInputsQueueSize() const;
   int GetFindEligibleDevicesInputsQueueSize() const;
@@ -123,10 +120,6 @@ class FakeDeviceSyncClient : public DeviceSyncClient {
 
   void InvokePendingForceEnrollmentNowCallback(bool success);
   void InvokePendingForceSyncNowCallback(bool success);
-  void InvokePendingGetBetterTogetherMetadataStatusCallback(
-      BetterTogetherMetadataStatus status);
-  void InvokePendingGetGroupPrivateKeyStatusCallback(
-      GroupPrivateKeyStatus status);
   void InvokePendingSetSoftwareFeatureStateCallback(
       mojom::NetworkRequestResult result_code);
   void InvokePendingSetFeatureStatusCallback(
