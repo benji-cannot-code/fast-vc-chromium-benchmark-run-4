912386160440bb350792dd14beaa739ab79064ac
Mustafa Acer <meacer@chromium.org>
2023-01-06 19:59:52 +0000

    [ios] Implement mixed content autoupgrade with WKWebView content filters
    This CL implements a basic mixed content auto-upgrade mechanism on iOS
    using WKWebView's content filtering API. Chrome on Desktop/Android
    automatically upgrades passive mixed content (i.e. images, audio and
    video) to HTTPS. It does this by way of using the
    Upgrade-Insecure-Requests code path in blink.
    
    As we don't have blink on iOS, we use the existing content blocking
    API instead. See
    https://developer.apple.com/documentation/safariservices/creating_a_content_blocker
    for details on the content blocker rules.
    
    This CL doesn't have an integration test as there is no good way to
    test autoupgrades:
    A proper test case would have to embed an http resource from an https
    test server. These URLs would have non-default ports as they'd be
    served from local test servers. However, the content blocking API
    doesn't upgrade requests with non-default ports so this doesn't work.
    The only other alternative is to use live external servers that use
    default ports, which is generally an anti-pattern.
    
    Bug: 1405311
    Change-Id: Ie3f986a00aa8a8a8fd3e683398899b27851685f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4100004
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089842}

--- ios/web/web_state/ui/wk_content_rule_list_provider.mm
+++ ios/web/web_state/ui/wk_content_rule_list_provider.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
 
+#import "ios/components/security_interstitials/https_only_mode/feature.h"
 #import "ios/web/public/browser_state.h"
 #import "ios/web/web_state/ui/wk_content_rule_list_util.h"
 #import "ios/web/web_state/ui/wk_web_view_configuration_provider.h"
@@ -33,6 +34,23 @@ WKContentRuleListProvider::WKContentRuleListProvider()
                           block_local_rule_list_ = rule_list;
                           InstallContentRuleLists();
                         }];
+
+  if (base::FeatureList::IsEnabled(
+          security_interstitials::features::kMixedContentAutoupgrade)) {
+    // Auto-upgrade mixed content.
+    [WKContentRuleListStore.defaultStore
+        compileContentRuleListForIdentifier:@"mixed-content-autoupgrade"
+                     encodedContentRuleList:
+                         CreateMixedContentAutoUpgradeJsonRuleList()
+                          completionHandler:^(WKContentRuleList* rule_list,
+                                              NSError* error) {
+                            if (!weak_this.get()) {
+                              return;
+                            }
+                            mixed_content_autoupgrade_rule_list_ = rule_list;
+                            InstallContentRuleLists();
+                          }];
+  }
 }
 
 WKContentRuleListProvider::~WKContentRuleListProvider() {}
@@ -52,11 +70,19 @@ void WKContentRuleListProvider::InstallContentRuleLists() {
   if (block_local_rule_list_) {
     [user_content_controller_ addContentRuleList:block_local_rule_list_];
   }
+  if (mixed_content_autoupgrade_rule_list_) {
+    [user_content_controller_
+        addContentRuleList:mixed_content_autoupgrade_rule_list_];
+  }
 }
 
 void WKContentRuleListProvider::UninstallContentRuleLists() {
   if (block_local_rule_list_) {
     [user_content_controller_ removeContentRuleList:block_local_rule_list_];
+  }
+  if (mixed_content_autoupgrade_rule_list_) {
+    [user_content_controller_
+        removeContentRuleList:mixed_content_autoupgrade_rule_list_];
   }
 }
 
