0802ea05bfb7828df3e0601951c13d2e808f357f
Benoit Lize <lizeb@chromium.org>
2023-01-06 09:25:28 +0000

    [viz] Enable eager compositor frame garbage collection by default
    This CL enables two features by default, and updates the tests and
    testing config accordingly: AggressiveFrameCulling and
    EagerSurfaceGarbageCollection.
    
    This improves memory usage on all platforms, by reclaiming unlocked
    compositor frames after 5 minutes of inactivity, and reclaiming surfaces
    marked for destruction immediately.
    
    This has two concrete consequences:
    - For a user with 1 browser window and 5 tabs, typical savings are to 5x
      tiles for a single tab, which is ~screen resolution. Concretely, on a
      laptop with a High-DPI screen, this can reach 150MiB (32MiB per screen
      on a 4K screen for instance).
    - On Android, when the browser is in background, since no new compostor
      frames are produced and frame eviction is now synchronous, we do evict
      tiles (vs previously not doing it even in the case of critical memory
      pressure). Savings can reach >100MiB on a QHD phone (e.g. Pixel 6/7
      Pro)
    
    These observations are confirmed by a Finch experiment (10% stable). See
    https://docs.google.com/document/d/1a-xd0qB2pix7VNn3LURLi1GbCz_0YZR5C8_6BrUiIWE/edit#heading=h.9ympgyv76dpv
    (Google-internal) for results, and
    https://groups.google.com/a/google.com/g/chrome-gpu-memory/c/xswTuU_hxX0
    for an analysis of the dependency on the number of browser windows.
    
    Bug: 1355820
    Change-Id: Iee36f81eb6a2d8a78a1258a30ff45a2d5451b9bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134079
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089650}

--- components/viz/client/frame_eviction_manager_unittest.cc
+++ components/viz/client/frame_eviction_manager_unittest.cc
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/memory_pressure_listener.h"
 #include "base/ranges/algorithm.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/test_mock_time_task_runner.h"
-#include "components/viz/common/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace viz {
@@ -77,7 +75,6 @@ TEST_F(FrameEvictionManagerTest, ScopedPause) {
 }
 
 TEST_F(FrameEvictionManagerTest, PeriodicCulling) {
-  base::test::ScopedFeatureList feature_list{features::kAggressiveFrameCulling};
   // Cannot use a TaskEnvironment as there is already one which is not using
   // MOCK_TIME.
   auto task_runner = base::MakeRefCounted<base::TestMockTimeTaskRunner>();
@@ -122,20 +119,7 @@ TEST_F(FrameEvictionManagerTest, MemoryPressure) {
   manager->AddFrame(&frame1, false);
   manager->AddFrame(&frame2, false);
 
-  // We keep one frame around, no matter how many times we get a memory pressure
-  // notification.
-  manager->OnMemoryPressure(
-      base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL);
-  EXPECT_FALSE(frame1.has_frame());
-  EXPECT_TRUE(frame2.has_frame());
-
-  manager->OnMemoryPressure(
-      base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL);
-  EXPECT_FALSE(frame1.has_frame());
-  EXPECT_TRUE(frame2.has_frame());
-
-  // Unless aggressive frame culling is enabled.
-  base::test::ScopedFeatureList feature_list{features::kAggressiveFrameCulling};
+  // Critical memory pressure culls all unlocked frames.
   manager->OnMemoryPressure(
       base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL);
   EXPECT_FALSE(frame1.has_frame());
