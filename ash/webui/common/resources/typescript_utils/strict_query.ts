8488b616154b7fc2230874bb2de89a4ffab4ef3e
Cam Bickel <cambickel@google.com>
2023-01-07 00:34:26 +0000

    Add strictQuery util to ash/webui/common
    This CL adds a new utility function called strictQuery that will be used
    to query for elements that are known to exist. See this doc for more
    details: http://shortn/_bYxciRtIGj
    
    This CL creates a new directory "typescript_utils" in ash/webui/common
    that can be used for future TS utilities, such as the strictQueryMixin
    implemented in the next downstream CL.
    
    This CL also adds tests for the new strictQuery function.
    
    Bug: b/263270456
    Test: browser_tests --gtest_filter=AshCommonResourcesTypescriptUtilsStrictQueryTest*
    Change-Id: I826d1424748661e85add7ec757aa715a8baedd92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4137582
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Auto-Submit: Camden Bickel <cambickel@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089973}

---
+++ ash/webui/common/resources/typescript_utils/strict_query.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {assert} from 'chrome://resources/js/assert_ts.js';
+
+/**
+ * Queries |selector| on |root| and returns the first matching element. Throws
+ * exception if there is no resulting element or if element is not of type
+ * |type|.
+ */
+export function strictQuery<T>(
+    selector: string, root: Element|ShadowRoot|null, type: Constructor<T>): T {
+  const element = root!.querySelector(selector);
+  assert(element, 'Queried element is not defined.');
+  assert(
+      element instanceof type, 'Queried element is not an instance of type T.');
+  return element;
+}
+
+/** Helper type used in {@link strictQuery}. */
+type Constructor<T> = new (...args: any[]) => T;
