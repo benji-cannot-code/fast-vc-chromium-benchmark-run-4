4355a227085043058bc7855d32fbce5b73e24b0d
Andrey Kosyakov <caseq@chromium.org>
2023-01-09 18:48:07 +0000

    Headless: remove unused configuration options
    This removes most unused configuration options in HeadlessBrowser::Options
    and HeadlessBrowserContext::Options along with associated builders. Some
    context options are kept for the sake of test coverage, because tests override
    these options whereas production code overrides Browser options.
    
    Change-Id: I8ec24b0df0a130a4b4db19939087d850ccb95262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143935
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090424}

--- headless/lib/browser/headless_browser_context_options.h
+++ headless/lib/browser/headless_browser_context_options.h
@@ -8,12 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/callback.h"
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "headless/public/headless_browser.h"
-#include "headless/public/headless_browser_context.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/gfx/font_render_params.h"
 
 namespace headless {
 
@@ -33,7 +32,6 @@ class HeadlessBrowserContextOptions {
   HeadlessBrowserContextOptions& operator=(
       HeadlessBrowserContextOptions&& options);
 
-  const std::string& product_name_and_version() const;
   const std::string& accept_language() const;
   const std::string& user_agent() const;
 
@@ -54,11 +52,6 @@ class HeadlessBrowserContextOptions {
   // See HeadlessBrowser::Options::font_render_hinting.
   gfx::FontRenderParams::Hinting font_render_hinting() const;
 
-  // Callback that is invoked to override WebPreferences for RenderViews
-  // created within this HeadlessBrowserContext.
-  base::RepeatingCallback<void(blink::web_pref::WebPreferences*)>
-  override_web_preferences_callback() const;
-
  private:
   friend class HeadlessBrowserContext::Builder;
 
@@ -66,18 +59,13 @@ class HeadlessBrowserContextOptions {
 
   raw_ptr<HeadlessBrowser::Options> browser_options_;
 
-  absl::optional<std::string> product_name_and_version_;
   absl::optional<std::string> accept_language_;
   absl::optional<std::string> user_agent_;
   std::unique_ptr<net::ProxyConfig> proxy_config_;
-  absl::optional<std::string> host_resolver_rules_;
   absl::optional<gfx::Size> window_size_;
   absl::optional<base::FilePath> user_data_dir_;
   absl::optional<bool> incognito_mode_;
   absl::optional<bool> block_new_web_contents_;
-  absl::optional<
-      base::RepeatingCallback<void(blink::web_pref::WebPreferences*)>>
-      override_web_preferences_callback_;
 
   absl::optional<gfx::FontRenderParams::Hinting> font_render_hinting_;
 };
