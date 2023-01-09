a349344a2c9e8e87b67b2ec02f1fd39ff98e1cb0
Anders Hartvoll Ruud <andruud@chromium.org>
2023-01-09 23:14:20 +0000

    [StyleBuilder] Remove StyleResolverState::Style
    Replace remaining calls with ComputedStyleBuilder::InternalStyle to
    avoid new unwanted callers of StyleResolverState::Style.
    
    There should be no behavior change.
    
    Bug: 1377295
    Change-Id: I5461a666fcd7911a868b3b2dc79b3d3ce81459bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144804
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090566}

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
@@ -120,7 +120,8 @@ scoped_refptr<ComputedStyle> StyleResolverState::TakeStyle() {
 
 void StyleResolverState::UpdateLengthConversionData() {
   css_to_length_conversion_data_ = CSSToLengthConversionData(
-      Style(), ParentStyle(), RootElementStyle(), GetDocument().GetLayoutView(),
+      style_builder_.InternalStyle(), ParentStyle(), RootElementStyle(),
+      GetDocument().GetLayoutView(),
       CSSToLengthConversionData::ContainerSizes(container_unit_context_),
       StyleBuilder().EffectiveZoom(), length_conversion_flags_);
   element_style_resources_.UpdateLengthConversionData(
@@ -133,7 +134,8 @@ CSSToLengthConversionData StyleResolverState::UnzoomedLengthConversionData(
   const ComputedStyle* root_font_style = RootElementStyle();
   CSSToLengthConversionData::FontSizes font_sizes(font_style, root_font_style);
   CSSToLengthConversionData::LineHeightSize line_height_size(
-      ParentStyle() ? *ParentStyle() : *Style(), root_font_style);
+      ParentStyle() ? *ParentStyle() : *style_builder_.InternalStyle(),
+      root_font_style);
   CSSToLengthConversionData::ViewportSize viewport_size(
       GetDocument().GetLayoutView());
   CSSToLengthConversionData::ContainerSizes container_sizes(
@@ -149,7 +151,7 @@ CSSToLengthConversionData StyleResolverState::FontSizeConversionData() {
 }
 
 CSSToLengthConversionData StyleResolverState::UnzoomedLengthConversionData() {
-  return UnzoomedLengthConversionData(Style());
+  return UnzoomedLengthConversionData(style_builder_.InternalStyle());
 }
 
 void StyleResolverState::SetParentStyle(
@@ -168,7 +170,7 @@ void StyleResolverState::LoadPendingResources() {
   if (pseudo_request_type_ == StyleRequest::kForComputedStyle ||
       (ParentStyle() && ParentStyle()->IsEnsuredInDisplayNone()) ||
       (StyleBuilder().Display() == EDisplay::kNone &&
-       !GetElement().LayoutObjectIsNeeded(*Style())) ||
+       !GetElement().LayoutObjectIsNeeded(*style_builder_.InternalStyle())) ||
       StyleBuilder().IsEnsuredOutsideFlatTree()) {
     return;
   }
@@ -254,15 +256,16 @@ const CSSValue& StyleResolverState::ResolveLightDarkPair(
 
 void StyleResolverState::UpdateFont() {
   GetFontBuilder().CreateFont(StyleBuilder(), ParentStyle());
-  SetConversionFontSizes(
-      CSSToLengthConversionData::FontSizes(Style(), RootElementStyle()));
+  SetConversionFontSizes(CSSToLengthConversionData::FontSizes(
+      style_builder_.InternalStyle(), RootElementStyle()));
   SetConversionZoom(StyleBuilder().EffectiveZoom());
 }
 
 void StyleResolverState::UpdateLineHeight() {
   css_to_length_conversion_data_.SetLineHeightSize(
       CSSToLengthConversionData::LineHeightSize(
-          *Style(), GetDocument().documentElement()->GetComputedStyle()));
+          *style_builder_.InternalStyle(),
+          GetDocument().documentElement()->GetComputedStyle()));
 }
 
 }  // namespace blink
