130b6fcd0edc42e961b330d2400f915129b89968
Chris Lu <thegreenfrog@chromium.org>
2023-01-09 18:39:33 +0000

    Reland "[ios] Remove ContentSuggestionsHeaderSynchronizer"
    This is a reland of commit c6547826eec5e31beeb79e0dc7c5c81eb865a86c
    
    The reland has the following new change:
    Originally, NTPCoordinator's -focusFakebox directly calls the
    header's -focusFakebox if the feed is not visible, which essentially
    assumes that the animation can happen at any moment. This is because
    the surface does not need to wait for the feed contents to load in
    order to be scrollable. To accommodate for this, NTPViewController's
    -focusFakebox implementation will check `isFeedVisible` before
    delaying the animation to -viewDidAppear:. in -viewDidAppear:,
    the [self collectionViewHasLoaded] is removed since it will always be
    NO if the feed is not being shown. This should not be a problem for
    when the feed is being shown since -updateFeedInsetsForMinimumHeight
    helps give the surface scrollability while the contents are loading.
    
    Original change's description:
    > [ios] Remove ContentSuggestionsHeaderSynchronizer
    >
    > By removing this class, its logic is split into between
    > NewTabPageViewController and ContentSuggestionsHeaderViewController.
    > Both now own their animation changes in response to omnibox focusing
    > and defocusing. NewTabPageViewController now owns all logic covering its ScrollView offset math. NewTabPageViewController also now drives all changes to the surface. That means all omnibox focus animations, even from taps on the fake omnibox, will go through the
    > NewTabPageViewController.
    >
    > In all instances, function logic is not changed. To separate
    > animation ownership, logic that used to be in completion blocks
    > now exist in new expandHeaderForFocus and
    > completeHeaderFakeOmniboxFocusAnimationWithFinalPosition:
    > HeaderViewController functions. Improvements to how the logic
    > operates will be handled in future changes in order to make this
    > one much easier to review.
    >
    > Bug: 1404977
    > Change-Id: Iecf69c200bc0730260fd5052b86ec595d125bff7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116237
    > Reviewed-by: Adam Arcaro <adamta@google.com>
    > Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1088941}
    
    Bug: 1404977
    Change-Id: Id7ff6de110749678a71db49daee6441bd42f816d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4138559
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090418}

--- ios/chrome/browser/ui/ntp/new_tab_page_mediator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_mediator.mm
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_return_to_recent_tab_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
-#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_header_synchronizer.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller_audience.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_consumer.h"
@@ -180,7 +179,7 @@ const char kFeedLearnMoreURL[] = "https://support.google.com/chrome/"
     return;
   }
 
-  CGFloat scrollPosition = [self.ntpViewController scrollPosition];
+  CGFloat scrollPosition = [self.NTPViewController scrollPosition];
 
   if ([self.suggestionsMediator mostRecentTabStartSurfaceTileIsShowing]) {
     // Return to Recent tab tile is only shown one time, so subtract it's
@@ -189,14 +188,12 @@ const char kFeedLearnMoreURL[] = "https://support.google.com/chrome/"
         ReturnToRecentTabHeight() +
         content_suggestions::kReturnToRecentTabSectionBottomMargin;
     if (scrollPosition >
-        tileSectionHeight +
-            [self.headerCollectionInteractionHandler pinnedOffsetY]) {
+        tileSectionHeight + [self.NTPViewController pinnedOffsetY]) {
       scrollPosition -= tileSectionHeight;
     }
   }
 
-  scrollPosition -=
-      self.headerCollectionInteractionHandler.collectionShiftingOffset;
+  scrollPosition -= self.NTPViewController.collectionShiftingOffset;
 
   NewTabPageTabHelper* NTPHelper = NewTabPageTabHelper::FromWebState(webState);
 
@@ -327,12 +324,12 @@ const char kFeedLearnMoreURL[] = "https://support.google.com/chrome/"
     offsetFromSavedState = -CGFLOAT_MAX;
   }
 
-  CGFloat minimumOffset = -[self.ntpViewController heightAboveFeed];
+  CGFloat minimumOffset = -[self.NTPViewController heightAboveFeed];
   if (offsetFromSavedState > minimumOffset) {
-    [self.ntpViewController setSavedContentOffset:offsetFromSavedState];
+    [self.NTPViewController setSavedContentOffset:offsetFromSavedState];
   } else {
     // Remove this if NTPs are ever scoped back to the WebState.
-    [self.ntpViewController setContentOffsetToTop];
+    [self.NTPViewController setContentOffsetToTop];
     // Refresh NTP content if there is is no saved scrolled state or when a new
     // NTP is opened. Since the same NTP is being shared across tabs, this
     // ensures that new content is being fetched.
