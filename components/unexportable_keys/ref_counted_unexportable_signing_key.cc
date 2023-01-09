21f4a7445d7e396f3fc771e4d7776105626fa730
Alex Ilin <alexilin@chromium.org>
2023-01-09 11:39:50 +0000

    [BSC] Add components/unexportable_keys
    This CL adds a new unexportable_keys component with a stub
    implementation of one of its main classes, UnexportableKeyTaskScheduler.
    This class facilitates work with crypto::UnexportableSigningKey which
    contains methods for creating encryption keys and signing data on a
    specialized secure hardware (TPM).
    
    This hardware can only process one command at a time, and some commands
    can run for a significant period of time. Thus, it's important to
    utilize TPM hardware as optimally as possible.
    
    All TPM commands issued by Chrome will go through a centralized
    UnexportableKeyTaskScheduler which schedules commands according to their
    priorities and ensures that cancelled commands don't get scheduled.
    
    This code is put in its own component because:
    1) Another component (components/signin) is going to depend on it
    in the short term
    2) We have another consumer not directly related to sign-in that will
    need it in the medium term (design is still in progress)
    3) There are existing use-cases of crypto::UnexportableSigningKey that
    can be migrated to this component
    
    Bug: b/263249728
    Change-Id: I84c8da341d24adba1bd8dd1cc6bbaa74a0d2f70c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136194
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090269}

---
+++ components/unexportable_keys/ref_counted_unexportable_signing_key.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/unexportable_keys/ref_counted_unexportable_signing_key.h"
+
+#include <memory>
+
+#include "crypto/unexportable_key.h"
+
+namespace unexportable_keys {
+
+namespace {
+RefCountedUnexportableSigningKey::KeyId GetNextKeyId() {
+  static uint32_t next_id = 0;
+  return RefCountedUnexportableSigningKey::KeyId(next_id++);
+}
+}  // namespace
+
+RefCountedUnexportableSigningKey::RefCountedUnexportableSigningKey(
+    std::unique_ptr<crypto::UnexportableSigningKey> key)
+    : key_(std::move(key)), key_id_(GetNextKeyId()) {
+  DCHECK(key_);
+}
+
+RefCountedUnexportableSigningKey::~RefCountedUnexportableSigningKey() = default;
+
+}  // namespace unexportable_keys
