012aca20bfda2cf8e9a8bf62ed28ab2ba7d20b61
Bruce Dawson <brucedawson@chromium.org>
2023-01-09 20:18:31 +0000

    Set thread name in TestLauncher::Run
    Thread names are useful when debugging for identifying which process is
    which. While investigating some test failures I noticed that the
    
    TestLauncher::Run thread didn't have a thread name so this gives it one.
    See also crrev.com/c/4129799 for utility thread names.
    
    Change-Id: I63c92cb8ed7b98213cae72c310c1512460d67a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127916
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090472}

--- base/test/launcher/test_launcher.cc
+++ base/test/launcher/test_launcher.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_file_util.h"
 #include "base/test/test_switches.h"
 #include "base/test/test_timeouts.h"
+#include "base/threading/platform_thread.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -1065,6 +1066,8 @@ TestLauncher::~TestLauncher() {
 }
 
 bool TestLauncher::Run(CommandLine* command_line) {
+  base::PlatformThread::SetName("TestLauncherMain");
+
   if (!Init((command_line == nullptr) ? CommandLine::ForCurrentProcess()
                                       : command_line))
     return false;
