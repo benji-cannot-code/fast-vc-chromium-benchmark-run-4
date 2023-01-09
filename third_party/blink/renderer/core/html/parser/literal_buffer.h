3591aecec340acf07f4c701a55920ce3a7324597
Scott Violet <sky@chromium.org>
2023-01-09 16:26:51 +0000

    blink: removes kLiteralBufferCreateStringWithEncoding feature
    This has been enabled for a while with no ill effects.
    
    BUG=1331076
    
    Change-Id: Ia888adc00baa030de20e080669971be913ef56e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4137225
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090359}

--- third_party/blink/renderer/core/html/parser/literal_buffer.h
+++ third_party/blink/renderer/core/html/parser/literal_buffer.h
@@ -28,12 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BUFFER_INLINE_CAPACITY kInlineSize
 #endif
 
-// Controls whether strings created by LiteralBuffer have an encoding specified.
-// Specifying the encoding may avoid unnecessary allocations and checks to
-// determine encoding, and allows for a fast path when copying UChars to
-// LChars.
-CORE_EXPORT extern bool g_literal_buffer_create_string_with_encoding;
-
 // LiteralBufferBase is an optimized version of Vector for LChar and UChar
 // characters. In particular `AddChar` is faster than `push_back`, since
 // it avoids unnecessary register spills. See https://crbug.com/1205338.
@@ -281,8 +275,9 @@ class UCharLiteralBuffer : public LiteralBufferBase<UChar, kInlineSize> {
   }
 
   String AsString() const {
-    if (g_literal_buffer_create_string_with_encoding && Is8Bit())
+    if (Is8Bit()) {
       return String::Make8BitFrom16BitSource(this->data(), this->size());
+    }
     return String(this->data(), this->size());
   }
 
@@ -291,8 +286,6 @@ class UCharLiteralBuffer : public LiteralBufferBase<UChar, kInlineSize> {
   }
 
   AtomicString AsAtomicString() const {
-    if (!g_literal_buffer_create_string_with_encoding)
-      return AtomicString(this->data(), this->size());
     return AtomicString(this->data(), this->size(),
                         Is8Bit() ? WTF::AtomicStringUCharEncoding::kIs8Bit
                                  : WTF::AtomicStringUCharEncoding::kIs16Bit);
