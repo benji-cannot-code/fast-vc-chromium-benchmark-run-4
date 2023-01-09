17f6846b7674350d62e24ab719bd56c281a4e204
Mikel Astiz <mastiz@chromium.org>
2023-01-09 11:28:09 +0000

    [ReadingList] Improve unit-tests for ReadingListModelImpl
    Before this patch, a home-grown AssertObserverCount() was used to verify
    the number of invocations to observer functions. This function grew
    over time and it took 10 parameters already, representing individual
    counter expectations, which significantly harm test readability and
    maintenance.
    
    With this patch, a standard gMock mock is adopted instead, which offers
    several advantages, including:
    1) You can read in the test body which functions are expected to be
       called, rather than having to guess what each of the 10 counters
       represents.
    2) Tests don't need to be overly strict: they can verify only the
       functions that are relevant for the particular test.
    3) Function parameters can be verified as necessary.
    4) Ordering can be verified.
    5) Failure messages point to a relevant line in the test body, rather
       than AssertObserverCount().
    
    Change-Id: I35d4c178d7c7237b7bfe30b73ae6b61068950ecb
    Bug: 1402196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128931
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090265}

---
+++ components/reading_list/core/mock_reading_list_model_observer.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/reading_list/core/mock_reading_list_model_observer.h"
+
+MockReadingListModelObserver::MockReadingListModelObserver() = default;
+
+MockReadingListModelObserver::~MockReadingListModelObserver() = default;
