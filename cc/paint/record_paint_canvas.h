1de348c915a0b42e8cf497f20c2804e5c953bed0
Scott Violet <sky@chromium.org>
2023-01-09 17:22:31 +0000

    cc: removes a handful of functions that aren't needed
    These functions have no callers, so removing.
    
    Change-Id: I6087f529a0bb10bc81f0259935fca454c4566996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144304
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090377}

--- cc/paint/record_paint_canvas.h
+++ cc/paint/record_paint_canvas.h
@@ -70,11 +70,8 @@ class CC_PAINT_EXPORT RecordPaintCanvas : public PaintCanvas {
   // the constructor. With this restriction, we don't need to create
   // SkNoDrawCanvas for clients that only need recording.
   SkImageInfo imageInfo() const override;
-  SkRect getLocalClipBounds() const override;
   bool getLocalClipBounds(SkRect* bounds) const override;
-  SkIRect getDeviceClipBounds() const override;
   bool getDeviceClipBounds(SkIRect* bounds) const override;
-  bool isClipEmpty() const override;
   SkM44 getLocalToDevice() const override;
 
   void drawColor(SkColor4f color, SkBlendMode mode) override;
@@ -222,11 +219,8 @@ class CC_PAINT_EXPORT InspectableRecordPaintCanvas : public RecordPaintCanvas {
   void clipRect(const SkRect& rect, SkClipOp op, bool antialias) override;
 
   SkImageInfo imageInfo() const override;
-  SkRect getLocalClipBounds() const override;
   bool getLocalClipBounds(SkRect* bounds) const override;
-  SkIRect getDeviceClipBounds() const override;
   bool getDeviceClipBounds(SkIRect* bounds) const override;
-  bool isClipEmpty() const override;
   SkM44 getLocalToDevice() const override;
 
   // Don't shadow non-virtual helper functions.
