46869401346cfdd0f93350a4ed2d2ecaddb2722e
Wolfgang Billenstein <bwolfgang@google.com>
2023-01-09 17:45:03 +0000

    [RELAND] [Fast Checkout] Refactor to remove delegate
    Puts FastCheckoutDelegate logic into chrome/browser/fast_checkout and
    removes the delegate.
    
    Reland of crrev.com/c/4075573.
    
    Bug: 1334642, 1403003, 1403097
    Change-Id: I16eae82c645a142c72f5498356759abff4f17923
    Low-Coverage-Reason: AutofillClient interface change.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129526
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090388}

--- components/autofill/core/browser/browser_autofill_manager.h
+++ components/autofill/core/browser/browser_autofill_manager.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_driver.h"
 #include "components/autofill/core/browser/autofill_external_delegate.h"
 #include "components/autofill/core/browser/autofill_manager.h"
-#include "components/autofill/core/browser/fast_checkout_delegate.h"
 #include "components/autofill/core/browser/field_filler.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/form_types.h"
@@ -329,11 +328,6 @@ class BrowserAutofillManager : public AutofillManager,
   void SetTouchToFillDelegateImplForTest(
       std::unique_ptr<TouchToFillDelegateImpl> touch_to_fill_delegate) {
     touch_to_fill_delegate_ = std::move(touch_to_fill_delegate);
-  }
-
-  void SetFastCheckoutDelegateForTest(
-      std::unique_ptr<FastCheckoutDelegate> fast_checkout_delegate) {
-    fast_checkout_delegate_ = std::move(fast_checkout_delegate);
   }
 
   static void DeterminePossibleFieldTypesForUploadForTest(
@@ -722,7 +716,6 @@ class BrowserAutofillManager : public AutofillManager,
   // Delegates to perform external processing (display, selection) on
   // our behalf.
   std::unique_ptr<AutofillExternalDelegate> external_delegate_;
-  std::unique_ptr<FastCheckoutDelegate> fast_checkout_delegate_;
   std::unique_ptr<TouchToFillDelegateImpl> touch_to_fill_delegate_;
 
   std::string app_locale_;
