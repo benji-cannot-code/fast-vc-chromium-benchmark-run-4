32db514f15a38ceee9959336a98ee6bb20ee5979
Michael Thiessen <mthiesse@chromium.org>
2023-01-09 17:42:56 +0000

    Remove VR Browser code in blink/
    The VR browser has been disabled for over 2 years, but the code hasn't
    yet been cleaned up. This removes the blink/ code whose only purpose
    was supporting the VR browser.
    
    Bug: 1095740
    Change-Id: I40ef5de2084b0e7dac6f8ba14cb069fcf0c3905d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4141607
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090387}

--- third_party/blink/renderer/core/page/chrome_client_impl.cc
+++ third_party/blink/renderer/core/page/chrome_client_impl.cc
@@ -704,10 +704,6 @@ ColorChooser* ChromeClientImpl::OpenColorChooser(
   NotifyPopupOpeningObservers();
   ColorChooserUIController* controller = nullptr;
 
-  // TODO(crbug.com/779126): add support for the chooser in immersive mode.
-  if (frame->GetDocument()->GetSettings()->GetImmersiveModeEnabled())
-    return nullptr;
-
   if (RuntimeEnabledFeatures::PagePopupEnabled()) {
     controller = MakeGarbageCollected<ColorChooserPopupUIController>(
         frame, this, chooser_client);
@@ -726,13 +722,6 @@ DateTimeChooser* ChromeClientImpl::OpenDateTimeChooser(
     LocalFrame* frame,
     DateTimeChooserClient* picker_client,
     const DateTimeChooserParameters& parameters) {
-  // TODO(crbug.com/779126): add support for the chooser in immersive mode.
-  if (picker_client->OwnerElement()
-          .GetDocument()
-          .GetSettings()
-          ->GetImmersiveModeEnabled())
-    return nullptr;
-
   NotifyPopupOpeningObservers();
   if (RuntimeEnabledFeatures::InputMultipleFieldsUIEnabled()) {
     return MakeGarbageCollected<DateTimeChooserImpl>(frame, picker_client,
