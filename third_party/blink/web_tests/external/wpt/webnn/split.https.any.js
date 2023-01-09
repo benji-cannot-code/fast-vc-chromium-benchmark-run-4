8287ce8d8f8f6ce043b32dd5fd0a48903e978044
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-01-09 09:01:40 +0000

    Import wpt@39f51bf650f4234cb5d70f8437425126f22e4fa4
    Using wpt-import in Chromium 8f15f3a99d34cd23dcef040a126182203322ae5b.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Ic293aecc730b7df640e517d07931dc072d976c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144782
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1090228}

---
+++ third_party/blink/web_tests/external/wpt/webnn/split.https.any.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=test WebNN API split operation
+// META: global=window,dedicatedworker
+// META: script=./resources/utils.js
+// META: timeout=long
+
+'use strict';
+
+// https://webmachinelearning.github.io/webnn/#api-mlgraphbuilder-split
+
+const buildSplit = (operationName, builder, resources) => {
+  // sequence<MLOperand> split(MLOperand input,
+  //                           (unsigned long or sequence<unsigned long>) splits,
+  //                           optional MLSplitOptions options = {});
+  const namedOutputOperand = {};
+  const inputOperand = createSingleInputOperand(builder, resources);
+  // invoke builder.split()
+  const outputOperands = builder[operationName](inputOperand, resources.splits, resources.options);
+  resources.expected.forEach((resourceDict, index) => {
+    namedOutputOperand[resourceDict.name] = outputOperands[index];
+  });
+  return namedOutputOperand;
+};
+
+testWebNNOperation('split', '/webnn/resources/test_data/split.json', buildSplit);
