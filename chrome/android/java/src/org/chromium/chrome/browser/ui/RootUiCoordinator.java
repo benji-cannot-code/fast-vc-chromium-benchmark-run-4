bb81b25ddb3edd13db609dac633ca49cf8d9244f
Matt Jones <mdjones@chromium.org>
2023-01-09 14:59:03 +0000

    Remove commerce subscriptions desync logic
    This patch removes the java subscriptions desync logic and marks the
    corresponding histogram that tracked the results as obsolete. The
    native subscriptions manager now handles these cases and the logic
    causes issues now that we are running on multiple platforms.
    
    Bug: 1293885, 1375296
    Change-Id: I3367b56cf10b2d2e59168f6691b40689052ee063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964148
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090317}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -43,10 +43,8 @@ import org.chromium.chrome.browser.app.omnibox.OmniboxPedalDelegateImpl;
 import org.chromium.chrome.browser.app.tab_activity_glue.TabReparentingController;
 import org.chromium.chrome.browser.back_press.BackPressManager;
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
-import org.chromium.chrome.browser.bookmarks.PowerBookmarkUtils;
 import org.chromium.chrome.browser.bookmarks.TabBookmarker;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
-import org.chromium.chrome.browser.commerce.ShoppingFeatures;
 import org.chromium.chrome.browser.commerce.ShoppingServiceFactory;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
 import org.chromium.chrome.browser.compositor.bottombar.OverlayPanel;
@@ -106,7 +104,6 @@ import org.chromium.chrome.browser.share.ShareDelegate.ShareOrigin;
 import org.chromium.chrome.browser.share.ShareUtils;
 import org.chromium.chrome.browser.share.qrcode.QrCodeDialog;
 import org.chromium.chrome.browser.share.scroll_capture.ScrollCaptureManager;
-import org.chromium.chrome.browser.subscriptions.CommerceSubscriptionsServiceFactory;
 import org.chromium.chrome.browser.tab.AccessibilityVisibilityHandler;
 import org.chromium.chrome.browser.tab.AutofillSessionLifetimeController;
 import org.chromium.chrome.browser.tab.RequestDesktopUtils;
@@ -776,17 +773,6 @@ public class RootUiCoordinator
 
         initMerchantTrustSignals();
         initScrollCapture();
-
-        // TODO(1293885): Remove this validator once we have an API on the backend that sends
-        //                success/failure information back.
-        if (ShoppingFeatures.isShoppingListEnabled()) {
-            mBookmarkModelSupplier.addObserver((bridge) -> {
-                PowerBookmarkUtils.validateBookmarkedCommerceSubscriptions(bridge,
-                        new CommerceSubscriptionsServiceFactory()
-                                .getForLastUsedProfile()
-                                .getSubscriptionsManager());
-            });
-        }
 
         new OneShotCallback<>(mProfileSupplier, this::initHistoryClustersCoordinator);
 
