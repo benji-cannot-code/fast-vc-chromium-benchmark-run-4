d00ffb5a7d32adef552553ee5aed16a31800f143
David Bienvenu <davidbienvenu@chromium.org>
2023-01-09 22:58:20 +0000

    win7dep: Remove more Win7/8 code from content/*
    Also fixes lint warnings.
    
    Bug: 1385856
    Change-Id: I7e5bf1c7a36120b2cb70b432bf26dec27fc8787d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4140539
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090556}

--- content/child/font_warmup_win.cc
+++ content/child/font_warmup_win.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <dwrite.h>
 #include <stdint.h>
 #include <map>
+#include <string>
+#include <utility>
 
 #include "base/debug/alias.h"
 #include "base/files/file_path.h"
@@ -23,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_byteorder.h"
 #include "base/trace_event/trace_event.h"
 #include "base/win/iat_patch_function.h"
-#include "base/win/windows_version.h"
 #include "build/build_config.h"
 #include "ppapi/buildflags/buildflags.h"
 #include "third_party/skia/include/core/SkFontMgr.h"
@@ -392,10 +393,7 @@ void PatchServiceManagerCalls() {
   static bool is_patched = false;
   if (is_patched)
     return;
-  const char* service_provider_dll =
-      (base::win::GetVersion() >= base::win::Version::WIN8
-           ? "api-ms-win-service-management-l1-1-0.dll"
-           : "advapi32.dll");
+  const char* service_provider_dll = "api-ms-win-service-management-l1-1-0.dll";
 
   is_patched = true;
 
