8ddb6378dab23ac336aeb770149a59047a2837db
Simon Zünd <szuend@chromium.org>
2023-01-09 05:59:58 +0000

    [devtools] Add test that pauses an infinite loop
    This CL adds a test for the V8 CL: https://crrev.com/c/4134246
    
    V8 inspector tests don't support interrupting protocol messages, i.e.
    the Debugger.paused command won't interrupt V8 execution. That
    behavior is critical for this test so we implement it as content_shell
    test instead.
    
    R=jarin@chromium.org
    
    Bug: 1401674
    Change-Id: If55c311f827f0950b3c9f8fe9921ddd1e02d7591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136058
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090212}

---
+++ third_party/blink/web_tests/inspector-protocol/debugger/debugger-pause-infinite-loop.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {dp} = await testRunner.startBlank('Tests that an infinite loop can be interrupted and paused from a step-over.');
+
+  await dp.Debugger.enable();
+
+  dp.Runtime.evaluate({
+    expression: `
+      function f() {
+        let i = 0;
+        while (true) {i++}
+      }
+
+      debugger;
+      f();`,
+  });
+
+  testRunner.log('didEval');
+  await dp.Debugger.oncePaused();
+  dp.Debugger.stepInto();
+  await dp.Debugger.oncePaused();
+
+  testRunner.log('Expecting the debugger to pause the loop ...');
+  await dp.Debugger.stepOver();
+
+  // Delay the pause slightly to let the loop do some spinning.
+  await new Promise(r => setTimeout(r, 0));
+
+  dp.Debugger.pause();
+  await dp.Debugger.oncePaused();
+
+  testRunner.log('SUCCESS');
+  testRunner.completeTest();
+});
