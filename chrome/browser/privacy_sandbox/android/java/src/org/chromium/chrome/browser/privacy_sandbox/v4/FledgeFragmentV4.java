5035a8290fca31eb372937ca334137a978715561
Rubin Deliallisi <rubindl@chromium.org>
2023-01-09 15:09:48 +0000

    Privacy Sandbox Settings: Fledge pages UI update
    - Update the Fledge page empty/disabled state UI.
    - Update the condition for displaying "See all sites" pref.
    
    Bug: 1378703, b/261819444
    Change-Id: I4b9f42e755626f356416e028a9bd6b99fcc109b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111604
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090325}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/FledgeFragmentV4.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/FledgeFragmentV4.java
@@ -12,7 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
 
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.preferences.Pref;
@@ -27,7 +26,6 @@ import org.chromium.components.browser_ui.settings.ChromeBasePreference;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.ClickableSpansTextMessagePreference;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
-import org.chromium.components.browser_ui.settings.TextMessagePreference;
 import org.chromium.components.favicon.LargeIconBridge;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.user_prefs.UserPrefs;
@@ -52,12 +50,13 @@ public class FledgeFragmentV4 extends PrivacySandboxSettingsBaseFragment
     private static final String FOOTER_PREFERENCE = "fledge_page_footer";
 
     private ChromeSwitchPreference mFledgeTogglePreference;
-    private PreferenceCategory mCurrentSitesCategory;
-    private TextMessagePreference mEmptyFledgePreference;
-    private TextMessagePreference mDisabledFledgePreference;
+    private PreferenceCategoryWithClickableSummary mCurrentSitesCategory;
+    private PreferenceCategoryWithClickableSummary mEmptyFledgePreference;
+    private PreferenceCategoryWithClickableSummary mDisabledFledgePreference;
     private ChromeBasePreference mAllSitesPreference;
     private LargeIconBridge mLargeIconBridge;
     private ClickableSpansTextMessagePreference mFooterPreference;
+    private boolean mMoreThanMaxSitesToDisplay;
 
     static boolean isFledgePrefEnabled() {
         PrefService prefService = UserPrefs.get(Profile.getLastUsedRegularProfile());
@@ -90,6 +89,7 @@ public class FledgeFragmentV4 extends PrivacySandboxSettingsBaseFragment
         mFledgeTogglePreference.setChecked(isFledgePrefEnabled());
         mFledgeTogglePreference.setOnPreferenceChangeListener(this);
         mFledgeTogglePreference.setManagedPreferenceDelegate(createManagedPreferenceDelegate());
+        mMoreThanMaxSitesToDisplay = false;
 
         mCurrentSitesCategory.setSummary(SpanApplier.applySpans(
                 getResources().getString(R.string.settings_fledge_page_current_sites_description),
@@ -178,6 +178,8 @@ public class FledgeFragmentV4 extends PrivacySandboxSettingsBaseFragment
         }
 
         mCurrentSitesCategory.removeAll();
+        mMoreThanMaxSitesToDisplay = currentSites.size() > MAX_DISPLAYED_SITES;
+
         int nrDisplayedSites = Math.min(currentSites.size(), MAX_DISPLAYED_SITES);
         for (int i = 0; i < nrDisplayedSites; i++) {
             String site = currentSites.get(i);
@@ -190,6 +192,8 @@ public class FledgeFragmentV4 extends PrivacySandboxSettingsBaseFragment
             preference.setOnPreferenceClickListener(this);
             mCurrentSitesCategory.addPreference(preference);
         }
+
+        updatePreferenceVisibility();
     }
 
     private void updatePreferenceVisibility() {
@@ -204,7 +208,9 @@ public class FledgeFragmentV4 extends PrivacySandboxSettingsBaseFragment
 
         // Visible when Fledge is enabled and the current sites list is not empty.
         mCurrentSitesCategory.setVisible(fledgeEnabled && !sitesEmpty);
-        mAllSitesPreference.setVisible(fledgeEnabled && !sitesEmpty);
+
+        // Visible when Fledge is enabled and has more than MAX_DISPLAYED_SITES allowed sites.
+        mAllSitesPreference.setVisible(fledgeEnabled && mMoreThanMaxSitesToDisplay);
     }
 
     private ChromeManagedPreferenceDelegate createManagedPreferenceDelegate() {
