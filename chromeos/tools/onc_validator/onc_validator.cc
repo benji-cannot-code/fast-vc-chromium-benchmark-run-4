c04f8255395a27847ca91080a1ab89d6ee66c4d5
Andrew Williams <awillia@chromium.org>
2023-01-09 18:38:30 +0000

    [CodeHealth] Replace DictionaryValue usage in chromeos/tools/onc_validator
    Bug: 1187061
    Change-Id: Ic20158518ee6161e512778154e459ca6eca74988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143883
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090417}

--- chromeos/tools/onc_validator/onc_validator.cc
+++ chromeos/tools/onc_validator/onc_validator.cc
@@ -89,8 +89,7 @@ void PrintHelp() {
           kStatusArgumentError);
 }
 
-std::unique_ptr<base::DictionaryValue> ReadDictionary(
-    const std::string& filename) {
+base::Value::Dict ReadDictionary(const std::string& filename) {
   base::FilePath path(filename);
   JSONFileValueDeserializer deserializer(path,
                                          base::JSON_ALLOW_TRAILING_COMMAS);
@@ -104,15 +103,13 @@ std::unique_ptr<base::DictionaryValue> ReadDictionary(
     return nullptr;
   }
 
-  std::unique_ptr<base::DictionaryValue> dict =
-      base::DictionaryValue::From(std::move(value));
-  if (!dict) {
+  if (!value->is_dict()) {
     LOG(ERROR) << "File '" << filename
                << "' does not contain a dictionary as expected, but type "
-               << value->GetType();
+               << base::Value::GetTypeName(value->type());
   }
 
-  return dict;
+  return std::move(*value).TakeDict();
 }
 
 int main(int argc, const char* argv[]) {
@@ -126,7 +123,7 @@ int main(int argc, const char* argv[]) {
     return kStatusArgumentError;
   }
 
-  std::unique_ptr<base::DictionaryValue> onc_object = ReadDictionary(args[1]);
+  base::Value::Dict onc_object = ReadDictionary(args[1]);
 
   if (!onc_object)
     return kStatusJsonError;
@@ -158,7 +155,8 @@ int main(int argc, const char* argv[]) {
   }
 
   chromeos::onc::Validator::Result result;
-  validator.ValidateAndRepairObject(signature, *onc_object, &result);
+  validator.ValidateAndRepairObject(
+      signature, Base::Value(std::move(onc_object)), &result);
 
   switch (result) {
     case chromeos::onc::Validator::VALID:
