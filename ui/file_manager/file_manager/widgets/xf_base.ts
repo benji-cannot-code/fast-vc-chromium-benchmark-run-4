2150ac38444824842d08d27926f89ac65893ab20
Zain Afzal <zafzal@google.com>
2023-01-09 04:19:47 +0000

    Add ash_ts_library.
    See design doc (go/cros-jellybean-build-tooling) for more information.
    
    This CL adds the first iteration of ash_ts_library which acts as a thin
    wrapper over ts_library allowing for the use of a slightly more
    developer friendly import syntax.
    
    As a proof of concept this CL opts color_internals and the files app
    into the new tooling. This CL in itself does not functionally effect
    any code and acts as a refactor for both of these apps.
    
    NOTE: ash_ts_library:
      * Does not produce code that optimize_web_ui can inject yet.
      * Does not allow for a neat import for lit directives.
    
    A follow up will add these remaining bits of functionality.
    
    BUG: b/264069966
    Change-Id: Ia50e502b52edc983791a85a817163216f327c72d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129218
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090200}

--- ui/file_manager/file_manager/widgets/xf_base.ts
+++ ui/file_manager/file_manager/widgets/xf_base.ts
@@ -8,14 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @suppress {checkTypes} closure can't recognize LitElement
  */
 
-import '../common/js/tslib_shim.js';
-
-import {customElement, property, query, state} from 'chrome://resources/mwc/lit/decorators.js';
 import {classMap} from 'chrome://resources/mwc/lit/directives/class-map.js';
 import {ifDefined} from 'chrome://resources/mwc/lit/directives/if-defined.js';
 import {repeat} from 'chrome://resources/mwc/lit/directives/repeat.js';
 import {styleMap} from 'chrome://resources/mwc/lit/directives/style-map.js';
-import {css, CSSResult, CSSResultGroup, html, LitElement, PropertyValues} from 'chrome://resources/mwc/lit/index.js';
+import {css, CSSResult, CSSResultGroup, html, LitElement, PropertyValues} from 'lit';
+import {customElement, property, query, state} from 'lit/decorators';
 
 export {
   classMap,
