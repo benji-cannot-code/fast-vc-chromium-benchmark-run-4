6484548286ac2805e463ad3fbc3b02b0d341fa7b
Josh Hilke <jrhilke@google.com>
2023-01-09 18:50:57 +0000

    Add event-driven USB telemetry.
    Report USB telemetry with a short delay when USB events are triggered. The delay mitigates a race condition in which the croshealthd telemetry API queries the firmware updater tool (fwupd) for USB firmware version info before fwupd has a chance to register all of the USB peripherals plugged into the device.
    
    Change-Id: I38bd4dbf90d163edac1f0c8b6afbf6a4c6914157
    Bug: b:238796802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074864
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1090426}

--- chrome/browser/chromeos/reporting/metric_reporting_manager_delegate_base.cc
+++ chrome/browser/chromeos/reporting/metric_reporting_manager_delegate_base.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reporting/client/report_queue_factory.h"
 #include "components/reporting/metrics/collector_base.h"
 #include "components/reporting/metrics/event_driven_telemetry_collector_pool.h"
+#include "components/reporting/metrics/manual_collector.h"
 #include "components/reporting/metrics/metric_event_observer.h"
 #include "components/reporting/metrics/metric_report_queue.h"
 #include "components/reporting/metrics/one_shot_collector.h"
@@ -93,6 +94,18 @@ MetricReportingManagerDelegateBase::CreateOneShotCollector(
   return std::make_unique<OneShotCollector>(
       sampler, metric_report_queue, reporting_settings, enable_setting_path,
       setting_enabled_default_value, init_delay);
+}
+
+std::unique_ptr<CollectorBase>
+MetricReportingManagerDelegateBase::CreateManualCollector(
+    Sampler* sampler,
+    MetricReportQueue* metric_report_queue,
+    ReportingSettings* reporting_settings,
+    const std::string& enable_setting_path,
+    bool setting_enabled_default_value) {
+  return std::make_unique<ManualCollector>(
+      sampler, metric_report_queue, reporting_settings, enable_setting_path,
+      setting_enabled_default_value);
 }
 
 std::unique_ptr<MetricEventObserverManager>
