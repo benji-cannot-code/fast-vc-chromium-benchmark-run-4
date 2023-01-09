25c20cb0e72965930361baa122040245718ada1a
Duong Dac <ddac@chromium.org>
2023-01-09 20:57:22 +0000

    [sync] Hide email for applicable accounts
    This change concerns accounts that are not capable to have the email
    address displayed.
    
    For fields that typically display the email address,
    - Replace with the user's display name (First Last) if available.
    - Otherwise, use fallback string.
    
    Feature flag:
    crrev.com/c/4047627
    
    In action:
    https://docs.google.com/document/d/1_W3he3EidbBUUmkXqDfBp1HyWwynx3N0jclAyeE5Ies
    
    This CL was uploaded by git cl split.
    
    R=treib@chromium.org
    
    Bug: 1284725, b/250624181, b/240319819
    Change-Id: I206c88c0a6d6e11b5bfe67fcb23fd6503f04e5a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055858
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090503}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SignInPreference.java
@@ -52,6 +52,10 @@ public class SignInPreference
     private final ProfileDataCache mProfileDataCache;
     private final AccountManagerFacade mAccountManagerFacade;
 
+    public ProfileDataCache getProfileDataCache() {
+        return mProfileDataCache;
+    }
+
     /**
      * Constructor for inflating from XML.
      */
@@ -182,9 +186,12 @@ public class SignInPreference
 
     private void setupSignedIn(String accountName) {
         DisplayableProfileData profileData = mProfileDataCache.getProfileDataOrDefault(accountName);
-
-        setTitle(profileData.getFullNameOrEmail());
-        setSummary(accountName);
+        final boolean canShowEmailAddress = profileData.hasDisplayableEmailAddress()
+                || !ChromeFeatureList.isEnabled(
+                        ChromeFeatureList.HIDE_NON_DISPLAYABLE_ACCOUNT_EMAIL);
+        setSummary(canShowEmailAddress ? accountName : "");
+        setTitle(SyncSettingsUtils.getDisplayableFullNameOrEmailWithPreference(
+                profileData, getContext(), SyncSettingsUtils.TitlePreference.FULL_NAME));
         setFragment(AccountManagementFragment.class.getName());
         setIcon(profileData.getImage());
         setViewEnabled(true);
