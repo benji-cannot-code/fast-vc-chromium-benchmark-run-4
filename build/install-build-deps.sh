463e579bc5cfa81a3c82892f2084fab1951c2d13
Sebastian Amend <sebastian.amend@googlemail.com>
2023-01-09 21:09:32 +0000

    Install 32-bit libraries only if requested
    The script attempts to install certain 32-bit libraries regardless of
    the command line flags given, which makes it fail on systems that don't
    have i386 packages.
    
    In earlier versions, these libraries were only added to `lib_list` if
    they were found on the system. Now the script attempts to install them
    regardless, causing failures on stock Ubuntu installations that don't
    have packages for the i386 architecture. Move them to `lib32_list`,
    where they only get installed if a command line flag requests 32-bit
    libraries.
    
    R=thomasanderson@chromium.org
    
    Change-Id: I37c8fcac50a41d78426c6c2443393a3b3700f7f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4146419
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090513}

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -248,7 +248,6 @@ common_lib_list="\
   libcups2
   libdrm2
   libegl1
-  libegl1:i386
   libevdev2
   libexpat1
   libfontconfig1
@@ -256,12 +255,10 @@ common_lib_list="\
   libgbm1
   libglib2.0-0
   libgl1
-  libgl1:i386
   libgtk-3-0
   libpam0g
   libpango-1.0-0
   libpangocairo-1.0-0
-  libpangocairo-1.0-0:i386
   libpci3
   libpcre3
   libpixman-1-0
@@ -270,7 +267,6 @@ common_lib_list="\
   libsqlite3-0
   libuuid1
   libwayland-egl1
-  libwayland-egl1:i386
   libwayland-egl1-mesa
   libx11-6
   libx11-xcb1
@@ -318,11 +314,15 @@ lib32_list="$lib32_list
   libatk1.0-0:i386
   libatspi2.0-0:i386
   libdbus-1-3:i386
+  libegl1:i386
+  libgl1:i386
   libglib2.0-0:i386
   libncurses5:i386
   libnss3:i386
   libpango-1.0-0:i386
+  libpangocairo-1.0-0:i386
   libstdc++6:i386
+  libwayland-egl1:i386
   libx11-xcb1:i386
   libxcomposite1:i386
   libxdamage1:i386
