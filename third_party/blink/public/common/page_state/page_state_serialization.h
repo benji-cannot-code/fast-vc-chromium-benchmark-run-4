aef0f20b4e136855b3f50006157e3ee4df759e13
W. James MacLean <wjmaclean@chromium.org>
2023-01-10 03:44:09 +0000

    Fix ExplodedFrameState::assign for initiator_origin.
    When `initiator_origin` was originally added to `ExplodedFrameState`, it
    was mistakenly omitted from ExplodedFrameState::assign(). This CL
    adds it so that, going forward, operator= and the copy constructor for
    ExplodedFrameState will behave as one might expect.
    
    The original baseline data in serialized_v28.dat (and later), while
    intending to test decoding of two ExplodedFrameState objects both with
    non-empty initiator_origin values, actually wrote one with (the parent)
    and one without (the child). The child ExplodedFrameState object lost
    its `initiator_origin` value at [1] since the push_back() operation
    uses assign(), and at the time copying `initiator_origin` was missing.
    In this CL, to avoid trying to recreate the old baseline data files with
    the corrected behavior, we instead create a new version number and
    associated baseline data. The test function that populates the
    ExplodedFrameState data is modified to provide the expected baseline
    data for versions before the one introduced here.
    
    An additional test verifies the correct operation of both operator=
    and the copy constructor for ExplodedFrameState.
    
    This didn't result in actually missing any subframe initiator origins
    when persisting state to disk, because we separately assigned that
    value at [2]. So we haven't discovered any user-facing issues due to
    this bug, at least so far. But future code changes could invoke
    assign() in a way that would potentially be problematic, so fixing
    this is necessary.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/common/page_state/page_state_serialization_unittest.cc;drc=b0e54334b250494abc774fa1db5fa3ef257c0aed;l=244
    
    [2] https://source.chromium.org/chromium/chromium/src/+/main:content/browser/renderer_host/navigation_entry_impl.cc;drc=b0e54334b250494abc774fa1db5fa3ef257c0aed;l=172
    
    
    Bug: 1405812
    Change-Id: I84cbd79073ea2b79442d520b631baeb551764d64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112051
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090683}

--- third_party/blink/public/common/page_state/page_state_serialization.h
+++ third_party/blink/public/common/page_state/page_state_serialization.h
@@ -35,6 +35,7 @@ struct BLINK_COMMON_EXPORT ExplodedHttpBody {
 };
 
 struct BLINK_COMMON_EXPORT ExplodedFrameState {
+  // When adding a new member, also add it to ExplodedFrameState::assign.
   absl::optional<std::u16string> url_string;
   absl::optional<std::u16string> referrer;
   absl::optional<url::Origin> initiator_origin;
