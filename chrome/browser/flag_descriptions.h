0d160c00b4ae942fc71b5e3b9015425b1a51e315
Nick Birnie <birnie@google.com>
2023-01-10 22:36:17 +0000

    Add base::Feature flag for Bottom of Feed Sync Banner and Chrome://flags ui control for manual opt-in.
    Bug: b/250924526
    Change-Id: I13312cdd16c3f1bfaff3bf85a062be71c6c30168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148713
    Commit-Queue: Nick Birnie <birnie@google.com>
    Reviewed-by: Jonathan Freed <freedjm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091040}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2056,6 +2056,9 @@ extern const char
 extern const char kFeedBackToTopName[];
 extern const char kFeedBackToTopDescription[];
 
+extern const char kFeedBottomSyncBannerName[];
+extern const char kFeedBottomSyncBannerDescription[];
+
 extern const char kFeedHeaderStickToTopName[];
 extern const char kFeedHeaderStickToTopDescription[];
 
