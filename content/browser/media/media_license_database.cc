bfde7817c49bc8d41b2629dfd8039408a8f9196f
Vikram Pasupathy <vpasupathy@chromium.org>
2023-01-10 20:34:29 +0000

    media: Add Histogram to log Errors for MediaLicenseDatabase Open
    This CL adds a histogram to report errors to get more details about
    why the MediaLicenseDatabase fails to open on certain occasions.
    
    Bug: b/253105991
    Change-Id: I0c933a0e8bdb230167f250308b09f7202bcf63cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133724
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090983}

--- content/browser/media/media_license_database.cc
+++ content/browser/media/media_license_database.cc
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+using MediaLicenseStorageHostOpenError =
+    MediaLicenseStorageHost::MediaLicenseStorageHostOpenError;
+
 namespace {
 
 static const int kVersionNumber = 1;
@@ -30,17 +33,15 @@ MediaLicenseDatabase::MediaLicenseDatabase(const base::FilePath& path)
                                .page_size = 32768,
                                .cache_size = 8}) {}
 
-bool MediaLicenseDatabase::OpenFile(const media::CdmType& cdm_type,
-                                    const std::string& file_name) {
+MediaLicenseStorageHostOpenError MediaLicenseDatabase::OpenFile(
+    const media::CdmType& cdm_type,
+    const std::string& file_name) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  if (!OpenDatabase())
-    return false;
 
   // The media license code doesn't distinguish between an empty file and a
   // file which does not exist, so don't bother inserting an empty row into
   // the database.
-  return true;
+  return OpenDatabase();
 }
 
 absl::optional<std::vector<uint8_t>> MediaLicenseDatabase::ReadFile(
@@ -48,8 +49,9 @@ absl::optional<std::vector<uint8_t>> MediaLicenseDatabase::ReadFile(
     const std::string& file_name) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (!OpenDatabase())
+  if (OpenDatabase() != MediaLicenseStorageHostOpenError::kOk) {
     return absl::nullopt;
+  }
 
   static constexpr char kSelectSql[] =
       "SELECT data FROM licenses WHERE cdm_type=? AND file_name=?";
@@ -81,8 +83,9 @@ bool MediaLicenseDatabase::WriteFile(const media::CdmType& cdm_type,
                                      const std::vector<uint8_t>& data) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (!OpenDatabase())
+  if (OpenDatabase() != MediaLicenseStorageHostOpenError::kOk) {
     return false;
+  }
 
   static constexpr char kInsertSql[] =
       // clang-format off
@@ -107,8 +110,9 @@ bool MediaLicenseDatabase::DeleteFile(const media::CdmType& cdm_type,
                                       const std::string& file_name) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (!OpenDatabase())
+  if (OpenDatabase() != MediaLicenseStorageHostOpenError::kOk) {
     return false;
+  }
 
   static constexpr char kDeleteSql[] =
       "DELETE FROM licenses WHERE cdm_type=? AND file_name=?";
@@ -140,11 +144,12 @@ bool MediaLicenseDatabase::ClearDatabase() {
 }
 
 // Opens and sets up a database if one is not already set up.
-bool MediaLicenseDatabase::OpenDatabase(bool is_retry) {
+MediaLicenseStorageHostOpenError MediaLicenseDatabase::OpenDatabase(
+    bool is_retry) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (db_.is_open())
-    return true;
+    return MediaLicenseStorageHostOpenError::kOk;
 
   bool success = false;
   if (path_.empty()) {
@@ -155,7 +160,7 @@ bool MediaLicenseDatabase::OpenDatabase(bool is_retry) {
     if (!base::CreateDirectoryAndGetError(path_.DirName(), &error)) {
       DVLOG(1) << "Failed to open CDM database: "
                << base::File::ErrorToString(error);
-      return false;
+      return MediaLicenseStorageHostOpenError::kBucketNotFound;
     }
     DCHECK_EQ(error, base::File::Error::FILE_OK);
 
@@ -164,7 +169,7 @@ bool MediaLicenseDatabase::OpenDatabase(bool is_retry) {
 
   if (!success) {
     DVLOG(1) << "Failed to open CDM database: " << db_.GetErrorMessage();
-    return false;
+    return MediaLicenseStorageHostOpenError::kDatabaseOpenError;
   }
 
   sql::MetaTable meta_table;
@@ -173,7 +178,8 @@ bool MediaLicenseDatabase::OpenDatabase(bool is_retry) {
     // Wipe the database and start over. If we've already wiped the database and
     // are still failing, just return false.
     db_.Raze();
-    return is_retry ? false : OpenDatabase(/*is_retry=*/true);
+    return is_retry ? MediaLicenseStorageHostOpenError::kDatabaseRazeError
+                    : OpenDatabase(/*is_retry=*/true);
   }
 
   if (meta_table.GetCompatibleVersionNumber() > kVersionNumber) {
@@ -184,7 +190,8 @@ bool MediaLicenseDatabase::OpenDatabase(bool is_retry) {
              << kVersionNumber << ", GetCompatibleVersionNumber="
              << meta_table.GetCompatibleVersionNumber();
     db_.Raze();
-    return is_retry ? false : OpenDatabase(/*is_retry=*/true);
+    return is_retry ? MediaLicenseStorageHostOpenError::kDatabaseRazeError
+                    : OpenDatabase(/*is_retry=*/true);
   }
 
   // Set up the table.
@@ -200,10 +207,10 @@ bool MediaLicenseDatabase::OpenDatabase(bool is_retry) {
 
   if (!db_.Execute(kCreateTableSql)) {
     DVLOG(1) << "Failed to execute " << kCreateTableSql;
-    return false;
+    return MediaLicenseStorageHostOpenError::kSQLExecutionError;
   }
 
-  return true;
+  return MediaLicenseStorageHostOpenError::kOk;
 }
 
 }  // namespace content
--- content/browser/media/media_license_database.h
+++ content/browser/media/media_license_database.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_MEDIA_MEDIA_LICENSE_DATABASE_H_
 
 #include "base/sequence_checker.h"
+#include "content/browser/media/media_license_storage_host.h"
 #include "media/cdm/cdm_type.h"
 #include "sql/database.h"
 
@@ -21,7 +22,9 @@ class MediaLicenseDatabase {
   // The database will be in-memory if `path` is empty.
   explicit MediaLicenseDatabase(const base::FilePath& path);
 
-  bool OpenFile(const media::CdmType& cdm_type, const std::string& file_name);
+  MediaLicenseStorageHost::MediaLicenseStorageHostOpenError OpenFile(
+      const media::CdmType& cdm_type,
+      const std::string& file_name);
   absl::optional<std::vector<uint8_t>> ReadFile(const media::CdmType& cdm_type,
                                                 const std::string& file_name);
   bool WriteFile(const media::CdmType& cdm_type,
@@ -33,7 +36,8 @@ class MediaLicenseDatabase {
 
  private:
   // Opens and sets up a database if one is not already set up.
-  bool OpenDatabase(bool is_retry = false);
+  MediaLicenseStorageHost::MediaLicenseStorageHostOpenError OpenDatabase(
+      bool is_retry = false);
 
   SEQUENCE_CHECKER(sequence_checker_);
 
