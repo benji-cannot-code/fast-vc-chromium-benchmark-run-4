17e55f1fed735ee004773154652ffdc19635ecd6
Nancy Wang <nancylingwang@chromium.org>
2023-01-10 05:16:32 +0000

    Remove the mojom IconKey, IconType.
    Modify callers to use the non mojom IconKey, IconType.
    
    BUG=1253250
    
    Change-Id: I53a8bca9afe7877ec6886d6ed713afe7fe9e0318
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4146622
    Reviewed-by: David Munro <davidmunro@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090707}

--- chrome/browser/ash/guest_os/guest_os_registry_service.h
+++ chrome/browser/ash/guest_os/guest_os_registry_service.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/vm_applications/apps.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/services/app_service/public/cpp/icon_types.h"
-#include "components/services/app_service/public/mojom/app_service.mojom.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/resource/resource_scale_factor.h"
 
