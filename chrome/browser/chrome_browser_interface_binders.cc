ef196681948cd4b3316a4feb34c98c95ab49dadc
Georg Neis <neis@chromium.org>
2023-01-10 01:17:44 +0000

    lacros: Remove inactive WebUIs
    Some WebUIs (e.g. CrashesUI) are part of Lacros but are inactive because
    when asked to navigate to the corresponding URL (e.g. chrome://crashes),
    Lacros will actually forward the request to Ash and thus invoke the Ash
    instance of that WebUI. Remove the following such WebUIs from Lacros entirely:
    
    chrome://app-disabled
    chrome://bluetooth-internals
    chrome://crashes
    
    Also remove an unused constant (chrome::kChromeUIFirmwareUpdatesAppURL)
    and fix a typo (chrome::kChromeUIInternetConfigDialogURL).
    
    Change-Id: Ie4985686705852967e4b6adda2b51144ca946f97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4122017
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090635}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/web_applications/draggable_region_host_impl.h"
 #include "chrome/browser/ui/web_applications/sub_apps_service_impl.h"
-#include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals.mojom.h"
 #include "chrome/browser/ui/webui/browsing_topics/browsing_topics_internals_ui.h"
 #include "chrome/browser/ui/webui/engagement/site_engagement_ui.h"
 #include "chrome/browser/ui/webui/internals/internals_ui.h"
@@ -318,6 +317,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/digital_goods/digital_goods_lacros.h"
 #include "chromeos/lacros/lacros_service.h"
 #else
+#include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals.mojom.h"  // nogncheck
 #include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_ui.h"  // nogncheck
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
--- chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_ui.cc
+++ chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_ui.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_handler.h"
-#include "chrome/browser/ui/webui/webui_util.h"
+#include "chrome/browser/ui/webui/webui_util.h"  // nogncheck
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/bluetooth_internals_resources.h"
 #include "chrome/grit/bluetooth_internals_resources_map.h"
