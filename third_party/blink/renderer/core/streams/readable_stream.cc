c1fb9e45a7be197e0d343c9fba58ca6dbcc32ea3
Kenichi Ishibashi <bashi@chromium.org>
2023-01-10 01:07:50 +0000

    [streams] Check whether ScriptState is valid in StreamAlgorithm
    Otherwise we hit DCHECK in ScroptState::Scope(). If the script state
    is invalid we reject a promise. This is a workaround similar to
    https://crrev.com/c/4080291
    
    Manually tested the regression test
    (datagram-cancel-crash.https.window.html) passed. The test will
    be enabled once we are ready to run WebTransport WPTs.
    
    Bug: 1250221
    Change-Id: I11ebaa8bb1144a9b22314315c518a6b15c3dd437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4141619
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090630}

--- third_party/blink/renderer/core/streams/readable_stream.cc
+++ third_party/blink/renderer/core/streams/readable_stream.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding.h"
+#include "third_party/blink/renderer/platform/bindings/v8_throw_exception.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
@@ -64,11 +65,15 @@ class ReadableStream::PullAlgorithm final : public StreamAlgorithm {
     ExceptionState exception_state(script_state->GetIsolate(),
                                    ExceptionState::kUnknownContext, "", "");
     ScriptPromise promise;
-    {
+    if (script_state->ContextIsValid()) {
       // This is needed because the realm of the underlying source can be
       // different from the realm of the readable stream.
       ScriptState::Scope scope(underlying_byte_source_->GetScriptState());
       promise = underlying_byte_source_->Pull(controller_, exception_state);
+    } else {
+      return PromiseReject(script_state,
+                           V8ThrowException::CreateTypeError(
+                               script_state->GetIsolate(), "invalid realm"));
     }
     if (exception_state.HadException()) {
       auto exception = exception_state.GetException();
@@ -108,11 +113,15 @@ class ReadableStream::CancelAlgorithm final : public StreamAlgorithm {
     ExceptionState exception_state(script_state->GetIsolate(),
                                    ExceptionState::kUnknownContext, "", "");
     ScriptPromise promise;
-    {
+    if (script_state->ContextIsValid()) {
       // This is needed because the realm of the underlying source can be
       // different from the realm of the readable stream.
       ScriptState::Scope scope(underlying_byte_source_->GetScriptState());
       promise = underlying_byte_source_->Cancel(argv[0], exception_state);
+    } else {
+      return PromiseReject(script_state,
+                           V8ThrowException::CreateTypeError(
+                               script_state->GetIsolate(), "invalid realm"));
     }
     if (exception_state.HadException()) {
       auto exception = exception_state.GetException();
