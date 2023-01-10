181f288be14593da44e85d62eb4cad6d51e7b799
Bo Liu <boliu@chromium.org>
2023-01-10 22:07:40 +0000

    Enable AvoidRasterDuringElasticOverscroll
    Bug: 1335220
    Change-Id: I8ff217b9af4f01c09904acac02510940d22d9ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143763
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091033}

--- cc/layers/picture_layer_impl_unittest.cc
+++ cc/layers/picture_layer_impl_unittest.cc
@@ -2804,7 +2804,7 @@ TEST_F(LegacySWPictureLayerImplTest, HighResTilingDuringAnimation) {
   // at a source scale that the rasterized layer will not be larger than the
   // viewport.
   contents_scale = 0.1f;
-  maximum_animation_scale = 11.f;
+  maximum_animation_scale = 12.f;
 
   SetContentsAndAnimationScalesOnBothLayers(contents_scale, device_scale,
                                             page_scale, maximum_animation_scale,
@@ -2899,7 +2899,7 @@ TEST_F(LegacySWPictureLayerImplTest, HighResTilingDuringAnimationWideLayer) {
   // at a source scale that the rasterized visible rect will not be larger than
   // the viewport.
   contents_scale = 0.1f;
-  maximum_animation_scale = 11.f;
+  maximum_animation_scale = 12.f;
 
   SetContentsAndAnimationScalesOnBothLayers(contents_scale, device_scale,
                                             page_scale, maximum_animation_scale,
