b14e55d0b26adc1651db36a4e3cbeb4ac607511f
Vikas Soni <vikassoni@chromium.org>
2023-01-10 20:15:36 +0000

    Use vmaGetBudget() to calculate used and allocated memory.
    vmaCalculateStats() is costly and should not be used every frame.
    Hence update code to use vmaGetBudget() to query total allocated and
    used vma memory.
    
    Bug: 1400802, 1393913
    Change-Id: Ia8b25d0c0ef606343b616abd1604681f2cf08da4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112618
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090962}

--- gpu/vulkan/vma_wrapper.h
+++ gpu/vulkan/vma_wrapper.h
@@ -9,13 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vulkan/vulkan_core.h>
 
 #include "base/component_export.h"
+#include "ui/gfx/extension_set.h"
 
 VK_DEFINE_HANDLE(VmaAllocator)
 VK_DEFINE_HANDLE(VmaAllocation)
 
 struct VmaAllocationCreateInfo;
 struct VmaAllocationInfo;
-struct VmaStats;
+struct VmaBudget;
 
 namespace gpu {
 namespace vma {
@@ -24,6 +25,7 @@ COMPONENT_EXPORT(VULKAN)
 VkResult CreateAllocator(VkPhysicalDevice physical_device,
                          VkDevice device,
                          VkInstance instance,
+                         const gfx::ExtensionSet& enabled_extensions,
                          const VkDeviceSize* heap_size_limit,
                          const bool is_thread_safe,
                          VmaAllocator* allocator);
@@ -96,7 +98,7 @@ void GetPhysicalDeviceProperties(
     const VkPhysicalDeviceProperties** physical_device_properties);
 
 COMPONENT_EXPORT(VULKAN)
-void CalculateStats(VmaAllocator allocator, VmaStats* stats);
+void GetBudget(VmaAllocator allocator, VmaBudget* budget);
 
 COMPONENT_EXPORT(VULKAN)
 uint64_t GetTotalAllocatedMemory(VmaAllocator allocator);
