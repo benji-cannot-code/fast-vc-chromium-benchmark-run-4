cdb07ca47013b6dd18eb1b0c7f1d98b2d9fb26d4
Dan Harrington <harringtond@chromium.org>
2023-01-10 19:10:43 +0000

    Updates for integration tests
    Allow feed hosts to be overridden with command line
    switches.
    
    Bug: b/200602220
    Change-Id: I6145858823b01fa82c7795c77a8f8f3b8404c4d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149859
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090932}

--- components/feed/core/v2/feed_network_impl.cc
+++ components/feed/core/v2/feed_network_impl.cc
@@ -51,9 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/protobuf/src/google/protobuf/io/coded_stream.h"
 #include "third_party/zlib/google/compression_utils.h"
 
-// Token override for Feedv2NewTabPageCardInstrumentationTest.java:
-// #define TOKEN_OVERRIDE_FOR_TESTING  "put-test-token-here"
-
 namespace feed {
 namespace {
 constexpr char kApplicationXProtobuf[] = "application/x-protobuf";
@@ -380,9 +377,12 @@ class FeedNetworkImpl::NetworkFetch {
     variations::SignedIn signed_in_status = variations::SignedIn::kNo;
     if (!access_token_.empty()) {
       base::StringPiece token = access_token_;
-#ifdef TOKEN_OVERRIDE_FOR_TESTING
-      token = TOKEN_OVERRIDE_FOR_TESTING;
-#endif
+      std::string token_override =
+          base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+              "feed-token-override");
+      if (!token_override.empty()) {
+        token = token_override;
+      }
       request.headers.SetHeader(net::HttpRequestHeaders::kAuthorization,
                                 base::StrCat({"Bearer ", token}));
       signed_in_status = variations::SignedIn::kYes;
@@ -605,7 +605,12 @@ void FeedNetworkImpl::SendDiscoverApiRequest(
   GURL url(base::StrCat({kDiscoverHost, request_path}));
   // Override url if requested.
   std::string host_override =
-      pref_service_->GetString(feed::prefs::kDiscoverAPIEndpointOverride);
+      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+          "feedv2-discover-host-override");
+  if (host_override.empty()) {
+    host_override =
+        pref_service_->GetString(feed::prefs::kDiscoverAPIEndpointOverride);
+  }
   if (!host_override.empty()) {
     GURL override_url(host_override);
     if (override_url.is_valid()) {
