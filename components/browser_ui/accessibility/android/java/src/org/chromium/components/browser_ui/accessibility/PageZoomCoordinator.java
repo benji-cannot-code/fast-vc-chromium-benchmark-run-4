a85ad73c5a0766cef2cf2e144920f1ba4813ca0d
Amanda Lin Dietz <aldietz@google.com>
2023-01-10 23:17:30 +0000

    Reland "[PageZoom] Add UKM for new zoom value on slider dismissal"
    This is a reland of commit 3d3d5d0cdb3722eabec11ef2a6c162e91966581a
    
    This reland has been updated to include bucketing in intervals of 5
    for the zoom value and a unit test.
    
    See go/clank-page-zoom-design for the feature design document and
    UKM request, as well as approval from chrome-privacy-core.
    
    Original change's description:
    > [PageZoom] Add UKM for new zoom value on slider dismissal
    >
    > This CL adds logging for UKM at the same point when existing UMA
    > is logged: at dismissal of the Page Zoom slider if the user
    > changed the slider value before dismissal.
    >
    > See UKM Collection in the Privacy Considerations section at
    > go/clank-page-zoom-design for doc and UKM request.
    >
    > Bug: 1232536
    > Fixed: 1402526
    > Change-Id: I431d0a05efc1f841650340503f6a3c274eb75307
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4094081
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1085286}
    
    Bug: 1232536
    Change-Id: I55054d0ebbfd1085de7ed939fa528d892a352543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4118930
    Reviewed-by: Yue Ru Sun <yrsun@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Amanda Lin Dietz <aldietz@google.com>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091057}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomCoordinator.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomCoordinator.java
@@ -132,6 +132,7 @@ public class PageZoomCoordinator {
             // Ensure that the user has set a zoom value during this session.
             double zoomValue = mMediator.latestZoomValue();
             if (zoomValue != 0.0) {
+                mMediator.logZoomLevelUKM(zoomValue);
                 PageZoomUma.logAppMenuSliderZoomLevelChangedHistogram();
                 PageZoomUma.logAppMenuSliderZoomLevelValueHistogram(zoomValue);
             }
