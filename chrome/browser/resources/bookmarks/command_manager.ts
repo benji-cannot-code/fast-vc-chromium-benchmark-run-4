ec38aa5741218a6d503e1eba6266400f7bde503c
Cole Horvitz <colehorvitz@chromium.org>
2023-01-10 23:08:20 +0000

    chrome.bookmarkManagerPrivate: Migrate to Promise-based extension APIs
    This is a reland of https://crrev.com/c/4114862.
    
    This change fixes a test which previously reassigned an API method to
    omit an optional callback which is executed in production. Upon
    migration to Promises, this callback is now called via Promise-chain,
    independent of the method's arguments, and therefore cannot be
    reassigned and omitted in tests. This call caused an error in
    toolbar_test.ts. This is resolved by ensuring that toolbar_test
    properly attaches a cr-toast-manager.
    
    See diff
    https://chromium-review.googlesource.com/c/chromium/src/+/4143869/1..6
    
    Bug: 1368302, 1405441
    Change-Id: Id37529fa144bb5fedc9a29beca4ca6b68156e227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143869
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091051}

--- chrome/browser/resources/bookmarks/command_manager.ts
+++ chrome/browser/resources/bookmarks/command_manager.ts
@@ -324,7 +324,7 @@ export class BookmarksCommandManagerElement extends
       }
       case Command.COPY: {
         const idList = Array.from(itemIds);
-        chrome.bookmarkManagerPrivate.copy(idList, () => {
+        chrome.bookmarkManagerPrivate.copy(idList).then(() => {
           let labelPromise: Promise<string>;
           if (idList.length === 1) {
             labelPromise =
@@ -362,7 +362,7 @@ export class BookmarksCommandManagerElement extends
               'toastItemsDeleted', idList.length);
         }
 
-        chrome.bookmarkManagerPrivate.removeTrees(idList, () => {
+        chrome.bookmarkManagerPrivate.removeTrees(idList).then(() => {
           this.showTitleToast_(labelPromise, title, true);
         });
         break;
@@ -407,12 +407,12 @@ export class BookmarksCommandManagerElement extends
         const selectedFolder = state.selectedFolder;
         const selectedItems = state.selection.items;
         trackUpdatedItems();
-        chrome.bookmarkManagerPrivate.paste(
-            selectedFolder, Array.from(selectedItems), highlightUpdatedItems);
+        chrome.bookmarkManagerPrivate
+            .paste(selectedFolder, Array.from(selectedItems))
+            .then(highlightUpdatedItems);
         break;
       case Command.SORT:
         chrome.bookmarkManagerPrivate.sortChildren(state.selectedFolder);
-        getToastManager().querySelector('dom-if')!.if = true;
         getToastManager().show(loadTimeData.getString('toastFolderSorted'));
         break;
       case Command.ADD_BOOKMARK:
@@ -761,16 +761,12 @@ export class BookmarksCommandManagerElement extends
           result.collapsible = !!p.arg;
           return result;
         });
-    getToastManager().querySelector('dom-if')!.if = canUndo;
-    getToastManager().showForStringPieces(pieces);
+    getToastManager().showForStringPieces(pieces, /*hideSlotted*/ !canUndo);
   }
 
   private updateCanPaste_(targetId: string): Promise<void> {
-    return new Promise(resolve => {
-      chrome.bookmarkManagerPrivate.canPaste(`${targetId}`, result => {
-        this.canPaste_ = result;
-        resolve();
-      });
+    return chrome.bookmarkManagerPrivate.canPaste(targetId).then(result => {
+      this.canPaste_ = result;
     });
   }
 
--- chrome/test/data/webui/bookmarks/toolbar_test.ts
+++ chrome/test/data/webui/bookmarks/toolbar_test.ts
@@ -19,7 +19,9 @@ suite('<bookmarks-toolbar>', function() {
   let testCommandManager: TestCommandManager;
 
   suiteSetup(function() {
-    chrome.bookmarkManagerPrivate.removeTrees = function() {};
+    chrome.bookmarkManagerPrivate.removeTrees = function() {
+      return Promise.resolve();
+    };
   });
 
   setup(function() {
@@ -52,6 +54,9 @@ suite('<bookmarks-toolbar>', function() {
 
     testCommandManager = new TestCommandManager();
     document.body.appendChild(testCommandManager.getCommandManager());
+
+    const toastManager = document.createElement('cr-toast-manager');
+    document.body.appendChild(toastManager);
   });
 
   test('selecting multiple items shows toolbar overlay', function() {
