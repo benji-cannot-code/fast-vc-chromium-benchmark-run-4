ba46551f6526557bba8c796b314088784696bc03
Mitsuru Oshima <oshima@chromium.org>
2023-01-10 01:30:12 +0000

    Let ToplevelWindowEventHandler handle events before exo::Pointer
    Currently, exo::Pointer is added before ToplevelWindowEventHandler[1],
    which should be higher level than, and should be handled before exo::Pointer. This means that exo handles events before WM and may sends events when it should not (e.g. window dragging).
    
    This CL puts Pointer after ToplevelWindowEventHandler. This also fixes the removal of the ToplevelWindowEventHandler.
    
    Better way to handle this would be to add it to each exo window, but it will be major refactoring and the best way could be to handle event in the target's handler, instead of pre-target-handler, but it'd be a major refactoring which probably should happen after launch..
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:ash/shell.cc;drc=d308dbecb9c92f9eacf3b7262eb8e7cca9ce50c7;l=1621
    
    Bug: 1392980
    Test: covered by unit test.
    Change-Id: I178333154e8ac973bb633e4c3513068c51a967be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133773
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090640}

--- components/exo/pointer.h
+++ components/exo/pointer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/shell_observer.h"
 #include "base/memory/weak_ptr.h"
 #include "base/unguessable_token.h"
 #include "components/exo/surface_observer.h"
@@ -52,7 +53,8 @@ class Pointer : public SurfaceTreeHost,
                 public ui::EventHandler,
                 public aura::client::DragDropClientObserver,
                 public aura::client::CursorClientObserver,
-                public aura::client::FocusChangeObserver {
+                public aura::client::FocusChangeObserver,
+                public ash::ShellObserver {
  public:
   Pointer(PointerDelegate* delegate, Seat* seat);
 
@@ -77,13 +79,13 @@ class Pointer : public SurfaceTreeHost,
   // Set delegate for pinch events.
   void SetGesturePinchDelegate(PointerGesturePinchDelegate* delegate);
 
-  // Overridden from SurfaceDelegate:
+  // SurfaceDelegate:
   void OnSurfaceCommit() override;
 
-  // Overridden from SurfaceObserver:
+  // SurfaceObserver:
   void OnSurfaceDestroying(Surface* surface) override;
 
-  // Overridden from ui::EventHandler:
+  // ui::EventHandler:
   void OnMouseEvent(ui::MouseEvent* event) override;
   void OnScrollEvent(ui::ScrollEvent* event) override;
   void OnGestureEvent(ui::GestureEvent* event) override;
@@ -92,13 +94,17 @@ class Pointer : public SurfaceTreeHost,
   void OnDragStarted() override;
   void OnDragCompleted(const ui::DropTargetEvent& event) override;
 
-  // Overridden from aura::client::CursorClientObserver:
+  // aura::client::CursorClientObserver:
   void OnCursorSizeChanged(ui::CursorSize cursor_size) override;
   void OnCursorDisplayChanged(const display::Display& display) override;
 
-  // Overridden from aura::client::FocusChangeObserver;
+  // aura::client::FocusChangeObserver;
   void OnWindowFocused(aura::Window* gained_focus,
                        aura::Window* lost_focus) override;
+
+  // ash::ShellObserver:
+  void OnRootWindowAdded(aura::Window* root_window) override;
+  void OnRootWindowWillShutdown(aura::Window* root_window) override;
 
   // Relative motion registration.
   void RegisterRelativePointerDelegate(RelativePointerDelegate* delegate);
