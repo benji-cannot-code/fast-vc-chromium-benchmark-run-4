4d7cdf9498d3c44982afbde1fcd11c8594b592bb
K. Moon <kmoon@chromium.org>
2023-01-10 02:22:14 +0000

    Eliminate PDF plugin name constants
    Replaces non-display uses of the kPDFExtensionPluginName and
    kPDFInternalPluginName constants with the corresponding
    kPDFExtensionPluginPath and kPDFInternalPluginPath constants. The few
    remaining display-only uses have been inlined, allowing the constants to
    be removed entirely.
    
    Using the names insteads of the paths resulted in bugs like
    crbug.com/1404260, as the actual names did not always match the
    compile-time constants.
    
    Fixed: 1405531
    Change-Id: I27b28aa525778a9eb442f6bc8e3b33337086a0de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148058
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090652}

--- chrome/browser/pdf/pdf_extension_util.cc
+++ chrome/browser/pdf/pdf_extension_util.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/values.h"
+#include "build/branding_buildflags.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/common/chrome_content_client.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
@@ -163,13 +163,18 @@ void AddPdfViewerStrings(base::Value::Dict* dict) {
 }  // namespace
 
 std::string GetManifest() {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  static constexpr char kExtensionName[] = "Chrome PDF Viewer";
+#else
+  static constexpr char kExtensionName[] = "Chromium PDF Viewer";
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+
   std::string manifest_contents(
       ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
           IDR_PDF_MANIFEST));
   DCHECK(manifest_contents.find(kNameTag) != std::string::npos);
-  base::ReplaceFirstSubstringAfterOffset(
-      &manifest_contents, 0, kNameTag,
-      ChromeContentClient::kPDFExtensionPluginName);
+  base::ReplaceFirstSubstringAfterOffset(&manifest_contents, 0, kNameTag,
+                                         kExtensionName);
 
   return manifest_contents;
 }
