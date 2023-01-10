ec9b7deb7abd369494add3470cfed77c135231a7
Venkatesh Srinivas <venkateshs@chromium.org>
2023-01-10 06:33:30 +0000

    [device/bluetooth] Convert active_socket_count_ to std::atomic
    active_socket_count_ counts when BluetoothSocketThread is Activated/
    Deactivated and is used for DCHECKs. Convert from Atomic32 to
    std::atomic<int> - they are broadly supported, do not implicitly
    convert to/from integers, and we are slowly converting code to use
    them.
    
    No behavior change expected.
    
    Bug: 1194917
    Change-Id: Iaba77af59610fc2ac0b133bbee89c67f0aaa2cec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134240
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Venkatesh Srinivas <venkateshs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090714}

--- device/bluetooth/bluetooth_socket_thread.h
+++ device/bluetooth/bluetooth_socket_thread.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEVICE_BLUETOOTH_BLUETOOTH_SOCKET_THREAD_H_
 #define DEVICE_BLUETOOTH_BLUETOOTH_SOCKET_THREAD_H_
 
+#include <atomic>
 #include <memory>
 
 #include "base/atomicops.h"
@@ -46,7 +47,7 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothSocketThread
   void EnsureStarted();
 
   base::ThreadChecker thread_checker_;
-  base::subtle::Atomic32 active_socket_count_ = 0;
+  std::atomic<int> active_socket_count_{0};
   std::unique_ptr<base::Thread> thread_;
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
 };
