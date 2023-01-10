216a22ab78fc0c964fd201edf489378d91195a8e
Keren Zhu <kerenzhu@chromium.org>
2023-01-10 23:33:03 +0000

    ash: defer re-ordering in CaptureModeSession::ParentContainerObserver()
    CaptureModeSession::ParentContainerObserver() will re-order windows
    during window adding / removing. In general, reentrantly modifying a
    tree is not a good idea. In practice, this is causing crash in
    UI DevTools that tracks the window hierarchy, because at the time
    re-ordering happens the DevTools have not yet updated its tracked
    windows. Fix this by deferring re-ordering in a posted task.
    
    Bug: 1361204
    Change-Id: I06217595715d382c6b957e0b90931c7cbf391e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4152795
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091067}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ptr_util.h"
+#include "base/task/single_thread_task_runner.h"
 #include "cc/paint/paint_flags.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/capture_client.h"
@@ -537,13 +538,11 @@ class CaptureModeSession::ParentContainerObserver
 
   // aura::WindowObserver:
   void OnWindowAdded(aura::Window* window) override {
-    capture_mode_session_->RefreshStackingOrder();
-    capture_mode_session_->MaybeUpdateCaptureUisOpacity();
+    DeferredRefreshStackingOrder();
   }
 
   void OnWindowRemoved(aura::Window* window) override {
-    capture_mode_session_->RefreshStackingOrder();
-    capture_mode_session_->MaybeUpdateCaptureUisOpacity();
+    DeferredRefreshStackingOrder();
   }
 
   void OnWindowDestroying(aura::Window* window) override {
@@ -552,6 +551,22 @@ class CaptureModeSession::ParentContainerObserver
   }
 
  private:
+  // There might be other classes that track window hierarchy and re-ordering
+  // windows during window adding / deleting will break them. Therefore, defer
+  // the re-ordering.
+  void DeferredRefreshStackingOrder() {
+    base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
+        FROM_HERE,
+        base::BindOnce(
+            [](base::WeakPtr<CaptureModeSession> capture_mode_session) {
+              if (capture_mode_session) {
+                capture_mode_session->RefreshStackingOrder();
+                capture_mode_session->MaybeUpdateCaptureUisOpacity();
+              }
+            },
+            capture_mode_session_->weak_ptr_factory_.GetWeakPtr()));
+  }
+
   aura::Window* parent_container_;
 
   // Pointer to current capture session. Not nullptr during this lifecycle.
