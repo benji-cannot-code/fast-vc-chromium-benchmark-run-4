43e3e1b9e91ca279a5e5d8bcaee7cacc10a2d516
Mark Pearson <mpearson@chromium.org>
2023-01-10 00:21:07 +0000

    Revert "Update the HTML <s> element accessibility mappings"
    This reverts commit 993b9cf9876798329d3e19af7982021e02aa5881.
    
    Reason for revert:
    causes consistent failures in
    DumpAccessibilityTreeTest.AccessibilityS/fuchsia
    
    on these four fuschia bots:
    https://ci.chromium.org/p/chromium/builders/ci/fuchsia-arm64-cast-receiver-rel
    https://ci.chromium.org/p/chromium/builders/ci/fuchsia-arm64-rel
    https://ci.chromium.org/p/chromium/builders/ci/fuchsia-x64-cast-receiver-rel
    https://ci.chromium.org/p/chromium/builders/ci/fuchsia-x64-rel
    
    content_browsertests failed because of:
    All/DumpAccessibilityTreeTest.AccessibilityS/fuchsia
    
    Failure Reason:
    dump_accessibility_browsertest_base.cc(415): Value of: matches_expectation
      Actual: false
    Expected: true
    
    
    Original change's description:
    > Update the HTML <s> element accessibility mappings
    >
    > Bug: 1376572
    > Change-Id: Ie0fb1f323393ce8f251ea8b2f115409f386db4fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108548
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1090515}
    
    Bug: 1376572
    Change-Id: Ia3681c0b37d13517b047fdd2f66351623672bdb4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149268
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090607}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -1236,10 +1236,8 @@ ax::mojom::blink::Role AXNodeObject::NativeRoleIgnoringAria() const {
   if (GetNode()->HasTagName(html_names::kStrongTag))
     return ax::mojom::blink::Role::kStrong;
 
-  if (GetNode()->HasTagName(html_names::kDelTag) ||
-      GetNode()->HasTagName(html_names::kSTag)) {
+  if (GetNode()->HasTagName(html_names::kDelTag))
     return ax::mojom::blink::Role::kContentDeletion;
-  }
 
   if (GetNode()->HasTagName(html_names::kInsTag))
     return ax::mojom::blink::Role::kContentInsertion;
