e08fb1d8cd0d20735a4ad0757054a4e696ba52bb
Ryan Sturm <ryansturm@chromium.org>
2023-01-10 00:14:02 +0000

    Adding a default match for search prefetch/prerender experiment
    This experiment scopes the prefetch/prerender prediction to only trigger when the suggestion is the default match (i.e., autocompleted). See chrome://omnibox for examples of how the search suggestions are weighted and how suggestions move up and down as you type.
    
    BUG: b/262915418
    
    Change-Id: Ie04de59cfb8dae3ee9add6d5146b7a4c9c09e663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4112584
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090602}

--- chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.cc
@@ -95,3 +95,12 @@ bool PrefetchSearchHistorySuggestions() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kSearchNavigationPrefetch, "prefetch_search_history", true);
 }
+
+BASE_FEATURE(kSearchPrefetchOnlyAllowDefaultMatchPreloading,
+             "SearchPrefetchOnlyAllowDefaultMatchPreloading",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool OnlyAllowDefaultMatchPreloading() {
+  return base::FeatureList::IsEnabled(
+      kSearchPrefetchOnlyAllowDefaultMatchPreloading);
+}
