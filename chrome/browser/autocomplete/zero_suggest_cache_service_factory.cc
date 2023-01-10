e0af2da0d9c3a741cdf413ac07571f922abc3293
Khalid Peer <khalidpeer@chromium.org>
2023-01-10 02:24:48 +0000

    [ZPS] Enable cross-session persistence for cached ZPS response on NTP.
    The current implementation of the in-memory ZPS cache is unable to
    persist cached entries across multiple user sessions due to storing
    entries entirely in memory. As such, when the user restarts their
    browser and/or starts a new session from scratch, it's possible for them
    to be presented with zero remote ZPS suggestions when focusing the
    omnibox (e.g. on a slow internet connection).
    
    In order to address this edge-case, this CL enhances the in-memory ZPS
    caching service with the ability to persist cached entries by means of
    the user preferences system which stores data on disk.
    
    Furthermore, this CL introduces a dedicated cache entry for storing
    ZPS responses received on the NTP (as opposed to simply caching them
    alongside ZPS responses for SRP/Web). This change will ensure that
    the NTP-related entry will no longer be evicted out of the cache due
    to a large number of SRP/Web-related cache entries.
    
    Bug: 1351224
    Change-Id: Ia3a697e2487c37f48b5ad4e8e3a55bd5d2147bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148352
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090655}

--- chrome/browser/autocomplete/zero_suggest_cache_service_factory.cc
+++ chrome/browser/autocomplete/zero_suggest_cache_service_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autocomplete/zero_suggest_cache_service_factory.h"
 
+#include "chrome/browser/profiles/profile.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 
 // static
@@ -21,8 +22,9 @@ ZeroSuggestCacheServiceFactory* ZeroSuggestCacheServiceFactory::GetInstance() {
 
 KeyedService* ZeroSuggestCacheServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
+  Profile* profile = Profile::FromBrowserContext(context);
   return new ZeroSuggestCacheService(
-      OmniboxFieldTrial::kZeroSuggestCacheMaxSize.Get());
+      profile->GetPrefs(), OmniboxFieldTrial::kZeroSuggestCacheMaxSize.Get());
 }
 
 ZeroSuggestCacheServiceFactory::ZeroSuggestCacheServiceFactory()
--- components/omnibox/browser/zero_suggest_cache_service.h
+++ components/omnibox/browser/zero_suggest_cache_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OMNIBOX_BROWSER_ZERO_SUGGEST_CACHE_SERVICE_H_
 #define COMPONENTS_OMNIBOX_BROWSER_ZERO_SUGGEST_CACHE_SERVICE_H_
 
+#include <memory>
 #include <string>
 
 #include "base/containers/lru_cache.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
 #include "components/omnibox/browser/search_suggestion_parser.h"
+#include "components/prefs/pref_service.h"
 
 class ZeroSuggestCacheService : public KeyedService {
  public:
@@ -51,7 +53,7 @@ class ZeroSuggestCacheService : public KeyedService {
                                               const CacheEntry& response) {}
   };
 
-  explicit ZeroSuggestCacheService(size_t cache_size);
+  ZeroSuggestCacheService(PrefService* prefs, size_t cache_size);
 
   ZeroSuggestCacheService(const ZeroSuggestCacheService&) = delete;
   ZeroSuggestCacheService& operator=(const ZeroSuggestCacheService&) = delete;
@@ -74,11 +76,16 @@ class ZeroSuggestCacheService : public KeyedService {
   void RemoveObserver(Observer* observer);
 
  private:
+  // Pref service used for in-memory cache data persistence. Not owned.
+  const raw_ptr<PrefService> prefs_;
   // Cache mapping each page URL to the corresponding zero suggest response
   // (serialized JSON). |mutable| is used here because reading from the cache,
   // while logically const, will actually modify the internal recency list of
   // the HashingLRUCache object.
   mutable base::HashingLRUCache<std::string, CacheEntry> cache_;
+  // Dedicated cache entry for "ZPS on NTP" data in order to minimize any
+  // negative impact due to cache eviction policy.
+  CacheEntry ntp_entry_;
   base::ObserverList<Observer> observers_;
 };
 
