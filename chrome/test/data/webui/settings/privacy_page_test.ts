33646af054d98c99cba62858af54b78441f8cf75
Olesia Marukhno <olesiamarukhno@google.com>
2023-01-10 15:36:04 +0000

    [privacy sandbox] Update subtitle for third-party cookies rows
    Observe the 3PC preference and update the 3PC row subtitle based on
    the value in the site settings.
    
    Clean up CrSettingsSiteSettingsPageTest tests. There are three tests
    and each one ran all 3 suites. Only run the related suite in the test.
    
    Bug: 1378703
    Change-Id: I8d32960562f5bcadfeaacfc4651c4348feb50aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136119
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1090838}

--- chrome/test/data/webui/settings/privacy_page_test.ts
+++ chrome/test/data/webui/settings/privacy_page_test.ts
@@ -133,7 +133,7 @@ suite('PrivacyPage', function() {
         'prefs.profile.cookie_controls_mode.value',
         CookieControlsMode.INCOGNITO_ONLY);
     assertEquals(
-        page.i18n('thirdPartyCookiesLinkRowSublabelDisabledInIncognito'),
+        page.i18n('thirdPartyCookiesLinkRowSublabelDisabledIncognito'),
         page.shadowRoot!
             .querySelector<CrLinkRowElement>(
                 '#thirdPartyCookiesLinkRow')!.subLabel);
