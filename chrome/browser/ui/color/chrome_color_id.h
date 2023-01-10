34ed06c2fcc2a606d6e220953c30ac2b4321f628
Yiren Wang <yrw@chromium.org>
2023-01-10 21:38:13 +0000

    Use default browser theme for PiP 2.0 window
    Only the title bar of PiP 2.0 window should stay in dark theme, and
    other dialogs in the window should be the default browser theme.
    
    Bug: 1400080
    Change-Id: Ib15cdba149da155ec29753e9ac5b29874887f40f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4141059
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091016}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -257,7 +257,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorPipWindowBackToTabButtonBackground) \
   E_CPONLY(kColorPipWindowBackground) \
   E_CPONLY(kColorPipWindowControlsBackground) \
+  E_CPONLY(kColorPipWindowTopBarBackground) \
   E_CPONLY(kColorPipWindowForeground) \
+  E_CPONLY(kColorPipWindowForegroundInactive) \
   E_CPONLY(kColorPipWindowHangUpButtonForeground) \
   E_CPONLY(kColorPipWindowSkipAdButtonBackground) \
   E_CPONLY(kColorPipWindowSkipAdButtonBorder) \
--- chrome/browser/ui/views/frame/browser_frame.cc
+++ chrome/browser/ui/views/frame/browser_frame.cc
@@ -441,6 +441,5 @@ bool BrowserFrame::RegenerateFrameOnThemeChange(
 }
 
 bool BrowserFrame::ShouldUseDarkTheme() const {
-  return browser_view_->browser()->profile()->IsIncognitoProfile() ||
-         browser_view_->GetIsPictureInPictureType();
+  return browser_view_->browser()->profile()->IsIncognitoProfile();
 }
