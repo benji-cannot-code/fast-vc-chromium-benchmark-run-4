a2f602a2d390af203c5aeebaa132c2942daee4eb
Anastasia Helfinstein <anastasi@google.com>
2023-01-10 16:04:40 +0000

    [ChromeVox] Clean up getVirtualDisplayState
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:260599332
    Test: cq
    Change-Id: Ic2ac9a98180273fcf10bc63165e4270c83e2004b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149249
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090849}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_display_manager.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_display_manager.js
@@ -245,8 +245,8 @@ export class BrailleDisplayManager {
       processDisplayState(newState);
       LocalStorage.set('menuBrailleCommands', true);
     } else {
-      BrailleCaptionsBackground.getVirtualDisplayState().then(
-          processDisplayState);
+      const state = BrailleCaptionsBackground.getVirtualDisplayState();
+      processDisplayState(state);
       LocalStorage.set('menuBrailleCommands', false);
     }
   }
