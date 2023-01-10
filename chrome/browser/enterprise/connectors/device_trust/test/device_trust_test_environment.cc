1fe2bc84189953a550dde0c47266e3fe2dd2df7e
Zonghan Xu <xzonghan@chromium.org>
2023-01-10 18:27:33 +0000

    [DTC] Create reattempted key creation browser tests in device_trust_browsertest
    Add a new test case where first attestation flow fails but the following one succeeds. Various checks are performed such as presence of the DT attestation key and metrics.
    
    Bug: b:260882402
    Change-Id: Ife12718d9b3d6fd9030f18473a63eaaab99ceb28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113988
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090915}

---
+++ chrome/browser/enterprise/connectors/device_trust/test/device_trust_test_environment.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/device_trust/test/device_trust_test_environment.h"
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/key_persistence_delegate.h"
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/key_persistence_delegate_factory.h"
+
+namespace enterprise_connectors {
+
+DeviceTrustTestEnvironment::DeviceTrustTestEnvironment(
+    base::StringPiece thread_name,
+    HttpResponseCode upload_response_code)
+    : worker_thread_(std::string(thread_name)),
+      upload_response_code_(upload_response_code) {
+  key_persistence_delegate_ = KeyPersistenceDelegateFactory::GetInstance()
+                                  ->CreateKeyPersistenceDelegate();
+}
+
+DeviceTrustTestEnvironment::~DeviceTrustTestEnvironment() = default;
+
+bool DeviceTrustTestEnvironment::KeyExists() {
+  if (key_persistence_delegate_->LoadKeyPair()) {
+    return true;
+  }
+  return false;
+}
+
+void DeviceTrustTestEnvironment::SetUploadResult(
+    HttpResponseCode upload_response_code) {
+  upload_response_code_ = upload_response_code;
+}
+
+}  // namespace enterprise_connectors
