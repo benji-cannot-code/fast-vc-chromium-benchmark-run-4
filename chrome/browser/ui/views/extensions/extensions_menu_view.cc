ee093340332c1386c7735d87c8c36419b91292a9
Emilia Paz <emiliapaz@chromium.org>
2023-01-10 20:19:06 +0000

    [Extensions cleanup] Remove allow_pinning variable from menu
    Extensions menu can get directly from the container whether the actions
    can be shown or not in the container. Therefore, there is no need to
    pass the allow_pinning variable to the menu.
    
    Bug: N/A
    Change-Id: I56735053136a6cb1860798da124646681427158f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144546
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090968}

--- chrome/browser/ui/views/extensions/extensions_menu_view.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/extensions/extension_action_view_controller.h"
+#include "chrome/browser/ui/extensions/extensions_container.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
 #include "chrome/browser/ui/views/bubble_menu_item_factory.h"
@@ -72,13 +73,11 @@ InstalledExtensionMenuItemView* GetAsMenuItemView(views::View* view) {
 ExtensionsMenuView::ExtensionsMenuView(
     views::View* anchor_view,
     Browser* browser,
-    ExtensionsContainer* extensions_container,
-    bool allow_pinning)
+    ExtensionsContainer* extensions_container)
     : BubbleDialogDelegateView(anchor_view,
                                views::BubbleBorder::Arrow::TOP_RIGHT),
       browser_(browser),
       extensions_container_(extensions_container),
-      allow_pinning_(allow_pinning),
       toolbar_model_(ToolbarActionsModel::Get(browser_->profile())),
       cant_access_{nullptr, nullptr,
                    IDS_EXTENSIONS_MENU_CANT_ACCESS_SITE_DATA_SHORT,
@@ -307,7 +306,8 @@ void ExtensionsMenuView::CreateAndInsertNewItem(
   // The bare `new` is safe here, because InsertMenuItem is guaranteed to
   // be added to the view hierarchy, which takes ownership.
   auto* item = new InstalledExtensionMenuItemView(
-      browser_, std::move(controller), allow_pinning_);
+      browser_, std::move(controller),
+      extensions_container_->CanShowActionsInToolbar());
   extensions_menu_items_.insert(item);
   InsertMenuItem(item);
   // Sanity check that the item was added.
@@ -487,15 +487,14 @@ base::AutoReset<bool> ExtensionsMenuView::AllowInstancesForTesting() {
 views::Widget* ExtensionsMenuView::ShowBubble(
     views::View* anchor_view,
     Browser* browser,
-    ExtensionsContainer* extensions_container,
-    bool allow_pinning) {
+    ExtensionsContainer* extensions_container) {
   DCHECK(!g_extensions_dialog);
   // Experiment `kExtensionsMenuAccessControl` is introducing a new menu. Check
   // `ExtensionsMenuView` is only constructed when the experiment is disabled.
   DCHECK(!base::FeatureList::IsEnabled(
       extensions_features::kExtensionsMenuAccessControl));
-  g_extensions_dialog = new ExtensionsMenuView(
-      anchor_view, browser, extensions_container, allow_pinning);
+  g_extensions_dialog =
+      new ExtensionsMenuView(anchor_view, browser, extensions_container);
   views::Widget* widget =
       views::BubbleDialogDelegateView::CreateBubble(g_extensions_dialog);
   widget->Show();
