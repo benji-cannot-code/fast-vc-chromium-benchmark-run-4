026eda23fb1f36d1f9e96d9cdc5d70e301ad4041
Michele Fan <michelefan@chromium.org>
2023-01-11 01:45:30 +0000

    snap-group: Build skeleton for the feature
    This CL implements the initial skeleton for the snap group
    feature as described in go/snapgroup.
    
    Fixed: b:264306685
    Test: ash_unittests --gtest_filter=SnapGroupTest.*
    Change-Id: I15c93a679c94e6627817ee2f849e02917ce65a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4140191
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091137}

--- ash/shell.cc
+++ ash/shell.cc
@@ -186,6 +186,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/resize_shadow_controller.h"
 #include "ash/wm/screen_pinning_controller.h"
+#include "ash/wm/snap_group/snap_group_controller.h"
 #include "ash/wm/system_gesture_event_filter.h"
 #include "ash/wm/system_modal_container_event_filter.h"
 #include "ash/wm/system_modal_container_layout_manager.h"
@@ -789,6 +790,8 @@ Shell::~Shell() {
   snooping_protection_controller_.reset();
   human_presence_orientation_controller_.reset();
 
+  snap_group_controller_.reset();
+
   // Shutdown tablet mode controller early on since it has some observers which
   // need to be removed. It will be destroyed later after all windows are closed
   // since it might be accessed during this process.
@@ -1233,6 +1236,10 @@ void Shell::Init(
 
   env_filter_ = std::make_unique<::wm::CompoundEventFilter>();
   AddPreTargetHandler(env_filter_.get());
+
+  if (features::IsSnapGroupEnabled()) {
+    snap_group_controller_ = std::make_unique<SnapGroupController>();
+  }
 
   // FocusController takes ownership of AshFocusRules.
   focus_rules_ = new AshFocusRules();
--- ash/shell.h
+++ ash/shell.h
@@ -212,6 +212,7 @@ struct ShellInitParams;
 class ShellObserver;
 class ShutdownControllerImpl;
 class SmsObserver;
+class SnapGroupController;
 class SnoopingProtectionController;
 class StickyKeysController;
 class SystemGestureEventFilter;
@@ -626,6 +627,9 @@ class ASH_EXPORT Shell : public SessionObserver,
   SessionControllerImpl* session_controller() {
     return session_controller_.get();
   }
+  SnapGroupController* snap_group_controller() {
+    return snap_group_controller_.get();
+  }
   FeatureDiscoveryDurationReporterImpl* feature_discover_reporter() {
     return feature_discover_reporter_.get();
   }
@@ -889,6 +893,7 @@ class ASH_EXPORT Shell : public SessionObserver,
   std::unique_ptr<GlanceablesController> glanceables_controller_;
   std::unique_ptr<HoldingSpaceController> holding_space_controller_;
   std::unique_ptr<PowerPrefs> power_prefs_;
+  std::unique_ptr<SnapGroupController> snap_group_controller_;
   std::unique_ptr<SnoopingProtectionController> snooping_protection_controller_;
   std::unique_ptr<HumanPresenceOrientationController>
       human_presence_orientation_controller_;
