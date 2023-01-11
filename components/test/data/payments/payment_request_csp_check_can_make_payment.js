ddfe2547f352e2970e85a94b8d7f11ef4ba884c9
Rouslan Solomakhin <rouslan@chromium.org>
2023-01-11 00:11:10 +0000

    [Payment] Launch enforcing Content Security Policy
    Before this patch, calling `new PaymentRequest(https://example.test)`
    would always ping https://example.test, even if the Content Security
    Policy (CSP) prohibited connections to that URL.
    
    This patch launches the enforcement of CSP in the Web Payment API.
    
    After this patch, if CSP connect-src directive blocks access to
    https://example.test, then calling
    `new PaymentRequest(https://example.test)` will not ping the blocked
    URL, `PaymentRequest.canMakePayment()` will return "false" and
    `PaymentRequest.show()` will return "NotSupportedError", unless the user
    has the temporary flag chrome://flags/#ignore-csp-in-web-payment-api
    enabled or the website participates in the (temporary) reverse origin
    trial.
    
    Chrome status:
    https://chromestatus.com/feature/6286595631087616
    
    Intent:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/mlTnVIovBc0/m/uWMMIM2MCAAJ
    
    Launch:
    http://launch/4200427
    
    Bug: 1013080, 1349091
    Change-Id: I483a97503bc646384a565b766124c0d9f64bed84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4139782
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091086}

---
+++ components/test/data/payments/payment_request_csp_check_can_make_payment.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Checks canMakePayment() for the given payment method identifier.
+ * @param {string} method - Payment method identifier to check.
+ * @return {Promise<string>} - Either 'true', 'false', or an error message.
+ */
+async function checkCanMakePayment(method) {
+  try {
+    const request = new PaymentRequest(
+      [
+        {supportedMethods: method},
+      ],
+      {total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}}});
+    const result = await request.canMakePayment();
+    return (result ? 'true' : 'false');
+  } catch (error) {
+    return error.toString();
+  }
+}
---
+++ third_party/blink/web_tests/virtual/payment-request-csp-violation/README.md
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Ignore Content Security Policy (CSP) in Web Payment API with
+--enable-blink-features=IgnoreCSPInWebPaymentAPI to verify that DevTools Console
+prints a deprecation warning message when CSP is being ignored.
