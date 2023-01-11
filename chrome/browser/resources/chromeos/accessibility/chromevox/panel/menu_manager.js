1223e105cc6c06f32c2cb4d8e731eb03c392040b
Anastasia Helfinstein <anastasi@google.com>
2023-01-11 01:53:07 +0000

    [ChromeVox] Move getSelectedMenu() to MenuManager
    Part of the incremental migration of ChromeVox menu code from panel.js
    to menu_manager.js.
    
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:260599332
    Test: cq
    Change-Id: I332e06e2839513120d706bde99fed1377ff953bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148216
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091143}

--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/menu_manager.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/menu_manager.js
@@ -14,7 +14,7 @@ export class MenuManager {
   constructor() {
     /**
      * The array of top-level menus.
-     * @private {!Array<PanelMenu>}
+     * @private {!Array<!PanelMenu>}
      */
     this.menus_ = [];
 
@@ -32,6 +32,16 @@ export class MenuManager {
         }
       }
     }
+  }
+
+  /**
+   * @param {string|undefined} opt_menuTitle
+   * @return {!PanelMenu}
+   */
+  getSelectedMenu(opt_menuTitle) {
+    const specifiedMenu =
+        this.menus_.find(menu => menu.menuMsg === opt_menuTitle);
+    return specifiedMenu || this.searchMenu_ || this.menus_[0];
   }
 
   /**
