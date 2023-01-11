02d100d1317c6ec955b3dcd0a0a4504b91ea50df
Rainhard Findling <rainhard@chromium.org>
2023-01-11 15:14:27 +0000

    [Site settings] Update intrusive ads strings
    The old Skia Gold Pixeltest images will be invalidated in a follow-up CL.
    
    Screenshots: crbug.com/1378703#c290, crbug.com/1378703#c292
    Bug: 1378703
    Change-Id: Iacf9c40721e04932eaf3eed9dee3cac2d28570ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4150491
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Auto-Submit: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091326}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_dialog_browsertest.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_dialog_browsertest.cc
@@ -409,6 +409,8 @@ IN_PROC_BROWSER_TEST_F(PageInfoBubbleViewDialogBrowserTest,
 // set. All permissions will show regardless of its factory default value.
 IN_PROC_BROWSER_TEST_F(PageInfoBubbleViewDialogBrowserTest,
                        InvokeUi_AllowAllPermissions) {
+  // Last updated in crrev.com/c/4150491.
+  set_baseline("4150491");
   ShowAndVerifyUi();
 }
 
@@ -416,6 +418,8 @@ IN_PROC_BROWSER_TEST_F(PageInfoBubbleViewDialogBrowserTest,
 // set. All permissions will show regardless of its factory default value.
 IN_PROC_BROWSER_TEST_F(PageInfoBubbleViewDialogBrowserTest,
                        InvokeUi_BlockAllPermissions) {
+  // Last updated in crrev.com/c/4150491.
+  set_baseline("4150491");
   ShowAndVerifyUi();
 }
 
