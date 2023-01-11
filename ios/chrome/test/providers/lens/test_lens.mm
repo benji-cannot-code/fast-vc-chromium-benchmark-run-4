577dea3c7d4509054d73b2aedb8a38710e0a4b62
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-01-11 22:20:25 +0000

    Add missing ostream include.
    These files have a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    The <ostream> include is needed by the `NOTREACHED() << "..."`
    statements. base/check.h only forward declares <ostream> by
    including <iosfwd>. The documentation in base/check.h mentions:
    
    // Additional information can be streamed to these macros and will be included
    // in the log output if the condition doesn't hold (you may need to include
    // <ostream>):
    //
    //   CHECK(condition) << "Additional info.";
    
    Change-Id: Ia98da76beb3c35150f9284e1c2011e1834d0ae47
    Bug: 1270812, 1372522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4152495
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091490}

--- ios/chrome/test/providers/lens/test_lens.mm
+++ ios/chrome/test/providers/lens/test_lens.mm
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/public/provider/chrome/browser/lens/lens_api.h"
-#import "ios/public/provider/chrome/browser/lens/lens_configuration.h"
-#import "url/url_constants.h"
+#import <UIKit/UIKit.h>
+#import <ostream>
 
 #import "base/bind.h"
 #import "base/notreached.h"
-
-#import <UIKit/UIKit.h>
+#import "ios/public/provider/chrome/browser/lens/lens_api.h"
+#import "ios/public/provider/chrome/browser/lens/lens_configuration.h"
+#import "url/url_constants.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
