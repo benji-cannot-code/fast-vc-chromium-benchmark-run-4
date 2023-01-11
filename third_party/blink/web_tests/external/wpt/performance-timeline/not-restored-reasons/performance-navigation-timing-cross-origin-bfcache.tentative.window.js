c1e55eb1cc94b47d3d69f354921f2e4199d05ad2
Yuzu Saijo <yuzus@chromium.org>
2023-01-11 07:45:46 +0000

    [bfcache] Expose src, id and frame name for cross-origin frames
    This CL starts exposing src, id and frame for cross-origin frames for
    NotRestoredReasons API. This is for better debugging and conforms to the  explainer: https://github.com/rubberyuzu/bfcache-not-retored-reason/blob/main/NotRestoredReason.md
    
    We send the frame tree at the time of being navigated away, and the frame attributes will help debug the page by associating the possibly modified frame tree and each frame.
    
    Bug: 1379247
    Change-Id: Ic41450d7ef9de0cb3429b644299ed8157c7ff503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986625
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091205}

--- third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-cross-origin-bfcache.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-cross-origin-bfcache.tentative.window.js
@@ -31,7 +31,6 @@ promise_test(async t => {
   );
   // Use WebSocket to block BFCache.
   await useWebSocket(rc1_child);
-
   const rc1_child_url = await rc1_child.executeScript(() => {
     return location.href;
   });
@@ -53,9 +52,9 @@ promise_test(async t => {
       /*reasons=*/[],
       /*children=*/[{
         'blocked': true,
-        'url': '',
-        'src': '',
-        'id': '',
+        'url': null,
+        'src': rc1_child_url,
+        'id': 'test-id',
         'name': '',
         'reasons': [],
         'children': []
