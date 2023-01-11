6b03fa916a1347a9ec137649064c984f64c6fcc3
Ari Chivukula <arichiv@chromium.org>
2023-01-11 11:53:10 +0000

    base::UnsafeDanglingUntriaged in base/observer_list_threadsafe.h
    Unfortunately there doesn't seem to be a clean way to undangle the
    pointer here without re-working the memory management of the entire
    observer stack. That said, the current design already forces mgmt to the
    ownership layer so the intent of the function really is to have dangling
    pointers.
    
    Design Doc:
    https://docs.google.com/document/d/164SnySRX6MZgFkX8qLLLClTnZ0S8s5wLiNFWNyQ8f4E/edit#
    
    Bug: 1381412
    Change-Id: I4d723b0d39ec857268c2e3b169bca9bab0b50812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4125916
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091267}

--- base/observer_list_threadsafe.h
+++ base/observer_list_threadsafe.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/containers/contains.h"
 #include "base/dcheck_is_on.h"
+#include "base/functional/bind.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
@@ -156,7 +157,12 @@ class ObserverListThreadSafe : public internal::ObserverListThreadSafeBase {
         task_runner->PostTask(
             current_notification->from_here,
             BindOnce(&ObserverListThreadSafe<ObserverType>::NotifyWrapper, this,
-                     UnsafeDanglingUntriaged(observer),
+                     // While `observer` may be dangling, we pass it and
+                     // check it wasn't deallocated in NotifyWrapper() which can
+                     // check `observers_` to verify presence (the owner of the
+                     // observer is responsible for removing it from that list
+                     // before deallocation).
+                     UnsafeDangling(observer),
                      NotificationData(this, observer_id,
                                       current_notification->from_here,
                                       notification_data->method)));
@@ -202,7 +208,12 @@ class ObserverListThreadSafe : public internal::ObserverListThreadSafeBase {
       observer.second.task_runner->PostTask(
           from_here,
           BindOnce(&ObserverListThreadSafe<ObserverType>::NotifyWrapper, this,
-                   base::UnsafeDanglingUntriaged(observer.first),
+                   // While `observer.first` may be dangling, we pass it and
+                   // check it wasn't deallocated in NotifyWrapper() which can
+                   // check `observers_` to verify presence (the owner of the
+                   // observer is responsible for removing it from that list
+                   // before deallocation).
+                   UnsafeDangling(observer.first),
                    NotificationData(this, observer.second.observer_id,
                                     from_here, method)));
     }
@@ -226,7 +237,7 @@ class ObserverListThreadSafe : public internal::ObserverListThreadSafeBase {
 
   ~ObserverListThreadSafe() override = default;
 
-  void NotifyWrapper(ObserverType* observer,
+  void NotifyWrapper(MayBeDangling<ObserverType> observer,
                      const NotificationData& notification) {
     {
       AutoLock auto_lock(lock_);
