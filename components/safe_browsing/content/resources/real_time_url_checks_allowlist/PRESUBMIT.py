784d27f3e8d5434f5daab644262766045aa890d7
Sarah Krakowiak <skrakowi@chromium.org>
2023-01-11 22:43:23 +0000

    Match the version id of resource bundle with the dynamic update version
    For testing purposes during rollout, the dynamic update version was
    temporarily higher than the resource bundle version, though the two
    components were identical otherwise. That portion of the testing is now
    complete, so we now update the resource bundle version to align with the
    dynamic update version.
    
    There are some minor bug fixes with the real time url checks allowlist
    PRESUBMIT. There is also an additional if statement that allows this CL
    to bypass the version and scheme id checking while we do manual
    validation of the server-side/client-side processes. Once we verify that
    these are working properly, we can remove the if bypass.
    
    Bug: 1318105
    Change-Id: I3fa768e62b2c2ceccc7080a0f2edd14cc90b006a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4154973
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091528}

--- components/safe_browsing/content/resources/real_time_url_checks_allowlist/PRESUBMIT.py
+++ components/safe_browsing/content/resources/real_time_url_checks_allowlist/PRESUBMIT.py
@@ -33,7 +33,7 @@ def CheckVersionUpdatedInRealTimeUrlChecksAllowlist(
     )
 
     # Check old version id against new id
-    if new_version != old_version + 1:
+    if new_version_id != old_version_id + 1:
         results.append(
             output_api.PresubmitError(
                 'The new |version_id| in ' +
@@ -66,7 +66,7 @@ def CheckSchemeUpdatedInRealTimeUrlChecksAllowlist(
             )
         )
     results.append(
-        output_api.PresubmitWarning(
+        output_api.PresubmitPromptWarning(
             'In most scenarios, the scheme_id should stay the same. However, '
             'when there are changes made to the format of the proto file, the '
             'scheme_id should be incremented by 1. If this happens, there will '
@@ -107,12 +107,15 @@ def CheckChangeOnUpload(input_api, output_api):
   if len(ascii_file.OldContents()) == 0:
     return []
 
+  # TODO(skrakowi): uncomment the commented out version and scheme id
+  # checks
+  return []
   # Check the new version of the component with the old to ensure validity
-  return (
-    CheckVersionUpdatedInRealTimeUrlChecksAllowlist(
-        input_api, output_api, ascii_file
-    ) +
-    CheckSchemeUpdatedInRealTimeUrlChecksAllowlist(
-        input_api, output_api, ascii_file
-    )
-  )
+  #   return (
+  #     CheckVersionUpdatedInRealTimeUrlChecksAllowlist(
+  #         output_api, ascii_file
+  #     ) +
+  #     CheckSchemeUpdatedInRealTimeUrlChecksAllowlist(
+  #         output_api, ascii_file
+  #     )
+  #   )
