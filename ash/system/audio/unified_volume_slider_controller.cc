ffe6b91b3be4ba5ba54470fd664158b24efae2f4
Sylvie Liu <sylvieliu@chromium.org>
2023-01-11 06:51:13 +0000

    Updated the audio subpage for QsRevamp
    This CL updated the audio subpage for QsRevamp.
    1. The Live Caption container is added on the top of the scroll list as
       it will always show up for the revamped audio subpage.
    2. The Live Caption has a toggle button on the right.
    3. Each QuickSettingsSlider for each device is added. Both the input and
       output device will now have a slider.
    4. The device name is now stacked on top of the slider, and a checkmark
       on the right side will indicate the selected slider.
    5. The selected slider has the style as kRadioActive, and the unselected
       slider has the style as kRadioInactive. The focus ring for the active
       slider will encompass the value part while that for the inactive
       slider will encompass the whole slider.
    6. The Live Caption button in the main page is now enabled.
    7. Unit test for the audio subpage is parameterized and new test for
       output sliders is added.
    
    Screenshot for Live Caption On/Off status:
    https://screenshot.googleplex.com/7SLshM2q7RfDhKP
    
    Screenshot for focus on sliders and Live Caption toggle:
    https://screenshot.googleplex.com/4xhNRBewsyZ5Hpx
    
    Bug: b/252870986
    Change-Id: I4dd0859459ef144e2071ad876456f8e577cc6c8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109077
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091199}

--- ash/system/audio/unified_volume_slider_controller.cc
+++ ash/system/audio/unified_volume_slider_controller.cc
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/audio/unified_volume_slider_controller.h"
 
 #include "ash/constants/quick_settings_catalogs.h"
-#include "ash/shell.h"
 #include "ash/system/audio/unified_volume_view.h"
-#include "ash/system/machine_learning/user_settings_event_logger.h"
 
 namespace ash {
+
+namespace {
+UnifiedVolumeSliderController::MapDeviceSliderCallback*
+    g_map_slider_device_callback = nullptr;
+}  // namespace
 
 UnifiedVolumeSliderController::Delegate::Delegate() = default;
 
@@ -22,10 +25,31 @@ UnifiedVolumeSliderController::UnifiedVolumeSliderController(
   DCHECK(delegate);
 }
 
+UnifiedVolumeSliderController::UnifiedVolumeSliderController()
+    : delegate_(nullptr) {}
+
 UnifiedVolumeSliderController::~UnifiedVolumeSliderController() = default;
 
+std::unique_ptr<UnifiedVolumeView>
+UnifiedVolumeSliderController::CreateVolumeSlider(uint64_t device_id) {
+  auto slider = std::make_unique<UnifiedVolumeView>(
+      this, device_id, /*is_active_output_node=*/false);
+
+  if (g_map_slider_device_callback) {
+    g_map_slider_device_callback->Run(device_id, slider.get());
+  }
+
+  return slider;
+}
+
+// static
+void UnifiedVolumeSliderController::SetMapDeviceSliderCallbackForTest(
+    MapDeviceSliderCallback* map_slider_device_callback) {
+  g_map_slider_device_callback = map_slider_device_callback;
+}
+
 views::View* UnifiedVolumeSliderController::CreateView() {
-  return new UnifiedVolumeView(this, delegate_);
+  return new UnifiedVolumeView(this, delegate_, /*is_active_output_node=*/true);
 }
 
 QsSliderCatalogName UnifiedVolumeSliderController::GetCatalogName() {
