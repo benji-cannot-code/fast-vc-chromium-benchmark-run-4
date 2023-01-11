b0d684c28a78c27f0924ff0fc0afc58d24d60713
David Awogbemila <awogbemila@chromium.org>
2023-01-11 15:40:35 +0000

    [Scroll Unif.] Apply device_scale_factor in ScrollUnification path
    This patch adjusts the ScrollUnification-enabled scrolling path so that
    device_scale_factor is applied to the scroll offset when handling
    kScrollByLine which is more similar to what
    ScrollableArea::ResolveScrollDelta does.
    
    Also, according to the comment in cc/input/input_handler.cc:
    
    // CC side always uses fractional scroll deltas.
    
    so mouse-interactions-dsf-2.html should expect fractional scroll offsets even if
    FractionalScrollOffsets is not enabled.
    
    This causes virtual/hidpi/fast/scrolling/scrollbars/dsf-ready/mouse-interactions-dsf-2.html
    to now pass with ScrollUnification enabled.
    
    Bug: 1312036
    Change-Id: I23ba8f2e476ae3f76a3a1f05d534afcbe3048e9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143616
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091336}

--- cc/input/input_handler.cc
+++ cc/input/input_handler.cc
@@ -1278,7 +1278,7 @@ bool InputHandler::IsMainThreadScrolling(
 }
 
 float InputHandler::LineStep() const {
-  return kPixelsPerLineStep;
+  return kPixelsPerLineStep * ActiveTree().painted_device_scale_factor();
 }
 
 // TODO(mehdika): There is some redundancy between this function and
