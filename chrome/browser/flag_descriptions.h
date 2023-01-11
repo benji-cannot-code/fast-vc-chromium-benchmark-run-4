8fbc5b58e037d0d135ba0926f5ed78a726241c2c
Jennifer Serrano <jennserrano@google.com>
2023-01-11 01:00:56 +0000

    Phone Hub: Add feature flag.
    Create Phone Hub Nudge feature flag for nudge experiment.
    
    Change-Id: I8c1cc6f9acbcc1f746fc27c55d8b67512e15c91a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4126322
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091120}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3271,6 +3271,9 @@ extern const char kPhoneHubCameraRollDescription[];
 extern const char kPhoneHubFeatureSetupErrorHandlingName[];
 extern const char kPhoneHubFeatureSetupErrorHandlingDescription[];
 
+extern const char kPhoneHubNudgeName[];
+extern const char kPhoneHubNudgeDescription[];
+
 extern const char kPreferDcheckName[];
 extern const char kPreferDcheckDescription[];
 
