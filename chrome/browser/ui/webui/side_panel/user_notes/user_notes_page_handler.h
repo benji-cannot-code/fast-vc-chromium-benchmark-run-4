069cacc94dc9e2843a33a5cc0fe4c5935a6e0032
Tom Lukaszewicz <tluk@google.com>
2023-01-11 17:18:42 +0000

    Revert "[power bookmarks] Move observer methods to the backend"
    This reverts commit a72364a92bafd9f20487a56400d7f6e953bb7e3f.
    
    Reason for revert:
    Speculatively reverting due to failures on Linux TSan bots
    
    First failing build
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20TSan%20Tests/81180/overview
    
    Original change's description:
    > [power bookmarks] Move observer methods to the backend
    >
    > This will allow plumbing events driven through sync to propagate to
    > observers registered on the service.
    > * Extract power_bookmark_observer to its own class.
    > * Plumb through the ui thread task runner.
    > * Move observer triggering to power_bookmark_backend.
    > * Move metrics to power_bookmark_backend.
    >
    > Bug: 1376612
    > Change-Id: I23ccd0ea5b40976e8e60921d708cbf2aec1c6b2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134115
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Reviewed-by: bttk - <bttk@chromium.org>
    > Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1091124}
    
    Bug: 1376612
    Change-Id: I12e889c3ddc8e22ca79b0e9607f1f46b10e1cab0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4157371
    Commit-Queue: Tom Lukaszewicz <tluk@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tom Lukaszewicz <tluk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091380}

--- chrome/browser/ui/webui/side_panel/user_notes/user_notes_page_handler.h
+++ chrome/browser/ui/webui/side_panel/user_notes/user_notes_page_handler.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/webui/side_panel/user_notes/user_notes.mojom.h"
-#include "components/power_bookmarks/common/power_bookmark_observer.h"
 #include "components/power_bookmarks/core/power_bookmark_service.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -24,10 +23,11 @@ class Browser;
 class UserNotesSidePanelUI;
 class Profile;
 
-class UserNotesPageHandler : public side_panel::mojom::UserNotesPageHandler,
-                             public power_bookmarks::PowerBookmarkObserver,
-                             public TabStripModelObserver,
-                             public content::WebContentsObserver {
+class UserNotesPageHandler
+    : public side_panel::mojom::UserNotesPageHandler,
+      public power_bookmarks::PowerBookmarkService::Observer,
+      public TabStripModelObserver,
+      public content::WebContentsObserver {
  public:
   explicit UserNotesPageHandler(
       mojo::PendingReceiver<side_panel::mojom::UserNotesPageHandler> receiver,
@@ -59,7 +59,7 @@ class UserNotesPageHandler : public side_panel::mojom::UserNotesPageHandler,
   GURL GetCurrentTabUrlForTesting() { return current_tab_url_; }
 
  private:
-  // power_bookmarks::PowerBookmarkObserver:
+  // power_bookmarks::PowerBookmarkService::Observer:
   void OnPowersChanged() override;
 
   // TabStripModelObserver:
--- ios/chrome/browser/power_bookmarks/power_bookmark_service_factory.mm
+++ ios/chrome/browser/power_bookmarks/power_bookmark_service_factory.mm
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/bookmarks/bookmark_model_factory.h"
 #import "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#import "ios/web/public/thread/web_task_traits.h"
-#import "ios/web/public/thread/web_thread.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -48,7 +46,6 @@ PowerBookmarkServiceFactory::BuildServiceInstanceFor(
       ios::BookmarkModelFactory::GetInstance()->GetForBrowserState(
           chrome_state),
       state->GetStatePath().AppendASCII("power_bookmarks"),
-      web::GetUIThreadTaskRunner({}),
       base::ThreadPool::CreateSequencedTaskRunner(
           {base::MayBlock(), base::TaskPriority::USER_BLOCKING,
            base::TaskShutdownBehavior::BLOCK_SHUTDOWN}));
