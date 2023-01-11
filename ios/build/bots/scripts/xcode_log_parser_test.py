19aadb491c2cae1e49b131790a84b3aacd4f8cea
Jeff Yoon <jeffyoon@google.com>
2023-01-11 17:56:13 +0000

    iOS test runner to accept Expected Failures as successful runs
    Tests with expected failures are not accepted as passes because
    the xcode log parser only confirms 'success' messages.
    This change includes 'Expected Failure' as an expected
    failing test.
    
    Bug: 1404878
    Change-Id: I78e595ddbe92deff9f72211eb770ec99cfbd021b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148697
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091400}

--- ios/build/bots/scripts/xcode_log_parser_test.py
+++ ios/build/bots/scripts/xcode_log_parser_test.py
@@ -175,7 +175,7 @@ TESTS_REF = """
                           },
                           {
                             "testStatus": {
-                              "_value": "Success"
+                              "_value": "Expected Failure"
                             },
                             "duration" : {
                               "_type" : {
