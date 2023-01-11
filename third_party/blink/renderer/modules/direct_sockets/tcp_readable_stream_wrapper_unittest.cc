d8b36066d30bfde318f46bad8a18280809c25081
Andrew Rayskiy <greengrape@google.com>
2023-01-11 22:48:59 +0000

    [DirectSockets] Support BYOB Reading for TCP, Part 2/2
    This CL converts TCPReadableStreamWrapper to inherit from
    ReadableByteStreamWrapper and wires up the necessary bits to make BYOB
    reading work.
    
    1. crrev.com/c/4114288
    2. [this CL]
    
    Bug: 1166811
    Change-Id: Idf693d91c57ed61333e3a4dce28923ac4b5b55ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4118768
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091538}

--- third_party/blink/renderer/modules/direct_sockets/tcp_readable_stream_wrapper_unittest.cc
+++ third_party/blink/renderer/modules/direct_sockets/tcp_readable_stream_wrapper_unittest.cc
@@ -335,9 +335,9 @@ TEST_P(TCPReadableStreamWrapperCloseTest, ResetCancelError) {
 
   stream_creator->ResetPipe();
 
+  // cancel() always has priority.
   ASSERT_EQ(tcp_readable_stream_wrapper->GetState(),
-            graceful ? StreamWrapper::State::kClosed
-                     : StreamWrapper::State::kAborted);
+            StreamWrapper::State::kClosed);
 }
 
 }  // namespace
