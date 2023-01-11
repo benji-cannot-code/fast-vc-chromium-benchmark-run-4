f05a2f432ce1ff81d13b82a3dd1ae74a26238819
Scott Violet <sky@chromium.org>
2023-01-11 22:13:45 +0000

    [reland] Enable CombinedNavigationOptimizations2 features by default
    This is a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/4103922
    
    As this patch landed almost a month ago I did not create a revert.
    The only change is to disable an additional test
    (WebFrameTest.ChangeResourcePriority) which is what led to the
    revert.
    
    Original description:
    
    Enable CombinedNavigationOptimizations2 features by default
    
    This will be launching soon, so enabling everything by default.
    
    Bug: 1355598
    Change-Id: I3cdc4c5d0bb9e353fe19e39434a7b513d6cc4222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4152859
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091479}

--- third_party/blink/renderer/core/frame/web_frame_test.cc
+++ third_party/blink/renderer/core/frame/web_frame_test.cc
@@ -9608,7 +9608,9 @@ TEST_F(WebFrameSwapTest, AdHighlightEarlyApply) {
             SkColorSetARGB(128, 255, 0, 0));
 }
 
-TEST_F(WebFrameSwapTest, DoNotPropagateDisplayNonePropertyOnSwap) {
+// TODO(crbug.com/1314493): This test is flaky with the TimedHTMLParserBudget
+// feature enabled.
+TEST_F(WebFrameSwapTest, DISABLED_DoNotPropagateDisplayNonePropertyOnSwap) {
   WebFrameSwapTestClient* main_frame_client =
       static_cast<WebFrameSwapTestClient*>(MainFrame()->Client());
   EXPECT_FALSE(main_frame_client->DidPropagateDisplayNoneProperty());
@@ -11899,7 +11901,9 @@ class TestResourcePriorityWebFrameClient
   HashMap<KURL, std::unique_ptr<ExpectedRequest>> expected_requests_;
 };
 
-TEST_F(WebFrameTest, ChangeResourcePriority) {
+// TODO(crbug.com/1314493): This test is flaky with the TimedHTMLParserBudget
+// feature enabled.
+TEST_F(WebFrameTest, DISABLED_ChangeResourcePriority) {
   TestResourcePriorityWebFrameClient client;
   RegisterMockedHttpURLLoad("promote_img_in_viewport_priority.html");
   RegisterMockedHttpURLLoad("image_slow.pl");
