bc53c474973d9053d1be35ca26dd99704f6f9306
Will Harris <wfh@chromium.org>
2023-01-11 07:47:58 +0000

    Remove expiring NetworkService.GrantSandboxToCacheResult histogram
    This histogram is no longer useful to keep around, so remove it.
    
    Bug: 1405972
    Fixed: 1405972
    
    Change-Id: I25376948a145c5fe8acb4d1dad491f3cdabe548d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4147440
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091208}

--- content/browser/network_service_browsertest.cc
+++ content/browser/network_service_browsertest.cc
@@ -1085,9 +1085,6 @@ void MigrationTestInternal(const base::FilePath& tempdir_one,
       EXPECT_FALSE(base::PathExists(
           tempdir_two.Append(kNetworkSubpath).Append(kCheckpointFileName)));
       histogram_tester.ExpectUniqueSample(
-          "NetworkService.GrantSandboxToCacheResult", /*sample=kSuccess=*/0,
-          /*expected_bucket_count=*/1);
-      histogram_tester.ExpectUniqueSample(
           "NetworkService.GrantSandboxResult",
           /*sample=kFailedToCreateDataDirectory=*/2,
           /*expected_bucket_count=*/1);
@@ -1137,10 +1134,6 @@ void MigrationTestInternal(const base::FilePath& tempdir_one,
       break;
 #endif  // BUILDFLAG(IS_WIN)
     case FailureType::kCacheDirIsAFile:
-      histogram_tester.ExpectUniqueSample(
-          "NetworkService.GrantSandboxToCacheResult",
-          /*sample=kFailedToCreateCacheDirectory=*/1,
-          /*expected_bucket_count=*/1);
       histogram_tester.ExpectUniqueSample("NetworkService.GrantSandboxResult",
                                           /*sample=kSuccess=*/0,
                                           /*expected_bucket_count=*/1);
