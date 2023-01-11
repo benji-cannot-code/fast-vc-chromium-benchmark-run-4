0b4ca386dad2a94232200a9c5f50feb499ed376f
Kevin McNee <mcnee@chromium.org>
2023-01-11 22:52:04 +0000

    Ship speculation rules referrer policy key
    Intent to Ship:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/u6V9hMIsTiU/m/43FtZAwNAAAJ
    
    Bug: 1355146
    Change-Id: Iaf38512bf6f6165ad22c4ccfb5872003992ddcc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4157933
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091543}

--- third_party/blink/renderer/core/speculation_rules/speculation_rule_set_test.cc
+++ third_party/blink/renderer/core/speculation_rules/speculation_rule_set_test.cc
@@ -346,9 +346,6 @@ TEST_F(SpeculationRuleSetTest, IgnoresUnknownOrDifferentlyTypedTopLevelKeys) {
 }
 
 TEST_F(SpeculationRuleSetTest, DropUnrecognizedRules) {
-  ScopedSpeculationRulesReferrerPolicyKeyForTest enable_referrer_policy_key{
-      true};
-
   auto* rule_set = CreateRuleSet(
       R"({"prefetch": [)"
 
@@ -502,9 +499,6 @@ TEST_F(SpeculationRuleSetTest, RulesWithTargetHint_CaseInsensitive) {
 }
 
 TEST_F(SpeculationRuleSetTest, ReferrerPolicy) {
-  ScopedSpeculationRulesReferrerPolicyKeyForTest enable_referrer_policy_key{
-      true};
-
   auto* rule_set =
       CreateRuleSet(R"({
         "prefetch": [{
@@ -528,9 +522,6 @@ TEST_F(SpeculationRuleSetTest, ReferrerPolicy) {
 }
 
 TEST_F(SpeculationRuleSetTest, EmptyReferrerPolicy) {
-  ScopedSpeculationRulesReferrerPolicyKeyForTest enable_referrer_policy_key{
-      true};
-
   // If an empty string is used for referrer_policy, treat this as if the key
   // were omitted.
   auto* rule_set = CreateRuleSet(
@@ -1924,9 +1915,6 @@ TEST_F(DocumentRulesTest, DisconnectedLinkInShadowTree) {
 
 // Tests that a document rule's specified referrer policy is used.
 TEST_F(DocumentRulesTest, ReferrerPolicy) {
-  ScopedSpeculationRulesReferrerPolicyKeyForTest enable_referrer_policy_key{
-      true};
-
   DummyPageHolder page_holder;
   StubSpeculationHost speculation_host;
   Document& document = page_holder.GetDocument();
@@ -1955,12 +1943,6 @@ TEST_F(DocumentRulesTest, ReferrerPolicy) {
 // Tests that a link's referrer-policy value is used if one is not specified
 // in the document rule.
 TEST_F(DocumentRulesTest, LinkReferrerPolicy) {
-  // This test does not use the "referrer_policy" key itself. This is used to
-  // disable a temporary workaround related to the use of a lax policy. See
-  // https://crbug.com/1398772.
-  ScopedSpeculationRulesReferrerPolicyKeyForTest enable_referrer_policy_key{
-      true};
-
   DummyPageHolder page_holder;
   StubSpeculationHost speculation_host;
   Document& document = page_holder.GetDocument();
