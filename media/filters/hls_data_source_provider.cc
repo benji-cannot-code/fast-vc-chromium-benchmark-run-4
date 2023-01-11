44aedb1556d0445e695c7180d74a7bacc7725d39
Will Cassella <cassew@chromium.org>
2023-01-11 00:29:31 +0000

    HLS: Add HlsDataSource and HlsDataSourceProvider
    This CL implements a utility interface of HlsDemuxer which allows it
    to make requests through blink for additional data sources (which may
    be media playlists or media segments), since its otherwise incapable
    of doing so within `//media`.
    
    Bug: 1266991
    Change-Id: I940d3ee6dcda280a4bdb24f085d55dd2ed0ea16b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950067
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091098}

---
+++ media/filters/hls_data_source_provider.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/filters/hls_data_source_provider.h"
+
+namespace media {
+
+HlsDataSource::~HlsDataSource() = default;
+
+HlsDataSourceProvider::~HlsDataSourceProvider() = default;
+
+}  // namespace media
