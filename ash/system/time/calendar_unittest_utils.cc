7c2881af9f4d0749b8d55281c7bd8850cc56cd59
Sam Cackett <samcackett@google.com>
2023-01-11 10:44:23 +0000

    Make timezones agnostic in calendar unit tests
    Add a fix for an issue with base::Time using the execution environment timezone and ignoring the one set in the test
    
    Low-Coverage-Reason:Helper class added to test utils file
    
    Bug: b:264359595
    Change-Id: I90e6c1cc6fc576261e3cd16c08ceb239d2f1e4d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144910
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091251}

--- ash/system/time/calendar_unittest_utils.cc
+++ ash/system/time/calendar_unittest_utils.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/ash_export.h"
+#include "base/environment.h"
 #include "base/i18n/time_formatting.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
@@ -16,6 +17,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 namespace calendar_test_utils {
+
+ScopedLibcTimeZone::ScopedLibcTimeZone(const std::string& timezone) {
+  auto env = base::Environment::Create();
+  std::string old_timezone_value;
+  if (env->GetVar(kTimeZoneEnvVarName, &old_timezone_value)) {
+    old_timezone_ = old_timezone_value;
+  }
+  if (!env->SetVar(kTimeZoneEnvVarName, timezone)) {
+    success_ = false;
+  }
+  tzset();
+}
+
+ScopedLibcTimeZone::~ScopedLibcTimeZone() {
+  auto env = base::Environment::Create();
+  if (old_timezone_.has_value()) {
+    CHECK(env->SetVar(kTimeZoneEnvVarName, old_timezone_.value()));
+  } else {
+    CHECK(env->UnSetVar(kTimeZoneEnvVarName));
+  }
+}
 
 std::unique_ptr<google_apis::calendar::CalendarEvent> CreateEvent(
     const char* id,
