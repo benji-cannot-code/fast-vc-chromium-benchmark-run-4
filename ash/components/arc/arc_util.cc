9efa9100b6d4ea19fc373ce56269db15ce8bbdd9
Yury Khmel <khmel@chromium.org>
2023-01-12 19:56:18 +0000

    arc: Add host ureadahead generation flag.
    This introduces new flag that indicate session with active host
    ureadahead generation. That sets ARC session in order not to run
    perfetto services that affect ureadahead generation.
    
    BUG=b:263415559
    TEST=Locally with all CL, unit tests
    
    Change-Id: Ic8588b4cff65af59a8c3fb700b5b0617d3eaa846
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133513
    Reviewed-by: Alan Ding <alanding@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092000}

--- ash/components/arc/arc_util.cc
+++ ash/components/arc/arc_util.cc
@@ -158,6 +158,11 @@ bool IsUreadaheadDisabled() {
       ash::switches::kArcDisableUreadahead);
 }
 
+bool IsHostUreadaheadGeneration() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      ash::switches::kArcHostUreadaheadGeneration);
+}
+
 ArcVmUreadaheadMode GetArcVmUreadaheadMode() {
   ArcVmUreadaheadMode mode = IsUreadaheadDisabled()
                                  ? ArcVmUreadaheadMode::DISABLED
