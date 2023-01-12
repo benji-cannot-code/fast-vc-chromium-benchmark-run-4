43154bb9dd75aa5d6d0116ff29180e0b765bb5c8
Sijin Soon <sijins@google.com>
2023-01-12 02:13:09 +0000

    Add ability to clear all GIFs from the recently used category.
    Bug: b:264959847
    Change-Id: I084211cbba7b08dd2a1a8b429fdae93cb8d1e409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4150844
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Sijin Soon <sijins@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091637}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_group.ts
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_group.ts
@@ -371,6 +371,10 @@ export class EmojiGroupComponent extends PolymerElement {
   getIndex(item: EmojiVariants): number {
     return this.data.indexOf(item);
   }
+
+  formatCategory(category: CategoryEnum): string {
+    return category === CategoryEnum.GIF ? 'GIF' : category;
+  }
 }
 
 declare global {
