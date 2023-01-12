810797dd915ff25d9b51b29cd39dd6e92ad10da5
James Forshaw <forshaw@chromium.org>
2023-01-12 01:57:47 +0000

    [Windows] Cleanup AppContainer profile methods.
    This CL cleans up the AppContainer class methods to take advantage of
    Sid factory methods which no longer fail. It also adds tests to verify
    the list of capabilities added to specific sandbox profiles.
    
    Bug: 1394854
    Change-Id: If1debdabfbebc889dcf881208526f16f8a15d849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149433
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091630}

--- content/browser/ppapi_plugin_sandboxed_process_launcher_delegate_unittest.cc
+++ content/browser/ppapi_plugin_sandboxed_process_launcher_delegate_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include "base/win/windows_version.h"
 #include "sandbox/policy/win/sandbox_policy_feature_test.h"
-#include "sandbox/policy/win/sandbox_test_utils.h"
 #include "sandbox/policy/win/sandbox_win.h"
 #include "sandbox/win/src/app_container_base.h"
 #include "sandbox/win/src/sandbox_factory.h"
--- sandbox/policy/win/sandbox_policy_feature_test.h
+++ sandbox/policy/win/sandbox_policy_feature_test.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 #if BUILDFLAG(IS_WIN)
-#include "sandbox/policy/win/sandbox_test_utils.h"
 #include "sandbox/policy/win/sandbox_win.h"
 #include "sandbox/win/src/app_container_base.h"
 #include "sandbox/win/src/sandbox_factory.h"
