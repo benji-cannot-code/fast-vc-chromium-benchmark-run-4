cb5d24032f1d6fc903b38a76a6b700888297760e
Fredrik Söderquist <fs@opera.com>
2023-01-12 09:39:41 +0000

    Add ViewportDimension() helper to SVGLengthContext
    For resolving the viewport for the context element and computing the
    right value based on the length mode.
    
    Not having a viewport (element) is an error condition, and shouldn't
    need to get preferential treatment, so just map it to zero in those
    cases and then handle/use that value.
    
    This leaves the DimensionForLengthMode() helper with a single caller,
    so fold it.
    
    Bug: 1286904
    Change-Id: I330460149340725d26244c76726e914864bf0df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4156912
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091744}

--- third_party/blink/renderer/core/svg/svg_length_context.h
+++ third_party/blink/renderer/core/svg/svg_length_context.h
@@ -97,6 +97,7 @@ class CORE_EXPORT SVGLengthContext {
   float ResolveValue(const CSSPrimitiveValue&, SVGLengthMode) const;
 
  private:
+  float ViewportDimension(SVGLengthMode) const;
   float ValueForLength(const Length&, float zoom, SVGLengthMode) const;
   static float ValueForLength(const Length&, float zoom, float dimension);
 
