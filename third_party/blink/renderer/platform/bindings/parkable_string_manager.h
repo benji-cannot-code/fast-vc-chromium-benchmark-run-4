fafa8a6bfe24c3324a12567b20fb2db6663e4e5a
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2023-01-12 03:21:03 +0000

    Improve ParkableStrings thread-safety.
    The V8 team has been working on improving and introducing multithreading features to JavaScript. A requirement is to make V8 strings thread-safe, this includes externalized strings which can be backed by blink ParkableStrings, so we need to improve their thread-safety too.
    
    This CL:
    - Adds an atomic ref count to ParkableStringImpl and guarantees that the counter can only reach 0 in the main thread. Hence, changes to the ParkableStringManager only happen in the main thread.
    - Allows unparking to happen in background threads, but updates to the ParkableStringManager are posted as tasks for the main thread.
    - Adds tests to cover the new behaviors.
    
    Bug: 1384165
    Change-Id: I5240957d06f4d7567e7edf100d31a50e8b2d5a08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900465
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Commit-Queue: Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091667}

--- third_party/blink/renderer/platform/bindings/parkable_string_manager.h
+++ third_party/blink/renderer/platform/bindings/parkable_string_manager.h
@@ -50,6 +50,11 @@ class PLATFORM_EXPORT ParkableStringManagerDumpProvider
 // Manages all the ParkableStrings, and parks eligible strings after the
 // renderer has been backgrounded.
 // Main Thread only.
+// When a `ParkableString` is unparked on a background thread, a task is posted
+// to the main thread to update the entries in the manager. Hence, it is
+// possible to temporarily have an unparked `ParkableString` inaccessible
+// through `unparked_strings_`. This can cause aging of the string to be
+// delayed or a variation on the sizes recorded in 'ComputeStatistics()`.
 class PLATFORM_EXPORT ParkableStringManager {
   USING_FAST_MALLOC(ParkableStringManager);
 
@@ -92,6 +97,9 @@ class PLATFORM_EXPORT ParkableStringManager {
                                  ParkableStringImpl*,
                                  SecureDigestHash>;
 
+  bool IsOnParkedMapForTesting(ParkableStringImpl* string);
+  bool IsOnDiskMapForTesting(ParkableStringImpl* string);
+
  private:
   friend class ParkableString;
   friend class ParkableStringImpl;
@@ -99,12 +107,25 @@ class PLATFORM_EXPORT ParkableStringManager {
   scoped_refptr<ParkableStringImpl> Add(
       scoped_refptr<StringImpl>&&,
       std::unique_ptr<ParkableStringImpl::SecureDigest> digest);
-  void Remove(ParkableStringImpl*);
+
+  void RemoveOnMainThread(MayBeDangling<ParkableStringImpl> string);
+  // If on a background thread, posts a `RemoveOnMainThread` task to the Main
+  // thread. Calls `RemoveOnMainThread` otherwise.
+  void Remove(ParkableStringImpl* string);
 
   void OnParked(ParkableStringImpl*);
   void OnWrittenToDisk(ParkableStringImpl*);
-  void OnReadFromDisk(ParkableStringImpl*);
-  void OnUnparked(ParkableStringImpl*);
+  void OnUnparked(ParkableStringImpl*, bool);
+
+  // If on a background thread, posts a `CompleteUnparkOnMainThread` task to
+  // the Main thread. Calls `CompleteUnparkOnMainThread` otherwise.
+  void CompleteUnpark(ParkableStringImpl* string,
+                      base::TimeDelta elapsed,
+                      base::TimeDelta disk_elapsed);
+
+  void CompleteUnparkOnMainThread(ParkableStringImpl* string,
+                                  base::TimeDelta elapsed,
+                                  base::TimeDelta disk_elapsed);
 
   void ParkAll(ParkableStringImpl::ParkingMode mode);
   void RecordStatisticsAfter5Minutes() const;
@@ -147,6 +168,7 @@ class PLATFORM_EXPORT ParkableStringManager {
     return task_runner_;
   }
 
+  void AssertRemoved(ParkableStringImpl* string);
   void ResetForTesting();
   ParkableStringManager();
 
