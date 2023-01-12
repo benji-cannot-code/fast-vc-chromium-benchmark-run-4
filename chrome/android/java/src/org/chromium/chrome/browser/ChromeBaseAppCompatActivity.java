28c3781b2ecc06d61dacc94ef7a0ed6eb63f4728
Sinan Sahin <sinansahin@google.com>
2023-01-12 02:07:49 +0000

    [Fixit] Fix Recents icon
    The app icon in Recents was appearing with an outline because we were
    calling Actvity#setTaskDescription() to fix an issue where the Recents
    item header in older versions of Android would have a colorPrimary
    background. The reason was that we were providing R.mipmap.app_icon with
    the call, which resolves to the app icon that should only be used
    below API 26.
    
    This CL passes null for values in the TaskDescription that we don't
    intend to change.
    
    Bug: 1220734
    Change-Id: I4b3567b85019e0a65ae83699e319c5d1066d2d8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159812
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091635}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
@@ -11,8 +11,6 @@ import android.app.ActivityManager.TaskDescription;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -258,11 +256,8 @@ public class ChromeBaseAppCompatActivity extends AppCompatActivity
      * Sets the default task description that will appear in the recents UI.
      */
     protected void setDefaultTaskDescription() {
-        final Resources res = getResources();
         final TaskDescription taskDescription =
-                new TaskDescription(res.getString(R.string.app_name),
-                        BitmapFactory.decodeResource(res, R.mipmap.app_icon),
-                        res.getColor(R.color.default_task_description_color));
+                new TaskDescription(null, null, getColor(R.color.default_task_description_color));
         setTaskDescription(taskDescription);
     }
 
