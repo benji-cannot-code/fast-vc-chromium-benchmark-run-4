76464002ec77aa3a6b9174b682c45c7ddd551f17
Ian Barkley-Yeung <iby@chromium.org>
2023-01-12 09:13:36 +0000

    Allow some Tracing unit tests to test ChromeOS
    Update
    tracing::TracingSamplerProfiler::IsStackUnwindingSupportedForTesting()
    to possibly return true on ChromeOS once stack profiling is turned on
    again in base::StackSamplingProfiler::IsSupportedForCurrentPlatform().
    
    This has no immediate effect since
    base::StackSamplingProfiler::IsSupportedForCurrentPlatform() currently
    returns false on ChromeOS, but this change will ensure that when it is
    reactivated, the unit tests act appropriately.
    
    Also allowed Linux to follow
    base::StackSamplingProfiler::IsSupportedForCurrentPlatform() in case
    that ever supports stack profiling.
    
    BUG=b:214117401,chromium:1324337
    
    Change-Id: I2ad76567f7fa0279fb196ed96c01514f73f29024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074584
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Auto-Submit: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091736}

--- services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.cc
+++ services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.cc
@@ -374,8 +374,9 @@ struct FrameDetails {
 #endif
 };
 
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) && defined(_WIN64) || \
-    ANDROID_ARM64_UNWINDING_SUPPORTED || ANDROID_CFI_UNWINDING_SUPPORTED
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) && defined(_WIN64) ||            \
+    ANDROID_ARM64_UNWINDING_SUPPORTED || ANDROID_CFI_UNWINDING_SUPPORTED || \
+    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 // Returns whether stack sampling is supported on the current platform.
 bool IsStackSamplingSupported() {
   return base::StackSamplingProfiler::IsSupportedForCurrentPlatform();
@@ -802,8 +803,9 @@ void TracingSamplerProfiler::RegisterDataSource() {
 
 // static
 bool TracingSamplerProfiler::IsStackUnwindingSupportedForTesting() {
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) && defined(_WIN64) || \
-    ANDROID_ARM64_UNWINDING_SUPPORTED || ANDROID_CFI_UNWINDING_SUPPORTED
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) && defined(_WIN64) ||            \
+    ANDROID_ARM64_UNWINDING_SUPPORTED || ANDROID_CFI_UNWINDING_SUPPORTED || \
+    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
   return IsStackSamplingSupported();
 #else
   return false;
