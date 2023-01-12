63e5cb8d9ed6862f115cdfdae62c24c6791ba667
Paul Adedeji <pauladedeji@google.com>
2023-01-12 22:28:31 +0000

    [ntp] Add metric that tracks when Customize Chrome button is clicked
    This metric is part of the launch requirements for Panorama.
    
    Change-Id: I957d8bec89b1cf128c755bd373afc04d3ccb31d4
    Bug: 1384258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149809
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1092069}

--- chrome/test/data/webui/new_tab_page/app_test.ts
+++ chrome/test/data/webui/new_tab_page/app_test.ts
@@ -488,6 +488,7 @@ suite('NewTabPageAppTest', () => {
       ['cr-most-visited', NtpElement.MOST_VISITED],
       ['ntp-middle-slot-promo', NtpElement.MIDDLE_SLOT_PROMO],
       ['ntp-modules', NtpElement.MODULE],
+      ['#customizeButton', NtpElement.CUSTOMIZE_BUTTON],
     ] as Array<[string, NtpElement]>)
         .forEach(([selector, element]) => {
           test(`clicking '${selector}' records click`, () => {
@@ -508,7 +509,10 @@ suite('NewTabPageAppTest', () => {
 
       // Assert.
       assertEquals(2, metrics.count('NewTabPage.Click'));
-      assertEquals(2, metrics.count('NewTabPage.Click', NtpElement.CUSTOMIZE));
+      assertEquals(
+          1, metrics.count('NewTabPage.Click', NtpElement.CUSTOMIZE_BUTTON));
+      assertEquals(
+          1, metrics.count('NewTabPage.Click', NtpElement.CUSTOMIZE_DIALOG));
     });
 
     test('clicking OGB records click', () => {
