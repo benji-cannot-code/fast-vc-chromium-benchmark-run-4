860ca0ee827d518a6a5e51345b8afb960c029b96
Morten Stenshorne <mstensho@chromium.org>
2023-01-12 20:46:32 +0000

    Include shadow offsets when clipping fragmented shadows.
    Box shadows were sometimes missing when fragmented, because we didn't
    handle shadow offsets (the first two length values in 'box-shadow')
    correctly. It worked fine-ish in many cases when blur was involved,
    though, due to the fuzz by which we expand the clip rectangles in such
    cases.
    
    Bug: 1402736
    Change-Id: Ife8a5ea00cc7492068ae90c21f322e76496a8ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161432
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092032}

--- third_party/blink/renderer/core/layout/geometry/box_sides.h
+++ third_party/blink/renderer/core/layout/geometry/box_sides.h
@@ -78,6 +78,7 @@ struct PhysicalBoxSides {
   }
 
   bool IsEmpty() const { return !top && !right && !bottom && !left; }
+  bool HasAllSides() const { return top && right && bottom && left; }
 };
 
 }  // namespace blink
