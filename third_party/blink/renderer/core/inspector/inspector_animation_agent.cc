5d77881bf62d2d94845875d9bdc65e3cc24268c3
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-01-12 21:48:50 +0000

    Revert "[view-timeline]: Update naming for delay as timeline offset"
    This reverts commit 8c67ecf3a1d46af987a2097bfa685fdc00937b43.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8792142657514411521
    
    Sample failed build: https://ci.chromium.org/b/8792142657514411521
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8792142657514411521&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4081107
    
    Original change's description:
    > [view-timeline]: Update naming for delay as timeline offset
    >
    > Patch implements rename based on most recent recommendation in
    > https://github.com/w3c/csswg-drafts/issues/7589.
    >
    > Next steps:
    > * CSS support for animation-range.
    > * Support for % delays.
    > * Support for range(Start|End) as string in animation effect timing options.
    > * Removal of <name> <percent> from animation-delay.
    > * Support range(Start|End) as CSSNumericValue (offset only with implied 'cover' as named range).
    >
    > Bug: 1383162
    > Change-Id: I9d2e9572aac31c79fb937514c1f65e6940a9e27e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081107
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Kevin Ellis <kevers@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1092031}
    >
    
    Bug: 1383162
    Change-Id: Id7c8f3ca84675b454d14a18a45aa1895c87d303f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4162958
    Commit-Queue: Jonathan Njeunje <njeunje@chromium.org>
    Reviewed-by: Jonathan Njeunje <njeunje@chromium.org>
    Owners-Override: Jonathan Njeunje <njeunje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092052}

--- third_party/blink/renderer/core/inspector/inspector_animation_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_animation_agent.cc
@@ -43,7 +43,7 @@ namespace blink {
 
 namespace {
 
-double AsDoubleOrZero(Timing::V8Delay* value) {
+double AsDoubleOrZero(V8UnionDoubleOrTimelineOffset* value) {
   if (!value->IsDouble())
     return 0;
 
@@ -406,7 +406,7 @@ Response InspectorAnimationAgent::setTiming(const String& animation_id,
   timing->setDuration(
       MakeGarbageCollected<V8UnionCSSNumericValueOrStringOrUnrestrictedDouble>(
           duration));
-  timing->setDelay(MakeGarbageCollected<Timing::V8Delay>(delay));
+  timing->setDelay(MakeGarbageCollected<V8UnionDoubleOrTimelineOffset>(delay));
   animation->effect()->updateTiming(timing, exception_state);
   return Response::Success();
 }
