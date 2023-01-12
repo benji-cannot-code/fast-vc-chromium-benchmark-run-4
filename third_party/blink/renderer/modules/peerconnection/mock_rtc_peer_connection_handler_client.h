d17d35c07fdf8772c0d71f52b4c44c5155b9e62d
Tsuyoshi Horo <horo@chromium.org>
2023-01-12 05:39:31 +0000

    Revert "Make PeerConnectionHandler have a Persistent client link"
    This reverts commit 4732044f24e366373a8d30a37a98226320592d58.
    
    Reason for revert: Caused failure on WebKit Linux Leak bot. See crbug.com/1406723
    
    Bug: 1406723
    
    Original change's description:
    > Make PeerConnectionHandler have a Persistent client link
    >
    > This prevents garbage collection of RTCPeerConnection while
    > an RTCPeerConnectionHandler is active. The handler is passivated
    > when disconnected.
    >
    > Bug: chromium:1405256
    > Change-Id: I317d203321698a8f8145565085c9a16747dfb129
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4154998
    > Commit-Queue: Harald Alvestrand <hta@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1091533}
    
    Bug: chromium:1405256
    Change-Id: I57b2a7af5bc33cf4d807d612febf755e7d6af6da
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161131
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091694}

--- third_party/blink/renderer/modules/peerconnection/mock_rtc_peer_connection_handler_client.h
+++ third_party/blink/renderer/modules/peerconnection/mock_rtc_peer_connection_handler_client.h
@@ -19,8 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class MockRTCPeerConnectionHandlerClient
-    : public GarbageCollected<MockRTCPeerConnectionHandlerClient>,
-      public RTCPeerConnectionHandlerClient {
+    : public RTCPeerConnectionHandlerClient {
  public:
   MockRTCPeerConnectionHandlerClient();
 
--- third_party/blink/renderer/platform/peerconnection/rtc_peer_connection_handler_client.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_peer_connection_handler_client.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -57,8 +56,7 @@ struct PLATFORM_EXPORT WebRTCSctpTransportSnapshot {
       webrtc::DtlsTransportInformation(webrtc::DtlsTransportState::kNew);
 };
 
-class PLATFORM_EXPORT RTCPeerConnectionHandlerClient
-    : public GarbageCollectedMixin {
+class PLATFORM_EXPORT RTCPeerConnectionHandlerClient {
  public:
   virtual ~RTCPeerConnectionHandlerClient();
 
