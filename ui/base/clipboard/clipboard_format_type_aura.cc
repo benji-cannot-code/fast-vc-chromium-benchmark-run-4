dcb9d49fa60f179e5c3b546665c94e920ca6d2a5
Avi Drissman <avi@chromium.org>
2023-01-12 17:59:32 +0000

    Change web custom format pasteboard types
    Pasteboard types need to be reverse-DNS names. The specification is
    from the W3C, so "org.w3" should be used.
    
    Do minor documentation and implementation cleanup as well.
    
    Bug: https://github.com/w3c/editing/issues/412
    Bug: https://github.com/w3c/editing/pull/416
    Change-Id: Idefd0a9bcaccaef199153aab7d9a8c318a45ee51
    Fixed: 1401040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108170
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1091921}

--- ui/base/clipboard/clipboard_format_type_aura.cc
+++ ui/base/clipboard/clipboard_format_type_aura.cc
@@ -61,14 +61,9 @@ std::string ClipboardFormatType::WebCustomFormatName(int index) {
 }
 
 // static
-std::string ClipboardFormatType::WebCustomFormatMapName() {
-  return "application/web;type=\"custom/formatmap\"";
-}
-
-// static
 const ClipboardFormatType& ClipboardFormatType::WebCustomFormatMap() {
   static base::NoDestructor<ClipboardFormatType> type(
-      ClipboardFormatType::WebCustomFormatMapName());
+      "application/web;type=\"custom/formatmap\"");
   return *type;
 }
 
