67f406985b43ecbbfb3d9bedd8e6c2fca97915fd
Hidehiko Abe <hidehiko@chromium.org>
2023-01-12 17:22:40 +0000

    Remove non-overriding methods from ProfileHelperImpl.
    There are four methods that are no longer needed:
    - GetProfileDir(): just unused. Removed.
    - IsSigninProfileInitialized(), GetSigninProfile(),
      GetLockScreenProfile(): they can be just inlined in static methods
      of ProfileHelper implementation.
    
    BUG=1325210
    TEST=Tryjob
    
    Change-Id: Ib2f90d00244f521e156c7870d685c101fc5d7130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4160260
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091899}

--- chrome/browser/ash/profiles/profile_helper.cc
+++ chrome/browser/ash/profiles/profile_helper.cc
@@ -61,19 +61,6 @@ class ProfileHelperImpl : public ProfileHelper {
       std::unique_ptr<BrowserContextHelper::Delegate> delegate);
   ~ProfileHelperImpl() override;
 
-  // Returns the path that corresponds to the passed profile.
-  base::FilePath GetProfileDir(base::StringPiece profile);
-
-  // Returns true if the signin profile has been initialized.
-  bool IsSigninProfileInitialized();
-
-  // Returns OffTheRecord profile for use during signing phase.
-  Profile* GetSigninProfile();
-
-  // Returns OffTheRecord profile for use during online authentication on the
-  // lock screen.
-  Profile* GetLockScreenProfile();
-
   // ProfileHelper overrides
   base::FilePath GetActiveUserProfileDir() override;
   void Initialize() override;
@@ -110,17 +97,6 @@ class ProfileHelperImpl : public ProfileHelper {
 
   std::unique_ptr<FileFlusher> profile_flusher_;
 };
-
-namespace {
-// Convenient utility to obtain ProfileHelperImpl.
-// Currently ProfileHelper interface is implemented by only ProfileHelperImpl,
-// so safe to cast.
-// TODO(crbug.com/1325210): Remove this after ProfileHelper is moved out from
-// chrome/browser.
-ProfileHelperImpl* GetImpl() {
-  return static_cast<ProfileHelperImpl*>(ProfileHelper::Get());
-}
-}  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 // ProfileHelper, public
@@ -159,7 +135,8 @@ base::FilePath ProfileHelper::GetSigninProfileDir() {
 
 // static
 Profile* ProfileHelper::GetSigninProfile() {
-  return GetImpl()->GetSigninProfile();
+  return Profile::FromBrowserContext(
+      BrowserContextHelper::Get()->DeprecatedGetOrCreateSigninBrowserContext());
 }
 
 // static
@@ -182,7 +159,7 @@ bool ProfileHelper::IsSigninProfile(const Profile* profile) {
 
 // static
 bool ProfileHelper::IsSigninProfileInitialized() {
-  return GetImpl()->IsSigninProfileInitialized();
+  return BrowserContextHelper::Get()->GetSigninBrowserContext();
 }
 
 // static
@@ -202,7 +179,8 @@ base::FilePath ProfileHelper::GetLockScreenProfileDir() {
 
 // static
 Profile* ProfileHelper::GetLockScreenProfile() {
-  return GetImpl()->GetLockScreenProfile();
+  return Profile::FromBrowserContext(
+      BrowserContextHelper::Get()->GetLockScreenBrowserContext());
 }
 
 // static
@@ -285,28 +263,6 @@ ProfileHelperImpl::ProfileHelperImpl(
           std::make_unique<BrowserContextHelper>(std::move(delegate))) {}
 
 ProfileHelperImpl::~ProfileHelperImpl() = default;
-
-base::FilePath ProfileHelperImpl::GetProfileDir(base::StringPiece profile) {
-  const base::FilePath* user_data_dir =
-      browser_context_helper_->delegate()->GetUserDataDir();
-  if (!user_data_dir)
-    return base::FilePath();
-  return user_data_dir->AppendASCII(profile);
-}
-
-bool ProfileHelperImpl::IsSigninProfileInitialized() {
-  return browser_context_helper_->GetSigninBrowserContext();
-}
-
-Profile* ProfileHelperImpl::GetSigninProfile() {
-  return Profile::FromBrowserContext(
-      browser_context_helper_->DeprecatedGetOrCreateSigninBrowserContext());
-}
-
-Profile* ProfileHelperImpl::GetLockScreenProfile() {
-  return Profile::FromBrowserContext(
-      browser_context_helper_->GetLockScreenBrowserContext());
-}
 
 base::FilePath ProfileHelperImpl::GetActiveUserProfileDir() {
   return ProfileHelper::GetUserProfileDir(active_user_id_hash_);
