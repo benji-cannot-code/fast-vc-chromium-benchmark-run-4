11604163bf17b8757731b21758a463e25d271e56
Josiah Krutz <josiahk@google.com>
2023-01-12 17:54:12 +0000

    [ChromeVox] Replace chrome.storage.local with LocalStorage
    Replace last few instances of chrome.storage.local with new
    LocalStorage class. [1] This change is a pure refactor.
    
    [1] https://crrev.com/c/3902624
    
    AX-Relnotes: N/A.
    Bug: b/260589686
    Test: CQ
    Change-Id: I4e6ee48e4e619f53dd3e26cbea01721c1730d0fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159653
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091920}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/tts_background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/tts_background.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * extension API.
  */
 
+import {LocalStorage} from '../../common/local_storage.js';
 import {BridgeConstants} from '../common/bridge_constants.js';
 import {BridgeHelper} from '../common/bridge_helper.js';
 import {Msgs} from '../common/msgs.js';
@@ -78,7 +79,7 @@ export class TtsBackground {
     chrome.settingsPrivate.setPref('settings.tts.speech_rate', rate);
     chrome.settingsPrivate.setPref('settings.tts.speech_pitch', pitch);
     chrome.settingsPrivate.setPref('settings.tts.speech_volume', volume);
-    chrome.storage.local.remove('voiceName');
+    LocalStorage.remove('voiceName');
     TtsBackground.primary.updateVoice('', () => {
       // Ensure this announcement doesn't get cut off by speech triggered by
       // updateFromPrefs_().
