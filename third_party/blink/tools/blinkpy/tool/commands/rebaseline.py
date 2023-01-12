5ae7e16904c8504a6cbae00a6f6e6953283d46bf
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-01-12 22:46:21 +0000

    Remove disable-layout-ng bot, and rebaseline tool logic.
    This removes the disable-layout-ng CQ bot, and the associated blink
    rebaseline tool logic.
    
    Bug: 1229581
    Change-Id: I3484725bc7af786657a8e4f3d99aefcd90415fa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4152833
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092082}

--- third_party/blink/tools/blinkpy/tool/commands/rebaseline.py
+++ third_party/blink/tools/blinkpy/tool/commands/rebaseline.py
@@ -113,7 +113,7 @@ class AbstractRebaseliningCommand(Command):
         '--flag-specific',
         # TODO(crbug/1291020): build the list from builders.json
         choices=[
-            "disable-layout-ng", "disable-site-isolation-trials", "highdpi",
+            "disable-site-isolation-trials", "highdpi",
             "skia-vulkan-swiftshader"
         ],
         default=None,
