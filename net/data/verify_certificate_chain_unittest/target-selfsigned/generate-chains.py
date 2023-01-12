414d10b6d4320a0e3fb2d44ad15d3b67bb1664fc
Matt Mueller <mattm@chromium.org>
2023-01-12 00:37:40 +0000

    Add VerifyCertificateIsSelfSigned, make X509Certificate::IsSelfSigned use it
    Change-Id: I16bc0e8918f7b1048e2a9bb5572821427f66fad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4156538
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091599}

---
+++ net/data/verify_certificate_chain_unittest/target-selfsigned/generate-chains.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Single certificate chain for serverAuth which is self-signed."""
+
+import sys
+sys.path += ['../..']
+
+import gencerts
+
+# Self-signed target certificate.
+target = gencerts.create_self_signed_end_entity_certificate('Target')
+target.get_extensions().set_property('extendedKeyUsage', 'serverAuth')
+
+chain = [target]
+gencerts.write_chain(__doc__, chain, 'chain.pem')
