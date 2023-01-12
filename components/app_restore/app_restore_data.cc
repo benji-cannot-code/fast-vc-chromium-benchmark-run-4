214606489d4890768ff3c97ff46066079a9b7a10
Nancy Wang <nancylingwang@chromium.org>
2023-01-12 23:29:31 +0000

    Remove the mojom intent filter in intent_filter_util.
    The mojom file types.mojom.h is removed from some files, which causes
    some build issues:
    1. base::OnceCallback is not defined in application_launch.h
    2. DVLOG is not defined in app_restore_data.cc and restore_data.cc,
    preferred_apps_converter.cc.
    
    Modify application_launch.h to add the header file for OnceCallback.
    
    Modify app_restore_data.cc and restore_data.cc,
    preferred_apps_converter.cc to add the header file for DVLOG.
    
    BUG=1253250
    
    Change-Id: I062ca5f1e6aa88158a6c83894bb90e61b20a0122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159663
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092108}

--- components/app_restore/app_restore_data.cc
+++ components/app_restore/app_restore_data.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "components/app_restore/app_launch_info.h"
--- components/services/app_service/public/cpp/preferred_apps_converter.cc
+++ components/services/app_service/public/cpp/preferred_apps_converter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/logging.h"
 #include "components/services/app_service/public/cpp/intent_filter.h"
 #include "components/services/app_service/public/cpp/intent_filter_util.h"
 #include "components/services/app_service/public/cpp/preferred_apps_converter.h"
