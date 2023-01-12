d0108f13eeff63dd78ba0af8e96ecce899c5f898
Dave Vandyke <kzar@chromium.org>
2023-01-12 18:53:46 +0000

    [DNR] Add rule filter for get dynamic/session rules APIs
    The declarativeNetRequest.getSessionRules and
    declarativeNetRequest.getDynamicRules APIs return a list of the
    extension's current declarativeNetRequest rules. Up to 5000 rules can
    be returned[1]. The rules are returned in an array not sorted by rule
    ID.
    
    Quite often, an extension will only need to inspect a specific
    rule. This is painful, since all returned rules need to be checked for
    the desired rule ID, until a match is found or the rules are
    exhausted. In the worst case, this means iterating through 5000 rules
    to find that the rule isn't present.
    
    Let's add an optional filter argument to the APIs that allows the
    caller to specify rule IDs to filter the results by. For the above
    example use-case, one rule ID can be specified at call time and then
    the length of the returned array can be quickly checked.
    
    1 - https://developer.chrome.com/docs/extensions/reference/declarativeNetRequest/#property-MAX_NUMBER_OF_DYNAMIC_AND_SESSION_RULES
    
    Bug: 1379699
    Change-Id: I36c7e7c841e4a864788ce7f6b17e5e7d275ca3f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4059106
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Dave Vandyke <kzar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091956}

--- extensions/browser/api/declarative_net_request/declarative_net_request_api.h
+++ extensions/browser/api/declarative_net_request/declarative_net_request_api.h
@@ -16,6 +16,10 @@ namespace declarative_net_request {
 struct ReadJSONRulesResult;
 }  // namespace declarative_net_request
 
+namespace api::declarative_net_request::GetDynamicRules {
+struct Params;
+}
+
 class DeclarativeNetRequestUpdateDynamicRulesFunction
     : public ExtensionFunction {
  public:
@@ -47,6 +51,7 @@ class DeclarativeNetRequestGetDynamicRulesFunction : public ExtensionFunction {
 
  private:
   void OnDynamicRulesFetched(
+      std::unique_ptr<api::declarative_net_request::GetDynamicRules::Params>,
       declarative_net_request::ReadJSONRulesResult read_json_result);
 };
 
