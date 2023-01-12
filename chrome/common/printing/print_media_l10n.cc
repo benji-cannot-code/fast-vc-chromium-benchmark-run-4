1b101155213a80b3e8a26534be1c8b59f48e57f0
Benjamin Gordon <bmgordon@chromium.org>
2023-01-12 01:09:26 +0000

    Drop borderless sizes from media size list
    The paper size dropdown only allows one entry for any given size.  When
    a borderless and non-borderless version of the same size are available,
    only one of them will make it into the list.  In the past the
    non-borderless size generally happened to be selected because the
    printer would list the borderless version second.  With
    crrev.com/c/4022966, Chrome started sorting the list into a consistent
    order.  Depending on how the printer names its borderless sizes, it
    could end up with things like "A 4.borderless" sorting ahead of "A4",
    which causes the regular A4 size to disappear.
    
    Since borderless sizes don't currently produce different output from
    non-borderless sizes, let's remove the borderless sizes from the list
    entirely.  This ensures that the "regular" sizes are always selected.
    We don't currently have a good way to detect all the borderless patterns
    without just dropping non-standard paper names entirely, but the two
    patterns added here cover all of the problem patterns that were observed
    in our test lab.
    
    Bug: b/263452851
    Test: Manually compared paper size lists on lab printers
    Change-Id: Ie34f104a3c3f285c836eb6fdb0382931df749eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4157931
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091614}

--- chrome/common/printing/print_media_l10n.cc
+++ chrome/common/printing/print_media_l10n.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "base/containers/contains.h"
 #include "base/i18n/string_compare.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_piece.h"
@@ -700,6 +701,13 @@ void SortPaperDisplayNames(std::vector<PaperWithSizeInfo>& papers) {
 
   // Break apart the list into separate sort groups.
   for (auto& p : papers) {
+    // Drop borderless sizes so they don't get sorted ahead of standard sizes.
+    // TODO(b/218752273): Remove once borderless sizes are handled properly.
+    if (base::Contains(p.paper.vendor_id, ".borderless_") ||
+        base::Contains(p.paper.vendor_id, ".fb_")) {
+      continue;
+    }
+
     switch (p.size_info.sort_group) {
       case MediaSizeGroup::kSizeMm:
         mm_sizes.emplace_back(p);
