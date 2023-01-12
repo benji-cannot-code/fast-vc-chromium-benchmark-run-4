50b763c298e87185a9899d8e400332233608f9b1
Dan Stahr <danstahr@google.com>
2023-01-12 09:32:17 +0000

    Call PiiElider using the generated headers instead of hardcoding class names and looking up methods manually.
    Change-Id: I2af1189dbcda05b51fc7595eda7e460d7c71518a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4156471
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Dan Stahr <danstahr@google.com>
    Auto-Submit: Dan Stahr <danstahr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091741}

--- base/android/java/src/org/chromium/base/PiiElider.java
+++ base/android/java/src/org/chromium/base/PiiElider.java
@@ -8,7 +8,7 @@ package org.chromium.base;
 import android.text.TextUtils;
 import android.util.Patterns;
 
-import org.chromium.build.annotations.UsedByReflection;
+import org.chromium.base.annotations.CalledByNative;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -218,7 +218,6 @@ public class PiiElider {
      * @param stacktrace Multiline stacktrace as a string.
      * @return Stacktrace with elided URLs.
      */
-    @UsedByReflection("jni_android.cc")
     public static String sanitizeStacktrace(String stacktrace) {
         if (TextUtils.isEmpty(stacktrace)) {
             return "";
@@ -233,5 +232,14 @@ public class PiiElider {
             }
         }
         return TextUtils.join("\n", frames);
+    }
+
+    /**
+     * Returns a sanitized stacktrace (per {@link #sanitizeStacktrace(String)}) for the given
+     * throwable.
+     */
+    @CalledByNative
+    public static String getSanitizedStacktrace(Throwable throwable) {
+        return sanitizeStacktrace(Log.getStackTraceString(throwable));
     }
 }
