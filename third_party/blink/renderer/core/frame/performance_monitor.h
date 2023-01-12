9b69272420f5cea44ecdee39a465061434d0b410
Noam Rosenthal <nrosenthal@chromium.org>
2023-01-12 06:53:33 +0000

    Initialize long task reporting for ScriptPromiseResolver
    This tells the PerformanceMonitor to report longtasks
    in promise chains that were not started from a callback,
    e.g. fetch().then(() => {...long_task})
    
    The PerformanceMonitor relies on different probes for "attribution": connecting a long task with one or more frames, to avoid reporting a long task to an unrelated frame tree that happens to be on the same thread.
    
    This design is a bit brittle, as we need to patch through all the things that can invoke a frame-attributed task into PerformanceMonitor.
    
    This CL doesn't attempt to solve this brittleness, in lack of a better solution, but rather to mitigate the particular issue with tasks invoked with a promise, by patching through ScriptPromiseResolver to PerformanceMonitor.
    
    Bug: 1251964
    Change-Id: I745780f834f9d1dfc8e8a5c8295326f06276cba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129480
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091706}

--- third_party/blink/renderer/core/frame/performance_monitor.h
+++ third_party/blink/renderer/core/frame/performance_monitor.h
@@ -100,6 +100,8 @@ class CORE_EXPORT PerformanceMonitor final
 
   void DocumentWriteFetchScript(Document*);
 
+  void WillReactToScriptPromise(ExecutionContext*);
+
   // Direct API for core.
   void Subscribe(Violation, base::TimeDelta threshold, Client*);
   void UnsubscribeAll(Client*);
