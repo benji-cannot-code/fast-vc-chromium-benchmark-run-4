6908275a08dea58ee61dff575987a417062faaad
Kelvin Jiang <kelvinjiang@chromium.org>
2023-01-13 21:28:52 +0000

    [Extensions] Fix CrExtensionsA11yTestWithMultipleExensions flakes
    This CL fixes test flakes in the aforementioned test by waiting for the
    extensions-manager element to finish its initial load of extensions info
    before proceeding using a promiseResolver.
    
    During flaky runs, it seems that the test tried to access properties in
    the extensions info object before they were loaded which led to
    failures.
    
    The logic is similar to a fix in crrev.com/c/4150310
    
    Bug: 1397832
    Change-Id: I72b06e374c30f911f45fd319e3d9cadeaeee02d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4153431
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092622}

--- chrome/test/data/webui/extensions/a11y/extensions_a11y_test.js
+++ chrome/test/data/webui/extensions/a11y/extensions_a11y_test.js
@@ -33,6 +33,13 @@ var CrExtensionsA11yTest = class extends PolymerTest {
     ];
   }
 
+  /** @override */
+  setUp() {
+    const runTest = this.deferRunTest(WhenTestDone.DEFAULT);
+    const manager = document.querySelector('extensions-manager');
+    manager.whenPageInitializedForTest().then(runTest);
+  }
+
   // Default accessibility audit options. Specify in test definition to use.
   static get axeOptions() {
     return {
@@ -131,10 +138,6 @@ AccessibilityTest.define('CrExtensionsA11yTest', {
 CrExtensionsA11yTestWithMultipleExensions = class extends CrExtensionsA11yTest {
   /** @override */
   testGenPreamble() {
-    // (crbug.com/1397832): Flaky on mac builder.
-    GEN('#if BUILDFLAG(IS_MAC)');
-    GEN('#define DISABLED_All');
-    GEN('#endif');
     GEN('  InstallGoodExtension();');
     GEN('  InstallPackagedApp();');
     GEN('  InstallHostedApp();');
