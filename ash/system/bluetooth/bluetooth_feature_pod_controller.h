9d087360de3d20327a9ae53101993fe515e1e4a1
James Cook <jamescook@chromium.org>
2023-01-13 18:23:34 +0000

    Make quick settings bluetooth tile visible by default for QsRevamp
    The actual visibility of the bluetooth feature pod / tile is computed
    asynchronously, due to the bluetooth adapter initializion being async
    and the use of a mojo interface. This results in quick settings being
    constructed with an invisible bluetooth tile, then the tile becoming
    visible later.
    
    For QsRevamp the initial layout computations are simplified if all
    tiles that will be visible start visible. Make the bluetooth tile
    start visible by changing the default bluetooth system state to
    "enabled" (which is the common case in production). Guard this with
    the QsRevamp flag to avoid potential regressions to the existing
    quick settings.
    
    This fixes some quick settings height computations and focus ring
    visibility problems.
    
    Bug: b:264459727
    Test: added to ash_unittests
    Change-Id: I8f15503da3c0c19a02b2193deeafae48080d6802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4163454
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092510}

--- ash/system/bluetooth/bluetooth_feature_pod_controller.h
+++ ash/system/bluetooth/bluetooth_feature_pod_controller.h
@@ -91,8 +91,7 @@ class ASH_EXPORT BluetoothFeaturePodController
   bluetooth_config::mojom::BluetoothModificationState modification_state_ =
       bluetooth_config::mojom::BluetoothModificationState::
           kCannotModifyBluetooth;
-  bluetooth_config::mojom::BluetoothSystemState system_state_ =
-      bluetooth_config::mojom::BluetoothSystemState::kUnavailable;
+  bluetooth_config::mojom::BluetoothSystemState system_state_;
   FeaturePodButton* button_ = nullptr;  // Owned by views hierarchy.
   FeatureTile* tile_ = nullptr;         // Owned by views hierarchy.
   UnifiedSystemTrayController* tray_controller_;
