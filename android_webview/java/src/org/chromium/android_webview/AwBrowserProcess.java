d9d6e5fe822d498ef6043d23838cb915df0bee59
Andrew Grieve <agrieve@chromium.org>
2023-01-13 15:58:26 +0000

    Catch IllegalStateException in MetricsBridgeService
    Bug: 1403976
    Change-Id: I94a9311c7e691ed2fb5b867b211c96ade9b644ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4126766
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092422}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
@@ -12,7 +12,6 @@ import android.content.ServiceConnection;
 import android.content.pm.ApplicationInfo;
 import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
-import android.os.RemoteException;
 import android.os.StrictMode;
 
 import androidx.annotation.IntDef;
@@ -452,9 +451,9 @@ public final class AwBrowserProcess {
                     } catch (InvalidProtocolBufferException e) {
                         Log.d(TAG, "Malformed metrics log proto", e);
                         logTransmissionResult(TransmissionResult.MALFORMED_PROTOBUF);
-                    } catch (RemoteException e) {
-                        Log.d(TAG, "Remote Exception calling MetricsBridgeService#retrieveMetrics",
-                                e);
+                    } catch (Exception e) {
+                        // RemoteException, IllegalArgumentException (https://crbug.com/1403976)
+                        Log.d(TAG, "Remote Exception in MetricsBridgeService#retrieveMetrics", e);
                         logTransmissionResult(TransmissionResult.REMOTE_EXCEPTION);
                     } finally {
                         appContext.unbindService(this);
