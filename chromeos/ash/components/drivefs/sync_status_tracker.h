37e790159b9fdb4073fcfa411224018735154c23
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-01-13 02:06:08 +0000

    Add missing ostream include.
    This file has a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    Change-Id: I11c1df1f3affc86642f89317f149ec1143adac08
    Bug: 1270812, 1372522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4163639
    Reviewed-by: Marcello Salomao <msalomao@google.com>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092202}

--- chromeos/ash/components/drivefs/sync_status_tracker.h
+++ chromeos/ash/components/drivefs/sync_status_tracker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <ostream>
 #include <string>
 #include <string_view>
 
