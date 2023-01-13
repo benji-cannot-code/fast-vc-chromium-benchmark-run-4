31e1eaa7b81c7e56b767a67399016cff1cc7e274
Simon Hangl <simonha@google.com>
2023-01-13 15:33:59 +0000

    [MSC] Extended feature flags for getDisplayMediaSet to Linux.
    Bug: 1300883
    Change-Id: Iaf0cd9af61c8662140841b9e8868e478e9ec05c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161442
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1092413}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3513,6 +3513,14 @@ extern const char kLacrosScreenCoordinatesEnabledName[];
 extern const char kLacrosScreenCoordinatesEnabledDescription[];
 #endif  // #if BUILDFLAG(IS_CHROMEOS_LACROS)
 
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+extern const char kGetDisplayMediaSetName[];
+extern const char kGetDisplayMediaSetDescription[];
+
+extern const char kGetDisplayMediaSetAutoSelectAllScreensName[];
+extern const char kGetDisplayMediaSetAutoSelectAllScreensDescription[];
+#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+
 #if BUILDFLAG(IS_CHROMEOS)
 extern const char kCrOSDspBasedAecAllowedName[];
 extern const char kCrOSDspBasedAecAllowedDescription[];
@@ -3546,12 +3554,6 @@ extern const char kDisableIdleSocketsCloseOnMemoryPressureDescription[];
 
 extern const char kDisableOfficeEditingComponentAppName[];
 extern const char kDisableOfficeEditingComponentAppDescription[];
-
-extern const char kGetDisplayMediaSetName[];
-extern const char kGetDisplayMediaSetDescription[];
-
-extern const char kGetDisplayMediaSetAutoSelectAllScreensName[];
-extern const char kGetDisplayMediaSetAutoSelectAllScreensDescription[];
 
 extern const char kIntentChipSkipsPickerName[];
 extern const char kIntentChipSkipsPickerDescription[];
