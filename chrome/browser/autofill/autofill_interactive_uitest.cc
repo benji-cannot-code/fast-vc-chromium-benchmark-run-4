af1b2a6f4c8412060f6ef07316e18b4f05e503f8
Henrique Ferreiro <hferreiro@igalia.com>
2023-01-13 09:19:36 +0000

    Update c/b/autofill:interactive_ui_tests deps
    Bug: 1164001
    Change-Id: Ia3fdd9f673d01497e38dd16f93ef831ad363daa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4154581
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092296}

--- chrome/browser/autofill/autofill_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_interactive_uitest.cc
@@ -99,7 +99,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/ash/accessibility/speech_monitor.h"
 #include "chrome/browser/ui/aura/accessibility/automation_manager_aura.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "extensions/browser/browsertest_util.h"
 #include "ui/base/test/ui_controls.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH) && BUILDFLAG(ENABLE_EXTENSIONS)
