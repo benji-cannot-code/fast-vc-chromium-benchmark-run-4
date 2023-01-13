17483de66b9ceeb51d4c037093f6caed1f669164
Garrick Evans <garrick@google.com>
2023-01-13 05:33:11 +0000

    Remove use of dns-proxy feature flags.
    These flags expire in M111 and with dns-proxy at 100% in stable, they
    can be removed. This patch also addresses the "dead link" problem for
    managed users in Lacros.
    
    BUG=b:265091596
    BUG=b:241150078
    BUG=1360841
    TEST=manual testing on brya
    
    Change-Id: Iacd2765637434918f81614730369e37f0fedc7d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4160416
    Commit-Queue: Garrick Evans <garrick@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092247}

--- chrome/browser/ui/webui/settings/ash/privacy_section.cc
+++ chrome/browser/ui/webui/settings/ash/privacy_section.cc
@@ -284,12 +284,7 @@ const std::vector<SearchConcept>& GetPrivacyControlsSearchConcepts() {
 }
 
 bool IsSecureDnsAvailable() {
-  return
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-      base::FeatureList::IsEnabled(features::kEnableDnsProxy) &&
-      base::FeatureList::IsEnabled(::features::kDnsProxyEnableDOH) &&
-#endif
-      ::features::kDnsOverHttpsShowUiParam.Get();
+  return ::features::kDnsOverHttpsShowUiParam.Get();
 }
 
 }  // namespace
--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -1656,10 +1656,7 @@ void AddPeopleStrings(content::WebUIDataSource* html_source, Profile* profile) {
 }
 
 bool ShouldLinkSecureDnsOsSettings() {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  return base::FeatureList::IsEnabled(ash::features::kEnableDnsProxy) &&
-         base::FeatureList::IsEnabled(::features::kDnsProxyEnableDOH);
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+#if BUILDFLAG(IS_CHROMEOS)
   return true;
 #else
   return false;
