366cf3f4dbb2e18d8f150101acf214aef6e219d7
Nicholas Verne <nverne@chromium.org>
2023-01-13 00:12:26 +0000

    crostini: Shutdown Linux now stops all VMs of type termina
    Adds StopRunningVms to CrostiniManager. The concept of shutting down
    Linux (for Crostini) is session-wide. Users will still be able to stop
    individual containers via Settings (and their VMs will stop if it has no
    more running containers), so fine control is still available.
    
    Bug: 1261319
    Change-Id: I43171dbb211b532a11ded04597108d4498abbb7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4150609
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Auto-Submit: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092137}

--- chrome/browser/ash/crostini/crostini_manager.cc
+++ chrome/browser/ash/crostini/crostini_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/constants/ash_features.h"
+#include "base/barrier_callback.h"
 #include "base/barrier_closure.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
@@ -1572,6 +1573,36 @@ void CrostiniManager::StopVm(std::string name,
       std::move(request),
       base::BindOnce(&CrostiniManager::OnStopVm, weak_ptr_factory_.GetWeakPtr(),
                      std::move(name), std::move(callback)));
+}
+
+void CrostiniManager::StopRunningVms(CrostiniResultCallback callback) {
+  std::vector<std::string> names;
+  LOG(WARNING) << "StopRunningVms";
+  for (const auto& it : running_vms_) {
+    if (it.second.state != VmState::STOPPING) {
+      names.push_back(it.first);
+    }
+  }
+  auto barrier = base::BarrierCallback<CrostiniResult>(
+      names.size(), base::BindOnce(
+                        [](CrostiniResultCallback callback,
+                           std::vector<CrostiniResult> results) {
+                          auto result = CrostiniResult::SUCCESS;
+                          for (auto res : results) {
+                            if (res != CrostiniResult::SUCCESS) {
+                              LOG(ERROR) << "StopVm failure code "
+                                         << static_cast<int>(res);
+                              result = res;
+                              break;
+                            }
+                          }
+                          std::move(callback).Run(result);
+                        },
+                        std::move(callback)));
+  for (const auto& name : names) {
+    LOG(WARNING) << "Stopping vm " << name;
+    StopVm(name, barrier);
+  }
 }
 
 void CrostiniManager::UpdateTerminaVmKernelVersion() {
