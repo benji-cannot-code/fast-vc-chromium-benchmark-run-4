4f5e2b04a3a0ea5ce0dc50e2233d1edb5cce2553
Jood Hajeer <jood@google.com>
2023-01-13 15:21:31 +0000

    [ios/save password message] Expose account in delegate
    No behavior is changed. Refactoring for next CLs.
    - Remove 2 coupled fields in the delegate ctor. user_email is displayed iff
    is_sync_user is true. Instead pass the email as an absl::optional<std::string>.
    - Expose a getter for the account in the delegate.
    
    Bug: 1392708
    Change-Id: Iae0bb7bb559d458a2195724874e5e64ca65465ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161670
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1092408}

--- ios/chrome/browser/passwords/test/mock_ios_chrome_save_passwords_infobar_delegate.h
+++ ios/chrome/browser/passwords/test/mock_ios_chrome_save_passwords_infobar_delegate.h
@@ -19,7 +19,6 @@ class MockIOSChromeSavePasswordInfoBarDelegate
     : public IOSChromeSavePasswordInfoBarDelegate {
  public:
   MockIOSChromeSavePasswordInfoBarDelegate(
-      bool is_sync_user,
       bool password_update,
       std::unique_ptr<password_manager::PasswordFormManagerForUI> form_to_save);
   ~MockIOSChromeSavePasswordInfoBarDelegate() override;
@@ -29,7 +28,8 @@ class MockIOSChromeSavePasswordInfoBarDelegate
   static std::unique_ptr<MockIOSChromeSavePasswordInfoBarDelegate> Create(
       NSString* username,
       NSString* password,
-      const GURL& url = GURL::EmptyGURL());
+      const GURL& url = GURL::EmptyGURL(),
+      absl::optional<std::string> account_to_store_password = absl::nullopt);
 
   MOCK_METHOD0(InfoBarDismissed, void());
   MOCK_METHOD2(UpdateCredentials, void(NSString* username, NSString* password));
@@ -41,7 +41,8 @@ class MockIOSChromeSavePasswordInfoBarDelegate
  private:
   MockIOSChromeSavePasswordInfoBarDelegate(
       std::unique_ptr<password_manager::PasswordForm> form,
-      std::unique_ptr<GURL> url);
+      std::unique_ptr<GURL> url,
+      absl::optional<std::string> account_to_store_password);
 
   std::unique_ptr<password_manager::PasswordForm> form_;
   std::unique_ptr<GURL> url_;
