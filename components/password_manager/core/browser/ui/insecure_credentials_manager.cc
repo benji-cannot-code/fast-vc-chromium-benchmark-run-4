8098fc45bec1f7d0ab7bde9c56208248bb73ea7f
Viktor Semeniuk <vsemeniuk@google.com>
2023-01-13 12:51:10 +0000

    [Password Manager] Check for reused passwords
    This CL adds basic implementation to check for reused passwords.
    
    Bug: 1406472
    Change-Id: I27770b332ccb5c82719297934e20f8acbafffb84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127438
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1092357}

--- components/password_manager/core/browser/ui/insecure_credentials_manager.cc
+++ components/password_manager/core/browser/ui/insecure_credentials_manager.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#include "components/password_manager/core/browser/ui/reuse_check_utility.h"
 #include "components/password_manager/core/browser/ui/weak_check_utility.h"
 #endif
 
@@ -65,6 +66,16 @@ InsecureCredentialsManager::InsecureCredentialsManager(
 InsecureCredentialsManager::~InsecureCredentialsManager() = default;
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+void InsecureCredentialsManager::StartReuseCheck(
+    base::OnceClosure on_check_done) {
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
+      base::BindOnce(&BulkReuseCheck, presenter_->GetSavedPasswords()),
+      base::BindOnce(&InsecureCredentialsManager::OnReuseCheckDone,
+                     weak_ptr_factory_.GetWeakPtr(), base::ElapsedTimer())
+          .Then(std::move(on_check_done)));
+}
+
 void InsecureCredentialsManager::StartWeakCheck(
     base::OnceClosure on_check_done) {
   base::ThreadPool::PostTaskAndReplyWithResult(
@@ -141,6 +152,12 @@ InsecureCredentialsManager::GetInsecureCredentialEntries() const {
            password_manager::InsecurityMetadata(
                base::Time(), password_manager::IsMuted(false))});
     }
+    if (reused_passwords_.contains(credential.password)) {
+      credential.password_issues.insert(
+          {password_manager::InsecureType::kReused,
+           password_manager::InsecurityMetadata(
+               base::Time(), password_manager::IsMuted(false))});
+    }
   }
   base::EraseIf(credentials, [](const auto& credential) {
     return credential.password_issues.empty();
@@ -155,6 +172,13 @@ void InsecureCredentialsManager::AddObserver(Observer* observer) {
 
 void InsecureCredentialsManager::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
+}
+
+void InsecureCredentialsManager::OnReuseCheckDone(
+    base::ElapsedTimer timer_since_reuse_check_start,
+    base::flat_set<std::u16string> reused_passwords) {
+  reused_passwords_ = std::move(reused_passwords);
+  NotifyInsecureCredentialsChanged();
 }
 
 void InsecureCredentialsManager::OnWeakCheckDone(
---
+++ components/password_manager/core/browser/ui/reuse_check_utility.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_UI_REUSE_CHECK_UTILITY_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_UI_REUSE_CHECK_UTILITY_H_
+
+#include <string>
+
+#include "base/containers/flat_set.h"
+#include "components/password_manager/core/browser/ui/credential_ui_entry.h"
+
+namespace password_manager {
+
+// Returns reused passwords. Password is considered reused only if:
+// * there are at least two credentials with the same non-normalized password
+//   and those credentials:
+//    # have different normalized usernames,
+//    # aren't affiliated and/or PSL-matched,
+//    # don't belong to internal network.
+base::flat_set<std::u16string> BulkReuseCheck(
+    const std::vector<CredentialUIEntry>& credentials);
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_UI_REUSE_CHECK_UTILITY_H_
