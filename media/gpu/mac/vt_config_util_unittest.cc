d523a82b9551fe222e7479077e2834159f93a231
Avi Drissman <avi@chromium.org>
2023-01-13 20:00:25 +0000

    Remove sdk_forward_declarations
    It's a remnant of the days when we would stay on old SDKs for long
    stretches of time. Given that we now aggressively roll forward to new
    SDKs, remove it.
    
    Bug: none
    Change-Id: I1ebe46ca71835795ec68272a632aa98d63fd263b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166680
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Owners-Override: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092572}

--- media/gpu/mac/vt_config_util_unittest.cc
+++ media/gpu/mac/vt_config_util_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/mac_util.h"
-#include "base/mac/sdk_forward_declarations.h"
 #include "base/strings/sys_string_conversions.h"
 #include "media/base/mac/color_space_util_mac.h"
 #include "media/formats/mp4/box_definitions.h"
--- media/gpu/mac/vt_video_decode_accelerator_mac.cc
+++ media/gpu/mac/vt_video_decode_accelerator_mac.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/mac_logging.h"
 #include "base/mac/mac_util.h"
-#include "base/mac/sdk_forward_declarations.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/scoped_policy.h"
 #include "base/metrics/histogram_macros.h"
