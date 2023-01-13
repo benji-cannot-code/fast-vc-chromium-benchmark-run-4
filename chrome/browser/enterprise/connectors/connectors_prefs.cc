91f8f3ad9e54701c186b80cb7d42e11a96898932
Nasser Al-shawwa <alshawwa@chromium.org>
2023-01-13 14:32:20 +0000

    [Remove FileSystemConnector] Remove kSendDownloadToCloudPref and references to it
    Bug: b/262991271
    Change-Id: I66ff665c273ca55dd2be76baead032496f521d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4162677
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092386}

--- chrome/browser/enterprise/connectors/connectors_prefs.cc
+++ chrome/browser/enterprise/connectors/connectors_prefs.cc
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_connectors {
 
-const char kSendDownloadToCloudPref[] =
-    "enterprise_connectors.send_download_to_cloud";
-
 const char kOnFileAttachedPref[] = "enterprise_connectors.on_file_attached";
 
 const char kOnFileDownloadedPref[] = "enterprise_connectors.on_file_downloaded";
@@ -48,7 +45,6 @@ const char kOnSecurityEventScopePref[] =
     "enterprise_connectors.scope.on_security_event";
 
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
-  registry->RegisterListPref(kSendDownloadToCloudPref);
   registry->RegisterListPref(kOnFileAttachedPref);
   registry->RegisterListPref(kOnFileDownloadedPref);
   registry->RegisterListPref(kOnBulkDataEntryPref);
