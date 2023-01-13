2544a0675b4484a2f0bf93db124243891ef7fa87
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-01-13 03:57:48 +0000

    Reland "Remove unused #include <ostream> in base/numerics/safe_conversions.h"
    This is a reland of commit 9ffd34c88a1cea18f1cceda678caf9248fccbc0f
    
    Original change's description:
    > Remove unused #include <ostream> in base/numerics/safe_conversions.h
    >
    > The code using <ostream> was removed from safe_conversions.h in
    > https://source.chromium.org/chromium/chromium/src/+/60c8cdf472bcf204443e26e9f2d7c74f948746ee,
    > but the #include was left there because there were still compilation
    > units having transitive dependencies on ostream. These dependencies
    > have now been fixed, so we can now remove the #include.
    >
    > Change-Id: I9f869c4a9eca1c1ff48d6a442708938603be6bc3
    > Bug: 1372522
    > Fixed: 1270812
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053484
    > Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1082482}
    
    Bug: 1372522
    Fixed: 1270812
    Change-Id: Icf231abd751ae77e225022d03b57e13918dcd74d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4152245
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092231}

--- base/numerics/safe_conversions.h
+++ base/numerics/safe_conversions.h
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_HAS_OPTIMIZED_SAFE_CONVERSIONS (0)
 #endif
 
-#if !BASE_NUMERICS_DISABLE_OSTREAM_OPERATORS
-#include <ostream>
-#endif
-
 namespace base {
 namespace internal {
 
