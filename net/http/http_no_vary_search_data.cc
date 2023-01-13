a080a1a4dd7c8a0b979ba2089591b073af4f0ae8
Liviu Tinta <liviutinta@chromium.org>
2023-01-13 18:19:04 +0000

    [No-Vary-Search] Add non-ASCII key support to the response header
    Implementation of "parse a key" algorithm from
    https://wicg.github.io/nav-speculation/no-vary-search.html#parsing
    
    "parse a key" algorithm allows the encoding of non-ASCII keys in the
    ASCII structured header format.
    
    The non-ASCII key would be represented in a percent encoded format
    when specified in the No-Vary-Search header.
    
    Example: No-Vary-Search: params=("%C3%A9+%E6%B0%97")
    
    "%C3%A9+%E6%B0%97" in this example represents the key "é 気".
    
    Bug: 1399848, 1378072, 1378075
    Change-Id: I02ca2e4956dd242ee0ec28706707b1903b6c8bea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4150191
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092505}

--- net/http/http_no_vary_search_data.cc
+++ net/http/http_no_vary_search_data.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/containers/flat_set.h"
 #include "net/base/url_search_params.h"
+#include "net/base/url_util.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/structured_headers.h"
 #include "url/gurl.h"
@@ -24,7 +25,7 @@ absl::optional<std::vector<std::string>> ParseStringList(
   for (const auto& item : items) {
     if (!item.item.is_string())
       return absl::nullopt;
-    keys.push_back(item.item.GetString());
+    keys.push_back(UnescapePercentEncodedUrl(item.item.GetString()));
   }
   return keys;
 }
