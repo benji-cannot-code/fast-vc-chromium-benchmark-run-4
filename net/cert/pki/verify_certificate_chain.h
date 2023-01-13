0dc2cb69b3051f9d576340dddffa5be986d6daab
Matt Mueller <mattm@chromium.org>
2023-01-13 01:19:07 +0000

    enforce_anchor_constraints: policyMappings
    (enforce_anchor_constraints is not enabled by default, so this change
    is expected to have no behavior change in chrome at this time.)
    
    Bug: 1072083
    Change-Id: I08ff6186398a4c5d0241bae840f25a478306f542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148837
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092178}

--- net/cert/pki/verify_certificate_chain.h
+++ net/cert/pki/verify_certificate_chain.h
@@ -234,7 +234,7 @@ class NET_EXPORT VerifyCertificateChainDelegate {
 //  * Basic constraints:     Yes
 //  * Name constraints:      Yes
 //  * Certificate policies:  Yes
-//  * Policy Mappings:       No
+//  * Policy Mappings:       Yes
 //  * inhibitAnyPolicy:      Yes
 //  * PolicyConstraints:     Yes
 //
