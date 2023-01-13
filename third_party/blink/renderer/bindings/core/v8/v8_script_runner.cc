2b74d014dacd8bbb89f6fc7565effeabc1ed1488
Marja Hölttä <marja@chromium.org>
2023-01-13 12:36:19 +0000

    [bindings] Make Blink not fail if V8 adds compilation options
    Bug: chromium:1406506
    Change-Id: I05b573fa94714a493696bd8addab3c11900ccebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161906
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092351}

--- third_party/blink/renderer/bindings/core/v8/v8_script_runner.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_script_runner.cc
@@ -207,6 +207,8 @@ v8::MaybeLocal<v8::Script> CompileScriptInternal(
       }
       return script;
     }
+    default:
+      NOTREACHED();
   }
 
   // All switch branches should return and we should never get here.
@@ -352,6 +354,8 @@ v8::MaybeLocal<v8::Module> V8ScriptRunner::CompileModule(
                 cached_data->length, cached_data->rejected));
         break;
       }
+      default:
+        NOTREACHED();
     }
   }
 
