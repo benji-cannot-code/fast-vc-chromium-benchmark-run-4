3007e0c0d65f6e0f3b5ea41464e65eb7641c056c
Matt Jones <mdjones@chromium.org>
2023-01-13 17:00:31 +0000

    [Shopping list] Allow old bookmarks on android to be tracked with new info
    In the original version of price tracking, we weren't able to update
    old bookmarks to use new info if available. Consequently we blocked UI
    from showing in the menu if a bookmark didn't have meta. Since then
    we've gained the ability to update on the fly and no longer need the
    menu logic to prevent showing the "track" option.
    
    Bug: 1404035
    Change-Id: I424f5dc8633d969e7c351fc47ae21b82c13c37c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4157735
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092458}

--- chrome/android/java/src/org/chromium/chrome/browser/app/appmenu/AppMenuPropertiesDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/appmenu/AppMenuPropertiesDelegateImpl.java
@@ -83,7 +83,6 @@ import org.chromium.components.commerce.core.ShoppingService;
 import org.chromium.components.dom_distiller.core.DomDistillerUrlUtils;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.embedder_support.util.UrlUtilities;
-import org.chromium.components.power_bookmarks.PowerBookmarkMeta;
 import org.chromium.components.webapk.lib.client.WebApkValidator;
 import org.chromium.components.webapps.AppBannerManager;
 import org.chromium.components.webapps.WebappsUtils;
@@ -1171,14 +1170,6 @@ public class AppMenuPropertiesDelegateImpl implements AppMenuPropertiesDelegate 
         if (!ShoppingFeatures.isShoppingListEnabled()
                 || !PowerBookmarkUtils.isPriceTrackingEligible(currentTab)
                 || mIsTypeSpecificBookmarkItemRowPresent || !mBookmarkModelSupplier.hasValue()) {
-            startPriceTrackingMenuItem.setVisible(false);
-            stopPriceTrackingMenuItem.setVisible(false);
-            return;
-        }
-
-        PowerBookmarkMeta existingBookmarkMeta = PowerBookmarkUtils.getBookmarkBookmarkMetaForTab(
-                mBookmarkModelSupplier.get(), currentTab);
-        if (existingBookmarkMeta != null && !existingBookmarkMeta.hasShoppingSpecifics()) {
             startPriceTrackingMenuItem.setVisible(false);
             stopPriceTrackingMenuItem.setVisible(false);
             return;
