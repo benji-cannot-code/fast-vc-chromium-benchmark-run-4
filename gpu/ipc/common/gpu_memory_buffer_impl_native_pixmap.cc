68be80b30de203c6726cd0aecb96fd084f848b46
Peter Varga <pvarga@inf.u-szeged.hu>
2023-01-13 19:52:00 +0000

    Remove vulkan header include leftover
    This CL amends https://crrev.com/c/4013439
    
    Bug: none
    Change-Id: I6dfc3fecfd0d7c7dd6a5989d95441f93abc19713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165241
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092567}

--- gpu/ipc/common/gpu_memory_buffer_impl_native_pixmap.cc
+++ gpu/ipc/common/gpu_memory_buffer_impl_native_pixmap.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/ipc/common/gpu_memory_buffer_impl_native_pixmap.h"
 
-#include <vulkan/vulkan.h>
-
 #include <utility>
 
 #include "base/functional/bind.h"
