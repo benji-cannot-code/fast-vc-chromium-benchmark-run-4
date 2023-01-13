e0897f461aa49fb45ae4d6620392f7166f74bbb2
Olesia Marukhno <olesiamarukhno@google.com>
2023-01-13 18:11:54 +0000

    [privacy sandbox] Report prompt actions for More button
    Report prompt actions when user clicks on the More button in consent and
    notice prompts.
    
    Bug: 1378703
    Change-Id: Ic43dae8db82f8a1b66e128b77468fbded9340b2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4164977
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1092501}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
@@ -1157,6 +1157,16 @@ void PrivacySandboxService::RecordPromptActionMetrics(
           "Settings.PrivacySandbox.Notice.LearnMoreClosed"));
       break;
     }
+    case (PromptAction::kConsentMoreButtonClicked): {
+      base::RecordAction(base::UserMetricsAction(
+          "Settings.PrivacySandbox.Consent.MoreButtonClicked"));
+      break;
+    }
+    case (PromptAction::kNoticeMoreButtonClicked): {
+      base::RecordAction(base::UserMetricsAction(
+          "Settings.PrivacySandbox.Notice.MoreButtonClicked"));
+      break;
+    }
   }
 }
 
--- chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_test.ts
+++ chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_test.ts
@@ -324,6 +324,8 @@ suite('PrivacySandboxDialogCombined', function() {
     const moreButton: HTMLElement =
         consentStep.shadowRoot!.querySelector('#moreButton')!;
     moreButton.click();
+    await verifyActionOccured(
+        PrivacySandboxPromptAction.CONSENT_MORE_BUTTON_CLICKED);
     await consentStep.whenWasScrolledToBottomForTest();
 
     // After scrolling down, the "More" button is hidden and dialog button are
@@ -539,6 +541,8 @@ suite('PrivacySandboxDialogNoticeEEA', function() {
     const moreButton: HTMLElement =
         noticeStep.shadowRoot!.querySelector('#moreButton')!;
     moreButton.click();
+    await verifyActionOccured(
+        PrivacySandboxPromptAction.NOTICE_MORE_BUTTON_CLICKED);
     await noticeStep.whenWasScrolledToBottomForTest();
 
     // After scrolling down, the "More" button is hidden and dialog button are
@@ -677,6 +681,8 @@ suite('PrivacySandboxDialogNoticeROW', function() {
     const moreButton: HTMLElement =
         page.shadowRoot!.querySelector('#moreButton')!;
     moreButton.click();
+    await verifyActionOccured(
+        PrivacySandboxPromptAction.NOTICE_MORE_BUTTON_CLICKED);
     await page.whenWasScrolledToBottomForTest();
 
     // After scrolling down, the "More" button is hidden and dialog button are
@@ -799,7 +805,7 @@ suite('PrivacySandboxDialogMixin', function() {
             </div>
           </div>
           <div id="showMoreOverlay" hidden="[[wasScrolledToBottom]]">
-            <button id="moreButton" on-click="onMoreClicked">More</button>
+            <button id="moreButton" on-click="onMoreClicked_">More</button>
           </div>
         </div>
       `;
