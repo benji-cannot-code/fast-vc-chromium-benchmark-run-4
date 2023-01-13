26cf2d75719bc17a8a7f2de27040677fa0133418
Andrew Paseltiner <apaseltiner@chromium.org>
2023-01-13 21:00:59 +0000

    Remove actual report times from simulator output
    They are irrelevant for interop tests due to their dependence on
    implementation details.
    
    Change-Id: I0105b9179b34be62ecb9e22b9b3dc9ac43e97733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166841
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092603}

--- content/browser/attribution_reporting/attribution_interop_unittest.cc
+++ content/browser/attribution_reporting/attribution_interop_unittest.cc
@@ -113,7 +113,6 @@ TEST_P(AttributionInteropTest, HasExpectedOutput) {
           AttributionSimulationOutputOptions{
               .remove_report_ids = true,
               .remove_assembled_report = true,
-              .remove_actual_report_times = true,
           },
   };
 
--- tools/attribution_reporting/simulator_main.cc
+++ tools/attribution_reporting/simulator_main.cc
@@ -37,7 +37,6 @@ constexpr char kSwitchRandomizedResponseRateNavigation[] =
     "randomized_response_rate_navigation";
 constexpr char kSwitchRandomizedResponseRateEvent[] =
     "randomized_response_rate_event";
-constexpr char kSwitchRemoveActualReportTimes[] = "remove_actual_report_times";
 constexpr char kSwitchRemoveAssembledReport[] = "remove_assembled_report";
 
 constexpr const char* kAllowedSwitches[] = {
@@ -53,7 +52,6 @@ constexpr const char* kAllowedSwitches[] = {
     kSwitchCopyInputToOutput,
     kSwitchRandomizedResponseRateNavigation,
     kSwitchRandomizedResponseRateEvent,
-    kSwitchRemoveActualReportTimes,
 };
 
 constexpr char kHelpMsg[] = R"(
@@ -66,7 +64,6 @@ attribution_reporting_simulator
   [--input_mode=<input_mode>]
   [--remove_report_ids]
   [--remove_assembled_report]
-  [--remove_actual_report_times]
 
 attribution_reporting_simulator is a command-line tool that simulates the
 Attribution Reporting API for for sources and triggers specified in an input
@@ -140,11 +137,6 @@ Switches:
                               aggregatable report bodies, as they are randomly
                               generated. Use this switch to make the tool's
                               output more deterministic.
-
-  --remove_actual_report_times
-                            - Optional. If present, removes the `report_time`
-                              field from reports, as they are subject to
-                              implementation details.
 
   --version                 - Outputs the tool version and exits.
 
@@ -318,8 +310,6 @@ int main(int argc, char* argv[]) {
                   command_line.HasSwitch(kSwitchRemoveReportIds),
               .remove_assembled_report =
                   command_line.HasSwitch(kSwitchRemoveAssembledReport),
-              .remove_actual_report_times =
-                  command_line.HasSwitch(kSwitchRemoveActualReportTimes),
           },
   });
 
