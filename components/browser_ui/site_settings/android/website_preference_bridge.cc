31dea2c8d882cdb34bfd2470d50e97c32cffb0d1
Andrey Zaytsev <andzaytsev@google.com>
2023-01-13 15:35:43 +0000

    Kartoffel Storage Partitioning: Update the Clank local storage clearing logic to support SP
    Also tested manually following https://docs.google.com/document/d/1CVVtYDt6zVw-xlwVccmqx0cqywSzRSzrS-WO_uKdCLA/edit?pli=1#heading=h.zi3bmw4078yg
    
    Bug: 1405808
    Change-Id: I6b389fa586ce6ca98c459a4b3241bc14c939b77b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4154429
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092414}

--- components/browser_ui/site_settings/android/website_preference_bridge.cc
+++ components/browser_ui/site_settings/android/website_preference_bridge.cc
@@ -20,10 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/memory/raw_ptr.h"
 #include "base/metrics/user_metrics.h"
 #include "base/notreached.h"
 #include "components/browser_ui/site_settings/android/site_settings_jni_headers/WebsitePreferenceBridge_jni.h"
 #include "components/browser_ui/site_settings/android/storage_info_fetcher.h"
+#include "components/browser_ui/site_settings/android/website_preference_bridge_util.h"
 #include "components/browsing_data/content/cookie_helper.h"
 #include "components/browsing_data/content/local_storage_helper.h"
 #include "components/cdm/browser/media_drm_storage_impl.h"
@@ -662,14 +664,11 @@ static void JNI_WebsitePreferenceBridge_ClearLocalStorageData(
     const JavaParamRef<jobject>& jbrowser_context_handle,
     const JavaParamRef<jstring>& jorigin,
     const JavaParamRef<jobject>& java_callback) {
-  BrowserContext* browser_context = unwrap(jbrowser_context_handle);
-  auto local_storage_helper =
-      base::MakeRefCounted<browsing_data::LocalStorageHelper>(browser_context);
-  auto storage_key = blink::StorageKey(
-      url::Origin::Create(GURL(ConvertJavaStringToUTF8(env, jorigin))));
-  local_storage_helper->DeleteStorageKey(
-      storage_key, base::BindOnce(&OnLocalStorageCleared,
-                                  ScopedJavaGlobalRef<jobject>(java_callback)));
+  ClearLocalStorageHelper::ClearLocalStorage(
+      unwrap(jbrowser_context_handle),
+      url::Origin::Create(GURL(ConvertJavaStringToUTF8(env, jorigin))),
+      base::BindOnce(&OnLocalStorageCleared,
+                     ScopedJavaGlobalRef<jobject>(java_callback)));
 }
 
 static void JNI_WebsitePreferenceBridge_ClearStorageData(
---
+++ components/browser_ui/site_settings/android/website_preference_bridge_util.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_BROWSER_UI_SITE_SETTINGS_ANDROID_WEBSITE_PREFERENCE_BRIDGE_UTIL_H_
+#define COMPONENTS_BROWSER_UI_SITE_SETTINGS_ANDROID_WEBSITE_PREFERENCE_BRIDGE_UTIL_H_
+
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/browsing_data_remover.h"
+
+class ClearLocalStorageHelper : public content::BrowsingDataRemover::Observer {
+ public:
+  ~ClearLocalStorageHelper() override;
+
+  static void ClearLocalStorage(content::BrowserContext* browser_context,
+                                const url::Origin& origin,
+                                base::OnceClosure callback);
+
+ private:
+  ClearLocalStorageHelper(content::BrowsingDataRemover* remover,
+                          base::OnceClosure callback);
+
+  // Implements content::BrowsingDataRemover::Observer.
+  void OnBrowsingDataRemoverDone(uint64_t failed_data_types) override;
+
+  content::BrowsingDataRemover* remover_;
+  base::OnceClosure callback_;
+};
+
+#endif  // COMPONENTS_BROWSER_UI_SITE_SETTINGS_ANDROID_WEBSITE_PREFERENCE_BRIDGE_UTIL_H_
