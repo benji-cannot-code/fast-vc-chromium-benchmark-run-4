ef1fbe0ca3267e97155e6f4b2cd0e6d8047dc2c1
anthie@google.com <anthie@google.com>
2023-01-13 20:56:13 +0000

    Add test case for parental control child web filter
    Extend the current tests for the family info feedback source
    to include child-specific information.
    
    Bug: b/260093095
    Change-Id: I639166327b4b925888a110b677b116e100247bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110791
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092596}

--- chrome/browser/feedback/android/family_info_feedback_source.h
+++ chrome/browser/feedback/android/family_info_feedback_source.h
@@ -40,6 +40,7 @@ class FamilyInfoFeedbackSource : public FamilyInfoFetcher::Consumer {
 
  private:
   friend class FamilyInfoFeedbackSourceTest;
+  friend class FamilyInfoFeedbackSourceForChildFilterBehaviorTest;
 
   // FamilyInfoFetcher::Consumer implementation.
   void OnGetFamilyMembersSuccess(
