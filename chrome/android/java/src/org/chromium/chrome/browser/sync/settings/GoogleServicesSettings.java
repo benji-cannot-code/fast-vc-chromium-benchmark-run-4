bc3ca01a2604da1e031315a6b3bd439658133390
Rubin Deliallisi <rubindl@chromium.org>
2023-01-13 10:26:55 +0000

    Privacy Sandbox Settings: Move Digital Wellbeing preference
    - Move the digital wellbeing preference from the P&S page to Google
    services.
    
    Bug: 1378703, b/259362554
    Change-Id: I59f9da162775694b9a7b76bbc9f40bcdea9d0d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161917
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092313}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/GoogleServicesSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/GoogleServicesSettings.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.sync.settings;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -35,6 +36,7 @@ import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.chrome.browser.signin.services.UnifiedConsentServiceBridge;
 import org.chromium.chrome.browser.ui.signin.SignOutDialogCoordinator;
 import org.chromium.chrome.browser.ui.signin.SignOutDialogCoordinator.Listener;
+import org.chromium.chrome.browser.usage_stats.UsageStatsConsentDialog;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.ManagedPreferenceDelegate;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
@@ -66,6 +68,8 @@ public class GoogleServicesSettings extends PreferenceFragmentCompat
     @VisibleForTesting
     public static final String PREF_AUTOFILL_ASSISTANT_SUBSECTION = "autofill_assistant_subsection";
     @VisibleForTesting
+    public static final String PREF_USAGE_STATS_REPORTING = "usage_stats_reporting";
+    @VisibleForTesting
     public static final String PREF_METRICS_SETTINGS = "metrics_settings";
     @VisibleForTesting
     public static final String PREF_PRICE_TRACKING_ANNOTATIONS = "price_tracking_annotations";
@@ -84,6 +88,7 @@ public class GoogleServicesSettings extends PreferenceFragmentCompat
     private ChromeSwitchPreference mPriceTrackingAnnotations;
     private @Nullable Preference mContextualSearch;
     private Preference mPriceNotificationSection;
+    private Preference mUsageStatsReporting;
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, String rootKey) {
@@ -150,6 +155,14 @@ public class GoogleServicesSettings extends PreferenceFragmentCompat
         } else {
             removePreference(getPreferenceScreen(), mPriceNotificationSection);
             mPriceNotificationSection = null;
+        }
+
+        mUsageStatsReporting = findPreference(PREF_USAGE_STATS_REPORTING);
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_4)) {
+            mUsageStatsReporting.setVisible(true);
+        } else {
+            removePreference(getPreferenceScreen(), mUsageStatsReporting);
+            mUsageStatsReporting = null;
         }
 
         updatePreferences();
@@ -249,6 +262,25 @@ public class GoogleServicesSettings extends PreferenceFragmentCompat
         if (mPriceTrackingAnnotations != null) {
             mPriceTrackingAnnotations.setChecked(
                     PriceTrackingUtilities.isTrackPricesOnTabsEnabled());
+        }
+        if (mUsageStatsReporting != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+                    && mPrefService.getBoolean(Pref.USAGE_STATS_ENABLED)) {
+                mUsageStatsReporting.setOnPreferenceClickListener(preference -> {
+                    UsageStatsConsentDialog
+                            .create(getActivity(), true,
+                                    (didConfirm) -> {
+                                        if (didConfirm) {
+                                            updatePreferences();
+                                        }
+                                    })
+                            .show();
+                    return true;
+                });
+            } else {
+                removePreference(getPreferenceScreen(), mUsageStatsReporting);
+                mUsageStatsReporting = null;
+            }
         }
     }
 
