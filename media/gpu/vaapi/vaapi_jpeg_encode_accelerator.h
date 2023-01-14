594b53b5f97e246e67402e7615ad0f8107fdda0e
Hirokazu Honda <hiroh@chromium.org>
2023-01-14 07:36:27 +0000

    media/gpu/vaapiJEA: Use Sequence for JPEG encoding
    VaapiJpegEncodeAccelerator creates a new thread to perform JPEG
    encoding. This CL changes it to use the Sequence for better load
    balancing and reducing context switches in GPU process.
    
    Bug: b:264632353
    Test: Chrome Camera App on volteer and grunt
    Change-Id: I73ea23174fb59e97de44aea31d9c332033f95230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4151133
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092746}

--- media/gpu/vaapi/vaapi_jpeg_encode_accelerator.h
+++ media/gpu/vaapi/vaapi_jpeg_encode_accelerator.h
@@ -10,12 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/shared_memory_mapping.h"
 #include "base/memory/weak_ptr.h"
-#include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread.h"
 #include "components/chromeos_camera/jpeg_encode_accelerator.h"
 #include "media/base/bitstream_buffer.h"
 #include "media/gpu/media_gpu_export.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
+
+namespace base {
+class SingleThreadTaskRunner;
+class SequencedTaskRunner;
+}  // namespace base
 
 namespace media {
 
@@ -96,11 +100,8 @@ class MEDIA_GPU_EXPORT VaapiJpegEncodeAccelerator
   // The client of this class.
   Client* client_ = nullptr;
 
-  base::Thread encoder_thread_;
-
-  // Use this to post tasks to encoder thread.
-  scoped_refptr<base::SingleThreadTaskRunner> encoder_task_runner_;
-
+  // The task runner on which the functions of |encoder_| are executed.
+  scoped_refptr<base::SequencedTaskRunner> encoder_task_runner_;
   std::unique_ptr<Encoder> encoder_;
 
   // |weak_this_| is used to post tasks from |encoder_task_runner_| to
