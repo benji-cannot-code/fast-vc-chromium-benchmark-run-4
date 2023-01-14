d37294b5a074411a0d195846bc0c8b58c2f1c3aa
Dana Fried <dfried@chromium.org>
2023-01-14 00:06:36 +0000

    Reland "[Test Automation/User Education] Instrument Mac RenderView context menus"
    This reverts commit db277237ec0e3c1703c3516ebc079a995f13f3cb.
    
    Reason for revert: Fixed crash bug
    
    Original change's description:
    > Revert "[Test Automation/User Education] Instrument Mac RenderView context menus"
    >
    > This reverts commit 3ca99106e2bf3fcb0452d0a9851b8267f7ae012a.
    >
    > Reason for revert: Crashes; https://crbug.com/1407122
    >
    > Original change's description:
    > > [Test Automation/User Education] Instrument Mac RenderView context menus
    > >
    > > This CL fixes two issues with running and testing render view context
    > > menus on Mac (these are the default context menus when right-clicking on
    > > a web page).
    > >
    > > First: while other context menus (tabs, bookmarks, etc.) had been
    > > instrumented for the following:
    > >  - Display the "New" badge
    > >  - Highlight during Tutorial/IPH
    > >  - Attach help bubbles during Tutorials
    > >  - Find and select menu items during Kombucha tests
    > >
    > > The render view context menu, which did not use a cocoa menu delegate,
    > > did not include these improvements.
    > >
    > > This CL takes the delegate object which provides all of this
    > > functionality and promotes it to its own file with a public API. It
    > > then adds it to the render view context menu, which immediately
    > > enables all of the missing functionality.
    > >
    > > This was added because it was one of the reasons a test we were trying
    > > to add was failing; that test (along with related tests which are
    > > unrelated to this bug) will be added in a downstream CL.
    > >
    > > Change-Id: I768ea0b8246b26923261b9d0981ef42dae4321f1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159894
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Commit-Queue: Dana Fried <dfried@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1091941}
    >
    > Change-Id: Ia07f8d611b35befb6bc1d5576b8f9fb202b0e1fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165925
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1092587}
    
    Change-Id: Ibc09f5a8950389e20efc3ab6f2ae98098dde292e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166621
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092682}

--- chrome/browser/ui/cocoa/renderer_context_menu/render_view_context_menu_mac_cocoa.h
+++ chrome/browser/ui/cocoa/renderer_context_menu/render_view_context_menu_mac_cocoa.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/renderer_context_menu/render_view_context_menu_mac.h"
 
 @class MenuControllerCocoa;
+@class MenuControllerCocoaDelegateImpl;
 
 // Mac Cocoa implementation of the renderer context menu display code. Uses a
 // NSMenu to display the context menu. Internally uses an Obj-C object as the
@@ -45,6 +46,8 @@ class RenderViewContextMenuMacCocoa : public RenderViewContextMenuMac {
 
   // The Cocoa menu controller for this menu.
   base::scoped_nsobject<MenuControllerCocoa> menu_controller_;
+  base::scoped_nsobject<MenuControllerCocoaDelegateImpl>
+      menu_controller_delegate_;
 
   // The Cocoa parent view.
   NSView* parent_view_;
---
+++ ui/views/controls/menu/menu_controller_cocoa_delegate_impl.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_CONTROLS_MENU_MENU_CONTROLLER_COCOA_DELEGATE_IMPL_H_
+#define UI_VIEWS_CONTROLS_MENU_MENU_CONTROLLER_COCOA_DELEGATE_IMPL_H_
+
+#import "base/mac/scoped_nsobject.h"
+#import "ui/base/cocoa/menu_controller.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/views/views_export.h"
+
+VIEWS_EXPORT
+@interface MenuControllerCocoaDelegateImpl
+    : NSObject <MenuControllerCocoaDelegate>
+- (void)setAnchorRect:(gfx::Rect)rect;
+@end
+
+#endif  // UI_VIEWS_CONTROLS_MENU_MENU_CONTROLLER_COCOA_DELEGATE_IMPL_H_
