f90c06e360fd2c4bd25bbac507912d3d061b8124
Michael Martis <martis@chromium.org>
2023-01-14 01:02:58 +0000

    [Live Caption] Lacros live caption browsertests.
    Now that LaCrOS supports speech recognition and therefore live caption,
    reenable the live caption browsertests on LaCros, running them alongside
    a test Ash process.
    
    AX-Relnotes: n/a.
    
    Bug: b:223493879
    Change-Id: Ie4db54d83f196c30e244b47dece286216abca1b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4078647
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092702}

--- chrome/browser/accessibility/live_caption_speech_recognition_host_browsertest.cc
+++ chrome/browser/accessibility/live_caption_speech_recognition_host_browsertest.cc
@@ -74,7 +74,7 @@ class LiveCaptionSpeechRecognitionHostTest : public LiveCaptionBrowserTest {
   }
 
   void SetUpOnMainThread() override {
-    InProcessBrowserTest::SetUpOnMainThread();
+    LiveCaptionBrowserTest::SetUpOnMainThread();
     ASSERT_TRUE(embedded_test_server()->Start());
   }
 
--- chrome/browser/accessibility/live_caption_test_util.cc
+++ chrome/browser/accessibility/live_caption_test_util.cc
@@ -22,9 +22,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/crosapi/mojom/crosapi.mojom.h"
+#include "chromeos/lacros/lacros_service.h"
+#include "chromeos/startup/browser_init_params.h"
+#endif
+
 namespace captions {
 
 namespace {
+
 // Chrome feature flags that gate Live Caption.
 std::vector<base::test::FeatureRef> RequiredFeatureFlags() {
   std::vector<base::test::FeatureRef> features = {media::kLiveCaption};
@@ -33,11 +40,30 @@ std::vector<base::test::FeatureRef> RequiredFeatureFlags() {
 #endif
   return features;
 }
+
+// LaCrOS learns about ondevice-speech support via BrowserInitParams.
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+void SetRequiredLacrosInitParams() {
+  crosapi::mojom::BrowserInitParamsPtr init_params =
+      chromeos::BrowserInitParams::GetForTests()->Clone();
+  init_params->is_ondevice_speech_supported = true;
+  chromeos::BrowserInitParams::SetInitParamsForTests(std::move(init_params));
+}
+#endif
+
 }  // namespace
 
 void LiveCaptionBrowserTest::SetUp() {
   scoped_feature_list_.InitWithFeatures(RequiredFeatureFlags(), {});
   InProcessBrowserTest::SetUp();
+}
+
+void LiveCaptionBrowserTest::CreatedBrowserMainParts(
+    content::BrowserMainParts* browser_main_parts) {
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  SetRequiredLacrosInitParams();
+#endif
+  InProcessBrowserTest::CreatedBrowserMainParts(browser_main_parts);
 }
 
 void LiveCaptionBrowserTest::SetLiveCaptionEnabled(bool enabled) {
