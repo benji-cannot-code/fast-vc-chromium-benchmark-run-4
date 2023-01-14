b5edbb16f16d3af80ecfaeda32a0ba4a0f047f26
Emily Shack <emshack@chromium.org>
2023-01-14 01:15:19 +0000

    Bookmarks++: Use iron-list for bookmark rows
    Improve performance on large bookmark counts by lazily loading bookmark
    rows in the side panel.
    
    Bug: 1365606
    Change-Id: I13e0341a19e8a39b1d12126d24ab3b5421f00cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4157932
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092705}

--- chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_list_test.ts
+++ chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_list_test.ts
@@ -94,6 +94,8 @@ suite('SidePanelPowerBookmarksListTest', () => {
   });
 
   test('DefaultsToSortByNewest', () => {
+    flush();
+
     const bookmarkElements = getBookmarkElements(powerBookmarksList);
     // All folders should come first
     assertEquals(bookmarkElements[0]!.id, 'bookmark-5');
@@ -109,6 +111,7 @@ suite('SidePanelPowerBookmarksListTest', () => {
       title: 'New title',
       url: 'http://new/url',
     });
+    flush();
 
     const bookmarkElement = getBookmarkElements(powerBookmarksList)[2]!;
     assertEquals(
