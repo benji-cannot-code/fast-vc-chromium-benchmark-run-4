3d53d9d97098320cc49d926aa6f30a4afeb553a4
Moe Ahmadi <mahmadi@chromium.org>
2023-01-14 01:53:18 +0000

    [webui] Adds ability to chrome://images to encode images as static WebP
    Currently chrome://images re-encodes downloaded images into PNG. This
    is done in part to preserve transparency. Based on local benchmarking
    Webp images are >500% smaller than PNGs and ~25% smaller than JPEGs.
    Aslo, encoding to WebP (at 90% precision) is ~10-20% faster than
    encoding to PNG while also preserving transparency.
    
    Therefore, all evidence suggests using WebP encoding would dramatically
    reduce the size of external images used in WebUI and slightly improve
    the time it takes to transcode them.
    
    Bug: 1406579
    Change-Id: I4bf83bbd15bad42fff18ad1813919a8b3b3fd925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4160512
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092714}

--- chrome/test/data/webui/cr_elements/cr_auto_img_test.ts
+++ chrome/test/data/webui/cr_elements/cr_auto_img_test.ts
@@ -95,7 +95,7 @@ suite('CrAutoImgElementTest', () => {
         const autoSrc = 'https://foo.com/img.png';
 
         // Act.
-        img.setAttribute('is-google-photos', '');
+        img.toggleAttribute('is-google-photos', true);
         img.autoSrc = autoSrc;
 
         // Assert.
@@ -165,7 +165,7 @@ suite('CrAutoImgElementTest', () => {
         const autoSrc = 'https://foo.com/img.png';
 
         // Act.
-        img.setAttribute('static-encode', '');
+        img.toggleAttribute('static-encode', true);
         img.autoSrc = autoSrc;
 
         // Assert.
@@ -182,12 +182,57 @@ suite('CrAutoImgElementTest', () => {
       });
 
   test(
+      'setting encodeType creates a URL with autoSrc and encodeType as params',
+      () => {
+        const autoSrc = 'https://foo.com/img.png';
+
+        // Act.
+        img.encodeType = 'jpeg';
+        img.autoSrc = autoSrc;
+
+        // Assert.
+        assertEquals(
+            `chrome://image/?url=${
+                encodeURIComponent(autoSrc)}&encodeType=jpeg`,
+            img.src);
+
+        // Act.
+        img.encodeType = '';
+
+        // Assert.
+        assertEquals(`chrome://image/?${autoSrc}`, img.src);
+      });
+
+  test(
+      'setting encode-type creates a URL with autoSrc and encodeType as params',
+      () => {
+        const autoSrc = 'https://foo.com/img.png';
+
+        // Act.
+        img.setAttribute('encode-type', 'webp');
+        img.autoSrc = autoSrc;
+
+        // Assert.
+        assertEquals(
+            `chrome://image/?url=${
+                encodeURIComponent(autoSrc)}&encodeType=webp`,
+            img.src);
+
+        // Act.
+        img.removeAttribute('encode-type');
+
+        // Assert.
+        assertEquals(`chrome://image/?${autoSrc}`, img.src);
+      });
+
+  test(
       'setting multiple attributes creates a URL with all of the params',
       () => {
         const autoSrc = 'https://foo.com/img.png';
 
         // Act.
-        img.setAttribute('static-encode', '');
+        img.toggleAttribute('static-encode', true);
+        img.toggleAttribute('encode-type', true);
         img.isGooglePhotos = true;
         img.autoSrc = autoSrc;
 
@@ -195,7 +240,7 @@ suite('CrAutoImgElementTest', () => {
         assertEquals(
             `chrome://image/?url=${
                 encodeURIComponent(
-                    autoSrc)}&isGooglePhotos=true&staticEncode=true`,
+                    autoSrc)}&isGooglePhotos=true&staticEncode=true&encodeType=`,
             img.src);
       });
 });
--- ui/webui/resources/cr_elements/cr_auto_img/cr_auto_img.ts
+++ ui/webui/resources/cr_elements/cr_auto_img/cr_auto_img.ts
@@ -17,8 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *      <img is="cr-auto-img" auto-src="https://foo.com/bar.png">
  *
  *      If your image URL points to Google Photos storage, meaning it needs an
- *      auth token to be downloaded, you can use the is-google-photos attribute
- *      as follows:
+ *      auth token, you can use the is-google-photos attribute as follows:
  *
  *      <img is="cr-auto-img" auto-src="https://foo.com/bar.png"
  *          is-google-photos>
@@ -28,11 +27,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  *      <img is="cr-auto-img" auto-src="[[calculateSrc()]]" clear-src>
  *
- *      If you want your image to be always encoded as static PNG image (even if
+ *      If you want your image to be always encoded as a static image (even if
  *      the source image is animated), set the static-encode attribute:
  *
  *      <img is="cr-auto-img" auto-src="https://foo.com/bar.png"
  *          static-encode>
+ *
+ *      Static images are encoded as PNG by default. If you want your image to
+ *      be encoded as a Webp image, set the encode-type attribute to "webp".
+ *
+ *      <img is="cr-auto-img" auto-src="https://foo.com/bar.png"
+ *          static-encode encode-type="webp">
  *
  * NOTE: Since <cr-auto-img> may use the chrome://image data source some images
  * may be transcoded to PNG.
@@ -46,15 +51,17 @@ const IS_GOOGLE_PHOTOS: string = 'is-google-photos';
 
 const STATIC_ENCODE: string = 'static-encode';
 
+const ENCODE_TYPE: string = 'encode-type';
+
 export class CrAutoImgElement extends HTMLImageElement {
   static get observedAttributes() {
-    return [AUTO_SRC, IS_GOOGLE_PHOTOS, STATIC_ENCODE];
+    return [AUTO_SRC, IS_GOOGLE_PHOTOS, STATIC_ENCODE, ENCODE_TYPE];
   }
 
   attributeChangedCallback(
       name: string, oldValue: string|null, newValue: string|null) {
     if (name !== AUTO_SRC && name !== IS_GOOGLE_PHOTOS &&
-        name !== STATIC_ENCODE) {
+        name !== STATIC_ENCODE && name !== ENCODE_TYPE) {
       return;
     }
 
@@ -89,7 +96,7 @@ export class CrAutoImgElement extends HTMLImageElement {
       return;
     }
     if (!this.hasAttribute(IS_GOOGLE_PHOTOS) &&
-        !this.hasAttribute(STATIC_ENCODE)) {
+        !this.hasAttribute(STATIC_ENCODE) && !this.hasAttribute(ENCODE_TYPE)) {
       this.src = 'chrome://image?' + url.href;
       return;
     }
@@ -101,6 +108,9 @@ export class CrAutoImgElement extends HTMLImageElement {
     if (this.hasAttribute(STATIC_ENCODE)) {
       this.src += `&staticEncode=true`;
     }
+    if (this.hasAttribute(ENCODE_TYPE)) {
+      this.src += `&encodeType=${this.getAttribute(ENCODE_TYPE)}`;
+    }
   }
 
   set autoSrc(src: string) {
@@ -108,7 +118,7 @@ export class CrAutoImgElement extends HTMLImageElement {
   }
 
   get autoSrc(): string {
-    return this.getAttribute(AUTO_SRC)!;
+    return this.getAttribute(AUTO_SRC) || '';
   }
 
   set clearSrc(_: string) {
@@ -116,7 +126,7 @@ export class CrAutoImgElement extends HTMLImageElement {
   }
 
   get clearSrc(): string {
-    return this.getAttribute(CLEAR_SRC)!;
+    return this.getAttribute(CLEAR_SRC) || '';
   }
 
   set isGooglePhotos(enabled: boolean) {
@@ -141,6 +151,18 @@ export class CrAutoImgElement extends HTMLImageElement {
 
   get staticEncode(): boolean {
     return this.hasAttribute(STATIC_ENCODE);
+  }
+
+  set encodeType(type: string) {
+    if (type) {
+      this.setAttribute(ENCODE_TYPE, type);
+    } else {
+      this.removeAttribute(ENCODE_TYPE);
+    }
+  }
+
+  get encodeType(): string {
+    return this.getAttribute(ENCODE_TYPE) || '';
   }
 }
 
