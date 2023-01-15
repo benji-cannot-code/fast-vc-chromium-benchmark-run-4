d2a7671b1262bd8137da02e3e5eec27ce78fe0a4
ssid <ssid@chromium.org>
2023-01-15 09:16:45 +0000

    Add new optimization target for device switcher
    Estimate the number of users who switch from other devices.
    Also clean up SegmentId list.
    
    BUG=1386310
    
    Change-Id: I429e8c0cf54fc3709e1ed074aba8cd17ba421466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117124
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092842}

--- components/optimization_guide/core/model_util.cc
+++ components/optimization_guide/core/model_util.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "components/optimization_guide/core/optimization_guide_switches.h"
+#include "components/optimization_guide/proto/models.pb.h"
 #include "net/base/url_util.h"
 #include "url/url_canon.h"
 
@@ -90,6 +91,8 @@ std::string GetStringNameForOptimizationTarget(
       return "ClientSidePhishing";
     case proto::OPTIMIZATION_TARGET_OMNIBOX_URL_SCORING:
       return "OmniboxUrlScoring";
+    case proto::OPTIMIZATION_TARGET_SEGMENTATION_DEVICE_SWITCHER:
+      return "SegmentationDeviceSwitcher";
     case proto::OPTIMIZATION_TARGET_SEGMENTATION_ADAPTIVE_TOOLBAR:
       return "SegmentationAdaptiveToolbar";
       // Whenever a new value is added, make sure to add it to the OptTarget
