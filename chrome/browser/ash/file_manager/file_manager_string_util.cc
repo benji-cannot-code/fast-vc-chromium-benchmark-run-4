6d76ccec2414409cc725b151cf4f0c30cd5a0788
Noel Gordon <noel@chromium.org>
2023-01-16 06:14:00 +0000

    [conflict dialog] Add feature flag
    Bug: b:255264604
    Change-Id: I8d70e6566ce91702ae9a08e2a25214cf415c36cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4170125
    Auto-Submit: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092951}

--- chrome/browser/ash/file_manager/file_manager_string_util.cc
+++ chrome/browser/ash/file_manager/file_manager_string_util.cc
@@ -1128,6 +1128,9 @@ void AddFileManagerFeatureStrings(const std::string& locale,
   dict->Set("FILES_APP_EXPERIMENTAL",
             base::FeatureList::IsEnabled(ash::features::kFilesAppExperimental));
 
+  dict->Set("FILES_CONFLICT_DIALOG",
+            base::FeatureList::IsEnabled(ash::features::kFilesConflictDialog));
+
   dict->Set("FUSEBOX_DEBUG",
             base::FeatureList::IsEnabled(ash::features::kFuseBoxDebug));
 
--- ui/file_manager/file_manager/common/js/util.js
+++ ui/file_manager/file_manager/common/js/util.js
@@ -1098,6 +1098,14 @@ util.isFilesAppExperimental = () => {
 };
 
 /**
+ * Returns true if the conflict dialog is enabled.
+ * @return {boolean}
+ */
+util.isFilesConflictDialogEnabled = () => {
+  return loadTimeData.getBoolean('FILES_CONFLICT_DIALOG');
+};
+
+/**
  * Returns true if FuseBoxDebug flag is enabled.
  * @return {boolean}
  */
