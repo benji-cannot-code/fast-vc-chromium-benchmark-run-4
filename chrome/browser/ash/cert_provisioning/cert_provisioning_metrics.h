0b4447af5716f49784d665cac279b8e82cf3506e
Pavol Marko <pmarko@google.com>
2023-01-16 22:06:18 +0000

    cert_prov: Add "CsrSignTime" metric for "dynamic" flow
    Add the "CsrSignTime" metric for the "dynamic" certificate
    provisioning flow.
    
    I have not renamed it to "DataSignTime" so it's easier to compare them
    in the future.
    
    Bug: b:230478084
    Test: Run unit_tests (dry run) in CL:4127302 before submitting
    Change-Id: Ifb9143c4bf2366d40bb6b549d913140148288342
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4145862
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093151}

--- chrome/browser/ash/cert_provisioning/cert_provisioning_metrics.h
+++ chrome/browser/ash/cert_provisioning/cert_provisioning_metrics.h
@@ -55,8 +55,10 @@ void RecordVerifiedAccessTime(ProtocolVersion protocol_version,
                               CertScope scope,
                               base::TimeDelta sample);
 
-// Records time of signing a CSR by certificate provisioning worker.
-void RecordCsrSignTime(CertScope scope, base::TimeDelta sample);
+// Records time of generating a signature by certificate provisioning worker.
+void RecordDataSignTime(ProtocolVersion protocol_version,
+                        CertScope scope,
+                        base::TimeDelta sample);
 
 }  // namespace cert_provisioning
 }  // namespace ash
