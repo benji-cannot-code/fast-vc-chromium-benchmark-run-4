13e0d26117c547e1bb7999345b59d9dc601d488f
Jaewon Jung <jw.jung@navercorp.com>
2023-01-16 15:46:53 +0000

    Remove expired sanitizer-api-v0 flag
    This flag has been expired, so remove it.
    
    Signed-off-by: Jaewon Jung <jw.jung@navercorp.com>
    Bug: 1401818
    Change-Id: Ib5718b0207c0232fcbe37e691f2726bde89f1ca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4145321
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093068}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1908,9 +1908,6 @@ extern const char kDrawPredictedPointsName[];
 extern const char kSanitizerApiName[];
 extern const char kSanitizerApiDescription[];
 
-extern const char kSanitizerApiv0Name[];
-extern const char kSanitizerApiv0Description[];
-
 extern const char kUsePassthroughCommandDecoderName[];
 extern const char kUsePassthroughCommandDecoderDescription[];
 
