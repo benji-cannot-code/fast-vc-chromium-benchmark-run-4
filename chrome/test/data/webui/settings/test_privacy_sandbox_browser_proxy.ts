364c25bd84e53858892a8a3bb14310e78c96f8e1
Gabriel Oliveira <gabolvr@google.com>
2023-01-16 11:33:12 +0000

    [Privacy Sandbox] Add tests for empty topics lists in Topics subpage
    Bug: 1378703, b/254412602
    Change-Id: I1ff931b6c9c3ae3159e980825bab108ddc831d08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4163290
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093007}

--- chrome/test/data/webui/settings/test_privacy_sandbox_browser_proxy.ts
+++ chrome/test/data/webui/settings/test_privacy_sandbox_browser_proxy.ts
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CanonicalTopic, FledgeState, PrivacySandboxBrowserProxy} from 'chrome://settings/settings.js';
+import {CanonicalTopic, FledgeState, PrivacySandboxBrowserProxy, TopicsState} from 'chrome://settings/settings.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
 export class TestPrivacySandboxBrowserProxy extends TestBrowserProxy implements
     PrivacySandboxBrowserProxy {
   private fledgeState_: FledgeState;
+  private topicsState_: TopicsState;
 
   constructor() {
     super([
@@ -21,6 +22,13 @@ export class TestPrivacySandboxBrowserProxy extends TestBrowserProxy implements
     this.fledgeState_ = {
       joiningSites: ['test-site-one.com'],
       blockedSites: ['test-site-two.com'],
+    };
+
+    this.topicsState_ = {
+      topTopics:
+          [{topicId: 1, taxonomyVersion: 1, displayString: 'test-topic-1'}],
+      blockedTopics:
+          [{topicId: 2, taxonomyVersion: 1, displayString: 'test-topic-2'}],
     };
   }
 
@@ -37,14 +45,13 @@ export class TestPrivacySandboxBrowserProxy extends TestBrowserProxy implements
     this.methodCalled('setFledgeJoiningAllowed', [site, allowed]);
   }
 
+  setTopicsState(topicsState: TopicsState) {
+    this.topicsState_ = topicsState;
+  }
+
   getTopicsState() {
     this.methodCalled('getTopicsState');
-    return Promise.resolve({
-      topTopics:
-          [{topicId: 1, taxonomyVersion: 1, displayString: 'test-topic-1'}],
-      blockedTopics:
-          [{topicId: 2, taxonomyVersion: 1, displayString: 'test-topic-2'}],
-    });
+    return Promise.resolve(this.topicsState_);
   }
 
   setTopicAllowed(topic: CanonicalTopic, allowed: boolean) {
