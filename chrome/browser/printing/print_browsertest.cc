5c44041823f5b63c441aeb6fc2e356b86f101cc8
Fergal Daly <fergal@chromium.org>
2023-01-16 08:03:50 +0000

    Update callers to check return value of TestNavigationManager::Wait*
    This is a no-op change to /chrome/browser/printing to update all
    callers of WaitForFirstYieldAfterDidStartNavigation and
    WaitForNavigationFinished to check for success. Failure could be due
    to a test timeout or other error in which case we should stop the
    test. Proceeding further with the test will just produce confusing
    output.
    
    The return values will be marked `[[nodiscard]]` in a follow-up
    change.
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 1403181
    Change-Id: I5d5b2881a2b854030f0abc2b877f986df15e9a55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4122185
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092968}

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -2172,7 +2172,7 @@ class PrintFencedFrameBrowserTest : public PrintBrowserTest {
     })";
     EXPECT_TRUE(ExecJs(fenced_frame_parent,
                        content::JsReplace(kAddFencedFrameScript, url)));
-    navigation.WaitForNavigationFinished();
+    EXPECT_TRUE(navigation.WaitForNavigationFinished());
 
     content::RenderFrameHost* new_frame = ChildFrameAt(fenced_frame_parent, 0);
 
