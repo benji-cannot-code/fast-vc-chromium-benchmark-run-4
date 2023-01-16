dcedac3973e0cb3eb41a1d90dc9f1f16d7d478d6
Fergal Daly <fergal@chromium.org>
2023-01-16 08:08:40 +0000

    Update callers to check return value of TestNavigationManager::Wait*
    This is a no-op change to /chrome/browser/pdf to update all callers of
    WaitForFirstYieldAfterDidStartNavigation and WaitForNavigationFinished
    to check for success. Failure could be due to a test timeout or other
    error in which case we should stop the test. Proceeding further with
    the test will just produce confusing output.
    
    The return values will be marked `[[nodiscard]]` in a follow-up
    change.
    
    This CL was uploaded by git cl split.
    
    R=mlippautz@chromium.org
    
    Bug: 1403181
    Change-Id: I1a6548e9f304df7bb6e5ac9aea77d59da6ea1c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119934
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092971}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -4756,7 +4756,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionTest, PdfNavigationDuringProfileShutdown) {
   {
     content::TestNavigationManager manager(guest_view->web_contents(),
                                            stream_url);
-    manager.WaitForFirstYieldAfterDidStartNavigation();
+    ASSERT_TRUE(manager.WaitForFirstYieldAfterDidStartNavigation());
   }
 
   // Now, close Incognito and destroy its profile.  This is subtle: simply
