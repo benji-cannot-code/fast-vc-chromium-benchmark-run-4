a4278a23a1bfb491821cb82885d3ee241d3fd06a
Alexander Cooper <alcooper@chromium.org>
2023-01-17 20:25:36 +0000

    Switch construction pattern of XRRuntimeManagerObservers
    an error on https://crrev.com/c/4160186, rather than try to add an
    exemption to work around this which is supposed to be fixed anyway, this
    just tries to remove the (likely) static initializer.
    
    base: :LazyInstance is deprecated and is to be replaced with
    base: :NoDestructor. It seems as though base::LazyInstance may be causing
    Change-Id: I76c41b61e81061591a81f4f862b966e3001a4c95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166231
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093501}

--- content/browser/xr/service/xr_runtime_manager_impl.cc
+++ content/browser/xr/service/xr_runtime_manager_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/lazy_instance.h"
 #include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "base/observer_list.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/trace_event/trace_event.h"
@@ -44,11 +45,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+using XrRuntimeManagerObservers =
+    base::ObserverList<XRRuntimeManager::Observer>;
+
 namespace {
 XRRuntimeManagerImpl* g_xr_runtime_manager = nullptr;
 
-base::LazyInstance<base::ObserverList<XRRuntimeManager::Observer>>::Leaky
-    g_xr_runtime_manager_observers;
+XrRuntimeManagerObservers& GetXrRuntimeManagerObservers() {
+  static base::NoDestructor<XrRuntimeManagerObservers>
+      xr_runtime_manager_observers;
+  return *xr_runtime_manager_observers;
+}
 
 #if !BUILDFLAG(IS_ANDROID)
 bool IsEnabled(const base::CommandLine* command_line,
@@ -84,11 +91,11 @@ XRRuntimeManager* XRRuntimeManager::GetInstanceIfCreated() {
 }
 
 void XRRuntimeManager::AddObserver(XRRuntimeManager::Observer* observer) {
-  g_xr_runtime_manager_observers.Get().AddObserver(observer);
+  GetXrRuntimeManagerObservers().AddObserver(observer);
 }
 
 void XRRuntimeManager::RemoveObserver(XRRuntimeManager::Observer* observer) {
-  g_xr_runtime_manager_observers.Get().RemoveObserver(observer);
+  GetXrRuntimeManagerObservers().RemoveObserver(observer);
 }
 
 void XRRuntimeManager::ExitImmersivePresentation() {
@@ -498,8 +505,9 @@ void XRRuntimeManagerImpl::AddRuntime(
   runtimes_[id] = std::make_unique<BrowserXRRuntimeImpl>(
       id, std::move(device_data), std::move(runtime));
 
-  for (Observer& obs : g_xr_runtime_manager_observers.Get())
+  for (Observer& obs : GetXrRuntimeManagerObservers()) {
     obs.OnRuntimeAdded(runtimes_[id].get());
+  }
 
   for (VRServiceImpl* service : services_) {
     // TODO(sumankancherla): Consider combining with XRRuntimeManager::Observer.
