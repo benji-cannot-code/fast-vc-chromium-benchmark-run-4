e4d188e3ece531d42570909beb013e2c7aa652a8
Hirokazu Honda <hiroh@chromium.org>
2023-01-17 08:29:25 +0000

    media/gpu/vaapi/test/decode_test: Don't log Frame index
    Logging the frame index every frame floods the log of `decode_test`
    too much. This CL removes the logging.
    
          `decode_test.txt` becomes 345 Bytes from 334 MBytes.
    
    Bug: b:264815484
    Test: video.PlatformInterop.h264_180_vaapi_to_vaapi, the size of
    Change-Id: Ia40baa900c8cb7024ebfdb546d9999b70371a096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4170016
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093236}

--- media/gpu/vaapi/test/decode.cc
+++ media/gpu/vaapi/test/decode.cc
@@ -280,7 +280,6 @@ int main(int argc, char** argv) {
     }
 
     for (int i = 0; i < n_frames || n_frames == 0; i++) {
-      LOG(INFO) << "Frame " << i << "...";
       const VideoDecoder::Result res = dec->DecodeNextFrame();
 
       if (res == VideoDecoder::kEOStream) {
