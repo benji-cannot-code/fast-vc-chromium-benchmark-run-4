f7f0d8119ba055d4d2ea5a781f1584a0987ad03c
Susanne Westphal <swestphal@google.com>
2023-01-17 19:22:59 +0000

    [WebEngine] Fix: reset sandbox connection when unbinding the service
    Change-Id: I44ded181d74e6f665bda688b6385c7f0e5c519d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173995
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093447}

--- weblayer/public/java/org/chromium/webengine/WebSandbox.java
+++ weblayer/public/java/org/chromium/webengine/WebSandbox.java
@@ -184,6 +184,7 @@ public class WebSandbox {
 
         void unbind() {
             mContext.unbindService(this);
+            sSandboxConnectionInstance = null;
             sInstance = null;
         }
 
