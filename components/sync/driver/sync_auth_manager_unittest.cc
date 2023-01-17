8864e34ee8063406be7b6b8b5f632a637e2c4591
Mikel Astiz <mastiz@chromium.org>
2023-01-17 13:27:21 +0000

    [sync] Clean up feature SyncPauseUponAnyPersistentAuthError
    The feature has been enabled since https://crrev.com/3934839 which
    rolled out recently to users, without any reported issue. It is unlikely
    that new issues are reported, so this patch cleans up the legacy code
    and the feature toggle itself.
    
    SyncAuthManager gets simplified because the change removes the one
    last codepath that populated last_auth_error_ with a transient error.
    Note however that, subtly enough, SyncAuthManager::GetLastAuthError()
    can still return transient errors, given the special-casing for HTTP
    errors returned by the Sync server.
    
    Remaining non-trivial cleanups linked to the same bug and subject to
    follow-up patches.
    
    Bug: 1156584
    Change-Id: I3e468ce23877eaa032b4cc6e7cbe97049358a6a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165948
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093293}

--- components/sync/driver/sync_auth_manager_unittest.cc
+++ components/sync/driver/sync_auth_manager_unittest.cc
@@ -8,13 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/run_loop.h"
 #include "base/test/mock_callback.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
-#include "components/sync/base/features.h"
 #include "components/sync/engine/connection_status.h"
 #include "components/sync/engine/sync_credentials.h"
 #include "net/base/net_errors.h"
@@ -761,8 +759,6 @@ TEST_F(SyncAuthManagerTest, ClearsCredentialsOnInvalidRefreshToken) {
 }
 
 TEST_F(SyncAuthManagerTest, EntersPausedStateOnPersistentAuthError) {
-  base::test::ScopedFeatureList feature(kSyncPauseUponAnyPersistentAuthError);
-
   CoreAccountId account_id =
       identity_env()
           ->MakePrimaryAccountAvailable("test@email.com",
