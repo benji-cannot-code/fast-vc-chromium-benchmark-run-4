324908ee664b66183d4c6f3ad1943825891f101b
Jonathan Lee <jonathanjlee@google.com>
2023-01-17 23:53:56 +0000

    [blinkpy] Fix method name for deriving suite name
    Also, left a note to dedupe `update_expectations_for_flag_specific`,
    which doesn't have test coverage (`update_expectations` does).
    
    Bug: 1406978, 1344709
    Change-Id: I234b0d9239683e4f6a4c1b1520137b3c220cb385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174143
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093616}

--- third_party/blink/tools/blinkpy/w3c/wpt_expectations_updater.py
+++ third_party/blink/tools/blinkpy/w3c/wpt_expectations_updater.py
@@ -155,6 +155,8 @@ class WPTExpectationsUpdater(object):
             mapping tests that couldn't be rebaselined to lists of expectation
             lines written to flag specific test expectations.
         """
+        # TODO(crbug.com/1344709): This method has no coverage and should be
+        # merged with `update_expectations`.
         self.port.wpt_manifest.cache_clear()
 
         issue_number = self.get_issue_number()
@@ -165,7 +167,7 @@ class WPTExpectationsUpdater(object):
         # instead of hardcoding.
         builder = 'linux-blink-rel'
         builder_names = [builder]
-        test_suite = self.flag_specific_suite(builder, flag_specific)
+        test_suite = self.suite_for_builder(builder, flag_specific)
 
         build_to_status = self.git_cl.latest_try_jobs(
             builder_names=builder_names,
