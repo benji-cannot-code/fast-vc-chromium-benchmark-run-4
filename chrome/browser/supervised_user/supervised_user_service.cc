8a9a4f28e72d6a55f65da73db44cdb936fb94ecc
James Lee <ljjlee@google.com>
2023-01-17 15:52:30 +0000

    Remove unused parameter from SupervisedUserURLFilter::InitAsyncURLChecker
    Bug: b/265146253
    Change-Id: I7e92b2d180550258ae55a0b732eca050b1ecefe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4171823
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093339}

--- chrome/browser/supervised_user/supervised_user_service.cc
+++ chrome/browser/supervised_user/supervised_user_service.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "extensions/buildflags/buildflags.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
-#include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -544,9 +543,7 @@ void SupervisedUserService::UpdateAsyncUrlChecker() {
 
   if (use_online_check != url_filter_.HasAsyncURLChecker()) {
     if (use_online_check) {
-      url_filter_.InitAsyncURLChecker(
-          profile_->GetDefaultStoragePartition()
-              ->GetURLLoaderFactoryForBrowserProcess());
+      url_filter_.InitAsyncURLChecker();
     } else {
       url_filter_.ClearAsyncURLChecker();
     }
--- chrome/browser/supervised_user/supervised_user_url_filter.h
+++ chrome/browser/supervised_user/supervised_user_url_filter.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/sequence_checker.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/supervised_user/supervised_users.h"
 #include "components/safe_search_api/url_checker.h"
 #include "components/supervised_user/core/browser/supervised_user_error_page.h"
 
@@ -30,10 +29,6 @@ class TaskRunner;
 namespace content {
 class WebContents;
 }  // namespace content
-
-namespace network {
-class SharedURLLoaderFactory;
-}  // namespace network
 
 // This class manages the filtering behavior for URLs, i.e. it tells callers
 // if a URL should be allowed or blocked. It uses information
@@ -215,8 +210,7 @@ class SupervisedUserURLFilter {
   void SetManualURLs(std::map<GURL, bool> url_map);
 
   // Initializes the experimental asynchronous checker.
-  void InitAsyncURLChecker(
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
+  void InitAsyncURLChecker();
 
   // Clears any asynchronous checker.
   void ClearAsyncURLChecker();
