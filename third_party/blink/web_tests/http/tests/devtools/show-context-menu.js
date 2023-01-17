691cd5e3e09843dd7cba525ab9728b06e2a620a4
Danil Somsikov <dsv@chromium.org>
2023-01-17 05:28:30 +0000

    Check arguments type in DevToolsHost.showContextMenuAtPoint
    Bug: 1405574
    Change-Id: Id06637839096402e05a2278b06f2f84b3037e21d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165089
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093205}

---
+++ third_party/blink/web_tests/http/tests/devtools/show-context-menu.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests type checks in DevToolsHost.showContextMenuAtPoint\n`);
+  InspectorFrontendHost.showContextMenuAtPoint(1.1, 2.2, [0x41414141]);
+  TestRunner.completeTest();
+})();
+
