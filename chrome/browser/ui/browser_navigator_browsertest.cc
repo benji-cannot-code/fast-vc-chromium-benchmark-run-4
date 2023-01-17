c334f1cb72b0ffcc1281203d1b2a082d2c57c6f2
Frank Liberato <liberato@chromium.org>
2023-01-17 07:40:26 +0000

    Re-enable lacros for Document Picture in Picture
    After local and bot testing, I think that Document Picture in Picture
    is no longer causing test flakiness.  Or, if it is, the data I'd get
    from those flakes would really help to figure out what's broken.
    
    What I checked:
     - Re-ran several weeks ago with the offending DCHECK[1]
       converted into a CHECK.  All runs passed.
     - Couldn't repro locally on lacros-on-linux.
     - At some point [1] later, the rest of the previously failing test
       was turned off for lacros for being flaky, even with the Document
       Picture in Picture changes still turned off.  The failure [2]
       is the same DCHECK that Document PiP triggered sometimes.
        - Check was for IsVisible, here [3].
     - Re-based and re-ran again.  No issues, though wouldn't expect
       any now.
    
    I don't know why the test was more flaky with Document Picture
    in Picture enabled.  We've made quite a few modifications to
    Document PiP for other platforms, including CrOS, so it's
    believable that some change we made was relevant in making it
    hard to repro now.  However, since the test is flaky anyway, it
    suggests that Document PiP was just the messenger.
    
    [1] https://bugs.chromium.org/p/chromium/issues/detail?id=1394195
    [2] https://chromium-swarm.appspot.com/task?id=5ecf3f3c5b84a011&w=true
    [3] https://source.chromium.org/chromium/chromium/src/+/main:components/exo/shell_surface_base.cc;drc=5789d6770e1553c95309d917270098e5937d2d82;l=1883
    
    Bug: 1320453
    Change-Id: Idbbbdced5809b7aa321a52ff912cef929b9eaaa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946517
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093225}

--- chrome/browser/ui/browser_navigator_browsertest.cc
+++ chrome/browser/ui/browser_navigator_browsertest.cc
@@ -1860,11 +1860,9 @@ IN_PROC_BROWSER_TEST_F(BrowserNavigatorTest, SubFrameNavigationUIData) {
 }
 #endif
 
-#if !BUILDFLAG(IS_CHROMEOS_LACROS)
-// Helper class to enable picture in picture V2 for those tests that need it.
-// Once the feature is enabled permanently, these can be merged back to
-// BrowserNavigatorTest instead.
-// See crbug.com/1320453 for why this is off for lacros.
+//  Helper class to enable picture in picture V2 for those tests that need it.
+//  Once the feature is enabled permanently, these can be merged back to
+//  BrowserNavigatorTest instead.
 class BrowserNavigatorWithPictureInPictureTest : public BrowserNavigatorTest {
   base::test::ScopedFeatureList scoped_feature_list_{
       blink::features::kDocumentPictureInPictureAPI};
@@ -1918,6 +1916,5 @@ IN_PROC_BROWSER_TEST_F(BrowserNavigatorTest,
 
   EXPECT_EQ(params.browser, nullptr);
 }
-#endif  // !IS_CHROMEOS_LACROS
 
 }  // namespace
