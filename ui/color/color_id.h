fdae090c986400542de67217ef7880ee0a9d939d
Thomas Lukaszewicz <tluk@chromium.org>
2023-01-17 23:21:59 +0000

    [color] Add missing Neutral ref tokens
    See GM3 token overview below
    https://carbon.googleplex.com/google-material-3/pages/gm-3-color-scheme/tokens#f759f4d6-cdc9-44cb-8843-80b026e4daea
    
    Bug: None
    Change-Id: I2dab15cd0c03d113c42106b94aac957fe0e385c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4170205
    Commit-Queue: Sean Kau <skau@chromium.org>
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093592}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -67,16 +67,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefError99) \
   E_CPONLY(kColorRefError100) \
   E_CPONLY(kColorRefNeutral0) \
+  E_CPONLY(kColorRefNeutral4) \
+  E_CPONLY(kColorRefNeutral6) \
   E_CPONLY(kColorRefNeutral10) \
+  E_CPONLY(kColorRefNeutral12) \
+  E_CPONLY(kColorRefNeutral17) \
   E_CPONLY(kColorRefNeutral20) \
+  E_CPONLY(kColorRefNeutral22) \
+  E_CPONLY(kColorRefNeutral24) \
   E_CPONLY(kColorRefNeutral30) \
   E_CPONLY(kColorRefNeutral40) \
   E_CPONLY(kColorRefNeutral50) \
   E_CPONLY(kColorRefNeutral60) \
   E_CPONLY(kColorRefNeutral70) \
   E_CPONLY(kColorRefNeutral80) \
+  E_CPONLY(kColorRefNeutral87) \
   E_CPONLY(kColorRefNeutral90) \
+  E_CPONLY(kColorRefNeutral92) \
+  E_CPONLY(kColorRefNeutral94) \
   E_CPONLY(kColorRefNeutral95) \
+  E_CPONLY(kColorRefNeutral96) \
+  E_CPONLY(kColorRefNeutral98) \
   E_CPONLY(kColorRefNeutral99) \
   E_CPONLY(kColorRefNeutral100) \
   E_CPONLY(kColorRefNeutralVariant0) \
