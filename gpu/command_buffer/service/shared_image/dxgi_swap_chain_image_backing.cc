bfc64472ccab4feed2d67492a61a340b1a1f2201
David Bienvenu <davidbienvenu@chromium.org>
2023-01-17 19:45:13 +0000

    win7dep: Remove Win7/8 code from gpu/*
    Also fixes some lint warnings.
    
    Bug: 1385856
    Change-Id: Ib7332b3b81a9a0a787dd92e0a7b950b6122c0bd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135202
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093468}

--- gpu/command_buffer/service/shared_image/dxgi_swap_chain_image_backing.cc
+++ gpu/command_buffer/service/shared_image/dxgi_swap_chain_image_backing.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/trace_event/trace_event.h"
-#include "base/win/windows_version.h"
 #include "gpu/command_buffer/common/mailbox.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/service/memory_tracking.h"
@@ -42,9 +41,7 @@ namespace {
 const char* kDXGISwapChainImageBackingLabel = "DXGISwapChainImageBacking";
 
 bool IsWaitableSwapChainEnabled() {
-  // Waitable swap chains were first enabled in Win 8.1/DXGI 1.3
-  return (base::win::GetVersion() >= base::win::Version::WIN8_1) &&
-         base::FeatureList::IsEnabled(features::kDXGIWaitableSwapChain);
+  return base::FeatureList::IsEnabled(features::kDXGIWaitableSwapChain);
 }
 
 UINT GetMaxWaitableQueuedFrames() {
