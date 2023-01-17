ff95b725d0d9ea4a8fbd82bf53e1eea16db69d81
Wes Okuhara <wesokuhara@google.com>
2023-01-17 21:58:58 +0000

    CrOS Settings: Load HTML templates at runtime for multidevice page
    Uses html_to_wrapper() instead of html_to_js() for web component files
    to aid in loading HTML templates at runtime. This is done so that the
    line numbers of reported errors match the checked-in code.
    
    Bug: chromium:1292025
    Test: browser_tests --gtest_filter='*OSSettingsMultidevice*'
    Change-Id: Ia9b9f2d038a2f9fc52f7ed973a22049c48214ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148835
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093546}

--- chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_feature_toggle.js
+++ chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_feature_toggle.js
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_toggle/cr_toggle.js';
 
-import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {MultiDeviceFeature, MultiDeviceFeatureState} from './multidevice_constants.js';
 import {MultiDeviceFeatureBehavior, MultiDeviceFeatureBehaviorInterface} from './multidevice_feature_behavior.js';
-
+import {getTemplate} from './multidevice_feature_toggle.html.js';
 
 /**
  * @constructor
@@ -36,7 +36,7 @@ export class SettingsMultideviceFeatureToggleElement extends
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 
   static get properties() {
--- chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_smartlock_item.js
+++ chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_smartlock_item.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './multidevice_feature_item.js';
 
 import {WebUIListenerBehavior, WebUIListenerBehaviorInterface} from 'chrome://resources/ash/common/web_ui_listener_behavior.js';
-import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {recordSettingChange} from '../metrics_recorder.js';
 import {routes} from '../os_route.js';
@@ -23,6 +23,7 @@ import {OsSettingsRoutes} from '../os_settings_routes.js';
 import {MultiDeviceBrowserProxy, MultiDeviceBrowserProxyImpl} from './multidevice_browser_proxy.js';
 import {MultiDeviceFeature, MultiDevicePageContentData, MultiDeviceSettingsMode} from './multidevice_constants.js';
 import {MultiDeviceFeatureBehavior, MultiDeviceFeatureBehaviorInterface} from './multidevice_feature_behavior.js';
+import {getTemplate} from './multidevice_smartlock_item.html.js';
 
 /**
  * @constructor
@@ -41,7 +42,7 @@ class SettingsMultideviceSmartlockItemElement extends
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 
   static get properties() {
