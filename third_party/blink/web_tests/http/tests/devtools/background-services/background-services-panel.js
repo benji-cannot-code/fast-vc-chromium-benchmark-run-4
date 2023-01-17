42ce238956c8515365a7b4a36f196a4dfcb27526
Danil Somsikov <dsv@chromium.org>
2023-01-17 15:41:26 +0000

    Don't enable background services experiment in layout tests.
    Bug: 1407922
    Change-Id: Ib3e889e7a2a8ed375011b85e1061f71fe1afea81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165836
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093333}

--- third_party/blink/web_tests/http/tests/devtools/background-services/background-services-panel.js
+++ third_party/blink/web_tests/http/tests/devtools/background-services/background-services-panel.js
@@ -41,8 +41,6 @@ async function toggleRecord(model) {
 }
 
 (async function() {
-  Root.Runtime.experiments.setEnabled('backgroundServices', true);
-
   TestRunner.addResult(`Tests the bottom panel shows information as expected.\n`);
   await TestRunner.loadLegacyModule('resources');
   await TestRunner.showPanel('resources');
