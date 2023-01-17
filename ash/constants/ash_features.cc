89b2a1ddd323c5b86c4cdc24d780a07646c47ef3
Sparik Hayrapetyan <ust@google.com>
2023-01-17 23:55:17 +0000

    [ipp-client-info] Return client-info values in GetIppClientInfo
    Return client-info values in LocalPrinter::GetIppClientInfo. The value
    with OS version info is always returned for IPP printers, while the
    admin-configured value with device info is only returned if the IPP
    destination uses a secure scheme for connection and the current user is
    an affiliated user.
    Add a kIppClientInfo feature flag and only send client-info values if
    the feature is enabled.
    
    Fixed: b/262623335
    Change-Id: Ic4ce44488dfe2047c9f487f32f0751d7c83f5e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4108510
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Sparik Hayrapetyan <ust@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093618}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1235,6 +1235,9 @@ BASE_FEATURE(kInternalServerSideSpeechRecognition,
              "InternalServerSideSpeechRecognition",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables sending `client-info` values to IPP printers on ChromeOS.
+BASE_FEATURE(kIppClientInfo, "IppClientInfo", base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Enables Jelly features.
 BASE_FEATURE(kJelly, "Jelly", base::FEATURE_DISABLED_BY_DEFAULT);
 
@@ -2721,6 +2724,10 @@ bool IsInternalServerSideSpeechRecognitionEnabled() {
 #else
   return false;
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+}
+
+bool IsIppClientInfoEnabled() {
+  return base::FeatureList::IsEnabled(kIppClientInfo);
 }
 
 bool IsJellyEnabled() {
