0311a40bede2ae64375679d6963b9c2980d56de9
Noel Gordon <noel@chromium.org>
2023-01-17 06:43:44 +0000

    quick view: remove files-ng attribute test in onMetadataButtonTap_
    More quick view cleaning: files-ng is a given. No change in behavior
    so no new tests.
    
    Bug: b/245854340
    Test: browser_tests --gtest_filter="QuickView*FilesApp*"
    Change-Id: I0e5f4a3f7a2559d0f92276e1928a81104aeff337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4170094
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093213}

--- ui/file_manager/file_manager/foreground/elements/files_quick_view.js
+++ ui/file_manager/file_manager/foreground/elements/files_quick_view.js
@@ -235,9 +235,7 @@ export const FilesQuickView = Polymer({
    * @private
    */
   onMetadataButtonTap_: function(event) {
-    if (this.hasAttribute('files-ng')) {
-      this.metadataBoxActive = !this.metadataBoxActive;
-    }
+    this.metadataBoxActive = !this.metadataBoxActive;
   },
 
   /**
