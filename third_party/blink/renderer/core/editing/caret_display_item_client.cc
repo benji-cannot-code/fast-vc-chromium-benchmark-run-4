eda1827e4f28ad18b8071ecff588f17e735b59a5
Philip Rogers <pdr@chromium.org>
2023-01-17 19:39:34 +0000

    Ensure changes to painted caret property state cause a full update
    We have an optimization to do a fast "repaint-only" update when paint
    chunks do not change state. If changes can affect layerization, a full
    paint artifact compositor update is needed though. This patch fixes a
    bug where the caret's clip could change without causing a full update.
    
    Bug: 1402110
    Change-Id: I4324edff5f5b5119f39074a67fbc536a9bd8301f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144277
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093464}

--- third_party/blink/renderer/core/editing/caret_display_item_client.cc
+++ third_party/blink/renderer/core/editing/caret_display_item_client.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/editing/visible_units.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
-#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/layout/layout_block.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
@@ -40,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/paint_invalidator.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
+#include "third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h"
 #include "third_party/blink/renderer/platform/graphics/dark_mode_filter.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_context.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
@@ -161,8 +161,13 @@ void CaretDisplayItemClient::UpdateStyleAndLayoutIfNeeded(
     if (layout_block_)
       layout_block_->SetShouldCheckForPaintInvalidation();
     layout_block_ = new_layout_block;
-    if (new_layout_block)
+
+    if (new_layout_block) {
       needs_paint_invalidation_ = true;
+      // The caret property tree space may have changed.
+      layout_block_->GetFrameView()->SetPaintArtifactCompositorNeedsUpdate(
+          PaintArtifactCompositorUpdateReason::kFrameCaretPaint);
+    }
   }
 
   if (!new_layout_block) {
@@ -174,6 +179,10 @@ void CaretDisplayItemClient::UpdateStyleAndLayoutIfNeeded(
   const NGPhysicalBoxFragment* const new_box_fragment =
       rect_and_block.box_fragment;
   if (new_box_fragment != box_fragment_) {
+    // The caret property tree space may have changed.
+    layout_block_->GetFrameView()->SetPaintArtifactCompositorNeedsUpdate(
+        PaintArtifactCompositorUpdateReason::kFrameCaretPaint);
+
     if (new_box_fragment)
       needs_paint_invalidation_ = true;
     box_fragment_ = new_box_fragment;
