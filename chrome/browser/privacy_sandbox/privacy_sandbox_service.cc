1e6bce93eb0bd2a3b613a5ce88123bce99516592
Rohit Agarwal <roagarwal@chromium.org>
2023-01-17 14:52:13 +0000

    Privacy Sandbox Settings: Record start up metrics for the APIs.
    This CL adds new metrics to record whether the API is on/off during
    profile startup time.
    
    Bug: 1378703
    Change-Id: Id73a41a9f80342081e7c5c8035a992745bbd40af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173853
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093318}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
@@ -524,8 +524,17 @@ void PrivacySandboxService::RecordPrivacySandbox3StartupMetrics() {
 }
 
 void PrivacySandboxService::RecordPrivacySandbox4StartupMetrics() {
-  // TODO(crbug.com/1378703): Add metrics for inidividial PS APIs being enabled
-  // or not in the beginning here.
+  // Record the status of the APIs.
+  const bool topics_enabled =
+      pref_service_->GetBoolean(prefs::kPrivacySandboxM1TopicsEnabled);
+  base::UmaHistogramBoolean("Settings.PrivacySandbox.Topics.Enabled",
+                            topics_enabled);
+  base::UmaHistogramBoolean(
+      "Settings.PrivacySandbox.Fledge.Enabled",
+      pref_service_->GetBoolean(prefs::kPrivacySandboxM1FledgeEnabled));
+  base::UmaHistogramBoolean(
+      "Settings.PrivacySandbox.AdMeasurement.Enabled",
+      pref_service_->GetBoolean(prefs::kPrivacySandboxM1AdMeasurementEnabled));
 
   const std::string privacy_sandbox_prompt_startup_histogram =
       "Settings.PrivacySandbox.PromptStartupState";
@@ -592,8 +601,6 @@ void PrivacySandboxService::RecordPrivacySandbox4StartupMetrics() {
     }
 
     // Consent decision made at this point.
-    const bool topics_enabled =
-        pref_service_->GetBoolean(prefs::kPrivacySandboxM1TopicsEnabled);
 
     // Notice Acknowledged
     const bool notice_acknowledged = pref_service_->GetBoolean(
--- chrome/browser/privacy_sandbox/privacy_sandbox_service.h
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service.h
@@ -366,6 +366,8 @@ class PrivacySandboxService : public KeyedService {
   FRIEND_TEST_ALL_PREFIXES(
       PrivacySandboxServiceM1Test,
       RecordPrivacySandbox4StartupMetrics_PromptNotSuppressed_ROW);
+  FRIEND_TEST_ALL_PREFIXES(PrivacySandboxServiceM1Test,
+                           RecordPrivacySandbox4StartupMetrics_APIs);
 
   // Should be used only for tests when mocking the service.
   PrivacySandboxService();
