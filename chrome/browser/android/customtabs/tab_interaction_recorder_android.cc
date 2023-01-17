de4fb8f73c8a7c0f17ce1b6fc433c0a486a7a1a8
Zach Katz <katzz@google.com>
2023-01-17 19:59:37 +0000

    Revert "[RCTS] FormInteractionData impl"
    This reverts commit 98d54e0800edabd8b7d113860fb15e1dea8c45bd.
    
    Reason for revert: Crashing on Canary crbug.com/1407486
    
    Original change's description:
    > [RCTS] FormInteractionData impl
    >
    > -Impl of DocumentUserData for RenderFrameHost's
    > -Used to track form interactions within each RFH
    >
    > Bug: 1404752
    > Change-Id: I54b27761711f3f32355be5e0d2aad7556976a3f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149407
    > Auto-Submit: Zach Katz <katzz@google.com>
    > Commit-Queue: Zach Katz <katzz@google.com>
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1092680}
    
    Bug: 1404752
    Change-Id: If51eaba1120002630b2bbc5ad09b7923ff511692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174673
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Zach Katz <katzz@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1093484}

--- chrome/browser/android/customtabs/tab_interaction_recorder_android.cc
+++ chrome/browser/android/customtabs/tab_interaction_recorder_android.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/core/browser/autofill_manager.h"
-#include "content/public/browser/document_user_data.h"
 #include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
@@ -27,7 +26,6 @@ namespace customtabs {
 using autofill::AutofillManager;
 using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
-using content::GlobalRenderFrameHostId;
 using content::RenderFrameHost;
 
 namespace {
@@ -39,14 +37,13 @@ AutofillManager* GetAutofillManager(RenderFrameHost* render_frame_host) {
     return nullptr;
   return autofill_driver->autofill_manager();
 }
+
 }  // namespace
 
 AutofillObserverImpl::AutofillObserverImpl(
-    GlobalRenderFrameHostId id,
     autofill::AutofillManager* autofill_manager,
     OnFormInteractionCallback form_interaction_callback)
-    : global_id_(id),
-      autofill_manager_(autofill_manager),
+    : autofill_manager_(autofill_manager),
       form_interaction_callback_(std::move(form_interaction_callback)) {
   autofill_manager->AddObserver(this);
 }
@@ -81,22 +78,7 @@ void AutofillObserverImpl::Invalidate() {
 
 void AutofillObserverImpl::OnFormInteraction() {
   Invalidate();
-  std::move(form_interaction_callback_).Run(global_id_);
-}
-
-DOCUMENT_USER_DATA_KEY_IMPL(FormInteractionData);
-
-FormInteractionData::~FormInteractionData() = default;
-
-FormInteractionData::FormInteractionData(RenderFrameHost* rfh)
-    : DocumentUserData<FormInteractionData>(rfh) {}
-
-void FormInteractionData::SetHasFormInteractionData() {
-  had_form_interaction_data_ = true;
-}
-
-bool FormInteractionData::GetHasFormInteractionData() {
-  return static_cast<jboolean>(had_form_interaction_data_);
+  std::move(form_interaction_callback_).Run();
 }
 
 TabInteractionRecorderAndroid::~TabInteractionRecorderAndroid() = default;
@@ -152,10 +134,7 @@ void TabInteractionRecorderAndroid::DidGetUserInteraction(
       .OnUserInteraction();
 }
 
-void TabInteractionRecorderAndroid::SetHasFormInteractions(
-    GlobalRenderFrameHostId id) {
-  FormInteractionData::GetForCurrentDocument(RenderFrameHost::FromID(id))
-      ->SetHasFormInteractionData();
+void TabInteractionRecorderAndroid::SetHasFormInteractions() {
   has_form_interactions_ = true;
   rfh_observer_map_.clear();
 }
@@ -172,11 +151,9 @@ void TabInteractionRecorderAndroid::StartObservingFrame(
   if (!autofill_manager)
     return;
 
-  FormInteractionData::CreateForCurrentDocument(render_frame_host);
-
   rfh_observer_map_[render_frame_host->GetGlobalId()] =
       std::make_unique<AutofillObserverImpl>(
-          render_frame_host->GetGlobalId(), autofill_manager,
+          autofill_manager,
           base::BindOnce(&TabInteractionRecorderAndroid::SetHasFormInteractions,
                          weak_factory_.GetWeakPtr()));
 }
--- chrome/browser/android/customtabs/tab_interaction_recorder_android.h
+++ chrome/browser/android/customtabs/tab_interaction_recorder_android.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "components/autofill/core/browser/autofill_manager.h"
-#include "content/public/browser/document_user_data.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 #include "url/gurl.h"
@@ -22,11 +21,9 @@ namespace customtabs {
 // Autofill observer impl for the TabInteractionRecorderAndroid to use.
 class AutofillObserverImpl : public autofill::AutofillManager::Observer {
  public:
-  using OnFormInteractionCallback =
-      base::OnceCallback<void(content::GlobalRenderFrameHostId)>;
+  using OnFormInteractionCallback = base::OnceCallback<void()>;
 
   explicit AutofillObserverImpl(
-      content::GlobalRenderFrameHostId id,
       autofill::AutofillManager* autofill_manager,
       OnFormInteractionCallback form_interaction_callback);
 
@@ -44,27 +41,8 @@ class AutofillObserverImpl : public autofill::AutofillManager::Observer {
   void OnFormInteraction();
   void Invalidate();
 
-  content::GlobalRenderFrameHostId global_id_;
   raw_ptr<autofill::AutofillManager, DanglingUntriaged> autofill_manager_;
   OnFormInteractionCallback form_interaction_callback_;
-};
-
-// DocumentUserData stored inside each RenderFrameHost indicating whether
-// the hosting RFH experienced a form interaction.
-class FormInteractionData
-    : public content::DocumentUserData<FormInteractionData> {
- public:
-  explicit FormInteractionData(content::RenderFrameHost* rfh);
-
-  ~FormInteractionData() override;
-  void SetHasFormInteractionData();
-  bool GetHasFormInteractionData();
-
- private:
-  bool had_form_interaction_data_;
-
-  friend DocumentUserData;
-  DOCUMENT_USER_DATA_KEY_DECL();
 };
 
 // Class that record interaction from the web contents. The definition
@@ -122,7 +100,7 @@ class TabInteractionRecorderAndroid
 
   friend class AutofillObserverImpl;
   void StartObservingFrame(content::RenderFrameHost* render_frame_host);
-  void SetHasFormInteractions(content::GlobalRenderFrameHostId id);
+  void SetHasFormInteractions();
 
   void ResetImpl();
 
