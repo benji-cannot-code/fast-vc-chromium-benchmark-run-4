da5d506bb1814c67a0d2e79809486b26f3331b52
kylechar <kylechar@chromium.org>
2023-01-17 20:45:23 +0000

    Refactor GLTextureImageBacking internals
    To help support multi-planar shared images, refactor the internals of
    GLTextureImageBacking that allocate/hold/destroy a GL texture into a
    separate GLTextureHolder class. This way GLTextureImageBacking can hold
    N GLTextureHolders, one for each plane of multi-planar textures.
    
    Bug: 1406253
    Change-Id: I0bca7a6a642ffd660b0136b422a441c33d53b196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4150192
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093514}

--- gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory.cc
@@ -9,10 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "build/build_config.h"
-#include "components/viz/common/resources/resource_sizes.h"
 #include "gpu/command_buffer/common/mailbox.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
-#include "gpu/command_buffer/service/service_utils.h"
 #include "gpu/command_buffer/service/shared_image/gl_texture_image_backing.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_format_utils.h"
 #include "gpu/config/gpu_preferences.h"
@@ -21,12 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/progress_reporter.h"
 
 namespace gpu {
-
-namespace {
-
-using ScopedRestoreTexture = GLTextureImageBackingHelper::ScopedRestoreTexture;
-
-}  // anonymous namespace
 
 ///////////////////////////////////////////////////////////////////////////////
 // GLTextureImageBackingFactory
@@ -180,9 +172,8 @@ GLTextureImageBackingFactory::CreateSharedImageInternal(
     uint32_t usage,
     base::span<const uint8_t> pixel_data) {
   const FormatInfo& format_info = GetFormatInfo(format);
-  GLenum target = GL_TEXTURE_2D;
+  DCHECK(CanCreateSharedImage(size, pixel_data, format_info, GL_TEXTURE_2D));
 
-  const bool is_cleared = !pixel_data.empty();
   const bool for_framebuffer_attachment =
       (usage & (SHARED_IMAGE_USAGE_RASTER |
                 SHARED_IMAGE_USAGE_GLES2_FRAMEBUFFER_HINT)) != 0;
@@ -192,52 +183,9 @@ GLTextureImageBackingFactory::CreateSharedImageInternal(
   auto result = std::make_unique<GLTextureImageBacking>(
       mailbox, format, size, color_space, surface_origin, alpha_type, usage,
       use_passthrough_);
-  result->InitializeGLTexture(format_info, is_cleared,
+  result->InitializeGLTexture(format_info, pixel_data, progress_reporter_,
                               framebuffer_attachment_angle);
 
-  gl::GLApi* api = gl::g_current_gl_context;
-  ScopedRestoreTexture scoped_restore(api, target);
-  api->glBindTextureFn(target, result->GetGLServiceId());
-
-  if (format_info.supports_storage) {
-    {
-      gl::ScopedProgressReporter scoped_progress_reporter(progress_reporter_);
-      api->glTexStorage2DEXTFn(target, 1,
-                               format_info.adjusted_storage_internal_format,
-                               size.width(), size.height());
-    }
-
-    if (!pixel_data.empty()) {
-      ScopedUnpackState scoped_unpack_state(
-          /*uploading_data=*/true);
-      gl::ScopedProgressReporter scoped_progress_reporter(progress_reporter_);
-      api->glTexSubImage2DFn(target, 0, 0, 0, size.width(), size.height(),
-                             format_info.adjusted_format, format_info.gl_type,
-                             pixel_data.data());
-    }
-  } else if (format_info.is_compressed) {
-    ScopedUnpackState scoped_unpack_state(!pixel_data.empty());
-    gl::ScopedProgressReporter scoped_progress_reporter(progress_reporter_);
-    api->glCompressedTexImage2DFn(target, 0, format_info.image_internal_format,
-                                  size.width(), size.height(), 0,
-                                  pixel_data.size(), pixel_data.data());
-  } else {
-    ScopedUnpackState scoped_unpack_state(!pixel_data.empty());
-    gl::ScopedProgressReporter scoped_progress_reporter(progress_reporter_);
-    api->glTexImage2DFn(target, 0, format_info.image_internal_format,
-                        size.width(), size.height(), 0,
-                        format_info.adjusted_format, format_info.gl_type,
-                        pixel_data.data());
-  }
-
-  if (gl::g_current_gl_driver->ext.b_GL_KHR_debug) {
-    const std::string label =
-        "SharedImage_GLTexture" + CreateLabelForSharedImageUsage(usage);
-    api->glObjectLabelFn(GL_TEXTURE, result->GetGLServiceId(), -1,
-                         label.c_str());
-  }
-
-  result->SetCompatibilitySwizzle(format_info.swizzle);
   return std::move(result);
 }
 
