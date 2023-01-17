4be9ec1dcb51d0e009c36832a122a894e1064ca1
Peter Kvitek <kvitekp@chromium.org>
2023-01-17 23:08:24 +0000

    [headless] Implemented headless commands support in chrome/browser.
    This CL adds support for the --screenshot, --print-to-pdf and --dump-dom commands to the new headless mode that does not rely on the alternate browser implementation in //headless.
    
    Bug: 1382533, 1382535, 1382553, 1382556
    Change-Id: I400aeb9d5f573866a37ed9cdb4e25d4709b7f409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4160268
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093571}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
 #include "chrome/browser/defaults.h"
+#include "chrome/browser/headless/headless_command_processor.h"
 #include "chrome/browser/infobars/simple_alert_infobar_creator.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
@@ -341,6 +342,23 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(
       chrome::AddTabAt(browser, GURL(), -1, true);
     else
       browser->tab_strip_model()->ActivateTabAt(0);
+  }
+
+  if (headless::ShouldProcessHeadlessCommands()) {
+    // Headless mode is restricted to only one url in the command line, so
+    // just grab the actave tab assuming it's the target.
+    content::WebContents* web_contents =
+        browser->tab_strip_model()->GetActiveWebContents();
+    if (web_contents) {
+      headless::ProcessHeadlessCommands(profile_, web_contents->GetVisibleURL(),
+                                        base::BindOnce(
+                                            [](base::WeakPtr<Browser> browser) {
+                                              if (browser->window()) {
+                                                browser->window()->Close();
+                                              }
+                                            },
+                                            browser->AsWeakPtr()));
+    }
   }
 
 #if BUILDFLAG(IS_MAC)
