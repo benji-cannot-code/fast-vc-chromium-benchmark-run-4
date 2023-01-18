c3b94a52559d640aa67634dfc73c3b1a8bb0b036
Dmitry Vykochko <vykochko@google.com>
2023-01-18 14:37:40 +0000

    Address source (AutofillProfile::Source) in Clank data structures.
    This CL is a part of bigger functionality where we need to differentiate
    profile source in settings (e.g. to notify the user about potential
    side effects of updating/deleting an address).
    
    Bug: 1408424
    
    Change-Id: Idf680df47ce485166b493306081ea1c7ad49f370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165107
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093852}

--- chrome/browser/autofill/android/personal_data_manager_android.cc
+++ chrome/browser/autofill/android/personal_data_manager_android.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_data_util.h"
 #include "components/autofill/core/browser/autofill_experiments.h"
 #include "components/autofill/core/browser/autofill_type.h"
+#include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/geo/address_i18n.h"
 #include "components/autofill/core/browser/geo/autofill_country.h"
@@ -327,6 +328,7 @@ PersonalDataManagerAndroid::CreateJavaProfileFromNative(
       env, ConvertUTF8ToJavaString(env, profile.guid()),
       ConvertUTF8ToJavaString(env, profile.origin()),
       profile.record_type() == AutofillProfile::LOCAL_PROFILE,
+      static_cast<jint>(profile.source()),
       ConvertUTF16ToJavaString(
           env, profile.GetInfo(AutofillType(NAME_HONORIFIC_PREFIX),
                                g_browser_process->GetApplicationLocale())),
