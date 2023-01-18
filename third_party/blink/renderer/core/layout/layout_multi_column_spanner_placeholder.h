f61f6af8219e1e0c3bdd8c94cdf977a39d637d46
Kent Tamura <tkent@chromium.org>
2023-01-18 23:37:23 +0000

    NoCopyBack: No copying back for LayoutMultiColumnSpannerPlaceholder
    Do not call SetLocation() and SetSize() for
    LayoutMultiColumnSpannerPlaceholder, and Location() and Size() pull the
    corresponding values from layout_object_in_flow_thread_ instead.
    
    This CL also introduces a runtime flag for the stop-copying-back
    project.
    
    This CL should have no behavior changes.
    
    Bug: 1353190
    Change-Id: Ib1c015041b508bf6b78172e85da818b9d8bc3650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4172419
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094150}

--- third_party/blink/renderer/core/layout/layout_multi_column_spanner_placeholder.h
+++ third_party/blink/renderer/core/layout/layout_multi_column_spanner_placeholder.h
@@ -92,6 +92,8 @@ class LayoutMultiColumnSpannerPlaceholder final : public LayoutBox {
     NOTREACHED();
     return MinMaxSizes();
   }
+  LayoutPoint Location() const override;
+  LayoutSize Size() const override;
 
   // The actual column-span:all layoutObject inside the flow thread.
   Member<LayoutBox> layout_object_in_flow_thread_;
