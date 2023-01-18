7ef7ca24398e140353859c13797629fa8da11726
Toni Barzic <tbarzic@chromium.org>
2023-01-18 03:00:07 +0000

    Remove unused app context menu "launch type" commands
    Removes USE_LAUNCH_TYPE_PINNED and USE_LAUNCH_TYPE_FULLSCREEN app
    context menu commands, as they are no longer used. These were once
    supported for hosted (extension based) apps, but got deprecated with
    BookmarkApps feature few years ago.
    The feature flag was cleaned up in CL:1551574 but context menu code for
    since then unsupported launch types remained around.
    
    BUG=None
    
    Change-Id: I88eb5508fbec8252b652bbbc5f89940fa11488ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4156559
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093711}

--- chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu.cc
@@ -69,15 +69,18 @@ apps::WindowMode ConvertLaunchTypeCommandToWindowMode(int command_id) {
 extensions::LaunchType ConvertLaunchTypeCommandToExtensionLaunchType(
     int command_id) {
   switch (command_id) {
-    case ash::USE_LAUNCH_TYPE_PINNED:
-      return extensions::LAUNCH_TYPE_PINNED;
     case ash::USE_LAUNCH_TYPE_REGULAR:
       return extensions::LAUNCH_TYPE_REGULAR;
     case ash::USE_LAUNCH_TYPE_WINDOW:
       return extensions::LAUNCH_TYPE_WINDOW;
-    case ash::USE_LAUNCH_TYPE_FULLSCREEN:
-      return extensions::LAUNCH_TYPE_FULLSCREEN;
+    case ash::USE_LAUNCH_TYPE_TABBED_WINDOW:
+      // Not supported for extensions.
+      [[fallthrough]];
+    case ash::DEPRECATED_USE_LAUNCH_TYPE_PINNED:
+    case ash::DEPRECATED_USE_LAUNCH_TYPE_FULLSCREEN:
+      [[fallthrough]];
     default:
+      NOTREACHED();
       return extensions::LAUNCH_TYPE_INVALID;
   }
 }
@@ -204,15 +207,16 @@ void AppServiceShelfContextMenu::ExecuteCommand(int command_id,
 
     case ash::USE_LAUNCH_TYPE_TABBED_WINDOW:
       [[fallthrough]];
-    case ash::USE_LAUNCH_TYPE_PINNED:
-      [[fallthrough]];
     case ash::USE_LAUNCH_TYPE_REGULAR:
       [[fallthrough]];
     case ash::USE_LAUNCH_TYPE_WINDOW:
-      [[fallthrough]];
-    case ash::USE_LAUNCH_TYPE_FULLSCREEN:
       launch_new_string_id_ = apps::StringIdForUseLaunchTypeCommand(command_id);
       SetLaunchType(command_id);
+      break;
+
+    case ash::DEPRECATED_USE_LAUNCH_TYPE_FULLSCREEN:
+    case ash::DEPRECATED_USE_LAUNCH_TYPE_PINNED:
+      NOTREACHED();
       break;
 
     case ash::CROSTINI_USE_LOW_DENSITY:
@@ -548,10 +552,6 @@ void AppServiceShelfContextMenu::SetLaunchType(int command_id) {
 
 void AppServiceShelfContextMenu::SetExtensionLaunchType(int command_id) {
   switch (static_cast<ash::CommandId>(command_id)) {
-    case ash::USE_LAUNCH_TYPE_PINNED:
-      extensions::SetLaunchType(controller()->profile(), item().id.app_id,
-                                extensions::LAUNCH_TYPE_PINNED);
-      break;
     case ash::USE_LAUNCH_TYPE_REGULAR:
       extensions::SetLaunchType(controller()->profile(), item().id.app_id,
                                 extensions::LAUNCH_TYPE_REGULAR);
@@ -567,9 +567,10 @@ void AppServiceShelfContextMenu::SetExtensionLaunchType(int command_id) {
                                 launch_type);
       break;
     }
-    case ash::USE_LAUNCH_TYPE_FULLSCREEN:
-      extensions::SetLaunchType(controller()->profile(), item().id.app_id,
-                                extensions::LAUNCH_TYPE_FULLSCREEN);
+    case ash::USE_LAUNCH_TYPE_TABBED_WINDOW:
+    case ash::DEPRECATED_USE_LAUNCH_TYPE_FULLSCREEN:
+    case ash::DEPRECATED_USE_LAUNCH_TYPE_PINNED:
+      NOTREACHED();
       break;
     default:
       return;
