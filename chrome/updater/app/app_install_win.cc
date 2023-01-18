b35b380f1900ff5f5d652b403d7195ed60e28a35
S. Ganesh <ganesh@chromium.org>
2023-01-18 19:08:04 +0000

    Preliminary code to load the logo from a URL
    This CL uses `::OleLoadPicturePath` to load the logo from a URL. The url
    for the logo is in BMP format, and is expected to be deduced based on
    the `app_id_`.
    
    Bug: 1406963
    Change-Id: I925bd5edeb9abe1a0fa9cf598d5631be2f7bce41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4172198
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093999}

--- chrome/updater/app/app_install_win.cc
+++ chrome/updater/app/app_install_win.cc
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 #include <vector>
 
+#include <ocidl.h>
+#include <olectl.h>
 #include <shldisp.h>
 #include <shlobj.h>
+#include <windows.h>
 #include <wrl/client.h>
 
 #include "base/check.h"
@@ -54,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/win/install_progress_observer.h"
 #include "chrome/updater/win/manifest_util.h"
 #include "chrome/updater/win/scoped_impersonation.h"
+#include "chrome/updater/win/ui/resources/resources.grh"
 #include "chrome/updater/win/user_info.h"
 #include "chrome/updater/win/win_constants.h"
 
@@ -386,6 +390,9 @@ class AppInstallControllerImpl : public AppInstallController,
 
   // Overrides for WTL::CMessageFilter.
   BOOL PreTranslateMessage(MSG* msg) override;
+
+  // This function is called on a dedicated COM STA thread.
+  void LoadLogo(std::wstring url, HWND progress_hwnd);
 
   // These functions are called on the UI thread.
   void InitializeUI();
@@ -794,6 +801,42 @@ ObserverCompletionInfo AppInstallControllerImpl::HandleInstallResult(
   return observer_info;
 }
 
+// Loads the logo in BMP format if it exists at the provided `url`, and sets the
+// resultant image onto the app bitmap for the progress window.
+void AppInstallControllerImpl::LoadLogo(std::wstring url, HWND progress_hwnd) {
+  if (url.empty()) {
+    VLOG(1) << __func__ << "No url specified";
+    return;
+  }
+
+  Microsoft::WRL::ComPtr<IPicture> picture;
+  HRESULT hr =
+      ::OleLoadPicturePath(&url[0], nullptr, 0, 0, IID_PPV_ARGS(&picture));
+  if (FAILED(hr)) {
+    VLOG(1) << __func__ << "::OleLoadPicturePath failed: " << std::hex << hr
+            << ": " << logging::SystemErrorCodeToString(hr);
+    return;
+  }
+
+  HBITMAP bitmap = nullptr;
+  hr = picture->get_Handle(reinterpret_cast<UINT*>(&bitmap));
+  if (FAILED(hr)) {
+    VLOG(1) << __func__ << "picture->get_Handle failed: " << std::hex << hr
+            << ": " << logging::SystemErrorCodeToString(hr);
+    return;
+  }
+
+  if (!::IsWindow(progress_hwnd)) {
+    VLOG(1) << __func__ << "progress_hwnd not valid anymore";
+    return;
+  }
+
+  ::SendDlgItemMessage(progress_hwnd, IDC_APP_BITMAP, STM_SETIMAGE,
+                       IMAGE_BITMAP,
+                       reinterpret_cast<LPARAM>(::CopyImage(
+                           bitmap, IMAGE_BITMAP, 0, 0, LR_COPYRETURNORG)));
+}
+
 // Creates the install progress observer. The observer has thread affinity. It
 // must be created, process its messages, and be destroyed on the same thread.
 void AppInstallControllerImpl::InitializeUI() {
@@ -813,6 +856,14 @@ void AppInstallControllerImpl::InitializeUI() {
     progress_wnd->SetEventSink(this);
     progress_wnd->Initialize();
     progress_wnd->Show();
+
+    // TODO(crbug/1406963): Provide the url for the logo here based on the
+    // `app_id_`.
+    base::ThreadPool::CreateCOMSTATaskRunner({base::MayBlock()})
+        ->PostTask(FROM_HERE,
+                   base::BindOnce(&AppInstallControllerImpl::LoadLogo, this,
+                                  L"", progress_wnd->m_hWnd));
+
     observer_.reset(progress_wnd.release());
   }
 }
