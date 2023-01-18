5ab8178dcdaefcb92f4a442ac52b6a1ba56e1f3b
David Padlipsky <dpad@google.com>
2023-01-18 22:02:22 +0000

    Settings Split: Refactor input device observing in controller
    Move logic for observing input devices from the controller to a new
    InputDeviceNotifier which will observe DeviceDataManager and notify the
    controller whenever devices are connected/disconnected.
    
    Bug: b/241965700
    Test: ash_unittests
    
    Change-Id: Ia778c32c9a53804c7080eb04b8d5e8e7cf5e09b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4139408
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094097}

--- ash/system/input_device_settings/input_device_settings_controller_impl.h
+++ ash/system/input_device_settings/input_device_settings_controller_impl.h
@@ -8,17 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/input_device_settings_controller.h"
+#include "ash/public/mojom/input_device_settings.mojom.h"
+#include "ash/system/input_device_settings/input_device_notifier.h"
 #include "ash/system/input_device_settings/input_device_pref_manager.h"
 #include "base/containers/flat_map.h"
 #include "base/observer_list.h"
-#include "ui/events/devices/input_device_event_observer.h"
+#include "ui/events/devices/input_device.h"
 
 namespace ash {
 
 // Controller to manage input device settings.
 class ASH_EXPORT InputDeviceSettingsControllerImpl
-    : public InputDeviceSettingsController,
-      public ui::InputDeviceEventObserver {
+    : public InputDeviceSettingsController {
  public:
   InputDeviceSettingsControllerImpl();
   explicit InputDeviceSettingsControllerImpl(
@@ -36,22 +37,20 @@ class ASH_EXPORT InputDeviceSettingsControllerImpl
   void AddObserver(Observer* observer) override;
   void RemoveObserver(Observer* observer) override;
 
+  void OnKeyboardListUpdated(std::vector<ui::InputDevice> keyboards_to_add,
+                             std::vector<DeviceId> keyboard_ids_to_remove);
+
  private:
   void Init();
-
-  void RefreshDeviceLists();
-  void RefreshKeyboardList();
 
   void DispatchKeyboardConnected(DeviceId id);
   void DispatchKeyboardDisconnected(DeviceId id);
 
-  // ui::InputDeviceEventObserver
-  void OnInputDeviceConfigurationChanged(uint8_t input_device_type) override;
-  void OnDeviceListsComplete() override;
-
   std::unique_ptr<InputDevicePrefManager> pref_manager_;
   base::flat_map<DeviceId, mojom::KeyboardPtr> keyboards_;
   base::ObserverList<InputDeviceSettingsController::Observer> observers_;
+
+  std::unique_ptr<InputDeviceNotifier<mojom::KeyboardPtr>> keyboard_notifier_;
 };
 
 }  // namespace ash
