4f0172b32e46d1ff980541b8d700f544244b0e08
Peter Kvitek <kvitekp@chromium.org>
2023-01-18 00:27:35 +0000

    [headless] Added 'nogncheck' to an #ifdef'ed include.
    This fixes the linux-headless-shell-rel bot build issue with gn --check.
    
    Change-Id: I5566a1c80d59803f2b19f75d7b686e6e07918a88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174586
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093635}

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(HEADLESS_ENABLE_COMMANDS)
-#include "components/headless/command_handler/headless_command_handler.h"
+#include "components/headless/command_handler/headless_command_handler.h"  // nogncheck
 #endif
 
 namespace headless {
