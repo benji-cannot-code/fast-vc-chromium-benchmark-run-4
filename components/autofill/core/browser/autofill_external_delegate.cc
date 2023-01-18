ee3981f255355777e06f5dbbab7526a57eea3510
Peter Kotwicz <pkotwicz@chromium.org>
2023-01-18 16:44:53 +0000

    [Refactor] Move Autofill.SuggestionAcceptedIndex UMA to dedicated file
    This CL moves Autofill.SuggestionAcceptedIndex UMA to a dedicated file
    suggestions_list_metrics.cc This is in preparation for adding more
    metrics for the selected entry in the autofill popup.
    
    BUG=1406348
    
    Change-Id: I6e810567a1764b63e576f5ae7e281a9a36305a1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4168687
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093914}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_driver.h"
 #include "components/autofill/core/browser/browser_autofill_manager.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
+#include "components/autofill/core/browser/metrics/suggestions_list_metrics.h"
 #include "components/autofill/core/browser/ui/popup_item_ids.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
@@ -295,7 +296,7 @@ void AutofillExternalDelegate::DidAcceptSuggestion(const Suggestion& suggestion,
       break;
     default:
       if (suggestion.frontend_id > 0) {  // Denotes an Autofill suggestion.
-        AutofillMetrics::LogAutofillSuggestionAcceptedIndex(
+        autofill_metrics::LogAutofillSuggestionAcceptedIndex(
             position, popup_type_, driver_->IsIncognito());
       }
       FillAutofillFormData(suggestion.frontend_id, false);
---
+++ components/autofill/core/browser/metrics/suggestions_list_metrics.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_METRICS_SUGGESTIONS_LIST_METRICS_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_METRICS_SUGGESTIONS_LIST_METRICS_H_
+
+namespace autofill {
+enum class PopupType;
+
+namespace autofill_metrics {
+
+// Log the index of the selected Autofill suggestion in the popup.
+void LogAutofillSuggestionAcceptedIndex(int index,
+                                        autofill::PopupType popup_type,
+                                        bool off_the_record);
+
+}  // namespace autofill_metrics
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_METRICS_SUGGESTIONS_LIST_METRICS_H_
