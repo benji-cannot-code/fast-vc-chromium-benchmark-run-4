89dc7deff6e2d0fc29df399af65dd3ee77da4755
Thiago Perrotta <tperrotta@chromium.org>
2023-01-18 13:09:17 +0000

    Rename feature Chrome for Testing GN args, GRIT vars and C++ buildflags
    GN variables (build/config/chrome_build.gni):
    
    - Feature: is_chrome_for_testing_branded ->
               is_chrome_for_testing
    
    C++ buildflags (chrome/browser/chrome_for_testing/BUILD.gn):
    
    - Feature: GOOGLE_CHROME_FOR_TESTING_BRANDING ->
               CHROME_FOR_TESTING
    
    Design document: https://goo.gle/chrome-for-testing?disco=AAAAi75kgLI
    
    Split from: http://crrev.com/c/4027840
    
    Bug: 1336611
    Change-Id: I65f5cc53bec18cdbcd534a82ea0084f777955397
    Cq-Include-Trybots: luci.chromium.try:linux-rel-cft,mac-rel-cft,win-rel-cft
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4172729
    Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thiago Perrotta <tperrotta@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093822}

--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "google_apis/google_api_keys.h"
 
-#if BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+#if BUILDFLAG(CHROME_FOR_TESTING)
 #include "chrome/browser/ui/startup/chrome_for_testing_infobar_delegate.h"
 #endif
 
@@ -64,7 +64,7 @@ bool IsKioskModeEnabled() {
       switches::kKioskMode);
 }
 
-#if BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+#if BUILDFLAG(CHROME_FOR_TESTING)
 bool IsGpuTest() {
   return base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
              switches::kTestType) == "gpu";
@@ -89,7 +89,7 @@ void AddInfoBarsIfNecessary(Browser* browser,
   DCHECK(web_contents);
 
   if (show_bad_flags_security_warnings) {
-#if BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+#if BUILDFLAG(CHROME_FOR_TESTING)
     if (!IsGpuTest()) {
       ChromeForTestingInfoBarDelegate::Create();
     }
