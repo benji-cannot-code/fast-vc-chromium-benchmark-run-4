532b2019b5e6dc53623a8ae7646d99a59b031c1c
Aaron Krajeski <aaronhk@chromium.org>
2023-01-18 17:38:32 +0000

    Store a blink::Color on CanvasStyle
    Previously the private color_ member of CanvasStyle was an RGB32,
    which in turn is just an alias for an unsigned integer. With changes
    to CSS Color 4 added color depth and color space information would
    be lost using this format. The simple fix is just to store a
    blink::Color on the class.
    
    Bug: 1399566
    Change-Id: I7d8d08209165890ce6daf6e807b5fa654f80ae1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4162199
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093945}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_state.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_state.cc
@@ -71,10 +71,10 @@ bool StringToNumWithUnit(String spacing,
 }
 
 CanvasRenderingContext2DState::CanvasRenderingContext2DState()
-    : stroke_style_(MakeGarbageCollected<CanvasStyle>(SK_ColorBLACK)),
-      fill_style_(MakeGarbageCollected<CanvasStyle>(SK_ColorBLACK)),
+    : stroke_style_(MakeGarbageCollected<CanvasStyle>(Color::kBlack)),
+      fill_style_(MakeGarbageCollected<CanvasStyle>(Color::kBlack)),
       shadow_blur_(0.0),
-      shadow_color_(SK_ColorTRANSPARENT),
+      shadow_color_(Color::kTransparent),
       global_alpha_(1.0),
       line_dash_offset_(0.0),
       unparsed_font_(defaultFont),
@@ -238,9 +238,10 @@ void CanvasRenderingContext2DState::UpdateLineDash() const {
   line_dash_dirty_ = false;
 }
 
-void CanvasRenderingContext2DState::SetStrokeColor(RGBA32 color) {
-  if (stroke_style_->IsEquivalentRGBA(color))
+void CanvasRenderingContext2DState::SetStrokeColor(Color color) {
+  if (stroke_style_->IsEquivalentColor(color)) {
     return;
+  }
 
   if (stroke_style_->is_shared()) {
     SetStrokeStyle(MakeGarbageCollected<CanvasStyle>(color));
@@ -291,9 +292,10 @@ void CanvasRenderingContext2DState::SetStrokeStyle(CanvasStyle* style) {
   stroke_style_dirty_ = true;
 }
 
-void CanvasRenderingContext2DState::SetFillColor(RGBA32 color) {
-  if (fill_style_->IsEquivalentRGBA(color))
+void CanvasRenderingContext2DState::SetFillColor(Color color) {
+  if (fill_style_->IsEquivalentColor(color)) {
     return;
+  }
 
   if (fill_style_->is_shared()) {
     SetFillStyle(MakeGarbageCollected<CanvasStyle>(color));
@@ -349,8 +351,9 @@ void CanvasRenderingContext2DState::UpdateStrokeStyle() const {
 
   DCHECK(stroke_style_);
   stroke_style_->ApplyToFlags(stroke_flags_);
+  Color stroke_flag_color = stroke_style_->PaintColor();
   stroke_flags_.setColor(
-      ScaleAlpha(stroke_style_->PaintColor(), global_alpha_));
+      stroke_flag_color.CombineWithAlpha(global_alpha_).toSkColor4f());
   stroke_style_dirty_ = false;
 }
 
@@ -360,7 +363,9 @@ void CanvasRenderingContext2DState::UpdateFillStyle() const {
 
   DCHECK(fill_style_);
   fill_style_->ApplyToFlags(fill_flags_);
-  fill_flags_.setColor(ScaleAlpha(fill_style_->PaintColor(), global_alpha_));
+  Color fill_flag_color = fill_style_->PaintColor();
+  fill_flags_.setColor(
+      fill_flag_color.CombineWithAlpha(global_alpha_).toSkColor4f());
   fill_style_dirty_ = false;
 }
 
@@ -544,10 +549,10 @@ sk_sp<PaintFilter> CanvasRenderingContext2DState::GetFilterForOffscreenCanvas(
   // incorporate the global alpha, which isn't applicable here.
   cc::PaintFlags fill_flags_for_filter;
   fill_style_->ApplyToFlags(fill_flags_for_filter);
-  fill_flags_for_filter.setColor(fill_style_->PaintColor());
+  fill_flags_for_filter.setColor(fill_style_->PaintColor().toSkColor4f());
   cc::PaintFlags stroke_flags_for_filter;
   stroke_style_->ApplyToFlags(stroke_flags_for_filter);
-  stroke_flags_for_filter.setColor(stroke_style_->PaintColor());
+  stroke_flags_for_filter.setColor(stroke_style_->PaintColor().toSkColor4f());
 
   FilterEffectBuilder filter_effect_builder(
       gfx::RectF(gfx::SizeF(canvas_size)),
@@ -616,10 +621,10 @@ sk_sp<PaintFilter> CanvasRenderingContext2DState::GetFilter(
   // incorporate the global alpha, which isn't applicable here.
   cc::PaintFlags fill_flags_for_filter;
   fill_style_->ApplyToFlags(fill_flags_for_filter);
-  fill_flags_for_filter.setColor(fill_style_->PaintColor());
+  fill_flags_for_filter.setColor(fill_style_->PaintColor().toSkColor4f());
   cc::PaintFlags stroke_flags_for_filter;
   stroke_style_->ApplyToFlags(stroke_flags_for_filter);
-  stroke_flags_for_filter.setColor(stroke_style_->PaintColor());
+  stroke_flags_for_filter.setColor(stroke_style_->PaintColor().toSkColor4f());
 
   FilterEffectBuilder filter_effect_builder(
       gfx::RectF(gfx::SizeF(canvas_size)),
@@ -664,7 +669,7 @@ sk_sp<SkDrawLooper>& CanvasRenderingContext2DState::ShadowOnlyDrawLooper()
   if (!shadow_only_draw_looper_) {
     DrawLooperBuilder draw_looper_builder;
     draw_looper_builder.AddShadow(shadow_offset_, ClampTo<float>(shadow_blur_),
-                                  Color::FromSkColor(shadow_color_),
+                                  shadow_color_,
                                   DrawLooperBuilder::kShadowIgnoresTransforms,
                                   DrawLooperBuilder::kShadowRespectsAlpha);
     shadow_only_draw_looper_ = draw_looper_builder.DetachDrawLooper();
@@ -677,7 +682,7 @@ CanvasRenderingContext2DState::ShadowAndForegroundDrawLooper() const {
   if (!shadow_and_foreground_draw_looper_) {
     DrawLooperBuilder draw_looper_builder;
     draw_looper_builder.AddShadow(shadow_offset_, ClampTo<float>(shadow_blur_),
-                                  Color::FromSkColor(shadow_color_),
+                                  shadow_color_,
                                   DrawLooperBuilder::kShadowIgnoresTransforms,
                                   DrawLooperBuilder::kShadowRespectsAlpha);
     draw_looper_builder.AddUnmodifiedContent();
@@ -691,11 +696,9 @@ sk_sp<PaintFilter>& CanvasRenderingContext2DState::ShadowOnlyImageFilter()
   using ShadowMode = DropShadowPaintFilter::ShadowMode;
   if (!shadow_only_image_filter_) {
     const auto sigma = BlurRadiusToStdDev(shadow_blur_);
-    // TODO(crbug/1308932): Remove FromColor and make all SkColor4f.
     shadow_only_image_filter_ = sk_make_sp<DropShadowPaintFilter>(
         shadow_offset_.x(), shadow_offset_.y(), sigma, sigma,
-        SkColor4f::FromColor(shadow_color_), ShadowMode::kDrawShadowOnly,
-        nullptr);
+        shadow_color_.toSkColor4f(), ShadowMode::kDrawShadowOnly, nullptr);
   }
   return shadow_only_image_filter_;
 }
@@ -708,8 +711,8 @@ CanvasRenderingContext2DState::ShadowAndForegroundImageFilter() const {
     // TODO(crbug/1308932): Remove FromColor and make all SkColor4f.
     shadow_and_foreground_image_filter_ = sk_make_sp<DropShadowPaintFilter>(
         shadow_offset_.x(), shadow_offset_.y(), sigma, sigma,
-        SkColor4f::FromColor(shadow_color_),
-        ShadowMode::kDrawShadowAndForeground, nullptr);
+        shadow_color_.toSkColor4f(), ShadowMode::kDrawShadowAndForeground,
+        nullptr);
   }
   return shadow_and_foreground_image_filter_;
 }
@@ -736,7 +739,7 @@ void CanvasRenderingContext2DState::SetShadowBlur(double shadow_blur) {
   ShadowParameterChanged();
 }
 
-void CanvasRenderingContext2DState::SetShadowColor(SkColor shadow_color) {
+void CanvasRenderingContext2DState::SetShadowColor(Color shadow_color) {
   shadow_color_ = shadow_color;
   ShadowParameterChanged();
 }
