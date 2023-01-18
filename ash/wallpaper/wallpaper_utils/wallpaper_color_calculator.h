3098b593617b893037110c38cc782d5917b06c88
Sean Kau <skau@chromium.org>
2023-01-18 18:40:15 +0000

    Rewrite WallpaperColorCalculator with a Callback
    Removes the Observer interface in favor of a callback since only one
    client receives the result. Rewrite test to use RunLoop and
    cleanup as appropriate.
    
    Bug: b:258750361
    Change-Id: I719bdd1c16d0158f5ce2553725156c56fa9a5c53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4164138
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093985}

--- ash/wallpaper/wallpaper_utils/wallpaper_color_calculator.h
+++ ash/wallpaper/wallpaper_utils/wallpaper_color_calculator.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/wallpaper/wallpaper_utils/wallpaper_calculated_colors.h"
+#include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/image/image_skia.h"
 
@@ -25,8 +27,6 @@ struct ColorProfile;
 
 namespace ash {
 
-class WallpaperColorCalculatorObserver;
-
 // Calculates colors based on a wallpaper image.
 class ASH_EXPORT WallpaperColorCalculator {
  public:
@@ -34,24 +34,21 @@ class ASH_EXPORT WallpaperColorCalculator {
   // that is executed on the |task_runner|.
   WallpaperColorCalculator(
       const gfx::ImageSkia& image,
-      const std::vector<color_utils::ColorProfile>& color_profiles,
-      scoped_refptr<base::TaskRunner> task_runner);
+      const std::vector<color_utils::ColorProfile>& color_profiles);
 
   WallpaperColorCalculator(const WallpaperColorCalculator&) = delete;
   WallpaperColorCalculator& operator=(const WallpaperColorCalculator&) = delete;
 
   ~WallpaperColorCalculator();
 
-  void AddObserver(WallpaperColorCalculatorObserver* observer);
-
-  void RemoveObserver(WallpaperColorCalculatorObserver* observer);
-
+  using WallpaperColorCallback =
+      base::OnceCallback<void(const WallpaperCalculatedColors&)>;
   // Initiates the calculation and returns false if the calculation fails to be
   // initiated. Observers may be notified synchronously or asynchronously.
   // Callers should be aware that this will make |image_| read-only.
-  [[nodiscard]] bool StartCalculation();
+  [[nodiscard]] bool StartCalculation(WallpaperColorCallback callback);
 
-  WallpaperCalculatedColors const get_calculated_colors() {
+  absl::optional<const WallpaperCalculatedColors> get_calculated_colors() {
     return calculated_colors_;
   }
 
@@ -68,15 +65,11 @@ class ASH_EXPORT WallpaperColorCalculator {
   // record duration metrics.
   void OnAsyncCalculationComplete(
       base::TimeTicks async_start_time,
-      const WallpaperCalculatedColors& calculated_colors);
-
-  // Notifies observers that a color calulation has completed. Called on the
-  // same thread that constructed |this|.
-  void NotifyCalculationComplete(
+      WallpaperColorCallback callback,
       const WallpaperCalculatedColors& calculated_colors);
 
   // The result of the color calculation.
-  WallpaperCalculatedColors calculated_colors_;
+  absl::optional<WallpaperCalculatedColors> calculated_colors_;
 
   // The image to calculate colors from.
   gfx::ImageSkia image_;
@@ -86,8 +79,6 @@ class ASH_EXPORT WallpaperColorCalculator {
 
   // The task runner to run the calculation on.
   scoped_refptr<base::TaskRunner> task_runner_;
-
-  base::ObserverList<WallpaperColorCalculatorObserver>::Unchecked observers_;
 
   base::WeakPtrFactory<WallpaperColorCalculator> weak_ptr_factory_{this};
 };
