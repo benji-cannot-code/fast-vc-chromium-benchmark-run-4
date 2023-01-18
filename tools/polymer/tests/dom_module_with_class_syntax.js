954f986e75b0d8021595d9571e85c8eb19fde9bb
dpapad <dpapad@chromium.org>
2023-01-18 02:38:54 +0000

    WebUI: Delete polymer_modulizer(), no longer used.
    This GN rule was used to auto-generate Polymer3 code from existing
    checked-in Polymer2 code and was useful during migration of Polymer2 to
    Polymer3. The last usages of this rule (in CrOS OOBE) were recently
    removed and this migration tool can finally be deleted!
    
    Bug: 1184053
    Change-Id: I9d6fb2a875d1617b2e14ca1e865538b6178b166d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4171731
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093704}

--- tools/polymer/tests/dom_module_with_class_syntax.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/* #js_imports_placeholder */
-
-class CrTestFoo extends PolymerElement {
-  static get is() { return 'cr-test-foo'; }
-
-  /* #html_template_placeholder */
-
-}
