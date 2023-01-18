ae196b6a7d2609c47a852be953700b420381a7d7
Jason Zhang <jiajunz@google.com>
2023-01-18 00:20:27 +0000

    [CrOS Hotspot] Update SetTetheringEnabled shill API
    This CL updates the SetTetheringEnabled shill API since platform side
    updates the corresponding API to return a StringCallback with the
    result.
    
    Bug: b/239477916
    Change-Id: I0dd9e28a50f7604436e48a381b40b2ff8876a97f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4163782
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093630}

--- chromeos/ash/services/hotspot_config/cros_hotspot_config_unittest.cc
+++ chromeos/ash/services/hotspot_config/cros_hotspot_config_unittest.cc
@@ -261,6 +261,8 @@ TEST_F(CrosHotspotConfigTest, EnableHotspot) {
   SetReadinessCheckResultReady();
   SetValidHotspotCapabilities();
   AddActiveCellularService();
+  helper()->manager_test()->SetSimulateTetheringEnableResult(
+      FakeShillSimulatedResult::kSuccess, shill::kTetheringEnableResultSuccess);
   base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(mojom::HotspotControlResult::kSuccess, EnableHotspot());
@@ -276,6 +278,10 @@ TEST_F(CrosHotspotConfigTest, EnableHotspot) {
 }
 
 TEST_F(CrosHotspotConfigTest, DisableHotspot) {
+  helper()->manager_test()->SetSimulateTetheringEnableResult(
+      FakeShillSimulatedResult::kSuccess, shill::kTetheringEnableResultSuccess);
+  base::RunLoop().RunUntilIdle();
+
   EXPECT_EQ(mojom::HotspotControlResult::kSuccess, DisableHotspot());
 }
 
