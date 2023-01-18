17fcc45b82bba332595034a87eeffc82b0043eda
Stefan Zager <szager@chromium.org>
2023-01-18 18:45:33 +0000

    Run a11y serialization during post-lifecycle steps
    Change-Id: I8b5b561d945c5f53dfe74c11820dcac95219d31b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166261
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093988}

--- content/renderer/accessibility/render_accessibility_manager.cc
+++ content/renderer/accessibility/render_accessibility_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "content/renderer/accessibility/render_accessibility_impl.h"
 #include "content/renderer/render_frame_impl.h"
+#include "third_party/blink/public/common/features.h"
 
 namespace content {
 
@@ -56,8 +57,10 @@ void RenderAccessibilityManager::SetMode(const ui::AXMode& new_mode) {
 
   if (new_mode.has_mode(ui::AXMode::kWebContents) &&
       !old_mode.has_mode(ui::AXMode::kWebContents)) {
-    render_accessibility_ =
-        std::make_unique<RenderAccessibilityImpl>(this, render_frame_);
+    render_accessibility_ = std::make_unique<RenderAccessibilityImpl>(
+        this, render_frame_,
+        base::FeatureList::IsEnabled(
+            blink::features::kSerializeAccessibilityPostLifecycle));
   } else if (!new_mode.has_mode(ui::AXMode::kWebContents) &&
              old_mode.has_mode(ui::AXMode::kWebContents)) {
     render_accessibility_.reset();
