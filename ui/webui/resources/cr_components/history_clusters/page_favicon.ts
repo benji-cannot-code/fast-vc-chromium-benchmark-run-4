bb49fde681737050af6f3f526718cbca13b044de
Tommy C. Li <tommycli@chromium.org>
2023-01-18 19:32:52 +0000

    [journeys] Use enhanced favicons when visit is known to Sync
    This brings Journeys favicons up to parity with History favicons,
    assuming that full History Sync is enabled. (It's rolling out now.)
    
    In practice `is_known_to_sync` won't be set to true until this flag
    is enabled:
    chrome://flags/#enable-sync-history-datatype
    
    Therefore this code path is de-facto gated by that flag.
    
    Bug: 1256979
    Change-Id: I4e719e39caa0b7eea1dc39684068056c5c7d5392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174582
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094018}

--- ui/webui/resources/cr_components/history_clusters/page_favicon.ts
+++ ui/webui/resources/cr_components/history_clusters/page_favicon.ts
@@ -67,6 +67,12 @@ class PageFavicon extends PolymerElement {
        * this defined, in which case we fallback to the favicon.
        */
       imageUrl: Object,
+
+      /**
+       * Whether this visit is known to sync already. Used for the purpose of
+       * fetching higher quality favicons in that case.
+       */
+      isKnownToSync: Boolean,
     };
   }
 
@@ -76,6 +82,7 @@ class PageFavicon extends PolymerElement {
 
   url: Url;
   imageUrl: Url;
+  isKnownToSync: boolean;
 
   //============================================================================
   // Helper methods
@@ -92,7 +99,7 @@ class PageFavicon extends PolymerElement {
     }
     return `background-image:${
         getFaviconForPageURL(
-            this.url.url, false, '', /** --favicon-size */ 16)}`;
+            this.url.url, this.isKnownToSync, '', /** --favicon-size */ 16)}`;
   }
 }
 
