9d1b3613c5ca986ba4904922bf86d4e5aff84203
David Padlipsky <dpad@google.com>
2023-01-18 23:56:33 +0000

    Settings Split: Remove unused pref initializers
    Instead of splitting pref initializers and updates like in original
    design, both will be handled by device specific pref handlers.
    
    Bug: b/241965700
    Test: ash_unittests
    Change-Id: I44ae392b68dbe0e324691185d3f7c73d66a7a52c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148695
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094160}

--- ash/system/input_device_settings/fake_input_device_tracker.h
+++ ash/system/input_device_settings/fake_input_device_tracker.h
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INITIALIZERS_FAKE_INPUT_DEVICE_TRACKER_H_
-#define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INITIALIZERS_FAKE_INPUT_DEVICE_TRACKER_H_
+#ifndef ASH_SYSTEM_INPUT_DEVICE_SETTINGS_FAKE_INPUT_DEVICE_TRACKER_H_
+#define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_FAKE_INPUT_DEVICE_TRACKER_H_
 
 #include <vector>
 
-#include "ash/system/input_device_settings/initializers/input_device_tracker.h"
+#include "ash/system/input_device_settings/input_device_tracker.h"
 
 namespace ash {
 
@@ -49,4 +49,4 @@ class FakeInputDeviceTracker : public InputDeviceTracker {
 
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INITIALIZERS_FAKE_INPUT_DEVICE_TRACKER_H_
+#endif  // ASH_SYSTEM_INPUT_DEVICE_SETTINGS_FAKE_INPUT_DEVICE_TRACKER_H_
--- ash/system/input_device_settings/input_device_tracker_impl.h
+++ ash/system/input_device_settings/input_device_tracker_impl.h
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INITIALIZERS_INPUT_DEVICE_TRACKER_IMPL_H_
-#define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INITIALIZERS_INPUT_DEVICE_TRACKER_IMPL_H_
+#ifndef ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INPUT_DEVICE_TRACKER_IMPL_H_
+#define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INPUT_DEVICE_TRACKER_IMPL_H_
 
 #include <memory>
 
 #include "ash/ash_export.h"
-#include "ash/system/input_device_settings/initializers/input_device_tracker.h"
+#include "ash/system/input_device_settings/input_device_tracker.h"
 #include "components/prefs/pref_member.h"
 
 class PrefService;
@@ -43,4 +43,4 @@ class ASH_EXPORT InputDeviceTrackerImpl : public InputDeviceTracker {
 
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INITIALIZERS_INPUT_DEVICE_TRACKER_IMPL_H_
+#endif  // ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INPUT_DEVICE_TRACKER_IMPL_H_
