5b23c6256fecef2c84d67df67acbd99cd2480174
Anthony Garant <anthonygarant@chromium.org>
2023-01-18 15:00:23 +0000

    [Trigger Attestation] Add attribution request helper
    AttributionRequestHelper handles attribution-reporting-api related
    operations that must happen in the network service. It is meant to be
    optionally hooked to a url_loader instance.
    
    As part of this CL, the helper is used to orchestrate Trigger
    Attestation. It could also be used to handle attribution response
    parsing that currently happens in blink.
    
    This class can be viewed in context in this Trigger Attestation
    prototype CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/4048207
    
    Bug: 1399826
    Change-Id: Ie9fc973069f510724b5c6984bd3ea1641ab32eed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4097288
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093863}

---
+++ services/network/attribution/boringssl_attestation_cryptographer.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/attribution/boringssl_attestation_cryptographer.h"
+
+namespace network {
+
+// TODO(crbug.com/1395818): An empty implementation has been added when
+// `AttributionRequestHelper` was added as it instantiates an
+// `BoringsslAttestationCryptographer`. The implementation should be added here.
+
+BoringsslAttestationCryptographer::BoringsslAttestationCryptographer() =
+    default;
+
+BoringsslAttestationCryptographer::~BoringsslAttestationCryptographer() =
+    default;
+
+bool BoringsslAttestationCryptographer::Initialize(
+    mojom::TrustTokenProtocolVersion issuer_configured_version) {
+  return false;
+}
+
+bool BoringsslAttestationCryptographer::AddKey(base::StringPiece key) {
+  return false;
+}
+
+absl::optional<std::string> BoringsslAttestationCryptographer::BeginIssuance(
+    base::StringPiece message) {
+  return absl::nullopt;
+}
+
+absl::optional<std::string>
+BoringsslAttestationCryptographer::ConfirmIssuanceAndBeginRedemption(
+    base::StringPiece response_header) {
+  return absl::nullopt;
+}
+
+}  // namespace network
