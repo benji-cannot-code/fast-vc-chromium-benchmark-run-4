266dffaa7902fa303d4323e09f9952dded66d164
Jason Zhang <jiajunz@google.com>
2023-01-18 06:07:53 +0000

    [CrOS Hotspot] Support all band in hotspot config
    This CL adds WiFiBand::KAll in WiFiBand enums in hotspot mojom file.
    Platform team suggests using shill::kBandAll or shill::kBand2GHz for
    hotspot config band property. When setting band to kAll in hotspot
    config, shill will choose the best bandwidth for the hotspot based on
    the device.
    
    Bug: b/239477916
    Change-Id: Ib9cb4dad5470a9a2e45ebf583d6327e8a685e4dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4157642
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093740}

--- chrome/test/data/webui/settings/chromeos/hotspot_config_dialog_tests.js
+++ chrome/test/data/webui/settings/chromeos/hotspot_config_dialog_tests.js
@@ -56,7 +56,7 @@ suite('HotspotConfigDialog', function() {
         security: WiFiSecurityMode.kWpa2,
         ssid: 'test_ssid',
         passphrase: 'test_passphrase',
-        band: WiFiBand.k5GHz,
+        band: WiFiBand.kAutoChoose,
         bssidRandomization: true,
       },
     };
