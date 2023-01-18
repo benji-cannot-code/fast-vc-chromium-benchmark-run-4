d44f24a6895b02eca7258edd4bd1d7c099804eee
dpapad <dpapad@chromium.org>
2023-01-18 00:57:03 +0000

    WebUI: Decouple polymer/html_to_js.py from polymer/polymer.py.
    html_to_js() was reusing some parts of polymer_modulizer()'s
    implementation. Given that polymer_modulizer() is no longer used and
    will be deleted, need to decouple the tow GN rules to unblock the
    deletion, by pulling out the parts that are needed by html_to_js().
    
    Also adding dedicated tests for html_to_js.py, which did not exist
    previously.
    
    This is in preparation of fully remomving polymer_modulizer().
    
    Bug: 1184053
    Change-Id: I0c20577f215cf6009fa6f511ad9dfc3f61e4f41f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173117
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093655}

---
+++ tools/polymer/tests/html_to_js/foo.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer({
+  is: 'cr-foo',
+
+  _template: html`
+{__html_template__}
+  `,
+});
---
+++ tools/polymer/tests/html_to_js/foo_expected.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer({
+  is: 'cr-foo',
+
+  _template: html`
+<!--_html_template_start_-->
+<style>
+  div {
+    font-size: 2rem;
+  }
+</style>
+<div>Hello world</div>
+<!--_html_template_end_-->
+  `,
+});
