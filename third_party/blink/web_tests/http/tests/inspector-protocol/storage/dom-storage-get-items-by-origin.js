79b0a82d69782ca0fee8ffdd7367b0fef6bc0f00
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-01-18 17:55:38 +0000

    Fix condition for getting frame
    bool operator for WTF::String only checks for null, not emptiness
    
    Bug: 1402553
    Change-Id: I87a6a54958c288d55f49afd6b3469805727038e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177178
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093958}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/dom-storage-get-items-by-origin.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {dp} = await testRunner.startBlank(
+      `Tests that getting DOMStorage items by origin doesn't return an error response\n`);
+
+  await dp.DOMStorage.enable();
+  await dp.Page.enable();
+
+  const securityOrigin = (await dp.Page.getResourceTree()).result.frameTree.frame.securityOrigin;
+  const storageId = {securityOrigin, isLocalStorage: true};
+
+  const items = (await dp.DOMStorage.getDOMStorageItems({storageId})).result;
+
+  testRunner.log("Get DOM storage items");
+  testRunner.log(items);
+
+  testRunner.completeTest();
+})
