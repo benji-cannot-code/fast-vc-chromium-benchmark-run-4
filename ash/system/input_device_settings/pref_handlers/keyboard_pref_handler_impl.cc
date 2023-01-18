ce14f45e01d6c930361055fb1e01107e0d442625
David Padlipsky <dpad@google.com>
2023-01-18 22:55:02 +0000

    Settings Split: Initial commit for keyboard pref handler
    The KeyboardPrefHandler will:
    1. Initialize new connected keyboards to the appropriate default
       settings
    2. Pull previously connected keyboards settings and populate the
       mojom::Keyboard object
    3. Update keyboard settings to match what is in the mojom::Keyboard
       object
    
    Bug: b/241965700
    Test: ash_unittests
    Change-Id: I5ace28929068ea7f98d7482e51bb1d2a998e4011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4141125
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094123}

---
+++ ash/system/input_device_settings/pref_handlers/keyboard_pref_handler_impl.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/input_device_settings/pref_handlers/keyboard_pref_handler_impl.h"
+
+#include "ash/public/mojom/input_device_settings.mojom-forward.h"
+#include "base/notreached.h"
+#include "components/prefs/pref_service.h"
+
+namespace ash {
+
+KeyboardPrefHandlerImpl::KeyboardPrefHandlerImpl() = default;
+KeyboardPrefHandlerImpl::~KeyboardPrefHandlerImpl() = default;
+
+// TODO(dpad): Implement keyboard settings initialization.
+void KeyboardPrefHandlerImpl::InitializeKeyboardSettings(
+    PrefService* pref_service,
+    mojom::Keyboard* keyboard) {
+  NOTIMPLEMENTED();
+}
+
+// TODO(dpad): Implement keyboard settings updates.
+void KeyboardPrefHandlerImpl::UpdateKeyboardSettings(
+    PrefService* pref_service,
+    const mojom::Keyboard& keyboard) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace ash
---
+++ ash/system/input_device_settings/pref_handlers/keyboard_pref_handler_unittest.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/input_device_settings/pref_handlers/keyboard_pref_handler_impl.h"
+
+#include "ash/test/ash_test_base.h"
+
+namespace ash {
+
+class KeyboardPrefHandlerTest : public AshTestBase {
+ public:
+  KeyboardPrefHandlerTest() = default;
+  KeyboardPrefHandlerTest(const KeyboardPrefHandlerTest&) = delete;
+  KeyboardPrefHandlerTest& operator=(const KeyboardPrefHandlerTest&) = delete;
+  ~KeyboardPrefHandlerTest() override = default;
+
+  // testing::Test:
+  void SetUp() override {
+    AshTestBase::SetUp();
+    pref_handler_ = std::make_unique<KeyboardPrefHandlerImpl>();
+  }
+
+  void TearDown() override {
+    pref_handler_.reset();
+    AshTestBase::TearDown();
+  }
+
+ protected:
+  std::unique_ptr<KeyboardPrefHandlerImpl> pref_handler_;
+};
+
+TEST_F(KeyboardPrefHandlerTest, InitializationTest) {
+  EXPECT_NE(pref_handler_.get(), nullptr);
+}
+
+}  // namespace ash
