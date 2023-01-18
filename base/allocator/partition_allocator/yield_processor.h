6c9206a5435b7adb076659c9362ac3629310ff0d
Kalvin Lee <kdlee@chromium.org>
2023-01-18 02:36:23 +0000

    [PA] Introduce PA_CONFIG(IS_NONCLANG_MSVC)
    PA nominally supports MSVC and holds noticeable amounts of code that
    only build when we're being compiled by MSVC for real (as opposed to
    clang-cl masquerading as the same). Rather than repeat the condition
    all around, this CL introduces a terser form.
    
    This CL deliberately avoids changing things in `partition_alloc_base/`
    (e.g. `bits.h`), where we prize fidelity to upstream.
    
    Change-Id: Idcc371d0790487182586531d309518c0ebd362a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4172416
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093701}

--- base/allocator/partition_allocator/yield_processor.h
+++ base/allocator/partition_allocator/yield_processor.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_YIELD_PROCESSOR_H_
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_YIELD_PROCESSOR_H_
 
+#include "base/allocator/partition_allocator/partition_alloc_config.h"
 #include "build/build_config.h"
 
 // The PA_YIELD_PROCESSOR macro wraps an architecture specific-instruction that
@@ -17,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_NACL)
 // Inline assembly not allowed.
 #define PA_YIELD_PROCESSOR ((void)0)
-#elif defined(COMPILER_MSVC) && !defined(__clang__)
+#elif PA_CONFIG(IS_NONCLANG_MSVC)
 // MSVC is in its own assemblyless world (crbug.com/1351310#c6).
 #include <windows.h>
 #define PA_YIELD_PROCESSOR (YieldProcessor())
