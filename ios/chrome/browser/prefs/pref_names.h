2d7cbac3b287e1fab310947f17cd000a49bf9fbf
Benjamin Williams <bwwilliams@google.com>
2023-01-18 15:19:05 +0000

    Reland "[iOS] Enable Smart Sorting to add/remove destinations."
    This is a reland of I9fc4943708a466869d652db55fc3e51385b83dc7.
    
    Original change's description:
    > [iOS] Enable Smart Sorting to add/remove destinations.
    >
    > This change enables the Smart Sorting algorithm to support adding new
    > destinations, or removing existing destinations, by introducing the
    > method `runHistoryDiagnostic`.
    >
    > `runHistoryDiagnostic` is called before the Smart Sorting algorithm is
    > run and detects whether or not the destinations list has changed
    > between presentations of the overflow menu.
    >
    > When `runHistoryDiagnostic` detects the carousel and ranking differ,
    > it ensures the ranking gets updated to reflect the items currently in
    > the carousel.
    >
    > This change will be gated behind the feature
    > `kSmartSortingPriceTrackingDestination`, as the Price Tracking
    > destination is the first new destination (with Smart Sorting) to be
    > added to the overflow menu.
    >
    > Feature demo (video):
    > https://drive.google.com/open?id=1Qs3vTh3Lz_GB7PcdrCuQEisTJ5xmYvCw&authuser=0&resourcekey=0-29BLMhhJcj5UCaFOOrjCRg&usp=drive_link
    >
    > Change-Id: I9fc4943708a466869d652db55fc3e51385b83dc7 Bug: 1405265
    > Reviewed-on:
    > https://chromium-review.googlesource.com/c/chromium/src/+/4133648
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org> Commit-Queue: Benjamin
    > Williams <bwwilliams@google.com> Cr-Commit-Position:
    > refs/heads/main@{#1093416}
    
    What's changed in this reland: The unit tests should only be run on iOS
    15+ devices, so each test is now wrapped in an available call.
    
    Bug: 1405265
    Change-Id: Iddb24b3aa222d537f734b4ba6ffa6230a4c6fd36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174762
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093870}

--- ios/chrome/browser/prefs/pref_names.h
+++ ios/chrome/browser/prefs/pref_names.h
@@ -47,6 +47,7 @@ extern const char kNTPContentSuggestionsForSupervisedUserEnabled[];
 extern const char kNTPFollowingFeedSortType[];
 extern const char kDefaultFollowingFeedSortTypeChanged[];
 extern const char kOverflowMenuDestinationUsageHistory[];
+extern const char kOverflowMenuNewDestinations[];
 extern const char kPrintingEnabled[];
 extern const char kSearchSuggestEnabled[];
 extern const char kTrackPricesOnTabsEnabled[];
