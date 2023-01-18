91bc070bbe00066f44723328cc91f1f8eaa8495d
andrewxu <andrewxu@chromium.org>
2023-01-18 17:28:06 +0000

    A test class to wait until the event target receives the target event
    This CL adds a test class that waits until the event target receives
    the target event.
    
    This CL should not bring any difference to the product code.
    
    Bug: b/264934296
    Change-Id: I0ce57c9ece07f14aac852833bad143560ff803e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4172707
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093937}

---
+++ ui/events/test/scoped_event_waiter.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/test/scoped_event_waiter.h"
+
+#include "ui/events/event.h"
+#include "ui/events/event_target.h"
+
+namespace ui::test {
+
+ScopedEventWaiter::ScopedEventWaiter(EventTarget* target,
+                                     EventType target_event_type)
+    : target_(target), target_event_type_(target_event_type) {
+  DCHECK(target);
+  target->AddPreTargetHandler(this);
+}
+
+ScopedEventWaiter::~ScopedEventWaiter() {
+  target_->RemovePreTargetHandler(this);
+}
+
+void ScopedEventWaiter::Wait() {
+  run_loop_.Run();
+}
+
+void ScopedEventWaiter::OnEvent(Event* event) {
+  if (event->type() == target_event_type_) {
+    run_loop_.Quit();
+  }
+}
+
+}  // namespace ui::test
