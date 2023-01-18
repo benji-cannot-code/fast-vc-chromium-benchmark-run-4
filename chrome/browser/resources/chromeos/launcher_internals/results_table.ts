74be0f6739e7a507ed649ceb06b3429b66b6bb0e
Rachel Wong <wrong@chromium.org>
2023-01-18 23:16:28 +0000

    Update the internals page for the productivity launcher.
    The launcher internals page previously reflected the old launcher's UI,
    and results were split by zero state and search based on the content of
    the previous query.
    
    Launcher infrastructure has changed now and there are three separate UI
    surfaces: recent files (also known as "continue"), recent apps, and
    search. This CL updates the launcher internals page to split results
    into these three categories based on their display type. The display
    types are hardcoded but stable strings.
    
    Also update the results table to use trusted types if available. See
    https://source.chromium.org/chromium/chromium/src/+/main:docs/trusted_types_on_webui.md
    for more details.
    
    Bug: b/259860720
    Change-Id: I7d6b49be6d06edcde10bddecc5a2a5f49fd30719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176212
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094133}

--- chrome/browser/resources/chromeos/launcher_internals/results_table.ts
+++ chrome/browser/resources/chromeos/launcher_internals/results_table.ts
@@ -50,7 +50,8 @@ export class LauncherResultsTableElement extends PolymerElement {
 
   clearResults() {
     this.results.clear();
-    this.$.resultsSection.innerHTML = '';
+    this.$.resultsSection.innerHTML =
+        window.trustedTypes ? (window.trustedTypes.emptyHTML) : '';
     for (const cell of this.headerCells.values()) {
       this.$.headerRow.removeChild(cell);
     }
@@ -101,7 +102,8 @@ export class LauncherResultsTableElement extends PolymerElement {
     }
 
     // Clear and repopulate the results table.
-    this.$.resultsSection.innerHTML = '';
+    this.$.resultsSection.innerHTML =
+        window.trustedTypes ? (window.trustedTypes.emptyHTML) : '';
     for (const result of sortedResults) {
       const newRow = this.$.resultsSection.insertRow();
       newRow.addEventListener('click', (e: Event) => this.toggleRowSelected(e));
