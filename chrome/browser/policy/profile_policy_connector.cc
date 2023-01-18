23799a525298c5d99998e4db77a66baeb27875fa
Maksim Ivanov <emaxx@google.com>
2023-01-18 17:24:43 +0000

    Add UMA for initial policy load time
    Collect data on how much it takes for the policy stack to
    initialize (regardless of whether via disk cache or by fetching
    data from DMServer).
    
    This data will help measure the impact of the planned tweaking of
    policy stack's worker task priorities.
    
    Bug: b:263949579
    Change-Id: I1d83907f76eb59fbfdd4bf01d9166abf690ecb55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128137
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093936}

--- chrome/browser/policy/profile_policy_connector.cc
+++ chrome/browser/policy/profile_policy_connector.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/check.h"
+#include "base/check_op.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
@@ -34,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/proxy_policy_provider.h"
 #include "components/policy/core/common/schema_registry_tracking_policy_provider.h"
 #include "components/policy/policy_constants.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/policy/restricted_mgs_policy_provider.h"
@@ -49,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
+#include "components/user_manager/user_type.h"
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
@@ -157,7 +161,12 @@ ProxyPolicyProvider* GetProxyPolicyProvider() {
 
 ProfilePolicyConnector::ProfilePolicyConnector() = default;
 
-ProfilePolicyConnector::~ProfilePolicyConnector() = default;
+ProfilePolicyConnector::~ProfilePolicyConnector() {
+  if (policy_service_) {
+    // We should've subscribed by this point, but in case not that's a no-op.
+    policy_service_->RemoveObserver(POLICY_DOMAIN_CHROME, this);
+  }
+}
 
 void ProfilePolicyConnector::Init(
     const user_manager::User* user,
@@ -166,10 +175,15 @@ void ProfilePolicyConnector::Init(
     const CloudPolicyStore* policy_store,
     policy::ChromeBrowserPolicyConnector* connector,
     bool force_immediate_load) {
+  DCHECK(!configuration_policy_provider_);
+  DCHECK(!policy_store_);
+  DCHECK(!policy_service_);
+
   configuration_policy_provider_ = configuration_policy_provider;
   policy_store_ = policy_store;
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+  user_ = user;
   auto* browser_policy_connector =
       static_cast<BrowserPolicyConnectorAsh*>(connector);
 #else
@@ -225,10 +239,12 @@ void ProfilePolicyConnector::Init(
         std::make_unique<LoginProfilePolicyProvider>(
             browser_policy_connector->GetPolicyService());
   } else {
+    auto* const manager = user_manager::UserManager::Get();
     // |user| should never be nullptr except for the signin and the lock screen
     // app profile.
-    is_primary_user_ =
-        user == user_manager::UserManager::Get()->GetPrimaryUser();
+    is_primary_user_ = user == manager->GetPrimaryUser();
+    is_user_new_ =
+        user == manager->GetActiveUser() && manager->IsCurrentUserNew();
     // Note that |DeviceLocalAccountPolicyProvider::Create| returns nullptr when
     // the user supplied is not a device-local account user.
     special_user_policy_provider_ = DeviceLocalAccountPolicyProvider::Create(
@@ -290,12 +306,15 @@ void ProfilePolicyConnector::Init(
   policy_service_ = std::make_unique<PolicyServiceImpl>(policy_providers_,
                                                         std::move(migrators));
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+  DoPostInit();
 }
 
 void ProfilePolicyConnector::InitForTesting(
     std::unique_ptr<PolicyService> service) {
   DCHECK(!policy_service_);
   policy_service_ = std::move(service);
+  DoPostInit();
 }
 
 void ProfilePolicyConnector::OverrideIsManagedForTesting(bool is_managed) {
@@ -380,6 +399,35 @@ base::flat_set<std::string> ProfilePolicyConnector::user_affiliation_ids()
   return {ids.begin(), ids.end()};
 }
 
+void ProfilePolicyConnector::OnPolicyServiceInitialized(PolicyDomain domain) {
+  DCHECK_EQ(domain, POLICY_DOMAIN_CHROME);
+  ReportChromePolicyInitialized();
+}
+
+void ProfilePolicyConnector::DoPostInit() {
+  DCHECK(policy_service_);
+  creation_time_for_metrics_ = base::TimeTicks::Now();
+  policy_service_->AddObserver(POLICY_DOMAIN_CHROME, this);
+  if (policy_service_->IsInitializationComplete(POLICY_DOMAIN_CHROME)) {
+    ReportChromePolicyInitialized();
+  }
+}
+
+void ProfilePolicyConnector::ReportChromePolicyInitialized() {
+  // Protect against multiple notifications: we want to report the metric only
+  // once per profile session.
+  if (!creation_time_for_metrics_.has_value()) {
+    return;
+  }
+  std::string metric_suffix = GetTimeToFirstPolicyLoadMetricSuffix();
+  if (!metric_suffix.empty()) {
+    base::UmaHistogramMediumTimes(
+        "Enterprise.TimeToFirstPolicyLoad.Profile." + metric_suffix,
+        base::TimeTicks::Now() - creation_time_for_metrics_.value());
+  }
+  creation_time_for_metrics_.reset();
+}
+
 const CloudPolicyStore* ProfilePolicyConnector::GetActualPolicyStore() const {
   if (policy_store_)
     return policy_store_;
@@ -418,6 +466,48 @@ void ProfilePolicyConnector::AppendPolicyProviderWithSchemaTracking(
   wrapped_policy_provider->Init(schema_registry);
   policy_providers_.push_back(wrapped_policy_provider.get());
   wrapped_policy_providers_.push_back(std::move(wrapped_policy_provider));
+}
+
+std::string ProfilePolicyConnector::GetTimeToFirstPolicyLoadMetricSuffix()
+    const {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (!is_primary_user_) {
+    // Don't report the metric for secondary users: we're only interested in the
+    // delay during the initial load as it blocks any other UX.
+    return "";
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+  if (!IsManaged()) {
+    return "Unmanaged";
+  }
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  DCHECK(user_);
+  switch (user_->GetType()) {
+    case user_manager::USER_TYPE_REGULAR:
+      if (user_manager::UserManager::Get()->IsUserCryptohomeDataEphemeral(
+              user_->GetAccountId())) {
+        return "Managed.Ephemeral";
+      }
+      return is_user_new_ ? "Managed.NewPersistent" : "Managed.Existing";
+    case user_manager::USER_TYPE_CHILD:
+      return "Child";
+    case user_manager::USER_TYPE_PUBLIC_ACCOUNT:
+      return "ManagedGuestSession";
+    case user_manager::USER_TYPE_KIOSK_APP:
+    case user_manager::USER_TYPE_ARC_KIOSK_APP:
+    case user_manager::USER_TYPE_WEB_KIOSK_APP:
+      return "Kiosk";
+    case user_manager::USER_TYPE_GUEST:
+    case user_manager::USER_TYPE_ACTIVE_DIRECTORY:
+    case user_manager::NUM_USER_TYPES:
+      // Don't report the metric in uninteresting or unreachable cases.
+      return "";
+  }
+#else   // BUILDFLAG(IS_CHROMEOS_ASH)
+  return "Managed";
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
