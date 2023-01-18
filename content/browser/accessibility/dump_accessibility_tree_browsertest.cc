a342cdef76713ce9af8b579f09070a1b72c52928
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-01-18 19:09:10 +0000

    Revert "Run a11y serialization during post-lifecycle steps"
    This reverts commit c9d172b75633b769ff79b2c4caaf57c837763091.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8791606563358015553
    
    Sample failed build: https://ci.chromium.org/b/8791606563358015553
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8791606563358015553&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4166261
    
    Original change's description:
    > Run a11y serialization during post-lifecycle steps
    >
    > Change-Id: I8b5b561d945c5f53dfe74c11820dcac95219d31b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166261
    > Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: Stefan Zager <szager@chromium.org>
    > Commit-Queue: Stefan Zager <szager@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1093988}
    >
    
    Change-Id: I5dead94d1ae24d64531222097007b4390d32353a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178593
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1094001}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
-#include "third_party/blink/public/common/features.h"
 #include "ui/accessibility/platform/inspect/ax_api_type.h"
 
 #if BUILDFLAG(IS_MAC)
@@ -2907,11 +2906,6 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 #endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
                        MAYBE_AccessibilitySelectMenu) {
-  // Fails when synchronous a11y serialization is enabled - crbug.com/1401767
-  if (base::FeatureList::IsEnabled(
-          blink::features::kSerializeAccessibilityPostLifecycle)) {
-    return;
-  }
   RunHtmlTest(FILE_PATH_LITERAL("selectmenu.html"));
 }
 
