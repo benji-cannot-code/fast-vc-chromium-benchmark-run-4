e15a183372e5f1997dfcee697a524b3fd1b9d754
Rakina Zata Amni <rakina@chromium.org>
2023-01-18 03:25:06 +0000

    Revert "Remove GetOfflineWallpaperList()."
    This reverts commit a96969e3c0af7ebeee423a02aee08c48f22a3531.
    
    Reason for revert: Potentially causing failure for WallpaperControllerTest.SetOnlineWallpaper
     https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/63336/blamelist
    
    Original change's description:
    > Remove GetOfflineWallpaperList().
    >
    > It looks like it's unused, except for one spot in the unit tests and
    > it's easy to rewrite that test expectations.
    >
    > Bug: None
    > Change-Id: I47ccb6563f0f8794c5aaefee07ffbe85b7a77f12
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4160526
    > Commit-Queue: Eric Sum <esum@google.com>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Reviewed-by: Tao Wu <wutao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1093491}
    
    Bug: None
    Change-Id: I175525a8b254cc772e07b6ec48e49319873b0b94
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176292
    Owners-Override: Rakina Zata Amni <rakina@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1093715}

--- chrome/browser/ui/ash/test_wallpaper_controller.cc
+++ chrome/browser/ui/ash/test_wallpaper_controller.cc
@@ -226,6 +226,11 @@ void TestWallpaperController::RemovePolicyWallpaper(
   NOTIMPLEMENTED();
 }
 
+void TestWallpaperController::GetOfflineWallpaperList(
+    GetOfflineWallpaperListCallback callback) {
+  NOTIMPLEMENTED();
+}
+
 void TestWallpaperController::SetAnimationDuration(
     base::TimeDelta animation_duration) {
   NOTIMPLEMENTED();
