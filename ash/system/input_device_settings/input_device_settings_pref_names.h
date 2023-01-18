9f27fbfebbe5b6129326efbde47583be3f82e9aa
David Padlipsky <dpad@google.com>
2023-01-18 23:19:00 +0000

    Settings Split: Implement updating of keyboard settings in prefs
    Bug: b/241965700
    Test: ash_unittests
    Change-Id: Ia98e65e1ea982db566f8b8fb41d61b9a3fd036cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4141003
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094135}

--- ash/system/input_device_settings/input_device_settings_pref_names.h
+++ ash/system/input_device_settings/input_device_settings_pref_names.h
@@ -7,13 +7,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INPUT_DEVICE_SETTINGS_PREF_NAMES_H_
 
 namespace ash::prefs {
-constexpr char kMouseObservedDevicesPref[] = "settings.mouse.observed_devices";
-constexpr char kTouchpadObservedDevicesPref[] =
-    "settings.touchpad.observed_devices";
-constexpr char kPointingStickObservedDevicesPref[] =
-    "settings.pointing_stick.observed_devices";
+// Prefs which contain lists of observed devices for a few milestones before
+// per-device settings are enabled.
 constexpr char kKeyboardObservedDevicesPref[] =
     "settings.keyboard.observed_devices";
+constexpr char kMouseObservedDevicesPref[] = "settings.mouse.observed_devices";
+constexpr char kPointingStickObservedDevicesPref[] =
+    "settings.pointing_stick.observed_devices";
+constexpr char kTouchpadObservedDevicesPref[] =
+    "settings.touchpad.observed_devices";
+
+// Prefs which contain dictionaries of settings for each connected device.
+constexpr char kKeyboardDeviceSettingsDictPref[] = "settings.keyboard.devices";
+constexpr char kMouseDeviceSettingsDictPref[] = "settings.mouse.devices";
+constexpr char kPointingStickDeviceSettingsDictPref[] =
+    "settings.pointing_stick.devices";
+constexpr char kTouchpadDeviceSettingsDictPref[] = "settings.touchpad.devices";
+
+// Keyboard settings dictionary keys.
+constexpr char kKeyboardSettingAutoRepeatDelay[] = "auto_repeat_delay";
+constexpr char kKeyboardSettingAutoRepeatEnabled[] = "auto_repeat_enabled";
+constexpr char kKeyboardSettingAutoRepeatInterval[] = "auto_repeat_interval";
+constexpr char kKeyboardSettingModifierRemappings[] = "modifier_remappings";
+constexpr char kKeyboardSettingSuppressMetaFKeyRewrites[] =
+    "suppress_meta_fkey_rewrites";
+constexpr char kKeyboardSettingTopRowAreFKeys[] = "top_row_are_fkeys";
 }  // namespace ash::prefs
 
 #endif  // ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INPUT_DEVICE_SETTINGS_PREF_NAMES_H_
