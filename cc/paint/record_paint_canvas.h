c83aad82ff4799c5bc07513b0f24fd25be230271
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-01-19 21:10:33 +0000

    Update PaintCanvas::saveLayerAlpha to accept floats.
    `saveLayerAlpha` was converting the alpha to float in any case, but
    accepting `uint8_t` meant that callers already having float alphas
    had to cast them to int, dropping numerical precision.
    
    Change-Id: Idd37357d193c5f044cffb524d9cbc6f085b3f13e
    Bug: 1399566, 1407289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4175373
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094643}

--- cc/paint/record_paint_canvas.h
+++ cc/paint/record_paint_canvas.h
@@ -47,8 +47,8 @@ class CC_PAINT_EXPORT RecordPaintCanvas : public PaintCanvas {
   int save() override;
   int saveLayer(const PaintFlags& flags) override;
   int saveLayer(const SkRect& bounds, const PaintFlags& flags) override;
-  int saveLayerAlpha(uint8_t alpha) override;
-  int saveLayerAlpha(const SkRect& bounds, uint8_t alpha) override;
+  int saveLayerAlphaf(float alpha) override;
+  int saveLayerAlphaf(const SkRect& bounds, float alpha) override;
   void restore() override;
   int getSaveCount() const final;
   void restoreToCount(int save_count) override;
@@ -206,8 +206,8 @@ class CC_PAINT_EXPORT InspectableRecordPaintCanvas : public RecordPaintCanvas {
   int save() override;
   int saveLayer(const PaintFlags& flags) override;
   int saveLayer(const SkRect& bounds, const PaintFlags& flags) override;
-  int saveLayerAlpha(uint8_t alpha) override;
-  int saveLayerAlpha(const SkRect& bounds, uint8_t alpha) override;
+  int saveLayerAlphaf(float alpha) override;
+  int saveLayerAlphaf(const SkRect& bounds, float alpha) override;
   void restore() override;
 
   void translate(SkScalar dx, SkScalar dy) override;
