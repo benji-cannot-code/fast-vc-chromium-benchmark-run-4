30bc9b7753ff459896eca4a5eb339d0cf439cf15
Akihiro Ota <akihiroota@chromium.org>
2023-01-19 17:34:48 +0000

    Reland "Dictation: Remove polling from DictationTestSupport functions"
    This is a reland of commit 6885c766143bb936ab554d6b254e80bd47e160c7
    
    The original change was reverted due to flakiness (see [1] or [2] for
    examples). The culprit of the flakes was in the waitForEditableValue
    function. In that function, if the actual value didn't already
    match the expected value, then we attached an event listener and
    waited for value_in_text_field_changed events. However, upon closer
    inspection, that listener never actually gets called for content
    editables, which caused certain content editable tests to timeout;
    it's unclear why the listener never gets called, since the same event
    gets fired for <textarea>s and <input>s. Also, printing the editableNode
    returns the expected AutomationNode.
    
    The solution here is to attach an additional event listener that gets
    called whenever the surrounding text changes via the IME API. I verified that this approach works by running each of the flaky tests
    like so:
    
    out/linux-cros-rel/browser_tests
    --gtest_filter="*Content*DictationTest.NoExtraSpaceForPunctuation"
    --gtest_repeat=70 --test-launcher-jobs=70
    
    [1] https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/63183/test-results?sortby=&groupby=
    
    [2] https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/63170/test-results?sortby=&groupby=
    
    Original change's description:
    > Dictation: Remove polling from DictationTestSupport functions
    >
    > This change transitions the waitFor... functions in
    > dictation_test_support.js to use events and listeners, rather than polling.
    >
    > AX-Relnotes: N/A
    > Bug: b:264535324
    > Change-Id: Ie601c3fec819de21756d19417544a2536b55511e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134029
    > Reviewed-by: Katie Dektar <katie@chromium.org>
    > Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1090982}
    
    Bug: b:264535324, 1407194
    Change-Id: Ia21acb434c0d3a87b801d4a7631eadc2c9fe301b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177594
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094537}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/input_controller.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/input_controller.js
@@ -59,11 +59,17 @@ export class InputController {
     /** @private {?function(number):void} */
     this.onBlurListener_ = null;
 
-    /** @private {?function(string, !SurroundingInfo):void} */
+    /**
+     * A listener for chrome.input.ime.onSurroundingTextChanged events.
+     * @private {?function(string, !SurroundingInfo):void}
+     */
     this.onSurroundingTextChangedListener_ = null;
 
     /** @private {?SurroundingInfo} */
     this.surroundingInfo_ = null;
+
+    /** @private {?function(): void} */
+    this.onSurroundingTextChangedForTesting_ = null;
 
     this.initialize_();
   }
@@ -209,6 +215,9 @@ export class InputController {
     }
 
     this.surroundingInfo_ = surroundingInfo;
+    if (this.onSurroundingTextChangedForTesting_) {
+      this.onSurroundingTextChangedForTesting_();
+    }
   }
 
   /**
