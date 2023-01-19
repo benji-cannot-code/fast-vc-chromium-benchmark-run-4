9190d1a47bd386ca2dfe4022a3a99043c065619b
Gabriel Brito <gabrielbrito@microsoft.com>
2023-01-19 10:41:48 +0000

    [Notification] Include HRESULT check in if statement
    clang-tidy has flagged this file as having an unused variable. We should
    also check the `hr` variable in the following "if" statement.
    
    Bug: 1406405
    Change-Id: I0a2f45cba74cb8681dacc9d3c38bf5f8c33a2269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4152867
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094379}

--- chrome/browser/notifications/win/notification_util.cc
+++ chrome/browser/notifications/win/notification_util.cc
@@ -44,7 +44,7 @@ NotificationLaunchId GetNotificationLaunchId(
 
   UINT32 length;
   hr = elements->get_Length(&length);
-  if (length == 0) {
+  if (FAILED(hr) || length == 0) {
     LogGetNotificationLaunchIdStatus(
         GetNotificationLaunchIdStatus::kMissingToastElementInDoc);
     DLOG(ERROR) << "No <toast> elements in document.";
