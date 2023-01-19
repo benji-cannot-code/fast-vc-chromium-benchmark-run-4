70339ba7861eb34cfbe198836c03fec0e32dcf61
Maciek Swiech <drmasquatch@google.com>
2023-01-19 17:04:27 +0000

    Add observer for apps sync opt-in changes
    Add a SyncServiceObserver that watches for changes to AppsSync opt-in
    for profiles. On startup as well as changes, it will write out an opt-in
    file to /run/daemon-store to allow ChromOS processes to assess if
    AppsSync opt-in has been granted without needing to communicate
    directly with Chrome.
    
    Bug: chromium:1193485
    Change-Id: If9d61e4f80ee80a58ca8731fa5ce2d86a3aa3b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3823147
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Maciek Swiech <drmasquatch@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094511}

---
+++ chrome/browser/ash/sync/sync_appsync_service.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/sync/sync_appsync_service.h"
+
+#include "chrome/browser/ash/sync/sync_appsync_optin_client.h"
+
+namespace ash {
+
+SyncAppsyncService::SyncAppsyncService(
+    syncer::SyncService* sync_service,
+    user_manager::UserManager* user_manager) {
+  appsync_optin_client_ =
+      std::make_unique<SyncAppsyncOptinClient>(sync_service, user_manager);
+}
+
+SyncAppsyncService::~SyncAppsyncService() = default;
+
+void SyncAppsyncService::Shutdown() {
+  appsync_optin_client_ = nullptr;
+}
+
+}  // namespace ash
---
+++ chrome/browser/ash/sync/sync_appsync_service_factory.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_SYNC_SYNC_APPSYNC_SERVICE_FACTORY_H_
+#define CHROME_BROWSER_ASH_SYNC_SYNC_APPSYNC_SERVICE_FACTORY_H_
+
+#include "chrome/browser/profiles/profile_keyed_service_factory.h"
+
+class Profile;
+
+namespace base {
+template <typename T>
+struct DefaultSingletonTraits;
+}  // namespace base
+
+namespace ash {
+
+class SyncAppsyncService;
+
+class SyncAppsyncServiceFactory : public ProfileKeyedServiceFactory {
+ public:
+  static SyncAppsyncService* GetForProfile(Profile* profile);
+  static SyncAppsyncServiceFactory* GetInstance();
+
+  SyncAppsyncServiceFactory(const SyncAppsyncServiceFactory& other) = delete;
+  SyncAppsyncServiceFactory& operator=(const SyncAppsyncServiceFactory& other) =
+      delete;
+
+ private:
+  friend struct base::DefaultSingletonTraits<SyncAppsyncServiceFactory>;
+
+  SyncAppsyncServiceFactory();
+  ~SyncAppsyncServiceFactory() override;
+
+  // BrowserContextKeyedServiceFactory implementation.
+  KeyedService* BuildServiceInstanceFor(
+      content::BrowserContext* context) const override;
+};
+
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_SYNC_SYNC_APPSYNC_SERVICE_FACTORY_H_
