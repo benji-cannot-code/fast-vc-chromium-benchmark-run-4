b8ddab5856b5b590b2c24310eb0ff4cb699a4aac
Jonathan Njeunje <njeunje@chromium.org>
2023-01-19 18:47:19 +0000

    Remove of obsolete TODOs
    This TODOs have already been carried out via http://go/crrv/c/3854640.
    
    Bug: 1199077
    Change-Id: Ic6010bd46869c70e657dc0bd29e0c35de7c56b77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181641
    Auto-Submit: Jonathan Njeunje <njeunje@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094581}

--- content/browser/devtools/protocol/storage_handler.cc
+++ content/browser/devtools/protocol/storage_handler.cc
@@ -195,14 +195,14 @@ class StorageHandler::IndexedDBObserver
 
   ~IndexedDBObserver() override { DCHECK_CURRENTLY_ON(BrowserThread::UI); }
 
-  void TrackOrigin(const blink::StorageKey& storage_key) {
+  void TrackStorageKey(const blink::StorageKey& storage_key) {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
     if (storage_keys_.find(storage_key) != storage_keys_.end())
       return;
     storage_keys_.insert(storage_key);
   }
 
-  void UntrackOrigin(const blink::StorageKey& storage_key) {
+  void UntrackStorageKey(const blink::StorageKey& storage_key) {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
     storage_keys_.erase(storage_key);
   }
@@ -544,8 +544,6 @@ void StorageHandler::OverrideQuotaForOrigin(
                      std::move(callback)));
 }
 
-// TODO(https://crbug.com/1199077): We should think about how this function
-// should be exposed when migrating to storage keys.
 Response StorageHandler::TrackCacheStorageForOrigin(
     const std::string& origin_string) {
   if (!storage_partition_)
@@ -574,8 +572,6 @@ Response StorageHandler::TrackCacheStorageForStorageKey(
   return Response::Success();
 }
 
-// TODO(https://crbug.com/1199077): We should think about how this function
-// should be exposed when migrating to storage keys.
 Response StorageHandler::UntrackCacheStorageForOrigin(
     const std::string& origin_string) {
   if (!storage_partition_)
@@ -614,9 +610,7 @@ Response StorageHandler::TrackIndexedDBForOrigin(
   if (!origin_url.is_valid() || origin.opaque())
     return Response::InvalidParams(origin_string + " is not a valid URL");
 
-  // TODO(https://crbug.com/1199077): Pass the real StorageKey into this
-  // function once the Chrome DevTools Protocol (CDP) supports StorageKey.
-  GetIndexedDBObserver()->TrackOrigin(blink::StorageKey(origin));
+  GetIndexedDBObserver()->TrackStorageKey(blink::StorageKey(origin));
   return Response::Success();
 }
 
@@ -630,7 +624,7 @@ Response StorageHandler::TrackIndexedDBForStorageKey(
   if (!key)
     return Response::InvalidParams("Unable to deserialize storage key");
 
-  GetIndexedDBObserver()->TrackOrigin(*key);
+  GetIndexedDBObserver()->TrackStorageKey(*key);
   return Response::Success();
 }
 
@@ -644,9 +638,7 @@ Response StorageHandler::UntrackIndexedDBForOrigin(
   if (!origin_url.is_valid() || origin.opaque())
     return Response::InvalidParams(origin_string + " is not a valid URL");
 
-  // TODO(https://crbug.com/1199077): Pass the real StorageKey into this
-  // function once the Chrome DevTools Protocol (CDP) supports StorageKey.
-  GetIndexedDBObserver()->UntrackOrigin(blink::StorageKey(origin));
+  GetIndexedDBObserver()->UntrackStorageKey(blink::StorageKey(origin));
   return Response::Success();
 }
 
@@ -660,7 +652,7 @@ Response StorageHandler::UntrackIndexedDBForStorageKey(
   if (!key)
     return Response::InvalidParams("Unable to deserialize storage key");
 
-  GetIndexedDBObserver()->UntrackOrigin(*key);
+  GetIndexedDBObserver()->UntrackStorageKey(*key);
   return Response::Success();
 }
 
