516fa5280761058c94cc1a696f3b04c6a75c6b54
Arthur Milchior <arthurmilchior@chromium.org>
2023-01-19 15:07:29 +0000

    [iOS] Remove TableViewPresentationController
    It was not instantiated anymore
    
    
    Bug: 1110609
    Change-Id: I5c180f2dcb9d87e4437a1407c2c3f308fd98bdb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177066
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094464}

--- ios/chrome/browser/ui/history/history_clear_browsing_data_coordinator.mm
+++ ios/chrome/browser/ui/history/history_clear_browsing_data_coordinator.mm
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_ui_delegate.h"
 #import "ios/chrome/browser/ui/table_view/table_view_navigation_controller.h"
-#import "ios/chrome/browser/ui/table_view/table_view_presentation_controller.h"
-#import "ios/chrome/browser/ui/table_view/table_view_presentation_controller_delegate.h"
 #import "ios/chrome/browser/url_loading/url_loading_browser_agent.h"
 #import "ios/chrome/browser/url_loading/url_loading_params.h"
 #import "ios/web/public/navigation/referrer.h"
