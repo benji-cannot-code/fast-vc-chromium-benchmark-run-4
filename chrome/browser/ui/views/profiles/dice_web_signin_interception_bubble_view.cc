1caaea50f5889fd68dc8f4ab6dca879938c5bb9f
Gabriel Oliveira <gabolvr@google.com>
2023-01-19 12:40:00 +0000

    [Signin] Add managed disclaimer in Signin Intercept bubble on Sync Promo
    This CL adds an enterprise disclaimer for managed profile in the Signin
    Intercept bubble V1 when kSyncPromoAfterSigninIntercept feature is
    enabled.
    
    Preview: https://crbug.com/1282157#c90
    
    Bug: 1282157
    Change-Id: I5bbd1fabbe7d343cbe9a9da29a454ec0ca906cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173655
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094413}

--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 constexpr int kInterceptionBubbleWithoutGuestHeight = 326;
 constexpr int kInterceptionBubbleGuestFooterHeight = 36;
-constexpr int kInterceptionBubbleManagedDisclaimerHeight = 52;
+constexpr int kInterceptionBubbleManagedDisclaimerHeight = 84;
 constexpr int kInterceptionBubbleExtraTextHeight = 30;
 constexpr int kInterceptionBubbleWidth = 290;
 
