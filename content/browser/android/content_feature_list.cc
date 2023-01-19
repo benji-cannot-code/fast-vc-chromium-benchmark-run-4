45349926a194ec6b7aaace7413b242493f4a1ab9
Patrick Noland <pnoland@google.com>
2023-01-19 18:40:49 +0000

    Add extra check before hiding keyboard in ImeAdapterImpl
    We currently only check if InputMethodManager is active for the view,
    which is true most of the time, causing us to spam requests to hide the
    keyboard. These requests are expensive-ish and happen relatively often
    in the course of processing input events so contribute to jank. We can
    reduce the incidence by checking that IMM is accepting text, indicating
    the presence of a keyboard. We don't want to use
    KeyboardVisibilityDelegate here because it ignores floating keyboards,
    which I think we want to continue to dismiss to maintain current
    behavior.
    
    Bug: 1406932
    Change-Id: I5286b0b1767c5faa611cfbcf4c88697b9cd2bab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161649
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094578}

--- content/browser/android/content_feature_list.cc
+++ content/browser/android/content_feature_list.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/feature_list.h"
 #include "base/notreached.h"
+#include "content/common/features.h"
 #include "content/public/android/content_jni_headers/ContentFeatureListImpl_jni.h"
 #include "content/public/common/content_features.h"
 #include "third_party/blink/public/common/features.h"
@@ -32,6 +33,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kComputeAXMode,
     &features::kFedCm,
     &features::kOnDemandAccessibilityEvents,
+    &kOptimizeImmHideCalls,
     &features::kProcessSharingWithStrictSiteInstances,
     &features::kReduceGpuPriorityOnBackground,
     &features::kRequestDesktopSiteAdditions,
