17de23588d2cd1f8916efb288766921c5228858c
Daniel White <danieltwhite@google.com>
2023-01-19 17:31:36 +0000

    Implement Price Tracking IPH Trigger Policy
    In a previous CL, the mechanism for displaying Price Tracking IPHs was
    implemented. However, the policy by which the mechanism used to decide
    whether to display an IPH was arbitrarily defined. In this CL, the
    policy was defined to display the IPH if the user is on page with a
    trackable product once per session for up to three sessions or until the
    price tracking destination was clicked. Furthermore, the policy dictates
    that between each IPH display, a minimum of one week needs to have
    passed before another IPH can be displayed.
    
    Bug: 1382913
    Change-Id: Id24296406cddec91ba863e6a8964bb7bab92a666
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166602
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094534}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -1802,6 +1802,8 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
 // Dismisses the menu and opens price notifications list.
 - (void)openPriceNotifications {
   RecordAction(UserMetricsAction("MobileMenuPriceNotifications"));
+  _engagementTracker->NotifyEvent(
+      feature_engagement::events::kPriceNotificationsUsed);
   [self.popupMenuCommandsHandler dismissPopupMenuAnimated:YES];
   [self.dispatcher showPriceNotifications];
 }
