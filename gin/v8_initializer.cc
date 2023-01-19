33a9f1027671744bb72ddf25dfe1d530ef71b3b9
David Bienvenu <davidbienvenu@chromium.org>
2023-01-19 23:43:37 +0000

    win8dep: Remove pre Win8.1 code from gin
    Win7/8 support is deprecated. Remove Win7/8 only code from gin.
    
    Also fix lint warnings.
    
    Bug: 1385856
    Change-Id: I6eab12eba44cde38dcff7df8164a5d3fbe55a8f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181361
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094767}

--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 #include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "base/allocator/partition_allocator/page_allocator.h"
 #include "base/allocator/partition_allocator/partition_address_space.h"
@@ -41,10 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/v8_context_snapshot/buildflags.h"
 #include "v8/include/v8-initialization.h"
 #include "v8/include/v8-snapshot.h"
-
-#if BUILDFLAG(IS_WIN)
-#include "base/win/windows_version.h"
-#endif
 
 #if defined(V8_USE_EXTERNAL_STARTUP_DATA)
 #if BUILDFLAG(IS_ANDROID)
@@ -455,22 +454,6 @@ void V8Initializer::Initialize(IsolateHolder::ScriptMode mode,
   const size_t min_pool_size = partition_alloc::internal::
       PartitionAddressSpace::ConfigurablePoolMinSize();
   size_t pool_size = max_pool_size;
-#if BUILDFLAG(IS_WIN)
-  // On Windows prior to 8.1 we allocate a smaller Pool since reserving
-  // virtual memory is expensive on these OSes.
-  if (base::win::GetVersion() < base::win::Version::WIN8_1) {
-    // The size chosen here should be synchronized with the size of the
-    // virtual memory reservation for the V8 sandbox on these platforms.
-    // Currently, that is 8GB, of which 4GB are used for V8's pointer
-    // compression region.
-    // TODO(saelo) give this constant a proper name and maybe move it
-    // somewhere else.
-    constexpr size_t kGB = 1ULL << 30;
-    pool_size = 4ULL * kGB;
-    DCHECK_LE(pool_size, max_pool_size);
-    DCHECK_GE(pool_size, min_pool_size);
-  }
-#endif
   // Try to reserve the maximum size of the pool at first, then keep halving
   // the size on failure until it succeeds.
   uintptr_t pool_base = 0;
