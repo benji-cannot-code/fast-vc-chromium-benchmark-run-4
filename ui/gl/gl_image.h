c277e94e7a72881f9b500dee536b7af7c424a01a
Colin Blundell <blundell@chromium.org>
2023-01-19 15:26:41 +0000

    [gl] Trim down friends in gl_image.h
    Removes friending in gl_image.h that has become unnecessary following
    recent refactorings.
    
    Bug: 1310018
    Change-Id: I81c7f1fd7f52f972b618cdd64fe16855d4fee3c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177166
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094470}

--- ui/gl/gl_image.h
+++ ui/gl/gl_image.h
@@ -31,7 +31,6 @@ class ProcessMemoryDump;
 }  // namespace base
 
 namespace gpu {
-class DawnEGLImageRepresentation;
 class D3DImageBacking;
 class D3DImageBackingFactoryTest;
 class GLTextureImageBacking;
@@ -43,13 +42,12 @@ FORWARD_DECLARE_TEST(GpuOESEGLImageTest, EGLImageToTexture);
 }  // namespace gpu
 
 namespace gpu::gles2 {
-class BackTexture;
 class GLES2DecoderImpl;
 class GLES2DecoderPassthroughImpl;
 class PassthroughAbstractTextureImpl;
 class Texture;
 class ValidatingAbstractTextureImpl;
-}
+}  // namespace gpu::gles2
 
 namespace media {
 class GLImageEGLStream;
@@ -64,8 +62,6 @@ class VTVideoDecodeAccelerator;
 
 namespace ui {
 class NativePixmapGLBinding;
-class NativePixmapEGLBinding;
-class NativePixmapEGLX11Binding;
 class SurfacelessGlRenderer;
 class SurfacelessSkiaGlRenderer;
 }  // namespace ui
@@ -132,12 +128,10 @@ class GL_EXPORT GLImage : public base::RefCounted<GLImage> {
   static media::GLImageEGLStream* ToGLImageEGLStream(GLImage* image);
   static media::GLImagePbuffer* ToGLImagePbuffer(GLImage* image);
 
-  friend class gpu::DawnEGLImageRepresentation;
   friend class gpu::D3DImageBacking;
   friend class gpu::D3DImageBackingFactoryTest;
   friend class gpu::GLTextureImageBacking;
   friend class gpu::GLTexturePassthroughD3DImageRepresentation;
-  friend class gpu::gles2::BackTexture;
   friend class gpu::gles2::GLES2DecoderImpl;
   friend class gpu::gles2::GLES2DecoderPassthroughImpl;
   friend class gpu::gles2::PassthroughAbstractTextureImpl;
@@ -150,8 +144,6 @@ class GL_EXPORT GLImage : public base::RefCounted<GLImage> {
   friend class media::V4L2SliceVideoDecodeAccelerator;
   friend class media::VTVideoDecodeAccelerator;
   friend class ui::NativePixmapGLBinding;
-  friend class ui::NativePixmapEGLBinding;
-  friend class ui::NativePixmapEGLX11Binding;
   friend class ui::SurfacelessGlRenderer;
   friend class ui::SurfacelessSkiaGlRenderer;
   friend class viz::ImageContextImpl;
