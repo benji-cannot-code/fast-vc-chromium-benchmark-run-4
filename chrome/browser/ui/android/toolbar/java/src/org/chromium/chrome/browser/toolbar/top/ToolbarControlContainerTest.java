4ab6bc8f098408ade114b98a9da16c04864d3956
Patrick Noland <pnoland@google.com>
2023-01-19 17:02:23 +0000

    Optimize readiness check in ToolbarControlContainer
    We can:
    * Check visibility before calling isReadyForTextureCapture
    * Clear isDirty when snapshot is the same
    
    Local tracing says this is about half as expensive but the measurement
    is a bit noisy.
    
    Bug: 1063346
    
    Change-Id: I21e5ff330b308e600e021ef13ff12bf8096e1c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4175875
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094508}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarControlContainerTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarControlContainerTest.java
@@ -352,4 +352,18 @@ public class ToolbarControlContainerTest {
 
         Assert.assertTrue(adapter.isDirty());
     }
+
+    @Test
+    public void testInMotion_viewNotVisible() {
+        ToolbarViewResourceAdapter adapter =
+                new ToolbarViewResourceAdapter(mToolbarContainer, false);
+        initAdapter(adapter);
+        Mockito.doReturn(CaptureReadinessResult.readyWithSnapshotDifference(
+                                 ToolbarSnapshotDifference.URL_TEXT))
+                .when(mToolbar)
+                .isReadyForTextureCapture();
+        mIsVisible = false;
+
+        changeInMotion(true, false);
+    }
 }
