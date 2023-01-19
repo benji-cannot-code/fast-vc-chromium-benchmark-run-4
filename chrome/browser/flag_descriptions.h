096b187753cc6b0c7aee001015225199daa070b7
Garrick Evans <garrick@google.com>
2023-01-19 05:43:08 +0000

    Add flag to disable dnsproxy on CrOS
    This flag is different from the "enable" version that was used to
    control and test the rollout of the feature. It will have a different
    underlying implementation in the service.
    
    BUG=b:265609123
    TEST=unit_tests --gtest_filter=AboutFlagsHistogramTest.CheckHistograms
    TEST=unit_tests --gtest_filter=AboutFlagsTest.*
    
    Change-Id: I4cab06340a254001f512124b7763903d542215bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167110
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Garrick Evans <garrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094298}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2930,6 +2930,9 @@ extern const char kEnhancedClipboardNudgeSessionResetDescription[];
 extern const char kEnableCrOSActionRecorderName[];
 extern const char kEnableCrOSActionRecorderDescription[];
 
+extern const char kDisableDnsProxyName[];
+extern const char kDisableDnsProxyDescription[];
+
 extern const char kEnableDnsProxyName[];
 extern const char kEnableDnsProxyDescription[];
 
