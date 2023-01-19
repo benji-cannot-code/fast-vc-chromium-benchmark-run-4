659e4f270e04b413fea80d54b6f42d6dfb146aca
Gordon Seto <gordonseto@google.com>
2023-01-19 00:17:55 +0000

    [CrOS Cellular] Fix ApnMigrator unit test failure
    Forward fix for ApnMigratorTest unit tests failing on linux-cfm-rel:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-cfm-rel/36974/overview.
    
    This was caused by https://chromium-review.googlesource.com/c/chromium/src/+/4143863.
    
    This is because the number of times ContainsApnMigratedIccid() will be
    called depends on whether DCHECKs are enabled or not, which depends on
    the builder. Rather than having the unit test handle the different
    environments, this CL removes the assertion for the specific number of
    method invocations. This will also make future changes easier as test
    writers don't have to keep track of what the count should be for
    different environments. The number of times the method is called isn't
    critical to the coverage of the test.
    
    Change-Id: Idaf6549bfa7b568d891ca0bc09384869ee7b574a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177140
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094171}

--- chrome/browser/ash/net/apn_migrator_unittest.cc
+++ chrome/browser/ash/net/apn_migrator_unittest.cc
@@ -315,11 +315,9 @@ TEST_F(ApnMigratorTest, ApnRevampFlagEnabled_MigrateNetworksWithoutCustomApns) {
   // migrated.
   EXPECT_CALL(*managed_cellular_pref_handler(),
               ContainsApnMigratedIccid(Eq(kTestCellularIccid1)))
-      .Times(2)
       .WillRepeatedly(Return(false));
   EXPECT_CALL(*managed_cellular_pref_handler(),
               ContainsApnMigratedIccid(Eq(kTestCellularIccid2)))
-      .Times(2)
       .WillRepeatedly(Return(false));
 
   // Simulate that all networks do not have custom APNs
@@ -381,7 +379,6 @@ TEST_F(ApnMigratorTest, ApnRevampFlagEnabled_MigrateNetwork) {
   {
     EXPECT_CALL(*managed_cellular_pref_handler(),
                 ContainsApnMigratedIccid(Eq(kTestCellularIccid1)))
-        .Times(2)
         .WillRepeatedly(Return(false));
 
     base::Value::Dict custom_apn_1;
