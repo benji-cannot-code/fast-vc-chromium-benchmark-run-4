bfeed7a81a7123bf52aaaaafb09057797a2c4ef8
Lingqi Chi <lingqi@chromium.org>
2023-01-19 02:55:24 +0000

    SearchPrefetch: remove unused method SearchPrefetchURLLoaderInterceptor::MaybeInterceptNavigation
    SearchPrefetchURLLoaderInterceptor::MaybeInterceptNavigation is
    declared but no longer used.
    This CL removes it.
    
    Bug: N/A
    Change-Id: I8d95b85d8139156571d91d6415470f736450ab14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4172685
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094248}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/callback.h"
-#include "chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_factory.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader.h"
