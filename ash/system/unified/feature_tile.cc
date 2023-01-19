3d0ceef91e25e0c1056fc05dbe86ddaff3d5e376
Kevin Radtke <kradtke@chromium.org>
2023-01-19 21:17:20 +0000

    Fix feature tile tooltips
    Make title and label container and in some cases the drill_in_button_
    not process subevents within the subtree in order to let the parent
    feature tile view handle the hover event so its tooltip can be shown.
    
    b: 263306250
    Change-Id: I1c8907c30f2b52737eb457ec7d210d7adc503561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179207
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094655}

--- ash/system/unified/feature_tile.cc
+++ ash/system/unified/feature_tile.cc
@@ -98,6 +98,7 @@ void FeatureTile::CreateChildViews() {
   SetPreferredSize(is_compact ? kCompactSize : kDefaultSize);
 
   auto* icon_container = AddChildView(std::make_unique<FlexLayoutView>());
+  icon_container->SetCanProcessEventsWithinSubtree(false);
   icon_container->SetMainAxisAlignment(views::LayoutAlignment::kCenter);
   icon_container->SetCrossAxisAlignment(is_compact
                                             ? views::LayoutAlignment::kEnd
@@ -109,6 +110,7 @@ void FeatureTile::CreateChildViews() {
   icon_ = icon_container->AddChildView(std::make_unique<views::ImageView>());
 
   auto* title_container = AddChildView(std::make_unique<FlexLayoutView>());
+  title_container->SetCanProcessEventsWithinSubtree(false);
   title_container->SetOrientation(is_compact
                                       ? views::LayoutOrientation::kHorizontal
                                       : views::LayoutOrientation::kVertical);
@@ -117,8 +119,7 @@ void FeatureTile::CreateChildViews() {
   title_container->SetPreferredSize(is_compact ? kCompactTitleContainerSize
                                                : kTitlesContainerSize);
 
-  label_ = AddChildView(std::make_unique<views::Label>());
-  title_container->AddChildView(label_);
+  label_ = title_container->AddChildView(std::make_unique<views::Label>());
 
   if (is_compact) {
     label_->SetPreferredSize(kCompactTitleLabelSize);
@@ -130,12 +131,12 @@ void FeatureTile::CreateChildViews() {
     label_->SetFontList(views::Label::GetDefaultFontList().Derive(
         -1, gfx::Font::FontStyle::NORMAL, gfx::Font::Weight::NORMAL));
   } else {
-    sub_label_ = AddChildView(std::make_unique<views::Label>());
+    sub_label_ =
+        title_container->AddChildView(std::make_unique<views::Label>());
     // TODO(b/252873172): update FontList.
     sub_label_->SetFontList(views::Label::GetDefaultFontList().Derive(
         -1, gfx::Font::FontStyle::NORMAL, gfx::Font::Weight::NORMAL));
     sub_label_->SetLineHeight(kPrimarySubtitleLineHeight);
-    title_container->AddChildView(sub_label_);
   }
 }
 
@@ -161,9 +162,12 @@ void FeatureTile::CreateDrillInButton(base::RepeatingCallback<void()> callback,
   // `drill_in_button_` handle the button events.
   drill_in_arrow->SetCanProcessEventsWithinSubtree(false);
 
-  // Only buttons with Toggle + Drill-in behavior can focus the drill-in arrow.
-  if (!is_togglable_)
+  // Only buttons with Toggle + Drill-in behavior can focus the drill-in arrow
+  // and process drill-in button events.
+  if (!is_togglable_) {
+    drill_in_button->SetCanProcessEventsWithinSubtree(false);
     drill_in_arrow->SetFocusBehavior(FocusBehavior::NEVER);
+  }
 
   drill_in_button_ = AddChildView(std::move(drill_in_button));
   drill_in_button_->AddChildView(std::move(drill_in_arrow));
