f9c29b051e79e8c83a1a1ba7286667d4947c71a7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-01-19 05:11:00 +0000

    Import wpt@ad7fcb93363494d53af41c5ef25230e07c5b1d42
    Using wpt-import in Chromium bea842e08a9f7116b6ce0a24131430d55f2cd41d.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    andruud@chromium.org:
      external/wpt/css/css-properties-values-api
    asully@chromium.org, mek@chromium.org:
      external/wpt/fs
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    eae@chromium.org, szager@chromium.org:
      external/wpt/intersection-observer
    igrigorik@chromium.org, yoavweiss@chromium.org:
      external/wpt/preload
    japhet@chromium.org:
      external/wpt/fetch
    jsbell@chromium.org, mek@chromium.org:
      external/wpt/FileAPI
    ksakamoto@chromium.org, npm@chromium.org, yoavweiss@chromium.org:
      external/wpt/resource-timing
    rego@igalia.com:
      external/wpt/css/selectors
    yosin@chromium.org, pcupp@microsoft.com:
      external/wpt/editing
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I7fe283baa725173779d174283eb28ece14fe97fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178978
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1094291}

--- third_party/blink/web_tests/external/wpt/xhr/xhr-authorization-redirect.any.js
+++ third_party/blink/web_tests/external/wpt/xhr/xhr-authorization-redirect.any.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: global=window,worker
+// META: global=window,sharedworker,dedicatedworker
 // META: script=/common/get-host-info.sub.js
 
 const authorizationValue = "Basic " + btoa("user:pass");
