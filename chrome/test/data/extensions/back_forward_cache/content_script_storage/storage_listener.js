5d3edb96e69b3353bc7eaecd3ee6ca210cabfd9a
Fergal Daly <fergal@chromium.org>
2023-01-19 06:17:36 +0000

    Fix flaky ExtensionBackForwardCacheBrowserTest.StorageCallbackEvicts.
    This test was failing with `document.getElementById('callback')` being null.
    
    This implies that event handler that injects the callback element did
    not run before checking for the callback.
    
    The fix is to add an explicit signal for when it ran and wait for that
    before looking for the callback.
    
    To avoid sending domAutomationController from both pages, split the
    content script into 2 parts that run on different pages, one to
    install the storage listener and one to change storage.
    
    Fixed: 1405748,1382285
    Change-Id: I5603d6d1bc5f052174aea0172ce8f47993fa5df0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4170015
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Yuzu Saijo <yuzus@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094307}

--- chrome/test/data/extensions/back_forward_cache/content_script_storage/storage_listener.js
+++ chrome/test/data/extensions/back_forward_cache/content_script_storage/storage_listener.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2021 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,9 +8,6 @@ chrome.storage.onChanged.addListener((changes, area) => {
   input.id = "callback"
   input.value += "called";
   document.documentElement.appendChild(input);
+  // Let the test know that we ran.
+  window.domAutomationController.send("event handler ran");
 });
-
-if (window.location.host.indexOf('b.com') != -1) {
-  var options = {test: true};
-  chrome.storage.sync.set({options});
-}
