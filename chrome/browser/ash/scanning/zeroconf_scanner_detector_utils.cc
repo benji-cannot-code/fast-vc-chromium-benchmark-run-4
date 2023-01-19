d659e72f120fda1ecb824043e02f4fded478612f
Paul Moy <pmoy@chromium.org>
2023-01-19 15:57:17 +0000

    scanning: check for // in eSCL URL
    Some scanners report rs=/eSCL instead of rs=eSCL. This leads to a URL
    with //, which is unsupported. Trim leading slashes when constructing
    scanner URLs.
    
    Bug: b:265480543
    Change-Id: I59f793821aa756c71270a376bb59dc445d36b2b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174139
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094477}

--- chrome/browser/ash/scanning/zeroconf_scanner_detector_utils.cc
+++ chrome/browser/ash/scanning/zeroconf_scanner_detector_utils.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/logging.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/ash/scanning/zeroconf_scanner_detector.h"
@@ -45,10 +46,16 @@ std::string CreateDeviceName(const std::string& name,
                              int port,
                              const std::string& backend_prefix) {
   std::string path;
-  if (rs == "none")
+  if (rs == "none") {
     path = "eSCL/";
-  else if (!rs.empty())
-    path = rs + "/";
+  } else if (!rs.empty()) {
+    // Some scanners report rs=/eSCL instead of rs=eSCL. This causes problems
+    // without our URL construction, so trim any leading slashes here. We also
+    // trim trailing slashes in case any scanners report rs=eSCL/, since we
+    // append a slash after.
+    base::TrimString(rs, "/", &path);
+    path += "/";
+  }
 
   // Replace colons in the instance name to prevent them from breaking
   // sane-airscan's device name parsing logic.
@@ -85,14 +92,17 @@ absl::optional<Scanner> CreateSaneScanner(const std::string& name,
   // If the name contains "EPSON" and is a generic service type, use the
   // "epsonds:net" backend.
   if (service_type == ZeroconfScannerDetector::kGenericScannerServiceType &&
-      base::StartsWith(name, "EPSON"))
+      base::StartsWith(name, "EPSON")) {
     device_name = base::StringPrintf("%s:%s", "epsonds:net",
                                      ip_address.ToString().c_str());
-  else if (service_type != ZeroconfScannerDetector::kGenericScannerServiceType)
+  } else if (service_type !=
+             ZeroconfScannerDetector::kGenericScannerServiceType) {
     device_name =
         CreateDeviceName(name, scheme, rs, ip_address, port, "airscan:escl");
-  if (device_name.empty())
+  }
+  if (device_name.empty()) {
     return absl::nullopt;
+  }
 
   Scanner scanner;
   scanner.display_name = name;
