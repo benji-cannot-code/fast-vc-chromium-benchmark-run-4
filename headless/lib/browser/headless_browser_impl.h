221e8d24cdf5247fcfab4fcdf9353cceab36eda4
Andrey Kosyakov <caseq@chromium.org>
2023-01-19 20:15:47 +0000

    Remove HeadlessBrowserImpl::did_shutdown_
    We needed it to troubleshoot the bug that was elsewhere and is already fixed.
    
    Bug: 1342152
    Change-Id: Id844697838e9262d0ce8e3a5c28cba547fa965da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178573
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094635}

--- headless/lib/browser/headless_browser_impl.h
+++ headless/lib/browser/headless_browser_impl.h
@@ -122,8 +122,6 @@ class HEADLESS_EXPORT HeadlessBrowserImpl : public HeadlessBrowser,
   policy::PolicyService* GetPolicyService();
 #endif
 
-  bool did_shutdown() const { return did_shutdown_; }
-
  private:
 #if BUILDFLAG(IS_MAC)
   std::unique_ptr<display::ScopedNativeScreen> screen_;
@@ -138,7 +136,6 @@ class HEADLESS_EXPORT HeadlessBrowserImpl : public HeadlessBrowser,
       browser_contexts_;
   raw_ptr<HeadlessBrowserContext, DanglingUntriaged> default_browser_context_ =
       nullptr;
-  bool did_shutdown_ = false;  // TODO(1342152): remove once the bug is fixed.
   scoped_refptr<content::DevToolsAgentHost> agent_host_;
   std::unique_ptr<HeadlessRequestContextManager>
       system_request_context_manager_;
