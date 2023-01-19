7daf5912cdaeaef039f6ede8001d49ec5b594e63
Lei Zhang <thestig@chromium.org>
2023-01-19 17:32:28 +0000

    Add a "strict" build config in //printing/BUILD.gn.
    The strict config currently just enables -Wexit-time-destructors, just
    like in //pdf/BUILD.gn. The strict config is not applied to test code,
    where static initializers and exit-time destructors have traditionally
    been tolerated. Then fix code that triggers the new warning.
    
    Bug: 101600
    Change-Id: Ief6002062c5c49744792d2adff417843b4f3b7c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179674
    Reviewed-by: Sparik Hayrapetyan <ust@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094535}

--- printing/client_info_helpers.cc
+++ printing/client_info_helpers.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "printing/client_info_helpers.h"
 
+#include "base/no_destructor.h"
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "base/types/optional_util.h"
@@ -21,9 +22,9 @@ bool ValidateClientType(mojom::IppClientInfo::ClientType type) {
 }
 
 bool ValidateStringMember(const std::string* value, size_t max_length) {
-  static const RE2 kStringRegex = "[a-zA-Z0-9_.-]*";
+  static const base::NoDestructor<RE2> kStringRegex("[a-zA-Z0-9_.-]*");
   return value == nullptr ||
-         (value->size() <= max_length && RE2::FullMatch(*value, kStringRegex));
+         (value->size() <= max_length && RE2::FullMatch(*value, *kStringRegex));
 }
 
 // Returns true if all members of `client_info` are valid.
