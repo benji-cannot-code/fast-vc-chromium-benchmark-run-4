f9ff6bf6778792aac5da821478c37c6bf2e14cd6
Chris Fredrickson <cfredric@chromium.org>
2023-01-19 19:55:16 +0000

    Add tests for per-frame semantics for Storage Access API
    Specifically, these tests check that:
    * A frame can use a permission grant only after it has called
      document.requestStorageAccess().
    * Self-initiated same-origin navigations preserve existing storage
      access.
    
    Bug: 1401089
    Change-Id: I064d63c7213054e50b224e0b770f8208a3d88ac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117243
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Johann Hofmann <johannhof@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094621}

---
+++ third_party/blink/web_tests/external/wpt/storage-access-api/resources/embedded_responder.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+"use strict";
+
+test_driver.set_test_context(window.top);
+
+window.addEventListener("message", async (event) => {
+  function reply(data) {
+    event.source.postMessage(data, event.origin);
+  }
+
+  switch (event.data["command"]) {
+    case "hasStorageAccess":
+      reply(await document.hasStorageAccess());
+      break;
+    case "requestStorageAccess": {
+      const obtainedAccess = await document.requestStorageAccess()
+        .then(() => true, () => false);
+      reply(obtainedAccess);
+    }
+      break;
+    case "document.cookie":
+      reply(document.cookie);
+      break;
+    case "set_permission":
+      await test_driver.set_permission(...event.data.args);
+      reply(undefined);
+      break;
+    case "reload":
+      window.location.reload();
+      break;
+    default:
+  }
+});
---
+++ third_party/blink/web_tests/external/wpt/storage-access-api/resources/script-with-cookie-header.py
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+  script = request.GET.first(b"script")
+  cookie_header = request.headers.get(b"Cookie", b"")
+
+  body = b"""
+  <!DOCTYPE html>
+  <meta charset="utf-8">
+  <title>Subframe with HTTP Cookies</title>
+  <script src="/resources/testharness.js"></script>
+  <script src="/resources/testdriver.js"></script>
+  <script src="/resources/testdriver-vendor.js"></script>
+  <script>
+    var httpCookies = "%s";
+  </script>
+
+  <script src="%s"></script>
+  """ % (cookie_header, script)
+
+  return (200, [], body)
