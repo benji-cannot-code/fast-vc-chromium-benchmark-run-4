7156529f3b0deea26ba4dba803ded827efd935b5
Wes Okuhara <wesokuhara@google.com>
2023-01-19 19:06:04 +0000

    CrOS Settings: Use TS version of various js helpers
    Replace JS version of various helper modules with TS version for
    improved typechecking in TS files.
    
    Bug: b:263413628
    Test: browser_tests --gtest_filter='*OSSettings*'
    Change-Id: Ic36471b9355179fa9d8649f8d653e1dcddee4595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178265
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094589}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/dom_switch.ts
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/dom_switch.ts
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // TODO(crbug.com/992795) Merge with cr-view-manager.
-import {PromiseResolver} from 'chrome://resources/ash/common/promise_resolver.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
+import {PromiseResolver} from 'chrome://resources/js/promise_resolver.js';
 import {PolymerElement, TemplateInstanceBase, templatize} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './dom_switch.html.js';
--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/fake_page_handler.ts
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/fake_page_handler.ts
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {PromiseResolver} from 'chrome://resources/ash/common/promise_resolver.js';
 import {App, AppType, ExtensionAppPermissionMessage, OptionalBool, PageHandlerInterface, PageHandlerReceiver, PageHandlerRemote, PageRemote, Permission, PermissionType, RunOnOsLoginMode, TriState, WindowMode} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {InstallReason, InstallSource} from 'chrome://resources/cr_components/app_management/constants.js';
 import {createBoolPermission, createTriStatePermission, getTriStatePermissionValue} from 'chrome://resources/cr_components/app_management/permission_util.js';
 import {assert, assertNotReached} from 'chrome://resources/js/assert_ts.js';
+import {PromiseResolver} from 'chrome://resources/js/promise_resolver.js';
 
 import {PermissionOption} from './browser_proxy.js';
 import {AppManagementStore} from './store.js';
