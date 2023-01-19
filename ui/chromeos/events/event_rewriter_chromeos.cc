db29394f4fa6c9d52a284ab8e7ec04702abf3886
David Padlipsky <dpad@google.com>
2023-01-19 06:34:43 +0000

    Rewriter: Fix clang-tidy errors
    - Removes unused include
    - Use range-based for loop
    - Replace push_back(Object(...)) with emplace_back(...)
    
    Change-Id: I165abbe1e787272277b4fea95a2db81a3b7301ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166814
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094310}

--- ui/chromeos/events/event_rewriter_chromeos.cc
+++ ui/chromeos/events/event_rewriter_chromeos.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
-#include "base/system/sys_info.h"
 #include "device/udev_linux/scoped_udev.h"
 #include "ui/base/ime/ash/ime_keyboard.h"
 #include "ui/base/ime/ash/input_method_manager.h"
@@ -544,12 +543,12 @@ KeyboardCapability::DeviceType IdentifyKeyboardType(
   // Parse |device_name| to help classify it.
   bool found_apple = false;
   bool found_keyboard = false;
-  for (size_t i = 0; i < tokens.size(); ++i) {
-    if (!found_apple && base::EqualsCaseInsensitiveASCII(tokens[i], "apple")) {
+  for (const auto& token : tokens) {
+    if (!found_apple && base::EqualsCaseInsensitiveASCII(token, "apple")) {
       found_apple = true;
     }
     if (!found_keyboard &&
-        base::EqualsCaseInsensitiveASCII(tokens[i], "keyboard")) {
+        base::EqualsCaseInsensitiveASCII(token, "keyboard")) {
       found_keyboard = true;
     }
   }
@@ -1880,8 +1879,7 @@ EventDispatchDetails EventRewriterChromeOS::RewriteKeyEventInContext(
     // When a key is pressed, store |current_key_state| if it is not stored
     // before.
     if (iter == pressed_key_states_.end()) {
-      pressed_key_states_.push_back(
-          std::make_pair(current_key_state, original_key_state));
+      pressed_key_states_.emplace_back(current_key_state, original_key_state);
     }
 
     if (status == EventRewriteStatus::EVENT_REWRITE_CONTINUE) {
