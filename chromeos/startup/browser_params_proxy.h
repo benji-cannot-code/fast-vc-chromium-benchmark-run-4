e28fef7b758b5c8abe4eaf3fed4d63389611e7ae
Jenny Zhang <jennyz@chromium.org>
2023-01-19 21:26:31 +0000

    Publish apps running in both ash and lacros in app service properly.
    Currently the chrome apps or extensions running in both ash
    and lacros are not published in app service, therefore, they
    do NOT show up in launcher. This cl intends to fix the issue.
    
    For an app or extension running in both ash and lacros, it can be
    publish by either ash or lacros, but not both. This cl implements
    the following:
    1. The app service blocklists are used to configure whether an app or extension should blocked (NOT be published) in app service from ash
    or lacros.
    2. The app service blocklists are kept in ash and passed to lacros
    via crosapi::mojom::BrowserInitParams.
    3. The app or extension running in both ash and lacros will only
    be published in either ash or lacros according to the app service
    blocklist configuration.
    4. gnubbyd app runs in both ash and lacros, should be published
    in Lacros only.
    
    Low-Coverage-Reason=I tested the cl manually with a chrome app and with a hacked automated test (where I hard coded the testing app id
    in ash). The app is published and launched fine. This should fix the
    gnubbyd app launching issue but it needs to be tested by QA
    (gnubbydapp can't run on testing image). For the automated test, we
    need to modify the ash keep list and blocklist in ash before launching
    lacros browser test, which is not easy to do now. It might be easier to write such a test after crrev.com/c/4134109 lands.
    
    Bug: 1371250,1373665
    Change-Id: I0de33d3a9cdf80c4a99cf412ef329dbab53fdcac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167074
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094669}

--- chromeos/startup/browser_params_proxy.h
+++ chromeos/startup/browser_params_proxy.h
@@ -130,6 +130,9 @@ class COMPONENT_EXPORT(CHROMEOS_STARTUP) BrowserParamsProxy {
 
   bool VcControlsUiEnabled() const;
 
+  const crosapi::mojom::StandaloneBrowserAppServiceBlockListPtr&
+  StandaloneBrowserAppServiceBlockList() const;
+
  private:
   friend base::NoDestructor<BrowserParamsProxy>;
 
