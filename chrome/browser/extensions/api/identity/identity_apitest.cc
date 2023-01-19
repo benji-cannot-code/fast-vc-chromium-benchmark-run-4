8a609a832f930675be188ac740ac94e2abb6ef80
Chris Fredrickson <cfredric@chromium.org>
2023-01-19 11:39:28 +0000

    Simplify callback usage with cookie_util helper
    The AdaptCookieAccessResultToBool helper was primarily to make callback
    composition easier. But callbacks already have a method for that
    (namely `.Then`), so we don't have to reimplement it here.
    
    This CL also simplifies some adjacent callback binding in a test.
    
    Change-Id: I810c29af3fe819219aa35bb2e3ee297ee5f3aa34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4175077
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094396}

--- chrome/browser/extensions/api/identity/identity_apitest.cc
+++ chrome/browser/extensions/api/identity/identity_apitest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
@@ -3650,17 +3651,14 @@ IN_PROC_BROWSER_TEST_P(ClearAllCachedAuthTokensFunctionTestWithPartitionParam,
       "test_name", "test_value", "test.com", "/", base::Time(), base::Time(),
       base::Time(), base::Time(), true, false,
       net::CookieSameSite::NO_RESTRICTION, net::COOKIE_PRIORITY_DEFAULT, false);
-  base::RunLoop set_cookie_loop;
+  base::test::TestFuture<bool> future;
   GetCookieManager()->SetCanonicalCookie(
       *test_cookie,
       net::cookie_util::SimulatedCookieSource(*test_cookie, url::kHttpsScheme),
       net::CookieOptions(),
-      net::cookie_util::AdaptCookieAccessResultToBool(
-          base::BindLambdaForTesting([&](bool include) {
-            set_cookie_loop.Quit();
-            EXPECT_TRUE(include);
-          })));
-  set_cookie_loop.Run();
+      base::BindOnce(net::cookie_util::IsCookieAccessResultInclude)
+          .Then(future.GetCallback()));
+  EXPECT_TRUE(future.Get());
 
   EXPECT_FALSE(GetCookies().empty());
   ASSERT_TRUE(RunClearAllCachedAuthTokensFunction());
