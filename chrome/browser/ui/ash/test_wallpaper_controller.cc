9f5f40ddbfa7763eb0d516b510844d9c77b697f2
Eric Sum <esum@google.com>
2023-01-19 19:12:01 +0000

    Remove SetOnlineWallpaperIfExists().
    This will be helpful in an upcoming refactor. It makes the
    final changes in WallpaperControllerImpl more manageable.
    
    Bug: b:259730933
    Change-Id: I413dfca4e2fb3edc00b8ae583b0ec0bf12e57e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179044
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094592}

--- chrome/browser/ui/ash/test_wallpaper_controller.cc
+++ chrome/browser/ui/ash/test_wallpaper_controller.cc
@@ -127,12 +127,6 @@ bool TestWallpaperController::GetDailyGooglePhotosWallpaperIdCache(
   return true;
 }
 
-void TestWallpaperController::SetOnlineWallpaperIfExists(
-    const ash::OnlineWallpaperParams& params,
-    SetWallpaperCallback callback) {
-  NOTIMPLEMENTED();
-}
-
 void TestWallpaperController::SetDefaultWallpaper(
     const AccountId& account_id,
     bool show_wallpaper,
