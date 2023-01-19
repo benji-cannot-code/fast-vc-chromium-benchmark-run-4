1b40ded1d667cc233e6bf3705f088e10b3125a1f
andrewxu <andrewxu@chromium.org>
2023-01-19 21:24:12 +0000

    A test helper to wait for notification popup animations
    This CL creates a test helper to wait for all the notification popup
    animations managed by the given message popup collection to complete.
    
    This CL should not bring any difference to product code.
    
    Bug: b/264934296
    Change-Id: I50df03b9b85eb121f51fe30d77e5c607627f77f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174764
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094666}

---
+++ ash/system/message_center/message_center_test_util.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/message_center/message_center_test_util.h"
+
+#include "ui/message_center/public/cpp/notification.h"
+
+namespace ash {
+
+std::unique_ptr<message_center::Notification> CreateSimpleNotification(
+    const std::string& id) {
+  return std::make_unique<message_center::Notification>(
+      message_center::NOTIFICATION_TYPE_SIMPLE, id, u"test_title",
+      u"test message", ui::ImageModel(), std::u16string() /* display_source */,
+      GURL(), message_center::NotifierId(),
+      message_center::RichNotificationData(),
+      new message_center::NotificationDelegate());
+}
+
+}  // namespace ash
---
+++ ash/system/message_center/message_center_test_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_MESSAGE_CENTER_MESSAGE_CENTER_TEST_UTIL_H_
+#define ASH_SYSTEM_MESSAGE_CENTER_MESSAGE_CENTER_TEST_UTIL_H_
+
+#include <memory>
+#include <string>
+
+namespace message_center {
+class Notification;
+}  // namespace message_center
+
+namespace ash {
+
+// Creates a simple notification with the given id.
+std::unique_ptr<message_center::Notification> CreateSimpleNotification(
+    const std::string& id);
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_MESSAGE_CENTER_MESSAGE_CENTER_TEST_UTIL_H_
