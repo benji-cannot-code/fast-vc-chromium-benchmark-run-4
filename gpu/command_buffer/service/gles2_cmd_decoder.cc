0ae7169ea00f080dd384d0b5b0c44346949ce932
Colin Blundell <blundell@chromium.org>
2023-01-19 15:28:39 +0000

    [GPU] Eliminate usage of gl::BufferFormatToDataType() in //gpu
    The two callsites here pass values that are guaranteed to be one of the
    following:
    
    - gfx::BufferFormat::RGBX_8888
    - gfx::BufferFormat::BGRX_8888
    - gfx::BufferFormat::RGBA_8888
    
    For all of these, the function returns GL_UNSIGNED_BYTE [1]. This CL
    replaces the function calls with the hardcoded value, paving the way to
    inlining this function in native_pixmap_gl_binding in a followup.
    
    Followup to comment here:
    https://chromium-review.googlesource.com/c/chromium/src/+/4165822/3..7/gpu/command_buffer/service/gles2_cmd_decoder.cc#b3271
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:ui/gl/buffer_format_utils.cc;l=58-62;drc=09769fa654c1c2d43005594163616ffbc51bc62f;bpv=1;bpt=1
    
    Bug: 1310018
    Change-Id: I6c5ae09b29b9eea2090b7e62a0a345160d728432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176909
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094472}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -99,7 +99,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/overlay_plane_data.h"
 #include "ui/gfx/overlay_priority_hint.h"
 #include "ui/gfx/video_types.h"
-#include "ui/gl/buffer_format_utils.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_enums.h"
@@ -3226,7 +3225,7 @@ bool BackTexture::AllocateNativeGpuMemoryBuffer(const gfx::Size& size,
   image_ = image;
   decoder_->texture_manager()->SetLevelInfo(
       texture_ref_.get(), Target(), 0, format, size.width(), size.height(), 1,
-      0, format, gl::BufferFormatToGLDataType(buffer_format), gfx::Rect(size));
+      0, format, GL_UNSIGNED_BYTE, gfx::Rect(size));
   decoder_->texture_manager()->SetLevelImage(texture_ref_.get(), Target(), 0,
                                              image_.get(), Texture::BOUND);
 
