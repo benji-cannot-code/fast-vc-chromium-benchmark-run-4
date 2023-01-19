f0944942a468aae8a84975c18183cb06749488a5
Sophie Chang <sophiechang@chromium.org>
2023-01-19 01:26:55 +0000

    Set more aggressive visit deduping to default as it is a positive change
    Bug: b/254360152
    Change-Id: I05b21452e348144c75dba6051a212bc28aad8c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177842
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094214}

--- components/history_clusters/core/clusterer_unittest.cc
+++ components/history_clusters/core/clusterer_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/task_environment.h"
 #include "components/history_clusters/core/clustering_test_utils.h"
+#include "components/history_clusters/core/config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -17,7 +18,12 @@ using ::testing::ElementsAre;
 
 class ClustererTest : public ::testing::Test {
  public:
-  void SetUp() override { clusterer_ = std::make_unique<Clusterer>(); }
+  void SetUp() override {
+    config_.use_host_for_visit_deduping = false;
+    SetConfigForTesting(config_);
+
+    clusterer_ = std::make_unique<Clusterer>();
+  }
 
   void TearDown() override { clusterer_.reset(); }
 
@@ -27,6 +33,7 @@ class ClustererTest : public ::testing::Test {
   }
 
  private:
+  Config config_;
   std::unique_ptr<Clusterer> clusterer_;
   base::test::TaskEnvironment task_environment_;
 };
