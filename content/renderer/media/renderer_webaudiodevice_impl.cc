d5842b5083374bf3f64ac2bcb87575c81cbfd6e9
Thomas Guilbert <tguilbert@chromium.org>
2023-01-19 21:16:20 +0000

    Remove |session_id_| from RendererWebAudioDeviceImpl
    This CL removes any mention of a session ID from
    RendererWebAudioDeviceImpl. It is only ever instantiated with an empty
    UnguessableToken and can be removed, since the
    NewAudioRenderingMixingStrategy experiment never shipped.
    
    Bug: 870836
    Change-Id: Ic152a86dfb9457ca12b99e348aca3425d2b6b9af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179738
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094654}

--- content/renderer/media/renderer_webaudiodevice_impl.cc
+++ content/renderer/media/renderer_webaudiodevice_impl.cc
@@ -93,10 +93,9 @@ int GetOutputBufferSize(const blink::WebAudioLatencyHint& latency_hint,
 
 media::AudioParameters GetOutputDeviceParameters(
     const blink::LocalFrameToken& frame_token,
-    const base::UnguessableToken& session_id,
     const std::string& device_id) {
   return AudioDeviceFactory::GetInstance()
-      ->GetOutputDeviceInfo(frame_token, {session_id, device_id})
+      ->GetOutputDeviceInfo(frame_token, {base::UnguessableToken(), device_id})
       .output_params();
 }
 
@@ -112,12 +111,11 @@ std::unique_ptr<RendererWebAudioDeviceImpl> RendererWebAudioDeviceImpl::Create(
     media::ChannelLayout layout,
     int number_of_output_channels,
     const blink::WebAudioLatencyHint& latency_hint,
-    media::AudioRendererSink::RenderCallback* callback,
-    const base::UnguessableToken& session_id) {
+    media::AudioRendererSink::RenderCallback* callback) {
   return std::unique_ptr<RendererWebAudioDeviceImpl>(
       new RendererWebAudioDeviceImpl(
           sink_descriptor, layout, number_of_output_channels, latency_hint,
-          callback, session_id, base::BindOnce(&GetOutputDeviceParameters),
+          callback, base::BindOnce(&GetOutputDeviceParameters),
           base::BindRepeating(&GetNullAudioSink)));
 }
 
@@ -127,13 +125,11 @@ RendererWebAudioDeviceImpl::RendererWebAudioDeviceImpl(
     int number_of_output_channels,
     const blink::WebAudioLatencyHint& latency_hint,
     media::AudioRendererSink::RenderCallback* callback,
-    const base::UnguessableToken& session_id,
     OutputDeviceParamsCallback device_params_cb,
     CreateSilentSinkCallback create_silent_sink_cb)
     : sink_descriptor_(sink_descriptor),
       latency_hint_(latency_hint),
       client_callback_(callback),
-      session_id_(session_id),
       frame_token_(sink_descriptor.Token()),
       create_silent_sink_cb_(std::move(create_silent_sink_cb)) {
   DCHECK(client_callback_);
@@ -151,7 +147,7 @@ RendererWebAudioDeviceImpl::RendererWebAudioDeviceImpl(
   }
 
   media::AudioParameters hardware_params(
-      std::move(device_params_cb).Run(frame_token_, session_id_, device_id));
+      std::move(device_params_cb).Run(frame_token_, device_id));
 
   // On systems without audio hardware the returned parameters may be invalid.
   // In which case just choose whatever we want for the fake device.
@@ -197,7 +193,7 @@ void RendererWebAudioDeviceImpl::Start() {
     case blink::WebAudioSinkDescriptor::kAudible:
       sink_ = AudioDeviceFactory::GetInstance()->NewAudioRendererSink(
           GetLatencyHintSourceType(latency_hint_.Category()), frame_token_,
-          media::AudioSinkParameters(session_id_,
+          media::AudioSinkParameters(base::UnguessableToken(),
                                      sink_descriptor_.SinkId().Utf8()));
 
       // Use a task runner instead of the render thread for fake Render() calls
@@ -297,14 +293,7 @@ RendererWebAudioDeviceImpl::GetSilentSinkTaskRunner() {
 }
 
 void RendererWebAudioDeviceImpl::SendLogMessage(const std::string& message) {
-  if (session_id_.is_empty()) {
-    blink::WebRtcLogMessage(
-        base::StringPrintf("[WA]RWADI::%s", message.c_str()));
-  } else {
-    blink::WebRtcLogMessage(base::StringPrintf("[WA]RWADI::%s [session_id=%s]",
-                                               message.c_str(),
-                                               session_id_.ToString().c_str()));
-  }
+  blink::WebRtcLogMessage(base::StringPrintf("[WA]RWADI::%s", message.c_str()));
 }
 
 }  // namespace content
