bb8e847b6e9970dda9dd6f27641004bcf6a91df1
Jack Yammine <jyammine@google.com>
2023-01-19 10:46:19 +0000

    [fyfre]Reverse button order on Windows
    Fixed: 1408389
    Change-Id: I96f5d0b6c5e21c2b76dc7f24b14a0de53f4467de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177330
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094381}

--- chrome/browser/resources/intro/sign_in_promo.ts
+++ chrome/browser/resources/intro/sign_in_promo.ts
@@ -21,7 +21,7 @@ import {getTemplate} from './sign_in_promo.html.js';
 export interface SignInPromoElement {
   $: {
     acceptSignInButton: CrButtonElement,
-    buttonContainer: HTMLElement,
+    buttonRow: HTMLElement,
     contentArea: HTMLElement,
     declineSignInButton: CrButtonElement,
     safeZone: HTMLElement,
@@ -113,7 +113,7 @@ export class SignInPromoElement extends SignInPromoElementBase {
     const safeZone = this.$.safeZone;
 
     this.divisionLineResizeObserver_ = new ResizeObserver(() => {
-      this.$.buttonContainer.classList.toggle(
+      this.$.buttonRow.classList.toggle(
           'division-line', safeZone.scrollHeight > safeZone.clientHeight);
     });
     this.divisionLineResizeObserver_.observe(safeZone);
