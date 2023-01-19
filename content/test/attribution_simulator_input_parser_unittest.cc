0ed03dbdbcc266c1ef9808c1af82a3d9b5987b16
Andrew Paseltiner <apaseltiner@chromium.org>
2023-01-19 20:01:30 +0000

    Allow rate-limit data deletion to be controlled in attribution simulator
    This will allow interop tests to be more comprehensive regarding data
    deletion, i.e. user-initiated vs site-initiated.
    
    Change-Id: I4b8037398c0cbd62a7f6abe687562826880d0664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174772
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094627}

--- content/test/attribution_simulator_input_parser_unittest.cc
+++ content/test/attribution_simulator_input_parser_unittest.cc
@@ -60,7 +60,8 @@ std::ostream& operator<<(std::ostream& out,
 
 bool operator==(const AttributionDataClear& a, const AttributionDataClear& b) {
   return a.time == b.time && a.delete_begin == b.delete_begin &&
-         a.delete_end == b.delete_end && a.origins == b.origins;
+         a.delete_end == b.delete_end && a.origins == b.origins &&
+         a.delete_rate_limit_data == b.delete_rate_limit_data;
 }
 
 std::ostream& operator<<(std::ostream& out, const AttributionDataClear& c) {
@@ -81,7 +82,7 @@ std::ostream& operator<<(std::ostream& out, const AttributionDataClear& c) {
     out << "null";
   }
 
-  return out;
+  return out << ",delete_rate_limit_data=" << c.delete_rate_limit_data << "}";
 }
 
 namespace {
@@ -508,7 +509,8 @@ TEST(AttributionSimulatorInputParserTest, ValidDataClearParses) {
       "origins": [
         "https://r.test",
         "https://s.test"
-      ]
+      ],
+      "delete_rate_limit_data": false
     }
   ]})json";
 
@@ -522,7 +524,8 @@ TEST(AttributionSimulatorInputParserTest, ValidDataClearParses) {
               /*time=*/kOffsetTime + base::Milliseconds(1643235574123),
               /*delete_begin=*/kOffsetTime + base::Milliseconds(1643235573123),
               /*delete_end=*/base::Time::Max(),
-              /*origins=*/absl::nullopt),
+              /*origins=*/absl::nullopt,
+              /*delete_rate_limit_data=*/true),
           AttributionDataClear(
               /*time=*/kOffsetTime + base::Milliseconds(1643235574123),
               /*delete_begin=*/base::Time::Min(),
@@ -531,7 +534,8 @@ TEST(AttributionSimulatorInputParserTest, ValidDataClearParses) {
               base::flat_set<url::Origin>{
                   url::Origin::Create(GURL("https://r.test")),
                   url::Origin::Create(GURL("https://s.test")),
-              }))));
+              },
+              /*delete_rate_limit_data=*/false))));
   EXPECT_THAT(error_stream.str(), IsEmpty());
 }
 
