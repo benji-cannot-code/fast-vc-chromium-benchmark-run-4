6439443fb1183e71c83cb6ff5ed7e05d41c30832
Jan Keitel <jkeitel@google.com>
2023-01-19 17:43:35 +0000

    Remove Autofill Assistant consent protos.
    Bug: b/263360694
    Change-Id: I2da3337b2a2f6d05f44c149d986b1a0456bfdd04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136070
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Markus Heintz <markusheintz@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094543}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -1278,7 +1278,6 @@ VISIT_PROTO_FIELDS(const sync_pb::UserConsentSpecifics& proto) {
   VISIT(arc_play_terms_of_service_consent);
   VISIT(assistant_activity_control_consent);
   VISIT(account_passwords_consent);
-  VISIT(autofill_assistant_consent);
 }
 
 VISIT_PROTO_FIELDS(
@@ -1322,13 +1321,6 @@ VISIT_PROTO_FIELDS(const sync_pb::UserConsentTypes::UnifiedConsent& proto) {
 
 VISIT_PROTO_FIELDS(
     const sync_pb::UserConsentTypes::AccountPasswordsConsent& proto) {
-  VISIT_REP(description_grd_ids);
-  VISIT(confirmation_grd_id);
-  VISIT_ENUM(status);
-}
-
-VISIT_PROTO_FIELDS(
-    const sync_pb::UserConsentTypes::AutofillAssistantConsent& proto) {
   VISIT_REP(description_grd_ids);
   VISIT(confirmation_grd_id);
   VISIT_ENUM(status);
