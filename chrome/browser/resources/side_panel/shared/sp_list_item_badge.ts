76a107c1623a5e7a4b6be6de19021ea39e7a16db
John Lee <johntlee@chromium.org>
2023-01-19 00:08:23 +0000

    Side Panel: Rename sp-label to sp-list-item-badge
    The name for the component was agreed upon by UX and WebUI eng to
    differentiate it from Views labels and filter/suggestion chips, and
    signify that the component should only really be used in list items.
    
    Bug: 1408552, 1402857
    Change-Id: Iebdaad504b7ab1a77299b4ab547981e208097d41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178459
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094168}

--- chrome/browser/resources/side_panel/shared/sp_list_item_badge.ts
+++ chrome/browser/resources/side_panel/shared/sp_list_item_badge.ts
@@ -4,19 +4,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @fileoverview A label component that is used to denote additional information
- * or an updated state.
+ * @fileoverview A badge component that is used to denote additional information
+ * or an updated state and is displayed inside of a cr-url-list-item.
  */
 
 import '//resources/cr_elements/cr_shared_vars.css.js';
 
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {getTemplate} from './sp_label.html.js';
+import {getTemplate} from './sp_list_item_badge.html.js';
 
-export class SpLabel extends PolymerElement {
+export class SpListItemBadge extends PolymerElement {
   static get is() {
-    return 'sp-label';
+    return 'sp-list-item-badge';
   }
 
   static get template() {
@@ -55,8 +55,8 @@ export class SpLabel extends PolymerElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'sp-label': SpLabel;
+    'sp-list-item-badge': SpListItemBadge;
   }
 }
 
-customElements.define(SpLabel.is, SpLabel);
+customElements.define(SpListItemBadge.is, SpListItemBadge);
