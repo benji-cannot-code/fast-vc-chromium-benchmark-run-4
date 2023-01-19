a0435b3cac3f4d87f9a1f3d4ad5fd3db08f1983d
Nasser Al-shawwa <alshawwa@chromium.org>
2023-01-19 15:24:13 +0000

    [Remove FileSystemConnector] Remove unused Box API token references
    Bug: b/262991271
    Change-Id: Iae1b91b61f873140ddba799030fd58fae3e4aad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174753
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094469}

--- chrome/browser/enterprise/connectors/service_provider_config_unittest.cc
+++ chrome/browser/enterprise/connectors/service_provider_config_unittest.cc
@@ -25,7 +25,6 @@ TEST(ServiceProviderConfigTest, Google) {
 
   ASSERT_TRUE(service_provider.analysis);
   ASSERT_TRUE(service_provider.reporting);
-  ASSERT_FALSE(service_provider.file_system);
 
   ASSERT_EQ("https://safebrowsing.google.com/safebrowsing/uploads/scan",
             std::string(service_provider.analysis->url));
@@ -53,7 +52,6 @@ TEST(ServiceProviderConfigTest, LocalTest1) {
 
   ASSERT_TRUE(service_provider.analysis);
   ASSERT_FALSE(service_provider.reporting);
-  ASSERT_FALSE(service_provider.file_system);
 
   ASSERT_FALSE(service_provider.analysis->url);
   ASSERT_TRUE(service_provider.analysis->local_path);
@@ -75,7 +73,6 @@ TEST(ServiceProviderConfigTest, LocalTest2) {
 
   ASSERT_TRUE(service_provider.analysis);
   ASSERT_FALSE(service_provider.reporting);
-  ASSERT_FALSE(service_provider.file_system);
 
   ASSERT_FALSE(service_provider.analysis->url);
   ASSERT_TRUE(service_provider.analysis->local_path);
@@ -97,7 +94,6 @@ TEST(ServiceProviderConfigTest, BrcmChrmCas) {
 
   ASSERT_TRUE(service_provider.analysis);
   ASSERT_FALSE(service_provider.reporting);
-  ASSERT_FALSE(service_provider.file_system);
 
   ASSERT_FALSE(service_provider.analysis->url);
   ASSERT_TRUE(service_provider.analysis->local_path);
@@ -111,29 +107,6 @@ TEST(ServiceProviderConfigTest, BrcmChrmCas) {
             "dlp");
   ASSERT_EQ(service_provider.analysis->supported_tags[0].max_file_size,
             kMaxFileSize);
-}
-
-TEST(ServiceProviderConfigTest, Box) {
-  const ServiceProviderConfig* config = GetServiceProviderConfig();
-  ASSERT_TRUE(config->count("box"));
-  ServiceProvider service_provider = config->at("box");
-
-  ASSERT_FALSE(service_provider.analysis);
-  ASSERT_FALSE(service_provider.reporting);
-  ASSERT_TRUE(service_provider.file_system);
-
-  ASSERT_EQ("https://box.com", std::string(service_provider.file_system->home));
-  ASSERT_EQ("https://account.box.com/api/oauth2/authorize",
-            std::string(service_provider.file_system->authorization_endpoint));
-  ASSERT_EQ("https://api.box.com/oauth2/token",
-            std::string(service_provider.file_system->token_endpoint));
-  ASSERT_EQ(20u * 1024 * 1024, service_provider.file_system->max_direct_size);
-  ASSERT_TRUE(service_provider.file_system->scopes.empty());
-  ASSERT_EQ(2u, service_provider.file_system->disable.size());
-  ASSERT_EQ("box.com",
-            std::string(service_provider.file_system->disable.at(0)));
-  ASSERT_EQ("boxcloud.com",
-            std::string(service_provider.file_system->disable.at(1)));
 }
 
 }  // namespace enterprise_connectors
