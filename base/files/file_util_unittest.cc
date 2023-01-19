2d5b7b58c7ec6ab327373aef8756940c180671f6
James Forshaw <forshaw@chromium.org>
2023-01-19 00:13:34 +0000

    [Windows] Disable EnforceNoExecutableFileHandles feature.
    This CL disabled the EnforceNoExecutableFileHandles feature until its
    behavior can be fixed to support the component installer. This CL also
    disables some tests when the feature is disabled to prevent them
    failing.
    
    Bug: 1408517
    Change-Id: I28e6843e1f5f95c27b8158f96f07fbd2cbb990ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179040
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094170}

--- base/files/file_util_unittest.cc
+++ base/files/file_util_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/command_line.h"
 #include "base/environment.h"
+#include "base/features.h"
 #include "base/files/file.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
 #include "base/test/multiprocess_test.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_file_util.h"
 #include "base/test/test_timeouts.h"
@@ -779,6 +781,9 @@ TEST_F(FileUtilTest, CreateWinHardlinkTest) {
 }
 
 TEST_F(FileUtilTest, PreventExecuteMappingNewFile) {
+  base::test::ScopedFeatureList enforcement_feature;
+  enforcement_feature.InitAndEnableFeature(
+      features::kEnforceNoExecutableFileHandles);
   FilePath file = temp_dir_.GetPath().Append(FPL("afile.txt"));
 
   ASSERT_FALSE(PathExists(file));
@@ -798,6 +803,9 @@ TEST_F(FileUtilTest, PreventExecuteMappingNewFile) {
 }
 
 TEST_F(FileUtilTest, PreventExecuteMappingExisting) {
+  base::test::ScopedFeatureList enforcement_feature;
+  enforcement_feature.InitAndEnableFeature(
+      features::kEnforceNoExecutableFileHandles);
   FilePath file = temp_dir_.GetPath().Append(FPL("afile.txt"));
   CreateTextFile(file, bogus_content);
   ASSERT_TRUE(PathExists(file));
@@ -817,6 +825,9 @@ TEST_F(FileUtilTest, PreventExecuteMappingExisting) {
 }
 
 TEST_F(FileUtilTest, PreventExecuteMappingOpenFile) {
+  base::test::ScopedFeatureList enforcement_feature;
+  enforcement_feature.InitAndEnableFeature(
+      features::kEnforceNoExecutableFileHandles);
   FilePath file = temp_dir_.GetPath().Append(FPL("afile.txt"));
   CreateTextFile(file, bogus_content);
   ASSERT_TRUE(PathExists(file));
@@ -842,6 +853,9 @@ TEST_F(FileUtilTest, PreventExecuteMappingOpenFile) {
 }
 
 TEST(FileUtilDeathTest, DisallowNoExecuteOnUnsafeFile) {
+  base::test::ScopedFeatureList enforcement_feature;
+  enforcement_feature.InitAndEnableFeature(
+      features::kEnforceNoExecutableFileHandles);
   base::FilePath local_app_data;
   // This test places a file in %LOCALAPPDATA% to verify that the checks in
   // IsPathSafeToSetAclOn work correctly.
