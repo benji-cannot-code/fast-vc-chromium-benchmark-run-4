3fd4c176b9d607142036b61c671dc64e273db1c6
Victor Tan <victortan@chromium.org>
2023-01-19 23:16:05 +0000

    Split client hints fetch latency metrics
    The current client hints fetch latency includes:
    * Latency reading accept_ch cache (stored in PrefService) for outer most
      main frame origin.
    * Latency reading client hints cache (stored in hash map) for prerender
      host outer most main frame tree node.
    * Latency to check whether needs to send UA reduction client hints
      across all parent frame tree nodes.
    
    It would be better for us to understand the fetch latency when we break
    out the total fetch latency into above three different categories.
    
    See details:
    
    [Google Internal Only]: https://docs.google.com/document/d/1wNNm98HxRE4ie8qusuIya2VyNnsFODn3i946hSzsYW0
    
    
    Bug: 1406891
    Change-Id: I5f0e18b1cf39d26bd75fb564cbe2d2b19924e96b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4163177
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094744}

--- content/browser/client_hints/client_hints.cc
+++ content/browser/client_hints/client_hints.cc
@@ -622,6 +622,7 @@ struct ClientHintsExtendedData {
     const base::TimeTicks start_time = base::TimeTicks::Now();
     delegate->GetAllowedClientHintsFromSource(outermost_main_frame_origin,
                                               &hints);
+    const base::TimeTicks pref_read_time = base::TimeTicks::Now();
 
     // If this is a prerender tree, also capture prerender local setting. The
     // setting was given by navigation requests on the prerendering page, and
@@ -633,6 +634,7 @@ struct ClientHintsExtendedData {
               *frame_tree_node))
         host->GetAllowedClientHintsOnPage(outermost_main_frame_origin, &hints);
     }
+    const base::TimeTicks prerender_host_time = base::TimeTicks::Now();
 
     // If this is not a top-level frame, then check if any of the ancestors
     // in the path that led to this request have Sec-CH-UA-Reduced set.
@@ -652,8 +654,17 @@ struct ClientHintsExtendedData {
     }
 
     // Record the time spent getting the client hints.
-    base::TimeDelta duration = base::TimeTicks::Now() - start_time;
-    base::UmaHistogramTimes("ClientHints.FetchLatency", duration);
+    const base::TimeTicks end_time = base::TimeTicks::Now();
+    base::UmaHistogramMicrosecondsTimes("ClientHints.FetchLatency_PrefRead",
+                                        pref_read_time - start_time);
+    base::UmaHistogramMicrosecondsTimes(
+        "ClientHints.FetchLatency_PrerenderHost",
+        prerender_host_time - pref_read_time);
+    base::UmaHistogramMicrosecondsTimes(
+        "ClientHints.FetchLatency_OriginTrialCheck",
+        end_time - prerender_host_time);
+    base::UmaHistogramMicrosecondsTimes("ClientHints.FetchLatency_Total",
+                                        end_time - start_time);
   }
 
   blink::EnabledClientHints hints;
