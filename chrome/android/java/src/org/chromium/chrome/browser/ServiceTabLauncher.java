d32574c86b2ca00c03c72fc5e7a5b86ee08a739f
Peter E Conn <peconn@chromium.org>
2023-01-19 11:49:30 +0000

    🔔 Remove WebPlatformNotificationMetrics.
    The metrics have expired, so removing them. A snapshot of the metrics
    has been saved in the linked bug.
    
    Bug: 1403705
    Change-Id: I3ab7d6a1faff0136c37ccc636aed4c270ca68a9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128082
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094398}

--- chrome/android/java/src/org/chromium/chrome/browser/ServiceTabLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ServiceTabLauncher.java
@@ -19,7 +19,6 @@ import org.chromium.base.task.PostTask;
 import org.chromium.chrome.browser.browserservices.TrustedWebActivityClient;
 import org.chromium.chrome.browser.browserservices.intents.WebappConstants;
 import org.chromium.chrome.browser.browserservices.metrics.BrowserServicesTimingMetrics;
-import org.chromium.chrome.browser.notifications.WebPlatformNotificationMetrics;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.AsyncTabCreationParams;
@@ -72,8 +71,6 @@ public class ServiceTabLauncher {
     public static void launchTab(final int requestId, boolean incognito, GURL url, int disposition,
             String referrerUrl, int referrerPolicy, String extraHeaders,
             ResourceRequestBody postData) {
-        WebPlatformNotificationMetrics.getInstance().onNewTabLaunched();
-
         // Open popup window in custom tab.
         // Note that this is used by PaymentRequestEvent.openWindow().
         if (disposition == WindowOpenDisposition.NEW_POPUP) {
