1f79b99cedf8d557e3b12996e13ffd611c242e01
Austin Sullivan <asully@chromium.org>
2023-01-19 00:24:43 +0000

    Revert "Remove timeout increase for integration test."
    This reverts commit c283485269da8f5b303316016d6d93cc1cf29cc1.
    
    Reason for revert: Suspected breakage of Linux ubsan bot. See
    https://bugs.chromium.org/p/chromium/issues/detail?id=1408550
    
    Original change's description:
    > Remove timeout increase for integration test.
    >
    > Default timeout has increased from 5 to 8 seconds, and it was suggested that we might not need the 60 seconds in https://crrev.com/c/4173998/.
    >
    > A test with `sync_integration_tests --gtest_filter="SingleClientSharingMessage*" --gtest_repeat=50` showed no failures.
    >
    > Bug: 1404664
    > Change-Id: I0dcca5a7fc6f7bd9e2977df160aa85079d2a6e4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177109
    > Auto-Submit: Jens Mueller <muellerj@google.com>
    > Commit-Queue: Jens Mueller <muellerj@google.com>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1093950}
    
    Bug: 1404664
    Change-Id: I3208b17626d683dbec2f5953350435a320ba702d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177959
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Owners-Override: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094175}

--- chrome/browser/sync/test/integration/single_client_sharing_message_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_sharing_message_sync_test.cc
@@ -203,7 +203,13 @@ class SharingMessageCommitChecker : public SingleClientStatusChangeChecker {
 
 class SingleClientSharingMessageSyncTest : public SyncTest {
  public:
-  SingleClientSharingMessageSyncTest() : SyncTest(SINGLE_CLIENT) {}
+  SingleClientSharingMessageSyncTest() : SyncTest(SINGLE_CLIENT) {
+    // Replace the default value (5 seconds) with 1 minute to reduce possibility
+    // of test flakiness.
+    features_override_.InitAndEnableFeatureWithParameters(
+        kSharingMessageBridgeTimeout,
+        {{"SharingMessageBridgeTimeoutSeconds", "60"}});
+  }
 
   bool WaitForSharingMessage(
       std::vector<SharingMessageSpecifics> expected_specifics) {
@@ -211,6 +217,9 @@ class SingleClientSharingMessageSyncTest : public SyncTest {
                                          std::move(expected_specifics))
         .Wait();
   }
+
+ private:
+  base::test::ScopedFeatureList features_override_;
 };
 
 IN_PROC_BROWSER_TEST_F(SingleClientSharingMessageSyncTest, ShouldSubmit) {
