fd1fd2a23826404f49b8177af67f1ed2aabb1813
Sijin Soon <sijins@google.com>
2023-01-20 04:11:19 +0000

    Validate recently used GIFs every 24 hours.
    Tenor, the codebase which GIFs are fetched from, has a policy where deleted GIFs cannot be rendered 24 hours after deletion. This CL validates that recently used GIFs still exist when opening the Emoji Picker if it has been more than 24 hours since the last validation.
    
    Bug: b:264936352
    Change-Id: I54b20105b41c66b2eb9fb4b48b1d287388ebf4ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4170003
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Sijin Soon <sijins@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094865}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_picker.ts
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_picker.ts
@@ -95,6 +95,7 @@ export class EmojiPicker extends PolymerElement {
   private highlightBarMoving: boolean;
   private groupTabsMoving: boolean;
   private gifDataInitialised: boolean;
+  private previousGifValidation: Date;
 
   constructor() {
     super();
@@ -113,6 +114,7 @@ export class EmojiPicker extends PolymerElement {
     this.autoScrollingToGroup = false;
     this.highlightBarMoving = false;
     this.groupTabsMoving = false;
+    this.previousGifValidation = this.loadPreviousGifValidationTime();
 
     this.addEventListener(
         events.GROUP_BUTTON_CLICK,
@@ -303,6 +305,7 @@ export class EmojiPicker extends PolymerElement {
     );
 
     if (this.gifSupport) {
+      this.validateRecentlyUsedGifs();
       const categoriesFetchPromise =
           prevFetchPromise.then(() => this.apiProxy.getCategories());
 
@@ -1193,6 +1196,40 @@ export class EmojiPicker extends PolymerElement {
                           icon: data.icon,
                           active: data.name === category,
                         }));
+  }
+
+  /**
+   * Checks if recently used GIFs are still valid if we open the emoji picker
+   * and it has been 24 hours since the last validation.
+   */
+  async validateRecentlyUsedGifs() {
+    // This check ensures that we don't try and validate recently used GIFs
+    // if the validating process is already currently happening.
+    const currentTime = new Date();
+
+    if ((currentTime.getTime() - this.previousGifValidation.getTime()) >
+        constants.TWENTY_FOUR_HOURS) {
+      const updated = await this.categoriesHistory[CategoryEnum.GIF]?.validate(
+          this.apiProxy);
+
+      this.previousGifValidation = currentTime;
+      window.localStorage.setItem(
+          constants.GIF_VALIDATION_DATE, currentTime.toJSON());
+
+      if (updated) {
+        this.categoryHistoryUpdated(CategoryEnum.GIF);
+      }
+    }
+  }
+
+  private loadPreviousGifValidationTime(): Date {
+    const stored = window.localStorage.getItem(constants.GIF_VALIDATION_DATE);
+    if (!stored) {
+      // First time opening the Emoji Picker so there should be no recently used
+      // GIFs to render.
+      return new Date();
+    }
+    return new Date(stored);
   }
 
   private getTabs() {
