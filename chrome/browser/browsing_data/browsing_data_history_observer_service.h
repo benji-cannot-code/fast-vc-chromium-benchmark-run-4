c79927c72fbae28b67abc923aa9eb9ebc7541e1a
Andrew Paseltiner <apaseltiner@chromium.org>
2023-01-20 14:41:07 +0000

    Stop deleting attribution/aggregation data when history is cleared
    Bug: 1408452
    Change-Id: I635a1d94bfc50dc79e5d736104819be3c45d762a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178510
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095028}

--- chrome/browser/browsing_data/browsing_data_history_observer_service.h
+++ chrome/browser/browsing_data/browsing_data_history_observer_service.h
@@ -20,10 +20,6 @@ template <typename T>
 struct DefaultSingletonTraits;
 }  // namespace base
 
-namespace content {
-class StoragePartition;
-}  // namespace content
-
 // BrowsingDataHistoryObserverService is listening for history deletions to
 // remove navigation, session and recent tab entries.
 class BrowsingDataHistoryObserverService
@@ -59,12 +55,8 @@ class BrowsingDataHistoryObserverService
     bool ServiceIsCreatedWithBrowserContext() const override;
   };
 
-  void OverrideStoragePartitionForTesting(content::StoragePartition* partition);
-
  private:
   raw_ptr<Profile> profile_;
-
-  raw_ptr<content::StoragePartition> storage_partition_for_testing_ = nullptr;
 
   base::ScopedObservation<history::HistoryService,
                           history::HistoryServiceObserver>
