fef1aa16a951420a01692ebec4dd73f88ff60d1b
Norge Vizcay <vizcay@google.com>
2023-01-20 10:17:27 +0000

    [AutofillFeedback] Add Autofill feedback option to Autofill context menu.
    The Cl adds one item at the bottom of the Autofill context menu. The item serves to open the Feedback dialog that submits a feedback report to Google servers.
    
    See screenshot: https://screenshot.googleplex.com/yXhG2ygzkzN6P4y
    
    Bug: 1407646
    Change-Id: Iac621902c9760362283542ea923096b1ec1bd5e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173396
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094949}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -474,6 +474,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_DEBUG_PRINT_VIEW_TREE_DETAILS 52512
 // Please leave a gap here for new debug commands.
 
+// Autofill feedback.
+#define IDC_CONTENT_CONTEXT_AUTOFILL_FEEDBACK 52990
 // Autofill context menu commands
 #define IDC_CONTENT_CONTEXT_AUTOFILL_CUSTOM_FIRST 53000
 #define IDC_CONTENT_CONTEXT_AUTOFILL_CUSTOM_LAST 53250
--- chrome/browser/ui/autofill/autofill_context_menu_manager_unittest.cc
+++ chrome/browser/ui/autofill/autofill_context_menu_manager_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/autofill/content/browser/content_autofill_driver_factory.h"
 #include "components/autofill/content/browser/content_autofill_driver_factory_test_api.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/mojom/context_menu/context_menu.mojom.h"
+#include "ui/base/l10n/l10n_util.h"
 
 using testing::_;
 
@@ -63,8 +65,10 @@ class MockAutofillDriver : public TestAutofillDriver {
 class AutofillContextMenuManagerTest : public ChromeRenderViewHostTestHarness {
  public:
   AutofillContextMenuManagerTest() {
-    feature_.InitAndEnableFeature(
-        features::kAutofillShowManualFallbackInContextMenu);
+    feature_.InitWithFeatures(
+        {features::kAutofillShowManualFallbackInContextMenu,
+         features::kAutofillFeedback},
+        {});
   }
 
   AutofillContextMenuManagerTest(const AutofillContextMenuManagerTest&) =
@@ -143,11 +147,14 @@ TEST_F(AutofillContextMenuManagerTest, AutofillContextMenuContents) {
   std::vector<std::u16string> all_added_strings;
 
   // Check for top level menu with autofill options.
-  ASSERT_EQ(2u, menu_model()->GetItemCount());
+  ASSERT_EQ(3u, menu_model()->GetItemCount());
   ASSERT_EQ(u"Fill Address Info", menu_model()->GetLabelAt(0));
   ASSERT_EQ(u"Fill Payment", menu_model()->GetLabelAt(1));
+  ASSERT_EQ(l10n_util::GetStringUTF16(IDS_CONTENT_CONTEXT_AUTOFILL_FEEDBACK),
+            menu_model()->GetLabelAt(2));
   ASSERT_EQ(menu_model()->GetTypeAt(0), ui::MenuModel::ItemType::TYPE_SUBMENU);
   ASSERT_EQ(menu_model()->GetTypeAt(1), ui::MenuModel::ItemType::TYPE_SUBMENU);
+  ASSERT_EQ(menu_model()->GetTypeAt(2), ui::MenuModel::ItemType::TYPE_COMMAND);
 
   // Check for submenu with address descriptions.
   auto* address_menu_model = menu_model()->GetSubmenuModelAt(0);
