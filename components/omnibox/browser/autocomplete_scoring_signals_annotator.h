34fc97225801309cf7aedf78a1a9cfb562ef089b
Jun Zou <junzou@chromium.org>
2023-01-20 01:19:06 +0000

    [Omnibox][ML] Reland: Implement history scoring signal annotator for url suggestions.
    Relands CL with some follow-up changes: https://chromium-review.googlesource.com/c/chromium/src/+/4087509?tab=comments
    
    - Currently, only synchronously looks up URLs from in-memory URL DB.
    - Annotates the suggestions with the following signals:
    typed_count, visit_count, elapsed_time_last_visit_secs, total_title_match_length, num_input_terms_matched_by_title.
    - Guarded by feature param `enable_scoring_signals_annotators`.
    
    Bug: b/261520284
    Change-Id: Idc3f7b54777629dec6ede22497f730e7d3359474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4137176
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Jun Zou <junzou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094813}

--- components/omnibox/browser/autocomplete_scoring_signals_annotator.h
+++ components/omnibox/browser/autocomplete_scoring_signals_annotator.h
@@ -6,7 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OMNIBOX_BROWSER_AUTOCOMPLETE_SCORING_SIGNALS_ANNOTATOR_H_
 #define COMPONENTS_OMNIBOX_BROWSER_AUTOCOMPLETE_SCORING_SIGNALS_ANNOTATOR_H_
 
-#include "components/omnibox/browser/autocomplete_result.h"
+#include "third_party/metrics_proto/omnibox_event.pb.h"
+
+class AutocompleteInput;
+class AutocompleteResult;
+
+using ScoringSignals = ::metrics::OmniboxEventProto::Suggestion::ScoringSignals;
 
 // Base class for annotating suggestions in autocomplete results with various
 // signals for ML training and scoring.
@@ -20,7 +25,8 @@ class AutocompleteScoringSignalsAnnotator {
   virtual ~AutocompleteScoringSignalsAnnotator() = default;
 
   // Annotate the autocomplete result.
-  virtual void AnnotateResult(AutocompleteResult* result) = 0;
+  virtual void AnnotateResult(const AutocompleteInput& input,
+                              AutocompleteResult* result) = 0;
 };
 
 #endif  // COMPONENTS_OMNIBOX_BROWSER_AUTOCOMPLETE_SCORING_SIGNALS_ANNOTATOR_H_
