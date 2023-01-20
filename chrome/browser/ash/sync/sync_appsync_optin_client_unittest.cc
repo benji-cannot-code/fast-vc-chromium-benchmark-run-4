cecf8600833f8e60350746111f7e229ccadd2f61
Maciek Swiech <drmasquatch@google.com>
2023-01-20 00:17:11 +0000

    metrics: add AppsSynOptinFileWrite to UMA enums and emit
    Add a trackable metric that will be sent if Chrome fails to write out a file to track AppsSync opt-in to user daemon-store.
    
    Bug: b/264678143, chromium:1193485
    Change-Id: I6f1b29424b36a76576108ae220acb1f290fd6a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148691
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Maciek Swiech <drmasquatch@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094781}

--- chrome/browser/ash/sync/sync_appsync_optin_client_unittest.cc
+++ chrome/browser/ash/sync/sync_appsync_optin_client_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/observer_list.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "components/account_id/account_id.h"
@@ -27,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
+
+constexpr char kAppsSyncOptinIOHistogram[] =
+    "Cros.AppsSyncOptinFileWriteAttempts";
 
 namespace {
 
@@ -165,24 +169,29 @@ TEST_F(SyncAppsyncOptinClientTest, ServiceCreatesDirectory) {
 
 TEST_F(SyncAppsyncOptinClientTest, ServiceCreatesOptInFile) {
   EXPECT_TRUE(base::IsDirectoryEmpty(tmp_dir_path_));
+  base::HistogramTester histogram_tester;
 
+  test_sync_service_->SetAppsyncOptin(false);
   test_appsync_optin_client_ = std::make_unique<SyncAppsyncOptinClient>(
       test_sync_service_.get(), test_user_manager_.get(),
       test_daemon_dir_.GetPath());
-  test_sync_service_->SetAppsyncOptin(false);
 
   // Wait for file IO to finish.
   task_environment_.RunUntilIdle();
 
   EXPECT_FALSE(base::IsDirectoryEmpty(tmp_dir_path_));
   EXPECT_TRUE(base::PathExists(tmp_dir_path_.Append("opted-in")));
+
+  histogram_tester.ExpectUniqueSample(
+      kAppsSyncOptinIOHistogram,
+      SyncAppsyncOptinClient::AppsSyncOptinFileWrite::kAttempt, 1);
 }
 
 TEST_F(SyncAppsyncOptinClientTest, LoggedInUser) {
+  test_sync_service_->SetAppsyncOptin(false);
   test_appsync_optin_client_ = std::make_unique<SyncAppsyncOptinClient>(
       test_sync_service_.get(), test_user_manager_.get(),
       test_daemon_dir_.GetPath());
-  test_sync_service_->SetAppsyncOptin(false);
 
   // Wait for file IO to finish.
   task_environment_.RunUntilIdle();
@@ -196,10 +205,10 @@ TEST_F(SyncAppsyncOptinClientTest, LoggedInUser) {
 }
 
 TEST_F(SyncAppsyncOptinClientTest, LoggedInUserWithPermission) {
+  test_sync_service_->SetAppsyncOptin(true);
   test_appsync_optin_client_ = std::make_unique<SyncAppsyncOptinClient>(
       test_sync_service_.get(), test_user_manager_.get(),
       test_daemon_dir_.GetPath());
-  test_sync_service_->SetAppsyncOptin(true);
 
   // Wait for file IO to finish.
   task_environment_.RunUntilIdle();
@@ -213,10 +222,12 @@ TEST_F(SyncAppsyncOptinClientTest, LoggedInUserWithPermission) {
 }
 
 TEST_F(SyncAppsyncOptinClientTest, UserChangesPermission) {
+  base::HistogramTester histogram_tester;
+
+  test_sync_service_->SetAppsyncOptin(true);
   test_appsync_optin_client_ = std::make_unique<SyncAppsyncOptinClient>(
       test_sync_service_.get(), test_user_manager_.get(),
       test_daemon_dir_.GetPath());
-  test_sync_service_->SetAppsyncOptin(true);
 
   // Wait for file IO to finish.
   task_environment_.RunUntilIdle();
@@ -238,6 +249,29 @@ TEST_F(SyncAppsyncOptinClientTest, UserChangesPermission) {
   EXPECT_TRUE(
       base::ReadFileToString(tmp_dir_path_.Append("opted-in"), &contents));
   EXPECT_EQ("0", contents);
+
+  histogram_tester.ExpectUniqueSample(
+      kAppsSyncOptinIOHistogram,
+      SyncAppsyncOptinClient::AppsSyncOptinFileWrite::kAttempt, 2);
+}
+
+TEST_F(SyncAppsyncOptinClientTest, WriteFails) {
+  base::HistogramTester histogram_tester;
+  test_appsync_optin_client_ = std::make_unique<SyncAppsyncOptinClient>(
+      test_sync_service_.get(), test_user_manager_.get(),
+      test_daemon_dir_.GetPath().Append("NOT-A-REAL-PATH"));
+
+  // Wait for file IO to finish.
+  task_environment_.RunUntilIdle();
+
+  EXPECT_TRUE(base::IsDirectoryEmpty(tmp_dir_path_));
+
+  histogram_tester.ExpectBucketCount(
+      kAppsSyncOptinIOHistogram,
+      SyncAppsyncOptinClient::AppsSyncOptinFileWrite::kAttempt, 1);
+  histogram_tester.ExpectBucketCount(
+      kAppsSyncOptinIOHistogram,
+      SyncAppsyncOptinClient::AppsSyncOptinFileWrite::kFailure, 1);
 }
 
 }  // namespace ash
