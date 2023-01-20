1dd9dd506b7e03e8cb82eae2610bb86825ddb6a3
Traian Captan <tcaptan@chromium.org>
2023-01-20 22:49:27 +0000

    Add 'no url function' support in css image-set
    Currently, if the url of an image-set option is specified as a string
    without the 'url()' function notation, parsing fails.
    
    Based on the spec, the url can be just a string.
    
    Spec definition:
    
    "The syntax for image-set() is:
    
    <image-set()> = image-set( <image-set-option># )
    <image-set-option> = [ <image> | <string> ]
                         [ <resolution> || type(<string>) ]
    
    Each <string> inside image-set() represents a <url>."
    
    https://w3c.github.io/csswg-drafts/css-images-4/#image-set-notation
    
    With this change Chrome will match the spec as well as Firefox for
    both standard and '-webkit-' prefixed syntax.
    Safari does not allow empty strings for standard syntax, and does not
    allow strings for '-webkit-' prefixed syntax because of [1] and fixed
    in [2].
    The issue observed in Safari of "showing the missing image icon" is
    not observed in Chrome.
    
    [1] https://bugs.webkit.org/show_bug.cgi?id=206909
    [2] https://github.com/WebKit/WebKit/commit/bfc08b19e20ed4bf9a9769b14f192843bf7e6fbe
    
    R=futhark, masonf, pdr, nrosenthal
    
    Bug: 1400901
    Change-Id: Iaf7e4b3128153664f3d311384b77b025d27bbf6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179000
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095212}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -1405,6 +1405,14 @@ CSSStringValue* ConsumeString(CSSParserTokenRange& range) {
       range.ConsumeIncludingWhitespace().Value().ToString());
 }
 
+StringView ConsumeStringAsStringView(CSSParserTokenRange& range) {
+  if (range.Peek().GetType() != CSSParserTokenType::kStringToken) {
+    return StringView();
+  }
+
+  return range.ConsumeIncludingWhitespace().Value();
+}
+
 StringView ConsumeUrlAsStringView(CSSParserTokenRange& range,
                                   const CSSParserContext& context) {
   StringView url;
@@ -1438,6 +1446,15 @@ StringView ConsumeUrlAsStringView(CSSParserTokenRange& range,
   }
 
   return url;
+}
+
+StringView ConsumeUrlOrStringAsStringView(CSSParserTokenRange& range,
+                                          const CSSParserContext& context) {
+  if (range.Peek().GetType() == CSSParserTokenType::kStringToken) {
+    return ConsumeStringAsStringView(range);
+  }
+
+  return ConsumeUrlAsStringView(range, context);
 }
 
 cssvalue::CSSURIValue* ConsumeUrl(CSSParserTokenRange& range,
@@ -3308,7 +3325,10 @@ static CSSValue* ConsumeImageSet(CSSParserTokenRange& range,
   auto* image_set = MakeGarbageCollected<CSSImageSetValue>();
   do {
     AtomicString url_value =
-        ConsumeUrlAsStringView(args, context).ToAtomicString();
+        (RuntimeEnabledFeatures::CSSImageSetEnabled()
+             ? ConsumeUrlOrStringAsStringView(args, context)
+             : ConsumeUrlAsStringView(args, context))
+            .ToAtomicString();
     if (url_value.IsNull()) {
       return nullptr;
     }
