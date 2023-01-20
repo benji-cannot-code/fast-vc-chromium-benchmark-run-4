5b37a4580a7771d23a07f1e12dafeb073a011e26
mlcui <mlcui@google.com>
2023-01-20 00:34:43 +0000

    chrome.inputMethodPrivate: Migrate to Promise-based extension APIs
    inputMethodPrivate was (very) recently migrated to a Promise-based API
    in https://crrev.com/c/4172683. This moves all uses of
    inputMethodPrivate in c/b/r/settings to the Promise-based API in
    preparation for the upcoming TypeScript migration.
    
    Note: There are still some uses in c/b/r/cros/a11y which should be
    migrated separately.
    
    Bug: 1368302, b:263829183
    Change-Id: I7d6ba38ebbd9cf83010cd43a13701e7890caaa1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179413
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094790}

--- chrome/test/data/webui/settings/chromeos/fake_input_method_private.js
+++ chrome/test/data/webui/settings/chromeos/fake_input_method_private.js
@@ -16,11 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 export function FakeInputMethodPrivate() {}
 
 FakeInputMethodPrivate.prototype = {
-  getCurrentInputMethod: function(callback) {
-    callback(null);
-  },
+  getCurrentInputMethod: () => Promise.resolve(null),
 
-  setCurrentInputMethod: () => {},
+  setCurrentInputMethod: () => Promise.resolve(),
 
   get onChanged() {
     return {
