a039fff6bc5b915e81d458427e23cf6048ba81c7
Pu Shi <pushi@google.com>
2023-01-20 06:11:28 +0000

    [Exo][2/4] Add method to add and remove apps from app list
    Add method to add and remove apps from app list in
    AppStreaLauncherDataModel.
    
    Test: build successfully and unit tests passed. Manually tested and
    existing feature worked fine.
    
    Bug: b/265940190
    Change-Id: I8f46c19c4152b37e95b684100bdeeeb628b416a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178252
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094885}

--- chromeos/ash/components/phonehub/app_stream_launcher_data_model.h
+++ chromeos/ash/components/phonehub/app_stream_launcher_data_model.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
 #include "chromeos/ash/components/phonehub/notification.h"
+#include "chromeos/ash/components/phonehub/proto/phonehub_api.pb.h"
 
 namespace ash::phonehub {
 
@@ -49,6 +50,8 @@ class AppStreamLauncherDataModel {
       const std::vector<Notification::AppMetadata>& streamable_apps);
   const std::vector<Notification::AppMetadata>* GetAppsList();
   const std::vector<Notification::AppMetadata>* GetAppsListSortedByName();
+  void AddAppToList(const Notification::AppMetadata& app);
+  void RemoveAppFromList(const proto::App app);
 
  private:
   // Indicates if the Mini Launcher should be shown when the status is
