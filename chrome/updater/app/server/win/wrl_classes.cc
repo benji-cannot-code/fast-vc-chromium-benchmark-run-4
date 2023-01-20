663ac4e9843a7fdf30e019c15521f551f312d334
S. Ganesh <ganesh@chromium.org>
2023-01-20 22:58:33 +0000

    Add support for CLSID_GoogleUpdate3WebServiceClass
    Some areas in the Chrome codebase are using
    `CLSID_GoogleUpdate3WebServiceClass` instead of using
    `CLSID_GoogleUpdate3WebMachineClass`. The latter CLSID is what was
    supported before this CL, and is actually called
    `CLSID_GoogleUpdate3WebSystemClass` in the updater codebase.
    
    For backward compatibility, this CL now adds support for
    `CLSID_GoogleUpdate3WebServiceClass` in the updater. Both
    CLSIDs create instances of the same class, and the functionality is
    therefore identical.
    
    Bug: 1408896
    Change-Id: I3fadd1b671a93f1bb665fb1b761beca233565d7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4184800
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095228}

--- chrome/updater/app/server/win/wrl_classes.cc
+++ chrome/updater/app/server/win/wrl_classes.cc
@@ -38,6 +38,10 @@ CoCreatableClassWithFactoryEx(
     Microsoft::WRL::SimpleClassFactory<LegacyOnDemandImpl>,
     ActiveSystem);
 CoCreatableClassWithFactoryEx(
+    GoogleUpdate3WebServiceClass,
+    Microsoft::WRL::SimpleClassFactory<LegacyOnDemandImpl>,
+    ActiveSystem);
+CoCreatableClassWithFactoryEx(
     ProcessLauncherClass,
     Microsoft::WRL::SimpleClassFactory<LegacyProcessLauncherImpl>,
     ActiveSystem);
