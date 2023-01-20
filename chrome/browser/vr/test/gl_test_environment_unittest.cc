f7c569093b500b1e82c5b49e4d2e303e47862e7c
David Bienvenu <davidbienvenu@chromium.org>
2023-01-20 14:19:06 +0000

    win7dep: Remove remaining Win7/8 code from chrome/browser
    Win7/8 support is deprecated. This CL removes the remaining Win7/8
    code in chrome/browser/*.
    
    Bug: 1385856
    Change-Id: If6063dcb916c2068dead5b29f802869a2c840e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179005
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095013}

--- chrome/browser/vr/test/gl_test_environment_unittest.cc
+++ chrome/browser/vr/test/gl_test_environment_unittest.cc
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/size.h"
 
-#if BUILDFLAG(IS_WIN)
-#include "base/win/windows_version.h"
-#endif
-
 namespace vr {
 
 // TODO(crbug.com/1394319): Re-enable this test on MSAN if not removed.
@@ -24,11 +20,6 @@ namespace vr {
 #define MAYBE_InitializeAndCleanup InitializeAndCleanup
 #endif
 TEST(GlTestEnvironmentTest, MAYBE_InitializeAndCleanup) {
-#if BUILDFLAG(IS_WIN)
-  // VR is not supported on Windows 7.
-  if (base::win::GetVersion() <= base::win::Version::WIN7)
-    return;
-#endif
   GlTestEnvironment gl_test_environment(gfx::Size(100, 100));
   EXPECT_NE(gl_test_environment.GetFrameBufferForTesting(), 0u);
   EXPECT_EQ(glGetError(), (GLenum)GL_NO_ERROR);
