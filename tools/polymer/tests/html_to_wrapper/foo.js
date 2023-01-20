7e44b2a163412d958355a0ef92dc9c466689ed44
dpapad <dpapad@chromium.org>
2023-01-20 19:08:30 +0000

    WebUI: Add ability to detect 'native' vs 'polymer' in html_to_wrapper().
    Some WebUI surfaces/folders (currently c/b/r/webui_gallery/ and
    ui/webui/resources/cr_elements) have a mix of Polymer and native
    custom elements. Being able to automatically detect the type of a
    custom element makes it easier to build such WebUIs using build_webui()
    or with build_cr_component() without exposing unnecessary
    implementation details to WebUI developers.
    
    Done by accepting a new value of "detect" in html_to_wrapper()'s
    |template| proprety. This is still kept as an opt-in, since detecting
    the custom element's type automatically does require a bit of additional
    work during the build (looking into a web component's definition file).
    We can consider whether to make it the default (and only) option
    separately.
    
    Bug: 1340376
    Change-Id: I2df9b10a4c564bcccb37863b95c3075e6059b32d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3803471
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095154}

---
+++ tools/polymer/tests/html_to_wrapper/foo.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './foo.html.js';
+
+export class CrFooElement extends PolymerElement {
+  static override get template() {
+    return getTemplate();
+  }
+}
