87f0270ed46237e72c3ec611853bd8d725472e74
Markus Handell <handellm@google.com>
2023-01-20 19:01:26 +0000

    WebmMuxer: Add traces.
    Bug: 1409024
    Change-Id: I751e46cc83a91b51f14520baabd3ad8c7506aa86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181326
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Markus Handell <handellm@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095141}

--- media/muxers/webm_muxer.cc
+++ media/muxers/webm_muxer.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#include "base/trace_event/trace_event.h"
 #include "media/base/audio_codecs.h"
 #include "media/base/audio_parameters.h"
 #include "media/base/limits.h"
@@ -282,6 +283,8 @@ bool WebmMuxer::OnEncodedVideo(const VideoParameters& params,
                                std::string encoded_alpha,
                                base::TimeTicks timestamp,
                                bool is_key_frame) {
+  TRACE_EVENT2("media", __func__, "timestamp", timestamp - base::TimeTicks(),
+               "is_key_frame", is_key_frame);
   DVLOG(2) << __func__ << " - " << encoded_data.size() << "B ts " << timestamp;
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(params.codec == VideoCodec::kVP8 || params.codec == VideoCodec::kVP9 ||
@@ -333,6 +336,7 @@ bool WebmMuxer::OnEncodedVideo(const VideoParameters& params,
 bool WebmMuxer::OnEncodedAudio(const AudioParameters& params,
                                std::string encoded_data,
                                base::TimeTicks timestamp) {
+  TRACE_EVENT1("media", __func__, "timestamp", timestamp - base::TimeTicks());
   DVLOG(2) << __func__ << " - " << encoded_data.size() << "B ts " << timestamp;
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
@@ -561,6 +565,8 @@ bool WebmMuxer::FlushNextFrame() {
   }
 
   DCHECK(frame.data.data());
+  TRACE_EVENT2("media", __func__, "is_video", queue == &video_frames_,
+               "recorded_timestamp", recorded_timestamp);
   bool result =
       frame.alpha_data.empty()
           ? segment_.AddFrame(
@@ -588,6 +594,7 @@ void WebmMuxer::MaybeForceNewCluster() {
   // media timestamps
   if (base::TimeTicks::Now() - delegate_->last_data_output_timestamp() >=
       max_data_output_interval_) {
+    TRACE_EVENT0("media", "ForceNewClusterOnNextFrame");
     segment_.ForceNewClusterOnNextFrame();
   }
 }
