c2779e7b19961bd8dc63cec20fc497e458cddd42
Ivana Žužić <izuzic@google.com>
2023-01-20 09:43:23 +0000

    [TTFCC] Wait for the test activity to completely load
    The Touch to Fill BottomSheet render tests are failing because the
    page in the activity for testing is sometimes still loading at the
    time when the screenshot is taken and the loading bar is sometimes
    visible and sometimes not, so all the tests that screenshot the whole
    activity are flaky.
    
    Waiting for the page to fully load should fix this issue.
    
    Example of a screenshot containing a loading bar:
    https://chrome-gold.skia.org/img/images/b328fdd1d8ed10139e41597c4139e6a6.png
    
    Bug: 1407272
    Change-Id: I275918c315b8da2c37c266b0d1c09159f42b5fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181107
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094937}

--- chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardRenderTest.java
+++ chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardRenderTest.java
@@ -46,8 +46,8 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * These tests render screenshots of touch to fill for credit cards sheet and compare them
- * to a gold standard.
+ * These tests render screenshots of touch to fill for credit cards sheet and compare them to a gold
+ * standard.
  */
 @RunWith(ParameterizedRunner.class)
 @Batch(Batch.PER_CLASS)
@@ -102,6 +102,7 @@ public class TouchToFillCreditCardRenderTest {
     @Before
     public void setUp() throws InterruptedException {
         mActivityTestRule.startMainActivityOnBlankPage();
+        mActivityTestRule.waitForActivityCompletelyLoaded();
         mBottomSheetController = mActivityTestRule.getActivity()
                                          .getRootUiCoordinatorForTesting()
                                          .getBottomSheetController();
