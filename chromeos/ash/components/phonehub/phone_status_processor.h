486abf554bec04303a57f233f67768c0dfa359c6
Pu Shi <pushi@google.com>
2023-01-20 06:58:50 +0000

    [Exo][4/4 Process app list incremental update message
    Add methods detail to process app list incremental update message and
    update list in AppStreamingLauncherDataModel and
    RecentAppsInteractionHandlerImpl.
    
    Test: manually verified the app add/removal messages are correctly
    processed.
    Fixed:b/265940190
    
    Change-Id: I952fda53fcc9097af2c476e4c7e70e35b26f43dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178553
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094898}

--- chromeos/ash/components/phonehub/phone_status_processor.h
+++ chromeos/ash/components/phonehub/phone_status_processor.h
@@ -42,7 +42,8 @@ class PhoneStatusProcessor
     kOnlyRecentApps = 0,
     kOnlyLauncherApps,
     kBoth,
-    kMaxValue = kBoth
+    kIncrementalAppUpdate,
+    kMaxValue = kIncrementalAppUpdate
   };
 
   PhoneStatusProcessor(
@@ -80,6 +81,9 @@ class PhoneStatusProcessor
       const proto::AppStreamUpdate app_stream_update) override;
   void OnAppListUpdateReceived(
       const proto::AppListUpdate app_list_update) override;
+  void OnAppListIncrementalUpdateReceived(
+      const proto::AppListIncrementalUpdate app_list_incremental_update)
+      override;
 
   // MultiDeviceSetupClient::Observer:
   void OnHostStatusChanged(
