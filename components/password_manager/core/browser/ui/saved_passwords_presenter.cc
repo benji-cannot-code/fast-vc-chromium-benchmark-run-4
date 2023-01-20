e23355c0ea01776a59d1827fcbbc493b31e9f216
Andrii Natiahlyi <natiahlyi@google.com>
2023-01-20 09:13:33 +0000

    Stop emitting to PasswordManager.PasswordEditUpdatedValues
    For some time now, SavedPasswordsPresenter::EditSavedCredentials
    is used not only by "password edit dialog", so the metrics is not
    accurate.
    
    Change-Id: I03e4d06e71cddb257c0c8438f1f66f88ab4f8905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177332
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094932}

--- components/password_manager/core/browser/ui/saved_passwords_presenter.cc
+++ components/password_manager/core/browser/ui/saved_passwords_presenter.cc
@@ -334,8 +334,6 @@ SavedPasswordsPresenter::EditSavedCredentials(
   // Nothing changed.
   if (!username_changed && !password_changed && !note_changed &&
       !issues_changed) {
-    password_manager::metrics_util::LogPasswordEditResult(username_changed,
-                                                          password_changed);
     return EditResult::kNothingChanged;
   }
 
@@ -384,8 +382,6 @@ SavedPasswordsPresenter::EditSavedCredentials(
     }
   }
 
-  password_manager::metrics_util::LogPasswordEditResult(username_changed,
-                                                        password_changed);
   return EditResult::kSuccess;
 }
 
--- components/password_manager/core/browser/ui/saved_passwords_presenter_unittest.cc
+++ components/password_manager/core/browser/ui/saved_passwords_presenter_unittest.cc
@@ -307,10 +307,6 @@ TEST_F(SavedPasswordsPresenterTest, EditOnlyUsername) {
       store().stored_passwords(),
       ElementsAre(Pair(form.signon_realm, ElementsAre(updated_username))));
 
-  histogram_tester.ExpectUniqueSample(
-      "PasswordManager.PasswordEditUpdatedValues",
-      metrics_util::PasswordEditUpdatedValues::kUsername, 1);
-
   presenter().RemoveObserver(&observer);
 }
 
@@ -405,9 +401,6 @@ TEST_F(SavedPasswordsPresenterTest, EditOnlyPassword) {
   EXPECT_THAT(
       store().stored_passwords(),
       ElementsAre(Pair(form.signon_realm, ElementsAre(updated_password))));
-  histogram_tester.ExpectUniqueSample(
-      "PasswordManager.PasswordEditUpdatedValues",
-      metrics_util::PasswordEditUpdatedValues::kPassword, 1);
 
   presenter().RemoveObserver(&observer);
 }
@@ -601,9 +594,6 @@ TEST_F(SavedPasswordsPresenterTest, EditUsernameAndPassword) {
   RunUntilIdle();
   EXPECT_THAT(store().stored_passwords(),
               ElementsAre(Pair(form.signon_realm, ElementsAre(updated_both))));
-  histogram_tester.ExpectBucketCount(
-      "PasswordManager.PasswordEditUpdatedValues",
-      metrics_util::PasswordEditUpdatedValues::kBoth, 1);
 
   presenter().RemoveObserver(&observer);
 }
@@ -666,9 +656,6 @@ TEST_F(SavedPasswordsPresenterTest, EditPasswordWithoutChanges) {
             presenter().EditSavedCredentials(CredentialUIEntry(form),
                                              CredentialUIEntry(form)));
   RunUntilIdle();
-  histogram_tester.ExpectBucketCount(
-      "PasswordManager.PasswordEditUpdatedValues",
-      metrics_util::PasswordEditUpdatedValues::kNone, 1);
 
   presenter().RemoveObserver(&observer);
 }
