08ab835ff9ae96e4de822a70f4229db6afae43e9
Xinghui Lu <xinghuilu@chromium.org>
2023-01-21 00:02:12 +0000

    [DownloadBubble] Show origin for background fetch
    In the old download UI, the origin will be shown if
    ShouldPromoteOrigin is true. Add the same check in the new UI.
    
    Before: http://screen/588E32Z4BEEfQh6
    After: http://screen/BpaU2PWyXmQzjvr
    
    Bug: 1399360
    Change-Id: Ia12c75f82374864857b9c3be8ac5081792ceee1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167009
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095283}

--- chrome/browser/download/download_ui_model.cc
+++ chrome/browser/download/download_ui_model.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/browser.h"
+#include "components/url_formatter/elide_url.h"
 #include "ui/views/vector_icons.h"
 #endif
 
@@ -240,6 +241,21 @@ DownloadUIModel::BubbleStatusTextBuilder::GetProgressSizesString() const {
 std::u16string DownloadUIModel::GetStatusText() const {
   return status_text_builder_->GetStatusText(GetState());
 }
+
+#if !BUILDFLAG(IS_ANDROID)
+std::u16string DownloadUIModel::GetStatusTextForLabel(
+    const gfx::FontList& font_list,
+    float available_pixel_width) const {
+  if (!ShouldPromoteOrigin()) {
+    return GetStatusText();
+  }
+  // TODO(crbug.com/1409167): Avoid calling the deprecated function.
+  const GURL url = GetOriginalURL().DeprecatedGetOriginAsURL();
+  return url.is_valid()
+             ? url_formatter::ElideUrl(url, font_list, available_pixel_width)
+             : GetStatusText();
+}
+#endif
 
 std::u16string DownloadUIModel::StatusTextBuilderBase::GetStatusText(
     download::DownloadItem::DownloadState state) const {
