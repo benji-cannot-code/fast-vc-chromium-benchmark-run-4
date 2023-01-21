bed0ed6cef4cd32b4249dc55e41476a7b30c3fec
Lei Zhang <thestig@chromium.org>
2023-01-21 05:35:37 +0000

    Fix dangling pointers in TestPrintRenderFrame.
    PrintBrowserTest owns TestPrintRenderFrame objects, which in turn has
    raw_ptrs to some //content objects. These pointers hang around for too
    long and point to objects for a tab after the tab went away during tear
    down. Fix this issue by freeing the TestPrintRenderFrame objects in
    PrintBrowserTest::TearDownOnMainThread() before all the tabs close.
    
    Bug: 1409186
    Change-Id: Iaa2061311b69a818d605ecb6c7c684efbc2cfd8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4184949
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095338}

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -459,8 +459,8 @@ class TestPrintRenderFrame
   }
 
  private:
-  raw_ptr<content::RenderFrameHost, DanglingUntriaged> frame_host_;
-  raw_ptr<content::WebContents, DanglingUntriaged> web_contents_;
+  raw_ptr<content::RenderFrameHost> frame_host_;
+  raw_ptr<content::WebContents> web_contents_;
   const int document_cookie_;
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
   base::RepeatingClosure msg_callback_;
@@ -880,6 +880,9 @@ class PrintBrowserTest : public InProcessBrowserTest {
   }
 
   void TearDownOnMainThread() override {
+    // Remove map of objects pointing to //content objects before they go away.
+    frame_content_.clear();
+
     SetShowPrintErrorDialogForTest(base::NullCallback());
     InProcessBrowserTest::TearDownOnMainThread();
   }
