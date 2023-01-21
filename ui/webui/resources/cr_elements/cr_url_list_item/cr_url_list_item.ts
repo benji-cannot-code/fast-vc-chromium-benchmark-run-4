5b4e928ded74d668ae131c13fb07a0781bf978a6
dpapad <dpapad@chromium.org>
2023-01-21 16:22:09 +0000

    WebUI: Add dedicated ts_library() for ui/webui/resources/cr_elements/
     - Removing //ui/webui/resources:preprocess target and updating all
       references to use //ui/webui/resources:library instead.
     - Adding new //ui/webui/resources/cr_elements:build_ts target, via
       build_cr_component()
     - Changing build_cr_components()'s underlying html_to_wrapper()
       target to use scheme=relative.
     - Modifying //ui/webui/resources:library target to only have deps and
       no input_files so that its removal can happen in an incremental
       fashion.
     - Converting a few import URLs from absolute to relative as required
       by the new build design.
    
    After this change, the generated folder
    gen/ui/webui/resources/preprocessed/ no longer holds any .ts, .css or
    .html files, only transpiled .js files and d.ts definition files
    instead. It will be renamed to 'tsc' in a follow-up CL.
    
    This is part of the effort to break apart ui/webui/resources:library to
    two separate targets, ui/webui/resources/cr_elements:build_ts and
    ui/webui/resources/js:build_ts.
    
    Bug: 1402829,1340376
    Change-Id: Ib4c6fd4dd4b126c338d33a27ec7999d3bcea74b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181864
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095384}

--- ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
+++ ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
@@ -3,13 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_icons.css.js';
-import '//resources/cr_elements/cr_shared_vars.css.js';
+import '../cr_icons.css.js';
+import '../cr_shared_vars.css.js';
 
 import {getFaviconForPageURL} from '//resources/js/icon.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {MouseHoverableMixin} from 'chrome://resources/cr_elements/mouse_hoverable_mixin.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
+
+import {MouseHoverableMixin} from '../mouse_hoverable_mixin.js';
 
 import {getTemplate} from './cr_url_list_item.html.js';
 
