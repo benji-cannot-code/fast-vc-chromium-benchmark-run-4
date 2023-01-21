7bf5633ac7a0410655cbaa5bf8e2861b6c71d225
Rakina Zata Amni <rakina@chromium.org>
2023-01-21 12:59:25 +0000

    Disable kAvoidUnnecessaryNavigationCancellations by default and add debugging
    We're seeing crashes when destructing speculative RFH from
    RenderFrameHostManager::RestorePage() which might be caused by
    crrev.com/c/4060247. Disable the flag that enables the changes in that
    CL (we'll re-enable the flag for 50/50 of Canary/Dev via Finch) and
    also add debugging to investigate the crash further.
    
    Bug: 1407526
    Change-Id: If1b7167f2b01994fad8446aaaacc0ea680fee090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4180144
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095352}

--- content/common/features.h
+++ content/common/features.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/feature_list.h"
+#include "content/common/content_export.h"
 
 namespace content {
 
@@ -15,9 +16,10 @@ namespace content {
 
 // When enabled, stops canceling navigation when another navigation commits or
 // starts. This supports the same goal as kQueueNavigationsWhileWaitingForCommit
-// but for the non-queueing parts, and is enabled by default.
+// but for the non-queueing parts, and is disabled by default.
 // See https://crbug.com/838348 and https://crbug.com/1220337.
-BASE_DECLARE_FEATURE(kAvoidUnnecessaryNavigationCancellations);
+// Exported for tests.
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kAvoidUnnecessaryNavigationCancellations);
 
 #if BUILDFLAG(IS_ANDROID)
 // Unifies RenderWidgetHostViewAndroid with the other platforms in their usage
