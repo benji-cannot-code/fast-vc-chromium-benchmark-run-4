25a6a9c34335750aa73f1adbca9fcf35f6fa5ee8
Wei4 Wang <wei4.wang@intel.com>
2023-01-23 17:57:44 +0000

    [ComputePressure] Send PressureUpdate from PressureManagerImpl to PressureObserverManager directly
    Currently, PressureUpdate is sent from PressureManagerImpl to
    PressureServiceimpl firstly and then sent to PressureObserverManager.
    However, PressureUpdate will not be filtered in PressureServiceImpl
    anymore. So PressureUpdate can be sent from PressureManagerImpl
    to PressureObserverManager directly.
    
    This Cl also removes PressureServiceimpl, because it is no longer
    needed.
    
    Bug: 1391848
    Change-Id: Iee01b9dd66246f659bb92ee307b3fc64b0962925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4126666
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095704}

--- third_party/blink/web_tests/external/wpt/resources/chromium/mock-pressure-service.js
+++ third_party/blink/web_tests/external/wpt/resources/chromium/mock-pressure-service.js
@@ -1,12 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import {PressureManager, PressureManagerReceiver, PressureStatus} from '/gen/services/device/public/mojom/pressure_manager.mojom.m.js'
 import {PressureFactor, PressureState} from '/gen/services/device/public/mojom/pressure_update.mojom.m.js'
-import {PressureService, PressureServiceReceiver, PressureStatus} from '/gen/third_party/blink/public/mojom/compute_pressure/pressure_service.mojom.m.js'
 
 class MockPressureService {
   constructor() {
-    this.receiver_ = new PressureServiceReceiver(this);
+    this.receiver_ = new PressureManagerReceiver(this);
     this.interceptor_ =
-        new MojoInterfaceInterceptor(PressureService.$interfaceName);
+        new MojoInterfaceInterceptor(PressureManager.$interfaceName);
     this.interceptor_.oninterfacerequest = e => {
       this.receiver_.$.bindHandle(e.handle);
     };
@@ -48,7 +48,7 @@ class MockPressureService {
     this.updatesDelivered_ = 0;
   }
 
-  async bindObserver(observer) {
+  async addClient(observer) {
     if (this.observer_ !== null)
       throw new Error('BindObserver() has already been called');
 
@@ -90,7 +90,7 @@ class MockPressureService {
       this.pressureUpdate_.timestamp = {
         internalValue: BigInt((new Date().getTime() + epochDeltaInMs) * 1000)
       };
-      this.observer_.onUpdate(this.pressureUpdate_);
+      this.observer_.onPressureUpdated(this.pressureUpdate_);
       this.updatesDelivered_++;
     }, timeout);
   }
