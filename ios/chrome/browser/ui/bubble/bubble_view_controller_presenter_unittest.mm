69fdbc462000ed68309d1c7bd52df30ee7545d74
Federica Germinario <fedegermi@google.com>
2023-01-23 12:29:00 +0000

    [iOS] Expose BubbleViewControllerPresenter internals in +private.h
    Creation of bubble_view_controller_presenter+private.h where some
    properties are exposed.
    
    Fixed: 1379035
    Change-Id: I3a3ddf621387bd58af6a6cfae3a4ae3099a4091d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181335
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095584}

--- ios/chrome/browser/ui/bubble/bubble_view_controller_presenter_unittest.mm
+++ ios/chrome/browser/ui/bubble/bubble_view_controller_presenter_unittest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/bubble/bubble_unittest_util.h"
 #import "ios/chrome/browser/ui/bubble/bubble_view.h"
 #import "ios/chrome/browser/ui/bubble/bubble_view_controller.h"
+#import "ios/chrome/browser/ui/bubble/bubble_view_controller_presenter+private.h"
 #import "ios/chrome/browser/ui/bubble/bubble_view_controller_presenter.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
@@ -17,19 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-@interface BubbleViewControllerPresenter (Testing)
-
-// Underlying bubble view controller managed by the presenter.
-@property(nonatomic, strong) BubbleViewController* bubbleViewController;
-
-// Underlying timer used to dismiss the bubble automatically.
-@property(nonatomic, strong) NSTimer* bubbleDismissalTimer;
-
-// Underlying timer used to mark the user as no longer engaged automatically.
-@property(nonatomic, strong) NSTimer* engagementTimer;
-
-@end
 
 // Test fixture to test the BubbleViewControllerPresenter.
 class BubbleViewControllerPresenterTest : public PlatformTest {
