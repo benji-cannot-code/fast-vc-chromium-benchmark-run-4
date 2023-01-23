32852aaf414e0f2d32147f3985b37cf3e9b628af
Paul Semel <paulsemel@chromium.org>
2023-01-23 19:12:20 +0000

    blink: triage mojo::Remote in PresentationController
    Bug: 1381979
    Change-Id: Iaafea03203a5d718db9580387bef2db1a1270459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4187442
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095741}

--- third_party/blink/renderer/modules/presentation/presentation_controller.h
+++ third_party/blink/renderer/modules/presentation/presentation_controller.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PRESENTATION_PRESENTATION_CONTROLLER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_PRESENTATION_PRESENTATION_CONTROLLER_H_
 
-#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/presentation/presentation.mojom-blink.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_url.h"
@@ -17,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/gc_plugin_ignore.h"
@@ -68,7 +68,7 @@ class MODULES_EXPORT PresentationController
   // Returns a reference to the PresentationService remote, requesting the
   // remote service if needed. May return an invalid remote if the associated
   // Document is detached.
-  mojo::Remote<mojom::blink::PresentationService>& GetPresentationService();
+  HeapMojoRemote<mojom::blink::PresentationService>& GetPresentationService();
 
   // Returns the PresentationAvailabilityState owned by |this|, creating it if
   // needed. Always non-null.
@@ -106,8 +106,8 @@ class MODULES_EXPORT PresentationController
   HeapHashSet<WeakMember<ControllerPresentationConnection>> connections_;
 
   // Holder of the Mojo connection to the PresentationService remote.
-  GC_PLUGIN_IGNORE("https://crbug.com/1381979")
-  mojo::Remote<mojom::blink::PresentationService> presentation_service_remote_;
+  HeapMojoRemote<mojom::blink::PresentationService>
+      presentation_service_remote_;
 
   // Lazily-initialized binding for mojom::blink::PresentationController. Sent
   // to |presentation_service_|'s implementation.
