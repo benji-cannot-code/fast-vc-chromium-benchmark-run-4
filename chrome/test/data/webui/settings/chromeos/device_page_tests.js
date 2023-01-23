8a7c89524d246806b7bbbbc04f5419a95cad4243
Ashley Prasad <ashleydp@google.com>
2023-01-23 20:48:41 +0000

    audio-settings: hide input section when there are no input devices
    Hide the div of the section with input devices when there are none.
    
    https://screenshot.googleplex.com/7KgPKmVprW6S87Z
    
    Bug: b:260277007
    Test: browser_tests --gtest_filter=*OSSettingsDevicePage*
    Change-Id: I13c044cbf36f6242a7bf32083c892da9575f4b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4182270
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095800}

--- chrome/test/data/webui/settings/chromeos/device_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/device_page_tests.js
@@ -698,18 +698,18 @@ suite('SettingsDevicePage', function() {
       const sectionHeader =
           audioPage.shadowRoot.querySelector('#audioInputTitle');
       assertTrue(isVisible(sectionHeader));
-      assertEquals('Input', sectionHeader.textContent);
+      assertEquals('Input', sectionHeader.textContent.trim());
       const deviceSubsectionHeader =
           audioPage.shadowRoot.querySelector('#audioInputDeviceLabel');
       assertTrue(isVisible(deviceSubsectionHeader));
-      assertEquals('Device', deviceSubsectionHeader.textContent);
+      assertEquals('Device', deviceSubsectionHeader.textContent.trim());
       const deviceSubsectionDropdown =
           audioPage.shadowRoot.querySelector('#audioInputDeviceDropdown');
       assertTrue(isVisible(deviceSubsectionDropdown));
       const inputGainSubsectionHeader =
           audioPage.shadowRoot.querySelector('#audioInputGainLabel');
       assertTrue(isVisible(inputGainSubsectionHeader), 'audioInputGainLabel');
-      assertEquals('Volume', inputGainSubsectionHeader.textContent);
+      assertEquals('Volume', inputGainSubsectionHeader.textContent.trim());
       const inputVolumeButton =
           audioPage.shadowRoot.querySelector('#audioInputGainMuteButton');
       assertTrue(isVisible(inputVolumeButton), 'audioInputGainMuteButton');
@@ -721,7 +721,8 @@ suite('SettingsDevicePage', function() {
               '#audioInputNoiseCancellationLabel');
       assertTrue(isVisible(noiseCancellationSubsectionHeader));
       assertEquals(
-          'Noise Cancellation', noiseCancellationSubsectionHeader.textContent);
+          'Noise Cancellation',
+          noiseCancellationSubsectionHeader.textContent.trim());
       const noiseCancellationToggle = audioPage.shadowRoot.querySelector(
           '#audioInputNoiseCancellationToggle');
       assertTrue(isVisible(noiseCancellationToggle));
@@ -897,6 +898,23 @@ suite('SettingsDevicePage', function() {
       inputDevices: [
         fakeCrosAudioConfig.fakeInternalMicActive,
       ],
+    };
+
+    /** @type {!AudioSystemProperties} */
+    const emptyInputDevicesFakeAudioSystemProperties = {
+      outputVolumePercent: 75,
+
+      /** @type {!MuteState} */
+      outputMuteState: crosAudioConfigMojomWebui.MuteState.kNotMuted,
+
+      /** @type {!Array<!AudioDevice>} */
+      outputDevices: [
+        fakeCrosAudioConfig.fakeSpeakerActive,
+        fakeCrosAudioConfig.fakeMicJackInactive,
+      ],
+
+      /** @type {!Array<!AudioDevice>} */
+      inputDevices: [],
     };
 
     /** @type {!AudioSystemProperties} */
@@ -1166,6 +1184,21 @@ suite('SettingsDevicePage', function() {
           fakeCrosAudioConfig.defaultFakeAudioSystemProperties.inputDevices
               .length,
           inputDeviceDropdown.length);
+
+      // Test empty input devices case.
+      crosAudioConfig.setAudioSystemProperties(
+          emptyInputDevicesFakeAudioSystemProperties);
+      await flushTasks();
+      assertTrue(!inputDeviceDropdown.value);
+      assertEquals(
+          emptyInputDevicesFakeAudioSystemProperties.inputDevices.length,
+          inputDeviceDropdown.length);
+
+      // If the input devices are empty, the input section should be hidden.
+      const inputDeviceSection = audioPage.shadowRoot.querySelector('#input');
+      assertFalse(isVisible(inputDeviceSection));
+      const outputDeviceSection = audioPage.shadowRoot.querySelector('#output');
+      assertTrue(isVisible(outputDeviceSection));
     });
 
     test('simulate setting active input device', async function() {
