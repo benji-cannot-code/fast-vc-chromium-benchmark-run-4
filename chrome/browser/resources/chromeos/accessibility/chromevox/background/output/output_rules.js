f19ae3e4d55472671a659c1720390120a7e9ca24
Anastasia Helfinstein <anastasi@google.com>
2023-01-23 20:28:58 +0000

    [ChromeVox] Calculate enterFormat in AncestryOutputRule
    Part of an incremental migration of logic from Output to OutputRule.
    
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:265481751
    Test: cq
    Change-Id: Icb1e310e8645714f88f96802e5eb1ffc00567094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4183065
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095787}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/output/output_rules.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/output/output_rules.js
@@ -142,6 +142,15 @@ export class AncestryOutputRule extends OutputRule {
       this.output_ = OutputFormatType.BRAILLE;
     }
   }
+
+  /** @return {string} */
+  get enterFormat() {
+    const rule = OutputRule.RULES[this.event_][this.role_][this.formatName_];
+    if (this.output_) {
+      return rule[this.output_];
+    }
+    return rule || '';
+  }
 }
 
 /**
