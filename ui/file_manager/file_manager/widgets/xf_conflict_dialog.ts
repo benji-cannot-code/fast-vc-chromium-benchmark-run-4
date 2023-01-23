f1208be8baa774840e6c5e406e8ab3a89adff97c
Noel Gordon <noel@chromium.org>
2023-01-23 04:09:20 +0000

    [conflict dialog] Update dialog for GAR4 ARIA conformance
    Conflict dialog 1) has no title, and 2) uses a <cr-dialog> element
    which assumes there is a title, and will focus that title when the
    dialog is shown. ARIA announces the empty title because it has the
    initial focus as "Heading 2 Heading 2".
    
    Fix this: remove <cr-dialog> #title element and all <dialog> aria-
    attributes that make ARIA tools read out irrelevant content.
    
    Instead, add tabindex="0" to the dialog #message element and focus
    it when the dialog is shown. On show, ARIA now reads out:
    
      "Dialog A file named ... exists. Do you want to replace it
       with the one you are moving?"
    
    Update the testDialogShowFocus test to check that the #message has
    the initial focus (ARIA announced on show), and check the <dialog>
    "aria-*" attributes are removed (block spurious announcements).
    
    Add an aria-modal="true" attribute to the <dialog> element per W3C
    ARIA role=dialog best-practice recommendations [1].
    
    [1] https://w3c.github.io/aria-practices/#dialog_roles_states_props
    
    Bug: b:255264604
    Test: browser_tests --gtest_filter="FileManagerJsTest.XfConflictDialog*"
    Change-Id: I9973b1da1554e06e2a959442b420f05924ae837b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179424
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095506}

--- ui/file_manager/file_manager/widgets/xf_conflict_dialog.ts
+++ ui/file_manager/file_manager/widgets/xf_conflict_dialog.ts
@@ -118,6 +118,26 @@ export class XfConflictDialog extends HTMLElement {
     this.action_ = '';
     this.checked_();
     this.dialog_.showModal();
+    this.setFocus_();
+  }
+
+  /**
+   * The conflict dialog has no title. Remove the <cr-dialog> title child that
+   * would focus, remove its <dialog> aria-labelledby and aria-describedby, so
+   * ARIA announces the #message element that is the initial focus.
+   *
+   * Per https://w3c.github.io/aria-practices/#dialog_roles_states_props, adds
+   * aria-modal='true' meaning all content outside the <dialog> is inert.
+   */
+  private setFocus_() {
+    this.dialog_.shadowRoot!.querySelector('#title')?.remove();
+
+    const element = this.getHtmlDialogElement();
+    element.setAttribute('aria-modal', 'true');
+    element.removeAttribute('aria-labelledby');
+    element.removeAttribute('aria-describedby');
+
+    this.getMessageElement().focus();
   }
 
   /**
@@ -125,6 +145,13 @@ export class XfConflictDialog extends HTMLElement {
    */
   getDialogElement(): CrDialogElement {
     return this.shadowRoot!.querySelector('#conflict-dialog')!;
+  }
+
+  /**
+   * Returns 'dialog' <dialog> element.
+   */
+  getHtmlDialogElement(): HTMLDialogElement {
+    return this.dialog_.getNative()!;
   }
 
   /**
@@ -156,6 +183,7 @@ export class XfConflictDialog extends HTMLElement {
     }
 
     this.toggleAttribute('checked', checked);
+    this.getCheckboxElement().focus();
   }
 
   /**
