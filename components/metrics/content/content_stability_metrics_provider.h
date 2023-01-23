636b298293cd4525ccf91b8eea6ad006d6789f46
Mike Wittman <wittman@chromium.org>
2023-01-23 22:57:21 +0000

    Revert "[code health] Remove host created notification use by ContentStabilityMetricsProvider"
    This reverts commit a8eb04ff2a5059b03c74cba6080ced2d2af2591a.
    
    Reason for revert: may be causing crashes on creation observation call
    
    Original change's description:
    > [code health] Remove host created notification use by ContentStabilityMetricsProvider
    >
    > Removes the use of the render process host created notification
    > in ContentStabilityMetricsProvider. Usage is replaced with the
    > RenderProcessHostCreationObserver.
    >
    > Bug: 357627
    > Change-Id: I7df0d465cf6c0c7c6b483b492328d6297403cb44
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181761
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Weilun Shi <sweilun@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095259}
    
    Bug: 357627, 1409290
    Change-Id: I41c48e2aae19b296e720164da410bf01c60b6c62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189879
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095880}

--- components/metrics/content/content_stability_metrics_provider.h
+++ components/metrics/content/content_stability_metrics_provider.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_child_process_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/render_process_host_creation_observer.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "components/crash/content/browser/crash_metrics_reporter_android.h"
@@ -36,7 +35,6 @@ class ContentStabilityMetricsProvider
 #if BUILDFLAG(IS_ANDROID)
       public crash_reporter::CrashMetricsReporter::Observer,
 #endif
-      public content::RenderProcessHostCreationObserver,
       public content::NotificationObserver {
  public:
   // |extensions_helper| is used to determine if a process corresponds to an
@@ -72,9 +70,6 @@ class ContentStabilityMetricsProvider
                            NotificationObserver);
   FRIEND_TEST_ALL_PREFIXES(ContentStabilityMetricsProviderTest,
                            ExtensionsNotificationObserver);
-
-  // content::RenderProcessHostCreationObserver:
-  void OnRenderProcessHostCreated(content::RenderProcessHost* host) override;
 
   // content::NotificationObserver:
   void Observe(int type,
