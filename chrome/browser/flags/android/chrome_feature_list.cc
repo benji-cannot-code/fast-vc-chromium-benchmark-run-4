baf22f76f5dbd24c04a2338b435ccf5ffdf66f63
Peilin Wang <peilinwang@google.com>
2023-01-23 17:11:42 +0000

    [Cleanup] Remove ToolbarPhoneOptimizations experiment code
    Bug: 1330626
    Change-Id: I5acca6818b50755d9a95225e81db1c08fc638958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4183119
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095679}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -287,7 +287,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kTestDefaultDisabled,
     &kTestDefaultEnabled,
     &kToolbarMicIphAndroid,
-    &kToolbarPhoneOptimizations,
     &kToolbarScrollAblationAndroid,
     &kTrustedWebActivityPostMessage,
     &kTrustedWebActivityQualityEnforcement,
@@ -923,10 +922,6 @@ BASE_FEATURE(kTestDefaultEnabled,
 BASE_FEATURE(kToolbarMicIphAndroid,
              "ToolbarMicIphAndroid",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kToolbarPhoneOptimizations,
-             "ToolbarPhoneOptimizations",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kToolbarScrollAblationAndroid,
              "ToolbarScrollAblationAndroid",
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -51,7 +51,6 @@ import org.chromium.base.TraceEvent;
 import org.chromium.chrome.browser.browser_controls.BrowserStateBrowserControlsVisibilityDelegate;
 import org.chromium.chrome.browser.device.DeviceClassManager;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.omnibox.LocationBar;
 import org.chromium.chrome.browser.omnibox.LocationBarCoordinator;
 import org.chromium.chrome.browser.omnibox.NewTabPageDelegate;
@@ -271,7 +270,6 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
     private boolean mOptionalButtonAnimationRunning;
     private int mUrlFocusTranslationX;
 
-    private boolean mOptimizationsEnabled;
     private boolean mDropdownListScrolled;
 
     // The following are some properties used during animation.  We use explicit property classes
@@ -487,9 +485,6 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
     protected void onNativeLibraryReady() {
         super.onNativeLibraryReady();
 
-        mOptimizationsEnabled =
-                ChromeFeatureList.isEnabled(ChromeFeatureList.TOOLBAR_PHONE_OPTIMIZATIONS);
-
         enableTabSwitchingResources();
         mHomeButton.setOnClickListener(this);
 
@@ -576,20 +571,9 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
         if (!mDisableLocationBarRelayout) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-            boolean changed;
-            if (mOptimizationsEnabled) {
-                // Without optimizations, layoutLocationBar sometimes calls
-                // updateLocationBarLayoutForExpansionAnimation, followed always by a second call to
-                // updateLocationBarLayoutForExpansionAnimation. The location bar shouldn't change
-                // between these two calls, so we can optimize by removing the call that is made
-                // sometimes.
-                changed = layoutLocationBarWithoutAnimationExpansion(
-                        MeasureSpec.getSize(widthMeasureSpec));
-                updateUrlExpansionAnimation();
-            } else {
-                changed = layoutLocationBar(MeasureSpec.getSize(widthMeasureSpec));
-                if (!isInTabSwitcherMode()) updateUrlExpansionAnimation();
-            }
+            boolean changed = layoutLocationBarWithoutAnimationExpansion(
+                    MeasureSpec.getSize(widthMeasureSpec));
+            updateUrlExpansionAnimation();
             if (!changed) return;
         } else {
             updateUnfocusedLocationBarLayoutParams();
@@ -1016,7 +1000,7 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
      */
     private void updateLocationBarLayoutForExpansionAnimation() {
         TraceEvent.begin("ToolbarPhone.updateLocationBarLayoutForExpansionAnimation");
-        if (mOptimizationsEnabled && isInTabSwitcherMode()) return;
+        if (isInTabSwitcherMode()) return;
 
         boolean isLocationBarShownInNTP = isLocationBarShownInNTP();
         FrameLayout.LayoutParams locationBarLayoutParams =
@@ -1827,12 +1811,6 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
                 // so that the final translation position is correct (since onMeasure updates
                 // won't happen in tab switcher mode). crbug.com/518795.
                 layoutLocationBar(getMeasuredWidth());
-                if (!mOptimizationsEnabled) {
-                    // We're in tab switcher mode here. updateUrlExpansionAnimation doesn't need to
-                    // get called because we don't see the url bar, and that this gets called as
-                    // soon as we exit tab switcher mode.
-                    updateUrlExpansionAnimation();
-                }
             }
 
             updateViewsForTabSwitcherMode();
