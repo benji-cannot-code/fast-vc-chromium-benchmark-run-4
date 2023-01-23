ec531317b804fb57be1971ca834dc33a4464925c
Yoshifumi Inoue <yosin@chromium.org>
2023-01-23 07:26:20 +0000

    Replace NOTREACHED() in text_suggestion_controller.cc
    This NOTREACHED() is known to hit, so replace it temporarily with
    base::debug::DumpWithoutCrashing() until reports go away and we can use
    `CHECK()` and `CHECK_EQ()` instead.
    
    Bug: 1409155
    Change-Id: I66b5dd7b6ee9ee747cef152d090964f9dfdcd156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4186977
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095524}

--- third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.cc
+++ third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "base/ranges/algorithm.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/renderer/core/clipboard/data_transfer.h"
@@ -211,12 +212,14 @@ void TextSuggestionController::HandlePotentialSuggestionTap(
     const PositionInFlatTree& caret_position) {
   if (!IsAvailable()) {
     // TODO(crbug.com/1054955): We should fix caller not to make this happens.
-    NOTREACHED();
+    // TODO(crbug.com/1409155): We should use `CHECK()`.
+    base::debug::DumpWithoutCrashing();
     return;
   }
   if (GetFrame() != GetDocument().GetFrame()) {
     // TODO(crbug.com/1054955): We should fix caller not to make this happens.
-    NOTREACHED();
+    // TODO(crbug.com/1409155): We should use `CHECK_EQ()`.
+    base::debug::DumpWithoutCrashing();
     return;
   }
 
