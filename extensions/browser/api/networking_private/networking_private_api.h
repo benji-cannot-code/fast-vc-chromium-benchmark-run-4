3e27167f60366f6cabfff22a5a9ea3994dede193
Matt Menke <mmenke@chromium.org>
2023-01-23 22:42:53 +0000

    Migrate EnabledNetworkTypesCallback to take a Value::List.
    It was taking a unique_ptr<Value>, which was never null. It's now
    preferred to pass Values by value, and using a Value::List is more
    typesafe.
    
    Also, implementations were using the deprecated Value::Append() method,
    which this CL switches to the new Value::List::Append().
    
    Bug: 1404827
    Change-Id: Ia4bcb569ec1947f1bdd18e037344cb3532159623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4186398
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095870}

--- extensions/browser/api/networking_private/networking_private_api.h
+++ extensions/browser/api/networking_private/networking_private_api.h
@@ -244,7 +244,7 @@ class NetworkingPrivateGetEnabledNetworkTypesFunction
   ResponseAction Run() override;
 
  private:
-  void Result(std::unique_ptr<base::Value> enabled_networks_onc_types);
+  void Result(base::Value::List enabled_networks_onc_types);
 };
 
 // Implements the chrome.networkingPrivate.getDeviceStates method.
