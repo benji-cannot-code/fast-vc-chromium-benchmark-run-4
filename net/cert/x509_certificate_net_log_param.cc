267f2af4beb65a890521b47e932e78680b143211
Matt Menke <mmenke@chromium.org>
2023-01-23 22:55:06 +0000

    Migrate Value::Append() calls in net/.
    Value::Append() is deprecated in favor of Value::List::Append().
    
    Bug: 1404827
    Change-Id: I4c5a7fb48e2620ac364278c08ffe4d758e787978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188978
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095876}

--- net/cert/x509_certificate_net_log_param.cc
+++ net/cert/x509_certificate_net_log_param.cc
@@ -17,12 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 base::Value NetLogX509CertificateList(const X509Certificate* certificate) {
-  base::Value certs(base::Value::Type::LIST);
+  base::Value::List certs;
   std::vector<std::string> encoded_chain;
   certificate->GetPEMEncodedChain(&encoded_chain);
   for (auto& pem : encoded_chain)
     certs.Append(std::move(pem));
-  return certs;
+  return base::Value(std::move(certs));
 }
 
 }  // namespace net
