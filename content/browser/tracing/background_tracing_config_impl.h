033b4ca9c56659b357265c845dd6666d1355cd50
Etienne Pierre-doray <etiennep@chromium.org>
2023-01-23 20:10:27 +0000

    [tracing] Delete unsupported rule category preset
    'category' preset on rule is only used for startup scenario, though
    it doesn't actually affect category settings; it's only used to identify
    startup rule. This CL gets rid of category field, and uses rule_id
    (derived from trigger_name) to identifying startup rule instead.
    
    Change-Id: I46d00f18d4d11659c45fbefdfd86cccce52a9f02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116437
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095772}

--- content/browser/tracing/background_tracing_config_impl.h
+++ content/browser/tracing/background_tracing_config_impl.h
@@ -52,9 +52,7 @@ class CONTENT_EXPORT BackgroundTracingConfigImpl
   }
 
   void AddPreemptiveRule(const base::Value::Dict& dict);
-  void AddReactiveRule(
-      const base::Value::Dict& dict,
-      BackgroundTracingConfigImpl::CategoryPreset category_preset);
+  void AddReactiveRule(const base::Value::Dict& dict);
   void AddSystemRule(const base::Value::Dict& dict);
 
   base::trace_event::TraceConfig GetTraceConfig() const;
--- content/browser/tracing/background_tracing_rule.cc
+++ content/browser/tracing/background_tracing_rule.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kConfigRuleKey[] = "rule";
-const char kConfigCategoryKey[] = "category";
 const char kConfigRuleTriggerNameKey[] = "trigger_name";
 const char kConfigRuleTriggerDelay[] = "trigger_delay";
 const char kConfigRuleTriggerChance[] = "trigger_chance";
@@ -85,12 +84,6 @@ base::Value::Dict BackgroundTracingRule::ToDict() const {
 
   if (rule_id_ != GetDefaultRuleId()) {
     dict.Set(kConfigRuleIdKey, rule_id_);
-  }
-
-  if (category_preset_ != BackgroundTracingConfigImpl::CATEGORY_PRESET_UNSET) {
-    dict.Set(
-        kConfigCategoryKey,
-        BackgroundTracingConfigImpl::CategoryPresetToString(category_preset_));
   }
 
   if (is_crash_) {
