fa895160afba130d20c699273d0fa17f36d6bded
Patrick Monette <pmonette@chromium.org>
2023-01-23 23:56:52 +0000

    Add BrowserChildProcessHostProxy
    This new proxy class will allow polices in the PM graph to
    communicate back with BrowserChildProcessHost instances.
    
    Bug: 1406969
    Change-Id: I21e436eb70c81e3a3f75a65e74c058c8e4bd184f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4162196
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095909}

--- components/performance_manager/graph/process_node_impl.h
+++ components/performance_manager/graph/process_node_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/graph/node_attached_data.h"
 #include "components/performance_manager/graph/node_base.h"
 #include "components/performance_manager/graph/properties.h"
+#include "components/performance_manager/public/browser_child_process_host_proxy.h"
 #include "components/performance_manager/public/graph/process_node.h"
 #include "components/performance_manager/public/mojom/coordination_unit.mojom.h"
 #include "components/performance_manager/public/mojom/v8_contexts.mojom.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
 
 namespace performance_manager {
@@ -63,7 +65,9 @@ class ProcessNodeImpl
   explicit ProcessNodeImpl(RenderProcessHostProxy render_process_host_proxy);
 
   // Constructor for a non-renderer child process.
-  explicit ProcessNodeImpl(content::ProcessType process_type);
+  ProcessNodeImpl(
+      content::ProcessType process_type,
+      BrowserChildProcessHostProxy browser_child_process_host_proxy);
 
   ProcessNodeImpl(const ProcessNodeImpl&) = delete;
   ProcessNodeImpl& operator=(const ProcessNodeImpl&) = delete;
@@ -158,7 +162,11 @@ class ProcessNodeImpl
   }
 
   const RenderProcessHostProxy& render_process_host_proxy() const {
-    return render_process_host_proxy_;
+    return absl::get<RenderProcessHostProxy>(child_process_host_proxy_);
+  }
+
+  const BrowserChildProcessHostProxy& browser_child_process_host_proxy() const {
+    return absl::get<BrowserChildProcessHostProxy>(child_process_host_proxy_);
   }
 
   base::TaskPriority priority() const {
@@ -224,6 +232,8 @@ class ProcessNodeImpl
   uint64_t GetResidentSetKb() const override;
   RenderProcessHostId GetRenderProcessHostId() const override;
   const RenderProcessHostProxy& GetRenderProcessHostProxy() const override;
+  const BrowserChildProcessHostProxy& GetBrowserChildProcessHostProxy()
+      const override;
   base::TaskPriority GetPriority() const override;
   ContentTypes GetHostedContentTypes() const override;
 
@@ -253,9 +263,11 @@ class ProcessNodeImpl
   const content::ProcessType process_type_
       GUARDED_BY_CONTEXT(sequence_checker_);
 
-  // This is used during frame node initialization.
-  // TODO(siggi): It seems unnecessary to initialize this at creation time?
-  const RenderProcessHostProxy render_process_host_proxy_;
+  // The proxy that allows access to either the RenderProcessHost or the
+  // BrowserChildProcessHost associated with this process, if `this` is a
+  // process node for a child process (process_type() != PROCESS_TYPE_BROWSER).
+  const absl::variant<RenderProcessHostProxy, BrowserChildProcessHostProxy>
+      child_process_host_proxy_;
 
   ObservedProperty::NotifiesOnlyOnChanges<
       bool,
