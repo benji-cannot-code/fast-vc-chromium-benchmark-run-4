2e868f9f6eeb3e88a34bd67b8f3d1b60d53c80f1
Daniel Rubery <drubery@chromium.org>
2023-01-23 18:53:08 +0000

    Remove chrome://settings and Safety Check cleanup integrations
    Since this feature is being deprecated, start by removing the clean
    up triggers in UX.
    
    Bug: 1407233
    Change-Id: Ie41e9319c0b20a87ef4ff6dd7a537e4e6706565f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174588
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095733}

--- chrome/browser/ui/webui/settings/safety_check_handler.h
+++ chrome/browser/ui/webui/settings/safety_check_handler.h
@@ -30,19 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
 
-#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-#include "chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_win.h"
-#include "chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_scanner_results_win.h"
-#endif
-
 // Delegate for accessing external timestamps, overridden for tests.
 class TimestampDelegate {
  public:
   virtual ~TimestampDelegate() = default;
   virtual base::Time GetSystemTime();
-#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  virtual base::Time FetchChromeCleanerScanCompletionTimestamp();
-#endif
 };
 
 // Settings page UI handler that checks four areas of browser safety:
@@ -50,9 +42,6 @@ class TimestampDelegate {
 // software.
 class SafetyCheckHandler
     : public settings::SettingsPageUIHandler,
-#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-      public safe_browsing::ChromeCleanerController::Observer,
-#endif
       public password_manager::BulkLeakCheckServiceInterface::Observer,
       public password_manager::InsecureCredentialsManager::Observer {
  public:
@@ -135,28 +124,6 @@ class SafetyCheckHandler
   std::u16string GetStringForParentRan(base::Time safety_check_completion_time,
                                        base::Time system_time);
 
-#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  // Constructs the string for the Chrome cleaner 'safe' state which depicts
-  // how long ago its last check ran.
-  std::u16string GetStringForChromeCleanerRan();
-  std::u16string GetStringForChromeCleanerRan(base::Time cct_completion_time,
-                                              base::Time system_time);
-
-  // safe_browsing::ChromeCleanerController::Observer overrides.
-  void OnIdle(
-      safe_browsing::ChromeCleanerController::IdleReason idle_reason) override;
-  void OnReporterRunning() override;
-  void OnScanning() override;
-  void OnInfected(bool is_powered_by_partner,
-                  const safe_browsing::ChromeCleanerScannerResults&
-                      scanner_results) override;
-  void OnCleaning(bool is_powered_by_partner,
-                  const safe_browsing::ChromeCleanerScannerResults&
-                      scanner_results) override;
-  void OnRebootRequired() override;
-  void OnRebootFailed() override;
-#endif
-
  protected:
   SafetyCheckHandler(
       std::unique_ptr<safety_check::UpdateCheckHelper> update_helper,
@@ -208,11 +175,6 @@ class SafetyCheckHandler
   // that case, if any of those were re-enabled.
   void CheckExtensions();
 
-#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  // Checks for unwanted software via the Chrome Cleanup Tool. Only on Windows.
-  void CheckChromeCleaner();
-#endif
-
   // Callbacks that get triggered when each check completes.
   void OnUpdateCheckResult(UpdateStatus status);
   void OnPasswordsCheckResult(PasswordsStatus status,
@@ -224,9 +186,6 @@ class SafetyCheckHandler
                                Blocklisted blocklisted,
                                ReenabledUser reenabled_user,
                                ReenabledAdmin reenabled_admin);
-#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  void OnChromeCleanerCheckResult(ChromeCleanerResult result);
-#endif
 
   // Methods for building user-visible strings based on the safety check
   // state.
@@ -242,11 +201,6 @@ class SafetyCheckHandler
                                         Blocklisted blocklisted,
                                         ReenabledUser reenabled_user,
                                         ReenabledAdmin reenabled_admin);
-#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  std::u16string GetStringForChromeCleaner(ChromeCleanerStatus status,
-                                           base::Time cct_completion_time,
-                                           base::Time system_time);
-#endif
 
   // A generic error state often includes the offline state. This method is used
   // as a callback for |UpdateCheckHelper| to check connectivity.
