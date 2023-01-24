bfc6dafa62e3d2f1c6ce6c529f629fd294b7846e
Florian Leimgruber <fleimgruber@google.com>
2023-01-24 21:18:44 +0000

    Add feature to enable ContactInfoSyncBridge in transport mode.
    Introduces a custom ModelTypeController for the CONTACT_INFO type that
    allows it to run in transport mode. This behavior is guarded by a Finch
    feature.
    
    The same delegate and same storage layer is used in transport and full-
    sync mode.
    
    Bug: 1348294
    Change-Id: I9dedd1ab91f05ad49e888ad4d68391da2d70db49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161669
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096368}

---
+++ components/autofill/core/browser/webdata/contact_info_model_type_controller.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_CONTACT_INFO_MODEL_TYPE_CONTROLLER_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_CONTACT_INFO_MODEL_TYPE_CONTROLLER_H_
+
+#include "components/sync/driver/model_type_controller.h"
+
+namespace autofill {
+
+class ContactInfoModelTypeController : public syncer::ModelTypeController {
+ public:
+  ContactInfoModelTypeController(
+      std::unique_ptr<syncer::ModelTypeControllerDelegate>
+          delegate_for_full_sync_mode,
+      std::unique_ptr<syncer::ModelTypeControllerDelegate>
+          delegate_for_transport_mode);
+
+  ContactInfoModelTypeController(const ContactInfoModelTypeController&) =
+      delete;
+  ContactInfoModelTypeController& operator=(
+      const ContactInfoModelTypeController&) = delete;
+
+  // ModelTypeController overrides.
+  bool ShouldRunInTransportOnlyMode() const override;
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_CONTACT_INFO_MODEL_TYPE_CONTROLLER_H_
--- components/browser_sync/sync_api_component_factory_impl.cc
+++ components/browser_sync/sync_api_component_factory_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/autofill_wallet_sync_bridge.h"
 #include "components/autofill/core/browser/webdata/autofill_wallet_usage_data_sync_bridge.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
+#include "components/autofill/core/browser/webdata/contact_info_model_type_controller.h"
 #include "components/autofill/core/browser/webdata/contact_info_sync_bridge.h"
 #include "components/browser_sync/active_devices_provider_impl.h"
 #include "components/browser_sync/browser_sync_client.h"
@@ -227,12 +228,21 @@ SyncApiComponentFactoryImpl::CreateCommonDataTypeControllers(
     // disabled.
     if (base::FeatureList::IsEnabled(syncer::kSyncEnableContactInfoDataType) &&
         !disabled_types.Has(syncer::CONTACT_INFO)) {
-      controllers.push_back(std::make_unique<syncer::ModelTypeController>(
-          syncer::CONTACT_INFO,
-          std::make_unique<syncer::ProxyModelTypeControllerDelegate>(
-              db_thread_, base::BindRepeating(
-                              &ContactInfoDelegateFromDataService,
-                              base::RetainedRef(web_data_service_on_disk_)))));
+      // The same delegate is used for full sync and transport mode.
+      controllers.push_back(
+          std::make_unique<autofill::ContactInfoModelTypeController>(
+              /*delegate_for_full_sync_mode=*/
+              std::make_unique<syncer::ProxyModelTypeControllerDelegate>(
+                  db_thread_,
+                  base::BindRepeating(
+                      &ContactInfoDelegateFromDataService,
+                      base::RetainedRef(web_data_service_on_disk_))),
+              /*delegate_for_transport_mode=*/
+              std::make_unique<syncer::ProxyModelTypeControllerDelegate>(
+                  db_thread_,
+                  base::BindRepeating(
+                      &ContactInfoDelegateFromDataService,
+                      base::RetainedRef(web_data_service_on_disk_)))));
     }
 
     // Wallet data sync is enabled by default. Register unless explicitly
