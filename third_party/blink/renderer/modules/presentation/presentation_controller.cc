eb4f7f3170a03eab0b77898a1013aad71d586322
Olesia Marukhno <olesiamarukhno@google.com>
2023-01-24 13:03:48 +0000

    Revert "blink: triage mojo::Remote in PresentationController"
    This reverts commit 04c7ba42639eb195fb21f6a6e69e5ae99341b7b4.
    
    Reason for revert: The CL caused test failures (http://crbug.com/1409687) and PresentationConnectionCallbacksTest.HandleReconnect is flaky on Linux Chromium OS ASan LSan Tests (1):
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29. 
    
    Original change's description:
    > blink: triage mojo::Remote in PresentationController
    >
    > Bug: 1381979
    > Change-Id: Iaafea03203a5d718db9580387bef2db1a1270459
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4187442
    > Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    > Commit-Queue: Paul Semel <paulsemel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095741}
    
    Bug: 1381979
    Change-Id: Ifeeffb073579a949ea81bb6179cbbc54ece299af
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4191904
    Owners-Override: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096126}

--- third_party/blink/renderer/modules/presentation/presentation_controller.cc
+++ third_party/blink/renderer/modules/presentation/presentation_controller.cc
@@ -18,7 +18,6 @@ namespace blink {
 
 PresentationController::PresentationController(LocalDOMWindow& window)
     : Supplement<LocalDOMWindow>(window),
-      presentation_service_remote_(&window),
       presentation_controller_receiver_(this, &window) {}
 
 PresentationController::~PresentationController() = default;
@@ -46,7 +45,6 @@ PresentationController* PresentationController::FromContext(
 }
 
 void PresentationController::Trace(Visitor* visitor) const {
-  visitor->Trace(presentation_service_remote_);
   visitor->Trace(presentation_controller_receiver_);
   visitor->Trace(presentation_);
   visitor->Trace(connections_);
@@ -142,9 +140,9 @@ PresentationController::FindExistingConnection(
   return nullptr;
 }
 
-HeapMojoRemote<mojom::blink::PresentationService>&
+mojo::Remote<mojom::blink::PresentationService>&
 PresentationController::GetPresentationService() {
-  if (!presentation_service_remote_.is_bound() && GetSupplementable()) {
+  if (!presentation_service_remote_ && GetSupplementable()) {
     scoped_refptr<base::SingleThreadTaskRunner> task_runner =
         GetSupplementable()->GetTaskRunner(TaskType::kPresentation);
     GetSupplementable()->GetBrowserInterfaceBroker().GetInterface(
