76ae4c2f3f800c803e6bc104fd88cf8055dabaf4
Matt Menke <mmenke@chromium.org>
2023-01-24 01:58:21 +0000

    Migrate Value::Append() calls in chrome/browser/devtools/.
    Value::Append() is deprecated in favor of Value::List::Append(). Also
    migrate some nearby Value::Set*Key() calls to Value::Dict::Set(), for
    the same reason.
    
    Bug: 1404827
    Change-Id: I6beb5a52c436088841084b23d23668784011d3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189084
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095967}

--- chrome/browser/devtools/devtools_targets_ui.h
+++ chrome/browser/devtools/devtools_targets_ui.h
@@ -11,11 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback.h"
+#include "base/values.h"
 #include "chrome/browser/devtools/device/devtools_android_bridge.h"
-
-namespace base {
-class Value;
-}
 
 class Profile;
 
@@ -52,7 +49,7 @@ class DevToolsTargetsUIHandler {
   virtual void ForceUpdate();
 
  protected:
-  base::Value Serialize(content::DevToolsAgentHost* host);
+  base::Value::Dict Serialize(content::DevToolsAgentHost* host);
   void SendSerializedTargets(const base::Value& list);
 
   using TargetMap =
