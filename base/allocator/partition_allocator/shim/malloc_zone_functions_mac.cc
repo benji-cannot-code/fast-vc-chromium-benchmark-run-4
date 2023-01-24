4cc5b35fb38addeb8d8c12a6af200b6c8315dafc
Hans Wennborg <hans@chromium.org>
2023-01-24 13:08:44 +0000

    Include missing header for std::is_pod
    Bug: 1409829
    Change-Id: I483674dc8359dc1130c3151854e463c4777b9a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188523
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096130}

--- base/allocator/partition_allocator/shim/malloc_zone_functions_mac.cc
+++ base/allocator/partition_allocator/shim/malloc_zone_functions_mac.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/shim/malloc_zone_functions_mac.h"
 
 #include <atomic>
+#include <type_traits>
 
 #include "base/synchronization/lock.h"
 
