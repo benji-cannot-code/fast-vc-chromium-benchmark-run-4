90997bc8157a6912496e2ca06cc7e64f3089745d
Cole Horvitz <colehorvitz@chromium.org>
2023-01-24 00:07:22 +0000

    Bookmarks: Migrate TestTimerProxy to TypeScript
    Bug: 1401496
    Change-Id: I4ada8106afb37b45061310327a7470b26cfd447a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181777
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095916}

--- chrome/test/data/webui/bookmarks/dnd_manager_test.ts
+++ chrome/test/data/webui/bookmarks/dnd_manager_test.ts
@@ -4,10 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {BookmarkElement, BookmarkManagerApiProxyImpl, BookmarksAppElement, BookmarksFolderNodeElement, BookmarksItemElement, BookmarksListElement, BrowserProxyImpl, DndManager, DragInfo, overrideFolderOpenerTimeoutDelay, setDebouncerForTesting} from 'chrome://bookmarks/bookmarks.js';
-import {assertNotReached} from 'chrome://resources/js/assert_ts.js';
 import {middleOfNode, topLeftOfNode} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertDeepEquals, assertEquals, assertFalse, assertNotReached, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
 import {TestBookmarkManagerApiProxy} from './test_bookmark_manager_api_proxy.js';
