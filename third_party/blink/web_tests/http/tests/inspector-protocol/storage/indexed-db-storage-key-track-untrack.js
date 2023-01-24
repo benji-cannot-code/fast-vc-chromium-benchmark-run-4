a74a355794bbc76c00a641577b20f910bc5b3d79
Chris Mumford <cmumford@google.com>
2023-01-24 18:11:33 +0000

    Revert "Reland "Clean-up database in indexed db test via js API""
    This reverts commit 40aaed2ad0202b31b150a6b11ddab315cc1350e0.
    
    Reason for revert: Still causing test failures(1)
    
    (1) https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/42680/overview
    
    Original change's description:
    > Reland "Clean-up database in indexed db test via js API"
    >
    > This is a reland of commit 3e1deb4faf409301bd1c8b2dbff0fbfe84939e8b
    >
    > Original change's description:
    > > Clean-up database in indexed db test via js API
    > >
    > > Bug: 1366717
    > > Change-Id: Ic8df55abb437f957c605857c87aeefa6a0bdddc9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4187597
    > > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > > Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1096110}
    >
    > Bug: 1366717
    > Change-Id: Ia38eb0b2041c63c6a101c7b5794b00d15a29b217
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192004
    > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1096184}
    
    Bug: 1366717
    Change-Id: I3a854068fadbde840f0f2d2c7b709a4787403c52
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192748
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096268}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   const {dp, session} = await testRunner.startBlank(
       `Tests that tracking and untracking IndexedDB for storage key works\n`);
+
   await dp.Page.enable();
   const protocolMessages = [];
   const originalDispatchMessage = DevToolsAPI.dispatchMessage;
@@ -20,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   setTimeout(() => {
     testRunner.log(protocolMessages);
     testRunner.die('Timeout', errorForLog);
-  }, 145000);
+  }, 5000);
 
   const frameId = (await dp.Page.getResourceTree()).result.frameTree.frame.id;
   errorForLog = new Error();
@@ -37,12 +38,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       message => {return `indexedDB content updated for ${message.params}`});
 
   testRunner.log(`Open database, object store and set value`);
-  const id = Math.random();
 
   // Create database, objectStore and add a key-value pair.
   const valuePromise = session.evaluateAsync(`
     new Promise(async resolve => {
-      const request = window.indexedDB.open("test-database${id}");
+      const request = window.indexedDB.open("test-database");
       request.onerror = (event) => {
         resolve('failed to create a database');
       };
@@ -55,11 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     })
   `);
 
-  const [listUpdatedEvent, contentUpdatedEvent, value] = await Promise.all(
-    [listUpdatedPromise, contentUpdatedPromise, valuePromise]);
-  testRunner.log(listUpdatedEvent);
-  testRunner.log(contentUpdatedEvent, "Title", ['databaseName', 'sessionId']);
-  testRunner.log(value);
+  testRunner.log(await Promise.all(
+      [listUpdatedPromise, contentUpdatedPromise, valuePromise]));
   errorForLog = new Error();
 
   testRunner.log('\nUntrack IndexedDB for storage key');
@@ -74,7 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Open database, objectStore and add another value.
   const oneMoreValue = await session.evaluateAsync(`
     new Promise(async resolve => {
-      const openreq = window.indexedDB.open("test-database${id}");
+      const openreq = window.indexedDB.open("test-database");
       openreq.onerror = (event) => {
         resolve("not able to open database");
       }
@@ -90,19 +87,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   testRunner.log(oneMoreValue);
 
-    // Clean up
-  try {
-    await session.evaluateAsync(`
-      new Promise(async (resolve, reject) => {
-        const req = window.indexedDB.deleteDatabase("test-database${id}");
-        req.onsuccess = resolve;
-        req.onerror = reject;
-      });
-    `);
-  } catch (e) {
-    testRunner.log(e);
-  } finally {
-    testRunner.completeTest();
-  }
+  // Clean up
+  await dp.IndexedDB.deleteDatabase({storageKey, databaseName: "test-database"});
+  errorForLog = new Error();
 
+  testRunner.completeTest();
 })
