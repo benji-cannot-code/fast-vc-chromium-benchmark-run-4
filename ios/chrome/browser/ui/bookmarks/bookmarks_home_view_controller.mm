d1258d4ccb9935a1c852f5096011ae23a1011db8
Arthur Milchior <arthurmilchior@chromium.org>
2023-01-24 13:30:01 +0000

    [iOS] Split present editor method
    With a method for URL and one for Folders, it’ll be easier to split the code into their own coordinators
    
    Bug: 1404311
    Change-Id: If8920d9e83ae2c4ff86f68f0e6db6fd6536b726d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176850
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096136}

--- ios/chrome/browser/ui/bookmarks/bookmarks_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmarks_home_view_controller.mm
@@ -660,16 +660,30 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
   [self setTableViewEditing:NO];
 }
 
-// Opens the editor on the given node.
-- (void)editNode:(const BookmarkNode*)node {
+// Ensures bookmarkInteractionController is set.
+- (void)ensureBookmarksCoordinator {
   if (!self.bookmarksCoordinator) {
     self.bookmarksCoordinator =
         [[BookmarksCoordinator alloc] initWithBrowser:self.browser];
     self.bookmarksCoordinator.baseViewController = self;
     self.bookmarksCoordinator.delegate = self;
   }
+}
 
-  [self.bookmarksCoordinator presentEditorForNode:node];
+// Opens the editor on the given URL node.
+- (void)editNodeURL:(const BookmarkNode*)node {
+  DCHECK(node);
+  DCHECK_EQ(node->type(), BookmarkNode::URL);
+  [self ensureBookmarksCoordinator];
+  [self.bookmarksCoordinator presentEditorForURLNode:node];
+}
+
+// Opens the editor on the given Folder node.
+- (void)editNodeFolder:(const BookmarkNode*)node {
+  DCHECK(node);
+  DCHECK_EQ(node->type(), BookmarkNode::FOLDER);
+  [self ensureBookmarksCoordinator];
+  [self.bookmarksCoordinator presentEditorForFolderNode:node];
 }
 
 - (void)openAllURLs:(std::vector<GURL>)urls
@@ -810,7 +824,8 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
     const bookmarks::BookmarkNode* node = *nodes.begin();
     if (node->is_url()) {
       [self setContextBarState:BookmarksContextBarSingleURLSelection];
-    } else if (node->is_folder()) {
+    } else {
+      DCHECK_EQ(node->type(), BookmarkNode::FOLDER);
       [self setContextBarState:BookmarksContextBarSingleFolderSelection];
     }
     return;
@@ -1815,7 +1830,7 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
                                bookmark_utils_ios::FindNodeById(
                                    strongSelf.bookmarks, nodeId);
                            if (nodeFromId) {
-                             [strongSelf editNode:nodeFromId];
+                             [strongSelf editNodeURL:nodeFromId];
                            }
                          }
                           style:UIAlertActionStyleDefault
@@ -1903,7 +1918,7 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
                                bookmark_utils_ios::FindNodeById(
                                    strongSelf.bookmarks, nodeId);
                            if (nodeFromId) {
-                             [strongSelf editNode:nodeFromId];
+                             [strongSelf editNodeFolder:nodeFromId];
                            }
                          }
                           style:UIAlertActionStyleDefault
@@ -2366,7 +2381,7 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
         const bookmarks::BookmarkNode* nodeFromId =
             bookmark_utils_ios::FindNodeById(innerStrongSelf.bookmarks, nodeId);
         if (nodeFromId) {
-          [innerStrongSelf editNode:nodeFromId];
+          [innerStrongSelf editNodeURL:nodeFromId];
         }
       }];
       [menuElements addObject:editAction];
@@ -2436,7 +2451,7 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
         const bookmarks::BookmarkNode* nodeFromId =
             bookmark_utils_ios::FindNodeById(innerStrongSelf.bookmarks, nodeId);
         if (nodeFromId) {
-          [innerStrongSelf editNode:nodeFromId];
+          [innerStrongSelf editNodeFolder:nodeFromId];
         }
       }];
       UIAction* moveAction = [actionFactory actionToMoveFolderWithBlock:^{
