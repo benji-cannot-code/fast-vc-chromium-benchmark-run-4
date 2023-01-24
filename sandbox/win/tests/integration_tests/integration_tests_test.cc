4b96564b2c0e74f72b0abad1edd7f844aee4ffea
Alex Gough <ajgo@chromium.org>
2023-01-24 01:27:20 +0000

    Remove Win7 escape in integration_tests_test
    Bug: 1385495
    Change-Id: If9a9d7303a841159cea6bacd76ffbc66499cebfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190192
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095956}

--- sandbox/win/tests/integration_tests/integration_tests_test.cc
+++ sandbox/win/tests/integration_tests/integration_tests_test.cc
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
-// Must be after windows.h.
-#include <versionhelpers.h>
-
 #include "base/memory/raw_ptr.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -362,17 +359,10 @@ TEST(IntegrationTestsTest, RunChildFromInsideJob) {
 // Running from inside job that doesn't allow us to escape from it should fail
 // on any windows prior to 8.
 TEST(IntegrationTestsTest, RunChildFromInsideJobNoEscape) {
-  int expect_result = 4;  // Means the runner has failed to execute the child.
-  // Check if we are on Win8 or newer and expect a success as newer windows
-  // versions support nested jobs.
-  if (IsWindows8OrGreater()) {
-    expect_result = SBOX_TEST_SUCCEEDED;
-  }
-
   TestRunner runner;
   runner.SetUnsandboxed(true);
   runner.SetTimeout(TestTimeouts::action_timeout());
-  ASSERT_EQ(expect_result,
+  ASSERT_EQ(SBOX_TEST_SUCCEEDED,
             runner.RunTest(L"IntegrationTestsTest_job with_job"));
 }
 
