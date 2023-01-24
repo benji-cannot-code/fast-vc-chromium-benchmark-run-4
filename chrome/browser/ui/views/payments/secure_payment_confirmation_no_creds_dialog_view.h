21c6085a25608c048f8affe8644b0cc4419ce4c2
Stephen McGruer <smcgruer@chromium.org>
2023-01-24 14:25:35 +0000

    [SPC] Test accidental clicks on the no-matching-credentials dialog [Continue] button
    The SPC no matching credentials dialog already has an input protector
    by virtue of being a views::DialogDelegateView; however there were
    no tests of this behavior.
    
    This CL also refactors the browsertest to remove use of EventWaiter.
    All calls in the test/code were synchronous, so the code would never
    actually sleep and instead was equivalent to checking a bool.
    
    Bug: 1407247
    Change-Id: I50ac3fb4f6a62891c0b69fa07f4ba5aac16337c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188805
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096157}

--- chrome/browser/ui/views/payments/secure_payment_confirmation_no_creds_dialog_view.h
+++ chrome/browser/ui/views/payments/secure_payment_confirmation_no_creds_dialog_view.h
@@ -23,7 +23,6 @@ class SecurePaymentConfirmationNoCredsDialogView
 
   class ObserverForTest {
    public:
-    virtual void OnDialogOpened() = 0;
     virtual void OnDialogClosed() = 0;
     virtual void OnOptOutClicked() = 0;
   };
