42d24ac186a92b80524c05e8160f025039f65735
Robert Ogden <robertogden@chromium.org>
2023-01-24 22:25:54 +0000

    Enable page-visibility-page-content-annotations flag on Android
    Bug: b/244641834
    Change-Id: Ib94d22cb5136012c88aabe7f17a3001eea3a7192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192714
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096432}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -5509,7 +5509,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"page-visibility-page-content-annotations",
      flag_descriptions::kPageVisibilityPageContentAnnotationsName,
      flag_descriptions::kPageVisibilityPageContentAnnotationsDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(
          optimization_guide::features::kPageVisibilityPageContentAnnotations)},
 
