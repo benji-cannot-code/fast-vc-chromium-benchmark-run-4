83a49afe818ce991c50ac35b1c5937948b8996c8
wenyu zhang <zhangwenyu@google.com>
2023-01-24 20:53:39 +0000

    Use new HasLauncherButton function in //ash/clipboard
    This is a continuous work following this CL: http://go/crrv/c/4114303.
    
    Bug: b:216049298, b/260608280
    Test: ./ash/clipboard/*_unittests.cc
    Change-Id: Iacf95a5c31ff6d753a71d6163fa4718a71a06682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143871
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096348}

--- ash/clipboard/clipboard_history_controller_impl.cc
+++ ash/clipboard/clipboard_history_controller_impl.cc
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/image_model.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/base/webui/web_ui_util.h"
-#include "ui/chromeos/events/keyboard_layout_util.h"
+#include "ui/chromeos/events/keyboard_capability.h"
 #include "ui/events/event.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
@@ -685,7 +685,8 @@ void ClipboardHistoryControllerImpl::OnOperationConfirmed(bool copy) {
       base::RecordAction(base::UserMetricsAction("Ash_Clipboard_CopiedItem"));
 
       // TODO(b/264913203): Add proper string resources for toast.
-      bool use_launcher_key = ui::DeviceUsesKeyboardLayout2();
+      bool use_launcher_key =
+          Shell::Get()->keyboard_capability()->HasLauncherButton();
       std::u16string shortcut_key = l10n_util::GetStringUTF16(
           use_launcher_key ? IDS_ASH_SHORTCUT_MODIFIER_LAUNCHER
                            : IDS_ASH_SHORTCUT_MODIFIER_SEARCH);
--- ash/clipboard/clipboard_nudge.cc
+++ ash/clipboard/clipboard_nudge.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ash/system/tray/system_nudge_label.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/chromeos/events/keyboard_layout_util.h"
+#include "ui/chromeos/events/keyboard_capability.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/color_palette.h"
@@ -70,7 +70,8 @@ ClipboardNudge::ClipboardNudge(ClipboardNudgeType nudge_type)
 ClipboardNudge::~ClipboardNudge() = default;
 
 std::unique_ptr<SystemNudgeLabel> ClipboardNudge::CreateLabelView() const {
-  bool use_launcher_key = ui::DeviceUsesKeyboardLayout2();
+  bool use_launcher_key =
+      Shell::Get()->keyboard_capability()->HasLauncherButton();
   std::u16string shortcut_key = l10n_util::GetStringUTF16(
       use_launcher_key ? IDS_ASH_SHORTCUT_MODIFIER_LAUNCHER
                        : IDS_ASH_SHORTCUT_MODIFIER_SEARCH);
