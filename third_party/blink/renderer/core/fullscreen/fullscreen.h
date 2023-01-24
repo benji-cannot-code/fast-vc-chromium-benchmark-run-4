63709faf86a7e205ac04578a2b26132420189261
Michael Lippautz <mlippautz@chromium.org>
2023-01-24 08:33:54 +0000

    Fullscreen::IsFullscreenElement: Speed up check for fullscreen elements
    Adds a fast bailout that avoids iterating a Document's top layer for
    finding a fullscreen element in case none exists.
    
    This results in a small progression on Speedometer as the fullscreen
    check is present in node insertion and removal hot paths.
    
    Change-Id: Iee307101d3e503669b771afa23ae31505ab17182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188559
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096065}

--- third_party/blink/renderer/core/fullscreen/fullscreen.h
+++ third_party/blink/renderer/core/fullscreen/fullscreen.h
@@ -66,6 +66,7 @@ class CORE_EXPORT Fullscreen final : public GarbageCollected<Fullscreen>,
   static Element* FullscreenElementForBindingFrom(TreeScope&);
   static bool IsFullscreenElement(const Element&);
   static bool IsInFullscreenElementStack(const Element&);
+  static bool HasFullscreenElements();
 
   static void RequestFullscreen(Element&);
   static ScriptPromise RequestFullscreen(
@@ -148,7 +149,8 @@ class CORE_EXPORT Fullscreen final : public GarbageCollected<Fullscreen>,
 };
 
 inline bool Fullscreen::IsFullscreenElement(const Element& element) {
-  return FullscreenElementFrom(element.GetDocument()) == &element;
+  return UNLIKELY(HasFullscreenElements()) &&
+         FullscreenElementFrom(element.GetDocument()) == &element;
 }
 
 }  // namespace blink
