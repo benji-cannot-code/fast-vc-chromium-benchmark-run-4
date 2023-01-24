2976402ea1ae682ccd35aac4355180b3a05f64a6
Federica Germinario <fedegermi@google.com>
2023-01-24 14:56:37 +0000

    [iOS] Expose InstallationNotifier internals in +private.h
    Creation of installation_notifier+private.h where some private methods
    are exposed.
    
    Fixed: 1379044
    Change-Id: Id66057eae838a7db941ce9c9535d1e5c249e0f24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188758
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096172}

--- ios/chrome/browser/download/installation_notifier_unittest.mm
+++ ios/chrome/browser/download/installation_notifier_unittest.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <stdint.h>
 
 #import "base/ios/block_types.h"
+#import "ios/chrome/browser/download/installation_notifier+private.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "net/base/backoff_entry.h"
 #import "testing/platform_test.h"
@@ -78,17 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _notificationCount++;
 }
 
-@end
-
-@interface InstallationNotifier (Testing)
-- (void)setDispatcher:(id<DispatcherProtocol>)dispatcher;
-- (void)resetDispatcher;
-- (void)dispatchInstallationNotifierBlock;
-- (void)registerForInstallationNotifications:(id)observer
-                                withSelector:(SEL)notificationSelector
-                                   forScheme:(NSString*)scheme
-                                startPolling:(BOOL)poll;
-- (net::BackoffEntry::Policy const*)backOffPolicy;
 @end
 
 namespace {
