39395c300f9439bf48b59c80c9d5715c3363c29f
Ari Chivukula <arichiv@chromium.org>
2023-01-24 05:10:01 +0000

    [StorageKey] Ensure that if nonce is set top_level_site and ancestor_chain_bit contain no unique information
    The serialization path in StorageKey when a nonce is set doesn't
    serialize top_level_site or ancestor_chain_bit at all. We should enforce
    this invariant in the constructor to make sure there's no dependency on
    information lost over a serialization/deserialization path.
    
    Bug: 1407243
    Change-Id: I3bb395e911cc923e53ec5e267f2c457ba98101f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189377
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096023}

--- third_party/blink/common/storage_key/storage_key_unittest.cc
+++ third_party/blink/common/storage_key/storage_key_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/feature_list.h"
+#include "base/test/gtest_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
 #include "net/base/features.h"
@@ -778,6 +779,40 @@ TEST(StorageKeyTest, ToCookiePartitionKey) {
       EXPECT_EQ(test_case.expected,
                 test_case.storage_key.ToCookiePartitionKey());
     }
+  }
+}
+
+TEST(StorageKeyTest, NonceRequiresMatchingOriginSiteAndSameSite) {
+  const url::Origin origin = url::Origin::Create(GURL("https://foo.com"));
+  const net::SchemefulSite site(origin);
+  const net::SchemefulSite opaque_site;
+  const net::SchemefulSite other_site(GURL("https://notfoo.com"));
+  base::UnguessableToken nonce = base::UnguessableToken::Create();
+
+  for (const bool toggle : {false, true}) {
+    base::test::ScopedFeatureList scope_feature_list;
+    scope_feature_list.InitWithFeatureState(
+        net::features::kThirdPartyStoragePartitioning, toggle);
+
+    // A nonce key with a matching origin/site that's SameSite works.
+    (void)StorageKey::CreateWithOptionalNonce(
+        origin, site, &nonce, mojom::AncestorChainBit::kSameSite);
+
+    // A nonce key with a non-matching origin/site that's SameSite fails.
+    EXPECT_DCHECK_DEATH(StorageKey::CreateWithOptionalNonce(
+        origin, opaque_site, &nonce, mojom::AncestorChainBit::kSameSite));
+    EXPECT_DCHECK_DEATH(StorageKey::CreateWithOptionalNonce(
+        origin, other_site, &nonce, mojom::AncestorChainBit::kSameSite));
+
+    // A nonce key with a matching origin/site that's CrossSite fails.
+    EXPECT_DCHECK_DEATH(StorageKey::CreateWithOptionalNonce(
+        origin, site, &nonce, mojom::AncestorChainBit::kCrossSite));
+
+    // A nonce key with a non-matching origin/site that's CrossSite fails.
+    EXPECT_DCHECK_DEATH(StorageKey::CreateWithOptionalNonce(
+        origin, opaque_site, &nonce, mojom::AncestorChainBit::kCrossSite));
+    EXPECT_DCHECK_DEATH(StorageKey::CreateWithOptionalNonce(
+        origin, other_site, &nonce, mojom::AncestorChainBit::kCrossSite));
   }
 }
 
--- third_party/blink/renderer/platform/storage/blink_storage_key_test.cc
+++ third_party/blink/renderer/platform/storage/blink_storage_key_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/storage/blink_storage_key.h"
 
 #include "base/memory/scoped_refptr.h"
+#include "base/test/gtest_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
 #include "net/base/features.h"
@@ -235,6 +236,45 @@ TEST(BlinkStorageKeyTest, CopyWithForceEnabledThirdPartyStoragePartitioning) {
               BlinkSchemefulSite(origin2));
     EXPECT_EQ(storage_key_with_3psp.GetAncestorChainBit(),
               mojom::blink::AncestorChainBit::kCrossSite);
+  }
+}
+
+TEST(BlinkStorageKeyTest, NonceRequiresMatchingOriginSiteAndSameSite) {
+  scoped_refptr<const SecurityOrigin> origin =
+      SecurityOrigin::CreateFromString("https://foo.com");
+  const BlinkSchemefulSite site(origin);
+  const BlinkSchemefulSite opaque_site;
+  const BlinkSchemefulSite other_site(
+      SecurityOrigin::CreateFromString("https://notfoo.com"));
+  base::UnguessableToken nonce = base::UnguessableToken::Create();
+
+  for (const bool toggle : {false, true}) {
+    base::test::ScopedFeatureList scope_feature_list;
+    scope_feature_list.InitWithFeatureState(
+        net::features::kThirdPartyStoragePartitioning, toggle);
+
+    // A nonce key with a matching origin/site that's SameSite works.
+    (void)BlinkStorageKey(origin, site, &nonce,
+                          mojom::blink::AncestorChainBit::kSameSite);
+
+    // A nonce key with a non-matching origin/site that's SameSite fails.
+    EXPECT_DCHECK_DEATH(
+        BlinkStorageKey(origin, opaque_site, &nonce,
+                        mojom::blink::AncestorChainBit::kSameSite));
+    EXPECT_DCHECK_DEATH(BlinkStorageKey(
+        origin, other_site, &nonce, mojom::blink::AncestorChainBit::kSameSite));
+
+    // A nonce key with a matching origin/site that's CrossSite fails.
+    EXPECT_DCHECK_DEATH(BlinkStorageKey(
+        origin, site, &nonce, mojom::blink::AncestorChainBit::kCrossSite));
+
+    // A nonce key with a non-matching origin/site that's CrossSite fails.
+    EXPECT_DCHECK_DEATH(
+        BlinkStorageKey(origin, opaque_site, &nonce,
+                        mojom::blink::AncestorChainBit::kCrossSite));
+    EXPECT_DCHECK_DEATH(
+        BlinkStorageKey(origin, other_site, &nonce,
+                        mojom::blink::AncestorChainBit::kCrossSite));
   }
 }
 
