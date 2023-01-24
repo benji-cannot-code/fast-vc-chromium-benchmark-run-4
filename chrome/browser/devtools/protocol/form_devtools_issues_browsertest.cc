cd013d972cb589e580ed3ebd222392e4ff2574c0
Bruno Braga <brunobraga@google.com>
2023-01-24 14:46:48 +0000

    [Autofill in Devtools] Emit issues when autocomplete attributes are empty.
    Bug: 1399414, 1404889
    Change-Id: I89f7c9ad540f4398cf70db79219eec79e325ad11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144705
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096164}

--- chrome/browser/devtools/protocol/form_devtools_issues_browsertest.cc
+++ chrome/browser/devtools/protocol/form_devtools_issues_browsertest.cc
@@ -89,4 +89,15 @@ IN_PROC_BROWSER_TEST_F(AutofillFormDevtoolsProtocolTest,
                   .has_value());
 }
 
+IN_PROC_BROWSER_TEST_F(AutofillFormDevtoolsProtocolTest,
+                       FormHasInputWithEmptyAutocompleteAttribute) {
+  NavigateToFormPageAndEnableAudits();
+  base::Value::Dict notification =
+      WaitForGenericIssueAdded("FormAutocompleteAttributeEmptyError");
+  EXPECT_TRUE(notification
+                  .FindIntByDottedPath(
+                      "issue.details.genericIssueDetails.violatingNodeId")
+                  .has_value());
+}
+
 }  // namespace autofill
