626d427dd126f24769ff9967305f93251ce62857
Noel Gordon <noel@chromium.org>
2023-01-24 23:27:42 +0000

    [conflict dialog] define IOTask Pause and Resume params
    copy_or_move can be paused if there is a file name conflict during
    the copy_or_move task. Files app, or notifications UI will be used
    to ask the user how they would like to proceed.
    
    Define the Pause params as a struct in io task and in file manager
    private IDL. Pause params provide the reasons why the copy_or_move
    task entered state::PAUSED.
    
    Similarly define Resume params: these indicate the user's decision
    from the UI about how they would like to proceed.
    
    copy_or_move task will be the only user of Pause and Resume params
    today. Other io task types might like to Pause-Resume in future so
    prefix the param names with "conflict" to indicate they're used to
    resolve copy_or_move task file name conflicts [1].
    
    [1] These "conflict" prefixed IDL params are marked type? optional
    in the IDL therefore, to allow for said future expansion.
    
    Bug: b:255264604
    Change-Id: I0eaedd1b8dad994615f3bcbfca09f01ca47e0af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190483
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096485}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -743,6 +743,23 @@ chrome.fileManagerPrivate.IOTaskParams;
 
 /**
  * @typedef {{
+ *   conflictName: (string|undefined),
+ *   conflictMultiple: (boolean|undefined),
+ *   conflictIsDirectory: (boolean|undefined),
+ * }}
+ */
+chrome.fileManagerPrivate.PauseParams;
+
+/**
+ * @typedef {{
+ *   conflictResolve: (string|undefined),
+ *   conflictApplyToAll: (boolean|undefined),
+ * }}
+ */
+chrome.fileManagerPrivate.ResumeParams;
+
+/**
+ * @typedef {{
  *   type: !chrome.fileManagerPrivate.IOTaskType,
  *   state: !chrome.fileManagerPrivate.IOTaskState,
  *   numRemainingItems: number,
@@ -756,6 +773,7 @@ chrome.fileManagerPrivate.IOTaskParams;
  *   showNotification: boolean,
  *   errorName: string,
  *   outputs: (Array<Entry>|undefined),
+ *   pauseParams:(chrome.fileManagerPrivate.PauseParams|undefined),
  * }}
  */
 chrome.fileManagerPrivate.ProgressStatus;
