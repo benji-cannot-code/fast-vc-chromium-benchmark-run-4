f13d5167fca1e1110b2cbe51f8f3be004d00bf7b
Camden King <camdenking@google.com>
2023-01-24 19:53:27 +0000

    [dPWA] Creating URL Handling Sub Manager & implementing Configure()
    This CL introduces the UrlHandlingSubManager interface and the
    implementation of the Configure function.
    
    Implementation guide: go/url-handling-sub-manager
    
    Bug: 1295044, b/260791052
    Change-Id: I97a4fe60617871fe6fa6dccade4566fb67ec36a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173124
    Commit-Queue: Camden King <camdenking@google.com>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096311}

--- chrome/browser/web_applications/os_integration/os_integration_manager.cc
+++ chrome/browser/web_applications/os_integration/os_integration_manager.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/os_integration/shortcut_menu_handling_sub_manager.h"
 #include "chrome/browser/web_applications/os_integration/shortcut_sub_manager.h"
 #include "chrome/browser/web_applications/os_integration/uninstallation_via_os_settings_sub_manager.h"
+#include "chrome/browser/web_applications/os_integration/url_handling_sub_manager.h"
 #include "chrome/browser/web_applications/os_integration/web_app_shortcut.h"
 #include "chrome/browser/web_applications/os_integration/web_app_uninstallation_via_os_settings_registration.h"
 #include "chrome/browser/web_applications/proto/web_app_os_integration_state.pb.h"
@@ -190,6 +191,8 @@ void OsIntegrationManager::SetSubsystems(WebAppSyncBridge* sync_bridge,
       std::make_unique<FileHandlingSubManager>(*registrar);
   auto protocol_handling_sub_manager =
       std::make_unique<ProtocolHandlingSubManager>(profile_, *registrar);
+  auto url_handling_sub_manager =
+      std::make_unique<UrlHandlingSubManager>(*registrar);
   auto shortcut_menu_handling_sub_manager =
       std::make_unique<ShortcutMenuHandlingSubManager>(*icon_manager,
                                                        *registrar);
@@ -200,6 +203,7 @@ void OsIntegrationManager::SetSubsystems(WebAppSyncBridge* sync_bridge,
   sub_managers_.push_back(std::move(shortcut_sub_manager));
   sub_managers_.push_back(std::move(file_handling_sub_manager));
   sub_managers_.push_back(std::move(protocol_handling_sub_manager));
+  sub_managers_.push_back(std::move(url_handling_sub_manager));
   sub_managers_.push_back(std::move(shortcut_menu_handling_sub_manager));
   sub_managers_.push_back(std::move(run_on_os_login_sub_manager));
   sub_managers_.push_back(
