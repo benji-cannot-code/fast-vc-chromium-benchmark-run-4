c00c82bafc796f7d362d61fe3db44c427ccf5ff3
Mike Dougherty <michaeldo@chromium.org>
2023-01-24 18:07:33 +0000

    Remove __gCrWeb.windowId
    __gCrWeb.windowId was used to ensure that JavaScript was executed on
    the intended webpage. This is important because some scripts may deal
    with user information. Such information is now all passed through
    APIs using WebKit's FrameInfo instance which also has the same
    guarantee because FrameInfo instances are tied to a particular
    navigation.
    
    Fixed: 905939
    
    Change-Id: I19a2e0a7a7ef0f737af99962ec41294c5cbbbd04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189036
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096265}

--- ios/web/navigation/resources/navigation.js
+++ ios/web/navigation/resources/navigation.js
@@ -115,7 +115,3 @@ window.history.replaceState = function(stateObject, pageTitle, pageUrl) {
     'frame_id': __gCrWeb.message.getFrameId()
   });
 };
-
-window.addEventListener('__gCrWebWindowIdInjected', function() {
-  sendQueuedMessages();
-});
