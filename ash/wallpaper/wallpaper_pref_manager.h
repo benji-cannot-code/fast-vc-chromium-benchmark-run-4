8dbff71df76c70498dbe92c4aab56a82c94a65f7
Sean Kau <skau@chromium.org>
2023-01-24 23:18:40 +0000

    Move GetCachedCalculatedColors to WallpaperPrefManager
    Its easier to test in the WallpaperPrefManager and it makes sense to
    abstract some of the logic from the controller.
    
    Change-Id: I61ab84656baa9e74bbf3d5d40091fb75a79b07e6
    Bug: b:258750361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189048
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096482}

--- ash/wallpaper/wallpaper_pref_manager.h
+++ ash/wallpaper/wallpaper_pref_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/style/color_mode_observer.h"
 #include "ash/public/cpp/wallpaper/wallpaper_controller.h"
 #include "ash/public/cpp/wallpaper/wallpaper_info.h"
+#include "ash/wallpaper/wallpaper_utils/wallpaper_calculated_colors.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/wall_clock_timer.h"
 #include "components/account_id/account_id.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class PrefService;
 class PrefRegistrySimple;
@@ -119,6 +121,12 @@ class ASH_EXPORT WallpaperPrefManager
 
   // Remove the wallpaper entry for |account_id|.
   virtual void RemoveUserWallpaperInfo(const AccountId& account_id) = 0;
+
+  // Returns a WallpaperCalculatedColors for a wallpaper with the corresponding
+  // `location`, if one can be found. The result is synthesized from Prominent
+  // and KMean colors.
+  virtual absl::optional<WallpaperCalculatedColors> GetCachedWallpaperColors(
+      base::StringPiece location) const = 0;
 
   virtual void CacheProminentColors(const AccountId& account_id,
                                     const std::vector<SkColor>& colors) = 0;
