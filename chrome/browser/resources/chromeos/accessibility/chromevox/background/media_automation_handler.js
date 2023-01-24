c46869796a0ec23c56410b2bdb23d4f7faba0e48
Anastasia Helfinstein <anastasi@google.com>
2023-01-24 02:37:44 +0000

    [ChromeVox] Move TtsInterface to background/
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:265481751
    Test: cq
    Change-Id: Ida8fbb366bf9769a9fc3cf537c7d2ed982fc6274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4175298
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095981}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/media_automation_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/media_automation_handler.js
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import {AsyncUtil} from '../../common/async_util.js';
 import {LocalStorage} from '../../common/local_storage.js';
-import {TtsCapturingEventListener} from '../common/tts_interface.js';
 
 import {BaseAutomationHandler} from './base_automation_handler.js';
 import {ChromeVox} from './chromevox.js';
+import {TtsCapturingEventListener} from './tts_interface.js';
 
 const AutomationEvent = chrome.automation.AutomationEvent;
 const AutomationNode = chrome.automation.AutomationNode;
