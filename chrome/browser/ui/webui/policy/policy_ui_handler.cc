256fbcd035f2238064885c041ad02f92dcec0bbf
Salma Elmahallawy <esalma@google.com>
2023-01-24 01:03:51 +0000

    [PolicyLogs] Policy Logs Page Added with chrome.send()
    This CL adds the policy logs page for chrome://policy/logs on Android.
    The page has a simple web UI to display the version information
    and the policy event logs from PolicyLogger.
    
    The CL contains the basic skeleton for the WebUI page which displays
    the logs and version information as well as the communication
    between the browser and the renderer.
    
    PolicyUI will initialize the page's html source for the
    chrome://policy/logs path, and its script (policy_logs.js)
    will make the requests to the browser when the page is first created and
    when the Refresh Logs button is clicked.
    
    A button is also added to download the page contents as a JSON file.
    
    Bug: b:262939718, b:263116406
    Change-Id: I0da2cb9d11669f025f2f3ae02334a9eada089dac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144212
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095945}

--- chrome/browser/ui/webui/policy/policy_ui_handler.cc
+++ chrome/browser/ui/webui/policy/policy_ui_handler.cc
@@ -73,6 +73,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/webui/web_ui_util.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "components/policy/core/common/policy_logger.h"
+#endif  // BUILDFLAG(IS_ANDROID)
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ash/policy/active_directory/active_directory_policy_manager.h"
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
@@ -176,6 +180,14 @@ void PolicyUIHandler::RegisterMessages() {
       "copyPoliciesJSON",
       base::BindRepeating(&PolicyUIHandler::HandleCopyPoliciesJson,
                           base::Unretained(this)));
+
+#if BUILDFLAG(IS_ANDROID)
+  web_ui()->RegisterMessageCallback(
+      "getPolicyLogs",
+      base::BindRepeating(&PolicyUIHandler::HandleGetPolicyLogs,
+                          base::Unretained(this)));
+#endif  // BUILDFLAG(IS_ANDROID)
+
 #if !BUILDFLAG(IS_CHROMEOS)
   web_ui()->RegisterMessageCallback(
       "uploadReport", base::BindRepeating(&PolicyUIHandler::HandleUploadReport,
@@ -285,6 +297,15 @@ void PolicyUIHandler::HandleCopyPoliciesJson(const base::Value::List& args) {
   ui::ScopedClipboardWriter scw(ui::ClipboardBuffer::kCopyPaste);
   scw.WriteText(base::UTF8ToUTF16(policies_json));
 }
+
+#if BUILDFLAG(IS_ANDROID)
+void PolicyUIHandler::HandleGetPolicyLogs(const base::Value::List& args) {
+  DCHECK(policy::PolicyLogger::GetInstance()->IsPolicyLoggingEnabled());
+  AllowJavascript();
+  ResolveJavascriptCallback(args[0],
+                            policy::PolicyLogger::GetInstance()->GetAsValue());
+}
+#endif  // BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_CHROMEOS)
 void PolicyUIHandler::HandleUploadReport(const base::Value::List& args) {
--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -35,6 +35,12 @@ BASE_FEATURE(kDmTokenDeletion,
              "DmTokenDeletion",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+#if BUILDFLAG(IS_ANDROID)
+BASE_FEATURE(kPolicyLogsPageAndroid,
+             "PolicyLogsPageAndroid",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(IS_ANDROID)
+
 }  // namespace features
 
 }  // namespace policy
