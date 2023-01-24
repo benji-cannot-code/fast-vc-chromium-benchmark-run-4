56050fae5beea8abbe1ba39ab4f721172d0aa190
Valerie Young <spectranaut@igalia.com>
2023-01-24 19:24:48 +0000

    AXAPI: Add method test for accessibilityCustomContent
    Bug: 1350145
    Change-Id: Iebed5cf5ba8eb1926c61f9f0990253f74d437d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4147433
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096304}

--- content/browser/accessibility/dump_accessibility_scripts_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_scripts_browsertest.cc
@@ -513,6 +513,11 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityScriptTest,
   RunTypedTest<kMacMethods>("accessibility-column-header-ui-elements.html");
 }
 
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityScriptTest,
+                       AccessibilityCustomContent) {
+  RunTypedTest<kMacMethods>("accessibility-custom-content.html");
+}
+
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityScriptTest, AccessibilityIsIgnored) {
   RunTypedTest<kMacMethods>("accessibility-is-ignored.html");
 }
