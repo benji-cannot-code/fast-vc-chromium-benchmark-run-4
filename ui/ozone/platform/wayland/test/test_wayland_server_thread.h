7463e809cd6fedc840edf3e3c447ff4708514671
Phan Quang Minh <phanquangminh217@gmail.com>
2023-01-25 04:54:13 +0000

    ozone/wayland: Fixes for XDG activation
    Most current compositor implementations specifically require that only
    focused toplevels (which are distinct from active toplevels) can request
    XDG activation. Also, as it's not strictly required to provide a
    requestor surface, we should simply not provide one if none is
    available. The compositor reserves the right to delay or silently refuse
    to activate the window in this case (for instance, GNOME Shell asks for
    user permission to activate).
    
    Change-Id: I661227809bb6c17bca869034fd0acb707e8cf3df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166961
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1096623}

--- ui/ozone/platform/wayland/test/test_wayland_server_thread.h
+++ ui/ozone/platform/wayland/test/test_wayland_server_thread.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/test/global_object.h"
 #include "ui/ozone/platform/wayland/test/mock_wayland_zcr_color_manager.h"
 #include "ui/ozone/platform/wayland/test/mock_wp_presentation.h"
+#include "ui/ozone/platform/wayland/test/mock_xdg_activation_v1.h"
 #include "ui/ozone/platform/wayland/test/mock_xdg_shell.h"
 #include "ui/ozone/platform/wayland/test/mock_zwp_linux_dmabuf.h"
 #include "ui/ozone/platform/wayland/test/test_alpha_compositing.h"
@@ -160,6 +161,8 @@ class TestWaylandServerThread : public base::Thread,
     return &zcr_color_manager_v1_;
   }
 
+  MockXdgActivationV1* xdg_activation_v1() { return &xdg_activation_v1_; }
+
   void set_output_delegate(OutputDelegate* delegate) {
     output_delegate_ = delegate;
   }
@@ -231,6 +234,7 @@ class TestWaylandServerThread : public base::Thread,
   MockZwpLinuxDmabufV1 zwp_linux_dmabuf_v1_;
   MockWpPresentation wp_presentation_;
   TestWpPointerGestures wp_pointer_gestures_;
+  MockXdgActivationV1 xdg_activation_v1_;
   std::unique_ptr<TestSelectionDeviceManager> primary_selection_device_manager_;
 
   std::vector<std::unique_ptr<GlobalObject>> globals_;
