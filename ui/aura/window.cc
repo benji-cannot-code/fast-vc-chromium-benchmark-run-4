2cb3c8ecd5fda85801b258c9ff3cc343416d766b
Bo Liu <boliu@chromium.org>
2023-01-25 17:06:50 +0000

    Remove AsyncLayerTreeFrameSink::InitParams::client_name
    It's not used for anything since
    https://chromium-review.googlesource.com/c/chromium/src/+/2120362
    
    Change-Id: I905204cc17eb0fdc3b86ceebf1269565113bca0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192350
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096822}

--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -124,7 +124,6 @@ enum BoundsCallbackIndex : int {
 
 namespace aura {
 namespace {
-static const char* kExo = "Exo";
 
 class ScopedCursorHider {
  public:
@@ -1297,7 +1296,6 @@ std::unique_ptr<cc::LayerTreeFrameSink> Window::CreateLayerTreeFrameSink() {
       Env::GetInstance()->context_factory()->GetGpuMemoryBufferManager();
   params.pipes.compositor_frame_sink_remote = std::move(sink_remote);
   params.pipes.client_receiver = std::move(client_receiver);
-  params.client_name = kExo;
   auto frame_sink =
       std::make_unique<cc::mojo_embedder::AsyncLayerTreeFrameSink>(
           nullptr /* context_provider */, nullptr /* worker_context_provider */,
