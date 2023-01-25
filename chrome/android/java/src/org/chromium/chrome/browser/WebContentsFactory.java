d1586c30899521079706e104e24b5957c6aae456
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2023-01-25 20:25:07 +0000

    [WebContents] Dangling BrowserContext*: include Java stack trace
    ~WebContentsOfBrowserContext() uploads a crash report when it detects
    a dangling pointer to BrowserContext, which will likely lead to a
    use-after-free. The crash report includes a crash key with the
    "creator location", i.e. the location of the C++ function that created
    the leaked WebContents.
    
    This is fine on most platforms; but on Android, WebContentses are
    created via Java and JNI. In practice, all WebContentses are created
    in the same C++ function, so the creator location doesn't give us
    anything actionable...
    
    On Android, upload a separate crash report with the Java stack trace of
    the creator. Do this by saving (but not throwing) a java.lang.Exception
    in WebContentsFactory.createWebContents(). If/when a crash report is
    generated, it includes that Exception.
    
    Bug: 1407197
    Change-Id: I215ef04d923421e0fba6bfa16645a9600a0c4976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192226
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096941}

--- chrome/android/java/src/org/chromium/chrome/browser/WebContentsFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/WebContentsFactory.java
@@ -23,6 +23,15 @@ public class WebContentsFactory {
     public WebContentsFactory() {}
 
     /**
+     * For capturing where WebContentsImpl is created.
+     */
+    private static class WebContentsCreationException extends RuntimeException {
+        WebContentsCreationException() {
+            super("vvv This is where WebContents was created. vvv");
+        }
+    }
+
+    /**
      * A factory method to build a {@link WebContents} object.
      * @param profile         The profile with which the {@link WebContents} should be built.
      * @param initiallyHidden Whether or not the {@link WebContents} should be initially hidden.
@@ -30,14 +39,15 @@ public class WebContentsFactory {
      */
     // TODO(https://crbug.com/1099138): Remove static for unit-testability.
     public static WebContents createWebContents(Profile profile, boolean initiallyHidden) {
-        return WebContentsFactoryJni.get().createWebContents(profile, initiallyHidden, false);
+        return WebContentsFactoryJni.get().createWebContents(
+                profile, initiallyHidden, false, new WebContentsCreationException());
     }
 
     // TODO(https://crbug.com/1033955): Remove after check discard error is fixed.
     private static WebContents createWebContents(
             Profile profile, boolean initiallyHidden, boolean initializeRenderer) {
         return WebContentsFactoryJni.get().createWebContents(
-                profile, initiallyHidden, initializeRenderer);
+                profile, initiallyHidden, initializeRenderer, new WebContentsCreationException());
     }
 
     /**
@@ -55,7 +65,7 @@ public class WebContentsFactory {
 
     @NativeMethods
     interface Natives {
-        WebContents createWebContents(
-                Profile profile, boolean initiallyHidden, boolean initializeRenderer);
+        WebContents createWebContents(Profile profile, boolean initiallyHidden,
+                boolean initializeRenderer, Throwable javaCreator);
     }
 }
