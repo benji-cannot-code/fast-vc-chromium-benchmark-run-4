bb6c12fe3bd3a3bb1c3dcbf14a6b9e2de462ba86
Guillem Perez <guiperez@google.com>
2023-01-25 00:00:41 +0000

    [iOS] Feature flag for hiding Shortcuts and MVT for first time users.
    Created a feature flag to allow for experimenting with hiding the Most
    Visited Tiles and Shortcuts for first time users.
    
    Change-Id: I63e50a46259d7537e6eb3a6fc09fa470200a5096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189499
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096500}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.mm
@@ -48,6 +48,10 @@ const char kContentSuggestionsUIModuleRefreshMinimizeSpacingParam[] =
 const char kContentSuggestionsUIModuleRefreshRemoveHeadersParam[] =
     "remove_headers";
 
+BASE_FEATURE(kHideMVTAndShortcutsForNewUsers,
+             "HideMostVisitedAndShortcutsForNewUsers",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Feature disabled by default.
 BASE_FEATURE(kTrendingQueriesModule,
              "TrendingQueriesModule",
@@ -71,6 +75,10 @@ const char kDiscoverFeedIsNativeUIEnabled[] = "DiscoverFeedIsNativeUIEnabled";
 
 bool IsDiscoverFeedEnabled() {
   return base::FeatureList::IsEnabled(kDiscoverFeedInNtp);
+}
+
+bool IsHideMVTAndShortcutsEnabled() {
+  return base::FeatureList::IsEnabled(kHideMVTAndShortcutsForNewUsers);
 }
 
 // Returns true if the client is bucketed into an enabled experiment group of
