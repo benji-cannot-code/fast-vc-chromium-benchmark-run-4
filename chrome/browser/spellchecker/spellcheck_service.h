258f5499e4b2c5cda67d650ca525802c99f7a5d4
Mike Wittman <wittman@chromium.org>
2023-01-25 15:07:20 +0000

    Revert "[code health] Remove NotificationService use by SpellcheckService"
    This reverts commit 101e1efeffd8220a2975c5a5b724609a5c2b805b.
    
    Reason for revert: may be causing crashes on creation observation call
    
    Original change's description:
    > [code health] Remove NotificationService use by SpellcheckService
    >
    > Removes the use of the NotificationService to receive notification of
    > render process creation in SpellcheckService. NotificationService usage
    > is replaced with the RenderProcessHostCreationObserver.
    >
    > Bug: 357627
    > Change-Id: I96f3dbe99a1613ec18aa4815d8cb0bf4a8306228
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179204
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Guillaume Jenkins <gujen@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1095254}
    
    Bug: 357627, 1409290
    Change-Id: I012a7aa51221c494210c3bb7e640ad63c0f73182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189718
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1096762}

--- chrome/browser/spellchecker/spellcheck_service.h
+++ chrome/browser/spellchecker/spellcheck_service.h
@@ -25,7 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/spellcheck/browser/platform_spell_checker.h"
 #include "components/spellcheck/common/spellcheck.mojom-forward.h"
 #include "components/spellcheck/spellcheck_buildflags.h"
-#include "content/public/browser/render_process_host_creation_observer.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 class SpellCheckHostMetrics;
@@ -36,6 +37,8 @@ class WaitableEvent;
 
 namespace content {
 class BrowserContext;
+class NotificationDetails;
+class NotificationSource;
 class RenderProcessHost;
 }
 
@@ -49,7 +52,7 @@ class LanguageSettingsPrivateApiTestDelayInit;
 // profile and each is created by the SpellCheckServiceFactory.  The
 // SpellcheckService maintains any per-profile information about spellcheck.
 class SpellcheckService : public KeyedService,
-                          public content::RenderProcessHostCreationObserver,
+                          public content::NotificationObserver,
                           public SpellcheckCustomDictionary::Observer,
                           public SpellcheckHunspellDictionary::Observer {
  public:
@@ -136,8 +139,10 @@ class SpellcheckService : public KeyedService,
   // dictionaries available.
   bool IsSpellcheckEnabled() const;
 
-  // content::RenderProcessHostCreationObserver implementation.
-  void OnRenderProcessHostCreated(content::RenderProcessHost* host) override;
+  // NotificationProfile implementation.
+  void Observe(int type,
+               const content::NotificationSource& source,
+               const content::NotificationDetails& details) override;
 
   // SpellcheckCustomDictionary::Observer implementation.
   void OnCustomDictionaryLoaded() override;
@@ -296,6 +301,7 @@ class SpellcheckService : public KeyedService,
   std::unique_ptr<PlatformSpellChecker> platform_spell_checker_;
 
   PrefChangeRegistrar pref_change_registrar_;
+  content::NotificationRegistrar registrar_;
 
   // A pointer to the BrowserContext which this service refers to.
   raw_ptr<content::BrowserContext> context_;
