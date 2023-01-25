0d24b326d029b52cd2d60a0ec7bcdd4d95b635b4
Michelle <michellegc@google.com>
2023-01-25 00:18:39 +0000

    Use base and extent in TouchSelectionControllerImpl drag updates.
    Rewrite some views touch selection methods in preparation for
    implementing an "expand by word, shrink by character" feature (part of
    go/cros-touch-text-editing, for visualisation of feature see
    https://drive.google.com/file/d/1LdcaCgzLQCrSaqXF9gxgTPOj_l1Eetup/view?usp=sharing):
    
    1. In TouchSelectionControllerImpl, replace SetDraggingHandle and
    SelectionHandleDragged by OnDragBegin, OnDragUpdate and OnDragEnd,
    based on the corresponding methods in TouchSelectionController.
    
    2. In TouchEditable, replace MoveCaretTo and SelectRect by MoveCaret,
    MoveRangeSelectionExtent and SelectBetweenCoordinates, based on the
    TouchSelectionControllerClient interface.
    
    This CL doesn't change any visible behaviour. The new "expand by word,
    shrink by character" behaviour will be implemented in later CLs.
    
    Bug: b:244116654, b:264824692
    Change-Id: I0b3e3549b277297611d3f0765e0751579898d94b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4172677
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096516}

--- ui/views/touchui/touch_selection_controller_impl_unittest.cc
+++ ui/views/touchui/touch_selection_controller_impl_unittest.cc
@@ -409,9 +409,9 @@ TEST_F(TouchSelectionControllerImplTest, SelectionInBidiTextfieldTest) {
   VerifyHandlePositions(false, true, FROM_HERE);
 }
 
-// Tests if the SelectRect callback is called appropriately when selection
-// handles are moved.
-TEST_F(TouchSelectionControllerImplTest, SelectRectCallbackTest) {
+// Tests if the selection update callbacks are called appropriately when
+// selection handles are moved.
+TEST_F(TouchSelectionControllerImplTest, SelectionUpdateCallbackTest) {
   CreateTextfield();
   textfield_->SetText(u"textfield with selected text");
   // Tap the textfield to invoke touch selection.
@@ -453,7 +453,7 @@ TEST_F(TouchSelectionControllerImplTest, SelectRectCallbackTest) {
   VerifyHandlePositions(false, true, FROM_HERE);
 }
 
-TEST_F(TouchSelectionControllerImplTest, SelectRectInBidiCallbackTest) {
+TEST_F(TouchSelectionControllerImplTest, SelectionUpdateInBidiCallbackTest) {
   CreateTextfield();
   textfield_->SetText(
       u"abc\x05e1\x05e2\x05e3"
@@ -669,10 +669,14 @@ class TestTouchEditable : public ui::TouchEditable {
 
  private:
   // Overridden from ui::TouchEditable.
-  void SelectRect(const gfx::Point& start, const gfx::Point& end) override {
+  void MoveCaret(const gfx::Point& position) override { NOTREACHED(); }
+  void MoveRangeSelectionExtent(const gfx::Point& extent) override {
     NOTREACHED();
   }
-  void MoveCaretTo(const gfx::Point& point) override { NOTREACHED(); }
+  void SelectBetweenCoordinates(const gfx::Point& base,
+                                const gfx::Point& extent) override {
+    NOTREACHED();
+  }
   void GetSelectionEndPoints(gfx::SelectionBound* anchor,
                              gfx::SelectionBound* focus) override {
     *anchor = *focus = cursor_bound_;
