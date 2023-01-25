f1e20910b4a5688b72a7fbeb6d32fe235b566188
Will Cassella <cassew@chromium.org>
2023-01-25 00:13:21 +0000

    HDR MediaCapabilities: Add flag to report smpteSt2086 as supported
    This CL adds a feature ("SupportSmpteSt2086HdrMetadata") which
    enables the MediaCapabilities.decodingInfo API to treat 'smpteSt2086' as
    a supported value for the `video.hdrMetadataType` field. It also updates
    the relevant test cases to cover this value, as well as known
    unsupported values.
    
    Bug: 1048045
    Change-Id: I8bd3fe0ca73e72310b329d19e86a072da3de7a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4123310
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096509}

--- media/base/supported_types.cc
+++ media/base/supported_types.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "media/media_buildflags.h"
 #include "ui/display/display_switches.h"
+#include "ui/gfx/hdr_metadata.h"
 
 #if BUILDFLAG(ENABLE_LIBVPX)
 // TODO(dalecurtis): This technically should not be allowed in media/base. See
@@ -65,6 +66,8 @@ bool IsSupportedHdrMetadata(const gfx::HdrMetadataType& hdr_metadata_type) {
       return true;
 
     case gfx::HdrMetadataType::kSmpteSt2086:
+      return base::FeatureList::IsEnabled(kSupportSmpteSt2086HdrMetadata);
+
     case gfx::HdrMetadataType::kSmpteSt2094_10:
     case gfx::HdrMetadataType::kSmpteSt2094_40:
       return false;
