f36472dc89f30ca4958ac5fcdd1aacb84b513a9c
Tommy Steimel <steimel@chromium.org>
2023-01-25 23:09:52 +0000

    PiP 2.0: Add origin trial feature name
    This CL adds the origin trial feature name and OSes to the
    DocumentPictureInPictureAPI Blink feature.
    
    Change-Id: Idc4d2185bf3171061cef209695c835cdae059382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190826
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097081}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -397,7 +397,6 @@ ui::mojom::blink::WindowOpenDisposition NavigationPolicyToDisposition(
     case kNavigationPolicyNewPopup:
       return ui::mojom::blink::WindowOpenDisposition::NEW_POPUP;
     case kNavigationPolicyPictureInPicture:
-      DCHECK(RuntimeEnabledFeatures::DocumentPictureInPictureAPIEnabled());
       return ui::mojom::blink::WindowOpenDisposition::NEW_PICTURE_IN_PICTURE;
   }
   NOTREACHED() << "Unexpected NavigationPolicy";
--- third_party/blink/renderer/core/html/media/html_media_element.cc
+++ third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -648,7 +648,8 @@ void HTMLMediaElement::DidMoveToNewDocument(Document& old_document) {
 
 bool HTMLMediaElement::ShouldReusePlayer(Document& old_document,
                                          Document& new_document) const {
-  if (!RuntimeEnabledFeatures::DocumentPictureInPictureAPIEnabled()) {
+  if (!RuntimeEnabledFeatures::DocumentPictureInPictureAPIEnabled(
+          GetExecutionContext())) {
     return false;
   }
 
