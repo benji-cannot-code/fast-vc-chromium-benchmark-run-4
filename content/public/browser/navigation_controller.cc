7938b19671fef3cc5f3d26f3c701a4582c1aa471
Sharon Yang <yangsharon@chromium.org>
2023-01-25 17:53:29 +0000

    Set OpenURLParams::is_renderer_intiated default in header file
    All bool fields of OpenURLParams are set in the header file, except
    is_renderer_intiated, which is set to be false by default in the
    constructor. Move this default to the header for consistency.
    
    Bug: 1394513
    Change-Id: Ic3e229aa820163d532258a63ed98dfe15022d6a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4191748
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096846}

--- content/public/browser/navigation_controller.cc
+++ content/public/browser/navigation_controller.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 NavigationController::LoadURLParams::LoadURLParams(const GURL& url)
-    : url(url), is_renderer_initiated(false) {}
+    : url(url) {}
 
 NavigationController::LoadURLParams::LoadURLParams(
     NavigationController::LoadURLParams&&) = default;
