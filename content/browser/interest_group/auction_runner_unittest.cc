b59fa01c836cdbe346c273c349c973e86634e68e
Matt Menke <mmenke@chromium.org>
2023-01-25 23:31:53 +0000

    [FLEDGE] Run reporting scripts after auction completes.
    This is a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/4148715,
    which made a test flaky.
    
    If the frame that ran the auction is destroyed before reporting scripts
    complete, no reports are sent. I plan to add histograms for that in a
    future CL.
    
    There are still a lot of cleanups to do - moving all reporting network
    calls into InterestGroupAuction, and add unit tests for it, is next in
    my queue.
    
    Bug: 1394777
    Change-Id: Ibcb5ed71d100ab4805c75c46663dfbd8a1016b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190585
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097100}

--- content/browser/interest_group/auction_runner_unittest.cc
+++ content/browser/interest_group/auction_runner_unittest.cc
@@ -2046,6 +2046,8 @@ class AuctionRunnerTest : public RenderViewHostTestHarness,
 
     reporter_->Start(base::BindOnce(&AuctionRunnerTest::OnReportingComplete,
                                     base::Unretained(this)));
+    // Invoke callback immediately, so as not to block reporter completion.
+    reporter_->OnNavigateToWinningAdCallback().Run();
   }
 
   void OnReportingComplete() {
