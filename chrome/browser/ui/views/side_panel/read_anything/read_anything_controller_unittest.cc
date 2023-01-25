286ee4d5bfbde76fa992f1bbf0640663f4b508ab
Jocelyn Tran <jocelyntran@google.com>
2023-01-25 16:26:48 +0000

    [Read Anything] Update font list
    This CL removes font choices that do not currently work (Avenir, Comic
    Neue, Poppins) and adds temporary font choices that are available on
    all platforms.
    
    A future CL will remove the temporary fonts and add the fonts from the
    finalized list.
    
    Bug: 1266555
    Change-Id: Ie5e8dac9afd1679ec52211e873883933b2016d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189545
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096806}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_controller_unittest.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_controller_unittest.cc
@@ -98,7 +98,7 @@ class ReadAnythingControllerTest : public TestWithBrowserView {
 };
 
 TEST_F(ReadAnythingControllerTest, ValidIndexUpdatesFontNamePref) {
-  std::string expected_font_name = "Avenir";
+  std::string expected_font_name = "Arial";
 
   MockOnFontChoiceChanged(3);
 
