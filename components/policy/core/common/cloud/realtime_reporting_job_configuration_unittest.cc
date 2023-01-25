b27431c37ce3a3e1e01189469b3436cf12d4a49f
Roman Sorokin <rsorokin@chromium.org>
2023-01-25 09:19:13 +0000

    [Code Health] Migrate FindKey to new base::Value::Dict API
    Migrate FindKey to new base::Value::Dict API in
    realtime_reporting_job_configuration_unittest.cc
    
    Bug: 1406815
    Change-Id: Id20b361ed1d579ec38ad0dabe463dbc049337ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189678
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096663}

--- components/policy/core/common/cloud/realtime_reporting_job_configuration_unittest.cc
+++ components/policy/core/common/cloud/realtime_reporting_job_configuration_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
+#include "base/check_deref.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/test/task_environment.h"
@@ -35,8 +36,6 @@ using testing::_;
 using testing::StrictMock;
 
 namespace policy {
-
-std::vector<std::string> ids = {"id1", "id2", "id3"};
 
 constexpr char kAppPackage[] = "appPackage";
 constexpr char kEventType[] = "eventType";
@@ -80,8 +79,8 @@ class RealtimeReportingJobConfigurationTest : public testing::Test {
     base::Value::Dict context;
     context.SetByDottedPath("browser.userAgent", "dummyAgent");
     base::Value::List events;
-    for (size_t i = 0; i < ids.size(); ++i) {
-      base::Value::Dict event = CreateEvent(ids[i], i);
+    for (size_t i = 0; i < kIds.size(); ++i) {
+      base::Value::Dict event = CreateEvent(kIds[i], i);
       events.Append(std::move(event));
     }
 
@@ -91,7 +90,9 @@ class RealtimeReportingJobConfigurationTest : public testing::Test {
   }
 
  protected:
-  static base::Value::Dict CreateEvent(std::string& event_id, int type) {
+  const std::vector<std::string> kIds = {"id1", "id2", "id3"};
+
+  static base::Value::Dict CreateEvent(const std::string& event_id, int type) {
     base::Value::Dict event;
     event.Set(kAppPackage, kPackage);
     event.Set(kEventType, type);
@@ -210,18 +211,22 @@ TEST_F(RealtimeReportingJobConfigurationTest, ValidatePayload) {
 
   base::Value* events =
       payload->FindListKey(RealtimeReportingJobConfiguration::kEventListKey);
-  EXPECT_EQ(ids.size(), events->GetList().size());
+  EXPECT_EQ(kIds.size(), events->GetList().size());
   int i = -1;
-  for (const auto& event : events->GetList()) {
-    auto* id = event.FindStringKey(kEventId);
-    EXPECT_EQ(ids[++i], *id);
-    auto type = event.FindKey(kAppInstallEvent)->FindIntKey(kEventType);
+  for (const base::Value& event_val : events->GetList()) {
+    const base::Value::Dict& event = event_val.GetDict();
+    const std::string& id = CHECK_DEREF(event.FindString(kEventId));
+    EXPECT_EQ(kIds[++i], id);
+    const absl::optional<int> type =
+        event.FindDict(kAppInstallEvent)->FindInt(kEventType);
+    ASSERT_TRUE(type.has_value());
     EXPECT_EQ(i, *type);
   }
 }
 
 TEST_F(RealtimeReportingJobConfigurationTest, OnURLLoadComplete_Success) {
-  base::Value::Dict response = CreateResponse({ids[0], ids[1], ids[2]}, {}, {});
+  base::Value::Dict response =
+      CreateResponse({kIds[0], kIds[1], kIds[2]}, {}, {});
   EXPECT_CALL(callback_observer_,
               OnURLLoadComplete(&job_, DM_STATUS_SUCCESS,
                                 DeviceManagementService::kSuccess,
@@ -290,7 +295,7 @@ TEST_F(RealtimeReportingJobConfigurationTest, OnURLLoadComplete_UnknownError) {
 
 TEST_F(RealtimeReportingJobConfigurationTest, ShouldRetry_Success) {
   auto response_string =
-      CreateResponseString(CreateResponse({ids[0], ids[1], ids[2]}, {}, {}));
+      CreateResponseString(CreateResponse({kIds[0], kIds[1], kIds[2]}, {}, {}));
   auto should_retry = configuration_->ShouldRetry(
       DeviceManagementService::kSuccess, response_string);
   EXPECT_EQ(DeviceManagementService::Job::NO_RETRY, should_retry);
@@ -299,7 +304,7 @@ TEST_F(RealtimeReportingJobConfigurationTest, ShouldRetry_Success) {
 TEST_F(RealtimeReportingJobConfigurationTest, ShouldRetry_PartialFalure) {
   // Batch failures are retried
   auto response_string =
-      CreateResponseString(CreateResponse({ids[0], ids[1]}, {ids[2]}, {}));
+      CreateResponseString(CreateResponse({kIds[0], kIds[1]}, {kIds[2]}, {}));
   auto should_retry = configuration_->ShouldRetry(
       DeviceManagementService::kSuccess, response_string);
   EXPECT_EQ(DeviceManagementService::Job::RETRY_WITH_DELAY, should_retry);
@@ -308,7 +313,7 @@ TEST_F(RealtimeReportingJobConfigurationTest, ShouldRetry_PartialFalure) {
 TEST_F(RealtimeReportingJobConfigurationTest, ShouldRetry_PermanentFailure) {
   // Permanent failures are not retried.
   auto response_string =
-      CreateResponseString(CreateResponse({ids[0], ids[1]}, {}, {ids[2]}));
+      CreateResponseString(CreateResponse({kIds[0], kIds[1]}, {}, {kIds[2]}));
   auto should_retry = configuration_->ShouldRetry(
       DeviceManagementService::kSuccess, response_string);
   EXPECT_EQ(DeviceManagementService::Job::NO_RETRY, should_retry);
@@ -326,7 +331,7 @@ TEST_F(RealtimeReportingJobConfigurationTest, OnBeforeRetry_PartialBatch) {
   // Only those events whose ids are in failed_uploads should be in the payload
   // after the OnBeforeRetry call.
   auto response_string =
-      CreateResponseString(CreateResponse({ids[0]}, {ids[1]}, {ids[2]}));
+      CreateResponseString(CreateResponse({kIds[0]}, {kIds[1]}, {kIds[2]}));
   configuration_->OnBeforeRetry(DeviceManagementService::kSuccess,
                                 response_string);
   absl::optional<base::Value> payload =
@@ -335,7 +340,7 @@ TEST_F(RealtimeReportingJobConfigurationTest, OnBeforeRetry_PartialBatch) {
       payload->FindListKey(RealtimeReportingJobConfiguration::kEventListKey);
   EXPECT_EQ(1u, events->GetList().size());
   auto& event = events->GetList()[0];
-  EXPECT_EQ(ids[1], *event.FindStringKey(kEventId));
+  EXPECT_EQ(kIds[1], *event.FindStringKey(kEventId));
 }
 
 }  // namespace policy
