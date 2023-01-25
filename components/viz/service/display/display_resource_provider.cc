4cc4029b9103873cb1d16b2430be2ec6887f8941
Saifuddin Hitawala <hitawala@chromium.org>
2023-01-25 19:20:16 +0000

    Multiplanar: Remove resource_format() usages
    This change removes some resource_format() usages thus reducing
    the assumption that a SharedImageFormat can always be
    ResourceFormat. It removes DisplayResourceProvider::GetResourceFormat
    
    Bug: 1378707
    Change-Id: Ie15ba7d35867241f969397bc4f41caee6fc92d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192345
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096911}

--- components/viz/service/display/display_resource_provider.cc
+++ components/viz/service/display/display_resource_provider.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/viz/common/resources/resource_sizes.h"
 #include "gpu/command_buffer/common/shared_image_trace_utils.h"
+#include "gpu/command_buffer/service/shared_image/shared_image_format_utils.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gl/trace_util.h"
 
@@ -86,11 +87,8 @@ bool DisplayResourceProvider::OnMemoryDump(
     // Texture resources may not come with a size, in which case don't report
     // one.
     if (!resource.transferable.size.IsEmpty()) {
-      // TODO (hitawala): Update size check to use multiplanar
-      // SharedImageFormat.
-      uint64_t total_bytes = ResourceSizes::UncheckedSizeInBytesAligned<size_t>(
-          resource.transferable.size,
-          resource.transferable.format.resource_format());
+      uint64_t total_bytes = resource.transferable.format.EstimatedSizeInBytes(
+          resource.transferable.size);
       dump->AddScalar(base::trace_event::MemoryAllocatorDump::kNameSize,
                       base::trace_event::MemoryAllocatorDump::kUnitsBytes,
                       static_cast<uint64_t>(total_bytes));
@@ -169,12 +167,8 @@ const gfx::Size DisplayResourceProvider::GetResourceBackedSize(ResourceId id) {
 }
 
 gfx::BufferFormat DisplayResourceProvider::GetBufferFormat(ResourceId id) {
-  return BufferFormat(GetResourceFormat(id));
-}
-
-ResourceFormat DisplayResourceProvider::GetResourceFormat(ResourceId id) {
   ChildResource* resource = GetResource(id);
-  return resource->transferable.format.resource_format();
+  return gpu::ToBufferFormat(resource->transferable.format);
 }
 
 const gfx::ColorSpace& DisplayResourceProvider::GetOverlayColorSpace(
