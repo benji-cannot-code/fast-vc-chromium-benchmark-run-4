c1fb59ce46bf880fccd99a34180008bcd8a8d41e
Anna Tsvirchkova <atsvirchkova@google.com>
2023-01-25 16:21:28 +0000

    [TTFCC] Add FillingCorrectness metric for payments bottom sheet
    This CL introduces a metric to track if the submitted form required some user editing after autofilling it with the suggestion selected in TTF.
    
    Note: This metric is a separate additional metric to the general FillingCorrectness metric which tracks any autofilled form, no matter by which method.
    
    Bug: 1247698
    Change-Id: I4ed11ca5c1f637fc1593e9499157b9133a4cc9a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161710
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096803}

--- components/autofill/core/browser/metrics/autofill_metrics_utils.h
+++ components/autofill/core/browser/metrics/autofill_metrics_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_METRICS_AUTOFILL_METRICS_UTILS_H_
 
 #include "components/autofill/core/browser/autofill_field.h"
+#include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 
 namespace autofill {
@@ -51,6 +52,11 @@ struct FormGroupFillingStats {
 // Returns the filling status of `field`.
 AutofillMetrics::FieldFillingStatus GetFieldFillingStatus(
     const AutofillField& field);
+
+// Returns the suffix for metrics histogram name depending on the autofill
+// suggestions method used to fill in the value and the form type.
+// E. g. "CreditCard.TouchToFill".
+std::string GetMetricsSuffixByAutofillMethod(AutofillSuggestionMethod method);
 
 }  // namespace autofill
 
