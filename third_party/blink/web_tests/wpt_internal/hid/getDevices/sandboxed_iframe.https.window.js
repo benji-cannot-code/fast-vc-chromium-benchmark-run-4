173dbf0982f226c569e3b91c3859a6c67f83a2cc
Sina Firoozabadi <sinafirooz@google.com>
2023-01-25 22:32:26 +0000

    Reject Web HID requests with an opaque origin
    The Web HID API tracks permissions using the origin of the top-level document in the frame tree. If this document has an opaque origin then there is no way to format the origin for display to the user in permission prompts or to write their decision in the preferences file.
    
    Access to the Web HID API from such contexts should therefore be blocked.
    
    Bug: 1375133
    Change-Id: I7992b2886e882bbbb097b0460114f0a02a02e34f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133535
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097051}

---
+++ third_party/blink/web_tests/wpt_internal/hid/getDevices/sandboxed_iframe.https.window.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+let iframe = document.createElement('iframe');
+
+promise_test(async () => {
+  await new Promise(resolve => {
+    iframe.src = '../resources/open-in-iframe.html';
+    iframe.sandbox.add('allow-scripts');
+    iframe.allow = 'hid';
+    document.body.appendChild(iframe);
+    iframe.addEventListener('load', resolve);
+  });
+
+  await new Promise(resolve => {
+    iframe.contentWindow.postMessage({type: 'GetDevices'}, '*');
+
+    window.addEventListener('message', (messageEvent) => {
+      assert_equals(messageEvent.data, 'Success');
+      resolve();
+    });
+  });
+}, 'Calls to HID APIs from a sandboxed iframe are valid.');
