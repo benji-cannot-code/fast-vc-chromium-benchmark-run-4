414000dd71055eafbf8172b7de13b5e9c4e4b2ef
Nan Lin <linnan@chromium.org>
2023-01-25 21:47:46 +0000

    Log metric for verbose debug report sending
    This is to monitor the status of verbose debug report sending.
    
    Change-Id: Ie95ff6a43e17fa4fcf262e0d58d6f2f898dd5a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4138639
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097012}

--- content/browser/attribution_reporting/attribution_report_network_sender_unittest.cc
+++ content/browser/attribution_reporting/attribution_report_network_sender_unittest.cc
@@ -65,6 +65,9 @@ const char kDebugAggregatableReportUrl[] =
     "https://report.test/.well-known/attribution-reporting/debug/"
     "report-aggregate-attribution";
 
+const char kVerboseDebugReportMetricName[] =
+    "Conversions.VerboseDebugReport.HttpResponseOrNetErrorCode";
+
 AttributionReport DefaultEventLevelReport() {
   return ReportBuilder(
              AttributionInfoBuilder(SourceBuilder(base::Time()).BuildStored())
@@ -1002,6 +1005,8 @@ TEST_F(AttributionReportNetworkSenderTest,
 
 TEST_F(AttributionReportNetworkSenderTest,
        ErrorReportSent_ReportBodySetCorrectly) {
+  base::HistogramTester histograms;
+
   static constexpr char kExpectedReportBody[] =
       R"([{)"
       R"("body":{)"
@@ -1036,10 +1041,15 @@ TEST_F(AttributionReportNetworkSenderTest,
   EXPECT_EQ(kExpectedReportBody, network::GetUploadData(*pending_request));
   EXPECT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
       kErrorReportUrl, ""));
+
+  histograms.ExpectUniqueSample(kVerboseDebugReportMetricName,
+                                net::HttpStatusCode::HTTP_OK, 1);
 }
 
 TEST_F(AttributionReportNetworkSenderTest,
        ErrorReportSent_CallbackInvokedWithNetworkError) {
+  base::HistogramTester histograms;
+
   static constexpr char kErrorReportUrl[] =
       "https://report.test/.well-known/attribution-reporting/debug/verbose";
 
@@ -1066,6 +1076,9 @@ TEST_F(AttributionReportNetworkSenderTest,
       GURL(kErrorReportUrl),
       network::URLLoaderCompletionStatus(net::ERR_CONNECTION_ABORTED),
       network::mojom::URLResponseHead::New(), "");
+
+  histograms.ExpectUniqueSample(kVerboseDebugReportMetricName,
+                                net::ERR_CONNECTION_ABORTED, 1);
 }
 
 }  // namespace content
