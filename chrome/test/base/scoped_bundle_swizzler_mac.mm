5321b70382b251c96f81a265bdb9e4744673610a
Leonard Grey <lgrey@chromium.org>
2023-01-25 23:38:01 +0000

    Mac: cache [TestBundle bundleIdentifier]
    macOS 13's beta analytics code has something roughly like:
    `static _NSSendConfigToAnalytics.appIdentifier =
      [[NSBundle mainBundle] bundleIdentifier];`
    
    If we return an ivar rather than an autoreleased temporary, we can
    work around this.
    
    Bug: 1373041
    Change-Id: Iff088a9bb21976575c5001aa8715749ca5e80062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193230
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097102}

--- chrome/test/base/scoped_bundle_swizzler_mac.mm
+++ chrome/test/base/scoped_bundle_swizzler_mac.mm
@@ -26,6 +26,7 @@ static id g_swizzled_main_bundle = nil;
 
 @implementation TestBundle {
   base::scoped_nsobject<NSBundle> _mainBundle;
+  base::scoped_nsobject<NSString> _bundleID;
 }
 
 + (NSBundle*)mainBundle {
@@ -34,11 +35,13 @@ static id g_swizzled_main_bundle = nil;
 
 - (instancetype)initWithRealBundle:(NSBundle*)bundle {
   _mainBundle.reset([bundle retain]);
+  _bundleID.reset(base::SysUTF8ToNSString(base::mac::BaseBundleID()),
+                  base::scoped_policy::RETAIN);
   return self;
 }
 
 - (NSString*)bundleIdentifier {
-  return base::SysUTF8ToNSString(base::mac::BaseBundleID());
+  return _bundleID.get();
 }
 
 - (void)forwardInvocation:(NSInvocation*)invocation {
