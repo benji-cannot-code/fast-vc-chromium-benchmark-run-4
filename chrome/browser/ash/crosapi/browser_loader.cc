2009340193b0528f94672b71cf25563131e9f1f2
Andrea Orru <andreaorru@chromium.org>
2023-01-25 02:18:13 +0000

    Fix delay in loading stateful Lacros
    This commit fixes a bug which causes a long delay (~1min+ depending
    on the connection) when launching stateful Lacros after an update
    to a different release channel.
    
    The issue is due to CheckRegisteredMayBlock only checking whether
    the Lacros component directory exists, without checking for the
    existence of the actual image.
    
    See http://go/stateful-lacros-delay for further details.
    
    Bug: 1366257
    Change-Id: Ia15107e7f7613e006d17476be44d02cfd8dfa9bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190641
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096575}

--- chrome/browser/ash/crosapi/browser_loader.cc
+++ chrome/browser/ash/crosapi/browser_loader.cc
@@ -227,7 +227,7 @@ void BrowserLoader::Load(LoadCompletionCallback callback) {
 
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, {base::MayBlock()},
-      base::BindOnce(&CheckRegisteredMayBlock, component_manager_),
+      base::BindOnce(&IsInstalledMayBlock, GetLacrosComponentName()),
       base::BindOnce(&BrowserLoader::OnLoadSelection,
                      weak_factory_.GetWeakPtr(), std::move(callback)));
 }
