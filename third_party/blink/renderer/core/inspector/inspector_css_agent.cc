3bf22b9fa225a75720b29a0ca48f64392aed8372
Xiaocheng Hu <xiaochengh@chromium.org>
2023-01-25 22:02:52 +0000

    Propagate tree scope to keyframes
    As a follow up patch of crrev.com/c/4167268, this patch resolves
    tree-scopes references in keyframes by propagating the tree scope
    to the keyframes, and use that to create tree-scope-populated
    CSSValues for interpolations.
    
    Change-Id: I4cb21b0fbfdc9d8072ad77705ae3516c52e41df2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167011
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097028}

--- third_party/blink/renderer/core/inspector/inspector_css_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_css_agent.cc
@@ -1150,8 +1150,10 @@ InspectorCSSAgent::AnimationsForNode(Element* element,
     AtomicString animation_name(animation_data->NameList()[i]);
     if (animation_name == CSSAnimationData::InitialName())
       continue;
-    StyleRuleKeyframes* keyframes_rule = style_resolver.FindKeyframesRule(
-        element, animating_element, animation_name);
+    StyleRuleKeyframes* keyframes_rule =
+        style_resolver
+            .FindKeyframesRule(element, animating_element, animation_name)
+            .rule;
     if (!keyframes_rule)
       continue;
 
