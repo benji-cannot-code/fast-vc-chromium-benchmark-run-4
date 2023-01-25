fc8b98729fb77a51c6f990318c2509abf6a2b2f3
Kush Sinha <sinhak@chromium.org>
2023-01-25 19:01:11 +0000

    CodeHealth: Migrate `base::Value::FindKey()` to v2 API
    Migrate `base::Value::FindKey()` to `base::Value::Dict::Find()` and
    `base::Value::Dict::contains()` in -
    /components/bookmarks/browser
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1406815
    Change-Id: I5ce219b8d74616d96c35893463a60fdbe41d736d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189452
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096896}

--- components/bookmarks/browser/bookmark_codec.h
+++ components/bookmarks/browser/bookmark_codec.h
@@ -14,11 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/guid.h"
 #include "base/hash/md5.h"
+#include "base/values.h"
 #include "components/bookmarks/browser/bookmark_node.h"
-
-namespace base {
-class Value;
-}
 
 namespace bookmarks {
 
@@ -168,7 +165,7 @@ class BookmarkCodec {
   // Decodes the meta info from the supplied sub-node dictionary. The values
   // found will be inserted in meta_info_map with the given prefix added to the
   // start of their keys.
-  void DecodeMetaInfoHelper(const base::Value& dict,
+  void DecodeMetaInfoHelper(const base::Value::Dict& dict,
                             const std::string& prefix,
                             BookmarkNode::MetaInfoMap* meta_info_map);
 
