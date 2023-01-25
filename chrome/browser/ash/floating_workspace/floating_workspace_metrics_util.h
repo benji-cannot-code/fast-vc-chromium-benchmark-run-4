7e6a3feb479b5fd9fdec1fc1d535b8ad3ed79d2d
Geng Li <ligeng@google.com>
2023-01-25 01:03:50 +0000

    add metrics for floating workspace v1
    Bug: b:266467715
    Change-Id: Ie8b35d8d2a54f4f8e9038dfa2ade648702cbbe34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189502
    Commit-Queue: Geng Li <ligeng@google.com>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096541}

---
+++ chrome/browser/ash/floating_workspace/floating_workspace_metrics_util.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_FLOATING_WORKSPACE_FLOATING_WORKSPACE_METRICS_UTIL_H_
+#define CHROME_BROWSER_ASH_FLOATING_WORKSPACE_FLOATING_WORKSPACE_METRICS_UTIL_H_
+
+namespace ash::floating_workspace_metrics_util {
+
+enum RestoredBrowserSessionType {
+  // Unknown browser session.
+  kUnknown = 0,
+  // Local session restored.
+  kLocal,
+  // Remote Session restored.
+  kRemote,
+  kMaxValue = kRemote,
+};
+
+constexpr char kFloatingWorkspaceV1Initialized[] =
+    "Ash.FloatingWorkspace.FloatingWorkspaceV1Initialized";
+constexpr char kFloatingWorkspaceV1RestoredSessionType[] =
+    "Ash.FloatingWorkspace.FloatingWorkspaceV1RestoredSessionType";
+
+void RecordFloatingWorkspaceV1InitializedHistogram();
+void RecordFloatingWorkspaceV1RestoredSessionType(
+    RestoredBrowserSessionType type);
+
+}  // namespace ash::floating_workspace_metrics_util
+
+#endif  // CHROME_BROWSER_ASH_FLOATING_WORKSPACE_FLOATING_WORKSPACE_METRICS_UTIL_H_
