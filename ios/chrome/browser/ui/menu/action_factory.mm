7dd600044059429db94ecf204882a53b2e149ccd
Anudeep Palanki <apalanki@google.com>
2023-01-25 20:30:03 +0000

    [Lens] Add alternate context menu string to Bling
    Default text: https://screenshot.googleplex.com/ApdB77rc5dTtYtk.png
    With alternate text turned on: https://screenshot.googleplex.com/BGjvJKDVvjBhG4z.png
    
    Bug: b/261508050
    Change-Id: I13af6adddfe4e5e51fe023ab6a469ac6eeaeaeb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192952
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Anudeep Palanki <apalanki@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096946}

--- ios/chrome/browser/ui/menu/action_factory.mm
+++ ios/chrome/browser/ui/menu/action_factory.mm
@@ -339,9 +339,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   UIImage* image = UseSymbols() ? CustomSymbolWithPointSize(
                                       kCameraLensSymbol, kSymbolActionPointSize)
                                 : [UIImage imageNamed:@"lens_icon"];
+  int actionTitleMessageId =
+      base::FeatureList::IsEnabled(kEnableLensContextMenuAltText)
+          ? IDS_IOS_CONTEXT_MENU_SEARCHIMAGEWITHGOOGLE_ALT_TEXT
+          : IDS_IOS_CONTEXT_MENU_SEARCHIMAGEWITHGOOGLE;
   UIAction* action =
-      [self actionWithTitle:l10n_util::GetNSString(
-                                IDS_IOS_CONTEXT_MENU_SEARCHIMAGEWITHGOOGLE)
+      [self actionWithTitle:l10n_util::GetNSString(actionTitleMessageId)
                       image:image
                        type:MenuActionType::SearchImageWithLens
                       block:block];
