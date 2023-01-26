fe974707ca2d8efda0a557508763bddbfdb96012
Ce Chen <cch@chromium.org>
2023-01-26 22:16:04 +0000

    Add metadata for omnibox on device tail suggest model at client side.
    Bug: 1372112
    Change-Id: Ifc63a0cec7d2a7de80d1881596fd7e3e3d15af47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198116
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: C Ch <cch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097631}

--- components/optimization_guide/core/prediction_manager.cc
+++ components/optimization_guide/core/prediction_manager.cc
@@ -161,6 +161,10 @@ bool IsModelMetadataTypeOnServerAllowlist(const proto::Any& model_metadata) {
   return model_metadata.type_url() ==
              "type.googleapis.com/"
              "google.internal.chrome.optimizationguide.v1."
+             "OnDeviceTailSuggestModelMetadata" ||
+         model_metadata.type_url() ==
+             "type.googleapis.com/"
+             "google.internal.chrome.optimizationguide.v1."
              "PageEntitiesModelMetadata" ||
          model_metadata.type_url() ==
              "type.googleapis.com/"
