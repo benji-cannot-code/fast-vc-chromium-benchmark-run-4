b7dee23d6e55c103a59477d59c0d58228f2ddfd4
Mustaq Ahmed <mustaq@google.com>
2023-01-26 15:44:18 +0000

    Make SnapScrollController snap_bound dynamic to support stylus slop
    Bug: 1181872
    Change-Id: Iea735994657531210df6f029be8e526281e2f852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179197
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097398}

--- ui/events/gesture_detection/gesture_provider_unittest.cc
+++ ui/events/gesture_detection/gesture_provider_unittest.cc
@@ -1724,7 +1724,7 @@ TEST_F(GestureProviderTest, NoStylusScrollWithinSlop) {
 // slop.
 //
 // TODO(mustaq@chromium.org): Refactor SnapScrollController states to fix this.
-TEST_F(GestureProviderTest, DISABLED_ScrollSnappingWithStylusSlop) {
+TEST_F(GestureProviderTest, SnapScrollWithStylusSlop) {
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitAndEnableFeature(features::kStylusSpecificTapSlop);
 
