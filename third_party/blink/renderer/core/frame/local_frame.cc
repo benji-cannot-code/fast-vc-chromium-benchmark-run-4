540641ac88c3f9f320e680f55e9bedf67a8c63fe
Kunihiko Sakamoto <ksakamoto@chromium.org>
2023-01-26 01:23:34 +0000

    Remove "Save page as Web Bundle" feature (3/4)
    This removes LocalFrame::GetResourceSnapshotForWebBundle.
    
    Bug: 1409346
    Change-Id: I4b0214506206362280eeb85971a1816ba6698366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4151360
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097173}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "mojo/public/cpp/system/message_pipe.h"
-#include "services/data_decoder/public/mojom/resource_snapshot_for_web_bundle.mojom-blink.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/mojom/content_security_policy.mojom-blink.h"
 #include "services/network/public/mojom/source_location.mojom-blink.h"
