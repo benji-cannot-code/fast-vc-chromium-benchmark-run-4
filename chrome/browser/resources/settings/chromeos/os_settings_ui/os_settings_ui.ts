99365c30c9e7fdc1a776c043be00a017c1b01eb0
Wes Okuhara <wesokuhara@google.com>
2023-01-26 22:39:00 +0000

    CrOS Settings: Remove FindShortcutBehavior
    FindShortcutBehavior is no longer used in CrOS Settings, so it can be
    removed. Going forward, FindShortcutMixin provided by cr_elements is
    preferred.
    
    Bug: b:263413628
    Test: browser_tests --gtest_filter='OSSettings*'
    Change-Id: I90cbd3c46f64871d4f5fe8d8193aa717af37cc17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198637
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097652}

--- chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.ts
+++ chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.ts
@@ -348,7 +348,7 @@ class OsSettingsUiElement extends OsSettingsUiElementBase {
     }
   }
 
-  // Override FindShortcutBehavior methods.
+  // Override FindShortcutMixin methods.
   override handleFindShortcut(modalContextOpen: boolean) {
     if (modalContextOpen || !this.showToolbar_) {
       return false;
@@ -359,7 +359,7 @@ class OsSettingsUiElement extends OsSettingsUiElementBase {
     return true;
   }
 
-  // Override FindShortcutBehavior methods.
+  // Override FindShortcutMixin methods.
   override searchInputHasFocus() {
     if (!this.showToolbar_) {
       return false;
