6516718f94ce425279125ddf4695b635cb9c7472
Ehimare Okoyomon <eokoyomon@chromium.org>
2023-01-26 00:39:26 +0000

    Privacy Sandbox Settings: Connect Privacy Sandbox to Sentiment Tracker
    Inform Sentiment Service of consent and notice prompt action responses
    
    Bug: 1378703, b/265792045
    Change-Id: I7961b705c8d91e72fc8cebae2a5880c859adec4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194298
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097149}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
@@ -268,7 +268,6 @@ PrivacySandboxService::GetRequiredPromptType() {
 
 void PrivacySandboxService::PromptActionOccurred(
     PrivacySandboxService::PromptAction action) {
-  InformSentimentService(action);
   RecordPromptActionMetrics(action);
 
   if (base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4)) {
@@ -276,6 +275,7 @@ void PrivacySandboxService::PromptActionOccurred(
     return;
   }
 
+  InformSentimentService(action);
   if (PromptAction::kNoticeShown == action &&
       PromptType::kNotice == GetRequiredPromptType()) {
     // The Privacy Sandbox pref can be enabled when the notice has been
@@ -294,6 +294,10 @@ void PrivacySandboxService::PromptActionOccurred(
 
 void PrivacySandboxService::PromptActionOccurredM1(
     PrivacySandboxService::PromptAction action) {
+  DCHECK(
+      base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4));
+
+  InformSentimentServiceM1(action);
   if (PromptAction::kNoticeAcknowledge == action ||
       PromptAction::kNoticeOpenSettings == action) {
     if (privacy_sandbox::kPrivacySandboxSettings4ConsentRequired.Get()) {
@@ -1426,6 +1430,38 @@ void PrivacySandboxService::InformSentimentService(
   }
 
   sentiment_service_->InteractedWithPrivacySandbox3(area);
+#endif
+}
+
+void PrivacySandboxService::InformSentimentServiceM1(
+    PrivacySandboxService::PromptAction action) {
+#if !BUILDFLAG(IS_ANDROID)
+  if (!sentiment_service_) {
+    return;
+  }
+
+  TrustSafetySentimentService::FeatureArea area;
+  switch (action) {
+    case PromptAction::kNoticeOpenSettings:
+      area = TrustSafetySentimentService::FeatureArea::
+          kPrivacySandbox4NoticeSettings;
+      break;
+    case PromptAction::kNoticeAcknowledge:
+      area = TrustSafetySentimentService::FeatureArea::kPrivacySandbox4NoticeOk;
+      break;
+    case PromptAction::kConsentAccepted:
+      area = TrustSafetySentimentService::FeatureArea::
+          kPrivacySandbox4ConsentAccept;
+      break;
+    case PromptAction::kConsentDeclined:
+      area = TrustSafetySentimentService::FeatureArea::
+          kPrivacySandbox4ConsentDecline;
+      break;
+    default:
+      return;
+  }
+
+  sentiment_service_->InteractedWithPrivacySandbox4(area);
 #endif
 }
 
