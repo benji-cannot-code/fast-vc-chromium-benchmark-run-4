6dc169b28781b06abdb2f123c0b08ffe8aeb572e
YuhanYang <yyhyyh@google.com>
2023-01-26 00:07:03 +0000

    Device split: Get provider function in mojo interface provider
    Implement setup fake provider function and getInputDeviceSettingsProvider function in mojo interface provider.
    
    Bug: b/241965700
    Test: browser_tests
    Change-Id: Ic49ebf596ede5e525ecf6108a04ea139c94232e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181071
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097128}

---
+++ chrome/test/data/webui/settings/chromeos/input_device_mojo_interface_provider_test.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {getInputDeviceSettingsProvider} from 'chrome://os-settings/chromeos/os_settings.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+suite('InputDeviceMojoInterfaceProvider', function() {
+  test('SettingGettingTestProvider', () => {
+    // Test that if there was no provider, getInputDeviceSettingsProvider
+    // method will create one.
+    const fakeProvider = getInputDeviceSettingsProvider();
+    assertTrue(!!fakeProvider);
+    assertEquals(fakeProvider, getInputDeviceSettingsProvider());
+  });
+});
