af44a856d1d68a80934b0a28b6165484089efb9d
Viktor Semeniuk <vsemeniuk@google.com>
2023-01-26 14:35:23 +0000

    [Password Manager] Use group name and icon on checkup details page
    This change updates checkup details list item to use group icon and
    group name. The same logic is used for passwords.google.com.
    
    Bug: 1401001
    Change-Id: I4beba732bf36c6590eb0b5e69cf46085090d9a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188536
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097367}

--- chrome/browser/resources/password_manager/checkup_details_section.ts
+++ chrome/browser/resources/password_manager/checkup_details_section.ts
@@ -68,6 +68,7 @@ export class CheckupDetailsSectionElement extends
 
   private pageTitle_: string;
   private insecurityType_: CheckupSubpage|undefined;
+  private groups_: chrome.passwordsPrivate.CredentialGroup[] = [];
   private allInsecureCredentials_: chrome.passwordsPrivate.PasswordUiEntry[];
   private shownInsecureCredentials_: chrome.passwordsPrivate.PasswordUiEntry[];
   private mutedCompromisedCredentials_:
@@ -84,10 +85,17 @@ export class CheckupDetailsSectionElement extends
   override connectedCallback() {
     super.connectedCallback();
 
-    this.insecureCredentialsChangedListener_ = insecureCredentials => {
-      this.allInsecureCredentials_ = insecureCredentials;
+    const updateGroups = () => {
+      PasswordManagerImpl.getInstance().getCredentialGroups().then(
+          groups => this.groups_ = groups);
     };
 
+    this.insecureCredentialsChangedListener_ = insecureCredentials => {
+      this.allInsecureCredentials_ = insecureCredentials;
+      updateGroups();
+    };
+
+    updateGroups();
     PasswordManagerImpl.getInstance().getInsecureCredentials().then(
         this.insecureCredentialsChangedListener_);
     PasswordManagerImpl.getInstance().addInsecureCredentialsListener(
@@ -209,6 +217,12 @@ export class CheckupDetailsSectionElement extends
           this.activeListItem_.item);
     }
     this.$.moreActionsMenu.close();
+  }
+
+  private getCurrentGroup_(id: number): chrome.passwordsPrivate.CredentialGroup
+      |undefined {
+    return this.groups_.find(
+        group => group.entries.some(entry => entry.id === id));
   }
 }
 
--- chrome/test/data/webui/password_manager/checkup_details_section_test.ts
+++ chrome/test/data/webui/password_manager/checkup_details_section_test.ts
@@ -15,7 +15,7 @@ import {isVisible} from 'chrome://webui-test/test_util.js';
 
 import {TestPasswordManagerProxy} from './test_password_manager_proxy.js';
 import {TestPrefsBrowserProxy} from './test_prefs_browser_proxy.js';
-import {makeInsecureCredential, makePasswordManagerPrefs} from './test_util.js';
+import {createCredentialGroup, makeInsecureCredential, makePasswordManagerPrefs} from './test_util.js';
 
 suite('CheckupDetailsSectionTest', function() {
   const CompromiseType = chrome.passwordsPrivate.CompromiseType;
@@ -105,10 +105,17 @@ suite('CheckupDetailsSectionTest', function() {
         elapsedMinSinceCompromise: 100,
       }),
     ];
+    passwordManager.data.groups = [
+      createCredentialGroup({
+        name: 'Affiliation.com',
+        credentials: passwordManager.data.insecureCredentials,
+      }),
+    ];
 
     const section = document.createElement('checkup-details-section');
     document.body.appendChild(section);
     await passwordManager.whenCalled('getInsecureCredentials');
+    await passwordManager.whenCalled('getCredentialGroups');
     const params = await pluralString.whenCalled('getPluralString');
     await flushTasks();
 
@@ -134,7 +141,7 @@ suite('CheckupDetailsSectionTest', function() {
 
       assertTrue(!!listItemElement);
       assertEquals(
-          expectedCredential.urls.shown,
+          passwordManager.data.groups[0]?.name,
           listItemElement.$.shownUrl.textContent!.trim());
       assertEquals(
           expectedCredential.username,
@@ -166,10 +173,17 @@ suite('CheckupDetailsSectionTest', function() {
       ],
       elapsedMinSinceCompromise: 1,
     })];
+    passwordManager.data.groups = [
+      createCredentialGroup({
+        name: 'Best test site',
+        credentials: passwordManager.data.insecureCredentials,
+      }),
+    ];
 
     const section = document.createElement('checkup-details-section');
     document.body.appendChild(section);
     await passwordManager.whenCalled('getInsecureCredentials');
+    await passwordManager.whenCalled('getCredentialGroups');
     const params = await pluralString.whenCalled('getPluralString');
     await flushTasks();
 
@@ -182,7 +196,9 @@ suite('CheckupDetailsSectionTest', function() {
     const weakItem = listItemElements[0];
 
     assertTrue(!!weakItem);
-    assertEquals('test.com', weakItem.$.shownUrl.textContent!.trim());
+    assertEquals(
+        passwordManager.data.groups[0]!.name,
+        weakItem.$.shownUrl.textContent!.trim());
     assertEquals('viking', weakItem.$.username.textContent!.trim());
 
     assertFalse(!!weakItem.shadowRoot!.querySelector('#compromiseType'));
