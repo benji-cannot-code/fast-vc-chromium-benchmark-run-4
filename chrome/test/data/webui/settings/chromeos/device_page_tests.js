5ac9f9b68d16a2305d77f15b2125ff28527c399c
YuhanYang <yyhyyh@google.com>
2023-01-26 23:59:35 +0000

    Device split: Fetch Connected Touchpads in Touchpad Page
    Call getInputDeviceSettingsProvider in the per-device touchpad page to fetch the touchpad data.
    
    Bug: b/241965700
    Test: browser_tests
    Change-Id: I737469544c42c1011b5180ec078c99430026d475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4195274
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097700}

--- chrome/test/data/webui/settings/chromeos/device_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/device_page_tests.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 
-import {crosAudioConfigMojomWebui, DevicePageBrowserProxyImpl, fakeCrosAudioConfig, FakeInputDeviceSettingsProvider, fakeKeyboards, IdleBehavior, LidClosedBehavior, NoteAppLockScreenSupport, Router, routes, setCrosAudioConfigForTesting, setDisplayApiForTesting, setInputDeviceSettingsProviderForTesting, StorageSpaceState} from 'chrome://os-settings/chromeos/os_settings.js';
+import {crosAudioConfigMojomWebui, DevicePageBrowserProxyImpl, fakeCrosAudioConfig, FakeInputDeviceSettingsProvider, fakeKeyboards, fakeTouchpads, IdleBehavior, LidClosedBehavior, NoteAppLockScreenSupport, Router, routes, setCrosAudioConfigForTesting, setDisplayApiForTesting, setInputDeviceSettingsProviderForTesting, StorageSpaceState} from 'chrome://os-settings/chromeos/os_settings.js';
 import {assert} from 'chrome://resources/ash/common/assert.js';
 import {webUIListenerCallback} from 'chrome://resources/ash/common/cr.m.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
@@ -771,6 +771,13 @@ suite('SettingsDevicePage', function() {
 
   suite(assert(TestNames.PerDeviceTouchpad), function() {
     let perDeviceTouchpadPage;
+    let inputDeviceSettingsProvider;
+
+    suiteSetup(() => {
+      inputDeviceSettingsProvider = new FakeInputDeviceSettingsProvider();
+      inputDeviceSettingsProvider.setFakeKeyboards(fakeTouchpads);
+      setInputDeviceSettingsProviderForTesting(inputDeviceSettingsProvider);
+    });
 
     setup(async function() {
       await init();
@@ -792,6 +799,12 @@ suite('SettingsDevicePage', function() {
           routes.PER_DEVICE_TOUCHPAD, Router.getInstance().getCurrentRoute());
       assertTrue(isVisible(perDeviceTouchpadPage.shadowRoot.querySelector(
           '#perDeviceTouchpadSubpageTitle')));
+    });
+
+    test('per-device touchpad page populated', function() {
+      const connectedTouchpads = perDeviceTouchpadPage.touchpads;
+      assertTrue(!!connectedTouchpads);
+      assertDeepEquals(connectedTouchpads, fakeTouchpads);
     });
   });
 
