62e44f2945c901418faa0c7ea3b89fc6fcfa4cdb
Nidhi Jaju <nidhijaju@chromium.org>
2023-01-26 01:43:34 +0000

    Revert "Propagate tree scope to keyframes"
    This reverts commit 50c25ed46796e593de8358b69af78127fde1c8e9.
    
    Reason for revert: Suspect for blink_wpt_tests and blink_web_tests failing on WebKit Linux Leak builder
    i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20Leak/47313/overview
    
    Original change's description:
    > Propagate tree scope to keyframes
    >
    > As a follow up patch of crrev.com/c/4167268, this patch resolves
    > tree-scopes references in keyframes by propagating the tree scope
    > to the keyframes, and use that to create tree-scope-populated
    > CSSValues for interpolations.
    >
    > Change-Id: I4cb21b0fbfdc9d8072ad77705ae3516c52e41df2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167011
    > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1097028}
    
    Change-Id: I23493cd16574750061a5c1a8764e8a8a09ddc448
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4196234
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097181}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -1911,7 +1911,7 @@ bool StyleResolver::ApplyAnimatedStyle(StyleResolverState& state,
   return apply;
 }
 
-StyleResolver::FindKeyframesRuleResult StyleResolver::FindKeyframesRule(
+StyleRuleKeyframes* StyleResolver::FindKeyframesRule(
     const Element* element,
     const Element* animating_element,
     const AtomicString& animation_name) {
@@ -1925,14 +1925,14 @@ StyleResolver::FindKeyframesRuleResult StyleResolver::FindKeyframesRule(
   for (auto& resolver : resolvers) {
     if (StyleRuleKeyframes* keyframes_rule =
             resolver->KeyframeStylesForAnimation(animation_name)) {
-      return FindKeyframesRuleResult{keyframes_rule, &resolver->GetTreeScope()};
+      return keyframes_rule;
     }
   }
 
   if (StyleRuleKeyframes* keyframes_rule =
           GetDocument().GetStyleEngine().KeyframeStylesForAnimation(
               animation_name)) {
-    return FindKeyframesRuleResult{keyframes_rule, nullptr};
+    return keyframes_rule;
   }
 
   // Match UA keyframe rules after user and author rules.
@@ -1947,13 +1947,13 @@ StyleResolver::FindKeyframesRuleResult StyleResolver::FindKeyframesRule(
   };
   ForEachUARulesForElement(*animating_element, nullptr, func);
   if (matched_keyframes_rule) {
-    return FindKeyframesRuleResult{matched_keyframes_rule, nullptr};
+    return matched_keyframes_rule;
   }
 
   for (auto& resolver : resolvers) {
     resolver->SetHasUnresolvedKeyframesRule();
   }
-  return FindKeyframesRuleResult();
+  return nullptr;
 }
 
 void StyleResolver::InvalidateMatchedPropertiesCache() {
