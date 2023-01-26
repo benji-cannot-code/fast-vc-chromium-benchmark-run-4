430af49d09a853b450a3dd1c8dd79defaeacafc1
Sergio Collazos <sczs@chromium.org>
2023-01-26 19:03:41 +0000

    [ios] Creates Feed Feature Flags
    Adds flags for image caching and synthetic capabilities.
    
    Change-Id: Iaeacbbde331b42b4c181ad5a37f7fcc5fa2637f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194038
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097523}

--- ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
@@ -56,6 +56,14 @@ BASE_FEATURE(kOverrideFeedSettings,
              "OverrideFeedSettings",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableFeedImageCaching,
+             "EnableFeedImageCaching",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kEnableFeedSyntheticCapabilities,
+             "EnableFeedSyntheticCapabilities",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 #pragma mark - Feature parameters
 
 const char kDiscoverFeedSRSReconstructedTemplatesEnabled[] =
@@ -150,6 +158,14 @@ bool IsStickyHeaderDisabledForFollowingFeed() {
 bool IsDotEnabledForNewFollowedContent() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kFeedHeaderSettings, kEnableDotForNewFollowedContent, false);
+}
+
+bool IsFeedImageCachingEnabled() {
+  return base::FeatureList::IsEnabled(kEnableFeedImageCaching);
+}
+
+bool IsFeedSyntheticCapabilitiesEnabled() {
+  return base::FeatureList::IsEnabled(kEnableFeedSyntheticCapabilities);
 }
 
 int FollowingFeedHeaderHeight() {
