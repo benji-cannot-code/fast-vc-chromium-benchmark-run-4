a1e0be32a295bbb6d16a8722ac56946eb14470b8
Ewann Pelle <ewannpv@chromium.org>
2023-01-26 14:46:23 +0000

    [iOS][PT] Display the pinned view when dropping from another app
    This CL displays the pinned view when a drop interaction comes from
    another window/app.
    
    To do that, when an a drop interaction is detected in the grid view,
    it notifies its delegate which triggers `dragSessionEnabled:`.
    
    demo:
    https://drive.google.com/file/d/1qrlcJDaJ39Z7IrN4TUwb9rUqysug6KuA/view?usp=share_link&resourcekey=0-fU1uN2_uJAAvJlsV6qGIJw
    
    Fixed: 1408732
    Change-Id: Ibd8888d240f3201474563888bf5d5d53fda31705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4196635
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097368}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller+private.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/horizontal_layout.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/plus_sign_cell.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/features.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/suggested_actions/suggested_actions_delegate.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/suggested_actions/suggested_actions_grid_cell.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/suggested_actions/suggested_actions_view_controller.h"
@@ -949,6 +950,22 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
                                          toIndex:destinationIndex
                               placeholderContext:context];
     }
+  }
+}
+
+- (void)collectionView:(UICollectionView*)collectionView
+    dropSessionDidEnter:(id<UIDropSession>)session {
+  if (IsPinnedTabsEnabled()) {
+    // Notify the delegate that a drag cames from another app.
+    [self.delegate gridViewControllerDragSessionWillBegin:self];
+  }
+}
+
+- (void)collectionView:(UICollectionView*)collectionView
+     dropSessionDidEnd:(id<UIDropSession>)session {
+  if (IsPinnedTabsEnabled()) {
+    // Notify the delegate that a drag ends from another app.
+    [self.delegate gridViewControllerDropAnimationDidEnd:self];
   }
 }
 
