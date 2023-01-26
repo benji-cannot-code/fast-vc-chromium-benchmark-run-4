5b75086d580ca8d79c0c6b39aa131499df582a73
Frank Liberato <liberato@chromium.org>
2023-01-26 18:53:36 +0000

    Don't show Document PiP WebContents in capture picker.
    While it works, we expect that it won't be a common use-case to
    capture a document Picture in Picture window based on early feedback.
    Turn it off for now, since it can be fairly confusing to the user in
    every case we've identified.
    
    Change-Id: I4ce297c1c7ed27d7026bacd14eccbd3bdc5a3534
    Bug: 1410387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4191758
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097517}

--- chrome/browser/media/webrtc/capture_policy_utils.cc
+++ chrome/browser/media/webrtc/capture_policy_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/picture_in_picture/picture_in_picture_window_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
@@ -165,13 +166,18 @@ DesktopMediaList::WebContentsFilter GetIncludableWebContentsFilter(
       return base::BindRepeating(
           [](const GURL& request_origin, content::WebContents* web_contents) {
             DCHECK(web_contents);
-            return url::IsSameOriginWith(
-                request_origin,
-                web_contents->GetLastCommittedURL().DeprecatedGetOriginAsURL());
+            return !PictureInPictureWindowManager::IsChildWebContents(
+                       web_contents) &&
+                   url::IsSameOriginWith(request_origin,
+                                         web_contents->GetLastCommittedURL()
+                                             .DeprecatedGetOriginAsURL());
           },
           request_origin);
     default:
-      return base::BindRepeating([](content::WebContents* wc) { return true; });
+      return base::BindRepeating([](content::WebContents* web_contents) {
+        DCHECK(web_contents);
+        return !PictureInPictureWindowManager::IsChildWebContents(web_contents);
+      });
   }
 }
 
