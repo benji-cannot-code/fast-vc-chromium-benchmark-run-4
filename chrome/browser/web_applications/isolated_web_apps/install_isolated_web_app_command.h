4d30311ac0251bfe0036a5360c811ca03c65ef98
Christian Flach <cmfcmf@chromium.org>
2023-01-26 15:22:58 +0000

    iwa: Add explicit signature and verification step during installation
    As a new, initial installation step, this CL uses the new
    `IsolatedWebAppResponseReaderFactory` to read the Web Bundle as if
    it wanted to read a file from the Web Bundle. This makes sure that
    integrity block, signatures, and metadata are valid, and that the
    Web Bundle's public key is trusted.
    
    Bug: 1366309
    Change-Id: I0e90a6d8b836c00d4ded426e03aa3e3e747ba966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173793
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Sonja Laurila <laurila@google.com>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097386}

--- chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.h
+++ chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/expected.h"
 #include "base/values.h"
 #include "chrome/browser/web_applications/commands/web_app_command.h"
+#include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_response_reader_factory.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/web_app_id.h"
@@ -84,6 +85,20 @@ class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
           void(base::expected<InstallIsolatedWebAppCommandSuccess,
                               InstallIsolatedWebAppCommandError>)> callback);
 
+  // Same constructor as above, but additionally exposes the
+  // `response_reader_factory` for providing a mock factory in testing.
+  explicit InstallIsolatedWebAppCommand(
+      const IsolatedWebAppUrlInfo& isolation_info,
+      const IsolationData& isolation_data,
+      std::unique_ptr<content::WebContents> web_contents,
+      std::unique_ptr<WebAppUrlLoader> url_loader,
+      content::BrowserContext& browser_context,
+      base::OnceCallback<
+          void(base::expected<InstallIsolatedWebAppCommandSuccess,
+                              InstallIsolatedWebAppCommandError>)> callback,
+      std::unique_ptr<IsolatedWebAppResponseReaderFactory>
+          response_reader_factory);
+
   InstallIsolatedWebAppCommand(const InstallIsolatedWebAppCommand&) = delete;
   InstallIsolatedWebAppCommand& operator=(const InstallIsolatedWebAppCommand&) =
       delete;
@@ -114,6 +129,11 @@ class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
                   std::map<GURL, std::vector<SkBitmap>> icons_map,
                   std::map<GURL, int /*http_status_code*/> icons_http_results);
 
+  void CheckTrustAndSignatures();
+  void CheckTrustAndSignaturesOfBundle(const base::FilePath& path);
+  void OnTrustAndSignaturesChecked(
+      absl::optional<IsolatedWebAppResponseReaderFactory::Error> error);
+
   void CreateStoragePartition();
 
   void LoadUrl();
@@ -140,6 +160,8 @@ class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
 
   IsolatedWebAppUrlInfo isolation_info_;
   IsolationData isolation_data_;
+
+  std::unique_ptr<IsolatedWebAppResponseReaderFactory> response_reader_factory_;
 
   std::unique_ptr<content::WebContents> web_contents_;
 
--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_reader_registry.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_reader_registry.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/functional/overloaded.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/ranges/algorithm.h"
@@ -226,31 +225,8 @@ IsolatedWebAppReaderRegistry::GetStatusFromError(
 IsolatedWebAppReaderRegistry::ReadResponseError
 IsolatedWebAppReaderRegistry::ReadResponseError::ForError(
     const IsolatedWebAppResponseReaderFactory::Error& error) {
-  return ForOtherError(absl::visit(
-      base::Overloaded{
-          [](const web_package::mojom::BundleIntegrityBlockParseErrorPtr&
-                 error) {
-            return base::StringPrintf("Failed to parse integrity block: %s",
-                                      error->message.c_str());
-          },
-          [](const IntegrityBlockError& error) {
-            return base::StringPrintf("Failed to validate integrity block: %s",
-                                      error.message.c_str());
-          },
-          [](const web_package::SignedWebBundleSignatureVerifier::Error&
-                 error) {
-            return base::StringPrintf("Failed to verify signatures: %s",
-                                      error.message.c_str());
-          },
-          [](const web_package::mojom::BundleMetadataParseErrorPtr& error) {
-            return base::StringPrintf("Failed to parse metadata: %s",
-                                      error->message.c_str());
-          },
-          [](const MetadataError& error) {
-            return base::StringPrintf("Failed to validate metadata: %s",
-                                      error.message.c_str());
-          }},
-      error));
+  return ForOtherError(
+      IsolatedWebAppResponseReaderFactory::ErrorToString(error));
 }
 
 // static
