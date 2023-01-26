e2cc2f5792352ac290cc857ee162efa9a108d80e
Ming-Ying Chung <mych@chromium.org>
2023-01-26 02:47:40 +0000

    [bfcache] Clean up legacy TTL param and replace its usage with new flag
    - Remove the logic that uses legacy TTL param from `content/browser/renderer_host/back_forward_cache_impl.cc`
    - Move the dedicated TTL feature flag `kBackForwardCacheTimeToLiveControl` to `content/public/common/content_features.h` so that it can replace the usage of legacy param `kBackForwardCache:TimeToLiveInBackForwardCacheInSeconds`
    
    Details:
    1. In the previous launch crrv/c/3924019, BackForwardCache's default TTL
       has been updated to 600 seconds. The launch also uses a new dedicated
       feature flag `kBackForwardCacheTimeToLiveControl` to overwrite the
       default from server-side for the older clients.
    2. However, in the gigantic flag `kBackForwardCache`, there is a param
       called `TimeToLiveInBackForwardCacheInSeconds` which can also
       overwrite the default flag. Due to historical reason, the server-side
       Finch config still set this flag to 180 seconds.
    3. This CL replaces all the references to the legacy param
       `kBackForwardCache:TimeToLiveInBackForwardCacheInSeconds` to the new flag `kBackForwardCacheTimeToLiveControl`.
    
    Bug: 1305878
    Change-Id: Ia6f5f2c84b1a7f219c31ef681b9ca0a918674ea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177505
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097212}

--- chrome/browser/task_manager/task_manager_browsertest.cc
+++ chrome/browser/task_manager/task_manager_browsertest.cc
@@ -1472,8 +1472,9 @@ class PrerenderTaskBrowserTest : public TaskManagerBrowserTest {
     feature_list_.InitWithFeaturesAndParameters(
         /*enabled_features=*/
         {
-            {features::kBackForwardCache,
-             {{"TimeToLiveInBackForwardCacheInSeconds", "3600"}}},
+            {features::kBackForwardCache, {{}}},
+            {features::kBackForwardCacheTimeToLiveControl,
+             {{"time_to_live_seconds", "3600"}}},
             {features::kOmniboxTriggerForPrerender2, {}},
         },
         /*disabled_features=*/{});
