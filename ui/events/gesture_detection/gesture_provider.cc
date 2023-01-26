40ee119ed7ac582081f62ae60d3320c85b9bdcb8
Mustaq Ahmed <mustaq@google.com>
2023-01-26 15:39:13 +0000

    Define a separate slop region for stylus tap gesture
    Adds max_stylus_move_in_pixels_for_click_ to parallel existing touch
    specific parameter in GestureConfiguration, and makes it a bit bigger
    to make clicking with a stylus easier.  Since there is less friction
    with a stylus than a finger, it is much easier to slip by 10px+ when
    making a tap gesture with a stylus.
    
    Bug: 1181872
    Change-Id: Ie92af9a317b25fd7915d2a6e2916bf2d96a6e1b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117511
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097396}

--- ui/events/gesture_detection/gesture_provider.cc
+++ ui/events/gesture_detection/gesture_provider.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cmath>
 
-#include "base/auto_reset.h"
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
