07eed8c862d733405a9da51facd5c0055e36a32f
rajendrant <rajendrant@chromium.org>
2023-01-26 21:18:50 +0000

    Avoid crash when opt target is empty
    Records metric on the cases when opt target is empty.
    
    Fixed: 1410523
    Change-Id: I6e82306683a88e1c34f8c309e6328550aae35c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4196864
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097590}

--- components/optimization_guide/core/prediction_model_download_manager.cc
+++ components/optimization_guide/core/prediction_model_download_manager.cc
@@ -225,7 +225,10 @@ void PredictionModelDownloadManager::OnDownloadSucceeded(
 
   base::UmaHistogramBoolean(
       "OptimizationGuide.PredictionModelDownloadManager.DownloadSucceeded",
-      true);
+      optimization_target.has_value());
+  if (!optimization_target) {
+    return;
+  }
 
   base::FilePath base_model_dir =
       get_base_model_dir_for_download_callback_.Run(*optimization_target);
@@ -236,7 +239,7 @@ void PredictionModelDownloadManager::OnDownloadSucceeded(
                      download_file_path, base_model_dir,
                      /*delete_file_on_error=*/true),
       base::BindOnce(&PredictionModelDownloadManager::StartUnzipping,
-                     ui_weak_ptr_factory_.GetWeakPtr(), optimization_target,
+                     ui_weak_ptr_factory_.GetWeakPtr(), *optimization_target,
                      download_file_path, base_model_dir));
 }
 
@@ -313,16 +316,14 @@ bool PredictionModelDownloadManager::VerifyDownload(
 }
 
 void PredictionModelDownloadManager::StartUnzipping(
-    absl::optional<proto::OptimizationTarget> optimization_target,
+    proto::OptimizationTarget optimization_target,
     const base::FilePath& download_file_path,
     const base::FilePath& base_model_dir,
     bool is_verify_success) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (!is_verify_success) {
-    if (optimization_target) {
-      NotifyModelDownloadFailed(*optimization_target);
-    }
+    NotifyModelDownloadFailed(optimization_target);
     return;
   }
 
@@ -339,7 +340,7 @@ void PredictionModelDownloadManager::StartUnzipping(
 }
 
 void PredictionModelDownloadManager::OnDownloadUnzipped(
-    absl::optional<proto::OptimizationTarget> optimization_target,
+    proto::OptimizationTarget optimization_target,
     const base::FilePath& original_file_path,
     const base::FilePath& base_model_dir,
     bool success) {
@@ -350,9 +351,7 @@ void PredictionModelDownloadManager::OnDownloadUnzipped(
       FROM_HERE, base::GetDeleteFileCallback(original_file_path));
 
   if (!success) {
-    if (optimization_target) {
-      NotifyModelDownloadFailed(*optimization_target);
-    }
+    NotifyModelDownloadFailed(optimization_target);
     RecordPredictionModelDownloadStatus(
         PredictionModelDownloadStatus::kFailedCrxUnzip);
     return;
@@ -430,7 +429,7 @@ PredictionModelDownloadManager::ProcessUnzippedContents(
 }
 
 void PredictionModelDownloadManager::NotifyModelReady(
-    absl::optional<proto::OptimizationTarget> optimization_target,
+    proto::OptimizationTarget optimization_target,
     const base::FilePath& base_model_dir,
     const absl::optional<proto::PredictionModel>& model) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -439,9 +438,7 @@ void PredictionModelDownloadManager::NotifyModelReady(
     // Clean up model dir.
     background_task_runner_->PostTask(
         FROM_HERE, base::GetDeletePathRecursivelyCallback(base_model_dir));
-    if (optimization_target) {
-      NotifyModelDownloadFailed(*optimization_target);
-    }
+    NotifyModelDownloadFailed(optimization_target);
     return;
   }
 
