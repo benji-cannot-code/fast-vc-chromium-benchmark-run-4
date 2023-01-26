aa5d0c1b3fbc5218994757ab0aa51a4058f09634
Ewann Pelle <ewannpv@chromium.org>
2023-01-26 16:39:42 +0000

    [iOS][PT] Add Tab Strip support
    This CL adds Tab Strip support for pinned tabs.
    When a Tab Strip item is pinned, we only display its favicon and
    its width is set to 78 pts.
    
    When the pinned tabs feature is disabled, `_pinnedTabCount` equals to 0,
    that means updated computations are not impacted.
    
    Bug: 1409866
    Change-Id: If5a78f8e5a1958e935248b824cc3cead5ddc0bcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189681
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097422}

--- ios/chrome/browser/ui/tabs/tab_view.h
+++ ios/chrome/browser/ui/tabs/tab_view.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, assign, getter=isCollapsed) BOOL collapsed;
 @property(nonatomic, strong) UIImage* background;
 @property(nonatomic, assign) BOOL incognitoStyle;
+@property(nonatomic, assign) BOOL pinned;
 
 // Designated initializer.  Creates a TabView with frame equal to CGRectZero.
 // If `emptyView` is YES, it creates a TabView without buttons or spinner.
--- ios/chrome/browser/ui/tabs/tab_view.mm
+++ ios/chrome/browser/ui/tabs/tab_view.mm
@@ -73,7 +73,11 @@ UIImage* DefaultFaviconImage() {
 
   // Background image for this tab.
   UIImageView* _backgroundImageView;
+
   BOOL _incognitoStyle;
+
+  // Whether the Tab is pinned or not.
+  BOOL _pinned;
 
   // Set to YES when the layout constraints have been initialized.
   BOOL _layoutConstraintsInitialized;
@@ -111,6 +115,7 @@ UIImage* DefaultFaviconImage() {
 
 - (id)initWithEmptyView:(BOOL)emptyView selected:(BOOL)selected {
   if ((self = [super initWithFrame:CGRectZero])) {
+    _pinned = NO;
     [self setOpaque:NO];
     [self createCommonViews];
     if (!emptyView)
@@ -140,8 +145,10 @@ UIImage* DefaultFaviconImage() {
 }
 
 - (void)setCollapsed:(BOOL)collapsed {
-  if (_collapsed != collapsed)
+  // If the item is pinned the `_closeButton` should remain hidden.
+  if (_collapsed != collapsed && !_pinned) {
     [_closeButton setHidden:collapsed];
+  }
 
   _collapsed = collapsed;
 }
@@ -179,6 +186,16 @@ UIImage* DefaultFaviconImage() {
                                         ? UIUserInterfaceStyleDark
                                         : UIUserInterfaceStyleUnspecified;
   return;
+}
+
+- (void)setPinned:(BOOL)pinned {
+  if (_pinned == pinned) {
+    return;
+  }
+  _pinned = pinned;
+  _titleLabel.hidden = pinned;
+  _closeButton.hidden = pinned;
+  [self setFrame:self.frame];
 }
 
 - (void)startProgressSpinner {
