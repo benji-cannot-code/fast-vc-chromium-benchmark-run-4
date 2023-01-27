2338a968ed853669f7b691b8ff58715086fffc3b
Kelvin Jiang <kelvinjiang@chromium.org>
2023-01-27 03:39:39 +0000

    [Side Panel] Fix crash if browser closes before side panel entry loads
    Fix a crash that happens if the browser closes with the side panel open
    but the active entry has not been set, which can happen when the entry
    to be shown is currently loading. This triggers the
    OnTabStripModelChanged event where GetContentView() returns true but
    current_entry_ is null. The crash was caused by attempting to inspect
    a property of current_entry_ when null.
    
    The fix is very simple: check current_entry_ to be non-null before
    attempting to use it in IsExtensionEntry(current_entry_).
    
    A follow up task could involve trying to abort the load when the browser
    closes, documented in crbug.com/1410685
    
    Bug: 1408947
    Change-Id: Ic8a1ae8e25b89ffa6ed2370cd9c1cea32e9aeb4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4185060
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097775}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator_unittest.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator_unittest.cc
@@ -78,7 +78,7 @@ class SidePanelCoordinatorTest : public TestWithBrowserView {
         base::BindRepeating([]() { return std::make_unique<views::View>(); })));
 
     coordinator_ = browser_view()->side_panel_coordinator();
-    coordinator_->SetNoDelaysForTesting();
+    coordinator_->SetNoDelaysForTesting(true);
     global_registry_ = coordinator_->global_registry_;
 
     // Verify the first tab has one entry, kSideSearch.
@@ -1011,6 +1011,18 @@ TEST_F(SidePanelCoordinatorTest, ComboboxAdditionsDoNotChangeSelection) {
                 ->GetKeyAt(selected_index.value())
                 .id(),
             later_sorted_entry);
+}
+
+// Test that a crash does not occur when the browser is closed when the side
+// panel view is shown but before the entry to be displayed has finished
+// loading. Regression for crbug.com/1408947.
+TEST_F(SidePanelCoordinatorTest, BrowserClosedBeforeEntryLoaded) {
+  EXPECT_FALSE(browser_view()->unified_side_panel()->GetVisible());
+
+  // Allow content delays to more closely mimic real behavior.
+  coordinator_->SetNoDelaysForTesting(false);
+  coordinator_->Toggle();
+  browser_view()->Close();
 }
 
 // Test that Show() shows the contextual extension entry if available for the
