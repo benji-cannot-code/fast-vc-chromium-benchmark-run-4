8e879b927db340fa28d0487997c690f4d977241d
Lijin Shen <lazzzis@google.com>
2023-01-27 21:53:12 +0000

    Fix both StartSurface and TabSwitcher recorded on a single back press
    StartSurface and TabSwitcher are both recorded when exiting tab switcher
    when SS is enabled.
    
    By design, a back press should be handled by only one handler.
    Recording two back press handlers can cause confusion for data
    analysis and debugging.
    
    This CL makes TabSwitcher recorded only when SS is disabled.
    
    Ref: https://crrev.com/c/4116635
    
    Bug: 1347089
    Change-Id: I46b15d74a8caf603f434dee0922d711145dac14c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4195696
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098131}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
@@ -109,6 +109,8 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
     private final BrowserControlsStateProvider.Observer mBrowserControlsObserver;
     private final ViewGroup mContainerView;
     private final TabContentManager mTabContentManager;
+    private final boolean mIsStartSurfaceEnabled;
+    private final boolean mIsStartSurfaceRefactorEnabled;
     private final MultiWindowModeStateDispatcher mMultiWindowModeStateDispatcher;
     private final MultiWindowModeStateDispatcher.MultiWindowModeObserver mMultiWindowModeObserver;
     private final ObservableSupplierImpl<Boolean> mBackPressChangedSupplier =
@@ -293,6 +295,8 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
         mMode = mode;
         mContainerViewModel.set(MODE, mode);
         mContext = context;
+        mIsStartSurfaceEnabled = ReturnToChromeUtil.isStartSurfaceEnabled(context);
+        mIsStartSurfaceRefactorEnabled = ReturnToChromeUtil.isStartSurfaceRefactorEnabled(context);
 
         if (incognitoReauthControllerSupplier != null) {
             mCallbackController = new CallbackController();
@@ -496,6 +500,7 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
             mContainerViewModel.set(BOTTOM_PADDING,
                     (int) context.getResources().getDimension(R.dimen.tab_grid_bottom_padding));
             if (backPressManager != null && BackPressManager.isEnabled()) {
+                assert !mIsStartSurfaceEnabled || mIsStartSurfaceRefactorEnabled;
                 backPressManager.addHandler(this, BackPressHandler.Type.TAB_SWITCHER);
                 notifyBackPressStateChangedInternal();
             }
@@ -832,7 +837,9 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
     @Override
     public boolean onBackPressed() {
         boolean ret = onBackPressedInternal();
-        if (ret) {
+        if (ret && (!mIsStartSurfaceEnabled || mIsStartSurfaceRefactorEnabled)) {
+            // When SS is enabled or refactor is disabled, StartSurfaceMediator will consume back
+            // press and call this method if necessary.
             BackPressManager.record(BackPressHandler.Type.TAB_SWITCHER);
         }
         return ret;
