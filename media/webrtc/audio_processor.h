2ff04fce36066cd38c37bd2a1d73f7e8047e4675
Alessio Bazzica <alessiob@chromium.org>
2023-01-27 13:17:30 +0000

    Let WebRTC APM tell whether the playout reference is needed
    Currently, we determine if APM needs the playout reference based on
    two media constraints (namely, echo cancellation and
    automatic gain control). However, this decision should be taken by
    APM based on its internal configuration, which is determined not
    only by the active media constraints, but also by any enabled
    features and field-trials.
    
    This CL takes a first step towards the solution described above and
    deprecates `AudioProcessingSettings::NeedPlayoutReference`, which
    is replaced by `AudioProcessor::needs_playout_reference`.
    
    Next steps (in two separate CLs):
    - Add `webrtc::AudioProcessing::NeedsPlayoutReference`
    - Initialize `AudioProcessor::needs_playout_reference_` using
      the newly added method in WebRTC APM
    
    Tested:
    - Chromium compiled on CrOS and test call made using the AGC1
      configuration with browser APM and CRAS
    - Bit-exactness verified on one AEC dump with both the AGC1 and
      the AGC2 setups between ToT audioproc_f and a modified version
      of it in which the playout reference data is not passed (credits
      to saza@chromium.org)
    - https://jsfiddle.net/ynmrdc3g/ on Mac with the enabled/disabled
      combos of chrome://flags/#chrome-wide-echo-cancellation and
      `echoCancellation`
    
    The change is added behind a kill-switch.
    
    Bug: chromium:1410129
    Change-Id: Ie93f2de143bc3cae4be7a4804868e07b375e368a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193794
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Sam Zackrisson <saza@chromium.org>
    Commit-Queue: Ale Bzk <alessiob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097869}

--- media/webrtc/audio_processor.h
+++ media/webrtc/audio_processor.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_processing.h"
 #include "media/base/audio_push_fifo.h"
 #include "media/webrtc/audio_delay_stats_reporter.h"
+#include "media/webrtc/webrtc_features.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/webrtc/modules/audio_processing/include/audio_processing.h"
 #include "third_party/webrtc/modules/audio_processing/include/audio_processing_statistics.h"
@@ -84,7 +85,8 @@ class COMPONENT_EXPORT(MEDIA_WEBRTC) AudioProcessor {
       const media::AudioParameters& input_format,
       const media::AudioParameters& output_format,
       rtc::scoped_refptr<webrtc::AudioProcessing> webrtc_audio_processing,
-      bool stereo_mirroring);
+      bool stereo_mirroring,
+      bool needs_playout_reference);
 
   ~AudioProcessor();
 
@@ -163,6 +165,9 @@ class COMPONENT_EXPORT(MEDIA_WEBRTC) AudioProcessor {
       const AudioParameters& input_format,
       const AudioProcessingSettings& settings);
 
+  // Returns true if `OnPlayoutData()` should be called.
+  bool needs_playout_reference() const { return needs_playout_reference_; }
+
  private:
   friend class AudioProcessorTest;
 
@@ -201,6 +206,9 @@ class COMPONENT_EXPORT(MEDIA_WEBRTC) AudioProcessor {
   // If true, then the audio processor should swap the left and right channel of
   // captured stereo audio.
   const bool stereo_mirroring_;
+
+  // If true, `OnPlayoutData()` should be called.
+  const bool needs_playout_reference_;
 
   // Members accessed only by the owning sequence:
 
