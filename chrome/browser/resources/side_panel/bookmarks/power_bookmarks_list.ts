173960ffde729fb19b084d90b4774d9ac69a51a5
John Lee <johntlee@chromium.org>
2023-01-27 00:08:44 +0000

    Bookmarks++: Fix iron-list not resizing when item height changes
    This CL fixes a bug where if you switch between compact and visual
    mode, the iron-list does not resize itself with the new heights.
    
    Bug: 1365606
    Change-Id: I5ebd75f94534c76b36dd2f2df29edafe4ff01087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198254
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097703}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
@@ -33,6 +33,7 @@ import {CrToastElement} from '//resources/cr_elements/cr_toast/cr_toast.js';
 import {loadTimeData} from '//resources/js/load_time_data.js';
 import {PluralStringProxyImpl} from '//resources/js/plural_string_proxy.js';
 import {listenOnce} from '//resources/js/util_ts.js';
+import {IronListElement} from '//resources/polymer/v3_0/iron-list/iron-list.js';
 import {DomRepeatEvent, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {ActionSource} from './bookmarks.mojom-webui.js';
@@ -51,6 +52,7 @@ export interface PowerBookmarksListElement {
     contextMenu: CrLazyRenderElement<PowerBookmarksContextMenuElement>,
     deletionToast: CrLazyRenderElement<CrToastElement>,
     powerBookmarksContainer: HTMLElement,
+    shownBookmarksIronList: IronListElement,
     sortMenu: CrActionMenuElement,
     editDialog: PowerBookmarksEditDialogElement,
   };
@@ -565,6 +567,7 @@ export class PowerBookmarksListElement extends PolymerElement {
     event.stopPropagation();
     this.$.sortMenu.close();
     this.compact_ = false;
+    this.$.shownBookmarksIronList.notifyResize();
   }
 
   private onCompactViewClicked_(event: MouseEvent) {
@@ -572,6 +575,7 @@ export class PowerBookmarksListElement extends PolymerElement {
     event.stopPropagation();
     this.$.sortMenu.close();
     this.compact_ = true;
+    this.$.shownBookmarksIronList.notifyResize();
   }
 
   private onAddTabClicked_() {
