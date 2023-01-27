31c41f562702594df702e9b1089f08a4dcf62a46
Elly Fong-Jones <ellyjones@chromium.org>
2023-01-27 23:09:06 +0000

    sharing: remove #persist-share-hub-on-app-switch
    We ran this experiment on beta for a while and didn't see much benefit,
    never tried it for real on stable, and we're not likely to pick it back
    up.
    
    Fixed: 1410841
    Change-Id: Icfbe7a329fe1822b7655a082c121c76885091b72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201128
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098170}

--- chrome/browser/share/share_features.cc
+++ chrome/browser/share/share_features.cc
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace share {
 
-BASE_FEATURE(kPersistShareHubOnAppSwitch,
-             "PersistShareHubOnAppSwitch",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kScreenshotsForAndroidV2,
              "ScreenshotsForAndroidV2",
              base::FEATURE_DISABLED_BY_DEFAULT);
--- chrome/browser/share/share_features.h
+++ chrome/browser/share/share_features.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace share {
 
-BASE_DECLARE_FEATURE(kPersistShareHubOnAppSwitch);
 BASE_DECLARE_FEATURE(kScreenshotsForAndroidV2);
 BASE_DECLARE_FEATURE(kSharingDesktopScreenshotsEdit);
 BASE_DECLARE_FEATURE(kUpcomingSharingFeatures);
