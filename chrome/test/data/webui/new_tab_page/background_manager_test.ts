22e1a77f66f5a0554eb96caa9eec1aaed5461033
Paul Adedeji <pauladedeji@google.com>
2023-01-27 20:38:03 +0000

    [ntp][side-panel] Add metric that tracks NTP image source
    Metric records whether user is using Classic Chrome, an NTP theme, a CWS theme, refresh daily, or an uploaded background.
    
    Change-Id: Icb4306019e3654c9db9103df7aef914386bd18e3
    Bug: 1384258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193130
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098088}

--- chrome/test/data/webui/new_tab_page/background_manager_test.ts
+++ chrome/test/data/webui/new_tab_page/background_manager_test.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://webui-test/mojo_webui_test_support.js';
 
 import {BackgroundManager} from 'chrome://new-tab-page/new_tab_page.js';
+import {NtpBackgroundImageSource} from 'chrome://new-tab-page/new_tab_page.mojom-webui.js';
 import {assertEquals, assertFalse, assertNotReached, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 import {createBackgroundImage} from './test_support.js';
@@ -92,6 +93,7 @@ suite('NewTabPageBackgroundManagerTest', () => {
       positionX: 'left',
       positionY: 'top',
       scrimDisplay: 'none',
+      imageSource: NtpBackgroundImageSource.kFirstPartyThemeWithoutDailyRefresh,
     });
 
     // Assert.
