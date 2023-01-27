5ee43798f7a95c77cf498fddaac60c3b4274843a
Peter Boström <pbos@chromium.org>
2023-01-27 18:06:22 +0000

    Use Kernel32Version() for Pre-RS5 API contract
    This is supposed to fetch the "real" OS version inside
    GetPreRS5UniversalApiContractVersion() even if Chrome runs in
    compatibility mode. Before this change crash reports that look like
    pre-Win10 hit this NOTREACHED() in suspicious numbers even though we're
    not releasing that anymore.
    
    The fix also drive-by changes OSInfo::Kernel* methods that were
    essentially static to be static so we don't need to call
    OSInfo::GetInstance() to fetch non-members.
    
    Bug: 1404448
    Change-Id: I99ee4a0c4680aee35b5d55d8b44ca4b14fe7a00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4196866
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097992}

--- chrome/credential_provider/gaiacp/gaia_credential_provider_module.cc
+++ chrome/credential_provider/gaiacp/gaia_credential_provider_module.cc
@@ -141,8 +141,7 @@ void CGaiaCredentialProviderModule::LogProcessDetails() {
 
   LOGFN(INFO) << "GCPW Initialized in " << process_name
               << " GCPW Version: " << (CHROME_VERSION_STRING)
-              << " Windows Build: "
-              << base::win::OSInfo::GetInstance()->Kernel32BaseVersion()
+              << " Windows Build: " << base::win::OSInfo::Kernel32BaseVersion()
               << " Version:" << GetWindowsVersion();
 }
 
