7d755402d81a69865e740f9950ace4228e51083f
Michael Thiessen <mthiesse@chromium.org>
2023-01-27 17:22:06 +0000

    Fix Instance ID re-used in multi-window
    In playing around with multi-window intents I hit the assert
    "assert !MultiWindowUtils.isMultiInstanceApi31Enabled() || assignedIndex == mWindowId;"
    in ChromeTabbedActivity#createTabModels
    
    The problem seems to be that we wait until an onResume to update the
    allocated IDs. This is racy, and possibly just wrong in cases where
    Activities get finished before resuming. We should instead record the
    ID as used immediately when choosing it for the Activity.
    
    Change-Id: I5067a69553e44b3e101dbccf5a8619df2187e8d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198784
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097963}

--- chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31UnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31UnitTest.java
@@ -636,6 +636,8 @@ public class MultiInstanceManagerApi31UnitTest {
     private int allocInstanceIndex(int passedId, Activity activity, boolean preferNew) {
         int index =
                 mMultiInstanceManager.allocInstanceId(passedId, activity.getTaskId(), preferNew);
+        assertTrue(index == INVALID_INSTANCE_ID
+                || MultiInstanceManagerApi31.instanceEntryExists(index));
 
         // Does what TabModelOrchestrator.createTabModels() would do to simulate production code.
         Pair<Integer, TabModelSelector> pair =
@@ -649,7 +651,6 @@ public class MultiInstanceManagerApi31UnitTest {
 
         // Store minimal data to get the instance recognized.
         MultiInstanceManagerApi31.writeUrl(instanceId, "url" + instanceId);
-        MultiInstanceManagerApi31.writeLastAccessedTime(index);
         SharedPreferencesManager.getInstance().writeInt(
                 MultiInstanceManagerApi31.tabCountKey(index), 1);
         return instanceId;
