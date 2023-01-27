f9bf5e2733cdada0813a07c70031597ebc8a0534
Tommy Steimel <steimel@chromium.org>
2023-01-27 20:00:36 +0000

    PiP 2.0: Assume the feature is enabled on the browser side
    Due to the way origin trials work, on the browser side we need to just
    assume that the origin trial is enabled on the renderer that requests a
    new document pip window.
    
    Bug: 1410552
    Change-Id: I40280e3bce088708db78c8a841ddbed9ee0cc25a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198715
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098068}

--- chrome/browser/ui/browser_navigator.cc
+++ chrome/browser/ui/browser_navigator.cc
@@ -56,9 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/site_isolation_policy.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/common/content_features.h"
 #include "extensions/buildflags/buildflags.h"
-#include "third_party/blink/public/common/features.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "url/url_constants.h"
@@ -275,11 +273,6 @@ std::pair<Browser*, int> GetBrowserAndTabForDisposition(
       return {GetOrCreateBrowser(profile, params.user_gesture), -1};
     case WindowOpenDisposition::NEW_PICTURE_IN_PICTURE:
 #if !BUILDFLAG(IS_ANDROID)
-      if (!base::FeatureList::IsEnabled(
-              blink::features::kDocumentPictureInPictureAPI)) {
-        return {nullptr, -1};
-      }
-
       // Picture in picture windows may not be opened by other picture in
       // picture windows.
       if (params.browser->is_type_picture_in_picture())
