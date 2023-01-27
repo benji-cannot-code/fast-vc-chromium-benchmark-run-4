23b69244b82b191f2ffe811e0402469304027d74
Olivier ROBIN <olivierrobin@google.com>
2023-01-27 22:36:29 +0000

    Enable kTFLiteLanguageDetectionEnabled by default on iOS
    Also enable DownloadServiceForegroundSessionIOSFeature as it
    is a requirement for the feature.
    
    Bug: 1285224
    Change-Id: I0dff332d4b568156f2309e3ac2e7a19f81a8cc1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613399
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098155}

--- components/download/public/background_service/features.cc
+++ components/download/public/background_service/features.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/download/public/background_service/features.h"
 
+#include "build/build_config.h"
+
 namespace download {
 
 BASE_FEATURE(kDownloadServiceFeature,
@@ -13,6 +15,11 @@ BASE_FEATURE(kDownloadServiceFeature,
 
 BASE_FEATURE(kDownloadServiceForegroundSessionIOSFeature,
              "DownloadServiceForegroundSessionIOSFeature",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_IOS)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 }  // namespace download
