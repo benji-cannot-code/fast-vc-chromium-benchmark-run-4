af36f3e6be2f035d23518df40198df27f89dd09e
Viktor Semeniuk <vsemeniuk@google.com>
2023-01-27 09:16:07 +0000

    [Password Manager] Help button
    This CL adds aria label and an action to the help button. When clicked
    a help article is opened in a new tab.
    
    Fixed: 1409905
    Change-Id: Ia21364dd37010c39da576bf5a4f1506fd7be3d17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4191285
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097824}

--- chrome/browser/ui/webui/password_manager/password_manager_ui.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui.cc
@@ -102,6 +102,7 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
       {"exportPasswordsTryAgain", IDS_PASSWORD_MANAGER_UI_EXPORT_TRY_AGAIN},
       {"exportingPasswordsTitle", IDS_PASSWORD_MANAGER_UI_EXPORTING_TITLE},
       {"federationLabel", IDS_PASSWORD_MANAGER_UI_FEDERATION_LABEL},
+      {"help", IDS_PASSWORD_MANAGER_UI_HELP},
       {"hidePassword", IDS_PASSWORD_MANAGER_UI_HIDE_PASSWORD},
       {"importPasswords", IDS_PASSWORD_MANAGER_UI_IMPORT_BANNER_TITLE},
       {"importPasswordsDescription",
@@ -172,6 +173,9 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
           password_manager::GetPasswordCheckupURL(
               password_manager::PasswordCheckupReferrer::kPasswordCheck)
               .spec()));
+
+  source->AddString("passwordManagerLearnMoreURL",
+                    chrome::kPasswordManagerLearnMoreURL);
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   // Overwrite ubranded logo for Chrome-branded builds.
