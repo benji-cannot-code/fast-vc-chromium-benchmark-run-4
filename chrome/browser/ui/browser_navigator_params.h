ced73633de1b1e41a46b9c0f32a401765a165a07
Mike Wasserman <msw@chromium.org>
2023-01-27 19:44:45 +0000

    Window placement: Support opening popups at (0,0) origin.
    Plumb signals for which window.open() features were specified
    - Plumb has_x|y|width|height from WebViewImpl to the browser
    - Store WindowFeatures in NavigateParams, not just a Rect
    - Add Browser::CreateParams::initial_origin_specified
    
    Respect explicitly specified (0,0) origins in BrowserView.
    
    Store the original features struct with blocked popups.
    (inherits opener bounds when unblocked, not when blocked)
    
    Add a browser test and update some existing tests trivially.
    
    Bug: 1392876
    Test: `window.open('.', '', 'left=0,top=0,width=100,height=100')` WAI
    Change-Id: Ia10d64f2289c7e931a1c25410324bda632bcbbba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167001
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098059}

--- chrome/browser/ui/browser_navigator_params.h
+++ chrome/browser/ui/browser_navigator_params.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/navigation/impression.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/mojom/navigation/was_activated_option.mojom.h"
+#include "third_party/blink/public/mojom/window_features/window_features.mojom.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/gfx/geometry/rect.h"
@@ -206,12 +207,8 @@ struct NavigateParams {
   // If non-empty, the new tab is an app tab.
   std::string app_id;
 
-  // If non-empty, specifies the desired initial position and size of the
-  // window if |disposition| == NEW_POPUP.
-  // TODO(beng): Figure out if this can be used to create Browser windows
-  //             for other callsites that use set_override_bounds, or
-  //             remove this comment.
-  gfx::Rect window_bounds;
+  // Specifies the desired window features if `disposition` is NEW_POPUP.
+  blink::mojom::WindowFeatures window_features;
 
   // Determines if and how the target window should be made visible at the end
   // of the call to Navigate().
