7c2ae94f3a97c75dc5d0537f4e459c10f45c5238
Will Yeager <wyeager@google.com>
2023-01-27 16:56:43 +0000

    Use env var to disable discover feed for perf testing
    It turns out that passing a command line argument from the Hawkeye test
    configuration to the app under test is not supported. Setting
    environment variables, however, is supported by setting
    extra_params.env_vars in the Hawkeye project's BUILD file
    
    Change-Id: I084d9c7fc5eacae88c9c5f0d8daca7c6b3184672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4195099
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Will Yeager <wyeager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1097952}

--- ios/chrome/app/perf_tests_hook.mm
+++ ios/chrome/app/perf_tests_hook.mm
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/app/tests_hook.h"
 
-#import "base/command_line.h"
-#import "ios/chrome/browser/flags/chrome_switches.h"
+#import <Foundation/Foundation.h>
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -25,10 +24,13 @@ bool DisableContentSuggestions() {
   return false;
 }
 bool DisableDiscoverFeed() {
-  // Performance tests must explicitly enable the discover feed by passing the
-  // --enable-discover-feed command line switch.
-  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableDiscoverFeed);
+  // Performance tests may disable the discover feed by setting the
+  // DISABLE_DISCOVER_FEED environment variable. Possible values
+  // the variable may be set to are described in the apple documentation for
+  // boolValue:
+  // https://developer.apple.com/documentation/foundation/nsstring/1409420-boolvalue
+  return [[NSProcessInfo.processInfo.environment
+      objectForKey:@"DISABLE_DISCOVER_FEED"] boolValue];
 }
 bool DisableFirstRun() {
   // Always disable FRE for perf tests.
