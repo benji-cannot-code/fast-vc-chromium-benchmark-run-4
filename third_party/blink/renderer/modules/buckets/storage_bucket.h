00567ec0dbc4eaed792bdf2aa634171e1ccab3fc
Ayu Ishii <ayui@chromium.org>
2023-01-27 23:20:51 +0000

    StorageBuckets: remove unnecessary ActiveScriptWrappable
    Bug: none
    Change-Id: I378f85753dcd54473a21667d107d21c98a00c461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201066
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098174}

--- third_party/blink/renderer/modules/buckets/storage_bucket.h
+++ third_party/blink/renderer/modules/buckets/storage_bucket.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/buckets/bucket_manager_host.mojom-blink.h"
-#include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h"
@@ -28,7 +27,6 @@ class LockManager;
 class ScriptState;
 
 class StorageBucket final : public ScriptWrappable,
-                            public ActiveScriptWrappable<StorageBucket>,
                             public ExecutionContextLifecycleObserver {
   DEFINE_WRAPPERTYPEINFO();
 
@@ -48,9 +46,6 @@ class StorageBucket final : public ScriptWrappable,
   LockManager* locks();
   CacheStorage* caches(ExceptionState&);
   ScriptPromise getDirectory(ScriptState*, ExceptionState&);
-
-  // ActiveScriptWrappable
-  bool HasPendingActivity() const final;
 
   // GarbageCollected
   void Trace(Visitor*) const override;
