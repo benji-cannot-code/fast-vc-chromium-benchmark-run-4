76a175f59e081a7a984d098aaa7f4a06f210fb6f
Aishwarya Rajesh <aishwaryarj@google.com>
2023-01-27 19:07:21 +0000

    [RDSC] Implement Request Desktop Site per-site settings IPH (arm 1)
    - Add feature engagement config for RDS per-site IPH arm 1
    - Update DesktopSiteSettingsIPHController to manage blue tooltip implementation of the IPH
    - Unify checks/tests common to both arms of the IPHs
    - Trigger the IPH in both CTA and CCT
    - Add unit tests in DesktopSiteSettingsIPHController
    
    Demo: https://drive.google.com/file/d/1qewGb7He5U8O4W3oRWlX2cJRXDJKpyG3/view?usp=sharing
    
    Bug: 1402566
    Change-Id: I619d895e6fc30b1d1ef3818e2c6955db509d1bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176152
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098033}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -82,6 +82,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHRequestDesktopSiteDefaultOnFeature,
     &kIPHRequestDesktopSiteOptInFeature,
     &kIPHRequestDesktopSiteExceptionsGenericFeature,
+    &kIPHRequestDesktopSiteExceptionsSpecificFeature,
     &kIPHShoppingListMenuItemFeature,
     &kIPHShoppingListSaveFlowFeature,
     &kIPHTabGroupsQuicklyComparePagesFeature,
