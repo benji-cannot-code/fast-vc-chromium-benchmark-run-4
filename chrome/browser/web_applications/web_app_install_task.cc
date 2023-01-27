5c7024795ce749148e6b96669973ea6c752ec506
Dustin J. Mitchell <djmitche@chromium.org>
2023-01-27 18:28:26 +0000

    TakeErrorDict returns Dict
    This updates `InstallErrorLogEntry::TakeErrorDict` and
    `WebAppInstallTask::TakeErrorDict` to return `base::Value::Dict` instead
    of `base::Value`, with followups noted.
    
    Bug: 1303949
    Change-Id: Ifc85b4dd4a5d6f46ad8ecd380cdf38eb8d3b674b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198075
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098003}

--- chrome/browser/web_applications/web_app_install_task.cc
+++ chrome/browser/web_applications/web_app_install_task.cc
@@ -324,7 +324,7 @@ void WebAppInstallTask::WebContentsDestroyed() {
                       webapps::InstallResultCode::kWebContentsDestroyed);
 }
 
-base::Value WebAppInstallTask::TakeErrorDict() {
+base::Value::Dict WebAppInstallTask::TakeErrorDict() {
   DCHECK(log_entry_.HasErrorDict());
   return log_entry_.TakeErrorDict();
 }
