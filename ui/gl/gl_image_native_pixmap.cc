10a1f58be85501152bb3f84a74d0ceab32b28e87
Colin Blundell <blundell@chromium.org>
2023-01-27 16:57:48 +0000

    [GL] Eliminate GLImageNativePixmap::GetNativePixmap()
    After refactoring of clients in //ui/ozone/demo, there are no remaining
    callsites of this function.
    
    Bug: 1310018
    Change-Id: I9d72ca79c26f06d4767cac0f336d9dde8acb79d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188519
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097954}

--- ui/gl/gl_image_native_pixmap.cc
+++ ui/gl/gl_image_native_pixmap.cc
@@ -433,8 +433,4 @@ unsigned GLImageNativePixmap::GetInternalFormat() {
   return GLInternalFormat(format_);
 }
 
-scoped_refptr<gfx::NativePixmap> GLImageNativePixmap::GetNativePixmap() {
-  return pixmap_;
-}
-
 }  // namespace gl
