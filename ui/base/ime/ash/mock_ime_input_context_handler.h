149e8da5fe75b53507ec241ae2e988795c3a0fca
Akihiro Ota <akihiroota@chromium.org>
2023-01-27 03:04:12 +0000

    Dictation: Remove SuccessWaiter class
    We are trying to remove all polling from Dictation tests, so this
    change removes the SuccessWaiter class. Instead of polling for the
    commit text, we modify the MockImeInputContextHandler to notify
    observers whenever the commit text is updated. This allows us to
    swap out a polling-based approach for an event-based approach.
    
    CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-betty-pi-arc-chrome
    
    AX-Relnotes: N/A
    Bug: b:264535324
    Change-Id: I03e2dc6c870c90fac621048c1e67810e3edf678f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4195041
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097761}

--- ui/base/ime/ash/mock_ime_input_context_handler.h
+++ ui/base/ime/ash/mock_ime_input_context_handler.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/component_export.h"
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "ui/base/ime/ash/text_input_target.h"
 #include "ui/base/ime/composition_text.h"
 #include "ui/events/event.h"
@@ -35,6 +37,15 @@ class COMPONENT_EXPORT(UI_BASE_IME_ASH) MockIMEInputContextHandler
     uint32_t num_char16s_before_cursor;
     uint32_t num_char16s_after_cursor;
   };
+
+  class Observer : public base::CheckedObserver {
+   public:
+    // Called whenever the commit text is updated.
+    virtual void OnCommitText(const std::u16string& text) = 0;
+  };
+
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
 
   MockIMEInputContextHandler();
   virtual ~MockIMEInputContextHandler();
@@ -125,6 +136,7 @@ class COMPONENT_EXPORT(UI_BASE_IME_ASH) MockIMEInputContextHandler
   bool autocorrect_enabled_ = true;
   std::vector<ui::GrammarFragment> grammar_fragments_;
   gfx::Range cursor_range_;
+  base::ObserverList<Observer> observers_;
 };
 
 }  // namespace ash
