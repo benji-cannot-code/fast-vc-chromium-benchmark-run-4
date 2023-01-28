a7377cd6a55997dd77d39f4c031faf4ad2b08e0b
Yao Li <yaohuali@google.com>
2023-01-28 01:08:58 +0000

    Split shared type from cert_store.mojom
    One mojo type will be shared between keymaster and keymint, such that
    they can share CertStoreService.
    
    Bug: b:247941366
    Test: autoninja -C out/Default/ ash_components_unittests browser_tests
    unit_tests && testing/xvfb.py ./out/Default/ash_components_unittests &&
    testing/xvfb.py ./out/Default/unit_tests
    --gtest_filter="*ArcCertInstallerTest*" && testing/xvfb.py
    ./out/Default/browser_tests --gtest_filter="*CertStoreServiceTest*"
    Test: (on hatch/ARC-R) tast run dut arc.VTSKeymaster.vm
    Test: (on trogdor/ARC-P) tast run dut arc.VTSKeymaster
    
    Change-Id: Ic202d0f7f27b6a0810ea25a9d84f9eb95e343e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189368
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Commit-Queue: Yao Li <yaohuali@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098219}

--- chrome/browser/ash/arc/enterprise/cert_store/arc_cert_installer.h
+++ chrome/browser/ash/arc/enterprise/cert_store/arc_cert_installer.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
-#include "chrome/services/keymaster/public/mojom/cert_store.mojom.h"
+#include "chrome/services/keymanagement/public/mojom/cert_store_types.mojom.h"
 #include "components/policy/core/common/remote_commands/remote_command_job.h"
 #include "components/policy/core/common/remote_commands/remote_commands_queue.h"
 #include "crypto/rsa_private_key.h"
@@ -34,7 +34,7 @@ namespace arc {
 struct CertDescription {
   CertDescription(crypto::RSAPrivateKey* placeholder_key,
                   CERTCertificate* nss_cert,
-                  keymaster::mojom::ChapsSlot slot,
+                  keymanagement::mojom::ChapsSlot slot,
                   std::string label,
                   std::string id);
   CertDescription(CertDescription&& other);
@@ -48,7 +48,7 @@ struct CertDescription {
   // The NSS certificate that corresponds to this object.
   net::ScopedCERTCertificate nss_cert;
   // The chaps slot where this key is stored.
-  keymaster::mojom::ChapsSlot slot;
+  keymanagement::mojom::ChapsSlot slot;
   // The PKCS#11 CKA_LABEL of this key.
   std::string label;
   // The PKCS#11 CKA_ID of this key.
--- chrome/browser/ash/arc/enterprise/cert_store/cert_store_service.h
+++ chrome/browser/ash/arc/enterprise/cert_store/cert_store_service.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/queue.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/arc/enterprise/cert_store/arc_cert_installer.h"
-#include "chrome/services/keymaster/public/mojom/cert_store.mojom.h"
+#include "chrome/services/keymanagement/public/mojom/cert_store_types.mojom.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/browser_context.h"
@@ -96,7 +96,7 @@ class CertStoreService : public KeyedService,
 
   void UpdateCertificates();
 
-  void OnCertificatesListed(keymaster::mojom::ChapsSlot slot,
+  void OnCertificatesListed(keymanagement::mojom::ChapsSlot slot,
                             std::vector<CertDescription> certificates,
                             net::ScopedCERTCertificateList cert_list);
 
@@ -105,7 +105,7 @@ class CertStoreService : public KeyedService,
   // initially empty. Must be recursive because of async calls.
   void BuildAllowedCertDescriptionsRecursively(
       BuildAllowedCertDescriptionsCallback callback,
-      keymaster::mojom::ChapsSlot slot,
+      keymanagement::mojom::ChapsSlot slot,
       base::queue<net::ScopedCERTCertificate> cert_queue,
       std::vector<CertDescription> allowed_certs) const;
   // Decides to either proceed to build a |CertDescription| for the given |cert|
@@ -113,7 +113,7 @@ class CertStoreService : public KeyedService,
   // recursive call to BuildAllowedCertDescriptionsRecursively.
   void BuildAllowedCertDescriptionAndRecurse(
       BuildAllowedCertDescriptionsCallback callback,
-      keymaster::mojom::ChapsSlot slot,
+      keymanagement::mojom::ChapsSlot slot,
       base::queue<net::ScopedCERTCertificate> cert_queue,
       std::vector<CertDescription> allowed_certs,
       net::ScopedCERTCertificate cert,
@@ -122,7 +122,7 @@ class CertStoreService : public KeyedService,
   // the recursive call to BuildAllowedCertDescriptionsRecursively.
   void AppendCertDescriptionAndRecurse(
       BuildAllowedCertDescriptionsCallback callback,
-      keymaster::mojom::ChapsSlot slot,
+      keymanagement::mojom::ChapsSlot slot,
       base::queue<net::ScopedCERTCertificate> cert_queue,
       std::vector<CertDescription> allowed_certs,
       absl::optional<CertDescription> cert_description) const;
@@ -131,13 +131,13 @@ class CertStoreService : public KeyedService,
   // restart the process to gather certificates on the system slot (when |slot|
   // is the user slot), or proceed to update keymaster keys.
   void OnBuiltAllowedCertDescriptions(
-      keymaster::mojom::ChapsSlot slot,
+      keymanagement::mojom::ChapsSlot slot,
       std::vector<CertDescription> cert_descriptions) const;
 
   // Processes metadata from |allowed_certs| stored in the given |slot| and
   // appends them to |certificates|.
   void OnFilteredAllowedCertificates(
-      keymaster::mojom::ChapsSlot slot,
+      keymanagement::mojom::ChapsSlot slot,
       std::vector<CertDescription> certificates,
       net::ScopedCERTCertificateList allowed_certs);
   void OnUpdatedKeymasterKeys(std::vector<CertDescription> certificates,
