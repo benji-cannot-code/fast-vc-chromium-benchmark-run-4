563ad5fc9aefd84050b280450b46c42d395adfc3
Kevin Haslett <khaslett@chromium.org>
2023-01-28 00:06:52 +0000

    Revert "Support BufferQueue on Mac"
    This reverts commit 3427060de395ec4dfd45b1d4727df8c2c24a2d50.
    
    Reason for revert: b/266848353 - broke Canvas app on CrOS
    
    Original change's description:
    > Support BufferQueue on Mac
    >
    > This CL does NOT enable BQ on Mac.
    >
    > - Don't call PrepareRenderPassOverlay for root render passes
    > - Ignore InUseByWindowServer for root rp overlays
    > - Don't bother calling DidReceiveReleasedOverlays for root rp overlays
    > - Also cleanup some render pass backing code for root rps, making logic
    >   simpler.
    >
    > Bug: 1322528
    > Change-Id: I4ae0897a5803ded347d05159ddc1b06031948595
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4089938
    > Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095874}
    
    Bug: 1322528
    Change-Id: I0fbf816d25e382c3070f03afec84123edf044d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4197743
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098196}

--- components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
+++ components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
@@ -63,12 +63,10 @@ class SkiaOutputDeviceBufferQueue::OverlayData {
 
   OverlayData(std::unique_ptr<gpu::OverlayImageRepresentation> representation,
               std::unique_ptr<gpu::OverlayImageRepresentation::ScopedReadAccess>
-                  scoped_read_access,
-              bool is_root_render_pass)
+                  scoped_read_access)
       : representation_(std::move(representation)),
         scoped_read_access_(std::move(scoped_read_access)),
-        ref_(1),
-        is_root_render_pass_(is_root_render_pass) {
+        ref_(1) {
     DCHECK(representation_);
     DCHECK(scoped_read_access_);
   }
@@ -78,7 +76,7 @@ class SkiaOutputDeviceBufferQueue::OverlayData {
   ~OverlayData() { Reset(); }
 
   OverlayData& operator=(OverlayData&& other) {
-    DCHECK(is_root_render_pass_ || !IsInUseByWindowServer());
+    DCHECK(!IsInUseByWindowServer());
     DCHECK(!ref_);
     DCHECK(!scoped_read_access_);
     DCHECK(!representation_);
@@ -86,20 +84,13 @@ class SkiaOutputDeviceBufferQueue::OverlayData {
     representation_ = std::move(other.representation_);
     ref_ = other.ref_;
     other.ref_ = 0;
-    is_root_render_pass_ = other.is_root_render_pass_;
     return *this;
   }
 
   bool IsInUseByWindowServer() const {
 #if BUILDFLAG(IS_MAC)
-    if (!scoped_read_access_) {
+    if (!scoped_read_access_)
       return false;
-    }
-    // The root render pass buffers are managed by SkiaRenderer so we don't care
-    // if they're in use by the window server.
-    if (is_root_render_pass_) {
-      return false;
-    }
     return scoped_read_access_->IsInUseByWindowServer();
 #else
     return false;
@@ -113,7 +104,7 @@ class SkiaOutputDeviceBufferQueue::OverlayData {
     if (ref_ > 1) {
       --ref_;
     } else if (ref_ == 1) {
-      DCHECK(is_root_render_pass_ || !IsInUseByWindowServer());
+      DCHECK(!IsInUseByWindowServer());
       Reset();
     }
   }
@@ -127,8 +118,6 @@ class SkiaOutputDeviceBufferQueue::OverlayData {
     return scoped_read_access_.get();
   }
 
-  bool IsRootRenderPass() { return is_root_render_pass_; }
-
  private:
   void Reset() {
     scoped_read_access_.reset();
@@ -140,7 +129,6 @@ class SkiaOutputDeviceBufferQueue::OverlayData {
   std::unique_ptr<gpu::OverlayImageRepresentation::ScopedReadAccess>
       scoped_read_access_;
   int ref_ = 0;
-  bool is_root_render_pass_ = false;
 };
 
 SkiaOutputDeviceBufferQueue::SkiaOutputDeviceBufferQueue(
@@ -334,7 +322,6 @@ void SkiaOutputDeviceBufferQueue::SchedulePrimaryPlane(
 
 SkiaOutputDeviceBufferQueue::OverlayData*
 SkiaOutputDeviceBufferQueue::GetOrCreateOverlayData(const gpu::Mailbox& mailbox,
-                                                    bool is_root_render_pass,
                                                     bool* is_existing) {
   if (is_existing)
     *is_existing = false;
@@ -368,9 +355,8 @@ SkiaOutputDeviceBufferQueue::GetOrCreateOverlayData(const gpu::Mailbox& mailbox,
   }
 
   bool result;
-  std::tie(it, result) =
-      overlays_.emplace(std::move(shared_image), std::move(shared_image_access),
-                        is_root_render_pass);
+  std::tie(it, result) = overlays_.emplace(std::move(shared_image),
+                                           std::move(shared_image_access));
   DCHECK(result);
   DCHECK(it->unique());
 
@@ -409,8 +395,8 @@ void SkiaOutputDeviceBufferQueue::ScheduleOverlays(
 
     OutputPresenter::ScopedOverlayAccess* access = nullptr;
     bool overlay_has_been_submitted;
-    auto* overlay_data = GetOrCreateOverlayData(
-        mailbox, overlay.is_root_render_pass, &overlay_has_been_submitted);
+    auto* overlay_data =
+        GetOrCreateOverlayData(mailbox, &overlay_has_been_submitted);
     if (overlay_data) {
       access = overlay_data->scoped_read_access();
       pending_overlay_mailboxes_.emplace_back(mailbox);
@@ -604,17 +590,11 @@ void SkiaOutputDeviceBufferQueue::DoFinishSwapBuffers(
   // Go through backings of all overlays, and release overlay backings which are
   // not used.
   base::EraseIf(overlays_, [&on_overlay_release](auto& overlay) {
-    if (!overlay.unique()) {
+    if (!overlay.unique())
       return false;
-    }
-    if (overlay.IsInUseByWindowServer()) {
+    if (overlay.IsInUseByWindowServer())
       return false;
-    }
-    // The root render pass buffers are managed by SkiaRenderer so we don't need
-    // to explicitly return them via callback.
-    if (!overlay.IsRootRenderPass()) {
-      on_overlay_release(overlay);
-    }
+    on_overlay_release(overlay);
     overlay.Unref();
     return true;
   });
