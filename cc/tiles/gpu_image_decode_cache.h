632ae49298ca912209a1bda1ad50dd188a5e4701
Ken Rockot <rockot@google.com>
2023-01-28 20:16:31 +0000

    cc: Experimental age-based decode cache limits
    Introduces a new LimitImageDecodeCacheAge feature with a single
    integral "seconds" parameter controlling the max desired age of
    any entry within the GpuImageDecodeCache's internal LRU cache.
    
    When enabled, the limit is enforced any time a new entry is
    added to the cache.
    
    Bug: 1410541
    Change-Id: I40a98f094d574773190770ad6e025d0d4e82a637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198012
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1098324}

--- cc/tiles/gpu_image_decode_cache.h
+++ cc/tiles/gpu_image_decode_cache.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/memory_pressure_listener.h"
 #include "base/memory/raw_ptr.h"
 #include "base/synchronization/lock.h"
+#include "base/time/time.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "cc/cc_export.h"
 #include "cc/paint/image_transfer_cache_entry.h"
@@ -544,6 +545,7 @@ class CC_EXPORT GpuImageDecodeCache
     bool is_bitmap_backed;
     bool is_budgeted = false;
     absl::optional<SkYUVAPixmapInfo> yuva_pixmap_info;
+    base::TimeTicks last_use;
 
     // If true, this image is no longer in our |persistent_cache_| and will be
     // deleted as soon as its ref count reaches zero.
@@ -770,6 +772,10 @@ class CC_EXPORT GpuImageDecodeCache
                             scoped_refptr<ImageData> data);
   template <typename Iterator>
   Iterator RemoveFromPersistentCache(Iterator it);
+
+  // Purges any old entries from the PersistentCache if the feature to enable
+  // this behavior is turned on.
+  void MaybePurgeOldCacheEntries();
 
   // Adds mips to an image if required.
   void UpdateMipsIfNeeded(const DrawImage& draw_image, ImageData* image_data);
