ca1681b80943a16dc9e20276f3c4a4c9f48c781b
Colin Blundell <blundell@chromium.org>
2023-01-30 16:34:30 +0000

    [GPU] Restrict concept of unbound images in Texture to Windows/Mac
    It is only possible to set Texture::LevelInfo::image_state to UNBOUND
    on Windows/Mac (the only codepath for doing so is via
    Texture::SetUnboundLevelImage(), which is only present on Windows/Mac).
    
    This CL restricts the state UNBOUND, all Texture methods that deal with
    that state, and all callsites of those methods to being
    Windows/Mac-only. This paves the road to eliminating that code once the
    validating command decoder is no longer built on Windows/Mac.
    
    Bug: 1323341
    Change-Id: I041f23df98641f32d51ebdf98975c997145eee4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4199023
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098631}

--- gpu/command_buffer/service/texture_manager.h
+++ gpu/command_buffer/service/texture_manager.h
@@ -152,10 +152,12 @@ class GPU_GLES2_EXPORT TexturePassthrough final
 class GPU_GLES2_EXPORT Texture final : public TextureBase {
  public:
   enum ImageState {
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
     // If an image is associated with the texture and image state is UNBOUND,
     // then sampling out of the texture or using it as a target for drawing
     // will not read/write from/to the image.
     UNBOUND,
+#endif
     // If image state is BOUND, then sampling from the texture will return the
     // contents of the image and using it as a target will modify the image.
     BOUND,
@@ -335,8 +337,10 @@ class GPU_GLES2_EXPORT Texture final : public TextureBase {
   void BindToServiceId(GLuint service_id);
 #endif
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   // Marks the image for the given level as bound.
   void MarkLevelImageBound(GLenum target, GLint level);
+#endif
 
   bool CompatibleWithSamplerUniformType(
       GLenum type,
@@ -346,9 +350,11 @@ class GPU_GLES2_EXPORT Texture final : public TextureBase {
   // does not exist.
   gl::GLImage* GetLevelImage(GLint target, GLint level) const;
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   // Returns true iff (a) there is an image associated with the particular
   // level, and (b) the image is unbound.
   bool HasUnboundLevelImage(GLint target, GLint level) const;
+#endif
 
   bool HasImages() const {
     return has_images_;
