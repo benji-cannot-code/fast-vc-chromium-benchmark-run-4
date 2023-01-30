7a9861da78e90fb0a5288525f7b6dd82d1f35fac
Kush Sinha <sinhak@chromium.org>
2023-01-30 16:29:07 +0000

    CodeHealth: Migrate `base::Value::FindKey()` to v2 API
    Migrate `base::Value::FindKey()` to `base::Value::Dict::Find()` and
    `base::Value::Dict::contains()` in -
    /chromeos/ash/components/network
    
    This CL was uploaded by git cl split.
    
    R=stevenjb@chromium.org
    
    Bug: 1406815
    Change-Id: Id085db79b1e318a9e8194610a8fe66e96a9c41af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200229
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098628}

--- chromeos/ash/components/network/onc/onc_merger_unittest.cc
+++ chromeos/ash/components/network/onc/onc_merger_unittest.cc
@@ -102,7 +102,7 @@ TEST_F(ONCMergerTest, UserValueAndRecommendedUnset) {
 TEST_F(ONCMergerTest, UserDictionaryAndNoPolicyValue) {
   base::Value merged(
       MergeSettingsAndPoliciesToEffective(&policy_, nullptr, &user_, nullptr));
-  EXPECT_FALSE(merged.FindKey("ProxySettings"));
+  EXPECT_FALSE(merged.GetDict().contains("ProxySettings"));
 }
 
 TEST_F(ONCMergerTest, MergeWithEmptyPolicyProhibitsEverything) {
