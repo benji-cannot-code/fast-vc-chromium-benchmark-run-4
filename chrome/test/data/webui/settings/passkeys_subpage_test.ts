c1721ca6401ff0b595027a877323a4ba347109c9
dpapad <dpapad@chromium.org>
2023-01-30 20:31:04 +0000

    WebUI: Replace references to //ui/webui/resources:library, part 5.
    In this CL updating all references in chrome/test/data/. Also adding
    missing references in several cases.
    
    This target has been deprecated and replaced by:
     - //ui/webui/resources/cr_elements:build_ts
     - //ui/webui/resources/js:build_ts
     - //ui/webui/resources/mojo:library
    
    This is part of the effort to make ts_library() automatically infer
    |path_mappings| from |deps|, which will eliminate missing dependencies,
    and consequently fix a certain class TypeScript related flaky build
    errors.
    
    Bug: 1402829
    Change-Id: Ic55d19d3a30d874ff18eac04da4d84e46fb1ff5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201187
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098821}

--- chrome/test/data/webui/settings/passkeys_subpage_test.ts
+++ chrome/test/data/webui/settings/passkeys_subpage_test.ts
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Tests for the passkeys subpage.
  */
 
-import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
-import {CrLazyRenderElement} from 'chrome://resources/cr_elements/cr_lazy_render/cr_lazy_render.js';
 import {Passkey, PasskeysBrowserProxy, PasskeysBrowserProxyImpl, SettingsPasskeysDeleteConfirmationDialogElement, SettingsPasskeysSubpageElement} from 'chrome://settings/lazy_load.js';
 import {assertDeepEquals, assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
@@ -229,9 +227,7 @@ suite('PasskeysSubpage', function() {
     await flushTasks();
     assertEquals(browserProxy.getCallCount('enumerate'), 1);
 
-    const lazyDialog =
-        page.shadowRoot!.querySelector<CrLazyRenderElement<CrDialogElement>>(
-            '#deleteErrorDialog');
+    const lazyDialog = page.$.deleteErrorDialog;
     assertTrue(lazyDialog !== null, 'Dialog not found');
     assertTrue(
         lazyDialog.getIfExists() === null, 'Dialog should not be showing');
