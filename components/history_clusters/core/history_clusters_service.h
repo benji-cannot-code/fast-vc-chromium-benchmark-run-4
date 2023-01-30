8dee38acdbc6b1b4f199a41657fef7496f0fd005
manukh <manukh@chromium.org>
2023-01-30 17:00:03 +0000

    [memories][persist-clusters] Test `HCServiceTaskUpdateClusters`.
    Adds tests for `HistoryClustersServiceTaskUpdateClusters` including 1 to
    verify that days with more than `persist_clusters_recluster_window_days`
    (1000) visits aren't causing the persisted boundary to be 'stuck' and
    always resume from that day.
    
    Also fixes an edge case where a new unclustered visit made on the same
    day as the current clustering boundary would not recluster those
    clusters of the same day. E.g. if today is 1/2, the user has clusters on
    1/1 and 1/2, and the user makes a new visit on 1/2, then we would only
    recluster the 1/1 cluster and not the 1/2 cluster. This probably doesn't
    occur much in the real world, so the fix isn't feature guarded.
    
    Bug: 1184879, 1184875, 1171352
    Change-Id: Ia576217335cca0dc8ee4f6592d92fb233b953a3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4097370
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098640}

--- components/history_clusters/core/history_clusters_service.h
+++ components/history_clusters/core/history_clusters_service.h
@@ -210,6 +210,7 @@ class HistoryClustersService : public base::SupportsUserData,
 
  private:
   friend class HistoryClustersServiceTestApi;
+  friend class HistoryClustersServiceTestBase;
 
   // Starts a keyword cache refresh, if necessary.
   // TODO(manukh): `StartKeywordCacheRefresh()` and
