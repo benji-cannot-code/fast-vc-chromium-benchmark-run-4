948d0a09bb81094bcb2e56726f024e9b72c6002d
dpapad <dpapad@chromium.org>
2023-01-30 20:54:51 +0000

    Settings: Remove incorrect imports from chrome://resources/cr_elements/
    In Settings tests, cr_elements/ deps should be pulled in from
    settings.js or lazy_load.js only.
    
    This is in preparation of turning such imports into build time errors.
    
    Bug: 1402829
    Change-Id: I06f6cf9fa19545f11a5c1ce48d91619ab6cb007a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200958
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098842}

--- chrome/test/data/webui/settings/passkey_edit_dialog_test.ts
+++ chrome/test/data/webui/settings/passkey_edit_dialog_test.ts
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Tests for the passkeys subpage.
  */
 
-import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
-import {Passkey, PasskeysBrowserProxy, PasskeysBrowserProxyImpl, SettingsPasskeysSubpageElement} from 'chrome://settings/lazy_load.js';
+import {CrInputElement, Passkey, PasskeysBrowserProxy, PasskeysBrowserProxyImpl, SettingsPasskeysSubpageElement} from 'chrome://settings/lazy_load.js';
 import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
