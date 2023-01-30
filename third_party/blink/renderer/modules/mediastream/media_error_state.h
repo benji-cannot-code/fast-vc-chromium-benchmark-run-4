62c3601db5be40ca58e36de186b283ce37281edd
Palak Agarwal <agpalak@chromium.org>
2023-01-30 09:57:41 +0000

    Remove kConstraintError and kDOMException from MediaErrorState
    This CL removes DOMException and ConstraintError from media_error_state.h as they had no usage. It also renames
    ThrowTypeError to MarkTypeError as the name is more appropriate for what
    it does.
    
    This CL also starts recording results using SPRwT for MediaErrorState
    exceptions.
    
    Bug: 1373398
    Change-Id: Idd7019807422cd961ad2d3f7be122f6221d454a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4130274
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098496}

--- third_party/blink/renderer/modules/mediastream/media_error_state.h
+++ third_party/blink/renderer/modules/mediastream/media_error_state.h
@@ -33,15 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_ERROR_STATE_H_
 
 #include "third_party/blink/renderer/modules/modules_export.h"
-#include "third_party/blink/renderer/platform/bindings/exception_code.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
 class ExceptionState;
-class OverconstrainedError;
-class V8UnionDOMExceptionOrOverconstrainedError;
 
 // A class that is able to be used like ExceptionState for carrying
 // information about an error up the stack, but it is up to the higher
@@ -51,24 +48,18 @@ class MODULES_EXPORT MediaErrorState {
 
  public:
   MediaErrorState();
-  void ThrowTypeError(const String& message);
-  void ThrowDOMException(DOMExceptionCode, const String& message);
-  void ThrowConstraintError(const String& message, const String& constraint);
+  void MarkTypeError(const String& message);
   void Reset();
 
   bool HadException();
-  bool CanGenerateException();
-  void RaiseException(ExceptionState&);
+  void Throw(ExceptionState&);
   String GetErrorMessage();
-  V8UnionDOMExceptionOrOverconstrainedError* CreateError();
 
  private:
-  enum ErrorType { kNoError, kTypeError, kDOMException, kConstraintError };
-  ErrorType error_type_;
+  enum ErrorType { kNoError, kTypeError };
+  ErrorType error_type_ = kNoError;
   String name_;
-  DOMExceptionCode code_;
   String message_;
-  String constraint_;
 };
 
 }  // namespace blink
