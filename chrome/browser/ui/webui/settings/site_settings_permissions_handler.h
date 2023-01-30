2b1d5cdded74a373721cba7011803de25941c32b
Eduard Hez <ehez@chromium.org>
2023-01-30 11:54:11 +0000

    [SafetyCheck] Add undo for 'Allow again' action for unused sites module
    When clicking 'Allow again' for an origin's revoked permissions in the unused sites module the permission(s) is(are) granted again, the item is removed from the list of revoked permissions, and the origin is added to an ignore list for the future. This CL adds the undo functionality on the CPP side (TS side already landed).
    
    Bug: 1345920, 1409416
    Change-Id: If88b1a7d5a988dabfa0260c1725b4d1998523fed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176863
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Illia Klimov <elklm@chromium.org>
    Commit-Queue: Eduard Hez <ehez@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098533}

--- chrome/browser/ui/webui/settings/site_settings_permissions_handler.h
+++ chrome/browser/ui/webui/settings/site_settings_permissions_handler.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/clock.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
+#include "components/content_settings/core/common/content_settings_constraints.h"
+#include "components/content_settings/core/common/content_settings_types.h"
+#include "url/origin.h"
 
 /**
  * This handler deals with the permission-related operations on the site
@@ -42,9 +45,14 @@ class SiteSettingsPermissionsHandler : public settings::SettingsPageUIHandler {
   // "Unused site permissions" module.
   void HandleGetRevokedUnusedSitePermissionsList(const base::Value::List& args);
 
-  // Re-grant the revoked permissions and remove the origin from the revoked
-  // permissions list.
+  // Re-grant the revoked permissions and remove the given origin from the
+  // revoked permissions list.
   void HandleAllowPermissionsAgainForUnusedSite(const base::Value::List& args);
+
+  // Reverse the changes made by |HandleAllowPermissionsAgainForUnusedSite| for
+  // the given |UnusedSitePermission| object.
+  void HandleUndoAllowPermissionsAgainForUnusedSite(
+      const base::Value::List& args);
 
   // Clear the list of revoked permissions so they are not shown again.
   // Permission settings themselves are not affected by this.
@@ -52,7 +60,8 @@ class SiteSettingsPermissionsHandler : public settings::SettingsPageUIHandler {
       const base::Value::List& args);
 
   // Reverse the changes made by
-  // |HandleAcknowledgeRevokedUnusedSitePermissionsList|. List of revoked
+  // |HandleAcknowledgeRevokedUnusedSitePermissionsList| for the given list of
+  // |UnusedSitePermission| objects. List of revoked
   // permissions is repopulated. Permission settings are not changed.
   void HandleUndoAcknowledgeRevokedUnusedSitePermissionsList(
       const base::Value::List& args);
@@ -63,6 +72,14 @@ class SiteSettingsPermissionsHandler : public settings::SettingsPageUIHandler {
 
   // Sends the list of unused site permissions to review to the WebUI.
   void SendUnusedSitePermissionsReviewList();
+
+  // Get values from |UnusedSitePermission| object in
+  // site_settings_permissions_browser_proxy.ts.
+  std::tuple<url::Origin,
+             std::set<ContentSettingsType>,
+             content_settings::ContentSettingConstraints>
+  GetUnusedSitePermissionsFromDict(
+      const base::Value::Dict& unused_site_permissions);
 
   const raw_ptr<Profile> profile_;
 
--- chrome/browser/ui/webui/settings/site_settings_permissions_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/site_settings_permissions_handler_unittest.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 constexpr char kUnusedTestSite[] = "https://example1.com";
 constexpr char kUsedTestSite[] = "https://example2.com";
+constexpr ContentSettingsType kUnusedPermission =
+    ContentSettingsType::GEOLOCATION;
 
 class SiteSettingsPermissionsHandlerTest : public testing::Test {
  public:
@@ -94,6 +96,18 @@ class SiteSettingsPermissionsHandlerTest : public testing::Test {
     }
   }
 
+  void ExpectRevokedPermission() {
+    ContentSettingsForOneType revoked_permissions_list;
+    hcsm()->GetSettingsForOneType(
+        ContentSettingsType::REVOKED_UNUSED_SITE_PERMISSIONS,
+        &revoked_permissions_list);
+    EXPECT_EQ(1U, revoked_permissions_list.size());
+    EXPECT_EQ(
+        ContentSetting::CONTENT_SETTING_ASK,
+        hcsm()->GetContentSetting(GURL(kUnusedTestSite), GURL(kUnusedTestSite),
+                                  kUnusedPermission));
+  }
+
   TestingProfile* profile() { return profile_.get(); }
   content::TestWebUI* web_ui() { return &web_ui_; }
   SiteSettingsPermissionsHandler* handler() { return handler_.get(); }
@@ -134,6 +148,9 @@ TEST_F(SiteSettingsPermissionsHandlerTest,
   // Advance 14 days; this will be the expected histogram sample.
   clock()->Advance(base::Days(14));
   base::HistogramTester histogram_tester;
+  base::Value::List initial_unused_site_permissions =
+      handler()->PopulateUnusedSitePermissionsData();
+  ExpectRevokedPermission();
 
   // Allow the revoked permission for the unused site again.
   base::Value::List args;
@@ -158,7 +175,12 @@ TEST_F(SiteSettingsPermissionsHandlerTest,
   EXPECT_EQ(
       ContentSetting::CONTENT_SETTING_ALLOW,
       hcsm()->GetContentSetting(GURL(kUnusedTestSite), GURL(kUnusedTestSite),
-                                ContentSettingsType::GEOLOCATION));
+                                kUnusedPermission));
+
+  // Undoing restores the initial state.
+  handler()->HandleUndoAllowPermissionsAgainForUnusedSite(
+      std::move(initial_unused_site_permissions));
+  ExpectRevokedPermission();
 }
 
 TEST_F(SiteSettingsPermissionsHandlerTest,
