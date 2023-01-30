aabd21e8b4379aa797084d58dc1d254a8b77752d
Sean Kau <skau@chromium.org>
2023-01-30 21:31:49 +0000

    Implement sampling with Celebi in WallpaperColorCalculator
    Computes the Celebi color if Jelly is enabled. Otherwise, the color is
    left as transparent. Saving and using the color will require the flag
    to be enabled.
    
    Bug: b:264941162
    Change-Id: Ia7c1b830ac1a4ef60f185da35a6e40f2496680bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194887
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098877}

--- ash/wallpaper/wallpaper_utils/wallpaper_color_calculator.cc
+++ ash/wallpaper/wallpaper_utils/wallpaper_color_calculator.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "ash/constants/ash_features.h"
+#include "ash/wallpaper/wallpaper_utils/scored_sample.h"
 #include "ash/wallpaper/wallpaper_utils/wallpaper_calculated_colors.h"
 #include "ash/wallpaper/wallpaper_utils/wallpaper_color_extraction_result.h"
 #include "base/functional/bind.h"
@@ -19,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/color_analysis.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_operations.h"
@@ -85,8 +88,14 @@ WallpaperCalculatedColors CalculateWallpaperColor(
       *resized_image.bitmap(), resized_image.height(), kNoBounds, kNoBounds,
       /*find_closest=*/true);
 
+  // Compute result with with the improved clustering algorithm.
+  SkColor celebi_color = ash::features::IsJellyEnabled()
+                             ? ComputeWallpaperSeedColor(resized_image)
+                             : SK_ColorTRANSPARENT;
+
   DVLOG(2) << __func__ << " image_size=" << image.size().ToString()
            << " time=" << base::TimeTicks::Now() - start_time;
+
   WallpaperColorExtractionResult result = NUM_COLOR_EXTRACTION_RESULTS;
   for (size_t i = 0; i < color_profiles.size(); ++i) {
     bool is_result_transparent = prominent_colors[i] == SK_ColorTRANSPARENT;
@@ -132,7 +141,7 @@ WallpaperCalculatedColors CalculateWallpaperColor(
   }
   DCHECK_NE(NUM_COLOR_EXTRACTION_RESULTS, result);
   return WallpaperCalculatedColors(prominent_colors, k_mean_color,
-                                   SK_ColorTRANSPARENT);
+                                   celebi_color);
 }
 
 bool ShouldCalculateSync(const gfx::ImageSkia& image) {
