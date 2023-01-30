ae71a06215cfcc1c46bba5cd5d65f1d87e48277c
Nan Lin <linnan@chromium.org>
2023-01-30 16:58:23 +0000

    Remove context origin and scope from DataKey
    We decided to scope DataKey to reporting origin only for now, removing
    these fields until they're actually used.
    
    Bug: 1411316
    Change-Id: I6d22ad9ad59022d95960445ceee818f8d392ef92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4203767
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098638}

--- content/public/browser/attribution_data_model.cc
+++ content/public/browser/attribution_data_model.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/attribution_data_model.h"
 
-#include <tuple>
 #include <utility>
 
 #include "base/check.h"
@@ -13,14 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-AttributionDataModel::DataKey::DataKey(url::Origin reporting_origin,
-                                       url::Origin context_origin,
-                                       Scope scope)
-    : reporting_origin_(std::move(reporting_origin)),
-      context_origin_(std::move(context_origin)),
-      scope_(scope) {
+AttributionDataModel::DataKey::DataKey(url::Origin reporting_origin)
+    : reporting_origin_(std::move(reporting_origin)) {
   DCHECK(!reporting_origin_.opaque());
-  DCHECK(!context_origin_.opaque());
 }
 
 AttributionDataModel::DataKey::DataKey(const DataKey&) = default;
@@ -37,14 +31,12 @@ AttributionDataModel::DataKey::~DataKey() = default;
 
 bool AttributionDataModel::DataKey::operator<(
     const AttributionDataModel::DataKey& other) const {
-  return std::tie(reporting_origin_, context_origin_, scope_) <
-         std::tie(other.reporting_origin_, other.context_origin_, other.scope_);
+  return reporting_origin_ < other.reporting_origin_;
 }
 
 bool AttributionDataModel::DataKey::operator==(
     const AttributionDataModel::DataKey& other) const {
-  return std::tie(reporting_origin_, context_origin_, scope_) ==
-         std::tie(other.reporting_origin_, other.context_origin_, other.scope_);
+  return reporting_origin_ == other.reporting_origin_;
 }
 
 }  // namespace content
