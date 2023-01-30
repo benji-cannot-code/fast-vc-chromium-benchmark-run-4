96d151d05156e1f7a5985b55799cb74a3cb14951
Florian Leimgruber <fleimgruber@google.com>
2023-01-30 12:56:07 +0000

    ContactInfo: Add feature parameter to disable custom passphrase support.
    Adds a feature parameter to kSyncEnableContactInfoDataType which allows
    to control the custom passphrase user support for the CONTACT_INFO
    data type. By default, custom passphrase users are supported.
    
    Bug: 1348294
    Change-Id: I576d4868c35d5aba879acd1befa08f8b314e6994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200258
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1098545}

--- components/autofill/core/browser/webdata/contact_info_model_type_controller.h
+++ components/autofill/core/browser/webdata/contact_info_model_type_controller.h
@@ -6,17 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_CONTACT_INFO_MODEL_TYPE_CONTROLLER_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_CONTACT_INFO_MODEL_TYPE_CONTROLLER_H_
 
+#include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "components/sync/driver/model_type_controller.h"
+#include "components/sync/driver/sync_service.h"
+#include "components/sync/driver/sync_service_observer.h"
 
 namespace autofill {
 
-class ContactInfoModelTypeController : public syncer::ModelTypeController {
+class ContactInfoModelTypeController : public syncer::ModelTypeController,
+                                       public syncer::SyncServiceObserver {
  public:
   ContactInfoModelTypeController(
       std::unique_ptr<syncer::ModelTypeControllerDelegate>
           delegate_for_full_sync_mode,
       std::unique_ptr<syncer::ModelTypeControllerDelegate>
-          delegate_for_transport_mode);
+          delegate_for_transport_mode,
+      syncer::SyncService* sync_service);
+  ~ContactInfoModelTypeController() override;
 
   ContactInfoModelTypeController(const ContactInfoModelTypeController&) =
       delete;
@@ -25,6 +32,15 @@ class ContactInfoModelTypeController : public syncer::ModelTypeController {
 
   // ModelTypeController overrides.
   bool ShouldRunInTransportOnlyMode() const override;
+  PreconditionState GetPreconditionState() const override;
+
+  // SyncServiceObserver overrides.
+  void OnStateChanged(syncer::SyncService* sync) override;
+
+ private:
+  const raw_ptr<syncer::SyncService> sync_service_;
+  base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
+      sync_service_observation_{this};
 };
 
 }  // namespace autofill
