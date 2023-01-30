227a0dc7a2c270a1b9a1ca84beeb3dbe216d31be
Koji Ishii <kojii@chromium.org>
2023-01-30 04:03:42 +0000

    Avoid `NOTREACHED` in `RecalcInkOverflowForCursor`
    By changing it to `DumpWithoutCrashing`.
    
    Fixed: 1409461
    Change-Id: I7128f1d0787e8501b7358dc106d2590fe024a983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194912
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098449}

--- third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "third_party/blink/renderer/core/editing/bidi_adjustment.h"
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/layout_ng_text_combine.h"
@@ -818,7 +819,7 @@ PhysicalRect NGFragmentItem::RecalcInkOverflowForCursor(
     if (UNLIKELY(item->IsLayoutObjectDestroyedOrMoved())) {
       // TODO(crbug.com/1099613): This should not happen, as long as it is
       // layout-clean. It looks like there are cases where the layout is dirty.
-      NOTREACHED();
+      base::debug::DumpWithoutCrashing();
       continue;
     }
     if (UNLIKELY(item->HasSelfPaintingLayer()))
