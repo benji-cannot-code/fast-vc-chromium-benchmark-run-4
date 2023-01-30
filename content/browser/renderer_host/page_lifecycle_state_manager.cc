d3d5575474e2fcaeceb17a19ce7f2fdcd805af9e
Fergal Daly <fergal@chromium.org>
2023-01-30 12:07:53 +0000

    Increase BFCache entry timeout to 12s with ASAN.
    6s seems to have helped a little but was not enough. It seems like it
    should be enough. This switch to 12s is mostly to verify that there
    isn't some other problem causing this.
    
    Bug: 1224355
    Change-Id: Ic0f8c63fde8cc32f836df0381713d00353486d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4202278
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098535}

--- content/browser/renderer_host/page_lifecycle_state_manager.cc
+++ content/browser/renderer_host/page_lifecycle_state_manager.cc
@@ -16,10 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/interface_provider.h"
 
 namespace {
-// ASAN builds are slow and we see flakes caused by reaching this timeout.
+// ASAN builds are slow and we see flakes caused by reaching this timeout. 6s
+// was not enough to stop the flakes. Trying 12s just to ensure that there isn't
+// something else going on that we don't understand.
 // See https://crbug.com/1224355.
 #if defined(ADDRESS_SANITIZER)
-constexpr base::TimeDelta kBackForwardCacheTimeout = base::Seconds(6);
+constexpr base::TimeDelta kBackForwardCacheTimeout = base::Seconds(12);
 #else
 constexpr base::TimeDelta kBackForwardCacheTimeout = base::Seconds(3);
 #endif
