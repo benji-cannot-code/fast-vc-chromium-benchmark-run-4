b4f40836aae837187abef84cc7d0d6b91a619ee8
andrewxu <andrewxu@chromium.org>
2023-01-30 20:16:35 +0000

    [Notification Drag] Implement image notification drag - Part 2
    In this CL, when the drag is initiated from a notification popup view,
    the dragged popup view is hidden.
    
    Bug: b/264934296
    Change-Id: I0ff33562df9a5889f5f7f68e91e9e985c27ee874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189700
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098806}

--- ash/system/message_center/ash_notification_drag_controller.h
+++ ash/system/message_center/ash_notification_drag_controller.h
@@ -6,7 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_MESSAGE_CENTER_ASH_NOTIFICATION_DRAG_CONTROLLER_H_
 #define ASH_SYSTEM_MESSAGE_CENTER_ASH_NOTIFICATION_DRAG_CONTROLLER_H_
 
+#include <string>
+
+#include "base/scoped_observation.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/aura/client/drag_drop_client_observer.h"
 #include "ui/views/drag_controller.h"
+
+namespace aura::client {
+class DragDropClient;
+}  // namespace aura::client
 
 namespace gfx {
 class Point;
@@ -21,9 +30,12 @@ class View;
 }  // namespace views
 
 namespace ash {
+class AshNotificationView;
 
 // Handles drag on Ash notification views.
-class AshNotificationDragController : public views::DragController {
+class AshNotificationDragController
+    : public aura::client::DragDropClientObserver,
+      public views::DragController {
  public:
   AshNotificationDragController();
   AshNotificationDragController(const AshNotificationDragController&) = delete;
@@ -32,6 +44,10 @@ class AshNotificationDragController : public views::DragController {
   ~AshNotificationDragController() override;
 
  private:
+  // aura::client::DragDropClientObserver:
+  void OnDragCompleted(const ui::DropTargetEvent& event) override;
+  void OnDragCancelled() override;
+
   // views::DragController:
   void WriteDragDataForView(views::View* sender,
                             const gfx::Point& press_pt,
@@ -41,6 +57,19 @@ class AshNotificationDragController : public views::DragController {
   bool CanStartDragForView(views::View* sender,
                            const gfx::Point& press_pt,
                            const gfx::Point& p) override;
+
+  void OnNotificationViewDragStarted(AshNotificationView* dragged_view);
+  void OnNotificationViewDragEnded();
+
+  // Corresponds to the notification view under drag. Set/reset when the drag on
+  // a notification view starts/ends.
+  absl::optional<std::string> dragged_notification_id_;
+
+  // Helps to track drag-and-drop events. Set/reset when the drag on a
+  // notification view starts/ends.
+  base::ScopedObservation<aura::client::DragDropClient,
+                          aura::client::DragDropClientObserver>
+      drag_drop_client_observer_{this};
 };
 
 }  // namespace ash
