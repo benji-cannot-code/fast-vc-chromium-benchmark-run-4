6f8cbabf3c8aeb06ca1eca07f9ba5e03a29ddc37
Tao Wu <wutao@chromium.org>
2023-01-30 23:43:55 +0000

    libassistant-v2: Destroy libassistant gRPC services first
    Currently in the Assistant shutdown, we destroy the assistant_client
    first. This causes a problem that libassistant services try to send
    heart beat to assistant_client, but failed. This will lead to
    unnecessary retries.
    
    This cl reorders the shutdown sequence, to destroy libassistant gPRC
    services first and then the assistant_client gRPC services to avoid the
    problem mentioned.
    
    Bug: b:252891019
    Test: mannual test
    Change-Id: I10ee1ea21cd7bf675d761a9f311771ec637159de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205135
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098967}

--- chromeos/ash/services/libassistant/grpc/external_services/grpc_services_initializer.h
+++ chromeos/ash/services/libassistant/grpc/external_services/grpc_services_initializer.h
@@ -58,6 +58,7 @@ class GrpcServicesInitializer : public ServicesInitializerBase {
   bool Start();
 
   void StartGrpcHttpConnectionClient(assistant_client::HttpConnectionFactory*);
+  void StopGrpcHttpConnectionClient();
 
   // Add observer for each handler driver.
   void AddAlarmTimerEventObserver(
