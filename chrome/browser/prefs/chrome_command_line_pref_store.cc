d60026332417b0badedbaa890bbb0a2988d638d0
Matt Menke <mmenke@chromium.org>
2023-01-30 20:22:19 +0000

    Migrate all Value::Append() calls in /chrome/browser/prefs
    Value::Append() is deprecated in favor of Value::List::Append().
    
    In some directories, these CLs also migrate some nearby
    Value::SetKey() and related method calls to the
    Value::Dict::Set() family of methods, for similar reasons.
    
    This CL was uploaded by git cl split.
    
    R=gab@chromium.org
    
    Bug: 1404827
    Change-Id: I2f966333db70cd2896eb31058a8345a7d31c06bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4204068
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098812}

--- chrome/browser/prefs/chrome_command_line_pref_store.cc
+++ chrome/browser/prefs/chrome_command_line_pref_store.cc
@@ -187,7 +187,7 @@ void ChromeCommandLinePrefStore::ApplyExplicitlyAllowedPortSwitch() {
     return;
   }
 
-  base::Value integer_list(base::Value::Type::LIST);
+  base::Value::List integer_list;
   std::string switch_value =
       command_line()->GetSwitchValueASCII(switches::kExplicitlyAllowedPorts);
   const auto& split = base::SplitStringPiece(
@@ -200,6 +200,7 @@ void ChromeCommandLinePrefStore::ApplyExplicitlyAllowedPortSwitch() {
       continue;
     integer_list.Append(base::Value(port));
   }
-  SetValue(prefs::kExplicitlyAllowedNetworkPorts, std::move(integer_list),
+  SetValue(prefs::kExplicitlyAllowedNetworkPorts,
+           base::Value(std::move(integer_list)),
            WriteablePrefStore::DEFAULT_PREF_WRITE_FLAGS);
 }
