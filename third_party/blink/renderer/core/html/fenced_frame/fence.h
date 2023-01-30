306e7a212cdef2fd5b906b209d3b6885ab0d60fc
Liam Brady <lbrady@google.com>
2023-01-30 17:09:43 +0000

    Introduce fence.setReportEventDataForAutomaticBeacons()
    This CL introduces a new API for window.fence that allows setting the
    payload of an automatic beacon in advance. Because automatic beacons
    will send without the web platform being aware of it, the web platform
    needs to tell the browser what data should be sent as part of the
    beacon before the beacon fires.
    
    `setReportEventDataForAutomaticBeacons` will send an IPC to the frame's
    `RenderFrameHost`, which in turn will store the payload in its
    associated FrameTreeNode's `fenced_frame_properties_` object. A follow
    up CL will have popups and `_unfencedTop` navigations read the payload
    and send it to any beacons registered with the `reserved.top_navigation`
    event.
    
    See design doc:
    https://docs.google.com/document/d/1a4Z-EA9j9RnAkxtxIeapN3CrfFDwYTvUmRZdyS12YTw/edit?usp=sharing&resourcekey=0-KAsNrfiZ2Q1X66BoJnyDFQ
    
    Change-Id: If44f9ad91e71f5e04a1580865390368518da01d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167776
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098653}

--- third_party/blink/renderer/core/html/fenced_frame/fence.h
+++ third_party/blink/renderer/core/html/fenced_frame/fence.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/html/fenced_frame/fenced_frame_config.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
@@ -33,6 +34,13 @@ class CORE_EXPORT Fence final : public ScriptWrappable,
                    const FenceEvent* event,
                    ExceptionState& exception_state);
 
+  // Saves the event data that will be used when an automatic beacon of type
+  // event.eventType is sent. Right now, it only supports saving data for the
+  // "reserved.top_navigation" beacon.
+  void setReportEventDataForAutomaticBeacons(ScriptState* script_state,
+                                             const FenceEvent* event,
+                                             ExceptionState& exception_state);
+
   // Returns a list of nested inner configurations for the fenced frame, if any
   // exist. This will return configs only when called from the DOM window that
   // is the main frame of a fenced frame. This will return a non-empty array if
@@ -43,6 +51,17 @@ class CORE_EXPORT Fence final : public ScriptWrappable,
   void Trace(Visitor*) const override;
 
  private:
+  // Returns one of the following:
+  // - The current frame if it is a fenced frame.
+  // - The current iframe (not fenced frame) if we're treating the iframe as a
+  //   fenced frame via support for URNs, and the iframe is not in a fenced
+  //   frame tree.
+  // - The main frame (which is a fenced frame), if the current frame is a
+  //   same-origin iframe underneath the fenced frame that we're communicating
+  //   with.
+  // This function expects that the frame associated with this fence object is
+  // attached to the DOMWindow.
+  LocalFrame* GetAssociatedFencedFrameForReporting();
   void AddConsoleMessage(const String& message);
 };
 
