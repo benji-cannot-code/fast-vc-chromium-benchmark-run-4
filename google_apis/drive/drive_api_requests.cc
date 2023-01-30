98e206e2c4f812a11599d51a4b04485a8dfa98ab
Matt Menke <mmenke@chromium.org>
2023-01-30 18:30:57 +0000

    Migrate all Value::Append() calls in /google_apis/drive
    Value::Append() is deprecated in favor of Value::List::Append().
    
    In some directories, these CLs also migrate some nearby
    Value::SetKey() and related method calls to the
    Value::Dict::Set() family of methods, for similar reasons.
    
    This CL was uploaded by git cl split.
    
    R=ayui@chromium.org
    
    Bug: 1404827
    Change-Id: I17f489b3edc463367dcbef5234615d5a55533e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4203970
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098720}

--- google_apis/drive/drive_api_requests.cc
+++ google_apis/drive/drive_api_requests.cc
@@ -1041,7 +1041,7 @@ bool PermissionsInsertRequest::GetContentData(std::string* upload_content_type,
     case PERMISSION_ROLE_COMMENTER:
       root.Set("role", "reader");
       {
-        base::Value list(base::Value::Type::LIST);
+        base::Value::List list;
         list.Append("commenter");
         root.Set("additionalRoles", std::move(list));
       }
