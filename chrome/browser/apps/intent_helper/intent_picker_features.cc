487cdd36d608546ae59fe0223f3ea9786da23dbb
Tim Sergeant <tsergeant@chromium.org>
2023-01-30 03:24:13 +0000

    Intents: Enable new link capturing UI on Chrome OS
    We are rolling out the new visual design for the chip and intent picker,
    without the in-app infobar prompt.
    
    Bug: 1279653
    Change-Id: I84c3f26e04a43579fbe112e1b7cf48347cb0803a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198601
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098443}

--- chrome/browser/apps/intent_helper/intent_picker_features.cc
+++ chrome/browser/apps/intent_helper/intent_picker_features.cc
@@ -4,13 +4,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/apps/intent_helper/intent_picker_features.h"
+
 #include "base/feature_list.h"
+#include "build/build_config.h"
 
 namespace apps::features {
 
 BASE_FEATURE(kLinkCapturingUiUpdate,
              "LinkCapturingUiUpdate",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_CHROMEOS)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 BASE_FEATURE(kLinkCapturingInfoBar,
              "LinkCapturingInfoBar",
@@ -22,7 +29,12 @@ BASE_FEATURE(kIntentChipSkipsPicker,
 
 BASE_FEATURE(kIntentChipAppIcon,
              "AppIconInIntentChip",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_CHROMEOS)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 BASE_FEATURE(kLinkCapturingAutoDisplayIntentPicker,
              "LinkCapturingAutoDisplayIntentPicker",
