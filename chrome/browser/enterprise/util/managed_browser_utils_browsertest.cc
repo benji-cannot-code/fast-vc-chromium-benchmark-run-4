78560129e6239a9db5774143502001c91e1e112a
Matt Menke <mmenke@chromium.org>
2023-01-30 16:42:02 +0000

    Migrate all Value::Append() calls in /chrome/browser/enterprise
    Value::Append() is deprecated in favor of Value::List::Append().
    
    In some directories, these CLs also migrate some nearby
    Value::SetKey() and related method calls to the
    Value::Dict::Set() family of methods, for similar reasons.
    
    This CL was uploaded by git cl split.
    
    R=ftirelo@chromium.org
    
    Bug: 1404827
    Change-Id: I45dfb1adcc2298d445418511160320e7385d412b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4203716
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098632}

--- chrome/browser/enterprise/util/managed_browser_utils_browsertest.cc
+++ chrome/browser/enterprise/util/managed_browser_utils_browsertest.cc
@@ -32,9 +32,9 @@ class ManagedBrowserUtilsBrowserTest
         }
       }
     })";
-    base::Value list(base::Value::Type::LIST);
+    base::Value::List list;
     list.Append(kAutoSelectCertificateValue);
-    return list;
+    return base::Value(std::move(list));
   }
 };
 
