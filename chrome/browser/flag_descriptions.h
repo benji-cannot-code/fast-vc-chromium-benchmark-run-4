1cf380d3642ac0915ecd233788b547a97a26e6e6
Salma Elmahallawy <esalma@google.com>
2023-01-30 18:42:24 +0000

    [PolicyLogs] Flag for chrome://policy/logs Page on Android
    Feature: PolicyLogsPageAndroid
    
    Bug: b/264230517
    Change-Id: I9eb6c4cdd2a57c31134d40aef9db67bb63806a21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194967
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1098731}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2178,6 +2178,9 @@ extern const char kPageInfoStoreInfoDescription[];
 extern const char kPageInfoAboutThisSiteImprovedBottomSheetName[];
 extern const char kPageInfoAboutThisSiteImprovedBottomSheetDescription[];
 
+extern const char kPolicyLogsPageAndroidName[];
+extern const char kPolicyLogsPageAndroidDescription[];
+
 extern const char kQueryTilesName[];
 extern const char kQueryTilesDescription[];
 extern const char kQueryTilesNTPName[];
