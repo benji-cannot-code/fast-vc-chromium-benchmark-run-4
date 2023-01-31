5b341fdafb414bdf545fa06dadfd5591485425cd
Samuel Huang <huangs@chromium.org>
2023-01-31 06:16:06 +0000

    [SuperSize] Viewer: Remove implicit type coercion in comparison.
    This CL removes usage of "==" and "!=" in SuperSize Viewer. The JS style
    guide allows "== null" or "!= null" to cover both {null, undefined}, but
    we manage to avoid it for now. If it gets used in the future then each
    use should be accompanied by a comment. Cases:
    * Explicit using "===" or "!==":
      * Known {string, number} variables and constants.
      * With |undefined|:
        * Check existence of fields.
        * Check Map / Array / HTMLCollection get() or [] operator for
          non-primitive (Number, String, Boolean, etc.) types.
      * With |null|:
        * Only where explicitly required, e.g., nullable variables or null
        results from Web API such as getAttribute().
    * Implicit Boolean cast as (foo) or (!foo) (e.g., in conditionals),
      including foo?.bar usage:
      * Where variable or field is non-primitive.
    
    Change-Id: I4578db548205213190716e4605cac54d4155d993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205870
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099100}

--- tools/binary_size/libsupersize/viewer/static/state.js
+++ tools/binary_size/libsupersize/viewer/static/state.js
@@ -40,7 +40,7 @@ const dom = {
   replace(parent, newChild) {
     while (parent.firstChild)
       parent.removeChild(parent.firstChild);
-    if (newChild != null)
+    if (newChild)
       parent.appendChild(newChild);
   },
   /**
@@ -100,7 +100,8 @@ function _initState() {
     toString() {
       const copy = new URLSearchParams(_filterParams);
       const types = [...new Set(copy.getAll(_TYPE_STATE_KEY))];
-      if (types.length > 0) copy.set(_TYPE_STATE_KEY, types.join(''));
+      if (types.length > 0)
+        copy.set(_TYPE_STATE_KEY, types.join(''));
 
       const queryString = copy.toString();
       return queryString.length > 0 ? `?${queryString}` : '';
@@ -112,7 +113,7 @@ function _initState() {
      * @param {string | null} value
      */
     set(key, value) {
-      if (value == null) {
+      if (value === null) {
         _filterParams.delete(key);
       } else {
         _filterParams.set(key, value);
@@ -304,7 +305,7 @@ function _makeIconTemplateGetter() {
   };
 
 
-  /** @type {Map<string, {color:string,description:string}>} */
+  /** @type {Map<string, {color:string, description:string}>} */
   const iconInfoCache = new Map();
 
   /**
@@ -327,7 +328,7 @@ function _makeIconTemplateGetter() {
    */
   function getIconStyle(type) {
     let info = iconInfoCache.get(type);
-    if (info == null) {
+    if (!info) {
       const icon = getIconTemplate(type, true);
       info = {
         color: icon.getAttribute('fill'),
@@ -349,7 +350,7 @@ function _makeIconTemplateGetter() {
     const isLeaf = node.children && node.children.length === 0;
     const entries = Object.entries(node.childStats);
     let key = 'unchanged';
-    if (isLeaf && entries.length != 0) {
+    if (isLeaf && entries.length !== 0) {
       const statsEntry = entries[0][1];
       if (statsEntry.added) {
         key = 'added';
@@ -358,9 +359,9 @@ function _makeIconTemplateGetter() {
       } else if (statsEntry.changed) {
         key = 'changed';
       }
-    } else if (node.diffStatus == _DIFF_STATUSES.ADDED) {
+    } else if (node.diffStatus === _DIFF_STATUSES.ADDED) {
       key = 'added';
-    } else if (node.diffStatus == _DIFF_STATUSES.REMOVED) {
+    } else if (node.diffStatus === _DIFF_STATUSES.REMOVED) {
       key = 'removed';
     }
     return statusIcons[key].cloneNode(true);
--- tools/binary_size/libsupersize/viewer/static/tree-ui.js
+++ tools/binary_size/libsupersize/viewer/static/tree-ui.js
@@ -85,7 +85,7 @@ class TreeUi {
     const element = (typeof indexOrEl === 'number') ?
         this._liveNodeList[indexOrEl] :
         indexOrEl;
-    if (element != null) {
+    if (element) {
       // Update DOM.
       element.tabIndex = 0;
       element.focus();
@@ -116,7 +116,7 @@ class TreeUi {
       // Get data for the children of this tree node element. If the children
       // have not yet been loaded, request for the data from the worker.
       let data = this._uiNodeData.get(link);
-      if (data == null || data.children == null) {
+      if (!data?.children) {
         /** @type {HTMLSpanElement} */
         const symbolName = link.querySelector('.symbol-name');
         const idPath = symbolName.title;
@@ -222,7 +222,7 @@ class TreeUi {
       // If closed tree, open tree. Otherwise, move to first child.
       case 'ArrowRight': {
         const expanded = link.parentElement.getAttribute('aria-expanded');
-        if (expanded != null) {
+        if (expanded !== null) {
           // Leafs do not have the aria-expanded property.
           if (expanded === 'true') {
             _focusNext();
@@ -355,7 +355,7 @@ class TreeUi {
     if (this._isLeafNode(node)) {
       node.addEventListener('mousedown', this._handleRefocus.bind(this));
     }
-    displayInfocard(this._uiNodeData.get(node));
+    displayInfocard(/** @type {!TreeNode} */ (this._uiNodeData.get(node)));
     /** @type {HTMLElement} */ (event.currentTarget)
         .parentElement.classList.add('focused');
   }
