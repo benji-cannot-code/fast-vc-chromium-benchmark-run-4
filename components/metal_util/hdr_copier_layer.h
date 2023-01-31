a08a4162fce62dbcc23a683dc43d4872ae3a4bf1
Dave Tapuska <dtapuska@chromium.org>
2023-01-31 21:44:46 +0000

    Allow metal_util to be compiled for iOS.
    - Change include to IOSurfaceRef.h since IOSurface.h isn't available
    in the iOS SDK.
    - Do not call MTLCopyAllDevices as there is only the default system
    device on iOS.
    
    Bug: 1411704
    Change-Id: Ida168b50addfb1e30cb15454d4050e26eb99476c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4209769
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099452}

--- components/metal_util/hdr_copier_layer.h
+++ components/metal_util/hdr_copier_layer.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/hdr_metadata.h"
 
-#include <IOSurface/IOSurface.h>
+#include <IOSurface/IOSurfaceRef.h>
 
 namespace gfx {
 class ColorSpace;
