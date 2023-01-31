79a8288750b5b76248459cc2c42b5e48beecd6b0
Zhaoyang Li <zhaoyangli@chromium.org>
2023-01-31 14:53:54 +0000

    Enable sending variant to result sink from test runners
    Result sink is accepting additional or overriding variant definitions at reporting since crrev.com/c/4037071. This change enables that for test runners reporting results using build/util/lib/results/result_sink.py.
    
    Bug: 1215346
    Change-Id: I9cc8e6fe942f68c9f1dc4d50e6d34f0c8f42f428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4131380
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Reviewed-by: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099239}

--- build/util/lib/results/result_sink.py
+++ build/util/lib/results/result_sink.py
@@ -78,6 +78,7 @@ class ResultSinkClient(object):
            duration,
            test_log,
            test_file,
+           variant=None,
            artifacts=None,
            failure_reason=None,
            html_artifact=None):
@@ -92,6 +93,9 @@ class ResultSinkClient(object):
       duration: An int representing time in ms.
       test_log: A string representing the test's output.
       test_file: A string representing the file location of the test.
+      variant: An optional dict of variant key value pairs as the
+          additional variant sent from test runners, which can override
+          or add to the variants passed to `rdb stream` command.
       artifacts: An optional dict of artifacts to attach to the test.
       failure_reason: An optional string with the reason why the test failed.
           Should be None if the test did not fail.
@@ -128,6 +132,9 @@ class ResultSinkClient(object):
             'name': test_id,
         }
     }
+
+    if variant:
+      tr['variant'] = {'def': variant}
 
     artifacts = artifacts or {}
     tr['summaryHtml'] = html_artifact if html_artifact else ''
