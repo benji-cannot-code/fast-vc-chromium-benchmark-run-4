b41d1cf4d4f178a8ead66bb02241b48245d40212
Ho Cheung <uioptt24@gmail.com>
2023-01-31 16:01:21 +0000

    Remove `kSkipReadbackToSharedMemory`
    According to the TODO in the code,
    we should delete it after M110.
    
    Bug: 1293509
    Change-Id: I3cdb1bee7de30e32a95f7f7533dc195ea5e7d778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205698
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099263}

--- gpu/command_buffer/service/shared_image/compound_image_backing.cc
+++ gpu/command_buffer/service/shared_image/compound_image_backing.cc
@@ -37,11 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 namespace {
 
-// TODO(crbug.com/1293509): Remove after M110 branch.
-BASE_FEATURE(kSkipReadbackToSharedMemory,
-             "SkipReadbackToSharedMemory",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 constexpr AccessStreamSet kMemoryStreamSet = {SharedImageAccessStream::kMemory};
 
 // Unique GUIDs for child backings.
@@ -486,9 +481,7 @@ bool CompoundImageBacking::CopyToGpuMemoryBuffer() {
 
   auto& shm_element = GetElement(SharedImageAccessStream::kMemory);
 
-  // If shared memory already contains the latest content skip readback.
-  if (HasLatestContent(shm_element) &&
-      base::FeatureList::IsEnabled(kSkipReadbackToSharedMemory)) {
+  if (HasLatestContent(shm_element)) {
     return true;
   }
 
