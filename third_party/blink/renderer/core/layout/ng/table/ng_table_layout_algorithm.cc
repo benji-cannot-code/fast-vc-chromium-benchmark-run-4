1b07c2b9de975705c2b206918c3807076ada7cbe
Morten Stenshorne <mstensho@chromium.org>
2023-01-31 20:34:09 +0000

    Fix for repeated table sections inside clipped container.
    When we reach the block-end of a container with clipped overflow, we
    re-lay out the last fragment with block fragmentation forced off. That's
    RelayoutWithoutFragmentation() in NGLayoutAlgorithm. In this mode we
    didn't handle repeated table sections correctly.
    
    First of all, we wouldn't detect that the sections were repeatable at
    all, because IsAvoidBreakValue() would return false, since the
    constraint space says that we're not inside any block fragmentation (and
    therefore break-inside:avoid should have no effect). Move that check
    into code that's only run when at the first table box fragment.
    
    Secondly, we were bypassing some code that was needed to detect that we
    need to RelayoutAsLastTableBox() at this point. Fix that as well. We
    could perhaps have set the is_known_to_be_last_table_box_ in
    NGTableLayoutAlgorithm instead when in RelayoutWithoutFragmentation, but
    fixing it this way instead seemed more robust, less convoluted, albeit a
    bit more expensive, since we're performing an unnecessary layout pass at
    the last table fragment (but that should really be no big deal).
    
    Bug: 1410896
    Change-Id: If63da5a7388bc7dbc70681195c079b9cbe03907e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4208191
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099436}

--- third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
@@ -1085,9 +1085,6 @@ const NGLayoutResult* NGTableLayoutAlgorithm::GenerateFragment(
       if (child != grouped_children.header && child != grouped_children.footer)
         continue;
 
-      if (!IsAvoidBreakValue(ConstraintSpace(), child.Style().BreakInside()))
-        continue;
-
       const NGBlockBreakToken* child_break_token = entry.GetBreakToken();
       // If we've already broken inside the section, it's not going to repeat,
       // but rather perform regular fragmentation.
@@ -1125,6 +1122,11 @@ const NGLayoutResult* NGTableLayoutAlgorithm::GenerateFragment(
         if (!ConstraintSpace().HasKnownFragmentainerBlockSize() ||
             block_size > max_section_block_size)
           continue;
+
+        if (!IsAvoidBreakValue(ConstraintSpace(),
+                               child.Style().BreakInside())) {
+          continue;
+        }
       }
 
       if (child == grouped_children.header) {
@@ -1601,7 +1603,7 @@ const NGLayoutResult* NGTableLayoutAlgorithm::GenerateFragment(
   }
 
   bool has_entered_table_box = false;
-  if (ConstraintSpace().HasBlockFragmentation()) {
+  if (InvolvedInBlockFragmentation(container_builder_)) {
     LayoutUnit consumed_table_box_block_size =
         previously_consumed_table_box_block_size;
     if (incoming_table_break_data)
