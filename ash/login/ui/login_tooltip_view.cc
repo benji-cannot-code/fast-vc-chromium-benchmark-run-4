0ca30cd50568098771af498cffd51dc60b0abaff
Istvan Nagy <iscsi@google.com>
2023-01-31 17:35:01 +0000

    Use ColorId instead of OnThemeChaned in LoginTooltipView.
    Change-Id: I6de6ebbf46f6ae6d302e6b4d68308212fa526f81
    Bug: b:263227123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4197077
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099313}

--- ash/login/ui/login_tooltip_view.cc
+++ ash/login/ui/login_tooltip_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/login/ui/non_accessible_view.h"
 #include "ash/login/ui/views_utils.h"
+#include "ash/style/ash_color_id.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
@@ -30,24 +31,17 @@ LoginTooltipView::LoginTooltipView(const std::u16string& message,
     : LoginBaseBubbleView(std::move(anchor_view)) {
   info_icon_ = AddChildView(std::make_unique<views::ImageView>());
   info_icon_->SetPreferredSize(gfx::Size(kInfoIconSizeDp, kInfoIconSizeDp));
+  info_icon_->SetImage(ui::ImageModel::FromVectorIcon(
+      views::kInfoIcon, kColorAshIconColorPrimary));
 
   label_ = AddChildView(login_views_utils::CreateBubbleLabel(message, this));
+  label_->SetEnabledColorId(kColorAshTextColorPrimary);
 }
 
 LoginTooltipView::~LoginTooltipView() = default;
 
 void LoginTooltipView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   node_data->role = ax::mojom::Role::kTooltip;
-}
-
-void LoginTooltipView::OnThemeChanged() {
-  LoginBaseBubbleView::OnThemeChanged();
-  info_icon_->SetImage(gfx::CreateVectorIcon(
-      views::kInfoIcon,
-      AshColorProvider::Get()->GetContentLayerColor(
-          AshColorProvider::ContentLayerType::kIconColorPrimary)));
-  label_->SetEnabledColor(AshColorProvider::Get()->GetContentLayerColor(
-      AshColorProvider::ContentLayerType::kTextColorPrimary));
 }
 
 }  // namespace ash
