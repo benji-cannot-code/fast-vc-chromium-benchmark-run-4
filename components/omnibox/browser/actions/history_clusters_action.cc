2699a52dbff5e69c3211418774a842486c015c97
Ho Cheung <uioptt24@gmail.com>
2023-01-31 17:58:30 +0000

    Remove `omnibox_action_on_entities`
    According to the TODO in the code,
    we should remove it in versions other than M110.
    
    Change-Id: Id50a19ab63be7239b904d31afe3ed0200e8b7a4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205744
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099331}

--- components/omnibox/browser/actions/history_clusters_action.cc
+++ components/omnibox/browser/actions/history_clusters_action.cc
@@ -249,10 +249,6 @@ void AttachHistoryClustersActions(
     if (match.type == AutocompleteMatchType::SEARCH_SUGGEST_TAIL) {
       continue;
     }
-    if (!GetConfig().omnibox_action_on_entities &&
-        match.type == AutocompleteMatchType::SEARCH_SUGGEST_ENTITY) {
-      continue;
-    }
 
     if (AutocompleteMatch::IsSearchType(match.type)) {
       std::string query = base::UTF16ToUTF8(match.contents);
