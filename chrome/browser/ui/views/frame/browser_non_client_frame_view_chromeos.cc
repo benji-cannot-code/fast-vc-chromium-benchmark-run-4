fb9f635855ed467c55430237a087c23e04dee264
Marijn Kruisselbrink <mek@chromium.org>
2023-01-31 21:59:09 +0000

    Refactor WebAppFrameToolbarView ownership 4/x: Chrome OS.
    This makes Chrome OS work with the WebAppFrameToolbarInBrowserView
    flag enabled. Also slightly tweaks the layout of tabbed PWA windows
    on all platforms when the flag is enabled, making sure they look and
    behave the same on Chrome OS with and regardless of the state of the WebAppFrameToolbarInBrowserView flag. Their behavior and layout on
    other platforms likely will need further tweaking, but this at least
    is better than the behavior with WebAppFrameToolbarInBrowserView
    disabled on those platforms (tabbed PWA windows haven't shipped yet
    anywhere).
    
    Bug: 1407240
    Change-Id: Ic27e1f4caa9944a0497327e3e1caaa027aa8e9e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181004
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099477}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/browser/ui/views/web_apps/frame_toolbar/web_app_frame_toolbar_view.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
+#include "chrome/common/chrome_features.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/base/chromeos_ui_constants.h"
 #include "chromeos/ui/base/tablet_state.h"
@@ -192,8 +193,11 @@ void BrowserNonClientFrameViewChromeOS::Init() {
        browser_view()->AppUsesWindowControlsOverlay() ||
        browser_view()->AppUsesBorderlessMode())) {
     // Add the container for extra web app buttons (e.g app menu button).
-    set_web_app_frame_toolbar(
-        AddChildView(std::make_unique<WebAppFrameToolbarView>(browser_view())));
+    if (!base::FeatureList::IsEnabled(
+            features::kWebAppFrameToolbarInBrowserView)) {
+      set_web_app_frame_toolbar(AddChildView(
+          std::make_unique<WebAppFrameToolbarView>(browser_view())));
+    }
     if (AppIsBorderlessPwa())
       UpdateBorderlessModeEnabled();
   }
@@ -212,16 +216,29 @@ gfx::Rect BrowserNonClientFrameViewChromeOS::GetBoundsForTabStripRegion(
 
 gfx::Rect BrowserNonClientFrameViewChromeOS::GetBoundsForWebAppFrameToolbar(
     const gfx::Size& toolbar_preferred_size) const {
-  // TODO(https://crbug.com/1407240): Implement this method to make
-  // WebAppFrameToolbar-in-BrowserView work.
-  return gfx::Rect();
+  if (!GetShowCaptionButtons()) {
+    return gfx::Rect();
+  }
+  if (browser_view()->browser()->is_type_app_popup() &&
+      !browser_view()->AppUsesWindowControlsOverlay() &&
+      !browser_view()->AppUsesBorderlessMode()) {
+    return gfx::Rect();
+  }
+
+  const int x = GetToolbarLeftInset();
+  const int available_width = caption_button_container_->x() - x;
+  int painted_height = GetTopInset(false);
+  if (browser_view()->GetTabStripVisible()) {
+    painted_height += browser_view()->tabstrip()->GetPreferredSize().height();
+  }
+  return gfx::Rect(x, 0, std::max(0, available_width), painted_height);
 }
 
 void BrowserNonClientFrameViewChromeOS::LayoutWebAppWindowTitle(
     const gfx::Rect& available_space,
     views::Label& window_title_label) const {
-  // TODO(https://crbug.com/1407240): Implement this method to make
-  // WebAppFrameToolbar-in-BrowserView work.
+  // No window titles on Chrome OS, so just hide the window title.
+  window_title_label.SetVisible(false);
 }
 
 int BrowserNonClientFrameViewChromeOS::GetTopInset(bool restored) const {
@@ -253,6 +270,10 @@ int BrowserNonClientFrameViewChromeOS::GetTopInset(bool restored) const {
   if (web_app_frame_toolbar()) {
     header_height = std::max(
         header_height, web_app_frame_toolbar()->GetPreferredSize().height());
+  }
+  auto toolbar_size = browser_view()->GetWebAppFrameToolbarPreferredSize();
+  if (!toolbar_size.IsEmpty()) {
+    header_height = std::max(header_height, toolbar_size.height());
   }
   if (browser_view()->GetTabStripVisible())
     return header_height - browser_view()->GetTabStripHeight();
@@ -436,7 +457,9 @@ void BrowserNonClientFrameViewChromeOS::LayoutWindowControlsOverlay() {
 }
 
 void BrowserNonClientFrameViewChromeOS::UpdateBorderlessModeEnabled() {
-  web_app_frame_toolbar()->UpdateBorderlessModeEnabled();
+  if (web_app_frame_toolbar()) {
+    web_app_frame_toolbar()->UpdateBorderlessModeEnabled();
+  }
   caption_button_container_->UpdateBorderlessModeEnabled(
       browser_view()->IsBorderlessModeEnabled());
 }
@@ -474,6 +497,8 @@ void BrowserNonClientFrameViewChromeOS::Layout() {
                                                  caption_button_container_->x(),
                                                  0, painted_height);
     }
+  } else if (AppIsBorderlessPwa()) {
+    UpdateBorderlessModeEnabled();
   }
 
   BrowserNonClientFrameView::Layout();
@@ -907,8 +932,15 @@ bool BrowserNonClientFrameViewChromeOS::GetShouldPaint() const {
 void BrowserNonClientFrameViewChromeOS::OnAddedToOrRemovedFromOverview() {
   const bool should_show_caption_buttons = GetShowCaptionButtons();
   caption_button_container_->SetVisible(should_show_caption_buttons);
-  if (web_app_frame_toolbar())
+  if (web_app_frame_toolbar()) {
     web_app_frame_toolbar()->SetVisible(should_show_caption_buttons);
+  }
+  if (base::FeatureList::IsEnabled(
+          features::kWebAppFrameToolbarInBrowserView)) {
+    // The WebAppFrameToolbarView is part of the BrowserView, so make sure the
+    // BrowserView is re-layed out to take into account these changes.
+    browser_view()->InvalidateLayout();
+  }
 }
 
 std::unique_ptr<chromeos::FrameHeader>
