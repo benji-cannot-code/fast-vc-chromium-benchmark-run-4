8c4f9be8a74d5a681da6082a6d608c49faf3040b
Jayson Adams <shrike@chromium.org>
2023-01-31 21:56:55 +0000

    [Mac Occlusion Tests] Disable manual occlusion testing on macOS13 (Ventura)
    macOS13 breaks manual occlusion detection. This cl prevents those tests
    from running on macOS13.
    
    Manual occlusion detection has not been deployed, so skipping these
    tests for now on Ventura is not a problem.
    
    Bug: 1372419, 883031
    
    Low-Coverage-Reason: the checker is complaining about low incremental test coverage for changes made to the tests!
    
    Test: out/Default/content_browsertests \
      --gtest_filter=*WindowOcclusionBrowserTestMac*
    
    Change-Id: Ib7ae5038a865875a38164624179eb1e99a721822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205933
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099473}

--- content/app_shim_remote_cocoa/window_occlusion_browsertest_mac.mm
+++ content/app_shim_remote_cocoa/window_occlusion_browsertest_mac.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "base/mac/foundation_util.h"
+#import "base/mac/mac_util.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/mac/scoped_objc_class_swizzler.h"
 #import "base/task/single_thread_task_runner.h"
@@ -340,6 +341,14 @@ class WindowOcclusionBrowserTestMac
     } else {
       _features.InitAndDisableFeature(features::kMacWebContentsOcclusion);
     }
+  }
+
+  void SetUp() override {
+    if (base::mac::IsAtLeastOS13()) {
+      GTEST_SKIP()
+          << "Manual window occlusion detection is broken on macOS Ventura.";
+    }
+    ContentBrowserTest::SetUp();
   }
 
   ~WindowOcclusionBrowserTestMac() {
