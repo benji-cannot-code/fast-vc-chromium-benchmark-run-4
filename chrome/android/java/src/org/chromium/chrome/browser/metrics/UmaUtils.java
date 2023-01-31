181bff223b8b7dacaa29c6065844d97355c80955
Henrique Nakashima <hnakashima@google.com>
2023-01-31 19:38:05 +0000

    [Android] Handle all statuses from getAppStandbyBucket(), add unit tests
    Handle "Never" and add an UMA enum for other statuses.
    
    Add unit tests for UmaUtils#recordBackgroundRestrictions() and
    UmaUtils#recordMinidumpUploadingTime().
    
    Bug: 1360927
    Change-Id: I35f06fa516a71a9b0c6ec6632ab941de38735b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4204879
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099410}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaUtils.java
@@ -72,7 +72,7 @@ public class UmaUtils {
     @IntDef({StandbyBucketStatus.ACTIVE, StandbyBucketStatus.WORKING_SET,
             StandbyBucketStatus.FREQUENT, StandbyBucketStatus.RARE, StandbyBucketStatus.RESTRICTED,
             StandbyBucketStatus.UNSUPPORTED, StandbyBucketStatus.EXEMPTED,
-            StandbyBucketStatus.COUNT})
+            StandbyBucketStatus.NEVER, StandbyBucketStatus.OTHER, StandbyBucketStatus.COUNT})
     private @interface StandbyBucketStatus {
         int ACTIVE = 0;
         int WORKING_SET = 1;
@@ -81,7 +81,9 @@ public class UmaUtils {
         int RESTRICTED = 4;
         int UNSUPPORTED = 5;
         int EXEMPTED = 6;
-        int COUNT = 7;
+        int NEVER = 7;
+        int OTHER = 8;
+        int COUNT = 9;
     }
 
     /**
@@ -204,6 +206,10 @@ public class UmaUtils {
                 return "Unsupported";
             case StandbyBucketStatus.EXEMPTED:
                 return "Exempted";
+            case StandbyBucketStatus.NEVER:
+                return "Never";
+            case StandbyBucketStatus.OTHER:
+                return "Other";
             default:
                 assert false : "Unexpected standby bucket " + standbyBucket;
                 return "Unknown";
@@ -217,7 +223,7 @@ public class UmaUtils {
         UsageStatsManager usageStatsManager =
                 (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
         int standbyBucket = usageStatsManager.getAppStandbyBucket();
-        int standbyBucketUma = StandbyBucketStatus.UNSUPPORTED;
+        int standbyBucketUma;
         switch (standbyBucket) {
             case UsageStatsManager.STANDBY_BUCKET_ACTIVE:
                 standbyBucketUma = StandbyBucketStatus.ACTIVE;
@@ -237,8 +243,12 @@ public class UmaUtils {
             case 5: // STANDBY_BUCKET_EXEMPTED
                 standbyBucketUma = StandbyBucketStatus.EXEMPTED;
                 break;
+            case 50: // STANDBY_BUCKET_NEVER
+                standbyBucketUma = StandbyBucketStatus.NEVER;
+                break;
             default:
-                assert false : "Unexpected standby bucket " + standbyBucket;
+                standbyBucketUma = StandbyBucketStatus.OTHER;
+                break;
         }
         return standbyBucketUma;
     }
