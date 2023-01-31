900d81bef8b4a3584624c0b40a3721a7fe489178
David Bienvenu <davidbienvenu@chromium.org>
2023-01-31 01:05:26 +0000

    win7dep: Remove pre-win10 Dpi awareness code
    In Win10, Chrome processes should all be dpi aware, so we can remove
    the checks for it.
    
    This CL also makes base link with shcore.lib, so we can call
    SetProcessDpiAwareness directly, without a wrapper.
    
    Bug: 1385856
    Change-Id: I0a269785eafe4b5cab8fe3162de4c9af40a5b060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4185498
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099026}

--- ui/display/win/screen_win.cc
+++ ui/display/win/screen_win.cc
@@ -45,20 +45,11 @@ ScreenWin* g_instance = nullptr;
 
 // Gets the DPI for a particular monitor.
 absl::optional<int> GetPerMonitorDPI(HMONITOR monitor) {
-  if (!base::win::IsProcessPerMonitorDpiAware())
-    return absl::nullopt;
-
-  static auto get_dpi_for_monitor_func = []() {
-    const HMODULE shcore_dll = ::LoadLibrary(L"shcore.dll");
-    return reinterpret_cast<decltype(&::GetDpiForMonitor)>(
-        shcore_dll ? ::GetProcAddress(shcore_dll, "GetDpiForMonitor")
-                   : nullptr);
-  }();
   UINT dpi_x, dpi_y;
-  if (!get_dpi_for_monitor_func ||
-      !SUCCEEDED(
-          get_dpi_for_monitor_func(monitor, MDT_EFFECTIVE_DPI, &dpi_x, &dpi_y)))
+  if (!SUCCEEDED(
+          ::GetDpiForMonitor(monitor, MDT_EFFECTIVE_DPI, &dpi_x, &dpi_y))) {
     return absl::nullopt;
+  }
 
   DCHECK_EQ(dpi_x, dpi_y);
   return static_cast<int>(dpi_x);
@@ -759,11 +750,13 @@ absl::optional<bool> ScreenWin::IsWindowOnCurrentVirtualDesktop(
   return absl::nullopt;
 }
 
-ScreenWin::ScreenWin(bool initialize) {
+ScreenWin::ScreenWin(bool initialize_from_system)
+    : per_process_dpi_awareness_disabled_for_testing_(!initialize_from_system) {
   DCHECK(!g_instance);
   g_instance = this;
-  if (initialize)
+  if (initialize_from_system) {
     Initialize();
+  }
 }
 
 gfx::Point ScreenWin::GetCursorScreenPoint() {
@@ -1014,7 +1007,7 @@ ScreenWinDisplay ScreenWin::GetScreenWinDisplayVia(Getter getter,
 
 int ScreenWin::GetSystemMetricsForScaleFactor(float scale_factor,
                                               int metric) const {
-  if (base::win::IsProcessPerMonitorDpiAware()) {
+  if (!PerProcessDPIAwarenessDisabledForTesting()) {
     static const auto get_system_metrics_for_dpi =
         reinterpret_cast<decltype(&::GetSystemMetricsForDpi)>(
             base::win::GetUser32FunctionPointer("GetSystemMetricsForDpi"));
@@ -1024,7 +1017,7 @@ int ScreenWin::GetSystemMetricsForScaleFactor(float scale_factor,
     }
   }
 
-  // Windows 8.1 doesn't support GetSystemMetricsForDpi(), yet does support
+  // Versions < WIN10_RS1 don't support GetSystemMetricsForDpi, but do support
   // per-process dpi awareness.
   return base::ClampRound(GetSystemMetrics(metric) * scale_factor /
                           GetPrimaryDisplay().device_scale_factor());
@@ -1053,6 +1046,10 @@ void ScreenWin::OnUwpTextScaleFactorChanged() {
 void ScreenWin::OnUwpTextScaleFactorCleanup(UwpTextScaleFactor* source) {
   scale_factor_observation_.Reset();
   UwpTextScaleFactor::Observer::OnUwpTextScaleFactorCleanup(source);
+}
+
+bool ScreenWin::PerProcessDPIAwarenessDisabledForTesting() const {
+  return per_process_dpi_awareness_disabled_for_testing_;
 }
 
 }  // namespace win
