7ecdb3be9bffcde5c64a1f5654ad1850a28f9ee1
Colin Blundell <blundell@chromium.org>
2023-01-31 08:10:52 +0000

    [GL] Eliminate NativePixmapGLBinding::BindTexImage()
    NativePixmapGLBinding::BindTexImage() is one of the few remaining
    usages of GLImage::BindTexImage() that is not on a single concrete
    subclass. This CL eliminates this static helper in favor of having
    NativePixmapEGLBinding and NativePixmapEGLX11Binding both inline its
    implementation at their callsite. There is a minor amount of code
    duplication, but we would likely eventually have gone in this direction
    anyway when eliminating GLImage{Native, EGL}Pixmap, which have different
    implementations of BindTexImage().
    
    The immediate benefit is that we will be able to remove BindTexImage()
    from the cross-platform GLImage interface in a followup CL.
    
    Bug: 1310018
    Change-Id: I8924f57e5c257e6dbeb9072a4eb034d725b98e81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200659
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099120}

--- ui/ozone/common/native_pixmap_egl_binding.cc
+++ ui/ozone/common/native_pixmap_egl_binding.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_image_native_pixmap.h"
+#include "ui/gl/scoped_binders.h"
 
 namespace ui {
 
@@ -82,8 +83,21 @@ std::unique_ptr<NativePixmapGLBinding> NativePixmapEGLBinding::Create(
 }
 
 bool NativePixmapEGLBinding::BindTexture(GLenum target, GLuint texture_id) {
-  return NativePixmapGLBinding::BindTexture(gl_image_.get(), target,
-                                            texture_id);
+  gl::ScopedTextureBinder binder(target, texture_id);
+
+  gl::GLApi* api = gl::g_current_gl_context;
+  DCHECK(api);
+  api->glTexParameteriFn(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  api->glTexParameteriFn(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  api->glTexParameteriFn(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  api->glTexParameteriFn(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  if (!gl_image_->BindTexImage(target)) {
+    LOG(ERROR) << "Unable to bind GL image to target = " << target;
+    return false;
+  }
+
+  return true;
 }
 
 GLuint NativePixmapEGLBinding::GetInternalFormat() {
