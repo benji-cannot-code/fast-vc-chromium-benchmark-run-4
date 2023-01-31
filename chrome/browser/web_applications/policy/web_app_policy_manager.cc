30ff18b646843d5c0f8f78fe7033e0ef9b315216
Dibyajyoti Pal <dibyapal@google.com>
2023-01-31 17:02:21 +0000

    [dPWA] Disable policy install of web apps on guest profiles
    This CL adds logic that limits policy installs to:
    1. ChromeOS guest sessions.
    2. On profiles where web applications are allowed.
    
    On other use-cases (like incognito/guest profiles), policy installs
    are skipped.
    
    Bug: b/264282803, crbug.com/1336566
    Change-Id: I72c991f1266ecd05ac2db433fae622079b9789ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4178465
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Camden King <camdenking@google.com>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099291}

--- chrome/browser/web_applications/policy/web_app_policy_manager.cc
+++ chrome/browser/web_applications/policy/web_app_policy_manager.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/browser/web_applications/web_app_sync_bridge.h"
 #include "chrome/browser/web_applications/web_app_system_web_app_delegate_map_utils.h"
+#include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -57,6 +58,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "components/policy/core/common/policy_pref_names.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "components/user_manager/user_manager.h"
+#endif
 
 namespace {
 
@@ -82,6 +87,18 @@ void LogIsolatedWebAppInstallResult(
   }
 }
 #endif
+
+// Policy installed apps are only allowed on:
+// 1. ChromeOS guest sessions (current only on Ash).
+// 2. All Chrome profiles apart from incognito/guest profiles.
+bool AreForceInstalledAppsAllowed(Profile* profile) {
+  bool allowed = web_app::AreWebAppsUserInstallable(profile);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  allowed = allowed || user_manager::UserManager::Get()->IsLoggedInAsGuest() ||
+            user_manager::UserManager::Get()->IsLoggedInAsPublicAccount();
+#endif
+  return allowed;
+}
 
 }  // namespace
 
@@ -275,6 +292,11 @@ bool WebAppPolicyManager::IsDisabledAppsModeHidden() const {
 }
 
 void WebAppPolicyManager::RefreshPolicyInstalledApps() {
+  if (!AreForceInstalledAppsAllowed(profile_)) {
+    OnWebAppForceInstallPolicyParsed();
+    return;
+  }
+
   // If this is called again while in progress, we will run it again once the
   // |SynchronizeInstalledApps| call is finished.
   if (is_refreshing_) {
@@ -644,13 +666,7 @@ void WebAppPolicyManager::OnAppsSynchronized(
                                   url_and_result.second.code);
   }
 
-  if (on_apps_synchronized_for_testing_) {
-    std::move(on_apps_synchronized_for_testing_).Run();
-  }
-
-  if (initialization_complete_) {
-    std::move(initialization_complete_).Run();
-  }
+  OnWebAppForceInstallPolicyParsed();
 }
 
 WebAppPolicyManager::WebAppSetting::WebAppSetting() {
@@ -792,6 +808,16 @@ void WebAppPolicyManager::PopulateDisabledWebAppsIdsLists() {
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 #endif  // BUILDFLAG(IS_CHROMEOS)
+}
+
+void WebAppPolicyManager::OnWebAppForceInstallPolicyParsed() {
+  if (on_apps_synchronized_for_testing_) {
+    std::move(on_apps_synchronized_for_testing_).Run();
+  }
+
+  if (initialization_complete_) {
+    std::move(initialization_complete_).Run();
+  }
 }
 
 }  // namespace web_app
