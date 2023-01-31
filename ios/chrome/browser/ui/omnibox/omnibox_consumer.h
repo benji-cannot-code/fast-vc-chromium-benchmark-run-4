fb068c1d60d1388fb674626bc27a94d52d9fd30a
Ameur Hosni <ameurhosni@google.com>
2023-01-31 09:15:00 +0000

    [IOS][Omnibox] Cleanup omnibox leading image a11y flag
    CL to remove the use of a flag to have a11y on
    omnibox leading image.
    
    Bug: 1405433
    Change-Id: I45571677b0f31bb98c832cde50e3c23b647983ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4203531
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099134}

--- ios/chrome/browser/ui/omnibox/omnibox_consumer.h
+++ ios/chrome/browser/ui/omnibox/omnibox_consumer.h
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol OmniboxConsumer<NSObject>
 
 // Notifies the consumer to update the autocomplete icon for the currently
-// highlighted autocomplete result.
-- (void)updateAutoCompleteIconWithoutAccessibilityIdentifier:(UIImage*)icon;
-
-// Notifies the consumer to update the autocomplete icon for the currently
 // highlighted autocomplete result with given accessibility identifier.
 - (void)updateAutocompleteIcon:(UIImage*)icon
     withAccessibilityIdentifier:(NSString*)accessibilityIdentifier;
--- ios/chrome/browser/ui/omnibox/omnibox_container_view.h
+++ ios/chrome/browser/ui/omnibox/omnibox_container_view.h
@@ -42,9 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setLeadingImage:(UIImage*)image
     withAccessibilityIdentifier:(NSString*)accessibilityIdentifier;
 
-// Sets the leading button's image.
-- (void)setLeadingImageWithoutAccessibilityIdentifier:(UIImage*)image;
-
 // Sets the alpha level of the leading image view.
 - (void)setLeadingImageAlpha:(CGFloat)alpha;
 
