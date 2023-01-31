5b36c0674f2b02863a5089ac0fa140e6279ea01c
Moe Ahmadi <mahmadi@chromium.org>
2023-01-31 20:36:26 +0000

    [suggest-internals] Adds Observer interface to RemoteSuggestionsService
    This allows the chrome://suggest-internals WebUI handler to observe
    RemoteSuggestionsService and update the UI with the requests/responses.
    
    Since observers get notified of "request sent" and "response received"
    in two callbacks, the RemoteSuggestionsService uses an UnguessableToken
    to uniquely identify the request/response pairs.
    
    In the "response received" callback, the observers receive a const
    references to the unique pointer to the response body. This is done
    for two reasons, one is to avoid making copies of the response body
    and two, is to allow the chrome://suggest-internals WebUI handler to
    intercept the response, giving it an opportunity to replace it with
    a hardcoded mock response.
    
    Bug: 1410570
    Change-Id: I578bea136d8dfea97d12a8cf0566ad757ad0f00c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4206606
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099439}

--- components/omnibox/browser/zero_suggest_provider.h
+++ components/omnibox/browser/zero_suggest_provider.h
@@ -115,6 +115,7 @@ class ZeroSuggestProvider : public BaseSearchProvider {
   void OnURLLoadComplete(const AutocompleteInput& input,
                          const ResultType result_type,
                          const network::SimpleURLLoader* source,
+                         const bool response_received,
                          std::unique_ptr<std::string> response_body);
   // Called when the prefetch network request has completed.
   // `input` and `result_type` are bound to this callback. The former is the
@@ -123,6 +124,7 @@ class ZeroSuggestProvider : public BaseSearchProvider {
   void OnPrefetchURLLoadComplete(const AutocompleteInput& input,
                                  const ResultType result_type,
                                  const network::SimpleURLLoader* source,
+                                 const bool response_received,
                                  std::unique_ptr<std::string> response_body);
 
   // Returns an AutocompleteMatch for a navigational suggestion |navigation|.
