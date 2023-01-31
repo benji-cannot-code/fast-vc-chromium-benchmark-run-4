5fe671440a7743e4739be4aadc75c9a4c89ea7a2
Danila Kuzmin <dkuzmin@google.com>
2023-01-31 14:57:50 +0000

    [OOBE][LoginDisplayHost]: Remove unused variable from LDHWebUI
    `restore_path_` isn't used anywhere
    
    Bug: b:266930990
    Change-Id: I1d13fa1e585e85ac8b3b1dc6361edd8f8fa63d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4199028
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099241}

--- chrome/browser/ash/login/ui/login_display_host_webui.cc
+++ chrome/browser/ash/login/ui/login_display_host_webui.cc
@@ -540,8 +540,6 @@ void LoginDisplayHostWebUI::StartWizard(OobeScreenId first_screen) {
 
   TryToPlayOobeStartupSound();
 
-  // Keep parameters to restore if renderer crashes.
-  restore_path_ = RESTORE_WIZARD;
   first_screen_ = first_screen;
 
   VLOG(1) << "Login WebUI >> wizard";
@@ -580,7 +578,6 @@ void LoginDisplayHostWebUI::CancelUserAdding() {
 void LoginDisplayHostWebUI::OnStartSignInScreen() {
   DisableKeyboardOverscroll();
 
-  restore_path_ = RESTORE_SIGN_IN;
   finalize_animation_type_ = ANIMATION_WORKSPACE;
 
   VLOG(1) << "Login WebUI >> sign in";
