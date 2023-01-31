6c55e8176a68faa10996a538a85093f4bfeb5639
Danny Wang <wangdanny@google.com>
2023-01-31 18:57:57 +0000

    Settings Split: Add settings to mouse page (use existing prefs)
    Screenshot: http://screen/7uHfQG5wAQECKXD
    
    Bug: b/241965700
    Test: browser_tests OSSettingsPerDeviceMouseSubsectionV3Test
    Change-Id: Iefac542b8d184dd0e6eb67726fcb2f125402ee6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149908
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099382}

--- chrome/browser/resources/settings/chromeos/device_page/per_device_mouse.ts
+++ chrome/browser/resources/settings/chromeos/device_page/per_device_mouse.ts
@@ -11,8 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../../icons.html.js';
 import '../../settings_shared.css.js';
+import 'chrome://resources/cr_components/localized_link/localized_link.js';
+import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
+import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
+import '../../controls/settings_radio_group.js';
+import '../../controls/settings_slider.js';
+import '../../controls/settings_toggle_button.js';
+import '../../settings_shared.css.js';
+import 'chrome://resources/cr_elements/cr_slider/cr_slider.js';
 
-import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import {PolymerElementProperties} from 'chrome://resources/polymer/v3_0/polymer/interfaces.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {routes} from '../os_route.js';
@@ -23,25 +31,19 @@ import {getInputDeviceSettingsProvider} from './input_device_mojo_interface_prov
 import {InputDeviceSettingsProviderInterface, Mouse} from './input_device_settings_types.js';
 import {getTemplate} from './per_device_mouse.html.js';
 
-const SettingsPerDeviceMouseElementBase =
-    RouteObserverMixin(I18nMixin(PolymerElement));
+const SettingsPerDeviceMouseElementBase = RouteObserverMixin(PolymerElement);
 
 class SettingsPerDeviceMouseElement extends SettingsPerDeviceMouseElementBase {
-  static get is() {
+  static get is(): string {
     return 'settings-per-device-mouse';
   }
 
-  static get template() {
+  static get template(): HTMLTemplateElement {
     return getTemplate();
   }
 
-  static get properties() {
+  static get properties(): PolymerElementProperties {
     return {
-      isDeviceSettingsSplitEnabled: {
-        type: Boolean,
-        value: false,
-      },
-
       mice: {
         type: Array,
       },
@@ -72,7 +74,7 @@ class SettingsPerDeviceMouseElement extends SettingsPerDeviceMouseElementBase {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'settings-mouse': SettingsPerDeviceMouseElement;
+    'settings-per-device-mouse': SettingsPerDeviceMouseElement;
   }
 }
 
