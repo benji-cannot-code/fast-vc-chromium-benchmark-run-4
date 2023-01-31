c7ea5c86839573c9758b5ff5dbf27828592239e4
Youkichi Hosoi <youkichihosoi@chromium.org>
2023-01-31 02:34:13 +0000

    arc: Acquire wake lock while ARCVM /data migration screen is shown
    Bug: b:258278176
    Test: Manually go through the ARCVM /data migration flow.
    
    Change-Id: Ifa0c476c66207c6e778c8832d3942b52f86a0fc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4199040
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Youkichi Hosoi <youkichihosoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099064}

--- chrome/browser/ash/login/screens/arc_vm_data_migration_screen.h
+++ chrome/browser/ash/login/screens/arc_vm_data_migration_screen.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/screens/base_screen.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/login/arc_vm_data_migration_screen_handler.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "services/device/public/mojom/wake_lock.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace ash {
@@ -39,7 +41,11 @@ class ArcVmDataMigrationScreen : public BaseScreen {
 
   void HandleFatalError();
 
+  device::mojom::WakeLock* GetWakeLock();
+
   Profile* profile_;
+
+  mojo::Remote<device::mojom::WakeLock> wake_lock_;
 
   base::WeakPtr<ArcVmDataMigrationScreenView> view_;
 
