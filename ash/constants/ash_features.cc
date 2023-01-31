2ffabd6915e946242b8f220e6ef839165630c907
Ben Becker <benbecker@chromium.org>
2023-01-31 21:59:27 +0000

    CloseAll: Remove legacy code paths
    This CL removes all code mentioning the DeskMiniView's close_all_button_
    or the DesksCloseAll flag.
    
    Bug: b/263166880
    Change-Id: Ie29293c1f1003074ffe3f5485970730f6a978b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205141
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099478}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2408,12 +2408,6 @@ bool IsCryptauthAttestationSyncingEnabled() {
   return base::FeatureList::IsEnabled(kCryptauthAttestationSyncing);
 }
 
-bool IsDesksCloseAllEnabled() {
-  // TODO(b/263166880): Remove this function and all code paths where this is
-  // false.
-  return true;
-}
-
 bool IsDnsOverHttpsWithIdentifiersReuseOldPolicyEnabled() {
   return base::FeatureList::IsEnabled(
       kDnsOverHttpsWithIdentifiersReuseOldPolicy);
--- ash/wm/desks/desk_mini_view.h
+++ ash/wm/desks/desk_mini_view.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class CloseButton;
 class DeskActionContextMenu;
 class DeskActionView;
 class DeskNameView;
@@ -55,8 +54,6 @@ class ASH_EXPORT DeskMiniView : public views::View,
   Desk* desk() { return desk_; }
 
   DeskNameView* desk_name_view() { return desk_name_view_; }
-
-  const CloseButton* close_desk_button() const { return close_desk_button_; }
 
   const DeskActionView* desk_action_view() const { return desk_action_view_; }
   DeskActionView* desk_action_view() { return desk_action_view_; }
@@ -113,11 +110,7 @@ class ASH_EXPORT DeskMiniView : public views::View,
   // Closes context menu on this mini view if one exists.
   void MaybeCloseContextMenu();
 
-  // Sets either the `desk_action_view_` or the `close_desk_button_` visibility
-  // to false depending on whether the `kDesksCloseAll` feature is active, and
-  // then removes the desk. If `close_type` is `kCloseAllWindows*`, this
-  // function tells the `DesksController` to remove `desk_`'s windows as well,
-  // and wait for the user to confirm.
+  // Invoked when the user has clicked a desk close button.
   void OnRemovingDesk(DeskCloseType close_type);
 
   // views::View:
@@ -171,11 +164,7 @@ class ASH_EXPORT DeskMiniView : public views::View,
   // The editable desk name.
   DeskNameView* desk_name_view_ = nullptr;
 
-  // The close button that shows on hover.
-  CloseButton* close_desk_button_ = nullptr;
-
-  // When the Close-All flag is enabled, we store the hover interface for desk
-  // actions here.
+  // Stores the hover interface for desk actions.
   DeskActionView* desk_action_view_ = nullptr;
 
   // The context menu that appears when `desk_preview_` is right-clicked or
