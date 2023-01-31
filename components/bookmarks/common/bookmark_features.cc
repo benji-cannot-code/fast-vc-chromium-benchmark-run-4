c3485fc7502c7e5b5c0dd946f2e470f7b53ba3ea
Michael Wilson <mjwilson@chromium.org>
2023-01-31 21:56:00 +0000

    Revert "[omnibox][bookmark-paths] Launch for desktop."
    This reverts commit 17f744e23ab9e0e93dde109cc514392dc43ad109.
    
    Reason for revert: BookmarkProviderTest.Positions is failing on Linux ChromiumOS MSan Tests, causing a build failure.
    
    
    
    Original change's description:
    > [omnibox][bookmark-paths] Launch for desktop.
    >
    > Enables by default short bookmarks and bookmark paths with the 3
    > optimizations:
    > - BookmarkApproximateNodeMatch
    > - BookmarkLimitNumNodesForBookmarkSearch (1000)
    > - BookmarkIndexPaths
    >
    > `OmniboxShortBookmarkSuggestions` is not launching. It's a more
    > aggressive alternative to
    > `OmniboxShortBookmarkSuggestionsByTotalInputLength`, which is launching.
    >
    > We originally launched a while back, then unlaunched due to latency
    > regressions, which were almost completely resolved with the index
    > optimization. The other optimizations. The other 2 optimizations had
    > significant improvements as well, so we're launching those too.
    >
    > Three's another optimization BookmarkTypedUrlsMap that didn't improve
    > latency, so we're not launching that.
    >
    > Mobile beta looked good as well, so we'll likely run mobile stable
    > experiments in m109.
    >
    > Desktop stable:
    > docs.google.com/document/d/1YkGqS9SLmpy8XpktBMrZ4EN5NTWxCRjsHPBNXZ-T8QI
    > Mobile beta:
    > docs.google.com/document/d/1ccRLZiP46tyWB8LvIonwsGp16mnKaTOmCfzuxUjgZhQ
    >
    > Tests were updated by:
    > - Tests that specifically tested the path and shorts features continue
    >   to do so. Some `ScopedFeatureList` had to be made more explicit since
    >   we can no longer assume the features are disabled by default on all
    >   platforms.
    > - Tests that didn't specifically test the features now explicitly
    >   disable the features so they continue to test the same behavior. Once
    >   mobile launches, we can update them to test the enabled behavior
    >   instead.
    > - An exception to the above, `BookmarkProviderTest.Positions` was
    >   updated to test the enabled behavior.
    > - Most tests don't care about the features, and didn't need to
    >   explicitly disable the feautres to pass.
    >
    > Bug: 1129524
    > Change-Id: Ibd5ca133b6fd03ba4f3dbbbf80766fe8eecf9f24
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143836
    > Commit-Queue: manuk hovanesian <manukh@chromium.org>
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1099243}
    
    Bug: 1129524
    Change-Id: I99bd605eeb260438828985f08da8a309e51d6f68
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4209499
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099472}

--- components/bookmarks/common/bookmark_features.cc
+++ components/bookmarks/common/bookmark_features.cc
@@ -9,13 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace bookmarks {
 
-constexpr auto kEnabledByDefaultDesktopOnly =
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-    base::FEATURE_DISABLED_BY_DEFAULT;
-#else
-    base::FEATURE_ENABLED_BY_DEFAULT;
-#endif
-
 // If enabled, uses an approximate pre-check to determine if an input matches a
 // particular bookmark index node. This pre-check is faster than the more
 // accurate check, but it returns false positives; therefore, it's only a
@@ -23,7 +16,7 @@ constexpr auto kEnabledByDefaultDesktopOnly =
 // `omnibox::kBookmarkPaths` is disabled.
 BASE_FEATURE(kApproximateNodeMatch,
              "BookmarkApproximateNodeMatch",
-             kEnabledByDefaultDesktopOnly);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // If enabled, uses an alternative approach to loading typed counts for URLs
 // when fetching bookmark matches for the bookmark provider.
@@ -47,13 +40,13 @@ BASE_FEATURE(kTypedUrlsMap,
 // `omnibox::kBookmarkPaths` is disabled.
 BASE_FEATURE(kLimitNumNodesForBookmarkSearch,
              "BookmarkLimitNumNodesForBookmarkSearch",
-             kEnabledByDefaultDesktopOnly);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // See `kLimitNumNodesForBookmarkSearch`.
 const base::FeatureParam<int> kLimitNumNodesForBookmarkSearchCount(
     &kLimitNumNodesForBookmarkSearch,
     "BookmarkLimitNumNodesForBookmarkSearchCount",
-    1000);
+    3000);
 
 // If enabled, creates and uses a lightweight (compared to the existing
 // `TitledUrlIndex`). The index maps the terms in paths and the number of paths
@@ -65,6 +58,6 @@ const base::FeatureParam<int> kLimitNumNodesForBookmarkSearchCount(
 // unnecessarily.
 BASE_FEATURE(kIndexPaths,
              "BookmarkIndexPaths",
-             kEnabledByDefaultDesktopOnly);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace bookmarks
