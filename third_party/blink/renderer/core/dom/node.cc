8f5fe602f122506c3904ccf6b0624dc51f56a391
Camillo Bruni <cbruni@chromium.org>
2023-02-01 20:33:40 +0000

    [blink] Only use NodeCount in debug builds
    It previously had no effect in non-DCHECK builds. This CL removes the
    call overhead in release builds.
    
    Bug: 808503
    Change-Id: I9a007042f960eab4189755f1dccafa02ecab4461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4209074
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100046}

--- third_party/blink/renderer/core/dom/node.cc
+++ third_party/blink/renderer/core/dom/node.cc
@@ -2230,7 +2230,9 @@ Node::InsertionNotificationRequest Node::InsertedInto(
          IsContainerNode());
   if (insertion_point.isConnected()) {
     SetFlag(kIsConnectedFlag);
+#if DCHECK_IS_ON()
     insertion_point.GetDocument().IncrementNodeCount();
+#endif
   }
   if (ParentOrShadowHostNode()->IsInShadowTree())
     SetFlag(kIsInShadowTreeFlag);
@@ -2249,7 +2251,9 @@ void Node::RemovedFrom(ContainerNode& insertion_point) {
     ClearNeedsStyleInvalidation();
     ClearChildNeedsStyleInvalidation();
     ClearFlag(kIsConnectedFlag);
+#if DCHECK_IS_ON()
     insertion_point.GetDocument().DecrementNodeCount();
+#endif
   }
   if (IsInShadowTree() && !ContainingTreeScope().RootNode().IsShadowRoot())
     ClearFlag(kIsInShadowTreeFlag);
