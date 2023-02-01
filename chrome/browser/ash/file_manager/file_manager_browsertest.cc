01984cde4ceb6e92097c354842040d2337ca8cdf
Aida Zolic <aidazolic@chromium.org>
2023-02-01 11:44:10 +0000

    DLP: Add blocked Drive integ test
    Adds an integration test that checks that Google Drive volume is
    disabled when needed.
    
    R=wenbojie@chromium.org
    
    Bug: b/263072872
    Change-Id: I40664ce5c1e14da6e7f59564db514a14d730b10a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176849
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099767}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1576,6 +1576,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("saveAsDlpRestrictedCrostini").EnableDlp(),
         TestCase("saveAsDlpRestrictedVm").EnableDlp(),
         TestCase("saveAsDlpRestrictedUsb").EnableDlp(),
+        TestCase("saveAsDlpRestrictedDrive").EnableDlp(),
         TestCase("saveAsNonDlpRestricted").EnableDlp(),
         TestCase("saveAsDlpRestrictedRedirectsToMyFiles").EnableDlp(),
         TestCase("openDlpRestrictedFile").EnableDlp(),
