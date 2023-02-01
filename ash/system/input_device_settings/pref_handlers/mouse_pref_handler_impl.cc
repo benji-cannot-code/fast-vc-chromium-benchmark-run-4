5b5a385997b3954a59ff8985ba8e783b0e6f8aee
Michael Checo <michaelcheco@google.com>
2023-02-01 17:23:13 +0000

    Settings Split: Implement updating of mouse settings in prefs
    Bug: b/241965700
    Change-Id: I619418de114070391bcf0b5a49383a6f8d28f0d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193407
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099910}

--- ash/system/input_device_settings/pref_handlers/mouse_pref_handler_impl.cc
+++ ash/system/input_device_settings/pref_handlers/mouse_pref_handler_impl.cc
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/input_device_settings/pref_handlers/mouse_pref_handler_impl.h"
 
 #include "ash/public/mojom/input_device_settings.mojom-forward.h"
+#include "ash/public/mojom/input_device_settings.mojom.h"
+#include "ash/system/input_device_settings/input_device_settings_pref_names.h"
+#include "base/check.h"
 #include "base/notreached.h"
 #include "components/prefs/pref_service.h"
 
@@ -20,10 +23,40 @@ void MousePrefHandlerImpl::InitializeMouseSettings(PrefService* pref_service,
   NOTIMPLEMENTED();
 }
 
-// TODO(michaelcheco): Implement mouse settings updates.
 void MousePrefHandlerImpl::UpdateMouseSettings(PrefService* pref_service,
                                                const mojom::Mouse& mouse) {
-  NOTIMPLEMENTED();
+  DCHECK(mouse.settings);
+  const mojom::MouseSettings& settings = *mouse.settings;
+  // Populate `settings_dict` with all settings in `settings`.
+  base::Value::Dict settings_dict;
+  settings_dict.Set(prefs::kMouseSettingSwapRight, settings.swap_right);
+  settings_dict.Set(prefs::kMouseSettingSensitivity, settings.sensitivity);
+  settings_dict.Set(prefs::kMouseSettingReverseScrolling,
+                    settings.reverse_scrolling);
+  settings_dict.Set(prefs::kMouseSettingAccelerationEnabled,
+                    settings.acceleration_enabled);
+  settings_dict.Set(prefs::kMouseSettingScrollSensitivity,
+                    settings.scroll_sensitivity);
+  settings_dict.Set(prefs::kMouseSettingScrollAcceleration,
+                    settings.scroll_acceleration);
+
+  // Retrieve old settings and merge with the new ones.
+  base::Value::Dict devices_dict =
+      pref_service->GetDict(prefs::kMouseDeviceSettingsDictPref).Clone();
+
+  // If an old settings dict already exists for the device, merge the updated
+  // settings into the old settings. Otherwise, insert the dict at
+  // `mouse.device_key`.
+  base::Value::Dict* old_settings_dict =
+      devices_dict.FindDict(mouse.device_key);
+  if (old_settings_dict) {
+    old_settings_dict->Merge(std::move(settings_dict));
+  } else {
+    devices_dict.Set(mouse.device_key, std::move(settings_dict));
+  }
+
+  pref_service->SetDict(std::string(prefs::kMouseDeviceSettingsDictPref),
+                        std::move(devices_dict));
 }
 
 }  // namespace ash
