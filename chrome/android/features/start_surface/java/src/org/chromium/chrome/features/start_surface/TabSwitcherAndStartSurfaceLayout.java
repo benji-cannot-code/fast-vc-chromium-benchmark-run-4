d79785da76d265dc26cfc9502c2b08f2092ee4ec
Calder Kitagawa <ckitagawa@chromium.org>
2023-02-01 17:19:13 +0000

    [GTS] Fix zoom/shrink animation jumpiness
    Fix the zoom/shrink animation jumpiness by ensuring a layout occurs
    before a dynamic view bitmap is captured. This requires deferring the
    start of the animation until after the next layout pass. This also
    requires fixing a bug with StartSurface that causes the descendants of
    the SwipeRefreshLayout to be View.GONE.
    
    While an attempt was made at reducing the capture count to just the initial capture this resulted in issues with thumbnails being missing
    during the transition. Instead the existing throttle implementation is
    kept.
    
    This change also reorders bitmap deletion so that the DynamicResource is
    unregistered before dropping the bitmap to mitigate possible flickering.
    
    Tested both with and without the start surface refactoring.
    
    See additional details and captures in the bug.
    
    Bug: 1386265
    Change-Id: Ie6887e6afee381d3d29ef1726def81a64c591bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4103704
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099906}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayout.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayout.java
@@ -86,6 +86,8 @@ public class TabSwitcherAndStartSurfaceLayout extends Layout {
     // The transition animation from a tab to the tab switcher.
     private AnimatorSet mTabToSwitcherAnimation;
     private boolean mIsAnimatingHide;
+    @Nullable
+    private Runnable mDeferredAnimationRunnable;
 
     private TabListSceneLayer mSceneLayer;
     private final StartSurface mStartSurface;
@@ -272,21 +274,37 @@ public class TabSwitcherAndStartSurfaceLayout extends Layout {
         if (isShowingStartSurfaceHomepage) {
             quick = getCarouselOrSingleTabListDelegate().prepareTabSwitcherView();
         } else {
+            mStartSurface.beforeShowTabSwitcherView();
             quick = getGridTabListDelegate().prepareTabSwitcherView();
         }
 
         // Skip animation when there is no tab in current tab model or If it's showing
         // start surface, we don't show the shrink tab animatio.
         boolean isCurrentTabModelEmpty = mTabModelSelector.getCurrentModel().getCount() == 0;
-        animate = animate && !isCurrentTabModelEmpty && !isShowingStartSurfaceHomepage;
+        final boolean shouldAnimate =
+                animate && !isCurrentTabModelEmpty && !isShowingStartSurfaceHomepage;
 
         if (TabUiFeatureUtilities.isTabletGridTabSwitcherPolishEnabled(getContext())) {
-            showOverviewWithTranslateUp(animate);
+            showOverviewWithTranslateUp(shouldAnimate);
         } else {
-            showOverviewWithTabShrink(animate,
-                    ()
-                            -> getGridTabListDelegate().getThumbnailLocationOfCurrentTab(false),
-                    isShowingStartSurfaceHomepage, quick);
+            if (isShowingStartSurfaceHomepage) {
+                mStartSurface.showOverview(shouldAnimate);
+                return;
+            }
+            // Ensure the SceneLayer image for the GTS is in the correct position by deferring until
+            // the next layout pass.
+            mDeferredAnimationRunnable = () -> {
+                showOverviewWithTabShrink(shouldAnimate, () -> {
+                    return getGridTabListDelegate().getThumbnailLocationOfCurrentTab(false);
+                }, isShowingStartSurfaceHomepage, quick);
+            };
+            getGridTabListDelegate().runAnimationOnNextLayout(() -> {
+                if (mDeferredAnimationRunnable != null) {
+                    Runnable deferred = mDeferredAnimationRunnable;
+                    mDeferredAnimationRunnable = null;
+                    deferred.run();
+                }
+            });
         }
     }
 
@@ -436,6 +454,11 @@ public class TabSwitcherAndStartSurfaceLayout extends Layout {
     @Override
     protected void forceAnimationToFinish() {
         super.forceAnimationToFinish();
+        if (mDeferredAnimationRunnable != null) {
+            Runnable deferred = mDeferredAnimationRunnable;
+            mDeferredAnimationRunnable = null;
+            deferred.run();
+        }
         if (mTabToSwitcherAnimation != null) {
             if (mTabToSwitcherAnimation.isRunning()) {
                 mTabToSwitcherAnimation.end();
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
@@ -619,4 +619,8 @@ public class TabListCoordinator
     public void showPriceDropTooltip(int index) {
         mModel.get(index).model.set(TabProperties.SHOULD_SHOW_PRICE_DROP_TOOLTIP, true);
     }
+
+    void runAnimationOnNextLayout(Runnable runnable) {
+        mRecyclerView.runAnimationOnNextLayout(runnable);
+    }
 }
