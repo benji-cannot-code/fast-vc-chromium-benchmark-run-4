4b80377e3ffd96adc3a7c42a8fa5be374fa42888
lbowen <lbowen@google.com>
2023-02-01 18:54:18 +0000

    Remove the hardcoded default setting for keyboard color of Demo Mode.
    Use policy to set the default keyboard backlight color instead of the
    hardcoded value.
    
    Bug: b:243716427
    Change-Id: Ib10269762bb6c8cc7f6acb4c5484c3ba1ab9d634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4209476
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Bowen Li <lbowen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099973}

--- chrome/browser/ash/login/demo_mode/demo_session.cc
+++ chrome/browser/ash/login/demo_mode/demo_session.cc
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/locale_update_controller.h"
-#include "ash/shell.h"
-#include "ash/system/keyboard_brightness/keyboard_backlight_color_controller.h"
-#include "ash/webui/personalization_app/mojom/personalization_app.mojom.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/containers/flat_set.h"
@@ -586,13 +583,6 @@ void DemoSession::OnSessionStateChanged() {
                                                    current_locale_iso_code);
       }
       RestoreDefaultLocaleForNextSession();
-
-      if (Shell::HasInstance() &&
-          user_manager::UserManager::Get()->GetActiveUser()) {
-        Shell::Get()->keyboard_backlight_color_controller()->SetBacklightColor(
-            personalization_app::mojom::BacklightColor::kRainbow,
-            user_manager::UserManager::Get()->GetActiveUser()->GetAccountId());
-      }
 
       if (chromeos::PowerManagerClient::Get()) {
         chromeos::PowerManagerClient::Get()->GetKeyboardBrightnessPercent(
