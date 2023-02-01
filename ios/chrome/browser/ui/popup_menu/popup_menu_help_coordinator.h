16893c9be0d19bd35df268011218c18327cfee37
Robbie Gibson <rkgibson@google.com>
2023-02-01 10:55:42 +0000

    [iOS] Delay Overflow Menu IPH until feature engagement is initialized
    This promo shows on app startup, but the Feature Engagement Tracker
    needs to do a bit of initial setup itself, so only check if the promo
    can be shown after the FET has finished startup.
    
    Bug: 1344522
    Change-Id: Iab0c7dc83e8f1474cef2ac03644a8ac8e9c76d68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4208911
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099753}

--- ios/chrome/browser/ui/popup_menu/popup_menu_help_coordinator.h
+++ ios/chrome/browser/ui/popup_menu/popup_menu_help_coordinator.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // UI updater.
 @property(nonatomic, weak) id<PopupMenuUIUpdating> UIUpdater;
 
-- (void)showPopupMenuButtonIPH;
-
 - (void)showOverflowMenuIPHInViewController:(UIViewController*)menu;
 
 @end
