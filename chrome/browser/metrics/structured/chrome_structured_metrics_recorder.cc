b7a16fd6881e1513a1ecf25e48e90a508235719a
Andrew Bregger <andrewbregger@google.com>
2023-02-01 02:22:03 +0000

    [Event Sequencing] Use platform2 reset counter
    Removes a Prefs implementation to determine the reset counter with a platform2 determined value. This removes the prefs associated with the original implementation.
    
    Bug: crbug/1350322
    Change-Id: Ief9dcb8c9d871ec712bd417df5ce47e44804d7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205072
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099623}

--- chrome/browser/metrics/structured/chrome_structured_metrics_recorder.cc
+++ chrome/browser/metrics/structured/chrome_structured_metrics_recorder.cc
@@ -78,7 +78,7 @@ void ChromeStructuredMetricsRecorder::Initialize() {
   if (base::FeatureList::IsEnabled(kEventSequenceLogging)) {
     Recorder::GetInstance()->AddEventsProcessor(
         std::make_unique<cros_event::CrOSEventsProcessor>(
-            g_browser_process->local_state()));
+            cros_event::kResetCounterPath));
   }
 
   LogInitializationInStructuredMetrics(StructuredMetricsPlatform::kAshChrome);
