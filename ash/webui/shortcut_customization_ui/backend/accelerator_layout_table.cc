0cc2dad1d623d4734c0b6c58cb71577e9fbac71c
Michael Checo <michaelcheco@google.com>
2023-02-01 19:19:05 +0000

    shortcuts: Remove unused includes
    Bug: b:216049298
    Test: ash_webui_unittests AcceleratorConfigurationProviderTest
    Change-Id: I223ab521c90ed9a6f8272f00c4981e03df663111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166800
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099989}

--- ash/webui/shortcut_customization_ui/backend/accelerator_layout_table.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_layout_table.cc
@@ -6,13 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/shortcut_customization_ui/backend/accelerator_layout_table.h"
 #include <string>
 
-#include "ash/public/cpp/accelerators.h"
 #include "ash/public/cpp/accelerators_util.h"
 #include "ash/public/mojom/accelerator_info.mojom.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "base/check_op.h"
+#include "base/no_destructor.h"
 #include "base/notreached.h"
-#include "ui/base/l10n/l10n_util.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
 
