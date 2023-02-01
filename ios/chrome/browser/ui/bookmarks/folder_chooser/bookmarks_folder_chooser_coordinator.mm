5099b631bc58f24d9e9a0fdbb85001518f4d4e9c
Tanmoy Mollik <triploblastic@chromium.org>
2023-02-01 17:11:20 +0000

    [IOS][Bkmrks] Use BookmarksFolderChooserCoordinator in BookmarksFolderEditorViewController
    Bug: 1405746
    Change-Id: Ie2b9768b7bbefa21fac61fd798f4593522e40248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4206693
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099901}

--- ios/chrome/browser/ui/bookmarks/folder_chooser/bookmarks_folder_chooser_coordinator.mm
+++ ios/chrome/browser/ui/bookmarks/folder_chooser/bookmarks_folder_chooser_coordinator.mm
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // List of nodes to hide when displaying folders. This is to avoid to move a
   // folder inside a child folder.
   std::set<const bookmarks::BookmarkNode*> _hiddenNodes;
+  // The currently selected folder to show in the UI.
+  const bookmarks::BookmarkNode* _selectedFolder;
 }
 
 @end
@@ -47,11 +49,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)
     initWithNavigationController:(UINavigationController*)navigationController
                          browser:(Browser*)browser
+                  selectedFolder:(const bookmarks::BookmarkNode*)folder
                      hiddenNodes:
                          (const std::set<const bookmarks::BookmarkNode*>&)
                              hiddenNodes {
-  self = [super initWithBaseViewController:navigationController
-                                   browser:browser];
+  self = [self initWithBaseViewController:navigationController
+                                  browser:browser
+                           selectedFolder:folder];
   if (self) {
     _baseNavigationController = navigationController;
     _hiddenNodes = hiddenNodes;
@@ -60,8 +64,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                                   browser:(Browser*)browser {
-  return [super initWithBaseViewController:viewController browser:browser];
+                                   browser:(Browser*)browser
+                            selectedFolder:
+                                (const bookmarks::BookmarkNode*)folder {
+  self = [super initWithBaseViewController:viewController browser:browser];
+  if (self) {
+    _selectedFolder = folder;
+  }
+  return self;
 }
 
 - (void)start {
@@ -75,7 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
            allowsNewFolders:YES
                 editedNodes:_hiddenNodes
                allowsCancel:YES
-             selectedFolder:nil
+             selectedFolder:_selectedFolder
                     browser:self.browser];
   _folderChooserViewController.delegate = self;
 
@@ -112,6 +122,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else {
     DCHECK(!self.baseViewController.presentedViewController);
   }
+  _delegate = nil;
   _folderChooserViewController = nil;
 }
 
