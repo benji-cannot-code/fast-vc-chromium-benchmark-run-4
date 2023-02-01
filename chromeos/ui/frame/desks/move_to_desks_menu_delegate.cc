9c6e581d33daa6fad5ab4855b613106183ada6c3
Judy Wang <shidi@chromium.org>
2023-02-01 18:49:35 +0000

    FloatWindow: Disallow float and Visible to all desk to coexist.
    Temporarily disallow floated window to be assign to all desk
    and unassign window to all desk if float that window.
    
    Followup will be allow them to coexist, see b/267363112.
    
    Test: Manual test.
    Change-Id: I2613b96b8f04a5f02c993bf5cbb9239a8df2175f
    Fixed: b/265486481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4210937
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099969}

--- chromeos/ui/frame/desks/move_to_desks_menu_delegate.cc
+++ chromeos/ui/frame/desks/move_to_desks_menu_delegate.cc
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
+#include "chromeos/ui/base/window_properties.h"
+#include "chromeos/ui/base/window_state_type.h"
 #include "chromeos/ui/frame/desks/move_to_desks_menu_model.h"
 #include "chromeos/ui/wm/desks/chromeos_desks_histogram_enums.h"
 #include "chromeos/ui/wm/desks/desks_helper.h"
+#include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/views/widget/widget.h"
 
@@ -54,8 +57,13 @@ bool MoveToDesksMenuDelegate::IsCommandIdChecked(int command_id) const {
 }
 
 bool MoveToDesksMenuDelegate::IsCommandIdEnabled(int command_id) const {
-  if (IsAssignToAllDesksCommand(command_id))
-    return true;
+  if (IsAssignToAllDesksCommand(command_id)) {
+    // TODO(b/267363112): Allow a floated window to be assigned to all desks.
+    // If the window is floated, do not execute the command.
+    return widget_->GetNativeWindow()->GetProperty(
+               chromeos::kWindowStateTypeKey) !=
+           chromeos::WindowStateType::kFloated;
+  }
 
   if (!IsMoveToDeskCommand(command_id))
     return false;
