65d5f66afa1197d03d5c0487326fbbedf0c35394
Michael Thiessen <mthiesse@chromium.org>
2023-02-01 19:43:08 +0000

    Remove BlockExternalFormSubmitWithoutGesture kill switch
    This shipped in M108 and can be safely removed now.
    
    Change-Id: I45a65487c2e9b4000f9104849b0429dc9d275082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4212136
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100015}

--- components/external_intents/android/external_intents_features.h
+++ components/external_intents/android/external_intents_features.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace external_intents {
 
 // Alphabetical:
-BASE_DECLARE_FEATURE(kBlockExternalFormSubmitWithoutGesture);
 BASE_DECLARE_FEATURE(kExternalNavigationDebugLogs);
 BASE_DECLARE_FEATURE(kExternalNavigationSubframeRedirects);
 BASE_DECLARE_FEATURE(kBlockSubframeIntentToSelf);
