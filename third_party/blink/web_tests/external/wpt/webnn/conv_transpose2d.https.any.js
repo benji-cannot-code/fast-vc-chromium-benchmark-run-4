cdc6fef35ba4b5844095393c8d3011fdf6435e71
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-02-01 18:41:40 +0000

    Import wpt@2b5668ac25e1495b8c3e3cbc5d8bfa22be28e0e7
    Using wpt-import in Chromium 99fcacd3a16a091c9cb37e2030c3e704c5bf6cff.
    With Chromium commits locally applied on WPT:
    840495eeab "WPT: Rename assertBFCache to assertBFCacheEligibility"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: If06fc8b0ff33f8ed5c33979cb184dd8dce781814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4213232
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1099962}

---
+++ third_party/blink/web_tests/external/wpt/webnn/conv_transpose2d.https.any.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=test WebNN API convTranspose2d operation
+// META: global=window,dedicatedworker
+// META: script=./resources/utils.js
+// META: timeout=long
+
+'use strict';
+
+// https://webmachinelearning.github.io/webnn/#api-mlgraphbuilder-convtranspose2d
+
+const buildConvTranspose2d = (operationName, builder, resources) => {
+  // MLOperand convTranspose2d(MLOperand input, MLOperand filter, optional MLConvTranspose2dOptions options = {});
+  const namedOutputOperand = {};
+  const [inputOperand, filterOperand] = createMultiInputOperands(builder, resources);
+  let convTranspose2dOptions = {...resources.options};
+  if (convTranspose2dOptions.bias) {
+    convTranspose2dOptions.bias = createConstantOperand(builder, convTranspose2dOptions.bias);
+  }
+  if (convTranspose2dOptions.activation) {
+    convTranspose2dOptions.activation = builder[convTranspose2dOptions.activation]();
+  }
+  namedOutputOperand[resources.expected.name] = builder[operationName](inputOperand, filterOperand, convTranspose2dOptions);
+  return namedOutputOperand;
+};
+
+testWebNNOperation('convTranspose2d', buildConvTranspose2d);
