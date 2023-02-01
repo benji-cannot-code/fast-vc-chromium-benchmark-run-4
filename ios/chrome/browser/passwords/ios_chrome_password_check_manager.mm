bfcff012e1eb6f2ab5c1f32a0b56f0a8f2f2d2cf
Ernesto Izquierdo Clua <eic@google.com>
2023-02-01 16:57:00 +0000

    [ios] Start weak and reused credential checks.
    This patch adds logic to the Password Manager triggering weak and reused
    credential checks.
    
    Bug: 1406540
    Change-Id: I4f0e251f9d0e90a16e0657d3f214871760bb288c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4197671
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099888}

--- ios/chrome/browser/passwords/ios_chrome_password_check_manager.mm
+++ ios/chrome/browser/passwords/ios_chrome_password_check_manager.mm
@@ -122,6 +122,17 @@ void IOSChromePasswordCheckManager::StartPasswordCheck() {
         scoped_refptr<IOSChromePasswordCheckManager>(this));
     bulk_leak_check_service_adapter_.StartBulkLeakCheck(kPasswordCheckDataKey,
                                                         &data);
+
+    if (IsPasswordCheckupEnabled()) {
+      insecure_credentials_manager_.StartWeakCheck(base::BindOnce(
+          &IOSChromePasswordCheckManager::OnWeakOrReuseCheckFinished,
+          weak_ptr_factory_.GetWeakPtr()));
+
+      insecure_credentials_manager_.StartReuseCheck(base::BindOnce(
+          &IOSChromePasswordCheckManager::OnWeakOrReuseCheckFinished,
+          weak_ptr_factory_.GetWeakPtr()));
+    }
+
     is_check_running_ = true;
     start_time_ = base::Time::Now();
   } else {
@@ -144,8 +155,11 @@ PasswordCheckState IOSChromePasswordCheckManager::GetPasswordCheckState()
 }
 
 base::Time IOSChromePasswordCheckManager::GetLastPasswordCheckTime() const {
-  return base::Time::FromDoubleT(browser_state_->GetPrefs()->GetDouble(
-      password_manager::prefs::kLastTimePasswordCheckCompleted));
+  base::Time last_password_check =
+      base::Time::FromDoubleT(browser_state_->GetPrefs()->GetDouble(
+          password_manager::prefs::kLastTimePasswordCheckCompleted));
+
+  return std::max(last_password_check, last_completed_weak_or_reuse_check_);
 }
 
 std::vector<CredentialUIEntry>
@@ -247,6 +261,11 @@ void IOSChromePasswordCheckManager::OnCredentialDone(
   if (is_leaked) {
     insecure_credentials_manager_.SaveInsecureCredential(credential);
   }
+}
+
+void IOSChromePasswordCheckManager::OnWeakOrReuseCheckFinished() {
+  last_completed_weak_or_reuse_check_ = base::Time::Now();
+  NotifyPasswordCheckStatusChanged();
 }
 
 void IOSChromePasswordCheckManager::NotifyPasswordCheckStatusChanged() {
