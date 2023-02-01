b345c615111b029ace4142f11d86b8af79ab0687
ssid <ssid@chromium.org>
2023-02-01 02:51:38 +0000

    [segmentation] Support options for delay in periodic collection
    Adds supports for various kinds of delay settings in periodic training
    data collection.
    
    BUG=1361041
    
    Change-Id: I285b01a7e209e4ed792824f01c25737abf032d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190488
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099629}

--- components/segmentation_platform/internal/data_collection/training_data_collector_impl.h
+++ components/segmentation_platform/internal/data_collection/training_data_collector_impl.h
@@ -58,6 +58,8 @@ class TrainingDataCollectorImpl : public TrainingDataCollector,
                                 base::HistogramBase::Sample sample) override;
 
  private:
+  struct TrainingTimings;
+
   void OnGetSegmentsInfoList(DefaultModelManager::SegmentInfoList segment_list);
 
   void ReportForSegmentsInfoList(
@@ -77,6 +79,7 @@ class TrainingDataCollectorImpl : public TrainingDataCollector,
 
   void OnGetTrainingTensorsAtDecisionTime(
       TrainingDataCache::RequestId request_id,
+      const TrainingTimings& training_request,
       const proto::SegmentInfo& segment_info,
       bool has_error,
       const ModelProvider::Request& input_tensors,
@@ -102,6 +105,10 @@ class TrainingDataCollectorImpl : public TrainingDataCollector,
   // meet the collection requirement.
   bool CanReportTrainingData(const proto::SegmentInfo& segment_info,
                              bool include_output);
+
+  TrainingTimings ComputeDecisionTiming(const proto::SegmentInfo& info) const;
+  base::Time ComputeObservationTiming(const proto::SegmentInfo& info,
+                                      base::Time prediction_time) const;
 
   const raw_ptr<SegmentInfoDatabase> segment_info_database_;
   const raw_ptr<processing::FeatureListQueryProcessor>
