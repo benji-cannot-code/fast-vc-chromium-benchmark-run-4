a332a28e65df5772a6a9d8a6d275ca1ee28b8304
Pavol Marko <pmarko@google.com>
2023-02-01 00:05:49 +0000

    cert_prov: Introduce CertProvisioningWorkerDynamic
    The CertProvisioningWorkerDynamic implements the "dynamic" certificate
    provisioning flow. See go/cert-prov-api-dm-changes .
    
    Bug: b:230478084
    
    Change-Id: I25695a199fe8fa4b791879674a37e0e4a15888ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127302
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099556}

--- chrome/browser/ash/cert_provisioning/cert_provisioning_serializer.h
+++ chrome/browser/ash/cert_provisioning/cert_provisioning_serializer.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_CERT_PROVISIONING_CERT_PROVISIONING_SERIALIZER_H_
 
 #include "base/values.h"
+#include "chrome/browser/ash/cert_provisioning/cert_provisioning_common.h"
+#include "chrome/browser/ash/cert_provisioning/cert_provisioning_worker_dynamic.h"
 #include "chrome/browser/ash/cert_provisioning/cert_provisioning_worker_static.h"
 
 class PrefService;
@@ -20,18 +22,31 @@ class CertProvisioningSerializer {
   static void SerializeWorkerToPrefs(
       PrefService* pref_service,
       const CertProvisioningWorkerStatic& worker);
+  static void SerializeWorkerToPrefs(
+      PrefService* pref_service,
+      const CertProvisioningWorkerDynamic& worker);
   // Deletes saved state of the |worker| from preference storage.
   static void DeleteWorkerFromPrefs(PrefService* pref_service,
                                     const CertProvisioningWorkerStatic& worker);
+  static void DeleteWorkerFromPrefs(
+      PrefService* pref_service,
+      const CertProvisioningWorkerDynamic& worker);
   // Deserializes saved worker state |saved_worker| into a just created
   // |worker|. Consider using CertProvisioningWorkerFactory::Deserialize
   // instead of calling it directly.
   static bool DeserializeWorker(const base::Value::Dict& saved_worker,
                                 CertProvisioningWorkerStatic* worker);
+  static bool DeserializeWorker(const base::Value::Dict& saved_worker,
+                                CertProvisioningWorkerDynamic* worker);
+
+  static absl::optional<ProtocolVersion> GetProtocolVersion(
+      const base::Value::Dict& saved_worker);
 
  private:
   static base::Value::Dict SerializeWorker(
       const CertProvisioningWorkerStatic& worker);
+  static base::Value::Dict SerializeWorker(
+      const CertProvisioningWorkerDynamic& worker);
 };
 
 }  // namespace cert_provisioning
