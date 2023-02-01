19b11a750f9bcd9031780be8e9e74a90406b8c44
Nidhi Jaju <nidhijaju@chromium.org>
2023-02-01 06:11:01 +0000

    Record CacheTransparency UKMs in browser process
    This CL passes the UKM information related to Cache Transparency from
    the renderer to the browser process, so that they are logged in the same
    process as where the rest of the Page Load Metric UKMs are recorded,
    to avoid running into downsampling issues when analyzing metrics. To
    do this, SubresourceLoadMetrics is used to store the required
    information.
    
    We also add this information to DeferredFinishLoadingInfo in
    ResourceLoader to make sure we can pass the correct information to
    the browser process after all the body is received.
    
    In this CL, we:
    - deprecate the old Network.CacheTransparency UKM event
    - add a new Network.CacheTransparency2 UKM event
    - add a new page load metrics observer (PLMO) for Cache Transparency
    
    UKM collection review doc: https://docs.google.com/document/d/1ZhhDiqvSwab6SG32tEItw9CRYQBjuAGDBOCGk0MPTAU/edit?usp=sharing&resourcekey=0-Z7t00UitAV17yuCEOKDR9w
    
    Bug: 1309002
    Change-Id: Ia98cce5d7341898e923fb4e6f2a0c5643c0a63d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161203
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099680}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/page_load_metrics/browser/observers/ad_metrics/ads_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/assert_page_load_metrics_observer.h"
+#include "components/page_load_metrics/browser/observers/cache_transparency_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/page_load_metrics_embedder_base.h"
 #include "components/page_load_metrics/browser/page_load_metrics_memory_tracker.h"
 #include "components/page_load_metrics/browser/page_load_tracker.h"
@@ -194,6 +195,8 @@ void PageLoadMetricsEmbedder::RegisterEmbedderObservers(
           tracker->GetWebContents());
   if (translate_observer)
     tracker->AddObserver(std::move(translate_observer));
+  tracker->AddObserver(
+      std::make_unique<CacheTransparencyPageLoadMetricsObserver>());
 }
 
 bool PageLoadMetricsEmbedder::IsNewTabPageUrl(const GURL& url) {
