afedc863dace3a5353f60cd300451bb51f6e163b
Matt Menke <mmenke@chromium.org>
2023-02-01 21:11:02 +0000

    [FLEDGE] Move debug reporting into AuctionRunner when no winner.
    Since the InterestGroupAuctionReporter now sends debug reports when
    there is a winner, and it's managed by the InterestGroupAuction layer,
    moving debug reporting when there's no winner down into AuctionRunner
    instead of the higher level AdAuctionServiceImpl layer seems a bit
    more consistent.
    
    Bug: 1410791
    Change-Id: I48ea16f756325d8cb560f39d3402f6bca68204a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4210153
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100069}

--- content/browser/interest_group/ad_auction_service_impl.cc
+++ content/browser/interest_group/ad_auction_service_impl.cc
@@ -318,8 +318,8 @@ void AdAuctionServiceImpl::RunAdAuction(
   }
 
   std::unique_ptr<AuctionRunner> auction = AuctionRunner::CreateAndStart(
-      &auction_worklet_manager_, &GetInterestGroupManager(), config,
-      GetClientSecurityState(),
+      &auction_worklet_manager_, &GetInterestGroupManager(), config, origin(),
+      GetClientSecurityState(), GetRefCountedTrustedURLLoaderFactory(),
       base::BindRepeating(&AdAuctionServiceImpl::IsInterestGroupAPIAllowed,
                           base::Unretained(this)),
       std::move(abort_receiver),
@@ -594,7 +594,6 @@ void AdAuctionServiceImpl::OnAuctionComplete(
     absl::optional<GURL> render_url,
     std::vector<GURL> ad_component_urls,
     std::string winning_group_ad_metadata,
-    std::vector<GURL> debug_loss_report_urls,
     std::map<url::Origin,
              std::vector<auction_worklet::mojom::PrivateAggregationRequestPtr>>
         private_aggregation_requests,
@@ -648,10 +647,6 @@ void AdAuctionServiceImpl::OnAuctionComplete(
       auction_result_metrics->ReportAuctionResult(
           AdAuctionResultMetrics::AuctionResult::kFailed);
     }
-    GetInterestGroupManager().EnqueueReports(
-        InterestGroupManagerImpl::ReportType::kDebugLoss,
-        debug_loss_report_urls, origin(), *GetClientSecurityState(),
-        GetRefCountedTrustedURLLoaderFactory());
     return;
   }
 
@@ -660,8 +655,6 @@ void AdAuctionServiceImpl::OnAuctionComplete(
   DCHECK(private_aggregation_requests.empty());
   // Should always be present with a render_url.
   DCHECK(winning_group_key);
-  // Should have been passed to the reporter in this case.
-  DCHECK(debug_loss_report_urls.empty());
   DCHECK(!winning_group_ad_metadata.empty());
   DCHECK(blink::IsValidFencedFrameURL(*render_url));
   DCHECK(urn_uuid.is_valid());
@@ -695,15 +688,11 @@ void AdAuctionServiceImpl::OnAuctionComplete(
   // callback returned by the InterestGroupAuctionReporter's
   // OnNavitationToWinningAdCallback() method (invoked just above).
   reporters_.emplace_front(std::move(reporter));
-  reporters_.front()->Start(
-      origin(), GetClientSecurityState(),
-      GetRefCountedTrustedURLLoaderFactory(),
-      base::BindOnce(
-          &AdAuctionServiceImpl::OnReporterComplete, base::Unretained(this),
-          reporters_.begin(), std::move(urn_uuid),
-          std::move(*winning_group_key), std::move(winning_group_ad_metadata),
-          std::move(fenced_frame_reporter), std::move(interest_groups_that_bid),
-          std::move(k_anon_keys_to_join)));
+  reporters_.front()->Start(base::BindOnce(
+      &AdAuctionServiceImpl::OnReporterComplete, base::Unretained(this),
+      reporters_.begin(), std::move(urn_uuid), std::move(*winning_group_key),
+      std::move(winning_group_ad_metadata), std::move(fenced_frame_reporter),
+      std::move(interest_groups_that_bid), std::move(k_anon_keys_to_join)));
   if (auction_result_metrics) {
     auction_result_metrics->ReportAuctionResult(
         AdAuctionResultMetrics::AuctionResult::kSucceeded);
