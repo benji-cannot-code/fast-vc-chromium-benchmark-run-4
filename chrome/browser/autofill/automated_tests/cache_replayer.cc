e8702d20ff8810f6153c6a3d33695bab514aa742
Avi Drissman <avi@chromium.org>
2023-02-02 10:22:59 +0000

    Remove Value methods that take paths by list or span
    Value has deprecated API that find paths by std::initializer_list
    or base::span. These aren't used a lot, so rewrite all uses of
    them using modern API to allow them to be removed.
    
    This CL was uploaded by git cl split.
    
    R=kolos@chromium.org
    
    Bug: 646113
    Change-Id: Icb8610c99dcb7a43dc0f4f4ac0a622da0ba45709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4210692
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100335}

--- chrome/browser/autofill/automated_tests/cache_replayer.cc
+++ chrome/browser/autofill/automated_tests/cache_replayer.cc
@@ -33,8 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/protobuf/src/google/protobuf/repeated_field.h"
 #include "third_party/zlib/google/compression_utils.h"
 
-namespace autofill {
-namespace test {
+namespace autofill::test {
 
 using base::JSONParserOptions;
 using base::JSONReader;
@@ -437,17 +436,13 @@ ServerCacheReplayer::Status PopulateCacheFromQueryNode(
   return ServerCacheReplayer::Status{ServerCacheReplayer::StatusCode::kOk, ""};
 }
 
-// Finds the Autofill server Query node in dictionary node. Gives nullptr if
-// cannot find the node or |domain_dict| is invalid. The |domain_dict| has to
-// outlive any usage of the returned value node pointers.
+// Finds the Autofill server Query nodes in a dictionary node. The |domain| has
+// to outlive any usage of the returned value node pointers.
 std::vector<QueryNode> FindQueryNodesInDomainDict(
-    const base::Value& domain_dict,
+    const base::Value::Dict& domain,
     const std::string& url_prefix) {
-  if (!domain_dict.is_dict()) {
-    return {};
-  }
   std::vector<QueryNode> nodes;
-  for (auto pair : domain_dict.DictItems()) {
+  for (auto pair : domain) {
     if (pair.first.find(url_prefix) != std::string::npos) {
       nodes.push_back(QueryNode{GURL(pair.first), &pair.second});
     }
@@ -494,12 +489,19 @@ ServerCacheReplayer::Status PopulateCacheFromJSONFile(
   }
 
   {
-    const base::Value* domain_node =
-        root_node.FindPath({"Requests", kApiServerDomain});
-    std::vector<QueryNode> query_nodes =
-        domain_node
-            ? FindQueryNodesInDomainDict(*domain_node, kApiServerUrlGetPrefix)
-            : std::vector<QueryNode>();
+    std::vector<QueryNode> query_nodes;
+    const base::Value::Dict* root_node_dict = root_node.GetIfDict();
+    if (root_node_dict) {
+      const base::Value::Dict* requests = root_node_dict->FindDict("Requests");
+      if (requests) {
+        const base::Value::Dict* domain_node =
+            requests->FindDict(kApiServerDomain);
+        if (domain_node) {
+          query_nodes =
+              FindQueryNodesInDomainDict(*domain_node, kApiServerUrlGetPrefix);
+        }
+      }
+    }
 
     // Fill cache with the content of each Query node. There are 3 possible
     // situations: (1) there is a single Query node that contains POST requests
@@ -906,5 +908,4 @@ bool ServerUrlLoader::InterceptAutofillRequest(
   return false;
 }
 
-}  // namespace test
-}  // namespace autofill
+}  // namespace autofill::test
