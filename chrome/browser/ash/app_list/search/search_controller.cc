72c3b92bdb1d115711a828d1b4447e6639fc45db
Amanda Deacon <amandadeacon@chromium.org>
2023-02-02 03:22:04 +0000

    [Launcher FA] Partially implement and test FederatedMetricsManager.
    Note: Feature is currently behind a flag which is disabled by default.
    
    DD: go/launcher-query-fa-dd
    
    Test: As added.
    Low-Coverage-Reason: FederatedMetricsManager is incomplete.
    Bug: b/262611120
    Change-Id: I313792342464208a48ef04633ecfd405af684fb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127496
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100253}

--- chrome/browser/ash/app_list/search/search_controller.cc
+++ chrome/browser/ash/app_list/search/search_controller.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_metrics.h"
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "ash/public/cpp/tablet_mode.h"
+#include "ash/shell.h"
+#include "ash/system/federated/federated_service_controller_impl.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
