0cd55a84873cdcb1f85f3c23a5ca3209e2e0f941
Jimmy <jimmyxgong@chromium.org>
2023-02-02 00:00:27 +0000

    shortcut: fix missing param name
    Bug: b:216049298
    Test: None
    Change-Id: Ia40a1e3ed2d514bef42248f1b6a5ddfd8e13b044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4215917
    Auto-Submit: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100171}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h
@@ -89,7 +89,7 @@ class AcceleratorConfigurationProvider
           shortcut_customization::mojom::AcceleratorConfigurationProvider>
           receiver);
 
-  void InitializeNonConfigurableAccelerators(NonConfigurableActionsMap);
+  void InitializeNonConfigurableAccelerators(NonConfigurableActionsMap mapping);
 
   const NonConfigurableActionsMap& GetNonConfigurableAcceleratorsForTesting() {
     return non_configurable_actions_mapping_;
