ebeac0d816dfb37d9c85c0fa7793284fb918636d
Alison Gale <agale@chromium.org>
2023-02-02 18:39:37 +0000

    Use unplugged state to update kLastBatteryUseTimestamp
    Also add PSD to the survey configs so the surveys can be launched while
    passing that data.
    
    We want to trigger the HaTS survey for users that have been on battery
    AND unplugged in the last week. When moving around the code I lost the
    signal and was just checking whether the device has a battery and not
    whether they were unplugged.
    
    Change-Id: I304f727e457df00f8776c73c516858b704ad5cfd
    Bug: 1400430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214913
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100519}

--- components/performance_manager/public/user_tuning/prefs.h
+++ components/performance_manager/public/user_tuning/prefs.h
@@ -26,6 +26,7 @@ enum class BatterySaverModeState {
 
 extern const char kBatterySaverModeState[];
 
+// Stores the timestamp of the last battery usage while unplugged.
 extern const char kLastBatteryUseTimestamp[];
 
 // The pref storing the list of URL patterns that prevent a tab from being
