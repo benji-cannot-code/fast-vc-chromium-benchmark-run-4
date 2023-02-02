91fa34bf95eb4172582221fb570821ae83cbd801
Rebekah Potter <rbpotter@chromium.org>
2023-02-02 19:46:34 +0000

    WebUI: Update Rollup to v3.12.0
    Bug: 1412155
    Change-Id: I7862c0ada426f68ef2c524db2552664bd6245ddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214416
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100580}

--- ui/file_manager/base/gn/files_webui_bundle/rollup_plugin.mjs
+++ ui/file_manager/base/gn/files_webui_bundle/rollup_plugin.mjs
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @fileoverview Plugin for rollup to correctly resolve resources.
  */
-const path = require('path');
+import * as path from 'path';
 
 function normalizeSlashes(filepath) {
   return filepath.replace(/\\/gi, '/');
