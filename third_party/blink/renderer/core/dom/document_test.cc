c26f2fe90918862922ce6cc98eff4ff9cc897ec8
Rune Lillesveen <futhark@chromium.org>
2023-02-02 09:40:36 +0000

    Popover dialog in top layer is not modal
    Missing check for IsModal() in ActiveModalDialog()
    
    Change-Id: Idd0794f1168f84b4d7754ab50ce7d052cfeae8df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4206690
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100325}

--- third_party/blink/renderer/core/dom/document_test.cc
+++ third_party/blink/renderer/core/dom/document_test.cc
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
 #include "third_party/blink/renderer/core/html/custom/custom_element_test_helpers.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
+#include "third_party/blink/renderer/core/html/html_dialog_element.h"
 #include "third_party/blink/renderer/core/html/html_head_element.h"
 #include "third_party/blink/renderer/core/html/html_iframe_element.h"
 #include "third_party/blink/renderer/core/html/html_link_element.h"
@@ -2108,6 +2109,37 @@ TEST_F(DocumentSimTest, HeaderPreloadRemoveReaddClient) {
 
   Element* target = GetDocument().QuerySelector(".target");
   EXPECT_EQ(100, target->OffsetWidth());
+}
+
+TEST_F(DocumentTest, ActiveModalDialog) {
+  SetHtmlInnerHTML(R"HTML(
+    <dialog id="modal"></dialog>
+    <dialog popover id="popover"></dialog>
+  )HTML");
+
+  HTMLDialogElement* modal =
+      DynamicTo<HTMLDialogElement>(GetDocument().getElementById("modal"));
+  HTMLDialogElement* popover =
+      DynamicTo<HTMLDialogElement>(GetDocument().getElementById("popover"));
+
+  ASSERT_TRUE(modal);
+  ASSERT_TRUE(popover);
+
+  EXPECT_EQ(GetDocument().ActiveModalDialog(), nullptr);
+
+  NonThrowableExceptionState exception_state;
+  modal->showModal(exception_state);
+
+  EXPECT_EQ(GetDocument().ActiveModalDialog(), modal);
+  ASSERT_FALSE(GetDocument().TopLayerElements().empty());
+  EXPECT_EQ(GetDocument().TopLayerElements().back(), modal);
+
+  popover->showPopover(exception_state);
+
+  // The popover is the last of the top layer elements, but it's not modal.
+  ASSERT_FALSE(GetDocument().TopLayerElements().empty());
+  EXPECT_EQ(GetDocument().TopLayerElements().back(), popover);
+  EXPECT_EQ(GetDocument().ActiveModalDialog(), modal);
 }
 
 }  // namespace blink
