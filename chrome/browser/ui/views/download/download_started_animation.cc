ab1571b8c218d98c22e4427b4c1462c9a28f9c21
Lily Chen <chlily@chromium.org>
2023-02-02 19:14:45 +0000

    [Downloads] Refactor downloads animation code
    This refactors the current download animation (small down-arrow that
    moves towards the download shelf), in preparation for reusing the code
    for a new animation for the new download bubble UI.
    
    Bug: 1410789
    Change-Id: I54df7222b3742ebd818460e1b1f062d0b994da61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4210433
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100563}

---
+++ chrome/browser/ui/views/download/download_started_animation.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/download/download_started_animation.h"
+
+#include "chrome/browser/ui/views/download/download_shelf_started_animation_views.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+// static
+void DownloadStartedAnimation::Show(content::WebContents* web_contents) {
+  // The animation will delete itself when it's finished.
+  new DownloadShelfStartedAnimationViews(web_contents);
+}
