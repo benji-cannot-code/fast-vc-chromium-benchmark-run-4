3e79e24cd12ae362d46c0ca7be962e91183b2981
Minoru Chikamune <chikamune@chromium.org>
2023-02-02 08:18:13 +0000

    Add more trace logs
    This CL adds the following two traces for visibility.
    
    - ServiceWorker.MaybeCreateLoaderToContinueWithRegistration
    - ServiceWorkerRegistry::RunFindRegistrationCallbacks
    
    Bug: 1375174
    Change-Id: I494f18f1881e3dd139e79dbb56520e0795e79e84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4199616
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100302}

--- content/browser/service_worker/service_worker_registry.cc
+++ content/browser/service_worker/service_worker_registry.cc
@@ -1090,6 +1090,10 @@ void ServiceWorkerRegistry::RunFindRegistrationCallbacks(
     const blink::StorageKey& key,
     scoped_refptr<ServiceWorkerRegistration> registration,
     blink::ServiceWorkerStatusCode status) {
+  TRACE_EVENT2("ServiceWorker",
+               "ServiceWorkerRegistry::RunFindRegistrationCallbacks",
+               "client_url", client_url, "status",
+               blink::ServiceWorkerStatusToString(status));
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   auto iter =
       find_registration_callbacks_.find(std::make_pair(client_url, key));
