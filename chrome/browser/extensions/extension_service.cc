4e637f76204d02aaaa3c5a2e5a7ef05abd02f3ed
Justin Lulejian <jlulejian@chromium.org>
2023-02-02 22:37:57 +0000

    [Extensions] Create new on-install incremented histograms that check that the profile can use non-component extensions before emitting.
    This change is limited to installed_loader.cc (RecordExtensionsMetrics()
    and LoadAllExtensions()), but there are other stragglers in other files
    that will be addressed in a follow-up CL.
    
    This is a mix of histograms in various states, below is the state and
    the intended outcome:
    
    * >1 month expired histograms: delete the previous histogram (because
      it is no longer being recorded) and replace with the incremented
      version.
    * <1 month expired histograms: extend the previous histograms
      `expires_after` date (so we can compare data) and create the
      incremented version.
    * >1 month valid histograms: expire the previous histograms in a couple
      months (so they will be cleaned up automatically) and create the
      incremented version.
    
    The unittest tests a subset of the incremented histograms, as the
    effort/complexity in generating a test extension that exercises all the
    code lines might not have value and would make the test brittle.
    
    Bug: 1383740,1284659
    Change-Id: I205f0e5a21fbe0b71abcd049a59c67b8ca776158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4122980
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100691}

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -1078,7 +1078,8 @@ void ExtensionService::GrantPermissions(const Extension* extension) {
 // static
 void ExtensionService::RecordPermissionMessagesHistogram(
     const Extension* extension,
-    const char* histogram_basename) {
+    const char* histogram_basename,
+    bool log_user_profile_histograms) {
   PermissionIDSet permissions =
       PermissionMessageProvider::Get()->GetAllPermissionIDs(
           extension->permissions_data()->active_permissions(),
@@ -1091,6 +1092,19 @@ void ExtensionService::RecordPermissionMessagesHistogram(
       base::StringPrintf("Extensions.Permissions_%s3", histogram_basename);
   for (const PermissionID& id : permissions)
     base::UmaHistogramEnumeration(permissions_histogram_name, id.id());
+
+  if (log_user_profile_histograms) {
+    base::UmaHistogramBoolean(
+        base::StringPrintf("Extensions.HasPermissions_%s4", histogram_basename),
+        !permissions.empty());
+
+    std::string permissions_histogram_name_incremented =
+        base::StringPrintf("Extensions.Permissions_%s4", histogram_basename);
+    for (const PermissionID& id : permissions) {
+      base::UmaHistogramEnumeration(permissions_histogram_name_incremented,
+                                    id.id());
+    }
+  }
 }
 
 // TODO(michaelpg): Group with other ExtensionRegistrar::Delegate overrides
@@ -1688,7 +1702,8 @@ void ExtensionService::OnExtensionInstalled(
                               100);
     UMA_HISTOGRAM_ENUMERATION("Extensions.InstallSource",
                               extension->location());
-    RecordPermissionMessagesHistogram(extension, "Install");
+    // TODO(crbug.com/1383740): Address Install metrics below in a follow-up CL.
+    RecordPermissionMessagesHistogram(extension, "Install", false);
   }
 
   const Extension::State initial_state =
