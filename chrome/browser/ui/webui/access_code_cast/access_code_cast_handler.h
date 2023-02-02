f523f1b894a0872480c07b8116233a1e2e278dee
Amanda Deacon <amandadeacon@chromium.org>
2023-02-02 00:18:38 +0000

    Remove unneeded include.
    Bug: none
    Change-Id: I66de9d8bdf1fcf7cd23f2b2a010714e631015284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4216073
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100179}

--- chrome/browser/ui/webui/access_code_cast/access_code_cast_handler.h
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_handler.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_ACCESS_CODE_CAST_ACCESS_CODE_CAST_HANDLER_H_
 
 #include "base/scoped_observation.h"
-#include "chrome/browser/ash/app_list/search/search_controller.h"
 #include "chrome/browser/media/router/discovery/access_code/access_code_cast_sink_service.h"
 #include "chrome/browser/media/router/discovery/access_code/discovery_resources.pb.h"
 #include "chrome/browser/media/router/discovery/mdns/cast_media_sink_service_impl.h"
