afa25488b9826e8657d1447b0ada4d41ab3d0c8e
Lei Zhang <thestig@chromium.org>
2023-02-02 18:19:57 +0000

    Get rid of PrintCompositor::SetDiscardableSharedMemoryManager() API.
    The discardable shared memory manager is available to all child
    processes, [1] so PrintCompositor can just request it. That is one fewer
    Mojom API vs. injecting it from the browser. [2]
    
    [1] https://crrev.com/469593
    [2] https://crrev.com/686061
    
    Change-Id: I53c54bc1925b52f3812646ebab0b345dc67455c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214879
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100508}

--- components/services/print_compositor/print_compositor_impl.cc
+++ components/services/print_compositor/print_compositor_impl.cc
@@ -39,6 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/platform.h"
 #endif
 
+using MojoDiscardableSharedMemoryManager =
+    discardable_memory::mojom::DiscardableSharedMemoryManager;
+
 namespace printing {
 
 PrintCompositorImpl::PrintCompositorImpl(
@@ -46,8 +49,19 @@ PrintCompositorImpl::PrintCompositorImpl(
     bool initialize_environment,
     scoped_refptr<base::SingleThreadTaskRunner> io_task_runner)
     : io_task_runner_(std::move(io_task_runner)) {
-  if (receiver)
+  if (receiver) {
     receiver_.Bind(std::move(receiver));
+
+    mojo::PendingRemote<MojoDiscardableSharedMemoryManager> manager_remote;
+    content::ChildThread::Get()->BindHostReceiver(
+        manager_remote.InitWithNewPipeAndPassReceiver());
+    DCHECK(io_task_runner_);
+    discardable_shared_memory_manager_ = base::MakeRefCounted<
+        discardable_memory::ClientDiscardableSharedMemoryManager>(
+        std::move(manager_remote), io_task_runner_);
+    base::DiscardableMemoryAllocator::SetInstance(
+        discardable_shared_memory_manager_.get());
+  }
 
   if (!initialize_environment)
     return;
@@ -82,19 +96,6 @@ PrintCompositorImpl::~PrintCompositorImpl() {
 #if BUILDFLAG(IS_WIN)
   content::UninitializeDWriteFontProxy();
 #endif
-}
-
-void PrintCompositorImpl::SetDiscardableSharedMemoryManager(
-    mojo::PendingRemote<
-        discardable_memory::mojom::DiscardableSharedMemoryManager> manager) {
-  // Set up discardable memory manager.
-  mojo::PendingRemote<discardable_memory::mojom::DiscardableSharedMemoryManager>
-      manager_remote(std::move(manager));
-  discardable_shared_memory_manager_ = base::MakeRefCounted<
-      discardable_memory::ClientDiscardableSharedMemoryManager>(
-      std::move(manager_remote), io_task_runner_);
-  base::DiscardableMemoryAllocator::SetInstance(
-      discardable_shared_memory_manager_.get());
 }
 
 void PrintCompositorImpl::NotifyUnavailableSubframe(uint64_t frame_guid) {
