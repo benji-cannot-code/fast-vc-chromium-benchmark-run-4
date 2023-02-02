af8f9ef4a4354fa4b722fb89d830cb39962a6f29
Nick Burris <nburris@chromium.org>
2023-02-02 01:18:55 +0000

    Move static webauth security checker methods to content/public
    We previously partially duplicated some logic in
    SecurePaymentConfirmationAppFactory since we could not directly use the
    static method OriginIsAllowedToClaimRelyingPartyId. This patch refactors
    the static methods from webauth_request_security_checker to
    content/public/browser/ so that it can be imported and used in the
    payments component. The WebAuthRequestSecurityChecker class remains
    entirely in content/browser/webauth as it's used by RenderFrameHostImpl.
    
    See bug for more historical context.
    
    Bug: 1365347
    Change-Id: I53ee8034aab9c9074947cc89c59f9d3a9a719955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198519
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100216}

--- components/payments/content/secure_payment_confirmation_app_factory.cc
+++ components/payments/content/secure_payment_confirmation_app_factory.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/webdata/common/web_data_service_base.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/browser/webauthn_security_utils.h"
 #include "content/public/common/content_features.h"
 #include "services/data_decoder/public/cpp/decode_image.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -129,15 +130,10 @@ bool IsValid(const mojom::SecurePaymentConfirmationRequestPtr& request,
 // Determine if a given origin that is calling SPC with a given RP ID requires
 // the credentials to be third-party enabled (i.e., the calling party is not the
 // RP ID).
-//
-// TODO(crbug.com/1365347): Use OriginIsAllowedToClaimRelyingPartyId instead.
 bool RequiresThirdPartyPaymentBit(const url::Origin& caller_origin,
                                   const std::string& relying_party_id) {
-  // An origin may utilize credentials (without the third-party payment bit) for
-  // either its own exact domain (e.g. 'www.site.example' on www.site.example),
-  // or if it is in the same domain (e.g., 'site.example' on www.site.example).
-  return caller_origin.host() != relying_party_id &&
-         !caller_origin.DomainIs(relying_party_id);
+  return !content::OriginIsAllowedToClaimRelyingPartyId(relying_party_id,
+                                                        caller_origin);
 }
 
 }  // namespace
