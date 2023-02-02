976b5ea2dda6a1707d3cbeb63762f99fec217b3d
Sijin Soon <sijins@google.com>
2023-02-02 02:49:08 +0000

    Implement infinite scroll for GIF search results.
    Scrolling preview: https://drive.google.com/file/d/1S0CivOg0154gK1P2QDhZJQsy4BX5TgOz/view?usp=sharing
    
    Bug: b:267110764
    Change-Id: I149000a55b25da5f69cedf9960f6f356d242cf24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4202275
    Commit-Queue: Sijin Soon <sijins@google.com>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100241}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_search.ts
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_search.ts
@@ -41,6 +41,8 @@ export class EmojiSearch extends PolymerElement {
       searchResults: {type: Array},
       needIndexing: {type: Boolean, value: false},
       gifSupport: {type: Boolean, value: false},
+      searchQuery: {type: String, value: ''},
+      nextGifPos: {type: String, value: ''},
     };
   }
   categoriesData: EmojiGroupData;
@@ -60,6 +62,9 @@ export class EmojiSearch extends PolymerElement {
         ],
   };
   private fuseInstances = new Map<CategoryEnum, Fuse<EmojiVariants>>();
+  private nextGifPos: string;  // This variable ensures that we get the correct
+                               // set of GIFs when fetching more.
+  private scrollTimeout: number|null;
 
   static get observers() {
     return [
@@ -80,7 +85,7 @@ export class EmojiSearch extends PolymerElement {
   private onSearch(newSearch: string): void {
     this.set('searchResults', this.computeLocalSearchResults(newSearch));
     if (this.gifSupport) {
-      this.computeGifSearchResults(newSearch).then((searchResults) => {
+      this.computeInitialGifSearchResults(newSearch).then((searchResults) => {
         this.push('searchResults', ...searchResults);
       });
     }
@@ -244,7 +249,54 @@ export class EmojiSearch extends PolymerElement {
     return searchResults;
   }
 
-  private async computeGifSearchResults(search: string):
+  private onSearchScroll(): void {
+    if (this.gifSupport) {
+      if (this.scrollTimeout) {
+        clearTimeout(this.scrollTimeout);
+      }
+      this.scrollTimeout = setTimeout(() => {
+        this.checkScrollPosition();
+      }, 100);
+    }
+  }
+
+  /**
+   * Checks the current scroll position and decides if new GIF elements need to
+   * be fetched and displayed.
+   */
+  private checkScrollPosition(): void {
+    const thisRect = this.shadowRoot?.getElementById('results');
+    const searchResultRect = this.shadowRoot?.getElementById('search-results');
+
+    if (!thisRect || !searchResultRect) {
+      return;
+    }
+
+    // No need to append more GIFs if the first set of GIFs is still rendering.
+    if (searchResultRect.getBoundingClientRect().height <=
+        thisRect.getBoundingClientRect().height) {
+      return;
+    }
+
+    // Append more GIFs to show if user is near the bottom of the currently
+    // rendered GIFs (300px is around the average height of 2 GIFs).
+    if (searchResultRect!.getBoundingClientRect().bottom -
+            thisRect!.getBoundingClientRect().bottom <=
+        300) {
+      const gifIndex = this.searchResults.findIndex(
+          group => group.category === CategoryEnum.GIF);
+      if (gifIndex === -1) {
+        return;
+      }
+
+      this.computeFollowingGifSearchResults(this.$.search.getValue())
+          .then((searchResults) => {
+            this.push(['searchResults', gifIndex, 'emoji'], ...searchResults);
+          });
+    }
+  }
+
+  private async computeInitialGifSearchResults(search: string):
       Promise<EmojiGroupData> {
     if (!search) {
       return [];
@@ -253,6 +305,7 @@ export class EmojiSearch extends PolymerElement {
     const searchResults: EmojiGroupData = [];
     const apiProxy = EmojiPickerApiProxyImpl.getInstance();
     const {searchGifs} = await apiProxy.searchGifs(search);
+    this.nextGifPos = searchGifs.next;
     searchResults.push({
       'category': CategoryEnum.GIF,
       'group': '',
@@ -260,6 +313,18 @@ export class EmojiSearch extends PolymerElement {
       'searchOnly': false,
     });
     return searchResults;
+  }
+
+  private async computeFollowingGifSearchResults(search: string):
+      Promise<EmojiVariants[]> {
+    if (!search) {
+      return [];
+    }
+
+    const apiProxy = EmojiPickerApiProxyImpl.getInstance();
+    const {searchGifs} = await apiProxy.searchGifs(search, this.nextGifPos);
+    this.nextGifPos = searchGifs.next;
+    return apiProxy.convertTenorGifsToEmoji(searchGifs);
   }
 
   private onResultClick(ev: MouseEvent): void {
