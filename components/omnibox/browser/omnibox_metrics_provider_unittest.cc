3f78cd72abac173e67aeaf033eb28283b864825b
Moe Ahmadi <mahmadi@chromium.org>
2023-02-02 01:50:41 +0000

    [omnibox][ml] Creates a utility MLConfig struct to hold feature states
    This struct replaces the current method of caching the feature values
    via static local variables. It provides the same efficiency with the
    additional flexibility in (re)setting the feature values in tests.
    
    Bug: 1405555
    Change-Id: I3aea88b20e4946560fc4504e3beb09f2a82ccb8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214502
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100226}

--- components/omnibox/browser/omnibox_metrics_provider_unittest.cc
+++ components/omnibox/browser/omnibox_metrics_provider_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/omnibox/browser/autocomplete_result.h"
 #include "components/omnibox/browser/match_compare.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_log.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -146,8 +147,8 @@ TEST_F(OmniboxMetricsProviderTest, ClientSummarizedResultTypeMultipleSearch) {
 #if !(BUILDFLAG(IS_IOS) || BUILDFLAG(IS_ANDROID))
 TEST_F(OmniboxMetricsProviderTest, LogScoringSignals) {
   // Enable feature flag to log scoring signals.
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(omnibox::kLogUrlScoringSignals);
+  OmniboxFieldTrial::ScopedMLConfigForTesting scoped_ml_config;
+  scoped_ml_config.GetMLConfig().log_url_scoring_signals = true;
 
   // Populate a set of scoring signals with some test values. This will be used
   // to ensure the scoring signals are being propagated correctly.
