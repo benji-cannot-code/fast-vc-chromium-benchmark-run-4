e481306e3a70ed29ebeadeb13a30dbe9a86126cd
Gang Wu <gangwu@chromium.org>
2023-02-02 16:54:39 +0000

    [Omnibox] Enable the Omnibox Pedals Render tests
    From the test revivier, it looks the tests are stable now.
    http://shortn/_uBp3M14I4J
    http://shortn/_NRlnXxokaD
    
    
    Fixed: 1348691,1313949
    Change-Id: I81b708d9737fa3be0922d2fdc9661d44c08d6e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214911
    Auto-Submit: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100468}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
@@ -20,7 +20,6 @@ import org.chromium.base.test.params.ParameterAnnotations;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.night_mode.ChromeNightModeTestUtils;
@@ -121,7 +120,6 @@ public class OmniboxPedalsRenderTest {
     @Test
     @MediumTest
     @Feature("RenderTest")
-    @DisabledTest(message = "crbug.com/1348691")
     public void testRunChromeSafetyCheckPedal() throws IOException, InterruptedException {
         List<AutocompleteMatch> suggestionsList = new ArrayList<>();
         suggestionsList.add(
@@ -138,7 +136,6 @@ public class OmniboxPedalsRenderTest {
     @Test
     @MediumTest
     @Feature("RenderTest")
-    @DisabledTest(message = "crbug.com/1348691")
     public void testPlayChromeDinoGamePedal() throws IOException, InterruptedException {
         List<AutocompleteMatch> suggestionsList = new ArrayList<>();
         suggestionsList.add(
