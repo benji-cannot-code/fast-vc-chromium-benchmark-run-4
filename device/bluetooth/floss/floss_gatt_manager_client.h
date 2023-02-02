3552966b408ead32583594dd49fb8b693cdd439b
Sarvesh <sarveshkalwit@google.com>
2023-02-02 19:38:01 +0000

    Floss: Support Add/Remove/Clear Service(s) Gatt Server
    This change adds functions for a Floss Gatt Server to add, remove, and
    clear Gatt services.
    
    BUG=b:193686557
    TEST=Successful compilation of Floss unit tests
    
    Change-Id: I48f18028d038c989d3e4f0794045fdd45c318ebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4215192
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Sarvesh Kalwit <sarveshkalwit@google.com>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100576}

--- device/bluetooth/floss/floss_gatt_manager_client.h
+++ device/bluetooth/floss/floss_gatt_manager_client.h
@@ -240,6 +240,9 @@ class DEVICE_BLUETOOTH_EXPORT FlossGattServerObserver
                                          int32_t server_id,
                                          bool connected,
                                          std::string address) {}
+
+  // A Gatt service has been added to the server.
+  virtual void GattServerServiceAdded(GattStatus status, GattService service) {}
 };
 
 class DEVICE_BLUETOOTH_EXPORT FlossGattManagerClient
@@ -361,6 +364,15 @@ class DEVICE_BLUETOOTH_EXPORT FlossGattManagerClient
   virtual void ServerDisconnect(ResponseCallback<Void> callback,
                                 const std::string& remote_device);
 
+  // Add a Gatt service.
+  virtual void AddService(ResponseCallback<Void> callback, GattService service);
+
+  // Remove Gatt service.
+  virtual void RemoveService(ResponseCallback<Void> callback, int32_t handle);
+
+  // Clear all Gatt services.
+  virtual void ClearServices(ResponseCallback<Void> callback);
+
   // Initialize the gatt client for the given adapter.
   void Init(dbus::Bus* bus,
             const std::string& service_name,
@@ -426,6 +438,7 @@ class DEVICE_BLUETOOTH_EXPORT FlossGattManagerClient
                                  int32_t server_id,
                                  bool connected,
                                  std::string address) override;
+  void GattServerServiceAdded(GattStatus status, GattService service) override;
 
   // Managed by FlossDBusManager - we keep local pointer to access object proxy.
   base::raw_ptr<dbus::Bus> bus_ = nullptr;
