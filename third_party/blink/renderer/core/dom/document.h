1087d79709bfa89c9fa103020c294d2c4a6e66b9
Jeremy Roman <jbroman@chromium.org>
2023-02-02 08:25:59 +0000

    Make Document::node_count_ itself DCHECK-only.
    Trivial followup from
      https://chromium-review.googlesource.com/c/chromium/src/+/4209074
    
    Bug: 808503
    Change-Id: Ia0db8371ad49bef2ee1ee66034721acf62310d95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214724
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100304}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1637,7 +1637,6 @@ class CORE_EXPORT Document : public ContainerNode,
     DCHECK_GT(node_count_, 0);
     node_count_--;
   }
-  int NodeCount() const { return node_count_; }
 #endif  // DCHECK_IS_ON()
 
   SnapCoordinator& GetSnapCoordinator();
@@ -2457,7 +2456,9 @@ class CORE_EXPORT Document : public ContainerNode,
 
   Member<IntersectionObserverController> intersection_observer_controller_;
 
-  int node_count_;
+#if DCHECK_IS_ON()
+  int node_count_ = 0;
+#endif
 
   Member<SnapCoordinator> snap_coordinator_;
 
