d3f04a02526241c91fb7a730816a0fe5f3abcaf3
Jonathan Lee <jonathanjlee@google.com>
2023-02-02 18:36:11 +0000

    [blinkpy] Improve `WebTestResults` API ergonomics
    * Replace the callback-based `for_each_test` by making the container
      iterable, which is more consistent with the imperative style.
    * `actual_results` returns the space-delimited statuses pre-split, which
      is what most callers want anyway.
    
    Bug: 1149035
    Change-Id: I8bf89d93387a97ad31a1b45c5cfca848e645050b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4217997
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100518}

--- third_party/blink/tools/blinkpy/common/net/web_test_results.py
+++ third_party/blink/tools/blinkpy/common/net/web_test_results.py
@@ -89,13 +89,13 @@ class WebTestResult:
         return self._result_dict.get('is_missing_audio', False)
 
     def actual_results(self):
-        return self._result_dict['actual']
+        return self._result_dict['actual'].split()
 
     def expected_results(self):
         return self._result_dict['expected']
 
     def last_retry_result(self):
-        return self.actual_results().split()[-1]
+        return self.actual_results()[-1]
 
     def has_non_reftest_mismatch(self):
         """Returns true if a test without reference failed due to mismatch.
@@ -103,7 +103,7 @@ class WebTestResult:
         This happens when the actual output of a non-reftest does not match the
         baseline, including an implicit all-PASS testharness baseline (i.e. a
         previously all-PASS testharness test starts to fail)."""
-        actual_results = self.actual_results().split(' ')
+        actual_results = self.actual_results()
         artifact_names = self._result_dict.get('artifacts', {}).keys()
         return ('FAIL' in actual_results and any(
             artifact_name.startswith('actual')
@@ -201,6 +201,9 @@ class WebTestResults:
         self.interrupted = interrupted
         self.builder_name = builder_name
 
+    def __iter__(self):
+        yield from self._results_by_name.values()
+
     def step_name(self):
         return self._step_name
 
@@ -215,10 +218,6 @@ class WebTestResults:
 
     def result_for_test(self, test):
         return self._results_by_name.get(test)
-
-    def for_each_test(self, handler):
-        for result in self._results_by_name.values():
-            handler(result)
 
     def didnt_run_as_expected_results(self):
         return [
