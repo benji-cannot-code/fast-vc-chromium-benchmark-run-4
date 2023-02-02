5bdf486066e77095a66acb73891f7144d81e10f6
Sean Kau <skau@chromium.org>
2023-02-02 23:56:13 +0000

    Add a feature flag to switch between static and variable fonts
    Roll out a new font format in the rootfs for Google Sans. At browser
    startup, choose the desired path and load it into fontconfig for Chrome.
    Once stability of the variable font is confirmed, runtime font selection
    will be removed.
    
    Bug: chromium:1348674
    Change-Id: I068ee4aff378a1a2d714cd63db40c467da22f01d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074784
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Quick-Run: Sean Kau <skau@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100727}

--- ui/gfx/switches.h
+++ ui/gfx/switches.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "ui/gfx/switches_export.h"
 
 namespace switches {
@@ -27,9 +28,16 @@ GFX_SWITCHES_EXPORT extern const char kNoXshm[];
 }  // namespace switches
 
 namespace features {
+
 GFX_SWITCHES_EXPORT BASE_DECLARE_FEATURE(kOddHeightMultiPlanarBuffers);
 GFX_SWITCHES_EXPORT BASE_DECLARE_FEATURE(kOddWidthMultiPlanarBuffers);
 GFX_SWITCHES_EXPORT BASE_DECLARE_FEATURE(kPPAPISharedImagesSwapChain);
+
+#if BUILDFLAG(IS_CHROMEOS)
+GFX_SWITCHES_EXPORT BASE_DECLARE_FEATURE(kVariableGoogleSansFont);
+GFX_SWITCHES_EXPORT bool UseVariableGoogleSansFont();
+#endif
+
 }  // namespace features
 
 #endif  // UI_GFX_SWITCHES_H_
