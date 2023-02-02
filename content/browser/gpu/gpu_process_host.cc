ac1bfaccb6b6c4327f7e64640b0e74e84721f70d
Patrick Monette <pmonette@chromium.org>
2023-02-02 00:36:49 +0000

    [MacOS] New feature to adjust the backgrounded state of the GPU process
    The new feature (named "AdjustGpuProcessPriority") ensures the
    backgrounded state of the GPU process mirrors the backgrounded state
    of the browser process.
    
    Bug: 1400472
    Change-Id: I284c21ef27536abe3548aa1338e1464e25ddde89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085787
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100196}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -113,6 +113,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_MAC)
+#include "content/browser/gpu/browser_child_process_backgrounded_bridge.h"
 #include "content/browser/gpu/ca_transaction_gpu_coordinator.h"
 #endif
 
@@ -940,6 +941,14 @@ void GpuProcessHost::OnProcessLaunched() {
     process_id_ = process_->GetProcess().Pid();
     DCHECK_NE(base::kNullProcessId, process_id_);
     gpu_host_->SetProcessId(process_id_);
+
+#if BUILDFLAG(IS_MAC)
+    if (base::FeatureList::IsEnabled(features::kAdjustGpuProcessPriority)) {
+      browser_child_process_backgrounded_bridge_ =
+          std::make_unique<BrowserChildProcessBackgroundedBridge>(
+              process_.get());
+    }
+#endif
   }
 }
 
