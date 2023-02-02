275efbadcd76b5e2ea5095971a3b99203e346086
Ashley Prasad <ashleydp@google.com>
2023-02-02 19:06:43 +0000

    audio-settings: add noise_cancellation_state to mojom
    - Replaces fake `noise_cancellation_state` and AudioEffectState enum
      with actual mojom implementation.
    - Adds helper to setting `noise_cancellation_state` based on provided
      audio device.
    - Test audio device state reflects device and audio device state.
    - Add TODO to update helper after noise cancellation refactor in
      CrasAudioHandler.
    
    Bug: b:260277007
    Test: chromeos_unittests --gtest_filter=*CrosAudioConfigImpl*
    Test: browser_tests --gtest_filter=*FakeCrosAudioConfig*
    Change-Id: I048a0c5baa19996436ad9cbd8f780db2a95e0a0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193250
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100552}

--- chrome/browser/resources/settings/chromeos/device_page/fake_cros_audio_config.ts
+++ chrome/browser/resources/settings/chromeos/device_page/fake_cros_audio_config.ts
@@ -10,30 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from 'chrome://resources/js/assert_ts.js';
 
-import {AudioDevice as AudioDeviceMojom, AudioDeviceType, AudioSystemProperties as AudioSystemPropertiesMojom, CrosAudioConfigInterface, MuteState} from '../../mojom-webui/audio/cros_audio_config.mojom-webui.js';
-
-// TODO(b/260277007): Remove enum and update imports when mojo updated with
-// AudioEffectState.
-export enum AudioEffectState {
-  NOT_SUPPORTED,
-  NOT_ENABLED,
-  ENABLED,
-}
-
-// TODO(b/260277007): Remove type alias and unused types when mojo updated to
-// handle noise cancellation.
-export interface FakeAudioDevice extends AudioDeviceMojom {
-  noiseCancellationState: AudioEffectState;
-}
-
-export type AudioDevice = AudioDeviceMojom&Partial<FakeAudioDevice>;
+import {AudioDevice, AudioDeviceType, AudioEffectState, AudioSystemProperties, CrosAudioConfigInterface, MuteState} from '../../mojom-webui/audio/cros_audio_config.mojom-webui.js';
 
 export const defaultFakeMicJack: AudioDevice = {
   id: BigInt(1),
   displayName: 'Mic Jack',
   isActive: true,
   deviceType: AudioDeviceType.kInternalMic,
-  noiseCancellationState: AudioEffectState.NOT_SUPPORTED,
+  noiseCancellationState: AudioEffectState.kNotSupported,
 };
 
 export const fakeSpeakerActive: AudioDevice = {
@@ -41,7 +25,7 @@ export const fakeSpeakerActive: AudioDevice = {
   displayName: 'Speaker',
   isActive: true,
   deviceType: AudioDeviceType.kInternalSpeaker,
-  noiseCancellationState: AudioEffectState.NOT_SUPPORTED,
+  noiseCancellationState: AudioEffectState.kNotSupported,
 };
 
 export const fakeMicJackInactive: AudioDevice = {
@@ -49,7 +33,7 @@ export const fakeMicJackInactive: AudioDevice = {
   displayName: 'Mic Jack',
   isActive: false,
   deviceType: AudioDeviceType.kInternalSpeaker,
-  noiseCancellationState: AudioEffectState.NOT_SUPPORTED,
+  noiseCancellationState: AudioEffectState.kNotSupported,
 };
 
 export const defaultFakeSpeaker: AudioDevice = {
@@ -57,7 +41,7 @@ export const defaultFakeSpeaker: AudioDevice = {
   displayName: 'Speaker',
   isActive: false,
   deviceType: AudioDeviceType.kInternalSpeaker,
-  noiseCancellationState: AudioEffectState.NOT_SUPPORTED,
+  noiseCancellationState: AudioEffectState.kNotSupported,
 };
 
 export const fakeInternalFrontMic: AudioDevice = {
@@ -65,7 +49,7 @@ export const fakeInternalFrontMic: AudioDevice = {
   displayName: 'FrontMic',
   isActive: true,
   deviceType: AudioDeviceType.kFrontMic,
-  noiseCancellationState: AudioEffectState.NOT_ENABLED,
+  noiseCancellationState: AudioEffectState.kNotEnabled,
 };
 
 export const fakeBluetoothMic: AudioDevice = {
@@ -73,7 +57,7 @@ export const fakeBluetoothMic: AudioDevice = {
   displayName: 'Bluetooth Mic',
   isActive: false,
   deviceType: AudioDeviceType.kBluetoothNbMic,
-  noiseCancellationState: AudioEffectState.NOT_SUPPORTED,
+  noiseCancellationState: AudioEffectState.kNotSupported,
 };
 
 export const fakeInternalMicActive: AudioDevice = {
@@ -81,17 +65,8 @@ export const fakeInternalMicActive: AudioDevice = {
   displayName: 'Internal Mic',
   isActive: true,
   deviceType: AudioDeviceType.kInternalMic,
-  noiseCancellationState: AudioEffectState.NOT_SUPPORTED,
+  noiseCancellationState: AudioEffectState.kNotSupported,
 };
-
-// TODO(b/260277007): Remove type alias and unused types when mojo updated to
-// handle audio input.
-export interface FakeAudioSystemProperties extends AudioSystemPropertiesMojom {
-  inputDevices: AudioDevice[];
-}
-
-export type AudioSystemProperties =
-    AudioSystemPropertiesMojom&Partial<FakeAudioSystemProperties>;
 
 export interface FakePropertiesObserverInterface {
   onPropertiesUpdated(properties: AudioSystemProperties): void;
@@ -171,12 +146,12 @@ export class FakeCrosAudioConfig implements FakeCrosAudioConfigInterface {
 
     const activeIndex = this.audioSystemProperties.inputDevices.findIndex(
         (device: AudioDevice) => device.isActive &&
-            device.noiseCancellationState !== AudioEffectState.NOT_SUPPORTED);
+            device.noiseCancellationState !== AudioEffectState.kNotSupported);
     if (activeIndex === -1) {
       return;
     }
     const nextState: AudioEffectState =
-        enabled ? AudioEffectState.ENABLED : AudioEffectState.NOT_ENABLED;
+        enabled ? AudioEffectState.kEnabled : AudioEffectState.kNotEnabled;
     this.audioSystemProperties.inputDevices[activeIndex]!
         .noiseCancellationState = nextState;
     this.notifyAudioSystemPropertiesUpdated();
--- chrome/test/data/webui/settings/chromeos/fake_cros_audio_config_test.js
+++ chrome/test/data/webui/settings/chromeos/fake_cros_audio_config_test.js
@@ -188,7 +188,7 @@ suite('FakeCrosAudioConfig', function() {
         const ncSupportedNotEnabled = fakeCrosAudioConfig.createAudioDevice(
             fakeCrosAudioConfig.fakeInternalFrontMic, /*isActive=*/ true);
         ncSupportedNotEnabled.noiseCancellationState =
-            fakeCrosAudioConfig.AudioEffectState.NOT_ENABLED;
+            crosAudioConfigMojomWebui.AudioEffectState.kNotEnabled;
 
         /** @type {AudioSystemProperties} */
         const defaultNoiseCancellation = {
@@ -204,7 +204,7 @@ suite('FakeCrosAudioConfig', function() {
         const ncSupportedEnabled = fakeCrosAudioConfig.createAudioDevice(
             fakeCrosAudioConfig.fakeInternalFrontMic, /*isActive=*/ true);
         ncSupportedEnabled.noiseCancellationState =
-            fakeCrosAudioConfig.AudioEffectState.ENABLED;
+            crosAudioConfigMojomWebui.AudioEffectState.kEnabled;
 
         /** @type {AudioSystemProperties} */
         const enabledNoiseCancellation = {
@@ -225,7 +225,7 @@ suite('FakeCrosAudioConfig', function() {
         const ncNotSupported = fakeCrosAudioConfig.createAudioDevice(
             fakeCrosAudioConfig.fakeInternalFrontMic, /*isActive=*/ true);
         ncNotSupported.noiseCancellationState =
-            fakeCrosAudioConfig.AudioEffectState.NOT_SUPPORTED;
+            crosAudioConfigMojomWebui.AudioEffectState.kNotSupported;
 
         /** @type {AudioSystemProperties} */
         const noNoiseCancellation = {
