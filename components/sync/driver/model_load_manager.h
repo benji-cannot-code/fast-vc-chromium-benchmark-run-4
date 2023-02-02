9c8b3634ae0eb49bffbbff59dca0b34f3e354559
Ankush Singh <ankushkush@google.com>
2023-02-02 11:50:22 +0000

    [sync] ModelLoadManager: Add a timer to track loading datatypes
    Currently, if a single datatype is stuck at loading, i.e. controller is
    in MODEL_STARTING state and never goes to MODEL_LOADED nor FAILED state,
    then Sync as a whole fails to start up, as
    ModelLoadManager::NotifyDelegateIfReadyForConfigure is never triggered.
    This CL fixes this by adding a timeout for loading datatypes. All types
    which fail to load by this time are treated as having errors.
    
    Bug: 992340
    Change-Id: I24004d171ae497f21125574ecebcf90ce93403c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166150
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100358}

--- components/sync/driver/model_load_manager.h
+++ components/sync/driver/model_load_manager.h
@@ -6,11 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SYNC_DRIVER_MODEL_LOAD_MANAGER_H_
 #define COMPONENTS_SYNC_DRIVER_MODEL_LOAD_MANAGER_H_
 
+#include <memory>
+
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "components/sync/driver/configure_context.h"
 #include "components/sync/driver/data_type_controller.h"
 #include "components/sync/engine/shutdown_reason.h"
+
+namespace base {
+class ElapsedTimer;
+}  // namespace base
 
 namespace syncer {
 
@@ -90,6 +98,10 @@ class ModelLoadManager {
   // Initialize().
   void NotifyDelegateIfReadyForConfigure();
 
+  // Called by `load_models_timeout_timer_`. Issues stop signal (with
+  // error) to controllers for all types which have not started till now.
+  void OnLoadModelsTimeout();
+
   // Set of all registered controllers.
   const raw_ptr<const DataTypeController::TypeMap> controllers_;
 
@@ -103,6 +115,14 @@ class ModelLoadManager {
 
   // Data types that are loaded.
   ModelTypeSet loaded_types_;
+
+  // Timer to track LoadDesiredTypes() timeout. All types not loaded by now are
+  // treated as having errors.
+  base::OneShotTimer load_models_timeout_timer_;
+
+  // Timer to measure time by which all types have finished loading (or timed
+  // out).
+  std::unique_ptr<base::ElapsedTimer> load_models_elapsed_timer_;
 
   bool notified_about_ready_for_configure_ = false;
 
