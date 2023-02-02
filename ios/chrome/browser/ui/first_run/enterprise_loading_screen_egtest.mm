b025180f0156830e4572448a645fe0fc6a17b541
Christian Xu <christianxu@google.com>
2023-02-02 16:52:37 +0000

    [iOS] Enterprise policy test are flaky
    Set tests to flaky due to consistent failure of
    [PolicyAppInterface clearDMTokenDirectory].
    
    Bug: 1412454
    Change-Id: If93803f2095785040f71d04cf76d7bde728c1b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218474
    Owners-Override: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100466}

--- ios/chrome/browser/ui/first_run/enterprise_loading_screen_egtest.mm
+++ ios/chrome/browser/ui/first_run/enterprise_loading_screen_egtest.mm
@@ -121,7 +121,8 @@ constexpr char kEnrollmentToken[] = "enrollment_token";
 
 // Ensures that the loading screen is dismissed when registration and policy
 // fetch succeed.
-- (void)testLoadingScreenDismissedByPolicyFetchSuccess {
+// TODO(crbug.com/1412454): Flaky.
+- (void)FLAKY_testLoadingScreenDismissedByPolicyFetchSuccess {
   [self configureAppWithEnrollmentTokenValid:YES];
   [self verifyLoadingScreenIsDismissed];
   GREYAssertTrue([PolicyAppInterface isCloudPolicyClientRegistered],
@@ -129,7 +130,8 @@ constexpr char kEnrollmentToken[] = "enrollment_token";
 }
 
 // Ensures that the loading screen is dismissed when registration fails.
-- (void)testLoadingScreenDismissedByRegisterFail {
+// TODO(crbug.com/1412454): Flaky.
+- (void)FLAKY_testLoadingScreenDismissedByRegisterFail {
   [self configureAppWithEnrollmentTokenValid:NO];
   [self verifyLoadingScreenIsDismissed];
   GREYAssertFalse(
