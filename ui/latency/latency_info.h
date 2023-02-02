2865db10d313ae9558cacaa68d4a9a036d7b574f
Dave Tapuska <dtapuska@chromium.org>
2023-02-02 22:34:39 +0000

    [ios] Allow LatencyInfo to be sent over IPC for blink on iOS
    Use USE_BLINK instead of IS_IOS.
    
    Bug: 1411704
    Change-Id: I2cbc410b47eafd83869f992168023af651482351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220035
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100689}

--- ui/latency/latency_info.h
+++ ui/latency/latency_info.h
@@ -10,18 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/time/time.h"
+#include "build/blink_buildflags.h"
 #include "build/build_config.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/perfetto/protos/perfetto/trace/track_event/chrome_latency_info.pbzero.h"
 
-#if !BUILDFLAG(IS_IOS)
+#if BUILDFLAG(USE_BLINK)
 #include "ipc/ipc_param_traits.h"  // nogncheck
 #include "mojo/public/cpp/bindings/struct_traits.h"  // nogncheck
 #endif
 
 namespace ui {
 
-#if !BUILDFLAG(IS_IOS)
+#if BUILDFLAG(USE_BLINK)
 namespace mojom {
 class LatencyInfoDataView;
 }
@@ -200,7 +201,7 @@ class LatencyInfo {
   // gesture_scroll_id_.
   int64_t touch_trace_id_ = 0;
 
-#if !BUILDFLAG(IS_IOS)
+#if BUILDFLAG(USE_BLINK)
   friend struct IPC::ParamTraits<ui::LatencyInfo>;
   friend struct mojo::StructTraits<ui::mojom::LatencyInfoDataView,
                                    ui::LatencyInfo>;
