2b694c4b013a81678d36444067a526606616e2b4
Samuel Huang <huangs@chromium.org>
2023-02-02 19:25:53 +0000

    [SuperSize] Fix UI issues related to group_by and diff_mode.
    This CL addresses the following UI issues:
    1. Issue 1290946: Fake and redundant query param "&diff_mode=on"
       gets in the way.
    2. Issue 1186921: Query param "&group_by=container" appears, even though
       it's pretty much the desired default (with multi-container).
    3. Issue 1411787: Under diff mode for multi-container .size / .sizediff
       files, group symbols infocard fail to show add / remove / changed
       stats.
    
    Causes:
    1. diff_mode is obtained from Caspian (since in general it can depend on
       Upload data loads a .size or .sizediff file), and disseminated via
       |state|.{set,has,get}() for use by UI. However, |state| also writes
       query param, resulting in &diff_mode=on noise.
    2. The default value for "group_by" is "source_path" (for the "Nothing"
       radio button). Meanwhile, processLoadTreeResponse(), on loading
       multi-container files, forces "group_by" to "container", which being
       the non-default, required query param to get "&group_by=container".
    3. 3 = 2 + 1: The "group_by" reassignment in (2) is followed by
       triggering "change" event in form#options, whose handler calls
       rebuildTree(). The "fake" state (since there's no <input> element)
       &diff_mode=on vanishes, so the UI code misses it, and does not show
       the desired add / remove / changed stats.
    
    We're applying relatively simple code changes to fix the above, and
    will refactor in follow-up CLs.
    
    For (3), a quick fix would be to add diff_mode as an <input> element
    with type="hidden" with data-dynamic. However, this would make
    &diff_mode=on an "official" state, conflicting with (1). So instead,
    we make State store diff_mode separately, with specialized getter /
    setter. This also fixes (1).
    
    For (2), we make &group_by=container the default, and remove the
    processLoadTreeResponse() part that forces "group_by" to "container". To
    accommodate single-container .size / .sizediff files, we make the
    "container" option always available (instead of being disabled), and
    live with having "(Default container)" as the symbol tree root.
    
    Details:
    * State: Add {get/set}DiffMode(), and replace state.{has,set}() calls on
      "diff_mode" to use this. This relies on closure variable |_diffMode|,
      initialized by checking initial load_url / before_url params.
    * processLoadTreeResponse():
      * Remove line to disable #group-by-container for single-container.
      * Remove forced group_by-container assignment and refresh, and just
        call processBuildTreeResponse() directly.
    * Assign default &group_by=container:
      * viewer.html: Move checked attribute from #group-by-sourcepath to
        #group-by-container.
      * tree-worker-wasm.js: Update parseOptions(), which redundantly sets
        the default.
    
    Bug: 1290946, 1186921, 1411787
    Change-Id: I945886c8227c118cfdda655db7e3c513a1f4839e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219254
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100572}

--- tools/binary_size/libsupersize/viewer/static/state.js
+++ tools/binary_size/libsupersize/viewer/static/state.js
@@ -75,6 +75,9 @@ function _initState() {
     _filterParams.append(_TYPE_STATE_KEY, type);
   }
 
+  /** @type {boolean} */
+  let _diffMode = false;
+
   const state = Object.freeze({
     /**
      * Returns a string from the current query string state.
@@ -121,7 +124,25 @@ function _initState() {
       // Passing empty `state` leads to no change, so use `location.pathname`.
       history.replaceState(null, null, state.toString() || location.pathname);
     },
+
+    /** @return {boolean} */
+    getDiffMode() {
+      return _diffMode;
+    },
+
+    /** @param {boolean} diffMode */
+    setDiffMode(diffMode) {
+      _diffMode = diffMode;
+    }
   });
+
+  // Deduce diff mode from initial params.
+  {
+    const loadUrl = _filterParams.get('load_url');
+    const beforeUrl = _filterParams.get('before_url');
+    state.setDiffMode(
+        Boolean(loadUrl && (loadUrl.endsWith('.sizediff') || beforeUrl)));
+  }
 
   // Update form inputs to reflect the state from URL.
   for (const element of
@@ -433,8 +454,7 @@ function _makeSizeTextGetter() {
    */
   function setSizeClasses(sizeElement, value) {
     const cutOff = methodCountInput.checked ? 10 : 50000;
-    const shouldHaveStyle =
-      state.has('diff_mode') && Math.abs(value) > cutOff;
+    const shouldHaveStyle = state.getDiffMode() && Math.abs(value) > cutOff;
 
     if (shouldHaveStyle) {
       if (value < 0) {
