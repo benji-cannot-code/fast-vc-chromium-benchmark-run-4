baf77019b7a05218ffe22078ed2525857bed59a2
Lei Zhang <thestig@chromium.org>
2023-02-02 18:23:42 +0000

    Clean up includes in Blink's style_rule_keyframe.h.
    Include only what's required. Then add required includes to
    style_rule_keyframe.cc to fix the build.
    
    Change-Id: I0f65dc6af847d231fa4b7ae1759373cb8628690f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4215128
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100512}

--- third_party/blink/renderer/core/css/style_rule_keyframe.cc
+++ third_party/blink/renderer/core/css/style_rule_keyframe.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_rule_keyframe.h"
 
 #include <memory>
+
+#include "third_party/blink/renderer/core/animation/timing.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
