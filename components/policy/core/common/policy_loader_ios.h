9039dbd68fe39dac5c14721da54a6e59f56ad1a1
Vincent Boisselle <vincb@google.com>
2023-02-02 21:53:28 +0000

    [ios]Don't monitor NSUserDefaults changes to refresh platform policies
    Handling the platform policy refresh on NSUserDefaults change
    notifications can be costly and is fragile (can lead to long
    chain of calls with re-entries). There can be hundreds of
    NSUserDefaults updates in a second. Re-evaluating every few seconds
    large policy payloads with thousands of policy items  could make the
    browser janky.
    
    The monitoring of NSUserDefaults didn't seem problematic until
    its conceptual flaw was exposed by the
    recently deployed ScopedCriticalActionReuseEnabled experiment.
      - We discussed the option of turning off the experiment for now,
        the time we figure a viable solution, but this is too
        patchy and will just kick the can; we need something a bit more
        viable and durable.
    
    The AsyncPolicyLoader has already a baked in refresh scheduling where
    it ensures a platform policy refresh every 15 minutes.
    
    We have already explored most of the possible options:
     1) Use dispatch_async instead of PostTask() to avoid the
        PostTask() re-entry.
         - This doesn't prevent from other re-entry situations
          that may trigger similar issues.
     2) Skip NSUserDefaults changes that aren't on the AppConfig key.
         - Compare value object references.
         - Compare values.
         - Use KVO.
            * Can't be done with NSUserDefaults dictionary.
     3) Stop monitoring NSUserDefaults and rely on the baked in
        refresh interval. That interval can be adjusted if needed if there
        is pushback on the 15 minutes interval.
     4) Trigger updates on UI events such as foregrounding.
    
    Considering that AsyncPolicyLoader can already handle refreshes,
    option 3) seems the best and simplest option. Option 3) saves
    computation time and is more robust at the expense of freshness.
    The refresh interval can be adjusted.
    
    Option 3) can be deployed immediately with a high confidence that
    there won't be unexpected side effects.
    
    
    Bug: 1412137, 1412066
    Change-Id: I6d0b4e85f9c5f8aded0d99e3104b768c9d0398fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4213472
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100660}

--- components/policy/core/common/policy_loader_ios.h
+++ components/policy/core/common/policy_loader_ios.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
-#include "base/time/time.h"
 #include "components/policy/core/common/async_policy_loader.h"
 #include "components/policy/policy_export.h"
 
@@ -17,7 +16,6 @@ namespace policy {
 class SchemaRegistry;
 
 // A policy loader that loads policy from the managed app configuration
-// introduced in iOS 7.
 class POLICY_EXPORT PolicyLoaderIOS : public AsyncPolicyLoader {
  public:
   explicit PolicyLoaderIOS(
@@ -30,11 +28,8 @@ class POLICY_EXPORT PolicyLoaderIOS : public AsyncPolicyLoader {
   // AsyncPolicyLoader implementation.
   void InitOnBackgroundThread() override;
   PolicyBundle Load() override;
-  base::Time LastModificationTime() override;
 
  private:
-  void UserDefaultsChanged();
-
   // Loads the Chrome policies in |dictionary| into the given |bundle|.
   void LoadNSDictionaryToPolicyBundle(NSDictionary* dictionary,
                                       PolicyBundle* bundle);
@@ -47,13 +42,6 @@ class POLICY_EXPORT PolicyLoaderIOS : public AsyncPolicyLoader {
 
   // The schema used by |ValidatePolicyData()|.
   const Schema* policy_schema_;
-
-  // Used to manage the registration for NSNotificationCenter notifications.
-  __strong id notification_observer_;
-
-  // Timestamp of the last notification.
-  // Used to coalesce repeated notifications into a single Load() call.
-  base::Time last_notification_time_;
 
   // Used to Bind() a WeakPtr to |this| for the callback passed to the
   // |notification_observer_|.
