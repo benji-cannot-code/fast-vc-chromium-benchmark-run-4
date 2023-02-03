b2e724efc5f89caaa2a9c68ebc2c2bec43894f24
David Bienvenu <davidbienvenu@chromium.org>
2023-02-03 19:23:07 +0000

    win7dep: call UiaRaiseActiveTextPositionChangedEvent directly
    Now that Windows 7 support is deprecated,
    UiaRaiseActiveTextPositionChangedEvent should always be available and
    can be called directly. This should have no functional change.
    
    AX-Relnotes: n/a.
    Bug: 1408307
    Change-Id: I748877e6da282b8ebaedf0fcbfbfee097eea81f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220763
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101132}

--- content/browser/accessibility/browser_accessibility_manager_win.h
+++ content/browser/accessibility/browser_accessibility_manager_win.h
@@ -22,9 +22,6 @@ namespace content {
 class BrowserAccessibilityWin;
 class WebAXPlatformTreeManagerDelegate;
 
-using UiaRaiseActiveTextPositionChangedEventFunction =
-    HRESULT(WINAPI*)(IRawElementProviderSimple*, ITextRangeProvider*);
-
 // Manages a tree of BrowserAccessibilityWin objects.
 class CONTENT_EXPORT BrowserAccessibilityManagerWin
     : public BrowserAccessibilityManager {
@@ -40,7 +37,6 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
   ~BrowserAccessibilityManagerWin() override;
 
   static ui::AXTreeUpdate GetEmptyDocument();
-  static bool IsUiaActiveTextPositionChangedEventSupported();
 
   // Get the closest containing HWND.
   HWND GetParentHWND();
@@ -113,10 +109,6 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
       SelectionEventsMap& selection_events_map,
       IsSelectedPredicate is_selected_predicate,
       FirePlatformSelectionEventsCallback fire_platform_events_callback);
-
-  // Retrieve UIA RaiseActiveTextPositionChangedEvent function if supported.
-  static UiaRaiseActiveTextPositionChangedEventFunction
-  GetUiaActiveTextPositionChangedEventFunction();
 
   void HandleAriaPropertiesChangedEvent(BrowserAccessibility& node);
   void EnqueueTextChangedEvent(BrowserAccessibility& node);
