1f0a5b3ec36040452946fe1ba1d49b7b39bed0b5
Michael Checo <michaelcheco@google.com>
2023-02-03 00:47:58 +0000

    Settings Split: Initial commit for pointing stick pref handler
    Bug: b/241965700
    Change-Id: I1959430d3531c659ec72309f5f426761a5a348bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205028
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100761}

---
+++ ash/system/input_device_settings/pref_handlers/pointing_stick_pref_handler_impl.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/input_device_settings/pref_handlers/pointing_stick_pref_handler_impl.h"
+
+#include "ash/public/mojom/input_device_settings.mojom-forward.h"
+#include "base/notreached.h"
+#include "components/prefs/pref_service.h"
+
+namespace ash {
+
+PointingStickPrefHandlerImpl::PointingStickPrefHandlerImpl() = default;
+PointingStickPrefHandlerImpl::~PointingStickPrefHandlerImpl() = default;
+
+// TODO(michaelcheco): Implement pointing stick settings initialization.
+void PointingStickPrefHandlerImpl::InitializePointingStickSettings(
+    PrefService* pref_service,
+    mojom::PointingStick* pointing_stick) {
+  NOTIMPLEMENTED();
+}
+
+// TODO(michaelcheco): Implement pointing stick settings updates.
+void PointingStickPrefHandlerImpl::UpdatePointingStickSettings(
+    PrefService* pref_service,
+    const mojom::PointingStick& pointing_stick) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace ash
---
+++ ash/system/input_device_settings/pref_handlers/pointing_stick_pref_handler_impl.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_INPUT_DEVICE_SETTINGS_PREF_HANDLERS_POINTING_STICK_PREF_HANDLER_IMPL_H_
+#define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_PREF_HANDLERS_POINTING_STICK_PREF_HANDLER_IMPL_H_
+
+#include "ash/ash_export.h"
+#include "ash/system/input_device_settings/pref_handlers/pointing_stick_pref_handler.h"
+
+class PrefService;
+
+namespace ash {
+
+class ASH_EXPORT PointingStickPrefHandlerImpl
+    : public PointingStickPrefHandler {
+ public:
+  PointingStickPrefHandlerImpl();
+  PointingStickPrefHandlerImpl(const PointingStickPrefHandlerImpl&) = delete;
+  PointingStickPrefHandlerImpl& operator=(const PointingStickPrefHandlerImpl&) =
+      delete;
+  ~PointingStickPrefHandlerImpl() override;
+
+  // PointingStickPrefHandler:
+  void InitializePointingStickSettings(
+      PrefService* pref_service,
+      mojom::PointingStick* pointing_stick) override;
+  void UpdatePointingStickSettings(
+      PrefService* pref_service,
+      const mojom::PointingStick& pointing_stick) override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_INPUT_DEVICE_SETTINGS_PREF_HANDLERS_POINTING_STICK_PREF_HANDLER_IMPL_H_
