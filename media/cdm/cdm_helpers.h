87e5c10193d3e0d614de134d742f6c7fd94c8948
John Rummell <jrummell@chromium.org>
2023-02-03 02:52:22 +0000

    Fix dangling raw_ptrs in cdm_helpers
    The cdm::Buffer objects use a C interface as they can be shared with
    other libraries, so destruction of the objects happen using a Destroy()
    method. Updating the code to avoid dangling ptr detection.
    
    Bug: 1291138
    Change-Id: I3f8329ce7be8e603246e93f16b290eeefe109ba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220292
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100810}

--- media/cdm/cdm_helpers.h
+++ media/cdm/cdm_helpers.h
@@ -36,7 +36,7 @@ class DecryptedBlockImpl final : public cdm::DecryptedBlock {
   int64_t Timestamp() const final;
 
  private:
-  raw_ptr<cdm::Buffer, DanglingUntriaged> buffer_;
+  raw_ptr<cdm::Buffer> buffer_;
   int64_t timestamp_;
 };
 
@@ -92,7 +92,7 @@ class MEDIA_EXPORT VideoFrameImpl : public cdm::VideoFrame,
   cdm::Size size_;
 
   // The video frame buffer.
-  raw_ptr<cdm::Buffer, DanglingUntriaged> frame_buffer_;
+  raw_ptr<cdm::Buffer> frame_buffer_;
 
   // Array of data pointers to each plane in the video frame buffer.
   uint32_t plane_offsets_[cdm::kMaxPlanes];
@@ -124,7 +124,7 @@ class AudioFramesImpl final : public cdm::AudioFrames {
   cdm::Buffer* PassFrameBuffer();
 
  private:
-  raw_ptr<cdm::Buffer, DanglingUntriaged> buffer_;
+  raw_ptr<cdm::Buffer> buffer_;
   cdm::AudioFormat format_;
 };
 
