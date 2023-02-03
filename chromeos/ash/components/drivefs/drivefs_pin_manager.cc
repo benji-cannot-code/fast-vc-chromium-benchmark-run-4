b3f28a6e0a3d090795ffc6bd1a7aa953390db052
François Degros <fdegros@chromium.org>
2023-02-03 01:59:28 +0000

    PinManager: Stay in kSyncing state
    After listing and syncing all the required files, the PinManager stays
    in the syncing state to monitor for further events.
    
    Bug: b:267683036
    Test: chromeos_unittests --gtest_filter=DriveFsPinManagerTest.*
    Change-Id: I8fd550ec02b8e3eac4e14e0d3a18ad11b9af52e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218832
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100794}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.cc
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.cc
@@ -335,8 +335,6 @@ bool PinManager::Add(const Id id,
   progress_.bytes_to_pin += size;
   progress_.required_space += RoundToBlockSize(size);
   progress_.files_to_pin++;
-  DCHECK_EQ(static_cast<size_t>(progress_.files_to_pin),
-            files_to_track_.size());
 
   if (pinned) {
     progress_.syncing_files++;
@@ -640,14 +638,6 @@ void PinManager::StartPinning() {
     return Complete(Stage::kSuccess);
   }
 
-  if (files_to_track_.empty()) {
-    VLOG(1) << "Nothing to pin or track";
-    DCHECK(files_to_pin_.empty());
-    DCHECK_EQ(progress_.files_to_pin, 0);
-    DCHECK_EQ(progress_.syncing_files, 0);
-    return Complete(Stage::kSuccess);
-  }
-
   timer_ = base::ElapsedTimer();
   progress_.stage = Stage::kSyncing;
   NotifyProgress();
@@ -666,11 +656,6 @@ void PinManager::PinSomeFiles() {
 
   if (progress_.stage != Stage::kSyncing) {
     return;
-  }
-
-  if (files_to_track_.empty() && files_to_pin_.empty()) {
-    VLOG(1) << "Nothing left to pin or track";
-    return Complete(Stage::kSuccess);
   }
 
   while (progress_.syncing_files < 50 && !files_to_pin_.empty()) {
