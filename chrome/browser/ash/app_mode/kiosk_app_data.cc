6c3fc810716ff5d0eb0e5f5a191aa3d4b33b90f2
Oleg Davydov <burunduk@chromium.org>
2023-02-03 14:28:29 +0000

    [Kiosk] Remove SetIgnoreKioskAppDataLoadFailuresForTesting
    The method KioskAppData::SetIgnoreKioskAppDataLoadFailuresForTesting was
    a workaround to let kiosk apps be installed in tests while we fail to
    fetch their details (outside of tests this is done via a request to
    Chrome Web Store.
    
    This change adds a minimal setup to affected tests to handle this
    request, after which we don't need a special test-only method which
    disables all the checks.
    
    Bug: b:246912087
    Change-Id: I86a2456d565621bbbfca4adbb9adeb90e1fd10c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4208934
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100971}

--- chrome/browser/ash/app_mode/kiosk_app_data.cc
+++ chrome/browser/ash/app_mode/kiosk_app_data.cc
@@ -50,8 +50,6 @@ namespace {
 // Keys for local state data. See sample layout in KioskAppManager.
 constexpr char kKeyRequiredPlatformVersion[] = "required_platform_version";
 
-bool ignore_kiosk_app_data_load_failures_for_testing = false;
-
 // Returns true for valid kiosk app manifest.
 bool IsValidKioskAppManifest(const extensions::Manifest& manifest) {
   return manifest.FindBoolPath(extensions::manifest_keys::kKioskEnabled)
@@ -266,12 +264,7 @@ KioskAppData::KioskAppData(KioskAppDataDelegate* delegate,
       delegate_(delegate),
       status_(Status::kInit),
       update_url_(update_url),
-      crx_file_(cached_crx) {
-  if (ignore_kiosk_app_data_load_failures_for_testing) {
-    LOG(WARNING) << "Force KioskAppData loaded for testing.";
-    SetStatus(Status::kLoaded);
-  }
-}
+      crx_file_(cached_crx) {}
 
 KioskAppData::~KioskAppData() = default;
 
@@ -346,12 +339,6 @@ std::unique_ptr<KioskAppData> KioskAppData::CreateForTest(
 }
 
 void KioskAppData::SetStatus(Status status) {
-  if (status == Status::kError &&
-      ignore_kiosk_app_data_load_failures_for_testing) {
-    LOG(WARNING) << "Ignoring KioskAppData error for testing. Force OK.";
-    status = Status::kLoaded;
-  }
-
   if (status_ == status) {
     return;
   }
@@ -455,11 +442,6 @@ void KioskAppData::OnIconLoadDone(absl::optional<gfx::ImageSkia> icon) {
 
   icon_ = icon.value();
   SetStatus(Status::kLoaded);
-}
-
-// static
-void KioskAppData::SetIgnoreKioskAppDataLoadFailuresForTesting(bool value) {
-  ignore_kiosk_app_data_load_failures_for_testing = value;
 }
 
 void KioskAppData::OnWebstoreParseSuccess(
