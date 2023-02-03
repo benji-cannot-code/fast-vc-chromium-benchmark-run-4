223d911f087e6c167ce1f297b7f20bc55171f580
dpapad <dpapad@chromium.org>
2023-02-03 23:44:06 +0000

    CrOS Camera app: Fix new ESLint errors revealed by ESLint v8.33.0.
    Bug: 1412862
    Change-Id: If900f4035b7ddb5bef12514d692e0171363fff07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4222072
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101259}

--- ash/webui/camera_app_ui/resources/js/device/mode/video.ts
+++ ash/webui/camera_app_ui/resources/js/device/mode/video.ts
@@ -222,7 +222,7 @@ export class Video extends ModeBase {
   /**
    * HTMLElement displaying warning about low storage.
    */
-  private lowStorageWarningNudge = dom.get('#nudge', HTMLDivElement);
+  private readonly lowStorageWarningNudge = dom.get('#nudge', HTMLDivElement);
 
   constructor(
       video: PreviewVideo,
