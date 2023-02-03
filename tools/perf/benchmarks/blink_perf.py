2978a9c9b4634d0c0b8a53d288f3794520e19d56
Kent Tamura <tkent@chromium.org>
2023-02-03 08:27:34 +0000

    DeferredShaping: Disable the feature in non-blink tests
    We decided not to ship the feature.
    
    Bug: 1259085, 1359553, 1359625, 1359575
    Change-Id: Ic03c14f52ec0b779c170e01269eac70aecbf2e41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218174
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100883}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -393,9 +393,7 @@ class BlinkPerfAccessibility(_BlinkPerfBenchmark):
     return 'blink_perf.accessibility'
 
   def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs([
-        '--force-renderer-accessibility', '--disable-features=DeferredShaping'
-    ])
+    options.AppendExtraBrowserArgs(['--force-renderer-accessibility'])
 
 
 @benchmark.Info(
@@ -621,9 +619,6 @@ class BlinkPerfPaint(_BlinkPerfBenchmark):
   @classmethod
   def Name(cls):
     return 'blink_perf.paint'
-
-  def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(['--disable-features=DeferredShaping'])
 
 
 @benchmark.Info(emails=['yoavweiss@chromium.org'],
