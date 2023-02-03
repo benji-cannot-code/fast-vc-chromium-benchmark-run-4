30186b1c3f6467ba8bbf4ae67a713ba2de19e7f2
Petro Akzhygitov <pakzhygitov@google.com>
2023-02-03 19:44:50 +0000

    [Pinned Tabs][2/N] Add transition animation.
    Creating a transition from the tab view to the grid for the pinned tabs.
    
    Implemented `transitionLayout` and `isSelectedCellVisible` methods for
    the `PinnedTabsViewController`.
    
    Bug: 1406524
    Change-Id: Id5ce07323ef125c214d175fc1d5ae95dfe9dd546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218491
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101144}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.h
@@ -58,6 +58,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Tracks if a drop animation is in progress.
 @property(nonatomic, assign) BOOL dropAnimationInProgress;
 
+// YES if the selected cell is visible in the Pinned Tabs collection.
+@property(nonatomic, readonly, getter=isSelectedCellVisible)
+    BOOL selectedCellVisible;
+
 // Updates the view when starting or ending a drag action.
 - (void)dragSessionEnabled:(BOOL)enabled;
 
