f3ccbab546aa62ff6874c3d2c1b1bb0af0481fba
Jiaming Cheng <jiamingc@chromium.org>
2023-02-03 22:34:23 +0000

    Implement the revamped network off states
    Hide the wifi or mobile network list when the toggle is off.
    If the network list if hidden, make the header all rounded.
    
    Screenshot:
    https://screenshot.googleplex.com/6pMDeJ2LdEz42Ys
    
    Bug: b/266011184
    Change-Id: I0026e69662001f289238ebf83e00f56fd7486bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4195387
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101229}

--- ash/system/tray/hover_highlight_view.cc
+++ ash/system/tray/hover_highlight_view.cc
@@ -60,8 +60,9 @@ void HoverHighlightView::AddRightView(views::View* view,
   // removed.
   tri_view_->SetContainerBorder(TriView::Container::CENTER, nullptr);
 
-  if (border)
+  if (border) {
     tri_view_->SetContainerBorder(TriView::Container::END, std::move(border));
+  }
 
   right_view_ = view;
   right_view_->SetEnabled(GetEnabled());
@@ -69,10 +70,16 @@ void HoverHighlightView::AddRightView(views::View* view,
   tri_view_->SetContainerVisible(TriView::Container::END, true);
 }
 
+void HoverHighlightView::AddAdditionalRightView(views::View* view) {
+  DCHECK(is_populated_);
+  tri_view_->AddViewAt(TriView::Container::END, view, /*index=*/0);
+}
+
 void HoverHighlightView::SetRightViewVisible(bool visible) {
   DCHECK(is_populated_);
-  if (!right_view_)
+  if (!right_view_) {
     return;
+  }
 
   tri_view_->SetContainerVisible(TriView::Container::END, visible);
   right_view_->SetVisible(visible);
@@ -189,8 +196,9 @@ void HoverHighlightView::SetExpandable(bool expandable) {
 void HoverHighlightView::SetAccessibilityState(
     AccessibilityState accessibility_state) {
   accessibility_state_ = accessibility_state;
-  if (accessibility_state_ != AccessibilityState::DEFAULT)
+  if (accessibility_state_ != AccessibilityState::DEFAULT) {
     NotifyAccessibilityEvent(ax::mojom::Event::kCheckedStateChanged, true);
+  }
 }
 
 void HoverHighlightView::Reset() {
@@ -205,10 +213,12 @@ void HoverHighlightView::Reset() {
 }
 
 void HoverHighlightView::OnSetTooltipText(const std::u16string& tooltip_text) {
-  if (text_label_)
+  if (text_label_) {
     text_label_->SetTooltipText(tooltip_text);
-  if (sub_text_label_)
+  }
+  if (sub_text_label_) {
     sub_text_label_->SetTooltipText(tooltip_text);
+  }
   if (left_view_) {
     DCHECK(views::IsViewClass<views::ImageView>(left_view_));
     static_cast<views::ImageView*>(left_view_)->SetTooltipText(tooltip_text);
@@ -216,8 +226,9 @@ void HoverHighlightView::OnSetTooltipText(const std::u16string& tooltip_text) {
 }
 
 bool HoverHighlightView::PerformAction(const ui::Event& event) {
-  if (!listener_)
+  if (!listener_) {
     return false;
+  }
   listener_->OnViewClicked(this);
   return true;
 }
@@ -241,12 +252,13 @@ void HoverHighlightView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
 
   ax::mojom::CheckedState checked_state;
 
-  if (accessibility_state_ == AccessibilityState::CHECKED_CHECKBOX)
+  if (accessibility_state_ == AccessibilityState::CHECKED_CHECKBOX) {
     checked_state = ax::mojom::CheckedState::kTrue;
-  else if (accessibility_state_ == AccessibilityState::UNCHECKED_CHECKBOX)
+  } else if (accessibility_state_ == AccessibilityState::UNCHECKED_CHECKBOX) {
     checked_state = ax::mojom::CheckedState::kFalse;
-  else
+  } else {
     return;  // Not a checkbox
+  }
 
   // Checkbox
   node_data->role = ax::mojom::Role::kCheckBox;
@@ -260,8 +272,9 @@ const char* HoverHighlightView::GetClassName() const {
 gfx::Size HoverHighlightView::CalculatePreferredSize() const {
   gfx::Size size = ActionableView::CalculatePreferredSize();
 
-  if (!expandable_ || size.height() < kTrayPopupItemMinHeight)
+  if (!expandable_ || size.height() < kTrayPopupItemMinHeight) {
     size.set_height(kTrayPopupItemMinHeight);
+  }
 
   return size;
 }
@@ -287,12 +300,15 @@ void HoverHighlightView::AddSubRowContainer() {
 }
 
 void HoverHighlightView::OnEnabledChanged() {
-  if (left_view_)
+  if (left_view_) {
     left_view_->SetEnabled(GetEnabled());
-  if (text_label_)
+  }
+  if (text_label_) {
     text_label_->SetEnabled(GetEnabled());
-  if (right_view_)
+  }
+  if (right_view_) {
     right_view_->SetEnabled(GetEnabled());
+  }
 }
 
 }  // namespace ash
