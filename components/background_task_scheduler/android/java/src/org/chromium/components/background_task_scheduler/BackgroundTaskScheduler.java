aafc278697a83ef14e3d486717fef049ce04e4c5
Tommy Nyquist <nyquist@chromium.org>
2023-02-03 00:31:22 +0000

    [android] Remove proto migration for background task scheduler
    The proto persistence format was introduced in M78, and has been the
    one in use since then. Initially we supported moving from key:value
    pair based proto formats.
    
    There are very few use-cases for upgrading from M78 to a newer version
    left, so removing this migration path.
    
    Bug: 1406114
    Change-Id: I2a8261d7377f79cede62486a47a08bde7bad7097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200837
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100753}

--- components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskScheduler.java
+++ components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskScheduler.java
@@ -47,8 +47,7 @@ public interface BackgroundTaskScheduler {
     boolean isScheduled(Context context, int taskId);
 
     /**
-     * Performs data migrations and flushes cached UMA data. Must not be invoked until native has
-     * been loaded.
+     * Flushes cached UMA data. Must not be invoked until native has been loaded.
      */
     @MainThread
     void doMaintenance();
