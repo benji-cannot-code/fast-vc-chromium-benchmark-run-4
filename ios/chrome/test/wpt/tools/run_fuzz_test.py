07e635c018c154decbc5528ef149340e9aca3160
Ali Juma <ajuma@chromium.org>
2023-02-03 17:12:41 +0000

    [iOS] Fix dependencies for Clusterfuzz test scripts
    The Python scripts used for fuzzing iOS builds on Clusterfuzz depend
    on various Python libraries (directly and transitively), and some of
    these are not installed by default on the Clusterfuzz bots, causing
    iOS fuzzing jobs to fail.
    
    The normal way to encode python dependencies in Chromium is by using
    .vpython3 files and then invoking the Python script using vpython3,
    which then takes care of setting up a virtual environment which
    contains the dependencies.
    
    This CL updates fuzzing scripts to use vpython3 and adds a file
    listing the required dependencies.
    
    This CL also adds a new run_clusterfuzz_test.sh script that includes
    the device type and OS version used for fuzzing, so that changes to
    these values are version-controlled rather than changed ad-hoc in
    the Clusterfuzz configuration UI.
    
    Bug: 1380928
    Change-Id: I6989e97b219e0b7476bec86ba75aa4c588097bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003809
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101036}

--- ios/chrome/test/wpt/tools/run_fuzz_test.py
+++ ios/chrome/test/wpt/tools/run_fuzz_test.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python3
+#!/usr/bin/env vpython3
 # Copyright 2021 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -47,7 +47,7 @@ def StartServer(*, port, build_dir, device, os_version):
       else:
         time.sleep(1)
 
-def IsCurrentVersion(*, version, revision, build_dir):
+def IsCurrentVersion(*, version, build_dir):
   plist_path = os.path.join(build_dir, 'ios_cwt_chromedriver_tests.app',
                             'Info.plist')
   version_command = 'defaults read ' + plist_path + ' CFBundleVersion'
@@ -55,16 +55,7 @@ def IsCurrentVersion(*, version, revision, build_dir):
                                      capture_output=True)
   current_version = completed_process.stdout.decode('utf-8').strip()
 
-  if version != current_version:
-    return False
-
-  revision_command = 'defaults read ' + plist_path + ' SCMRevision'
-  completed_process = subprocess.run(revision_command, shell=True,
-                                     capture_output=True)
-  full_revision = completed_process.stdout.decode('utf-8').strip()
-  current_revision = re.search('.*{#(.+?)}', full_revision).group(1)
-
-  return current_revision == revision
+  return version == current_version
 
 def KillServer():
   # Gather all running run_cwt_chromedriver.py and xcodebuild instances. There
@@ -99,9 +90,7 @@ def EnsureServerStarted(*, port, build_dir, device, os_version):
     response = requests.get(server_url + '/session/chrome_versionInfo')
     assert response.status_code == 200
     chrome_version = response.json()['value']['browserVersion']
-    chrome_revision = response.json()['value']['chrome_revisionNumber']
-    if IsCurrentVersion(version=chrome_version, revision=chrome_revision,
-                        build_dir=build_dir):
+    if IsCurrentVersion(version=chrome_version, build_dir=build_dir):
       return True
     else:
       KillServer()
