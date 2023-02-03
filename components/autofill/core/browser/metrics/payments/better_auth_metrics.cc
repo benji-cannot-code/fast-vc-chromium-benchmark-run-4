1d5f4dbdf226e534924a9a3450ae0705f299dcfd
Vinny Persky <vinnypersky@google.com>
2023-02-03 16:34:02 +0000

    Add metrics logging for every card unmask preflight flow initiated
    This CL adds merics every time we initiate a card unmask preflight flow.
    These metrics will be important in conjunction with
    Autofill.BetterAuth.CardUnmaskPreflightCalledWithFidoOptInStatus to
    understand the proportion of devices that have FIDO capabilities.
    
    Bug: 1410900, 1412782
    Change-Id: Ieb833a3575f8601db7e3ce3a6006fbd610bf4e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200998
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101012}

--- components/autofill/core/browser/metrics/payments/better_auth_metrics.cc
+++ components/autofill/core/browser/metrics/payments/better_auth_metrics.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/payments/better_auth_metrics.h"
 
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 
 namespace autofill::autofill_metrics {
@@ -24,8 +23,13 @@ void LogCardUnmaskDurationAfterWebauthn(
       duration);
 }
 
+void LogCardUnmaskPreflightInitiated() {
+  base::UmaHistogramBoolean("Autofill.BetterAuth.CardUnmaskPreflightInitiated",
+                            true);
+}
+
 void LogCardUnmaskPreflightCalled(bool is_user_opted_in) {
-  UMA_HISTOGRAM_BOOLEAN(
+  base::UmaHistogramBoolean(
       "Autofill.BetterAuth.CardUnmaskPreflightCalledWithFidoOptInStatus",
       is_user_opted_in);
 }
