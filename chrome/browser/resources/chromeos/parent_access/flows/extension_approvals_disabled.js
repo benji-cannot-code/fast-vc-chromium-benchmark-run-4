19af4e582f6e98e2238e03fde455fa78a86bcfdc
Courtney Wong <courtneywong@chromium.org>
2023-02-03 17:35:55 +0000

    Render flow-specific disabled screen content for extension approvals
    Also create an interface for the Parent Access base screens to
    implement.
    
    The disabled screen is part of the parent access flow if parent
    permission is not allowed.
    Screenshot: https://screenshot.googleplex.com/38eXacgM6Fpt2nt
    
    Bug: b:267536630
    Change-Id: I69813bb55d5e97d8c902f5ffa762fd5ffd2c116a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4216092
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101050}

---
+++ chrome/browser/resources/chromeos/parent_access/flows/extension_approvals_disabled.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+export class ExtensionApprovalsDisabled extends PolymerElement {
+  static get is() {
+    return 'extension-approvals-disabled';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+}
+
+customElements.define(
+    ExtensionApprovalsDisabled.is, ExtensionApprovalsDisabled);
---
+++ chrome/browser/resources/chromeos/parent_access/parent_access_screen.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * An interface to be implemented by the generic Parent Access screens that
+ * render different content depending on flow type.
+ * @interface
+ */
+export class ParentAccessScreenInterface {
+  /** Renders the correct content for the screen depending on the flow type. */
+  async renderFlowSpecificContent() {}
+}
