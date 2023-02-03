29ff86bb18f3640f7169570886704a75fd0d8a4b
Justin Lulejian <jlulejian@chromium.org>
2023-02-03 23:24:05 +0000

    [Extensions] Fix chrome.windows.create minimized failing on linux X11.
    Before this change calls like:
    
    `chrome.windows.create(
        {url: "https://example.com", state: "minimized"});`
    
    Would not create a minimized window on Linux X11. With this we call
    Minimize() after Show() to force this when the API call requests it.
    
    I also pulled a convenient waiter method out of widget test utils for
    reuse since the asynchronous nature of IsMinimized() for X11 is (per the
    code comment) still true.
    
    Wayland is not fixed by this change and will need more investigation.
    
    Bug: 1406188
    Change-Id: I7677d044292a43b8ca6c8c4beb908153213094ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201093
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101247}

--- ui/views/widget/widget_interactive_uitest.cc
+++ ui/views/widget/widget_interactive_uitest.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/root_view.h"
 #include "ui/views/widget/unique_widget_ptr.h"
 #include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_interactive_uitest_utils.h"
 #include "ui/views/widget/widget_utils.h"
 #include "ui/views/window/dialog_delegate.h"
 #include "ui/wm/public/activation_client.h"
@@ -273,43 +274,6 @@ void ShowInactiveSync(Widget* widget) {
   widget->ShowInactive();
   RunPendingMessagesForActiveStatusChange();
 }
-
-// Wait until |callback| returns |expected_value|, but no longer than 1 second.
-class PropertyWaiter {
- public:
-  PropertyWaiter(base::RepeatingCallback<bool(void)> callback,
-                 bool expected_value)
-      : callback_(std::move(callback)), expected_value_(expected_value) {}
-
-  bool Wait() {
-    if (callback_.Run() == expected_value_) {
-      success_ = true;
-      return success_;
-    }
-    start_time_ = base::TimeTicks::Now();
-    timer_.Start(FROM_HERE, base::TimeDelta(), this, &PropertyWaiter::Check);
-    run_loop_.Run();
-    return success_;
-  }
-
- private:
-  void Check() {
-    DCHECK(!success_);
-    success_ = callback_.Run() == expected_value_;
-    if (success_ || base::TimeTicks::Now() - start_time_ > kTimeout) {
-      timer_.Stop();
-      run_loop_.Quit();
-    }
-  }
-
-  const base::TimeDelta kTimeout = base::Seconds(1);
-  base::RepeatingCallback<bool(void)> callback_;
-  const bool expected_value_;
-  bool success_ = false;
-  base::TimeTicks start_time_;
-  base::RunLoop run_loop_;
-  base::RepeatingTimer timer_;
-};
 
 std::unique_ptr<Textfield> CreateTextfield() {
   auto textfield = std::make_unique<Textfield>();
