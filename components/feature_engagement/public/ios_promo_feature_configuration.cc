d3705e490c1757df1c1aa6924e9c499432c07247
Robbie Gibson <rkgibson@google.com>
2023-02-03 18:08:09 +0000

    Split iOS Promo FET client-side configs out into separate file
    Bug: 1408214
    Change-Id: I9c134a170a7e4bb29e5cff43fffc0eaed44ab3d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214955
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101075}

---
+++ components/feature_engagement/public/ios_promo_feature_configuration.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/feature_engagement/public/ios_promo_feature_configuration.h"
+
+#include "base/strings/string_util.h"
+#include "build/build_config.h"
+#include "components/feature_engagement/public/configuration.h"
+#include "components/feature_engagement/public/feature_constants.h"
+
+namespace feature_engagement {
+
+absl::optional<FeatureConfig> GetClientSideiOSPromoFeatureConfig(
+    const base::Feature* feature) {
+  if (kIPHiOSAppStorePromoFeature.name == feature->name) {
+    absl::optional<FeatureConfig> config = FeatureConfig();
+    config->valid = true;
+    config->availability = Comparator(ANY, 0);
+    config->session_rate = Comparator(ANY, 0);
+    config->used =
+        EventConfig("app_store_promo_used", Comparator(EQUAL, 0), 365, 730);
+    config->trigger =
+        EventConfig("app_store_promo_trigger", Comparator(EQUAL, 0), 365, 730);
+    return config;
+  }
+
+  return absl::nullopt;
+}
+
+}  // namespace feature_engagement
