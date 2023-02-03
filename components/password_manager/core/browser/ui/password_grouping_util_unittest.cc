91655af266898e04077bc02a3e8bc7826c40af6e
Veronique Nguyen <veronguyen@google.com>
2023-02-03 18:17:27 +0000

    [iOS] User can't delete blocked sites when password grouping is enabled
    - Add logic to remove blocked site from cache.
    - Convert PasswordForm vector to CredentialUIEntry vector.
    
    Bug: 1412142
    Change-Id: I7a4252a40e35117f051c600d7ae963f54049e054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4215772
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101082}

--- components/password_manager/core/browser/ui/password_grouping_util_unittest.cc
+++ components/password_manager/core/browser/ui/password_grouping_util_unittest.cc
@@ -144,7 +144,7 @@ TEST(PasswordGroupingUtilTest, GroupPasswords) {
   PasswordGroupingInfo expected_password_grouping_info;
   expected_password_grouping_info.map_group_id_to_forms = map_group_id_to_forms;
 
-  std::vector<password_manager::CredentialUIEntry> expected_blocked_sites;
+  std::vector<PasswordForm> expected_blocked_sites;
   expected_blocked_sites.emplace_back(blocked_form);
 
   PasswordGroupingInfo password_grouping_info =
@@ -211,7 +211,7 @@ TEST(PasswordGroupingUtilTest, GroupPasswordsWithoutAffiliation) {
   PasswordGroupingInfo expected_password_grouping_info;
   expected_password_grouping_info.map_group_id_to_forms = map_group_id_to_forms;
 
-  std::vector<password_manager::CredentialUIEntry> expected_blocked_sites;
+  std::vector<PasswordForm> expected_blocked_sites;
   expected_blocked_sites.emplace_back(blocked_form);
 
   PasswordGroupingInfo password_grouping_info =
