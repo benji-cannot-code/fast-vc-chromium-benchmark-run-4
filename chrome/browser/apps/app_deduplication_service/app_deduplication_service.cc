5caeebac5708de38079569047a629e44f65af0bc
Nikki Fang <nikkifang@google.com>
2023-02-03 05:57:37 +0000

    [Deduplication] Allow write/read in cache to run on another thread
    Modify AppDeduplicationCache and unit tests to add the task runner. The write and read methods are blocking functions and cannot run on the main thread so the task runner is used to post them to a background thread.
    
    BUG: b/267541567
    Change-Id: Ib3e3a4dafb254694828115469b6e7e6e81e0671e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4216292
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Nikki Fang <nikkifang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100850}

--- chrome/browser/apps/app_deduplication_service/app_deduplication_service.cc
+++ chrome/browser/apps/app_deduplication_service/app_deduplication_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/files/file_path.h"
 #include "base/functional/callback_helpers.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/apps/app_deduplication_service/app_deduplication_service.h"
@@ -13,6 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/services/app_service/public/cpp/types_util.h"
+
+namespace {
+// Folder path to where the deduplication data will be stored on disk.
+constexpr char kAppDeduplicationFolderPath[] =
+    "app_deduplication_service/deduplication_data/";
+}  // namespace
 
 namespace apps::deduplication {
 
@@ -24,6 +31,10 @@ AppDeduplicationService::AppDeduplicationService(Profile* profile)
   app_registry_cache_observation_.Observe(
       &apps::AppServiceProxyFactory::GetForProfile(profile)
            ->AppRegistryCache());
+
+  base::FilePath path =
+      profile_->GetPath().AppendASCII(kAppDeduplicationFolderPath);
+  cache_ = std::make_unique<AppDeduplicationCache>(path);
 }
 
 AppDeduplicationService::~AppDeduplicationService() = default;
@@ -199,6 +210,23 @@ void AppDeduplicationService::GetDeduplicateDataFromServer() {
 void AppDeduplicationService::OnGetDeduplicateDataFromServerCompleted(
     absl::optional<proto::DeduplicateData> response) {
   // TODO(b/264216262): handle response data and store in disk.
+}
+
+void AppDeduplicationService::OnWriteDeduplicationCacheCompleted(bool result) {
+  if (!result) {
+    LOG(ERROR) << "Writing deduplication data to disk failed.";
+    return;
+  }
+
+  cache_->ReadDeduplicationCache(base::BindOnce(
+      &AppDeduplicationService::OnReadDeduplicationCacheCompleted,
+      weak_ptr_factory_.GetWeakPtr()));
+  return;
+}
+
+void AppDeduplicationService::OnReadDeduplicationCacheCompleted(
+    absl::optional<proto::DeduplicateData> data) {
+  // TODO(b/267549842): handle data and map to list of Entries.
 }
 
 }  // namespace apps::deduplication
