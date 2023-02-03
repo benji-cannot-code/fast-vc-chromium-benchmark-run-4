30190dbb99357ac9778bcbbed692b14089727943
Elmehdi Rahmaoui <erahmaoui@google.com>
2023-02-03 12:02:11 +0000

    [iOS] Email experience metrics
    Add the email experience metrics for one tap and long press. An
    additional CL will be uploaded to use the long press metrics. Long press
    metrics are used here:
    https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/5408769
    
    Bug: 1406473
    Change-Id: If6ed7e656264ab27a2a4a989b7d83cd230aed5c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219012
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100929}

--- ios/chrome/browser/web/annotations/annotations_tab_helper.mm
+++ ios/chrome/browser/web/annotations/annotations_tab_helper.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/annotations/annotations_tab_helper.h"
 
 #import "base/metrics/histogram_functions.h"
+#import "base/metrics/user_metrics.h"
 #import "base/strings/string_util.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
@@ -117,6 +118,8 @@ void AnnotationsTabHelper::OnClick(web::WebState* web_state,
                   options:@{}
         completionHandler:nil];
   } else if (web::annotations::IsNSTextCheckingResultEmail(match)) {
+    base::RecordAction(
+        base::UserMetricsAction("IOS.EmailExperience.OneTap.CreateEmail"));
     MailtoHandlerServiceFactory::GetForBrowserState(
         ChromeBrowserState::FromBrowserState(web_state->GetBrowserState()))
         ->HandleMailtoURL(match.URL);
