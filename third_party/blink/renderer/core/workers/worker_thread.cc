0f26bbcd6fd419d652bdfbac0806f649e1fcd9e4
Austin Sullivan <asully@chromium.org>
2023-02-03 23:52:25 +0000

    Add new task type for Storage
    The new task source was recently specified here:
    https://storage.spec.whatwg.org/#storage-task-source
    
    This will be used for all storage APIs that interact with quota.
    
    Bug: 1412596
    Change-Id: I679382ca480e05bfecc363aef0878d5ef0726cab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219319
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101265}

--- third_party/blink/renderer/core/workers/worker_thread.cc
+++ third_party/blink/renderer/core/workers/worker_thread.cc
@@ -578,6 +578,7 @@ void WorkerThread::InitializeSchedulerOnWorkerThread(
       TaskType::kPermission,
       TaskType::kPostedMessage,
       TaskType::kRemoteEvent,
+      TaskType::kStorage,
       TaskType::kUserInteraction,
       TaskType::kWakeLock,
       TaskType::kWebGL,
