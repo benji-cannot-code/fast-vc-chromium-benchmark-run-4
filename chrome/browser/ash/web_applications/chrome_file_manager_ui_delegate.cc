e28917ff4505d3aa8ceffa0f47835012991be33c
Noel Gordon <noel@chromium.org>
2023-02-03 06:11:27 +0000

    [conflict dialog] add FileManagerUIDelegate ProgressPausedTasks API
    Add file manager UI delegate ProgressPausedTasks API that can call
    io_task_controller ProgressPausedTasks API.
    
    Bug: b:255264604
    Change-Id: Ia922dc840c633906a4ffbdd8b2770d77baf21299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220791
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100851}

--- chrome/browser/ash/web_applications/chrome_file_manager_ui_delegate.cc
+++ chrome/browser/ash/web_applications/chrome_file_manager_ui_delegate.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/values.h"
 #include "chrome/browser/ash/file_manager/file_manager_string_util.h"
+#include "chrome/browser/ash/file_manager/io_task_controller.h"
+#include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/web_ui.h"
@@ -22,4 +24,13 @@ base::Value::Dict ChromeFileManagerUIDelegate::GetLoadTimeData() const {
   const std::string locale = g_browser_process->GetApplicationLocale();
   AddFileManagerFeatureStrings(locale, Profile::FromWebUI(web_ui_), &dict);
   return dict;
+}
+
+void ChromeFileManagerUIDelegate::ProgressPausedTasks() const {
+  file_manager::VolumeManager* const volume_manager =
+      file_manager::VolumeManager::Get(Profile::FromWebUI(web_ui_));
+
+  if (volume_manager && volume_manager->io_task_controller()) {
+    volume_manager->io_task_controller()->ProgressPausedTasks();
+  }
 }
