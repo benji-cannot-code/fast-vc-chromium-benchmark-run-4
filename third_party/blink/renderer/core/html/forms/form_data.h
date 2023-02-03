cdcbc8fa124b2f43285a58d63197305e5edef814
Jon Jensen <jonj@netflix.com>
2023-02-03 00:00:36 +0000

    Add optional submitter argument to FormData constructor
    This patch adds support for the new submitter argument to the FormData constructor.
    
    Spec: https://xhr.spec.whatwg.org/#interface-formdata
    Spec PR (merged): https://github.com/whatwg/xhr/pull/366
    WPT PR: https://github.com/web-platform-tests/wpt/pull/37895
    
    Bug: 1410542
    Change-Id: If17f782f75ae40ae21241c169afc52761fc89544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189297
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100734}

--- third_party/blink/renderer/core/html/forms/form_data.h
+++ third_party/blink/renderer/core/html/forms/form_data.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_sync_iterator_form_data.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_typedefs.h"
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/network/encoded_form_data.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
@@ -58,6 +59,9 @@ class CORE_EXPORT FormData final : public ScriptWrappable,
     return MakeGarbageCollected<FormData>();
   }
   static FormData* Create(HTMLFormElement* form,
+                          ExceptionState& exception_state);
+  static FormData* Create(HTMLFormElement* form,
+                          HTMLElement* submitter,
                           ExceptionState& exception_state);
 
   explicit FormData(const WTF::TextEncoding&);
