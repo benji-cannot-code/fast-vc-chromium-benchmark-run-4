ca46a1e7dec5fdde939f96f24fb663aa917f9285
Viktor Semeniuk <vsemeniuk@google.com>
2023-02-03 12:29:35 +0000

    [Password Manager] Using correct pref for Biometric auth before filling
    Bug: 1409861
    Change-Id: Ic43409f0c155278b9462e0115749a9a91a63c2d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218335
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1100935}

--- chrome/test/data/webui/password_manager/test_util.ts
+++ chrome/test/data/webui/password_manager/test_util.ts
@@ -132,7 +132,7 @@ export function makePasswordManagerPrefs():
     },
     // <if expr="is_win or is_macosx">
     {
-      key: 'biometric_authentication_filling',
+      key: 'password_manager.biometric_authentication_filling',
       type: chrome.settingsPrivate.PrefType.BOOLEAN,
       value: true,
     },
