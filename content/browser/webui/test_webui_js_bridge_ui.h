9aa9b1145fc13e8b4eabb6969b44c233c56fb096
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2023-02-06 21:15:24 +0000

    mojo-webui: Rename JsInterfaceBinder to WebUIJsBridge
    The term JsInterfaceBinder is quite overloaded so renaming to
    WebUIJsBridge.
    
    Bug: 1407936
    Change-Id: I8079c364b5794525bc3e31a3ed09e5e8ded1cbaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4211069
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101785}

---
+++ content/browser/webui/test_webui_js_bridge_ui.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_WEBUI_TEST_WEBUI_JS_BRIDGE_UI_H_
+#define CONTENT_BROWSER_WEBUI_TEST_WEBUI_JS_BRIDGE_UI_H_
+
+#include "content/public/browser/web_ui_controller.h"
+
+namespace content {
+
+// WebUIController for WebUIJsBridge unittests.
+class TestWebUIJsBridgeUI : public WebUIController {
+ public:
+  explicit TestWebUIJsBridgeUI() : WebUIController(nullptr) {}
+  ~TestWebUIJsBridgeUI() override = default;
+
+  TestWebUIJsBridgeUI(const TestWebUIJsBridgeUI&) = delete;
+  TestWebUIJsBridgeUI& operator=(const TestWebUIJsBridgeUI&) = delete;
+
+  WEB_UI_CONTROLLER_TYPE_DECL();
+};
+
+// WebUIController for WebUIJsBridge unittests.
+class TestWebUIJsBridgeIncorrectUI : public WebUIController {
+ public:
+  explicit TestWebUIJsBridgeIncorrectUI() : WebUIController(nullptr) {}
+  ~TestWebUIJsBridgeIncorrectUI() override = default;
+
+  TestWebUIJsBridgeIncorrectUI(const TestWebUIJsBridgeIncorrectUI&) = delete;
+  TestWebUIJsBridgeIncorrectUI& operator=(const TestWebUIJsBridgeIncorrectUI&) =
+      delete;
+
+  WEB_UI_CONTROLLER_TYPE_DECL();
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_WEBUI_TEST_WEBUI_JS_BRIDGE_UI_H_
