08bd7dd463f88d7b746c02f39c9076f4045f81ef
Marcello Salomao <msalomao@google.com>
2023-02-06 01:21:53 +0000

    [Mirror sync] Small fixes
    - Fix missing _ in metadataModel_
    - setAttribute to li instead of inlineStatus
    
    Bug: None
    Test: Manual
    Change-Id: Ice610d4ae61e520f361ecd6989a000b61928f29a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218559
    Auto-Submit: Marcello Salomao <msalomao@google.com>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101383}

--- ui/file_manager/file_manager/foreground/js/ui/file_grid.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_grid.js
@@ -953,7 +953,7 @@ export class FileGrid extends Grid {
         break;
     }
 
-    inlineStatus.setAttribute('data-sync-status', syncStatus);
+    li.setAttribute('data-sync-status', syncStatus);
     inlineStatus.querySelector('.progress')
         .setAttribute('progress', (progress || 0).toFixed(2));
   }
