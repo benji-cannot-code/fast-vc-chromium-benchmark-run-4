1ac54248d731ef224f76c99402cf39ab8ad0146c
Ben Reich <benreich@chromium.org>
2023-02-06 22:35:09 +0000

    [js-code-coverage] Only attach to background ChromeVox pages
    A known bug that foreground and background extension pages shared the
    same v8 isolate means when one collects coverage the others coverage is
    turned off. This leads to a DCHECK when trying to collect coverage for
    the remaining page, to avoid this provide a way on the
    DevToolsAgentCoverageObserver to filter out DevToolsAgentHost's.
    
    Bug: None
    Change-Id: Ia138b3b670b93d7ba80c1312e677ddec1068d0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4216152
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101822}

--- chrome/test/base/devtools_agent_coverage_observer.h
+++ chrome/test/base/devtools_agent_coverage_observer.h
@@ -9,6 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/devtools_listener.h"
 #include "content/public/browser/devtools_agent_host_observer.h"
 
+// Callback to filter out the `DevToolsAgentHost*` that get attached to.
+// Extensions with background / foreground pages share the same v8 isolate
+// (see crbug.com/v8/10820) so don't get coverage for one of those to avoid a
+// DCHECK.
+using ShouldInspectDevToolsAgentHostCallback =
+    base::RepeatingCallback<bool(content::DevToolsAgentHost*)>;
+
 // Observes new DevToolsAgentHosts and ensures code coverage is enabled and
 // can be collected.
 class DevToolsAgentCoverageObserver
@@ -16,6 +23,9 @@ class DevToolsAgentCoverageObserver
  public:
   explicit DevToolsAgentCoverageObserver(
       base::FilePath devtools_code_coverage_dir);
+  DevToolsAgentCoverageObserver(
+      base::FilePath devtools_code_coverage_dir,
+      ShouldInspectDevToolsAgentHostCallback should_inspect_callback);
   ~DevToolsAgentCoverageObserver() override;
 
   bool CoverageEnabled();
@@ -37,6 +47,7 @@ class DevToolsAgentCoverageObserver
                std::unique_ptr<coverage::DevToolsListener>>;
   base::FilePath devtools_code_coverage_dir_;
   DevToolsAgentMap devtools_agents_;
+  ShouldInspectDevToolsAgentHostCallback should_inspect_callback_;
 };
 
 #endif  // CHROME_TEST_BASE_DEVTOOLS_AGENT_COVERAGE_OBSERVER_H_
