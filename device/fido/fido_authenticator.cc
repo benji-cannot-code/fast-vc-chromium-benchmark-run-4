47944fc2dd344fbadd9354ec2097727d3816d02e
Adam Langley <agl@chromium.org>
2023-02-06 01:17:19 +0000

    device/fido: move credBlob to options.
    FidoAuthenticator has grown a number of virtual methods over time which
    really should just be booleans. This change starts to eliminate them in
    favour of values in `AuthenticatorSupportedOptions`.
    
    Change-Id: If229b2aedd4cbe5e8e8c6ae8a0789935d00efaf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201220
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101375}

--- device/fido/fido_authenticator.cc
+++ device/fido/fido_authenticator.cc
@@ -211,10 +211,6 @@ bool FidoAuthenticator::SupportsEnterpriseAttestation() const {
   return false;
 }
 
-bool FidoAuthenticator::SupportsCredBlobOfSize(size_t num_bytes) const {
-  return false;
-}
-
 bool FidoAuthenticator::SupportsDevicePublicKey() const {
   return false;
 }
