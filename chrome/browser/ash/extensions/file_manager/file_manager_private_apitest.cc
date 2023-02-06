1d5445660fb3f94c62a983e6646755e0d9f80ad3
Bo Majewski <majewski@chromium.org>
2023-02-06 05:14:10 +0000

    [Files]: Adds missing header.
    It is possible to compile this file in a way that the base/time/time.h
    is not included. This causes an error in compilation.
    
    Change-Id: Ib6adf07a733cda8f931fd27217bec88ddc26ef6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221139
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101443}

--- chrome/browser/ash/extensions/file_manager/file_manager_private_apitest.cc
+++ chrome/browser/ash/extensions/file_manager/file_manager_private_apitest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/time/time.h"
 #include "chrome/browser/ash/crostini/crostini_manager.h"
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
 #include "chrome/browser/ash/crostini/fake_crostini_features.h"
@@ -725,7 +726,7 @@ IN_PROC_BROWSER_TEST_F(FileManagerPrivateApiTest, SearchFiles) {
     ASSERT_TRUE(jan_15_file.IsValid());
     base::Time jan_15_2020_noon;
     ASSERT_TRUE(base::Time::FromUTCExploded(
-        base::Time::Exploded(2020, 1, 3, 15, 12, 0, 0, 0), &jan_15_2020_noon));
+        base::Time::Exploded{2020, 1, 3, 15, 12, 0, 0, 0}, &jan_15_2020_noon));
     jan_15_file.SetTimes(jan_15_2020_noon, jan_15_2020_noon);
 
     base::File jan_01_file(downloads_dir.Append("bar_01012020.jpg"),
@@ -733,7 +734,7 @@ IN_PROC_BROWSER_TEST_F(FileManagerPrivateApiTest, SearchFiles) {
     ASSERT_TRUE(jan_01_file.IsValid());
     base::Time jan_01_2020_noon;
     ASSERT_TRUE(base::Time::FromUTCExploded(
-        base::Time::Exploded(2020, 1, 3, 1, 12, 0, 0, 0), &jan_01_2020_noon));
+        base::Time::Exploded{2020, 1, 3, 1, 12, 0, 0, 0}, &jan_01_2020_noon));
     jan_01_file.SetTimes(jan_01_2020_noon, jan_01_2020_noon);
   }
 
