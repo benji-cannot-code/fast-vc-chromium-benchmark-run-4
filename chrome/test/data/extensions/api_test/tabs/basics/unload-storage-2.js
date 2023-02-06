6a372128fac5a2cdfa78d3c794cd6e0919b45e16
David Bertoni <dbertoni@chromium.org>
2023-02-06 20:09:39 +0000

    [Extensions] Fix some Tabs tests to be compatible with service workers.
    This tests used window.localStorage to communicate between pages and
    the extension. This CL modifies these tests to send messages instead.
    
    These tests have also been tested for flakiness, since there was some
    concern that extension API calls from the window.unload event handler
    might not be synchronous and could get lost.
    
    Bug: 1093066
    Change-Id: Ic5751d8a027ebf26247560a2d82d77663649979f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220514
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101742}

--- chrome/test/data/extensions/api_test/tabs/basics/unload-storage-2.js
+++ chrome/test/data/extensions/api_test/tabs/basics/unload-storage-2.js
@@ -4,5 +4,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 window.addEventListener('unload', (e) => {
-  localStorage.setItem('did_run_unload_2', 'yes');
+  chrome.runtime.sendMessage('did_run_unload_2');
 });
