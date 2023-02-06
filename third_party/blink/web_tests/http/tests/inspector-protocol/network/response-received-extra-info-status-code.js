5636837483ada1af34b26d7b6f029e5799813d12
Danil Somsikov <dsv@chromium.org>
2023-02-06 18:15:14 +0000

    Deflake response-received-extra-info-status-code.js
    Bug: 1412929
    Change-Id: I90a5e5f73de68f7cec6ea4f892ac71bef655f1a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224433
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101666}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/response-received-extra-info-status-code.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/response-received-extra-info-status-code.js
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.Network.enable();
   testRunner.log('Network Enabled');
 
-  dp.Network.onResponseReceivedExtraInfo(event => {
-    testRunner.log(event.params.statusCode);
-  });
-
-  await session.evaluateAsync(`fetch('index.html');`);
-  await session.evaluateAsync(
+  session.evaluateAsync(`fetch('index.html');`);
+  let event = await dp.Network.onceResponseReceivedExtraInfo();
+  testRunner.log(event.params.statusCode);
+  session.evaluateAsync(
       `fetch('/inspector-protocol/network/resources/hello-world.html');`);
+  event = await dp.Network.onceResponseReceivedExtraInfo();
+  testRunner.log(event.params.statusCode);
   testRunner.completeTest();
 })
