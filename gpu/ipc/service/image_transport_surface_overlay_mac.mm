b5f37871258c38986423bc6d06dd8f2209c1abe7
Tiago Vignatti <tvignatti@igalia.com>
2023-02-06 21:50:53 +0000

    [ios] Opt out GPU switch on iOS
    Bug: 1411704
    Change-Id: Ic79c7bb7220313dc7a5433c5bfcbf5f3dd9fd673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225997
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1101798}

--- gpu/ipc/service/image_transport_surface_overlay_mac.mm
+++ gpu/ipc/service/image_transport_surface_overlay_mac.mm
@@ -25,13 +25,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/video_types.h"
 #include "ui/gl/ca_renderer_layer_params.h"
-#include "ui/gl/gl_context.h"
 #include "ui/gl/gl_features.h"
 #include "ui/gl/gpu_switching_manager.h"
 
 #if BUILDFLAG(IS_MAC)
 #include "ui/accelerated_widget_mac/io_surface_context.h"
 #include "ui/base/cocoa/remote_layer_api.h"
+#include "ui/gl/gl_context.h"
 #include "ui/gl/scoped_cgl.h"
 #endif
 
@@ -299,6 +299,7 @@ bool ImageTransportSurfaceOverlayMacEGL::Resize(
 
 void ImageTransportSurfaceOverlayMacEGL::OnGpuSwitched(
     gl::GpuPreference active_gpu_heuristic) {
+#if BUILDFLAG(IS_MAC)
   // Create a new context, and use the GL renderer ID that the new context gets.
   scoped_refptr<ui::IOSurfaceContext> context_on_new_gpu =
       ui::IOSurfaceContext::Get(ui::IOSurfaceContext::kCALayerContext);
@@ -318,6 +319,7 @@ void ImageTransportSurfaceOverlayMacEGL::OnGpuSwitched(
   // surface that is observing the GPU switch.
   base::SingleThreadTaskRunner::GetCurrentDefault()->ReleaseSoon(
       FROM_HERE, std::move(context_on_new_gpu));
+#endif
 }
 
 void ImageTransportSurfaceOverlayMacEGL::SetCALayerErrorCode(
