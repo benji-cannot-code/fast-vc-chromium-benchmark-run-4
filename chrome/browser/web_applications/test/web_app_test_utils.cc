e2cbcb9e664496adab57e1ab7c6a5f9723fca410
Lingqi Chi <lingqi@chromium.org>
2023-02-06 04:27:00 +0000

    Revert "iwa: Remove WebApp::is_storage_isolated_"
    This reverts commit a3abed430fa5cb26f4dc10414f26e4c116265015.
    
    Reason for revert:
    Linux CFI and linux-ubsan-vptr are failing.
    See https://crbug.com/1413071 for details.
    
    Original change's description:
    > iwa: Remove WebApp::is_storage_isolated_
    >
    > This field reflected the isolated_storage manifest field, which was
    > removed in crrev.com/c/4214426.
    >
    > Bug: 1412210
    > Change-Id: I60aedec02fe4bb1349cdb675a12003911a3d53e2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4215562
    > Reviewed-by: Phillis Tang <phillis@chromium.org>
    > Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1101280}
    
    Bug: 1412210, 1413071
    Change-Id: Ia96afb871de5434836307024e48a055c8e4f6494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4223652
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Quick-Run: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101434}

--- chrome/browser/web_applications/test/web_app_test_utils.cc
+++ chrome/browser/web_applications/test/web_app_test_utils.cc
@@ -609,6 +609,8 @@ std::unique_ptr<WebApp> CreateRandomWebApp(const GURL& base_url,
   }
   app->SetDisallowedLaunchProtocols(std::move(disallowed_launch_protocols));
 
+  app->SetStorageIsolated(random.next_bool());
+
   app->SetWindowControlsOverlayEnabled(false);
 
   WebApp::SyncFallbackData sync_fallback_data;
