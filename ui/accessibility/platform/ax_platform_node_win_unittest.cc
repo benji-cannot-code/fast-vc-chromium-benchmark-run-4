4836877893bdd9cd46fd3eeae34306aae77f96c9
Kush Sinha <sinhak@chromium.org>
2023-02-06 17:35:41 +0000

    CodeHealth: Migrate `base::Value::FindKey()` to v2 API
    Migrate `base::Value::FindKey()` to `base::Value::Dict::Find()` and
    `base::Value::Dict::contains()` in -
    /ui/accessibility/platform
    
    This CL was uploaded by git cl split.
    
    R=mrobinson@igalia.com
    
    Bug: 1406815
    AX-Relnotes: n/a.
    Change-Id: Ie6ff09b52f5d2c4bff0b9514496606f668ff334c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201445
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101643}

--- ui/accessibility/platform/ax_platform_node_win_unittest.cc
+++ ui/accessibility/platform/ax_platform_node_win_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/auto_reset.h"
+#include "base/check_deref.h"
 #include "base/containers/contains.h"
 #include "base/json/json_reader.h"
 #include "base/run_loop.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "base/values.h"
 #include "base/win/atl.h"
 #include "base/win/scoped_bstr.h"
 #include "base/win/scoped_safearray.h"
@@ -7832,11 +7834,12 @@ TEST_F(AXPlatformNodeWinTest, DISABLED_BulkFetch) {
 
   // Note: base::JSONReader is fine for unit tests, but production code
   // that parses untrusted JSON should always use DataDecoder instead.
-  absl::optional<base::Value> result =
+  absl::optional<base::Value> result_val =
       base::JSONReader::Read(response, base::JSON_ALLOW_TRAILING_COMMAS);
-  ASSERT_TRUE(result);
-  ASSERT_TRUE(result->FindKey("role"));
-  ASSERT_EQ("scrollBar", result->FindKey("role")->GetString());
+  ASSERT_TRUE(result_val);
+  const base::Value::Dict& result = result_val->GetDict();
+  ASSERT_TRUE(result.contains("role"));
+  ASSERT_EQ("scrollBar", CHECK_DEREF(result.FindString("role")));
 }
 
 TEST_F(AXPlatformNodeWinTest, AsyncHitTest) {
