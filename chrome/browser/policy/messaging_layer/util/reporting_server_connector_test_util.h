7a5b9296aa77c87fb8cde400e7ca3725dde8c102
Leonid Baraz <lbaraz@chromium.org>
2023-02-06 22:11:11 +0000

    Improve ReportingServerConnector coverage
    Improve coverage of reporting_server_connector.cc  (from 62% to 69%)
    by refactoring ReportingServerConnector and its TestEnvironment.
    
    Non-covered code is now mostly failure handling.
    
    Attempted to cover callbacks and UMA by creating a local connector instance, but it conflicts with the existing singleton, so had to be reverted.
    
    Bug: b:259564655
    Change-Id: I92b13a7c566244a594137cf8a186b6bbf0200e31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4222137
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101812}

--- chrome/browser/policy/messaging_layer/util/reporting_server_connector_test_util.h
+++ chrome/browser/policy/messaging_layer/util/reporting_server_connector_test_util.h
@@ -9,21 +9,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/policy/messaging_layer/util/reporting_server_connector.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
+#include "components/policy/core/common/cloud/cloud_policy_core.h"
 #include "components/policy/core/common/cloud/mock_cloud_policy_client.h"
 
 namespace reporting {
 
 class ReportingServerConnector::TestEnvironment {
  public:
-  explicit TestEnvironment(::policy::MockCloudPolicyClient* client);
+  TestEnvironment();
   TestEnvironment(const TestEnvironment& other) = delete;
   TestEnvironment& operator=(const TestEnvironment& other) = delete;
   ~TestEnvironment();
 
+  ::policy::MockCloudPolicyClient* client() const;
+
  private:
-  const raw_ptr<::policy::MockCloudPolicyClient> test_client_;
-  const raw_ptr<::policy::CloudPolicyClient> saved_client_;
-  const raw_ptr<::policy::CloudPolicyCore> saved_core_;
+  std::unique_ptr<::policy::CloudPolicyStore> store_;
+  std::unique_ptr<::policy::CloudPolicyCore> core_;
 };
 }  // namespace reporting
 
--- chrome/browser/policy/messaging_layer/util/reporting_server_connector_unittest.cc
+++ chrome/browser/policy/messaging_layer/util/reporting_server_connector_unittest.cc
@@ -44,7 +44,7 @@ class ReportingServerConnectorTest : public ::testing::Test {
     // Prepare to respond to `ReportingServerConnector::UploadEncryptedReport`.
     // Callback is expected to be called exactly once on UI task runner,
     // regardless of the launching thread.
-    EXPECT_CALL(mock_client_, UploadEncryptedReport(_, _, _))
+    EXPECT_CALL(*test_env_.client(), UploadEncryptedReport(_, _, _))
         .WillOnce(WithArg<2>(
             Invoke([](::policy::CloudPolicyClient::ResponseCallback callback) {
               ASSERT_TRUE(::content::BrowserThread::CurrentlyOn(
@@ -54,8 +54,7 @@ class ReportingServerConnectorTest : public ::testing::Test {
   }
   content::BrowserTaskEnvironment task_environment_;
 
-  policy::MockCloudPolicyClient mock_client_;
-  ReportingServerConnector::TestEnvironment test_env_{&mock_client_};
+  ReportingServerConnector::TestEnvironment test_env_;
 };
 
 TEST_F(ReportingServerConnectorTest,
