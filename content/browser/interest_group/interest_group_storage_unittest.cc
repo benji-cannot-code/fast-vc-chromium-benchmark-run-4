8a693b053807b30bc606385876d38bcdb8e31448
Liam Brady <lbrady@google.com>
2023-02-06 07:09:25 +0000

    Add size-related fields to joinAdInterestGroup().
    This CL adds 2 new size-related fields to joinAdInterestGroup:
    - adSizes: a dictionary that maps size names to a dictionary containing
    a width and height string. The width and height are expected to be
    in the format "##units". Acceptable values include "10px" and "200sw".
    The values must be > 0. Disallowed values include "-20px" and "0sw".
    - sizeGroups: a dictionary that maps group names to a list of size names
    
    This adds database support for this fields as well as parsing the fields
    and passing them over mojo. Right now, the field has no effect when
    running auctions.
    
    See Turtledove issue: https://github.com/WICG/turtledove/issues/312
    See Turtledove PR: https://github.com/WICG/turtledove/pull/417
    
    Fuchsia-Binary-Size: Size increase is unavoidable due to new Mojo types.
    Binary-Size: Size increase is unavoidable due to new Mojo types.
    Change-Id: I32f1fbb11988793172e7dbbbc50fbd9f86827ab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4167800
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101466}

--- content/browser/interest_group/interest_group_storage_unittest.cc
+++ content/browser/interest_group/interest_group_storage_unittest.cc
@@ -125,7 +125,18 @@ class InterestGroupStorageTest : public testing::Test {
             blink::InterestGroup::Ad(
                 GURL("https://full.example.com/adcomponent1"), "metadata1c"),
             blink::InterestGroup::Ad(
-                GURL("https://full.example.com/adcomponent2"), "metadata2c")});
+                GURL("https://full.example.com/adcomponent2"), "metadata2c")},
+        /*ad_sizes=*/
+        {{{"size_1", blink::InterestGroup::Size(
+                         300, blink::InterestGroup::Size::LengthUnit::kPixels,
+                         150, blink::InterestGroup::Size::LengthUnit::kPixels)},
+          {"size_2",
+           blink::InterestGroup::Size(
+               640, blink::InterestGroup::Size::LengthUnit::kPixels, 480,
+               blink::InterestGroup::Size::LengthUnit::kPixels)}}},
+        /*size_groups=*/
+        {{{"group_1", std::vector<std::string>{"size_1"}},
+          {"group_2", std::vector<std::string>{"size_1", "size_2"}}}});
 
     std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
 
@@ -1112,6 +1123,10 @@ TEST_F(InterestGroupStorageTest, UpgradeFromV6) {
                                 Field("metadata", &InterestGroup::Ad::metadata,
                                       "[\"4\",\"5\",null,\"6\"]"))))),
                   Field("ad_components", &InterestGroup::ad_components,
+                        absl::nullopt),
+                  Field("ad_sizes", &InterestGroup::ad_components,
+                        absl::nullopt),
+                  Field("size_groups", &InterestGroup::ad_components,
                         absl::nullopt))),
           Field(
               "bidding_browser_signals",
@@ -1200,6 +1215,10 @@ TEST_F(InterestGroupStorageTest, UpgradeFromV6) {
                                 Field("metadata", &InterestGroup::Ad::metadata,
                                       "[\"4\",\"5\",null,\"6\"]"))))),
                   Field("ad_components", &InterestGroup::ad_components,
+                        absl::nullopt),
+                  Field("ad_sizes", &InterestGroup::ad_components,
+                        absl::nullopt),
+                  Field("size_groups", &InterestGroup::ad_components,
                         absl::nullopt))),
           Field(
               "bidding_browser_signals",
@@ -1288,6 +1307,10 @@ TEST_F(InterestGroupStorageTest, UpgradeFromV6) {
                                 Field("metadata", &InterestGroup::Ad::metadata,
                                       "[\"4\",\"5\",null,\"6\"]"))))),
                   Field("ad_components", &InterestGroup::ad_components,
+                        absl::nullopt),
+                  Field("ad_sizes", &InterestGroup::ad_components,
+                        absl::nullopt),
+                  Field("size_groups", &InterestGroup::ad_components,
                         absl::nullopt))),
           Field(
               "bidding_browser_signals",
