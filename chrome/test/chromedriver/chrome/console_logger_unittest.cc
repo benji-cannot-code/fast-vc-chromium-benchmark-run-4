4f3cf5b2943049c7c67e0b2432ad14660b98d94e
Tom Sepez <tsepez@chromium.org>
2023-02-06 19:02:20 +0000

    Avoid spurious danling ptr warning in ConsoleLogger.ConsoleMessages
    Re-order local variable destruction order so that the logger outlives
    the client which maintains a reference back to it.
    
    Bug: 1401495
    Change-Id: I94cb379166340871e7f0b495e5828e82557f30d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225373
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101699}

--- chrome/test/chromedriver/chrome/console_logger_unittest.cc
+++ chrome/test/chromedriver/chrome/console_logger_unittest.cc
@@ -142,10 +142,9 @@ void ConsoleLogParams(base::Value::Dict* out_dict,
 }  // namespace
 
 TEST(ConsoleLogger, ConsoleMessages) {
-  FakeDevToolsClient client("webview");
   FakeLog log;
   ConsoleLogger logger(&log);
-
+  FakeDevToolsClient client("webview");
   client.AddListener(&logger);
   logger.OnConnected(&client);
   EXPECT_EQ("Log.enable", client.PopSentCommand());
