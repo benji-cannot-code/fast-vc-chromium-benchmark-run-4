a3e23730243c54ce35e6bb9d449714be39f1b5ba
Xinyi Ji <xinyiji@google.com>
2023-02-06 22:39:42 +0000

    [Start] Build histograms for click rates of Tab switcher button on Start and NTP
    In this CL, we completed the two histograms(StartSurface.Module.Click and NewTabPage.Module.Click) by recording the clicking actions on the Tab switcher button on Start/NTP and the tests for these 2 sub-histograms. The histograms are recorded when the user clicks on the Tab switcher button.
    
    Bug: 1384156
    Change-Id: I942ce531e4fc7316c46b4a221651916fc15d23ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201005
    Commit-Queue: Xinyi Ji <xinyiji@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101823}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -816,11 +816,10 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
             return;
         }
 
-        if (isInOverviewMode() && !ReturnToChromeUtil.isStartSurfaceEnabled(this)) {
-            hideOverview();
-        } else {
-            showOverview(StartSurfaceState.SHOWING_TABSWITCHER);
-        }
+        ReturnToChromeUtil.recordClickTabSwitcher(
+                isInOverviewMode(), getTabModelSelector().getCurrentTab());
+
+        showOverview(StartSurfaceState.SHOWING_TABSWITCHER);
     }
 
     private void initializeToolbarManager() {
