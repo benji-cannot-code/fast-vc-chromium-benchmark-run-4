bc205ed8bd2a7f7ad79319a2c329e7ecb1bda3bc
Kent Tamura <tkent@chromium.org>
2023-02-06 22:44:39 +0000

    NoCopyBack: Fix a bad cast in LayoutMultiColumnFlowThread::ComputeSize()
    During FinishLayoutFromNG() for a LayoutMultiColumnFlowThread,
    ComputeSize() for an ancestor LayoutMultiColumnFlowThread was called.
    It didn't work well.
    
    This CL avoids it by skipping UpdateLogicalWidth() during
    FinishLayoutFromNG().
    
    
    Bug: 1412020
    Change-Id: Iadfcd5e1aef3a0347fef5ffff3f0493ff229d78d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4216291
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101825}

--- third_party/blink/renderer/core/layout/layout_multi_column_flow_thread.cc
+++ third_party/blink/renderer/core/layout/layout_multi_column_flow_thread.cc
@@ -1549,6 +1549,7 @@ void LayoutMultiColumnFlowThread::ComputeLogicalHeight(
 
 void LayoutMultiColumnFlowThread::UpdateLogicalWidth() {
   NOT_DESTROYED();
+  DCHECK(!RuntimeEnabledFeatures::LayoutNGNoCopyBackEnabled());
   LayoutUnit column_width;
   CalculateColumnCountAndWidth(column_width, column_count_);
   SetLogicalWidth(column_width);
