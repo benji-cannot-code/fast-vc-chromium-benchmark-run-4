68f68004cfc0885d15dd24be95432954769c05a1
Dustin J. Mitchell <djmitche@chromium.org>
2023-02-07 00:57:44 +0000

    Keep a StorageKey in BackgroundFetchMetadata
    This structure previously held only an origin, with conversions to/from
    StorageKey. This loses storage-partitioning information such as
    `top_level_site` and `ancestor_chain_bit`.
    
    With this change, the entire StorageKey is serialized into the metadata.
    
    This metadata is stored on-disk, so the implementation must also handle
    "old" values not containing a storage key. It handles this case by
    generating a storage key from the existing `origin` field, which was
    the existing behavior.
    
    If both fields are missing, then the existing behavior remains: the
    situation is interpreted as a storage key with an opaque origin.
    
    Bug: 1402863
    Change-Id: I5787acad27028e2d41fc030a7bc73a05c74b5c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221513
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101891}

--- content/browser/background_fetch/storage/database_helpers_unittest.cc
+++ content/browser/background_fetch/storage/database_helpers_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -33,6 +34,26 @@ TEST(BackgroundFetchDatabaseHelpers, CacheUrlRoundTrip) {
   EXPECT_TRUE(CacheUrlRoundTrip("https://example.com/path1/path2"));
   EXPECT_TRUE(CacheUrlRoundTrip("https://example.com/path?a=b&c=d"));
   EXPECT_TRUE(CacheUrlRoundTrip("https://example.com/path/?a=b&c=d"));
+}
+
+TEST(BackgroundFetchDatabaseHelpers, GetMetadataStorageKeyFromStorageKey) {
+  blink::StorageKey key(url::Origin::Create(GURL("http://example.com")));
+  proto::BackgroundFetchMetadata metadata_proto;
+  metadata_proto.set_storage_key(key.Serialize());
+  EXPECT_EQ(GetMetadataStorageKey(metadata_proto).Serialize(), key.Serialize());
+}
+
+TEST(BackgroundFetchDatabaseHelpers, GetMetadataStorageKeyFromOrigin) {
+  auto origin = url::Origin::Create(GURL("http://example.com"));
+  blink::StorageKey key(origin);
+  proto::BackgroundFetchMetadata metadata_proto;
+  metadata_proto.set_origin(origin.Serialize());
+  EXPECT_EQ(GetMetadataStorageKey(metadata_proto).Serialize(), key.Serialize());
+}
+
+TEST(BackgroundFetchDatabaseHelpers, GetMetadataStorageKeyNoData) {
+  proto::BackgroundFetchMetadata metadata_proto;
+  EXPECT_TRUE(GetMetadataStorageKey(metadata_proto).origin().opaque());
 }
 
 }  // namespace
