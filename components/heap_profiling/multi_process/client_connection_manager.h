4bf9cabe43a9938188ab4ebd0ff55c71e56b0636
Mike Wittman <wittman@chromium.org>
2023-02-07 00:50:01 +0000

    Reland "[code health] Remove host created notification use by ClientConnectionManager"
    This reverts commit 1ea45d11b9dadfa8467d3578c816047fbf4d9453.
    
    Reason for revert: the crashes were attributable to https://chromium-review.googlesource.com/c/chromium/src/+/4179204 rather than this CL
    
    Original change's description:
    > Revert "[code health] Remove host created notification use by ClientConnectionManager"
    >
    > This reverts commit fa3c9b5f4fa49c8143af3948ca8ad343c472ff38.
    >
    > Reason for revert: may be causing crashes on creation observation call
    >
    > Original change's description:
    > > [code health] Remove host created notification use by ClientConnectionManager
    > >
    > > Removes the use of the render process host created notification
    > > in ClientConnectionManager. Usage is replaced with the
    > > RenderProcessHostCreationObserver.
    > >
    > > Bug: 357627
    > > Change-Id: Id00aa4ffd4602f0a4c9bcb959aff1e807978df50
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179548
    > > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1095271}
    >
    > Bug: 357627, 1409290
    > Change-Id: Ic0fa41068d66fc2d91381912a655e400f2b221bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189878
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095834}
    
    Bug: 357627, 1409290
    Change-Id: Ia0a5df57b133353a480c026d4968df2d1f4639b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200541
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1101884}

--- components/heap_profiling/multi_process/client_connection_manager.h
+++ components/heap_profiling/multi_process/client_connection_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/child_process_data.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/render_process_host_creation_observer.h"
 
 namespace content {
 class RenderProcessHost;
@@ -42,8 +43,10 @@ enum class Mode;
 // This class can be subclassed for exactly one reason: to allow embedders to
 // override AllowedToProfileRenderer in order to prevent incognito renderers
 // from being profiled.
-class ClientConnectionManager : public content::BrowserChildProcessObserver,
-                                content::NotificationObserver {
+class ClientConnectionManager
+    : public content::BrowserChildProcessObserver,
+      public content::RenderProcessHostCreationObserver,
+      content::NotificationObserver {
  public:
   // The owner of this instance must guarantee that |controller_| outlives this
   // class.
@@ -87,6 +90,9 @@ class ClientConnectionManager : public content::BrowserChildProcessObserver,
 
   void StartProfilingNonRendererChild(const content::ChildProcessData& data);
 
+  // content::RenderProcessHostCreationObserver
+  void OnRenderProcessHostCreated(content::RenderProcessHost* host) override;
+
   // NotificationObserver
   // Observe connection of renderer child processes.
   void Observe(int type,
@@ -109,11 +115,11 @@ class ClientConnectionManager : public content::BrowserChildProcessObserver,
   // only be accessed on the UI thread and their values should be considered
   // opaque.
   //
-  // Semantically, the elements must be something that identifies which
-  // specific RenderProcess is being profiled. When the underlying RenderProcess
-  // goes away, the element must be removed. The RenderProcessHost
-  // pointer and the NOTIFICATION_RENDERER_PROCESS_CREATED notification can be
-  // used to provide these semantics.
+  // Semantically, the elements must be something that identifies which specific
+  // RenderProcess is being profiled. When the underlying RenderProcess goes
+  // away, the element must be removed. The RenderProcessHost pointer and the
+  // RenderProcessHostCreationObserver notification can be used to provide these
+  // semantics.
   //
   // This variable represents renderers that have been instructed to start
   // profiling - it does not reflect whether a renderer is currently still being
