d2afad0122bdb72c06c91af4c7e9a31f910ab10f
Nihar Damarasingu <nihardamar@google.com>
2023-02-07 18:06:08 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 42ac0c1e812c9c3d0735a69caaf97771bc36da38
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I61d15036dff90c42e15995797d24e6450f2776a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226473
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Nihar Damarasingu <nihardamar@google.com>
    Commit-Queue: Nihar Damarasingu <nihardamar@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102239}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/testloader.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/testloader.py
@@ -12,6 +12,7 @@ from collections import defaultdict, deque, namedtuple
 
 from . import manifestinclude
 from . import manifestexpected
+from . import manifestupdate
 from . import mpcontext
 from . import wpttest
 from mozlog import structured
@@ -268,7 +269,7 @@ class TestLoader:
     def get_test(self, manifest_file, manifest_test, inherit_metadata, test_metadata):
         if test_metadata is not None:
             inherit_metadata.append(test_metadata)
-            test_metadata = test_metadata.get_test(manifest_test.id.rpartition('/')[-1])
+            test_metadata = test_metadata.get_test(manifestupdate.get_test_name(manifest_test.id))
 
         return wpttest.from_manifest(manifest_file, manifest_test, inherit_metadata, test_metadata)
 
