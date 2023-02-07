215a0010128dacaec69b83c251df616e116611bb
Yuki Awano <yawano@google.com>
2023-02-07 00:36:01 +0000

    Add spaces between 100 and GB
    - This is a string update.
    
    Bug: b:261644086
    Change-Id: I6ce1e5e9bf238a70f1be9e816b062f38bb107f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227093
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Yuki Awano <yawano@google.com>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Quick-Run: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101871}

--- chrome/browser/ui/ash/google_one_offer_iph_tab_helper_constants.h
+++ chrome/browser/ui/ash/google_one_offer_iph_tab_helper_constants.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Fallback texts are used if params are not provided, e.g. IPH demo mode. We
 // won't expect that those fallback texts are used in prod.
 constexpr char kFallbackNotificationDisplaySource[] = "ChromeOS Perks";
-constexpr char kFallbackNotificationTitle[] = "Get 100GB of cloud storage";
+constexpr char kFallbackNotificationTitle[] = "Get 100 GB of cloud storage";
 constexpr char kFallbackNotificationMessage[] =
-    "Your Chromebook comes with 100GB for files and photos, free of charge for "
-    "12 months";
+    "Your Chromebook comes with 100 GB for files and photos, free of charge "
+    "for 12 months";
 constexpr char kFallbackGetPerkButtonTitle[] = "Get storage";
 
 static constexpr char kIPHGoogleOneOfferNotificationId[] =
