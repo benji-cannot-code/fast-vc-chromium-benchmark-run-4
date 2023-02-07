2130746d3ae8cf5ffa9321e0a77e65c612440c01
Caroline Rising <corising@chromium.org>
2023-02-07 02:00:24 +0000

    Notes: allow notes to be sorted in the side panel ui.
    Add a sort button with a menu containing options to sort by newest or
    oldest. Move the notes list ui into a separate file. This moves the
    persistent entry note into the dom-repeat so that it can be included
    when sorting happens. This also moves the filter icon from bookmarks
    into a place where it can be used by other side panels.
    
    Bug: 1394044
    Change-Id: I593a9a3d55322a60a06ccb33e336f4296ceb89c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198336
    Reviewed-by: John Lee <johntlee@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101935}

--- chrome/browser/resources/side_panel/user_notes/user_note.ts
+++ chrome/browser/resources/side_panel/user_notes/user_note.ts
@@ -34,7 +34,7 @@ export class UserNoteElement extends PolymerElement {
   static get properties() {
     return {
       /**
-       * The `note` is undefined if the UserNoteElement is the persistent entry
+       * The `note` is null if the UserNoteElement is the persistent entry
        * note and not a preexisting note.
        */
       note: {
@@ -65,7 +65,7 @@ export class UserNoteElement extends PolymerElement {
     };
   }
 
-  note?: Note;
+  note: Note|null;
   private characterCounter_: string;
   private editing_: boolean;
   private noteContent_: string;
@@ -76,7 +76,7 @@ export class UserNoteElement extends PolymerElement {
 
   override ready() {
     super.ready();
-    this.editing_ = this.note === undefined;
+    this.editing_ = this.note === null;
   }
 
   private onNoteContentInput_() {
@@ -94,6 +94,10 @@ export class UserNoteElement extends PolymerElement {
   private onNoteChanged_() {
     if (this.note) {
       this.$.noteContent.textContent = this.note.text;
+      this.editing_ = false;
+    } else {
+      this.clearInput_();
+      this.editing_ = true;
     }
     this.onNoteContentInput_();
   }
@@ -108,22 +112,22 @@ export class UserNoteElement extends PolymerElement {
   }
 
   private onCancelClick_() {
-    if (this.note === undefined) {
+    if (this.note === null) {
       this.clearInput_();
     } else {
-      this.$.noteContent.textContent = this.note.text;
+      this.$.noteContent.textContent = this.note!.text;
       this.onNoteContentInput_();
       this.editing_ = false;
     }
   }
 
   private async onAddClick_() {
-    if (this.note === undefined) {
+    if (this.note === null) {
       await this.userNotesApi_.newNoteFinished(this.$.noteContent.textContent!);
       this.clearInput_();
     } else {
       await this.userNotesApi_.updateNote(
-          this.note.guid, this.$.noteContent.textContent!);
+          this.note!.guid, this.$.noteContent.textContent!);
       this.editing_ = false;
     }
   }
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -317,7 +317,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "includes": [1933],
   },
   "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/side_panel/shared/resources.grd": {
-    "META": {"sizes": {"includes": [10],}},
+    "META": {"sizes": {"includes": [15],}},
     "includes": [1934],
   },
   "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/side_panel/user_notes/resources.grd": {
