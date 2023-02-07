e8536c1deab1f50fce27c6ccfc13b752ac04cc17
Ilkin Safarli <safarli@google.com>
2023-02-07 19:15:29 +0000

    ambient: Show cursor when screensaver widget is closed
    Fixes: b:267665038
    Change-Id: I66744ccf20ac16d196e940ebd7ed0a657508798d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4222331
    Commit-Queue: Ilkin Safarli <safarli@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102302}

--- ash/ambient/ambient_controller.cc
+++ ash/ambient/ambient_controller.cc
@@ -581,6 +581,7 @@ void AmbientController::CloseUi(bool immediately) {
 
   close_widgets_immediately_ = immediately;
   ambient_ui_model_.SetUiVisibility(AmbientUiVisibility::kClosed);
+  Shell::Get()->cursor_manager()->ShowCursor();
 }
 
 void AmbientController::ToggleInSessionUi() {
