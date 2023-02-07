b5c3d9617f0fdda674ae1c2dae5405a3069580e0
Lei Zhang <thestig@chromium.org>
2023-02-07 16:14:50 +0000

    Slightly simplify OnDidFetchCapabilitiesFromPrintBackendService().
    Remove a parameter that's always set to false.
    
    Change-Id: Iffa530489f2634a05dd144831a559bbc84b61f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226724
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1102172}

--- chrome/browser/ui/webui/print_preview/local_printer_handler_default.cc
+++ chrome/browser/ui/webui/print_preview/local_printer_handler_default.cc
@@ -101,7 +101,6 @@ void OnDidEnumeratePrintersFromPrintBackendService(
 void OnDidFetchCapabilitiesFromPrintBackendService(
     const std::string& device_name,
     bool elevated_privileges,
-    bool has_secure_protocol,
     PrinterHandler::GetCapabilityCallback callback,
     mojom::PrinterCapsAndInfoResultPtr printer_caps_and_info) {
   if (printer_caps_and_info->is_result_code()) {
@@ -126,8 +125,7 @@ void OnDidFetchCapabilitiesFromPrintBackendService(
           device_name,
           base::BindOnce(&OnDidFetchCapabilitiesFromPrintBackendService,
                          device_name,
-                         /*elevated_privileges=*/true, has_secure_protocol,
-                         std::move(callback)));
+                         /*elevated_privileges=*/true, std::move(callback)));
       return;
     }
 
@@ -142,7 +140,7 @@ void OnDidFetchCapabilitiesFromPrintBackendService(
       printer_caps_and_info->get_printer_caps_and_info();
   base::Value::Dict settings = AssemblePrinterSettings(
       device_name, caps_and_info->printer_info,
-      caps_and_info->user_defined_papers, has_secure_protocol,
+      caps_and_info->user_defined_papers, /*has_secure_protocol=*/false,
       &caps_and_info->printer_caps);
   std::move(callback).Run(std::move(settings));
 }
@@ -304,7 +302,7 @@ void LocalPrinterHandlerDefault::StartGetCapability(
                        device_name,
                        service_mgr.PrinterDriverFoundToRequireElevatedPrivilege(
                            device_name),
-                       /*has_secure_protocol=*/false, std::move(cb)));
+                       std::move(cb)));
     return;
   }
 #endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
