cb38ef6bedb9cf1dfeed365d860a2510755f8dbb
Dan Riley <danielriley@google.com>
2023-02-07 22:01:01 +0000

    Add missing language detection feature to WebView client
    The issues is reproducible when running ios_web_view_shell
    
    michaeldo:We should probably also merge this back to M111 to keep that branch in a functioning state
    
    BUG=chromium:1413829
    
    Change-Id: I6f6ffe7aae0029f259ed98f82f65acd1c7201b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4228332
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dan Riley <danielriley@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102401}

--- ios/web_view/internal/web_view_web_client.mm
+++ ios/web_view/internal/web_view_web_client.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/ios/browser/autofill_java_script_feature.h"
 #import "components/autofill/ios/browser/suggestion_controller_java_script_feature.h"
 #import "components/autofill/ios/form_util/form_handlers_java_script_feature.h"
+#import "components/language/ios/browser/language_detection_java_script_feature.h"
 #import "components/password_manager/ios/password_manager_java_script_feature.h"
 #import "components/security_interstitials/core/unsafe_resource.h"
 #include "components/ssl_errors/error_info.h"
@@ -99,6 +100,7 @@ std::vector<web::JavaScriptFeature*> WebViewWebClient::GetJavaScriptFeatures(
   return {autofill::AutofillJavaScriptFeature::GetInstance(),
           autofill::FormHandlersJavaScriptFeature::GetInstance(),
           autofill::SuggestionControllerJavaScriptFeature::GetInstance(),
+          language::LanguageDetectionJavaScriptFeature::GetInstance(),
           password_manager::PasswordManagerJavaScriptFeature::GetInstance(),
           security_interstitials::IOSSecurityInterstitialJavaScriptFeature::
               GetInstance(),
