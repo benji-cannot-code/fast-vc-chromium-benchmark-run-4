9397d8a21b6099e0663d0c5d16b241eed296e76e
Yue Zhang <yuezhanggg@chromium.org>
2023-02-07 02:24:56 +0000

    [ChromeCart] Expose HasCartForURL API
    This CL exposes HasCartForURL API in CartService to support checking
    if there is an active abandoned cart for the domain of the URL. Please
    note that this API is asynchronous.
    
    Bug: 1412609
    Change-Id: I3239e9a24e47b427a3e88ef9fe0f6b345c8a27a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220268
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101945}

--- chrome/browser/cart/cart_service.h
+++ chrome/browser/cart/cart_service.h
@@ -158,6 +158,10 @@ class CartService : public history::HistoryServiceObserver,
   // * "utm_campaign=chrome-cart-discount-off" for partner merchant carts with
   //   discount disabled.
   const GURL AppendUTM(const GURL& base_url);
+  // Checks if there is an active abandoned cart for the domain of |url|, and
+  // returns the result in the callback.
+  void HasActiveCartForURL(const GURL& url,
+                           base::OnceCallback<void(bool)> callback);
 
  private:
   friend class CartServiceFactory;
@@ -249,6 +253,10 @@ class CartService : public history::HistoryServiceObserver,
   void RecordDiscountConsentStatusAtLoad(bool should_show_consent);
   // Checks if a cart has expired.
   bool IsCartExpired(const cart_db::ChromeCartContentProto& proto);
+  void HasActiveCartForURLCallback(
+      base::OnceCallback<void(bool)> callback,
+      bool success,
+      std::vector<CartDB::KeyAndValue> proto_pairs);
 
   raw_ptr<Profile> profile_;
   std::unique_ptr<CartDB> cart_db_;
