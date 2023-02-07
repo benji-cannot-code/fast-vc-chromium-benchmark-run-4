ae10d240390912af4dcd670356b89da422860aaa
Stefan Zager <szager@chromium.org>
2023-02-07 01:04:49 +0000

    Fix STACK_ALLOCATED usage
    STACK_ALLOCATED will be ported to base/ and enforced via new code in
    the chromium clang plugin:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/4137572
    
    The new implementation is somewhat stricter than the existing one; in
    particular, the existing blink clang plugin does not enforce
    STACK_ALLOCATED for types that don't participate in tracing/gc.
    
    This CL fixes the handful of places in the chromium code where the new
    implementation currently flags errors. The most significant change is
    in WTF hash iterators -- they are marked STACK_ALLOCATED to avoid the
    need for Trace() methods, but there are existing cases where iterators
    are used in a non-stack-allocated way -- for valid reasons that can't
    simply be refactored away -- with hash tables that don't store traceable
    values. This patch creates template specializations for the iterator
    types so that they will be STACK_ALLOCATED whenever the underlying
    hash table stores traceable types.
    
    Examples of persistent iterators can be found in:
    
    blink::EventCountsIterationSource
    blink::BluetoothManufacturerDataMapIterationSource
    blink::BluetoothServiceDataMapIterationSource
    blink::KeyboardLayoutMapIterationSource
    blink::StorageAreaMap
    blink::GPUSupportedFeatures
    
    Bug: 1409156
    Change-Id: I2171d42839de700eed1551969ce721cec30177b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225998
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1101895}

--- third_party/blink/renderer/platform/wtf/gc_plugin.h
+++ third_party/blink/renderer/platform/wtf/gc_plugin.h
@@ -12,9 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // where the bug describes what needs to happen to remove the GC_PLUGIN_IGNORE
 // again.
 #if defined(__clang__)
-#define GC_PLUGIN_IGNORE(reason) \
-  __attribute__((annotate("blink_gc_plugin_ignore")))
+#define STACK_ALLOCATED_IGNORE(reason) \
+  __attribute__((annotate("stack_allocated_ignore")))
+#define GC_PLUGIN_IGNORE(reason)                     \
+  __attribute__((annotate("blink_gc_plugin_ignore"), \
+                 annotate("stack_allocated_ignore")))
 #else  // !defined(__clang__)
+#define STACK_ALLOCATED_IGNORE(reason)
 #define GC_PLUGIN_IGNORE(reason)
 #endif  // !defined(__clang__)
 
