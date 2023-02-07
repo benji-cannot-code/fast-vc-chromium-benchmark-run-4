e0f0d39b949572207dbed625b6ec5761a70677ec
Maria Kazinova <kazinova@google.com>
2023-02-07 17:26:12 +0000

    [UPM-Desktop] Add favicon source to PasswordManager WebUI.
    This will make favicon loading more reliable, especially in the WebApp.
    Without this change PasswordManager page uses favicon source created
    by other Chrome elements. This can lead to bugs when the WebApp is
    opened and there are no other elements to add this source.
    
    Bug: 1350947
    Change-Id: Iccc9c471cc9be2e4095f394eb49f3f2b86b1fd59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225506
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102214}

--- chrome/browser/ui/webui/password_manager/password_manager_ui.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/managed_ui_handler.h"
 #include "chrome/browser/ui/webui/plural_string_handler.h"
 #include "chrome/browser/ui/webui/sanitized_image_source.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/password_manager_resources.h"
 #include "chrome/grit/password_manager_resources_map.h"
+#include "components/favicon_base/favicon_url_parser.h"
 #include "components/grit/components_scaled_resources.h"
 #include "components/password_manager/content/common/web_ui_constants.h"
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
@@ -215,6 +217,10 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
   source->AddResourcePath("images/password_manager_pwa_icon.svg",
                           IDR_CHROME_PASSWORD_MANAGER_PWA_ICON);
 #endif
+
+  content::URLDataSource::Add(
+      profile, std::make_unique<FaviconSource>(
+                   profile, chrome::FaviconUrlFormat::kFavicon2));
 
   return source;
 }
