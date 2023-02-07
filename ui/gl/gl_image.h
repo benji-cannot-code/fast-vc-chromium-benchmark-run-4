1a9916ce0ffc915c45e7c29d5b9c3fb142cf37d4
Colin Blundell <blundell@chromium.org>
2023-02-07 08:13:40 +0000

    [GL] Clean up includes in gl_image.h
    Clarifies what's still used.
    
    Bug: 1310018
    Change-Id: I0012c2a830a9bde2da7910619ab0749bc0cf2f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224334
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102029}

--- ui/gl/gl_image.h
+++ ui/gl/gl_image.h
@@ -6,19 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GL_GL_IMAGE_H_
 #define UI_GL_GL_IMAGE_H_
 
-#include <stdint.h>
-
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
-#include "build/build_config.h"
-#include "ui/gfx/buffer_types.h"
-#include "ui/gfx/geometry/point.h"
-#include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
-#include "ui/gfx/gpu_fence.h"
-#include "ui/gfx/native_widget_types.h"
-#include "ui/gfx/overlay_transform.h"
 #include "ui/gl/gl_export.h"
 
 namespace gpu {
--- ui/gl/gl_image_egl_pixmap.h
+++ ui/gl/gl_image_egl_pixmap.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/x/glx.h"
 #include "ui/gl/gl_export.h"
