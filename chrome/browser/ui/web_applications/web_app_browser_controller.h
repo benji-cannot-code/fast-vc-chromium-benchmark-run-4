e1c2b95cb0b0c599f5c78f8638f7d040a4bd7f4d
Grace Kan <gracekan@google.com>
2023-02-07 01:29:49 +0000

    Read Home Tab Icons From Disk Storage For Tabbed PWAs
    Home Tab and File Handling Icons have been successfully stored on the disk, however there exists no previous functionality to read them from it. A placeholder icon is required while an asynchronous request to read the real home tab icon is run.
    
    Design-Document: https://docs.google.com/document/d/1lG-IzUbq4dYgNcgyicLk0_RiGHZsJA6H1vv9fBjHd2w/edit?usp=sharing&resourcekey=0-b5ch9EAJKJ8n04rKeUqlTQ
    
    Change-Id: I28979604c867d748f15fd2e5bf9e4f54637d8b85
    Bug: 1381377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190757
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Louise Brett <loubrett@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Grace Kan <gracekan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101912}

--- chrome/browser/ui/web_applications/web_app_browser_controller.h
+++ chrome/browser/ui/web_applications/web_app_browser_controller.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/web_applications/app_registrar_observer.h"
+#include "chrome/browser/web_applications/web_app_icon_manager.h"
 #include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_app_install_manager.h"
 #include "chrome/browser/web_applications/web_app_install_manager_observer.h"
@@ -74,7 +75,10 @@ class WebAppBrowserController : public AppBrowserController,
   ~WebAppBrowserController() override;
 
   // AppBrowserController:
+  using HomeTabCallbackList = base::OnceCallbackList<void()>;
   bool HasMinimalUiButtons() const override;
+  bool DoesHomeTabIconExist() const;
+  gfx::ImageSkia GetHomeTabIcon() const;
   ui::ImageModel GetWindowAppIcon() const override;
   ui::ImageModel GetWindowIcon() const override;
   absl::optional<SkColor> GetThemeColor() const override;
@@ -120,6 +124,8 @@ class WebAppBrowserController : public AppBrowserController,
                                base::StringPiece old_name) override;
   void OnWebAppInstallManagerDestroyed() override;
 
+  base::CallbackListSubscription AddHomeTabIconLoadCallbackForTesting(
+      const base::OnceClosure callback);
   static void SetIconLoadCallbackForTesting(base::OnceClosure callback);
   static void SetManifestUpdateAppliedCallbackForTesting(
       base::OnceClosure callback);
@@ -130,14 +136,16 @@ class WebAppBrowserController : public AppBrowserController,
   void OnTabRemoved(content::WebContents* contents) override;
 
  private:
+  mutable HomeTabCallbackList home_tab_callback_list_;
   const WebAppRegistrar& registrar() const;
   const WebAppInstallManager& install_manager() const;
 
   // Helper function to call AppServiceProxy to load icon.
   void LoadAppIcon(bool allow_placeholder_icon) const;
+
   // Invoked when the icon is loaded.
   void OnLoadIcon(apps::IconValuePtr icon_value);
-
+  void OnReadHomeTabIcon(HomeTabIconBitmaps home_tab_icon_bitmaps) const;
   void OnReadIcon(IconPurpose purpose, SkBitmap bitmap);
   void PerformDigitalAssetLinkVerification(Browser* browser);
 
@@ -169,6 +177,7 @@ class WebAppBrowserController : public AppBrowserController,
   raw_ptr<const ash::SystemWebAppDelegate> system_app_;
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   mutable absl::optional<ui::ImageModel> app_icon_;
+  mutable absl::optional<gfx::ImageSkia> home_tab_icon_;
 
 #if BUILDFLAG(IS_CHROMEOS)
   // The result of digital asset link verification of the web app.
