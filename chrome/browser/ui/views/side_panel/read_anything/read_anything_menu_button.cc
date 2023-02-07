6a18b58b845ca5e0c5ca534a84ab83e7d4e23649
Kristi Saney <kristislee@google.com>
2023-02-07 18:47:15 +0000

    Update the ink drop color when the theme changes.
    demo: https://photos.app.goo.gl/gqr6rsFgKJ6GbHqe6
    
    Bug: 1266555
    Change-Id: Ifd5c94b5061bb53c1d6a7e1b76e5c1e59daca74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227493
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102273}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_menu_button.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_menu_button.cc
@@ -23,6 +23,7 @@ ReadAnythingMenuButton::ReadAnythingMenuButton(
                                      base::Unretained(this))) {
   ConfigureInkDropForToolbar(this);
   views::InstallCircleHighlightPathGenerator(this);
+  views::InkDrop::Get(this)->SetBaseColorCallback({});
   SetIcon(icon, kIconSize, gfx::kPlaceholderColor);
   SetAccessibleName(tooltip);
   SetTooltipText(tooltip);
@@ -63,6 +64,7 @@ void ReadAnythingMenuButton::SetIcon(const gfx::VectorIcon& icon,
   SetImageModel(views::Button::STATE_NORMAL,
                 ui::ImageModel::FromImageSkia(
                     gfx::CreateVectorIcon(icon, icon_size, icon_color)));
+  views::InkDrop::Get(this)->SetBaseColor(icon_color);
 }
 
 BEGIN_METADATA(ReadAnythingMenuButton, MenuButton)
