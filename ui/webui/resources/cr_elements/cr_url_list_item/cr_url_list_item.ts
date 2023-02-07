5178c743eae0234e88756c7c9b5aadbf031ce129
John Lee <johntlee@chromium.org>
2023-02-07 00:53:42 +0000

    Side Panel: Show folder count in cr-url-list-item
    Bug: 1402857
    Change-Id: Ieb9a66f5cf5711afc32f7c0ecd9a74a138142710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205397
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101887}

--- ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
+++ ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '../cr_hidden_style.css.js';
 import '../cr_icons.css.js';
 import '../cr_shared_vars.css.js';
 
@@ -57,6 +58,15 @@ export class CrUrlListItemElement extends CrUrlListItemElementBase {
   override ready() {
     super.ready();
     FocusOutlineManager.forDocument(document);
+  }
+
+  private getDisplayedCount_() {
+    if (this.count && this.count > 999) {
+      // The square to display the count only fits 3 characters.
+      return '99+';
+    }
+
+    return this.count;
   }
 
   private getFavicon_(): string {
