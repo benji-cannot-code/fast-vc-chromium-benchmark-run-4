8c20802b448af3215ec4caf6d6bf76b99735ea29
Federica Germinario <fedegermi@google.com>
2023-02-07 18:16:10 +0000

    [ios] Changed position of startTabLifeCycleMediator
    StartTabLifeCycleMediator should be called as soon as possible in
    browser_coordinator.mm.
    
    Change-Id: I791fb7fc40b8a6de3760533e07a5dade26c7a3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4217331
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1102250}

--- ios/chrome/browser/ui/browser_view/tab_lifecycle_mediator.mm
+++ ios/chrome/browser/ui/browser_view/tab_lifecycle_mediator.mm
@@ -50,9 +50,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   std::unique_ptr<WebStateDependencyInstallerBridge> _dependencyInstallerBridge;
 }
 
-- (void)startWithWebStateList:(WebStateList*)webStateList {
-  _dependencyInstallerBridge =
-      std::make_unique<WebStateDependencyInstallerBridge>(self, webStateList);
+- (instancetype)initWithWebStateList:(WebStateList*)webStateList {
+  if (self = [super init]) {
+    _dependencyInstallerBridge =
+        std::make_unique<WebStateDependencyInstallerBridge>(self, webStateList);
+  }
+  return self;
 }
 
 - (void)disconnect {
