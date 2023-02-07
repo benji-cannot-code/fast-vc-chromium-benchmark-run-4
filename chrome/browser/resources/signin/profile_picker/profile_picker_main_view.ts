260674b41bcdcc1a27fa43536578535cbe0a9428
Jack Yammine <jyammine@google.com>
2023-02-07 09:25:02 +0000

    [fyfre]Add tangible sync style to profile picker main view.
    Change the profile picker main view illustrations and title font-family when the tangible sync feature is enabled to match its style.
    
    Fixed: 1407190
    Change-Id: Icd50bd1b3ab9ef498551777ecb3d6af2f3a94c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224571
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102046}

--- chrome/browser/resources/signin/profile_picker/profile_picker_main_view.ts
+++ chrome/browser/resources/signin/profile_picker/profile_picker_main_view.ts
@@ -76,6 +76,13 @@ export class ProfilePickerMainViewElement extends
           return loadTimeData.getBoolean('askOnStartup');
         },
       },
+
+      isTangibleSyncEnabled_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('isTangibleSyncEnabled');
+        },
+      },
     };
   }
 
@@ -87,6 +94,7 @@ export class ProfilePickerMainViewElement extends
       ManageProfilesBrowserProxyImpl.getInstance();
   private resizeObserver_: ResizeObserver|null = null;
   private previousRoute_: Routes|null = null;
+  private isTangibleSyncEnabled_: boolean;
 
   override ready() {
     super.ready();
@@ -199,6 +207,10 @@ export class ProfilePickerMainViewElement extends
   private computeHideAskOnStartup_(): boolean {
     return !isAskOnStartupAllowed() || !this.profilesList_ ||
         this.profilesList_.length < 2;
+  }
+
+  private getTangibleSyncStyleClass_() {
+    return this.isTangibleSyncEnabled_ ? 'tangible-sync-style' : '';
   }
 }
 
