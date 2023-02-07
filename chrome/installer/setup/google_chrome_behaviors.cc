193b6f57065d4f0174bdd80005bb790dff0231ab
Avi Drissman <avi@chromium.org>
2023-02-07 00:46:19 +0000

    Remove Value methods that take dotted paths by span
    An earlier version of this CL was landed and reverted; this
    version includes a fix to code that was not included in the
    standard trybots.
    
    Bug: 646113
    Cq-Include-Trybots: luci.chrome.try:linux-chrome,mac-chrome,win-chrome
    Change-Id: Id34ea1ce098bfaa4d08bc62d17daad79f878c30a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221797
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101882}

--- chrome/installer/setup/google_chrome_behaviors.cc
+++ chrome/installer/setup/google_chrome_behaviors.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/strings/string_piece.h"
-#include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -84,12 +83,10 @@ bool IsMetricsEnabled(const base::FilePath& file_path) {
   if (!root || !root->is_dict())
     return false;
 
-  auto path =
-      base::SplitStringPiece(metrics::prefs::kMetricsReportingEnabled, ".",
-                             base::KEEP_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
-  const auto* value = root->FindPathOfType(path, base::Value::Type::BOOLEAN);
+  const absl::optional<bool> value = root->GetDict().FindBoolByDottedPath(
+      metrics::prefs::kMetricsReportingEnabled);
 
-  return value && value->GetBool();
+  return value.value_or(false);
 }
 
 }  // namespace
