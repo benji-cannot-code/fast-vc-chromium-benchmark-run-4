2020c8f8a910f4a69dc4a9d6b60e523fb8b81142
Jordan Bayles <jophba@chromium.org>
2023-02-07 04:10:56 +0000

    [Cast Streaming] Use proper remoting timebase
    Currently, the remoting sender completely ignores the frame sender
    configuration, and uses whatever RTP timebase it wants. This can cause
    problems for receivers that actually use the OFFER message
    configuration.
    
    This patch changes the remoting sender to properly use the RTP
    timestamp, and modifies the mirror settings to offer the RTP timebase
    that the remoting sender was previously using (90k for audio instead of
    the standard 48k for mirroring audio).
    
    Bug: 1413561
    Change-Id: I8d8923fcbf7ba1100352222d91c4c52ff035309d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220289
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101982}

--- components/mirroring/service/mirror_settings.cc
+++ components/mirroring/service/mirror_settings.cc
@@ -30,7 +30,7 @@ namespace {
 constexpr base::TimeDelta kDefaultPlayoutDelay = base::Milliseconds(400);
 
 constexpr int kAudioTimebase = 48000;
-constexpr int kVidoTimebase = 90000;
+constexpr int kVideoTimebase = 90000;
 constexpr int kAudioChannels = 2;
 constexpr int kAudioFramerate = 100;  // 100 FPS for 10ms packets.
 constexpr int kMinVideoBitrate = 300000;
@@ -96,7 +96,9 @@ FrameSenderConfig MirrorSettings::GetDefaultAudioConfig(
   config.min_playout_delay = playout_delay;
   config.max_playout_delay = playout_delay;
   config.rtp_payload_type = payload_type;
-  config.rtp_timebase = kAudioTimebase;
+  config.rtp_timebase = (payload_type == RtpPayloadType::REMOTE_AUDIO)
+                            ? media::cast::kRemotingRtpTimebase
+                            : kAudioTimebase;
   config.channels = kAudioChannels;
   config.min_bitrate = config.max_bitrate = config.start_bitrate =
       kAudioBitrate;
@@ -116,7 +118,9 @@ FrameSenderConfig MirrorSettings::GetDefaultVideoConfig(
   config.min_playout_delay = playout_delay;
   config.max_playout_delay = playout_delay;
   config.rtp_payload_type = payload_type;
-  config.rtp_timebase = kVidoTimebase;
+  config.rtp_timebase = (payload_type == RtpPayloadType::REMOTE_VIDEO)
+                            ? media::cast::kRemotingRtpTimebase
+                            : kVideoTimebase;
   config.channels = 1;
   config.min_bitrate = kMinVideoBitrate;
   config.max_bitrate = kMaxVideoBitrate;
