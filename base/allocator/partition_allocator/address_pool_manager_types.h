05f96fcb0f1be51ef319ae7c030c91594e98bdd3
Takashi Sakamoto <tasak@google.com>
2023-02-07 04:49:59 +0000

    [PA] Make pool_handle enum.
    Change-Id: Ied0f637fd3121a23f76904be1afd2e2b75b2e8b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220162
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1101992}

--- base/allocator/partition_allocator/address_pool_manager_types.h
+++ base/allocator/partition_allocator/address_pool_manager_types.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace partition_alloc::internal {
 
-using pool_handle = unsigned;
+enum pool_handle : unsigned;
 
 }  // namespace partition_alloc::internal
 
