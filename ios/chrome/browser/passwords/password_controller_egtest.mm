d46e868366bb5bc67736d7bdf022727989c81ab8
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-02-07 07:11:06 +0000

    Add fieldtrial testing config for IOSShowPasswordStorageInSaveInfobar
    Requires fixing one test suite for the feature enabled case.
    
    Bug: 1392708
    Change-Id: I43ef0ee5b4e37e7fa54e59a749e3ab240b7d69ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224079
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102014}

--- ios/chrome/browser/passwords/password_controller_egtest.mm
+++ ios/chrome/browser/passwords/password_controller_egtest.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
+#import "ios/testing/earl_grey/matchers.h"
 #import "net/test/embedded_test_server/default_handlers.h"
 #import "ui/base/l10n/l10n_util.h"
 
@@ -32,23 +33,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 constexpr char kFormUsername[] = "un";
 constexpr char kFormPassword[] = "pw";
-NSString* const kSavedCredentialLabel = @"Eguser, Hidden, Password";
 
 namespace {
 
 using base::test::ios::kWaitForActionTimeout;
 using base::test::ios::kWaitForUIElementTimeout;
 using base::test::ios::WaitUntilConditionOrTimeout;
-using base::test::ios::kWaitForActionTimeout;
 using chrome_test_util::TapWebElementWithId;
 using chrome_test_util::UseSuggestedPasswordMatcher;
+using testing::ElementWithAccessibilityLabelSubstring;
 
 id<GREYMatcher> PasswordInfobar(int prompt_id) {
-  NSString* bannerLabel =
-      [NSString stringWithFormat:@"%@,%@", l10n_util::GetNSString(prompt_id),
-                                 kSavedCredentialLabel];
-  return grey_allOf(grey_accessibilityID(kInfobarBannerViewIdentifier),
-                    grey_accessibilityLabel(bannerLabel), nil);
+  return grey_allOf(
+      grey_accessibilityID(kInfobarBannerViewIdentifier),
+      ElementWithAccessibilityLabelSubstring(l10n_util::GetNSString(prompt_id)),
+      nil);
 }
 
 id<GREYMatcher> PasswordInfobarButton(int button_id) {
