af9810e07316ca9794185ace78cb5d8c8367468e
Charles Hager <clhager@google.com>
2023-02-07 21:52:47 +0000

    Hide download sharing on automotive
    Share buttons hidden on download list item more menu and SelectableListToolbar
    
    Selectable List Top Toolbar: https://screenshot.googleplex.com/4XCoCzgjpy3z8o2.png
    
    List Item More Menu:
    https://screenshot.googleplex.com/79ARNsJ6fKJEgKi.png
    
    Bug: 1408145
    Change-Id: I00fe52ad8709f287ee721eb318831b79e432c09c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4199210
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102396}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/list/UiUtils.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/list/UiUtils.java
@@ -12,6 +12,7 @@ import android.text.format.Formatter;
 
 import androidx.annotation.DrawableRes;
 
+import org.chromium.base.BuildInfo;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.MathUtils;
 import org.chromium.chrome.browser.download.StringUtils;
@@ -355,6 +356,11 @@ public final class UiUtils {
 
     /** @return Whether the given {@link OfflineItem} can be shared. */
     public static boolean canShare(OfflineItem item) {
+        // Sharing functionality that leads directly to the Android share sheet is
+        // currently disabled.
+        if (BuildInfo.getInstance().isAutomotive) {
+            return false;
+        }
         return (item.state == OfflineItemState.COMPLETE)
                 && (LegacyHelpers.isLegacyDownload(item.id)
                         || LegacyHelpers.isLegacyOfflinePage(item.id));
