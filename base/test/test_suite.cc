48d1284a4921e1f2461074d5d70092e3110407d1
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-02-07 19:21:17 +0000

    Revert "Reconfigure PartitionAlloc to enable BackupRefPtr in every test."
    This reverts commit 74efe47a6f644ef69c51b086973666af50d5fb75.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8789793708874874961
    
    Sample failed build: https://ci.chromium.org/b/8789793708874874961
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8789793708874874961&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4194138
    
    Original change's description:
    > Reconfigure PartitionAlloc to enable BackupRefPtr in every test.
    >
    > Previously, it was only enabled when executing ContentMain. Most of the
    > unittests are more lightweight and do not call it.
    >
    > This patch enables it.
    > As a side effect, it also enables UnretainedDanglingPointer.
    >
    > I had to keep some exceptions:
    > - Skip Windows: There are bugs detected. We need to fix them.
    > - Skip Android: There are bugs detected. We need to fix them.
    > - Skip DanglingPointerDetector: not all the dangling pointers in
    >   unittests have been triaged yet.
    > - Skip ASAN: some AsanBackupRefPtrImpl are failing.
    >
    > I am hoping landing this patch early could help investigating BRP
    > blockers.
    >
    > Bug: 1400058
    > Cq-Include-Trybots: luci.chromium.try:android-pie-arm64-dbg,android-x64-cast,android_compile_dbg,android_compile_x64_dbg,android_compile_x86_dbg,android_cronet,chromeos-amd64-generic-dbg,chromeos-arm-generic-rel,dawn-android-arm-deps-rel,dawn-mac-x64-deps-rel,fuchsia-arm64-rel,fuchsia-x64-cast-receiver-rel,ios-simulator,lacros-amd64-generic-rel,lacros-arm-generic-rel,linux-chromeos-compile-dbg,linux-libfuzzer-asan-rel,linux_chromium_asan_rel_ng,linux_chromium_compile_dbg_ng,linux_chromium_tsan_rel_ng,linux_vr,mac_chromium_compile_dbg_ng,win-libfuzzer-asan-rel,win_chromium_compile_dbg_ng,android-arm64-rel,android-nougat-x86-rel,android_optional_gpu_tests_rel,chromeos-amd64-generic-rel,dawn-linux-x64-deps-rel,dawn-win10-x64-deps-rel,dawn-win10-x86-deps-rel,linux-chromeos-rel,linux-lacros-rel,linux-rel,linux-wayland-rel,linux-x64-castos,linux_optional_gpu_tests_rel,mac_optional_gpu_tests_rel,win_optional_gpu_tests_rel
    > Change-Id: Ic2334a889888194bed11691be8c1fab7f5a739fc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194138
    > Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1102282}
    >
    
    Bug: 1400058
    Change-Id: If54ed030bc30db376b3895d78c1c651e6aa6a973
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227347
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1102309}

--- base/test/test_suite.cc
+++ base/test/test_suite.cc
@@ -9,13 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/allocator/partition_alloc_support.h"
 #include "base/allocator/partition_allocator/tagging.h"
 #include "base/at_exit.h"
 #include "base/base_paths.h"
 #include "base/base_switches.h"
 #include "base/command_line.h"
-#include "base/debug/asan_service.h"
 #include "base/debug/debugger.h"
 #include "base/debug/profiler.h"
 #include "base/debug/stack_trace.h"
@@ -172,16 +170,6 @@ class FeatureListScopedToEachTest : public testing::EmptyTestEventListener {
       new_command_line.AppendSwitchNative(iter.first, iter.second);
 
     *CommandLine::ForCurrentProcess() = new_command_line;
-
-    // TODO(https://crbug.com/1400059): Enable dangling pointer detector.
-    // TODO(https://crbug.com/1400058): Enable BackupRefPtr in unittests on
-    // Windows and Android too.
-    // TODO(https://crbug.com/1413674): Enable PartitionAlloc in unittests with
-    // ASAN.
-#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_ANDROID) && !defined(ADDRESS_SANITIZER)
-    allocator::PartitionAllocSupport::Get()->ReconfigureAfterFeatureListInit(
-        "", /*configure_dangling_pointer_detector=*/false);
-#endif
   }
 
   void OnTestEnd(const testing::TestInfo& test_info) override {
@@ -584,21 +572,6 @@ void TestSuite::SuppressErrorDialogs() {
 
 void TestSuite::Initialize() {
   DCHECK(!is_initialized_);
-
-  // The AsanService causes ASAN errors to emit additional information. It is
-  // helpful on its own. It is also required by ASAN BackupRefPtr when
-  // reconfiguring PartitionAlloc below.
-#if defined(ADDRESS_SANITIZER)
-  base::debug::AsanService::GetInstance()->Initialize();
-#endif
-
-  // TODO(https://crbug.com/1400058): Enable BackupRefPtr in unittests on
-  // Windows and Android too. Same for ASAN.
-  // TODO(https://crbug.com/1413674): Enable PartitionAlloc in unittests with
-  // ASAN.
-#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_ANDROID) && !defined(ADDRESS_SANITIZER)
-  allocator::PartitionAllocSupport::Get()->ReconfigureForTests();
-#endif  // BUILDFLAG(IS_WIN)
 
   test::ScopedRunLoopTimeout::SetAddGTestFailureOnTimeout();
 
