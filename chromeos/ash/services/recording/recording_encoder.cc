37b37ad26f06e28d415b6d516ea4f7926a1c4e67
Ahmed Fakhry <afakhry@chromium.org>
2023-02-07 01:50:19 +0000

    gif_recording: Extract remaining free space logic
    The code, which performs the remaining free disk space, or
    remaining DriveFS quota, has to be extracted out so that it
    can be shared between various encoder types.
    
    This is another refactoring CL that's needed for adding the
    GIF recording capability.
    
    Fixed: b/267792892
    Test: Manually, existing tests.
    Change-Id: I5133149e9c162f4e2b37f41a32f6d0152d991f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221987
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101932}

--- chromeos/ash/services/recording/recording_encoder.cc
+++ chromeos/ash/services/recording/recording_encoder.cc
@@ -19,6 +19,14 @@ RecordingEncoder::~RecordingEncoder() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 }
 
+void RecordingEncoder::NotifyFailure(mojom::RecordingStatus status) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (on_failure_callback_) {
+    std::move(on_failure_callback_).Run(status);
+  }
+}
+
 void RecordingEncoder::OnEncoderStatus(bool for_video,
                                        media::EncoderStatus status) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -31,14 +39,6 @@ void RecordingEncoder::OnEncoderStatus(bool for_video,
              << " frame: " << status.message();
   NotifyFailure(for_video ? mojom::RecordingStatus::kVideoEncodingError
                           : mojom::RecordingStatus::kAudioEncodingError);
-}
-
-void RecordingEncoder::NotifyFailure(mojom::RecordingStatus status) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  if (on_failure_callback_) {
-    std::move(on_failure_callback_).Run(status);
-  }
 }
 
 }  // namespace recording
