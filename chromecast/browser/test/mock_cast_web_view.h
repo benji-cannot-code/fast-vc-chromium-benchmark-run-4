a4d6a27785e51fb0fe3aef736b613bc169b5a247
Lukasz Anforowicz <lukasza@chromium.org>
2023-02-07 18:54:38 +0000

    [CodeHealth] Migrate chromecast::...::mojom::Feature to DictionaryValue.
    Bug: 1301890
    Change-Id: I1fe5654e2f7e4ed42759dbc95910454403cdbd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4211090
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102279}

--- chromecast/browser/test/mock_cast_web_view.h
+++ chromecast/browser/test/mock_cast_web_view.h
@@ -32,7 +32,7 @@ class MockCastWebContents : public CastWebContents {
               media_blocker,
               (),
               (const override));
-  MOCK_METHOD(void, AddRendererFeatures, (base::Value), (override));
+  MOCK_METHOD(void, AddRendererFeatures, (base::Value::Dict), (override));
   MOCK_METHOD(void,
               SetInterfacesForRenderer,
               (mojo::PendingRemote<mojom::RemoteInterfaces>),
