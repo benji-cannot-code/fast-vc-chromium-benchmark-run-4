dd9029196378728879e1dff9dd85c18f3b00e7c8
Sergii Bykov <sbykov@google.com>
2023-02-07 09:33:10 +0000

    Minor cleanup of DeviceCloudPolicyManagerAsh
    Removed undefined method and unused includes.
    
    Change-Id: Ie5beaabe83448fbf044d6a5f86b23ce3e2205d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226671
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102048}

--- chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.cc
+++ chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.cc
@@ -10,17 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "ash/constants/ash_features.h"
 #include "ash/constants/ash_paths.h"
 #include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/functional/callback_helpers.h"
 #include "base/path_service.h"
-#include "base/strings/string_number_conversions.h"
-#include "base/system/sys_info.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/attestation/attestation_policy_observer.h"
@@ -52,16 +48,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/cloud_policy_service.h"
 #include "components/policy/core/common/cloud/cloud_policy_store.h"
 #include "components/policy/core/common/cloud/policy_invalidation_scope.h"
-#include "components/policy/core/common/policy_types.h"
 #include "components/policy/core/common/remote_commands/remote_commands_factory.h"
 #include "components/policy/core/common/schema_registry.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/network_service_instance.h"
-#include "crypto/sha2.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
-#include "url/gurl.h"
 
 namespace policy {
 
