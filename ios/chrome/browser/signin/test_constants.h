7f3cc901ff636df25c85d9be92f61f41e73a3053
Menghan YANG <myuu@google.com>
2023-02-08 18:26:33 +0000

    [iOS] Cleanup buttons in FakeAuthActivityViewController
    Remove unused _signInButton from FakeAuthActivityViewController
    and make the use of _cancelButton explicit (previously selected
    by hard-coded a11y label instead of a11y id in EG tests)
    
    
    Fixed: 1400696
    Change-Id: Ibad6aa124277971be79a30227d025a27a77e5c92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231691
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102813}

--- ios/chrome/browser/signin/test_constants.h
+++ ios/chrome/browser/signin/test_constants.h
@@ -10,5 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Accessibility identifier for FakeAuthActivityViewController.
 extern NSString* const kFakeAuthActivityViewIdentifier;
+// Accessibility identifier for the cancel button in
+// FakeAuthActivityViewController.
+extern NSString* const kFakeAuthCancelButtonIdentifier;
 
 #endif  // IOS_CHROME_BROWSER_SIGNIN_TEST_CONSTANTS_H_
--- ios/chrome/browser/signin/test_constants.mm
+++ ios/chrome/browser/signin/test_constants.mm
@@ -11,3 +11,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 NSString* const kFakeAuthActivityViewIdentifier =
     @"FakeAuthActivityViewIdentifier";
+NSString* const kFakeAuthCancelButtonIdentifier =
+    @"FakeAuthCancelButtonIdentifier";
