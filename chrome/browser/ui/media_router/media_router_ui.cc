4b159f216b8165b70595d8246864ff066d779209
mark a. foltz <mfoltz@chromium.org>
2023-02-08 18:45:36 +0000

    Reland "[Media Router] Add sink_id to the IssueInfo ctor."
    This reverts commit 2f82add9a447feb81667626c244dd6774edf8958.
    
    Reason for revert: Fixed additional callsite.
    
    Original change's description:
    > Revert "[Media Router] Add sink_id to the IssueInfo ctor."
    >
    > This reverts commit 8a2509ab3a16ff2f61aba50a5bced843719ecc4b.
    >
    > Reason for revert: Causes build failure on mac-arm64-on-arm64-rel 
    >
    > Original change's description:
    > > [Media Router] Add sink_id to the IssueInfo ctor.
    > >
    > > Every IssueInfo has an associated sink.  This adds the sink_id to the
    > > IssueInfo constructor.
    > >
    > > Bug: 1407333
    > > Change-Id: Ide992080c011c585a6a59be2f00db3af58c284be
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227943
    > > Reviewed-by: Ahmed Moussa <ahmedmoussa@google.com>
    > > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > > Auto-Submit: Mark Foltz <mfoltz@chromium.org>
    > > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1102629}
    >
    > Bug: 1407333
    > Change-Id: I4f5a1034771dd692c28cd73cd5c03019b84c1061
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231931
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    > Owners-Override: Anders Hartvoll Ruud <andruud@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1102633}
    
    Bug: 1407333
    Change-Id: I5c15b869c28078da67f9b159e41c5d63acbb0e17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4232184
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102825}

--- chrome/browser/ui/media_router/media_router_ui.cc
+++ chrome/browser/ui/media_router/media_router_ui.cc
@@ -416,8 +416,7 @@ void MediaRouterUI::SendIssueForRouteTimeout(
       break;
   }
 
-  IssueInfo issue_info(issue_title, IssueInfo::Severity::NOTIFICATION);
-  issue_info.sink_id = sink_id;
+  IssueInfo issue_info(issue_title, IssueInfo::Severity::NOTIFICATION, sink_id);
   AddIssue(issue_info);
 }
 
@@ -435,8 +434,7 @@ void MediaRouterUI::SendIssueForUserNotAllowed(const MediaSink::Id& sink_id) {
   std::string issue_title = l10n_util::GetStringFUTF8(
       IDS_MEDIA_ROUTER_ISSUE_CREATE_ROUTE_USER_NOT_ALLOWED,
       GetSinkFriendlyNameFromId(sink_id));
-  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING);
-  issue_info.sink_id = sink_id;
+  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING, sink_id);
   AddIssue(issue_info);
 }
 
@@ -445,8 +443,7 @@ void MediaRouterUI::SendIssueForNotificationDisabled(
   std::string issue_title = l10n_util::GetStringFUTF8(
       IDS_MEDIA_ROUTER_ISSUE_CREATE_ROUTE_NOTIFICATION_DISABLED,
       GetSinkFriendlyNameFromId(sink_id));
-  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING);
-  issue_info.sink_id = sink_id;
+  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING, sink_id);
   AddIssue(issue_info);
 }
 
@@ -454,8 +451,7 @@ void MediaRouterUI::SendIssueForScreenPermission(const MediaSink::Id& sink_id) {
 #if BUILDFLAG(IS_MAC)
   std::string issue_title = l10n_util::GetStringUTF8(
       IDS_MEDIA_ROUTER_ISSUE_MAC_SCREEN_CAPTURE_PERMISSION_ERROR);
-  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING);
-  issue_info.sink_id = sink_id;
+  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING, sink_id);
   AddIssue(issue_info);
 #else
   NOTREACHED() << "Only valid for MAC OS!";
@@ -472,8 +468,7 @@ void MediaRouterUI::SendIssueForUnableToCast(MediaCastMode cast_mode,
                 IDS_MEDIA_ROUTER_ISSUE_UNABLE_TO_CAST_DESKTOP)
           : l10n_util::GetStringUTF8(
                 IDS_MEDIA_ROUTER_ISSUE_CREATE_ROUTE_TIMEOUT_FOR_TAB);
-  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING);
-  issue_info.sink_id = sink_id;
+  IssueInfo issue_info(issue_title, IssueInfo::Severity::WARNING, sink_id);
   AddIssue(issue_info);
 }
 
@@ -481,8 +476,7 @@ void MediaRouterUI::SendIssueForTabAudioNotSupported(
     const MediaSink::Id& sink_id) {
   IssueInfo issue_info(
       l10n_util::GetStringUTF8(IDS_MEDIA_ROUTER_ISSUE_TAB_AUDIO_NOT_SUPPORTED),
-      IssueInfo::Severity::NOTIFICATION);
-  issue_info.sink_id = sink_id;
+      IssueInfo::Severity::NOTIFICATION, sink_id);
   AddIssue(issue_info);
 }
 
