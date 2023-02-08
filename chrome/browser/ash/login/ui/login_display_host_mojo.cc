bd1d82ba2a626cbda53f7b565faac05200113a20
Brahim Chikhaoui <bchikhaoui@google.com>
2023-02-08 12:06:51 +0000

    [OOBE] Cancel PowerWash in kiosk mode.
    Fix the issue of canceling the powerwash screen in kiosk mode as it triggers GaiaScreen.
    
    Bug: b:257860301
    Change-Id: I2d6ec7b1620b9513b414344279f5570c96feb5be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4217752
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102648}

--- chrome/browser/ash/login/ui/login_display_host_mojo.cc
+++ chrome/browser/ash/login/ui/login_display_host_mojo.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/login_screen.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
+#include "ash/system/model/enterprise_domain_model.h"
+#include "ash/system/model/system_tray_model.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -419,7 +421,13 @@ void LoginDisplayHostMojo::HideOobeDialog(bool saml_page_closed) {
   const bool no_users = GetExistingUserController() &&
                         !GetExistingUserController()->IsSigninInProgress() &&
                         !has_user_pods_;
-  if (no_users && !saml_page_closed) {
+
+  const bool kiosk_license_mode =
+      Shell::Get()
+          ->system_tray_model()
+          ->enterprise_domain()
+          ->management_device_mode() == ManagementDeviceMode::kKioskSku;
+  if (no_users && !saml_page_closed && !kiosk_license_mode) {
     return;
   }
 
@@ -431,7 +439,7 @@ void LoginDisplayHostMojo::HideOobeDialog(bool saml_page_closed) {
   // timeout or ESC button) and there are no user pods and the user isn't using
   // ChromeVox - let the user go back to login flow with any action. Otherwise
   // the user can go back to login by pressing the arrow button.
-  if (saml_page_closed && !has_user_pods_ &&
+  if (saml_page_closed && !has_user_pods_ && !kiosk_license_mode &&
       !scoped_activity_observation_.IsObserving() &&
       !AccessibilityManager::Get()->IsSpokenFeedbackEnabled()) {
     scoped_activity_observation_.Observe(ui::UserActivityDetector::Get());
