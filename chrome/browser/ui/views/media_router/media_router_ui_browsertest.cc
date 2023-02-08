c2cd2fea2d9335335d336848ef63e24aba70dc0f
mark a. foltz <mfoltz@chromium.org>
2023-02-08 10:25:06 +0000

    [Media Router] Add sink_id to the IssueInfo ctor.
    Every IssueInfo has an associated sink.  This adds the sink_id to the
    IssueInfo constructor.
    
    Bug: 1407333
    Change-Id: Ide992080c011c585a6a59be2f00db3af58c284be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227943
    Reviewed-by: Ahmed Moussa <ahmedmoussa@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102629}

--- chrome/browser/ui/views/media_router/media_router_ui_browsertest.cc
+++ chrome/browser/ui/views/media_router/media_router_ui_browsertest.cc
@@ -44,7 +44,8 @@ class MediaRouterUIBrowserTest : public InProcessBrowserTest {
  public:
   MediaRouterUIBrowserTest()
       : issue_(IssueInfo("title notification",
-                         IssueInfo::Severity::NOTIFICATION)) {}
+                         IssueInfo::Severity::NOTIFICATION,
+                         "sinkId1")) {}
   ~MediaRouterUIBrowserTest() override {}
 
   void SetUpOnMainThread() override {
