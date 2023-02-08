a363c6cf3ee16f1f3a2af4a634cdac29e29788eb
mark a. foltz <mfoltz@chromium.org>
2023-02-08 18:01:04 +0000

    Roll Open Screen from ccc269d77256 to cf5b8ca7bc6f (1 revision)
    https://chromium.googlesource.com/openscreen.git/+log/ccc269d77256..cf5b8ca7bc6f
    
    2023-02-08 mfoltz@chromium.org [Open Screen] Replace ByteView with a generic Span type.
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/openscreen-chromium
    Please CC jophba@chromium.org on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Bug: None
    Change-Id: I254c91df7076e6425b9ea5b51691e95009ec0ce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4229102
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102797}

--- components/cast_streaming/test/cast_streaming_test_sender.cc
+++ components/cast_streaming/test/cast_streaming_test_sender.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "components/cast_streaming/public/config_conversions.h"
 #include "components/cast_streaming/test/cast_message_port_sender_impl.h"
-#include "third_party/openscreen/src/platform/base/byte_view.h"
+#include "third_party/openscreen/src/platform/base/span.h"
 
 namespace cast_streaming {
 
--- media/cast/common/openscreen_conversion_helpers.cc
+++ media/cast/common/openscreen_conversion_helpers.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/cast/common/openscreen_conversion_helpers.h"
 
-#include "third_party/openscreen/src/platform/base/byte_view.h"
+#include "third_party/openscreen/src/platform/base/span.h"
 
 namespace media::cast {
 
