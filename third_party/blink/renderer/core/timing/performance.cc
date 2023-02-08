f6847420ff7f735116195683b326fc52883a1377
Hao Liu <haoliuk@chromium.org>
2023-02-08 18:37:18 +0000

    Guard all options of getEntries() with flag
    Previously only the includeChildFrames option is guarded by the flag.
    This CL moves the flag to guard all options of getEntries().
    
    Added a wpt that also runs in virtual/stable where it is expected to
    fail. In virtual/stable, the CrossOriginPerformanceTimeline feature
    is off, the filter option is ignored, and the getEntries(filterOptions)
    behaves the same way as getEntries() does.
    
    Change-Id: I7cf7fcbbf5d440066eef4c664c48ce550859c8d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225793
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102819}

--- third_party/blink/renderer/core/timing/performance.cc
+++ third_party/blink/renderer/core/timing/performance.cc
@@ -326,8 +326,9 @@ PerformanceEntryVector Performance::getEntries() {
 PerformanceEntryVector Performance::getEntries(
     ScriptState* script_state,
     PerformanceEntryFilterOptions* options) {
-  if (!options) {
-    return getEntries();
+  if (!RuntimeEnabledFeatures::CrossFramePerformanceTimelineEnabled() ||
+      !options) {
+    return GetEntriesForCurrentFrame();
   }
 
   PerformanceEntryVector entries;
@@ -340,8 +341,7 @@ PerformanceEntryVector Performance::getEntries(
                                 : g_null_atom;
 
   // Get sorted entry list based on provided input.
-  if (options->getIncludeChildFramesOr(false) &&
-      RuntimeEnabledFeatures::CrossFramePerformanceTimelineEnabled()) {
+  if (options->getIncludeChildFramesOr(false)) {
     entries = GetEntriesWithChildFrames(script_state, entry_type, name);
   } else {
     if (!entry_type) {
