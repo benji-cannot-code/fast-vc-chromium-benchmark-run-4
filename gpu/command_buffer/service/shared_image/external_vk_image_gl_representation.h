eba6b9454befe91770d1d1a7eb3a86a68c6601c4
kylechar <kylechar@chromium.org>
2023-02-08 18:36:55 +0000

    Prepare ExternalVkImageBacking for multi-planar formats
    This CL contains no-op refactors to make it easier to add multi-planar
    format support to ExternalVkImageBacking.
    
    Bug: 1406253
    Change-Id: I951159a645ee6f6de73ceeaafb9f9aaa2c9c9110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4228478
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102818}

--- gpu/command_buffer/service/shared_image/external_vk_image_gl_representation.h
+++ gpu/command_buffer/service/shared_image/external_vk_image_gl_representation.h
@@ -56,8 +56,7 @@ class ExternalVkImageGLRepresentation : public GLTextureImageRepresentation {
   ExternalVkImageGLRepresentation(SharedImageManager* manager,
                                   SharedImageBacking* backing,
                                   MemoryTypeTracker* tracker,
-                                  gles2::Texture* texture,
-                                  GLuint texture_service_id);
+                                  gles2::Texture* texture);
 
   ExternalVkImageGLRepresentation(const ExternalVkImageGLRepresentation&) =
       delete;
@@ -79,10 +78,11 @@ class ExternalVkImageGLRepresentation : public GLTextureImageRepresentation {
 class ExternalVkImageGLPassthroughRepresentation
     : public GLTexturePassthroughImageRepresentation {
  public:
-  ExternalVkImageGLPassthroughRepresentation(SharedImageManager* manager,
-                                             SharedImageBacking* backing,
-                                             MemoryTypeTracker* tracker,
-                                             GLuint texture_service_id);
+  ExternalVkImageGLPassthroughRepresentation(
+      SharedImageManager* manager,
+      SharedImageBacking* backing,
+      MemoryTypeTracker* tracker,
+      scoped_refptr<gles2::TexturePassthrough> texture);
 
   ExternalVkImageGLPassthroughRepresentation(
       const ExternalVkImageGLPassthroughRepresentation&) = delete;
@@ -98,6 +98,7 @@ class ExternalVkImageGLPassthroughRepresentation
   void EndAccess() override;
 
  private:
+  scoped_refptr<gles2::TexturePassthrough> texture_;
   ExternalVkImageGLRepresentationShared representation_shared_;
 };
 
