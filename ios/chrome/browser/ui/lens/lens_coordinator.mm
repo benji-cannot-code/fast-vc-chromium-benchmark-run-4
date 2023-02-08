9415b6f68c4450167eb782cc01e73c4f0f0e803b
Jason Hu <hujasonx@google.com>
2023-02-08 21:15:25 +0000

    [Lens]Prepare Lens to use intermediate animation stage
    Depends on https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/5398042
    
    Also prepares the Lens Coordinator to provide the web content frame
    when needed, instead of when the Lens UI is created.
    
    Bug: 5398042
    Change-Id: I983cec99517062caa2aadddfe3b4fcd431c66af5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214254
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102866}

--- ios/chrome/browser/ui/lens/lens_coordinator.mm
+++ ios/chrome/browser/ui/lens/lens_coordinator.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/lens/lens_coordinator.h"
 
 #import "base/strings/sys_string_conversions.h"
-#import "base/task/sequenced_task_runner.h"
 #import "ios/chrome/browser/application_context/application_context.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/main/browser.h"
@@ -196,8 +195,8 @@ const base::TimeDelta kCloseLensViewTimeout = base::Seconds(10);
     contentArea = [delegate webContentAreaForLensCoordinator:self];
   }
 
-  UIViewController* viewController = [lensController
-      inputSelectionViewControllerWithWebContentFrame:contentArea];
+  UIViewController* viewController =
+      [lensController inputSelectionViewController];
 
   // TODO(crbug.com/1353430): the returned UIViewController
   // must not be nil, remove this check once the internal
@@ -285,6 +284,15 @@ const base::TimeDelta kCloseLensViewTimeout = base::Seconds(10);
   [self dismissViewController];
 }
 
+- (CGRect)webContentFrame {
+  id<LensPresentationDelegate> delegate = self.delegate;
+  if (delegate) {
+    return [delegate webContentAreaForLensCoordinator:self];
+  }
+
+  return [UIScreen mainScreen].bounds;
+}
+
 #pragma mark - WebStateListObserving methods
 
 - (void)webStateList:(WebStateList*)webStateList
@@ -315,6 +323,13 @@ const base::TimeDelta kCloseLensViewTimeout = base::Seconds(10);
     id<ToolbarCommands> toolbarCommandsHandler =
         HandlerForProtocol(dispatcher, ToolbarCommands);
     [toolbarCommandsHandler triggerToolbarSlideInAnimation];
+  }
+}
+
+- (void)webStateDidStartLoading:(web::WebState*)webState {
+  const id<ChromeLensController> lensController = self.lensController;
+  if (self.lensWebPageLoadTriggeredFromInputSelection && lensController) {
+    [lensController triggerSecondaryTransitionAnimation];
   }
 }
 
