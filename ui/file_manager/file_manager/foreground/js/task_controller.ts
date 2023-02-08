dba8f859bab9e81cdcf00910be5bb4f78119020d
Luciano Pacheco <lucmult@chromium.org>
2023-02-08 03:14:40 +0000

    Files F2: Clear file tasks from context menu
    When deselecting a file the context menu should be cleared.
    
    Bug: b:268130972
    Change-Id: I1f2fcadbb95da5415805b7485d56e0b241d29b9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227173
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102531}

--- ui/file_manager/file_manager/foreground/js/task_controller.ts
+++ ui/file_manager/file_manager/foreground/js/task_controller.ts
@@ -446,6 +446,7 @@ export class TaskController {
 
     if (shouldDisableTasks) {
       this.ui_.taskMenuButton.hidden = true;
+      this.updateContextMenuTaskItems_([]);
       if (window.IN_TEST) {
         this.ui_.taskMenuButton.toggleAttribute('get-tasks-completed', true);
       }
