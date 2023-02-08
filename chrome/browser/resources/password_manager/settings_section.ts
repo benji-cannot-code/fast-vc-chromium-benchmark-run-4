90b71427e0d04b534999cdc1aa93adad2a7b8b60
Maria Kazinova <kazinova@google.com>
2023-02-08 15:34:23 +0000

    [UPM-Desktop] Update add shortcut banner wording.
    Stop mentioning 'Google Password Manager' in unbranded Chrome builds,
    leave it as 'Password Manager' in this case.
    
    Bug: 1358448
    Change-Id: I654f7478f58e95a2b4fd0c28d2487880e259398b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225509
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102717}

--- chrome/browser/resources/password_manager/settings_section.ts
+++ chrome/browser/resources/password_manager/settings_section.ts
@@ -67,6 +67,8 @@ export class SettingsSectionElement extends I18nMixin
   private setBlockedSitesListListener_: BlockedSitesListChangedListener|null =
       null;
 
+  private addShortcutDescription_: string;
+
   override connectedCallback() {
     super.connectedCallback();
     this.setBlockedSitesListListener_ = blockedSites => {
@@ -77,6 +79,9 @@ export class SettingsSectionElement extends I18nMixin
         blockedSites => this.blockedSites_ = blockedSites);
     PasswordManagerImpl.getInstance().addBlockedSitesListChangedListener(
         this.setBlockedSitesListListener_);
+
+    this.addShortcutDescription_ =
+        this.i18n('addShortcutDescription', this.i18n('localPasswordManager'));
   }
 
   override disconnectedCallback() {
@@ -116,6 +121,10 @@ export class SettingsSectionElement extends I18nMixin
     PasswordManagerImpl.getInstance().switchBiometricAuthBeforeFillingState();
   }
   // </if>
+
+  private getShortcutBannerDescription_(): string {
+    return this.addShortcutDescription_;
+  }
 }
 
 declare global {
