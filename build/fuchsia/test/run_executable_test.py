7e82271343492e38952181164a8dc57cd5e84fa1
Chong Gu <chonggu@google.com>
2023-02-08 18:42:04 +0000

    [Fuchsia] Remove unnecessaary code-coverage flag
    Coverage data for binaries built with coverage enabled is always
    produced, so there's no need to use another flag.
    
    Bug: 1405061
    Change-Id: I3fb72f195ed159ecd1e52b8507fceb12b651e7ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4229319
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Auto-Submit: Chong Gu <chonggu@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1102821}

--- build/fuchsia/test/run_executable_test.py
+++ build/fuchsia/test/run_executable_test.py
@@ -39,11 +39,11 @@ def _copy_custom_output_file(test_runner: FfxTestRunner, file: str,
 
 
 def _copy_coverage_files(test_runner: FfxTestRunner, dest: str) -> None:
-    """Copy debug data file from the device to the host."""
+    """Copy debug data file from the device to the host if it exists."""
 
     coverage_dir = test_runner.get_debug_data_directory()
     if not coverage_dir:
-        logging.error(
+        logging.info(
             'Failed to parse coverage data directory from test summary '
             'output files. Not copying coverage files from the device.')
         return
@@ -77,13 +77,13 @@ class ExecutableTestRunner(TestRunner):
             test_args: List[str],
             test_name: str,
             target_id: Optional[str],
-            code_coverage_dir: Optional[str],
+            code_coverage_dir: str,
             logs_dir: Optional[str] = None) -> None:
         super().__init__(out_dir, test_args, [test_name], target_id)
         if not self._test_args:
             self._test_args = []
         self._test_name = test_name
-        self._code_coverage_dir = code_coverage_dir
+        self._code_coverage_dir = os.path.basename(code_coverage_dir)
         self._custom_artifact_directory = None
         self._isolated_script_test_output = None
         self._isolated_script_test_perf_output = None
@@ -193,9 +193,7 @@ class ExecutableTestRunner(TestRunner):
                 test_runner,
                 os.path.basename(self._isolated_script_test_perf_output),
                 self._isolated_script_test_perf_output)
-        if self._code_coverage_dir:
-            _copy_coverage_files(test_runner,
-                                 os.path.basename(self._code_coverage_dir))
+        _copy_coverage_files(test_runner, self._code_coverage_dir)
 
     def run_test(self) -> subprocess.Popen:
         test_args = self._get_args()
@@ -227,8 +225,6 @@ def create_executable_test_runner(runner_args: argparse.Namespace,
                                   test_args: List[str]):
     """Helper for creating an ExecutableTestRunner."""
 
-    if not runner_args.code_coverage:
-        runner_args.code_coverage_dir = None
     return ExecutableTestRunner(runner_args.out_dir, test_args,
                                 runner_args.test_type, runner_args.target_id,
                                 runner_args.code_coverage_dir,
@@ -239,15 +235,12 @@ def register_executable_test_args(parser: argparse.ArgumentParser) -> None:
     """Register common arguments for ExecutableTestRunner."""
 
     test_args = parser.add_argument_group('test', 'arguments for test running')
-    test_args.add_argument('--code-coverage',
-                           default=False,
-                           action='store_true',
-                           help='Gather code coverage information.')
     test_args.add_argument('--code-coverage-dir',
                            default=os.getcwd(),
                            help='Directory to place code coverage '
-                           'information. Only relevant when --code-coverage '
-                           'is set to true. Defaults to current directory.')
+                           'information. Only relevant when the target was '
+                           'built with |fuchsia_code_coverage| set to true. '
+                           'Defaults to current directory.')
     test_args.add_argument('--test-name',
                            dest='test_type',
                            help='Name of the test package (e.g. '
