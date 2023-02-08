51a74bd2c96276810ad3f7623f0166bb94321606
Sergey Ulanov <sergeyu@chromium.org>
2023-02-08 15:45:14 +0000

    [Fuchsia] Avoid dependency BadgeService on Fuchsia
    Blink depends on mojom::blink::BadgeService service, but it's not
    provided in WebEngine. As result, pages that use Navigator.setAppBadge()
    didn't work properly. Add ifdef to disable this dependency on Fuchsia.
    This is a short-term fix for the problem. Added TODO for
    crbug.com/1413916, which tracks long-term solution.
    
    Bug: 1316721
    Change-Id: Ie82ddebf6a894681eaaf18f23093c695d5ca64ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4229855
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102726}

--- third_party/blink/renderer/modules/badging/navigator_badge.cc
+++ third_party/blink/renderer/modules/badging/navigator_badge.cc
@@ -104,7 +104,9 @@ ScriptPromise NavigatorBadge::SetAppBadgeHelper(
     return ScriptPromise();
   }
 
-#if !BUILDFLAG(IS_ANDROID)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+  // TODO(crbug.com/1413916): The service is implemented in Chrome, so it may
+  // not be provided in other embedders. Ensure that case is handled properly.
   From(script_state).badge_service()->SetBadge(std::move(badge_value));
 #endif
   return ScriptPromise::CastUndefined(script_state);
@@ -121,7 +123,9 @@ ScriptPromise NavigatorBadge::ClearAppBadgeHelper(
     return ScriptPromise();
   }
 
-#if !BUILDFLAG(IS_ANDROID)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+  // TODO(crbug.com/1413916): The service is implemented in Chrome, so it may
+  // not be provided in other embedders. Ensure that case is handled properly.
   From(script_state).badge_service()->ClearBadge();
 #endif
   return ScriptPromise::CastUndefined(script_state);
