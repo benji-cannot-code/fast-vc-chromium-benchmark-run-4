2d30c0e8989f63d3eb11d13d8dbcd286f7af740e
mark a. foltz <mfoltz@chromium.org>
2023-02-08 21:34:58 +0000

    [Cast Channel] Adds unit test coverage for channel flags.
    This adds unit test coverage to the histogram added in
    crrev.com/c/4222271.
    
    Bug: 1412904,b:239475260
    Change-Id: Ib97332a60f1495b90d5bf8f6ab52270cd6a61e65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4233678
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102898}

--- components/media_router/common/providers/cast/channel/cast_channel_metrics.cc
+++ components/media_router/common/providers/cast/channel/cast_channel_metrics.cc
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cast_channel {
 
-constexpr char kLaunchSessionChannelFlagsHistogram[] =
-    "Cast.Channel.LaunchSession.Flags";
-
 void RecordLaunchSessionChannelFlags(CastChannelFlags flags) {
   if (!flags) {
     UMA_HISTOGRAM_ENUMERATION(kLaunchSessionChannelFlagsHistogram,
--- components/media_router/common/providers/cast/channel/cast_channel_metrics.h
+++ components/media_router/common/providers/cast/channel/cast_channel_metrics.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cast_channel {
 
+constexpr char kLaunchSessionChannelFlagsHistogram[] =
+    "Cast.Channel.LaunchSession.Flags";
+
 // Records the flags set on a Cast channel when a Cast LAUNCH message is sent.
 void RecordLaunchSessionChannelFlags(CastChannelFlags flags);
 
