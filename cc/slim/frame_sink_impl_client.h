365fdff570c92a8b4bd1b3066a619fef13179d40
Bo Liu <boliu@chromium.org>
2023-02-08 15:58:42 +0000

    slim: Use viz::ResourceId in FrameSinkImpl
    Bug: 1408128
    Change-Id: Id612efc19ff41cc6fab8ed2fcbb2fa332a945989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226822
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102733}

--- cc/slim/frame_sink_impl_client.h
+++ cc/slim/frame_sink_impl_client.h
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_SLIM_FRAME_SINK_IMPL_CLIENT_H_
 
 #include "base/containers/flat_set.h"
-#include "cc/resources/ui_resource_manager.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/frame_timing_details.h"
 #include "components/viz/common/hit_test/hit_test_region_list.h"
 #include "components/viz/common/quads/compositor_frame.h"
+#include "components/viz/common/resources/resource_id.h"
 
 namespace cc::slim {
 
@@ -21,7 +21,7 @@ class FrameSinkImplClient {
 
   virtual bool BeginFrame(const viz::BeginFrameArgs& args,
                           viz::CompositorFrame& out_frame,
-                          base::flat_set<cc::UIResourceId>& out_resource_ids,
+                          base::flat_set<viz::ResourceId>& out_resource_ids,
                           viz::HitTestRegionList& out_hit_test_region_list) = 0;
   virtual void DidReceiveCompositorFrameAck() = 0;
   virtual void DidSubmitCompositorFrame() = 0;
