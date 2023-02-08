fee111b9e638a8eb48ee2778846205fa175aebbe
Eugene Zemtsov <eugene@chromium.org>
2023-02-08 21:11:58 +0000

    Raw AAC support in MF Audio Encoder
    Before this change MF Audio Encoder would only emit ADTS.
    
    Bug: 1407013
    Change-Id: Ic63d75bb18e1b80416ce756458ce763b525a3b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226559
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102860}

--- media/mojo/mojom/audio_encoder_config_mojom_traits.h
+++ media/mojo/mojom/audio_encoder_config_mojom_traits.h
@@ -14,6 +14,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 template <>
+struct EnumTraits<media::mojom::AacOutputFormat,
+                  media::AudioEncoder::AacOutputFormat> {
+  static media::mojom::AacOutputFormat ToMojom(
+      media::AudioEncoder::AacOutputFormat input);
+
+  static bool FromMojom(media::mojom::AacOutputFormat,
+                        media::AudioEncoder::AacOutputFormat* output);
+};
+
+template <>
+struct StructTraits<media::mojom::AacAudioEncoderConfigDataView,
+                    media::AudioEncoder::AacOptions> {
+  static media::AudioEncoder::AacOutputFormat format(
+      media::AudioEncoder::AacOptions& input) {
+    return input.format;
+  }
+
+  static bool Read(media::mojom::AacAudioEncoderConfigDataView input,
+                   media::AudioEncoder::AacOptions* output);
+};
+
+template <>
 struct StructTraits<media::mojom::AudioEncoderConfigDataView,
                     media::AudioEncoderConfig> {
   static media::AudioCodec codec(const media::AudioEncoderConfig& input) {
@@ -30,6 +52,11 @@ struct StructTraits<media::mojom::AudioEncoderConfigDataView,
 
   static uint32_t bitrate(const media::AudioEncoderConfig& input) {
     return input.bitrate.value_or(0);
+  }
+
+  static media::AudioEncoder::AacOptions aac(
+      const media::AudioEncoderConfig& input) {
+    return input.aac.value_or(media::AudioEncoder::AacOptions());
   }
 
   static bool Read(media::mojom::AudioEncoderConfigDataView input,
