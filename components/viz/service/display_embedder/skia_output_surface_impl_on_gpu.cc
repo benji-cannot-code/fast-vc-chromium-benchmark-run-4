bab92f7ab470bc604e985b91586a2dfcc6c7940c
Piotr Bialecki <bialpio@chromium.org>
2023-02-08 22:19:31 +0000

    0c tab capture - implement cache for NV12 COR's intermediate texture
    In addition, rely on SkSurface::MakeRenderTarget() for the intermediate
    surface instead of engaging the SharedImage machinery.
    
    Change-Id: I142f8b4cd325282db53bb169062dac5afd79a380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221973
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102950}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/task/bind_post_task.h"
 #include "base/threading/thread_checker.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/viz/common/features.h"
 #include "components/viz/common/frame_sinks/blit_request.h"
@@ -55,10 +56,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/rgba_to_yuva.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/libyuv/include/libyuv/planar_functions.h"
+#include "third_party/skia/include/core/SkAlphaType.h"
 #include "third_party/skia/include/core/SkBlendMode.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
+#include "third_party/skia/include/core/SkColorType.h"
 #include "third_party/skia/include/core/SkDeferredDisplayList.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 #include "third_party/skia/include/core/SkPromiseImageTexture.h"
@@ -1141,24 +1144,18 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutputNV12(
   }
 
   // Create a destination for the scaled & clipped result:
-  auto intermediate_representation = CreateSharedImageRepresentationSkia(
-      ResourceFormat::RGBA_8888, intermediate_dst_size, color_space);
-  if (!intermediate_representation) {
-    DVLOG(1) << "failed to create shared image representation for the "
-                "intermediate surface";
+  auto intermediate_surface = SkSurface::MakeRenderTarget(
+      gr_context(), skgpu::Budgeted::kYes,
+      SkImageInfo::Make(gfx::SizeToSkISize(intermediate_dst_size),
+                        SkColorType::kRGBA_8888_SkColorType,
+                        SkAlphaType::kPremul_SkAlphaType,
+                        color_space.ToSkColorSpace()));
+
+  if (!intermediate_surface) {
+    DVLOG(1) << "failed to create surface for the intermediate texture";
     // Send empty result.
     return;
   }
-
-  SkSurfaceProps surface_props{0, kUnknown_SkPixelGeometry};
-  std::vector<GrBackendSemaphore> begin_semaphores;
-  std::vector<GrBackendSemaphore> end_semaphores;
-
-  auto intermediate_scoped_write =
-      intermediate_representation->BeginScopedWriteAccess(
-          /*final_msaa_count=*/1, surface_props, &begin_semaphores,
-          &end_semaphores,
-          gpu::SharedImageRepresentation::AllowUnclearedAccess::kYes);
 
   absl::optional<SkVector> scaling;
   if (request->is_scaled()) {
@@ -1168,20 +1165,18 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutputNV12(
                                  request->scale_from().y());
   }
 
-  intermediate_scoped_write->surface()->wait(begin_semaphores.size(),
-                                             begin_semaphores.data());
-
   RenderSurface(surface, src_rect, scaling,
                 is_downscale_or_identity_in_both_dimensions,
-                intermediate_scoped_write->surface());
+                intermediate_surface.get());
 
   if (request->has_blit_request()) {
-    BlendBitmapOverlays(intermediate_scoped_write->surface()->getCanvas(),
+    BlendBitmapOverlays(intermediate_surface->getCanvas(),
                         request->blit_request());
   }
 
-  auto intermediate_image =
-      intermediate_scoped_write->surface()->makeImageSnapshot();
+  intermediate_surface->flush();
+
+  auto intermediate_image = intermediate_surface->makeImageSnapshot();
   if (!intermediate_image) {
     DLOG(ERROR) << "failed to retrieve `intermediate_image`.";
     return;
@@ -1280,16 +1275,6 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutputNV12(
       FailedSkiaFlush("CopyOutputNV12 plane_surfaces[i]->flush()");
       return;
     }
-  }
-
-  should_submit |= !end_semaphores.empty();
-
-  intermediate_representation->SetCleared();
-  if (!FlushSurface(intermediate_scoped_write->surface(), end_semaphores,
-                    intermediate_scoped_write->TakeEndState())) {
-    // TODO(penghuang): handle vulkan device lost.
-    FailedSkiaFlush("CopyOutputNV12 dest_surface->flush()");
-    return;
   }
 
   if (should_submit && !gr_context()->submit()) {
--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/threading/thread_checker.h"
+#include "base/time/time.h"
 #include "base/types/pass_key.h"
 #include "build/build_config.h"
 #include "components/viz/common/display/renderer_settings.h"
