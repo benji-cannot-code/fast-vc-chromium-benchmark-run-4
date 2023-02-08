6527ec0472e5931ce886689dd941428e511fd4f3
Dominic Farolino <dom@chromium.org>
2023-02-08 15:50:19 +0000

    Remove redundant self-include
    Change-Id: Ia269f939974b50a2d23a10220d7d9018e1b151b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4232999
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Auto-Submit: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102729}

--- third_party/blink/renderer/core/html/fenced_frame/fenced_frame_reporting.h
+++ third_party/blink/renderer/core/html/fenced_frame/fenced_frame_reporting.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FENCED_FRAME_FENCED_FRAME_REPORTING_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FENCED_FRAME_FENCED_FRAME_REPORTING_H_
 
-#include "third_party/blink/renderer/core/html/fenced_frame/fenced_frame_reporting.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
