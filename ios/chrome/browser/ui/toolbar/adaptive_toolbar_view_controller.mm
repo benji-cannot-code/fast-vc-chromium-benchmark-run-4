d9370fe4fea2565cc01248519320a0d240a1bfba
Gauthier Ambard <gambard@chromium.org>
2023-02-08 15:36:29 +0000

    [iOS] Add haptic feedback to toolbar menus
    This CL adds a haptic feedback when the user is displaying a menu from
    the bottom toolbar.
    
    Fixed: 1396357
    Change-Id: I1e9dcd3245045be28a352fe86577a49c369e8a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231836
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102718}

--- ios/chrome/browser/ui/toolbar/adaptive_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/adaptive_toolbar_view_controller.mm
@@ -490,6 +490,7 @@ const base::TimeDelta kToobarSlideInAnimationDuration = base::Milliseconds(500);
               handler:^(UIAction* uiAction) {
                 base::RecordAction(
                     base::UserMetricsAction("MobileMenuToolbarMenuTriggered"));
+                TriggerHapticFeedbackForImpact(UIImpactFeedbackStyleHeavy);
                 weakButton.menu =
                     [weakSelf.menuProvider menuForButtonOfType:buttonType];
               }];
