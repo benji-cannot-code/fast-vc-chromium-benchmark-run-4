cab5f812101299c7c0b69ec1ec3c51eca36cd460
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-02-09 04:45:06 +0000

    Adds a generic frame sink host class.
    The frame sink host that can be extended by other
    classes in ash and have the flexibility to create, configure and submit
    their own compositor frames to display compositor.
    
    It encapsulates the logic of communicating with the display compositor
    and handling and reusing of resources that are currently being used by
    the display compositor.
    
    Bug: 1392895
    Test: Covered by unittests
    Change-Id: Iceb5ba99f1c795488e482fe4c1f5beeb88efa12b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4038821
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Addison Luh <aluh@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103120}

---
+++ ash/frame_sink/ui_resource.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/frame_sink/ui_resource.h"
+
+#include "gpu/command_buffer/client/shared_image_interface.h"
+
+namespace ash {
+
+UiResource::UiResource() = default;
+
+UiResource::~UiResource() {
+  if (!context_provider) {
+    return;
+  }
+
+  gpu::SharedImageInterface* sii = context_provider->SharedImageInterface();
+  DCHECK(!mailbox.IsZero());
+  sii->DestroySharedImage(sync_token, mailbox);
+}
+
+}  // namespace ash
