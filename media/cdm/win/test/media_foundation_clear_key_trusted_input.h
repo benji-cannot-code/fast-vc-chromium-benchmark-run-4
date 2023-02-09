2c4d2b85bda728ea902625d01fe6527d11dc6542
Sangbaek Park <sangbaekpark@chromium.org>
2023-02-09 23:52:59 +0000

    media: Media Foundation Clear Key CDM basic implementation (part 2)
    - All required interfaces for MediaFoundation-based CDM are implemented
    or marked as `NOTIMPLEMENTED()`.
    - Tested the instantiation of Media Foundation Clear Key CDM Factory,
    CdmAccess, Cdm, Session, ITA and TrustedInput work
    
    Build command:
    ```
    autoninja -C out\Default browser_tests
    autoninja -C out\Default media_foundation_clear_key_cdm
    ```
    
    Bug: 1235887
    Change-Id: I3cc53efbe2171684a34956d4cd10fbe9cae2cd8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127820
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Sangbaek Park <sangbaekpark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103567}

---
+++ media/cdm/win/test/media_foundation_clear_key_trusted_input.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_CDM_WIN_TEST_MEDIA_FOUNDATION_CLEAR_KEY_TRUSTED_INPUT_H_
+#define MEDIA_CDM_WIN_TEST_MEDIA_FOUNDATION_CLEAR_KEY_TRUSTED_INPUT_H_
+
+#include <mfidl.h>
+#include <wrl/implements.h>
+
+namespace media {
+
+class MediaFoundationClearKeyTrustedInput final
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IMFTrustedInput,
+          Microsoft::WRL::FtmBase> {
+ public:
+  MediaFoundationClearKeyTrustedInput();
+  ~MediaFoundationClearKeyTrustedInput() override;
+  MediaFoundationClearKeyTrustedInput(
+      const MediaFoundationClearKeyTrustedInput&) = delete;
+  MediaFoundationClearKeyTrustedInput& operator=(
+      const MediaFoundationClearKeyTrustedInput&) = delete;
+
+  HRESULT RuntimeClassInitialize();
+
+  // IMFTrustedInput
+  STDMETHODIMP GetInputTrustAuthority(
+      _In_ DWORD stream_id,
+      _In_ REFIID riid,
+      _COM_Outptr_ IUnknown** authority) override;
+};
+
+}  // namespace media
+
+#endif  // MEDIA_CDM_WIN_TEST_MEDIA_FOUNDATION_CLEAR_KEY_TRUSTED_INPUT_H_
