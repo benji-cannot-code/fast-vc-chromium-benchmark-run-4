0a8ad0db67e2c39e37311cbc749627b18defbede
David Bertoni <dbertoni@chromium.org>
2023-02-09 18:32:35 +0000

    [Extensions] Convert more tabs API tests to run as service workers. (2)
    This CL creates separate extensions for the affected tests. Test
    files that are shared remain in the parent directory and are
    referenced through the pageUrl utility function.
    
    Bug: 1093066
    Change-Id: Ie7bfa8c355782a76a8c5e04c7b56decf2914e90d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234087
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103374}

--- chrome/test/data/extensions/api_test/tabs/basics/duplicate/duplicate.js
+++ chrome/test/data/extensions/api_test/tabs/basics/duplicate/duplicate.js
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var firstTabId;
 var firstWindowId;
 
+const scriptUrl = '_test_resources/api_test/tabs/basics/tabs_util.js';
+let loadScript = chrome.test.loadScript(scriptUrl);
+
+loadScript.then(async function() {
 chrome.test.runTests([
   function setupWindow() {
     createWindow([pageUrl("a")], {},
@@ -52,4 +56,4 @@ chrome.test.runTests([
     });
   }
  */
-]);
+])});
--- chrome/test/data/extensions/api_test/tabs/basics/relative_urls/relative_urls.js
+++ chrome/test/data/extensions/api_test/tabs/basics/relative_urls/relative_urls.js
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var firstWindowId;
 var testTabId;
 
+const scriptUrl = '_test_resources/api_test/tabs/basics/tabs_util.js';
+let loadScript = chrome.test.loadScript(scriptUrl);
+
+loadScript.then(async function() {
 function resolveOnMessage(resolve) {
   chrome.runtime.onMessage.addListener(function local(message) {
     chrome.runtime.onMessage.removeListener(local);
@@ -62,4 +66,4 @@ chrome.test.runTests([
     Promise.all([onMessagePromise, createPromise]).then(chrome.test.succeed);
   }
 
-]);
+])});
