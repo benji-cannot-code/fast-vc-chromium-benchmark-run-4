88ad29e7883ffc32de4afa9659530960b80d2db8
Lijin Shen <lazzzis@google.com>
2023-02-09 18:21:46 +0000

    Add back event in native side
    Clone of https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/java/android/window/BackEvent.java
    
    The consuming code for this event in content/ will be cross-platform so it shouldn't be limited to Android.
    
    Bug: 1410585
    Change-Id: I38775c666d37969588b488b2cc183b717e75dac4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214278
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1103367}

---
+++ ui/events/back_gesture_event.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/back_gesture_event.h"
+
+namespace ui {
+
+BackGestureEvent::BackGestureEvent(const gfx::PointF& location,
+                                   float progress,
+                                   BackGestureEventSwipeEdge edge)
+    : location_(location), progress_(progress), edge_(edge) {}
+
+}  // namespace ui
