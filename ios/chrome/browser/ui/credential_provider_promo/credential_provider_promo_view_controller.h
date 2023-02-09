19679c7566e37c757e93a39ba511db2f7ed76ff7
Hira Mahmood <hiramahmood@google.com>
2023-02-09 19:57:56 +0000

    [iOS] Handle user interaction with CredentialProviderPromoViewController
    This change handles user interaction with the VC by:
    
    * Conforming the CredentialProviderPromoMediator to the
    ConfirmationAlertActionHandler protocol
    * Creating the CredentialProviderPromoPresenter protocol
    * Conforming the coordinator to the CredentialProviderPromoPresenter
    protocol
    
    The UI Deck linked in the bug provides context on the UI flow.
    
    Bug: 1402963
    Change-Id: I45d7214bccf50d45f4cea6e7c894cd0ee88cf1f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219294
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103441}

--- ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_view_controller.h
+++ ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_view_controller.h
@@ -9,11 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_consumer.h"
+#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 
 // Container view controller for the Credential Provider Extension promo. Can be
 // configured to display the half-sheet or full-screen promo.
 @interface CredentialProviderPromoViewController
     : UIViewController <CredentialProviderPromoConsumer>
+
+// The action handler for interactions in this view controller.
+@property(nonatomic, weak) id<ConfirmationAlertActionHandler> actionHandler;
 
 @end
 
