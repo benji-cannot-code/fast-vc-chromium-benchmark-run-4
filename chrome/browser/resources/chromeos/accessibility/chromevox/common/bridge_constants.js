1be99b2aa1330793d9a8a45de97480bbdbdffc04
Anastasia Helfinstein <anastasi@google.com>
2023-02-09 22:12:09 +0000

    [ChromeVox] Fix virtual braille display routing
    the panel via the onscreen buttons
    
    AX-Relnotes: (optional) Fixes routing of the virtual braille display in
    Bug: b:268110872
    Test: Added a new test
    Change-Id: Ia5079b61ad35a2870b3600dbc25afc4045d87a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234397
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103523}

--- chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
@@ -40,6 +40,8 @@ BridgeConstants.Braille = {
   TARGET: 'Braille',
   Action: {
     BACK_TRANSLATE: 'backTranslate',
+    PAN_LEFT: 'panLeft',
+    PAN_RIGHT: 'panRight',
     WRITE: 'write',
   },
 };
