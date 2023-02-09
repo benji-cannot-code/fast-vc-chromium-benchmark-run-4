cf72d0208b43f63f709104b1455154db26d9d394
Eugene Zemtsov <eugene@chromium.org>
2023-02-09 07:33:29 +0000

    webcodecs: Add ADTS support to AudioToolboxAudioEncoder
    Bug: 1407013
    Change-Id: Id2299d0d99b5ae0e795c3f16720e8e3de36e1b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227101
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103156}

--- media/filters/mac/audio_toolbox_audio_encoder.h
+++ media/filters/mac/audio_toolbox_audio_encoder.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_bus.h"
 #include "media/base/audio_encoder.h"
 #include "media/base/media_export.h"
+#include "media/formats/mp4/aac.h"
 
 namespace media {
 class AudioTimestampHelper;
@@ -64,6 +65,8 @@ class MEDIA_EXPORT AudioToolboxAudioEncoder : public AudioEncoder {
   std::unique_ptr<AudioTimestampHelper> timestamp_helper_;
 
   std::vector<uint8_t> codec_desc_;
+  std::vector<uint8_t> temp_output_buf_;
+  mp4::AAC aac_config_parser_;
 
   // Ensures the data sent to Encode() matches the encoder's input format.
   std::unique_ptr<ConvertingAudioFifo> fifo_;
