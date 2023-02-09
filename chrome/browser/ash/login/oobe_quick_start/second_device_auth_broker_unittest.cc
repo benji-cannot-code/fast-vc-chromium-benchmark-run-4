2899fc98145b5987ce79a0f9a23f07c23b6959c7
Anastasiia N <anastasiian@chromium.org>
2023-02-09 11:53:46 +0000

    CodeHealth: Replace RunLoop with TestFuture
    Replace the usage of `base::RunLoop` with `base::test::TestFuture` in
    second_device_auth_broker_unittest.cc.
    
    Bug: b/266783937
    Test: unit_tests --gtest_filter="*SecondDeviceAuthBrokerTest*"
    Change-Id: Ic8b3be550e4b004031834875b7d7cc6900e32f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231782
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103224}

--- chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker_unittest.cc
+++ chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker_unittest.cc
@@ -10,9 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/scoped_refptr.h"
-#include "base/run_loop.h"
-#include "base/test/bind.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_future.h"
 #include "base/types/expected.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/fido_assertion_info.h"
 #include "chromeos/ash/components/attestation/attestation_flow.h"
@@ -182,61 +181,31 @@ class SecondDeviceAuthBrokerTest : public ::testing::Test {
 
  protected:
   base::expected<std::string, GoogleServiceAuthError> GetChallengeBytes() {
-    base::expected<std::string, GoogleServiceAuthError> response;
-    base::RunLoop run_loop;
-    SecondDeviceAuthBroker::ChallengeBytesCallback callback =
-        base::BindLambdaForTesting(
-            [&response, &run_loop](
-                const base::expected<std::string, GoogleServiceAuthError>&
-                    returned_response) -> void {
-              response = returned_response;
-              run_loop.Quit();
-            });
-    second_device_auth_broker_.GetChallengeBytes(std::move(callback));
-    run_loop.Run();
-
-    return response;
+    base::test::TestFuture<
+        const base::expected<std::string, GoogleServiceAuthError>&>
+        future;
+    second_device_auth_broker_.GetChallengeBytes(future.GetCallback());
+    return future.Get();
   }
 
   base::expected<std::string, SecondDeviceAuthBroker::AttestationErrorType>
   FetchAttestationCertificate(const std::string& fido_credential_id) {
-    base::expected<std::string, SecondDeviceAuthBroker::AttestationErrorType>
-        response;
-    base::RunLoop run_loop;
-    SecondDeviceAuthBroker::AttestationCertificateCallback callback =
-        base::BindLambdaForTesting(
-            [&response, &run_loop](
-                const base::expected<
-                    std::string, SecondDeviceAuthBroker::AttestationErrorType>&
-                    returned_response) -> void {
-              response = returned_response;
-              run_loop.Quit();
-            });
-    second_device_auth_broker_.FetchAttestationCertificate(fido_credential_id,
-                                                           std::move(callback));
-    run_loop.Run();
-
-    return response;
+    base::test::TestFuture<const base::expected<
+        std::string, SecondDeviceAuthBroker::AttestationErrorType>&>
+        future;
+    second_device_auth_broker_.FetchAttestationCertificate(
+        fido_credential_id, future.GetCallback());
+    return future.Get();
   }
 
   SecondDeviceAuthBroker::RefreshTokenResponse FetchRefreshToken(
       const FidoAssertionInfo& fido_assertion_info,
       const std::string& certificate) {
-    SecondDeviceAuthBroker::RefreshTokenResponse response;
-    base::RunLoop run_loop;
-    SecondDeviceAuthBroker::RefreshTokenCallback callback =
-        base::BindLambdaForTesting(
-            [&response,
-             &run_loop](const SecondDeviceAuthBroker::RefreshTokenResponse&
-                            returned_response) -> void {
-              response = returned_response;
-              run_loop.Quit();
-            });
+    base::test::TestFuture<const SecondDeviceAuthBroker::RefreshTokenResponse&>
+        future;
     second_device_auth_broker_.FetchRefreshToken(
-        fido_assertion_info, certificate, std::move(callback));
-    run_loop.Run();
-
-    return response;
+        fido_assertion_info, certificate, future.GetCallback());
+    return future.Get();
   }
 
   void AddFakeResponse(const std::string& url,
