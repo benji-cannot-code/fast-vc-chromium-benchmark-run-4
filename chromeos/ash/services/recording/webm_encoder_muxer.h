afbd5d3c915fa74774854582c03de95616c1291a
Ahmed Fakhry <afakhry@chromium.org>
2023-02-09 22:44:31 +0000

    gif_recording: Implement the skeleton of GIF encoding
    This CL implements the skeleton code needed for adding the support
    for GIF encoding. It also renames the `RecordingEncoderMuxer` class
    to `WebmEncoderMuxer` so that the recording type is now clear.
    
    Later CLs will add fill in the details of the GIF encoding.
    
    Fixed: b/268123327
    Test: Manually, existing tests.
    Change-Id: I924f921ad5c02d10b4c2de23ba64f83ebbd2a523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4230191
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103535}

--- chromeos/ash/services/recording/webm_encoder_muxer.h
+++ chromeos/ash/services/recording/webm_encoder_muxer.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMEOS_ASH_SERVICES_RECORDING_RECORDING_ENCODER_MUXER_H_
-#define CHROMEOS_ASH_SERVICES_RECORDING_RECORDING_ENCODER_MUXER_H_
+#ifndef CHROMEOS_ASH_SERVICES_RECORDING_WEBM_ENCODER_MUXER_H_
+#define CHROMEOS_ASH_SERVICES_RECORDING_WEBM_ENCODER_MUXER_H_
 
 #include <memory>
 
@@ -39,7 +39,6 @@ namespace mojom {
 class DriveFsQuotaDelegate;
 }  // namespace mojom
 
-// TODO(afakhry): Rename this class to `WebmEncoderMuxer`.
 // Encapsulates encoding and muxing audio and video frame. An instance of this
 // object can only be interacted with via a |base::SequenceBound| wrapper, which
 // guarantees all encoding and muxing operations as well as destruction of the
@@ -49,9 +48,9 @@ class DriveFsQuotaDelegate;
 //
 // This object performs VP8 video encoding and Opus audio encoding, and mux the
 // audio and video encoded frames into a Webm container.
-class RecordingEncoderMuxer : public RecordingEncoder {
+class WebmEncoderMuxer : public RecordingEncoder {
  private:
-  using PassKey = base::PassKey<RecordingEncoderMuxer>;
+  using PassKey = base::PassKey<WebmEncoderMuxer>;
 
  public:
   // Creates an instance of this class that is bound to the given sequenced
@@ -67,7 +66,7 @@ class RecordingEncoderMuxer : public RecordingEncoder {
   // EncodeAudio() will be ignored.
   //
   // By default, |on_failure_callback| will be called on the same sequence of
-  // |blocking_task_runner| (unless the caller binds the given callbacks to a
+  // |blocking_task_runner| (unless the caller binds the given callback to a
   // different sequence by means of base::BindPostTask()).
   static base::SequenceBound<RecordingEncoder> Create(
       scoped_refptr<base::SequencedTaskRunner> blocking_task_runner,
@@ -77,16 +76,16 @@ class RecordingEncoderMuxer : public RecordingEncoder {
       const base::FilePath& webm_file_path,
       OnFailureCallback on_failure_callback);
 
-  RecordingEncoderMuxer(
+  WebmEncoderMuxer(
       PassKey,
       const media::VideoEncoder::Options& video_encoder_options,
       const media::AudioParameters* audio_input_params,
       mojo::PendingRemote<mojom::DriveFsQuotaDelegate> drive_fs_quota_delegate,
       const base::FilePath& webm_file_path,
       OnFailureCallback on_failure_callback);
-  RecordingEncoderMuxer(const RecordingEncoderMuxer&) = delete;
-  RecordingEncoderMuxer& operator=(const RecordingEncoderMuxer&) = delete;
-  ~RecordingEncoderMuxer() override;
+  WebmEncoderMuxer(const WebmEncoderMuxer&) = delete;
+  WebmEncoderMuxer& operator=(const WebmEncoderMuxer&) = delete;
+  ~WebmEncoderMuxer() override;
 
   // RecordingEncoder:
   void InitializeVideoEncoder(
@@ -205,10 +204,10 @@ class RecordingEncoderMuxer : public RecordingEncoder {
   bool is_audio_encoder_initialized_ GUARDED_BY_CONTEXT(sequence_checker_) =
       false;
 
-  base::WeakPtrFactory<RecordingEncoderMuxer> weak_ptr_factory_
+  base::WeakPtrFactory<WebmEncoderMuxer> weak_ptr_factory_
       GUARDED_BY_CONTEXT(sequence_checker_){this};
 };
 
 }  // namespace recording
 
-#endif  // CHROMEOS_ASH_SERVICES_RECORDING_RECORDING_ENCODER_MUXER_H_
+#endif  // CHROMEOS_ASH_SERVICES_RECORDING_WEBM_ENCODER_MUXER_H_
