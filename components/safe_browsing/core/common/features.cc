2bee34aba0184fc5b04c8570b07f6ba38fd966bb
Daniel Rubery <drubery@chromium.org>
2023-02-09 01:55:57 +0000

    Add feature flag for enforcing timeout over entire download inspection
    Ensuring that download inspection terminates in a timely fashion
    improves the user experience, but may have a negative effect on
    protection. This CL adds a feature flag and conditionally enables
    the stricter timeout so that we can measure both impacts.
    
    Bug: 1373679
    Change-Id: Ia83e8eec8edc1820f91a2bdb243270b9bb92ad60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4233977
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103071}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -203,6 +203,14 @@ BASE_FEATURE(kSimplifiedUrlDisplay,
              "SimplifiedUrlDisplay",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kStrictDownloadTimeout,
+             "SafeBrowsingStrictDownloadtimeout",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+constexpr base::FeatureParam<int> kStrictDownloadTimeoutMilliseconds{
+    &kStrictDownloadTimeout, "TimeoutMilliseconds",
+    /*default_value=*/7000};
+
 BASE_FEATURE(kSuspiciousSiteTriggerQuotaFeature,
              "SafeBrowsingSuspiciousSiteTriggerQuota",
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -275,6 +283,7 @@ constexpr struct {
     {&kSafeBrowsingRemoveCookiesInAuthRequests, true},
     {&kSevenZipEvaluationEnabled, true},
     {&kSimplifiedUrlDisplay, true},
+    {&kStrictDownloadTimeout, true},
     {&kSuspiciousSiteTriggerQuotaFeature, true},
     {&kTailoredSecurityDesktopNotice, true},
     {&kTailoredSecurityIntegration, true},
