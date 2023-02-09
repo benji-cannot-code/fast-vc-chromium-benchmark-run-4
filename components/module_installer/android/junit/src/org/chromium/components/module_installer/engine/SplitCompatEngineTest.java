fe7801cc5379fecb4fdf762652b0553c1865319d
Theresa Sullivan <twellington@chromium.org>
2023-02-09 17:49:36 +0000

    Revert "Android: Update play core library"
    This reverts commit 2b1a87b21ec3c942ee7d78955f9a20a714f55ccf.
    
    Reason for revert: compilation failure on https://ci.chromium.org/ui/p/chrome/builders/ci/clang-builder/28346/overview
    
    Original change's description:
    > Android: Update play core library
    >
    > Also allow downstream to override the play core library.
    >
    > Bug: 1411347, 1413741
    > Binary-Size: Updated google play basement/libs.
    > Change-Id: I18741b4e4b5c38f923e722daeea2c3d055cc389c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4228108
    > Auto-Submit: Peter Wen <wnwen@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Peter Wen <wnwen@chromium.org>
    > Owners-Override: Peter Wen <wnwen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1103299}
    
    Bug: 1411347, 1413741
    Change-Id: Ia521af19b28401ffc7ab25d90f68423a45f99dd4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236514
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Owners-Override: Theresa Sullivan <twellington@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1103343}

--- components/module_installer/android/junit/src/org/chromium/components/module_installer/engine/SplitCompatEngineTest.java
+++ components/module_installer/android/junit/src/org/chromium/components/module_installer/engine/SplitCompatEngineTest.java
@@ -16,8 +16,6 @@ import static org.mockito.Mockito.verify;
 
 import android.app.Activity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.play.core.splitinstall.SplitInstallException;
 import com.google.android.play.core.splitinstall.SplitInstallManager;
 import com.google.android.play.core.splitinstall.SplitInstallRequest;
@@ -25,6 +23,8 @@ import com.google.android.play.core.splitinstall.SplitInstallSessionState;
 import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener;
 import com.google.android.play.core.splitinstall.model.SplitInstallErrorCode;
 import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus;
+import com.google.android.play.core.tasks.OnFailureListener;
+import com.google.android.play.core.tasks.Task;
 
 import org.junit.Before;
 import org.junit.Test;
