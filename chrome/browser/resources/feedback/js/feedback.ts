8d00058be041fb97faef97b3af3b15c760880a95
Norge Vizcay <vizcay@google.com>
2023-02-09 11:36:25 +0000

    [AutofillFeedback] Push send_autofill_metadata feedback param to native.
    The CL sends to native the state (checked/unchecked) of the autofill metadata checkbox present on the feedback UI.
    
    Bug: 1407646
    Change-Id: I0f880d740a364314ec43494b94ddc2b51b831434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224640
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103218}

--- chrome/browser/resources/feedback/js/feedback.ts
+++ chrome/browser/resources/feedback/js/feedback.ts
@@ -425,7 +425,6 @@ function sendReport(): boolean {
   feedbackInfo.email =
       getRequiredElement<HTMLSelectElement>('user-email-drop-down').value;
 
-  // TODO(crbug.com/1407646): Send autofill metadata if checkbox is checked.
   let useSystemInfo = false;
   let useHistograms = false;
   const checkbox = $<HTMLInputElement>('sys-info-checkbox');
@@ -433,6 +432,12 @@ function sendReport(): boolean {
     // Send histograms along with system info.
     useHistograms = true;
     useSystemInfo = true;
+  }
+
+  const autofillCheckbox = $<HTMLInputElement>('autofill-metadata-checkbox');
+  if (autofillCheckbox != null && autofillCheckbox.checked &&
+      !getRequiredElement('autofill-checkbox-container').hidden) {
+    feedbackInfo.sendAutofillMetadata = true;
   }
 
   // <if expr="chromeos_ash">
@@ -659,8 +664,7 @@ function initialize() {
     const autofillMetadataUrlElement = $('autofill-metadata-url');
 
     if (autofillMetadataUrlElement) {
-      // Opens a new window showing the full anonymized system+app
-      // information.
+      // Opens a new window showing the full anonymized autofill metadata.
       autofillMetadataUrlElement.onclick = function(e) {
         e.preventDefault();
 
--- tools/typescript/definitions/feedback_private.d.ts
+++ tools/typescript/definitions/feedback_private.d.ts
@@ -41,6 +41,7 @@ declare namespace chrome {
       attachedFileBlobUuid?: string;
       screenshotBlobUuid?: string;
       useSystemWindowFrame?: boolean;
+      sendAutofillMetadata?: boolean;
       sendBluetoothLogs?: boolean;
       sendTabTitles?: boolean;
       assistantDebugInfoAllowed?: boolean;
