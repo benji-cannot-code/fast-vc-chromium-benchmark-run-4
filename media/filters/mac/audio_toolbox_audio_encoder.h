03b94eba40e772e375ae3e6b71a4116a098c4ab6
Rushan Suleymanov <rushans@google.com>
2023-02-09 08:19:38 +0000

    Revert "webcodecs: Add ADTS support to AudioToolboxAudioEncoder"
    This reverts commit 6c30ad1fbb58d56094fb32189291b391ab048b53.
    
    Reason for revert: Tree is closed due to build failure.
    
    Original change's description:
    > webcodecs: Add ADTS support to AudioToolboxAudioEncoder
    >
    > Bug: 1407013
    > Change-Id: Id2299d0d99b5ae0e795c3f16720e8e3de36e1b71
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227101
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1103156}
    
    Bug: 1407013
    Change-Id: I894f6ae24135c9047b5344e3d5c33b61100721ee
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236397
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Owners-Override: Rushan Suleymanov <rushans@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1103164}

--- media/filters/mac/audio_toolbox_audio_encoder.h
+++ media/filters/mac/audio_toolbox_audio_encoder.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_bus.h"
 #include "media/base/audio_encoder.h"
 #include "media/base/media_export.h"
-#include "media/formats/mp4/aac.h"
 
 namespace media {
 class AudioTimestampHelper;
@@ -65,8 +64,6 @@ class MEDIA_EXPORT AudioToolboxAudioEncoder : public AudioEncoder {
   std::unique_ptr<AudioTimestampHelper> timestamp_helper_;
 
   std::vector<uint8_t> codec_desc_;
-  std::vector<uint8_t> temp_output_buf_;
-  mp4::AAC aac_config_parser_;
 
   // Ensures the data sent to Encode() matches the encoder's input format.
   std::unique_ptr<ConvertingAudioFifo> fifo_;
