bad642281768f278c2a5425f6c201c4df792740e
Yue Zhang <yuezhanggg@chromium.org>
2023-02-09 18:34:02 +0000

    [ChromeCart][DOM] Add DOM-based specific patterns for AddToCart
    Previously, we monitor almost all POST XHR requests for AddToCart
    detection. When DOM-based AddToCart heuristics are enabled, we are
    monitoring way less requests, which allow us to relax our matching
    pattern a bit. This CL adds another set of heuristics in order to
    capture more AddToCart actions when DOM-based AddToCart is enabled.
    Please note that this set of heuristics should only be used with DOM-
    based AddToCart heuristics. This solution will help us capture
    AddToCart actions that we weren't able to before.
    
    Bug: 1412503
    Change-Id: I9fbec9eaad707d29d6937fa71c80c1ec3bab769e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226159
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103376}

--- chrome/renderer/cart/commerce_hint_agent_unittest.cc
+++ chrome/renderer/cart/commerce_hint_agent_unittest.cc
@@ -45,12 +45,27 @@ const char* kAddToCart[] = {
   "isQuickAddToCartButton=true", // costco.com
 };
 
+const char* kAddToCartWithDOMBasedHeuristics[] = {
+  "{\"productId\":\"123\",\"quantity\":14}",
+  "{\"productId\":\"123\",\"quantity\":\"151\"}",
+  "{\"Quantity\":\"2\"}",
+  "{\"cart_quantity\":\"23\"}",
+  "{\"product_quantity\":76}",
+};
+
 const char* kNotAddToCart[] = {
   "misadd-to-cart",
   "add_to_cartoon",
   "_add_to_basketball",
   "cart/address",
   "golfcart/add",
+};
+
+const char* kNotAddToCartWithDOMBasedHeuristics[] = {
+  "{\"productName\":\"quantity_calculator\"}",
+  "quantity=1",
+  "{\"product_quantity\":ab}",
+  "{\"quality\":\"2\"}",
 };
 
 const char* kVisitCart[] = {
@@ -823,6 +838,17 @@ TEST_F(CommerceHintAgentUnitTest, IsAddToCart) {
 #endif
       {{"add-to-cart-pattern", "foo"}});
   EXPECT_FALSE(CommerceHintAgent::IsAddToCart("request_bar"));
+}
+
+TEST_F(CommerceHintAgentUnitTest, IsAddToCartForDomBasedHeuristics) {
+  for (auto* str : kAddToCartWithDOMBasedHeuristics) {
+    EXPECT_TRUE(CommerceHintAgent::IsAddToCartForDomBasedHeuristics(str))
+        << str;
+  }
+  for (auto* str : kNotAddToCartWithDOMBasedHeuristics) {
+    EXPECT_FALSE(CommerceHintAgent::IsAddToCartForDomBasedHeuristics(str))
+        << str;
+  }
 }
 
 TEST_F(CommerceHintAgentUnitTest, IsAddToCart_SkipLengthLimit) {
