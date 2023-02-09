158d0ca5ab0b6cfaddeb63dfa3ef06bb72d05b7c
Mustaq Ahmed <mustaq@google.com>
2023-02-09 16:00:13 +0000

    Make PointerEventManager gracefully handle stray pointerup events.
    PointerEventManager's user activation trigger code currently works
    with the (hidden) assumption that it won't receive a pointerup event
    while the page is scrolling.  While this is a correct assumption for
    the events it fires to JS, internally this is not true when the page
    has neither pointer nor touch event listeners!  At the end of a
    gesture scroll on such a page, WidgetInputHandlerManager have been
    firing a pointerup to the renderer for years, the exact reason for
    which now hidden under years of changes in the long event pipeline!
    
    At PEM, this assumption didn't apparently cause any issue (perhaps!) until we refactored some TouchEventManager code to PEM gradually.  At one point of the refactoring, the broken assumption exposed as an unexpected user activation trigger during scrolling.  More precisely, when PEM clears the state representing an "on-going touch-scroll"
    (i.e. `non_hovering_pointers_canceled_`) at the end of a touch
    gesture, the pointerup clears the state a bit too early.
    
    This CL correctly skips user activation for such pointerup events
    without changing any legacy state elsewhere.
    
    Bug: 1313076
    Change-Id: I9ad07c507d8a2337a21ba424167c43b45f590db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219420
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103291}

--- third_party/blink/renderer/core/input/pointer_event_manager.cc
+++ third_party/blink/renderer/core/input/pointer_event_manager.cc
@@ -678,12 +678,6 @@ WebInputEventResult PointerEventManager::HandlePointerEvent(
     return WebInputEventResult::kHandledSystem;
   }
 
-  if (!event.hovering) {
-    if (!touch_event_manager_->IsAnyTouchActive()) {
-      non_hovering_pointers_canceled_ = false;
-    }
-  }
-
   // The rest of this function does not handle hovering
   // (i.e. mouse like) events yet.
 
@@ -742,6 +736,9 @@ WebInputEventResult PointerEventManager::HandlePointerEvent(
         mojom::blink::UserActivationNotificationType::kInteraction);
   }
 
+  if (!event.hovering && !IsAnyTouchActive()) {
+    non_hovering_pointers_canceled_ = false;
+  }
   Node* pointerdown_node = nullptr;
   if (event.GetType() == WebInputEvent::Type::kPointerDown) {
     pointerdown_node =
