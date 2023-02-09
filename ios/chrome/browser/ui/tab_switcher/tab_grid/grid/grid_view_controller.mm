f6f5c26ad6a54e109ed0f65270c83cfc90e67e97
Louis Romero <lpromero@google.com>
2023-02-09 14:54:01 +0000

    Add a proper reuse identifier in GridViewController
    Prior to this CL, UICollectionElementKindSectionHeader was used as reuse
    identifier. While it technically works with just one header class to
    reuse, it's not a proper usage.
    
    In any case, a follow-up CL for Inactive Tabs is adding a new header
    class, so we'll need to have proper reuse identifiers anyway.
    
    Change-Id: Ie6419af9c7c29caca33fa5fd1cad0da7e96c9822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4233060
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103266}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -67,6 +67,7 @@ NSString* const kCellIdentifier = @"GridCellIdentifier";
 NSString* const kPlusSignCellIdentifier = @"PlusSignCellIdentifier";
 NSString* const kSuggestedActionsCellIdentifier =
     @"SuggestedActionsCellIdentifier";
+NSString* const kGridHeaderIdentifier = @"GridHeaderIdentifier";
 
 // Creates an NSIndexPath with `index` in section 0.
 NSIndexPath* CreateIndexPath(NSInteger index) {
@@ -204,7 +205,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
       forCellWithReuseIdentifier:kSuggestedActionsCellIdentifier];
   [collectionView registerClass:[GridHeader class]
       forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
-             withReuseIdentifier:UICollectionElementKindSectionHeader];
+             withReuseIdentifier:kGridHeaderIdentifier];
 
   // During deletion (in horizontal layout) the backgroundView can resize,
   // revealing temporarily the collectionView background. This makes sure
@@ -511,10 +512,10 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 - (UICollectionReusableView*)collectionView:(UICollectionView*)collectionView
           viewForSupplementaryElementOfKind:(NSString*)kind
                                 atIndexPath:(NSIndexPath*)indexPath {
-  GridHeader* headerView =
-      [collectionView dequeueReusableSupplementaryViewOfKind:kind
-                                         withReuseIdentifier:kind
-                                                forIndexPath:indexPath];
+  GridHeader* headerView = [collectionView
+      dequeueReusableSupplementaryViewOfKind:kind
+                         withReuseIdentifier:kGridHeaderIdentifier
+                                forIndexPath:indexPath];
   if ([kind isEqualToString:UICollectionElementKindSectionHeader]) {
     switch (indexPath.section) {
       case kOpenTabsSectionIndex: {
