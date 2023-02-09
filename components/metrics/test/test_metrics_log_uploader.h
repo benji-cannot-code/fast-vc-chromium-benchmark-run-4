08b60fcf0c4977e14c837e34771b9d968fc23de7
Tom Sepez <tsepez@chromium.org>
2023-02-09 20:40:31 +0000

    Use actual weak ptr in TestMetricsServiceClient::uploader_
    Avoids a dangling raw_ptr<> warning, and makes implementation
    safely match the comment.
    
    Bug: 1401495
    Change-Id: I700361ab3d05beff0a77e5437049f4abbe8a66ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234350
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103460}

--- components/metrics/test/test_metrics_log_uploader.h
+++ components/metrics/test/test_metrics_log_uploader.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_METRICS_TEST_TEST_METRICS_LOG_UPLOADER_H_
 #define COMPONENTS_METRICS_TEST_TEST_METRICS_LOG_UPLOADER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "components/metrics/metrics_log_uploader.h"
 #include "third_party/metrics_proto/reporting_info.pb.h"
 
 namespace metrics {
 
-class TestMetricsLogUploader : public MetricsLogUploader {
+class TestMetricsLogUploader
+    : public MetricsLogUploader,
+      public base::SupportsWeakPtr<TestMetricsLogUploader> {
  public:
   explicit TestMetricsLogUploader(
       const MetricsLogUploader::UploadCallback& on_upload_complete);
