31a64e88eff56c70edd7b8ff44596313fb4b9cc9
Phillis Tang <phillis@chromium.org>
2023-02-09 21:33:39 +0000

    Revert "[iOS] Add EG tests for feed header IPH"
    This reverts commit 7296ffe6161eb122bafa2d2a35bab743f7c28abc.
    
    Reason for revert: cause test failures on ios-simulator-noncq, e.g.:
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/31870/overview
    
    Original change's description:
    > [iOS] Add EG tests for feed header IPH
    >
    > Bug: 1413744
    > Change-Id: I19d1315fab77a69756119eafb605c76d3acbdc23
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234138
    > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > Commit-Queue: Adam Arcaro <adamta@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1103260}
    
    Bug: 1413744
    Change-Id: I977724cf584a716de3c321fe8ec8bb28cc137638
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237447
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103496}

--- ios/chrome/browser/feature_engagement/feature_engagement_app_interface.h
+++ ios/chrome/browser/feature_engagement/feature_engagement_app_interface.h
@@ -56,11 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // NO if FeatureEngagementTracker failed to load.
 + (BOOL)enableOverflowMenuTipTriggering [[nodiscard]];
 
-// Enables the feed header tip to be triggered. Clients must call +reset after
-// the test finish running. Returns NO if FeatureEngagementTracker failed to
-// load.
-+ (BOOL)enableFeedHeaderTipTriggering [[nodiscard]];
-
 // Starts manual page translation.
 + (void)showTranslate;
 
