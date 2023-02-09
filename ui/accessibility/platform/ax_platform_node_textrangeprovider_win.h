fd82526e62e1987eb87a84df1193a85864800e37
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2023-02-09 02:33:23 +0000

    Revert "ax_ui: change AXTreeObserver::OnTreeManagerWillBeRemoved"
    This reverts commit 830cb7c8e84d75125746a4d37e98748562ace0e1.
    
    Reason for revert: The TextRangeEndpoints observer doesn't get removed from the map of observers in AXTree since this change landed. This is causing many crashes with Narrator and UIA enabled. Reverting for now, but need to investigate more.
    
    Original change's description:
    > ax_ui: change AXTreeObserver::OnTreeManagerWillBeRemoved
    >
    > Make OnTreeManagerWillBeRemoved dealing with AXTree instance directly
    > rather than operating with ax tree ids simplify code logic as well
    > it helps to avoid poking into ax tree map.
    >
    > A code cleanup in support of bug 1394743 which was revealed because of
    > tree id divergence between an ax tree and ax positions when ax tree id
    > is changed.
    >
    > Bug: 1394743
    > Change-Id: I6251a1819dedf55886a673cf1832942319994637
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4122368
    > Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1097589}
    
    Bug: 1394743
    Change-Id: I98ed7e811e1363082092df9cf99210e8fbdf4fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234400
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1103087}

--- ui/accessibility/platform/ax_platform_node_textrangeprovider_win.h
+++ ui/accessibility/platform/ax_platform_node_textrangeprovider_win.h
@@ -266,7 +266,7 @@ class COMPONENT_EXPORT(AX_PLATFORM) __declspec(uuid(
 
     void OnSubtreeWillBeDeleted(AXTree* tree, AXNode* node) override;
     void OnNodeDeleted(AXTree* tree, AXNodeID node_id) override;
-    void OnTreeManagerWillBeRemoved(AXTree* tree) override;
+    void OnTreeManagerWillBeRemoved(AXTreeID previous_tree_id) override;
 
    private:
     struct DeletionOfInterest {
