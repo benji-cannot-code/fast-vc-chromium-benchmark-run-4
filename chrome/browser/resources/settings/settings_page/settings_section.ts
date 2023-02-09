8ad3ac4a1317559339023f800f74211176ce0cde
Charles Meng <charlesmeng@chromium.org>
2023-02-09 01:16:00 +0000

    Settings: add optional send feedback button to section headers
    Adds a send feedback button to settings-section, which can be enabled
    by adding a `send-feedback` attribute and a `on-send-feedback` event
    handler.
    
    This is a precursor to moving the performance settings page's send
    feedback entrypoint outside of the toggle button sub-labels.
    
    Bug: 1377051
    Change-Id: I33c276308747a0913bbe10ba2cc739e4008e467b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218648
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103057}

--- chrome/browser/resources/settings/settings_page/settings_section.ts
+++ chrome/browser/resources/settings/settings_page/settings_section.ts
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *    </settings-section>
  */
 
+import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import '//resources/cr_elements/cr_shared_vars.css.js';
 
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -59,12 +60,22 @@ export class SettingsSectionElement extends PolymerElement {
         value: false,
         reflectToAttribute: true,
       },
+
+      /**
+       * When this attribute is enabled, a send feedback button will be shown
+       * that emits a 'send-feedback' event.
+       */
+      showSendFeedbackButton: {
+        type: Boolean,
+        value: false,
+      },
     };
   }
 
   section: string;
   pageTitle: string;
   hiddenBySearch: boolean;
+  showSendFeedbackButton: boolean;
 
   /**
    * Get the value to which to set the aria-hidden attribute of the section
@@ -79,6 +90,11 @@ export class SettingsSectionElement extends PolymerElement {
 
   override focus() {
     this.shadowRoot!.querySelector<HTMLElement>('.title')!.focus();
+  }
+
+  private onSendFeedbackClick_() {
+    this.dispatchEvent(
+        new CustomEvent('send-feedback', {bubbles: true, composed: true}));
   }
 }
 
