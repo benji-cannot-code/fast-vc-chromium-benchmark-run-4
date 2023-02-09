43d37041c495777dae277482c056d9d0e1d0a6e0
Lei Zhang <thestig@chromium.org>
2023-02-09 01:56:57 +0000

    Remove base::SequenceBound usage in content::EmbeddedWorkerInstance.
    It is no longer needed, since EmbeddedWorkerInstance and its
    ServiceWorkerContentSettingsProxyImpl both run on the UI thread now.
    
    Bug: 824858
    Change-Id: I6baa89dffe5edf49210fde5c1bdfe5c1b596640e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4207049
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103072}

--- content/browser/service_worker/embedded_worker_instance.h
+++ content/browser/service_worker/embedded_worker_instance.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
-#include "base/threading/sequence_bound.h"
 #include "base/unguessable_token.h"
 #include "components/services/storage/public/cpp/buckets/bucket_locator.h"
 #include "content/browser/service_worker/embedded_worker_status.h"
@@ -376,9 +375,7 @@ class CONTENT_EXPORT EmbeddedWorkerInstance
   ServiceWorkerMetrics::StartSituation start_situation_ =
       ServiceWorkerMetrics::StartSituation::UNKNOWN;
 
-  // TODO(crbug.com/824858): Remove SequenceBound when the core is the UI
-  // thread.
-  base::SequenceBound<ServiceWorkerContentSettingsProxyImpl> content_settings_;
+  std::unique_ptr<ServiceWorkerContentSettingsProxyImpl> content_settings_;
 
   mojo::SelfOwnedReceiverRef<network::mojom::URLLoaderFactory>
       script_loader_factory_;
