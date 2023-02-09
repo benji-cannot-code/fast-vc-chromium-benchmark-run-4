f35fc8a3d6fa9ea4aef6ad8d3456d0ae5a644e79
dpapad <dpapad@chromium.org>
2023-02-09 22:40:40 +0000

    CrOS Settings: Move a11y tests over to chrome://webui-test/ source.
    Also, simplify a bit chrome/test/data/webui/settings/chromeos/BUILD.gn
    by using source_target_relative in copy() commands, to avoid needing
    multiple such commands, just to specify different output folders.
    
    Bug: 1260297
    Change-Id: I3de3c0dc84f084b5abfd362e54963e4d46616465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236909
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103533}

--- chrome/test/data/webui/settings/chromeos/a11y/v3_os_a11y_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/a11y/v3_os_a11y_browsertest.js
@@ -148,7 +148,7 @@ function defineTest(testName, module, config) {
     /** @override */
     get browsePreload() {
       return `chrome://os-settings/test_loader.html?module=settings/chromeos/a11y/${
-          module}&host=test`;
+          module}`;
     }
 
     /** @override */
