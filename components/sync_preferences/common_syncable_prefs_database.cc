5a6d2d40e3e93f6e444e5bed572a8550588661fb
Ankush Singh <ankushkush@google.com>
2023-02-09 10:02:42 +0000

    [sync] Introduce CommonSyncablePrefsAllowlist for common syncable prefs
    This CL builds upon crrev.com/c/4224757 providing an implementation of
    SyncablePrefsAllowlist for common syncable preferences. By "common",
    we're referring to the preferences that are platform-independant are
    shared by all.
    
    Note: This CL does not populate the allowlist.
    
    Bug: 1401271
    Change-Id: I52d20b909ca1d654ec55131487ae2942c68b900f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224695
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103190}

---
+++ components/sync_preferences/common_syncable_prefs_database.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync_preferences/common_syncable_prefs_database.h"
+
+#include "base/containers/fixed_flat_set.h"
+
+namespace sync_preferences {
+
+constexpr auto kCommonSyncablePrefsAllowlist =
+    base::MakeFixedFlatSet<std::string_view>({"dummy"});
+
+bool CommonSyncablePrefsDatabase::IsPreferenceSyncable(
+    const std::string& pref_name) const {
+  return kCommonSyncablePrefsAllowlist.count(pref_name);
+}
+
+}  // namespace sync_preferences
