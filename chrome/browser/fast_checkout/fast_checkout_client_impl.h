07740948fbd87b94b4d1433402e1af12e4f01aad
Wolfgang Billenstein <bwolfgang@google.com>
2023-02-09 17:52:43 +0000

    [Fast Checkout] Make accessibility announcements
    Adds accessibility announcements for when a form was autofilled by Fast
    Checkout.
    
    Bug: 1334642
    Change-Id: I01f5d5207ecd7ca6f488380d08350e82ce339956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4232580
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103345}

--- chrome/browser/fast_checkout/fast_checkout_client_impl.h
+++ chrome/browser/fast_checkout/fast_checkout_client_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/scoped_observation.h"
+#include "chrome/browser/fast_checkout/fast_checkout_accessibility_service.h"
 #include "chrome/browser/fast_checkout/fast_checkout_capabilities_fetcher.h"
 #include "chrome/browser/fast_checkout/fast_checkout_client.h"
 #include "chrome/browser/fast_checkout/fast_checkout_enums.h"
@@ -163,6 +164,10 @@ class FastCheckoutClientImpl
   // notification.
   void UpdateFillingStates();
 
+  // Makes accessibility announcements for when a form was filled.
+  void A11yAnnounce(autofill::FormSignature form_signature,
+                    bool is_credit_card_form);
+
   // Triggers reparse with a delay of `kSleepBetweenTriggerReparseCalls`.
   // Reparsing updates the forms cache `autofill_manager_->form_structures()`
   // with current data from the renderer, eventually calling
@@ -194,6 +199,9 @@ class FastCheckoutClientImpl
 
   // Checks whether a run should be permitted or not.
   std::unique_ptr<FastCheckoutTriggerValidator> trigger_validator_;
+
+  // Makes a11y announcements.
+  std::unique_ptr<FastCheckoutAccessibilityService> accessibility_service_;
 
   // True if a run is ongoing; used to avoid multiple runs in parallel.
   bool is_running_ = false;
