bfb36c91f800c5f9a74474299278609b28fcb1ec
Brad Triebwasser <btriebw@chromium.org>
2023-02-09 02:04:12 +0000

    Rename "window-placement" permission to "window-management" in web platform tests.
    Rename "window-placement" permission to
    "window-management" in web platform tests.
    
    Bug: 1328581
    Change-Id: I983a8e4e20393b973d69be86ed11eb048df07016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000319
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103075}

--- third_party/blink/web_tests/external/wpt/screen-details/getScreenDetails.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/screen-details/getScreenDetails.tentative.https.window.js
@@ -9,7 +9,7 @@ promise_test(async t => {
 }, 'getScreenDetails() is present');
 
 promise_test(async t => {
-  await test_driver.set_permission({name: 'window-placement'}, 'granted');
+  await test_driver.set_permission({name: 'window-management'}, 'granted');
   const screenDetails = await self.getScreenDetails();
   assert_true(ScreenDetails.prototype.isPrototypeOf(screenDetails));
   assert_true(ScreenDetailed.prototype.isPrototypeOf(screenDetails.currentScreen));
@@ -20,14 +20,14 @@ promise_test(async t => {
 }, 'getScreenDetails() returns the expected interface with permission granted');
 
 promise_test(async t => {
-  await test_driver.set_permission({name: 'window-placement'}, 'granted');
+  await test_driver.set_permission({name: 'window-management'}, 'granted');
   assert_greater_than((await self.getScreenDetails()).screens.length, 0);
-  await test_driver.set_permission({name: 'window-placement'}, 'denied');
+  await test_driver.set_permission({name: 'window-management'}, 'denied');
   await promise_rejects_dom(t, 'NotAllowedError', self.getScreenDetails());
 }, 'getScreenDetails() rejects the promise with permission denied');
 
 promise_test(async t => {
-  await test_driver.set_permission({name: 'window-placement'}, 'granted');
+  await test_driver.set_permission({name: 'window-management'}, 'granted');
   let iframe = document.body.appendChild(document.createElement('iframe'));
   assert_greater_than((await iframe.contentWindow.getScreenDetails()).screens.length, 0);
 
@@ -51,7 +51,7 @@ promise_test(async t => {
 }, 'getScreenDetails() resolves for attached iframe; rejects for detached iframe');
 
 promise_test(async t => {
-  await test_driver.set_permission({name: 'window-placement'}, 'granted');
+  await test_driver.set_permission({name: 'window-management'}, 'granted');
   let iframe = document.body.appendChild(document.createElement('iframe'));
   const screenDetails = await iframe.contentWindow.getScreenDetails();
   assert_greater_than(screenDetails.screens.length, 0);
