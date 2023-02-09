8501274256c00a45ae20d1697c77521b32bec1c1
Pi-Hsun Shih <pihsun@chromium.org>
2023-02-09 08:05:39 +0000

    CCA: Always stop recording when Video.stop() is called
    There's an issue that Video.stop() doesn't always cancel the
    Video.start() call if the call is before the recording actually start.
    
    To solve this, add a stopped boolean that indicates that stop() has been
    called after start(), and check if it's set after the async operations
    before the recording actually starts.
    
    Bug: b:263560417
    Test: manually
    Change-Id: I44d9427240895a65c84726c5bca21562f13e4683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236277
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103163}

--- ash/webui/camera_app_ui/resources/js/device/mode/video.ts
+++ ash/webui/camera_app_ui/resources/js/device/mode/video.ts
@@ -220,6 +220,11 @@ export class Video extends ModeBase {
   private autoStopped = false;
 
   /**
+   * Whether the current recording should be stopped.
+   */
+  private stopped = false;
+
+  /**
    * HTMLElement displaying warning about low storage.
    */
   private readonly lowStorageWarningNudge = dom.get('#nudge', HTMLDivElement);
@@ -462,6 +467,7 @@ export class Video extends ModeBase {
     this.togglePausedInternal = null;
     this.everPaused = false;
     this.autoStopped = false;
+    this.stopped = false;
 
     if (this.recordingType === RecordType.NORMAL) {
       const canStart = await this.startMonitorStorage();
@@ -500,6 +506,10 @@ export class Video extends ModeBase {
     } catch (e) {
       toast.show(I18nString.ERROR_MSG_RECORD_START_FAILED);
       throw e;
+    }
+
+    if (this.stopped) {
+      throw new CanceledError('Recording stopped');
     }
 
     this.recordingType = this.getToggledRecordOption();
@@ -582,6 +592,7 @@ export class Video extends ModeBase {
   }
 
   override stop(): void {
+    this.stopped = true;
     if (loadTimeData.getChromeFlag(Flag.LOW_STORAGE_WARNING)) {
       ChromeHelper.getInstance().stopMonitorStorage();
     }
@@ -699,14 +710,17 @@ export class Video extends ModeBase {
         const onStart = () => {
           assert(this.mediaRecorder !== null);
 
+          if (this.stopped) {
+            this.mediaRecorder.stop();
+            return;
+          }
           state.set(state.State.RECORDING, true);
-          this.mediaRecorder.removeEventListener('start', onStart);
         };
 
         assert(this.mediaRecorder !== null);
         this.mediaRecorder.addEventListener('dataavailable', onDataAvailable);
         this.mediaRecorder.addEventListener('stop', onStop);
-        this.mediaRecorder.addEventListener('start', onStart);
+        this.mediaRecorder.addEventListener('start', onStart, {once: true});
 
         window.addEventListener('beforeunload', beforeUnloadListener);
 
