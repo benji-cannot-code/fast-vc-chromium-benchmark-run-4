e55ed66d8c167b43b3f437eb7970435a5b35fcff
Colin Blundell <blundell@chromium.org>
2023-02-10 09:31:39 +0000

    [GPU] Have SharedImageInterfaceInProcess pass SIFormat to GPU thread
    As a precursor to introducing SharedImageInterface methods that take in
    SharedImageFormat rather than ResourceFormat, this CL changes
    SharedImageInterfaceInProcess to convert ResourceFormat to
    SharedImageFormat in its implementations of the existing methods, rather
    than passing the ResourceFormat to GPU-thread methods that do the
    conversion. This will facilitate directly calling the GPU-thread methods
    from the new versions that take in SharedImageFormat.
    
    Bug: 1414192
    Change-Id: I2a1d6b9b730da616b78efa46a8cc86ac1bdd0065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236346
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103736}

--- gpu/command_buffer/service/shared_image_interface_in_process.cc
+++ gpu/command_buffer/service/shared_image_interface_in_process.cc
@@ -189,7 +189,8 @@ Mailbox SharedImageInterfaceInProcess::CreateSharedImage(
     ScheduleGpuTask(
         base::BindOnce(
             &SharedImageInterfaceInProcess::CreateSharedImageOnGpuThread,
-            base::Unretained(this), mailbox, format, surface_handle, size,
+            base::Unretained(this), mailbox,
+            viz::SharedImageFormat::SinglePlane(format), surface_handle, size,
             color_space, surface_origin, alpha_type, usage,
             MakeSyncToken(next_fence_sync_release_++)),
         {});
@@ -199,7 +200,7 @@ Mailbox SharedImageInterfaceInProcess::CreateSharedImage(
 
 void SharedImageInterfaceInProcess::CreateSharedImageOnGpuThread(
     const Mailbox& mailbox,
-    viz::ResourceFormat format,
+    viz::SharedImageFormat format,
     gpu::SurfaceHandle surface_handle,
     const gfx::Size& size,
     const gfx::ColorSpace& color_space,
@@ -215,9 +216,8 @@ void SharedImageInterfaceInProcess::CreateSharedImageOnGpuThread(
     return;
 
   DCHECK(shared_image_factory_);
-  auto si_format = viz::SharedImageFormat::SinglePlane(format);
   if (!shared_image_factory_->CreateSharedImage(
-          mailbox, si_format, size, color_space, surface_origin, alpha_type,
+          mailbox, format, size, color_space, surface_origin, alpha_type,
           surface_handle, usage)) {
     context_state_->MarkContextLost();
     return;
@@ -245,7 +245,8 @@ Mailbox SharedImageInterfaceInProcess::CreateSharedImage(
     ScheduleGpuTask(
         base::BindOnce(&SharedImageInterfaceInProcess::
                            CreateSharedImageWithDataOnGpuThread,
-                       base::Unretained(this), mailbox, format, size,
+                       base::Unretained(this), mailbox,
+                       viz::SharedImageFormat::SinglePlane(format), size,
                        color_space, surface_origin, alpha_type, usage,
                        MakeSyncToken(next_fence_sync_release_++),
                        std::move(pixel_data_copy)),
@@ -256,7 +257,7 @@ Mailbox SharedImageInterfaceInProcess::CreateSharedImage(
 
 void SharedImageInterfaceInProcess::CreateSharedImageWithDataOnGpuThread(
     const Mailbox& mailbox,
-    viz::ResourceFormat format,
+    viz::SharedImageFormat format,
     const gfx::Size& size,
     const gfx::ColorSpace& color_space,
     GrSurfaceOrigin surface_origin,
@@ -272,10 +273,9 @@ void SharedImageInterfaceInProcess::CreateSharedImageWithDataOnGpuThread(
     return;
 
   DCHECK(shared_image_factory_);
-  auto si_format = viz::SharedImageFormat::SinglePlane(format);
   if (!shared_image_factory_->CreateSharedImage(
-          mailbox, si_format, size, color_space, surface_origin, alpha_type,
-          usage, pixel_data)) {
+          mailbox, format, size, color_space, surface_origin, alpha_type, usage,
+          pixel_data)) {
     context_state_->MarkContextLost();
     return;
   }
