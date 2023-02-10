a50b67b65fa73609e2d2093a78572594ad99344c
Kevin Lubick <kjlubick@google.com>
2023-02-10 23:18:38 +0000

    Remove unused include of SkRandom.h
    The Skia team intends to remove SkRandom.h from the public API.
    
    This was the only known use of it in Chrome.
    
    Change-Id: If16da227b45db34b43f70206da05b795700b63d7
    Bug: skia:13983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242158
    Commit-Queue: Scott Violet <sky@chromium.org>
    Quick-Run: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104100}

--- components/tab_groups/tab_group_color.cc
+++ components/tab_groups/tab_group_color.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/strings/grit/components_strings.h"
-#include "third_party/skia/include/utils/SkRandom.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace tab_groups {
