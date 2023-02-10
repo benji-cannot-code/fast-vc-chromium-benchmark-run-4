81c352e55ad0fd0ee40172089a08312fed5a3b0d
Dave Tapuska <dtapuska@chromium.org>
2023-02-10 19:25:19 +0000

    [ios] Provide a module name for mojo test
    When blink is compiled in we get two objects in the generated code in
    the global namespace. But this goes away when you use module names,
    this is an easier fix than fixing the mojo code generation.
    
    Bug: 1411704
    Change-Id: I3033bb305854a5e63c26791be53a97824234959f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4241180
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103973}

--- ios/web/test/data/mojo_test.js
+++ ios/web/test/data/mojo_test.js
@@ -13,7 +13,7 @@ var pageImpl, browserProxy;
 
 /** @constructor */
 function TestPageImpl() {
-  this.binding = new mojo.Binding(TestPage, this);
+  this.binding = new mojo.Binding(web.mojom.TestPage, this);
 }
 
 TestPageImpl.prototype = {
@@ -38,12 +38,12 @@ function whenDomContentLoaded() {
 
 function main() {
   whenDomContentLoaded().then(function() {
-    browserProxy = new TestUIHandlerMojoPtr();
-    Mojo.bindInterface(TestUIHandlerMojo.name,
+    browserProxy = new web.mojom.TestUIHandlerMojoPtr();
+    Mojo.bindInterface(web.mojom.TestUIHandlerMojo.name,
                        mojo.makeRequest(browserProxy).handle);
 
     pageImpl = new TestPageImpl();
-    var pagePtr = new TestPagePtr();
+    var pagePtr = new web.mojom.TestPagePtr();
     pageImpl.binding.bind(mojo.makeRequest(pagePtr));
     browserProxy.setClientPage(pagePtr);
 
