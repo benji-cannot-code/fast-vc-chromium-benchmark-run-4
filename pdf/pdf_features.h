b2e81bea659946accdc86b0b30477ab1e140034a
Lei Zhang <thestig@chromium.org>
2023-02-10 01:21:33 +0000

    Delete chrome_pdf::kTabAcrossPDFAnnotations feature flag.
    The flag has been on by default for quite some time.
    
    Bug: 994500
    Change-Id: Id28d80740cef7ef83d463b56a8d02d88bd431d76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237817
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103613}

--- pdf/pdf_features.h
+++ pdf/pdf_features.h
@@ -19,7 +19,6 @@ BASE_DECLARE_FEATURE(kPdfIncrementalLoading);
 BASE_DECLARE_FEATURE(kPdfPartialLoading);
 BASE_DECLARE_FEATURE(kPdfUseSkiaRenderer);
 BASE_DECLARE_FEATURE(kPdfXfaSupport);
-BASE_DECLARE_FEATURE(kTabAcrossPDFAnnotations);
 
 }  // namespace features
 }  // namespace chrome_pdf
