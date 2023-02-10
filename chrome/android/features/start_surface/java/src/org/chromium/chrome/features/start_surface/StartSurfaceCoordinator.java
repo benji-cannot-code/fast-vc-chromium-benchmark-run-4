48b00e95983959760b3b52f213a1a14e86e5c061
Xi Han <hanxi@google.com>
2023-02-10 21:25:17 +0000

    [StartRefactor] Polish StartSurfaceMediator
    This CL makes the StartSurfaceMediator get the TabSwitcher#Controller
    from the TabSwitcherModule of the coordinator, rather than a parameter
    from its constructor. This is a follow up polish for CL:
    https://crrev.com/c/4220695.
    
    Bug: 1315676
    Change-Id: Ie8fbaf9b5b2369b1db185466f95250be479169bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4235180
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1104058}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinator.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinator.java
@@ -327,7 +327,7 @@ public class StartSurfaceCoordinator implements StartSurface {
                 || !ChromeFeatureList.sQueryTilesOnStart.isEnabled();
         mIsStartSurfaceRefactorEnabled =
                 ReturnToChromeUtil.isStartSurfaceRefactorEnabled(mActivity);
-        TabSwitcher.Controller controller;
+        TabSwitcher.Controller controller = null;
         Runnable initializeMVTilesRunnable = null;
         View logoContainerView = null;
         ViewGroup feedPlaceholderParentView = null;
@@ -359,7 +359,6 @@ public class StartSurfaceCoordinator implements StartSurface {
             // createSwipeRefreshLayout has to be called before creating any surface.
             createSwipeRefreshLayout();
             createStartSurfaceWithoutTasksSurface(excludeQueryTiles);
-            controller = mTabSwitcherModule.getController();
             initializeMVTilesRunnable = this::initializeMVTiles;
             logoContainerView = mView.findViewById(R.id.logo_container);
             feedPlaceholderParentView = mView.findViewById(R.id.tasks_surface_body);
