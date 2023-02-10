51d081507e17728c2d2741873026355f761799b6
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-02-10 10:19:04 +0000

    Remove unused argument in BeforeUnloadCompleted
    This is a follow-up for:
    https://chromium-review.googlesource.com/c/chromium/src/+/4160015
    
    Bug: None
    Change-Id: I2534e68e0c0c6c7ff63386f8e98c929b269e0134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173660
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103755}

--- chrome/browser/devtools/devtools_browsertest.cc
+++ chrome/browser/devtools/devtools_browsertest.cc
@@ -359,8 +359,7 @@ class DevToolsWindowBeforeUnloadObserver
   void Wait();
  private:
   // Invoked when the beforeunload handler fires.
-  void BeforeUnloadFired(bool proceed,
-                         const base::TimeTicks& proceed_time) override;
+  void BeforeUnloadFired(bool proceed) override;
 
   bool m_fired;
   scoped_refptr<content::MessageLoopRunner> message_loop_runner_;
@@ -380,9 +379,7 @@ void DevToolsWindowBeforeUnloadObserver::Wait() {
   message_loop_runner_->Run();
 }
 
-void DevToolsWindowBeforeUnloadObserver::BeforeUnloadFired(
-    bool proceed,
-    const base::TimeTicks& proceed_time) {
+void DevToolsWindowBeforeUnloadObserver::BeforeUnloadFired(bool proceed) {
   m_fired = true;
   if (message_loop_runner_.get())
     message_loop_runner_->Quit();
