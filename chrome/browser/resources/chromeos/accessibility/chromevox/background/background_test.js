e701075b6d453e397eae2228ce7f2c9bf369abd5
Anastasia Helfinstein <anastasi@google.com>
2023-02-10 00:50:33 +0000

    [ChromeVox] Clean up PointerHandler
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:265481751
    Test: cq
    Change-Id: I1f87702342104fb6d0bb5091c06fb214b07cd610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218137
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103591}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
@@ -87,9 +87,8 @@ ChromeVoxBackgroundTest = class extends ChromeVoxE2ETest {
   }
 
   simulateHitTestResult(node) {
-    return () => {
-      GestureCommandHandler.instance.pointerHandler_.handleHitTestResult(node);
-    };
+    return () => GestureCommandHandler.instance.pointerHandler_
+                     .handleHitTestResult_(node);
   }
 
   press(keyCode, modifiers) {
