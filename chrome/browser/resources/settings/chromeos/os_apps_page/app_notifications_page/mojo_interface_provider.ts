71ad0bcefc554c91a821ee5550cf02ed1643a2e4
Wes Okuhara <wesokuhara@google.com>
2023-02-10 21:48:04 +0000

    CrOS Settings: Generate TS version of app_notification_handler.mojom
    Bug: b:263413628, chromium:1002798
    Test: browser_tests --gtest_filter="OSSettingsApp*"
    Change-Id: Iee94299275478b42dd5abf188efbd0fbcd833843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4222140
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104068}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_notifications_page/mojo_interface_provider.ts
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_notifications_page/mojo_interface_provider.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {AppNotificationsHandler, AppNotificationsHandlerInterface} from '../../../mojom-webui/os_apps_page/app_notification_handler.mojom-webui.js';
+import {AppNotificationsHandler, AppNotificationsHandlerInterface} from '../../../mojom-webui/app_notification_handler.mojom-webui.js';
 
 let appNotificationProvider: AppNotificationsHandlerInterface|null = null;
 
--- chrome/test/data/webui/settings/chromeos/apps_page_test.js
+++ chrome/test/data/webui/settings/chromeos/apps_page_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://os-settings/chromeos/os_settings.js';
 
-import {AndroidAppsBrowserProxyImpl, appNotificationHandlerMojomWebui, createBoolPermission, Router, routes, routesMojomWebui, setAppNotificationProviderForTesting} from 'chrome://os-settings/chromeos/os_settings.js';
+import {AndroidAppsBrowserProxyImpl, appNotificationHandlerMojom, createBoolPermission, Router, routes, routesMojomWebui, setAppNotificationProviderForTesting} from 'chrome://os-settings/chromeos/os_settings.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 import {getDeepActiveElement} from 'chrome://resources/ash/common/util.js';
 import {PromiseResolver} from 'chrome://resources/js/promise_resolver.js';
@@ -15,7 +15,7 @@ import {flushTasks, waitAfterNextRender} from 'chrome://webui-test/polymer_test_
 import {TestAndroidAppsBrowserProxy} from './test_android_apps_browser_proxy.js';
 
 const {App, AppNotificationsObserverRemote, Readiness} =
-    appNotificationHandlerMojomWebui;
+    appNotificationHandlerMojom;
 
 /** @type {?OsSettingsAppsPageElement} */
 let appsPage = null;
