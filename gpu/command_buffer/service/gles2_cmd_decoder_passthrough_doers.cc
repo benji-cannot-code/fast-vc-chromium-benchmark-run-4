e7084ea56d77883c7c13b81c583800404ba48f67
Geoff Lang <geofflang@chromium.org>
2023-02-10 19:09:38 +0000

    Log the query types when glBeginQuery fails.
    This helps investigate errors in logs from the wild.
    
    Bug: chromium:1414653
    Change-Id: I5f448633dee071f8a84c1e2b01c022ef7cb03390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240777
    Auto-Submit: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103960}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/overlay_plane_data.h"
 #include "ui/gfx/overlay_priority_hint.h"
+#include "ui/gl/gl_enums.h"
 #include "ui/gl/gl_utils.h"
 #include "ui/gl/gl_version_info.h"
 #include "ui/gl/scoped_make_current.h"
@@ -3616,7 +3617,10 @@ error::Error GLES2DecoderPassthroughImpl::DoBeginQueryEXT(
 
     if (query_info->type != GL_NONE && query_info->type != target) {
       InsertError(GL_INVALID_OPERATION,
-                  "Query type does not match the target.");
+                  base::StringPrintf(
+                      "Query type (%s) does not match the target (%s).",
+                      gl::GLEnums::GetStringEnum(query_info->type).c_str(),
+                      gl::GLEnums::GetStringEnum(target).c_str()));
       return error::kNoError;
     }
   } else {
