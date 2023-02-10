948361706456f7f40465eb05408bcd7628a5dec2
Avi Drissman <avi@chromium.org>
2023-02-10 03:01:47 +0000

    Update chrome/browser/ash/input_method to new Value API
    Also fix a few typos while here.
    
    Bug: 646113
    Change-Id: I2417854812fedd07aa06776d1e0984c8d9c52364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236759
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103659}

--- chrome/browser/ash/input_method/pref_change_recorder_unittest.cc
+++ chrome/browser/ash/input_method/pref_change_recorder_unittest.cc
@@ -21,7 +21,7 @@ namespace ash::input_method {
 namespace {
 
 constexpr char kUsEnglish[] = "xkb:us::eng";
-constexpr char kBrazilPortugese[] = "xkb:br::por";
+constexpr char kBrazilPortuguese[] = "xkb:br::por";
 constexpr char kSpainSpanish[] = "xkb:es::spa";
 constexpr char kFranceFrench[] = "xkb:fr::fra";
 
@@ -126,7 +126,7 @@ TEST_P(UserChangesAutocorrectPrefMetric,
        RecordsTheCorrectValueForPkAndBucketsToLangOtherThenEnglish) {
   const AutocorrectPrefChangeCase& test_case = GetParam();
   base::HistogramTester histograms_;
-  FakeInputMethodOptions options(profile_.GetPrefs(), kBrazilPortugese);
+  FakeInputMethodOptions options(profile_.GetPrefs(), kBrazilPortuguese);
 
   // Set the initial autocorrect level (simulating previous values set by user).
   if (test_case.autocorrect_level_from)
@@ -146,7 +146,7 @@ TEST_P(UserChangesAutocorrectPrefMetric,
        RecordsTheCorrectValueForVkAndBucketsToLangOtherThenEnglish) {
   const AutocorrectPrefChangeCase& test_case = GetParam();
   base::HistogramTester histograms_;
-  FakeInputMethodOptions options(profile_.GetPrefs(), kBrazilPortugese);
+  FakeInputMethodOptions options(profile_.GetPrefs(), kBrazilPortuguese);
 
   // Set the initial autocorrect level (simulating previous values set by user).
   if (test_case.autocorrect_level_from)
@@ -361,8 +361,8 @@ INSTANTIATE_TEST_SUITE_P(
             /*metric_name=*/
             "InputMethod.Assistive.AutocorrectV2.UserPrefChange.English.PK"},
         EnabledByDefaultMetricCase{
-            "BrazilianPortugese",
-            /*engine_id=*/kBrazilPortugese,
+            "BrazilianPortuguese",
+            /*engine_id=*/kBrazilPortuguese,
             /*metric_name=*/
             "InputMethod.Assistive.AutocorrectV2.UserPrefChange.All.PK"},
         EnabledByDefaultMetricCase{
