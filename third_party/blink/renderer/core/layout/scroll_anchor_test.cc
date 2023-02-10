760f0f247413a4154ca94af8d7dea60abc0826f2
Kent Tamura <tkent@chromium.org>
2023-02-10 09:05:30 +0000

    ScrollAnchor: Do not search never-laid-out LayoutObjects for candidates
    We had a crash in LayoutMultiColumnFlowThread::ComputeSize() on a box
    became a multi-column container dynamically.
    
    - ScrollAnchor doesn't need to search LayoutObject subtrees which are
      not laid out yet.
    - LayoutMultiColumnFlowThread::Size() should not call ComputeSize()
      if it has never been laid out.
    
    Either of them is necessary to fix crbug.com/1413945. This CL applies
    both because the former improves performance, and we can avoid similar
    issues by the latter.
    
    Bug: 1413945
    Change-Id: I7c7fceb912239dce70341deb4ad05568ef9ae44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234939
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103722}

--- third_party/blink/renderer/core/layout/scroll_anchor_test.cc
+++ third_party/blink/renderer/core/layout/scroll_anchor_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "third_party/blink/public/common/input/web_mouse_event.h"
+#include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/static_node_list.h"
 #include "third_party/blink/renderer/core/editing/finder/text_finder.h"
@@ -1018,6 +1019,21 @@ TEST_F(MAYBE_ScrollAnchorTest, ClampAdjustsAnchorAnimation) {
   EXPECT_EQ(gfx::Vector2d(0, 0), LayoutViewport()
                                      ->GetScrollAnimator()
                                      .ImplOnlyAnimationAdjustmentForTesting());
+}
+
+// crbug.com/1413945
+TEST_F(MAYBE_ScrollAnchorTest, DynamicMultiColumnCrash) {
+  SetBodyInnerHTML(R"HTML(
+    <div id="id125" style="container:foo/size; overflow-y:hidden;
+        writing-mode:vertical-rl;">
+    x</div>)HTML");
+  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kTest);
+  Element* target = GetDocument().getElementById("id125");
+  target->SetInlineStyleProperty(CSSPropertyID::kFontSize, "0");
+  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kTest);
+  target->SetInlineStyleProperty(CSSPropertyID::kColumns, "2");
+  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kTest);
+  // Pass if no crashes.
 }
 
 class ScrollAnchorTestFindInPageClient : public mojom::blink::FindInPageClient {
