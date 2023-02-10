ca1476c02a1083cad8239517ca7499c50c0324df
Andrew Grieve <agrieve@chromium.org>
2023-02-10 20:27:18 +0000

    Android: Build androidx.core.os.BuildCompat from source
    This will let us roll it separately from the rest of androidx.
    
    Bug: 1414452
    Change-Id: I29b02449125c1e3601fef513b34190a7ee40240f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4241482
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104006}

---
+++ third_party/androidx/local_modifications/buildcompat/update_source.sh
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+
+URL='https://android.googlesource.com/platform/frameworks/support/+/androidx-main/core/core/src/main/java/androidx/core/os/BuildCompat.java?format=TEXT'
+this_dir=$(dirname $0)
+
+set -e
+set -x
+
+# Strip out a few lines that don't compile due to missing symbols.
+curl -s "$URL" | base64 -d | grep -Ev 'AD_SERVICES|RequiresOptIn' > "$this_dir/java/androidx/core/os/BuildCompat.java"
