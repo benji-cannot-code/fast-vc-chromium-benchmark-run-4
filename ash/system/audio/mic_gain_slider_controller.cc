e6d7d399fe77e76075ed207d9fede5d5e92c0945
Sylvie Liu <sylvieliu@chromium.org>
2023-02-10 18:37:01 +0000

    Fixed the mic mute state inconsistency issue between VC panel and audio subpage
    The issue is that when the mic is muted in the VC panel, the mic in the
    audio subpage isn't muted. The root cause of it is that the level is
    preserved even if the mic is muted in the pre-revamped view, so the
    slider doesn't visually appear to be muted. In this CL, we fixed this
    issue by setting the level to be 0 in the mic gain slider controller and
    in the mic gain slider view to render the slider to be muted when the
    mic is muted by VC panel or keyboard shortcut.
    
    Screen recording:
    https://drive.google.com/file/d/1dUciLCVrH6WXMTIBQ6-uK3BbYkwj4yKl/view?usp=share_link
    
    Bug: b/266996200
    Change-Id: I69bce4aeb17818a2682614c76b4936ecdf0f3bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4233404
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103947}

--- ash/system/audio/mic_gain_slider_controller.cc
+++ ash/system/audio/mic_gain_slider_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/audio/mic_gain_slider_controller.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/constants/quick_settings_catalogs.h"
 #include "ash/system/audio/mic_gain_slider_view.h"
 #include "base/metrics/user_metrics.h"
@@ -65,6 +66,12 @@ void MicGainSliderController::SliderValueChanged(
                         CrasAudioHandler::Get()->GetInputGainPercent());
   }
 
+  // For QsRevamp: Manually sets the mute state since we don't distinguish muted
+  // and level is 0 state in QsRevamp.
+  if (features::IsQsRevampEnabled() && level == 0) {
+    CrasAudioHandler::Get()->SetMuteForDevice(
+        CrasAudioHandler::Get()->GetPrimaryActiveInputNode(), true);
+  }
   CrasAudioHandler::Get()->SetInputGainPercent(level);
 }
 
