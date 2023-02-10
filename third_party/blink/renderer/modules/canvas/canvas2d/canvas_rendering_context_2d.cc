968ed63f29fcc8cc0f3ea04c390b2130de89935d
Scott Violet <sky@chromium.org>
2023-02-10 16:00:35 +0000

    canvas: small optimizations to draw path
    This does the following:
    . Removes duplicate lookup of PaintCanvas. Draw() already looks up
      the paintcanvas, so no need for other places to also look it up.
    . Renames GetPaintCanvasForDraw to WillDraw() as by the time this
      is called we know there is a PaintCanvas.
    . Changes some rect types to gfx types as most places already take
      gfx types and we were needlessly converting between the two.
    
    Bug: none
    
    Change-Id: I25bf6d246c1659b0a7c7822bf98a3426c52f7a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4235062
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103870}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -452,13 +452,9 @@ cc::PaintCanvas* CanvasRenderingContext2D::GetPaintCanvas() {
   return canvas()->GetCanvas2DLayerBridge()->GetPaintCanvas();
 }
 
-cc::PaintCanvas* CanvasRenderingContext2D::GetPaintCanvasForDraw(
+void CanvasRenderingContext2D::WillDraw(
     const SkIRect& dirty_rect,
     CanvasPerformanceMonitor::DrawType draw_type) {
-  if (UNLIKELY(isContextLost() || !canvas() ||
-               !canvas()->GetCanvas2DLayerBridge() ||
-               !canvas()->GetCanvas2DLayerBridge()->ResourceProvider()))
-    return nullptr;
   CanvasRenderingContext::DidDraw(dirty_rect, draw_type);
   // Always draw everything during printing.
   if (!layer_count_) {
@@ -468,7 +464,6 @@ cc::PaintCanvas* CanvasRenderingContext2D::GetPaintCanvasForDraw(
         ->ResourceProvider()
         ->FlushIfRecordingLimitExceeded();
   }
-  return canvas()->GetCanvas2DLayerBridge()->GetPaintCanvas();
 }
 
 void CanvasRenderingContext2D::FlushCanvas() {
@@ -981,7 +976,7 @@ void CanvasRenderingContext2D::drawFormattedText(
         [&recording](cc::PaintCanvas* c,
                      const cc::PaintFlags* flags)  // draw lambda
         { c->drawPicture(std::move(recording)); },
-        [](const SkIRect& rect) { return false; }, gfx::RectFToSkRect(bounds),
+        [](const SkIRect& rect) { return false; }, bounds,
         CanvasRenderingContext2DState::PaintType::kFillPaintType,
         CanvasRenderingContext2DState::kNoImage,
         CanvasPerformanceMonitor::DrawType::kText);
@@ -1096,8 +1091,7 @@ void CanvasRenderingContext2D::DrawTextInternal(
       },
       [](const SkIRect& rect)  // overdraw test lambda
       { return false; },
-      gfx::RectFToSkRect(bounds), paint_type,
-      CanvasRenderingContext2DState::kNoImage,
+      bounds, paint_type, CanvasRenderingContext2DState::kNoImage,
       CanvasPerformanceMonitor::DrawType::kText);
 }
 
