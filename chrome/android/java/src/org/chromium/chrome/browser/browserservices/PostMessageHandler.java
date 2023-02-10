f83f216caf7e2d90e054b59a1428ca11a256012b
Peilin Wang <peilinwang@google.com>
2023-02-10 21:43:43 +0000

    [Cleanup] Remove DocumentLoadedInFrame noop code
    Bug: 1353846
    Change-Id: Ibe4abda4a7271dacf41d06faeda9cc1cc9926d9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240764
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104067}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/PostMessageHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/PostMessageHandler.java
@@ -107,14 +107,6 @@ public class PostMessageHandler implements OriginVerificationListener {
                 }
                 initializeWithWebContents(webContents);
             }
-
-            @Override
-            public void documentLoadedInFrameNoop(GlobalRenderFrameHostId rfhId,
-                    boolean isInPrimaryMainFrame, @LifecycleState int rfhLifecycleState) {
-                if (!isInPrimaryMainFrame) {
-                    return;
-                }
-            }
         };
     }
 
