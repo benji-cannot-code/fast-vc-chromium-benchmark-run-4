5ffd762226427a37ba2406deff9092807ad63611
Dave Tapuska <dtapuska@chromium.org>
2023-02-10 17:52:05 +0000

    [ios] Use the IsFileQuarantied for IOS
    The IS_APPLE macro was inappropriately used since the IsFileQuarantied
    mac version is only built on Mac and not Apple.
    
    Bug: 1411704
    Change-Id: I3fc57d6d692d28d9bd78c7d108b9693ec0956189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240922
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103924}

--- components/services/quarantine/test_support.cc
+++ components/services/quarantine/test_support.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_APPLE)
+#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_MAC)
 
 namespace quarantine {
 
