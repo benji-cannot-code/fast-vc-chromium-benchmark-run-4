b5936a4c442d64052c997311d79392fca880eb02
Denis Kuznetsov <antrim@chromium.org>
2023-02-10 15:33:46 +0000

    Password changed dialog: add Cancel button
    This CL simplifies previous logic where `cancel` button
    in `password` dialog served as `forgot password` when
    Cryptohome Recovery is enabled - now there are two
    separate buttons, and Cryptohome Recovery flag only
    controls their visibility.
    
    It also adds explicit "Cancel" button on "Forgot password"
    screen when Cryptohome Recovery flag is enabled.
    
    Bug: 257225574
    Change-Id: I29ddea83bd78235c197737945bcf9518b90039fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4239797
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103859}

--- chrome/browser/resources/chromeos/login/screens/login/gaia_password_changed.js
+++ chrome/browser/resources/chromeos/login/screens/login/gaia_password_changed.js
@@ -146,7 +146,6 @@ class GaiaPasswordChanged extends GaiaPasswordChangedBase {
     this.email = data && 'email' in data && data.email;
     this.passwordInvalid_ = data && 'showError' in data && data.showError;
     if (this.isCryptohomeRecoveryUIFlowEnabled_) {
-      this.$.cancel.textKey = 'continueWithoutLocalDataButton';
       this.$.tryAgain.textKey = 'oldPasswordHint';
       this.$.proceedAnyway.textKey = 'continueAndDeleteDataButton';
     }
@@ -204,6 +203,9 @@ class GaiaPasswordChanged extends GaiaPasswordChangedBase {
 
   /** @private */
   onForgotPasswordClicked_() {
+    if (this.disabled) {
+      return;
+    }
     this.setUIStep(GaiaPasswordChangedUIState.FORGOT);
     this.clearPassword();
   }
@@ -232,18 +234,6 @@ class GaiaPasswordChanged extends GaiaPasswordChangedBase {
     this.disabled = true;
     this.clearPassword();
     this.userActed('resync');
-  }
-
-  /** @private */
-  onSecondaryButton_() {
-    if (this.disabled) {
-      return;
-    }
-    if (this.isCryptohomeRecoveryUIFlowEnabled_) {
-      this.onForgotPasswordClicked_();
-    } else {
-      this.onCancel_();
-    }
   }
 
   onNoRecovery_() {
