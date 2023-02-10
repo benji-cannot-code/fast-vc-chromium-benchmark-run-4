b8a496ec9ce255ed6f9d8a1dc10d2eeb1c3e01c1
dpapad <dpapad@chromium.org>
2023-02-10 20:30:30 +0000

    WebUI: Replace references to chrome://test/mojo_webui_test_support.js.
    Replaced remaining references with chrome://webui-test/ instead. This is
    in preparation of migrating mojo_webui_test_support.js to TypeScript.
    
    Also removing mojo_webui_test_support.js from
    interactive_ui_tests_js_mojo_webui and browser_tests_js_mojo_webui
    targets |data| properties, as is no longer needed there.
    
    Bug: 1260297
    Change-Id: Ic9be2d615d3571ed84d50453b71e295f37bfe0a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237271
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104011}

--- chrome/test/data/webui/chromeos/parent_access/parent_access_controller_test.js
+++ chrome/test/data/webui/chromeos/parent_access/parent_access_controller_test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../../mojo_webui_test_support.js';
+import 'chrome://webui-test/mojo_webui_test_support.js';
 
 import {ParentAccessController} from 'chrome://parent-access/parent_access_controller.js';
 import {assert} from 'chrome://resources/ash/common/assert.js';
