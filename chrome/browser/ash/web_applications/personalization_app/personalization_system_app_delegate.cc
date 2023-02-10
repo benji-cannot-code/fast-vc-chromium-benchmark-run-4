6aad8384c7305df24d61a09598fa09d5d00e30a2
Erica Lee <ericamlee@google.com>
2023-02-10 00:48:59 +0000

    personalization: update PUB default sizing
    Update the default sizing for personalization hub for jelly.
    
    RGB on, jelly off: https://screenshot.googleplex.com/7kpXiFvjtHEdzhh
    RGB off, jelly off: https://screenshot.googleplex.com/AECQJZkWJ9oYQxL
    RGB on, jelly on: https://screenshot.googleplex.com/8ip8vQrjiRE6DUm
    RGB off, jelly on: https://screenshot.googleplex.com/bHZwm3rp4BK4QuX
    
    BUG=b:267332833
    TEST=manual
    
    Change-Id: Id922ef5e23b7f7c4dc72969e9ef2b50cdc446498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237406
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103590}

--- chrome/browser/ash/web_applications/personalization_app/personalization_system_app_delegate.cc
+++ chrome/browser/ash/web_applications/personalization_app/personalization_system_app_delegate.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/constants/ash_features.h"
+#include "ash/rgb_keyboard/rgb_keyboard_manager.h"
+#include "ash/shell.h"
 #include "ash/webui/grit/ash_personalization_app_resources.h"
 #include "ash/webui/personalization_app/personalization_app_url_constants.h"
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_utils.h"
@@ -80,7 +82,17 @@ gfx::Rect PersonalizationSystemAppDelegate::GetDefaultBounds(
     Browser* browser) const {
   gfx::Rect bounds =
       display::Screen::GetScreen()->GetDisplayForNewWindows().work_area();
-  bounds.ClampToCenteredSize({826, 745});
+  if (ash::features::IsPersonalizationJellyEnabled()) {
+    // TODO(b/267332833): The sizing does not look right. May need updating as
+    // Jelly is implemented.
+    if (ash::Shell::Get()->rgb_keyboard_manager()->IsRgbKeyboardSupported()) {
+      bounds.ClampToCenteredSize({826, 881});
+    } else {
+      bounds.ClampToCenteredSize({826, 608});
+    }
+  } else {
+    bounds.ClampToCenteredSize({826, 745});
+  }
   return bounds;
 }
 
