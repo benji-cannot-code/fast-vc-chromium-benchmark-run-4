f955c80beec2be2183618a1fb7d5fb0932e3d418
Basia Zimirska <basiaz@google.com>
2023-02-10 22:46:18 +0000

    Log NetError for contextual search requests.
    This is required for investigation of high error rates for some requests. Details in the bug.
    If there was no error, status OK is returned.
    
    Bug: 1414459
    Change-Id: I59a8e1a09efd337e3d5b4359378e346ddc33f8bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237039
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104090}

--- components/contextual_search/core/browser/contextual_search_delegate_impl.cc
+++ components/contextual_search/core/browser/contextual_search_delegate_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/json/json_string_value_serializer.h"
 #include "base/json/json_writer.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -234,6 +235,10 @@ void ContextualSearchDelegateImpl::OnUrlLoadComplete(
     std::unique_ptr<std::string> response_body) {
   if (!context)
     return;
+
+  // Network error codes are negative. See: src/net/base/net_error_list.h.
+  base::UmaHistogramSparse("Search.ContextualSearch.NetError",
+                           std::abs(url_loader_->NetError()));
 
   int response_code = ResolvedSearchTerm::kResponseCodeUninitialized;
   if (url_loader_->ResponseInfo() && url_loader_->ResponseInfo()->headers) {
