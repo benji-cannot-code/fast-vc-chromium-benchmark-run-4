cc5a94078d52af388c80255a1efd9f85c24da91b
Michele Fan <michelefan@chromium.org>
2023-02-10 00:21:59 +0000

    snap-group: Implement the entry point for experiment arm 1
    This CL implements entry point arm 1 to automatically create a
    snap group on two windows snapped in clamshell mode. The snap
    group will be removed on split view mode ended.
    
    To run this experiment, append this command:
    --enable-features=SnapGroup:"AutomaticLockGroup"/true
    Demo for this feature is available at:
    http://b/264604628#comment4
    
    Fixed: b:264604628
    Test: Manually + added unit test
    Change-Id: Iea32c81745a1bf51052e748732ec67c4c93ab144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227572
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103582}

--- ash/wm/default_state.cc
+++ ash/wm/default_state.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desks_util.h"
 #include "ash/wm/float/float_controller.h"
 #include "ash/wm/screen_pinning_controller.h"
-#include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_metrics_controller.h"
 #include "ash/wm/window_positioning_utils.h"
 #include "ash/wm/window_state.h"
@@ -412,8 +411,9 @@ void DefaultState::HandleTransitionEvents(WindowState* window_state,
     return;
   }
 
-  if (type == WM_EVENT_SNAP_PRIMARY || type == WM_EVENT_SNAP_SECONDARY)
+  if (type == WM_EVENT_SNAP_PRIMARY || type == WM_EVENT_SNAP_SECONDARY) {
     HandleWindowSnapping(window_state, type);
+  }
 
   if (next_state_type == current_state_type && window_state->IsSnapped()) {
     DCHECK(window_state->snap_ratio());
