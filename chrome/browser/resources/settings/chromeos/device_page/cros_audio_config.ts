9fd2926269208d09bb83065e34894d7fed45722b
Ashley Prasad <ashleydp@google.com>
2023-02-10 05:48:27 +0000

    audio-settings: use real mojo by default
    - Change default behavior of cros_audio_config to use the real mojo
      remote.
    - Remove TODO.
    
    Bug: b:260277007
    Test: browser_test --gtest_filter=*OSSettingsDevicePage*
    Test: Manually confirm real mojo used in page.
    Change-Id: Ib53ac2a233725b6a3dbf8c670020d219ec15d1d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237408
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103701}

--- chrome/browser/resources/settings/chromeos/device_page/cros_audio_config.ts
+++ chrome/browser/resources/settings/chromeos/device_page/cros_audio_config.ts
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * ability to override it with a fake implementation needed for tests.
  */
 
-import {assert} from 'chrome://resources/js/assert_ts.js';
-
 import {CrosAudioConfig, CrosAudioConfigInterface as CrosAudioConfigMojomInterface} from '../../mojom-webui/audio/cros_audio_config.mojom-webui.js';
 
 import {FakeCrosAudioConfig, FakeCrosAudioConfigInterface} from './fake_cros_audio_config.js';
@@ -23,9 +21,7 @@ export type CrosAudioConfigInterface = Required<CrosAudioConfigMojomInterface>&
 let crosAudioConfig: CrosAudioConfigInterface|null = null;
 
 /** Use FakeCrosAudioConfig implementation in `getCrosAudioConfig`. */
-// TODO(b/260277007): When mojo is stable set useFakeMojo to false and remove
-// assert on `useFakeMojo` value.
-const useFakeMojo = true;
+const useFakeMojo = false;
 
 /**
  * The CrosAudioConfig implementation used for testing. Passing null reverses
@@ -40,7 +36,7 @@ export function getCrosAudioConfig(): CrosAudioConfigInterface {
   if (!crosAudioConfig && useFakeMojo) {
     crosAudioConfig = new FakeCrosAudioConfig();
   }
-  assert(useFakeMojo, '`useFakeMojo` should be false until mojo is stable.');
+
   if (!crosAudioConfig) {
     crosAudioConfig = CrosAudioConfig.getRemote();
   }
