c173a11414be2b74eaa87a6607f208921217d638
Fredrik Söderquist <fs@opera.com>
2023-02-10 14:52:12 +0000

    Fix LOG typos in ChromeBrowserCloudManagementController
    Change-Id: I06a6755ca845aed244e440cdf1a0538feb183697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4239879
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1103844}

--- components/enterprise/browser/controller/chrome_browser_cloud_management_controller.cc
+++ components/enterprise/browser/controller/chrome_browser_cloud_management_controller.cc
@@ -86,7 +86,7 @@ ChromeBrowserCloudManagementController::CreatePolicyManager(
     ConfigurationPolicyProvider* platform_provider) {
   if (!IsEnabled()) {
     LOG_POLICY(WARNING, CBCM_ENROLLMENT)
-        << "Could not create policy mananger as CBCM is not enabled.";
+        << "Could not create policy manager as CBCM is not enabled.";
     return nullptr;
   }
 
@@ -161,7 +161,7 @@ void ChromeBrowserCloudManagementController::Init(
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory) {
   if (!IsEnabled()) {
     LOG_POLICY(ERROR, CBCM_ENROLLMENT)
-        << "Cloud management constroller initialization aborted as CBCM is not "
+        << "Cloud management controller initialization aborted as CBCM is not "
            "enabled.";
 
     LOG_POLICY(INFO, CBCM_ENROLLMENT)
