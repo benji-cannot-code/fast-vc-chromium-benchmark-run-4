242938c5aa62067e87dd7e8e7daa0a7f50d01980
anthie@google.com <anthie@google.com>
2023-02-10 09:55:45 +0000

    CodeHealth: Migrate base::Value::FindBoolKey in web_history_service
    Migrate usages of base::Value::FindBoolKey to base::Value::Dict::FindBool(), as well as any other
    occurrence of the API v1 base::Value that can be found
    in the affected methods.
    
    Bug: 1187001
    Change-Id: I4303aea2dc16dd5fa432c45306bee561b333e685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236267
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103743}

--- components/history/core/browser/web_history_service_unittest.cc
+++ components/history/core/browser/web_history_service_unittest.cc
@@ -333,8 +333,9 @@ TEST_F(WebHistoryServiceTest, VerifyReadResponse) {
   response_value = TestingWebHistoryService::ReadResponse(request.get());
   bool enabled_value = false;
   if (absl::optional<bool> enabled =
-          response_value->FindBoolKey("history_recording_enabled"))
+          response_value->GetDict().FindBool("history_recording_enabled")) {
     enabled_value = *enabled;
+  }
   EXPECT_TRUE(enabled_value);
 
   // Test that properly formatted response with good response code returns false
@@ -349,8 +350,9 @@ TEST_F(WebHistoryServiceTest, VerifyReadResponse) {
   response_value2 = TestingWebHistoryService::ReadResponse(request2.get());
   enabled_value = true;
   if (absl::optional<bool> enabled =
-          response_value2->FindBoolKey("history_recording_enabled"))
+          response_value2->GetDict().FindBool("history_recording_enabled")) {
     enabled_value = *enabled;
+  }
   EXPECT_FALSE(enabled_value);
 
   // Test that a bad response code returns false.
@@ -388,7 +390,8 @@ TEST_F(WebHistoryServiceTest, VerifyReadResponse) {
   absl::optional<base::Value> response_value5;
   // ReadResponse deletes the request
   response_value5 = TestingWebHistoryService::ReadResponse(request5.get());
-  EXPECT_FALSE(response_value5->FindBoolKey("history_recording_enabled"));
+  EXPECT_FALSE(
+      response_value5->GetDict().FindBool("history_recording_enabled"));
 }
 
 }  // namespace history
