399af1a9f9a9a02f82bc1d6e256585d18346b6f6
Tony Yeoman <tby@google.com>
2023-02-10 04:46:34 +0000

    [Chat] Add skeleton of launcher chat controller
    Bug: b/268140386
    Change-Id: I4f387842028e11921b964af55916805cfd901499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4235261
    Reviewed-by: Lauren Commeignes <laurencom@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103691}

---
+++ chrome/browser/ash/app_list/chat/chat_controller.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_APP_LIST_CHAT_CHAT_CONTROLLER_H_
+#define CHROME_BROWSER_ASH_APP_LIST_CHAT_CHAT_CONTROLLER_H_
+
+#include "chrome/browser/ash/app_list/search/chrome_search_result.h"
+#include "chrome/browser/ash/app_list/search/search_provider.h"
+
+class Profile;
+
+// TODO(b/268140386): This file is a work in progress.
+
+namespace app_list {
+
+class ChatController {
+ public:
+  explicit ChatController(Profile* profile);
+  ~ChatController();
+
+  ChatController(const ChatController&) = delete;
+  ChatController& operator=(const ChatController&) = delete;
+
+ private:
+  Profile* profile_;
+};
+
+}  // namespace app_list
+
+#endif  // CHROME_BROWSER_ASH_APP_LIST_CHAT_CHAT_CONTROLLER_H_
