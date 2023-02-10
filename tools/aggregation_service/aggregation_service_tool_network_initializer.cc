cff84ba983fbc0f5347d606f3dda56e75182e5d8
Alex Turner <alexmt@chromium.org>
2023-02-10 21:14:49 +0000

    Fix segmentation fault in aggregation_service_tool
    Ensures that all necessary objects for the network service are
    available before use
    
    Bug: 1409981
    Change-Id: I85b8991d525806d3f86ec52744c78c19db0458f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189073
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104051}

--- tools/aggregation_service/aggregation_service_tool_network_initializer.cc
+++ tools/aggregation_service/aggregation_service_tool_network_initializer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "content/public/browser/network_service_instance.h"
+#include "content/public/test/content_test_suite_base.h"
 #include "mojo/core/embedder/embedder.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/cert_verifier/public/mojom/cert_verifier_service_factory.mojom.h"
@@ -17,7 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aggregation_service {
 
-ToolNetworkInitializer::ToolNetworkInitializer() {
+ToolNetworkInitializer::ToolNetworkInitializer()
+    : content::ContentTestSuiteBase(/*argc=*/0, /*argv=*/nullptr) {
+  ContentTestSuiteBase::Initialize();
+
   // Initialize the network state as this tool runs independently from the
   // command line.
   mojo::core::Init();
