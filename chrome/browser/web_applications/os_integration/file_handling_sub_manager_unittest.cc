1d44b0ff780f7a46818277feec144be2949110a1
Daniel Murphy <dmurph@google.com>
2023-02-11 01:20:10 +0000

    [dPWA] Disabling file handling tests mac
    Bug: 1414947
    Change-Id: Iba41058af5ee0bc2b4d20f73df2977e622154783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242105
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dana Fried <dfried@google.com>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Quick-Run: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dana Fried <dfried@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104154}

--- chrome/browser/web_applications/os_integration/file_handling_sub_manager_unittest.cc
+++ chrome/browser/web_applications/os_integration/file_handling_sub_manager_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
+#include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/os_integration/file_handling_sub_manager.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
@@ -437,8 +438,14 @@ TEST_P(FileHandlingSubManagerConfigureAndExecuteTest, Uninstall) {
   }
 }
 
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_All DISABLED_All
+#else
+#define MAYBE_All All
+#endif
+
 INSTANTIATE_TEST_SUITE_P(
-    All,
+    MAYBE_All,
     FileHandlingSubManagerConfigureAndExecuteTest,
     ::testing::Values(OsIntegrationSubManagersState::kSaveStateToDB,
                       OsIntegrationSubManagersState::kSaveStateAndExecute,
