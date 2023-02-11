d15c038e053f5db9beb8a01aec2efc839431a57a
sophiewen <sophiewen@chromium.org>
2023-02-11 00:44:24 +0000

    float: Change tablet menu to handle touch events
    This modifies TabletModeMultitaskMenuEventHandler to handle touch
    instead of gesture events so they are consumed before Lacros windows.
    This may also avoid unexpected scroll DCHECKs on web contents render
    widgets.
    
    Note that only active windows will show the menu now, i.e. if 2 windows
    are in splitview or one floated, the first touch will need to activate
    the window first before it shows the menu. Since the multitask cue only
    appears on window activation, this seems reasonable, but a follow-up
    change can be added if needed.
    
    Test: modified
    Bug: b/266748266
    Change-Id: I049aef8601fc3b3e4b41cb9bb1da0727e08d1a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237432
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104144}

--- ash/wm/tablet_mode/tablet_mode_multitask_cue_unittest.cc
+++ ash/wm/tablet_mode/tablet_mode_multitask_cue_unittest.cc
@@ -59,7 +59,7 @@ TEST_F(TabletModeMultitaskCueTest, BasicShowCue) {
   auto* multitask_cue = GetMultitaskCue();
   ASSERT_TRUE(multitask_cue);
 
-  ui::Layer* cue_layer = multitask_cue->cue_layer_for_testing();
+  ui::Layer* cue_layer = multitask_cue->cue_layer();
   ASSERT_TRUE(cue_layer);
 
   EXPECT_EQ(gfx::Rect((window_bounds.width() - kCueWidth) / 2, kCueYOffset,
@@ -80,7 +80,7 @@ TEST_F(TabletModeMultitaskCueTest, SplitCueBounds) {
   gfx::Rect split_bounds((window1->bounds().width() - kCueWidth) / 2,
                          kCueYOffset, kCueWidth, kCueHeight);
 
-  ui::Layer* cue_layer = GetMultitaskCue()->cue_layer_for_testing();
+  ui::Layer* cue_layer = GetMultitaskCue()->cue_layer();
   ASSERT_TRUE(cue_layer);
   EXPECT_EQ(cue_layer->bounds(), split_bounds);
 
@@ -88,7 +88,7 @@ TEST_F(TabletModeMultitaskCueTest, SplitCueBounds) {
   split_view_controller->SnapWindow(
       window2.get(), SplitViewController::SnapPosition::kSecondary);
 
-  cue_layer = GetMultitaskCue()->cue_layer_for_testing();
+  cue_layer = GetMultitaskCue()->cue_layer();
   ASSERT_TRUE(cue_layer);
   EXPECT_EQ(cue_layer->bounds(), split_bounds);
 }
@@ -101,7 +101,7 @@ TEST_F(TabletModeMultitaskCueTest, DismissTimerFiring) {
   auto window = CreateAppWindow();
 
   auto* multitask_cue = GetMultitaskCue();
-  ui::Layer* cue_layer = multitask_cue->cue_layer_for_testing();
+  ui::Layer* cue_layer = multitask_cue->cue_layer();
   ASSERT_TRUE(cue_layer);
 
   // Wait for fade in to finish.
@@ -112,7 +112,7 @@ TEST_F(TabletModeMultitaskCueTest, DismissTimerFiring) {
 
   // Wait for fade out to finish.
   animation_waiter.Wait(cue_layer);
-  EXPECT_FALSE(multitask_cue->cue_layer_for_testing());
+  EXPECT_FALSE(multitask_cue->cue_layer());
 }
 
 // Tests that the cue dismisses properly during the fade out animation.
@@ -123,7 +123,7 @@ TEST_F(TabletModeMultitaskCueTest, DismissEarly) {
   auto window = CreateAppWindow();
 
   auto* multitask_cue = GetMultitaskCue();
-  ui::Layer* cue_layer = multitask_cue->cue_layer_for_testing();
+  ui::Layer* cue_layer = multitask_cue->cue_layer();
   ASSERT_TRUE(cue_layer);
 
   // Wait for fade in to finish.
@@ -131,7 +131,7 @@ TEST_F(TabletModeMultitaskCueTest, DismissEarly) {
 
   multitask_cue->FireCueDismissTimerForTesting();
   multitask_cue->DismissCue();
-  EXPECT_FALSE(multitask_cue->cue_layer_for_testing());
+  EXPECT_FALSE(multitask_cue->cue_layer());
 }
 
 // Tests that the cue dismisses properly when the float keyboard accelerator is
@@ -143,7 +143,7 @@ TEST_F(TabletModeMultitaskCueTest, FloatWindow) {
 
   auto* multitask_cue = GetMultitaskCue();
   ASSERT_TRUE(multitask_cue);
-  EXPECT_FALSE(multitask_cue->cue_layer_for_testing());
+  EXPECT_FALSE(multitask_cue->cue_layer());
 }
 
 }  // namespace ash
