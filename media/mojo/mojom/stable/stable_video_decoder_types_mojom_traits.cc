4febda8406522f7a5daf7187aabf5b705729b166
Andres Calderon Jaramillo <andrescj@chromium.org>
2023-02-11 00:17:48 +0000

    StableVideoDecoder: Validate DecoderBuffer::duration.
    This is a small addition to the validation of the
    DecoderBuffer::duration field during de-serialization: I noticed that
    DecoderBuffer::set_duration() has a DCHECK() asserting the valid range
    of the duration [1]. This CL just validates that assertion.
    
    Additionally, unit tests revealed that the duration was not being
    deserialized correctly. That's also fixed.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:media/base/decoder_buffer.h;l=161-163;drc=abfd18a816ef914f07b1892fc2c6814e25fc02eb
    
    Bug: b:195769334
    Test: unit tests
    Change-Id: I71a050125094f1d33feca7aee6541ec4c830d286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236829
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104134}

--- media/mojo/mojom/stable/stable_video_decoder_types_mojom_traits.cc
+++ media/mojo/mojom/stable/stable_video_decoder_types_mojom_traits.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "media/base/format_utils.h"
+#include "media/base/timestamp_constants.h"
 #include "media/gpu/buffer_validation.h"
 #include "mojo/public/cpp/bindings/optional_as_pointer.h"
 
@@ -454,8 +455,13 @@ bool StructTraits<media::stable::mojom::DecoderBufferDataView,
   decoder_buffer->set_timestamp(timestamp);
 
   base::TimeDelta duration;
-  if (!input.ReadTimestamp(&duration))
+  if (!input.ReadDuration(&duration)) {
     return false;
+  }
+  if (duration != media::kNoTimestamp &&
+      (duration < base::TimeDelta() || duration == media::kInfiniteDuration)) {
+    return false;
+  }
   decoder_buffer->set_duration(duration);
 
   decoder_buffer->set_is_key_frame(input.is_key_frame());
