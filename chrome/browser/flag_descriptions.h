658e6f0582dbe34967cce6f615ccc78bb5a01d76
whalechang <whalechang@google.com>
2023-02-11 16:35:19 +0000

    Add feature flag for cras refactor
    CRAS is going to have a refactor, create a feature flag to avoid regression.
    
    BUG=b:247732405
    TEST=CQ
    
    Change-Id: Iac9fe216eb20e0153a1f880c5bd1cd70ca3b3c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4235259
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Ching Yun Chang <whalechang@google.com>
    Auto-Submit: Ching Yun Chang <whalechang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104232}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -940,6 +940,9 @@ extern const char kHeavyAdInterventionDescription[];
 extern const char kTabAudioMutingName[];
 extern const char kTabAudioMutingDescription[];
 
+extern const char kCrasSplitAlsaUsbInternalName[];
+extern const char kCrasSplitAlsaUsbInternalDescription[];
+
 extern const char kTabSelectionEditorV2Name[];
 extern const char kTabSelectionEditorV2Description[];
 
