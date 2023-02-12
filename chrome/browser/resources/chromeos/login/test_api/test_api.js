74c752a9a877e9cbad97baa2edda669f73ef58f9
Bohdan Tyshchenko <bohdanty@google.com>
2023-02-12 19:45:05 +0000

    [OOBE] - Extend test API coverage for guest tos and consent screens
    Add logic to fetch names of the Google/CrOS/Privacy/Arc ToS buttons on
    the consolidated consent/guest tos screens. Add browse as guest button
    name to the strings list.
    Also, seems like there was a change to the JS formatter that is used
    by the `git cl format --js`, therefore some unrelated classes were
    refactored.
    
    Bug: b:268172049
    Change-Id: I3b02665ee95c32f98b6a5577f14e18db488d69ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225438
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104329}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -227,8 +227,9 @@ class GaiaScreenTester extends ScreenElementApi {
    * @return {boolean}
    */
   isReadyForTesting() {
-    return this.isVisible() && !this.gaiaLoading.isVisible() &&
-        this.signinFrame.isVisible() && this.gaiaDialog.isVisible();
+    return (
+        this.isVisible() && !this.gaiaLoading.isVisible() &&
+        this.signinFrame.isVisible() && this.gaiaDialog.isVisible());
   }
 }
 
@@ -276,8 +277,9 @@ class AssistantScreenTester extends ScreenElementApi {
    * @return {boolean}
    */
   isReadyForTesting() {
-    return this.isVisible() &&
-        (this.valueProp.isVisible() || this.relatedInfo.isVisible());
+    return (
+        this.isVisible() &&
+        (this.valueProp.isVisible() || this.relatedInfo.isVisible()));
   }
 
   getSkipButtonName() {
@@ -393,8 +395,9 @@ class ThemeSelectionScreenTester extends ScreenElementApi {
    * @return {boolean}
    */
   isReadyForTesting() {
-    return this.isVisible() && this.lightThemeButton.isVisible() &&
-        this.darkThemeButton.isVisible() && this.autoThemeButton.isVisible();
+    return (
+        this.isVisible() && this.lightThemeButton.isVisible() &&
+        this.darkThemeButton.isVisible() && this.autoThemeButton.isVisible());
   }
 
   /**
@@ -517,8 +520,9 @@ class EnrollmentSignInStep extends PolymerElementApi {
    * @return {boolean}
    */
   isReadyForTesting() {
-    return this.isVisible() && this.signInFrame.isVisible() &&
-        this.nextButton.isVisible();
+    return (
+        this.isVisible() && this.signInFrame.isVisible() &&
+        this.nextButton.isVisible());
   }
 }
 
@@ -684,12 +688,17 @@ class ArcTosScreenTester extends ScreenElementApi {
   }
 }
 
-
 class GuestTosScreenTester extends ScreenElementApi {
   constructor() {
     super('guest-tos');
     this.loadedStep = new PolymerElementApi(this, '#loaded');
     this.nextButton = new PolymerElementApi(this, '#acceptButton');
+
+    this.googleEulaDialog = new PolymerElementApi(this, '#googleEulaDialog');
+    this.crosEulaDialog = new PolymerElementApi(this, '#crosEulaDialog');
+
+    this.googleEulaDialogLink = new PolymerElementApi(this, '#googleEulaLink');
+    this.crosEulaDialogLink = new PolymerElementApi(this, '#crosEulaLink');
   }
 
   /** @override */
@@ -706,8 +715,32 @@ class GuestTosScreenTester extends ScreenElementApi {
   getNextButtonName() {
     return loadTimeData.getString('guestTosAccept');
   }
-}
 
+  /** @return {string} */
+  getEulaButtonName() {
+    return loadTimeData.getString('guestTosOk');
+  }
+
+  /** @return {boolean} */
+  isGoogleEulaDialogShown() {
+    return this.googleEulaDialog.isVisible();
+  }
+
+  /** @return {boolean} */
+  isCrosEulaDialogShown() {
+    return this.crosEulaDialog.isVisible();
+  }
+
+  /** @return {string} */
+  getGoogleEulaLinkName() {
+    return this.googleEulaDialogLink.element().text.trim();
+  }
+
+  /** @return {string} */
+  getCrosEulaLinkName() {
+    return this.crosEulaDialogLink.element().text.trim();
+  }
+}
 
 class GestureNavigationScreenTester extends ScreenElementApi {
   constructor() {
@@ -728,6 +761,17 @@ class ConsolidatedConsentScreenTester extends ScreenElementApi {
     this.readMoreButton =
         new PolymerElementApi(this.loadedStep, '#readMoreButton');
     this.recoveryToggle = new PolymerElementApi(this, '#recoveryOptIn');
+
+    this.googleEulaDialog = new PolymerElementApi(this, '#googleEulaDialog');
+    this.crosEulaDialog = new PolymerElementApi(this, '#crosEulaDialog');
+    this.arcTosDialog = new PolymerElementApi(this, '#arcTosDialog');
+    this.privacyPolicyDialog =
+        new PolymerElementApi(this, '#privacyPolicyDialog');
+
+    this.googleEulaLink = new PolymerElementApi(this, '#googleEulaLink');
+    this.crosEulaLink = new PolymerElementApi(this, '#crosEulaLink');
+    this.arcTosLink = new PolymerElementApi(this, '#arcTosLink');
+    this.privacyPolicyLink = new PolymerElementApi(this, '#privacyPolicyLink');
   }
 
   /** @override */
@@ -745,8 +789,9 @@ class ConsolidatedConsentScreenTester extends ScreenElementApi {
     // The read more button is inside a <dom-if> element, if it's hidden, the
     // element would be removed entirely from dom, so we need to check if the
     // element exists before checking if it's visible.
-    return this.readMoreButton.element() != null &&
-        this.readMoreButton.isVisible();
+    return (
+        this.readMoreButton.element() != null &&
+        this.readMoreButton.isVisible());
   }
 
   /** @return {string} */
@@ -760,6 +805,51 @@ class ConsolidatedConsentScreenTester extends ScreenElementApi {
    */
   enableRecoveryToggle() {
     this.recoveryToggle.element().checked = true;
+  }
+
+  /** @return {string} */
+  getEulaOkButtonName() {
+    return loadTimeData.getString('consolidatedConsentOK');
+  }
+
+  /** @return {boolean} */
+  isGoogleEulaDialogShown() {
+    return this.googleEulaDialog.isVisible();
+  }
+
+  /** @return {boolean} */
+  isCrosEulaDialogShown() {
+    return this.crosEulaDialog.isVisible();
+  }
+
+  /** @return {boolean} */
+  isArcTosDialogShown() {
+    return this.arcTosDialog.isVisible();
+  }
+
+  /** @return {boolean} */
+  isPrivacyPolicyDialogShown() {
+    return this.privacyPolicyDialog.isVisible();
+  }
+
+  /** @return {string} */
+  getGoogleEulaLinkName() {
+    return this.googleEulaLink.element().text.trim();
+  }
+
+  /** @return {string} */
+  getCrosEulaLinkName() {
+    return this.crosEulaLink.element().text.trim();
+  }
+
+  /** @return {string} */
+  getArcTosLinkName() {
+    return this.arcTosLink.element().text.trim();
+  }
+
+  /** @return {string} */
+  getPrivacyPolicyLinkName() {
+    return this.privacyPolicyLink.element().text.trim();
   }
 }
 
@@ -846,6 +936,10 @@ export class OobeApiProvider {
 
     this.isGaiaDialogVisible = function() {
       chrome.send('OobeTestApi.isGaiaDialogVisible');
+    };
+
+    this.getBrowseAsGuestButtonName = function() {
+      return loadTimeData.getString('testapi_browseAsGuest');
     };
   }
 }
--- chrome/browser/ui/webui/ash/login/testapi/oobe_test_api_handler.cc
+++ chrome/browser/ui/webui/ash/login/testapi/oobe_test_api_handler.cc
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/login_screen_client_impl.h"
 #include "chrome/browser/ui/webui/ash/login/hid_detection_screen_handler.h"
 #include "chromeos/ash/components/assistant/buildflags.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/account_id/account_id.h"
+#include "components/login/localized_values_builder.h"
 #include "services/device/public/mojom/input_service.mojom.h"
 
 namespace ash {
@@ -37,7 +39,9 @@ OobeTestAPIHandler::OobeTestAPIHandler() = default;
 OobeTestAPIHandler::~OobeTestAPIHandler() = default;
 
 void OobeTestAPIHandler::DeclareLocalizedValues(
-    ::login::LocalizedValuesBuilder* builder) {}
+    ::login::LocalizedValuesBuilder* builder) {
+  builder->Add("testapi_browseAsGuest", IDS_ASH_BROWSE_AS_GUEST_BUTTON);
+}
 
 void OobeTestAPIHandler::DeclareJSCallbacks() {
   AddCallback("OobeTestApi.loginWithPin", &OobeTestAPIHandler::LoginWithPin);
