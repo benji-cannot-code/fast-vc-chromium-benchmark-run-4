295ac010e8507ab18fbe6fe446518747f822080f
Bruce Dawson <brucedawson@chromium.org>
2023-02-12 17:24:40 +0000

    Move update_idl.py to tools\win
    update_idl.py was written for updating IDL files in chrome/updater but
    turns out to be a crucial tool for updating all IDL files. This moves
    update_idl.py in preparation for some updates to generalize and improve
    it.
    
    Change-Id: I62af367cf8b1d3c4c3cc8c6a0688c7fbaee46e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242108
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104322}

--- tools/win/update_idl.py
+++ tools/win/update_idl.py
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 This tool must be run from a Windows machine at the source root directory.
 
 Example:
-    python3 chrome/updater/tools/update_idl.py
+    python3 tools/win/update_idl.py
 """
 
 import os
