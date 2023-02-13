5f9281dc88944dcb87a157e6438c33b7e17a6d22
Avi Drissman <avi@chromium.org>
2023-02-13 19:26:39 +0000

    Update chromeos/ash/components/network/cellular_policy_handler to new Value API
    Bug: 1412465
    Change-Id: Ieaf134f97fe874a4ea3e212b2c4b297086c6d085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236148
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104604}

--- chromeos/ash/components/network/cellular_policy_handler.cc
+++ chromeos/ash/components/network/cellular_policy_handler.cc
@@ -46,7 +46,7 @@ constexpr base::TimeDelta kCellularDeviceWaitTime = base::Seconds(30);
 
 CellularPolicyHandler::InstallPolicyESimRequest::InstallPolicyESimRequest(
     const std::string& smdp_address,
-    const base::Value& onc_config)
+    const base::Value::Dict& onc_config)
     : smdp_address(smdp_address),
       onc_config(onc_config.Clone()),
       retry_backoff(&kRetryBackoffPolicy) {}
@@ -82,7 +82,7 @@ void CellularPolicyHandler::Init(
 }
 
 void CellularPolicyHandler::InstallESim(const std::string& smdp_address,
-                                        const base::Value& onc_config) {
+                                        const base::Value::Dict& onc_config) {
   PushRequestAndProcess(
       std::make_unique<InstallPolicyESimRequest>(smdp_address, onc_config));
 }
@@ -240,13 +240,13 @@ base::Value::Dict CellularPolicyHandler::GetNewShillProperties() {
       network_profile_handler_->GetProfileForUserhash(
           /*userhash=*/std::string());
   const std::string* guid =
-      remaining_install_requests_.front()->onc_config.FindStringKey(
+      remaining_install_requests_.front()->onc_config.FindString(
           ::onc::network_config::kGUID);
   DCHECK(guid);
 
   return policy_util::CreateShillConfiguration(
       *profile, *guid, /*global_policy=*/nullptr,
-      &(remaining_install_requests_.front()->onc_config.GetDict()),
+      &(remaining_install_requests_.front()->onc_config),
       /*user_settings=*/nullptr);
 }
 
@@ -271,7 +271,7 @@ void CellularPolicyHandler::OnConfigureESimService(
   NET_LOG(EVENT) << "Successfully configured service for existing eSIM profile";
   current_request->retry_backoff.InformOfRequest(/*succeeded=*/true);
   const std::string* iccid =
-      policy_util::GetIccidFromONC(current_request->onc_config.GetDict());
+      policy_util::GetIccidFromONC(current_request->onc_config);
   managed_cellular_pref_handler_->AddIccidSmdpPair(
       *iccid, current_request->smdp_address);
   ProcessRequests();
@@ -349,7 +349,7 @@ void CellularPolicyHandler::PopRequest() {
 absl::optional<dbus::ObjectPath>
 CellularPolicyHandler::FindExistingMatchingESimProfile() {
   const std::string* iccid = policy_util::GetIccidFromONC(
-      remaining_install_requests_.front()->onc_config.GetDict());
+      remaining_install_requests_.front()->onc_config);
   if (!iccid) {
     return absl::nullopt;
   }
