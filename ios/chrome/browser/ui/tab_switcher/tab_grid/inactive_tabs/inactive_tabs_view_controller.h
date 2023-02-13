94b936e27e4517f9aa92121fdbc08b93117a0b70
Louis Romero <lpromero@google.com>
2023-02-13 15:02:53 +0000

    Add an empty InactiveTabsViewController
    This CL adds the coordinator and empty view controller.
    
    Bug: 1410515
    Change-Id: I3d0d4ea11ce02843c28c5ed2cb440a1c3303d374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237795
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104480}

---
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_view_controller.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_INACTIVE_TABS_INACTIVE_TABS_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_INACTIVE_TABS_INACTIVE_TABS_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+@class InactiveTabsViewController;
+
+// Protocol used to relay relevant user interactions from the
+// InactiveTabsViewController.
+@protocol InactiveTabsViewControllerDelegate
+
+// Invoked when the inactive tabs view controller's back button is tapped.
+- (void)inactiveTabsViewControllerDidTapBackButton:
+    (InactiveTabsViewController*)inactiveTabsViewController;
+
+@end
+
+// Displays the list of inactive tabs.
+@interface InactiveTabsViewController : UIViewController
+
+// Delegate to handle interactions.
+@property(nonatomic, weak) id<InactiveTabsViewControllerDelegate> delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_INACTIVE_TABS_INACTIVE_TABS_VIEW_CONTROLLER_H_
--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -55,6 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/sharing/sharing_params.h"
 #import "ios/chrome/browser/ui/snackbar/snackbar_coordinator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_commands.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/features.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_coordinator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_mediator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_context_menu/tab_context_menu_helper.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_context_menu/tab_item.h"
@@ -83,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface TabGridCoordinator () <RecentTabsPresentationDelegate,
                                   HistoryPresentationDelegate,
+                                  InactiveTabsCoordinatorDelegate,
                                   SceneStateObserver,
                                   SnackbarCoordinatorDelegate,
                                   TabContextMenuDelegate,
@@ -136,6 +139,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) SnackbarCoordinator* snackbarCoordinator;
 // Coordinator for snackbar presentation on `_incognitoBrowser`.
 @property(nonatomic, strong) SnackbarCoordinator* incognitoSnackbarCoordinator;
+// Coordinator for inactive tabs.
+@property(nonatomic, strong) InactiveTabsCoordinator* inactiveTabsCoordinator;
 // The timestamp of the user entering the tab grid.
 @property(nonatomic, assign) base::TimeTicks tabGridEnterTime;
 // The timestamp of the user exiting the tab grid.
@@ -1040,6 +1045,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       UrlLoadStrategy::ALWAYS_NEW_FOREGROUND_TAB;
   self.historyCoordinator.presentationDelegate = self;
   [self.historyCoordinator start];
+}
+
+- (void)showInactiveTabs {
+  DCHECK(IsInactiveTabsEnabled());
+  if (self.inactiveTabsCoordinator) {
+    return;
+  }
+
+  // TODO(crbug.com/1414536): Pass the inactive tabs browser instead.
+  self.inactiveTabsCoordinator = [[InactiveTabsCoordinator alloc]
+      initWithBaseViewController:self.baseViewController
+                         browser:self.regularBrowser];
+  self.inactiveTabsCoordinator.delegate = self;
+  [self.inactiveTabsCoordinator start];
+}
+
+#pragma mark - InactiveTabsCoordinatorDelegate
+
+- (void)inactiveTabsCoordinatorDidFinish:
+    (InactiveTabsCoordinator*)inactiveTabsCoordinator {
+  DCHECK(IsInactiveTabsEnabled());
+  [self.inactiveTabsCoordinator stop];
+  self.inactiveTabsCoordinator = nil;
 }
 
 #pragma mark - RecentTabsPresentationDelegate
