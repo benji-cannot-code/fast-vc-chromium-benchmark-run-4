a178f559f0d45b895a25c9022c2947ce63f5e20c
Dana Fried <dfried@chromium.org>
2023-02-13 19:34:33 +0000

    [Test Automation] Conditional and parallel subsequences
    This CL adds the following Kombucha modifiers:
    - If, IfElement, IfView
    - InParallel
    - AnyOf
    
    As per the proposal here:
    https://docs.google.com/document/d/1d24EycII3P8SlAsFV8MMlXVmSdLAadg7YBgYgXUpR88/edit?usp=sharing
    
    Bug: 1412149
    Change-Id: I66e489d3a4886130fbd0edf75270d188a3dd58fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214917
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Kuan Huang <kuanhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104612}

--- ui/base/interaction/interactive_test_internal.h
+++ ui/base/interaction/interactive_test_internal.h
@@ -230,6 +230,9 @@ void SpecifyElement(ui::InteractionSequence::StepBuilder& builder,
 
 std::string DescribeElement(ElementSpecifier spec);
 
+InteractionSequence::Builder BuildSubsequence(
+    InteractiveTestPrivate::MultiStep steps);
+
 }  // namespace internal
 
 }  // namespace ui::test
--- ui/views/interaction/interactive_views_test.h
+++ ui/views/interaction/interactive_views_test.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
+#include "ui/base/interaction/element_tracker.h"
 #include "ui/base/interaction/interaction_test_util.h"
 #include "ui/base/interaction/interactive_test.h"
 #include "ui/base/interaction/interactive_test_internal.h"
@@ -60,6 +61,8 @@ class InteractiveViewsTestApi : public ui::test::InteractiveTestApi {
   // a views::TrackedElementViews and of type `T`.
   template <typename T = View>
   static T* AsView(ui::TrackedElement* el);
+  template <typename T = View>
+  static const T* AsView(const ui::TrackedElement* el);
 
   // Runs a test InteractionSequence from a series of Steps or StepBuilders with
   // RunSynchronouslyForTesting(). Hooks both the completed and aborted
@@ -250,6 +253,14 @@ class InteractiveViewsTestApi : public ui::test::InteractiveTestApi {
   [[nodiscard]] StepBuilder ReleaseMouse(
       ui_controls::MouseButton button = ui_controls::LEFT);
 
+  // As IfElement(), but `condition` takes a single argument that is a const
+  // View pointer. If `element` is not a view of type V, then the test will
+  // fail.
+  template <template <typename...> typename C, typename V, typename T>
+  [[nodiscard]] static StepBuilder IfView(ElementSpecifier element,
+                                          C<bool(const V*)> condition,
+                                          T&& steps);
+
   // Sets the context widget. Must be called before RunTestSequence() or any of
   // the mouse functions.
   void SetContextWidget(Widget* context_widget);
@@ -321,6 +332,16 @@ T* InteractiveViewsTestApi::AsView(ui::TrackedElement* el) {
   auto* const views_el = el->AsA<TrackedElementViews>();
   CHECK(views_el);
   T* const view = AsViewClass<T>(views_el->view());
+  CHECK(view);
+  return view;
+}
+
+// static
+template <class T>
+const T* InteractiveViewsTestApi::AsView(const ui::TrackedElement* el) {
+  const auto* const views_el = el->AsA<TrackedElementViews>();
+  CHECK(views_el);
+  const T* const view = AsViewClass<T>(views_el->view());
   CHECK(view);
   return view;
 }
@@ -399,6 +420,26 @@ ui::InteractionSequence::StepBuilder InteractiveViewsTestApi::WithView(
          ui::TrackedElement* el) { std::move(function).Run(AsView<V>(el)); },
       base::OnceCallback<void(V*)>(std::move(function))));
   return builder;
+}
+
+// static
+template <template <typename...> typename C, typename V, typename T>
+ui::InteractionSequence::StepBuilder InteractiveViewsTestApi::IfView(
+    ElementSpecifier element,
+    C<bool(const V*)> condition,
+    T&& steps) {
+  return std::move(
+      IfElement(element,
+                base::BindOnce(
+                    [](base::OnceCallback<bool(const V*)> condition,
+                       const ui::InteractionSequence* seq,
+                       const ui::TrackedElement* el) {
+                      const V* const view = el ? AsView<V>(el) : nullptr;
+                      return std::move(condition).Run(view);
+                    },
+                    base::OnceCallback<bool(const V*)>(std::move(condition))),
+                std::move(steps))
+          .SetDescription("IfView()"));
 }
 
 // static
