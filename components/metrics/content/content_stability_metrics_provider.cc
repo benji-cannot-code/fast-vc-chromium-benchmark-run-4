273d157e909719dbe20e07cfb7682a0b8853697c
Mike Wittman <wittman@chromium.org>
2023-02-13 20:57:00 +0000

    Reland "[code health] Remove host created notification use by ContentStabilityMetricsProvider"
    This reverts commit a6d5d22a53d252da6d0f5c01bc0dbddec1a9848e.
    
    Reason for revert: the crashes were attributable to https://chromium-review.googlesource.com/c/chromium/src/+/4179204 rather than this CL
    
    Original change's description:
    > Revert "[code health] Remove host created notification use by ContentStabilityMetricsProvider"
    >
    > This reverts commit a8eb04ff2a5059b03c74cba6080ced2d2af2591a.
    >
    > Reason for revert: may be causing crashes on creation observation call
    >
    > Original change's description:
    > > [code health] Remove host created notification use by ContentStabilityMetricsProvider
    > >
    > > Removes the use of the render process host created notification
    > > in ContentStabilityMetricsProvider. Usage is replaced with the
    > > RenderProcessHostCreationObserver.
    > >
    > > Bug: 357627
    > > Change-Id: I7df0d465cf6c0c7c6b483b492328d6297403cb44
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181761
    > > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > > Reviewed-by: Weilun Shi <sweilun@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1095259}
    >
    > Bug: 357627, 1409290
    > Change-Id: I41c48e2aae19b296e720164da410bf01c60b6c62
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189879
    > Reviewed-by: Weilun Shi <sweilun@chromium.org>
    > Commit-Queue: Weilun Shi <sweilun@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Mike Wittman <wittman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095880}
    
    Bug: 357627, 1409290
    Change-Id: If9fb2e78cfdcda83e811b2d855d38a23c63f2d35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4201048
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104658}

--- components/metrics/content/content_stability_metrics_provider.cc
+++ components/metrics/content/content_stability_metrics_provider.cc
@@ -37,8 +37,6 @@ ContentStabilityMetricsProvider::ContentStabilityMetricsProvider(
                  content::NotificationService::AllSources());
   registrar_.Add(this, content::NOTIFICATION_RENDER_WIDGET_HOST_HANG,
                  content::NotificationService::AllSources());
-  registrar_.Add(this, content::NOTIFICATION_RENDERER_PROCESS_CREATED,
-                 content::NotificationService::AllSources());
 
 #if BUILDFLAG(IS_ANDROID)
   auto* crash_manager = crash_reporter::CrashMetricsReporter::GetInstance();
@@ -67,6 +65,13 @@ void ContentStabilityMetricsProvider::ClearSavedStabilityMetrics() {
 }
 #endif  // BUILDFLAG(IS_ANDROID)
 
+void ContentStabilityMetricsProvider::OnRenderProcessHostCreated(
+    content::RenderProcessHost* host) {
+  bool was_extension_process =
+      extensions_helper_ && extensions_helper_->IsExtensionProcess(host);
+  helper_.LogRendererLaunched(was_extension_process);
+}
+
 void ContentStabilityMetricsProvider::Observe(
     int type,
     const content::NotificationSource& source,
@@ -92,16 +97,8 @@ void ContentStabilityMetricsProvider::Observe(
       break;
     }
 
-    case content::NOTIFICATION_RENDER_WIDGET_HOST_HANG:
+    case content::NOTIFICATION_RENDER_WIDGET_HOST_HANG: {
       helper_.LogRendererHang();
-      break;
-
-    case content::NOTIFICATION_RENDERER_PROCESS_CREATED: {
-      bool was_extension_process =
-          extensions_helper_ &&
-          extensions_helper_->IsExtensionProcess(
-              content::Source<content::RenderProcessHost>(source).ptr());
-      helper_.LogRendererLaunched(was_extension_process);
       break;
     }
 
