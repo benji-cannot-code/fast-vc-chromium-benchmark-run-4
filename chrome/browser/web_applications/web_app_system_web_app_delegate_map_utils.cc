61d0652c7b8ef296ef3ebbdd613546f6bea97c80
Lei Zhang <thestig@chromium.org>
2023-02-13 23:21:13 +0000

    Remove platform checks from web_app_system_web_app_delegate_map_utils.
    These files are now Ash-only, so remove the IS_CHROMEOS_ASH checks
    within.
    
    Change-Id: I429827327cd8ef9624b7fdd9c1439947eca17020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244549
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104733}

--- chrome/browser/web_applications/web_app_system_web_app_delegate_map_utils.cc
+++ chrome/browser/web_applications/web_app_system_web_app_delegate_map_utils.cc
@@ -29,7 +29,6 @@ absl::optional<AppId> GetAppIdForSystemApp(
   return registrar.LookupExternalAppId(app_install_url.value());
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 absl::optional<ash::SystemWebAppType> GetSystemAppTypeForAppId(
     const WebAppRegistrar& registrar,
     const ash::SystemWebAppDelegateMap& delegates,
@@ -59,6 +58,5 @@ bool IsSystemWebApp(const WebAppRegistrar& registrar,
                     const AppId& app_id) {
   return GetSystemAppTypeForAppId(registrar, delegates, app_id).has_value();
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 }  // namespace web_app
