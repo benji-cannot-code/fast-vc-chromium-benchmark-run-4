4531620ff3a6004d2c5c06f0652ce98093947ac1
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-02-13 18:08:22 +0000

    Import wpt@5e08ba6e6409d5c7dc5dc6612eab7f05c0f0be3e
    Using wpt-import in Chromium 62953744eaf936edd3a5a932a5795d93fc4646e0.
    With Chromium commits locally applied on WPT:
    e03e0dfc48 "[bfcache] Use onopen listener for WebSocket in webtests"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I933b3063330373dc3a88005c04f68a200ec73a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245020
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1104558}

--- third_party/blink/web_tests/external/wpt/css/css-transitions/support/properties.js
+++ third_party/blink/web_tests/external/wpt/css/css-transitions/support/properties.js
@@ -260,7 +260,6 @@ var unspecified_properties = {
     'box-shadow': ['box-shadow'],
     'font-size-adjust': ['number'],
     'font-stretch': ['font-stretch'],
-    'marker-offset': ['length'],
     'text-decoration-color': ['color'],
     'column-count': ['integer'],
     'column-gap': ['length'],
@@ -270,10 +269,6 @@ var unspecified_properties = {
     'transform': ['transform'],
     'transform-origin': ['horizontal'],
     'zoom': ['number'],
-    'outline-radius-topleft': ['length', 'percentage'],
-    'outline-radius-topright': ['length', 'percentage'],
-    'outline-radius-bottomright': ['length', 'percentage'],
-    'outline-radius-bottomleft': ['length', 'percentage'],
     'display': ['display'],
     'position': ['position'],
     'object-view-box': ['object-view-box']
