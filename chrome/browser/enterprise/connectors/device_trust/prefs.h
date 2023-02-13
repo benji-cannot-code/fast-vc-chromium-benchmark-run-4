13b2549493ed09dbbc1a3ee326e76c4a461b4d8f
hmare <hmare@google.com>
2023-02-13 17:12:38 +0000

    Revert "[DTC-Key-Rotation] Create AutomatedKeyRotationEnabled policy"
    This reverts commit 9b943719f945fc9cf3bb51b55239cfca924df1fc.
    
    Reason for revert: This policy isn't needed for right now.
    
    Original change's description:
    > [DTC-Key-Rotation] Create AutomatedKeyRotationEnabled policy
    >
    > Change to create a new automated key rotation policy. This policy is
    > only modifiable by the DM Server and will be later used by the server
    > to set a nonce allowing the key rotation process to start.
    >
    > Design: go/automated-key-rotation
    >
    > Bug: b/267460175
    > Change-Id: I70b1d2906cae5db4f400ad3670bbed6a239ddd9b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220768
    > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Hamda Mare <hmare@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1102125}
    
    Bug: b/267460175
    Change-Id: Idd76dcfd904fae601cd69477f33d95e79fd5e788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234474
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104526}

--- chrome/browser/enterprise/connectors/device_trust/prefs.h
+++ chrome/browser/enterprise/connectors/device_trust/prefs.h
@@ -14,9 +14,6 @@ namespace enterprise_connectors {
 // Pref that maps to the "ContextAwareAccessSignalsAllowlistPref" policy.
 extern const char kContextAwareAccessSignalsAllowlistPref[];
 
-// Prefs that maps to the "AutomatedKeyRotationEnabled" policy.
-extern const char kAutomatedKeyRotationEnabledPref[];
-
 // Registers the device trust connectors profile preferences.
 void RegisterDeviceTrustConnectorProfilePrefs(PrefRegistrySimple* registry);
 
