fd6e4e1f09cebed69854825ab74c869a693b5286
Andrew Paseltiner <apaseltiner@chromium.org>
2023-02-13 17:54:02 +0000

    Check return value of sql::MetaTable::SetVersionNumber/SetCompatibleVersionNumber
    Without this, it is possible for a schema to be partially initialized,
    e.g. the tables are changed but the version is not, which could lead to
    unintended behavior, including crashes.
    
    In the future, these methods will be marked [[nodiscard]] to help
    prevent erroneous usage.
    
    This CL was uploaded by git cl split.
    
    R=vsemeniuk@google.com
    
    Bug: 1414092
    Change-Id: Iac120a3b908a73fc2bb1ad0c1f67decb56a80dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244840
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104551}

--- components/password_manager/core/browser/affiliation/affiliation_database.cc
+++ components/password_manager/core/browser/affiliation/affiliation_database.cc
@@ -164,7 +164,9 @@ bool AffiliationDatabase::Init(const base::FilePath& path) {
   }
 
   if (version < kVersion) {
-    metatable.SetVersionNumber(kVersion);
+    if (!metatable.SetVersionNumber(kVersion)) {
+      return false;
+    }
   }
 
   return true;
