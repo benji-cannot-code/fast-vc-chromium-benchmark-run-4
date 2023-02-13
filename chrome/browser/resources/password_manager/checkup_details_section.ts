cf55645737a25309e5b99010135b736bfb546efe
Viktor Semeniuk <vsemeniuk@google.com>
2023-02-13 12:51:31 +0000

    [Password Manager] Show delete disclaimer dialog
    This CL shows edit disclaimer dialog when clicking delete action menu
    item. After confirming deletion on this dialog password is removed.
    
    Bug: 1401001
    Change-Id: Ia7e2361efb459e03a0e2b0121edab201b9c31fc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236779
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104446}

--- chrome/browser/resources/password_manager/checkup_details_section.ts
+++ chrome/browser/resources/password_manager/checkup_details_section.ts
@@ -41,7 +41,7 @@ export interface CheckupDetailsSectionElement {
     moreActionsMenu: CrActionMenuElement,
     menuShowPassword: HTMLButtonElement,
     menuEditPassword: HTMLButtonElement,
-    menuRemovePassword: HTMLButtonElement,
+    menuDeletePassword: HTMLButtonElement,
     subtitle: HTMLElement,
   };
 }
@@ -239,6 +239,12 @@ export class CheckupDetailsSectionElement extends
     this.activeListItem_ = null;
     PasswordManagerImpl.getInstance().recordPasswordCheckInteraction(
         PasswordCheckInteraction.EDIT_PASSWORD);
+  }
+
+  private async onMenuDeletePasswordClick_() {
+    this.activeListItem_?.showDeleteDialog();
+    this.$.moreActionsMenu.close();
+    this.activeListItem_ = null;
   }
 
   private getShowHideTitle_(): string {
