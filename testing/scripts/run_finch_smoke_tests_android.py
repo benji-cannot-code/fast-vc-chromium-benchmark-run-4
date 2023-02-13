59c41970ef49812324e48b99c907970b295e5ea7
Rakib M. Hasan <rmhasan@google.com>
2023-02-13 20:03:58 +0000

    Move the add_emulator_args function to the common.py module
    Bug: None
    Change-Id: I37edfd04981940e431ae16a0abb7224297dbe720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245737
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Rakib Hasan <rmhasan@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104629}

--- testing/scripts/run_finch_smoke_tests_android.py
+++ testing/scripts/run_finch_smoke_tests_android.py
@@ -80,7 +80,7 @@ from scripts import common
 from skia_gold_infra.finch_skia_gold_properties import FinchSkiaGoldProperties
 from skia_gold_infra import finch_skia_gold_session_manager
 from skia_gold_infra import finch_skia_gold_utils
-from run_wpt_tests import add_emulator_args, get_device
+from run_wpt_tests import get_device
 
 LOGCAT_TAG = 'finch_test_runner_py'
 LOGCAT_FILTERS = [
@@ -1190,7 +1190,7 @@ def main(args):
         required=False,
         help='path to write test results JSON object to')
 
-  add_emulator_args(parser)
+  common.add_emulator_args(parser)
   script_common.AddDeviceArguments(parser)
   script_common.AddEnvironmentArguments(parser)
   logging_common.AddLoggingArguments(parser)
