5e53d722ba05c49907b34e675c940c2a836a2a97
Steinar H. Gunderson <sesse@chromium.org>
2023-02-13 11:07:25 +0000

    Trust bucketing decisions when doing selector matching.
    When we find a selector in a given RuleSet bucket, it generally
    means that part of that selector is already known to match;
    e.g. if we have a rule “div.c” and find it in the bucket for
    .c, we don't need to go ahead and re-scan the class="" attribute
    for the c class. There are some subtleties here, but in general,
    this allows us to save time when matching selectors at the cost
    of some time during initial setup.
    
    Even better, if there is only one simple selector, we can skip
    matching entirely, which drops both all setup and the cache miss.
    
    Style perftest (Zen 2, LTO but no PGO):
    
    Initial style (µs)     Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce               10156     10081   +0.7%  [ -0.4%,  +1.1%]
    Encyclopedia           100775    100314   +0.5%  [ +0.3%,  +0.6%]
    Extension              131210    131110   +0.1%  [ -0.1%,  +0.3%]
    News                    40024     40048   -0.1%  [ -0.2%,  +0.2%]
    Search                   2512      2566   -2.1%  [ -2.2%,  -1.9%]
    Social1                 22349     21863   +2.2%  [ +2.1%,  +2.3%]
    Social2                  1052      1049   +0.3%  [ -0.1%,  +1.6%]
    Sports                  37773     38532   -2.0%  [ -2.2%,  -1.6%]
    Video                   41085     42808   -4.0%  [ -4.2%,  -3.7%]
    Geometric mean                            -0.5%  [ -0.6%,  -0.4%]
    
    Recalc style (µs)      Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce               10808     10840   -0.3%  [ -0.5%,  -0.1%]
    Encyclopedia            83408     83029   +0.5%  [ +0.2%,  +0.7%]
    Extension              117280    117035   +0.2%  [ +0.0%,  +0.4%]
    News                    28583     28564   +0.1%  [ -0.1%,  +0.2%]
    Search                    233       231   +1.0%  [ +0.6%,  +1.6%]
    Social1                 15961     15487   +3.1%  [ +2.8%,  +3.2%]
    Social2                   572       562   +1.7%  [ +0.9%,  +3.7%]
    Sports                  22621     22604   +0.1%  [ -0.1%,  +0.5%]
    Video                   25166     25004   +0.6%  [ +0.5%,  +0.8%]
    Geometric mean                            +0.8%  [ +0.6%,  +1.0%]
    
    The negative initial style result for the Video subtest is annoying,
    but it seems this is some benchmarking oddity that only happens on Zen 2,
    and only on the initial run (repeated runs do not show it). We don't
    see this issue on Zen 3 or Intel, so we'll let it pass as a general
    net positive, not the least because the upcoming patch with easy
    selector checking seems to go in synergy with the perf effects
    of this patch (the combined effect is stronger than the two patches
    individually).
    
    Bug: 1410018
    Change-Id: Ia0e0b92709512b57e7dec53db27e47c938d93f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224934
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104421}

--- third_party/blink/renderer/core/css/rule_set.h
+++ third_party/blink/renderer/core/css/rule_set.h
@@ -117,7 +117,15 @@ class CORE_EXPORT RuleData {
   const CSSSelector& Selector() const {
     return rule_->SelectorAt(selector_index_);
   }
+  CSSSelector& MutableSelector() {
+    return rule_->MutableSelectorAt(selector_index_);
+  }
   unsigned SelectorIndex() const { return selector_index_; }
+  bool IsEntirelyCoveredByBucketing() const {
+    return is_entirely_covered_by_bucketing_;
+  }
+  void ComputeEntirelyCoveredByBucketing();
+  void ResetEntirelyCoveredByBucketing();
 
   bool ContainsUncommonAttributeSelector() const {
     return contains_uncommon_attribute_selector_;
@@ -183,7 +191,8 @@ class CORE_EXPORT RuleData {
   unsigned link_match_type_ : 2;
   unsigned has_document_security_origin_ : 1;
   unsigned valid_property_filter_ : 3;
-  // 30 bits above
+  unsigned is_entirely_covered_by_bucketing_ : 1;
+  // 31 bits above
   union {
     // Used by RuleMap before compaction, to hold what bucket this RuleData
     // is to be sorted into. (If the RuleData lives in a RuleMap, the hashes
@@ -509,6 +518,7 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
 
 #ifndef NDEBUG
   void Show() const;
+  const HeapVector<RuleData>& AllRulesForTest() const { return all_rules_; }
 #endif
 
   void Trace(Visitor*) const;
@@ -540,7 +550,7 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
                      const ContainerQuery*,
                      CascadeLayer*,
                      const StyleScope*);
-  void FindBestRuleSetAndAdd(const CSSSelector&, const RuleData&);
+  void FindBestRuleSetAndAdd(CSSSelector&, const RuleData&);
   void AddRule(StyleRule*,
                unsigned selector_index,
                AddRuleFlags,
