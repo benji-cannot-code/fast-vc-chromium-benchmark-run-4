03db4897fa06781b22e77533af887252573ed145
Mahmoud Rashad <mmrashad@google.com>
2023-02-13 13:55:07 +0000

    [ReadingList] Implement DualReadingListModel::GetEntryByURL()
    This CL implements DualReadingListModel::GetEntryByURL(), which
    encounters two cases:
    
    1- There is at most one entry with this url, which exists (if it does)
    either in the account_model or in the local_or_syncable_model, for this
    case, it leans on ReadingListModelImpl::GetEntryByURL() (which returns
    nullptr if there's no entry with this url).
    
    2- There are two entries with this url, one of them in the account_model
    and the other one in the local_or_syncable_model, for this case, it
    returns a merged entry. The entries get merged by using
    ReadingListEntry::MergeWithEntry().
    
    This CL also introduces a Clone() function to ReadingListEntry, since at
    least a copy from one of the entries that have a common url is needed.
    
    Bug: 1402196
    Change-Id: I9e5f21cfae2317f0d87dc659868f984810548627
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218993
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104459}

--- components/reading_list/core/dual_reading_list_model.h
+++ components/reading_list/core/dual_reading_list_model.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reading_list/core/reading_list_entry.h"
 #include "components/reading_list/core/reading_list_model.h"
 #include "components/reading_list/core/reading_list_model_observer.h"
+#include "url/gurl.h"
 
 namespace reading_list {
 
