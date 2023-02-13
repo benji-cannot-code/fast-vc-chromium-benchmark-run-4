e7473e0245759e79bfb1f8374462fab98fa5ff6a
David Bertoni <dbertoni@chromium.org>
2023-02-13 18:41:07 +0000

    [Extensions] Modernize tabs API TabReload test.
    This test used a function defined on the extension's background page to
    communicate from a window to the background page. This CL instead sends
    a message from the window to the background page, making it compatible
    with a service worker-based extension.
    
    It also makes the test simpler and easier to understand.
    
    Bug: 1093066
    Change-Id: Ia436712c8fb36ed623ec7b1d7cd2adc9dab00d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237414
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104579}

--- chrome/test/data/extensions/api_test/tabs/reload/a.js
+++ chrome/test/data/extensions/api_test/tabs/reload/a.js
@@ -4,6 +4,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 window.onload = function() {
-  var href = window.location.href;
-  chrome.extension.getBackgroundPage().onTabLoad(href);
+  chrome.runtime.sendMessage(window.location.href);
 };
