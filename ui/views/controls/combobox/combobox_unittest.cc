d6922489c44b83599287e7af16834ab7cc42654c
Elaine Chien <elainechien@chromium.org>
2023-02-13 23:36:38 +0000

    [ChromeRefresh2023] Add disabled state for combobox and appropriate colors
    Bug: 1400024
    Change-Id: Icea9fe75445f5cbf27edb825064315f906dc1313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234399
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104759}

--- ui/views/controls/combobox/combobox_unittest.cc
+++ ui/views/controls/combobox/combobox_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "ui/accessibility/ax_action_data.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -349,6 +350,22 @@ TEST_F(ComboboxTest, DisabilityTest) {
   View* container = widget_->SetContentsView(std::make_unique<View>());
   combobox_ = container->AddChildView(std::move(combobox));
   EXPECT_FALSE(combobox_->GetEnabled());
+}
+
+// Ensure the border on the combobox is set correctly when Enabled state
+// changes.
+TEST_F(ComboboxTest, DisabledBorderTest) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(features::kChromeRefresh2023);
+  InitCombobox(nullptr);
+  ASSERT_TRUE(combobox_->GetEnabled());
+  ASSERT_NE(combobox_->GetBorder(), nullptr);
+  combobox_->SetEnabled(false);
+  ASSERT_FALSE(combobox_->GetEnabled());
+  ASSERT_EQ(combobox_->GetBorder(), nullptr);
+  combobox_->SetEnabled(true);
+  ASSERT_TRUE(combobox_->GetEnabled());
+  ASSERT_NE(combobox_->GetBorder(), nullptr);
 }
 
 // On Mac, key events can't change the currently selected index directly for a
