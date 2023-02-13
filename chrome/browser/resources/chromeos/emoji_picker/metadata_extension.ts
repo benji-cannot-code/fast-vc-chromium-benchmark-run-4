43bad4510700f29f76a77c6ce7277ce74f2afb33
Sijin Soon <sijins@google.com>
2023-02-13 03:49:51 +0000

    Updated category header styling to match specifications.
    This CL fixes styling for Emoji Picker with GIF support flag on and off.
    
    Header layout with GIF support flag on: https://screenshot.googleplex.com/4H5Zb89DMRQZRsG.png
    
    Bug: b:266867388
    Change-Id: I548e7cb8248b70959b3470ab7b8ca3d060a131a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242320
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Sijin Soon <sijins@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104355}

--- chrome/browser/resources/chromeos/emoji_picker/metadata_extension.ts
+++ chrome/browser/resources/chromeos/emoji_picker/metadata_extension.ts
@@ -72,7 +72,7 @@ export const CATEGORY_METADATA:
     Array<{name: CategoryEnum, icon: string, active: boolean}> = [
       {
         name: CategoryEnum.EMOJI,
-        icon: 'emoji_picker_v2:emoji_emojis',
+        icon: 'emoji_picker_v2:emoji_emojis_outlined',
         active: true,
       },
       {
@@ -101,7 +101,7 @@ export const CATEGORY_TABS = {
   'emoji': [
     {
       name: 'Smileys & Emotions',
-      icon: 'emoji_picker_v2:emoji_emojis',
+      icon: 'emoji_picker_v2:emoji_emojis_filled',
       pagination: 1,
     },
     {
