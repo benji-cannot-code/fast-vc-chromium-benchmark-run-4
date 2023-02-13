5a4dce6da859e6d997e17e3f8544dd37b4df2905
Stefan Zager <szager@chromium.org>
2023-02-13 17:11:16 +0000

    Don't delay marking AX object dirty if layout is clean.
    Fixes the test TestCharacterExtentsInScrollableInputField when
    kSerializeAccessibilityPostLifecycle is enabled.
    
    Change-Id: I8cf35f1aa5ea0f397c46c2c738b236cff84c06a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242601
    Auto-Submit: Stefan Zager <szager@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104523}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -3536,7 +3536,7 @@ void AXObjectCacheImpl::PostPlatformNotification(
 }
 
 void AXObjectCacheImpl::EnsureMarkDirtyWithCleanLayout(Node* node) {
-  MarkAXObjectDirty(GetOrCreate(node));
+  MarkAXObjectDirtyWithCleanLayout(GetOrCreate(node));
 }
 
 void AXObjectCacheImpl::MarkAXObjectDirtyWithCleanLayoutHelper(
