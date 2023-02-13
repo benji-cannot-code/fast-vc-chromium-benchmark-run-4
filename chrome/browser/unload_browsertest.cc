ed67642e820b383fffe2b810fcd576dee6ed596e
Di Zhang <dizhangg@chromium.org>
2023-02-13 23:17:41 +0000

    Safeguard beforeunload cancel by preventDefault behind flag
    We want to put the new behavior (can cancel unload by calling
    preventDefault on beforeunload event) behind a feature flag. There is a
    low change of causing regressions, but we want to make this behavior
    easy to disable when needed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4133847/comment/14522696_e33a1dfe/
    
    Fixed: 866818
    Change-Id: I8dac44930b5d1afef6e56415cfbb99aa74e43a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236391
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104727}

--- chrome/browser/unload_browsertest.cc
+++ chrome/browser/unload_browsertest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_navigation_observer.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "third_party/blink/public/common/features.h"
 
 using content::BrowserThread;
 
@@ -886,6 +887,31 @@ IN_PROC_BROWSER_TEST_F(UnloadTest, BeforeUnloadListenerCancelByReturnValue) {
 IN_PROC_BROWSER_TEST_F(UnloadTest, BeforeUnloadListenerCancelByReturn) {
   std::string html =
       GenerateDataURL("return 'hello world'", /*is_onbeforeunload=*/false);
+  NavigateToDataURL(html.c_str(), "beforeunload");
+
+  CloseBrowsersVerifyUnloadSuccess(false);
+}
+
+// TODO(crbug/866818): Remove below test when feature
+// BeforeunloadEventCancelByPreventDefault is fully stable.
+class UnloadTestCancelByPreventDefaultDisabled : public UnloadTest {
+ public:
+  UnloadTestCancelByPreventDefaultDisabled() {
+    scoped_feature_list.InitAndDisableFeature(
+        blink::features::kBeforeunloadEventCancelByPreventDefault);
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list;
+};
+
+// Tests closing the browser with onbeforeunload handler and
+// event.preventDefault() will not prompt confirmation dialog when
+// BeforeunloadEventCancelByPreventDefault is disabled.
+IN_PROC_BROWSER_TEST_F(UnloadTestCancelByPreventDefaultDisabled,
+                       OnBeforeUnload) {
+  std::string html =
+      GenerateDataURL("event.preventDefault()", /*is_onbeforeunload=*/true);
   NavigateToDataURL(html.c_str(), "beforeunload");
 
   CloseBrowsersVerifyUnloadSuccess(false);
