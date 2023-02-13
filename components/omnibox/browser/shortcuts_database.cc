6c42be60af854a6203be678834853429aa0ba4fb
Andrew Paseltiner <apaseltiner@chromium.org>
2023-02-13 23:25:46 +0000

    Check return value of sql::MetaTable::SetVersionNumber/SetCompatibleVersionNumber
    Without this, it is possible for a schema to be partially initialized,
    e.g. the tables are changed but the version is not, which could lead to
    unintended behavior, including crashes.
    
    In the future, these methods will be marked [[nodiscard]] to help
    prevent erroneous usage.
    
    This CL was uploaded by git cl split.
    
    R=mfacey@chromium.org
    
    Bug: 1414092
    Change-Id: I2986b0a29e1f60e4b11a4cddfa8be5263d064748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245037
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104741}

--- components/omnibox/browser/shortcuts_database.cc
+++ components/omnibox/browser/shortcuts_database.cc
@@ -307,7 +307,9 @@ bool ShortcutsDatabase::EnsureTable() {
   for (int i = current_version + 1; i <= kCurrentVersionNumber; ++i) {
     if (!DoMigration(i))
       return false;
-    meta_table_.SetVersionNumber(i);
+    if (!meta_table_.SetVersionNumber(i)) {
+      return false;
+    }
   }
 
   return true;
