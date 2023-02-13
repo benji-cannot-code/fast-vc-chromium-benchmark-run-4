dd922dbc7d185facfb66e7b6e8dcb07e66dbd3dd
Andrew Paseltiner <apaseltiner@chromium.org>
2023-02-13 20:39:51 +0000

    Check return value of sql::MetaTable::SetVersionNumber/SetCompatibleVersionNumber
    Without this, it is possible for a schema to be partially initialized,
    e.g. the tables are changed but the version is not, which could lead to
    unintended behavior, including crashes.
    
    In the future, these methods will be marked [[nodiscard]] to help
    prevent erroneous usage.
    
    This CL was uploaded by git cl split.
    
    R=jkarlin@chromium.org
    
    Bug: 1414092
    Change-Id: I90d041f399f3dbce29035eb7d57605a4bbeac31a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244780
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104646}

--- components/services/storage/shared_storage/shared_storage_database_migrations.cc
+++ components/services/storage/shared_storage/shared_storage_database_migrations.cc
@@ -74,8 +74,7 @@ bool MigrateToVersion2(sql::Database& db,
   if (!db.Execute(kDropLastUsedTimeIndexSql))
     return false;
 
-  meta_table.SetVersionNumber(2);
-  return transaction.Commit();
+  return meta_table.SetVersionNumber(2) && transaction.Commit();
 }
 
 }  // namespace
