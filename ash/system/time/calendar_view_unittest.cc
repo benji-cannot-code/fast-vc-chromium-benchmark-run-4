23d532d38e1d2c57d3c4c05459553945849c9083
Sam Cackett <samcackett@google.com>
2023-02-14 11:11:36 +0000

    Ensure upcoming events from next day are included.
    This CL fixes an issue where events that were "upcoming" on the subsequent day i.e. starting in the next ten mins on the subsequent day, were not included in the `UpcomingEvents` results.
    
    Bug: b/269121140
    Change-Id: I58e8f489105706fb6c22e088d236b66ce0a8c1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244922
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104982}

--- ash/system/time/calendar_view_unittest.cc
+++ ash/system/time/calendar_view_unittest.cc
@@ -2591,7 +2591,7 @@ TEST_F(
                     CreateMockEventListStartingFivePastMidnight());
 
   // Up next view should be shown.
-  EXPECT_TRUE(up_next_view());
+  ASSERT_TRUE(up_next_view());
 
   LeftClickOn(up_next_todays_events_button());
 
