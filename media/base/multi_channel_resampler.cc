cec4992c4a2240c605edbb7b02e2da1f2355e432
Thomas Guilbert <tguilbert@chromium.org>
2023-02-14 19:51:54 +0000

    Support variable resampler kernel sizes
    A recent change [1] doubled the resampler kernel size from 32 to 64,
    improving the quality of resampling. However, this causes us to run into
    a CHECK and crash when using very low resampling rates: 10ms of audio
    at 8kHz is only 80 frames, which is less than the minimum of 96 frames
    (1.5 * kernel size) required to use a resampler of kernel size 64.
    
    This CL fixes the issue by dynamically choosing between kernel sizes.
    In the vast majority of cases, we will use a kernel size of 64
    frames, but will fallback to 32 frames if the request frames size is
    too low.
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/4168827
    
    Bug: 1412315, 1407622
    Change-Id: Ia2cc56c814a3974e13d015a8079329e364af8146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236081
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105210}

--- media/base/multi_channel_resampler.cc
+++ media/base/multi_channel_resampler.cc
@@ -135,4 +135,9 @@ void MultiChannelResampler::PrimeWithSilence() {
     resamplers_[i]->PrimeWithSilence();
 }
 
+int MultiChannelResampler::KernelSize() const {
+  DCHECK(!resamplers_.empty());
+  return resamplers_[0]->KernelSize();
+}
+
 }  // namespace media
