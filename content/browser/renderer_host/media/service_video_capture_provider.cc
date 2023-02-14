fc0127e82c81aae2a7e412ad902009e367fe80dd
Florent Castelli <orphis@chromium.org>
2023-02-14 12:07:01 +0000

    Add UMA statistics to track the result for GetDeviceInfosAsync
    We're interested in tracking if the retries lead to a successful
    enumeration or not. Those retries will typically happen after a
    crash of the VideoCapture service on macOS.
    
    Bug: 1379392
    Change-Id: Ic9b61bc6e8a415c88b83912f53d5fdbf17c7cc56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245218
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104991}

--- content/browser/renderer_host/media/service_video_capture_provider.cc
+++ content/browser/renderer_host/media/service_video_capture_provider.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -42,6 +42,20 @@ CreateAcceleratorFactory() {
       content::DelegateToBrowserGpuServiceAcceleratorFactory>();
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+// Do not reorder, used for UMA Media.VideoCapture.GetDeviceInfosResult
+enum class GetDeviceInfosResult {
+  kSucessNoRetry = 0,
+  kFailureNoRetry = 1,
+  kSucessAfterRetry = 2,
+  kFailureAfterRetry = 3,
+  kMaxValue = kFailureAfterRetry,
+};
+
+void LogGetDeviceInfosResult(GetDeviceInfosResult result) {
+  base::UmaHistogramEnumeration("Media.VideoCapture.GetDeviceInfosResult",
+                                result);
+}
 
 }  // anonymous namespace
 
@@ -269,6 +283,9 @@ void ServiceVideoCaptureProvider::OnDeviceInfosReceived(
     scoped_refptr<RefCountedVideoSourceProvider> service_connection,
     const std::vector<media::VideoCaptureDeviceInfo>& infos) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
+  LogGetDeviceInfosResult(get_device_infos_retried_
+                              ? GetDeviceInfosResult::kSucessAfterRetry
+                              : GetDeviceInfosResult::kSucessNoRetry);
   for (GetDeviceInfosCallback& callback : get_device_infos_pending_callbacks_) {
     std::move(callback).Run(media::mojom::DeviceEnumerationResult::kSuccess,
                             infos);
@@ -292,6 +309,10 @@ void ServiceVideoCaptureProvider::OnDeviceInfosRequestDropped(
         "ServiceVideoCaptureProvider::OnDeviceInfosRequestDropped: Too many "
         "retries");
   }
+
+  LogGetDeviceInfosResult(get_device_infos_retried_
+                              ? GetDeviceInfosResult::kFailureAfterRetry
+                              : GetDeviceInfosResult::kFailureNoRetry);
 
   // After too many retries, we just return an empty list
   for (GetDeviceInfosCallback& callback : get_device_infos_pending_callbacks_) {
