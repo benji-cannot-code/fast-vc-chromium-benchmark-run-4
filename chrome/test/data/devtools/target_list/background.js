8786260aef9da60f6141b7157e7f304c9405f3ed
Danil Somsikov <dsv@chromium.org>
2023-02-14 21:36:25 +0000

    Update and enable RemoteDebuggingTest.RemoteDebugger.
    checkInspectablePagesUI is removed because the implementation was removed in the crrev.com/c/3440208
    
    checkWebSocketAPI needs switches::kRemoteAllowOrigins to account for the  crrev.com/c/4106102
    
    new{Default,Specific}Page need to use PUT HTTP method to account for the crrev.com/c/4110715
    
    It is not clear if this addresses the original brokenness but the logs are long gone, so we need to re-enable anyway.
    
    Bug: 997911
    Change-Id: I86450602461d923cd81ede265fe6bbcab3d34037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244918
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105279}

--- chrome/test/data/devtools/target_list/background.js
+++ chrome/test/data/devtools/target_list/background.js
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var REMOTE_DEBUGGER_HOST = 'localhost:9222';
 
-function requestUrl(path, callback) {
+function requestUrl(path, callback, opt_method) {
   var req = new XMLHttpRequest();
-  req.open('GET', 'http://' + REMOTE_DEBUGGER_HOST + path, true);
+  req.open(opt_method || 'GET', 'http://' + REMOTE_DEBUGGER_HOST + path, true);
   req.onload = function() {
     if (req.status == 200)
       callback(req.responseText);
@@ -110,7 +110,7 @@ function runNewPageTest(devtoolsUrl, expectedUrl) {
       function(text) {
         json = JSON.parse(text);
         checkResult();
-      });
+      }, 'PUT');
 }
 
 var extensionTargetId;
@@ -175,16 +175,6 @@ chrome.test.runTests([
                 chrome.test.succeed();
               });
         });
-  },
-
-  function checkInspectablePagesUI() {
-    requestUrl("", function(text) {
-      console.log(text);
-      chrome.test.assertTrue(
-          /<html[\s\S]*<head[\s\S]*<title[\s\S]*<script[\s\S]*<body/
-          .test(text));
-      chrome.test.succeed();
-    });
   },
 
   function checkDebuggerUI() {
