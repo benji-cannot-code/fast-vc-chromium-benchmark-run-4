82f0e11f7bfd4f1b3135a07d38fd4476ed74265b
Tom Sepez <tsepez@chromium.org>
2023-02-14 19:10:50 +0000

    Avoid one dangling ptr warning in ~ShellExtensionLoaderTest()
    During TeadDown(), drop unowned reference to TestEventRouter
    before invoking superclass TearDown() which will destroy it.
    
    Bug: 1401495
    Change-Id: I62d002ca7de1d1a8a860791d9390f61db8948480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240464
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105185}

--- extensions/shell/browser/shell_extension_loader_unittest.cc
+++ extensions/shell/browser/shell_extension_loader_unittest.cc
@@ -105,6 +105,7 @@ class ShellExtensionLoaderTest : public ExtensionsTest {
 
   void TearDown() override {
     EXPECT_FALSE(KeepAliveRegistry::GetInstance()->IsKeepingAlive());
+    event_router_ = nullptr;
     ExtensionsTest::TearDown();
   }
 
