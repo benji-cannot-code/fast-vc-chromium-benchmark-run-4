5a44fe6ebf49c0be7a7ab71cf70171623047c0d2
Joe Antonetti <joeantonetti@google.com>
2023-02-14 17:48:36 +0000

    [QuickStart] Removed TargetDeviceClientBase
    Removed an outdated class - TargetDeviceClientBase
    
    This change also removes subclasses, none of
    which contained business logic.
    
    The codebase has moved away from this, warranting
    it's removal
    
    Bug: b/234655072
    Test: Added Unit tests to Connection in connection_unittest.cc
    
    Change-Id: I4f10e277dbbc301919d3b66cba1f534190059e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240758
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105130}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/authenticated_connection.cc
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/authenticated_connection.cc
@@ -63,6 +63,8 @@ const uint8_t kAuthenticatorGetAssertionCommand = 0x02;
 const char kUserPresenceMapKey[] = "up";
 const char kUserVerificationMapKey[] = "uv";
 
+const char kNotifySourceOfUpdateMessageKey[] = "isForcedUpdateRequired";
+
 }  // namespace
 
 AuthenticatedConnection::AuthenticatedConnection(
@@ -94,6 +96,12 @@ void AuthenticatedConnection::RequestAccountTransferAssertion(
 
   // Call into SetBootstrapOptions, starting the chain of callbacks.
   SendBootstrapOptions(std::move(get_info));
+}
+
+void AuthenticatedConnection::NotifySourceOfUpdate() {
+  base::Value::Dict message_payload;
+  message_payload.Set(kNotifySourceOfUpdateMessageKey, true);
+  SendPayload(message_payload);
 }
 
 void AuthenticatedConnection::SendBootstrapOptions(
