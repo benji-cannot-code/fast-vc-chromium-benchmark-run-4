b82da7ec7407b6eb31c180894d2b3008d245657f
Alex Rudenko <alexrudenko@chromium.org>
2023-02-14 06:52:45 +0000

    Allow extensions to use the Accessibility domain
    Change-Id: Id3d4f9f7bbd1309a695454fc28a557c7894418e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245419
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104927}

--- third_party/blink/renderer/core/inspector/devtools_session.h
+++ third_party/blink/renderer/core/inspector/devtools_session.h
@@ -34,6 +34,7 @@ class Document;
 class DocumentLoader;
 class InspectorAgent;
 class LocalFrame;
+class InspectorAccessibilityAgent;
 class InspectorAuditsAgent;
 class InspectorCSSAgent;
 class InspectorCacheStorageAgent;
@@ -139,6 +140,7 @@ class CORE_EXPORT DevToolsSession : public GarbageCollected<DevToolsSession>,
     return std::disjunction_v<std::is_same<T, InspectorAuditsAgent>,
                               std::is_same<T, InspectorCSSAgent>,
                               std::is_same<T, InspectorCacheStorageAgent>,
+                              std::is_same<T, InspectorAccessibilityAgent>,
                               std::is_same<T, InspectorDOMAgent>,
                               std::is_same<T, InspectorDOMDebuggerAgent>,
                               std::is_same<T, InspectorDOMSnapshotAgent>,
