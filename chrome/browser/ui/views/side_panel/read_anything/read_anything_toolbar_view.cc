70a4b8b6d838ef65d6f0b88d79cd3c3464597d7e
Kristi Saney <kristislee@google.com>
2023-02-14 00:51:27 +0000

    Add padding between menu buttons.
    Before: https://screenshot.googleplex.com/BgXgGaGTXomgsA5
    After: https://screenshot.googleplex.com/hBTyyKssfdip34h
    
    Bug: 1266555
    Change-Id: I3ef7dd266315d4127f29586e3ef3fbc4067717f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244459
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104799}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_toolbar_view.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_toolbar_view.cc
@@ -39,6 +39,7 @@ ReadAnythingToolbarView::ReadAnythingToolbarView(
   // Set a FlexLayout LayoutManager for this view.
   SetLayoutManager(std::make_unique<views::FlexLayout>())
       ->SetOrientation(views::LayoutOrientation::kHorizontal)
+      .SetDefault(views::kMarginsKey, gfx::Insets::VH(0, kButtonPadding))
       .SetMainAxisAlignment(views::LayoutAlignment::kStart)
       .SetInteriorMargin(gfx::Insets(kInternalInsets));
 
