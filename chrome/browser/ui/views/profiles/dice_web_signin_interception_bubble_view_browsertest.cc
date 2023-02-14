389714dad210540cddd6bc57cbedc83d6c6834bf
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2023-02-14 16:38:28 +0000

    Dedupe ProfileDestructionWatcher in tests
    3 different tests define a helper class that implements ProfileObserver.
    It listens for OnProfileWillBeDestroyed(), and lets you wait for this
    event synchronously.
    
    Move this helper class to a dedicated file, to reduce code duplication.
    
    Change-Id: I9c421047ac908fb6dc0f91990b094d709546ad8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220740
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105091}

--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view_browsertest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/profile_destruction_waiter.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "components/google/core/common/google_util.h"
 #include "components/policy/core/common/management/scoped_management_service_override_for_testing.h"
@@ -55,35 +56,6 @@ struct TestParam {
   bool use_dark_theme = false;
   SkColor4f intercepted_profile_color = SkColors::kLtGray;
   SkColor4f primary_profile_color = SkColors::kBlue;
-};
-
-class ProfileDestructionWatcher : public ProfileObserver {
- public:
-  ProfileDestructionWatcher() = default;
-
-  ProfileDestructionWatcher(const ProfileDestructionWatcher&) = delete;
-  ProfileDestructionWatcher& operator=(const ProfileDestructionWatcher&) =
-      delete;
-
-  ~ProfileDestructionWatcher() override = default;
-
-  void Watch(Profile* profile) { observed_profiles_.AddObservation(profile); }
-  void WaitForDestruction() { run_loop_.Run(); }
-  bool destroyed() const { return destroyed_; }
-
- private:
-  // ProfileObserver:
-  void OnProfileWillBeDestroyed(Profile* profile) override {
-    DCHECK(!destroyed_) << "Double profile destruction";
-    destroyed_ = true;
-    observed_profiles_.RemoveObservation(profile);
-    run_loop_.Quit();
-  }
-
-  bool destroyed_ = false;
-  base::RunLoop run_loop_;
-  base::ScopedMultiSourceObservation<Profile, ProfileObserver>
-      observed_profiles_{this};
 };
 
 // To be passed as 4th argument to `INSTANTIATE_TEST_SUITE_P()`, allows the test
@@ -487,14 +459,13 @@ IN_PROC_BROWSER_TEST_F(DiceWebSigninInterceptionBubbleBrowserTest,
   EXPECT_FALSE(callback_result_.has_value());
 
   // Close the browser without closing the bubble.
-  ProfileDestructionWatcher profile_destruction_watcher;
-  profile_destruction_watcher.Watch(new_profile);
+  ProfileDestructionWaiter profile_destruction_waiter(new_profile);
   new_browser->window()->Close();
 
   // The profile is not destroyed, because the bubble is retaining it.
   EXPECT_TRUE(g_browser_process->profile_manager()->HasKeepAliveForTesting(
       new_profile, ProfileKeepAliveOrigin::kDiceWebSigninInterceptionBubble));
-  EXPECT_FALSE(profile_destruction_watcher.destroyed());
+  EXPECT_FALSE(profile_destruction_waiter.destroyed());
 
   // Close the bubble.
   views::test::WidgetDestroyedWaiter widget_destroyed_waiter(widget);
@@ -504,7 +475,7 @@ IN_PROC_BROWSER_TEST_F(DiceWebSigninInterceptionBubbleBrowserTest,
   EXPECT_EQ(callback_result_, SigninInterceptionResult::kIgnored);
 
   // The keep-alive is released and the profile is destroyed.
-  profile_destruction_watcher.WaitForDestruction();
+  profile_destruction_waiter.Wait();
 
   // Check that histograms are recorded.
   histogram_tester.ExpectUniqueSample("Signin.InterceptResult.MultiUser",
---
+++ chrome/test/base/profile_destruction_waiter.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/base/profile_destruction_waiter.h"
+
+ProfileDestructionWaiter::ProfileDestructionWaiter(Profile* profile) {
+  profile_observer_.Observe(profile);
+}
+
+ProfileDestructionWaiter::~ProfileDestructionWaiter() = default;
+
+void ProfileDestructionWaiter::Wait() {
+  run_loop_.Run();
+}
+
+void ProfileDestructionWaiter::OnProfileWillBeDestroyed(Profile* profile) {
+  DCHECK(!destroyed_);
+  destroyed_ = true;
+  run_loop_.Quit();
+  profile_observer_.Reset();
+}
