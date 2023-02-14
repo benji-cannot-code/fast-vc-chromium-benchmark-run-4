0e7b37561535b05707d87267ab086e0f5c0dd7bd
Ovidio Ruiz-Henríquez <odejesush@chromium.org>
2023-02-14 15:40:54 +0000

    extensions: Add Feature::RequiresDelegatedAvailabilityCheck()
    This change updates Feature, SimpleFeature, and ComplexFeature to
    support setting and retrieving a "requires delegated availability check"
    boolean flag.
    
    The RequiresDelegatedAvailabilityCheck() value is set by auto-generated
    C++ code written by json_schema_compiler. The value will be used to
    determine if a feature should be available contingent on the
    availability check being present and returning true. Both the value
    setting and usage will be added in later changes.
    
    Bug: 1233993, b/264596222, b/267683775
    Change-Id: I8f3a24e9648d84c13d1f74631b4b75b181dc7fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4209582
    Commit-Queue: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105065}

--- extensions/common/features/simple_feature.h
+++ extensions/common/features/simple_feature.h
@@ -96,6 +96,7 @@ class SimpleFeature : public Feature {
   bool IsInternal() const override;
   bool IsIdInBlocklist(const HashedExtensionId& hashed_id) const override;
   bool IsIdInAllowlist(const HashedExtensionId& hashed_id) const override;
+  bool RequiresDelegatedAvailabilityCheck() const override;
 
   static bool IsIdInArray(const std::string& extension_id,
                           const char* const array[],
@@ -129,6 +130,11 @@ class SimpleFeature : public Feature {
   void set_session_types(
       std::initializer_list<mojom::FeatureSessionType> types);
   void set_internal(bool is_internal) { is_internal_ = is_internal; }
+  void set_requires_delegated_availability_check(
+      bool requires_delegated_availability_check) {
+    requires_delegated_availability_check_ =
+        requires_delegated_availability_check;
+  }
   void set_developer_mode_only(bool is_developer_mode_only) {
     developer_mode_only_ = is_developer_mode_only;
   }
@@ -290,6 +296,7 @@ class SimpleFeature : public Feature {
 
   bool component_extensions_auto_granted_;
   bool is_internal_;
+  bool requires_delegated_availability_check_{false};
   bool developer_mode_only_{false};
   bool disallow_for_service_workers_;
 };
