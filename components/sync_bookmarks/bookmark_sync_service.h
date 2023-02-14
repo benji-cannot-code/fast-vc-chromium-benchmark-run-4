8dbfb6864268c829729b60c2f645814b0f88b4db
Boris Sazonov <bsazonov@google.com>
2023-02-14 12:50:20 +0000

    [Bookmarks] Remove redundant null checks from BookmarkSyncService
    bookmark_model_type_processor_ in BookmarkSyncService is always
    non-null and never changes, so this CL marks it as const and removes
    redundant null checks.
    
    Bug: 1404250
    Change-Id: Ia6759c89a9c0cbaa44b429c9cffd7f66531442cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240198
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105006}

--- components/sync_bookmarks/bookmark_sync_service.h
+++ components/sync_bookmarks/bookmark_sync_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/sync_bookmarks/bookmark_model_type_processor.h"
 
 class BookmarkUndoService;
 
@@ -67,8 +68,7 @@ class BookmarkSyncService : public KeyedService {
  private:
   // BookmarkModelTypeProcessor handles communications between sync engine and
   // BookmarkModel/HistoryService.
-  std::unique_ptr<sync_bookmarks::BookmarkModelTypeProcessor>
-      bookmark_model_type_processor_;
+  BookmarkModelTypeProcessor bookmark_model_type_processor_;
 };
 
 }  // namespace sync_bookmarks
