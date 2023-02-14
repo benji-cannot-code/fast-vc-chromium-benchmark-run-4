74ce497cbbac10f6ea37523aeb236120a94c2f63
Andrew Paseltiner <apaseltiner@chromium.org>
2023-02-14 18:27:40 +0000

    Store report URL in AttributionDebugReport class
    Instead of generating it on demand, which requires additional string
    copies.
    
    Change-Id: I4352bad294fb2d727842bb2b62ae84b048fc45b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4247662
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105158}

--- content/browser/attribution_reporting/attribution_debug_report.cc
+++ content/browser/attribution_reporting/attribution_debug_report.cc
@@ -359,6 +359,15 @@ base::Value::Dict GetReportData(DebugDataType type, base::Value::Dict body) {
   return dict;
 }
 
+GURL ReportURL(const attribution_reporting::SuitableOrigin& reporting_origin) {
+  static constexpr char kPath[] =
+      "/.well-known/attribution-reporting/debug/verbose";
+
+  GURL::Replacements replacements;
+  replacements.SetPathStr(kPath);
+  return reporting_origin->GetURL().ReplaceComponents(replacements);
+}
+
 }  // namespace
 
 // static
@@ -426,10 +435,10 @@ absl::optional<AttributionDebugReport> AttributionDebugReport::Create(
 
 AttributionDebugReport::AttributionDebugReport(
     base::Value::List report_body,
-    attribution_reporting::SuitableOrigin reporting_origin,
+    const attribution_reporting::SuitableOrigin& reporting_origin,
     base::Time original_report_time)
     : report_body_(std::move(report_body)),
-      reporting_origin_(std::move(reporting_origin)),
+      report_url_(ReportURL(reporting_origin)),
       original_report_time_(original_report_time) {
   DCHECK(!report_body_.empty());
 }
@@ -441,14 +450,5 @@ AttributionDebugReport::AttributionDebugReport(AttributionDebugReport&&) =
 
 AttributionDebugReport& AttributionDebugReport::operator=(
     AttributionDebugReport&&) = default;
-
-GURL AttributionDebugReport::ReportURL() const {
-  static constexpr char kPath[] =
-      "/.well-known/attribution-reporting/debug/verbose";
-
-  GURL::Replacements replacements;
-  replacements.SetPathStr(kPath);
-  return reporting_origin_->GetURL().ReplaceComponents(replacements);
-}
 
 }  // namespace content
