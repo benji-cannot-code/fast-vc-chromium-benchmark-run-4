3f98dd775dcabcdd9d5a18f54146b97e4cfa1638
Tao Wu <wutao@chromium.org>
2023-02-14 19:44:18 +0000

    assistant: Check if assistant status is ready
    If assistant status is not ready, the `assistant_settings` is not
    initialized. In this case, we do not call functions on its members.
    
    Bug: b:265093297
    Test: speculative fix
    Change-Id: Id3a44b4fce076f3694f95ce2fa02cc3fec910f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226157
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105206}

--- chrome/browser/ui/webui/ash/login/assistant_optin_flow_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/assistant_optin_flow_screen_handler.cc
@@ -340,8 +340,11 @@ void AssistantOptInFlowScreenHandler::SendGetSettingsRequest() {
 void AssistantOptInFlowScreenHandler::StopSpeakerIdEnrollment() {
   DCHECK(voice_match_enrollment_started_);
   voice_match_enrollment_started_ = false;
-  CHECK(assistant::AssistantSettings::Get());
-  if (AssistantState::Get()->settings_enabled().value_or(false)) {
+
+  if (AssistantState::Get()->settings_enabled().value_or(false) &&
+      AssistantState::Get()->assistant_status() ==
+          assistant::AssistantStatus::READY) {
+    CHECK(assistant::AssistantSettings::Get());
     assistant::AssistantSettings::Get()->StopSpeakerIdEnrollment();
   }
   // Reset the mojom receiver of |SpeakerIdEnrollmentClient|.
@@ -554,8 +557,11 @@ void AssistantOptInFlowScreenHandler::HandleRelatedInfoScreenUserAction(
 
 void AssistantOptInFlowScreenHandler::HandleVoiceMatchScreenUserAction(
     const std::string& action) {
-  // If the Assistant is disabled, discard the action and end the flow instead.
-  if (!AssistantState::Get()->settings_enabled().value_or(false)) {
+  // If the Assistant is disabled or is not ready, discard the action and end
+  // the flow instead.
+  if (!AssistantState::Get()->settings_enabled().value_or(false) ||
+      AssistantState::Get()->assistant_status() ==
+          assistant::AssistantStatus::NOT_READY) {
     HandleFlowFinished();
     return;
   }
