9f97385e8e034517d53138f6f24457f7f7e72b0c
Ashwin Verleker <ashwinpv@google.com>
2023-02-14 16:45:41 +0000

    [chromeperf] Use login url in wpr record mode
    Bug: 1410350
    Change-Id: I037cbb67822c0fd2d9f0b8a6a0ede95b7b65da59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4246734
    Commit-Queue: Ashwin Verleker <ashwinpv@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105096}

--- tools/perf/page_sets/system_health/browsing_stories.py
+++ tools/perf/page_sets/system_health/browsing_stories.py
@@ -1475,6 +1475,8 @@ class _GmailBrowsingStory(system_health_story.SystemHealthStory):
   """
   SKIP_LOGIN = False
 
+  USE_LOGINURL = False
+
   # Patch performance.mark and measure to get notified about page events.
   PERFOMANCE_MARK_AND_MEASURE = '''
     window.__telemetry_observed_page_events = new Set();
@@ -1517,7 +1519,9 @@ class _GmailBrowsingStory(system_health_story.SystemHealthStory):
         performance_mark_and_measure=self.PERFOMANCE_MARK_AND_MEASURE)
 
   def _Login(self, action_runner):
-    if self.wpr_mode == wpr_modes.WPR_OFF:
+    if self.USE_LOGINURL or self.wpr_mode in [
+        wpr_modes.WPR_OFF, wpr_modes.WPR_RECORD
+    ]:
       self._url = google_login.GetLoginUrl(self.URL)
     else:
       google_login.NewLoginGoogleAccount(action_runner, 'googletest')
