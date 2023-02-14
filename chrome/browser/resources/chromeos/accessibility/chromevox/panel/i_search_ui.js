0e685deaf83f8b8d74cd3528ad02e7a9e8488e79
Anastasia Helfinstein <anastasi@google.com>
2023-02-14 18:36:04 +0000

    [ChromeVox] Clean up ISearchUI
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:265481751
    Test: cq
    Change-Id: Ia96f1128994e17c9d1d099f14d9f4698eb490cdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4246805
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105166}

--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/i_search_ui.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/i_search_ui.js
@@ -20,8 +20,8 @@ export class ISearchUI {
     this.input_ = input;
     this.dir_ = Dir.FORWARD;
 
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onTextInput = this.onTextInput.bind(this);
+    this.onKeyDown = event => this.onKeyDown_(event);
+    this.onTextInput = event => this.onTextInput_(event);
 
     input.addEventListener('keydown', this.onKeyDown, true);
     input.addEventListener('textInput', this.onTextInput, false);
@@ -47,8 +47,9 @@ export class ISearchUI {
    * Listens to key down events.
    * @param {Event} evt
    * @return {boolean}
+   * @private
    */
-  onKeyDown(evt) {
+  onKeyDown_(evt) {
     switch (evt.key) {
       case 'ArrowUp':
         this.dir_ = Dir.BACKWARD;
@@ -79,8 +80,9 @@ export class ISearchUI {
    * Listens to text input events.
    * @param {Event} evt
    * @return {boolean}
+   * @private
    */
-  onTextInput(evt) {
+  onTextInput_(evt) {
     const searchStr = evt.target.value + evt.data;
     BackgroundBridge.PanelBackground.incrementalSearch(searchStr, this.dir_);
     return true;
