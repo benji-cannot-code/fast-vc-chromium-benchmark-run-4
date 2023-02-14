94521c3b15a3e165c4bb88b3c1d5e08359afe3c7
Pi-Hsun Shih <pihsun@chromium.org>
2023-02-14 06:10:43 +0000

    CCA: ESLint: Fix floating promise warning in error.ts
    `cca.py lint --eslintrc .eslintrc_floating_promise.js` error count goes
    down from 115 to 114.
    
    Bug: b:172336355
    Test: cca.py lint --eslintrc .eslintrc_floating_promise.js
    Change-Id: Ib5705ba61edd56864854392020a2f32c1708b142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4238262
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104913}

--- ash/webui/camera_app_ui/resources/js/error.ts
+++ ash/webui/camera_app_ui/resources/js/error.ts
@@ -82,7 +82,7 @@ export function reportError(
   triggeredErrorSet.add(hash);
 
   if (appWindow !== null) {
-    appWindow.reportError({
+    void appWindow.reportError({
       type: errorType,
       level,
       stack: stackStr,
