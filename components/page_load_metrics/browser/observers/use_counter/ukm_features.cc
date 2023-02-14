16e148fb1fca038cd0400c895b785a0c9eedd8a3
Mathias Bynens <mathias@chromium.org>
2023-02-14 17:47:34 +0000

    Add UseCounter for incompatible `pattern` attribute values
    A proposed change to the HTML pattern attribute [1] swaps the `u`
    RegExp flag for the new `v` flag [2], resulting in some potential
    incompatibility.
    
    Some previously valid patterns are now errors, specifically those
    with a character class including either an unescaped special
    character or a double punctuator:
    
        pattern="[(]"
        pattern="[)]"
        pattern="[[]"
        pattern="[{]"
        pattern="[}]"
        pattern="[/]"
        pattern="[-]"
        pattern="[|]"
        pattern="[&&]"
        pattern="[!!]"
        pattern="[##]"
        pattern="[$$]"
        pattern="[%%]"
        pattern="[**]"
        pattern="[++]"
        pattern="[,,]"
        pattern="[..]"
        pattern="[::]"
        pattern="[;;]"
        pattern="[<<]"
        pattern="[==]"
        pattern="[>>]"
        pattern="[??]"
        pattern="[@@]"
        pattern="[``]"
        pattern="[~~]"
        pattern="[_^^]"
    
    We don’t expect such patterns to be very common. This UseCounter
    aims to validate that assumption.
    
    Note that throwing patterns result in `inputElement.validity.valid
    === true` for any input value, so the only compatibility risk is
    that some value/pattern combinations that would previously result
    in `inputElement.validity.valid === false` now result in
    `inputElement.validity.valid === true`.
    
    [1]: https://github.com/whatwg/html/pull/7908
    [2]: https://v8.dev/features/regexp-v-flag
    
    Bug: chromium:1412729
    Change-Id: Ifa8bcc27dbf6e8a2a7098643dbb27a7633bb97de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4249120
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105129}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -276,6 +276,8 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kRequestFileSystem,
           WebFeature::kRequestFileSystemWorker,
           WebFeature::kRequestFileSystemSyncWorker,
+          WebFeature::
+              kHTMLPatternRegExpUnicodeSetIncompatibilitiesWithUnicodeMode,
       }));
   return *opt_in_features;
 }
