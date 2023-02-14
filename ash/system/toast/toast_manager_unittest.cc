acc829e882909d76a8639b60798b5228d29a9bcb
Kevin Radtke <kradtke@chromium.org>
2023-02-14 21:54:04 +0000

    Revert "An update on toasts"
    This reverts commit 81b1aab79b5136ff7f1d2b3ae1adae784dab4dee.
    
    Reason for revert: reverting to reland once the following edge cases are
    considered: lock screen, side shelf, overlap with sliders and other UI.
    
    Kept the removal of some code that was not meant to be landed on
    `ToastManagerImpl::OnRootWindowWillShutdown()`.
    
    More context here: https://groups.google.com/a/google.com/g/cros-status-area/c/VWUhdVLOIyg/m/sgZ_f2BNBAAJ
    
    Original change's description:
    > An update on toasts
    >
    > Anchor toasts on the right side, using the same offset as
    > notification popups, and other shelf bubbles.
    >
    > Update the window for toasts to ensure they layer on top of
    > notifications.
    >
    > After changing the window, it appears that the toast's window is null
    > during shutdown (if I explicitly stop tablet mode before shutdown, the window is not null). Remove the code which prevents deletion of toast overlays when root windows are shutting down. After talking to the code author it was revealed that this code was not meant to be landed.
    >
    > Screenshot: https://screenshot.googleplex.com/8jjTMHpyb3Vppye.png
    >
    > Bug: b:254869752, b:256685837
    > Change-Id: I01a4cbec5553ad3ec62cfcd98c6e1675d925f952
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997092
    > Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1081859}
    
    Bug: b:254869752, b:256685837
    Change-Id: Ib58a5293bb43afabc90f1cc43c8c89aca82e4549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4248478
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105299}

--- ash/system/toast/toast_manager_unittest.cc
+++ ash/system/toast/toast_manager_unittest.cc
@@ -281,7 +281,7 @@ TEST_F(ToastManagerImplTest, PositionWithVisibleBottomShelf) {
 
   EXPECT_TRUE(toast_bounds.Intersects(
       GetPrimaryWorkAreaInsets()->user_work_area_bounds()));
-  EXPECT_EQ(root_bounds.right(), toast_bounds.right() + ToastOverlay::kOffset);
+  EXPECT_NEAR(root_bounds.CenterPoint().x(), toast_bounds.CenterPoint().x(), 1);
 
   gfx::Rect shelf_bounds = shelf->GetIdealBounds();
   EXPECT_FALSE(toast_bounds.Intersects(shelf_bounds));
@@ -362,29 +362,6 @@ TEST_F(ToastManagerImplTest, PositionWithHotseatShownForMultipleMonitors) {
             toast_bounds.bottom());
 }
 
-// Tests that `ToastOverlay`'s are cleaned up properly on shutdown with hotseat
-// extended on multi-monitor
-TEST_F(ToastManagerImplTest, ShutdownWithExtendedHotseat) {
-  UpdateDisplay("600x400,600x400");
-  Shelf* const shelf =
-      Shell::GetRootWindowControllerWithDisplayId(GetSecondaryDisplay().id())
-          ->shelf();
-  EXPECT_EQ(ShelfAlignment::kBottom, shelf->alignment());
-  EXPECT_EQ(SHELF_VISIBLE, shelf->GetVisibilityState());
-
-  Shell::Get()->tablet_mode_controller()->SetEnabledForTest(true);
-  display_manager()->SetMirrorMode(display::MirrorMode::kOff, absl::nullopt);
-
-  std::unique_ptr<aura::Window> window(
-      CreateTestWindow(gfx::Rect(700, 100, 200, 200)));
-
-  GetPrimaryShelf()->hotseat_widget()->SetState(HotseatState::kExtended);
-
-  ShowToast("DUMMY", ToastData::kInfiniteDuration);
-
-  // Shutdown, there should be no crash.
-}
-
 TEST_F(ToastManagerImplTest, PositionWithHotseatExtendedOnSecondMonitor) {
   UpdateDisplay("600x400,600x400");
   Shelf* const shelf =
@@ -435,7 +412,7 @@ TEST_F(ToastManagerImplTest, PositionWithAutoHiddenBottomShelf) {
 
   EXPECT_TRUE(toast_bounds.Intersects(
       GetPrimaryWorkAreaInsets()->user_work_area_bounds()));
-  EXPECT_EQ(root_bounds.right(), toast_bounds.right() + ToastOverlay::kOffset);
+  EXPECT_NEAR(root_bounds.CenterPoint().x(), toast_bounds.CenterPoint().x(), 1);
   EXPECT_EQ(root_bounds.bottom() -
                 ShelfConfig::Get()->hidden_shelf_in_screen_portion() -
                 ToastOverlay::kOffset,
@@ -457,7 +434,7 @@ TEST_F(ToastManagerImplTest, PositionWithHiddenBottomShelf) {
 
   EXPECT_TRUE(toast_bounds.Intersects(
       GetPrimaryWorkAreaInsets()->user_work_area_bounds()));
-  EXPECT_EQ(root_bounds.right(), toast_bounds.right() + ToastOverlay::kOffset);
+  EXPECT_NEAR(root_bounds.CenterPoint().x(), toast_bounds.CenterPoint().x(), 1);
   EXPECT_EQ(root_bounds.bottom() - ToastOverlay::kOffset,
             toast_bounds.bottom());
 }
@@ -482,7 +459,9 @@ TEST_F(ToastManagerImplTest, PositionWithVisibleLeftShelf) {
 
   gfx::Rect shelf_bounds = shelf->GetIdealBounds();
   EXPECT_FALSE(toast_bounds.Intersects(shelf_bounds));
-  EXPECT_EQ(root_bounds.right(), toast_bounds.right() + ToastOverlay::kOffset);
+  EXPECT_NEAR(
+      shelf_bounds.right() + (root_bounds.width() - shelf_bounds.width()) / 2.0,
+      precise_toast_bounds.CenterPoint().x(), 1.f /* accepted error */);
 }
 
 TEST_F(ToastManagerImplTest, PositionWithUnifiedDesktop) {
@@ -503,7 +482,7 @@ TEST_F(ToastManagerImplTest, PositionWithUnifiedDesktop) {
   EXPECT_TRUE(toast_bounds.Intersects(
       GetPrimaryWorkAreaInsets()->user_work_area_bounds()));
   EXPECT_TRUE(root_bounds.Contains(toast_bounds));
-  EXPECT_EQ(root_bounds.right(), toast_bounds.right() + ToastOverlay::kOffset);
+  EXPECT_NEAR(root_bounds.CenterPoint().x(), toast_bounds.CenterPoint().x(), 1);
 
   gfx::Rect shelf_bounds = shelf->GetIdealBounds();
   EXPECT_FALSE(toast_bounds.Intersects(shelf_bounds));
