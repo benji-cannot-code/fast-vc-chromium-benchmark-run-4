4fc83d43c3dbbc6bea4c4e94d40e7f0e9977ff08
Steinar H. Gunderson <sesse@chromium.org>
2023-02-14 16:29:06 +0000

    Accelerate CSSTokenizer::ConsumeName() using SIMD.
    Use SSE2 or NEON to make ConsumeName() consider 16 bytes at a time
    instead of one by one. This also generally streamlines the function
    so that it doesn't have to distinguish the bogus zero bytes created by
    PeekWithoutReplacement() on EOF from real zero bytes.
    
    Style perftest (Zen 3, LTO but no PGO):
    
    Parse (µs)             Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce                1104      1079   +2.3%  [ +1.0%,  +3.5%]
    Encyclopedia             6009      5896   +1.9%  [ +1.0%,  +2.9%]
    Extension                1083      1072   +1.0%  [ -0.4%,  +2.6%]
    News                     6636      6441   +3.0%  [ +2.1%,  +4.0%]
    Search                   3999      3941   +1.5%  [ +0.5%,  +2.3%]
    Social1                 12037     11620   +3.6%  [ +2.6%,  +4.6%]
    Social2                   514       500   +2.7%  [ +1.5%,  +4.1%]
    Sports                  43124     42475   +1.5%  [ +0.6%,  +2.4%]
    Video                   28542     27593   +3.4%  [ +2.7%,  +4.3%]
    Geometric mean                            +2.3%  [ +1.8%,  +2.7%]
    
    M1 is similar:
    
    Parse (µs)             Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce                 889       862   +3.0%  [ +2.0%,  +3.9%]
    Encyclopedia             4675      4543   +2.9%  [ +2.7%,  +3.1%]
    Extension                 732       731   +0.2%  [ -0.2%,  +0.6%]
    News                     4809      4624   +4.0%  [ +3.6%,  +4.2%]
    Search                   2839      2771   +2.5%  [ +2.1%,  +2.9%]
    Social1                  8422      7979   +5.5%  [ +5.2%,  +5.8%]
    Social2                   373       360   +3.6%  [ +2.7%,  +4.5%]
    Sports                  30758     30072   +2.3%  [ +2.1%,  +2.4%]
    Video                   20434     19281   +6.0%  [ +5.8%,  +6.2%]
    Geometric mean                            +3.3%  [ +3.2%,  +3.5%]
    
    Change-Id: I0fb0fbc9bbb9e7bcc352c61c9ead0ae926657e19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4249222
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105087}

--- third_party/blink/renderer/core/css/parser/css_tokenizer_input_stream.h
+++ third_party/blink/renderer/core/css/parser/css_tokenizer_input_stream.h
@@ -42,6 +42,9 @@ class CSSTokenizerInputStream {
     }
     return (*string_)[offset_ + lookahead_offset];
   }
+  StringView Peek() const {
+    return StringView(*string_, offset_, length() - offset_);
+  }
 
   void Advance(unsigned offset = 1) { offset_ += offset; }
   void PushBack(UChar cc) {
