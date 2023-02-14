aa5d0fe5282e8def2497d6eb29dcbd9ce59a9212
Tom Sepez <tsepez@chromium.org>
2023-02-14 23:00:58 +0000

    Convert raw_ptr to unique_ptr in InputMethodAuraLinuxTest.
    Avoid invoking bare `delete` on raw_ptr, which will lead to dangling
    ptr warnings otherwise.
    
    -- initialize members in declaration.
    -- re-order members to match order of init in SetUp().
    
    Bug: 1401495
    Change-Id: Ic130be29175e223660c4528c0cea33c7423d572c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250883
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105335}

--- ui/base/ime/linux/input_method_auralinux_unittest.cc
+++ ui/base/ime/linux/input_method_auralinux_unittest.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <memory>
+
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
@@ -317,10 +319,7 @@ class InputMethodAuraLinuxTest : public testing::Test {
   InputMethodAuraLinuxTest& operator=(const InputMethodAuraLinuxTest&) = delete;
 
  protected:
-  InputMethodAuraLinuxTest()
-      : input_method_auralinux_(nullptr),
-        delegate_(nullptr),
-        context_(nullptr) {
+  InputMethodAuraLinuxTest() {
     GetInputMethodContextFactoryForTest() =
         base::BindRepeating([](LinuxInputMethodContextDelegate* delegate)
                                 -> std::unique_ptr<LinuxInputMethodContext> {
@@ -334,8 +333,9 @@ class InputMethodAuraLinuxTest : public testing::Test {
   }
 
   void SetUp() override {
-    delegate_ = new InputMethodDelegateForTesting();
-    input_method_auralinux_ = new InputMethodAuraLinux(delegate_);
+    delegate_ = std::make_unique<InputMethodDelegateForTesting>();
+    input_method_auralinux_ =
+        std::make_unique<InputMethodAuraLinux>(delegate_.get());
     input_method_auralinux_->OnFocus();
     context_ = static_cast<LinuxInputMethodContextForTesting*>(
         input_method_auralinux_->GetContextForTesting());
@@ -345,16 +345,13 @@ class InputMethodAuraLinuxTest : public testing::Test {
     context_->SetSyncMode(false);
     context_->SetEatKey(false);
     context_ = nullptr;
-
-    delete input_method_auralinux_;
-    input_method_auralinux_ = nullptr;
-    delete delegate_;
-    delegate_ = nullptr;
+    input_method_auralinux_.reset();
+    delegate_.reset();
   }
 
-  raw_ptr<InputMethodAuraLinux> input_method_auralinux_;
-  raw_ptr<InputMethodDelegateForTesting> delegate_;
-  raw_ptr<LinuxInputMethodContextForTesting> context_;
+  std::unique_ptr<InputMethodDelegateForTesting> delegate_;
+  std::unique_ptr<InputMethodAuraLinux> input_method_auralinux_;
+  raw_ptr<LinuxInputMethodContextForTesting> context_ = nullptr;
   raw_ptr<TestResult> test_result_;
 };
 
@@ -699,12 +696,12 @@ void DeadKeyTest(TextInputType text_input_type,
 }
 
 TEST_F(InputMethodAuraLinuxTest, DeadKeyTest) {
-  DeadKeyTest(TEXT_INPUT_TYPE_TEXT, input_method_auralinux_, context_,
+  DeadKeyTest(TEXT_INPUT_TYPE_TEXT, input_method_auralinux_.get(), context_,
               test_result_);
 }
 
 TEST_F(InputMethodAuraLinuxTest, DeadKeyTestTypeNone) {
-  DeadKeyTest(TEXT_INPUT_TYPE_NONE, input_method_auralinux_, context_,
+  DeadKeyTest(TEXT_INPUT_TYPE_NONE, input_method_auralinux_.get(), context_,
               test_result_);
 }
 
