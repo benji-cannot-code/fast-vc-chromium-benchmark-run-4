759da11aba292e787603b8b5184bb9744ff53c20
Marc Treib <treib@chromium.org>
2023-02-14 16:56:34 +0000

    PrefModelAssociator: Use PrefStore directly instead of PrefService
    PrefModelAssociator is the "bridge" between Sync and the native
    Preferences model. It synchronizes user pref values (as opposed to
    policy-controlled values, default values, etc) between this Chrome
    instance and the Sync server.
    
    Before this CL, PrefModelAssociator was hooked up to PrefService.
    After this CL, it's instead hooked up directly to the user-controlled
    PrefStore.
    
    This is simpler/more natural in some ways:
    Only user prefs should be synced, so hooking up directly to those
    makes sense. E.g. PrefModelAssociator now no longer needs to filter
    out pref changes if the pref has a non-user-controlled value.
    
    There is one behavior change, when the user-controlled value of a pref
    is changed while a "higher-level" pref value also exists (e.g. policy-
    or extension-controlled):
    Before this CL, the pref change would be ignored.
    After this CL, the user pref value will still get sent to the Sync
    server.
    This should be a very rare situation and not have any notable impact.
    
    Bug: 1404937
    Change-Id: Ia9e72effb14b52dd470007bed902441bf6f0aeb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219172
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105099}

--- components/sync_preferences/pref_service_syncable.cc
+++ components/sync_preferences/pref_service_syncable.cc
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sync_preferences {
 
-// TODO(tschumann): Handing out pointers to this in the constructor is an
-// anti-pattern. Instead, introduce a factory method which first constructs
-// the PrefServiceSyncable instance and then the members which need a reference
-// to the PrefServiceSycnable instance.
 PrefServiceSyncable::PrefServiceSyncable(
     std::unique_ptr<PrefNotifierImpl> pref_notifier,
     std::unique_ptr<PrefValueStore> pref_value_store,
@@ -52,13 +48,18 @@ PrefServiceSyncable::PrefServiceSyncable(
                   std::move(read_error_callback),
                   async),
       pref_service_forked_(false),
-      pref_sync_associator_(pref_model_associator_client, syncer::PREFERENCES),
+      pref_sync_associator_(pref_model_associator_client,
+                            user_prefs,
+                            syncer::PREFERENCES),
       priority_pref_sync_associator_(pref_model_associator_client,
+                                     user_prefs,
                                      syncer::PRIORITY_PREFERENCES),
 #if BUILDFLAG(IS_CHROMEOS_ASH)
       os_pref_sync_associator_(pref_model_associator_client,
+                               user_prefs,
                                syncer::OS_PREFERENCES),
       os_priority_pref_sync_associator_(pref_model_associator_client,
+                                        user_prefs,
                                         syncer::OS_PRIORITY_PREFERENCES),
 #endif
       pref_registry_(std::move(pref_registry)) {
@@ -68,10 +69,6 @@ PrefServiceSyncable::PrefServiceSyncable(
   os_pref_sync_associator_.SetPrefService(this);
   os_priority_pref_sync_associator_.SetPrefService(this);
 #endif
-
-  // Let PrefModelAssociators know about changes to preference values.
-  pref_value_store_->set_callback(base::BindRepeating(
-      &PrefServiceSyncable::ProcessPrefChange, base::Unretained(this)));
 
   // Add already-registered syncable preferences to PrefModelAssociator.
   for (const auto& [path, value] : *pref_registry_) {
@@ -230,19 +227,17 @@ void PrefServiceSyncable::AddRegisteredSyncablePreference(
 #endif
 }
 
+base::Value::Type PrefServiceSyncable::GetRegisteredPrefType(
+    const std::string& pref_name) const {
+  const Preference* pref = FindPreference(pref_name);
+  DCHECK(pref);
+  return pref->GetType();
+}
+
 void PrefServiceSyncable::OnIsSyncingChanged() {
   for (auto& observer : observer_list_) {
     observer.OnIsSyncingChanged();
   }
-}
-
-void PrefServiceSyncable::ProcessPrefChange(const std::string& name) {
-  pref_sync_associator_.ProcessPrefChange(name);
-  priority_pref_sync_associator_.ProcessPrefChange(name);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  os_pref_sync_associator_.ProcessPrefChange(name);
-  os_priority_pref_sync_associator_.ProcessPrefChange(name);
-#endif
 }
 
 }  // namespace sync_preferences
