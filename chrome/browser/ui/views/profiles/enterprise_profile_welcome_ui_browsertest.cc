56636305849fd03b904ec5a8f9a0adde231b3793
Jack Yammine <jyammine@google.com>
2023-02-14 10:37:30 +0000

    [fyfre]Fix dialog pixel tests being flaky on Linux.
    Fixed: 1414803
    Change-Id: I9af9198f4ef0d4d262de19f1eaf3b23668a43810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4239583
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104975}

--- chrome/browser/ui/views/profiles/enterprise_profile_welcome_ui_browsertest.cc
+++ chrome/browser/ui/views/profiles/enterprise_profile_welcome_ui_browsertest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
+#include "ui/views/widget/any_widget_observer.h"
 
 #if !BUILDFLAG(ENABLE_DICE_SUPPORT)
 #error Platform not supported
@@ -259,12 +260,21 @@ class EnterpriseWelcomeUIDialogPixelTest
     content::TestNavigationObserver observer(url);
     observer.StartWatchingNewWebContents();
 
+    // ShowUi() can sometimes return before the dialog widget is shown because
+    // the call to show the latter is asynchronous. Adding
+    // NamedWidgetShownWaiter will prevent that from happening.
+    views::NamedWidgetShownWaiter widget_waiter(
+        views::test::AnyWidgetTestPasskey{},
+        "SigninViewControllerDelegateViews");
+
     auto* controller = browser()->signin_view_controller();
     controller->ShowModalEnterpriseConfirmationDialog(
         account_info, GetParam().profile_creation_required_by_policy,
         GetParam().show_link_data_checkbox,
         GetDefaultProfileThemeColors().profile_highlight_color,
         base::DoNothing());
+
+    widget_waiter.WaitIfNeededAndGet();
     observer.Wait();
   }
 
