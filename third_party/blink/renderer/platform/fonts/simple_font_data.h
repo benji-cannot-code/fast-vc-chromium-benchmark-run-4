9d85eb6c066012cd2057ae7ca26ccbbf33b9fcb2
Dave Tapuska <dtapuska@chromium.org>
2023-02-14 17:27:14 +0000

    Make blink_platform_unittests work by specifying bundle data
    Activate IS_MAC code paths for iOS by changing them to IS_APPLE.
    
    Tests are running with only 3 tests failing which will be fixed
    separately.
    
    Bug: 1411461
    Change-Id: Ide392178ca42349070b726f45fc3bf662304a0d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4249757
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105116}

--- third_party/blink/renderer/platform/fonts/simple_font_data.h
+++ third_party/blink/renderer/platform/fonts/simple_font_data.h
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkFont.h"
 #include "ui/gfx/geometry/rect_f.h"
 
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
 #include "third_party/blink/renderer/platform/fonts/glyph_metrics_map.h"
 #endif
 
@@ -225,14 +225,14 @@ class PLATFORM_EXPORT SimpleFontData final : public FontData {
 // https://bugs.chromium.org/p/skia/issues/detail?id=5328 :
 // On Mac we're still using path based glyph metrics, and they seem to be
 // too slow to be able to remove the caching layer we have here.
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
   mutable std::unique_ptr<GlyphMetricsMap<gfx::RectF>> glyph_to_bounds_map_;
   mutable GlyphMetricsMap<float> glyph_to_width_map_;
 #endif
 };
 
 ALWAYS_INLINE gfx::RectF SimpleFontData::BoundsForGlyph(Glyph glyph) const {
-#if !BUILDFLAG(IS_MAC)
+#if !BUILDFLAG(IS_APPLE)
   return PlatformBoundsForGlyph(glyph);
 #else
   if (glyph_to_bounds_map_) {
@@ -252,7 +252,7 @@ ALWAYS_INLINE gfx::RectF SimpleFontData::BoundsForGlyph(Glyph glyph) const {
 }
 
 ALWAYS_INLINE float SimpleFontData::WidthForGlyph(Glyph glyph) const {
-#if !BUILDFLAG(IS_MAC)
+#if !BUILDFLAG(IS_APPLE)
   return PlatformWidthForGlyph(glyph);
 #else
   if (absl::optional<float> width = glyph_to_width_map_.MetricsForGlyph(glyph))
