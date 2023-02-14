72955a78fb1972f495fc2c7986f900db285ec118
Florian Leimgruber <fleimgruber@google.com>
2023-02-14 16:35:13 +0000

    Command line flag to import profiles for testing.
    Implements the proposal linked in the bug.
    
    All logic is contained in a new manual_testing_profile_import file.
    - A command line flag is used to enable the feature.
    - Reading and parsing is done in a separate thread.
    - If reading the file or parsing fails, CHECKs fail, which crash the
      browser ungracefully. This is intended, to prevent manual testers from
      testing with invalid data.
    - A function AutofillProfilesFromJSON() is created that converts the
      JSON representation described in the doc to AutofillProfiles.
      If parsing fails, nullopt is returned (rather than a failed CHECK).
      This is done to make the function reusable for the DevTools project.
    - Basic unit tests are implemented.
    
    Fixed: 1413177
    Change-Id: I4bafc29b99762437fa7df5c0cb1239de75f1aca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224332
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105090}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/geo/country_data.h"
 #include "components/autofill/core/browser/geo/country_names.h"
 #include "components/autofill/core/browser/geo/phone_number_i18n.h"
+#include "components/autofill/core/browser/manual_testing_profile_import.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/iban_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/offers_metrics.h"
@@ -347,6 +348,11 @@ void PersonalDataManager::Init(
 
   personal_data_manager_cleaner_ = std::make_unique<PersonalDataManagerCleaner>(
       this, alternative_state_name_map_updater_.get(), pref_service);
+
+  // Potentially import profiles for testing. `Init()` is called whenever the
+  // corresponding Chrome profile is created. This is either during start-up or
+  // when the Chrome profile is changed (including incognito mode).
+  MaybeImportProfilesForManualTesting(weak_factory_.GetWeakPtr());
 }
 
 PersonalDataManager::~PersonalDataManager() {
