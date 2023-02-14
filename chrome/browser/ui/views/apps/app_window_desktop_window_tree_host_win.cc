36d4b3083e1292fa69460c2ff1ad864ce3d0c42c
Daniel Libby <dlibby@microsoft.com>
2023-02-14 23:51:48 +0000

    Remove unused Windows includes
    These includes aren't used and can be removed. Found when
    auditing uxtheme.dll usage.
    
    Change-Id: Ie581ed109eb0625ee4a2e4f6c93c94cd88ac37a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237443
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1105365}

--- chrome/browser/ui/views/apps/app_window_desktop_window_tree_host_win.cc
+++ chrome/browser/ui/views/apps/app_window_desktop_window_tree_host_win.cc
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
-#include <dwmapi.h>
-#include <uxtheme.h>
-
 #include "base/win/windows_version.h"
 #include "chrome/browser/ui/views/apps/chrome_native_app_window_views_win.h"
 #include "chrome/browser/ui/views/apps/glass_app_window_frame_view_win.h"
--- ui/views/controls/menu/menu_config_win.cc
+++ ui/views/controls/menu/menu_config_win.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>  // Must come before other Windows system headers.
 
 #include <Vssym32.h>
-#include <uxtheme.h>
 
 #include "base/check.h"
 #include "base/feature_list.h"
