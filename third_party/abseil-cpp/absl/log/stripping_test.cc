f5b4ffbd29c447263b882ec4d7810468d3735216
Mirko Bonadei <mbonadei@chromium.org>
2023-02-14 08:59:52 +0000

    Roll abseil_revision dcaed1a05a..d6a45e95ab
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/dcaed1a05a..d6a45e95ab
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/dcaed1a05a..d6a45e95ab
    
    Bug: 1414573
    Change-Id: I677fb595e72833bc61bfaeb10910e6bff15fccb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4247357
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Auto-Submit: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104948}

--- third_party/abseil-cpp/absl/log/stripping_test.cc
+++ third_party/abseil-cpp/absl/log/stripping_test.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "absl/base/internal/strerror.h"
+#include "absl/base/log_severity.h"
 #include "absl/flags/internal/program_name.h"
 #include "absl/log/check.h"
 #include "absl/log/internal/test_helpers.h"
@@ -57,6 +58,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "absl/strings/escaping.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
+
+// Set a flag that controls whether we actually execute fatal statements, but
+// prevent the compiler from optimizing it out.
+static volatile bool kReallyDie = false;
 
 namespace {
 using ::testing::_;
@@ -305,7 +310,10 @@ TEST_F(StrippingTest, Fatal) {
   // as would happen if we used a literal.  We might (or might not) leave it
   // lying around later; that's what the tests are for!
   const std::string needle = absl::Base64Escape("StrippingTest.Fatal");
-  EXPECT_DEATH_IF_SUPPORTED(LOG(FATAL) << "U3RyaXBwaW5nVGVzdC5GYXRhbA==", "");
+  // We don't care if the LOG statement is actually executed, we're just
+  // checking that it's stripped.
+  if (kReallyDie) LOG(FATAL) << "U3RyaXBwaW5nVGVzdC5GYXRhbA==";
+
   std::unique_ptr<FILE, std::function<void(FILE*)>> exe = OpenTestExecutable();
   ASSERT_THAT(exe, NotNull());
   if (absl::LogSeverity::kFatal >= kAbslMinLogLevel) {
