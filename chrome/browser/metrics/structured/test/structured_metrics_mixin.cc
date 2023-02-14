d805ccc38b80e54f3016137134d2b5925b5906f9
Helen Huang <helenhuang@chromium.org>
2023-02-14 06:09:30 +0000

    [CrOSEvents] Add integration test support for CrOS events in chromium
    Bug: 268357434
    Change-Id: I5a84212d1f154f4817b3453c793ed05b373a5668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4233481
    Commit-Queue: Helen Huang <helenhuang@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104912}

---
+++ chrome/browser/metrics/structured/test/structured_metrics_mixin.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/metrics/structured/test/structured_metrics_mixin.h"
+#include "components/metrics/metrics_switches.h"
+
+namespace metrics::structured {
+
+StructuredMetricsMixin::StructuredMetricsMixin(
+    InProcessBrowserTestMixinHost* host)
+    : InProcessBrowserTestMixin(host) {
+  test_structured_metrics_provider_ =
+      std::make_unique<TestStructuredMetricsProvider>();
+}
+
+StructuredMetricsMixin::~StructuredMetricsMixin() = default;
+
+void StructuredMetricsMixin::SetUpCommandLine(base::CommandLine* command_line) {
+  EnableMetricsRecordingOnlyForTesting(command_line);
+}
+
+TestStructuredMetricsProvider*
+StructuredMetricsMixin::GetTestStructuredMetricsProvider() {
+  return test_structured_metrics_provider_.get();
+}
+
+}  // namespace metrics::structured
