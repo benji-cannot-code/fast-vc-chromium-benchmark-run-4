46db8c714b04a581010184af4188ce1b2e037094
Maksim Sisov <msisov@igalia.com>
2023-02-14 17:13:38 +0000

    [ios]: fix dcheck in GPUTestConfig
    GPUTestConfig knows about kOsIOS, but ::set_os DCHECKS in
    gpu_unittests.
    
    Also fixes "GPUTestBotConfig::IsValid()"
    
    Bug: 1411704, 1413716
    Change-Id: I6b1271944f4ae6663087354e6fabd892ba61ee0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234262
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105107}

--- gpu/config/gpu_test_config.cc
+++ gpu/config/gpu_test_config.cc
@@ -108,7 +108,7 @@ GPUTestConfig::~GPUTestConfig() = default;
 
 void GPUTestConfig::set_os(int32_t os) {
   DCHECK_EQ(0, os & ~(kOsAndroid | kOsWin | kOsMac | kOsLinux | kOsChromeOS |
-                      kOsFuchsia));
+                      kOsFuchsia | kOsIOS));
   os_ = os;
 }
 
@@ -228,6 +228,7 @@ bool GPUTestBotConfig::IsValid() const {
     case kOsChromeOS:
     case kOsAndroid:
     case kOsFuchsia:
+    case kOsIOS:
       break;
     default:
       return false;
