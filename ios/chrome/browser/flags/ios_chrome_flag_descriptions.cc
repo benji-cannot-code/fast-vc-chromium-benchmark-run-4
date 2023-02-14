d492b51128df34264f79ceb9707b8d1ef05240f8
Daniel White <danieltwhite@google.com>
2023-02-14 14:19:55 +0000

    [iOS] Adding OptimizationGuide Flag to chrome://flags
    Previously, users were not being redirected to a product's webpage when
    tapping on a price tracking push notification. After investigation, it
    was discovered that there was an OptimizationGuide flag that controlled
    whether the OptimizationGuide's push notification client was added to
    the PushNotificationClientManager effectively controlling whether or not
    tapping on a push notification for price tracking was supported.
    
    In this CL, the OptimizationGuide's flag is added to chrome://flags to
    allow users to enable it for their devices.
    
    Fixed: 1415496
    Change-Id: I3da221744d51fffaa5d99081b19645499106e8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4246878
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105036}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
@@ -688,6 +688,12 @@ const char kOptimizationGuideDebugLogsDescription[] =
     "Enables the optimization guide to log and save debug messages that can be "
     "shown in the internals page.";
 
+const char kOptimizationGuidePushNotificationClientName[] =
+    "Enable optimization guide push notification client";
+const char kOptimizationGuidePushNotificationClientDescription[] =
+    "Enables the client that handles incoming push notifications on behalf of "
+    "the optimization guide.";
+
 const char kOptimizationGuideModelDownloadingName[] =
     "Allow optimization guide model downloads";
 const char kOptimizationGuideModelDownloadingDescription[] =
