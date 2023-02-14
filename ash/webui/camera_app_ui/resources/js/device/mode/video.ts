bab2b54e99225be2776ee78dea4a1af3137f85ec
Pi-Hsun Shih <pihsun@chromium.org>
2023-02-14 14:53:13 +0000

    CCA: Fix beforeunload listener not properly cleaned.
    The beforeunload used to prevent user accidentally closing CCA is not
    cleaned properly in the same scenario when b:263560417 is triggered.
    
    Bug: b:263560417
    Test: manually
    
    Change-Id: Icab6a1d819f0c16bec9b0ae8dcb14c8719a71ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4249057
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105047}

--- ash/webui/camera_app_ui/resources/js/device/mode/video.ts
+++ ash/webui/camera_app_ui/resources/js/device/mode/video.ts
@@ -712,6 +712,7 @@ export class Video extends ModeBase {
 
           if (this.stopped) {
             this.mediaRecorder.stop();
+            window.removeEventListener('beforeunload', beforeUnloadListener);
             return;
           }
           state.set(state.State.RECORDING, true);
