bd4b9deac0172b9ca7b6b39d91379ad04cbd79f3
YuhanYang <yyhyyh@google.com>
2023-02-14 21:54:25 +0000

    Settings Split: Display correct meta key label
    Screenshot:
    metaKey - Command:
    https://screenshot.googleplex.com/44yyaHYmZcaLbB6
    metaKey - Search:
    https://screenshot.googleplex.com/LR56nsxfqsmhHpf
    
    Test: browser_tests OSSettingsPerDeviceKeyboardSubsectionV3Test
    Bug: b/241965700
    Change-Id: Ie961f5bcaecefb984525c16ff834ba9e25805a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250377
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105300}

--- chrome/browser/resources/settings/chromeos/device_page/per_device_keyboard_remap_keys.ts
+++ chrome/browser/resources/settings/chromeos/device_page/per_device_keyboard_remap_keys.ts
@@ -25,7 +25,7 @@ import {RouteObserverMixin, RouteObserverMixinInterface} from '../route_observer
 import {Route, Router} from '../router.js';
 
 import {getInputDeviceSettingsProvider} from './input_device_mojo_interface_provider.js';
-import {InputDeviceSettingsProviderInterface, Keyboard, ModifierKey} from './input_device_settings_types.js';
+import {InputDeviceSettingsProviderInterface, Keyboard, MetaKey, ModifierKey} from './input_device_settings_types.js';
 import {getTemplate} from './per_device_keyboard_remap_keys.html.js';
 
 const SettingsPerDeviceKeyboardRemapKeysElementBase =
@@ -138,6 +138,10 @@ export class SettingsPerDeviceKeyboardRemapKeysElement extends
 
       /** Menu items for key mapping. */
       keyMapTargets: Object,
+
+      metaKeyLabel: {
+        type: String,
+      },
     };
   }
 
@@ -166,12 +170,7 @@ export class SettingsPerDeviceKeyboardRemapKeysElement extends
   private fakeMetaPref: chrome.settingsPrivate.PrefObject;
   private hasAssistantKey: boolean;
   private hasCapsLockKey: boolean;
-
-  override ready() {
-    super.ready();
-
-    this.setUpKeyMapTargets_();
-  }
+  private metaKeyLabel: string;
 
   override currentRouteChanged(route: Route): void {
     // Does not apply to this page.
@@ -203,6 +202,10 @@ export class SettingsPerDeviceKeyboardRemapKeysElement extends
     this.hasCapsLockKey =
         searchedKeyboard.modifierKeys.includes(ModifierKey.CAPS_LOCK);
 
+    // Get MetaKey label from keyboard settings.
+    this.metaKeyLabel = this.getMetaKeyLabel();
+    this.setUpKeyMapTargets();
+
     // Update Prefs according to keyboard modifierRemappings.
     Array.from(this.keyboard.settings.modifierRemappings.keys())
         .forEach(originalKey => {
@@ -213,12 +216,12 @@ export class SettingsPerDeviceKeyboardRemapKeysElement extends
   /**
    * Initializes the dropdown menu options for remapping keys.
    */
-  private setUpKeyMapTargets_() {
+  private setUpKeyMapTargets() {
     // Ordering is according to UX, but values match ModifierKey.
     this.keyMapTargets = [
       {
         value: ModifierKey.META,
-        name: 'Meta',
+        name: this.metaKeyLabel,
       },
       {
         value: ModifierKey.CONTROL,
@@ -299,6 +302,23 @@ export class SettingsPerDeviceKeyboardRemapKeysElement extends
   private onSettingsChanged() {
     // TODO(yyhyyh@): Call update keyboard settings API when user changes
     // settings value.
+  }
+
+  private getMetaKeyLabel(): string {
+    switch (this.keyboard.metaKey) {
+      case MetaKey.COMMAND: {
+        return this.i18n('keyboardKeyCommand');
+      }
+      case MetaKey.EXTERNAL_META: {
+        return this.i18n('keyboardKeyExternalMeta');
+      }
+      case MetaKey.LAUNCHER: {
+        return this.i18n('keyboardKeyLauncher');
+      }
+      case MetaKey.SEARCH: {
+        return this.i18n('keyboardKeySearch');
+      }
+    }
   }
 }
 
