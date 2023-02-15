94ff0e580acd1af7aa6788bd888ce7ca1dcc2557
Andrew Rayskiy <greengrape@google.com>
2023-02-15 13:36:34 +0000

    [DirectSockets] Enable network.mojom.DnsQueryType in blink
    This CL extracts host_resolver.mojom into its own component and exposes
    network.mojom.DnsQueryType so that it can be used in blink.
    
    Change-Id: Iede688179865c00154e0e7d7eb9fe9519ba372e8
    Bug: 1413211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224633
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105618}

--- services/network/public/cpp/host_resolver_mojom_traits.cc
+++ services/network/public/cpp/host_resolver_mojom_traits.cc
@@ -17,34 +17,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/ip_address_mojom_traits.h"
 #include "services/network/public/cpp/ip_endpoint_mojom_traits.h"
 #include "services/network/public/mojom/host_resolver.mojom-shared.h"
-#include "services/network/public/mojom/host_resolver.mojom.h"
 
 namespace mojo {
 
-using network::mojom::DnsConfigOverrides;
+using Tristate = network::mojom::DnsConfigOverrides_Tristate;
+using HostResolverSource = network::mojom::ResolveHostParameters_Source;
+using MdnsListenerUpdateType = network::mojom::MdnsListenClient_UpdateType;
+
 using network::mojom::DnsConfigOverridesDataView;
 using network::mojom::DnsQueryType;
-using network::mojom::MdnsListenClient;
 using network::mojom::OptionalSecureDnsMode;
-using network::mojom::ResolveHostParameters;
 
 namespace {
 
-DnsConfigOverrides::Tristate ToTristate(absl::optional<bool> optional) {
+Tristate ToTristate(absl::optional<bool> optional) {
   if (!optional)
-    return DnsConfigOverrides::Tristate::NO_OVERRIDE;
+    return Tristate::NO_OVERRIDE;
   if (optional.value())
-    return DnsConfigOverrides::Tristate::TRISTATE_TRUE;
-  return DnsConfigOverrides::Tristate::TRISTATE_FALSE;
+    return Tristate::TRISTATE_TRUE;
+  return Tristate::TRISTATE_FALSE;
 }
 
-absl::optional<bool> FromTristate(DnsConfigOverrides::Tristate tristate) {
+absl::optional<bool> FromTristate(Tristate tristate) {
   switch (tristate) {
-    case DnsConfigOverrides::Tristate::NO_OVERRIDE:
+    case Tristate::NO_OVERRIDE:
       return absl::nullopt;
-    case DnsConfigOverrides::Tristate::TRISTATE_TRUE:
+    case Tristate::TRISTATE_TRUE:
       return true;
-    case DnsConfigOverrides::Tristate::TRISTATE_FALSE:
+    case Tristate::TRISTATE_FALSE:
       return false;
   }
 }
@@ -63,8 +63,6 @@ OptionalSecureDnsMode ToOptionalSecureDnsMode(
   }
 }
 
-}  // namespace
-
 absl::optional<net::SecureDnsMode> FromOptionalSecureDnsMode(
     OptionalSecureDnsMode mode) {
   switch (mode) {
@@ -78,6 +76,8 @@ absl::optional<net::SecureDnsMode> FromOptionalSecureDnsMode(
       return net::SecureDnsMode::kSecure;
   }
 }
+
+}  // namespace
 
 // static
 bool StructTraits<network::mojom::DnsOverHttpsServerConfigDataView,
@@ -111,22 +111,20 @@ bool StructTraits<network::mojom::DnsOverHttpsConfigDataView,
 }
 
 // static
-DnsConfigOverrides::Tristate
-StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::
+Tristate StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::
     append_to_multi_label_name(const net::DnsConfigOverrides& overrides) {
   return ToTristate(overrides.append_to_multi_label_name);
 }
 
 // static
-DnsConfigOverrides::Tristate
+Tristate
 StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::rotate(
     const net::DnsConfigOverrides& overrides) {
   return ToTristate(overrides.rotate);
 }
 
 // static
-DnsConfigOverrides::Tristate
-StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::
+Tristate StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::
     use_local_ipv6(const net::DnsConfigOverrides& overrides) {
   return ToTristate(overrides.use_local_ipv6);
 }
@@ -139,8 +137,7 @@ StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::
 }
 
 // static
-DnsConfigOverrides::Tristate
-StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::
+Tristate StructTraits<DnsConfigOverridesDataView, net::DnsConfigOverrides>::
     allow_dns_over_https_upgrade(const net::DnsConfigOverrides& overrides) {
   return ToTristate(overrides.allow_dns_over_https_upgrade);
 }
@@ -239,72 +236,72 @@ bool EnumTraits<DnsQueryType, net::DnsQueryType>::FromMojom(
 }
 
 // static
-ResolveHostParameters::Source
-EnumTraits<ResolveHostParameters::Source, net::HostResolverSource>::ToMojom(
+HostResolverSource
+EnumTraits<HostResolverSource, net::HostResolverSource>::ToMojom(
     net::HostResolverSource input) {
   switch (input) {
     case net::HostResolverSource::ANY:
-      return ResolveHostParameters::Source::ANY;
+      return HostResolverSource::ANY;
     case net::HostResolverSource::SYSTEM:
-      return ResolveHostParameters::Source::SYSTEM;
+      return HostResolverSource::SYSTEM;
     case net::HostResolverSource::DNS:
-      return ResolveHostParameters::Source::DNS;
+      return HostResolverSource::DNS;
     case net::HostResolverSource::MULTICAST_DNS:
-      return ResolveHostParameters::Source::MULTICAST_DNS;
+      return HostResolverSource::MULTICAST_DNS;
     case net::HostResolverSource::LOCAL_ONLY:
-      return ResolveHostParameters::Source::LOCAL_ONLY;
+      return HostResolverSource::LOCAL_ONLY;
   }
 }
 
 // static
-bool EnumTraits<ResolveHostParameters::Source, net::HostResolverSource>::
-    FromMojom(ResolveHostParameters::Source input,
-              net::HostResolverSource* output) {
+bool EnumTraits<HostResolverSource, net::HostResolverSource>::FromMojom(
+    HostResolverSource input,
+    net::HostResolverSource* output) {
   switch (input) {
-    case ResolveHostParameters::Source::ANY:
+    case HostResolverSource::ANY:
       *output = net::HostResolverSource::ANY;
       return true;
-    case ResolveHostParameters::Source::SYSTEM:
+    case HostResolverSource::SYSTEM:
       *output = net::HostResolverSource::SYSTEM;
       return true;
-    case ResolveHostParameters::Source::DNS:
+    case HostResolverSource::DNS:
       *output = net::HostResolverSource::DNS;
       return true;
-    case ResolveHostParameters::Source::MULTICAST_DNS:
+    case HostResolverSource::MULTICAST_DNS:
       *output = net::HostResolverSource::MULTICAST_DNS;
       return true;
-    case ResolveHostParameters::Source::LOCAL_ONLY:
+    case HostResolverSource::LOCAL_ONLY:
       *output = net::HostResolverSource::LOCAL_ONLY;
       return true;
   }
 }
 
 // static
-MdnsListenClient::UpdateType
-EnumTraits<MdnsListenClient::UpdateType, net::MdnsListenerUpdateType>::ToMojom(
+MdnsListenerUpdateType
+EnumTraits<MdnsListenerUpdateType, net::MdnsListenerUpdateType>::ToMojom(
     net::MdnsListenerUpdateType input) {
   switch (input) {
     case net::MdnsListenerUpdateType::kAdded:
-      return MdnsListenClient::UpdateType::ADDED;
+      return MdnsListenerUpdateType::ADDED;
     case net::MdnsListenerUpdateType::kChanged:
-      return MdnsListenClient::UpdateType::CHANGED;
+      return MdnsListenerUpdateType::CHANGED;
     case net::MdnsListenerUpdateType::kRemoved:
-      return MdnsListenClient::UpdateType::REMOVED;
+      return MdnsListenerUpdateType::REMOVED;
   }
 }
 
 // static
-bool EnumTraits<MdnsListenClient::UpdateType, net::MdnsListenerUpdateType>::
-    FromMojom(MdnsListenClient::UpdateType input,
-              net::MdnsListenerUpdateType* output) {
+bool EnumTraits<MdnsListenerUpdateType, net::MdnsListenerUpdateType>::FromMojom(
+    MdnsListenerUpdateType input,
+    net::MdnsListenerUpdateType* output) {
   switch (input) {
-    case MdnsListenClient::UpdateType::ADDED:
+    case MdnsListenerUpdateType::ADDED:
       *output = net::MdnsListenerUpdateType::kAdded;
       return true;
-    case MdnsListenClient::UpdateType::CHANGED:
+    case MdnsListenerUpdateType::CHANGED:
       *output = net::MdnsListenerUpdateType::kChanged;
       return true;
-    case MdnsListenClient::UpdateType::REMOVED:
+    case MdnsListenerUpdateType::REMOVED:
       *output = net::MdnsListenerUpdateType::kRemoved;
       return true;
   }
