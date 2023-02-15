356cbf3a7f8ef81512cec516c623f3c5fafd93d5
Tom Sepez <tsepez@chromium.org>
2023-02-15 18:47:56 +0000

    Avoid dangling ptr warnings in MessagePumpLibeventTest
    Re-order so that the delegate outlives the pump and controller and
    remove unused cyclical pointer in QuitWatcher.
    
    Bug: 1401495
    Change-Id: Id3b9014e5ccbd2d9892ea899abcc294dfb0135a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4228043
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105764}

--- base/message_loop/message_pump_libevent_unittest.cc
+++ base/message_loop/message_pump_libevent_unittest.cc
@@ -156,9 +156,9 @@ class DeleteWatcher : public BaseWatcher {
 };
 
 TEST_P(MessagePumpLibeventTest, DeleteWatcher) {
-  std::unique_ptr<MessagePumpLibevent> pump = CreateMessagePump();
   DeleteWatcher delegate(
       std::make_unique<MessagePumpLibevent::FdWatchController>(FROM_HERE));
+  std::unique_ptr<MessagePumpLibevent> pump = CreateMessagePump();
   pump->WatchFileDescriptor(pipefds_[1], false,
                             MessagePumpLibevent::WATCH_READ_WRITE,
                             delegate.controller(), &delegate);
@@ -177,17 +177,17 @@ class StopWatcher : public BaseWatcher {
   }
 
  private:
-  raw_ptr<MessagePumpLibevent::FdWatchController> controller_;
+  raw_ptr<MessagePumpLibevent::FdWatchController> controller_ = nullptr;
 };
 
 TEST_P(MessagePumpLibeventTest, StopWatcher) {
   std::unique_ptr<MessagePumpLibevent> pump = CreateMessagePump();
-  MessagePumpLibevent::FdWatchController watcher(FROM_HERE);
-  StopWatcher delegate(&watcher);
+  MessagePumpLibevent::FdWatchController controller(FROM_HERE);
+  StopWatcher delegate(&controller);
   pump->WatchFileDescriptor(pipefds_[1], false,
-                            MessagePumpLibevent::WATCH_READ_WRITE, &watcher,
+                            MessagePumpLibevent::WATCH_READ_WRITE, &controller,
                             &delegate);
-  SimulateIOEvent(pump.get(), &watcher);
+  SimulateIOEvent(pump.get(), &controller);
 }
 
 void QuitMessageLoopAndStart(OnceClosure quit_closure) {
@@ -215,12 +215,12 @@ class NestedPumpWatcher : public MessagePumpLibevent::FdWatcher {
 };
 
 TEST_P(MessagePumpLibeventTest, NestedPumpWatcher) {
-  std::unique_ptr<MessagePumpLibevent> pump = CreateMessagePump();
-  MessagePumpLibevent::FdWatchController watcher(FROM_HERE);
   NestedPumpWatcher delegate;
+  std::unique_ptr<MessagePumpLibevent> pump = CreateMessagePump();
+  MessagePumpLibevent::FdWatchController controller(FROM_HERE);
   pump->WatchFileDescriptor(pipefds_[1], false, MessagePumpLibevent::WATCH_READ,
-                            &watcher, &delegate);
-  SimulateIOEvent(pump.get(), &watcher);
+                            &controller, &delegate);
+  SimulateIOEvent(pump.get(), &controller);
 }
 
 void FatalClosure() {
@@ -229,9 +229,8 @@ void FatalClosure() {
 
 class QuitWatcher : public BaseWatcher {
  public:
-  QuitWatcher(MessagePumpLibevent::FdWatchController* controller,
-              base::OnceClosure quit_closure)
-      : controller_(controller), quit_closure_(std::move(quit_closure)) {}
+  QuitWatcher(base::OnceClosure quit_closure)
+      : quit_closure_(std::move(quit_closure)) {}
 
   void OnFileCanReadWithoutBlocking(int /* fd */) override {
     // Post a fatal closure to the MessageLoop before we quit it.
@@ -243,7 +242,6 @@ class QuitWatcher : public BaseWatcher {
   }
 
  private:
-  raw_ptr<MessagePumpLibevent::FdWatchController> controller_;
   base::OnceClosure quit_closure_;
 };
 
@@ -264,8 +262,8 @@ TEST_P(MessagePumpLibeventTest, QuitWatcher) {
   MessagePumpLibevent* pump = executor_pump.get();
   SingleThreadTaskExecutor executor(std::move(executor_pump));
   RunLoop run_loop;
+  QuitWatcher delegate(run_loop.QuitClosure());
   MessagePumpLibevent::FdWatchController controller(FROM_HERE);
-  QuitWatcher delegate(&controller, run_loop.QuitClosure());
   WaitableEvent event(WaitableEvent::ResetPolicy::AUTOMATIC,
                       WaitableEvent::InitialState::NOT_SIGNALED);
   std::unique_ptr<WaitableEventWatcher> watcher(new WaitableEventWatcher);
