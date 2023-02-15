b050f6754f8c0b5dfc3167db91e9f011fe8da42f
Susanne Westphal <swestphal@google.com>
2023-02-15 15:14:06 +0000

    Provide implementation for GetDownloadManager for WebEngine
    Follow up of crrev.com/c/4165604
    
    Change-Id: Ie4efd70ba3b8bb12c473d3bac9b3a75b7602bb12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4249169
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105650}

--- weblayer/browser/autofill_client_impl.cc
+++ weblayer/browser/autofill_client_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "build/build_config.h"
+#include "components/autofill/core/browser/autofill_download_manager.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/ui/suggestion.h"
 #include "content/public/browser/browser_context.h"
@@ -35,8 +36,12 @@ AutofillClientImpl::GetURLLoaderFactory() {
 }
 
 autofill::AutofillDownloadManager* AutofillClientImpl::GetDownloadManager() {
-  NOTREACHED();
-  return nullptr;
+  if (!download_manager_) {
+    // Lazy initialization to avoid virtual function calls in the constructor.
+    download_manager_ = std::make_unique<autofill::AutofillDownloadManager>(
+        this, GetChannel(), GetLogManager());
+  }
+  return download_manager_.get();
 }
 
 autofill::PersonalDataManager* AutofillClientImpl::GetPersonalDataManager() {
