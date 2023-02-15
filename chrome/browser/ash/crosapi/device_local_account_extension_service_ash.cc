88975821ed4d06f65878ece9cfc73fd2baef38a1
Miriam Zimmerman <mutexlox@google.com>
2023-02-15 16:35:30 +0000

    crosapi: Use proper DumpWithoutCrashing function.
    DeviceLocalAccountExtensionServiceAsh is the only place in chromium that
    directly calls crashpad's DumpWithoutCrashing function. Change it to use
    //base/debug's version instead.
    
    This has two benefits:
    
    1) it ensures it works on breakpad builds (which are fewer these days,
       but still exist)
    2) With future changes to add a crash key in DumpWithoutCrashing
       (https://crrev.com/c/4246677), this change ensures that this dump
       will have the appropriate crash key.
    
    BUG=b:269159625
    TEST=CQ
    
    Change-Id: I91d3c423d04e12a37baa69486a85b96cdc9e6ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4246459
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105686}

--- chrome/browser/ash/crosapi/device_local_account_extension_service_ash.cc
+++ chrome/browser/ash/crosapi/device_local_account_extension_service_ash.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "components/account_id/account_id.h"
-#include "components/crash/core/app/crashpad.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -59,7 +58,7 @@ void DeviceLocalAccountExtensionServiceAsh::BindExtensionInstaller(
         broker->GetCachedExtensions());
   } else {
     LOG(ERROR) << "Missing broker for DeviceLocalAccount";
-    crash_reporter::DumpWithoutCrashing();
+    base::debug::DumpWithoutCrashing();
   }
 }
 
