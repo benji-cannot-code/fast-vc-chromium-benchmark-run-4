547b553857c581c6e8261237c0103bf37bdbb5dc
Adam Langley <agl@chromium.org>
2023-02-15 23:27:40 +0000

    webauthn: support `prf` extension on Android.
    This change adds support for the PRF extension[1] both for webpages on
    Android and via hybrid CTAP. Note that this depends on support in Google
    Play Services, which hasn't yet shipped at the time of writing.
    
    Bug: 1106961
    Change-Id: I2252bec28aaf1223633bb8c91a2fee4198b7c200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4223052
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105938}

--- chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticator.java
+++ chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticator.java
@@ -145,7 +145,7 @@ class CableAuthenticator {
             Fido2Api.appendBrowserMakeCredentialOptionsToParcel(
                     params, Uri.parse("https://" + params.relyingParty.id), params.challenge, args);
         } catch (NoSuchAlgorithmException e) {
-            onAuthenticatorAttestationResponse(CTAP2_ERR_UNSUPPORTED_ALGORITHM, null, null);
+            onAuthenticatorAttestationResponse(CTAP2_ERR_UNSUPPORTED_ALGORITHM, null, null, false);
             return;
         }
 
@@ -292,7 +292,7 @@ class CableAuthenticator {
                 }
 
                 onAuthenticatorAttestationResponse(
-                        CTAP2_OK, r.attestationObject, devicePublicKeySignature);
+                        CTAP2_OK, r.attestationObject, devicePublicKeySignature, r.prf);
                 result = Result.REGISTER_OK;
             }
         } else {
@@ -309,7 +309,7 @@ class CableAuthenticator {
 
         if (result != Result.REGISTER_OK && result != Result.SIGN_OK) {
             if (isMakeCredential) {
-                onAuthenticatorAttestationResponse(ctapStatus, null, null);
+                onAuthenticatorAttestationResponse(ctapStatus, null, null, false);
             } else {
                 onAuthenticatorAssertionResponse(ctapStatus, null);
             }
@@ -318,12 +318,13 @@ class CableAuthenticator {
         mUi.onAuthenticatorResult(result);
     }
 
-    private void onAuthenticatorAttestationResponse(
-            int ctapStatus, byte[] attestationObject, byte[] devicePublicKeySignature) {
+    private void onAuthenticatorAttestationResponse(int ctapStatus, byte[] attestationObject,
+            byte[] devicePublicKeySignature, boolean prfEnabled) {
         mTaskRunner.postTask(
                 ()
                         -> CableAuthenticatorJni.get().onAuthenticatorAttestationResponse(
-                                ctapStatus, attestationObject, devicePublicKeySignature));
+                                ctapStatus, attestationObject, devicePublicKeySignature,
+                                prfEnabled));
     }
 
     private void onAuthenticatorAssertionResponse(int ctapStatus, byte[] responseBytes) {
@@ -453,8 +454,8 @@ class CableAuthenticator {
         /**
          * Called to alert native code of a response to a makeCredential request.
          */
-        void onAuthenticatorAttestationResponse(
-                int ctapStatus, byte[] attestationObject, byte[] devicePublicKeySignature);
+        void onAuthenticatorAttestationResponse(int ctapStatus, byte[] attestationObject,
+                byte[] devicePublicKeySignature, boolean prfEnabled);
 
         /**
          * Called to alert native code of a response to a getAssertion request.
