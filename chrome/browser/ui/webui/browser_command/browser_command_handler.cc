886b90e72c7b18a2bc0c2dbeaca2e050b744ced5
Rebekah Potter <rbpotter@chromium.org>
2023-02-15 21:35:13 +0000

    What's New: Add browser command to deep link to performance settings
    Adding a new browser command to open chrome://settings/performance,
    and modifying the list of supported browser commands for What's New to
    only include this command (other commands are only needed in versions
    before 110).
    
    The browser command should only be executed if the performance settings
    page exists, so it is conditioned on the feature flags that control
    the page.
    
    To locally test:
    - Build patch and run Chrome (branded build) with at least one of
      the feature flags enabled.
    - Navigate to chrome://whats-new, open devtools in iframe, and run:
    window.top.postMessage({'data': {'commandId': 8, 'clickInfo': {}}}, 'chrome://whats-new')
    
    Bug: 1416402
    Change-Id: Ic1b26c02587da7c9567494e89b16cd375ad3b343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4251563
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105885}

--- chrome/browser/ui/webui/browser_command/browser_command_handler.cc
+++ chrome/browser/ui/webui/browser_command/browser_command_handler.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/user_education/browser_user_education_service.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/performance_manager/public/features.h"
 #include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing/core/common/safe_browsing_policy_handler.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
@@ -97,6 +98,13 @@ void BrowserCommandHandler::CanExecuteCommand(
     case Command::kNoOpCommand:
       can_execute = true;
       break;
+    case Command::kOpenPerformanceSettings:
+      can_execute =
+          base::FeatureList::IsEnabled(
+              performance_manager::features::kBatterySaverModeAvailable) ||
+          base::FeatureList::IsEnabled(
+              performance_manager::features::kHighEfficiencyModeAvailable);
+      break;
   }
   std::move(callback).Run(can_execute);
 }
@@ -160,6 +168,10 @@ void BrowserCommandHandler::ExecuteCommandWithDisposition(
       break;
     case Command::kNoOpCommand:
       // Nothing to do.
+      break;
+    case Command::kOpenPerformanceSettings:
+      NavigateToURL(GURL(chrome::GetSettingsUrl(chrome::kPerformanceSubPage)),
+                    disposition);
       break;
     default:
       NOTREACHED() << "Unspecified behavior for command " << id;
