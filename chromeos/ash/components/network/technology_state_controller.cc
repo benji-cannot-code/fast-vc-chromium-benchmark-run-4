a85c98a84ac6cadcaa0923e9bf2acefeaf89042f
Jason Zhang <jiajunz@google.com>
2023-02-15 00:42:46 +0000

    [CrOS Hotspot] Introduce TechnologStateController
    This CL introduces TechnologStateController, which will serve as
    the primary entry point for enabling and disabling network devices and
    technologies. There are no logical changes at this point, a following CL
    will be uploaded to change NetworkStateHandler::SetTechnologyEnabled()
    to a private function and add TechnologStateController as a private
    friend class, then update the existing NSH::SetTechnologyEnabled() to
    call this new class instead.
    
    Bug: b/239477916
    Change-Id: Ice670ca1e3dfc77181ca0fab86c97a36c71d97f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4229014
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105403}

---
+++ chromeos/ash/components/network/technology_state_controller.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/network/technology_state_controller.h"
+
+#include "chromeos/ash/components/network/network_state_handler.h"
+
+namespace ash {
+
+TechnologyStateController::TechnologyStateController() = default;
+
+TechnologyStateController::~TechnologyStateController() = default;
+
+void TechnologyStateController::Init(
+    NetworkStateHandler* network_state_handler) {
+  network_state_handler_ = network_state_handler;
+}
+
+void TechnologyStateController::SetTechnologiesEnabled(
+    const NetworkTypePattern& type,
+    bool enabled,
+    network_handler::ErrorCallback error_callback) {
+  network_state_handler_->SetTechnologyEnabled(type, enabled,
+                                               std::move(error_callback));
+}
+
+}  // namespace ash
---
+++ chromeos/ash/components/network/technology_state_controller.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_NETWORK_TECHNOLOGY_STATE_CONTROLLER_H_
+#define CHROMEOS_ASH_COMPONENTS_NETWORK_TECHNOLOGY_STATE_CONTROLLER_H_
+
+#include "base/component_export.h"
+
+#include "chromeos/ash/components/network/network_handler.h"
+#include "chromeos/ash/components/network/network_handler_callbacks.h"
+#include "chromeos/ash/components/network/network_type_pattern.h"
+
+namespace ash {
+
+class NetworkStateHandler;
+
+// This class serves as the primary entry point for enabling and disabling
+// network devices and technologies, and ensures handling of hotspot/Wifi
+// concurrency issues by disabling one before enabling the other.
+class COMPONENT_EXPORT(CHROMEOS_NETWORK) TechnologyStateController {
+ public:
+  TechnologyStateController();
+  TechnologyStateController(const TechnologyStateController&) = delete;
+  TechnologyStateController& operator=(const TechnologyStateController&) =
+      delete;
+  ~TechnologyStateController();
+
+  void Init(NetworkStateHandler* network_state_handler);
+
+  // Asynchronously sets the technologies enabled property for |type|.Calls
+  // |error_callback| upon failure.
+  void SetTechnologiesEnabled(const NetworkTypePattern& type,
+                              bool enabled,
+                              network_handler::ErrorCallback error_callback);
+
+ private:
+  NetworkStateHandler* network_state_handler_ = nullptr;
+};
+
+}  // namespace ash
+
+#endif  // CHROMEOS_ASH_COMPONENTS_NETWORK_TECHNOLOGY_STATE_CONTROLLER_H_
