8d37ec2f5f22bf213812b603e772cb9d6996245e
Yuki Awano <yawano@google.com>
2023-02-15 03:47:47 +0000

    Delete enable-assistant-routines flag
    - This flag is no longer used. Details are put in the bug.
    
    Bug: b:267826174
    Fixed: b:267826174
    Change-Id: Ifa195dca814e17812603e3cc240f257f72e63d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4251836
    Quick-Run: Yuki Awano <yawano@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105476}

--- chromeos/ash/services/assistant/public/cpp/features.cc
+++ chromeos/ash/services/assistant/public/cpp/features.cc
@@ -24,10 +24,6 @@ BASE_FEATURE(kAssistantDebugging,
              "AssistantDebugging",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAssistantRoutines,
-             "AssistantRoutines",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kAssistantWaitScheduling,
              "AssistantWaitScheduling",
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -84,10 +80,6 @@ bool IsPowerManagerEnabled() {
 
 bool IsLibAssistantBetaBackendEnabled() {
   return base::FeatureList::IsEnabled(kEnableLibAssistantBetaBackend);
-}
-
-bool IsRoutinesEnabled() {
-  return base::FeatureList::IsEnabled(kAssistantRoutines);
 }
 
 bool IsStereoAudioInputEnabled() {
