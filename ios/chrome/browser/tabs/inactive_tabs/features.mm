d9078355b9834a383f742f6b114942f66d31714b
Aliona DANGLA <alionadangla@google.com>
2023-02-15 16:52:30 +0000

    [iOS] Move inactive tabs feature flags outside of ui folder
    Browser and session restoration will need to check the feature flag, so
    this CL moves the feature flag outside of ui folder to be able to
    include it everywhere.
    
    Change-Id: Ibcdf9dbc792829150e3b61945911cb0d029dc1bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4248194
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105693}

--- ios/chrome/browser/tabs/inactive_tabs/features.mm
+++ ios/chrome/browser/tabs/inactive_tabs/features.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/features.h"
+#import "ios/chrome/browser/tabs/inactive_tabs/features.h"
 
 #import "base/metrics/field_trial_params.h"
 #import "base/time/time.h"
--- ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_button.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_button.mm
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <QuartzCore/QuartzCore.h>
 
+#import "ios/chrome/browser/tabs/inactive_tabs/features.h"
 #import "ios/chrome/browser/ui/icons/symbols.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/features.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
 
