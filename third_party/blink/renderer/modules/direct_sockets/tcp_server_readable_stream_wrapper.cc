2e8014fed045cb3b0736da6d3ffdcd8380d13154
Andrew Rayskiy <greengrape@google.com>
2023-02-15 13:07:41 +0000

    [DirectSockets] Add a stub implementation of TCPServerSocket
    This CL introduces an .idl for TCPServerSocket and wires up the
    necessary C++ definitions. This new class is guarded by a separate blink
    flag called DirectSocketsExperimental so that the new symbols do not
    show up in IWAs while under construction.
    
    Explainer: go/direct-sockets-exp
    
    Bug: 1408140
    
    Change-Id: Iaced4381e0c5d018cea0588e8f004645f8de8c3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173394
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105607}

---
+++ third_party/blink/renderer/modules/direct_sockets/tcp_server_readable_stream_wrapper.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/direct_sockets/tcp_server_readable_stream_wrapper.h"
+
+#include "base/notreached.h"
+
+namespace blink {
+
+TCPServerReadableStreamWrapper::TCPServerReadableStreamWrapper(
+    ScriptState* script_state)
+    : ReadableStreamDefaultWrapper(script_state) {}
+
+void TCPServerReadableStreamWrapper::Pull() {
+  NOTIMPLEMENTED();
+}
+
+void TCPServerReadableStreamWrapper::CloseStream() {
+  NOTIMPLEMENTED();
+}
+
+void TCPServerReadableStreamWrapper::ErrorStream(int32_t error_code) {
+  NOTIMPLEMENTED();
+}
+
+void TCPServerReadableStreamWrapper::Trace(Visitor* visitor) const {
+  ReadableStreamDefaultWrapper::Trace(visitor);
+}
+
+}  // namespace blink
---
+++ third_party/blink/renderer/modules/direct_sockets/tcp_server_socket.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/direct_sockets/tcp_server_socket.h"
+
+#include "base/functional/callback_helpers.h"
+#include "base/notreached.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_tcp_server_socket_open_info.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_tcp_server_socket_options.h"
+#include "third_party/blink/renderer/modules/direct_sockets/tcp_server_readable_stream_wrapper.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/script_state.h"
+
+namespace blink {
+
+TCPServerSocket::TCPServerSocket(ScriptState* script_state)
+    : Socket(script_state) {}
+
+TCPServerSocket::~TCPServerSocket() = default;
+
+// static
+TCPServerSocket* TCPServerSocket::Create(ScriptState* script_state,
+                                         const String& local_address,
+                                         const TCPServerSocketOptions* options,
+                                         ExceptionState& exception_state) {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+ScriptPromise TCPServerSocket::close(ScriptState*, ExceptionState&) {
+  NOTIMPLEMENTED();
+  return ScriptPromise();
+}
+
+void TCPServerSocket::Trace(Visitor* visitor) const {
+  visitor->Trace(readable_stream_wrapper_);
+
+  ScriptWrappable::Trace(visitor);
+  Socket::Trace(visitor);
+}
+
+}  // namespace blink
