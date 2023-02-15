de84590fc01ea2eb575077b34e45c6620f45292d
Avi Drissman <avi@chromium.org>
2023-02-15 20:21:00 +0000

    Update components/update_client to new Value API
    This was landed as https://crrev.com/c/4240982 but reverted. This
    reland updates call sites that were missed.
    
    Bug: 646113
    Cq-Include-Trybots: luci.chrome.try:linux-chrome,mac-chrome,win-chrome
    Change-Id: Ic50137ad26d34132b94f383fb95d60483a4ca5f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4246862
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105834}

--- components/update_client/protocol_serializer_json_unittest.cc
+++ components/update_client/protocol_serializer_json_unittest.cc
@@ -23,9 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/re2/src/re2/re2.h"
 
-using base::Value;
-using std::string;
-
 namespace update_client {
 
 TEST(SerializeRequestJSON, Serialize) {
@@ -41,12 +38,10 @@ TEST(SerializeRequestJSON, Serialize) {
     std::vector<std::string> items = {"id1"};
     test::SetDateLastData(metadata.get(), items, 1234);
 
-    std::vector<base::Value> events;
-    events.emplace_back(Value::Type::DICT);
-    events.emplace_back(Value::Type::DICT);
-    events[0].SetKey("a", Value(1));
-    events[0].SetKey("b", Value("2"));
-    events[1].SetKey("error", Value(0));
+    std::vector<base::Value::Dict> events(2);
+    events[0].Set("a", 1);
+    events[0].Set("b", "2");
+    events[1].Set("error", 0);
 
     std::vector<protocol_request::App> apps;
     apps.push_back(MakeProtocolApp(
--- components/update_client/puffin_patcher_unittest.cc
+++ components/update_client/puffin_patcher_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
-#include "base/values.h"
 #include "components/services/patch/in_process_file_patcher.h"
 #include "components/update_client/patch/patch_impl.h"
 #include "components/update_client/test_installer.h"
