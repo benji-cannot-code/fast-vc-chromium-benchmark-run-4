8dae474ca0ae9934e602503107cebd26c4e8516c
Andreea Costinas <acostinas@google.com>
2023-02-15 14:39:46 +0000

    Add a flag to allow less secure EAP configs
    This CL adds a flag which will allow users to configure networks to use
    EAP methods that accept the default server CA certificate without
    verifying the identity of the server.
    
    Bug: b/268176792
    Change-Id: Ia912e57cd7c629c6e571ac3dee6b2bfdcc4bc03f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240155
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105638}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2672,6 +2672,9 @@ extern const char kAdaptiveChargingDescription[];
 extern const char kAdaptiveChargingForTestingName[];
 extern const char kAdaptiveChargingForTestingDescription[];
 
+extern const char kAllowEapDefaultCasWithoutSubjectVerificationName[];
+extern const char kAllowEapDefaultCasWithoutSubjectVerificationDescription[];
+
 extern const char kAllowRepeatedUpdatesName[];
 extern const char kAllowRepeatedUpdatesDescription[];
 
