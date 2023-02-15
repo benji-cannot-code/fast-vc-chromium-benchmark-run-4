d793dc349ab7812836c4e42675508758bc179c11
Jeremy Leconte <jleconte@google.com>
2023-02-15 15:08:42 +0000

    Add missing no_destructor include.
    This is breaking Chromium roll in WebRTC.
    
    Bug: chromium:1411340
    Change-Id: I390dc40504ae3e441f29b720ab8d4ec3e6c8ae89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4251158
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Auto-Submit: Jeremy Leconte <jleconte@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105647}

--- base/android/meminfo_dump_provider.h
+++ base/android/meminfo_dump_provider.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_ANDROID_MEMINFO_DUMP_PROVIDER_H_
 
 #include "base/base_export.h"
+#include "base/no_destructor.h"
 #include "base/time/time.h"
 #include "base/trace_event/base_tracing.h"
 
