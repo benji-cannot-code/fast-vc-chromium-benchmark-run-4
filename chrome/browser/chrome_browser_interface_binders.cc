539fa575af58adab8c0f169d904881e5eb584c9b
Tsuyoshi Horo <horo@chromium.org>
2023-02-15 02:07:49 +0000

    Revert "shortcuts: Add search.mojom and implementation of SearchHandler"
    This reverts commit bfe480f04823faf884a3edf9bff72e3402dbb4b8.
    
    Reason for revert: SearchHandlerTest.SearchWithFakeResults failing on ASan LSan bots.
    Bug: 1416413
    
    Original change's description:
    > shortcuts: Add search.mojom and implementation of SearchHandler
    >
    > Add search.mojom which adds the following:
    > - SearchResult struct
    > - SearchHandler interface
    >
    > Also adds the implementation of SearchHandler + unit tests.
    > For now, we're returning fake data from the Search method.
    > This is temporary so that the frontend work is unblocked,
    > specifically so that the frontend can complete an e2e search request.
    > We will remove this temporary fake data when we implement real search
    > using the LocalSearchService index.
    >
    > Note that searching is gated behind the flag kSearchInShortcutsApp.
    >
    > Finally, this CL also binds SearchHandler to the Shortcuts
    > Mojo WebUI controller.
    >
    > See the design docs in the bug (b/266085562) for full context.
    >
    > Test: ash_webui_unittests --gtest_filter=SearchHandlerTest.*
    > Bug: b:266085562
    > Change-Id: I7c561f40232163fd31e587470efe08a737f7b795
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4235076
    > Commit-Queue: Camden Bickel <cambickel@google.com>
    > Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1105310}
    
    Bug: b:266085562
    Change-Id: I00ce39eb60ef11330d34e75ffc40858a7073cb57
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4253065
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105446}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -251,7 +251,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/scanning/mojom/scanning.mojom.h"
 #include "ash/webui/scanning/scanning_ui.h"
 #include "ash/webui/shimless_rma/shimless_rma.h"
-#include "ash/webui/shortcut_customization_ui/backend/search/search.mojom.h"
 #include "ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom.h"
 #include "ash/webui/shortcut_customization_ui/shortcut_customization_app_ui.h"
 #include "ash/webui/system_extensions_internals_ui/mojom/system_extensions_internals_ui.mojom.h"
@@ -1290,12 +1289,6 @@ void PopulateChromeWebUIFrameBinders(
     RegisterWebUIControllerInterfaceBinder<
         ash::shortcut_customization::mojom::AcceleratorConfigurationProvider,
         ash::ShortcutCustomizationAppUI>(map);
-
-    if (ash::features::IsSearchInShortcutsAppEnabled()) {
-      RegisterWebUIControllerInterfaceBinder<
-          ash::shortcut_customization::mojom::SearchHandler,
-          ash::ShortcutCustomizationAppUI>(map);
-    }
   }
 
   RegisterWebUIControllerInterfaceBinder<
