6d74bdff5739b3b83dbed343b11619e74a74477c
David Roger <droger@chromium.org>
2023-02-15 14:07:24 +0000

    [Profiles] Add histograms recording the StartupProfileMode
    Add histograms to better understand the effect of the
    kObserverBasedPostProfileInit experiment on the ProfilePicker.
    
    Current metrics show that the experiment causes the ProfilePicker to be
    triggered less often on Windows platform. These histograms may help
    understand if this is because a browser window is opened more often, or
    if the startup code as a whole is called less.
    
    See go/picker-without-guest-metrics (google-only) for some more details.
    
    Bug: 1150326
    Change-Id: I1a147464ce953a71558aa06033e318d6ed63df12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4239864
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105626}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/field_trial.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
@@ -433,8 +434,12 @@ StartupProfileInfo CreateInitialProfile(
 
   profile_info = GetStartupProfile(cur_dir, parsed_command_line);
 
-  if (profile_info.mode == StartupProfileMode::kError && !last_used_profile_set)
+  if (profile_info.mode == StartupProfileMode::kError &&
+      !last_used_profile_set) {
     profile_info = GetFallbackStartupProfile();
+    base::UmaHistogramEnumeration(
+        "ProfilePicker.StartupMode.FallbackProfileUsed", profile_info.mode);
+  }
 
   if (profile_info.mode == StartupProfileMode::kError) {
     ProfileErrorType error_type =
@@ -477,6 +482,9 @@ void ProcessSingletonNotificationCallbackImpl(
   StartupProfilePathInfo startup_profile_path_info =
       GetStartupProfilePath(current_directory, command_line,
                             /*ignore_profile_picker=*/false);
+  base::UmaHistogramEnumeration(
+      "ProfilePicker.StartupMode.NotificationCallback",
+      startup_profile_path_info.mode);
 
   DCHECK_NE(startup_profile_path_info.mode, StartupProfileMode::kError);
 
@@ -1544,6 +1552,8 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
   // and more directly Profile.CreateAndInitializeProfile.
   StartupProfileInfo profile_info = CreateInitialProfile(
       /*cur_dir=*/base::FilePath(), *base::CommandLine::ForCurrentProcess());
+  base::UmaHistogramEnumeration(
+      "ProfilePicker.StartupMode.CreateInitialProfile", profile_info.mode);
 
   if (profile_info.mode == StartupProfileMode::kError)
     return content::RESULT_CODE_NORMAL_EXIT;
