e28b295fdc63f37cf569106e629af96e990088af
anthie@google.com <anthie@google.com>
2023-02-15 16:44:43 +0000

    CodeHealth: Migrate base::Value::FindBoolKey in user_policies
    Migrate usages of base::Value::FindBoolKey to base::Value::Dict::FindBool(), as well as any other
    occurrence of the API v1 base::Value that can be found
    in the affected methods.
    
    Bug: 1187001
    Change-Id: Ib30f4180a9a1b2aa99405515c24800ad89fa5792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4228075
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105690}

--- chrome/credential_provider/gaiacp/user_policies_manager.cc
+++ chrome/credential_provider/gaiacp/user_policies_manager.cc
@@ -302,7 +302,7 @@ bool UserPoliciesManager::GetUserPolicies(const std::wstring& sid,
   }
 
   // Override policies with those we just read.
-  *user_policies = UserPolicies::FromValue(*policies);
+  *user_policies = UserPolicies::FromValue(policies->GetDict());
 
   return true;
 }
