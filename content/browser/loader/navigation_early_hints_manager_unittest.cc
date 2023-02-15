d5f5015e2d09a2345de12a9de5192723200eadd6
Patrick Meenan <pmeenan@chromium.org>
2023-02-15 15:30:47 +0000

    Adjust the font preload priorities to match blink
    A few years back we changed the priority of font preloads so they don't
    block the loading of other critical resources. It looks like the
    header-based and predictor preload priorities got out of sync with the
    blink priorities.
    
    This brings them back into alignment and adds a comment to all 3
    locations to make sure future changes are aligned.
    
    Bug: 1413558
    Change-Id: I275ddff34a15f2f5dc331441ed6a99f350631b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245538
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Patrick Meenan <pmeenan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105660}

--- content/browser/loader/navigation_early_hints_manager_unittest.cc
+++ content/browser/loader/navigation_early_hints_manager_unittest.cc
@@ -381,7 +381,7 @@ TEST_F(NavigationEarlyHintsManagerTest, PreloadPriority) {
   EXPECT_EQ(early_hints_manager().CalculateRequestPriority(CreateLinkHeader(
                 network::mojom::LinkAsAttribute::kFont,
                 network::mojom::FetchPriorityAttribute::kAuto)),
-            net::HIGHEST);
+            net::MEDIUM);
   EXPECT_EQ(early_hints_manager().CalculateRequestPriority(CreateLinkHeader(
                 network::mojom::LinkAsAttribute::kScript,
                 network::mojom::FetchPriorityAttribute::kAuto)),
@@ -415,7 +415,11 @@ TEST_F(NavigationEarlyHintsManagerTest, PreloadPriority) {
   EXPECT_EQ(early_hints_manager().CalculateRequestPriority(CreateLinkHeader(
                 network::mojom::LinkAsAttribute::kFont,
                 network::mojom::FetchPriorityAttribute::kHigh)),
-            net::HIGHEST);
+            net::MEDIUM);
+  EXPECT_EQ(early_hints_manager().CalculateRequestPriority(
+                CreateLinkHeader(network::mojom::LinkAsAttribute::kFont,
+                                 network::mojom::FetchPriorityAttribute::kLow)),
+            net::LOWEST);
 }
 
 }  // namespace content
