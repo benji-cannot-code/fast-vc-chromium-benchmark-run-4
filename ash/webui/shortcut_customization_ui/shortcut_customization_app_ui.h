bef144bd39fc8e8caa23218ed0542898aae33c67
Cam Bickel <cambickel@google.com>
2023-02-15 22:07:11 +0000

    shortcuts: Add search.mojom and implementation of SearchHandler
    Add search.mojom which adds the following:
    - SearchResult struct
    - SearchHandler interface
    
    Also adds the implementation of SearchHandler + unit tests.
    For now, we're returning fake data from the Search method.
    This is temporary so that the frontend work is unblocked,
    specifically so that the frontend can complete an e2e search request.
    We will remove this temporary fake data when we implement real search
    using the LocalSearchService index.
    
    Note that searching is gated behind the flag kSearchInShortcutsApp.
    
    Finally, this CL also binds SearchHandler to the Shortcuts
    Mojo WebUI controller.
    
    See the design docs in the bug (b/266085562) for full context.
    
    NOTE: This CL fixes the memory leak that caused the original CL to
    be reverted.
    
    Test: ash_webui_unittests --gtest_filter=SearchHandlerTest.*
    Bug: b:266085562
    Change-Id: Ifa381fe76e16e6fa22b1999a932c99568d7df188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4251685
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105897}

--- ash/webui/shortcut_customization_ui/shortcut_customization_app_ui.h
+++ ash/webui/shortcut_customization_ui/shortcut_customization_app_ui.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h"
+#include "ash/webui/shortcut_customization_ui/backend/search/search.mojom.h"
+#include "ash/webui/shortcut_customization_ui/backend/search/search_handler.h"
 #include "ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom.h"
 #include "ash/webui/shortcut_customization_ui/url_constants.h"
 #include "ash/webui/system_apps/public/system_web_app_ui_config.h"
@@ -45,8 +47,13 @@ class ShortcutCustomizationAppUI : public ui::MojoWebUIController {
           shortcut_customization::mojom::AcceleratorConfigurationProvider>
           receiver);
 
+  void BindInterface(
+      mojo::PendingReceiver<shortcut_customization::mojom::SearchHandler>
+          receiver);
+
  private:
   std::unique_ptr<shortcut_ui::AcceleratorConfigurationProvider> provider_;
+  std::unique_ptr<shortcut_ui::SearchHandler> search_handler_;
   WEB_UI_CONTROLLER_TYPE_DECL();
 };
 
