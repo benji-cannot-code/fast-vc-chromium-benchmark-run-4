ee2a1c86876e981d37808def9b0dfd1b3349ce62
kylechar <kylechar@chromium.org>
2023-02-15 00:12:11 +0000

    Add GPU.SharedImage.BackingType histogram
    Keep track of what shared image backings are used on various platforms.
    
    Bug: none
    Change-Id: I42a670c74e4fee571dbf7ae88af20929baede2d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245779
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105385}

--- gpu/command_buffer/service/shared_image/shared_image_backing.h
+++ gpu/command_buffer/service/shared_image/shared_image_backing.h
@@ -64,6 +64,8 @@ class MemoryTypeTracker;
 class SharedImageFactory;
 class VaapiDependenciesFactory;
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
 enum class SharedImageBackingType {
   kTest = 0,
   kExternalVkImage = 1,
@@ -83,6 +85,7 @@ enum class SharedImageBackingType {
   kIOSurface = 15,
   kDCompSurface = 16,
   kDXGISwapChain = 17,
+  kMaxValue = kDXGISwapChain
 };
 
 #if BUILDFLAG(IS_WIN)
