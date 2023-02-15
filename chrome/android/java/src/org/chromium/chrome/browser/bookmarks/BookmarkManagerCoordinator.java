cdba6fc4d766f13acd05fd56b0378b25937869e9
Brandon Wylie <wylieb@chromium.org>
2023-02-15 20:16:30 +0000

    [bookmarks] Add scaffolding for BookmarkManagerMVC
    Bug: 1416611
    Change-Id: Ic4a6f9895d70d25dd7a3f5e08a351ad5a8027145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4256847
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1105831}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.bookmarks;
+
+/** Responsible for setting up sub-components and routing incoming/outgoing signals */
+public class BookmarkManagerCoordinator {}
