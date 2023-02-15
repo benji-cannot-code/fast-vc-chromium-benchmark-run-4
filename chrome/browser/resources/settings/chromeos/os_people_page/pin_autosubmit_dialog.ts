0454d98365c687df42faa67945f3b7ade1e5f54d
Wes Okuhara <wesokuhara@google.com>
2023-02-15 01:03:50 +0000

    CrOS Settings: Use TS versions of common resources
    Use TS version of JS resources for improved typechecking. These were
    leftover from previous TS migrations.
    
    Bug: b:263413628
    Test: browser_tests --gtest_filter='OSSettings*'
    Change-Id: I00e6b714d089dda1ad91759ea1e712307cbb63d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244464
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105413}

--- chrome/browser/resources/settings/chromeos/os_people_page/pin_autosubmit_dialog.ts
+++ chrome/browser/resources/settings/chromeos/os_people_page/pin_autosubmit_dialog.ts
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/ash/common/quick_unlock/pin_keyboard.js';
 import 'chrome://resources/ash/common/quick_unlock/setup_pin_keyboard.js';
-import 'chrome://resources/ash/common/assert.js';
+import 'chrome://resources/js/assert_ts.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
@@ -25,6 +25,7 @@ import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {assertExists} from '../assert_extras.js';
+
 import {getTemplate} from './pin_autosubmit_dialog.html.js';
 
 // Maximum length supported by auto submit
