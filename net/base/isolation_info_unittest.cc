252afb2ada2063967ab5a3202f81aa97a3262493
Dustin J. Mitchell <djmitche@chromium.org>
2023-02-16 14:37:44 +0000

    Add IsolationInfo::DebugString
    This complements similar methods on NIK, NAK, and so on.
    
    Change-Id: I9fd916d820801d50b841f20807968fa4eda11243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258565
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Brianna Goldstein <brgoldstein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1106193}

--- net/base/isolation_info_unittest.cc
+++ net/base/isolation_info_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/isolation_info.h"
 
 #include <iostream>
+#include "base/strings/strcat.h"
 #include "base/test/gtest_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
@@ -144,6 +145,28 @@ TEST_P(IsolationInfoTest, IsFrameSiteEnabled) {
   } else {
     EXPECT_TRUE(IsolationInfo::IsFrameSiteEnabled());
   }
+}
+
+TEST_P(IsolationInfoTest, DebugString) {
+  IsolationInfo isolation_info = IsolationInfo::Create(
+      IsolationInfo::RequestType::kMainFrame, kOrigin1, kOrigin2,
+      SiteForCookies::FromOrigin(kOrigin1), kPartyContextEmpty, &kNonce1);
+  std::vector<std::string> parts;
+  parts.push_back(
+      "request_type: kMainFrame; top_frame_origin: https://a.foo.test; ");
+  if (IsolationInfo::IsFrameSiteEnabled()) {
+    parts.push_back("frame_origin: https://b.bar.test; ");
+  }
+  parts.push_back("network_anonymization_key: ");
+  parts.push_back(isolation_info.network_anonymization_key().ToDebugString());
+  parts.push_back("; network_isolation_key: ");
+  parts.push_back(isolation_info.network_isolation_key().ToDebugString());
+  parts.push_back("; party_context: {}; nonce: ");
+  parts.push_back(isolation_info.nonce().value().ToString());
+  parts.push_back(
+      "; site_for_cookies: SiteForCookies: {site=https://foo.test; "
+      "schemefully_same=true}");
+  EXPECT_EQ(isolation_info.DebugString(), base::StrCat(parts));
 }
 
 TEST_P(IsolationInfoTest, CreateNetworkAnonymizationKeyForIsolationInfo) {
