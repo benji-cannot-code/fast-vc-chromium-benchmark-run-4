9bc67026a6d2fb32aed84a2738cd24db902f19cb
Lily Chen <chlily@chromium.org>
2023-02-16 14:42:20 +0000

    [Downloads] Distinguish path validation success with resolved conflict
    This adds an extra enum value to distinguish path validation successes
    that were uneventful vs those that required conflict resolution via
    uniquification, in the path reservation tracker logic.
    
    Allowing the target determiner to see whether the path reservation
    tracker had to resolve a conflict will inform the target determiner
    to go through an extra state in the state machine (in later CLs)
    to prompt the user about a duplicate download in the new bubble UI.
    
    No behavior change is intended here; the new result will be used in
    subsequent CLs.
    
    Bug: 1361503
    Change-Id: Ic297a3ee6cd10120b664ff84dc04b01a886a003b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194403
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106195}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -1196,7 +1196,7 @@ void ChromeDownloadManagerDelegate::GenerateUniqueFileNameDone(
   // After a new, unique filename has been generated, display the error dialog
   // with the filename automatically set to be the unique filename.
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (result == PathValidationResult::SUCCESS) {
+  if (download::IsPathValidationSuccessful(result)) {
     if (download_prefs_->PromptForDownload()) {
       ShowDownloadDialog(
           native_window, 0 /* total_bytes */,
