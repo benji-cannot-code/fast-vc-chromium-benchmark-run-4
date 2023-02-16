37d07114304866c2a7081a8aee3a8aa3a7e8de48
Emilia Paz <emiliapaz@chromium.org>
2023-02-16 19:25:26 +0000

    [Code Health] Use FindInt in c/b/extensions/api files
    Replaces the use of Value::FindIntKey() with Value::Dict::FindInt().
    This required changing the type of some variables and other calls.
    
    No functionality changed.
    
    Bug: 1187001
    Change-Id: I568128b2c73835a49d125273b5cce900ee462266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258065
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106364}

--- chrome/browser/extensions/api/tabs/tabs_test.cc
+++ chrome/browser/extensions/api/tabs/tabs_test.cc
@@ -1315,7 +1315,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabsTest, DiscardedProperty) {
     // Make sure the returned tab is the correct one.
     int tab_id_a = ExtensionTabUtil::GetTabId(web_contents_a);
 
-    absl::optional<int> id = result[0].FindIntKey(keys::kIdKey);
+    ASSERT_TRUE(result[0].is_dict());
+    absl::optional<int> id = result[0].GetDict().FindInt(keys::kIdKey);
     ASSERT_TRUE(id);
 
     EXPECT_EQ(tab_id_a, *id);
@@ -1333,7 +1334,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabsTest, DiscardedProperty) {
     int tab_id_c =
         ExtensionTabUtil::GetTabId(tab_strip_model->GetWebContentsAt(0));
 
-    absl::optional<int> id = result[0].FindIntKey(keys::kIdKey);
+    ASSERT_TRUE(result[0].is_dict());
+    absl::optional<int> id = result[0].GetDict().FindInt(keys::kIdKey);
     ASSERT_TRUE(id);
 
     EXPECT_EQ(tab_id_c, *id);
@@ -1535,7 +1537,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabsTest, AutoDiscardableProperty) {
   ASSERT_EQ(1u, query_result.size());
 
   // Make sure the returned tab is the correct one.
-  absl::optional<int> tab_id = query_result[0].FindIntKey(keys::kIdKey);
+  ASSERT_TRUE(query_result[0].is_dict());
+  absl::optional<int> tab_id = query_result[0].GetDict().FindInt(keys::kIdKey);
   ASSERT_TRUE(tab_id);
   EXPECT_EQ(tab_id_a, *tab_id);
 
@@ -1551,7 +1554,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabsTest, AutoDiscardableProperty) {
   EXPECT_EQ(1u, query_result.size());
 
   // Make sure the returned tab is the correct one.
-  absl::optional<int> id_value = query_result[0].FindIntKey(keys::kIdKey);
+  ASSERT_TRUE(query_result[0].is_dict());
+  absl::optional<int> id_value =
+      query_result[0].GetDict().FindInt(keys::kIdKey);
   ASSERT_TRUE(id_value);
   EXPECT_EQ(ExtensionTabUtil::GetTabId(tab_strip_model->GetWebContentsAt(0)),
             *id_value);
