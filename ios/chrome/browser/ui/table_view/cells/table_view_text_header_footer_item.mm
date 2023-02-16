09574ca3a632b4e0aacdb2039b0a59cc864269c0
Quentin Pubert <qpubert@google.com>
2023-02-16 15:42:34 +0000

    [iOS] Fix table view text header footer subtitle view spacing
    This CL fixes the issue of too much space below the main text label of
    TableViewTextHeaderFooterView when there is no subtitle by hiding the
    subtitle view if the subtitle string is nil.
    
    Fixed: 1416801
    Change-Id: I22ef074f172e4e2fb358387e7c3832d11af1228e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4260067
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1106210}

--- ios/chrome/browser/ui/table_view/cells/table_view_text_header_footer_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_header_footer_item.mm
@@ -166,8 +166,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setSubtitle:(NSString*)subtitle {
   if (!subtitle) {
+    // If no subtitle, hide the subtitle view to avoid taking space for nothing.
+    self.subtitleView.hidden = YES;
     return;
   }
+  // Else, ensure the subtitle view is visible.
+  self.subtitleView.hidden = NO;
 
   StringWithTags parsedString = ParseStringWithLinks(subtitle);
 
