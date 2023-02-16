0ae821579a2d2b63f225f7ae190a70ef3b37452d
Dmitry Vykochko <vykochko@google.com>
2023-02-16 12:05:28 +0000

    Fix crash in ChromePasswordManagerClient::ShowPasswordGenerationPopup.
    Part of stack from crash report:
    base::ImmediateCrash()
    logging::CheckFailure()
    base::WeakPtr<PasswordGenerationPopupControllerImpl>::operator->()
    ...
    
    `popup_controller_` was invalidated, presumably through the following
    calls:
    
    Show() ->
    PasswordGenerationPopupView::Create() failed to create view ->
    HideImpl ->
    delete this;
    
    Bug: 1414043
    Change-Id: I7f4933fe45d59f32f41e116936586773b3161050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4255411
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106151}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -1675,7 +1675,7 @@ void ChromePasswordManagerClient::ShowPasswordGenerationPopup(
 
   driver->SetSuggestionAvailability(
       ui_data.generation_element_id,
-      popup_controller_->IsVisible()
+      popup_controller_ && popup_controller_->IsVisible()
           ? autofill::mojom::AutofillState::kAutofillAvailable
           : autofill::mojom::AutofillState::kNoSuggestions);
 }
