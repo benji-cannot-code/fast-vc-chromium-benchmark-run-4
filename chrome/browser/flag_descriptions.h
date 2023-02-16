84c6ea34e9e73d6e33b873ff7f7ee27f19781743
Rob Schonberger <robsc@chromium.org>
2023-02-16 05:43:19 +0000

    flags: Delete long expired flag enable-heuristic-stylus-palm-rejection
    This flag has long been enabled and in production.
    
    Bug: chromium:1207487
    Test: browser_tests pass.
    Change-Id: Ie7de34ed5d6c9c3e5e5958cdba0cc138613b2a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257807
    Auto-Submit: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106071}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2986,9 +2986,6 @@ extern const char kEnableGoogleAssistantStereoInputDescription[];
 extern const char kEnableGoogleAssistantAecName[];
 extern const char kEnableGoogleAssistantAecDescription[];
 
-extern const char kEnableHeuristicStylusPalmRejectionName[];
-extern const char kEnableHeuristicStylusPalmRejectionDescription[];
-
 extern const char kEnableInputEventLoggingName[];
 extern const char kEnableInputEventLoggingDescription[];
 
