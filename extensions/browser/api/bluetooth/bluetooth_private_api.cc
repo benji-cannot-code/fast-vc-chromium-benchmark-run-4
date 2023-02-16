add30312ebe6ca9592358030c7d2980332c7cf4a
Solomon Kinard <solomonkinard@chromium.org>
2023-02-16 18:29:52 +0000

    Extensions: Fixit: Use '= default' to define a trivial *structor
    This CL was uploaded by git cl split.
    
    R=hansberry@chromium.org
    
    Bug: 1416739
    Change-Id: I7a5e5f7459da39e4e9a2f9680494cd9c91c9e872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257400
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106313}

--- extensions/browser/api/bluetooth/bluetooth_private_api.cc
+++ extensions/browser/api/bluetooth/bluetooth_private_api.cc
@@ -153,7 +153,7 @@ BluetoothPrivateAPI::BluetoothPrivateAPI(content::BrowserContext* context)
       ->RegisterObserver(this, bt_private::OnPairing::kEventName);
 }
 
-BluetoothPrivateAPI::~BluetoothPrivateAPI() {}
+BluetoothPrivateAPI::~BluetoothPrivateAPI() = default;
 
 void BluetoothPrivateAPI::Shutdown() {
   EventRouter::Get(browser_context_)->UnregisterObserver(this);
@@ -459,9 +459,11 @@ void BluetoothPrivateDisconnectAllFunction::OnErrorCallback(
 
 ////////////////////////////////////////////////////////////////////////////////
 
-BluetoothPrivateForgetDeviceFunction::BluetoothPrivateForgetDeviceFunction() {}
+BluetoothPrivateForgetDeviceFunction::BluetoothPrivateForgetDeviceFunction() =
+    default;
 
-BluetoothPrivateForgetDeviceFunction::~BluetoothPrivateForgetDeviceFunction() {}
+BluetoothPrivateForgetDeviceFunction::~BluetoothPrivateForgetDeviceFunction() =
+    default;
 
 bool BluetoothPrivateForgetDeviceFunction::CreateParams() {
   params_ = bt_private::ForgetDevice::Params::Create(args());
@@ -576,9 +578,9 @@ void BluetoothPrivateSetDiscoveryFilterFunction::OnErrorCallback() {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-BluetoothPrivateConnectFunction::BluetoothPrivateConnectFunction() {}
+BluetoothPrivateConnectFunction::BluetoothPrivateConnectFunction() = default;
 
-BluetoothPrivateConnectFunction::~BluetoothPrivateConnectFunction() {}
+BluetoothPrivateConnectFunction::~BluetoothPrivateConnectFunction() = default;
 
 bool BluetoothPrivateConnectFunction::CreateParams() {
   params_ = bt_private::Connect::Params::Create(args());
@@ -623,9 +625,9 @@ void BluetoothPrivateConnectFunction::OnConnect(
 
 ////////////////////////////////////////////////////////////////////////////////
 
-BluetoothPrivatePairFunction::BluetoothPrivatePairFunction() {}
+BluetoothPrivatePairFunction::BluetoothPrivatePairFunction() = default;
 
-BluetoothPrivatePairFunction::~BluetoothPrivatePairFunction() {}
+BluetoothPrivatePairFunction::~BluetoothPrivatePairFunction() = default;
 bool BluetoothPrivatePairFunction::CreateParams() {
   params_ = bt_private::Pair::Params::Create(args());
   return params_ != nullptr;
