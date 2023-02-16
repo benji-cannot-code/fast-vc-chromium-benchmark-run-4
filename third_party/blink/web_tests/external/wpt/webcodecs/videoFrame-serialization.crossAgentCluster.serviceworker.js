ebb95544b5096643c47f095c817a9fcf022ce510
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-02-16 01:36:27 +0000

    Import wpt@5c23bc82588c19a0ae21358da8b056f0b5f1ccb4
    Using wpt-import in Chromium afbe9423edf11033f31dcdc12151be12b458d5d8.
    With Chromium commits locally applied on WPT:
    85af4a9a0d "usb: Use Mojo union to separate device open success from error codes"
    a801b78e4a "[Private Network Access] Test insecure fetches within workers loaded from blob urls"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    antoniosartori@chromium.org, arthursonzogni@chromium.org:
      external/wpt/content-security-policy
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I7cb865cdb90f83ad14fe10092fbf923ed18e2fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258025
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1106013}

--- third_party/blink/web_tests/external/wpt/webcodecs/videoFrame-serialization.crossAgentCluster.serviceworker.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/videoFrame-serialization.crossAgentCluster.serviceworker.js
@@ -1,6 +1,25 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const data = new Uint8Array([
+  1, 2, 3, 4, 5, 6, 7, 8,
+  9, 10, 11, 12, 13, 14, 15, 16,
+]);
 let received = new Map();
 self.onmessage = (e) => {
+  if (e.data == 'create-frame') {
+    let frameOrError = null;
+    try {
+      frameOrError = new VideoFrame(data, {
+        timestamp: 0,
+        codedWidth: 2,
+        codedHeight: 2,
+        format: 'RGBA',
+      });
+    } catch (error) {
+      frameOrError = error
+    }
+    e.source.postMessage(frameOrError);
+    return;
+  }
   if (e.data.hasOwnProperty('id')) {
     e.source.postMessage(
       received.get(e.data.id) ? 'RECEIVED' : 'NOT_RECEIVED');
