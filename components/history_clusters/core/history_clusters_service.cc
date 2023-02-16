68674052b616c8b0b914fb9e3c98119db8c4e5f7
Sophie Chang <sophiechang@chromium.org>
2023-02-16 16:21:02 +0000

    Add TestHistoryClustersService so HistoryClustersService can be more usable by consumers
    Bug: 1409691
    Change-Id: If1ce3a1856e7cabc6c281665005242dc0b6663bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257702
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106228}

--- components/history_clusters/core/history_clusters_service.cc
+++ components/history_clusters/core/history_clusters_service.cc
@@ -77,8 +77,6 @@ HistoryClustersService::HistoryClustersService(
                                   template_url_service,
                                   optimization_guide_decider,
                                   engagement_score_provider) {
-  DCHECK(history_service_);
-
   if (prefs && is_journeys_enabled_) {
     // Log whether the user has Journeys enabled if they are eligible for it.
     base::UmaHistogramBoolean(
@@ -86,7 +84,9 @@ HistoryClustersService::HistoryClustersService(
         prefs->GetBoolean(prefs::kVisible));
   }
 
-  visit_deletion_observer_.AttachToHistoryService(history_service);
+  if (history_service_) {
+    visit_deletion_observer_.AttachToHistoryService(history_service);
+  }
 
   backend_ = FileClusteringBackend::CreateIfEnabled();
   if (!backend_) {
