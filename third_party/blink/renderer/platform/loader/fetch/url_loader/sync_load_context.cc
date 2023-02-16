8c5f482faa93b6ed8190255dc17915eb65f07ab5
Kenichi Ishibashi <bashi@chromium.org>
2023-02-16 10:40:20 +0000

    Add a UseCounter for cross origin redirects with Authorization header
    To measure how often it happens.
    
    Bug: 1393520
    Change-Id: Idd8125250600381affc0f66349d77abdcb2dc9ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4235597
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106130}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/sync_load_context.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/sync_load_context.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
+#include "net/http/http_request_headers.h"
 #include "net/url_request/redirect_info.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/url_loader_completion_status.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/resource_load_info_notifier_wrapper.h"
 #include "third_party/blink/renderer/platform/loader/fetch/url_loader/sync_load_response.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
+#include "url/origin.h"
 
 namespace blink {
 
@@ -152,6 +154,9 @@ SyncLoadContext::SyncLoadContext(
   // Initialize the final URL with the original request URL. It will be
   // overwritten on redirects.
   response_->url = request->url;
+
+  has_authorization_header_ =
+      request->headers.HasHeader(net::HttpRequestHeaders::kAuthorization);
 }
 
 SyncLoadContext::~SyncLoadContext() {}
@@ -163,6 +168,12 @@ bool SyncLoadContext::OnReceivedRedirect(
     network::mojom::URLResponseHeadPtr head,
     std::vector<std::string>* removed_headers) {
   DCHECK(!Completed());
+
+  if (has_authorization_header_ &&
+      !url::IsSameOriginWith(response_->url, redirect_info.new_url)) {
+    response_->has_authorization_header_between_cross_origin_redirect_ = true;
+  }
+
   if (removed_headers) {
     // TODO(yoav): Get the actual PermissionsPolicy here to support selective
     // removal for sync XHR.
--- third_party/blink/renderer/platform/loader/fetch/url_loader/url_loader_client.h
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/url_loader_client.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/base/big_buffer.h"
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "services/network/public/mojom/referrer_policy.mojom-shared.h"
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-shared.h"
 #include "third_party/blink/public/platform/web_common.h"
 #include "third_party/blink/public/platform/web_url_request.h"
 #include "third_party/blink/public/platform/web_vector.h"
@@ -124,6 +125,11 @@ class BLINK_PLATFORM_EXPORT URLLoaderClient {
                        int64_t total_encoded_data_length,
                        uint64_t total_encoded_body_length,
                        int64_t total_decoded_body_length) {}
+
+  // Counts a WebFeature use.
+  // TODO(https://crbug.com/1393520): Remove this method once we get enough
+  // stats to make a decision.
+  virtual void CountFeature(blink::mojom::WebFeature) {}
 
   // Value passed to DidFinishLoading when total encoded data length isn't
   // known.
