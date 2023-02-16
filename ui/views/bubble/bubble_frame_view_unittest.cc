a6595ec793817e98210bb677d38bbd52eb2a4ba1
Jayson Adams <shrike@chromium.org>
2023-02-16 22:27:03 +0000

    [Mac Views Unittests] Fix BFVT_.IgnorePyUnintendedClicksMin for macOS 13
    BubbleFrameViewTest.IgnorePossiblyUnintendedClicksMinimize fails on
    macOS 13 (Ventura). Timing has apparently changed for operations like
    window miniaturize and deminiaturize to complete natively, causing the
    test to fail. This cl changes the test to wait for state to finish
    changing in the native environment before proceeding.
    
    Bug: 1373040
    
      --gtest_filter= BubbleFrameViewTest.IgnorePossiblyUnintendedClicksMinimize
    
    Test: out/Default/views_unittests \
    Change-Id: I7ea01b430efb6507bd74f364b9db8799c1f72486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261129
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106468}

--- ui/views/bubble/bubble_frame_view_unittest.cc
+++ ui/views/bubble/bubble_frame_view_unittest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
+#include "ui/views/widget/widget_interactive_uitest_utils.h"
 #include "ui/views/window/dialog_client_view.h"
 
 namespace views {
@@ -1359,11 +1360,15 @@ TEST_F(BubbleFrameViewTest, IgnorePossiblyUnintendedClicksMinimize) {
                                   ui::EF_NONE, ui::EF_NONE));
   EXPECT_FALSE(bubble->IsClosed());
 
+  views::test::PropertyWaiter minimize_waiter(
+      base::BindRepeating(&Widget::IsMinimized, base::Unretained(bubble)),
+      true);
   test::ButtonTestApi(frame->minimize_)
       .NotifyClick(ui::MouseEvent(
           ui::ET_MOUSE_PRESSED, gfx::Point(), gfx::Point(),
           ui::EventTimeForNow() + base::Milliseconds(GetDoubleClickInterval()),
           ui::EF_NONE, ui::EF_NONE));
+  EXPECT_TRUE(minimize_waiter.Wait());
   EXPECT_TRUE(bubble->IsMinimized());
 }
 
