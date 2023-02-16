53997ca034781b8cb91413b4d433eba144f95fb2
Ewann Pelle <ewannpv@chromium.org>
2023-02-16 16:26:47 +0000

    [iOS][TabStrip] Add drag and tap metrics
    This CL adds 2 metrics:
    
    - IOS.TabStrip.DragInteraction: tracks whether the user moved a tabStrip
    tab to a new index.
    - IOS.TabStrip.TapInteraction: tracks whether the user tapped on a
    tabStrip tab that is not the active one.
    
    Bug: 1416508
    Change-Id: I59b80ef3ce8dd6d62a14d458627386f84e6e453c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4255185
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106231}

--- ios/chrome/browser/ui/tabs/tab_strip_controller.mm
+++ ios/chrome/browser/ui/tabs/tab_strip_controller.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/i18n/rtl.h"
 #import "base/mac/bundle_locations.h"
 #import "base/mac/foundation_util.h"
+#import "base/metrics/histogram_functions.h"
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
 #import "base/numerics/safe_conversions.h"
@@ -84,6 +85,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::UserMetricsAction;
 
 namespace {
+
+// Keys of the UMA IOS.TabStrip histograms.
+const char kUMATabStripDragInteractionHistogram[] =
+    "IOS.TabStrip.DragInteraction";
+const char kUMATabStripTapInteractionHistogram[] =
+    "IOS.TabStrip.TapInteraction";
 
 // Animation duration for tab animations.
 const NSTimeInterval kTabAnimationDuration = 0.25;
@@ -1050,6 +1057,8 @@ const CGFloat kSymbolSize = 18;
   int toIndex = _placeholderGapWebStateListIndex;
   DCHECK_NE(WebStateList::kInvalidIndex, toIndex);
   DCHECK_LT(toIndex, _webStateList->count());
+  base::UmaHistogramBoolean(kUMATabStripDragInteractionHistogram,
+                            fromIndex != toIndex);
 
   // Reset drag state variables before notifying the model that the tab moved.
   [self resetDragState];
@@ -1933,6 +1942,9 @@ const CGFloat kSymbolSize = 18;
   DCHECK_NE(WebStateList::kInvalidIndex, index);
   if (index == WebStateList::kInvalidIndex)
     return;
+
+  base::UmaHistogramBoolean(kUMATabStripTapInteractionHistogram,
+                            index != _webStateList->active_index());
 
   if ((ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) &&
       (_webStateList->active_index() != static_cast<int>(index))) {
