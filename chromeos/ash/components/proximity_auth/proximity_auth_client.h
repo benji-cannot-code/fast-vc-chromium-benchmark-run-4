773ad7da9bcdf36496cfe5c765434ab535377f91
Brittany Hartmire <bhartmire@google.com>
2023-02-16 00:10:52 +0000

    [SmartLock] Remove deprecated sign in challenge
    We no longer need to generate or send a sign in challenge now that
    Sign in with Smart Lock is deprecated
    
    TEST=existing unittests pass. manually tested with strongbad and pixel4a
    
    Change-Id: I46bfbeecb433c8e767e02e9309c8dc7a0217387d
    Bug: b/227674947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250880
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Curt Clemens <cclem@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105968}

--- chromeos/ash/components/proximity_auth/proximity_auth_client.h
+++ chromeos/ash/components/proximity_auth/proximity_auth_client.h
@@ -6,11 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_ASH_COMPONENTS_PROXIMITY_AUTH_PROXIMITY_AUTH_CLIENT_H_
 #define CHROMEOS_ASH_COMPONENTS_PROXIMITY_AUTH_PROXIMITY_AUTH_CLIENT_H_
 
-#include <memory>
-#include <string>
-
 #include "ash/public/cpp/smartlock_state.h"
-#include "base/functional/callback_forward.h"
 #include "chromeos/ash/components/proximity_auth/proximity_auth_pref_manager.h"
 #include "chromeos/ash/services/device_sync/proto/cryptauth_api.pb.h"
 
@@ -31,18 +27,6 @@ class ProximityAuthClient {
   // the screen is unlocked; otherwise, the auth attempt is rejected. An auth
   // attempt must be in progress before calling this function.
   virtual void FinalizeUnlock(bool success) = 0;
-
-  // Gets the wrapped challenge for the given |user_id| and |remote_public_key|
-  // of the user's remote device. The challenge binds to the secure channel
-  // using |channel_binding_data|.
-  // |callback| will be invoked when the challenge is acquired.
-  // TODO(b/227674947): Delete this method now that sign in with Smart Lock is
-  // deprecated.
-  virtual void GetChallengeForUserAndDevice(
-      const std::string& user_email,
-      const std::string& remote_public_key,
-      const std::string& channel_binding_data,
-      base::OnceCallback<void(const std::string& challenge)> callback) = 0;
 
   // Returns the manager responsible for EasyUnlock preferences.
   virtual ProximityAuthPrefManager* GetPrefManager() = 0;
