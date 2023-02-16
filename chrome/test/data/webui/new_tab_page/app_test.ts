ab5ccb30efc11b94607dcd7e5c3d52015f5af299
Paul Adedeji <pauladedeji@google.com>
2023-02-16 20:00:17 +0000

    [ntp][side-panel] Add aria-pressed to customize chrome button
    Change-Id: I191002e193488da157e68405b69b4b596fa342b1
    Bug: 1415555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257576
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106391}

--- chrome/test/data/webui/new_tab_page/app_test.ts
+++ chrome/test/data/webui/new_tab_page/app_test.ts
@@ -86,6 +86,10 @@ suite('NewTabPageAppTest', () => {
 
       // Assert.
       assertTrue(!!app.shadowRoot!.querySelector('ntp-customize-dialog'));
+      assertEquals(
+          'true',
+          $$<HTMLElement>(
+              app, '#customizeButton')!.getAttribute('aria-pressed'));
     });
 
     test('logs height', async () => {
@@ -232,6 +236,10 @@ suite('NewTabPageAppTest', () => {
       // Assert.
       assertDeepEquals(
           theme, app.shadowRoot!.querySelector('ntp-customize-dialog')!.theme);
+      assertEquals(
+          'true',
+          $$<HTMLElement>(
+              app, '#customizeButton')!.getAttribute('aria-pressed'));
     });
 
     test('setting theme updates ntp', async () => {
@@ -628,6 +636,10 @@ suite('NewTabPageAppTest', () => {
           1, metrics.count('NewTabPage.Click', NtpElement.CUSTOMIZE_BUTTON));
       assertEquals(
           1, metrics.count('NewTabPage.Click', NtpElement.CUSTOMIZE_DIALOG));
+      assertEquals(
+          'true',
+          $$<HTMLElement>(
+              app, '#customizeButton')!.getAttribute('aria-pressed'));
     });
 
     test('clicking OGB records click', () => {
@@ -833,6 +845,21 @@ suite('NewTabPageAppTest', () => {
               NtpCustomizeChromeEntryPoint.CUSTOMIZE_BUTTON));
       assertEquals(
           0, handler.getCallCount('incrementCustomizeChromeButtonOpenCount'));
+    });
+
+    test('clicking customize button is accessible', async () => {
+      callbackRouterRemote.setCustomizeChromeSidePanelVisibility(true);
+      await callbackRouterRemote.$.flushForTesting();
+      assertEquals(
+          'true',
+          $$<HTMLElement>(
+              app, '#customizeButton')!.getAttribute('aria-pressed'));
+      callbackRouterRemote.setCustomizeChromeSidePanelVisibility(false);
+      await callbackRouterRemote.$.flushForTesting();
+      assertEquals(
+          'false',
+          $$<HTMLElement>(
+              app, '#customizeButton')!.getAttribute('aria-pressed'));
     });
 
     suite('modules', () => {
