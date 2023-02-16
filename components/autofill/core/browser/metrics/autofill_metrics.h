f670c8e62c4bf5fc143af7c622eaf03c73e8d613
Nancy Wang <nancylingwang@chromium.org>
2023-02-16 02:22:17 +0000

    Revert "Add Autofill2.FormSummary UKM Event"
    This reverts commit 42a103ef3e51c457c9a0817fb8c48ec8dda29cb2.
    
    Reason for revert: 
    Break components_unittests failed because of:
    AutofillMetricsFromLogEventsTest.AddressSubmittedFormLogEvents
    AutofillMetricsFromLogEventsTest.AutofillFieldInfoMetricsEditedFieldWithoutFill
    https://ci.chromium.org/p/chromium/builders/ci/mac11-arm64-rel-tests
    https://ci.chromium.org/p/chromium/builders/ci/mac12-arm64-rel-tests
    
    Original change's description:
    > Add Autofill2.FormSummary UKM Event
    >
    > This CL is to record the information of a HTML form element on the web
    > page, and it is recorded at form submission or form destruction time.
    >
    > Autofill UKM Data Structure doc:
    > https://docs.google.com/document/d/1Tgp5Op4tGhf03lYT4CfWr5f_FjvcpqWhqY0qPJcR-cM/edit#heading=h.k5jx6iluw4yt
    > https://docs.google.com/document/d/1ZH0JbL6bES3cD4KqZWsGR6n8I-rhnkx6no6nQOgYq5w/.
    >
    > Bug: 1325851
    > Change-Id: Ide305536fffe7deb8795e5f58466f30a5a29cda2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193147
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Reviewed-by: Dominic Battré <battre@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1105892}
    
    Bug: 1325851
    Change-Id: I963bd069ef2cc018d986a0defd070efb84e58530
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258668
    Auto-Submit: Nancy Wang <nancylingwang@chromium.org>
    Owners-Override: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1106035}

--- components/autofill/core/browser/metrics/autofill_metrics.h
+++ components/autofill/core/browser/metrics/autofill_metrics.h
@@ -609,8 +609,6 @@ class AutofillMetrics {
     kMaxValue = kLeftEmpty
   };
 
-  using FormEventSet = DenseSet<FormEvent, NUM_FORM_EVENTS>;
-
   // Utility class for determining the seamlessness of a credit card fill.
   class CreditCardSeamlessness {
    public:
@@ -707,12 +705,6 @@ class AutofillMetrics {
                       ServerFieldType actual_type);
     void LogAutofillFieldInfoAtFormRemove(const FormStructure& form,
                                           const AutofillField& field);
-    void LogAutofillFormSummaryAtFormRemove(
-        const FormStructure& form_structure,
-        FormEventSet form_events,
-        bool is_in_any_main_frame,
-        const base::TimeTicks& initial_interaction_timestamp,
-        const base::TimeTicks& form_submitted_timestamp);
     void LogFormSubmitted(bool is_for_credit_card,
                           bool has_upi_vpa_field,
                           const DenseSet<FormType>& form_types,
@@ -1099,7 +1091,7 @@ class AutofillMetrics {
   static void LogTouchToFillCreditCardPerfectFilling(bool perfect_filling);
 
   struct LogCreditCardSeamlessnessParam {
-    const raw_ref<FormEventLoggerBase> event_logger;
+    const raw_ref<const FormEventLoggerBase> event_logger;
     const raw_ref<const FormStructure> form;
     const raw_ref<const AutofillField> field;
     const raw_ref<const base::flat_set<FieldGlobalId>> newly_filled_fields;
