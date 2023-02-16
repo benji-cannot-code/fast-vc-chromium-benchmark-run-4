39bdf9528e5b9be811f3e121e291e82fc1cac3bd
Lei Zhang <thestig@chromium.org>
2023-02-16 16:23:06 +0000

    Print Preview: Fix a variable name typo.
    Spell "restoration" correctly.
    
    Change-Id: I6b8807c94fc9b6dbaabca38c05c5c7d47b65e869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258166
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106229}

--- chrome/browser/resources/print_preview/ui/pages_settings.ts
+++ chrome/browser/resources/print_preview/ui/pages_settings.ts
@@ -158,7 +158,7 @@ export class PrintPreviewPagesSettingsElement extends
    * `PagesValue.ODDS` and `PagesValue.EVEN` become invalid due to a changed
    * page count.
    */
-  private resorationValue_: PagesValue = PagesValue.ALL;
+  private restorationValue_: PagesValue = PagesValue.ALL;
 
   override ready() {
     super.ready();
@@ -488,11 +488,11 @@ export class PrintPreviewPagesSettingsElement extends
     // they can be re-applied if the page count exceeds 1 again.
     if (this.selection_ !== PagesValue.CUSTOM) {
       if (current === 1) {
-        this.resorationValue_ = this.selection_;
+        this.restorationValue_ = this.selection_;
         this.setSelectedValue_(PagesValue.ALL);
       } else if (previous === 1) {
-        assert(this.resorationValue_ !== PagesValue.CUSTOM);
-        this.setSelectedValue_(this.resorationValue_);
+        assert(this.restorationValue_ !== PagesValue.CUSTOM);
+        this.setSelectedValue_(this.restorationValue_);
       }
     }
 
