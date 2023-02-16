7998fc133275455f41463ff613e523eb5071de60
Sergii Bykov <sbykov@google.com>
2023-02-16 10:30:30 +0000

    Add a screenshot remote command browsertest for kiosk.
    This browser test uses a fake screenshot delegate to skip a screenshot
    upload job.
    
    Test: browser_tests --gtest_filter=*ScreenshotWithRemoteCommand
    Bug: b:268321006
    Change-Id: Ic6735f1731e6248ad60ffa48f4d8298cb3ccae3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240560
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106126}

--- chrome/browser/ash/policy/remote_commands/device_command_screenshot_job.cc
+++ chrome/browser/ash/policy/remote_commands/device_command_screenshot_job.cc
@@ -49,9 +49,6 @@ const char* const kFileTypeHeaderName = "File-Type";
 // String constant signalling that the data segment contains screenshots.
 const char* const kFileTypeScreenshotFile = "screenshot_file";
 
-// String constant identifying the upload url field in the command payload.
-const char* const kUploadUrlFieldName = "fileUploadUrl";
-
 // Helper method to hide the |screen_index| and `std::make_pair` from the
 // |DeviceCommandScreenshotJob::Delegate|.
 void CallCollectAndUpload(
@@ -72,6 +69,10 @@ std::string CreatePayload(ResultCode result_code) {
 }
 
 }  // namespace
+
+// String constant identifying the upload url field in the command payload.
+constexpr char DeviceCommandScreenshotJob::kUploadUrlFieldName[] =
+    "fileUploadUrl";
 
 DeviceCommandScreenshotJob::DeviceCommandScreenshotJob(
     std::unique_ptr<Delegate> screenshot_delegate)
--- chrome/browser/ash/policy/remote_commands/device_commands_factory_ash.h
+++ chrome/browser/ash/policy/remote_commands/device_commands_factory_ash.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "chrome/browser/ash/policy/remote_commands/device_command_screenshot_job.h"
 #include "chrome/browser/ash/policy/remote_commands/device_command_start_crd_session_job.h"
 #include "components/policy/core/common/remote_commands/remote_commands_factory.h"
 
@@ -30,11 +31,18 @@ class DeviceCommandsFactoryAsh : public RemoteCommandsFactory {
       enterprise_management::RemoteCommand_Type type,
       RemoteCommandsService* service) override;
 
+  static void set_commands_for_testing(bool device_commands_test);
+
  private:
+  // TODO(b/269432279): Consider removing when test uses a local upload server
+  static bool device_commands_test_;
+
   DeviceCloudPolicyManagerAsh* policy_manager_;
   std::unique_ptr<DeviceCommandStartCrdSessionJob::Delegate> crd_host_delegate_;
 
   DeviceCommandStartCrdSessionJob::Delegate* GetCrdHostDelegate();
+  std::unique_ptr<DeviceCommandScreenshotJob::Delegate>
+  CreateScreenshotDelegate();
 };
 
 }  // namespace policy
