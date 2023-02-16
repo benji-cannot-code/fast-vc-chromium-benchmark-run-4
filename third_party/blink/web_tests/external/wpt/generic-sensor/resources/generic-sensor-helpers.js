e972b6683ae455254aa4ec85b00fa7aab02c35ad
Wei4 Wang <wei4.wang@intel.com>
2023-02-16 02:06:33 +0000

    [GenericSensor] Enable visibility test in generic-sensor-tests.js
    The visibility test in generic-sensor-tests.js is disabled before,
    because there is no infrastructure for simulating loss of visibility.
    This CL enables the visibility test by simulating loss of visibility
    through minimizing the window.
    
    Bug: none
    Change-Id: Ifa008f2af9c9bd2e3e76f350dcf547b9ef7002b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4248683
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1106026}

--- third_party/blink/web_tests/external/wpt/generic-sensor/resources/generic-sensor-helpers.js
+++ third_party/blink/web_tests/external/wpt/generic-sensor/resources/generic-sensor-helpers.js
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 async function loadChromiumResources() {
   await loadScript('/resources/testdriver.js');
   await loadScript('/resources/testdriver-vendor.js');
+  await loadScript('/page-visibility/resources/window_state_context.js');
   await import('/resources/chromium/generic_sensor_mocks.js');
 }
 
