a0aec7177b3147517a5866e2d5d8432a06e1b0f5
Solomon Kinard <solomonkinard@chromium.org>
2023-02-16 04:31:26 +0000

    Extensions: Fixit: Use '= default' to define a trivial *structor
    This CL was uploaded by git cl split.
    
    R=fdegros@chromium.org
    
    Bug: 1416739
    Change-Id: I0e3a59ebe54ffe2d300c1a70b5b5b538332a90be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258273
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106060}

--- extensions/browser/api/file_handlers/app_file_handler_util.cc
+++ extensions/browser/api/file_handlers/app_file_handler_util.cc
@@ -238,7 +238,7 @@ void WritableFileChecker::Check() {
   }
 }
 
-WritableFileChecker::~WritableFileChecker() {}
+WritableFileChecker::~WritableFileChecker() = default;
 
 void WritableFileChecker::TaskDone() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
