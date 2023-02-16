36a1d6e75e7f242c8f4724699016e9d74a97a547
Yunke Zhou <yunkez@google.com>
2023-02-16 15:48:43 +0000

    Cryptohome recovery: Add util function to log AuthenticationError
    Error message will be encoded in the server response for malformed or
    invalid requests. In those cases, we will get error info from `get_cryptohome_code` as CRYPTOHOME_ERROR_RECOVERY_TRANSIENT or
    CRYPTOHOME_ERROR_RECOVERY_FATAL will be returned, instead of from
    `get_cryptohome_recovery_server_error`.
    
    Bug: b:257073746
    Change-Id: I773d778901aa715dcce0b337429a963cab755978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4256445
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106215}

--- chromeos/ash/components/login/auth/public/authentication_error.cc
+++ chromeos/ash/components/login/auth/public/authentication_error.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chromeos/ash/components/login/auth/public/authentication_error.h"
+#include "base/strings/strcat.h"
+#include "base/strings/stringprintf.h"
 
 namespace ash {
 
@@ -23,6 +25,28 @@ AuthenticationError::~AuthenticationError() = default;
 void AuthenticationError::ResolveToFailure(
     AuthFailure::FailureReason auth_failure_reason) {
   auth_failure_ = AuthFailure{auth_failure_reason};
+}
+
+std::string AuthenticationError::ToDebugString() const {
+  std::string result = "AuthenticationError: ";
+  if (origin_ == Origin::kCryptohome) {
+    base::StrAppend(&result, {base::StringPrintf(
+                                 "Origin: Cryptohome, CryptohomeErrorCode: %d",
+                                 static_cast<int>(get_cryptohome_code()))});
+  } else if (origin_ == Origin::kChrome) {
+    base::StrAppend(&result, {base::StringPrintf(
+                                 "Origin: Chrome, FailureReason: %d",
+                                 static_cast<int>(get_resolved_failure()))});
+    if (get_resolved_failure() ==
+        AuthFailure::FailureReason::CRYPTOHOME_RECOVERY_SERVICE_ERROR) {
+      base::StrAppend(
+          &result,
+          {base::StringPrintf(
+              ", CryptohomeRecoveryServerStatusCode: %d",
+              static_cast<int>(get_cryptohome_recovery_server_error()))});
+    }
+  }
+  return result;
 }
 
 }  // namespace ash
--- chromeos/ash/components/login/auth/public/authentication_error.h
+++ chromeos/ash/components/login/auth/public/authentication_error.h
@@ -48,6 +48,8 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_LOGIN_AUTH_PUBLIC)
     return cryptohome_code_;
   }
 
+  std::string ToDebugString() const;
+
  private:
   Origin origin_;
   // Cryptohome-specific fields:
