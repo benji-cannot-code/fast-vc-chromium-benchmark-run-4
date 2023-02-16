e04e009c798a6813febe3e208c780a7371f937e8
Maksim Sisov <msisov@igalia.com>
2023-02-16 22:00:03 +0000

    fixup! [ios]: fix native GMB configuration.
    Bug: 1413716, 1413716, 1413718
    Change-Id: Ib5d981b0b1384e011f4707ff3ca8244841504388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4259810
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106450}

--- gpu/ipc/host/gpu_memory_buffer_support.cc
+++ gpu/ipc/host/gpu_memory_buffer_support.cc
@@ -58,7 +58,7 @@ GpuMemoryBufferConfigurationSet GetNativeGpuMemoryBufferConfigurations(
         configurations.insert(gfx::BufferUsageAndFormat(usage, format));
     }
   }
-#endif  // BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) ||
+#endif  // BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_WIN) ||
         // BUILDFLAG(IS_ANDROID)
 
   return configurations;
@@ -68,7 +68,7 @@ bool GetImageNeedsPlatformSpecificTextureTarget(gfx::BufferFormat format,
                                                 gfx::BufferUsage usage) {
   if (!NativeBufferNeedsPlatformSpecificTextureTarget(format))
     return false;
-#if BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || \
+#if BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_WIN) || \
     BUILDFLAG(IS_ANDROID)
   GpuMemoryBufferSupport support;
   GpuMemoryBufferConfigurationSet native_configurations =
