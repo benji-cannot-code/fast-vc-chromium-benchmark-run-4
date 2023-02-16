5eb84b072161c6d71583b82459a6e6af77bcf208
Solomon Kinard <solomonkinard@chromium.org>
2023-02-16 18:52:14 +0000

    Extensions: Fixit: Use '= default' to define a trivial *structor
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 1416739
    Change-Id: Ie7f22240665fa5fcb9c3d35997d0855c118c2b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258275
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106335}

--- extensions/browser/api/storage/value_store_cache.cc
+++ extensions/browser/api/storage/value_store_cache.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-ValueStoreCache::~ValueStoreCache() {}
+ValueStoreCache::~ValueStoreCache() = default;
 
 void ValueStoreCache::ShutdownOnUI() {}
 
