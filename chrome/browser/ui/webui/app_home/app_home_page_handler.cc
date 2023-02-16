43d76281e9935104ccaa1bf41bcaa3b34b0aa575
Camden King <camdenking@google.com>
2023-02-16 19:21:39 +0000

    [AppHome] Making apps size increase by 10% when focused on
    Bug: 1350406, b/269181435
    Change-Id: I7e2c56349749e6a2e3b0126d53cef398c1e559b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257848
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Camden King <camdenking@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106362}

--- chrome/browser/ui/webui/app_home/app_home_page_handler.cc
+++ chrome/browser/ui/webui/app_home/app_home_page_handler.cc
@@ -65,7 +65,7 @@ namespace webapps {
 
 namespace {
 
-const int kWebAppIconSize = 64;
+const int kWebAppIconSize = 128;
 
 // The Youtube app is incorrectly hardcoded to be a 'bookmark app'. However, it
 // is a platform app.
