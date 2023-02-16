c5c7fa513f86d7006b82a6ece2e6458591268a15
Iman Saboori <isaboori@google.com>
2023-02-16 20:11:52 +0000

    Revert "[webauthn] Finish wiring up large blob on Windows"
    This reverts commit a8abbba4bb54d380ba5681d778685ed7ace6de7e.
    
    Reason for revert: [Build sheriff] WebAuthnBrowserTest.WinLargeBlob test fails: crbug.com/1416977
    
    Original change's description:
    > [webauthn] Finish wiring up large blob on Windows
    >
    > Wire the large blob webauthn extension on Windows for GetAssertion
    > requests.
    >
    > Also, work around a bug on Windows where large blob = required
    > requests will happily create a platform credential without large blob by
    > rejecting such requests before they make it to the Windows API and
    > forcing the attachment to cross-platform otherwise.
    >
    > Bug: 1406632
    > Change-Id: I378a226067cea22567fd3b04f8ef3c5a9a83326e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4251318
    > Reviewed-by: Adam Langley <agl@chromium.org>
    > Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1106293}
    
    Bug: 1406632
    Change-Id: I1159eec6b8a680693c65025a0872ba1af6f47064
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4262689
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1106397}

--- device/fido/win/type_conversions.h
+++ device/fido/win/type_conversions.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "device/fido/authenticator_get_assertion_response.h"
 #include "device/fido/authenticator_make_credential_response.h"
-#include "device/fido/ctap_get_assertion_request.h"
 #include "device/fido/fido_constants.h"
 #include "device/fido/fido_types.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -34,7 +33,7 @@ COMPONENT_EXPORT(DEVICE_FIDO)
 absl::optional<AuthenticatorGetAssertionResponse>
 ToAuthenticatorGetAssertionResponse(
     const WEBAUTHN_ASSERTION& credential_attestation,
-    const CtapGetAssertionOptions& request_options);
+    const std::vector<PublicKeyCredentialDescriptor>& allow_list);
 
 COMPONENT_EXPORT(DEVICE_FIDO)
 uint32_t ToWinUserVerificationRequirement(
