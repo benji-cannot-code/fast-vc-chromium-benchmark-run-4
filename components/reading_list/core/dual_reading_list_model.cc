6ab7d8c273af7145cc075a64de9487a82135fcfa
Mahmoud Rashad <mmrashad@google.com>
2023-02-16 15:46:37 +0000

    ReadingList: Notify DualReadingListModel observers about SyncRemoveEntry
    This CL makes DualReadingListModel observe its two underlying
    ReadingListModelImpl instances and notify its observers. It also
    introduces a flag on DualReadingListModel that indicates whether
    a removing entry operation is currently performing on its two underlying
    instances. Then, it makes DualReadingListModel forward the notifications
    comes from them only if the flag is `false`, which means the removal
    came from sync.
    
    Bug: 1402196
    Change-Id: I289b6fb64236fc5b78a710efa6c75c1c2d318ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4247620
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1106213}

--- components/reading_list/core/dual_reading_list_model.cc
+++ components/reading_list/core/dual_reading_list_model.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/reading_list/core/dual_reading_list_model.h"
 
+#include "base/auto_reset.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
 #include "base/stl_util.h"
@@ -180,17 +181,17 @@ void DualReadingListModel::RemoveEntryByURL(const GURL& url) {
     return;
   }
 
-  for (auto& observer : observers_) {
-    observer.ReadingListWillRemoveEntry(this, url);
+  NotifyObserversWithWillRemoveEntry(url);
+
+  {
+    base::AutoReset<bool> auto_reset_ongoing_remove_entry_by_url(
+        &ongoing_remove_entry_by_url_, true);
+    local_or_syncable_model_->RemoveEntryByURL(url);
+    account_model_->RemoveEntryByURL(url);
   }
 
-  local_or_syncable_model_->RemoveEntryByURL(url);
-  account_model_->RemoveEntryByURL(url);
-
-  for (auto& observer : observers_) {
-    observer.ReadingListDidRemoveEntry(this, url);
-    observer.ReadingListDidApplyChanges(this);
-  }
+  NotifyObserversWithDidRemoveEntry(url);
+  NotifyObserversWithDidApplyChanges();
 }
 
 void DualReadingListModel::SetReadStatusIfExists(const GURL& url, bool read) {
@@ -256,6 +257,28 @@ void DualReadingListModel::ReadingListModelLoaded(
   }
 }
 
+void DualReadingListModel::ReadingListWillRemoveEntry(
+    const ReadingListModel* model,
+    const GURL& url) {
+  if (!ongoing_remove_entry_by_url_) {
+    NotifyObserversWithWillRemoveEntry(url);
+  }
+}
+
+void DualReadingListModel::ReadingListDidRemoveEntry(
+    const ReadingListModel* model,
+    const GURL& url) {
+  if (!ongoing_remove_entry_by_url_) {
+    NotifyObserversWithDidRemoveEntry(url);
+  }
+}
+
+void DualReadingListModel::ReadingListDidApplyChanges(ReadingListModel* model) {
+  if (!ongoing_remove_entry_by_url_) {
+    NotifyObserversWithDidApplyChanges();
+  }
+}
+
 DualReadingListModel::StorageStateForTesting
 DualReadingListModel::GetStorageStateForURLForTesting(const GURL& url) {
   const bool exists_in_local_or_syncable_model =
@@ -272,6 +295,24 @@ DualReadingListModel::GetStorageStateForURLForTesting(const GURL& url) {
     return StorageStateForTesting::kExistsInAccountModelOnly;
   }
   return StorageStateForTesting::kNotFound;
+}
+
+void DualReadingListModel::NotifyObserversWithWillRemoveEntry(const GURL& url) {
+  for (auto& observer : observers_) {
+    observer.ReadingListWillRemoveEntry(this, url);
+  }
+}
+
+void DualReadingListModel::NotifyObserversWithDidRemoveEntry(const GURL& url) {
+  for (auto& observer : observers_) {
+    observer.ReadingListDidRemoveEntry(this, url);
+  }
+}
+
+void DualReadingListModel::NotifyObserversWithDidApplyChanges() {
+  for (auto& observer : observers_) {
+    observer.ReadingListDidApplyChanges(this);
+  }
 }
 
 }  // namespace reading_list
