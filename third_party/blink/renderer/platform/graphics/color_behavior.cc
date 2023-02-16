98d3edc41be5ff3a8242a4f87f45feb001463383
Kevin Lubick <kjlubick@google.com>
2023-02-16 12:55:02 +0000

    Fix includes of SkICC.h
    I was planning on moving include/core/SkICC.h to include/encode/SkICC.h
    and as a precursor, I looked at existing uses.
    
    It appears 3 files included SkICC.h but did not actually need it.
    The one file that needed it was not including it.
    
    While I was fixing things up, it seemed some of these files
    were depending on SkCMS but not actually including it.
    
    Bug: skia:13502
    Change-Id: I8adc1b585b99de62726669edb54ffa3a20730389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4256251
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106161}

--- third_party/blink/renderer/platform/graphics/color_behavior.cc
+++ third_party/blink/renderer/platform/graphics/color_behavior.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "third_party/blink/renderer/platform/graphics/bitmap_image_metrics.h"
-#include "third_party/skia/include/core/SkICC.h"
 #include "ui/gfx/icc_profile.h"
 #include "ui/gfx/skia_color_space_util.h"
 
--- ui/gfx/skia_color_space_util.h
+++ ui/gfx/skia_color_space_util.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_GFX_SKIA_COLOR_SPACE_UTIL_H_
 
 #include "third_party/skia/include/core/SkColorSpace.h"
-#include "third_party/skia/include/core/SkICC.h"
 #include "third_party/skia/include/core/SkM44.h"
+#include "third_party/skia/modules/skcms/skcms.h"
 #include "ui/gfx/color_space_export.h"
 
 namespace gfx {
