a136d13ac7cf044e8af75199346b53180a59ad99
Stefan Zager <szager@chromium.org>
2023-02-16 21:05:44 +0000

    Filter for complete class definitions in StackAllocatedChecker
    FindBadConstructsConsumer::VisitCXXRecordDecl was only called with
    complete class definitions in previous versions of clang, but in the
    latest version it is called repeatedly with incomplete definitions.
    The incomplete definitions typically have complete information about member variables and their types, but not the IsStackAllocatedTypeMarker
    type alias. This causes incorrect and redundant error reporting when
    a STACK_ALLOCATED class has member variables that are also
    STACK_ALLOCATED.
    
    Change-Id: I41f6d4adc4573b5d9292a1564768967b1cb9f049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258167
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106422}

--- tools/clang/plugins/StackAllocatedChecker.h
+++ tools/clang/plugins/StackAllocatedChecker.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TOOLS_CLANG_PLUGINS_STACKALLOCATEDCHECKER_H_
 
 #include <map>
-#include <vector>
 
 namespace clang {
 class CompilerInstance;
