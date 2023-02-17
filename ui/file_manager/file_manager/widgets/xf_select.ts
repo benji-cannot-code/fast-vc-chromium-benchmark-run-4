26f68f83c3cf881c38939866c0625bf2dbca41de
Bo Majewski <majewski@chromium.org>
2023-02-17 20:57:25 +0000

    [Files]: Adds ability to specify the alignment of the dropdown menu.
    By default the dropdown elements in xf-select item are center aligned.
    We add a property that allows one to left, center or right align these
    items. This is used in the xf-search-options to left align all the
    menus. To deal with rtl conventions these are named start, center and
    end, rather than left, center and right.
    
    Bug: 269691652
    Change-Id: Iae25cc94ff4be80cd665b7e8cfd12e89f786d28d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4263628
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106974}

--- ui/file_manager/file_manager/widgets/xf_select.ts
+++ ui/file_manager/file_manager/widgets/xf_select.ts
@@ -78,6 +78,12 @@ export class XfSelect extends XfBase {
    */
   @property({type: String, reflect: true}) value: string = '';
 
+  /**
+   * The alignment of items in the dropdown menu. Can be one of
+   * 'start', 'center', 'end'.
+   */
+  @property({type: String, reflect: true}) menuAlignment: string = 'center';
+
   static get events() {
     return {
       /** emits when the currently selected option changed. */
@@ -162,10 +168,11 @@ export class XfSelect extends XfBase {
    * Returns a template of the dropdown which shows available choices.
    */
   private renderDropdown_() {
+    const alignment = this.menuAlignment || 'center';
     return html`<cr-action-menu>
         ${this.options.map((option, index) => html`
           <cr-button
-              class="dropdown-item"
+              class="dropdown-item dropdown-item-${alignment}"
               role="menuitem"
               @click=${() => this.onOptionSelected_(index)}
               ?selected=${this.selectedOption_!.value === option.value}>
@@ -352,6 +359,15 @@ function getCSS(): CSSResultGroup {
     }
     cr-button.dropdown-item {
       --focus-shadow-color: none;
+    }
+    cr-button.dropdown-item-center {
+      justify-content: center;
+    }
+    cr-button.dropdown-item-start {
+      justify-content: start;
+    }
+    cr-button.dropdown-item-end {
+      justify-content: end;
     }
   `;
 }
