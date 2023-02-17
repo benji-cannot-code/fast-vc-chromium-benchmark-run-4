962d054cf42bdf7a8f5ae2823ebdfa4356deb360
Solomon Kinard <solomonkinard@chromium.org>
2023-02-17 17:53:46 +0000

    Extensions: Fixit: Use '= default' to define a trivial *structor
    This CL was uploaded by git cl split.
    
    R=wjmaclean@chromium.org
    
    Bug: 1416739
    Change-Id: If3ec4364b15eabc37e5bdb1067a98522bd6e75ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258902
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106856}

--- extensions/browser/api/guest_view/web_view/web_view_internal_api.cc
+++ extensions/browser/api/guest_view/web_view/web_view_internal_api.cc
@@ -969,9 +969,11 @@ ExtensionFunction::ResponseAction WebViewInternalStopFunction::Run() {
   return RespondNow(NoArguments());
 }
 
-WebViewInternalSetAudioMutedFunction::WebViewInternalSetAudioMutedFunction() {}
+WebViewInternalSetAudioMutedFunction::WebViewInternalSetAudioMutedFunction() =
+    default;
 
-WebViewInternalSetAudioMutedFunction::~WebViewInternalSetAudioMutedFunction() {}
+WebViewInternalSetAudioMutedFunction::~WebViewInternalSetAudioMutedFunction() =
+    default;
 
 ExtensionFunction::ResponseAction WebViewInternalSetAudioMutedFunction::Run() {
   std::unique_ptr<web_view_internal::SetAudioMuted::Params> params(
@@ -982,9 +984,11 @@ ExtensionFunction::ResponseAction WebViewInternalSetAudioMutedFunction::Run() {
   return RespondNow(NoArguments());
 }
 
-WebViewInternalIsAudioMutedFunction::WebViewInternalIsAudioMutedFunction() {}
+WebViewInternalIsAudioMutedFunction::WebViewInternalIsAudioMutedFunction() =
+    default;
 
-WebViewInternalIsAudioMutedFunction::~WebViewInternalIsAudioMutedFunction() {}
+WebViewInternalIsAudioMutedFunction::~WebViewInternalIsAudioMutedFunction() =
+    default;
 
 ExtensionFunction::ResponseAction WebViewInternalIsAudioMutedFunction::Run() {
   std::unique_ptr<web_view_internal::IsAudioMuted::Params> params(
@@ -995,9 +999,11 @@ ExtensionFunction::ResponseAction WebViewInternalIsAudioMutedFunction::Run() {
   return RespondNow(OneArgument(base::Value(web_contents->IsAudioMuted())));
 }
 
-WebViewInternalGetAudioStateFunction::WebViewInternalGetAudioStateFunction() {}
+WebViewInternalGetAudioStateFunction::WebViewInternalGetAudioStateFunction() =
+    default;
 
-WebViewInternalGetAudioStateFunction::~WebViewInternalGetAudioStateFunction() {}
+WebViewInternalGetAudioStateFunction::~WebViewInternalGetAudioStateFunction() =
+    default;
 
 ExtensionFunction::ResponseAction WebViewInternalGetAudioStateFunction::Run() {
   std::unique_ptr<web_view_internal::GetAudioState::Params> params(
