184054b0b20f3d4197ad868ac1ce7efbd2acadaf
Danila Kuzmin <dkuzmin@google.com>
2023-02-17 12:21:49 +0000

    [OOBE]: Remove ShowOobeUI from CoreOobeHandler
    CoreOobeHandler::ShowOobeUI was used to update the class of the top
    level oobe html document that differentiates "oobe" and "login" flows.
    However this class is also set during OobeUI construction and shouldn't
    be modified.
    
    Bug: b:266930990
    Change-Id: I77896fd88b6d829f55e2d9f2429fcccc59d5baae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231981
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1106719}

--- chrome/browser/ui/webui/ash/login/core_oobe_handler.h
+++ chrome/browser/ui/webui/ash/login/core_oobe_handler.h
@@ -79,9 +79,6 @@ class CoreOobeHandler : public BaseWebUIHandler,
   // ui::EventSource implementation:
   ui::EventSink* GetEventSink() override;
 
-  // Show or hide OOBE UI.
-  void ShowOobeUI(bool show);
-
  private:
   // CoreOobeView implementation:
   void ShowScreenWithData(const OobeScreenId& screen,
--- chrome/browser/ui/webui/ash/login/oobe_ui.h
+++ chrome/browser/ui/webui/ash/login/oobe_ui.h
@@ -81,9 +81,6 @@ class OobeUI : public ui::MojoWebUIController {
 
   bool IsJSReady(base::OnceClosure display_is_ready_callback);
 
-  // Shows or hides OOBE UI elements.
-  void ShowOobeUI(bool show);
-
   gfx::NativeView GetNativeView();
 
   gfx::NativeWindow GetTopLevelNativeWindow();
