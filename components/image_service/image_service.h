1b39aae9db0b5b705539a27d51defd62e24fc0e4
Tommy C. Li <tommycli@chromium.org>
2023-02-17 22:47:36 +0000

    [image_service] Add in-component mojom WebUI handler
    This CL adds a mojom WebUI handler within the component.
    
    It also changes the BUILD.gn from a component to a source_set.
    
    The documentation on components suggests that this directory is too
    small to be a component. It also makes exporting mojom structs give a
    whole bunch of linker errors. I think it's just best to use source_set.
    
    https://chromium.googlesource.com/chromium/src/+/main/docs/component_build.md#component-granularity
    
    Bug: b/244507194, 1367485, b/248367751
    Change-Id: I4339fd99bf254cd446b8e30f03471fc5e348b213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265691
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107040}

--- components/image_service/image_service.h
+++ components/image_service/image_service.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/component_export.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "components/image_service/mojom/image_service.mojom.h"
@@ -22,7 +21,7 @@ namespace image_service {
 
 // Used to get the image URL associated with a cluster. It doesn't actually
 // fetch the image, that's up to the UI to do.
-class COMPONENT_EXPORT(IMAGE_SERVICE) ImageService : public KeyedService {
+class ImageService : public KeyedService {
  public:
   using ResultCallback = base::OnceCallback<void(const GURL& image_url)>;
 
