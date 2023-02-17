dbca44a678a079edd180116b9a83fb17ddfe67a4
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-02-17 10:58:13 +0000

    Clear DOMStorage before test to avoid leakage from other tests
    Bug: 1410608
    Change-Id: Iab287d0e236714bc2bc3d4cd346791431aa39f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4264148
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106706}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/dom-storage-get-items-by-origin.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/dom-storage-get-items-by-origin.js
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   const securityOrigin = (await dp.Page.getResourceTree()).result.frameTree.frame.securityOrigin;
   const storageId = {securityOrigin, isLocalStorage: true};
+  // clear storage to avoid leakage from other tests
+  await dp.DOMStorage.clear({storageId});
 
   const items = (await dp.DOMStorage.getDOMStorageItems({storageId})).result;
 
