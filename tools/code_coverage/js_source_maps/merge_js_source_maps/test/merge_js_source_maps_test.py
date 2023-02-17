aba0aa1991f0d86c5275b7bdbeb451d32cbea6cf
dpapad <dpapad@chromium.org>
2023-02-17 20:55:23 +0000

    WebUI: Address TODO in merge_js_source_maps() regarding output folder.
    The code was previously assuming that the output directory must be the
    parent directory of the root dir specified within input manifest files.
    Adding an explicit --out-dir parameter instead, to allow callers to
    explicitly specify the output directory.
    
    Further improvements will be possible after c/b/r/new_tab_page/BUILD.gn
    which is currently the only caller of merge_js_source_maps() is migrated
    to build_webui().
    
    This is in preparation of integrating merge_js_source_maps() in
    build_webui().
    
    Bug: 1416356,1337530
    Change-Id: I17c23db18089cb335604748f4a1260088643b8a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261595
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106972}

--- tools/code_coverage/js_source_maps/merge_js_source_maps/test/merge_js_source_maps_test.py
+++ tools/code_coverage/js_source_maps/merge_js_source_maps/test/merge_js_source_maps_test.py
@@ -75,6 +75,8 @@ class MergeSourceMapsTest(unittest.TestCase):
         str(input_file_name),
         '--outputs',
         str(output_file_name),
+        '--out-dir',
+        'tsc',
     ])
 
     source_map = None
@@ -174,6 +176,8 @@ class MergeSourceMapsTest(unittest.TestCase):
         str(output_file_name),
         '--manifest-files',
         str(manifest_file),
+        '--out-dir',
+        'tsc',
     ])
 
     manifest_file_contents = '{"base_dir":"tsc"}'
