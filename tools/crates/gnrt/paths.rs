342a2bf9ee77e0d7b0f34830befdc8c145c477e4
Collin Baker <collinbaker@chromium.org>
2023-02-17 21:55:25 +0000

    Add gnrt_config.toml for extra std build configuration
    Instead of hard-coding these into gnrt, which necessitates rebuilding
    it for any cfg changes, this adds a gnrt_config.toml to supply these
    options at runtime.
    
    This CL also adds a couple libc cfgs required as of the latest Rust
    roll, and adds an env definition for libtest (which was blocked on
    https://github.com/rust-lang/rust/pull/106767).
    
    Bug: 1368806
    Change-Id: I542485b9cbed3f2a6fa7c2e72cb9df65da417281
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-dbg,android-rust-arm-rel,linux-rust-x64-dbg,linux-rust-x64-rel
    Change-Id: I542485b9cbed3f2a6fa7c2e72cb9df65da417281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261814
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107012}

--- tools/crates/gnrt/paths.rs
+++ tools/crates/gnrt/paths.rs
@@ -21,6 +21,7 @@ pub struct ChromiumPaths {
     pub rust_src: &'static Path,
     pub rust_src_vendor: &'static Path,
     pub rust_std: &'static Path,
+    pub std_config_file: &'static Path,
     pub std_build: &'static Path,
     pub std_fake_root: &'static Path,
 }
@@ -38,6 +39,7 @@ impl ChromiumPaths {
             rust_src: check_path(&cur_dir, RUST_SRC_DIR)?,
             rust_src_vendor: check_path(&cur_dir, RUST_SRC_VENDOR_DIR)?,
             rust_std: check_path(&cur_dir, RUST_STD_DIR)?,
+            std_config_file: check_path(&cur_dir, STD_CONFIG_FILE)?,
             std_build: check_path(&cur_dir, STD_BUILD_DIR)?,
             std_fake_root: check_path(&cur_dir, STD_FAKE_ROOT)?,
         })
@@ -55,7 +57,7 @@ impl ChromiumPaths {
 
 fn check_path<'a>(root: &Path, p_str: &'a str) -> io::Result<&'a Path> {
     let p = Path::new(p_str);
-    if !root.join(p).is_dir() {
+    if !root.join(p).exists() {
         return Err(io::Error::new(
             io::ErrorKind::Other,
             format!(
@@ -72,5 +74,6 @@ static RUST_THIRD_PARTY_DIR: &str = "third_party/rust";
 static RUST_SRC_DIR: &str = "third_party/rust-toolchain/lib/rustlib/src/rust";
 static RUST_SRC_VENDOR_DIR: &str = "third_party/rust-toolchain/lib/rustlib/src/rust/vendor";
 static RUST_STD_DIR: &str = "third_party/rust-toolchain/lib/rustlib/src/rust/library/std";
+static STD_CONFIG_FILE: &str = "build/rust/std/gnrt_config.toml";
 static STD_BUILD_DIR: &str = "build/rust/std/rules";
 static STD_FAKE_ROOT: &str = "build/rust/std/fake_root";
