94e7c8a5cd15b1c51e08dc289bbab0979dd1dbe5
andrewxu <andrewxu@chromium.org>
2023-02-17 23:14:58 +0000

    [Notification Drag] Implement image notification drag - Part 5
    In this CL, if a notification's large image is backed by a file,
    the file path is attached to the drag-and-drop data. In contrast, if
    the notification's large image is not backed by a file, the image
    binary data is attached to the drag-and-drop data, which is implemented
    by the previous CL.
    
    This CL also adds a field in `message_center::Notification` to indicate
    the file path to the notification's big image if this image is file
    backed. In the future, the screen capture notification will set this
    field.
    
    Bug: b/264934296
    Change-Id: I10ef101a7ba53cb55a813070118b222dce0f6dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225651
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107054}

--- ui/message_center/public/cpp/notification.h
+++ ui/message_center/public/cpp/notification.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/files/file_path.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/time/time.h"
 #include "build/chromeos_buildflags.h"
@@ -125,6 +126,11 @@ class MESSAGE_CENTER_PUBLIC_EXPORT RichNotificationData {
 
   // Large image to display on the notification. Optional.
   gfx::Image image;
+
+#if BUILDFLAG(IS_CHROMEOS)
+  // The path to the file that backs `image`. Set if `image` is file backed.
+  absl::optional<base::FilePath> image_path;
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   // Small badge to display on the notification to illustrate the source of the
   // notification. Optional.
