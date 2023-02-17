c3d6ac693ff8fb4497dc0579668c9f4e3a6a13e3
Manya Agarwal <magarwal@igalia.com>
2023-02-17 12:02:49 +0000

    Use WTF Bind in Blink code
    Replace base::BindOnce and base::BindRepeating with WTF::BindOnce and WTF::BindRepeating in b/r/core/{frame,fragment_directive}
    
    Change-Id: I4fc8351920e5c9d947a7897ccae3d01c7b09506d
    Bug: 1258806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4260825
    Commit-Queue: Manya Agarwal <magarwal@igalia.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106717}

--- third_party/blink/renderer/core/fragment_directive/text_fragment_anchor_metrics_test.cc
+++ third_party/blink/renderer/core/fragment_directive/text_fragment_anchor_metrics_test.cc
@@ -42,8 +42,7 @@ class TextFragmentAnchorMetricsTest : public SimTest {
   void RunAsyncMatchingTasks() {
     auto* scheduler =
         blink::scheduler::WebThreadScheduler::MainThreadScheduler();
-    blink::scheduler::RunIdleTasksForTesting(scheduler,
-                                             base::BindOnce([]() {}));
+    blink::scheduler::RunIdleTasksForTesting(scheduler, WTF::BindOnce([]() {}));
     RunPendingTasks();
   }
 
