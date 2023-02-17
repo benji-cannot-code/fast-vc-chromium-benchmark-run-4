9f51be609a3a9b59e258b6d6c11b4ad13d88c2bb
Jeremy Leconte <jleconte@google.com>
2023-02-17 11:05:16 +0000

    Fix unused include when base tracing is disabled.
    Change-Id: I51745c331fc78d36f2e0bf2b8bba0a5fd0fd9388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4260008
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Jeremy Leconte <jleconte@google.com>
    Cr-Commit-Position: refs/heads/main@{#1106707}

--- base/android/meminfo_dump_provider.cc
+++ base/android/meminfo_dump_provider.cc
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/meminfo_dump_provider.h"
 #include <jni.h>
 #include "base/android/jni_android.h"
-#include "base/base_jni_headers/MemoryInfoBridge_jni.h"
 #include "base/logging.h"
 #include "base/time/time.h"
 #include "base/trace_event/base_tracing.h"
+
+#if BUILDFLAG(ENABLE_BASE_TRACING)
+#include "base/base_jni_headers/MemoryInfoBridge_jni.h"
+#endif  // BUILDFLAG(ENABLE_BASE_TRACING)
 
 namespace base::android {
 
