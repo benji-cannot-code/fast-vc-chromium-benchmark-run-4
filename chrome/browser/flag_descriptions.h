bd03b5814be9e49a7cb93409f5680ea43ae50a9f
Khalid Peer <khalidpeer@chromium.org>
2023-02-17 01:43:28 +0000

    [omnibox][GM3] Add feature flag for Omnibox steady state height.
    Bug: 1416351
    Change-Id: I2d100332091fa0a1e34ae7c0d0302e2de96dfbec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258285
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106569}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1148,6 +1148,9 @@ extern const char kOmniboxExperimentalSuggestScoringDescription[];
 extern const char kOmniboxFuzzyUrlSuggestionsName[];
 extern const char kOmniboxFuzzyUrlSuggestionsDescription[];
 
+extern const char kOmniboxGM3SteadyStateHeightName[];
+extern const char kOmniboxGM3SteadyStateHeightDescription[];
+
 extern const char kOmniboxGroupingFrameworkName[];
 extern const char kOmniboxGroupingFrameworkDescription[];
 
