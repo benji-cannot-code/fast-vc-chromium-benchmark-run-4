e6723002b4f36ab745e1ede871467fe9cc2c232d
Sylvie Liu <sylvieliu@chromium.org>
2023-02-17 21:41:53 +0000

    Added a margin for PageIndicatorView in the Qs bubble
    This CL polished the UI that when PageIndicatorView is visible, both the
    top and bottom padding is 16px, and when it is invisible, the spacing
    between feature tile and slider is 24px. When there's the media control
    component and when there's no pagination, both the top and bottom
    padding should be 24px. When there's the media control component as well
    as pagination, the padding above and below pagination should be 16px and
    the padding below media control should be 24px.
    
    Screenshot:
    Without media control:
    - Without pagination:
    https://screenshot.googleplex.com/6CWEiJJc9Wg3r3E
    - With pagination:
    https://screenshot.googleplex.com/8am4TqEJf6hommu
    
    With media control:
    - Without pagination:
    https://screenshot.googleplex.com/7rHfpV5XtpGpZ6N
    - With pagination:
    https://screenshot.googleplex.com/8mBc3KXfycsep9H
    
    Bug: b/268734360
    Change-Id: I99c9ac1a2d2b83c7c658a88a79233d6f67477caa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250885
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107002}

--- ash/system/unified/quick_settings_view.cc
+++ ash/system/unified/quick_settings_view.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <numeric>
 
-#include "ash/constants/ash_features.h"
 #include "ash/system/media/unified_media_controls_container.h"
 #include "ash/system/tray/interacted_by_tap_recorder.h"
 #include "ash/system/tray/tray_constants.h"
@@ -26,15 +25,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/flex_layout_view.h"
+#include "ui/views/view_class_properties.h"
 
 namespace ash {
 
 namespace {
+
+constexpr auto kPageIndicatorMargin = gfx::Insets::TLBR(0, 0, 8, 0);
+constexpr auto kSlidersContainerMargin = gfx::Insets::TLBR(4, 0, 0, 0);
 
 class DetailedViewContainer : public views::View {
  public:
@@ -145,6 +149,7 @@ QuickSettingsView::QuickSettingsView(UnifiedSystemTrayController* controller)
           controller_, /*initially_expanded=*/controller_->model()
                                ->pagination_model()
                                ->total_pages() > 1));
+  page_indicator_view_->SetProperty(views::kMarginsKey, kPageIndicatorMargin);
 
   if (base::FeatureList::IsEnabled(media::kGlobalMediaControlsForChromeOS)) {
     media_controls_container_ = system_tray_container_->AddChildView(
@@ -155,6 +160,7 @@ QuickSettingsView::QuickSettingsView(UnifiedSystemTrayController* controller)
   sliders_container_ = system_tray_container_->AddChildView(
       std::make_unique<views::FlexLayoutView>());
   sliders_container_->SetOrientation(views::LayoutOrientation::kVertical);
+  sliders_container_->SetProperty(views::kMarginsKey, kSlidersContainerMargin);
 
   footer_ = system_tray_container_->AddChildView(
       std::make_unique<QuickSettingsFooter>(controller_));
