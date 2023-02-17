565359875ada366b5ccb4e3cbe7e7127af35775a
Tony Herre <toprice@chromium.org>
2023-02-17 10:29:00 +0000

    Use webrtc::MockTransformableVideoFrame in encodedVideoFrameTest
    I'm about to delete the version in the Chromium codebase in
    crrev.com/c/4236916.
    
    Bug: 1414370
    Change-Id: I08dd21382442418c59106ea17bffffc66ad55eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261677
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106698}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame_test.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame_test.cc
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_codec_specifics_vp_8.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_encoded_video_frame_metadata.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame_delegate.h"
-#include "third_party/blink/renderer/modules/peerconnection/testing/mock_transformable_video_frame.h"
+#include "third_party/webrtc/api/test/mock_transformable_video_frame.h"
 
 using testing::Return;
 using testing::ReturnRef;
+
+using webrtc::MockTransformableVideoFrame;
 
 namespace blink {
 
