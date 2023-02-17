b2086e4eaa00004984eae27d28cf30d9ee20fee6
David Baron <dbaron@chromium.org>
2023-02-17 18:24:07 +0000

    Revert "Simple frame production"
    This reverts commit 23a50951556c98fab168260ac37c32006fe546d1.
    
    Reason for revert: compilation failure in https://ci.chromium.org/ui/p/chromium/builders/ci/mac-arm64-on-arm64-rel/60768/overview :
    
    ../../cc/slim/slim_layer_tree_compositor_frame_unittest.cc:70:51: error: too few arguments to function call, expected 4, have 2
    frame_sink_->OnBeginFrame(begin_frame_args, {});
    ~~~~~~~~~~~~~~~~~~~~~~~~~                     ^
    ../../cc/slim/frame_sink_impl.h:78:8: note: 'OnBeginFrame' declared here
    void OnBeginFrame(const viz::BeginFrameArgs& begin_frame_args,
    ^
    
    Original change's description:
    > Simple frame production
    >
    > A simple frame production algorithm. Supports:
    > * a single root render pass only
    > * Layer position, transform
    > * SetMaskToBounds for axis-aligned layer
    >
    > Add AppendQuads for SolidColorLayer as the simplest layer for unit
    > testing.
    >
    > Notable missing features:
    > * Non-axis aligned clip and filters (requires non-root pass)
    > * Damage tracking
    > * Quad occlusion culling
    > * Visible rect on quads
    >
    > Add test harness to unit test frame generation.
    >
    > Bug: 1408128
    > Change-Id: I5508292876ddc66b59b118742dbceacb2c6a7d8e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245000
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Commit-Queue: Bo Liu <boliu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1106868}
    
    Bug: 1408128
    Change-Id: I0f9cdab7ce4c8afc4412e85abcc20d6b032fcd55
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261220
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: David Baron <dbaron@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106874}

--- cc/slim/test_frame_sink_impl.h
+++ cc/slim/test_frame_sink_impl.h
@@ -25,7 +25,6 @@ class TestFrameSinkImpl : public FrameSinkImpl {
     DCHECK(!bind_to_client_called_);
     bind_to_client_result_ = result;
   }
-  viz::CompositorFrame TakeLastFrame();
   bool bind_to_client_called() const { return bind_to_client_called_; }
   bool needs_begin_frames() const { return needs_begin_frames_; }
 
@@ -34,7 +33,6 @@ class TestFrameSinkImpl : public FrameSinkImpl {
   void SetNeedsBeginFrame(bool needs_begin_frame) override;
 
  private:
-  class TestMojoCompositorFrameSink;
   TestFrameSinkImpl(
       scoped_refptr<base::SingleThreadTaskRunner> task_runner,
       mojo::PendingAssociatedRemote<viz::mojom::CompositorFrameSink>
@@ -45,7 +43,8 @@ class TestFrameSinkImpl : public FrameSinkImpl {
       mojo::PendingAssociatedReceiver<viz::mojom::CompositorFrameSink>
           sink_receiver);
 
-  std::unique_ptr<TestMojoCompositorFrameSink> mojo_sink_;
+  mojo::PendingAssociatedReceiver<viz::mojom::CompositorFrameSink>
+      pending_sink_receiver_;
 
   bool bind_to_client_called_ = false;
   bool bind_to_client_result_ = true;
