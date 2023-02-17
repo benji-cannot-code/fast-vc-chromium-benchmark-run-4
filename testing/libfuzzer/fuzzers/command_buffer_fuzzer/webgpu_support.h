bb0db163057a915e24efb129a3ad0685cf11079c
Chris Bookholt <bookholt@chromium.org>
2023-02-17 16:58:51 +0000

    Command buffer fuzzer
    This fuzzer is intended to express and exercise low level CommandBuffer functionality in a way that is agnostic to and composable with other fuzzers targeting higher level protocols, such as WebGPU/DawnLPMFuzzer [1] and eventually also WebGL.
    
    Its implementation is limited by the functionality accessible from a single process unittest-like environment. Hence it cannot exercise the full CommandBuffer feature set such as IPC. However, it does exercise enough features to be able to find unexpected behavior [2].
    
    [1] https://dawn-review.googlesource.com/c/dawn/+/109406
    [2] https://crbug.com/1406115
    
    Bug: 1412950
    Change-Id: Idb2fc19fafd5fc93da103a21d5d9613b48de8466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261996
    Reviewed-by: Brendon Tiszka <tiszka@google.com>
    Commit-Queue: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106824}

---
+++ testing/libfuzzer/fuzzers/command_buffer_fuzzer/webgpu_support.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <dawn/webgpu_cpp.h>
+#include "gpu/command_buffer/client/webgpu_cmd_helper.h"
+#include "gpu/command_buffer/client/webgpu_implementation.h"
+
+#ifndef TESTING_LIBFUZZER_FUZZERS_COMMAND_BUFFER_FUZZER_WEBGPU_SUPPORT_H_
+#define TESTING_LIBFUZZER_FUZZERS_COMMAND_BUFFER_FUZZER_WEBGPU_SUPPORT_H_
+
+namespace gpu::cmdbuf::fuzzing {
+
+// borrowed directly from DawnWireServerFuzzer.cpp for now
+// TODO(bookholt): do something different.
+class DawnWireSerializerFuzzer : public dawn::wire::CommandSerializer {
+ public:
+  DawnWireSerializerFuzzer();
+  ~DawnWireSerializerFuzzer() override;
+  size_t GetMaximumAllocationSize() const override;
+  void* GetCmdSpace(size_t size) override;
+  bool Flush() override;
+
+ private:
+  std::vector<char> buf;
+};
+
+}  // namespace gpu::cmdbuf::fuzzing
+
+#endif  // TESTING_LIBFUZZER_FUZZERS_COMMAND_BUFFER_FUZZER_WEBGPU_SUPPORT_H_
