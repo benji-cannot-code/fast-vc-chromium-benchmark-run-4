4919c0742e8a6a83fde960bf5fe8ea14d22b8713
Gil Dekel <gildekel@chromium.org>
2023-02-17 22:14:35 +0000

    ozone/drm: Constify getter functions in DrmWrapper
    This CL adds the `const` tag to all getter functions in DrmWrapper and
    modifies the subclasses to match. No functionality changes otherwise.
    
    Bug: b:261628741
    Test: display_unittests && ozone_unittests
    Change-Id: I3aaeb0e630007ce2c23fec8f14f65cab54c0931e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261209
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107026}

--- ui/ozone/platform/drm/gpu/drm_gpu_util.h
+++ ui/ozone/platform/drm/gpu/drm_gpu_util.h
@@ -7,14 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_OZONE_PLATFORM_DRM_GPU_DRM_GPU_UTIL_H_
 
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
-#include "ui/ozone/platform/drm/common//drm_util.h"
+#include "ui/ozone/platform/drm/common/drm_util.h"
 #include "ui/ozone/platform/drm/common/scoped_drm_types.h"
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
 
 namespace ui {
 
+class DrmWrapper;
+
 // Helper function that finds the property with the specified name.
-bool GetDrmPropertyForName(DrmDevice* drm,
+bool GetDrmPropertyForName(DrmWrapper* drm,
                            drmModeObjectProperties* properties,
                            const std::string& name,
                            DrmDevice::Property* property);
