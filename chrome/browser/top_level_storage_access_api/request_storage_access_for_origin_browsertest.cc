6e7943452823a75127c5d6de83fe3b56845d6fec
Shuran Huang <shuuran@chromium.org>
2023-02-17 16:02:50 +0000

    Remove unnecessary histogram fetch calls
    According to the header documentation, in browser tests,
    SubprocessMetricsProvider::MergeHistogramDeltasForTesting() is needed
    to sync the histogram data between the renderer and browser processes, whereas content::FetchHistogramsFromChildProcesses() should be used in
    content browser test.
    
    The modified browser test is not content browser test, and
    "Blink.UseCounter.Features" histogram is fired in the renderer.
    
    Change-Id: Icc9fbe899a7c2e1a33b5e810fc738344e3a21da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4262152
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106803}

--- chrome/browser/top_level_storage_access_api/request_storage_access_for_origin_browsertest.cc
+++ chrome/browser/top_level_storage_access_api/request_storage_access_for_origin_browsertest.cc
@@ -523,8 +523,6 @@ IN_PROC_BROWSER_TEST_F(
                                             /*cors_enabled=*/true),
             "");
 
-  content::FetchHistogramsFromChildProcesses();
-
   EXPECT_THAT(histogram_tester.GetBucketCount(
                   kRequestOutcomeHistogram,
                   0 /*RequestOutcome::kGrantedByFirstPartySet*/),
@@ -562,7 +560,6 @@ IN_PROC_BROWSER_TEST_F(
                                             /*cors_enabled=*/true),
             "");
 
-  content::FetchHistogramsFromChildProcesses();
   EXPECT_THAT(histogram_tester.GetBucketCount(
                   kRequestOutcomeHistogram,
                   5 /*RequestOutcome::kDeniedByPrerequisites*/),
@@ -639,7 +636,6 @@ IN_PROC_BROWSER_TEST_F(
                                             /*cors_enabled=*/true),
             "");
 
-  content::FetchHistogramsFromChildProcesses();
   EXPECT_THAT(histogram_tester.GetBucketCount(
                   kRequestOutcomeHistogram,
                   3 /*RequestOutcome::kDeniedByFirstPartySet*/),
@@ -793,7 +789,6 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_EQ(GetFrameContent(), "None");
   EXPECT_EQ(ReadCookiesViaJS(GetFrame()), "");
 
-  content::FetchHistogramsFromChildProcesses();
   EXPECT_THAT(histogram_tester.GetBucketCount(
                   kRequestOutcomeHistogram,
                   5 /*RequestOutcome::kDeniedByPrerequisites*/),
