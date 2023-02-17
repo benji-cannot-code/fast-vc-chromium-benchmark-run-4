6b5a3babb4818dc86f31809da5c8d2b6508a36de
Tom Sepez <tsepez@chromium.org>
2023-02-17 18:36:49 +0000

    Better page boundary handling under use_asan_unowed_ptr=true.
    Now that the AllowPtrArithmetic trait has been applied, use it
    to control whether end-of-allocation exemptions are applied.
    
    Change-Id: I79c7bda2dfdc664e2e9ba776a70dd62570d01978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250600
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106885}

--- base/allocator/partition_allocator/pointers/raw_ptr_asan_unowned_impl.h
+++ base/allocator/partition_allocator/pointers/raw_ptr_asan_unowned_impl.h
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base::internal {
 
+bool EndOfAliveAllocation(const volatile void* ptr, bool is_adjustable_ptr);
+bool LikelySmuggledScalar(const volatile void* ptr);
+
+template <bool IsAdjustablePtr>
 struct RawPtrAsanUnownedImpl {
   // Wraps a pointer.
   template <typename T>
@@ -92,13 +96,10 @@ struct RawPtrAsanUnownedImpl {
   template <typename T>
   static void ProbeForLowSeverityLifetimeIssue(T* wrapped_ptr) {
     if (wrapped_ptr && !LikelySmuggledScalar(wrapped_ptr) &&
-        !EndOfAliveAllocation(wrapped_ptr)) {
+        !EndOfAliveAllocation(wrapped_ptr, IsAdjustablePtr)) {
       reinterpret_cast<const volatile uint8_t*>(wrapped_ptr)[0];
     }
   }
-
-  static bool EndOfAliveAllocation(const volatile void* ptr);
-  static bool LikelySmuggledScalar(const volatile void* ptr);
 
   // `WrapRawPtrForDuplication` and `UnsafelyUnwrapPtrForDuplication` are used
   // to create a new raw_ptr<T> from another raw_ptr<T> of a different flavor.
