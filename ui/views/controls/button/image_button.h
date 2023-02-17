a2a8fea1261ba75814c02e7778790626fb20fce6
Sean Kau <skau@chromium.org>
2023-02-17 21:06:45 +0000

    Trigger paint on theme change in ImageButton
    ImageButton uses ImageModel to draw appropriately colored icons. ImageModel only affects the colored icon if repaint is requested. If a button happens to not get painted anyway, we can show images that use
    the previous style. Scheduling paint in OnThemeChanged fixes this.
    
    Bug: b/269648367
    Change-Id: I2d2f0527aa71f496bd3bf279238ce83dfc2e9ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4262693
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Quick-Run: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106981}

--- ui/views/controls/button/image_button.h
+++ ui/views/controls/button/image_button.h
@@ -74,6 +74,7 @@ class VIEWS_EXPORT ImageButton : public Button {
   // Overridden from View:
   gfx::Size CalculatePreferredSize() const override;
   views::PaintInfo::ScaleType GetPaintScaleType() const override;
+  void OnThemeChanged() override;
 
  protected:
   // Overridden from Button:
