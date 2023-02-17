8ec493f9a09a76e00d76dabc7a848e754a6b67e2
Bo Majewski <majewski@chromium.org>
2023-02-17 07:40:20 +0000

    [Files]: Fix search on other than My Files volume
    searchFilesFunction made an implicit assumption that the search is
    always conducted on My Files volume. Previous code change corrected
    this in the Run method, but not in the OnSearchByPattern method. This
    CL adds root_path parameter to OnSearchByPattern to pass the root path
    computed in the Run method. It also computes the rootDir based on the
    entry in the SearchV2ContentScanner. Previously, it would pass
    undefined, which always resulted in My Files root.
    
    Bug: b:268388891
    Change-Id: Ifdbc39b850ac82a42a0587f62fc8d85a6bba20ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4229655
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106669}

--- chrome/browser/ash/extensions/file_manager/private_api_file_system.h
+++ chrome/browser/ash/extensions/file_manager/private_api_file_system.h
@@ -486,7 +486,7 @@ class FileManagerPrivateInternalSearchFilesFunction
   // ExtensionFunction overrides.
   ResponseAction Run() override;
 
-  void OnSearchByPattern(
+  void OnSearchByPatternDone(
       const std::vector<std::pair<base::FilePath, bool>>& results);
 };
 
