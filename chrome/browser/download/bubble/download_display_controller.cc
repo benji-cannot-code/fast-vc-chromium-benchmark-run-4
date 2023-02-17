416a7fdac989e91219fa38c69934dafed0752c84
Lily Chen <chlily@chromium.org>
2023-02-17 23:41:02 +0000

    [DownloadBubble] Trigger download started animation on updating button
    This moves the code that triggers the "download started" animation from
    ShowDetails() to UpdateToolbarButton(), in order to decouple the
    download started event from showing the partial bubble dialog.
    
    Bug: 1416926
    Change-Id: I241b34debb0557c3459d321714701228954c77b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4264740
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107069}

--- chrome/browser/download/bubble/download_display_controller.cc
+++ chrome/browser/download/bubble/download_display_controller.cc
@@ -144,7 +144,8 @@ void DownloadDisplayController::OnNewItem(bool show_details,
           /*force_update=*/true);
     }
   } else {
-    display_->ShowDetails(show_animation);
+    display_->ShowDetails();
+    display_->UpdateDownloadIcon(show_animation);
   }
 }
 
@@ -204,7 +205,7 @@ void DownloadDisplayController::HandleButtonPressed() {
   if (icon_info_.icon_state == DownloadIconState::kComplete) {
     icon_info_.is_active = false;
   }
-  display_->UpdateDownloadIcon();
+  display_->UpdateDownloadIcon(/*show_animation=*/false);
 }
 
 void DownloadDisplayController::ShowToolbarButton() {
@@ -244,7 +245,7 @@ void DownloadDisplayController::OnFullscreenStateChanged() {
   int in_progress_count = InProgressDownloadCount(all_models);
   if (in_progress_count > 0 &&
       download::ShouldShowDownloadBubble(browser_->profile())) {
-    display_->ShowDetails(/*show_animation=*/false);
+    display_->ShowDetails();
   }
 }
 
@@ -295,12 +296,12 @@ void DownloadDisplayController::UpdateToolbarButtonState(
                                 last_complete_time)) {
     ShowToolbarButton();
   }
-  display_->UpdateDownloadIcon();
+  display_->UpdateDownloadIcon(/*show_animation=*/false);
 }
 
 void DownloadDisplayController::UpdateDownloadIconToInactive() {
   icon_info_.is_active = false;
-  display_->UpdateDownloadIcon();
+  display_->UpdateDownloadIcon(/*show_animation=*/false);
 }
 
 void DownloadDisplayController::ScheduleToolbarDisappearance(
