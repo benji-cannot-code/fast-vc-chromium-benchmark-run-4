dcb174b492fcafd43bfe202e0f15a5c9103d0287
Wes Okuhara <wesokuhara@google.com>
2023-02-17 01:53:09 +0000

    CrOS Settings: Remove references of Closure compilation
    Removes any code/workarounds/comments referring to Closure
    compilation. This includes comments related to crbug/1315757.
    
    Bug: b:263413628
    Test: browser_tests --gtest_filter='OSSettings*'
    Change-Id: Ica50c75270d895c074218f436c885e6770812562
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4251849
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1106572}

--- chrome/browser/resources/settings/chromeos/os_languages_page/languages_types.ts
+++ chrome/browser/resources/settings/chromeos/os_languages_page/languages_types.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @fileoverview Closure typedefs for dictionaries and interfaces used by
+ * @fileoverview Typedefs for dictionaries and interfaces used by
  * language settings.
  */
 
