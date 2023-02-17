9feb51879045fbbc556465d1d9a97266c8f32a4f
Peter Boström <pbos@chromium.org>
2023-02-17 03:27:46 +0000

    Update DataObjectBuilderTest to EXPECT_DEATH
    DataObjectBuilderTest.UnusableAfterBuild used to verify that a DCHECK
    was hit by using a ScopedLogAssertHandler, effectively cancelling a
    LOG(FATAL).
    
    We're moving towards making LOG(FATAL) [[noreturn]], which requires
    removing anything that can cancel it. Also code that invokes LOG(FATAL)
    should not have to handle that call returning.
    
    Bug: 1409729
    Change-Id: I5eed8a3e4f4b1e825a0df19e2cd0c7b8c20be0bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4260696
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106607}

--- gin/data_object_builder_unittest.cc
+++ gin/data_object_builder_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/data_object_builder.h"
 
 #include "base/check_op.h"
+#include "base/debug/debugging_buildflags.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "gin/dictionary.h"
@@ -86,7 +87,8 @@ TEST_F(DataObjectBuilderTest, DoesNotInvokeSetters) {
 // The internal handle is cleared when the builder is finished.
 // This makes the class harder to abuse, so that its methods cannot be used
 // after something may have modified the object in unexpected ways.
-#if DCHECK_IS_ON()
+// TODO(pbos): Consider making this a CHECK and test this everywhere.
+#if DCHECK_IS_ON() && !BUILDFLAG(DCHECK_IS_CONFIGURABLE)
 TEST_F(DataObjectBuilderTest, UnusableAfterBuild) {
   v8::Isolate* isolate = instance_->isolate();
   v8::HandleScope handle_scope(isolate);
@@ -94,13 +96,8 @@ TEST_F(DataObjectBuilderTest, UnusableAfterBuild) {
   DataObjectBuilder builder(isolate);
   EXPECT_FALSE(builder.Build().IsEmpty());
 
-  bool has_dcheck_failure = false;
-  logging::ScopedLogAssertHandler handler(base::BindRepeating(
-      [](bool* flag, const char* file, int line, base::StringPiece message,
-         base::StringPiece stack_trace) { *flag = true; },
-      base::Unretained(&has_dcheck_failure)));
-  builder.Build();
-  EXPECT_TRUE(has_dcheck_failure);
+  EXPECT_DEATH_IF_SUPPORTED(builder.Build(),
+                            "Check failed: !object_.IsEmpty\\(\\)");
 }
 #endif  // DCHECK_IS_ON()
 
