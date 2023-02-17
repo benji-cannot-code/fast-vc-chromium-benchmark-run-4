3d3c14baa8376f99bcfc7d6d5b3ba71c3955c183
Xianzhu Wang <wangxianzhu@chromium.org>
2023-02-17 18:53:12 +0000

    Add trace events for RTree::Build and RTree::Search
    This will help us know some details about rasterization.
    The overhead of the events will be trivial compared to the
    rasterization work.
    
    Bug: 1401086
    Change-Id: I0b72096fe436d76df48d7b87c094d0d2e7a74d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261994
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106903}

--- cc/base/rtree.h
+++ cc/base/rtree.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/memory/raw_ptr.h"
 #include "base/numerics/clamped_math.h"
+#include "base/trace_event/trace_event.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace cc {
@@ -179,6 +180,7 @@ template <typename Container, typename BoundsFunctor, typename PayloadFunctor>
 void RTree<T>::Build(const Container& items,
                      const BoundsFunctor& bounds_getter,
                      const PayloadFunctor& payload_getter) {
+  TRACE_EVENT1("cc", "RTree::Build", "size", items.size());
   DCHECK_EQ(0u, num_data_elements_);
 
   std::vector<Branch<T>> branches;
@@ -318,11 +320,14 @@ void RTree<T>::Search(const gfx::Rect& query,
   results->clear();
   if (num_data_elements_ == 0)
     return;
+
+  TRACE_EVENT_BEGIN1("cc", "RTree::Search", "size", num_data_elements_);
   if (!has_valid_bounds_) {
     SearchRecursiveFallback(root_.subtree.get(), query, results, rects);
   } else if (query.Intersects(root_.bounds)) {
     SearchRecursive(root_.subtree.get(), query, results, rects);
   }
+  TRACE_EVENT_END1("cc", "RTree::Search", "result_size", results->size());
 }
 
 template <typename T>
