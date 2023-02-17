487077482e29738c98544c6b2f8f0cb83290ef18
Cici Ruan <cuicuiruan@google.com>
2023-02-17 08:52:47 +0000

    arc/gio: Keep ink drop color same for light and dark theme
    For buttons in save&reset buttons, keep the ink drop color the same for
    both light and dark theme. Re-implement SetUpInkDropForButton because
    StyleUtil set up color according to the theme.
    
    Remove the ink drop when save&reset button groups start to drag.
    
    Bug: b/260868602
    Test: Inkdrop color stays the same for both dark and light theme.
    Change-Id: I34e2ad7baf98b59df2345f11df8142e8a6a16adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4259445
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106682}

--- chrome/browser/ash/arc/input_overlay/ui/edit_finish_view.cc
+++ chrome/browser/ash/arc/input_overlay/ui/edit_finish_view.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/input_overlay/ui/edit_finish_view.h"
 
+#include <memory>
+
 #include "ash/app_list/app_list_util.h"
 #include "ash/style/style_util.h"
 #include "base/functional/bind.h"
@@ -18,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #include "ui/events/event.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/views/animation/flood_fill_ink_drop_ripple.h"
+#include "ui/views/animation/ink_drop.h"
+#include "ui/views/animation/ink_drop_highlight.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/layout/box_layout.h"
@@ -59,12 +64,38 @@ constexpr int kButtonHeightAlpha = 56;
 // Since we only need the dark mode for the kEdit mode, so dark background is
 // passed for setting up the ink drop.
 constexpr SkColor kEditBackgroundColor = SkColorSetA(SK_ColorBLACK, 0x99);
+constexpr SkColor kInkDropBaseColor = SK_ColorWHITE;
+constexpr float kInkDropOpacity = 0.08f;
 
 // About focus ring.
 // Gap between focus ring outer edge to label.
 constexpr float kHaloInset = -6;
 // Thickness of focus ring.
 constexpr float kHaloThickness = 4;
+
+std::unique_ptr<views::InkDrop> CreateInkDrop(views::Button* view) {
+  return views::InkDrop::CreateInkDropForFloodFillRipple(
+      views::InkDrop::Get(view), /*highlight_on_hover=*/true,
+      /*highlight_on_focus=*/true);
+}
+
+std::unique_ptr<views::InkDropRipple> CreateInkDropRipple(
+    const views::Button* view) {
+  return std::make_unique<views::FloodFillInkDropRipple>(
+      const_cast<views::InkDropHost*>(views::InkDrop::Get(view)), view->size(),
+      gfx::Insets(),
+      views::InkDrop::Get(view)->GetInkDropCenterBasedOnLastEvent(),
+      kInkDropBaseColor, kInkDropOpacity);
+}
+
+std::unique_ptr<views::InkDropHighlight> CreateInkDropHighlight(
+    views::Button* view) {
+  auto highlight = std::make_unique<views::InkDropHighlight>(
+      gfx::SizeF(view->size()), kInkDropBaseColor);
+  highlight->set_visible_opacity(kInkDropOpacity);
+  return highlight;
+}
+
 }  // namespace
 
 class EditFinishView::ChildButton : public views::LabelButton {
@@ -110,10 +141,14 @@ class EditFinishView::ChildButton : public views::LabelButton {
     focus_ring->SetHaloInset(kHaloInset);
     focus_ring->SetHaloThickness(kHaloThickness);
     focus_ring->SetColorId(ui::kColorAshInputOverlayFocusRing);
-    ash::StyleUtil::SetUpInkDropForButton(this, gfx::Insets(),
-                                          /*highlight_on_hover=*/true,
-                                          /*highlight_on_focus=*/true,
-                                          kEditBackgroundColor);
+    if (AllowReposition()) {
+      SetUpInkDropForButton();
+    } else {
+      ash::StyleUtil::SetUpInkDropForButton(this, gfx::Insets(),
+                                            /*highlight_on_hover=*/true,
+                                            /*highlight_on_focus=*/true,
+                                            kEditBackgroundColor);
+    }
   }
   ~ChildButton() override = default;
 
@@ -124,11 +159,15 @@ class EditFinishView::ChildButton : public views::LabelButton {
   }
 
   bool OnMouseDragged(const ui::MouseEvent& event) override {
-    if (AllowReposition()) {
-      is_dragging_ = true;
-      on_mouse_dragged_callback_.Run(event);
-    }
-    return LabelButton::OnMouseDragged(event);
+    if (!AllowReposition())
+      return LabelButton::OnMouseDragged(event);
+
+    is_dragging_ = true;
+    on_mouse_dragged_callback_.Run(event);
+    views::InkDrop::Get(this)->GetInkDrop()->SetHovered(false);
+    views::InkDrop::Get(this)->AnimateToState(views::InkDropState::HIDDEN,
+                                              /*event=*/nullptr);
+    return true;
   }
 
   void OnMouseReleased(const ui::MouseEvent& event) override {
@@ -136,11 +175,25 @@ class EditFinishView::ChildButton : public views::LabelButton {
       LabelButton::OnMouseReleased(event);
       return;
     }
+    // Don't trigger button pressed event when it is dragged.
     is_dragging_ = false;
     on_mouse_released_callback_.Run(event);
   }
 
  private:
+  // Set inkdrop without theme.
+  void SetUpInkDropForButton() {
+    auto* ink_drop = views::InkDrop::Get(this);
+    ink_drop->SetMode(views::InkDropHost::InkDropMode::ON);
+    SetHasInkDropActionOnClick(true);
+    ink_drop->SetCreateInkDropCallback(
+        base::BindRepeating(&CreateInkDrop, this));
+    ink_drop->SetCreateRippleCallback(
+        base::BindRepeating(&CreateInkDropRipple, this));
+    ink_drop->SetCreateHighlightCallback(
+        base::BindRepeating(&CreateInkDropHighlight, this));
+  }
+
   bool is_dragging_ = false;
 
   // Callbacks for dragging.
