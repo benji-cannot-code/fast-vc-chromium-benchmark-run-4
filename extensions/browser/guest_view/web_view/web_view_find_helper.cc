0be7038ab8f9a9b0aea4aebb80a936450ee8638e
Solomon Kinard <solomonkinard@chromium.org>
2023-02-17 17:53:02 +0000

    Extensions: Fixit: Use '= default' to define a trivial *structor
    This CL was uploaded by git cl split.
    
    R=wjmaclean@chromium.org
    
    Bug: 1416739
    Change-Id: If85b8ed8b5a48caef4743dff7915c53aec48f43c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258276
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106855}

--- extensions/browser/guest_view/web_view/web_view_find_helper.cc
+++ extensions/browser/guest_view/web_view/web_view_find_helper.cc
@@ -282,6 +282,6 @@ void WebViewFindHelper::FindInfo::SendResponse(bool canceled) {
   find_function_->ForwardResponse(std::move(results));
 }
 
-WebViewFindHelper::FindInfo::~FindInfo() {}
+WebViewFindHelper::FindInfo::~FindInfo() = default;
 
 }  // namespace extensions
--- extensions/browser/guest_view/web_view/web_view_permission_helper.cc
+++ extensions/browser/guest_view/web_view/web_view_permission_helper.cc
@@ -362,6 +362,7 @@ WebViewPermissionHelper::PermissionResponseInfo&
 WebViewPermissionHelper::PermissionResponseInfo::operator=(
     WebViewPermissionHelper::PermissionResponseInfo&& other) = default;
 
-WebViewPermissionHelper::PermissionResponseInfo::~PermissionResponseInfo() {}
+WebViewPermissionHelper::PermissionResponseInfo::~PermissionResponseInfo() =
+    default;
 
 }  // namespace extensions
