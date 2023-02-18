3e5d6b2fa607b17f2be507556a3a0dc5c0023511
Gil Dekel <gildekel@chromium.org>
2023-02-18 00:40:59 +0000

    ozone/drm: Use DrmWrapper in DrmGpuDisplayManager and drm/common
    This CL modifies all utility functions in ui/ozone/platform/drm/common
    to utilized DrmWrapper instead DRM FDs directly.
    
    In addition, this CL updates DrmGpuDisplayManager in a similar way.
    Specifically, its call into GetDisplayInfosAndUpdateCrtcs() and
    CreateDisplaySnapshot(), which were the main blocker in testing
    DrmDisplay.
    
    Bug: b:261628741
    Test: display_unittests && ozone_unittests
    Change-Id: Ia6031c227597712a2e5ac305d8ba1fdd1d68477d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4264742
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107090}

--- ui/ozone/platform/drm/gpu/drm_gpu_display_manager.h
+++ ui/ozone/platform/drm/gpu/drm_gpu_display_manager.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "ui/display/types/display_configuration_params.h"
 #include "ui/display/types/display_constants.h"
-#include "ui/gfx/geometry/point.h"
-#include "ui/gfx/native_widget_types.h"
 #include "ui/ozone/platform/drm/common/display_types.h"
 
 using drmModeModeInfo = struct _drmModeModeInfo;
