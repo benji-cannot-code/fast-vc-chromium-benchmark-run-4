b8da56098e5728f801f6717e8feb365cb05acb6f
Yuki Awano <yawano@google.com>
2023-02-18 08:10:41 +0000

    Update UI strings
    - Update UI strings of Google One offer IPH to reflect the latest UX
      spec.
    
    Bug: b:265208998
    Change-Id: Icaf438a2f13e2577b6c4e069f57978585dd16780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265616
    Auto-Submit: Yuki Awano <yawano@google.com>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107135}

--- chrome/browser/ui/ash/google_one_offer_iph_tab_helper_constants.h
+++ chrome/browser/ui/ash/google_one_offer_iph_tab_helper_constants.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 constexpr char kFallbackNotificationDisplaySource[] = "ChromeOS Perks";
 constexpr char kFallbackNotificationTitle[] = "Get 100 GB of cloud storage";
 constexpr char kFallbackNotificationMessage[] =
-    "Your Chromebook comes with 100 GB for files and photos, free of charge "
-    "for 12 months";
-constexpr char kFallbackGetPerkButtonTitle[] = "Get storage";
+    "Keep your files & photos backed up with 12 months of Google One at no "
+    "charge";
+constexpr char kFallbackGetPerkButtonTitle[] = "Claim now";
 
 static constexpr char kIPHGoogleOneOfferNotificationId[] =
     "iph-google-one-offer-notification-id";
