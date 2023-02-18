8f5fff0f5106cd239e256eb3d59e059b54e6663e
Ari Chivukula <arichiv@chromium.org>
2023-02-18 05:10:47 +0000

    [StorageKey] (8) Define opaque origin behavior
    There's a need to keep StorageKey and BlinkStorageKey partial sync, but
    as each organically grew this has gotten lost. As we wrap up and launch
    storage partitioning, we should leave this code in a good state.
    
    We should only maintain storage key states that provide a meaningful
    distinction. Right now, there isn't a context in which we need to
    distinguish between an opaque origin with kCrossSite or kSameSite
    ancestor_chain_bits as there is no circumstance in which a nested opaque
    context could be identical to an outer context with some other context
    in between them. We should require that for any opaque origin kCrossSite
    must be set.
    
    This caps off a long set of CLs and *should* fully define StorageKey
    behavior.
    
    This CL is part of a series:
    (1) Organize public functions and map work
    (2) Rename CreateFirstParty constructors
    (3) Rename CreateWithNonce constructors
    (4) Rename Create constructors
    (5) Organize private functions
    (6) Consolidate validation
    (7) Remove old test constructors
    (8) Define opaque origin behavior
    
    Bug: 1410254
    Change-Id: I2c148b5865e591d6f56453f05fe2009ec94a4d95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261596
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107126}

--- third_party/blink/renderer/platform/storage/blink_storage_key_test.cc
+++ third_party/blink/renderer/platform/storage/blink_storage_key_test.cc
@@ -392,6 +392,8 @@ TEST(BlinkStorageKeyTest, FromWireReturnValue) {
        AncestorChainBit::kCrossSite, true},
       {o1, opaque_site, opaque_site, absl::nullopt,
        AncestorChainBit::kCrossSite, AncestorChainBit::kCrossSite, true},
+      {opaque, opaque_site, opaque_site, absl::nullopt,
+       AncestorChainBit::kCrossSite, AncestorChainBit::kCrossSite, true},
       // Failing cases:
       // If a 3p key is indicated, the *if_third_party_enabled pieces should
       // match their counterparts.
@@ -421,6 +423,13 @@ TEST(BlinkStorageKeyTest, FromWireReturnValue) {
        AncestorChainBit::kSameSite, false},
       {o1, opaque_site, opaque_site, absl::nullopt, AncestorChainBit::kSameSite,
        AncestorChainBit::kSameSite, false},
+      // If the origin is opaque, the ancestor_chain_bit* must be cross-site.
+      {opaque, opaque_site, opaque_site, absl::nullopt,
+       AncestorChainBit::kSameSite, AncestorChainBit::kSameSite, false},
+      {opaque, opaque_site, opaque_site, absl::nullopt,
+       AncestorChainBit::kCrossSite, AncestorChainBit::kSameSite, false},
+      {opaque, opaque_site, opaque_site, absl::nullopt,
+       AncestorChainBit::kSameSite, AncestorChainBit::kCrossSite, false},
   };
 
   const BlinkStorageKey starting_key;
@@ -446,9 +455,11 @@ TEST(BlinkStorageKeyTest, WithOrigin) {
       SecurityOrigin::CreateFromString("https://foo.com");
   scoped_refptr<const SecurityOrigin> other_origin =
       SecurityOrigin::CreateFromString("https://notfoo.com");
+  scoped_refptr<const SecurityOrigin> opaque_origin =
+      SecurityOrigin::CreateUniqueOpaque();
   const BlinkSchemefulSite site(origin);
   const BlinkSchemefulSite other_site(other_origin);
-  const BlinkSchemefulSite opaque_site;
+  const BlinkSchemefulSite opaque_site(opaque_origin);
   const base::UnguessableToken nonce = base::UnguessableToken::Create();
 
   base::test::ScopedFeatureList scoped_feature_list;
@@ -535,6 +546,14 @@ TEST(BlinkStorageKeyTest, WithOrigin) {
           BlinkStorageKey::CreateWithNonce(origin, nonce),
           other_origin,
           BlinkStorageKey::CreateWithNonce(other_origin, nonce),
+      },
+      // Change in opaque top_level_site key updated with opaque origin.
+      {
+          BlinkStorageKey::Create(origin, opaque_site,
+                                  mojom::AncestorChainBit::kCrossSite),
+          opaque_origin,
+          BlinkStorageKey::Create(opaque_origin, opaque_site,
+                                  mojom::AncestorChainBit::kCrossSite),
       },
   };
 
