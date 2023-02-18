7f45efb4f631fb90deaa6954e36061790fd72c7c
Fergal Daly <fergal@chromium.org>
2023-02-18 02:49:03 +0000

    BFCache: Do not check IsDOMContentLoaded of a frame if last url in the frame is invalid
    IsDOMContentLoaded can be completed when the url is valid.
    If a subframe with an invalid url exists in a page, bfcache does not work because IsDOMContentLoaded of the frame is always false.
    
    This required test updates:
    - ChildFrameCaptureContentFirst disable BFCache because the test requires that the frame be evicted
    - the data= version of embedded-not-found-expected.txt now passes
    
    This adds WebContentsWaiter a base class for making test-waiters that wait for a WebContentObserver event to occur.
    
    R=toyoshim@chromium.org
    
    Bug: 1366707
    Change-Id: I371cd9ad55f342d9c71ba24eaf5b8b5981d818ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3912822
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107116}

--- components/content_capture/browser/content_capture_receiver_test.cc
+++ components/content_capture/browser/content_capture_receiver_test.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_mock_time_task_runner.h"
 #include "build/build_config.h"
 #include "components/content_capture/browser/content_capture_test_helper.h"
+#include "content/public/browser/back_forward_cache.h"
 #include "content/public/common/content_features.h"
+#include "content/public/test/back_forward_cache_util.h"
 #include "content/public/test/test_renderer_host.h"
 #include "content/public/test/test_utils.h"
 #include "third_party/blink/public/mojom/favicon/favicon_url.mojom.h"
@@ -372,6 +374,9 @@ TEST_P(ContentCaptureReceiverTest, TitleUpdateTaskDelay) {
 #define MAYBE_ChildFrameCaptureContentFirst ChildFrameCaptureContentFirst
 #endif
 TEST_P(ContentCaptureReceiverTest, MAYBE_ChildFrameCaptureContentFirst) {
+  // This test performs navigations, expecting the frames to be destroyed.
+  content::DisableBackForwardCacheForTesting(
+      web_contents(), content::BackForwardCache::TEST_REQUIRES_NO_CACHING);
   // Simulate add child frame.
   SetupChildFrame();
   // Simulate to capture the content from child frame.
