064d0c7e28bb43eb44ac8d86743e47de0e2eeccb
Mitsuru Oshima <oshima@chromium.org>
2023-02-20 10:59:58 +0000

    Remove ScopedScreenOverride definition and include
    I forgot to remove them in crrev.com/c/4262543, sorry.
    
    Bug: 1415156
    
    Change-Id: I684f2cf8db832c25f509b02393407e2b9d54ad7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4264854
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107385}

--- extensions/shell/browser/shell_desktop_controller_aura_unittest.cc
+++ extensions/shell/browser/shell_desktop_controller_aura_unittest.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method_minimal.h"
 #include "ui/display/display.h"
 #include "ui/display/screen_base.h"
-#include "ui/display/test/scoped_screen_override.h"
 #include "ui/events/event.h"
 #include "ui/events/event_dispatcher.h"
 #include "ui/events/event_utils.h"
@@ -65,7 +64,6 @@ class ShellDesktopControllerAuraTest : public ShellTestBaseAura {
         display::Display(200, gfx::Rect(1920, 1080, 800, 600)),
         display::DisplayList::Type::NOT_PRIMARY);
     display::Screen::SetScreenInstance(screen_.get());
-
     ShellTestBaseAura::SetUp();
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -96,7 +94,6 @@ class ShellDesktopControllerAuraTest : public ShellTestBaseAura {
   }
 
   std::unique_ptr<display::ScreenBase> screen_;
-  std::unique_ptr<display::test::ScopedScreenOverride> screen_override_;
   std::unique_ptr<ShellDesktopControllerAura> controller_;
 };
 
