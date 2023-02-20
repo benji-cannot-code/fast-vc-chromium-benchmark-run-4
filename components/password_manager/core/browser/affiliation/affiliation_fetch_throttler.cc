c849d142c9a07757ee81935808d3382fb0bdfff3
Viktor Semeniuk <vsemeniuk@google.com>
2023-02-20 16:55:44 +0000

    [Passwords Grouping] API to refreshes cache in AffiliationService
    This change adds a new function UpdateAffiliationsAndBranding() to
    AffiliationService which will clear existing cache and request new
    branding/grouping information from the affiliation service. This is
    useful when PasswordsGrouping feature was just enabled and cache
    doesn't contain any grouping information.
    
    Callback will be invoke immediately if there is no internet connection
    and after the response is received. It doesn't matter whether the
    request was successful or not.
    
    Bug: 1354196
    Change-Id: Id10c415c0ddd53d5d7c7cbcfc8a69920cfa40456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4259966
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107491}

--- components/password_manager/core/browser/affiliation/affiliation_fetch_throttler.cc
+++ components/password_manager/core/browser/affiliation/affiliation_fetch_throttler.cc
@@ -91,6 +91,10 @@ void AffiliationFetchThrottler::InformOfNetworkRequestComplete(bool success) {
   exponential_backoff_->InformOfRequest(success);
 }
 
+bool AffiliationFetchThrottler::HasInternetConnection() const {
+  return !network_connection_tracker_->IsOffline();
+}
+
 void AffiliationFetchThrottler::EnsureCallbackIsScheduled() {
   DCHECK_EQ(state_, FETCH_NEEDED);
   DCHECK(has_network_connectivity_);
