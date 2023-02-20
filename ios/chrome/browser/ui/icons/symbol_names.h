587d7a797063b75d0dac5ff99e0dc1d2f9d279c3
Gauthier Ambard <gambard@chromium.org>
2023-02-20 21:16:50 +0000

    [iOS] Change symbol for set default browser
    Use the Chrome symbol instead.
    
    To be landed after https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/5486012
    
    Bug: 1408719
    Change-Id: Idb614c7abbed3c5b93bd39edaa5bffc777c1aba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265112
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107535}

--- ios/chrome/browser/ui/icons/symbol_names.h
+++ ios/chrome/browser/ui/icons/symbol_names.h
@@ -16,12 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// *******
 
 // Custom symbol names.
+#if BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
+extern NSString* const kGoogleIconSymbol;
+extern NSString* const kGoogleShieldSymbol;
+extern NSString* const kChromeSymbol;
+#endif  // BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
 extern NSString* const kPrivacySymbol;
 extern NSString* const kSyncDisabledSymbol;
 extern NSString* const kSafetyCheckSymbol;
-#if BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
-extern NSString* const kGoogleIconSymbol;
-#endif  // BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
 extern NSString* const kArrowClockWiseSymbol;
 extern NSString* const kIncognitoSymbol;
 extern NSString* const kSquareNumberSymbol;
@@ -44,9 +46,6 @@ extern NSString* const kMulticolorPasswordSymbol;
 #endif  // BUILDFLAG(IS_IOS_MACCATALYST)
 extern NSString* const kCameraLensSymbol;
 extern NSString* const kDownTrendSymbol;
-#if BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
-extern NSString* const kGoogleShieldSymbol;
-#endif  // BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
 extern NSString* const kShieldSymbol;
 extern NSString* const kCloudSlashSymbol;
 extern NSString* const kCloudAndArrowUpSymbol;
