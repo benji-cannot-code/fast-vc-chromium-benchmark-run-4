8267994aba9384f4bb7ff21001c2603a08f7b7be
Jack Yammine <jyammine@google.com>
2023-02-20 10:19:06 +0000

    [fyfre]Add gshield icon for branded builds.
    Replace the security icon with an internal gshield icon for branded builds.
    Keep the security icon for non-branded builds.
    
    Fixed: 1402759
    Change-Id: Iff58d33e47238b5d7a4c2f4ff2a33c9e18b9d0e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4251640
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107375}

--- chrome/browser/resources/intro/sign_in_promo.ts
+++ chrome/browser/resources/intro/sign_in_promo.ts
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
+import 'chrome://resources/cr_elements/cr_icons.css.js';
 import 'chrome://resources/cr_elements/icons.html.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
-import './icons.html.js';
 import './strings.m.js';
 
 import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
@@ -33,7 +33,7 @@ export interface SignInPromoElement {
 export interface BenefitCard {
   title: string;
   description: string;
-  iconName: string;
+  iconId: string;
 }
 
 const SignInPromoElementBase = WebUiListenerMixin(I18nMixin(PolymerElement));
@@ -49,17 +49,17 @@ export class SignInPromoElement extends SignInPromoElementBase {
       {
         title: this.i18n('devicesCardTitle'),
         description: this.i18n('devicesCardDescription'),
-        iconName: 'intro:devices',
+        iconId: 'devices',
       },
       {
         title: this.i18n('securityCardTitle'),
         description: this.i18n('securityCardDescription'),
-        iconName: 'cr:security',
+        iconId: 'security',
       },
       {
         title: this.i18n('backupCardTitle'),
         description: this.i18n('backupCardDescription'),
-        iconName: 'intro:cloud-upload',
+        iconId: 'cloud-upload',
       },
     ];
   }
