1ab570703a1ab998be76e70e2e1d906140de571c
James Lee <ljjlee@google.com>
2023-02-21 15:17:46 +0000

    Remove unused parent_permission_dialog include from supervised_user_service
    Bug: b/265758589
    Change-Id: If4a7ce6f497beba6134326de4e6943c71c3757c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4272851
    Commit-Queue: James Lee <ljjlee@google.com>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107741}

--- chrome/browser/supervised_user/supervised_user_service.h
+++ chrome/browser/supervised_user/supervised_user_service.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/ui/supervised_user/parent_permission_dialog.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "extensions/browser/management_policy.h"
