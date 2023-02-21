c52892d24fe5889f2c304df54810818570bfd9a0
Sam Thiesen <samicolon@google.com>
2023-02-21 10:35:35 +0000

    [Multi Apps] Add crosapi to get all sub-app ids
    Adds a function that can be called from ash chrome to retrieve
    information about web apps (the ids of the given app's sub apps) stored
    in lacros chrome.
    Also adds an ash browsertest that connects to the lacros-side
    web_app_provider_bridge. To be able to install sub apps on Lacros from
    the ash-based test, the change adds a function on the test controller.
    
    Bug: 1354567
    Change-Id: Iba5158fec02ddd7d0e01767126a4b540697a4f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4249166
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Commit-Queue: Sam Thiesen <samicolon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107658}

--- chrome/browser/lacros/web_app_provider_bridge_lacros.h
+++ chrome/browser/lacros/web_app_provider_bridge_lacros.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_LACROS_WEB_APP_PROVIDER_BRIDGE_LACROS_H_
 #define CHROME_BROWSER_LACROS_WEB_APP_PROVIDER_BRIDGE_LACROS_H_
 
+#include "chrome/browser/web_applications/web_app_id.h"
 #include "chromeos/crosapi/mojom/web_app_service.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 
@@ -32,6 +33,8 @@ class WebAppProviderBridgeLacros : public mojom::WebAppProviderBridge {
       const std::string& app_id,
       GetWebApkCreationParamsCallback callback) override;
   void InstallMicrosoft365(InstallMicrosoft365Callback callback) override;
+  void GetSubAppIds(const web_app::AppId& app_id,
+                    GetSubAppIdsCallback callback) override;
 
  private:
   static void WebAppInstalledInArcImpl(
@@ -48,6 +51,9 @@ class WebAppProviderBridgeLacros : public mojom::WebAppProviderBridge {
       Profile* profile);
   static void InstallMicrosoft365Impl(InstallMicrosoft365Callback callback,
                                       Profile* profile);
+  static void GetSubAppIdsImpl(const web_app::AppId& app_id,
+                               GetSubAppIdsCallback callback,
+                               Profile* profile);
 
   mojo::Receiver<mojom::WebAppProviderBridge> receiver_{this};
 };
