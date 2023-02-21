c6e4e1843c7cb2bf339e219a1339f89a617bb7a2
Colin Kincaid <ckincaid@chromium.org>
2023-02-21 22:28:47 +0000

    multipaste: Eliminate unchecked calls to CalculateMainFormat()
    clipboard_history_util::CalculateMainFormat() returns an optional value
    that is not always checked. This CL reduces the number of unchecked uses
    to one, which is justified by a precondition and enforced by CQ tests.
    
    Bug: b/266947643
    Change-Id: Iaa815d5a1ecaaee1c69c1970204d42f314b081ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4277641
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107960}

--- ash/clipboard/views/clipboard_history_bitmap_item_view.cc
+++ ash/clipboard/views/clipboard_history_bitmap_item_view.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/clipboard/clipboard_history_item.h"
 #include "ash/clipboard/clipboard_history_resource_manager.h"
-#include "ash/clipboard/clipboard_history_util.h"
 #include "ash/clipboard/views/clipboard_history_delete_button.h"
 #include "ash/clipboard/views/clipboard_history_view_constants.h"
 #include "base/containers/contains.h"
@@ -315,8 +314,7 @@ ClipboardHistoryBitmapItemView::ClipboardHistoryBitmapItemView(
     views::MenuItemView* container)
     : ClipboardHistoryItemView(item_id, clipboard_history, container),
       resource_manager_(resource_manager),
-      data_format_(*clipboard_history_util::CalculateMainFormat(
-          GetClipboardHistoryItem()->data())) {
+      data_format_(GetClipboardHistoryItem()->main_format()) {
   switch (data_format_) {
     case ui::ClipboardInternalFormat::kHtml:
       SetAccessibleName(
