387ec1dde5dd7461002a55956341913548549187
Ankush Singh <ankushkush@google.com>
2023-02-21 17:35:31 +0000

    Fix namespace in read_anything_prefs
    Bug: 1401271
    Change-Id: I7bd58bc860b0a2516468114327ddce756e3e72c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4274625
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107806}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_prefs.cc
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_prefs.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/accessibility/read_anything.mojom.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 
-namespace prefs {
-
 #if !BUILDFLAG(IS_ANDROID)
+
+namespace prefs {
 // String to represent the user's preferred font name for the read anything UI.
 const char kAccessibilityReadAnythingFontName[] =
     "settings.a11y.read_anything.font_name";
--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_prefs.h
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_prefs.h
@@ -9,13 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/common/buildflags.h"
 
+#if !BUILDFLAG(IS_ANDROID)
+
 namespace user_prefs {
 class PrefRegistrySyncable;
 }
 
 namespace prefs {
-
-#if !BUILDFLAG(IS_ANDROID)
 
 extern const char kAccessibilityReadAnythingFontName[];
 extern const char kAccessibilityReadAnythingFontScale[];
