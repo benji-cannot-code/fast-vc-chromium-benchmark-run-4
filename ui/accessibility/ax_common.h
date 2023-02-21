39e02fbbbf6b2beffdc95b04e92e315d08564a16
Gyuyoung Kim <gyuyoung@igalia.com>
2023-02-21 17:31:25 +0000

    [iOS] Enable a11y fast build if USE_BLINK is true
    This CL enables AX_FAIL_FAST_BUILD macro if USE_BLINK is true
    in order to make BrowserAccessibilityManagerTest.TestOnNodeReparented
    pass on iOS content_unittests.
    
    TEST: BrowserAccessibilityManagerTest.TestOnNodeReparented
    Bug: 1418048
    Change-Id: I29a53c7ffa210ef7105bcec753d25e505dc975d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4273894
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107803}

--- ui/accessibility/ax_common.h
+++ ui/accessibility/ax_common.h
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ACCESSIBILITY_AX_COMMON_H_
 #define UI_ACCESSIBILITY_AX_COMMON_H_
 
+#include "build/blink_buildflags.h"
 #include "build/build_config.h"
 
 #if (!defined(NDEBUG) || defined(ADDRESS_SANITIZER) ||            \
      defined(LEAK_SANITIZER) || defined(MEMORY_SANITIZER) ||      \
      defined(THREAD_SANITIZER) || defined(UNDEFINED_SANITIZER) || \
      DCHECK_IS_ON()) &&                                           \
-    !BUILDFLAG(IS_IOS)
+    BUILDFLAG(USE_BLINK)
 // Enable fast fails on clusterfuzz and other builds used to debug Chrome,
 // in order to help narrow down illegal states more quickly.
 #define AX_FAIL_FAST_BUILD
