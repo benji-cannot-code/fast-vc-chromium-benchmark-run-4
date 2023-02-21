c569a70d42d0741a62e3eb7d47b3a577a5def5e6
Greg Thompson <grt@chromium.org>
2023-02-21 14:56:40 +0000

    [fuchsia] Make MockRealm available for use in other tests
    Bug: 1065710
    Change-Id: I43fa1864038ede3ead2fad66a583204ffe4153a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4271193
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107729}

---
+++ components/fuchsia_component_support/mock_realm.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/fuchsia_component_support/mock_realm.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace fuchsia_component_support {
+
+MockRealm::MockRealm(sys::OutgoingDirectory* outgoing)
+    : binding_(outgoing, this) {}
+
+MockRealm::~MockRealm() = default;
+
+void MockRealm::NotImplemented_(const std::string& name) {
+  ADD_FAILURE() << "NotImplemented_: " << name;
+}
+
+}  // namespace fuchsia_component_support
