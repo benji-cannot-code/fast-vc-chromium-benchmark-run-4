31911b762fdbb9ddd762e3aa431104704ac12982
Sam Thiesen <samicolon@google.com>
2023-02-21 09:18:21 +0000

    [Multi Apps] Add parent_app_id field to WebAppInstallInfo
    This replaces the parent_app_id field from WebAppInstallParams since the
    parent app id is a persisted configuration.
    
    
    Bug: 1090940
    Change-Id: Iafda4fcba7b8a05f26d60f71ffcb32483e666b8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4269600
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Sam Thiesen <samicolon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107642}

--- chrome/browser/web_applications/web_app_install_info.h
+++ chrome/browser/web_applications/web_app_install_info.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
 #include "chrome/browser/web_applications/scope_extension_info.h"
+#include "chrome/browser/web_applications/web_app_id.h"
 #include "components/services/app_service/public/cpp/file_handler.h"
 #include "components/services/app_service/public/cpp/icon_info.h"
 #include "components/services/app_service/public/cpp/protocol_handler_info.h"
@@ -337,6 +338,11 @@ struct WebAppInstallInfo {
   // Customisations to the tab strip. This field is only used when the
   // display mode is set to 'tabbed'.
   absl::optional<blink::Manifest::TabStrip> tab_strip;
+
+  // Id of the app that called the SUB_APP API to install this app. This field
+  // is only used when the app is installed as a sub app through the SUB_APP
+  // API.
+  absl::optional<web_app::AppId> parent_app_id;
 
  private:
   // Used this method in Clone() method. Use Clone() to deep copy explicitly.
