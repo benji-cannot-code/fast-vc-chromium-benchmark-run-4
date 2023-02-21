a65cf2b4561be116ffa1563e002ececa1b394189
Wolfgang Billenstein <bwolfgang@google.com>
2023-02-21 18:39:43 +0000

    [Fast Checkout] Make TTF expandable
    Makes the bottom sheet expandable when displaying addresses or credit
    cards.
    
    Bug: 1334642
    Change-Id: I4b9b758c3d635797505e2d52a342f418bb99dffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265116
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107844}

---
+++ chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutSheetState.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.fast_checkout;
+
+import org.chromium.chrome.browser.ui.fast_checkout.FastCheckoutProperties.ScreenType;
+
+/**
+ * Provides read-only information about the state of the Fast Checkout bottomsheet.
+ */
+public interface FastCheckoutSheetState {
+    /**
+     * Returns the current screen type of the bottomsheet.
+     */
+    @ScreenType
+    int getCurrentScreen();
+
+    /**
+     * Returns the number of Autofill profiles that would currently be displayed to the user.
+     */
+    int getNumOfAutofillProfiles();
+
+    /**
+     * Returns the number of credit cards that would currently be displayed to the user.
+     */
+    int getNumOfCreditCards();
+
+    /**
+     * Returns the height of the bottomsheet's container, i.e. the screen.
+     */
+    int getContainerHeight();
+}
--- chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/detail_screen/DetailScreenViewBinder.java
+++ chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/detail_screen/DetailScreenViewBinder.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.fast_checkout.detail_screen;
 
 import static org.chromium.chrome.browser.ui.fast_checkout.FastCheckoutProperties.DETAIL_SCREEN_BACK_CLICK_HANDLER;
-import static org.chromium.chrome.browser.ui.fast_checkout.FastCheckoutProperties.DETAIL_SCREEN_LIST_HEIGHT_IN_PX;
 import static org.chromium.chrome.browser.ui.fast_checkout.FastCheckoutProperties.DETAIL_SCREEN_MODEL_LIST;
 import static org.chromium.chrome.browser.ui.fast_checkout.FastCheckoutProperties.DETAIL_SCREEN_SETTINGS_CLICK_HANDLER;
 import static org.chromium.chrome.browser.ui.fast_checkout.FastCheckoutProperties.DETAIL_SCREEN_SETTINGS_MENU_TITLE;
@@ -83,9 +82,6 @@ public class DetailScreenViewBinder {
             adapter.registerType(DetailItemType.PROFILE, AutofillProfileItemViewBinder::create,
                     AutofillProfileItemViewBinder::bind);
             view.setAdapter(adapter);
-        } else if (propertyKey == DETAIL_SCREEN_LIST_HEIGHT_IN_PX) {
-            view.mSheetItemListContainer.getLayoutParams().height =
-                    model.get(DETAIL_SCREEN_LIST_HEIGHT_IN_PX);
         }
     }
 }
