76f1026e60ee6b80bb39c4dc78704c17e51aa21f
David Bienvenu <davidbienvenu@chromium.org>
2023-02-21 21:10:36 +0000

    win: remove glass from GlassAppWindowFrameViewWin
    Now that Win7/8 are deprecated, all our AppWindowFrameViewWin's are
    glass, so Glass isn't that meaningful in the names.
    
    Bug: 1408439
    Change-Id: Iccd53c2ea546a28619c5fcb4c10ffa6adf9cac20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265595
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107925}

--- chrome/browser/ui/views/apps/app_window_frame_view_win.h
+++ chrome/browser/ui/views/apps/app_window_frame_view_win.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_APPS_GLASS_APP_WINDOW_FRAME_VIEW_WIN_H_
-#define CHROME_BROWSER_UI_VIEWS_APPS_GLASS_APP_WINDOW_FRAME_VIEW_WIN_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_APPS_APP_WINDOW_FRAME_VIEW_WIN_H_
+#define CHROME_BROWSER_UI_VIEWS_APPS_APP_WINDOW_FRAME_VIEW_WIN_H_
 
 #include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -12,18 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/metadata/view_factory.h"
 #include "ui/views/window/non_client_view.h"
 
-// A glass style app window frame view.
-class GlassAppWindowFrameViewWin : public views::NonClientFrameView {
+// A Windows app window frame view.
+class AppWindowFrameViewWin : public views::NonClientFrameView {
  public:
-  METADATA_HEADER(GlassAppWindowFrameViewWin);
-  explicit GlassAppWindowFrameViewWin(views::Widget* widget);
-  GlassAppWindowFrameViewWin(const GlassAppWindowFrameViewWin&) = delete;
-  GlassAppWindowFrameViewWin& operator=(const GlassAppWindowFrameViewWin&) =
-      delete;
-  ~GlassAppWindowFrameViewWin() override;
+  METADATA_HEADER(AppWindowFrameViewWin);
+  explicit AppWindowFrameViewWin(views::Widget* widget);
+  AppWindowFrameViewWin(const AppWindowFrameViewWin&) = delete;
+  AppWindowFrameViewWin& operator=(const AppWindowFrameViewWin&) = delete;
+  ~AppWindowFrameViewWin() override;
 
-  // The insets to the client area due to the glass frame.
-  gfx::Insets GetGlassInsets() const;
+  // The insets to the client area due to the frame.
+  gfx::Insets GetFrameInsets() const;
 
   // Additional insets to the client area.  |monitor| is the monitor this
   // window is on.  Normally that would be determined from the HWND, but
@@ -52,10 +51,10 @@ class GlassAppWindowFrameViewWin : public views::NonClientFrameView {
 };
 
 BEGIN_VIEW_BUILDER(/* no export */,
-                   GlassAppWindowFrameViewWin,
+                   AppWindowFrameViewWin,
                    views::NonClientFrameView)
 END_VIEW_BUILDER
 
-DEFINE_VIEW_BUILDER(/* no export */, GlassAppWindowFrameViewWin)
+DEFINE_VIEW_BUILDER(/* no export */, AppWindowFrameViewWin)
 
-#endif  // CHROME_BROWSER_UI_VIEWS_APPS_GLASS_APP_WINDOW_FRAME_VIEW_WIN_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_APPS_APP_WINDOW_FRAME_VIEW_WIN_H_
