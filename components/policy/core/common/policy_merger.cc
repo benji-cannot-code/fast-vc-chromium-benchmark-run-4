38b0c7755a62fc51c8039b778fb3d7e8cc2d4126
Igor Ruvinov <igorruvinov@chromium.org>
2023-02-21 15:27:32 +0000

    Set policy source to merged only if merging from multiple sources.
    Previously, if the policy was eligible for merging, its source was set
    to "merged" even if its value originated from only one source.
    
    Following this change, policies eligible for merging whose value was set
    by one source will keep their original source. Policies whose value is
    merged from at least two sources will have their source updated to
    "merged".
    
    This change is implemented behind a Finch flag for increased safety.
    The feature list is not initialized when platform policies are first
    applied during browser startup. The feature value is eventually used to
    apply the correct merging logic before the browser launches since
    policies are applied multiple times during startup.
    
    Bug: 1399824, b/268362234
    Change-Id: I754061f532de5bc20c82e8b5bca3437b9b507347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4232963
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107746}

--- components/policy/core/common/policy_merger.cc
+++ components/policy/core/common/policy_merger.cc
@@ -3,13 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/policy/core/common/policy_merger.h"
+
 #include <array>
 #include <map>
 #include <set>
 
+#include "base/feature_list.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "components/policy/core/common/policy_merger.h"
+#include "components/policy/core/common/features.h"
 #include "components/policy/core/common/policy_pref_names.h"
 #include "components/policy/policy_constants.h"
 #include "components/strings/grit/components_strings.h"
@@ -113,8 +116,19 @@ bool PolicyListMerger::CanMerge(const std::string& policy_name,
   if (policy.source == POLICY_SOURCE_MERGED)
     return false;
 
-  if (policies_to_merge_.find("*") != policies_to_merge_.end())
-    return policy.value(base::Value::Type::LIST) != nullptr;
+  // `base::FeatureList` is not initialized when platform policies are first
+  // applied during browser startup. The feature value is eventually used to
+  // apply the correct merging logic before the browser launches since policies
+  // are applied multiple times during startup.
+  const bool can_merge_conflicts =
+      !base::FeatureList::GetInstance() ||
+      !base::FeatureList::IsEnabled(
+          policy::features::kPolicyMergeMultiSource) ||
+      policy.HasConflicts();
+  if (policies_to_merge_.find("*") != policies_to_merge_.end()) {
+    return can_merge_conflicts &&
+           policy.value(base::Value::Type::LIST) != nullptr;
+  }
 
   if (policies_to_merge_.find(policy_name) == policies_to_merge_.end())
     return false;
@@ -125,7 +139,7 @@ bool PolicyListMerger::CanMerge(const std::string& policy_name,
     return false;
   }
 
-  return true;
+  return can_merge_conflicts;
 }
 
 bool PolicyListMerger::AllowUserCloudPolicyMerging() const {
@@ -218,8 +232,19 @@ bool PolicyDictionaryMerger::CanMerge(const std::string& policy_name,
   const bool allowed_to_merge =
       allowed_policies_.find(policy_name) != allowed_policies_.end();
 
-  if (policies_to_merge_.find("*") != policies_to_merge_.end())
-    return allowed_to_merge && policy.value(base::Value::Type::DICT);
+  // `base::FeatureList` is not initialized when platform policies are first
+  // applied during browser startup. The feature value is eventually used to
+  // apply the correct merging logic before the browser launches since policies
+  // are applied multiple times during startup.
+  const bool can_merge_conflicts =
+      !base::FeatureList::GetInstance() ||
+      !base::FeatureList::IsEnabled(
+          policy::features::kPolicyMergeMultiSource) ||
+      policy.HasConflicts();
+  if (policies_to_merge_.find("*") != policies_to_merge_.end()) {
+    return allowed_to_merge && can_merge_conflicts &&
+           policy.value(base::Value::Type::DICT);
+  }
 
   if (policies_to_merge_.find(policy_name) == policies_to_merge_.end())
     return false;
@@ -237,7 +262,7 @@ bool PolicyDictionaryMerger::CanMerge(const std::string& policy_name,
     return false;
   }
 
-  return true;
+  return can_merge_conflicts;
 }
 
 bool PolicyDictionaryMerger::AllowUserCloudPolicyMerging() const {
