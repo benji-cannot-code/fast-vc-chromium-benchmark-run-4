7ab7bb5b50d59f609f503a0723948196ac474385
Anders Hartvoll Ruud <andruud@chromium.org>
2023-02-21 13:53:20 +0000

    [@scope] Parse <scope-start> and <scope-end> with arena
    This is a preparation for integrating @scope with css-nesting (not
    for performance reasons).
    
    Bug: 1280240
    Change-Id: Ide0bcf965bef51bad7f7089f56b4b68ab73d5559
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4272327
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107709}

--- third_party/blink/renderer/core/css/style_scope.cc
+++ third_party/blink/renderer/core/css/style_scope.cc
@@ -45,8 +45,10 @@ unsigned StyleScope::Specificity() const {
 StyleScope* StyleScope::Parse(CSSParserTokenRange prelude,
                               const CSSParserContext* context,
                               StyleSheetContents* style_sheet) {
-  CSSSelectorList* from = nullptr;
-  CSSSelectorList* to = nullptr;
+  HeapVector<CSSSelector> arena;
+
+  absl::optional<base::span<CSSSelector>> from;
+  absl::optional<base::span<CSSSelector>> to;
 
   prelude.ConsumeWhitespace();
 
@@ -62,8 +64,9 @@ StyleScope* StyleScope::Parse(CSSParserTokenRange prelude,
   // <scope-start>
   {
     auto block = prelude.ConsumeBlock();
-    from = CSSSelectorParser::ParseScopeBoundary(block, context, style_sheet);
-    if (!from) {
+    from = CSSSelectorParser::ParseScopeBoundary(block, context, style_sheet,
+                                                 arena);
+    if (!from.has_value()) {
       return nullptr;
     }
   }
@@ -77,8 +80,9 @@ StyleScope* StyleScope::Parse(CSSParserTokenRange prelude,
     }
 
     auto block = prelude.ConsumeBlock();
-    to = CSSSelectorParser::ParseScopeBoundary(block, context, style_sheet);
-    if (!to) {
+    to = CSSSelectorParser::ParseScopeBoundary(block, context, style_sheet,
+                                               arena);
+    if (!to.has_value()) {
       return nullptr;
     }
   }
@@ -89,7 +93,14 @@ StyleScope* StyleScope::Parse(CSSParserTokenRange prelude,
     return nullptr;
   }
 
-  return MakeGarbageCollected<StyleScope>(from, to);
+  CSSSelectorList* from_list =
+      from.has_value() ? CSSSelectorList::AdoptSelectorVector(from.value())
+                       : nullptr;
+  CSSSelectorList* to_list =
+      to.has_value() ? CSSSelectorList::AdoptSelectorVector(to.value())
+                     : nullptr;
+
+  return MakeGarbageCollected<StyleScope>(from_list, to_list);
 }
 
 void StyleScope::Trace(blink::Visitor* visitor) const {
