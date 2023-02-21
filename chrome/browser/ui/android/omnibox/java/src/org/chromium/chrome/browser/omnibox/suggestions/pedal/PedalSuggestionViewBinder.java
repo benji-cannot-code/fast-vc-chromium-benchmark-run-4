94a6b4f820f83253d751866d754df03807ba8672
Ender <ender@google.com>
2023-02-21 23:33:48 +0000

    Suggestions: Use Foreground color for Focus and Background for decor
    This change applies explicit meaning to different forms of suggestion
    decorations.
    Until now we have been using just Background to define both the static
    (normal) and dynamic (focused/selected/...) visuals of every
    suggestion element. This has made further works on suggestion
    dcorations more difficult.
    
    Bug: 1418077
    Change-Id: I35d05a46e08e75a54b4c8d63ab3120a0d81fc221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4276603
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1108012}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/pedal/PedalSuggestionViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/pedal/PedalSuggestionViewBinder.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.omnibox.suggestions.pedal;
 
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.view.View;
 
 import androidx.core.view.ViewCompat;
@@ -70,9 +69,8 @@ public final class PedalSuggestionViewBinder<T extends View>
             view.getPedalChipView().setOnClickListener(
                     omniboxPedalListItem.model.get(PedalViewProperties.ON_PEDAL_CLICK));
         } else if (SuggestionCommonProperties.COLOR_SCHEME == propertyKey) {
-            Drawable backgroundDrawable =
-                    BaseSuggestionViewBinder.getSelectableBackgroundDrawable(view, model);
-            view.getPedalView().setBackground(backgroundDrawable);
+            view.getPedalView().setForeground(
+                    BaseSuggestionViewBinder.getForegroundDrawable(model, view));
         } else if (SuggestionCommonProperties.LAYOUT_DIRECTION == propertyKey) {
             ViewCompat.setLayoutDirection(
                     view.getPedalView(), model.get(SuggestionCommonProperties.LAYOUT_DIRECTION));
