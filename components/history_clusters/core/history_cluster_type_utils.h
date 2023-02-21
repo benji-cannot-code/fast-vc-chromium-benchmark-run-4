654667bea3960b5ef12a04029195c92d87091d46
Roman Arora <romanarora@chromium.org>
2023-02-21 19:13:18 +0000

    Move history cluster types utils to components dir
    Moves helper functions for history_clusters type to mojom conversion
    to the components/history_clusters directory.
    
    See: https://chromium-review.googlesource.com/c/chromium/src/+/4198000/comment/1dd2e2d0_bc2494b4/
    Low-Coverage-Reason: Coverage tests to be added in a follow up.
    Bug: 1409691
    Change-Id: I6d1c0938699e9c876743a2defa0a093a037a904a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4267950
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107868}

--- components/history_clusters/core/history_cluster_type_utils.h
+++ components/history_clusters/core/history_cluster_type_utils.h
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_HISTORY_CLUSTERS_HISTORY_CLUSTER_TYPE_UTILS_H_
-#define CHROME_BROWSER_UI_WEBUI_HISTORY_CLUSTERS_HISTORY_CLUSTER_TYPE_UTILS_H_
+#ifndef COMPONENTS_HISTORY_CLUSTERS_CORE_HISTORY_CLUSTER_TYPE_UTILS_H_
+#define COMPONENTS_HISTORY_CLUSTERS_CORE_HISTORY_CLUSTER_TYPE_UTILS_H_
 
 #include "components/history_clusters/public/mojom/history_cluster_types.mojom-forward.h"
 
-class Profile;
+class TemplateURLService;
 
 namespace history {
 struct Cluster;
@@ -17,9 +17,9 @@ struct Cluster;
 namespace history_clusters {
 
 // Creates a `mojom::Cluster` from a `history_clusters::Cluster`.
-mojom::ClusterPtr ClusterToMojom(Profile* profile,
+mojom::ClusterPtr ClusterToMojom(const TemplateURLService* template_url_service,
                                  const history::Cluster cluster);
 
 }  // namespace history_clusters
 
-#endif  // CHROME_BROWSER_UI_WEBUI_HISTORY_CLUSTERS_HISTORY_CLUSTER_TYPE_UTILS_H_
+#endif  // COMPONENTS_HISTORY_CLUSTERS_CORE_HISTORY_CLUSTER_TYPE_UTILS_H_
