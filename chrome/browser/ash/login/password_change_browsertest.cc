449ab50b90876a34f178a6c3adfdb4261ba6ce16
Denis Kuznetsov <antrim@chromium.org>
2023-02-21 17:57:51 +0000

    Treat chrome as branded when checking for onboarding during user re-creation
    Bug: b:257225574
    Change-Id: I8dcae82d0238f5f3167898563222e86a0f590675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4272457
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107815}

--- chrome/browser/ash/login/password_change_browsertest.cc
+++ chrome/browser/ash/login/password_change_browsertest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/oobe_window_visibility_waiter.h"
 #include "chrome/browser/ash/login/test/session_manager_state_waiter.h"
 #include "chrome/browser/ash/login/ui/login_display_host.h"
+#include "chrome/browser/ash/login/wizard_context.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/lifetime/termination_notification.h"
@@ -162,6 +163,7 @@ class PasswordChangeTest : public PasswordChangeTestBase {
                                  {test_user_info_},
                                  &fake_gaia_,
                                  &cryptohome_};
+  base::AutoReset<bool> branded_build{&WizardContext::g_is_branded_build, true};
 
  private:
   base::FilePath GetTestingFilePath() const {
