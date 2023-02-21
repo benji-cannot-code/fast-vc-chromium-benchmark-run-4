ab73a2c0a154ecdcc8a3c8983ba396f07c18147c
Ningxin Hu <ningxin.hu@intel.com>
2023-02-21 02:04:26 +0000

    WebNN: Implement MLContext.compute() method
    This CL implements MLContext.compute() method according to the latest
    WebNN spec that fixes the input and output resources race condition
    issue of asynchronous execution [1]. In particular, this CL implements
    the TransferNamedArrayBufferViews() method for MLGraph that transfers
    the input and output ArrayBufferViews.
    MLGraphXnnpack::ComputeAsyncImpl() takes these transferred
    ArrayBufferViews, invokes the XNNPACK Runtime object in a worker thread
    and resolves the promise that returns the transferred ArrayBufferViews
    to user code with an MLComputeResult object.
    
    This CL implements ComputeAsyncTest of MLGraphXnnpackTest that tests
    ArrayBufferViews transferring and MLComputeResult. Other
    MLGraphXnnpackTest cases are updated to support asynchronous execution
    mode.
    
    This CL also passes additional WPT WebNN test cases that call
    asynchronous compute. The baselines are updated according to the test
    results.
    
    [1]: https://github.com/webmachinelearning/webnn/issues/318
    
    Bug: 1273291
    Change-Id: I90d130106cfabcb727f5cf11264caa49cea1d9e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237384
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107571}

--- third_party/blink/renderer/modules/ml/ml_context.cc
+++ third_party/blink/renderer/modules/ml/ml_context.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/ml/ml_context.h"
 
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/ml/ml.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -48,6 +49,31 @@ void MLContext::Trace(Visitor* visitor) const {
   visitor->Trace(ml_);
 
   ScriptWrappable::Trace(visitor);
+}
+
+ScriptPromise MLContext::compute(ScriptState* script_state,
+                                 MLGraph* graph,
+                                 const MLNamedArrayBufferViews& inputs,
+                                 const MLNamedArrayBufferViews& outputs,
+                                 ExceptionState& exception_state) {
+  if (!script_state->ContextIsValid()) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
+                                      "Invalid script state");
+    return ScriptPromise();
+  }
+
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+  auto promise = resolver->Promise();
+
+  if (graph->Context() != this) {
+    resolver->Reject(MakeGarbageCollected<DOMException>(
+        DOMExceptionCode::kDataError,
+        "The graph isn't built within this context."));
+  } else {
+    graph->ComputeAsync(inputs, outputs, resolver, exception_state);
+  }
+
+  return promise;
 }
 
 void MLContext::computeSync(MLGraph* graph,
--- third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_ML_GRAPH_TEST_BASE_H_
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_compute_result.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h"
@@ -43,15 +44,15 @@ class MLGraphTestBase : public ::testing::Test,
                          MLGraphBuilder* builder,
                          const MLNamedOperands& named_operands);
 
-  // Helper method for testinh both ComputeAsync() and ComputeSync() with the
+  // Helper method for testing both ComputeAsync() and ComputeSync() with the
   // same input/output buffers and expected results. If the graph computes
   // successfully, it returns nullptr and the results are produced into the
   // output buffers. Otherwise, it returns the pointer to the DOMException
   // thrown by the graph computing.
   DOMException* ComputeGraph(V8TestingScope& scope,
                              MLGraph* graph,
-                             const MLNamedArrayBufferViews& inputs,
-                             const MLNamedArrayBufferViews& outputs);
+                             MLNamedArrayBufferViews& inputs,
+                             MLNamedArrayBufferViews& outputs);
 
   // Test operations with different parameters such as tensor dimensions, data
   // layout. Each test case will builds a graph and computes it with input data
