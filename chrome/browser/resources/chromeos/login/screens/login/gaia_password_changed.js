289769b0f386565165db5127c5ed3cf02095f9a5
Denis Kuznetsov <antrim@chromium.org>
2023-02-21 13:36:22 +0000

    Update password changed screen according to latest UI mocks
    UI: https://docs.google.com/presentation/d/1YOgJVOuIl7JqZsaFKLyZoYecIZBNoKVh6ab8lJPd2Fw/edit?resourcekey=0-lEqjxzz1mnglJnV_JmifkQ#slide=id.g13ec87d00fb_0_179
    (slide 30)
    
    Bug: b:257225574
    Change-Id: If2090751d5ae79b153661230497268be9f896ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4274722
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107705}

--- chrome/browser/resources/chromeos/login/screens/login/gaia_password_changed.js
+++ chrome/browser/resources/chromeos/login/screens/login/gaia_password_changed.js
@@ -216,6 +216,11 @@ class GaiaPasswordChanged extends GaiaPasswordChangedBase {
   }
 
   /** @private */
+  onBackButtonClicked_() {
+    this.setUIStep(GaiaPasswordChangedUIState.PASSWORD);
+  }
+
+  /** @private */
   onAnimationFinish_() {
     this.focus();
   }
