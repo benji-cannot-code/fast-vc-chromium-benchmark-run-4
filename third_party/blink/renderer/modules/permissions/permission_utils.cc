2b12ab5eac4d50eb5ab0ef6c01a88d41b77ff69a
Matt Reichhoff <mreichhoff@chromium.org>
2023-02-21 16:01:38 +0000

    [saa] Switch permission name to hyphen
    This appears to match the spec.
    
    Bug: 1401266
    Change-Id: Ia25e3f0d321ab354ad5c24752022eea4c40805d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261807
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Reviewed-by: Johann Hofmann <johannhof@chromium.org>
    Reviewed-by: Brandon Maslen <brandm@microsoft.com>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107763}

--- third_party/blink/renderer/modules/permissions/permission_utils.cc
+++ third_party/blink/renderer/modules/permissions/permission_utils.cc
@@ -98,7 +98,7 @@ String PermissionNameToString(PermissionName name) {
     case PermissionName::NFC:
       return "nfc";
     case PermissionName::STORAGE_ACCESS:
-      return "storage_access";
+      return "storage-access";
     case PermissionName::WINDOW_MANAGEMENT:
       if (RuntimeEnabledFeatures::WindowManagementPermissionAliasEnabled()) {
         return "window-management";
--- third_party/blink/web_tests/external/wpt/storage-access-api/storage-access-permission.sub.https.window.js
+++ third_party/blink/web_tests/external/wpt/storage-access-api/storage-access-permission.sub.https.window.js
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   promise_test(async t => {
     const permission = await navigator.permissions.query({name: "storage-access"});
-    assert_equals(permission.name, "storage_access");
+    assert_equals(permission.name, "storage-access");
     assert_equals(permission.state, "prompt");
   }, "Permission default state can be queried");
 
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     await test_driver.set_permission({ name: 'storage-access' }, 'granted');
 
     const permission = await navigator.permissions.query({name: "storage-access"});
-    assert_equals(permission.name, "storage_access");
+    assert_equals(permission.name, "storage-access");
     assert_equals(permission.state, "granted");
   }, "Permission granted state can be queried");
 
@@ -62,7 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     await test_driver.set_permission({ name: 'storage-access' }, 'denied');
 
     const permission = await navigator.permissions.query({name: "storage-access"});
-    assert_equals(permission.name, "storage_access");
+    assert_equals(permission.name, "storage-access");
     assert_equals(permission.state, "denied");
 
     await test_driver.set_permission({ name: 'storage-access' }, 'prompt');
