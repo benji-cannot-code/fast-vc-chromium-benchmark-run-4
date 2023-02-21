8f74d2663bf8106fa708a55d811969fd14a6a118
Joe Mason <joenotcharles@google.com>
2023-02-21 14:04:15 +0000

    Remove browser_watcher (1/3): Move ExitCodeWatcher to chrome/app
    It's unrelated to the rest of the mechanisms in components/browser_watcher. The rest of components/browser_watcher will be deleted in followups.
    
    Also cleans up ExitCodeWatcher a bit:
    
    * Add missing IWYU headers
    * Remove LOG strings that are only useful in development
    
    Bug: 1415328
    Change-Id: Ib115cec2334a71451ebb97882c5300ef9a6e34b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261434
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107713}

--- chrome/app/exit_code_watcher_win_unittest.cc
+++ chrome/app/exit_code_watcher_win_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/browser_watcher/exit_code_watcher_win.h"
+#include "chrome/app/exit_code_watcher_win.h"
 
 #include <stdint.h>
 
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/multiprocess_func_list.h"
 
-namespace browser_watcher {
-
 namespace {
 
 MULTIPROCESS_TEST_MAIN(Sleeper) {
@@ -34,8 +32,7 @@ MULTIPROCESS_TEST_MAIN(Sleeper) {
 
 class ScopedSleeperProcess {
  public:
-   ScopedSleeperProcess() : is_killed_(false) {
-  }
+  ScopedSleeperProcess() : is_killed_(false) {}
 
   ~ScopedSleeperProcess() {
     if (process_.IsValid()) {
@@ -79,9 +76,7 @@ class ExitCodeWatcherTest : public testing::Test {
 
   ExitCodeWatcherTest() : cmd_line_(base::CommandLine::NO_PROGRAM) {}
 
-  void SetUp() override {
-    Super::SetUp();
-  }
+  void SetUp() override { Super::SetUp(); }
 
   base::Process OpenSelfWithAccess(uint32_t access) {
     return base::Process::OpenWithAccess(base::GetCurrentProcId(), access);
@@ -155,5 +150,3 @@ TEST_F(ExitCodeWatcherTest, ExitCodeWatcherStopWatching) {
   ASSERT_NO_FATAL_FAILURE(sleeper.Kill(kExitCode, true));
   base::PlatformThread::Sleep(TestTimeouts::tiny_timeout());
 }
-
-}  // namespace browser_watcher
