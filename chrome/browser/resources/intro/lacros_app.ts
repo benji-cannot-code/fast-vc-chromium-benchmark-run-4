1f8487302e8175e66f11a04e49bacc6780f0ea02
Jack Yammine <jyammine@google.com>
2023-02-21 14:55:40 +0000

    [fyfre] Fix FRE intro on lacros showing blank screen.
    Bug: 1358845
    Change-Id: I21418f194d2fc560b760eea543a0962c35d4a0d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4272270
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107728}

--- chrome/browser/resources/intro/lacros_app.ts
+++ chrome/browser/resources/intro/lacros_app.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/cr_elements/icons.html.js';
+import './strings.m.js';
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
