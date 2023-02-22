f58f9196f0dcfa5df5be2a480d3d45a0c5f32ee4
Tommy Nyquist <nyquist@chromium.org>
2023-02-22 17:28:26 +0000

    [android] Disable some MultiWindowUtilsTest tests.
    The tests have become flaky after this CL landed:
    https://chromium-review.googlesource.com/c/chromium/src/+/4199060
    
    R=salg@google.com
    
    Bug: 1417018
    Change-Id: Iad2d0ac9160694ed0d984c7da58a02224ed6cadb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4263087
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108409}

--- chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsTest.java
@@ -35,6 +35,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -131,6 +132,7 @@ public class MultiWindowUtilsTest {
     @Test
     @SmallTest
     @Feature("MultiWindow")
+    @DisabledTest(message = "https://crbug.com/1417018")
     public void testTabbedActivityForIntentOnlyActivity1IsRunning() {
         ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
         ChromeTabbedActivity2 activity2 = createSecondChromeTabbedActivity(activity1);
@@ -187,6 +189,7 @@ public class MultiWindowUtilsTest {
     @Test
     @SmallTest
     @Feature("MultiWindow")
+    @DisabledTest(message = "https://crbug.com/1417018")
     public void testTabbedActivity2TaskRunning() {
         ChromeTabbedActivity activity2 =
                 createSecondChromeTabbedActivity(mActivityTestRule.getActivity());
