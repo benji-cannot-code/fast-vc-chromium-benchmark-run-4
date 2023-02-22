efcf2fb6acdc646b81efdc9b80eabf1e3262beed
Eugene Zemtsov <eugene@chromium.org>
2023-02-22 08:44:29 +0000

    media: Add some video frame access integrity checks to the release mode
    Bug: 1412991
    Change-Id: I4bb4181045adad9fe3007af618a318afa59c8f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4278687
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108217}

--- media/base/video_frame.h
+++ media/base/video_frame.h
@@ -15,11 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_op.h"
+#include "base/compiler_specific.h"
 #include "base/functional/callback.h"
 #include "base/hash/md5.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/ref_counted.h"
+#include "base/notreached.h"
 #include "base/process/memory.h"
 #include "base/synchronization/lock.h"
 #include "base/thread_annotations.h"
@@ -539,8 +541,11 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   const gfx::Size& natural_size() const { return natural_size_; }
 
   int stride(size_t plane) const {
-    DCHECK(IsValidPlane(format(), plane));
-    DCHECK_LT(plane, layout_.num_planes());
+    if (UNLIKELY(!IsValidPlane(format(), plane) ||
+                 plane >= layout_.num_planes())) {
+      NOTREACHED();
+      return 0;
+    }
     return layout_.planes()[plane].stride;
   }
 
@@ -560,11 +565,19 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   const uint8_t* data(size_t plane) const {
     DCHECK(IsValidPlane(format(), plane));
     DCHECK(IsMappable());
+    if (UNLIKELY(plane >= kMaxPlanes)) {
+      NOTREACHED();
+      return nullptr;
+    }
     return data_[plane];
   }
   uint8_t* writable_data(size_t plane) {
     DCHECK(IsValidPlane(format(), plane));
     DCHECK(IsMappable());
+    if (UNLIKELY(plane >= kMaxPlanes)) {
+      NOTREACHED();
+      return nullptr;
+    }
     return data_[plane];
   }
 
