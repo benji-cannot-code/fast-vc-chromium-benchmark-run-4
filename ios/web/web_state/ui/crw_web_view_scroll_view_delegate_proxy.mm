1c9043e753e25f46bfe97c937b7e4ae9426481e3
Aliona DANGLA <alionadangla@google.com>
2023-02-22 18:02:19 +0000

    [iOS] Reset zoom state when the user is not performing a zoom gesture
    This CL reset the value of the zoom state to minimal when the user is
    not performing a zoom related gesture. This solve unexpected behavior
    with zoom state when for example, zoom is applied after going to
    landscape mode if you performed a zoom in and out in portrait.
    
    See videos:
    For bug 1249501,
    https://drive.google.com/file/d/1iGF8uB8blYp7kwHjvU7QkqTR2L4cn8iC/view?usp=share_link
    For bug 1302600
    https://drive.google.com/file/d/1tIsm8yfeDk57g8VZS4PmeEgKzHGT3fqW/view?usp=share_link
    
    Pair-programming with ajuma@
    
    Fixed: 1249501, 1302600
    Change-Id: I40732c3b63daffcb63a79dafc774d7910b2936aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4232582
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108431}

--- ios/web/web_state/ui/crw_web_view_scroll_view_delegate_proxy.mm
+++ ios/web/web_state/ui/crw_web_view_scroll_view_delegate_proxy.mm
@@ -19,6 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @property(nonatomic, weak) CRWWebViewScrollViewProxy* scrollViewProxy;
 
+// Return YES if the user is currently performing a zoom gestures.
+@property(nonatomic, assign) BOOL userIsZooming;
+
 @end
 
 // Calls to methods supported by CRWWebViewScrollViewProxyObserver are forwarded
@@ -201,13 +204,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self.delegateOfProxy
         scrollViewDidZoom:[self.scrollViewProxy asUIScrollView]];
   }
-  [self.scrollViewProxy.observers
-      webViewScrollViewDidZoom:self.scrollViewProxy];
+  if (self.userIsZooming) {
+    [self.scrollViewProxy.observers
+        webViewScrollViewDidZoom:self.scrollViewProxy];
+  } else {
+    if (@available(iOS 16.0, *)) {
+      // In iOS < 16 versions, changing the value of `zoomScale` calls
+      // `scrollViewDidZoom`.
+      scrollView.zoomScale = scrollView.minimumZoomScale;
+    }
+  }
 }
 
 - (void)scrollViewWillBeginZooming:(UIScrollView*)scrollView
                           withView:(UIView*)view {
   DCHECK_EQ(self.scrollViewProxy.underlyingScrollView, scrollView);
+  self.userIsZooming = YES;
   if ([self.delegateOfProxy
           respondsToSelector:@selector(scrollViewWillBeginZooming:withView:)]) {
     [self.delegateOfProxy
@@ -222,6 +234,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                        withView:(UIView*)view
                         atScale:(CGFloat)scale {
   DCHECK_EQ(self.scrollViewProxy.underlyingScrollView, scrollView);
+  self.userIsZooming = NO;
   if ([self.delegateOfProxy respondsToSelector:@selector
                             (scrollViewDidEndZooming:withView:atScale:)]) {
     [self.delegateOfProxy
