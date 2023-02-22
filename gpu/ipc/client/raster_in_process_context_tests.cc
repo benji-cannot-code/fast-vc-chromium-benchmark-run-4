b2af1be1a0183aeba5b784cc18b708cfba6af5b7
Colin Blundell <blundell@chromium.org>
2023-02-22 08:31:58 +0000

    [//gpu] Convert uses of SII::CreateSharedImage() to SIF
    This CL converts the remaining usages of
    SII::CreateSharedImage(ResourceFormat) in //gpu to the variant that
    takes in SharedImageFormat. I left some followup cleanup to do in
    webgpu_mailbox_unittest.cc to limit the amount of toil in this CL.
    
    Bug: 1414192
    Change-Id: I9ffdb7d90f0e6c15816ba486c6d857fa86e47a42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4270571
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108213}

--- gpu/ipc/client/raster_in_process_context_tests.cc
+++ gpu/ipc/client/raster_in_process_context_tests.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
-#include "components/viz/common/resources/resource_format.h"
+#include "components/viz/common/resources/shared_image_format.h"
 #include "components/viz/test/test_gpu_memory_buffer_manager.h"
 #include "gpu/command_buffer/client/raster_implementation.h"
 #include "gpu/command_buffer/client/shared_image_interface.h"
@@ -24,7 +24,8 @@ namespace gpu {
 
 namespace {
 
-constexpr viz::ResourceFormat kResourceFormat = viz::RGBA_8888;
+constexpr viz::SharedImageFormat kSharedImageFormat =
+    viz::SinglePlaneFormat::kRGBA_8888;
 constexpr gfx::Size kBufferSize(100, 100);
 
 class RasterInProcessCommandBufferTest : public ::testing::Test {
@@ -88,7 +89,7 @@ TEST_F(RasterInProcessCommandBufferTest, AllowedBetweenBeginEndRasterCHROMIUM) {
   uint32_t flags = gpu::SHARED_IMAGE_USAGE_RASTER |
                    gpu::SHARED_IMAGE_USAGE_OOP_RASTERIZATION;
   gpu::Mailbox mailbox = sii->CreateSharedImage(
-      kResourceFormat, kBufferSize, color_space, kTopLeft_GrSurfaceOrigin,
+      kSharedImageFormat, kBufferSize, color_space, kTopLeft_GrSurfaceOrigin,
       kPremul_SkAlphaType, flags, kNullSurfaceHandle);
   ri_->WaitSyncTokenCHROMIUM(sii->GenUnverifiedSyncToken().GetConstData());
 
