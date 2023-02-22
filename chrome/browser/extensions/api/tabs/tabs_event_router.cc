0409a1056551482d282de46e835522eeefaafb11
Christoph Schwering <schwering@google.com>
2023-02-22 18:14:34 +0000

    [Zoom] Fix ZoomController destruction in TabsEventRouter
    Bug: 1417363
    Change-Id: Ib98ef29474b470c04efff69e26806d1fd464e143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4281444
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Quick-Run: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108435}

--- chrome/browser/extensions/api/tabs/tabs_event_router.cc
+++ chrome/browser/extensions/api/tabs/tabs_event_router.cc
@@ -274,7 +274,9 @@ void TabsEventRouter::TabGroupedStateChanged(
 
 void TabsEventRouter::OnZoomControllerDestroyed(
     zoom::ZoomController* zoom_controller) {
-  zoom_scoped_observations_.RemoveObservation(zoom_controller);
+  if (zoom_scoped_observations_.IsObservingSource(zoom_controller)) {
+    zoom_scoped_observations_.RemoveObservation(zoom_controller);
+  }
 }
 
 void TabsEventRouter::OnZoomChanged(
@@ -611,8 +613,10 @@ void TabsEventRouter::RegisterForTabNotifications(WebContents* contents) {
 }
 
 void TabsEventRouter::UnregisterForTabNotifications(WebContents* contents) {
-  zoom_scoped_observations_.RemoveObservation(
-      ZoomController::FromWebContents(contents));
+  if (auto* zoom_controller = ZoomController::FromWebContents(contents);
+      zoom_scoped_observations_.IsObservingSource(zoom_controller)) {
+    zoom_scoped_observations_.RemoveObservation(zoom_controller);
+  }
   favicon_scoped_observations_.RemoveObservation(
       favicon::ContentFaviconDriver::FromWebContents(contents));
 
