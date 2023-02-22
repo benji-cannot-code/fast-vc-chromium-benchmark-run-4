97a757ce1fc406a33b59e7f601dc1a34612ae0f3
Nate Chapin <japhet@chromium.org>
2023-02-22 18:27:45 +0000

    Move commit for an intercept()ed navigation to NavigateEvent
    Change-Id: Ia569311ce08bbdb09c89f849f7bd75817fa4ac42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265692
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108451}

--- third_party/blink/renderer/core/navigation_api/navigate_event.h
+++ third_party/blink/renderer/core/navigation_api/navigate_event.h
@@ -29,6 +29,7 @@ class NavigateEventInit;
 class NavigationInterceptOptions;
 class ExceptionState;
 class FormData;
+class SoftNavigationEventScope;
 class ScriptPromise;
 class V8NavigationInterceptHandler;
 
@@ -44,11 +45,16 @@ class NavigateEvent final : public Event,
     return MakeGarbageCollected<NavigateEvent>(context, type, init);
   }
 
+  // Web-exposed constructor
   NavigateEvent(ExecutionContext* context,
                 const AtomicString& type,
                 NavigateEventInit* init);
 
-  void SetUrl(const KURL& url) { url_ = url; }
+  // Constructor used by DispatchNavigateEvent().
+  NavigateEvent(ExecutionContext* context,
+                NavigateEventInit* init,
+                NavigateEventDispatchParams* dispatch_params,
+                ScriptState* script_state);
 
   String navigationType() { return navigation_type_; }
   NavigationDestination* destination() { return destination_; }
@@ -62,6 +68,8 @@ class NavigateEvent final : public Event,
 
   void intercept(NavigationInterceptOptions*, ExceptionState&);
 
+  void DoCommit();
+
   void scroll(ExceptionState&);
   void PotentiallyProcessScrollBehavior();
 
@@ -73,8 +81,6 @@ class NavigateEvent final : public Event,
 
   void ResetFocusIfNeeded();
   bool ShouldSendAxEvents() const;
-
-  void SaveStateFromDestinationItem(HistoryItem*);
 
   // FocusedElementChangeObserver implementation:
   void DidChangeFocus() final;
@@ -96,9 +102,10 @@ class NavigateEvent final : public Event,
   ScriptValue info_;
   absl::optional<V8NavigationFocusReset> focus_reset_behavior_ = absl::nullopt;
   absl::optional<V8NavigationScrollBehavior> scroll_behavior_ = absl::nullopt;
-  absl::optional<HistoryItem::ViewState> history_item_view_state_;
 
-  KURL url_;
+  Member<NavigateEventDispatchParams> dispatch_params_;
+  std::unique_ptr<SoftNavigationEventScope> soft_navigation_scope_;
+
   bool has_navigation_actions_ = false;
   HeapVector<ScriptPromise> navigation_action_promises_list_;
   HeapVector<Member<V8NavigationInterceptHandler>>
