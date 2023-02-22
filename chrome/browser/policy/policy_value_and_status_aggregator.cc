b3b13793bfa6e180185059b7d5e15b3c32977638
Salma Elmahallawy <esalma@google.com>
2023-02-22 16:09:25 +0000

    [PolicyLogs] Policy Validation Logs Cleanup + New Ones
    This CL removes the logs that are redundant. The logs were being logged
    for policy errors twice as they are all printed in
    ConfigurationPolicyPrefStore::LogErrors() function.
    The new logs show the machine policy and user policy status.
    
    Bug: b/251799820
    Change-Id: Ice213334cf7161da9d7e8c551190c646907c7078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4277710
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108362}

--- chrome/browser/policy/policy_value_and_status_aggregator.cc
+++ chrome/browser/policy/policy_value_and_status_aggregator.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/value_provider/policy_value_provider.h"
 #include "components/policy/core/browser/webui/policy_status_provider.h"
 #include "components/policy/core/browser/webui/policy_webui_constants.h"
+#include "components/policy/core/common/policy_logger.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ash/policy/active_directory/active_directory_policy_manager.h"
@@ -209,9 +210,17 @@ PolicyValueAndStatusAggregator::CreateDefaultPolicyValueAndStatusAggregator(
   policy::MachineLevelUserCloudPolicyManager* manager =
       g_browser_process->browser_policy_connector()
           ->machine_level_user_cloud_policy_manager();
-  if (manager)
+  if (manager) {
+    policy::BrowserDMTokenStorage* dmTokenStorage =
+        policy::BrowserDMTokenStorage::Get();
+    LOG_POLICY(INFO, POLICY_PROCESSING)
+        << "Retrieved Enrollment Token = "
+        << dmTokenStorage->RetrieveEnrollmentToken()
+        << " and client ID = " << dmTokenStorage->RetrieveClientId();
+
     aggregator->AddPolicyStatusProvider(
         kMachineStatusKey, GetMachinePolicyStatusProvider(manager));
+  }
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
     // Updater policies.
@@ -229,6 +238,9 @@ PolicyValueAndStatusAggregator::~PolicyValueAndStatusAggregator() = default;
 base::Value::Dict PolicyValueAndStatusAggregator::GetAggregatedPolicyStatus() {
   base::Value::Dict status;
   for (auto& status_provider_description_pair : status_providers_) {
+    DVLOG_POLICY(3, POLICY_PROCESSING)
+        << status_provider_description_pair.first
+        << " status: " << status_provider_description_pair.second->GetStatus();
     status.Set(status_provider_description_pair.first,
                status_provider_description_pair.second->GetStatus());
   }
