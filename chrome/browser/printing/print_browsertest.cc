1ea1a298ec9535f812249a0b6941645cd91b079b
Alan Screen <awscreen@chromium.org>
2023-02-22 19:35:29 +0000

    Improve TestPrintingContext setting simulation
    Platform implementations of PrintingContext apply PrintSettings to the
    respective device contexts.  Once the upper printing layers call
    TakeAndResetSettings(), the PrintSettings values no longer reflect what
    the printer driver's device context are set for.
    
    Mimic this behavior by including an extra PrintSettings field in
    TestPrintingContext which represents the applied settings in a
    platform's device context.
    
    Modify the TestPrintingContext callback used to notify when a new
    document is called.  Expand this to expose the applied settings of a
    platform's device context.
    
    Introduce a new TestPrintingContext call to allow test frameworks to
    make test-specific choices for AskUserForSettings().  This allows for
    extra refinement of system print dialog tests.
    
    Update system print tests to include verification that the settings
    requested from a simulated system print dialog are correctly propagated
    to the start of printing.  This illustrates an existing issue that the
    settings are not always propagating properly.
    
    Bug: 1414968
    Change-Id: Ie8b23f390ab07d6c95ea9883e915ded9bdae4aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242561
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108492}

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -155,13 +155,8 @@ constexpr int kDefaultDocumentCookie = 1234;
 constexpr char kFakeDmToken[] = "fake-dm-token";
 #endif  // BUILDFLAG(ENABLE_PRINT_CONTENT_ANALYSIS)
 
-std::unique_ptr<TestPrintingContext> MakeDefaultTestPrintingContext(
-    PrintingContext::Delegate* delegate,
-    bool skip_system_calls,
+std::unique_ptr<PrintSettings> MakeDefaultPrintSettings(
     const std::string& printer_name) {
-  auto context =
-      std::make_unique<TestPrintingContext>(delegate, skip_system_calls);
-
   // Setup a sample page setup, which is needed to pass checks in
   // `PrintRenderFrameHelper` that the print params are valid.
   constexpr gfx::Size kPhysicalSize = gfx::Size(200, 200);
@@ -176,8 +171,29 @@ std::unique_ptr<TestPrintingContext> MakeDefaultTestPrintingContext(
   settings->set_dpi(kTestPrintingDpi);
   settings->set_page_setup_device_units(kPageSetup);
   settings->set_device_name(base::ASCIIToUTF16(printer_name));
-  context->SetDeviceSettings(printer_name, std::move(settings));
+  return settings;
+}
+
+std::unique_ptr<TestPrintingContext> MakeDefaultTestPrintingContext(
+    PrintingContext::Delegate* delegate,
+    bool skip_system_calls,
+    const std::string& printer_name) {
+  auto context =
+      std::make_unique<TestPrintingContext>(delegate, skip_system_calls);
+
+  context->SetDeviceSettings(printer_name,
+                             MakeDefaultPrintSettings(printer_name));
   return context;
+}
+
+// Make some settings which are different than the generated defaults, to
+// be used if test calls `AskUserForSettings()`.
+std::unique_ptr<PrintSettings> MakeUserModifiedPrintSettings(
+    const std::string& printer_name) {
+  std::unique_ptr<PrintSettings> settings =
+      MakeDefaultPrintSettings(printer_name);
+  settings->set_copies(kTestPrintSettingsCopies + 1);
+  return settings;
 }
 
 void OnDidUpdatePrintSettings(
@@ -242,8 +258,10 @@ class BrowserPrintingContextFactoryForTest
       context->SetAskUserForSettingsCanceled();
 #endif
 
-    context->SetNewDocumentCalledClosure(base::BindRepeating(
-        &BrowserPrintingContextFactoryForTest::NewDocumentCalled,
+    context->SetUserSettings(*MakeUserModifiedPrintSettings(printer_name_));
+
+    context->SetOnNewDocumentCallback(base::BindRepeating(
+        &BrowserPrintingContextFactoryForTest::OnNewDocument,
         base::Unretained(this)));
 
     return std::move(context);
@@ -293,9 +311,16 @@ class BrowserPrintingContextFactoryForTest
   }
 #endif
 
-  void NewDocumentCalled() { ++new_document_called_count_; }
+  void OnNewDocument(const PrintSettings& settings) {
+    ++new_document_called_count_;
+    document_print_settings_ = settings;
+  }
 
   int new_document_called_count() { return new_document_called_count_; }
+
+  const absl::optional<PrintSettings>& document_print_settings() const {
+    return document_print_settings_;
+  }
 
  private:
   std::string printer_name_;
@@ -313,6 +338,7 @@ class BrowserPrintingContextFactoryForTest
   bool cancel_on_ask_user_for_settings_ = false;
 #endif
   int new_document_called_count_ = 0;
+  absl::optional<PrintSettings> document_print_settings_;
 };
 
 class PrintPreviewObserver : PrintPreviewUI::TestDelegate {
@@ -1090,6 +1116,10 @@ class PrintBrowserTest : public InProcessBrowserTest {
 
   void set_rendered_page_count(uint32_t page_count) {
     rendered_page_count_ = page_count;
+  }
+
+  const absl::optional<PrintSettings>& document_print_settings() const {
+    return test_printing_context_factory_.document_print_settings();
   }
 
  private:
@@ -3593,6 +3623,8 @@ IN_PROC_BROWSER_TEST_P(SystemAccessProcessPrintBrowserTest,
     // updates to support this scenario with out-of-process are in place.
   }
 #endif
+  ASSERT_EQ(*MakeUserModifiedPrintSettings("printer1"),
+            *document_print_settings());
   EXPECT_EQ(error_dialog_shown_count(), 0u);
   EXPECT_EQ(print_job_destruction_count(), 1);
 }
@@ -3646,6 +3678,10 @@ IN_PROC_BROWSER_TEST_P(SystemAccessProcessServicePrintBrowserTest,
   EXPECT_EQ(use_default_settings_result(), mojom::ResultCode::kSuccess);
   EXPECT_EQ(ask_user_for_settings_result(), mojom::ResultCode::kSuccess);
 #endif
+  // TODO(crbug.com/1414968)  Correct expectation once system print settings
+  // are properly reflected at start of job print.
+  ASSERT_NE(*MakeUserModifiedPrintSettings("printer1"),
+            *document_print_settings());
   EXPECT_EQ(start_printing_result(), mojom::ResultCode::kSuccess);
 #if BUILDFLAG(IS_WIN)
   // TODO(crbug.com/1008222)  Include Windows coverage of
