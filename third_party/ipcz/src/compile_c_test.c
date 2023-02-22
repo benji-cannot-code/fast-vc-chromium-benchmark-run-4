0d65ca6a6722bd5257340336d61d465e0712f809
Ken Rockot <rockot@google.com>
2023-02-22 00:33:08 +0000

    ipcz: Fix C compile
    ipcz.h should be suitable for C99 compilation.
    
    This CL fixes some missing `struct` keywords on typenames and replaces a
    bool in IpczCreateNodeOptions with an integer flags field.
    
    Bug: None
    Change-Id: I720f0e9d979962db88a2c7f755dc58cc1508611a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4275242
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108052}

---
+++ third_party/ipcz/src/compile_c_test.c
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Basic compilation test to ensure that we can include ipcz.h from a pure C
+// source file and have it compile successfully. There's no actual test to run
+// here.
+#include "ipcz/ipcz.h"
