18490dd6bf662aeee953d0d5ee203075a21ecc96
Lei Zhang <thestig@chromium.org>
2023-02-22 18:30:28 +0000

    Fix PwgRasterSettings initialization in PwgRasterConverter.
    In PwgRasterConverter::GetBitmapSettings(), a PwgRasterSettings
    variable's `duplex_mode` isn't always getting initialized. Initialize it
    to the proper value to make up for the accidental deletion in
    https://crrev.com/631626. While looking at the code, also make sure
    `odd_page_transform` is always initialized.
    
    The ExtensionPrinterHandlerTest.PrintPwgNonDefaultSettings test already
    exercises this code, but it did not verify the `duplex_mode` value. This
    is why the test has not been able to discover this issue. Add this
    missing check to the relevant ExtensionPrinterHandlerTest test cases.
    
    Change-Id: Ibfe669fb894a2f603fcae5d18c2b9971385c6007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4278397
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108454}

--- chrome/browser/printing/pwg_raster_converter.cc
+++ chrome/browser/printing/pwg_raster_converter.cc
@@ -254,12 +254,15 @@ PwgRasterSettings PwgRasterConverter::GetBitmapSettings(
       result.odd_page_transform = TRANSFORM_NORMAL;
       break;
     case cloud_devices::printer::DuplexType::LONG_EDGE:
+      result.duplex_mode = mojom::DuplexMode::kLongEdge;
       if (document_sheet_back ==
           cloud_devices::printer::DocumentSheetBack::ROTATED) {
         result.odd_page_transform = TRANSFORM_ROTATE_180;
       } else if (document_sheet_back ==
                  cloud_devices::printer::DocumentSheetBack::FLIPPED) {
         result.odd_page_transform = TRANSFORM_FLIP_VERTICAL;
+      } else {
+        result.odd_page_transform = TRANSFORM_NORMAL;
       }
       break;
     case cloud_devices::printer::DuplexType::SHORT_EDGE:
@@ -270,6 +273,8 @@ PwgRasterSettings PwgRasterConverter::GetBitmapSettings(
       } else if (document_sheet_back ==
                  cloud_devices::printer::DocumentSheetBack::FLIPPED) {
         result.odd_page_transform = TRANSFORM_FLIP_HORIZONTAL;
+      } else {
+        result.odd_page_transform = TRANSFORM_NORMAL;
       }
       break;
   }
