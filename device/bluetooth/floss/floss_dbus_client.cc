c5e77509cda0dfa30b30a78b0db83e7b43349d69
Manish Mandlik <mmandlik@google.com>
2023-02-22 04:43:13 +0000

    Add support to enable devcoredump in floss
    Use a feature flag to enable/disable bluetooth devcoredump.
    
    BUG=b:203034370
    TEST=Change chrome flag and verify changes reflect in the
         '/sys/class/bluetooth/hci0/device/coredump_disabled' file
    
    Change-Id: I27fef230b7353937fa01c7c52e44e20661be18f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4228041
    Commit-Queue: Manish Mandlik <mmandlik@chromium.org>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108167}

--- device/bluetooth/floss/floss_dbus_client.cc
+++ device/bluetooth/floss/floss_dbus_client.cc
@@ -260,6 +260,7 @@ const char kSetDebugLogging[] = "SetDebugLogging";
 
 namespace experimental {
 const char kSetLLPrivacy[] = "SetLLPrivacy";
+const char kSetDevCoredump[] = "SetDevCoredump";
 }  // namespace experimental
 
 namespace {
--- device/bluetooth/floss/floss_manager_client.cc
+++ device/bluetooth/floss/floss_manager_client.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "dbus/message.h"
 #include "dbus/object_proxy.h"
 #include "device/bluetooth/bluez/bluez_features.h"
+#include "device/bluetooth/chromeos_platform_features.h"
 #include "device/bluetooth/floss/floss_dbus_client.h"
 #include "device/bluetooth/floss/floss_features.h"
 
@@ -200,6 +201,12 @@ void FlossManagerClient::SetLLPrivacy(ResponseCallback<Void> callback,
                                enable);
 }
 
+void FlossManagerClient::SetDevCoredump(ResponseCallback<Void> callback,
+                                        const bool enable) {
+  CallExperimentalMethod<Void>(std::move(callback),
+                               experimental::kSetDevCoredump, enable);
+}
+
 // Register manager client against manager.
 void FlossManagerClient::RegisterWithManager() {
   DCHECK(!manager_available_);
@@ -291,6 +298,17 @@ void FlossManagerClient::Init(dbus::Bus* bus,
                   kSetFlossRetryDelayMs,
                   base::BindOnce(&FlossManagerClient::CompleteSetFlossEnabled,
                                  weak_ptr_factory_.GetWeakPtr()));
+
+#if BUILDFLAG(IS_CHROMEOS)
+  SetDevCoredump(base::BindOnce([](DBusResult<Void> ret) {
+                   if (!ret.has_value()) {
+                     LOG(ERROR) << "Fail to set devcoredump.\n";
+                   }
+                 }),
+                 base::FeatureList::IsEnabled(
+                     chromeos::bluetooth::features::kBluetoothCoredump));
+#endif  // BUILDFLAG(IS_CHROMEOS)
+
   SetLLPrivacy(
       base::BindOnce([](DBusResult<Void> ret) {
         if (!ret.has_value())
