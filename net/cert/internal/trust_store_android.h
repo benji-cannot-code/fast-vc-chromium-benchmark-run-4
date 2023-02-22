7be2e98f1071d8c331f28adca4a943d29bd0cfdb
Hubert Chao <hchao@chromium.org>
2023-02-22 23:23:11 +0000

    TrustStoreAndroid: Listen for changes to the system trust store and reload roots upon changes.
    Change-Id: Ie26d5d95edb8ab2c2c4a41405d04404ebe9e1cff
    Bug: 1365571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226436
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108650}

--- net/cert/internal/trust_store_android.h
+++ net/cert/internal/trust_store_android.h
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_CERT_INTERNAL_TRUST_STORE_ANDROID_H_
 #define NET_CERT_INTERNAL_TRUST_STORE_ANDROID_H_
 
+#include <atomic>
+
 #include "base/memory/ptr_util.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/synchronization/lock.h"
 #include "net/base/net_export.h"
+#include "net/cert/cert_database.h"
 #include "net/cert/pki/trust_store.h"
 #include "net/cert/pki/trust_store_in_memory.h"
 
@@ -17,7 +21,8 @@ namespace net {
 // TrustStoreAndroid is an implementation of TrustStore which uses the Android
 // cert systems to find user-added trust anchors for path building. It ignores
 // the Android builtin trust anchors.
-class NET_EXPORT TrustStoreAndroid : public TrustStore {
+class NET_EXPORT TrustStoreAndroid : public TrustStore,
+                                     public CertDatabase::Observer {
  public:
   TrustStoreAndroid();
   ~TrustStoreAndroid() override;
@@ -33,16 +38,28 @@ class NET_EXPORT TrustStoreAndroid : public TrustStore {
   CertificateTrust GetTrust(const ParsedCertificate* cert,
                             base::SupportsUserData* debug_data) override;
 
+  // CertDatabase::Observer:
+  void OnCertDBChanged() override;
+
+  // Have this object start listening for CertDB changes.
+  // This function is not thread safe, and must be called from a sequence.
+  void ObserveCertDBChanges();
+
  private:
+  bool is_observing_certdb_changes = false;
+
   // Inner Impl class for use in initializing stores.
   class Impl;
 
   // Loads user settings from Windows CertStores if not already done and
-  // returns pointer to the Impl.
-  Impl* MaybeInitializeAndGetImpl();
+  // returns scoped_refptr<Impl>.
+  scoped_refptr<Impl> MaybeInitializeAndGetImpl();
 
   base::Lock init_lock_;
-  std::unique_ptr<Impl> impl_ GUARDED_BY(init_lock_);
+  scoped_refptr<Impl> impl_ GUARDED_BY(init_lock_);
+  // Generation number that is incremented whenever the backing Android trust
+  // store changes.
+  std::atomic_int generation_ = 0;
 };
 
 }  // namespace net
