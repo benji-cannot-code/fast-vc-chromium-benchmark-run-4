b1f5cb62af84160056ef55dc0ee39ad50234913e
Lingqi Chi <lingqi@chromium.org>
2023-02-22 02:33:09 +0000

    SearchPre: fix prerendering serving reason
    If a loader serves to prerendering navigation, it should report
    kServed. kPrerendered is supposed to be reported when the loader
    has been served to annother prerendering navigation.
    
    Bug: 1295170
    Change-Id: I1c873e370164dc53e2dea1353a861b4cf611042a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4273887
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108121}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.cc
@@ -471,7 +471,7 @@ SearchPrefetchService::TakePrerenderFromMemoryCache(
   // is about to expire.
   DCHECK_NE(iter->second->current_status(),
             SearchPrefetchStatus::kRequestFailed);
-  recorder.reason_ = SearchPrefetchServingReason::kPrerendered;
+  recorder.reason_ = SearchPrefetchServingReason::kServed;
 
   iter->second->MarkPrefetchAsPrerendered();
   std::unique_ptr<SearchPrefetchURLLoader> response =
