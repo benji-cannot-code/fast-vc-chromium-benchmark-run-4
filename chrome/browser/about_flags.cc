6ff50d67163c5c022037b6df204055676ded1541
Jared Saul <jsaul@google.com>
2023-02-22 18:53:27 +0000

    [AF] Introduce alternate secure data type for credit card upload save
    Change-Id: Id1ef5bd876027d0ff3410c41ba2081560e011f80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279141
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108469}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -9423,6 +9423,12 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAll,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableNewCardArtAndNetworkImages)},
+    {"autofill-upstream-use-alternate-secure-data-type",
+     flag_descriptions::kAutofillUpstreamUseAlternateSecureDataTypeName,
+     flag_descriptions::kAutofillUpstreamUseAlternateSecureDataTypeDescription,
+     kOsAll,
+     FEATURE_VALUE_TYPE(
+         autofill::features::kAutofillUpstreamUseAlternateSecureDataType)},
 
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
