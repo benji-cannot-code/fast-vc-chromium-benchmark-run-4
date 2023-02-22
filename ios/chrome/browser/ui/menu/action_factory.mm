dd5c1fd6b655f9a050098ee7967f9a80866f7da5
Ewann Pelle <ewannpv@chromium.org>
2023-02-22 11:45:18 +0000

    [iOS][PT] Fix pin / unpin symbol
    Fixed: 1418002
    Change-Id: Ifb242a53f203b2d74cf20af4108d649d0f1003a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4274630
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108262}

--- ios/chrome/browser/ui/menu/action_factory.mm
+++ ios/chrome/browser/ui/menu/action_factory.mm
@@ -78,7 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (UIAction*)actionToPinTabWithBlock:(ProceduralBlock)block {
-  UIImage* image = UseSymbols() ? CustomSymbolWithPointSize(
+  UIImage* image = UseSymbols() ? DefaultSymbolWithPointSize(
                                       kPinSymbol, kSymbolActionPointSize)
                                 : [UIImage imageNamed:@"pin"];
   return [self
@@ -89,7 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (UIAction*)actionToUnpinTabWithBlock:(ProceduralBlock)block {
-  UIImage* image = UseSymbols() ? CustomSymbolWithPointSize(
+  UIImage* image = UseSymbols() ? DefaultSymbolWithPointSize(
                                       kPinSlashSymbol, kSymbolActionPointSize)
                                 : [UIImage imageNamed:@"unpin"];
   return [self
