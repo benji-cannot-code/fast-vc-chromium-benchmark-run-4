861b3f6b6f3ae8f62821b4a296c9ce7cde31f9ba
Andy Phan <andyphan@chromium.org>
2023-02-22 00:12:29 +0000

    Provide a default DPI if DPI is missing
    When the DPI entry is missing, the page size, content size, and
    printable area of PrintSettings will be empty. Some printers may not set
    their DPI, so provide a OS-dependent default DPI.
    
    Change-Id: Ic18cd6543801228bd18e69e3b9607c48250551d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4222980
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108038}

--- printing/backend/cups_helper_unittest.cc
+++ printing/backend/cups_helper_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/mojom/print.mojom.h"
 #include "printing/print_settings.h"
 #include "printing/printing_utils.h"
+#include "printing/units.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -765,6 +766,16 @@ TEST(PrintBackendCupsHelperTest,
 }
 
 TEST(PrintBackendCupsHelperTest, PpdParsingResolutionNoResolution) {
+  // If the PPD does not have a valid resolution, the DPI should still be set to
+  // an OS-dependent default value.
+#if BUILDFLAG(IS_MAC)
+  constexpr gfx::Size kExpectedDpi(kDefaultMacDpi, kDefaultMacDpi);
+#elif BUILDFLAG(IS_LINUX)
+  constexpr gfx::Size kExpectedDpi(kPixelsPerInch, kPixelsPerInch);
+#else
+  constexpr gfx::Size kExpectedDpi(kDefaultPdfDpi, kDefaultPdfDpi);
+#endif
+
   constexpr char kTestPpdData[] =
       R"(*PPD-Adobe: "4.3"
 *OpenUI *Resolution/Resolution: PickOne
@@ -773,12 +784,12 @@ TEST(PrintBackendCupsHelperTest, PpdParsingResolutionNoResolution) {
   PrinterSemanticCapsAndDefaults caps;
   EXPECT_TRUE(ParsePpdCapabilities(/*dest=*/nullptr, /*locale=*/"",
                                    kTestPpdData, &caps));
-  EXPECT_TRUE(caps.dpis.empty());
-  EXPECT_TRUE(caps.default_dpi.IsEmpty());
+  EXPECT_EQ(std::vector<gfx::Size>{kExpectedDpi}, caps.dpis);
+  EXPECT_EQ(caps.default_dpi, kExpectedDpi);
   EXPECT_TRUE(ParsePpdCapabilities(/*dest=*/nullptr, /*locale=*/"",
                                    "*PPD-Adobe: \"4.3\"", &caps));
-  EXPECT_TRUE(caps.dpis.empty());
-  EXPECT_TRUE(caps.default_dpi.IsEmpty());
+  EXPECT_EQ(std::vector<gfx::Size>{kExpectedDpi}, caps.dpis);
+  EXPECT_EQ(caps.default_dpi, kExpectedDpi);
 }
 
 TEST(PrintBackendCupsHelperTest, PpdParsingResolutionNoDefaultResolution) {
