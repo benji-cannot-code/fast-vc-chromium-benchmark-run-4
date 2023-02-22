107f41035ade07a978399bf5910dd321a6c7629e
Samar <samarchehade@google.com>
2023-02-22 13:52:08 +0000

    [Signin][Android] Add enterprise policy support to the signed-out avatar.
    For users with sign-in disabled by their enterprise policy, the signed-out avatar should lead to the Settings page, which gives more details about the policy.
    
    Bug: 1385758
    Change-Id: I033b88c67b5ba7ebe10bf98b474e426fca84e1c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4253888
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108297}

--- chrome/android/java/src/org/chromium/chrome/browser/identity_disc/IdentityDiscController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/identity_disc/IdentityDiscController.java
@@ -31,6 +31,7 @@ import org.chromium.chrome.browser.signin.SyncConsentActivityLauncherImpl;
 import org.chromium.chrome.browser.signin.services.DisplayableProfileData;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.ProfileDataCache;
+import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tasks.ReturnToChromeUtil;
 import org.chromium.chrome.browser.toolbar.ButtonData;
@@ -391,7 +392,10 @@ public class IdentityDiscController implements NativeInitObserver, ProfileDataCa
 
     private void onClick() {
         recordIdentityDiscUsed();
-        if (getSignedInAccountInfo() == null) {
+
+        SigninManager signinManager = IdentityServicesProvider.get().getSigninManager(
+                Profile.getLastUsedRegularProfile());
+        if (getSignedInAccountInfo() == null && !signinManager.isSigninDisabledByPolicy()) {
             SyncConsentActivityLauncherImpl.get().launchActivityIfAllowed(
                     mContext, SigninAccessPoint.NTP_SIGNED_OUT_ICON);
         } else {
