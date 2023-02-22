5bff9fcf522f082bbaa49edc618752c9156ca23b
François Degros <fdegros@chromium.org>
2023-02-22 03:59:57 +0000

    DriveFsAsyncFileUtil: Ensure PinManager is retrieved in UI thread
    BUG=chromium:1415008
    TEST=Manual testing
    
    Change-Id: I2251240df4a1afafe53db93de3d8a4c02f5c8644
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279063
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Marcello Salomao <msalomao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108153}

--- chrome/browser/ash/drive/drive_integration_service.cc
+++ chrome/browser/ash/drive/drive_integration_service.cc
@@ -83,11 +83,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/strings/grit/ui_chromeos_strings.h"
 
-using content::BrowserContext;
-using content::BrowserThread;
-
 namespace drive {
 namespace {
+
+using content::BrowserContext;
+using content::BrowserThread;
+using drivefs::pinning::PinManager;
 
 // Name of the directory used to store metadata.
 const base::FilePath::CharType kMetadataDirectory[] = FILE_PATH_LITERAL("meta");
@@ -1088,8 +1089,9 @@ void DriveIntegrationService::OnMounted(const base::FilePath& mount_path) {
   }
 
   if (ash::features::IsDriveFsBulkPinningEnabled()) {
-    pin_manager_ = std::make_unique<drivefs::pinning::PinManager>(
-        profile_->GetPath(), GetDriveFsInterface());
+    DCHECK_CURRENTLY_ON(BrowserThread::UI);
+    pin_manager_ = std::make_unique<PinManager>(profile_->GetPath(),
+                                                GetDriveFsInterface());
     pin_manager_->ShouldCheckStalledFiles(true);
     GetDriveFsHost()->AddObserver(pin_manager_.get());
     ToggleBulkPinning();
@@ -1200,6 +1202,7 @@ void DriveIntegrationService::PinFiles(
 }
 
 void DriveIntegrationService::ToggleBulkPinning() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   if (!pin_manager_) {
     VLOG(1) << "No bulk-pinning manager";
     return;
@@ -1601,6 +1604,11 @@ void DriveIntegrationService::GetReadOnlyAuthenticationToken(
   }
 
   auth_service_->StartAuthentication(std::move(callback));
+}
+
+PinManager* DriveIntegrationService::GetPinManager() const {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  return pin_manager_.get();
 }
 
 //===================== DriveIntegrationServiceFactory =======================
