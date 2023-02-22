6bc1fa8e03d1e9aa1d19cae1633592ca6a1bc654
Steve Cho <stevecho@google.com>
2023-02-22 22:03:36 +0000

    media/gpu/v4l2: clean up TODOs in V4L2 AV1 platform decoding code
    This CL cleans up remaining TODOs in V4L2 AV1 platform decoding code.
    All existing TODOs are revisited and some of them are removed if
    possible or no longer relevant.
    
    Bug: b:270390919
    TEST: tast run <DUT_IP> video.PlatformDecoding.v4l2_stateless_av1
    Change-Id: Iaa12eec9d33a0a1e1f2c931945585ac83cd123e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4282865
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108600}

--- media/gpu/v4l2/test/av1_decoder.h
+++ media/gpu/v4l2/test/av1_decoder.h
@@ -19,15 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/libgav1/src/src/obu_parser.h"
 
-// TODO(stevecho): RESTORATION_TILESIZE_MAX in the spec is not available in the
-// AV1 uAPI. It was recommended to be added in the userspace code. If the uAPI
-// stays as it is for upstreaming, then #ifndef can be removed. If the uAPI ends
-// up adding this constant, then we can remove this define at that time.
-// https://patchwork.linuxtv.org/project/linux-media/patch/20210810220552.298140-2-daniel.almeida@collabora.com/
-#ifndef V4L2_AV1_RESTORATION_TILESIZE_MAX
-#define V4L2_AV1_RESTORATION_TILESIZE_MAX 256
-#endif
-
 struct v4l2_ctrl_av1_frame;
 
 namespace media {
@@ -47,7 +38,6 @@ class Av1Decoder : public VideoDecoder {
   static std::unique_ptr<Av1Decoder> Create(
       const base::MemoryMappedFile& stream);
 
-  // TODO(stevecho): implement DecodeNextFrame() function
   // Parses next frame from IVF stream and decodes the frame. This method will
   // place the Y, U, and V values into the respective vectors and update the
   // size with the display area size of the decoded frame.
