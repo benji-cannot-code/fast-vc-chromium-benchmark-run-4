61d787c9dd285746644d62bea271ab7162454a3e
Chris Fredrickson <cfredric@chromium.org>
2023-02-22 19:58:36 +0000

    Add metrics to track when documents inherit the `has_storage_access` bit
    Bug: 1401089
    Change-Id: I4a4fdcb0353b00e6641061dd70736654633f97cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283377
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108502}

--- third_party/blink/renderer/core/loader/document_loader_test.cc
+++ third_party/blink/renderer/core/loader/document_loader_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/auto_reset.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -586,6 +587,7 @@ TEST_P(DocumentLoaderTest, SameOriginNavigation_WithStorageAccess) {
   params->has_storage_access = true;
   LocalFrame* local_frame =
       To<LocalFrame>(web_view_impl->GetPage()->MainFrame());
+  base::HistogramTester histogram_tester;
   local_frame->Loader().CommitNavigation(std::move(params), nullptr);
 
   EXPECT_TRUE(local_frame->DomWindow()->HasStorageAccess());
@@ -593,6 +595,13 @@ TEST_P(DocumentLoaderTest, SameOriginNavigation_WithStorageAccess) {
   EXPECT_TRUE(local_frame->Loader()
                   .GetDocumentLoader()
                   ->LastNavigationHadTrustedInitiator());
+
+  histogram_tester.ExpectUniqueSample(
+      "API.StorageAccess.DocumentLoadedWithStorageAccess", /*sample=*/true,
+      /*expected_bucket_count=*/1);
+  histogram_tester.ExpectUniqueSample(
+      "API.StorageAccess.DocumentInheritedStorageAccess", /*sample=*/true,
+      /*expected_bucket_count=*/1);
 }
 
 TEST_P(DocumentLoaderTest, CrossOriginNavigation) {
@@ -611,6 +620,7 @@ TEST_P(DocumentLoaderTest, CrossOriginNavigation) {
       SecurityOrigin::Create(other_origin_url));
   LocalFrame* local_frame =
       To<LocalFrame>(web_view_impl->GetPage()->MainFrame());
+  base::HistogramTester histogram_tester;
   local_frame->Loader().CommitNavigation(std::move(params), nullptr);
 
   EXPECT_EQ(BlinkStorageKey::CreateFirstParty(
@@ -620,6 +630,13 @@ TEST_P(DocumentLoaderTest, CrossOriginNavigation) {
   EXPECT_FALSE(local_frame->Loader()
                    .GetDocumentLoader()
                    ->LastNavigationHadTrustedInitiator());
+
+  histogram_tester.ExpectUniqueSample(
+      "API.StorageAccess.DocumentLoadedWithStorageAccess", /*sample=*/false,
+      /*expected_bucket_count=*/1);
+  histogram_tester.ExpectUniqueSample(
+      "API.StorageAccess.DocumentInheritedStorageAccess", /*sample=*/false,
+      /*expected_bucket_count=*/1);
 }
 
 TEST_P(DocumentLoaderTest, StorageKeyFromNavigationParams) {
