27d6639e8a3186c98226230b122ed27002bb92b1
Joemer Ramos <joemerramos@chromium.org>
2023-02-22 14:09:09 +0000

    [iOS] Refactor usage of UIButton.contentEdgeInsets Pt 1
    UIButton.contentEdgeInsets is deprecated as of iOS 15. As part of the
    effort of bumping the min version of iOS to 15, we refactor the usage
    of UIButton.contentEdgeInsets and replace it with
    UIButton.configuration.contentInsets. A particular issue in creating
    this CL was that documentation gives the idea that contentEdgeInsets,
    titleEdgeInsets, and imageEdgeInsets should be replaced by
    contentInsets. We are asking the CL reviewer to verify that this is
    correct. Additionally, we use compile guards instead of @available
    because we get deprecation warnings with @available. This may be because
    @available is checked at runtime and these warnings happen during compiling.
    
    Bug: 1406561
    Change-Id: I1669c371a3026b6b3ed6be133f35032f7d24e82c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4276801
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108302}

--- ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
+++ ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
@@ -467,11 +467,23 @@ constexpr CGFloat kNonProfileImageHeightWidth = 56.0;
       self.primaryButton.layer.cornerRadius =
           kStandardPromoStyle.kButtonCornerRadius;
       self.primaryButton.clipsToBounds = YES;
+
+      // TODO(crbug.com/1418068): Remove after minimum version required is >=
+      // iOS 15.
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_15_0
+      self.primaryButton.configuration.contentInsets =
+          NSDirectionalEdgeInsetsMake(
+              kStandardPromoStyle.kButtonTitleVerticalContentInset,
+              kStandardPromoStyle.kButtonTitleHorizontalContentInset,
+              kStandardPromoStyle.kButtonTitleVerticalContentInset,
+              kStandardPromoStyle.kButtonTitleHorizontalContentInset);
+#else
       self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
           kStandardPromoStyle.kButtonTitleVerticalContentInset,
           kStandardPromoStyle.kButtonTitleHorizontalContentInset,
           kStandardPromoStyle.kButtonTitleVerticalContentInset,
           kStandardPromoStyle.kButtonTitleHorizontalContentInset);
+#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_15_0
 
       constraintsToActivate = self.standardLayoutConstraints;
       break;
@@ -504,11 +516,23 @@ constexpr CGFloat kNonProfileImageHeightWidth = 56.0;
       self.primaryButton.layer.cornerRadius =
           kTitledPromoStyle.kButtonCornerRadius;
       self.primaryButton.clipsToBounds = YES;
+
+      // TODO(crbug.com/1418068): Remove after minimum version required is >=
+      // iOS 15.
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_15_0
+      self.primaryButton.configuration.contentInsets =
+          NSDirectionalEdgeInsetsMake(
+              kTitledPromoStyle.kButtonTitleVerticalContentInset,
+              kTitledPromoStyle.kButtonTitleHorizontalContentInset,
+              kTitledPromoStyle.kButtonTitleVerticalContentInset,
+              kTitledPromoStyle.kButtonTitleHorizontalContentInset);
+#else
       self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
           kTitledPromoStyle.kButtonTitleVerticalContentInset,
           kTitledPromoStyle.kButtonTitleHorizontalContentInset,
           kTitledPromoStyle.kButtonTitleVerticalContentInset,
           kTitledPromoStyle.kButtonTitleHorizontalContentInset);
+#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_15_0
 
       constraintsToActivate = self.titledLayoutConstraints;
       break;
@@ -539,11 +563,23 @@ constexpr CGFloat kNonProfileImageHeightWidth = 56.0;
       self.primaryButton.layer.cornerRadius =
           kTitledCompactPromoStyle.kButtonCornerRadius;
       self.primaryButton.clipsToBounds = NO;
+
+      // TODO(crbug.com/1418068): Remove after minimum version required is >=
+      // iOS 15.
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_15_0
+      self.primaryButton.configuration.contentInsets =
+          NSDirectionalEdgeInsetsMake(
+              kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
+              kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset,
+              kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
+              kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset);
+#else
       self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
           kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
           kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset,
           kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
           kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset);
+#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_15_0
 
       constraintsToActivate = self.titledCompactLayoutConstraints;
       break;
