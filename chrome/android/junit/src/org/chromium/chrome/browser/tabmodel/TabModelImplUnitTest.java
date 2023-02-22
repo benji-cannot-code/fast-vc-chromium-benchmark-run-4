0cde5e774e8479cf90efe0e301e520b418384fa2
Calder Kitagawa <ckitagawa@chromium.org>
2023-02-22 17:32:44 +0000

    [Tab Model] Find nearby rather than adjacent tab when closing
    When closing a group of tabs in incognito mode it was possible to end
    up in the normal tab switcher even if the incognito switcher still
    contained tabs.
    
    This was the result of when closing multiple tabs the TabModel would
    only check for an adjacent tab that wasn't closing. When closing
    multiple tabs this is insufficient and the search should look for
    a nearby non closing tab.
    
    Fix this by making the search more exhaustive, but to avoid causing
    slowdown skip this behavior when closing all tabs. For multiple
    tab closures, this process should only happen once when the first
    tab is closed at which point a non-closing tab will be selected.
    
    Bug: 1405321
    Change-Id: I13986ee73778756e4808478f1e972665582cbf38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261135
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108412}

--- chrome/android/junit/src/org/chromium/chrome/browser/tabmodel/TabModelImplUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tabmodel/TabModelImplUnitTest.java
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tabmodel;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import androidx.test.filters.SmallTest;
@@ -15,15 +18,18 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.flags.ActivityType;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.homepage.HomepageManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.MockTab;
@@ -33,12 +39,17 @@ import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tab.TabSelectionType;
 import org.chromium.chrome.browser.tab.state.CriticalPersistedTabData;
 
+import java.util.Arrays;
+
 /**
  * Unit tests for {@link TabModelImpl}.
  */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class TabModelImplUnitTest {
+    @Rule
+    public TestRule mProcessor = new Features.JUnitProcessor();
+
     private static final long FAKE_NATIVE_ADDRESS = 123L;
 
     /**
@@ -269,5 +280,25 @@ public class TabModelImplUnitTest {
         Tab tab0 = createTab(activeNormal);
         selectTab(activeNormal, tab0);
         Assert.assertNull(activeNormal.getNextTabIfClosed(tab0.getId(), false));
+    }
+
+    @Test
+    @SmallTest
+    @Features.EnableFeatures(ChromeFeatureList.TAB_STATE_V1_OPTIMIZATIONS)
+    public void testDontSwitchModelsIfIncognitoGroupClosed() {
+        TabModel activeIncognito = createTabModel(true, true);
+        TabModel inactiveNormal = createTabModel(false, false);
+
+        Tab incognitoTab0 = createTab(activeIncognito);
+        Tab incognitoTab1 = createTab(activeIncognito);
+        Tab incognitoTab2 = createTab(activeIncognito);
+        Tab tab0 = createTab(inactiveNormal);
+
+        selectTab(activeIncognito, incognitoTab0);
+
+        activeIncognito.closeMultipleTabs(
+                Arrays.asList(new Tab[] {incognitoTab0, incognitoTab1}), false);
+        verify(mTabModelSelector, never()).selectModel(anyBoolean());
+        Assert.assertEquals(incognitoTab2, activeIncognito.getTabAt(activeIncognito.index()));
     }
 }
