e5e70533aab2a8c18acf80473fc5f15673480c4c
Nan Lin <linnan@chromium.org>
2023-02-22 00:05:26 +0000

    Add DCHECK that ARA is enabled in FencedFrameReporter::SendReport
    ARA is required to be enabled in opaque-ads fenced frame and therefore
    it's fine to always add ARA headers in the beacon requests.
    
    Change-Id: Icb91efa8e8fd15b5cadfb572527e90fe8c6cfaed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261217
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108032}

--- content/browser/fenced_frame/fenced_frame_reporter.cc
+++ content/browser/fenced_frame/fenced_frame_reporter.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/fenced_frame/redacted_fenced_frame_config.h"
+#include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom-shared.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
@@ -236,6 +237,13 @@ bool FencedFrameReporter::SendReport(
          ReportingDestinationAsString(reporting_destination), "'."});
     return false;
   }
+
+  // Attribution Reporting is required to be enabled for an opaque-ads fenced
+  // frame to load, therefore it's fine to always add the Attribution Reporting
+  // headers in `SendReportInternal()`. This should be revisited if this
+  // restriction changes.
+  DCHECK(request_initiator_frame->IsFeatureEnabled(
+      blink::mojom::PermissionsPolicyFeature::kAttributionReporting));
 
   static base::AtomicSequenceNumber unique_id_counter;
 
