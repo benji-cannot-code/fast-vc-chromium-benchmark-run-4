5f215bdb903714e93ebce10ae5501bc554326cd6
Jong Ahn <jongahn@google.com>
2023-02-22 07:56:51 +0000

    [App-Discovery] Add initial logging to launcher.
    Bug: b/267218014
    Change-Id: I7b8e33cd110fc363254de7af4e90c0e82175f4b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4228480
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108206}

--- chrome/browser/ash/app_list/search/search_controller.h
+++ chrome/browser/ash/app_list/search/search_controller.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list_types.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/ash/app_list/search/app_discovery_metrics_manager.h"
 #include "chrome/browser/ash/app_list/search/burn_in_controller.h"
 #include "chrome/browser/ash/app_list/search/common/keyword_util.h"
 #include "chrome/browser/ash/app_list/search/federated_metrics_manager.h"
@@ -96,8 +97,10 @@ class SearchController {
 
   virtual void StartZeroState(base::OnceClosure on_done,
                               base::TimeDelta timeout);
-  // Stops zero state.
-  void AppListClosing();
+
+  // Callback made when app list view is open or closed. |is_visible| should be
+  // true when the view is open.
+  void AppListViewChanging(bool is_visible);
 
   void OpenResult(ChromeSearchResult* result, int event_flags);
   void InvokeResultAction(ChromeSearchResult* result,
@@ -212,6 +215,7 @@ class SearchController {
   std::unique_ptr<SearchSessionMetricsManager> session_metrics_manager_;
   std::unique_ptr<federated::FederatedMetricsManager>
       federated_metrics_manager_;
+  std::unique_ptr<AppDiscoveryMetricsManager> app_discovery_metrics_manager_;
 
   std::unique_ptr<AppSearchDataSource> app_search_data_source_;
 
