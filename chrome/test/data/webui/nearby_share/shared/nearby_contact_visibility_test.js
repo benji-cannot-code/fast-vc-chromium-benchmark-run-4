5cb0e2d6807a08b6d45e29f73504933481c053d7
Wes Okuhara <wesokuhara@google.com>
2023-02-22 23:42:31 +0000

    Nearby Share: Convert shared code to TS
    Convert shared Nearby share code to TS. Temporary externs are created
    to satisfy Closure compilation until the main chrome://nearby app is
    also converted to TS.
    
    Bug: b/263413628
    Test: browser_tests --gtest_filter="Nearby*"
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: I656abf4a8c49e400fd8e36de23d1de6985dfa8ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4272932
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Pu Shi <pushi@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108662}

--- chrome/test/data/webui/nearby_share/shared/nearby_contact_visibility_test.js
+++ chrome/test/data/webui/nearby_share/shared/nearby_contact_visibility_test.js
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://nearby/strings.m.js';
+import 'chrome://nearby/shared/nearby_contact_visibility.js';
 
 import {setContactManagerForTesting} from 'chrome://nearby/shared/nearby_contact_manager.js';
-import {NearbyContactVisibilityElement} from 'chrome://nearby/shared/nearby_contact_visibility.js';
 import {DataUsage, FastInitiationNotificationState, Visibility} from 'chrome://resources/mojo/chromeos/ash/services/nearby/public/mojom/nearby_share_settings.mojom-webui.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 
@@ -29,7 +29,7 @@ suite('nearby-contact-visibility', () => {
     visibilityElement = /** @type {!NearbyContactVisibilityElement} */ (
         document.createElement('nearby-contact-visibility'));
 
-    visibilityElement.settings = {
+    visibilityElement.settings = /** @type {!NearbySettings} */ ({
       enabled: false,
       fastInitiationNotificationState: FastInitiationNotificationState.kEnabled,
       isFastInitiationHardwareSupported: true,
@@ -38,7 +38,7 @@ suite('nearby-contact-visibility', () => {
       visibility: Visibility.kUnknown,
       isOnboardingComplete: false,
       allowedContacts: [],
-    };
+    });
 
     document.body.appendChild(visibilityElement);
   });
--- chrome/test/data/webui/nearby_share/shared/nearby_preview_test.js
+++ chrome/test/data/webui/nearby_share/shared/nearby_preview_test.js
@@ -3,8 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://nearby/shared/nearby_preview.js';
+
 import {ShareType} from 'chrome://nearby/mojo/nearby_share_share_type.mojom-webui.js';
-import {NearbyPreviewElement} from 'chrome://nearby/shared/nearby_preview.js';
 
 import {assertEquals} from '../../chromeos/chai_assert.js';
 
