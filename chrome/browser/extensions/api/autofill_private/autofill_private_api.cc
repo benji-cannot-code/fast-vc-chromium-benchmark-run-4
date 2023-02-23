88e2308c77974b844cbe4bdd812247ac60f7227c
Florian Leimgruber <fleimgruber@google.com>
2023-02-23 19:01:39 +0000

    Add feature flag to create kAccount profiles from settings.
    To manually test the feature compatibility launch, testers need a way
    to create new kAccount profiles. Since the migration UI is not ready
    yet, this test features serves as a workaround.
    
    Bug: 1348294
    Change-Id: I42f220263ad8289e4aab537b2ef6e6e2597b5778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234263
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109110}

--- chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
+++ chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
@@ -155,6 +155,22 @@ autofill::AutofillManager* GetAutofillManager(
   return autofill_driver->autofill_manager();
 }
 
+autofill::AutofillProfile CreateNewAutofillProfile() {
+  if (!base::FeatureList::IsEnabled(
+          autofill::features::test::
+              kAutofillCreateAccountProfilesFromSettings)) {
+    return autofill::AutofillProfile(base::GenerateGUID(), kSettingsOrigin);
+  }
+  autofill::AutofillProfile profile(
+      base::GenerateGUID(), kSettingsOrigin,
+      autofill::AutofillProfile::Source::kAccount);
+  profile.set_initial_creator_id(
+      autofill::AutofillProfile::kInitialCreatorOrModifierChrome);
+  profile.set_last_modifier_id(
+      autofill::AutofillProfile::kInitialCreatorOrModifierChrome);
+  return profile;
+}
+
 }  // namespace
 
 namespace extensions {
@@ -207,9 +223,7 @@ ExtensionFunction::ResponseAction AutofillPrivateSaveAddressFunction::Run() {
       return RespondNow(Error(kErrorDataUnavailable));
   }
   autofill::AutofillProfile profile =
-      existing_profile
-          ? *existing_profile
-          : autofill::AutofillProfile(base::GenerateGUID(), kSettingsOrigin);
+      existing_profile ? *existing_profile : CreateNewAutofillProfile();
 
   if (address->full_names) {
     std::string full_name;
