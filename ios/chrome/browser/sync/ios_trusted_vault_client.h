30e71d0440587884ed49b7708e99e4b72a7a5e0c
Mikel Astiz <mastiz@chromium.org>
2023-02-23 10:02:34 +0000

    [sync] Invoke new APIs in TrustedVaultClientBackend
    This includes:
    1. ClearLocalData, as per pre-existing TODO.
    2. GetPublicKeyForIdentity, as part of registration verification.
    3. SetDeviceRegistrationPublicKeyVerifierForUMA, guarded behind
       experiment toggle, to actually trigger registration verification.
    
    The internal implementation of the APIs being invoked in this patch
    landed recently with https://crrev.com/i/5496191.
    
    The actual logic to verify the registration will be tackled in future
    patches as it is quite complex.
    
    Bug: 1416626, 1273080
    Change-Id: I0c16aed7de2be3c63d1c213000e3f01b7da499ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4275301
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108830}

--- ios/chrome/browser/sync/ios_trusted_vault_client.h
+++ ios/chrome/browser/sync/ios_trusted_vault_client.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SYNC_IOS_TRUSTED_VAULT_CLIENT_H_
 #define IOS_CHROME_BROWSER_SYNC_IOS_TRUSTED_VAULT_CLIENT_H_
 
+#include "base/memory/weak_ptr.h"
 #include "components/sync/driver/trusted_vault_client.h"
 
 class ChromeAccountManagerService;
@@ -48,9 +49,14 @@ class IOSTrustedVaultClient : public syncer::TrustedVaultClient {
  private:
   // Returns the identity for `account_info`.
   id<SystemIdentity> IdentityForAccount(const CoreAccountInfo& account_info);
+  void VerifyDeviceRegistration(const std::string& gaia_id);
+  void VerifyDeviceRegistrationWithPublicKey(
+      const std::string& gaia_id,
+      const std::vector<uint8_t>& public_key);
 
   ChromeAccountManagerService* const account_manager_service_ = nullptr;
   TrustedVaultClientBackend* const backend_ = nullptr;
+  base::WeakPtrFactory<IOSTrustedVaultClient> weak_ptr_factory_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_SYNC_IOS_TRUSTED_VAULT_CLIENT_H_
