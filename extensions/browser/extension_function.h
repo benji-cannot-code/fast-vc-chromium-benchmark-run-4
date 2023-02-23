a0a16b902ed61de2694d8b17a4a9d2c9f1469661
Claudio DeSouza <cdesouza@igalia.com>
2023-02-23 05:56:40 +0000

    [CodeHealth] Cleaning up ExtensionFunction::Error overloads
    This CL removes the Error overloads spelling out the number of arguments
    in favour of a parameter pack to pass along however many formatting
    arguments are necessary. The same has been done to
    ErrorUtils::FormatErrorMessage.
    
    Bug: 1417490
    Change-Id: I87dea18ea46b90a9161839b325aaefa99aa0caca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4270630
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1108784}

--- extensions/browser/extension_function.h
+++ extensions/browser/extension_function.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_function_histogram_value.h"
 #include "extensions/browser/quota_service.h"
 #include "extensions/common/constants.h"
+#include "extensions/common/error_utils.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/features/feature.h"
 #include "extensions/common/mojom/extra_response_data.mojom.h"
@@ -435,14 +436,11 @@ class ExtensionFunction : public base::RefCountedThreadSafe<
   // ErrorUtils::FormatErrorMessage, that is, each occurrence of * is replaced
   // by the corresponding |s*|:
   // Error("Error in *: *", "foo", "bar") <--> Error("Error in foo: bar").
-  ResponseValue Error(const std::string& format, const std::string& s1);
-  ResponseValue Error(const std::string& format,
-                      const std::string& s1,
-                      const std::string& s2);
-  ResponseValue Error(const std::string& format,
-                      const std::string& s1,
-                      const std::string& s2,
-                      const std::string& s3);
+  template <typename... Args>
+  ResponseValue Error(const std::string& format, const Args&... args) {
+    return CreateErrorResponseValue(
+        (extensions::ErrorUtils::FormatErrorMessage(format, args), ...));
+  }
   // Error with a list of arguments |args| to pass to caller.
   // Using this ResponseValue indicates something is wrong with the API.
   // It shouldn't be possible to have both an error *and* some arguments.
