f5b8ad34372afcfdc8385289520ebf8679a2fab9
Peter Kasting <pkasting@chromium.org>
2023-02-23 18:40:59 +0000

    Fix crashes on startup for dump_colors tool.
    Bug: none
    Change-Id: I42c3220e9ffe89e53a002c351d2a50110d5f0b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4284357
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109085}

--- chrome/browser/win/titlebar_config.cc
+++ chrome/browser/win/titlebar_config.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 bool ShouldCustomDrawSystemTitlebar() {
   // Cache flag lookup.
   static const bool custom_titlebar_disabled =
+      base::CommandLine::InitializedForCurrentProcess() &&
       base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisableWindows10CustomTitlebar);
 
--- ui/color/win/accent_color_observer.cc
+++ ui/color/win/accent_color_observer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/no_destructor.h"
+#include "base/task/sequenced_task_runner.h"
 #include "skia/ext/skia_utils_win.h"
 #include "ui/gfx/color_utils.h"
 
@@ -83,8 +84,10 @@ void AccentColorObserver::OnDwmKeyUpdated() {
 
   callbacks_.Notify();
 
-  // Watch for future changes.
-  if (!dwm_key_->StartWatching(base::BindOnce(
+  // Watch for future changes. If there is no task runner, this is a test or
+  // tool context and watching is unnecessary.
+  if (!base::SequencedTaskRunner::HasCurrentDefault() ||
+      !dwm_key_->StartWatching(base::BindOnce(
           &AccentColorObserver::OnDwmKeyUpdated, base::Unretained(this)))) {
     dwm_key_.reset();
   }
