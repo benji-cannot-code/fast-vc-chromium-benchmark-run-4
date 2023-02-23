ac11f1c0c5193d07ba4da0a9965d796d9943fb01
Adithya Srinivasan <adithyas@chromium.org>
2023-02-23 23:06:03 +0000

    DocumentRules: Exclude links with display locked ancestors
    Elements with display-locked ancestors may not be rendered. When they
    aren't being rendered, they might be skipped during the style update
    and will have ComputedStyles that are not up-to-date. This CL updates
    DocumentSpeculationRules to keep track of such links, and ignores them
    when matching document rules.
    
    Bug: 1371522
    Change-Id: I4bc4d2c97eb75b16f2d20f20b5e0a30f7da7f40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4240776
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109272}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -245,6 +245,10 @@ class DisplayLockStyleScope {
     if (auto* context = element_->GetDisplayLockContext()) {
       if (did_update_children_) {
         context->DidStyleChildren();
+        if (auto* document_rules = DocumentSpeculationRules::FromIfExists(
+                element_->GetDocument())) {
+          document_rules->DidStyleChildren(element_);
+        }
       }
     }
   }
@@ -275,6 +279,10 @@ class DisplayLockStyleScope {
     DCHECK(element_->GetDisplayLockContext());
 
     element_->GetDisplayLockContext()->NotifyChildStyleRecalcWasBlocked(change);
+    if (auto* document_rules =
+            DocumentSpeculationRules::FromIfExists(element_->GetDocument())) {
+      document_rules->ChildStyleRecalcBlocked(element_);
+    }
   }
 
  private:
