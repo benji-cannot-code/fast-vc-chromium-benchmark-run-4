f9dae885b088e990a6e17847f56d1d8471a63517
Renato Silva <rrsilva@google.com>
2023-02-23 13:25:59 +0000

    [Sheriff] Revert "[Privacy Hub] Add Privacy Hub button to combined notification"
    This reverts commit ba83a8001bf9dd9aecf850b41b4bb5a9fb0994c5.
    
    Reason for revert: Seems to be the cause of failures in
    FirmwareUpdateStartupNotificationTest.StartupNotificationShownRegularUser
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/34109
    
    Also https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/30626/overview
    
    Original change's description:
    > [Privacy Hub] Add Privacy Hub button to combined notification
    >
    > Remove the behavior of clicking the notification text to open Privacy
    > Hub as only the combined notification should have the interaction to
    > open Privacy Hub. This interaction is supposed to be obvious to the user
    > through the newly added button.
    >
    > Bug: b:262832229
    > Change-Id: I0d8f7125625f64df0332b54f6762c6d6cc1aa173
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4270640
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Auto-Submit: Christoph Schlosser <cschlosser@chromium.org>
    > Commit-Queue: Christoph Schlosser <cschlosser@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1108825}
    
    Bug: b:262832229
    Change-Id: I6e6ce14b3a4437ee4a60a5e35944db04bc13ce6e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4287216
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Owners-Override: Renato Silva <rrsilva@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1108894}

--- ash/system/privacy_hub/microphone_privacy_switch_controller_unittest.cc
+++ ash/system/privacy_hub/microphone_privacy_switch_controller_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/privacy_hub_delegate.h"
 #include "ash/public/cpp/sensor_disabled_notification_delegate.h"
 #include "ash/public/cpp/test/test_new_window_delegate.h"
+#include "ash/public/cpp/test/test_system_tray_client.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -357,6 +358,11 @@ TEST_F(PrivacyHubMicrophoneControllerTest, SwMuteNotificationActionButton) {
   ASSERT_TRUE(notification);
   EXPECT_EQ(1u, notification->buttons().size());
 
+  EXPECT_EQ(histogram_tester().GetBucketCount(
+                privacy_hub_metrics::
+                    kPrivacyHubMicrophoneEnabledFromNotificationHistogram,
+                true),
+            0);
   // Clicking the action button should unmute device.
   ClickOnNotificationButton();
   EXPECT_FALSE(CrasAudioHandler::Get()->IsInputMuted());
@@ -378,9 +384,20 @@ TEST_F(PrivacyHubMicrophoneControllerTest, SwMuteNotificationActionBody) {
   ASSERT_TRUE(notification);
   EXPECT_EQ(1u, notification->buttons().size());
 
+  EXPECT_EQ(histogram_tester().GetBucketCount(
+                privacy_hub_metrics::kPrivacyHubOpenedHistogram,
+                privacy_hub_metrics::PrivacyHubNavigationOrigin::kNotification),
+            0);
+
   // Clicking the action button should unmute device.
   ClickOnNotificationBody();
+  EXPECT_EQ(GetSystemTrayClient()->show_os_settings_privacy_hub_count(), 1);
   EXPECT_TRUE(CrasAudioHandler::Get()->IsInputMuted());
+
+  EXPECT_EQ(histogram_tester().GetBucketCount(
+                privacy_hub_metrics::kPrivacyHubOpenedHistogram,
+                privacy_hub_metrics::PrivacyHubNavigationOrigin::kNotification),
+            1);
 
   EXPECT_FALSE(GetNotification());
 }
