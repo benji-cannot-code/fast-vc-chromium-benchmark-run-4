da8e6a638556f6887117ad7a7765ee8c63fd80a4
Chad Duffin <chadduffin@chromium.org>
2023-02-23 23:46:02 +0000

    wifi: Ensure open networks can be not shared
    Open WiFi networks i.e. networks with no security are currently forced
    to always be shared. However, being able to not share an open network is
    good for Privacy since there are reasons why a person may not want to
    have a network without security be shared. This change updates the logic
    to have open networks shared by default, but allows the user to opt out.
    
    Irrefutable proof: https://drive.google.com/file/d/1SSVnO7xAak6mzL3DYwq9f8Qb_Hu9abo5/view?usp=sharing
    
    Bug: b/269781809
    Test: Unit test and verified on Linux build.
    Change-Id: Icb30044c9826a0da8e1bf4bb2e0dd954cb914872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265450
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109307}

--- ash/webui/common/resources/network/network_config.js
+++ ash/webui/common/resources/network/network_config.js
@@ -1827,12 +1827,6 @@ Polymer({
         this.managedProperties_.source !== OncSource.kNone) {
       return false;
     }
-
-    // Insecure WiFi networks are always shared.
-    if (this.mojoType_ === NetworkType.kWiFi &&
-        this.securityType_ === SecurityType.kNone) {
-      return false;
-    }
     return true;
   },
 
