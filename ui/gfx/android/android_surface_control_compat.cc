62020e6d0555f00b6d20d1763c3b9e7ec1fb6c4c
Edgar Arriaga <edgararriaga@google.com>
2023-02-23 01:45:17 +0000

    Enable surface backpressure for buffers sent to SurfaceFlinger
    This commit improves framerate by enabling backpressure on Android
    devices for buffers submitted to SurfaceFlinger to avoid transactions
    being dropped when getting out of sync due to slow chrome or hardware.
    
    Bug: 1418421
    Change-Id: I9aa53b7473295ce2898575e846e447354ecfdd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283243
    Quick-Run: Edgar Arriaga García <edgararriaga@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Auto-Submit: Edgar Arriaga García <edgararriaga@google.com>
    Commit-Queue: Edgar Arriaga García <edgararriaga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108720}

--- ui/gfx/android/android_surface_control_compat.cc
+++ ui/gfx/android/android_surface_control_compat.cc
@@ -127,6 +127,10 @@ using pASurfaceTransactionStats_releaseASurfaceControls =
     void (*)(ASurfaceControl** surface_controls);
 using pASurfaceTransactionStats_getPreviousReleaseFenceFd =
     int (*)(ASurfaceTransactionStats* stats, ASurfaceControl* surface_control);
+using pASurfaceTransaction_setEnableBackPressure =
+    void (*)(ASurfaceTransaction* transaction,
+             ASurfaceControl* surface_control,
+             bool enable_back_pressure);
 }
 
 namespace gfx {
@@ -252,6 +256,8 @@ struct SurfaceControlMethods {
                   ASurfaceTransactionStats_releaseASurfaceControls);
     LOAD_FUNCTION(main_dl_handle,
                   ASurfaceTransactionStats_getPreviousReleaseFenceFd);
+    LOAD_FUNCTION_MAYBE(main_dl_handle,
+                        ASurfaceTransaction_setEnableBackPressure);
   }
 
   ~SurfaceControlMethods() = default;
@@ -288,6 +294,8 @@ struct SurfaceControlMethods {
       ASurfaceTransaction_setHdrMetadata_smpte2086Fn;
   pASurfaceTransaction_setFrameRate ASurfaceTransaction_setFrameRateFn;
   pASurfaceTransaction_setFrameTimeline ASurfaceTransaction_setFrameTimelineFn;
+  pASurfaceTransaction_setEnableBackPressure
+      ASurfaceTransaction_setEnableBackPressureFn;
 
   // TransactionStats methods.
   pASurfaceTransactionStats_getPresentFenceFd
@@ -556,6 +564,12 @@ bool SurfaceControl::SupportsSetFrameTimeline() {
 bool SurfaceControl::SupportsSurfacelessControl() {
   return IsSupported() &&
          !!SurfaceControlMethods::Get().ASurfaceControl_fromJavaFn;
+}
+
+bool SurfaceControl::SupportsSetEnableBackPressure() {
+  return IsSupported() &&
+         SurfaceControlMethods::Get()
+                 .ASurfaceTransaction_setEnableBackPressureFn != nullptr;
 }
 
 void SurfaceControl::SetStubImplementationForTesting() {
@@ -874,6 +888,12 @@ void SurfaceControl::Transaction::PrepareCallbacks() {
         transaction_, ack_ctx, &OnTransactionCompletedOnAnyThread);
     need_to_apply_ = true;
   }
+}
+
+void SurfaceControl::Transaction::SetEnableBackPressure(const Surface& surface,
+                                                        bool enable) {
+  SurfaceControlMethods::Get().ASurfaceTransaction_setEnableBackPressureFn(
+      transaction_, surface.surface(), enable);
 }
 
 }  // namespace gfx
