3414e7958a822e635975004f745b9cbce711e620
Ankush Singh <ankushkush@google.com>
2023-02-23 17:59:06 +0000

    [sync] PrefModelAssociator: fix RegisterPref DCHECK
    This CL achieves the following:
    1. PrefModelAssociatorClient instance can be null, updates the DCHECK in
    PrefModelAssociator::RegisterPref() accordingly.
    2. Updates allowed prefs in pref_model_associator_unittest to avoid
    issues when DCHECK is enabled in RegisterPref().
    
    Bug: 1401271
    Change-Id: Ic5738f7a54099492e7912fd05737bbbb022cd18f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4286272
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109036}

--- components/sync_preferences/pref_model_associator.cc
+++ components/sync_preferences/pref_model_associator.cc
@@ -442,6 +442,7 @@ void PrefModelAssociator::RegisterPref(const std::string& name) {
   DCHECK(!base::Contains(registered_preferences_, name));
   DCHECK(
       !base::FeatureList::IsEnabled(syncer::kSyncEnforcePreferencesAllowlist) ||
+      !client_ ||
       client_->GetSyncablePrefsDatabase().IsPreferenceSyncable(name))
       << "Preference " << name
       << " has not been added to syncable prefs allowlist";
@@ -454,6 +455,7 @@ void PrefModelAssociator::RegisterPrefWithLegacyModelType(
   DCHECK(!base::Contains(registered_preferences_, name));
   DCHECK(
       !base::FeatureList::IsEnabled(syncer::kSyncEnforcePreferencesAllowlist) ||
+      !client_ ||
       client_->GetSyncablePrefsDatabase().IsPreferenceSyncable(name))
       << "Preference " << name
       << " has not been added to syncable prefs allowlist";
