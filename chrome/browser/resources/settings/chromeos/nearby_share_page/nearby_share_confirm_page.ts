5fbd9f70be55ac159a3d658bc1b3454fd61688f8
Wes Okuhara <wesokuhara@google.com>
2023-02-23 23:13:03 +0000

    CrOS Settings: Update bundling of Nearby Share code
    Now that the shared Nearby share code is in TS, this CL modifies the
    CrOS Settings build:
    - Configures ts_library() path mappings for Nearby share absolute
      imports "/mojo" and "/shared"
    - Configures optimize_webui() external_paths argument to bundle the
      shared code
    
    Bug: b/263413628
    Test: browser_tests --gtest_filter="Nearby*"
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: I087aa5e9751c092b765b2f41ea0585f185d0d2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283600
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109279}

--- chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_confirm_page.ts
+++ chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_confirm_page.ts
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import 'chrome://resources/cr_elements/cr_lottie/cr_lottie.js';
-import '../../shared/nearby_page_template.js';
-import '../../shared/nearby_device.js';
-import '../../shared/nearby_preview.js';
-import '../../shared/nearby_progress.js';
+import '/shared/nearby_page_template.js';
+import '/shared/nearby_device.js';
+import '/shared/nearby_preview.js';
+import '/shared/nearby_progress.js';
 
 import {ShareTarget, TransferStatus} from '/mojo/nearby_share.mojom-webui.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
--- chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_high_visibility_page.ts
+++ chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_high_visibility_page.ts
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_lottie/cr_lottie.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/cr_components/localized_link/localized_link.js';
-import '../../shared/nearby_page_template.js';
-import '../../shared/nearby_shared_icons.html.js';
+import '/shared/nearby_page_template.js';
+import '/shared/nearby_shared_icons.html.js';
 
 import {RegisterReceiveSurfaceResult} from '/mojo/nearby_share.mojom-webui.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
