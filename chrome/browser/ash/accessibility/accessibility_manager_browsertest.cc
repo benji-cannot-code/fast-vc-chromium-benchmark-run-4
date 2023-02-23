e099aa4fc2be60563b195ad8f2978ce9a2052a4a
Akihiro Ota <akihiroota@chromium.org>
2023-02-23 01:11:54 +0000

    Accessibility: Fix OnPumpkinDataCreated crash
    From the crash logs in the bug, it seems that
    AccessibilityManager::OnPumpkinInstalled is being called twice. This
    leads to a scenario where `install_pumpkin_callback_` is null. We don't
    catch the bug in AccessibilityManager::OnPumpkinInstalled because
    we use a DCHECK, which doesn't run in production builds. Instead, we
    catch the bug in AccessibilityManager::OnPumpkinDataCreated.
    
    The fix is to change the CHECKS/DCHECKS into if statements, since it's
    possible for `install_pumpkin_callback_` to be null in both methods.
    
    Both tests fail with the original code and pass with the modifications
    to accessibility_manager.cc.
    
    AX-Relnotes: Fixes a crash when installing pumpkin for Dictation
    Bug: b:268510062
    Change-Id: I221e0a89be21b398e7f312e20ca17e925c2c3f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4284398
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108693}

--- chrome/browser/ash/accessibility/accessibility_manager_browsertest.cc
+++ chrome/browser/ash/accessibility/accessibility_manager_browsertest.cc
@@ -1007,6 +1007,16 @@ class AccessibilityManagerDlcTest : public AccessibilityManagerTest {
     AccessibilityManager::Get()->OnPumpkinError("Error");
   }
 
+  void OnPumpkinInstalled(bool success) {
+    AccessibilityManager::Get()->OnPumpkinInstalled(success);
+  }
+
+  void OnPumpkinDataCreated(
+      std::unique_ptr<extensions::api::accessibility_private::PumpkinData>
+          data) {
+    AccessibilityManager::Get()->OnPumpkinDataCreated(std::move(data));
+  }
+
   speech::SodaInstaller* soda_installer() {
     return speech::SodaInstaller::GetInstance();
   }
@@ -1484,6 +1494,26 @@ IN_PROC_BROWSER_TEST_F(AccessibilityManagerDlcTest,
   // again.
   InstallPumpkinAndWait();
   AssertMessageCenterEmpty();
+}
+
+// Ensures that AccessibilityManager can handle when OnPumpkinInstalled is
+// called multiple times, which can happen in production.
+IN_PROC_BROWSER_TEST_F(AccessibilityManagerDlcTest,
+                       OnPumpkinInstalledMultiple) {
+  SetDictationLocale("en-US");
+  SetDictationEnabled(true);
+  InstallPumpkinAndWait();
+  OnPumpkinInstalled(true);
+}
+
+// Ensures that AccessibilityManager can handle when OnPumpkinDataCreated is
+// called multiple times, which can happen in production.
+IN_PROC_BROWSER_TEST_F(AccessibilityManagerDlcTest,
+                       OnPumpkinDataCreatedMultiple) {
+  SetDictationLocale("en-US");
+  SetDictationEnabled(true);
+  InstallPumpkinAndWait();
+  OnPumpkinDataCreated(nullptr);
 }
 
 enum DictationDialogTestVariant {
