54a33cf67e10cc28ec986066e0926064c354a2c3
Chris Lu <thegreenfrog@chromium.org>
2023-02-23 22:55:48 +0000

    [ios] Remove updateStickyElements call in -feedLayoutDidEndUpdates
    Updating the state of elements that could stick to the top of the NTP
    are handled well in scrollViewDidScroll:, which is called in response
    to the feed updating after a card removal, for example. It is not
    necessary to do it in -feedLayoutDidEndUpdates even though it is the
    correct signal source for it. This was added to fix layout issues in response to a card removal.
    
    Video: https://drive.google.com/file/d/1a2Eaye9WdCc9IxTBzegbBjvF3aIMYaFT/view?usp=sharing
    
    Bug: 1406940
    Change-Id: Ie27f8100edfcbb7538085d708f4f6f60f70e92cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285598
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109265}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.h
@@ -130,10 +130,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Signals that the feed has completed its updates (i.e. loading cards).
 - (void)feedLayoutDidEndUpdates;
 
-// Forces the elements that stick to the top when scrolling (eg. omnibox, feed
-// header) to update for the current scroll position.
-- (void)updateStickyElements;
-
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_NTP_NEW_TAB_PAGE_VIEW_CONTROLLER_H_
--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -541,11 +541,6 @@ const CGFloat kShiftTilesUpAnimationDuration = 0.1;
   if (self.hasSavedOffsetFromPreviousScrollState) {
     [self setContentOffset:self.savedScrollOffset];
   }
-  [self updateStickyElements];
-}
-
-- (void)updateStickyElements {
-  [self handleStickyElementsForScrollPosition:[self scrollPosition] force:YES];
 }
 
 #pragma mark - NewTabPageConsumer
