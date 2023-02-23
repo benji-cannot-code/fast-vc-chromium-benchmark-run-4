67f9258273d4ac70df03785a13b5393e6f61bfb8
Filipa Senra <fsenra@google.com>
2023-02-23 10:20:20 +0000

    Reland "[AboutThisSite][Android] Add new Google branded icon"
    This is a reland of commit e44a3477a9d9d48d133e55869b0bddf575311639
    
    Added a missing dependency for java/ResourceId.template that has as an input /browser/android/resource_id.h. I also updated chrome/browser/page_info/BUILD.gn with the most recent copyright header template.
    
    Original change's description:
    > [AboutThisSite][Android] Add new Google branded icon
    >
    > This Cl changes the icon associated with the AboutThisSite feature in
    > Page Info to have Google branding in Android. It changes the icon on the
    > entry point inside Page Info.
    >
    > Icon on entry point: http://screen/9CHcnVVBz6XGBy6
    >
    > Bug: 1412704
    > Change-Id: I9af710d68094c4dfd06c249cbf8eae101f7e9bcc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4223384
    > Commit-Queue: Filipa Senra <fsenra@google.com>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Peter Conn <peconn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1108481}
    
    Bug: 1412704
    Change-Id: I70f7881621ee3f119983bd276f27d6caeb887b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4286715
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108834}

--- chrome/browser/page_info/about_this_site_controller_android.cc
+++ chrome/browser/page_info/about_this_site_controller_android.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 #include "base/android/jni_array.h"
+#include "chrome/browser/android/android_theme_resources.h"
+#include "chrome/browser/android/resource_mapper.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/page_info/about_this_site_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -22,6 +24,12 @@ static jboolean JNI_PageInfoAboutThisSiteController_IsFeatureEnabled(
     JNIEnv* env) {
   return page_info::IsAboutThisSiteFeatureEnabled(
       g_browser_process->GetApplicationLocale());
+}
+
+static jint JNI_PageInfoAboutThisSiteController_GetJavaDrawableIconId(
+    JNIEnv* env) {
+  return ResourceMapper::MapToJavaDrawableId(
+      IDR_ANDROID_ABOUT_THIS_SITE_LOGO_24DP);
 }
 
 static base::android::ScopedJavaLocalRef<jbyteArray>
