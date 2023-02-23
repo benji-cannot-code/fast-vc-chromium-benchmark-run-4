c01192be6b99537577ea210d605c1da5cc6cff81
Allen Webb <allenwebb@google.com>
2023-02-23 18:31:43 +0000

    interstitial_ui_browsertest: Run clang format
    The file wasn't formatted correctly before leading to odd indentation in
    a later CL, so format it first.
    
      chrome/browser/ui/webui/interstitials/interstitial_ui_browsertest.cc
    
    Bug: b:265445049
    Test: clang-format -i \
    Change-Id: I826e9b3548b527f5081fccf6969136805a19b7c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285665
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109071}

--- chrome/browser/ui/webui/interstitials/interstitial_ui_browsertest.cc
+++ chrome/browser/ui/webui/interstitials/interstitial_ui_browsertest.cc
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class InterstitialUITest : public InProcessBrowserTest {
  public:
-   InterstitialUITest() {}
-   ~InterstitialUITest() override {}
+  InterstitialUITest() {}
+  ~InterstitialUITest() override {}
 
  protected:
   // Tests interstitial displayed at url to verify that it has the given
@@ -35,9 +35,8 @@ class InterstitialUITest : public InProcessBrowserTest {
                         const std::string& page_title,
                         const std::u16string& body_text) {
     ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
-    EXPECT_EQ(
-      base::ASCIIToUTF16(page_title),
-      browser()->tab_strip_model()->GetActiveWebContents()->GetTitle());
+    EXPECT_EQ(base::ASCIIToUTF16(page_title),
+              browser()->tab_strip_model()->GetActiveWebContents()->GetTitle());
 
     // Should also be able to open and close devtools.
     DevToolsWindow* window =
@@ -45,8 +44,9 @@ class InterstitialUITest : public InProcessBrowserTest {
     EXPECT_TRUE(window);
     DevToolsWindowTesting::CloseDevToolsWindowSync(window);
 
-    if (body_text.empty())
+    if (body_text.empty()) {
       return;
+    }
 
     content::WebContents* contents =
         browser()->tab_strip_model()->GetActiveWebContents();
@@ -57,8 +57,7 @@ class InterstitialUITest : public InProcessBrowserTest {
   }
 
   // Convenience function to test interstitial pages without provided body_text.
-  void TestInterstitial(GURL url,
-                        const std::string& page_title) {
+  void TestInterstitial(GURL url, const std::string& page_title) {
     TestInterstitial(url, page_title, std::u16string());
   }
 
@@ -72,16 +71,12 @@ class InterstitialUITest : public InProcessBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(InterstitialUITest, HomePage) {
-  TestInterstitial(
-      GURL("chrome://interstitials"),
-      "Interstitials");
+  TestInterstitial(GURL("chrome://interstitials"), "Interstitials");
 }
 
 IN_PROC_BROWSER_TEST_F(InterstitialUITest, InvalidURLShouldOpenHomePage) {
   // Invalid path should open the main page:
-  TestInterstitial(
-      GURL("chrome://interstitials/--invalid--"),
-      "Interstitials");
+  TestInterstitial(GURL("chrome://interstitials/--invalid--"), "Interstitials");
 }
 
 IN_PROC_BROWSER_TEST_F(InterstitialUITest,
