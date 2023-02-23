2ad3a48c201f21a5849282d7478be43621245909
Sean Kau <skau@chromium.org>
2023-02-23 23:43:27 +0000

    Add Secondary 15 to ref color mixer
    Browser and CrOS UX have asked for a compromise between secondary10 and
    secondary20. This is for headers.
    
    Change-Id: Ifd95cdf9758f3285eaccf6283c333a3161716729
    Bug: b/267817808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285522
    Commit-Queue: Sean Kau <skau@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109304}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefPrimary100) \
   E_CPONLY(kColorRefSecondary0) \
   E_CPONLY(kColorRefSecondary10) \
+  E_CPONLY(kColorRefSecondary15) \
   E_CPONLY(kColorRefSecondary20) \
   E_CPONLY(kColorRefSecondary30) \
   E_CPONLY(kColorRefSecondary40) \
