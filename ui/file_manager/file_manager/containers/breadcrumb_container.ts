087331bb2bd4e09f49d373a7c684dee20f981fa3
Bo Majewski <majewski@chromium.org>
2023-02-23 02:20:57 +0000

    [Files]: Shows 'Search results' message when searching.
    Adds special code for handing searching in version V2 of search.
    
    Bug: b:269358182
    Change-Id: Icc340d207feaa58042b76de44f17d52867d6808e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279464
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108738}

--- ui/file_manager/file_manager/containers/breadcrumb_container.ts
+++ ui/file_manager/file_manager/containers/breadcrumb_container.ts
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import '../widgets/xf_breadcrumb.js';
 
 import {metrics} from '../common/js/metrics.js';
+import {SEARCH_RESULTS_KEY} from '../common/js/url_constants.js';
+import {str, util} from '../common/js/util.js';
 import {VolumeManagerCommon} from '../common/js/volume_manager_types.js';
 import {PathComponent, PropStatus, State} from '../externs/ts/state.js';
 import {changeDirectory} from '../state/actions/current_directory.js';
@@ -38,6 +40,22 @@ export class BreadcrumbContainer {
     if (!key || !currentDirectory) {
       this.hide_();
       return;
+    }
+
+    if (util.isSearchV2Enabled()) {
+      if (search && search.status !== undefined) {
+        // Search results do not have the corresponding directory in the
+        // directory tree. When V2 version of search is active, we short-circuit
+        // the process to show the correct label and exit.
+        this.show_(SEARCH_RESULTS_KEY, [
+          {
+            name: 'search',
+            label: str('SEARCH_RESULTS_LABEL'),
+            key: SEARCH_RESULTS_KEY,
+          },
+        ]);
+        return;
+      }
     }
 
     // If the current location is somewhere in Drive, all files in Drive can
