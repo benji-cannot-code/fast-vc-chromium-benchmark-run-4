e7a9e8ae40dcc43b8757255f63344214d0e14938
Xianzhu Wang <wangxianzhu@chromium.org>
2023-02-23 01:57:53 +0000

    Trace events for op counts in rasterization
    The RTree trace events added in crrev.com/1106903 were not very
    helpful because the op counts didn't include sub-ops in DrawRecordOps.
    They were helpful to know that RTree::Search() is really fast though.
    
    This CL removes the RTree trace events, and adds trace events in
    PaintOpBufferSerializer::Serialize() (mainly for GPU rasterization)
    and DisplayItemList::Raster() (mainly for software rasterization) for
    the total op count and serialized/rastered op count, including sub-ops
    in DrawRecordOps.
    
    Bug: 1401086
    Change-Id: Ifc3ba52fd97be4af3a829ee0d5c4904fda4562a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4278523
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108727}

--- cc/paint/paint_op_buffer_serializer.cc
+++ cc/paint/paint_op_buffer_serializer.cc
@@ -61,6 +61,10 @@ PaintOpBufferSerializer::~PaintOpBufferSerializer() = default;
 void PaintOpBufferSerializer::Serialize(const PaintOpBuffer& buffer,
                                         const std::vector<size_t>* offsets,
                                         const Preamble& preamble) {
+  TRACE_EVENT_BEGIN1("cc", "PaintOpBufferSerializer::Serialize",
+                     "total_op_count", buffer.total_op_count());
+  DCHECK_EQ(serialized_op_count_, 0u);
+
   std::unique_ptr<SkCanvas> canvas = MakeAnalysisCanvas(options_);
 
   // These PlaybackParams use the initial (identity) canvas matrix, as they are
@@ -74,6 +78,8 @@ void PaintOpBufferSerializer::Serialize(const PaintOpBuffer& buffer,
   SerializePreamble(canvas.get(), preamble, params);
   SerializeBuffer(canvas.get(), buffer, offsets);
   RestoreToCount(canvas.get(), saveCount, params);
+  TRACE_EVENT_END1("cc", "PaintOpBufferSerializer::Serialize",
+                   "serialized_op_count", serialized_op_count_);
 }
 
 void PaintOpBufferSerializer::Serialize(const PaintOpBuffer& buffer) {
@@ -311,6 +317,7 @@ bool PaintOpBufferSerializer::SerializeOp(SkCanvas* canvas,
     return false;
   }
 
+  ++serialized_op_count_;
   DCHECK_GE(bytes, PaintOpWriter::kHeaderBytes);
   return true;
 }
