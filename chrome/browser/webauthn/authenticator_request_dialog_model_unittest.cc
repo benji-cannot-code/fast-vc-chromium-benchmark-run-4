5db1f00bb3c5689e8ba88b0217e9596545d31a55
Nina Satragno <nsatragno@chromium.org>
2023-02-23 20:28:03 +0000

    [passkeys] Default enable QR-code first mode
    Default enable the QR-code first mode for passkeys.
    
    This CL also adds an equality operator overload for FeatureRef, which is
    useful to implement table tests where some features should be either
    explicitly enabled or disabled.
    
    Fixed: 1412877
    Change-Id: I7f38cfcfdc19fea1393f43b720a5927aca99d103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283820
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109161}

--- chrome/browser/webauthn/authenticator_request_dialog_model_unittest.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/contains.h"
+#include "base/containers/cxx20_erase_vector.h"
 #include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
 #include "base/strings/string_util.h"
@@ -205,7 +206,9 @@ TEST_F(AuthenticatorRequestDialogModelTest, Mechanisms) {
   const auto use_pk = Step::kPreSelectSingleAccount;
   const auto use_pk_multi = Step::kPreSelectAccount;
   const auto qr = Step::kCableV2QRCode;
+
   const auto qr1st = base::test::FeatureRef(device::kWebAuthPasskeysUI);
+  const std::vector<base::test::FeatureRef> kAllFeatures = {qr1st};
 
   const struct {
     int line_num;
@@ -470,8 +473,12 @@ TEST_F(AuthenticatorRequestDialogModelTest, Mechanisms) {
     SCOPED_TRACE(RequestTypeToString(test.request_type));
     SCOPED_TRACE(testing::Message() << "At line number: " << test.line_num);
 
-    base::test::ScopedFeatureList features;
-    features.InitWithFeatures(test.features, {});
+    std::vector<base::test::FeatureRef> disabled_features = kAllFeatures;
+    base::EraseIf(disabled_features, [&test](const auto& feature) {
+      return base::Contains(test.features, feature);
+    });
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitWithFeatures(test.features, disabled_features);
 
     TransportAvailabilityInfo transports_info;
     transports_info.is_ble_powered = true;
@@ -592,6 +599,7 @@ TEST_F(AuthenticatorRequestDialogModelTest, WinCancel) {
     tai.resident_key_requirement =
         is_passkey_request ? device::ResidentKeyRequirement::kRequired
                            : device::ResidentKeyRequirement::kDiscouraged;
+    tai.is_ble_powered = true;
 
     AuthenticatorRequestDialogModel model(/*render_frame_host=*/nullptr);
     model.set_cable_transport_info(absl::nullopt, {}, base::DoNothing(),
@@ -609,7 +617,9 @@ TEST_F(AuthenticatorRequestDialogModelTest, WinCancel) {
     }
 
     // The mechanism selection sheet should now be showing.
-    EXPECT_EQ(model.current_step(), Step::kMechanismSelection);
+    EXPECT_EQ(model.current_step(), is_passkey_request
+                                        ? Step::kCableV2QRCode
+                                        : Step::kMechanismSelection);
     // Canceling the Windows UI ends the request because the user must have
     // selected the Windows option first.
     EXPECT_FALSE(model.OnWinUserCancelled());
