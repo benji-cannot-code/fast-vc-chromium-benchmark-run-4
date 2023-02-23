53e5b5880e40aaca33ac8deb65062a308a75fd66
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-02-23 17:35:34 +0000

    Import wpt@3783e82d21df557df9b2b8db717ea6cb516bcda4
    Using wpt-import in Chromium eb7b1241ad337d602596113c25a26cf090a2230a.
    With Chromium commits locally applied on WPT:
    fac2787c0c "WPT: Update IDB WPT helper to avoid duplicate object store creation"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    antoniosartori@chromium.org, arthursonzogni@chromium.org:
      external/wpt/content-security-policy
    dcheng@chromium.org, jarhar@chromium.org:
      external/wpt/domxpath
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    igrigorik@chromium.org, yoavweiss@chromium.org:
      external/wpt/preload
    japhet@chromium.org:
      external/wpt/fetch
    jsbell@chromium.org:
      external/wpt/resources
    kenneth.r.christiansen@intel.com, rijubrata.bhaumik@intel.com:
      external/wpt/web-nfc
    mstensho@chromium.org:
      external/wpt/css/css-break
    nsatragno@chromium.org:
      external/wpt/webauthn
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    rego@igalia.com:
      external/wpt/css/css-contain
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I8377b7a1564b92dadb601362a38f2a0efa83535e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4286580
    Reviewed-by: Jie Sheng <jiesheng@google.com>
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109007}

--- third_party/blink/web_tests/external/wpt/common/security-features/resources/common.sub.js
+++ third_party/blink/web_tests/external/wpt/common/security-features/resources/common.sub.js
@@ -486,9 +486,13 @@ function dedicatedWorkerUrlThatFetches(url) {
       .catch((e) => postMessage(e.message));`;
 }
 
-function workerUrlThatImports(url) {
+function workerUrlThatImports(url, additionalAttributes) {
+  let csp = "";
+  if (additionalAttributes && additionalAttributes.contentSecurityPolicy) {
+    csp=`&contentSecurityPolicy=${additionalAttributes.contentSecurityPolicy}`;
+  }
   return `/common/security-features/subresource/static-import.py` +
-      `?import_url=${encodeURIComponent(url)}`;
+      `?import_url=${encodeURIComponent(url)}${csp}`;
 }
 
 function workerDataUrlThatImports(url) {
@@ -908,8 +912,8 @@ const subresourceMap = {
   },
   "worker-import": {
     path: "/common/security-features/subresource/worker.py",
-    invoker: url =>
-        requestViaDedicatedWorker(workerUrlThatImports(url), {type: "module"}),
+    invoker: (url, additionalAttributes) =>
+        requestViaDedicatedWorker(workerUrlThatImports(url, additionalAttributes), {type: "module"}),
   },
   "worker-import-data": {
     path: "/common/security-features/subresource/worker.py",
@@ -926,8 +930,8 @@ const subresourceMap = {
   },
   "sharedworker-import": {
     path: "/common/security-features/subresource/shared-worker.py",
-    invoker: url =>
-        requestViaSharedWorker(workerUrlThatImports(url), {type: "module"}),
+    invoker: (url, additionalAttributes) =>
+        requestViaSharedWorker(workerUrlThatImports(url, additionalAttributes), {type: "module"}),
   },
   "sharedworker-import-data": {
     path: "/common/security-features/subresource/shared-worker.py",
@@ -1110,6 +1114,10 @@ function invokeRequest(subresource, sourceContextList) {
         additionalAttributes[policyDelivery.key] = policyDelivery.value;
       } else if (policyDelivery.deliveryType === "rel-noref") {
         additionalAttributes["rel"] = "noreferrer";
+      } else if (policyDelivery.deliveryType === "http-rp") {
+        additionalAttributes[policyDelivery.key] = policyDelivery.value;
+      } else if (policyDelivery.deliveryType === "meta") {
+        additionalAttributes[policyDelivery.key] = policyDelivery.value;
       }
     }
 
