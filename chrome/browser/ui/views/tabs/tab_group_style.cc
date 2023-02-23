4eba458a57de4b25205be079b0273867a49c2267
Shibalik Mohapatra <shibalik@chromium.org>
2023-02-23 11:47:44 +0000

    [Chrome Refresh 2023] Update tab group header layout for ChromeRefresh23
    This change updated the tab group header layout for a collapsed header to be a rounded rect instead of a circle along with other layout changes like insets and minimum tab group header size.
    
    Also, updated the size and layout for the sync icon.
    
    
    Bug: 1399944
    Change-Id: Ic4a0cb2e36d549dd54334cbf246d8d8fa535b69e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192708
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108861}

--- chrome/browser/ui/views/tabs/tab_group_style.cc
+++ chrome/browser/ui/views/tabs/tab_group_style.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/views/tabs/tab_group_style.h"
+#include <string>
 #include "chrome/browser/ui/layout_constants.h"
+#include "chrome/browser/ui/tabs/tab_style.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/browser/ui/views/tabs/tab_group_header.h"
 #include "chrome/browser/ui/views/tabs/tab_group_underline.h"
@@ -13,8 +15,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/skia_conversions.h"
+#include "ui/views/background.h"
+#include "ui/views/controls/label.h"
 #include "ui/views/view.h"
 #include "ui/views/view_utils.h"
+
+namespace {
+
+constexpr int kHeaderChipVerticalInset = 1;
+constexpr int kTitleAdjustmentForEmptyHeader = 2;
+constexpr int kTitleAdjustmentForNonEmptyHeader = -2;
+constexpr int kEmptyChipSize = 14;
+
+constexpr int kChromeRefreshHeaderChipVerticalInset = 2;
+constexpr int kChromeRefreshEmptyChipSize = 20;
+constexpr int kChromeRefreshSyncIconWidth = 16;
+
+}  // namespace
 
 TabGroupStyle::TabGroupStyle(const TabGroupViews& tab_group_views)
     : tab_group_views_(tab_group_views) {}
@@ -49,6 +66,49 @@ SkPath TabGroupStyle::GetUnderlinePath(const gfx::Rect local_bounds) const {
   path.close();
 
   return path;
+}
+
+gfx::Rect TabGroupStyle::GetEmptyTitleChipBounds(
+    const TabGroupHeader* const header) const {
+  const int y = (GetLayoutConstant(TAB_HEIGHT) - GetEmptyChipSize()) / 2;
+  return gfx::Rect(TabGroupUnderline::GetStrokeInset(), y, GetEmptyChipSize(),
+                   GetEmptyChipSize());
+}
+
+std::unique_ptr<views::Background> TabGroupStyle::GetEmptyTitleChipBackground(
+    const SkColor color) const {
+  return views::CreateRoundedRectBackground(color, GetEmptyChipSize() / 2);
+}
+
+gfx::Insets TabGroupStyle::GetInsetsForHeaderChip() const {
+  return gfx::Insets::TLBR(kHeaderChipVerticalInset,
+                           GetChipCornerRadius() + kHeaderChipVerticalInset,
+                           kHeaderChipVerticalInset,
+                           GetChipCornerRadius() + kHeaderChipVerticalInset);
+}
+
+int TabGroupStyle::GetHighlightPathGeneratorCornerRadius(
+    const views::View* const title) const {
+  return title->GetVisible() ? GetChipCornerRadius() : GetEmptyChipSize() / 2;
+}
+
+int TabGroupStyle::GetTitleAdjustmentToTabGroupHeaderDesiredWidth(
+    const std::u16string title) const {
+  return title.empty() ? kTitleAdjustmentForEmptyHeader
+                       : kTitleAdjustmentForNonEmptyHeader;
+}
+
+float TabGroupStyle::GetEmptyChipSize() const {
+  return kEmptyChipSize;
+}
+
+float TabGroupStyle::GetSyncIconWidth() const {
+  return GetEmptyChipSize();
+}
+
+// static
+int TabGroupStyle::GetChipCornerRadius() {
+  return TabStyle::GetCornerRadius() - TabGroupUnderline::kStrokeThickness;
 }
 
 ChromeRefresh2023TabGroupStyle::ChromeRefresh2023TabGroupStyle(
@@ -89,4 +149,44 @@ SkPath ChromeRefresh2023TabGroupStyle::GetUnderlinePath(
                     TabGroupUnderline::kStrokeThickness / 2,
                     TabGroupUnderline::kStrokeThickness / 2);
   return path;
+}
+
+gfx::Rect ChromeRefresh2023TabGroupStyle::GetEmptyTitleChipBounds(
+    const TabGroupHeader* const header) const {
+  const int y = (GetLayoutConstant(TAB_HEIGHT) - GetEmptyChipSize()) / 2;
+  return gfx::Rect(TabGroupUnderline::GetStrokeInset(), y, GetEmptyChipSize(),
+                   GetEmptyChipSize());
+}
+
+gfx::Insets ChromeRefresh2023TabGroupStyle::GetInsetsForHeaderChip() const {
+  return gfx::Insets::TLBR(
+      kChromeRefreshHeaderChipVerticalInset, GetChipCornerRadius(),
+      kChromeRefreshHeaderChipVerticalInset, GetChipCornerRadius());
+}
+
+int ChromeRefresh2023TabGroupStyle::
+    GetTitleAdjustmentToTabGroupHeaderDesiredWidth(
+        const std::u16string title) const {
+  // Since the shape of the header in ChromeRefresh23 is a rounded rect this
+  // value should be `kTitleAdjustmentForNonEmptyHeader`.
+  return kTitleAdjustmentForNonEmptyHeader;
+}
+
+std::unique_ptr<views::Background>
+ChromeRefresh2023TabGroupStyle::GetEmptyTitleChipBackground(
+    const SkColor color) const {
+  return views::CreateRoundedRectBackground(color, GetChipCornerRadius());
+}
+
+int ChromeRefresh2023TabGroupStyle::GetHighlightPathGeneratorCornerRadius(
+    const views::View* const title) const {
+  return GetChipCornerRadius();
+}
+
+float ChromeRefresh2023TabGroupStyle::GetEmptyChipSize() const {
+  return kChromeRefreshEmptyChipSize;
+}
+
+float ChromeRefresh2023TabGroupStyle::GetSyncIconWidth() const {
+  return kChromeRefreshSyncIconWidth;
 }
