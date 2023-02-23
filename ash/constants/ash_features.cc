6265d0e0debf56062cbfeecd5820b97c0b38a8e5
Yilkal <yilkal@chromium.org>
2023-02-23 21:27:12 +0000

    Expose Server side transcription feature in chrome://flags
    This cl exposes the server side speech recognition feature
    flag to chrome://flags.
    The flag's exposure is controlled by a different flag that was
    introduced in this cl as well.
    
    Bug: b/265957535
    Change-Id: I9ccee5f93528c6cce64bc97bc88c6fc083cce529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4287729
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109207}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1234,6 +1234,11 @@ BASE_FEATURE(kInternalServerSideSpeechRecognition,
              "InternalServerSideSpeechRecognition",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Feature overrides the `InternalServerSideSpeechRecognition` flag if disabled.
+BASE_FEATURE(kInternalServerSideSpeechRecognitionControl,
+             "InternalServerSideSpeechRecognitionControl",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Enables sending `client-info` values to IPP printers on ChromeOS.
 BASE_FEATURE(kIppClientInfo, "IppClientInfo", base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -2700,8 +2705,18 @@ bool IsInternalServerSideSpeechRecognitionEnabled() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   // TODO(b/245614967): Once ready, enable this feature under
   // kProjectorBleedingEdgeExperience flag as well.
-  return ShouldForceEnableServerSideSpeechRecognitionForDev() ||
-         base::FeatureList::IsEnabled(kInternalServerSideSpeechRecognition);
+  return IsInternalServerSideSpeechRecognitionControlEnabled() &&
+         (ShouldForceEnableServerSideSpeechRecognitionForDev() ||
+          base::FeatureList::IsEnabled(kInternalServerSideSpeechRecognition));
+#else
+  return false;
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+}
+
+bool IsInternalServerSideSpeechRecognitionControlEnabled() {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  return base::FeatureList::IsEnabled(
+      kInternalServerSideSpeechRecognitionControl);
 #else
   return false;
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3376,6 +3376,9 @@ extern const char kProjectorLocalPlaybackDescription[];
 extern const char kProjectorAppDebugName[];
 extern const char kProjectorAppDebugDescription[];
 
+extern const char kProjectorServerSideSpeechRecognitionName[];
+extern const char kProjectorServerSideSpeechRecognitionDescription[];
+
 extern const char kQsRevampName[];
 extern const char kQsRevampDescription[];
 
