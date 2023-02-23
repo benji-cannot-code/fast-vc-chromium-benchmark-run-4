50c5ec148c2da308725a3700756551d42c05113b
Nicolas MacBeth <nicolasmacbeth@google.com>
2023-02-23 15:22:09 +0000

    [flags-ui][ios] Fixed "reset all" button in chrome://flags
    The "reset all" button on the iOS page of chrome://flags was broken.
    It was being caused by a code path erroring out due to checking for
    unavailable experiments which are undefined on iOS. Adding a guard to
    remove that check on iOS fixes the JS console errors, as well as fixing
    the "reset all" button. Also made the button return the proper "needs
    restart" state. The actual restart button is already removed on iOS,
    this re-enables the restart **toast** to appear when needed.
    
    Bug: 1418418
    Change-Id: I53f491fa618f5463c69a12117fd7a00df3d01019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283481
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108946}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1688,6 +1688,10 @@ void ResetAllFlags(flags_ui::FlagsStorage* flags_storage) {
   GetGlobalFlagsState().ResetAllFlags(flags_storage);
 }
 
+bool IsRestartNeededToCommitChanges() {
+  return GetGlobalFlagsState().IsRestartNeededToCommitChanges();
+}
+
 namespace testing {
 
 base::span<const flags_ui::FeatureEntry> GetFeatureEntries() {
