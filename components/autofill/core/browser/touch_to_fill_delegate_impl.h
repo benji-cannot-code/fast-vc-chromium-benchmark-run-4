758727df2523b527e9d7ccd80bc228a89bd160ff
Christoph Schwering <schwering@google.com>
2023-02-23 23:10:13 +0000

    [Autofill] Preemptively suppress keyboard in TTF
    This CL moves the keyboard suppression *before* the asynchronous
    parsing in order to prevent flickering.
    
    Now, TouchToFillDelegateImpl performs a dry run for showing TTF
    *before* the parse. The TTF is shown iff
    - the pre-parse dry run is successful and
    - the post-parse dry-run is successful and
    - no more than 1 second passes between the pre- and post-parse
      dry run.
    
    In particular, no TTF is shown if
    - the form is not known to be a credit card form or
    - the form is no longer known to be a credit card form or
    - parsing takes longer than 1 second.
    
    In the latter two cases, the keyboard remains suppressed until either
    the parse is completed or 1 second has elapsed.
    
    Bug: 1379149
    Change-Id: I02c7dcbeeedbac65d8b82c40fa8bc37bded43563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4266331
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109276}

--- components/autofill/core/browser/touch_to_fill_delegate_impl.h
+++ components/autofill/core/browser/touch_to_fill_delegate_impl.h
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_TOUCH_TO_FILL_DELEGATE_IMPL_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
+#include "base/timer/timer.h"
+#include "components/autofill/core/browser/autofill_manager.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/ui/touch_to_fill_delegate.h"
@@ -50,7 +53,13 @@ enum class TouchToFillCreditCardTriggerOutcome {
   // TouchToFill is not supported for this field type. This value is not logged
   // to UMA.
   kUnsupportedFieldType = 9,
-  kMaxValue = kUnsupportedFieldType
+  // Keyboard is not suppressed (anymore). This can happen due to race
+  // conditions involving the form parse that may happen between
+  // OnBeforeAskForValuesToFill() and TryToShowTouchToFill(). In particular, the
+  // focused field's type may change (changing DryRun()'s value from a
+  // non-`kShown` value to `kShown`).
+  kKeyboardNotSuppressed = 10,
+  kMaxValue = kKeyboardNotSuppressed
 };
 
 constexpr const char kUmaTouchToFillCreditCardTriggerOutcome[] =
@@ -71,7 +80,8 @@ class BrowserAutofillManager;
 //
 // Public methods are marked virtual for testing.
 // TODO(crbug.com/1324900): Consider using more descriptive name.
-class TouchToFillDelegateImpl : public TouchToFillDelegate {
+class TouchToFillDelegateImpl : public TouchToFillDelegate,
+                                public AutofillManager::Observer {
  public:
   explicit TouchToFillDelegateImpl(BrowserAutofillManager* manager);
   TouchToFillDelegateImpl(const TouchToFillDelegateImpl&) = delete;
@@ -100,6 +110,15 @@ class TouchToFillDelegateImpl : public TouchToFillDelegate {
   void ShowCreditCardSettings() override;
   void SuggestionSelected(std::string unique_id) override;
   void OnDismissed(bool dismissed_by_user) override;
+
+  // AutofillManager::Observer:
+  void OnAutofillManagerDestroyed(AutofillManager& manager) override;
+  void OnBeforeAskForValuesToFill(AutofillManager& manager,
+                                  FormGlobalId form_id,
+                                  FieldGlobalId field_id) override;
+  void OnAfterAskForValuesToFill(AutofillManager& manager,
+                                 FormGlobalId form_id,
+                                 FieldGlobalId field_id) override;
 
   void LogMetricsAfterSubmission(const FormStructure& submitted_form) const;
 
@@ -135,6 +154,10 @@ class TouchToFillDelegateImpl : public TouchToFillDelegate {
   // Sets whether or not to suppress the on-screen keyboard in following
   // requests that would usually display the keyboard.
   //
+  // If `suppress == true` and had been false before, starts a timer to reset it
+  // again. This timer is stopped by subsequent calls where `suppress == false`
+  // or when TTF is indeed shown.
+  //
   // No-op if `suppress` if the previous call had the same value as `suppress`.
   void SetShouldSuppressKeyboard(bool suppress);
 
@@ -152,10 +175,13 @@ class TouchToFillDelegateImpl : public TouchToFillDelegate {
 
   const raw_ptr<BrowserAutofillManager> manager_;
   bool keyboard_is_suppressed_ = false;
+  base::OneShotTimer keyboard_unsuppress_timer_;
   FormData query_form_;
   FormFieldData query_field_;
   bool dismissed_by_user_;
 
+  base::ScopedObservation<AutofillManager, AutofillManager::Observer>
+      autofill_manager_observation_{this};
   base::WeakPtrFactory<TouchToFillDelegateImpl> weak_ptr_factory_{this};
 };
 
