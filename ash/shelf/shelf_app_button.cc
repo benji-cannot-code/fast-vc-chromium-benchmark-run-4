d079b13e99789ed79309af1710442a07e12bc883
Ben Becker <benbecker@chromium.org>
2023-02-23 01:03:20 +0000

    CloseAll: Interacting with shelf closes toast
    This CL prevents shelf interactions from affecting windows on a closing
    desk. We do this by committing the pending desk removal as soon as the
    user interacts with a shelf item. This fixes a bug where the user could
    close the app for a window that is already in the closing desk, which
    triggers some potentially undesirable behavior. We also add a unit test
    to verify the new behavior.
    
    Bug: b/265489743
    Change-Id: I42fbe58033cbc58e5975c28a83c4f593a24298ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177783
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108680}

--- ash/shelf/shelf_app_button.cc
+++ ash/shelf/shelf_app_button.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/default_color_constants.h"
 #include "ash/style/default_colors.h"
 #include "ash/style/dot_indicator.h"
+#include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/functional/bind.h"
 #include "base/i18n/rtl.h"
@@ -652,6 +653,10 @@ const char* ShelfAppButton::GetClassName() const {
 }
 
 bool ShelfAppButton::OnMousePressed(const ui::MouseEvent& event) {
+  // Clear any closing desks so that the user does not try to interact with an
+  // app that is open on a closing desk.
+  DesksController::Get()->MaybeCommitPendingDeskRemoval();
+
   // TODO: This call should probably live somewhere else (such as inside
   // |ShelfView.PointerPressedOnButton|.
   // No need to scale up the app for mouse right click since the app can't be
