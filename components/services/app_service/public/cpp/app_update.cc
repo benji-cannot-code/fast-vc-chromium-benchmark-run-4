2635817e9647299c62a883e7331abef63088e6cf
Tim Sergeant <tsergeant@chromium.org>
2023-02-23 23:29:43 +0000

    App Management: Work around crash when formatting negative app sizes
    Some ARC apps appear to have a negative data size when formatting the
    app size for use in App Management. As of crrev.com/c/4209750,
    ui::FormatBytes will crash when given a negative value. This CL works
    around the immediate stability issue by checking for a negative value
    and doing a DumpWithoutCrashing if found, which we can then investigate
    with less urgency later.
    
    This CL also changes how missing size values are displayed on
    chrome://app-service-internals, which makes debugging easier.
    
    Bug: 1418064
    Change-Id: I1d7e26be3c8903af3a26ee8985838b40b1c48aa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288847
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109289}

--- components/services/app_service/public/cpp/app_update.cc
+++ components/services/app_service/public/cpp/app_update.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_update.h"
 
 #include "base/logging.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "components/services/app_service/public/cpp/icon_types.h"
@@ -27,6 +28,10 @@ static const std::vector<std::string>& EmptyStringVector() {
 absl::optional<apps::RunOnOsLogin> CloneRunOnOsLogin(
     const apps::RunOnOsLogin& login_data) {
   return apps::RunOnOsLogin(login_data.login_mode, login_data.is_managed);
+}
+
+std::string FormatBytes(absl::optional<uint64_t> bytes) {
+  return bytes.has_value() ? base::NumberToString(bytes.value()) : "null";
 }
 
 }  // namespace
@@ -494,8 +499,8 @@ std::ostream& operator<<(std::ostream& out, const AppUpdate& app) {
         << EnumToString(app.RunOnOsLogin().value().login_mode) << std::endl;
   }
 
-  out << "App Size: " << app.AppSizeInBytes().value_or(-1) << std::endl;
-  out << "Data Size: " << app.DataSizeInBytes().value_or(-1) << std::endl;
+  out << "App Size: " << FormatBytes(app.AppSizeInBytes()) << std::endl;
+  out << "Data Size: " << FormatBytes(app.DataSizeInBytes()) << std::endl;
 
   return out;
 }
