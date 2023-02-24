0baf6a315c52f9b147473083c48fc550a273471c
Meredith Lane <meredithl@chromium.org>
2023-02-24 04:39:08 +0000

    Revert "[CrOS Hotspot] Add hotspot allow status metrics"
    This reverts commit 83e1b196050843f8f919d2a3d681780ad5ca094e.
    
    Reason for revert: Suspected culprit for failing `HotspotMetricsHelperTest.HotspotAllowStatusHistogram` on Linux bots starting from https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/49470/overview
    
    Original change's description:
    > [CrOS Hotspot] Add hotspot allow status metrics
    >
    > This CL adds the HotspotMetricsHelper which used to track hotspot status
    > updates and emit to the corresponding UMA metrics. This CL specifically
    > adds the following histogram:
    > 1. Network.Ash.Hotspot.Upstream.Cellular.Capability.AllowStatus
    > 2. Network.Ash.Hotspot.Upstream.Cellular.Capability.AllowStatusAtLogin
    >
    > Bug: b/269356222, b/239477916
    > Change-Id: I5c2d2fe5d0a105afd85f13096102996e266ca264
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265596
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Commit-Queue: Jason Zhang <jiajunz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1109340}
    
    Bug: b/269356222, b/239477916
    Change-Id: I7408f325b1f2eb96ebacd19cabf4d8971aac7a35
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290269
    Auto-Submit: Meredith Lane <meredithl@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Owners-Override: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109435}

--- chromeos/ash/components/network/hotspot_capabilities_provider.h
+++ chromeos/ash/components/network/hotspot_capabilities_provider.h
@@ -72,6 +72,11 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotCapabilitiesProvider
   // Return the latest hotspot capabilities
   const HotspotCapabilities& GetHotspotCapabilities() const;
 
+  // Update the hotspot allow status with the given |new_allow_status|
+  // and then notify observers if it changes.
+  void SetHotspotAllowStatus(
+      hotspot_config::mojom::HotspotAllowStatus new_allow_status);
+
   // Return callback for the CheckTetheringReadiness method.
   using CheckTetheringReadinessCallback =
       base::OnceCallback<void(CheckTetheringReadinessResult result)>;
@@ -87,8 +92,6 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotCapabilitiesProvider
   bool HasObserver(Observer* observer) const;
 
  private:
-  friend class HotspotMetricsHelperTest;
-
   // ShillPropertyChangedObserver overrides
   void OnPropertyChanged(const std::string& key,
                          const base::Value& value) override;
@@ -99,11 +102,6 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotCapabilitiesProvider
 
   // Callback to handle the manager properties with hotspot related properties.
   void OnManagerProperties(absl::optional<base::Value::Dict> properties);
-
-  // Update the hotspot allow status with the given |new_allow_status|
-  // and then notify observers if it changes.
-  void SetHotspotAllowStatus(
-      hotspot_config::mojom::HotspotAllowStatus new_allow_status);
 
   // Notify observer that hotspot capabilities was changed.
   void NotifyHotspotCapabilitiesChanged();
