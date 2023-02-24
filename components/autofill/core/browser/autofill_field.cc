c0701a9924215dfeee5aa5c269db08bc206eea64
Jihad Hanna <jihadghanna@google.com>
2023-02-24 13:26:54 +0000

    [Autofill] Fixed typos
    Bug: 1007974
    Change-Id: I36e2682041f68f3578431ef4db0fdfd4d28c2a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289131
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109536}

--- components/autofill/core/browser/autofill_field.cc
+++ components/autofill/core/browser/autofill_field.cc
@@ -333,7 +333,7 @@ AutofillType AutofillField::ComputedType() const {
         !(heuristic_type() == NUMERIC_QUANTITY &&
           server_type() != UNKNOWN_TYPE &&
           base::FeatureList::IsEnabled(
-              features::kAutofillGivePrecedenceToNumericQuantitites));
+              features::kAutofillGivePrecedenceToNumericQuantities));
 
     if (believe_server)
       return AutofillType(server_type());
--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -1174,7 +1174,7 @@ void FormStructure::LogQualityMetrics(
       if ((field->is_autofilled || field->previously_autofilled()) &&
           field_has_non_empty_server_prediction &&
           !base::FeatureList::IsEnabled(
-              features::kAutofillGivePrecedenceToNumericQuantitites)) {
+              features::kAutofillGivePrecedenceToNumericQuantities)) {
         AutofillMetrics::
             LogAcceptedFilledFieldWithNumericQuantityHeuristicPrediction(
                 !field->previously_autofilled());
