e463a68f913d1976b2f2b38efb4634264de77652
Shengsong Tan <sstan@google.com>
2023-02-24 01:59:32 +0000

    arc: Create ARCVM swap scheduler by flag parameter
    This CL initialize the vmm swap scheduler for ARCVM by ArcVmmSwapPolicy
    flag.
    
    Bug: b:265383671
    TEST=ArcVmmSwapSchedulerTest
    TEST=manually enable the flag
    
    Change-Id: I9412b6b3b390b0b59ed960880a29add88e17223b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4270470
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109389}

--- chrome/browser/ash/arc/vmm/arc_vmm_manager.h
+++ chrome/browser/ash/arc/vmm/arc_vmm_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback_forward.h"
+#include "chrome/browser/ash/arc/vmm/arc_vmm_swap_scheduler.h"
 #include "chromeos/ash/components/dbus/concierge/concierge_service.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/browser_context.h"
@@ -52,8 +53,16 @@ class ArcVmmManager : public KeyedService {
 
   void PostWithSwapDelay(base::OnceClosure callback);
 
+  // The default delay from swap enabled and swap out. Basically it's used for
+  // keyboard swap. In finch, it will be replaced by the flag parameter.
+  base::TimeDelta swap_out_delay_ = base::Seconds(3);
+
   // Accelerator for experimental usage. Always behind the feature flag.
   std::unique_ptr<AcceleratorTarget> accelerator_;
+
+  // Swap request scheduler for experimental usage. Always behind the feature
+  // flag and parameters.
+  std::unique_ptr<ArcVmmSwapScheduler> scheduler_;
 
   std::string user_id_hash_;
 
