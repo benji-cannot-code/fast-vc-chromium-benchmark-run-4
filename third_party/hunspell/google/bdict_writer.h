19e6dbd6367b41af7a70159441c962d8b08f4438
Alan Zhao <ayzhao@google.com>
2023-02-24 19:59:09 +0000

    Explicitly #include <utility> in bdict_writer.h
    bdict_writer.h uses std::pair, which is declared in <utility> and is
    currently provided via a transitive include in libc++. This transitive
    include is not guaranteed to exist, and will no longer be present once
    libc++ modules are enabled.
    
    Bug: 543704
    Change-Id: I40298fb8ac452b80c69ee415b7b99acb98d4d390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289152
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109737}

--- third_party/hunspell/google/bdict_writer.h
+++ third_party/hunspell/google/bdict_writer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_HUNSPELL_GOOGLE_BDICT_WRITER_H_
 
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace hunspell {
