bd0d595177f162119a00e54de234cc298a6d7b09
Jack Yammine <jyammine@google.com>
2023-02-24 14:41:05 +0000

    [fyfre]Fix lacros intro code and add tests.
    Fix user's avatar and name not loaded.
    Add observers for elements that could load at a later time.
    Add tests.
    
    Bug: 1358845
    Change-Id: Ibc5c019fdc10fd054e1e8124b1ce2a39d0ea407c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4280699
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109553}

--- chrome/test/data/webui/intro/intro_browsertest.js
+++ chrome/test/data/webui/intro/intro_browsertest.js
@@ -9,9 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
 
 GEN('#include "chrome/browser/signin/signin_features.h"')
+GEN('#include "components/signin/public/base/signin_buildflags.h"');
 GEN('#include "content/public/test/browser_test.h"');
 
-class DiceAppBrowserTest extends PolymerTest {
+class IntroBrowserTest extends PolymerTest {
   /** @override */
   get browsePreload() {
     throw new Error('this is abstract and should be overriden by subclasses');
@@ -23,11 +24,12 @@ class DiceAppBrowserTest extends PolymerTest {
   }
 }
 
+GEN('#if BUILDFLAG(ENABLE_DICE_SUPPORT)');
 /**
  * Test that the Sign in and Don't sign in buttons behave correctly when clicked
  * in chrome/browser/resources/intro/sign_in_promo.html
  */
-var SignInPromoTest = class extends DiceAppBrowserTest {
+var SignInPromoTest = class extends IntroBrowserTest {
   /** @override */
   get browsePreload() {
     return 'chrome://intro/test_loader.html?module=' +
@@ -43,7 +45,7 @@ TEST_F('SignInPromoTest', 'All', function() {
   Test that the correct view is loaded when we add or remove the
   noAnimation query parameter
  */
-var DiceAppTest = class extends DiceAppBrowserTest {
+var DiceAppTest = class extends IntroBrowserTest {
   /** @override */
   get browsePreload() {
     return 'chrome://intro/test_loader.html?module=' +
@@ -54,3 +56,21 @@ var DiceAppTest = class extends DiceAppBrowserTest {
 TEST_F('DiceAppTest', 'All', function() {
   mocha.run();
 });
+GEN('#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)');
+
+/**
+  Tests intro/lacros_app.ts
+ */
+GEN('#if BUILDFLAG(IS_CHROMEOS_LACROS)');
+var LacrosAppTest = class extends IntroBrowserTest {
+  /** @override */
+  get browsePreload() {
+    return 'chrome://intro/test_loader.html?module=' +
+        'intro/lacros_app_test.js';
+  }
+};
+
+TEST_F('LacrosAppTest', 'All', function() {
+  mocha.run();
+});
+GEN('#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)');
