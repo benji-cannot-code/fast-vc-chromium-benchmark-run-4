7d66aae80c1546d8df632b9efe2c7e61679b1063
Mohamed Amir Yosef <mamir@chromium.org>
2023-02-24 08:09:51 +0000

    Fix include order in device_info_sync_bridge_unittest.cc
    This is fixing the wrong include order introduced in
    https://crrev.com/c/4251883
    
    Change-Id: Id5e159a648eee43f41debf99acdfe80b2c4b96cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290328
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109466}

--- components/sync_device_info/device_info_sync_bridge_unittest.cc
+++ components/sync_device_info/device_info_sync_bridge_unittest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/sync_device_info/device_info_sync_bridge.h"
-#include "build/build_config.h"
 
 #include <algorithm>
 #include <map>
@@ -22,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/simple_test_clock.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/sync/base/model_type.h"
