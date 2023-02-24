10b9f7622397a5f15ef4f362454c46b86641ea8c
Alan Zhao <ayzhao@google.com>
2023-02-24 02:29:31 +0000

    Explicitly #include <tuple> in trap_registry.h
    trap_registry.h uses std::tie, which is provided in the <tuple> header
    and is currently available due to transitive includes in libc++. These
    includes however are not guaranteed to exist and will go away once
    libc++ is modularized.
    
    Bug: 543704
    Change-Id: Ic744b60b651c9ed7b26edc12444e2ef104f71433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289669
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109401}

--- sandbox/linux/bpf_dsl/trap_registry.h
+++ sandbox/linux/bpf_dsl/trap_registry.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <tuple>
+
 #include "base/check.h"
 #include "sandbox/linux/system_headers/linux_seccomp.h"
 #include "sandbox/sandbox_export.h"
