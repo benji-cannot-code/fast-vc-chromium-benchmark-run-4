a7ef19051b11f3d45402d8e620f0ecc63f72bff9
Mohamed Amir Yosef <mamir@chromium.org>
2023-02-24 09:18:50 +0000

    [UI] EditablePasswordCombobox callback doesn't change the obfuscation
    This CL pulls out the logic for masking/unmasking the password in the
    EditablePasswordCombobox to the embedder.
    Reasons are:
    1- This is more aligned with other control such that ToggleButton.
    2- Follow-up CLs will replace the authentication technique to support
    BioMetric authentication which will have to happen asynchronously.
    i.e. it will be quite tricky to have a synchronous method that decides
    whether to reveal the password.
    
    Bug: 1371026
    Change-Id: I1f980bef24c70e60ab76ab7ca59e9b9c6a8bfd71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4272326
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109481}

--- chrome/browser/ui/views/passwords/password_save_update_view.cc
+++ chrome/browser/ui/views/passwords/password_save_update_view.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/vector_icon_utils.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/bubble/bubble_frame_view.h"
+#include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/combobox/combobox.h"
 #include "ui/views/controls/editable_combobox/editable_combobox.h"
@@ -195,8 +196,7 @@ std::unique_ptr<views::EditableCombobox> CreateUsernameEditableCombobox(
 std::unique_ptr<views::EditablePasswordCombobox> CreateEditablePasswordCombobox(
     const password_manager::PasswordForm& form,
     bool are_passwords_revealed,
-    views::EditablePasswordCombobox::IsPasswordRevealPermittedCheck
-        reveal_permitted_check) {
+    views::Button::PressedCallback reveal_password_callback) {
   DCHECK(!form.IsFederatedCredential());
   std::vector<std::u16string> passwords =
       form.all_possible_passwords.empty()
@@ -210,12 +210,12 @@ std::unique_ptr<views::EditablePasswordCombobox> CreateEditablePasswordCombobox(
       std::make_unique<ui::SimpleComboboxModel>(
           std::vector<ui::SimpleComboboxModel::Item>(passwords.begin(),
                                                      passwords.end())),
-      views::style::CONTEXT_BUTTON, STYLE_PRIMARY_MONOSPACED, display_arrow);
+      views::style::CONTEXT_BUTTON, STYLE_PRIMARY_MONOSPACED, display_arrow,
+      std::move(reveal_password_callback));
   combobox->SetText(form.password_value);
   combobox->SetPasswordIconTooltips(
       l10n_util::GetStringUTF16(IDS_MANAGE_PASSWORDS_SHOW_PASSWORD),
       l10n_util::GetStringUTF16(IDS_MANAGE_PASSWORDS_HIDE_PASSWORD));
-  combobox->SetIsPasswordRevealPermittedCheck(std::move(reveal_permitted_check));
   combobox->RevealPasswords(are_passwords_revealed);
   combobox->SetAccessibleName(
       l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_PASSWORD_LABEL));
@@ -341,8 +341,8 @@ PasswordSaveUpdateView::PasswordSaveUpdateView(
         CreateEditablePasswordCombobox(
             password_form,
             controller_.are_passwords_revealed_when_bubble_is_opened(),
-            base::BindRepeating(&SaveUpdateBubbleController::RevealPasswords,
-                                base::Unretained(&controller_)));
+            base::BindRepeating(&PasswordSaveUpdateView::TogglePasswordRevealed,
+                                base::Unretained(this)));
     // Set up layout:
     SetLayoutManager(std::make_unique<AutoResizingLayout>());
     views::View* root_view = AddChildView(std::make_unique<views::View>());
@@ -699,4 +699,12 @@ void PasswordSaveUpdateView::UpdateFootnote() {
   // The footnote size could have changed since it depends on whether it
   // affects the account store, and hence resize.
   SizeToContents();
+}
+
+void PasswordSaveUpdateView::TogglePasswordRevealed() {
+  if (password_dropdown_->ArePasswordsRevealed()) {
+    password_dropdown_->RevealPasswords(false);
+  } else if (controller_.RevealPasswords()) {
+    password_dropdown_->RevealPasswords(true);
+  }
 }
