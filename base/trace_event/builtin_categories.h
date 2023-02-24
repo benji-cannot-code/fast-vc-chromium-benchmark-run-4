d45c17c405a73ed75616065949bee67a66c9727b
Xianzhu Wang <wangxianzhu@chromium.org>
2023-02-24 18:24:02 +0000

    Add trace event counters for LCDText
    The counters are under category disabled-by-default-cc.debug.lcd_text.
    
    Bug: 1353921
    Change-Id: I18e16e651259983c5f4a5f4fc943432b84b3b17f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283718
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109673}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -207,6 +207,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("cc.debug"))                               \
   X(TRACE_DISABLED_BY_DEFAULT("cc.debug.cdp-perf"))                      \
   X(TRACE_DISABLED_BY_DEFAULT("cc.debug.display_items"))                 \
+  X(TRACE_DISABLED_BY_DEFAULT("cc.debug.lcd_text"))                      \
   X(TRACE_DISABLED_BY_DEFAULT("cc.debug.picture"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("cc.debug.scheduler"))                     \
   X(TRACE_DISABLED_BY_DEFAULT("cc.debug.scheduler.frames"))              \
