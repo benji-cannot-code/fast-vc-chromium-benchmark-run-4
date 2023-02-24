15a136a31c252938b5d5b75d49368b34ec2e484a
Jan Keitel <jkeitel@google.com>
2023-02-24 17:31:07 +0000

    Remove unnecessary includes in c/b/ui/autofill.
    Bug: 1411172
    Change-Id: I10d26a91098fa230a4200d6b2f5d9406ef161478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291430
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109638}

--- chrome/browser/ui/autofill/autofill_popup_view_delegate.h
+++ chrome/browser/ui/autofill/autofill_popup_view_delegate.h
@@ -6,20 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_POPUP_VIEW_DELEGATE_H_
 #define CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_POPUP_VIEW_DELEGATE_H_
 
-#include <stddef.h>
-
 #include "components/autofill/core/browser/ui/popup_types.h"
 #include "ui/gfx/native_widget_types.h"
-
-namespace gfx {
-class Point;
-class Rect;
-class RectF;
-}  // namespace gfx
 
 namespace content {
 class WebContents;
 }  // namespace content
+
+namespace gfx {
+class RectF;
+}  // namespace gfx
+
 namespace autofill {
 
 // Base class for Controllers of Autofill-style popups. This interface is
