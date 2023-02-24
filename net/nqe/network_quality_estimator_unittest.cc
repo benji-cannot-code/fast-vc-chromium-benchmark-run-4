c6d453a866a0c828e506b7cded5de5dd3afa1603
Tom Sepez <tsepez@chromium.org>
2023-02-24 19:47:36 +0000

    Re-work local destruction order in NetworkQualityEstimatorTest
    Avoid spurious dangling ptr warnings by ensuring that the tick
    clock outlives the estimator that has a raw_ptr<> back to it.
    
    Bug: 1401495
    Change-Id: I46c728cf68c8e4302a399ac76491238d1096550c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4255178
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109733}

--- net/nqe/network_quality_estimator_unittest.cc
+++ net/nqe/network_quality_estimator_unittest.cc
@@ -1468,12 +1468,12 @@ TEST_F(NetworkQualityEstimatorTest, TestTransportRttUsedForHttpRttComputation) {
   for (const auto& test : tests) {
     std::map<std::string, std::string> variation_params;
     variation_params["add_default_platform_observations"] = "false";
-    TestNetworkQualityEstimator estimator(variation_params);
 
     base::SimpleTestTickClock tick_clock;
     tick_clock.Advance(base::Seconds(1));
-    estimator.SetTickClockForTesting(&tick_clock);
 
+    TestNetworkQualityEstimator estimator(variation_params);
+    estimator.SetTickClockForTesting(&tick_clock);
     estimator.SetStartTimeNullHttpRtt(test.http_rtt);
     estimator.SetStartTimeNullTransportRtt(test.transport_rtt);
 
@@ -1558,12 +1558,12 @@ TEST_F(NetworkQualityEstimatorTest, TestEndToEndRttUsedForHttpRttComputation) {
     std::map<std::string, std::string> variation_params;
     variation_params["add_default_platform_observations"] = "false";
     variation_params["use_end_to_end_rtt"] = "true";
-    TestNetworkQualityEstimator estimator(variation_params);
 
-    base::SimpleTestTickClock tick_clock;
+    base::SimpleTestTickClock tick_clock;  // Must outlive `estimator`.
     tick_clock.Advance(base::Seconds(1));
-    estimator.SetTickClockForTesting(&tick_clock);
 
+    TestNetworkQualityEstimator estimator(variation_params);
+    estimator.SetTickClockForTesting(&tick_clock);
     estimator.SetStartTimeNullHttpRtt(test.http_rtt);
     estimator.set_start_time_null_end_to_end_rtt(test.end_to_end_rtt);
 
@@ -2895,14 +2895,15 @@ TEST_F(NetworkQualityEstimatorTest, HangingRequestUsingTransportAndHttpOnly) {
 }
 
 TEST_F(NetworkQualityEstimatorTest, PeerToPeerConnectionCounts) {
-  TestNetworkQualityEstimator estimator;
   base::SimpleTestTickClock tick_clock;
+  TestNetworkQualityEstimator estimator;
   estimator.SetTickClockForTesting(&tick_clock);
-  base::HistogramTester histogram_tester;
-
   estimator.OnPeerToPeerConnectionsCountChange(3u);
+
   base::TimeDelta advance_1 = base::Minutes(4);
   tick_clock.Advance(advance_1);
+
+  base::HistogramTester histogram_tester;
   histogram_tester.ExpectTotalCount("NQE.PeerToPeerConnectionsDuration", 0);
 
   estimator.OnPeerToPeerConnectionsCountChange(1u);
