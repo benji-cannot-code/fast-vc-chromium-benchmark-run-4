3058b053690af5f283c80c4ae8a0d6c47dbc9da9
Elliot Tuck <etuck@chromium.org>
2023-02-24 19:46:38 +0000

    Revert "capture_mode: Don't reuse existing notification"
    This reverts commit 9d8e55972b6bbd0167bff3b39bf12847be0ae6d4.
    
    Reason for revert: We decided to go back on this change in favor of using Tote to view recent screenshots (see https://b.corp.google.com/issues/251660986#comment7).
    
    Original change's description:
    > capture_mode: Don't reuse existing notification
    >
    > Currently when a screenshot is taken the generated notification replaces
    > any existing notification about a previous screenshot. This changes the
    > behavior so that each time a screenshot is taken it generates a unique
    > notification that will not be replaced by a future screenshot's
    > notification.
    >
    > Bug: b:251660986
    > Change-Id: I441a9e2bab784f92e8ce18513d7e0a37d924482a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983254
    > Commit-Queue: Elliot Tuck <etuck@chromium.org>
    > Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1064568}
    
    Bug: b:251660986
    Change-Id: I04d2b900738ec5cf713acf33ebe7c18838fa9e3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4277301
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Elliot Tuck <etuck@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109732}

--- ash/system/message_center/ash_notification_view_pixeltest.cc
+++ ash/system/message_center/ash_notification_view_pixeltest.cc
@@ -25,6 +25,8 @@ namespace ash {
 
 namespace {
 
+constexpr char kScreenCaptureNotificationId[] = "capture_mode_notification";
+
 constexpr char kShortTitleString[] = "Short Title";
 constexpr char kMediumTitleString[] = "Test Notification's Multiline Title";
 constexpr char kLongTitleString[] =
@@ -220,13 +222,11 @@ TEST_P(ScreenCaptureNotificationPixelTest, VerifyPopup) {
       .Wait();
 
   // Get the notification view.
-  const std::string notification_id =
-      capture_mode_util::GetScreenCaptureNotificationIdForPath(image_file_path);
-
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       base::StrCat({"screen_capture_popup_notification_",
                     GetDisplayTypeName(GetParam())}),
-      /*revision_number=*/0, test_api()->GetPopupViewForId(notification_id)));
+      /*revision_number=*/0,
+      test_api()->GetPopupViewForId(kScreenCaptureNotificationId)));
 }
 
 }  // namespace ash
