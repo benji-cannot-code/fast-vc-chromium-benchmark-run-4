c063e698fdc0ba9452517c74abfb901f5488e003
Joemer Ramos <joemerramos@chromium.org>
2023-02-24 20:01:41 +0000

    [iOS] Adding metrics for TailoredSecurityIntegration infobars
    We didn't add metrics to record the actions for the infobar. Now,
    for consented infobars, we record going to the settings menu and for
    unconsented infobars we record the infobar button being pressed or
    accepted which also indicates how many people turn on ESB from the
    infobar in the unconsented case.
    
    Bug: 1418982
    Change-Id: I17174838ed2861b40575589633fceeb805b5e833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291365
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109742}

--- ios/chrome/browser/safe_browsing/tailored_security/tailored_security_service_infobar_delegate.mm
+++ ios/chrome/browser/safe_browsing/tailored_security/tailored_security_service_infobar_delegate.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/safe_browsing/tailored_security/tailored_security_service_infobar_delegate.h"
 
+#import "base/metrics/histogram_functions.h"
+#import "components/safe_browsing/core/browser/tailored_security_service/tailored_security_outcome.h"
 #import "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/grit/ios_chromium_strings.h"
@@ -82,13 +84,25 @@ bool TailoredSecurityServiceInfobarDelegate::EqualsDelegate(
 
 bool TailoredSecurityServiceInfobarDelegate::Accept() {
   if (web_state_) {
+    bool enabled =
+        message_state_ ==
+        TailoredSecurityServiceMessageState::kConsentedAndFlowEnabled;
+    std::string consentedHistogram =
+        enabled
+            ? "SafeBrowsing.TailoredSecurityConsentedEnabledMessageOutcome"
+            : "SafeBrowsing.TailoredSecurityConsentedDisabledMessageOutcome";
     switch (message_state_) {
       case TailoredSecurityServiceMessageState::kConsentedAndFlowEnabled:
       case TailoredSecurityServiceMessageState::kConsentedAndFlowDisabled:
+        base::UmaHistogramEnumeration(consentedHistogram,
+                                      TailoredSecurityOutcome::kSettings);
         SafeBrowsingTabHelper::FromWebState(web_state_.get())
             ->OpenSafeBrowsingSettings();
         break;
       case TailoredSecurityServiceMessageState::kUnconsentedAndFlowEnabled:
+        base::UmaHistogramEnumeration(
+            "SafeBrowsing.TailoredSecurityUnconsentedInFlowMessageOutcome",
+            TailoredSecurityOutcome::kAccepted);
         ChromeBrowserState* browser_state =
             ChromeBrowserState::FromBrowserState(web_state_->GetBrowserState());
         SetSafeBrowsingState(
