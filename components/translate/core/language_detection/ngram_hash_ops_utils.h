cf4f896352b9486647f55024490fe5538f11aa15
Alan Zhao <ayzhao@google.com>
2023-02-24 02:02:21 +0000

    Explicitly #include <utility> in ngram_hash_ops_utils.h
    ngram_hash_ops_utils.h uses std::pair, which is currently provided via
    <utility> via a transitive include in libc++ that is not guaranteed to
    exist and will go away once libc++ is modularized.
    
    Bug: 543704
    Change-Id: I9a05c207df0ba7783fc16fdc6c62471b0451a575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289709
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Commit-Queue: Alan Zhao <ayzhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1109391}

--- components/translate/core/language_detection/ngram_hash_ops_utils.h
+++ components/translate/core/language_detection/ngram_hash_ops_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_TRANSLATE_CORE_LANGUAGE_DETECTION_NGRAM_HASH_OPS_UTILS_H_
 
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace translate {
