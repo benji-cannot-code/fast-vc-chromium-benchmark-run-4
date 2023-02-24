5008c13e3b4ee121422e801ad420d43a95f7f932
Nick Burris <nburris@chromium.org>
2023-02-24 14:39:03 +0000

    [Reland] Add PaymentHandlerMinimalHeaderUX UseCounter
    This is a reland of commit 695d0696a8dd5c281e9e2ac2d280363153517be3. The
    flaky test should be fixed by adding a navigation before checking
    histograms like we do in other payments browsertests.
    
    This adds a blink UseCounter for the PaymentHandlerMinimalHeaderUX
    feature. This is useful for metrics, and is a requirement for running
    an origin trial.
    
    Bug: 1415497
    Change-Id: Id9d25b7baf09f4011d02bd122f6285f818566592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289641
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Auto-Submit: Nick Burris <nburris@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109552}

--- chrome/browser/ui/views/payments/payment_handler_header_view_ui_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_handler_header_view_ui_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/views/payments/payment_request_browsertest_base.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features_generated.h"
 
 namespace payments {
 namespace {
@@ -36,11 +38,22 @@ class PaymentHandlerHeaderViewUITest
     }
   }
 
+  void ExpectHistograms() {
+    // Navigate away in order to flush use counters.
+    ASSERT_TRUE(content::NavigateToURL(GetActiveWebContents(),
+                                       GURL(url::kAboutBlankURL)));
+    histograms_.ExpectBucketCount(
+        "Blink.UseCounter.Features",
+        blink::mojom::WebFeature::kPaymentHandlerMinimalHeaderUX,
+        minimal_header_ux_enabled_ ? 1 : 0);
+  }
+
  protected:
   bool minimal_header_ux_enabled_;
 
  private:
   base::test::ScopedFeatureList features_;
+  base::HistogramTester histograms_;
 };
 
 IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest,
@@ -94,6 +107,8 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest,
     EXPECT_EQ(u"Payment App",
               GetLabelText(DialogViewID::SHEET_TITLE, view_stack->top()));
   }
+
+  ExpectHistograms();
 }
 
 IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, HeaderWithoutIcon) {
@@ -132,6 +147,8 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, HeaderWithoutIcon) {
 
   // The payment app has no icon, so it should not be displayed on the header.
   EXPECT_FALSE(IsViewVisible(DialogViewID::PAYMENT_APP_HEADER_ICON));
+
+  ExpectHistograms();
 }
 
 IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, CloseButtonPressed) {
@@ -185,6 +202,8 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, CloseButtonPressed) {
     ResetEventWaiter(DialogEvent::BACK_NAVIGATION);
     ClickOnDialogViewAndWait(DialogViewID::BACK_BUTTON);
   }
+
+  ExpectHistograms();
 }
 
 // Test that the header and dialog heights are consistent with when there is no
@@ -245,6 +264,8 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, ConsistentHeaderHeight) {
           ->height());
 
   ClickOnCancel();
+
+  ExpectHistograms();
 }
 
 INSTANTIATE_TEST_SUITE_P(All, PaymentHandlerHeaderViewUITest, testing::Bool());
