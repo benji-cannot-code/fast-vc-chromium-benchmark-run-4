122d088c19e1de422d34520ba7669d01d231b71e
Matt Menke <mmenke@chromium.org>
2023-02-24 12:28:32 +0000

    Remove Value::ExtractKey().
    All consumers have been migrated to Value::Dict::Extract().
    
    R=mark@chromium.org
    
    Bug: 1406815
    Change-Id: I3b0b2354c49f42452e05ba2017e0ed7a9cff6ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265171
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109522}

--- base/values_unittest.cc
+++ base/values_unittest.cc
@@ -1399,7 +1399,7 @@ TEST(ValuesTest, SetStringPath) {
   ASSERT_FALSE(root.SetByDottedPath("foo.bar.zoo", "ola mundo"));
 }
 
-TEST(ValuesTest, RemoveKey) {
+TEST(ValuesTest, Remove) {
   Value::Dict root;
   root.Set("one", Value(123));
 
@@ -1413,7 +1413,7 @@ TEST(ValuesTest, RemoveKey) {
   EXPECT_FALSE(root.Remove("one"));
 }
 
-TEST(ValuesTest, ExtractKey) {
+TEST(ValuesTest, Extract) {
   Value::Dict root;
   root.Set("one", Value(123));
 
@@ -1427,7 +1427,7 @@ TEST(ValuesTest, ExtractKey) {
   EXPECT_EQ(absl::nullopt, root.Extract("one"));
 }
 
-TEST(ValuesTest, RemovePath) {
+TEST(ValuesTest, RemoveByDottedPath) {
   Value::Dict root;
   root.SetByDottedPath("one.two.three", Value(123));
 
