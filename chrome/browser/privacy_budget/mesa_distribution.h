abe7c97ede0679b2fef5baf38754e1fd030010ca
Alan Zhao <ayzhao@google.com>
2023-02-24 01:01:21 +0000

    Explicitly #include <cmath> in mesa_distribution.h
    mesa_distribution.h calls std::ceil(...), which is defined in <cmath>
    and is previously provided via transitive libc++ includes. However,
    these includes are not guaranteed, and they will go away once libc++ is
    modularized.
    
    Bug: 543704
    Change-Id: I5fd97a433088ceeb83e8ff36f345d30c6dd0ee28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289667
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109366}

--- chrome/browser/privacy_budget/mesa_distribution.h
+++ chrome/browser/privacy_budget/mesa_distribution.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PRIVACY_BUDGET_MESA_DISTRIBUTION_H_
 #define CHROME_BROWSER_PRIVACY_BUDGET_MESA_DISTRIBUTION_H_
 
+#include <cmath>
 #include <random>
 #include <set>
 #include <type_traits>
