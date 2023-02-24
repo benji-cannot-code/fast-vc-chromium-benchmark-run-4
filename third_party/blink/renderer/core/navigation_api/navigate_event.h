4f6e7b8f1e356374bfa1942032fc49edca0682a9
Nate Chapin <japhet@chromium.org>
2023-02-24 01:10:14 +0000

    Move commit for an intercept()ed navigation to NavigateEvent, attempt #2
    Change-Id: I4f1fc606f285976889a506ab49055b0d0371903d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4284797
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109369}

--- third_party/blink/renderer/core/navigation_api/navigate_event.h
+++ third_party/blink/renderer/core/navigation_api/navigate_event.h
@@ -48,7 +48,9 @@ class NavigateEvent final : public Event,
                 const AtomicString& type,
                 NavigateEventInit* init);
 
-  void SetUrl(const KURL& url) { url_ = url; }
+  void SetDispatchParams(NavigateEventDispatchParams* dispatch_params) {
+    dispatch_params_ = dispatch_params;
+  }
 
   String navigationType() { return navigation_type_; }
   NavigationDestination* destination() { return destination_; }
@@ -62,6 +64,8 @@ class NavigateEvent final : public Event,
 
   void intercept(NavigationInterceptOptions*, ExceptionState&);
 
+  void DoCommit();
+
   void scroll(ExceptionState&);
   void PotentiallyProcessScrollBehavior();
 
@@ -73,8 +77,6 @@ class NavigateEvent final : public Event,
 
   void ResetFocusIfNeeded();
   bool ShouldSendAxEvents() const;
-
-  void SaveStateFromDestinationItem(HistoryItem*);
 
   // FocusedElementChangeObserver implementation:
   void DidChangeFocus() final;
@@ -96,9 +98,9 @@ class NavigateEvent final : public Event,
   ScriptValue info_;
   absl::optional<V8NavigationFocusReset> focus_reset_behavior_ = absl::nullopt;
   absl::optional<V8NavigationScrollBehavior> scroll_behavior_ = absl::nullopt;
-  absl::optional<HistoryItem::ViewState> history_item_view_state_;
 
-  KURL url_;
+  Member<NavigateEventDispatchParams> dispatch_params_;
+
   bool has_navigation_actions_ = false;
   HeapVector<ScriptPromise> navigation_action_promises_list_;
   HeapVector<Member<V8NavigationInterceptHandler>>
