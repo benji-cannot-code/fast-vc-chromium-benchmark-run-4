b6324f2c13becee758a678ea9757d72333f5941a
Caroline Rising <corising@chromium.org>
2023-02-24 19:08:21 +0000

    Notes: add headings to the user notes overviews side panel ui.
    Updating overviews sorting so that the overview for the current tab is
    listed first, if one exists. Add a heading for the current tab and one
    for all of the rest of the notes.
    
    Bug: 1409894
    Change-Id: I1fefb29e2202c67f3002a65041b086f9ba118c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4255929
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109699}

--- chrome/test/data/webui/side_panel/user_notes/user_note_overviews_list_test.ts
+++ chrome/test/data/webui/side_panel/user_notes/user_note_overviews_list_test.ts
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://webui-test/mojo_webui_test_support.js';
 import 'chrome://user-notes-side-panel.top-chrome/user_note_overviews_list.js';
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
+import {UserNoteOverviewRowElement} from 'chrome://user-notes-side-panel.top-chrome/user_note_overview_row.js';
 import {UserNoteOverviewsListElement} from 'chrome://user-notes-side-panel.top-chrome/user_note_overviews_list.js';
 import {NoteOverview} from 'chrome://user-notes-side-panel.top-chrome/user_notes.mojom-webui.js';
 import {UserNotesApiProxyImpl} from 'chrome://user-notes-side-panel.top-chrome/user_notes_api_proxy.js';
@@ -37,7 +39,7 @@ suite('UserNoteOverviewsListTest', () => {
     },
   ];
 
-  function queryNoteOverviews() {
+  function queryNoteOverviews(): NodeListOf<UserNoteOverviewRowElement> {
     return userNoteOverviewsList.shadowRoot!.querySelectorAll(
         'user-note-overview-row');
   }
@@ -48,6 +50,10 @@ suite('UserNoteOverviewsListTest', () => {
     testProxy = new TestUserNotesApiProxy();
     UserNotesApiProxyImpl.setInstance(testProxy);
     testProxy.setNoteOverviews(noteOverviews);
+    loadTimeData.resetForTesting({
+      currentTab: 'Current Tab',
+      allNotes: 'All Notes',
+    });
 
     userNoteOverviewsList = document.createElement('user-note-overviews-list');
     userNoteOverviewsList.overviews = noteOverviews;
@@ -64,5 +70,15 @@ suite('UserNoteOverviewsListTest', () => {
               .textContent!.trim(),
           noteOverviews[i]!.title);
     }
+  });
+
+  test('headers and separator shown', () => {
+    const noteOverviewElements = queryNoteOverviews();
+    assertEquals(noteOverviewElements.length, 2);
+    const headers =
+        userNoteOverviewsList.shadowRoot!.querySelectorAll('sp-heading');
+    assertEquals(headers.length, 2);
+    const separator = userNoteOverviewsList.shadowRoot!.querySelectorAll('.hr');
+    assertEquals(separator.length, 1);
   });
 });
