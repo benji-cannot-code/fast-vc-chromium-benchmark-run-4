51e685bc66ec87e533c77cca08f7f11184d4b39a
Sean Kau <skau@chromium.org>
2023-02-24 00:59:54 +0000

    Cleanup legacy color for ARC overlay
    kColorAshShieldAndBase60Light is in a dead codepath. Feature flag
    is always true. Removing.
    
    Change-Id: Ie40c3ebfeba00c831f26851312deea0cd82c245c
    Bug: b:270470717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285919
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109365}

--- ash/components/arc/compat_mode/overlay_dialog.cc
+++ ash/components/arc/compat_mode/overlay_dialog.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/compat_mode/overlay_dialog.h"
 
 #include "ash/components/arc/compat_mode/style/arc_color_provider.h"
-#include "ash/constants/ash_features.h"
 #include "ash/style/ash_color_id.h"
 #include "base/functional/bind.h"
 #include "components/exo/shell_surface_base.h"
@@ -55,10 +54,8 @@ void OverlayDialog::AddedToWidget() {
 
 void OverlayDialog::OnThemeChanged() {
   views::View::OnThemeChanged();
-  const ui::ColorId color_id = ash::features::IsDarkLightModeEnabled()
-                                   ? ash::kColorAshShieldAndBase60
-                                   : ash::kColorAshShieldAndBase60Light;
-  SetBackground(views::CreateThemedSolidBackground(color_id));
+  SetBackground(
+      views::CreateThemedSolidBackground(ash::kColorAshShieldAndBase60));
 }
 
 OverlayDialog::OverlayDialog(base::OnceClosure on_destroying,
