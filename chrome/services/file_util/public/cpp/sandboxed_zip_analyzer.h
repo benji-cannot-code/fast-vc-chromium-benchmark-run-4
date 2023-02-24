48b6a644f375918274510f7793b73c342e896960
Adam Psarouthakis <psarouthakis@google.com>
2023-02-24 16:25:49 +0000

    Temporary File Getter Interface
    This CL contains the initial implementation of the Temporary File Getter Interface which will provide temporary files to assist in the unpacking of nested archives for analysis. It is integrated into the Safe Archive Analyzer's Analyze Zip File function. Right now the overall behavior of the Safe Archive Analyzer is unchanged.
    
    Bug: 1373671
    Change-Id: Idd92d299463468c9570068ad1bff8043637098f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265394
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109601}

--- chrome/services/file_util/public/cpp/sandboxed_zip_analyzer.h
+++ chrome/services/file_util/public/cpp/sandboxed_zip_analyzer.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted_delete_on_sequence.h"
+#include "chrome/services/file_util/public/cpp/temporary_file_getter.h"
 #include "chrome/services/file_util/public/mojom/file_util_service.mojom.h"
 #include "chrome/services/file_util/public/mojom/safe_archive_analyzer.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -53,7 +54,7 @@ class SandboxedZipAnalyzer {
   void ReportFileFailure(safe_browsing::ArchiveAnalysisResult reason);
 
   // Starts the utility process and sends it a file analyze request.
-  void AnalyzeFile(base::File file, base::File temp);
+  void AnalyzeFile(base::File file);
 
   // The response containing the file analyze results.
   void AnalyzeFileDone(const safe_browsing::ArchiveAnalyzerResults& results);
@@ -70,6 +71,7 @@ class SandboxedZipAnalyzer {
   // Remote interfaces to the file util service. Only used from the UI thread.
   mojo::Remote<chrome::mojom::FileUtilService> service_;
   mojo::Remote<chrome::mojom::SafeArchiveAnalyzer> remote_analyzer_;
+  TemporaryFileGetter temp_file_getter_;
 
   base::WeakPtrFactory<SandboxedZipAnalyzer> weak_ptr_factory_{this};
 };
