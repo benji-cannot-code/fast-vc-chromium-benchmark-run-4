71bedff180df601d240f3671642f28718c6906da
David Bertoni <dbertoni@chromium.org>
2023-02-24 03:31:00 +0000

    [Extensions] Move FontSettingsEventRouter to the source file.
    This API has a custom event router class is that is defined in
    the header file, which is unnecessary and creates additional header
    file dependencies. This CL moves the definition to the source file,
    removing a number of header files in the process.
    
    Bug: 883570
    Change-Id: Ibb1bf9d3b5fa3f5c4e5b63cb94487167aa7ccad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381739
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109416}

--- chrome/browser/extensions/api/font_settings/font_settings_api.cc
+++ chrome/browser/extensions/api/font_settings/font_settings_api.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/json/json_writer.h"
 #include "base/lazy_instance.h"
+#include "base/memory/raw_ptr.h"
 #include "base/strings/string_util.h"
 #include "base/trace_event/trace_event.h"
 #include "base/values.h"
@@ -23,15 +24,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/api/preference/preference_helpers.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/font_pref_change_notifier.h"
 #include "chrome/browser/font_pref_change_notifier_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/font_settings.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_names_util.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/font_list_async.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
+#include "extensions/browser/event_router.h"
+#include "extensions/browser/extension_event_histogram_value.h"
 #include "extensions/browser/extension_prefs_helper.h"
 #include "extensions/browser/extension_prefs_helper_factory.h"
 #include "extensions/browser/extension_system.h"
@@ -96,6 +101,57 @@ void MaybeUnlocalizeFontName(std::string* font_name) {
 }
 
 }  // namespace
+
+// This class observes pref changed events on a profile and dispatches the
+// corresponding extension API events to extensions.
+class FontSettingsEventRouter {
+ public:
+  // Constructor for observing pref changed events on `profile`. Stores a
+  // pointer to `profile` but does not take ownership. `profile` must be
+  // non-NULL and remain alive for the lifetime of the instance.
+  explicit FontSettingsEventRouter(Profile* profile);
+
+  FontSettingsEventRouter(const FontSettingsEventRouter&) = delete;
+  FontSettingsEventRouter& operator=(const FontSettingsEventRouter&) = delete;
+
+  virtual ~FontSettingsEventRouter();
+
+ private:
+  // Observes browser pref `pref_name`. When a change is observed, dispatches
+  // event `event_name` to extensions. A JavaScript object is passed to the
+  // extension event function with the new value of the pref in property `key`.
+  void AddPrefToObserve(const char* pref_name,
+                        events::HistogramValue histogram_value,
+                        const char* event_name,
+                        const char* key);
+
+  // Decodes a preference change for a font family map and invokes
+  // OnFontNamePrefChange with the right parameters.
+  void OnFontFamilyMapPrefChanged(const std::string& pref_name);
+
+  // Dispatches a changed event for the font setting for `generic_family` and
+  // `script` to extensions. The new value of the setting is the value of
+  // browser pref `pref_name`.
+  void OnFontNamePrefChanged(const std::string& pref_name,
+                             const std::string& generic_family,
+                             const std::string& script);
+
+  // Dispatches the setting changed event `event_name` to extensions. The new
+  // value of the setting is the value of browser pref `pref_name`. This value
+  // is passed in the JavaScript object argument to the extension event function
+  // under the key `key`.
+  void OnFontPrefChanged(events::HistogramValue histogram_value,
+                         const std::string& event_name,
+                         const std::string& key,
+                         const std::string& pref_name);
+
+  // Manages pref observation registration.
+  PrefChangeRegistrar registrar_;
+  FontPrefChangeNotifier::Registrar font_change_registrar_;
+
+  // Weak, owns us (transitively via ExtensionService).
+  raw_ptr<Profile> profile_;
+};
 
 FontSettingsEventRouter::FontSettingsEventRouter(Profile* profile)
     : profile_(profile) {
@@ -220,7 +276,7 @@ ExtensionFunction::ResponseAction FontSettingsClearFontFunction::Run() {
   std::string pref_path = GetFontNamePrefPath(params->details.generic_family,
                                               params->details.script);
 
-  // Ensure |pref_path| really is for a registered per-script font pref.
+  // Ensure `pref_path` really is for a registered per-script font pref.
   EXTENSION_FUNCTION_VALIDATE(profile->GetPrefs()->FindPreference(pref_path));
 
   ExtensionPrefsHelper::Get(profile)->RemoveExtensionControlledPref(
@@ -274,7 +330,7 @@ ExtensionFunction::ResponseAction FontSettingsSetFontFunction::Run() {
   std::string pref_path = GetFontNamePrefPath(params->details.generic_family,
                                               params->details.script);
 
-  // Ensure |pref_path| really is for a registered font pref.
+  // Ensure `pref_path` really is for a registered font pref.
   EXTENSION_FUNCTION_VALIDATE(profile->GetPrefs()->FindPreference(pref_path));
 
   ExtensionPrefsHelper::Get(profile)->SetExtensionControlledPref(
