73024c669a9fc873cda8fef9c33b587b63de8a11
Claudio DeSouza <cdesouza@igalia.com>
2023-02-24 03:18:01 +0000

    Switch to simpler base::WriteFile() variants (37/N)
    This CL is part of a batch of CLs to replace the use of the less
    friendly base::WriteFile call with simpler variants, which are easier to
    read, and less prone to mistakes.
    
    Changes in this particular CL address files under the path
    /chrome/browser/sync_file_system.
    
    This CL was uploaded by git cl split.
    
    R=nhiroki@chromium.org
    
    AX-Relnotes: n/a.
    Bug: 418837
    Change-Id: I7640919e803a9f3d5d34041561aea097e9f7590a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4286219
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109412}

--- chrome/browser/sync_file_system/local/local_file_sync_context_unittest.cc
+++ chrome/browser/sync_file_system/local/local_file_sync_context_unittest.cc
@@ -805,13 +805,13 @@ TEST_F(LocalFileSyncContextTest, ApplyRemoteChangeForAddOrUpdate) {
   const FileSystemURL kFile2(file_system.URL("file2"));
   const FileSystemURL kDir(file_system.URL("dir"));
 
-  const char kTestFileData0[] = "0123456789";
-  const char kTestFileData1[] = "Lorem ipsum!";
-  const char kTestFileData2[] = "This is sample test data.";
+  const std::string kTestFileData0 = "0123456789";
+  const std::string kTestFileData1 = "Lorem ipsum!";
+  const std::string kTestFileData2 = "This is sample test data.";
 
   // Create kFile1 and populate it with kTestFileData0.
   EXPECT_EQ(base::File::FILE_OK, file_system.CreateFile(kFile1));
-  EXPECT_EQ(static_cast<int64_t>(std::size(kTestFileData0) - 1),
+  EXPECT_EQ(static_cast<int>(kTestFileData0.size()),
             file_system.WriteString(kFile1, kTestFileData0));
 
   // kFile2 and kDir are not there yet.
@@ -831,12 +831,8 @@ TEST_F(LocalFileSyncContextTest, ApplyRemoteChangeForAddOrUpdate) {
   const base::FilePath kFilePath1(temp_dir.GetPath().Append(FPL("file1")));
   const base::FilePath kFilePath2(temp_dir.GetPath().Append(FPL("file2")));
 
-  ASSERT_EQ(static_cast<int>(std::size(kTestFileData1) - 1),
-            base::WriteFile(kFilePath1, kTestFileData1,
-                            std::size(kTestFileData1) - 1));
-  ASSERT_EQ(static_cast<int>(std::size(kTestFileData2) - 1),
-            base::WriteFile(kFilePath2, kTestFileData2,
-                            std::size(kTestFileData2) - 1));
+  ASSERT_TRUE(base::WriteFile(kFilePath1, kTestFileData1));
+  ASSERT_TRUE(base::WriteFile(kFilePath2, kTestFileData2));
 
   // Record the usage.
   int64_t usage = -1, new_usage = -1;
@@ -866,8 +862,7 @@ TEST_F(LocalFileSyncContextTest, ApplyRemoteChangeForAddOrUpdate) {
                               SYNC_FILE_TYPE_FILE));
 
   // Check if the usage has been increased by (kTestFileData1 - kTestFileData0).
-  const int updated_size =
-      std::size(kTestFileData1) - std::size(kTestFileData0);
+  const int updated_size = kTestFileData1.size() - kTestFileData0.size();
   EXPECT_EQ(blink::mojom::QuotaStatusCode::kOk,
             file_system.GetUsageAndQuota(&new_usage, &quota));
   EXPECT_EQ(updated_size, new_usage - usage);
@@ -917,8 +912,7 @@ TEST_F(LocalFileSyncContextTest, ApplyRemoteChangeForAddOrUpdate) {
   new_usage = usage;
   EXPECT_EQ(blink::mojom::QuotaStatusCode::kOk,
             file_system.GetUsageAndQuota(&new_usage, &quota));
-  EXPECT_GT(new_usage,
-            static_cast<int64_t>(usage + std::size(kTestFileData2) - 1));
+  EXPECT_GT(new_usage, static_cast<int64_t>(usage + kTestFileData2.size()));
 
   // The changes applied by ApplyRemoteChange should not be recorded in
   // the change tracker.
@@ -952,7 +946,7 @@ TEST_F(LocalFileSyncContextTest, ApplyRemoteChangeForAddOrUpdate_NoParent) {
             file_system.MaybeInitializeFileSystemContext(sync_context_.get()));
   ASSERT_EQ(base::File::FILE_OK, file_system.OpenFileSystem());
 
-  const char kTestFileData[] = "Lorem ipsum!";
+  constexpr base::StringPiece kTestFileData = "Lorem ipsum!";
   const FileSystemURL kDir(file_system.URL("dir"));
   const FileSystemURL kFile(file_system.URL("dir/file"));
 
@@ -962,9 +956,7 @@ TEST_F(LocalFileSyncContextTest, ApplyRemoteChangeForAddOrUpdate_NoParent) {
 
   // Prepare a temporary file which represents remote file data.
   const base::FilePath kFilePath(temp_dir.GetPath().Append(FPL("file")));
-  ASSERT_EQ(
-      static_cast<int>(std::size(kTestFileData) - 1),
-      base::WriteFile(kFilePath, kTestFileData, std::size(kTestFileData) - 1));
+  ASSERT_TRUE(base::WriteFile(kFilePath, kTestFileData));
 
   // Calling ApplyChange's with kFilePath should create
   // kFile along with kDir.
