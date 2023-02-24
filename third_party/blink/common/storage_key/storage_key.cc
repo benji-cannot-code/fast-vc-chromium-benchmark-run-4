5a3d902143624b366d53f77da7a25b6b19a7adfe
Ari Chivukula <arichiv@chromium.org>
2023-02-24 23:28:59 +0000

    [StorageKey] Prevent deserialization of 3P storage keys when partitioning is disabled
    If the browser isn't in a state to reason about the underlying
    StorageKey it shouldn't be possible to load it into memory.
    This prevents the serialization path from lying about what the
    deserialization path started with.
    
    Bug: 1419017
    Change-Id: Id865afeaad0614c017444dac363046b8a44c4c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291920
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109879}

--- third_party/blink/common/storage_key/storage_key.cc
+++ third_party/blink/common/storage_key/storage_key.cc
@@ -155,6 +155,11 @@ absl::optional<StorageKey> StorageKey::Deserialize(base::StringPiece in) {
 
   switch (first_attribute.value()) {
     case EncodedAttribute::kTopLevelSite: {
+      // Cross-Origin keys cannot be read if partitioning is off.
+      if (!IsThirdPartyStoragePartitioningEnabled()) {
+        return absl::nullopt;
+      }
+
       // A top-level site is serialized and has only one encoded attribute.
       if (pos_second_caret != std::string::npos) {
         return absl::nullopt;
@@ -199,6 +204,11 @@ absl::optional<StorageKey> StorageKey::Deserialize(base::StringPiece in) {
                         blink::mojom::AncestorChainBit::kCrossSite);
     }
     case EncodedAttribute::kAncestorChainBit: {
+      // Same-Origin kCrossSite keys cannot be read if partitioning is off.
+      if (!IsThirdPartyStoragePartitioningEnabled()) {
+        return absl::nullopt;
+      }
+
       // An ancestor chain bit is serialized and has only one encoded attribute.
       if (pos_second_caret != std::string::npos) {
         return absl::nullopt;
@@ -301,6 +311,11 @@ absl::optional<StorageKey> StorageKey::Deserialize(base::StringPiece in) {
     }
     case EncodedAttribute::kTopLevelSiteOpaqueNonceHigh: {
       // An opaque `top_level_site` is serialized.
+
+      // Cross-Origin keys cannot be read if partitioning is off.
+      if (!IsThirdPartyStoragePartitioningEnabled()) {
+        return absl::nullopt;
+      }
 
       // Make sure we found the next separator, it's valid, that it's the
       // correct attribute.
@@ -602,8 +617,7 @@ std::string StorageKey::Serialize() const {
   // Else if storage partitioning is enabled we need to serialize the key to fit
   // one of the following schemes:
   //
-  // Case 1: If the origin matches top_level_site and the ancestor_chain_bit is
-  // kSameSite:
+  // Case 1: If the ancestor_chain_bit is kSameSite or partitioning is disabled:
   //
   // <StorageKey `key`.origin> + "/"
   //
@@ -626,8 +640,7 @@ std::string StorageKey::Serialize() const {
   // `key`.top_level_site.nonce.Low64Bits>  + "^6" + <StorageKey
   // `key`.top_level_site.precursor>
   if (IsThirdPartyStoragePartitioningEnabled() &&
-      (top_level_site_ != net::SchemefulSite(origin_) ||
-       ancestor_chain_bit_ == blink::mojom::AncestorChainBit::kCrossSite)) {
+      ancestor_chain_bit_ == blink::mojom::AncestorChainBit::kCrossSite) {
     if (top_level_site_.opaque()) {
       // Case 4.
       return base::StrCat({
