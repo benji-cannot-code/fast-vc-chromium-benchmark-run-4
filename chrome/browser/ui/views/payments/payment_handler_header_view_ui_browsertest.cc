589ae027447ae7e320445b57f1ff141fbfbb1609
Meredith Lane <meredithl@chromium.org>
2023-02-24 01:14:19 +0000

    Revert "Add PaymentHandlerMinimalHeaderUX UseCounter"
    This reverts commit 695d0696a8dd5c281e9e2ac2d280363153517be3.
    
    Reason for revert: Suspected culprit for breakages starting from https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(Wayland)/30944/overview
    
    Original change's description:
    > Add PaymentHandlerMinimalHeaderUX UseCounter
    >
    > This adds a blink UseCounter for the PaymentHandlerMinimalHeaderUX
    > feature. This is useful for metrics, and is a requirement for running
    > an origin trial.
    >
    > Bug: 1415497
    > Change-Id: I394436cfaa049ecd4a8b0eef778f0a71aa9d9025
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283982
    > Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    > Commit-Queue: Nick Burris <nburris@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1109172}
    
    Bug: 1415497
    Change-Id: I39421cc1f17733eacf4b9777cb404e416106ce36
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289549
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Owners-Override: Meredith Lane <meredithl@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Quick-Run: Meredith Lane <meredithl@chromium.org>
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Auto-Submit: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109372}

--- chrome/browser/ui/views/payments/payment_handler_header_view_ui_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_handler_header_view_ui_browsertest.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/views/payments/payment_request_browsertest_base.h"
@@ -13,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/common/features_generated.h"
 
 namespace payments {
 namespace {
@@ -38,19 +36,11 @@ class PaymentHandlerHeaderViewUITest
     }
   }
 
-  void ExpectHistograms() {
-    histograms_.ExpectBucketCount(
-        "Blink.UseCounter.Features",
-        blink::mojom::WebFeature::kPaymentHandlerMinimalHeaderUX,
-        minimal_header_ux_enabled_ ? 1 : 0);
-  }
-
  protected:
   bool minimal_header_ux_enabled_;
 
  private:
   base::test::ScopedFeatureList features_;
-  base::HistogramTester histograms_;
 };
 
 IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest,
@@ -104,8 +94,6 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest,
     EXPECT_EQ(u"Payment App",
               GetLabelText(DialogViewID::SHEET_TITLE, view_stack->top()));
   }
-
-  ExpectHistograms();
 }
 
 IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, HeaderWithoutIcon) {
@@ -144,8 +132,6 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, HeaderWithoutIcon) {
 
   // The payment app has no icon, so it should not be displayed on the header.
   EXPECT_FALSE(IsViewVisible(DialogViewID::PAYMENT_APP_HEADER_ICON));
-
-  ExpectHistograms();
 }
 
 IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, CloseButtonPressed) {
@@ -199,8 +185,6 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, CloseButtonPressed) {
     ResetEventWaiter(DialogEvent::BACK_NAVIGATION);
     ClickOnDialogViewAndWait(DialogViewID::BACK_BUTTON);
   }
-
-  ExpectHistograms();
 }
 
 // Test that the header and dialog heights are consistent with when there is no
@@ -261,8 +245,6 @@ IN_PROC_BROWSER_TEST_P(PaymentHandlerHeaderViewUITest, ConsistentHeaderHeight) {
           ->height());
 
   ClickOnCancel();
-
-  ExpectHistograms();
 }
 
 INSTANTIATE_TEST_SUITE_P(All, PaymentHandlerHeaderViewUITest, testing::Bool());
