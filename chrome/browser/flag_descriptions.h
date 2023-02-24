9e80e25f80cad8d3456c70c3520f5f896961920d
Jason Thai <jasontt@chromium.org>
2023-02-24 04:09:23 +0000

    ambient: add feature flag for Time of Day Screen Saver
    flag: time-of-day-screen-saver
    
    BUG=b/270597524
    TEST=unit_tests "AboutFlagsHistogramTest.*" "AboutFlagsTest.*"
    R=assistive-code-review@google.com
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I593f49e9095913f152f845cd62e4c98e119062a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4287926
    Reviewed-by: Eric Sum <esum@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109428}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3479,6 +3479,9 @@ extern const char kTerminalTmuxIntegrationDescription[];
 extern const char kTetherName[];
 extern const char kTetherDescription[];
 
+extern const char kTimeOfDayScreenSaverName[];
+extern const char kTimeOfDayScreenSaverDescription[];
+
 extern const char kTimeOfDayWallpaperName[];
 extern const char kTimeOfDayWallpaperDescription[];
 
