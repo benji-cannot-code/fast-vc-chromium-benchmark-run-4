e09c3b8a40b47bc66aacf0c106ff31a5398473c2
Markus Handell <handellm@google.com>
2023-02-24 15:46:13 +0000

    Implement support for Chrome task origin tracing. #1/4
    This prepares for re-introduction of a Location class in WebRTC
    which is aliased to be base::Location in Chromium builds, by
    introducing the api/location.h file.
    
    Bug: 1416199
    Change-Id: I4eab0ba3b7c10935b3edc4b7329def50d4aa8d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4286945
    Commit-Queue: Markus Handell <handellm@google.com>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109577}

---
+++ third_party/webrtc_overrides/api/location.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_WEBRTC_OVERRIDES_API_LOCATION_H_
+#define THIRD_PARTY_WEBRTC_OVERRIDES_API_LOCATION_H_
+
+#include "base/location.h"
+
+namespace webrtc {
+using Location = base::Location;
+}  // namespace webrtc
+
+#endif  // THIRD_PARTY_WEBRTC_OVERRIDES_API_LOCATION_H_
