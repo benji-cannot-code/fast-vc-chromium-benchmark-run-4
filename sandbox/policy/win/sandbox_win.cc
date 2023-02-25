25c72f7ebaad7459a49c29e1f92309e5eb3abf11
Alex Gough <ajgo@chromium.org>
2023-02-25 18:12:08 +0000

    Remove feature for shared sandbox policies
    This has been enabled without problems so remove the flag.
    
    Fixed: 549319
    Change-Id: Ief3d7099291e5a50ec2f98243882c9141b1de23b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289774
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110017}

--- sandbox/policy/win/sandbox_win.cc
+++ sandbox/policy/win/sandbox_win.cc
@@ -1019,11 +1019,7 @@ ResultCode SandboxWin::StartSandboxedProcess(
                                 process);
   }
 
-  std::string tag;
-  if (base::FeatureList::IsEnabled(features::kSharedSandboxPolicies))
-    tag = delegate->GetSandboxTag();
-
-  auto policy = g_broker_services->CreatePolicy(tag);
+  auto policy = g_broker_services->CreatePolicy(delegate->GetSandboxTag());
   ResultCode result = GeneratePolicyForSandboxedProcess(
       cmd_line, process_type, handles_to_inherit, delegate, policy.get());
   if (SBOX_ALL_OK != result)
