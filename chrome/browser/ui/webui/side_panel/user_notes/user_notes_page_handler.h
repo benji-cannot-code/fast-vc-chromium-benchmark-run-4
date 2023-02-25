e2f02e2b27c04acbae9775b0f957f016caec1806
Yuheng Huang <yuhengh@chromium.org>
2023-02-25 01:35:54 +0000

    Notes: Display bookmark title in note overview
    When there are bookmarks with the same url, note overview will display title of the bookmark instead of the URL in the title field.
    
    Bug: 1378131
    Change-Id: I80c8a91eae405f8c0f9f60ccd89f4dd727abdbf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265690
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109920}

--- chrome/browser/ui/webui/side_panel/user_notes/user_notes_page_handler.h
+++ chrome/browser/ui/webui/side_panel/user_notes/user_notes_page_handler.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
+namespace bookmarks {
+class BookmarkModel;
+}
+
 namespace power_bookmarks {
 class PowerBookmarkService;
 }
@@ -88,6 +92,7 @@ class UserNotesPageHandler : public side_panel::mojom::UserNotesPageHandler,
   const raw_ptr<Profile> profile_;
   PrefChangeRegistrar pref_change_registrar_;
   const raw_ptr<power_bookmarks::PowerBookmarkService> service_;
+  const raw_ptr<bookmarks::BookmarkModel> bookmark_model_;
   const raw_ptr<Browser> browser_;
   raw_ptr<UserNotesSidePanelUI> user_notes_ui_ = nullptr;
   bool start_creation_after_tab_change_ = false;
