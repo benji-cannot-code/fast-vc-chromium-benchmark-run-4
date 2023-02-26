fcbf3d4f9f6c2d3c12963c3957b782eb7a683c04
sanjeetnd <sanjeetnd@google.com>
2023-02-26 23:54:39 +0000

    [VCManager] Implement request-on-mute on VC Client
    Add functionality on video conference browser client to notify
    manager of an app capturing camera or microphone while the device
    being captured is disabled on the system.
    
    Bug: b/253284704
    Test: "testing/xvfb.py tools/autotest.py -C out_linux_ash/Release --run_all ./chrome/browser/chromeos/video_conference"
    Change-Id: I36ed907b0a3d7d808d88502373e722de5935c6d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4229933
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Sanjeet Dasharath <sanjeetnd@google.com>
    Commit-Queue: Sanjeet Dasharath <sanjeetnd@google.com>
    Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110133}

--- chrome/browser/chromeos/video_conference/video_conference_media_listener.h
+++ chrome/browser/chromeos/video_conference/video_conference_media_listener.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_VIDEO_CONFERENCE_VIDEO_CONFERENCE_MEDIA_LISTENER_H_
 #define CHROME_BROWSER_CHROMEOS_VIDEO_CONFERENCE_VIDEO_CONFERENCE_MEDIA_LISTENER_H_
 
+#include <string>
 #include "base/functional/callback_forward.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
+#include "chromeos/crosapi/mojom/video_conference.mojom-forward.h"
 #include "content/public/browser/web_contents.h"
 
 namespace video_conference {
@@ -26,13 +28,20 @@ class VideoConferenceMediaListener
   VideoConferenceMediaListener(
       base::RepeatingCallback<void()> media_usage_update_callback,
       base::RepeatingCallback<VideoConferenceWebApp*(content::WebContents*)>
-          create_vc_web_app_callback);
+          create_vc_web_app_callback,
+      base::RepeatingCallback<void(crosapi::mojom::VideoConferenceMediaDevice,
+                                   const std::u16string&)>
+          device_used_while_disabled_callback);
 
   VideoConferenceMediaListener(const VideoConferenceMediaListener&) = delete;
   VideoConferenceMediaListener& operator=(const VideoConferenceMediaListener&) =
       delete;
 
   ~VideoConferenceMediaListener() override;
+
+  void SetSystemMediaDeviceStatus(
+      crosapi::mojom::VideoConferenceMediaDevice device,
+      bool disabled);
 
   // MediaStreamCaptureIndicator::Observer overrides
   void OnIsCapturingVideoChanged(content::WebContents* contents,
@@ -45,6 +54,9 @@ class VideoConferenceMediaListener
                                    bool is_capturing_display) override;
 
  private:
+  friend class FakeVideoConferenceManagerClient;
+  friend class FakeVideoConferenceMediaListener;
+
   // Returns the `VideoConferenceWebApp` corresponding to this
   // webcontents. If it doesn't exist, also first creates it if `is_capturing`
   // is true.
@@ -56,9 +68,17 @@ class VideoConferenceMediaListener
   void OnIsCapturingScreenChanged(content::WebContents* contents,
                                   bool is_capturing_screen);
 
+  // The following two fields are true if the camera/microphone is system-wide
+  // software disabled OR disabled via a hardware switch.
+  bool camera_system_disabled_{false};
+  bool microphone_system_disabled_{false};
+
   base::RepeatingCallback<void()> media_usage_update_callback_;
   base::RepeatingCallback<VideoConferenceWebApp*(content::WebContents*)>
       create_vc_web_app_callback_;
+  base::RepeatingCallback<void(crosapi::mojom::VideoConferenceMediaDevice,
+                               const std::u16string&)>
+      device_used_while_disabled_callback_;
 
   base::ScopedObservation<MediaStreamCaptureIndicator,
                           MediaStreamCaptureIndicator::Observer>
