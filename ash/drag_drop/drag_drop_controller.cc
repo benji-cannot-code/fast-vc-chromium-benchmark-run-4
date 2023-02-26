d0ddcd516f4d26eda34bbbfc59087c20f68d832f
andrewxu <andrewxu@chromium.org>
2023-02-26 19:03:22 +0000

    [Drag Drop] Notify observers of drop completion
    This CL adds a new API in `aura::client::DragDropClientObserver`:
    OnDropCompleted(). OnDropCompleted() is called when drop is performed.
    
    This CL should not bring any noticeable UI change.
    
    Bug: b/264934296
    Change-Id: I41fab882898d6d2b29756c4aa24c940774ebcbe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261790
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110109}

--- ash/drag_drop/drag_drop_controller.cc
+++ ash/drag_drop/drag_drop_controller.cc
@@ -629,6 +629,10 @@ void DragDropController::Drop(aura::Window* target,
   e.set_flags(event.flags());
   ui::Event::DispatcherApi(&e).set_target(target);
 
+  for (aura::client::DragDropClientObserver& observer : observers_) {
+    observer.OnDragCompleted(e);
+  }
+
   if (delegate) {
     delegate_drop_cb_animation = delegate->GetDropCallbackWithAnimation(e);
     delegate_drop_cb = delegate->GetDropCallback(e);
@@ -643,7 +647,6 @@ void DragDropController::Drop(aura::Window* target,
   const bool is_tab_drag_drop = (tab_drag_drop_delegate_.get() != nullptr);
 
   DCHECK_EQ(drag_window_, target);
-  aura::WindowTracker window_tracker({drag_window_, drag_window_->parent()});
 
   DropIfAllowed(
       drag_data_.get(), current_drag_info_,
@@ -653,19 +656,6 @@ void DragDropController::Drop(aura::Window* target,
           std::move(delegate_drop_cb), std::move(delegate_drop_cb_animation),
           std::move(tab_drag_drop_delegate_), std::move(drag_cancel)));
 
-  // During the drop, the event target (or its ancestors) might have
-  // been destroyed, eg by the client reaction. Adapt the DropTargetEvent
-  // accordingly.
-  //
-  // TODO(https://crbug.com/1160925): Avoid nested RunLoop in exo
-  // DataDevice::GetDropCallback() - remove the block below when it is fixed.
-  if (!window_tracker.Contains(drag_window_) ||
-      !window_tracker.Contains(drag_window_->parent())) {
-    ui::Event::DispatcherApi(&e).set_target(nullptr);
-  }
-
-  for (aura::client::DragDropClientObserver& observer : observers_)
-    observer.OnDragCompleted(e);
   Cleanup();
 
   // Tab drag-n-drop should never be async.
@@ -707,8 +697,9 @@ void DragDropController::DoDragCancel(
   if (toplevel_window_drag_delegate_)
     toplevel_window_drag_delegate_->OnToplevelWindowDragCancelled();
 
-  for (aura::client::DragDropClientObserver& observer : observers_)
+  for (aura::client::DragDropClientObserver& observer : observers_) {
     observer.OnDragCancelled();
+  }
   Cleanup();
 
   StartCanceledAnimation(drag_cancel_animation_duration);
@@ -827,7 +818,7 @@ void DragDropController::PerformDrop(
     aura::client::DragDropDelegate::DropCallback drop_cb,
     aura::client::DragDropDelegate::DropCallbackWithAnimation drop_cb_animation,
     std::unique_ptr<TabDragDropDelegate> tab_drag_drop_delegate,
-    base::ScopedClosureRunner drag_cancel) {
+    base::ScopedClosureRunner cancel_drag_callback) {
   // Event copy constructor dooesn't copy the target. That's why we set it here.
   // DragDropController observes the `drag_window_`, so if it's destroyed, the
   // target will be set to nullptr.
@@ -861,7 +852,14 @@ void DragDropController::PerformDrop(
   if (toplevel_window_drag_delegate_) {
     operation_ = toplevel_window_drag_delegate_->OnToplevelWindowDragDropped();
   }
-  drag_cancel.ReplaceClosure(base::DoNothing());
+
+  for (aura::client::DragDropClientObserver& observer : observers_) {
+    observer.OnDropCompleted(operation_);
+  }
+
+  // Replace `cancel_drag_callback` with an empty closure. Drop completed, so no
+  // need to cancel the drop.
+  cancel_drag_callback.ReplaceClosure(base::DoNothing());
 }
 
 void DragDropController::CancelIfInProgress() {
