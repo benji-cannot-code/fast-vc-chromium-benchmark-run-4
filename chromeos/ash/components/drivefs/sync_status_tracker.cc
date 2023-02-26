4b33bc5f29469c80425f523a86b6aab3a5094b59
Marcello Salomao <msalomao@google.com>
2023-02-26 23:58:26 +0000

    [Mirror Sync] Add sequence checker to sync status tracker
    Bug: b:253969074
    Test: Manual
    Change-Id: I962e491f36d865b2bb47aadd3919dd98b71d72b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289354
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110134}

--- chromeos/ash/components/drivefs/sync_status_tracker.cc
+++ chromeos/ash/components/drivefs/sync_status_tracker.cc
@@ -47,6 +47,8 @@ SyncStatusTracker::SyncStatusTracker() = default;
 SyncStatusTracker::~SyncStatusTracker() = default;
 
 SyncState SyncStatusTracker::GetSyncState(const base::FilePath path) const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
   if (path.empty() || !path.IsAbsolute()) {
     return SyncState::CreateNotFound(path);
   }
@@ -58,6 +60,8 @@ SyncState SyncStatusTracker::GetSyncState(const base::FilePath path) const {
 }
 
 const std::vector<const SyncState> SyncStatusTracker::GetChangesAndClean() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
   std::vector<const SyncState> updated_sync_states;
 
   // Traverse trie.
@@ -112,6 +116,8 @@ void SyncStatusTracker::SetSyncState(const int64_t id,
                                      const SyncStatus status,
                                      const int64_t transferred,
                                      const int64_t total) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
   if (path.empty() || !path.IsAbsolute()) {
     return;
   }
