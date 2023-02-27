42422db6fb5edf3aa4e05d65686012c25852b236
Weizhong Xia <weizhong@google.com>
2023-02-27 17:04:45 +0000

    Keep test duration in wpt report
    On wpt builders, some shards take as much as twice the time of other
    shards to finish. Run time variation sometimes is more than 3 minutes.
    
    Keep the test duration in wpt report. Such data will be useful for the
    analysis.
    
    Bug: 1394572
    Change-Id: I4072da6de07a33c232af0e1189559277b442e05e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4293414
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110393}

--- third_party/blink/tools/blinkpy/w3c/wpt_results_processor_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/wpt_results_processor_unittest.py
@@ -993,5 +993,7 @@ class WPTResultsProcessorTest(LoggingTestCase):
             }],
             'status':
             'OK',
+            'duration':
+            1000,
             'known_intermittent': ['CRASH'],
         }])
