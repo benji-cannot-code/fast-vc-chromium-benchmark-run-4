7cb640afdfb0e8e7dc00fec13f8307ef320ce1e5
Alexei Svitkine <asvitkine@chromium.org>
2023-02-27 21:56:29 +0000

    Remove some unnecessary breakpad includes.
    Bug: None
    Change-Id: I2cfd9bf0d9555831ec91d15d1c4afaafbae71672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4277325
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110597}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -208,8 +208,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/timezone/timezone_provider.h"
 #include "chromeos/ash/components/timezone/timezone_request.h"
 #include "chromeos/ash/services/rollback_network_config/public/mojom/rollback_network_config.mojom.h"
-#include "components/crash/core/app/breakpad_linux.h"
-#include "components/crash/core/app/crashpad.h"
 #include "components/metrics/structured/neutrino_logging.h"
 #include "components/metrics/structured/neutrino_logging_util.h"
 #include "components/prefs/pref_registry_simple.h"
