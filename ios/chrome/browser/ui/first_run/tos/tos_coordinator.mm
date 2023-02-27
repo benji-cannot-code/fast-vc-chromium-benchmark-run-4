1e2dfe4f68231f6cd44e3b37e73208939f55aeef
Mihai Sardarescu <msarda@chromium.org>
2023-02-27 16:35:43 +0000

    Rename welcome -> tos and cleanup
    The first_run/welcome folder only contained the TOS screens after the
    sign-in focused FRE launch. This CL renames the folder to tos to avoid confusion.
    
    It also cleans up the build file and removes the
    welcome_screen_banner.imageset as it is no longer used.
    
    Bug: None
    Change-Id: I9bf205db90244ad9acec438958cd58417d4c48f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291896
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110370}

--- ios/chrome/browser/ui/first_run/tos/tos_coordinator.mm
+++ ios/chrome/browser/ui/first_run/tos/tos_coordinator.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/first_run/welcome/tos_coordinator.h"
+#import "ios/chrome/browser/ui/first_run/tos/tos_coordinator.h"
 
 #import <WebKit/WebKit.h>
 
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/tos_commands.h"
-#import "ios/chrome/browser/ui/first_run/welcome/tos_view_controller.h"
+#import "ios/chrome/browser/ui/first_run/tos/tos_view_controller.h"
 #import "ios/chrome/browser/ui/util/terms_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/common/web_view_creation_util.h"
--- ios/chrome/browser/ui/first_run/tos/tos_unittest.mm
+++ ios/chrome/browser/ui/first_run/tos/tos_unittest.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/first_run/welcome/tos_coordinator.h"
+#import "ios/chrome/browser/ui/first_run/tos/tos_coordinator.h"
 
 #import <WebKit/WebKit.h>
 #import <gtest/gtest.h>
