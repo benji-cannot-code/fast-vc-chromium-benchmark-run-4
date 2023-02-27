33c960dcfb9a91cbed2f7376a461e07560c77557
Thiemo Nagel <tnagel@chromium.org>
2023-02-27 21:58:52 +0000

    🤠 Disable 10 Autofill tests broken on ubsan
    ContentAutofillDriverBrowserTest.PrerenderNavigationDoesntHideAutofillPopup
    ContentAutofillDriverBrowserTest.SameDocumentNavigationHideAutofillPopup
    ContentAutofillDriverBrowserTest.SubframeNavigationDoesntHideAutofillPopup
    ContentAutofillDriverBrowserTest.SwitchTabAndHideAutofillPopup
    ContentAutofillDriverBrowserTest.TestPageNavigationHidingAutofillPopup
    ContentAutofillDriverPrerenderBrowserTest.PrerenderingDoesNotSubmitForm
    AutofillContextMenuManagerTest.AutofillContextMenuContents
    AutofillContextMenuManagerTest.ExecuteCommand
    AutofillContextMenuManagerTest.RecordContextMenuIsShownOnField
    AutofillContextMenuTest/RenderViewContestMenuAutofillTest.ShowAutofillOptions
    
    Bug: 1419799
    Change-Id: I88317c1669a21b7422911b53d7bccff89d00109d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296533
    Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    Auto-Submit: Thiemo Nagel <tnagel@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Owners-Override: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110599}

--- chrome/browser/autofill/content_autofill_driver_browsertest.cc
+++ chrome/browser/autofill/content_autofill_driver_browsertest.cc
@@ -161,7 +161,7 @@ class ContentAutofillDriverBrowserTest : public InProcessBrowserTest,
 };
 
 IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
-                       SwitchTabAndHideAutofillPopup) {
+                       DISABLED_SwitchTabAndHideAutofillPopup) {
   EXPECT_CALL(autofill_client(),
               HideAutofillPopup(PopupHidingReason::kTabGone));
 
@@ -174,7 +174,7 @@ IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
-                       SameDocumentNavigationHideAutofillPopup) {
+                       DISABLED_SameDocumentNavigationHideAutofillPopup) {
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(),
       embedded_test_server()->GetURL("/autofill/autofill_test_form.html")));
@@ -197,7 +197,7 @@ IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
-                       PrerenderNavigationDoesntHideAutofillPopup) {
+                       DISABLED_PrerenderNavigationDoesntHideAutofillPopup) {
   GURL initial_url =
       embedded_test_server()->GetURL("/autofill/autofill_test_form.html");
   GURL prerender_url = embedded_test_server()->GetURL("/empty.html");
@@ -222,7 +222,7 @@ IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
-                       SubframeNavigationDoesntHideAutofillPopup) {
+                       DISABLED_SubframeNavigationDoesntHideAutofillPopup) {
   // Main frame is on a.com, iframe is on b.com.
   GURL url = embedded_test_server()->GetURL(
       "a.com", "/autofill/cross_origin_iframe.html");
@@ -244,7 +244,7 @@ IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ContentAutofillDriverBrowserTest,
-                       TestPageNavigationHidingAutofillPopup) {
+                       DISABLED_TestPageNavigationHidingAutofillPopup) {
   // HideAutofillPopup is called once for each navigation.
   EXPECT_CALL(autofill_client(),
               HideAutofillPopup(PopupHidingReason::kNavigation))
@@ -276,7 +276,7 @@ class ContentAutofillDriverPrerenderBrowserTest
 };
 
 IN_PROC_BROWSER_TEST_F(ContentAutofillDriverPrerenderBrowserTest,
-                       PrerenderingDoesNotSubmitForm) {
+                       DISABLED_PrerenderingDoesNotSubmitForm) {
   GURL initial_url =
       embedded_test_server()->GetURL("/autofill/autofill_test_form.html");
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), initial_url));
