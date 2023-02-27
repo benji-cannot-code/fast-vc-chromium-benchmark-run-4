a6916354f19a9b9f639f46134da3bebbe4814950
Collin Baker <collinbaker@chromium.org>
2023-02-27 21:38:31 +0000

    Simplify system.rs tests and make them more idiomatic
    Bug: 1274864
    Change-Id: I1d5e6243acf1adb0dcb639e807d081b79ad28d84
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-dbg,android-rust-arm-rel,linux-rust-x64-dbg,linux-rust-x64-rel
    Change-Id: I1d5e6243acf1adb0dcb639e807d081b79ad28d84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289836
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110586}

--- mojo/public/rust/tests/lib.rs
+++ mojo/public/rust/tests/lib.rs
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![feature(assert_matches)]
+#![feature(maybe_uninit_write_slice)]
+
 #[macro_use]
 extern crate mojo;
 
