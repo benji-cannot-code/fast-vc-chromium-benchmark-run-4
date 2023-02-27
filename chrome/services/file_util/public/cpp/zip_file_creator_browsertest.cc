d72ace138c67cf3680cca3961c5a649c63ca66f7
Claudio DeSouza <cdesouza@igalia.com>
2023-02-27 18:52:22 +0000

    Switch to simpler base::WriteFile() variants (56/N)
    This CL is part of a batch of CLs to replace the use of the less
    friendly base::WriteFile call with simpler variants, which are easier to
    read, and less prone to mistakes.
    
    Changes in this particular CL address files under the path
    /chrome/services.
    
    This CL was uploaded by git cl split.
    
    R=thestig@chromium.org, xinghuilu@chromium.org
    
    AX-Relnotes: n/a.
    Bug: 418837
    Change-Id: I57f24d014955f5be816fef63674be6b51387ff3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291993
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Quick-Run: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1110471}

--- chrome/services/file_util/public/cpp/zip_file_creator_browsertest.cc
+++ chrome/services/file_util/public/cpp/zip_file_creator_browsertest.cc
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-bool CreateFile(const base::FilePath& file, const std::string& content) {
-  return base::WriteFile(file, content.c_str(), content.size()) ==
-         static_cast<int>(content.size());
-}
-
 class ZipFileCreatorTest : public InProcessBrowserTest {
  protected:
   void SetUpOnMainThread() override {
@@ -277,7 +272,7 @@ IN_PROC_BROWSER_TEST_F(ZipFileCreatorTest, ZipDirectoryWithManyFiles) {
     for (int i = 1; i < 90; i++) {
       base::FilePath file(std::to_string(i) + ".txt");
       std::string content = "Hello" + std::to_string(i);
-      ASSERT_TRUE(CreateFile(root_dir.Append(file), content));
+      ASSERT_TRUE(base::WriteFile(root_dir.Append(file), content));
       file_tree_content[file] = content;
     }
     for (int i = 1; i <= 10; i++) {
@@ -288,7 +283,7 @@ IN_PROC_BROWSER_TEST_F(ZipFileCreatorTest, ZipDirectoryWithManyFiles) {
         base::FilePath file = dir.Append(std::to_string(j) + ".txt");
         std::string content =
             "Hello" + std::to_string(i) + "/" + std::to_string(j);
-        ASSERT_TRUE(CreateFile(root_dir.Append(file), content));
+        ASSERT_TRUE(base::WriteFile(root_dir.Append(file), content));
         file_tree_content[file] = content;
       }
     }
