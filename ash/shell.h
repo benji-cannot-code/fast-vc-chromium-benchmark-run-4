108450bda1eb3db8be1dfa027fa2671b370388f5
Sean Kau <skau@chromium.org>
2023-02-27 19:47:16 +0000

    Remove include from shell.h
    Forward declare ash/system class since not all dependants on
    //ash include //ash/system
    
    Change-Id: I9230f85696a71b3041dd5d790f810d459c135442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4210111
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110511}

--- ash/shell.h
+++ ash/shell.h
@@ -17,9 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/system_sounds_delegate.h"
-#include "ash/system/input_device_settings/input_device_settings_controller_impl.h"
-#include "ash/system/input_device_settings/input_device_tracker.h"
-#include "ash/system/input_device_settings/keyboard_modifier_metrics_recorder.h"
 #include "ash/wm/system_modal_container_event_filter_delegate.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
@@ -151,11 +148,14 @@ class ColorEnhancementController;
 class HoldingSpaceController;
 class HumanPresenceOrientationController;
 class ImeControllerImpl;
+class InputDeviceSettingsControllerImpl;
+class InputDeviceTracker;
 class WebAuthNDialogControllerImpl;
 class KeyAccessibilityEnabler;
 class KeyboardBacklightColorController;
 class KeyboardBrightnessControlDelegate;
 class KeyboardControllerImpl;
+class KeyboardModifierMetricsRecorder;
 class LaserPointerController;
 class LocaleUpdateControllerImpl;
 class LockStateController;
