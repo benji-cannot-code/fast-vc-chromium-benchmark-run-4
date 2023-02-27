2ea70096764d41d516ac32360bec14ed54f7729b
Roman Arora <romanarora@chromium.org>
2023-02-27 22:17:17 +0000

    [NTP Journeys] Add about flag
    Bug: 1410895
    Change-Id: I88025a26548f7dc19149aac91140eff04af90417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200863
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110606}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2457,6 +2457,9 @@ extern const char kNtpDummyModulesName[];
 extern const char kNtpDummyModulesDescription[];
 #endif
 
+extern const char kNtpHistoryClustersModuleName[];
+extern const char kNtpHistoryClustersModuleDescription[];
+
 extern const char kNtpMiddleSlotPromoDismissalName[];
 extern const char kNtpMiddleSlotPromoDismissalDescription[];
 
