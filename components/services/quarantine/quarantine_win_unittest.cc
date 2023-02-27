cf3cb32771e6ca82d712432e62e4a98e12c8acc6
Claudio DeSouza <cdesouza@igalia.com>
2023-02-27 00:15:12 +0000

    Switch to simpler base::WriteFile() variants (89/N)
    This CL is part of a batch of CLs to replace the use of the less
    friendly base::WriteFile call with simpler variants, which are easier to
    read, and less prone to mistakes.
    
    Changes in this particular CL address files under the path
    /components/services.
    
    This CL was uploaded by git cl split.
    
    R=asully@chromium.org, dominickn@chromium.org, katie@chromium.org, wfh@chromium.org
    
    AX-Relnotes: n/a.
    Bug: 418837
    Change-Id: Ic35436981baf26ba73946ba5d5dd9d510804230e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288934
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110136}

--- components/services/quarantine/quarantine_win_unittest.cc
+++ components/services/quarantine/quarantine_win_unittest.cc
@@ -48,8 +48,7 @@ const char* const kUntrustedURLs[] = {
 bool CreateFile(const base::FilePath& file_path) {
   constexpr char kTestData[] = "Hello world!";
 
-  return base::WriteFile(file_path, kTestData, std::size(kTestData)) ==
-         static_cast<int>(std::size(kTestData));
+  return base::WriteFile(file_path, kTestData);
 }
 
 base::FilePath GetZoneIdentifierStreamPath(const base::FilePath& file_path) {
@@ -122,9 +121,7 @@ ScopedZoneForSite::~ScopedZoneForSite() {
 bool AddInternetZoneIdentifierDirectly(const base::FilePath& file_path) {
   static const char kMotwForInternetZone[] = "[ZoneTransfer]\r\nZoneId=3\r\n";
   return base::WriteFile(GetZoneIdentifierStreamPath(file_path),
-                         kMotwForInternetZone,
-                         std::size(kMotwForInternetZone)) ==
-         static_cast<int>(std::size(kMotwForInternetZone));
+                         kMotwForInternetZone);
 }
 
 void CheckQuarantineResult(QuarantineFileResult result,
@@ -312,7 +309,7 @@ TEST_F(QuarantineWinTest, EmptySource_DependsOnLocalConfig) {
 // it and the test would fail.
 TEST_F(QuarantineWinTest, EmptyFile) {
   base::FilePath test_file = GetTempDir().AppendASCII("foo.exe");
-  ASSERT_EQ(0, base::WriteFile(test_file, "", 0u));
+  ASSERT_TRUE(base::WriteFile(test_file, ""));
 
   QuarantineFile(
       test_file, net::FilePathToFileURL(test_file), GURL(), kDummyClientGuid,
