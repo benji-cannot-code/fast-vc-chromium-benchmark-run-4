98e89fca0ad0d6eb0d35384e5cddd5cc667ca926
David Padlipsky <dpad@google.com>
2023-02-27 20:10:09 +0000

    Settings Split: Clear dictionary settings when flag is disabled
    Bug: b/241965700
    Change-Id: I4b6690846a98dd8826035781dff135132fb944ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245825
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110530}

--- ash/system/input_device_settings/input_device_settings_controller_impl.cc
+++ ash/system/input_device_settings/input_device_settings_controller_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/input_device_settings_controller.h"
 #include "ash/public/mojom/input_device_settings.mojom.h"
 #include "ash/session/session_controller_impl.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/notreached.h"
 #include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
 #include "ui/events/devices/input_device.h"
 
 namespace ash {
@@ -77,9 +79,7 @@ InputDeviceSettingsControllerImpl::InputDeviceSettingsControllerImpl(
 }
 
 void InputDeviceSettingsControllerImpl::Init() {
-  if (features::IsInputDeviceSettingsSplitEnabled()) {
-    Shell::Get()->session_controller()->AddObserver(this);
-  }
+  Shell::Get()->session_controller()->AddObserver(this);
   keyboard_notifier_ =
       std::make_unique<InputDeviceNotifier<mojom::KeyboardPtr>>(
           &keyboards_,
@@ -105,9 +105,7 @@ void InputDeviceSettingsControllerImpl::Init() {
 }
 
 InputDeviceSettingsControllerImpl::~InputDeviceSettingsControllerImpl() {
-  if (features::IsInputDeviceSettingsSplitEnabled()) {
-    Shell::Get()->session_controller()->RemoveObserver(this);
-  }
+  Shell::Get()->session_controller()->RemoveObserver(this);
 }
 
 void InputDeviceSettingsControllerImpl::RegisterProfilePrefs(
@@ -121,6 +119,15 @@ void InputDeviceSettingsControllerImpl::RegisterProfilePrefs(
 
 void InputDeviceSettingsControllerImpl::OnActiveUserPrefServiceChanged(
     PrefService* pref_service) {
+  // If the flag is disabled, clear all the settings dictionaries.
+  if (!features::IsInputDeviceSettingsSplitEnabled()) {
+    active_pref_service_ = nullptr;
+    pref_service->SetDict(prefs::kKeyboardDeviceSettingsDictPref, {});
+    pref_service->SetDict(prefs::kMouseDeviceSettingsDictPref, {});
+    pref_service->SetDict(prefs::kPointingStickDeviceSettingsDictPref, {});
+    pref_service->SetDict(prefs::kTouchpadDeviceSettingsDictPref, {});
+    return;
+  }
   active_pref_service_ = pref_service;
   // TODO(michaelcheco): Initialize settings and notify observers.
 }
