6578e4475906aa1973b1c0a7e9c7916f9a3a254d
Tamir Duberstein <tamird@google.com>
2023-02-27 17:51:51 +0000

    [fuchsia] Restore move constructors
    Previously removed in commit 215051d4065fcabb8f6685ba8dbc3391916654dd.
    
    Requires https://fxrev.dev/808392.
    
    Change-Id: Ib3b88a11607137eeb850c4e07cbe8d75084a92d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4284858
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Tamir Duberstein <tamird@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110429}

--- fuchsia_web/webengine/test/context_provider_for_test.h
+++ fuchsia_web/webengine/test/context_provider_for_test.h
@@ -17,11 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // system log.
 class ContextProviderForTest {
  public:
-  explicit ContextProviderForTest(const base::CommandLine& command_line);
+  static ContextProviderForTest Create(const base::CommandLine& command_line);
 
-  ContextProviderForTest(const ContextProviderForTest&) = delete;
-  ContextProviderForTest& operator=(const ContextProviderForTest&) = delete;
-
+  ContextProviderForTest(ContextProviderForTest&&) noexcept;
+  ContextProviderForTest& operator=(ContextProviderForTest&&) noexcept;
   ~ContextProviderForTest();
 
   ::fuchsia::web::ContextProviderPtr& ptr() { return context_provider_; }
@@ -29,6 +28,9 @@ class ContextProviderForTest {
   ::component_testing::RealmRoot& realm_root() { return realm_root_; }
 
  private:
+  ContextProviderForTest(::component_testing::RealmRoot realm_root,
+                         ::fuchsia::web::ContextProviderPtr context_provider);
+
   ::component_testing::RealmRoot realm_root_;
   ::fuchsia::web::ContextProviderPtr context_provider_;
 };
@@ -37,8 +39,12 @@ class ContextProviderForTest {
 // WebEngine's fuchsia::web::Debug interface.
 class ContextProviderForDebugTest {
  public:
-  explicit ContextProviderForDebugTest(const base::CommandLine& command_line);
+  static ContextProviderForDebugTest Create(
+      const base::CommandLine& command_line);
 
+  ContextProviderForDebugTest(ContextProviderForDebugTest&&) noexcept;
+  ContextProviderForDebugTest& operator=(
+      ContextProviderForDebugTest&&) noexcept;
   ~ContextProviderForDebugTest();
 
   ::fuchsia::web::ContextProviderPtr& ptr() { return context_provider_.ptr(); }
@@ -48,6 +54,8 @@ class ContextProviderForDebugTest {
       ::fidl::InterfaceRequest<::fuchsia::web::Debug> debug_request);
 
  private:
+  explicit ContextProviderForDebugTest(ContextProviderForTest context_provider);
+
   ContextProviderForTest context_provider_;
 };
 
