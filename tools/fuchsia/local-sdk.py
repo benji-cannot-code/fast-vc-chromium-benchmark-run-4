1edc034a9127ba72c04f2ce3c03a248c6b7adede
Tamir Duberstein <tamird@google.com>
2023-02-27 19:38:19 +0000

    [fuchsia] Fix local-sdk.py without arch arg
    Use shutil.copytree rather than hand-rolling an equivalent function.
    
    Provide a copy function that skips copying when the destination exists
    and is identical to the source.
    
    The previous implementation would produce an access denied exception
    when copying over an existing read-only file. This would occur when
    local-sdk.py generated multiple SDKs (e.g. when invoked without its
    optional architecture argument) because certain read-only host tools are
    common to all SDKs.
    
    Change-Id: I13fff4d2b6d0b5b94b3324e06243e42a92e0ce9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4284278
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Chong Gu <chonggu@google.com>
    Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110502}

--- tools/fuchsia/local-sdk.py
+++ tools/fuchsia/local-sdk.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import filecmp
 import hashlib
 import json
 import os
@@ -39,16 +40,10 @@ def BuildForArch(arch):
   Run('scripts/fx', 'build', 'sdk', 'build/images')
 
 
-def _CopyFilesIntoExistingDirectory(src, dst):
-  for entry in os.listdir(src):
-    src_path = os.path.join(src, entry)
-    dst_path = os.path.join(dst, entry)
-    if os.path.isdir(src_path):
-      if not os.path.exists(dst_path):
-        os.mkdir(dst_path)
-      _CopyFilesIntoExistingDirectory(src_path, dst_path)
-    else:
-      shutil.copy(src_path, dst_path)
+def Copy(src, dst):
+  if os.path.exists(dst) and filecmp.cmp(src, dst, shallow=False):
+    return
+  shutil.copy2(src, dst)
 
 
 def main(args):
@@ -100,7 +95,10 @@ def main(args):
       Run('scripts/sdk/gn/generate.py', '--archive', sdk_tar_path, '--output',
           sdk_gn_dir)
 
-      _CopyFilesIntoExistingDirectory(sdk_gn_dir, sdk_output_dir)
+      shutil.copytree(sdk_gn_dir,
+                      sdk_output_dir,
+                      copy_function=Copy,
+                      dirs_exist_ok=True)
 
       # Merge the manifests.
       manifest_path = os.path.join(sdk_output_dir, 'meta', 'manifest.json')
