38ff4744c922ad54df1939907c539839a77c4f90
Claudio DeSouza <cdesouza@igalia.com>
2023-02-27 17:55:47 +0000

    Switch to simpler base::WriteFile() variants (57/N)
    This CL is part of a batch of CLs to replace the use of the less
    friendly base::WriteFile call with simpler variants, which are easier to
    read, and less prone to mistakes.
    
    Changes in this particular CL address files under the path
    /chrome/test.
    
    This CL was uploaded by git cl split.
    
    R=dgozman@chromium.org, dschuff@chromium.org, nechaev@chromium.org
    
    AX-Relnotes: n/a.
    Bug: 418837
    Change-Id: I5d76c54a891eff81eef59c6a66853e840024f0da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290025
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110434}

--- chrome/test/chromedriver/chrome_launcher_unittest.cc
+++ chrome/test/chromedriver/chrome_launcher_unittest.cc
@@ -226,7 +226,7 @@ TEST(DesktopLauncher, ParseDevToolsActivePortFile_Success) {
   char data[] = "12345\nblahblah";
   base::FilePath temp_file =
       temp_dir.GetPath().Append(FILE_PATH_LITERAL("DevToolsActivePort"));
-  ASSERT_TRUE(base::WriteFile(temp_file, data, strlen(data)));
+  ASSERT_TRUE(base::WriteFile(temp_file, data));
   int port;
   ASSERT_TRUE(
       internal::ParseDevToolsActivePortFile(temp_dir.GetPath(), &port).IsOk());
@@ -239,7 +239,7 @@ TEST(DesktopLauncher, ParseDevToolsActivePortFile_NoNewline) {
   char data[] = "12345";
   base::FilePath temp_file =
       temp_dir.GetPath().Append(FILE_PATH_LITERAL("DevToolsActivePort"));
-  ASSERT_TRUE(base::WriteFile(temp_file, data, strlen(data)));
+  ASSERT_TRUE(base::WriteFile(temp_file, data));
   int port = 1111;
   ASSERT_FALSE(
       internal::ParseDevToolsActivePortFile(temp_dir.GetPath(), &port).IsOk());
@@ -252,7 +252,7 @@ TEST(DesktopLauncher, ParseDevToolsActivePortFile_NotNumber) {
   char data[] = "12345asdf\nblahblah";
   base::FilePath temp_file =
       temp_dir.GetPath().Append(FILE_PATH_LITERAL("DevToolsActivePort"));
-  ASSERT_TRUE(base::WriteFile(temp_file, data, strlen(data)));
+  ASSERT_TRUE(base::WriteFile(temp_file, data));
   int port;
   ASSERT_FALSE(
       internal::ParseDevToolsActivePortFile(temp_dir.GetPath(), &port).IsOk());
@@ -275,7 +275,7 @@ TEST(DesktopLauncher, RemoveOldDevToolsActivePortFile_Success) {
   base::FilePath temp_file =
       temp_dir.GetPath().Append(FILE_PATH_LITERAL("DevToolsActivePort"));
   char data[] = "12345asdf\nblahblah";
-  base::WriteFile(temp_file, data, strlen(data));
+  base::WriteFile(temp_file, data);
   ASSERT_TRUE(
       internal::RemoveOldDevToolsActivePortFile(temp_dir.GetPath()).IsOk());
   ASSERT_FALSE(base::PathExists(temp_file));
