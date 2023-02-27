4309c438b00cecdc0db4f9c7e54660d42a7b1c31
Ali Juma <ajuma@chromium.org>
2023-02-27 21:44:32 +0000

    [iOS] Put Web Inspector support behind a feature flag
    This adds a kill switch for Web Inspector support, and also ensures
    that we only try to set the new `inspectable` property of WKWebView
    when the feature is enabled.
    
    Bug: 1418431
    Change-Id: Id011af9a344b31baaf31eb09e5c1d151e7d3d328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296333
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110589}

--- ios/web/web_state/web_view_internal_creation_util.mm
+++ ios/web/web_state/web_view_internal_creation_util.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check_op.h"
 #import "base/strings/sys_string_conversions.h"
+#import "ios/web/common/features.h"
 #import "ios/web/public/web_client.h"
 #import "ios/web/web_state/crw_web_view.h"
 #import "ios/web/web_state/ui/wk_web_view_configuration_provider.h"
@@ -74,7 +75,10 @@ WKWebView* BuildWKWebView(CGRect frame,
   web_view.scrollView.decelerationRate = UIScrollViewDecelerationRateNormal;
 
   if (@available(iOS 16.4, *)) {
-    web_view.inspectable = web::GetWebClient()->EnableWebInspector();
+    if (base::FeatureList::IsEnabled(features::kEnableWebInspector) &&
+        web::GetWebClient()->EnableWebInspector()) {
+      web_view.inspectable = YES;
+    }
   }
 
   return web_view;
