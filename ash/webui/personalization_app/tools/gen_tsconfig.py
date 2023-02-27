04451b124bc7a80a9d3c2718b35d68ba0e2c52e5
Wenbo Jie <wenbojie@chromium.org>
2023-02-27 23:13:07 +0000

    Support allowJs when generating tsconfig file
    A recent CL:4258545 has removed `allowJs: true` from all
    tsconfig_base.json and add the `allow_js` flag to the ts_library,
    in order to make sure the generated tsconfig file also has the
    correct allowJs value, we need to capture this in the script.
    
    Bug: b:269964889
    Change-Id: I26fbdebcc58cf809a7dc40f2bb6848beb34f9919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4270030
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110632}

--- ash/webui/personalization_app/tools/gen_tsconfig.py
+++ ash/webui/personalization_app/tools/gen_tsconfig.py
@@ -79,6 +79,7 @@ def main(args):
         'extends': normalize_path(out_json_dir, out_json['extends']),
         'compilerOptions': {
             'baseUrl': '.',
+            'allowJs': out_json['compilerOptions']['allowJs'],
             'rootDirs': [
                 '.',
                 normalize_path(out_json_dir,
