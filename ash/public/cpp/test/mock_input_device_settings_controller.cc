d228d63dc7b7790295b6dc732e6be13cf7456ad0
David Padlipsky <dpad@google.com>
2023-02-27 22:46:52 +0000

    Settings Split: Add mock input device settings controller
    Bug: b/241965700
    Change-Id: I58bbf80355d5b1f8f3ab010f93ad36316bca68f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288572
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110616}

---
+++ ash/public/cpp/test/mock_input_device_settings_controller.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/test/mock_input_device_settings_controller.h"
+
+namespace ash {
+
+MockInputDeviceSettingsController::MockInputDeviceSettingsController() =
+    default;
+
+MockInputDeviceSettingsController::~MockInputDeviceSettingsController() =
+    default;
+
+}  // namespace ash
