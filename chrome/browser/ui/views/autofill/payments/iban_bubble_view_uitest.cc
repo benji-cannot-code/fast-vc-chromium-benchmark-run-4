918df134d8ef136a404abbb42dd9a8f7a61dbda9
Jan Keitel <jkeitel@google.com>
2023-02-27 13:30:07 +0000

    Avoid dangling pointers in IbanBubbleViewFullFormBrowserTest.
    Bug: 1419832
    Change-Id: I4f30760f6557e81c6a851056a3d21ec36890167a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294073
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110299}

--- chrome/browser/ui/views/autofill/payments/iban_bubble_view_uitest.cc
+++ chrome/browser/ui/views/autofill/payments/iban_bubble_view_uitest.cc
@@ -122,6 +122,13 @@ class IbanBubbleViewFullFormBrowserTest
     AddEventObserverToController();
   }
 
+  void TearDownOnMainThread() override {
+    // Explicitly set to null to avoid that it becomes dangling.
+    iban_save_manager_ = nullptr;
+
+    SyncTest::TearDownOnMainThread();
+  }
+
   // The primary main frame's AutofillManager.
   TestAutofillManager* autofill_manager() {
     return autofill_manager_injector_[GetActiveWebContents()];
@@ -369,7 +376,7 @@ class IbanBubbleViewFullFormBrowserTest
 
   void WaitForObservedEvent() { event_waiter_->Wait(); }
 
-  raw_ptr<IBANSaveManager, DanglingUntriaged> iban_save_manager_ = nullptr;
+  raw_ptr<IBANSaveManager> iban_save_manager_ = nullptr;
 
  private:
   AutofillBubbleBase* GetIbanBubbleView() {
