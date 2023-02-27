5425f1d0043e12c8670e47cd98f495f4bf114e88
Kenichi Ishibashi <bashi@chromium.org>
2023-02-27 05:14:46 +0000

    Replace base::LazyInstance with base::NoDestructor in host_resolver.cc
    To reconcile with the static initializer check.
    
    Bug: 1417863
    Change-Id: I7e2604976b2aa98ac4c025e33c7f831fe8980b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288851
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110179}

--- services/network/host_resolver.cc
+++ services/network/host_resolver.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/bind.h"
-#include "base/lazy_instance.h"
+#include "base/no_destructor.h"
 #include "base/task/sequenced_task_runner.h"
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/enum_traits.h"
@@ -34,11 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace network {
 namespace {
-static base::LazyInstance<HostResolver::ResolveHostCallback>::Leaky
-    resolve_host_callback;
+
+HostResolver::ResolveHostCallback& GetResolveHostCallback() {
+  static base::NoDestructor<HostResolver::ResolveHostCallback> callback;
+  return *callback;
 }
 
-namespace {
 absl::optional<net::HostResolver::ResolveHostParameters>
 ConvertOptionalParameters(
     const mojom::ResolveHostParametersPtr& mojo_parameters) {
@@ -111,10 +112,11 @@ void HostResolver::ResolveHost(
          optional_parameters->source != net::HostResolverSource::MULTICAST_DNS);
 #endif  // !BUILDFLAG(ENABLE_MDNS)
 
-  if (resolve_host_callback.Get())
-    resolve_host_callback.Get().Run(host->is_host_port_pair()
-                                        ? host->get_host_port_pair().host()
-                                        : host->get_scheme_host_port().host());
+  if (!GetResolveHostCallback().is_null()) {
+    GetResolveHostCallback().Run(host->is_host_port_pair()
+                                     ? host->get_host_port_pair().host()
+                                     : host->get_scheme_host_port().host());
+  }
 
   auto request = std::make_unique<ResolveHostRequest>(
       internal_resolver_, std::move(host), network_anonymization_key,
@@ -172,7 +174,7 @@ size_t HostResolver::GetNumOutstandingRequestsForTesting() const {
 
 void HostResolver::SetResolveHostCallbackForTesting(
     ResolveHostCallback callback) {
-  resolve_host_callback.Get() = std::move(callback);
+  GetResolveHostCallback() = std::move(callback);
 }
 
 void HostResolver::AsyncSetUp() {
