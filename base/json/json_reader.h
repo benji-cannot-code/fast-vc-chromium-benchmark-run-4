f69b191dcc62f82e2ae7c2692cef1c494d807ea7
Claudio DeSouza <cdesouza@igalia.com>
2023-02-27 17:42:24 +0000

    Remove base::JSONReader::ReadDeprecated
    All uses of ReadDeprecated have been removed from the codebase.
    
    Bug: 925165, 1339314
    Change-Id: Ifcd2cb13d04ef40adfe9a966c02c4b2beb10fba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289789
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1110417}

--- base/json/json_reader.h
+++ base/json/json_reader.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_JSON_JSON_READER_H_
 #define BASE_JSON_JSON_READER_H_
 
-#include <memory>
 #include <string>
 
 #include "base/base_export.h"
@@ -104,16 +103,6 @@ class BASE_EXPORT JSONReader {
   // Reads and parses |json|, returning a Value.
   // If |json| is not a properly formed JSON string, returns absl::nullopt.
   static absl::optional<Value> Read(
-      StringPiece json,
-      int options = JSON_PARSE_CHROMIUM_EXTENSIONS,
-      size_t max_depth = internal::kAbsoluteMaxDepth);
-
-  // Deprecated. Use the Read() method above.
-  // Reads and parses |json|, returning a Value.
-  // If |json| is not a properly formed JSON string, returns nullptr.
-  // Wrap this in base::FooValue::From() to check the Value is of type Foo and
-  // convert to a FooValue at the same time.
-  static std::unique_ptr<Value> ReadDeprecated(
       StringPiece json,
       int options = JSON_PARSE_CHROMIUM_EXTENSIONS,
       size_t max_depth = internal::kAbsoluteMaxDepth);
