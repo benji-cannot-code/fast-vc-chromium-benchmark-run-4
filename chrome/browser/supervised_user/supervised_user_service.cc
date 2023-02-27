bc47524b95a1a298425e0efd513b328b3fa9f8b3
anthie@google.com <anthie@google.com>
2023-02-27 11:19:39 +0000

    Remove unused dependencies from Supervised User Service
    Bug: b/265758915
    Change-Id: I6b49cc52c23571f96359730f2ef4f9f83cb36344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291893
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110252}

--- chrome/browser/supervised_user/supervised_user_service.cc
+++ chrome/browser/supervised_user/supervised_user_service.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/supervised_user/core/common/supervised_user_constants.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_user_settings.h"
-#include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
 #include "extensions/buildflags/buildflags.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
