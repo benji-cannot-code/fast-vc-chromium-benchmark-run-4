9c52f7dfe4fc79ee9c35d0f96595d8cad4f260e1
Jacob Francis <francisjp@google.com>
2023-02-27 19:40:34 +0000

    Converted select_to_speak_main to typescript
    AX-RelNotes: N/A
    Bug: b/270623046
    Change-Id: Iab8957ff4514a248ef906aede47e6686eeac4194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289348
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110504}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_main.ts
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_main.ts
@@ -7,7 +7,7 @@ import {InstanceChecker} from '../common/instance_checker.js';
 
 import {SelectToSpeak} from './select_to_speak.js';
 
-export let selectToSpeak;
+export let selectToSpeak: SelectToSpeak;
 
 if (InstanceChecker.isActiveInstance()) {
   selectToSpeak = new SelectToSpeak();
