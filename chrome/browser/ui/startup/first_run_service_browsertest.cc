a56ed2eda41dc48a6580facb5248592794de80a8
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-02-27 16:25:58 +0000

    [fyfre] Improve the name of the session-tagging feature
    ForYouFreStudy => ForYouFreSyntheticTrialRegistration
    
    The new name aims to be better at differentiating from the
    ForYouFre feature and conveying its purpose
    
    Bug: 1402712
    Change-Id: If03f1cd025f73b9f920c6e5926e1c04ae6dd339d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294733
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110362}

--- chrome/browser/ui/startup/first_run_service_browsertest.cc
+++ chrome/browser/ui/startup/first_run_service_browsertest.cc
@@ -409,7 +409,8 @@ class FirstRunServiceCohortBrowserTest : public FirstRunServiceBrowserTest {
 
     scoped_feature_list_.InitWithFeaturesAndParameters(
         {
-            {kForYouFreStudy, {{"group_name", kStudyTestGroupName1}}},
+            {kForYouFreSyntheticTrialRegistration,
+             {{"group_name", kStudyTestGroupName1}}},
             {kForYouFre, {}},
         },
         {});
