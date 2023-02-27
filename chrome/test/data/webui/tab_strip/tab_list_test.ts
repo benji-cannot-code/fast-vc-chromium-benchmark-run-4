babec2d90f047ad4c2ee4d8d783926e5fceff486
Yuheng Huang <yuhengh@chromium.org>
2023-02-27 19:13:43 +0000

    WebUI tab strip: Remove slide in animation
    This CL removes the slide in animation which causes the jittering.
    There are reports on certain ChromeOS devices causing jittering when WebUI tab strip is open right after switching to tablet mode. Since the WebUI tab strip closes itself when new tab is created, the slide in animation would not be quite visible to users anyways.
    
    Bug: 1419587
    Change-Id: If9e3f174dc2b84ae63690ad410a78e6df4e4a161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4276602
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110483}

--- chrome/test/data/webui/tab_strip/tab_list_test.ts
+++ chrome/test/data/webui/tab_strip/tab_list_test.ts
@@ -307,6 +307,7 @@ suite('TabList', () => {
 
   test('PlacePinnedTabElementAnimatesTabsWithinSameColumn', async () => {
     tabs.forEach(pinTabAt);
+    await flushTasks();
     await tabList.animationPromises;
 
     // Test moving a tab within the same column. If a tab is moved from index 0
