ba35e16f1db97c91cdb2a695bbd1e16dd4d66552
Alexei Svitkine <asvitkine@chromium.org>
2023-02-27 21:21:43 +0000

    Add histograms to determine if we can delete field trial fallback code.
    Bug: 867558
    Change-Id: I9a21ab216b5124f6aceea00c65d1f8c176927998
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294537
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110570}

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/metrics/field_trial_param_associator.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
@@ -643,6 +644,9 @@ void FieldTrialList::GetInitiallyActiveFieldTrials(
   DCHECK(global_->create_trials_from_command_line_called_);
 
   if (!global_->field_trial_allocator_) {
+    UmaHistogramBoolean(
+        "ChildProcess.FieldTrials.GetInitiallyActiveFieldTrials.FromString",
+        true);
     GetActiveFieldTrialGroupsFromString(
         command_line.GetSwitchValueASCII(switches::kForceFieldTrials),
         active_groups);
@@ -739,6 +743,8 @@ void FieldTrialList::PopulateLaunchOptionsWithFieldTrialState(
 
   // If the readonly handle did not get created, fall back to flags.
   if (!global_ || !global_->readonly_allocator_region_.IsValid()) {
+    UmaHistogramBoolean(
+        "ChildProcess.FieldTrials.PopulateLaunchOptions.CommandLine", true);
     AddFeatureAndFieldTrialFlags(command_line);
     return;
   }
