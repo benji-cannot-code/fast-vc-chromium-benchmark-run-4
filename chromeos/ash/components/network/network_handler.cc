cadf088e724548a546c048fbad1bdb4e6d6ca6a0
Jason Zhang <jiajunz@google.com>
2023-02-28 21:50:50 +0000

    Reland "[CrOS Hotspot] Add hotspot allow status metrics"
    This relands commit 83e1b196050843f8f919d2a3d681780ad5ca094e.
    
    Reason for revert: Suspected culprit for failing `HotspotMetricsHelperTest.HotspotAllowStatusHistogram` on Linux bots starting from https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/49470/overview
    
    This is because hotspot_capabilities_provider_ got reset() before
    hotspot_metrics_helper_ get reset() in the unit tests
    
    Original change's description:
    > [CrOS Hotspot] Add hotspot allow status metrics
    >
    > This CL adds the HotspotMetricsHelper which used to track hotspot status
    > updates and emit to the corresponding UMA metrics. This CL specifically
    > adds the following histogram:
    > 1. Network.Ash.Hotspot.Upstream.Cellular.Capability.AllowStatus
    > 2. Network.Ash.Hotspot.Upstream.Cellular.Capability.AllowStatusAtLogin
    >
    > Bug: b/269356222, b/239477916
    > Change-Id: I5c2d2fe5d0a105afd85f13096102996e266ca264
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265596
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Commit-Queue: Jason Zhang <jiajunz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1109340}
    
    Change-Id: If7b02fe9b874f5db8ac41c8c4f7f50cf661e0362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296058
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111183}

--- chromeos/ash/components/network/network_handler.cc
+++ chromeos/ash/components/network/network_handler.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/network/metrics/connection_info_metrics_logger.h"
 #include "chromeos/ash/components/network/metrics/esim_policy_login_metrics_logger.h"
 #include "chromeos/ash/components/network/metrics/hidden_network_metrics_helper.h"
+#include "chromeos/ash/components/network/metrics/hotspot_metrics_helper.h"
 #include "chromeos/ash/components/network/metrics/vpn_network_metrics_helper.h"
 #include "chromeos/ash/components/network/network_activation_handler_impl.h"
 #include "chromeos/ash/components/network/network_cert_loader.h"
@@ -82,6 +83,7 @@ NetworkHandler::NetworkHandler()
     hotspot_capabilities_provider_.reset(new HotspotCapabilitiesProvider());
     hotspot_state_handler_.reset(new HotspotStateHandler());
     hotspot_controller_.reset(new HotspotController());
+    hotspot_metrics_helper_.reset(new HotspotMetricsHelper());
   }
   if (NetworkCertLoader::IsInitialized()) {
     network_cert_migrator_.reset(new NetworkCertMigrator());
@@ -150,6 +152,7 @@ void NetworkHandler::Init() {
     hotspot_controller_->Init(hotspot_capabilities_provider_.get(),
                               hotspot_state_handler_.get(),
                               technology_state_controller_.get());
+    hotspot_metrics_helper_->Init(hotspot_capabilities_provider_.get());
   }
   managed_cellular_pref_handler_->Init(network_state_handler_.get());
   esim_policy_login_metrics_logger_->Init(
