8f8a6ce55eefae0b6b88633b0eab42be9323f080
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-02-28 01:38:46 +0000

    Revert "Settings Split: Implement updating of keyboard settings"
    This reverts commit 96063fadcd32a49228b1f7afd24cc54cf5053abd.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8787957583041961553
    
    Sample failed build: https://ci.chromium.org/b/8787957583041961553
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8787957583041961553&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4294121
    
    Original change's description:
    > Settings Split: Implement updating of keyboard settings
    >
    > Bug: b/241965700
    > Change-Id: I6ef3f7e38841d3c2a10bf10542674f335c02ce88
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294121
    > Commit-Queue: Michael Checo <michaelcheco@google.com>
    > Reviewed-by: David Padlipsky <dpad@google.com>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1110704}
    >
    
    Bug: b/241965700
    Change-Id: I1f898315f3a3f2f3afd86fe4556d76df2392c55e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295885
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1110706}

--- ash/system/input_device_settings/input_device_settings_controller_unittest.cc
+++ ash/system/input_device_settings/input_device_settings_controller_unittest.cc
@@ -45,21 +45,14 @@ class FakeKeyboardPrefHandler : public KeyboardPrefHandler {
     num_keyboard_settings_initialized_++;
   }
   void UpdateKeyboardSettings(PrefService* pref_service,
-                              const mojom::Keyboard& keyboard) override {
-    num_keyboard_settings_updated_++;
-  }
+                              const mojom::Keyboard& keyboard) override {}
 
   uint32_t num_keyboard_settings_initialized() {
     return num_keyboard_settings_initialized_;
   }
 
-  uint32_t num_keyboard_settings_updated() {
-    return num_keyboard_settings_updated_;
-  }
-
  private:
   uint32_t num_keyboard_settings_initialized_ = 0;
-  uint32_t num_keyboard_settings_updated_ = 0;
 };
 
 class FakeInputDeviceSettingsControllerObserver
@@ -73,18 +66,10 @@ class FakeInputDeviceSettingsControllerObserver
     num_keyboards_connected_--;
   }
 
-  void OnKeyboardSettingsUpdated(const mojom::Keyboard& keyboard) override {
-    num_keyboards_settings_updated_++;
-  }
-
   uint32_t num_keyboards_connected() { return num_keyboards_connected_; }
-  uint32_t num_keyboards_settings_updated() {
-    return num_keyboards_settings_updated_;
-  }
 
  private:
   uint32_t num_keyboards_connected_;
-  uint32_t num_keyboards_settings_updated_;
 };
 
 class InputDeviceSettingsControllerTest : public AshTestBase {
@@ -224,18 +209,6 @@ TEST_F(InputDeviceSettingsControllerTest, DeletesPrefsWhenFlagDisabled) {
                 prefs::kPointingStickDeviceSettingsDictPref));
   EXPECT_EQ(base::Value::Dict(), active_pref_service->GetDict(
                                      prefs::kTouchpadDeviceSettingsDictPref));
-}
-
-TEST_F(InputDeviceSettingsControllerTest, KeyboardSettingsUpdated) {
-  controller()->OnKeyboardListUpdated({kSampleKeyboardUsb}, {});
-
-  EXPECT_EQ(observer_->num_keyboards_connected(), 1u);
-  EXPECT_EQ(keyboard_pref_handler_->num_keyboard_settings_initialized(), 1u);
-  controller()->SetKeyboardSettings((DeviceId)kSampleKeyboardUsb.id,
-                                    mojom::KeyboardSettings::New());
-
-  EXPECT_EQ(observer_->num_keyboards_settings_updated(), 1u);
-  EXPECT_EQ(keyboard_pref_handler_->num_keyboard_settings_updated(), 1u);
 }
 
 }  // namespace ash
