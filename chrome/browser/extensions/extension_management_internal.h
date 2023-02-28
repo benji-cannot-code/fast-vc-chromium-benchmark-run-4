71a2539bc23a2f02ff0e0087e5367918a12f881c
Anunoy Ghosh <anunoy@chromium.org>
2023-02-28 23:55:56 +0000

    Add check function for ExtensionUnpublishedAvailability policy
    Add a check function in ExtensionManagement that will be used by
    StandardManagementPolicyProvider to enforce the policy. The actual
    invocation of this check in the provider's MustRemainDisabled method
    is deferred to a separate CL.
    The policy check function uses a new keyed service to retrieve
    current publish status of an extension. The implementation of
    this service is also deferred to a separate CL.
    See https://crrev.com/c/4188995 for the policy definition.
    
    Bug: 1406308
    Change-Id: I2741cf9b26f97615178360372b94606c34a8dd19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4233080
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111268}

--- chrome/browser/extensions/extension_management_internal.h
+++ chrome/browser/extensions/extension_management_internal.h
@@ -159,6 +159,12 @@ struct GlobalSettings {
     kEnabled,
     kEnabledForForceInstalled,
   };
+
+  enum class UnpublishedAvailability {
+    kAllowUnpublished = 0,
+    kDisableUnpublished = 1,
+  };
+
   GlobalSettings();
 
   GlobalSettings(const GlobalSettings&) = delete;
@@ -178,6 +184,9 @@ struct GlobalSettings {
 
   // An enum setting indicates if manifest v2 is allowed.
   ManifestV2Setting manifest_v2_setting = ManifestV2Setting::kDefault;
+
+  UnpublishedAvailability unpublished_availability_setting =
+      UnpublishedAvailability::kAllowUnpublished;
 };
 
 }  // namespace internal
