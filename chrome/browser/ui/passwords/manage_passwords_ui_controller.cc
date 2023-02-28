7a034fec396f145015f230d74e504f4fe4c27294
Mohamed Amir Yosef <mamir@chromium.org>
2023-02-28 15:02:17 +0000

    [Passwords] RequestAuthenticationAndReopenBubble doesn't reopen anymore
    Fix the name of the method since the behavior has changed in
    https://crrev.com/c/4272454 and the method doesn't reopen the bubble
    anymore.
    
    This a mechanical change.
    
    Bug: 1371026
    Change-Id: I0f680ddee333883a2d74b9051a59566a2d61259a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295428
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110951}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller.cc
@@ -726,9 +726,8 @@ void ManagePasswordsUIController::AuthenticateUser(
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
-      base::BindOnce(
-          &ManagePasswordsUIController::RequestAuthenticationAndReopenBubble,
-          weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+      base::BindOnce(&ManagePasswordsUIController::RequestAuthentication,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 #else
   std::move(callback).Run(true);
 #endif
@@ -895,10 +894,8 @@ void ManagePasswordsUIController::WebContentsDestroyed() {
   HidePasswordBubble();
 }
 
-void ManagePasswordsUIController::RequestAuthenticationAndReopenBubble(
+void ManagePasswordsUIController::RequestAuthentication(
     AvailabilityCallback callback) {
-  base::WeakPtr<ManagePasswordsUIController> weak_ptr =
-      weak_ptr_factory_.GetWeakPtr();
   bool auth_is_successful = ShowAuthenticationDialog();
   std::move(callback).Run(auth_is_successful);
 }
