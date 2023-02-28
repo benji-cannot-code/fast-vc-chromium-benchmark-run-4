627f47c543224f683440aa11547c2749bd982cdf
Andre Le <leandre@chromium.org>
2023-02-28 20:23:08 +0000

    VC UI: Implement stop screen sharing for the VC panel
    Modify ScreenSecurityController so that it can be used within
    VideoConferenceTray to perform stop screen sharing.
    
    Fixed: b:253277644
    Change-Id: I2f14e9bef3fe339b4e6eb5e4d73586ac4dd8de43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296474
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111131}

--- ash/system/video_conference/video_conference_tray.cc
+++ ash/system/video_conference/video_conference_tray.cc
@@ -10,9 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shelf/shelf.h"
+#include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/icon_button.h"
+#include "ash/system/privacy/screen_security_controller.h"
+#include "ash/system/system_notification_controller.h"
 #include "ash/system/tray/tray_background_view.h"
 #include "ash/system/tray/tray_bubble_wrapper.h"
 #include "ash/system/tray/tray_constants.h"
@@ -328,7 +331,10 @@ void VideoConferenceTray::OnAudioButtonClicked(const ui::Event& event) {
 }
 
 void VideoConferenceTray::OnScreenShareButtonClicked(const ui::Event& event) {
-  // TODO(b/253277644): Implement the callback for `screen_share_icon_`.
+  Shell::Get()
+      ->system_notification_controller()
+      ->screen_security_controller()
+      ->StopAllSessions(/*is_screen_access=*/true);
 }
 
 BEGIN_METADATA(VideoConferenceTray, TrayBackgroundView)
