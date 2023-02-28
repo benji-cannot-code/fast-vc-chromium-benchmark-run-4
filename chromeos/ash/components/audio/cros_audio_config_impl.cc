3fa128b3bfec90b9226a373baf0b81985fdef1b8
Gavin Williams <gavinwill@chromium.org>
2023-02-28 17:41:53 +0000

    audio-settings: Record noise cancellation enabled
    Records when the user enables/disables noise cancellation from the Audio
    Settings page.
    
    Bug: b:260277007
    Change-Id: I3f1783a6adc03f141182de060994b6a14702f6f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258067
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111040}

--- chromeos/ash/components/audio/cros_audio_config_impl.cc
+++ chromeos/ash/components/audio/cros_audio_config_impl.cc
@@ -22,6 +22,8 @@ constexpr char kOutputMuteChangeHistogramName[] =
     "ChromeOS.CrosAudioConfig.OutputMuteStateChange";
 constexpr char kInputMuteChangeHistogramName[] =
     "ChromeOS.CrosAudioConfig.InputMuteStateChange";
+constexpr char kNoiseCancellationEnabledHistogramName[] =
+    "ChromeOS.CrosAudioConfig.NoiseCancellationEnabled";
 
 // Creates an inactive input device with default property configuration.
 AudioDevice CreateStubInternalMic() {
@@ -293,6 +295,7 @@ void CrosAudioConfigImpl::SetNoiseCancellationEnabled(bool enabled) {
   }
 
   audio_handler->SetNoiseCancellationState(enabled);
+  base::UmaHistogramBoolean(kNoiseCancellationEnabledHistogramName, enabled);
 }
 
 void CrosAudioConfigImpl::OnOutputNodeVolumeChanged(uint64_t node_id,
