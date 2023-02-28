7276912146f894b57922ebb6f92a54f420097c27
dpapad <dpapad@chromium.org>
2023-02-28 05:14:13 +0000

    CrOS WebUI: Migrate cros_hotspot_config.mojom-webui.js to TypeScript.
    This is done as a preparatory step for introducing a new
    ash/webui/resources:build_ts ts_library() target, which is easier if no
    Mojo JS files are involved.
    
    Bug: 1002798
    Change-Id: I66c436f485a3f496b28d00d09c38aac6e0501bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4292454
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110764}

--- chrome/test/data/webui/settings/chromeos/hotspot_config_dialog_tests.js
+++ chrome/test/data/webui/settings/chromeos/hotspot_config_dialog_tests.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {Router, routes} from 'chrome://os-settings/chromeos/os_settings.js';
 import {setHotspotConfigForTesting} from 'chrome://resources/ash/common/hotspot/cros_hotspot_config.js';
 import {FakeHotspotConfig} from 'chrome://resources/ash/common/hotspot/fake_hotspot_config.js';
-import {CrosHotspotConfigInterface, HotspotAllowStatus, HotspotConfig, HotspotInfo, HotspotState, SetHotspotConfigResult, WiFiBand, WiFiSecurityMode} from 'chrome://resources/mojo/chromeos/ash/services/hotspot_config/public/mojom/cros_hotspot_config.mojom-webui.js';
+import {HotspotAllowStatus, HotspotState, SetHotspotConfigResult, WiFiBand, WiFiSecurityMode} from 'chrome://resources/mojo/chromeos/ash/services/hotspot_config/public/mojom/cros_hotspot_config.mojom-webui.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
