beb69f636f9210b7b1d8a3bd6a2b21234ecd60d8
Antonio Gomes <tonikitoo@igalia.com>
2023-02-28 17:43:00 +0000

    Use font_list_mac.mm for ios/blink
    BUG=1413552
    R=avi@chromium.org
    
    Change-Id: I0217e3f721b4f075beaa820dcd035b6a6689b5fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297989
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111042}

--- content/common/font_list_mac.mm
+++ content/common/font_list_mac.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/font_list.h"
 
-#import <Cocoa/Cocoa.h>
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreText/CoreText.h>
 
