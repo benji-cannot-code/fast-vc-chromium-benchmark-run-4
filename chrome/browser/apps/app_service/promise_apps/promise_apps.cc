faadb818fd59b40ce0025d885d907e08a44d961e
Vivian Pao <vpao@google.com>
2023-02-28 08:09:20 +0000

    [Promise Icons] Create PromiseAppUpdate to deal with promise app deltas
    Replace AddPromiseApp and UpdatePromiseAppProgress with OnPromiseApp
    which uses a new class PromiseAppUpdate to apply deltas to existing
    promise apps or create new ones.
    
    Bug: b/261907233
    Change-Id: Iaa1ca58bc5581b8ca7693cd47f23b5871fe0dd3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4293196
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110811}

---
+++ chrome/browser/apps/app_service/promise_apps/promise_apps.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/apps/app_service/promise_apps/promise_apps.h"
+
+namespace apps {
+
+PromiseAppPtr PromiseApp::Clone() const {
+  auto app = std::make_unique<PromiseApp>(package_id);
+  if (progress.has_value()) {
+    app->progress = progress;
+  }
+  return app;
+}
+
+}  // namespace apps
