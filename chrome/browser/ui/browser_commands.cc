cf98d0fe2f9e18446171cfc3552d62d807ea7aec
dljames <dljames@google.com>
2023-02-28 23:07:39 +0000

    Fix bug "Move Group To New Window" causing saved groups to unsave
    This fixes a bug where clicking the "Move Group To New Window" button
    in the TabGroupEditorBubbleView would cause a saved group to unsave.
    
    Change-Id: Iecf01b9e9922f8945c4b134bba75ed0387c4c32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289665
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111235}

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -83,6 +83,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/status_bubble.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "chrome/browser/ui/tab_dialogs.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_keyed_service.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_service_factory.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_group.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
@@ -946,6 +948,22 @@ void MoveTabsToNewWindow(Browser* browser,
       Browser::Create(Browser::CreateParams(browser->profile(), true));
 
   if (group.has_value()) {
+    SavedTabGroupKeyedService* const service =
+        SavedTabGroupServiceFactory::GetForProfile(browser->profile());
+    if (service && service->model()->Contains(group.value())) {
+      // If the group we are looking to move is saved:
+      // 1) Stop listening to changes on it
+      // 2) Close the group in the browser
+      // 3) Open the group in a new browser and link it to the saved guid.
+      const base::GUID& saved_guid =
+          service->model()->Get(group.value())->saved_guid();
+
+      service->DisconnectLocalTabGroup(group.value());
+      browser->tab_strip_model()->CloseAllTabsInGroup(group.value());
+      service->OpenSavedTabGroupInBrowser(new_browser, saved_guid);
+      return;
+    }
+
     const tab_groups::TabGroupVisualData* old_visual_data =
         browser->tab_strip_model()
             ->group_model()
