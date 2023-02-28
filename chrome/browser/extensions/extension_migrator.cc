a0ab96a994bd3f116f43d6451a785fa61496ec64
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2023-02-28 22:13:03 +0000

    ExtensionMigrator: fix extensions stuck as force-installed
    The Google Docs Offline extension is kept installed via
    ExtensionMigrator. This is fine in most cases, but if the extension is
    ever force-installed, it won't auto-uninstalled when removed from the
    force-list... and the user can't remove it either, because it's in
    ManifestLocation::kExternalPolicyDownload.
    
    Tweak ExtensionMigrator, so it doesn't preserve the extension if it's
    force-installed. Removing it from ExtensionInstallForcelist now
    auto-uninstalls the extension (just like any other extension).
    
    TESTED=manually: force-installed the extension, removed it from the
           force-list, and checked that the extension was uninstalled
    
    Bug: 1416682
    Change-Id: Ic1538fc0c256ae60be1251e99e930c583e17e11c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294541
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111200}

--- chrome/browser/extensions/extension_migrator.cc
+++ chrome/browser/extensions/extension_migrator.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension_urls.h"
+#include "extensions/common/manifest.h"
 
 namespace extensions {
 
@@ -40,7 +41,13 @@ void ExtensionMigrator::StartLoading() {
 }
 
 bool ExtensionMigrator::IsAppPresent(const std::string& app_id) {
-  return !!ExtensionRegistry::Get(profile_)->GetInstalledExtension(app_id);
+  const Extension* extension =
+      ExtensionRegistry::Get(profile_)->GetInstalledExtension(app_id);
+
+  // If the extension was previously force-installed by policy, don't migrate it
+  // or keep it installed. This prevents the extension getting stuck in a
+  // "non-uninstallable" state. crbug.com/1416682
+  return extension && !Manifest::IsPolicyLocation(extension->location());
 }
 
 }  // namespace extensions
