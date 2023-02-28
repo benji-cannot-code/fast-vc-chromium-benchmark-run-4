4be664d4eac7361a0a19d6e0249232410d1ecb9f
Sophie Chang <sophiechang@chromium.org>
2023-02-28 00:53:09 +0000

    Add a feature that just records metrics for History Clusters module without rendering
    Bug: 1418730,b/265301309
    Change-Id: Ida408efe2f218d06a8b4ecda7658434ea05de523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4292453
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110692}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -1051,7 +1051,9 @@ void PopulateChromeWebUIFrameBinders(
                                            NewTabPageUI>(map);
   }
 
-  if (base::FeatureList::IsEnabled(ntp_features::kNtpHistoryClustersModule)) {
+  if (base::FeatureList::IsEnabled(ntp_features::kNtpHistoryClustersModule) ||
+      base::FeatureList::IsEnabled(
+          ntp_features::kNtpHistoryClustersModuleLoad)) {
     RegisterWebUIControllerInterfaceBinder<
         ntp::history_clusters::mojom::PageHandler, NewTabPageUI>(map);
   }
