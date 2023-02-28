dc44352ee130ba41c4cc8a2a2986a5a812271616
Xianzhu Wang <wangxianzhu@chromium.org>
2023-02-28 21:23:58 +0000

    PaintChunkSubset::operator[]
    We didn't have it because the implementation would not be simple
    with the range variant. Now it's simple to implement with only
    the indices variant. This can simplify usages like
    `(chunks.begin + i)->`.
    
    Change-Id: I136ca0f3fc9db0170ead6c0932a70abb8ea3a0c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4292435
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111171}

--- third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
@@ -833,7 +833,7 @@ TEST_P(PaintPropertyTreeUpdateTest, ViewportAddRemoveDeviceEmulationNode) {
   EXPECT_FALSE(visual_viewport.LayerForVerticalScrollbar());
   ASSERT_TRUE(GetLayoutView().GetScrollableArea());
   {
-    auto& chunk = *(ContentPaintChunks().begin() + 1);
+    auto& chunk = ContentPaintChunks()[1];
     EXPECT_EQ(DisplayItem::kScrollbarHorizontal, chunk.id.type);
     EXPECT_EQ(&TransformPaintPropertyNode::Root(),
               &chunk.properties.Transform());
@@ -846,7 +846,7 @@ TEST_P(PaintPropertyTreeUpdateTest, ViewportAddRemoveDeviceEmulationNode) {
   UpdateAllLifecyclePhasesForTest();
   EXPECT_TRUE(visual_viewport.GetDeviceEmulationTransformNode());
   {
-    auto& chunk = *(ContentPaintChunks().begin() + 1);
+    auto& chunk = ContentPaintChunks()[1];
     EXPECT_EQ(DisplayItem::kScrollbarHorizontal, chunk.id.type);
     EXPECT_EQ(visual_viewport.GetDeviceEmulationTransformNode(),
               &chunk.properties.Transform());
@@ -859,7 +859,7 @@ TEST_P(PaintPropertyTreeUpdateTest, ViewportAddRemoveDeviceEmulationNode) {
   UpdateAllLifecyclePhasesForTest();
   EXPECT_FALSE(visual_viewport.GetDeviceEmulationTransformNode());
   {
-    auto& chunk = *(ContentPaintChunks().begin() + 1);
+    auto& chunk = ContentPaintChunks()[1];
     EXPECT_EQ(DisplayItem::kScrollbarHorizontal, chunk.id.type);
     EXPECT_EQ(&TransformPaintPropertyNode::Root(),
               &chunk.properties.Transform());
