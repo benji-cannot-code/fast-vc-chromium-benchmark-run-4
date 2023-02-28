0d3bd9acd992759fc1946763a2bd028fd5062a29
Quentin Pubert <qpubert@google.com>
2023-02-28 20:36:16 +0000

    [iOS] Disable Native Find in Page on NTP
    This CL explicitly disables Native Find in Page for the NTP.
    
    Fixed: 1419575
    Change-Id: If584fd0150ccd4e514f78565fd9e70491fafd108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295430
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111137}

--- ios/chrome/browser/find_in_page/find_in_page_controller.mm
+++ ios/chrome/browser/find_in_page/find_in_page_controller.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/ukm/ios/ukm_url_recorder.h"
 #import "ios/chrome/browser/find_in_page/find_in_page_model.h"
 #import "ios/chrome/browser/find_in_page/find_in_page_response_delegate.h"
+#import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #import "ios/public/provider/chrome/browser/find_in_page/find_in_page_api.h"
 #import "ios/web/public/find_in_page/find_in_page_manager.h"
 #import "ios/web/public/find_in_page/find_in_page_manager_delegate_bridge.h"
@@ -90,7 +91,9 @@ NSString* gSearchTerm;
 }
 
 - (BOOL)canFindInPage {
-  return _findInPageManager->CanSearchContent();
+  NewTabPageTabHelper* ntpHelper = NewTabPageTabHelper::FromWebState(_webState);
+  BOOL isNTPActive = ntpHelper->IsActive();
+  return !isNTPActive && _findInPageManager->CanSearchContent();
 }
 
 - (void)saveSearchTerm {
