07c0c69635d38d02e82a144ceab781f95949f324
Florian Leimgruber <fleimgruber@google.com>
2023-02-28 13:02:46 +0000

    Fix CONTACT_INFO Finch-feature names.
    They currently start with a 'k', but shouldn't.
    
    Bug: 1348294
    Change-Id: I72903705680a926873b63333649b4a577e603fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294917
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110909}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -100,11 +100,11 @@ BASE_FEATURE(kSyncEnableContactInfoDataType,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncEnableContactInfoDataTypeInTransportMode,
-             "kSyncEnableContactInfoDataTypeInTransportMode",
+             "SyncEnableContactInfoDataTypeInTransportMode",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncEnableContactInfoDataTypeForCustomPassphraseUsers,
-             "kSyncEnableContactInfoDataTypeForCustomPassphraseUsers",
+             "SyncEnableContactInfoDataTypeForCustomPassphraseUsers",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncEnforceBookmarksCountLimit,
