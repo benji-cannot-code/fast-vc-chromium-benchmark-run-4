b585a9075ceaa6fcfba9b991f7baae9ef9180e75
Alvin Ji <alvinji@chromium.org>
2023-02-28 22:20:01 +0000

    Handle sink failure for AudioContext.setSinkId - Part 3
    This CL is the follow up to CL4242564. It includes:
    1. Remove sink start action
    from RendererWebAudioDeviceImpl::CreateSinkAndGetDeviceStatus()
    2. Add DeviceState in RendererWebAudioDeviceImpl to guard case like repeated Start.
    
    Change-Id: Ibc361c589f0c70c03f3ec3bd26ca368561a99666
    Bug: 1410553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279442
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111204}

--- content/renderer/media/renderer_webaudiodevice_impl.cc
+++ content/renderer/media/renderer_webaudiodevice_impl.cc
@@ -181,19 +181,26 @@ RendererWebAudioDeviceImpl::RendererWebAudioDeviceImpl(
 }
 
 RendererWebAudioDeviceImpl::~RendererWebAudioDeviceImpl() {
-  DCHECK(!sink_);
+  // In case device is not stopped, we can stop it here.
+  Stop();
 }
 
 void RendererWebAudioDeviceImpl::Start() {
   DCHECK(thread_checker_.CalledOnValidThread());
   SendLogMessage(base::StringPrintf("%s", __func__));
 
-  if (sink_)
-    return;  // Already started.
+  // Already started.
+  if (!is_stopped_) {
+    return;
+  }
 
-  CreateAudioRendererSink();
+  if (!sink_) {
+    CreateAudioRendererSink();
+  }
+
   sink_->Start();
   sink_->Play();
+  is_stopped_ = false;
 }
 
 void RendererWebAudioDeviceImpl::Pause() {
@@ -221,6 +228,7 @@ void RendererWebAudioDeviceImpl::Stop() {
   }
 
   silent_sink_suspender_.reset();
+  is_stopped_ = true;
 }
 
 double RendererWebAudioDeviceImpl::SampleRate() {
@@ -286,7 +294,8 @@ void RendererWebAudioDeviceImpl::SendLogMessage(const std::string& message) {
 }
 
 void RendererWebAudioDeviceImpl::CreateAudioRendererSink() {
-  DCHECK(!sink_);
+  DCHECK(thread_checker_.CalledOnValidThread());
+  CHECK(!sink_);
 
   switch (sink_descriptor_.Type()) {
     case blink::WebAudioSinkDescriptor::kAudible:
@@ -312,6 +321,7 @@ void RendererWebAudioDeviceImpl::CreateAudioRendererSink() {
 
 media::OutputDeviceStatus
 RendererWebAudioDeviceImpl::CreateSinkAndGetDeviceStatus() {
+  DCHECK(thread_checker_.CalledOnValidThread());
   CreateAudioRendererSink();
 
   // The device status of a silent sink is always OK.
@@ -324,12 +334,7 @@ RendererWebAudioDeviceImpl::CreateSinkAndGetDeviceStatus() {
   // If sink status is not OK, reset `sink_` and `silent_sink_suspender_`
   // because this instance will be destroyed.
   if (status != media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_OK) {
-    sink_->Stop();
-    sink_ = nullptr;
-    silent_sink_suspender_.reset();
-  } else {
-    sink_->Start();
-    sink_->Play();
+    Stop();
   }
   return status;
 }
