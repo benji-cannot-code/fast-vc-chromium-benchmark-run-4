83dadb06b3372dcd2d9401efe47aa5dbc078d439
Hidehiko Abe <hidehiko@chromium.org>
2023-02-28 16:13:53 +0000

    Replace MockUserManager in SigninErrorNotifierTest
    Use FakeChromeUserManager instead along with MockUserManager's
    deprecation.
    
    BUG=1420135
    TEST=Ran tryjob
    
    Change-Id: I47843aedcb0f3cd0b2df09e3997f97c8de55fa46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297558
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110986}

--- chrome/browser/ash/login/signin/signin_error_notifier_unittest.cc
+++ chrome/browser/ash/login/signin/signin_error_notifier_unittest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/ash/login/signin/signin_error_notifier_factory.h"
-#include "chrome/browser/ash/login/users/mock_user_manager.h"
+#include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_display_service_tester.h"
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
@@ -52,9 +52,8 @@ class SigninErrorNotifierTest : public BrowserWithTestWindowTest {
   void SetUp() override {
     BrowserWithTestWindowTest::SetUp();
 
-    mock_user_manager_ = new MockUserManager();
     user_manager_enabler_ = std::make_unique<user_manager::ScopedUserManager>(
-        base::WrapUnique(mock_user_manager_));
+        std::make_unique<FakeChromeUserManager>());
 
     SigninErrorNotifierFactory::GetForProfile(GetProfile());
     display_service_ =
@@ -89,7 +88,6 @@ class SigninErrorNotifierTest : public BrowserWithTestWindowTest {
 
  protected:
   std::unique_ptr<NotificationDisplayServiceTester> display_service_;
-  MockUserManager* mock_user_manager_;  // Not owned.
   std::unique_ptr<user_manager::ScopedUserManager> user_manager_enabler_;
   std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
       identity_test_env_profile_adaptor_;
