300cfcd4c017b7190d0016015039b5ca7a06422f
Tony Herre <toprice@chromium.org>
2023-02-28 21:34:22 +0000

    Remove calls to WebRTC TransformableVideoFrame GetMetadata()
    Instead use Metadata(), returning a new instance by value rather than
    a const ref which will become invalid whenever anyone calls
    SetMetadata().
    
    GetMetadata() will be deprecated in https://webrtc-review.googlesource.com/c/src/+/295481 and eventually removed from the
    WebRTC API.
    
    Bug: 1420245
    Change-Id: I1737c8980713885dc6e3a279657fe4436f77f475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295638
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111176}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.cc
@@ -244,7 +244,9 @@ RTCEncodedVideoFrameMetadata* RTCEncodedVideoFrame::getMetadata() const {
       metadata->setCaptureTimestamp(delegate_->CaptureTimeIdentifier()->us());
     }
   }
-  const auto* webrtc_metadata = delegate_->GetMetadata();
+
+  const absl::optional<webrtc::VideoFrameMetadata> webrtc_metadata =
+      delegate_->GetMetadata();
   if (!webrtc_metadata)
     return metadata;
 
@@ -348,7 +350,8 @@ void RTCEncodedVideoFrame::setMetadata(RTCEncodedVideoFrameMetadata* metadata,
     return;
   }
 
-  const auto* original_webrtc_metadata = delegate_->GetMetadata();
+  const absl::optional<webrtc::VideoFrameMetadata> original_webrtc_metadata =
+      delegate_->GetMetadata();
   if (!original_metadata) {
     exception_state.ThrowDOMException(
         DOMExceptionCode::kInvalidModificationError,
