0274a962b7502b389607d7d63cf6102195ef91c1
Jinsuk Kim <jinsukkim@chromium.org>
2023-02-28 16:05:20 +0000

    [PCCT-SideSheet] Remove side shadow in fullscreen
    Calls |updateShadowOffset| to update the presence of shadow on the
    side when going in and out of fullscreen mode.
    
    Bug: 1418554
    Change-Id: I7c98ac6625bb3887dcf7a0cc3d475ed2de33e927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296933
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1110979}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBaseStrategy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBaseStrategy.java
@@ -200,6 +200,7 @@ public abstract class PartialCustomTabBaseStrategy
         attrs.y = 0;
         attrs.x = 0;
         mActivity.getWindow().setAttributes(attrs);
+        updateShadowOffset();
         maybeInvokeResizeCallback();
     }
 
@@ -208,6 +209,7 @@ public abstract class PartialCustomTabBaseStrategy
         // |mNavbarHeight| is zero now. Post the task instead.
         new Handler().post(() -> {
             initializeSize();
+            updateShadowOffset();
             maybeInvokeResizeCallback();
         });
     }
@@ -284,7 +286,7 @@ public abstract class PartialCustomTabBaseStrategy
     }
 
     protected void updateShadowOffset() {
-        if (isFullHeight() || mDrawOutlineShadow || shouldHaveNoShadowOffset()) {
+        if (isFullHeight() || isFullscreen() || mDrawOutlineShadow || shouldHaveNoShadowOffset()) {
             mShadowOffset = 0;
         } else {
             mShadowOffset = mActivity.getResources().getDimensionPixelSize(
@@ -432,5 +434,10 @@ public abstract class PartialCustomTabBaseStrategy
     int getTopMarginForTesting() {
         var mlp = (ViewGroup.MarginLayoutParams) mToolbarCoordinator.getLayoutParams();
         return mlp.topMargin;
+    }
+
+    @VisibleForTesting
+    int getShadowOffsetForTesting() {
+        return mShadowOffset;
     }
 }
