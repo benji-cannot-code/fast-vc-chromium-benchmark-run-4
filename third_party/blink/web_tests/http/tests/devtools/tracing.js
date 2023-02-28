434454eec899dfa16e8c90082683ce5c594a3203
Jack Franklin <jacktfranklin@chromium.org>
2023-02-28 13:41:40 +0000

    Update devtools tracing.js test
    We are restructuring how we store and refer to the event tracing Phases,
    and this test in its current form is blocking a devtools-frontend CL
    from landing [crrev.com/c/4296537].
    
    Rather than refer to the enum phase, I have updated this test to use the
    letter itself. This ensures that this test will continue to function and
    test the trace events regardless of how we refactor devtools-frontend to
    deal with that data.
    
    Fixed: 1420246
    Change-Id: Idf43efbda4fb692b925ee63cb3669db2178f81c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295427
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110922}

--- third_party/blink/web_tests/http/tests/devtools/tracing.js
+++ third_party/blink/web_tests/http/tests/devtools/tracing.js
@@ -78,7 +78,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     for (var i = 0; i < events.length; ++i) {
       var event = events[i];
-      if (event.phase === SDK.TracingModel.Phase.Complete)
+      // X is the letter used to mark an event's phase as being Complete.
+      if (event.phase === "X")
         ++phaseComplete;
       if (event.name in knownEvents)
         ++knownEvents[event.name];
