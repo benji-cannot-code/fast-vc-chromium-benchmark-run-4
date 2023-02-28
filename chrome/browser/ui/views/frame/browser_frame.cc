b335626b8cf14fee4337c0ec6b042c612a92a995
Sammie Quon <sammiequon@chromium.org>
2023-02-28 21:08:37 +0000

    float: Remove separator for desks menu
    Float windows are currently not supported to be visible on all desks.
    In this case, we should not show the separator of the submenu.
    
    Test: manual
    Change-Id: Ifb6616d9695947818c56e45aa8f9b31b5a00aa63
    Fixed: b/270751428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4292455
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111159}

--- chrome/browser/ui/views/frame/browser_frame.cc
+++ chrome/browser/ui/views/frame/browser_frame.cc
@@ -41,7 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/native_widget.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/ui/base/window_properties.h"
+#include "chromeos/ui/base/window_state_type.h"
 #include "chromeos/ui/wm/desks/desks_helper.h"
+#include "ui/aura/window.h"
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -330,6 +333,8 @@ void BrowserFrame::ShowContextMenuForViewImpl(views::View* source,
 }
 
 ui::MenuModel* BrowserFrame::GetSystemMenuModel() {
+  // TODO(b/271137301): Refactor this class to remove chromeos specific code to
+  // subclasses.
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   if (user_manager::UserManager::IsInitialized() &&
       user_manager::UserManager::Get()->GetLoggedInUsers().size() > 1) {
@@ -348,6 +353,14 @@ ui::MenuModel* BrowserFrame::GetSystemMenuModel() {
     // changes happened since the last invocation.
     menu_model_builder_.reset();
     num_desks_ = current_num_desks;
+  }
+
+  bool is_float_state_type =
+      GetNativeWindow()->GetProperty(chromeos::kWindowStateTypeKey) ==
+      chromeos::WindowStateType::kFloated;
+  if (is_float_state_type != is_float_state_type_) {
+    menu_model_builder_.reset();
+    is_float_state_type_ = is_float_state_type;
   }
 #endif
   if (!menu_model_builder_.get()) {
