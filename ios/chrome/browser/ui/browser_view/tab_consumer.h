aefffe5d987dcfc503c3d18765dd4f9555a39993
Federica Germinario <fedegermi@google.com>
2023-02-28 14:45:44 +0000

    [ios] Move didInsertWebState observation logic out of BVC
    This is a second step to completely remove WebStateList observation
    logic from BVC. In this CL didInsertWebState was moved inside
    TabEventsMediator. Since it triggers UI changes, a consumer was used to
    notify BVC of the updates.
    
    Bug: 1329088
    Change-Id: I4b445724aafa458892c0daa0c9e5a7fadc4188f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244938
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110942}

--- ios/chrome/browser/ui/browser_view/tab_consumer.h
+++ ios/chrome/browser/ui/browser_view/tab_consumer.h
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Tells the consumer to reset the content view.
 - (void)resetTab;
 
+// Tells the consumer to start an animation for a background tab.
+- (void)animateNewBackgroundTab;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_BROWSER_VIEW_TAB_CONSUMER_H_
