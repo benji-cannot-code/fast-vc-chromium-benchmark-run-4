2ef40684e955e5c4d64c329ada1dd1be64ffea3c
Wes Okuhara <wesokuhara@google.com>
2023-02-28 21:56:01 +0000

    CrOS Settings: Convert add user dialog browser tests to TS
    Additionally moves the test under a new folder `os_people_page/` to
    mirror the app folder structure. Renames the element and test file to
    add_user_dialog.ts and add_user_dialog_tests.ts respectively.
    
    Bug: b/270728282
    Test: browser_tests --gtest_filter="OSSettingsOsPeoplePageAddUserDialog*"
    Change-Id: I2d3cf43de273aea365d508094fe2ab9b43e7adb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296042
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111189}

--- chrome/test/data/webui/settings/chromeos/os_people_page/add_user_dialog_tests.ts
+++ chrome/test/data/webui/settings/chromeos/os_people_page/add_user_dialog_tests.ts
@@ -5,52 +5,53 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://os-settings/chromeos/os_settings.js';
 
-import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {SettingsUsersAddUserDialogElement} from 'chrome://os-settings/chromeos/os_settings.js';
+import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import {assertEquals, assertFalse, assertNotEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 
-import {FakeUsersPrivate} from './fake_users_private.js';
+import {FakeUsersPrivate} from '../fake_users_private.js';
 
-suite('AddPersonDialog', function() {
-  let dialog = null;
+suite('<settings-users-add-user-dialog>', () => {
+  let dialog: SettingsUsersAddUserDialogElement|null = null;
 
-  setup(function() {
-    PolymerTest.clearBody();
-
+  setup(() => {
     dialog = document.createElement('settings-users-add-user-dialog');
+    // @ts-ignore:next-line - Overriding private member for test
     dialog.usersPrivate_ = new FakeUsersPrivate();
-
     document.body.appendChild(dialog);
 
     dialog.open();
   });
 
-  teardown(function() {
-    dialog.remove();
+  teardown(() => {
+    dialog!.remove();
     dialog = null;
   });
-
 
   /**
    * Test that the dialog reacts to valid and invalid input correctly.
    */
-  test('Add user', function() {
-    const userInputBox = dialog.shadowRoot.querySelector('#addUserInput');
+  test('Add user', () => {
+    const userInputBox =
+        dialog!.shadowRoot!.querySelector<CrInputElement>('#addUserInput');
     assertTrue(!!userInputBox);
+    assertFalse(userInputBox.invalid);
 
-    const addButton = dialog.shadowRoot.querySelector('.action-button');
+    const addButton =
+        dialog!.shadowRoot!.querySelector<HTMLButtonElement>('.action-button');
     assertTrue(!!addButton);
     assertTrue(addButton.disabled);
-    assertTrue(!userInputBox.invalid);
 
     // Try to add a valid username without domain
     userInputBox.value = 'abcdef';
-    assertTrue(!addButton.disabled);
-    assertTrue(!userInputBox.invalid);
+    assertFalse(addButton.disabled);
+    assertFalse(userInputBox.invalid);
 
     // Try to add a valid username with domain
     userInputBox.value = 'abcdef@xyz.com';
-    assertTrue(!addButton.disabled);
-    assertTrue(!userInputBox.invalid);
+    assertFalse(addButton.disabled);
+    assertFalse(userInputBox.invalid);
 
     // Try to add an invalid username
     userInputBox.value = 'abcdef@';
@@ -58,12 +59,17 @@ suite('AddPersonDialog', function() {
     assertTrue(userInputBox.invalid);
   });
 
-  test('Add duplicate user', async function() {
-    const userInputBox = dialog.shadowRoot.querySelector('#addUserInput');
-    const addButton = dialog.shadowRoot.querySelector('.action-button');
-    const duplicateUserEmail = 'duplicateUser@google.com';
+  test('Add duplicate user', async () => {
+    const userInputBox =
+        dialog!.shadowRoot!.querySelector<CrInputElement>('#addUserInput');
+    assertTrue(!!userInputBox);
+
+    const addButton =
+        dialog!.shadowRoot!.querySelector<HTMLButtonElement>('.action-button');
+    assertTrue(!!addButton);
 
     // Add user for the first time.
+    const duplicateUserEmail = 'duplicateUser@google.com';
     userInputBox.value = duplicateUserEmail;
     addButton.click();
     await waitAfterNextRender(userInputBox);
@@ -81,11 +87,16 @@ suite('AddPersonDialog', function() {
     assertNotEquals('', userInputBox.errorMessage);
   });
 
-  test('Add new user', async function() {
-    const userInputBox = dialog.shadowRoot.querySelector('#addUserInput');
-    const addButton = dialog.shadowRoot.querySelector('.action-button');
-    const newUserEmail = 'newUser@google.com';
+  test('Add new user', async () => {
+    const userInputBox =
+        dialog!.shadowRoot!.querySelector<CrInputElement>('#addUserInput');
+    assertTrue(!!userInputBox);
 
+    const addButton =
+        dialog!.shadowRoot!.querySelector<HTMLButtonElement>('.action-button');
+    assertTrue(!!addButton);
+
+    const newUserEmail = 'newUser@google.com';
     userInputBox.value = newUserEmail;
     addButton.click();
     await waitAfterNextRender(userInputBox);
@@ -94,9 +105,15 @@ suite('AddPersonDialog', function() {
     assertEquals('', userInputBox.errorMessage);
   });
 
-  test('Add two new users', async function() {
-    const userInputBox = dialog.shadowRoot.querySelector('#addUserInput');
-    const addButton = dialog.shadowRoot.querySelector('.action-button');
+  test('Add two new users', async () => {
+    const userInputBox =
+        dialog!.shadowRoot!.querySelector<CrInputElement>('#addUserInput');
+    assertTrue(!!userInputBox);
+
+    const addButton =
+        dialog!.shadowRoot!.querySelector<HTMLButtonElement>('.action-button');
+    assertTrue(!!addButton);
+
     const firstUserEmail = 'firstUser@google.com';
     const secondUserEmail = 'secondUser@google.com';
 
