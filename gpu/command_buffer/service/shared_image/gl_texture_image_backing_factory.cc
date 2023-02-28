2653f0ba975abaeec08b380356cf7b87100f0469
Maksim Sisov <msisov@igalia.com>
2023-02-28 16:08:37 +0000

    [ios]: exclude macOS special shared image usages on iOS.
    This excludes special usages from iOS, which makes
    GLTextureImageBackingFactory::IsSupported return false when
    Angle/Metal is used. There is no benefit for iOS to use IOSurface
    instead of GLTextures for some of the SI usages as it doesn't
    either have multi-gpu support or GLTextures are sufficient
    in terms of functionality.
    
    This change fixes the VideoRendererPixelHiLoTest.* tests that use
    high bit (>8) video frames and also GLTextureImageBackingFactory* tests on iOS.
    
    Bug: 1413718, 1413717
    Change-Id: I2a7e7a6d3d3fc61599484c95780738700e70450d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4287236
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1110981}

--- gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory.cc
@@ -149,6 +149,9 @@ bool GLTextureImageBackingFactory::IsSupported(
     }
   }
 
+  // This is not beneficial on iOS. The main purpose of this is a multi-gpu
+  // support.
+#if BUILDFLAG(IS_MAC)
   if (gl::GetGLImplementation() == gl::kGLImplementationEGLANGLE &&
       gl::GetANGLEImplementation() == gl::ANGLEImplementation::kMetal) {
     constexpr uint32_t kMetalInvalidUsages =
@@ -158,6 +161,7 @@ bool GLTextureImageBackingFactory::IsSupported(
       return false;
     }
   }
+#endif  // BUILDFLAG(IS_MAC)
 
   // Doesn't support contexts other than GL for OOPR Canvas
   if (gr_context_type != GrContextType::kGL &&
