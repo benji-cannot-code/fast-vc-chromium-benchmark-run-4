6effe2e634cf19e8eb15286d81fdbac132482ca2
Jason Jeremy Iman <jasongustaman@google.com>
2023-03-01 03:23:00 +0000

    arc: net: Add unit tests for Passpoint dialog
    BUG=b:266151265
    TEST=out/Default/ash_components_unittests \
         --gtest_filter="Passpoint*";
    
    Change-Id: Ifaafb2f669226247c4baa3337bef76573c1db931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4253989
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Jason Jeremy Iman <jasongustaman@chromium.org>
    Commit-Queue: Jason Jeremy Iman <jasongustaman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111397}

--- ash/components/arc/net/passpoint_dialog_view.cc
+++ ash/components/arc/net/passpoint_dialog_view.cc
@@ -120,6 +120,7 @@ std::unique_ptr<views::View> PasspointDialogView::MakeContentsView() {
                             kDialogBodyMargin[2], kDialogBodyMargin[3]))
       .AddChildren(
           views::Builder<views::StyledLabel>()
+              .CopyAddressTo(&body_text_)
               .SetText(label)
               .SetHorizontalAlignment(gfx::ALIGN_LEFT)
               .AddStyleRange(
@@ -138,6 +139,7 @@ std::unique_ptr<views::View> PasspointDialogView::MakeButtonsView() {
           views::DistanceMetric::DISTANCE_RELATED_BUTTON_HORIZONTAL))
       .AddChildren(
           views::Builder<views::MdTextButton>()  // Don't allow button.
+              .CopyAddressTo(&dont_allow_button_)
               .SetCallback(base::BindRepeating(
                   &PasspointDialogView::OnButtonClicked,
                   weak_factory_.GetWeakPtr(), /*allow=*/false))
@@ -146,6 +148,7 @@ std::unique_ptr<views::View> PasspointDialogView::MakeButtonsView() {
               .SetProminent(false)
               .SetIsDefault(false),
           views::Builder<views::MdTextButton>()  // Allow button.
+              .CopyAddressTo(&allow_button_)
               .SetCallback(base::BindRepeating(
                   &PasspointDialogView::OnButtonClicked,
                   weak_factory_.GetWeakPtr(), /*allow=*/true))
--- ash/components/arc/net/passpoint_dialog_view.h
+++ ash/components/arc/net/passpoint_dialog_view.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string_piece.h"
+#include "ui/views/controls/button/md_text_button.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/box_layout_view.h"
 
 namespace aura {
@@ -29,6 +31,21 @@ class PasspointDialogView : public views::BoxLayoutView {
   // as its argument. Otherwise, with false.
   using PasspointDialogCallback =
       base::OnceCallback<void(mojom::PasspointApprovalResponsePtr)>;
+
+  // TestApi is used only in tests to get internal views.
+  class TestApi {
+   public:
+    explicit TestApi(PasspointDialogView* view) : view_(view) {}
+
+    views::StyledLabel* body_text() const { return view_->body_text_; }
+    views::MdTextButton* allow_button() const { return view_->allow_button_; }
+    views::MdTextButton* dont_allow_button() const {
+      return view_->dont_allow_button_;
+    }
+
+   private:
+    PasspointDialogView* const view_;
+  };
 
   PasspointDialogView(base::StringPiece app_name,
                       PasspointDialogCallback callback);
@@ -53,6 +70,11 @@ class PasspointDialogView : public views::BoxLayoutView {
   void OnButtonClicked(bool allow);
 
   PasspointDialogCallback callback_;
+
+  // Added for testing.
+  views::StyledLabel* body_text_{nullptr};
+  views::MdTextButton* allow_button_{nullptr};
+  views::MdTextButton* dont_allow_button_{nullptr};
 
   base::WeakPtrFactory<PasspointDialogView> weak_factory_{this};
 };
