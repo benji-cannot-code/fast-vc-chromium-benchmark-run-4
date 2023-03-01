91bd6f761005e23c394b0c0f239b2b35ac444951
Andrew Paseltiner <apaseltiner@chromium.org>
2023-03-01 15:49:51 +0000

    Rename attribution_observer_types to create_report_result
    This header declares one type, CreateReportResult, so it is more
    readable to use the same name for the files.
    
    Change-Id: Ia52cb3a44f43a2d8510e356fcfc2f5cd2213135c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295832
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111615}

--- content/browser/attribution_reporting/attribution_storage.cc
+++ content/browser/attribution_reporting/attribution_storage.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/attribution_reporting/attribution_storage.h"
 
 #include "base/check.h"
-#include "content/browser/attribution_reporting/attribution_observer_types.h"
 #include "content/browser/attribution_reporting/storable_source.h"
 
 namespace content {
--- content/browser/attribution_reporting/create_report_result.cc
+++ content/browser/attribution_reporting/create_report_result.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/attribution_reporting/attribution_observer_types.h"
+#include "content/browser/attribution_reporting/create_report_result.h"
 
 #include <utility>
 
