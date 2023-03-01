33b6ace8f8c6260a1ce4c93557554b3d4be2f328
Ramin Halavati <rhalavati@chromium.org>
2023-03-01 19:01:57 +0000

    Increase the min required version of ScreenAI library.
    To enforce updating the dll for Windows from unsigned one.
    
    AX-Relnotes: n/a
    Bug: 1278249, 1420350
    Change-Id: I78006015309163e446089313e55cec6689f882ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295119
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111735}

--- components/services/screen_ai/public/cpp/screen_ai_install_state.cc
+++ components/services/screen_ai/public/cpp/screen_ai_install_state.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const int kScreenAICleanUpDelayInDays = 30;
-const char kMinExpectedVersion[] = "112.1";
+const char kMinExpectedVersion[] = "112.3";
 }
 
 namespace {
