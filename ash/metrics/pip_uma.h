5240200b925510c179415692fb2aca432b07c77d
Ari Chivukula <arichiv@chromium.org>
2023-03-01 22:37:55 +0000

    Remove expired histograms Ash.Pip.FreeResizeFinishArea, Ash.Pip.FreeResizeInitialArea, Ash.Pip.Position
    These histograms haven't been recorded since 2021 so removal seems to be
    the correct path here. This is a code-health task.
    
    Bug: 1384717
    Change-Id: I43e0aa12e0fa4c2082f2debdbd406a1ef6918b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300452
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111870}

--- ash/metrics/pip_uma.h
+++ ash/metrics/pip_uma.h
@@ -11,11 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 constexpr char kAshPipEventsHistogramName[] = "Ash.Pip.Events";
-constexpr char kAshPipFreeResizeInitialAreaHistogramName[] =
-    "Ash.Pip.FreeResizeInitialArea";
-constexpr char kAshPipFreeResizeFinishAreaHistogramName[] =
-    "Ash.Pip.FreeResizeFinishArea";
-constexpr char kAshPipPositionHistogramName[] = "Ash.Pip.Position";
 constexpr char kAshPipAndroidPipUseTimeHistogramName[] =
     "Ash.Pip.AndroidPipUseTime";
 
