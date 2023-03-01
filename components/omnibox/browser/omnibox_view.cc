590b2f8db722d0ddda5e35d776af81feb50ccfc6
manukh <manukh@chromium.org>
2023-03-01 16:50:25 +0000

    [omnibox][perf] Optimize navigation classifier calls.
    Omnibox navigation reverts the omnibox state (model & popup), which
    invokes `AutocompleteClassifier::Classify()` 3 times, which adds
    non-negligible latency (3ms).
    
    It seems unnecessary to revert and updating the omnibox when it's about
    to be redone due to navigation. But it's actually necessary because
    a) sometimes the navigation isn't in the same tab (e.g. opening
    chrome://settings in incognito) or b) the navigation isn't immediate. So
    instead, this CL tries to reduce the time taken to do the revert.
    
    This CL adds 2 optimizations:
    
    a) tldr; when reverting the omnibox view, 1st revert the edit model then
    close the popup, instead of vice versa.
    
    Currently on revert, 1st the popup is closed, then the edit model is
    reverted. Closing the popup will stop the running autocomplete
    providers, which notifies autocomplete observers that autocomplete state
    has changed, which updates the location bar view. However, because the
    model hasn't yet been reverted, the location bar view thinks there's
    still input in progress, so it tries to autocomplete the temp text to
    determine the icons and text bolding. This results in 3 redundant
    `Classify()` calls (i.e. all calls are classifying the same temp text).
    To add insult to injury, this expensive update is immediately discarded
    when the edit model revert re-updates the location bar view.
    
    This CL flips the order, 1st reverting the model then closing the popup.
    This ensures that when the popup is closed, we know there is no temp
    text and don't need to run `Classify()`. We still update the location
    bar view twice though; I haven't looked into how expensive that is,
    since now at least it's being updated with the same text and icons so
    perhaps the views are smart enough to short circuit.
    
    b) tldr; fix the `current_match_` cache to not be almost always empty.
    
    `OmniboxController` has a `current_match_` that is intended to cache the
    current match. When it's unavailable, the edit model resorts to
    classifying the view text, which runs the sync autocomplete pass, which
    is expensive (1ms). This cached match is almost always empty. It's set
    only after the sync autocomplete pass, but it's cleared almost
    immediately afterwards.
    
    This CL sets `current_match_` on autocomplete change, selection change,
    and tab switch. And it's never cleared, instead being replacing with
    the new match; so its hit rate is now 100%.
    
    This CL also moves `current_match_` from the omnibox controller to the
    edit model since that's where it's both used and update.
    
    
    The touched code is convoluted. To ensure we don't introduce unwanted
    consequences, and to measure the improvements, these 2 changes are
    guarded by independent features.
    
    Also adds 3 timing histograms for `Classify()`, `RevertAll()`, and
    `OpenMatch()`.
    
    Bug: 806262, 1418490
    Change-Id: I08558b2269a43d6a2ddbd777de6403f6a561afa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4273874
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111638}

--- components/omnibox/browser/omnibox_view.cc
+++ components/omnibox/browser/omnibox_view.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/feature_list.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -259,9 +261,28 @@ void OmniboxView::SetUserText(const std::u16string& text, bool update_popup) {
 }
 
 void OmniboxView::RevertAll() {
-  CloseOmniboxPopup();
-  if (model_)
-    model_->Revert();
+  // TODO(manukh): Remove this histogram when `kRedoCurrentMatch` &
+  //   `kRevertModelBeforeClosingPopup` launch or are abandoned.
+  SCOPED_UMA_HISTOGRAM_TIMER_MICROS("Omnibox.OmniboxViewRevertAll");
+
+  if (base::FeatureList::IsEnabled(omnibox::kRevertModelBeforeClosingPopup)) {
+    // This will clear the model's `user_input_in_progress_`.
+    if (model_)
+      model_->Revert();
+
+    // This will stop the `AutocompleteController`. This should happen after
+    // `user_input_in_progress_` is cleared above; otherwise, closing the popup
+    // will trigger unnecessary `AutocompleteClassifier::Classify()` calls to
+    // try to update the views which are unnecessary since they'll be thrown
+    // away during the model revert anyways.
+    CloseOmniboxPopup();
+  } else {
+    // Same as above, but in reverse order.
+    CloseOmniboxPopup();
+    if (model_)
+      model_->Revert();
+  }
+
   TextChanged();
 }
 
