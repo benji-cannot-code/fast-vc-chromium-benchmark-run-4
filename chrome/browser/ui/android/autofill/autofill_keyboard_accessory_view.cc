1275659bcb02c78b03b30aa465a7b853a6ca72f6
Sergey Poromov <poromov@chromium.org>
2023-03-01 12:03:07 +0000

    Revert "Introduce thresholds for keyboard events on Autofill popup."
    This reverts commit 271787037ac4d99c17e0f09dff9f77b38d4ef7bc.
    
    Reason for revert: Consistent unit test failures on MSan builder:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/38635/overview
    
    Original change's description:
    > Introduce thresholds for keyboard events on Autofill popup.
    >
    > Bug: 1418364
    > Change-Id: I212bde424e11592a97d2142ceb0850d242c42e0c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4281445
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Commit-Queue: Jan Keitel <jkeitel@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1111462}
    
    Bug: 1418364
    Change-Id: I32f7a3d8e1f5604af171e4ffe6b6e4c383265150
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295987
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111532}

--- chrome/browser/ui/android/autofill/autofill_keyboard_accessory_view.cc
+++ chrome/browser/ui/android/autofill/autofill_keyboard_accessory_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "chrome/android/features/keyboard_accessory/jni_headers/AutofillKeyboardAccessoryViewBridge_jni.h"
 #include "chrome/browser/android/resource_mapper.h"
@@ -130,7 +131,8 @@ void AutofillKeyboardAccessoryView::SuggestionSelected(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     jint list_index) {
-  controller_->AcceptSuggestionWithoutThreshold(list_index);
+  controller_->AcceptSuggestion(list_index,
+                                /*show_threshold=*/base::TimeDelta());
 }
 
 void AutofillKeyboardAccessoryView::DeletionRequested(
--- chrome/browser/ui/autofill/autofill_popup_controller_unittest.cc
+++ chrome/browser/ui/autofill/autofill_popup_controller_unittest.cc
@@ -181,7 +181,6 @@ class TestAutofillPopupController : public AutofillPopupControllerImpl {
 
   // Making protected functions public for testing
   using AutofillPopupControllerImpl::AcceptSuggestion;
-  using AutofillPopupControllerImpl::AcceptSuggestionWithoutThreshold;
   using AutofillPopupControllerImpl::element_bounds;
   using AutofillPopupControllerImpl::FireControlsChangedEvent;
   using AutofillPopupControllerImpl::GetLineCount;
@@ -737,7 +736,8 @@ TEST_F(AutofillPopupControllerUnitTest, SelectInvalidSuggestion) {
   EXPECT_CALL(*delegate(), DidAcceptSuggestion).Times(0);
 
   // The following should not crash:
-  popup_controller().AcceptSuggestion(1);  // Out of bounds!
+  popup_controller().AcceptSuggestion(
+      1, /*show_threshold=*/base::Milliseconds(0));  // Out of bounds!
 }
 
 TEST_F(AutofillPopupControllerUnitTest, AcceptSuggestionRespectsTimeout) {
@@ -745,21 +745,16 @@ TEST_F(AutofillPopupControllerUnitTest, AcceptSuggestionRespectsTimeout) {
 
   // Calls before the threshold are ignored.
   EXPECT_CALL(*delegate(), DidAcceptSuggestion).Times(0);
-  popup_controller().AcceptSuggestion(0);
+  popup_controller().AcceptSuggestion(
+      0, /*show_threshold=*/base::Milliseconds(500));
   task_environment()->FastForwardBy(base::Milliseconds(100));
-  popup_controller().AcceptSuggestion(0);
+  popup_controller().AcceptSuggestion(
+      0, /*show_threshold=*/base::Milliseconds(500));
 
   EXPECT_CALL(*delegate(), DidAcceptSuggestion);
   task_environment()->FastForwardBy(base::Milliseconds(400));
-  popup_controller().AcceptSuggestion(0);
-}
-
-TEST_F(AutofillPopupControllerUnitTest, AcceptSuggestionWithoutThreshold) {
-  ShowSuggestions({1});
-
-  // Calls are accepted immediately.
-  EXPECT_CALL(*delegate(), DidAcceptSuggestion).Times(1);
-  popup_controller().AcceptSuggestionWithoutThreshold(0);
+  popup_controller().AcceptSuggestion(
+      0, /*show_threshold=*/base::Milliseconds(500));
 }
 
 TEST_F(AutofillPopupControllerUnitTest,
@@ -768,9 +763,11 @@ TEST_F(AutofillPopupControllerUnitTest,
 
   // Calls before the threshold are ignored.
   EXPECT_CALL(*delegate(), DidAcceptSuggestion).Times(0);
-  popup_controller().AcceptSuggestion(0);
+  popup_controller().AcceptSuggestion(
+      0, /*show_threshold=*/base::Milliseconds(500));
   task_environment()->FastForwardBy(base::Milliseconds(100));
-  popup_controller().AcceptSuggestion(0);
+  popup_controller().AcceptSuggestion(
+      0, /*show_threshold=*/base::Milliseconds(500));
 
   task_environment()->FastForwardBy(base::Milliseconds(400));
   // Show the suggestions again (simulating, e.g., a click somewhere slightly
@@ -778,12 +775,14 @@ TEST_F(AutofillPopupControllerUnitTest,
   ShowSuggestions({1});
 
   EXPECT_CALL(*delegate(), DidAcceptSuggestion).Times(0);
-  popup_controller().AcceptSuggestion(0);
+  popup_controller().AcceptSuggestion(
+      0, /*show_threshold=*/base::Milliseconds(500));
 
   EXPECT_CALL(*delegate(), DidAcceptSuggestion);
   // After waiting, suggestions are accepted again.
   task_environment()->FastForwardBy(base::Milliseconds(500));
-  popup_controller().AcceptSuggestion(0);
+  popup_controller().AcceptSuggestion(
+      0, /*show_threshold=*/base::Milliseconds(500));
 }
 
 }  // namespace autofill
