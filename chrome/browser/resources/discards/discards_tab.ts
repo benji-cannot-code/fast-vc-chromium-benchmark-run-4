6ba528f06c61de5f25287329ae989acfd7965c3c
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2023-03-01 00:30:11 +0000

    Remove high efficiency mode toggle from chrome://discards
    This toggle is redundant with the Memory Saver one in chrome://settings.
    
    This CL leaves the battery saver toggle in because it's still relevant
    for testing since the "always on" option isn't available in the UI.
    
    Bug: 1420344
    Change-Id: I8c6fd96287e4acfb1619a6848f06178628f457dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294501
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111297}

--- chrome/browser/resources/discards/discards_tab.ts
+++ chrome/browser/resources/discards/discards_tab.ts
@@ -375,10 +375,6 @@ class DiscardsTabElement extends DiscardsTabElementBase {
     this.discardImpl_();
   }
 
-  private toggleHighEfficiencyMode_(_e: Event) {
-    this.discardsDetailsProvider_!.toggleHighEfficiencyMode();
-  }
-
   private toggleBatterySaverMode_(_e: Event) {
     this.discardsDetailsProvider_!.toggleBatterySaverMode();
   }
