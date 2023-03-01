1b878241b61fbc185db1ec2f07cd2a39f4ee04e1
Arthur Milchior <arthurmilchior@chromium.org>
2023-03-01 14:54:47 +0000

    [iOS] Bookmark Editor explicitly disconnects observers.
    Bug: 1420210
    Change-Id: Ia4f3a1ded7068e83d80f2ad1767cf0bec94b5bbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297799
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111584}

--- ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_mediator.mm
+++ ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_mediator.mm
@@ -67,6 +67,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _bookmarkModel = nullptr;
   _bookmark = nullptr;
   _folder = nullptr;
+  _bookmarkModelBridgeObserver = nil;
+  _syncObserverModelBridge = nil;
 }
 
 #pragma mark - BookmarksEditorMutator
