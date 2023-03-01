0fcc1a3ee3923bceb5f878f064150b38bd94ccfc
Claudio DeSouza <cdesouza@igalia.com>
2023-03-01 17:10:50 +0000

    [json-schema-compiler] Refactor to Params::Create (43/74)
    This CL removes the use of Params::CreateDeprecated, replacing it with
    the modernised Params::Create.
    
    Changes for path:
      /chrome/browser/extensions/api/web_authentication_proxy
    
    This CL was uploaded by git cl split.
    
    R=agl@chromium.org
    
    Bug: 1354063, 1414632
    Change-Id: I8f79fc4239e2f3951f7eb734f16cfc49f42bfbee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299975
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111648}

--- chrome/browser/extensions/api/web_authentication_proxy/web_authentication_proxy_api.cc
+++ chrome/browser/extensions/api/web_authentication_proxy/web_authentication_proxy_api.cc
@@ -115,9 +115,10 @@ void WebAuthenticationProxyCompleteCreateRequestFunction::DoRespond(
 ExtensionFunction::ResponseAction
 WebAuthenticationProxyCompleteCreateRequestFunction::Run() {
   DCHECK(extension());
-  auto params = api::web_authentication_proxy::CompleteCreateRequest::Params::
-      CreateDeprecated(args());
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+  auto params =
+      api::web_authentication_proxy::CompleteCreateRequest::Params::Create(
+          args());
+  EXTENSION_FUNCTION_VALIDATE(params);
   WebAuthenticationProxyService* proxy_service =
       WebAuthenticationProxyService::GetIfProxyAttached(browser_context());
   if (!proxy_service || proxy_service->GetActiveRequestProxy() != extension()) {
@@ -144,9 +145,9 @@ void WebAuthenticationProxyCompleteGetRequestFunction::DoRespond(
 ExtensionFunction::ResponseAction
 WebAuthenticationProxyCompleteGetRequestFunction::Run() {
   DCHECK(extension());
-  auto params = api::web_authentication_proxy::CompleteGetRequest::Params::
-      CreateDeprecated(args());
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+  auto params =
+      api::web_authentication_proxy::CompleteGetRequest::Params::Create(args());
+  EXTENSION_FUNCTION_VALIDATE(params);
   WebAuthenticationProxyService* proxy_service =
       WebAuthenticationProxyService::GetIfProxyAttached(browser_context());
   if (!proxy_service || proxy_service->GetActiveRequestProxy() != extension()) {
@@ -167,9 +168,10 @@ WebAuthenticationProxyCompleteIsUvpaaRequestFunction::
 ExtensionFunction::ResponseAction
 WebAuthenticationProxyCompleteIsUvpaaRequestFunction::Run() {
   DCHECK(extension());
-  auto params = api::web_authentication_proxy::CompleteIsUvpaaRequest::Params::
-      CreateDeprecated(args());
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+  auto params =
+      api::web_authentication_proxy::CompleteIsUvpaaRequest::Params::Create(
+          args());
+  EXTENSION_FUNCTION_VALIDATE(params);
   WebAuthenticationProxyService* proxy_service =
       WebAuthenticationProxyService::GetIfProxyAttached(browser_context());
   if (!proxy_service || proxy_service->GetActiveRequestProxy() != extension()) {
