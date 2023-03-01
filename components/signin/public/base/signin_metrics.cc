840d8c20740e69c02a2456a989fe0f6e030679fc
David Roger <droger@chromium.org>
2023-03-01 14:51:00 +0000

    [signin] Put RecordSigninAccountType() behind BUILDFLAG(IS_IOS)
    Bug: None
    Change-Id: I2c23e8479ea6c88d2ad94cad74ff7432706fdf19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295986
    Auto-Submit: David Roger <droger@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111580}

--- components/signin/public/base/signin_metrics.cc
+++ components/signin/public/base/signin_metrics.cc
@@ -281,6 +281,7 @@ void RecordRefreshTokenRevokedFromSource(
   UMA_HISTOGRAM_ENUMERATION("Signin.RefreshTokenRevoked.Source", source);
 }
 
+#if BUILDFLAG(IS_IOS)
 void RecordSigninAccountType(signin::ConsentLevel consent_level,
                              bool is_managed_account) {
   SigninAccountType account_type = is_managed_account
@@ -297,6 +298,7 @@ void RecordSigninAccountType(signin::ConsentLevel consent_level,
       break;
   }
 }
+#endif
 
 // --------------------------------------------------------------
 // User actions
--- components/signin/public/base/signin_metrics.h
+++ components/signin/public/base/signin_metrics.h
@@ -579,9 +579,11 @@ void RecordRefreshTokenUpdatedFromSource(bool refresh_token_is_valid,
 // Records the source that revoked a refresh token.
 void RecordRefreshTokenRevokedFromSource(SourceForRefreshTokenOperation source);
 
+#if BUILDFLAG(IS_IOS)
 // Records the account type when the user signs in.
 void RecordSigninAccountType(signin::ConsentLevel consent_level,
                              bool is_managed_account);
+#endif
 
 // -----------------------------------------------------------------------------
 // User actions
