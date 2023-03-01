a3109ef70593cffa92d5b9bb5cf3573276f13316
dpapad <dpapad@chromium.org>
2023-03-01 18:41:49 +0000

    [Reland] CrOS WebUI: Add ash/webui/common/resources:build_ts target.
    Original CL was reverted due to missing dependencies to the newly
    introduced target, as well as an incorrect path in the
    :generate_definitions target. Fixing both in this reland attempt.
    
    Removing previous ts_library targets
     - ash/webui/common/resources/typescript_utils:build_ts
     - ash/webui/common/resources/hotspot:build_ts
    
    This has a few benefits
     1) Makes it easier to migrate files under ash/webui/common/resources to
        Typescript.
     2) Prepares the ground for automatically adding ts_library()
        path_mappings for chrome://resources/ash/common/ URLs based on
        whether the ash/webui/common/resources:build_ts dependency exist,
        eliminating the chance of missing deps and flaky build errors, like
        the one at b/270458061.
    
    Bug: b/270458061
    Change-Id: I02d67af04e937dfdf1fa93a939ad20e4800c1b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295849
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111723}

--- ash/webui/common/resources/hotspot/fake_hotspot_config.ts
+++ ash/webui/common/resources/hotspot/fake_hotspot_config.ts
@@ -3,8 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {FakeMethodResolver} from 'chrome://resources/ash/common/fake_method_resolver.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
+
+import {FakeMethodResolver} from '../fake_method_resolver.js';
 
 import {CrosHotspotConfigInterface, CrosHotspotConfigObserverRemote, HotspotAllowStatus, HotspotConfig, HotspotControlResult, HotspotEnabledStateObserverRemote, HotspotInfo, HotspotState, SetHotspotConfigResult} from './cros_hotspot_config.mojom-webui.js';
 
