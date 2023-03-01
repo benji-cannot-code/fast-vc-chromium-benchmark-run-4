8b92aaf2b587b2e439d0808c28dc5b3fc21e898b
Marcello Salomao <msalomao@google.com>
2023-03-01 03:47:26 +0000

    [Mirror Sync] Report Sync Status Tracker memory usage
    This is useful to detect memory leaks and understand how
    much memory this new data structure consumes in the
    average and worst case scenarios.
    
    Bug: b:252902825
    Test: Manual
    Change-Id: Idb11e6b2c6661c40670406a7ef506c6f1f4f75c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290628
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111412}

--- chromeos/ash/components/drivefs/sync_status_tracker.cc
+++ chromeos/ash/components/drivefs/sync_status_tracker.cc
@@ -5,14 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/drivefs/sync_status_tracker.h"
 
+#include <math.h>
 #include <cstdint>
 #include <memory>
 #include <ranges>
 #include <utility>
 #include <vector>
 
+#include <base/logging.h>
 #include "base/containers/span.h"
 #include "base/files/file_path.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 
 namespace drivefs {
@@ -64,6 +67,8 @@ const std::vector<const SyncState> SyncStatusTracker::GetChangesAndClean() {
 
   std::vector<const SyncState> updated_sync_states;
 
+  int64_t total_mem_usage_in_bytes = 0;
+
   // Traverse trie.
   std::vector<Node*> stack = {root_.get()};
   while (!stack.empty()) {
@@ -73,6 +78,10 @@ const std::vector<const SyncState> SyncStatusTracker::GetChangesAndClean() {
     for (auto& child : node->children) {
       stack.emplace_back(child.second.get());
     }
+
+    total_mem_usage_in_bytes +=
+        sizeof(SyncStatusTracker::Node) +
+        node->path_part.size() * sizeof(base::FilePath::StringType::value_type);
 
     // Collect dirty nodes and flip them back to pristine.
     if (node->state.IsDirty()) {
@@ -89,6 +98,9 @@ const std::vector<const SyncState> SyncStatusTracker::GetChangesAndClean() {
   if (root_->children.empty()) {
     root_->state.Set(kNotFound, 0, 0);
   }
+
+  UMA_HISTOGRAM_MEMORY_KB("FileBrowser.SyncStatusTrackerMemoryUsage",
+                          total_mem_usage_in_bytes / 1024);
 
   return updated_sync_states;
 }
