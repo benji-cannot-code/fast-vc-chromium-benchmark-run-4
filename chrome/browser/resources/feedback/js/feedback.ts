1925518d838a0585664d587e4fae9ad42c0e9aaf
Sean Paul <seanpaul@chromium.org>
2023-03-01 18:52:53 +0000

    feedback: Add display-related questionnaires
    Add a questionnaire for Googler feedback resembling a display issue.
    
    Bug: b:269615874
    Test: browser_tests, --gtest_filter=OSFeedbackBrowserTest*
    Change-Id: I871e7b732333a35b9e2b067ca6ff10ad2bd9c8ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261312
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Sean Paul <seanpaul@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111729}

--- chrome/browser/resources/feedback/js/feedback.ts
+++ chrome/browser/resources/feedback/js/feedback.ts
@@ -158,6 +158,19 @@ const cellularRegEx: RegExp = buildWordMatcher([
 ]);
 
 /**
+ * Regular expression to check for display-related keywords.
+ */
+const displayRegEx = buildWordMatcher([
+  'display',
+  'displayport',
+  'dock',
+  'hdmi',
+  'monitor',
+  'panel',
+  'screen',
+]);
+
+/**
  * Regular expression to check for all strings indicating that a user can't
  * connect to a HID or Audio device. This is also a likely indication of a
  * Bluetooth related issue.
@@ -325,6 +338,10 @@ function checkForShowQuestionnaire(inputEvent: Event) {
 
   if (cellularRegEx.test(matchedText)) {
     toAppend.push(...domainQuestions['cellular']);
+  }
+
+  if (displayRegEx.test(matchedText)) {
+    toAppend.push(...domainQuestions['display']);
   }
 
   if (toAppend.length === 0) {
