d77f0d9dc9f6c649d6c2b752240212ef3cedd049
Peter Marshall <petermarshall@chromium.org>
2023-03-01 04:45:44 +0000

    [Office upload] Add animation to the move confirmation dialog
    Add a lottie animation and tweak the layout for the move
    confirmation dialog.
    
    Lottie requires a change to CSP setup.
    
    Bug: b:270095565
    Change-Id: Ia86cb8fb32dc8c0dbfabf01d574557f2d23e5774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4286197
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111430}

--- chrome/browser/resources/chromeos/cloud_upload/move_confirmation_page.ts
+++ chrome/browser/resources/chromeos/cloud_upload/move_confirmation_page.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
+import 'chrome://resources/cr_elements/cr_lottie/cr_lottie.js';
 
 import {UserAction} from './cloud_upload.mojom-webui.js';
 import {CloudUploadBrowserProxy} from './cloud_upload_browser_proxy.js';
@@ -76,6 +77,12 @@ export class MoveConfirmationPageElement extends HTMLElement {
     this.shadowRoot!.getElementById('app-name')!.innerText = appName;
     this.shadowRoot!.getElementById('provider-short-name')!.innerText =
         shortName;
+
+    const animationUrl = this.cloudProvider === CloudProvider.ONE_DRIVE ?
+        'move_confirmation_animation_onedrive.json' :
+        'move_confirmation_animation_drive.json';
+    this.shadowRoot!.querySelector('cr-lottie')!.setAttribute(
+        'animation-url', animationUrl);
   }
 
   private onMoveButtonClick(): void {
