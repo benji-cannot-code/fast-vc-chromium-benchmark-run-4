c5a8d7f14b077b1e294ebbfcf9ed275290ee6029
Gyuyoung Kim <gyuyoung@igalia.com>
2023-03-01 00:46:15 +0000

    Remove unnecessary header includes in //printing/metafile.h
    This CL removes uncessary CoreFoundation.h and
    base/mac/scoped_cftyperef.h header includes in
    /printing/metafile.h because there is no use in the file.
    
    Bug: 1415248
    Change-Id: Ia6193bc20c1c6cfe5fbde81d26638f5fc43f97ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290549
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1111311}

--- printing/metafile.h
+++ printing/metafile.h
@@ -20,9 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
 #elif BUILDFLAG(IS_APPLE)
-#include <CoreFoundation/CoreFoundation.h>
 #include <CoreGraphics/CoreGraphics.h>
-#include "base/mac/scoped_cftyperef.h"
 #endif
 
 namespace base {
