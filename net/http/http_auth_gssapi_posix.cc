fc91fbb2d81f438b551a293ceb03df611b16f183
slutskii <slutskii@google.com>
2023-03-01 23:46:06 +0000

    Allow external credentials for Negotiate on ChromeOS
    A new Kerberos (which is an implementation of Negotiate) feature on
    ChromeOS suggests user to enter their credentials to obtain a new
    TGT in the cases where initial authentication failed. Right now,
    whenether the failure happens, browser switches to another
    less secure authentication option (for example NTLM), because
    the GSSAPI library, which is used for checking the TGT, does not
    allow external credentials. This CL changes it for ChromeOS builds.
    Note that the credentials are NOT passed to browser/GSSAPI in any
    form, everything is handled on the OS level.
    
    Bug: b:260522530
    Change-Id: I450a1f5edbff0a891fafb796a188eb4b16cff54a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291014
    Commit-Queue: Michael Slutskii <slutskii@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111911}

--- net/http/http_auth_gssapi_posix.cc
+++ net/http/http_auth_gssapi_posix.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/compiler_specific.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/format_macros.h"
 #include "base/logging.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/values.h"
 #include "build/build_config.h"
+#include "net/base/features.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_auth.h"
 #include "net/http/http_auth_gssapi_posix.h"
@@ -665,7 +667,15 @@ bool HttpAuthGSSAPI::NeedsIdentity() const {
 }
 
 bool HttpAuthGSSAPI::AllowsExplicitCredentials() const {
+#if BUILDFLAG(IS_CHROMEOS)
+  if (base::FeatureList::IsEnabled(features::kKerberosInBrowserRedirect)) {
+    return true;
+  } else {
+    return false;
+  }
+#else
   return false;
+#endif
 }
 
 void HttpAuthGSSAPI::SetDelegation(DelegationType delegation_type) {
