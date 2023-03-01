addb0ac7ae41c2035195d461eefc46f15917ebc2
Peter Boström <pbos@chromium.org>
2023-03-01 00:00:38 +0000

    Use DumpWithoutCrashing() in LogInvalidEnumValue()
    This replaces an existing NOTREACHED() with non-fatal crash dumping that
    includes the failing data to help debug misconfigurations remotely.
    
    Note that this makes us generate crash dumps when misconfiguring
    non-DCHECK channels, so we can detect bad configs sent to Dev, Beta and
    Stable channels as well.
    
    Bug: 1419580
    Change-Id: Iaee65083e8f100d9511522c33b7fc3c192ed7e76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295575
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111271}

--- base/metrics/field_trial_params.cc
+++ base/metrics/field_trial_params.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/field_trial_param_associator.h"
@@ -21,6 +23,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace base {
+
+void LogInvalidValue(const Feature& feature,
+                     const char* type,
+                     const std::string& param_name,
+                     const std::string& value_as_string,
+                     const std::string& default_value_as_string) {
+  // To anyone noticing these crash dumps in the wild, these parameters come
+  // from server-side experiment confiuration. If you're seeing an increase it
+  // is likely due to a bad experiment rollout rather than changes in the client
+  // code.
+  SCOPED_CRASH_KEY_STRING32("FieldTrialParams", "feature_name", feature.name);
+  SCOPED_CRASH_KEY_STRING32("FieldTrialParams", "param_name", param_name);
+  SCOPED_CRASH_KEY_STRING32("FieldTrialParams", "value", value_as_string);
+  SCOPED_CRASH_KEY_STRING32("FieldTrialParams", "default",
+                            default_value_as_string);
+  LOG(ERROR) << "Failed to parse field trial param " << param_name
+             << " with string value " << value_as_string << " under feature "
+             << feature.name << " into " << type
+             << ". Falling back to default value of "
+             << default_value_as_string;
+  base::debug::DumpWithoutCrashing();
+}
 
 std::string UnescapeValue(const std::string& value) {
   return UnescapeURLComponent(
@@ -135,11 +159,8 @@ int GetFieldTrialParamByFeatureAsInt(const Feature& feature,
   int value_as_int = 0;
   if (!StringToInt(value_as_string, &value_as_int)) {
     if (!value_as_string.empty()) {
-      NOTREACHED() << "Failed to parse field trial param " << param_name
-                   << " with string value " << value_as_string
-                   << " under feature " << feature.name
-                   << " into an int. Falling back to default value of "
-                   << default_value;
+      LogInvalidValue(feature, "an int", param_name, value_as_string,
+                      base::NumberToString(default_value));
     }
     value_as_int = default_value;
   }
@@ -154,11 +175,8 @@ double GetFieldTrialParamByFeatureAsDouble(const Feature& feature,
   double value_as_double = 0;
   if (!StringToDouble(value_as_string, &value_as_double)) {
     if (!value_as_string.empty()) {
-      NOTREACHED() << "Failed to parse field trial param " << param_name
-                   << " with string value " << value_as_string
-                   << " under feature " << feature.name
-                   << " into a double. Falling back to default value of "
-                   << default_value;
+      LogInvalidValue(feature, "a double", param_name, value_as_string,
+                      base::NumberToString(default_value));
     }
     value_as_double = default_value;
   }
@@ -176,11 +194,8 @@ bool GetFieldTrialParamByFeatureAsBool(const Feature& feature,
     return false;
 
   if (!value_as_string.empty()) {
-    NOTREACHED() << "Failed to parse field trial param " << param_name
-                 << " with string value " << value_as_string
-                 << " under feature " << feature.name
-                 << " into a bool. Falling back to default value of "
-                 << default_value;
+    LogInvalidValue(feature, "a bool", param_name, value_as_string,
+                    default_value ? "true" : "false");
   }
   return default_value;
 }
@@ -197,11 +212,8 @@ base::TimeDelta GetFieldTrialParamByFeatureAsTimeDelta(
 
   absl::optional<base::TimeDelta> ret = TimeDeltaFromString(value_as_string);
   if (!ret.has_value()) {
-    NOTREACHED() << "Failed to parse field trial param " << param_name
-                 << " with string value " << value_as_string
-                 << " under feature " << feature.name
-                 << " into a base::TimeDelta. Falling back to default value of "
-                 << default_value;
+    LogInvalidValue(feature, "a base::TimeDelta", param_name, value_as_string,
+                    base::NumberToString(default_value.InSecondsF()) + " s");
     return default_value;
   }
 
@@ -233,11 +245,8 @@ void LogInvalidEnumValue(const Feature& feature,
                          const std::string& param_name,
                          const std::string& value_as_string,
                          int default_value_as_int) {
-  NOTREACHED() << "Failed to parse field trial param " << param_name
-               << " with string value " << value_as_string << " under feature "
-               << feature.name
-               << " into an enum. Falling back to default value of "
-               << default_value_as_int;
+  LogInvalidValue(feature, "an enum", param_name, value_as_string,
+                  base::NumberToString(default_value_as_int));
 }
 
 }  // namespace base
