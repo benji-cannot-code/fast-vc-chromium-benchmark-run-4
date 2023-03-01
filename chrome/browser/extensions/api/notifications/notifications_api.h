ce17e15dc0394505d1ff530a1d623fa946a15a4f
Claudio DeSouza <cdesouza@igalia.com>
2023-03-01 17:23:41 +0000

    [json-schema-compiler] Refactor to Params::Create (31/74)
    This CL removes the use of Params::CreateDeprecated, replacing it with
    the modernised Params::Create.
    
    Changes for path:
      /chrome/browser/extensions/api/notifications
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: 1354063, 1414632
    Change-Id: I41b6d465320d722d3e0ad4b26acd6e8907299368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299990
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111657}

--- chrome/browser/extensions/api/notifications/notifications_api.h
+++ chrome/browser/extensions/api/notifications/notifications_api.h
@@ -75,7 +75,7 @@ class NotificationsCreateFunction : public NotificationsApiFunction {
   ~NotificationsCreateFunction() override;
 
  private:
-  std::unique_ptr<api::notifications::Create::Params> params_;
+  absl::optional<api::notifications::Create::Params> params_;
 
   DECLARE_EXTENSION_FUNCTION("notifications.create", NOTIFICATIONS_CREATE)
 };
@@ -91,7 +91,7 @@ class NotificationsUpdateFunction : public NotificationsApiFunction {
   ~NotificationsUpdateFunction() override;
 
  private:
-  std::unique_ptr<api::notifications::Update::Params> params_;
+  absl::optional<api::notifications::Update::Params> params_;
 
   DECLARE_EXTENSION_FUNCTION("notifications.update", NOTIFICATIONS_UPDATE)
 };
@@ -107,7 +107,7 @@ class NotificationsClearFunction : public NotificationsApiFunction {
   ~NotificationsClearFunction() override;
 
  private:
-  std::unique_ptr<api::notifications::Clear::Params> params_;
+  absl::optional<api::notifications::Clear::Params> params_;
 
   DECLARE_EXTENSION_FUNCTION("notifications.clear", NOTIFICATIONS_CLEAR)
 };
