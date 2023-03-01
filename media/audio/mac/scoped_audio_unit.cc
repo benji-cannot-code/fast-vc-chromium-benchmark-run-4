88fa073e5baf9c6006adca45c206bdcdd9035f51
Abhijeet Kandalkar <abhijeet@igalia.com>
2023-03-01 17:54:23 +0000

    [ios] Audio Manager code cleanup
    Bug: 1413450
    Change-Id: I5a3c9e5c93b5f8e4a3002819d81624160ca3a605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297225
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1111689}

--- media/audio/mac/scoped_audio_unit.cc
+++ media/audio/mac/scoped_audio_unit.cc
@@ -9,14 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-constexpr AudioComponentDescription desc = {kAudioUnitType_Output,
-#if BUILDFLAG(IS_MAC)
-                                            kAudioUnitSubType_HALOutput,
-#else
-                                            kAudioUnitSubType_RemoteIO,
-#endif
-                                            kAudioUnitManufacturer_Apple, 0, 0};
-
 static void DestroyAudioUnit(AudioUnit audio_unit) {
   OSStatus result = AudioUnitUninitialize(audio_unit);
   OSSTATUS_DLOG_IF(ERROR, result != noErr, result)
@@ -27,6 +19,18 @@ static void DestroyAudioUnit(AudioUnit audio_unit) {
 }
 
 ScopedAudioUnit::ScopedAudioUnit(AudioDeviceID device, AUElement element) {
+  constexpr AudioComponentDescription desc = {
+    kAudioUnitType_Output,
+#if BUILDFLAG(IS_MAC)
+    kAudioUnitSubType_HALOutput,
+#else
+    kAudioUnitSubType_RemoteIO,  // for iOS
+#endif
+    kAudioUnitManufacturer_Apple,
+    0,
+    0
+  };
+
   AudioComponent comp = AudioComponentFindNext(0, &desc);
   if (!comp)
     return;
