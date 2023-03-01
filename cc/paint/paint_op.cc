b07c86babdb7fdaa6481a1df177b6cadeacd3f26
Scott Violet <sky@chromium.org>
2023-03-01 00:34:18 +0000

    cc: adds ability for DrawLineOp to raster as a path
    This patch does two things:
    
    1. Adds the ability for DrawLineOp to use drawPath() (via a boolean).
    2. Adds logic to RecordPaintCanvas to make use of (1).
    
    DrawLineOp is much easier to serialize (and keeps around less memory)
    then SkPath. OTOH, using SkCanvas::drawLine() may result in batching
    being turned off in the gpu when rastering. For this reason this adds
    logic to RecordPaintCanvas such that DrawLineOp may raster as a
    path if there have been a number of recent drawpaths. I fully expect
    the exact specifics of when drawline should raster as a path to
    change, this is a good start though. By good start I means it improves
    the benchmark that draws a lot of lines, while not regressing the one
    that draws a lot of paths (much).
    
    Bug: 1420380
    Change-Id: I3cbaac343b92f047004936f940ac34eee54c52f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296617
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111300}

--- cc/paint/paint_op.cc
+++ cc/paint/paint_op.cc
@@ -542,6 +542,7 @@ void DrawLineOp::Serialize(PaintOpWriter& writer,
   writer.Write(y0);
   writer.Write(x1);
   writer.Write(y1);
+  writer.Write(draw_as_path);
 }
 
 void DrawOvalOp::Serialize(PaintOpWriter& writer,
@@ -862,6 +863,7 @@ PaintOp* DrawLineOp::Deserialize(PaintOpReader& reader, void* output) {
   reader.Read(&op->y0);
   reader.Read(&op->x1);
   reader.Read(&op->y1);
+  reader.Read(&op->draw_as_path);
   return op;
 }
 
@@ -1286,9 +1288,15 @@ void DrawLineOp::RasterWithFlags(const DrawLineOp* op,
                                  const PaintFlags* flags,
                                  SkCanvas* canvas,
                                  const PlaybackParams& params) {
-  SkPaint paint = flags->ToSkPaint();
   flags->DrawToSk(canvas, [op](SkCanvas* c, const SkPaint& p) {
-    c->drawLine(op->x0, op->y0, op->x1, op->y1, p);
+    if (op->draw_as_path) {
+      SkPath path;
+      path.moveTo(op->x0, op->y0);
+      path.lineTo(op->x1, op->y1);
+      c->drawPath(path, p);
+    } else {
+      c->drawLine(op->x0, op->y0, op->x1, op->y1, p);
+    }
   });
 }
 
