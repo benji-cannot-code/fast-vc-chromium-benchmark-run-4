8fd00d58a31c551654a258b375922b241d67c702
Longbo Wei <longbowei@google.com>
2023-03-01 22:24:06 +0000

    shortcuts: Remove the special key section.
    * Remove six-pack-keys and re-enable accelerators that previously
      conflict with the six-pack-keys
    
    Before: http://screen/5CfPHGiR4wW22pN
    After: http://screen/9wWho84UWjmwp3m
    
    Bug: b:216049298
    Test: ash_webui_unittests AcceleratorConfigurationProviderTest
    Change-Id: Ia2229b69fae59b3fdb713f35a991a83507946b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4298385
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111859}

--- ash/webui/shortcut_customization_ui/resources/js/fake_data.ts
+++ ash/webui/shortcut_customization_ui/resources/js/fake_data.ts
@@ -65,25 +65,6 @@ const cycleTabsAccelerator: MojoAcceleratorInfo = {
   },
 };
 
-const sixPackDeleteAccelerator: MojoAcceleratorInfo = {
-  type: AcceleratorType.kDefault,
-  state: AcceleratorState.kEnabled,
-  locked: true,
-  layoutProperties: {
-    standardAccelerator: {
-      keyDisplay: stringToMojoString16('backspace'),
-      accelerator: {
-        modifiers: Modifier.COMMAND,
-        keyCode: 8,
-        keyState: 0,
-        timeStamp: fakeTimestamp,
-      },
-    },
-    textAccelerator: undefined,
-
-  },
-};
-
 export const fakeAcceleratorConfig: MojoAcceleratorConfig = {
   [AcceleratorSource.kAsh]: {
     // Snap Window Left
@@ -167,7 +148,6 @@ export const fakeAcceleratorConfig: MojoAcceleratorConfig = {
     // New Tab
     [0]: [newTabAccelerator],
     [1]: [cycleTabsAccelerator],
-    [2]: [sixPackDeleteAccelerator],
   },
 };
 
@@ -175,7 +155,6 @@ export const fakeAmbientConfig: MojoAcceleratorConfig = {
   [AcceleratorSource.kAmbient]: {
     [0]: [newTabAccelerator],
     [1]: [cycleTabsAccelerator],
-    [2]: [sixPackDeleteAccelerator],
   },
 };
 
@@ -227,14 +206,6 @@ export const fakeLayoutInfo: MojoLayoutInfo[] = [
     style: LayoutStyle.kText,
     source: AcceleratorSource.kAmbient,
     action: 1,
-  },
-  {
-    category: AcceleratorCategory.kEventRewriter,
-    subCategory: AcceleratorSubcategory.kSixPackKeys,
-    description: stringToMojoString16('Delete'),
-    style: LayoutStyle.kDefault,
-    source: AcceleratorSource.kAmbient,
-    action: 2,
   },
 ];
 
