7d101e87ee48536b8897ae284efaba7a27545af4
Brahim Chikhaoui <bchikhaoui@google.com>
2023-03-01 14:19:50 +0000

    [OOBE]:  Add Skip Button to GestureNavigationScreen
    Add Skip option in tablet gesture education module in OOBE
    And add a new browser test for the new button,
    
    Bug: b:268398421
    Change-Id: I205cd66a9778aad1c30fff4e0f0c6ee522466054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296535
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111570}

--- chrome/browser/resources/chromeos/login/screens/common/gesture_navigation.js
+++ chrome/browser/resources/chromeos/login/screens/common/gesture_navigation.js
@@ -31,6 +31,16 @@ const GesturePage = {
 };
 
 /**
+ * Available user actions.
+ * @enum {string}
+ */
+const UserAction = {
+  SKIP: 'skip',
+  EXIT: 'exit',
+};
+
+
+/**
  * @constructor
  * @extends {PolymerElement}
  * @implements {LoginScreenBehaviorInterface}
@@ -75,6 +85,14 @@ class GestureNavigation extends GestureScreenElementBase {
   }
 
   /**
+   * This is the 'on-tap' event handler for the skip button.
+   * @private
+   */
+  onSkip_() {
+    this.userActed(UserAction.SKIP);
+  }
+
+  /**
    * This is the 'on-tap' event handler for the 'next' or 'get started' button.
    * @private
    */
@@ -94,7 +112,7 @@ class GestureNavigation extends GestureScreenElementBase {
         // report exit. Keep the currentPage_ value so the UI does not get
         // updated until the next screen is shown.
         this.setPlayCurrentScreenAnimation(false);
-        this.userActed('exit');
+        this.userActed(UserAction.EXIT);
         break;
     }
   }
