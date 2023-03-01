8a13f34ec359d5b1665454cc94a488446dddee47
Takumi Fujimoto <takumif@chromium.org>
2023-03-01 00:12:26 +0000

    Revert "Roll src/third_party/dav1d/libdav1d/ ed63a7459..0d6a31f27 (1 commit)"
    This reverts commit 5267fe3ec8dd1ed6500be9bb3fd183aad3c0fc96.
    
    Reason for revert: Causing bot failures e.g.
    https://ci.chromium.org/ui/p/chromium/builders/ci/MSAN%20Release%20(no%20origins)/291218/overview
    
    Original change's description:
    > Roll src/third_party/dav1d/libdav1d/ ed63a7459..0d6a31f27 (1 commit)
    >
    > https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/ed63a7459376..0d6a31f275dc
    >
    > $ git log ed63a7459..0d6a31f27 --date=short --no-merges --format='%ad %ae %s'
    > 2023-01-12 gramner x86: Remove stack alignment compiler flags
    >
    > Created with:
    >   roll-dep src/third_party/dav1d/libdav1d
    >
    > Also port the following meson build system changes to our build system.
    > 1. Delete the stackalign_flag and stackrealign_flag variables.
    > 2. Simplify the assignment of the stack_alignment variable.
    > 3. Delete libdav1d_entrypoints_sources. Move lib.c and thread_task.c to
    >    libdav1d_sources.
    >
    > Change-Id: I1df8c3b6543edc48a9345428d68f278e1f131f77
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288936
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Wan-Teh Chang <wtc@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1111123}
    
    Change-Id: I53875c908731ced2e180aab0848a9b0bda3b9493
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296839
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Quick-Run: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111282}

--- third_party/dav1d/config/win/x64/config.h
+++ third_party/dav1d/config/win/x64/config.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define HAVE_IO_H 1
 
+// #define STACK_ALIGNMENT 16 -- Stack alignment is controlled by Chromium
+
 #define TRIM_DSP_FUNCTIONS 1
 
 #define UNICODE 1
--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.0.0-106-g0d6a31f"
+#define DAV1D_VERSION "1.0.0-105-ged63a74"
