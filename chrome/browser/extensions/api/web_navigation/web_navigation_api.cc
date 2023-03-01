c9089aece6695439f60e26d10245bac200579b76
Claudio DeSouza <cdesouza@igalia.com>
2023-03-01 21:28:59 +0000

    [json-schema-compiler] Refactor to Params::Create (44/74)
    This CL removes the use of Params::CreateDeprecated, replacing it with
    the modernised Params::Create.
    
    Changes for path:
      /chrome/browser/extensions/api/web_navigation
    
    This CL was uploaded by git cl split.
    
    R=alexmos@chromium.org
    
    Bug: 1354063, 1414632
    Change-Id: I256eda66a35b464dc82e9d65956a3a6a7bd513bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300012
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111826}

--- chrome/browser/extensions/api/web_navigation/web_navigation_api.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_api.cc
@@ -447,9 +447,8 @@ void WebNavigationTabObserver::RenderFrameHostPendingDeletion(
 }
 
 ExtensionFunction::ResponseAction WebNavigationGetFrameFunction::Run() {
-  std::unique_ptr<GetFrame::Params> params(
-      GetFrame::Params::CreateDeprecated(args()));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+  absl::optional<GetFrame::Params> params = GetFrame::Params::Create(args());
+  EXTENSION_FUNCTION_VALIDATE(params);
 
   int tab_id = api::tabs::TAB_ID_NONE;
   int frame_id = -1;
@@ -470,7 +469,7 @@ ExtensionFunction::ResponseAction WebNavigationGetFrameFunction::Run() {
             document_id);
 
     if (!render_frame_host)
-      return RespondNow(OneArgument(base::Value()));
+      return RespondNow(WithArguments(base::Value()));
 
     content::WebContents* web_contents =
         content::WebContents::FromRenderFrameHost(render_frame_host);
@@ -478,7 +477,7 @@ ExtensionFunction::ResponseAction WebNavigationGetFrameFunction::Run() {
     // see if the WebContents is actually in our BrowserContext.
     if (!ExtensionTabUtil::IsWebContentsInContext(
             web_contents, browser_context(), include_incognito_information())) {
-      return RespondNow(OneArgument(base::Value()));
+      return RespondNow(WithArguments(base::Value()));
     }
 
     tab_id = ExtensionTabUtil::GetTabId(web_contents);
@@ -488,7 +487,7 @@ ExtensionFunction::ResponseAction WebNavigationGetFrameFunction::Run() {
     // return.
     if ((params->details.tab_id && *params->details.tab_id != tab_id) ||
         (params->details.frame_id && *params->details.frame_id != frame_id)) {
-      return RespondNow(OneArgument(base::Value()));
+      return RespondNow(WithArguments(base::Value()));
     }
   } else {
     // If documentId is not provided, tab_id and frame_id must be. Return early
@@ -506,7 +505,7 @@ ExtensionFunction::ResponseAction WebNavigationGetFrameFunction::Run() {
                                       include_incognito_information(),
                                       &web_contents) ||
         !web_contents) {
-      return RespondNow(OneArgument(base::Value()));
+      return RespondNow(WithArguments(base::Value()));
     }
 
     render_frame_host = ExtensionApiFrameIdMap::Get()->GetRenderFrameHostById(
@@ -518,11 +517,11 @@ ExtensionFunction::ResponseAction WebNavigationGetFrameFunction::Run() {
           ? FrameNavigationState::GetForCurrentDocument(render_frame_host)
           : nullptr;
   if (!frame_navigation_state)
-    return RespondNow(OneArgument(base::Value()));
+    return RespondNow(WithArguments(base::Value()));
 
   GURL frame_url = frame_navigation_state->GetUrl();
   if (!FrameNavigationState::IsValidUrl(frame_url))
-    return RespondNow(OneArgument(base::Value()));
+    return RespondNow(WithArguments(base::Value()));
 
   GetFrame::Results::Details frame_details;
   frame_details.url = frame_url.spec();
@@ -547,9 +546,9 @@ ExtensionFunction::ResponseAction WebNavigationGetFrameFunction::Run() {
 }
 
 ExtensionFunction::ResponseAction WebNavigationGetAllFramesFunction::Run() {
-  std::unique_ptr<GetAllFrames::Params> params(
-      GetAllFrames::Params::CreateDeprecated(args()));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+  absl::optional<GetAllFrames::Params> params =
+      GetAllFrames::Params::Create(args());
+  EXTENSION_FUNCTION_VALIDATE(params);
   int tab_id = params->details.tab_id;
 
   content::WebContents* web_contents = nullptr;
@@ -557,7 +556,7 @@ ExtensionFunction::ResponseAction WebNavigationGetAllFramesFunction::Run() {
                                     include_incognito_information(),
                                     &web_contents) ||
       !web_contents) {
-    return RespondNow(OneArgument(base::Value()));
+    return RespondNow(WithArguments(base::Value()));
   }
 
   std::vector<GetAllFrames::Results::DetailsType> result_list;
