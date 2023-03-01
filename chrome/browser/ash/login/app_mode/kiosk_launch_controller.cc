3d3ea1c5f79ecf6722448f4b6cdc5877178513f2
Jana Grill <janagrill@google.com>
2023-03-01 17:11:58 +0000

    Trigger Lacros backward migration for kiosk
    Re-introduce the code that triggers Lacros backward migration for kiosk.
    
    Bug: b/271261281
    Change-Id: Ie12eb614b52d116dd54cad41f5ae73718d876819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299351
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Jana Grill <janagrill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111650}

--- chrome/browser/ash/login/app_mode/kiosk_launch_controller.cc
+++ chrome/browser/ash/login/app_mode/kiosk_launch_controller.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_launcher.h"
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_manager.h"
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_service_launcher.h"
+#include "chrome/browser/ash/crosapi/browser_data_back_migrator.h"
 #include "chrome/browser/ash/crosapi/browser_data_migrator.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/login/app_mode/force_install_observer.h"
@@ -332,11 +333,20 @@ void KioskLaunchController::OnProfileLoaded(Profile* profile) {
   // TODO(b/257210467): Remove the need for CHECK_IS_TEST
   if (!user) {
     CHECK_IS_TEST();
-  } else if (BrowserDataMigratorImpl::MaybeRestartToMigrate(
-                 user->GetAccountId(), user->username_hash(),
-                 crosapi::browser_util::PolicyInitState::kAfterInit)) {
-    LOG(WARNING) << "Restarting chrome to run profile migration.";
-    return;
+  } else {
+    if (BrowserDataMigratorImpl::MaybeRestartToMigrate(
+            user->GetAccountId(), user->username_hash(),
+            crosapi::browser_util::PolicyInitState::kAfterInit)) {
+      LOG(WARNING) << "Restarting chrome to run profile migration.";
+      return;
+    }
+
+    if (BrowserDataBackMigrator::MaybeRestartToMigrateBack(
+            user->GetAccountId(), user->username_hash(),
+            crosapi::browser_util::PolicyInitState::kAfterInit)) {
+      LOG(WARNING) << "Restarting chrome to run backward profile migration.";
+      return;
+    }
   }
 
   // This is needed to trigger input method extensions being loaded.
