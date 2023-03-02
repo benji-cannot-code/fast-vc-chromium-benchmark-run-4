fa446200a554ae22602d13d9af374b937eb42734
Ewann Pelle <ewannpv@chromium.org>
2023-03-02 14:33:35 +0000

    [iOS][PT] Remove drop animation
    Change-Id: I81e43c7dec5a4b637a1d0ace5d9cd8d0d567eba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4301373
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112197}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.mm
@@ -550,16 +550,10 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 
     NSIndexPath* dropIndexPath = CreateIndexPath(destinationIndex);
     // Drop synchronously if local object is available.
-    __weak __typeof(self) weakSelf = self;
     if (item.dragItem.localObject) {
-      _dropAnimationInProgress = YES;
-      [[coordinator dropItem:item.dragItem toItemAtIndexPath:dropIndexPath]
-          addCompletion:^(UIViewAnimatingPosition finalPosition) {
-            [weakSelf dropAnimationDidEnd];
-          }];
-
       // The sourceIndexPath is non-nil if the drop item is from this same
       // collection view.
+      _dropAnimationInProgress = YES;
       [self.dragDropHandler dropItem:item.dragItem
                              toIndex:destinationIndex
                   fromSameCollection:(item.sourceIndexPath != nil)];
@@ -782,6 +776,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
   // disappear from the user's sight.
   [self scrollCollectionViewToLastItemAnimated:YES];
 
+  [self dropAnimationDidEnd];
   [self pinnedTabsAvailable:_available];
 }
 
