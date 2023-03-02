f333b298bae4a767425044d2ecff3c54b5ea2e69
Mahmoud Rashad <mmrashad@google.com>
2023-03-02 12:39:38 +0000

    Revert "WebNN: Continue moving generic end2end tests to MLGraphTestBase"
    This reverts commit 72fa141d4dd397dc0053680da57b7b52faaafa2d.
    
    Reason for revert: I suspect this change caused the consistent failures on builder "chromium/ci/linux-ubsan-vptr" (crbug.com/1420937).
    
    Original change's description:
    > WebNN: Continue moving generic end2end tests to MLGraphTestBase
    >
    > The CL-4236177 [1] has extracted elementwise binary operator into
    > MLGraphTestBase, this CL continue doing for relu, clamp, conv2d, gemm,
    > hardswish, pool2d and reshape operator.
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/4236177
    >
    > Bug: 1273291
    > Change-Id: I5f05601bcc38bfe8378c598d3c4e1f54209205eb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4277630
    > Reviewed-by: Jiewei Qian <qjw@chromium.org>
    > Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1112023}
    
    Bug: 1273291, 1420937
    Change-Id: I42e7b3c9c64e1824163efdf27a079c56b81afa4d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303310
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mahmoud Rashad <mmrashad@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112154}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h
@@ -22,16 +22,12 @@ class V8TestingScope;
 
 // The utility methods for graph test.
 enum ExecutionMode { kAsync, kSync };
-// The backends share the unit tests in the MLGraphTest.
-enum BackendType { kFake, kXnnpack };
 
-using TestVariety = std::tuple<BackendType, ExecutionMode>;
-
-std::string TestVarietyToString(
-    const ::testing::TestParamInfo<TestVariety>& info);
+std::string ExecutionModeParamToString(
+    const ::testing::TestParamInfo<ExecutionMode>& execution_mode);
 
 class MLGraphTestBase : public ::testing::Test,
-                        public ::testing::WithParamInterface<TestVariety> {
+                        public ::testing::WithParamInterface<ExecutionMode> {
  public:
   // BuildResult is returned by Build() method. Only one member of BuildResult
   // is valid. If the graph building is successful, graph points to the MLGraph
@@ -41,8 +37,6 @@ class MLGraphTestBase : public ::testing::Test,
     Persistent<MLGraph> graph;
     Persistent<DOMException> exception;
   };
-
-  ExecutionMode GetExecutionMode();
 
   // Helper method for testing both BuildAsyncImpl() and BuildSyncImpl() with
   // the same named operands and expected results.
@@ -59,6 +53,11 @@ class MLGraphTestBase : public ::testing::Test,
                              MLGraph* graph,
                              MLNamedArrayBufferViews& inputs,
                              MLNamedArrayBufferViews& outputs);
+
+  // Test operations with different parameters such as tensor dimensions, data
+  // layout. Each test case will builds a graph and computes it with input data
+  // to check the expected value.
+  void TestElementWiseBinary(V8TestingScope& scope);
 };
 
 template <typename T>
