f555b8fe3fdec7f74984eccf1150fa9f17c005e9
Jimmy <jimmyxgong@chromium.org>
2023-03-02 23:04:41 +0000

    ash/webui: Remove tsconfig_base.json references
    Per crrev/c/4296556, the tsconfig_base.json is no longer
    required to be explicitly created and is removed. This
    also unblocks trying to upload any TS changes to the
    affected directories.
    
    Bug: None
    Test: None
    Change-Id: I26b229ed0d274e4843939d03a0fc658009c25741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299818
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Auto-Submit: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112503}

--- ash/webui/diagnostics_ui/resources/.eslintrc.js
+++ ash/webui/diagnostics_ui/resources/.eslintrc.js
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // clang-format off
 module.exports = {
     'ignorePatterns': ['**/*.js'],
-    'parserOptions': {
-      'project': ['./tsconfig_base.json'],
-    },
     'rules': {
       '@typescript-eslint/explicit-function-return-type': ['error'],
     },
--- ash/webui/print_management/resources/.eslintrc.js
+++ ash/webui/print_management/resources/.eslintrc.js
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Disable clang-format because it produces odd formatting.
 // clang-format off
 module.exports = {
-  'parserOptions': {
-    'project': ['./tsconfig_base.json'],
-  },
   'rules': {
     '@typescript-eslint/explicit-function-return-type': ['error'],
   },
