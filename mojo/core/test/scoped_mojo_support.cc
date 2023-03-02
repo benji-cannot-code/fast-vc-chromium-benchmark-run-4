8bc942c7034145da1066f2d819d89bf7aeccb245
Daniel Cheng <dcheng@chromium.org>
2023-03-02 23:12:50 +0000

    Add a crash key to log the interface name when an endpoint closes.
    This is intended to help narrow down what is causing an increase in
    crashes with NotifyError() on the stack. Due to the magic of tail call
    optimizations, the actual responsible stack frames are not actually
    present in the crash report.
    
    Bug: 1414218
    Change-Id: I48fc12c61d85afeca0b60cc3bfae0f95f75e7264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297334
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112506}

--- mojo/core/test/scoped_mojo_support.cc
+++ mojo/core/test/scoped_mojo_support.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/test/bind.h"
 #include "base/test/test_io_thread.h"
+#include "components/crash/core/common/crash_key.h"
 #include "mojo/core/embedder/configuration.h"
 #include "mojo/core/embedder/embedder.h"
 #include "mojo/core/embedder/scoped_ipc_support.h"
@@ -39,6 +40,8 @@ class TestSupportInitializer {
 class ScopedMojoSupport::CoreInstance {
  public:
   CoreInstance() {
+    crash_reporter::InitializeCrashKeys();
+
     mojo::core::Configuration mojo_config;
 
     // A relatively low limit to make it easier to test behavior at the limit.
