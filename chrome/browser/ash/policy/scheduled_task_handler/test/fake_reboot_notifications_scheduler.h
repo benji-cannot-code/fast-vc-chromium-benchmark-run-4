3beef3aeaaca0f9f1c0f62bcf227fa6b0b3bcd61
Artem Sumaneev <asumaneev@google.com>
2023-03-02 11:17:22 +0000

    Handle pending reboot notifications
    go/cros-reboot-command-dd
    
    Before the change, RebootNotificationsScheduler worked only with the
    first scheduled notifications request, all the following requests were
    ignored.
    
    Now the scheduler has a pending notification requests queue. If ongoing
    notification is reset, one of the pending requests is picked to take
    place if preset. The requests are ordered by reboot time with the
    earliest being top one.
    
    The queue is designed to handle simultaneous notifications from the
    reboot remote command and the DeviceScheduledReboot policy.
    
          DeviceCommandRebootJobWithScheduledRebootPolicyTest.*
    
    Bug: b:225913691
    Test: unit_tests --gtest_filter=\
    Test: unit_tests --gtest_filter=RebootNotificationsSchedulerTest.*
    Change-Id: I2a4f77575af69ca129edbd1476a91bfe1a1bffc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4132853
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112131}

--- chrome/browser/ash/policy/scheduled_task_handler/test/fake_reboot_notifications_scheduler.h
+++ chrome/browser/ash/policy/scheduled_task_handler/test/fake_reboot_notifications_scheduler.h
@@ -34,6 +34,9 @@ class FakeRebootNotificationsScheduler : public RebootNotificationsScheduler {
   void SimulateRebootButtonClick();
   void SetWaitFullRestoreInit(bool should_wait);
 
+  using RebootNotificationsScheduler::GetCurrentRequesterForTesting;
+  using RebootNotificationsScheduler::GetRequestersForTesting;
+
  private:
   void MaybeShowPendingRebootNotification() override;
 
