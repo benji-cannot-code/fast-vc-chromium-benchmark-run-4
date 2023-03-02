bcce1fee01d87b60359ef35f2976e7ada8e26181
Michael Ershov <miersh@google.com>
2023-03-02 09:53:27 +0000

    OwnerKeyLoader: Don't check install attributes
    Install attributes on consumer devices proved to be not
    the most reliable way to detect the state of a device,
    especially considering edge cases.
    
    Switch back to using LocalState prefs to detect the first
    sign in. Remove InstallAttributes::IsFirstSignIn() method
    that was introduced earlier for that and is now unused.
    
    In OwnerKeyLoader prefer to rely on device policies. When
    not available use "owner account" pref that OwnerKeyLoader
    now creates itself. When not available, rely on the
    pre-existing method to detect first sign-in.
    
    Bug: b:244407123
    Test: OwnerKeyLoader* unit tests, session.* tast tests
    Change-Id: I269ab4cd273e86859404b70358f5fa88951b2a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4293111
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112108}

--- chrome/browser/ash/ownership/owner_key_loader.cc
+++ chrome/browser/ash/ownership/owner_key_loader.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_is_test.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/ash/ownership/ownership_histograms.h"
+#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
 #include "chrome/browser/net/nss_service.h"
 #include "chrome/browser/net/nss_service_factory.h"
@@ -128,8 +129,9 @@ void GetCertDbAndPostOnWorkerThreadOnIO(
 
   net::NSSCertDatabase* database =
       std::move(nss_getter).Run(std::move(callback_split.first));
-  if (database)
+  if (database) {
     std::move(callback_split.second).Run(database);
+  }
 }
 
 void GetCertDbAndPostOnWorkerThread(
@@ -254,21 +256,9 @@ void OwnerKeyLoader::OnPrivateKeyLoaded(
 }
 
 void OwnerKeyLoader::MaybeGenerateNewKey() {
-  // device_settings_service_ indicates that the current user should become the
-  // owner, generate a new owner key pair for them.
-  if (device_settings_service_->GetWillEstablishConsumerOwnership()) {
-    // This should only happen on the first sign in when there's no previous
-    // public key.
-    RecordOwnerKeyEvent(OwnerKeyEvent::kEstablishingConsumerOwnership,
-                        /*success=*/!IsKeyPresent(public_key_));
-    LOG(WARNING) << "Establishing consumer ownership.";
-    return GenerateNewKey();
-  }
-
-  // Otherwise maybe it's not the first sign in. Check device policies.
-  // If the owner key was never generated before, the policies will be empty.
-  // Also, in theory ChromeOS is allowed to lose the policies and recover, so
-  // be prepared for them to still be empty.
+  // Check device policies. If the owner key was never generated before, the
+  // policies will be empty. Also, in theory ChromeOS is allowed to lose the
+  // policies and recover, so be prepared for them to still be empty.
   const enterprise_management::PolicyData* policy_data =
       device_settings_service_->policy_data();
   if (policy_data && policy_data->has_username()) {
@@ -306,6 +296,20 @@ void OwnerKeyLoader::MaybeGenerateNewKey() {
     RecordOwnerKeyEvent(OwnerKeyEvent::kUserNotAnOwnerBasedOnLocalState,
                         /*success=*/IsKeyPresent(public_key_));
     return std::move(callback_).Run(std::move(public_key_), nullptr);
+    // `this` might be deleted here.
+  }
+
+  // If everything else is empty, check DeviceSettingsService. It remembers from
+  // the login screen whether this is the first user. It's checked after
+  // policies because it relies on local state (same as `GetOwnerEmail()`) and
+  // is less trustworthy.
+  if (device_settings_service_->GetWillEstablishConsumerOwnership()) {
+    // This should only happen on the first sign in when there's no previous
+    // public key.
+    RecordOwnerKeyEvent(OwnerKeyEvent::kEstablishingConsumerOwnership,
+                        /*success=*/!IsKeyPresent(public_key_));
+    LOG(WARNING) << "Establishing consumer ownership.";
+    return GenerateNewKey();
   }
 
   RecordOwnerKeyEvent(OwnerKeyEvent::kUnsureUserNotAnOwner,
@@ -317,6 +321,13 @@ void OwnerKeyLoader::MaybeGenerateNewKey() {
 }
 
 void OwnerKeyLoader::GenerateNewKey() {
+  // Ensure owner account id is stored for the next time.
+  if (!user_manager::UserManager::Get()->GetOwnerEmail().has_value()) {
+    user_manager::User* user =
+        ash::ProfileHelper::Get()->GetUserByProfile(profile_);
+    user_manager::UserManager::Get()->RecordOwner(user->GetAccountId());
+  }
+
   GetCertDbAndPostOnWorkerThread(
       profile_,
       base::BindOnce(&GenerateNewOwnerKeyOnWorkerThread, owner_key_util_,
