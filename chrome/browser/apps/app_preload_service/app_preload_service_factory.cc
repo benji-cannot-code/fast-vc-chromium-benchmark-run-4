66d0ff0267cd0b770119fd775878ea614c83cc94
Tim Sergeant <tsergeant@chromium.org>
2023-03-02 23:31:51 +0000

    Create BUILD.gn files for //c/b/apps and //c/b/apps/app_preload_service
    This pulls C++ code for these directories out of the blob of //c/browser
    and into isolated directories. There's two awkward circular includes
    on policy/profile code, but this is now isolated to one file,
    user_type_filter.cc.
    
    This CL doesn't move any tests, but that should be straightforward to do
    in a follow-up if desired. However, one relevant unittest was
    accidentally included in the browser_tests binary, so, whoops, that's
    fixed here.
    
    Bug: 1420759
    Change-Id: I3beb9f852a93873f445e7572903d920df0ab012b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4301714
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112515}

--- chrome/browser/apps/app_preload_service/app_preload_service_factory.cc
+++ chrome/browser/apps/app_preload_service/app_preload_service_factory.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_preload_service/app_preload_service.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
-#include "chrome/browser/policy/profile_policy_connector.h"
+#include "chrome/browser/apps/user_type_filter.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
@@ -59,8 +59,8 @@ bool AppPreloadServiceFactory::IsAvailable(Profile* profile) {
 
   // App Preload Service is currently only available for unmanaged, unsupervised
   // accounts.
-  if (profile->IsGuestSession() || profile->IsChild() ||
-      profile->GetProfilePolicyConnector()->IsManaged()) {
+  std::string user_type = apps::DetermineUserType(profile);
+  if (user_type != apps::kUserTypeUnmanaged) {
     return false;
   }
 
--- chrome/browser/apps/user_type_filter.cc
+++ chrome/browser/apps/user_type_filter.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/policy/profile_policy_connector.h"
+#include "chrome/browser/policy/profile_policy_connector.h"  // nogncheck crbug.com/1420759
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profiles_state.h"
+#include "chrome/browser/profiles/profiles_state.h"  // nogncheck crbug.com/1420759
 #include "content/public/browser/browser_thread.h"
 
 namespace apps {
