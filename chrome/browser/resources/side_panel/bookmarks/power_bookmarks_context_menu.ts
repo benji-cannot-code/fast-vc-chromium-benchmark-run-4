ee4bf3a6fc15d03e612b550de66b24ac8e676e00
Emily Shack <emshack@chromium.org>
2023-03-02 00:26:37 +0000

    Bookmarks++: Add context menu edit option
    Includes adding name and url inputs to the edit dialog when triggered
    via the context menu.
    
    This completes all components of the edit dialog, apart from the
    functionality of the New Folder button.
    
    Bug: 1365606
    Change-Id: I5b4fced85b91cbe85e0668603c2d692f1caa2d1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257847
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111932}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_context_menu.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_context_menu.ts
@@ -29,12 +29,13 @@ export enum MenuItemId {
   OPEN_NEW_TAB = 0,
   OPEN_NEW_WINDOW = 1,
   OPEN_INCOGNITO = 2,
-  ADD_TO_BOOKMARKS_BAR = 3,
-  REMOVE_FROM_BOOKMARKS_BAR = 4,
-  TRACK_PRICE = 5,
-  RENAME = 6,
-  DELETE = 7,
-  DIVIDER = 8,
+  EDIT = 3,
+  ADD_TO_BOOKMARKS_BAR = 4,
+  REMOVE_FROM_BOOKMARKS_BAR = 5,
+  TRACK_PRICE = 6,
+  RENAME = 7,
+  DELETE = 8,
+  DIVIDER = 9,
 }
 
 export interface MenuItem {
@@ -116,6 +117,13 @@ export class PowerBookmarksContextMenuElement extends PolymerElement {
       return menuItems;
     }
 
+    if (this.bookmarks_[0]!.url) {
+      menuItems.push({
+        id: MenuItemId.EDIT,
+        label: loadTimeData.getString('menuEdit'),
+      });
+    }
+
     if (this.bookmarks_[0]!.parentId ===
         loadTimeData.getString('bookmarksBarId')) {
       menuItems.push({id: MenuItemId.DIVIDER}, {
@@ -145,12 +153,18 @@ export class PowerBookmarksContextMenuElement extends PolymerElement {
       );
     }
 
+    menuItems.push({id: MenuItemId.DIVIDER});
+
+    if (!this.bookmarks_[0]!.url) {
+      menuItems.push(
+          {
+            id: MenuItemId.RENAME,
+            label: loadTimeData.getString('menuRename'),
+          },
+      );
+    }
+
     menuItems.push(
-        {id: MenuItemId.DIVIDER},
-        {
-          id: MenuItemId.RENAME,
-          label: loadTimeData.getString('menuRename'),
-        },
         {
           id: MenuItemId.DELETE,
           label: loadTimeData.getString('tooltipDelete'),
@@ -221,6 +235,15 @@ export class PowerBookmarksContextMenuElement extends PolymerElement {
                 'Commerce.PriceTracking.SidePanel.Track.ContextMenu');
           }
         }
+        break;
+      case MenuItemId.EDIT:
+        this.dispatchEvent(new CustomEvent('edit-clicked', {
+          bubbles: true,
+          composed: true,
+          detail: {
+            id: this.bookmarks_[0]!.id,
+          },
+        }));
         break;
       case MenuItemId.RENAME:
         if (editingDisabledByPolicy(this.bookmarks_)) {
--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_edit_dialog.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_edit_dialog.ts
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../strings.m.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
 
 import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import {assertNotReached} from 'chrome://resources/js/assert_ts.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {DomRepeatEvent, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -18,6 +20,8 @@ import {getTemplate} from './power_bookmarks_edit_dialog.html.js';
 export interface PowerBookmarksEditDialogElement {
   $: {
     dialog: CrDialogElement,
+    nameInput: CrInputElement,
+    urlInput: CrInputElement,
   };
 }
 
@@ -60,6 +64,11 @@ export class PowerBookmarksEditDialogElement extends PolymerElement {
         type: Array,
         value: () => [],
       },
+
+      moveOnly_: {
+        type: Boolean,
+        value: false,
+      },
     };
   }
 
@@ -68,16 +77,41 @@ export class PowerBookmarksEditDialogElement extends PolymerElement {
   private selectedFolder_: chrome.bookmarks.BookmarkTreeNode|undefined;
   private activeFolderPath_: chrome.bookmarks.BookmarkTreeNode[];
   private newFolders_: chrome.bookmarks.BookmarkTreeNode[];
+  private moveOnly_: boolean;
 
   showDialog(
       activeFolderPath: chrome.bookmarks.BookmarkTreeNode[],
       topLevelBookmarks: chrome.bookmarks.BookmarkTreeNode[],
-      selectedBookmarks: chrome.bookmarks.BookmarkTreeNode[]) {
+      selectedBookmarks: chrome.bookmarks.BookmarkTreeNode[],
+      moveOnly: boolean) {
     this.activeFolderPath_ = activeFolderPath.slice();
     this.topLevelBookmarks_ = topLevelBookmarks;
     this.selectedBookmarks_ = selectedBookmarks;
     this.newFolders_ = [];
+    this.moveOnly_ = moveOnly;
     this.$.dialog.showModal();
+  }
+
+  private getDialogTitle_(): string {
+    if (this.moveOnly_) {
+      return loadTimeData.getString('editMoveFolderTo');
+    } else {
+      return loadTimeData.getString('editBookmark');
+    }
+  }
+
+  private getBookmarkName_(): string {
+    if (this.selectedBookmarks_.length === 1) {
+      return this.selectedBookmarks_[0]!.title;
+    }
+    return '';
+  }
+
+  private getBookmarkUrl_(): string {
+    if (this.selectedBookmarks_.length === 1) {
+      return this.selectedBookmarks_[0]!.url!;
+    }
+    return '';
   }
 
   private getActiveFolder_(): chrome.bookmarks.BookmarkTreeNode|undefined {
@@ -131,6 +165,24 @@ export class PowerBookmarksEditDialogElement extends PolymerElement {
     return folder.children!.filter(isFolder).length > 0;
   }
 
+  private validateUrl_(): boolean {
+    const urlInput = this.$.urlInput;
+    const originalValue = urlInput.inputElement.value;
+
+    if (urlInput.validate()) {
+      return true;
+    }
+
+    urlInput.inputElement.value = 'http://' + originalValue;
+
+    if (urlInput.validate()) {
+      return true;
+    }
+
+    urlInput.inputElement.value = originalValue;
+    return false;
+  }
+
   private isSelected_(folder: chrome.bookmarks.BookmarkTreeNode): boolean {
     return folder === this.selectedFolder_;
   }
@@ -178,6 +230,9 @@ export class PowerBookmarksEditDialogElement extends PolymerElement {
   }
 
   private onSave_() {
+    if (!this.moveOnly_ && !this.validateUrl_()) {
+      return;
+    }
     const activeFolder = this.getActiveFolder_();
     let folderId;
     if (this.selectedFolder_) {
@@ -191,6 +246,9 @@ export class PowerBookmarksEditDialogElement extends PolymerElement {
       bubbles: true,
       composed: true,
       detail: {
+        bookmarks: this.selectedBookmarks_,
+        name: this.moveOnly_ ? undefined : this.$.nameInput.inputElement.value,
+        url: this.moveOnly_ ? undefined : this.$.urlInput.inputElement.value,
         folderId: folderId,
         newFolders: this.newFolders_,
       },
