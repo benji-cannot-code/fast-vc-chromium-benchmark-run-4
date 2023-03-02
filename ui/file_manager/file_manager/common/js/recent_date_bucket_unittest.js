8dd8082898f8c279005ca50f350131629c7bed2e
Bo Majewski <majewski@chromium.org>
2023-03-02 11:20:15 +0000

    [Files]: Moves getEarliestTimestamp to recency date bucket module.
    This is the first step that aims at unifying the search behavior with
    recent files behavior. We just move the code and add unit tests. The
    long term principle is that the time buckets used by search and recency
    must behave the same way (not yet part of this CL; do be done
    subsequently).
    
    Bug: b:269358182
    Change-Id: Ie12e6528e74a115e3265ffe27658f76eaa762f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289621
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112133}

--- ui/file_manager/file_manager/common/js/recent_date_bucket_unittest.js
+++ ui/file_manager/file_manager/common/js/recent_date_bucket_unittest.js
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 import {assertEquals} from 'chrome://webui-test/chromeos/chai_assert.js';
 
-import {getRecentDateBucket} from './recent_date_bucket.js';
+import {SearchRecency} from '../../externs/ts/state.js';
+
+import {getEarliestTimestamp, getRecentDateBucket} from './recent_date_bucket.js';
 
 export function setUp() {
   loadTimeData.overrideValues({
@@ -138,5 +140,35 @@ export function testGetRecentDateBucket() {
       assertEquals(
           getRecentDateBucket(test.date, testSample.today), test.bucket);
     }
+  }
+}
+
+export function testGetEarliestTimestamp() {
+  const testNow = new Date(2023, 1, 27, 15, 40);  // ms = 1677472800000
+  const testData = [
+    {
+      recency: SearchRecency.TODAY,
+      want: new Date(2023, 1, 27, 0, 0).getTime(),
+    },
+    {
+      recency: SearchRecency.YESTERDAY,
+      want: new Date(2023, 1, 26, 0, 0).getTime(),
+    },
+    {
+      recency: SearchRecency.LAST_WEEK,
+      want: new Date(2023, 1, 21, 0, 0).getTime(),
+    },
+    {
+      recency: SearchRecency.LAST_MONTH,
+      want: new Date(2023, 0, 28, 0, 0).getTime(),
+    },
+    {
+      recency: SearchRecency.LAST_YEAR,
+      want: new Date(2022, 1, 27, 0, 0).getTime(),
+    },
+  ];
+  for (const datum of testData) {
+    const got = getEarliestTimestamp(datum.recency, testNow);
+    assertEquals(datum.want, got);
   }
 }
