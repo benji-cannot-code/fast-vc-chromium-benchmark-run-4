104efa955c95f54b1496dd12b92b2a27fb646177
Mahmoud Rashad <mmrashad@google.com>
2023-03-02 09:48:48 +0000

    ReadingList: Implement DualReadingListModel::SetEntryTitleIfExists
    This CL implements DualReadingListModel::SetEntryTitleIfExists by
    propagating the update to both underlying ReadingListModelImpl
    instances `local_or_syncable_model_` and `account_model_`.
    
    Bug: 1402196
    Change-Id: I957240daaf301e81203a730e4c53c59163ef8692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294683
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112107}

--- components/reading_list/core/reading_list_model_impl.cc
+++ components/reading_list/core/reading_list_model_impl.cc
@@ -351,7 +351,7 @@ const ReadingListEntry& ReadingListModelImpl::AddOrReplaceEntry(
     RemoveEntryByURL(url);
   }
 
-  std::string trimmed_title = base::CollapseWhitespaceASCII(title, false);
+  std::string trimmed_title = TrimTitle(title);
 
   auto entry =
       base::MakeRefCounted<ReadingListEntry>(url, trimmed_title, clock_->Now());
@@ -403,7 +403,7 @@ void ReadingListModelImpl::SetEntryTitleIfExists(const GURL& url,
     return;
   }
   ReadingListEntry& entry = *(iterator->second);
-  std::string trimmed_title = base::CollapseWhitespaceASCII(title, false);
+  std::string trimmed_title = TrimTitle(title);
   if (entry.Title() == trimmed_title) {
     return;
   }
@@ -558,6 +558,11 @@ ReadingListModelImpl::BeginBatchUpdatesWithSyncMetadata() {
 bool ReadingListModelImpl::IsTrackingSyncMetadata() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return sync_bridge_.change_processor()->IsTrackingMetadata();
+}
+
+// static
+std::string ReadingListModelImpl::TrimTitle(const std::string& title) {
+  return base::CollapseWhitespaceASCII(title, false);
 }
 
 // static
