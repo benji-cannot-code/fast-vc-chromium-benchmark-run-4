4252b2243822d7dfb26c5a1da2cc0825e39df962
Kevin Ellis <kevers@google.com>
2023-03-02 00:38:57 +0000

    [view-timeline] calculate range using untransformed box.
    Bug: 1411743
    Change-Id: I09bc2c7ca3d35f9e3c6a8feb946a61bab75c26e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295308
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111944}

--- third_party/blink/renderer/core/animation/view_timeline.cc
+++ third_party/blink/renderer/core/animation/view_timeline.cc
@@ -36,7 +36,7 @@ double ComputeOffset(Element* source_element,
                      LayoutBox* subject_layout,
                      LayoutBox* source_layout,
                      ScrollOrientation physical_orientation) {
-  MapCoordinatesFlags flags = kIgnoreScrollOffset;
+  MapCoordinatesFlags flags = kIgnoreScrollOffset | kIgnoreTransforms;
   gfx::PointF point = gfx::PointF(subject_layout->LocalToAncestorPoint(
       PhysicalOffset(), source_layout, flags));
 
