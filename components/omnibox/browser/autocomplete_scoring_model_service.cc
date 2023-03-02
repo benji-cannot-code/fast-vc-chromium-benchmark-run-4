30751026d04ccd24e036a8446a1852abffa5cb04
Angela Yoeurng <yoangela@chromium.org>
2023-03-02 02:23:51 +0000

    [omnibox][ml] Call Ml scoring model and create skeleton callbacks
    This CL adds the code to actually invoke the async ml scoring model
    and registers some skeleton callbacks that will process the new scores
    when the model returns. These callbacks currently don't do anything
    functionally.
    
    Bug: 1405555
    Change-Id: Ib7c244ffe654d19b4d457becd9e62da9deb4650c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4287609
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112003}

--- components/omnibox/browser/autocomplete_scoring_model_service.cc
+++ components/omnibox/browser/autocomplete_scoring_model_service.cc
@@ -41,7 +41,7 @@ void AutocompleteScoringModelService::ScoreAutocompleteUrlMatch(
     base::OnceCallback<void(
         const absl::optional<AutocompleteScoringModelExecutor::ModelOutput>&)>
         scoring_callback) {
-  if (!url_scoring_model_handler_) {
+  if (!UrlScoringModelAvailable()) {
     return;
   }
 
@@ -53,4 +53,8 @@ void AutocompleteScoringModelService::ScoreAutocompleteUrlMatch(
 
   url_scoring_model_handler_->ExecuteModelWithInput(std::move(scoring_callback),
                                                     *input_signals);
+}
+
+bool AutocompleteScoringModelService::UrlScoringModelAvailable() {
+  return url_scoring_model_handler_ != nullptr;
 }
