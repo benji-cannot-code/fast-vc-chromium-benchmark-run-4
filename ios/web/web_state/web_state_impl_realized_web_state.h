0bcf8b82c115a5b568745658bcf22b057db0fed0
Mike Dougherty <michaeldo@chromium.org>
2023-03-02 00:33:53 +0000

    Move ownership of WebFramesManagerImpl to WebState
    Moves the ownership of WebFramesManagerImpl instances from a
    WebStateUserData Container to being owned by the realized web state
    directly.
    
    Bug: 1380918
    Change-Id: I2dc8ec7c2f742ce10087732dd945252019b94062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4301709
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111939}

--- ios/web/web_state/web_state_impl_realized_web_state.h
+++ ios/web/web_state/web_state_impl_realized_web_state.h
@@ -6,12 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_WEB_STATE_WEB_STATE_IMPL_REALIZED_WEB_STATE_H_
 #define IOS_WEB_WEB_STATE_WEB_STATE_IMPL_REALIZED_WEB_STATE_H_
 
+#include <map>
+
 #import "ios/web/web_state/web_state_impl.h"
 
+#import "ios/web/public/js_messaging/content_world.h"
 #import "ios/web/public/web_state_observer.h"
 
 namespace web {
 
+class WebFramesManagerImpl;
 class WebUIIOS;
 
 // Internal implementation of a realized WebStateImpl.
@@ -56,9 +60,13 @@ class WebStateImpl::RealizedWebState final : public NavigationManagerDelegate {
   const NavigationManagerImpl& GetNavigationManager() const;
   NavigationManagerImpl& GetNavigationManager();
 
-  // Returns the WebFrameManagerImpl associated with the owning WebStateImpl.
-  const WebFramesManagerImpl& GetPageWorldWebFramesManager() const;
+  // Returns the WebFrameManagerImpl associated with the owning WebStateImpl for
+  // the page content world.
   WebFramesManagerImpl& GetPageWorldWebFramesManager();
+
+  // Returns the WebFrameManagerImpl associated with the owning WebStateImpl for
+  // the given `world`.
+  WebFramesManagerImpl& GetWebFramesManager(ContentWorld world);
 
   // Returns the SessionCertificationPolicyCacheImpl associated with the owning
   // WebStateImpl.
@@ -305,6 +313,9 @@ class WebStateImpl::RealizedWebState final : public NavigationManagerDelegate {
 
   // The fake CRWWebViewNavigationProxy used for testing. Nil in production.
   __strong id<CRWWebViewNavigationProxy> web_view_for_testing_;
+
+  // A map which stores the web frame manager for each content world.
+  std::map<ContentWorld, std::unique_ptr<WebFramesManagerImpl>> managers_;
 };
 
 }  // namespace web
