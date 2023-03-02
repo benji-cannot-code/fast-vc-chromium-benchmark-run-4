a172928bd0db9ea8a3aa3321c9a7cfb537ed42dc
Yuly Novikov <ynovikov@chromium.org>
2023-03-02 15:13:24 +0000

    Revert "Apply auto expiry mechanism to GPUExternalTexture"
    This reverts commit ad2a6bfd37ef8a2d8c6eeea447a8ace302fb99cd.
    
    Reason for revert: breaks WebCodecs_GPUExternalTexture_*
    https://luci-milo.appspot.com/ui/p/chromium/builders/ci/Win10%20FYI%20x64%20Release%20(Intel)/956/overview
    
    Original change's description:
    > Apply auto expiry mechanism to GPUExternalTexture
    >
    > WebGPU spec updated GPUExternalTexture expire mechanism. It removes
    > GPUExternalTexture.expire and uses auto expiry mechanism.
    >
    > The basic rules are:
    > - ImportExternalTexture should return same object if underly
    >   resource is the same.
    > - GPUExternalTexture should be expire if current task scope finished.
    >   And could be refreshed by importExternalTexture in new task scope.
    >
    > This CL adds auto expiry mechanism and a cache to address these requirements.
    >
    > Bug: chromium:1412338
    > Change-Id: I6ca72d2ea9a013d7729072cb76cb412ecf3a1e86
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4258147
    > Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1112071}
    
    Bug: chromium:1412338
    Change-Id: I1a9c0b6a2903c6b1e11e2532e4f3ad81081603c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303861
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112220}

--- third_party/blink/renderer/modules/webgpu/gpu_device.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_device.cc
@@ -11,11 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_compute_pipeline_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_device_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_error_filter.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_external_texture_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_feature_name.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_query_set_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_queue_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_render_pipeline_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_uncaptured_error_event_init.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_union_htmlvideoelement_videoframe.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
@@ -168,8 +170,6 @@ GPUDevice::GPUDevice(ExecutionContext* execution_context,
 
   if (descriptor->defaultQueue()->hasLabel())
     queue_->setLabel(descriptor->defaultQueue()->label());
-
-  external_texture_cache_ = MakeGarbageCollected<ExternalTextureCache>(this);
 }
 
 GPUDevice::~GPUDevice() {
@@ -419,13 +419,9 @@ GPUQueue* GPUDevice::queue() {
   return queue_;
 }
 
-bool GPUDevice::destroyed() const {
-  return destroyed_;
-}
-
 void GPUDevice::destroy(v8::Isolate* isolate) {
   destroyed_ = true;
-  external_texture_cache_->Destroy();
+  DestroyAllExternalTextures();
   // Dissociate mailboxes before destroying the device. This ensures that
   // mailbox operations which run during dissociation can succeed.
   DissociateMailboxes();
@@ -460,7 +456,12 @@ GPUSampler* GPUDevice::createSampler(const GPUSamplerDescriptor* descriptor) {
 GPUExternalTexture* GPUDevice::importExternalTexture(
     const GPUExternalTextureDescriptor* descriptor,
     ExceptionState& exception_state) {
-  return external_texture_cache_->Import(descriptor, exception_state);
+  // Ensure the GPUExternalTexture created from a destroyed GPUDevice will be
+  // expired immediately.
+  if (destroyed_)
+    return GPUExternalTexture::CreateExpired(this, descriptor, exception_state);
+
+  return GPUExternalTexture::Create(this, descriptor, exception_state);
 }
 
 GPUBindGroup* GPUDevice::createBindGroup(
@@ -652,7 +653,7 @@ void GPUDevice::Trace(Visitor* visitor) const {
   visitor->Trace(limits_);
   visitor->Trace(queue_);
   visitor->Trace(lost_property_);
-  visitor->Trace(external_texture_cache_);
+  visitor->Trace(active_external_textures_);
   visitor->Trace(textures_with_mailbox_);
   visitor->Trace(mappable_buffers_);
   ExecutionContextClient::Trace(visitor);
@@ -662,7 +663,14 @@ void GPUDevice::Trace(Visitor* visitor) const {
 void GPUDevice::Dispose() {
   // This call accesses other GC objects, so it cannot be called inside GC
   // objects destructors. Instead call it in the pre-finalizer.
-  external_texture_cache_->Destroy();
+  DestroyAllExternalTextures();
+}
+
+void GPUDevice::DestroyAllExternalTextures() {
+  for (auto& external_texture : active_external_textures_) {
+    external_texture->Destroy();
+  }
+  active_external_textures_.clear();
 }
 
 void GPUDevice::DissociateMailboxes() {
@@ -684,6 +692,17 @@ void GPUDevice::TrackMappableBuffer(GPUBuffer* buffer) {
 
 void GPUDevice::UntrackMappableBuffer(GPUBuffer* buffer) {
   mappable_buffers_.erase(buffer);
+}
+
+void GPUDevice::AddActiveExternalTexture(GPUExternalTexture* external_texture) {
+  DCHECK(external_texture);
+  active_external_textures_.insert(external_texture);
+}
+
+void GPUDevice::RemoveActiveExternalTexture(
+    GPUExternalTexture* external_texture) {
+  DCHECK(external_texture);
+  active_external_textures_.erase(external_texture);
 }
 
 void GPUDevice::TrackTextureWithMailbox(GPUTexture* texture) {
--- third_party/blink/renderer/modules/webgpu/gpu_device.h
+++ third_party/blink/renderer/modules/webgpu/gpu_device.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ExecutionContext;
-class ExternalTextureCache;
 class HTMLCanvasElement;
 class GPUAdapter;
 class GPUBuffer;
@@ -83,7 +82,6 @@ class GPUDevice final : public EventTargetWithInlineData,
   ScriptPromise lost(ScriptState* script_state);
 
   GPUQueue* queue();
-  bool destroyed() const;
 
   void destroy(v8::Isolate* isolate);
 
@@ -95,7 +93,6 @@ class GPUDevice final : public EventTargetWithInlineData,
                                         unsigned int usage_flags,
                                         ExceptionState& exception_state);
   GPUSampler* createSampler(const GPUSamplerDescriptor* descriptor);
-
   GPUExternalTexture* importExternalTexture(
       const GPUExternalTextureDescriptor* descriptor,
       ExceptionState& exception_state);
@@ -145,6 +142,9 @@ class GPUDevice final : public EventTargetWithInlineData,
   void InjectError(WGPUErrorType type, const char* message);
   void AddConsoleWarning(const char* message);
 
+  void AddActiveExternalTexture(GPUExternalTexture* external_texture);
+  void RemoveActiveExternalTexture(GPUExternalTexture* external_texture);
+
   void TrackTextureWithMailbox(GPUTexture* texture);
   void UntrackTextureWithMailbox(GPUTexture* texture);
 
@@ -165,6 +165,8 @@ class GPUDevice final : public EventTargetWithInlineData,
 
   // Used by USING_PRE_FINALIZER.
   void Dispose();
+
+  void DestroyAllExternalTextures();
   void DissociateMailboxes();
   void UnmapAllMappableBuffers(v8::Isolate* isolate);
 
@@ -213,12 +215,14 @@ class GPUDevice final : public EventTargetWithInlineData,
   static constexpr int kMaxAllowedConsoleWarnings = 500;
   int allowed_console_warnings_remaining_ = kMaxAllowedConsoleWarnings;
 
+  // Keep a list of all active GPUExternalTexture. Eagerly destroy them
+  // when the device is destroyed (via .destroy) to free the memory.
+  HeapHashSet<WeakMember<GPUExternalTexture>> active_external_textures_;
+
   // Textures with mailboxes that should be dissociated before device.destroy().
   HeapHashSet<WeakMember<GPUTexture>> textures_with_mailbox_;
 
   HeapHashSet<WeakMember<GPUBuffer>> mappable_buffers_;
-
-  Member<ExternalTextureCache> external_texture_cache_;
 
   // This attribute records that whether GPUDevice is destroyed (via destroy()).
   bool destroyed_ = false;
