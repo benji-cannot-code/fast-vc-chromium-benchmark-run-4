aa86fd206061dd90c76ec4c3a33900e901548460
Andrey Kosyakov <caseq@chromium.org>
2023-03-02 21:24:46 +0000

    Move GlobalScope::WillBeginLoading() call to locked section of InitializeOnWorkerThread
    Bug: 1418561
    Change-Id: I03cd6a8fee3bfbc139b63e1c96e7f63092074c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300139
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112437}

--- third_party/blink/renderer/core/workers/worker_thread.cc
+++ third_party/blink/renderer/core/workers/worker_thread.cc
@@ -639,6 +639,7 @@ void WorkerThread::InitializeOnWorkerThread(
       debugger->WorkerThreadCreated(this);
 
     GlobalScope()->ScriptController()->Initialize(url_for_debugger);
+    GlobalScope()->WillBeginLoading();
     v8::HandleScope handle_scope(GetIsolate());
     Platform::Current()->WorkerContextCreated(
         GlobalScope()->ScriptController()->GetContext());
@@ -669,7 +670,9 @@ void WorkerThread::InitializeOnWorkerThread(
   // from another thread and try to resume "pause on start" before
   // we even paused.
   worker_inspector_controller_->WaitForDebuggerIfNeeded();
-  GlobalScope()->WillBeginLoading();
+  // Note the above call runs nested message loop which may result in
+  // worker thread being torn down by request from the parent thread,
+  // while waiting for debugger.
 }
 
 void WorkerThread::EvaluateClassicScriptOnWorkerThread(
