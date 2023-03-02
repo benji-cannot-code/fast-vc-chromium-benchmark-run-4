91fd48b699fc96b287817c6740b40f4394e6823c
Jérôme Lebel <jlebel@chromium.org>
2023-03-02 19:23:47 +0000

    [iOS] Add user actions for IdentityChooserCoordinator
    Add user action:
     * Signin_AccountPicker_Open
     * Signin_AccountPicker_Close
    
    And add DCHECK in
    -[SigninScreenCoordinator showAccountPickerFromPoint:] to not open
    twice the account picker.
    
    Change-Id: Idb3e9165408529caa8933c1eef89c5626530ae78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4301372
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112359}

--- ios/chrome/browser/ui/authentication/unified_consent/identity_chooser/identity_chooser_coordinator.mm
+++ ios/chrome/browser/ui/authentication/unified_consent/identity_chooser/identity_chooser_coordinator.mm
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <ostream>
 
 #import "base/check_op.h"
+#import "base/metrics/user_metrics.h"
+#import "base/metrics/user_metrics_action.h"
 #import "base/notreached.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
@@ -66,6 +68,7 @@ typedef NS_ENUM(NSInteger, IdentityChooserCoordinatorState) {
 
 - (void)start {
   [super start];
+  base::RecordAction(base::UserMetricsAction("Signin_AccountPicker_Open"));
   DCHECK_EQ(IdentityChooserCoordinatorStateNotStarted, self.state);
   self.state = IdentityChooserCoordinatorStateStarted;
   // Creates the controller.
@@ -99,6 +102,7 @@ typedef NS_ENUM(NSInteger, IdentityChooserCoordinatorState) {
 
 - (void)stop {
   [super stop];
+  base::RecordAction(base::UserMetricsAction("Signin_AccountPicker_Close"));
   [self.identityChooserMediator disconnect];
   self.identityChooserMediator = nil;
 }
--- ios/chrome/browser/ui/first_run/signin/signin_screen_coordinator.mm
+++ ios/chrome/browser/ui/first_run/signin/signin_screen_coordinator.mm
@@ -270,6 +270,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - SigninScreenViewControllerDelegate
 
 - (void)showAccountPickerFromPoint:(CGPoint)point {
+  DCHECK(!self.identityChooserCoordinator);
   self.identityChooserCoordinator = [[IdentityChooserCoordinator alloc]
       initWithBaseViewController:self.viewController
                          browser:self.browser];
