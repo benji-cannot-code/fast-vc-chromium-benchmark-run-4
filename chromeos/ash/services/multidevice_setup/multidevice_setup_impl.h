befc112f9362fe72f4326275ec91b386dced1c49
Brittany Hartmire <bhartmire@google.com>
2023-03-02 20:06:05 +0000

    [QuickStart]: Setup mojo service to determine whether user completed QS
    This CL adds 2 mojo methods to communicate whether a user has completed QS: SetQuickStartPhoneInstanceID and GetQuickStartPhoneInstanceID.
    In a follow-up cl, the MultiDeviceSetupScreen will check the ID in the OOBE wizard context and use its setup_client_ to set the ID in the MultiDeviceSetup mojo service if necessary.
    
    Design doc: go/cros-quick-start-unified-setup-screen-enhancements
    
    TEST=added new unittests inside MultiDeviceSetupServiceTest and MultDeviceSetupImplTest
    
    Bug: b/234655072
    Change-Id: I9c928c6ccf2720f105a866199ba6485eaee68c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289773
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112389}

--- chromeos/ash/services/multidevice_setup/multidevice_setup_impl.h
+++ chromeos/ash/services/multidevice_setup/multidevice_setup_impl.h
@@ -120,6 +120,10 @@ class MultiDeviceSetupImpl : public MultiDeviceSetupBase,
   void TriggerEventForDebugging(
       mojom::EventTypeForDebugging type,
       TriggerEventForDebuggingCallback callback) override;
+  void SetQuickStartPhoneInstanceID(
+      const std::string& qs_phone_instance_id) override;
+  void GetQuickStartPhoneInstanceID(
+      GetQuickStartPhoneInstanceIDCallback callback) override;
 
   // MultiDeviceSetupBase:
   void SetHostDeviceWithoutAuthToken(
@@ -158,6 +162,7 @@ class MultiDeviceSetupImpl : public MultiDeviceSetupBase,
   std::unique_ptr<AndroidSmsAppInstallingStatusObserver>
       android_sms_app_installing_host_observer_;
   AuthTokenValidator* auth_token_validator_;
+  std::string qs_phone_instance_id_;
 
   mojo::RemoteSet<mojom::HostStatusObserver> host_status_observers_;
   mojo::RemoteSet<mojom::FeatureStateObserver> feature_state_observers_;
