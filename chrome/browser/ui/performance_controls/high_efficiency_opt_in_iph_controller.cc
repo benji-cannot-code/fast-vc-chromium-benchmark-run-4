dc2d8d48d1906640d7c8a62d0172dc4cd076a476
Steven Luong <stluong@chromium.org>
2023-03-02 18:38:20 +0000

    Rename HighEfficiencyIPHController
    Rename HighEfficiencyIPHController to HighEfficiencyOptInController.
    There are two IPH that are shown to users depending on whether they have
    opted in or out of high efficiency mode. This controller is used to
    show the IPH for those who have opted into the high efficiency mode
    and thus is renamed to prevent any confusion of when this controller is
    used.
    
    Bug: None
    Change-Id: I86b4fa232aa0da0ec4149ce6ec89da8dc1dc9012
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261334
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112306}

--- chrome/browser/ui/performance_controls/high_efficiency_opt_in_iph_controller.cc
+++ chrome/browser/ui/performance_controls/high_efficiency_opt_in_iph_controller.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/performance_controls/high_efficiency_iph_controller.h"
+#include "chrome/browser/ui/performance_controls/high_efficiency_opt_in_iph_controller.h"
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/performance_manager/public/user_tuning/user_performance_tuning_manager.h"
@@ -14,28 +14,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/user_tuning/prefs.h"
 #include "components/prefs/pref_service.h"
 
-HighEfficiencyIPHController::HighEfficiencyIPHController(Browser* browser)
+HighEfficiencyOptInIPHController::HighEfficiencyOptInIPHController(
+    Browser* browser)
     : browser_(browser) {
   auto* manager = performance_manager::user_tuning::
       UserPerformanceTuningManager::GetInstance();
   high_efficiency_observer_.Observe(manager);
 }
 
-HighEfficiencyIPHController::~HighEfficiencyIPHController() = default;
+HighEfficiencyOptInIPHController::~HighEfficiencyOptInIPHController() = default;
 
-void HighEfficiencyIPHController::OnMemoryThresholdReached() {
+void HighEfficiencyOptInIPHController::OnMemoryThresholdReached() {
   MaybeTriggerPromo();
 }
 
-void HighEfficiencyIPHController::OnTabCountThresholdReached() {
+void HighEfficiencyOptInIPHController::OnTabCountThresholdReached() {
   MaybeTriggerPromo();
 }
 
-void HighEfficiencyIPHController::OnJankThresholdReached() {
+void HighEfficiencyOptInIPHController::OnJankThresholdReached() {
   MaybeTriggerPromo();
 }
 
-void HighEfficiencyIPHController::MaybeTriggerPromo() {
+void HighEfficiencyOptInIPHController::MaybeTriggerPromo() {
   BrowserWindow* browser_window = browser_->window();
   PrefService* prefs = g_browser_process->local_state();
   if (browser_window != nullptr &&
