a62e88f931542567cc3049866f4f18d18e5fef2f
Moe Ahmadi <mahmadi@chromium.org>
2023-03-02 18:27:02 +0000

    Revert "Unify initialisation of memory system"
    This reverts commit 2292e533ba1fc7597bf068c4004e79eac1a71e3c.
    
    Reason for revert: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-asan/52364/overview
    
    Original change's description:
    > Unify initialisation of memory system
    >
    > Initialisation of memory components like GWP-ASan was performed in each
    > delegate separately. This CL unifies the initialisation and provides a
    > common implementation.
    >
    > Bug: 1137393
    > Change-Id: I7e43f1ce9d9608a48d00d74c9037e5fd1dcd1218
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4217593
    > Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1112276}
    
    Bug: 1137393
    Change-Id: Ica619914e5efa8f03fb40e96da45c9674e09ddd0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304679
    Reviewed-by: Kuan Huang <kuanhuang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Gregory Guterman <guterman@google.com>
    Owners-Override: Gregory Guterman <guterman@google.com>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112296}

--- chrome/app/chrome_main_delegate.h
+++ chrome/app/chrome_main_delegate.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/startup_data.h"
 #include "chrome/common/chrome_content_client.h"
-#include "components/memory_system/memory_system.h"
 #include "content/public/app/content_main_delegate.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -24,6 +23,10 @@ class CommandLine;
 
 namespace chromeos {
 class LacrosService;
+}
+
+namespace heap_profiling {
+class HeapProfilerController;
 }
 
 namespace tracing {
@@ -97,7 +100,10 @@ class ChromeMainDelegate : public content::ContentMainDelegate {
 
   ChromeContentClient chrome_content_client_;
 
-  memory_system::MemorySystem memory_system_;
+  // The controller schedules UMA heap profiles collections and forwarding down
+  // the reporting pipeline.
+  std::unique_ptr<heap_profiling::HeapProfilerController>
+      heap_profiler_controller_;
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   std::unique_ptr<chromeos::LacrosService> lacros_service_;
