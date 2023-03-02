3b74b393c9d8a1eba86cd69577325d68ee5d5808
Yann Dago <ydago@chromium.org>
2023-03-02 20:08:25 +0000

    Ensure primary account can't change with signout disallowed
    Bug: 1369982
    Change-Id: I1728a6ea950a618199ff86a7899c92e85b660387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4213772
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112390}

--- components/signin/internal/identity_manager/primary_account_mutator_impl.h
+++ components/signin/internal/identity_manager/primary_account_mutator_impl.h
@@ -15,6 +15,7 @@ class AccountTrackerService;
 class PrefService;
 class ProfileOAuth2TokenService;
 class PrimaryAccountManager;
+class SigninClient;
 
 namespace signin {
 
@@ -27,6 +28,7 @@ class PrimaryAccountMutatorImpl : public PrimaryAccountMutator {
       ProfileOAuth2TokenService* token_service,
       PrimaryAccountManager* primary_account_manager,
       PrefService* pref_service,
+      SigninClient* signin_client,
       signin::AccountConsistencyMethod account_consistency);
   ~PrimaryAccountMutatorImpl() override;
 
@@ -58,6 +60,7 @@ class PrimaryAccountMutatorImpl : public PrimaryAccountMutator {
   raw_ptr<PrimaryAccountManager, DanglingUntriaged> primary_account_manager_ =
       nullptr;
   raw_ptr<PrefService> pref_service_ = nullptr;
+  raw_ptr<SigninClient> signin_client_ = nullptr;
   signin::AccountConsistencyMethod account_consistency_;
 };
 
--- components/signin/public/identity_manager/identity_test_environment.h
+++ components/signin/public/identity_manager/identity_test_environment.h
@@ -115,6 +115,8 @@ class IdentityTestEnvironment : public IdentityManager::DiagnosticsObserver,
   // The IdentityManager instance associated with this instance.
   IdentityManager* identity_manager();
 
+  SigninClient* signin_client();
+
   // Returns the |TestIdentityManagerObserver| watching the IdentityManager.
   TestIdentityManagerObserver* identity_manager_observer();
 
