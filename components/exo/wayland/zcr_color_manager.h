278460b50bab9ec68037495b7a34e9a5850f13ca
Femi Adegunloye <mrfemi@google.com>
2023-03-02 20:12:09 +0000

    lacros/hdr: use highest supported ColorSpace protocol version
    This code properly uprevs the various chrome-color-management interfaces
    to the same versions, following wayland protocol standards. Exo will now
    only bind the minimum of it's latest version of the protocol and the
    version requested by the client. This should prevent errors when
    different versions of the server and client are running.
    
    Before the version of a color_space_v1 object could not be inferred from
    its parent color_management_output or color_management_surface object,
    since the the parent was at a lower version. Now color_space and every
    object that returns a new one has been bumped to version 4.
    
    BUG=b:243150091
    TEST=build and deploy on dut
    
    Change-Id: Id0be7c69afeab20312f5374b1bcc5dbf01bd01c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297788
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Femi Adegunloye <mrfemi@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112397}

--- components/exo/wayland/zcr_color_manager.h
+++ components/exo/wayland/zcr_color_manager.h
@@ -13,7 +13,7 @@ struct wl_client;
 namespace exo {
 namespace wayland {
 
-constexpr uint32_t kZcrColorManagerVersion = 3;
+constexpr uint32_t kZcrColorManagerVersion = 4;
 
 void bind_zcr_color_manager(wl_client* client,
                             void* data,
