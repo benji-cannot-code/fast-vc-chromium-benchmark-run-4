d5cb11ccfd3b75a2c16db297b9bd026b80a837c2
Avi Drissman <avi@chromium.org>
2023-03-02 22:10:52 +0000

    Remove setCurrentAppearance
    It is deprecated; update native_color_mixers_mac to use non-deprecated
    API on newer macOS releases.
    
    Also do cleanup on other related code.
    
    Bug: 1420707
    Fixed: 1420646
    Change-Id: I6538374263b01f142a90d52d57e5bde3900e0c9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300392
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112467}

--- ui/native_theme/native_theme_mac.mm
+++ ui/native_theme/native_theme_mac.mm
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/color/color_provider.h"
-#include "ui/color/mac/scoped_current_nsappearance.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
