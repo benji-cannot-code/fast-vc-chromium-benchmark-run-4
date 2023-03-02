658a8cf394197c1bed987b0f117508aa3298b5b8
Claudio DeSouza <cdesouza@igalia.com>
2023-03-02 11:28:34 +0000

    [json-schema-compiler] Refactor to Params::Create (33/74)
    This CL removes the use of Params::CreateDeprecated, replacing it with
    the modernised Params::Create.
    
    Changes for path:
      /chrome/browser/extensions/api/platform_keys
    
    This CL was uploaded by git cl split.
    
    R=pmarko@chromium.org
    
    Bug: 1354063, 1414632
    Change-Id: I30723de3f5d4404e220ba3d890db84e8ed5f7636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299694
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112138}

--- chrome/browser/extensions/api/platform_keys/verify_trust_api.cc
+++ chrome/browser/extensions/api/platform_keys/verify_trust_api.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_verify_result.h"
 #include "net/cert/x509_certificate.h"
 #include "net/log/net_log_with_source.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace extensions {
 
@@ -47,7 +48,7 @@ class VerifyTrustAPI::IOPart {
   // with the result (see the declaration of VerifyCallback).
   // Will not call back after this object is destructed or the verifier for this
   // extension is deleted (see OnExtensionUnloaded).
-  void Verify(std::unique_ptr<Params> params,
+  void Verify(absl::optional<Params> params,
               const std::string& extension_id,
               VerifyCallback callback);
 
@@ -102,7 +103,7 @@ VerifyTrustAPI::~VerifyTrustAPI() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 }
 
-void VerifyTrustAPI::Verify(std::unique_ptr<Params> params,
+void VerifyTrustAPI::Verify(absl::optional<Params> params,
                             const std::string& extension_id,
                             VerifyCallback ui_callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
@@ -155,7 +156,7 @@ VerifyTrustAPI::IOPart::~IOPart() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
 }
 
-void VerifyTrustAPI::IOPart::Verify(std::unique_ptr<Params> params,
+void VerifyTrustAPI::IOPart::Verify(absl::optional<Params> params,
                                     const std::string& extension_id,
                                     VerifyCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
