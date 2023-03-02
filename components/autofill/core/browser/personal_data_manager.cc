432a54d5b6543ec8a0da3bcc877481f2e0e8d121
Marc Treib <treib@chromium.org>
2023-03-02 20:57:04 +0000

    PersonalDataManager cleanup: Use SyncService::GetAccountInfo()
    ...instead of the standalone syncer::DetermineAccountToUse() from
    sync_auth_util.h.
    The two are basically equivalent (SyncService internally uses
    DetermineAccountToUse()), and this will allow us to stop exposing
    DetermineAccountToUse().
    
    Bug: none
    Change-Id: Ia3c966afac007a1dfc2755077eae21e5aee8e702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300389
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112423}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -71,11 +71,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
-#include "components/sync/driver/sync_auth_util.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_service_utils.h"
 #include "components/version_info/version_info.h"
-#include "google_apis/gaia/gaia_auth_util.h"
 #include "third_party/libaddressinput/src/cpp/include/libaddressinput/address_data.h"
 #include "third_party/libaddressinput/src/cpp/include/libaddressinput/address_formatter.h"
 #include "third_party/libaddressinput/src/cpp/include/libaddressinput/source.h"
@@ -642,7 +640,7 @@ absl::optional<CoreAccountInfo> PersonalDataManager::GetPrimaryAccountInfo()
 AutofillSyncSigninState PersonalDataManager::GetSyncSigninState() const {
   // Check if the user is signed out.
   if (!sync_service_ || !identity_manager_ ||
-      syncer::DetermineAccountToUse(identity_manager_).account_info.IsEmpty()) {
+      sync_service_->GetAccountInfo().IsEmpty()) {
     return AutofillSyncSigninState::kSignedOut;
   }
 
