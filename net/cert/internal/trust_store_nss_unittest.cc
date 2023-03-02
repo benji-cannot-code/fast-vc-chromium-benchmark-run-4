0eab72a5253afb19ead6293f51a1f3abf610bc29
Matt Mueller <mattm@chromium.org>
2023-03-02 20:39:57 +0000

    add debug_data for TrustStoreNSS and pipe it through trial verifier code
    When running the dual verifier trial on linux & chromeos, this will
    record in any trial reports the NSS library version as well as the
    TrustStoreNSS parameters (whether system trust settings are being
    ignored, and whether a slot filter is being used).
    
    Bug: 1340420
    Change-Id: I10c665b2e474a93a7588dfccd8a8ffa4b75134c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296558
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112413}

--- net/cert/internal/trust_store_nss_unittest.cc
+++ net/cert/internal/trust_store_nss_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/supports_user_data.h"
 #include "base/test/scoped_feature_list.h"
 #include "crypto/nss_util_internal.h"
 #include "crypto/scoped_test_nss_db.h"
@@ -452,11 +453,12 @@ class TrustStoreNSSTestBase : public ::testing::Test {
 
 // Specifies which kind of per-slot filtering the TrustStoreNSS is supposed to
 // perform in the parametrized TrustStoreNSSTestWithSlotFilterType.
-enum class SlotFilterType {
-  kDontFilter,
-  kDoNotAllowUserSlots,
-  kAllowSpecifiedUserSlot
-};
+// TODO(https://crbug.com/1412591): The SlotFilterType enum is shared with
+// TrustStoreNSS::ResultDebugData::SlotFilterType for convenience. Once the old
+// code path and trial code is cleaned up, the type definition can be moved
+// back to here.
+using SlotFilterType = TrustStoreNSS::ResultDebugData::SlotFilterType;
+
 std::string SlotFilterTypeToString(SlotFilterType slot_filter_type) {
   switch (slot_filter_type) {
     case SlotFilterType::kDontFilter:
@@ -500,6 +502,23 @@ class TrustStoreNSSTestWithSlotFilterType
     }
   }
 };
+
+class DebugData : public base::SupportsUserData {
+ public:
+  ~DebugData() override = default;
+};
+
+TEST_P(TrustStoreNSSTestWithSlotFilterType, DebugData) {
+  DebugData debug_data;
+  trust_store_nss_->GetTrust(target_.get(), &debug_data);
+  const TrustStoreNSS::ResultDebugData* trust_debug_data =
+      TrustStoreNSS::ResultDebugData::Get(&debug_data);
+  ASSERT_TRUE(trust_debug_data);
+  EXPECT_EQ(system_trust_setting() ==
+                TrustStoreNSS::SystemTrustSetting::kIgnoreSystemTrust,
+            trust_debug_data->ignore_system_trust_settings());
+  EXPECT_EQ(slot_filter_type(), trust_debug_data->slot_filter_type());
+}
 
 // Without adding any certs to the NSS DB, should get no anchor results for
 // any of the test certs.
