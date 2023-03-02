e274aef9299c7541dabb70797fc94291b6737bf7
David Padlipsky <dpad@google.com>
2023-03-02 23:13:37 +0000

    Settings Split: Add per device settings methods to input controller
    Adds optional value for mouse/pointing stick/touchpad settings functions
    to enable settings to be applied per-device.
    
    Bug: b/241965700
    Change-Id: Ia2180c4515489e2665ba67161c625fc3e81d49e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237624
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112507}

--- ash/touch/touch_devices_controller.cc
+++ ash/touch/touch_devices_controller.cc
@@ -169,7 +169,7 @@ void TouchDevicesController::UpdateTapDraggingEnabled() {
   UMA_HISTOGRAM_BOOLEAN("Touchpad.TapDragging.Changed", enabled);
 
   ui::OzonePlatform::GetInstance()->GetInputController()->SetTapDragging(
-      enabled);
+      absl::nullopt, enabled);
 }
 
 void TouchDevicesController::UpdateTouchpadEnabled() {
--- ui/events/ozone/evdev/input_controller_evdev_unittest.cc
+++ ui/events/ozone/evdev/input_controller_evdev_unittest.cc
@@ -12,8 +12,8 @@ namespace ui {
 
 TEST(InputControllerEvdevTest, AccelerationSuspension) {
   InputControllerEvdev controller(nullptr, nullptr, nullptr);
-  controller.SetMouseAcceleration(true);
-  controller.SetPointingStickAcceleration(true);
+  controller.SetMouseAcceleration(absl::nullopt, true);
+  controller.SetPointingStickAcceleration(absl::nullopt, true);
 
   EXPECT_TRUE(controller.input_device_settings_.GetMouseSettings()
                   .acceleration_enabled);
@@ -37,8 +37,8 @@ TEST(InputControllerEvdevTest, AccelerationSuspension) {
 
 TEST(InputControllerEvdevTest, AccelerationChangeDuringSuspension) {
   InputControllerEvdev controller(nullptr, nullptr, nullptr);
-  controller.SetMouseAcceleration(true);
-  controller.SetPointingStickAcceleration(true);
+  controller.SetMouseAcceleration(absl::nullopt, true);
+  controller.SetPointingStickAcceleration(absl::nullopt, true);
 
   // Suspending should disable the acceleration temporarily.
   controller.SuspendMouseAcceleration();
@@ -48,16 +48,16 @@ TEST(InputControllerEvdevTest, AccelerationChangeDuringSuspension) {
                    .acceleration_enabled);
 
   // Settings changes while suspended should not take effect immediately...
-  controller.SetMouseAcceleration(true);
-  controller.SetPointingStickAcceleration(true);
+  controller.SetMouseAcceleration(absl::nullopt, true);
+  controller.SetPointingStickAcceleration(absl::nullopt, true);
   EXPECT_FALSE(controller.input_device_settings_.GetMouseSettings()
                    .acceleration_enabled);
   EXPECT_FALSE(controller.input_device_settings_.GetPointingStickSettings()
                    .acceleration_enabled);
 
   // ...instead being applied when the suspension ends.
-  controller.SetMouseAcceleration(false);
-  controller.SetPointingStickAcceleration(false);
+  controller.SetMouseAcceleration(absl::nullopt, false);
+  controller.SetPointingStickAcceleration(absl::nullopt, false);
   controller.EndMouseAccelerationSuspension();
   EXPECT_FALSE(controller.input_device_settings_.GetMouseSettings()
                    .acceleration_enabled);
