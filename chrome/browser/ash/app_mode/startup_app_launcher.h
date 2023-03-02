a2b13a94c8d4c1509026e1bea0e108e0e2a85a98
Ben Franz <bfranz@google.com>
2023-03-02 21:58:55 +0000

    Make sure Lacros is launched in Kiosk crash recovery flow
    During kiosk crash recovery flow, we skip the install step. This was the step that normally ensures that Lacros was running. This meant that previously we couldn't recover from crashes, as Lacros wasn't started.
    
    Note that in this flow Lacros is actually being started from BrowserManager::OnSessionStateChanged and we're only waiting for Lacros to come up now.
    
    Bug: b:269458215
    Test: StartupAppLauncherUsingLacrosTest
    Test: Manual crash recover
    Change-Id: I05dfcf193db04a521665b170f402ac80068bba23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4272249
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112464}

--- chrome/browser/ash/app_mode/startup_app_launcher.h
+++ chrome/browser/ash/app_mode/startup_app_launcher.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_launcher.h"
@@ -61,6 +62,7 @@ class StartupAppLauncher : public KioskAppLauncher,
   void LaunchApp() override;
 
   void BeginInstall();
+  void LaunchLacros(base::OnceClosure);
   void InstallAppInAsh();
   void InstallAppInLacros();
   void OnInstallComplete(ChromeKioskAppInstaller::InstallResult result);
