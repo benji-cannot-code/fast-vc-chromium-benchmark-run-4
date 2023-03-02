aa12f6734f097eeb77ef225e90b79497c279bf6d
Brahim Chikhaoui <bchikhaoui@google.com>
2023-03-02 11:41:49 +0000

    [CrOS] Introduce feature flag for OSSyncConsentScreen Revamp
    Introduce a feature flag to enable the new OSSyncConsent Screen
    Revamp during OOBE and Settings.
    
    Bug: b:271242994
    Change-Id: Ibca854b6bb21eab85081cadf44d4c78dfb3f3c1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294781
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112140}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1531,6 +1531,12 @@ BASE_FEATURE(kOsSettingsAppNotificationsPage,
              "OsSettingsAppNotificationsPage",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// If enabled, OsSyncConsent Revamp will be shown.
+// enabling this without enabling Lacros flag will have no effect
+BASE_FEATURE(kOsSyncConsentRevamp,
+             "OsSyncConsentRevamp",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables app badging toggle to be displayed in app notification page in
 // ChromeOS Settings.
 BASE_FEATURE(kOsSettingsAppBadgingToggle,
@@ -2940,6 +2946,10 @@ bool IsOsSettingsAppBadgingToggleEnabled() {
 
 bool IsOsSettingsSearchFeedbackEnabled() {
   return base::FeatureList::IsEnabled(kOsSettingsSearchFeedback);
+}
+
+bool IsOsSyncConsentRevampEnabled() {
+  return base::FeatureList::IsEnabled(kOsSyncConsentRevamp);
 }
 
 bool IsOverviewDeskNavigationEnabled() {
