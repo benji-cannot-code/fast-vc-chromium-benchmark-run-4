504f6394358056ca75b4531611775b44ae1c4082
Jan Keitel <jkeitel@google.com>
2023-03-02 12:32:51 +0000

    Avoid dangling pointer in Autocomplete browsertest.
    Bug: 1419786
    Change-Id: Iec48d976a4e3bb0bbe64c999ea64342fb152becf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4302737
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112152}

--- chrome/browser/autofill/autofill_autocomplete_browsertest.cc
+++ chrome/browser/autofill/autofill_autocomplete_browsertest.cc
@@ -94,6 +94,7 @@ class AutofillAutocompleteTest : public InProcessBrowserTest {
     // Make sure to close any showing popups prior to tearing down the UI.
     content::WebContents* web_contents =
         active_browser_->tab_strip_model()->GetActiveWebContents();
+    active_browser_ = nullptr;
     ContentAutofillDriverFactory::FromWebContents(web_contents)
         ->DriverForFrame(web_contents->GetPrimaryMainFrame())
         ->autofill_manager()
@@ -239,7 +240,7 @@ class AutofillAutocompleteTest : public InProcessBrowserTest {
   Profile* current_profile() { return active_browser_->profile(); }
 
   test::AutofillEnvironment autofill_environment_;
-  raw_ptr<Browser, DanglingUntriaged> active_browser_;
+  raw_ptr<Browser> active_browser_ = nullptr;
 };
 
 // Tests that a user can save a simple Autocomplete value.
