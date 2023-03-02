450e8f6f581ffd315b76d25024c34da788f8cf65
Ari Chivukula <arichiv@chromium.org>
2023-03-02 21:13:24 +0000

    Remove expired histograms Ash.Shelf.Palette.ModeCancellation, Ash.Shelf.Palette.Usage.AutoOpened, Ash.Shelf.Palette.Usage.Shortcut
    These histograms haven't been recorded since 2021 so removal seems to be
    the correct path here. This is a code-health task.
    
    Bug: 1384719
    Change-Id: Iba13d7911a356651f912e64c260698a0425b6213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300449
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112431}

--- ash/system/palette/mock_palette_tool_delegate.h
+++ ash/system/palette/mock_palette_tool_delegate.h
@@ -26,10 +26,6 @@ class MockPaletteToolDelegate : public PaletteTool::Delegate {
               RecordPaletteOptionsUsage,
               (PaletteTrayOptions option, PaletteInvocationMethod method),
               (override));
-  MOCK_METHOD(void,
-              RecordPaletteModeCancellation,
-              (PaletteModeCancelType type),
-              (override));
 };
 
 }  // namespace ash
