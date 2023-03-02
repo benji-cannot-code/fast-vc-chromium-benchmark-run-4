2599a9416aedde75275d5f7c1311bdc646213dc8
Rafał Godlewski <rgod@google.com>
2023-03-02 01:27:23 +0000

    [iOS] Add handling long notes in password details view
    * Adds section for note footer and handles displaying it over 900
     chars in note field
    * Adds disabling save button when note has over 1000 chars
    
    Screencast: https://drive.google.com/file/d/1UjwUe0omjDYitx5btIaHPlpHewRIseh5
    
    Bug: 1414897
    Change-Id: I70b08111e5c76c9f3ed391a08205b20640cda87f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290478
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111966}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_constants.h
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_constants.h
@@ -19,6 +19,7 @@ typedef NS_ENUM(NSInteger, PasswordDetailsItemType) {
   PasswordDetailsItemTypeUsername,
   PasswordDetailsItemTypePassword,
   PasswordDetailsItemTypeNote,
+  PasswordDetailsItemTypeNoteFooter,
   PasswordDetailsItemTypeFederation,
   PasswordDetailsItemTypeChangePasswordButton,
   PasswordDetailsItemTypeChangePasswordRecommendation,
