3228b2af4cd2088c9931165ac0ca2694a42a155c
Kent Tamura <tkent@chromium.org>
2023-03-03 08:25:02 +0000

    NoCopyBack: Fix a crash in LayoutMultiColumnFlowThread::UpdateGeometry()
    ClearNeedsLayout() for a LayoutMultiColumnFlowThread was called too
    early, and UpdateGeometry() tried to compute values from old physical
    fragments.  A ClearNeedsLayout() call from
    LayoutMultiColumnFlowThread::FinishLayoutFromNG() is enough.
    
    Bug: 1420475
    Change-Id: I1637d97b8d946bd34dad6335838195c7561166e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4302189
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112677}

--- third_party/blink/renderer/core/layout/ng/ng_block_node.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_node.cc
@@ -2192,7 +2192,6 @@ void NGBlockNode::StoreColumnSizeAndCount(LayoutUnit inline_size, int count) {
   // We have no chance to unregister the inline size for the
   // LayoutMultiColumnFlowThread.
   TextAutosizer::MaybeRegisterInlineSize(*flow_thread, inline_size);
-  flow_thread->ClearNeedsLayout();
 
   flow_thread->SetColumnCountFromNG(count);
 }
