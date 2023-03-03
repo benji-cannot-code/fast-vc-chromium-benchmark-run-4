f94f0c13c19e37a4441af68905384ef428f9ec0f
Alex Chen <alexwchen@chromium.org>
2023-03-03 17:25:41 +0000

    Add RealTimeDownloadProtectionAllowed policy
    Add policy definition, pref, and policy to pref mapping. This policy
    allows admins to disable file download protection from sending data
    to Google Safe Browsing servers. This policy will not impact hash
    based download URL checks nor will it impact enterprise connector
    settings. If RealTimeDownloadProtectionAllowed is true, file download
    protection will follow existing behavior from
    SafeBrowsingProtectionLevel and any connector settings. If the new
    policy is false, RealTimeDownloadProtectionAllowed will override
    SafeBrowsingProtectionLevel and disable sending ClientDownloadRequests to Google Safe Browsing to check content of a file.
    
    Unit/browser tests have been added to:
     - Confirm existing behavior with policy enabled/not set
     - Confirm new behavior with policy disabled
    
    Bug: b:264433736
    Change-Id: I8814019800b00ead6a1f0c5c9c4039d0ab3f651c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4162452
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112843}

--- components/safe_browsing/core/common/safe_browsing_prefs.cc
+++ components/safe_browsing/core/common/safe_browsing_prefs.cc
@@ -119,6 +119,8 @@ const char kExtensionTelemetryConfig[] =
     "safebrowsing.extension_telemetry_configuration";
 const char kExtensionTelemetryFileData[] =
     "safebrowsing.extension_telemetry_file_data";
+const char kRealTimeDownloadProtectionRequestAllowedByPolicy[] =
+    "safebrowsing.real_time_download_protection_request_allowed_by_policy";
 }  // namespace prefs
 
 namespace safe_browsing {
@@ -188,6 +190,11 @@ bool IsSafeBrowsingPolicyManaged(const PrefService& prefs) {
          prefs.IsManagedPreference(prefs::kSafeBrowsingEnhanced);
 }
 
+bool IsRealTimeDownloadProtectionRequestAllowed(const PrefService& prefs) {
+  return prefs.GetBoolean(
+      prefs::kRealTimeDownloadProtectionRequestAllowedByPolicy);
+}
+
 void RecordExtendedReportingMetrics(const PrefService& prefs) {
   // This metric tracks the extended browsing opt-in based on whichever setting
   // the user is currently seeing. It tells us whether extended reporting is
@@ -243,6 +250,8 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
                                    base::Value::Dict());
   registry->RegisterDictionaryPref(prefs::kExtensionTelemetryFileData,
                                    base::Value::Dict());
+  registry->RegisterBooleanPref(
+      prefs::kRealTimeDownloadProtectionRequestAllowedByPolicy, true);
 }
 
 const base::Value::Dict& GetExtensionTelemetryConfig(const PrefService& prefs) {
@@ -349,6 +358,11 @@ base::Value::List GetSafeBrowsingPoliciesList(PrefService* prefs) {
   }
   preferences_list.Append(login_urls);
   preferences_list.Append(prefs::kPasswordProtectionLoginURLs);
+
+  preferences_list.Append(prefs->GetBoolean(
+      prefs::kRealTimeDownloadProtectionRequestAllowedByPolicy));
+  preferences_list.Append(
+      prefs::kRealTimeDownloadProtectionRequestAllowedByPolicy);
   return preferences_list;
 }
 
