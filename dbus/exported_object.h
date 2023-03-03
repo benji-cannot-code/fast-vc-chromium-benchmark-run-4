33c667390a6476087b5197669ba06bd84f3aa3fa
Ari Chivukula <arichiv@chromium.org>
2023-03-03 20:01:26 +0000

    Remove expired histograms DBus.ExportedMethodHandleSuccess, DBus.ExportedMethodHandleTime, DBus.SignalSendTime
    These histograms haven't been recorded since M85 so removal seems to be
    the correct path here. This is a code-health task.
    
    Bug: 1384732
    Change-Id: I25f14eec343fb76fdc95e3684bc6db30fa93cbc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307349
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112930}

--- dbus/exported_object.h
+++ dbus/exported_object.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/platform_thread.h"
-#include "base/time/time.h"
 #include "dbus/dbus_export.h"
 #include "dbus/object_path.h"
 
@@ -161,8 +160,7 @@ class CHROME_DBUS_EXPORT ExportedObject
                     bool success);
 
   // Helper function for SendSignal().
-  void SendSignalInternal(base::TimeTicks start_time,
-                          DBusMessage* signal_message);
+  void SendSignalInternal(DBusMessage* signal_message);
 
   // Registers this object to the bus.
   // Returns true on success, or the object is already registered.
@@ -177,21 +175,18 @@ class CHROME_DBUS_EXPORT ExportedObject
 
   // Runs the method. Helper function for HandleMessage().
   void RunMethod(const MethodCallCallback& method_call_callback,
-                 std::unique_ptr<MethodCall> method_call,
-                 base::TimeTicks start_time);
+                 std::unique_ptr<MethodCall> method_call);
 
   // Callback invoked by service provider to send a response to a method call.
   // Can be called immediately from a MethodCallCallback to implement a
   // synchronous service or called later to implement an asynchronous service.
-  void SendResponse(base::TimeTicks start_time,
-                    std::unique_ptr<MethodCall> method_call,
+  void SendResponse(std::unique_ptr<MethodCall> method_call,
                     std::unique_ptr<Response> response);
 
   // Called on completion of the method run from SendResponse().
   // Takes ownership of |method_call| and |response|.
   void OnMethodCompleted(std::unique_ptr<MethodCall> method_call,
-                         std::unique_ptr<Response> response,
-                         base::TimeTicks start_time);
+                         std::unique_ptr<Response> response);
 
   // Called when the object is unregistered.
   void OnUnregistered(DBusConnection* connection);
