3c6cb90ac2ffdfa2e8f6d178c503d7cb394806e0
Roman Arora <romanarora@chromium.org>
2023-03-03 21:05:39 +0000

    [NTP Journeys] History Clusters - Fake data variation
    Bug: 1418691
    Change-Id: I3fa1f7e5d144a2287b757dc86b5a06d1024494c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288466
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112974}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -293,6 +293,8 @@ const char kNtpDriveModuleCacheMaxAgeSParam[] =
     "NtpDriveModuleCacheMaxAgeSParam";
 const char kNtpDriveModuleExperimentGroupParam[] =
     "NtpDriveModuleExperimentGroupParam";
+const char kNtpHistoryClustersModuleDataParam[] =
+    "NtpRecipeTasksModuleDataParam";
 const char kNtpMiddleSlotPromoDismissalParam[] =
     "NtpMiddleSlotPromoDismissalParam";
 const char kNtpPhotosModuleDataParam[] = "NtpPhotosModuleDataParam";
