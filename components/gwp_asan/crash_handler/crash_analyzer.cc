20dce353a981cee2ae501d299675392ec789cfa7
Ari Chivukula <arichiv@chromium.org>
2023-03-03 02:06:21 +0000

    Remove expired histograms GwpAsan.CrashAnalysisResult, GwpAsan.CrashAnalysisResult.Malloc, GwpAsan.CrashAnalysisResult.PartitionAlloc
    These histograms haven't been recorded since 2020 so removal seems to be
    the correct path here. This is a code-health task.
    
    Bug: 1384744
    Change-Id: I1c69c0273f2987af2e8e90e150bd3d3fbcee494e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304140
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112604}

--- components/gwp_asan/crash_handler/crash_analyzer.cc
+++ components/gwp_asan/crash_handler/crash_analyzer.cc
@@ -38,30 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gwp_asan {
 namespace internal {
 
-namespace {
-
-// Report failure for a particular allocator's histogram.
-void ReportHistogram(Crash_Allocator allocator,
-                     GwpAsanCrashAnalysisResult result) {
-  DCHECK_LE(result, GwpAsanCrashAnalysisResult::kMaxValue);
-
-  switch (allocator) {
-    case Crash_Allocator_MALLOC:
-      UMA_HISTOGRAM_ENUMERATION("GwpAsan.CrashAnalysisResult.Malloc", result);
-      break;
-
-    case Crash_Allocator_PARTITIONALLOC:
-      UMA_HISTOGRAM_ENUMERATION("GwpAsan.CrashAnalysisResult.PartitionAlloc",
-                                result);
-      break;
-
-    default:
-      DCHECK(false) << "Unknown allocator value!";
-  }
-}
-
-}  // namespace
-
 using GetMetadataReturnType = AllocatorState::GetMetadataReturnType;
 
 bool CrashAnalyzer::GetExceptionInfo(
@@ -147,8 +123,6 @@ bool CrashAnalyzer::GetAllocatorState(
 
   if (!exception->Context()) {
     DLOG(ERROR) << "Missing crash CPU context information.";
-    ReportHistogram(allocator,
-                    GwpAsanCrashAnalysisResult::kErrorNullCpuContext);
     return false;
   }
 
@@ -162,31 +136,22 @@ bool CrashAnalyzer::GetAllocatorState(
   // state bitness-independently.
   if (exception->Context()->Is64Bit() != is_64_bit) {
     DLOG(ERROR) << "Mismatched process bitness.";
-    ReportHistogram(allocator,
-                    GwpAsanCrashAnalysisResult::kErrorMismatchedBitness);
     return false;
   }
 
   const crashpad::ProcessMemory* memory = process_snapshot.Memory();
   if (!memory) {
     DLOG(ERROR) << "Null ProcessMemory.";
-    ReportHistogram(allocator,
-                    GwpAsanCrashAnalysisResult::kErrorNullProcessMemory);
     return false;
   }
 
   if (!memory->Read(gpa_addr, sizeof(*state), state)) {
     DLOG(ERROR) << "Failed to read AllocatorState from process.";
-    ReportHistogram(allocator,
-                    GwpAsanCrashAnalysisResult::kErrorFailedToReadAllocator);
     return false;
   }
 
   if (!state->IsValid()) {
     DLOG(ERROR) << "Allocator sanity check failed!";
-    ReportHistogram(
-        allocator,
-        GwpAsanCrashAnalysisResult::kErrorAllocatorFailedSanityCheck);
     return false;
   }
 
@@ -232,8 +197,6 @@ bool CrashAnalyzer::AnalyzeCrashedAllocator(
           sizeof(AllocatorState::SlotMetadata) * valid_state.num_metadata,
           metadata_arr.get())) {
     proto->set_internal_error("Failed to read metadata.");
-    ReportHistogram(allocator,
-                    GwpAsanCrashAnalysisResult::kErrorFailedToReadSlotMetadata);
     return true;
   }
 
@@ -245,9 +208,6 @@ bool CrashAnalyzer::AnalyzeCrashedAllocator(
                                            valid_state.total_reserved_pages,
                                        slot_to_metadata.get())) {
     proto->set_internal_error("Failed to read slot_to_metadata.");
-    ReportHistogram(
-        allocator,
-        GwpAsanCrashAnalysisResult::kErrorFailedToReadSlotMetadataMapping);
     return true;
   }
 
@@ -256,14 +216,6 @@ bool CrashAnalyzer::AnalyzeCrashedAllocator(
   auto ret = valid_state.GetMetadataForAddress(
       exception_addr, metadata_arr.get(), slot_to_metadata.get(), &metadata_idx,
       &error);
-  if (ret == GetMetadataReturnType::kErrorBadSlot)
-    ReportHistogram(allocator, GwpAsanCrashAnalysisResult::kErrorBadSlot);
-  if (ret == GetMetadataReturnType::kErrorBadMetadataIndex)
-    ReportHistogram(allocator,
-                    GwpAsanCrashAnalysisResult::kErrorBadMetadataIndex);
-  if (ret == GetMetadataReturnType::kErrorOutdatedMetadataIndex)
-    ReportHistogram(allocator,
-                    GwpAsanCrashAnalysisResult::kErrorOutdatedMetadataIndex);
   if (!error.empty()) {
     proto->set_internal_error(error);
     return true;
@@ -288,7 +240,6 @@ bool CrashAnalyzer::AnalyzeCrashedAllocator(
                          metadata.dealloc, proto->mutable_deallocation());
   }
 
-  ReportHistogram(allocator, GwpAsanCrashAnalysisResult::kGwpAsanCrash);
   return true;
 }
 
--- components/gwp_asan/crash_handler/crash_analyzer_unittest.cc
+++ components/gwp_asan/crash_handler/crash_analyzer_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/stack_trace.h"
 #include "base/functional/callback_helpers.h"
 #include "base/test/gtest_util.h"
-#include "base/test/metrics/histogram_tester.h"
 #include "build/build_config.h"
 #include "components/gwp_asan/client/guarded_page_allocator.h"
 #include "components/gwp_asan/common/allocator_state.h"
@@ -36,15 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gwp_asan {
 namespace internal {
-
-namespace {
-
-constexpr const char* kMallocHistogramName =
-    "GwpAsan.CrashAnalysisResult.Malloc";
-constexpr const char* kPartitionAllocHistogramName =
-    "GwpAsan.CrashAnalysisResult.PartitionAlloc";
-
-}  // namespace
 
 class CrashAnalyzerTest : public testing::Test {
  protected:
@@ -114,16 +104,10 @@ TEST_F(CrashAnalyzerTest, StackTraceCollection) {
   // Lets pretend a double free() occurred on the allocation we saw previously.
   gpa_.state_.double_free_address = reinterpret_cast<uintptr_t>(ptr);
 
-  base::HistogramTester histogram_tester;
   gwp_asan::Crash proto;
   bool proto_present =
       CrashAnalyzer::GetExceptionInfo(process_snapshot_, &proto);
   ASSERT_TRUE(proto_present);
-
-  int result = static_cast<int>(GwpAsanCrashAnalysisResult::kGwpAsanCrash);
-  EXPECT_THAT(histogram_tester.GetAllSamples(kMallocHistogramName),
-              testing::ElementsAre(base::Bucket(result, 1)));
-  histogram_tester.ExpectTotalCount(kPartitionAllocHistogramName, 0);
 
   ASSERT_TRUE(proto.has_allocation());
   ASSERT_TRUE(proto.has_deallocation());
@@ -168,17 +152,10 @@ TEST_F(CrashAnalyzerTest, InternalError) {
   // single entry slot/metadata entry.
   gpa_.slot_to_metadata_idx_[0] = 5;
 
-  base::HistogramTester histogram_tester;
   gwp_asan::Crash proto;
   bool proto_present =
       CrashAnalyzer::GetExceptionInfo(process_snapshot_, &proto);
   ASSERT_TRUE(proto_present);
-
-  int result =
-      static_cast<int>(GwpAsanCrashAnalysisResult::kErrorBadMetadataIndex);
-  EXPECT_THAT(histogram_tester.GetAllSamples(kMallocHistogramName),
-              testing::ElementsAre(base::Bucket(result, 1)));
-  histogram_tester.ExpectTotalCount(kPartitionAllocHistogramName, 0);
 
   EXPECT_TRUE(proto.has_internal_error());
   ASSERT_TRUE(proto.has_missing_metadata());
