216350548cf0cb0e48c3eca9186e74eca3383817
Philipp Hancke <phancke@microsoft.com>
2023-03-03 16:03:21 +0000

    webrtc-internals: shorten description of GUM/GDM tab
    and update the casing to match the API names.
    BUG=chromium:1375217
    
    Change-Id: I9b08577f980a5dde08da99b73eced114d2c9c605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303878
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Philipp Hancke <phancke@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1112799}

--- content/browser/webrtc/resources/user_media.js
+++ content/browser/webrtc/resources/user_media.js
@@ -34,7 +34,7 @@ export class UserMediaTable {
    */
   createTab() {
     const container = this.tabView.addTab(USER_MEDIA_TAB_ID,
-        'GetUserMedia/GetDisplayMedia Requests');
+        'getUserMedia/getDisplayMedia');
     // Create the filter input field and label.
     appendChildWithText(container, 'label', 'Filter by origin including ');
     const input = document.createElement('input');
