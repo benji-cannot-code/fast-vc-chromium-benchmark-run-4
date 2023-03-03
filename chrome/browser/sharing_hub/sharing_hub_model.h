9290d063f1deaaa1cc4555b354fe01ac98b6d2d0
Elly Fong-Jones <ellyjones@chromium.org>
2023-03-03 19:18:29 +0000

    desktop-sh: make an a11y announcement when copying a link
    There's an existing announcement message used for this on Android, so
    this change:
    1. Copies that message from the Android-specific strings file to the
       shared one
    2. Adds an announcement field to SharingHubAction - currently only link
       copy needs an announcement since all the other actions have other
       obvious feedback (new windows appearing, etc)
    3. Refactors the sharing hub to plumb the entire action structure
       through rather than a subset of its fields
    4. Adds code to SharingHubBubbleViewImpl to speak the action's
       announcement (if it has one) when calling back to the controller,
       if it has one
    
    This change has no automated tests; there is no cross-platform mechanism
    for observing a11y announcements, and adding per-platform unit test
    behavior for this seems excessively complex.
    
    Change-Id: Id8d3b8202e77a5d667b8d75fcbabbf589b6c8dec
    Fixed: 1268482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289663
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112910}

--- chrome/browser/sharing_hub/sharing_hub_model.h
+++ chrome/browser/sharing_hub/sharing_hub_model.h
@@ -32,7 +32,8 @@ struct SharingHubAction {
   SharingHubAction(int command_id,
                    std::u16string title,
                    const gfx::VectorIcon* icon,
-                   std::string feature_name_for_metrics);
+                   std::string feature_name_for_metrics,
+                   int announcement_id);
   SharingHubAction(const SharingHubAction&);
   SharingHubAction& operator=(const SharingHubAction&);
   SharingHubAction(SharingHubAction&&);
@@ -42,6 +43,7 @@ struct SharingHubAction {
   std::u16string title;
   raw_ptr<const gfx::VectorIcon> icon;
   std::string feature_name_for_metrics;
+  int announcement_id;
 };
 
 // The Sharing Hub model contains a list of first and third party actions.
--- chrome/browser/ui/views/sharing_hub/sharing_hub_bubble_view_impl.cc
+++ chrome/browser/ui/views/sharing_hub/sharing_hub_bubble_view_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/controls/separator.h"
@@ -118,8 +119,15 @@ void SharingHubBubbleViewImpl::OnActionSelected(
   if (!controller_)
     return;
 
-  controller_->OnActionSelected(button->action_command_id(),
-                                button->action_name_for_metrics());
+  // The announcement has to happen here rather than in the button itself: the
+  // button doesn't know whether controller_ will be null, so it doesn't know
+  // whether the action will actually happen.
+  const SharingHubAction& action = button->action_info();
+  if (action.announcement_id) {
+    GetViewAccessibility().AnnounceText(
+        l10n_util::GetStringUTF16(action.announcement_id));
+  }
+  controller_->OnActionSelected(action);
 
   Hide();
 }
