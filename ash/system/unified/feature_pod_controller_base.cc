bae012f4d4c70d4f2568d02cf6c58c8fcb92f394
Kevin Radtke <kradtke@chromium.org>
2023-03-03 23:30:46 +0000

    Feature Tiles Container Pagination Animation
    Animate switching pages in the Feature Tiles Container through scroll,
    drag gesture or by clicking a pagination dot.
    This is done by updating the bounds of the pages multiple times per
    transition, since we need a middle state when using drag gestures.
    
    Also removed placeholder feature tile code since all of Feature Pod
    controllers have implemented the `CreateTile()` function.
    
    Bug: b:262794866, b:266001863
    Change-Id: Ife344a110524d986ce96fdbc8640fc4eaba5877e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4305202
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113062}

--- ash/system/unified/feature_pod_controller_base.cc
+++ ash/system/unified/feature_pod_controller_base.cc
@@ -12,15 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-// TODO(b/252871301): Remove after implementing every FeatureTile and making
-// this function pure virtual.
-std::unique_ptr<FeatureTile> FeaturePodControllerBase::CreateTile(
-    bool compact) {
-  return std::make_unique<FeatureTile>(compact
-                                           ? FeatureTile::TileType::kCompact
-                                           : FeatureTile::TileType::kPrimary);
-}
-
 void FeaturePodControllerBase::OnLabelPressed() {
   return OnIconPressed();
 }
