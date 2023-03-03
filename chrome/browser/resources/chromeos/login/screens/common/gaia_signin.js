15965255c2566c197f963f0ab383e1a3b3ccafff
Bohdan Tyshchenko <bohdanty@google.com>
2023-03-03 13:32:34 +0000

    [OOBE] - Clear timer inside of gaia screen instead of using undefined
    Clear timeout timer inside the gaia screen using dedicated function
    rather than assigning undefined to it.
    
    Bug: b:269211070
    Change-Id: Id940b97477d67f9212cbac6fe10fd61f87d36a61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261153
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112745}

--- chrome/browser/resources/chromeos/login/screens/common/gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screens/common/gaia_signin.js
@@ -473,7 +473,7 @@ class GaiaSigninElement extends GaiaSigninElementBase {
     if (Oobe.getInstance().currentScreen.id != 'gaia-signin') {
       return;
     }
-    this.loadingTimer_ = undefined;
+    this.clearLoadingTimer_();
     chrome.send('showLoadingTimeoutError');
   }
 
