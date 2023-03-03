9f71a058fd46d22d87ebd2f2300042a27e7afbfb
Richard Zhang <richardzh@chromium.org>
2023-03-03 21:09:33 +0000

    remove EventRouter::Add/RemoveLazyEventListener
    Function `AddLazyEventListener()` and `RemoveLazyEventListener()` are
    only used for adding/removing lazy listeners for main thread (i.e. for
    background pages).
    
    `AddLazyListenerForMainThread()` and `RemoveLazyListenerForMainThread()`
    are merely thin wrappers which do nothing but calling the above two
    functions.
    
    Merging `Add/RemoveLazyEventListener()` into
    `Add/RemoveLazyListenerForMainThread()` clears the confusion of if they
    are used for service worker.
    
    It also reduces unnecessary layer that makes the code easy to
    understand. Especially for the caller function
    `DispatchEventWithLazyListener()`. The logic there becomes pairs of
    `...ForServiceWorker()` and `...ForMainThread()` functions.
    
    Bug: None
    Change-Id: I07edabf421ed827e789cc4e925dba04527fac61c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304966
    Commit-Queue: Richard Zhang <richardzh@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112977}

--- extensions/browser/event_router.cc
+++ extensions/browser/event_router.cc
@@ -339,7 +339,9 @@ void EventRouter::AddListenerForServiceWorker(const std::string& extension_id,
 void EventRouter::AddLazyListenerForMainThread(const std::string& extension_id,
                                                const std::string& event_name) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  AddLazyEventListener(event_name, extension_id);
+  std::unique_ptr<EventListener> listener = EventListener::CreateLazyListener(
+      event_name, extension_id, browser_context_, false, GURL(), absl::nullopt);
+  AddLazyEventListenerImpl(std::move(listener), RegisteredEventType::kLazy);
 }
 
 void EventRouter::AddLazyListenerForServiceWorker(
@@ -444,7 +446,9 @@ void EventRouter::RemoveLazyListenerForMainThread(
     const std::string& extension_id,
     const std::string& event_name) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  RemoveLazyEventListener(event_name, extension_id);
+  std::unique_ptr<EventListener> listener = EventListener::CreateLazyListener(
+      event_name, extension_id, browser_context_, false, GURL(), absl::nullopt);
+  RemoveLazyEventListenerImpl(std::move(listener), RegisteredEventType::kLazy);
 }
 
 void EventRouter::RemoveLazyListenerForServiceWorker(
@@ -637,20 +641,6 @@ void EventRouter::RenderProcessHostDestroyed(RenderProcessHost* host) {
   listeners_.RemoveListenersForProcess(host);
   observed_process_set_.erase(host);
   host->RemoveObserver(this);
-}
-
-void EventRouter::AddLazyEventListener(const std::string& event_name,
-                                       const ExtensionId& extension_id) {
-  std::unique_ptr<EventListener> listener = EventListener::CreateLazyListener(
-      event_name, extension_id, browser_context_, false, GURL(), absl::nullopt);
-  AddLazyEventListenerImpl(std::move(listener), RegisteredEventType::kLazy);
-}
-
-void EventRouter::RemoveLazyEventListener(const std::string& event_name,
-                                          const ExtensionId& extension_id) {
-  std::unique_ptr<EventListener> listener = EventListener::CreateLazyListener(
-      event_name, extension_id, browser_context_, false, GURL(), absl::nullopt);
-  RemoveLazyEventListenerImpl(std::move(listener), RegisteredEventType::kLazy);
 }
 
 void EventRouter::AddFilteredEventListener(
@@ -872,7 +862,7 @@ void EventRouter::DispatchEventWithLazyListener(const std::string& extension_id,
           extension_id, Extension::GetBaseURLFromExtensionId(extension_id),
           event_name);
     } else {
-      AddLazyEventListener(event_name, extension_id);
+      AddLazyListenerForMainThread(extension_id, event_name);
     }
   }
 
@@ -884,7 +874,7 @@ void EventRouter::DispatchEventWithLazyListener(const std::string& extension_id,
           extension_id, Extension::GetBaseURLFromExtensionId(extension_id),
           event_name);
     } else {
-      RemoveLazyEventListener(event_name, extension_id);
+      RemoveLazyListenerForMainThread(extension_id, event_name);
     }
   }
 }
