9a4a6f06b5bd49da37951b74847dff00aedd3245
Corentin Wallez <cwallez@chromium.org>
2023-03-03 01:08:49 +0000

    Run WebGPU CTS and pixel/context_lost/etc test with developer features.
    Running with developer features will allow the tests to access a few
    APIs that can help check correct behavior of Chromium. For example (not
    all are currently implemented):
    
     - GPUExternalTexture.isZeroCopy to check 0-copy code paths don't
       silently regress.
     - GPUDevice.loseForTesting() to fake device losses in tests.
     - Features gated behind origin trials like WebGPUWebCodecs.
    
    Bug: chromium:1409086
    Change-Id: Ied6155a64ba6d494ecba3834a9693168d37f082e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300434
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112584}

--- content/test/gpu/gpu_tests/context_lost_integration_test.py
+++ content/test/gpu/gpu_tests/context_lost_integration_test.py
@@ -348,9 +348,7 @@ class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
   def _ContextLost_WebGPUContextLostFromGPUProcessExit(self,
                                                        test_path: str) -> None:
-    self.RestartBrowserIfNecessaryWithArgs([
-        '--enable-unsafe-webgpu',
-    ])
+    self.RestartBrowserIfNecessaryWithArgs(cba.ENABLE_WEBGPU_FOR_TESTING)
     self._NavigateAndWaitForLoad(test_path)
     self.tab.EvaluateJavaScript(
         'chrome.gpuBenchmarking.terminateGpuProcessNormally()')
@@ -363,9 +361,7 @@ class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
   def _ContextLost_WebGPUStressRequestDeviceAndRemoveLoop(self, test_path: str
                                                           ) -> None:
-    self.RestartBrowserIfNecessaryWithArgs([
-        '--enable-unsafe-webgpu',
-    ])
+    self.RestartBrowserIfNecessaryWithArgs(cba.ENABLE_WEBGPU_FOR_TESTING)
     self._NavigateAndWaitForLoad(test_path)
 
     # Test runs for 90 seconds; wait for 120 seconds.
@@ -680,9 +676,7 @@ class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
     self._RestartBrowser('must restart after tests that kill the GPU process')
 
   def _ContextLost_WebGPUBlockedAfterJSNavigation(self, test_path: str) -> None:
-    self.RestartBrowserIfNecessaryWithArgs([
-        '--enable-unsafe-webgpu',
-    ])
+    self.RestartBrowserIfNecessaryWithArgs(cba.ENABLE_WEBGPU_FOR_TESTING)
     self._NavigateAndWaitForLoad(test_path)
 
     tab = self.tab
@@ -728,9 +722,7 @@ class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
     'Success' state while the second run only needs to reach 'Loaded' state to
     verify that a WebGPU has been unblocked.
     """
-    self.RestartBrowserIfNecessaryWithArgs([
-        '--enable-unsafe-webgpu',
-    ])
+    self.RestartBrowserIfNecessaryWithArgs(cba.ENABLE_WEBGPU_FOR_TESTING)
     # Make sure the tab loaded and initially got a WebGPU device.
     self._NavigateAndWaitForLoad(test_path)
     tab = self.tab
@@ -759,9 +751,7 @@ class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
   def _GpuNormalTermination_WebGPUNotBlocked(self, test_path: str) -> None:
     """Tests that normal GPU process termination does not block WebGPU.
     """
-    self.RestartBrowserIfNecessaryWithArgs([
-        '--enable-unsafe-webgpu',
-    ])
+    self.RestartBrowserIfNecessaryWithArgs(cba.ENABLE_WEBGPU_FOR_TESTING)
     self._NavigateAndWaitForLoad(test_path)
     tab = self.tab
 
