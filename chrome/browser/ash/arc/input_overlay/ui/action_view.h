5d2f411d828c8a948df0b1540d28bff6146a6f6b
Cici Ruan <cuicuiruan@google.com>
2023-03-03 00:27:03 +0000

    arc/gio: Reorder the ActionView
    Reorder the ActionView(s) in InputMappingView, so the Tab focus order:
    If the aspect-ratio > 1
    - Start from the left half side of the window first, the focus from top
      to bottom.
    - Then focus from top to bottom for the right side of the window.
    If the aspect-ratio <= 1
    - Focus from top to bottom.
    
    Bug: b/260868602
    Test: unittest added and manual test
    Change-Id: I70e5ac2d8538ad9c8c4f1b8fd921c8ed3a9c9b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300002
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112548}

--- chrome/browser/ash/arc/input_overlay/ui/action_view.h
+++ chrome/browser/ash/arc/input_overlay/ui/action_view.h
@@ -94,7 +94,7 @@ class ActionView : public views::View {
   bool ApplyKeyReleased(const ui::KeyEvent& event);
 
   void SetTouchPointCenter(const gfx::Point& touch_point_center);
-  gfx::Point GetTouchCenterInWindow();
+  gfx::Point GetTouchCenterInWindow() const;
 
   Action* action() { return action_; }
   const std::vector<ActionLabel*>& labels() const { return labels_; }
