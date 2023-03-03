8e52c375163b8a6bc27e910c906fff52d45fb848
Joemer Ramos <joemerramos@chromium.org>
2023-03-03 18:30:10 +0000

    Revert "[iOS] Refactor usage of UIButton.contentEdgeInsets Pt 2"
    This reverts commit f9f69cb80dd409f9e88b517ebaf8621eeef93575.
    
    Reason for revert: UIButtons are broken because of this
    
    Original change's description:
    > [iOS] Refactor usage of UIButton.contentEdgeInsets Pt 2
    >
    > Outside of this effort's background, notable deprecations in this CL
    > is adjustsImageWhenHighlighted. The current implementation implies
    > that we ignore this property with this deprecation, but feel free to
    > let me know if this isn't what we expect to happen.
    >
    > Background:
    > UIButton.contentEdgeInsets is deprecated as of iOS 15. As part of the
    > effort of bumping the min version of iOS to 15, we refactor the usage
    > of UIButton.contentEdgeInsets and replace it with
    > UIButton.configuration.contentInsets. A particular issue in creating
    > this CL was that documentation gives the idea that contentEdgeInsets,
    > titleEdgeInsets, and imageEdgeInsets should be replaced by
    > contentInsets. We are asking the CL reviewer to verify that this is
    > correct. Additionally, we use compile guards instead of @available
    > because we get deprecation warnings with @available. This may be because
    > @available is checked at runtime and these warnings happen during compiling.
    >
    > Bug: 1406561
    > Change-Id: Iae269fb87e8565ac766b58da1b63c51069b677f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4283980
    > Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1108912}
    
    Bug: 1406561
    Change-Id: Ic64ecb291fd94aeaa2439ceeb5dedba2168a8c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307236
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112880}

--- ios/chrome/browser/ui/bubble/bubble_view.mm
+++ ios/chrome/browser/ui/bubble/bubble_view.mm
@@ -161,10 +161,9 @@ UIButton* BubbleCloseButton() {
                                             kCloseButtonTopTrailingPadding -
                                             buttonImage.size.width;
   UIButton* button;
-
-  // TODO(crbug.com/1418068): Simplify after minimum version required is >=
-  // iOS 15.
-  if (@available(iOS 15, *)) {
+  // setImageEdgeInsets from UIButton is deprecated since iOS 15.0, the new
+  // API uses UIButtonConfiguration to set the image inset.
+  if (@available(iOS 15.0, *)) {
     UIButtonConfiguration* buttonConfiguration =
         UIButtonConfiguration.plainButtonConfiguration;
     [buttonConfiguration setImage:buttonImage];
@@ -175,9 +174,7 @@ UIButton* BubbleCloseButton() {
                                               kCloseButtonTopTrailingPadding)];
     button = [UIButton buttonWithConfiguration:buttonConfiguration
                                  primaryAction:nil];
-  }
-#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
-  else {
+  } else {
     button = [UIButton buttonWithType:UIButtonTypeSystem];
     [button setImage:buttonImage forState:UIControlStateNormal];
     [button.imageView setBounds:CGRectZero];
@@ -188,8 +185,6 @@ UIButton* BubbleCloseButton() {
                                    closeButtonBottomPadding,
                                    kCloseButtonTopTrailingPadding)];
   }
-#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
-
   [button setTintColor:[UIColor colorNamed:kSolidButtonTextColor]];
   [button setAccessibilityLabel:l10n_util::GetNSString(IDS_IOS_ICON_CLOSE)];
   [button setAccessibilityIdentifier:kBubbleViewCloseButtonIdentifier];
