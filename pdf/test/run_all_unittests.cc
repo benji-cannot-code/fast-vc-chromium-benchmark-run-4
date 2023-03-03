603186d23d051939fb85d2a013096c72f405762b
Brian Osman <brianosman@google.com>
2023-03-03 20:12:18 +0000

    Always use Skia's analytic AA in pdf_unittests
    Bug: chromium:1421297
    Change-Id: I35588dbb41615f1707b10ffb694d1ea379cb3cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307915
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112940}

--- pdf/test/run_all_unittests.cc
+++ pdf/test/run_all_unittests.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
 #include "third_party/blink/public/web/blink.h"
+#include "third_party/skia/include/core/SkGraphics.h"
 #include "tools/v8_context_snapshot/buildflags.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
@@ -81,6 +82,8 @@ class PdfTestSuite final : public base::TestSuite {
                       platform_->GetMainThreadScheduler());
 
     InitializeResourceBundle();
+
+    SkGraphics::SetPathAnalyticAADecider([](const SkPath&) { return true; });
   }
 
   void Shutdown() override {
