a564a57d8dd923fbba8b7394317561aeb9b1a5b1
Sean Maher <spvw@chromium.org>
2023-03-03 18:02:50 +0000

    Remove catan_investigation as a category, because it isn't used
    This is an antiquated category, and as it stands, won't be used in the
    near future, either.
    
    Change-Id: I027522c910a11a550e6eae7e3db340919626f0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304325
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112864}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("cast.mdns")                                                         \
   X("cast.mdns.socket")                                                  \
   X("cast.stream")                                                       \
-  X("catan_investigation")                                               \
   X("cc")                                                                \
   X("cc.debug")                                                          \
   X("cdp.perf")                                                          \
