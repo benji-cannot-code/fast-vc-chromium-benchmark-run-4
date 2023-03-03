4216a9ee8944fc470fbf88cff710bb97edae1981
Joey Arhar <jarhar@chromium.org>
2023-03-03 04:20:31 +0000

    Implement CSSTransitionDiscrete which enables transitions on discrete properties.
    This feature enables transitioning discrete properties.
    
    This is based on the resolved CSSWG issue:
    - Transition discrete properties: https://github.com/w3c/csswg-drafts/issues/4441
    
    You can test display transitions with this CL on:
    https://output.jsbin.com/buquher/quiet
    
    Original patch: http://crrev.com/c/4182089
    
    Bug: 1399631, 1413556
    Change-Id: Icf5364b970233186794fa9faaa016e0e6542ec2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219315
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112645}

--- third_party/blink/renderer/core/animation/transition_interpolation.cc
+++ third_party/blink/renderer/core/animation/transition_interpolation.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/animation/transition_interpolation.h"
+#include <memory>
 
 #include "third_party/blink/renderer/core/animation/css/compositor_keyframe_value.h"
 
@@ -12,8 +13,11 @@ namespace blink {
 void TransitionInterpolation::Interpolate(int iteration, double fraction) {
   if (!cached_fraction_ || *cached_fraction_ != fraction ||
       cached_iteration_ != iteration) {
-    merge_.start_interpolable_value->Interpolate(
-        *merge_.end_interpolable_value, fraction, *cached_interpolable_value_);
+    if (merge_) {
+      merge_.start_interpolable_value->Interpolate(
+          *merge_.end_interpolable_value, fraction,
+          *cached_interpolable_value_);
+    }
     cached_iteration_ = iteration;
     cached_fraction_.emplace(fraction);
   }
@@ -21,12 +25,20 @@ void TransitionInterpolation::Interpolate(int iteration, double fraction) {
 
 const InterpolableValue& TransitionInterpolation::CurrentInterpolableValue()
     const {
-  return *cached_interpolable_value_;
+  if (merge_) {
+    return *cached_interpolable_value_;
+  }
+  return cached_fraction_ < 0.5 ? *start_.interpolable_value
+                                : *end_.interpolable_value;
 }
 
 const NonInterpolableValue*
 TransitionInterpolation::CurrentNonInterpolableValue() const {
-  return merge_.non_interpolable_value.get();
+  if (merge_) {
+    return merge_.non_interpolable_value.get();
+  }
+  return cached_fraction_ < 0.5 ? start_.non_interpolable_value.get()
+                                : end_.non_interpolable_value.get();
 }
 
 void TransitionInterpolation::Apply(
