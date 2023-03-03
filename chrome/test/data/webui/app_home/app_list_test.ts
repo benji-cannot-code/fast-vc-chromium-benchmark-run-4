44df354a1a0b22a2768f6a098e744d7f15489bac
Camden King <camdenking@google.com>
2023-03-03 18:51:50 +0000

    [AppHome] Sort app list alphabetically
    Bug: 1350406, b/270454255
    Change-Id: I4d2123495752af43189f0e1603bab6410105a715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303675
    Commit-Queue: Camden King <camdenking@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112894}

--- chrome/test/data/webui/app_home/app_list_test.ts
+++ chrome/test/data/webui/app_home/app_list_test.ts
@@ -87,7 +87,7 @@ suite('AppListTest', () => {
     testAppInfo = {
       id: 'mmfbcljfglbokpmkimbfghdkjmjhdgbg',
       startUrl: {url: 'https://test.google.com/testapp3'},
-      name: 'Test App 3',
+      name: 'A Test App 3',
       iconUrl: {
         url: 'chrome://app-icon/mmfbcljfglbokpmkimbfghdkjmjhdgbg/128/1',
       },
@@ -163,10 +163,10 @@ suite('AppListTest', () => {
     flush();
     let appItemList =
         Array.from(appListElement.shadowRoot!.querySelectorAll('app-item'));
-    assertTrue(!!appItemList.find(
-        appItem =>
-            appItem.shadowRoot!.querySelector('#textContainer')!.textContent ===
-            testAppInfo.name));
+    assertTrue(
+        appItemList[0]!.shadowRoot!.querySelector(
+                                       '#textContainer')!.textContent ===
+        testAppInfo.name);
 
     // Test removing an app
     callbackRouterRemote.removeApp(testAppInfo);
