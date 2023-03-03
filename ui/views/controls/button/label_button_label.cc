257915394e2bb88d2f8a06708fb665329d4176e7
Jan Keitel <jkeitel@google.com>
2023-03-03 15:45:35 +0000

    Replace style::GetColor with style::GetColorId inside of //ui.
    GetColor does nothing other than call the passed view's ColorProvider
    to resolve the color id obtained from GetColorId. This CL moves this
    logic to the call sites to allow removing the (deprecated) GetColor
    function.
    
    Bug: 1412688
    Change-Id: I5dd3825b54f4f1e3ee10a40aece7cdf0ddafad43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224331
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112790}

--- ui/views/controls/button/label_button_label.cc
+++ ui/views/controls/button/label_button_label.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/controls/button/label_button_label.h"
 
+#include <string>
+
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/color/color_provider.h"
 
 namespace views::internal {
 
@@ -43,8 +47,9 @@ void LabelButtonLabel::SetColorForEnableState() {
   } else if (GetWidget()) {
     // If there is no widget, we can't actually get the colors here.
     // An OnThemeChanged() will fire once a widget is available.
-    int style = GetEnabled() ? style::STYLE_PRIMARY : style::STYLE_DISABLED;
-    Label::SetEnabledColor(style::GetColor(*this, GetTextContext(), style));
+    Label::SetEnabledColor(GetColorProvider()->GetColor(style::GetColorId(
+        GetTextContext(),
+        GetEnabled() ? style::STYLE_PRIMARY : style::STYLE_DISABLED)));
   }
 }
 
