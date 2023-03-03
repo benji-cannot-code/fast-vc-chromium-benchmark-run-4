d45de6fcd82a40677d540795c79837f604692148
Claudio DeSouza <cdesouza@igalia.com>
2023-03-03 01:25:18 +0000

    [json-schema-compiler] Refactor to Params::Create (60/74)
    This CL removes the use of Params::CreateDeprecated, replacing it with
    the modernised Params::Create.
    
    Changes for path:
      /extensions/browser/api/media_perception_private
    
    This CL was uploaded by git cl split.
    
    R=tbarzic@chromium.org
    
    Bug: 1354063, 1414632
    Change-Id: I3c281901ffee7d8d42ffccdfbd68b1a0c4735424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299577
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112591}

--- extensions/browser/api/media_perception_private/media_perception_private_api.cc
+++ extensions/browser/api/media_perception_private/media_perception_private_api.cc
@@ -25,7 +25,7 @@ MediaPerceptionPrivateGetStateFunction::Run() {
 
 void MediaPerceptionPrivateGetStateFunction::GetStateCallback(
     extensions::api::media_perception_private::State state) {
-  Respond(OneArgument(base::Value(state.ToValue())));
+  Respond(WithArguments(state.ToValue()));
 }
 
 MediaPerceptionPrivateSetStateFunction ::
@@ -36,10 +36,11 @@ MediaPerceptionPrivateSetStateFunction ::
 
 ExtensionFunction::ResponseAction
 MediaPerceptionPrivateSetStateFunction::Run() {
-  std::unique_ptr<extensions::api::media_perception_private::SetState::Params>
-      params = extensions::api::media_perception_private::SetState::Params::
-          CreateDeprecated(args());
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+  absl::optional<extensions::api::media_perception_private::SetState::Params>
+      params =
+          extensions::api::media_perception_private::SetState::Params::Create(
+              args());
+  EXTENSION_FUNCTION_VALIDATE(params);
   if (params->state.status !=
           extensions::api::media_perception_private::STATUS_RUNNING &&
       params->state.status !=
@@ -95,7 +96,7 @@ MediaPerceptionPrivateSetStateFunction::Run() {
 
 void MediaPerceptionPrivateSetStateFunction::SetStateCallback(
     extensions::api::media_perception_private::State state) {
-  Respond(OneArgument(base::Value(state.ToValue())));
+  Respond(WithArguments(state.ToValue()));
 }
 
 MediaPerceptionPrivateGetDiagnosticsFunction ::
@@ -116,7 +117,7 @@ MediaPerceptionPrivateGetDiagnosticsFunction::Run() {
 
 void MediaPerceptionPrivateGetDiagnosticsFunction::GetDiagnosticsCallback(
     extensions::api::media_perception_private::Diagnostics diagnostics) {
-  Respond(OneArgument(base::Value(diagnostics.ToValue())));
+  Respond(WithArguments(diagnostics.ToValue()));
 }
 
 MediaPerceptionPrivateSetAnalyticsComponentFunction::
@@ -127,11 +128,11 @@ MediaPerceptionPrivateSetAnalyticsComponentFunction::
 
 ExtensionFunction::ResponseAction
 MediaPerceptionPrivateSetAnalyticsComponentFunction::Run() {
-  std::unique_ptr<
+  absl::optional<
       extensions::api::media_perception_private::SetAnalyticsComponent::Params>
       params = extensions::api::media_perception_private::
-          SetAnalyticsComponent::Params::CreateDeprecated(args());
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+          SetAnalyticsComponent::Params::Create(args());
+  EXTENSION_FUNCTION_VALIDATE(params);
 
   MediaPerceptionAPIManager* manager =
       MediaPerceptionAPIManager::Get(browser_context());
@@ -147,7 +148,7 @@ void MediaPerceptionPrivateSetAnalyticsComponentFunction::
     OnAnalyticsComponentSet(
         extensions::api::media_perception_private::ComponentState
             component_state) {
-  Respond(OneArgument(base::Value(component_state.ToValue())));
+  Respond(WithArguments(component_state.ToValue()));
 }
 
 MediaPerceptionPrivateSetComponentProcessStateFunction::
@@ -158,11 +159,11 @@ MediaPerceptionPrivateSetComponentProcessStateFunction::
 
 ExtensionFunction::ResponseAction
 MediaPerceptionPrivateSetComponentProcessStateFunction::Run() {
-  std::unique_ptr<extensions::api::media_perception_private::
-                      SetComponentProcessState::Params>
+  absl::optional<extensions::api::media_perception_private::
+                     SetComponentProcessState::Params>
       params = extensions::api::media_perception_private::
-          SetComponentProcessState::Params::CreateDeprecated(args());
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+          SetComponentProcessState::Params::Create(args());
+  EXTENSION_FUNCTION_VALIDATE(params);
   if (params->process_state.status !=
           extensions::api::media_perception_private::PROCESS_STATUS_STARTED &&
       params->process_state.status !=
@@ -185,7 +186,7 @@ MediaPerceptionPrivateSetComponentProcessStateFunction::Run() {
 void MediaPerceptionPrivateSetComponentProcessStateFunction::
     OnComponentProcessStateSet(
         extensions::api::media_perception_private::ProcessState process_state) {
-  Respond(OneArgument(base::Value(process_state.ToValue())));
+  Respond(WithArguments(process_state.ToValue()));
 }
 
 }  // namespace extensions
