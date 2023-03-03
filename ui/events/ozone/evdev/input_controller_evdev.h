a76bbfdd502e28226031c5db726eab2eb2afea0c
David Padlipsky <dpad@google.com>
2023-03-03 00:52:36 +0000

    Settings Split: Implement acceleration in event factory
    Move acceleration suspending/resuming handling from input controller to
    the event factory to reduce complexity.
    
    Bug: b/241965700
    Change-Id: I48d70a3f6c0cb2fbe3bd976808746e47782d254e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242565
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112573}

--- ui/events/ozone/evdev/input_controller_evdev.h
+++ ui/events/ozone/evdev/input_controller_evdev.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/component_export.h"
+#include "base/containers/flat_map.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -133,13 +134,6 @@ class COMPONENT_EXPORT(EVDEV) InputControllerEvdev : public InputController {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(InputControllerEvdevTest, AccelerationSuspension);
-  FRIEND_TEST_ALL_PREFIXES(InputControllerEvdevTest,
-                           AccelerationChangeDuringSuspension);
-
-  struct StoredAccelerationSettings {
-    bool mouse = false;
-    bool pointing_stick = false;
-  };
 
   // Post task to update settings.
   void ScheduleUpdateDeviceSettings();
@@ -150,17 +144,8 @@ class COMPONENT_EXPORT(EVDEV) InputControllerEvdev : public InputController {
   // Send caps lock update to input_device_factory_.
   void UpdateCapsLockLed();
 
-  // Indicates whether the mouse acceleration is turned off for PointerLock.
-  bool is_mouse_acceleration_suspended() {
-    return stored_acceleration_settings_.get() != nullptr;
-  }
-
   // Configuration that needs to be passed on to InputDeviceFactory.
   InputDeviceSettingsEvdev input_device_settings_;
-
-  // Holds acceleration settings while suspended. Should only be considered
-  // valid while |mouse_acceleration_suspended| is true.
-  std::unique_ptr<StoredAccelerationSettings> stored_acceleration_settings_;
 
   // Task to update config from input_device_settings_ is pending.
   bool settings_update_pending_ = false;
