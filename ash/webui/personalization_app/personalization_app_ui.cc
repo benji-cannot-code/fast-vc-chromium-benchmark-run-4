e92017461a6f6fb8252559e8dc7d6e32fc23811b
Sean Kau <skau@chromium.org>
2023-03-03 20:58:53 +0000

    Migrate callers of IsJellyEnabled() in /ash/webui/personalization_app
    The Jelly flag moved to chromeos/ from ash/.  The
    convenience function was left to ease migration.
    Cleanup callers so the old function can be deleted.
    
    This CL was uploaded by git cl split.
    
    R=wutao@chromium.org
    
    Bug: b:270741618
    Change-Id: Icfbd2397aae405f53518ddcac7c8e1acc5d00a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307951
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112967}

--- ash/webui/personalization_app/personalization_app_ui.cc
+++ ash/webui/personalization_app/personalization_app_ui.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
@@ -372,7 +373,7 @@ void PersonalizationAppUI::BindInterface(
 
 void PersonalizationAppUI::BindInterface(
     mojo::PendingReceiver<color_change_listener::mojom::PageHandler> receiver) {
-  DCHECK(features::IsJellyEnabled());
+  DCHECK(chromeos::features::IsJellyEnabled());
   color_provider_handler_ = std::make_unique<ui::ColorChangeHandler>(
       web_ui()->GetWebContents(), std::move(receiver));
 }
