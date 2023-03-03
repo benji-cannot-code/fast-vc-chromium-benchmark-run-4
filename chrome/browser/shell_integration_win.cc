81f48a2c1d338e910ee80c476ba3c77d909ca1f4
David Bienvenu <davidbienvenu@chromium.org>
2023-03-03 21:10:11 +0000

    win: remove ScopedProtocolEntry
    This was only used for setting Chrome as the default browser on pre
    Win10 version of Windows.
    
    Bug: 1385856
    Change-Id: I35b81898512a57ffdd892dd12506c4b252b0ef17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4298558
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112978}

--- chrome/browser/shell_integration_win.cc
+++ chrome/browser/shell_integration_win.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/install_static/install_util.h"
 #include "chrome/installer/util/install_util.h"
-#include "chrome/installer/util/scoped_user_protocol_entry.h"
 #include "chrome/installer/util/shell_util.h"
 #include "chrome/services/util_win/public/mojom/util_win.mojom.h"
 #include "components/variations/variations_associated_data.h"
@@ -349,8 +348,7 @@ class OpenSystemSettingsHelper {
 
   OpenSystemSettingsHelper(const wchar_t* const schemes[],
                            base::OnceClosure on_finished_callback)
-      : scoped_user_protocol_entry_(schemes[0]),
-        on_finished_callback_(std::move(on_finished_callback)) {
+      : on_finished_callback_(std::move(on_finished_callback)) {
     static const wchar_t kUrlAssociationFormat[] =
         L"SOFTWARE\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\"
         L"%ls\\UserChoice";
@@ -422,10 +420,6 @@ class OpenSystemSettingsHelper {
 
   // Used to make sure only one instance is alive at the same time.
   static OpenSystemSettingsHelper* instance_;
-
-  // This is needed to make sure that Windows displays an entry for the scheme
-  // inside the "Choose default apps by protocol" settings page.
-  ScopedUserProtocolEntry scoped_user_protocol_entry_;
 
   // The function to call when the interaction with the system settings is
   // finished.
