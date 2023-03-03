ddf66b7b5f9bb99742932777d7f7bddbb913bde5
Mike Dougherty <michaeldo@chromium.org>
2023-03-03 23:05:40 +0000

    Add JavaScriptFeature::GetWebFramesManager
    With the creation of a WebFramesManager for the isolated world in
    crrev.com/c/4297679, some features can now move from the page content
    world to the isolated world. In order to support this move, this new API
    allows for features to obtain the correct WebFramesManager without
    needing to manually specify the desired content world.
    
    Bug: 1380918
    Change-Id: I15ad7b498ebd4ac701e458672eb2f3018f511ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4308472
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113042}

--- ios/web/js_messaging/java_script_feature.mm
+++ ios/web/js_messaging/java_script_feature.mm
@@ -11,11 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/time/time.h"
 #import "ios/web/js_messaging/java_script_content_world.h"
+#import "ios/web/js_messaging/java_script_content_world_util.h"
 #import "ios/web/js_messaging/java_script_feature_manager.h"
 #import "ios/web/js_messaging/page_script_util.h"
 #import "ios/web/js_messaging/web_frame_internal.h"
 #import "ios/web/public/js_messaging/content_world.h"
 #import "ios/web/public/js_messaging/web_frame.h"
+#import "ios/web/public/js_messaging/web_frames_manager.h"
+#import "ios/web/public/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -137,6 +140,15 @@ JavaScriptFeature::~JavaScriptFeature() = default;
 
 ContentWorld JavaScriptFeature::GetSupportedContentWorld() const {
   return supported_world_;
+}
+
+WebFramesManager* JavaScriptFeature::GetWebFramesManager(WebState* web_state) {
+  JavaScriptContentWorld* java_script_content_world =
+      JavaScriptFeatureManager::FromBrowserState(web_state->GetBrowserState())
+          ->GetContentWorldForFeature(this);
+  ContentWorld content_world = ContentWorldIdentifierForWKContentWorld(
+      java_script_content_world->GetWKContentWorld());
+  return web_state->GetWebFramesManager(content_world);
 }
 
 const std::vector<const JavaScriptFeature::FeatureScript>
