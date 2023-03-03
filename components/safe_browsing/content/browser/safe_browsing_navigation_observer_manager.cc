a585d4ea8f5cb9bad65ebcf8da870695a32f03f5
Daniel Rubery <drubery@chromium.org>
2023-03-03 22:54:55 +0000

    Sanitize data:// URLs in referrer chains
    For certain sites, a data:// URL is generated to download files. When
    this URL is included in the referrer chain, it can leak download
    content.
    
    Bug: 1377808
    Change-Id: I87101a1630d73e33f940e31a5ce06ff05ac540e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303145
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113039}

--- components/safe_browsing/content/browser/safe_browsing_navigation_observer_manager.cc
+++ components/safe_browsing/content/browser/safe_browsing_navigation_observer_manager.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
+#include "crypto/sha2.h"
 
 using content::WebContents;
 
@@ -77,8 +78,15 @@ ReferrerChainEntry::URLType GetURLTypeAndAdjustAttributionResult(
   }
 }
 
-std::string GetOrigin(const std::string& url) {
-  return GURL(url).DeprecatedGetOriginAsURL().spec();
+std::string ShortOriginForReporting(const std::string& url) {
+  GURL gurl(url);
+  if (gurl.SchemeIsLocal()) {
+    std::string sha_url = crypto::SHA256HashString(url);
+    return gurl.scheme() + "://" +
+           base::HexEncode(sha_url.data(), sha_url.size());
+  } else {
+    return gurl.DeprecatedGetOriginAsURL().spec();
+  }
 }
 
 }  // namespace
@@ -351,17 +359,19 @@ void SafeBrowsingNavigationObserverManager::SanitizeReferrerChain(
     ReferrerChainEntry entry_copy(*entry);
     entry->Clear();
     if (entry_copy.has_url())
-      entry->set_url(GetOrigin(entry_copy.url()));
+      entry->set_url(ShortOriginForReporting(entry_copy.url()));
     if (entry_copy.has_main_frame_url())
-      entry->set_main_frame_url(GetOrigin(entry_copy.main_frame_url()));
+      entry->set_main_frame_url(
+          ShortOriginForReporting(entry_copy.main_frame_url()));
     entry->set_type(entry_copy.type());
     for (int j = 0; j < entry_copy.ip_addresses_size(); j++)
       entry->add_ip_addresses(entry_copy.ip_addresses(j));
     if (entry_copy.has_referrer_url())
-      entry->set_referrer_url(GetOrigin(entry_copy.referrer_url()));
+      entry->set_referrer_url(
+          ShortOriginForReporting(entry_copy.referrer_url()));
     if (entry_copy.has_referrer_main_frame_url())
       entry->set_referrer_main_frame_url(
-          GetOrigin(entry_copy.referrer_main_frame_url()));
+          ShortOriginForReporting(entry_copy.referrer_main_frame_url()));
     entry->set_is_retargeting(entry_copy.is_retargeting());
     entry->set_navigation_time_msec(entry_copy.navigation_time_msec());
     entry->set_navigation_initiation(entry_copy.navigation_initiation());
@@ -370,7 +380,7 @@ void SafeBrowsingNavigationObserverManager::SanitizeReferrerChain(
           entry->add_server_redirect_chain();
       if (entry_copy.server_redirect_chain(j).has_url()) {
         server_redirect_entry->set_url(
-            GetOrigin(entry_copy.server_redirect_chain(j).url()));
+            ShortOriginForReporting(entry_copy.server_redirect_chain(j).url()));
       }
     }
   }
