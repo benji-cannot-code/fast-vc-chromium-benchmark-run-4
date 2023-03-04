f3ca3c22469a9d8998741bd1a3eb3ab924048dd2
Pengchao Cai <pengchaocai@chromium.org>
2023-03-04 01:03:23 +0000

    [side search user edu] Trigger IPH after first result navigation
    This CL changes IPH triggering condition from 2nd result navigation[1] to 1st result navigation[2]. This yields IPH competing with page action label for displaying as they now share the same triggering logic. To address this, we show IPH first if both available.
    
    Also fixes a typo.
    
    [1] Land on SRP -> Go to non-SRP result -> Go back to SRP -> Go to non-SRP result
    [2] Land on SRP -> Go to non-SRP result
    
    Bug: 1419658
    Change-Id: Ida6cb613603022e23a983d74ce01319ed7b5a0c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300934
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: pengchao Cai <pengchaocai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113097}

--- chrome/browser/ui/views/side_search/unified_side_search_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/side_search/unified_side_search_controller_interactive_uitest.cc
@@ -923,7 +923,6 @@ class SideSearchIPHAndTutorialBrowserTest
   auto CheckIPHTriggeredCorrectly(const ui::ElementIdentifier& primary_tab_id) {
     const auto srp_url = GetMatchingSearchUrl();
     const auto non_srp_url_1 = GetNonMatchingUrl();
-    const auto non_srp_url_2 = GetNonMatchingUrl();
     return Steps(
         InstrumentTab(primary_tab_id),
         // Navigate to a SRP URL and then once to a non-SRP URL.
@@ -933,12 +932,6 @@ class SideSearchIPHAndTutorialBrowserTest
         // panel isn't open.
         WaitForShow(kSideSearchButtonElementId),
         EnsureNotPresent(kSidePanelElementId),
-
-        // Detects a po-go action.
-        PressButton(kBackButtonElementId),
-        WaitForWebContentsNavigation(primary_tab_id),
-        NavigateWebContents(primary_tab_id, non_srp_url_2),
-        WaitForShow(kSideSearchButtonElementId),
 
         // IPH bubble appears.
         // Verify it's created with correct body text and anchored to side
