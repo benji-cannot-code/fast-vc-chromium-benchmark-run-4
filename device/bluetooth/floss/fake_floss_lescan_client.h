52312ef2f06948d5a9a92f011c8dc03547bf5f01
Sonny Sasaka <sonnysasaka@chromium.org>
2023-03-04 00:56:24 +0000

    [Floss] Wire OnDeviceFound/Lost based on specific scanner_id
    After https://r.android.com/2459177, Floss daemon provides scanner_id
    for OnAdvertisementFound/Lost events. This patch modifies the API to
    accept scanner_id and use it to determine whether to notify a
    LowEnergyScanSession.
    
    Bug: b:268682529
    
    Change-Id: Idda6a361da6099a6a08e00daf8a689baf05c7978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295018
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113096}

--- device/bluetooth/floss/fake_floss_lescan_client.h
+++ device/bluetooth/floss/fake_floss_lescan_client.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace floss {
 
 constexpr char kTestUuidStr[] = "00010203-0405-0607-0809-0a0b0c0d0e0f";
+constexpr char kTestUuidStr2[] = "02010203-0405-0607-0809-0a0b0c0d0e0f";
 
 class DEVICE_BLUETOOTH_EXPORT FakeFlossLEScanClient : public FlossLEScanClient {
  public:
@@ -32,11 +33,21 @@ class DEVICE_BLUETOOTH_EXPORT FakeFlossLEScanClient : public FlossLEScanClient {
                  const ScanSettings& scan_settings,
                  const absl::optional<ScanFilter>& filters) override;
 
+  void SetNextScannerUUID(const device::BluetoothUUID& uuid) {
+    next_scanner_uuid_ = uuid;
+  }
+
   // For test observation
   int scanners_registered_ = 0;
   std::unordered_set<uint8_t> scanner_ids_;
 
  private:
+  // Next UUID for registered scanner.
+  // TODO(b/271318036): Replace fake with mocks for easier control of fake
+  // values.
+  device::BluetoothUUID next_scanner_uuid_ =
+      device::BluetoothUUID(kTestUuidStr);
+
   base::WeakPtrFactory<FakeFlossLEScanClient> weak_ptr_factory_{this};
 };
 
