f68b878eff83bd714bce0e3d5ad05442cda58a98
Mohamed Amir Yosef <mamir@chromium.org>
2023-03-06 14:50:23 +0000

    [Passwords] Use the title in the controller instead of a fixed string
    This required exposing the GetTitle() method to be public in the
    controller such that the view can consume it.
    
    Bug: 1382017
    Change-Id: Ic9895fd410736d9f2fc9d86d9af02478a7b0e1aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4308627
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113364}

--- chrome/browser/ui/views/passwords/manage_passwords_list_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_list_view.cc
@@ -21,7 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/separator.h"
 
 // static
-std::unique_ptr<views::View> ManagePasswordsListView::CreateTitleView() {
+std::unique_ptr<views::View> ManagePasswordsListView::CreateTitleView(
+    const std::u16string& title) {
   const ChromeLayoutProvider* layout_provider = ChromeLayoutProvider::Get();
   auto header = std::make_unique<views::BoxLayoutView>();
   // Set the space between the icon and title similar to the default behavior in
@@ -33,10 +34,7 @@ std::unique_ptr<views::View> ManagePasswordsListView::CreateTitleView() {
           GooglePasswordManagerVectorIcon(), ui::kColorIcon,
           layout_provider->GetDistanceMetric(
               DISTANCE_BUBBLE_HEADER_VECTOR_ICON_SIZE))));
-  // TODO(crbug.com/1382017): refactor to use the title provided by the
-  // controller instead.
-  header->AddChildView(views::BubbleFrameView::CreateDefaultTitleLabel(
-      u"Saved passwords for this site"));
+  header->AddChildView(views::BubbleFrameView::CreateDefaultTitleLabel(title));
   return header;
 }
 
--- chrome/browser/ui/views/passwords/manage_passwords_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_view.cc
@@ -83,7 +83,7 @@ void ManagePasswordsView::AddedToWidget() {
   // BubbleDialogDelegateView::CreateBubble() *after* the construction of the
   // ManagePasswordsView, the title view cannot be set in the constructor.
   GetBubbleFrameView()->SetTitleView(
-      ManagePasswordsListView::CreateTitleView());
+      ManagePasswordsListView::CreateTitleView(controller_.GetTitle()));
 }
 
 bool ManagePasswordsView::Accept() {
@@ -215,7 +215,8 @@ void ManagePasswordsView::RecreateLayout() {
                                      ->GetInsetsMetric(views::INSETS_DIALOG)
                                      .bottom()));
   } else {
-    frame_view->SetTitleView(ManagePasswordsListView::CreateTitleView());
+    frame_view->SetTitleView(
+        ManagePasswordsListView::CreateTitleView(controller_.GetTitle()));
     frame_view->SetFootnoteView(CreateFooterView());
     page_container_->SwitchToPage(CreatePasswordListView());
     page_container_->SetProperty(views::kMarginsKey, gfx::Insets());
