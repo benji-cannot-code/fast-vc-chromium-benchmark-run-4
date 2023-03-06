667650fe4963ebc348275d31e3d5cb6c6ade7247
Olivier Robin <olivierrobin@google.com>
2023-03-06 16:47:51 +0000

    Partial translate support iframe selection sourceRect
    When selecting text in an iFrame, the frame itself does not know
    the position of the selection on screen.
    
    To compute the correct position, pass the offset of the frame
    down the frame tree to compute the global position.
    
    Some more refactoring are needed to make this work:
    - As the calls to JS tree are now traversing the frame tree, the
      frame that find a selection now report it by sending a message
      to native code
    - to make sure the correct selection is matched to the correct
      WebState, completion callbacks are now kept in
      WebSelectionTabHelper.
    - there is no easy way to know when the full frame tree has been
      explored. So in case there is no selection, the timeout will
      trigger and call the completion. This should not be a problem
      in case the selection is retrieved as a result of an edit menu
      action as there is always a selection in that case.
    
    Bug: 1416451
    Change-Id: I31c42f9039b17205b51d1fc77122f3ca61a3f5f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300272
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113406}

---
+++ ios/chrome/browser/web_selection/web_selection_java_script_feature_observer.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_WEB_SELECTION_WEB_SELECTION_JAVA_SCRIPT_FEATURE_OBSERVER_H_
+#define IOS_CHROME_BROWSER_WEB_SELECTION_WEB_SELECTION_JAVA_SCRIPT_FEATURE_OBSERVER_H_
+
+#include "base/observer_list.h"
+
+@class WebSelectionResponse;
+
+namespace web {
+class WebState;
+}
+
+class WebSelectionJavaScriptFeatureObserver : public base::CheckedObserver {
+ public:
+  // Called whenever a JavaScript message with a selection is received.
+  virtual void OnSelectionRetrieved(web::WebState* web_state,
+                                    WebSelectionResponse* response) {}
+};
+
+#endif  // IOS_CHROME_BROWSER_WEB_SELECTION_WEB_SELECTION_JAVA_SCRIPT_FEATURE_OBSERVER_H_
--- ios/chrome/browser/web_selection/web_selection_tab_helper.h
+++ ios/chrome/browser/web_selection/web_selection_tab_helper.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_WEB_SELECTION_WEB_SELECTION_TAB_HELPER_H_
 #define IOS_CHROME_BROWSER_WEB_SELECTION_WEB_SELECTION_TAB_HELPER_H_
 
+#import "base/timer/timer.h"
+#import "ios/chrome/browser/web_selection/web_selection_java_script_feature_observer.h"
 #import "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
 
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // page.
 class WebSelectionTabHelper
     : public web::WebStateObserver,
+      public WebSelectionJavaScriptFeatureObserver,
       public web::WebStateUserData<WebSelectionTabHelper> {
  public:
   ~WebSelectionTabHelper() override;
@@ -27,11 +30,17 @@ class WebSelectionTabHelper
   // successful, will invoke `callback` with the selected text (which can be
   // empty). If the selection could not be retrieved, the `response.valid` will
   // be NO.
+  // Note: If there is no selection in the page, the callback will be called
+  // after a timeout (currently 1s).
   void GetSelectedText(
       base::OnceCallback<void(WebSelectionResponse*)> callback);
 
   // Return whether the JS to retrieve the selected text can be called.
   bool CanRetrieveSelectedText();
+
+  // WebSelectionJavaScriptFeatureObserver methods.
+  void OnSelectionRetrieved(web::WebState* web_state,
+                            WebSelectionResponse* response) override;
 
  private:
   friend class web::WebStateUserData<WebSelectionTabHelper>;
@@ -41,11 +50,22 @@ class WebSelectionTabHelper
   // WebStateObserver:
   void WebStateDestroyed(web::WebState* web_state) override;
 
+  // Called when the selection retrieval times out.
+  void Timeout();
+
   // The WebState this instance is observing. Will be null after
   // WebStateDestroyed has been called.
   web::WebState* web_state_ = nullptr;
 
+  // The callback to call when the selection is finally retrieved.
+  base::OnceCallback<void(WebSelectionResponse*)> final_callback_;
+
+  // A timer to limit the time taken to retrieve the selection.
+  base::OneShotTimer time_out_callback_;
+
   WEB_STATE_USER_DATA_KEY_DECL();
+
+  base::WeakPtrFactory<WebSelectionTabHelper> weak_ptr_factory_;
 };
 
 #endif  // IOS_CHROME_BROWSER_WEB_SELECTION_WEB_SELECTION_TAB_HELPER_H_
