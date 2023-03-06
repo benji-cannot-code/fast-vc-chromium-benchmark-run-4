ec8dfb06714c9af9255643182625a1ac917f3772
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-03-06 17:06:59 +0000

    Import wpt@51750bc8d749bd80930506f603940a6bafda459f
    Using wpt-import in Chromium c26fb060e617d26a72168cd83fa2de7eda4aec3b.
    With Chromium commits locally applied on WPT:
    32672ad2c1 "Update WPT OffscreenCanvas test template"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I17bf1e97fad4fc32ca59b1fe0717663ac9cdeabb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4311244
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1113413}

--- third_party/blink/web_tests/external/wpt/streams/readable-byte-streams/enqueue-with-detached-buffer.any.js
+++ third_party/blink/web_tests/external/wpt/streams/readable-byte-streams/enqueue-with-detached-buffer.any.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+
 promise_test(async t => {
   const error = new Error('cannot proceed');
   const rs = new ReadableStream({
@@ -6,7 +8,7 @@ promise_test(async t => {
     pull: t.step_func((controller) => {
       const buffer = controller.byobRequest.view.buffer;
       // Detach the buffer.
-      postMessage(buffer, '*', [buffer]);
+      structuredClone(buffer, { transfer: [buffer] });
 
       // Try to enqueue with a new buffer.
       assert_throws_js(TypeError, () => controller.enqueue(new Uint8Array([42])));
