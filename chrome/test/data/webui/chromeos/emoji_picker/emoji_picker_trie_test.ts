102bfc47e89a3bccad711236bd2b2c8fa962fd3e
John Palmer <jopalmer@chromium.org>
2023-03-06 03:55:09 +0000

    [Emoji picker tests TS migration 3/N] Migrate a test file
    Had to also migrate the util file to make this work
    
    Bug: b:270220102
    Change-Id: I508f9a80a5cdbecaab86ca726527ac8d25bce8bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279162
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113229}

--- chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_trie_test.ts
+++ chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_trie_test.ts
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {Trie} from 'chrome://emoji-picker/structs/trie.js';
-import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 suite('TrieUnitTest', () => {
-  let trie;
+  let trie: Trie;
   setup(() => {
     trie = new Trie();
   });
