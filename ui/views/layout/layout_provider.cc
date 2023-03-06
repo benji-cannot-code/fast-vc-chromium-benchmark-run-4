b39d282604540b7cb556a082d4a9479c6dfd6251
Allen Bauer <kylixrd@chromium.org>
2023-03-06 17:38:34 +0000

    [ChromeRefresh2023] Set new default corner radius for Dialog/Bubbles.
    This sets the default radius to 12dp according to the Figma Mockups.
    
    Bug: 1399913
    Change-Id: I4e2c0b8295dc5ca717c5d41ed9b4905f4a29a86e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4308291
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113435}

--- ui/views/layout/layout_provider.cc
+++ ui/views/layout/layout_provider.cc
@@ -182,8 +182,9 @@ ShapeSysTokens GetShapeSysToken(ShapeContextTokens id) {
       base::MakeFixedFlatMap<ShapeContextTokens, ShapeSysTokens>({
           {ShapeContextTokens::kBadgeRadius, ShapeSysTokens::kXSmall},
           {ShapeContextTokens::kButtonRadius, ShapeSysTokens::kFull},
-          {ShapeContextTokens::kTextfieldRadius, ShapeSysTokens::kSmall},
           {ShapeContextTokens::kComboboxRadius, ShapeSysTokens::kSmall},
+          {ShapeContextTokens::kDialogRadius, ShapeSysTokens::kMediumSmall},
+          {ShapeContextTokens::kTextfieldRadius, ShapeSysTokens::kSmall},
       });
   const auto* it = shape_token_map.find(id);
   return it == shape_token_map.end() ? ShapeSysTokens::kDefault : it->second;
@@ -196,10 +197,12 @@ int LayoutProvider::GetCornerRadiusMetric(ShapeContextTokens id,
       case ShapeContextTokens::kBadgeRadius:
         return 3;
       case ShapeContextTokens::kButtonRadius:
-        return 4;
+        return GetCornerRadiusMetric(Emphasis::kMedium, size);
       case ShapeContextTokens::kComboboxRadius:
       case ShapeContextTokens::kTextfieldRadius:
         return FocusRing::kDefaultCornerRadiusDp;
+      case ShapeContextTokens::kDialogRadius:
+        return GetCornerRadiusMetric(Emphasis::kMedium, size);
       default:
         return 0;
     }
@@ -213,6 +216,8 @@ int LayoutProvider::GetCornerRadiusMetric(ShapeContextTokens id,
       return 4;
     case ShapeSysTokens::kSmall:
       return 8;
+    case ShapeSysTokens::kMediumSmall:
+      return 12;
     case ShapeSysTokens::kMedium:
       return 16;
     case ShapeSysTokens::kLarge:
--- ui/views/layout/layout_provider.h
+++ ui/views/layout/layout_provider.h
@@ -139,8 +139,9 @@ enum class Emphasis {
 enum class ShapeContextTokens {
   kBadgeRadius,
   kButtonRadius,
-  kTextfieldRadius,
   kComboboxRadius,
+  kDialogRadius,
+  kTextfieldRadius,
 };
 
 // ShapeSysTokens are tokens that map to a fixed value that aligns with UX/UI.
@@ -153,6 +154,7 @@ enum class ShapeSysTokens {
   kDefault,
   kXSmall,
   kSmall,
+  kMediumSmall,
   kMedium,
   kLarge,
   kFull,
