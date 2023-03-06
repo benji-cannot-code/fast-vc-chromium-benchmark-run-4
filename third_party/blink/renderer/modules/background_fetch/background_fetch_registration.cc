ac8d3ddec0d55b72dd2c9ca71733735fabacd093
Ari Chivukula <arichiv@chromium.org>
2023-03-06 13:26:13 +0000

    Remove expired histograms BackgroundFetch.MatchCalledFromDocumentScope, BackgroundFetch.MatchCalledWhenFetchIsIncomplete
    These histograms haven't been recorded since 2021 so removal seems to be
    the correct path here. This is a code-health task.
    
    Bug: 1384804
    Change-Id: I37efc9725073810aa27aec303e52f9f236749409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303316
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113347}

--- third_party/blink/renderer/modules/background_fetch/background_fetch_registration.cc
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_registration.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/metrics/histogram_macros.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
 #include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
@@ -183,10 +182,6 @@ ScriptPromise BackgroundFetchRegistration::MatchImpl(
     ExceptionState& exception_state,
     bool match_all) {
   DCHECK(script_state);
-  UMA_HISTOGRAM_BOOLEAN("BackgroundFetch.MatchCalledFromDocumentScope",
-                        LocalDOMWindow::From(script_state));
-  UMA_HISTOGRAM_BOOLEAN("BackgroundFetch.MatchCalledWhenFetchIsIncomplete",
-                        result_ == mojom::BackgroundFetchResult::UNSET);
 
   if (!records_available_) {
     exception_state.ThrowDOMException(
