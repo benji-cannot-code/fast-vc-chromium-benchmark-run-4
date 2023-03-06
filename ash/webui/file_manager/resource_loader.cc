699f3d2c61b3b9cb88a7b131043a3c021018da38
Sean Kau <skau@chromium.org>
2023-03-06 18:46:37 +0000

    Migrate callers of IsJellyEnabled() in /ash/webui/file_manager
    The Jelly flag moved to chromeos/ from ash/.  The
    convenience function was left to ease migration.
    Cleanup callers so the old function can be deleted.
    
    This CL was uploaded by git cl split.
    
    R=benreich@chromium.org
    
    Bug: b:270741618
    Change-Id: I5866caf9c54452b5c1dafce9970dd7d9cfeafaed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4308292
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113485}

--- ash/webui/file_manager/resource_loader.cc
+++ ash/webui/file_manager/resource_loader.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/file_manager/resource_loader.h"
 
-#include "ash/constants/ash_features.h"
 #include "base/files/file_path.h"
 #include "base/strings/string_util.h"
+#include "chromeos/constants/chromeos_features.h"
 
 namespace ash {
 namespace file_manager {
@@ -17,7 +17,7 @@ void AddFilesAppResources(content::WebUIDataSource* source,
                           size_t size) {
   // For Jelly we need to remap some resource dependencies.
   std::map<std::string, int> resource_map;
-  if (ash::features::IsJellyEnabled()) {
+  if (chromeos::features::IsJellyEnabled()) {
     for (size_t i = 0; i < size; ++i) {
       const base::FilePath file_path(entries[i].path);
       if (file_path.Extension() == ".css") {
@@ -33,7 +33,7 @@ void AddFilesAppResources(content::WebUIDataSource* source,
       // Files app UI has all paths relative to //ui/file_manager/file_manager/
       // so we remove the leading file_manager/ to match the existing paths.
       base::ReplaceFirstSubstringAfterOffset(&path, 0, "file_manager/", "");
-      if (ash::features::IsJellyEnabled()) {
+      if (chromeos::features::IsJellyEnabled()) {
         // Serve CSS files that have the suffix _gm3.css as if they were named
         // without the suffix, serve the content of foo_gm3.css for requests
         // for foo.css.
