11f8a7d43aad17e9aa7c4a9efd77d823033f46d4
Peter Kasting <pkasting@chromium.org>
2023-03-06 18:35:56 +0000

    Use base::ranges::transform(): base/
    Bug: 1368812
    Change-Id: I3e2a3aa3f0cc2d4a3f7626a8a1e6d4acf17d6651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310049
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113477}

--- base/win/sid.cc
+++ base/win/sid.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <stdlib.h>
 
-#include <algorithm>
 #include <iterator>
 #include <map>
 #include <utility>
@@ -19,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/no_destructor.h"
 #include "base/rand_util.h"
+#include "base/ranges/algorithm.h"
 #include "base/strings/string_util_win.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/scoped_localalloc.h"
@@ -252,24 +252,23 @@ absl::optional<std::vector<Sid>> Sid::FromSddlStringVector(
 std::vector<Sid> Sid::FromNamedCapabilityVector(
     const std::vector<std::wstring>& capability_names) {
   std::vector<Sid> sids;
-  std::transform(capability_names.cbegin(), capability_names.cend(),
-                 std::back_inserter(sids), FromNamedCapability);
+  ranges::transform(capability_names, std::back_inserter(sids),
+                    FromNamedCapability);
   return sids;
 }
 
 std::vector<Sid> Sid::FromKnownCapabilityVector(
     const std::vector<WellKnownCapability>& capabilities) {
   std::vector<Sid> sids;
-  std::transform(capabilities.cbegin(), capabilities.cend(),
-                 std::back_inserter(sids), FromKnownCapability);
+  ranges::transform(capabilities, std::back_inserter(sids),
+                    FromKnownCapability);
   return sids;
 }
 
 std::vector<Sid> Sid::FromKnownSidVector(
     const std::vector<WellKnownSid>& known_sids) {
   std::vector<Sid> sids;
-  std::transform(known_sids.cbegin(), known_sids.cend(),
-                 std::back_inserter(sids), FromKnownSid);
+  ranges::transform(known_sids, std::back_inserter(sids), FromKnownSid);
   return sids;
 }
 
