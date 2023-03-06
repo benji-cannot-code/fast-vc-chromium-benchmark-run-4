e44d2d24fe864cb758a657e2f8f876366ca606b4
Joemer Ramos <joemerramos@chromium.org>
2023-03-06 17:02:38 +0000

    Reland "[iOS] Refactor usage of UIButton.contentEdgeInsets Pt 1"
    This is a reland of commit 898d944601288790044bf8bbba2a87fdd0177fc3
    We properly set the UIButtonConfiguration in this reland.
    
    Original change's description:
    > [iOS] Refactor usage of UIButton.contentEdgeInsets Pt 1
    >
    > UIButton.contentEdgeInsets is deprecated as of iOS 15. As part of the
    > effort of bumping the min version of iOS to 15, we refactor the usage
    > of UIButton.contentEdgeInsets and replace it with
    > UIButton.configuration.contentInsets. A particular issue in creating
    > this CL was that documentation gives the idea that contentEdgeInsets,
    > titleEdgeInsets, and imageEdgeInsets should be replaced by
    > contentInsets. We are asking the CL reviewer to verify that this is
    > correct. Additionally, we use compile guards instead of @available
    > because we get deprecation warnings with @available. This may be because
    > @available is checked at runtime and these warnings happen during compiling.
    >
    > Bug: 1406561
    > Change-Id: I1669c371a3026b6b3ed6be133f35032f7d24e82c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4276801
    > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1108302}
    
    Bug: 1406561
    Change-Id: If4af661bc5699fbb90a8a562ff5d5cf38d4e3c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4306882
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113411}

--- ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
+++ ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
@@ -176,7 +176,20 @@ constexpr CGFloat kSignInPromoHeadlineFontSize = 17.0;
     _textLabel.lineBreakMode = NSLineBreakByWordWrapping;
 
     // Create and setup primary button.
-    _primaryButton = [[UIButton alloc] init];
+    // TODO(crbug.com/1418068): Simplify after minimum version required is >=
+    // iOS 15.
+    if (@available(iOS 15, *)) {
+      UIButtonConfiguration* buttonConfiguration =
+          UIButtonConfiguration.plainButtonConfiguration;
+      _primaryButton = [UIButton buttonWithConfiguration:buttonConfiguration
+                                           primaryAction:nil];
+    }
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
+    else {
+      _primaryButton = [[UIButton alloc] init];
+    }
+#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
+
     [_primaryButton.titleLabel
         setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleHeadline]];
     _primaryButton.titleLabel.adjustsFontSizeToFitWidth = YES;
@@ -503,11 +516,26 @@ constexpr CGFloat kSignInPromoHeadlineFontSize = 17.0;
       self.primaryButton.layer.cornerRadius =
           kStandardPromoStyle.kButtonCornerRadius;
       self.primaryButton.clipsToBounds = YES;
-      self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
-          kStandardPromoStyle.kButtonTitleVerticalContentInset,
-          kStandardPromoStyle.kButtonTitleHorizontalContentInset,
-          kStandardPromoStyle.kButtonTitleVerticalContentInset,
-          kStandardPromoStyle.kButtonTitleHorizontalContentInset);
+
+      // TODO(crbug.com/1418068): Simplify after minimum version required is >=
+      // iOS 15.
+      if (@available(iOS 15, *)) {
+        self.primaryButton.configuration.contentInsets =
+            NSDirectionalEdgeInsetsMake(
+                kStandardPromoStyle.kButtonTitleVerticalContentInset,
+                kStandardPromoStyle.kButtonTitleHorizontalContentInset,
+                kStandardPromoStyle.kButtonTitleVerticalContentInset,
+                kStandardPromoStyle.kButtonTitleHorizontalContentInset);
+      }
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
+      else {
+        self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
+            kStandardPromoStyle.kButtonTitleVerticalContentInset,
+            kStandardPromoStyle.kButtonTitleHorizontalContentInset,
+            kStandardPromoStyle.kButtonTitleVerticalContentInset,
+            kStandardPromoStyle.kButtonTitleHorizontalContentInset);
+      }
+#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
 
       constraintsToActivate = self.standardLayoutConstraints;
       break;
@@ -550,11 +578,26 @@ constexpr CGFloat kSignInPromoHeadlineFontSize = 17.0;
       self.primaryButton.layer.cornerRadius =
           kTitledPromoStyle.kButtonCornerRadius;
       self.primaryButton.clipsToBounds = YES;
-      self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
-          kTitledPromoStyle.kButtonTitleVerticalContentInset,
-          kTitledPromoStyle.kButtonTitleHorizontalContentInset,
-          kTitledPromoStyle.kButtonTitleVerticalContentInset,
-          kTitledPromoStyle.kButtonTitleHorizontalContentInset);
+
+      // TODO(crbug.com/1418068): Simplify after minimum version required is >=
+      // iOS 15.
+      if (@available(iOS 15, *)) {
+        self.primaryButton.configuration.contentInsets =
+            NSDirectionalEdgeInsetsMake(
+                kTitledPromoStyle.kButtonTitleVerticalContentInset,
+                kTitledPromoStyle.kButtonTitleHorizontalContentInset,
+                kTitledPromoStyle.kButtonTitleVerticalContentInset,
+                kTitledPromoStyle.kButtonTitleHorizontalContentInset);
+      }
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
+      else {
+        self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
+            kTitledPromoStyle.kButtonTitleVerticalContentInset,
+            kTitledPromoStyle.kButtonTitleHorizontalContentInset,
+            kTitledPromoStyle.kButtonTitleVerticalContentInset,
+            kTitledPromoStyle.kButtonTitleHorizontalContentInset);
+      }
+#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
 
       constraintsToActivate = self.titledLayoutConstraints;
       break;
@@ -591,11 +634,26 @@ constexpr CGFloat kSignInPromoHeadlineFontSize = 17.0;
       self.primaryButton.layer.cornerRadius =
           kTitledCompactPromoStyle.kButtonCornerRadius;
       self.primaryButton.clipsToBounds = NO;
-      self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
-          kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
-          kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset,
-          kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
-          kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset);
+
+      // TODO(crbug.com/1418068): Simplify after minimum version required is >=
+      // iOS 15.
+      if (@available(iOS 15, *)) {
+        self.primaryButton.configuration.contentInsets =
+            NSDirectionalEdgeInsetsMake(
+                kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
+                kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset,
+                kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
+                kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset);
+      }
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
+      else {
+        self.primaryButton.contentEdgeInsets = UIEdgeInsetsMake(
+            kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
+            kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset,
+            kTitledCompactPromoStyle.kButtonTitleVerticalContentInset,
+            kTitledCompactPromoStyle.kButtonTitleHorizontalContentInset);
+      }
+#endif  // __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
 
       constraintsToActivate = self.titledCompactLayoutConstraints;
       break;
