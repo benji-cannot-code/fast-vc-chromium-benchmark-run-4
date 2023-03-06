ded6287e228ec35ce6b6c7001667553e98cef8ea
Colin Blundell <blundell@chromium.org>
2023-03-06 09:43:02 +0000

    [GL] Inline GLImageStub in two remaining usecases
    This CL eliminates gl_image_stub.*, instead inlining the (trivial)
    subclass in the two remaining usecases. This reduces the probability of
    further usage of this stub as we continue towards elimination of
    GLImage altogether.
    
    Bug: 1310018
    Change-Id: I64950923fe27578f36f29aa807bff930e61c4840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290331
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113295}

--- gpu/command_buffer/service/raster_decoder_unittest.cc
+++ gpu/command_buffer/service/raster_decoder_unittest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/test_helper.h"
 #include "gpu/config/gpu_preferences.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/gl/gl_image_stub.h"
 #include "ui/gl/gl_mock.h"
 #include "ui/gl/gl_surface_stub.h"
 #include "ui/gl/init/gl_factory.h"
