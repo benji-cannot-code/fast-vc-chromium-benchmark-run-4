2a9d0fe45d70e08da98c1488090a01532a74cf9d
Duong Dac <ddac@chromium.org>
2023-03-06 12:08:14 +0000

    Replace Profile::IsChild with IsURLFilteringEnabled if applicable
    Follow up of crrev/c/4218333
    
    Bug: b/270563369
    Change-Id: I4ce466cb91a34141cf249e919553f56bd8ac54d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297916
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113330}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -3259,9 +3259,10 @@ bool RenderViewContextMenu::IsSaveLinkAsEnabled() const {
 
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
   Profile* const profile = Profile::FromBrowserContext(browser_context_);
-  if (profile->IsChild()) {
-    SupervisedUserService* supervised_user_service =
-        SupervisedUserServiceFactory::GetForProfile(profile);
+  SupervisedUserService* supervised_user_service =
+      SupervisedUserServiceFactory::GetForProfile(profile);
+  if (supervised_user_service &&
+      supervised_user_service->IsURLFilteringEnabled()) {
     SupervisedUserURLFilter* url_filter =
         supervised_user_service->GetURLFilter();
     if (url_filter->GetFilteringBehaviorForURL(params_.link_url) !=
