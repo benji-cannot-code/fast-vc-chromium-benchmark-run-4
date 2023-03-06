090b485b2f8d14e02c7ef668c09bb794f73eb7d8
Lijin Shen <lazzzis@google.com>
2023-03-06 08:42:50 +0000

    [Messages] Add a11y support for stacking animation
    1. replace "dismiss" with "dismiss and show next available action"
    when messages are stacked
    2. Announce when a background message is displayed.
    
    Screenshot:
    go/strpic/c8dd087b4a05eb7d31de8c1611887fce43014021
    go/strpic/df8ed0d5d09098d8a861196d4b2870adfd485daf
    
    Bug: 1396141, 1200974
    Change-Id: Ie27ef718131cfbbfca8f5640a342f2b58e46376f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307956
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113282}

--- components/messages/android/java/src/org/chromium/components/messages/MessageContainer.java
+++ components/messages/android/java/src/org/chromium/components/messages/MessageContainer.java
@@ -15,7 +15,6 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.ViewCompat;
-import androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat;
 
 import org.chromium.base.Log;
 import org.chromium.base.TraceEvent;
@@ -66,18 +65,11 @@ public class MessageContainer extends FrameLayout {
 
     private MessageContainerA11yDelegate mA11yDelegate;
     private boolean mIsInitializingLayout;
+    private int mA11yDismissActionId = NO_ID;
 
     public MessageContainer(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setAccessibilityDelegate(new MessageContainerA11yDelegateProxy());
-        ViewCompat.replaceAccessibilityAction(
-                this, AccessibilityActionCompat.ACTION_DISMISS, null, (v, c) -> {
-                    if (mA11yDelegate != null) {
-                        mA11yDelegate.onA11yDismiss();
-                        return true;
-                    }
-                    return false;
-                });
     }
 
     /**
@@ -103,6 +95,7 @@ public class MessageContainer extends FrameLayout {
                     "Should not contain any view when adding a new message.");
         }
         super.addView(view, index);
+        onChildCountChanged();
 
         // TODO(crbug.com/1178965): clipChildren should be set to false only when the message is in
         // motion.
@@ -120,6 +113,22 @@ public class MessageContainer extends FrameLayout {
         if (getChildCount() == 0) {
             mA11yDelegate = null;
         }
+        onChildCountChanged();
+    }
+
+    private void onChildCountChanged() {
+        ViewCompat.removeAccessibilityAction(this, mA11yDismissActionId);
+        if (getChildCount() == 0) return;
+        String label = getResources().getString(
+                getChildCount() == 1 ? R.string.dismiss : R.string.message_dismiss_and_show_next);
+        mA11yDismissActionId = ViewCompat.addAccessibilityAction(this, label, (v, c) -> {
+            if (mA11yDelegate != null) {
+                assert getChildCount() != 0;
+                mA11yDelegate.onA11yDismiss();
+                return true;
+            }
+            return false;
+        });
     }
 
     public int getMessageBannerHeight() {
@@ -206,5 +215,9 @@ public class MessageContainer extends FrameLayout {
     @Deprecated
     public final void removeView(View view) {
         throw new RuntimeException("Use removeMessage instead.");
+    }
+
+    public int getA11yDismissActionIdForTesting() {
+        return mA11yDismissActionId;
     }
 }
