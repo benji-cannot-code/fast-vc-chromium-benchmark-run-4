f67b8cf93b03876f9c7a1e4e9818853a999aaed6
Ilkin Safarli <safarli@google.com>
2023-03-06 22:55:26 +0000

    CrOS Settings: Convert parental_controls_page browser test to TS
    Bug: b:270728282
    Test: browser_tests --gtest_filter="OSSettingsParentalControlsPage*"
    Change-Id: I441e4236b2a995acc8ec1496f680644e470b7ad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4305324
    Commit-Queue: Ilkin Safarli <safarli@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113633}

--- chrome/test/data/webui/settings/chromeos/parental_controls_page/test_parental_controls_browser_proxy.ts
+++ chrome/test/data/webui/settings/chromeos/parental_controls_page/test_parental_controls_browser_proxy.ts
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {ParentalControlsBrowserProxy} from 'chrome://os-settings/chromeos/os_settings.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
-export class TestParentalControlsBrowserProxy extends TestBrowserProxy {
+export class TestParentalControlsBrowserProxy extends TestBrowserProxy
+    implements ParentalControlsBrowserProxy {
   constructor() {
     super([
       'showAddSupervisionDialog',
