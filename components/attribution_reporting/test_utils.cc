cf91951d624ac164a8297bdee3b614d7f3f4c2b6
Jose Dapena Paz <jdapena@igalia.com>
2023-03-06 17:56:33 +0000

    GCC: fix filters initialization
    Fix build error initializing attribution_reporting::FiltersForSourceType:
        ../../components/attribution_reporting/test_utils.cc:35:4: error: call of overloaded ‘Filters(<brace-enclosed initializer list>)’ is ambiguous
           35 |   });
              |    ^
    
    Bug: 819294
    Change-Id: I8cdb997b24d7d94c47c1b1ec9841ac500902487c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4311723
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1113448}

--- components/attribution_reporting/test_utils.cc
+++ components/attribution_reporting/test_utils.cc
@@ -29,11 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace attribution_reporting {
 
 Filters FiltersForSourceType(mojom::SourceType source_type) {
-  return Filters({
+  return Filters{{
       {
           {FilterData::kSourceTypeFilterKey, {SourceTypeName(source_type)}},
       },
-  });
+  }};
 }
 
 bool operator==(const AggregationKeys& a, const AggregationKeys& b) {
