11901d4de16bf66a80b3e2c13d00a30e22e99709
Mike Dougherty <michaeldo@chromium.org>
2023-03-06 20:19:08 +0000

    Move WebFramesManagerImpl map to WebStateImpl
    The `managers_` map needs to live on WebStateImpl instead of the
    RealizedWebState class in order to prevent over realization of webstates
    at launch.
    
    Bug: 1380918
    Change-Id: Ib8e5dec9350791db572c2ced55dd4692052ff61f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4311307
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113539}

--- ios/web/web_state/web_state_impl_realized_web_state.mm
+++ ios/web/web_state/web_state_impl_realized_web_state.mm
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/navigation/wk_navigation_util.h"
 #import "ios/web/public/browser_state.h"
 #import "ios/web/public/favicon/favicon_url.h"
-#import "ios/web/public/js_messaging/content_world.h"
 #import "ios/web/public/js_messaging/web_frame.h"
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/navigation/web_state_policy_decider.h"
@@ -76,7 +75,7 @@ void WebStateImpl::RealizedWebState::Init(const CreateParams& params,
   navigation_manager_->SetBrowserState(params.browser_state);
   web_controller_ = [[CRWWebController alloc] initWithWebState:owner_];
 
-  GetPageWorldWebFramesManager().AddObserver(this);
+  owner_->GetPageWorldWebFramesManager()->AddObserver(this);
 
   // Restore session history last because NavigationManagerImpl relies on
   // CRWWebController to restore history into the web view.
@@ -131,7 +130,7 @@ void WebStateImpl::RealizedWebState::Init(const CreateParams& params,
 
 void WebStateImpl::RealizedWebState::TearDown() {
   [web_controller_ close];
-  GetPageWorldWebFramesManager().RemoveObserver(this);
+  owner_->GetPageWorldWebFramesManager()->RemoveObserver(this);
 
   // WebUI depends on web state so it must be destroyed first in case any WebUI
   // implementations depends on accessing web state during destruction.
@@ -153,21 +152,6 @@ WebStateImpl::RealizedWebState::GetNavigationManager() const {
 
 NavigationManagerImpl& WebStateImpl::RealizedWebState::GetNavigationManager() {
   return *navigation_manager_;
-}
-
-WebFramesManagerImpl&
-WebStateImpl::RealizedWebState::GetPageWorldWebFramesManager() {
-  return GetWebFramesManagerImpl(ContentWorld::kPageContentWorld);
-}
-
-WebFramesManagerImpl& WebStateImpl::RealizedWebState::GetWebFramesManagerImpl(
-    ContentWorld world) {
-  DCHECK_NE(world, ContentWorld::kAllContentWorlds);
-
-  if (!managers_[world]) {
-    managers_[world] = base::WrapUnique(new WebFramesManagerImpl());
-  }
-  return *managers_[world].get();
 }
 
 const SessionCertificatePolicyCacheImpl&
@@ -506,12 +490,6 @@ void WebStateImpl::RealizedWebState::RetrieveExistingFrames() {
   for (JavaScriptContentWorld* world : feature_manager->GetAllContentWorlds()) {
     [web_controller_
         retrieveExistingFramesInContentWorld:world->GetWKContentWorld()];
-  }
-}
-
-void WebStateImpl::RealizedWebState::RemoveAllWebFrames() {
-  for (const auto& iterator : managers_) {
-    iterator.second->RemoveAllWebFrames();
   }
 }
 
