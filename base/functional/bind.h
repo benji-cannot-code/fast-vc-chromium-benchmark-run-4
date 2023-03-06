37a102a84a1ab3183ccbe42100d1191fbcb78156
Bartek Nowierski <bartekn@chromium.org>
2023-03-06 01:05:11 +0000

    Avoid raw_ref<T> -> T* -> raw_ref<T> trip in UnretainedRefWrapper
    This is now possible, because raw_ref now supports cross-kind
    conversion that adds kMayDangle trait.
    
    Change-Id: I819fd948d54dbb9b00f356bb8563c7d00129ca68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190762
    Reviewed-by: Sergei Glazunov <glazunov@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113193}

--- base/functional/bind.h
+++ base/functional/bind.h
@@ -196,7 +196,7 @@ inline auto Unretained(const raw_ref<T, Traits>& o) {
 template <typename T, RawPtrTraits Traits>
 inline auto Unretained(raw_ref<T, Traits>&& o) {
   return internal::UnretainedRefWrapper<T, unretained_traits::MayNotDangle,
-                                        Traits>(o);
+                                        Traits>(std::move(o));
 }
 
 // Similar to `Unretained()`, but allows dangling pointers, e.g.:
@@ -261,7 +261,7 @@ auto UnsafeDangling(const raw_ref<T, Traits>& o) {
 template <typename T, RawPtrTraits Traits>
 auto UnsafeDangling(raw_ref<T, Traits>&& o) {
   return internal::UnretainedRefWrapper<T, unretained_traits::MayDangle,
-                                        Traits>(o);
+                                        Traits>(std::move(o));
 }
 
 // Like `UnsafeDangling()`, but used to annotate places that still need to be
@@ -297,7 +297,7 @@ auto UnsafeDanglingUntriaged(const raw_ref<T, Traits>& o) {
 template <typename T, RawPtrTraits Traits>
 auto UnsafeDanglingUntriaged(raw_ref<T, Traits>&& o) {
   return internal::UnretainedRefWrapper<
-      T, unretained_traits::MayDangleUntriaged, Traits>(o);
+      T, unretained_traits::MayDangleUntriaged, Traits>(std::move(o));
 }
 
 // RetainedRef() accepts a ref counted object and retains a reference to it.
