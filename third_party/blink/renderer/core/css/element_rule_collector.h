775e3d2d40f8f6a52c981b5696dac4d1f9694a5d
Anders Hartvoll Ruud <andruud@chromium.org>
2023-03-06 19:35:35 +0000

    Add :visited duplicate rules to regular buckets
    During RuleSet::AddRule, when adding a rule which depends on visited
    link status, we add a duplicate rule (but with different a
    LinkMatchType) to a special bucket for visited rules. However, this
    approach does not quite work, since we depend on certain rules being
    present in specific buckets in some situations. In particular, rules
    which contain ::part() must be in the part bucket, because we don't
    provide the relevant part names when when matching rules from other
    buckets.
    
    This CL drops the special 'visited_dependent_rules_' and instead
    adds the duplicate rules via FindBestRuleSetAndAdd. Calling
    FindBestRuleSetAndAdd on the duplicate RuleData could incorrectly
    mark the selector as "covered by bucketing", so a BucketCoverage
    parameter is added to suppress this behavior.
    
    Note that the function StyleResolver::StyleRulesForElement (only
    used for Editing) now needs to suppress :visited styles (see
    SerializationTest.Link and crbug.com/938590), because we try to match
    the visited-dependent rules regardless of the value of
    ElementRuleCollector::inside_link_.
    
    Fixed: 1413904
    Change-Id: Id360bef4f1b977107f109c4520db08f6e602a034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295376
    Auto-Submit: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113519}

--- third_party/blink/renderer/core/css/element_rule_collector.h
+++ third_party/blink/renderer/core/css/element_rule_collector.h
@@ -147,6 +147,10 @@ class CORE_EXPORT ElementRuleCollector {
   void SetMatchingUARules(bool matching_ua_rules) {
     matching_ua_rules_ = matching_ua_rules;
   }
+  // If true, :visited will never match. Has no effect otherwise.
+  void SetSuppressVisited(bool suppress_visited) {
+    suppress_visited_ = suppress_visited;
+  }
 
   const MatchResult& MatchedResult() const;
   StyleRuleList* MatchedStyleRuleList();
@@ -278,6 +282,7 @@ class CORE_EXPORT ElementRuleCollector {
   bool can_use_fast_reject_;
   bool same_origin_only_;
   bool matching_ua_rules_;
+  bool suppress_visited_;
   EInsideLink inside_link_;
 
   HeapVector<MatchedRule, 32> matched_rules_;
