1cf046c62f54a3a1cd08a5d512e10fa8e0b0cd41
Roman Arora <romanarora@chromium.org>
2023-03-06 17:54:21 +0000

    [NTP Journeys] Comprehensive theming colors
    Updates color properties to use NTP comprehensive colors that are
    defined in the new_tab_page_color_mixer. Requires a custom item
    background color because it uses a different classic color than the
    existing kColorNewTabPageModuleItemBackground. This discrepancy will
    be revisited in a follow up UX revision of module colors.
    
    The module header icon's color will be addressed as a separate effort.
    
    Screenshots:
    Classic light:
    https://drive.google.com/file/d/1l8dpoE4Rzxt4ClI7Itz6v2-oIkjdj2Ny/view
    
    Classic dark:
    https://drive.google.com/file/d/1KFRCcz7IrXPc6UnsKl5BV18SVyILgNbf/view
    
    Comprehensive light:
    https://drive.google.com/file/d/1QI_AerfJnkNDev6eF4jnbZDaJCP3NBdu/view
    
    Comprehensive dark:
    https://drive.google.com/file/d/1R0ODnMuHWCASqD-UcyG423n471ILO6n_/view
    
    Bug: 1414484
    Change-Id: If2bfe0d7b19cedab2fb38d69af19ab55c8e531b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307316
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113447}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -164,6 +164,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageFirstRunBackground) \
   E_CPONLY(kColorNewTabPageFocusRing) \
   E_CPONLY(kColorNewTabPageHeader) \
+  E_CPONLY(kColorNewTabPageHistoryClustersModuleItemBackground) \
   E_CPONLY(kColorNewTabPagePromoBackground) \
   E_CPONLY(kColorNewTabPagePromoImageBackground) \
   E_CPONLY(kColorNewTabPageIconButtonBackground) \
