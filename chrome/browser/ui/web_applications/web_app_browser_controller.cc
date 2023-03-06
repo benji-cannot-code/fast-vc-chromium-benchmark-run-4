4d5a7d71120ec7c3e92f7895e57a158db18be466
Sean Kau <skau@chromium.org>
2023-03-06 00:20:59 +0000

    Migrate callers of IsJellyEnabled() in /chrome/browser/ui/web_applications
    The Jelly flag moved to chromeos/ from ash/.  The
    convenience function was left to ease migration.
    Cleanup callers so the old function can be deleted.
    
    This CL was uploaded by git cl split.
    
    R=benwells@chromium.org
    
    Bug: b:270741618
    Change-Id: I8bc6cfaf29f9935db5552010f097942dede7ed32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307952
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113183}

--- chrome/browser/ui/web_applications/web_app_browser_controller.cc
+++ chrome/browser/ui/web_applications/web_app_browser_controller.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/apps/apk_web_app_service.h"
 #include "chrome/browser/ash/system_web_apps/color_helpers.h"
 #include "chrome/browser/ash/system_web_apps/types/system_web_app_delegate.h"
+#include "chromeos/constants/chromeos_features.h"
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
@@ -417,7 +418,7 @@ absl::optional<SkColor> WebAppBrowserController::GetThemeColor() const {
   // System Apps with dynamic color ignore manifest and pull theme color from
   // the OS.
   if (system_app() && system_app()->UseSystemThemeColor() &&
-      ash::features::IsJellyEnabled()) {
+      chromeos::features::IsJellyEnabled()) {
     return ash::GetSystemThemeColor();
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
@@ -445,7 +446,7 @@ absl::optional<SkColor> WebAppBrowserController::GetBackgroundColor() const {
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   if (system_app()) {
-    if (ash::features::IsJellyEnabled()) {
+    if (chromeos::features::IsJellyEnabled()) {
       // System Apps with dynamic color ignore the manifest and pull background
       // color from the OS in situations where a background color can not be
       // extracted from the web contents.
