7dfef4035d35cdf7332054a53307f3fec759c631
Kristi Saney <kristislee@google.com>
2023-03-06 20:18:11 +0000

    Log color and font scale.
    Font name will be done in a followup because it will be more complicated
    (requires some refactoring).
    
    Bug: 1266555
    Change-Id: I30e228887147c3fbb190a597027049e9766735fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291986
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113538}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_container_view.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_container_view.cc
@@ -22,6 +22,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/separator.h"
 #include "ui/views/layout/flex_layout.h"
 
+namespace {
+
+int GetNormalizedFontScale(double font_scale) {
+  DCHECK(font_scale >= kReadAnythingMinimumFontScale &&
+         font_scale <= kReadAnythingMaximumFontScale);
+  return (font_scale - kReadAnythingMinimumFontScale) *
+         (1 / kReadAnythingFontScaleIncrement);
+}
+
+}  // namespace
+
 ReadAnythingContainerView::ReadAnythingContainerView(
     ReadAnythingCoordinator* coordinator,
     std::unique_ptr<ReadAnythingToolbarView> toolbar,
@@ -83,6 +94,15 @@ void ReadAnythingContainerView::OnCoordinatorDestroyed() {
 }
 
 void ReadAnythingContainerView::LogTextStyle() {
+  int maximum_font_scale_logging =
+      GetNormalizedFontScale(kReadAnythingMaximumFontScale);
+  double font_scale = coordinator_->GetModel()->GetFontScale();
+  base::UmaHistogramExactLinear(string_constants::kFontScaleHistogramName,
+                                GetNormalizedFontScale(font_scale),
+                                maximum_font_scale_logging + 1);
+  ReadAnythingColorsModel::ReadAnythingColor color =
+      coordinator_->GetModel()->color_logging_value();
+  base::UmaHistogramEnumeration(string_constants::kColorHistogramName, color);
   read_anything::mojom::LineSpacing line_spacing =
       coordinator_->GetModel()->line_spacing();
   base::UmaHistogramEnumeration(string_constants::kLineSpacingHistogramName,
--- chrome/common/accessibility/read_anything_constants.cc
+++ chrome/common/accessibility/read_anything_constants.cc
@@ -14,5 +14,7 @@ const char kLetterSpacingHistogramName[] =
     "Accessibility.ReadAnything.LetterSpacing";
 const char kLineSpacingHistogramName[] =
     "Accessibility.ReadAnything.LineSpacing";
+const char kColorHistogramName[] = "Accessibility.ReadAnything.Color";
+const char kFontScaleHistogramName[] = "Accessibility.ReadAnything.FontScale";
 
 }  // namespace string_constants
