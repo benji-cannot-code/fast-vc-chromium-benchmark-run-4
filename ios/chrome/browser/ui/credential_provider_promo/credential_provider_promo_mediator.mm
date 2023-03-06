852eac9fc6b80bdcffa12f7111d94669f51e2711
Hira Mahmood <hiramahmood@google.com>
2023-03-06 21:14:57 +0000

    [iOS] Create CredentialProviderPromoEnabled enterprise policy
    This CL:
     * Creates the CredentialProviderPromoEnabled policy.
     * Checks if the policy is enabled before displaying the Credential
       Provider Promo.
    
    Bug: 1402963
    Change-Id: I32348b872e0e7bbadb788cbbb3b36b5fe372090c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307770
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113576}

--- ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_mediator.mm
+++ ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_mediator.mm
@@ -66,7 +66,10 @@ NSString* const kLearnMoreAnimation = @"CPE_promo_animation_edu_how_to_enable";
           prefs::kIosCredentialProviderPromoStopPromo) ||
       (promoSeenInCurrentSession &&
        trigger != CredentialProviderPromoTrigger::RemindMeLater);
+  BOOL policyEnabled = GetApplicationContext()->GetLocalState()->GetBoolean(
+      prefs::kIosCredentialProviderPromoPolicyEnabled);
   return !impressionLimitMet && IsCredentialProviderExtensionPromoEnabled() &&
+         policyEnabled &&
          !password_manager_util::IsCredentialProviderEnabledOnStartup(
              self.prefService);
 }
