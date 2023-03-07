eeeb4467961ed1e2d01cee75a0315b5b5aba62d7
Hira Mahmood <hiramahmood@google.com>
2023-03-07 20:25:00 +0000

    [iOS] Localize text in Lottie animations for Credential Provider Promo
    This CL:
     * Adds new Lottie animation jsons that allow for text localization
     * Localizes the "Password Options" text in the animation files
       in the VC
    
    Bug: 1402963
    Change-Id: I6cb4eb9c77e4c6e0fdc51229e196af497d55f96a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4313339
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114136}

--- ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_view_controller.mm
+++ ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_view_controller.mm
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/values.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/lottie/lottie_animation_api.h"
 #import "ios/public/provider/chrome/browser/lottie/lottie_animation_configuration.h"
+#import "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -21,6 +23,7 @@ constexpr CGFloat kCustomSpacingAfterImageWithAnimation = 24;
 constexpr CGFloat kCustomSpacingAfterImageWithoutAnimation = 0;
 constexpr CGFloat kPreferredCornerRadius = 20;
 NSString* const kDarkModeAnimationSuffix = @"_darkmode";
+NSString* const kPasswordOptionsKeypath = @"text_password_options";
 }  // namespace
 
 @interface CredentialProviderPromoViewController ()
@@ -100,6 +103,20 @@ NSString* const kDarkModeAnimationSuffix = @"_darkmode";
   _animationViewWrapperDarkMode = [self
       createAnimation:[animationAssetName
                           stringByAppendingString:kDarkModeAnimationSuffix]];
+
+  NSString* passwordSettingsTitle;
+  if (@available(iOS 16, *)) {
+    passwordSettingsTitle = l10n_util::GetNSString(
+        IDS_IOS_CREDENTIAL_PROVIDER_PROMO_OS_PASSWORDS_SETTINGS_TITLE_IOS16);
+  } else {
+    passwordSettingsTitle = l10n_util::GetNSString(
+        IDS_IOS_CREDENTIAL_PROVIDER_PROMO_OS_PASSWORDS_SETTINGS_TITLE_BELOW_IOS16);
+  }
+  // Set the text localization.
+  NSDictionary* textProvider =
+      @{kPasswordOptionsKeypath : passwordSettingsTitle};
+  [_animationViewWrapper setDictionaryTextProvider:textProvider];
+  [_animationViewWrapperDarkMode setDictionaryTextProvider:textProvider];
 }
 
 #pragma mark - Private
