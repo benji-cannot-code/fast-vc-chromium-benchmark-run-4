b776a34649ce8f96b27f1f177d2d244a7ec89636
Jack Hsieh <chengweih@chromium.org>
2023-03-07 22:00:02 +0000

    Switch to use VNDetectBarcodesRequest.supportedSymbologiesAndReturnError
    Use VNDetectBarcodesRequest.supportedSymbologiesAndReturnError instead of
    VNDetectBarcodesRequest.supportedSymbologies as that is marked deprecated
    starting from MacOS 12.0.
    
    Bug: 1420435
    Change-Id: I8cf9fbd1038e0d70b359db4b3d9836391b611354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4313189
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114200}

--- services/shape_detection/barcode_detection_impl_mac_vision_api.mm
+++ services/shape_detection/barcode_detection_impl_mac_vision_api.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/shape_detection/barcode_detection_impl_mac_vision_api.h"
 
 #include "base/logging.h"
+#include "base/mac/scoped_nsobject.h"
+#include "base/strings/sys_string_conversions.h"
 
 namespace shape_detection {
 
@@ -18,7 +20,19 @@ class VisionAPI : public VisionAPIInterface {
   ~VisionAPI() override = default;
 
   NSArray<VNBarcodeSymbology>* GetSupportedSymbologies() const override {
-    return [VNDetectBarcodesRequest supportedSymbologies];
+    if (@available(macOS 12.0, *)) {
+      base::scoped_nsobject<VNDetectBarcodesRequest> barcodes_request(
+          [[VNDetectBarcodesRequest alloc] init]);
+      NSError* error = nil;
+      NSArray<VNBarcodeSymbology>* symbologies =
+          [barcodes_request supportedSymbologiesAndReturnError:&error];
+      if (error) {
+        DLOG(ERROR) << base::SysNSStringToUTF8([error localizedDescription]);
+      }
+      return symbologies;
+    } else {
+      return [VNDetectBarcodesRequest supportedSymbologies];
+    }
   }
 };
 
