300c5677a9a0183e00433cf594f06aadbd178dcc
Peter Kasting <pkasting@chromium.org>
2023-03-07 21:18:36 +0000

    Use base::ranges::transform(): cc/
    Bug: 1368812
    Change-Id: Ib6b096539c8b9907ad6db6518c5d0586c16d6f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310652
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114164}

--- cc/trees/ukm_manager_unittest.cc
+++ cc/trees/ukm_manager_unittest.cc
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/trees/ukm_manager.h"
 
-#include <algorithm>
 #include <utility>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
+#include "base/ranges/algorithm.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "base/time/time.h"
 #include "cc/metrics/begin_main_frame_metrics.h"
@@ -183,9 +183,9 @@ class UkmManagerTest : public testing::Test {
       const EventMetrics::List& events_metrics) {
     std::vector<DispatchTimestamps> event_times;
     event_times.reserve(events_metrics.size());
-    std::transform(
-        events_metrics.cbegin(), events_metrics.cend(),
-        std::back_inserter(event_times), [](const auto& event_metrics) {
+    base::ranges::transform(
+        events_metrics, std::back_inserter(event_times),
+        [](const auto& event_metrics) {
           return DispatchTimestamps{
               event_metrics->GetDispatchStageTimestamp(
                   EventMetrics::DispatchStage::kGenerated),
