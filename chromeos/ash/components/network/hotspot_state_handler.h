4d7d23172f3795b233314a1a78a5c30bbf03208c
Nikhil Nayunigari <nikhilcn@google.com>
2023-03-07 16:22:54 +0000

    [CrOS Hotspot] HotspotStateHandler changes to update disable reason
    This CL extracts idle reason from shill when the hotspot is disabling or
    disabled, converts that to the DisableReason enum and notifies
    observers.
    
    Bug: b/269353677
    Change-Id: I5e99d5fbd2839d34e22c3fc1ef3c092b5d4e8d2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304523
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113966}

--- chromeos/ash/components/network/hotspot_state_handler.h
+++ chromeos/ash/components/network/hotspot_state_handler.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chromeos/ash/components/dbus/shill/shill_property_changed_observer.h"
 #include "chromeos/ash/components/login/login_state/login_state.h"
+#include "chromeos/ash/services/hotspot_config/public/cpp/hotspot_enabled_state_provider.h"
 #include "chromeos/ash/services/hotspot_config/public/mojom/cros_hotspot_config.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -26,7 +27,8 @@ namespace ash {
 // configurations.
 class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotStateHandler
     : public ShillPropertyChangedObserver,
-      public LoginState::Observer {
+      public LoginState::Observer,
+      public hotspot_config::HotspotEnabledStateProvider {
  public:
   class Observer : public base::CheckedObserver {
    public:
@@ -81,6 +83,9 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotStateHandler
   // Update the cached hotspot_state_ and active_client_count_ from hotspot
   // status in Shill.
   void UpdateHotspotStatus(const base::Value::Dict& status);
+
+  // Updates the reason for hotspot getting disabled and notifies observers.
+  void UpdateDisableReason(const base::Value::Dict& status);
 
   // Notify observers that hotspot state or active client count was changed.
   void NotifyHotspotStatusChanged();
