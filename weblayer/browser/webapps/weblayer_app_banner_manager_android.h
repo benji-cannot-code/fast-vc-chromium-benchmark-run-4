4c2e344987df193e613c8c50044455cf80b72ca7
Ella Ge <eirage@chromium.org>
2023-03-07 18:24:40 +0000

    [CodeHealth] Move pwa show bottomsheet check to AppBannerManagerAndroid
    PWA bottom sheet and `MaybeShowPwaBottomSheetController` was moved into
    components a while ago (crrev.com/c/3320277). There is no need to keep
    the override ShowAmbientBadge/ShowBannerUi in chrome/
    
    Besides, base AppBannerManager::ParamsToPerformInstallableWebAppCheck
    already has `fetch_screenshots = true`. No need to override as well
    
    Change-Id: Ife407dbe81f63fa2108b8b493fceaf48a06b9bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300752
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114056}

--- weblayer/browser/webapps/weblayer_app_banner_manager_android.h
+++ weblayer/browser/webapps/weblayer_app_banner_manager_android.h
@@ -26,7 +26,6 @@ class WebLayerAppBannerManagerAndroid
       WebLayerAppBannerManagerAndroid>::FromWebContents;
 
  protected:
-  webapps::InstallableParams ParamsToPerformInstallableWebAppCheck() override;
   void ShowAmbientBadge() override;
   void ShowBannerUi(webapps::WebappInstallSource install_source) override;
 
