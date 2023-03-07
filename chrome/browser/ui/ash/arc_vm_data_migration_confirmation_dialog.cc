20f0dea87f084c4248a128a099b5729ed6afbe66
Youkichi Hosoi <youkichihosoi@chromium.org>
2023-03-07 04:05:52 +0000

    arc: Move ARCVM /data migration dialog code to //chrome/browser/ui/ash
    Its UI needs to depend on //chrome/browser/ui/views (CL:4308490).
    
    Bug: b:258278176
    Test: unit_tests
    
    Change-Id: I59898582a6623ba2b8984f2dcd5c4ccc1d830333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310581
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Youkichi Hosoi <youkichihosoi@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113768}

--- chrome/browser/ui/ash/arc_vm_data_migration_confirmation_dialog.cc
+++ chrome/browser/ui/ash/arc_vm_data_migration_confirmation_dialog.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/components/arc/session/arc_vm_data_migration_confirmation_dialog.h"
+#include "chrome/browser/ui/ash/arc_vm_data_migration_confirmation_dialog.h"
 
 #include "base/strings/utf_string_conversions.h"
 #include "ui/views/controls/image_view.h"
--- chrome/browser/ui/ash/arc_vm_data_migration_confirmation_dialog.h
+++ chrome/browser/ui/ash/arc_vm_data_migration_confirmation_dialog.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_COMPONENTS_ARC_SESSION_ARC_VM_DATA_MIGRATION_CONFIRMATION_DIALOG_H_
-#define ASH_COMPONENTS_ARC_SESSION_ARC_VM_DATA_MIGRATION_CONFIRMATION_DIALOG_H_
+#ifndef CHROME_BROWSER_UI_ASH_ARC_VM_DATA_MIGRATION_CONFIRMATION_DIALOG_H_
+#define CHROME_BROWSER_UI_ASH_ARC_VM_DATA_MIGRATION_CONFIRMATION_DIALOG_H_
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
@@ -44,4 +44,4 @@ void ShowArcVmDataMigrationConfirmationDialog(
 
 }  // namespace arc
 
-#endif  // ASH_COMPONENTS_ARC_SESSION_ARC_VM_DATA_MIGRATION_CONFIRMATION_DIALOG_H_
+#endif  // CHROME_BROWSER_UI_ASH_ARC_VM_DATA_MIGRATION_CONFIRMATION_DIALOG_H_
