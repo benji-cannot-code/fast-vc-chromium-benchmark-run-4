e570b357f1ab0b6bbf1c210f6d12c7f607f2706a
Louis Romero <lpromero@google.com>
2023-03-07 16:46:23 +0000

    Add histogram for rescued inactive tabs
    If the Inactive Tabs feature flag is disabled, the tabs previously
    persisted in the inactive browser need to be restored to the regular
    browser.
    This CL adds a count histogram for the number of tabs restored.
    
    Fixed: 1418800
    Change-Id: I668216ead4a7488d72b4872e112dbb173fefadcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288867
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113977}

--- ios/chrome/browser/tabs/inactive_tabs/utils.mm
+++ ios/chrome/browser/tabs/inactive_tabs/utils.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/tabs/inactive_tabs/utils.h"
 
+#import "base/metrics/histogram_functions.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/tabs/inactive_tabs/features.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
@@ -88,6 +89,10 @@ void RestoreAllInactiveTabs(Browser* inactive_browser,
   DCHECK(!IsInactiveTabsEnabled());
   WebStateList* active_web_state_list = active_browser->GetWebStateList();
   WebStateList* inactive_web_state_list = inactive_browser->GetWebStateList();
+  // Record the number of tabs restored from the inactive browser after Inactive
+  // Tabs has been disabled.
+  base::UmaHistogramCounts100("Tabs.RestoredFromInactiveCount",
+                              inactive_web_state_list->count());
   for (int index = inactive_web_state_list->count() - 1; index >= 0; index--) {
     MoveTab(inactive_web_state_list, index, active_web_state_list,
             active_web_state_list->GetIndexOfFirstNonPinnedWebState());
