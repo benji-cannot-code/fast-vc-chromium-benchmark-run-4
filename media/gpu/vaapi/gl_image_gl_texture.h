4b07e0a1498b86e86c3d07ca0fefbc43ad1b8d76
Colin Blundell <blundell@chromium.org>
2023-03-07 08:47:57 +0000

    [GL] Move GLImageGLTexture into //media/gpu/vaapi
    This CL moves gl_image_gl_texture* into //media/gpu/vaapi to sit next to
    its only consumer as part of the continuing dismantling and eventual
    elimination of the general-purpose GLImage abstraction.
    
    Note: The unittest has to stay in //ui/gl due to complexities of its
    setup that make it infeasible to run as part of media_unittests. It will
    be eliminated when gl_image_gl_texture.* itself is eliminated.
    
    Bug: 1310018
    Change-Id: I797b8d7a8ae7e042a787a68fbae1be9fe6d31c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290131
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113818}

--- media/gpu/vaapi/gl_image_gl_texture.h
+++ media/gpu/vaapi/gl_image_gl_texture.h
@@ -3,20 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_GL_GL_IMAGE_GL_TEXTURE_H_
-#define UI_GL_GL_IMAGE_GL_TEXTURE_H_
+#ifndef MEDIA_GPU_VAAPI_GL_IMAGE_GL_TEXTURE_H_
+#define MEDIA_GPU_VAAPI_GL_IMAGE_GL_TEXTURE_H_
 
 #include <stdint.h>
 
 #include "base/memory/raw_ptr.h"
 #include "base/threading/thread_checker.h"
 #include "ui/gfx/native_pixmap_handle.h"
-#include "ui/gl/gl_export.h"
 #include "ui/gl/gl_image.h"
 
-namespace gl {
+namespace media {
 
-class GL_EXPORT GLImageGLTexture : public GLImage {
+class GLImageGLTexture : public gl::GLImage {
  public:
   // Create an EGLImage from a given GL texture.
   static scoped_refptr<GLImageGLTexture> CreateFromTexture(
@@ -53,6 +52,6 @@ class GL_EXPORT GLImageGLTexture : public GLImage {
   bool has_image_dma_buf_export_;
 };
 
-}  // namespace gl
+}  // namespace media
 
-#endif  // UI_GL_GL_IMAGE_GL_TEXTURE_H_
+#endif  // MEDIA_GPU_VAAPI_GL_IMAGE_GL_TEXTURE_H_
--- media/gpu/vaapi/vaapi_picture_native_pixmap_egl.h
+++ media/gpu/vaapi/vaapi_picture_native_pixmap_egl.h
@@ -18,12 +18,9 @@ namespace gfx {
 class NativePixmap;
 }  // namespace gfx
 
-namespace gl {
-class GLImageGLTexture;
-}
-
 namespace media {
 
+class GLImageGLTexture;
 class VaapiWrapper;
 
 // Implementation of VaapiPictureNativePixmap for EGL backends, see
@@ -57,7 +54,7 @@ class VaapiPictureNativePixmapEgl : public VaapiPictureNativePixmap {
   VaapiStatus Initialize(scoped_refptr<gfx::NativePixmap> pixmap);
 
   // GLImage bound to the GL textures used by the VDA client.
-  scoped_refptr<gl::GLImageGLTexture> gl_image_;
+  scoped_refptr<GLImageGLTexture> gl_image_;
 };
 
 }  // namespace media
