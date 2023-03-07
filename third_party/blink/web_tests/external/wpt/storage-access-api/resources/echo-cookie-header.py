11bf4308dc8343c6b28df3dcdd0a3f81db2714f0
Shuran Huang <shuuran@chromium.org>
2023-03-07 18:08:35 +0000

    Add WPT for cross-site sibling iframes case
    Bug: 1401089
    Change-Id: I121b79869175311de2a4a6c47f2d58a2992f7ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4305139
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114045}

--- third_party/blink/web_tests/external/wpt/storage-access-api/resources/echo-cookie-header.py
+++ third_party/blink/web_tests/external/wpt/storage-access-api/resources/echo-cookie-header.py
@@ -1,5 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
+  # Set the cors enabled headers.
+  origin = request.headers.get(b"Origin")
+  headers = []
+  if origin is not None and origin != b"null":
+      headers.append((b"Content-Type", b"text/plain"))
+      headers.append((b"Access-Control-Allow-Origin", origin))
+      headers.append((b"Access-Control-Allow-Credentials", 'true'))
+
   cookie_header = request.headers.get(b"Cookie", b"")
 
-  return (200, [], cookie_header)
+  return (200, headers, cookie_header)
