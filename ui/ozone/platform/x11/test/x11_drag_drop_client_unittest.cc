c9a6171558191a0faeb610c8505dfaf28c5d7f89
Tom Sepez <tsepez@chromium.org>
2023-03-07 18:00:13 +0000

    Simplify ownership of SimpleTestDragDropClient::loop_
    Currently, it is owned by a local in the method StartDragAndDrop()
    and tracked unowned in the longer lived test class itself. This is
    backwards and leads to dangling ptr warnings. Make the class own
    the loop itself.
    
    Bug: 1401495
    Change-Id: Ib05d84d3130201a0dad4b40bb0b9a3f0a7887c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289111
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114037}

--- ui/ozone/platform/x11/test/x11_drag_drop_client_unittest.cc
+++ ui/ozone/platform/x11/test/x11_drag_drop_client_unittest.cc
@@ -150,13 +150,11 @@ class SimpleTestDragDropClient : public XDragDropClient,
   void OnMouseReleased() override;
   void OnMoveLoopEnded() override;
 
-  std::unique_ptr<X11MoveLoop> CreateMoveLoop(X11MoveLoopDelegate* delegate);
-
   // The x11::Window of the window which is simulated to be the topmost window.
   x11::Window target_window_ = x11::Window::None;
 
-  // The move loop. Not owned.
-  raw_ptr<TestMoveLoop> loop_ = nullptr;
+  // The move loop.
+  std::unique_ptr<TestMoveLoop> loop_;
 
   base::OnceClosure quit_closure_;
 };
@@ -294,12 +292,6 @@ bool SimpleTestDragDropClient::IsMoveLoopRunning() {
   return loop_->IsRunning();
 }
 
-std::unique_ptr<X11MoveLoop> SimpleTestDragDropClient::CreateMoveLoop(
-    X11MoveLoopDelegate* delegate) {
-  loop_ = new TestMoveLoop(delegate);
-  return base::WrapUnique(loop_.get());
-}
-
 DragOperation SimpleTestDragDropClient::StartDragAndDrop(
     std::unique_ptr<OSExchangeData> data,
     X11Window* source_window,
@@ -307,13 +299,14 @@ DragOperation SimpleTestDragDropClient::StartDragAndDrop(
     mojom::DragEventSource source) {
   InitDrag(allowed_operations, data.get());
 
-  auto loop = CreateMoveLoop(this);
+  loop_ = std::make_unique<TestMoveLoop>(this);
 
   // Cursors are not set. Thus, pass nothing.
   loop_->RunMoveLoop(!source_window->HasCapture(), {}, {});
 
   auto resulting_operation = negotiated_operation();
   CleanupDrag();
+  loop_.reset();
   return resulting_operation;
 }
 
