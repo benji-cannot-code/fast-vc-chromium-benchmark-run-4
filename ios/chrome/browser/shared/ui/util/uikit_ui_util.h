e3ae87a6429033cd66d8e143d9cbee78c69aeff8
Gauthier Ambard <gambard@chromium.org>
2023-03-07 10:45:38 +0000

    [iOS] Prepare to move the ios/c/b/ui/util folder
    This CL prepares to move the ui/util folder to a new shared location.
    This CL is here for downstream compatibility.
    
    The goal of this new structure is to have a better separation of UI
    and coordinators and explicit DEPS rules for includes. This new shared
    folder will allow to have better DEPS rules (every UI will be able to
    depend on this folder).
    
    Bug: 1319852
    
    Change-Id: I0f051a833635eb64262247f0bead9610c56714bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295663
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113856}

---
+++ ios/chrome/browser/shared/ui/util/uikit_ui_util.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_UI_UTIL_UIKIT_UI_UTIL_H_
+#define IOS_CHROME_BROWSER_SHARED_UI_UTIL_UIKIT_UI_UTIL_H_
+
+// Temporary include.
+#import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_UI_UTIL_UIKIT_UI_UTIL_H_
