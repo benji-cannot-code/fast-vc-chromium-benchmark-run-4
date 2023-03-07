78d1e4bebe66f912ce7f84ec28f3da462a5cb425
Zachary Tan <tanzachary@chromium.org>
2023-03-07 19:56:18 +0000

    [FedCM] Add iframe domain Android UI
    In a follow-up patch, we will fetch from client metadata whether to show
    the iframe domain. A screenshot of the UI is available on
    crbug.com/1418719. Please note that redlines are not finalized yet.
    
    Bug: 1418719
    Change-Id: I30e4e9c83fa1260bf54e45a4bfa51de0afa5f6fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4312259
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114118}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionIntegrationTest.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionIntegrationTest.java
@@ -91,6 +91,7 @@ public class AccountSelectionIntegrationTest {
 
     private static final String EXAMPLE_ETLD_PLUS_ONE = "example.com";
     private static final String TEST_ETLD_PLUS_ONE_1 = "one.com";
+    private static final String TEST_ETLD_PLUS_ONE_2 = "two.com";
     private static final GURL TEST_PROFILE_PIC =
             JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_1_WITH_PATH);
 
@@ -139,7 +140,8 @@ public class AccountSelectionIntegrationTest {
     public void testBackDismissesAndCallsCallback() {
         runOnUiThreadBlocking(() -> {
             mAccountSelection.showAccounts(EXAMPLE_ETLD_PLUS_ONE, TEST_ETLD_PLUS_ONE_1,
-                    Arrays.asList(ANA, BOB), IDP_METADATA, mClientIdMetadata, false);
+                    TEST_ETLD_PLUS_ONE_2, Arrays.asList(ANA, BOB), IDP_METADATA, mClientIdMetadata,
+                    false);
         });
         pollUiThread(() -> getBottomSheetState() == BottomSheetController.SheetState.FULL);
 
@@ -152,7 +154,8 @@ public class AccountSelectionIntegrationTest {
     private void testClickOnConsentLink(int linkIndex, String expectedUrl) {
         runOnUiThreadBlocking(() -> {
             mAccountSelection.showAccounts(EXAMPLE_ETLD_PLUS_ONE, TEST_ETLD_PLUS_ONE_1,
-                    Arrays.asList(BOB), IDP_METADATA, mClientIdMetadata, false);
+                    TEST_ETLD_PLUS_ONE_2, Arrays.asList(BOB), IDP_METADATA, mClientIdMetadata,
+                    false);
         });
         pollUiThread(() -> getBottomSheetState() == BottomSheetController.SheetState.FULL);
 
@@ -209,7 +212,8 @@ public class AccountSelectionIntegrationTest {
 
         runOnUiThreadBlocking(() -> {
             mAccountSelection.showAccounts(EXAMPLE_ETLD_PLUS_ONE, TEST_ETLD_PLUS_ONE_1,
-                    Arrays.asList(ANA, BOB), IDP_METADATA, mClientIdMetadata, false);
+                    TEST_ETLD_PLUS_ONE_2, Arrays.asList(ANA, BOB), IDP_METADATA, mClientIdMetadata,
+                    false);
         });
         waitForEvent(mMockBridge).onDismissed(IdentityRequestDialogDismissReason.OTHER);
         verify(mMockBridge, never()).onAccountSelected(any(), any());
--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionMediator.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionMediator.java
@@ -84,7 +84,8 @@ class AccountSelectionMediator {
     public static final long POTENTIALLY_UNINTENDED_INPUT_THRESHOLD = 500;
 
     private HeaderType mHeaderType;
-    private String mRpForDisplay;
+    private String mTopFrameForDisplay;
+    private String mIframeForDisplay;
     private String mIdpForDisplay;
     private IdentityProviderMetadata mIdpMetadata;
     private Bitmap mBrandIcon;
@@ -152,12 +153,13 @@ class AccountSelectionMediator {
 
     private void handleBackPress() {
         mSelectedAccount = null;
-        showAccountsInternal(mRpForDisplay, mIdpForDisplay, mAccounts, mIdpMetadata,
-                mClientMetadata, /*isAutoReauthn=*/false, /*focusItem=*/ItemProperties.HEADER);
+        showAccountsInternal(mTopFrameForDisplay, mIframeForDisplay, mIdpForDisplay, mAccounts,
+                mIdpMetadata, mClientMetadata, /*isAutoReauthn=*/false,
+                /*focusItem=*/ItemProperties.HEADER);
     }
 
-    private PropertyModel createHeaderItem(HeaderType headerType, String rpForDisplay,
-            String idpForDisplay, IdentityProviderMetadata idpMetadata) {
+    private PropertyModel createHeaderItem(HeaderType headerType, String topFrameForDisplay,
+            String iframeForDisplay, String idpForDisplay, IdentityProviderMetadata idpMetadata) {
         Runnable closeOnClickRunnable = () -> {
             onDismissed(IdentityRequestDialogDismissReason.CLOSE_BUTTON);
 
@@ -171,7 +173,8 @@ class AccountSelectionMediator {
                 .with(HeaderProperties.IDP_BRAND_ICON, mBrandIcon)
                 .with(HeaderProperties.CLOSE_ON_CLICK_LISTENER, closeOnClickRunnable)
                 .with(HeaderProperties.IDP_FOR_DISPLAY, idpForDisplay)
-                .with(HeaderProperties.TOP_FRAME_FOR_DISPLAY, rpForDisplay)
+                .with(HeaderProperties.TOP_FRAME_FOR_DISPLAY, topFrameForDisplay)
+                .with(HeaderProperties.IFRAME_FOR_DISPLAY, iframeForDisplay)
                 .with(HeaderProperties.TYPE, headerType)
                 .build();
     }
@@ -226,9 +229,9 @@ class AccountSelectionMediator {
         if (!mWasDismissed) hideContent();
     }
 
-    void showAccounts(String rpForDisplay, String idpForDisplay, List<Account> accounts,
-            IdentityProviderMetadata idpMetadata, ClientIdMetadata clientMetadata,
-            boolean isAutoReauthn) {
+    void showAccounts(String topFrameForDisplay, String iframeForDisplay, String idpForDisplay,
+            List<Account> accounts, IdentityProviderMetadata idpMetadata,
+            ClientIdMetadata clientMetadata, boolean isAutoReauthn) {
         if (!TextUtils.isEmpty(idpMetadata.getBrandIconUrl())) {
             // Use placeholder icon so that the header text wrapping does not change when the icon
             // is fetched.
@@ -238,8 +241,8 @@ class AccountSelectionMediator {
         }
 
         mSelectedAccount = accounts.size() == 1 ? accounts.get(0) : null;
-        showAccountsInternal(rpForDisplay, idpForDisplay, accounts, idpMetadata, clientMetadata,
-                isAutoReauthn, /*focusItem=*/ItemProperties.HEADER);
+        showAccountsInternal(topFrameForDisplay, iframeForDisplay, idpForDisplay, accounts,
+                idpMetadata, clientMetadata, isAutoReauthn, /*focusItem=*/ItemProperties.HEADER);
         setComponentShowTime(SystemClock.elapsedRealtime());
 
         if (!TextUtils.isEmpty(idpMetadata.getBrandIconUrl())) {
@@ -264,10 +267,11 @@ class AccountSelectionMediator {
         mComponentShowTime = componentShowTime;
     }
 
-    private void showAccountsInternal(String rpForDisplay, String idpForDisplay,
-            List<Account> accounts, IdentityProviderMetadata idpMetadata,
+    private void showAccountsInternal(String topFrameForDisplay, String iframeForDisplay,
+            String idpForDisplay, List<Account> accounts, IdentityProviderMetadata idpMetadata,
             ClientIdMetadata clientMetadata, boolean isAutoReauthn, PropertyKey focusItem) {
-        mRpForDisplay = rpForDisplay;
+        mTopFrameForDisplay = topFrameForDisplay;
+        mIframeForDisplay = iframeForDisplay;
         mIdpForDisplay = idpForDisplay;
         mAccounts = accounts;
         mIdpMetadata = idpMetadata;
@@ -316,8 +320,8 @@ class AccountSelectionMediator {
     }
 
     private void updateHeader() {
-        PropertyModel headerModel =
-                createHeaderItem(mHeaderType, mRpForDisplay, mIdpForDisplay, mIdpMetadata);
+        PropertyModel headerModel = createHeaderItem(
+                mHeaderType, mTopFrameForDisplay, mIframeForDisplay, mIdpForDisplay, mIdpMetadata);
         mModel.set(ItemProperties.HEADER, headerModel);
     }
 
@@ -385,8 +389,8 @@ class AccountSelectionMediator {
         Account oldSelectedAccount = mSelectedAccount;
         mSelectedAccount = selectedAccount;
         if (oldSelectedAccount == null && !mSelectedAccount.isSignIn()) {
-            showAccountsInternal(mRpForDisplay, mIdpForDisplay, mAccounts, mIdpMetadata,
-                    mClientMetadata, /*isAutoReauthn=*/false,
+            showAccountsInternal(mTopFrameForDisplay, mIframeForDisplay, mIdpForDisplay, mAccounts,
+                    mIdpMetadata, mClientMetadata, /*isAutoReauthn=*/false,
                     /*focusItem=*/ItemProperties.CONTINUE_BUTTON);
             return;
         }
