d74940e6097cbad9ab384893a08a1f494bdbd47c
Emilia Paz <emiliapaz@chromium.org>
2023-03-07 19:25:13 +0000

    [Extensions c2s] Use coordinator pattern for request button hover card
    Use coordinator to display hover card as part of the MVC refactor for
    BubbleDialogDelegateView. No functionality changed.
    
    Screencast: https://drive.google.com/file/d/1omrT_aXZj1mW2zUV_oupReWK-3Ttj_-W/view?usp=sharing
    
    Bug: 1239772, 1342246
    Change-Id: I96654602754f1bec7417179d523c341def113de1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257591
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114103}

--- chrome/browser/ui/views/extensions/extensions_request_access_button.cc
+++ chrome/browser/ui/views/extensions/extensions_request_access_button.cc
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_action_runner.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/views/extensions/extensions_request_access_button_hover_card.h"
+#include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
+#include "chrome/browser/ui/views/extensions/extensions_request_access_hover_card_coordinator.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -44,7 +45,9 @@ ExtensionsRequestAccessButton::ExtensionsRequestAccessButton(
           base::BindRepeating(&ExtensionsRequestAccessButton::OnButtonPressed,
                               base::Unretained(this))),
       browser_(browser),
-      extensions_container_(extensions_container) {}
+      extensions_container_(extensions_container),
+      hover_card_coordinator_(
+          std::make_unique<ExtensionsRequestAccessHoverCardCoordinator>()) {}
 
 ExtensionsRequestAccessButton::~ExtensionsRequestAccessButton() = default;
 
@@ -68,12 +71,13 @@ void ExtensionsRequestAccessButton::Update(
 }
 
 void ExtensionsRequestAccessButton::MaybeShowHoverCard() {
-  if (ExtensionsRequestAccessButtonHoverCard::IsShowing() ||
-      !GetWidget()->IsMouseEventsEnabled())
+  if (hover_card_coordinator_->IsShowing() ||
+      !GetWidget()->IsMouseEventsEnabled()) {
     return;
+  }
 
-  ExtensionsRequestAccessButtonHoverCard::ShowBubble(
-      GetActiveWebContents(), this, extensions_container_, extension_ids_);
+  hover_card_coordinator_->ShowBubble(GetActiveWebContents(), this,
+                                      extensions_container_, extension_ids_);
 }
 
 std::u16string ExtensionsRequestAccessButton::GetTooltipText(
@@ -83,15 +87,16 @@ std::u16string ExtensionsRequestAccessButton::GetTooltipText(
 }
 
 void ExtensionsRequestAccessButton::OnButtonPressed() {
-  if (ExtensionsRequestAccessButtonHoverCard::IsShowing()) {
-    ExtensionsRequestAccessButtonHoverCard::HideBubble();
+  if (hover_card_coordinator_->IsShowing()) {
+    hover_card_coordinator_->HideBubble();
   }
 
   content::WebContents* web_contents = GetActiveWebContents();
   extensions::ExtensionActionRunner* action_runner =
       extensions::ExtensionActionRunner::GetForWebContents(web_contents);
-  if (!action_runner)
+  if (!action_runner) {
     return;
+  }
 
   DCHECK_GT(extension_ids_.size(), 0u);
   std::vector<const extensions::Extension*> extensions_to_run =
@@ -115,7 +120,7 @@ void ExtensionsRequestAccessButton::OnMouseEntered(
 }
 
 void ExtensionsRequestAccessButton::OnMouseExited(const ui::MouseEvent& event) {
-  ExtensionsRequestAccessButtonHoverCard::HideBubble();
+  hover_card_coordinator_->HideBubble();
 }
 
 content::WebContents* ExtensionsRequestAccessButton::GetActiveWebContents() {
--- chrome/browser/ui/views/extensions/extensions_request_access_button.h
+++ chrome/browser/ui/views/extensions/extensions_request_access_button.h
@@ -15,9 +15,10 @@ class WebContents;
 
 class Browser;
 class ExtensionsContainer;
+class ExtensionsRequestAccessHoverCardCoordinator;
 
-// Button in the toolbar bar that displays the extensions that requests access,
-// and grants them access.
+// Button in the toolbar bar that displays the extensions that requests
+// access, and are allowed to do so, and grants them access.
 class ExtensionsRequestAccessButton : public ToolbarButton {
  public:
   explicit ExtensionsRequestAccessButton(
@@ -46,6 +47,10 @@ class ExtensionsRequestAccessButton : public ToolbarButton {
   std::vector<extensions::ExtensionId> GetExtensionIdsForTesting() {
     return extension_ids_;
   }
+  ExtensionsRequestAccessHoverCardCoordinator*
+  GetHoverCardCoordinatorForTesting() {
+    return hover_card_coordinator_.get();
+  }
 
  private:
   // Runs `extension_ids_` actions in the current site.
@@ -55,6 +60,9 @@ class ExtensionsRequestAccessButton : public ToolbarButton {
 
   raw_ptr<Browser> browser_;
   raw_ptr<ExtensionsContainer> extensions_container_;
+
+  std::unique_ptr<ExtensionsRequestAccessHoverCardCoordinator>
+      hover_card_coordinator_;
 
   // Extensions included in the request access button.
   std::vector<extensions::ExtensionId> extension_ids_;
