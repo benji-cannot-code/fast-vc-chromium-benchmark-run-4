fcb5ab3e6a67b61609ce93b48b55d08c849210fe
Petro Akzhygitov <pakzhygitov@google.com>
2023-03-07 11:52:53 +0000

    Fixed regular tabs "flashing" during the tab view to grid animation.
    I was investigating this issue for a while a found out the problem. Before the animation is started `TabGrid` is requesting snapshots for regular tab cells but now also for the pinned tab cells as well (even though the snapshots on the pinned tabs are not visible they are still used during the grid to the tab view animation). The snapshots are preloaded in the so called `appearanceCache` to make access to them during the animation quicker. But filling the `appearanceCache` is not always finishes before the animation begins. It is because the LRU cache we use to avoid extensive disk reads is not big enough.
    
    Here is a simple example. Previously, tab grid on the iPhone was requiring on average 8 snapshots. Now, with the pinned tabs feature it requires on average 12 snapshots. 12 snapshots is twice more than LRU cache could currently fill in. Therefore, displaying a `TabGrid` on the iPhone takes on average 6 disk reads. In a lot of times these reads are not ready before the animation begins. Which leads to the animation with an empty (or white) snapshot that user sees as a "flash".
    
    Fixed: 1420944
    Change-Id: I07d028fab780fda981b398651f06edafa74199e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303859
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113879}

--- ios/chrome/browser/snapshots/snapshot_cache.mm
+++ ios/chrome/browser/snapshots/snapshot_cache.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/time/time.h"
 #import "ios/chrome/browser/snapshots/snapshot_cache_observer.h"
 #import "ios/chrome/browser/snapshots/snapshot_lru_cache.h"
+#import "ios/chrome/browser/tabs/features.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ui/base/device_form_factor.h"
 
@@ -75,6 +76,18 @@ const CGFloat kJPEGImageQuality = 1.0;  // Highest quality. No compression.
 // Maximum size in number of elements that the LRU cache can hold before
 // starting to evict elements.
 const NSUInteger kLRUCacheMaxCapacity = 6;
+
+// Maximum size in number of elements that the LRU cache can hold before
+// starting to evict elements when PinnedTabs feature is enabled.
+//
+// To calculate the cache size number we'll start with the assumption that
+// currently snapshot preloading feature "works fine". In the reality it might
+// not be the case for large screen devices such as iPad. Another assumption
+// here is that pinned tabs feature requires on average 4 more snapshots to be
+// used. Based on that kLRUCacheMaxCapacityForPinnedTabsEnabled is
+// kLRUCacheMaxCapacity which "works fine" + on average 4 more snapshots needed
+// for pinned tabs feature.
+const NSUInteger kLRUCacheMaxCapacityForPinnedTabsEnabled = 10;
 
 // Returns the path of the image for `snapshot_id`, in `cache_directory`,
 // of type `image_type` and scale `image_scale`.
@@ -370,8 +383,10 @@ UIImage* GreyImageFromCachedImage(const base::FilePath& cache_directory,
 - (instancetype)initWithStoragePath:(const base::FilePath&)storagePath {
   DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
   if ((self = [super init])) {
-    _lruCache =
-        [[SnapshotLRUCache alloc] initWithCacheSize:kLRUCacheMaxCapacity];
+    NSUInteger cacheSize = IsPinnedTabsEnabled()
+                               ? kLRUCacheMaxCapacityForPinnedTabsEnabled
+                               : kLRUCacheMaxCapacity;
+    _lruCache = [[SnapshotLRUCache alloc] initWithCacheSize:cacheSize];
     _cacheDirectory = storagePath;
     _snapshotsScale = ImageScaleForDevice();
 
