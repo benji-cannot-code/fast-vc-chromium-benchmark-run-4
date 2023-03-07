f1445e455e08d4580c79d23fb9e36550bfa7f569
Kush Sinha <sinhak@chromium.org>
2023-03-07 04:32:55 +0000

    Signin: Add network delay status to chrome://signin-internals
    Add a field to chrome://signin-internals to indicate if network calls
    are being delayed.
    
    Please check the attached bug for context. In cases like this, it is
    difficult to pinpoint if the bug is in network service,
    AccountReconcilor, or somewhere else. It will be useful to add a field
    to chrome://signin-internals to indicate if network calls are being
    delayed by `SigninClient`. If this is the case, then the requests for
    minting cookies may not be even reaching Gaia.
    
    Bug: b/266402339
    Change-Id: Ib5e7c00d0b5e828d8265fb541a86aba52a254c81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307113
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113775}

--- ios/web_view/internal/signin/ios_web_view_signin_client.mm
+++ ios/web_view/internal/signin/ios_web_view_signin_client.mm
@@ -69,6 +69,10 @@ void IOSWebViewSigninClient::PreSignOut(
   std::move(on_signout_decision_reached).Run(SignoutDecision::ALLOW);
 }
 
+bool IOSWebViewSigninClient::AreNetworkCallsDelayed() {
+  return network_callback_helper_->AreNetworkCallsDelayed();
+}
+
 void IOSWebViewSigninClient::DelayNetworkCall(base::OnceClosure callback) {
   network_callback_helper_->HandleCallback(std::move(callback));
 }
