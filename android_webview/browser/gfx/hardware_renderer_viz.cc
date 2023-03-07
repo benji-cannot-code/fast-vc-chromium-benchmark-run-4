78748b3a8c0021bb32347eed2e4fd5cf4de3ac05
Colin Blundell <blundell@chromium.org>
2023-03-07 15:33:10 +0000

    [WebView] Make GL context current when drawing in HardwareRendererViz
    HardwareRendererViz has an implicit assumption that the GL context is
    current when drawing. With ANGLE, this is not necessarily the case,
    leading to crashes. This CL changes HardwareRendererViz to make the
    context current at the beginning of the draw. For non-ANGLE, this should
    be a no-op as the context was already made current by
    ScopedAppGLStateRestoreImpl in its constructor. Note that the context is
    already released at the end of the draw (for both ANGLE and non-ANGLE).
    
    To test:
    
    - Enable ANGLE (e.g., turn on the passthrough decoder in WebView's
      flags, or run "build/android/adb_system_webview_command_line
      --use-cmd-decoder=validating --use-gl=angle" if building locally)
    - Run WebView Shell's animation test
    - Tap on "rounded corners"
    - WebView should not crash (note: the square will disappear, which
      appears to be a separate issue that I am still working on)
    
    Bug: 1416197
    Change-Id: I8919d2339b0e264cb53708295160f8ad7af2ff64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315219
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113939}

--- android_webview/browser/gfx/hardware_renderer_viz.cc
+++ android_webview/browser/gfx/hardware_renderer_viz.cc
@@ -55,13 +55,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace android_webview {
 namespace {
 
-class ContextReleaser {
+class ScopedCurrentContext {
  public:
-  explicit ContextReleaser(gpu::SharedContextState* state) : state_(state) {}
-  ~ContextReleaser() { state_->ReleaseCurrent(nullptr); }
+  explicit ScopedCurrentContext(gpu::SharedContextState* state,
+                                gl::GLSurface* surface)
+      : state_(state), surface_(surface) {
+    state_->MakeCurrent(surface_);
+  }
+  ~ScopedCurrentContext() { state_->ReleaseCurrent(surface_); }
 
  private:
   const raw_ptr<gpu::SharedContextState> state_;
+  gl::GLSurface* surface_;
 };
 
 }  // namespace
@@ -409,11 +414,14 @@ void HardwareRendererViz::DrawAndSwap(const HardwareRendererDrawParams& params,
 
   DCHECK_CALLED_ON_VALID_THREAD(render_thread_checker_);
 
-  // Release the context before returning, it is required for the external ANGLE
-  // context. For non-ANGLE case, fake context and surface are used, so
-  // releasing current context should be very cheap.
-  ContextReleaser context_releaser(
-      output_surface_provider_.shared_context_state().get());
+  // Ensure that the context is current and that it is released before
+  // returning. When using ANGLE, the former is not guaranteed to be true and
+  // the latter is required for the external ANGLE context. For non-ANGLE case,
+  // fake context and surface are used, so releasing current context should be
+  // very cheap.
+  ScopedCurrentContext scoped_context(
+      output_surface_provider_.shared_context_state().get(),
+      output_surface_provider_.gl_surface().get());
 
   viz::FrameTimingDetailsMap timing_details;
 
