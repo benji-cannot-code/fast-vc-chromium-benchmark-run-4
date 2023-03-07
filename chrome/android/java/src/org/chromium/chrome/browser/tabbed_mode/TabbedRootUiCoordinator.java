fa5df0c1d0b9762375bcb3048b85ef97fc9e3ea3
Sophey <sophey@chromium.org>
2023-03-07 20:18:01 +0000

    [Crow] Remove IPH for Crow feature
    Bug: 1406011
    Change-Id: I4e96ca39a73a081c33f9ccc8908881af4a8ed41c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179191
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114133}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -85,8 +85,6 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.read_later.ReadLaterIPHController;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.chrome.browser.share.ShareDelegate;
-import org.chromium.chrome.browser.share.crow.CrowButtonDelegateImpl;
-import org.chromium.chrome.browser.share.crow.CrowIphController;
 import org.chromium.chrome.browser.share.link_to_text.LinkToTextIPHController;
 import org.chromium.chrome.browser.signin.SyncConsentActivityLauncherImpl;
 import org.chromium.chrome.browser.status_indicator.StatusIndicatorCoordinator;
@@ -156,7 +154,6 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
     private @Nullable ToolbarButtonInProductHelpController mToolbarButtonInProductHelpController;
     private AddToHomescreenIPHController mAddToHomescreenIPHController;
     private LinkToTextIPHController mLinkToTextIPHController;
-    private CrowIphController mCrowIphController;
     private AddToHomescreenMostVisitedTileClickObserver mAddToHomescreenMostVisitedTileObserver;
     private AppBannerInProductHelpController mAppBannerInProductHelpController;
     private PwaBottomSheetController mPwaBottomSheetController;
@@ -371,10 +368,6 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
 
         if (mAppBannerInProductHelpController != null) {
             AppBannerInProductHelpControllerFactory.detach(mAppBannerInProductHelpController);
-        }
-
-        if (mCrowIphController != null) {
-            mCrowIphController.destroy();
         }
 
         if (mPwaBottomSheetController != null) {
@@ -776,9 +769,6 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                         () -> mToolbarManager.getMenuButtonView(), R.id.add_to_homescreen_id);
         AppBannerInProductHelpControllerFactory.attach(
                 mWindowAndroid, mAppBannerInProductHelpController);
-        mCrowIphController = new CrowIphController(mActivity,
-                mAppMenuCoordinator.getAppMenuHandler(), new CrowButtonDelegateImpl(),
-                mActivityTabProvider, mToolbarManager.getMenuButtonView());
 
         if (!didTriggerPromo
                 && ChromeFeatureList.isEnabled(
--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -38,7 +38,6 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHChromeReengagementNotification3Feature,
     &kIPHContextualPageActionsQuietVariantFeature,
     &kIPHContextualPageActionsActionChipFeature,
-    &kIPHCrowFeature,
     &kIPHDownloadSettingsFeature,
     &kIPHDownloadInfoBarDownloadContinuingFeature,
     &kIPHDownloadInfoBarDownloadsAreFasterFeature,
