7f5a373bc3fa944ee42ebab6aef734550a7efeb8
Jack Hsieh <chengweih@chromium.org>
2023-03-07 04:00:43 +0000

    Reland "Reject Web Serial requests with an opaque origin"
    This reverts commit cd2f352906cbfa254100390271e51e9890ed2bbe.
    
    Reason for revert: The fixed parent CL https://chromium-review.googlesource.com/c/chromium/src/+/4307819 is re-landed.
    
    Original change's description:
    > Revert "Reject Web Serial requests with an opaque origin"
    >
    > Revert submission 4112689
    >
    > Reason for revert: suspect for introducing test failures for
    > DedicatedWorkerTest.TopLevelFrameSecurityOrigin, for example
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-ubsan-vptr/21391/overview
    >
    > Reverted changes: /q/submissionid:4112689
    >
    > Change-Id: I496ec21cc6a6e39dd3c2943b8fc7ce3d179a1f73
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307114
    > Owners-Override: Mikel Astiz <mastiz@google.com>
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1112768}
    
    Change-Id: Ic3f4fe58a946b53a23dfba57d30ee889ec3cc359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4311852
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113766}

---
+++ third_party/blink/web_tests/external/wpt/serial/requestPort/sandboxed_iframe.https.window.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+let iframe = document.createElement('iframe');
+
+promise_test(async () => {
+  await new Promise(resolve => {
+    iframe.src = '../resources/open-in-iframe.html';
+    iframe.sandbox.add('allow-scripts');
+    iframe.allow = 'serial';
+    document.body.appendChild(iframe);
+    iframe.addEventListener('load', resolve);
+  });
+
+  await new Promise(resolve => {
+    iframe.contentWindow.postMessage({type: 'RequestPort'}, '*');
+
+    window.addEventListener('message', (messageEvent) => {
+      // The failure message of no device chosen is expected. The point here is
+      // to validate not failing because of a sandboxed iframe.
+      assert_equals(
+          'FAIL: NotFoundError: Failed to execute \'requestPort\' on \'Serial\': No port selected by the user.',
+          messageEvent.data);
+      resolve();
+    });
+  });
+}, 'RequestPort from a sandboxed iframe is valid.');
