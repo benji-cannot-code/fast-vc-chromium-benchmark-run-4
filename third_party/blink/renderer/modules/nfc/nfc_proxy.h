290da62132458b7dfb11db403026496419516870
Paul Semel <paulsemel@chromium.org>
2023-03-07 10:49:30 +0000

    blink: triage mojo::Remote in NFCProxy
    Bug: 1381979
    Change-Id: I4f6eeb29e9e4beee93d0fadddb94754832fd66dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181117
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113857}

--- third_party/blink/renderer/modules/nfc/nfc_proxy.h
+++ third_party/blink/renderer/modules/nfc/nfc_proxy.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/gc_plugin.h"
@@ -81,8 +82,7 @@ class MODULES_EXPORT NFCProxy final : public GarbageCollected<NFCProxy>,
   using WriterSet = HeapHashSet<WeakMember<NDEFReader>>;
   WriterSet writers_;
 
-  GC_PLUGIN_IGNORE("https://crbug.com/1381979")
-  mojo::Remote<device::mojom::blink::NFC> nfc_remote_;
+  HeapMojoRemote<device::mojom::blink::NFC> nfc_remote_;
   HeapMojoReceiver<device::mojom::blink::NFCClient, NFCProxy> client_receiver_;
 };
 
