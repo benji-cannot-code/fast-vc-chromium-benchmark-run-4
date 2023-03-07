6ffc129bbbd5c15fb6c7cc6afda3fffff24051c8
CJ Huang <chenjih@google.com>
2023-03-07 07:05:02 +0000

    [Showoff] Integration persistence to the help app.
    New data will be saved to persistence and for each user login, the help
    app will try to load from persistence first.
    
    out/Default/ash_webui_unittests --gtest_filter='HelpAppSearchHandlerTest*'
    
    Bug: b/270091661
    Test: autoninja -C out/Default/ ash_webui_unittests &&
    Change-Id: I7d1647a5af325f13e8efc6136eabdbb5adb36aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297515
    Reviewed-by: Callistus Tan <callistus@google.com>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Auto-Submit: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113802}

--- ash/webui/help_app_ui/search/search_tag_registry.cc
+++ ash/webui/help_app_ui/search/search_tag_registry.cc
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "chromeos/ash/components/local_search_service/public/cpp/local_search_service_proxy.h"
 
-namespace ash {
-namespace help_app {
+namespace ash::help_app {
 namespace {
 
 // Converts from search concept to the format required by LSS for indexing.
@@ -79,6 +78,17 @@ void SearchTagRegistry::Update(
           .Then(std::move(callback)));
 }
 
+void SearchTagRegistry::ClearAndUpdate(
+    std::vector<mojom::SearchConceptPtr> search_tags,
+    base::OnceCallback<void()> callback) {
+  // Reset the metadata map and the index in the local search service before
+  // feeding the new data.
+  result_id_to_metadata_list_map_.clear();
+  index_remote_->ClearIndex(
+      base::BindOnce(&SearchTagRegistry::Update, weak_ptr_factory_.GetWeakPtr(),
+                     std::move(search_tags), std::move(callback)));
+}
+
 const SearchMetadata& SearchTagRegistry::GetTagMetadata(
     const std::string& result_id) const {
   const auto it = result_id_to_metadata_list_map_.find(result_id);
@@ -98,5 +108,4 @@ void SearchTagRegistry::NotifyRegistryAdded() {
   NotifyRegistryUpdated();
 }
 
-}  // namespace help_app
-}  // namespace ash
+}  // namespace ash::help_app
--- ash/webui/help_app_ui/search/search_tag_registry.h
+++ ash/webui/help_app_ui/search/search_tag_registry.h
@@ -19,8 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/local_search_service/public/mojom/index.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
-namespace ash {
-namespace help_app {
+namespace ash::help_app {
 
 // Processes all registered search tags by adding them to the LocalSearchService
 // index and providing the additional metadata needed for search results via
@@ -52,6 +51,11 @@ class SearchTagRegistry {
   void Update(const std::vector<mojom::SearchConceptPtr>& search_tags,
               base::OnceCallback<void()> callback);
 
+  // Clear the cached search concepts first. Then adds search concepts to the
+  // index. Callbacks when the LSS index is done updating.
+  void ClearAndUpdate(std::vector<mojom::SearchConceptPtr> search_tags,
+                      base::OnceCallback<void()> callback);
+
   // Returned by GetTagMetadata if the id was not found.
   static const SearchMetadata not_found_;
 
@@ -71,7 +75,6 @@ class SearchTagRegistry {
   base::WeakPtrFactory<SearchTagRegistry> weak_ptr_factory_{this};
 };
 
-}  // namespace help_app
-}  // namespace ash
+}  // namespace ash::help_app
 
 #endif  // ASH_WEBUI_HELP_APP_UI_SEARCH_SEARCH_TAG_REGISTRY_H_
