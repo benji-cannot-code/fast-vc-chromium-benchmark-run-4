4448d4ea34af3b21c6cc189c1a56699f8f441191
Philip Rogers <pdr@chromium.org>
2023-03-07 19:09:27 +0000

    Add a flag for SVG raster optimizations
    This patch adds SvgRasterOptimizations for the following optimizations:
    1) crbug.com/1404998: SVG sprite sheet culling
    2) crrev.com/1112415: Increased raster invalidator error tolerance
    
    The error tolerance change applies to non-SVG transforms as well, but
    we think it is most common with SVG due to the viewbox transform that
    has scale components. This patch also contains a change to
    fieldtrial_testing_config and a webview flag to set the experiment up
    for experimentation.
    
    Bug: 1404998
    Change-Id: Ie512812f884e7aeca2453b1ae2c73faa10772931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316282
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114088}

--- third_party/blink/renderer/core/svg/graphics/svg_image.cc
+++ third_party/blink/renderer/core/svg/graphics/svg_image.cc
@@ -534,7 +534,9 @@ absl::optional<PaintRecord> SVGImage::PaintRecordForCurrentFrame(
 
   view->UpdateAllLifecyclePhases(DocumentUpdateReason::kSVGImage);
 
-  return view->GetPaintRecord(cull_rect);
+  return view->GetPaintRecord(
+      RuntimeEnabledFeatures::SvgRasterOptimizationsEnabled() ? cull_rect
+                                                              : nullptr);
 }
 
 static bool DrawNeedsLayer(const cc::PaintFlags& flags) {
