85498ec5d18f5ae3ace738251ac9866019ef5f44
Caroline Rising <corising@chromium.org>
2023-03-08 18:13:07 +0000

    Make companion side panel contextual and registered for all tabs.
    Follow up work should prevent the companion page handler from updating
    when the tab is switched since there is now a separate view for each web
    contents.
    
    Bug: b:271322050
    Change-Id: I93346c8c029ae5c7251a9ec7fe4bd4a5c1ca6325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4319965
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114639}

--- chrome/browser/ui/views/side_panel/search_companion/search_companion_side_panel_coordinator.h
+++ chrome/browser/ui/views/side_panel/search_companion/search_companion_side_panel_coordinator.h
@@ -10,10 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/browser_user_data.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
 
 class Browser;
-class SidePanelRegistry;
 
 namespace views {
 class View;
@@ -22,7 +23,8 @@ class View;
 // SearchCompanionSidePanelCoordinator handles the creation and registration of
 // the search companion SidePanelEntry.
 class SearchCompanionSidePanelCoordinator
-    : public BrowserUserData<SearchCompanionSidePanelCoordinator> {
+    : public BrowserUserData<SearchCompanionSidePanelCoordinator>,
+      public TabStripModelObserver {
  public:
   explicit SearchCompanionSidePanelCoordinator(Browser* browser);
   SearchCompanionSidePanelCoordinator(
@@ -31,15 +33,24 @@ class SearchCompanionSidePanelCoordinator
       const SearchCompanionSidePanelCoordinator&) = delete;
   ~SearchCompanionSidePanelCoordinator() override;
 
-  void CreateAndRegisterEntry(SidePanelRegistry* global_registry);
+  void CreateAndRegisterEntriesForExistingWebContents(
+      TabStripModel* tab_strip_model);
 
   bool Show();
   BrowserView* GetBrowserView();
+
+  // TabStripModelObserver:
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
 
  private:
   raw_ptr<Browser> browser_;
 
   friend class BrowserUserData<SearchCompanionSidePanelCoordinator>;
+
+  std::unique_ptr<SidePanelEntry> CreateCompanionEntry();
 
   std::unique_ptr<views::View> CreateCompanionWebView();
 
