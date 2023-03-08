e99c042e93662fd126a353eddbae4dc442d6bd10
Zhaoyang Li <zhaoyangli@chromium.org>
2023-03-08 15:53:32 +0000

    Use a constant for multiprocess mode suffix at AwJUnit4ClassRunner
    Bug: 1215346
    Change-Id: I12217f2c3e80739cc4079368d8f2b99759717a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316127
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114552}

--- android_webview/test/shell/src/org/chromium/android_webview/test/AwJUnit4ClassRunner.java
+++ android_webview/test/shell/src/org/chromium/android_webview/test/AwJUnit4ClassRunner.java
@@ -30,6 +30,9 @@ import java.util.List;
  * which exercise code which cannot depend on single vs. multi process.
  */
 public final class AwJUnit4ClassRunner extends BaseJUnit4ClassRunner {
+    // This should match the definition in Android test runner scripts: bit.ly/3ynoREM
+    private static final String MULTIPROCESS_TEST_NAME_SUFFIX = "__multiprocess_mode";
+
     private final TestHook mWebViewMultiProcessHook = (targetContext, testMethod) -> {
         if (testMethod instanceof WebViewMultiProcessFrameworkMethod) {
             CommandLine.getInstance().appendSwitch(AwSwitches.WEBVIEW_SANDBOXED_RENDERER);
@@ -96,7 +99,7 @@ public final class AwJUnit4ClassRunner extends BaseJUnit4ClassRunner {
     /**
      * Custom FrameworkMethod class indicate this test method will run in multiprocess mode.
      *
-     * The clas also add "__multiprocess_mode" postfix to the test name.
+     * The class also adds MULTIPROCESS_TEST_NAME_SUFFIX postfix to the test name.
      */
     private static class WebViewMultiProcessFrameworkMethod extends FrameworkMethod {
         public WebViewMultiProcessFrameworkMethod(FrameworkMethod method) {
@@ -105,7 +108,7 @@ public final class AwJUnit4ClassRunner extends BaseJUnit4ClassRunner {
 
         @Override
         public String getName() {
-            return super.getName() + "__multiprocess_mode";
+            return super.getName() + MULTIPROCESS_TEST_NAME_SUFFIX;
         }
 
         @Override
