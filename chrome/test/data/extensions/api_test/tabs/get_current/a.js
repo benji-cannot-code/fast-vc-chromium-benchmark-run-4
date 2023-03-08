b2cc807a4dd0e62e9473dfbcf47548b0446f0586
David Bertoni <dbertoni@chromium.org>
2023-03-08 18:11:50 +0000

    [Extensions] Modernize the TabGetCurrent test.
    This CL modernizes this test by:
    
    1. Running the test from the extension's background script.
    2. Using chrome.runtime.sendMessage to send results from a window
    context to the extension's background script. This change makes the
    test compatible with a service worker-based extension.
    
    Bug: 1093066
    Change-Id: Ic3aaed2c654b6a36a9aa1a16a30f6d969b5a707b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318345
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114637}

--- chrome/test/data/extensions/api_test/tabs/get_current/a.js
+++ chrome/test/data/extensions/api_test/tabs/get_current/a.js
@@ -5,6 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 window.onload = function() {
   chrome.tabs.getCurrent(function(tab) {
-    chrome.extension.getBackgroundPage().onCompleteGetCurrentTab(tab);
+    chrome.runtime.sendMessage(tab);
   });
 };
