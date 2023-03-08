332fcffeb3f06056d1625a1b4123ad6bc9e3bd23
Peter Kasting <pkasting@chromium.org>
2023-03-08 22:50:09 +0000

    Use thread_local: base/win/
    Bug: 1416710
    Change-Id: I4c0716e32587597feb4cedd4b19ee0df89680938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4320826
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114817}

--- base/win/scoped_handle_verifier.h
+++ base/win/scoped_handle_verifier.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/hash.h"
 #include "base/memory/raw_ptr.h"
 #include "base/synchronization/lock_impl.h"
-#include "base/threading/thread_local.h"
 #include "base/win/windows_types.h"
 
 namespace base {
@@ -58,11 +57,9 @@ struct ScopedHandleVerifierInfo {
 // from emitting an unrecognized attribute warning.
 #pragma warning(push)
 #pragma warning(disable : 5030)
-class [[clang::lto_visibility_public]] ScopedHandleVerifier {
+class [[clang::lto_visibility_public, nodiscard]] ScopedHandleVerifier {
 #pragma warning(pop)
  public:
-  explicit ScopedHandleVerifier(bool enabled);
-
   ScopedHandleVerifier(const ScopedHandleVerifier&) = delete;
   ScopedHandleVerifier& operator=(const ScopedHandleVerifier&) = delete;
 
@@ -82,6 +79,7 @@ class [[clang::lto_visibility_public]] ScopedHandleVerifier {
   virtual HMODULE GetModule() const;
 
  private:
+  explicit ScopedHandleVerifier(bool enabled);
   ~ScopedHandleVerifier();  // Not implemented.
 
   void StartTrackingImpl(HANDLE handle, const void* owner, const void* pc1,
@@ -97,7 +95,6 @@ class [[clang::lto_visibility_public]] ScopedHandleVerifier {
 
   base::debug::StackTrace creation_stack_;
   bool enabled_;
-  base::ThreadLocalBoolean closing_;
   raw_ptr<base::internal::LockImpl> lock_;
   std::unordered_map<HANDLE, ScopedHandleVerifierInfo, HandleHash> map_;
 };
