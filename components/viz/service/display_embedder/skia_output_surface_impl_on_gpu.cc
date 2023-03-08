ad84cf28cea4c875ab9c546b30744bd7302b20fa
Vasiliy Telezhnikov <vasilyt@chromium.org>
2023-03-08 17:27:55 +0000

    Remove gl::Presenter::OnMakeCurrent
    No subclasses override it anymore. This is the last GL'y dependency of
    the presenter.
    
    Bug: 1380884
    Change-Id: Idbf5e365389d11ba76a05dae2cc4746fcf9292e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307949
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114603}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -1959,9 +1959,6 @@ bool SkiaOutputSurfaceImplOnGpu::MakeCurrent(bool need_framebuffer) {
     if (gl_surface_) {
       gl_surface_->OnMakeCurrent(context_state_->context());
     }
-    if (presenter_) {
-      presenter_->OnMakeCurrent(context_state_->context());
-    }
   }
 
   context_state_->set_need_context_state_reset(true);
--- ui/gl/presenter.h
+++ ui/gl/presenter.h
@@ -44,7 +44,6 @@ struct CARendererLayerParams;
 }  // namespace ui
 
 namespace gl {
-class GLContext;
 struct DCLayerOverlayParams;
 
 // OverlayImage is a platform specific type for overlay plane image data.
@@ -85,8 +84,6 @@ class GL_EXPORT Presenter : public base::RefCounted<Presenter> {
   virtual bool SupportsGpuVSync() const;
   virtual void SetGpuVSyncEnabled(bool enabled) {}
   virtual void SetVSyncDisplayID(int64_t display_id) {}
-
-  virtual bool OnMakeCurrent(GLContext* context);
 
   // Resizes the presenter, returning success.
   virtual bool Resize(const gfx::Size& size,
