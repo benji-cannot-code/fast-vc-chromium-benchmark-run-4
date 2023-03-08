c418eb9c980abecea0a973662800463c62977a0a
Judith Hemp <hempjudith@google.com>
2023-03-08 17:33:31 +0000

    Unit tests for tracing tabs in extensions with chrome.debugger API.
    This cl adds unit tests for basic tracing functionality using the event based approach.
    
    Bug: None
    
    Change-Id: Ie2ead42e012342beb2d2cc6b44c2802194451f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4190212
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114609}

--- chrome/browser/extensions/api/debugger/debugger_extension_apitest.cc
+++ chrome/browser/extensions/api/debugger/debugger_extension_apitest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/common/switches.h"
+#include "net/dns/mock_host_resolver.h"
 
 class ExtensionApiTestWithSwitch : public extensions::ExtensionApiTest {
  public:
@@ -16,8 +17,19 @@ class ExtensionApiTestWithSwitch : public extensions::ExtensionApiTest {
     command_line->AppendSwitch(switches::kSilentDebuggerExtensionAPI);
     command_line->AppendSwitch(extensions::switches::kExtensionsOnChromeURLs);
   }
+
+ protected:
+  void SetUpOnMainThread() override {
+    ExtensionApiTest::SetUpOnMainThread();
+    host_resolver()->AddRule("*", "127.0.0.1");
+    ASSERT_TRUE(StartEmbeddedTestServer());
+  }
 };
 
 IN_PROC_BROWSER_TEST_F(ExtensionApiTestWithSwitch, ExtensionDebugger) {
   ASSERT_TRUE(RunExtensionTest("debugger_extension")) << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTestWithSwitch, ExtensionTracing) {
+  ASSERT_TRUE(RunExtensionTest("tracing_extension")) << message_;
 }
