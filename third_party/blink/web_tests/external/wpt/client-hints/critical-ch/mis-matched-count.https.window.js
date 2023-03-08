33a5cdfd7cca64ad8168ed828af1bdfbeaeb05c7
Ari Chivukula <arichiv@chromium.org>
2023-03-08 14:55:23 +0000

    [Critical-CH] Ensure multiple headers are respected
    As per https://www.rfc-editor.org/rfc/rfc2068#section-4.2
    we need to be sure we treat:
    Critical-CH: A, B
    the same as:
    Critical-CH: A
    Critical-CH: B
    
    Bug: 1422562
    Change-Id: I5ae8b5e7be3f05899c564415bf8785affd6c322a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4319465
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114527}

--- third_party/blink/web_tests/external/wpt/client-hints/critical-ch/mis-matched-count.https.window.js
+++ third_party/blink/web_tests/external/wpt/client-hints/critical-ch/mis-matched-count.https.window.js
@@ -3,3 +3,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/common/utils.js
 
 async_test(make_message_test(ECHO_URL+"?mismatch=true&token="+token(), "1"), "Critical-CH no restart on mismatched hints")
+async_test(make_message_test(ECHO_URL+"?multiple=true&mismatch=true&token="+token(), "1"), "Critical-CH w/ multiple headers and no restart on mismatched hints")
