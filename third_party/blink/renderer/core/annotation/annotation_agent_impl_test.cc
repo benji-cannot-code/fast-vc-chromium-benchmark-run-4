03b55aa5ccb57607d4d0e0930b84c6c6b03395b8
David Bokan <bokan@chromium.org>
2023-03-08 22:11:21 +0000

    Handle IsCollapsed range in AnnotationAgentImpl
    This is a follow-up to the to-be-merged https://crrev.com/c/4304486.
    That CL had to be safe to merge so provided very minimal change. This CL
    fixes the issue in a more global way so that we never try to use a range
    that turns into a collapsed ephemeral range.
    
    Bug: 1419712
    Change-Id: I80bc5da7cb4a605f50ca1b3cc176cafeaa01ec7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304223
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114803}

--- third_party/blink/renderer/core/annotation/annotation_agent_impl_test.cc
+++ third_party/blink/renderer/core/annotation/annotation_agent_impl_test.cc
@@ -781,6 +781,10 @@ TEST_F(AnnotationAgentImplTest, ScrollIntoViewCollapsedRange) {
   host.BindToAgent(*agent);
   agent->Attach();
 
+  // Attachment should fail for this collapsed range.
+  EXPECT_FALSE(agent->IsAttached());
+  host.FlushForTesting();
+
   // Ensure calling ScrollIntoView doesn't crash.
   host.agent_->ScrollIntoView();
   host.FlushForTesting();
