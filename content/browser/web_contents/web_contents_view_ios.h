18b206fb396229001251cd1bbe2cf84004693273
Julie Jeongeun Kim <jkim@igalia.com>
2023-03-08 01:25:24 +0000

    [iOS] Add {Got,Lost}Focus in WebContentsViewIOS
    This CL implements GotFocus() and LostFocus() which are
    RenderViewHostDelegateView methods and enables
    BluetoothScanningPermissionRevokedWhenFocusIsLost test.
    
    Bug: 1420032
    Change-Id: I1e4cf652b5ed069c68ac9f1c8de45a939f9ef926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4308494
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114327}

--- content/browser/web_contents/web_contents_view_ios.h
+++ content/browser/web_contents/web_contents_view_ios.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 class RenderWidgetHostViewIOS;
+class RenderWidgetHostImpl;
 class WebContentsImpl;
 class WebContentsViewDelegate;
 class WebContentsUIViewHolder;
@@ -62,6 +63,10 @@ class WebContentsViewIOS : public WebContentsView,
   void OnCapturerCountChanged() override;
   void FullscreenStateChanged(bool is_fullscreen) override;
   void UpdateWindowControlsOverlay(const gfx::Rect& bounding_rect) override;
+
+  // RenderViewHostDelegateView:
+  void GotFocus(RenderWidgetHostImpl* render_widget_host) override;
+  void LostFocus(RenderWidgetHostImpl* render_widget_host) override;
 
   using RenderWidgetHostViewCreateFunction =
       RenderWidgetHostViewIOS* (*)(RenderWidgetHost*);
