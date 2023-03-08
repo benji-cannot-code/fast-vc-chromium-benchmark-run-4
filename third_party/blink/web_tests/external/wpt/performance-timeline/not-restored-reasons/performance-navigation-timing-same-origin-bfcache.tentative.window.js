fbeb32f1e0644d2f9d6d37e458b64bce241ccace
Yuzu Saijo <yuzus@chromium.org>
2023-03-08 06:05:13 +0000

    [bfcache] Update notRestoredReasons expected src, id and name to null
    This CL fixes the expectation to reflect changes to report null when
    attributes are not present. We failed to update the expecation because
    this test is marked as flaky and the failure did not show up.
    
    Change-Id: I08d442502d0122021d6b227e319de8a91f4aa740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318683
    Auto-Submit: Yuzu Saijo <yuzus@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114400}

--- third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-same-origin-bfcache.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-same-origin-bfcache.tentative.window.js
@@ -39,9 +39,9 @@ promise_test(async t => {
       rc1,
       /*blocked=*/ false,
       /*url=*/ rc1_url,
-      /*src=*/ '',
-      /*id=*/ '',
-      /*name=*/ '',
+      /*src=*/ null,
+      /*id=*/ null,
+      /*name=*/ null,
       /*reasons=*/[],
       /*children=*/[{
         'blocked': true,
