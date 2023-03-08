ac7dc611eac4341826f0840b9a359de5420d6ee2
Antonio Gomes <tonikitoo@igalia.com>
2023-03-08 20:07:01 +0000

    Remove CachingWordShaperTest::fallback_fonts class var
    It is not used anymore since [1].
    
    [1] https://codereview.chromium.org/2718043003
    
    BUG=NONE
    R=dtapuska@chromium.org
    
    Change-Id: I36add9d4395a17efaf922e05bc396c78799d5a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316684
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114725}

--- third_party/blink/renderer/platform/fonts/shaping/caching_word_shaper_test.cc
+++ third_party/blink/renderer/platform/fonts/shaping/caching_word_shaper_test.cc
@@ -26,7 +26,6 @@ class CachingWordShaperTest : public FontTestBase {
 
     font = Font(font_description);
     ASSERT_TRUE(font.CanShapeWordByWord());
-    fallback_fonts = nullptr;
     cache = std::make_unique<ShapeCache>();
   }
 
@@ -34,7 +33,6 @@ class CachingWordShaperTest : public FontTestBase {
   FontDescription font_description;
   Font font;
   std::unique_ptr<ShapeCache> cache;
-  HashSet<const SimpleFontData*>* fallback_fonts;
   unsigned start_index = 0;
   unsigned num_glyphs = 0;
   hb_script_t script = HB_SCRIPT_INVALID;
