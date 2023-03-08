b04888d08c6b2613cecab6dd880cd8c7af7a3950
Michele Fan <michelefan@chromium.org>
2023-03-08 18:52:36 +0000

    snap-group: Update comments and format files in split view
    Change-Id: I1984115c1d3878aec696b858e67abbb8ab4153f1
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317512
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114662}

--- ash/wm/desks/desks_unittests.cc
+++ ash/wm/desks/desks_unittests.cc
@@ -6804,7 +6804,7 @@ TEST_P(DesksTest, ReorderDesksInRTLMode) {
   base::i18n::SetRTLForTesting(default_rtl);
 }
 
-// Tests the behavior when drag a desk on the scroll button.
+// Tests the behavior when dragging a desk on the scroll button.
 TEST_P(DesksTest, ScrollBarByDraggedDesk) {
   // Make a flat long window to generate multiple pages on desks bar.
   UpdateDisplay("800x150");
