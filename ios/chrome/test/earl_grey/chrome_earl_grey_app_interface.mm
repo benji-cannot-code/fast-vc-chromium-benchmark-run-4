2481216119dfdf13369a838abb92f56e77f19631
Peter Kasting <pkasting@chromium.org>
2023-03-08 10:49:49 +0000

    Fix missing #include.
    Bug: 1416710
    Change-Id: I09620483499ae095a3ee18a8ff878756f40e335a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318444
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114463}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "base/test/scoped_feature_list.h"
+#import "base/threading/thread_restrictions.h"
 #import "base/values.h"
 #import "components/autofill/core/browser/personal_data_manager.h"
 #import "components/autofill/core/common/autofill_features.h"
