78aa416413d5f5ae49992575bc22b8b59d4d221f
Jason Thai <jasontt@chromium.org>
2023-03-08 19:49:40 +0000

    Reland "wallpaper: observe changes to schedule checkpoints"
    This is a reland of commit 4d25aa2b31eeb9c981e4133456d79075f641b599
    
    Also address flaky test.
    
    Original change's description:
    > wallpaper: observe changes to schedule checkpoints
    >
    > replace |OnColorModeChanged| with |OnCheckpointChanged| to observe
    > additional time of the day checkpoints.
    >
    > BUG=b/266612412
    > TEST=ash_unittests --gtest_filter="WallpaperControllerTest.*"
    > R=assistive-code-review@google.com
    >
    > Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    > Change-Id: I16fc36b2cafd7d1380b95ff8932099a346bd29ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4301809
    > Reviewed-by: Yuki Awano <yawano@google.com>
    > Commit-Queue: Jason Thai <jasontt@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1114113}
    
    Bug: b/266612412
    Change-Id: I0de4386c1a72bc78b142453ce4b670bdb3e87f2a
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318082
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114705}

--- ash/wallpaper/wallpaper_controller_unittest.cc
+++ ash/wallpaper/wallpaper_controller_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_pref_names.h"
 #include "ash/constants/ash_switches.h"
+#include "ash/public/cpp/schedule_enums.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/test/shell_test_api.h"
 #include "ash/public/cpp/test/test_image_downloader.h"
@@ -4131,8 +4132,11 @@ TEST_F(WallpaperControllerTest,
             controller_->GetDailyRefreshCollectionId(kAccountId1));
 }
 
-TEST_F(WallpaperControllerTest, UpdateWallpaperOnColorModeChanged) {
+TEST_F(WallpaperControllerTest, UpdateWallpaperOnScheduleCheckpointChanged) {
   SimulateUserLogin(kAccountId1);
+
+  // Enable dark mode by default.
+  Shell::Get()->dark_light_mode_controller()->SetDarkModeEnabledForTest(true);
 
   auto run_loop = std::make_unique<base::RunLoop>();
   ClearWallpaperCount();
@@ -4157,10 +4161,10 @@ TEST_F(WallpaperControllerTest, UpdateWallpaperOnColorModeChanged) {
   EXPECT_EQ(1, GetWallpaperCount());
   EXPECT_EQ(controller_->GetWallpaperType(), WallpaperType::kOnline);
 
-  // Change to light mode.
-  Shell::Get()->session_controller()->GetActivePrefService()->SetBoolean(
-      prefs::kDarkModeEnabled, false);
-  controller_->OnColorModeChanged(false);
+  // Switch to light mode and simulate schedule checkpoint change to reflect
+  // light mode.
+  EXPECT_TRUE(Shell::Get()->dark_light_mode_controller()->IsDarkModeEnabled());
+  Shell::Get()->dark_light_mode_controller()->ToggleColorMode();
   RunAllTasksUntilIdle();
   EXPECT_EQ(2, GetWallpaperCount());
   WallpaperInfo expected = WallpaperInfo(OnlineWallpaperParams(
