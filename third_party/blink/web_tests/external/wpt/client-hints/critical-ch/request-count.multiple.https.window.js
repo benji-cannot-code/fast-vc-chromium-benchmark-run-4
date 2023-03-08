e5942bf76de8b6dfb72f7f9a7e2e94b77a52e8c9
Ari Chivukula <arichiv@chromium.org>
2023-03-08 17:23:07 +0000

    [Critical-CH] Ensure multiple headers are respected {FIX}
    Some important new tests were omitted in
    https://chromium-review.googlesource.com/c/chromium/src/+/4319465
    due to an oversight.
    
    Bug: 1422562
    Change-Id: I6d5ca1d9f914098c3fa07ee8901e8074135da374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4319846
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114599}

---
+++ third_party/blink/web_tests/external/wpt/client-hints/critical-ch/request-count.multiple.https.window.js
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=resources/util.js
+// META: script=/common/utils.js
+
+async_test(make_message_test(ECHO_URL+"?multiple=true&token="+token(), "2"), "Critical-CH w/ multiple headers and navigation restart")
