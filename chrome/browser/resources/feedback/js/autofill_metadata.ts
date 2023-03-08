9815e4ed5daf476632300b4340d6b55b9c833c35
Norge Vizcay <vizcay@google.com>
2023-03-08 04:54:11 +0000

    [AutofillFeedback] Set up autofill feedback child page skeleton.
    The new autofill metadata child page will have a similar table-like UI to the system information page. To that end, this CL refactors strings that will be used on both pages and creates a shared .css file.
    
    The CL also creates the basic html for the autofill metadata page and introduces a js template file.
    
    Screenshots as of this CL:
    
    Linux:
    - System page: https://screenshot.googleplex.com/8fYi5eJ7L5rdKJG
    - Autofill page: https://screenshot.googleplex.com/7LhFjbPTUL3jtiv
    
    CrOS:
    - System page: https://screenshot.googleplex.com/4vrh3aDWuN4teWx
    - Autofill page: https://screenshot.googleplex.com/5AL3nSPwik98ZYe
    
    Bug: 1407646
    Change-Id: I67e76d763ffb838c59b504d23408a3bd88f17dc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291007
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114390}

---
+++ chrome/browser/resources/feedback/js/autofill_metadata.ts
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Builds the autofill metadata table.
+ */
+function createTable() {
+  // TODO(crbug.com/1407646): Create autofill metadata table.
+}
+
+/**
+ * Initializes the page when the window is loaded.
+ */
+window.onload = function() {
+  createTable();
+};
