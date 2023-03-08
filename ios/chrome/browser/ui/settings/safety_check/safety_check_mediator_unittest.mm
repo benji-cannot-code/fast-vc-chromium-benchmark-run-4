143baeda6d9476c60c219838bcb36ae7b22fd76e
Gauthier Ambard <gambard@chromium.org>
2023-03-08 11:52:35 +0000

    [iOS] Enable SF Symbols by default
    Bug: None
    Change-Id: Ic2a5f02fa28947d7dfefc6247c14c19c878c0694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316306
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114478}

--- ios/chrome/browser/ui/settings/safety_check/safety_check_mediator_unittest.mm
+++ ios/chrome/browser/ui/settings/safety_check/safety_check_mediator_unittest.mm
@@ -77,7 +77,7 @@ typedef NS_ENUM(NSInteger, SafetyCheckItemType) {
 };
 
 // The size of trailing symbol icons.
-NSInteger kTrailingSymbolImagePointSize = 22;
+NSInteger kTrailingSymbolImagePointSize = 18;
 
 using password_manager::InsecureCredential;
 using password_manager::InsecureType;
