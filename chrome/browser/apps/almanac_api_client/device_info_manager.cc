0f910f696a32395c7e68fd161398c1bfb847f47b
Tim Sergeant <tsergeant@chromium.org>
2023-03-08 22:54:45 +0000

    [APS] Fall back to UI language when there's no locale preference
    The APS server requires a language be set, but the profile language
    preference is only reliably set at the end of OOBE. As a fallback, if
    there's no preference available, we will fall back to the current UI
    language. This will normally be the same language, but could be
    different if OOBE is showing in a different language to synced data.
    
    Bug: b/267518768
    Change-Id: I2660150fba2a18271f67c1b91b37c48f6d37f4d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318686
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114822}

--- chrome/browser/apps/almanac_api_client/device_info_manager.cc
+++ chrome/browser/apps/almanac_api_client/device_info_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/apps/user_type_filter.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/channel_info.h"
 #include "chromeos/version/version_loader.h"
@@ -56,6 +57,11 @@ void DeviceInfoManager::GetDeviceInfo(
   PrefService* prefs = profile_->GetPrefs();
   DCHECK(prefs);
   device_info.locale = prefs->GetString(language::prefs::kApplicationLocale);
+  // If there's no stored locale preference, fall back to the current UI
+  // language.
+  if (device_info.locale.empty()) {
+    device_info.locale = g_browser_process->GetApplicationLocale();
+  }
 
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, {base::MayBlock()},
