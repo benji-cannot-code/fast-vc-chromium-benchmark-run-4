9d72c12f2d0de2bdb78b02438c984bb2cb3a9808
Zach Katz <katzz@google.com>
2023-03-08 18:31:35 +0000

    [PCCT-SideSheet] No top/side shadow at max height/width
    - Update side sheet elevations to have be lower
    - Remove shadowOffset multiplier on side sheet and 900dp bottom sheet
    
    Bug: 1421336
    Change-Id: I136dc5db0107ea7725e7399890052072ffc351af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310022
    Commit-Queue: Zach Katz <katzz@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114651}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBottomSheetStrategy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBottomSheetStrategy.java
@@ -89,7 +89,6 @@ public class PartialCustomTabBottomSheetStrategy extends PartialCustomTabBaseStr
     private final AnimatorListener mSpinnerFadeoutAnimatorListener;
     private final @Px int mUnclampedInitialHeight;
     private final boolean mIsFixedHeight;
-    private final int mSideShadowOffsetPx;
 
     private @Px int mFullyExpandedAdjustmentHeight;
     private TabAnimator mTabAnimator;
@@ -158,9 +157,6 @@ public class PartialCustomTabBottomSheetStrategy extends PartialCustomTabBaseStr
 
         mUnclampedInitialHeight = initialHeight;
         mIsFixedHeight = isFixedHeight;
-        mSideShadowOffsetPx =
-                mActivity.getResources().getDimensionPixelSize(R.dimen.custom_tabs_shadow_offset)
-                * SIDE_SHADOW_MULTIPLIER;
     }
 
     @Override
@@ -456,7 +452,7 @@ public class PartialCustomTabBottomSheetStrategy extends PartialCustomTabBaseStr
             }
         }
 
-        int sideMargin = isMaxWidthBottomSheet ? mSideShadowOffsetPx : 0;
+        int sideMargin = isMaxWidthBottomSheet ? shadowOffset : 0;
         if (handleView != null) {
             ViewGroup.MarginLayoutParams lp =
                     (ViewGroup.MarginLayoutParams) handleView.getLayoutParams();
@@ -476,7 +472,9 @@ public class PartialCustomTabBottomSheetStrategy extends PartialCustomTabBaseStr
 
     @Override
     protected boolean shouldHaveNoShadowOffset() {
-        return mStatus == HeightStatus.TOP;
+        return mStatus == HeightStatus.TOP
+                || (ChromeFeatureList.sCctResizableSideSheet.isEnabled()
+                        && mActivity.getWindow().getAttributes().y <= getFullyExpandedY());
     }
 
     @Override
