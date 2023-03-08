3778f89fa61ceea53cbc925e77d728a006d696d3
Lily Chen <chlily@chromium.org>
2023-03-08 19:41:12 +0000

    [DownloadBubble] Add a pref to record when the bubble is interacted with
    This adds a pref to record when the bubble is interacted with, for the
    purpose of suppressing the to-be-added in-product-help (IPH) bubble.
    
    We don't want to show the IPH for users who already know about the
    bubble and have been using it, so this pref records the fact that the
    user is using it. When the IPH is added, the code that records this pref
    will be removed, and the IPH system will take over keeping track of when
    the user has used the feature.
    
    Bug: 1420750
    Change-Id: I041df08565f082d378941ab3a9541dd7bd2d8010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4313881
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114698}

--- chrome/browser/download/bubble/download_bubble_controller.cc
+++ chrome/browser/download/bubble/download_bubble_controller.cc
@@ -153,6 +153,7 @@ void DownloadBubbleUIController::HideDownloadUi() {
 }
 
 void DownloadBubbleUIController::HandleButtonPressed() {
+  RecordDownloadBubbleInteraction();
   display_controller_->HandleButtonPressed();
 }
 
@@ -503,6 +504,7 @@ void DownloadBubbleUIController::ProcessDownloadButtonPress(
     DownloadUIModel* model,
     DownloadCommands::Command command,
     bool is_main_view) {
+  RecordDownloadBubbleInteraction();
   DownloadCommands commands(model->GetWeakPtr());
   base::UmaHistogramExactLinear("Download.Bubble.ProcessedCommand", command,
                                 DownloadCommands::MAX + 1);
@@ -594,4 +596,9 @@ void DownloadBubbleUIController::ScheduleCancelForEphemeralWarning(
       download_core_service->GetDownloadManagerDelegate();
   if (delegate)
     delegate->ScheduleCancelForEphemeralWarning(guid);
+}
+
+void DownloadBubbleUIController::RecordDownloadBubbleInteraction() {
+  download::SetShouldSuppressDownloadBubbleIph(
+      browser_->profile()->GetOriginalProfile(), true);
 }
