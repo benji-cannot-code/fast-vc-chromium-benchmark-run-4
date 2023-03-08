3cadd9d5c95bc79d5aac245a35064a07ab6316ca
Norge Vizcay <vizcay@google.com>
2023-03-08 06:29:39 +0000

    [AutofillFeedback] Refactor shared javascript code for info pages.
    The CL extracts from sys_info.ts the common java script code that will be shared by both info pages into an logs_map_page.ts  file.
    
    Both info pages will use the exported function `createLogsMapTable` to populate their corresponding tables.
    
    Bug: 1407646
    Change-Id: Ic6840faab3ca7365e554c6b636de53e727ab88dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4301376
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114404}

--- chrome/browser/resources/feedback/js/autofill_metadata.ts
+++ chrome/browser/resources/feedback/js/autofill_metadata.ts
@@ -2,17 +2,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import {createLogsMapTable} from './logs_map_page.js';
+
+const dialogArgs: string = chrome.getVariableValue('dialogArguments');
 
 /**
  * Builds the autofill metadata table.
  */
-function createTable() {
-  // TODO(crbug.com/1407646): Create autofill metadata table.
+function createAutofillMetadataTable() {
+  // TODO(crbug.com/1407646): Build autofill metadata table entries.
+  createLogsMapTable([]);
 }
 
 /**
  * Initializes the page when the window is loaded.
  */
 window.onload = function() {
-  createTable();
+  if (!dialogArgs) {
+    return;
+  }
+
+  createAutofillMetadataTable();
 };
