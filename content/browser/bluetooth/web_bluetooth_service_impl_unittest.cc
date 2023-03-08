7e34806a91f40c1f2d46f5c40e38444afd1691d6
Sina Firoozabadi <sinafirooz@google.com>
2023-03-08 02:29:36 +0000

    Drop Web Bluetooth mojoms from a renderer process of an opaque origin
    The Web Bluetooth API tracks permissions using the origin of the
    top-level document in the frame tree. If a document has an opaque origin
    then the requests to access Web Bluetooth get rejected on renderer side.
    
    To add a second layer of security in case of the renderer process being
    compromised, responding to the Web Bluetooth mojoms from such renderer
    process should be avoided.
    
    Disable-Rts: True
    Bug: 1375133
    Change-Id: Icbc6da4127e9c10f659d43e64cd5d7d1e207d02f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114125
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114352}

--- content/browser/bluetooth/web_bluetooth_service_impl_unittest.cc
+++ content/browser/bluetooth/web_bluetooth_service_impl_unittest.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/navigation_simulator.h"
+#include "content/public/test/test_browser_context.h"
+#include "content/public/test/test_web_contents_factory.h"
 #include "content/test/test_render_view_host.h"
 #include "content/test/test_web_contents.h"
 #include "device/bluetooth/public/cpp/bluetooth_uuid.h"
@@ -33,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_remote.h"
+#include "mojo/public/cpp/test_support/fake_message_dispatch_context.h"
+#include "mojo/public/cpp/test_support/test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -844,7 +848,7 @@ TEST_F(WebBluetoothServiceImplTest,
   // BluetoothRemoteGattCharacteristic::ValueCallback callback argument is that
   // when an error occurs, value must be ignored. This test verifies that
   // WebBluetoothServiceImpl::OnCharacteristicReadValue honors that contract
-  // and will not pass a value to it's callback
+  // and will not pass a value to its callback
   // (a RemoteCharacteristicReadValueCallback instance) when an error occurs
   // with a non-empty value array.
   const std::vector<uint8_t> read_error_value = {1, 2, 3};
@@ -1034,6 +1038,29 @@ TEST_F(WebBluetoothServiceImplTest, DeviceDisconnected) {
       get_primary_services_future.GetCallback());
   EXPECT_EQ(get_primary_services_future.Get<0>(),
             blink::mojom::WebBluetoothResult::NO_SERVICES_FOUND);
+}
+
+TEST_F(WebBluetoothServiceImplTest, RejectOpaqueOrigin) {
+  // Create a fake dispatch context to trigger a bad message in.
+  mojo::FakeMessageDispatchContext fake_dispatch_context;
+  mojo::test::BadMessageObserver bad_message_observer;
+
+  auto response_headers =
+      base::MakeRefCounted<net::HttpResponseHeaders>(std::string());
+  response_headers->SetHeader("Content-Security-Policy",
+                              "sandbox allow-scripts");
+  auto navigation_simulator = NavigationSimulator::CreateRendererInitiated(
+      GURL("http://whatever.com"), main_test_rfh());
+  navigation_simulator->SetResponseHeaders(response_headers);
+  navigation_simulator->Start();
+  navigation_simulator->Commit();
+
+  mojo::Remote<blink::mojom::WebBluetoothService> service;
+  contents()->GetPrimaryMainFrame()->CreateWebBluetoothServiceForTesting(
+      service.BindNewPipeAndPassReceiver());
+
+  EXPECT_EQ(bad_message_observer.WaitForBadMessage(),
+            "Web Bluetooth is not allowed from an opaque origin.");
 }
 
 }  // namespace content
