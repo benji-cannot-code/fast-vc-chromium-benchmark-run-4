5894c265fd86ab3d27fe42cfd86bf406a3bfda59
Peter Kasting <pkasting@chromium.org>
2023-03-08 23:02:52 +0000

    Add missing #include.
    Bug: 1416710
    Change-Id: I2f23d13cb07d779e203d3584896682dd44912191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4320825
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114834}

--- content/utility/services.cc
+++ content/utility/services.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/lazy_instance.h"
 #include "base/task/single_thread_task_runner.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
