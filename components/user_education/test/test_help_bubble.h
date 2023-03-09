f789f9501b52b2eb88cca26ae6aa30773298fb2f
Dana Fried <dfried@chromium.org>
2023-03-09 22:28:20 +0000

    [User Education] Tutorials with no bubbles will time out after 15s.
    Tutorials should always be showing one bubble at any given time. Failure
    to do this probably indicates that the Tutorial has gotten into some
    broken state where it can't show the next bubble, but the interaction
    sequence has not been broken due to a precondition violation.
    
    This CL detects when a Tutorial is in the "oops, no bubbles!" state and
    after a prescribed amount of time (currently 15 seconds), the tutorial
    is canceled. The user can always restart it from any of the usual
    places.
    
    In order to support delayed start (e.g., the user triggers the
    tutorial from the Internals page and then opens the surface or page
    that the tutorial actually starts on), the timeout before the initial
    bubble is shown is significantly longer (60 seconds).
    
    Tests have been added. In order to facilitate testing, TestHelpBubble
    will now correctly auto-dismiss when its anchor element goes away.
    
    Bug: 1422875
    Change-Id: Ie0c52873e83e87f4b90b578edb1afd3507628b98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4322476
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115384}

--- components/user_education/test/test_help_bubble.h
+++ components/user_education/test/test_help_bubble.h
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/auto_reset.h"
+#include "base/callback_list.h"
 #include "components/user_education/common/help_bubble.h"
 #include "components/user_education/common/help_bubble_factory.h"
 #include "components/user_education/common/help_bubble_params.h"
 #include "ui/base/interaction/element_identifier.h"
+#include "ui/base/interaction/element_tracker.h"
 
 namespace ui {
 class TrackedElement;
@@ -24,7 +26,7 @@ class TestHelpBubble : public HelpBubble {
  public:
   static constexpr int kNoButtonWithTextIndex = -1;
 
-  TestHelpBubble(ui::ElementContext context, HelpBubbleParams params);
+  TestHelpBubble(ui::TrackedElement* element, HelpBubbleParams params);
   ~TestHelpBubble() override;
 
   DECLARE_FRAMEWORK_SPECIFIC_METADATA()
@@ -55,7 +57,10 @@ class TestHelpBubble : public HelpBubble {
   ui::ElementContext GetContext() const override;
 
  private:
-  ui::ElementContext context_;
+  void OnElementHidden(ui::TrackedElement* element);
+
+  base::raw_ptr<ui::TrackedElement> element_;
+  base::CallbackListSubscription element_hidden_subscription_;
   HelpBubbleParams params_;
   int focus_count_ = 0;
 
