5ebdf091b41e6f9dc736b5bef01e5b3a80e7aba8
Roman Sorokin <rsorokin@chromium.org>
2023-03-09 09:28:43 +0000

    [Code Health] Replace RunLoop with TestFuture
    Replace the usage of `base::RunLoop` with `base::test::TestFuture` in
    account_apps_availability_unittest.cc
    
    Bug: b/266783937
    Test: unit_tests --gtest_filter="*AccountAppsAvailabilityTest*"
    Change-Id: I3218b9743f812001582d2315d747ed2cb2ce36a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317224
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115021}

--- chrome/browser/ash/account_manager/account_apps_availability_unittest.cc
+++ chrome/browser/ash/account_manager/account_apps_availability_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_future.h"
 #include "components/account_manager_core/account.h"
 #include "components/account_manager_core/account_manager_facade.h"
 #include "components/account_manager_core/mock_account_manager_facade.h"
@@ -64,16 +65,10 @@ class MockObserver : public AccountAppsAvailability::Observer {
 
 base::flat_set<account_manager::Account> GetAccountsAvailableInArcSync(
     AccountAppsAvailability* availability) {
-  base::flat_set<account_manager::Account> result;
-  base::RunLoop run_loop;
-  availability->GetAccountsAvailableInArc(base::BindLambdaForTesting(
-      [&result,
-       &run_loop](const base::flat_set<account_manager::Account>& accounts) {
-        result = accounts;
-        run_loop.Quit();
-      }));
-  run_loop.Run();
-  return result;
+  base::test::TestFuture<const base::flat_set<account_manager::Account>&>
+      future;
+  availability->GetAccountsAvailableInArc(future.GetCallback());
+  return future.Get();
 }
 
 MATCHER_P(AccountEqual, other, "") {
@@ -83,11 +78,13 @@ MATCHER_P(AccountEqual, other, "") {
 }  // namespace
 
 class AccountAppsAvailabilityTest : public testing::Test {
- protected:
-  AccountAppsAvailabilityTest() = default;
+ public:
   AccountAppsAvailabilityTest(const AccountAppsAvailabilityTest&) = delete;
   AccountAppsAvailabilityTest& operator=(const AccountAppsAvailabilityTest&) =
       delete;
+
+ protected:
+  AccountAppsAvailabilityTest() = default;
   ~AccountAppsAvailabilityTest() override = default;
 
   void SetUp() override {
@@ -181,19 +178,13 @@ TEST_F(AccountAppsAvailabilityTest, CallsBeforeInitialization) {
   // Make secondary account unavailable in ARC.
   account_apps_availability->SetIsAccountAvailableInArc(secondary_account,
                                                         false);
-  base::flat_set<account_manager::Account> result;
-  base::RunLoop run_loop;
-  account_apps_availability->GetAccountsAvailableInArc(
-      base::BindLambdaForTesting(
-          [&result, &run_loop](
-              const base::flat_set<account_manager::Account>& accounts) {
-            result = accounts;
-            run_loop.Quit();
-          }));
 
+  base::test::TestFuture<const base::flat_set<account_manager::Account>&>
+      future;
   // Wait for initialization and for `GetAccountsAvailableInArc` call
   // completion.
-  run_loop.Run();
+  account_apps_availability->GetAccountsAvailableInArc(future.GetCallback());
+  base::flat_set<account_manager::Account> result = future.Get();
   EXPECT_TRUE(account_apps_availability->IsInitialized());
 
   // Only primary account is available, secondary account was removed.
