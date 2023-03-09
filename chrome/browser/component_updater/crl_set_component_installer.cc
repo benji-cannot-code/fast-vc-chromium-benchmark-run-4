dc4683688ee955d62afb61d2b30fae8b6d6a7893
Peter Kasting <pkasting@chromium.org>
2023-03-09 20:38:59 +0000

    Use thread_local: chrome/browser/
    Most of the delta here is from moving things from class scope to
    namespace scope so I can declare a namespace-scope thread_local. The
    actual code diff is just the stuff that touches `test_instance`
    (previously `test_instance_`).
    
    Bug: 1416710
    Change-Id: I664c30c9b4395073fc5eaaf3c79bf2c664f98196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318446
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115314}

--- chrome/browser/component_updater/crl_set_component_installer.cc
+++ chrome/browser/component_updater/crl_set_component_installer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
+#include "base/lazy_instance.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/task/thread_pool.h"
