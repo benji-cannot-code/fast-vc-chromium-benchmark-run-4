8c93b4964fcf86ca100920b137c416ba206c204f
Susanne Westphal <swestphal@google.com>
2023-03-09 11:52:45 +0000

    [WebEngine] Re-enable Permissions for WebEngine
    Weblayer was providing a wrapped activity context to the PermissionDialogController this was updated for WebEngine, which provides a wrapped service context. This CL updates the check for the provided service to accommodate for this change.
    This CL also fixes the Context used to inflate the modal dialogs.
    
    Bug: 1417144
    Change-Id: I48c2272d0949661792e70e42b156eb6f6b49542b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4264254
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115066}

--- weblayer/browser/java/org/chromium/weblayer_private/BrowserViewController.java
+++ weblayer/browser/java/org/chromium/weblayer_private/BrowserViewController.java
@@ -49,6 +49,9 @@ public final class BrowserViewController
         implements WebContentsGestureStateTracker.OnGestureStateChangedListener,
                    ModalDialogManager.ModalDialogManagerObserver {
     private final ContentViewRenderView mContentViewRenderView;
+
+    private final Context mWeblayerContext;
+
     // Child of mContentViewRenderView. Be very careful adding Views to this, as any Views are not
     // accessible (ContentView provides it's own accessible implementation that interacts with
     // WebContents).
@@ -79,6 +82,9 @@ public final class BrowserViewController
     public BrowserViewController(Context embedderContext, FragmentWindowAndroid windowAndroid,
             boolean recreateForConfigurationChange) {
         mWindowAndroid = windowAndroid;
+
+        mWeblayerContext = windowAndroid.getContext().get();
+
         mContentViewRenderView =
                 new ContentViewRenderView(embedderContext, recreateForConfigurationChange);
 
@@ -94,16 +100,18 @@ public final class BrowserViewController
                 new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
         mWebContentsOverlayView = new FrameLayout(embedderContext);
-        RelativeLayout.LayoutParams overlayParams =
-                new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0);
+        RelativeLayout.LayoutParams overlayParams = new RelativeLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         mContentViewRenderView.addView(mWebContentsOverlayView, overlayParams);
         mWindowAndroid.setAnimationPlaceholderView(mWebContentsOverlayView);
 
         mModalDialogManager = new ModalDialogManager(
-                new AppModalPresenter(embedderContext), ModalDialogManager.ModalDialogType.APP);
+                new AppModalPresenter(mWeblayerContext), ModalDialogManager.ModalDialogType.APP);
+
         mModalDialogManager.addObserver(this);
         mModalDialogManager.registerPresenter(
-                new WebLayerTabModalPresenter(this, embedderContext), ModalDialogType.TAB);
+                new WebLayerTabModalPresenter(this, mWeblayerContext), ModalDialogType.TAB);
+
         mWindowAndroid.setModalDialogManager(mModalDialogManager);
 
         SystemUiScrimDelegate systemUiDelegate = new SystemUiScrimDelegate() {
@@ -116,9 +124,8 @@ public final class BrowserViewController
             public void setNavigationBarScrimFraction(float scrimFraction) {}
         };
         mScrim = new ScrimCoordinator(embedderContext, systemUiDelegate, mContentViewRenderView,
-                mWindowAndroid.getContext().get().getResources().getColor(
-                        R.color.default_scrim_color));
-        mBottomSheetContainer = new FrameLayout(mWindowAndroid.getContext().get());
+                mWeblayerContext.getResources().getColor(R.color.default_scrim_color));
+        mBottomSheetContainer = new FrameLayout(mWeblayerContext);
         mBottomSheetContainer.setLayoutParams(
                 new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         mBottomSheetContainer.setClipChildren(false);
@@ -146,8 +153,8 @@ public final class BrowserViewController
                 KeyboardVisibilityDelegate.getInstance(), () -> mBottomSheetContainer);
         BottomSheetControllerFactory.attach(mWindowAndroid, mBottomSheetController);
 
-        mPwaBottomSheetController = PwaBottomSheetControllerFactory.createPwaBottomSheetController(
-                mWindowAndroid.getContext().get());
+        mPwaBottomSheetController =
+                PwaBottomSheetControllerFactory.createPwaBottomSheetController(mWeblayerContext);
         PwaBottomSheetControllerFactory.attach(mWindowAndroid, mPwaBottomSheetController);
         mBottomSheetObserver = new EmptyBottomSheetObserver() {
             /** A token for suppressing app modal dialogs. */
@@ -268,7 +275,7 @@ public final class BrowserViewController
 
         if (mOnscreenContentProvider == null) {
             mOnscreenContentProvider = new OnscreenContentProvider(
-                    mWindowAndroid.getContext().get(), mContentViewRenderView, webContents);
+                    mWeblayerContext, mContentViewRenderView, webContents);
         } else {
             mOnscreenContentProvider.onWebContentsChanged(webContents);
         }
@@ -351,7 +358,7 @@ public final class BrowserViewController
                     }
                 });
 
-        Resources resources = mWindowAndroid.getContext().get().getResources();
+        Resources resources = mWeblayerContext.getResources();
         PropertyModel dialogModel =
                 new PropertyModel.Builder(ModalDialogProperties.ALL_KEYS)
                         .with(ModalDialogProperties.CONTROLLER, dialogController)
