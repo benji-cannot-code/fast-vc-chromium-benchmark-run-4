11ec3af32248181a2c9931bc6c90d6cd3ca112c2
Joshua Hood <jdh@chromium.org>
2023-03-09 16:44:06 +0000

    [DIPS] Throttle site storage timestamp updates
    To reduce the number of disk writes associated with cookie accesses,
    only record new storage events for a site to the DIPS db once per
    kTimestampUpdateInterval per pageload (where that site is in the
    primary main frame).
    
    Bug: 1416302
    Change-Id: Ifb874013797e8e7fa9e4c92fc23b645bc6b951a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300849
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115173}

--- chrome/browser/dips/dips_bounce_detector.h
+++ chrome/browser/dips/dips_bounce_detector.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/types/optional_ref.h"
 #include "chrome/browser/dips/cookie_access_filter.h"
 #include "chrome/browser/dips/dips_redirect_info.h"
 #include "chrome/browser/dips/dips_service.h"
@@ -45,6 +46,7 @@ class ClientBounceDetectionState {
   std::string current_site;
   base::TimeTicks page_load_time;
   absl::optional<base::Time> last_activation_time;
+  absl::optional<base::Time> last_storage_time;
   CookieAccessType cookie_access_type = CookieAccessType::kUnknown;
 };
 
@@ -161,7 +163,7 @@ class DIPSBounceDetector {
   // The amount of time since a page last received user interaction before a
   // subsequent user interaction event may be recorded to DIPS Storage for the
   // same page.
-  static const base::TimeDelta kInteractionUpdateInterval;
+  static const base::TimeDelta kTimestampUpdateInterval;
 
   explicit DIPSBounceDetector(DIPSBounceDetectorDelegate* delegate,
                               const base::TickClock* tick_clock,
@@ -179,7 +181,7 @@ class DIPSBounceDetector {
                                CookieOperation op);
   void DidFinishNavigation(DIPSNavigationHandle* navigation_handle);
   // Only records a new user activation event once per
-  // |kInteractionUpdateInterval| for a given page.
+  // |kTimestampUpdateInterval| for a given page.
   void OnUserActivation();
   void BeforeDestruction();
 
@@ -190,6 +192,12 @@ class DIPSBounceDetector {
   }
 
  private:
+  // Whether or not the `last_time` timestamp should be updated yet. This is
+  // used to enforce throttling of timestamp updates, reducing the number of
+  // writes to the DIPS db.
+  bool ShouldUpdateTimestamp(base::optional_ref<const base::Time> last_time,
+                             base::Time now);
+
   raw_ptr<const base::TickClock> tick_clock_;
   raw_ptr<const base::Clock> clock_;
   raw_ptr<DIPSBounceDetectorDelegate> delegate_;
