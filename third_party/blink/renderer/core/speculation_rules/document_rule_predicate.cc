1d59ec7ea3da147ac76a50eb9c7d55044500ca28
Jeremy Roman <jbroman@chromium.org>
2023-03-09 22:09:39 +0000

    Update feature control logic for SpeculationRulesPrefetchFuture experiment update.
    Bug: 1422746
    Change-Id: If0dde7f660cad84ff0db71a9b7314f94f1daff27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318244
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Iman Saboori <isaboori@google.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115372}

--- third_party/blink/renderer/core/speculation_rules/document_rule_predicate.cc
+++ third_party/blink/renderer/core/speculation_rules/document_rule_predicate.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/html/html_anchor_element.h"
+#include "third_party/blink/renderer/core/speculation_rules/speculation_rules_features.h"
 #include "third_party/blink/renderer/core/url_pattern/url_pattern.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
@@ -570,8 +571,8 @@ DocumentRulePredicate* DocumentRulePredicate::Parse(
 
   // If predicateType is "selector_matches"
   if (predicate_type == "selector_matches" && input->size() == 1) {
-    const bool selector_matches_enabled = RuntimeEnabledFeatures::
-        SpeculationRulesDocumentRulesSelectorMatchesEnabled(execution_context);
+    const bool selector_matches_enabled =
+        speculation_rules::SelectorMatchesEnabled(execution_context);
     if (!selector_matches_enabled) {
       SetParseErrorMessage(out_error,
                            "\"selector_matches\" is currently unsupported.");
--- third_party/blink/renderer/core/speculation_rules/speculation_rule_set.cc
+++ third_party/blink/renderer/core/speculation_rules/speculation_rule_set.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/inspector/identifiers_factory.h"
 #include "third_party/blink/renderer/core/speculation_rules/document_rule_predicate.h"
+#include "third_party/blink/renderer/core/speculation_rules/speculation_rules_features.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/json/json_parser.h"
 #include "third_party/blink/renderer/platform/json/json_values.h"
@@ -77,7 +78,7 @@ SpeculationRule* ParseSpeculationRule(JSONObject* input,
       "referrer_policy", "relative_to"};
   const auto kConditionalKnownKeys = [context]() {
     Vector<const char*, 4> conditional_known_keys;
-    if (RuntimeEnabledFeatures::SpeculationRulesEagernessEnabled(context)) {
+    if (speculation_rules::EagernessEnabled(context)) {
       conditional_known_keys.push_back("eagerness");
     }
     if (RuntimeEnabledFeatures::SpeculationRulesNoVarySearchHintEnabled(
@@ -304,7 +305,7 @@ SpeculationRule* ParseSpeculationRule(JSONObject* input,
   absl::optional<mojom::blink::SpeculationEagerness> eagerness;
   if (JSONValue* eagerness_value = input->Get("eagerness")) {
     // Feature gated due to known keys check above.
-    DCHECK(RuntimeEnabledFeatures::SpeculationRulesEagernessEnabled(context));
+    DCHECK(speculation_rules::EagernessEnabled(context));
 
     String eagerness_str;
     if (!eagerness_value->AsString(&eagerness_str)) {
