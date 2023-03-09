2f8441fc029de906b5e7ba6f5b0aaf26501c6fe3
Joe DeBlasio <jdeblasio@chromium.org>
2023-03-09 18:38:54 +0000

    Fix a bug in HTTPS request metrics
    This CL lets us disambiguate between "real" HTTP requests and those
    upgraded by HTTP Upgrading / HFM logic.
    
    Bug: 1422410
    Change-Id: I1efc3ee5ce209d73caabb3a05a5d9c5d0a19844d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321679
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115240}

--- chrome/browser/ssl/https_upgrades_interceptor.cc
+++ chrome/browser/ssl/https_upgrades_interceptor.cc
@@ -197,6 +197,14 @@ void HttpsUpgradesInterceptor::MaybeCreateLoader(
     return;
   }
 
+  // Exclude all other schemes other than HTTP.
+  if (!tentative_resource_request.url.SchemeIs(url::kHttpScheme)) {
+    RecordNavigationRequestSecurityLevel(
+        NavigationRequestSecurityLevel::kOtherScheme);
+    std::move(callback).Run({});
+    return;
+  }
+
   // Exclude "localhost" (and loopback addresses) as they do not expose traffic
   // over the network.
   // TODO(crbug.com/1394910): Extend the exemption list for HTTPS-Upgrades
@@ -247,13 +255,11 @@ void HttpsUpgradesInterceptor::MaybeCreateLoaderOnHstsQueryCompleted(
     return;
   }
 
-  RecordNavigationRequestSecurityLevel(
-      NavigationRequestSecurityLevel::kInsecure);
-
-  // Only serve upgrade redirects for main frame, GET requests to HTTP URLs.
+  // Only serve upgrade redirects for main frame, GET requests.
   if (!tentative_resource_request.is_outermost_main_frame ||
-      tentative_resource_request.method != "GET" ||
-      !tentative_resource_request.url.SchemeIs(url::kHttpScheme)) {
+      tentative_resource_request.method != "GET") {
+    RecordNavigationRequestSecurityLevel(
+        NavigationRequestSecurityLevel::kInsecure);
     std::move(callback).Run({});
     return;
   }
@@ -263,6 +269,8 @@ void HttpsUpgradesInterceptor::MaybeCreateLoaderOnHstsQueryCompleted(
   PrefService* prefs = profile->GetPrefs();
   if (IsHostnameInAllowlist(tentative_resource_request.url,
                             prefs->GetList(prefs::kHttpAllowlist))) {
+    RecordNavigationRequestSecurityLevel(
+        NavigationRequestSecurityLevel::kInsecure);
     std::move(callback).Run({});
     return;
   }
@@ -289,6 +297,8 @@ void HttpsUpgradesInterceptor::MaybeCreateLoaderOnHstsQueryCompleted(
     state->AllowHttpForHost(tentative_resource_request.url.host(),
                             storage_partition);
 
+    RecordNavigationRequestSecurityLevel(
+        NavigationRequestSecurityLevel::kInsecure);
     std::move(callback).Run({});
     return;
   }
@@ -313,6 +323,8 @@ void HttpsUpgradesInterceptor::MaybeCreateLoaderOnHstsQueryCompleted(
   auto* entry = web_contents->GetController().GetPendingEntry();
   if (entry && entry->GetTransitionType() & ui::PAGE_TRANSITION_FORWARD_BACK &&
       tab_helper->has_failed_upgrade(tentative_resource_request.url)) {
+    RecordNavigationRequestSecurityLevel(
+        NavigationRequestSecurityLevel::kInsecure);
     std::move(callback).Run({});
     return;
   }
@@ -327,9 +339,14 @@ void HttpsUpgradesInterceptor::MaybeCreateLoaderOnHstsQueryCompleted(
     // Don't upgrade the request and let the default loader continue, but record
     // that the request *would have* upgraded, had upgrading been enabled.
     RecordHttpsFirstModeNavigation(Event::kUpgradeNotAttempted);
+    RecordNavigationRequestSecurityLevel(
+        NavigationRequestSecurityLevel::kInsecure);
     std::move(callback).Run({});
     return;
   }
+
+  RecordNavigationRequestSecurityLevel(
+      NavigationRequestSecurityLevel::kUpgraded);
 
   // Mark navigation as upgraded.
   tab_helper->set_is_navigation_upgraded(true);
