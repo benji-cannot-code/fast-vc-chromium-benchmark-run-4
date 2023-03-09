7463d33ea0a7e36a64a2a86831331267999452aa
Vladimir Levin <vmpstr@chromium.org>
2023-03-09 23:37:49 +0000

    CV: Ensure canvas doesn't paint replaced element (and can be locked)
    This patch ensures two things happen:
    - We allow default canvas to be c-v locked, since it's a replaced
      element
    - We ensure that if locked, it doesn't paint its replaced contents.
    
    R=chrishtr@chromium.org, fserb@chromium.org
    
    Bug: 1315275
    Change-Id: Icdb590f1bf96a6ff03accd9a842cf3480fadb720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4324580
    Auto-Submit: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115427}

--- third_party/blink/renderer/core/display_lock/display_lock_context.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context.cc
@@ -1045,6 +1045,7 @@ const char* DisplayLockContext::ShouldForceUnlock() const {
     if (!object_element->UseFallbackContent())
       return nullptr;
   } else if (IsA<HTMLImageElement>(*element_) ||
+             IsA<HTMLCanvasElement>(*element_) ||
              (element_->IsFormControlElement() &&
               !element_->IsOutputElement()) ||
              element_->IsMediaElement() || element_->IsFrameOwnerElement() ||
