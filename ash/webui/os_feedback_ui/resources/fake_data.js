44603f167c964ff686e1b2c00437ed47789180d6
Markus Heintz <markusheintz@chromium.org>
2023-03-09 14:53:25 +0000

    Revert "[AutofillFeedback] CrOS: Add autofill metadata link to UI."
    This reverts commit 7df9b959e092a178b4dc78f25b787b6e5be3bae6.
    
    Reason for revert: Speculatively reverting because it may cause OSFeedbackBrowserTest.All to fail on CROS
    
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome
    https://chromium-review.googlesource.com/c/chromium/src/+/4311942
    
    Original change's description:
    > [AutofillFeedback] CrOS: Add autofill metadata link to UI.
    >
    > The CL includes a new checkbox on the share_data_page, where users can decide whether or not to share autofill metadata related to the current page. The checkbox string also offers a link that will open a new dialog displaying the corresponding the autofill metadata.
    >
    > The option is only offered to internal users that opened the feedback dialog through the corresponding context menu item.
    >
    > https://screenshot.googleplex.com/4sXMrY5q32einhJ.png
    >
    > Bug: 1407646
    > Change-Id: I1306c42f9489cb2f5fa427f13f0c7ef8254c014e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4311942
    > Commit-Queue: Norge Vizcay <vizcay@google.com>
    > Reviewed-by: Camille Lamy <clamy@chromium.org>
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1115024}
    
    Bug: 1407646
    Change-Id: I911f7aece296648a1d4d40c7639e6a8791ca9973
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4324040
    Auto-Submit: Markus Heintz <markusheintz@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115120}

--- ash/webui/os_feedback_ui/resources/fake_data.js
+++ ash/webui/os_feedback_ui/resources/fake_data.js
@@ -85,8 +85,6 @@ export const fakeFeedbackContext = {
   fromAssistant: false,
   assistantDebugInfoAllowed: false,
   fromSettingsSearch: false,
-  fromAutofill: false,
-  autofillMetadata: '',
   traceId: 1,
 };
 
@@ -98,8 +96,6 @@ export const fakeEmptyFeedbackContext = {
   fromAssistant: false,
   assistantDebugInfoAllowed: false,
   fromSettingsSearch: false,
-  fromAutofill: false,
-  autofillMetadata: '',
   traceId: 0,
 };
 
@@ -111,8 +107,6 @@ export const fakeInternalUserFeedbackContext = {
   fromAssistant: true,
   assistantDebugInfoAllowed: false,
   fromSettingsSearch: true,
-  fromAutofill: false,
-  autofillMetadata: '',
   traceId: 1,
 };
 
