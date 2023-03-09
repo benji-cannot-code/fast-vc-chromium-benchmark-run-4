b18e33db06a09cde18928ee9fae2c7c886a2a562
Hao Liu <haoliuk@chromium.org>
2023-03-09 13:28:24 +0000

    Fix wrong name in TaskAttributionTiming constructor arguments
    I think the argument name "type" should be "name" in the
    TaskAttributionTiming constructor. This is to change it to save a second
    look.
    
    I came across this when looking into a flaky long task wpt.
    
    Change-Id: I7940c022e12b7a4d01f32ea16c20f4237f2806a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321567
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115083}

--- third_party/blink/renderer/core/timing/task_attribution_timing.h
+++ third_party/blink/renderer/core/timing/task_attribution_timing.h
@@ -27,7 +27,7 @@ class TaskAttributionTiming final : public PerformanceEntry {
 
   void Trace(Visitor*) const override;
 
-  TaskAttributionTiming(const AtomicString& type,
+  TaskAttributionTiming(const AtomicString& name,
                         const AtomicString& container_type,
                         const AtomicString& container_src,
                         const AtomicString& container_id,
