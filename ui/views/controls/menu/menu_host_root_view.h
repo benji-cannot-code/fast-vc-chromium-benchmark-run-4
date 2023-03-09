68f9b9c8b409d1d7838b007652c467cbe5a162fd
Yichen <yichenz@chromium.org>
2023-03-09 23:33:03 +0000

    Cleanup: remove unused parameters in OnEventProcessingFinished
    Remove ui::EventTarget* and const ui::EventDispatchDetails&
    parameters because they are not used in WindowEventDispatcher::
    OnEventProcessingFinished anymore.
    
    BUG=b:271333725
    
    Change-Id: Ief358f909a0dd599c0565241e62a8e41f561a8d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321541
    Quick-Run: Yichen Zhou <yichenz@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yichen Zhou <yichenz@chromium.org>
    Auto-Submit: Yichen Zhou <yichenz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115420}

--- ui/views/controls/menu/menu_host_root_view.h
+++ ui/views/controls/menu/menu_host_root_view.h
@@ -49,10 +49,7 @@ class MenuHostRootView : public internal::RootView {
 
  private:
   // ui::EventProcessor:
-  void OnEventProcessingFinished(
-      ui::Event* event,
-      ui::EventTarget* target,
-      const ui::EventDispatchDetails& details) override;
+  void OnEventProcessingFinished(ui::Event* event) override;
 
   // Returns the MenuController for this MenuHostRootView.
   MenuController* GetMenuController();
