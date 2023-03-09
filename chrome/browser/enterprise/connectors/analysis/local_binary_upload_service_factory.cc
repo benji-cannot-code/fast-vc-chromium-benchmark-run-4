a7273858ef083bebcdd4e5749b8793e4ccf81f3c
Roger Tawa <rogerta@chromium.org>
2023-03-09 14:35:31 +0000

    [Reland] Do agent binary verification in LocalBinaryUploadService.
    See design doc: https://docs.google.com/document/d/1R8BNPdKxm7d6Y5iGiwE2ua9-JPa_2yjD4dhc0Ksodbo/edit#heading=h.5iuaydh7sh47
    
    Bug: b:216296663
    Change-Id: If1b99a6c19f302ec49365817e61857a22c4f5d64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4319525
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115112}

--- chrome/browser/enterprise/connectors/analysis/local_binary_upload_service_factory.cc
+++ chrome/browser/enterprise/connectors/analysis/local_binary_upload_service_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/analysis/local_binary_upload_service_factory.h"
 
 #include "chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.h"
+#include "chrome/browser/enterprise/signals/system_signals_service_host_factory.h"
 #include "chrome/browser/profiles/profile.h"
 
 namespace enterprise_connectors {
@@ -27,11 +28,14 @@ LocalBinaryUploadServiceFactory::GetInstance() {
 LocalBinaryUploadServiceFactory::LocalBinaryUploadServiceFactory()
     : ProfileKeyedServiceFactory(
           "LocalBinaryUploadService",
-          ProfileSelections::BuildForRegularAndIncognito()) {}
+          ProfileSelections::BuildForRegularAndIncognito()) {
+  DependsOn(enterprise_signals::SystemSignalsServiceHostFactory::GetInstance());
+}
 
 KeyedService* LocalBinaryUploadServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
-  return new LocalBinaryUploadService();
+  Profile* profile = Profile::FromBrowserContext(context);
+  return new LocalBinaryUploadService(profile);
 }
 
 }  // namespace enterprise_connectors
