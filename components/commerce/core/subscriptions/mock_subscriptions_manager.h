5db22e51da3d28a95176ba133cc1e31251af81f5
Matt Jones <mdjones@chromium.org>
2023-03-09 14:17:03 +0000

    Add test suite for ShoppingBookmarkModelObserver
    This patch also fixes a small bug where we'd clear the shopping
    specifics on a bookmark before calling the unsubscribe util in
    price_tracking_utils. The missing specifics would cause the
    unsubscribe to fail. Tests now exist for that case.
    
    Bug: b:271308950
    Change-Id: Ie036cf4363d0147f28d1284d696a0587f0a3017f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4300213
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115096}

---
+++ components/commerce/core/subscriptions/mock_subscriptions_manager.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_COMMERCE_CORE_SUBSCRIPTIONS_MOCK_SUBSCRIPTIONS_MANAGER_H_
+#define COMPONENTS_COMMERCE_CORE_SUBSCRIPTIONS_MOCK_SUBSCRIPTIONS_MANAGER_H_
+
+#include "components/commerce/core/subscriptions/subscriptions_manager.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace commerce {
+
+class MockSubscriptionsManager : public SubscriptionsManager {
+ public:
+  MockSubscriptionsManager();
+  ~MockSubscriptionsManager() override;
+
+  // SubscriptionsManager overrides:
+  MOCK_METHOD(void,
+              CheckTimestampOnBookmarkChange,
+              (int64_t bookmark_subscription_change_time),
+              (override));
+};
+
+}  // namespace commerce
+
+#endif  // COMPONENTS_COMMERCE_CORE_SUBSCRIPTIONS_MOCK_SUBSCRIPTIONS_MANAGER_H_
--- components/commerce/core/subscriptions/subscriptions_manager.cc
+++ components/commerce/core/subscriptions/subscriptions_manager.cc
@@ -55,8 +55,7 @@ SubscriptionsManager::SubscriptionsManager(
     : server_proxy_(std::move(server_proxy)),
       storage_(std::move(storage)),
       account_checker_(account_checker),
-      observers_(base::ObserverListPolicy::EXISTING_ONLY),
-      weak_ptr_factory_(this) {
+      observers_(base::ObserverListPolicy::EXISTING_ONLY) {
   // Populate the cache from local stoarge.
   storage_->LoadAllSubscriptions(base::BindOnce(
       [](base::WeakPtr<SubscriptionsManager> manager,
@@ -74,6 +73,8 @@ SubscriptionsManager::SubscriptionsManager(
   SyncSubscriptions();
   scoped_identity_manager_observation_.Observe(identity_manager);
 }
+
+SubscriptionsManager::SubscriptionsManager() = default;
 
 SubscriptionsManager::~SubscriptionsManager() = default;
 
