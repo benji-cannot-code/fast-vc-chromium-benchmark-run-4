f67cc3a91b3c8d8478a77b74cbb2655fe29b2e64
Ryan Sultanem <rsult@google.com>
2023-03-09 14:56:08 +0000

    Reland "[KeyedServiceFactory] Do not allow late initialisation of factories."
    This is a reland of commit c0b1dda334f66f72d106f4e7efbfbcc64aba47ba
    
    Original change's description:
    > [KeyedServiceFactory] Do not allow late initialisation of factories.
    >
    > Added a check after the main `EnsureBrowserContextKeyedServiceFactoriesBuilt()` function in chrome/ not to allow late initialisation of KeyedService Factories.
    >
    > If a late init is happening:
    > - DCHECK if DCHECKs are enabled. Otherwise
    > - DumpWithoutCrashing in order to get some user cases without crashing their session.
    >
    > Bug: 1412439
    > Change-Id: I38e755016b762b481253df05e94b1f8646278bc2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299071
    > Commit-Queue: Ryan Sultanem <rsult@google.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1114533}
    
    Bug: 1412439
    Change-Id: I812d2e1c4ca5741efbb78436ce1f1ba98289e88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4322009
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115121}

--- components/keyed_service/core/dependency_manager.cc
+++ components/keyed_service/core/dependency_manager.cc
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/dependency_manager.h"
 
 #include <ostream>
+#include <string>
 
 #include "base/check.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
+#include "base/logging.h"
 #include "base/notreached.h"
 #include "base/supports_user_data.h"
 #include "components/keyed_service/core/keyed_service_base_factory.h"
@@ -41,6 +43,20 @@ void DependencyManager::AddComponent(KeyedServiceBaseFactory* component) {
          "for all factories in a method called "
          "Ensure.*KeyedServiceFactoriesBuilt().";
 #endif  // DCHECK_IS_ON()
+
+  if (do_not_allow_factory_registration_) {
+    LOG(WARNING)
+        << "Trying to register KeyedService Factory: `" << component->name()
+        << "` after the call to the main registration function `"
+        << registration_function_name_error_message_
+        << "`. Please add a "
+           "call your factory `KeyedServiceFactory::GetInstance()` in the "
+           "previous method or to the appropriate "
+           "`EnsureBrowserContextKeyedServiceFactoriesBuilt()` function to "
+           "properly register your factory.";
+    base::debug::DumpWithoutCrashing();
+  }
+
   dependency_graph_.AddNode(component);
 }
 
@@ -207,4 +223,11 @@ void DependencyManager::DumpDependenciesAsGraphviz(
 
 DependencyGraph& DependencyManager::GetDependencyGraphForTesting() {
   return dependency_graph_;
+}
+
+void DependencyManager::DoNotAllowKeyedServiceFactoryRegistration(
+    const std::string& registration_function_name_error_message) {
+  do_not_allow_factory_registration_ = true;
+  registration_function_name_error_message_ =
+      registration_function_name_error_message;
 }
