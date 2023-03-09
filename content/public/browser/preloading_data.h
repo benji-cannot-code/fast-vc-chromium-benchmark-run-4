cbbfdcfe9f8cae50d675c96cf8c49e17c5e30ef5
Iman Saboori <isaboori@google.com>
2023-03-09 20:52:11 +0000

    Report preloading recall to UMA
    Reporting recall statistics for preloading predictors directly to UMA,
    will make it easier to access this aggregated information.
    
    Calling `SetIsNavigationInDomainCallback` for each predictor will be
    implemented in the next CLs.
    
    Bug: 1414064
    Change-Id: I33c1c8a1ef0191a7637327495dc8643b61d8f296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295733
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115321}

--- content/public/browser/preloading_data.h
+++ content/public/browser/preloading_data.h
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 class WebContents;
+class NavigationHandle;
 using PreloadingURLMatchCallback = base::RepeatingCallback<bool(const GURL&)>;
+using PredictorDomainCallback =
+    base::RepeatingCallback<bool(NavigationHandle*)>;
 
 // PreloadingPrediction and PreloadingAttempt are the preloading logging APIs
 // which allows us to set various metrics and log the values.
@@ -115,6 +118,20 @@ class CONTENT_EXPORT PreloadingData {
       PreloadingPredictor predictor,
       int64_t confidence,
       PreloadingURLMatchCallback url_match_predicate) = 0;
+
+  // To calculate the recall score of the `predictor`, we need to know if the
+  // `predictor` is potentially responsible for predicting the next navigation
+  // or not. Here, if caller provided `is_navigation_in_domain_callback`
+  // callback returns true for the navigation, it will be considered in the
+  // predictor's domain. The predictor's domain is the set of navigations a
+  // predictor is meant to handle (predict). For example, for Omnibox DUI, this
+  // is the set of navigations to history URLs started from the Omnibox
+  // (navigations where the user ends up choosing a Search query or where the
+  // user closes the Omnibox and clicks on a link in the page are not part of
+  // the domain of the Omnibox DUI predictor).
+  virtual void SetIsNavigationInDomainCallback(
+      PreloadingPredictor predictor,
+      PredictorDomainCallback is_navigation_in_domain_callback) = 0;
 
  protected:
   virtual ~PreloadingData() = default;
