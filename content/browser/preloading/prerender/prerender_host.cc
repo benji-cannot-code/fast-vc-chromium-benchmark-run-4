3fcdb54fb3bb7aad5d15a640fa39b172d335572d
Asami Doi <asamidoi@chromium.org>
2023-03-09 03:09:23 +0000

    Prerender: allow same-site cross-origin main frame navigations
    This CL allows the same-site cross-origin main frame navigation in a
    prerendering page. Note that the navigation only succeeds when the
    response header has `Supports-Loading-Mode: credentialed-prerender`.
    
    Bug: 1239281, 1422248
    Change-Id: I0c13b2e103921ce4f7af8b30460f3a8475c8b94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4040627
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114932}

--- content/browser/preloading/prerender/prerender_host.cc
+++ content/browser/preloading/prerender/prerender_host.cc
@@ -979,7 +979,6 @@ void PrerenderHost::SetFailureReason(PrerenderFinalStatus status) {
     case PrerenderFinalStatus::kCrossSiteNavigation:
     case PrerenderFinalStatus::kCrossSiteRedirect:
     case PrerenderFinalStatus::kSameSiteCrossOriginRedirect:
-    case PrerenderFinalStatus::kSameSiteCrossOriginNavigation:
     case PrerenderFinalStatus::kSameSiteCrossOriginRedirectNotOptIn:
     case PrerenderFinalStatus::kSameSiteCrossOriginNavigationNotOptIn:
     case PrerenderFinalStatus::kActivationNavigationParameterMismatch:
