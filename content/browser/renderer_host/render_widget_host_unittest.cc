1e8c5df8669b2e171dec7c93b3fd4df1a472d420
Dave Tapuska <dtapuska@chromium.org>
2023-03-09 23:36:15 +0000

    [ios] Fix RenderWidgetHostTest.Background
    The test was failing because no ImageTransportService was created.
    
    Bug: 1418048
    Change-Id: Iad976c0f2759d877da50d7498c6d9fc65611028f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327188
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115424}

--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -91,7 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/test_render_widget_host_view_ios_factory.h"
 #endif
 
-#if defined(USE_AURA) || BUILDFLAG(IS_MAC)
+#if defined(USE_AURA) || BUILDFLAG(IS_APPLE)
 #include "content/browser/compositor/test/test_image_transport_factory.h"
 #endif
 
@@ -567,7 +567,7 @@ class RenderWidgetHostTest : public testing::Test {
     site_instance_group_ = base::WrapRefCounted(new SiteInstanceGroup(
         SiteInstanceImpl::NextBrowsingInstanceId(), process_.get()));
     sink_ = &process_->sink();
-#if defined(USE_AURA) || BUILDFLAG(IS_MAC)
+#if defined(USE_AURA) || BUILDFLAG(IS_APPLE)
     ImageTransportFactory::SetFactory(
         std::make_unique<TestImageTransportFactory>());
 #endif
@@ -638,13 +638,12 @@ class RenderWidgetHostTest : public testing::Test {
     site_instance_group_.reset();
     process_.reset();
     browser_context_.reset();
-#if defined(USE_AURA) || BUILDFLAG(IS_MAC)
+#if defined(USE_AURA) || BUILDFLAG(IS_APPLE)
     ImageTransportFactory::Terminate();
+#endif
+#if defined(USE_AURA) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_ANDROID)
     display::Screen::SetScreenInstance(nullptr);
     screen_.reset();
-#endif
-#if BUILDFLAG(IS_ANDROID)
-    display::Screen::SetScreenInstance(nullptr);
 #endif
 
     // Process all pending tasks to avoid leaks.
