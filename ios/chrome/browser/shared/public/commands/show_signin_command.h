21f1193c0962d4e8736a57a6e183f55440087bf4
Gauthier Ambard <gambard@chromium.org>
2023-03-09 10:59:53 +0000

    [iOS] Prepare to move commands/
    This CL prepares to move the commands folder from ios/c/b/ui to
    ios/c/b/shared/public.
    This is for downstream compatibility.
    
    Bug: 1319852
    Change-Id: I10f71efc97f28cbbc9f1293025cb930a83e08087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4322195
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115047}

---
+++ ios/chrome/browser/shared/public/commands/show_signin_command.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_SHOW_SIGNIN_COMMAND_H_
+#define IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_SHOW_SIGNIN_COMMAND_H_
+
+// Temporary include.
+#import "ios/chrome/browser/ui/commands/show_signin_command.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_SHOW_SIGNIN_COMMAND_H_
