53b0e8702eae10123f83cdb90bf6e10c334036c0
Norge Vizcay <vizcay@google.com>
2023-03-09 08:54:47 +0000

    [AutofillFeedback] Remove url from autofill logs.
    The Cl adds logic to put in place the following requirement from the privacy team:
    
    - If the user decides to opt out of sharing the page url in the feedback form, all URL-related data points should be stripped out from the autofill logs.
    
    Bug: 1407646
    Change-Id: Ic2dbb4d4fdfe083e21971371f6a2f3f426896c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4314424
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115009}

--- components/feedback/feedback_util_unittest.cc
+++ components/feedback/feedback_util_unittest.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/json/json_writer.h"
 #include "base/rand_util.h"
+#include "base/test/values_test_util.h"
 #include "components/feedback/feedback_report.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -137,6 +139,39 @@ TEST_F(FeedbackUtilTest, LogsToStringShouldSkipFeedbackUserCtlConsentKey) {
   sys_info["fake_key"] = "fake_value";
   logs = feedback_util::LogsToString(sys_info);
   EXPECT_EQ("fake_key=fake_value\n", logs);
+}
+
+TEST_F(FeedbackUtilTest, RemoveUrlsFromAutofillData) {
+  base::Value::Dict autofill_data = base::test::ParseJsonDict(
+      R"({
+        "form_structures": [
+          {
+            "form_signature": "123",
+            "source_url": "https://www.example.com",
+            "main_frame_url": "https://www.example.com"
+          },
+          {
+            "form_signature": "456",
+            "source_url": "https://www.another-example.com",
+            "main_frame_url": "https://www.another-example.com"
+          }
+        ]})");
+  std::string autofill_data_str;
+  base::JSONWriter::Write(autofill_data, &autofill_data_str);
+
+  base::Value::List* form_structures =
+      autofill_data.FindList("form_structures");
+  ASSERT_TRUE(form_structures);
+  for (base::Value& item : *form_structures) {
+    item.RemoveKey("source_url");
+    item.RemoveKey("main_frame_url");
+  }
+
+  std::string expected_autofill_data_str;
+  base::JSONWriter::Write(autofill_data, &expected_autofill_data_str);
+
+  feedback_util::RemoveUrlsFromAutofillData(autofill_data_str);
+  EXPECT_EQ(autofill_data_str, expected_autofill_data_str);
 }
 
 }  // namespace feedback_util
