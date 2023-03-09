e6867452c042a15c2ffb9225185f82fbac0d4e71
Viktor Semeniuk <vsemeniuk@google.com>
2023-03-09 14:16:57 +0000

    Check reuse_manager before using it
    ReuseManager is not created for guest or incognito profiles. Accessing
    it without checking might cause a crash.
    
    Bug: 1421191
    Change-Id: I47d559dc6d06b3e903466bf12ceaf8277e7577ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4320708
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115095}

--- chrome/browser/safe_browsing/chrome_password_protection_service.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service.cc
@@ -1227,12 +1227,23 @@ void ChromePasswordProtectionService::OnWarningTriggerChanged() {
   password_manager::PasswordReuseManager* reuse_manager =
       GetPasswordReuseManager();
 
+  if (!reuse_manager) {
+    return;
+  }
+
   reuse_manager->ClearAllNonGmailPasswordHash();
   reuse_manager->ClearAllEnterprisePasswordHash();
 }
 
 void ChromePasswordProtectionService::OnEnterprisePasswordUrlChanged() {
-  GetPasswordReuseManager()->ScheduleEnterprisePasswordURLUpdate();
+  password_manager::PasswordReuseManager* reuse_manager =
+      GetPasswordReuseManager();
+
+  if (!reuse_manager) {
+    return;
+  }
+
+  reuse_manager->ScheduleEnterprisePasswordURLUpdate();
 }
 
 bool ChromePasswordProtectionService::CanShowInterstitial(
