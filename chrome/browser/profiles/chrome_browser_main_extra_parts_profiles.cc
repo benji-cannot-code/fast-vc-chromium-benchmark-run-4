32d5217be35d0cddb5a75347abbf8f091151ebe6
Alvin Jia <alvinjia@google.com>
2023-03-09 10:43:27 +0000

    Revert "[KeyedServiceFactory] Do not allow late initialisation of factories."
    This reverts commit c0b1dda334f66f72d106f4e7efbfbcc64aba47ba.
    
    Reason for revert: Breaking ChromeOS Tast tests, causing chrome crash: 2023-03-09T05:18:46.818590Z FATAL chrome[6010:6010]: [dependency_manager.cc(47)] Check failed: false. Trying to register KeyedService Factory: `CrostiniExportImportService` after the call to the main registration function `ChromeBrowserMainExtraPartsProfiles::EnsureBrowserContextKeyedServiceFactoriesBuilt()`. Please add a call your factory `KeyedServiceFactory::GetInstance()` in the previous method or to the appropriate `EnsureBrowserContextKeyedServiceFactoriesBuilt()` function to properly register your factory.
    
    see https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-eve-chrome/25761/overview
    
    
    Original change's description:
    > [KeyedServiceFactory] Do not allow late initialisation of factories.
    >
    > Added a check after the main `EnsureBrowserContextKeyedServiceFactoriesBuilt()` function in chrome/ not to allow late initialisation of KeyedService Factories.
    >
    > If a late init is happening:
    > - DCHECK if DCHECKs are enabled. Otherwise
    > - DumpWithoutCrashing in order to get some user cases without crashing their session.
    >
    > Bug: 1412439
    > Change-Id: I38e755016b762b481253df05e94b1f8646278bc2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299071
    > Commit-Queue: Ryan Sultanem <rsult@google.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1114533}
    
    Bug: 1412439
    Change-Id: If1a7d6933f9b53030a92d9fedb9326b72ad89888
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321737
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Quick-Run: Alvin Jia <alvinjia@google.com>
    Commit-Queue: Alvin Jia <alvinjia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115040}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -184,7 +184,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/commerce/core/proto/commerce_subscription_db_content.pb.h"
 #include "components/commerce/core/proto/persisted_state_db_content.pb.h"
 #include "components/enterprise/content/clipboard_restriction_service.h"
-#include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/omnibox/browser/autocomplete_controller_emitter.h"
 #include "components/optimization_guide/core/optimization_guide_switches.h"
 #include "components/optimization_guide/machine_learning_tflite_buildflags.h"
@@ -976,12 +975,4 @@ void ChromeBrowserMainExtraPartsProfiles::
 
 void ChromeBrowserMainExtraPartsProfiles::PreProfileInit() {
   EnsureBrowserContextKeyedServiceFactoriesBuilt();
-
-  // From this point, do not allow KeyedServiceFactories to be registered, all
-  // factories should be registered in the above call to
-  // `EnsureBrowserContextKeyedServiceFactoriesBuilt()`.
-  BrowserContextDependencyManager::GetInstance()
-      ->DoNotAllowKeyedServiceFactoryRegistration(
-          "ChromeBrowserMainExtraPartsProfiles::"
-          "EnsureBrowserContextKeyedServiceFactoriesBuilt()");
 }
