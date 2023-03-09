53aab2674fbcd3f060d0805486d88f7a437fb56a
Michelle <michellegc@google.com>
2023-03-09 23:52:27 +0000

    Remove unnecessary flag checks around touch selection config and test.
    1. The build flag check when creating the TouchSelectionController
       config in RenderWidgetHostViewAura is unnecessary since there is
       already a feature flag check.
    
    2. The flag checks in the test are unnecessary since the relevant new
       features (select menu commands, magnifier) are not implemented behind
       a flag in the TouchSelectionControllerClients. (These features are
       hidden when creating the menu and magnifier.) Remove the extra flag
       checks to simplify the TouchSelectionControllerClient tests.
    
    Bug: b:244116654
    Change-Id: I36cf4cbe610f4dc96b0163e493c3b598c588608f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321736
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115439}

--- content/browser/renderer_host/input/touch_selection_controller_client_aura.cc
+++ content/browser/renderer_host/input/touch_selection_controller_client_aura.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 #include "ui/base/pointer/touch_editing_controller.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/events/event_observer.h"
 #include "ui/gfx/geometry/point_conversions.h"
 #include "ui/gfx/geometry/size_conversions.h"
--- content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
+++ content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
@@ -13,9 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/test_timeouts.h"
-#include "build/chromeos_buildflags.h"
 #include "content/browser/renderer_host/render_widget_host_view_aura.h"
 #include "content/browser/renderer_host/render_widget_host_view_child_frame.h"
 #include "content/browser/renderer_host/render_widget_host_view_event_handler.h"
@@ -36,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/pointer/touch_editing_controller.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/display/display_switches.h"
 #include "ui/events/event_sink.h"
 #include "ui/events/event_utils.h"
@@ -177,12 +174,7 @@ class TestTouchSelectionControllerClientAura
 
 class TouchSelectionControllerClientAuraTest : public ContentBrowserTest {
  public:
-  TouchSelectionControllerClientAuraTest() {
-#if BUILDFLAG(IS_CHROMEOS)
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kTouchTextEditingRedesign);
-#endif  // BUILDFLAG(IS_CHROMEOS)
-  }
+  TouchSelectionControllerClientAuraTest() = default;
 
   TouchSelectionControllerClientAuraTest(
       const TouchSelectionControllerClientAuraTest&) = delete;
@@ -262,8 +254,6 @@ class TouchSelectionControllerClientAuraTest : public ContentBrowserTest {
 
   raw_ptr<TestTouchSelectionControllerClientAura> selection_controller_client_ =
       nullptr;
-
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 class TouchSelectionControllerClientAuraCAPFeatureTest
@@ -949,7 +939,6 @@ IN_PROC_BROWSER_TEST_P(TouchSelectionControllerClientAuraCAPFeatureTest,
             rwhva->selection_controller()->GetVisibleRectBetweenBounds());
 }
 
-#if BUILDFLAG(IS_CHROMEOS)
 // Tests that the select all command in the quick menu works correctly and that
 // the touch handles and quick menu are shown after the command is executed.
 IN_PROC_BROWSER_TEST_P(TouchSelectionControllerClientAuraCAPFeatureTest,
@@ -1043,7 +1032,6 @@ IN_PROC_BROWSER_TEST_P(TouchSelectionControllerClientAuraCAPFeatureTest,
       selection_controller_client()->GetActiveMenuClient()->GetSelectedText(),
       u"Text");
 }
-#endif
 
 class TouchSelectionControllerClientAuraScaleFactorTest
     : public TouchSelectionControllerClientAuraTest {
