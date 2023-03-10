82dd8167edd6b204524d9dde5f09e901ed731d1d
Maggie Chen <magchen@chromium.org>
2023-03-10 20:45:36 +0000

    Remove DumpWithoutCrashingForVideoProcessorBlt on Windows
    It reverts CL 4310035: Add crash keys for VideoProcessorBlt crash dump
    (https://crrev.com/c/4310035) and base::debug::DumpWithoutCrashing()
    
    Bug: 1421553
    
    Change-Id: I9e1db3058c335a23705211f854cc9da10fbc8fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318242
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115891}

--- ui/gl/swap_chain_presenter.cc
+++ ui/gl/swap_chain_presenter.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/trace_event/trace_event.h"
-#include "components/crash/core/common/crash_key.h"
 #include "media/base/win/mf_helpers.h"
 #include "ui/gfx/color_space_win.h"
 #include "ui/gfx/geometry/rect_conversions.h"
@@ -335,22 +334,6 @@ std::string OverlayTypeToString(DCLayerOverlayType overlay_type) {
     overlay_type_str = "software video frame";
   }
   return overlay_type_str;
-}
-
-void DumpWithoutCrashingForVideoProcessorBlt(HRESULT hr,
-                                             bool use_vp_super_resolution) {
-  static crash_reporter::CrashKeyString<16> vp_super_resolution(
-      "vp_super_resolution");
-  vp_super_resolution.Set(use_vp_super_resolution ? "on" : "off");
-
-  static crash_reporter::CrashKeyString<128> vp_blt_error_code(
-      "vp_blt_error_code");
-  vp_blt_error_code.Set(base::NumberToString(hr));
-
-  base::debug::DumpWithoutCrashing();
-
-  vp_super_resolution.Clear();
-  vp_blt_error_code.Clear();
 }
 
 }  // namespace
@@ -1637,10 +1620,6 @@ bool SwapChainPresenter::VideoProcessorBlt(
 
     if (FAILED(hr)) {
       DLOG(ERROR) << "VideoProcessorBlt failed with error 0x" << std::hex << hr;
-
-      // TODO(crbug.com/1318380): Remove this crash dump once we have collected
-      // some valid dumps.
-      DumpWithoutCrashingForVideoProcessorBlt(hr, use_vp_super_resolution);
 
       if (use_vp_super_resolution) {
         // Retry with VpSuperResolution off.
