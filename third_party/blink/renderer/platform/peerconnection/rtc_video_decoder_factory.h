d102ed1e961a7ba48d0e28c5a96519896d8db320
Qiu Jianlin <jianlin.qiu@intel.com>
2023-03-10 02:08:30 +0000

    Report supporting of packetization mode 0 & 1 for RTC hardware decoders.
    Chrome RTC decoder adapter is currently reporting support of AVC
    decoding for streams with packetization mode equals to "1" only. While
    packetization mode "0" restricts the slice size per-picture for AVC
    encoding, hardware AVC decoders are actually able to decode frames with
    short slices. Thus we report support of packetization mode "0" for
    decoder, and keep encoder capability reporting unchanged.
    
    Bug: 1422441
    Change-Id: Id2b023a70ada263fde0bc039366a210f00be819a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317954
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1115508}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_factory.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_factory.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task/sequenced_task_runner.h"
 #include "third_party/blink/renderer/platform/peerconnection/gpu_codec_support_waiter.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/webrtc/api/video_codecs/video_decoder_factory.h"
 #include "third_party/webrtc/modules/video_coding/include/video_codec_interface.h"
 #include "ui/gfx/color_space.h"
@@ -23,8 +24,8 @@ class GpuVideoAcceleratorFactories;
 
 namespace blink {
 
-// TODO(wuchengli): add unittest.
-class RTCVideoDecoderFactory : public webrtc::VideoDecoderFactory {
+class PLATFORM_EXPORT RTCVideoDecoderFactory
+    : public webrtc::VideoDecoderFactory {
  public:
   // The `decoder_factory` and `media_task_runner` are only needed if the
   // experiment `media::kUseDecoderStreamForWebRTC` is enabled. If the
