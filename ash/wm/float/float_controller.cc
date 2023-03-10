6dbe51119c6c5616fec8b1f28e0aec50e558283b
Sammie Quon <sammiequon@chromium.org>
2023-03-10 01:36:07 +0000

    float: Exit overview when floating a window
    Test: manual, ash_unitests *TabletWindowFloatSplitviewTest.OverviewNotActiveAfterFloat
    Change-Id: I429f03935d97f4f1795108c89955881644036e8f
    Fixed: b/266749011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316488
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115490}

--- ash/wm/float/float_controller.cc
+++ ash/wm/float/float_controller.cc
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/screen.h"
 #include "ui/wm/core/coordinate_conversion.h"
 
-using MagnetismCorner = ash::FloatController::MagnetismCorner;
-
 namespace ash {
 
 namespace {
