905c856ee928e63e9fae5e96ff1508086308883a
Scott Haseley <shaseley@chromium.org>
2023-03-10 00:58:54 +0000

    Move requestIdleCallback IDL to modules/scheduler for yield()
    We want to use a background task signal for for scheduler.yield()
    inheritance, but this doesn't work with the current layering since the
    idle task logic (core/) needs to talk to the scheduler (modules/). To
    fix this, this CL moves requestIdleCallback and cancelIdleCallback from
    window.idl to a new partial interface idl file in modules/scheduler.
    
    Bug: 979020
    Change-Id: Iba4d90c0f35764e205d0e128aa0f2e3422e5bdf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316774
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115474}

--- third_party/blink/renderer/core/frame/local_dom_window.h
+++ third_party/blink/renderer/core/frame/local_dom_window.h
@@ -75,7 +75,6 @@ class External;
 class Fence;
 class FrameConsole;
 class History;
-class IdleRequestOptions;
 class InputMethodController;
 class LocalFrame;
 class MediaQueryList;
@@ -94,7 +93,6 @@ class SourceLocation;
 class StyleMedia;
 class TrustedTypePolicyFactory;
 class V8FrameRequestCallback;
-class V8IdleRequestCallback;
 class V8VoidFunction;
 struct WebPictureInPictureWindowOptions;
 class WindowAgent;
@@ -345,10 +343,6 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
 
   // https://html.spec.whatwg.org/C/#dom-originagentcluster
   bool originAgentCluster() const;
-
-  // Idle callback extensions
-  int requestIdleCallback(V8IdleRequestCallback*, const IdleRequestOptions*);
-  void cancelIdleCallback(int id);
 
   // Custom elements
   CustomElementRegistry* customElements(ScriptState*) const;
