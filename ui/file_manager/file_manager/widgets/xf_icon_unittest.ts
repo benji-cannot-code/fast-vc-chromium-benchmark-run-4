b37b25c3fc4b87e3ffae4d141bd355765f264870
Traian Captan <tcaptan@chromium.org>
2023-03-10 00:45:41 +0000

    Enable CSSImageSet
    This patch enables the CSSImageSet flag by default. This adds the
    unprefixed "image-set" type so authors no longer need to use
    "-webkit-image-set". It also updates the implementation to the current
    spec with new resolution units (dppx, dpi, dpcm), image type support
    (e.g., type("image/avif")), raw urls without "url()", and gradient
    image options.
    
    Dependencies on `-webkit-` prefixed serialization are also
    updated because vendor prefixed `-webkit-image-set` is s parse time
    alias of standard `image-set`, it gets converted to it at parse time,
    and will serialize as standard `image-set` for both specified and
    computed values per css-images-4 spec. [1]
    For example setting:
    `-webkit-image-set(url(example.png) 1x)`
    serializes to
    `image-set(url("example.png") 1x` for specified value
    and to
    `image-set(url("example.png") 1dppx` for computed value.
    
    See intent-to-ship:
      https://groups.google.com/a/chromium.org/g/blink-dev/c/JXcRP4MJc9I
    
    This patch also updates UI tests to match standard image-set
    serialization.
    
    [1]
    https://w3c.github.io/csswg-drafts/css-images-4/#deprecated
    
    R=pdr
    
    Bug: 630597
    Change-Id: Ide55c4184f42220d71811c35dcf3a62e055774e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4320892
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115470}

--- ui/file_manager/file_manager/widgets/xf_icon_unittest.ts
+++ ui/file_manager/file_manager/widgets/xf_icon_unittest.ts
@@ -78,10 +78,10 @@ export async function testIconSetWithLowDPI(done: () => void) {
 
   const span = getSpanFromIcon(icon);
   assertTrue(span.classList.contains('keep-color'));
-  assertTrue(window.getComputedStyle(span).backgroundImage.includes(
-      '-webkit-image-set'));
-  assertTrue(window.getComputedStyle(span).backgroundImage.includes('1x'));
-  assertFalse(window.getComputedStyle(span).backgroundImage.includes('2x'));
+  assertTrue(
+      window.getComputedStyle(span).backgroundImage.includes('image-set'));
+  assertTrue(window.getComputedStyle(span).backgroundImage.includes('1dppx'));
+  assertFalse(window.getComputedStyle(span).backgroundImage.includes('2dppx'));
 
   done();
 }
@@ -97,10 +97,10 @@ export async function testIconSetWithHighDPI(done: () => void) {
 
   const span = getSpanFromIcon(icon);
   assertTrue(span.classList.contains('keep-color'));
-  assertTrue(window.getComputedStyle(span).backgroundImage.includes(
-      '-webkit-image-set'));
-  assertFalse(window.getComputedStyle(span).backgroundImage.includes('1x'));
-  assertTrue(window.getComputedStyle(span).backgroundImage.includes('2x'));
+  assertTrue(
+      window.getComputedStyle(span).backgroundImage.includes('image-set'));
+  assertFalse(window.getComputedStyle(span).backgroundImage.includes('1dppx'));
+  assertTrue(window.getComputedStyle(span).backgroundImage.includes('2dppx'));
 
   done();
 }
@@ -116,10 +116,10 @@ export async function testIconSetWithBothDPI(done: () => void) {
 
   const span = getSpanFromIcon(icon);
   assertTrue(span.classList.contains('keep-color'));
-  assertTrue(window.getComputedStyle(span).backgroundImage.includes(
-      '-webkit-image-set'));
-  assertTrue(window.getComputedStyle(span).backgroundImage.includes('1x'));
-  assertTrue(window.getComputedStyle(span).backgroundImage.includes('2x'));
+  assertTrue(
+      window.getComputedStyle(span).backgroundImage.includes('image-set'));
+  assertTrue(window.getComputedStyle(span).backgroundImage.includes('1dppx'));
+  assertTrue(window.getComputedStyle(span).backgroundImage.includes('2dppx'));
 
   done();
 }
