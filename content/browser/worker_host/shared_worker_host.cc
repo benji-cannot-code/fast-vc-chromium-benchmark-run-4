a08ce885e4db77d56ea0a0214bd9c5266b8ab916
Titouan Rigoudy <titouan@chromium.org>
2023-03-10 14:22:31 +0000

    [Local Network Access] Rename c/b/r/private_network_access_util.*.
    The contents of the file will be renamed later, to keep reviews simpler.
    
    Bug: chromium:1418287
    Change-Id: I3d95f81bbfde7ca3fadf02da2657cddeb3bf2218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4324677
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115686}

--- content/browser/worker_host/shared_worker_host.cc
+++ content/browser/worker_host/shared_worker_host.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/devtools/shared_worker_devtools_manager.h"
 #include "content/browser/network/cross_origin_embedder_policy_reporter.h"
 #include "content/browser/renderer_host/code_cache_host_impl.h"
-#include "content/browser/renderer_host/private_network_access_util.h"
+#include "content/browser/renderer_host/local_network_access_util.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/service_worker/service_worker_main_resource_handle.h"
 #include "content/browser/service_worker/service_worker_object_host.h"
