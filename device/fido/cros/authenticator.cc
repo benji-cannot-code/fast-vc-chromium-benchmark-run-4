72f901a565661a80aabb6683ede0598f978b7573
Howard Yang <hcyang@google.com>
2023-03-10 02:22:37 +0000

    chromeos: device: Correctly use IsU2fEnabled proto
    Currently the IsU2fEnabled API incorrectly uses the IsUvpaa proto. It
    has always been working correctly because those 2 proto definitions are
    exactly the same in format in the past.
    
    Bug: b:270509867
    Test: CQ
    Change-Id: If23aea54a9ff5ac22a802ccbccef5a74218c968a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4323058
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Howard Yang <hcyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115515}

--- device/fido/cros/authenticator.cc
+++ device/fido/cros/authenticator.cc
@@ -170,8 +170,9 @@ void ChromeOSAuthenticator::MakeCredential(
   }
 
   req.set_user_id(std::string(request.user.id.begin(), request.user.id.end()));
-  if (request.user.display_name.has_value())
+  if (request.user.display_name.has_value()) {
     req.set_user_display_name(request.user.display_name.value());
+  }
   req.set_resident_credential(request.resident_key_required);
   DCHECK(generate_request_id_callback_);
   DCHECK(current_request_id_.empty());
@@ -387,8 +388,9 @@ void ChromeOSAuthenticator::HasLegacyU2fCredentialForGetAssertionRequest(
 }
 
 void ChromeOSAuthenticator::Cancel() {
-  if (current_request_id_.empty())
+  if (current_request_id_.empty()) {
     return;
+  }
 
   u2f::CancelWebAuthnFlowRequest req;
   req.set_request_id_str(current_request_id_);
@@ -434,11 +436,11 @@ void ChromeOSAuthenticator::IsUVPlatformAuthenticatorAvailable(
 void ChromeOSAuthenticator::IsPowerButtonModeEnabled(
     base::OnceCallback<void(bool is_enabled)> callback) {
   chromeos::U2FClient::Get()->IsU2FEnabled(
-      u2f::IsUvpaaRequest(),
+      u2f::IsU2fEnabledRequest(),
       base::BindOnce(
           [](base::OnceCallback<void(bool is_enabled)> callback,
-             absl::optional<u2f::IsUvpaaResponse> response) {
-            std::move(callback).Run(response && response->available());
+             absl::optional<u2f::IsU2fEnabledResponse> response) {
+            std::move(callback).Run(response && response->enabled());
           },
           std::move(callback)));
 }
