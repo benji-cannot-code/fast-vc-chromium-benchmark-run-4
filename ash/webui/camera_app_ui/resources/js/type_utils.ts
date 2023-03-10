3120001e26fc59515d6e7641665d1947437d2660
Pi-Hsun Shih <pihsun@chromium.org>
2023-03-10 05:20:31 +0000

    CCA: static check that the state classes are not overlapping
    Since all the types in StateUnion can be set on state (represented
    currently as class on body tag), if the enum values overlap they will
    have the same runtime values, and might cause strange issue if the two
    enum are used as different things. Add a type level check for this.
    
    Bug: None
    Test: cca.py tsc <board>
    Test: cca.py tsc <board> failed with CL:4318103 reverted
    Change-Id: I8e25628abdca3b3e0322d728999db362c4f5ce57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321738
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115553}

---
+++ ash/webui/camera_app_ui/resources/js/type_utils.ts
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Asserts that the given type is never.
+ */
+export type AssertNever<T extends never> = T;
+
+type CheckEnumValuesOverlapImpl<Union, Overlap, Enums> =
+    Enums extends [infer Enum extends string, ...infer Rest] ?
+    CheckEnumValuesOverlapImpl<
+        Union|`${Enum}`, Overlap|(Union&`${Enum}`), Rest>:
+    Overlap;
+
+/**
+ * Checks that a tuple of enum have pairwise non-overlapping values.
+ *
+ * The result type will be |never| if there's no overlap, and union of the
+ * overlapped values if there's overlap.
+ */
+export type CheckEnumValuesOverlap<Enums extends string[]> =
+    CheckEnumValuesOverlapImpl<never, never, Enums>;
