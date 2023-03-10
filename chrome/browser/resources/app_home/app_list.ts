9b7f65cdc1b5f8b3ba03b3118de9837baca3a06d
Camden King <camdenking@google.com>
2023-03-10 19:06:56 +0000

    [AppHome] Adding "not locally installed" a11y label to app titles
    Bug: 1350406, b/271472633
    Change-Id: Iad24b2fc3ce3177e059cacfa0c7151126686ba3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4324120
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Camden King <camdenking@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115832}

--- chrome/browser/resources/app_home/app_list.ts
+++ chrome/browser/resources/app_home/app_list.ts
@@ -206,6 +206,13 @@ export class AppListElement extends PolymerElement {
     this.closeCurrentAppMenu();
     this.selectedAppItem_ = event.detail.appItem;
   }
+
+  private notLocallyInstalledString_(installed: boolean, i18nString: string) {
+    if (!installed) {
+      return ' (' + i18nString + ')';
+    }
+    return '';
+  }
 }
 
 declare global {
