f38a1c8ab06a429f170e19b0bd019d2fe9c367ac
ginnnnnnny <ginnyhuang@chromium.org>
2023-03-10 16:04:03 +0000

    [ios][byot] Minor update to BYOT metrics
    Slight update to crrev/c/4294486; tweaked the metrics description and
    added the count metric names to the bring_android_tabs_metrics.
    
    Fixed: 1418108
    Change-Id: I2b2acb896c0ee6e737ba3869f97592335abea32b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4325000
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115719}

--- ios/chrome/browser/ui/recent_tabs/bring_android_tabs/bring_android_tabs_metrics.h
+++ ios/chrome/browser/ui/recent_tabs/bring_android_tabs/bring_android_tabs_metrics.h
@@ -8,6 +8,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace bring_android_tabs {
 
+// Name of tab count histogram; logged when the bring android tabs prompt
+// appears.
+extern const char kTabCountHistogramName[];
+
+// Name of the action taken by the user after the Bring Android Tabs prompt is
+// presented.
+extern const char kPromptActionHistogramName[];
+
+// Name of the action taken by the user in response to the Bring Android Tabs
+// review tabs table.
+extern const char kTabListActionHistogramName[];
+
+// Name of tab count histogram; logged when the bring android tabs prompt
+// appears.
+extern const char kTabCountHistogramName[];
+
+// Name of deselected tab count histogram; logged the review Android tabs table
+// is dismissed.
+extern const char kDeselectedTabCountHistogramName[];
+
 // The result of prompting the set of tabs for an Android switcher. This is
 // mapped to the IOSPromptTabsForAndroidSwitcherState enum in enums.xml for
 // metrics.
@@ -19,7 +39,7 @@ enum class IOSPromptTabsForAndroidSwitcherState {
   kNoActiveTabs = 4,
   kSuccess = 5,
   kMaxValue = kSuccess,
-}
+};
 
 // Interactions with the initial Bring Android Tabs prompt. This is mapped to
 // the IOSBringAndroidTabsPromptActionType enum in enums.xml for metrics.
@@ -29,7 +49,7 @@ enum class IOSBringAndroidTabsPromptActionType {
   kCancel = 2,
   kSwipeToDismiss = 3,
   kMaxValue = kSwipeToDismiss,
-}
+};
 
 // Interactions with the Bring Android Tabs Tab List view. This is mapped to the
 // IOSBringAndroidTabsTabsListActionType enum in enums.xml for metrics.
@@ -38,7 +58,7 @@ enum class IOSBringAndroidTabsTabsListActionType {
   kSwipeDown = 1,
   kOpenTabs = 2,
   kMaxValue = kOpenTabs,
-}
+};
 
 }  // namespace bring_android_tabs
 
