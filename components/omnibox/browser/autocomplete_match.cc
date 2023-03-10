e9174f58ef89a730cf838e7c2d94ef85f5a8fa7e
manukh <manukh@chromium.org>
2023-03-10 19:15:49 +0000

    [omnibox][ui-simplification] Replace omnibox::BlankIcon with a null icon
    Currently, tail suggestions and some starter pack suggestiosn use the
    `omnibox::BlankIcon`, a 16x16 (or 32x32) blank vector icon.
    
    With GM3, we're adding backgrounds to icons, including these blank
    icons, which is bad.
    
    This CL replaces the blank icon with a null `{}` vector icon. The
    `OmniboxResultView` then checks: if the icon is null, don't draw the
    background and icon.
    
    The realbox also did something similar: if the vector icon was
    specifically the blank icon, it wasn't drawn. This is updated to instead
    check if the vector icon is null.
    
    Before: https://screenshot.googleplex.com/6WFFnA9nBitc8UL.png
    After: https://screenshot.googleplex.com/8c6RdJvqzDTB7Tn
    
    Bug: 1401498
    Change-Id: I5be2323211ab5cb6953821241980acfddfa5bdc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327790
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115839}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -44,6 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+#if (!BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_VR)) && !BUILDFLAG(IS_IOS)
+// Used for `SEARCH_SUGGEST_TAIL` and `NULL_RESULT_MESSAGE` (e.g. starter pack)
+// type suggestion icons.
+static gfx::VectorIcon empty_icon;
+#endif
+
 bool IsTrivialClassification(const ACMatchClassifications& classifications) {
   return classifications.empty() ||
          ((classifications.size() == 1) &&
@@ -514,7 +520,7 @@ const gfx::VectorIcon& AutocompleteMatch::GetVectorIcon(
 
     case Type::SEARCH_SUGGEST_TAIL:
     case Type::NULL_RESULT_MESSAGE:
-      return omnibox::kBlankIcon;
+      return empty_icon;
 
     case Type::DOCUMENT_SUGGESTION:
       switch (document_type) {
