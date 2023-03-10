3aae3981891fd1d9b612fde2a3936f40f275a166
Norge Vizcay <vizcay@google.com>
2023-03-10 14:43:57 +0000

    Reland "[AutofillFeedback] CrOS: Add autofill metadata link to UI."
    This is a reland of commit 7df9b959e092a178b4dc78f25b787b6e5be3bae6
    
    Original change's description:
    > [AutofillFeedback] CrOS: Add autofill metadata link to UI.
    >
    > The CL includes a new checkbox on the share_data_page, where users can decide whether or not to share autofill metadata related to the current page. The checkbox string also offers a link that will open a new dialog displaying the corresponding the autofill metadata.
    >
    > The option is only offered to internal users that opened the feedback dialog through the corresponding context menu item.
    >
    > https://screenshot.googleplex.com/4sXMrY5q32einhJ.png
    >
    > Bug: 1407646
    > Change-Id: I1306c42f9489cb2f5fa427f13f0c7ef8254c014e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4311942
    > Commit-Queue: Norge Vizcay <vizcay@google.com>
    > Reviewed-by: Camille Lamy <clamy@chromium.org>
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1115024}
    
    Bug: 1407646
    Change-Id: I94aeed6294726cb36aca4c40ea9b156f3fc6c31b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4325987
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115689}

--- ash/webui/os_feedback_ui/resources/feedback_flow.js
+++ ash/webui/os_feedback_ui/resources/feedback_flow.js
@@ -60,6 +60,8 @@ export const AdditionalContextQueryParam = {
   PAGE_URL: 'page_url',
   FROM_ASSISTANT: 'from_assistant',
   FROM_SETTINGS_SEARCH: 'from_settings_search',
+  FROM_AUTOFILL: 'from_autofill',
+  AUTOFILL_METADATA: 'autofill_metadata',
 };
 
 /**
@@ -187,6 +189,13 @@ export class FeedbackFlowElement extends PolymerElement {
     this.shouldShowBluetoothCheckbox_;
 
     /**
+     * Whether to show the autofill checkbox in share data page.
+     * @type {boolean}
+     * @protected
+     */
+    this.shouldShowAutofillCheckbox_;
+
+    /**
      * Whether to show the assistant checkbox in share data page.
      * @type {boolean}
      */
@@ -265,6 +274,8 @@ export class FeedbackFlowElement extends PolymerElement {
       this.shouldShowAssistantCheckbox_ = !!this.feedbackContext_ &&
           this.feedbackContext_.isInternalAccount &&
           this.feedbackContext_.fromAssistant;
+      this.shouldShowAutofillCheckbox_ =
+          !!this.feedbackContext_ && this.feedbackContext_.fromAutofill;
     });
 
     window.addEventListener('message', event => {
@@ -386,6 +397,14 @@ export class FeedbackFlowElement extends PolymerElement {
     const fromSettingsSearch =
         params.get(AdditionalContextQueryParam.FROM_SETTINGS_SEARCH);
     this.set('feedbackContext_.fromSettingsSearch', !!fromSettingsSearch);
+
+    const fromAutofill = params.get(AdditionalContextQueryParam.FROM_AUTOFILL);
+    this.feedbackContext_.fromAutofill = !!fromAutofill;
+    const autofillMetadata =
+        params.get(AdditionalContextQueryParam.AUTOFILL_METADATA);
+    if (autofillMetadata) {
+      this.feedbackContext_.autofillMetadata = autofillMetadata;
+    }
   }
 
   /**
