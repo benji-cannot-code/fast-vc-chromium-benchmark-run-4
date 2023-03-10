8b8dca3ae08b1aa13062e3861d36e7f97a69f138
Ankush Singh <ankushkush@google.com>
2023-03-10 12:23:51 +0000

    [sync] Create metadata for common syncable prefs
    This assigns an ID to each syncable pref. This should allow introducing
    new metrics for prefs sync.
    
    More metadata fields can be added later.
    
    Bug: 1422534
    Change-Id: I2e0aa7f5e33400b42b7ddc08eccba9da23eb74e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4323387
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115646}

---
+++ components/sync_preferences/syncable_prefs_database.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync_preferences/syncable_prefs_database.h"
+
+namespace sync_preferences {
+absl::optional<SyncablePrefMetadata>
+SyncablePrefsDatabase::GetSyncablePrefMetadata(
+    const std::string& pref_name) const {
+  NOTIMPLEMENTED();
+  return {};
+}
+
+bool SyncablePrefsDatabase::IsPreferenceSyncable(
+    const std::string& pref_name) const {
+  return GetSyncablePrefMetadata(pref_name).has_value();
+}
+
+}  // namespace sync_preferences
