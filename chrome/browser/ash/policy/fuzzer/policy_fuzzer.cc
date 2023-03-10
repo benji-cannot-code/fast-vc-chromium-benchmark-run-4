3e1cdedc325c4e041b3623408b82109b2bfd37c6
Ching-Kang Yen <chingkang@google.com>
2023-03-10 17:08:47 +0000

    Shutdown AttestationFeatures in policy_fuzzer
    The AttestationFeatures was not shutdown in policy_fuzzer, so there is
    some direct-leak detected.
    
    Bug: 1421383
    Change-Id: I1db15a9150e69fda8c2befbed7ada4fb9cc4ec21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328574
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Ching-Kang Yen <chingkang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115754}

--- chrome/browser/ash/policy/fuzzer/policy_fuzzer.cc
+++ chrome/browser/ash/policy/fuzzer/policy_fuzzer.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/settings/device_settings_service.h"
 #include "chrome/browser/policy/configuration_policy_handler_list_factory.h"
 #include "chrome/common/chrome_paths.h"
+#include "chromeos/ash/components/attestation/attestation_features.h"
 #include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "components/policy/core/browser/configuration_policy_handler_list.h"
 #include "components/policy/core/browser/policy_conversions_client.h"
@@ -101,6 +102,7 @@ struct PerInputEnvironment {
     ash::ShutdownDBus();
     ash::InstallAttributes::Shutdown();
     ash::DeviceSettingsService::Shutdown();
+    ash::attestation::AttestationFeatures::Shutdown();
   }
 
   base::test::TaskEnvironment task_environment;
