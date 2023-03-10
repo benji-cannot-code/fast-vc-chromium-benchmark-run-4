a3c764cdece48a03d838b23a07a2f241df5521fb
Yuly Novikov <ynovikov@chromium.org>
2023-03-10 18:03:59 +0000

    Revert "[vulkan] Add a memory-infra memory dump provider for VMA memory"
    This reverts commit a6965be6eca29fe21cfaa76ea610f4587cf48a8b.
    
    Reason for revert:
    breaks android_webview_pixel_skia_gold_test on Pixel 6
    https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20FYI%20Release%20(Pixel%206)/5212/overview
    
    Original change's description:
    > [vulkan] Add a memory-infra memory dump provider for VMA memory
    >
    > Bug: 1411340
    >
    > Change-Id: I90d6c0037d195bd10ce8875c6cded130048067bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295635
    > Reviewed-by: Victor Miura <vmiura@chromium.org>
    > Commit-Queue: Benoit Lize <lizeb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1115326}
    
    Bug: 1411340
    Change-Id: Iddd793e854a0b010c0a32945230c47e082bec302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328277
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Auto-Submit: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115790}

--- gpu/vulkan/vma_wrapper.h
+++ gpu/vulkan/vma_wrapper.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_VULKAN_VMA_WRAPPER_H_
 #define GPU_VULKAN_VMA_WRAPPER_H_
 
-#include <algorithm>
-
 #include <vulkan/vulkan_core.h>
 
 #include "base/component_export.h"
@@ -102,10 +100,8 @@ void GetPhysicalDeviceProperties(
 COMPONENT_EXPORT(VULKAN)
 void GetBudget(VmaAllocator allocator, VmaBudget* budget);
 
-// Allocated and used, respectively.
 COMPONENT_EXPORT(VULKAN)
-std::pair<uint64_t, uint64_t> GetTotalAllocatedAndUsedMemory(
-    VmaAllocator allocator);
+uint64_t GetTotalAllocatedMemory(VmaAllocator allocator);
 
 }  // namespace vma
 }  // namespace gpu
