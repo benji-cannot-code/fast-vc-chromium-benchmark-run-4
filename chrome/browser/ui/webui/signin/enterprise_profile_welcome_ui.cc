fa370ed340e91a2e23b0ba2b86eb61a33bdfc2ff
Cole Horvitz <colehorvitz@chromium.org>
2023-03-11 03:19:00 +0000

    CR2023: Add Chrome Refresh 2023 attribute to more UIs.
    This continues work from https://crrev.com/c/4327090.
    
    Bug: 1400032
    Change-Id: I60633614bb55b1475482e1c2a3508e8619945c95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327296
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116030}

--- chrome/browser/ui/webui/signin/enterprise_profile_welcome_ui.cc
+++ chrome/browser/ui/webui/signin/enterprise_profile_welcome_ui.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_data_source.h"
 #include "services/network/public/mojom/content_security_policy.mojom.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/base/webui/resource_path.h"
 #include "ui/resources/grit/webui_resources.h"
 
@@ -74,6 +75,9 @@ EnterpriseProfileWelcomeUI::EnterpriseProfileWelcomeUI(content::WebUI* web_ui)
   source->AddBoolean(
       "isTangibleSyncStyleEnabled",
       base::FeatureList::IsEnabled(kEnterpriseWelcomeTangibleSyncStyle));
+  source->AddString(
+      "chromeRefresh2023Attribute",
+      features::IsChromeRefresh2023() ? "chrome-refresh-2023" : "");
 }
 
 EnterpriseProfileWelcomeUI::~EnterpriseProfileWelcomeUI() = default;
