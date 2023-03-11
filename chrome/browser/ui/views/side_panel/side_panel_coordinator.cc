a826a04c851e4ab8b8d723eae76297c484edbaf9
Caroline Rising <corising@chromium.org>
2023-03-11 03:34:54 +0000

    Power bookmarks: make the side panel open to bookmarks by default.
    Bug: 1365606
    Change-Id: I51d4b50c93213eaf2a16c2882a1b33405b19bbd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328952
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116032}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/side_panel/side_panel.h"
@@ -53,7 +54,11 @@ const char kGlobalSidePanelRegistryKey[] = "global_side_panel_registry_key";
 constexpr int kSidePanelContentViewId = 42;
 constexpr int kSidePanelContentWrapperViewId = 43;
 
-constexpr SidePanelEntry::Id kDefaultEntry = SidePanelEntry::Id::kReadingList;
+SidePanelEntry::Id GetDefaultEntry() {
+  return base::FeatureList::IsEnabled(features::kPowerBookmarksSidePanel)
+             ? SidePanelEntry::Id::kBookmarks
+             : SidePanelEntry::Id::kReadingList;
+}
 
 std::unique_ptr<views::ImageButton> CreateControlButton(
     views::View* host,
@@ -197,7 +202,8 @@ void SidePanelCoordinator::Show(
   if (entry_id.has_value()) {
     Show(SidePanelEntry::Key(entry_id.value()), open_trigger);
   } else {
-    Show(GetLastActiveEntryKey().value_or(SidePanelEntry::Key(kDefaultEntry)),
+    Show(GetLastActiveEntryKey().value_or(
+             SidePanelEntry::Key(GetDefaultEntry())),
          open_trigger);
   }
 }
@@ -592,8 +598,9 @@ std::unique_ptr<views::Combobox> SidePanelCoordinator::CreateCombobox() {
   combobox->SetMenuSelectionAtCallback(
       base::BindRepeating(&SidePanelCoordinator::OnComboboxChangeTriggered,
                           base::Unretained(this)));
-  combobox->SetSelectedIndex(combobox_model_->GetIndexForKey(
-      (GetLastActiveEntryKey().value_or(SidePanelEntry::Key(kDefaultEntry)))));
+  combobox->SetSelectedIndex(
+      combobox_model_->GetIndexForKey((GetLastActiveEntryKey().value_or(
+          SidePanelEntry::Key(GetDefaultEntry())))));
   combobox->SetAccessibleName(
       l10n_util::GetStringUTF16(IDS_ACCNAME_SIDE_PANEL_SELECTOR));
   combobox->SetProperty(
@@ -698,8 +705,8 @@ void SidePanelCoordinator::OnEntryRegistered(SidePanelRegistry* registry,
                                              SidePanelEntry* entry) {
   combobox_model_->AddItem(entry);
   if (GetContentView()) {
-    SetSelectedEntryInCombobox(
-        GetLastActiveEntryKey().value_or(SidePanelEntry::Key(kDefaultEntry)));
+    SetSelectedEntryInCombobox(GetLastActiveEntryKey().value_or(
+        SidePanelEntry::Key(GetDefaultEntry())));
   }
 
   // If `entry` is a contextual entry and the global entry with the same key is
@@ -717,8 +724,8 @@ void SidePanelCoordinator::OnEntryWillDeregister(SidePanelRegistry* registry,
     combobox_model_->RemoveItem(entry->key());
 
     if (GetContentView()) {
-      SetSelectedEntryInCombobox(
-          GetLastActiveEntryKey().value_or(SidePanelEntry::Key(kDefaultEntry)));
+      SetSelectedEntryInCombobox(GetLastActiveEntryKey().value_or(
+          SidePanelEntry::Key(GetDefaultEntry())));
     }
   }
 
