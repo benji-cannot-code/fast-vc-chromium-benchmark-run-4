1bb69b53d3578b66d8cb3450c56070a69c1e2399
Luc Nguyen <lucnguyen@google.com>
2023-03-11 22:39:00 +0000

    Enable MetricsServiceAsyncCollection by default
    Since now `metrics_service.cc` posts to `base::ThreadPool` by default,
    I had to adjust some unit tests. I.e., we are not allowed to use
    `base::TestSimpleTaskRunner` in the tests anymore, we have to use
    a `base::test::TaskEnvironment`, which is better and more accurate.
    For example, the former did not respect delays properly: if your code
    posted a task "A" to run in 5 seconds, and a task "B" to run
    immediately, when you did
    `base::TestSimpleTaskRunner::RunPendingTasks()`, it would just run
    all tasks regardless of their delay, i.e., it would run task A first
    and then task B. At least, that's what I understood from
    https://source.chromium.org/chromium/chromium/src/+/main:base/test/test_simple_task_runner.h
    
    I also rewrote the `MetricsServiceTestWithFeatures.SplitRotation` test
    to be a lot more descriptive in what it's doing, and also more
    accurate in terms of timing.
    
    Bug: 1052796
    Change-Id: I310115db4fa3d7dd972d5ad923236da73c0b6b98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316968
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116114}

--- components/metrics/metrics_scheduler.h
+++ components/metrics/metrics_scheduler.h
@@ -32,6 +32,9 @@ class MetricsScheduler {
   // Stops scheduling uploads.
   void Stop();
 
+  // Returns the initial delay before the task is run for the first time.
+  static int GetInitialIntervalSeconds();
+
  protected:
   // Subclasses should provide task_callback with a wrapper to call this with.
   // This indicates the triggered task was completed/cancelled and the next
--- components/metrics/metrics_upload_scheduler.cc
+++ components/metrics/metrics_upload_scheduler.cc
@@ -43,18 +43,6 @@ base::TimeDelta BackOffUploadInterval(base::TimeDelta interval) {
   return interval;
 }
 
-// Time delay after a log is uploaded successfully before attempting another.
-// On mobile, keeping the radio on is very expensive, so prefer to keep this
-// short and send in bursts.
-base::TimeDelta GetUnsentLogsInterval() {
-  return base::Seconds(3);
-}
-
-// Initial time delay after a log uploaded fails before retrying it.
-base::TimeDelta GetInitialBackoffInterval() {
-  return base::Minutes(5);
-}
-
 }  // namespace
 
 MetricsUploadScheduler::MetricsUploadScheduler(
@@ -65,7 +53,17 @@ MetricsUploadScheduler::MetricsUploadScheduler(
       initial_backoff_interval_(GetInitialBackoffInterval()),
       backoff_interval_(initial_backoff_interval_) {}
 
-MetricsUploadScheduler::~MetricsUploadScheduler() {}
+MetricsUploadScheduler::~MetricsUploadScheduler() = default;
+
+// static
+base::TimeDelta MetricsUploadScheduler::GetUnsentLogsInterval() {
+  return base::Seconds(3);
+}
+
+// static
+base::TimeDelta MetricsUploadScheduler::GetInitialBackoffInterval() {
+  return base::Minutes(5);
+}
 
 void MetricsUploadScheduler::UploadFinished(bool server_is_healthy) {
   // If the server is having issues, back off. Otherwise, reset to default
