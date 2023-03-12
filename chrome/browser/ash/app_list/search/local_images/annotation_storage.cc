45afe0c410daca5f96730192ff4704eda2d1710a
Dmitry Grebenyuk <dgrebenyuk@google.com>
2023-03-12 23:21:13 +0000

    Fix sequence_checker.
    This CL fixes sequence_checker and clarifies the initialization
    procedure.
    
    Test: "on DUT with ad hoc enabled search provider"
    Test: "passes unittests"
    Bug: b:260646344
    Change-Id: I1cce9661fe8a0d17492ef073b79482a18c32d4fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328548
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116194}

--- chrome/browser/ash/app_list/search/local_images/annotation_storage.cc
+++ chrome/browser/ash/app_list/search/local_images/annotation_storage.cc
@@ -113,7 +113,7 @@ void AnnotationStorage::Initialize() {
   }
   if (annotation_worker_ != nullptr) {
     // Owns `annotation_worker_`.
-    annotation_worker_->Run(this);
+    annotation_worker_->Initialize(this);
   }
 }
 
