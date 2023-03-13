4d42e3c9ca2045c038e7225e4670ef6076a3b312
Mustafa Emre Acer <meacer@chromium.org>
2023-03-13 23:39:38 +0000

    Disable HTTPS Upgrades in site isolation tests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    Bug: 1424194
    Change-Id: I645d693f53b7ca96e307e2fb3331cf90cf666ed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335930
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116684}

--- chrome/browser/site_isolation/chrome_site_per_process_test.cc
+++ chrome/browser/site_isolation/chrome_site_per_process_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
@@ -15,7 +16,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-ChromeSitePerProcessTest::ChromeSitePerProcessTest() {}
+ChromeSitePerProcessTest::ChromeSitePerProcessTest() {
+  feature_list_.InitWithFeatures(
+      /*enabled_features=*/{},
+      // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having to
+      // disable this feature.
+      /*disabled_features=*/{features::kHttpsUpgrades});
+}
 
 ChromeSitePerProcessTest::~ChromeSitePerProcessTest() {}
 
--- chrome/browser/site_isolation/chrome_site_per_process_test.h
+++ chrome/browser/site_isolation/chrome_site_per_process_test.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SITE_ISOLATION_CHROME_SITE_PER_PROCESS_TEST_H_
 #define CHROME_BROWSER_SITE_ISOLATION_CHROME_SITE_PER_PROCESS_TEST_H_
 
+#include "base/feature_list.h"
 #include "chrome/test/base/in_process_browser_test.h"
 
 namespace base {
@@ -24,6 +25,11 @@ class ChromeSitePerProcessTest : public InProcessBrowserTest {
   void SetUpCommandLine(base::CommandLine* command_line) override;
 
   void SetUpOnMainThread() override;
+
+  base::test::ScopedFeatureList* feature_list() { return &feature_list_; }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 #endif  // CHROME_BROWSER_SITE_ISOLATION_CHROME_SITE_PER_PROCESS_TEST_H_
