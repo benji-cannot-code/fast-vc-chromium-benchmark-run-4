96541b9cea6f36ee200615c8266ee1069a41aadf
Jihad Hanna <jihadghanna@google.com>
2023-03-13 13:03:48 +0000

    Terminal logging of chrome://(autofill|password-manager)-internals
    This CL:
    - Adds two features `k(Autofill|PasswordManager)LoggingToTerminal`
    that toggles logging the content of the appropriate internals page to
    the terminal.
    - Removes a previous command line flag --show-autofill-internals that
    had a similar effect but only on tests.
    - Adds a TextLogReceiver member to LogRouter that observes the router
    depending on the features configuration and the router factory.
    - Fixes some typos in touched files.
    
    Bug: 928595
    Change-Id: Ib9bab23d794b61d4f39d81856fc3d3d500b8c873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279709
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116331}

--- components/autofill/core/browser/logging/log_router.h
+++ components/autofill/core/browser/logging/log_router.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/scoped_observation_traits.h"
 #include "base/values.h"
+#include "components/autofill/core/browser/logging/text_log_receiver.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace autofill {
@@ -40,6 +41,9 @@ class LogRouter : public KeyedService {
   void ProcessLog(const std::string& text);
   void ProcessLog(const base::Value::Dict& node);
 
+  // Enables recording autofill-internals logs to LOG(INFO).
+  void LogToTerminal();
+
   // All four (Unr|R)egister* methods below are safe to call from the
   // constructor of the registered object, because they do not call that object,
   // and the router only runs on a single thread.
@@ -58,10 +62,11 @@ class LogRouter : public KeyedService {
 
  private:
   // Observer lists for managers and receivers. The |true| in the template
-  // specialisation means that they will check that all observers were removed
+  // specialization means that they will check that all observers were removed
   // on destruction.
   base::ObserverList<RoutingLogManager, true>::Unchecked managers_;
   base::ObserverList<LogReceiver, true>::Unchecked receivers_;
+  TextLogReceiver text_log_receiver_;
 };
 
 }  // namespace autofill
