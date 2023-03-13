ffc71524d557fb59543713e5fb09d44f745cde75
Oleksandr Sarapulov <olsa@google.com>
2023-03-13 13:43:19 +0000

    Fixed race condition for EAP WiFi network settings
    WiFi settings for EAP-TLS, PEAP and EAP-TTLS requires Subject
    alternative name match or Domain suffix match if default Server
    CA is selected. This check had race condition and now it is
    fixed.
    
    Bug: b/268176792, b/271075905
    Change-Id: Iceacad7954a51fd3092edf291e22c6d1d6978135
    Testing: manual test and updated unit tests.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4322197
    Commit-Queue: Oleksandr Sarapulov <olsa@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116336}

--- ash/webui/common/resources/network/network_config.js
+++ ash/webui/common/resources/network/network_config.js
@@ -2516,7 +2516,7 @@ Polymer({
       return true;
     }
 
-    if (!this.eapProperties_.useSystemCas) {
+    if (this.selectedServerCaHash_ !== DEFAULT_HASH) {
       // Does not use default CA server certs.
       return true;
     }
