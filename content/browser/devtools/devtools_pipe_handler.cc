e22c1de18cde024a5d9640f3ddbb6ab815bc59f5
Peter Kvitek <kvitekp@chromium.org>
2023-03-13 20:37:56 +0000

    [DevTools] Added invalid parameter handler for FD _close() on Windows
    This is a follow up to https://crrev.com/c/4320956 that changed a Win32
    ::CloseHandle() call on a handle returned by _get_osfhandl() to a
    _close() call as prescribed by https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/get-osfhandle?view=msvc-170
    
    If given an invalid FD, _close() will call an invalid parameter handler
    that will terminate the process which is undesirable, so this CL adds
    a temporary invalid parameter handler override that will make _close() to return an error condition instead of crashing.
    
    Bug: 1423694
    Change-Id: I3b81ba58ffef36c1b232f7c9fe3320b360cebc11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334865
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116578}

--- content/browser/devtools/devtools_pipe_handler.cc
+++ content/browser/devtools/devtools_pipe_handler.cc
@@ -163,6 +163,7 @@ class PipeReaderBase : public PipeIOBase {
 #if BUILDFLAG(IS_WIN)
     // Cancel pending synchronous read.
     CancelIoEx(read_handle_, nullptr);
+    ScopedInvalidParameterHandlerOverride invalid_parameter_handler_override;
     _close(read_fd_);
     read_handle_ = INVALID_HANDLE_VALUE;
 #else
@@ -245,6 +246,7 @@ class PipeWriterBase : public PipeIOBase {
  protected:
   void ClosePipe() override {
 #if BUILDFLAG(IS_WIN)
+    ScopedInvalidParameterHandlerOverride invalid_parameter_handler_override;
     _close(write_fd_);
     write_handle_ = INVALID_HANDLE_VALUE;
 #else
