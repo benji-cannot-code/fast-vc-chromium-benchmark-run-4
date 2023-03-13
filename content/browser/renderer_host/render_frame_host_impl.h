d9c29ed59b924b923a4156d269b74831189cee3c
Mingyu Lei <leimy@chromium.org>
2023-03-13 08:21:58 +0000

    CCNS: Ignore the cookie changes made by the navigation request
    In https://crrev.com/c/4235289, we move the cookie change listener
    to NavigationRequest, which will observe all the cookie modification
    since the start of the navigation until the end of the document's life
    time. The cookie modification will be used as a signal indicating the
    necessity of inactive document destruction, so we should exclude the
    ones made by the navigation request itself.
    
    In this CL, instead of just keeping two boolean values in
    `CookieChangeListener`, we use two counters instead, which makes it
    possible to decrement the number of cookie modification observed from
    `CookieAccessObserver`.
    
    Bug: 1399741
    Change-Id: I567816a060b2548bee34a4a7741af553331d2974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328755
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116265}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <cstdint>
 #include <list>
 #include <map>
 #include <memory>
@@ -1383,10 +1384,10 @@ class CONTENT_EXPORT RenderFrameHostImpl
   class CookieChangeListener : public network::mojom::CookieChangeListener {
    public:
     struct CookieChangeInfo {
-      // Indicates whether any cookie modification has been observed.
-      bool cookie_modified = false;
-      // Indicates whether any HTTPOnly cookie modification has been observed.
-      bool http_only_cookie_modified = false;
+      // The number of observed cookie modifications.
+      int64_t cookie_modification_count_ = 0;
+      // The number of observed HTTPOnly cookie modifications.
+      int64_t http_only_cookie_modification_count_ = 0;
     };
 
     CookieChangeListener(StoragePartition* storage_partition, GURL& url);
@@ -1396,6 +1397,21 @@ class CONTENT_EXPORT RenderFrameHostImpl
 
     // Returns a copy of the `cookie_change_info_`.
     CookieChangeInfo cookie_change_info() { return cookie_change_info_; }
+
+    // We don't want to count the cookie modification made by the
+    // `NavigationRequest` itself, so provide this function to allow the count
+    // adjustment.
+    // Passing the `base::PassKey` to restrict the caller of this method to
+    // `NavigationRequest` only.
+    void RemoveNavigationCookieModificationCount(
+        base::PassKey<content::NavigationRequest> navigation_request,
+        uint64_t cookie_modification_count_delta,
+        uint64_t http_only_cookie_modification_count_delta) {
+      cookie_change_info_.cookie_modification_count_ -=
+          cookie_modification_count_delta;
+      cookie_change_info_.http_only_cookie_modification_count_ -=
+          http_only_cookie_modification_count_delta;
+    }
 
    private:
     // network::mojom::CookieChangeListener
