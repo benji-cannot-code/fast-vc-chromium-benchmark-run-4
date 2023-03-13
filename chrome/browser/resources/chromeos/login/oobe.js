0bcc37d5f24078e1e45d8fe0317a8cd3c5ecdff3
Renato Silva <rrsilva@google.com>
2023-03-13 18:03:09 +0000

    [OOBE] Make API Keys Notice a component
    Add a new component <api-keys-notice-element> that serves the purpose of
    displaying a message to the user when API keys are missing. This removes
    the markup from oobe.html and styles in oobe.css to a separate file. It
    also adds some missing closure compiler information in some files.
    
    Bug: b:268351296
    Change-Id: I4f51b9f360a612c06675bd5b981c5934569c8c89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231781
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116481}

--- chrome/browser/resources/chromeos/login/oobe.js
+++ chrome/browser/resources/chromeos/login/oobe.js
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './test_api/test_api.js';
 import './components/common_styles/oobe_flex_layout_styles.css.js';
+import './components/api_keys_notice.js';
+// clang-format on
+
 
 import {assert} from '//resources/ash/common/assert.js';
 import {$} from '//resources/ash/common/util.js';
@@ -33,7 +36,7 @@ traceExecution(TraceEvent.FIRST_LINE_AFTER_IMPORTS);
  * yields true it will be added, otherwise it is skipped.
  * @param {Array<{tag: string, id: string}>}
  */
- function addScreensToMainContainer(screenList) {
+function addScreensToMainContainer(screenList) {
   const screenContainer = $('inner-container');
   for (const screen of screenList) {
     if (screen.condition) {
@@ -50,8 +53,8 @@ traceExecution(TraceEvent.FIRST_LINE_AFTER_IMPORTS);
       screenElement.classList.add(...screen.extra_classes);
     }
     screenContainer.appendChild(screenElement);
-    assert(!!$(screen.id).shadowRoot,
-           `Error! No shadow root in <${screen.tag}>`);
+    assert(
+        !!$(screen.id).shadowRoot, `Error! No shadow root in <${screen.tag}>`);
   }
 }
 
