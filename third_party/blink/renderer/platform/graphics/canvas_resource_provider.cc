5ed9da1df6e8efcdf4eff0dbb8bfa860035bd2e1
Yi <yiyix@google.com>
2023-03-13 19:26:05 +0000

    Fix Clusterfuzz bug: nullptr resource in WillDraw
    The resource allocation could be released in |EndWriteAccess|, so using
    the resource afterwards resulted in nullptr error. In this cl, I added
    a nullptr check for |resource|.
    
    Bug: 1423680
    
    Change-Id: I662f83247cac88a9765480a554ddf0c177f113e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334685
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Yi Xu <yiyix@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116528}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -496,7 +496,9 @@ class CanvasResourceProviderSharedImage : public CanvasResourceProvider {
     else
       EndWriteAccess();
 
-    resource()->WillDraw();
+    if (resource()) {
+      resource()->WillDraw();
+    }
   }
 
   void WillDraw() override { WillDrawInternal(true); }
