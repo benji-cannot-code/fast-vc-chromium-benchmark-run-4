b958fbf319c574552ca092c17cafbd13b423eb8b
Denalex Orakwue <corakwue@google.com>
2023-03-13 17:34:45 +0000

    [Eche] Create AccessibilityTreeConverter interface
    Bug: b/267373353
    Change-Id: Id567ca035c2838864fca6f6c80d02b38e7af764b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317866
    Reviewed-by: Abbas Nayebi <nayebi@google.com>
    Auto-Submit: Denalex Orakwue <corakwue@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Denalex Orakwue <corakwue@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116467}

---
+++ ash/webui/eche_app_ui/accessibility_tree_converter.cc
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/webui/eche_app_ui/accessibility_tree_converter.h"
+
+namespace ash::eche_app {
+AccessibilityTreeConverter::AccessibilityTreeConverter() = default;
+AccessibilityTreeConverter::~AccessibilityTreeConverter() = default;
+}  // namespace ash::eche_app
