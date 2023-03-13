170a12c8071d77518d3801cff1381d58261dc1e4
Stefan Zager <szager@chromium.org>
2023-03-13 17:08:07 +0000

    Port STACK_ALLOCATED to base/, with clang plugin checks
    Change-Id: Ibcae800f7757dae9063c39ad8f1cc4660e8ec80c
    
    Bug: chromium:1423429
    Change-Id: Ibcae800f7757dae9063c39ad8f1cc4660e8ec80c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4308073
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116435}

--- third_party/blink/renderer/platform/wtf/gc_plugin.h
+++ third_party/blink/renderer/platform/wtf/gc_plugin.h
@@ -11,14 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // reason must be provided as an argument. In most cases this will be a bug id
 // where the bug describes what needs to happen to remove the GC_PLUGIN_IGNORE
 // again.
+//
+// Developer note: this macro must be kept in sync with the definition of
+// STACK_ALLOCATED_IGNORE in /base/memory/stack_allocated.h.
 #if defined(__clang__)
-#define STACK_ALLOCATED_IGNORE(reason) \
-  __attribute__((annotate("stack_allocated_ignore")))
 #define GC_PLUGIN_IGNORE(reason)                     \
   __attribute__((annotate("blink_gc_plugin_ignore"), \
                  annotate("stack_allocated_ignore")))
 #else  // !defined(__clang__)
-#define STACK_ALLOCATED_IGNORE(reason)
 #define GC_PLUGIN_IGNORE(reason)
 #endif  // !defined(__clang__)
 
