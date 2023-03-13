dcfe683092fe8dc280c50f7157177239939f2e8b
Kevin Ellis <kevers@google.com>
2023-03-13 15:10:09 +0000

    Fix NOTREACHED in SnapContainerData
    Bug: 1418246, 1423422
    Change-Id: Ie4dbeb865a9985ac7cdedfdecc9dbe61934d2e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321766
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116377}

--- third_party/blink/web_tests/external/wpt/css/css-scroll-snap/support/common.js
+++ third_party/blink/web_tests/external/wpt/css/css-scroll-snap/support/common.js
@@ -96,3 +96,16 @@ function waitForScrollTo(eventTarget, getValue, targetValue) {
       eventTarget.addEventListener('scroll', scrollListener);
   });
 }
+
+function waitForNextFrame() {
+  return new Promise(resolve => {
+    const start = performance.now();
+    requestAnimationFrame(frameTime => {
+      if (frameTime < start) {
+        requestAnimationFrame(resolve);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
