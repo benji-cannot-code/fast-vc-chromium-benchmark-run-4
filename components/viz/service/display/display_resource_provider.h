2c72911e105ef2fa3594bffd78c35c80cd66a1a8
Colin Blundell <blundell@chromium.org>
2023-03-13 17:29:25 +0000

    [//components/viz] Remove unneeded and problematic gl2.h include
    This include is blocking merging hardware_renderer.cc into
    hardware_renderer_viz.cc (see discussion in [1]). Fortunately, it isn't
    needed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4331599
    
    Bug: 1423817
    Change-Id: Ia8642f90eb958d2b96173e76e430a84f225dceeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334300
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116460}

--- components/viz/service/display/display_resource_provider.h
+++ components/viz/service/display/display_resource_provider.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/resource_fence.h"
 #include "components/viz/service/viz_service_export.h"
 #include "gpu/command_buffer/common/sync_token.h"
-#include "third_party/khronos/GLES2/gl2.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
 
