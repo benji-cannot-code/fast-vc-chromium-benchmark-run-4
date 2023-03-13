b5417ee225df01197cc658139dcb67c4cd909319
Paul Semel <paulsemel@chromium.org>
2023-03-13 13:59:26 +0000

    blink: replace mojo::remote by HeapMojoRemote in FontAccess
    We recently forbid the usage of `mojo::Remote` in garbage collected
    objects to avoid any possible issues with use-after-poison. This CL
    fixes one of the occurrences of this class in the code base.
    
    Bug: 1381979
    Change-Id: If33c03e2d6f25a37831724c35f3dd7993e8de51c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315808
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116344}

--- third_party/blink/renderer/modules/font_access/font_access.h
+++ third_party/blink/renderer/modules/font_access/font_access.h
@@ -9,12 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/read_only_shared_memory_region.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/font_access/font_access.mojom-blink.h"
-#include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
-#include "third_party/blink/renderer/platform/wtf/gc_plugin.h"
 
 namespace blink {
 
@@ -25,7 +24,6 @@ class ScriptPromise;
 class ScriptPromiseResolver;
 
 class FontAccess final : public GarbageCollected<FontAccess>,
-                         public ExecutionContextLifecycleObserver,
                          public Supplement<LocalDOMWindow> {
  public:
   static const char kSupplementName[];
@@ -33,9 +31,6 @@ class FontAccess final : public GarbageCollected<FontAccess>,
   explicit FontAccess(LocalDOMWindow* window);
 
   void Trace(blink::Visitor* visitor) const override;
-
-  // ExecutionContextLifecycleObserver:
-  void ContextDestroyed() override;
 
   // Web-exposed interface:
   static ScriptPromise queryLocalFonts(ScriptState* script_state,
@@ -63,8 +58,7 @@ class FontAccess final : public GarbageCollected<FontAccess>,
 
   void OnDisconnect();
 
-  GC_PLUGIN_IGNORE("https://crbug.com/1381979")
-  mojo::Remote<mojom::blink::FontAccessManager> remote_;
+  HeapMojoRemote<mojom::blink::FontAccessManager> remote_;
 };
 
 }  // namespace blink
