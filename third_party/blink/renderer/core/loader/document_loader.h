78a434b60f218a7fcdebe41c74562575c62145ef
Tsuyoshi Horo <horo@chromium.org>
2023-03-13 02:43:44 +0000

    Refactor flags for PreloadHelper::LoadLinksFromHeader()
    Two flags (CanLoadResources and MediaPreloadPolicy) are currently used
    to determine the behavior of PreloadHelper::LoadLinksFromHeader(). But
    the logic is a little more complicated.
    To make the behavior simpler, this CL replaces these flags with a new flag named LoadLinksFromHeaderMode.
    
    * Current logic
    
    For handling link headers on document, LoadLinksFromHeader() is called 3
    times:
      [1] Before commit from DocumentLoader::StartLoadingInternal().
      [2] After commit without viewport from
          DocumentLoader::CreateParserPostCommit().
      [3] After commit with viewport from
          HTMLDocumentParser::ProcessPreloadData()
    
    And these flags are set as following:
          CanLoadResources    MediaPreloadPolicy
      [1] kDoNotLoadResources kLoadAll
      [2] kOnlyLoadResources  kOnlyLoadNonMedia
      [3] kOnlyLoadResources  kOnlyLoadMedia
    
    So network hints (DnsPrefetch and Preconnect) are triggered at [1].
    And resource loads (Preload, Prefetch and  ModulePreload) for non
    viewport dependent resources are triggered at [2]. And resource loads
    for viewport dependent resources are triggered at [3].
    
    For handling link headers on subresources, the flags are set as
    following:
                                   CanLoadResources       MediaPreloadPolicy
      From Memory Cache    : [4]  kDoNotLoadResources          kLoadAll
      Not from Memory Cache: [5]  kLoadResourcesAndPreconnect  kLoadAll
    
    So network hints are triggered for both [4] and [5]. And resource loads
    are triggered only for [5].
    
    * New logic
    
    This CL introduces a new flag LoadLinksFromHeaderMode. This is used to
    determine if network hints can be triggered and if resource loading can
    be triggered inside PreloadHelper::LoadLinksFromHeader() method.
    
    
    Low-Coverage-Reason: Refactoring.
    Change-Id: I9dfab552578c6337340ac8fba87d2b9cf441f7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327853
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116218}

--- third_party/blink/renderer/core/loader/document_loader.h
+++ third_party/blink/renderer/core/loader/document_loader.h
@@ -319,7 +319,7 @@ class CORE_EXPORT DocumentLoader : public GarbageCollected<DocumentLoader>,
   enum State { kNotStarted, kProvisional, kCommitted, kSentDidFinishLoad };
 
   void DispatchLinkHeaderPreloads(const ViewportDescription*,
-                                  PreloadHelper::MediaPreloadPolicy);
+                                  PreloadHelper::LoadLinksFromHeaderMode);
 
   void LoadFailed(const ResourceError&);
 
