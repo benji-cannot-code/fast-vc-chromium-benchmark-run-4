920ed6e9c6b5ec90408c799d5af51d3ddacd73ce
Christian Biesinger <cbiesinger@chromium.org>
2023-03-13 21:05:20 +0000

    Implement selectAccount and cancelDialog in the FedCM domain
    This will later be exposed to webdriver so that browser automation can
    select an account or cancel the dialog when a FedCM dialog is shown.
    
    I have made FedCmHandler a friend class because OnDialogDismissed
    and OnAccountSelected are not public.
    
    Bug: 1356159
    Change-Id: Ie7009f030e8e6a95a5d14e49335623e1273f728d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317667
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116588}

--- content/browser/devtools/protocol/fedcm_handler.h
+++ content/browser/devtools/protocol/fedcm_handler.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 class FederatedAuthRequestImpl;
 class FederatedAuthRequestPageData;
+struct IdentityProviderData;
 }  // namespace content
 
 namespace content::protocol {
@@ -45,12 +46,18 @@ class FedCmHandler : public DevToolsDomainHandler, public FedCm::Backend {
   // FedCm::Backend
   DispatchResponse Enable() override;
   DispatchResponse Disable() override;
+  DispatchResponse SelectAccount(const String& in_dialogId,
+                                 int in_accountIndex) override;
+  DispatchResponse DismissDialog(const String& in_dialogId) override;
 
   FederatedAuthRequestPageData* GetPageData();
   FederatedAuthRequestImpl* GetFederatedAuthRequest();
+  const std::vector<IdentityProviderData>* GetIdentityProviderData(
+      FederatedAuthRequestImpl* auth_request);
 
   RenderFrameHostImpl* frame_host_ = nullptr;
   std::unique_ptr<FedCm::Frontend> frontend_;
+  std::string dialog_id_;
   bool enabled_ = false;
 };
 
--- third_party/blink/web_tests/http/tests/inspector-protocol/fedcm/fedcm-dismiss-dialog.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fedcm/fedcm-dismiss-dialog.js
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   const {page, session, dp} =
       await testRunner.startBlank(
-          "Check that the dialogShown event works after enabling the " +
-          "FedCm domain");
+          "Check that the dismissing a FedCm dialog works");
 
   await page.navigate(
       "https://devtools.test:8443/inspector-protocol/fedcm/resources/dialog-shown-event.https.html");
@@ -11,14 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.FedCm.enable();
 
   const dialogPromise = session.evaluateAsync("triggerDialog()");
-  let msg = await dp.FedCm.onceDialogShown();
-  if (msg.error) {
-    testRunner.log(msg.error);
-  } else {
-    testRunner.log(msg.params.accounts, "accounts: ");
-    dp.FedCm.selectAccount({dialogId: msg.params.dialogId, accountIndex: 0});
-    const token = await dialogPromise;
-    testRunner.log("token: " + token);
-  }
+  const msg = await dp.FedCm.onceDialogShown();
+  dp.FedCm.dismissDialog({dialogId: msg.params.dialogId});
+  // This should be a NetworkError
+  testRunner.log(await dialogPromise);
   testRunner.completeTest();
 })
