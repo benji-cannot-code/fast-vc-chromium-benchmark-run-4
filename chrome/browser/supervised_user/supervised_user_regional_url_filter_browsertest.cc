b384484d7563b75ea2666f2f8510e2210c00bf99
Mustafa Emre Acer <meacer@chromium.org>
2023-03-13 22:04:01 +0000

    Disable HTTPS Upgrades in supervised user tests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    R=courtneywong@chromium.org
    
    Bug: 1394910
    Change-Id: I93287b2a2d06101c5dbcd02ee7be553748941647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335987
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116625}

--- chrome/browser/supervised_user/supervised_user_regional_url_filter_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_regional_url_filter_browsertest.cc
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_piece_forward.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ash/login/test/logged_in_user_mixin.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/supervised_user/kids_chrome_management/kids_chrome_management_client_factory.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -54,7 +56,12 @@ constexpr base::StringPiece kRegionCode = "jp";
 class SupervisedUserRegionalURLFilterTest
     : public MixinBasedInProcessBrowserTest {
  public:
-  SupervisedUserRegionalURLFilterTest() = default;
+  SupervisedUserRegionalURLFilterTest() {
+    // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having to
+    // disable this feature.
+    feature_list_.InitAndDisableFeature(features::kHttpsUpgrades);
+  }
+
   ~SupervisedUserRegionalURLFilterTest() override = default;
 
  protected:
@@ -135,6 +142,7 @@ class SupervisedUserRegionalURLFilterTest
       embedded_test_server(), this};
 
  private:
+  base::test::ScopedFeatureList feature_list_;
   base::CallbackListSubscription create_services_subscription_;
 };
 
--- chrome/browser/supervised_user/supervised_user_url_filter_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_url_filter_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "chrome/browser/ash/login/test/logged_in_user_mixin.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
@@ -66,7 +68,11 @@ class SupervisedUserURLFilterTest : public MixinBasedInProcessBrowserTest {
     INTERSTITIAL_DONTPROCEED,
   };
 
-  SupervisedUserURLFilterTest() = default;
+  SupervisedUserURLFilterTest() {
+    // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having to
+    // disable this feature.
+    feature_list_.InitAndDisableFeature(features::kHttpsUpgrades);
+  }
   ~SupervisedUserURLFilterTest() override = default;
 
   bool ShownPageIsInterstitial(Browser* browser) {
@@ -136,6 +142,7 @@ class SupervisedUserURLFilterTest : public MixinBasedInProcessBrowserTest {
     run_loop.Run();  // Will go until ...Complete calls Quit.
   }
 
+  base::test::ScopedFeatureList feature_list_;
   SupervisedUserService* supervised_user_service_ = nullptr;
 
   ash::LoggedInUserMixin logged_in_user_mixin_{
