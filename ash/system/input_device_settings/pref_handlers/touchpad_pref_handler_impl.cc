ed4234bee65955e92ba04f2e94ce7ecf3418ec8b
David Padlipsky <dpad@google.com>
2023-03-14 19:09:57 +0000

    Settings Split: Fix spelling of "Retrieve" in pref handlers
    Bug: b/241965700
    Change-Id: I075e6ea2e79a136bd372e1567b02925d792f5742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334939
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117140}

--- ash/system/input_device_settings/pref_handlers/touchpad_pref_handler_impl.cc
+++ ash/system/input_device_settings/pref_handlers/touchpad_pref_handler_impl.cc
@@ -70,7 +70,7 @@ void TouchpadPrefHandlerImpl::InitializeTouchpadSettings(
     touchpad->settings = GetNewTouchpadSettings(pref_service, *touchpad);
   } else {
     touchpad->settings =
-        RetreiveTouchpadSettings(pref_service, *touchpad, *settings_dict);
+        RetrieveTouchpadSettings(pref_service, *touchpad, *settings_dict);
   }
   DCHECK(touchpad->settings);
 
@@ -136,7 +136,7 @@ mojom::TouchpadSettingsPtr TouchpadPrefHandlerImpl::GetNewTouchpadSettings(
   return GetDefaultTouchpadSettings();
 }
 
-mojom::TouchpadSettingsPtr TouchpadPrefHandlerImpl::RetreiveTouchpadSettings(
+mojom::TouchpadSettingsPtr TouchpadPrefHandlerImpl::RetrieveTouchpadSettings(
     PrefService* pref_service,
     const mojom::Touchpad& touchpad,
     const base::Value::Dict& settings_dict) {
