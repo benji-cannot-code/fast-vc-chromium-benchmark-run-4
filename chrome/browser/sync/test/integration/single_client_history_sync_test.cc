1be446d3845a856f61945038793f5b733753f072
Mustafa Emre Acer <meacer@chromium.org>
2023-03-14 11:40:06 +0000

    Disable HTTPS Upgrades in single client history sync tests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    R=treib@chromium.org
    
    Bug: 1424194
    Change-Id: I12410ce53ae0171724fde1a075be721489fc010c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335527
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116892}

--- chrome/browser/sync/test/integration/single_client_history_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_history_sync_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/test/integration/sync_test.h"
 #include "chrome/browser/sync/test/integration/typed_urls_helper.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/chrome_test_utils.h"
@@ -315,7 +316,11 @@ bool ServerHistoryMatchChecker::IsExitConditionSatisfied(std::ostream* os) {
 class SingleClientHistorySyncTest : public SyncTest {
  public:
   SingleClientHistorySyncTest() : SyncTest(SINGLE_CLIENT) {
-    features_.InitAndEnableFeature(syncer::kSyncEnableHistoryDataType);
+    features_.InitWithFeatures(
+        {syncer::kSyncEnableHistoryDataType},
+        // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having to
+        // disable this feature.
+        /*disabled_features=*/{features::kHttpsUpgrades});
   }
   ~SingleClientHistorySyncTest() override = default;
 
