175b6246442997953eaf5070f79112edd9341c27
Mustafa Emre Acer <meacer@chromium.org>
2023-03-14 09:50:33 +0000

    Disable HTTPS Upgrades in mirror browsertests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    R=sdefresne@chromium.org
    
    Bug: 1424194
    Change-Id: I8f36924000803f802af5881def0c896813592d8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335159
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116860}

--- chrome/browser/signin/mirror_browsertest.cc
+++ chrome/browser/signin/mirror_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_content_browser_client.h"
@@ -94,6 +95,11 @@ class ThrottleContentBrowserClient : public ChromeContentBrowserClient {
 // Subclass of DiceManageAccountBrowserTest with Mirror enabled.
 class MirrorBrowserTest : public InProcessBrowserTest {
  protected:
+  MirrorBrowserTest() {
+    // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having to
+    // disable this feature.
+    feature_list_.InitAndDisableFeature(features::kHttpsUpgrades);
+  }
   void RunExtensionConsentTest(extensions::WebAuthFlow::Partition partition,
                                bool expects_header) {
     net::EmbeddedTestServer https_server(net::EmbeddedTestServer::TYPE_HTTPS);
@@ -140,6 +146,8 @@ class MirrorBrowserTest : public InProcessBrowserTest {
     // https), but the test server runs on a random port.
     command_line->AppendSwitch(switches::kIgnoreGooglePortNumbers);
   }
+
+  base::test::ScopedFeatureList feature_list_;
 };
 
 // Verify the following items:
