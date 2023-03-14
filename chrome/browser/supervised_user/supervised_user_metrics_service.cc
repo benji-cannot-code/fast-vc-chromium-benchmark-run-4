f0f2f4f0637f3c6ea234c3913fd745d1d8eb4f59
Nohemi Fernandez <fernandex@chromium.org>
2023-03-14 13:48:28 +0000

    Move supervised user preferences to a dedicated list.
    Removing dependencies on chrome/common/ as a part of the migration of
    SupervisedUserPrefStore to components/.
    
    Bug: b/267471715
    Change-Id: Iea7c50799f0febe9b7d29005bfc6313830c31d45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4331858
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116939}

--- chrome/browser/supervised_user/supervised_user_metrics_service.cc
+++ chrome/browser/supervised_user/supervised_user_metrics_service.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/supervised_user/parental_control_metrics.h"
 #include "chrome/browser/supervised_user/supervised_user_service_factory.h"
-#include "chrome/common/pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
+#include "components/supervised_user/core/common/pref_names.h"
 #include "content/public/browser/browser_context.h"
 
 namespace {
--- chrome/browser/supervised_user/supervised_user_service_unittest.cc
+++ chrome/browser/supervised_user/supervised_user_service_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/supervised_user/core/common/features.h"
+#include "components/supervised_user/core/common/pref_names.h"
 #include "components/version_info/version_info.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_utils.h"
