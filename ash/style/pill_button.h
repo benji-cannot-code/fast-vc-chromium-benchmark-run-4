ba23c2b7758d972e8edbb064973cc7fbb85cc612
Xiaodan Zhu <zxdan@chromium.org>
2023-03-14 19:33:41 +0000

    cros_next: customize pill button colors and layout
    This cl makes PillButton accept color ID and allow clients to
    customize the layout.
    
    - Add interfaces to set background, text and icon colors with
    color ID,
    - Add background_color_id_, text_color_id_, and icon_color_id_.
    - Add a new ctor argument to set padding reduction for icon.
    - Add a padding_reduction_for_icon_ attribute which is used to
    calculate the preferred button size.
    - When calculate preferred button size, instead of using default
    label image spacing, we get the value from GetImageLabelSpacing
    in case the client customize this value.
    
    The effects are shown in the bug comments.
    
    Bug: b:272599388
    Change-Id: Iaab7fe460d0174df0b22ec327f809c70b9e4f42c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327805
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117157}

--- ash/style/pill_button.h
+++ ash/style/pill_button.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/color/color_id.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/metadata/view_factory.h"
 
@@ -21,6 +22,7 @@ class ASH_EXPORT PillButton : public views::LabelButton {
   METADATA_HEADER(PillButton);
 
   static constexpr int kPillButtonHorizontalSpacing = 16;
+  static constexpr int kPaddingReductionForIcon = 4;
 
   // The PillButton style is defined with 4 features:
   // 1. Color variant defines which background, text, and icon color scheme to
@@ -163,13 +165,15 @@ class ASH_EXPORT PillButton : public views::LabelButton {
   // `rounded_highlight_path` is true. This is special handlings for buttons
   // inside the old notifications UI, might can be removed once
   // `kNotificationsRefresh` is fully launched.
-  explicit PillButton(PressedCallback callback = PressedCallback(),
-                      const std::u16string& text = std::u16string(),
-                      Type type = Type::kDefaultWithoutIcon,
-                      const gfx::VectorIcon* icon = nullptr,
-                      int horizontal_spacing = kPillButtonHorizontalSpacing,
-                      bool use_light_colors = false,
-                      bool rounded_highlight_path = true);
+  explicit PillButton(
+      PressedCallback callback = PressedCallback(),
+      const std::u16string& text = std::u16string(),
+      Type type = Type::kDefaultWithoutIcon,
+      const gfx::VectorIcon* icon = nullptr,
+      int horizontal_spacing = kPillButtonHorizontalSpacing,
+      bool use_light_colors = false,
+      bool rounded_highlight_path = true,
+      int padding_reduction_for_icon = kPaddingReductionForIcon);
   PillButton(const PillButton&) = delete;
   PillButton& operator=(const PillButton&) = delete;
   ~PillButton() override;
@@ -186,8 +190,11 @@ class ASH_EXPORT PillButton : public views::LabelButton {
   // this only when the button wants to have different colors from the default
   // ones.
   void SetBackgroundColor(const SkColor background_color);
+  void SetBackgroundColorId(ui::ColorId background_color_id);
   void SetButtonTextColor(const SkColor text_color);
+  void SetButtonTextColorId(ui::ColorId text_color_id);
   void SetIconColor(const SkColor icon_color);
+  void SetIconColorId(ui::ColorId icon_color_id);
   void SetPillButtonType(Type type);
 
   // Sets the button's label to use the default label font, which is smaller
@@ -220,11 +227,16 @@ class ASH_EXPORT PillButton : public views::LabelButton {
   // The flag that indicates if highlight path is used for focus ring.
   const bool rounded_highlight_path_;
 
-  // Customized value for the button's background color, text's color and icon's
-  // color.
+  // The padding reduced by icon.
+  int padding_reduction_for_icon_;
+
+  // Custom colors and color IDs.
   absl::optional<SkColor> background_color_;
+  absl::optional<ui::ColorId> background_color_id_;
   absl::optional<SkColor> text_color_;
+  absl::optional<ui::ColorId> text_color_id_;
   absl::optional<SkColor> icon_color_;
+  absl::optional<ui::ColorId> icon_color_id_;
 
   // Called to update background color when the button is enabled/disabled.
   base::CallbackListSubscription enabled_changed_subscription_;
@@ -232,8 +244,11 @@ class ASH_EXPORT PillButton : public views::LabelButton {
 
 BEGIN_VIEW_BUILDER(ASH_EXPORT, PillButton, views::LabelButton)
 VIEW_BUILDER_PROPERTY(const SkColor, BackgroundColor)
-VIEW_BUILDER_PROPERTY(const SkColor, TextColor)
+VIEW_BUILDER_PROPERTY(ui::ColorId, BackgroundColorId)
+VIEW_BUILDER_PROPERTY(const SkColor, ButtonTextColor)
+VIEW_BUILDER_PROPERTY(ui::ColorId, ButtonTextColorId)
 VIEW_BUILDER_PROPERTY(const SkColor, IconColor)
+VIEW_BUILDER_PROPERTY(ui::ColorId, IconColorId)
 VIEW_BUILDER_PROPERTY(PillButton::Type, PillButtonType)
 END_VIEW_BUILDER
 
