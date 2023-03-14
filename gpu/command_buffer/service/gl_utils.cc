3344b849b16c6e6bb3880beb90a6419e94106b97
Robert Mader <robert.mader@collabora.com>
2023-03-14 15:46:18 +0000

    gpu: Limit DRM format/modifier discovery to chromeos-ash (Exo)
    This was intended only for builds with Exo, i.e. not chromeos-lacros.
    Apart from avoiding unnecessary work an lacros, this allows us to drop a
    blocklist entry for an outdated and broken driver.
    
    Bug: b:224580219
    Change-Id: I144da076dc2bc7f1ccd414dc91b5a9c74b21a82f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334459
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116996}

--- gpu/command_buffer/service/gl_utils.cc
+++ gpu/command_buffer/service/gl_utils.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/texture_manager.h"
 #include "ui/gl/gl_version_info.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ui/gl/gl_surface_egl.h"
 #endif
 
@@ -281,7 +281,7 @@ void PopulateNumericCapabilities(Capabilities* caps,
   }
 }
 
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 void PopulateDRMCapabilities(Capabilities* caps,
                              const FeatureInfo* feature_info) {
   DCHECK(caps != nullptr);
