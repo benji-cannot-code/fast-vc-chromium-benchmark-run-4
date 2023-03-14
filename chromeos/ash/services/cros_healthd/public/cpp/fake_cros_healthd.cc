dd2654f2dbda8d32fd6484a571ee59423fe22f1d
Chung-Sheng Wu <chungsheng@google.com>
2023-03-14 04:50:07 +0000

    cros_healthd: Sync event supportability API mojom
    This sync the mojom change landed in CL:4306257.
    
    Bug: b:271514913
    Test: autoninja -C out/Default chrome
    Change-Id: I9609995baf88bdccc70b714f42d6db6671a6ae18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328373
    Commit-Queue: Chung-sheng Wu <chungsheng@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116783}

--- chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.cc
+++ chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.cc
@@ -739,6 +739,12 @@ void FakeCrosHealthd::AddEventObserver(
   it->second.Add(std::move(observer));
 }
 
+void FakeCrosHealthd::IsEventSupported(
+    ash::cros_healthd::mojom::EventCategoryEnum category,
+    IsEventSupportedCallback callback) {
+  std::move(callback).Run(nullptr);
+}
+
 void FakeCrosHealthd::ProbeTelemetryInfo(
     const std::vector<mojom::ProbeCategoryEnum>& categories,
     ProbeTelemetryInfoCallback callback) {
--- chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h
+++ chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_diagnostics.mojom.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_events.mojom.h"
+#include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_exception.mojom.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
 #include "chromeos/services/network_health/public/mojom/network_health.mojom.h"
 #include "chromeos/services/network_health/public/mojom/network_health_types.mojom-forward.h"
@@ -312,6 +313,8 @@ class FakeCrosHealthd final : public mojom::CrosHealthdDiagnosticsService,
       ash::cros_healthd::mojom::EventCategoryEnum category,
       mojo::PendingRemote<ash::cros_healthd::mojom::EventObserver> observer)
       override;
+  void IsEventSupported(ash::cros_healthd::mojom::EventCategoryEnum category,
+                        IsEventSupportedCallback callback) override;
 
   // CrosHealthdProbeService overrides:
   void ProbeTelemetryInfo(
