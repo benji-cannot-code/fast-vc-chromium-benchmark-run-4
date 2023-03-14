fc910b73ebc676830a2b8d19fa439614db693ff2
Mustafa Emre Acer <meacer@chromium.org>
2023-03-14 16:07:44 +0000

    Disable HTTPS Upgrades in DIPS bounce detector tests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. Original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    Bug: 1424194
    Change-Id: I03ee3df4a175ee731d1e331807501d458502e21e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336064
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117007}

--- chrome/browser/dips/dips_bounce_detector_browsertest.cc
+++ chrome/browser/dips/dips_bounce_detector_browsertest.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/escape.h"
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/dips/dips_test_utils.h"
 #include "chrome/browser/dips/dips_utils.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "content/public/browser/cookie_access_details.h"
 #include "content/public/browser/navigation_handle.h"
@@ -152,6 +154,16 @@ WEB_CONTENTS_USER_DATA_KEY_IMPL(WCOCallbackLogger);
 
 class DIPSBounceDetectorBrowserTest : public PlatformBrowserTest {
  protected:
+  DIPSBounceDetectorBrowserTest() {
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{},
+        /*disabled_features=*/{
+            // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having
+            // to disable this feature.
+            features::kHttpsUpgrades,
+        });
+  }
+
   void SetUpOnMainThread() override {
     ASSERT_TRUE(embedded_test_server()->Start());
     host_resolver()->AddRule("a.test", "127.0.0.1");
@@ -216,6 +228,8 @@ class DIPSBounceDetectorBrowserTest : public PlatformBrowserTest {
   }
 
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+
   raw_ptr<DIPSWebContentsObserver, DanglingUntriaged> web_contents_observer_ =
       nullptr;
 };
