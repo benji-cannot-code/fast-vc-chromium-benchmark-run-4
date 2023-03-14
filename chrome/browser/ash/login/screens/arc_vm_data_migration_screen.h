59d45140e7e6cb3de1581491b3db7925d2953c8a
Youkichi Hosoi <youkichihosoi@chromium.org>
2023-03-14 23:20:06 +0000

    arc: Calculate /data migration dst size based on Android /data size
    When creating a new disk image, Concierge sets its size based on the
    free disk space unless a specific value is requested by the caller.
    
    Currently we do not specify the disk image size of the destination of
    ARCVM /data migration. As a result, the disk image can be smaller than
    the migration source (existing Android /data) when the source size is
    large compared to the free disk space.
    
    In order to avoid out-of-space migration failures, we need to specify a
    large-enough destination disk image size.
    
    From our experiments, given the size of existing Android /data A, the
    minimum required disk image size for the destination of the migration m
    can be approximated (to the safer side) by:
      m = floor(1.1 * A) + (reserved disk space 128 MB).
    
    Note that the above m is just enough space for the migration, and ARCVM
    needs more disk space e.g. to install further apps.
    
    The actual disk image size requested to Concierge is calculated using
    the free disk space F as:
      max{x : multiple of block size 4096 bytes | x <= m + floor(0.9 * F)}.
    
    Here, the additional disk space floor(0.9 * F) is (somewhat arbitrarily)
    taken from the default disk image size set by Concierge.
    
    Accordingly, we adjust the size of required free space as:
      max{x : multiple of 512 MB | x <= (m - A) + 1 GB}.
    
    Bug: b:271062919
    Test: unit_tests --gtest_filter="ArcVmDataMigrationScreenTest.*"
    Test: Confirm that the migration succeeds even when the size of existing
    Test: Android /data is larger than the available free disk space.
    
    Change-Id: Ia5c99c22b90cc4767713ce010ed9d85297315e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4325967
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Youkichi Hosoi <youkichihosoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117269}

--- chrome/browser/ash/login/screens/arc_vm_data_migration_screen.h
+++ chrome/browser/ash/login/screens/arc_vm_data_migration_screen.h
@@ -63,6 +63,9 @@ class ArcVmDataMigrationScreen : public BaseScreen,
 
   void OnGetFreeDiskSpace(absl::optional<int64_t> reply);
 
+  void OnGetAndroidDataSizeResponse(uint64_t free_disk_space,
+                                    absl::optional<int64_t> response);
+
   void CheckBatteryState();
 
   // chromeos::PowerManagerClient::Observer override:
@@ -114,6 +117,8 @@ class ArcVmDataMigrationScreen : public BaseScreen,
 
   ArcVmDataMigrationScreenView::UIState current_ui_state_ =
       ArcVmDataMigrationScreenView::UIState::kLoading;
+
+  uint64_t disk_size_ = 0;
 
   double battery_percent_ = 100.0;
   bool is_connected_to_charger_ = true;
