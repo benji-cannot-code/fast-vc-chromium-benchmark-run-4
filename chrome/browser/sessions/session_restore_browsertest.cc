d49a0379d99da890233c7fadaa1de4a034bf1c75
Mustafa Emre Acer <meacer@chromium.org>
2023-03-14 17:55:31 +0000

    Disable HTTPS Upgrades in session restore tests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    R=sky@chromium.org
    
    Bug: 1424194
    Change-Id: I4802ce0f13f5ef21ff37afb34d456629c5f7d9d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336516
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117092}

--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_tab_helper.h"
 #include "chrome/common/chrome_constants.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -167,11 +168,20 @@ class SessionRestoreTest : public InProcessBrowserTest {
  public:
   SessionRestoreTest() {
 #if !BUILDFLAG(GOOGLE_CHROME_BRANDING)
-    // Disable What's New for non-branded builds where the welcome page will be
-    // disabled. Otherwise the bots may run with a configuration (What's New
-    // enabled + Welcome disabled) that does not actually occur in production,
-    // and causes tests to flake.
-    scoped_feature_list_.InitAndDisableFeature(features::kChromeWhatsNewUI);
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{},
+        /*disabled_features=*/{// Disable What's New for non-branded builds
+                               // where the welcome page will be
+                               // disabled. Otherwise the bots may run with a
+                               // configuration (What's New
+                               // enabled + Welcome disabled) that does not
+                               // actually occur in production,
+                               // and causes tests to flake.
+                               features::kChromeWhatsNewUI,
+                               // TODO(crbug.com/1394910): Use HTTPS URLs in
+                               // tests to avoid having to
+                               // disable this feature.
+                               features::kHttpsUpgrades});
 #endif  // !BUILDFLAG(GOOGLE_CHROME_BRANDING)
   }
   ~SessionRestoreTest() override = default;
