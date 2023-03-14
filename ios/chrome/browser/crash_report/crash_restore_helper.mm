c99a499866e0c4bf9dcff2edb2e2ea3bbdc75d04
Louis Romero <lpromero@google.com>
2023-03-14 22:33:58 +0000

    Remove unnecessary dependency
    This was added in crrev/c/4192124 but never used.
    
    Bug: 1412440
    Change-Id: I74ad513b1abdb290613660105c7eda91ac02cc31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4337924
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117243}

--- ios/chrome/browser/crash_report/crash_restore_helper.mm
+++ ios/chrome/browser/crash_report/crash_restore_helper.mm
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sessions/session_service_ios.h"
 #import "ios/chrome/browser/sessions/session_window_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
-#import "ios/chrome/browser/tabs/inactive_tabs/utils.h"
 #import "ios/chrome/browser/ui/icons/symbols.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
 #import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
