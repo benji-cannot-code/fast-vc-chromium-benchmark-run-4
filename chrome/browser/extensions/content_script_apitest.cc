86833fcc3a8c465503e76836424b8a4fa17e6253
Mustafa Emre Acer <meacer@chromium.org>
2023-03-14 01:13:35 +0000

    Modify content script tests to work with the HTTPS Upgrades feature
    This CL modifies ContentScriptApiTest.ContentScriptAllFrames to load
    a URL with and without HTTPS Upgrades enabled. Both cases should pass.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    R=reillyg@chromium.org
    
    Bug: 1424194
    Change-Id: I675cee008883dbfd16069106ae08fb27b311d777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336417
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116725}

--- chrome/browser/extensions/content_script_apitest.cc
+++ chrome/browser/extensions/content_script_apitest.cc
@@ -23,11 +23,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_with_management_policy_apitest.h"
 #include "chrome/browser/extensions/identifiability_metrics_test_util.h"
 #include "chrome/browser/search/search.h"
+#include "chrome/browser/ssl/https_upgrades_interceptor.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/search/ntp_test_utils.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/javascript_dialogs/tab_modal_dialog_manager.h"
@@ -40,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/content_mock_cert_verifier.h"
 #include "content/public/test/prerender_test_util.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
@@ -129,7 +133,39 @@ class ContentScriptApiTest : public ExtensionApiTest {
   void SetUpOnMainThread() override {
     ExtensionApiTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
+
+    // Serve valid HTTPS from test server.
+    mock_cert_verifier_.mock_cert_verifier()->set_default_result(net::OK);
+    https_test_server_ = std::make_unique<net::EmbeddedTestServer>(
+        net::EmbeddedTestServer::TYPE_HTTPS);
+    https_test_server_->SetSSLConfig(net::EmbeddedTestServer::CERT_OK);
+    https_test_server_->ServeFilesFromSourceDirectory(GetChromeTestDataDir());
+    ASSERT_TRUE(https_test_server_->Start());
+
+    HttpsUpgradesInterceptor::SetHttpsPortForTesting(
+        https_test_server_->port());
+    HttpsUpgradesInterceptor::SetHttpPortForTesting(
+        embedded_test_server()->port());
   }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    mock_cert_verifier_.SetUpCommandLine(command_line);
+  }
+
+  void SetUpInProcessBrowserTestFixture() override {
+    ExtensionApiTest::SetUpInProcessBrowserTestFixture();
+    mock_cert_verifier_.SetUpInProcessBrowserTestFixture();
+  }
+
+  void TearDownInProcessBrowserTestFixture() override {
+    ExtensionApiTest::TearDownInProcessBrowserTestFixture();
+    mock_cert_verifier_.TearDownInProcessBrowserTestFixture();
+  }
+
+ private:
+  content::ContentMockCertVerifier mock_cert_verifier_;
+  std::unique_ptr<net::EmbeddedTestServer> https_test_server_;
 };
 
 IN_PROC_BROWSER_TEST_F(ContentScriptApiTest, ContentScriptAllFrames) {
@@ -958,9 +994,25 @@ IN_PROC_BROWSER_TEST_F(ContentScriptApiTest, CannotScriptTheNewTabPage) {
       did_script_inject(browser()->tab_strip_model()->GetActiveWebContents()));
 
   // The extension should inject on "normal" urls.
-  GURL unprotected_url = embedded_test_server()->GetURL(
-      "example.com", "/extensions/test_file.html");
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), unprotected_url));
+
+  // Test on an HTTP URL. Disable HTTPS upgrades on example1.com for this test
+  // to work.
+  auto* prefs = browser()->profile()->GetPrefs();
+  base::Value::List allowlist;
+  allowlist.Append("example1.com");
+  prefs->SetList(prefs::kHttpAllowlist, std::move(allowlist));
+
+  GURL unprotected_url1 = embedded_test_server()->GetURL(
+      "example1.com", "/extensions/test_file.html");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), unprotected_url1));
+  EXPECT_TRUE(
+      did_script_inject(browser()->tab_strip_model()->GetActiveWebContents()));
+
+  // Test on an HTTPS URL. If HTTPS-Upgrades feature is enabled, this URL is
+  // upgraded to HTTPS.
+  GURL unprotected_url2 = embedded_test_server()->GetURL(
+      "example2.com", "/extensions/test_file.html");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), unprotected_url2));
   EXPECT_TRUE(
       did_script_inject(browser()->tab_strip_model()->GetActiveWebContents()));
 }
