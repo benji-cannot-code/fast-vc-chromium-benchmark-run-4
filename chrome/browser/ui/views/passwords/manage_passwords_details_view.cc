334ac17f994646e8ad036c005f9a76da9dfbb50a
rgod <rgod@google.com>
2023-03-14 10:40:40 +0000

    [Passwords] Add metrics for note editing in management bubble
    Adds the following metrics for note updates in the bubble:
    - Note edit button clicked
    - Note added
    - Note edited
    - Note deleted
    
    Bug: 1416246
    Change-Id: I0cf096d54d7f29108cbdcd50daaebe972445be0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315813
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116873}

--- chrome/browser/ui/views/passwords/manage_passwords_details_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_details_view.cc
@@ -197,6 +197,7 @@ std::unique_ptr<views::View> CreateNoteLabel(
                                    views::DISTANCE_RELATED_CONTROL_HORIZONTAL) -
                            2 * kIconSize;
   note_label->SetMaximumWidth(kNoteLabelMaxWidth);
+  note_label->SetID(static_cast<int>(ManagePasswordsViewIDs::kNoteLabel));
 
   int line_height = views::style::GetLineHeight(note_label->GetTextContext(),
                                                 note_label->GetTextStyle());
@@ -265,6 +266,7 @@ std::unique_ptr<views::View> CreateEditNoteRow(
       views::kFlexBehaviorKey,
       views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
                                views::MaximumFlexSizeRule::kUnbounded));
+  (*textarea)->SetID(static_cast<int>(ManagePasswordsViewIDs::kNoteTextarea));
   return row;
 }
 
@@ -433,6 +435,8 @@ void ManagePasswordsDetailsView::SwitchToEditNoteMode() {
   DCHECK(note_textarea_);
   on_activity_callback_.Run();
   note_textarea_->RequestFocus();
+  LogUserInteractionsInPasswordManagementBubble(
+      PasswordManagementBubbleInteractions::kNoteEditButtonClicked);
 }
 
 void ManagePasswordsDetailsView::OnUserInputChanged() {
--- chrome/browser/ui/views/passwords/manage_passwords_view_ids.h
+++ chrome/browser/ui/views/passwords/manage_passwords_view_ids.h
@@ -27,6 +27,8 @@ enum class ManagePasswordsViewIDs {
 
   // The following ids are for note in the details views.
   kEditNoteButton,
+  kNoteTextarea,
+  kNoteLabel,
 };
 
 }  // namespace password_manager
