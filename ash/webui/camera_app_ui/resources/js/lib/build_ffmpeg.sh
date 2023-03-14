46cb3fe331c5cf022431283a442c521c2fa49fb3
Kam Kwankajornkiet <kamchonlathorn@google.com>
2023-03-14 07:24:51 +0000

    Reland "CCA: Add h264 demuxer to FFmpeg"
    This is a reland of commit 187db51d0a318bf496b488318176b46b3ba48067
    
    Original change's description:
    > CCA: Add h264 demuxer to FFmpeg
    >
    > To record time-lapse video using WebCodecs approach, h264 demuxer is
    > added to FFmpeg to allow saving a video file from h264
    > EncodedVideoChunks. The new FFmpeg wasm file size increases 2 kb.
    >
    > Also add a patch to allow reading the video dimension defined from JS to
    > avoid using decoder to read first few frames to extract the metadata.
    >
    > Bug: b:236800499
    > Test: tast run <DUT> camera.CCAUIRecordVideo*
    > Change-Id: I062d3407b73f47e5db088fabc5c255b28ecf8dd6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4270315
    > Reviewed-by: Wei Lee <wtlee@chromium.org>
    > Reviewed-by: Shik Chen <shik@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1116784}
    
    Bug: b:236800499
    Change-Id: Iafc1f81ff365f67b09fb4b10c9f49180c2c3e969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336858
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116812}

--- ash/webui/camera_app_ui/resources/js/lib/build_ffmpeg.sh
+++ ash/webui/camera_app_ui/resources/js/lib/build_ffmpeg.sh
@@ -23,6 +23,7 @@ do_copy_ffmpeg() {
 
   # checkout specific commit to keep code base consistent to the patch
   # TODO(b/199980849): Fix pthread issue to be able to use latest FFMpeg update.
+  # TODO(b/199980849): Remove video_size patch once the PR is sent upstream.
   local build_commit=90810bb
   (
     cd $ffmpeg_dir;
@@ -86,6 +87,7 @@ do_configure() {
     --enable-swscale
     --enable-protocol=file
     --enable-protocol=pipe
+    --enable-demuxer=h264
     --enable-demuxer=matroska
     --enable-demuxer=rawvideo
     --enable-muxer=gif
