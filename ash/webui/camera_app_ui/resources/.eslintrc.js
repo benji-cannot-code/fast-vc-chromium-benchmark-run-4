2b2267709cda57f3a8fc1442cbfe657a334bb19b
Pi-Hsun Shih <pihsun@chromium.org>
2023-03-14 07:02:04 +0000

    CCA: Use ash_ts_library for CCA
    As the first step of migrating CCA to use Lit, use ash_ts_library
    instead of ts_library, which add material_web_components as the
    dependency of CCA. This allows us to use both Lit and
    material_web_components custom elements for CCA.
    
    The gradual migration to Lit will be done in future CLs.
    
    Bug: b:230703573
    Test: build chrome
    Change-Id: Idd1543c95ce7a9e69c5485cec93779c449ea3109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707291
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116809}

--- ash/webui/camera_app_ui/resources/.eslintrc.js
+++ ash/webui/camera_app_ui/resources/.eslintrc.js
@@ -739,6 +739,9 @@ module.exports = {
     'jsdoc/require-yields-check': 'error',
     'jsdoc/tag-lines': 'error',
     'jsdoc/valid-types': 'error',
+
+    'no-invalid-this': 'off',
+    '@typescript-eslint/no-invalid-this': 'error',
   }),
   overrides: [{
     files: ['**/*.ts'],
