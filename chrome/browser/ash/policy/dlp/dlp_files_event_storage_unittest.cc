f6f275c0e87fe881be794132572aafac6cc251bd
Luca Accorsi <accorsi@google.com>
2023-03-14 16:28:38 +0000

    dlp: Simplify DlpFilesEventStorage testing
    Replace DlpFilesEventStorage::SimulateElapsedTimeForTesting usages
    with TestMockTimeTaskRunner::FastForwardBy.
    
    Bug: b/273515166
    Test: tools/autotest.py -C out/Default ./chrome/browser/ash/policy/dlp/
    Change-Id: I2cfc7dc6e6cd76d52b3b24ecf3b2de03fc9c072e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338136
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117019}

--- chrome/browser/ash/policy/dlp/dlp_files_event_storage_unittest.cc
+++ chrome/browser/ash/policy/dlp/dlp_files_event_storage_unittest.cc
@@ -79,7 +79,7 @@ TEST_F(DlpFilesEventStorageTest, UpsertEvents) {
 
   ASSERT_THAT(storage.GetSizeForTesting(), 4);
 
-  storage.SimulateElapsedTimeForTesting(kCooldownTimeout / 2);
+  task_runner->FastForwardBy(kCooldownTimeout / 2);
 
   // Update
   ASSERT_FALSE(storage.StoreEventAndCheckIfItShouldBeReported(kInode1, dst1));
@@ -90,7 +90,7 @@ TEST_F(DlpFilesEventStorageTest, UpsertEvents) {
   ASSERT_THAT(storage.GetSizeForTesting(), 4);
 
   // Automatic removal
-  storage.SimulateElapsedTimeForTesting(kCooldownTimeout);
+  task_runner->FastForwardBy(kCooldownTimeout);
   ASSERT_THAT(storage.GetSizeForTesting(), 0);
 
   histogram_tester_.ExpectBucketCount(
