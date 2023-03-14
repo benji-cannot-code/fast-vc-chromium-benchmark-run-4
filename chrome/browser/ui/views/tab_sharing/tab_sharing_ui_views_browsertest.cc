ba66ff981e61a13568e6caecaa71b4425b3c3e11
Mustafa Emre Acer <meacer@chromium.org>
2023-03-14 09:08:24 +0000

    Disable HTTPS Upgrades in tab sharing tests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    R=guidou@chromium.org
    
    Bug: 1424194
    Change-Id: Iab9b30a10a6fb2d141f862caea145a615cd1a1cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336439
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116837}

--- chrome/browser/ui/views/tab_sharing/tab_sharing_ui_views_browsertest.cc
+++ chrome/browser/ui/views/tab_sharing/tab_sharing_ui_views_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_user_gesture_details.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -163,8 +164,13 @@ class TabSharingUIViewsBrowserTest
  public:
   TabSharingUIViewsBrowserTest()
       : favicons_used_for_switch_to_tab_button_(GetParam()) {
+    // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having to
+    // disable kHttpsUpgrades feature.
 #if BUILDFLAG(IS_CHROMEOS)
-    features_.InitAndEnableFeature(features::kTabCaptureBlueBorderCrOS);
+    features_.InitWithFeatures({features::kTabCaptureBlueBorderCrOS},
+                               {features::kHttpsUpgrades});
+#else
+    features_.InitAndDisableFeature(features::kHttpsUpgrades);
 #endif  // BUILDFLAG(IS_CHROMEOS)
   }
 
@@ -356,9 +362,7 @@ class TabSharingUIViewsBrowserTest
         std::vector<content::DesktopMediaID>{});
   }
 
-#if BUILDFLAG(IS_CHROMEOS)
   base::test::ScopedFeatureList features_;
-#endif  // BUILDFLAG(IS_CHROMEOS)
 
   const bool favicons_used_for_switch_to_tab_button_;
 
