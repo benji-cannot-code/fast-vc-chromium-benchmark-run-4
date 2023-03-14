0f12bddec57b28dba6a4c7b03aae6221c6b79e9b
Roman Sorokin <rsorokin@chromium.org>
2023-03-14 14:07:10 +0000

    [Code Health] Replace RunLoop with TestFuture
    Replace the usage of `base::RunLoop` with `base::test::TestFuture` in
    device_id_browsertest.cc.
    
    R=sinhak@chromium.org
    
    Bug: b/266783937
    Test: browser_tests --gtest_filter="*DeviceIDTest*"
    Change-Id: I1f17832917ac93d8074fc479eda2a0fae9dd9ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335539
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116947}

--- chrome/browser/ash/login/signin/device_id_browsertest.cc
+++ chrome/browser/ash/login/signin/device_id_browsertest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/path_service.h"
-#include "base/run_loop.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/ash/login/test/js_checker.h"
 #include "chrome/browser/ash/login/test/oobe_base_test.h"
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
@@ -58,7 +58,7 @@ class DeviceIDTest : public OobeBaseTest,
   }
 
   void SetUpOnMainThread() override {
-    user_removal_loop_ = std::make_unique<base::RunLoop>();
+    user_removal_signal_ = std::make_unique<base::test::TestFuture<void>>();
     OobeBaseTest::SetUpOnMainThread();
     LoadRefreshTokenToDeviceIdMap();
     user_manager::UserManager::Get()->AddObserver(this);
@@ -136,12 +136,14 @@ class DeviceIDTest : public OobeBaseTest,
 
   void RemoveUser(const AccountId& account_id) {
     ASSERT_TRUE(LoginScreenTestApi::RemoveUser(account_id));
-    user_removal_loop_->Run();
+    EXPECT_TRUE(user_removal_signal_->Wait());
   }
 
  private:
   void LocalStateChanged(user_manager::UserManager* manager) override {
-    user_removal_loop_->Quit();
+    if (user_removal_signal_ && !user_removal_signal_->IsReady()) {
+      user_removal_signal_->SetValue();
+    }
   }
 
   base::FilePath GetRefreshTokenToDeviceIdMapFilePath() const {
@@ -186,7 +188,7 @@ class DeviceIDTest : public OobeBaseTest,
     }
   }
 
-  std::unique_ptr<base::RunLoop> user_removal_loop_;
+  std::unique_ptr<base::test::TestFuture<void>> user_removal_signal_;
   FakeGaiaMixin fake_gaia_{&mixin_host_};
 };
 
