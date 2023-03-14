e1e8e5badeac0419e16ee1fa3e165d53b03c574e
Louis Romero <lpromero@google.com>
2023-03-14 16:01:13 +0000

    Fix SyncErrorBrowserAgent and disable it for inactive browser
    This CL fixes a remaining issue not addressed in crrev/c/4329140: when
    there are never any UI providers set, the unrealized web states are
    never observed, but can be removed from the observation list, which
    leads to a crash.
    This CL instead always observes unrealized web states.
    
    Since the inactive browser never sets UI providers, we disabled
    attaching to it a SyncErrorBrowserAgent altogether.
    
    Bug: 1422753
    Change-Id: Ia4e7c2fa17bbabb3c6d08be21befa3ca68c48809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338003
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117006}

--- ios/chrome/browser/main/browser_agent_util.h
+++ ios/chrome/browser/main/browser_agent_util.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 
-// Attaches browser agents to `browser`.
+// Attaches browser agents to `browser`. If the Browser is inactive, or off the
+// record, some of the browser agents won't be attached because they are unused.
 void AttachBrowserAgents(Browser* browser);
 
 #endif  // IOS_CHROME_BROWSER_MAIN_BROWSER_AGENT_UTIL_H_
