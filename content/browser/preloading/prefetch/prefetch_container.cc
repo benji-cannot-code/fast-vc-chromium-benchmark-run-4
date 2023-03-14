3b37680a15c844ab0ea33c00727b4b9ae906e791
Max Curran <curranmax@chromium.org>
2023-03-14 21:25:44 +0000

    Fix possible crash when serving a prefetch.
    The crash would occur if 1) PrefetchURLLoaderInterceptor was given a
    prefetch to serve that had received just the head of the response,
    2) PrefetchURLLoaderInterceptor was waiting on either the probe or
    cookie copy process to complete, and 3) the prefetch was completed with
    some error code, which would cause it to be marked as not servable and
    its streaming URL loader would be deleted. In this case, the
    PrefetchURLLoaderInterceptor would assume the prefetch was still
    servable and its streaming URL loader was still alive, resulting in a
    crash.
    
    This CL adds a check to ensure that the prefetch is still servable after
    waiting for the probe and cookie copy process to complete. Additionally,
    the CL delays updating the prefetch's status based on the probe result
    until we know the prefetch can actually be served.
    
    Bug: 1424246
    Change-Id: I959d796e2c7ebc4a50d4d45d4b1f441c8ac68114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338559
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117210}

--- content/browser/preloading/prefetch/prefetch_container.cc
+++ content/browser/preloading/prefetch/prefetch_container.cc
@@ -517,8 +517,6 @@ void PrefetchContainer::OnPrefetchProbeResult(
 
   switch (probe_result) {
     case PrefetchProbeResult::kNoProbing:
-      SetPrefetchStatus(PrefetchStatus::kPrefetchUsedNoProbe);
-      break;
     case PrefetchProbeResult::kDNSProbeSuccess:
     case PrefetchProbeResult::kTLSProbeSuccess:
       SetPrefetchStatus(PrefetchStatus::kPrefetchResponseUsed);
