a80dca0841406a8cbee1da8841d65317fd20d52c
Eric Sum <esum@google.com>
2023-03-14 19:29:09 +0000

    Add utilities for ambient video file paths.
    It'll be used in an upcoming CL.
    
    Bug: b:271180547
    Change-Id: Ia840ed81472cc9133e3a8c734c475c306c692692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338435
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117155}

--- ash/ambient/ambient_ui_settings_unittest.cc
+++ ash/ambient/ambient_ui_settings_unittest.cc
@@ -50,14 +50,14 @@ TEST_F(AmbientUiSettingsTest, PrefManagement) {
       AmbientUiSettings::ReadFromPrefService(test_pref_service_).theme(),
       Eq(AmbientTheme::kFloatOnBy));
 
-  AmbientUiSettings(AmbientTheme::kVideo, AmbientVideo::kVideoClouds)
+  AmbientUiSettings(AmbientTheme::kVideo, AmbientVideo::kClouds)
       .WriteToPrefService(test_pref_service_);
   EXPECT_THAT(
       AmbientUiSettings::ReadFromPrefService(test_pref_service_).theme(),
       Eq(AmbientTheme::kVideo));
   EXPECT_THAT(
       AmbientUiSettings::ReadFromPrefService(test_pref_service_).video(),
-      Eq(AmbientVideo::kVideoClouds));
+      Eq(AmbientVideo::kClouds));
 }
 
 TEST_F(AmbientUiSettingsTest, HandlesCorruptedPrefStorage) {
@@ -99,8 +99,7 @@ TEST_F(AmbientUiSettingsTest, CrashesWithInvalidSettings) {
 TEST_F(AmbientUiSettingsTest, ToString) {
   EXPECT_THAT(AmbientUiSettings().ToString(), Eq("SlideShow"));
   EXPECT_THAT(
-      AmbientUiSettings(AmbientTheme::kVideo, AmbientVideo::kVideoClouds)
-          .ToString(),
+      AmbientUiSettings(AmbientTheme::kVideo, AmbientVideo::kClouds).ToString(),
       Eq("Video.Clouds"));
 }
 
