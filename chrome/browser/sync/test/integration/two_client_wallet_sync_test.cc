ba98f9c40d1375716428e3af9ee12eed2f59a269
Scott Yoder <scottyoder@google.com>
2023-03-14 15:18:53 +0000

    Revert "Add AutofillLeipzigFeatureCompatibilityLaunch to field trial config."
    This reverts commit 440142a294ee7ed28a644869f9568d2d59fb3444.
    
    Reason for revert: This seems to be causing the ios-simulator-noncq builder to fail in some EG tests.  Example failure: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/32590/overview
    
    Original change's description:
    > Add AutofillLeipzigFeatureCompatibilityLaunch to field trial config.
    >
    > As per go/finch-best-practices, this CL adds the relevant feature flags
    > for this experiment to the field trial config. This enables them in
    > browser tests and will allow us to move to beta.
    >
    > Several Sync integration tests break due to this change. This is because
    > the experiment deprecates the SERVER_PROFILE -> LOCAL_PROFILE address
    > conversion. See go/autofill-deprecate-payment-address-conversion for
    > more context.
    > The affected tests all wait for the address conversion to complete,
    > which doesn't happen anymore (more precisely, the signal that it has
    > completed is sent - but no addresses are converted. So the relevant
    > EXPECTs fail. See crrev.com/c/4145702).
    >
    > This is resolved by disabling AutofillAccountProfilesUnionView in those
    > tests. Once the experiment is launched, all the SERVER_PROFILE-related
    > tests can be removed entirely, since this mechanism will be removed.
    > The only exception is DeleteServerCardMetadataWhenDataGetsRemoved. See
    > code comment.
    >
    > Bug: 1348294
    > Change-Id: I06a0e88618d3ff5c664d277fc739232777625e31
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335536
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1116911}
    
    Bug: 1348294
    Change-Id: I22f411aef61a8b9d6598682df5dfa777c335aea2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335210
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1116979}

--- chrome/browser/sync/test/integration/two_client_wallet_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_wallet_sync_test.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/test_autofill_clock.h"
-#include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_util.h"
 #include "components/sync/driver/sync_service_impl.h"
 #include "components/sync/test/fake_server_http_post_provider.h"
@@ -77,19 +76,6 @@ class TwoClientWalletSyncTest : public SyncTest {
 
  private:
   autofill::TestAutofillClock test_clock_;
-};
-
-// AutofillAccountProfilesUnionView deprecates support for server profiles.
-// TODO(crbug.com/1348294): Remove when launched.
-class TwoClientWalletServerAddressSyncTest : public TwoClientWalletSyncTest {
- public:
-  TwoClientWalletServerAddressSyncTest() {
-    features_.InitAndDisableFeature(
-        autofill::features::kAutofillAccountProfilesUnionView);
-  }
-
- private:
-  base::test::ScopedFeatureList features_;
 };
 
 IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest, UpdateCreditCardMetadata) {
@@ -213,8 +199,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
   EXPECT_EQ(kEvenLaterTime, credit_cards[0]->use_date());
 }
 
-IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
-                       UpdateServerAddressMetadata) {
+IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest, UpdateServerAddressMetadata) {
   GetFakeServer()->SetWalletData(
       {CreateSyncWalletCard(/*name=*/"card-1", /*last_four=*/"0001",
                             kDefaultBillingAddressID),
@@ -247,7 +232,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
   EXPECT_EQ(kLaterTime, server_addresses[0]->use_date());
 }
 
-IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
+IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
                        UpdateServerAddressMetadataWhileNotSyncing) {
   GetFakeServer()->SetWalletData(
       {CreateSyncWalletAddress(/*name=*/"address-1", /*company=*/"Company-1"),
@@ -287,7 +272,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
   EXPECT_EQ(kLaterTime, server_addresses[0]->use_date());
 }
 
-IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
+IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
                        UpdateServerAddressMetadataConflictsWhileNotSyncing) {
   GetFakeServer()->SetWalletData(
       {CreateSyncWalletAddress(/*name=*/"address-1", /*company=*/"Company-1"),
@@ -478,7 +463,7 @@ IN_PROC_BROWSER_TEST_F(
 }
 
 // Flaky. http://crbug.com/917498
-IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
+IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
                        ServerAddressConvertsToSameLocalAddress) {
   GetFakeServer()->SetWalletData(
       {CreateSyncWalletAddress(/*name=*/"address-1", /*company=*/"Company-1"),
@@ -510,13 +495,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
   EXPECT_TRUE(local_address_0.EqualsForSyncPurposes(*local_addresses_1[0]));
 }
 
-// TODO(crbug.com/1348294): This test doesn't actually depend on the
-// SERVER_PROFILE -> LOCAL_ADDRESS conversion (which is deprecated by
-// AutofillAccountProfilesUnionView). However, since `SetupSyncAndInitialize()`
-// waits for the conversion to complete, this test is also using the fixture
-// `TwoClientWalletServerAddressSyncTest`. Once AutofillAccountProfilesUnionView
-// has launched, SERVER_PROFILEs will be removed and the test can be simplified.
-IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
+IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
                        DeleteServerCardMetadataWhenDataGetsRemoved) {
   GetFakeServer()->SetWalletData(
       {CreateSyncWalletCard(/*name=*/"card-1", /*last_four=*/"0001",
@@ -567,7 +546,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
   EXPECT_EQ(1U, GetServerAddressesMetadata(1).size());
 }
 
-IN_PROC_BROWSER_TEST_F(TwoClientWalletServerAddressSyncTest,
+IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
                        DeleteServerAddressMetadataWhenDataGetsRemoved) {
   GetFakeServer()->SetWalletData(
       {CreateSyncWalletCard(/*name=*/"card-1", /*last_four=*/"0001",
