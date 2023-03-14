60f58b20bc0714b356b2ad2a6dcbbdc0e8329701
Christian Xu <christianxu@google.com>
2023-03-14 15:51:39 +0000

    [iOS][Omnibox] Fix row padding
    This CL makes sure that the trailing button is not added multiple times
    causing conflict in the updated constraints.
    
    Bug: 1412484
    Change-Id: I80c067d9781baee9dfca0cfdf6cd717a1c2ac1f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316006
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116999}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell.mm
@@ -178,7 +178,10 @@ BOOL IsMultilineSearchSuggestionEnabled() {
     if (self.contentView.subviews.count == 0) {
       [self setupLayout];
     }
-    if (self.suggestion.isAppendable || self.suggestion.isTabMatch) {
+    BOOL suggestionNeedsTrailingButton =
+        self.suggestion.isAppendable || self.suggestion.isTabMatch;
+
+    if (suggestionNeedsTrailingButton && !self.trailingButton.superview) {
       [self setupTrailingButtonLayout];
     }
     [self attachToLayoutGuides];
@@ -519,8 +522,6 @@ BOOL IsMultilineSearchSuggestionEnabled() {
     self.textTruncatingLabel.lineBreakMode = NSLineBreakByClipping;
     self.textTruncatingLabel.numberOfLines = 1;
   }
-  [self updateTextConstraints:IsMultilineSearchSuggestionEnabled() &&
-                              suggestion.isWrapping];
 
   // URLs have have special layout requirements.
   self.detailTruncatingLabel.displayAsURL = suggestion.isURL;
@@ -542,6 +543,8 @@ BOOL IsMultilineSearchSuggestionEnabled() {
   if (suggestion.isAppendable || suggestion.isTabMatch) {
     [self setupTrailingButton];
   }
+  [self updateTextConstraints:IsMultilineSearchSuggestionEnabled() &&
+                              suggestion.isWrapping];
 
   self.leadingIconView.highlighted = self.highlighted;
   self.trailingButton.tintColor =
@@ -551,7 +554,7 @@ BOOL IsMultilineSearchSuggestionEnabled() {
 /// Setup the trailing button. This includes both setting up the button's layout
 /// and popuplating it with the correct image and color.
 - (void)setupTrailingButton {
-  if (self.window) {
+  if (self.window && !self.trailingButton.superview) {
     [self setupTrailingButtonLayout];
   }
   // Show append button for search history/search suggestions or
