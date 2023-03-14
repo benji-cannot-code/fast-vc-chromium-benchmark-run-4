53d909d90f5bf1e891e73f4ea2f4f232cd646967
Frank Liberato <liberato@chromium.org>
2023-03-14 16:53:04 +0000

    Make FFmpegVideoDecoder::GetVideoBuffer thread safe.
    Previously, ffmpeg would serialize calls into GetVideoBuffer onto a
    single thread. Now, it doesn't. While the method was almost thread
    safe already, making it actually thread safe was a bit of work:
    
     - `codec_context` is not reliable, due to what looks like a lack
        of memory barriers.  Use `frame` values instead.
    
     - We no longer know the visible rect / natural size, so we can't
       create the VideoFrame. So, now we use FrameBufferPool to allocate
       frame buffers, and defer creating the VideoFrame until decoding
       of that buffer is complete.
    
     - VideoFrame handled creating the underlying buffers using magical
       methods that do what ffmpeg expects. This has been refactored a
       bit to support allocation by the pool. There is more work to be
       done there; ideally we'd use VideoFrameLayout directly, rather
       than having some of the logic in VideoFrame. However, that was
       too big of a change for now.
    
     - Ownership of the buffer was tied to the VideoFrame, and ffmpeg
       had an (opaque) ref to the VideoFrame while it's in use. Now, we
       give ffmpeg a ref to the underlying buffer instead.
    
     - Similarly, VideoFrame no longer owns the buffers, but instead
       gets a destruction callback into the pool.
    
     - FrameBufferPool used to assume that it was single-threaded, but
       now it can't. This wasn't hard to fix, except that some care had
       to be taked around the threading and ownership assumptions of
       being a MemoryDumpProvider.
    
    Change-Id: Ia295153eb2042728a58054c0c6b3a63865da558e
    Bug: 1414135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303664
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117033}

--- media/filters/ffmpeg_video_decoder.h
+++ media/filters/ffmpeg_video_decoder.h
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/supported_video_decoder_config.h"
 #include "media/base/video_decoder.h"
 #include "media/base/video_decoder_config.h"
-#include "media/base/video_frame_pool.h"
 #include "media/ffmpeg/ffmpeg_deleters.h"
+#include "media/filters/frame_buffer_pool.h"
 
 struct AVCodecContext;
 struct AVFrame;
@@ -90,7 +90,7 @@ class MEDIA_EXPORT FFmpegVideoDecoder : public VideoDecoder {
 
   VideoDecoderConfig config_;
 
-  VideoFramePool frame_pool_;
+  scoped_refptr<FrameBufferPool> frame_pool_;
 
   bool decode_nalus_ = false;
 
