5622f7db679da3a8a838aa9d45a812c08401f918
Josiah Krutz <josiahk@google.com>
2023-03-14 17:07:46 +0000

    [ChromeVox] Fill Voice Section to New ChromeVox Settings Page
    Within the new ChromeVox settings page, fill out the ChromeVox Voice
    settings section, with all of the controls from the options page.
    
    Screenshot: https://screenshot.googleplex.com/6czeZSCmd5mVJrG
    
    Add the following controls:
    - Select current voice:
      - System Text-to-Speech voice
    - Automatically switch ChromeVox voice based on language
    - Open text-to-speech settings
      - Search + O, then S. Use to install, manage, and customize voices.
    
    Bug: b/271422242
    AX-Relnotes: N/A.
    Change-Id: I662a22f75221af98f49f3b1a248bb06d9f6e5bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304599
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117049}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/tts_subpage.ts
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/tts_subpage.ts
@@ -179,7 +179,7 @@ export class SettingsTtsSubpageElement extends SettingsTtsSubpageElementBase {
     this.extensions = [];
   }
 
-  override ready() {
+  override ready(): void {
     super.ready();
 
     // Populate the preview text with textToSpeechPreviewInput. Users can change
