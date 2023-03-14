663bff3d89cd89c4ad0073c6f707e8d8df150d8f
Adem Derinel <derinel@google.com>
2023-03-14 11:16:52 +0000

    [Android][Natsu] Remove sheet header from KeyboardAccessory*
    Since KAv1 does not display a back button or the sheet title as a
    header, these components can now be removed from the KeyboardAccessory*.
    They now belong to AccessorySheet*
    
    Bug: 1420520
    Change-Id: Ic571c8fa6ef7bdc7ea733c95950c835de4d0a662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315687
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116885}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryModernView.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryModernView.java
@@ -11,12 +11,9 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.OvershootInterpolator;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Px;
-import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -34,8 +31,6 @@ class KeyboardAccessoryModernView extends KeyboardAccessoryView {
     private static final float ARRIVAL_ANIMATION_BOUNCE_LENGTH_DIP = 200f;
     private static final float ARRIVAL_ANIMATION_TENSION = 1f;
 
-    private ImageView mKeyboardToggle;
-    private TextView mSheetTitle;
     private Callback<Integer> mObfuscatedLastChildAt;
     private ObjectAnimator mAnimator;
     private float mLastBarItemsViewPosition;
@@ -129,10 +124,6 @@ class KeyboardAccessoryModernView extends KeyboardAccessoryView {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        mSheetTitle = findViewById(R.id.sheet_title);
-        mKeyboardToggle = findViewById(R.id.show_keyboard);
-        mKeyboardToggle.setImageDrawable(
-                AppCompatResources.getDrawable(getContext(), R.drawable.ic_arrow_back_24dp));
 
         int pad = getResources().getDimensionPixelSize(R.dimen.keyboard_accessory_bar_item_padding);
         // Ensure the last element (although scrollable) is always end-aligned.
@@ -198,24 +189,6 @@ class KeyboardAccessoryModernView extends KeyboardAccessoryView {
             return lastChild;
         }
         return null;
-    }
-
-    void setKeyboardToggleVisibility(boolean hasActiveTab) {
-        TraceEvent.begin("KeyboardAccessoryModernView#setKeyboardToggleVisibility");
-        mKeyboardToggle.setVisibility(hasActiveTab ? VISIBLE : GONE);
-        mSheetTitle.setVisibility(hasActiveTab ? VISIBLE : GONE);
-        mBarItemsView.setVisibility(hasActiveTab ? GONE : VISIBLE);
-        if (!hasActiveTab) mBarItemsView.post(mBarItemsView::invalidateItemDecorations);
-        TraceEvent.end("KeyboardAccessoryModernView#setKeyboardToggleVisibility");
-    }
-
-    void setSheetTitle(String title) {
-        mSheetTitle.setText(title);
-    }
-
-    void setShowKeyboardCallback(Runnable showKeyboardCallback) {
-        mKeyboardToggle.setOnClickListener(
-                showKeyboardCallback == null ? null : view -> showKeyboardCallback.run());
     }
 
     void setObfuscatedLastChildAt(Callback<Integer> obfuscatedLastChildAt) {
