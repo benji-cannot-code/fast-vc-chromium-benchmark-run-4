6919b5eed7dedcdcd416a7bb09a6c37f1ce21055
Ari Chivukula <arichiv@chromium.org>
2023-03-14 13:26:56 +0000

    Split out extra mis-matched test
    This seems to be causing some conflict in the Accept-CH cache.
    Let's move it into its own file and see if the issue persists.
    
    Bug: 1423160
    Change-Id: Icfb30645591b041263c827b75566b0658abc3c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335564
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116931}

--- third_party/blink/web_tests/external/wpt/client-hints/critical-ch/mis-matched.https.window.js
+++ third_party/blink/web_tests/external/wpt/client-hints/critical-ch/mis-matched.https.window.js
@@ -2,4 +2,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=resources/util.js
 
 async_test(make_message_test(ECHO_URL+"?mismatch=true", "FAIL"), "Critical-CH Mis-matched hints")
-async_test(make_message_test(ECHO_URL+"?multiple=true&mismatch=true", "FAIL"), "Critical-CH w/ multiple headers and Mis-matched hints")
