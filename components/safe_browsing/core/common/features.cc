d26b1d272ad0e5b5fe8907a1a0e54238c8350e43
Daniel Rubery <drubery@chromium.org>
2023-03-14 22:01:45 +0000

    Enable 7Z evaluation by default
    This is ready to launch, so enable the feature in HEAD.
    
    Bug: 1355567
    Change-Id: Id92454b09495c51ae8d69cab5afe68ed8f5840f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336239
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117227}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -209,7 +209,7 @@ BASE_FEATURE(kSafeBrowsingSkipImageCssFont,
 
 BASE_FEATURE(kSevenZipEvaluationEnabled,
              "SafeBrowsingSevenZipEvaluationEnabled",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSimplifiedUrlDisplay,
              "SimplifiedUrlDisplay",
