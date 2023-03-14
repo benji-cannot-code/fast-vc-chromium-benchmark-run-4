69216a2adf69e90d66d12b68ad41315e3053fedd
Xiyuan Xia <xiyuan@chromium.org>
2023-03-14 22:08:53 +0000

    base: Add nodiscard to RunLoop::QuitClosure etc
    The intention is to fix the misuse of QuitClosure etc in place
    of Quit. The return closure should be used in some way.
    
    Also fixes the wrong use of QuitClosure found.
    
    Bug: None
    Change-Id: Idea7f513b38b1c7b0f57c6dac5335d6e75acc1b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336037
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117229}

--- chromeos/ash/components/network/hotspot_state_handler_unittest.cc
+++ chromeos/ash/components/network/hotspot_state_handler_unittest.cc
@@ -91,9 +91,9 @@ class HotspotStateHandlerTest : public ::testing::Test {
         base::BindLambdaForTesting(
             [&](hotspot_config::mojom::SetHotspotConfigResult success) {
               result = success;
-              run_loop.QuitClosure();
+              run_loop.Quit();
             }));
-    run_loop.RunUntilIdle();
+    run_loop.Run();
     return result;
   }
 
