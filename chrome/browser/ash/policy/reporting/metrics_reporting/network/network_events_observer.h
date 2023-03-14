33a33f234051c9a9431a94c894d65358e708ecb7
Ahmed Nasr <anasr@google.com>
2023-03-14 00:03:25 +0000

    Control network connection events reporting by a flag.
    Bug: b:260243544
    Change-Id: I85ff112bce6081410beb8dcf7b61261639c5bf49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4330501
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116699}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer.h
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace reporting {
 
 BASE_DECLARE_FEATURE(kEnableWifiSignalEventsReporting);
+BASE_DECLARE_FEATURE(kEnableNetworkConnectionStateEventsReporting);
 
 class NetworkEventsObserver
     : public ::chromeos::network_health::mojom::NetworkEventsObserver,
