6c19aac5cc46cc6e13b279bcbbc74cd310419fdd
Fredrik Söderquist <fs@opera.com>
2023-03-14 14:27:54 +0000

    Fix ImageLoader::image_complete_ management
    Clear the flag for the non-empty 'src' case, before queuing the
    microtask, and drop the check on |pending_task_| in
    ImageLoader::ImageComplete().
    
    Bug: 1000273, 1014449
    Change-Id: Id358d02a90a1f302d9ec7679d92acdf5abf1090d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321909
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1116957}

--- third_party/blink/renderer/core/loader/image_loader.h
+++ third_party/blink/renderer/core/loader/image_loader.h
@@ -86,7 +86,7 @@ class CORE_EXPORT ImageLoader : public GarbageCollected<ImageLoader>,
   void ElementDidMoveToNewDocument();
 
   Element* GetElement() const { return element_; }
-  bool ImageComplete() const { return image_complete_ && !pending_task_; }
+  bool ImageComplete() const { return image_complete_; }
 
   ImageResourceContent* GetContent() const { return image_content_.Get(); }
 
