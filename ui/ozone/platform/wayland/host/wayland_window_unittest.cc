661c1a3e0d5e478a854fe9a1d61d4e294bcd7c6a
Thomas Lukaszewicz <tluk@chromium.org>
2023-03-15 02:53:37 +0000

    [wayland] Refactor ozone/wayland output tests
    This CL makes several changes to how output unittests work.
    
    1. A TestOutputMetrics struct has been introduced that encapsulates
       any output state needed by TestOutput and its associated test
       extension classes.
    
       This avoids the current scattering of state across test output
       classes, making it easier to ensure the metrics are consistent
       (i.e. logical size, physical size, scale are set correctly).
    
       This consistency helps make the tests more understandable and
       correct (the tested state is more representative of what happens
       in actual production code).
    
    2. TestOutputMetrics are updated via various helpers and flushed
       collectively to clients (for the wl_output and all extensions)
       before the wl_output.done event.
    
       This is more consistent with what the spec describes and how exo
       actually handles output updates.
    
       Previously metrics could be sent one at a time and/or remain in
       a pending state depending on how the test had been written. This
       could result in clients getting stuck in invalid states (IsReady()
       implementations would return incorrect results as not all expected
       state had been sent).
    
    3. This CL makes it such that TestOutputMetrics are always flushed
       to clients after their outputs are bound.
    
       This change is consistent with what the spec describes.
    
       Previously this behavior was suppressed by not setting any pending
       metrics (rect/scale). This was not super clear and resulted in
       verbose tests. The default behavior of flushing on bind is the
       behavior expected for effectively all existing test cases.
    
       The only test case that required suppressing output metrics was
       LazilyConfiguredScreenTest.DualOutput. This CL instead adds a flag
       that can be optionally set to suppress output metrics specifically
       for this test case.
    
    This CL goes through and updates various test cases that were
    impacted by the above changes. In most places an explicit Flush()
    is no longer required when creating a TestOutput. This change also
    addresses existing problems such as test cases that had forgotten
    to Flush() updates or set an appropriate logical transform state.
    
    Bug: 1412420
    Change-Id: I6b9bacccc47511ce8fcdf06ec7e5246a0da8017d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4298177
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117341}

--- ui/ozone/platform/wayland/host/wayland_window_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_window_unittest.cc
@@ -2262,14 +2262,15 @@ TEST_P(WaylandWindowTest, ToplevelWindowUpdateWindowScale) {
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     // Configure first output with scale 1.
     wl::TestOutput* output1 = server->output();
-    output1->SetRect(gfx::Rect(1920, 1080));
-    output1->SetScale(1);
+    output1->SetPhysicalAndLogicalBounds({1920, 1080});
+    output1->Flush();
 
     // Creating an output with scale 2.
-    wl::TestOutput* output2 = server->CreateAndInitializeOutput();
-    output2->SetRect(gfx::Rect(1920, 1080));
+    auto* output2 =
+        server->CreateAndInitializeOutput(wl::TestOutputMetrics({1920, 1080}));
     output2->SetScale(2);
   });
+  WaitForAllDisplaysReady();
 
   auto* output_manager = connection_->wayland_output_manager();
   EXPECT_EQ(2u, output_manager->GetAllOutputs().size());
@@ -2321,14 +2322,15 @@ TEST_P(WaylandWindowTest, WaylandPopupSurfaceScale) {
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     // Configure an output with scale 1.
     wl::TestOutput* output1 = server->output();
-    output1->SetRect(gfx::Rect(1920, 1080));
-    output1->SetScale(1);
+    output1->SetPhysicalAndLogicalBounds({1920, 1080});
+    output1->Flush();
 
     // Creating an output with scale 2.
-    wl::TestOutput* output2 = server->CreateAndInitializeOutput();
-    output2->SetRect(gfx::Rect(1920, 0, 1920, 1080));
+    auto* output2 = server->CreateAndInitializeOutput(
+        wl::TestOutputMetrics({1920, 0, 1920, 1080}));
     output2->SetScale(2);
   });
+  WaitForAllDisplaysReady();
 
   auto* output_manager = connection_->wayland_output_manager();
   EXPECT_EQ(2u, output_manager->GetAllOutputs().size());
@@ -2416,13 +2418,12 @@ TEST_P(WaylandWindowTest, WaylandPopupInitialBufferScale) {
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     // Configure an output with scale 1.
     wl::TestOutput* main_output = server->output();
-    main_output->SetRect(gfx::Rect(1920, 1080));
-    main_output->SetScale(1);
+    main_output->SetPhysicalAndLogicalBounds({1920, 1080});
+    main_output->Flush();
 
-    // Creating an output with scale 2.
-    wl::TestOutput* secondary_output = server->CreateAndInitializeOutput();
-    secondary_output->SetRect(gfx::Rect(1921, 0, 1920, 1080));
-    secondary_output->SetScale(1);
+    // Creating an output with scale 1.
+    server->CreateAndInitializeOutput(
+        wl::TestOutputMetrics({1921, 0, 1920, 1080}));
   });
 
   auto* output_manager = connection_->wayland_output_manager();
@@ -2518,14 +2519,15 @@ TEST_P(WaylandWindowTest, WaylandPopupInitialBufferUsesParentScale) {
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     // Configure the the first output with scale 1.
     wl::TestOutput* main_output = server->output();
-    main_output->SetRect(gfx::Rect(1920, 1080));
-    main_output->SetScale(1);
+    main_output->SetPhysicalAndLogicalBounds({1920, 1080});
+    main_output->Flush();
 
     // Creating an output with scale 2.
-    wl::TestOutput* secondary_output = server->CreateAndInitializeOutput();
-    secondary_output->SetRect(gfx::Rect(1921, 0, 1920, 1080));
-    secondary_output->SetScale(2);
+    auto* output2 = server->CreateAndInitializeOutput(
+        wl::TestOutputMetrics({1921, 0, 1920, 1080}));
+    output2->SetScale(2);
   });
+  WaitForAllDisplaysReady();
 
   auto* output_manager = connection_->wayland_output_manager();
   EXPECT_EQ(2u, output_manager->GetAllOutputs().size());
@@ -2578,11 +2580,12 @@ TEST_P(WaylandWindowTest, GetPreferredOutput) {
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     // Update first output.
     wl::TestOutput* output1 = server->output();
-    output1->SetRect(gfx::Rect(1920, 1080));
+    output1->SetPhysicalAndLogicalBounds({1920, 1080});
+    output1->Flush();
 
     // Creating a 2nd output.
-    wl::TestOutput* output2 = server->CreateAndInitializeOutput();
-    output2->SetRect(gfx::Rect(1921, 0, 1920, 1080));
+    server->CreateAndInitializeOutput(
+        wl::TestOutputMetrics({1921, 0, 1920, 1080}));
   });
 
   WaitForAllDisplaysReady();
@@ -2625,8 +2628,8 @@ TEST_P(WaylandWindowTest, GetPreferredOutput) {
 
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     // Create the third output.
-    wl::TestOutput* output3 = server->CreateAndInitializeOutput();
-    output3->SetRect(gfx::Rect(0, 1081, 1920, 1080));
+    server->CreateAndInitializeOutput(
+        wl::TestOutputMetrics({0, 1081, 1920, 1080}));
   });
 
   WaitForAllDisplaysReady();
@@ -2719,11 +2722,12 @@ TEST_P(WaylandWindowTest, GetChildrenPreferredOutput) {
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
     // Update the first output.
     wl::TestOutput* output1 = server->output();
-    output1->SetRect(gfx::Rect(1920, 1080));
+    output1->SetPhysicalAndLogicalBounds({1920, 1080});
+    output1->Flush();
 
     // Create a 2nd output.
-    wl::TestOutput* output2 = server->CreateAndInitializeOutput();
-    output2->SetRect(gfx::Rect(1921, 0, 1920, 1080));
+    server->CreateAndInitializeOutput(
+        wl::TestOutputMetrics({1921, 0, 1920, 1080}));
   });
 
   WaitForAllDisplaysReady();
