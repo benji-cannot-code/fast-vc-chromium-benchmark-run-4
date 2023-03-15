3247b0a539441fad02484cd541d80252c559b2d9
David Padlipsky <dpad@google.com>
2023-03-15 22:46:03 +0000

    Settings Split: Do not record devices while flag is enabled
    Bug: b/241965700
    Change-Id: Ifd4714e59334a0b4e403b77fab28fcc5518138ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335532
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117804}

--- ash/system/input_device_settings/input_device_tracker.cc
+++ ash/system/input_device_settings/input_device_tracker.cc
@@ -74,7 +74,9 @@ void InputDeviceTracker::OnActiveUserPrefServiceChanged(
   // `StringListPrefMember`s and record that we have seen all currently
   // connected devices.
   Init(pref_service);
-  RecordConnectedDevices();
+  if (!features::IsInputDeviceSettingsSplitEnabled()) {
+    RecordConnectedDevices();
+  }
 }
 
 bool InputDeviceTracker::WasDevicePreviouslyConnected(
@@ -128,6 +130,10 @@ void InputDeviceTracker::Init(PrefService* pref_service) {
 void InputDeviceTracker::RecordDeviceConnected(
     InputDeviceCategory category,
     const base::StringPiece& device_key) {
+  if (features::IsInputDeviceSettingsSplitEnabled()) {
+    return;
+  }
+
   auto* const observed_devices = GetObservedDevicesForCategory(category);
   // If `observed_devices` is null, that means we are not yet in a valid chrome
   // session.
