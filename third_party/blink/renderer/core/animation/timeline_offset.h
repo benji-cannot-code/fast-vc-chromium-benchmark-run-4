406fa88c1acb19aee0c8f0782c1159c68b9f2448
Kevin Ellis <kevers@google.com>
2023-03-15 20:33:27 +0000

    Implement getter and setter for rangeStart and rangeEnd.
    Bug: 1424538
    
    Change-Id: I69506a04387ed76e5daf9d0c173f12afce8b0b6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335845
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117717}

--- third_party/blink/renderer/core/animation/timeline_offset.h
+++ third_party/blink/renderer/core/animation/timeline_offset.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_ANIMATION_TIMELINE_OFFSET_H_
 
 #include "third_party/blink/renderer/bindings/core/v8/v8_timeline_range.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_union_string_timelinerangeoffset.h"
 #include "third_party/blink/renderer/platform/geometry/length.h"
 
 namespace blink {
@@ -36,6 +37,12 @@ struct TimelineOffset {
   static absl::optional<TimelineOffset> Create(Element* element,
                                                String value,
                                                ExceptionState& exception_state);
+
+  static absl::optional<TimelineOffset> Create(
+      Element* element,
+      const V8UnionStringOrTimelineRangeOffset* range_offset,
+      double default_percent,
+      ExceptionState& exception_state);
 
   static Length ResolveLength(Element* element, const CSSValue* value);
 
