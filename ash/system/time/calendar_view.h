b59dc63fa150e020bed6eae615210ff12e1dd248
Sam Cackett <samcackett@google.com>
2023-03-15 11:48:03 +0000

    Use layer mask to hide contents when animating
    This CL extends the existing layer mask to also cover the event list view as it uses a transparent background as it's color. It follows the path of the up next view when animating, or the event list view if the up next view isn't currently showing.
    
    Result http://shortn/_37cGg0ZN4s
    
    Bug:b/271270201
    
    Change-Id: I120bbd46589649b9db88c8872e91e5f4dd2f748f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299892
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117461}

--- ash/system/time/calendar_view.h
+++ ash/system/time/calendar_view.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "calendar_model.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/compositor/compositor_animation_observer.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/view.h"
 
@@ -43,6 +44,7 @@ namespace ash {
 class CalendarEventListView;
 class CalendarMonthView;
 class IconButton;
+class CalendarSurfaceLayerMask;
 
 // The header of the calendar view, which shows the current month and year.
 class CalendarHeaderView : public views::View {
@@ -337,9 +339,6 @@ class ASH_EXPORT CalendarView : public CalendarModel::Observer,
   // Removes the `up_next_view_`.
   void RemoveUpNextView();
 
-  // Animation callback.
-  void OnShowUpNextComplete();
-
   // Used by the `CalendarUpNextView` to open the event list for today's date.
   void OpenEventListForTodaysDate();
 
@@ -351,6 +350,9 @@ class ASH_EXPORT CalendarView : public CalendarModel::Observer,
   // Used for clipping the calendar scroll view height to the different states
   // that the calendar view can be in.
   void ClipScrollViewHeight(ScrollViewState state_to_change_to);
+
+  // Creates the `CalendarSurfaceLayerMask` if it doesn't already exist.
+  void MaybeCreateLayerMask();
 
   // Setters for animation flags.
   void set_should_header_animate(bool should_animate) {
@@ -397,7 +399,8 @@ class ASH_EXPORT CalendarView : public CalendarModel::Observer,
   CalendarModel* calendar_model_ =
       Shell::Get()->system_tray_model()->calendar_model();
 
-  std::unique_ptr<ui::LayerOwner> up_next_view_mask_;
+  // Layer mask that sits over the scrollview and hides the content underneath.
+  std::unique_ptr<CalendarSurfaceLayerMask> calendar_surface_layer_mask_;
 
   // If it `is_resetting_scroll_`, we don't calculate the scroll position and we
   // don't need to check if we need to update the month or not.
