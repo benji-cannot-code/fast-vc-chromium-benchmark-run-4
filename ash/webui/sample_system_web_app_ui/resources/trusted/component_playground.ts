78a17122a9f22bbb3c00dace065bcc0671b5bf43
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2023-03-15 05:34:18 +0000

    cros-components: Add cros-components to chrome://resources and use it
    Compiles cros_components/button.ts and adds the compiled files to
    chrome://resources. Also uses them from the Sample System Web App.
    
    Bug: b/264070221
    Change-Id: Ia14408f6a0bfadc058c8da73e58dcbcae5c2f5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297057
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117376}

--- ash/webui/sample_system_web_app_ui/resources/trusted/component_playground.ts
+++ ash/webui/sample_system_web_app_ui/resources/trusted/component_playground.ts
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '@material/web/button/text-button.js';
+import 'chrome://resources/cros_components/button/button.js';
 
-customElements.whenDefined('md-text-button').then(() => {
+customElements.whenDefined('cros-button').then(() => {
   document.documentElement.classList.remove('loading');
 });
