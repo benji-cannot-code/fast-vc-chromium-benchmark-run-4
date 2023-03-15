56c9f478c985de1b66982f6be01f4e1137000bfc
Zain Afzal <zafzal@google.com>
2023-03-15 23:52:32 +0000

    Create [add,remove]colorChangeListener
    Bug: b/249403723
    Change-Id: Ie17b9f7f645398dac02aa94839e0c2019ba4354e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310074
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117843}

--- ash/webui/color_internals/resources/index.ts
+++ ash/webui/color_internals/resources/index.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {startColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
+import {addColorChangeListener, startColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 
 import {getRGBAFromComputedStyle} from './utils.js';
 import {startObservingWallpaperColors} from './wallpaper_colors.js';
@@ -93,8 +93,20 @@ async function populateTokenTable() {
   addTokens(sysTable, tokens.sys_tokens);
 }
 
+function onColorChange() {
+  const formatter = new Intl.DateTimeFormat('en', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  });
+  const span = document.querySelector('#last-updated') as HTMLSpanElement;
+  span.innerText = formatter.format(new Date());
+}
+
 window.onload = () => {
   populateTokenTable();
   startColorChangeUpdater();
   startObservingWallpaperColors();
+  addColorChangeListener(onColorChange);
+  onColorChange();
 };
