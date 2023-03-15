e228fcee086e3dcf69fde2ab8312d256a1f549f8
Jeffrey Young <cowmoo@google.com>
2023-03-15 21:38:58 +0000

    personalization: WallpaperImageDownloader for mocking
    Create a delegate class WallpaperImageDownloader to allow mocking
    network calls better in test. Previously accessed the global instance
    of ImageDownloader throughout wallpaper_controller_impl.cc.
    
    BUG=b:264461204
    TEST=ash_unittests --gtest_filter=*Wallpaper*
    TEST=unit_tests --gtest_filter=*PersonalizationAppWallpaper*
    TEST=browser_tests --gtest_filter=*WallpaperSubpage*
    
    Change-Id: Ic924fe109620850b8131e373548310c4625b8a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4313464
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117765}

--- ash/public/cpp/wallpaper/wallpaper_controller.cc
+++ ash/public/cpp/wallpaper/wallpaper_controller.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/wallpaper/wallpaper_controller.h"
 
-#include "base/strings/string_number_conversions.h"
-#include "ui/display/screen.h"
-
 namespace ash {
 
 namespace {
@@ -27,18 +24,6 @@ WallpaperController::~WallpaperController() {
 // static
 WallpaperController* WallpaperController::Get() {
   return g_instance;
-}
-
-// static
-std::string WallpaperController::GetBackdropWallpaperSuffix() {
-  // TODO(b/186807814) handle different display resolutions better.
-  // FIFE url is used for Backdrop wallpapers and the desired image size should
-  // be specified. Currently we are using two times the display size. This is
-  // determined by trial and error and is subject to change.
-  gfx::Size display_size =
-      display::Screen::GetScreen()->GetPrimaryDisplay().size();
-  return "=w" + base::NumberToString(
-                    2 * std::max(display_size.width(), display_size.height()));
 }
 
 }  // namespace ash
