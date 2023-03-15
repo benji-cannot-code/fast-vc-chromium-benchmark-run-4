2fdca0e22c76310c4f6fa9a54d47e389b1ea1c87
Rafał Godlewski <rgod@google.com>
2023-03-15 18:14:43 +0000

    [iOS] Add timeout for viewing password details with notes
    Adds timer in password details view that:
    * Starts counting on opening the password details view (which requires
    successful authentication with notes for passwords features enabled)
    * Resets after user interaction with the view (e.g. editing, copying
    details, showing/hiding password)
    * Navigates to the password manager list view on timeout
    
    Timeout is defined as a feature value currently set to 5 mins (same as
    on Desktop).
    
    Bug: 1414897
    Change-Id: Iefcedfdb7485e94b8f34457227fd4114028afd4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335225
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117624}

--- ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
@@ -576,7 +576,8 @@ id<GREYMatcher> EditDoneButton() {
       [self isRunningTest:@selector(testAddPasswordLayoutWithLongNotes)] ||
       [self isRunningTest:@selector
             (testAddPasswordSaveButtonStateOnFieldChanges)] ||
-      [self isRunningTest:@selector(testLayoutWithLongNotes)]) {
+      [self isRunningTest:@selector(testLayoutWithLongNotes)] ||
+      [self isRunningTest:@selector(testShowHidePasswordWithNotesEnabled)]) {
     config.features_enabled.push_back(syncer::kPasswordNotesWithBackup);
   }
 
@@ -1408,10 +1409,6 @@ id<GREYMatcher> EditDoneButton() {
   [[self interactionForSinglePasswordEntryWithDomain:@"example.com"
                                             username:@"concrete username"]
       performAction:grey_tap()];
-
-  [PasswordSettingsAppInterface setUpMockReauthenticationModule];
-  [PasswordSettingsAppInterface mockReauthenticationModuleExpectedResult:
-                                    ReauthenticationResult::kSuccess];
 
   TapEdit();
   [[EarlGrey selectElementWithMatcher:TooLongNoteFooter()]
@@ -2815,6 +2812,36 @@ id<GREYMatcher> EditDoneButton() {
   [PasswordSettingsAppInterface setUpMockReauthenticationModule];
   [PasswordSettingsAppInterface mockReauthenticationModuleExpectedResult:
                                     ReauthenticationResult::kSuccess];
+
+  [GetInteractionForPasswordDetailItem(ShowPasswordButton())
+      assertWithMatcher:grey_sufficientlyVisible()];
+  [GetInteractionForPasswordDetailItem(ShowPasswordButton())
+      performAction:grey_tap()];
+  [GetInteractionForPasswordDetailItem(HidePasswordButton())
+      assertWithMatcher:grey_sufficientlyVisible()];
+  [GetInteractionForPasswordDetailItem(HidePasswordButton())
+      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
+      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:SettingsDoneButton()]
+      performAction:grey_tap()];
+}
+
+// Tests that reauthentication is not required to show password when notes are
+// enabled since the reauthentication happens before navigating to the details
+// view in this scenario.
+- (void)testShowHidePasswordWithNotesEnabled {
+  SaveExamplePasswordForm();
+
+  OpenPasswordManager();
+  [PasswordSettingsAppInterface
+      setUpMockReauthenticationModuleForPasswordManager];
+  [PasswordSettingsAppInterface mockReauthenticationModuleExpectedResult:
+                                    ReauthenticationResult::kSuccess];
+
+  [[self interactionForSinglePasswordEntryWithDomain:@"example.com"
+                                            username:@"concrete username"]
+      performAction:grey_tap()];
 
   [GetInteractionForPasswordDetailItem(ShowPasswordButton())
       assertWithMatcher:grey_sufficientlyVisible()];
