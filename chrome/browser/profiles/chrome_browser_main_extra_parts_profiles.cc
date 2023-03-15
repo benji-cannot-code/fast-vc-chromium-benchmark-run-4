f9ecbd5f95fef8756c5b76a6e567cf61178c1d18
Ryan Sultanem <rsult@google.com>
2023-03-15 09:15:51 +0000

    [Factory Registration] Enrich the list from DumpWithoutCrash report
    DriveServiceFactory is causing a very high number of reports and should
    be treated as soon as possible.
    
    Bug: 1412439, 1422886
    Change-Id: I49b8a6e70b8133e9eedb733662c63d3552e7f8db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334350
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117418}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -234,6 +234,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/router/discovery/access_code/access_code_cast_sink_service_factory.h"
 #include "chrome/browser/media_galleries/gallery_watch_manager.h"
 #include "chrome/browser/metrics/desktop_session_duration/desktop_profile_session_durations_service_factory.h"
+#include "chrome/browser/new_tab_page/modules/drive/drive_service_factory.h"
 #include "chrome/browser/new_tab_page/modules/recipes/recipes_service_factory.h"
 #include "chrome/browser/performance_manager/persistence/site_data/site_data_cache_facade_factory.h"
 #include "chrome/browser/profile_resetter/reset_report_uploader_factory.h"
@@ -599,6 +600,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   DomainDiversityReporterFactory::GetInstance();
   dom_distiller::DomDistillerServiceFactory::GetInstance();
   DownloadCoreServiceFactory::GetInstance();
+#if !BUILDFLAG(IS_ANDROID)
+  DriveServiceFactory::GetInstance();
+#endif
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   enterprise_signals::SignalsAggregatorFactory::GetInstance();
 #endif
