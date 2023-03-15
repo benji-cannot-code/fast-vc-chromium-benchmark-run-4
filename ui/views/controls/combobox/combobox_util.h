189711088a5016a7a76d402bad4a27e4b63b2de4
Elaine Chien <elainechien@chromium.org>
2023-03-15 17:29:22 +0000

    Share ink drop configuration between combobox and editable combobox
    This will also make it easier to make Chrome Refresh changes for
    combobox ink drops.
    
    Bug: 1400024
    Change-Id: Id140972b55fd1117b1f0a0d4ce9ea0f55b131826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338661
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117601}

--- ui/views/controls/combobox/combobox_util.h
+++ ui/views/controls/combobox/combobox_util.h
@@ -18,6 +18,7 @@ class Rect;
 }  // namespace gfx
 
 namespace views {
+class Button;
 
 // Constants for the size of the combobox arrow.
 constexpr gfx::Size ComboboxArrowSize() {
@@ -34,6 +35,8 @@ int GetComboboxArrowContainerWidth();
 void PaintComboboxArrow(SkColor color,
                         const gfx::Rect& bounds,
                         gfx::Canvas* canvas);
+
+void ConfigureComboboxButtonInkDrop(Button* host_view);
 
 }  // namespace views
 
