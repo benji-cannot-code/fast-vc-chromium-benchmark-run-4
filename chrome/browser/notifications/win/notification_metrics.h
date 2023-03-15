43761c7c7b05816a5fd4e38c89a0f5868e88a667
Wissem Gamra <wiss@chromium.org>
2023-03-15 18:13:03 +0000

    [Code Health] Remove usage of Notifications.Windows.OnDismissedStatus for expired histogram
    Bug: 1384905
    Change-Id: I70ea30c421e3646f2a8723f0515299c3b233aae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334982
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Wissem Gamra <wiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117622}

--- chrome/browser/notifications/win/notification_metrics.h
+++ chrome/browser/notifications/win/notification_metrics.h
@@ -144,14 +144,6 @@ enum class SetReadyCallbackStatus {
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
-enum class OnDismissedStatus {
-  kSuccess = 0,
-  kGetDismissalReasonFailed = 1,
-  kMaxValue = kGetDismissalReasonFailed,
-};
-
-// These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
 enum class OnFailedStatus {
   kSuccess = 0,
   kGetErrorCodeFailed = 1,
@@ -173,7 +165,6 @@ void LogGetSettingStatusStartup(GetSettingStatus status);
 void LogHandleEventStatus(HandleEventStatus status);
 void LogActivationStatus(ActivationStatus status);
 void LogSetReadyCallbackStatus(SetReadyCallbackStatus status);
-void LogOnDismissedStatus(OnDismissedStatus status);
 void LogOnFailedStatus(OnFailedStatus status);
 
 }  // namespace notifications_uma
