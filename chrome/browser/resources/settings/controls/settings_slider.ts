cee58e72d49477795c5b1ad76f5e762fa39ef882
Wes Okuhara <wesokuhara@google.com>
2023-03-15 22:02:42 +0000

    CrOS Settings: Dynamic color support for cr-slider
    Light mode: http://screen/BxMWpNkYEJhZY3f.png
    Dark mode: http://screen/6dambXKSLVBDUTB.png
    Disabled: http://screen/5xAqU2QK4mCreVu.png
    
    Bug: b/266837484
    Change-Id: I10ffe8fee0351f0357a6a7aba7ac60194c0ef34f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336423
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117782}

--- chrome/browser/resources/settings/controls/settings_slider.ts
+++ chrome/browser/resources/settings/controls/settings_slider.ts
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import '../settings_vars.css.js';
 import '//resources/cr_elements/cr_slider/cr_slider.js';
+// <if expr='chromeos_ash'>
+import 'chrome://resources/cr_elements/chromeos/cros_color_overrides.css.js';
+
+// </if>
 
 import {CrSliderElement, SliderTick} from '//resources/cr_elements/cr_slider/cr_slider.js';
 import {assert} from '//resources/js/assert_ts.js';
