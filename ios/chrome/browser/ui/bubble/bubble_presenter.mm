4b0b32a83cb7e9d01b7db5294a1aa9068bffd76a
Louis Romero <lpromero@google.com>
2023-03-15 09:57:36 +0000

    Remove unnecessary dependency
    This dependency was not removed in crrev/c/4166602.
    
    Bug: 1382913
    Change-Id: Iddac5592be4c4ede99d9d891e130a4bee520f400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338141
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117430}

--- ios/chrome/browser/ui/bubble/bubble_presenter.mm
+++ ios/chrome/browser/ui/bubble/bubble_presenter.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/feature_engagement/public/feature_constants.h"
 #import "components/feature_engagement/public/tracker.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/commerce/push_notification/push_notification_feature.h"
 #import "ios/chrome/browser/feature_engagement/tracker_factory.h"
 #import "ios/chrome/browser/flags/system_flags.h"
 #import "ios/chrome/browser/shared/public/commands/toolbar_commands.h"
