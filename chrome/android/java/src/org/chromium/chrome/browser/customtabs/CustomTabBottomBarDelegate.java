78325d4c7917d2a82230d634148bb044c51034b3
Jinsuk Kim <jinsukkim@chromium.org>
2023-03-15 16:43:06 +0000

    [CCT] Keep PendingIntents from background app working
    Custom menu command/toolbar action from the host app via
    PendingIntent may be blocked in the future. This
    CL starts the intent in MODE_BACKGROUND_ACTIVITY_START_ALLOWED
    mode to keep them functioning.
    
    
    Low-Coverage-Reason: Android U bot to test this on not available yet
    
    Bug: 1401544
    Change-Id: I5a3408050cec1c2a0a1fd7345f187570017ec86d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328995
    Auto-Submit: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1117572}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabBottomBarDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabBottomBarDelegate.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.customtabs;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.app.PendingIntent;
 import android.app.PendingIntent.CanceledException;
 import android.content.Intent;
@@ -24,6 +25,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.browser.customtabs.CustomTabsIntent;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
 import org.chromium.base.metrics.RecordUserAction;
@@ -374,7 +376,9 @@ public class CustomTabBottomBarDelegate
         Tab tab = tabProvider.getTab();
         if (tab != null) addedIntent.setData(Uri.parse(tab.getUrl().getSpec()));
         try {
-            pendingIntent.send(activity, 0, addedIntent, null, null);
+            ActivityOptions options = ActivityOptions.makeBasic();
+            ApiCompatibilityUtils.setActivityOptionsBackgroundActivityStartMode(options);
+            pendingIntent.send(activity, 0, addedIntent, null, null, null, options.toBundle());
         } catch (CanceledException e) {
             Log.e(TAG, "CanceledException when sending pending intent.");
         }
