66857c4f3164d1fbc644c6b9853ab05cb3ef6608
Alison Maher <almaher@microsoft.com>
2023-03-15 16:21:49 +0000

    [FlexNG] Don't clamp flex row-gaps when fragmenting
    We don't clamp flex item margins when an item breaks before to more
    accurately handle alignment. Thus, we should similarly do the same
    for row gaps. Currently, we will clamp row gaps (or at least part
    of row gaps - there were other bugs uncovered while investigating),
    so this CL updates the logic to correctly preserve the row gap,
    matching Firefox's behavior.
    
    To accomplish this, we needed to adjust item offsets based on various
    cases, but we only want to make this adjustment the first time the
    row breaks before. Thus, the break before row tracking in
    NGFlexBreakTokenData was updated to an enum of three values to keep
    track of whether the break token represents a break before a row,
    as well as whether or not it is the first time we have broken
    before that row.
    
    Additionally, when we perform the logic to determine if a row should
    break before, we needed to update the call site to pass in the adjusted
    row offset to ensure we are handling breaks correctly.
    
    Bug: 1413089
    Change-Id: Ieb9cc4470cd96146cddf4f330aefdd06af1a1626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335769
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1117559}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_algorithm.h"
 
 #include "third_party/blink/renderer/core/layout/flexible_box_algorithm.h"
+#include "third_party/blink/renderer/core/layout/ng/flex/ng_flex_break_token_data.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.h"
 
 namespace blink {
@@ -97,7 +98,7 @@ class CORE_EXPORT NGFlexLayoutAlgorithm
   NGLayoutResult::EStatus GiveItemsFinalPositionAndSizeForFragmentation(
       HeapVector<NGFlexLine>* flex_line_outputs,
       Vector<EBreakBetween>* row_break_between_outputs,
-      bool* broke_before_row);
+      NGFlexBreakTokenData::NGFlexBreakBeforeRow* break_before_row);
   NGLayoutResult::EStatus PropagateFlexItemInfo(FlexItem* flex_item,
                                                 wtf_size_t flex_line_idx,
                                                 LogicalOffset offset,
@@ -138,10 +139,12 @@ class CORE_EXPORT NGFlexLayoutAlgorithm
   // a layout result, so when breaking before a row, we will insert a
   // fragmentainer break before the first child in a row. |child| should be
   // those associated with the first child in the row. |row|,
-  // |row_break_between|, |row_index|, |has_container_separation| and
-  // |is_first_for_row| are specific to the row itself. See
+  // |row_block_offset|, |row_break_between|, |row_index|,
+  // |has_container_separation| and |is_first_for_row| are specific to the row
+  // itself. See
   // |::blink::BreakBeforeChildIfNeeded()| for more documentation.
   NGBreakStatus BreakBeforeRowIfNeeded(const NGFlexLine& row,
+                                       LayoutUnit row_block_offset,
                                        EBreakBetween row_break_between,
                                        wtf_size_t row_index,
                                        NGLayoutInputNode child,
