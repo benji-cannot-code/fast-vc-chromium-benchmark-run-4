7ed23b1cdd9cec5e11187632ecc1eb51d072902d
Riley Tatum <rtatum@google.com>
2023-03-15 16:40:02 +0000

    [NTP][Quests] Fallback to favicon if no image
    Changes
    * Show the favicon if there is no imageUrl
    * Updated the page_favicon component to allow a size input
    * Changed the #image element background to the same color as the module
    background
    * Updated the image check to check for isKnownToSync
    * Updated the test data to set isKnownToSync to true
    
    Tests will be added in the next CL, since I'm having some issues with
    imageServiceHandler within our test file.
    
    Screenshot: screenshot/9LZb4wRLYxrwnbD
    
    Bug: 1423485
    Change-Id: I5215f367cd404b04719c1c4db4772c9f1a4705ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335163
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117570}

--- chrome/browser/resources/new_tab_page/modules/history_clusters/page_favicon.ts
+++ chrome/browser/resources/new_tab_page/modules/history_clusters/page_favicon.ts
@@ -47,6 +47,11 @@ class PageFavicon extends PolymerElement {
        * fetching higher quality favicons in that case.
        */
       isKnownToSync: Boolean,
+
+      size: {
+        type: Number,
+        value: 16,
+      },
     };
   }
 
@@ -56,6 +61,7 @@ class PageFavicon extends PolymerElement {
 
   url: Url;
   isKnownToSync: boolean;
+  size: number;
 
   //============================================================================
   // Helper methods
@@ -67,7 +73,8 @@ class PageFavicon extends PolymerElement {
     }
     return `background-image:${
         getFaviconForPageURL(
-            this.url.url, this.isKnownToSync, '', /** --favicon-size */ 16)}`;
+            this.url.url, this.isKnownToSync, '',
+            /** --favicon-size */ this.size)}`;
   }
 }
 
