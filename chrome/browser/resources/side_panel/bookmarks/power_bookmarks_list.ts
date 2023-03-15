2f9634def5687fcbb2af3174f7d238709082603b
Emily Shack <emshack@chromium.org>
2023-03-15 21:51:10 +0000

    Bookmarks++: Don't lazily render context menu
    Fixes a bug where the bookmarks side panel did not render at the correct
    size when first opened. There's only one context menu for the full list,
    not one per item, so this shouldn't have a significant performance
    impact.
    
    Screenshot before fix: https://screenshot.googleplex.com/AN8WefuSkEP2Tnd
    Screenshot after fix: https://screenshot.googleplex.com/5SzH2Cy9bQapBRE
    
    Bug: 1405239
    Change-Id: I288db9d04a190c51db953ea6aac308ad7ecd35c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342812
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117773}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
@@ -60,7 +60,7 @@ export interface SortOption {
 
 export interface PowerBookmarksListElement {
   $: {
-    contextMenu: CrLazyRenderElement<PowerBookmarksContextMenuElement>,
+    contextMenu: PowerBookmarksContextMenuElement,
     deletionToast: CrLazyRenderElement<CrToastElement>,
     powerBookmarksContainer: HTMLElement,
     shownBookmarksIronList: IronListElement,
@@ -640,11 +640,11 @@ export class PowerBookmarksListElement extends PolymerElement {
     const priceTrackingEligible =
         this.isPriceTrackingEligible_(event.detail.bookmark);
     if (event.detail.event.button === 0) {
-      this.$.contextMenu.get().showAt(
+      this.$.contextMenu.showAt(
           event.detail.event, [event.detail.bookmark], priceTracked,
           priceTrackingEligible);
     } else {
-      this.$.contextMenu.get().showAtPosition(
+      this.$.contextMenu.showAtPosition(
           event.detail.event, [event.detail.bookmark], priceTracked,
           priceTrackingEligible);
     }
@@ -763,7 +763,7 @@ export class PowerBookmarksListElement extends PolymerElement {
   private onEditMenuClicked_(event: MouseEvent) {
     event.preventDefault();
     event.stopPropagation();
-    this.$.contextMenu.get().showAt(
+    this.$.contextMenu.showAt(
         event, this.selectedBookmarks_.slice(), false, false);
   }
 
