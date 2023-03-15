7613b2145081ec5b50b229b899f105e413b6fcae
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-03-15 18:47:24 +0000

    Revert "Omnibox Inspire Me - Queries based on User Location and Interests."
    This reverts commit 0064de9f487d663ab179427d7d5d12c566284d25.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8786538036243415361
    
    Sample failed build: https://ci.chromium.org/b/8786538036243415361
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8786538036243415361&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4327989
    
    Original change's description:
    > Omnibox Inspire Me - Queries based on User Location and Interests.
    >
    > Bug: 1423135
    >
    > Change-Id: I3b306a3e3202b4bf50fa3fc49a1cf41e5d97962f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327989
    > Reviewed-by: manuk hovanesian <manukh@chromium.org>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1117585}
    >
    
    Bug: 1423135
    Change-Id: Ia9bb9e61ea31a84bcc878635f1f4f936de0d9321
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4343031
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1117654}

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -1124,18 +1124,6 @@ bool IsUrlScoringModelEnabled() {
 
 // <- ML Relevance Scoring
 // ---------------------------------------------------------
-// Inspire Me ->
-const base::FeatureParam<int> kInspireMeAdditionalRelatedQueries(
-    &omnibox::kInspireMe,
-    "AdditionalRelatedQueries",
-    0);
-
-const base::FeatureParam<int> kInspireMeAdditionalTrendingQueries(
-    &omnibox::kInspireMe,
-    "AdditionalTrendingQueries",
-    0);
-// <- Inspire Me
-// ---------------------------------------------------------
 
 }  // namespace OmniboxFieldTrial
 
