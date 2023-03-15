478b85d2702c90ce35f0ed12d1a28709a383a543
Torne (Richard Coles) <torne@google.com>
2023-03-15 21:34:45 +0000

    webview: enable chrome://inspect in debuggable apps.
    WebView was only automatically enabling the devtools protocol on
    debuggable builds of Android, but not in debuggable apps, forcing app
    developers to call setWebContentsDebuggingEnabled(true) to be able to
    use chrome://inspect on real devices.
    
    If the app is debuggable then it's already possible to use adb to bypass
    the normal Android sandbox restrictions on it or to just enable web
    contents debugging anyway by attaching a Java debugger, so there's no
    benefit to it being disabled, and it increases the risk that the
    developer will accidentally leave it enabled in their production build,
    so instead we just enable it for debuggable apps automatically.
    
    Fixed: 1423521
    Change-Id: Ic75ad3f396212c8af838bcdfd070e91f6c8612c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327285
    Commit-Queue: Richard Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117760}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
@@ -216,7 +216,7 @@ public class WebViewChromiumAwInit {
             AwBrowserProcess.initializeMetricsLogUploader();
 
             mSharedStatics = new SharedStatics();
-            if (BuildInfo.isDebugAndroid()) {
+            if (BuildInfo.isDebugAndroidOrApp()) {
                 mSharedStatics.setWebContentsDebuggingEnabledUnconditionally(true);
             }
 
