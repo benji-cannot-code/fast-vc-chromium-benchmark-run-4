696a667785fda92ed5ee49b8b9236dbb3e20a9b1
Andrew Paseltiner <apaseltiner@chromium.org>
2023-03-15 21:05:01 +0000

    Clean up AttributionDataHostManagerImpl
    - Move AttributionInputEvent instead of copying where applicable
    - Remove unnecessary move of DelayedTrigger into a local variable
    - Add helper for marking a SourceRegistrations structure
      navigation-complete
    - Change type of SourceRegistrations::navigation_complete from
      absl::optional<bool> to bool, since whenever it is null we used
      value_or(true) to retrieve it
    - DCHECK that NotifyFencedFrameReportingBeaconStarted is not called more
      than once with same beacon ID
    - Reduce scope of local variables containing iterators
    - Fix includes
    
    Change-Id: Ic13149af714e86e31cced93ffc392b7341483af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4341652
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117744}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.h
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_ATTRIBUTION_REPORTING_ATTRIBUTION_DATA_HOST_MANAGER_IMPL_H_
 
 #include <stddef.h>
-#include <stdint.h>
 
 #include <string>
 
@@ -20,13 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
-#include "components/attribution_reporting/source_type.mojom-forward.h"
 #include "content/browser/attribution_reporting/attribution_beacon_id.h"
 #include "content/browser/attribution_reporting/attribution_data_host_manager.h"
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/mojom/conversions/attribution_data_host.mojom.h"
