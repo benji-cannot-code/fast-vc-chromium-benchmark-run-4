43c670730c3eccde5969c9e383d4107aeff33a2b
Andrew Rayskiy <greengrape@google.com>
2023-03-15 14:59:52 +0000

    [DirectSockets] Wrap UDPSocketOptions in RestrictedUDPSocketParams
    This change is necessary to separate the actual socket options passed
    to the OS from additional socket creation params.
    
    Bug: 1420342
    Change-Id: I9f2a6415d01e661e5a34c257f48c359d5903423b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304010
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117520}

--- services/network/network_context_unittest.cc
+++ services/network/network_context_unittest.cc
@@ -3268,7 +3268,7 @@ TEST_F(NetworkContextTest, CreateRestrictedUDPSocket) {
     network_context->CreateRestrictedUDPSocket(
         server_addr, mojom::RestrictedUDPSocketMode::BOUND,
         net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS),
-        /*options=*/nullptr, server_socket.BindNewPipeAndPassReceiver(),
+        /*params=*/nullptr, server_socket.BindNewPipeAndPassReceiver(),
         socket_listener_receiver.BindNewPipeAndPassRemote(),
         create_future.GetCallback());
     ASSERT_EQ(create_future.Get<0>(), net::OK);
@@ -3288,7 +3288,7 @@ TEST_F(NetworkContextTest, CreateRestrictedUDPSocket) {
     network_context->CreateRestrictedUDPSocket(
         server_addr, mojom::RestrictedUDPSocketMode::CONNECTED,
         net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS),
-        /*options=*/nullptr, client_socket.BindNewPipeAndPassReceiver(),
+        /*params=*/nullptr, client_socket.BindNewPipeAndPassReceiver(),
         client_listener_receiver.BindNewPipeAndPassRemote(),
         create_future.GetCallback());
     ASSERT_EQ(create_future.Get<0>(), net::OK);
