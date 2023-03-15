d40df263e75698d3b644a59f5d1f0df34dcd3be3
Zelin Liu <zelin@chromium.org>
2023-03-15 20:12:29 +0000

    iwa: Use UrlIdentity to display IWA & Extension names in Chrome history
    Isolated Web apps and Chrome Extensions both have an encoded string of
    characters as hostname. This prevents the user from identifying the
    entity.
    
    UrlIdentity creates a user-identifiable string for a URL.
    
    We are updating BrowsingHistoryHandler to use UrlIdentity to format
    display string.
    
    After this change, normal sites and file URLs will keep their current
    formatting logic, while IWA/Extensions will display their respective
    app/extension name.
    
    Bug: 1311065
    Change-Id: I14b5f3f76f3b8e2fed0a30efc3039a58f6e839c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335050
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117707}

--- chrome/browser/ui/url_identity.cc
+++ chrome/browser/ui/url_identity.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/url_constants.h"
 #include "components/url_formatter/elide_url.h"
+#include "components/url_formatter/url_formatter.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
@@ -39,6 +40,8 @@ UrlIdentity CreateDefaultUrlIdentityFromUrl(const GURL& url,
           DefaultFormatOptions::kOmitCryptographicScheme)) {
     name = url_formatter::FormatUrlForSecurityDisplay(
         url, url_formatter::SchemeDisplay::OMIT_CRYPTOGRAPHIC);
+  } else if (options.default_options.Has(DefaultFormatOptions::kHostname)) {
+    name = url_formatter::IDNToUnicode(url.host());
   } else {
     name = url_formatter::FormatUrlForSecurityDisplay(url);
   }
