6d719fe078d5c4be79648d7f70aa729b1450dbb7
Nan Lin <linnan@chromium.org>
2023-03-15 23:21:16 +0000

    Remove waiting for navigation complete for source registrations on navigation beacons
    This avoids pending sources being dropped as web to app navigation may
    fail.
    
    Change-Id: Ic5788de7abf90ea6ae511583c9e42e842769adbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342615
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117822}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 #include <utility>
-#include <vector>
 
 #include "base/check.h"
 #include "base/check_op.h"
@@ -254,11 +253,6 @@ struct AttributionDataHostManagerImpl::SourceRegistrations {
   struct Beacon {
     BeaconId id;
 
-    // Navigation source data that has been received as part of this beacon.
-    // Navigation sources cannot be processed until `navigation_complete` is set
-    // to true.
-    std::vector<StorableSource> sources;
-
     // True if the beacon has completed. If true, no further calls will be made
     // to `NotifyFencedFrameReportingBeaconData()`.
     bool beacon_complete = false;
@@ -469,8 +463,8 @@ void AttributionDataHostManagerImpl::NotifyNavigationFailure(
   if (auto it =
           registrations_.find(BeaconId(NavigationBeaconId(navigation_id)));
       it != registrations_.end()) {
-    OnSourceEligibleDataHostFinished(it->register_time);
-    registrations_.erase(it);
+    it->CompleteNavigation();
+    MaybeOnRegistrationsFinished(it);
   }
 }
 
@@ -484,13 +478,6 @@ void AttributionDataHostManagerImpl::NotifyNavigationSuccess(
   }
 
   it->CompleteNavigation();
-
-  std::vector<StorableSource>& sources = it->GetBeacon().sources;
-
-  for (StorableSource& source : sources) {
-    attribution_manager_->HandleSource(std::move(source), it->render_frame_id);
-  }
-  sources.clear();
 
   MaybeOnRegistrationsFinished(it);
 }
@@ -869,25 +856,16 @@ void AttributionDataHostManagerImpl::OnSourceParsed(
   }
 
   if (source.has_value()) {
-    absl::visit(
-        base::Overloaded{
-            [&](const SourceRegistrations::NavigationRedirect& redirect) {
-              base::UmaHistogramEnumeration(
-                  "Conversions.SourceRegistration.NavigationType.Foreground",
-                  redirect.nav_type);
-              attribution_manager_->HandleSource(std::move(*source),
-                                                 registrations.render_frame_id);
-            },
-            [&](SourceRegistrations::Beacon& beacon) {
-              if (registrations.navigation_complete) {
-                attribution_manager_->HandleSource(
-                    std::move(*source), registrations.render_frame_id);
-              } else {
-                beacon.sources.push_back(std::move(*source));
-              }
-            },
-        },
-        registrations.data);
+    attribution_manager_->HandleSource(std::move(*source),
+                                       registrations.render_frame_id);
+
+    if (const auto* redirect =
+            absl::get_if<SourceRegistrations::NavigationRedirect>(
+                &registrations.data)) {
+      base::UmaHistogramEnumeration(
+          "Conversions.SourceRegistration.NavigationType.Foreground",
+          redirect->nav_type);
+    }
   } else {
     attribution_manager_->NotifyFailedSourceRegistration(
         header_value, registrations.source_origin, reporting_origin,
--- content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
@@ -1567,7 +1567,7 @@ TEST_F(AttributionDataHostManagerImplTest,
 
 TEST_F(AttributionDataHostManagerImplTest,
        NavigationBeaconSource_NavigationFailedBeforeAndAfterParsing) {
-  EXPECT_CALL(mock_manager_, HandleSource).Times(0);
+  EXPECT_CALL(mock_manager_, HandleSource).Times(2);
 
   auto reporting_origin = url::Origin::Create(GURL("https://report.test"));
   auto source_origin = *SuitableOrigin::Deserialize("https://source.test");
@@ -1601,7 +1601,7 @@ TEST_F(AttributionDataHostManagerImplTest,
 
 TEST_F(AttributionDataHostManagerImplTest,
        NavigationBeaconSource_NavigationFailedBeforeAndAfterData) {
-  EXPECT_CALL(mock_manager_, HandleSource).Times(0);
+  EXPECT_CALL(mock_manager_, HandleSource).Times(2);
 
   auto reporting_origin = url::Origin::Create(GURL("https://report.test"));
   auto source_origin = *SuitableOrigin::Deserialize("https://source.test");
