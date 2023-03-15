19fccab06aab450afdbbb26dfb7e6015ccfb01ce
Qihui Zhao <qihuizhao@google.com>
2023-03-15 19:05:12 +0000

    [UI fix] Remove Chrome logo from save IBAN prompt.
    This CL removes Chrome logo from save IBAN prompt to make alignment
    with greenline.
    
    UI mock: https://screenshot.googleplex.com/utQZf5HDQEMfCiH
    Screenshot: https://screenshot.googleplex.com/8x5EcPJC4Ew7QUU
    
    Bug: 1349109
    Change-Id: Ia4fb5ca9b2c10aa1465eb78d67ecfd4021696030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4337763
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117666}

--- chrome/browser/ui/views/autofill/payments/payments_view_util.h
+++ chrome/browser/ui/views/autofill/payments/payments_view_util.h
@@ -41,8 +41,6 @@ class TitleWithIconAndSeparatorView : public views::TableLayoutView {
     GOOGLE_PAY,
     // Google super G.
     GOOGLE_G,
-    // Product logo (Logo that represents the user agent).
-    PRODUCT_LOGO,
   };
 
   TitleWithIconAndSeparatorView(const std::u16string& window_title,
