0441816dd00d9438c5286eb021044424d60554d8
Dana Fried <dfried@chromium.org>
2023-03-16 21:48:35 +0000

    [User Education] Fix WebUI tutorial crash bug.
    This fixes a case where a potential UAF was triggered due to a
    combination of WebUI and tutorial help bubble logic. It simplifies the
    closing of help bubbles and makes them more robust to UAF.
    
    A stray log message has also been removed.
    
    A regression test that fails without the fix but passes with it has
    also been added.
    
    Bug: 1425161
    Change-Id: I10ce9c59c716b14001515dc1ea6a0e889a92771c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4346481
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118356}

--- components/user_education/common/help_bubble.h
+++ components/user_education/common/help_bubble.h
@@ -54,16 +54,11 @@ class HelpBubble : public ui::FrameworkSpecificImplementation {
   [[nodiscard]] base::CallbackListSubscription AddOnCloseCallback(
       ClosedCallback callback);
 
-  bool is_open() const { return !is_closed() && !closing_; }
+  bool is_open() const { return !is_closed(); }
 
  protected:
   // Actually close the bubble.
   virtual void CloseBubbleImpl() = 0;
-
-  // Updates internal state to indicate that the bubble has been closed.
-  // Called by Close(), but can also be called if the bubble is closed by user
-  // action, etc.
-  void NotifyBubbleClosed();
 
  private:
   // Closed callbacks are cleared out on close, so this keeps us from having to
@@ -72,7 +67,6 @@ class HelpBubble : public ui::FrameworkSpecificImplementation {
 
   using CallbackList = base::OnceCallbackList<ClosedCallback::RunType>;
   std::unique_ptr<CallbackList> on_close_callbacks_;
-  bool closing_ = false;
 };
 
 }  // namespace user_education
