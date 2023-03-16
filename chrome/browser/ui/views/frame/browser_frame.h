bb68aa9f31b380e8b94ec46d1f1eb2331e708004
Tom Lukaszewicz <tluk@chromium.org>
2023-03-16 20:02:32 +0000

    [color] Populate user_color for autogenerated browser themes
    This CL populates ColorProviderManager::Key::user_color with the
    seed colors used to generate autogenerated theme packs for Chrome
    colors.
    
    Widget::GetUserColor() has been implemented such that all widgets
    belonging to a UI hierarchy share the same user_color.
    
    All changes are currently gated behind the kChromeRefresh2023
    feature flag.
    
    Bug: None
    Change-Id: I5f6277826da4b0d309cec33e24590780bad7d3c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4344000
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118296}

--- chrome/browser/ui/views/frame/browser_frame.h
+++ chrome/browser/ui/views/frame/browser_frame.h
@@ -178,6 +178,7 @@ class BrowserFrame : public views::Widget, public views::ContextMenuController {
  protected:
   // views::Widget:
   ui::ColorProviderManager::Key GetColorProviderKey() const override;
+  absl::optional<SkColor> GetUserColor() const override;
 
  private:
   void OnTouchUiChanged();
--- ui/color/color_provider_source.cc
+++ ui/color/color_provider_source.cc
@@ -30,4 +30,8 @@ void ColorProviderSource::NotifyColorProviderChanged() {
     observer.OnColorProviderChanged();
 }
 
+absl::optional<SkColor> ColorProviderSource::GetUserColor() const {
+  return absl::nullopt;
+}
+
 }  // namespace ui
