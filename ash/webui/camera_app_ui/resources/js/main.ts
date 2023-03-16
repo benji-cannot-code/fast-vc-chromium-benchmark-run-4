9e7dc8b236738358edb47512eab5ecb465955bea
Kam Kwankajornkiet <kamchonlathorn@google.com>
2023-03-16 04:40:08 +0000

    Reland "CCA: Create feature flag for time-lapse recording"
    This is a manual reland of
    commit 7c766a721a7860d408a793688898dc79eb98e1a3
    
    There is no change in this CL as the fix was made in upstream CL.
    
    Original change's description:
    > CCA: Create feature flag for time-lapse recording
    >
    > Create a feature flag CameraAppTimeLapse to enable time-lapse video
    > recording in Chrome Camera App.
    >
    > Bug: b:236800499
    > Test: ./unit_tests --gtest_filter='AboutFlags*'
    > Change-Id: I81abab652bb647e3961f420eea41aece7980e899
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318323
    > Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    > Reviewed-by: Wei Lee <wtlee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1116788}
    
    Bug: b:236800499
    Test: ./unit_tests --gtest_filter='AboutFlags*'
    Change-Id: I4e66a316eea3e0949bc5094d8ef34344331dfe3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4339561
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117938}

--- ash/webui/camera_app_ui/resources/js/main.ts
+++ ash/webui/camera_app_ui/resources/js/main.ts
@@ -13,6 +13,7 @@ import {ModeConstraints} from './device/type.js';
 import * as dom from './dom.js';
 import {reportError} from './error.js';
 import * as expert from './expert.js';
+import {Flag} from './flag.js';
 import {GalleryButton} from './gallerybutton.js';
 import {I18nString} from './i18n_string.js';
 import {Intent} from './intent.js';
@@ -119,6 +120,7 @@ export class App {
     this.setupToggles();
     localStorage.cleanup();
     this.setupEffect();
+    this.setupExperimentalFeatures();
 
     // Set up views navigation by their DOM z-order.
     nav.setup([
@@ -214,6 +216,13 @@ export class App {
       subtree: true,
       childList: true,
     });
+  }
+
+  private setupExperimentalFeatures() {
+    if (loadTimeData.getChromeFlag(Flag.TIME_LAPSE)) {
+      const modeButton = dom.get('#time-lapse-mode', HTMLDivElement);
+      modeButton.classList.remove('hidden');
+    }
   }
 
   /**
