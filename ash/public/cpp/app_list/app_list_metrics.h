bbc33610422f1f71b88aa18a226eb165a2adc9c3
Dmitry Grebenyuk <dgrebenyuk@google.com>
2023-03-16 04:08:37 +0000

    Add Image Search to app_list
    Add Image search to app_list make UI working.
    
    Test: "on DUT I cant click a search result image"
    Bug: b:260646344
    Change-Id: Ia80d8139f0a021deafc01c49c2490749437a8581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4339178
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117928}

--- ash/public/cpp/app_list/app_list_metrics.h
+++ ash/public/cpp/app_list/app_list_metrics.h
@@ -181,6 +181,8 @@ enum SearchResultType {
   BRUSCHETTA_APP,
   // A System Info Answer Card Result.
   SYSTEM_INFO,
+  // A local image search result.
+  IMAGE_SEARCH,
   // Boundary is always last.
   SEARCH_RESULT_TYPE_BOUNDARY
 };
