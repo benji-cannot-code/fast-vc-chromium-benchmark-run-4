b9001c5b1b139d535d6404cb5838efad6af4c9ab
Peter Boström <pbos@chromium.org>
2023-03-16 18:25:23 +0000

    Remove redundant DWC in BrowserContextImpl
    NOTREACHED() currently uploads using DumpWithoutCrashing so we get
    duplicate reports for all of these.
    
    Bug: 1423567
    Change-Id: I9953c10772a0c7aba5ffee0c4c8b57a2aa5eb96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4345488
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118223}

--- content/browser/browser_context_impl.cc
+++ content/browser/browser_context_impl.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/memory/ref_counted.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/trace_event/trace_event.h"
@@ -77,7 +76,6 @@ BrowserContextImpl::~BrowserContextImpl() {
 
   if (!will_be_destroyed_soon_) {
     NOTREACHED();
-    base::debug::DumpWithoutCrashing();
   }
 
   // Verify that there are no outstanding RenderProcessHosts that reference
@@ -100,7 +98,6 @@ BrowserContextImpl::~BrowserContextImpl() {
     SCOPED_CRASH_KEY_STRING256("BrowserContext", "dangling_rph",
                                rph_crash_key_value);
     NOTREACHED() << "rph_with_bc_reference : " << rph_crash_key_value;
-    base::debug::DumpWithoutCrashing();
   }
 
   // Clean up any isolated origins and other security state associated with this
