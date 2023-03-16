62bef1547e5ae13be78ae709feaccfeab344ef91
Eric Sum <esum@google.com>
2023-03-16 02:05:37 +0000

    Add ambient video selection options to chrome://flags.
    These will be used temporarily for development until the Hub
    changes are ready and the video can be selected there. When that's
    ready, this CL can be reverted.
    
    Bug: b:271180547
    Change-Id: Iea3b53804e1217a996ad6cc9f2900c120fec265c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335242
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117910}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_CONSTANTS_ASH_FEATURES_H_
 #define ASH_CONSTANTS_ASH_FEATURES_H_
 
+#include "ash/constants/ambient_video.h"
 #include "base/component_export.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
@@ -571,6 +572,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kTerminalMultiProfile);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kTerminalSftp);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kTerminalTmuxIntegration);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kTimeOfDayScreenSaver);
+COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::FeatureParam<AmbientVideo> kTimeOfDayScreenSaverVideo;
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kTimeOfDayWallpaper);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kTrafficCountersEnabled);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kTrilinearFiltering);
