85880ba3f18339a0f441923de42fc1bac3d2dfca
Ewann Pelle <ewannpv@chromium.org>
2023-03-16 16:48:15 +0000

    [iOS][PT] Remove pinned tab overlay when dropSessionDidEnd
    Fixed: 1425105
    Change-Id: I6cc98e41c878c4a4e89728f5e36917bd4ce66cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4346253
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118152}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.mm
@@ -524,9 +524,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 - (void)collectionView:(UICollectionView*)collectionView
      dropSessionDidEnd:(id<UIDropSession>)session {
   [self.delegate pinnedViewControllerDropAnimationDidEnd:self];
-
-  // Reset the background if the drop cames from another app.
-  [self resetViewBackgrounds];
+  [self dropAnimationDidEnd];
 }
 
 - (UICollectionViewDropProposal*)
