99f95836799efa90483f5797c4f151756a154b3c
Nan Lin <linnan@chromium.org>
2023-03-16 17:46:01 +0000

    Revert "Remove AttributionDataHostManager::NotifyFencedFrameReportingBeaconSent"
    This reverts commit da8e7069838858fd5184c1e3f5eeb00d42d4317d.
    
    Reason for revert: Registering source eligible data host when the
    beacon is sent may cause more triggers to be buffered, but it's
    necessary to ensure that for navigation beacons triggers registered
    on the landing page are buffered properly even if the beacon response
    is delivered late.
    
    Original change's description:
    > Remove AttributionDataHostManager::NotifyFencedFrameReportingBeaconSent
    >
    > The source eligible data host can be registered when the first source
    > data is available, which is more aligned with navigation redirect.
    >
    >
    > Change-Id: Id13693545cf7c16d7c5e6dddf1e7ff8eb84c2605
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4322168
    > Auto-Submit: Nan Lin <linnan@chromium.org>
    > Commit-Queue: Garrett Tanzer <gtanzer@chromium.org>
    > Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    > Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1116409}
    
    Change-Id: I0c9f71a6615a134ccb271f318da52344cf18ba39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4345673
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118195}

--- content/browser/attribution_reporting/attribution_host_unittest.cc
+++ content/browser/attribution_reporting/attribution_host_unittest.cc
@@ -136,6 +136,11 @@ class MockDataHostManager : public AttributionDataHostManager {
               (override));
 
   MOCK_METHOD(void,
+              NotifyFencedFrameReportingBeaconSent,
+              (BeaconId beacon_id),
+              (override));
+
+  MOCK_METHOD(void,
               NotifyFencedFrameReportingBeaconData,
               (BeaconId beacon_id,
                url::Origin reporting_origin,
