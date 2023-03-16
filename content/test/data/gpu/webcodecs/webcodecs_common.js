160fad52bbcf1401f93bc4d0b1f3f027df051414
Eugene Zemtsov <eugene@chromium.org>
2023-03-16 01:57:02 +0000

    webcodecs: Strict enforcement of requested bitrate mode in hw encoders
    Old Android encoders don't support VBR, that's why all tests that don't
    care about bitrate are switched to CBR.
    
    Bug: 1424874
    Change-Id: I96e4408639f84cb07fcc8bb07869cc03c3c8bb93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342603
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117908}

--- content/test/data/gpu/webcodecs/webcodecs_common.js
+++ content/test/data/gpu/webcodecs/webcodecs_common.js
@@ -342,7 +342,8 @@ async function prepareDecoderSource(
     codec: codec,
     width: width,
     height: height,
-    bitrate: 1000000,
+    bitrate: 2000000,
+    bitrateMode: 'constant',
     framerate: 24
   };
 
