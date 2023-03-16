6054ce2c7c01cb46a18210e71bbad1b6f95d454e
Xiyuan Xia <xiyuan@chromium.org>
2023-03-16 16:24:35 +0000

    Revert "Do not pause compositor when video capture lock exists"
    This reverts commit eb7d2ff7769839ada39029d1b4701d5be539fda9.
    
    Reason for revert:
    crbug.com/1423172 Break ui.WindowArrangementCUJ.lacros.
    
    Original change's description:
    > Do not pause compositor when video capture lock exists
    >
    > Currently, the aura desktop window tree host will ALWAYS hide the
    > content and pause the compositor when the minimize state is received.
    > This breaks features which require the content (e.g.: mirroring) and
    > features which require that the compositor continues rendering frames
    > (e.g.: PiP). This change changes the tree host to ensure that there
    > are no video locks before attempting to pause the compositor.
    >
    > This change requires the video lock to be created in platforms which
    > do not support native occlusion tracking. The API is changed such that
    > the lock will always be created, but the state update will only run if
    > the platform supports native occlusion tracking.
    >
    > Web contents is also modified to create a video capture lock if
    > SetHasPictureInPicture* is called.
    >
    > Bug: 1325594
    > Change-Id: I87323d39ff6df031088ede7a3dc263f68ec2d467
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133888
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Fred Shih <ffred@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1093697}
    
    Bug: 1325594, 1423172
    Change-Id: Ib2f848abbbd719d76bfc42fe9cfcf15d64516108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342922
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118138}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2202,11 +2202,6 @@ void WebContentsImpl::SetHasPictureInPictureCommon(
   NotifyNavigationStateChanged(INVALIDATE_TYPE_TAB);
   observers_.NotifyObservers(&WebContentsObserver::MediaPictureInPictureChanged,
                              has_picture_in_picture);
-  if (has_picture_in_picture) {
-    pip_capture_handle_ = IncrementCapturerCount({}, true, false);
-  } else {
-    pip_capture_handle_.RunAndReset();
-  }
 
   // Picture-in-picture state can affect how we notify visibility for non-
   // visible pages.
@@ -3696,7 +3691,8 @@ PageVisibilityState WebContentsImpl::CalculatePageVisibilityState(
   if (visibility == Visibility::VISIBLE || visible_capturer_count_ > 0 ||
       web_contents_visible_in_vr) {
     return PageVisibilityState::kVisible;
-  } else if (hidden_capturer_count_ > 0) {
+  } else if (hidden_capturer_count_ > 0 || has_picture_in_picture_video_ ||
+             has_picture_in_picture_document_) {
     return PageVisibilityState::kHiddenButPainting;
   }
   return PageVisibilityState::kHidden;
