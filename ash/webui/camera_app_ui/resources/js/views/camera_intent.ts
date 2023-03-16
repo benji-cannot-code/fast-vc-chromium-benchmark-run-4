ca10fb4869afdb8b0945235557233ece5878b25b
Kam Kwankajornkiet <kamchonlathorn@google.com>
2023-03-16 03:56:33 +0000

    Reland "CCA: Add minimum working code for time-lapse recording"
    This is a reland of commit ac3bc1d67cd6069422f7ac3f148ef8c1a2841a70
    
    Difference from the original CL: Change video file creation from calling
    createVideoFile to calling createPrivateTempVideoFile instead.
    
    Original change's description:
    > CCA: Add minimum working code for time-lapse recording
    >
    > Add minimum code required for time-lapse video recording, with auto
    > speed adjustment. The implementations are mainly
    > 1. Use MediaStreamTrackProcessor to grab frame from video stream.
    > 2. Use VideoEncoder to encode a frame into an encoded chunk.
    > 3. Use FFMpeg to mux selected frames into a final file.
    >
    > This change is supposed to be submitted together with the change which
    > introduces UI implementation. Optimizations for time and quality
    > performance are also expected to be done.
    >
    > Bug: b:236800499
    > Test: Manually
    > Change-Id: I424559d648114eb92592937590f47bf5a2c94f8d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4274266
    > Reviewed-by: Wei Lee <wtlee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1116786}
    
    Bug: b:236800499
    Change-Id: I8e3017e7d5d2d73c1a24c2fc1ddfde10274eb7f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334798
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117927}

--- ash/webui/camera_app_ui/resources/js/views/camera_intent.ts
+++ ash/webui/camera_app_ui/resources/js/views/camera_intent.ts
@@ -60,6 +60,7 @@ export class CameraIntent extends Camera {
             return VideoSaver.createForIntent(intent, outputVideoRotation);
           },
           finishSaveVideo: async (video) => {
+            assert(video instanceof VideoSaver);
             this.videoResultFile = await video.endWrite();
           },
           saveGif: () => {
