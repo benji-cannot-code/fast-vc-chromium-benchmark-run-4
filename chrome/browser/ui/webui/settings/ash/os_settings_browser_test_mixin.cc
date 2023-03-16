a331bc2a3df67e35ba2030f3d44ff02dd3b50e38
dpapad <dpapad@chromium.org>
2023-03-16 19:21:45 +0000

    WebUI: Remove unnecessary "generated" part in webui_generated_resources.
    The "_generated" part of webui_generated_test_resources.{h,cc,pak} only
    existed to differentiate with the checked-in webui_test_resources.grd
    file, which already produced webui_test_resources.{h,cc,pak} files.
    
    As of http://crrev.com/c/4342171, the checked in
    webui_test_resources.grd file has been deleted and there is no longer
    a reason to keep using the "_generated" terminology. Removing it to
    reduce clutter.
    
    Bug: 1132403
    Change-Id: I938f2dca118753aecb9c617431ee0141cabddb06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335948
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118270}

--- chrome/browser/ui/webui/settings/ash/os_settings_browser_test_mixin.cc
+++ chrome/browser/ui/webui/settings/ash/os_settings_browser_test_mixin.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/webui/settings/ash/os_settings_ui.h"
 #include "chrome/test/base/web_ui_test_data_source.h"
-#include "chrome/test/data/grit/webui_generated_test_resources_map.h"
 #include "chrome/test/data/webui/settings/chromeos/test_api.test-mojom-test-utils.h"
 #include "chrome/test/data/webui/settings/chromeos/test_api.test-mojom.h"
 #include "content/public/browser/web_ui_data_source.h"
--- chrome/test/data/webui/mojo/mojo_web_ui_controller_browsertest.cc
+++ chrome/test/data/webui/mojo/mojo_web_ui_controller_browsertest.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
-#include "chrome/test/data/grit/webui_generated_test_resources.h"
+#include "chrome/test/data/grit/webui_test_resources.h"
 #include "chrome/test/data/webui/mojo/foobar.mojom.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_process_host_observer.h"
