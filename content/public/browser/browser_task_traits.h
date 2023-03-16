bb877d304ce506c907ab3e2b342e09d912c6f4b3
Michael Thiessen <mthiesse@chromium.org>
2023-03-16 03:14:53 +0000

    Remove NonNestable from BrowserTaskTraits
    NonNestable is no longer used (and we'd like to eventually not support
    NonNestable tasks entirely).
    
    Bug: 876272, 1424788
    Change-Id: I750dbd29104b7439ee8bc87fe0efcc032f8c2d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342817
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117920}

--- content/public/browser/browser_task_traits.h
+++ content/public/browser/browser_task_traits.h
@@ -13,15 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// Tasks with this trait will not be executed inside a nested RunLoop.
-//
-// Note: This should rarely be required. Drivers of nested loops should instead
-// make sure to be reentrant when allowing nested application tasks (also rare).
-//
-// TODO(https://crbug.com/876272): Investigate removing this trait -- and any
-// logic for deferred tasks in MessageLoop.
-struct NonNestable {};
-
 // Semantic annotations which tell the scheduler what type of task it's dealing
 // with. This will be used by the scheduler for dynamic prioritization and for
 // attribution in traces, etc... In general, BrowserTaskType::kDefault is what
@@ -77,7 +68,6 @@ class CONTENT_EXPORT BrowserTaskTraitsExtension {
 
     ValidTrait(BrowserThread::ID);
     ValidTrait(BrowserTaskType);
-    ValidTrait(NonNestable);
   };
 
   template <
@@ -90,26 +80,23 @@ class CONTENT_EXPORT BrowserTaskTraitsExtension {
                                          BrowserThread::ID_COUNT>(args...)),
         task_type_(
             base::trait_helpers::GetEnum<BrowserTaskType,
-                                         BrowserTaskType::kDefault>(args...)),
-        nestable_(!base::trait_helpers::HasTrait<NonNestable, ArgTypes...>()) {}
+                                         BrowserTaskType::kDefault>(args...)) {}
 
   // Keep in sync with UiThreadTaskTraits.java
   constexpr base::TaskTraitsExtensionStorage Serialize() const {
-    static_assert(12 == sizeof(BrowserTaskTraitsExtension),
+    static_assert(8 == sizeof(BrowserTaskTraitsExtension),
                   "Update Serialize() and Parse() when changing "
                   "BrowserTaskTraitsExtension");
-    return {
-        kExtensionId,
-        {static_cast<uint8_t>(browser_thread_),
-         static_cast<uint8_t>(task_type_), static_cast<uint8_t>(nestable_)}};
+    return {kExtensionId,
+            {static_cast<uint8_t>(browser_thread_),
+             static_cast<uint8_t>(task_type_)}};
   }
 
   static const BrowserTaskTraitsExtension Parse(
       const base::TaskTraitsExtensionStorage& extension) {
     return BrowserTaskTraitsExtension(
         static_cast<BrowserThread::ID>(extension.data[0]),
-        static_cast<BrowserTaskType>(extension.data[1]),
-        static_cast<bool>(extension.data[2]));
+        static_cast<BrowserTaskType>(extension.data[1]));
   }
 
   constexpr BrowserThread::ID browser_thread() const {
@@ -122,20 +109,13 @@ class CONTENT_EXPORT BrowserTaskTraitsExtension {
 
   constexpr BrowserTaskType task_type() const { return task_type_; }
 
-  // Returns true if tasks with these traits may run in a nested RunLoop.
-  constexpr bool nestable() const { return nestable_; }
-
  private:
   BrowserTaskTraitsExtension(BrowserThread::ID browser_thread,
-                             BrowserTaskType task_type,
-                             bool nestable)
-      : browser_thread_(browser_thread),
-        task_type_(task_type),
-        nestable_(nestable) {}
+                             BrowserTaskType task_type)
+      : browser_thread_(browser_thread), task_type_(task_type) {}
 
   BrowserThread::ID browser_thread_;
   BrowserTaskType task_type_;
-  bool nestable_;
 };
 
 template <class... ArgTypes,
@@ -152,7 +132,6 @@ class CONTENT_EXPORT BrowserTaskTraits : public base::TaskTraits {
  public:
   struct ValidTrait : public base::TaskTraits::ValidTrait {
     ValidTrait(BrowserTaskType);
-    ValidTrait(NonNestable);
 
     // TODO(1026641): Reconsider whether BrowserTaskTraits should really be
     // supporting base::TaskPriority.
@@ -165,8 +144,8 @@ class CONTENT_EXPORT BrowserTaskTraits : public base::TaskTraits {
     ValidTrait(base::TaskShutdownBehavior);
   };
 
-  // TODO(1026641): Get rid of BrowserTaskTraitsExtension and store its members
-  // (|task_type_| & |nestable_|) directly in BrowserTaskTraits.
+  // TODO(1026641): Get rid of BrowserTaskTraitsExtension and store its member
+  // |task_type_| directly in BrowserTaskTraits.
   template <
       class... ArgTypes,
       class CheckArgumentsAreValid = std::enable_if_t<
@@ -175,11 +154,6 @@ class CONTENT_EXPORT BrowserTaskTraits : public base::TaskTraits {
 
   BrowserTaskType task_type() {
     return GetExtension<BrowserTaskTraitsExtension>().task_type();
-  }
-
-  // Returns true if tasks with these traits may run in a nested RunLoop.
-  bool nestable() const {
-    return GetExtension<BrowserTaskTraitsExtension>().nestable();
   }
 };
 
