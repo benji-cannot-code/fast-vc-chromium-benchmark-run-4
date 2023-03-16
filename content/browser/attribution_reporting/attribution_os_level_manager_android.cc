c6d850062a79f3a12fa0132ec6cfcb7660a30b4e
Andrew Paseltiner <apaseltiner@chromium.org>
2023-03-16 22:19:11 +0000

    Remove unnecessary AtomicSequenceNumber from OS Attribution Manager
    This class is accessed on a single sequence; it is simpler to have an
    integer field.
    
    Change-Id: I4b69e21ac7a539ad1d72e9f61d6a54a88e3a54f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4345599
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118380}

--- content/browser/attribution_reporting/attribution_os_level_manager_android.cc
+++ content/browser/attribution_reporting/attribution_os_level_manager_android.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_array.h"
 #include "base/android/scoped_java_ref.h"
-#include "base/atomic_sequence_num.h"
 #include "base/dcheck_is_on.h"
 #include "base/functional/callback.h"
 #include "base/no_destructor.h"
@@ -194,8 +193,7 @@ void AttributionOsLevelManagerAndroid::ClearData(
         return url::GURLAndroid::FromNativeGURL(env, origin.GetURL());
       });
 
-  static base::AtomicSequenceNumber g_request_id_counter;
-  int request_id = g_request_id_counter.GetNext();
+  int request_id = next_pending_data_deletion_callback_id_++;
   pending_data_deletion_callbacks_.emplace(request_id, std::move(done));
 
   Java_AttributionOsLevelManager_deleteRegistrations(
