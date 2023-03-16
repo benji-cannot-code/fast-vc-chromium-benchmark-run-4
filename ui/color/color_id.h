e9e57e7ce545163c2335e59aeb6596252138b669
Tom Lukaszewicz <tluk@chromium.org>
2023-03-16 20:36:42 +0000

    [color] Add theme specific sys token overrides
    This CL adds sys token mixer definitions for when the user_color
    is defined in the ColorProviderManager::Key.
    
    The intention is to have sys tokens mapped slighyly differently
    when a custom color is specified by the user. The mappings
    reflect the spec in go/chrome-desktop-tokens.
    
    Bug: 1399923
    Change-Id: I580bff9db923947b368e4bb2ea23a78de0c25d59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4346712
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118317}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefPrimary0, kUiColorsStart, kUiColorsStart) \
   E_CPONLY(kColorRefPrimary10) \
   E_CPONLY(kColorRefPrimary20) \
+  E_CPONLY(kColorRefPrimary25) \
   E_CPONLY(kColorRefPrimary30) \
   E_CPONLY(kColorRefPrimary40) \
   E_CPONLY(kColorRefPrimary50) \
@@ -31,7 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefSecondary10) \
   E_CPONLY(kColorRefSecondary15) \
   E_CPONLY(kColorRefSecondary20) \
+  E_CPONLY(kColorRefSecondary25) \
   E_CPONLY(kColorRefSecondary30) \
+  E_CPONLY(kColorRefSecondary35) \
   E_CPONLY(kColorRefSecondary40) \
   E_CPONLY(kColorRefSecondary50) \
   E_CPONLY(kColorRefSecondary60) \
@@ -95,6 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefNeutral100) \
   E_CPONLY(kColorRefNeutralVariant0) \
   E_CPONLY(kColorRefNeutralVariant10) \
+  E_CPONLY(kColorRefNeutralVariant15) \
   E_CPONLY(kColorRefNeutralVariant20) \
   E_CPONLY(kColorRefNeutralVariant30) \
   E_CPONLY(kColorRefNeutralVariant40) \
