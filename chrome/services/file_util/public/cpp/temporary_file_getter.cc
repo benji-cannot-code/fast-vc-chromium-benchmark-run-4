e3b03e7d471b2f673d88b8fe5b6be156eba4d2e2
Adam Psarouthakis <psarouthakis@google.com>
2023-03-16 17:52:14 +0000

    Nested Zip Analyzer
    This CL contains the implementation for unpacking nested zip file by the
    safe archive analyzer. Unpacking nested zipped files allows the Chrome
    to combat cookie theft.
    
    Bug: 1373671
    Change-Id: Ic07540e787aa5a3065f7cf3d57bef1b853370f59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295078
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1118200}

--- chrome/services/file_util/public/cpp/temporary_file_getter.cc
+++ chrome/services/file_util/public/cpp/temporary_file_getter.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 
 namespace {
+
 constexpr int kMaxNumberOfFilesAllowed = 10;
 
 base::File TemporaryFileGetterHelper(int num_files_requested) {
