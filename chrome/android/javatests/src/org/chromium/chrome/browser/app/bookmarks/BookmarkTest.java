b7d4c6d668ea7f0a54c8668ced4a96bc708691bd
Sky Malice <skym@chromium.org>
2023-03-16 22:14:30 +0000

    Revert "Adjust bookmark ID capitilization."
    This reverts commit 198529baecb05f2c6abcc0bf603fcfa6679e4b48.
    
    Reason for revert: Breaks downstream PartnerBookmarksIntegrationTest.java
    
    Original change's description:
    > Adjust bookmark ID capitilization.
    >
    > Change-Id: Ibe426fb66df3b67afcaf196034bf91d2f9438079
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347412
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1118355}
    
    Change-Id: Ib4b06d6ebea5944a91201ed869c8e9d04d864294
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347994
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118374}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
@@ -910,7 +910,7 @@ public class BookmarkTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Assert.assertEquals("Bookmarks were not added in the expected order.", initial,
-                    mBookmarkModel.getChildIds(mBookmarkModel.getDefaultFolder()).subList(0, 3));
+                    mBookmarkModel.getChildIDs(mBookmarkModel.getDefaultFolder()).subList(0, 3));
         });
 
         expected.add(fooId);
@@ -951,7 +951,7 @@ public class BookmarkTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             List<BookmarkId> observed =
-                    mBookmarkModel.getChildIds(mBookmarkModel.getDefaultFolder());
+                    mBookmarkModel.getChildIDs(mBookmarkModel.getDefaultFolder());
             // Exclude partner bookmarks folder
             Assert.assertEquals(expected, observed.subList(0, 3));
             Assert.assertTrue("The selected item should stay selected", foo.isItemSelected());
@@ -975,7 +975,7 @@ public class BookmarkTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Assert.assertEquals("Bookmarks were not added in the expected order.", initial,
-                    mBookmarkModel.getChildIds(mBookmarkModel.getDefaultFolder()).subList(0, 4));
+                    mBookmarkModel.getChildIDs(mBookmarkModel.getDefaultFolder()).subList(0, 4));
         });
 
         expected.add(cId);
@@ -1018,7 +1018,7 @@ public class BookmarkTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             List<BookmarkId> observed =
-                    mBookmarkModel.getChildIds(mBookmarkModel.getDefaultFolder());
+                    mBookmarkModel.getChildIDs(mBookmarkModel.getDefaultFolder());
             // Exclude partner bookmarks folder
             Assert.assertEquals(expected, observed.subList(0, 4));
             Assert.assertTrue("The selected item should stay selected", test.isItemSelected());
@@ -1040,7 +1040,7 @@ public class BookmarkTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Assert.assertEquals("Bookmarks were not added in the expected order.", initial,
-                    mBookmarkModel.getChildIds(mBookmarkModel.getDefaultFolder()).subList(0, 3));
+                    mBookmarkModel.getChildIDs(mBookmarkModel.getDefaultFolder()).subList(0, 3));
         });
 
         expected.add(bId);
@@ -1081,7 +1081,7 @@ public class BookmarkTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             List<BookmarkId> observed =
-                    mBookmarkModel.getChildIds(mBookmarkModel.getDefaultFolder());
+                    mBookmarkModel.getChildIDs(mBookmarkModel.getDefaultFolder());
             // Exclude partner bookmarks folder
             Assert.assertEquals(expected, observed.subList(0, 3));
             Assert.assertTrue("The selected item should stay selected", test.isItemSelected());
--- chrome/browser/bookmarks/android/bookmark_bridge.cc
+++ chrome/browser/bookmarks/android/bookmark_bridge.cc
@@ -273,7 +273,7 @@ void BookmarkBridge::LoadFakePartnerBookmarkShimForTesting(
   DCHECK(partner_bookmarks_shim_->IsLoaded());
 }
 
-ScopedJavaLocalRef<jobject> BookmarkBridge::GetBookmarkById(
+ScopedJavaLocalRef<jobject> BookmarkBridge::GetBookmarkByID(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     jlong id,
@@ -290,7 +290,7 @@ bool BookmarkBridge::IsDoingExtensiveChanges(JNIEnv* env,
   return bookmark_model_->IsDoingExtensiveChanges();
 }
 
-void BookmarkBridge::GetTopLevelFolderParentIds(
+void BookmarkBridge::GetTopLevelFolderParentIDs(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& j_result_obj) {
@@ -300,7 +300,7 @@ void BookmarkBridge::GetTopLevelFolderParentIds(
       GetBookmarkType(bookmark_model_->root_node()));
 }
 
-void BookmarkBridge::GetTopLevelFolderIds(
+void BookmarkBridge::GetTopLevelFolderIDs(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     jboolean get_special,
@@ -489,7 +489,7 @@ jint BookmarkBridge::GetChildCount(JNIEnv* env,
   return static_cast<jint>(node->children().size());
 }
 
-void BookmarkBridge::GetChildIds(JNIEnv* env,
+void BookmarkBridge::GetChildIDs(JNIEnv* env,
                                  const JavaParamRef<jobject>& obj,
                                  jlong id,
                                  jint type,
