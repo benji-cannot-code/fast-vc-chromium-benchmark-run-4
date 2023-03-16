bcec93352bbe04064c4a3fcccb893e68178f1600
Steve Cho <stevecho@google.com>
2023-03-16 22:07:41 +0000

    Enable video decoder fallback rate per codec
    This CL makes changes to collect UMA "Media.VideoDecoderFallback" per
    video codec. This will be useful to detect any unexpected video decoder
    fallback rate increase even for less used video codecs.
    
    Bug: b:273578319
    Change-Id: Ib017caedf5b9e5fee672004844c81ac8f0ff1064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336173
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118366}

--- media/mojo/services/media_metrics_provider_unittest.cc
+++ media/mojo/services/media_metrics_provider_unittest.cc
@@ -227,7 +227,8 @@ TEST_F(MediaMetricsProviderTest, TestPipelineUMA) {
   base::RunLoop().RunUntilIdle();
   histogram_tester.ExpectBucketCount("Media.PipelineStatus.AudioVideo.VP9.SW",
                                      PIPELINE_OK, 1);
-  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback", false, 1);
+  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback.VP9", false,
+                                     1);
   histogram_tester.ExpectBucketCount("Media.HasEverPlayed", true, 1);
 }
 
@@ -247,7 +248,8 @@ TEST_F(MediaMetricsProviderTest, TestPipelineUMAMediaStream) {
   base::RunLoop().RunUntilIdle();
   histogram_tester.ExpectBucketCount("Media.PipelineStatus.AudioVideo.VP9.SW",
                                      PIPELINE_OK, 0);
-  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback", false, 0);
+  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback.VP9", false,
+                                     0);
   histogram_tester.ExpectBucketCount("Media.HasEverPlayed", true, 0);
 }
 
@@ -264,7 +266,8 @@ TEST_F(MediaMetricsProviderTest, TestPipelineUMANoAudioWithEme) {
   base::RunLoop().RunUntilIdle();
   histogram_tester.ExpectBucketCount("Media.PipelineStatus.VideoOnly",
                                      PIPELINE_OK, 1);
-  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback", false, 1);
+  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback.AV1", false,
+                                     1);
   histogram_tester.ExpectBucketCount("Media.HasEverPlayed", true, 1);
   histogram_tester.ExpectBucketCount("Media.EME.IsIncognito", false, 1);
 }
@@ -286,7 +289,7 @@ TEST_F(MediaMetricsProviderTest, TestPipelineUMADecoderFallback) {
   base::RunLoop().RunUntilIdle();
   histogram_tester.ExpectBucketCount("Media.PipelineStatus.AudioVideo.VP9.HW",
                                      PIPELINE_OK, 1);
-  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback", true, 1);
+  histogram_tester.ExpectBucketCount("Media.VideoDecoderFallback.VP9", true, 1);
   histogram_tester.ExpectBucketCount("Media.HasEverPlayed", true, 1);
 }
 
