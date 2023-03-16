a5710e65703c4b4741c8379f5bba4d3441d7877e
Kenichi Ishibashi <bashi@chromium.org>
2023-03-16 07:31:53 +0000

    IWYU: Include <iterator> in host_resolver_source.h
    Bug: N/A
    Change-Id: I23b65404580ab9c2a9ca6f2528bce5c57f6eb6e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4343831
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117970}

--- net/dns/public/host_resolver_source.h
+++ net/dns/public/host_resolver_source.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_DNS_PUBLIC_HOST_RESOLVER_SOURCE_H_
 #define NET_DNS_PUBLIC_HOST_RESOLVER_SOURCE_H_
 
+#include <iterator>
+
 namespace net {
 
 // Enumeration to specify the allowed results source for HostResolver
