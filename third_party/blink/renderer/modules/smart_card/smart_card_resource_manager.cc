4b0de2b624ff73caddb1b4367c6656ccf0773b9b
Daniel d'Andrada <dandrader@google.com>
2023-03-16 10:03:00 +0000

    SmartCard: add more response codes
    This enables the browser to inform the renderer about more
    possible errors coming from the platform's PC/SC stack.
    
    They are surfaced in the Web API either as SmartCardErrors or
    regular DOMExceptions with custom messages.
    
    There's also one new response code in the Web API itself,
    "server-too-busy", as Web applications can handle that differently
    (eg: try again with an exponential backoff).
    
    Bug: 1386175
    Change-Id: I8b1e7166f68162750245c9cb6eb8510609e239d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334291
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118003}

--- third_party/blink/renderer/modules/smart_card/smart_card_resource_manager.cc
+++ third_party/blink/renderer/modules/smart_card/smart_card_resource_manager.cc
@@ -163,8 +163,7 @@ void SmartCardResourceManager::FinishGetReaders(
   get_readers_promises_.erase(resolver);
 
   if (result->is_response_code()) {
-    auto* error =
-        MakeGarbageCollected<SmartCardError>(result->get_response_code());
+    auto* error = SmartCardError::Create(result->get_response_code());
     resolver->Reject(error);
     return;
   }
