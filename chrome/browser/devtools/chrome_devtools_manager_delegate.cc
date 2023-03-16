ada5dcd293df78ecb1ec8457942029a1e6130210
Danil Somsikov <dsv@chromium.org>
2023-03-16 07:54:04 +0000

    Add a dedicated target type for the extension popups
    Bug: 1414330
    Change-Id: I04fcae59c07ebebc40c8f8cb2ed53d40272d531c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245598
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117976}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.cc
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.cc
@@ -48,7 +48,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/process_manager.h"
+#include "extensions/browser/view_type_utils.h"
 #include "extensions/common/manifest.h"
+#include "extensions/common/mojom/view_type.mojom.h"
 #include "ui/base/resource/resource_bundle.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -63,6 +65,7 @@ using content::DevToolsAgentHost;
 const char ChromeDevToolsManagerDelegate::kTypeApp[] = "app";
 const char ChromeDevToolsManagerDelegate::kTypeBackgroundPage[] =
     "background_page";
+const char ChromeDevToolsManagerDelegate::kTypePage[] = "page";
 
 namespace {
 
@@ -91,7 +94,11 @@ bool GetExtensionInfo(content::WebContents* wc,
     *type = ChromeDevToolsManagerDelegate::kTypeApp;
     return true;
   }
-  return false;
+  // Note that we are intentionally not setting name here, so that we can
+  // construct a name based on the URL or page title in
+  // RenderFrameDevToolsAgentHost::GetTitle()
+  *type = ChromeDevToolsManagerDelegate::kTypePage;
+  return true;
 }
 
 ChromeDevToolsManagerDelegate* g_instance;
