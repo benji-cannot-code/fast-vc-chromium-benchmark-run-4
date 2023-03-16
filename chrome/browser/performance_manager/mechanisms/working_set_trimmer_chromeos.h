b255484d85e788f87e87cc98fd768edeb2a5a8aa
Alex Gimenez <raging@google.com>
2023-03-16 08:06:12 +0000

    Reclaim ARCVM memory from guest when enabled
    TEST=ash_components_unittests
    TEST=unit_tests
    TEST=Manually verified ARC ZRAM usage is increased under moderate memory
    pressure with features enabled together with http://ag/21892823
    BUG=b:266857252
    
    Change-Id: Ic342f4eeee907dcebb4e9ab24b09a30860c86709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316166
    Commit-Queue: Alexandre Marciano Gimenez <raging@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Alexandre Marciano Gimenez <raging@google.com>
    Auto-Submit: Hung Nguyen <hungmn@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1117980}

--- chrome/browser/performance_manager/mechanisms/working_set_trimmer_chromeos.h
+++ chrome/browser/performance_manager/mechanisms/working_set_trimmer_chromeos.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PERFORMANCE_MANAGER_MECHANISMS_WORKING_SET_TRIMMER_CHROMEOS_H_
 #define CHROME_BROWSER_PERFORMANCE_MANAGER_MECHANISMS_WORKING_SET_TRIMMER_CHROMEOS_H_
 
+#include "ash/components/arc/mojom/memory.mojom-forward.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/no_destructor.h"
@@ -73,6 +74,11 @@ class WorkingSetTrimmerChromeOS : public WorkingSetTrimmer {
                          ArcVmReclaimType reclaim_type,
                          int page_limit,
                          bool result);
+  void OnArcVmMemoryGuestReclaim(TrimArcVmWorkingSetCallback callback,
+                                 arc::mojom::ReclaimResultPtr result);
+
+  void LogErrorAndInvokeCallback(const char* error,
+                                 TrimArcVmWorkingSetCallback callback);
 
   // The constructor is made private to prevent instantiation of this class
   // directly, it should always be retrieved via
