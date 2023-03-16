e37f4446c6e6c3b0291c946c9527e0562fdb8511
Tom Sepez <tsepez@chromium.org>
2023-03-16 23:06:40 +0000

    Avoid dangling ptr warning in ~TestBluetoothLocalGattServiceDelegate()
    In BluetoothGattServerTest, `delegate_` and `service_` have cyclic
    unowned references to each other, so we must manually break one before
    continuing with TearDown(). Do so in CompleteGattSetup() to balance
    out the calls in StartGattSetup() where the cycles are created.
    
    Bug: 1401495
    Change-Id: I0d00dfd16e44fb466365b81e69f01b0de60e4793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347698
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118417}

--- device/bluetooth/test/bluetooth_gatt_server_test.cc
+++ device/bluetooth/test/bluetooth_gatt_server_test.cc
@@ -25,6 +25,7 @@ void BluetoothGattServerTest::StartGattSetup() {
 }
 
 void BluetoothGattServerTest::CompleteGattSetup() {
+  delegate_->set_expected_service(nullptr);
   service_->Register(GetCallback(Call::EXPECTED),
                      GetGattErrorCallback(Call::NOT_EXPECTED));
 }
