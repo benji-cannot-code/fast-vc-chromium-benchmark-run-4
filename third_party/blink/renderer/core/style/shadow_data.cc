29d99af0074671b1c5cbdf7565b8e95e30a029c9
Aaron Krajeski <aaronhk@chromium.org>
2023-03-16 16:17:04 +0000

    Make color hint interpolation match the gradient
    Color hints were previously being generated by a Blend() function that
    produced sRGBLegacy colors, no matter what gradient interpolation was
    set. This CL changes gradient hints to use the new
    Color::InterpolateColors function that can handle different color
    spaces. graphics/color_blend.h is no longer used and deleted.
    
    In the spec, when no interpolation space is specified, "legacy" colors
    are interpolated in the sRGB color space while non-legacy colors are
    interpolated in OKLAB. In order to support this a "kSRGBLegacy" color
    interpolation space is added to Color::ColorInterpolation space to
    match the Color::ColorSpace of the same name. This space interpolates
    exactly the same way as "kSRGB", but produces a legacy color for
    supporting legacy interpolation between points.
    
    Also, Color::ColorSpace::kRGBLegacy is renamed to kSRGBLegacy to match
    convention elsewhere (it is in the "sRGB" color space, after all).
    
    Some gradient expectations are rebaselined to match the results of using
    higher resolution colors to generate them.
    
    Bug: 1416273, 1411351
    Change-Id: I24446988e981291c6b0c24ea3b5d8afd39aaceca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250939
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118133}

--- third_party/blink/renderer/core/style/shadow_data.cc
+++ third_party/blink/renderer/core/style/shadow_data.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/style/shadow_data.h"
 
-#include "third_party/blink/renderer/platform/graphics/color_blend.h"
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 
 namespace blink {
