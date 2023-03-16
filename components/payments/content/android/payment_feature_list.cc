786af18b98b859ec2b3fb0d05c82e406cccdf9a7
Stephen McGruer <smcgruer@chromium.org>
2023-03-16 19:04:12 +0000

    [Payments] Clean up always-disabled WebPaymentsMethodSectionOrderV2 flag
    This CL was part of an old bug to experiment with section ordering on
    the Payment Request browser sheet on Android. The results of the
    experiment are unknown (last entry I can find is from 2017 and nobody
    makes it clear what the outcome was), but since the experiment expired
    in 2018 and we rarely show the browser sheet anyway, we should just
    clean up this code.
    
    Bug: 740260
    Change-Id: Ic20eda64414d24281257ca2b37a759613628fe67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342493
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118254}

--- components/payments/content/android/payment_feature_list.cc
+++ components/payments/content/android/payment_feature_list.cc
@@ -35,7 +35,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kEnforceFullDelegation,
     &features::kGPayAppDynamicUpdate,
     &features::kWebPaymentsExperimentalFeatures,
-    &features::kWebPaymentsMethodSectionOrderV2,
     &features::kWebPaymentsModifiers,
     &features::kWebPaymentsSingleAppUiSkip,
     &kAndroidAppPaymentUpdateEvents,
