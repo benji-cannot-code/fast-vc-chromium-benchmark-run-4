ee4a9ac87f52d8d5308ccb1f953b15e12e728cc5
Lei Zhang <thestig@chromium.org>
2023-03-16 22:54:24 +0000

    Always send valid data in PrintManagerHost::ScriptedPrint() on success
    Rather, consistently send nullptr if there is invalid data. Then
    PrintRenderFrameHelper, the data receiver, can rely solely on the
    nullptr check to determine success vs. failure.
    
    Bug: 1286556
    Change-Id: I98815a24fafe16a6744380702a589815c51d623b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338606
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118407}

--- android_webview/browser/aw_print_manager.cc
+++ android_webview/browser/aw_print_manager.cc
@@ -111,6 +111,12 @@ void AwPrintManager::ScriptedPrint(
   printing::RenderParamsFromPrintSettings(*settings_, params->params.get());
   params->params->document_cookie = scripted_params->cookie;
   params->pages = settings_->ranges();
+
+  if (!params->params->document_cookie || params->params->dpi.IsEmpty()) {
+    std::move(callback).Run(nullptr);
+    return;
+  }
+
   std::move(callback).Run(std::move(params));
 }
 
