eec94bca26c8b80fdbfef0dc4486f5b0b333724d
Judy Wang <shidi@chromium.org>
2023-03-17 07:29:36 +0000

    MaterialNext: Update Template Library Layout style.
    Updated grid label to use jellyroll style.
    
    Test: Manual
    Change-Id: Iebef8b34ff163365f6481092b6fc9d631cc0ab44
    Fixed: b/272582792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342830
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118549}

--- ash/wm/desks/templates/saved_desk_library_view.h
+++ ash/wm/desks/templates/saved_desk_library_view.h
@@ -99,7 +99,6 @@ class SavedDeskLibraryView : public views::View, public aura::WindowObserver {
   void AddedToWidget() override;
   void Layout() override;
   void OnKeyEvent(ui::KeyEvent* event) override;
-  void OnThemeChanged() override;
 
   // aura::WindowObserver:
   void OnWindowDestroying(aura::Window* window) override;
