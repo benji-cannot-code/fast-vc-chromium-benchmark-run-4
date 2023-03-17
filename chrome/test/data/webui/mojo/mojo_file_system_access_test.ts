6dd12bf396a2f1d56634007e7c47b19f55db9697
dpapad <dpapad@chromium.org>
2023-03-17 19:17:40 +0000

    WebUI: Migrate chrome/test/data/webui/mojo to TypeScript.
     - Add support for including Mojo TS files in build_webui_tests()
     - Deploy build_webui_tests() in chrome/test/data/webui/mojo/
     - Convert Mojo JS and other JS test files to TypeScript.
    
    Bug: 1002798
    Change-Id: Id46e8c1fcd2136a6df9ba5d967bdc65095ec1bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349112
    Reviewed-by: Cole Horvitz <colehorvitz@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118809}

--- chrome/test/data/webui/mojo/mojo_file_system_access_test.ts
+++ chrome/test/data/webui/mojo/mojo_file_system_access_test.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {MojoFileSystemAccessTest} from '/mojo_file_system_access_test.mojom-webui.js';
+import {MojoFileSystemAccessTest} from './mojo_file_system_access_test.mojom-webui.js';
 
 // Expose the interface for browsertest EvalJs.
-window.MojoFileSystemAccessTest = MojoFileSystemAccessTest;
+Object.assign(window, {MojoFileSystemAccessTest});
