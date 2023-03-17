7aaf4c0f7a68012191fc66f7029a318bbb28cbbb
Emilia Paz <emiliapaz@chromium.org>
2023-03-17 22:22:43 +0000

    [Extensions c2s] Update site settings when clicking toggle button
    When site settings toggle button is pressed, immediately update the
    user site setting accordingly. The effect will only be visible after
    the user refreshes the page (UI for this will be added in a next CL).
    
    Screencast: https://drive.google.com/file/d/1pZw2F7SUMDzPPJdSZoSvG8BpCg9PsaEI/view?usp=sharing (cbug.com/1422818: separate bug where anchor point changes after animation, will be fixed in a separate CL)
    
    Bug: 1390952
    Change-Id: I906096b4bd6cc1fa8d9d7b4316a4539bdd0d5458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294697
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118905}

--- chrome/browser/ui/views/extensions/extensions_menu_view_controller.h
+++ chrome/browser/ui/views/extensions/extensions_menu_view_controller.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
 #include "chrome/browser/ui/views/extensions/extensions_menu_navigation_handler.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/views/view_observer.h"
 
 namespace views {
 class BubbleDialogDelegate;
@@ -23,9 +24,12 @@ class ExtensionsMenuMainPageView;
 class ExtensionsMenuSitePermissionsPageView;
 class ToolbarActionsModel;
 
-class ExtensionsMenuViewController : public ExtensionsMenuNavigationHandler,
-                                     public TabStripModelObserver,
-                                     public ToolbarActionsModel::Observer {
+class ExtensionsMenuViewController
+    : public ExtensionsMenuNavigationHandler,
+      public TabStripModelObserver,
+      public ToolbarActionsModel::Observer,
+      public extensions::PermissionsManager::Observer,
+      public views::ViewObserver {
  public:
   ExtensionsMenuViewController(Browser* browser,
                                ExtensionsContainer* extensions_container,
@@ -63,6 +67,14 @@ class ExtensionsMenuViewController : public ExtensionsMenuNavigationHandler,
   void OnToolbarModelInitialized() override;
   void OnToolbarPinnedActionsChanged() override;
 
+  // PermissionsManager::Observer:
+  void OnUserPermissionsSettingsChanged(
+      const extensions::PermissionsManager::UserPermissionsSettings& settings)
+      override;
+
+  // views::ViewObserver
+  void OnViewIsDeleting(views::View* observed_view) override;
+
   // Accessors used by tests:
   // Returns the main page iff it's the `current_page_` one.
   ExtensionsMenuMainPageView* GetMainPageViewForTesting();
@@ -85,11 +97,19 @@ class ExtensionsMenuViewController : public ExtensionsMenuNavigationHandler,
   const raw_ptr<Browser> browser_;
   const raw_ptr<ExtensionsContainer> extensions_container_;
   const raw_ptr<views::View> bubble_contents_;
-  const raw_ptr<views::BubbleDialogDelegate> bubble_delegate_;
+  // TODO(crbug.com/1425522) There are no guarantee this pointer is safe
+  // to be used. In practice its lifetime is probably always shorter than
+  // `this`. This has to be fixed.
+  const raw_ptr<views::BubbleDialogDelegate, DisableDanglingPtrDetection>
+      bubble_delegate_;
 
   const raw_ptr<ToolbarActionsModel> toolbar_model_;
   base::ScopedObservation<ToolbarActionsModel, ToolbarActionsModel::Observer>
       toolbar_model_observation_{this};
+
+  base::ScopedObservation<extensions::PermissionsManager,
+                          extensions::PermissionsManager::Observer>
+      permissions_manager_observation_{this};
 
   // The current page visible in `bubble_contents_`.
   raw_ptr<views::View> current_page_ = nullptr;
