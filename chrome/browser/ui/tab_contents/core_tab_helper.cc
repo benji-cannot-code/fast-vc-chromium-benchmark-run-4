8369f3e354174300c8205cc88e8b43aeb61ca789
John Abd-El-Malek <jam@chromium.org>
2023-03-17 00:09:28 +0000

    Don't send loading state updates from the network service to the browser on Android.
    These are only used by desktop UI.
    
    Bug: 1425174
    Change-Id: I0a234e710a8edaaa2ad8d528efae15ee9cc95982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347657
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118440}

--- chrome/browser/ui/tab_contents/core_tab_helper.cc
+++ chrome/browser/ui/tab_contents/core_tab_helper.cc
@@ -298,6 +298,11 @@ std::unique_ptr<content::WebContents> CoreTabHelper::SwapWebContents(
 // static
 bool CoreTabHelper::GetStatusTextForWebContents(std::u16string* status_text,
                                                 content::WebContents* source) {
+#if BUILDFLAG(IS_ANDROID)
+  NOTREACHED() << "If this ends up being used on Android update "
+               << "ChromeContentBrowserClient::OverrideURLLoaderFactoryParams.";
+  return false;
+#else
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   auto* guest_manager = guest_view::GuestViewManager::FromBrowserContext(
       source->GetBrowserContext());
@@ -395,6 +400,7 @@ bool CoreTabHelper::GetStatusTextForWebContents(std::u16string* status_text,
 #else  // !BUILDFLAG(ENABLE_EXTENSIONS)
   return false;
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(IS_ANDROID)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
