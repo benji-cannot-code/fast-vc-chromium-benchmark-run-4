6235b3a36e2f9b9a9a160b0ce8f4d37107338d64
Lei Zhang <thestig@chromium.org>
2023-03-17 22:32:07 +0000

    Do IWYU for buckets.h inside components/services/storage
    Add missing includes / remove unused includes. Then do IWYU elsewhere to
    fix the build.
    
    Change-Id: I025b087c211ef73171f7fd2d72ea09669639a24c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348477
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118918}

--- components/services/storage/public/mojom/buckets/bucket_info_mojom_traits.cc
+++ components/services/storage/public/mojom/buckets/bucket_info_mojom_traits.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/services/storage/public/mojom/buckets/bucket_info_mojom_traits.h"
 
+#include <string>
+
 #include "third_party/blink/public/common/storage_key/storage_key_mojom_traits.h"
+#include "third_party/blink/public/mojom/buckets/bucket_manager_host.mojom.h"
 
 namespace mojo {
 
--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -127,6 +127,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/blob/blob_url_store.mojom-forward.h"
 #include "third_party/blink/public/mojom/bluetooth/web_bluetooth.mojom-forward.h"
 #include "third_party/blink/public/mojom/broadcastchannel/broadcast_channel.mojom.h"
+#include "third_party/blink/public/mojom/buckets/bucket_manager_host.mojom.h"
 #include "third_party/blink/public/mojom/feature_observer/feature_observer.mojom-forward.h"
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_manager.mojom-forward.h"
 #include "third_party/blink/public/mojom/filesystem/file_system.mojom-forward.h"
