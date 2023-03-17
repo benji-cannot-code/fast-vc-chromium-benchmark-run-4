cf9c3d0226438b0958156a16f278232af2536119
Colin Blundell <blundell@chromium.org>
2023-03-17 07:49:28 +0000

    [//cc] Add RasterBufferProvider::GetFormat() that returns SIF
    The RasterBufferProvider interface currently has a pure virtual
    GetResourceFormat() method. This CL does the following:
    
    - Introduces a pure virtual RasterBufferProvider::GetFormat(), which
      returns SharedImageFormat
    - Makes RasterBufferProvider::GetResourceFormat() a concrete method
      implemented on top of RasterBufferProvider::GetFormat()
    - Changes subclasses to implement GetFormat()
    
    Followup CLs will (a) change all clients of GetResourceFormat() and
    eliminate that method, and (b) change RasterBufferProvider subclasses
    to take in SharedImageFormat in their constructors.
    
    Bug: 1378708
    Change-Id: I48a8065f0f26a11a3660f7bf9455be4e652ae2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4343298
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118553}

--- cc/raster/raster_buffer_provider.cc
+++ cc/raster/raster_buffer_provider.cc
@@ -119,4 +119,8 @@ void RasterBufferProvider::PlaybackToMemory(
   NOTREACHED();
 }
 
+viz::ResourceFormat RasterBufferProvider::GetResourceFormat() const {
+  return GetFormat().resource_format();
+}
+
 }  // namespace cc
