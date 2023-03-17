f8afce4014856183481d8c385f5c01fb086c82f1
Justin Cohen <justincohen@google.com>
2023-03-17 16:24:01 +0000

    ios: Use WebMain instead of WebMainRunner.
    Change-Id: I768c6a8106fa430a61b363f75f50ece679988ca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4343472
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118691}

--- ios/web/init/web_main.mm
+++ ios/web/init/web_main.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/web/public/init/web_main.h"
+
 #import "ios/web/public/init/web_main_runner.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
