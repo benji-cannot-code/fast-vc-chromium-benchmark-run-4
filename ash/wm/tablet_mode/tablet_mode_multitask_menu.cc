f84198518ce9abbf3ab6824bab42c38d7346ebd2
Sammie Quon <sammiequon@chromium.org>
2023-03-17 18:32:47 +0000

    float: Fix issue with non resizable apps showing the multitask menu
    Some ARC apps cannot be resizable, so they can't be maximized, split or
    floated. The cue and multitask menu shouldn't be shown for these.
    
    Test: manually on DUT with non resizable ARC app
    Change-Id: I9e47a1c119dc346b52465c6ea1ed6a9195e84b25
    Fixed: b/273798890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347636
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118783}

--- ash/wm/tablet_mode/tablet_mode_multitask_menu.cc
+++ ash/wm/tablet_mode/tablet_mode_multitask_menu.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/tablet_mode/tablet_mode_multitask_menu.h"
 
+#include <bit>
+
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/screen_util.h"
@@ -78,8 +80,6 @@ class TabletModeMultitaskMenuView : public views::View {
 
     // Since this menu is only shown for maximizable windows, it can be
     // fullscreened.
-    // TODO(sophiewen): Ensure that there is always 2 buttons or more if this
-    // view is created.
     auto* window_state = WindowState::Get(window);
     DCHECK(window_state);
     DCHECK(window_state->CanMaximize());
@@ -105,8 +105,13 @@ class TabletModeMultitaskMenuView : public views::View {
       buttons |= chromeos::MultitaskMenuView::kPartialSplit;
     }
 
-    if (chromeos::wm::CanFloatWindow(window))
+    if (chromeos::wm::CanFloatWindow(window)) {
       buttons |= chromeos::MultitaskMenuView::kFloat;
+    }
+
+    // TODO(sophiewen): Ensure that there is always 2 buttons or more if this
+    // view is created.
+    DCHECK_GE(std::bitset<4 + 1>(buttons).count(), 1u);
 
     menu_view_base_ =
         AddChildView(std::make_unique<chromeos::MultitaskMenuView>(
