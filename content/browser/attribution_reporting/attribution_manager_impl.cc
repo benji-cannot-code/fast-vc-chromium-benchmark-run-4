538064cfd26d2c4ef0837b404898ad403aec2902
Nan Lin <linnan@chromium.org>
2023-03-17 15:18:00 +0000

    Always remove pending aggregatable reports on report sent
    The reports are no longer pending even if it's not allowed to be sent
    or is sent from the internals UI.
    
    Bug: 1422713
    Change-Id: I3bc4e675f71c8f0581993af6ce69f821c3c3d933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348318
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118663}

--- content/browser/attribution_reporting/attribution_manager_impl.cc
+++ content/browser/attribution_reporting/attribution_manager_impl.cc
@@ -956,6 +956,13 @@ void AttributionManagerImpl::SendReports(
       continue;
     }
 
+    if (auto id = report.ReportId();
+        const auto* aggregatable_id =
+            absl::get_if<AttributionReport::AggregatableAttributionData::Id>(
+                &id)) {
+      pending_aggregatable_reports_.erase(*aggregatable_id);
+    }
+
     if (!IsReportAllowed(report)) {
       // If measurement is disallowed, just drop the report on the floor. We
       // need to make sure we forward that the report was "sent" to ensure it is
@@ -967,13 +974,6 @@ void AttributionManagerImpl::SendReports(
     }
 
     if (!web_ui_callback) {
-      if (auto id = report.ReportId();
-          const auto* aggregatable_id =
-              absl::get_if<AttributionReport::AggregatableAttributionData::Id>(
-                  &id)) {
-        pending_aggregatable_reports_.erase(*aggregatable_id);
-      }
-
       LogMetricsOnReportSend(report, now);
     }
 
