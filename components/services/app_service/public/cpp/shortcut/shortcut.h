2e9886d0ebf3aa7381f2ab4847ac04b6cdbdcc12
Maggie Cai <mxcai@google.com>
2023-03-17 04:09:01 +0000

    [Shortcut] Create shortcut registry cache.
    This CL creates a ShortcutRegistryCache to store all the shortcuts
    created in the system.
    
    BUG=1412708
    
    Change-Id: Ifd6517c2209e7fd5437d309dcfe1b3fc2df43ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4329329
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118515}

--- components/services/app_service/public/cpp/shortcut/shortcut.h
+++ components/services/app_service/public/cpp/shortcut/shortcut.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_SHORTCUT_H_
-#define COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_SHORTCUT_H_
+#ifndef COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_SHORTCUT_SHORTCUT_H_
+#define COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_SHORTCUT_SHORTCUT_H_
 
 #include <memory>
 #include <string>
@@ -71,4 +71,4 @@ Shortcuts CloneShortcuts(const Shortcuts& source_shortcuts);
 
 }  // namespace apps
 
-#endif  // COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_SHORTCUT_H_
+#endif  // COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_SHORTCUT_SHORTCUT_H_
