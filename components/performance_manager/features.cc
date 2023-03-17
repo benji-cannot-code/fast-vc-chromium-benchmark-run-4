e263897897eeffb92c71dfea2ce9929b700d1bcf
Joe Mason <joenotcharles@google.com>
2023-03-17 19:05:08 +0000

    [PM] Add an experiment to run on a dedicated thread
    This gives the PM sequence a fixed name that can be searched in traces
    to find blocking calls.
    
    Also removes the RunningOnUIThread() helper since base::Feature now
    caches lookup results internally.
    
    R=gab
    
    Bug: 1189677
    Change-Id: I884eec5c46dd562d2cc902ae16ca5365c334a984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4346696
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1118801}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -19,6 +19,10 @@ BASE_FEATURE(kRunOnMainThread,
              "RunOnMainThread",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kRunOnDedicatedThreadPoolThread,
+             "RunOnDedicatedThreadPoolThread",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 #if !BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kBackgroundTabLoadingFromPerformanceManager,
              "BackgroundTabLoadingFromPerformanceManager",
