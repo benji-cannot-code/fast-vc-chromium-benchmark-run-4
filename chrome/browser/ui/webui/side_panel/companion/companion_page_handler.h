60d0f0ccac43c4be216ed24b1f6097db5ae0a543
Juan Mojica <juanmojica@google.com>
2023-03-17 23:26:09 +0000

    Only observe the original active web contents when view is created.
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: b:273785262
    Change-Id: I301b24817faaef9b5b5f12e2e4beadeb26824a9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350424
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1118941}

--- chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
+++ chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_SIDE_PANEL_COMPANION_COMPANION_PAGE_HANDLER_H_
 
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/webui/side_panel/companion/companion.mojom.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -28,7 +27,6 @@ inline constexpr char kOriginQueryParameterValue[] =
     "chrome-untrusted://companion-side-panel.top-chrome";
 
 class CompanionPageHandler : public side_panel::mojom::CompanionPageHandler,
-                             public TabStripModelObserver,
                              public content::WebContentsObserver {
  public:
   explicit CompanionPageHandler(
@@ -42,12 +40,6 @@ class CompanionPageHandler : public side_panel::mojom::CompanionPageHandler,
 
   // side_panel::mojom::CompanionPageHandler:
   void ShowUI() override;
-
-  // TabStripModelObserver:
-  void OnTabStripModelChanged(
-      TabStripModel* tab_strip_model,
-      const TabStripModelChange& change,
-      const TabStripSelectionChange& selection) override;
 
   // content::WebContentsObserver:
   void PrimaryPageChanged(content::Page& page) override;
@@ -65,7 +57,6 @@ class CompanionPageHandler : public side_panel::mojom::CompanionPageHandler,
 
   mojo::Receiver<side_panel::mojom::CompanionPageHandler> receiver_;
   mojo::Remote<side_panel::mojom::CompanionPage> page_;
-  const raw_ptr<Browser> browser_;
   raw_ptr<CompanionSidePanelUntrustedUI> companion_untrusted_ui_ = nullptr;
 };
 
