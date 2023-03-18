8c83f9b721c0b26d522ebebf16c2bb260786a72c
Claudio DeSouza <cdesouza@igalia.com>
2023-03-18 18:23:20 +0000

    [json-schema-compiler] Enum parsing with StringPiece
    This CL changes the type of the argument taken for enum parsing from a
    `std::string`, to `StringPiece`, to avoid unnecessary string
    instantiations, when the enum parsing functions with a constant.
    
    Bug: 1421546
    Change-Id: If09a51f4a847271ffeff2d6424341954ca326c50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310791
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119044}

--- tools/json_schema_compiler/manifest_parse_util.h
+++ tools/json_schema_compiler/manifest_parse_util.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/string_piece_forward.h"
 #include "base/values.h"
 #include "tools/json_schema_compiler/util.h"
@@ -23,7 +24,7 @@ namespace manifest_parse_util {
 // |value| at the given |key|.
 void PopulateInvalidEnumValueError(
     base::StringPiece key,
-    const std::string& value,
+    base::StringPiece value,
     std::u16string* error,
     std::vector<base::StringPiece>* error_path_reversed);
 
@@ -170,7 +171,7 @@ bool ParseFromDictionary(const base::Value::Dict& dict,
 
 // Alias for pointer to a function which converts a string to an enum of type T.
 template <typename T>
-using StringToEnumConverter = T (*)(const std::string&);
+using StringToEnumConverter = T (*)(base::StringPiece);
 
 // Parses enum |out| from |dict| at the given |key|. On failure, returns false
 // and populates |error| and |error_path_reversed|.
@@ -198,7 +199,7 @@ bool ParseEnumFromDictionary(
   if (!value)
     return false;
 
-  const std::string str = value->GetString();
+  const std::string& str = value->GetString();
   T enum_value = converter(str);
   if (enum_value == none_value) {
     PopulateInvalidEnumValueError(key, str, error, error_path_reversed);
