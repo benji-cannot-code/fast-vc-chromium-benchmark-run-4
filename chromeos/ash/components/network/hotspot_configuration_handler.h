d1bbd9851a4b1e72210de842c26e0c91869c1b48
Jason Zhang <jiajunz@google.com>
2023-03-18 05:07:02 +0000

    [CrOS Hotspot] Restart hotspot when update hotspot configuration
    This CL implements restart hotspot if hotspot configuration is updated
    during hotspot is active.  It also add additional check to see if the
    hotspot state is already enabled/disabled before enable/disable hotspot.
    
    Bug: b/272109141
    Change-Id: Id8c0bd7148c39e4a612f1f43c0598beb09e694dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338736
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119016}

--- chromeos/ash/components/network/hotspot_configuration_handler.h
+++ chromeos/ash/components/network/hotspot_configuration_handler.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class HotspotController;
+
 // This class caches hotspot configurations and implements methods to get and
 // update the hotspot configurations.
 class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotConfigurationHandler
@@ -38,7 +40,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotConfigurationHandler
       delete;
   ~HotspotConfigurationHandler() override;
 
-  void Init();
+  void Init(HotspotController* hotspot_controller);
 
   // Return the current hotspot configuration
   hotspot_config::mojom::HotspotConfigPtr GetHotspotConfig() const;
@@ -75,7 +77,8 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotConfigurationHandler
       absl::optional<base::Value::Dict> manager_properties);
 
   // Callback when the SetHotspotConfig operation succeeded.
-  void OnSetHotspotConfigSuccess(SetHotspotConfigCallback callback);
+  void OnSetHotspotConfigSuccess(bool restart_hotspot,
+                                 SetHotspotConfigCallback callback);
 
   // Callback when the SetHotspotConfig operation failed.
   void OnSetHotspotConfigFailure(SetHotspotConfigCallback callback,
@@ -83,6 +86,8 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotConfigurationHandler
                                  const std::string& error_message);
 
   absl::optional<base::Value::Dict> hotspot_config_;
+
+  HotspotController* hotspot_controller_ = nullptr;
 
   base::ObserverList<Observer> observer_list_;
   base::WeakPtrFactory<HotspotConfigurationHandler> weak_ptr_factory_{this};
--- chromeos/ash/components/network/metrics/hotspot_metrics_helper.cc
+++ chromeos/ash/components/network/metrics/hotspot_metrics_helper.cc
@@ -134,6 +134,8 @@ HotspotMetricsHelper::GetSetEnabledMetricsResult(
       return HotspotMetricsSetEnabledResult::kCellularAttachFailure;
     case HotspotControlResult::kShillOperationFailed:
       return HotspotMetricsSetEnabledResult::kShillOperationFailure;
+    case HotspotControlResult::kAlreadyFulfilled:
+      return HotspotMetricsSetEnabledResult::kAlreadyFulfilled;
     default:
       return HotspotMetricsSetEnabledResult::kUnknownFailure;
   }
