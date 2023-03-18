ab396341bdc19d7fe8f864f66172cf5c13602014
Thomas Guilbert <tguilbert@chromium.org>
2023-03-18 01:23:27 +0000

    Recreate AmplitudePeakDetector on Start/Stop
    On Windows, we recreate a new audio rendering thread each time we
    restart an output stream. AmplitudePeakDetector's thread checker DCHECKs
    when it is called from a new thread, despite there not being any
    threading risk in this case.
    
    This CL recreates AmplitudePeakDetectors so there is a 1:1
    correspondence between them and audio rendering threads.
    
    Fixed: 1425455
    Change-Id: I6132d1288fc49a16709c9c7863a8d1a0c1e46f73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348947
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118987}

--- media/audio/win/audio_low_latency_output_win.h
+++ media/audio/win/audio_low_latency_output_win.h
@@ -115,7 +115,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_io.h"
 #include "media/audio/system_glitch_reporter.h"
 #include "media/audio/win/audio_manager_win.h"
-#include "media/base/amplitude_peak_detector.h"
 #include "media/base/audio_parameters.h"
 #include "media/base/media_export.h"
 
@@ -123,6 +122,7 @@ namespace media {
 
 class AudioManagerWin;
 class AudioSessionEventListener;
+class AmplitudePeakDetector;
 
 // AudioOutputStream implementation using Windows Core Audio APIs.
 class MEDIA_EXPORT WASAPIAudioOutputStream
@@ -200,7 +200,7 @@ class MEDIA_EXPORT WASAPIAudioOutputStream
   // text logs (when a stream ends).
   SystemGlitchReporter glitch_reporter_;
 
-  AmplitudePeakDetector peak_detector_;
+  std::unique_ptr<AmplitudePeakDetector> peak_detector_;
 
   // Rendering is driven by this thread (which has no message loop).
   // All OnMoreData() callbacks will be called from this thread.
