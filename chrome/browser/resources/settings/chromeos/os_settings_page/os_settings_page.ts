34d7c1cebf08affc5fbe6084be43db6451a2c3e2
Wes Okuhara <wesokuhara@google.com>
2023-03-18 01:36:37 +0000

    CrOS Settings: Attempt to de-flake os-settings-page browser tests
    According to failures shown in the bug, this browser tests times out
    occasionally, causing flakiness. Attempt to de-flake the browser test
    for the <os-settings-page> element by:
    - Avoiding to create the <os-settings-page> for each test case. Instead
      we only create one element instance per test suite.
    - Separate tests into 3 distinct suites:
      - Basic page existence
      - Advanced page existence
      - Guest mode page existence (both basic and advanced)
    
    Bug: b/270646575
    Test: browser_tests --gtest_filter="OSSettingsOsSettingsPage*"
    Change-Id: I667cc39a62fc00ba13f170d1970388a7026bc4d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4351188
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118992}

--- chrome/browser/resources/settings/chromeos/os_settings_page/os_settings_page.ts
+++ chrome/browser/resources/settings/chromeos/os_settings_page/os_settings_page.ts
@@ -88,7 +88,9 @@ class OsSettingsPageElement extends OsSettingsPageElementBase {
        */
       isGuestMode_: {
         type: Boolean,
-        value: loadTimeData.getBoolean('isGuest'),
+        value: () => {
+          return loadTimeData.getBoolean('isGuest');
+        },
       },
 
       /**
