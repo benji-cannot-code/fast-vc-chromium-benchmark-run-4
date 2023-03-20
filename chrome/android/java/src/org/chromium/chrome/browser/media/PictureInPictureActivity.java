c1f5ed8a0be73c047aed61bdb6433ebd5d759762
François Beaufort <beaufort.francois@gmail.com>
2023-03-20 10:00:48 +0000

    Show slides actions in the Picture-in-Picture window on Android
    This CL adds the "previous slide" and "next slide" action to the
    Picture-in-Picture on Android when the website sets slides action
    handers through the Media Session API.
    
    Issue: https://github.com/GoogleChrome/samples/issues/796
    Test: https://googlechrome.github.io/samples/media-session/slides.html
    Bug: 1363548
    Change-Id: Ic8dd41f33748ebaf5d0586f679fa29dc229e6a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350267
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119232}

--- chrome/android/java/src/org/chromium/chrome/browser/media/PictureInPictureActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/PictureInPictureActivity.java
@@ -114,6 +114,8 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     class MediaActionButtonsManager {
         @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        final RemoteAction mPreviousSlide;
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
         final RemoteAction mPreviousTrack;
         @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
         final RemoteAction mPlay;
@@ -123,6 +125,8 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
         final RemoteAction mReplay;
         @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
         final RemoteAction mNextTrack;
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        final RemoteAction mNextSlide;
         @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
         final RemoteAction mHangUp;
         @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
@@ -163,6 +167,9 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
             mPreviousTrack = createRemoteAction(requestCode++, MediaSessionAction.PREVIOUS_TRACK,
                     R.drawable.ic_skip_previous_white_24dp, R.string.accessibility_previous_track,
                     /**controlState=*/null);
+            mPreviousSlide = createRemoteAction(requestCode++, MediaSessionAction.PREVIOUS_SLIDE,
+                    R.drawable.ic_skip_previous_white_24dp, R.string.accessibility_previous_slide,
+                    /**controlState=*/null);
             mPlay = createRemoteAction(requestCode++, MediaSessionAction.PLAY,
                     R.drawable.ic_play_arrow_white_24dp, R.string.accessibility_play,
                     /**controlState=*/null);
@@ -174,6 +181,9 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
                     /**controlState=*/null);
             mNextTrack = createRemoteAction(requestCode++, MediaSessionAction.NEXT_TRACK,
                     R.drawable.ic_skip_next_white_24dp, R.string.accessibility_next_track,
+                    /**controlState=*/null);
+            mNextSlide = createRemoteAction(requestCode++, MediaSessionAction.NEXT_SLIDE,
+                    R.drawable.ic_skip_next_white_24dp, R.string.accessibility_next_slide,
                     /**controlState=*/null);
             mHangUp = createRemoteAction(requestCode++, MediaSessionAction.HANG_UP,
                     R.drawable.ic_call_end_white_24dp, R.string.accessibility_hang_up,
@@ -202,13 +212,22 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
         ArrayList<RemoteAction> getActionsForPictureInPictureParams() {
             ArrayList<RemoteAction> actions = new ArrayList<>();
 
-            boolean shouldShowPreviousNextTrack =
+            final boolean shouldShowPreviousNextTrack =
                     mVisibleActions.contains(MediaSessionAction.PREVIOUS_TRACK)
                     || mVisibleActions.contains(MediaSessionAction.NEXT_TRACK);
             if (shouldShowPreviousNextTrack) {
                 mPreviousTrack.setEnabled(
                         mVisibleActions.contains(MediaSessionAction.PREVIOUS_TRACK));
                 actions.add(mPreviousTrack);
+            }
+
+            final boolean shouldShowPreviousNextSlide =
+                    mVisibleActions.contains(MediaSessionAction.PREVIOUS_SLIDE)
+                    || mVisibleActions.contains(MediaSessionAction.NEXT_SLIDE);
+            if (shouldShowPreviousNextSlide) {
+                mPreviousSlide.setEnabled(
+                        mVisibleActions.contains(MediaSessionAction.PREVIOUS_SLIDE));
+                actions.add(mPreviousSlide);
             }
 
             if (mVisibleActions.contains(MediaSessionAction.PLAY)) {
@@ -228,6 +247,11 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
             if (shouldShowPreviousNextTrack) {
                 mNextTrack.setEnabled(mVisibleActions.contains(MediaSessionAction.NEXT_TRACK));
                 actions.add(mNextTrack);
+            }
+
+            if (shouldShowPreviousNextSlide) {
+                mNextSlide.setEnabled(mVisibleActions.contains(MediaSessionAction.NEXT_SLIDE));
+                actions.add(mNextSlide);
             }
 
             if (mVisibleActions.contains(MediaSessionAction.TOGGLE_MICROPHONE)) {
@@ -346,6 +370,12 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
                     return;
                 case MediaSessionAction.NEXT_TRACK:
                     PictureInPictureActivityJni.get().nextTrack(nativeOverlayWindowAndroid);
+                    return;
+                case MediaSessionAction.PREVIOUS_SLIDE:
+                    PictureInPictureActivityJni.get().previousSlide(nativeOverlayWindowAndroid);
+                    return;
+                case MediaSessionAction.NEXT_SLIDE:
+                    PictureInPictureActivityJni.get().nextSlide(nativeOverlayWindowAndroid);
                     return;
                 case MediaSessionAction.TOGGLE_MICROPHONE:
                     PictureInPictureActivityJni.get().toggleMicrophone(
@@ -722,6 +752,8 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
         void togglePlayPause(long nativeOverlayWindowAndroid, boolean toggleOn);
         void nextTrack(long nativeOverlayWindowAndroid);
         void previousTrack(long nativeOverlayWindowAndroid);
+        void nextSlide(long nativeOverlayWindowAndroid);
+        void previousSlide(long nativeOverlayWindowAndroid);
         void toggleMicrophone(long nativeOverlayWindowAndroid, boolean toggleOn);
         void toggleCamera(long nativeOverlayWindowAndroid, boolean toggleOn);
         void hangUp(long nativeOverlayWindowAndroid);
--- chrome/browser/ui/android/overlay/overlay_window_android.h
+++ chrome/browser/ui/android/overlay/overlay_window_android.h
@@ -41,6 +41,8 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
   void TogglePlayPause(JNIEnv* env, bool toggleOn);
   void NextTrack(JNIEnv* env);
   void PreviousTrack(JNIEnv* env);
+  void NextSlide(JNIEnv* env);
+  void PreviousSlide(JNIEnv* env);
   void ToggleMicrophone(JNIEnv* env, bool toggleOn);
   void ToggleCamera(JNIEnv* env, bool toggleOn);
   void HangUp(JNIEnv* env);
@@ -76,8 +78,8 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
   void SetToggleMicrophoneButtonVisibility(bool is_visible) override;
   void SetToggleCameraButtonVisibility(bool is_visible) override;
   void SetHangUpButtonVisibility(bool is_visible) override;
-  void SetNextSlideButtonVisibility(bool is_visible) override {}
-  void SetPreviousSlideButtonVisibility(bool is_visible) override {}
+  void SetNextSlideButtonVisibility(bool is_visible) override;
+  void SetPreviousSlideButtonVisibility(bool is_visible) override;
   void SetSurfaceId(const viz::SurfaceId& surface_id) override;
 
  private:
