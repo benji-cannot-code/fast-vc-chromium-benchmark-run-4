eaa9e169758691b7d81518d6ba555ae896dc3966
Jan Keitel <jkeitel@google.com>
2023-03-20 09:08:54 +0000

    Use flat_map in ContentAutofillDriverFactory.
    The map here meets the requirements in //base/containers/ for using a
    flat_map:
    - Small number of objects
    - Small/ cheap to move objects
    
    Using flat_map should therefore
    - Save 100 + ~20*n_drivers bytes of memory
    - Be faster unless there are at least several hundred RFHs for a single
      WebContents - see bug for related benchmark.
    
    Bug: 1425804
    Change-Id: I3e56784c54d4ec0166d59c4085f9ebe726100cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349929
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1119218}

--- components/autofill/content/browser/content_autofill_driver_factory.h
+++ components/autofill/content/browser/content_autofill_driver_factory.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/supports_user_data.h"
 #include "components/autofill/content/browser/content_autofill_router.h"
@@ -126,8 +127,8 @@ class ContentAutofillDriverFactory : public content::WebContentsObserver {
   // Should be empty at destruction time because its elements are erased in
   // RenderFrameDeleted(). In case it is not empty, is must be destroyed before
   // |router_| because ~ContentAutofillDriver() may access |router_|.
-  std::unordered_map<content::RenderFrameHost*,
-                     std::unique_ptr<ContentAutofillDriver>>
+  base::flat_map<content::RenderFrameHost*,
+                 std::unique_ptr<ContentAutofillDriver>>
       driver_map_;
 
   base::ObserverList<Observer> observers_;
