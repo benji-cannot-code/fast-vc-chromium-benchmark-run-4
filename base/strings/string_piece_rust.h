53bd605ccd69eeb26802e713368e5de0fc85f8e8
Adrian Taylor <adetaylor@chromium.org>
2023-03-20 20:09:03 +0000

    [rust] Fix gncheck problem
    These headers are surrounded by #ifdef and so are not actually active
    except on builds where Rust features are explicitly enabled. gn gen
    --check is therefore wrong to complain about them; in cases where these
    headers are active there is a valid gn dependency.
    
    Bug: 1292038
    Change-Id: If8d538616f523811f6d0a5fa586ff4e144f39a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355012
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119548}

--- base/strings/string_piece_rust.h
+++ base/strings/string_piece_rust.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/strings/string_piece.h"
-#include "third_party/rust/cxx/v1/crate/include/cxx.h"
+#include "third_party/rust/cxx/v1/crate/include/cxx.h"  // nogncheck
 
 namespace base {
 
