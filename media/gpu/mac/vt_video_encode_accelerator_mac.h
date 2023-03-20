7931bb36e9b49c42b00b48ce327918b467bcd939
Dale Curtis <dalecurtis@chromium.org>
2023-03-20 17:42:21 +0000

    Reland #2: "Update macOS VideoToolbox encoder color space settings."
    This ensures the right color space information is set in the
    bitstream parameters. Sadly changing them while encodes are
    pending can hang the encoding session, so color space changes
    force creation of a new VTCompressionSession.
    
    This also cleans up the tests pretty substantially by adding
    an assert_eq() method for easier bot debugging. It also swaps
    the RGBA then I420 to I420 first since macOS defaults to a
    SRGB/rec601 color space when none is provided (so tests would
    pass w/o this change).
    
    No other hardware encoder sets a color space at all, but macOS
    has been silently assuming ~rec709 this whole time and seemingly
    converting frames to that space (see drawImage failures in
    earlier patch sets before WrapVideoFrameInCVPixelBuffer() fix).
    
    Since we set the same color space on inputs as we do the session,
    there should be no color space conversion happening by the OS.
    
    Bug: 1377842
    Change-Id: I54875e1a654f7039f1d41bc9ee5a13dba9eb87ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347820
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119445}

--- media/gpu/mac/vt_video_encode_accelerator_mac.h
+++ media/gpu/mac/vt_video_encode_accelerator_mac.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/media_gpu_export.h"
 #include "media/video/video_encode_accelerator.h"
 #include "third_party/webrtc/common_video/include/bitrate_adjuster.h"
+#include "ui/gfx/color_space.h"
 
 namespace media {
 
@@ -107,6 +108,8 @@ class MEDIA_GPU_EXPORT VTVideoEncodeAccelerator
   // encodes have been completed.
   void MaybeRunFlushCallback();
 
+  void SetEncoderColorSpace();
+
   base::ScopedCFTypeRef<VTCompressionSessionRef> compression_session_;
 
   gfx::Size input_visible_size_;
@@ -153,6 +156,10 @@ class MEDIA_GPU_EXPORT VTVideoEncodeAccelerator
   // pending encodes have been returned.
   int pending_encodes_ = 0;
   FlushCallback pending_flush_cb_;
+
+  // Color space of the first frame sent to Encode().
+  absl::optional<gfx::ColorSpace> encoder_color_space_;
+  bool can_set_encoder_color_space_ = true;
 
   // Declared last to ensure that all weak pointers are invalidated before
   // other destructors run.
