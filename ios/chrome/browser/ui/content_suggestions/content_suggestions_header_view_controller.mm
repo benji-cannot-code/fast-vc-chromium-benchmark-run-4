66dd692e884adc71b5e0bd5dcf6952e37dabd531
Gauthier Ambard <gambard@chromium.org>
2023-03-20 13:55:12 +0000

    Reland "[iOS] Cleanup SF Symbol in NTP and location bar"
    This is a reland of commit 3f7913e4c3cc1fcf67a43a75d138997d36fd2ba9
    
    Original change's description:
    > [iOS] Cleanup SF Symbol in NTP and location bar
    >
    > This CL removes the code to use PNGs in the NTP and the location bar.
    >
    > Bug: 1425175
    > Change-Id: I9cb85e4d2b3b85a42bd98c6109fb51e00f4ae929
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350113
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1119271}
    
    Bug: 1425175
    Change-Id: I5537e9024a5c8ebc01f921cc092a6ec8019c24cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4353592
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119295}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
@@ -53,7 +53,6 @@ using base::UserMetricsAction;
 namespace {
 
 NSString* const kScribbleFakeboxElementId = @"fakebox";
-NSString* const kSignOutIdentityIconName = @"sign_out_icon";
 
 }  // namespace
 
@@ -681,12 +680,9 @@ NSString* const kSignOutIdentityIconName = @"sign_out_icon";
 
 - (void)setSignedOutAccountImage {
   if (base::FeatureList::IsEnabled(switches::kIdentityStatusConsistency)) {
-    if (UseSymbols()) {
-      self.identityDiscImage = DefaultSymbolTemplateWithPointSize(
-          kPersonCropCircleSymbol, ntp_home::kSignedOutIdentityIconDimension);
-    } else {
-      self.identityDiscImage = [UIImage imageNamed:kSignOutIdentityIconName];
-    }
+    self.identityDiscImage = DefaultSymbolTemplateWithPointSize(
+        kPersonCropCircleSymbol, ntp_home::kSignedOutIdentityIconDimension);
+
     self.identityDiscAccessibilityLabel =
         l10n_util::GetNSString(IDS_IOS_IDENTITY_DISC_SIGNED_OUT);
   } else {
