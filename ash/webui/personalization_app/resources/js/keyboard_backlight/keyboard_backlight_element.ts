7277cf7e41477b52ee48cef935126bd6895f3e93
Thuong Phan <thuongphan@chromium.org>
2023-03-20 22:06:23 +0000

    personalization: records metric when customization dialog is open
    Creates a new metric
    "Ash.Personalization.KeyboardBacklight.OpenZoneCustomization" to record
    every time the user clicks on "Customize" button  open the zone
    customization dialog to view/set up zone colors. This metric is used to
    know the usage of zone customization dialog.
    
    BUG=b/269483232
    TEST=Click on "Customize" button several times, verify if the number
    matches in
    
    chrome: //histogram/Ash.Personalization.KeyboardBacklight.OpenZoneCustomization.
    Change-Id: Ie2845030c66cea7b0a07a0378b5a543114a59533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347792
    Reviewed-by: Angela Xiao <angelaxiao@chromium.org>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119621}

--- ash/webui/personalization_app/resources/js/keyboard_backlight/keyboard_backlight_element.ts
+++ ash/webui/personalization_app/resources/js/keyboard_backlight/keyboard_backlight_element.ts
@@ -17,6 +17,7 @@ import {SkColor} from 'chrome://resources/mojo/skia/public/mojom/skcolor.mojom-w
 
 import {BacklightColor, CurrentBacklightState} from '../../personalization_app.mojom-webui.js';
 import {isMultiZoneRgbKeyboardSupported} from '../load_time_booleans.js';
+import {logKeyboardBacklightOpenZoneCustomizationUMA} from '../personalization_metrics_logger.js';
 import {WithPersonalizationStore} from '../personalization_store.js';
 import {ColorInfo, getPresetColors, RAINBOW, WALLPAPER} from '../utils.js';
 
@@ -155,6 +156,7 @@ export class KeyboardBacklight extends WithPersonalizationStore {
     assert(
         this.isMultiZoneRgbKeyboardSupported_,
         'zone customization dialog only available if multi-zone is supported');
+    logKeyboardBacklightOpenZoneCustomizationUMA();
     this.$.zoneCustomizationRender.get().showModal();
   }
 
