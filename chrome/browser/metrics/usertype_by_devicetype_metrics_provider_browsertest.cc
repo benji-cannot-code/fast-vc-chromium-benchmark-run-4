0e7a4af53557517efe6ebb265166f084d47094ab
Oleh Lamzin <lamzin@google.com>
2023-03-20 15:04:05 +0000

    Extend DeviceLocalAccounts by adding ephemeral enum
    Add a new field ephemeral_mode to DeviceLocalAccountInfoProto which
    overrides the behaviour of existing DeviceEphemeralUsersEnabled policy
    for some device-local accounts.
    
    Add ephemeral_mode field to DeviceLocalAccount class and make changes to
    all its clients.
    
    More info about new policy can be found in go/gb-kiosk-ephemeral-ds.
    
    Bug: b:270528948
    Change-Id: I646299ee385519edea447034c0fa12736532e2a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4324045
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119328}

--- chrome/browser/metrics/usertype_by_devicetype_metrics_provider_browsertest.cc
+++ chrome/browser/metrics/usertype_by_devicetype_metrics_provider_browsertest.cc
@@ -358,6 +358,7 @@ class UserTypeByDeviceTypeMetricsProviderTest
   void PrepareAppLaunch() {
     std::vector<policy::DeviceLocalAccount> device_local_accounts = {
         policy::DeviceLocalAccount(
+            policy::DeviceLocalAccount::EphemeralMode::kUnset,
             policy::WebKioskAppBasicInfo(kAppInstallUrl, "", ""),
             kAppInstallUrl)};
 
