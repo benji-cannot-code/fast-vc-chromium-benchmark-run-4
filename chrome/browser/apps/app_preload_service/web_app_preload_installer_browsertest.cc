c490d3718a5149df29e1a3021b931c9e52e5e502
Tim Sergeant <tsergeant@chromium.org>
2023-03-20 02:43:34 +0000

    APS: Record histograms for web app success/failure
    This records metrics for the overall status of web app installation, as
    well as the specific result code from the web apps system.
    
    Bug: b/264493427
    Change-Id: I17a94c8c69d1398d9c60c60032cf6980a11e7f91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349497
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119172}

--- chrome/browser/apps/app_preload_service/web_app_preload_installer_browsertest.cc
+++ chrome/browser/apps/app_preload_service/web_app_preload_installer_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/apps/app_preload_service/preload_app_definition.h"
 #include "chrome/browser/apps/app_preload_service/proto/app_provisioning.pb.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/cpp/app_update.h"
+#include "components/webapps/browser/install_result_code.h"
 #include "content/public/test/browser_test.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -108,6 +110,7 @@ IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest, InstallOemApp) {
   })";
   SetManifestResponse(AddIconToManifest(kManifestTemplate));
 
+  base::HistogramTester histograms;
   base::test::TestFuture<bool> result;
   installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
   ASSERT_TRUE(result.Get());
@@ -120,6 +123,12 @@ IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest, InstallOemApp) {
         EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
       });
   ASSERT_TRUE(found);
+
+  histograms.ExpectBucketCount("AppPreloadService.WebAppInstall.InstallResult",
+                               WebAppPreloadResult::kSuccess, 1);
+  histograms.ExpectBucketCount(
+      "AppPreloadService.WebAppInstall.CommandResultCode",
+      webapps::InstallResultCode::kSuccessNewInstall, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
@@ -220,6 +229,7 @@ IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
     "icons": $1
   })"));
 
+  base::HistogramTester histograms;
   base::test::TestFuture<bool> result;
   installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
   ASSERT_FALSE(result.Get());
@@ -229,6 +239,12 @@ IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
   bool found =
       app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
   ASSERT_FALSE(found);
+
+  histograms.ExpectBucketCount("AppPreloadService.WebAppInstall.InstallResult",
+                               WebAppPreloadResult::kWebAppInstallError, 1);
+  histograms.ExpectBucketCount(
+      "AppPreloadService.WebAppInstall.CommandResultCode",
+      webapps::InstallResultCode::kExpectedAppIdCheckFailed, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
@@ -278,6 +294,7 @@ IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
     "is_valid": "no."
   })");
 
+  base::HistogramTester histograms;
   base::test::TestFuture<bool> result;
   installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
   ASSERT_FALSE(result.Get());
@@ -287,6 +304,10 @@ IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
   bool found =
       app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
   ASSERT_FALSE(found);
+
+  histograms.ExpectBucketCount(
+      "AppPreloadService.WebAppInstall.CommandResultCode",
+      webapps::InstallResultCode::kNotValidManifestForWebApp, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
@@ -322,9 +343,14 @@ IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
   SetManifestResponse(base::ReplaceStringPlaceholders(
       kManifestTemplate, {image_url.spec()}, nullptr));
 
+  base::HistogramTester histograms;
   base::test::TestFuture<bool> result;
   installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
   ASSERT_FALSE(result.Get());
+
+  histograms.ExpectBucketCount(
+      "AppPreloadService.WebAppInstall.CommandResultCode",
+      webapps::InstallResultCode::kIconDownloadingFailed, 1);
 }
 
 }  // namespace apps
