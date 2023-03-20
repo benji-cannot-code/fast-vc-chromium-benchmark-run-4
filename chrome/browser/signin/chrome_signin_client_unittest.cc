786bee97eba94c97bb361fdf60385ef760277279
James Lee <ljjlee@google.com>
2023-03-20 19:09:01 +0000

    Allow signout for supervised users on Desktop
    Bug: b/274157010
    Change-Id: I8b2a089639b5cb86d4029e13102af1d0c95a7b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4352657
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1119512}

--- chrome/browser/signin/chrome_signin_client_unittest.cc
+++ chrome/browser/signin/chrome_signin_client_unittest.cc
@@ -254,7 +254,6 @@ TEST_F(ChromeSigninClientSignoutTest, AllAllowed) {
 #endif
 }
 
-#if BUILDFLAG(ENABLE_SUPERVISED_USERS)
 TEST_F(ChromeSigninClientSignoutTest, ChildProfile) {
   TestingProfile::Builder builder;
   builder.SetIsSupervisedProfile();
@@ -262,7 +261,7 @@ TEST_F(ChromeSigninClientSignoutTest, ChildProfile) {
   EXPECT_TRUE(profile->IsChild());
 
   CreateClient(profile.get());
-#if !BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID)
   EXPECT_FALSE(
       client_->IsClearPrimaryAccountAllowed(/*has_sync_account=*/false));
 #else
@@ -271,7 +270,6 @@ TEST_F(ChromeSigninClientSignoutTest, ChildProfile) {
 #endif
   EXPECT_TRUE(client_->IsRevokeSyncConsentAllowed());
 }
-#endif
 
 class ChromeSigninClientSignoutSourceTest
     : public ::testing::WithParamInterface<signin_metrics::ProfileSignout>,
