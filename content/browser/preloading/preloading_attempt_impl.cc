4f5e38d4227bff9d2fa740fa3abde05cc5259122
Simon Pelchat <spelchat@chromium.org>
2023-03-20 16:20:43 +0000

    Add SamplingLikelihood to Preloading_Attempt* UKM events.
    SamplingLikelihood contains the sampling rate of the event, multiplied
    by 1,000,000 (because UKM logs can only contain integers). This allows
    scaling counts of the Preloading_Attempt* events to account for sampling
    when querying this data.
    
    UKM collection review:
    https://docs.google.com/document/d/183tVE7Yn3_ZkRNuEhP3YlgOGxB_1Qa6TPGarocbf-2Y/
    
    Bug: 1424844
    Change-Id: I2d795b7e10a1b2908f98b153edf334c21cc559fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349328
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Simon Pelchat <spelchat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119376}

--- content/browser/preloading/preloading_attempt_impl.cc
+++ content/browser/preloading/preloading_attempt_impl.cc
@@ -223,6 +223,11 @@ void PreloadingAttemptImpl::RecordPreloadingAttemptMetrics(
     return;
   }
 
+  // Turn sampling_likelihood into an int64_t for UKM logging. Multiply by one
+  // million to preserve accuracy.
+  int64_t sampling_likelihood_per_million =
+      static_cast<int64_t>(1'000'000 * sampling_likelihood);
+
   if (navigated_page_source_id != ukm::kInvalidSourceId) {
     ukm::builders::Preloading_Attempt builder(navigated_page_source_id);
     builder.SetPreloadingType(static_cast<int64_t>(preloading_type_))
@@ -231,7 +236,8 @@ void PreloadingAttemptImpl::RecordPreloadingAttemptMetrics(
         .SetHoldbackStatus(static_cast<int64_t>(holdback_status_))
         .SetTriggeringOutcome(static_cast<int64_t>(triggering_outcome_))
         .SetFailureReason(static_cast<int64_t>(failure_reason_))
-        .SetAccurateTriggering(is_accurate_triggering_);
+        .SetAccurateTriggering(is_accurate_triggering_)
+        .SetSamplingLikelihood(sampling_likelihood_per_million);
     if (time_to_next_navigation_) {
       builder.SetTimeToNextNavigation(ukm::GetExponentialBucketMinForCounts1000(
           time_to_next_navigation_->InMilliseconds()));
@@ -252,7 +258,8 @@ void PreloadingAttemptImpl::RecordPreloadingAttemptMetrics(
         .SetHoldbackStatus(static_cast<int64_t>(holdback_status_))
         .SetTriggeringOutcome(static_cast<int64_t>(triggering_outcome_))
         .SetFailureReason(static_cast<int64_t>(failure_reason_))
-        .SetAccurateTriggering(is_accurate_triggering_);
+        .SetAccurateTriggering(is_accurate_triggering_)
+        .SetSamplingLikelihood(sampling_likelihood_per_million);
     if (time_to_next_navigation_) {
       builder.SetTimeToNextNavigation(ukm::GetExponentialBucketMinForCounts1000(
           time_to_next_navigation_->InMilliseconds()));
