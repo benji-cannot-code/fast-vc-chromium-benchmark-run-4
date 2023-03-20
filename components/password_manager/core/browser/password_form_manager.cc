5b879f28d5c3b07e6b46b2addf2cbb2030c6e60f
Elias Khsheibun <eliaskh@chromium.org>
2023-03-20 09:05:32 +0000

    [Passwords] Add check before derefrencing observed_form().
    Bug: 1313893
    Change-Id: I71df2d7db5330593a4fadb08a36f5b5efaaf851e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350431
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Elias Khsheibun <eliaskh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119217}

--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -212,6 +212,7 @@ bool PasswordFormManager::DoesManage(
     const PasswordManagerDriver* driver) const {
   if (driver != driver_.get())
     return false;
+  CHECK(observed_form());
   return observed_form()->unique_renderer_id == form_renderer_id;
 }
 
@@ -885,6 +886,7 @@ void PasswordFormManager::Fill() {
   // There are additional signals (server-side data) and parse results in
   // filling and saving mode might be different so it is better not to cache
   // parse result, but to parse each time again.
+  CHECK(observed_form());
   std::unique_ptr<PasswordForm> observed_password_form =
       ParseFormAndMakeLogging(*observed_form(), FormDataParser::Mode::kFilling);
   RecordMetricOnReadonly(parser_.readonly_status(), !!observed_password_form,
@@ -920,6 +922,7 @@ void PasswordFormManager::Fill() {
 void PasswordFormManager::FillForm(
     const FormData& observed_form_data,
     const std::map<FormSignature, FormPredictions>& predictions) {
+  CHECK(observed_form());
   uint32_t differences_bitmask =
       FindFormsDifferences(*observed_form(), observed_form_data);
   metrics_recorder_->RecordFormChangeBitmask(differences_bitmask);
@@ -968,6 +971,7 @@ bool PasswordFormManager::FormHasPossibleUsername(
   if (!possible_username) {
     return false;
   }
+  CHECK(observed_form());
   if (possible_username->driver_id == driver_id_) {
     for (const auto& field : observed_form()->fields) {
       if (field.unique_renderer_id == possible_username->renderer_id) {
@@ -1199,6 +1203,7 @@ bool PasswordFormManager::IsPasswordFormAfterSingleUsernameForm(
 
 void PasswordFormManager::UpdatePredictionsForObservedForm(
     const std::map<FormSignature, FormPredictions>& predictions) {
+  CHECK(observed_form());
   FormSignature observed_form_signature =
       CalculateFormSignature(*observed_form());
   auto it = predictions.find(observed_form_signature);
