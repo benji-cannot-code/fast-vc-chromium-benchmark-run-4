9d4c22838e9eb8bbe4269fba6883a560817cab8b
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-03-20 10:01:00 +0000

    Add dep from SyncServiceFactory to IOSChromeAccountPasswordStoreFactory
    The dependency for the profile store was there but not the one for the
    account store.
    
    Bug: 1374244
    Change-Id: I3b7bcc1abb3ff46c12daa443ddcc93358939b6e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350727
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1119233}

--- ios/chrome/browser/sync/sync_service_factory.mm
+++ ios/chrome/browser/sync/sync_service_factory.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/favicon/favicon_service_factory.h"
 #import "ios/chrome/browser/gcm/ios_chrome_gcm_profile_service_factory.h"
 #import "ios/chrome/browser/history/history_service_factory.h"
+#import "ios/chrome/browser/passwords/ios_chrome_account_password_store_factory.h"
 #import "ios/chrome/browser/passwords/ios_chrome_password_store_factory.h"
 #import "ios/chrome/browser/reading_list/reading_list_model_factory.h"
 #import "ios/chrome/browser/search_engines/template_url_service_factory.h"
@@ -107,6 +108,7 @@ SyncServiceFactory::SyncServiceFactory()
   DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(IOSChromeGCMProfileServiceFactory::GetInstance());
   DependsOn(IOSChromePasswordStoreFactory::GetInstance());
+  DependsOn(IOSChromeAccountPasswordStoreFactory::GetInstance());
   DependsOn(IOSUserEventServiceFactory::GetInstance());
   DependsOn(ModelTypeStoreServiceFactory::GetInstance());
   DependsOn(ReadingListModelFactory::GetInstance());
