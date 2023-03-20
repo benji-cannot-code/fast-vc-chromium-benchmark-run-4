b9be907a499f3996eac19c75368efafb9bacb520
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-03-20 15:06:19 +0000

    Revert "Make Render{View,Widget}HostImpl::site_instance_group_ a base::SafeRef"
    This reverts commit 0eedcb3c04b89ff24ce154c2f8f724f9e8225195.
    
    Reason for revert: There are two bugs:
    
    1. The base::SafeRef is still dangling, but only on Windows, and in only
       one test. See https://crbug.com/1425901. This is blocking enabling
       the dangling pointer detector on windows.
    
    2. A fuchsia tests seems to be affected by the member reordering:
       See https://crbug.com/1424196
    
    I would like to revert this patch, and try relanding it once we figured
    out how to do it correctly with regards to those two bugs.
    
    
    Original change's description:
    > Make Render{View,Widget}HostImpl::site_instance_group_ a base::SafeRef
    >
    > They previously couldn't be a base::SafeRef, because they contain a
    > dangling pointer. The reason was the SiteInstanceGroup was deleted when
    > the AgentSchedulingGroup route was removed.
    >
    > The fix involves reordering the two operations in the C++ destructor.
    >
    > Change-Id: Ibecfc28b95fc0b8febd0e34cb27422ed66f68c24
    > Bug: 1420333
    > Fixed: 1420333
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304121
    > Reviewed-by: Charlie Reis <creis@chromium.org>
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1113908}
    
    Bug: 1420333, 1425901, 1424196
    Change-Id: Ib03530be334c162883fcf59b8161f8ef69d995cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4353195
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1119330}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/buckets/bucket_context.h"
 #include "content/browser/can_commit_status.h"
 #include "content/browser/network/cross_origin_opener_policy_reporter.h"
-#include "content/browser/renderer_host/agent_scheduling_group_host.h"
 #include "content/browser/renderer_host/back_forward_cache_impl.h"
 #include "content/browser/renderer_host/back_forward_cache_metrics.h"
 #include "content/browser/renderer_host/browsing_context_state.h"
@@ -246,6 +245,7 @@ namespace internal {
 class DocumentServiceBase;
 }  // namespace internal
 
+class AgentSchedulingGroupHost;
 class BrowsingContextState;
 class CodeCacheHostImpl;
 class CrossOriginEmbedderPolicyReporter;
@@ -378,7 +378,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   ~RenderFrameHostImpl() override;
 
   // RenderFrameHost
-  int GetRoutingID() const final;
+  int GetRoutingID() const override;
   const blink::LocalFrameToken& GetFrameToken() const override;
   const base::UnguessableToken& GetReportingSource() override;
 
@@ -1519,7 +1519,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
 
   // Returns the AgentSchedulingGroupHost associated with this
   // RenderFrameHostImpl.
-  AgentSchedulingGroupHost& GetAgentSchedulingGroup();
+  virtual AgentSchedulingGroupHost& GetAgentSchedulingGroup();
 
   // Returns associated remote for the blink::mojom::LocalMainFrame Mojo
   // interface. May be overridden by subclasses, e.g. tests which wish to
@@ -3895,8 +3895,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
 
   const raw_ptr<RenderFrameHostDelegate> delegate_;
 
-  const int routing_id_;
-
   // The SiteInstance associated with this RenderFrameHost. All content drawn
   // in this RenderFrameHost is part of this SiteInstance. Cannot change over
   // time.
@@ -3916,9 +3914,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // to access it or its associated `AgentSchedulingGroupHost` (see
   // crbug.com/1297030).
   const base::SafeRef<AgentSchedulingGroupHost> agent_scheduling_group_;
-
-  const AgentSchedulingGroupHost::RouteOwner route_{
-      this, *agent_scheduling_group_, routing_id_};
 
   // Reference to the whole frame tree that this RenderFrameHost belongs to.
   // Allows this RenderFrameHost to add and remove nodes in response to
@@ -4093,6 +4088,8 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // TODO(kenrb): Later this will also be used on the top-level frame, when
   // RenderFrameHost owns its RenderViewHost.
   std::unique_ptr<RenderWidgetHostImpl> owned_render_widget_host_;
+
+  const int routing_id_;
 
   // Boolean indicating whether this RenderFrameHost is being actively used or
   // is waiting for mojo::AgentSchedulingGroupHost::DidUnloadRenderFrame and
--- content/test/test_render_frame_host.h
+++ content/test/test_render_frame_host.h
@@ -86,6 +86,7 @@ class TestRenderFrameHost : public RenderFrameHostImpl,
   TestRenderViewHost* GetRenderViewHost() const override;
   TestPage& GetPage() override;
   MockRenderProcessHost* GetProcess() const override;
+  MockAgentSchedulingGroupHost& GetAgentSchedulingGroup() override;
   TestRenderWidgetHost* GetRenderWidgetHost() override;
   void AddMessageToConsole(blink::mojom::ConsoleMessageLevel level,
                            const std::string& message) override;
