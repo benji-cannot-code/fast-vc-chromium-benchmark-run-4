f772402a574b584e56e3ffa6a886119622a9a6b1
Mikel Astiz <mastiz@chromium.org>
2023-03-20 11:38:20 +0000

    [sheriff] Revert "Enable tmux integration and xterm.js flags for terminal by default"
    This reverts commit e8a6149866a10bb71be9b6425bf803ab135f8eaf.
    
    Reason for revert: speculative revert for tast failures starting
    with https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-eve-chrome/25826/overview including tests:
    crostini.CommandCd.bullseye_stable
    crostini.CommandPs.bullseye_stable
    crostini.CommandVim.bullseye_stable
    crostini.OpenWithTerminal.bullseye_stable
    crostini.RemoveCancel.bullseye_stable
    crostini.Restart.bullseye_stable
    
    Original change's description:
    > Enable tmux integration and xterm.js flags for terminal by default
    >
    > Change-Id: I544e05a2a44c22e031f00a5a13dbcd5c9bafc755
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349567
    > Auto-Submit: Jason Lin <lxj@google.com>
    > Commit-Queue: Jason Lin <lxj@google.com>
    > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > Quick-Run: Jason Lin <lxj@google.com>
    > Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1119149}
    
    Change-Id: I41cb52caad7d51f9cddb71743c842a327ae44519
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4352536
    Owners-Override: Mikel Astiz <mastiz@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119261}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1989,7 +1989,7 @@ BASE_FEATURE(kTelemetryExtension,
 // Enables the alternative emulator for the Terminal app.
 BASE_FEATURE(kTerminalAlternativeEmulator,
              "TerminalAlternativeEmulator",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 //
 // Enables Terminal System App to load from Downloads for developer testing.
 // Only works in dev and canary channels.
@@ -2006,7 +2006,7 @@ BASE_FEATURE(kTerminalSftp, "TerminalSftp", base::FEATURE_ENABLED_BY_DEFAULT);
 // Enables tmux integration in the Terminal System App.
 BASE_FEATURE(kTerminalTmuxIntegration,
              "TerminalTmuxIntegration",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Enables time of day screen saver.
 BASE_FEATURE(kTimeOfDayScreenSaver,
