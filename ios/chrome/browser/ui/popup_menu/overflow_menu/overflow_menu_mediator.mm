ebfb2d4d512257590ff06ba3b33620acf0b23451
Ewann Pelle <ewannpv@chromium.org>
2023-03-20 12:25:48 +0000

    [iOS][PT] Hide pin action in incognito
    Fixed: 1425585
    Change-Id: I3161f2774cb9fdc7d90b50b193f9240ab7db3422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4352948
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119273}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -1249,7 +1249,7 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
     [appActions addObject:self.openNewWindowAction];
   }
 
-  if (IsPinnedTabsOverflowEnabled()) {
+  if (IsPinnedTabsOverflowEnabled() && !self.isIncognito) {
     [appActions addObject:([self isTabPinned] ? self.unpinTabAction
                                               : self.pinTabAction)];
   }
