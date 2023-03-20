63715863e599b55b421608dfe1ca89fa40c43fb2
Marc Treib <treib@chromium.org>
2023-03-20 15:19:41 +0000

    Sync CTBMTP: Improve pending_clear_metadata_
    In Sync's ClientTagBasedModelTypeProcessor, pending_clear_metadata_ gets
    set to true if ClearMetadataWhileStopped() gets called before
    ModelReadyToSync(). In that case, the clear gets executed by
    CheckForInvalidPersistedMetadata(), called from ModelReadyToSync().
    
    This CL makes two small improvements to this flow:
    1) Execute the clear if entity metadata *or ModelTypeState* exists
       (previously, we only checked for existence of entity metadata).
    2) Check for pending_clear_metadata_ first, before other validity
       checks, to ensure that after CheckForInvalidPersistedMetadata(),
       pending_clear_metadata_ is false again. This keeps the state a bit
       "cleaner", and may be useful for [D]CHECKs etc.
    
    These changes required a minor test setup improvement:
    Tests that want to start without the initial sync being done already
    now actually leave the ModelTypeState empty (as it would happen in
    prod), rather than explicitly setting it to "UNSPECIFIED".
    
    Bug: 1423326
    Change-Id: Ia5ecac608025b4c25eea11d69d5a1708236335b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350392
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1119335}

--- components/sync/model/client_tag_based_model_type_processor_unittest.cc
+++ components/sync/model/client_tag_based_model_type_processor_unittest.cc
@@ -340,8 +340,10 @@ class ClientTagBasedModelTypeProcessorTest : public ::testing::Test {
     histogram_tester_ = std::make_unique<base::HistogramTester>();
   }
 
-  void InitializeToMetadataLoaded(bool initial_sync_done = true) {
-    bridge()->SetInitialSyncDone(initial_sync_done);
+  void InitializeToMetadataLoaded(bool set_initial_sync_done = true) {
+    if (set_initial_sync_done) {
+      bridge()->SetInitialSyncDone(true);
+    }
     ModelReadyToSync();
   }
 
@@ -2061,7 +2063,7 @@ TEST_F(ClientTagBasedModelTypeProcessorTest,
 // storage" for historical reasons) result in reporting setup duration.
 TEST_F(ClientTagBasedModelTypeProcessorTest,
        ShouldReportEphemeralConfigurationTime) {
-  InitializeToMetadataLoaded(/*initial_sync_done=*/false);
+  InitializeToMetadataLoaded(/*set_initial_sync_done=*/false);
   OnSyncStarting(kDefaultAuthenticatedAccountId, kCacheGuid,
                  SyncMode::kTransportOnly);
 
@@ -2086,7 +2088,7 @@ TEST_F(ClientTagBasedModelTypeProcessorTest,
 // for historical reasons) do not result in reporting setup duration.
 TEST_F(ClientTagBasedModelTypeProcessorTest,
        ShouldReportPersistentConfigurationTime) {
-  InitializeToMetadataLoaded(/*initial_sync_done=*/false);
+  InitializeToMetadataLoaded(/*set_initial_sync_done=*/false);
   OnSyncStarting();
 
   base::HistogramTester histogram_tester;
@@ -2155,7 +2157,7 @@ TEST_F(FullUpdateClientTagBasedModelTypeProcessorTest,
 // for historical reasons) result in reporting setup duration.
 TEST_F(FullUpdateClientTagBasedModelTypeProcessorTest,
        ShouldReportEphemeralConfigurationTimeOnlyForFirstFullUpdate) {
-  InitializeToMetadataLoaded(/*initial_sync_done=*/false);
+  InitializeToMetadataLoaded(/*set_initial_sync_done=*/false);
   OnSyncStarting(kDefaultAuthenticatedAccountId, kCacheGuid,
                  SyncMode::kTransportOnly);
 
@@ -2979,7 +2981,7 @@ TEST_F(ClientTagBasedModelTypeProcessorTest,
   db()->PutMetadata(kKey2, std::move(entity_metadata2));
   db()->PutMetadata(kKey3, std::move(entity_metadata3));
 
-  InitializeToMetadataLoaded(/*initial_sync_done=*/false);
+  InitializeToMetadataLoaded(/*set_initial_sync_done=*/false);
   OnSyncStarting();
 
   // Since initial_sync_done was false, metadata should have been cleared.
@@ -3058,7 +3060,7 @@ TEST_F(ClientTagBasedModelTypeProcessorTest,
 
 TEST_F(ClientTagBasedModelTypeProcessorTest,
        ShouldNotProcessInvalidRemoteFullUpdate) {
-  InitializeToMetadataLoaded(/*initial_sync_done=*/false);
+  InitializeToMetadataLoaded(/*set_initial_sync_done=*/false);
   OnSyncStarting();
 
   UpdateResponseDataList updates;
@@ -3128,6 +3130,7 @@ TEST_F(ClientTagBasedModelTypeProcessorTest, ShouldClearMetadataWhileStopped) {
   // Should clear the metadata even if already stopped.
   type_processor()->ClearMetadataWhileStopped();
   EXPECT_FALSE(type_processor()->IsTrackingMetadata());
+  EXPECT_EQ(0U, db()->model_type_state().ByteSizeLong());
   EXPECT_EQ(0U, db()->metadata_count());
   // Expect an entry to the histogram.
   histogram_tester.ExpectTotalCount(
@@ -3163,8 +3166,41 @@ TEST_F(ClientTagBasedModelTypeProcessorTest,
   InitializeToMetadataLoaded();
   // Tracker should have not been set.
   EXPECT_FALSE(type_processor()->IsTrackingMetadata());
-  // Metadata was cleared from the persistent storage.
+  // Metadata should have been cleared from the persistent storage.
+  EXPECT_EQ(0U, db()->model_type_state().ByteSizeLong());
   EXPECT_EQ(0U, db()->metadata_count());
+  // Expect recording of the delayed clear.
+  histogram_tester.ExpectTotalCount(
+      "Sync.ClearMetadataWhileStopped.ImmediateClear", 0);
+  histogram_tester.ExpectTotalCount(
+      "Sync.ClearMetadataWhileStopped.DelayedClear", 1);
+
+  // Metadata clearing logic should not trigger bridge's OnSyncStarting().
+  EXPECT_FALSE(bridge()->sync_started());
+}
+
+TEST_F(ClientTagBasedModelTypeProcessorTest,
+       ShouldClearMetadataWhileStoppedUponModelReadyToSyncWithoutEntities) {
+  base::HistogramTester histogram_tester;
+
+  // Called before ModelReadyToSync().
+  type_processor()->ClearMetadataWhileStopped();
+
+  // Nothing recorded to the histograms yet.
+  histogram_tester.ExpectTotalCount(
+      "Sync.ClearMetadataWhileStopped.ImmediateClear", 0);
+  histogram_tester.ExpectTotalCount(
+      "Sync.ClearMetadataWhileStopped.DelayedClear", 0);
+
+  // Don't set up any entity metadata - there will only be the non-empty
+  // ModelTypeState.
+
+  InitializeToMetadataLoaded();
+  // Tracker should have not been set.
+  EXPECT_FALSE(type_processor()->IsTrackingMetadata());
+  // Metadata should have been cleared from the persistent storage.
+  EXPECT_EQ(0U, db()->model_type_state().ByteSizeLong());
+  ASSERT_EQ(0U, db()->metadata_count());
   // Expect recording of the delayed clear.
   histogram_tester.ExpectTotalCount(
       "Sync.ClearMetadataWhileStopped.ImmediateClear", 0);
@@ -3195,7 +3231,7 @@ TEST_F(ClientTagBasedModelTypeProcessorTest,
 
 TEST_F(ClientTagBasedModelTypeProcessorTest,
        ShouldNotClearMetadataWhileStoppedWithoutMetadataInitially) {
-  InitializeToMetadataLoaded(/*initial_sync_done=*/false);
+  InitializeToMetadataLoaded(/*set_initial_sync_done=*/false);
   ASSERT_FALSE(type_processor()->IsTrackingMetadata());
 
   base::HistogramTester histogram_tester;
@@ -3215,7 +3251,7 @@ TEST_F(ClientTagBasedModelTypeProcessorTest,
   // Called before ModelReadyToSync().
   type_processor()->ClearMetadataWhileStopped();
 
-  InitializeToMetadataLoaded(/*initial_sync_done=*/false);
+  InitializeToMetadataLoaded(/*set_initial_sync_done=*/false);
   ASSERT_FALSE(type_processor()->IsTrackingMetadata());
   // Nothing recorded to the histograms.
   histogram_tester.ExpectTotalCount(
