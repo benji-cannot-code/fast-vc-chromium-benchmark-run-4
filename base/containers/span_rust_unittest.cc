19b0513ba6ba5dabc7bfd482f24ac3cd8ff8ef2c
Adrian Taylor <adetaylor@chromium.org>
2023-03-20 15:47:22 +0000

    Reland "[rust] Remove global Rust enablement defines"
    This is a reland of commit ab9ac7c971c99f63bf46cfb440e854e309b3209c
    
    Reason for revert: the original commit renamed a generated header file,
    but I missed a couple of spots where that header file was being
    #included. This didn't cause problems in local testing or in trybots
    because the originally-named file still existed, but caused a build
    failure in clean builds where this file didn't exist.
    
    Fix in the reland: fix the two places where the original file was #included.
    
    Original change's description:
    > [rust] Remove global Rust enablement defines
    >
    > We are aiming to enable Rust incrementally, feature-by-feature, so
    > global "this toolchain has Rust" flags are unhealthy. Remove them and
    > replace them with feature-by-feature flags.
    >
    > In particular, //base depended on Rust in a few ways even when
    > 'enable_rust_json' was switched off. With this CL, //base builds
    > identically whether 'enable_rust' is turned on or off. Rust is only
    > involved if 'enable_rust_json' is switched on.
    >
    > Bug: 1292038
    > Change-Id: Ie7ec0f998aea87807b3301c00d89f185bbcb5fe2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350448
    > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1119040}
    
    Bug: 1292038
    Change-Id: I932f5eb39203bac446f406a6273fc5b40fca506d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350022
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119357}

--- base/containers/span_rust_unittest.cc
+++ base/containers/span_rust_unittest.cc
@@ -4,11 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/containers/span_rust.h"
-#include "build/rust/rust_buildflags.h"
+#include "base/rust_buildflags.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(TOOLCHAIN_HAS_RUST)
+#if BUILDFLAG(BUILD_RUST_BASE_CONVERSIONS)
 
 namespace base {
 namespace {
@@ -24,4 +24,4 @@ TEST(BaseSpanRustTest, SliceConstruct) {
 }  // namespace
 }  // namespace base
 
-#endif  // BUILDFLAG(TOOLCHAIN_HAS_RUST)
+#endif  // BUILDFLAG(BUILD_RUST_BASE_CONVERSIONS)
--- base/strings/string_piece_rust_unittest.cc
+++ base/strings/string_piece_rust_unittest.cc
@@ -4,11 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/strings/string_piece_rust.h"
-#include "build/rust/rust_buildflags.h"
+#include "base/rust_buildflags.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(TOOLCHAIN_HAS_RUST)
+#if BUILDFLAG(BUILD_RUST_BASE_CONVERSIONS)
 
 namespace base {
 namespace {
@@ -33,4 +33,4 @@ TEST(BaseStringPieceRustTest, StrToSlice) {
 }  // namespace
 }  // namespace base
 
-#endif  // BUILDFLAG(TOOLCHAIN_HAS_RUST)
+#endif  // BUILDFLAG(BUILD_RUST_BASE_CONVERSIONS)
