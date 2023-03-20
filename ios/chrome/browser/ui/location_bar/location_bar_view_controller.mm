b3cdbdce42c6216cd725781c953e6123f9029c05
Gauthier Ambard <gambard@chromium.org>
2023-03-20 12:19:38 +0000

    [iOS] Cleanup SF Symbol in NTP and location bar
    This CL removes the code to use PNGs in the NTP and the location bar.
    
    Bug: 1425175
    Change-Id: I9cb85e4d2b3b85a42bd98c6109fb51e00f4ae929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350113
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119271}

--- ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
@@ -480,13 +480,9 @@ const NSString* kScribbleOmniboxElementId = @"omnibox";
           forControlEvents:UIControlEventTouchUpInside];
 
       UIImage* shareImage =
-          UseSymbols()
-              ? DefaultSymbolWithPointSize(kShareSymbol, kSymbolImagePointSize)
-              : [UIImage imageNamed:@"location_bar_share"];
-      [self.locationBarSteadyView.trailingButton
-          setImage:[shareImage imageWithRenderingMode:
-                                   UIImageRenderingModeAlwaysTemplate]
-          forState:UIControlStateNormal];
+          DefaultSymbolWithPointSize(kShareSymbol, kSymbolImagePointSize);
+      [self.locationBarSteadyView.trailingButton setImage:shareImage
+                                                 forState:UIControlStateNormal];
       self.locationBarSteadyView.trailingButton.accessibilityLabel =
           l10n_util::GetNSString(IDS_IOS_TOOLS_MENU_SHARE);
       self.locationBarSteadyView.trailingButton.accessibilityIdentifier =
@@ -504,14 +500,10 @@ const NSString* kScribbleOmniboxElementId = @"omnibox";
                     action:@selector(startVoiceSearch)
           forControlEvents:UIControlEventTouchUpInside];
 
-      UIImage* micImage = UseSymbols()
-                              ? DefaultSymbolWithPointSize(
-                                    kMicrophoneSymbol, kSymbolImagePointSize)
-                              : [UIImage imageNamed:@"location_bar_voice"];
-      [self.locationBarSteadyView.trailingButton
-          setImage:[micImage imageWithRenderingMode:
-                                 UIImageRenderingModeAlwaysTemplate]
-          forState:UIControlStateNormal];
+      UIImage* micImage =
+          DefaultSymbolWithPointSize(kMicrophoneSymbol, kSymbolImagePointSize);
+      [self.locationBarSteadyView.trailingButton setImage:micImage
+                                                 forState:UIControlStateNormal];
       self.locationBarSteadyView.trailingButton.accessibilityLabel =
           l10n_util::GetNSString(IDS_IOS_TOOLS_MENU_VOICE_SEARCH);
       self.locationBarSteadyView.trailingButton.accessibilityIdentifier =
