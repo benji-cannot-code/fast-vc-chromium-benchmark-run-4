13ca0df83617a261d28f529e7c7b204522d2a903
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2023-03-21 22:47:44 +0000

    cros-components: Adds cros-components loading test
    Adds a test that loads the component and waits for the custom element
    to be defined.
    
    If the resource couldn't be loaded for example if lit imports were
    not re-written or the files wasn't added to the resources, prints a
    messages indicating importing what URL failed e.g.
    
    Failed to load resource: net::ERR_FAILED - chrome://resources/mwc/lit/decorators.js
    
    Bug: 264070221
    Change-Id: I51ec16852117e5d549e632f56efe78954decebdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349760
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120247}

--- content/public/test/browser_test_utils.h
+++ content/public/test/browser_test_utils.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_frame.h"
 #include "content/public/browser/browser_message_filter.h"
 #include "content/public/browser/commit_deferring_condition.h"
+#include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/render_frame_metadata_provider.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_process_host_observer.h"
@@ -1981,6 +1982,29 @@ class WebContentsConsoleObserver : public WebContentsObserver {
   std::string pattern_;
   WaiterHelper waiter_helper_;
   std::vector<Message> messages_;
+};
+
+// A helper class to get DevTools inspector log messages (e.g. network errors).
+class DevToolsInspectorLogWatcher : public DevToolsAgentHostClient {
+ public:
+  explicit DevToolsInspectorLogWatcher(WebContents* web_contents);
+  ~DevToolsInspectorLogWatcher() override;
+
+  void FlushAndStopWatching();
+  std::string last_message() { return last_message_; }
+  GURL last_url() { return last_url_; }
+
+  // DevToolsAgentHostClient:
+  void DispatchProtocolMessage(DevToolsAgentHost* host,
+                               base::span<const uint8_t> message) override;
+  void AgentHostClosed(DevToolsAgentHost* host) override;
+
+ private:
+  scoped_refptr<DevToolsAgentHost> host_;
+  base::RunLoop run_loop_enable_log_;
+  base::RunLoop run_loop_disable_log_;
+  std::string last_message_;
+  GURL last_url_;
 };
 
 // Static methods that simulates Mojo methods as if they were called by a
