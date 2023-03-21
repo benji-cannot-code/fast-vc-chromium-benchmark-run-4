d22195b755bd3a946ee1d6c2f5ec22328d953727
Joemer Ramos <joemerramos@chromium.org>
2023-03-21 15:52:09 +0000

    [iOS] SecCertificate Changes for iOS 14 Deprecation
    When deployment target is bumped up, we need to add an else for the
    compile guard because current viable code is behind an available guard.
    Net code is used for both mac and cronet and need to support backwards
    compatibility.
    
    Bug: 1406561
    Change-Id: I4f55cc837d8e240a2496f0e636fe9ac9b8b21624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355891
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119949}

--- net/cert/x509_util_apple.cc
+++ net/cert/x509_util_apple.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
 #include "build/build_config.h"
 #include "net/cert/x509_certificate.h"
@@ -147,6 +148,9 @@ base::ScopedCFTypeRef<CFArrayRef> CertificateChainFromSecTrust(
         SecTrustCopyCertificateChain(trust));
   }
 
+#if (BUILDFLAG(IS_MAC) &&                                    \
+     MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_VERSION_12_0) || \
+    (BUILDFLAG(IS_IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0)
   base::ScopedCFTypeRef<CFMutableArrayRef> chain(
       CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks));
   const CFIndex chain_length = SecTrustGetCertificateCount(trust);
@@ -154,6 +158,16 @@ base::ScopedCFTypeRef<CFArrayRef> CertificateChainFromSecTrust(
     CFArrayAppendValue(chain, SecTrustGetCertificateAtIndex(trust, i));
   }
   return base::ScopedCFTypeRef<CFArrayRef>(chain.release());
+
+#else
+  // The other logic paths should be used, this is just to make the compiler
+  // happy.
+  NOTREACHED();
+  return base::ScopedCFTypeRef<CFArrayRef>(nullptr);
+#endif  // BUILDFLAG(IS_MAC) && MAC_OS_X_VERSION_MIN_REQUIRED <
+        // MAC_OS_VERSION_12_0
+        // || (BUILDFLAG(IS_IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED <
+        // __IPHONE_15_0)
 }
 
 }  // namespace x509_util
