8ca6b186444cb64cc805a9c25f044867418f462f
Kristi Saney <kristislee@google.com>
2023-03-21 17:41:10 +0000

    Ensure we don't scroll on a simple click.
    We should only be scrolling the main page or side panel if the user does a selection, not a click. See this comment thread for more details: https://docs.google.com/spreadsheets/d/1OoBjPjaQ4iYaLw5DJD2WCydDz93Q7YsJLhxvrsGtheo/r/0-vS_BVl-r_-WBApc9BRURMg/edit?disco=AAAAsrpOcXw.
    
    Bug: 1266555
    Change-Id: I193eebb064ddd243007382d557a7de0b5a7d339b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338707
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120038}

--- chrome/renderer/accessibility/read_anything_app_model.h
+++ chrome/renderer/accessibility/read_anything_app_model.h
@@ -84,7 +84,6 @@ class ReadAnythingAppModel {
   bool NodeIsContentNode(ui::AXNodeID ax_node_id) const;
   void OnThemeChanged(read_anything::mojom::ReadAnythingThemePtr new_theme);
 
-  void ResetSelection();
   void InsertDisplayNode(ui::AXNodeID node);
   void Reset(const std::vector<ui::AXNodeID>& content_node_ids);
 
