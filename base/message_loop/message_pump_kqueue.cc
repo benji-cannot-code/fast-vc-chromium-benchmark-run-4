d74215e702aa710fc1ecbc5f045e37e0af30c0ab
danakj <danakj@chromium.org>
2023-03-21 23:17:47 +0000

    Document more explicitly, and CHECK correctness in the IDMap API.
    Lookup() claims implicitly to return if an `id` is in the map,
    however it really returns if the `id` is there _and_ the value is
    not nullptr. Update the docs to say so. And use comparison against
    nullptr instead of operator bool to check the value, since it
    should be a pointer-like thing.
    
    Remove() had a NOTREACHED() that the id was in the map, but Lookup()
    has no way to tell you if the id is in the map, so the caller would
    need to track the set of ids in the map independently to get this
    right. Remove the NOTREACHED() and document that Remove() does nothing
    if the id is not in the map.
    
    AddWithID() would make sure the id was not queued for removal, and if
    so it would un-queue and replace it. Add() did not have the same
    protection even though the key-type can be provided by the user and
    may resolve to the same value more than once. Make Add() handle this
    case and test for it.
    
    CHECK against signed integer overflow, and CHECK instead of DCHECK
    that a value is not overwritten accidentally.
    
    R=thestig@chromium.org
    
    Bug: 1423404
    Change-Id: Ifb77d9c3aecbd18fb5bd14d5a6f8495e6dc83308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356312
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120263}

--- base/message_loop/message_pump_kqueue.cc
+++ base/message_loop/message_pump_kqueue.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/mac_util.h"
 #include "base/mac/mach_logging.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
+#include "base/notreached.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/time/time_override.h"
 
