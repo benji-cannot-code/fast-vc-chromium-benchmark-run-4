14d041398c609d2a7e578d692a20af0b538e2990
Moe Ahmadi <mahmadi@chromium.org>
2023-03-21 17:12:38 +0000

    [omnibox] Renames ACController's `in_start_` to `sync_pass_done_`
    `in_start_` is not a very intuitive name, especially when thought
    of in relation to `done_`. This change renames that property to
    `sync_pass_done_` for more consistency with `done_` and reverses
    its values and conditions everywhere it is checked in the code.
    
    This does not introduce a functional change to the ACController.
    
    Bug: 1405555
    Change-Id: I9d479c2a178678df88daed280f56a99ccad15515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355994
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120012}

--- components/omnibox/browser/autocomplete_controller_metrics.cc
+++ components/omnibox/browser/autocomplete_controller_metrics.cc
@@ -103,7 +103,7 @@ void AutocompleteControllerMetrics::OnStop() {
   // Done providers should already be logged by `OnProviderUpdate()`.
   for (const auto& provider : controller_->providers()) {
     if (!provider->done()) {
-      DCHECK(!controller_->done() || controller_->in_start());
+      DCHECK(!controller_->done() || !controller_->sync_pass_done());
       LogProviderTimeMetrics(*provider);
     }
   }
@@ -163,10 +163,11 @@ void AutocompleteControllerMetrics::LogSuggestionChangeIndexMetrics(
   size_t max = AutocompleteResult::kMaxAutocompletePositionValue;
   // These metrics are logged up to about 50 times per omnibox keystroke, so use
   // UMA macros for efficiency.
-  if (controller_->in_start())
+  if (!controller_->sync_pass_done()) {
     UMA_HISTOGRAM_EXACT_LINEAR(name + ".CrossInput", change_index, max);
-  else
+  } else {
     UMA_HISTOGRAM_EXACT_LINEAR(name + ".Async", change_index, max);
+  }
   UMA_HISTOGRAM_EXACT_LINEAR(name, change_index, max);
 }
 
@@ -175,9 +176,10 @@ void AutocompleteControllerMetrics::LogSuggestionChangeInAnyPositionMetrics(
   std::string name = "Omnibox.MatchStability2.MatchChangeInAnyPosition";
   // These metrics are logged up to about 5 times per omnibox keystroke, so
   // use UMA macros for efficiency.
-  if (controller_->in_start())
+  if (!controller_->sync_pass_done()) {
     UMA_HISTOGRAM_BOOLEAN(name + ".CrossInput", changed);
-  else
+  } else {
     UMA_HISTOGRAM_BOOLEAN(name + ".Async", changed);
+  }
   UMA_HISTOGRAM_BOOLEAN(name, changed);
 }
