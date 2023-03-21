3f1009d4c31d1b56e877ec28ec281227694856e9
Henrique Ferreiro <hferreiro@igalia.com>
2023-03-21 09:47:56 +0000

    Add additional tests for the software cursor
    Follow-up from https://crrev.com/c/4310794. Test that the software
    cursor representation, size, hotspot and window are correct.
    
    Bug: 1229231
    Change-Id: If113795abefd6ccde62946d1b1985eb58a6385ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4255949
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1119833}

--- ash/display/cursor_window_controller.cc
+++ ash/display/cursor_window_controller.cc
@@ -486,6 +486,10 @@ void CursorWindowController::UpdateCursorImage() {
     image =
         *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(resource_id);
   }
+  // Use `gfx::ToFlooredPoint` as `ImageSkiaRep::GetWidth` is implemented as
+  // `return static_cast<int>(pixel_width() / scale());`.
+  hot_point_ = gfx::ToFlooredPoint(
+      gfx::ConvertPointToDips(hot_point_in_physical_pixels, cursor_scale));
 
   gfx::ImageSkia resized = image;
 
@@ -500,8 +504,7 @@ void CursorWindowController::UpdateCursorImage() {
     resized = gfx::ImageSkiaOperations::CreateResizedImage(
         image, skia::ImageOperations::ResizeMethod::RESIZE_BEST,
         gfx::ScaleToCeiledSize(image.size(), rescale));
-    hot_point_in_physical_pixels =
-        gfx::ScaleToCeiledPoint(hot_point_in_physical_pixels, rescale);
+    hot_point_ = gfx::ScaleToCeiledPoint(hot_point_, rescale);
   }
 
   if (cursor_color_ != kDefaultCursorColor) {
@@ -511,10 +514,6 @@ void CursorWindowController::UpdateCursorImage() {
   }
 
   delegate_->SetCursorImage(resized.size(), resized);
-
-  // TODO(danakj): Should this be rounded? Or kept as a floating point?
-  hot_point_ = gfx::ToFlooredPoint(
-      gfx::ConvertPointToDips(hot_point_in_physical_pixels, cursor_scale));
 
   if (cursor_view_widget_) {
     static_cast<cursor::CursorView*>(cursor_view_widget_->GetContentsView())
