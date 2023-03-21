3ba5ce42dd032ec20532d65d1376e04b0412e373
Ritika Gupta <ritikagup@google.com>
2023-03-21 01:38:33 +0000

    Integrating pref migration code with SegmentationService.
    This CL ensures that migration from old prefs to new prefs runs before
    reading entries from last session for new prefs at startup.
    
    Change-Id: I0405ebdfedec4f73d19dac68bdbdb523705451ab
    Bug: 1424142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4352269
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119729}

--- components/segmentation_platform/internal/segmentation_platform_service_impl.h
+++ components/segmentation_platform/internal/segmentation_platform_service_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "components/segmentation_platform/internal/database/storage_service.h"
 #include "components/segmentation_platform/internal/metrics/field_trial_recorder.h"
+#include "components/segmentation_platform/internal/migration/prefs_migrator.h"
 #include "components/segmentation_platform/internal/platform_options.h"
 #include "components/segmentation_platform/internal/scheduler/execution_service.h"
 #include "components/segmentation_platform/internal/selection/cached_result_provider.h"
@@ -193,6 +194,9 @@ class SegmentationPlatformServiceImpl : public SegmentationPlatformService {
   std::unique_ptr<SegmentScoreProvider> segment_score_provider_;
 
   std::unique_ptr<ServiceProxyImpl> proxy_;
+
+  // Prefs Migration
+  std::unique_ptr<PrefsMigrator> prefs_migrator_;
 
   // PrefService from profile.
   raw_ptr<PrefService> profile_prefs_;
