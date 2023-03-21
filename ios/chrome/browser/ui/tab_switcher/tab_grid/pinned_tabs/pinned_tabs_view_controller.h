ce050b0fa679fdaaedc23097eaa47c0eb5fc5c02
Ewann Pelle <ewannpv@chromium.org>
2023-03-21 15:58:31 +0000

    [iOS][PT] Re-enable tab search when a drag session did end
    Fixed: 1426246
    Change-Id: I17c3152fee1d0cd7fa8703fe86772b6c264c1a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356512
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119953}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_view_controller.h
@@ -54,6 +54,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)pinnedViewControllerDropAnimationDidEnd:
     (PinnedTabsViewController*)pinnedTabsViewController;
 
+// Tells the delegate that a drag session did end.
+- (void)pinnedViewControllerDragSessionDidEnd:
+    (PinnedTabsViewController*)pinnedTabsViewController;
+
 @end
 
 // UICollectionViewController used to display pinned tabs.
