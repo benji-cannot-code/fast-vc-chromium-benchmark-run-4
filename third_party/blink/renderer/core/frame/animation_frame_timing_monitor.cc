8e4516f1356e0bed4049421273ea5e4c22ede244
Noam Rosenthal <nrosenthal@chromium.org>
2023-03-21 11:16:00 +0000

    LoAF: slightly improve names for event listeners
    Event targets with id/src expose that attribute as part of the name.
    (early feedback is that event target names are not specific enough)
    
    Bug: 1392685
    Change-Id: Ie7dd908bc148411c592eea95248c806d54d7146c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4358015
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119859}

--- third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
+++ third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_probe_sink.h"
 #include "third_party/blink/renderer/core/execution_context/agent.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/script/script.h"
 #include "third_party/blink/renderer/core/timing/animation_frame_timing_info.h"
 #include "third_party/blink/renderer/core/timing/dom_window_performance.h"
@@ -380,9 +381,20 @@ namespace {
 AtomicString GetClassLikeNameForEventTarget(EventTarget* event_target) {
   DCHECK(event_target);
   if (Node* node = event_target->ToNode()) {
-    // TODO: maybe use constructor name (e.g. HTMLImgElement) instead of node
-    // name (e.g. IMG)?
-    return AtomicString(node->nodeName());
+    StringBuilder builder;
+    builder.Append(node->nodeName());
+    if (Element* element = DynamicTo<Element>(node)) {
+      if (element->HasID()) {
+        builder.Append("#");
+        builder.Append(element->GetIdAttribute());
+      } else if (element->hasAttribute(html_names::kSrcAttr)) {
+        builder.Append("[src=");
+        builder.Append(element->getAttribute(html_names::kSrcAttr));
+        builder.Append("]");
+      }
+    }
+
+    return builder.ToAtomicString();
   } else {
     return event_target->InterfaceName();
   }
