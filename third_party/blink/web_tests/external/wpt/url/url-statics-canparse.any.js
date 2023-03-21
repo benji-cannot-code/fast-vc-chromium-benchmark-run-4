12cb885822fc6effa9a603f07233de6452723d6b
Weizhong Xia <weizhong@google.com>
2023-03-21 00:34:34 +0000

    Import wpt@8852d6723a0cb606e536d8d515db89f90c98d164
    Using wpt-import in Chromium 6956a020456d0e97dadf3fd192727470b0f953e0.
    With Chromium commits locally applied on WPT:
    5f92396bc3 "Use Fetch load image for Canvas tests"
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    japhet@chromium.org:
      external/wpt/fetch
    wolenetz@chromium.org:
      external/wpt/media-source
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I177a4db116611bb693e25817149789918452f6e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355051
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Nihar Damarasingu <nihardamar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1119696}

---
+++ third_party/blink/web_tests/external/wpt/url/url-statics-canparse.any.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This intentionally does not use resources/urltestdata.json to preserve resources.
+[
+  {
+    "url": undefined,
+    "base": undefined,
+    "expected": false
+  },
+  {
+    "url": "a:b",
+    "base": undefined,
+    "expected": true
+  },
+  {
+    "url": undefined,
+    "base": "a:b",
+    "expected": false
+  },
+  {
+    "url": "a:/b",
+    "base": undefined,
+    "expected": true
+  },
+  {
+    "url": undefined,
+    "base": "a:/b",
+    "expected": true
+  },
+  {
+    "url": "https://test:test",
+    "base": undefined,
+    "expected": false
+  },
+  {
+    "url": "a",
+    "base": "https://b/",
+    "expected": true
+  }
+].forEach(({ url, base, expected }) => {
+  test(() => {
+    assert_equals(URL.canParse(url, base), expected);
+  }, `URL.canParse(${url}, ${base})`);
+});
