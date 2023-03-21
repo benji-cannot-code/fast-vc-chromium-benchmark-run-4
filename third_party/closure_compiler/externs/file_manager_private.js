84da599cd0a1661ee31facbc0953d3e91f9487e6
Marcello Salomao <msalomao@google.com>
2023-03-21 04:59:29 +0000

    [DriveFsBulkPinning] Connect BulkPinning dialog to backend
    Update prefs when the 'continue' button is clicked and respond
    to prefs changes on the corresponding banner.
    
    Bug: b:273132989
    Test: Manual
    Test: chromeos_unittests SyncStatusTrackerTest.*
    Change-Id: I470eed05b234b90974ff5cf5a0a7a04bb15f244d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4333577
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119775}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -656,7 +656,8 @@ chrome.fileManagerPrivate.GetVolumeRootOptions;
  *   folderShortcuts: !Array<string>,
  *   trashEnabled: boolean,
  *   officeFileMovedOneDrive: number,
- *   officeFileMovedGoogleDrive: number
+ *   officeFileMovedGoogleDrive: number,
+ *   driveFsBulkPinningEnabled: boolean
  * }}
  */
 chrome.fileManagerPrivate.Preferences;
@@ -666,7 +667,8 @@ chrome.fileManagerPrivate.Preferences;
  *   cellularDisabled: (boolean|undefined),
  *   arcEnabled: (boolean|undefined),
  *   arcRemovableMediaAccessEnabled: (boolean|undefined),
- *   folderShortcuts: (!Array<string>|undefined)
+ *   folderShortcuts: (!Array<string>|undefined),
+ *   driveFsBulkPinningEnabled: (boolean|undefined)
  * }}
  */
 chrome.fileManagerPrivate.PreferencesChange;
@@ -1697,12 +1699,6 @@ chrome.fileManagerPrivate.openManageSyncSettings = function() {};
  *     callback
  */
 chrome.fileManagerPrivate.parseTrashInfoFiles = function(entries, callback) {};
-
-/**
- * Sets the bulk pinning feature as enabled (true) or disabled (false).
- * @param {boolean} shouldEnable
- */
-chrome.fileManagerPrivate.toggleBulkPinning = function(shouldEnable) {};
 
 /**
  * @type {!ChromeEvent}
--- ui/file_manager/file_manager/foreground/js/ui/banners/drive_bulk_pinning_banner.js
+++ ui/file_manager/file_manager/foreground/js/ui/banners/drive_bulk_pinning_banner.js
@@ -26,7 +26,7 @@ export class DriveBulkPinningBanner extends EducationalBanner {
   constructor() {
     super();
 
-    this.shadowRoot.querySelector('.learn-more')
+    this.shadowRoot.querySelector('.action-button')
         .addEventListener('click', (e) => {
           e.preventDefault();
           document.querySelector('xf-bulk-pinning-dialog').show();
