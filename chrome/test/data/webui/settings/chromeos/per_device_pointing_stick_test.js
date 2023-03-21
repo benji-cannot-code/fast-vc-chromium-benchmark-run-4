152c52e41aeb4a4833974534b1cf38d4aa2b15fc
Danny Wang <wangdanny@google.com>
2023-03-21 20:24:56 +0000

    Settings Split: Update pointing stick settings observer in device page
    Bug: b/241965700
    Test: browser_tests OSSettingsPerDevicePointingStickV3Test
    Change-Id: I9b6688374ca0b52a7c69f22be4938d6e25ba5787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355601
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Quick-Run: Danny Wang <wangdanny@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120150}

--- chrome/test/data/webui/settings/chromeos/per_device_pointing_stick_test.js
+++ chrome/test/data/webui/settings/chromeos/per_device_pointing_stick_test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {FakeInputDeviceSettingsProvider, fakePointingSticks, setInputDeviceSettingsProviderForTesting, SettingsPerDevicePointingStickElement} from 'chrome://os-settings/chromeos/os_settings.js';
+import {fakePointingSticks, fakePointingSticks2, SettingsPerDevicePointingStickElement} from 'chrome://os-settings/chromeos/os_settings.js';
 import {assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
@@ -12,27 +12,21 @@ suite('PerDevicePointingStick', function() {
    * @type {?SettingsPerDevicePointingStickElement}
    */
   let perDevicePointingStickPage = null;
-  /**
-   * @type {?FakeInputDeviceSettingsProvider}
-   */
-  let provider = null;
 
   setup(() => {
     PolymerTest.clearBody();
-    provider = new FakeInputDeviceSettingsProvider();
-    provider.setFakePointingSticks(fakePointingSticks);
-    setInputDeviceSettingsProviderForTesting(provider);
   });
 
   teardown(() => {
     perDevicePointingStickPage = null;
-    provider = null;
   });
 
-  function initializePerDevicePointingStickPage() {
+  function initializePerDevicePointingStickPage(
+      pointingSticks = fakePointingSticks) {
     perDevicePointingStickPage =
         document.createElement('settings-per-device-pointing-stick');
     assertTrue(perDevicePointingStickPage != null);
+    perDevicePointingStickPage.pointingSticks = pointingSticks;
     document.body.appendChild(perDevicePointingStickPage);
     return flushTasks();
   }
@@ -43,12 +37,12 @@ suite('PerDevicePointingStick', function() {
         'settings-per-device-pointing-stick-subsection');
     assertEquals(fakePointingSticks.length, subsections.length);
 
-    const newFakePointingSticks = fakePointingSticks.slice(1);
-    provider.setFakePointingSticks(newFakePointingSticks);
+    // Check the number of subsections when the pointing stick list is updated.
+    perDevicePointingStickPage.pointingSticks = fakePointingSticks2;
     await flushTasks();
     subsections = perDevicePointingStickPage.shadowRoot.querySelectorAll(
         'settings-per-device-pointing-stick-subsection');
-    assertEquals(newFakePointingSticks.length, subsections.length);
+    assertEquals(fakePointingSticks2.length, subsections.length);
   });
 
   test(
