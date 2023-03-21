ce14fab3ea5ec07058115232b71095fa5a701629
Marc Treib <treib@chromium.org>
2023-03-21 17:37:49 +0000

    Sync: Add HISTORY EarlGrey tests
    This adds a set of EarlGrey tests (iOS E2E tests) for the HISTORY data
    type. The tests are fairly minimal, but that's okay, since almost all
    of the code is shared across platforms, and covered by
    sync_integration_tests. They do cover the small part that is different
    on iOS (the TabHelper).
    
    Bug: 1411307
    Change-Id: If850acd00f104e23786a7b3e6988f3da96eb300f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4247439
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120035}

--- ios/chrome/browser/ui/settings/sync/utils/sync_fake_server_egtest.mm
+++ ios/chrome/browser/ui/settings/sync/utils/sync_fake_server_egtest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "base/time/time.h"
+#import "components/sync/base/command_line_switches.h"
 #import "components/sync/base/features.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
@@ -19,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #import "ios/web/public/test/http_server/http_server_util.h"
+#import "net/base/mac/url_conversions.h"
+#import "net/test/embedded_test_server/embedded_test_server.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -68,7 +71,11 @@ void WaitForAutofillProfileLocallyPresent(const std::string& guid,
   [ChromeEarlGrey clearBookmarks];
 
   [ChromeEarlGrey clearSyncServerData];
+
   WaitForEntitiesOnFakeServer(0, syncer::AUTOFILL_PROFILE);
+  WaitForEntitiesOnFakeServer(0, syncer::BOOKMARKS);
+  WaitForEntitiesOnFakeServer(0, syncer::HISTORY);
+  WaitForEntitiesOnFakeServer(0, syncer::TYPED_URLS);
 
   [super tearDown];
 }
@@ -76,15 +83,20 @@ void WaitForAutofillProfileLocallyPresent(const std::string& guid,
 - (void)setUp {
   [super setUp];
 
+  GREYAssertTrue(self.testServer->Start(), @"Server did not start.");
+
   [ChromeEarlGrey clearSyncServerData];
 
   WaitForEntitiesOnFakeServer(0, syncer::AUTOFILL_PROFILE);
   WaitForEntitiesOnFakeServer(0, syncer::BOOKMARKS);
+  WaitForEntitiesOnFakeServer(0, syncer::HISTORY);
   WaitForEntitiesOnFakeServer(0, syncer::TYPED_URLS);
 }
 
 - (AppLaunchConfiguration)appConfigurationForTestCase {
-  AppLaunchConfiguration config;
+  AppLaunchConfiguration config = [super appConfigurationForTestCase];
+  config.additional_args.push_back(std::string("--") +
+                                   syncer::kSyncShortNudgeDelayForTest);
   if ([self isRunningTest:@selector(testSyncInvalidationsEnabled)]) {
     config.features_enabled.push_back(syncer::kUseSyncInvalidations);
   } else if ([self isRunningTest:@selector(testSyncTypedURLUpload)] ||
@@ -94,6 +106,9 @@ void WaitForAutofillProfileLocallyPresent(const std::string& guid,
     // TypedURL tests need to disable the History data type, since that one
     // replaced TypedURLs.
     config.features_disabled.push_back(syncer::kSyncEnableHistoryDataType);
+  } else if ([self isRunningTest:@selector(testSyncHistoryUpload)] ||
+             [self isRunningTest:@selector(testSyncHistoryDownload)]) {
+    config.features_enabled.push_back(syncer::kSyncEnableHistoryDataType);
   }
   return config;
 }
@@ -400,9 +415,9 @@ void WaitForAutofillProfileLocallyPresent(const std::string& guid,
                                    syncTimeout:kSyncOperationTimeout];
 
   // Wait for typed url to appear on client.
-  [ChromeEarlGrey waitForTypedURL:mockURL
-                    expectPresent:YES
-                          timeout:kSyncOperationTimeout];
+  [ChromeEarlGrey waitForHistoryURL:mockURL
+                      expectPresent:YES
+                            timeout:kSyncOperationTimeout];
 }
 
 // Tests that when typed url is deleted on the client, sync the change gets
@@ -427,9 +442,9 @@ void WaitForAutofillProfileLocallyPresent(const std::string& guid,
                                    syncTimeout:kSyncOperationTimeout];
 
   // Wait for typed url to appear on client.
-  [ChromeEarlGrey waitForTypedURL:mockURL
-                    expectPresent:YES
-                          timeout:kSyncOperationTimeout];
+  [ChromeEarlGrey waitForHistoryURL:mockURL
+                      expectPresent:YES
+                            timeout:kSyncOperationTimeout];
   GREYAssert(
       [ChromeEarlGrey numberOfSyncEntitiesWithType:syncer::TYPED_URLS] == 1,
       @"There should be 1 typed URL entity");
@@ -470,9 +485,74 @@ void WaitForAutofillProfileLocallyPresent(const std::string& guid,
 
   // Trigger sync and wait for fake server to be updated.
   [ChromeEarlGrey triggerSyncCycleForType:syncer::TYPED_URLS];
-  [ChromeEarlGrey waitForTypedURL:mockURL
-                    expectPresent:NO
-                          timeout:kSyncOperationTimeout];
+  [ChromeEarlGrey waitForHistoryURL:mockURL
+                      expectPresent:NO
+                            timeout:kSyncOperationTimeout];
+}
+
+// Tests that browsing history is uploaded to the Sync server.
+- (void)testSyncHistoryUpload {
+  const GURL preSyncURL = self.testServer->GetURL("/console.html");
+  const GURL whileSyncURL = self.testServer->GetURL("/pony.html");
+  const GURL postSyncURL = self.testServer->GetURL("/destination.html");
+
+  [ChromeEarlGrey clearBrowsingHistory];
+  [self setTearDownHandler:^{
+    [ChromeEarlGrey clearBrowsingHistory];
+  }];
+
+  // Visit a URL before turning on Sync.
+  [ChromeEarlGrey loadURL:preSyncURL];
+
+  // Navigate away from that URL.
+  [ChromeEarlGrey loadURL:whileSyncURL];
+
+  // Sign in and wait for sync to become active.
+  FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
+  [SigninEarlGrey addFakeIdentity:fakeIdentity];
+  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+
+  [ChromeEarlGrey
+      waitForSyncTransportStateActiveWithTimeout:kSyncOperationTimeout];
+
+  // Navigate again. This URL, plus the URL that was currently open when Sync
+  // was turned on, should arrive on the Sync server.
+  [ChromeEarlGrey loadURL:postSyncURL];
+
+  // This URL, plus the URL that was currently open when Sync was turned on,
+  // should arrive on the Sync server.
+  NSArray<NSURL*>* URLs = @[
+    net::NSURLWithGURL(whileSyncURL),
+    net::NSURLWithGURL(postSyncURL),
+  ];
+  [ChromeEarlGrey waitForSyncServerHistoryURLs:URLs
+                                       timeout:kSyncOperationTimeout];
+}
+
+// Tests that typed url is downloaded from sync server.
+- (void)testSyncHistoryDownload {
+  const GURL mockURL("http://not-a-real-site/");
+
+  [ChromeEarlGrey clearBrowsingHistory];
+  [self setTearDownHandler:^{
+    [ChromeEarlGrey clearBrowsingHistory];
+  }];
+
+  // Inject a history visit on the server.
+  [ChromeEarlGrey addFakeSyncServerHistoryVisit:mockURL];
+
+  // Sign in and wait for sync to become active.
+  FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
+  [SigninEarlGrey addFakeIdentity:fakeIdentity];
+  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+
+  [ChromeEarlGrey
+      waitForSyncTransportStateActiveWithTimeout:kSyncOperationTimeout];
+
+  // Wait for the visit to appear on the client.
+  [ChromeEarlGrey waitForHistoryURL:mockURL
+                      expectPresent:YES
+                            timeout:kSyncOperationTimeout];
 }
 
 // Tests download of two legacy bookmarks with the same item id.
--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -399,8 +399,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                      originator_client_item_id:
                          (NSString*)originator_client_item_id;
 
-// Injects typed URL to sync FakeServer.
+// Injects a typed URL to the sync FakeServer.
 + (void)addFakeSyncServerTypedURL:(NSString*)URL;
+
+// Injects a HISTORY visit to the sync FakeServer.
++ (void)addFakeSyncServerHistoryVisit:(NSURL*)URL;
 
 // Injects device info to sync FakeServer.
 + (void)addFakeSyncServerDeviceInfo:(NSString*)deviceName
@@ -415,7 +418,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // If the provided URL `spec` is either present or not present in HistoryService
 // (depending on `expectPresent`), return YES. If the present status of `spec`
 // is not what is expected, or there is an error, return NO.
-+ (BOOL)isTypedURL:(NSString*)spec presentOnClient:(BOOL)expectPresent;
++ (BOOL)isURL:(NSString*)spec presentOnClient:(BOOL)expectPresent;
 
 // Triggers a sync cycle for a `type`.
 + (void)triggerSyncCycleForType:(syncer::ModelType)type;
