2f52ffefcedf2734138a4a0466dfae0cb157e8ff
kylechar <kylechar@chromium.org>
2023-03-21 22:05:06 +0000

    Convert ExternalVkImageBacking storage
    Make it hold a vector of VkImage plus skia classes and another vector of
    GL textures. This is so it can hold a VkImage and GL texture per plane
    for multi-planar formats.
    
    ExternalVkImageBacking behaviour isn't expected to change since it's not
    used with multi-planar formats yet. The logic for mapping staging
    buffers to copy pixels to/from VkImage changes so a single staging
    buffer is used for all planes and it's always 4 byte aligned. This
    should have make no observable difference now since there is always a
    single plane.
    
    Bug: 1414560
    Change-Id: I9d34a5e378c026623589d0379a6ddeb6c22b9b5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357377
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120221}

--- gpu/vulkan/vulkan_command_buffer.cc
+++ gpu/vulkan/vulkan_command_buffer.cc
@@ -245,9 +245,10 @@ void VulkanCommandBuffer::CopyBufferToImage(VkBuffer buffer,
                                             uint32_t buffer_width,
                                             uint32_t buffer_height,
                                             uint32_t width,
-                                            uint32_t height) {
+                                            uint32_t height,
+                                            uint64_t buffer_offset) {
   VkBufferImageCopy region = {};
-  region.bufferOffset = 0;
+  region.bufferOffset = buffer_offset;
   region.bufferRowLength = buffer_width;
   region.bufferImageHeight = buffer_height;
   region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
@@ -265,9 +266,10 @@ void VulkanCommandBuffer::CopyImageToBuffer(VkBuffer buffer,
                                             uint32_t buffer_width,
                                             uint32_t buffer_height,
                                             uint32_t width,
-                                            uint32_t height) {
+                                            uint32_t height,
+                                            uint64_t buffer_offset) {
   VkBufferImageCopy region = {};
-  region.bufferOffset = 0;
+  region.bufferOffset = buffer_offset;
   region.bufferRowLength = buffer_width;
   region.bufferImageHeight = buffer_height;
   region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
