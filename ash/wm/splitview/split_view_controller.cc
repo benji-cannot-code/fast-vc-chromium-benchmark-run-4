1bda4047ae6f31a72691a19faabd6f78663455c5
Michele Fan <michelefan@chromium.org>
2023-03-21 20:34:53 +0000

    snap-group: Don't limit the resizing with divider with fixed ratios
    This CL removes the limitation with fixed ratios when resizing with
    the split view divider for snap group.
    
    Demo: http:/b/269346049#comment2
    Fixed: b/269346049
    Test: Manually + add unit test
    Change-Id: Ia50ff1f7a0a88ec448661c4b86209fe4a99a09ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350869
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120156}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -1256,8 +1256,10 @@ int SplitViewController::GetDividerPosition(SnapPosition snap_position,
   int next_divider_position = snap_position == SnapPosition::kPrimary
                                   ? snap_width
                                   : divider_end_position - snap_width;
-  if (split_view_type_ == SplitViewType::kTabletType)
+  if (split_view_type_ == SplitViewType::kTabletType ||
+      ShouldAutomaticallyGroupOnWindowsSnappedInClamshell()) {
     next_divider_position -= kSplitviewDividerShortSideLength / 2;
+  }
   return next_divider_position;
 }
 
@@ -1406,7 +1408,8 @@ void SplitViewController::EndResizeWithDivider(
   NotifyWindowResized();
 
   const int target_divider_position = GetClosestFixedDividerPosition();
-  if (divider_position_ == target_divider_position) {
+  if (divider_position_ == target_divider_position ||
+      ShouldAutomaticallyGroupOnWindowsSnappedInClamshell()) {
     EndResizeWithDividerImpl();
     EndSplitViewAfterResizingAtEdgeIfAppropriate();
   } else {
@@ -2490,10 +2493,11 @@ int SplitViewController::GetClosestFixedDividerPosition() {
   divider_closest_ratio_ = FindClosestPositionRatio(
       divider_position_ + kSplitviewDividerShortSideLength / 2,
       divider_end_position);
-  int fix_position = divider_end_position * divider_closest_ratio_;
-  if (divider_closest_ratio_ > 0.f && divider_closest_ratio_ < 1.f)
-    fix_position -= kSplitviewDividerShortSideLength / 2;
-  return fix_position;
+  int fixed_position = divider_end_position * divider_closest_ratio_;
+  if (divider_closest_ratio_ > 0.f && divider_closest_ratio_ < 1.f) {
+    fixed_position -= kSplitviewDividerShortSideLength / 2;
+  }
+  return fixed_position;
 }
 
 void SplitViewController::StopAndShoveAnimatedDivider() {
@@ -2723,13 +2727,13 @@ void SplitViewController::OnSnappedWindowDetached(aura::Window* window,
 
 float SplitViewController::FindClosestPositionRatio(float distance,
                                                     float length) {
-  float current_ratio = distance / length;
+  const float current_ratio = distance / length;
   float closest_ratio = 0.f;
   std::vector<float> position_ratios(
       kFixedPositionRatios,
       kFixedPositionRatios + sizeof(kFixedPositionRatios) / sizeof(float));
   ModifyPositionRatios(&position_ratios);
-  for (float ratio : position_ratios) {
+  for (const float ratio : position_ratios) {
     if (std::abs(current_ratio - ratio) <
         std::abs(current_ratio - closest_ratio)) {
       closest_ratio = ratio;
@@ -3093,7 +3097,7 @@ void SplitViewController::DoSplitDividerSpawnAnimation(aura::Window* window) {
                                                                       : p.y());
 }
 
-bool SplitViewController::ShouldShowDividerWidgetInClamshell() {
+bool SplitViewController::ShouldShowDividerWidgetInClamshell() const {
   DCHECK(primary_window_);
   DCHECK(secondary_window_);
   return InClamshellSplitViewMode() &&
