a5c3a04620a4fb94d705b5efbe58957d3125dfb3
Viktor Semeniuk <vsemeniuk@google.com>
2023-03-21 10:34:30 +0000

    [Password Manager] Promo cards handler
    This CLs adds basic mechanism for handling promo cards in the Password
    Manager UI. Information about promo cards is saved to the preferences.
    
    See more: https://docs.google.com/document/d/1MW-54YAiy1osHqWwZlX4QG3GusTCZsiAIZCjuNj963Y/edit#
    
    Bug: 1423346
    Change-Id: I1b4afc2fc94b6fe3298737abcd62cb8de1307b95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4324682
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1119846}

--- chrome/browser/ui/webui/password_manager/password_manager_ui.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/managed_ui_handler.h"
+#include "chrome/browser/ui/webui/password_manager/promo_card.h"
+#include "chrome/browser/ui/webui/password_manager/promo_cards_handler.h"
 #include "chrome/browser/ui/webui/password_manager/sync_handler.h"
 #include "chrome/browser/ui/webui/plural_string_handler.h"
 #include "chrome/browser/ui/webui/sanitized_image_source.h"
@@ -322,6 +324,10 @@ PasswordManagerUI::PasswordManagerUI(content::WebUI* web_ui)
                                                                         true);
   web_ui->AddMessageHandler(
       std::make_unique<password_manager::SyncHandler>(profile));
+  std::vector<std::unique_ptr<password_manager::PromoCardInterface>> cards;
+  web_ui->AddMessageHandler(
+      std::make_unique<password_manager::PromoCardsHandler>(profile,
+                                                            std::move(cards)));
   auto* source = CreateAndAddPasswordsUIHTMLSource(profile, web_ui);
   AddPluralStrings(web_ui);
   ManagedUIHandler::Initialize(web_ui, source);
