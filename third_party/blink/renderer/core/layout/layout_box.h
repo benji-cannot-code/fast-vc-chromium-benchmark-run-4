679b44e6c103993e56a95915a6aa73d5529aebb4
Stefan Zager <szager@chromium.org>
2023-03-21 15:05:01 +0000

    Reland: Cache hypothetical scrollbar thickness
    This reverts https://chromium-review.googlesource.com/c/chromium/src/+/4354473
    
    This updated patch avoids the performance pitfall of the original by
    only caching hypothetical scrollbar thickness when necessary.
    
    Bug: chromium:1425169
    Change-Id: I64e90a4c5f2b743fbd9d263ecc0f17c3956e628d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356233
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119932}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -2153,6 +2153,8 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   bool HasAnchorScrollTranslation() const;
   PhysicalOffset AnchorScrollTranslationOffset() const;
 
+  bool HasScrollbarGutters(ScrollbarOrientation orientation) const;
+
   // This should be called when the border-box size of this box is changed.
   void SizeChanged();
 
@@ -2376,7 +2378,6 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
            StyleRef().IsScrollbarGutterAuto();
   }
 
-  bool HasScrollbarGutters(ScrollbarOrientation orientation) const;
   NGPhysicalBoxStrut ComputeScrollbarsInternal(
       ShouldClampToContentBox = kDoNotClampToContentBox,
       OverlayScrollbarClipBehavior = kIgnoreOverlayScrollbarSize,
