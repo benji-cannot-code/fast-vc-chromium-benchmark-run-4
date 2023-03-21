07e5a9d49e1921bcf480c3d54180ba3cd063f54a
Thomas Lukaszewicz <tluk@chromium.org>
2023-03-21 16:24:39 +0000

    [color] Add the material omnibox color mixer
    This CL adds the material_omnibox_color_mixer to the pipeline
    behind the kChromeRefresh2023 feature flag.
    
    The material omnibox color mixer is intended to override colors
    defined in the existing chrome color mixer when applied.
    
    The current omnibox color mixer defines many leaf node color
    relationships in terms of reasonable input colors. E.g.
    
      mixer[kColorOmniboxSecurityChipDangerous] =
        security_chip_color(gfx::kGoogleRed300, gfx::kGoogleRed600);
    
    The intention is to preserve and continue to leverage these
    relationships, only overriding colors as needed by the new
    GM3 spec.
    
    Following a complete rollout these GM3 overrides can be merged
    back into the original chrome color mixer.
    
    This CL also performs some small cleanups to existing mixers
    (eliminating whitespace, comment formatting etc).
    
    Bug: 1408542
    Change-Id: Ibf0d5921e252d1d8b1a50b4f667859871d5e5327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356938
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119978}

--- chrome/browser/ui/color/material_tab_strip_color_mixer.h
+++ chrome/browser/ui/color/material_tab_strip_color_mixer.h
@@ -13,7 +13,7 @@ class ColorProvider;
 }
 
 // Adds a color mixer that contains recipes for tab strip colors to `provider`
-// with |key|.
+// with `key`.
 void AddMaterialTabStripColorMixer(ui::ColorProvider* provider,
                                    const ui::ColorProviderManager::Key& key);
 
