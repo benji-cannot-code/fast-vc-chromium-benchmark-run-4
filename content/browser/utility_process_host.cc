047a167f28190e8d1327c0aed75d34818aa3fb6a
Alison Gale <agale@chromium.org>
2023-03-21 18:01:28 +0000

    Revert "Remove kOverrideUseSoftwareGLForTests."
    This reverts commit 5a1e5c5f9adf03e59e66fa24720c51e01aec3696.
    
    Reason for revert: Caused tree closure due to build break: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Builder/49411/overview
    
    Original change's description:
    > Remove kOverrideUseSoftwareGLForTests.
    >
    > Instead, we should use --use-gl=angle --use-angle=swiftshader for
    > tests that want to use SwiftShader instead of real GPU, for example, tests run on bots without real GPUs, but still want to exercise gpu accelerated paths instead of software fallback paths.
    >
    > Bug:1426236
    > TEST=bots
    >
    > Change-Id: If6d7e88fb3bf626f56f8f8d75e9d5ad6b13f1768
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355711
    > Reviewed-by: Geoff Lang <geofflang@chromium.org>
    > Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1120020}
    
    Bug: 1426236
    Change-Id: I66d6b124a6de397acbae21b3e2885bfdbcf9a772
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357756
    Auto-Submit: Alison Gale <agale@chromium.org>
    Owners-Override: Alison Gale <agale@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120048}

--- content/browser/utility_process_host.cc
+++ content/browser/utility_process_host.cc
@@ -270,6 +270,7 @@ bool UtilityProcessHost::StartProcess() {
       switches::kForceUIDirection,
       switches::kIgnoreCertificateErrors,
       switches::kLoggingLevel,
+      switches::kOverrideUseSoftwareGLForTests,
       switches::kOverrideEnabledCdmInterfaceVersion,
       switches::kProxyServer,
       switches::kDisableAcceleratedMjpegDecode,
