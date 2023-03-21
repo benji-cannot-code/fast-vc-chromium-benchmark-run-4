2793c61334a225bab046628f3e33f639b5bb94b4
Antonio Gomes <tonikitoo@igalia.com>
2023-03-21 21:31:18 +0000

    Remove SkTypeface_Factory::FromFamilyNameAndFontStyle()
    After [1], the API became unused in Blink. This CL removes it.
    
    [1] https://crrev.com/c/4346973
    
    BUG=1425389
    R=dtapuska@chromium.org
    
    Change-Id: I0809163fe613b57d47523249dc70031efb304e7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356436
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120198}

--- third_party/blink/renderer/platform/fonts/skia/sktypeface_factory.h
+++ third_party/blink/renderer/platform/fonts/skia/sktypeface_factory.h
@@ -24,9 +24,6 @@ class SkTypeface_Factory {
                                                                 int ttc_index);
   static sk_sp<SkTypeface> FromFilenameAndTtcIndex(const std::string& filename,
                                                    int ttc_index);
-  static sk_sp<SkTypeface> FromFamilyNameAndFontStyle(
-      const std::string& family_name,
-      const SkFontStyle& font_style);
 };
 
 }  // namespace blink
