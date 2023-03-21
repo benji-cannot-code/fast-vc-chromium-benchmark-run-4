5f7fdcd928ad37cf2e773827947d91f2fed8cc1d
James Lee <ljjlee@google.com>
2023-03-21 16:27:22 +0000

    Rename URL filtering feature to FilterWebsitesForSupervisedUsersOnDesktopAndIOS
    Bug: b/270563369
    Change-Id: Ice552b43cd88f6c7f6b102eb98991339e4572948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4353199
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119979}

--- ios/chrome/browser/supervised_user/supervised_user_url_filter_egtest.mm
+++ ios/chrome/browser/supervised_user/supervised_user_url_filter_egtest.mm
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
   config.features_enabled.push_back(
-      supervised_user::kFilterWebsitesForSupervisedUsersOnThirdParty);
+      supervised_user::kFilterWebsitesForSupervisedUsersOnDesktopAndIOS);
   return config;
 }
 
