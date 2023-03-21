dea1c51a707044dd9608cc9ea3b2a6cdc7acd5e6
Joey Arhar <jarhar@chromium.org>
2023-03-21 17:13:18 +0000

    Close popover stack when popovertarget attribute changes
    If the popovertarget attribute is changed on an element in a way which
    breaks the connection keeping multiple popovers open, then this patch
    will close all open popovers. This was pointed out here:
    https://github.com/whatwg/html/pull/9048#discussion_r1140885404
    
    Bug: 1307772, 1408546
    Change-Id: I0a42260e97b7ef3fde01f416ef090b50994b8c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4354171
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120013}

--- third_party/blink/renderer/core/html/forms/html_form_control_element.cc
+++ third_party/blink/renderer/core/html/forms/html_form_control_element.cc
@@ -119,6 +119,8 @@ void HTMLFormControlElement::AttributeChanged(
     if (params.reason == AttributeModificationReason::kDirectly &&
         IsDisabledFormControl() && AdjustedFocusedElementInTreeScope() == this)
       blur();
+  } else if (params.name == html_names::kPopovertargetAttr) {
+    CheckAndPossiblyClosePopoverStack();
   }
 }
 
