b8868c2a662bc0106cd880655fb16a88bb642b47
Ioana Pandele <ioanap@chromium.org>
2023-03-21 14:25:57 +0000

    Move PasswordSpecificsMetadata writing out of CommitContribution
    This should be the responsibility of password-specific code.
    
    Bug: 1413209
    
    Change-Id: Ibcc61542ecf9f329254e8835459de29b482faf8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4341732
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119918}

--- components/sync/engine/commit_contribution_impl_unittest.cc
+++ components/sync/engine/commit_contribution_impl_unittest.cc
@@ -196,7 +196,6 @@ TEST(CommitContributionImplTest, PopulateCommitProtoBookmarkFolder) {
 // is properly encrypted except for password metadata.
 TEST(CommitContributionImplTest,
      PopulateCommitProtoPasswordWithoutCustomPassphrase) {
-  const std::string kMetadataUrl = "http://foo.com";
   const std::string kSignonRealm = "signon_realm";
   const int64_t kBaseVersion = 7;
   const int kDummyTimestamp = 123;
@@ -209,7 +208,13 @@ TEST(CommitContributionImplTest,
   password_data->set_date_last_used(kDummyTimestamp);
 
   data->specifics.mutable_password()->mutable_unencrypted_metadata()->set_url(
-      kMetadataUrl);
+      kSignonRealm);
+  data->specifics.mutable_password()
+      ->mutable_unencrypted_metadata()
+      ->set_blacklisted(false);
+  data->specifics.mutable_password()
+      ->mutable_unencrypted_metadata()
+      ->set_date_last_used_windows_epoch_micros(kDummyTimestamp);
 
   auto request_data = std::make_unique<CommitRequestData>();
   request_data->sequence_number = 2;
@@ -263,8 +268,8 @@ TEST(CommitContributionImplTest,
 // |unencrypted_metadata| should be cleared.
 TEST(CommitContributionImplTest,
      PopulateCommitProtoPasswordWithCustomPassphrase) {
-  const std::string kMetadataUrl = "http://foo.com";
   const std::string kSignonRealm = "signon_realm";
+  const int kDummyTimestamp = 123;
   const int64_t kBaseVersion = 7;
 
   auto data = std::make_unique<syncer::EntityData>();
@@ -274,7 +279,13 @@ TEST(CommitContributionImplTest,
   password_data->set_signon_realm(kSignonRealm);
 
   data->specifics.mutable_password()->mutable_unencrypted_metadata()->set_url(
-      kMetadataUrl);
+      kSignonRealm);
+  data->specifics.mutable_password()
+      ->mutable_unencrypted_metadata()
+      ->set_blacklisted(false);
+  data->specifics.mutable_password()
+      ->mutable_unencrypted_metadata()
+      ->set_date_last_used_windows_epoch_micros(kDummyTimestamp);
 
   auto request_data = std::make_unique<CommitRequestData>();
   request_data->sequence_number = 2;
