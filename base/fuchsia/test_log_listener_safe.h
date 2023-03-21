91246e564f19f74845e7f12d7b72d10e7ebe0923
Bryant Chandler <bryantchandler@chromium.org>
2023-03-21 18:05:59 +0000

    Reland "[fuchsia] Add basic implementations of fidl::AsyncEventHandler"
    This is a reland of commit f412da959daf896da16eb7a4efe166447f7bc6d1
    
    Reason for reland: The original used `DLOG`, so it failed to log
    the message that the tests are waiting for in CI because the
    `DCHECK` is turned off. On thinking about this again it's
    probably more appropriate to `LOG` anyway, so it's changed to
    that for the reland.
    
    The reland also adds `FidlErrorEventProcessExiter`, which
    covers another common case in which the response to an
    unexpected disconnect is to log and exit the process.
    
    Original change's description:
    > [fuchsia] Add basic implementations of fidl::AsyncEventHandler
    >
    > HLCPP bindings had a `set_error_handler` method on the `InterfacePtr`
    > but that's no longer available for `fidl::Client`. Instead, it takes
    > a `fidl::AsyncEventHandler` that has an `on_fidl_error` method as
    > well as methods for other events available on the `Protocol`. The new
    > API shape creates quite a bit of extra boilerplate for just adding a
    > closure or logging on fidl error (when a service is disconnected).
    >
    > This CL adds two basic implementations, one that just logs an error,
    > and another that takes a callback.
    >
    > Bug: 1351487
    > Change-Id: If6ba87f24751af8daafb54904652164eb47d9875
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4325064
    > Reviewed-by: Guocheng Wei <guochengwei@chromium.org>
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1118929}
    
    Bug: 1351487
    Change-Id: I16f360cbe42e13346af994725668644c9b5bedc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4352568
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120055}

--- base/fuchsia/test_log_listener_safe.h
+++ base/fuchsia/test_log_listener_safe.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fidl/fuchsia.logger/cpp/fidl.h>
 
+#include <lib/async/default.h>
 #include <lib/fidl/cpp/binding.h>
 #include <lib/zx/time.h>
+
 #include <memory>
 #include <string>
 #include <vector>
@@ -87,6 +89,9 @@ class SimpleTestLogListener {
   base::circular_deque<fuchsia_logger::LogMessage> logged_messages_;
   TestLogListenerSafe::OnLogMessageCallback on_log_message_;
 };
+
+// Configures `listener` to listen for messages from the current process.
+void ListenFilteredByCurrentProcessId(SimpleTestLogListener& listener);
 
 }  // namespace base
 
