b76a148d1d7167cad6dbef3e0f973d018da38b32
Xinghui Lu <xinghuilu@chromium.org>
2023-03-21 22:21:16 +0000

    [SBv5] Add in memory cache in OhttpKeyService.
    Introduce in memory cache so the key doesn't need to be fetched every
    time. The key expiration time is set to 30 days.
    
    In the future, this key will be preserved in prefs.
    
    Bug: 1407283
    Change-Id: If68144b5a4255d388e61da5829bbed7ab6417100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310492
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120230}

--- components/safe_browsing/core/browser/hashprefix_realtime/ohttp_key_service.h
+++ components/safe_browsing/core/browser/hashprefix_realtime/ohttp_key_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_list.h"
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -25,6 +26,13 @@ class OhttpKeyService : public KeyedService {
  public:
   using Callback =
       base::OnceCallback<void(absl::optional<std::string> ohttp_key)>;
+
+  struct OhttpKeyAndExpiration {
+    // The OHTTP key in this struct is formatted as described in
+    // https://www.ietf.org/archive/id/draft-ietf-ohai-ohttp-02.html#name-key-configuration-encoding
+    std::string key;
+    base::Time expiration;
+  };
 
   explicit OhttpKeyService(
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
@@ -47,6 +55,9 @@ class OhttpKeyService : public KeyedService {
   // Called before the actual deletion of the object.
   void Shutdown() override;
 
+  void set_ohttp_key_for_testing(OhttpKeyAndExpiration ohttp_key);
+  absl::optional<OhttpKeyAndExpiration> get_ohttp_key_for_testing();
+
  private:
   // Called when the response from the Safe Browsing key hosting endpoint is
   // received.
@@ -60,6 +71,9 @@ class OhttpKeyService : public KeyedService {
   // All callbacks that have requested an OHTTP key but haven't received a
   // response yet.
   base::OnceCallbackList<Callback::RunType> pending_callbacks_;
+
+  // The key cached in memory.
+  absl::optional<OhttpKeyAndExpiration> ohttp_key_;
 
   base::WeakPtrFactory<OhttpKeyService> weak_factory_{this};
 };
