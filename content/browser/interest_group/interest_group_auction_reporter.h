62ec5e0358e84b8f696062f9ed54bcd7f7ec4d70
Russ Hamilton <behamilton@google.com>
2023-03-21 17:22:14 +0000

    Allow limiting precision of bid and score for FLEDGE reporting
    This CL modifies FLEDGE reporting so that the floating-point precision
    of the bid and score passed to FLEDGE event-level reporting is limited.
    By default the floating point exponent is limited to 8 bits, but the
    mantissa is still allowed the full 53 bits. Eventually we will decrease
    the precision further, to further limit the ability exfiltrate
    identifying information from FLEDGE auctions.
    
    Change-Id: Ic477ad41cc9ce5e66a7ce6a946bc30f0b0cfab2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4329459
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120021}

--- content/browser/interest_group/interest_group_auction_reporter.h
+++ content/browser/interest_group/interest_group_auction_reporter.h
@@ -14,10 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
+#include "base/feature_list.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
 #include "content/browser/fenced_frame/fenced_frame_reporter.h"
 #include "content/browser/interest_group/auction_worklet_manager.h"
@@ -46,6 +48,14 @@ class AttributionManager;
 class AuctionWorkletManager;
 class InterestGroupManagerImpl;
 class PrivateAggregationManager;
+
+// Configures rounding on reported results from FLEDGE. This feature is intended
+// to be always enabled, but available to attach FeatureParams to so that we can
+// adjust the rounding setting via Finch.
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kFledgeRounding);
+CONTENT_EXPORT extern const base::FeatureParam<int> kFledgeBidReportingBits;
+CONTENT_EXPORT extern const base::FeatureParam<int> kFledgeScoreReportingBits;
+CONTENT_EXPORT extern const base::FeatureParam<int> kFledgeAdCostReportingBits;
 
 // Handles the reporting phase of FLEDGE auctions with a winner. Loads the
 // bidder, seller, and (if present) component seller worklets and invokes
