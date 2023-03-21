0179c873033cd949e7ce44eb16788cc45df26cb0
Rafał Godlewski <rgod@google.com>
2023-03-21 21:15:57 +0000

    [iOS] Modify too long note string to take character count as param
    Also replaces locally defined constant for max note length with the
    one defined in password_manager_constants.h
    
    Bug: 1414897
    Change-Id: If793c43f1bcd53acfb410ac1347b5f4b5c24d4dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356278
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120188}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/histogram_macros.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/password_manager/core/browser/password_manager_metrics_util.h"
+#import "components/password_manager/core/common/password_manager_constants.h"
 #import "components/password_manager/core/common/password_manager_features.h"
 #import "components/sync/base/features.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
@@ -53,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 using base::UmaHistogramEnumeration;
+using password_manager::constants::kMaxPasswordNoteLength;
 using password_manager::metrics_util::LogPasswordSettingsReauthResult;
 using password_manager::metrics_util::PasswordCheckInteraction;
 using password_manager::metrics_util::ReauthResult;
@@ -86,8 +88,6 @@ const CGFloat kSymbolSize = 15;
 const CGFloat kRecommendationSymbolSize = 22;
 // Minimal amount of characters in password note to display the warning.
 const int kMinNoteCharAmountForWarning = 901;
-// Maximal amount of characters that a password note can contain.
-const int kMaxNoteCharAmount = 1000;
 
 }  // namespace
 
@@ -436,8 +436,9 @@ const int kMaxNoteCharAmount = 1000;
     int password = IsPasswordGroupingEnabled() ? section : 0;
     NSString* footerText =
         self.passwordDetailsInfoItems[password].isNoteFooterShown
-            ? l10n_util::GetNSString(
-                  IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION)
+            ? l10n_util::GetNSStringF(
+                  IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION,
+                  base::NumberToString16(kMaxPasswordNoteLength))
             : @"";
     [footer setSubtitle:footerText];
   }
@@ -692,7 +693,7 @@ const int kMaxNoteCharAmount = 1000;
   [self setOrExtendAuthValidityTimer];
   // Update save button state based on the note's length and validity of other
   // input fields.
-  BOOL noteValid = tableViewItem.text.length <= kMaxNoteCharAmount;
+  BOOL noteValid = tableViewItem.text.length <= kMaxPasswordNoteLength;
   tableViewItem.validText = noteValid;
   self.shouldEnableEditDoneButton =
       noteValid && [self checkIfValidUsernames] && [self checkIfValidPasswords];
@@ -718,8 +719,9 @@ const int kMaxNoteCharAmount = 1000;
         base::mac::ObjCCastStrict<TableViewTextHeaderFooterView>(footer);
     NSString* footerText =
         shouldDisplayNoteFooter
-            ? l10n_util::GetNSString(
-                  IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION)
+            ? l10n_util::GetNSStringF(
+                  IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION,
+                  base::NumberToString16(kMaxPasswordNoteLength))
             : @"";
     [textFooter setSubtitle:footerText];
   }
@@ -957,7 +959,7 @@ const int kMaxNoteCharAmount = 1000;
 
   for (NSUInteger i = 0; i < self.passwordDetailsInfoItems.count; i++) {
     if (self.passwordDetailsInfoItems[i].passwordNoteItem.text.length >
-        kMaxNoteCharAmount) {
+        kMaxPasswordNoteLength) {
       return NO;
     }
   }
@@ -1094,8 +1096,9 @@ const int kMaxNoteCharAmount = 1000;
                 initWithType:PasswordDetailsItemTypeNoteFooter];
         footer.subtitle =
             passwordItem.isNoteFooterShown
-                ? l10n_util::GetNSString(
-                      IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION)
+                ? l10n_util::GetNSStringF(
+                      IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION,
+                      base::NumberToString16(kMaxPasswordNoteLength))
                 : @"";
         [model setFooter:footer forSectionWithIdentifier:sectionForPassword];
       }
--- ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/wait_util.h"
 #import "base/test/scoped_feature_list.h"
 #import "base/time/time.h"
+#import "components/password_manager/core/common/password_manager_constants.h"
 #import "components/password_manager/core/common/password_manager_features.h"
 #import "components/policy/policy_constants.h"
 #import "components/strings/grit/components_strings.h"
@@ -383,8 +384,10 @@ id<GREYMatcher> ToolbarSettingsSubmenuButton() {
 // Returns matcher for the password details / add password view footer displayed
 // when the note length is approaching max limit.
 id<GREYMatcher> TooLongNoteFooter() {
-  return grey_text(l10n_util::GetNSString(
-      IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION));
+  return grey_text(l10n_util::GetNSStringF(
+      IDS_IOS_SETTINGS_PASSWORDS_TOO_LONG_NOTE_DESCRIPTION,
+      base::NumberToString16(
+          password_manager::constants::kMaxPasswordNoteLength)));
 }
 
 // Saves an example form in the store.
