a1fae6d0f1dee2f13303cd61112271f59914ec66
Lauren Commeignes <laurencom@chromium.org>
2023-03-21 05:09:24 +0000

    Include translation strings for input keywords of System Info Card provider
    Bug: b/263994165
    Change-Id: Ie874498d500b4b585b6d89698f50347f43816ab6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328370
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119777}

--- chrome/browser/ash/app_list/search/system_info/system_info_card_provider.h
+++ chrome/browser/ash/app_list/search/system_info/system_info_card_provider.h
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <bitset>
 #include <memory>
+#include <vector>
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/app_list/search/search_provider.h"
 #include "chrome/browser/ash/app_list/search/system_info/battery_health.h"
 #include "chrome/browser/ash/app_list/search/system_info/cpu_data.h"
 #include "chrome/browser/ash/app_list/search/system_info/cpu_usage_data.h"
+#include "chrome/browser/ash/app_list/search/system_info/system_info_keyword_input.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/settings/ash/calculator/size_calculator.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
@@ -107,6 +109,7 @@ class SystemInfoCardProvider : public SearchProvider,
   std::unique_ptr<BatteryHealth> battery_health_{nullptr};
   gfx::ImageSkia os_settings_icon_;
   gfx::ImageSkia diagnostics_icon_;
+  std::vector<SystemInfoKeywordInput> keywords_;
 
   base::WeakPtrFactory<SystemInfoCardProvider> weak_factory_{this};
 };
