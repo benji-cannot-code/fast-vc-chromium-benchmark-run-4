295cceb829657daf2ba6af5d1e9e5b4371e0019e
Scott Haseley <shaseley@chromium.org>
2023-03-21 16:03:16 +0000

    Scheduling APIs: yield() in rIC inherits background priority
    This adds support for yield() inheritance when called during
    requestIdleCallback(). requestIdleCallback() will create its own
    TaskScope and propagate a fixed priority non-abortable signal, which
    will make it yield() at background priority if the "inherit" option
    is set for either priority or signal. This also adds a WPT test and
    corresponding virtual test suite entry, since requestIdleCallback
    requires threaded compositor support.
    
    Bug: 979020
    Low-Coverage-Reason: task attribution tracing is not tested (crbug.com/1421427)
    Change-Id: Ic9bac2a9513b258d26ec4b068173ee4c92d49b5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327064
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119958}

--- third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h
+++ third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h
@@ -39,6 +39,7 @@ class PLATFORM_EXPORT TaskAttributionTracker {
     kPostMessage,
     kPopState,
     kSchedulerPostTask,
+    kRequestIdleCallback,
   };
 
   // A class maintaining the scope of the current task. Keeping it alive ensures
