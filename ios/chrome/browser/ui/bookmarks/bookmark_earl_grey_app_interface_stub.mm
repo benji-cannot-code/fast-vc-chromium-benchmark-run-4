85b7da3ee76afc73e429978b3a3e4afdd550843e
Cameron Higgins <cameronhiggins@google.com>
2023-03-21 17:43:45 +0000

    Refactors dependencies on Earl Grey.
    Earl Grey has changed the file name of a file we widely import across the codebase. To prepare for that roll of EG2 ios/testing/earl_grey/earl_grey_test.h will be used as an umbrella header for the EG2 test support library within ios code. ios/testing/earl_grey/earl_grey_test.h will import the EG2 test's umbrella header.
    
    Dependencies on "//ios/third_party/earl_grey2:test_lib" are replaced with "//ios/testing/earl_grey:eg_test_support+eg2" if that dependency is not already present.
    
    Bug: 1420367
    Change-Id: I1fd8fbc49d4a2aeb36fe504a23701376662f28b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303486
    Reviewed-by: Lindsay Pasricha <lindsayw@chromium.org>
    Commit-Queue: Cameron Higgins <cameronhiggins@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120040}

--- ios/chrome/browser/ui/bookmarks/bookmark_earl_grey_app_interface_stub.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_earl_grey_app_interface_stub.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/bookmarks/bookmark_earl_grey_app_interface.h"
 
-#import <TestLib/EarlGreyImpl/EarlGrey.h>
+#import "ios/testing/earl_grey/earl_grey_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
--- ios/web_view/shell/test/earl_grey/web_view_shell_matchers.mm
+++ ios/web_view/shell/test/earl_grey/web_view_shell_matchers.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/shell/test/earl_grey/web_view_shell_matchers.h"
 
-#import <EarlGrey/EarlGrey.h>
+#import "ios/testing/earl_grey/earl_grey_test.h"
 
 #include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
