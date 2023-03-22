a9fd919b8a91308ed42bdb32f582ae097e5163cf
Pu Shi <pushi@google.com>
2023-03-22 18:21:25 +0000

    [Exo] Introduce EcheConnectionStatusHandler to PhoneHubManager
    Add methods in PhoneHubManager to set EcheConnectionStatusHandler and
    check eche connection status when Phone Hub connected.
    
    Bug: NONE
    Change-Id: Iec414cfb25ea5376504b19f10277ad512dab1ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4361762
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120678}

--- ash/system/phonehub/phone_hub_ui_controller_unittest.cc
+++ ash/system/phonehub/phone_hub_ui_controller_unittest.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/system/phonehub/phone_hub_ui_controller.h"
+#include <memory>
 
+#include "ash/constants/ash_features.h"
 #include "ash/shell.h"
 #include "ash/system/eche/eche_tray.h"
 #include "ash/system/phonehub/phone_hub_view_ids.h"
@@ -46,7 +48,8 @@ class PhoneHubUiControllerTest : public AshTestBase,
 
   // AshTestBase:
   void SetUp() override {
-    feature_list_.InitWithFeatures({features::kEcheSWA}, {});
+    feature_list_.InitWithFeatures(
+        {features::kEcheSWA, features::kEcheNetworkConnectionState}, {});
 
     AshTestBase::SetUp();
 
@@ -90,6 +93,11 @@ class PhoneHubUiControllerTest : public AshTestBase,
       const absl::optional<phonehub::PhoneStatusModel>& phone_status_model) {
     phone_hub_manager_.mutable_phone_model()->SetPhoneStatusModel(
         phone_status_model);
+  }
+
+  void SetEcheConnectionStatusHandler(
+      eche_app::EcheConnectionStatusHandler* handler) {
+    phone_hub_manager_.set_eche_connection_hander(handler);
   }
 
   std::unique_ptr<PhoneHubContentView> OpenBubbleAndCreateView() {
@@ -272,6 +280,21 @@ TEST_F(PhoneHubUiControllerTest, TetherConnectionPending) {
 TEST_F(PhoneHubUiControllerTest, PhoneConnected) {
   base::HistogramTester histograms;
   SetPhoneStatusModel(phonehub::CreateFakePhoneStatusModel());
+  GetFeatureStatusProvider()->SetStatus(FeatureStatus::kEnabledAndConnected);
+  EXPECT_EQ(PhoneHubUiController::UiState::kPhoneConnected,
+            controller_->ui_state());
+
+  auto content_view = OpenBubbleAndCreateView();
+  EXPECT_EQ(kPhoneConnectedView, content_view->GetID());
+  histograms.ExpectBucketCount(kScreenOnOpenedMetric,
+                               phone_hub_metrics::Screen::kPhoneConnected, 1);
+}
+
+TEST_F(PhoneHubUiControllerTest, PhoneConnected_HasConnectionHandler) {
+  base::HistogramTester histograms;
+  SetPhoneStatusModel(phonehub::CreateFakePhoneStatusModel());
+  SetEcheConnectionStatusHandler(
+      std::make_unique<eche_app::EcheConnectionStatusHandler>().get());
   GetFeatureStatusProvider()->SetStatus(FeatureStatus::kEnabledAndConnected);
   EXPECT_EQ(PhoneHubUiController::UiState::kPhoneConnected,
             controller_->ui_state());
