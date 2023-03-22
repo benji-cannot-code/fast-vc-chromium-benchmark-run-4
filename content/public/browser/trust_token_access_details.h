ff3a245001949e328ab939965607dd0fb5704a3f
Lei Zhang <thestig@chromium.org>
2023-03-22 18:33:07 +0000

    Remove unnecessary imports from trust_token_access_observer.mojom
    Avoid unnecessary mojom imports, which becomes unnecessary C++ includes
    in mojom-generated headers. Then fix missing includes in
    trust_token_access_details.*.
    
    Change-Id: Ie0e78912d1e94ded6ca9c239f9bfbb5f7a9a4a15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356323
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120688}

--- content/public/browser/trust_token_access_details.h
+++ content/public/browser/trust_token_access_details.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/content_export.h"
 #include "services/network/public/mojom/trust_token_access_observer.mojom.h"
+#include "services/network/public/mojom/trust_tokens.mojom-forward.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/origin.h"
 
 namespace content {
