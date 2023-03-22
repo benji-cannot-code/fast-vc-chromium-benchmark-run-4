3e7f6e02589a3837595854c3e578e1e0076a87f9
Aliona DANGLA <alionadangla@google.com>
2023-03-22 17:05:38 +0000

    [iOS] Avoid moving NTP in inactive browser's web state list
    This CL avoid moving NTP in inactive tab list. It also moves the helper
    outside the ui folder as it is not ui related.
    
    Fixed: 1426406
    Change-Id: Id83058ce4bede2c7a805c4202ff6e3be2cf2a619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355834
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120612}

--- ios/chrome/browser/tabs/inactive_tabs/utils.mm
+++ ios/chrome/browser/tabs/inactive_tabs/utils.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/histogram_functions.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/main/browser_util.h"
+#import "ios/chrome/browser/ntp/new_tab_page_util.h"
 #import "ios/chrome/browser/tabs/inactive_tabs/features.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_opener.h"
@@ -53,8 +54,10 @@ void MoveTabsFromActiveToInactive(Browser* active_browser,
 
   for (int index = active_web_state_list->GetIndexOfFirstNonPinnedWebState();
        index < active_web_state_list->count();) {
-    if (IsInactive(inactivity_threshold,
-                   active_web_state_list->GetWebStateAt(index))) {
+    web::WebState* current_web_state =
+        active_web_state_list->GetWebStateAt(index);
+    if (!IsVisibleURLNewTabPage(current_web_state) &&
+        IsInactive(inactivity_threshold, current_web_state)) {
       MoveTabFromBrowserToBrowser(active_browser, index, inactive_browser,
                                   inactive_browser->GetWebStateList()->count());
     } else {
--- ios/chrome/browser/ui/location_bar/location_bar_steady_view_mediator.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_steady_view_mediator.mm
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/strings/sys_string_conversions.h"
 #import "components/omnibox/browser/location_bar_model.h"
+#import "ios/chrome/browser/ntp/new_tab_page_util.h"
 #import "ios/chrome/browser/overlays/public/overlay_presenter.h"
 #import "ios/chrome/browser/overlays/public/overlay_presenter_observer_bridge.h"
 #import "ios/chrome/browser/overlays/public/overlay_request.h"
 #import "ios/chrome/browser/overlays/public/web_content_area/http_auth_overlay.h"
 #import "ios/chrome/browser/ui/location_bar/location_bar_steady_view_consumer.h"
-#import "ios/chrome/browser/ui/ntp/new_tab_page_util.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_util.h"
 #import "ios/chrome/browser/web_state_list/active_web_state_observation_forwarder.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer_bridge.h"
