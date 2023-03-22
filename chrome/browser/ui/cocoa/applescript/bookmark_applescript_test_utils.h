ee310ffa5361969cdf6591d733a65cdbd89d8365
Avi Drissman <avi@chromium.org>
2023-03-22 22:55:22 +0000

    Update the swizzling in AppleScript code to use the standard swizzler
    Bug: 1426255
    Include-Ci-Only-Tests: true
    Change-Id: I73ba917226fd379dc0259a5934a85f7705a87d5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4361920
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120850}

--- chrome/browser/ui/cocoa/applescript/bookmark_applescript_test_utils.h
+++ chrome/browser/ui/cocoa/applescript/bookmark_applescript_test_utils.h
@@ -7,17 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_COCOA_APPLESCRIPT_BOOKMARK_APPLESCRIPT_TEST_UTILS_H_
 
 #import <Foundation/Foundation.h>
-#import <objc/objc-runtime.h>
 
 #include "base/mac/scoped_nsobject.h"
 #import "chrome/browser/ui/cocoa/applescript/bookmark_folder_applescript.h"
 #include "chrome/test/base/in_process_browser_test.h"
 
 // Used to emulate an active running script, useful for testing purposes.
-@interface FakeScriptCommand : NSScriptCommand {
-  Method _originalMethod;
-  Method _alternateMethod;
-}
+@interface FakeScriptCommand : NSScriptCommand
 @end
 
 // The base class for all our bookmark related unit tests.
