c3a555493d64ae843b29e8b0ad54738fa0d995b6
dpapad <dpapad@chromium.org>
2023-03-22 18:17:26 +0000

    WebUI: Move CSS related WebUI tests to a dedicated folder and target.
    This is in preparation of adding new test cases for the upcoming
    WebUiSystemFont feature flag, which will require preprocessing
    `<if expr>` statements.
    
    Also removing unnecessary usages of |extraLibraries| in the moved tests.
    
    Bug: 1419512,1158471
    Change-Id: I23cdffa42a872fe23970909b663f593599798f59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356557
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120675}

--- chrome/test/data/webui/css/color_provider_css_colors_test_chromeos.ts
+++ chrome/test/data/webui/css/color_provider_css_colors_test_chromeos.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assertNotEquals} from './chai_assert.js';
+import {assertNotEquals} from 'chrome://webui-test/chai_assert.js';
 
 suite('ColorProviderCSSColorsTest', function() {
   let link: HTMLLinkElement;
--- chrome/test/data/webui/css/css_browsertest.js
+++ chrome/test/data/webui/css/css_browsertest.js
@@ -1,15 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 GEN('#include "build/chromeos_buildflags.h"');
 GEN('#include "content/public/test/browser_test.h"');
 
-var ColorProviderCSSColorsTest = class extends testing.Test {
+var CssTest = class extends testing.Test {
   /** @override */
   get browsePreload() {
-    return 'chrome://webui-test/test_loader.html?module=color_provider_css_colors_test.js';
+    throw new Error('Should be overriden by subclasses');
   }
 
   /** @override */
@@ -18,16 +18,26 @@ var ColorProviderCSSColorsTest = class extends testing.Test {
   }
 
   /** @override */
-  get extraLibraries() {
-    return [
-      '//third_party/mocha/mocha.js',
-      '//chrome/test/data/webui/mocha_adapter.js',
-    ];
-  }
-
-  /** @override */
   get webuiHost() {
     return 'dummyurl';
+  }
+};
+
+var TextDefaultsTest = class extends CssTest {
+  /** @override */
+  get browsePreload() {
+    return 'chrome://webui-test/test_loader.html?module=css/text_defaults_test.js';
+  }
+};
+
+TEST_F('TextDefaultsTest', 'All', function() {
+  mocha.run();
+});
+
+var ColorProviderCSSColorsTest = class extends CssTest {
+  /** @override */
+  get browsePreload() {
+    return 'chrome://webui-test/test_loader.html?module=css/color_provider_css_colors_test.js';
   }
 };
 
@@ -35,15 +45,15 @@ TEST_F('ColorProviderCSSColorsTest', 'All', function() {
   mocha.run();
 });
 
+GEN('#if BUILDFLAG(IS_CHROMEOS_ASH)');
+
 var ColorProviderCSSColorsTestChromeOS =
     class extends ColorProviderCSSColorsTest {
   /** @override */
   get browsePreload() {
-    return 'chrome://webui-test/test_loader.html?module=color_provider_css_colors_test_chromeos.js';
+    return 'chrome://webui-test/test_loader.html?module=css/color_provider_css_colors_test_chromeos.js';
   }
 };
-
-GEN('#if BUILDFLAG(IS_CHROMEOS_ASH)');
 
 TEST_F('ColorProviderCSSColorsTestChromeOS', 'All', function() {
   mocha.run();
