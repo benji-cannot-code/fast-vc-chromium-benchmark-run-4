315c94e7c2f4cf28d25d51675fb7eecaa773b911
Jiawei Shao <jiawei.shao@intel.com>
2023-03-22 11:05:50 +0000

    WebGPU: add [NoAllocDirectCall] on dispatchWorkgroups()
    This patch adds [NoAllocDirectCall] on dispatchWorkgroups() to make this
    API an V8 fast call.
    
    Bug: chromium:1417558
    Change-Id: I5417d4253a2c2508b8ea76e827cc31e0599aadbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355459
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1120453}

--- third_party/blink/renderer/modules/webgpu/gpu_compute_pass_encoder.h
+++ third_party/blink/renderer/modules/webgpu/gpu_compute_pass_encoder.h
@@ -9,13 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webgpu/dawn_object.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_programmable_pass_encoder.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/no_alloc_direct_call_host.h"
 
 namespace blink {
 
 class GPUBindGroup;
 
 class GPUComputePassEncoder : public DawnObject<WGPUComputePassEncoder>,
-                              public GPUProgrammablePassEncoder {
+                              public GPUProgrammablePassEncoder,
+                              public NoAllocDirectCallHost {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
