51ff7176389dbdd1faf1ac6f46e372e02e92762b
Fredrik Hernqvist <fhernqvist@google.com>
2023-03-22 22:04:52 +0000

    Add feature for decreasing ProcessingAudioFifo size
    If ChromeWideEchoCancellation is enabled, we now use the
    DecreaseProcessingAudioFifoSize experiment to set the size of the
    ProcessingAudioFifo. If the experiment is disabled, the size defaults
    to 110.
    
    This simultaneously removes the
    kChromeWideEchoCancellationProcessingFifoSize parameter from the
    ChromeWideEchoCancellation feature. This parameter is always set to
    110 today, so this CL does not change existing behaviour.
    
    Bug: 1420568
    Change-Id: Ic344cf30b04a5ca3ee011b387999fb5572185afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356940
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120815}

--- services/audio/processing_audio_fifo.h
+++ services/audio/processing_audio_fifo.h
@@ -75,6 +75,8 @@ class ProcessingAudioFifo {
   void AttachOnProcessedCallbackForTesting(
       base::RepeatingClosure on_processed_callback);
 
+  int fifo_size() const { return fifo_size_; }
+
  private:
   friend class ProcessingAudioFifoTest;
 
