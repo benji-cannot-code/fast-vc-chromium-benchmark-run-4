c659a494e1fe68415d24099fd8c11e63d3061152
Mitsuru Oshima <oshima@chromium.org>
2023-03-22 00:09:42 +0000

    EventProcessor redispatch refactor
    Add "GetNewEventSinkForEvent" for the case that event needs
    to be re-dispatched to different event sink.
    
    Bug: 1345952
    Test: no functional change. All test should still pass
    
    Change-Id: Iec36a20730b452e8bf0917057ed6581cc4f92e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3823599
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120287}

--- ui/events/event_targeter.h
+++ ui/events/event_targeter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 class Event;
+class EventSink;
 class EventTarget;
 
 class EVENTS_EXPORT EventTargeter {
@@ -36,6 +37,14 @@ class EVENTS_EXPORT EventTargeter {
   // coordinate space).
   virtual EventTarget* FindNextBestTarget(EventTarget* previous_target,
                                           Event* event) = 0;
+
+  // Returns new event sink if the `in_out_event` should be dispatched to a
+  // different sink. The event will be updated so that it can be dispatched to
+  // the new sink correctly. Returns `nullptr` if the event do not have to be
+  // redirected.
+  virtual EventSink* GetNewEventSinkForEvent(const EventTarget* current_root,
+                                             EventTarget* target,
+                                             Event* in_out_event);
 
  private:
   friend class EventProcessor;
