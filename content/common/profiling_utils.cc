2fcaa684285f5f9938ecced847720cdd0b268654
Sven Zheng <svenzheng@chromium.org>
2023-03-22 19:26:18 +0000

    Add llvm-profile-file switch
    This switch will be used on Lacros PGO builder to train Lacros
    PGO profiles.
    Lacros is similar to Android that it's running on a device that is not
    the host machine and environment variables aren't well supported.
    But Lacros also need to pass in the path so it is the same path as
    isolate test output folder on bots.
    
    Bug: 1417071
    Change-Id: If68743a70e94f108eb6f6ed23f3aa99d3c2aebf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4358910
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120714}

--- content/common/profiling_utils.cc
+++ content/common/profiling_utils.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
+#include "content/public/common/content_switches.h"
 
 namespace content {
 
@@ -41,7 +42,15 @@ base::FilePath GetProfileFileDirectory() {
 #if BUILDFLAG(IS_ANDROID)
   base::PathService::Get(base::DIR_TEMP, &path);
   path = path.Append("pgo_profiles/");
-#else  // !BUILDFLAG(IS_ANDROID)
+  // Lacros is similar to Android that it's running on a device that is not
+  // the host machine and environment variables aren't well supported.
+  // But Lacros also need to pass in the path so it is the same path as
+  // isolate test output folder on bots.
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+  path = base::CommandLine::ForCurrentProcess()
+             ->GetSwitchValuePath(switches::kLLVMProfileFile)
+             .DirName();
+#else
   std::string prof_template;
   std::unique_ptr<base::Environment> env(base::Environment::Create());
   if (env->GetVar("LLVM_PROFILE_FILE", &prof_template)) {
