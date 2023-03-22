8d3434204996e0bcad3f4330d0319711302c1d5c
Catherine Liu <catliu@chromium.org>
2023-03-22 04:29:45 +0000

    Fix batch update cleanup issue.
    Bug: 1426634
    Change-Id: I93ebdc54e8dbbb5a4111d5f06264c3db42324ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4359110
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Catherine Liu <catliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120368}

--- components/optimization_guide/core/hints_manager.h
+++ components/optimization_guide/core/hints_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/containers/lru_cache.h"
 #include "base/functional/callback_forward.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -198,6 +199,8 @@ class HintsManager : public OptimizationHintsComponentObserver,
  private:
   friend class ::OptimizationGuideTestAppInterfaceWrapper;
   friend class HintsManagerTest;
+
+  FRIEND_TEST_ALL_PREFIXES(HintsManagerFetchingTest, BatchUpdateFetcherCleanup);
 
   // Processes the optimization filters contained in the hints component.
   void ProcessOptimizationFilters(
