aa88fa73969971099f0c54730e6410d5892aea2a
Ben Becker <benbecker@chromium.org>
2023-03-22 23:08:24 +0000

    DeskButton: Add flag
    This CL adds a flag to enable showing a button that allows the user to
    navigate between desks. It also adds that flag to the UI.
    
    Bug: b:272382583
    Change-Id: I7f5ab837fa7be1b3fff67c62cff2aab92a1e2480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363599
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120864}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2917,6 +2917,9 @@ extern const char kCaptureModeDemoToolsDescription[];
 extern const char kCaptureModeGifRecordingName[];
 extern const char kCaptureModeGifRecordingDescription[];
 
+extern const char kDeskButtonName[];
+extern const char kDeskButtonDescription[];
+
 extern const char kDesks16Name[];
 extern const char kDesks16Description[];
 
