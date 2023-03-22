e853eb8e447bf95dd321ccc79c785d7c4dbf5472
Jonathan Lee <jonathanjlee@google.com>
2023-03-22 16:48:51 +0000

    [rebaseline-cl] Add `optimize-baselines --check` mode
    This change adds a flag to `optimize-baselines` to simply test for
    non-optimal baselines. The command exits with code 0 iff no improvements
    are necessary.
    
    Also, fix `optimize-baselines` logs being too verbose without
    `--verbose`. The message pool now inherits the level from the root
    logger instead of defaulting to `NOTSET`, which is below `DEBUG`.
    Important optimizer messages have been upgraded from `DEBUG` to `INFO`
    to compensate.
    
    Bug: 1421188
    Test: ./blink_tool.py optimize-baselines --all --check; echo $?
    Test: All baselines are optimal, exit with code 0
    Change-Id: I1b81930898802f4dd0469a42c6a83b8689fd17d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307673
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120598}

--- third_party/blink/tools/blinkpy/common/message_pool.py
+++ third_party/blink/tools/blinkpy/common/message_pool.py
@@ -157,7 +157,7 @@ class _MessagePool(object):
             worker.start()
 
     def _worker_log_level(self):
-        log_level = logging.NOTSET
+        log_level = logging.root.level
         for handler in logging.root.handlers:
             if handler.level != logging.NOTSET:
                 if log_level == logging.NOTSET:
