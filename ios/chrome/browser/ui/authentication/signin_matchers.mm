8f5c0ffec7b3bb30ac2d6ad8850cd096c937af45
kalettuce <kalettuce@chromium.org>
2023-03-22 17:59:53 +0000

    [iOS] Deprecate WebSigninContinueButtonMatcher().
    This method is deprecated with the addition of WebSigninPrimaryButtonMatcher(), added in crrev.com/c/4356079; the last references to the deprecated function are updated in crrev.com/i/5630787.
    
    Bug: 1426293
    Change-Id: I635ff2280197ad49ec242b62f3bf7bc038677aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355662
    Commit-Queue: Kale Lettuce <kalettuce@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120659}

--- ios/chrome/browser/ui/authentication/signin_matchers.mm
+++ ios/chrome/browser/ui/authentication/signin_matchers.mm
@@ -45,10 +45,6 @@ id<GREYMatcher> WebSigninPrimaryButtonMatcher() {
       grey_sufficientlyVisible(), nil);
 }
 
-id<GREYMatcher> WebSigninContinueButtonMatcher() {
-  return WebSigninPrimaryButtonMatcher();
-}
-
 id<GREYMatcher> GoogleSyncSettingsButton() {
   return grey_allOf(grey_kindOfClass([UITableViewCell class]),
                     grey_accessibilityID(kSettingsGoogleSyncAndServicesCellId),
