541a82288378b9b8fa6a51613becdfeb14ea22a0
Ritika Gupta <ritikagup@google.com>
2023-03-22 14:36:02 +0000

    Adding new segment for Device Tiers.
    Change-Id: I6e6d9272bd60496d5a9cb33b01ddab9470228f74
    Bug: 1426615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4358914
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120521}

--- components/segmentation_platform/public/constants.cc
+++ components/segmentation_platform/public/constants.cc
@@ -47,6 +47,8 @@ const char* SegmentationKeyToUmaName(const std::string& segmentation_key) {
     return kDeviceSwitcherUmaName;
   } else if (segmentation_key == kTabletProductivityUserKey) {
     return kTabletProductivityUserUmaName;
+  } else if (segmentation_key == kDeviceTierKey) {
+    return kDeviceTierUmaName;
   } else if (base::StartsWith(segmentation_key, "test_key")) {
     return "TestKey";
   }
@@ -105,6 +107,8 @@ std::string SegmentIdToHistogramVariant(proto::SegmentId segment_id) {
     case proto::SegmentId::
         OPTIMIZATION_TARGET_SEGMENTATION_TABLET_PRODUCTIVITY_USER:
       return "TabletProductivityUserSegment";
+    case proto::SegmentId::DEVICE_TIER_SEGMENT:
+      return "DeviceTierSegment";
     default:
       // This case is reached when UNKNOWN segment is valid, in case of boolean
       // segment results.
@@ -136,7 +140,9 @@ proto::Predictor::PredictorTypeCase GetClassifierType(
     return proto::Predictor::kBinaryClassifier;
   } else if (segmentation_key == kFeedUserSegmentationKey ||
              segmentation_key == kPowerUserKey ||
-             segmentation_key == kSearchUserKey) {
+             segmentation_key == kSearchUserKey ||
+             segmentation_key == kDeviceTierKey ||
+             segmentation_key == kTabletProductivityUserKey) {
     return proto::Predictor::kBinnedClassifier;
   }
   // This case is reached when UNKNOWN segment is valid, in case of boolean
