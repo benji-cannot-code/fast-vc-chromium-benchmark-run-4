490b416782b8be0f8dd7a9d24348439138f3610b
Lily Chen <chlily@chromium.org>
2023-03-22 02:58:53 +0000

    [DownloadBubble] Add profile-level cache of items
    This adds a class that caches the top 120 download items and the top
    120 offline items in sorted order, so that item models can be produced
    more quickly for UI updates (i.e. without fetching and sorting all items
    for every update). It listens to updates through observer interfaces for
    downloads and offline items, and updates the caches when necessary.
    When items are requested by the UI, it merges the two sorted caches to
    produce a list of the top 100 items, while potentially pruning
    any expired items. If items are removed (either through such pruning or
    through the user removing them), then it potentially backfills any
    remaining space in the cache by fetching all items.
    
    This is intended to be a KeyedService, such that there is one cache per
    profile, as opposed to the current logic implemented in
    DownloadBubbleUIController, which is one per browser window. It will
    be instantiated as a KeyedService in the next CL.
    
    This is also intended to replace the bulk of the logic currently in
    DownloadBubbleUIController. Instead of each window-level controller
    listening for updates independently, the profile-level service will
    listen for updates and pass along notifications to each window-level
    controller. (This will also be hooked up in the next CL.)
    
    Bug: 1421426
    Change-Id: Ie8ff16be4304ffd7648f6826d1c084bafb4ccd57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348220
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120348}

--- chrome/browser/download/bubble/download_bubble_ui_controller.cc
+++ chrome/browser/download/bubble/download_bubble_ui_controller.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/content_index/content_index_provider_impl.h"
 #include "chrome/browser/download/bubble/download_bubble_prefs.h"
-#include "chrome/browser/download/bubble/download_bubble_ui_model_utils.h"
+#include "chrome/browser/download/bubble/download_bubble_utils.h"
 #include "chrome/browser/download/bubble/download_display_controller.h"
 #include "chrome/browser/download/chrome_download_manager_delegate.h"
 #include "chrome/browser/download/download_core_service.h"
--- chrome/browser/download/bubble/download_display_controller.cc
+++ chrome/browser/download/bubble/download_display_controller.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/download/bubble/download_bubble_prefs.h"
 #include "chrome/browser/download/bubble/download_bubble_ui_controller.h"
-#include "chrome/browser/download/bubble/download_bubble_ui_model_utils.h"
+#include "chrome/browser/download/bubble/download_bubble_utils.h"
 #include "chrome/browser/download/bubble/download_display.h"
 #include "chrome/browser/download/bubble/download_icon_state.h"
 #include "chrome/browser/download/download_core_service.h"
