70e565de8d36712e66df84319b102b2413901166
Jack Yammine <jyammine@google.com>
2023-03-22 13:40:00 +0000

    [fyfre] Make enterprise welcome dialog view use tangible sync style
    Screenshot: https://crbug.com/1385793#c14
    
    Bug: 1385793
    Change-Id: I959f6a2899082dc00a135a76f54178ced70565ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334351
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120497}

--- chrome/browser/resources/signin/enterprise_profile_welcome/enterprise_profile_welcome_app.ts
+++ chrome/browser/resources/signin/enterprise_profile_welcome/enterprise_profile_welcome_app.ts
@@ -164,7 +164,7 @@ export class EnterpriseProfileWelcomeAppElement extends
 
   private setProfileInfo_(info: EnterpriseProfileInfo) {
     // <if expr="not chromeos_lacros">
-    if (this.isModalDialog_ || !this.isTangibleSyncStyleEnabled_) {
+    if (!this.isTangibleSyncStyleEnabled_) {
       this.style.setProperty('--header-background-color', info.backgroundColor);
     }
     // </if>
@@ -179,11 +179,12 @@ export class EnterpriseProfileWelcomeAppElement extends
     this.linkData_ = info.checkLinkDataCheckboxByDefault;
   }
 
-  // TODO: Enable tangible sync style for dialog view.
   private getTangibleSyncStyleClass_() {
-    return this.isTangibleSyncStyleEnabled_ && !this.isModalDialog_ ?
-        'tangible-sync-style' :
-        '';
+    if (!this.isTangibleSyncStyleEnabled_) {
+      return '';
+    }
+    return this.isModalDialog_ ? 'tangible-sync-style dialog' :
+                                 'tangible-sync-style';
   }
 }
 
