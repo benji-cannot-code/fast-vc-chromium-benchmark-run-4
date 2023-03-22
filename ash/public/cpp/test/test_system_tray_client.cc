d1d3e4bc7db48431eb18fa87adcd245792c270c6
Matthew Mourgos <mmourgos@chromium.org>
2023-03-22 23:34:31 +0000

    [EOL Incentive]: Add metrics for notification and quick settings
    Add two types of metrics for end of life incentive ui. First, add a
    metric to record the ui source whenever an end of life incentive is
    shown. This will also record the time period shown in relation to the
    end of life date (approaching, recently passed, passed). Second, add a
    metric to record specific button presses in each incentive. This also
    records the time period alongside the button type.
    
    Bug: b/271150076
    Change-Id: Ic6da8775cd03fa0bfd59078962b6089868069584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4359020
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120874}

--- ash/public/cpp/test/test_system_tray_client.cc
+++ ash/public/cpp/test/test_system_tray_client.cc
@@ -160,4 +160,6 @@ void TestSystemTrayClient::ShowEolInfoPage() {
   ++show_eol_info_count_;
 }
 
+void TestSystemTrayClient::RecordEolNoticeShown() {}
+
 }  // namespace ash
--- ash/system/update/eol_notice_quick_settings_view.cc
+++ ash/system/update/eol_notice_quick_settings_view.cc
@@ -89,6 +89,8 @@ EolNoticeQuickSettingsView::EolNoticeQuickSettingsView()
 
   SetInstallFocusRingOnFocus(true);
   views::FocusRing::Get(this)->SetColorId(ui::kColorAshFocusRing);
+
+  Shell::Get()->system_tray_model()->client()->RecordEolNoticeShown();
 }
 
 EolNoticeQuickSettingsView::~EolNoticeQuickSettingsView() = default;
