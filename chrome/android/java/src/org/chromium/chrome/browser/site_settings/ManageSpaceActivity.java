7ca746870f6e589df91732607ff92860b3c0994b
Wissem Gamra <wiss@chromium.org>
2023-03-22 17:24:11 +0000

    [Code Health] Remove expired Android ManageSpace histograms
    Bug: 1384637
    Change-Id: I2d778267eccf8484e5d71a7c5334dd8ffba73695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4346996
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Wissem Gamra <wiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120634}

--- chrome/android/java/src/org/chromium/chrome/browser/site_settings/ManageSpaceActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/site_settings/ManageSpaceActivity.java
@@ -14,7 +14,6 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.text.TextUtils;
 import android.text.format.Formatter;
 import android.view.View;
@@ -26,7 +25,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.chromium.base.Log;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.about_settings.AboutChromeSettings;
@@ -46,7 +44,6 @@ import org.chromium.components.browser_ui.site_settings.SiteSettingsCategory;
 import org.chromium.components.browser_ui.site_settings.Website;
 import org.chromium.components.browser_ui.site_settings.Website.StoredDataClearedCallback;
 import org.chromium.components.browser_ui.site_settings.WebsitePermissionsFetcher;
-import org.chromium.components.browser_ui.util.ConversionUtils;
 import org.chromium.components.version_info.VersionInfo;
 
 import java.util.Collection;
@@ -60,12 +57,6 @@ import java.util.Collection;
  */
 public class ManageSpaceActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "ManageSpaceActivity";
-
-    // Do not change these constants except for the MAX entry, they are used with UMA histograms.
-    private static final int OPTION_CLEAR_UNIMPORTANT = 0;
-    private static final int OPTION_MANAGE_STORAGE = 1;
-    private static final int OPTION_CLEAR_APP_DATA = 2;
-    private static final int OPTION_MAX = 3;
 
     private TextView mUnimportantSiteDataSizeText;
     private TextView mSiteDataSizeText;
@@ -216,8 +207,6 @@ public class ManageSpaceActivity extends AppCompatActivity implements View.OnCli
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                         mUnimportantDialog = null;
-                        RecordHistogram.recordEnumeratedHistogram("Android.ManageSpace.ActionTaken",
-                                OPTION_CLEAR_UNIMPORTANT, OPTION_MAX);
                         clearUnimportantData();
                     }
                 });
@@ -233,8 +222,6 @@ public class ManageSpaceActivity extends AppCompatActivity implements View.OnCli
                     SiteSettingsCategory.preferenceKey(SiteSettingsCategory.Type.USE_STORAGE));
             initialArguments.putString(SingleCategorySettings.EXTRA_TITLE,
                     getString(R.string.website_settings_storage));
-            RecordHistogram.recordEnumeratedHistogram(
-                    "Android.ManageSpace.ActionTaken", OPTION_MANAGE_STORAGE, OPTION_MAX);
             SettingsLauncher settingsLauncher = new SettingsLauncherImpl();
             settingsLauncher.launchSettingsActivity(this, AllSiteSettings.class, initialArguments);
         } else if (view == mClearAllDataButton) {
@@ -244,13 +231,6 @@ public class ManageSpaceActivity extends AppCompatActivity implements View.OnCli
             builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int id) {
-                    if (mIsNativeInitialized) {
-                        // This probably won't actually be uploaded, as android will probably kill
-                        // all processes & data before it gets sent to the network.
-                        RecordHistogram.recordEnumeratedHistogram("Android.ManageSpace.ActionTaken",
-                                OPTION_CLEAR_APP_DATA, OPTION_MAX);
-                    }
-
                     SearchActivityPreferencesManager.resetCachedValues();
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                         SiteChannelsManager.getInstance().deleteAllSiteChannels();
@@ -266,10 +246,6 @@ public class ManageSpaceActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void onSiteStorageSizeCalculated(long totalSize, long unimportantSize) {
-        RecordHistogram.recordCount1MHistogram("Android.ManageSpace.TotalDiskUsageMB",
-                (int) ConversionUtils.bytesToMegabytes(totalSize));
-        RecordHistogram.recordCount1MHistogram("Android.ManageSpace.UnimportantDiskUsageMB",
-                (int) ConversionUtils.bytesToMegabytes(unimportantSize));
         mSiteDataSizeText.setText(Formatter.formatFileSize(this, totalSize));
         mUnimportantSiteDataSizeText.setText(Formatter.formatFileSize(this, unimportantSize));
     }
@@ -297,7 +273,6 @@ public class ManageSpaceActivity extends AppCompatActivity implements View.OnCli
         // We keep track of the number of sites waiting to be cleared, and when it reaches 0 we can
         // set our testing variable.
         private int mNumSitesClearing;
-        private long mClearStartTime;
 
         /**
          * We fetch all the websites and clear all the non-important data. This happens
@@ -305,7 +280,6 @@ public class ManageSpaceActivity extends AppCompatActivity implements View.OnCli
          */
         public void clearData() {
             Profile profile = Profile.getLastUsedRegularProfile();
-            mClearStartTime = SystemClock.elapsedRealtime();
             WebsitePermissionsFetcher fetcher = new WebsitePermissionsFetcher(profile, true);
             fetcher.fetchPreferencesForCategory(SiteSettingsCategory.createFromType(profile,
                                                         SiteSettingsCategory.Type.USE_STORAGE),
@@ -316,8 +290,6 @@ public class ManageSpaceActivity extends AppCompatActivity implements View.OnCli
         public void onStoredDataCleared() {
             mNumSitesClearing--;
             if (mNumSitesClearing <= 0) {
-                RecordHistogram.recordTimesHistogram("Android.ManageSpace.ClearUnimportantTime",
-                        SystemClock.elapsedRealtime() - mClearStartTime);
                 clearUnimportantDataDone();
             }
         }
