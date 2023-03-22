f4e9b640fa67967fb67f0d7232d54a07902cfc2b
David Black <dmblack@google.com>
2023-03-22 22:03:12 +0000

    Create new item types and client API for camera app items.
    The camera app will soon start contributing: gifs, jpgs, mp4s, pdfs.
    
    Where these items will render in Tote UI is still being investigated by
    PM/UX. As such, this CL does *not* add these items to UI but does all
    other necessary wiring to unblock the camera app team.
    
    This CL exposes a new `AddItemOfType()` client API with the intention
    to remove all one-off client/service APIs in a follow up CL.
    
    Bug: b:274479699, b:274484210, b:274477308
    Change-Id: I97ed374b8af65df5c2d908974e010cb13a54331e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356152
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120813}

--- ash/public/cpp/holding_space/holding_space_item.cc
+++ ash/public/cpp/holding_space/holding_space_item.cc
@@ -116,6 +116,11 @@ bool HoldingSpaceItem::IsDownload(HoldingSpaceItem::Type type) {
     case Type::kDownload:
     case Type::kLacrosDownload:
       return true;
+    case Type::kCameraAppPhoto:
+    case Type::kCameraAppScanJpg:
+    case Type::kCameraAppScanPdf:
+    case Type::kCameraAppVideoGif:
+    case Type::kCameraAppVideoMp4:
     case Type::kDiagnosticsLog:
     case Type::kDriveSuggestion:
     case Type::kLocalSuggestion:
@@ -139,6 +144,11 @@ bool HoldingSpaceItem::IsScreenCapture(HoldingSpaceItem::Type type) {
     case Type::kScreenshot:
       return true;
     case Type::kArcDownload:
+    case Type::kCameraAppPhoto:
+    case Type::kCameraAppScanJpg:
+    case Type::kCameraAppScanPdf:
+    case Type::kCameraAppVideoGif:
+    case Type::kCameraAppVideoMp4:
     case Type::kDiagnosticsLog:
     case Type::kDownload:
     case Type::kDriveSuggestion:
@@ -160,6 +170,11 @@ bool HoldingSpaceItem::IsSuggestion(HoldingSpaceItem::Type type) {
     case Type::kLocalSuggestion:
       return true;
     case Type::kArcDownload:
+    case Type::kCameraAppPhoto:
+    case Type::kCameraAppScanJpg:
+    case Type::kCameraAppScanPdf:
+    case Type::kCameraAppVideoGif:
+    case Type::kCameraAppVideoMp4:
     case Type::kDiagnosticsLog:
     case Type::kDownload:
     case Type::kLacrosDownload:
