215099e34cf93320917b396de63a58da981d95cf
Amy Huang <akhuang@google.com>
2023-03-22 17:17:25 +0000

    Update create_unwind_table.py because of changed json names upstream.
    Make the script work with both the old and new JSON fields.
    
    Bug: 1426287
    Change-Id: I5f5510c24f9e338894bec254df27dd42709d124b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4358598
    Reviewed-by: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Amy Huang <akhuang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120622}

--- build/android/gyp/create_unwind_table.py
+++ build/android/gyp/create_unwind_table.py
@@ -1028,6 +1028,12 @@ def ReadTextSectionStartAddress(readobj_path: str, libchrome_path: str) -> int:
   Returns:
     The text section start address as a number.
   """
+  def GetSectionName(section) -> str:
+    # See crbug.com/1426287 for context on different JSON names.
+    if 'Name' in section['Section']['Name']:
+      return section['Section']['Name']['Name']
+    return section['Section']['Name']['Value']
+
   proc = subprocess.Popen(
       [readobj_path, '--sections', '--elf-output-style=JSON', libchrome_path],
       stdout=subprocess.PIPE,
@@ -1037,7 +1043,7 @@ def ReadTextSectionStartAddress(readobj_path: str, libchrome_path: str) -> int:
   sections = elfs['Sections']
 
   return next(s['Section']['Address'] for s in sections
-              if s['Section']['Name']['Value'] == '.text')
+              if GetSectionName(s) == '.text')
 
 
 def main():
