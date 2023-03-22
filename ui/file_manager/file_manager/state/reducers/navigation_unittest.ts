1f27a1dc0252bcc3bec291fd65d4d5fdddd6fc7b
Wenbo Jie <wenbojie@chromium.org>
2023-03-22 04:38:09 +0000

    [Files F2] Fix some bugs before adopting actions to container
    * Stop using rootType from VolumeEntry to check if an entry is
      MyFiles or not.
    * Update logic for isEjectable: set is to false if the
      removable devices are grouped.
    * Fix bugs in some reducers regarding not creating a copy when
      updating children field in the store.
    * Fix a bug for navigation logic: MyFiles entry's separtor should
      not always be true.
    
    Bug: b:228139957
    Test: browser_tests --gtest_filter="*DirectoryTreeContainer*"
    Change-Id: I8bce4fc800baade1ede8ac2b5ff415f4b1e71ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335655
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120370}

--- ui/file_manager/file_manager/state/reducers/navigation_unittest.ts
+++ ui/file_manager/file_manager/state/reducers/navigation_unittest.ts
@@ -570,7 +570,7 @@ export async function testMultipleUsbPartitionsGrouping(done: () => void) {
     {
       key: myFilesEntryListKey,
       section: NavigationSection.MY_FILES,
-      separator: true,
+      separator: false,
       type: NavigationType.ENTRY_LIST,
     },
     // parent entry for all removable partitions.
@@ -620,7 +620,7 @@ export async function testNavigationRootsWithFilteredVolume(done: () => void) {
     {
       key: myFilesEntryListKey,
       section: NavigationSection.MY_FILES,
-      separator: true,
+      separator: false,
       type: NavigationType.ENTRY_LIST,
     },
     // volume1.
