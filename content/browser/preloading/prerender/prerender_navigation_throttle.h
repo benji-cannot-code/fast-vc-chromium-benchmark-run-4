ca09d6ccf29b62d64c550e19394fb33939cd9e40
Hiroki Nakagawa <nhiroki@chromium.org>
2023-03-22 08:19:17 +0000

    Prerender: Simplify PrerenderNavigationThrottle
    This is a pure refactoring. This doesn't change functional behavior.
    
    - Makes PrerenderNavigationThrottle keep a raw_ptr to PrerenderHost.
      This should be safe as PrerenderHost is never changed through the
      lifetime of PrerenderNavigationThrottle and outlives the throttle.
    - Factors out code to cancel prerendering into CancelPrerendering().
    - Adds IsCrossSite() helper. Before this change,
      PrerenderNavigationThrottle negated IsSameSite() for checking
      cross-site navigation. This is correct, but IsCrossSite() should be
      more intuitive.
    
    Bug: 1422248
    Change-Id: Id20790c922fe35f8bb64e6fe67fd89921f8f6757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4353255
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120398}

--- content/browser/preloading/prerender/prerender_navigation_throttle.h
+++ content/browser/preloading/prerender/prerender_navigation_throttle.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+class NavigationRequest;
+class PrerenderHost;
+enum class PrerenderFinalStatus;
+
 // PrerenderNavigationThrottle applies restrictions to prerendering navigation
 // on the main frame. Specifically this cancels prerendering in the following
 // cases.
@@ -31,9 +35,18 @@ class PrerenderNavigationThrottle : public NavigationThrottle {
   ThrottleCheckResult WillProcessResponse() override;
 
  private:
-  explicit PrerenderNavigationThrottle(NavigationHandle* navigation_handle);
+  explicit PrerenderNavigationThrottle(NavigationRequest* navigation_request);
 
   ThrottleCheckResult WillStartOrRedirectRequest(bool is_redirection);
+
+  // Returns true if this throttle is for prerender initial navigation.
+  bool IsInitialNavigation() const;
+
+  // Cancels prerendering hosting this navigation with `final_status`.
+  void CancelPrerendering(PrerenderFinalStatus final_status);
+
+  // Raw ptr should be safe as `prerender_host_` indirectly owns `this`.
+  const raw_ptr<PrerenderHost> prerender_host_ = nullptr;
 
   bool is_same_site_cross_origin_prerender_ = false;
   bool same_site_cross_origin_prerender_did_redirect_ = false;
