e9319561023e18c75e18b8692e41f803752fef9c
Ted Meyer <tmathmeyer@chromium.org>
2023-03-22 17:23:26 +0000

    Add SetPlaybackRate as a method on Demuxer
    The HlsDemuxer will need to use this in the future to implement it's
    playback loop.
    
    Bug: 1266991
    Change-Id: Ie49b850aaa1df566289c17a949633469ad54e0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356075
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120633}

--- media/base/mock_filters.h
+++ media/base/mock_filters.h
@@ -207,6 +207,8 @@ class MockDemuxer : public Demuxer {
                base::TimeDelta,
                TrackChangeCB),
               (override));
+
+  MOCK_METHOD(void, SetPlaybackRate, (double), (override));
 };
 
 class MockDemuxerStream : public DemuxerStream {
