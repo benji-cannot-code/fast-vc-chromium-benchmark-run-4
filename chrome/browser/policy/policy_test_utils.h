4769ff8d1a23f19565318e99931646ceca200df1
Alexander Hendrich <hendrich@chromium.org>
2023-03-22 22:03:23 +0000

    Clean-up unused method declarations
    The methods are actually re-declared and used at https://crsrc.org/c/chrome/browser/policy/test/screenshot_policy_browsertest.cc;drc=e2f5b425274d71c8bba6b3d1bf85239528b65775;l=65-78. 
    
    This is probably some leftover from a clean-up.
    
    Bug: none
    Change-Id: Id9046c534ec691ba3262727c8b607c4bc3c58cbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4360160
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120814}

--- chrome/browser/policy/policy_test_utils.h
+++ chrome/browser/policy/policy_test_utils.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/files/file_path.h"
-#include "build/chromeos_buildflags.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/security_interstitials/core/controller_client.h"
@@ -41,17 +40,11 @@ class PolicyTest : public PlatformBrowserTest {
 
   void SetUpOnMainThread() override;
 
-  void SetScreenshotPolicy(bool enabled);
-
   void UpdateProviderPolicy(const PolicyMap& policy);
 
   static void SetPolicy(PolicyMap* policies,
                         const char* key,
                         absl::optional<base::Value> value);
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  void TestScreenshotFile(bool enabled);
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   static bool FetchSubresource(content::WebContents* web_contents,
                                const GURL& url);
