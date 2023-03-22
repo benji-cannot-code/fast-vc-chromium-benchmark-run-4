3e77b44c0f1a5a446f167c10317d895898814aa6
Caroline Rising <corising@chromium.org>
2023-03-22 19:37:49 +0000

    Remove CSC coordinator creation and entrypoints in incognito and guest mode.
    Bug: b:269663858
    Change-Id: I2eb0d9e439a7fae0bceb0d33d2b4cf0f52a3d86b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4359933
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120720}

--- chrome/browser/ui/views/side_panel/search_companion/search_companion_side_panel_coordinator.h
+++ chrome/browser/ui/views/side_panel/search_companion/search_companion_side_panel_coordinator.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
 
 class Browser;
+class Profile;
 
 namespace views {
 class View;
@@ -32,6 +33,8 @@ class SearchCompanionSidePanelCoordinator
   SearchCompanionSidePanelCoordinator& operator=(
       const SearchCompanionSidePanelCoordinator&) = delete;
   ~SearchCompanionSidePanelCoordinator() override;
+
+  static bool IsSupported(Profile* profile);
 
   void CreateAndRegisterEntriesForExistingWebContents(
       TabStripModel* tab_strip_model);
