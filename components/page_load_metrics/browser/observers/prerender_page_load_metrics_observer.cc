c81e5987877222c8e41965a34aa677f3683df979
Lingqi Chi <lingqi@chromium.org>
2023-03-22 04:06:18 +0000

    SearchPre: [4.1] Add metric to track the loading failures
    We're worried about whether the loader might be destroyed before
    serving full content to a activated prerendered page.
    This CL adds a simple metric to monitor it for now.
    If the loader is destroyed before the page drained the full response,
    we will see net::Error::FAILED raising up (compared to other SRP loads)
    
    Bug: 1400881
    Change-Id: I9136a8d1d79703762539fda0826cc71304822af7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4339558
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Quick-Run: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120362}

--- components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "services/metrics/public/cpp/metrics_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/network/public/mojom/fetch_api.mojom.h"
 
 namespace internal {
 
@@ -57,6 +58,10 @@ const char
 const char kHistogramPrerenderWorstUserInteractionLatencyMaxEventDuration[] =
     "PageLoad.InteractiveTiming.WorstUserInteractionLatency.MaxEventDuration."
     "Prerender";
+
+// Monitors serving preload cache to prerender.
+const char kPageLoadPrerenderActivatedPageLoaderStatus[] =
+    "PageLoad.Internal.Prerender2.ActivatedPageLoaderStatus";
 
 // This metric is used for debugging https://crbug.com/1379491.
 // Intentionally this metric doesn't record observer events per trigger type
@@ -269,6 +274,34 @@ void PrerenderPageLoadMetricsObserver::OnComplete(
   RecordSessionEndHistograms(timing, /*app_entering_background=*/false);
 }
 
+void PrerenderPageLoadMetricsObserver::OnLoadedResource(
+    const page_load_metrics::ExtraRequestCompleteInfo&
+        extra_request_complete_info) {
+  // Return early if this load is not for the main resource of the main frame of
+  // the prerendered page.
+  if (extra_request_complete_info.request_destination !=
+      network::mojom::RequestDestination::kDocument) {
+    return;
+  }
+
+  CHECK(!main_resource_load_status_);
+  main_resource_load_status_ =
+      static_cast<net::Error>(extra_request_complete_info.net_error);
+}
+
+void PrerenderPageLoadMetricsObserver::MaybeRecordMainResourceLoadStatus() {
+  // Only record UMA for the activated prerendered page that has started
+  // loading. If the trigger type is not set, the page is not activated since it
+  // is set at `DidActivatePrerenderedPage`.  If the main_resource_load_status_
+  // is not set, the page is not loaded.
+  if (!trigger_type_ || !main_resource_load_status_.has_value()) {
+    return;
+  }
+  base::UmaHistogramSparse(
+      AppendSuffix(internal::kPageLoadPrerenderActivatedPageLoaderStatus),
+      std::abs(*main_resource_load_status_));
+}
+
 page_load_metrics::PageLoadMetricsObserver::ObservePolicy
 PrerenderPageLoadMetricsObserver::FlushMetricsOnAppEnterBackground(
     const page_load_metrics::mojom::PageLoadTiming& timing) {
@@ -285,6 +318,7 @@ void PrerenderPageLoadMetricsObserver::RecordSessionEndHistograms(
   base::UmaHistogramEnumeration(
       internal::kPageLoadPrerenderObserverEvent,
       internal::PageLoadPrerenderObserverEvent::kRecordSessionEndHistograms);
+  MaybeRecordMainResourceLoadStatus();
 
   if (!GetDelegate().WasPrerenderedThenActivatedInForeground() ||
       !main_frame_timing.activation_start) {
