00a6061bc029bd86baad1c47b9aec0f6d317aee0
Xiaohan Wang <xhwang@chromium.org>
2023-03-22 18:36:05 +0000

    media: Add Media.EME.MediaFoundationService.HardwareContextReset UMA
    This CL adds a UMA on whether a HardwareContextReset event happened in
    the MediaFoundationService process within 5 seconds after a power or
    display change. Normally this event should only happen after a power or
    display change. This UMA will help check whether this assumption is true
    or not.
    
    Bug: 1370844
    Change-Id: Ic3a4c280ac8424a97bc6f9965b05504deaba1653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4354678
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120692}

--- chrome/browser/media/cdm_document_service_impl.cc
+++ chrome/browser/media/cdm_document_service_impl.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/system/sys_info.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -369,11 +370,15 @@ void CdmDocumentServiceImpl::OnCdmEvent(media::CdmEvent event,
   auto* monitor = MediaFoundationServiceMonitor::GetInstance();
 
   // Hardware context reset after power or display change is expected.
-  if (event == media::CdmEvent::kHardwareContextReset &&
-      monitor->HasRecentPowerOrDisplayChange()) {
-    DVLOG(2) << __func__
-             << ": HardwareContextReset ignored after power or display change";
-    return;
+  if (event == media::CdmEvent::kHardwareContextReset) {
+    bool has_change = monitor->HasRecentPowerOrDisplayChange();
+    base::UmaHistogramBoolean(
+        "Media.EME.MediaFoundationService.HardwareContextReset", has_change);
+    if (has_change) {
+      DVLOG(2) << __func__
+               << ": HardwareContextReset ignored after power/display change";
+      return;
+    }
   }
 
   // CdmDocumentServiceImpl is shared by all CDMs in the same RenderFrame.
