7ec9a3ddce6d2f094c608478ff267c6031941e45
Tommy C. Li <tommycli@chromium.org>
2023-03-22 20:58:27 +0000

    [page_image_service] Add a throttle to stop racing with consent helper
    NTP Quests module often requests images very shortly after startup.
    
    During this time, Sync is still initializing, so the Consent Helper is
    still in the initializing state.
    
    This CL adds a throttle to allow the image requests to be enqueued
    (with a timeout) until the consent helper is actually ready.
    
    Bug: b/244507194, 1367485, b/248367751
    Change-Id: I9d294398cdee29b54c27cf05fd4b363deefd5716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348716
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120784}

--- components/unified_consent/url_keyed_data_collection_consent_helper.cc
+++ components/unified_consent/url_keyed_data_collection_consent_helper.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
+#include "base/ranges/algorithm.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "components/sync/base/model_type.h"
@@ -37,7 +38,7 @@ class PrefBasedUrlKeyedDataCollectionConsentHelper
   ~PrefBasedUrlKeyedDataCollectionConsentHelper() override = default;
 
   // UrlKeyedDataCollectionConsentHelper:
-  bool IsEnabled() override;
+  State GetConsentState() override;
 
  private:
   void OnPrefChanged();
@@ -61,7 +62,7 @@ class SyncBasedUrlKeyedDataCollectionConsentHelper
   ~SyncBasedUrlKeyedDataCollectionConsentHelper() override;
 
   // UrlKeyedDataCollectionConsentHelper:
-  bool IsEnabled() override;
+  State GetConsentState() override;
 
   // syncer::SyncServiceObserver:
   void OnStateChanged(syncer::SyncService* sync) override;
@@ -85,9 +86,13 @@ PrefBasedUrlKeyedDataCollectionConsentHelper::
           base::Unretained(this)));
 }
 
-bool PrefBasedUrlKeyedDataCollectionConsentHelper::IsEnabled() {
+UrlKeyedDataCollectionConsentHelper::State
+PrefBasedUrlKeyedDataCollectionConsentHelper::GetConsentState() {
+  // There's no initializing state for pref-based helpers.
   return pref_service_->GetBoolean(
-      prefs::kUrlKeyedAnonymizedDataCollectionEnabled);
+             prefs::kUrlKeyedAnonymizedDataCollectionEnabled)
+             ? State::kEnabled
+             : State::kDisabled;
 }
 
 void PrefBasedUrlKeyedDataCollectionConsentHelper::OnPrefChanged() {
@@ -116,21 +121,37 @@ SyncBasedUrlKeyedDataCollectionConsentHelper::
     sync_service_->RemoveObserver(this);
 }
 
-bool SyncBasedUrlKeyedDataCollectionConsentHelper::IsEnabled() {
+UrlKeyedDataCollectionConsentHelper::State
+SyncBasedUrlKeyedDataCollectionConsentHelper::GetConsentState() {
+  // Any sync type that's NOT_ACTIVE makes the whole consent kDisabled.
   for (const auto& sync_data_type_states : sync_data_type_states_) {
-    if (sync_data_type_states.second != syncer::UploadState::ACTIVE)
-      return false;
+    if (sync_data_type_states.second == syncer::UploadState::NOT_ACTIVE) {
+      return State::kDisabled;
+    }
   }
-  return true;
+
+  // If no sync type is NOT_ACTIVE, any sync type still INITIALIZING makes the
+  // whole consent kInitializing.
+  for (const auto& sync_data_type_states : sync_data_type_states_) {
+    if (sync_data_type_states.second == syncer::UploadState::INITIALIZING) {
+      return State::kInitializing;
+    }
+  }
+
+  DCHECK(base::ranges::all_of(sync_data_type_states_, [](auto& state) {
+    return state.second == syncer::UploadState::ACTIVE;
+  })) << "Nothing is NOT_ACTIVE or INITIALIZING, so all must be ACTIVE.";
+  return State::kEnabled;
 }
 
 void SyncBasedUrlKeyedDataCollectionConsentHelper::OnStateChanged(
     syncer::SyncService* sync_service) {
   DCHECK_EQ(sync_service_, sync_service);
-  bool enabled_before_state_updated = IsEnabled();
+  auto old_state = GetConsentState();
   UpdateSyncDataTypeStates();
-  if (enabled_before_state_updated != IsEnabled())
+  if (old_state != GetConsentState()) {
     FireOnStateChanged();
+  }
 }
 
 void SyncBasedUrlKeyedDataCollectionConsentHelper::OnSyncShutdown(
@@ -179,6 +200,10 @@ UrlKeyedDataCollectionConsentHelper::
   return std::make_unique<SyncBasedUrlKeyedDataCollectionConsentHelper>(
       sync_service,
       std::set<syncer::ModelType>({syncer::ModelType::BOOKMARKS}));
+}
+
+bool UrlKeyedDataCollectionConsentHelper::IsEnabled() {
+  return GetConsentState() == State::kEnabled;
 }
 
 void UrlKeyedDataCollectionConsentHelper::AddObserver(Observer* observer) {
--- components/unified_consent/url_keyed_data_collection_consent_helper_unittest.cc
+++ components/unified_consent/url_keyed_data_collection_consent_helper_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "components/sync/driver/sync_service.h"
 #include "components/sync/engine/cycle/sync_cycle_snapshot.h"
 #include "components/sync/test/test_sync_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
@@ -67,11 +68,15 @@ TEST_F(UrlKeyedDataCollectionConsentHelperTest, AnonymizedDataCollection) {
       UrlKeyedDataCollectionConsentHelper::
           NewAnonymizedDataCollectionConsentHelper(&pref_service_);
   helper->AddObserver(this);
+  EXPECT_EQ(helper->GetConsentState(),
+            UrlKeyedDataCollectionConsentHelper::State::kDisabled);
   EXPECT_FALSE(helper->IsEnabled());
   EXPECT_TRUE(state_changed_notifications_.empty());
 
   pref_service_.SetBoolean(prefs::kUrlKeyedAnonymizedDataCollectionEnabled,
                            true);
+  EXPECT_EQ(helper->GetConsentState(),
+            UrlKeyedDataCollectionConsentHelper::State::kEnabled);
   EXPECT_TRUE(helper->IsEnabled());
   ASSERT_EQ(1U, state_changed_notifications_.size());
   EXPECT_TRUE(state_changed_notifications_[0]);
@@ -79,6 +84,8 @@ TEST_F(UrlKeyedDataCollectionConsentHelperTest, AnonymizedDataCollection) {
   state_changed_notifications_.clear();
   pref_service_.SetBoolean(prefs::kUrlKeyedAnonymizedDataCollectionEnabled,
                            false);
+  EXPECT_EQ(helper->GetConsentState(),
+            UrlKeyedDataCollectionConsentHelper::State::kDisabled);
   EXPECT_FALSE(helper->IsEnabled());
   ASSERT_EQ(1U, state_changed_notifications_.size());
   EXPECT_FALSE(state_changed_notifications_[0]);
@@ -90,17 +97,32 @@ TEST_F(UrlKeyedDataCollectionConsentHelperTest, PersonalizedDataCollection) {
       UrlKeyedDataCollectionConsentHelper::
           NewPersonalizedDataCollectionConsentHelper(&sync_service_);
   helper->AddObserver(this);
+  EXPECT_EQ(helper->GetConsentState(),
+            UrlKeyedDataCollectionConsentHelper::State::kDisabled);
   EXPECT_FALSE(helper->IsEnabled());
   EXPECT_TRUE(state_changed_notifications_.empty());
 
+  sync_service_.SetTransportState(
+      syncer::SyncService::TransportState::INITIALIZING);
   sync_service_.GetUserSettings()->SetSelectedTypes(
       /*sync_everything=*/false,
       /*types=*/syncer::UserSelectableTypeSet(
           syncer::UserSelectableType::kHistory));
 
   sync_service_.FireOnStateChangeOnAllObservers();
+  EXPECT_EQ(helper->GetConsentState(),
+            UrlKeyedDataCollectionConsentHelper::State::kInitializing);
+  EXPECT_FALSE(helper->IsEnabled());
+  EXPECT_EQ(1U, state_changed_notifications_.size())
+      << "No state change notifications fired, because it's still not enabled, "
+         "it's just initializing.";
+
+  sync_service_.SetTransportState(syncer::SyncService::TransportState::ACTIVE);
+  sync_service_.FireOnStateChangeOnAllObservers();
+  EXPECT_EQ(helper->GetConsentState(),
+            UrlKeyedDataCollectionConsentHelper::State::kEnabled);
   EXPECT_TRUE(helper->IsEnabled());
-  EXPECT_EQ(1U, state_changed_notifications_.size());
+  EXPECT_EQ(2U, state_changed_notifications_.size());
   helper->RemoveObserver(this);
 }
 
