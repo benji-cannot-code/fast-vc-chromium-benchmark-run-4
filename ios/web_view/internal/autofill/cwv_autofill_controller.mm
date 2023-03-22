d7d7a83e4cb1124e19c35081a6f7bc36773479a6
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-03-22 15:33:01 +0000

    Reland "[ios/b4p] Show one-time account storage notice"
    This is a reland of commit 91e90a325e074d642209515f5d218541851eba80
    The original CL was reverted because
    PasswordControllerEGTest/testShowAccountStorageNoticeBeforeSaving
    was failing. That test is checking strings that are only enabled when
    kIOSShowPasswordStorageInSaveInfobar is, so we need to add that to
    the app configuration. That's also why the saving test fails but
    the filling one doesn't.
    
    Original change's description:
    > [ios/b4p] Show one-time account storage notice
    >
    > Behavior changes are behind a flag disabled by default.
    > https://crbug.com/1392707#c20 shows the 4 user journeys implemented by
    > this CL.
    >
    > Bug: 1392707
    > Change-Id: I750660c41ed5fc891bb5fcd007f191a0027cd4a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349924
    > Commit-Queue: Victor Vianna <victorvianna@google.com>
    > Commit-Queue: John Wu <jzw@chromium.org>
    > Auto-Submit: Victor Vianna <victorvianna@google.com>
    > Quick-Run: Victor Vianna <victorvianna@google.com>
    > Reviewed-by: Maria Kazinova <kazinova@google.com>
    > Reviewed-by: John Wu <jzw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1120030}
    
    Bug: 1392707
    Change-Id: Ie87e633d7b22c5872f3b0a28a6c44e7fdc2f9f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4360939
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Owners-Override: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120550}

--- ios/web_view/internal/autofill/cwv_autofill_controller.mm
+++ ios/web_view/internal/autofill/cwv_autofill_controller.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/functional/callback.h"
 #import "base/mac/foundation_util.h"
+#import "base/notreached.h"
 #import "base/ranges/algorithm.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/values.h"
@@ -726,6 +727,14 @@ using UserDecision =
 - (void)sharedPasswordController:(SharedPasswordController*)controller
              didAcceptSuggestion:(FormSuggestion*)suggestion {
   // No op.
+}
+
+- (BOOL)shouldShowAccountStorageNotice {
+  return false;
+}
+
+- (void)showAccountStorageNotice:(void (^)())completion {
+  NOTREACHED_NORETURN();
 }
 
 @end
