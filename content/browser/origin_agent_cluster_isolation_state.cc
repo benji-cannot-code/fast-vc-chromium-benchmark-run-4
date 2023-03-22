767c17eb7ec270b8958bb49b1c4be286ecafbc7f
W. James MacLean <wjmaclean@chromium.org>
2023-03-22 16:34:21 +0000

    Move AreOriginAgentClustersEnabledByDefault() to SiteIsolationPolicy.
    Refactoring CL to centralize logic for deciding when to use
    Origin-Agent-Cluster by default.
    
    Bug: 1421329
    Change-Id: I1c52f36ab2c522107af686eb1afdcea647013ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355660
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120589}

--- content/browser/origin_agent_cluster_isolation_state.cc
+++ content/browser/origin_agent_cluster_isolation_state.cc
@@ -17,11 +17,7 @@ namespace content {
 OriginAgentClusterIsolationState
 OriginAgentClusterIsolationState::CreateForDefaultIsolation(
     BrowserContext* context) {
-  if (SiteIsolationPolicy::IsOriginAgentClusterEnabled() &&
-      base::FeatureList::IsEnabled(
-          blink::features::kOriginAgentClusterDefaultEnabled) &&
-      !GetContentClient()->browser()->ShouldDisableOriginAgentClusterDefault(
-          context)) {
+  if (SiteIsolationPolicy::AreOriginAgentClustersEnabledByDefault(context)) {
     return CreateForOriginAgentCluster(
         false /* requires_origin_keyed_process */);
   }
