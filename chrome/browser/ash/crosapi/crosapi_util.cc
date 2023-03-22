451305b7d7d30a9a4ca7312210e20542db5312e0
Igor <igorcov@chromium.org>
2023-03-22 08:19:27 +0000

    lacros: Fix the ash_metrics_managed flag passed to lacros
    The old code was looking if the pref is managed. That was leading
    to incorrect results. Replaced with the function that was written
    to determine that.
    
    BUG=crbug:1424708
    TEST=Manual test on device
    
    Change-Id: Ieac83b1d58db52fa0a2cb3eebe9a882ff95f0646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355022
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120399}

--- chrome/browser/ash/crosapi/crosapi_util.cc
+++ chrome/browser/ash/crosapi/crosapi_util.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/extensions/extension_keeplist_chromeos.h"
+#include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/speech/tts_crosapi_util.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
@@ -472,7 +473,7 @@ void InjectBrowserInitParams(
   params->ash_metrics_enabled =
       local_state->GetBoolean(metrics::prefs::kMetricsReportingEnabled);
   params->ash_metrics_managed =
-      local_state->IsManagedPreference(metrics::prefs::kMetricsReportingEnabled)
+      IsMetricsReportingPolicyManaged()
           ? mojom::MetricsReportingManaged::kManaged
           : mojom::MetricsReportingManaged::kNotManaged;
 
