267814738d402e41f7250611f7916d5c80a3e9a8
Matt Mueller <mattm@chromium.org>
2023-03-23 17:43:56 +0000

    Regenerate crlset test data files with sequence number 1 instead of 0
    This makes them eligible to be considered as an update to the
    CRLSet::BuiltinCRLSet(). Right now this isn't necessary since
    CRLSetDistributor gets created with a nullptr crl_set_, so even a
    sequence of 0 is allowed to update the CRLSet. Future changes will make
    that no longer the case.
    
    Bug: 1046728
    Change-Id: I4f69a8623dd1d81a8a794032a7da87b50ae974c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363106
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121261}

--- net/data/ssl/scripts/crlsetutil.py
+++ net/data/ssl/scripts/crlsetutil.py
@@ -21,7 +21,7 @@ The input is taken on stdin and is a dict with the following keys:
       with the key will be restricted to the set of SPKIs extracted from the
       files in the values.
   - Sequence: An optional integer sequence number to use for the CRLSet. If
-      not present, defaults to 0.
+      not present, defaults to 1.
 
 For example:
 
@@ -289,7 +289,7 @@ def main():
   header_json = {
       'Version': 0,
       'ContentType': 'CRLSet',
-      'Sequence': int(config.get("Sequence", 0)),
+      'Sequence': int(config.get("Sequence", 1)),
       'NumParents': len(parents),
       'BlockedSPKIs': blocked_spkis,
       'LimitedSubjects': limited_subjects,
