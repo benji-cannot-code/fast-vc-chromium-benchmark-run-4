bc5dc0995b33f3099605b1c6e4ec14abeee4659a
Marc Treib <treib@chromium.org>
2023-03-23 10:55:39 +0000

    PasswordModelTypeController: Remove on-startup cleanup
    Before this CL, PasswordModelTypeController contained some special
    logic to wipe the account store if the user isn't opted in to its
    usage anymore for some reason. This was to handle edge cases where the
    store didn't get properly cleared during the actual opt-out moment
    (e.g. due to a poorly-timed crash).
    However, this logic wasn't entirely safe: There could be race
    conditions with things happening on the sync or database threads,
    which could lead to data loss.
    
    Luckily, Sync itself now handles such cases better, and clears the
    data in a safe way, see crbug.com/897628. So this CL removes the
    special logic in PasswordModelTypeController.
    
    This does require a change to the corresponding integration test:
    Previously, the test manually added a credential to the account store,
    but did not add the corresponding Sync metadata (which should be
    impossible in practice). With that setup, Sync's cleanup logic won't
    trigger.
    So this CL updates the test to add both a credential and the
    corresponding Sync metadata.
    
    Bug: 1425033
    Change-Id: Iec954d11e0e85671c4ec120a45f10ea5beda2086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4353319
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121074}

--- components/password_manager/core/browser/sync/password_model_type_controller.h
+++ components/password_manager/core/browser/sync/password_model_type_controller.h
@@ -32,6 +32,7 @@ class PasswordModelTypeController : public syncer::ModelTypeController,
                                     public syncer::SyncServiceObserver,
                                     public signin::IdentityManager::Observer {
  public:
+  // TODO(crbug.com/1425033): Remove account_password_store_for_cleanup param.
   PasswordModelTypeController(
       std::unique_ptr<syncer::ModelTypeControllerDelegate>
           delegate_for_full_sync_mode,
@@ -69,9 +70,6 @@ class PasswordModelTypeController : public syncer::ModelTypeController,
 
  private:
   void OnOptInStateMaybeChanged();
-
-  void MaybeClearStore(
-      scoped_refptr<PasswordStoreInterface> account_password_store_for_cleanup);
 
   const raw_ptr<PrefService> pref_service_;
   const raw_ptr<signin::IdentityManager> identity_manager_;
