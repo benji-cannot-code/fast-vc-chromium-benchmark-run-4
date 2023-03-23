7ece893d74096cd42fa690532c1e00d164dea9dc
Ewann Pelle <ewannpv@chromium.org>
2023-03-23 17:50:58 +0000

    [iOS][PT] Invalidate pinned view layout when bounds change
    The pinned view layout should be invalidated when its bounds change in
    order to correctly center pinned tabs.
    
    Fixed: 1426205
    Change-Id: Id32e2a4e315485107913889acca39dee57a0bff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366121
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121268}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_layout.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_layout.mm
@@ -53,4 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       kPinnedCellVerticalLayoutInsets, availableHorizontalLayoutInsets};
 }
 
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+  return YES;
+}
+
 @end
