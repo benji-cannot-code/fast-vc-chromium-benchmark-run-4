356f0c3b6f9750395db6e28d520e47b12ce2e21c
Caroline Rising <corising@chromium.org>
2023-03-23 02:28:26 +0000

    Update pinned side panel toolbar button highlighting and add ability to unpin/repin.
    This cl:
    - Adds a profile pref for whether the csc side panel should be pinned,
      the default state is pinned.
    - Adds a context menu for the pinned button with the ability to unpin
    - Adds a pin button in the side panel header only for csc with the
      ability to unpin/repin
    - Updates highlighting so that CSC button is highlighted if CSC is
      open and the button is visible (otherwise the side panel button is
      highlighted)
    - Updated toolbar button interaction so clicking csc or side panel
      button while highlighted closes the side panel, and clicking a
      non-highlighted button behaves the same whether the side panel is
      open or closed.
    
    Follow-up work includes updating the pin icon and tooltip, updating the
    side panel button tooltip while CSC is open, and logging metrics.
    
    Bug: b:272116476, b:272116688
    Change-Id: Icde01e4247ac37e2d5f8f1f2053d30bd06538cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4354637
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120954}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/prefs/prefs_tab_helper.h"
 #include "chrome/browser/ui/search_engines/keyword_editor_controller.h"
 #include "chrome/browser/ui/send_tab_to_self/send_tab_to_self_bubble.h"
+#include "chrome/browser/ui/side_panel/side_panel_prefs.h"
 #include "chrome/browser/ui/tabs/pinned_tab_codec.h"
 #include "chrome/browser/ui/toolbar/chrome_labs_prefs.h"
 #include "chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.h"
@@ -1719,12 +1720,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   registry->RegisterBooleanPref(
       webauthn::pref_names::kRemoteProxiedRequestsAllowed, false);
 
-  // When in RTL mode, the side panel should default to the left of the screen.
-  // Otherwise, the side panel should default to the right side of the screen.
-  // TODO(dljames): Add enum values kAlternateSide / kDefaultSide that will
-  // replace false and true respectively.
-  registry->RegisterBooleanPref(prefs::kSidePanelHorizontalAlignment,
-                                base::i18n::IsRTL() ? false : true);
+  side_panel_prefs::RegisterProfilePrefs(registry);
 #endif
 
   registry->RegisterBooleanPref(webauthn::pref_names::kAllowWithBrokenCerts,
--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -143,6 +143,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/side_panel.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_registry.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_toolbar_container.h"
 #include "chrome/browser/ui/views/status_bubble_views.h"
 #include "chrome/browser/ui/views/sync/one_click_signin_dialog_view.h"
 #include "chrome/browser/ui/views/tab_contents/chrome_web_contents_view_focus_helper.h"
@@ -3806,7 +3807,12 @@ void BrowserView::AddedToWidget() {
   // the ToolbarView does not create a button for them. This specifically seems
   // to hit web apps. See https://crbug.com/1267781.
   if (toolbar_->GetSidePanelButton() && unified_side_panel_) {
-    unified_side_panel_->AddObserver(side_panel_coordinator_.get());
+    if (toolbar()->side_panel_container()) {
+      toolbar()->side_panel_container()->ObserveSidePanelView(
+          unified_side_panel_);
+    } else {
+      unified_side_panel_->AddObserver(side_panel_coordinator_.get());
+    }
   }
 
 #if BUILDFLAG(IS_CHROMEOS)
