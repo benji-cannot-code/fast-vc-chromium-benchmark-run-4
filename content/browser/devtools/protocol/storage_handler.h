d34e5169fb9588dd349051b10321584bd78ea298
Nathan Memmott <memmott@chromium.org>
2023-03-23 22:51:20 +0000

    Add Storage Buckets to Chrome Devtools Protocol
    This adds Storage Buckets to the Chrome Devtools Protocol and implements
    their backend.
    
    Bug: 1406017
    Change-Id: I5eaf6a28b81e815bc821364ea203a786e42a20ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4298203
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121438}

--- content/browser/devtools/protocol/storage_handler.h
+++ content/browser/devtools/protocol/storage_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/shared_storage/shared_storage_worklet_host_manager.h"
+#include "storage/browser/quota/quota_manager.h"
 
 namespace storage {
 class QuotaOverrideHandle;
@@ -130,12 +131,21 @@ class StorageHandler
       const std::string& owner_origin_string,
       std::unique_ptr<ResetSharedStorageBudgetCallback> callback) override;
 
+  DispatchResponse SetStorageBucketTracking(
+      const std::string& serialized_storage_key,
+      bool enable) override;
+
+  DispatchResponse DeleteStorageBucket(
+      const std::string& serialized_storage_key,
+      const std::string& bucket_name) override;
+
  private:
   // See definition for lifetime information.
   class CacheStorageObserver;
   class IndexedDBObserver;
   class InterestGroupObserver;
   class SharedStorageObserver;
+  class QuotaManagerObserver;
 
   // Not thread safe.
   CacheStorageObserver* GetCacheStorageObserver();
@@ -144,6 +154,7 @@ class StorageHandler
   SharedStorageWorkletHostManager* GetSharedStorageWorkletHostManager();
   absl::variant<protocol::Response, storage::SharedStorageManager*>
   GetSharedStorageManager();
+  storage::QuotaManagerProxy* GetQuotaManagerProxy();
 
   // content::InterestGroupManagerImpl::InterestGroupObserver
   void OnInterestGroupAccessed(
@@ -169,6 +180,8 @@ class StorageHandler
                                      const std::string& storage_key,
                                      const std::u16string& database_name,
                                      const std::u16string& object_store_name);
+  void NotifyCreateOrUpdateBucket(const storage::BucketInfo& bucket_info);
+  void NotifyDeleteBucket(const storage::BucketLocator& bucket_locator);
 
   Response FindStoragePartition(const Maybe<std::string>& browser_context_id,
                                 StoragePartition** storage_partition);
@@ -179,6 +192,7 @@ class StorageHandler
   std::unique_ptr<CacheStorageObserver> cache_storage_observer_;
   std::unique_ptr<IndexedDBObserver> indexed_db_observer_;
   std::unique_ptr<SharedStorageObserver> shared_storage_observer_;
+  std::unique_ptr<QuotaManagerObserver> quota_manager_observer_;
 
   // Exposes the API for managing storage quota overrides.
   std::unique_ptr<storage::QuotaOverrideHandle> quota_override_handle_;
