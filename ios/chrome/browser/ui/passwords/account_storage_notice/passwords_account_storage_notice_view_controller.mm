092cc82d1c2b783f38bc57217262e57afac48d90
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-03-23 14:24:35 +0000

    [ios/b4p] Allow one-time notice sheet to be extended to full height
    Behavior changes are behind a flag disabled by default.
    If the screen is small and the text doesn't fit, it's preferable to
    allow the user to extend the sheet to full height rather than have them
    scroll on a tiny scroll view. The half height is still used by default.
    
    Context thread
    https://docs.google.com/spreadsheets/d/1LaziSEs7vW-DzE_jkhC_64Ubpj60_9uuyqCzjUPqj1s/edit?resourcekey=0-ZodMffP__A5lsGNpsadAHQ&disco=AAAAnljesbM
    
    Bug: 1392707
    Change-Id: I235e7a773357203bdeea353fabd8afddb037612c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4365858
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121154}

--- ios/chrome/browser/ui/passwords/account_storage_notice/passwords_account_storage_notice_view_controller.mm
+++ ios/chrome/browser/ui/passwords/account_storage_notice/passwords_account_storage_notice_view_controller.mm
@@ -38,12 +38,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (@available(iOS 15, *)) {
     self.modalPresentationStyle = UIModalPresentationPageSheet;
     self.sheetPresentationController.preferredCornerRadius = 20;
-    // Both prefersEdgeAttachedInCompactHeight and mediumDetent serve to keep
-    // the sheet at half-height.
-    self.sheetPresentationController.prefersEdgeAttachedInCompactHeight = YES;
     self.sheetPresentationController.detents = @[
       UISheetPresentationControllerDetent.mediumDetent,
+      UISheetPresentationControllerDetent.largeDetent,
     ];
+    // prefersEdgeAttachedInCompactHeight just controls attaching to the bottom,
+    // not the sheet height.
+    self.sheetPresentationController.prefersEdgeAttachedInCompactHeight = YES;
   } else {
     self.modalPresentationStyle = UIModalPresentationFormSheet;
   }
