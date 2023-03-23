bf96f459a4617da9f8b07ffb663c621491130d88
Pu Shi <pushi@google.com>
2023-03-23 02:45:51 +0000

    Reland "[Exo] Introduce EcheConnectionStatusHandler to PhoneHubManager"
    This reverts commit 15d704e3290da9dc947e8facdc342319cc18975e.
    
    Reason for revert: Error fixed.
    
    Original change's description:
    > Revert "[Exo] Introduce EcheConnectionStatusHandler to PhoneHubManager"
    >
    > This reverts commit 4b01251e9feff2a84ce707e917330465519bfacc.
    >
    > Reason for revert: Causing test failures on bots i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/34664/overview
    >
    > Original change's description:
    > > [Exo] Introduce EcheConnectionStatusHandler to PhoneHubManager
    > >
    > > Add methods in PhoneHubManager to set EcheConnectionStatusHandler and
    > > check eche connection status when Phone Hub connected.
    > >
    > > Bug: NONE
    > > Change-Id: Iec414cfb25ea5376504b19f10277ad512dab1ee1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4361762
    > > Reviewed-by: Crisrael Lucero <crisrael@google.com>
    > > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > > Commit-Queue: Pu Shi <pushi@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1120678}
    >
    > Bug: NONE
    > Change-Id: I9d6c7d19da33d95dba8f7b970f35f0dd98bc2a53
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4364178
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    > Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    > Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1120914}
    
    Bug: NONE
    Change-Id: Ia470a90a2a8d5811edeacb0e7b279c89630e11b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363115
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Commit-Queue: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120962}

--- chromeos/ash/components/phonehub/phone_hub_manager.h
+++ chromeos/ash/components/phonehub/phone_hub_manager.h
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace ash {
+
+namespace eche_app {
+class EcheConnectionStatusHandler;
+}
+
 namespace phonehub {
 
 class BrowserTabsModelProvider;
@@ -65,6 +70,11 @@ class PhoneHubManager {
   virtual UserActionRecorder* GetUserActionRecorder() = 0;
   virtual IconDecoder* GetIconDecoder() = 0;
   virtual AppStreamManager* GetAppStreamManager() = 0;
+  virtual eche_app::EcheConnectionStatusHandler*
+  GetEcheConnectionStatusHandler() = 0;
+  virtual void SetEcheConnectionStatusHandler(
+      eche_app::EcheConnectionStatusHandler*
+          eche_connection_status_handler) = 0;
 
   // Retrieves the timestamp of the last successful discovery for active host,
   // or nullopt if it hasn't been seen in the current Chrome session.
