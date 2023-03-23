cde73c9a73a9df7dd3205a73e100c38600ac1184
Xianzhu Wang <wangxianzhu@chromium.org>
2023-03-23 19:36:48 +0000

    Fix UnifiedScrollingDeathTest.EmptyMainThreadHitTest on non-DCHECK build
    I missed the change in the non-DCHECK branch in crrev.com/c/4363246.
    
    Bug: 1414955
    Change-Id: Ibbbf16d6ff359bd4a468ef45f2afb89499132ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366497
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121327}

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -18066,7 +18066,7 @@ TEST_F(UnifiedScrollingDeathTest, EmptyMainThreadHitTest) {
 #else
     status = ContinuedScrollBegin(kInvalidId);
     EXPECT_EQ(ScrollThread::SCROLL_IGNORED, status.thread);
-    EXPECT_EQ(MainThreadScrollingReason::kNoScrollingLayer,
+    EXPECT_EQ(MainThreadScrollingReason::kNotScrollingOnMain,
               status.main_thread_scrolling_reasons);
 #endif
   }
