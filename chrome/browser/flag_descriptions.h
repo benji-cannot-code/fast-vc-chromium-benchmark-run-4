816b81989f57bdbac6f3727f1128e82f1a7e1b79
Colin Kincaid <ckincaid@chromium.org>
2023-03-23 01:23:23 +0000

    multipaste: Expose flag for experimental Ctrl+V behavior
    Allows users enable/disable experimental behavior wherein long-pressing
    Ctrl+V gives users the option to replace pasted content with a different
    item from their recent clipboard history. This flag will be available on
    Dev/Canary/Beta/Unknown channels via chrome://flags.
    
    Fixed: b/274799719
    Change-Id: I1e8bb06c996d1e723705ddb6e758edb19611bd76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363699
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120937}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2947,6 +2947,9 @@ extern const char kCellularUseSecondEuiccDescription[];
 extern const char kClipboardHistoryReorderName[];
 extern const char kClipboardHistoryReorderDescription[];
 
+extern const char kClipboardHistoryLongpressName[];
+extern const char kClipboardHistoryLongpressDescription[];
+
 extern const char kComponentUpdaterTestRequestName[];
 extern const char kComponentUpdaterTestRequestDescription[];
 
