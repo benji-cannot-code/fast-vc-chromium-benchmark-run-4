8e7a10b393c0a88a68ffd947dff4279d794e2877
Nan Lin <linnan@chromium.org>
2023-03-23 01:26:06 +0000

    Add sliced pageload guardrail metrics for Privacy Sandbox Ads APIs
    The CL records pageload guardrail metrics when Privacy Sandbox Ads API
    web features are used, to monitor possible regressions.
    
    Bug: 1424384
    Change-Id: Idc3c439b0c2807f51f28a368d42e16a27484abd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335822
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120938}

--- components/page_load_metrics/browser/page_load_metrics_embedder_base.cc
+++ components/page_load_metrics/browser/page_load_metrics_embedder_base.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_load_metrics/browser/observers/early_hints_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/fenced_frames_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.h"
+#include "components/page_load_metrics/browser/observers/privacy_sandbox_ads_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/same_origin_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/shared_storage_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.h"
@@ -46,6 +47,8 @@ void PageLoadMetricsEmbedderBase::RegisterObservers(PageLoadTracker* tracker) {
       tracker->AddObserver(
           std::make_unique<SharedStoragePageLoadMetricsObserver>());
     }
+    tracker->AddObserver(
+        std::make_unique<PrivacySandboxAdsPageLoadMetricsObserver>());
   }
   // Allow the embedder to register any embedder-specific observers
   RegisterEmbedderObservers(tracker);
