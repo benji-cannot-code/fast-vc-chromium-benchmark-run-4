31ee13d3b0ac4c1d85716395b0448a689dcb46db
Alan Screen <awscreen@chromium.org>
2023-03-23 17:40:56 +0000

    Use client ID for printing and system print dialog
    To ensure that system print dialogs and document printing use the same
    Print Backend service, a printer name cannot be used because the name
    isn't known prior to the system dialog.  Introduce the use of a client
    ID to identify the service which will be used.  This enables it to be
    shared for system printing.
    
    Bug: 1414968
    Change-Id: I94d414cfaf612f22300d467fa5a6957a9e67c300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4256406
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121259}

--- chrome/browser/printing/printer_query.h
+++ chrome/browser/printing/printer_query.h
@@ -12,9 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "content/public/browser/global_routing_id.h"
+#include "printing/buildflags/buildflags.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/print_settings.h"
 #include "printing/printing_context.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+#if BUILDFLAG(ENABLE_OOP_PRINTING)
+#include "chrome/browser/printing/print_backend_service_manager.h"
+#endif
 
 namespace content {
 class WebContents;
@@ -81,6 +87,14 @@ class PrinterQuery {
   // Caller has to ensure that `this` is alive until `callback` is run.
   void SetSettingsFromPOD(std::unique_ptr<PrintSettings> new_settings,
                           base::OnceClosure callback);
+#endif
+
+#if BUILDFLAG(ENABLE_OOP_PRINTING)
+  // Provide the client ID when the caller has registered with the
+  // `PrintBackendServiceManager` for getting settings for system print.
+  // Only intended to be used when out-of-process printing is in use, will
+  // DCHECK if used for in-browser printing.
+  virtual void SetClientId(PrintBackendServiceManager::ClientId client_id);
 #endif
 
   int cookie() const;
