72109dc3a80bcc16080fc751282a9fdec155ff0d
DanielRyanSmith <danielrsmith@google.com>
2023-03-23 23:26:49 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 1c7c03b58d24fa64c4cd3240b8ce5bc6c4f62eb6
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I5c5e7581928922860e34e4d469fad3b2eff7f1cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367308
    Auto-Submit: Daniel Smith <danielrsmith@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Daniel Smith <danielrsmith@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121452}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/base.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/base.py
@@ -645,6 +645,7 @@ class WdspecExecutor(TestExecutor):
         session_config = {"host": self.browser.host,
                           "port": self.browser.port,
                           "capabilities": self.capabilities,
+                          "timeout_multiplier": self.timeout_multiplier,
                           "webdriver": {
                               "binary": self.webdriver_binary,
                               "args": self.webdriver_args
