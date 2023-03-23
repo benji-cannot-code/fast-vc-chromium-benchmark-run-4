a0e37fdb44caf104cb122ce60d262c20396c983a
Sean Kau <skau@chromium.org>
2023-03-23 21:53:34 +0000

    Connect ColorPaletteController to Wallpaper and Dark/Light Controllers
    Hooks up the controller to the appropriate data sources so that we
    can send appropriate change events and eventually take over NativeTheme
    updates from DarkLightModeController.
    
    Change-Id: If7ae29927853105192ec5f3c1f54eac421489163
    Bug: b:258719005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356311
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121411}

--- ash/style/color_palette_controller.h
+++ ash/style/color_palette_controller.h
@@ -19,6 +19,9 @@ class PrefRegistrySimple;
 
 namespace ash {
 
+class DarkLightModeController;
+class WallpaperControllerImpl;
+
 // Types of ColorSchemes. For a given seed color, each ColorScheme will generate
 // a different color palette/set of ref colors.
 enum class ASH_EXPORT ColorScheme {
@@ -70,7 +73,12 @@ class ASH_EXPORT ColorPaletteController {
     virtual void OnColorPaletteChanging(const ColorPaletteSeed& seed) = 0;
   };
 
-  static ASH_EXPORT std::unique_ptr<ColorPaletteController> Create();
+  // Temporary factory for migration.  DO NOT USE.
+  static std::unique_ptr<ColorPaletteController> Create();
+
+  static std::unique_ptr<ColorPaletteController> Create(
+      DarkLightModeController* dark_light_mode_controller,
+      WallpaperControllerImpl* wallpaper_controller);
 
   ColorPaletteController() = default;
 
