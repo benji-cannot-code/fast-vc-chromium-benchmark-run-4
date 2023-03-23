c0cdd9835792604bfd6e89c6731df465ed440cc0
Khushal Sagar <khushalsagar@chromium.org>
2023-03-23 13:22:51 +0000

    view-transitions: Clip snapshots to max texture size/viewport.
    If an element's painting exceeds viewport bounds, clip it down to max
    texture size or twice the viewport bounds by painting the subset which
    is closest to the snapshot root.
    
    This involves the following 2 changes:
    
    1) A new ClipNode is introduced to paint the desired subrect when
    rendering this element in paint, raster and compositing. All these
    stages should render only this subset.
    
    2) The object-view-box on the replaced element is still set to the
    ink overflow rect. So at paint time, we adjust the replaced content
    rect to the actual subset that will be painted based on the clipping
    above.
    
    R=pdr@chromium.org, vmpstr@chromium.org, bokan@chromium.org
    
    Bug: 1409713
    Change-Id: If81bc67ad6ffe49d68e4470a6717b704edafcd3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4330193
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121126}

--- third_party/blink/renderer/core/layout/layout_view_transition_content.h
+++ third_party/blink/renderer/core/layout/layout_view_transition_content.h
@@ -24,7 +24,8 @@ class CORE_EXPORT LayoutViewTransitionContent : public LayoutReplaced {
     NOT_DESTROYED();
     return "LayoutViewTransitionContent";
   }
-  void OnIntrinsicSizeUpdated(const LayoutSize& intrinsic_size);
+  void OnIntrinsicSizeUpdated(const gfx::RectF& ink_overflow_rect,
+                              const gfx::RectF& captured_subrect);
 
   bool IsViewTransitionContent() const override {
     NOT_DESTROYED();
@@ -38,7 +39,11 @@ class CORE_EXPORT LayoutViewTransitionContent : public LayoutReplaced {
                      const PhysicalOffset& paint_offset) const override;
 
  private:
+  PhysicalRect ReplacedContentRectForCapturedContent() const;
+
   scoped_refptr<cc::ViewTransitionContentLayer> layer_;
+  gfx::RectF ink_overflow_rect_;
+  gfx::RectF captured_subrect_;
 };
 
 template <>
