04b60fedc3909537df65e738b10b6f756e4aa4a3
Sneha Nagpaul <snehanagpaul@google.com>
2023-03-23 19:48:28 +0000

    Revert "Don't set/record main-thread scrolling reasons for ignored scrolls"
    This reverts commit 67b8ff6f4f67e6754ea43312a96ea884fd241b18.
    
    Reason for revert: UnifiedScrollingDeathTest.EmptyMainThreadHitTest fails across numerous bots eg. ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests/128105
    
    Original change's description:
    > Don't set/record main-thread scrolling reasons for ignored scrolls
    >
    > Bug: 1082601, 1155663, 1414955
    > Change-Id: I30d1517937aaef5d46a8acdd6364175086e8dc7c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363246
    > Reviewed-by: Steve Kobes <skobes@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1121244}
    
    Bug: 1082601, 1155663, 1414955
    Change-Id: I489d96070ed0fd6d2990a25bd85d93b2e766dee1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366861
    Reviewed-by: Sneha Nagpaul <snehanagpaul@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Sneha Nagpaul <snehanagpaul@google.com>
    Auto-Submit: Sneha Nagpaul <snehanagpaul@google.com>
    Owners-Override: Sneha Nagpaul <snehanagpaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121334}

--- cc/input/main_thread_scrolling_reason.h
+++ cc/input/main_thread_scrolling_reason.h
@@ -60,6 +60,8 @@ struct CC_EXPORT MainThreadScrollingReason {
     kNonFastScrollableRegion = 1 << 8,
     kFailedHitTest = 1 << 9,
     kNoScrollingLayer = 1 << 10,
+    kNotScrollable = 1 << 11,
+    kNonInvertibleTransform = 1 << 12,
     kWheelEventHandlerRegion = 1 << 13,
     kTouchEventHandlerRegion = 1 << 14,
 
@@ -84,7 +86,8 @@ struct CC_EXPORT MainThreadScrollingReason {
   static bool CompositorCanSetScrollReasons(uint32_t reasons) {
     constexpr uint32_t reasons_set_by_compositor =
         kNonFastScrollableRegion | kFailedHitTest | kNoScrollingLayer |
-        kWheelEventHandlerRegion | kTouchEventHandlerRegion;
+        kNotScrollable | kNonInvertibleTransform | kWheelEventHandlerRegion |
+        kTouchEventHandlerRegion;
     return (reasons & reasons_set_by_compositor) == reasons;
   }
 
