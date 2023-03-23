9591c1bb0f7f0f4d3ad81e263a7383488af9bec2
Jeongwoo Park <jwoo.park@navercorp.com>
2023-03-23 14:01:49 +0000

    Fix typo in GraphicsContext comment and remove unnecessary header
    Change-Id: Iaecb1f56bdae333037a10b3e4fd76dcbc8a98a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349452
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Jeongwoo Park <skeksk91@gmail.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121142}

--- third_party/blink/renderer/platform/graphics/graphics_context.cc
+++ third_party/blink/renderer/platform/graphics/graphics_context.cc
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "third_party/skia/include/pathops/SkPathOps.h"
 #include "third_party/skia/include/utils/SkNullCanvas.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/point_conversions.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
@@ -220,7 +219,7 @@ unsigned GraphicsContext::SaveCount() const {
 #endif
 
 void GraphicsContext::SetInDrawingRecorder(bool val) {
-  // Nested drawing recorers are not allowed.
+  // Nested drawing recorders are not allowed.
   DCHECK(!val || !in_drawing_recorder_);
   in_drawing_recorder_ = val;
 }
