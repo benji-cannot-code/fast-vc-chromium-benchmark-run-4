588c47c305f66dc221b850afeedb6cdf77f43abd
Antonio Gomes <tonikitoo@igalia.com>
2023-03-23 18:20:00 +0000

    [ios] Enable moar blink_unittests
    Namely the following tests pass:
    - WebFrameTest.SiteForCookiesFromChildWithRemoteMainFrame
    - StyleEngineTest.VideoControlsReject
    - HTMLSrcsetParserTest.SaveDataEnabledBasic
    - UseCounterImplBrowserReportTest.ReportOnlyHTTPFamily
    
    BUG=1411478
    R=dtapuska@chromium.org
    
    Change-Id: I9e97b76912cce1ec0b42ac6719131beef3a5e6bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366032
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1121285}

--- third_party/blink/renderer/core/css/style_engine_test.cc
+++ third_party/blink/renderer/core/css/style_engine_test.cc
@@ -4035,13 +4035,7 @@ TEST_F(StyleEngineTest, CSSViewportUnits4RuntimeFlag) {
   }
 }
 
-#if BUILDFLAG(IS_IOS)
-// TODO(crbug.com/1141478)
-#define MAYBE_VideoControlsReject DISABLED_VideoControlsReject
-#else
-#define MAYBE_VideoControlsReject VideoControlsReject
-#endif  // BUILDFLAG(IS_IOS)
-TEST_F(StyleEngineTest, MAYBE_VideoControlsReject) {
+TEST_F(StyleEngineTest, VideoControlsReject) {
   GetDocument().body()->setInnerHTML(R"HTML(
     <video controls></video>
     <div id="target"></div>
