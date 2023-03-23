c99e3aa04837851ef2f4c52c0fc60438ccb5d763
Yongshun Liu <yongshun@chromium.org>
2023-03-23 00:47:51 +0000

    bento_button: Decouple desk textfield from overview
    This decouples desk textfield from overview and also renames
    `DesksTextfield` to `DeskTextfield`.
    
    `OverviewHighlightableView` will be functioning outside of overview as
    well, and will be renamed in a separate CL.
    
    TEST=All/BentoButtonTest.DeskTextfieldOutsideOverview/*
    BUG=b:273976060
    
    Change-Id: I1e88d35b0d84daf82b53d7ba7b8b5fdfdbbe4faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363862
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120920}

--- ash/wm/desks/templates/saved_desk_name_view.h
+++ ash/wm/desks/templates/saved_desk_name_view.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "ash/wm/desks/desks_textfield.h"
+#include "ash/wm/desks/desk_textfield.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 
@@ -16,7 +16,7 @@ namespace ash {
 
 // Defines a textfield styled to normally look like a label. Allows modifying
 // the name of its corresponding saved desk.
-class SavedDeskNameView : public DesksTextfield {
+class SavedDeskNameView : public DeskTextfield {
  public:
   METADATA_HEADER(SavedDeskNameView);
 
@@ -41,7 +41,7 @@ class SavedDeskNameView : public DesksTextfield {
   // size of `this`, which invalidates the layout.
   void OnContentsChanged();
 
-  // DesksTextfield:
+  // DeskTextfield:
   gfx::Size CalculatePreferredSize() const override;
   void OnGestureEvent(ui::GestureEvent* event) override;
 
@@ -50,7 +50,7 @@ class SavedDeskNameView : public DesksTextfield {
   absl::optional<std::u16string> temporary_name_;
 };
 
-BEGIN_VIEW_BUILDER(/* no export */, SavedDeskNameView, DesksTextfield)
+BEGIN_VIEW_BUILDER(/* no export */, SavedDeskNameView, DeskTextfield)
 END_VIEW_BUILDER
 
 }  // namespace ash
