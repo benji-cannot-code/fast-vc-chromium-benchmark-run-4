aef153973cafa67ca72f6f86cf2a83bd7a685b8a
Ali Juma <ajuma@chromium.org>
2023-03-23 21:31:23 +0000

    [iOS] Flesh out ContentWebFramesManager and add skeleton ContentWebFrame
    Bug: 1419001
    Change-Id: I17ee9de32d7eecd3c5c877bb88fc2568cf64da5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307655
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121402}

--- ios/web/content/js_messaging/content_web_frames_manager.h
+++ ios/web/content/js_messaging/content_web_frames_manager.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/public/js_messaging/web_frames_manager.h"
 
+#import "base/observer_list.h"
 #import "build/blink_buildflags.h"
+#import "content/public/browser/global_routing_id.h"
+#import "content/public/browser/web_contents_observer.h"
 
 #if !BUILDFLAG(USE_BLINK)
 #error File can only be included when USE_BLINK is true
@@ -16,11 +19,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
+class ContentWebState;
+
 // ContentWebFramesManager is a WebFramesManager that is built on top
-// of //content.
-class ContentWebFramesManager : public WebFramesManager {
+// of //content. As a WebContentsObserver, it finds out about all frame creation
+// and destruction. It uses two id schemes to identify frames: content's id
+// scheme for RenderFrameHosts, and a scheme that mimics WebFrameImpl's string
+// id scheme.
+class ContentWebFramesManager : public WebFramesManager,
+                                public content::WebContentsObserver {
  public:
-  ContentWebFramesManager();
+  ContentWebFramesManager(ContentWebState* content_web_state);
   ~ContentWebFramesManager() override;
 
   // WebFramesManager impl.
@@ -29,6 +38,26 @@ class ContentWebFramesManager : public WebFramesManager {
   std::set<WebFrame*> GetAllWebFrames() override;
   WebFrame* GetMainWebFrame() override;
   WebFrame* GetFrameWithId(const std::string& frame_id) override;
+
+  // WebContentsObserver overrides.
+  void RenderFrameCreated(content::RenderFrameHost* render_frame_host) override;
+  void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
+  void PrimaryPageChanged(content::Page& page) override;
+
+ private:
+  // List of pointers to all web frames.
+  std::map<std::string, std::unique_ptr<WebFrame>> web_frames_;
+
+  // Map from content's id scheme to web's id scheme.
+  std::map<content::GlobalRenderFrameHostId, std::string>
+      content_to_web_id_map_;
+
+  // Reference to the current main frame
+  content::GlobalRenderFrameHostId main_frame_content_id_;
+  base::ObserverList<Observer, /*check_empty=*/false> observers_;
+
+  // The ContentWebState that owns this object.
+  raw_ptr<ContentWebState> content_web_state_;
 };
 
 }  // namespace web
