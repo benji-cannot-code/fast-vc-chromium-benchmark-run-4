8b65f8e77e4b990a4a6c4820aaab562c91182e7f
Tom Burgin <bur@chromium.org>
2023-03-23 16:13:19 +0000

    [ImmersiveFullscreenTabs] hide tab strip during content fullscreen
    Hiding a titlebar view controller that is hosted within the titlebar
    is a bit more involved due to a bug in AppKit https://crbug.com/1426944.
    We work around this bug by adding and removing the view controller
    from the tree.
    
    While we are at it, this CL also has a few cleanups:
    * The 10.15+ `titlebarHeight` property and subsequent availability
    branching has been removed in favor of using the existing
    `_titlebarHeight` getter override.
    
    * The subclass of NSTitlebarAccessoryViewController is no longer
    needed to propagate frame size. Switched to constraints instead.
    
    Bug: 1425944, 1373552
    Change-Id: I7cbc89b7075ea0022e777ae2d7bcda1daa078f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4362563
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Tom Burgin <bur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121212}

--- components/remote_cocoa/app_shim/immersive_mode_controller.h
+++ components/remote_cocoa/app_shim/immersive_mode_controller.h
@@ -40,6 +40,8 @@ class REMOTE_COCOA_APP_SHIM_EXPORT ImmersiveModeController {
   virtual ~ImmersiveModeController();
 
   virtual void Enable();
+  bool is_enabled() { return enabled_; }
+
   virtual void FullscreenTransitionCompleted();
   virtual void OnTopViewBoundsChanged(const gfx::Rect& bounds);
   virtual void UpdateToolbarVisibility(mojom::ToolbarVisibilityStyle style);
@@ -82,6 +84,9 @@ class REMOTE_COCOA_APP_SHIM_EXPORT ImmersiveModeController {
   void SetTitlebarFullyVisibleForTesting(bool fully_visible) {
     titlebar_fully_visible_ = fully_visible;
   }
+
+  // Returns true if kImmersiveFullscreenTabs is being used.
+  virtual bool IsTabbed();
 
  private:
   // Pin or unpin the titlebar.
