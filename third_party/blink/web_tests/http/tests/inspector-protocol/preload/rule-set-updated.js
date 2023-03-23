094a75b3ad6df03ee6d0fc793dffdd0834fead15
keno <kenoss@chromium.org>
2023-03-23 08:24:37 +0000

    PreloadingDevTools: Store error of SpeculationRules and enrich CDP events
    This CL
    
    - ensures that SpeculationRuleSet::Parse returns a rule set. If an
      error occurred, it is stored in the rule set.
    - makes empty rule set with parse error to be stored in
      DocumentSpeculationRules (for a CDP event).
    - adds error information to Preload.ruleSetUpdated event.
    - makes SpeculantionRuleSet::Parse reports error if a rule set for a key
      (e.g. ruleSet["prefetch"]) is not undefined nor an array.
    
    Bug: 1384419
    Change-Id: I16de724075dbb0b6aa46ea8533480eabea7cfc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4308602
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121037}

--- third_party/blink/web_tests/http/tests/inspector-protocol/preload/rule-set-updated.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/preload/rule-set-updated.js
@@ -24,6 +24,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ]
         }
       </script>
+      <script type="speculationrules" id="invalid-json">
+        {
+          "prefetch":[
+      </script>
+      <script type="speculationrules" id="not-object">
+        "invalid"
+      </script>
+      <script type="speculationrules" id="contains-invalid-rule">
+        {
+          "prefetch": [
+            {
+              "source": "list",
+              "urls": ["/subresource.js"]
+            }
+          ],
+          "prerender": "invalid"
+        }
+      </script>
     </head>
     <body>
     </body>
@@ -37,10 +55,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     await dp.Preload.enable();
 
     await new Promise(resolve => {
-      let count = 2;
+      let count = 5;
       dp.Preload.onRuleSetUpdated(ruleSet => {
+        // Format sourceText.
         ruleSet.params.ruleSet.sourceText =
-            JSON.parse(ruleSet.params.ruleSet.sourceText);
+            ruleSet.params.ruleSet.errorType === undefined ?
+            JSON.parse(ruleSet.params.ruleSet.sourceText) :
+            // Prevent failures due to non visible differences coming from LF.
+            ruleSet.params.ruleSet.sourceText.replaceAll(/[\n ]+/g, '');
         testRunner.log(ruleSet);
 
         --count;
