d07206d495e790b885cdd3d6253685efe7ed382f
Ben Reich <benreich@chromium.org>
2023-03-23 00:09:10 +0000

    [FilesTrash] Reorder the Trash bin to be the last item always
    The UI review mentioned the Trash bin must be the last item always.
    Unfortunately with FSPs, Removeables, ADPs etc. this is not true.
    Reorder the trash initialisation to ensure it is always last.
    
    UI review deck: http://shortn/_s4VewdLxdN
    
    Fixed: b:274675376
    Test: Manual and CQ
    Test: http://screen/34TKUHa9CKqgEMn.png
    Change-Id: I14caadffd598b75136cb7052d140de43751e8d16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355369
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120899}

--- ui/file_manager/file_manager/state/reducers/navigation.ts
+++ ui/file_manager/file_manager/state/reducers/navigation.ts
@@ -50,10 +50,10 @@ function getPrefixEntryOrEntry(state: State, volume: Volume): VolumeEntry|
  *  2. Shortcuts.
  *  3. "My-Files" (grouping), actually Downloads volume.
  *  4. Drive volumes.
- *  5. Trash.
- *  6. Other FSP (File System Provider) (when mounted).
- *  7. Other volumes (MTP, ARCHIVE, REMOVABLE).
- *  8. Android apps.
+ *  5. Other FSP (File System Provider) (when mounted).
+ *  6. Other volumes (MTP, ARCHIVE, REMOVABLE).
+ *  7. Android apps.
+ *  8. Trash.
  */
 export function refreshNavigationRoots(
     currentState: State, _action: RefreshNavigationRootsAction): State {
@@ -128,20 +128,7 @@ export function refreshNavigationRoots(
     processedEntryKeys.add(driveEntry.toURL());
   }
 
-  // 5. Trash
-  const trashEntry =
-      getEntry(currentState, trashRootKey) as FilesAppEntry | null;
-  if (trashEntry) {
-    roots.push({
-      key: trashRootKey,
-      section: NavigationSection.TRASH,
-      separator: true,
-      type: NavigationType.TRASH,
-    });
-    processedEntryKeys.add(trashRootKey);
-  }
-
-  // 6/7. Other volumes.
+  // 5/6. Other volumes.
   const volumesOrder = {
     [VolumeType.SMB]: 1,
     [VolumeType.PROVIDED]: 2,  // FSP.
@@ -201,7 +188,7 @@ export function refreshNavigationRoots(
     }
   }
 
-  // 8. Android Apps.
+  // 7. Android Apps.
   Object
       .values(
           androidApps as Record<string, chrome.fileManagerPrivate.AndroidApp>)
@@ -214,6 +201,19 @@ export function refreshNavigationRoots(
         });
         processedEntryKeys.add(app.packageName);
       });
+
+  // 8. Trash
+  const trashEntry =
+      getEntry(currentState, trashRootKey) as FilesAppEntry | null;
+  if (trashEntry) {
+    roots.push({
+      key: trashRootKey,
+      section: NavigationSection.TRASH,
+      separator: true,
+      type: NavigationType.TRASH,
+    });
+    processedEntryKeys.add(trashRootKey);
+  }
 
   return {
     ...currentState,
