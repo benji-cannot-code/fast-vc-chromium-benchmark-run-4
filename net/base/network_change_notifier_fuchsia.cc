37cd787450d2a34bfcfc9616a30ea0a8bc02ff6d
Wez <wez@chromium.org>
2023-03-24 15:06:11 +0000

    [net][base] Fix NetworkChangeNotifierFuchsia racey crash condition
    Fix the NetworkChangeNotifierFuchsia to deterministically exit the
    calling process if the fuchsia.net.interfaces.Watcher connection is
    closed before or during setup, or during normal execution.
    
    This removes some timing-dependent error-handling behaviour, including
    a timing-dependent crash condition.
    
    Bug: 1425913, b/274140103
    Change-Id: I35f09eb7f802f1523ee934ab22d59867eeab978a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4352543
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: David Song <wintermelons@google.com>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121693}

--- net/base/network_change_notifier_fuchsia.cc
+++ net/base/network_change_notifier_fuchsia.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/process_context.h"
 #include "base/logging.h"
+#include "base/process/process.h"
 #include "base/threading/thread_checker.h"
 #include "base/types/expected.h"
 #include "net/base/fuchsia/network_interface_cache.h"
@@ -40,7 +41,8 @@ NetworkChangeNotifierFuchsia::NetworkChangeNotifierFuchsia(
   auto handle_or_status = internal::ReadExistingNetworkInterfacesFromNewWatcher(
       std::move(watcher_handle), interfaces);
   if (!handle_or_status.has_value()) {
-    return;
+    ZX_LOG(ERROR, handle_or_status.error()) << "ReadExistingNetworkInterfaces";
+    base::Process::TerminateCurrentProcessImmediately(1);
   }
 
   HandleCacheStatus(cache_.AddInterfaces(std::move(interfaces)));
@@ -121,9 +123,10 @@ fuchsia::net::interfaces::WatcherHandle ConnectInterfacesWatcher() {
       base::ComponentContextForProcess()->svc()->Connect(state.NewRequest());
   ZX_CHECK(status == ZX_OK, status) << "Connect()";
 
+  // GetWatcher() is a feed-forward API, so failures will be observed via
+  // peer-closed events on the returned `watcher`.
   fuchsia::net::interfaces::WatcherHandle watcher;
   status = state->GetWatcher(/*options=*/{}, watcher.NewRequest());
-  ZX_CHECK(status == ZX_OK, status) << "GetWatcher()";
 
   return watcher;
 }
@@ -136,6 +139,11 @@ ReadExistingNetworkInterfacesFromNewWatcher(
 
   fuchsia::net::interfaces::WatcherSyncPtr watcher = watcher_handle.BindSync();
 
+  // fuchsia.net.interfaces.Watcher implements a hanging-get pattern, accepting
+  // a single Watch() call and returning an event when something changes.
+  // When a Watcher is first created, it emits a series of events describing
+  // existing interfaces, terminated by an "idle" event, before entering the
+  // normal hanging-get flow.
   while (true) {
     fuchsia::net::interfaces::Event event;
     if (auto watch_status = watcher->Watch(&event); watch_status != ZX_OK) {
@@ -153,7 +161,7 @@ ReadExistingNetworkInterfacesFromNewWatcher(
         return base::ok(watcher.Unbind());
       default:
         LOG(ERROR) << "Unexpected event " << event.Which();
-        return base::unexpected(ZX_ERR_NOT_SUPPORTED);
+        return base::unexpected(ZX_ERR_BAD_STATE);
     }
   }
 }
