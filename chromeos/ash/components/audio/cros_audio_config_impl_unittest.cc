559d2a8e6937f5ad9b6a0958943691282b546eaa
Ashley Prasad <ashleydp@google.com>
2023-03-24 21:26:22 +0000

    audio-settings: remove display name from stub
    Localized display name handled in UI based on device type so display
    name on stub only used in tests.
    
    - Do no set display_name property on stub internal_mic.
    - Remove TODO for localizing stub display_name.
    - Update tests to test for `device_type` instead of `display_name`.
    
    Bug: b:260277007
    Test: chromeos_unittest --gtest_filter=*CrosAudioConfigTest*
    Change-Id: I912ff0c82820eb63c7e6a836058c6d315ca08e4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4361759
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121934}

--- chromeos/ash/components/audio/cros_audio_config_impl_unittest.cc
+++ chromeos/ash/components/audio/cros_audio_config_impl_unittest.cc
@@ -950,8 +950,7 @@ TEST_F(CrosAudioConfigImplTest, StubInternalMicHandlesDualMicUpdates) {
       fake_observer->last_audio_system_properties_.value()
           ->input_devices[1]
           .Clone();
-  const std::string expected_display_name = "Internal Mic";
-  EXPECT_EQ(expected_display_name, internal_mic->display_name);
+  EXPECT_EQ(mojom::AudioDeviceType::kInternalMic, internal_mic->device_type);
   EXPECT_FALSE(internal_mic->is_active);
 
   SimulateSetActiveDevice(kInternalMicFrontId);
@@ -962,7 +961,7 @@ TEST_F(CrosAudioConfigImplTest, StubInternalMicHandlesDualMicUpdates) {
   internal_mic = fake_observer->last_audio_system_properties_.value()
                      ->input_devices[1]
                      .Clone();
-  EXPECT_EQ(expected_display_name, internal_mic->display_name);
+  EXPECT_EQ(mojom::AudioDeviceType::kInternalMic, internal_mic->device_type);
   EXPECT_TRUE(internal_mic->is_active);
 
   // Verify rear device ID will also set internal mic to active.
@@ -975,7 +974,7 @@ TEST_F(CrosAudioConfigImplTest, StubInternalMicHandlesDualMicUpdates) {
   internal_mic = fake_observer->last_audio_system_properties_.value()
                      ->input_devices[1]
                      .Clone();
-  EXPECT_EQ(expected_display_name, internal_mic->display_name);
+  EXPECT_EQ(mojom::AudioDeviceType::kInternalMic, internal_mic->device_type);
   EXPECT_TRUE(internal_mic->is_active);
 }
 
