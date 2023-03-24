31f4909c7f68d1d238577f509e2b18385aeb9cd4
Andrew Grieve <agrieve@chromium.org>
2023-03-24 20:29:37 +0000

    Android: Initialize ICU earlier
    And don't bother passing FD's via IPC to renderers. Have them open the
    file directly instead (isolated processes can access the APK).
    
    This will allow JNI methods to access ICU before content main is
    called (as is the issue on the attached bug).
    
    Pinpoint shows this change to be neutral to slightly positive for
    startup.mobile.
    
    Bug: 1418738
    Change-Id: Ibda66a1548bc1d1e76155fdd44dad1241e72e9fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357575
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121905}

--- content/app/android/content_jni_onload.cc
+++ content/app/android/content_jni_onload.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/base_jni_onload.h"
 #include "base/android/jni_android.h"
 #include "base/android/library_loader/library_loader_hooks.h"
-#include "base/functional/bind.h"
+#include "base/i18n/icu_util.h"
+#include "base/trace_event/trace_event.h"
 #include "content/app/android/library_loader_hooks.h"
-#include "content/public/app/content_main.h"
 
 namespace content {
 namespace android {
@@ -22,6 +22,13 @@ bool OnJNIOnLoadInit() {
     return false;
 
   base::android::SetLibraryLoadedHook(&content::LibraryLoaded);
+
+#if ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE
+  // Initialize ICU early so that it can be used by JNI calls before
+  // ContentMain() is called.
+  TRACE_EVENT0("startup", "InitializeICU");
+  CHECK(base::i18n::InitializeICU());
+#endif
   return true;
 }
 
