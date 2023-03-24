da649639d259d1f888f72668e4b90be40f72a2df
Ernesto Izquierdo Clua <eic@google.com>
2023-03-24 21:23:17 +0000

    [ios] Make third row label multiline in TableViewURLView
    This patch makes the label multiline so the text is readable when it
    doesn't fit in the available space.
    
    Before fix:
    https://screenshot.googleplex.com/BusERstqBZ7eJWz
    
    After fix:
    https://screenshot.googleplex.com/4N72xV5aPrjVPME
    
    Fixed: 1427618
    Change-Id: I32a0ed980838e0a59a43e0ea4e6a6560e2425a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368783
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121931}

--- ios/chrome/browser/shared/ui/table_view/cells/table_view_url_item.mm
+++ ios/chrome/browser/shared/ui/table_view/cells/table_view_url_item.mm
@@ -180,6 +180,8 @@ const char kDefaultSupplementalURLTextDelimiter[] = "•";
         [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
     _thirdRowLabel.adjustsFontForContentSizeCategory = YES;
     _thirdRowLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
+    _thirdRowLabel.numberOfLines = 0;
+    _thirdRowLabel.lineBreakMode = NSLineBreakByWordWrapping;
     _thirdRowLabel.hidden = YES;
     _metadataLabel.font =
         [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
