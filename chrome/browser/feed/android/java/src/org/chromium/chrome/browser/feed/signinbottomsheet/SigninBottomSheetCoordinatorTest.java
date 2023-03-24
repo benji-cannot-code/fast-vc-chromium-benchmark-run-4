117f8ee1cba80f498ff74859d995a485f8513fc7
Nick Birnie <birnie@google.com>
2023-03-24 01:46:33 +0000

    [BoC Signin] Add toast for when signin is attempted while prohibited.
    Bug: b/273822882
    Change-Id: I663e3eed40008ab2e94048f0484d842297952c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4354853
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121503}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/signinbottomsheet/SigninBottomSheetCoordinatorTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/signinbottomsheet/SigninBottomSheetCoordinatorTest.java
@@ -9,6 +9,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,6 +30,7 @@ import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.chrome.browser.ui.signin.account_picker.AccountPickerBottomSheetCoordinator;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.signin.AccountUtils;
 import org.chromium.components.signin.base.CoreAccountInfo;
@@ -48,6 +50,9 @@ public class SigninBottomSheetCoordinatorTest {
 
     @Mock
     private BottomSheetController mBottomSheetControllerMock;
+
+    @Mock
+    private BottomSheetContent mBottomSheetContentMock;
 
     @Mock
     private WindowAndroid mWindowAndroidMock;
@@ -72,6 +77,7 @@ public class SigninBottomSheetCoordinatorTest {
         IdentityServicesProvider.setInstanceForTests(mock(IdentityServicesProvider.class));
         when(IdentityServicesProvider.get().getSigninManager(mProfileMock))
                 .thenReturn(mSigninManagerMock);
+        when(mSigninManagerMock.isSigninAllowed()).thenReturn(true);
         mCoreAccountInfo = mAccountManagerTestRule.addAccount(TEST_EMAIL);
         mSigninCoordinator = new SigninBottomSheetCoordinator(
                 mWindowAndroidMock, mBottomSheetControllerMock, mProfileMock);
@@ -109,5 +115,14 @@ public class SigninBottomSheetCoordinatorTest {
                 mAccountPickerBottomSheetCoordinatorMock);
         mSigninCoordinator.signIn(TEST_EMAIL, error -> {});
         histogramWatcher.assertExpected();
+    }
+
+    @Test
+    public void testSignInNotAllowed() {
+        when(mSigninManagerMock.isSigninAllowed()).thenReturn(false);
+        mSigninCoordinator.setToastOverrideForTesting();
+        mSigninCoordinator.signIn(TEST_EMAIL, error -> {});
+        verify(mSigninManagerMock, never())
+                .signin(eq(AccountUtils.createAccountFromName(TEST_EMAIL)), any());
     }
 }
