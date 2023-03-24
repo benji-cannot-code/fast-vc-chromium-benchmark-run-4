25f79145042048dac4621e66ac5b17790bb7605b
Peter Kvitek <kvitekp@chromium.org>
2023-03-24 15:01:42 +0000

    [headless] Hide dock icon and menu bar on Macs when in headless mode
    Bug: 1414957
    Change-Id: I5c674f0f8812b82dcece17a098b55eb3874366e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368658
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121691}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -122,6 +122,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_mac.h"
 #include "chrome/app/chrome_main_mac.h"
 #include "chrome/browser/chrome_browser_application_mac.h"
+#include "chrome/browser/headless/headless_mode_util.h"
 #include "chrome/browser/mac/relauncher.h"
 #include "chrome/browser/shell_integration.h"
 #include "components/crash/core/common/objc_zombie.h"
@@ -1730,6 +1731,12 @@ absl::optional<int> ChromeMainDelegate::PreBrowserMain() {
 
   // Initialize NSApplication using the custom subclass.
   chrome_browser_application_mac::RegisterBrowserCrApp();
+
+  // Perform additional initialization when running in headless mode: hide
+  // dock icon and menu bar.
+  if (headless::IsHeadlessMode()) {
+    chrome_browser_application_mac::InitializeHeadlessMode();
+  }
 
   if (l10n_util::GetLocaleOverride().empty()) {
     // The browser process only wants to support the language Cocoa will use,
