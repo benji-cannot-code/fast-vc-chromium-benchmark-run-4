a2ddf0f4c3e3bb4698ee9e555915a322fdff7cb3
Martin Bidlingmaier <mbid@google.com>
2023-03-24 10:38:14 +0000

    Expose auth factor config services to OobeUI
    This is in preparation for migration away from the quickUnlockPrivate extension API to the AuthFactorConfig (and related) mojo services. The services need to be available during OOBE because of PIN setup.
    
    Bug: b:258481055
    Change-Id: I0b3b8f4b0b1c02e45f282e7ef961e8cd7266166c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348532
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121607}

--- chrome/browser/ui/webui/ash/login/oobe_ui.h
+++ chrome/browser/ui/webui/ash/login/oobe_ui.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/oobe_screen.h"
 #include "chrome/browser/ui/webui/ash/login/base_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/core_oobe_handler.h"
+#include "chromeos/ash/services/auth_factor_config/public/mojom/auth_factor_config.mojom-forward.h"
 #include "chromeos/ash/services/cellular_setup/public/mojom/esim_manager.mojom-forward.h"
 #include "chromeos/ash/services/multidevice_setup/public/mojom/multidevice_setup.mojom-forward.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom-forward.h"
@@ -156,6 +157,12 @@ class OobeUI : public ui::MojoWebUIController {
   void BindInterface(
       mojo::PendingReceiver<color_change_listener::mojom::PageHandler>
           receiver);
+
+  // Binds to the cros authentication factor editing services.
+  void BindInterface(
+      mojo::PendingReceiver<auth::mojom::AuthFactorConfig> receiver);
+  void BindInterface(
+      mojo::PendingReceiver<auth::mojom::PinFactorEditor> receiver);
 
   static void AddOobeComponents(content::WebUIDataSource* source);
 
