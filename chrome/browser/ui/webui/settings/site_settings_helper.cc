1d2d414d6e57538848d21b73acb4930b047fa507
Robbie McElrath <rmcelrath@chromium.org>
2023-03-24 17:58:13 +0000

    iwa: Use UrlIdentity in SiteSettingsHandler
    This makes GetIsolatedWebAppName use UrlIdentity instead of querying
    WebAppProvider directly.
    
    Bug: 1311065, b/273757632
    Change-Id: I65acae3428004436c7a7b98adb0d0853bf737a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368164
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121798}

--- chrome/browser/ui/webui/settings/site_settings_helper.cc
+++ chrome/browser/ui/webui/settings/site_settings_helper.cc
@@ -32,12 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/serial/serial_chooser_context.h"
 #include "chrome/browser/serial/serial_chooser_context_factory.h"
 #include "chrome/browser/subresource_filter/subresource_filter_profile_context_factory.h"
+#include "chrome/browser/ui/url_identity.h"
 #include "chrome/browser/usb/usb_chooser_context.h"
 #include "chrome/browser/usb/usb_chooser_context_factory.h"
-#include "chrome/browser/web_applications/web_app_provider.h"
-#include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/url_constants.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
@@ -1042,17 +1042,13 @@ base::Value::List GetChooserExceptionListFromProfile(
 
 absl::optional<std::string> GetIsolatedWebAppName(Profile* profile,
                                                   GURL origin) {
-  absl::optional<std::string> app_name;
-  if (auto* provider = web_app::WebAppProvider::GetForWebApps(profile)) {
-    if (absl::optional<web_app::AppId> app_id =
-            provider->registrar_unsafe().FindAppWithUrlInScope(origin)) {
-      if (!provider->registrar_unsafe().IsIsolated(*app_id)) {
-        return app_name;
-      }
-      app_name = provider->registrar_unsafe().GetAppShortName(*app_id);
-    }
+  if (!origin.SchemeIs(chrome::kIsolatedAppScheme)) {
+    return absl::nullopt;
   }
-  return app_name;
+  auto identity = UrlIdentity::CreateFromUrl(
+      profile, origin,
+      /*allowed_types=*/{UrlIdentity::Type::kIsolatedWebApp}, /*options=*/{});
+  return base::UTF16ToUTF8(identity.name);
 }
 
 absl::optional<std::string> GetExtensionDisplayName(Profile* profile,
