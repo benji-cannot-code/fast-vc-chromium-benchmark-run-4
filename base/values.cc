7cd8a4bccf83fdd3f54eef317e9672c554ee80c8
Emilia Paz <emiliapaz@chromium.org>
2023-03-24 03:22:05 +0000

    [Code Health] Remove base::Value::SetIntPath()
    This method was deprecated and all its usages have been removed.
    
    Bug: 1187001
    Change-Id: I8d18bc022cb501b2925152779e9374f7dfa4d077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4359932
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121522}

--- base/values.cc
+++ base/values.cc
@@ -1286,10 +1286,6 @@ Value* Value::SetPath(StringPiece path, Value&& value) {
   return GetDict().SetByDottedPath(path, std::move(value));
 }
 
-Value* Value::SetIntPath(StringPiece path, int value) {
-  return GetDict().SetByDottedPath(path, value);
-}
-
 Value* Value::SetDoublePath(StringPiece path, double value) {
   return GetDict().SetByDottedPath(path, value);
 }
