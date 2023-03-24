d7767d098b9ec35ab9791adb408d0d88848693eb
David Pennington <dpenning@chromium.org>
2023-03-24 18:00:57 +0000

    Add the background colors for ChromeRefresh2023 Tabstyle.
    - in order to access a const version of the tab, an accessor method is
    added to GM2TabStyle
    - GetTabBackgroundColor is now virtual and overridable.
    - The same color ids are used for CR23 background color so it
    currently looks the same.
    
    Change-Id: Ie73f4de5d3b5408f0bede9d4a0e4ce3d4e30d6eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366257
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121799}

--- chrome/browser/ui/views/tabs/tab_style_views.cc
+++ chrome/browser/ui/views/tabs/tab_style_views.cc
@@ -84,6 +84,8 @@ class GM2TabStyle : public TabStyleViews {
   GM2TabStyle(const GM2TabStyle&) = delete;
   GM2TabStyle& operator=(const GM2TabStyle&) = delete;
 
+  const Tab* tab() const { return tab_; }
+
  protected:
   // TabStyle:
   SkPath GetPath(
@@ -100,6 +102,9 @@ class GM2TabStyle : public TabStyleViews {
   void SetHoverLocation(const gfx::Point& location) override;
   void ShowHover(ShowHoverStyle style) override;
   void HideHover(HideHoverStyle style) override;
+
+  // Painting helper functions:
+  virtual SkColor GetTabBackgroundColor(TabActive active) const;
 
  private:
   // Gets the bounds for the leading and trailing separators for a tab.
@@ -146,8 +151,6 @@ class GM2TabStyle : public TabStyleViews {
 
   bool ShouldPaintTabBackgroundColor(TabActive active,
                                      bool has_custom_background) const;
-
-  SkColor GetTabBackgroundColor(TabActive active) const;
 
   // When selected, non-active, non-hovered tabs are adjacent to each other,
   // there are anti-aliasing artifacts in the overlapped lower arc region. This
@@ -953,9 +956,27 @@ gfx::RectF GM2TabStyle::ScaleAndAlignBounds(const gfx::Rect& bounds,
 class GM3TabStyle : public GM2TabStyle {
  public:
   explicit GM3TabStyle(Tab* tab);
+  SkColor GetTabBackgroundColor(TabActive active) const override;
 };
 
 GM3TabStyle::GM3TabStyle(Tab* tab) : GM2TabStyle(tab) {}
+
+SkColor GM3TabStyle::GetTabBackgroundColor(TabActive active) const {
+  const auto* cp = tab()->GetWidget()->GetColorProvider();
+  DCHECK(cp);
+  if (!cp) {
+    return gfx::kPlaceholderColor;
+  }
+
+  constexpr ChromeColorIds kColorIds[2][2] = {
+      {kColorTabBackgroundInactiveFrameInactive,
+       kColorTabBackgroundInactiveFrameActive},
+      {kColorTabBackgroundActiveFrameInactive,
+       kColorTabBackgroundActiveFrameActive}};
+
+  return cp->GetColor(kColorIds[int(active == TabActive::kActive)][int(
+      tab()->controller()->ShouldPaintAsActiveFrame())]);
+}
 
 }  // namespace
 
