2a9c1f3766be042e0ce8e5f4d98f0948baa93962
David Padlipsky <dpad@google.com>
2023-03-24 22:03:26 +0000

    Settings Split: Only persist pointing stick settings when user changed
    Bug: b/241965700
    Change-Id: I7b17cf3180e38d8fc2ab2c6241e01058f3bca294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4364245
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121949}

--- ash/system/input_device_settings/pref_handlers/pointing_stick_pref_handler_impl.h
+++ ash/system/input_device_settings/pref_handlers/pointing_stick_pref_handler_impl.h
@@ -30,15 +30,6 @@ class ASH_EXPORT PointingStickPrefHandlerImpl
   void UpdatePointingStickSettings(
       PrefService* pref_service,
       const mojom::PointingStick& pointing_stick) override;
-
- private:
-  mojom::PointingStickSettingsPtr GetNewPointingStickSettings(
-      PrefService* prefs,
-      const mojom::PointingStick& pointing_stick);
-  mojom::PointingStickSettingsPtr RetrievePointingStickSettings(
-      PrefService* prefs,
-      const mojom::PointingStick& pointing_stick,
-      const base::Value::Dict& settings_dict);
 };
 
 }  // namespace ash
