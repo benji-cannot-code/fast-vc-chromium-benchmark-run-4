c4fd2ba57d1d2549fbc3df46320ff30dcd03e24f
Christoph Schwering <schwering@google.com>
2023-03-24 12:45:47 +0000

    [Autofill] Remove BrowserAutofillManager::GetPersonalDataManager()
    This CL removes BrowserAutofillManager::GetPersonalDataManager()
    and the underlying member variable and instead calls
    AutofillClient::GetPersonalDataManager().
    
    This CL is a no-op.
    
    Bug: 1200511
    Change-Id: I39f5fb3c87329b6ea9350a9ca0796947676caf77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370143
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121637}

--- components/autofill/core/browser/test_browser_autofill_manager.cc
+++ components/autofill/core/browser/test_browser_autofill_manager.cc
@@ -174,7 +174,7 @@ void TestBrowserAutofillManager::StoreUploadVotesAndLogQualityCallback(
 }
 
 const gfx::Image& TestBrowserAutofillManager::GetCardImage(
-    const CreditCard& credit_card) const {
+    const CreditCard& credit_card) {
   return card_image_;
 }
 
