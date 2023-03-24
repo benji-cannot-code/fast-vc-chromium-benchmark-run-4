3f402486c80f559016ac01fc8ff07e2b1a9547f5
Rakina Zata Amni <rakina@chromium.org>
2023-03-24 18:20:13 +0000

    Add 'AllFrames' to GetRenderDocumentLevelNameForTestParams()
    This makes tests running with the "AllFrames" level of RenderDocument
    use "RDAllFrames" instead of "RDSubframe" in the test param name. This
    was taken out of crrev.com/c/4202277 because the test name changes
    causes some bots mistakenly think existing flaky tests are new flaky
    tests introduced by the CL, even though they're the same test, just
    with a different name due to the param name change.
    
    Bug: 936696
    Change-Id: Ie7475b7d889e43a8ddc35193c7464f497e3935f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4360399
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121821}

--- content/test/render_document_feature.cc
+++ content/test/render_document_feature.cc
@@ -40,8 +40,12 @@ std::string GetRenderDocumentLevelNameForTestParams(
   if (render_document_level ==
       GetRenderDocumentLevelName(RenderDocumentLevel::kCrashedFrame)) {
     return "RDCrashedFrame";
+  } else if (render_document_level ==
+             GetRenderDocumentLevelName(RenderDocumentLevel::kSubframe)) {
+    return "RDSubframe";
+  } else {
+    return "RDAllFrames";
   }
-  return "RDSubframe";
 }
 
 }  // namespace content
