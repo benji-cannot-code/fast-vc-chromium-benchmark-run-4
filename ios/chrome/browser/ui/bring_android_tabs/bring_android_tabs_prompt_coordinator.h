7f6862b840a154b3662eb457f02f70c6708e48ba
ginnnnnnny <ginnyhuang@chromium.org>
2023-03-24 18:10:03 +0000

    [ios][byot] Bring Android Tabs: UI architecture
    This CL defines the architecture/information and interaction flow for
    byot prompt. It will be followed by actual UIViewController prompt.
    
    Bug: 1418115
    Change-Id: Id6d967cccc8425d29a4ee63e7370d18c9d92e0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335381
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121812}

---
+++ ios/chrome/browser/ui/bring_android_tabs/bring_android_tabs_prompt_coordinator.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_BRING_ANDROID_TABS_BRING_ANDROID_TABS_PROMPT_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_UI_BRING_ANDROID_TABS_BRING_ANDROID_TABS_PROMPT_COORDINATOR_H_
+
+#import "ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.h"
+
+#import <UIKit/UIKit.h>
+
+// Coordinator that manages the "Bring Android Tabs" prompt presentation and
+// interaction.
+@interface BringAndroidTabsPromptCoordinator : ChromeCoordinator
+
+// View controller for the prompt.
+@property(nonatomic, strong) UIViewController* viewController;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_BRING_ANDROID_TABS_BRING_ANDROID_TABS_PROMPT_COORDINATOR_H_
