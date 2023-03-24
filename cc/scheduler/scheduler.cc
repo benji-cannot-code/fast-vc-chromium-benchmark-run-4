0e4dcb571c06ad7e43a76885648ee70d2d193531
Etienne Pierre-doray <etiennep@chromium.org>
2023-03-24 21:22:31 +0000

    Always use precise deadline
    This undoes some of https://chromium-review.googlesource.com/c/chromium/src/+/3926235
    with the goal of investigating smoothness regressions
    under ExplicitHighResolutionTimerWin.
    This changes has no effect outside of ExplicitHighResolutionTimerWin.
    
    Bug: 700165
    Change-Id: I5313673aff4086cee337463aa95e2ba5247efff9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4358994
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121930}

--- cc/scheduler/scheduler.cc
+++ cc/scheduler/scheduler.cc
@@ -778,9 +778,7 @@ void Scheduler::ScheduleBeginImplFrameDeadline() {
         FROM_HERE, deadline_,
         base::BindOnce(&Scheduler::OnBeginImplFrameDeadline,
                        base::Unretained(this)),
-        deadline_mode_ == DeadlineMode::LATE
-            ? base::subtle::DelayPolicy::kFlexibleNoSooner
-            : base::subtle::DelayPolicy::kPrecise);
+        base::subtle::DelayPolicy::kPrecise);
   }
 }
 
