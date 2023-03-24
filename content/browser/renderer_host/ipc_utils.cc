bd97205e97749b292b84a66e5bb7ca257681ebbc
Sharon Yang <yangsharon@chromium.org>
2023-03-24 17:49:24 +0000

    VerifyOpenURLParams with process instead of SiteInstance
    As part of SiteInstanceGroup, we want to transition RenderFrameProxyHost
    from all uses of SiteInstance to SiteInstanceGroup. One such use is in
    VerifyOpenURLParams, so update it to take a RenderProcessHost instead.
    The parameter is used to access the process and StoragePartition.
    
    Test: No behaviour change
    Bug: 1261963
    Change-Id: I6ee0084e30a579fae6cafe4b6b88f69e0676578f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368187
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121788}

--- content/browser/renderer_host/ipc_utils.cc
+++ content/browser/renderer_host/ipc_utils.cc
@@ -125,17 +125,16 @@ bool VerifyDownloadUrlParams(SiteInstance* site_instance,
 }
 
 bool VerifyOpenURLParams(RenderFrameHostImpl* current_rfh,
-                         SiteInstance* site_instance,
+                         RenderProcessHost* process,
                          const blink::mojom::OpenURLParamsPtr& params,
                          GURL* out_validated_url,
                          scoped_refptr<network::SharedURLLoaderFactory>*
                              out_blob_url_loader_factory) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   DCHECK(current_rfh);
-  DCHECK(site_instance);
+  DCHECK(process);
   DCHECK(out_validated_url);
   DCHECK(out_blob_url_loader_factory);
-  RenderProcessHost* process = site_instance->GetProcess();
   int process_id = process->GetID();
 
   // Verify |params.url| and populate |out_validated_url|.
@@ -149,14 +148,12 @@ bool VerifyOpenURLParams(RenderFrameHostImpl* current_rfh,
   if (params->blob_url_token.is_valid()) {
     *out_blob_url_loader_factory =
         ChromeBlobStorageContext::URLLoaderFactoryForToken(
-            site_instance->GetBrowserContext()->GetStoragePartition(
-                site_instance),
-            std::move(params->blob_url_token));
+            process->GetStoragePartition(), std::move(params->blob_url_token));
   }
 
   // Verify |params.post_body|.
   auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
-  if (!policy->CanReadRequestBody(site_instance, params->post_body)) {
+  if (!policy->CanReadRequestBody(process, params->post_body)) {
     bad_message::ReceivedBadMessage(process,
                                     bad_message::ILLEGAL_UPLOAD_PARAMS);
     return false;
@@ -206,7 +203,7 @@ bool VerifyBeginNavigationCommonParams(
 
   // Verify |post_data|.
   auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
-  if (!policy->CanReadRequestBody(site_instance, common_params->post_data)) {
+  if (!policy->CanReadRequestBody(process, common_params->post_data)) {
     bad_message::ReceivedBadMessage(process,
                                     bad_message::ILLEGAL_UPLOAD_PARAMS);
     return false;
