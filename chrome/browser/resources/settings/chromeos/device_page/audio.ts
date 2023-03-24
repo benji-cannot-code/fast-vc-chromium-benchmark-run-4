7edb56c9c57da39e6a69300522d16fe5de22e1d7
Ashley Prasad <ashleydp@google.com>
2023-03-24 21:27:23 +0000

    audio-settings: cleanup todos
    Remove TODO comments which were completed by mojo updates.
    
    Bug: b:260277007
    Test: browser_tests --gtest_filter=*OSSettingsDevicePage*
    Change-Id: I0f8f4153155a0633cb235620d7dd3f7534edadee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4361820
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121935}

--- chrome/browser/resources/settings/chromeos/device_page/audio.ts
+++ chrome/browser/resources/settings/chromeos/device_page/audio.ts
@@ -159,21 +159,11 @@ class SettingsAudioElement extends SettingsAudioElementBase {
   }
 
   protected onInputMuteClicked(): void {
-    // TODO(b/260277007): Remove condition when setInputMuted added to mojo
-    // definition.
-    if (!this.crosAudioConfig_.setInputMuted) {
-      return;
-    }
     this.crosAudioConfig_.setInputMuted(!this.isInputMuted_);
   }
 
   /** Handles updating active input device. */
   protected onInputDeviceChanged(): void {
-    // TODO(b/260277007): Remove condition when setActiveDevice added to mojo
-    // definition.
-    if (!this.crosAudioConfig_.setActiveDevice) {
-      return;
-    }
     const inputDeviceSelect = this.shadowRoot!.querySelector<HTMLSelectElement>(
         '#audioInputDeviceDropdown');
     assert(!!inputDeviceSelect);
@@ -218,11 +208,6 @@ class SettingsAudioElement extends SettingsAudioElementBase {
 
   /** Handles updating active output device. */
   protected onOutputDeviceChanged(): void {
-    // TODO(b/260277007): Remove condition when setActiveDevice added to mojo
-    // definition.
-    if (!this.crosAudioConfig_.setActiveDevice) {
-      return;
-    }
     const outputDeviceSelect =
         this.shadowRoot!.querySelector<HTMLSelectElement>(
             '#audioOutputDeviceDropdown');
@@ -232,11 +217,6 @@ class SettingsAudioElement extends SettingsAudioElementBase {
 
   /** Handles updating outputMuteState. */
   protected onOutputMuteButtonClicked(): void {
-    // TODO(b/260277007): Remove condition when setOutputMuted added to mojo
-    // definition.
-    if (!this.crosAudioConfig_.setOutputMuted) {
-      return;
-    }
     this.crosAudioConfig_.setOutputMuted(!this.isOutputMuted_);
   }
 
