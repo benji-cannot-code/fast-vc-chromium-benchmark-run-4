4b05c05e2d6ac5d5917831ae858f101981dafb22
Daniel Murphy <dmurph@google.com>
2023-03-24 22:22:03 +0000

    [dPWA] Checking that shortcut menu sizes are the same size
    Bug: 1417955
    Change-Id: I85d6a3f50491f276d0fce9dad50850b8a82e3499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367804
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121969}

--- chrome/browser/web_applications/web_app_icon_manager.cc
+++ chrome/browser/web_applications/web_app_icon_manager.cc
@@ -463,6 +463,7 @@ TypedResult<ShortcutsMenuIconBitmaps> ReadShortcutsMenuIconsBlocking(
     // item.
     results.value.push_back(std::move(result));
   }
+  CHECK_EQ(shortcuts_menu_icons_sizes.size(), results.value.size());
   return results;
 }
 
@@ -566,6 +567,7 @@ ReadShortcutMenuIconsWithTimestampBlocking(
     // item.
     results.value.push_back(std::move(data));
   }
+  CHECK_EQ(shortcuts_menu_icons_sizes.size(), results.value.size());
   return results;
 }
 
