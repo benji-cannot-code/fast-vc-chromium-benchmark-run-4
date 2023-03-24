333ae0836665f42ade7c8e955ea127c44f69db3d
Mikhail Khokhlov <khokhlov@google.com>
2023-03-24 15:24:24 +0000

    [Tracing w/SDK] Add missing category
    Change-Id: I8e16c814bc88d60969ed0b3207fdbfc02d695cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370083
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121702}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -94,6 +94,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("GAMEPAD")                                                           \
   X("gpu")                                                               \
   X("gpu.angle")                                                         \
+  X("gpu.angle.texture_metrics")                                         \
   X("gpu.capture")                                                       \
   X("headless")                                                          \
   /* Traces for //components/history. */                                 \
