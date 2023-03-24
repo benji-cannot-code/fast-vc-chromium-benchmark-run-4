565372edd3ba65d50e3f3e42d0974c8e606e670c
Andrew Paseltiner <apaseltiner@chromium.org>
2023-03-24 03:51:21 +0000

    Allow embedders to selectively enable attribution registrations for web and OS
    We introduce kOsSource and kOsTrigger to the
    AttributionReportingOperation enum to do so.
    
    Bug: 1427173, 1420704
    Disable-Rts: True
    Change-Id: I0525de9c6b19f123931f94ce6bb51456d3907ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366403
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121527}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3334,7 +3334,8 @@ bool ChromeContentBrowserClient::IsAttributionReportingOperationAllowed(
     return false;
 
   switch (operation) {
-    case AttributionReportingOperation::kSource: {
+    case AttributionReportingOperation::kSource:
+    case AttributionReportingOperation::kOsSource: {
       DCHECK(source_origin);
       DCHECK(reporting_origin);
       bool allowed = privacy_sandbox_settings->IsAttributionReportingAllowed(
@@ -3352,7 +3353,8 @@ bool ChromeContentBrowserClient::IsAttributionReportingOperationAllowed(
       DCHECK(reporting_origin);
       return privacy_sandbox_settings->IsAttributionReportingAllowed(
           *source_origin, *reporting_origin);
-    case AttributionReportingOperation::kTrigger: {
+    case AttributionReportingOperation::kTrigger:
+    case AttributionReportingOperation::kOsTrigger: {
       DCHECK(destination_origin);
       DCHECK(reporting_origin);
       bool allowed = privacy_sandbox_settings->IsAttributionReportingAllowed(
