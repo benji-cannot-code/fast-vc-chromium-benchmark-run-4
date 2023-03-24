515c13b00587db720505bc0d03332c287cc1d807
jchen10 <jie.a.chen@intel.com>
2023-03-24 02:28:51 +0000

    Add ShouldDrawWithBlending to root render pass
    This optimizes SurfaceAggregator to only add the color conversion
    pass if the root render pass also requires blending. Without this,
    when the display color space is set to HDR10(PQ) for external HDR
    minitors on ChromeOS, there will be such a color conversion pass being
    added, which doesn't favor video overlay promotion.
    
    Bug: 1395716
    Change-Id: Ied66e0b8bd2b7eeaa7a2f3cd558aa150ef7c571d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4351070
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Jie A Chen <jie.a.chen@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1121514}

--- components/viz/common/quads/aggregated_render_pass.cc
+++ components/viz/common/quads/aggregated_render_pass.cc
@@ -211,6 +211,15 @@ std::unique_ptr<AggregatedRenderPass> AggregatedRenderPass::DeepCopy() const {
   return copy_pass;
 }
 
+bool AggregatedRenderPass::ShouldDrawWithBlending() const {
+  for (auto* quad : quad_list) {
+    if (quad->ShouldDrawWithBlending()) {
+      return true;
+    }
+  }
+  return false;
+}
+
 void AggregatedRenderPass::AsValueInto(
     base::trace_event::TracedValue* value) const {
   RenderPassInternal::AsValueInto(value);
--- components/viz/common/quads/aggregated_render_pass.h
+++ components/viz/common/quads/aggregated_render_pass.h
@@ -90,6 +90,9 @@ class VIZ_COMMON_EXPORT AggregatedRenderPass : public RenderPassInternal {
     return quad_list.AllocateAndConstruct<DrawQuadType>();
   }
 
+  // Indicates if any its quad needs to draw with blending.
+  bool ShouldDrawWithBlending() const;
+
   // Uniquely identifies the render pass in the aggregated frame.
   AggregatedRenderPassId id;
 
