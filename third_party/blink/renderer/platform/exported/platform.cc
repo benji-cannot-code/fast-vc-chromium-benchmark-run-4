f94177e67bd258a0a5896f358c0d306d8ea61c4d
Scott Haseley <shaseley@chromium.org>
2023-03-24 19:24:34 +0000

    Move heap initialization to InitializeBlink()
    The main thread scheduler cannot create GCed objects before the heap
    is initialized, which is preventing moving more parts of the scheduler
    to oilpan. This moves heap init to InitializeBlink() (called
    before the main thread scheduler is created) to fix this.
    
    Change-Id: I02a888e09516af37bbc9835d4aa1f66ea2e9e4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335762
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121870}

--- third_party/blink/renderer/platform/exported/platform.cc
+++ third_party/blink/renderer/platform/exported/platform.cc
@@ -195,6 +195,8 @@ void Platform::InitializeBlink() {
   DCHECK(!did_initialize_blink_);
   WTF::Partitions::Initialize();
   WTF::Initialize();
+  ProcessHeap::Init();
+  ThreadState::AttachMainThread();
   did_initialize_blink_ = true;
 }
 
@@ -222,9 +224,8 @@ void Platform::InitializeMainThreadCommon(
   DCHECK(did_initialize_blink_);
   MainThread::SetMainThread(std::move(main_thread));
 
-  ProcessHeap::Init();
-
-  ThreadState* thread_state = ThreadState::AttachMainThread();
+  ThreadState* thread_state = ThreadState::Current();
+  CHECK(thread_state->IsMainThread());
   new BlinkGCMemoryDumpProvider(
       thread_state, base::SingleThreadTaskRunner::GetCurrentDefault(),
       BlinkGCMemoryDumpProvider::HeapType::kBlinkMainThread);
