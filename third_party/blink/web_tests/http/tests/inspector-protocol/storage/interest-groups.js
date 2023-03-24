ea7f47b687bc57003620c24d9de4fb15fa3b1277
Garrett Tanzer <gtanzer@chromium.org>
2023-03-24 13:50:10 +0000

    Fenced frames: Remove mode attribute [5/N]
    Remove vestigial mode setters in WPT, and update the generator_api as
    needed to stay as close to the original test case as possible.
    
    Bug: 1347953
    Change-Id: I98f64399cc0e4132356e2562a83dddb790552960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4362864
    Commit-Queue: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121653}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/interest-groups.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/interest-groups.js
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             interestGroupBuyers: ["${baseOrigin}"]});
 
         const fencedFrame = document.createElement("fencedframe");
-        fencedFrame.mode = "opaque-ads";
         fencedFrame.src = url;
         document.body.appendChild(fencedFrame);
       })();`;
