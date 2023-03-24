4a286f824807449fe839c780e2d92e1ff625b7cc
Ed Chin <edchin@google.com>
2023-03-24 22:41:27 +0000

    [ios] Fix refresh logic re: feed visibility
    App close refresh should not happen if the feed is visible while
    the app is backgrounding.
    
    The current logic is flawed because the value of `isFeedVisible` is
    not correct. It is intended to keep the state of the feed visibility
    toggle in the feed header menu. But it is not correctly initialized
    inside FeedMetricsRecorder.
    
    Because of the incorrect `isFeedVisible` value, app close refresh
    is always incorrectly refreshing.
    
    It is good enough to simply check isNTPVisible.
    
    
    This CL is part of a multi-step CL. After this CL:
    https://crrev.com/i/5654370
    https://crrev.com/c/4370625
    
    bug: 1427640
    Change-Id: I8b1f2c7931034882909b58ccdaff5e6a9f1eb7f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368210
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121980}

--- ios/chrome/browser/ui/ntp/metrics/feed_metrics_recorder.mm
+++ ios/chrome/browser/ui/ntp/metrics/feed_metrics_recorder.mm
@@ -88,9 +88,6 @@ using feed::FeedUserActionType;
 // YES if the NTP is visible.
 @property(nonatomic, assign) BOOL isNTPVisible;
 
-// YES if the feed is toggled on in the feed header menu.
-@property(nonatomic, assign) BOOL isFeedVisible;
-
 @end
 
 @implementation FeedMetricsRecorder
@@ -305,7 +302,6 @@ using feed::FeedUserActionType;
 }
 
 - (void)recordDiscoverFeedVisibilityChanged:(BOOL)visible {
-  self.isFeedVisible = visible;
   if (visible) {
     [self
         recordDiscoverFeedUserActionHistogram:FeedUserActionType::kTappedTurnOn
@@ -648,7 +644,8 @@ using feed::FeedUserActionType;
 #pragma mark - FeedRefreshStateTracker
 
 - (BOOL)isNTPAndFeedVisible {
-  return self.isNTPVisible && self.isFeedVisible;
+  // This method is deprecated and usage is replaced with `isNTPVisible`.
+  return self.isNTPVisible;
 }
 
 #pragma mark - Follow
