84a09433e54391277a6d105ba21c7618a17b5225
Jason Thai <jasontt@chromium.org>
2023-03-24 15:15:32 +0000

    wallpaper: add IMAGE_TYPE_PREVIEW_MODE
    Add this new type to support preview images for online wallpapers.
    
    BUG=b/273615271
    TEST=ash_unittests --gtest_filter="WallpaperOnlineVariantUtilsTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: If66bde731181c97f8229b149f03624be55481b5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367006
    Reviewed-by: Eric Sum <esum@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121698}

--- ash/webui/personalization_app/mojom/personalization_app_mojom_traits.cc
+++ ash/webui/personalization_app/mojom/personalization_app_mojom_traits.cc
@@ -159,6 +159,8 @@ EnumTraits<MojomOnlineImageType, ::backdrop::Image::ImageType>::ToMojom(
       return MojomOnlineImageType::kMorning;
     case ::backdrop::Image::IMAGE_TYPE_LATE_AFTERNOON_MODE:
       return MojomOnlineImageType::kLateAfternoon;
+    case ::backdrop::Image::IMAGE_TYPE_PREVIEW_MODE:
+      return MojomOnlineImageType::kPreview;
   }
 }
 
@@ -180,6 +182,9 @@ bool EnumTraits<MojomOnlineImageType, ::backdrop::Image::ImageType>::FromMojom(
       return true;
     case MojomOnlineImageType::kLateAfternoon:
       *output = ::backdrop::Image::IMAGE_TYPE_LATE_AFTERNOON_MODE;
+      return true;
+    case MojomOnlineImageType::kPreview:
+      *output = ::backdrop::Image::IMAGE_TYPE_PREVIEW_MODE;
       return true;
   }
   NOTREACHED();
