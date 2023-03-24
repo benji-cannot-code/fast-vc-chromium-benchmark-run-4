4b8c48da71640c5e3d80cadb7379b6333e614c34
Nicolas Dossou-Gbété <dgn@chromium.org>
2023-03-24 14:31:02 +0000

    Revert "Lacros: Remove deprecated `device_account_gaia_id` field"
    This reverts commit 99226aed9bc95ba877f3e1d03f9b8b52d233cb3d.
    
    Reason for revert: Suspected culprit for crbug.com/1427415
    
    Original change's description:
    > Lacros: Remove deprecated `device_account_gaia_id` field
    >
    > `device_account_gaia_id` crosapi Mojo field was deprecated in M91.
    > Remove it.
    >
    > Fixed: b/260706267
    > Test: Compilation succeeds
    > Change-Id: I1ae2a09d29a8a034ad65b4a995c2f1daf55564c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366118
    > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > Auto-Submit: Kush Sinha <sinhak@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1121528}
    
    Change-Id: I1c8c70138b249db5ac032c1050662275f5e97017
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370145
    Owners-Override: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121672}

--- chromeos/startup/browser_params_proxy.h
+++ chromeos/startup/browser_params_proxy.h
@@ -35,6 +35,8 @@ class COMPONENT_EXPORT(CHROMEOS_STARTUP) BrowserParamsProxy {
 
   const crosapi::mojom::DefaultPathsPtr& DefaultPaths() const;
 
+  const absl::optional<std::string>& DeviceAccountGaiaId() const;
+
   crosapi::mojom::MetricsReportingManaged AshMetricsManaged() const;
 
   crosapi::mojom::ExoImeSupport ExoImeSupport() const;
