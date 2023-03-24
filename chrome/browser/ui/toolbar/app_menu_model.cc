153dcbe6befe669584e186a2dbfefe459436fed3
Viktor Semeniuk <vsemeniuk@google.com>
2023-03-24 22:36:55 +0000

    [Password Manager] Displaying Password Manager menu item
    This CL adds Password Manager menu item to the toolbar if the feature
    is enabled and it's not off-record profile.
    
    Mock: https://docs.google.com/presentation/d/1uuFSa-AeuV5yHzY22-DfPwfUVC7xPPQFR1YZtW-Dldo/edit?resourcekey=0-xHwhkZTV2t5Cqxwj7NxHkA#slide=id.g1198acd1012_0_187
    
    Bug: 1416887
    Change-Id: I905c3b9d76d2123ee09d2173a0970422365822f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4328141
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121978}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/dom_distiller/core/dom_distiller_features.h"
 #include "components/dom_distiller/core/url_utils.h"
 #include "components/feature_engagement/public/event_constants.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/performance_manager/public/features.h"
 #include "components/prefs/pref_service.h"
 #include "components/profile_metrics/browser_profile_type.h"
@@ -115,6 +116,7 @@ DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(AppMenuModel, kHistoryMenuItem);
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(AppMenuModel, kExtensionsMenuItem);
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(AppMenuModel, kMoreToolsMenuItem);
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(AppMenuModel, kIncognitoMenuItem);
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(AppMenuModel, kPasswordManagerMenuItem);
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(ToolsMenuModel, kPerformanceMenuItem);
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(ExtensionsMenuModel,
                                       kManageExtensionsMenuItem);
@@ -955,6 +957,13 @@ void AppMenuModel::Build() {
                            bookmark_sub_menu_model_.get());
     SetElementIdentifierAt(GetIndexOfCommandId(IDC_BOOKMARKS_MENU).value(),
                            kBookmarksMenuItem);
+  }
+  if (!browser_->profile()->IsOffTheRecord() &&
+      base::FeatureList::IsEnabled(
+          password_manager::features::kPasswordManagerRedesign)) {
+    AddItemWithStringId(IDC_VIEW_PASSWORDS, IDS_VIEW_PASSWORDS);
+    SetElementIdentifierAt(GetIndexOfCommandId(IDC_VIEW_PASSWORDS).value(),
+                           kPasswordManagerMenuItem);
   }
 
   if (base::FeatureList::IsEnabled(features::kExtensionsMenuInAppMenu)) {
--- chrome/browser/ui/toolbar/app_menu_model_unittest.cc
+++ chrome/browser/ui/toolbar/app_menu_model_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/menu_model_test.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/performance_manager/public/features.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -378,3 +379,38 @@ TEST_F(AppMenuModelTest, DisableSettingsItem) {
 }
 
 #endif  // BUILDFLAG(IS_CHROMEOS)
+
+class AppMenuModelPasswordManagerTest
+    : public AppMenuModelTest,
+      public testing::WithParamInterface<bool> {
+ public:
+  AppMenuModelPasswordManagerTest() {
+    feature_list_.InitWithFeatureState(
+        password_manager::features::kPasswordManagerRedesign, GetParam());
+  }
+
+  AppMenuModelPasswordManagerTest(const AppMenuModelPasswordManagerTest&) =
+      delete;
+  AppMenuModelPasswordManagerTest& operator=(
+      const AppMenuModelPasswordManagerTest&) = delete;
+
+  ~AppMenuModelPasswordManagerTest() override = default;
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    AppMenuModelPasswordManagerTest,
+    /* features::kPasswordManagerRedesign enabled */ testing::Bool());
+
+TEST_P(AppMenuModelPasswordManagerTest, NewPasswordManagerHasMenuEntry) {
+  AppMenuModel model(this, browser());
+  model.Init();
+  auto index = model.GetIndexOfCommandId(IDC_VIEW_PASSWORDS);
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kPasswordManagerRedesign)) {
+    ASSERT_TRUE(index.has_value());
+    EXPECT_TRUE(model.IsVisibleAt(index.value()));
+  } else {
+    EXPECT_FALSE(index.has_value());
+  }
+}
