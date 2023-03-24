b3f73c293b21e40e7a88c10080b19ccb2c3a89fe
Kush Sinha <sinhak@chromium.org>
2023-03-24 03:54:35 +0000

    Lacros: Remove deprecated `device_account_gaia_id` field
    `device_account_gaia_id` crosapi Mojo field was deprecated in M91.
    Remove it.
    
    Fixed: b/260706267
    Test: Compilation succeeds
    Change-Id: I1ae2a09d29a8a034ad65b4a995c2f1daf55564c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366118
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121528}

--- chromeos/startup/browser_params_proxy.h
+++ chromeos/startup/browser_params_proxy.h
@@ -35,8 +35,6 @@ class COMPONENT_EXPORT(CHROMEOS_STARTUP) BrowserParamsProxy {
 
   const crosapi::mojom::DefaultPathsPtr& DefaultPaths() const;
 
-  const absl::optional<std::string>& DeviceAccountGaiaId() const;
-
   crosapi::mojom::MetricsReportingManaged AshMetricsManaged() const;
 
   crosapi::mojom::ExoImeSupport ExoImeSupport() const;
