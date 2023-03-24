87f679df812b947b46cc0ed03ca4f7d2c0e85e9e
Shu Yang <shuyng@google.com>
2023-03-24 02:25:16 +0000

    [RDSG] Use CurrentLog as annotation mode for Synthetic Trial
    NextLog is the default annotation mode for Synthetic Trial.
    We need CurrentLog to capture all metrics.
    
    Bug: 1427310
    Change-Id: I5b24ad1bec781d1e860a6e6a26dbcc1c87fecc2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368339
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121511}

--- chrome/android/junit/src/org/chromium/chrome/browser/tab/RequestDesktopUtilsUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tab/RequestDesktopUtilsUnitTest.java
@@ -157,7 +157,8 @@ public class RequestDesktopUtilsUnitTest {
         }
 
         @Implementation
-        public static void registerSyntheticFieldTrial(String trialName, String groupName) {
+        public static void registerSyntheticFieldTrial(
+                String trialName, String groupName, int annotationMode) {
             sGlobalDefaultsExperimentTrialName = trialName;
             sGlobalDefaultsExperimentGroupName = groupName;
         }
