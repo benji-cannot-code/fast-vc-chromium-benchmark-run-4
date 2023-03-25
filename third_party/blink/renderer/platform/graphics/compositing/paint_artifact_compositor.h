f468d0590f3ceefa906c9f58e6a53feeaa824d0a
David Awogbemila <awogbemila@chromium.org>
2023-03-25 01:18:18 +0000

    [Scroll Unif.] Filter compositor transform nodes
    Scroll Unification causes transform nodes to be created for all
    scrollers, even unpainted ones. This can cause a violation of a DCHECK
    in compositor_animations.cc which ensures that a given animation either
    has all property nodes on the compositor or none at all.
    
    This patch is to prototype solution given by
    https://bugs.chromium.org/p/chromium/issues/detail?id=1385575#c18
    in which we avoid creating property (transform) nodes for layers which
    are not painted.
    
    Bug: 1385575
    Change-Id: I3eaf7806d1c3019685e58a82f8be97ffc4aab503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117703
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122033}

--- third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
+++ third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_chunk_subset.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_controller.h"
+#include "third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -340,6 +341,10 @@ class PLATFORM_EXPORT PaintArtifactCompositor final
   using PendingLayers = Vector<PendingLayer, 0>;
   class OldPendingLayerMatcher;
   PendingLayers pending_layers_;
+
+  // ScrollTranslationNodes of the PaintArtifact which are painted.
+  // This member variable is only used in PaintArtifactCompositor::Update.
+  HashSet<const TransformPaintPropertyNode*> scroll_translation_nodes_;
 
   friend class StubChromeClientForCAP;
   friend class PaintArtifactCompositorTest;
