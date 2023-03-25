b4e8fd0fdd767b670d1b957d8ed0dfb7bf6f8a61
Sean Kau <skau@chromium.org>
2023-03-25 01:19:12 +0000

    Eliminate assistant_colors_util
    Code is dead. All callers were migrated to ui::ColorProvider.
    
    Change-Id: I7f21f927a792d18d615758083e98c01ec6b98eb6
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368666
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122034}

--- ash/assistant/ui/main_stage/assistant_query_view.cc
+++ ash/assistant/ui/main_stage/assistant_query_view.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/ui/assistant_ui_constants.h"
 #include "ash/assistant/ui/assistant_view_ids.h"
 #include "ash/assistant/ui/colors/assistant_colors.h"
-#include "ash/assistant/ui/colors/assistant_colors_util.h"
 #include "ash/constants/ash_features.h"
 #include "ash/style/ash_color_id.h"
 #include "base/strings/escape.h"
