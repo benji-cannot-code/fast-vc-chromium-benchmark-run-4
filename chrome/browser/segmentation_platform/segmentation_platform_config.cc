315e2538320a12507bd51ca52f777f4a6d4132a1
Ritika Gupta <ritikagup@google.com>
2023-03-25 18:35:55 +0000

    Adding model for device tier segment.
    Change-Id: I80ff6e64835497fdc0ebeb6881e9ea81260b73fa
    Bug: 1426615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366537
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1122116}

--- chrome/browser/segmentation_platform/segmentation_platform_config.cc
+++ chrome/browser/segmentation_platform/segmentation_platform_config.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "components/segmentation_platform/embedder/default_model/cross_device_user_segment.h"
 #include "components/segmentation_platform/embedder/default_model/device_switcher_model.h"
+#include "components/segmentation_platform/embedder/default_model/device_tier_segment.h"
 #include "components/segmentation_platform/embedder/default_model/feed_user_segment.h"
 #include "components/segmentation_platform/embedder/default_model/frequent_feature_user_model.h"
 #include "components/segmentation_platform/embedder/default_model/low_user_engagement_model.h"
@@ -131,6 +132,7 @@ std::vector<std::unique_ptr<Config>> GetSegmentationPlatformConfig(
   configs.emplace_back(IntentionalUserModel::GetConfig());
   configs.emplace_back(PowerUserSegment::GetConfig());
   configs.emplace_back(FrequentFeatureUserModel::GetConfig());
+  configs.emplace_back(DeviceTierSegment::GetConfig());
 #endif
 
   configs.emplace_back(LowUserEngagementModel::GetConfig());
