091797e88b8b720d8f787d42e65d488b950b3930
Sorin Jianu <sorin@chromium.org>
2023-03-25 02:09:51 +0000

    update_client: RequestSender logs response_body instead of the error
    Fixed: 1427733
    Change-Id: I7c666bf2a26074b99f6ad95c13c4886363581d6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4371253
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122046}

--- components/update_client/request_sender.cc
+++ components/update_client/request_sender.cc
@@ -126,6 +126,7 @@ void RequestSender::SendInternalComplete(
     const std::string& response_cup_server_proof,
     int retry_after_sec) {
   VLOG(2) << "Omaha response received: " << response_body;
+  VLOG_IF(2, error) << "Omaha send error: " << error;
 
   if (!error) {
     if (!use_signing_) {
@@ -160,7 +161,6 @@ void RequestSender::SendInternalComplete(
     return;
   }
 
-  VLOG(2) << "Omaha send error: " << response_body;
   HandleSendError(error, retry_after_sec);
 }
 
