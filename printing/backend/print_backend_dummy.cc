dc4ad65408304eb53578b377c1847fa73b557b0f
Lei Zhang <thestig@chromium.org>
2023-03-25 00:57:33 +0000

    Remove PrintBackend::GetPrinterCapsAndDefaults() from most platforms
    GetPrinterCapsAndDefaults() has no callers. Save the implementation in
    PrintBackendWin because that one looks useful.
    
    The PrintBackendCUPS version does have an internal caller. Simplify this
    version and rename it to GetPrinterCapabilities().
    
    Change-Id: I5475cfe01d1c5994e3937a250d4349b11c356c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368803
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122029}

--- printing/backend/print_backend_dummy.cc
+++ printing/backend/print_backend_dummy.cc
@@ -42,12 +42,6 @@ class DummyPrintBackend : public PrintBackend {
     return mojom::ResultCode::kFailed;
   }
 
-  mojom::ResultCode GetPrinterCapsAndDefaults(
-      const std::string& printer_name,
-      PrinterCapsAndDefaults* printer_info) override {
-    return mojom::ResultCode::kFailed;
-  }
-
   std::string GetPrinterDriverInfo(const std::string& printer_name) override {
     return std::string();
   }
