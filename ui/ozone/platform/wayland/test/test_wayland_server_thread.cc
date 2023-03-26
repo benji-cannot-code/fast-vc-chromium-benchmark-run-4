335a66c6ca3abc2ce46fc40cca501ed64e42fc55
Alex Yang <aycyang@chromium.org>
2023-03-26 00:27:14 +0000

    ozone: Send CanComposeInline to Wayland server
    Inline composition is an IME feature for certain languages (e.g. CJK)
    which displays the uncommitted composition inside the input field as it
    is being typed.
    
    For input fields that do not support inline composition (e.g. PDF input
    fields), Lacros did not communicate this information to Exo and Exo
    assumed it was always supported. The result is that users of CJK IME
    cannot see what they are typing until they have committed it.
    
    In a previous commit, we extended the Wayland text input protocol to
    make it possible for Lacros to communicate this information to Exo. In
    this commit, we update Lacros to start using the new API. When Ash is
    made aware that inline composition is not supported, it displays the
    composition in an external IME dialog.
    
    Bug: 1266614
    Change-Id: I637456e9d6da9c954dd2c67121784c298548096d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4292640
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122133}

--- ui/ozone/platform/wayland/test/test_wayland_server_thread.cc
+++ ui/ozone/platform/wayland/test/test_wayland_server_thread.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
 #include "ui/ozone/platform/wayland/test/test_gtk_primary_selection.h"
+#include "ui/ozone/platform/wayland/test/test_zcr_text_input_extension.h"
 #include "ui/ozone/platform/wayland/test/test_zwp_primary_selection.h"
 
 namespace wl {
@@ -135,8 +136,21 @@ bool TestWaylandServerThread::Start(const ServerConfig& config) {
 
   if (!zcr_stylus_.Initialize(display_.get()))
     return false;
-  if (!zcr_text_input_extension_v1_.Initialize(display_.get()))
+
+  switch (config.text_input_extension_version) {
+    case TextInputExtensionVersion::kV7:
+      zcr_text_input_extension_v1_ =
+          std::make_unique<TestZcrTextInputExtensionV1>(7);
+      break;
+    case TextInputExtensionVersion::kV8:
+      zcr_text_input_extension_v1_ =
+          std::make_unique<TestZcrTextInputExtensionV1>(8);
+      break;
+  }
+  if (!zcr_text_input_extension_v1_->Initialize(display_.get())) {
     return false;
+  }
+
   if (!zwp_text_input_manager_v1_.Initialize(display_.get()))
     return false;
   if (!SetupExplicitSynchronizationProtocol(
