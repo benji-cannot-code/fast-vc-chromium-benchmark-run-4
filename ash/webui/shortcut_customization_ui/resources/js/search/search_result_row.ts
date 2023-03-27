c7408041058debc358ecf48d72a043b71bb16eb9
Cam Bickel <cambickel@google.com>
2023-03-27 17:02:25 +0000

    shortcuts: Update search components to support keyboard navigation
    This CL is primarily a behavioral change, besides a few visual changes
    to support hover and selected states on the search results.
    
    This CL adds the concept of a "selected" item to the Shortcuts App's
    search components. When the search box is focused, the user can press
    the up and down arrow keys to select the next / previous search result.
    
    This CL also adds some minor functional improvements such as selecting
    all query text when the user re-focuses on the search box, and restoring
    the previous search results when the user re-focuses on the search box
    if the query isn't blank.
    
    This CL also adds tests to ensure that the behavior above works as
    expected.
    
    Note that much of the functionality in this change comes from the
    Settings app implementation.
    
    Screenshot showing hover and selected states:
    https://screenshot.googleplex.com/5RtfjjmAKE4HbRw
    No search results:
    https://screenshot.googleplex.com/5jLi4EDMQZkXxGn
    
    Test: browser_tests gtest_filter=ShortcutCustomizationAppSearchBox*
    Bug: b:266085562
    Change-Id: Iad9e5bc5b2f6db97b57139bb5d86425d8abca9ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357384
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122481}

--- ash/webui/shortcut_customization_ui/resources/js/search/search_result_row.ts
+++ ash/webui/shortcut_customization_ui/resources/js/search/search_result_row.ts
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+
+import {FocusRowMixin} from 'chrome://resources/cr_elements/focus_row_mixin.js';
 import {PolymerElementProperties} from 'chrome://resources/polymer/v3_0/polymer/interfaces.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {MojoSearchResult} from 'js/shortcut_types.js';
@@ -15,7 +18,9 @@ import {getTemplate} from './search_result_row.html.js';
  * 'search-result-row' is the container for one search result.
  */
 
-export class SearchResultRowElement extends PolymerElement {
+const SearchResultRowElementBase = FocusRowMixin(PolymerElement);
+
+export class SearchResultRowElement extends SearchResultRowElementBase {
   static get is(): string {
     return 'search-result-row';
   }
@@ -26,10 +31,17 @@ export class SearchResultRowElement extends PolymerElement {
       searchResult: {
         type: Object,
       },
+
+      /** Whether the search result row is selected. */
+      selected: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
     };
   }
 
   searchResult: MojoSearchResult;
+  selected: boolean;
 
   private getSearchResultText(): string {
     return mojoString16ToString(
