89e1f3511cfe7800d3ca3f36f4d7a3f2d5c6dcea
Scott Violet <sky@chromium.org>
2023-03-27 18:38:43 +0000

    sessions: ensure timestamp of file always increases
    This patch has a couple of things:
    . minor clean up to move state related to open file into a
      struct. This way, it's easier to ensure everything is
      cleared at the same time.
    . Ensures the timestamp part of the filename always increases.
      This way there isn't the possibility of clobbering an existing
      file if the clock happens to go backwards.
    . Moves checking for "timestamp <=" after checking timestamp
      of last session file. This is again to ensure timestamp of
      file always increases.
    
    I suspect it's a combination of 2/3 that is resulting in the
    DCHECK firing referenced in the bug.
    
    Bug: 1426757
    Change-Id: Ie2f86f0fa77732dd989be0999a7424b1a61507fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370374
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122558}

--- components/sessions/core/command_storage_backend.h
+++ components/sessions/core/command_storage_backend.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace base {
+class Clock;
 class File;
 }
 
@@ -90,7 +91,8 @@ class SESSIONS_EXPORT CommandStorageBackend
       scoped_refptr<base::SequencedTaskRunner> owning_task_runner,
       const base::FilePath& path,
       CommandStorageManager::SessionType type,
-      const std::vector<uint8_t>& decryption_key = {});
+      const std::vector<uint8_t>& decryption_key = {},
+      base::Clock* clock = nullptr);
   CommandStorageBackend(const CommandStorageBackend&) = delete;
   CommandStorageBackend& operator=(const CommandStorageBackend&) = delete;
 
@@ -98,7 +100,14 @@ class SESSIONS_EXPORT CommandStorageBackend
   static bool IsValidFile(const base::FilePath& path);
 
   // Returns the path the files are being written to.
-  const base::FilePath& current_path() const { return current_path_; }
+  const base::FilePath current_path() const {
+    return open_file_ ? open_file_->path : base::FilePath();
+  }
+
+  bool IsFileOpen() const {
+    return open_file_.get() != nullptr;
+    ;
+  }
 
   base::SequencedTaskRunner* owning_task_runner() {
     return base::RefCountedDeleteOnSequence<
@@ -151,6 +160,16 @@ class SESSIONS_EXPORT CommandStorageBackend
   struct SessionInfo {
     base::FilePath path;
     base::Time timestamp;
+  };
+
+  struct OpenFile {
+    OpenFile();
+    ~OpenFile();
+
+    base::FilePath path;
+    std::unique_ptr<base::File> file;
+    // Set to true once `kInitialStateMarkerCommandId` is written.
+    bool did_write_marker = false;
   };
 
   ~CommandStorageBackend();
@@ -244,11 +263,10 @@ class SESSIONS_EXPORT CommandStorageBackend
   // TaskRunner that the callback is added to.
   scoped_refptr<base::SequencedTaskRunner> callback_task_runner_;
 
-  // Path commands are currently being saved to.
-  base::FilePath current_path_;
+  raw_ptr<base::Clock> clock_;
 
-  // This may be null, created as necessary.
-  std::unique_ptr<base::File> file_;
+  // File and path commands are being written.
+  std::unique_ptr<OpenFile> open_file_;
 
   // Whether DoInit() was called. DoInit() is called on the background task
   // runner.
@@ -259,9 +277,6 @@ class SESSIONS_EXPORT CommandStorageBackend
 
   // Incremented every time a command is written.
   int commands_written_ = 0;
-
-  // Set to true once `kInitialStateMarkerCommandId` is written.
-  bool did_write_marker_ = false;
 
   // Timestamp when this session was started.
   base::Time timestamp_;
