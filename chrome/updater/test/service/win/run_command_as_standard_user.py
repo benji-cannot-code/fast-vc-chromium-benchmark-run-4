7d90c89bc8e6ead059090e56b670251994d457c0
Chenlin Fan <fancl@chromium.org>
2023-03-27 17:17:17 +0000

    Use explicit python_version: "3.8".
    Wheels are only supposed to work with a specific minor version of python.
    
    Bug: 1415212
    Change-Id: I2b7fee674e41e7d1499753d9836feb3c2c20c153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368384
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: Chenlin Fan <fancl@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122499}

--- chrome/updater/test/service/win/run_command_as_standard_user.py
+++ chrome/updater/test/service/win/run_command_as_standard_user.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 # [VPYTHON:BEGIN]
-# python_version: "3"
+# python_version: "3.8"
 # wheel: <
 #   name: "infra/python/wheels/pywin32/${vpython_platform}"
 #    version: "version:300"
