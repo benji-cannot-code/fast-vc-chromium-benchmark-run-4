15914baa3c5ba520d680c8db4cbc6864a42936e5
kylechar <kylechar@chromium.org>
2023-03-27 19:50:57 +0000

    Clarify remapped_resources with comment
    Bug: none
    Change-Id: Ifaf154931ece368794fbf73391ab2b95ae3cb657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4362521
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122597}

--- components/viz/service/display/resolved_frame_data.cc
+++ components/viz/service/display/resolved_frame_data.cc
@@ -167,6 +167,9 @@ void ResolvedFrameData::UpdateForActiveFrame(
         }
 
         referenced_resources.push_back(resource_id);
+
+        // Update `ResolvedQuadData::remapped_resources` to have the remapped
+        // display resource_id.
         resource_id = iter->second;
       }
     }
