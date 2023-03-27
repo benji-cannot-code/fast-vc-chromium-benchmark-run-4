85b42df3b7b205c8982f76cf7ef2848fdc6a2ba4
Jiaming Cheng <jiamingc@chromium.org>
2023-03-27 06:50:10 +0000

    Should check if to show a11y tile first then set it's visibility
    Also changed the controller unittest to parameterized test to cover
    the revamped view.
    
    Bug: b/274160215
    Change-Id: I041fed4520de21880f09c52e6f8536d551e53308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368160
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122277}

--- ash/system/accessibility/accessibility_feature_pod_controller.cc
+++ ash/system/accessibility/accessibility_feature_pod_controller.cc
@@ -44,8 +44,9 @@ FeaturePodButton* AccessibilityFeaturePodController::CreateButton() {
                        login_status == LoginStatus::LOCKED ||
                        delegate->ShouldShowAccessibilityMenu();
   button->SetVisible(visible);
-  if (visible)
+  if (visible) {
     TrackVisibilityUMA();
+  }
 
   return button;
 }
@@ -69,6 +70,17 @@ std::unique_ptr<FeatureTile> AccessibilityFeaturePodController::CreateTile(
       base::BindRepeating(&FeaturePodControllerBase::OnLabelPressed,
                           weak_ptr_factory_.GetWeakPtr()),
       tooltip_text);
+
+  AccessibilityDelegate* delegate = Shell::Get()->accessibility_delegate();
+  LoginStatus login_status = Shell::Get()->session_controller()->login_status();
+  const bool visible = login_status == LoginStatus::NOT_LOGGED_IN ||
+                       login_status == LoginStatus::LOCKED ||
+                       delegate->ShouldShowAccessibilityMenu();
+  feature_tile->SetVisible(visible);
+  if (visible) {
+    TrackVisibilityUMA();
+  }
+
   return feature_tile;
 }
 
