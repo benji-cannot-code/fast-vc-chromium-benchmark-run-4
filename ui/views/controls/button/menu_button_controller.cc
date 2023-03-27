f7c762e030cf77ee669e772dce8b99622754422f
Joanmarie Diggs <jdiggs@igalia.com>
2023-03-27 22:05:45 +0000

    Use SetAccessibilityProperties in more views/controls
    This is a preliminary step to migrating views to the new accessibility
    property setters.
    
    Views that are a common base class to many other views now set their
    accessible role via View::SetAccessibilityProperties rather than waiting
    for it to be requested in GetAccessibleNodeData. In some cases this
    rendered their GetAccessibleNodeData unnecessary. In cases where it did
    not, those views' GetAccessibleNodeData now call the parent class's to
    ensure properties set via the setters are included in AXNodeData.
    
    AX-Relnotes: n/a
    
    Bug: 1406781, 1363612
    Change-Id: Id09e08dd2814df8bcbdf96b171163728ac6af960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369851
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1122683}

--- ui/views/controls/button/menu_button_controller.cc
+++ ui/views/controls/button/menu_button_controller.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "ui/accessibility/ax_enums.mojom.h"
-#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/events/event_constants.h"
@@ -176,13 +175,6 @@ bool MenuButtonController::OnKeyPressed(const ui::KeyEvent& event) {
 bool MenuButtonController::OnKeyReleased(const ui::KeyEvent& event) {
   // A MenuButton always activates the menu on key press.
   return false;
-}
-
-void MenuButtonController::UpdateAccessibleNodeData(ui::AXNodeData* node_data) {
-  node_data->role = ax::mojom::Role::kPopUpButton;
-  node_data->SetHasPopup(ax::mojom::HasPopup::kMenu);
-  if (button()->GetEnabled())
-    node_data->SetDefaultActionVerb(ax::mojom::DefaultActionVerb::kOpen);
 }
 
 bool MenuButtonController::IsTriggerableEvent(const ui::Event& event) {
