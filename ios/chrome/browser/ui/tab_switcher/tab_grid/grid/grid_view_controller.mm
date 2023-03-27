84f23e7debce3bcd284f2736ef4822fb95c50abb
Petro Akzhygitov <pakzhygitov@google.com>
2023-03-27 09:36:30 +0000

    Reverting call's incorrect scope change in the `GridViewController`.
    The closing bracket was incorrectly removed here https://chromium-review.googlesource.com/c/chromium/src/+/4336478/comment/bee3d1b8_ecaf46cc/ which caused the call to change it's scope. This CL revert it back.
    
    Fixed: 1427958
    Change-Id: Idcf2715de42852af7e2eb847781ad229502c3d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4372283
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1122319}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -408,8 +408,9 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
                                 animated:NO
                           scrollPosition:UICollectionViewScrollPositionNone];
     [self updateFractionVisibleOfLastItem];
+
+    self.searchText = nil;
   }
-  self.searchText = nil;
 }
 
 - (void)setSearchText:(NSString*)searchText {
