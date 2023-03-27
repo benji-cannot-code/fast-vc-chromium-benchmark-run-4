4aefaea45a4d0902c53e7dcb7376e741e2841878
Boris Sazonov <bsazonov@google.com>
2023-03-27 10:04:08 +0000

    Add BookmarkModelTypeController
    Adds BookmarkModelTypeController that is needed to enable bookmark sync
    in the transport mode and to observe the opt-in status (the actual pref
    to control the opt-in status will be added in a subsequent CL).
    
    Bug: 1404250, 1427044
    Change-Id: I50b5f6b2093c94773db5310380c7cfca809719c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4360943
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122327}

--- components/browser_sync/sync_api_component_factory_impl.cc
+++ components/browser_sync/sync_api_component_factory_impl.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/send_tab_to_self/send_tab_to_self_sync_service.h"
 #include "components/sync/base/features.h"
 #include "components/sync/base/legacy_directory_deletion.h"
+#include "components/sync/base/model_type.h"
 #include "components/sync/base/report_unrecoverable_error.h"
 #include "components/sync/base/sync_prefs.h"
 #include "components/sync/driver/data_type_manager_impl.h"
@@ -55,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/invalidations/sync_invalidations_service.h"
 #include "components/sync/model/forwarding_model_type_controller_delegate.h"
 #include "components/sync/model/proxy_model_type_controller_delegate.h"
+#include "components/sync_bookmarks/bookmark_model_type_controller.h"
 #include "components/sync_bookmarks/bookmark_sync_service.h"
 #include "components/sync_device_info/device_info_sync_service.h"
 #include "components/sync_preferences/pref_service_syncable.h"
@@ -328,9 +330,10 @@ SyncApiComponentFactoryImpl::CreateCommonDataTypeControllers(
                         ->GetBookmarkSyncControllerDelegate(favicon_service)
                         .get())
               : nullptr;
-      controllers.push_back(std::make_unique<ModelTypeController>(
-          syncer::BOOKMARKS, std::move(full_mode_delegate),
-          std::move(transport_mode_delegate)));
+      controllers.push_back(
+          std::make_unique<sync_bookmarks::BookmarkModelTypeController>(
+              std::move(full_mode_delegate),
+              std::move(transport_mode_delegate)));
     }
 
     if (!disabled_types.Has(syncer::POWER_BOOKMARK) &&
