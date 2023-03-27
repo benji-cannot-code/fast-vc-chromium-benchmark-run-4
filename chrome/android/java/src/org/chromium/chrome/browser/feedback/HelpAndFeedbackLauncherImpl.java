b9b5cff477dce525fcaa4755a006793634b9b0a9
Ted Choc <tedchoc@chromium.org>
2023-03-27 21:45:24 +0000

    [2/3] Switch HelpAndFeedback to use the delegate as part of de-apphooks-ifying.
    BUG=
    
    Change-Id: I64b0a60d569a4a144682dbf597566033fe8afdf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367529
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122670}

--- chrome/android/java/src/org/chromium/chrome/browser/feedback/HelpAndFeedbackLauncherImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/feedback/HelpAndFeedbackLauncherImpl.java
@@ -39,6 +39,7 @@ public class HelpAndFeedbackLauncherImpl implements HelpAndFeedbackLauncher {
     private static final String TAG = "HelpAndFeedback";
 
     private static ProfileKeyedMap<HelpAndFeedbackLauncher> sProfileToLauncherMap;
+    private final HelpAndFeedbackLauncherDelegate mDelegate;
 
     private Profile mProfile;
 
@@ -62,6 +63,12 @@ public class HelpAndFeedbackLauncherImpl implements HelpAndFeedbackLauncher {
         });
     }
 
+    // TODO(tedchoc): Reduce visibility to private and pass in the Profile once the de-AppHooks
+    //                changes land.
+    public HelpAndFeedbackLauncherImpl() {
+        mDelegate = new HelpAndFeedbackLauncherDelegateImpl();
+    }
+
     /**
      * Starts an activity showing a help page for the specified context ID.
      *
@@ -71,6 +78,7 @@ public class HelpAndFeedbackLauncherImpl implements HelpAndFeedbackLauncher {
      *                    context and will be used to show a more relevant help page.
      * @param collector the {@link FeedbackCollector} to use for extra data. Must not be null.
      */
+    // TODO(tedchoc): Remove once the de-AppHooks changes land.
     protected void show(
             Activity activity, String helpContext, @Nonnull FeedbackCollector collector) {
         Log.d(TAG, "Feedback data: " + collector.getBundle());
@@ -84,6 +92,7 @@ public class HelpAndFeedbackLauncherImpl implements HelpAndFeedbackLauncher {
      *                 screenshot of.
      * @param collector the {@link FeedbackCollector} to use for extra data. Must not be null.
      */
+    // TODO(tedchoc): Remove once the de-AppHooks changes land.
     protected void showFeedback(Activity activity, @Nonnull FeedbackCollector collector) {
         Log.d(TAG, "Feedback data: " + collector.getBundle());
         launchFallbackSupportUri(activity);
@@ -104,7 +113,7 @@ public class HelpAndFeedbackLauncherImpl implements HelpAndFeedbackLauncher {
         new ChromeFeedbackCollector(activity, null /* categoryTag */, null /* description */,
                 new ScreenshotTask(activity),
                 new ChromeFeedbackCollector.InitParams(mProfile, url, helpContext),
-                collector -> show(activity, helpContext, collector), mProfile);
+                collector -> mDelegate.show(activity, helpContext, collector), mProfile);
     }
 
     /**
@@ -129,7 +138,7 @@ public class HelpAndFeedbackLauncherImpl implements HelpAndFeedbackLauncher {
                         -> {
                     RecordHistogram.recordLongTimesHistogram("Feedback.Duration.FormOpenToSubmit",
                             SystemClock.elapsedRealtime() - startTime);
-                    showFeedback(activity, collector);
+                    mDelegate.showFeedback(activity, collector);
                 },
                 mProfile);
     }
@@ -162,7 +171,7 @@ public class HelpAndFeedbackLauncherImpl implements HelpAndFeedbackLauncher {
         new FeedFeedbackCollector(activity, categoryTag, null /* description */,
                 new ScreenshotTask(activity),
                 new FeedFeedbackCollector.InitParams(mProfile, url, feedContext),
-                collector -> showFeedback(activity, collector), mProfile);
+                collector -> mDelegate.showFeedback(activity, collector), mProfile);
     }
 
     /**
