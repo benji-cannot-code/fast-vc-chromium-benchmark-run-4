c4482ff831937711ef96b5e7f9d4366ec2cfd716
Muyao Xu <muyaoxu@google.com>
2023-03-27 23:50:26 +0000

    Record MediaCastMode for starting/stopping casting
    The code to record metrics for starting/stopping casting was removed
    due to a refactor. This CL adds back the metrics to record the type of
    cast sessions started/stopped from GMC as it's used for the finch
    experiment MediaRemotingWithoutFullscreen.
    
    Bug: 1411139
    Change-Id: Ia341e83f6f2ef8e44a7e8f823dd7ab990924cd76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369794
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1122746}

--- chrome/browser/ui/global_media_controls/media_item_ui_metrics.cc
+++ chrome/browser/ui/global_media_controls/media_item_ui_metrics.cc
@@ -33,13 +33,23 @@ GlobalMediaControlsCastMode GetGlobalMediaControlsCastMode(
 
 }  // namespace
 
+void MediaItemUIMetrics::RecordStartCastMode(
+    media_router::MediaCastMode cast_mode) {
+  base::UmaHistogramEnumeration(kStartCastingModeHistogramName,
+                                GetGlobalMediaControlsCastMode(cast_mode));
+}
+void MediaItemUIMetrics::RecordStopCastMode(
+    media_router::MediaCastMode cast_mode) {
+  base::UmaHistogramEnumeration(kStopCastingModeHistogramName,
+                                GetGlobalMediaControlsCastMode(cast_mode));
+}
+
 void MediaItemUIMetrics::RecordStartCastingMetrics(
     media_router::SinkIconType sink_icon_type,
     media_router::MediaCastMode cast_mode,
     GlobalMediaControlsEntryPoint entry_point) {
   MediaRouterMetrics::RecordMediaSinkTypeForGlobalMediaControls(sink_icon_type);
-  base::UmaHistogramEnumeration(kStartCastingModeHistogramName,
-                                GetGlobalMediaControlsCastMode(cast_mode));
+  RecordStartCastMode(cast_mode);
 
   GlobalMediaControlsCastActionAndEntryPoint action;
   switch (entry_point) {
@@ -60,8 +70,7 @@ void MediaItemUIMetrics::RecordStartCastingMetrics(
 void MediaItemUIMetrics::RecordStopCastingMetrics(
     media_router::MediaCastMode cast_mode,
     GlobalMediaControlsEntryPoint entry_point) {
-  base::UmaHistogramEnumeration(kStopCastingModeHistogramName,
-                                GetGlobalMediaControlsCastMode(cast_mode));
+  RecordStopCastMode(cast_mode);
 
   GlobalMediaControlsCastActionAndEntryPoint action;
   switch (entry_point) {
