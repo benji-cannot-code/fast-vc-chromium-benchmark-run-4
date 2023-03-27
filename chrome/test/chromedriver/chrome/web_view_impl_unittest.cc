3184395e6cafb1b3fc4911115ddfa6df98fac8cd
Tom Sepez <tsepez@chromium.org>
2023-03-27 19:58:38 +0000

    Avoid danling out-of-scope reference in CallUserSyncScriptArgs::SetUp()
    The client passed to the WebViewImpl is maintained as a raw_ptr<> for
    the life of the WebViewImpl, so it must outlive it.
    
    -- Drop unowned client reference before destroying the WebViewImpl
       that owns it.
    
    Bug: 1401495
    Change-Id: Ida86f182e367285c3a58fffbb8469f074bba6f51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374824
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122602}

--- chrome/test/chromedriver/chrome/web_view_impl_unittest.cc
+++ chrome/test/chromedriver/chrome/web_view_impl_unittest.cc
@@ -771,7 +771,6 @@ class CallUserSyncScriptArgs
     : public testing::TestWithParam<std::pair<std::string, bool>> {
  public:
   void SetUp() override {
-    BrowserInfo browser_info;
     std::unique_ptr<FakeDevToolsClient> client_uptr =
         std::make_unique<FakeDevToolsClient>("root");
     client_ptr = client_uptr.get();
@@ -784,14 +783,15 @@ class CallUserSyncScriptArgs
   }
 
   void TearDown() override {
-    view.reset();
     client_ptr = nullptr;
+    view.reset();
   }
 
   bool IsW3C() { return GetParam().second; }
 
   std::string ElementKey() { return GetParam().first; }
 
+  BrowserInfo browser_info;
   std::unique_ptr<WebViewImpl> view;
   base::raw_ptr<FakeDevToolsClient> client_ptr;
 };
