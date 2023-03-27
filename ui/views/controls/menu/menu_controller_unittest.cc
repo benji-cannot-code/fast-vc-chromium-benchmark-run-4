3260184e393a57d301a6f329fb313752ae98db7c
Andrew Williams <awillia@chromium.org>
2023-03-27 16:08:03 +0000

    [Sheriff] Disable MenuControllerTest.SubmenuOpenByKey test on Windows
    Test has a long streak of failures on the Win11 Tests x64 bot
    
    Bug: 1428060
    Change-Id: I86ae4fa26f42aa5089f00d9545534a7135c14f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373957
    Auto-Submit: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122446}

--- ui/views/controls/menu/menu_controller_unittest.cc
+++ ui/views/controls/menu/menu_controller_unittest.cc
@@ -3352,7 +3352,12 @@ TEST_F(MenuControllerTest, BrowserHotkeysCancelMenusAndAreRedispatched) {
 }
 #endif
 
-TEST_F(MenuControllerTest, SubmenuOpenByKey) {
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_SubmenuOpenByKey DISABLED_SubmenuOpenByKey
+#else
+#define MAYBE_SubmenuOpenByKey SubmenuOpenByKey
+#endif
+TEST_F(MenuControllerTest, MAYBE_SubmenuOpenByKey) {
   // Create a submenu.
   MenuItemView* const child_menu = menu_item()->GetSubmenu()->GetMenuItemAt(0);
   SubmenuView* sub_menu = child_menu->CreateSubmenu();
