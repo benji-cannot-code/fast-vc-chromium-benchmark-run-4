1de4893f39c7510839f1783f5dbfb2040aa458a4
Ben Franz <bfranz@google.com>
2023-03-27 12:46:55 +0000

    Revert "Refactor network code out of KioskLaunchController"
    This reverts commit 35a426daf047ed2e3dacafd6117f810427cde2e7.
    
    Reason for revert: crbug.com/1428025
    
    Original change's description:
    > Refactor network code out of KioskLaunchController
    >
    > We're removing all network related code out of KioskLaunchController. This requires the state to move into the new class.
    >
    > Bug: b:256596278
    > Change-Id: I438babd0c9adc1b693f5582fc364ca59e6d6f5ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4287226
    > Commit-Queue: Ben Franz <bfranz@chromium.org>
    > Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    > Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    > Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1121585}
    
    Bug: b:256596278
    Change-Id: Ib21ad04875285b1d30c3188d8cf06969479b6fd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369913
    Owners-Override: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122357}

--- chrome/browser/ash/login/app_mode/test/kiosk_base_test.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_base_test.cc
@@ -17,11 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
-#include "base/test/test_future.h"
 #include "base/values.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_manager.h"
 #include "chrome/browser/ash/login/app_mode/kiosk_launch_controller.h"
-#include "chrome/browser/ash/login/app_mode/network_ui_controller.h"
 #include "chrome/browser/ash/login/app_mode/test/kiosk_apps_mixin.h"
 #include "chrome/browser/ash/login/app_mode/test/kiosk_test_helpers.h"
 #include "chrome/browser/ash/login/startup_utils.h"
@@ -188,7 +186,7 @@ void KioskBaseTest::SetUpOnMainThread() {
 void KioskBaseTest::TearDownOnMainThread() {
   owner_settings_service_.reset();
   settings_helper_.RestoreRealDeviceSettingsProvider();
-  NetworkUiController::SetNetworkTimeoutCallbackForTesting(nullptr);
+  KioskLaunchController::SetNetworkTimeoutCallbackForTesting(nullptr);
 
   OobeBaseTest::TearDownOnMainThread();
 }
@@ -326,11 +324,16 @@ void KioskBaseTest::WaitForAppLaunchSuccess() {
 }
 
 void KioskBaseTest::WaitForAppLaunchNetworkTimeout() {
-  base::test::TestFuture<void> network_timeout_future;
-  base::OnceClosure callback = base::BindOnce(
-      &OnNetworkWaitTimedOut, network_timeout_future.GetCallback());
-  NetworkUiController::SetNetworkTimeoutCallbackForTesting(&callback);
-  ASSERT_TRUE(network_timeout_future.Wait());
+  if (GetKioskLaunchController()->network_wait_timedout()) {
+    return;
+  }
+
+  base::RunLoop loop;
+  base::OnceClosure callback =
+      base::BindOnce(&OnNetworkWaitTimedOut, loop.QuitClosure());
+  KioskLaunchController::SetNetworkTimeoutCallbackForTesting(&callback);
+  loop.Run();
+  ASSERT_TRUE(GetKioskLaunchController()->network_wait_timedout());
 }
 
 void KioskBaseTest::RunAppLaunchNetworkDownTest() {
@@ -348,8 +351,7 @@ void KioskBaseTest::RunAppLaunchNetworkDownTest() {
   test::OobeJS().ExpectVisiblePath(kConfigNetwork);
 
   // Configure network should bring up lock screen for owner.
-  GetKioskLaunchController()
-      ->GetNetworkUiControllerForTesting()
+  static_cast<AppLaunchSplashScreenView::Delegate*>(GetKioskLaunchController())
       ->OnConfigureNetwork();
   EXPECT_FALSE(LoginScreenTestApi::IsOobeDialogVisible());
   // There should be only one owner pod on this screen.
