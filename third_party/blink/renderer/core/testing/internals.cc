8f1d745ee7db0b58bcf0e8ff62786121b1a2a19f
Kent Tamura <tkent@chromium.org>
2023-03-27 02:56:23 +0000

    internals: nodesFromRect() should prepare a WindowProxy
    internals.nodesFromRect() can return a Document in a child frame. It did
    not prepare a WindowProxy for the Document, and caused a crash in a
    later WindowProxy access.
    
    This CL has no behavior changes in production.
    
    Bug: 1407555
    Change-Id: I614f5c6c8bc498b03a084a166d7afe022d00233b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368899
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122233}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -2356,6 +2356,7 @@ Vector<AtomicString> Internals::svgTags() {
 }
 
 StaticNodeList* Internals::nodesFromRect(
+    ScriptState* script_state,
     Document* document,
     int x,
     int y,
@@ -2392,6 +2393,13 @@ StaticNodeList* Internals::nodesFromRect(
   HitTestResult result(request, location);
   frame->ContentLayoutObject()->HitTest(location, result);
   HeapVector<Member<Node>> matches(result.ListBasedTestResult());
+
+  // Ensure WindowProxy instances for child frames. crbug.com/1407555.
+  for (auto& node : matches) {
+    if (node->IsDocumentNode() && node.Get() != document) {
+      node->GetDocument().GetFrame()->GetWindowProxy(script_state->World());
+    }
+  }
 
   return StaticNodeList::Adopt(matches);
 }
