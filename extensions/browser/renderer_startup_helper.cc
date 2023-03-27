ae5225944bc07fdff0db22694801ab69637e6fb1
Daniel d'Andrada <dandrader@google.com>
2023-03-27 15:07:33 +0000

    Code health: remove some now unused NotificationService #includes
    Bug: 411569
    Change-Id: I7e9c475179ac5a6387f96be43fb06c9ddc6422b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370271
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Cr-Commit-Position: refs/heads/main@{#1122413}

--- extensions/browser/renderer_startup_helper.cc
+++ extensions/browser/renderer_startup_helper.cc
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/browser_context.h"
-#include "content/public/browser/notification_service.h"
-#include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_process_host.h"
 #include "extensions/browser/extension_function_dispatcher.h"
 #include "extensions/browser/extension_registry.h"
--- extensions/shell/browser/shell_browsertest.cc
+++ extensions/shell/browser/shell_browsertest.cc
@@ -3,11 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/browser/app_window/app_window_registry.h"
-#include "extensions/browser/notification_types.h"
 #include "extensions/shell/test/shell_apitest.h"
 
 #if defined(USE_AURA)
