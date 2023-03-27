66a4b64877d820021fc5b125dfe66511cfb88354
Xiaocheng Hu <xiaochengh@chromium.org>
2023-03-27 21:18:41 +0000

    Disable Event.path on WebView Stable and Beta channels
    The deprecated feature Event.path has been disabled at 50% on WebView
    Stable for two weeks (cl/516338607) without getting bug reports, so it
    should be safe to fully remove.
    
    This patch removes it in M114+ on the code side. A separate CL
    (cl/519776284) will disable it in previous versions via finch.
    
    Bug: 1277431
    Change-Id: I8c14872100a955b98cbe9cc43c6d090f7316af92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374828
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122651}

--- android_webview/lib/aw_main_delegate.cc
+++ android_webview/lib/aw_main_delegate.cc
@@ -71,7 +71,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/blink/public/common/features.h"
-#include "third_party/blink/public/common/switches.h"
 #include "tools/v8_context_snapshot/buildflags.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/base/ui_base_switches.h"
@@ -315,13 +314,6 @@ absl::optional<int> AwMainDelegate::BasicStartupComplete() {
 
     // FedCM is not yet supported on WebView.
     features.DisableIfNotSet(::features::kFedCm);
-  }
-
-  // Enable Event.path on Beta and Stable. The feature has been deprecated and
-  // removed on other platforms, but needs more time on WebView.
-  // See crbug.com/1277431 for more details.
-  if (version_info::android::GetChannel() >= version_info::Channel::BETA) {
-    cl->AppendSwitch(blink::switches::kEventPathEnabledByDefault);
   }
 
   android_webview::RegisterPathProvider();
