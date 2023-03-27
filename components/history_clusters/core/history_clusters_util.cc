77510e5b64cf1df325e03d618cca76628f115d72
Tommy C. Li <tommycli@chromium.org>
2023-03-27 22:25:25 +0000

    [journeys] Delete Action Chip on URLs feature flag and implementation
    We don't plan on experimenting again with this any time soon.
    
    Bug: 1314058
    Change-Id: Ibbf3bee591b3308a624753f190704d829b7675d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347853
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122694}

--- components/history_clusters/core/history_clusters_util.cc
+++ components/history_clusters/core/history_clusters_util.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/history/core/browser/history_types.h"
-#include "components/history/core/browser/visitsegment_database.h"
 #include "components/history_clusters/core/config.h"
 #include "components/history_clusters/core/features.h"
 #include "components/history_clusters/core/history_clusters_types.h"
@@ -166,11 +165,6 @@ GURL ComputeURLForDeduping(const GURL& url) {
 
   url_for_deduping = url_for_deduping.ReplaceComponents(replacements);
   return url_for_deduping;
-}
-
-std::string ComputeURLKeywordForLookup(const GURL& url) {
-  return history::VisitSegmentDatabase::ComputeSegmentName(
-      ComputeURLForDeduping(url));
 }
 
 std::u16string ComputeURLForDisplay(const GURL& url, bool trim_after_host) {
