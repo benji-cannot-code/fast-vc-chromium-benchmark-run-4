0bb21014e749a6f8dc4d52f9371b070eb11c0116
David Bokan <bokan@chromium.org>
2023-03-27 19:45:14 +0000

    Unexpire and mark obsolete viewport histograms
    This CL unexpires Viewport.MetaTagType which is still useful.
    
    It marks Viewport.PageScaleFactorRange and Viewport.BooleanDidScalePage
    as obsolete as they're expired and no longer useful.
    
    Bug: 1353024
    Change-Id: Ie5441fe0d24645e86f2c24b65b2ccfa033f6bb57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368138
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122587}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -2681,10 +2681,6 @@ WebInputEventResult WebFrameWidgetImpl::HandleInputEvent(
   if (IsProvisional())
     return WebInputEventResult::kHandledSuppressed;
 
-  // Only record metrics for the root frame.
-  if (ForTopMostMainFrame())
-    GetPage()->GetVisualViewport().StartTrackingPinchStats();
-
   // If a drag-and-drop operation is in progress, ignore input events except
   // PointerCancel and GestureLongPress.
   if (doing_drag_and_drop_ &&
