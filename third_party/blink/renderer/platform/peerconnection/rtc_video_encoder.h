b7bd7d549900c9832cee229f19a4b509fe06613b
Hirokazu Honda <hiroh@chromium.org>
2023-03-27 14:24:10 +0000

    RTCVideoEncoder: Factorize encoder initialization
    This CL factorize the code of RTCVideoEncoder of calling
    Impl::CreateAndInitializeVEA().
    
    Bug: None
    Test: blink_platform_unittests
    Change-Id: I6e21f23341324a0bb6469b9259f3621465abb88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4354847
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122380}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/synchronization/lock.h"
 #include "media/base/video_decoder_config.h"
+#include "media/video/video_encode_accelerator.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/webrtc/api/video/video_bitrate_allocation.h"
@@ -80,6 +81,8 @@ class PLATFORM_EXPORT RTCVideoEncoder : public webrtc::VideoEncoder {
  private:
   class Impl;
 
+  int32_t InitializeEncoder(
+      const media::VideoEncodeAccelerator::Config& vea_config);
   void UpdateEncoderInfo(
       media::VideoEncoderInfo encoder_info,
       std::vector<webrtc::VideoFrameBuffer::Type> preferred_pixel_formats);
