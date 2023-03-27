dddd5cf1b6d46d320b2fb0bd9f3b4af234231e85
Joanmarie Diggs <jdiggs@igalia.com>
2023-03-27 17:13:15 +0000

    Use new View accessibility-property setters in KioskAppInstructionBubble
    * Use SetAccessibilityProperties in constructor
    * Remove unneeded GetAccessibleNodeData override
    
    AX-Relnotes: n/a
    
    Bug: 1406781, 1363612, 1361972
    Change-Id: I719fb5de74ccf5063b62e5090e365106b3ab31bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373864
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122495}

--- ash/shelf/kiosk_app_instruction_bubble.h
+++ ash/shelf/kiosk_app_instruction_bubble.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
 namespace views {
@@ -23,6 +24,7 @@ namespace ash {
 class ASH_EXPORT KioskAppInstructionBubble
     : public views::BubbleDialogDelegateView {
  public:
+  METADATA_HEADER(KioskAppInstructionBubble);
   KioskAppInstructionBubble(views::View* anchor, ShelfAlignment alignment);
 
   KioskAppInstructionBubble(const KioskAppInstructionBubble&) = delete;
@@ -34,7 +36,6 @@ class ASH_EXPORT KioskAppInstructionBubble
   // views::View:
   void OnThemeChanged() override;
   gfx::Size CalculatePreferredSize() const override;
-  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
 
   views::Label* title_ = nullptr;
 };
