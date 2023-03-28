ee52a00e1d14bb52fb449f99c3660de4878a520d
Theodore Olsauskas-Warren <sauski@google.com>
2023-03-28 16:30:52 +0000

    Privacy Sandbox Settings: Make restriction permanent
    Adjust the delegate logic to store whether the sandbox was ever
    restricted into a pref, and then additionally consult that, making
    restriction a "latch".
    
    Bug: 1428571
    Change-Id: I085f206553310cc0a3e59b29185f9d436bbfbf8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373798
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123082}

--- components/privacy_sandbox/privacy_sandbox_prefs.cc
+++ components/privacy_sandbox/privacy_sandbox_prefs.cc
@@ -31,6 +31,8 @@ const char kPrivacySandboxM1FledgeEnabled[] =
 const char kPrivacySandboxM1AdMeasurementEnabled[] =
     "privacy_sandbox.m1.ad_measurement_enabled";
 
+const char kPrivacySandboxM1Restricted[] = "privacy_sandbox.m1.restricted";
+
 const char kPrivacySandboxApisEnabled[] = "privacy_sandbox.apis_enabled";
 
 const char kPrivacySandboxApisEnabledV2[] = "privacy_sandbox.apis_enabled_v2";
@@ -116,6 +118,7 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kPrivacySandboxM1FledgeEnabled, false);
   registry->RegisterBooleanPref(prefs::kPrivacySandboxM1AdMeasurementEnabled,
                                 false);
+  registry->RegisterBooleanPref(prefs::kPrivacySandboxM1Restricted, false);
 
   registry->RegisterBooleanPref(
       prefs::kPrivacySandboxManuallyControlled, false,
