bed3e8985b260ad82bc0ace90502eaa9b40c4dae
Lin Guo <sinyu890807@gmail.com>
2023-03-28 16:38:40 +0000

    ui: don't check for Android in global_error
    This file isn't built on Android, so it doesn't need Android conditionals.
    
    Change-Id: I584ebe24ea2a5c03f330579bf2d4fdd6d2eca7eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369165
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123091}

--- chrome/browser/ui/global_error/global_error.cc
+++ chrome/browser/ui/global_error/global_error.cc
@@ -5,18 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/global_error/global_error.h"
 
-#include "base/logging.h"
-#include "build/build_config.h"
-#include "chrome/browser/ui/global_error/global_error_bubble_view_base.h"
-#include "chrome/grit/theme_resources.h"
-#include "ui/base/resource/resource_bundle.h"
-#include "ui/base/ui_base_types.h"
-
-#if !BUILDFLAG(IS_ANDROID)
 #include "chrome/app/vector_icons/vector_icons.h"
-#include "ui/gfx/color_palette.h"
-#include "ui/gfx/paint_vector_icon.h"
-#endif
+#include "chrome/browser/ui/global_error/global_error_bubble_view_base.h"
+#include "ui/base/ui_base_types.h"
 
 // GlobalError ---------------------------------------------------------------
 
@@ -27,14 +18,8 @@ GlobalError::~GlobalError() {}
 GlobalError::Severity GlobalError::GetSeverity() { return SEVERITY_MEDIUM; }
 
 ui::ImageModel GlobalError::MenuItemIcon() {
-#if BUILDFLAG(IS_ANDROID)
-  return ui::ImageModel(
-      ui::ResourceBundle::GetSharedInstance().GetNativeImageNamed(
-          IDR_INPUT_ALERT_MENU));
-#else
   return ui::ImageModel::FromVectorIcon(kBrowserToolsErrorIcon,
                                         ui::kColorAlertMediumSeverityIcon);
-#endif
 }
 
 // GlobalErrorWithStandardBubble ---------------------------------------------
