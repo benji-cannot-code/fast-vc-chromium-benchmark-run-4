2f9013f0a92160dc7befb180a92c982a003fcd5b
Elly Fong-Jones <ellyjones@chromium.org>
2023-03-28 16:48:19 +0000

    desktop: remove Browser.AnyWindowHasName histogram
    As far as I know, nobody ever looks at it.
    
    Fixed: 1426042
    Change-Id: Ib04b37b305b30645eaab1e9d56b2bf3e719a2be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366323
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123099}

--- chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
+++ chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
@@ -29,14 +29,6 @@ enum class DarkModeStatus {
   kMaxValue = kDark,
 };
 
-bool AnyBrowserWindowHasName() {
-  for (auto* browser : *BrowserList::GetInstance()) {
-    if (!browser->user_title().empty())
-      return true;
-  }
-  return false;
-}
-
 }  // namespace
 
 DesktopPlatformFeaturesMetricsProvider::
@@ -67,6 +59,4 @@ void DesktopPlatformFeaturesMetricsProvider::ProvideCurrentSessionData(
                                 model->size() - model->unread_size());
     }
   }
-
-  UMA_HISTOGRAM_BOOLEAN("Browser.AnyWindowHasName", AnyBrowserWindowHasName());
 }
