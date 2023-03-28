94ac8ec7a59bb3a8f93e3a05ec14f7804eddd55f
Justin Huang <justinhuang@google.com>
2023-03-28 01:12:18 +0000

    Revert "Removes zxdg_shell"
    This reverts commit 37647e0b546a4d2ba210019f7232d5038f3c2f1f.
    
    Reason for revert: b/273702996. Parallels uses zxdg_shell :/
    
    Original change's description:
    > Removes zxdg_shell
    >
    > Sommelier has stopped using this and Lacros doesn't use it so it doesn't seem like it's being used anywhere
    >
    > Bug: N/A
    > Change-Id: I4e9daf3b4a39de3d19cac83c3f89c16ec4b93cca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4305659
    > Commit-Queue: Justin Huang <justinhuang@google.com>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1113197}
    
    Bug: N/A
    Change-Id: I9ac23cafd88f1717ed78ec60fb4b04d3101f4365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369248
    Commit-Queue: Justin Huang <justinhuang@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122780}

--- components/exo/wayland/clients/test/client_version_test.cc
+++ components/exo/wayland/clients/test/client_version_test.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <weston-test-server-protocol.h>
 #include <xdg-decoration-unstable-v1-server-protocol.h>
 #include <xdg-shell-server-protocol.h>
+#include <xdg-shell-unstable-v6-server-protocol.h>
 
 #include <memory>
 #include <string>
@@ -81,6 +82,7 @@ struct Globals {
   std::unique_ptr<wl_data_device_manager> wl_data_device_manager;
   std::unique_ptr<wp_content_type_manager_v1> wp_content_type_manager_v1;
   std::unique_ptr<wp_viewporter> wp_viewporter;
+  std::unique_ptr<zxdg_shell_v6> zxdg_shell_v6;
   std::unique_ptr<xdg_wm_base> xdg_wm_base;
   std::unique_ptr<zwp_text_input_manager_v1> zwp_text_input_manager_v1;
   std::unique_ptr<zcr_secure_output_v1> zcr_secure_output_v1;
@@ -175,6 +177,7 @@ void RegistryHandler(void* data,
           REGISTRY_CALLBACK(wp_content_type_manager_v1,
                             wp_content_type_manager_v1),
           REGISTRY_CALLBACK(wp_viewporter, wp_viewporter),
+          REGISTRY_CALLBACK(zxdg_shell_v6, zxdg_shell_v6),
           REGISTRY_CALLBACK(xdg_wm_base, xdg_wm_base),
           REGISTRY_CALLBACK(zwp_text_input_manager_v1,
                             zwp_text_input_manager_v1),
