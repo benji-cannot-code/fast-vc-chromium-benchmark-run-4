6d26c18a7b1fe334cdc4ef59ff8e46e255821de9
Morten Stenshorne <mstensho@chromium.org>
2023-03-28 17:16:11 +0000

    [RemoveLegacy] Prepare modules/accessibility for legacy table removal.
    The legacy table layout implementation is being removed.
    
    Bug: 1229581
    Change-Id: If52a8970f4ade8af954af072cc084267439dab8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4376762
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1123119}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -112,7 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
 #include "third_party/blink/renderer/core/layout/layout_inline.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
-#include "third_party/blink/renderer/core/layout/layout_table.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping.h"
