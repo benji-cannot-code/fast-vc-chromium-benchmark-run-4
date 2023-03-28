ad7cd2d166133c92b327bf225596c25f0fb2a7bd
Salma Elmahallawy <esalma@google.com>
2023-03-28 15:02:53 +0000

    [PolicyPageMigration] Migrating chrome://policy from JS to TS
    This CL changes all the Javascript files to Typescript and updates the
    BUILD file to leverage build_webui.
    
    Note: this CL is a follow up for crrev.com/c/4159012.
    
    Bug: b/265198461, crbug/1423920
    Change-Id: I9e152086605878bbf1d9031ad29fe4e314c662be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367008
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123027}

--- components/policy/resources/webui/policy_precedence_row.ts
+++ components/policy/resources/webui/policy_precedence_row.ts
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,7 +10,7 @@ import {CustomElement} from 'chrome://resources/js/custom_element.js';
 import {getTemplate} from './policy_precedence_row.html.js';
 
 export class PolicyPrecedenceRowElement extends CustomElement {
-  static get template() {
+  static override get template() {
     return getTemplate();
   }
 
@@ -20,12 +20,18 @@ export class PolicyPrecedenceRowElement extends CustomElement {
   }
 
   /**
-   * @param {Array<string>} precedenceOrder Array containing ordered strings
-   *     which represent the order of policy precedence.
+   * @param precedenceOrder array containing ordered strings
+   * which represent the order of policy precedence.
    */
-  initialize(precedenceOrder) {
-    this.shadowRoot.querySelector('.precedence.row > .value').textContent =
+  initialize(precedenceOrder: string[]) {
+    this.shadowRoot!.querySelector('.precedence.row > .value')!.textContent =
         precedenceOrder.join(' > ');
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'policy-precedence-row': PolicyPrecedenceRowElement;
   }
 }
 
