5a85efa2e83a1457a030cbb45d95825b08224d49
Pi-Hsun Shih <pihsun@chromium.org>
2023-03-28 07:36:00 +0000

    CCA: Expose jelly flag to CCA
    This is the flag used for chrome://flags#jelly-colors, and will be used
    to control whether dynamic color should be enabled in CCA.
    
    Bug: b:228796439
    Test: manually check window.loadTimeData.getBoolean("jelly")
    Change-Id: I73cd41b7077efb20141b435cbd1ae30def04dfc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369587
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122877}

--- chrome/browser/ash/web_applications/camera_app/chrome_camera_app_ui_delegate.cc
+++ chrome/browser/ash/web_applications/camera_app/chrome_camera_app_ui_delegate.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_launch_queue.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_tab_helper.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/constants/devicetype.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "components/services/app_service/public/cpp/app_launch_util.h"
@@ -311,6 +312,8 @@ void ChromeCameraAppUIDelegate::PopulateLoadTimeData(
                     DeviceTypeToString(chromeos::GetDeviceType()));
   source->AddBoolean("timeLapse", base::FeatureList::IsEnabled(
                                       ash::features::kCameraAppTimeLapse));
+  source->AddBoolean("jelly",
+                     base::FeatureList::IsEnabled(chromeos::features::kJelly));
 }
 
 bool ChromeCameraAppUIDelegate::IsMetricsAndCrashReportingEnabled() {
