7966503d113ad477d94469f915c7c798853fdde0
Henrique Nakashima <hnakashima@chromium.org>
2023-03-28 18:10:00 +0000

    [Android] Implement expectIntRecords()
    This method replaces multiple expectIntRecord() calls for the same
    histogram.
    
    Usage is: expectIntRecords(HISTOGRAM, 10, 11, 12, 13)
    to expect HISTOGRAM to be recorded four time, with values 10 to 13.
    
    Bug: 1411456
    Change-Id: Idb2db11a8db3be0086dbc75482d61741f29cc8b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374340
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123158}

--- base/android/javatests/src/org/chromium/base/test/metrics/HistogramWatcherWithoutNativeTest.java
+++ base/android/javatests/src/org/chromium/base/test/metrics/HistogramWatcherWithoutNativeTest.java
@@ -113,6 +113,18 @@ public class HistogramWatcherWithoutNativeTest extends HistogramWatcherTestBase 
 
     @Test
     @MediumTest
+    public void testExpectIntRecords_success() {
+        doTestExpectIntRecords_success(TestScenario.WITHOUT_NATIVE);
+    }
+
+    @Test
+    @MediumTest
+    public void testExpectIntRecords_failure() {
+        doTestExpectIntRecords_failure(TestScenario.WITHOUT_NATIVE);
+    }
+
+    @Test
+    @MediumTest
     public void testIgnoreOtherHistograms_success() {
         doTestIgnoreOtherHistograms_success(TestScenario.WITHOUT_NATIVE);
     }
