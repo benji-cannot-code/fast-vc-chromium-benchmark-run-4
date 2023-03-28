1c022fd16681407d5c2e7e6bfc6c38ed346a28e2
Mike Dougherty <michaeldo@chromium.org>
2023-03-28 00:29:39 +0000

    Update FormStructure tests to access the correct WebFramesManager
    Using the WebFramesManager associated with FormUtilJavaScriptFeature
    ensures that the correct WebFrames are used once the feature is moved
    to the isolated world.
    
    Bug: 1175793
    Change-Id: Idd1d912a84761b2289c7ad25d4d9077edd3a09ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374472
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122765}

--- ios/chrome/browser/autofill/form_structure_browsertest.mm
+++ ios/chrome/browser/autofill/form_structure_browsertest.mm
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/chrome_web_client.h"
 #import "ios/web/public/js_messaging/web_frame.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
+#import "ios/web/public/test/js_test_util.h"
 #import "ios/web/public/test/scoped_testing_web_client.h"
 #import "ios/web/public/test/task_observer_util.h"
 #import "ios/web/public/test/web_state_test_util.h"
@@ -279,11 +280,13 @@ bool FormStructureBrowserTest::LoadHtmlWithoutSubresourcesAndInitRendererIds(
     return false;
   }
 
+  autofill::FormUtilJavaScriptFeature* feature =
+      autofill::FormUtilJavaScriptFeature::GetInstance();
+
   __block web::WebFrame* main_frame = nullptr;
   success = WaitUntilConditionOrTimeout(kWaitForJSCompletionTimeout, ^bool {
     web::WebFramesManager* frames_manager =
-        autofill::FormUtilJavaScriptFeature::GetInstance()->GetWebFramesManager(
-            web_state());
+        feature->GetWebFramesManager(web_state());
     main_frame = frames_manager->GetMainWebFrame();
     return main_frame != nullptr;
   });
@@ -293,14 +296,13 @@ bool FormStructureBrowserTest::LoadHtmlWithoutSubresourcesAndInitRendererIds(
   DCHECK(main_frame);
 
   uint32_t next_available_id = 1;
-  autofill::FormUtilJavaScriptFeature::GetInstance()
-      ->SetUpForUniqueIDsWithInitialState(main_frame, next_available_id);
+  feature->SetUpForUniqueIDsWithInitialState(main_frame, next_available_id);
 
   // Wait for `SetUpForUniqueIDsWithInitialState` to complete.
   return WaitUntilConditionOrTimeout(kWaitForJSCompletionTimeout, ^bool {
-    return [web::test::ExecuteJavaScript(@"document[__gCrWeb.fill.ID_SYMBOL]",
-                                         web_state()) intValue] ==
-           static_cast<int>(next_available_id);
+    return [web::test::ExecuteJavaScriptForFeature(
+               web_state(), @"document[__gCrWeb.fill.ID_SYMBOL]", feature)
+               intValue] == static_cast<int>(next_available_id);
   });
 }
 
