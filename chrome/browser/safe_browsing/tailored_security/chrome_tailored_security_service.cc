5f270c1ff4372c5e98bdffeeafb60766432b40b6
Awad Osman <awado@google.com>
2023-03-28 23:06:37 +0000

    [TailoredSecurity] Add histogram for observer-based recovery mechanism
    Adding a histogram to record if our implementation of the observer-based
    recovery mechanism in crrev.com/c/4321765 is being triggered.
    
    
    Bug: 1427624
    Change-Id: Iaa9da7cede13e1501e43cf07f4f0f05cdb2688cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368368
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123348}

--- chrome/browser/safe_browsing/tailored_security/chrome_tailored_security_service.cc
+++ chrome/browser/safe_browsing/tailored_security/chrome_tailored_security_service.cc
@@ -33,6 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
+#if BUILDFLAG(IS_ANDROID)
+// Names for if the observer-based recovery mechanism is triggered.
+const bool kRetryMechanismTriggered = true;
+const bool kRetryMechanismNotTriggered = false;
+#endif
+
 namespace {
 
 #if BUILDFLAG(IS_ANDROID)
@@ -81,6 +87,9 @@ void ChromeTailoredSecurityService::OnSyncNotificationMessageRequest(
     if (base::FeatureList::IsEnabled(
             safe_browsing::kTailoredSecurityObserverRetries)) {
       AddTabModelListObserver();
+      base::UmaHistogramBoolean(
+          "SafeBrowsing.TailoredSecurity.IsRecoveryTriggered",
+          kRetryMechanismTriggered);
       return;
     }
     if (is_enabled) {
@@ -88,6 +97,12 @@ void ChromeTailoredSecurityService::OnSyncNotificationMessageRequest(
           TailoredSecurityNotificationResult::kNoWebContentsAvailable);
     }
     return;
+  }
+  if (base::FeatureList::IsEnabled(
+          safe_browsing::kTailoredSecurityObserverRetries)) {
+    base::UmaHistogramBoolean(
+        "SafeBrowsing.TailoredSecurity.IsRecoveryTriggered",
+        kRetryMechanismNotTriggered);
   }
 
   // Since the Android UX is a notice, we simply set Safe Browsing state.
