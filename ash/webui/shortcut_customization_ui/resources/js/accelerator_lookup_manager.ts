50e20d21c7755ac9f120866ed5647ea85e43ac36
Longbo Wei <longbowei@google.com>
2023-03-28 17:10:44 +0000

    shortcuts: Hide accelerators when certain keys are not available
    Some accelerators should be hidden when certain keys are not available
    on the keyboard.
    
    When alias_accelerators returned from CreateAcceleratorAlias() is
    empty(because certain keys are not available), return early and update
    the the AcceleartorState to be kDisabledByUnavailableKeys.
    
    In the frontend, if the AcceleratorState is set to disabled, the
    corresponding accelerator should be hidden. Additionally, if there is
    no accelerator for a given accelerator description, we will not add a
    row for that accelerator.
    
    Screenshot: Manually make 'LaunchAssistant' and 'LaunchApplication2'
    keys unavailable.
      - Before: http://screen/3omyatZZp4bKZNe
      - After:  http://screen/63tZsx4dchJXqHn
    
    Bug: b:216049298
    Test: browser_tests ShortcutCustomizationApp*
    Change-Id: Ia3d6ced84df779c0a26aec3f88e0c96fb54a5c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363002
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123116}

--- ash/webui/shortcut_customization_ui/resources/js/accelerator_lookup_manager.ts
+++ ash/webui/shortcut_customization_ui/resources/js/accelerator_lookup_manager.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {assert, assertNotReached} from 'chrome://resources/js/assert_ts.js';
 
 import {mojoString16ToString} from './mojo_utils.js';
-import {Accelerator, AcceleratorCategory, AcceleratorId, AcceleratorInfo, AcceleratorSource, AcceleratorState, AcceleratorSubcategory, AcceleratorType, LayoutInfo, MojoAcceleratorConfig, MojoAcceleratorInfo, MojoLayoutInfo, StandardAcceleratorInfo, TextAcceleratorInfo} from './shortcut_types.js';
+import {Accelerator, AcceleratorCategory, AcceleratorId, AcceleratorInfo, AcceleratorSource, AcceleratorState, AcceleratorSubcategory, AcceleratorType, LayoutInfo, LayoutStyle, MojoAcceleratorConfig, MojoAcceleratorInfo, MojoLayoutInfo, StandardAcceleratorInfo, TextAcceleratorInfo} from './shortcut_types.js';
 import {areAcceleratorsEqual, getAccelerator, getAcceleratorId, isStandardAcceleratorInfo, isTextAcceleratorInfo} from './shortcut_utils.js';
 
 // Convert from Mojo types to the app types.
@@ -111,8 +111,8 @@ export class AcceleratorLookupManager {
     return acceleratorInfos;
   }
 
-  isStandardAccelerator(source: number|string, action: number|string): boolean {
-    return this.standardAcceleratorLookup.has(getAcceleratorId(source, action));
+  isStandardAccelerator(style: number|string): boolean {
+    return style === LayoutStyle.kDefault;
   }
 
   getAcceleratorLayout(
