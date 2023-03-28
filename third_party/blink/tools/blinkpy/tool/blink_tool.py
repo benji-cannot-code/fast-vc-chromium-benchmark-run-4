660ca9b428268ffaebfa4d03e27ae671fd0c3c60
Jonathan Lee <jonathanjlee@google.com>
2023-03-28 17:46:58 +0000

    [rebaseline-cl] Remove `rebaseline-test-internal` command
    On the ResultDB-enabled path, this command just fetches `--result-url`
    and does not otherwise add value. This change replaces the internal
    command with a stateful process pool, which will allow us to:
     1. Use disk as a pixel baseline cache keyed on the baseline's hash,
        which we added in crrev.com/c/4371118. When possible, the cache will
        copy local existing baselines with the same hash to avoid going the
        network. The cache will be most effective for platform-independent
        rendering tests.
     2. Compare baselines from all retries to detect flakiness. This will be
        cheap for images because of [1].
     3. Clean up many flags/logic only used by the non-ResultDB path.
    
    The `--no-resultDB` flag, which was deprecated in crrev.com/c/4358192,
    is removed.
    
    Bug: 1411891, 1282507, 1406660
    Test: git cl issue 4371460
    Test: ./blink_tool.py rebaseline-cl --patchset=1 --no-trigger-jobs
    Change-Id: I28507413dd2c2fc2c3eb6eb57c587c5629432746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375978
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123140}

--- third_party/blink/tools/blinkpy/tool/blink_tool.py
+++ third_party/blink/tools/blinkpy/tool/blink_tool.py
@@ -50,7 +50,6 @@ from blinkpy.tool.commands.queries import PrintBaselines
 from blinkpy.tool.commands.queries import PrintExpectations
 from blinkpy.tool.commands.rebaseline import Rebaseline
 from blinkpy.tool.commands.rebaseline_cl import RebaselineCL
-from blinkpy.tool.commands.rebaseline_test import RebaselineTest
 from blinkpy.tool.commands.update_metadata import UpdateMetadata
 
 _log = logging.getLogger(__name__)
@@ -83,7 +82,6 @@ class BlinkTool(Host):
             PrintExpectations(),
             Rebaseline(),
             RebaselineCL(self),
-            RebaselineTest(),
             UpdateMetadata(self),
         ]
         self.help_command = HelpCommand(tool=self)
