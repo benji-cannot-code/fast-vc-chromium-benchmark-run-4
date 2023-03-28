9b5ff641a9e7fcb7b82dd282d54dfd43e971605f
Emilia Paz <emiliapaz@chromium.org>
2023-03-28 00:54:42 +0000

    [Extensions c2s] Dynamically update site permissions page
    Extensions menu listens for various dynamic updates (page navigation,
    extension updates, site settings updates). Site permissions page
    content is updated if extension should have a site permissions page
    (signaled by the site permissions button visibility). Otherwise, menu
    navigates back to the main page.
    
    Bug: 1390952
    Change-Id: I10dcc2ef2014a77c473a4fbe2f85bb8dae9040c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356177
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122773}

--- chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view.h
+++ chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view.h
@@ -14,6 +14,8 @@ class ImageModel;
 }  // namespace ui
 
 namespace views {
+class ImageView;
+class Label;
 class ToggleButton;
 }  // namespace views
 
@@ -26,16 +28,18 @@ class ExtensionsMenuSitePermissionsPageView : public views::View {
 
   explicit ExtensionsMenuSitePermissionsPageView(
       Browser* browser,
-      std::u16string extension_name,
-      ui::ImageModel extension_icon,
       extensions::ExtensionId extension_id,
-      bool is_show_requests_toggle_on,
       ExtensionsMenuNavigationHandler* navigation_handler);
   ExtensionsMenuSitePermissionsPageView(
       const ExtensionsMenuSitePermissionsPageView&) = delete;
   const ExtensionsMenuSitePermissionsPageView& operator=(
       const ExtensionsMenuSitePermissionsPageView&) = delete;
   ~ExtensionsMenuSitePermissionsPageView() override = default;
+
+  // Updates the page contents with the given parameters.
+  void Update(const std::u16string& extension_name,
+              const ui::ImageModel& extension_icon,
+              bool is_show_requests_toggle_on);
 
   // Updates `show_requests_toggle_` state to `is_on`.
   void UpdateShowRequestsToggle(bool is_on);
@@ -55,6 +59,8 @@ class ExtensionsMenuSitePermissionsPageView : public views::View {
   const raw_ptr<Browser> browser_;
   extensions::ExtensionId extension_id_;
 
+  raw_ptr<views::ImageView> extension_icon_;
+  raw_ptr<views::Label> extension_name_;
   raw_ptr<views::ToggleButton> show_requests_toggle_;
 };
 
