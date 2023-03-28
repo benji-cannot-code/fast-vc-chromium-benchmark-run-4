3961ae279fc20efe0b2e05cf735f4c2eb909a980
Yuzu Saijo <yuzus@chromium.org>
2023-03-28 07:42:40 +0000

    [bfcache] Save Blocking Details on the browser side
    This CL starts saving BackForwardCacheBlockingDetails (vector of mojom that has JS locations) in RenderFrameHostImpl and DedicatedWorkerHost, and use this mojom to determine which features are used.
    
    We are not using the JavaScript blocking details yet in this CL.
    
    Bug: 1366675
    
    Change-Id: Ie81227b823040b6d8c415df908f6a946c29eb7de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4322987
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122878}

--- content/browser/worker_host/dedicated_worker_host.h
+++ content/browser/worker_host/dedicated_worker_host.h
@@ -229,6 +229,9 @@ class DedicatedWorkerHost final
   blink::scheduler::WebSchedulerTrackedFeatures
   GetBackForwardCacheDisablingFeatures() const;
 
+  const BackForwardCacheBlockingDetails& GetBackForwardCacheBlockingDetails()
+      const;
+
   base::WeakPtr<ServiceWorkerContainerHost> GetServiceWorkerContainerHost();
 
   mojo::PendingRemote<blink::mojom::BackForwardCacheControllerHost>
@@ -414,7 +417,7 @@ class DedicatedWorkerHost final
   // JavaScript / WebAssembly resources.
   CodeCacheHostImpl::ReceiverSet code_cache_host_receivers_;
 
-  blink::scheduler::WebSchedulerTrackedFeatures bfcache_disabling_features_;
+  BackForwardCacheBlockingDetails bfcache_blocking_details_;
 
   base::WeakPtrFactory<DedicatedWorkerHost> weak_factory_{this};
 };
