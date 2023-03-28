7d2aa43e6417c98ef1d8b074ac2db90d61fce1bb
Sean Maher <spvw@chromium.org>
2023-03-28 17:32:32 +0000

    Add constructor for MessagePumpGlib::RunState
    Removes error-prone C-like construction of object field-by-field.
    
    Change-Id: If382521bd345b1502634db79d9127a249c324ba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4377557
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123129}

--- base/message_loop/message_pump_glib.cc
+++ base/message_loop/message_pump_glib.cc
@@ -163,10 +163,14 @@ GSourceFuncs g_fd_watch_source_funcs = {
 }  // namespace
 
 struct MessagePumpGlib::RunState {
-  raw_ptr<Delegate> delegate;
+  explicit RunState(raw_ptr<Delegate> delegate) : delegate(delegate) {
+    CHECK(delegate);
+  }
+
+  const raw_ptr<Delegate> delegate;
 
   // Used to flag that the current Run() invocation should return ASAP.
-  bool should_quit;
+  bool should_quit = false;
 
   // The information of the next task available at this run-level. Stored in
   // RunState because different set of tasks can be accessible at various
@@ -372,9 +376,7 @@ void MessagePumpGlib::HandleDispatch() {
 }
 
 void MessagePumpGlib::Run(Delegate* delegate) {
-  RunState state;
-  state.delegate = delegate;
-  state.should_quit = false;
+  RunState state(delegate);
 
   RunState* previous_state = state_;
   state_ = &state;
