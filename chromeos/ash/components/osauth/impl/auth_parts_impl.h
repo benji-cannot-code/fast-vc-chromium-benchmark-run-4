8869342b29b803675cbdf3f626626eb4b6f9de95
Denis Kuznetsov <antrim@chromium.org>
2023-03-28 16:38:52 +0000

    Add interface for AuthFactorEngineFactory
    Bug: b:271248265
    Change-Id: I66ddbbb30e81a45b0fcaff9d6633f30a995a1351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350788
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123092}

--- chromeos/ash/components/osauth/impl/auth_parts_impl.h
+++ chromeos/ash/components/osauth/impl/auth_parts_impl.h
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_COMPONENTS_OSAUTH_IMPL_AUTH_PARTS_IMPL_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/component_export.h"
+#include "base/containers/flat_map.h"
 #include "chromeos/ash/components/osauth/public/auth_parts.h"
+#include "chromeos/ash/components/osauth/public/common_types.h"
 
 namespace ash {
 
@@ -24,12 +27,18 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_OSAUTH) AuthPartsImpl
 
   // AuthParts implementation:
   AuthSessionStorage* GetAuthSessionStorage() override;
+  void RegisterEngineFactory(
+      std::unique_ptr<AuthFactorEngineFactory> factory) override;
+  const std::vector<std::unique_ptr<AuthFactorEngineFactory>>&
+  GetEngineFactories() override;
 
  private:
   friend class AuthParts;
   void CreateDefaultComponents();
 
   std::unique_ptr<AuthSessionStorage> session_storage_;
+
+  std::vector<std::unique_ptr<AuthFactorEngineFactory>> engine_factories_;
 };
 
 }  // namespace ash
