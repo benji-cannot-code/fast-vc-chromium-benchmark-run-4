4736a0e4037c1c96e7a78bc0b44d887bdbcc798e
ahmedmoussa <ahmedmoussa@google.com>
2023-03-28 22:54:26 +0000

    [Dial] Change message shown to the user when unable to disconnect from DIAL devices
    Bug: b:196065290
    Change-Id: If2dbcd4bf888ad086e51d3812504fa40b86af614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375053
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123341}

--- chrome/browser/media/router/providers/dial/dial_media_route_provider.cc
+++ chrome/browser/media/router/providers/dial/dial_media_route_provider.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "chrome/browser/media/router/data_decoder_util.h"
 #include "chrome/browser/media/router/providers/dial/dial_media_route_provider_metrics.h"
@@ -448,9 +447,9 @@ void DialMediaRouteProvider::DoTerminateRoute(const DialActivity& activity,
         activity.launch_info.client_id, sink));
     message_sender_->SendMessages(route_id, std::move(messages));
     activity_manager_->StopApp(
-        route_id, base::BindOnce(&DialMediaRouteProvider::HandleStopAppResult,
-                                 base::Unretained(this), route_id,
-                                 std::move(callback), sink));
+        route_id,
+        base::BindOnce(&DialMediaRouteProvider::HandleStopAppResult,
+                       base::Unretained(this), route_id, std::move(callback)));
   } else {
     logger_->LogError(
         mojom::LogCategory::kRoute, kLoggerComponent,
@@ -467,7 +466,6 @@ void DialMediaRouteProvider::DoTerminateRoute(const DialActivity& activity,
 void DialMediaRouteProvider::HandleStopAppResult(
     const MediaRoute::Id& route_id,
     TerminateRouteCallback callback,
-    const MediaSinkInternal& sink,
     const absl::optional<std::string>& message,
     mojom::RouteRequestResultCode result_code) {
   switch (result_code) {
@@ -496,9 +494,9 @@ void DialMediaRouteProvider::HandleStopAppResult(
           "", MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id),
           MediaRoute::GetPresentationIdFromMediaRouteId(route_id));
       media_router_->OnIssue(
-          {l10n_util::GetStringFUTF8(IDS_MEDIA_ROUTER_ISSUE_CANNOT_TERMINATE,
-                                     base::UTF8ToUTF16(sink.sink().name())),
-           IssueInfo::Severity::WARNING, sink.id()});
+          {l10n_util::GetStringUTF8(IDS_MEDIA_ROUTER_ISSUE_CANNOT_TERMINATE),
+           IssueInfo::Severity::WARNING,
+           MediaRoute::GetSinkIdFromMediaRouteId(route_id)});
   }
   // We set the PresentationConnection state to "terminated" per the API spec:
   // https://w3c.github.io/presentation-api/#terminating-a-presentation-in-a-controlling-browsing-context
