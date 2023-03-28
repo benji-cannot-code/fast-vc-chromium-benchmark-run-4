aa75f52d7afb34c8d84fdcd88b6c3c8e797e38bd
Bo Majewski <majewski@chromium.org>
2023-03-28 00:06:16 +0000

    [Files]: Fix open/close actions on xf-select
    xf-select button maintained its own optionsVisible_ field. However, due
    to the way cr-action-menu is implemented, we would never get an event on
    the toggle button, when options were opened and immediately closed. That
    event is internaly consumed by cr-action-menu component. This CL
    switches us to using the internal open property of cr-action-menu that
    is always in sync with the state of cr-action-menu.
    
    Bug: b:275267929
    Change-Id: I9948e2f4d699b411237d465c68e188372751fd32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369251
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122753}

--- ui/file_manager/file_manager/widgets/xf_select.ts
+++ ui/file_manager/file_manager/widgets/xf_select.ts
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {AnchorAlignment, CrActionMenuElement} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 
-import {css, CSSResultGroup, customElement, html, property, query, state, XfBase} from './xf_base.js';
+import {css, CSSResultGroup, customElement, html, property, query, XfBase} from './xf_base.js';
 
 /**
  * The data structure used to set the new options on the select element.
@@ -103,11 +103,6 @@ export class XfSelect extends XfBase {
   @query('cr-action-menu') private $optionsMenu_?: CrActionMenuElement;
 
   /**
-   * Keeps track of whether we are showing the options menu.
-   */
-  @state() private optionsVisible_: boolean = false;
-
-  /**
    * The currently selected option.
    */
   private selectedOption_: XfSelectedValue = {
@@ -139,7 +134,7 @@ export class XfSelect extends XfBase {
    * collapsed.
    */
   get expanded(): boolean {
-    return this.optionsVisible_;
+    return this.$optionsMenu_ ? this.$optionsMenu_.open : false;
   }
 
   /**
@@ -158,7 +153,7 @@ export class XfSelect extends XfBase {
     return html`
       <cr-button id="dropdown-toggle"
               aria-haspopup="menu"
-              aria-expanded=${this.optionsVisible_}
+              aria-expanded=${this.expanded}
               @click=${this.onToggleOptions_}>
         ${iconPart}${labelPart}<span id="dropdown-icon"></span>
       </cr-button>`;
@@ -249,7 +244,7 @@ export class XfSelect extends XfBase {
    * dropdown options.
    */
   private onToggleOptions_(): void {
-    if (this.optionsVisible_) {
+    if (this.expanded) {
       this.closeOptions_();
     } else {
       this.openOptions_();
@@ -260,12 +255,11 @@ export class XfSelect extends XfBase {
    * Opens the dropdown options, providing they were closed.
    */
   private openOptions_() {
-    if (!this.optionsVisible_) {
+    if (!this.expanded) {
       const element: HTMLElement = this.$toggleDropdownButton_!;
       const top = element.offsetTop + element.offsetHeight + 8;
       this.$optionsMenu_!.showAt(
           element, {top: top, anchorAlignmentX: AnchorAlignment.AFTER_START});
-      this.optionsVisible_ = true;
     }
   }
 
@@ -273,9 +267,8 @@ export class XfSelect extends XfBase {
    * Closes the dropdown options, providing they were open.
    */
   private closeOptions_() {
-    if (this.optionsVisible_) {
+    if (this.expanded) {
       this.$optionsMenu_!.close();
-      this.optionsVisible_ = false;
     }
   }
 
