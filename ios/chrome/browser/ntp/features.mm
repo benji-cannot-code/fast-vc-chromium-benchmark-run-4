c757e45a3934087f43f9156229e2c9da341bdae8
Ed Chin <edchin@google.com>
2023-03-28 20:25:45 +0000

    [ios][feed] Enable background capability at startup for AppCloseBackgroundRefresh
    This CL generalizes the code to enable background refresh capability at startup. Currently, it was only used for the background refresh feature.
    
    The new method IsFeedBackgroundRefreshCapabilityEnabled() gates the
    capability itself. The capability is enabled if either BackgroundRefresh
    or AppCloseBackgroundRefresh is enabled.
    
    Recall that it is done this way because registering for background refreshes must happen early in app initialization and FeatureList is not yet available. So the capability must be enabled for any background
    refresh feature.
    
    Bug: 1428446
    Change-Id: Iced7761a2ba07cb4913d5c4d877afdba2d8afbee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375907
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123246}

--- ios/chrome/browser/ntp/features.mm
+++ ios/chrome/browser/ntp/features.mm
@@ -17,6 +17,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+namespace {
+
+// Whether feed background refresh is enabled. This only checks if the feature
+// is enabled, not if the capability was enabled at startup.
+bool IsFeedBackgroundRefreshEnabledOnly() {
+  return base::FeatureList::IsEnabled(kEnableFeedBackgroundRefresh);
+}
+
+// Whether feed is refreshed in the background soon after the app is
+// backgrounded. This only checks if the feature is enabled, not if the
+// capability was enabled at startup.
+bool IsFeedAppCloseBackgroundRefreshEnabledOnly() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kEnableFeedInvisibleForegroundRefresh,
+      kEnableFeedAppCloseBackgroundRefresh,
+      /*default=*/false);
+}
+
+}  // namespace
+
 BASE_FEATURE(kEnableWebChannels,
              "EnableWebChannels",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -52,14 +72,14 @@ BASE_FEATURE(kEnableFeedExperimentTagging,
 BASE_FEATURE(kIOSSetUpList, "IOSSetUpList", base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Key for NSUserDefaults containing a bool indicating whether the next run
-// should enable feed background refresh. This is used because registering for
-// background refreshes must happen early in app initialization and FeatureList
-// is not yet available. Changing the `kEnableFeedBackgroundRefresh` feature
-// will always take effect after two cold starts after the feature has been
-// changed on the server (once for the Finch configuration, and another for
-// reading the stored value from NSUserDefaults).
-NSString* const kEnableFeedBackgroundRefreshForNextColdStart =
-    @"EnableFeedBackgroundRefreshForNextColdStart";
+// should enable feed background refresh capability. This is used because
+// registering for background refreshes must happen early in app initialization
+// and FeatureList is not yet available. Enabling or disabling background
+// refresh features will always take effect after two cold starts after the
+// feature has been changed on the server (once for the Finch configuration, and
+// another for reading the stored value from NSUserDefaults).
+NSString* const kEnableFeedBackgroundRefreshCapabilityForNextColdStart =
+    @"EnableFeedBackgroundRefreshCapabilityForNextColdStart";
 
 const char kEnableFollowingFeedBackgroundRefresh[] =
     "EnableFollowingFeedBackgroundRefresh";
@@ -98,21 +118,28 @@ bool IsDiscoverFeedServiceCreatedEarly() {
 }
 
 bool IsFeedBackgroundRefreshEnabled() {
+  return IsFeedBackgroundRefreshCapabilityEnabled() &&
+         IsFeedBackgroundRefreshEnabledOnly();
+}
+
+bool IsFeedBackgroundRefreshCapabilityEnabled() {
 #if !BUILDFLAG(IOS_BACKGROUND_MODE_ENABLED)
   return false;
 #else
   static bool feedBackgroundRefreshEnabled =
       [[NSUserDefaults standardUserDefaults]
-          boolForKey:kEnableFeedBackgroundRefreshForNextColdStart];
+          boolForKey:kEnableFeedBackgroundRefreshCapabilityForNextColdStart];
   return feedBackgroundRefreshEnabled;
 #endif  // BUILDFLAG(IOS_BACKGROUND_MODE_ENABLED)
 }
 
-void SaveFeedBackgroundRefreshEnabledForNextColdStart() {
+void SaveFeedBackgroundRefreshCapabilityEnabledForNextColdStart() {
   DCHECK(base::FeatureList::GetInstance());
+  BOOL enabled = IsFeedBackgroundRefreshEnabledOnly() ||
+                 IsFeedAppCloseBackgroundRefreshEnabledOnly();
   [[NSUserDefaults standardUserDefaults]
-      setBool:base::FeatureList::IsEnabled(kEnableFeedBackgroundRefresh)
-       forKey:kEnableFeedBackgroundRefreshForNextColdStart];
+      setBool:enabled
+       forKey:kEnableFeedBackgroundRefreshCapabilityForNextColdStart];
 }
 
 void SetFeedRefreshTimestamp(NSDate* timestamp, NSString* NSUserDefaultsKey) {
@@ -137,7 +164,7 @@ bool IsFeedBackgroundRefreshCompletedNotificationEnabled() {
   if (GetChannel() == version_info::Channel::STABLE) {
     return false;
   }
-  return IsFeedBackgroundRefreshEnabled() &&
+  return IsFeedBackgroundRefreshCapabilityEnabled() &&
          [[NSUserDefaults standardUserDefaults]
              boolForKey:@"FeedBackgroundRefreshNotificationEnabled"];
 }
@@ -217,10 +244,8 @@ bool IsFeedAppCloseForegroundRefreshEnabled() {
 }
 
 bool IsFeedAppCloseBackgroundRefreshEnabled() {
-  return base::GetFieldTrialParamByFeatureAsBool(
-      kEnableFeedInvisibleForegroundRefresh,
-      kEnableFeedAppCloseBackgroundRefresh,
-      /*default=*/false);
+  return IsFeedBackgroundRefreshCapabilityEnabled() &&
+         IsFeedAppCloseBackgroundRefreshEnabledOnly();
 }
 
 FeedRefreshEngagementCriteriaType GetFeedRefreshEngagementCriteriaType() {
