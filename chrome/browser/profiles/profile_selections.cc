5599861a50d5c235e6f409c32169fc52a26c52ec
Ryan Sultanem <rsult@google.com>
2023-03-28 08:30:06 +0000

    [SystemProfileSelectionDefaultNone] Enable feature by default
    Feature has been at the 1% stable for more than two weeks with a stable
    status.
    UMA stability is good https://uma.googleplex.com/p/chrome/variations?sid=c64b877df26cf72b69e6706f13a49fd5.
    
    No bugs or crashes were reported since the DEV stage of the feature
    rollout.
    
    Bug: 1284664
    Change-Id: I9667a976d6e80734e1471c9cb9d97560cb354613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373699
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122889}

--- chrome/browser/profiles/profile_selections.cc
+++ chrome/browser/profiles/profile_selections.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 BASE_FEATURE(kSystemProfileSelectionDefaultNone,
              "SystemProfileSelectionDefaultNone",
-             base::FeatureState::FEATURE_DISABLED_BY_DEFAULT);
+             base::FeatureState::FEATURE_ENABLED_BY_DEFAULT);
 
 bool AreKeyedServicesDisabledForProfileByDefault(const Profile* profile) {
   if (profile && profile->IsSystemProfile()) {
