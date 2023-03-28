70952deacd612f591c81e53276c676dc4e021335
Wenbo Jie <wenbojie@chromium.org>
2023-03-28 00:53:08 +0000

    [Files F2] Capture disabled in store's FileData
    Bug: b:271485133
    Test: browser_tests --gtest_filter="FileManagerJsTest*Reducer*"
    Test: browser_tests --gtest_filter="*DirectoryTreeContainer*"
    Change-Id: Ib5a93ecd40e17a63352f0fdc9dc0ebc9799927b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369541
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122771}

--- ui/file_manager/file_manager/externs/ts/state.js
+++ ui/file_manager/file_manager/externs/ts/state.js
@@ -54,6 +54,7 @@ export const EntryType = {
  *   shouldDelayLoadingChildren: !boolean,
  *   children: (!Array<!FileKey>),
  *   expanded: !boolean,
+ *   disabled: !boolean,
  * }}
  */
 export let FileData;
