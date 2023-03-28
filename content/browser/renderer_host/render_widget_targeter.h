46561e73d6dc0c84c211823b35d66d1eeaa83295
Jonathan Ross <jonross@chromium.org>
2023-03-28 21:48:18 +0000

    Remove expired Event.AsyncTargeting.ResponseTime
    This metric has not been monitored in a while and is expired. Let's
    remove it.
    
    Bug: 1404501
    Change-Id: I97f62d391e1d5750d97b171656488469063158ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4377690
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123297}

--- content/browser/renderer_host/render_widget_targeter.h
+++ content/browser/renderer_host/render_widget_targeter.h
@@ -51,8 +51,6 @@ struct CONTENT_EXPORT RenderWidgetTargetResult {
   bool latched_target = false;
 };
 
-class TracingUmaTracker;
-
 class RenderWidgetTargeter {
  public:
   using RenderWidgetHostAtPointCallback =
@@ -204,7 +202,6 @@ class RenderWidgetTargeter {
   void FoundFrameSinkId(base::WeakPtr<RenderWidgetHostViewBase> target,
                         uint32_t request_id,
                         const gfx::PointF& target_location,
-                        TracingUmaTracker tracker,
                         const viz::FrameSinkId& frame_sink_id,
                         const gfx::PointF& transformed_location);
 
