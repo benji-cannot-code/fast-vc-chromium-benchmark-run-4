fb791e54f925d73f8ae688f1480c899fb4cf5569
Chris Fredrickson <cfredric@chromium.org>
2023-03-28 14:47:52 +0000

    Rewrite WebRtcBrowserTest::MakeTypicalPeerConnectionCall tests to avoid manual replies
    Rewriting the tests to avoid using EXECUTE_SCRIPT_USE_MANUAL_REPLY
    forces them to use promises, which systematically prevents bugs due
    double-sends via domAutomationController.send, and also tends to make
    the control flow of the JS simpler to follow.
    
    Bug: 1423407
    Change-Id: Ibdc040d6f0e915d2a46dff23f4f17532ca25e5bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368786
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123017}

--- content/browser/webrtc/webrtc_browsertest.cc
+++ content/browser/webrtc/webrtc_browsertest.cc
@@ -50,10 +50,9 @@ class MAYBE_WebRtcBrowserTest : public WebRtcContentBrowserTestBase {
  protected:
   // Convenience function since most peerconnection-call.html tests just load
   // the page, kick off some javascript and wait for the title to change to OK.
-  void MakeTypicalPeerConnectionCall(const std::string& javascript,
-                                     bool use_manual_reply = true) {
+  void MakeTypicalPeerConnectionCall(const std::string& javascript) {
     MakeTypicalCall(javascript, "/media/peerconnection-call.html",
-                    use_manual_reply);
+                    /*use_manual_reply=*/false);
   }
 
   void SetConfigurationTest(const std::string& javascript) {
@@ -64,8 +63,7 @@ class MAYBE_WebRtcBrowserTest : public WebRtcContentBrowserTestBase {
 };
 
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest, CanSetupAudioAndVideoCall) {
-  MakeTypicalPeerConnectionCall("call({video: true, audio: true});",
-                                /*use_manual_reply=*/false);
+  MakeTypicalPeerConnectionCall("call({video: true, audio: true});");
 }
 
 // Flaky on Android and Linux ASAN https://crbug.com/1099365.
@@ -79,11 +77,9 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
                        MAYBE_NetworkProcessCrashRecovery) {
   if (!IsOutOfProcessNetworkService())
     return;
-  MakeTypicalPeerConnectionCall("call({video: true, audio: true});",
-                                /*use_manual_reply=*/false);
+  MakeTypicalPeerConnectionCall("call({video: true, audio: true});");
   SimulateNetworkServiceCrash();
-  MakeTypicalPeerConnectionCall("call({video: true, audio: true});",
-                                /*use_manual_reply=*/false);
+  MakeTypicalPeerConnectionCall("call({video: true, audio: true});");
 }
 
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
@@ -198,23 +194,20 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
 #endif
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
                        MAYBE_CanSetupAudioAndVideoCallWithoutMsidAndBundle) {
-  MakeTypicalPeerConnectionCall("callWithoutMsidAndBundle();",
-                                /*use_manual_reply=*/false);
+  MakeTypicalPeerConnectionCall("callWithoutMsidAndBundle();");
 }
 
 // This test will modify the SDP offer to an unsupported codec, which should
 // cause SetLocalDescription to fail.
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
                        NegotiateUnsupportedVideoCodec) {
-  MakeTypicalPeerConnectionCall("negotiateUnsupportedVideoCodec();",
-                                /*use_manual_reply=*/false);
+  MakeTypicalPeerConnectionCall("negotiateUnsupportedVideoCodec();");
 }
 
 // This test will modify the SDP offer to use no encryption, which should
 // cause SetLocalDescription to fail.
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest, NegotiateNonCryptoCall) {
-  MakeTypicalPeerConnectionCall("negotiateNonCryptoCall();",
-                                /*use_manual_reply=*/false);
+  MakeTypicalPeerConnectionCall("negotiateNonCryptoCall();");
 }
 
 // This test can negotiate an SDP offer that includes a b=AS:xx to control
@@ -244,8 +237,7 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest, CreateOfferWithOfferOptions) {
 }
 
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest, CallInsideIframe) {
-  MakeTypicalPeerConnectionCall("callInsideIframe({video: true, audio:true});",
-                                /*use_manual_reply=*/false);
+  MakeTypicalPeerConnectionCall("callInsideIframe({video: true, audio:true});");
 }
 
 // Tests that SetConfiguration succeeds and triggers an ICE restart on the next
