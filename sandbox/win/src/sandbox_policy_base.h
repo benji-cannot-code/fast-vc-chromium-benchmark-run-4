3bc3cae252b5d1a3fbd715590d7443f06ae43641
Alex Gough <ajgo@chromium.org>
2023-03-29 17:54:25 +0000

    Remove --allow-no-sandbox-job as has no effect
    We no longer use the GetAllowNoSandboxJob[0] property of the sandbox
    TargetConfig so we can remove these methods and the command line
    flag which set them.
    
    [0] https://chromium-review.googlesource.com/c/chromium/src/+/4032432/7/sandbox/policy/win/sandbox_win.cc
    
    Bug: 1385495,1428756
    Change-Id: If966f10e9c4e2577d22d287587b14d121f935436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4378343
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123700}

--- sandbox/win/src/sandbox_policy_base.h
+++ sandbox/win/src/sandbox_policy_base.h
@@ -61,8 +61,6 @@ class ConfigBase final : public TargetConfig {
   ResultCode SetJobLevel(JobLevel job_level, uint32_t ui_exceptions) override;
   JobLevel GetJobLevel() const override;
   void SetJobMemoryLimit(size_t memory_limit) override;
-  void SetAllowNoSandboxJob() override;
-  bool GetAllowNoSandboxJob() override;
   ResultCode AddRule(SubSystem subsystem,
                      Semantics semantics,
                      const wchar_t* pattern) override;
@@ -138,7 +136,6 @@ class ConfigBase final : public TargetConfig {
   MitigationFlags delayed_mitigations_;
   bool add_restricting_random_sid_;
   bool lockdown_default_dacl_;
-  bool allow_no_sandbox_job_;
   bool is_csrss_connected_;
   size_t memory_limit_;
   uint32_t ui_exceptions_;
