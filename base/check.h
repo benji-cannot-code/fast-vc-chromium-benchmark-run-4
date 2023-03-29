069f9bb638880eae5a9701936b2ef05e58a8f2bb
thefrog <thefrog@chromium.org>
2023-03-29 20:37:38 +0000

    Revert "Reland "Allow dumping for unrelated NOTREACHED()+DCHECK()s""
    This reverts commit 019a8927093b606c8ee92464d409ef927ae61ebc.
    
    Reason for revert: CheckDeathTest.NotReached test is failing.
    See https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chrome/63569/overview.
    
    Original change's description:
    > Reland "Allow dumping for unrelated NOTREACHED()+DCHECK()s"
    >
    > This reverts commit c2cc31ebc575ad26a8c6afa189e12bc3b18dfef5.
    >
    > Reason for revert: Reland with test fix.
    >
    > Original change's description:
    > > Revert "Allow dumping for unrelated NOTREACHED()+DCHECK()s"
    > >
    > > This reverts commit a2acec51ed7257d24903cd5aa6b71e2e03226b40.
    > >
    > > Reason for revert: CheckDeathTest.NotReached failing on Cast Audio Linux.
    > >
    > > https://ci.chromium.org/ui/p/chromium/builders/ci/Cast%20Audio%20Linux/163682/overview
    > >
    > > Original change's description:
    > > > Allow dumping for unrelated NOTREACHED()+DCHECK()s
    > > >
    > > > Before: DCHECK and NOTREACHED resulted in a single dump per process via
    > > >         DumpWithoutCrashingUnthrottled, for any/all locations.
    > > >
    > > > After: DCHECK and NOTREACHED will dump at most once per 30 days (so
    > > >        effectively, once) per location in source.
    > > >
    > > > This replaces previous NOTREACHED() and DCHECK() dumping that tried to
    > > > dump at most once per process.
    > > >
    > > > Instead a base::Location argument is supplied to all DCHECK() and
    > > > NOTREACHED() invocations and forwarded to
    > > > base::debug::DumpWithoutCrashing(). This effectively means that each
    > > > DCHECK() and NOTREACHED() gets a chance to dump and can do so up to once
    > > > every 30 days.
    > > >
    > > > This removes the risk of DCHECKs blocking other subsequent DCHECKs and
    > > > we should generate reports for all unrelated DCHECKs (and even related
    > > > ones).
    > > >
    > > > Bug: 1427580
    > > > Change-Id: I1bdf20052afecf4d1cb2cd09921433fc1cadacb6
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297747
    > > > Auto-Submit: Peter Boström <pbos@chromium.org>
    > > > Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    > > > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > > > Reviewed-by: Will Harris <wfh@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1123118}
    > >
    > > Bug: 1427580
    > > Change-Id: If67ace3bbb132babeb120f5941c006e7cc9540ca
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4376898
    > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > Reviewed-by: Will Harris <wfh@chromium.org>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Commit-Queue: Chris Mumford <cmumford@google.com>
    > > Reviewed-by: Peter Boström <pbos@chromium.org>
    > > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1123234}
    >
    > Bug: 1427580
    > Change-Id: Id69161a73c79d42c09e86048f47425763725b705
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375112
    > Auto-Submit: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1123681}
    
    Bug: 1427580
    Change-Id: Idbd5da47806a425688e22559ac7fdd604aef33d2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4383032
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Owners-Override: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123816}

--- base/check.h
+++ base/check.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/dcheck_is_on.h"
 #include "base/debug/debugging_buildflags.h"
 #include "base/immediate_crash.h"
-#include "base/location.h"
 
 // This header defines the CHECK, DCHECK, and DPCHECK macros.
 //
@@ -69,16 +68,12 @@ class BASE_EXPORT CheckError {
 
   static CheckError Check(const char* file, int line, const char* condition);
 
-  static CheckError DCheck(
-      const char* condition,
-      const base::Location& location = base::Location::Current());
+  static CheckError DCheck(const char* file, int line, const char* condition);
 
   static CheckError PCheck(const char* file, int line, const char* condition);
   static CheckError PCheck(const char* file, int line);
 
-  static CheckError DPCheck(
-      const char* condition,
-      const base::Location& location = base::Location::Current());
+  static CheckError DPCheck(const char* file, int line, const char* condition);
 
   static CheckError NotImplemented(const char* file,
                                    int line,
@@ -105,8 +100,7 @@ class BASE_EXPORT CheckError {
 
 class BASE_EXPORT NotReachedError : public CheckError {
  public:
-  static NotReachedError NotReached(
-      const base::Location& location = base::Location::Current());
+  static NotReachedError NotReached(const char* file, int line);
 
   // Used to trigger a NOTREACHED() without providing file or line while also
   // discarding log-stream arguments. See base/notreached.h.
@@ -189,10 +183,14 @@ class BASE_EXPORT NotReachedNoreturnError : public CheckError {
 
 #if DCHECK_IS_ON()
 
-#define DCHECK(condition) \
-  CHECK_FUNCTION_IMPL(::logging::CheckError::DCheck(#condition), condition)
-#define DPCHECK(condition) \
-  CHECK_FUNCTION_IMPL(::logging::CheckError::DPCheck(#condition), condition)
+#define DCHECK(condition)                                            \
+  CHECK_FUNCTION_IMPL(                                               \
+      ::logging::CheckError::DCheck(__FILE__, __LINE__, #condition), \
+      condition)
+#define DPCHECK(condition)                                            \
+  CHECK_FUNCTION_IMPL(                                                \
+      ::logging::CheckError::DPCheck(__FILE__, __LINE__, #condition), \
+      condition)
 
 #else
 
