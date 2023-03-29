bcc15eeef828ea31b22cb73cda3f77edb5909bec
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2023-03-29 21:31:42 +0000

    [ViewsAX] Remove unnecessary #include from BrowserAccessibilityStateImpl
    Redo of CL:4379339, which couldn't land for some reason.
    
    BrowserAccessibilityState and its impl file will soon be moved out of
    the content layer and into the UI one to allow for the ViewsAX project.
    This CL removes an unnecessary include of a file reachable from the
    content layer only.
    
    We're currently following the plan #1 outlined in
    https://docs.google.com/document/d/1zBXIH2ztsSHGmS0_5JKWPZRKgDG5lKWhct_FB8k8SL0/edit?usp=sharing.
    
    Bug: 1049261
    Change-Id: I192b92f4620b206e8296892ffa7780bde742a7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382569
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1123851}

--- content/browser/accessibility/browser_accessibility_state_impl.cc
+++ content/browser/accessibility/browser_accessibility_state_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
