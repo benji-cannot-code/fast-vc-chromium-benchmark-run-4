0799341b848382563412c84ae61859de6b0b7611
Jeff Yoon <jeffyoon@google.com>
2023-03-29 21:32:05 +0000

    Post process for ChromeOS by pulling profraw files.
    The existing flags used by Android are reworked for both Android and
    ChromeOS by introducing a platform flag for pulling devices.
    
    For Lacros, profiles are written on device and require a similar
    mechanism to pull the profraw files from device to the tester machine
    so that they can be included as part of the CAS output results
    for further processing by the builder.
    
    Bug: 1417071
    Change-Id: I10e1d76de4e03851bc99ab740b25821af516d95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370618
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123852}

--- tools/perf/core/path_util.py
+++ tools/perf/core/path_util.py
@@ -28,7 +28,7 @@ def GetChromiumSrcDir():
       os.path.dirname(__file__), '..', '..', '..'))
 
 
-def GetDeviceInteractionDir():
+def GetAndroidDeviceInteractionToPath():
   return os.path.join(GetChromiumSrcDir(), 'third_party', 'catapult', 'devil')
 
 
@@ -75,8 +75,8 @@ def GetVariationsDir():
   return os.path.join(GetChromiumSrcDir(), 'tools', 'variations')
 
 
-def AddDeviceInteractionToPath():
-  device_interaction_path = GetDeviceInteractionDir()
+def AddAndroidDeviceInteractionToPath():
+  device_interaction_path = GetAndroidDeviceInteractionToPath()
   if device_interaction_path not in sys.path:
     sys.path.insert(1, device_interaction_path)
 
