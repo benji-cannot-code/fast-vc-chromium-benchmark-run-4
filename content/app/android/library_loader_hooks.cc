6ee9fef6d9a3cd8395341db8594310090fd35b28
Andrew Grieve <agrieve@chromium.org>
2023-03-29 17:45:25 +0000

    Reland "Android: Initialize ICU earlier"
    This reverts commit 44052f5a3476c626da7dab2e1c1eefa9d9afa266.
    
    Reason for reland: Moved to OnLibraryLoaded to fix tests
    
    Original change's description:
    > Revert "Android: Initialize ICU earlier"
    >
    > This reverts commit 1a6b5c6c1e26394af4385df6e6899f13a7c607c8.
    >
    > Reason for revert: causes ChildProcessLauncher tests failures
    > (https://crbug.com/1427991)
    >
    > Original change's description:
    > > Android: Initialize ICU earlier
    > >
    > > And don't bother passing FD's via IPC to renderers. Have them open the
    > > file directly instead (isolated processes can access the APK).
    > >
    > > This will allow JNI methods to access ICU before content main is
    > > called (as is the issue on the attached bug).
    > >
    > > Pinpoint shows this change to be neutral to slightly positive for
    > > startup.mobile.
    > >
    > > Bug: 1418738
    > > Change-Id: Ibda66a1548bc1d1e76155fdd44dad1241e72e9fb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357575
    > > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1121905}
    >
    > Bug: 1418738, 1427991
    > Change-Id: I90fa716159d90882adbd9dfdc771c1f5cef22f38
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370076
    > Owners-Override: Alex Ilin <alexilin@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Alex Ilin <alexilin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1122352}
    
    Bug: 1418738, 1427991
    Change-Id: I4b982d64b8bb005019944e9ed72e77674363b32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4378169
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123698}

--- content/app/android/library_loader_hooks.cc
+++ content/app/android/library_loader_hooks.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/app/android/library_loader_hooks.h"
 
 #include "base/android/reached_code_profiler.h"
+#include "base/i18n/icu_util.h"
 #include "base/logging.h"
 #include "base/process/current_process.h"
 #include "base/trace_event/trace_event.h"
@@ -55,6 +56,13 @@ bool LibraryLoaded(JNIEnv* env,
     VLOG(0) << "Chromium logging enabled: level = " << logging::GetMinLogLevel()
             << ", default verbosity = " << logging::GetVlogVerbosity();
   }
+
+#if ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE
+  // Initialize ICU early so that it can be used by JNI calls before
+  // ContentMain() is called.
+  TRACE_EVENT0("startup", "InitializeICU");
+  CHECK(base::i18n::InitializeICU());
+#endif
 
   // Content Schemes need to be registered as early as possible after the
   // CommandLine has been initialized to allow java and tests to use GURL before
