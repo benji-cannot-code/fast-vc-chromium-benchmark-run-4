87c35dc94030df08834e90f5d7a741569117ac2e
Thomas Quintanilla <tquintanilla@google.com>
2023-03-29 14:14:27 +0000

    Validity checking for Finch report time field trial params
    Validate that the field trial params are not negative. Also verify that the first report window is not greater than the second.
    
    Bug: 1427582
    Change-Id: I9b030adc765f753ec06801e1b69a30182b4980fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370471
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123570}

--- content/browser/attribution_reporting/attribution_storage_delegate_impl.cc
+++ content/browser/attribution_reporting/attribution_storage_delegate_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/cxx17_backports.h"
 #include "base/guid.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/rand_util.h"
@@ -64,9 +65,23 @@ AttributionStorageDelegateImpl::AttributionStorageDelegateImpl(
     : AttributionStorageDelegateImpl(noise_mode,
                                      delay_mode,
                                      AttributionConfig()) {
-  // TODO(tquintanilla): Investigate techniques to valid these params.
-  config_.aggregate_limit.min_delay = kAggregateReportMinDelay.Get();
-  config_.aggregate_limit.delay_span = kAggregateReportDelaySpan.Get();
+  if (base::TimeDelta min_delay = kAggregateReportMinDelay.Get();
+      !min_delay.is_negative()) {
+    config_.aggregate_limit.min_delay = min_delay;
+  } else {
+    LOG(WARNING) << "Minimum aggregate delay declared negative, "
+                 << "using default value: "
+                 << AttributionConfig::AggregateLimit::kDefaultMinDelay;
+  }
+
+  if (base::TimeDelta delay_span = kAggregateReportDelaySpan.Get();
+      !delay_span.is_negative()) {
+    config_.aggregate_limit.delay_span = delay_span;
+  } else {
+    LOG(WARNING) << "Aggregate delay span declared negative, "
+                 << "using default value: "
+                 << AttributionConfig::AggregateLimit::kDefaultDelaySpan;
+  }
 }
 
 AttributionStorageDelegateImpl::AttributionStorageDelegateImpl(
