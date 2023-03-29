da9206e4354eb866ee841957720d518861265e22
Chris Fredrickson <cfredric@chromium.org>
2023-03-29 20:29:51 +0000

    Reuse helpers; stop running iframe tests concurrently; set test context
    This CL also adds some missing return statements that cause the test
    to drop some promises instead of awaiting them properly.
    
    Bug: 1427180
    Change-Id: I7e7411e871c13da78c30bf1308a2ada11f577553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382128
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123812}

--- third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess-non-fully-active.sub.https.window.js
+++ third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess-non-fully-active.sub.https.window.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 promise_test(t => {
-  const promise = RunRequestStorageAccessInDetachedFrame();
+  const promise = CreateDetachedFrame().requestStorageAccess();
   const description = "document.requestStorageAccess() call in a detached frame";
   // Can't use `promise_rejects_dom` here, since the error comes from the wrong global.
   return promise.then(t.unreached_func("Should have rejected: " + description), (e) => {
@@ -14,6 +14,6 @@ promise_test(t => {
 }, "[non-fully-active] document.requestStorageAccess() should not resolve when run in a detached frame");
 
 promise_test(t => {
-  return promise_rejects_dom(t, 'InvalidStateError', RunRequestStorageAccessViaDomParser(),
+  return promise_rejects_dom(t, 'InvalidStateError', CreateDocumentViaDOMParser().requestStorageAccess(),
    "document.requestStorageAccess() in a detached DOMParser result");
 }, "[non-fully-active] document.requestStorageAccess() should not resolve when run in a detached DOMParser document");
