f7aaf6f15e5179eb13c1be25917de4ab11be50e8
Thuong Phan <thuongphan@chromium.org>
2023-03-29 19:11:56 +0000

    personalization: Add metric for keyboard backlight display type
    Add a new metric
    "Ash.Personalization.KeyboardBacklight.DisplayType.Settled" to
    record the display type of keyboard backlight color that the user has
    settled on.
    
    BUG=b:274961222
    Test=Set keyboard backlight color as a static color. Verify that
    the value in
    shows correctly after UMA upload for the day.
    TEST=Set the keyboard backlight color with customized zones. Repeat the
    histogram verification step.
    
    chrome: //histograms/Ash.Personlization.KeyboardBacklight.Display.Settled
    Change-Id: I0121f25cd58b3b3526aef638143deafc6bdc844b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375042
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1123743}

--- chrome/browser/ash/web_applications/personalization_app/keyboard_backlight_color_metrics_provider.cc
+++ chrome/browser/ash/web_applications/personalization_app/keyboard_backlight_color_metrics_provider.cc
@@ -35,12 +35,16 @@ void KeyboardBacklightColorMetricsProvider::ProvideCurrentSessionData(
 
   const auto displayType =
       keyboard_backlight_color_controller->GetDisplayType(account_id);
+
   switch (displayType) {
     case DisplayType::kStatic: {
+      base::UmaHistogramEnumeration(
+          kPersonalizationKeyboardBacklightDisplayTypeSettledHistogramName,
+          DisplayType::kStatic);
       auto backlight_color =
           keyboard_backlight_color_controller->GetBacklightColor(account_id);
       base::UmaHistogramEnumeration(
-          "Ash.Personalization.KeyboardBacklight.Color.Settled",
+          kPersonalizationKeyboardBacklightColorSettledHistogramName,
           backlight_color);
       return;
     }
@@ -49,6 +53,9 @@ void KeyboardBacklightColorMetricsProvider::ProvideCurrentSessionData(
           ash::Shell::Get()->rgb_keyboard_manager()->GetZoneCount() <= 1) {
         return;
       }
+      base::UmaHistogramEnumeration(
+          kPersonalizationKeyboardBacklightDisplayTypeSettledHistogramName,
+          DisplayType::kMultiZone);
       auto zone_colors =
           keyboard_backlight_color_controller->GetBacklightZoneColors(
               account_id);
