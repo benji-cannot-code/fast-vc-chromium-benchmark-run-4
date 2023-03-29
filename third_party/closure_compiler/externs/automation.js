6c7feb5b223032fe916e5c2951f8ce8e1eae2a5d
Claudio DeSouza <cdesouza@igalia.com>
2023-03-29 18:34:27 +0000

    Run generate_all_externs.py
    This CL has corrections provided by a run of generate_all_externs.py, to
    quieten pre-submit warnings that are being triggered on the main branch.
    
    Bug: n/a
    Change-Id: I73c387f5310b73d3a516b9430821848ee9be686b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379200
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123731}

--- third_party/closure_compiler/externs/automation.js
+++ third_party/closure_compiler/externs/automation.js
@@ -455,10 +455,10 @@ chrome.automation.NameFromType = {
   CAPTION: 'caption',
   CONTENTS: 'contents',
   PLACEHOLDER: 'placeholder',
+  POPOVER_ATTRIBUTE: 'popoverAttribute',
   RELATED_ELEMENT: 'relatedElement',
   TITLE: 'title',
   VALUE: 'value',
-  POPOVER_ATTRIBUTE: 'popoverAttribute',
 };
 
 /**
