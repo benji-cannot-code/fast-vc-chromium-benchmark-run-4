e0e387e343a0bf2b70f13ab434a947869fee6803
Ari Chivukula <arichiv@chromium.org>
2023-03-29 15:43:22 +0000

    [Permissions Policy + CSP] (3) Use CSP parsing and comparison paths
    Let's stop using our custom system and just defer to the CSP logic where
    possible. This should be a no-op functionality wise.
    
    This CL is part of a series:
    (1) Ban opaque origins from OriginWithPossibleWildcard
    (2) Switch to using underlying CSP data type
    (3) Use CSP parsing and comparison paths
    (4) Fork CSP Mojom to avoid sending unused data
    
    Bug: 1418009
    Change-Id: I81b3be6f406241e6d7f278c7070c2b60fbdfb3d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4345816
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123611}

--- content/browser/site_per_process_browsertest.cc
+++ content/browser/site_per_process_browsertest.cc
@@ -7317,8 +7317,6 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessBrowserTest,
   const blink::ParsedPermissionsPolicy initial_effective_policy =
       root->child_at(2)->effective_frame_policy().container_policy;
   EXPECT_EQ(1UL, initial_effective_policy[0].allowed_origins.size());
-  EXPECT_FALSE(
-      initial_effective_policy[0].allowed_origins.begin()->origin.opaque());
 
   // Set the "sandbox" attribute; pending policy should update, and should now
   // be flagged as matching the opaque origin of the frame (without containing
@@ -7331,8 +7329,6 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessBrowserTest,
   const blink::ParsedPermissionsPolicy updated_pending_policy =
       root->child_at(2)->pending_frame_policy().container_policy;
   EXPECT_EQ(1UL, updated_effective_policy[0].allowed_origins.size());
-  EXPECT_FALSE(
-      updated_effective_policy[0].allowed_origins.begin()->origin.opaque());
   EXPECT_TRUE(updated_pending_policy[0].matches_opaque_src);
   EXPECT_EQ(0UL, updated_pending_policy[0].allowed_origins.size());
 
--- third_party/blink/public/common/permissions_policy/origin_with_possible_wildcards.h
+++ third_party/blink/public/common/permissions_policy/origin_with_possible_wildcards.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_PERMISSIONS_POLICY_ORIGIN_WITH_POSSIBLE_WILDCARDS_H_
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_PERMISSIONS_POLICY_ORIGIN_WITH_POSSIBLE_WILDCARDS_H_
 
+#include "services/network/public/mojom/content_security_policy.mojom.h"
 #include "third_party/blink/public/common/common_export.h"
 #include "url/origin.h"
 
@@ -58,8 +59,7 @@ struct BLINK_COMMON_EXPORT OriginWithPossibleWildcards {
   // https://github.com/w3c/webappsec-permissions-policy/pull/482
   bool DoesMatchOrigin(const url::Origin& match_origin) const;
 
-  url::Origin origin;
-  bool has_subdomain_wildcard{false};
+  network::mojom::CSPSource csp_source;
 };
 
 bool BLINK_COMMON_EXPORT operator==(const OriginWithPossibleWildcards& lhs,
