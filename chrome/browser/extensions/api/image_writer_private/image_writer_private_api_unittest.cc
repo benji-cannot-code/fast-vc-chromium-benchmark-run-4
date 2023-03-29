03c884561deb3fb84081bb61046c2501658338b9
Claudio DeSouza <cdesouza@igalia.com>
2023-03-29 22:02:48 +0000

    [extensions] Remove uses of extension_api_unittest.h (33/N)
    This CL is part of a batch of CLs migrating all uses of
    extension_api_unittest.h to more recent alternatives.
    
    This change corrects the file under the path:
       /chrome/browser/extensions/api/image_writer_private
    
    This CL was uploaded by git cl split.
    
    Bug: 394840
    Change-Id: Id7608fb998d7a094cb20a996216ae23dbb8284f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381895
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1123872}

--- chrome/browser/extensions/api/image_writer_private/image_writer_private_api_unittest.cc
+++ chrome/browser/extensions/api/image_writer_private/image_writer_private_api_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/image_writer_private/removable_storage_provider.h"
 #include "chrome/browser/extensions/api/image_writer_private/test_utils.h"
 #include "chrome/browser/extensions/extension_api_unittest.h"
-#include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/common/extensions/api/image_writer_private.h"
 #include "chromeos/components/disks/disks_prefs.h"
 #include "components/prefs/pref_service.h"
--- chrome/browser/extensions/api/image_writer_private/image_writer_private_apitest.cc
+++ chrome/browser/extensions/api/image_writer_private/image_writer_private_apitest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/image_writer_private/removable_storage_provider.h"
 #include "chrome/browser/extensions/api/image_writer_private/test_utils.h"
 #include "chrome/browser/extensions/extension_apitest.h"
-#include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/common/extensions/api/image_writer_private.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -22,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 using api::image_writer_private::RemovableStorageDevice;
-using extension_function_test_utils::RunFunctionAndReturnError;
 using extensions::image_writer::FakeImageWriterClient;
 
 class ImageWriterPrivateApiTest : public ExtensionApiTest {
