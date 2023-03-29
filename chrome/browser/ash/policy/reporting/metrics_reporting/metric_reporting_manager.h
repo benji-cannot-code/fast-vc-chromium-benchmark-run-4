c1e63798dc9cca574a8a442f8e6607bc76a0c63a
Vignesh Shenvi <vshenvi@google.com>
2023-03-29 17:34:12 +0000

    Initialize app usage telemetry collectors and samplers
    This change integrates the AppUsageCollector and
    AppUsageTelemetrySampler with the MetricReportingManager so we can start
    collecting and reporting app usage telemetry data. We use pre-existing
    policies for these components gated behind a feature flag until we
    introduce actual policies controlling these components when ready.
    
    Bug: b:260803048
    Change-Id: I741276aaf2f2400cfcb160cf8804365ce6f7bd72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375224
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123693}

--- chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.h
+++ chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.h
@@ -14,8 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/sequence_checker.h"
 #include "base/strings/string_piece_forward.h"
+#include "base/thread_annotations.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_collector.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_sampler_handler.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_reporting_settings.h"
 #include "chrome/browser/ash/policy/status_collector/managed_session_service.h"
@@ -173,6 +175,9 @@ class MetricReportingManager : public policy::ManagedSessionService::Observer,
   void InitNetworkConfiguredSampler(const std::string& sampler_name,
                                     std::unique_ptr<Sampler> sampler);
 
+  // Initializes app telemetry samplers.
+  void InitAppCollectors();
+
   void InitAudioCollectors();
 
   void InitPeripheralsCollectors();
@@ -231,6 +236,11 @@ class MetricReportingManager : public policy::ManagedSessionService::Observer,
 
   std::vector<std::unique_ptr<MetricEventObserverManager>>
       event_observer_managers_ GUARDED_BY_CONTEXT(sequence_checker_);
+
+  // App usage collector used to collect app usage reports from the
+  // `AppPlatformMetrics` component.
+  std::unique_ptr<AppUsageCollector> app_usage_collector_
+      GUARDED_BY_CONTEXT(sequence_checker_);
 
   std::unique_ptr<Delegate> delegate_;
 };
