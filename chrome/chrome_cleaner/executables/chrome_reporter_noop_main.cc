0b722ab7a83791d05e71ea09bb2f6c80570e3860
Daniel Rubery <drubery@chromium.org>
2023-03-29 17:03:59 +0000

    Create no-op software reporter tool
    As we turn down this feature, we want old versions of Chrome to fetch a
    no-op reporter so that they don't try to do cleanup. This makes it safer
    to remove cleaner infrastructure.
    
    Bug: b/272352918
    Change-Id: I7341be0bb4082f60a7d206ad529611be320938cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375858
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123665}

---
+++ chrome/chrome_cleaner/executables/chrome_reporter_noop_main.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <windows.h>
+
+#include "components/chrome_cleaner/public/constants/result_codes.h"
+
+int APIENTRY wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int) {
+  return chrome_cleaner::RESULT_CODE_NO_PUPS_FOUND;
+}
