37231501f050328b87f5edda89d7c04b2addb95e
Javier Castro <jacastro@chromium.org>
2023-03-29 03:57:21 +0000

    Reland "[Tailored Security] Store the user action strings as constants"
    This is a reland of commit eeeaa9196fa58017d16d4cb6bde92e668e462464
    Reason:
    The real culprit of the tree closure at that time was https://crrev.com/c/4355362 instead.
    Creating a reland of this CL.
    
    Original change's description:
    > [Tailored Security] Store the user action strings as constants
    >
    > The tailored security account integration needs to record user actions
    > across different platforms. This change updates the android and desktop
    > dialog implementations so that they depend on string constants that can
    > be used on either platform.
    >
    > Bug: 1428404
    > Change-Id: If58fbd320f2ea34651c0a00516c43f29f6de10c0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375341
    > Commit-Queue: Javier Castro <jacastro@chromium.org>
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1123390}
    
    Bug: 1428404
    Change-Id: I9fd49913c454831ce7777baca1066b70f81de79f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379602
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Owners-Override: Robert Lin <robertlin@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1123441}

--- chrome/browser/safe_browsing/tailored_security/consented_message_android.cc
+++ chrome/browser/safe_browsing/tailored_security/consented_message_android.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/messages/android/message_dispatcher_bridge.h"
 #include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_outcome.h"
+#include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service_util.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -78,10 +79,10 @@ TailoredSecurityConsentedModalAndroid::TailoredSecurityConsentedModalAndroid(
 
   if (is_enable_message_) {
     base::RecordAction(base::UserMetricsAction(
-        "SafeBrowsing.AccountIntegration.EnabledDialog.Shown"));
+        safe_browsing::kTailoredSecurityEnabledDialogShown));
   } else {
     base::RecordAction(base::UserMetricsAction(
-        "SafeBrowsing.AccountIntegration.DisabledDialog.Shown"));
+        safe_browsing::kTailoredSecurityDisabledDialogShown));
   }
   messages::MessageDispatcherBridge::Get()->EnqueueWindowScopedMessage(
       message_.get(), web_contents_->GetTopLevelNativeWindow(),
@@ -107,11 +108,10 @@ void TailoredSecurityConsentedModalAndroid::HandleSettingsClicked() {
                            SettingsAccessPoint::kTailoredSecurity);
   if (is_enable_message_) {
     base::RecordAction(base::UserMetricsAction(
-        "SafeBrowsing.AccountIntegration.EnabledDialog.SettingsButtonClicked"));
+        safe_browsing::kTailoredSecurityEnabledDialogSettingsButtonClicked));
   } else {
-    base::RecordAction(
-        base::UserMetricsAction("SafeBrowsing.AccountIntegration."
-                                "DisabledDialog.SettingsButtonClicked"));
+    base::RecordAction(base::UserMetricsAction(
+        safe_browsing::kTailoredSecurityDisabledDialogSettingsButtonClicked));
   }
   LogOutcome(TailoredSecurityOutcome::kSettings, is_enable_message_);
   DismissMessageInternal(messages::DismissReason::SECONDARY_ACTION);
@@ -121,10 +121,10 @@ void TailoredSecurityConsentedModalAndroid::HandleMessageDismissed(
     messages::DismissReason dismiss_reason) {
   if (is_enable_message_) {
     base::RecordAction(base::UserMetricsAction(
-        "SafeBrowsing.AccountIntegration.EnabledDialog.Dismissed"));
+        safe_browsing::kTailoredSecurityEnabledDialogDismissed));
   } else {
     base::RecordAction(base::UserMetricsAction(
-        "SafeBrowsing.AccountIntegration.DisabledDialog.Dismissed"));
+        safe_browsing::kTailoredSecurityDisabledDialogDismissed));
   }
   LogOutcome(TailoredSecurityOutcome::kDismissed, is_enable_message_);
   message_.reset();
@@ -135,10 +135,10 @@ void TailoredSecurityConsentedModalAndroid::HandleMessageDismissed(
 void TailoredSecurityConsentedModalAndroid::HandleMessageAccepted() {
   if (is_enable_message_) {
     base::RecordAction(base::UserMetricsAction(
-        "SafeBrowsing.AccountIntegration.EnabledDialog.OkButtonClicked"));
+        safe_browsing::kTailoredSecurityEnabledDialogOkButtonClicked));
   } else {
     base::RecordAction(base::UserMetricsAction(
-        "SafeBrowsing.AccountIntegration.DisabledDialog.OkButtonClicked"));
+        safe_browsing::kTailoredSecurityDisabledDialogOkButtonClicked));
   }
   LogOutcome(TailoredSecurityOutcome::kAccepted, is_enable_message_);
 }
