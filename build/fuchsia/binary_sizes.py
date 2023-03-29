7b294ba5843a6462695cea5a793d88fea87e3377
Zijie He <zijiehe@google.com>
2023-03-29 22:50:24 +0000

    [Fuchsia] Force test_name_prefix in binary_sizes to ensure resultsdb understand the location
    By adding the prefix to existing `chrome_fuchsia_compressed` to create a
    full path.
    After this CL, the test results of fuchsia_sizes will be considered as
    part of build/fuchsia/:
    https://ci.chromium.org/ui/p/chromium/builders/try/fuchsia-arm64-rel/147104/test-results?sortby=&groupby=&q=fuchsia_sizes+
    (The page needs a while to load.)
    
    Bug: 1364368
    Change-Id: I1b0a0f394fb1e2f5f6c9bc1dee052654427c5a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4330080
    Reviewed-by: Kuan Huang <kuanhuang@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123912}

--- build/fuchsia/binary_sizes.py
+++ build/fuchsia/binary_sizes.py
@@ -103,7 +103,9 @@ def CreateTestResults(test_status, timestamp):
   results = {
       'tests': {},
       'interrupted': False,
-      'path_delimiter': '.',
+      'metadata': {
+          'test_name_prefix': 'build/fuchsia/'
+      },
       'version': 3,
       'seconds_since_epoch': timestamp,
   }
