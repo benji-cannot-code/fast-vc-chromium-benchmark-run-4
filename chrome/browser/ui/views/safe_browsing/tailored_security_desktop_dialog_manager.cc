a5aa8088d8c746440aedd83635ed4d4613647fd8
Huanpo Lin <robertlin@chromium.org>
2023-03-29 01:10:42 +0000

    Revert "[Tailored Security] Store the user action strings as constants"
    This reverts commit eeeaa9196fa58017d16d4cb6bde92e668e462464.
    
    Reason for revert:
    Tree is closed, and it seems that the only CL in the range is this one.
    https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-amd64-generic-dbg/67258/overview
    
    Original change's description:
    > [Tailored Security] Store the user action strings as constants
    >
    > The tailored security account integration needs to record user actions
    > across different platforms. This change updates the android and desktop
    > dialog implementations so that they depend on string constants that can
    > be used on either platform.
    >
    > Bug: 1428404
    > Change-Id: If58fbd320f2ea34651c0a00516c43f29f6de10c0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375341
    > Commit-Queue: Javier Castro <jacastro@chromium.org>
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1123390}
    
    Bug: 1428404
    Change-Id: I8862a3c3d7c4eecdccb788e9473f5ece4e4679b2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379958
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Robert Lin <robertlin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123395}

--- chrome/browser/ui/views/safe_browsing/tailored_security_desktop_dialog_manager.cc
+++ chrome/browser/ui/views/safe_browsing/tailored_security_desktop_dialog_manager.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/theme_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_outcome.h"
-#include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service_util.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -89,11 +88,10 @@ class DisabledDialogModelDelegate : public TailoredSecurityDialogModelDelegate {
   DisabledDialogModelDelegate()
       : TailoredSecurityDialogModelDelegate(
             kDisabledDialogOutcome,
-            base::UserMetricsAction(
-                safe_browsing::kTailoredSecurityDisabledDialogOkButtonClicked),
-            base::UserMetricsAction(
-                safe_browsing::
-                    kTailoredSecurityDisabledDialogSettingsButtonClicked)) {}
+            base::UserMetricsAction("SafeBrowsing.AccountIntegration."
+                                    "DisabledDialog.OkButtonClicked"),
+            base::UserMetricsAction("SafeBrowsing.AccountIntegration."
+                                    "DisabledDialog.SettingsButtonClicked")) {}
 };
 
 class EnabledDialogModelDelegate : public TailoredSecurityDialogModelDelegate {
@@ -101,11 +99,10 @@ class EnabledDialogModelDelegate : public TailoredSecurityDialogModelDelegate {
   EnabledDialogModelDelegate()
       : TailoredSecurityDialogModelDelegate(
             kEnabledDialogOutcome,
-            base::UserMetricsAction(
-                safe_browsing::kTailoredSecurityEnabledDialogOkButtonClicked),
-            base::UserMetricsAction(
-                safe_browsing::
-                    kTailoredSecurityEnabledDialogSettingsButtonClicked)) {}
+            base::UserMetricsAction("SafeBrowsing.AccountIntegration."
+                                    "EnabledDialog.OkButtonClicked"),
+            base::UserMetricsAction("SafeBrowsing.AccountIntegration."
+                                    "EnabledDialog.SettingsButtonClicked")) {}
 };
 
 TailoredSecurityDesktopDialogManager::TailoredSecurityDesktopDialogManager() =
@@ -156,7 +153,7 @@ void TailoredSecurityDesktopDialogManager::ShowEnabledDialogForBrowser(
   }
   close_dialog_callback_ = model_delegate_ptr->GetCloseDialogCallback();
   base::RecordAction(base::UserMetricsAction(
-      safe_browsing::kTailoredSecurityEnabledDialogShown));
+      "SafeBrowsing.AccountIntegration.EnabledDialog.Shown"));
   constrained_window::ShowBrowserModal(std::move(dialog_model),
                                        browser->window()->GetNativeWindow());
 }
@@ -200,7 +197,7 @@ void TailoredSecurityDesktopDialogManager::ShowDisabledDialogForBrowser(
   }
   close_dialog_callback_ = model_delegate_ptr->GetCloseDialogCallback();
   base::RecordAction(base::UserMetricsAction(
-      safe_browsing::kTailoredSecurityDisabledDialogShown));
+      "SafeBrowsing.AccountIntegration.DisabledDialog.Shown"));
   constrained_window::ShowBrowserModal(std::move(dialog_model),
                                        browser->window()->GetNativeWindow());
 }
