6eb44079e2b06f032385511d4c882cd09663ce9b
YuhanYang <yyhyyh@google.com>
2023-03-29 19:22:19 +0000

    Settings Split: Call RecordKeyboardInitialMetrics in controller
    Calling RecordKeyboardInitialMetrics when keyboard list updates
    and a new user signed in.
    
    Test: ash_unittests
    Bug: b/241965700
    Change-Id: I3ba61330ca0301d04d162513415077d0b32f284a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375455
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123751}

--- ash/system/input_device_settings/input_device_settings_controller_unittest.cc
+++ ash/system/input_device_settings/input_device_settings_controller_unittest.cc
@@ -23,8 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/ranges/algorithm.h"
 #include "base/ranges/functional.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_simple_task_runner.h"
+#include "components/account_id/account_id.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/testing_pref_service.h"
 #include "ui/events/devices/device_data_manager_test_api.h"
@@ -53,6 +55,7 @@ class FakeKeyboardPrefHandler : public KeyboardPrefHandler {
  public:
   void InitializeKeyboardSettings(PrefService* pref_service,
                                   mojom::Keyboard* keyboard) override {
+    keyboard->settings = mojom::KeyboardSettings::New();
     num_keyboard_settings_initialized_++;
   }
   void UpdateKeyboardSettings(PrefService* pref_service,
@@ -318,6 +321,27 @@ TEST_F(InputDeviceSettingsControllerTest, KeyboardSettingsUpdateMultiple) {
                                    mojom::KeyboardSettings::New());
   EXPECT_EQ(observer_->num_keyboards_settings_updated(), 2u);
   EXPECT_EQ(keyboard_pref_handler_->num_keyboard_settings_updated(), 1u);
+}
+
+TEST_F(InputDeviceSettingsControllerTest, RecordsMetricsInitialSettings) {
+  // Initially expect no user preferences recorded.
+  base::HistogramTester histogram_tester;
+
+  controller_->OnKeyboardListUpdated(
+      {kSampleKeyboardUsb, kSampleKeyboardBluetooth}, {});
+
+  histogram_tester.ExpectTotalCount(
+      "ChromeOS.Settings.Device.Keyboard.ExternalChromeOS.TopRowAreFKeys."
+      "Initial",
+      /*expected_count=*/2u);
+  const AccountId account_id = AccountId::FromUserEmail(kUserEmail1);
+  SimulateUserLogin(account_id);
+  task_runner_->RunUntilIdle();
+
+  histogram_tester.ExpectTotalCount(
+      "ChromeOS.Settings.Device.Keyboard.ExternalChromeOS.TopRowAreFKeys."
+      "Initial",
+      /*expected_count=*/4u);
 }
 
 }  // namespace ash
