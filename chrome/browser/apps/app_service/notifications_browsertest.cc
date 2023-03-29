9f34c99ae477eabd592d6b6463f49716dd1e1b72
Claudio DeSouza <cdesouza@igalia.com>
2023-03-29 22:32:39 +0000

    [extensions] Remove uses of extension_api_unittest.h (1/30)
    This CL is part of a batch of CLs migrating all uses of
    extension_api_unittest.h to more recent alternatives.
    
    This change corrects the file under the path:
       /chrome/browser/apps/app_service
    
    This CL was uploaded by git cl split.
    
    R=tsergeant@chromium.org
    
    Bug: 394840
    Change-Id: Ib1847e35a8fedf587ec197e6826cda0558b370b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382945
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123899}

--- chrome/browser/apps/app_service/notifications_browsertest.cc
+++ chrome/browser/apps/app_service/notifications_browsertest.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/notifications/extension_notification_handler.h"
 #include "chrome/browser/extensions/api/notifications/notifications_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
-#include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/notifications/notification_display_service_tester.h"
 #include "chrome/browser/notifications/profile_notification.h"
 #include "chrome/browser/profiles/profile.h"
