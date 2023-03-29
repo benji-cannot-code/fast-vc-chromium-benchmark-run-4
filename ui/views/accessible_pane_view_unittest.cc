bd2d9b249baac0e0285fc73913373682f3cfda40
Rohan Pavone <rohpavone@chromium.org>
2023-03-29 22:31:21 +0000

    [asan] Clean up memory leak in accessible_pane_view_unittest.
    Alloc'd memory was using a raw ptr. Replace with a smart ptr.
    
    Bug: 1428959
    Change-Id: I14547d62b48029ed6f468ca6402104e5ea58f4af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382148
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Auto-Submit: Rohan Pavone <rohpavone@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123897}

--- ui/views/accessible_pane_view_unittest.cc
+++ ui/views/accessible_pane_view_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/accessible_pane_view.h"
 
+#include <memory>
 #include <utility>
 
 #include "base/memory/raw_ptr.h"
@@ -271,7 +272,7 @@ TEST_F(AccessiblePaneViewTest, MAYBE_DoesntCrashOnEscapeWithRemovedView) {
 }
 
 TEST_F(AccessiblePaneViewTest, AccessibleProperties) {
-  TestBarView* test_view = new TestBarView();
+  std::unique_ptr<TestBarView> test_view = std::make_unique<TestBarView>();
   test_view->SetAccessibleName(u"Name");
   test_view->SetAccessibleDescription(u"Description");
   EXPECT_EQ(test_view->GetAccessibleName(), u"Name");
