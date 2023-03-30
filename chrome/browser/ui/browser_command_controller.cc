c88e271d9c92e89b0c3adeb192440420b541d955
Louise Brett <loubrett@google.com>
2023-03-30 22:06:51 +0000

    Don't allow keyboard shortcut to close home tab for tabbed web apps.
    The home tab should always be open when the app is open so Ctrl+W
    should not close it unless it is the only tab remaining.
    
    Bug: 1381345
    Change-Id: Ieddc4f438c9b6378693dafe524afcefea01e2ff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090963
    Commit-Queue: Louise Brett <loubrett@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124454}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -168,6 +168,12 @@ bool CanOpenFile(Browser* browser) {
   return true;
 }
 
+bool IsPinnedHomeTab(Browser* browser) {
+  return web_app::AppBrowserController::IsWebApp(browser) &&
+         web_app::IsPinnedHomeTab(browser->tab_strip_model(),
+                                  browser->tab_strip_model()->active_index());
+}
+
 }  // namespace
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -1020,6 +1026,13 @@ void BrowserCommandController::OnSigninAllowedPrefChange() {
 
 // BrowserCommandController, TabStripModelObserver implementation:
 
+void BrowserCommandController::OnTabStripModelChanged(
+    TabStripModel* tab_strip_model,
+    const TabStripModelChange& change,
+    const TabStripSelectionChange& selection) {
+  UpdateCommandsForTabStripStateChanged();
+}
+
 void BrowserCommandController::TabBlockedStateChanged(
     content::WebContents* contents,
     int index) {
@@ -1395,13 +1408,9 @@ void BrowserCommandController::UpdateCommandsForTabState() {
   bool is_isolated_app = current_web_contents->GetPrimaryMainFrame()
                              ->GetWebExposedIsolationLevel() >=
                          WebExposedIsolationLevel::kMaybeIsolatedApplication;
-  bool is_pinned_home_tab =
-      web_app::AppBrowserController::IsWebApp(browser_) &&
-      web_app::IsPinnedHomeTab(browser_->tab_strip_model(),
-                               browser_->tab_strip_model()->active_index());
   command_updater_.UpdateCommandEnabled(
       IDC_OPEN_IN_CHROME,
-      IsWebAppOrCustomTab() && !is_isolated_app && !is_pinned_home_tab);
+      IsWebAppOrCustomTab() && !is_isolated_app && !IsPinnedHomeTab(browser_));
 
   command_updater_.UpdateCommandEnabled(
       IDC_TOGGLE_REQUEST_TABLET_SITE,
@@ -1744,6 +1753,12 @@ void BrowserCommandController::UpdateCommandsForWebContentsFocus() {
   command_updater_.UpdateCommandEnabled(IDC_CARET_BROWSING_TOGGLE,
                                         CanToggleCaretBrowsing(browser_));
 #endif  // BUILDFLAG(IS_MAC)
+}
+
+void BrowserCommandController::UpdateCommandsForTabStripStateChanged() {
+  command_updater_.UpdateCommandEnabled(
+      IDC_CLOSE_TAB,
+      !IsPinnedHomeTab(browser_) || browser_->tab_strip_model()->count() == 1);
 }
 
 BrowserWindow* BrowserCommandController::window() {
--- chrome/browser/ui/browser_command_controller.h
+++ chrome/browser/ui/browser_command_controller.h
@@ -99,6 +99,10 @@ class BrowserCommandController : public CommandUpdater,
                            LockedFullscreen);
 
   // Overridden from TabStripModelObserver:
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
   void TabBlockedStateChanged(content::WebContents* contents,
                               int index) override;
 
@@ -201,6 +205,9 @@ class BrowserCommandController : public CommandUpdater,
 
   // Updates commands that depend on whether web contents is focused or not.
   void UpdateCommandsForWebContentsFocus();
+
+  // Updates commands that depend on the state of the tab strip model.
+  void UpdateCommandsForTabStripStateChanged();
 
   inline BrowserWindow* window();
   inline Profile* profile();
