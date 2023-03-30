f7a3b6839e55d7156f4b34c82763c728a4f2e98b
Morten Stenshorne <mstensho@chromium.org>
2023-03-30 16:47:23 +0000

    [RemoveLegacy] Remove PaintLayer::EnclosingPaginationLayer().
    It was only used by legacy block fragmentation.
    
    Bug: 1229581
    Change-Id: I363c43b373157132571829f350c7921e80646826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381825
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124258}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -106,18 +106,6 @@ struct CORE_EXPORT PaintLayerRareData final
 
   std::unique_ptr<gfx::Transform> transform;
 
-  // Pointer to the enclosing Layer that caused us to be paginated. It is 0 if
-  // we are not paginated.
-  //
-  // See LayoutMultiColumnFlowThread and
-  // https://sites.google.com/a/chromium.org/dev/developers/design-documents/multi-column-layout
-  // for more information about the multicol implementation. It's important to
-  // understand the difference between flow thread coordinates and visual
-  // coordinates when working with multicol in Layer, since Layer is one of the
-  // few places where we have to worry about the visual ones. Internally we try
-  // to use flow-thread coordinates whenever possible.
-  Member<PaintLayer> enclosing_pagination_layer;
-
   Member<PaintLayerResourceInfo> resource_info;
 };
 
@@ -277,10 +265,6 @@ class CORE_EXPORT PaintLayer : public GarbageCollected<PaintLayer>,
 
   void UpdateLayerPositionsAfterLayout();
 
-  PaintLayer* EnclosingPaginationLayer() const {
-    return rare_data_ ? rare_data_->enclosing_pagination_layer : nullptr;
-  }
-
   void UpdateTransform();
 
   bool HasVisibleContent() const {
@@ -322,15 +306,6 @@ class CORE_EXPORT PaintLayer : public GarbageCollected<PaintLayer>,
   PhysicalOffset VisualOffsetFromAncestor(
       const PaintLayer* ancestor_layer,
       PhysicalOffset offset = PhysicalOffset()) const;
-
-  // Convert a bounding box from flow thread coordinates, relative to |this|, to
-  // visual coordinates, relative to |ancestorLayer|.
-  // See http://www.chromium.org/developers/design-documents/multi-column-layout
-  // for more info on these coordinate types.  This method requires this layer
-  // to be paginated; i.e. it must have an enclosingPaginationLayer().
-  void ConvertFromFlowThreadToVisualBoundingBoxInAncestor(
-      const PaintLayer* ancestor_layer,
-      PhysicalRect&) const;
 
   // The hitTest() method looks for mouse events by walking layers that
   // intersect the point from front to back.
@@ -751,9 +726,6 @@ class CORE_EXPORT PaintLayer : public GarbageCollected<PaintLayer>,
 
   void UpdateTransformAfterStyleChange(const ComputedStyle* old_style,
                                        const ComputedStyle& new_style);
-
-  void UpdatePaginationRecursive(bool needs_pagination_update = false);
-  void ClearPaginationRecursive();
 
   void MarkCompositingContainerChainForNeedsRepaint();
 
