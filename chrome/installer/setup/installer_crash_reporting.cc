42077fa8edb30d9b21a366d804ca2650025a29e5
S. Ganesh <ganesh@chromium.org>
2023-03-30 01:50:49 +0000

    Fix Chrome Crashpad arbitrary file create
    This CL uses a path `%systemroot%\SystemTemp`, if available, else uses a
    path under `%programfiles%` for the Crashpad directory.
    
    Both paths are only accessible to admin and system processes, and are
    therefore secure.
    
    Bug: 1427431
    Change-Id: Ic61cda0ff8bc3f0ef5b0d38afe7e27c28d07bedb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4380263
    Reviewed-by: Will Harris <wfh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123994}

--- chrome/installer/setup/installer_crash_reporting.cc
+++ chrome/installer/setup/installer_crash_reporting.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/debug/leak_annotations.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/install_static/install_details.h"
 #include "chrome/installer/setup/installer_crash_reporter_client.h"
 #include "chrome/installer/setup/installer_state.h"
+#include "chrome/installer/setup/setup_util.h"
 #include "chrome/installer/util/google_update_settings.h"
 #include "components/crash/core/app/crashpad.h"
 #include "components/crash/core/common/crash_key.h"
@@ -44,9 +46,14 @@ const char* OperationToString(InstallerState::Operation operation) {
   return "";
 }
 
-// Retrieve the SYSTEM version of TEMP. We do this instead of GetTempPath so
-// that both elevated and SYSTEM runs share the same directory.
+// Returns the path returned by `installer::GetSecureSystemTemp` if available.
+// Otherwise, retrieves the SYSTEM version of TEMP. We do this instead of
+// GetTempPath so that both elevated and SYSTEM runs share the same directory.
 bool GetSystemTemp(base::FilePath* temp) {
+  if (GetSecureSystemTemp(temp)) {
+    return true;
+  }
+
   base::win::RegKey reg_key(
       HKEY_LOCAL_MACHINE,
       L"SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment",
