3e4e41fa7e68d80349984e98b29036aefaf0575f
Hubert Chao <hchao@chromium.org>
2023-03-30 20:04:52 +0000

    Windows CCV/CRS Cleanup: Remove CertVerifyProcWin
    Bug: 1380536
    Change-Id: I332399593c8af5ae2b4cbf26e938aac8a5cd1d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348319
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124388}

--- net/cert/cert_verify_proc.cc
+++ net/cert/cert_verify_proc.cc
@@ -65,8 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_verify_proc_android.h"
 #elif BUILDFLAG(IS_IOS)
 #include "net/cert/cert_verify_proc_ios.h"
-#elif BUILDFLAG(IS_WIN)
-#include "net/cert/cert_verify_proc_win.h"
 #endif
 
 namespace net {
--- services/cert_verifier/trial_comparison_cert_verifier_mojo.cc
+++ services/cert_verifier/trial_comparison_cert_verifier_mojo.cc
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/internal/trust_store_mac.h"
 #endif
 
-#if BUILDFLAG(IS_WIN)
-#include "net/cert/cert_verify_proc_win.h"
-#endif
-
 #if BUILDFLAG(USE_NSS_CERTS)
 #include "crypto/nss_util.h"
 #include "net/cert/internal/trust_store_nss.h"
@@ -151,19 +147,6 @@ void TrialComparisonCertVerifierMojo::OnSendTrialReport(
         TrustImplTypeToMojom(mac_trust_debug_info->trust_impl());
   }
 #endif  // BUILDFLAG(IS_MAC)
-
-#if BUILDFLAG(IS_WIN)
-  auto* win_platform_debug_info =
-      net::CertVerifyProcWin::ResultDebugData::Get(&primary_result);
-  if (win_platform_debug_info) {
-    debug_info->win_platform_debug_info =
-        mojom::WinPlatformVerifierDebugInfo::New();
-    debug_info->win_platform_debug_info->authroot_this_update =
-        win_platform_debug_info->authroot_this_update();
-    debug_info->win_platform_debug_info->authroot_sequence_number =
-        win_platform_debug_info->authroot_sequence_number();
-  }
-#endif  // BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(USE_NSS_CERTS)
   crypto::EnsureNSSInit();
