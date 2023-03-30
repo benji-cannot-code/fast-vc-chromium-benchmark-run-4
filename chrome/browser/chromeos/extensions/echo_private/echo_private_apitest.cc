fa3f7fdebf83016de6716ca3f67cb5caf51e479e
Claudio DeSouza <cdesouza@igalia.com>
2023-03-30 17:01:46 +0000

    [extensions] Remove uses of extension_api_unittest.h (7/30)
    This CL is part of a batch of CLs migrating all uses of
    extension_api_unittest.h to more recent alternatives.
    
    This change corrects the file under the path:
       /chrome/browser/chromeos/extensions
    
    This CL was uploaded by git cl split.
    
    R=reillyg@chromium.org
    
    Bug: 394840
    Change-Id: Icafc4bfa3dd351aa738ea268a8b07bec6d05f339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381848
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124274}

--- chrome/browser/chromeos/extensions/echo_private/echo_private_apitest.cc
+++ chrome/browser/chromeos/extensions/echo_private/echo_private_apitest.cc
@@ -14,14 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/ash/settings/stub_cros_settings_provider.h"
 #include "chrome/browser/extensions/extension_apitest.h"
-#include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/browser/api_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace utils = extension_function_test_utils;
+namespace utils = extensions::api_test_utils;
 
 namespace chromeos {
 
@@ -55,10 +55,11 @@ class ExtensionEchoPrivateApiTest : public extensions::ExtensionApiTest {
     const std::string arguments = base::StringPrintf(
         R"([{"serviceName": "name", "origin": "https://test.com", "tabId": %d}])",
         tab_id);
-    std::unique_ptr<base::Value> result(utils::RunFunctionAndReturnSingleResult(
-        function.get(), arguments, browser()));
+    absl::optional<base::Value> result =
+        utils::RunFunctionAndReturnSingleResult(function.get(), arguments,
+                                                profile());
 
-    ASSERT_TRUE(result.get());
+    ASSERT_TRUE(result);
     ASSERT_EQ(base::Value::Type::BOOLEAN, result->type());
 
     EXPECT_EQ(expected_result, result->GetBool());
@@ -153,7 +154,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionEchoPrivateApiTest,
       base::StringPrintf(
           R"([{"serviceName": "name", "origin": "invalid", "tabId": %d}])",
           tab_id),
-      browser());
+      profile());
 
   EXPECT_EQ("Invalid origin.", error);
   EXPECT_EQ(0, dialog_invocation_count());
@@ -167,7 +168,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionEchoPrivateApiTest, GetUserConsent_NoTabIdSet) {
 
   std::string error = utils::RunFunctionAndReturnError(
       function.get(),
-      R"([{"serviceName": "name", "origin": "https://test.com"}])", browser());
+      R"([{"serviceName": "name", "origin": "https://test.com"}])", profile());
 
   EXPECT_EQ("Not called from an app window - the tabId is required.", error);
   EXPECT_EQ(0, dialog_invocation_count());
@@ -188,7 +189,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionEchoPrivateApiTest,
       R"([{"serviceName": "name", "origin": "https://test.com", "tabId": %d}])",
       tab_id);
   std::string error =
-      utils::RunFunctionAndReturnError(function.get(), arguments, browser());
+      utils::RunFunctionAndReturnError(function.get(), arguments, profile());
 
   EXPECT_EQ("Consent requested from an inactive tab.", error);
   EXPECT_EQ(0, dialog_invocation_count());
@@ -207,7 +208,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionEchoPrivateApiTest, GetUserConsent_ClosedTab) {
       R"([{"serviceName": "name", "origin": "https://test.com", "tabId": %d}])",
       tab_id);
   std::string error =
-      utils::RunFunctionAndReturnError(function.get(), arguments, browser());
+      utils::RunFunctionAndReturnError(function.get(), arguments, profile());
 
   EXPECT_EQ("Tab not found.", error);
   EXPECT_EQ(0, dialog_invocation_count());
