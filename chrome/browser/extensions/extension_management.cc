cabab897bf507ec38a0ebdf280197d83c7882876
Liza Bipin <mlbipin@google.com>
2023-03-30 13:26:56 +0000

    Replace Profile::IsChild with AreExtensionsPermissionsEnabled
    Bug: b/270551374
    Change-Id: Idd3e8ab0c5897c99cabd1d543729bf354a71a737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4341969
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124160}

--- chrome/browser/extensions/extension_management.cc
+++ chrome/browser/extensions/extension_management.cc
@@ -63,9 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 ExtensionManagement::ExtensionManagement(Profile* profile)
-    : profile_(profile),
-      pref_service_(profile_->GetPrefs()),
-      is_child_(profile_->IsChild()) {
+    : profile_(profile), pref_service_(profile_->GetPrefs()) {
   TRACE_EVENT0("browser,startup",
                "ExtensionManagement::ExtensionManagement::ctor");
 #if BUILDFLAG(IS_CHROMEOS_ASH)
