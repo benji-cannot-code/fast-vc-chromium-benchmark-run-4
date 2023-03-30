d0d41b9214686d8804d0812694e1601a2295e4c9
Georg Neis <neis@chromium.org>
2023-03-30 07:12:13 +0000

    Remove unused function UpstartClient::StartLacrosChrome
    Change-Id: Ia4f44753f10635dc893b2f88bedcf984b30f1303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4384166
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124058}

--- chromeos/ash/components/dbus/upstart/fake_upstart_client.cc
+++ chromeos/ash/components/dbus/upstart/fake_upstart_client.cc
@@ -73,9 +73,6 @@ void FakeUpstartClient::RestartAuthPolicyService() {
   FakeAuthPolicyClient::Get()->SetStarted(true);
 }
 
-void FakeUpstartClient::StartLacrosChrome(
-    const std::vector<std::string>& upstart_env) {}
-
 void FakeUpstartClient::StartMediaAnalytics(
     const std::vector<std::string>& /* upstart_env */,
     chromeos::VoidDBusMethodCallback callback) {
