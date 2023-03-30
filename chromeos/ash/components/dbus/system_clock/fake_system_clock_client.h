26e4315a1921ec36110e08fe23b31d03b2aa2300
Sergiy Belozorov <sergiyb@google.com>
2023-03-30 12:46:49 +0000

    Implement tests for EnrollmentStateFetcher
    We make it possible to simulate permanent failure to connect to the system clock service to test such scenarios.
    
    Bug: b:265923216
    Change-Id: I3d2b6eb236469e871cc6f3658f29f3083c8f7a97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315949
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Sergiy Belozorov <sergiyb@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124142}

--- chromeos/ash/components/dbus/system_clock/fake_system_clock_client.h
+++ chromeos/ash/components/dbus/system_clock/fake_system_clock_client.h
@@ -31,6 +31,7 @@ class COMPONENT_EXPORT(SYSTEM_CLOCK) FakeSystemClockClient
   void SetNetworkSynchronized(bool network_synchronized) override;
   void NotifyObserversSystemClockUpdated() override;
   void SetServiceIsAvailable(bool is_available) override;
+  void DisableService() override;
 
   // SystemClockClient overrides
   void AddObserver(Observer* observer) override;
@@ -45,6 +46,7 @@ class COMPONENT_EXPORT(SYSTEM_CLOCK) FakeSystemClockClient
 
  private:
   bool is_available_ = true;
+  bool is_enabled_ = true;
   bool network_synchronized_ = false;
 
   std::vector<dbus::ObjectProxy::WaitForServiceToBeAvailableCallback>
