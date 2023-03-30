31f4a79c9e815d976d999b8c1729224b6f97a144
Petro Akzhygitov <pakzhygitov@google.com>
2023-03-30 16:39:34 +0000

    Disabled "Edit" button in the Tab Grid after closing last pinned tab.
    – Refactored the common logic between the regular and the pinned tabs
    into a `handleTabCountChangeWithTabCount:` method.
    – Updated the crash keys.
    
    Fixed: 1426216, 1426302
    Change-Id: I37ecf51e563c5ab53988925eb314db4238b79173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370274
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124245}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -2097,6 +2097,27 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   }
 }
 
+// Updates the views, buttons, toolbars as well as broadcasts incognito tabs
+// visibility after the tab count has changed.
+- (void)handleTabCountChangeWithTabCount:(NSUInteger)tabCount {
+  if (self.tabGridMode == TabGridModeSelection) {
+    // Exit selection mode if there are no more tabs.
+    if (tabCount == 0) {
+      self.tabGridMode = TabGridModeNormal;
+    }
+
+    [self updateSelectionModeToolbars];
+  }
+
+  if (tabCount > 0) {
+    // Undo is only available when the tab grid is empty.
+    self.undoCloseAllAvailable = NO;
+  }
+
+  [self configureButtonsForActiveAndCurrentPage];
+  [self broadcastIncognitoContentVisibility];
+}
+
 // Broadcasts whether incognito tabs are showing.
 - (void)broadcastIncognitoContentVisibility {
   // It is programmer error to broadcast incognito content visibility when the
@@ -2408,6 +2429,11 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
             (PinnedTabsViewController*)pinnedTabsViewController
               didChangeItemCount:(NSUInteger)count {
   self.topToolbar.pageControl.pinnedTabCount = count;
+  const NSUInteger totalTabCount =
+      count + self.topToolbar.pageControl.regularTabCount;
+
+  crash_keys::SetRegularTabCount(totalTabCount);
+  [self handleTabCountChangeWithTabCount:totalTabCount];
 }
 
 - (void)pinnedTabsViewControllerVisibilityDidChange:
@@ -2547,26 +2573,18 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
 
 - (void)gridViewController:(GridViewController*)gridViewController
         didChangeItemCount:(NSUInteger)count {
-  if (self.tabGridMode == TabGridModeSelection) {
-    // Exit selection mode if there are no more tabs.
-    if (count == 0) {
-      self.tabGridMode = TabGridModeNormal;
-    }
-    [self updateSelectionModeToolbars];
-  }
-
-  if (count > 0) {
-    // Undo is only available when the tab grid is empty.
-    self.undoCloseAllAvailable = NO;
-  }
-  [self configureButtonsForActiveAndCurrentPage];
   if (gridViewController == self.regularTabsViewController) {
     self.topToolbar.pageControl.regularTabCount = count;
-    crash_keys::SetRegularTabCount(count);
+    const NSUInteger totalTabCount =
+        count + self.topToolbar.pageControl.pinnedTabCount;
+
+    crash_keys::SetRegularTabCount(totalTabCount);
+    [self handleTabCountChangeWithTabCount:totalTabCount];
+
   } else if (gridViewController == self.incognitoTabsViewController) {
     crash_keys::SetIncognitoTabCount(count);
+    [self handleTabCountChangeWithTabCount:count];
   }
-  [self broadcastIncognitoContentVisibility];
 }
 
 - (void)gridViewController:(GridViewController*)gridViewController
