09abed611b4a761e440cf523c660b904e09b244c
Crisrael Lucero <crisrael@google.com>
2023-03-30 19:41:33 +0000

    [Phone Hub][Eche] Log RecentApps UI State when Phone Hub bubble opened
    This change introduces a new histogram that tracks what UI state the
    recent apps UI is in when the Phone Hub bubble is opened (loading,
    error, or apps are visible).
    
    Test: manually verified with chrome://histograms and added unit tests
    Bug: None
    Change-Id: Ic56d8c44328b2c17bffcf4f587eae1a9dadec695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374456
    Reviewed-by: Pu Shi <pushi@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124371}

--- ash/system/phonehub/phone_hub_recent_apps_view.cc
+++ ash/system/phonehub/phone_hub_recent_apps_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ash/system/phonehub/phone_connected_view.h"
 #include "ash/system/phonehub/phone_hub_app_loading_icon.h"
+#include "ash/system/phonehub/phone_hub_metrics.h"
 #include "ash/system/phonehub/phone_hub_more_apps_button.h"
 #include "ash/system/phonehub/phone_hub_recent_app_button.h"
 #include "ash/system/phonehub/phone_hub_view_ids.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_constants.h"
 #include "ash/webui/eche_app_ui/mojom/eche_app.mojom.h"
 #include "base/cxx17_backports.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/ranges/algorithm.h"
 #include "chromeos/ash/components/phonehub/notification.h"
 #include "chromeos/ash/components/phonehub/phone_hub_manager.h"
@@ -219,6 +221,9 @@ PhoneHubRecentAppsView::PhoneHubRecentAppsView(
     loading_view_ =
         recent_apps_content->AddChildView(std::make_unique<LoadingView>());
   }
+
+  phone_hub_metrics::LogRecentAppsStateOnBubbleOpened(
+      recent_apps_interaction_handler_->ui_state());
 
   Update();
   recent_apps_interaction_handler_->AddObserver(this);
