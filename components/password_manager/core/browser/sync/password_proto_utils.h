b48d7091625b6f213830c0a2445ea4f09f1cbef8
Ioana Pandele <ioanap@chromium.org>
2023-03-30 14:15:03 +0000

    [ODPPC] Move PasswordIssues proto outside of PasswordSpecificsData
    This is in preparation of adding a PasswordIssues field to
    PasswordSpecificsMetadata.
    
    Bug: 1413209
    
    Change-Id: I3b856db8ac17ea33bd57f565a1f31e0341916822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4359893
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124189}

--- components/password_manager/core/browser/sync/password_proto_utils.h
+++ components/password_manager/core/browser/sync/password_proto_utils.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sync_pb {
 class PasswordSpecifics;
 class PasswordSpecificsData;
-class PasswordSpecificsData_PasswordIssues;
+class PasswordIssues;
 class PasswordSpecificsData_Notes;
 class PasswordSpecificsMetadata;
 }  // namespace sync_pb
@@ -23,7 +23,7 @@ namespace password_manager {
 
 // Converts a map of `form_password_issues` into the format required by the
 // proto.
-sync_pb::PasswordSpecificsData_PasswordIssues PasswordIssuesMapToProto(
+sync_pb::PasswordIssues PasswordIssuesMapToProto(
     const base::flat_map<InsecureType, InsecurityMetadata>&
         form_password_issues);
 
