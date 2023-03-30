937c9bb7fe803da88e879a078ee7f26ce324cbf2
Claudio DeSouza <cdesouza@igalia.com>
2023-03-30 00:43:09 +0000

    [extensions] Remove uses of extension_api_unittest.h (3/30)
    This CL is part of a batch of CLs migrating all uses of
    extension_api_unittest.h to more recent alternatives.
    
    This change corrects the file under the path:
       /chrome/browser/ash/app_list
    
    This CL was uploaded by git cl split.
    
    R=wrong@chromium.org
    
    Bug: 394840
    Change-Id: Ieda0d9955e82d4f2a9605eb9a63d696ad2d1ba71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381890
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123961}

--- chrome/browser/ash/app_list/app_service/app_service_app_model_builder_unittest.cc
+++ chrome/browser/ash/app_list/app_service/app_service_app_model_builder_unittest.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/plugin_vm/plugin_vm_features.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_test_helper.h"
 #include "chrome/browser/extensions/chrome_app_icon.h"
-#include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/install_tracker.h"
 #include "chrome/browser/extensions/install_tracker_factory.h"
