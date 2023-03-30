02cdaabd81bd5defece78ba887f25a1ccb25af78
Nikhil Nayunigari <nikhilcn@google.com>
2023-03-30 21:55:02 +0000

    [CrOS Hotspot] Add hotspot_enabled_state_notifier class
    hotspot_enabled_state_notifier collects notifications from
    hotspot_controller and hotspot_state_handler and relays them to its
    observers. This is the first step in migrating hotspot_notifier from
    subscribing to both those classes directly and instead use this new
    class.
    
    Bug: b/269354043
    Change-Id: Ia9f7d2014f0cbb37ea22edc6993fc1e52271c9f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382598
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124448}

--- chromeos/ash/components/network/hotspot_state_handler.h
+++ chromeos/ash/components/network/hotspot_state_handler.h
@@ -47,6 +47,9 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotStateHandler
   // Return the latest hotspot state
   const hotspot_config::mojom::HotspotState& GetHotspotState() const;
 
+  // Returns the reason for hotspot being disabled.
+  const hotspot_config::mojom::DisableReason& GetDisableReason() const;
+
   // Return the latest hotspot active client count
   size_t GetHotspotActiveClientCount() const;
 
@@ -74,6 +77,8 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotStateHandler
 
   hotspot_config::mojom::HotspotState hotspot_state_ =
       hotspot_config::mojom::HotspotState::kDisabled;
+
+  hotspot_config::mojom::DisableReason disable_reason_;
 
   size_t active_client_count_ = 0;
 
