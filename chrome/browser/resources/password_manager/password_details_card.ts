01c81e1dba5a753d3e56085a9c36e6bca30a7a56
Viktor Semeniuk <vsemeniuk@google.com>
2023-03-30 14:12:41 +0000

    [Password Manager] Update note during update
    This CL makes sure note value is updated during successful edit.
    
    Fixed: 1429192
    Change-Id: I4f8250fb39109ff3176344a1f48b4b64a5d8abdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381607
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124185}

--- chrome/browser/resources/password_manager/password_details_card.ts
+++ chrome/browser/resources/password_manager/password_details_card.ts
@@ -150,6 +150,9 @@ export class PasswordDetailsCardElement extends PasswordDetailsCardElementBase {
   }
 
   private onEditPasswordDialogClosed_() {
+    // Only note is notified because updating username or password triggers
+    // recomputing of an id which updates the whole list of displayed passwords.
+    this.notifyPath('password.note');
     this.showEditPasswordDialog_ = false;
     this.extendAuthValidity_();
   }
