865811eb9da4dfdc3f32d01a135be1e7a9b2a81a
Viktor Semeniuk <vsemeniuk@google.com>
2023-03-30 17:04:37 +0000

    [Password Manager] Fix a11y announcements for import, export, shortcut
    Bug: 1422200
    Change-Id: I1d3e8794120b7122c3b0cc852ac7b4d3b13dc88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382202
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124279}

--- chrome/browser/resources/password_manager/passwords_exporter.ts
+++ chrome/browser/resources/password_manager/passwords_exporter.ts
@@ -155,6 +155,13 @@ export class PasswordsExporterElement extends PasswordsExporterElementBase {
         this.exportedFilePath_);
     this.$.exportSuccessToast.hide();
   }
+
+  private getAriaLabel_(): string {
+    return [
+      this.i18n('exportPasswords'),
+      this.i18n('exportPasswordsDescription'),
+    ].join('. ');
+  }
 }
 
 declare global {
