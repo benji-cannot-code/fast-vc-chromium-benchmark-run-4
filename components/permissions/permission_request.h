17bed33d2d843dda76f4b9f51d88707600ce985d
Thomas Nguyen <tungnh@google.com>
2023-03-30 12:23:35 +0000

    Use weak pointer to store duplicate requests
    Bug: 1423304
    Change-Id: I7ab170f085c3d05c582f7065b88c1ad2510cc633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4365724
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124133}

--- components/permissions/permission_request.h
+++ components/permissions/permission_request.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
@@ -83,6 +84,9 @@ class PermissionRequest {
   // Returns prompt text appropriate for displaying in an Android dialog.
   virtual std::u16string GetDialogMessageText() const;
 #endif
+
+  // Returns a weak pointer to this instance.
+  base::WeakPtr<PermissionRequest> GetWeakPtr();
 
 #if !BUILDFLAG(IS_ANDROID)
   // Returns whether displaying a confirmation chip for the request is
@@ -164,6 +168,8 @@ class PermissionRequest {
   // Called when the request is no longer in use so it can be deleted by the
   // caller.
   base::OnceClosure delete_callback_;
+
+  base::WeakPtrFactory<PermissionRequest> weak_factory_{this};
 };
 
 }  // namespace permissions
