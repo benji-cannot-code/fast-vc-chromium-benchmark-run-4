39d975dec8d49d4f740db5c76f5ba384518ba817
Andrew Paseltiner <apaseltiner@chromium.org>
2023-03-30 17:21:55 +0000

    Propagate success/failure from OS registration Java methods
    This allows us to show more precise information in the Attribution
    Internals UI.
    
    Bug: 1428974, 1426450
    Change-Id: Idf43736c2333aa9f9b018cc0f58b86cb6df93c68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4383024
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124294}

--- content/browser/resources/attribution_reporting/attribution_internals.ts
+++ content/browser/resources/attribution_reporting/attribution_internals.ts
@@ -743,6 +743,9 @@ class OsRegistration {
       case OsRegistrationResult.kExcessiveQueueSize:
         this.result = 'Excessive queue size';
         break;
+      case OsRegistrationResult.kRejectedByOs:
+        this.result = 'Rejected by OS';
+        break;
       default:
         assertNotReached();
     }
