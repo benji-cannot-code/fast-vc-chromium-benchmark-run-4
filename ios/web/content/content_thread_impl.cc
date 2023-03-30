c40b3b3d23a68ebf8021b5127a7a7383891c78c4
Dave Tapuska <dtapuska@chromium.org>
2023-03-30 20:32:04 +0000

    [ios] Fix build failure from task_executor.h removal
    crrev.com/7f5dfb90a2d1b68efbfcde627b3a0da99079f200 removed
    task_executor.h fix the straggling include.
    
    Bug: 1424788
    Change-Id: I90b967b09a438c21c2abe030bcae74186fb152cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387073
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124411}

--- ios/web/content/content_thread_impl.cc
+++ ios/web/content/content_thread_impl.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
-#include "base/task/task_executor.h"
 #include "base/time/time.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
