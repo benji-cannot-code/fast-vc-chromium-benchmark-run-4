8baddc11263829b1ddb7da2e998b34197c1890bc
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-03-30 21:30:07 +0000

    Reland "Improves testing support for FrameSinkHost."
    This is a reland of commit 30c4647a50f089bf71fafe3da55c5550f2d85fef
    
    Reason for revert: It was reverted because of uninitialized member
    variable, host_window_ in `FrameSinkHost` causing a DCHECK to fail
    in `FrameSinkHost::SetHostWindow()` for unit tests.
    host_window_ was getting initialized with garbage data.
    
    
    Original change's description:
    > Improves testing support for FrameSinkHost.
    >
    > Adds InitForTesting() method for FrameSinkHost to allow implementations
    > of FrameSinkHost to add Mock/Stub for viz::LayerTreeFrameSink for
    > testing purposes.
    >
    > Also adds mock/test implementations for viz::LayerTreeFrameSink and
    > viz::BeginFrameSource.
    >
    > Bug: 1392895
    > Change-Id: I2854f8b960b7fca1288b65ecc7b47e9e3f369ce6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363861
    > Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    > Reviewed-by: Sean Kau <skau@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1124044}
    
    Bug: 1392895, 1429222
    Change-Id: I0e290931aaefd1596a299be4a2cde514f63c8b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385976
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124434}

--- ash/frame_sink/frame_sink_host.h
+++ ash/frame_sink/frame_sink_host.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/presentation_feedback.h"
 
+namespace cc {
+class LayerTreeFrameSink;
+}  // namespace cc
+
 namespace ash {
 
 class FrameSinkHolder;
@@ -46,7 +50,11 @@ class ASH_EXPORT FrameSinkHost : public aura::WindowObserver {
   void SetPresentationCallback(PresentationCallback callback);
 
   // Initializes the FrameSinkHost on the host_window.
-  void Init(aura::Window* host_window);
+  virtual void Init(aura::Window* host_window);
+
+  virtual void InitForTesting(
+      aura::Window* host_window,
+      std::unique_ptr<cc::LayerTreeFrameSink> layer_tree_frame_sink);
 
   // Updates the surface by submitting a compositor frame. With
   // synchronous_draw as true, we send a compositor frame as soon as we call the
@@ -100,8 +108,18 @@ class ASH_EXPORT FrameSinkHost : public aura::WindowObserver {
   const gfx::Rect& GetContentRect() const { return content_rect_; }
 
  private:
+  void InitFrameSinkHolder(
+      aura::Window* host_window,
+      std::unique_ptr<cc::LayerTreeFrameSink> layer_tree_frame_sink);
+
+  void SetHostWindow(aura::Window* host_window);
+
+  void InitInternal(
+      aura::Window* host_window,
+      std::unique_ptr<cc::LayerTreeFrameSink> layer_tree_frame_sink);
+
   // The window on which LayerTreeFrameSink is created on.
-  base::raw_ptr<aura::Window> host_window_;
+  base::raw_ptr<aura::Window> host_window_ = nullptr;
 
   // The bounds of the content to be displayed in host window coordinates.
   gfx::Rect content_rect_;
