3bf0e0194faf85f7048abfcb673f1d6c113ba912
Ahmed Elwasefi <a.m.elwasefi@gmail.com>
2023-03-30 16:42:49 +0000

    Added nameFrom to popover a11y test.
    Fixed the issue where nameFrom wasn't shown in the expected file of the test.
    
    Change-Id: Iaaaf280fa80343ec943e2eb953809be0ad8cf62b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382196
    Commit-Queue: Ahmed Elwasefi (Ahmad45123) <a.m.elwasefi@gmail.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124250}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -4978,6 +4978,7 @@ String AXNodeObject::TextAlternativeFromTooltip(
 
   if (name_sources) {
     NameSource& source = name_sources->back();
+    source.related_objects = *related_objects;
     source.text = *text_alternative;
     *found_text_alternative = true;
   }
