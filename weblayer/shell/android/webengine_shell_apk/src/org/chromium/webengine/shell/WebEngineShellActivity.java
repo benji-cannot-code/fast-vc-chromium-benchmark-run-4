f065d05086104a18e6f60f39586408252f535b4e
Parsa Mesgarha <parsam@chromium.org>
2023-03-30 09:29:56 +0000

    [WebEngine] Preserve App Bar Views state on configuration change
    1. Persist the active tab
    2. Restore progress bar and reload button
    3. Persist tab list dialog
    
    Change-Id: I86f1bc55ea146c8b21dcc98b30b1bb3a8e9ef65f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370687
    Reviewed-by: Susanne Westphal <swestphal@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Parsa Mesgarha <parsam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124092}

--- weblayer/shell/android/webengine_shell_apk/src/org/chromium/webengine/shell/WebEngineShellActivity.java
+++ weblayer/shell/android/webengine_shell_apk/src/org/chromium/webengine/shell/WebEngineShellActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,6 +46,7 @@ import org.chromium.webengine.TabManager;
 import org.chromium.webengine.WebEngine;
 import org.chromium.webengine.WebFragment;
 import org.chromium.webengine.WebSandbox;
+import org.chromium.webengine.shell.topbar.CustomSpinner;
 import org.chromium.webengine.shell.topbar.TabEventsDelegate;
 import org.chromium.webengine.shell.topbar.TabEventsObserver;
 
@@ -74,7 +76,7 @@ public class WebEngineShellActivity
     private ProgressBar mProgressBar;
     private EditText mUrlBar;
     private Button mTabCountButton;
-    private Spinner mTabListSpinner;
+    private CustomSpinner mTabListSpinner;
     private ArrayAdapter<TabWrapper> mTabListAdapter;
 
     private ImageButton mReloadButton;
@@ -84,11 +86,16 @@ public class WebEngineShellActivity
     private DefaultObservers mDefaultTabListObserver;
 
     private int mSystemVisibilityToRestore;
+    private boolean mIsTabListOpen;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            mIsTabListOpen = savedInstanceState.getBoolean("isTabListOpen");
+        }
         setContentView(R.layout.main);
+
         mContext = getApplicationContext();
         mDefaultTabListObserver = new DefaultObservers();
 
@@ -101,6 +108,7 @@ public class WebEngineShellActivity
         mReloadButton = findViewById(R.id.reload_button);
         mRefreshDrawable = getDrawable(R.drawable.ic_refresh);
         mStopDrawable = getDrawable(R.drawable.ic_stop);
+        setProgress(1.0);
 
         mUrlBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -135,7 +143,7 @@ public class WebEngineShellActivity
 
         mTabCountButton.setOnClickListener(v -> mTabListSpinner.performClick());
 
-        mTabListSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        mTabListSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 mTabListAdapter.getItem(pos).getTab().setActive();
@@ -198,10 +206,19 @@ public class WebEngineShellActivity
             mTabCountButton.setText(String.valueOf(getTabsCount()));
             mTabListAdapter = new ArrayAdapter<TabWrapper>(
                     this, android.R.layout.simple_spinner_dropdown_item);
-            for (Tab t : mTabManager.getAllTabs()) {
-                mTabListAdapter.add(new TabWrapper(t));
-            }
             mTabListSpinner.setAdapter(mTabListAdapter);
+
+            for (Tab t : mTabManager.getAllTabs()) {
+                TabWrapper tabWrapper = new TabWrapper(t);
+                mTabListAdapter.add(tabWrapper);
+                if (t.equals(mTabManager.getActiveTab())) {
+                    mTabListSpinner.setSelection(mTabListAdapter.getPosition(tabWrapper));
+                }
+            }
+
+            if (mIsTabListOpen) {
+                mTabListSpinner.performClick();
+            }
 
             for (Tab tab : mTabManager.getAllTabs()) {
                 tab.setFullscreenCallback(this);
@@ -230,13 +247,18 @@ public class WebEngineShellActivity
         mTabCountButton.setText(String.valueOf(getTabsCount()));
         mTabListAdapter =
                 new ArrayAdapter<TabWrapper>(this, android.R.layout.simple_spinner_dropdown_item);
-        for (Tab t : mTabManager.getAllTabs()) {
-            mTabListAdapter.add(new TabWrapper(t));
-        }
         mTabListSpinner.setAdapter(mTabListAdapter);
 
         mTabEventsDelegate = new TabEventsDelegate(mTabManager);
         mTabEventsDelegate.registerObserver(this);
+
+        for (Tab t : mTabManager.getAllTabs()) {
+            TabWrapper tabWrapper = new TabWrapper(t);
+            mTabListAdapter.add(tabWrapper);
+            if (t.equals(mTabManager.getActiveTab())) {
+                mTabListSpinner.setSelection(mTabListAdapter.getPosition(tabWrapper));
+            }
+        }
 
         activeTab.setFullscreenCallback(this);
         mTabManager.registerTabListObserver(new TabListObserver() {
@@ -302,6 +324,12 @@ public class WebEngineShellActivity
                 .setReorderingAllowed(true)
                 .add(R.id.fragment_container_view, webEngine.getFragment(), WEB_FRAGMENT_TAG)
                 .commit();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean("isTabListOpen", mTabListSpinner.isOpen());
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -410,11 +438,23 @@ public class WebEngineShellActivity
         }
     }
 
-    public int getTabsCount() {
+    int getTabsCount() {
         if (mTabManager == null) {
             return 0;
         }
         return mTabManager.getAllTabs().size();
+    }
+
+    void setProgress(double progress) {
+        int progressValue = (int) Math.rint(progress * 100);
+        if (progressValue != mProgressBar.getMax()) {
+            mReloadButton.setImageDrawable(mStopDrawable);
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mReloadButton.setImageDrawable(mRefreshDrawable);
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+        mProgressBar.setProgress(progressValue);
     }
 
     @Override
@@ -454,15 +494,7 @@ public class WebEngineShellActivity
 
     @Override
     public void onLoadProgressChanged(double progress) {
-        int progressValue = (int) Math.rint(progress * 100);
-        if (progressValue != mProgressBar.getMax()) {
-            mReloadButton.setImageDrawable(mStopDrawable);
-            mProgressBar.setVisibility(View.VISIBLE);
-        } else {
-            mReloadButton.setImageDrawable(mRefreshDrawable);
-            mProgressBar.setVisibility(View.INVISIBLE);
-        }
-        mProgressBar.setProgress(progressValue);
+        setProgress(progress);
     }
 
     static class TabWrapper {
