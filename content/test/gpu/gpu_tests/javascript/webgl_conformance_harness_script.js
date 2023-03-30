d1f5390fc412d69a3ee7bef03f80aac69f63aad9
Brian Sheedy <bsheedy@chromium.org>
2023-03-30 09:19:07 +0000

    Send heartbeats from more functions
    Updates the WebGL conformance harness script to send heartbeats when
    navigateToPage() and quietMode() are called. Running either of these is
    indicative of the page being alive, so sending a heartbeat is valid.
    
    quietMode() in particular should hopefully reduce the amount of time
    between heartbeats in longer tests since it is called anytime a test
    calls debug(), regardless of whether it results in visible logging or
    not. This should hopefully provide more reliable mid-test heartbeats
    than log() did since log() is typically only called near the end of
    the test due to the conformance tests buffering log messages.
    
    Bug: 1354797
    Change-Id: Idda840dc3df71b3f1117a79c3e2ca85de187c76a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382868
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124086}

--- content/test/gpu/gpu_tests/javascript/webgl_conformance_harness_script.js
+++ content/test/gpu/gpu_tests/javascript/webgl_conformance_harness_script.js
@@ -125,6 +125,7 @@ testHarness.notifyFinished = function(url) {
   testHarness._finished = true;
 };
 testHarness.navigateToPage = function(src) {
+  wrapper.sendHeartbeatThrottled();
   var testFrame = document.getElementById("test-frame");
   testFrame.src = src;
 };
@@ -136,4 +137,7 @@ window.onerror = function(message, url, line) {
   testHarness.reportResults(null, false, message);
   testHarness.notifyFinished(null);
 };
-window.quietMode = function() { return true; }
+window.quietMode = function() {
+  wrapper.sendHeartbeatThrottled();
+  return true;
+}
