b8dd37a2d9b9b7706e1437b77284260616c83f20
Claudio DeSouza <cdesouza@igalia.com>
2023-03-30 07:38:37 +0000

    [extensions] Remove uses of extension_api_unittest.h (6/30)
    This CL is part of a batch of CLs migrating all uses of
    extension_api_unittest.h to more recent alternatives.
    
    This change corrects the file under the path:
       /chrome/browser/background
    
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 394840
    Change-Id: I85d4242aca20dfde06a8e2104e2ddc5444814ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381799
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124062}

--- chrome/browser/background/background_mode_manager_unittest.cc
+++ chrome/browser/background/background_mode_manager_unittest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_simple_task_runner.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/test_extension_system.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
