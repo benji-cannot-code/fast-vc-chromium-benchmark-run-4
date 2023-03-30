0336cf5263df6fde0f7eb4bd3aa7ce2ad327dc17
Joseph Koshy <jkoshy@chromium.org>
2023-03-30 16:49:13 +0000

    sandbox, partition-alloc: Allow memory regions to be named on Linux.
    This change allows Chrome on Linux to name specific regions of its
    address space using `prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, ...)`,
    improving visibility into Chrome's memory behavior on Linux.
    
    Bug: 1428068
    Change-Id: I17abcbcd3c52e0b32fea793b714be309d9a6c2fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373860
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Joseph Koshy <jkoshy@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Joseph Koshy <jkoshy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124259}

--- base/allocator/partition_allocator/page_allocator_internals_posix.h
+++ base/allocator/partition_allocator/page_allocator_internals_posix.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Security/Security.h>
 #include <mach/mach.h>
 #endif
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
 #include <sys/prctl.h>
 #endif
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
@@ -63,7 +63,7 @@ namespace partition_alloc::internal {
 
 namespace {
 
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
 const char* PageTagToName(PageTag tag) {
   // Important: All the names should be string literals. As per prctl.h in
   // //third_party/android_ndk the kernel keeps a pointer to the name instead
@@ -196,9 +196,9 @@ uintptr_t SystemAllocPagesInternal(uintptr_t hint,
     ret = nullptr;
   }
 
-#if BUILDFLAG(IS_ANDROID)
-  // On Android, anonymous mappings can have a name attached to them. This is
-  // useful for debugging, and double-checking memory attribution.
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
+  // On Android and Linux, anonymous mappings can have a name attached to them.
+  // This is useful for debugging, and double-checking memory attribution.
   if (ret) {
     // No error checking on purpose, testing only.
     prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, ret, length,
