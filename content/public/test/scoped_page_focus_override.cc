e6b9bc3183ce9d8d1316b78551b8f45b9dfa7424
Matt Menke <mmenke@chromium.org>
2023-03-30 17:10:19 +0000

    Migrate Value::Find*Path() calls in /content/public/test
    Value::Find*Path() is deprecated in favor of
    Value::Dict::FindByDottedPath(), so swap out the former for the latter.
    Also switch callsites that were using a hard-coded path without dots to
    Value::Dict::Find() instead. Also, in some cases, migrate a couple other
    nearby deprecated base::Value methods as well.
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1187001
    Change-Id: I6d1a389048a575eee70df32dacddf8cf115719bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382905
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124284}

--- content/public/test/scoped_page_focus_override.cc
+++ content/public/test/scoped_page_focus_override.cc
@@ -40,7 +40,7 @@ void ScopedPageFocusOverride::DispatchProtocolMessage(
       base::JSONReader::Read(message_str);
   ASSERT_TRUE(parsed_message.has_value());
 
-  absl::optional<int> id = parsed_message->FindIntPath("id");
+  absl::optional<int> id = parsed_message->GetDict().FindInt("id");
   if (!id || !*id || *id != last_sent_id_)
     return;
 
