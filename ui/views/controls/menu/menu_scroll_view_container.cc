3bb4d0411d453498de35d7db28fa63e0ec749fdf
Xiaodan Zhu <zxdan@chromium.org>
2023-03-30 20:17:30 +0000

    cros_next: update border types for context menu
    This cl updates the highlight border types of context menu.
    
    For more details, refer to go/cros_next_highlight_border
    
    Bug: b:252365398
    Change-Id: I07a7e78eccb42dc4a2e3946b5cd8168357595700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375966
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124403}

--- ui/views/controls/menu/menu_scroll_view_container.cc
+++ ui/views/controls/menu/menu_scroll_view_container.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "cc/paint/paint_flags.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -493,7 +494,9 @@ void MenuScrollViewContainer::CreateBubbleBorder() {
       background_view_->SetBorder(std::make_unique<HighlightBorder>(
           GetRoundedCorners(),
           // corner_radius_,
-          HighlightBorder::Type::kHighlightBorder1,
+          chromeos::features::IsJellyrollEnabled()
+              ? HighlightBorder::Type::kHighlightBorderOnShadow
+              : HighlightBorder::Type::kHighlightBorder1,
           /*use_light_colors=*/false));
     }
 #endif
