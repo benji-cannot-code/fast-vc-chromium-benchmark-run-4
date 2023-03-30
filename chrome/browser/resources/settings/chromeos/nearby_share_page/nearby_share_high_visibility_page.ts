10a6700e61caa0e4bfabb04bb173c22132c3b14d
dpapad <dpapad@chromium.org>
2023-03-30 01:43:18 +0000

    CrOS Settings: Cleanup <iron-media-query> imports.
     - Several elements were using iron-media-query without importing it.
     - settings-setup-fingerprint-dialog was using a no-op obsolete HTML
       import.
    
    Bug: None
    Change-Id: I0ca75120b5c8ac24288924e799112e94c6fc1fc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382526
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123991}

--- chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_high_visibility_page.ts
+++ chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_high_visibility_page.ts
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_lottie/cr_lottie.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/cr_components/localized_link/localized_link.js';
+import 'chrome://resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import '/shared/nearby_page_template.js';
 import '/shared/nearby_shared_icons.html.js';
 
--- chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.ts
+++ chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.ts
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *    <settings-ui prefs="{{prefs}}"></settings-ui>
  */
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import 'chrome://resources/cr_elements/cr_drawer/cr_drawer.js';
 import 'chrome://resources/cr_elements/cr_page_host_style.css.js';
 import 'chrome://resources/cr_elements/icons.html.js';
