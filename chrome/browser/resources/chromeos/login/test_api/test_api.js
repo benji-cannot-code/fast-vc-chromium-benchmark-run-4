8e7632b7849051323d17472c44fdc20fe6148d44
Sergiy Belozorov <sergiyb@google.com>
2023-03-30 20:57:22 +0000

    Revert "Implement isGaiaDialogVisible test API"
    This reverts commit 31b8cfc6a18336d7e3194fef404907f982648623.
    
    Reasons for revert:
     - incorrectly implemented: OobeTestAPIHandler API does not appear to
       support return values;
     - not needed since GAIA login is not as flaky anymore;
     - there is OobeAPI.screens.GaiaScreen.isReadyForTesting, which could be
       adapted for this purpose if needed in the future.
    
    Original change's description:
    > Implement isGaiaDialogVisible test API
    >
    > The plan is to use this in Tast tests to determine if GAIA webview is visible and to force it to open using `showGaiaDialog` if not.
    >
    > BUG=b:237385867
    > TEST=tested on DUT with https://crrev.com/c/4128615
    >
    > Change-Id: Ic16dbf0a914e652ff121c1f726a5d5de87f2a306
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4145711
    > Reviewed-by: Roman Sorokin <rsorokin@google.com>
    > Reviewed-by: Renato Silva <rrsilva@google.com>
    > Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1091350}
    
    Bug: b:237385867
    Change-Id: Ia208f09cfd1079c692f5c3e12624dd9355d5ebc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381788
    Auto-Submit: Sergiy Belozorov <sergiyb@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124424}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -943,10 +943,6 @@ export class OobeApiProvider {
       chrome.send('OobeTestApi.showGaiaDialog');
     };
 
-    this.isGaiaDialogVisible = function() {
-      chrome.send('OobeTestApi.isGaiaDialogVisible');
-    };
-
     this.getBrowseAsGuestButtonName = function() {
       return loadTimeData.getString('testapi_browseAsGuest');
     };
