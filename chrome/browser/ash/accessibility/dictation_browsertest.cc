bd9e6ec3cc6c19ce6298e32613a7a2314f76cde8
Akihiro Ota <akihiroota@chromium.org>
2023-03-30 19:26:12 +0000

    Dictation: Re-enable disabled dictation tests
    This change re-enables two disabled dictation browsertests.
    I could not reproduce failures for UserEndsDictationWhenChromeVoxEnabled
    even when running many tests in parallel on an MSAN build. For
    ChromeVoxSilencedWhenToggledOn, the issue is that multiple requests
    to stop speech can be sent (see [1] for examples), so we adjust our
    expectation from strict equals to greater than. This makes the test
    stable on a local linux-cros-rel build.
    
    [1] https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/b8794718175011953153/overview
    
    Bug: b:259377386,b:262695761
    Change-Id: I45f72e9760136b8621b8889771a56af416fbe43c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385133
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124357}

--- chrome/browser/ash/accessibility/dictation_browsertest.cc
+++ chrome/browser/ash/accessibility/dictation_browsertest.cc
@@ -728,9 +728,7 @@ IN_PROC_BROWSER_TEST_P(DictationTest, RecognitionEndsWhenInputFieldLosesFocus) {
   EXPECT_EQ("Vega is a star", GetEditableValue());
 }
 
-// TODO(crbug.com/1352312): Flaky.
-IN_PROC_BROWSER_TEST_P(DictationTest,
-                       DISABLED_UserEndsDictationWhenChromeVoxEnabled) {
+IN_PROC_BROWSER_TEST_P(DictationTest, UserEndsDictationWhenChromeVoxEnabled) {
   EnableChromeVox();
   EXPECT_TRUE(GetManager()->IsSpokenFeedbackEnabled());
   InstallMockInputContextHandler();
@@ -744,8 +742,7 @@ IN_PROC_BROWSER_TEST_P(DictationTest,
   WaitForCommitText(kFinalSpeechResult16);
 }
 
-// TODO(crbug.com/1401298): Flaky.
-IN_PROC_BROWSER_TEST_P(DictationTest, DISABLED_ChromeVoxSilencedWhenToggledOn) {
+IN_PROC_BROWSER_TEST_P(DictationTest, ChromeVoxSilencedWhenToggledOn) {
   // Set up ChromeVox.
   test::SpeechMonitor sm;
   EXPECT_FALSE(GetManager()->IsSpokenFeedbackEnabled());
@@ -761,8 +758,9 @@ IN_PROC_BROWSER_TEST_P(DictationTest, DISABLED_ChromeVoxSilencedWhenToggledOn) {
   ToggleDictationWithKeystroke();
   WaitForRecognitionStarted();
 
-  // Assert ChromeVox was asked to stop speaking at the toggle.
-  EXPECT_EQ(1, sm.stop_count());
+  // Assert ChromeVox was asked to stop speaking at the toggle. Note: multiple
+  // requests to stop speech can be sent, so we just expect stop_count() > 0.
+  EXPECT_GT(sm.stop_count(), 0);
 }
 
 IN_PROC_BROWSER_TEST_P(DictationTest, EntersInterimSpeechWhenToggledOff) {
