130b6c2ee3a8a0d14881e918236e9f91d5b7c79c
Chad Duffin <chadduffin@chromium.org>
2023-03-31 16:27:53 +0000

    cellular: Hide "no profiles" when refreshing
    This change updates the cellular subpage UI to hide the "No available
    profiles" when we are refreshing the profile list.
    
    Existing behavior: https://drive.google.com/file/d/1CLyWtnPz3AdREnjHwxiEFNPP4XaqgB44/view?usp=sharing
    Updated behavior: https://drive.google.com/file/d/1C1GiG6tba6VNNWkgN88wIBDKm8WRxd0V/view?usp=sharing
    
    Bug: b/276331006
    Test: Unit tests and verified on a DUT.
    Change-Id: I28f995d686a34c96baec3a9f7e29495cc78020f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387296
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124772}

--- chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.ts
+++ chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.ts
@@ -567,7 +567,8 @@ class CellularNetworksListElement extends CellularNetworksListElementBase {
       inhibitReason: InhibitReason,
       eSimNetworks: NetworkList.NetworkListItemType[],
       eSimPendingProfiles: NetworkList.CustomItemState[]): boolean {
-    if (inhibitReason === InhibitReason.kInstallingProfile) {
+    if (inhibitReason === InhibitReason.kInstallingProfile ||
+        inhibitReason === InhibitReason.kRefreshingProfileList) {
       return false;
     }
 
--- chrome/test/data/webui/settings/chromeos/cellular_networks_list_test.js
+++ chrome/test/data/webui/settings/chromeos/cellular_networks_list_test.js
@@ -375,8 +375,7 @@ suite('CellularNetworksList', function() {
       });
 
   test(
-      'Hide download eSIM link when installing new profile or restrict ' +
-          'cellular network',
+      'Hide download eSIM link when installing / refreshing / restricted by policy',
       async () => {
         eSimManagerRemote.addEuiccForTest(0);
         init();
@@ -408,15 +407,19 @@ suite('CellularNetworksList', function() {
         assertFalse(esimLocalizedLink.hidden);
         assertTrue(noESimFoundMessage.hidden);
 
-        cellularNetworkList.cellularDeviceState = {
-          type: NetworkType.kCellular,
-          deviceState: DeviceStateType.kEnabled,
-          inhibitReason: InhibitReason.kInstallingProfile,
-        };
-        addESimSlot();
-        await flushAsync();
-        assertFalse(!!cellularNetworkList.shadowRoot.querySelector(
-            '#eSimNoNetworkFound'));
+        for (const inhibitReason
+                 of [InhibitReason.kInstallingProfile,
+                     InhibitReason.kRefreshingProfileList]) {
+          cellularNetworkList.cellularDeviceState = {
+            type: NetworkType.kCellular,
+            deviceState: DeviceStateType.kEnabled,
+            inhibitReason: inhibitReason,
+          };
+          addESimSlot();
+          await flushAsync();
+          assertFalse(!!cellularNetworkList.shadowRoot.querySelector(
+              '#eSimNoNetworkFound'));
+        }
       });
 
   test('Fire show cellular setup event on add cellular clicked', async () => {
@@ -515,7 +518,7 @@ suite('CellularNetworksList', function() {
     cellularNetworkList.cellularDeviceState = {
       type: NetworkType.kCellular,
       deviceState: DeviceStateType.kEnabled,
-      inhibitReason: InhibitReason.kRefreshingProfileList,
+      inhibitReason: InhibitReason.kResettingEuiccMemory,
     };
     addESimSlot();
     await flushAsync();
