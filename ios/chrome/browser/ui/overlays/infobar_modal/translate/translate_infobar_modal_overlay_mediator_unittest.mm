e8bd5c66f5ca8d60ff645b7dcf9e2b739bab77bb
Sylvain Defresne <sdefresne@chromium.org>
2023-03-31 13:02:07 +0000

    [ios] Remove duplicated `using` statement
    Remove duplicated line (and sort the `using` statement as required
    by clang-format).
    
    Bug: none
    Change-Id: I74ce5cb6d0a64cf023bd07a44a9c13e37452b571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386402
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124687}

--- ios/chrome/browser/ui/overlays/infobar_modal/translate/translate_infobar_modal_overlay_mediator_unittest.mm
+++ ios/chrome/browser/ui/overlays/infobar_modal/translate/translate_infobar_modal_overlay_mediator_unittest.mm
@@ -29,13 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-using translate_infobar_overlays::TranslateModalRequestConfig;
 using translate_infobar_modal_responses::RevertTranslation;
 using translate_infobar_modal_responses::ToggleAlwaysTranslate;
-using translate_infobar_modal_responses::ToggleNeverTranslateSourceLanguage;
 using translate_infobar_modal_responses::ToggleNeverPromptSite;
+using translate_infobar_modal_responses::ToggleNeverTranslateSourceLanguage;
 using translate_infobar_modal_responses::UpdateLanguageInfo;
-using translate_infobar_modal_responses::UpdateLanguageInfo;
+using translate_infobar_overlays::TranslateModalRequestConfig;
 
 // Base test fixture for TranslateInfobarModalOverlayMediator. The state of the
 // mediator's consumer is expected to vary based on the current TranslateStep.
