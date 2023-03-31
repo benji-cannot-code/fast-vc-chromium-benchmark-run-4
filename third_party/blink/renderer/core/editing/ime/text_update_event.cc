c56527fde7ed78ff3f724c6be0a89429cf408c4f
Dan Clark <daniec@microsoft.com>
2023-03-31 04:32:45 +0000

    EditContext: Update fields in TextUpdateEvent to match spec
    Update fields in TextUpdateEvent:
    updateText --> text
    newSelectionStart --> selectionStart
    newSelectionEnd --> selectionEnd
    
    Also update IDL spec links to point to the newer spec at
    https://w3c.github.io/edit-context
    
    Bug: 999184
    Change-Id: Ifda1854c004c386f7f27e20f520f0c3f6da6b8dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370629
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124570}

--- third_party/blink/renderer/core/editing/ime/text_update_event.cc
+++ third_party/blink/renderer/core/editing/ime/text_update_event.cc
@@ -14,8 +14,9 @@ namespace blink {
 TextUpdateEvent::TextUpdateEvent(const AtomicString& type,
                                  const TextUpdateEventInit* initializer)
     : Event(type, initializer) {
-  if (initializer->hasUpdateText())
-    update_text_ = initializer->updateText();
+  if (initializer->hasText()) {
+    text_ = initializer->text();
+  }
 
   if (initializer->hasUpdateRangeStart())
     update_range_start_ = initializer->updateRangeStart();
@@ -23,25 +24,27 @@ TextUpdateEvent::TextUpdateEvent(const AtomicString& type,
   if (initializer->hasUpdateRangeEnd())
     update_range_end_ = initializer->updateRangeEnd();
 
-  if (initializer->hasNewSelectionStart())
-    new_selection_start_ = initializer->newSelectionStart();
+  if (initializer->hasSelectionStart()) {
+    selection_start_ = initializer->selectionStart();
+  }
 
-  if (initializer->hasNewSelectionEnd())
-    new_selection_end_ = initializer->newSelectionEnd();
+  if (initializer->hasSelectionEnd()) {
+    selection_end_ = initializer->selectionEnd();
+  }
 }
 
 TextUpdateEvent::TextUpdateEvent(const AtomicString& type,
-                                 const String& update_text,
+                                 const String& text,
                                  uint32_t update_range_start,
                                  uint32_t update_range_end,
-                                 uint32_t new_selection_start,
-                                 uint32_t new_selection_end)
+                                 uint32_t selection_start,
+                                 uint32_t selection_end)
     : Event(type, Bubbles::kNo, Cancelable::kYes, ComposedMode::kComposed),
-      update_text_(update_text),
+      text_(text),
       update_range_start_(update_range_start),
       update_range_end_(update_range_end),
-      new_selection_start_(new_selection_start),
-      new_selection_end_(new_selection_end) {}
+      selection_start_(selection_start),
+      selection_end_(selection_end) {}
 
 TextUpdateEvent* TextUpdateEvent::Create(
     const AtomicString& type,
@@ -51,8 +54,8 @@ TextUpdateEvent* TextUpdateEvent::Create(
 
 TextUpdateEvent::~TextUpdateEvent() = default;
 
-String TextUpdateEvent::updateText() const {
-  return update_text_;
+String TextUpdateEvent::text() const {
+  return text_;
 }
 
 uint32_t TextUpdateEvent::updateRangeStart() const {
@@ -63,11 +66,11 @@ uint32_t TextUpdateEvent::updateRangeEnd() const {
   return update_range_end_;
 }
 
-uint32_t TextUpdateEvent::newSelectionStart() const {
-  return new_selection_start_;
+uint32_t TextUpdateEvent::selectionStart() const {
+  return selection_start_;
 }
-uint32_t TextUpdateEvent::newSelectionEnd() const {
-  return new_selection_end_;
+uint32_t TextUpdateEvent::selectionEnd() const {
+  return selection_end_;
 }
 
 const AtomicString& TextUpdateEvent::InterfaceName() const {
