2570f7729da592a5a89fbe9f6e9b21dcf22c9f21
Paulius Trucinskas <ptrucinskas@chromium.org>
2023-03-31 10:30:45 +0000

    [AW] Populate client_side_sampling_status field
    Populating the new system_profile field with the status of client side
    sampling, which is read from the Finch flag WebViewServerSideSampling.
    
    Also updated third_party/metrics_proto via automated script.
    
    Bug: 1417167
    Change-Id: Ie2556126de7944483fed002990a674bea718a0b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4377237
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Paulius Trucinskas <ptrucinskas@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124654}

--- android_webview/browser/aw_metrics_service_client_delegate.cc
+++ android_webview/browser/aw_metrics_service_client_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/aw_browser_process.h"
 #include "android_webview/browser/lifecycle/aw_contents_lifecycle_notifier.h"
+#include "android_webview/browser/metrics/aw_client_side_sampling_status_metrics_provider.h"
 #include "android_webview/browser/metrics/aw_component_metrics_provider_delegate.h"
 #include "android_webview/browser/metrics/aw_metrics_service_client.h"
 #include "android_webview/browser/metrics/aw_server_side_allowlist_metrics_provider.h"
@@ -38,6 +39,9 @@ void AwMetricsServiceClientDelegate::RegisterAdditionalMetricsProviders(
       std::make_unique<tracing::AwBackgroundTracingMetricsProvider>());
   service->RegisterMetricsProvider(
       std::make_unique<AwServerSideAllowlistMetricsProvider>(
+          AwMetricsServiceClient::GetInstance()));
+  service->RegisterMetricsProvider(
+      std::make_unique<AwClientSideSamplingStatusMetricsProvider>(
           AwMetricsServiceClient::GetInstance()));
 }
 
