df4f11b65590d56d027a8f22c1882940d34d073b
W. James MacLean <wjmaclean@chromium.org>
2023-03-31 21:35:52 +0000

    Cleanup handling of initiator_base_url to avoid empty urls.
    This CL was motivated by a desire to add a check in
    RenderFrameImpl to make sure that
    common_params.initiator_base_url was only valid for
    about:blank and about:srcdoc urls, and is never set
    to an empty url when it is valid. This lead to the
    discovery that, due to conversions between optional<GURL>
    and GURL values in the browser process, we were inadvertently
    introducing empty GURLs unnecessarily.
    
    To fix this, this CL converts DidCommitProvisionalLoadParams
    and OpenURLParams to use optional<GURL> for initiator_base_url,
    in order to decrease the number of places conversions are required.
    Then it makes sure that call sites setting initiator_base_url
    don't load empty URLs.
    
    It also installs a CHECK in RenderFrameImpl to make sure the
    common_params initiator_base_url value is only valid (and
    non-empty) for about:blank and about:srcdoc urls.
    
    Bug: 1356658
    Change-Id: Id44e7fce98781dc6616e4a17e34d58990e981e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370723
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124912}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -525,9 +525,10 @@ void FillNavigationParamsRequest(
   // Note: It's possible for initiator_base_url to be empty if this is an
   // error srcdoc page. See test
   // NavigationRequestBrowserTest.OriginForSrcdocErrorPageInSubframe.
-  if (blink::features::IsNewBaseUrlInheritanceBehaviorEnabled() &&
-      common_params.initiator_base_url &&
-      (common_params.url.IsAboutSrcdoc() || common_params.url.IsAboutBlank())) {
+  if (common_params.initiator_base_url) {
+    CHECK(blink::features::IsNewBaseUrlInheritanceBehaviorEnabled() &&
+          (common_params.url.IsAboutSrcdoc() ||
+           common_params.url.IsAboutBlank()));
     navigation_params->fallback_base_url =
         common_params.initiator_base_url.value();
   } else {
@@ -585,8 +586,9 @@ blink::mojom::CommonNavigationParamsPtr MakeCommonNavigationParams(
       has_download_sandbox_flag, from_ad);
 
   absl::optional<GURL> initiator_base_url;
-  if (info->requestor_base_url.IsValid())
+  if (info->requestor_base_url.IsValid()) {
     initiator_base_url = info->requestor_base_url;
+  }
   return blink::mojom::CommonNavigationParams::New(
       info->url_request.Url(), info->url_request.RequestorOrigin(),
       initiator_base_url, std::move(referrer),
