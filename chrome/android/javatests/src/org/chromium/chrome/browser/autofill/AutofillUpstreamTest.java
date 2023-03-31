bdcfb73e4237cd6070d0318c466519a82b0d1d8c
Lijin Shen <lazzzis@google.com>
2023-03-31 22:19:20 +0000

    Disable flaky tests in AutofillUpstreamTest on P builders
    Enable them on Q+.
    
    Bug: 1424178
    Change-Id: I0db2b83463a3e4af3f395f6ee629d69b6d53ebab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4390184
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124937}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillUpstreamTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillUpstreamTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill;
 
+import android.os.Build;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.MediumTest;
 
@@ -17,7 +19,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.infobar.AutofillSaveCardInfoBar;
@@ -105,7 +107,8 @@ public class AutofillUpstreamTest {
     @Test
     @MediumTest
     @Restriction(Restriction.RESTRICTION_TYPE_INTERNET)
-    @DisabledTest(message = "https://crbug.com/1424178")
+    @DisableIf.
+    Build(sdk_is_less_than = Build.VERSION_CODES.Q, message = "https://crbug.com/1424178")
     public void testSaveCardInfoBarWithAllFieldsFilled() throws TimeoutException {
         mActivityTestRule.loadUrl(mServer.getURL(TEST_FORM_URL));
         final WebContents webContents = mActivityTestRule.getActivity().getCurrentWebContents();
@@ -189,7 +192,8 @@ public class AutofillUpstreamTest {
     @Test
     @MediumTest
     @Restriction(Restriction.RESTRICTION_TYPE_INTERNET)
-    @DisabledTest(message = "https://crbug.com/1424178")
+    @DisableIf.
+    Build(sdk_is_less_than = Build.VERSION_CODES.Q, message = "https://crbug.com/1424178")
     public void testSaveCardInfoBarWithEmptyName() throws TimeoutException {
         mActivityTestRule.loadUrl(mServer.getURL(TEST_FORM_URL));
         final WebContents webContents = mActivityTestRule.getActivity().getCurrentWebContents();
