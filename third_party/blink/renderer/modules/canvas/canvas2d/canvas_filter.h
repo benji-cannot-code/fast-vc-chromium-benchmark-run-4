67d3556c03c253a25a74aba394c215b0708ea2bf
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-03-31 22:12:13 +0000

    Run include-what-you-use on b/r/m/c/canvas2d/canvas_filter*
    Change-Id: I8a7b6cb0add5174e951c3e7c2ea43f486ab501ba
    Bug: 1428652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386775
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124931}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_filter.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_filter.h
@@ -6,17 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_CANVAS2D_CANVAS_FILTER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_CANVAS2D_CANVAS_FILTER_H_
 
-#include "third_party/blink/renderer/bindings/core/v8/dictionary.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_union_object_objectarray.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
 #include "third_party/blink/renderer/core/style/filter_operations.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/heap/forward.h"
 
 namespace blink {
 
+class ExecutionContext;
 class ExceptionState;
-// class V8UnionCanvasFilterDictionaryOrCanvasFilterDictionaryArray;
 
 // This class stores an unresolved filter on CanvasRenderingContext2DState that
 // has been created from the CanvasFilter javascript object. It will be parsed
