afafebd9eb4e1c8a475de2b5ba2278989fef7298
David Padlipsky <dpad@google.com>
2023-03-31 01:10:11 +0000

    Reland "Settings Split: Implement keyboard policies in pref handler"
    This is a reland of commit 3db66e4a967c571e946d85e9a5f0f5e4a8fd595c
    
    This can land as the offending commits have been reverted and fixed in
    crrev.com/c/4385726 and crrev.com/c/4384247.
    
    Original change's description:
    > Settings Split: Implement keyboard policies in pref handler
    
    > Bug: b/241965700
    > Change-Id: I5579395a53df6054e3a23bba3086e3b543aecc0c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367666
    > Reviewed-by: Michael Checo <michaelcheco@google.com>
    > Commit-Queue: David Padlipsky <dpad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1123980}
    
    Bug: b/241965700
    Change-Id: Ife1118a775db7c5a6b06f43e52aeea12881291fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385800
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124531}

--- ash/system/input_device_settings/input_device_settings_controller_impl.cc
+++ ash/system/input_device_settings/input_device_settings_controller_impl.cc
@@ -227,8 +227,9 @@ void InputDeviceSettingsControllerImpl::OnActiveUserPrefServiceChanged(
 void InputDeviceSettingsControllerImpl::RefreshAllDeviceSettings() {
   settings_refresh_pending_ = false;
   for (const auto& [id, keyboard] : keyboards_) {
-    keyboard_pref_handler_->InitializeKeyboardSettings(active_pref_service_,
-                                                       keyboard.get());
+    keyboard_pref_handler_->InitializeKeyboardSettings(
+        active_pref_service_, policy_handler_->keyboard_policies(),
+        keyboard.get());
     DispatchKeyboardSettingsChanged(id);
   }
   for (const auto& [id, touchpad] : touchpads_) {
@@ -249,8 +250,12 @@ void InputDeviceSettingsControllerImpl::RefreshAllDeviceSettings() {
 }
 
 void InputDeviceSettingsControllerImpl::OnKeyboardPoliciesChanged() {
-  // TODO(dpad): Implement re-initialization of keyboard settings when the
-  // policies change.
+  for (const auto& [id, keyboard] : keyboards_) {
+    keyboard_pref_handler_->InitializeKeyboardSettings(
+        active_pref_service_, policy_handler_->keyboard_policies(),
+        keyboard.get());
+    DispatchKeyboardSettingsChanged(id);
+  }
 }
 
 const mojom::KeyboardSettings*
@@ -353,8 +358,9 @@ void InputDeviceSettingsControllerImpl::SetKeyboardSettings(
     return;
   }
   found_keyboard.settings = settings.Clone();
-  keyboard_pref_handler_->UpdateKeyboardSettings(active_pref_service_,
-                                                 found_keyboard);
+  keyboard_pref_handler_->UpdateKeyboardSettings(
+      active_pref_service_, policy_handler_->keyboard_policies(),
+      found_keyboard);
   DispatchKeyboardSettingsChanged(id);
   // Check the list of keyboards to see if any have the same |device_key|.
   // If so, their settings need to also be updated.
@@ -581,8 +587,9 @@ void InputDeviceSettingsControllerImpl::OnKeyboardListUpdated(
     // Get initial settings from the pref manager and generate our local
     // storage of the device.
     auto mojom_keyboard = BuildMojomKeyboard(keyboard);
-    keyboard_pref_handler_->InitializeKeyboardSettings(active_pref_service_,
-                                                       mojom_keyboard.get());
+    keyboard_pref_handler_->InitializeKeyboardSettings(
+        active_pref_service_, policy_handler_->keyboard_policies(),
+        mojom_keyboard.get());
     keyboards_.insert_or_assign(keyboard.id, std::move(mojom_keyboard));
     DispatchKeyboardConnected(keyboard.id);
   }
