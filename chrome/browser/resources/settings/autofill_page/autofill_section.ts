da1acbdf9ef885f0b41d1f90a422ab7e0202ea26
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-03-31 10:31:45 +0000

    Revert "[Leipzig][Desktop] Pure local address indication in Settings."
    This reverts commit ab5b1880c225ee067f88c36d5e544374191f861e.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8785117399243273841
    
    Sample failed build: https://ci.chromium.org/b/8785117399243273841
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8785117399243273841&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4342897
    
    Original change's description:
    > [Leipzig][Desktop] Pure local address indication in Settings.
    >
    > Mocks:
    > https://www.figma.com/file/pCvqK0UZhcoDWxbJuEXdvM/%F0%9F%9A%9B-Leipzig?node-id=2433%3A113943&t=sq0jjRvfRTlksvdY-4
    > Screenshot:
    > https://screenshot.googleplex.com/p2iVx5YtimQyitS.png
    >
    > Bug: 1421688
    > Change-Id: I4b1ad66fae69bff315f6f51559ce195863fabac0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342897
    > Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    > Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1124642}
    >
    
    Bug: 1421688
    Change-Id: I6dc96fb9d14ab4d121df5be0dd1d0eb6876d8bd8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4388653
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1124655}

--- chrome/browser/resources/settings/autofill_page/autofill_section.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_section.ts
@@ -68,8 +68,6 @@ export class SettingsAutofillSectionElement extends
 
   static get properties() {
     return {
-      accountInfo_: Object,
-
       /** An array of saved addresses. */
       addresses: Array,
 
@@ -233,13 +231,6 @@ export class SettingsAutofillSectionElement extends
     this.autofillManager_.saveAddress(event.detail);
   }
 
-  private isInAccountOrSyncable_(
-      address: chrome.autofillPrivate.AddressEntry,
-      accountInfo?: chrome.autofillPrivate.AccountInfo): boolean {
-    return address.metadata?.source ===
-        chrome.autofillPrivate.AddressSource.ACCOUNT ||
-        !!accountInfo?.isSyncEnabledForAutofillProfiles;
-  }
   /**
    * @returns the title for the More Actions button corresponding to the address
    *     which is described by `label` and `sublabel`.
