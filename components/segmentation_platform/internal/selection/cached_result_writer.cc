ca254add05271b946a01fcc33106643dee7e22bf
Salvador Guerrero <salg@google.com>
2023-03-31 21:41:00 +0000

    [Segmentation Platform] Fixed model execution to support multi-output models
    Bug: 1427321
    Change-Id: I6bfcd82f67a3f6ff383faa2be15dfbb56c7690d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4383435
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124914}

--- components/segmentation_platform/internal/selection/cached_result_writer.cc
+++ components/segmentation_platform/internal/selection/cached_result_writer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
 #include "components/segmentation_platform/internal/post_processor/post_processor.h"
 #include "components/segmentation_platform/internal/stats.h"
 #include "components/segmentation_platform/public/config.h"
@@ -42,20 +43,20 @@ bool CachedResultWriter::IsPrefUpdateRequiredForClient(
   proto::PredictionResult pred_result = client_result->client_result();
   base::TimeDelta ttl_to_use =
       post_processor.GetTTLForPredictedResult(pred_result);
-
-  bool force_refresh_results = platform_options.force_refresh_results;
-  bool has_expired_results =
+  base::Time expiration_time =
       base::Time::FromDeltaSinceWindowsEpoch(
           base::Microseconds(client_result->timestamp_us())) +
-          ttl_to_use <=
-      clock_->Now();
+      ttl_to_use;
+
+  bool force_refresh_results = platform_options.force_refresh_results;
+  bool has_expired_results = expiration_time <= clock_->Now();
 
   if (!force_refresh_results && !has_expired_results) {
     stats::RecordSegmentSelectionFailure(
         *config,
         stats::SegmentationSelectionFailureReason::kSelectionTtlNotExpired);
     VLOG(1) << __func__ << ": previous client_result"
-            << " has not yet expired.";
+            << " has not yet expired. Expiration: " << expiration_time;
     return false;
   }
   return true;
