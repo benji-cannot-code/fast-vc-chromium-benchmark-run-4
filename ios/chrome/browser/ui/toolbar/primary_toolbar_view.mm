39170e1b2cc11d29bd3eecc178ef23cd02a42516
Christian Xu <christianxu@google.com>
2023-03-31 12:08:30 +0000

    [iOS][Omnibox] Adjust omnibox leading constant
    This CL reverts changes from crrev.com/c/3584087.
    Resets the omnibox leading constant to 10 as the popup refresh has been
    canceled. Mocks available here: crbug.com/1311618
    
    Bug: 1399963
    Change-Id: Ib5648f3a4f9c7ee9f2b186e27ccb0d1c9ac2849b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386398
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124670}

--- ios/chrome/browser/ui/toolbar/primary_toolbar_view.mm
+++ ios/chrome/browser/ui/toolbar/primary_toolbar_view.mm
@@ -326,13 +326,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                        constant:kContractedLocationBarHorizontalMargin],
   ]];
 
-  CGFloat leadingMargin = kExpandedLocationBarLeadingMargin;
-
   // Constraints for contractedNoMarginConstraints.
   [self.contractedNoMarginConstraints addObjectsFromArray:@[
     [self.locationBarContainer.leadingAnchor
         constraintEqualToAnchor:safeArea.leadingAnchor
-                       constant:leadingMargin],
+                       constant:kExpandedLocationBarHorizontalMargin],
     [self.locationBarContainer.trailingAnchor
         constraintEqualToAnchor:safeArea.trailingAnchor
                        constant:-kExpandedLocationBarHorizontalMargin]
@@ -343,7 +341,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         constraintEqualToAnchor:self.cancelButton.leadingAnchor],
     [self.locationBarContainer.leadingAnchor
         constraintEqualToAnchor:safeArea.leadingAnchor
-                       constant:leadingMargin]
+                       constant:kExpandedLocationBarHorizontalMargin]
   ]];
 
   // Trailing StackView constraints.
