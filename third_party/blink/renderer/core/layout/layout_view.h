012a9466b15e142095ae9c08dea4076125dbeb9e
Morten Stenshorne <mstensho@chromium.org>
2023-03-31 14:10:14 +0000

    Don't include <memory> from layout_view.h
    git cl upload complained layout_view.h including unused STL headers when
    I uploaded a change, although my changes didn't affect the need for
    <memory>. Anyway, let's get rid of it, then.
    
    Change-Id: I31f953173c95059bfaa63812b589f38e11dff47b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386405
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124708}

--- third_party/blink/renderer/core/layout/layout_view.h
+++ third_party/blink/renderer/core/layout/layout_view.h
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_LAYOUT_VIEW_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_LAYOUT_VIEW_H_
 
-#include <memory>
-
 #include "base/check_op.h"
 #include "base/dcheck_is_on.h"
 #include "third_party/blink/public/mojom/scroll/scrollbar_mode.mojom-blink.h"
