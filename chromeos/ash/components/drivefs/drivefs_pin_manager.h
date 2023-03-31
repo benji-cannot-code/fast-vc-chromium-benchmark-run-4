8f62571abaf5fdfac6d96d59410ed3818f815392
François Degros <fdegros@chromium.org>
2023-03-31 05:28:43 +0000

    PinManager: Tweak logs
    Ensure that big numbers are formatted with thousands separators for
    better readability in logs.
    
    Bug: None
    Change-Id: I5a1bba9ade7d0cb9b6657a78fc100750ed5737a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387220
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124573}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.h
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.h
@@ -33,6 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace drivefs::pinning {
 
+// Imbue the output stream with a locale that prints numbers with thousands
+// separators.
+COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DRIVEFS)
+std::ostream& NiceNum(std::ostream& out);
+
 // Prints a size in bytes in a human-readable way.
 enum HumanReadableSize : int64_t;
 
