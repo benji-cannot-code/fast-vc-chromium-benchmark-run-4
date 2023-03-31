aecefbb1daaab8231fc45d9d4e79f2e44fcdc21d
Nathan Muggli <nmuggli@google.com>
2023-03-31 16:06:31 +0000

    printing: Add a reject list for IPP zeroconf setup
    Certain printers advertise IPP support but are known not to work with
    IPP.  Add a reject list to easily skip over IPP for specific printers
    during zeroconf setup.  Any other supported protocols for those
    printers are still used.
    
    BUG=b:268531843
    TEST=unit_tests --gtest_filter=ZeroconfPrinterDetectorTest*
    TEST=manual testing with Canon MG492
    
    Change-Id: I71502e750c17ebf84f70c43e6adb61048f560ac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382785
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Nathan Muggli <nmuggli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124760}

--- chrome/browser/ash/printing/zeroconf_printer_detector_unittest.cc
+++ chrome/browser/ash/printing/zeroconf_printer_detector_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
@@ -181,8 +182,9 @@ class ZeroconfPrinterDetectorTest : public testing::Test {
   }
   ~ZeroconfPrinterDetectorTest() override = default;
 
-  void CreateDetector() {
-    detector_ = ZeroconfPrinterDetector::CreateForTesting(&listers_);
+  void CreateDetector(base::flat_set<std::string> ippRejectList = {}) {
+    detector_ =
+        ZeroconfPrinterDetector::CreateForTesting(&listers_, ippRejectList);
     // The previously allocated listers_ are swapped into the detector_, and so
     // the unique_ptr values of the listers_ map are no longer valid at this
     // point.  The ipp[se]_lister_ raw pointers are kept as seperate members to
@@ -507,6 +509,33 @@ TEST_F(ZeroconfPrinterDetectorTest, ServiceTypePriorities) {
   ExpectPrintersAre({MakeExpectedPrinter("Printer5", ServiceType::kLpd)});
 
   lpd_lister_->Remove("Printer5");
+  CompleteTasks();
+  // No entries left.
+  ExpectPrintersEmpty();
+}
+
+// Test a printer that is known not to work with IPP/IPPS and make sure a
+// different protocol is chosen.
+TEST_F(ZeroconfPrinterDetectorTest, RejectIpp) {
+  std::string badIppPrinter = "manufacturer awesome printer-name";
+  base::flat_set<std::string> rejectList;
+  // We have to add the _ty suffix to match how MakeServiceDescription and
+  // MakeExpectedPrinter work.
+  rejectList.insert(badIppPrinter + "_ty");
+  // Advertise on IPP and LPD services.
+  ipp_lister_->Announce(MakeServiceDescription(
+      badIppPrinter, ZeroconfPrinterDetector::kIppServiceName));
+  ipps_lister_->Announce(MakeServiceDescription(
+      badIppPrinter, ZeroconfPrinterDetector::kIppsServiceName));
+  lpd_lister_->Announce(MakeServiceDescription(
+      badIppPrinter, ZeroconfPrinterDetector::kLpdServiceName));
+  CreateDetector(rejectList);
+  CompleteTasks();
+
+  // Should be rejected for IPPS-E and IPP-E, so it should only exist in LPD.
+  ExpectPrintersAre({MakeExpectedPrinter(badIppPrinter, ServiceType::kLpd)});
+
+  lpd_lister_->Remove(badIppPrinter);
   CompleteTasks();
   // No entries left.
   ExpectPrintersEmpty();
