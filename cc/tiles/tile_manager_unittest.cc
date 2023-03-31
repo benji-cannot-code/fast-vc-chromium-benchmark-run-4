1a60cc5e88e00e29c2705d91729c3c419c7fa342
Steve Kobes <skobes@chromium.org>
2023-03-31 16:01:23 +0000

    Flush GPU work before SetReadyToDrawCallback in TileManager.
    RasterBufferProvider wants Flush() before SetReadyToDrawCallback(), but
    this didn't always happen since callback registration might be triggered
    first in LTHI::PrepareToDraw (from BeginImplFrame deadline) rather than
    by the FlushAndIssueSignals() which is posted upon tile task completion.
    
    Landing behind a base::Feature to confirm impact (or lack thereof) on
    scroll unification renderer hang reports (crbug.com/1378021).
    
    Bug: 1378021,1421227
    Change-Id: Ia0ee9055fe1623bdee7448f821d835be769a130a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385454
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124757}

--- cc/tiles/tile_manager_unittest.cc
+++ cc/tiles/tile_manager_unittest.cc
@@ -1943,7 +1943,7 @@ class TileManagerOcclusionTest : public TileManagerTest {
         .WillOnce(testing::Invoke([&run_loop]() { run_loop.Quit(); }));
     tile_manager()->PrepareTiles(state);
     run_loop.Run();
-    tile_manager()->CheckForCompletedTasks();
+    tile_manager()->PrepareToDraw();
   }
 
   TileManager* tile_manager() { return host_impl()->tile_manager(); }
@@ -2098,7 +2098,7 @@ TEST_F(PixelInspectTileManagerTest, LowResHasNoImage) {
         .WillOnce(testing::Invoke([&run_loop]() { run_loop.Quit(); }));
     tile_manager->PrepareTiles(host_impl()->global_tile_state());
     run_loop.Run();
-    tile_manager->CheckForCompletedTasks();
+    tile_manager->PrepareToDraw();
 
     Tile* tile = tiling->TileAt(0, 0);
     // The tile in the tiling was rastered.
@@ -2537,7 +2537,7 @@ TEST_F(InvalidResourceTileManagerTest, InvalidResource) {
       .WillOnce(testing::Invoke([&run_loop]() { run_loop.Quit(); }));
   tile_manager->PrepareTiles(host_impl()->global_tile_state());
   run_loop.Run();
-  tile_manager->CheckForCompletedTasks();
+  tile_manager->PrepareToDraw();
 
   Tile* tile = tiling->TileAt(0, 0);
   ASSERT_TRUE(tile);
