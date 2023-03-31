83577b8c914f0d28beb52e7f323147331a0543e3
Brandon Fong <bjfong@google.com>
2023-03-31 13:20:31 +0000

    [X-Device Restore] Create model to store review tabs detail screen UI
    This CL aims to create the model that is responsible for updating the review tabs detail screen sheet.
    
    Bug: 1426921
    Change-Id: Ief65337611981190d62eb91026064d609aa841fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379638
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1124692}

--- chrome/browser/recent_tabs/internal/android/java/src/org/chromium/chrome/browser/recent_tabs/ui/ForeignSessionItemPropertiesUnitTest.java
+++ chrome/browser/recent_tabs/internal/android/java/src/org/chromium/chrome/browser/recent_tabs/ui/ForeignSessionItemPropertiesUnitTest.java
@@ -20,6 +20,8 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.recent_tabs.ForeignSessionHelper.ForeignSession;
 import org.chromium.ui.modelutil.PropertyModel;
 
+import java.util.ArrayList;
+
 /** Tests for the ForeignSessionItem model. */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
@@ -29,7 +31,7 @@ public class ForeignSessionItemPropertiesUnitTest {
 
     @Before
     public void setUp() {
-        mSession = new ForeignSession("tag", "John's iPhone 6", 32L);
+        mSession = new ForeignSession("tag", "John's iPhone 6", 32L, new ArrayList<>());
         mModel = ForeignSessionItemProperties.create(
                 /*device=*/mSession, /*isSelected=*/false, /*onClickListener=*/() -> {});
     }
