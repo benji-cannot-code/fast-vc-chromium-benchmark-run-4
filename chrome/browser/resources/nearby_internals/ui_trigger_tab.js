448e3ba2cd800c415ece66c1c83acf1485c98605
Alex Kingsborough <akingsb@google.com>
2023-03-31 20:18:48 +0000

    [NP] Create UI files for Nearby Presence Debugging
    Create the files and skeleton code needed to send a Start Scan message
    from the debug UI to browser.
    
    Image of changes: https://screenshot.googleplex.com/AxhnjHRf3DM4Xgb.png
    
    the correct function was called. All files in this cl are to be used only for internal testing and therefore unittests do not apply.
    
    Test: Manually tested with logs to see that the message was receive and
    Change-Id: Ic970bab0d98241f4aae8eb96b2be2b99491a3262
    Fixed: b/276311556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374736
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124877}

--- chrome/browser/resources/nearby_internals/ui_trigger_tab.js
+++ chrome/browser/resources/nearby_internals/ui_trigger_tab.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import './ui_trigger_list_object.js';
+import './cross_device_internals.js';
 import './shared_style.css.js';
 
 import {WebUIListenerBehavior} from 'chrome://resources/ash/common/web_ui_listener_behavior.js';
