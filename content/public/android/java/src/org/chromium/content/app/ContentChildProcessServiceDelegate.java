19d0929a3814f1dc1746d7616441af0b21e6bcc3
Sam Maier <smaier@chromium.org>
2023-03-31 16:16:02 +0000

    Removing concept of MainDex/non-MainDex native
    With automatic registration being the default now, there is no need to
    differentiate MainDex/non-MainDex as automatic registration is lazy
    anyways.
    
    Low-Coverage-Reason: Just a deletion of code
    Bug: 1371542
    Change-Id: I85cc1f72e71eb4b2ed36a06bd8b0a25a484ce3fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385433
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124766}

--- content/public/android/java/src/org/chromium/content/app/ContentChildProcessServiceDelegate.java
+++ content/public/android/java/src/org/chromium/content/app/ContentChildProcessServiceDelegate.java
@@ -13,7 +13,6 @@ import android.os.RemoteException;
 import android.util.SparseArray;
 import android.view.Surface;
 
-import org.chromium.base.JNIUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.UnguessableToken;
@@ -91,8 +90,6 @@ public class ContentChildProcessServiceDelegate implements ChildProcessServiceDe
             initializeLibrary();
             return;
         }
-
-        JNIUtils.enableSelectiveJniRegistration();
 
         LibraryLoader libraryLoader = LibraryLoader.getInstance();
         libraryLoader.getMediator().initInChildProcess();
--- third_party/gvr-android-sdk/gvr_api_jni.h
+++ third_party/gvr-android-sdk/gvr_api_jni.h
@@ -1726,9 +1726,6 @@ static const JNINativeMethod kMethodsGvrApi[] = {
 };
 
 static bool RegisterNativesImpl(JNIEnv* env) {
-  if (base::android::IsSelectiveJniRegistrationEnabled(env))
-    return true;
-
   const int kMethodsGvrApiSize = std::extent<decltype(kMethodsGvrApi)>();
 
   if (env->RegisterNatives(GvrApi_clazz(env), kMethodsGvrApi,
