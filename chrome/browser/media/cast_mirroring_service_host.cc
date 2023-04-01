56848ef2f16a38e6c47fec6945d32fc15128a7d2
Dorian Brandon <dorianbrandon@google.com>
2023-04-01 00:33:39 +0000

    Allow CastMirroringServiceHost to pause and resume the current mirroring session
    Bug: b/270973860
    Change-Id: I711af901ccc6b0403ca4d5ea8005883a9e3ca96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4342172
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Dorian Brandon <dorianbrandon@google.com>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124985}

--- chrome/browser/media/cast_mirroring_service_host.cc
+++ chrome/browser/media/cast_mirroring_service_host.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool.h"
+#include "cast_mirroring_service_host.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/media/cast_remoting_connector.h"
 #include "chrome/browser/media/router/discovery/access_code/access_code_cast_feature.h"
@@ -44,6 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "media/audio/audio_device_description.h"
 #include "media/base/media_switches.h"
+#include "media/capture/mojom/video_capture.mojom.h"
+#include "media/capture/video_capture_types.h"
 #include "media/mojo/mojom/audio_data_pipe.mojom.h"
 #include "media/mojo/mojom/audio_input_stream.mojom.h"
 #include "media/mojo/mojom/audio_processing.mojom.h"
@@ -77,7 +80,8 @@ static const char* kPassthroughSwitches[]{
     switches::kCastStreamingForceEnableHardwareVp8,
     switches::kCastStreamingForceDisableHardwareVp8};
 
-void CreateVideoCaptureHostOnIO(
+mojo::SelfOwnedReceiverRef<media::mojom::VideoCaptureHost>
+CreateVideoCaptureHostOnIO(
     const std::string& device_id,
     blink::mojom::MediaStreamType type,
     mojo::PendingReceiver<media::mojom::VideoCaptureHost> receiver) {
@@ -87,13 +91,27 @@ void CreateVideoCaptureHostOnIO(
           {base::TaskPriority::USER_BLOCKING,
            base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
           base::SingleThreadTaskRunnerThreadMode::DEDICATED);
-  mojo::MakeSelfOwnedReceiver(
+  return mojo::MakeSelfOwnedReceiver(
       std::make_unique<SingleClientVideoCaptureHost>(
           device_id, type,
           base::BindRepeating(&content::VideoCaptureDeviceLauncher::
                                   CreateInProcessVideoCaptureDeviceLauncher,
                               std::move(device_task_runner))),
       std::move(receiver));
+}
+
+void PauseVideoCaptureHostOnIO(media::mojom::VideoCaptureHost* host,
+                               base::UnguessableToken device_id) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  host->Pause(device_id);
+}
+
+void ResumeVideoCaptureHostOnIO(media::mojom::VideoCaptureHost* host,
+                                base::UnguessableToken device_id,
+                                base::UnguessableToken session_id,
+                                media::VideoCaptureParams params) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  host->Resume(device_id, session_id, params);
 }
 
 blink::mojom::MediaStreamType ConvertVideoStreamType(
@@ -130,6 +148,16 @@ bool IsAccessCodeCastTabSwitchingUIEnabled(
              Profile::FromBrowserContext(web_contents->GetBrowserContext()));
 }
 
+// Returns true if this user is allowed to use Access Codes to
+// discover cast devices, and IsAccessCodeCastFreezeUiEnabled flag is enabled.
+bool IsAccessCodeCastFreezeUIEnabled(
+    const content::WebContentsMediaCaptureId& id) {
+  auto* web_contents = GetContents(id);
+  return web_contents &&
+         media_router::IsAccessCodeCastFreezeUiEnabled(
+             Profile::FromBrowserContext(web_contents->GetBrowserContext()));
+}
+
 // Returns the size of the primary display in pixels, or absl::nullopt if it
 // cannot be determined.
 absl::optional<gfx::Size> GetScreenResolution() {
@@ -148,9 +176,10 @@ CastMirroringServiceHost::CastMirroringServiceHost(
     : source_media_id_(source_media_id),
       gpu_client_(nullptr, base::OnTaskRunnerDeleter(nullptr)),
       tab_switching_ui_enabled_(IsAccessCodeCastTabSwitchingUIEnabled(
-          source_media_id.web_contents_id)) {
+          source_media_id.web_contents_id)),
+      freeze_ui_enabled_(
+          IsAccessCodeCastFreezeUIEnabled(source_media_id.web_contents_id)) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
-
   // Observe the target WebContents for Tab mirroring.
   if (source_media_id_.type == content::DesktopMediaID::TYPE_WEB_CONTENTS)
     Observe(GetContents(source_media_id_.web_contents_id));
@@ -275,11 +304,21 @@ void CastMirroringServiceHost::BindGpu(
 
 void CastMirroringServiceHost::GetVideoCaptureHost(
     mojo::PendingReceiver<media::mojom::VideoCaptureHost> receiver) {
-  content::GetIOThreadTaskRunner({})->PostTask(
+  content::GetIOThreadTaskRunner({})->PostTaskAndReplyWithResult(
       FROM_HERE,
       base::BindOnce(&CreateVideoCaptureHostOnIO, source_media_id_.ToString(),
                      ConvertVideoStreamType(source_media_id_.type),
-                     std::move(receiver)));
+                     std::move(receiver)),
+      // Setting the video capture host inside of the
+      // CastMirroringServiceHost class is handled by the browser thread.
+      base::BindOnce(&CastMirroringServiceHost::SetVideoCaptureHost,
+                     weak_factory_for_ui_.GetWeakPtr()));
+}
+
+void CastMirroringServiceHost::SetVideoCaptureHost(
+    mojo::SelfOwnedReceiverRef<media::mojom::VideoCaptureHost>
+        video_capture_host) {
+  video_capture_host_ = video_capture_host;
 }
 
 void CastMirroringServiceHost::GetNetworkContext(
@@ -569,6 +608,25 @@ void CastMirroringServiceHost::OpenOffscreenTab(
   source_media_id_ = BuildMediaIdForWebContents(offscreen_tab_->web_contents());
   DCHECK_EQ(content::DesktopMediaID::TYPE_WEB_CONTENTS, source_media_id_.type);
   Observe(offscreen_tab_->web_contents());
+}
+
+void CastMirroringServiceHost::Pause() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (freeze_ui_enabled_ && video_capture_host_) {
+    content::GetIOThreadTaskRunner({})->PostTask(
+        FROM_HERE, base::BindOnce(&PauseVideoCaptureHostOnIO,
+                                  video_capture_host_->impl(), ignored_token_));
+  }
+}
+
+void CastMirroringServiceHost::Resume() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (freeze_ui_enabled_ && video_capture_host_) {
+    content::GetIOThreadTaskRunner({})->PostTask(
+        FROM_HERE,
+        base::BindOnce(&ResumeVideoCaptureHostOnIO, video_capture_host_->impl(),
+                       ignored_token_, ignored_token_, ignored_params_));
+  }
 }
 
 void CastMirroringServiceHost::GetMirroringStats(
--- chrome/browser/media/cast_mirroring_service_host_browsertest.cc
+++ chrome/browser/media/cast_mirroring_service_host_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
+#include "base/task/bind_post_task.h"
 #include "base/test/test_timeouts.h"
 #include "build/build_config.h"
 #include "chrome/browser/media/router/discovery/access_code/access_code_cast_feature.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/mirroring/mojom/session_observer.mojom.h"
 #include "components/mirroring/mojom/session_parameters.mojom.h"
 #include "components/prefs/pref_service.h"
+#include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
@@ -227,6 +229,24 @@ class CastMirroringServiceHostBrowserTest
     run_loop.Run();
   }
 
+  void PauseMirroring() {
+    base::RunLoop run_loop;
+    host_->Pause();
+    content::GetIOThreadTaskRunner({})->PostTask(
+        FROM_HERE,
+        base::BindPostTaskToCurrentDefault(run_loop.QuitWhenIdleClosure()));
+    run_loop.Run();
+  }
+
+  void ResumeMirroring() {
+    base::RunLoop run_loop;
+    host_->Resume();
+    content::GetIOThreadTaskRunner({})->PostTask(
+        FROM_HERE,
+        base::BindPostTaskToCurrentDefault(run_loop.QuitWhenIdleClosure()));
+    run_loop.Run();
+  }
+
   void StopMirroring() {
     if (video_frame_receiver_) {
       base::RunLoop run_loop;
@@ -420,6 +440,41 @@ IN_PROC_BROWSER_TEST_P(CastMirroringServiceHostBrowserTestTabSwitcher,
   SwitchTabSource();
   GetVideoCaptureHost();
   StartVideoCapturing();
+  StopMirroring();
+}
+
+class CastMirroringServiceHostBrowserTestFreezeSession
+    : public CastMirroringServiceHostBrowserTest {
+ public:
+  CastMirroringServiceHostBrowserTestFreezeSession() {
+    feature_list_.InitWithFeatures({features::kAccessCodeCastFreezeUI}, {});
+  }
+
+  CastMirroringServiceHostBrowserTestFreezeSession(
+      const CastMirroringServiceHostBrowserTestFreezeSession&) = delete;
+  CastMirroringServiceHostBrowserTestFreezeSession& operator=(
+      const CastMirroringServiceHostBrowserTestFreezeSession&) = delete;
+
+  ~CastMirroringServiceHostBrowserTestFreezeSession() override = default;
+
+  void VerifyEnabledFeatures() {
+    ASSERT_TRUE(
+        base::FeatureList::IsEnabled(features::kAccessCodeCastFreezeUI));
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(CastMirroringServiceHostBrowserTestFreezeSession,
+                       FreezeSession) {
+  VerifyEnabledFeatures();
+  EnableAccessCodeCast();
+  StartTabMirroring();
+  GetVideoCaptureHost();
+  StartVideoCapturing();
+  PauseMirroring();
+  ResumeMirroring();
   StopMirroring();
 }
 
