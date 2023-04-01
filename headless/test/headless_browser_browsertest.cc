bccf2d3d70fb0a1651f05a3f42419b32b502e380
Andrey Kosyakov <caseq@chromium.org>
2023-04-01 03:43:29 +0000

    headless: properly handle network service restart
    ... expect HeadlessProxyConfigMonitor::AddToNetworkContextParams() may
    be called more than once and the proxy config monitoring interface may
    have been bound before.
    
    Bug: 1414616
    Change-Id: I3db4fb446dbc0dd56d24f8896af7f0a9eb235b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4390904
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125009}

--- headless/test/headless_browser_browsertest.cc
+++ headless/test/headless_browser_browsertest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/no_renderer_crashes_assertion.h"
+#include "content/public/test/test_navigation_observer.h"
 #include "headless/lib/browser/headless_browser_context_impl.h"
 #include "headless/lib/browser/headless_browser_impl.h"
 #include "headless/lib/browser/headless_select_file_dialog_factory.h"
@@ -820,6 +821,37 @@ IN_PROC_BROWSER_TEST_P(SelectFileDialogHeadlessBrowserTest, SelectFileDialog) {
   WaitForSelectFileDialogCallback();
 
   EXPECT_EQ(select_file_dialog_type_, expected_type());
+}
+
+IN_PROC_BROWSER_TEST_F(HeadlessBrowserTest, NetworkServiceCrash) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  HeadlessBrowserContext* browser_context =
+      browser()->CreateBrowserContextBuilder().Build();
+
+  HeadlessWebContents* headless_web_contents =
+      browser_context->CreateWebContentsBuilder()
+          .SetInitialURL(embedded_test_server()->GetURL("/hello.html"))
+          .Build();
+  ASSERT_TRUE(WaitForLoad(headless_web_contents));
+
+  SimulateNetworkServiceCrash();
+
+  content::WebContents* wc =
+      HeadlessWebContentsImpl::From(headless_web_contents)->web_contents();
+  const GURL new_url = embedded_test_server()->GetURL("/blue_page.html");
+  // Wait for navigaitons including those non-committed and re-try as needed,
+  // as a navigation may be aborted during network service restart.
+  do {
+    wc->GetController().LoadURLWithParams(
+        content::NavigationController::LoadURLParams(new_url));
+
+    content::TestNavigationObserver nav_observer(
+        wc, /* expected_number_of_navigations */ 1,
+        content::MessageLoopRunner::QuitMode::IMMEDIATE,
+        /* ignore_uncommitted_navigations */ false);
+    nav_observer.Wait();
+  } while (wc->GetController().GetLastCommittedEntry()->GetURL() != new_url);
 }
 
 }  // namespace headless
