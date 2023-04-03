244a08e4db66c57825f67ac8eb53447ac36a6123
Viktor Semeniuk <vsemeniuk@google.com>
2023-04-03 13:41:31 +0000

    [Password Manager] Fix isEligibleForAccountStorage computation
    Return false immediately when primary account has sync consent. This
    covers case when sync is in progress.
    
    Fixed: 1429393
    Change-Id: Iaa5d1fa45490776d8839e3a3d7a4a0a271b6c512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386945
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1125322}

--- chrome/browser/ui/webui/password_manager/sync_handler.cc
+++ chrome/browser/ui/webui/password_manager/sync_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
+#include "chrome/browser/sync/sync_ui_util.h"
 #include "components/password_manager/core/browser/password_manager_features_util.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_service_utils.h"
@@ -85,9 +86,12 @@ base::Value::Dict SyncHandler::GetSyncInfo() const {
 
   syncer::SyncService* sync_service = GetSyncService();
   PrefService* pref_service = profile_->GetPrefs();
+
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile_);
   dict.Set("isEligibleForAccountStorage",
-           (IsOptedInForAccountStorage(pref_service, sync_service) ||
-            ShouldShowAccountStorageOptIn(pref_service, sync_service)));
+           (!identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSync) &&
+            (IsOptedInForAccountStorage(pref_service, sync_service) ||
+             ShouldShowAccountStorageOptIn(pref_service, sync_service))));
   return dict;
 }
 
