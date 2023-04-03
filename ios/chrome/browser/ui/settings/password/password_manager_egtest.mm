342583817acb969ddb4c6d0c158c579e0a545f87
Viktor Semeniuk <vsemeniuk@google.com>
2023-04-03 13:39:33 +0000

    Reenable password manager tests after changes to group name logic
    Fixed: 1429627
    Change-Id: I86f2ccca58b34c17f0f63bb53c100567742161fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394422
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125319}

--- ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
@@ -1744,8 +1744,7 @@ id<GREYMatcher> EditDoneButton() {
 // storing just about enough passwords to ensure filling more than one page on
 // any device. To limit the effect of (2), custom large scrolling steps are
 // added to the usual scrolling actions.
-// TODO(crbug.com/1429627): Re-enable after fixing hard-coded issue.
-- (void)DISABLED_testManyPasswords {
+- (void)testManyPasswords {
   if ([ChromeEarlGrey isIPadIdiom]) {
     // TODO(crbug.com/906551): Enable the test on iPad once the bug is fixed.
     EARL_GREY_TEST_DISABLED(@"Disabled for iPad.");
@@ -1775,7 +1774,7 @@ id<GREYMatcher> EditDoneButton() {
 
   if ([self groupingEnabled]) {
     [GetInteractionForPasswordEntry([NSString
-        stringWithFormat:@"www01.example.com, %d accounts", kPasswordsCount])
+        stringWithFormat:@"example.com, %d accounts", kPasswordsCount])
         performAction:grey_tap()];
   } else {
     // The scrolling in GetInteractionForPasswordEntry has too fine steps to
@@ -2312,8 +2311,7 @@ id<GREYMatcher> EditDoneButton() {
 }
 
 // Tests that removing multiple passwords works fine.
-// TODO(crbug.com/1429627): Re-enable after fixing hard-coded issue.
-- (void)DISABLED_testRemovingMultiplePasswords {
+- (void)testRemovingMultiplePasswords {
   constexpr int kPasswordsCount = 4;
 
   // Send the passwords to the queue to be added to the PasswordStore.
@@ -2331,7 +2329,7 @@ id<GREYMatcher> EditDoneButton() {
   TapEdit();
 
   if ([self groupingEnabled]) {
-    [[GetInteractionForPasswordEntry(@"www01.example.com, 4 accounts")
+    [[GetInteractionForPasswordEntry(@"example.com, 4 accounts")
         assertWithMatcher:grey_notNil()] performAction:grey_tap()];
     [[GetInteractionForPasswordEntry(@"example11.com")
         assertWithMatcher:grey_notNil()] performAction:grey_tap()];
