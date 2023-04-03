fea96e811eab591ce75fd22538b9863ebb7b4a2d
Cam Bickel <cambickel@google.com>
2023-04-03 16:12:27 +0000

    shortcuts: Navigate to page on search result click/enter
    This CL updates the Shortcuts app to navigate to the page that a
    shortcut is on when a search result is clicked (or enter is pressed).
    
    Note that focusing on the exact shortcut will be completed in a future
    CL.
    
    This is accomplished using the new Router singleton in router.ts.
    This class is used to manage the URL state of the Shortcuts app,
    and notify observers when the URL of the app changes due to
    Router.navigateTo() being called.
    
    I've also added a new utility function called getURLForSearchResult
    that is used by the SearchResultRow to create the URL used for
    navigation.
    
    To facilitate navigation, this CL also updates the IDs used by the
    ShortcutCustomizationAppElement to be simpler so that later on
    `selectPageById` doesn't need to call `getCategoryNameStringId`.
    
    Finally, this CL adds tests for all of the above.
    
    Test: browser_tests gtest_filter=*ShortcutCustomizationApp*
    Bug: b:266085562
    Change-Id: Ifd0c6f2b07b4323ce24f0402a8c5fd286b4e3451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382889
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125399}

---
+++ ash/webui/shortcut_customization_ui/resources/js/router.ts
@@ -0,0 +1,56 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chrome://resources/js/assert_ts.js';
+
+export interface RouteObserver {
+  onRouteChanged(url: URL): void;
+}
+
+/** The singleton instance. */
+let routerInstance: Router|null = null;
+
+export class Router {
+  static getInstance(): Router {
+    assert(routerInstance, 'Router instance has not been set yet.');
+    return routerInstance;
+  }
+
+  static setInstance(instance: Router): void {
+    assert(routerInstance === null, 'Router instance has already been set.');
+    routerInstance = instance;
+  }
+
+  static resetInstanceForTesting(newInstance: Router): void {
+    routerInstance = newInstance;
+  }
+
+  private routeObservers: Set<RouteObserver>;
+
+  constructor() {
+    this.routeObservers = new Set<RouteObserver>();
+  }
+
+  addObserver(observer: RouteObserver): void {
+    assert(!this.routeObservers.has(observer));
+    this.routeObservers.add(observer);
+  }
+
+  removeObserver(observer: RouteObserver): void {
+    assert(this.routeObservers.delete(observer));
+  }
+
+  /**
+   * Navigates to the given route, and notifies observers.
+   */
+  navigateTo(url: URL): void {
+    window.history.pushState({}, '', url);
+    this.routeObservers.forEach((observer) => {
+      observer.onRouteChanged(url);
+    });
+  }
+}
+
+Router.setInstance(new Router());
