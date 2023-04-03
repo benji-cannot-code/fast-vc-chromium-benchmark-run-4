03806e2f119f3ad58c0ccc5fa9dab5e6b62b934f
Ewann Pelle <ewannpv@chromium.org>
2023-04-03 19:02:11 +0000

    Reland "[iOS][PT][1/N] Add Drag and Drop EG2 tests"
    This is a reland of commit feff5443b1edf3d6f322d2177e8c9987a7da8bf1
    
    It skips tests on iPhone SE (1st gen).
    
    Original change's description:
    > [iOS][PT][1/N] Add Drag and Drop EG2 tests
    >
    > This CL starts implementing the PinnedTabsDragDropTestCase.
    > It also moves `pinned_tabs_constants` in a separate source_set.
    >
    > Bug: 1427970
    > Change-Id: I6319dd4b208537b07919d0a8beffda4b73e5e6bc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4384699
    > Reviewed-by: Petro Akzhygitov <pakzhygitov@google.com>
    > Reviewed-by: Christian Xu <christianxu@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1125251}
    
    Bug: 1427970
    Change-Id: Idec58f393716df1b21a29a557cdb33498b0294b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394431
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125483}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_constants.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/pinned_tabs_constants.h
@@ -28,6 +28,9 @@ extern const NSTimeInterval kPinnedViewPopAnimationTime;
 // Pinned cell identifier.
 extern NSString* const kPinnedCellIdentifier;
 
+// Pinned View identifier.
+extern NSString* const kPinnedViewIdentifier;
+
 // Pinned cell dimensions.
 extern const CGFloat kPinnedCellHeight;
 extern const CGFloat kPinnedCellMaxWidth;
