a111ae4acbb1ed0b9c408c425ec5580625441077
Sean Kau <skau@chromium.org>
2023-04-03 23:29:46 +0000

    Move shimless RMA check to IsDarkModeEnabled()
    Also, use the notification closure in OnSessionStateChanged()
    
    Change-Id: I29edb64b0679e23bc8dc1da662600876e9a00ac8
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387494
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125691}

--- ash/style/dark_light_mode_controller_impl.cc
+++ ash/style/dark_light_mode_controller_impl.cc
@@ -159,6 +159,12 @@ bool DarkLightModeControllerImpl::IsDarkModeEnabled() const {
     return false;
   }
 
+  // Disable dark mode for Shimless RMA.
+  if (features::IsShimlessRMADarkModeDisabled() &&
+      session_state == session_manager::SessionState::RMA) {
+    return false;
+  }
+
   if (is_dark_mode_enabled_in_oobe_for_testing_.has_value()) {
     return is_dark_mode_enabled_in_oobe_for_testing_.value();
   }
@@ -250,19 +256,11 @@ void DarkLightModeControllerImpl::OnActiveUserPrefServiceChanged(
 
 void DarkLightModeControllerImpl::OnSessionStateChanged(
     session_manager::SessionState state) {
+  auto closure = GetNotifyOnDarkModeChangeClosure();
   if (state != session_manager::SessionState::OOBE &&
       state != session_manager::SessionState::LOGIN_PRIMARY) {
     oobe_state_ = OobeDialogState::HIDDEN;
   }
-
-  // Disable dark mode for Shimless RMA
-  if (features::IsShimlessRMADarkModeDisabled() &&
-      state == session_manager::SessionState::RMA) {
-    RefreshColorsOnColorMode(/*is_dark_mode_enabled=*/false);
-    return;
-  }
-
-  RefreshColorsOnColorMode(IsDarkModeEnabled());
 }
 
 void DarkLightModeControllerImpl::RefreshFeatureState() {}
