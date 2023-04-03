283a23a92fdb1a6b25056dd45eba2292c4fd6aa1
Owen Min <zmin@chromium.org>
2023-04-03 20:08:44 +0000

    Disable DisplayResolutionBootTest.Reboot
    Disable PolicyDeviceDisplayResolution/DisplayResolutionBootTest.Reboot/*
    due to flakiness.
    
    Bug: b/276667320
    Change-Id: Ic3f6cc9a1f42df103c750f5427e36f19e6cfba0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395944
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Owners-Override: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Auto-Submit: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125527}

--- chrome/browser/ash/policy/display/display_resolution_handler_browsertest.cc
+++ chrome/browser/ash/policy/display/display_resolution_handler_browsertest.cc
@@ -325,7 +325,8 @@ class DisplayResolutionBootTest
   DeviceDisplayCrosTestHelper helper_;
 };
 
-IN_PROC_BROWSER_TEST_P(DisplayResolutionBootTest, PRE_Reboot) {
+// b/276667320 Disable flaky test
+IN_PROC_BROWSER_TEST_P(DisplayResolutionBootTest, DISABLED_PRE_Reboot) {
   const PolicyValue policy_value = GetParam();
 
   // Set policy.
@@ -357,7 +358,8 @@ IN_PROC_BROWSER_TEST_P(DisplayResolutionBootTest, PRE_Reboot) {
       << "Initial primary display scale after policy set";
 }
 
-IN_PROC_BROWSER_TEST_P(DisplayResolutionBootTest, Reboot) {
+// b/276667320 Disable flaky test
+IN_PROC_BROWSER_TEST_P(DisplayResolutionBootTest, DISABLED_Reboot) {
   const PolicyValue policy_value = GetParam();
 
   AddExternalDisplay(display_helper()->GetDisplayManager());
