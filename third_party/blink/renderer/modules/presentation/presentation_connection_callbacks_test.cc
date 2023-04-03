ad0f053eaa99b93c3cdd34eeeb88c625a483691a
Hui Yingst <nigi@chromium.org>
2023-04-03 19:16:20 +0000

    [Code Health] Remove mojo::remote from PresentationController
    The usage of `mojo::Remote` in garbage collected objects is forbidden
    to avoid any possible issues with use-after-poison and it should be
    replaced by HeapMojoRemote. This CL removes the occurrence of
    mojo::Remote in blink::PresentationController.
    
    Bug: 1381979,1426378
    Change-Id: Ic58a2d89a2cd8eb29a94ef126f62b4d474e5c25f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382442
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Nigi <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125488}

--- third_party/blink/renderer/modules/presentation/presentation_connection_callbacks_test.cc
+++ third_party/blink/renderer/modules/presentation/presentation_connection_callbacks_test.cc
@@ -70,6 +70,9 @@ TEST(PresentationConnectionCallbacksTest, HandleSuccess) {
   ControllerPresentationConnection* connection = callbacks.connection_.Get();
   ASSERT_TRUE(connection);
   EXPECT_EQ(connection->GetState(), PresentationConnectionState::CONNECTING);
+
+  // Connection must be closed before the next connection test.
+  connection->close();
 }
 
 TEST(PresentationConnectionCallbacksTest, HandleReconnect) {
@@ -103,6 +106,9 @@ TEST(PresentationConnectionCallbacksTest, HandleReconnect) {
   EXPECT_EQ(connection, new_connection);
   EXPECT_EQ(new_connection->GetState(),
             PresentationConnectionState::CONNECTING);
+
+  // Connection must be closed before the next connection test.
+  connection->close();
 }
 
 TEST(PresentationConnectionCallbacksTest, HandleError) {
