4e9543a0b391b8f4835a885f7cee4b6c7968a57b
Justin Cohen <justincohen@google.com>
2023-04-03 12:45:01 +0000

    ios: Add CrashKey to USE_BLINK builds.
    Change-Id: Iebd087fecade70a1733f5601a477bf33514e7524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4388549
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125292}

--- ios/web/content/init/ios_content_main_runner.cc
+++ ios/web/content/init/ios_content_main_runner.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/content/init/ios_content_main_runner.h"
 
+#import "components/crash/core/common/crash_key.h"
 #import "content/public/app/content_main.h"
 #import "content/public/app/content_main_runner.h"
 #import "ios/web/content/init/ios_main_delegate.h"
@@ -16,6 +17,8 @@ IOSContentMainRunner::IOSContentMainRunner() {}
 IOSContentMainRunner::~IOSContentMainRunner() {}
 
 int IOSContentMainRunner::Initialize(WebMainParams params) {
+  static crash_reporter::CrashKeyString<4> key("blink");
+  key.Set("yes");
   content_main_delegate_ = std::make_unique<IOSMainDelegate>();
   content::ContentMainParams content_params(content_main_delegate_.get());
   content_params.argc = params.argc;
