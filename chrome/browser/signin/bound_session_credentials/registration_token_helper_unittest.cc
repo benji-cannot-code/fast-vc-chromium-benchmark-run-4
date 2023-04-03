4cd297e1f8c4ed13b37c285bbead5ee397113123
Alex Ilin <alexilin@chromium.org>
2023-04-03 11:44:07 +0000

    [BSC] Fix unit tests under enable_bound_session_credentials
    Otherwise unit_test target fails to build when the buildflag is enabled.
    
    Bug: None
    Change-Id: Iaa995ec00f940e51242b2f17730befae3588f24a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386412
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125276}

--- chrome/browser/signin/bound_session_credentials/registration_token_helper_unittest.cc
+++ chrome/browser/signin/bound_session_credentials/registration_token_helper_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "components/signin/public/base/session_binding_test_utils.h"
 #include "components/unexportable_keys/unexportable_key_service.h"
+#include "components/unexportable_keys/unexportable_key_service_impl.h"
 #include "components/unexportable_keys/unexportable_key_task_manager.h"
 #include "crypto/scoped_mock_unexportable_key_provider.h"
 #include "crypto/signature_verifier.h"
@@ -31,7 +32,7 @@ class RegistrationTokenHelperTest : public testing::Test {
           QUEUED};  // QUEUED - tasks don't run until `RunUntilIdle()` is
                     // called.
   unexportable_keys::UnexportableKeyTaskManager task_manager_;
-  unexportable_keys::UnexportableKeyService unexportable_key_service_;
+  unexportable_keys::UnexportableKeyServiceImpl unexportable_key_service_;
 };
 
 TEST_F(RegistrationTokenHelperTest, Success) {
