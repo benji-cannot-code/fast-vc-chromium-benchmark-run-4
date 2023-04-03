0d844f6b8c3aa26254e99922a340c8015b9b63e9
Dan Harrington <harringtond@chromium.org>
2023-04-03 18:11:32 +0000

    Migrate to HeapMojoRemote for DocumentLoader->ContentSecurityNotifier
    Bug: 1426367
    Change-Id: Ic3c8e954b2952abe1f1b44e5593d18b72b20fc28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382328
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125436}

--- third_party/blink/renderer/core/loader/document_loader.h
+++ third_party/blink/renderer/core/loader/document_loader.h
@@ -86,9 +86,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/resource_error.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_response.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/storage/blink_storage_key.h"
 #include "third_party/blink/renderer/platform/weborigin/referrer.h"
-#include "third_party/blink/renderer/platform/wtf/gc_plugin.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
 
@@ -662,8 +662,7 @@ class CORE_EXPORT DocumentLoader : public GarbageCollected<DocumentLoader>,
   bool data_received_;
   const bool is_error_page_for_failed_navigation_;
 
-  GC_PLUGIN_IGNORE("https://crbug.com/1381979")
-  mojo::Remote<mojom::blink::ContentSecurityNotifier>
+  HeapMojoRemote<mojom::blink::ContentSecurityNotifier>
       content_security_notifier_;
 
   const scoped_refptr<SecurityOrigin> origin_to_commit_;
