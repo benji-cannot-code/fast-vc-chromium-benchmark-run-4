03ef6c782ecd684e94b374065fa9ec9c51c1fbd2
dljames <dljames@google.com>
2023-04-03 23:19:22 +0000

    Reduce nudge delay time for SavedTabGroups synced objects
    Reduces nudge delay time for SavedTabGroups from 11000ms -> 200ms.
    
    This should make cross device updates feel like they are happening in
    real time. The original value was an overlooked error.
    
    Change-Id: Ibd676456113f547ad6e4043d3ceb6033c7ef9e07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4391221
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125686}

--- components/sync/engine/cycle/data_type_tracker.cc
+++ components/sync/engine/cycle/data_type_tracker.cc
@@ -55,7 +55,6 @@ base::TimeDelta GetDefaultLocalChangeNudgeDelay(ModelType model_type) {
       return kVeryBigLocalChangeNudgeDelay;
     case SESSIONS:
     case HISTORY:
-    case SAVED_TAB_GROUP:
       // Sessions is the type that causes the most commit traffic. It gets a
       // custom nudge delay, tuned for a reasonable trade-off between traffic
       // and freshness.
@@ -98,6 +97,7 @@ base::TimeDelta GetDefaultLocalChangeNudgeDelay(ModelType model_type) {
     case PRINTERS_AUTHORIZATION_SERVERS:
     case READING_LIST:
     case USER_CONSENTS:
+    case SAVED_TAB_GROUP:
     case SEND_TAB_TO_SELF:
     case SECURITY_EVENTS:
     case WIFI_CONFIGURATIONS:
