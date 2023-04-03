04fa08e5a80730276ac71535c38088454b13f174
Dave Tapuska <dtapuska@chromium.org>
2023-04-03 22:04:18 +0000

    [autofill] Fix UAF in IBANManagerTest
    In crrev.com/7ba16219 the installed resource bundle was destroyed
    and a new one with a member variable for the mock delegate was
    created. This new resource bundle would then outlive the test and
    the delegate ptr would be pointing at a freed address. Properly
    stash the previous resource bundle and delete the newly allocated
    one after the test is done, restoring the old one.
    
    Bug: 1429078
    Change-Id: I826edee3dea43f4386186a6d4872167a5aa940fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395065
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125620}

--- components/autofill/core/browser/iban_manager_unittest.cc
+++ components/autofill/core/browser/iban_manager_unittest.cc
@@ -70,9 +70,9 @@ class IBANManagerTest : public testing::Test {
 
   void SetUp() override {
     personal_data_manager_.SetAutofillCreditCardEnabled(true);
-    if (ui::ResourceBundle::HasSharedInstance()) {
-      ui::ResourceBundle::CleanupSharedInstance();
-    }
+    original_resource_bundle_ =
+        ui::ResourceBundle::SwapSharedInstanceForTesting(nullptr);
+
     ui::ResourceBundle::InitSharedInstanceWithLocale(
         "en-US", &mock_resource_delegate_,
         ui::ResourceBundle::DO_NOT_LOAD_COMMON_RESOURCES);
@@ -83,6 +83,11 @@ class IBANManagerTest : public testing::Test {
                 autofill_client_.GetAutofillOptimizationGuide()),
             ShouldBlockSingleFieldSuggestions)
         .WillByDefault(testing::Return(false));
+  }
+
+  void TearDown() override {
+    ui::ResourceBundle::CleanupSharedInstance();
+    ui::ResourceBundle::SwapSharedInstanceForTesting(original_resource_bundle_);
   }
 
   // Sets up the TestPersonalDataManager with an IBAN.
@@ -142,6 +147,7 @@ class IBANManagerTest : public testing::Test {
   std::unique_ptr<FormStructure> form_structure_;
   IBANManager iban_manager_;
   testing::NiceMock<ui::MockResourceBundleDelegate> mock_resource_delegate_;
+  raw_ptr<ui::ResourceBundle> original_resource_bundle_;
 };
 
 MATCHER_P(MatchesTextAndFrontendId, suggestion, "") {
