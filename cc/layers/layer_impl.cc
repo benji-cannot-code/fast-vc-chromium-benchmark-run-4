729abba5b91eff415a587744e9a73821330cd51d
Maggie Chen <magchen@chromium.org>
2023-04-03 20:31:56 +0000

    Fix view transition damage in cc DamageTracker
    view_transition_content_surface corresponds to the render surface which produces content drawn by a ViewTransitionContentLayer. Ensure the damage of view_transition_content_surface propagate to the view transition layer's parent surface.
    
    
    Bug: 1422575, 1427717
    
    Change-Id: I1c3cfab4bac90fe31e8a331fdd551bf9921da569
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355996
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125554}

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -987,4 +987,9 @@ gfx::ContentColorUsage LayerImpl::GetContentColorUsage() const {
   return gfx::ContentColorUsage::kSRGB;
 }
 
+viz::ViewTransitionElementResourceId LayerImpl::ViewTransitionResourceId()
+    const {
+  return viz::ViewTransitionElementResourceId();
+}
+
 }  // namespace cc
