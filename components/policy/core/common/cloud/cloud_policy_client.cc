3c770d1297b367a90e8a055e15fe41a15c0787bc
Claudio DeSouza <cdesouza@igalia.com>
2023-04-03 22:37:49 +0000

    Replace base::GUID uses with base::Uuid (29/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/policy/core/common/cloud
    
    This CL was uploaded by git cl split.
    
    R=ftirelo@chromium.org
    
    Bug: 1428566
    Change-Id: I33f2b6f278870b0c07a75b886b9d9b4a798caf16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395927
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125650}

--- components/policy/core/common/cloud/cloud_policy_client.cc
+++ components/policy/core/common/cloud/cloud_policy_client.cc
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/json/json_reader.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/observer_list.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "components/policy/core/common/cloud/client_data_delegate.h"
@@ -258,7 +258,7 @@ void CloudPolicyClient::SetupRegistration(
 // Reusing IDs would mean the server could track clients by their registration
 // attempts.
 void CloudPolicyClient::SetClientId(const std::string& client_id) {
-  client_id_ = client_id.empty() ? base::GenerateGUID() : client_id;
+  client_id_ = client_id.empty() ? base::GenerateUuid() : client_id;
 }
 
 void CloudPolicyClient::Register(const RegistrationParameters& parameters,
