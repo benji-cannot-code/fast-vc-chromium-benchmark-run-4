cfc18d043e23737fee867fae8fd5364575ebb03a
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-04-03 22:05:24 +0000

    [Extensions] Add a sequence checker to IsolatedWorldManager
    The IsolatedWorldManager is only meant to be accessed on a given
    sequence. Add a SEQUENCE_CHECKER to verify this.
    
    Bug: 1429408
    Change-Id: I193bf73d55c6d9ccc6dd7bdadfa27648099d1f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396248
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125622}

--- extensions/renderer/isolated_world_manager.h
+++ extensions/renderer/isolated_world_manager.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "base/sequence_checker.h"
+
 class InjectionHost;
 
 namespace extensions {
@@ -57,6 +59,8 @@ class IsolatedWorldManager {
   using IsolatedWorldMap = std::map<std::string, int>;
 
   IsolatedWorldMap isolated_worlds_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace extensions
