35027261bdc13e5a9e9db8a6e5be96279e5b9d6b
Ben Reich <benreich@chromium.org>
2023-04-03 04:02:11 +0000

    [FilesBulkPinning] Ensure the pref toggle pins files
    When running CalculateRequiredSpace() it sets the `should_pin_` member
    variable to true, this needs to be reset when the preference gets
    toggled otherwise the next pin won't enable syncing.
    
    Fixed: b:275454974
    Test: chromeos_unittests --gtest_filter=*DriveFsPinManagerTest*
    Change-Id: Ieac23bb9ca6c4890d02f10469c15710d8b042cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4393903
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125177}

--- chrome/browser/ash/drive/drive_integration_service.cc
+++ chrome/browser/ash/drive/drive_integration_service.cc
@@ -1286,6 +1286,7 @@ void DriveIntegrationService::ToggleBulkPinning() {
   GetDriveFsHost()->SetAlwaysEnableDocsOffline(enabled);
 
   if (enabled) {
+    pin_manager_->ShouldPin(true);
     pin_manager_->Start();
   } else {
     pin_manager_->Stop();
