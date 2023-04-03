11af33bc141aa90a7f3195323a5cd440ac01c0c5
Irina Fedorova <irfedorova@google.com>
2023-04-03 15:07:34 +0000

    [Code Health] remove ChildProcess.BadMessgeTerminated expired histogram
    Removes ChildProcess.BadMessgeTerminated expired histogram and code
    related to it.
    
    Bug: 1384621
    Change-Id: Ida688f01b8c5c11a1e3781fb126fb6ee99512044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386663
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125358}

--- content/browser/browser_child_process_host_impl.cc
+++ content/browser/browser_child_process_host_impl.cc
@@ -364,12 +364,6 @@ void BrowserChildProcessHostImpl::LaunchWithoutExtraCommandLineSwitches(
     OnProcessConnected();
 }
 
-void BrowserChildProcessHostImpl::HistogramBadMessageTerminated(
-    ProcessType process_type) {
-  UMA_HISTOGRAM_ENUMERATION("ChildProcess.BadMessgeTerminated", process_type,
-                            PROCESS_TYPE_MAX);
-}
-
 #if !BUILDFLAG(IS_ANDROID)
 void BrowserChildProcessHostImpl::SetProcessBackgrounded(bool is_background) {
   DCHECK(child_process_);
@@ -773,8 +767,6 @@ void BrowserChildProcessHostImpl::TerminateProcessForBadMessage(
     const std::string& error) {
   if (!process)
     return;
-  HistogramBadMessageTerminated(
-      static_cast<ProcessType>(process->data_.process_type));
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisableKillAfterBadIPC)) {
     return;
