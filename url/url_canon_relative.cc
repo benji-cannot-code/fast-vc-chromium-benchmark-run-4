52eebb6d1a95e1615770a741c333d6d225e9c37f
Fredrik Söderquist <fs@opera.com>
2023-04-03 10:05:47 +0000

    Resolve bare fragments with a colon against non-hierarchical URLs
    Resolving a bare fragment with a colon against a non-hierarchical URL
    would caused resolution to fail. This in turn would mean that - for
    example - a lookup of a resource would fail.
    
    A bare fragment with a colon should be handled the same as one without.
    Since a scheme cannot start with - or otherwise contain - a '#' the
    IsValidScheme() check will fail. Handle a leading '#' in that case in
    the same way as it is handled for an empty scheme (bare fragment without
    colon).
    
    Bug: 1220361
    Change-Id: I92da53953244ec7e85dba097bc166f84b0b339ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379879
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1125250}

--- url/url_canon_relative.cc
+++ url/url_canon_relative.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_canon.h"
 #include "url/url_canon_internal.h"
 #include "url/url_constants.h"
+#include "url/url_features.h"
 #include "url/url_file.h"
 #include "url/url_parse_internal.h"
 #include "url/url_util.h"
@@ -163,7 +164,12 @@ bool DoIsRelativeURL(const char* base,
 
   // If the scheme isn't valid, then it's relative.
   if (!IsValidScheme(url, scheme)) {
-    if (!is_base_hierarchical) {
+    if (url[begin] == '#' &&
+        base::FeatureList::IsEnabled(
+            kResolveBareFragmentWithColonOnNonHierarchical)) {
+      // |url| is a bare fragment (e.g. "#foo:bar"). This can be resolved
+      // against any base. Fall-through.
+    } else if (!is_base_hierarchical) {
       // Don't allow relative URLs if the base scheme doesn't support it.
       return false;
     }
