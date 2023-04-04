a067cdfe0f3f8d87b068242d6d4b1abac15ef6ae
John Palmer <jopalmer@chromium.org>
2023-04-04 23:05:34 +0000

    Second flag for diacritics
    Add a new flag which allows us to separate the rollout of the feature into 2 phases
    
    1) Feature is available for users
    2) Feature is on by default.
    
    This may simplify any future rollbacks etc.
    
    Fixed: b:274690585
    Change-Id: I1bc8e4e9fd2e15acec83478062f8f34c7ad192a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369018
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126301}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3341,6 +3341,9 @@ extern const char kHotspotDescription[];
 extern const char kDiacriticsOnPhysicalKeyboardLongpressName[];
 extern const char kDiacriticsOnPhysicalKeyboardLongpressDescription[];
 
+extern const char kDiacriticsOnPhysicalKeyboardLongpressDefaultOnName[];
+extern const char kDiacriticsOnPhysicalKeyboardLongpressDefaultOnDescription[];
+
 extern const char kHoldingSpacePredictabilityName[];
 extern const char kHoldingSpacePredictabilityDescription[];
 
