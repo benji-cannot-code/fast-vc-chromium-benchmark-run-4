3d68afad360cd9c65b164f57e3b825c9819d3389
Claudio DeSouza <cdesouza@igalia.com>
2023-04-04 22:25:12 +0000

    Replace base::GUID uses with base::Uuid (39/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/sessions
    
    This CL was uploaded by git cl split.
    
    R=skuhne@chromium.org
    
    Bug: 1428566
    Change-Id: Ic489a29610a5ec487ea1f383528f2049efbedc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396266
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126279}

--- components/sessions/core/session_service_commands.cc
+++ components/sessions/core/session_service_commands.cc
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_set.h"
-#include "base/guid.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/pickle.h"
 #include "base/token.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "components/sessions/core/base_session_service_commands.h"
 #include "components/tab_groups/tab_group_color.h"
@@ -811,7 +811,7 @@ void CreateTabsAndWindows(
         SessionID::id_type tab_id = -1;
         std::string guid;
         if (!it.ReadInt(&tab_id) || !it.ReadString(&guid) ||
-            !base::IsValidGUID(guid)) {
+            !base::Uuid::ParseCaseInsensitive(guid).is_valid()) {
           VLOG(1) << "Failed reading command " << command->id();
           return;
         }
