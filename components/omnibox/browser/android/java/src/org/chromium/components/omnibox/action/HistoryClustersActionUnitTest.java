fe06d30562ad66fc4133bf704b87cb908b084cfc
Tomasz Wiszkowski <ender@google.com>
2023-04-04 00:25:49 +0000

    Simplify OmniboxActions icons. Add ActionSuggestions decorations.
    This change removes the need for `getIcon()` call and
    creates drawable references for icons used by the Omnibox Actions
    that are also used elsewhere.
    
    The references permit us to import a single R resource, which we
    want to be associated with the Omnibox component.
    
    Bug: 1418077
    Change-Id: I0f9eae3e9269c16bf04dfaf64030098b380d93bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382600
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125722}

--- components/omnibox/browser/android/java/src/org/chromium/components/omnibox/action/HistoryClustersActionUnitTest.java
+++ components/omnibox/browser/android/java/src/org/chromium/components/omnibox/action/HistoryClustersActionUnitTest.java
@@ -7,7 +7,6 @@ package org.chromium.components.omnibox.action;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +14,6 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.omnibox.action.OmniboxActionType;
-import org.chromium.components.browser_ui.styles.R;
 
 /**
  * Tests for {@link HistoryClustersAction}.
@@ -24,10 +22,9 @@ import org.chromium.components.browser_ui.styles.R;
 @Config(manifest = Config.NONE)
 public class HistoryClustersActionUnitTest {
     @Test
-    public void getIcon_returnsExpectedIcon() {
+    public void creation_usesExpectedIcon() {
         var action = new HistoryClustersAction("hint", "query");
-        assertEquals(R.drawable.ic_journeys, action.getIcon().iconRes);
-        assertTrue(action.getIcon().tintWithTextColor);
+        assertEquals(HistoryClustersAction.JOURNEYS_ICON, action.icon);
     }
 
     @Test
@@ -60,12 +57,7 @@ public class HistoryClustersActionUnitTest {
         assertThrows(AssertionError.class,
                 ()
                         -> HistoryClustersAction.from(
-                                new OmniboxAction(OmniboxActionType.HISTORY_CLUSTERS, "") {
-                                    @Override
-                                    public ChipIcon getIcon() {
-                                        return null;
-                                    }
-                                }));
+                                new OmniboxAction(OmniboxActionType.HISTORY_CLUSTERS, "", null)));
     }
 
     @Test
