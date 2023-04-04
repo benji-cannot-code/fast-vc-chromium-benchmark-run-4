7fa19ff8f6f7ba4932791b8649d0c0c05db724a4
Claudio DeSouza <cdesouza@igalia.com>
2023-04-04 16:14:09 +0000

    Replace base::GUID uses with base::Uuid (8/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/bookmarks
    
    This CL was uploaded by git cl split.
    
    R=mastiz@chromium.org
    
    Bug: 1428566
    Change-Id: If73f55b7421abe58a5b88619dfca95aa3a95c9a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394799
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126029}

--- components/bookmarks/managed/managed_bookmarks_tracker.cc
+++ components/bookmarks/managed/managed_bookmarks_tracker.cc
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/memory/ptr_util.h"
 #include "base/notreached.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_node.h"
@@ -62,7 +62,7 @@ int64_t ManagedBookmarksTracker::LoadInitial(BookmarkNode* folder,
       continue;
 
     BookmarkNode* child = folder->Add(std::make_unique<BookmarkNode>(
-        next_node_id++, base::GUID::GenerateRandomV4(), url));
+        next_node_id++, base::Uuid::GenerateRandomV4(), url));
     child->SetTitle(title);
     if (children) {
       child->set_date_folder_modified(base::Time::Now());
