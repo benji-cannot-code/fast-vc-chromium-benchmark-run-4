58ba81d29954e31edc9dcfc173038ca29f038e68
Yuzu Saijo <yuzus@chromium.org>
2023-04-04 02:58:30 +0000

    [bfcache] Expose JSLocation flag to blink/common
    This CL makes bfcache JSlocation flag exposed to blink/common so that we
    can use it in the browsertest.
    
    Bug: 1366675
    Change-Id: I9084eecd665f60037ba6d1efd98ce00a3e9bad4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394404
    Auto-Submit: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125775}

--- third_party/blink/renderer/platform/scheduler/common/frame_or_worker_scheduler.cc
+++ third_party/blink/renderer/platform/scheduler/common/frame_or_worker_scheduler.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/callback.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/platform/bindings/source_location.h"
 #include "v8/include/v8-isolate.h"
 
@@ -17,16 +18,11 @@ namespace blink {
 
 namespace {
 
-// When enabled, Source Location blocking BFCache is captured
-// to send it to the browser.
-BASE_FEATURE(kRegisterJSSourceLocationBlockingBFCache,
-             "RegisterJSSourceLocationBlockingBFCache",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // Returns whether features::kRegisterJSSourceLocationBlockingBFCache is
 // enabled.
 bool IsRegisterJSSourceLocationBlockingBFCache() {
-  return base::FeatureList::IsEnabled(kRegisterJSSourceLocationBlockingBFCache);
+  return base::FeatureList::IsEnabled(
+      blink::features::kRegisterJSSourceLocationBlockingBFCache);
 }
 
 }  // namespace
