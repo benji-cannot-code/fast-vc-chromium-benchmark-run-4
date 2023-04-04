ebbdc316ddcd46a211c15f15468535c380ddf48d
Jimmy <jimmyxgong@chromium.org>
2023-04-04 21:31:28 +0000

    shortcuts: Use real AddAccelerator Mojo api
    This change will add support to add a custom accelerator.
    
    Bug: b:216049298
    Change-Id: I60e3d06dc5eff74849aec567fe99aa62e6404d60
    Teset: browser_test
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368158
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126242}

--- ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
+++ ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
@@ -97,10 +97,6 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
   addAccelerator(
       _source: AcceleratorSource, _actionId: number,
       _accelerator: Accelerator): Promise<{result: AcceleratorResultData}> {
-    // Always return kSuccess in this fake.
-    const result = new AcceleratorResultData();
-    result.result = AcceleratorConfigResult.kSuccess;
-    this.methods.setResult('addAccelerator', {result});
     return this.methods.resolveMethod('addAccelerator');
   }
 
@@ -159,6 +155,10 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
 
   setFakeHasLauncherButton(hasLauncherButton: boolean): void {
     this.methods.setResult('hasLauncherButton', {hasLauncherButton});
+  }
+
+  setFakeAddAcceleratorResult(result: AcceleratorResultData): void {
+    this.methods.setResult('addAccelerator', {result});
   }
 
   // Sets up an observer for methodName.
