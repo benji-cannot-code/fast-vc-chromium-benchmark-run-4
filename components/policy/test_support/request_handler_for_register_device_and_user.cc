fa71583db71505ea42f68392ae094ca232c45604
Claudio DeSouza <cdesouza@igalia.com>
2023-04-04 03:07:29 +0000

    Replace base::GUID uses with base::Uuid (30/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/policy
    
    This CL was uploaded by git cl split.
    
    R=ftirelo@chromium.org
    
    Bug: 1428566
    Change-Id: Icc586af365393aa47e3d84943ffb94a599e14316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4392576
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1125782}

--- components/policy/test_support/request_handler_for_register_device_and_user.cc
+++ components/policy/test_support/request_handler_for_register_device_and_user.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
-#include "base/guid.h"
 #include "base/notreached.h"
 #include "base/strings/stringprintf.h"
+#include "base/uuid.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "components/policy/test_support/client_storage.h"
@@ -165,7 +165,7 @@ RequestHandlerForRegisterDeviceAndUser::RegisterDeviceAndSendResponse(
     const std::string& policy_user) {
   std::string device_id =
       KeyValueFromUrl(request.GetURL(), dm_protocol::kParamDeviceID);
-  std::string device_token = base::GUID::GenerateRandomV4().AsLowercaseString();
+  std::string device_token = base::Uuid::GenerateRandomV4().AsLowercaseString();
   std::string machine_name = base::StringPrintf(
       "%s - %s", register_request.machine_model().c_str(), device_id.c_str());
 
