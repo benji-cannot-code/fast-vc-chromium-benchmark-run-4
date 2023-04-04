c6901e722bc5a92f28dbaa49ffa555c07d837e14
Alexis Hétu <sugoi@chromium.org>
2023-04-04 15:12:33 +0000

    New BottomSheetViewController class for Password Bottom Sheet
    This CL adds a new BottomSheetViewController class which is responsible
    for creating the UI elements for the Password Bottom Sheet, which includes a table with suggestions, a button to use a suggestion and a button to dismiss the bottom sheet.
    
    This CL also adds support for a tableView in ConfirmationAlertViewController. The tableView object, if provided, will be located below the subtitle view. This tableView will be used to provide a list of credentials in the context of the Password Bottom Sheet feature.
    
    Design doc: go/ios-password-bottom-sheet
    
    Bug: 1422362, 1422364, 1422332
    Change-Id: I1e348c6746a8d0b9d5d3098d368e9254882cc6b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336305
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125989}

--- ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_mediator.mm
+++ ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_mediator.mm
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/raw_ptr.h"
 #import "components/autofill/ios/form_util/form_activity_params.h"
+#import "ios/chrome/browser/autofill/bottom_sheet/bottom_sheet_tab_helper.h"
 #import "ios/chrome/browser/autofill/form_input_suggestions_provider.h"
 #import "ios/chrome/browser/autofill/form_suggestion_tab_helper.h"
 #import "ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_consumer.h"
 #import "ios/chrome/browser/web_state_list/active_web_state_observation_forwarder.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer_bridge.h"
+#import "ios/web/public/js_messaging/web_frame_util.h"
 #import "ios/web/public/web_state.h"
 #import "ios/web/public/web_state_observer_bridge.h"
 
@@ -40,6 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Whether the field that triggered the bottom sheet will need to refocus when
   // the bottom sheet is dismissed. Default is true.
   bool _needsRefocus;
+
+  // Web Frame associated with this bottom sheet.
+  std::string _frameId;
 }
 
 - (instancetype)initWithWebStateList:(WebStateList*)webStateList
@@ -47,6 +52,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                   (const autofill::FormActivityParams&)params {
   if (self = [super init]) {
     _needsRefocus = true;
+    _frameId = params.frame_id;
+
     _webStateList = webStateList;
     web::WebState* activeWebState = _webStateList->GetActiveWebState();
 
@@ -104,6 +111,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _needsRefocus = false;
   FormSuggestion* suggestion = [self.suggestions objectAtIndex:row];
   [self.suggestionsProvider didSelectSuggestion:suggestion];
+}
+
+- (void)refocus {
+  if (_needsRefocus && _webStateList) {
+    web::WebState* activeWebState = _webStateList->GetActiveWebState();
+    web::WebFrame* frame = web::GetWebFrameWithId(activeWebState, _frameId);
+    BottomSheetTabHelper::FromWebState(activeWebState)
+        ->DetachListenersAndRefocus(frame);
+  }
 }
 
 #pragma mark - WebStateListObserver
