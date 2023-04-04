e97454a49cb1e8addcc0ed220e8a330d3b0ce2a1
S. Ganesh <ganesh@chromium.org>
2023-04-04 17:10:29 +0000

    Use SystemTemp for secure temp directories in Windows
    r1067808 introduced a Secure temp directory creation for Windows using
    %programfiles%. While that works well in terms of security, it is
    unexpected to create temp files under %programfiles%.
    
    Will (wfh@) discovered `%systemroot%\SystemTemp`, which is only
    accessible to admins and local system. This CL uses
    `%systemroot%\SystemTemp` instead of %programfiles%, if the former
    directory is available on the system.
    
    I tested various versions of Windows 10, and SystemTemp was added
    somewhere after build 17134 and before build 19042.
    
    Given that we would like to support older versions of Windows 10, I also
    added a fallback to the original behavior of using %programfiles%.
    
    Bug: 1429319
    Change-Id: I2087e98a188eae663a19627b6bbff3f7e3c6c793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385936
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126069}

--- chrome/installer/setup/installer_crash_reporting.cc
+++ chrome/installer/setup/installer_crash_reporting.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/install_static/install_details.h"
 #include "chrome/installer/setup/installer_crash_reporter_client.h"
 #include "chrome/installer/setup/installer_state.h"
-#include "chrome/installer/setup/setup_util.h"
 #include "chrome/installer/util/google_update_settings.h"
 #include "components/crash/core/app/crashpad.h"
 #include "components/crash/core/common/crash_key.h"
@@ -46,11 +45,11 @@ const char* OperationToString(InstallerState::Operation operation) {
   return "";
 }
 
-// Returns the path returned by `installer::GetSecureSystemTemp` if available.
+// Returns the path returned by `base::GetSecureSystemTemp` if available.
 // Otherwise, retrieves the SYSTEM version of TEMP. We do this instead of
 // GetTempPath so that both elevated and SYSTEM runs share the same directory.
 bool GetSystemTemp(base::FilePath* temp) {
-  if (GetSecureSystemTemp(temp)) {
+  if (base::GetSecureSystemTemp(temp)) {
     return true;
   }
 
--- chrome/installer/setup/setup_util_unittest.cc
+++ chrome/installer/setup/setup_util_unittest.cc
@@ -16,11 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
-#include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/logging.h"
-#include "base/path_service.h"
 #include "base/process/kill.h"
 #include "base/process/launch.h"
 #include "base/process/process_handle.h"
@@ -736,24 +734,6 @@ TEST(SetupUtilTest, DeleteDMTokenFromRegistryWhenKeyNotFound) {
 TEST(SetupUtilTest, WerHelperRegPath) {
   // Must return a valid regpath, never an empty string.
   ASSERT_FALSE(installer::GetWerHelperRegistryPath().empty());
-}
-
-TEST(SetupUtilTest, GetSecureSystemTemp) {
-  base::FilePath secure_system_temp;
-  ASSERT_EQ(installer::GetSecureSystemTemp(&secure_system_temp),
-            !!::IsUserAnAdmin());
-  if (!::IsUserAnAdmin()) {
-    return;
-  }
-
-  base::FilePath dir_windows;
-  ASSERT_TRUE(base::PathService::Get(base::DIR_WINDOWS, &dir_windows));
-  base::FilePath dir_program_files;
-  ASSERT_TRUE(
-      base::PathService::Get(base::DIR_PROGRAM_FILES, &dir_program_files));
-
-  ASSERT_TRUE((dir_windows.AppendASCII("SystemTemp") == secure_system_temp) ||
-              (dir_program_files == secure_system_temp));
 }
 
 namespace installer {
