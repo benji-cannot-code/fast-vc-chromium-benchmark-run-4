581fb97dec84956f9cccc7b61137012edd78b4c5
Elly Fong-Jones <ellyjones@chromium.org>
2023-04-04 15:42:41 +0000

    extensions: pass BrowserContext to InstallObserver
    This will enable a refactor away from a deprecated content notification
    (namely NOTIFICATION_EXTENSION_INSTALL_ERROR) by making InstallObserver
    usable for classes that need to observe multiple profiles. See the
    linked bug for details.
    
    Bug: 1174734
    Change-Id: Id875bdabef449a5ac16934b127cf4c3d3d559f17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367924
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126001}

--- chrome/browser/sync/test/integration/apps_helper.cc
+++ chrome/browser/sync/test/integration/apps_helper.cc
@@ -377,6 +377,7 @@ void AppsStatusChangeChecker::OnExtensionStateChanged(
 }
 
 void AppsStatusChangeChecker::OnAppsReordered(
+    content::BrowserContext* context,
     const absl::optional<std::string>& extension_id) {
   CheckExitCondition();
 }
--- chrome/browser/ui/webui/ntp/app_launcher_handler.h
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.h
@@ -91,6 +91,7 @@ class AppLauncherHandler
 
   // extensions::InstallObserver
   void OnAppsReordered(
+      content::BrowserContext* context,
       const absl::optional<std::string>& extension_id) override;
 
   // extensions::ExtensionRegistryObserver:
