ac019ba02096f78593919b926adfbb0d9ec9e3a5
Stephan Hartmann <stha09@googlemail.com>
2023-04-04 14:51:19 +0000

    IWYU: add stdint.h for uint64_t in EncounteredSurfaceTracker
    Bug: 957519
    Change-Id: Id454f688c3067c4755c2c1353f22e0f75643aed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396357
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125978}

--- chrome/browser/privacy_budget/encountered_surface_tracker.h
+++ chrome/browser/privacy_budget/encountered_surface_tracker.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PRIVACY_BUDGET_ENCOUNTERED_SURFACE_TRACKER_H_
 #define CHROME_BROWSER_PRIVACY_BUDGET_ENCOUNTERED_SURFACE_TRACKER_H_
 
+#include <stdint.h>
+
 #include <map>
 
 #include "base/containers/flat_set.h"
