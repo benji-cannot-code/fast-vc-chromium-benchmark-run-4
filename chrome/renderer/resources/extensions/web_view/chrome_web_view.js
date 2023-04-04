d1726c6d42ac815921844867299a4acd9ccc2a2b
Chase Phillips <cmp@chromium.org>
2023-04-04 02:25:32 +0000

    webview: Split ChromeWebViewElement out of the ChromeWebView source
    An upcoming Controlled Frame change will use ChromeWebViewImpl directly.
    To prepare for that change, split ChromeWebViewElement out of the
    "chromeWebView" source file.
    
    This change should have no behavior change.
    
    Bug: 1233993, b/275423695
    Change-Id: I7ab4335aab3ea727338b4a5a462d3964c28f6500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374834
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125762}

--- chrome/renderer/resources/extensions/web_view/chrome_web_view.js
+++ chrome/renderer/resources/extensions/web_view/chrome_web_view.js
@@ -12,11 +12,8 @@ var ChromeWebViewSchema =
 var CreateEvent = require('guestViewEvents').CreateEvent;
 var GuestViewInternalNatives = requireNative('guest_view_internal');
 var idGeneratorNatives = requireNative('id_generator');
-var registerElement = require('guestViewContainerElement').registerElement;
 var utils = require('utils');
-var WebViewElement = require('webViewElement').WebViewElement;
 var WebViewImpl = require('webView').WebViewImpl;
-var WebViewAttributeNames = require('webViewConstants').WebViewAttributeNames;
 
 // This is the only "webViewInternal.onClicked" named event for this renderer.
 //
@@ -207,15 +204,4 @@ ChromeWebViewImpl.prototype.setupContextMenus = function() {
       });
 };
 
-class ChromeWebViewElement extends WebViewElement {
-  static get observedAttributes() {
-    return WebViewAttributeNames;
-  }
-
-  constructor() {
-    super();
-    privates(this).internal = new ChromeWebViewImpl(this);
-  }
-}
-
-registerElement('WebView', ChromeWebViewElement);
+exports.$set('ChromeWebViewImpl', ChromeWebViewImpl);
