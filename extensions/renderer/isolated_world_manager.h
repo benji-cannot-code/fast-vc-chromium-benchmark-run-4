ba095cbe6260b7b74f3882e59ef7f51505e849fc
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-04-04 01:46:18 +0000

    [Extensions] Update IsolatedWorldManager for multiple worlds per host
    Update IsolatedWorldManager to support every host having multiple
    isolated worlds. This is in preparation for supporting a new user
    script context world for extensions.
    
    This change primarily involves changing the key of the map of worlds
    from using host ID to using world ID, since host IDs may soon have more
    than one world associated with them. This also adds a new struct to
    store details about the isolated world in the map.
    
    This CL should have no behavior change.
    
    Bug: 1429408
    Change-Id: Iaa540c9df6f32c0df5135765818b5e3c05331351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386235
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125754}

--- extensions/renderer/isolated_world_manager.h
+++ extensions/renderer/isolated_world_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/sequence_checker.h"
+#include "extensions/common/mojom/execution_world.mojom.h"
 
 class InjectionHost;
 
@@ -45,18 +46,28 @@ class IsolatedWorldManager {
   // or an empty string if none exists.
   std::string GetHostIdForIsolatedWorld(int world_id);
 
-  // Removes the isolated world associated with the given `host_id`, if any
-  // exists.
-  void RemoveIsolatedWorld(const std::string& host_id);
+  // Removes all isolated worlds associated with the given `host_id`, if any
+  // exist.
+  void RemoveIsolatedWorlds(const std::string& host_id);
 
   // Returns the id of the isolated world associated with the given
   // `injection_host`.  If none exists, creates a new world for it associated
   // with the host's name and CSP.
-  int GetOrCreateIsolatedWorldForHost(const InjectionHost& injection_host);
+  int GetOrCreateIsolatedWorldForHost(const InjectionHost& injection_host,
+                                      mojom::ExecutionWorld execution_world);
 
  private:
-  // A map between injection host ID and isolated world ID.
-  using IsolatedWorldMap = std::map<std::string, int>;
+  struct IsolatedWorldInfo {
+    // The id of the injection host the world is associated with. For
+    // extensions, this is the extension ID.
+    std::string host_id;
+    // The execution world for the isolated world. Currently, this is restricted
+    // to mojom::ExecutionWorld::kIsolated.
+    mojom::ExecutionWorld execution_world;
+  };
+
+  // A map between the isolated world ID and injection host ID.
+  using IsolatedWorldMap = std::map<int, IsolatedWorldInfo>;
 
   IsolatedWorldMap isolated_worlds_;
 
