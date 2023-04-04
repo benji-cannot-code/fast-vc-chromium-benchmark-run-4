f0e4b7923312113ef1ca806445c5124845973efd
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-04-04 03:43:31 +0000

    [RemoveLegacy] Remove offset_mapping_ & LayoutBlockFlowRareData.
    There should be no behaviour change.
    
    Bug: 1229581
    Change-Id: I6011a58e16c033418e932bd23f54f27465fcb2a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395570
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125793}

--- third_party/blink/renderer/core/layout/layout_block_flow_hot.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow_hot.cc
@@ -54,7 +54,7 @@ void LayoutBlockFlow::MarkAllDescendantsWithFloatsForLayout(
 
 void LayoutBlockFlow::Trace(Visitor* visitor) const {
   visitor->Trace(line_boxes_);
-  visitor->Trace(rare_data_);
+  visitor->Trace(multi_column_flow_thread_);
   LayoutBlock::Trace(visitor);
 }
 
