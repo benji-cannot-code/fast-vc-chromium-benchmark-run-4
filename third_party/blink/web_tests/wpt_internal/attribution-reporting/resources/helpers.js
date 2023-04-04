a119f16cd0a57808196e398036bf3205cf4d2635
Anthony Garant <anthonygarant@chromium.org>
2023-04-04 12:42:44 +0000

    Remove attribution reporting WPT tests dependency on Trigger Buffering
    Current WPT tests register a trigger right after the source and then
    wait for a report to be created. There is a race condition between the
    source & trigger registration with this setup. If the trigger is handled
    first, it won’t match any source and the test will fail.
    
    Today, this is mitigated by a trigger buffering strategy that delays
    triggers for 5 seconds if sources are being registered. This strategy is
    being replaced to be navigation-linked, see this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/4366461 which means that tests won’t be able to rely on trigger buffering.
    
    This CL makes the tests wait explicitly for a source to be registered
    before intending to register a trigger that should match the source,
    hence removing their dependency on trigger buffering.
    
    Bug: 1425414
    Change-Id: I1cd5425c17e82921e040b5c8a5de08eb7e2bad87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386776
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125924}

--- third_party/blink/web_tests/wpt_internal/attribution-reporting/resources/helpers.js
+++ third_party/blink/web_tests/wpt_internal/attribution-reporting/resources/helpers.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Helper functions for attribution reporting API tests.
  */
 
-const blankURL = (base = location.origin) => new URL('/wpt_internal/attribution-reporting/resources/empty.py', base);
+const blankURL = (base = location.origin) => new URL('/wpt_internal/attribution-reporting/resources/reporting_origin.py', base);
 
 const attribution_reporting_promise_test = (f, name) =>
     promise_test(async t => {
@@ -48,14 +48,10 @@ const pipeHeaderPattern = /[,)]/g;
 // , and ) in pipe values must be escaped with \
 const encodeForPipe = urlString => urlString.replace(pipeHeaderPattern, '\\$&');
 
-const blankURLWithHeaders = (headers, origin, status) => {
+const blankURLWithHeaders = (headers, origin) => {
   const url = blankURL(origin);
 
   const parts = headers.map(h => `header(${h.name},${encodeForPipe(h.value)})`);
-
-  if (status !== undefined) {
-    parts.push(`status(${encodeForPipe(status)})`);
-  }
 
   if (parts.length > 0) {
     url.searchParams.set('pipe', parts.join('|'));
@@ -129,7 +125,6 @@ const registerAttributionSrc = async (t, {
 
   const eligible = searchParams.get('eligible');
 
-  let status;
   let headers = [];
 
   if (source) {
@@ -158,12 +153,6 @@ const registerAttributionSrc = async (t, {
                   }]), reportingOrigin), {credentials: params.credentials}));
   }
 
-  // a and open with valueless attributionsrc support registrations on all
-  // but the last request in a redirect chain, so add a no-op redirect.
-  if (eligible !== null && (method === 'a' || method === 'open')) {
-    headers.push({name: 'Location', value: blankURL().toString()});
-    status = '302';
-  }
 
   let credentials;
   if (method === 'fetch') {
@@ -172,7 +161,12 @@ const registerAttributionSrc = async (t, {
     headers = headers.concat(params.headers);
   }
 
-  const url = blankURLWithHeaders(headers, reportingOrigin, status);
+  const url = blankURLWithHeaders(headers, reportingOrigin);
+  if (source && 'source_event_id' in source) {
+    // We add param indicating to stash the ID to be able to poll in
+    // `waitForSourceToBeRegistered` and know when a source has been processed.
+    url.searchParams.set("store-source-id", source.source_event_id);
+  }
 
   Object.entries(extraQueryParams)
       .forEach(([key, value]) => url.searchParams.set(key, value));
@@ -261,6 +255,14 @@ const registerAttributionSrc = async (t, {
   }
 };
 
+
+/**
+ * Generates a random pseudo-unique source event id.
+ */
+const generateSourceEventId = () => {
+  return `${Math.round(Math.random() * 10000000000000)}`;
+}
+
 /**
  * Delay method that waits for prescribed number of milliseconds.
  */
@@ -277,7 +279,28 @@ const pollAttributionReports = async (url, origin = location.origin, interval = 
     await delay(interval);
     return pollAttributionReports(url, origin, interval);
   }
-  return new Promise(resolve => resolve(payload));
+  return payload;
+};
+
+/**
+ * Waits for source `sourceId` to be done registering. Resolves when it is. If
+ * the source is not done registering after 2 seconds, it times out and throws
+ * an error.
+ */
+const waitForSourceToBeRegistered = async (
+  sourceId,
+  attempt = 0
+) => {
+  if (attempt > 20) {
+    throw new Error(`Timeout polling source ${sourceId} registration`);
+  }
+  const url = blankURL();
+  url.searchParams.set("check-source-id", sourceId);
+  const { status } = await fetch(url);
+  if (status === 404) {
+    await delay(100);
+    return waitForSourceToBeRegistered(sourceId, attempt + 1);
+  }
 };
 
 const pollEventLevelReports = (origin, interval) =>
