98d5cc1ac2eae6250abf0451c91af5a27e7f162a
Gavin Williams <gavinwill@chromium.org>
2023-04-04 16:12:49 +0000

    Shimless: Create skeleton diagnostic page
    This is the base page for the new diagnostic page. Future CLs will add
    service calls to gather and display system data.
    
    Bug: b:274628068
    Tests: browser_tests --gtest_filter=ShimlessRmaAppDiagnosticPageTest*
    Change-Id: I25eb9fd1763d935ffeb341fd371fb8f35e2fe885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395474
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126027}

--- ash/webui/shimless_rma/shimless_rma.cc
+++ ash/webui/shimless_rma/shimless_rma.cc
@@ -373,6 +373,8 @@ void AddFeatureFlags(content::WebUIDataSource* html_source) {
   html_source->AddBoolean(
       "osUpdateEnabled",
       base::FeatureList::IsEnabled(features::kShimlessRMAOsUpdate));
+  html_source->AddBoolean("diagnosticPageEnabled",
+                          features::IsShimlessRMADiagnosticPageEnabled());
 }
 
 }  // namespace
