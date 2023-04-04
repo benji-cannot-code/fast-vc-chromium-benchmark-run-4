82746a8b881fb96ac84e5b5d5c9261908d311f3f
Kent Tamura <tkent@chromium.org>
2023-04-04 03:42:35 +0000

    [RemoveLegacy] LayoutSVGInlineText should support storing NGInlineItems
    This CL has no user-visible behavior changes.
    
    Bug: 1229581
    Change-Id: I69ffbe7e4c017c77aefb70f5c210b455e919acfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394865
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125792}

--- third_party/blink/renderer/core/layout/svg/layout_svg_inline_text.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_inline_text.h
@@ -23,11 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_SVG_LAYOUT_SVG_INLINE_TEXT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_SVG_LAYOUT_SVG_INLINE_TEXT_H_
 
-#include "third_party/blink/renderer/core/layout/layout_text.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/layout_ng_text.h"
 
 namespace blink {
 
-class LayoutSVGInlineText final : public LayoutText {
+class LayoutSVGInlineText final : public LayoutNGText {
  public:
   LayoutSVGInlineText(Node*, String);
 
