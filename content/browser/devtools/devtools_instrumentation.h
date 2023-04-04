dcfe056bd6d33e132e4e6b8db0dee694509dcd30
Randolf Jung <jrandolf@chromium.org>
2023-04-04 20:31:03 +0000

    Implement `DragEnded`
    DragEnded is used to inform CDP if dragging ends. This lets CDP consolidate its state with the actual drag state.
    
    See bit.ly/3ZqYvgd for more information.
    
    Bug: 850071
    Change-Id: I71146fce5ea3154352ef8838fd3c11bb12777bdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4397971
    Commit-Queue: Randolf Jung <jrandolf@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126201}

--- content/browser/devtools/devtools_instrumentation.h
+++ content/browser/devtools/devtools_instrumentation.h
@@ -73,11 +73,9 @@ class WebContents;
 
 struct SignedExchangeError;
 
-namespace protocol {
-namespace Audits {
+namespace protocol::Audits {
 class InspectorIssue;
-}  // namespace Audits
-}  // namespace protocol
+}  // namespace protocol::Audits
 
 namespace devtools_instrumentation {
 
@@ -279,6 +277,8 @@ void WillStartDragging(FrameTreeNode* main_frame_tree_node,
                        const blink::mojom::DragDataPtr drag_data,
                        blink::DragOperationsMask drag_operations_mask,
                        bool* intercepted);
+
+void DragEnded(FrameTreeNode& node);
 
 // Asks any interested agents to handle the given certificate error. Returns
 // |true| if the error was handled, |false| otherwise.
--- content/browser/devtools/protocol/input_handler.cc
+++ content/browser/devtools/protocol/input_handler.cc
@@ -1023,6 +1023,8 @@ void InputHandler::StartDragging(const blink::mojom::DragData& drag_data,
                                  .Build());
 }
 
+void InputHandler::DragEnded() {}
+
 Response InputHandler::SetInterceptDrags(bool enabled) {
   intercept_drags_ = enabled;
   return Response::Success();
