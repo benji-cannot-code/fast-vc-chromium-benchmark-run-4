90b951be1b24f3687970302c5c28349bb08a7d70
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-04-04 21:02:51 +0000

    Add support for "dropShadow" in CanvasFilter.
    This is based on the SVG feDropShadow specification:
    https://drafts.fxtf.org/filter-effects/#feDropShadowElement
    
    There are a few known limitations right now:
     - SVG's feDropShadow supports different blur values for the X and
       the Y directions. We can't easily support this with CanvasFilter
       right now because it's implemented by leveraging the CSS code
       and CSS doesn't support different X/Y blur values.
    
     - For now, `currentColor` and `color-scheme` are not supported.
       These depend on the DOM element computed style, which is not
       available when we do `new CanvasFilter(...)`. To support
       `CurrentColor` and `color-schemes`, we would need maintain
       unresolved colors in the CanvasFilter object and resolve these
       colors only when the filter is assigned to a canvas element
       context. This is possible, but it's better if we only add this
       complexity if it's really needed.
    
    Change-Id: I402c8a43c9e227ac3e5f363aee1946619a408716
    Bug: 1428652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386673
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126225}

--- third_party/blink/renderer/core/style/shadow_data.h
+++ third_party/blink/renderer/core/style/shadow_data.h
@@ -46,12 +46,14 @@ class CORE_EXPORT ShadowData {
              float blur,
              float spread,
              ShadowStyle style,
-             StyleColor color)
+             StyleColor color,
+             float opacity = 1.0f)
       : location_(location),
         blur_(blur),
         spread_(spread),
         color_(color),
-        style_(style) {}
+        style_(style),
+        opacity_(opacity) {}
 
   bool operator==(const ShadowData&) const;
   bool operator!=(const ShadowData& o) const { return !(*this == o); }
@@ -65,6 +67,7 @@ class CORE_EXPORT ShadowData {
   float Spread() const { return spread_; }
   ShadowStyle Style() const { return style_; }
   StyleColor GetColor() const { return color_; }
+  float Opacity() const { return opacity_; }
 
   void OverrideColor(Color color) { color_ = StyleColor(color); }
 
@@ -78,6 +81,7 @@ class CORE_EXPORT ShadowData {
   float spread_;
   StyleColor color_;
   ShadowStyle style_;
+  float opacity_;
 };
 
 }  // namespace blink
