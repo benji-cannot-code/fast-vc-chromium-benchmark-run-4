6060fa9b673a59afedf926db6b1297a0d7a921d1
Fredrik Söderquist <fs@opera.com>
2023-04-04 17:02:53 +0000

    Use the initial style for resolving if nothing better is found
    We need a ComputedStyle to be able to resolve from a SVGLength (really:
    a CSSPrimitiveValue) to a Length. Previously[1] we could resolve
    absolute units since that did require any (style) context. By providing
    the initial computed style in the cases where we fail to find one by
    other means, we will manage to resolve a value. This will also mean that
    certain units, like font-relative ones, will start resolving based on
    the initial computed style.
    
    In the specific case we would reject the filter as invalid because the
    filter region would resolve to an empty rectangle. This happens to work
    for HTML content, because there we ignore this condition.
    
    [1] Before 25aa3b1a70b4c45070f70e5e2ea0e26e01697c49
    
    Bug: 1429510
    Change-Id: I9bb755ede064aa902ff7334356cc6e0660d25474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394132
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126063}

--- third_party/blink/renderer/core/svg/svg_length_context.cc
+++ third_party/blink/renderer/core/svg/svg_length_context.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/css_primitive_value.h"
 #include "third_party/blink/renderer/core/css/css_resolution_units.h"
+#include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
@@ -41,24 +42,24 @@ namespace blink {
 
 namespace {
 
-const ComputedStyle* ComputedStyleForLengthResolving(
+const ComputedStyle& ComputedStyleForLengthResolving(
     const SVGElement& context) {
   const ContainerNode* current_context = &context;
   do {
     if (current_context->GetLayoutObject()) {
-      return current_context->GetLayoutObject()->Style();
+      return current_context->GetLayoutObject()->StyleRef();
     }
     current_context = current_context->parentNode();
   } while (current_context);
 
   // We can end up here if trying to resolve values for elements in an
   // inactive document.
-  return nullptr;
+  return context.GetDocument().GetStyleResolver().InitialStyle();
 }
 
 const ComputedStyle* ComputedStyleForLengthResolving(
     const SVGElement* context) {
-  return context ? ComputedStyleForLengthResolving(*context) : nullptr;
+  return context ? &ComputedStyleForLengthResolving(*context) : nullptr;
 }
 
 const ComputedStyle* RootElementStyle(const Element& element) {
@@ -263,11 +264,8 @@ double SVGLengthContext::ConvertValueToUserUnitsUnclamped(
   }
   // For remaining units, just instantiate a CSSToLengthConversionData object
   // and use that for resolving.
-  const ComputedStyle* style = ComputedStyleForLengthResolving(*context_);
-  if (!style) {
-    return 0;
-  }
-  const SVGLengthConversionData conversion_data(*context_, *style);
+  const ComputedStyle& style = ComputedStyleForLengthResolving(*context_);
+  const SVGLengthConversionData conversion_data(*context_, style);
   return conversion_data.ZoomedComputedPixels(value, from_unit);
 }
 
@@ -323,11 +321,8 @@ float SVGLengthContext::ConvertValueFromUserUnits(
   }
   // For remaining units, just instantiate a CSSToLengthConversionData object
   // and use that for resolving.
-  const ComputedStyle* style = ComputedStyleForLengthResolving(*context_);
-  if (!style) {
-    return 0;
-  }
-  const SVGLengthConversionData conversion_data(*context_, *style);
+  const ComputedStyle& style = ComputedStyleForLengthResolving(*context_);
+  const SVGLengthConversionData conversion_data(*context_, style);
   const double reference = conversion_data.ZoomedComputedPixels(1, to_unit);
   if (!reference) {
     return 0;
