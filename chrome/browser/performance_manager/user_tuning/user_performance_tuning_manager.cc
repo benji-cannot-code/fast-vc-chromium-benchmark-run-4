0c1aaaf9e4e477eca9d768a412b13104fc7c4df0
Joe Mason <joenotcharles@google.com>
2023-04-04 20:43:44 +0000

    Switch PageDiscardingHelper to get background feature use from SiteData
    PageDiscardingHelper now finds a SiteDataReader for the PageNode to
    check if the page title or favicon were updated in the background.
    
    Also includes some refactoring for the browser test:
    
    * Adds a SiteDataRecorderHeuristics class for SiteDataRecorder that
      can be mocked in tests. SiteDataRecorder checks some heuristics,
      such as the time since a page was moved to the background, to
      decide whether to record background title and favicon updates or
      ignore them. This moves all these heuristics to a policy class that
      can be overridden in tests because some of them are hard to satisfy
      in browser tests.
    * Adds some more strict state checking so that
      SiteDataRecorder::OnLoadingStateChanged() only updates the internal
      state when the loading state actually changed. The code happened to
      work with the default heuristics because it was never called with
      two states where IsLoadedIdle() returned true in a row.
    * Splits the PageDiscardingHelper browser test into a separate test
      for each feature, since the browser must now be restarted to clear
      the SiteData DB between each test.
    
    And some misc cleanups:
    
    * Remove GetPageNodeLiveStateData which was never used.
    * Moves all checks in CanDiscardResult() that don't need
      PageLiveState data or a SiteDataReader to the top of the function.
    * Fixes an IWYU violation in user_performance_tuning_manager.cc.
    * Moves private SiteDataRecorder constants into the anonymous
      namespace.
    
    R=anthonyvd
    
    Bug: 1426484
    Change-Id: I6a90bd81d7421bd50fe5269e254fc97fe9dec93f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381526
    Reviewed-by: Sébastien Marchand <sebmarchand@google.com>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126210}

--- chrome/browser/performance_manager/user_tuning/user_performance_tuning_manager.cc
+++ chrome/browser/performance_manager/user_tuning/user_performance_tuning_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/power_monitor/power_monitor.h"
+#include "base/run_loop.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/browser/performance_manager/metrics/page_timeline_monitor.h"
