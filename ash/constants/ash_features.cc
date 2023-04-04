23996817f720af32012a4f97309a702c552c45c6
Matthew Mourgos <mmourgos@chromium.org>
2023-04-04 20:05:56 +0000

    Add quick app barebones UI implementation
    Add quick app button to be shown next to the home button. Quick app can
    be set by passing an app_id to the app list controller function
    SetHomeButtonQuickApp(). The home button will observe the new
    QuickAppAccessModel for the quick app to be changed and will then show
    the quick app button.
    
    Animations, icon loading, and additional metric to be added in follow
    ups.
    
    Bug=b:266734005
    
    Change-Id: I2840998e5860ab33a1607022417a55277e06d660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4380277
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126186}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1173,6 +1173,10 @@ BASE_FEATURE(kHoldingSpaceTour,
              "HoldingSpaceTour",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kHomeButtonQuickAppAccess,
+             "HomeButtonQuickAppAccess",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables a call-to-action label beside the home button.
 BASE_FEATURE(kHomeButtonWithText,
              "HomeButtonWithText",
@@ -2776,6 +2780,10 @@ bool IsHoldingSpaceSuggestionsEnabled() {
 
 bool IsHoldingSpaceTourEnabled() {
   return base::FeatureList::IsEnabled(kHoldingSpaceTour);
+}
+
+bool IsHomeButtonQuickAppAccessEnabled() {
+  return base::FeatureList::IsEnabled(kHomeButtonQuickAppAccess);
 }
 
 bool IsHomeButtonWithTextEnabled() {
