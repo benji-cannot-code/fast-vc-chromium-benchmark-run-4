3202979002544ef9ae18589dfe45ef04e5f01496
Jun Zou <junzou@chromium.org>
2023-04-04 08:41:44 +0000

    Implement a browser API for text search.
    - Adds a `TextFinder` class to perform text search via `AnnotationAgent` in the renderer process.
    - `TextFinderManager` manages `TextFinder`s.
    
    Related Docs:
    Text Detection API
    https://docs.google.com/document/d/1VFUPQG7CFOtQyV8UN9xesokCcCIcuMBGLVpAOOHWc_0/edit?resourcekey=0-FDzb_dJ4ip5VyyCX81lswg#heading=h.mbf72psa962w
    
    Bug: b/273307525
    Change-Id: I8b48b730c00afb3c675fba699f2e39da7a96ef06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370994
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Jun Zou <junzou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125862}

---
+++ chrome/browser/companion/text_finder/text_finder_unittest.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/companion/text_finder/text_finder.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/annotation/annotation.mojom.h"
+#include "ui/gfx/geometry/rect.h"
+
+namespace companion {
+
+class TextFinderTest : public testing::Test {
+ public:
+  TextFinderTest() = default;
+  ~TextFinderTest() override = default;
+
+  void SetFindingState(bool found) { is_found_ = found; }
+
+ protected:
+  bool is_found_ = false;
+};
+
+TEST_F(TextFinderTest, FoundTextTest) {
+  gfx::Rect rect(2, 4);
+  TextFinder finder("abc,def");
+  finder.SetDidFinishHandler(
+      base::BindOnce(&TextFinderTest::SetFindingState, base::Unretained(this)));
+  finder.DidFinishAttachment(rect);
+  EXPECT_TRUE(is_found_);
+}
+
+TEST_F(TextFinderTest, NotFoundTextTest) {
+  // Empty
+  gfx::Rect rect;
+  TextFinder finder("abc,def");
+  finder.SetDidFinishHandler(
+      base::BindOnce(&TextFinderTest::SetFindingState, base::Unretained(this)));
+  finder.DidFinishAttachment(rect);
+  EXPECT_FALSE(is_found_);
+}
+
+}  // namespace companion
