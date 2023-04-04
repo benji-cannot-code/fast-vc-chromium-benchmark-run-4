d5214d21569183d95bff508e2882ddf22f00cd9b
Claudio DeSouza <cdesouza@igalia.com>
2023-04-04 08:38:24 +0000

    Replace base::GUID uses with base::Uuid (5/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/autofill/core/browser/webdata
    
    This CL was uploaded by git cl split.
    
    R=fleimgruber@google.com, mastiz@chromium.org
    
    Bug: 1428566
    Change-Id: I4668a27cca62909425d675a4e46290cd38d0e58c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396344
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1125857}

--- components/autofill/core/browser/webdata/autofill_table.cc
+++ components/autofill/core/browser/webdata/autofill_table.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/containers/contains.h"
-#include "base/guid.h"
 #include "base/i18n/case_conversion.h"
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
@@ -29,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "base/uuid.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/data_model/autofill_metadata.h"
 #include "components/autofill/core/browser/data_model/autofill_offer_data.h"
@@ -565,7 +565,7 @@ std::u16string Truncate(const std::u16string& data) {
 void BindAutofillProfileToStatement(const AutofillProfile& profile,
                                     const base::Time& modification_date,
                                     sql::Statement* s) {
-  DCHECK(base::IsValidGUID(profile.guid()));
+  DCHECK(base::IsValidUuid(profile.guid()));
   int index = 0;
   s->BindString(index++, profile.guid());
 
@@ -614,7 +614,7 @@ void BindCreditCardToStatement(const CreditCard& credit_card,
                                const base::Time& modification_date,
                                sql::Statement* s,
                                const AutofillTableEncryptor& encryptor) {
-  DCHECK(base::IsValidGUID(credit_card.guid()));
+  DCHECK(base::IsValidUuid(credit_card.guid()));
   int index = 0;
   s->BindString(index++, credit_card.guid());
 
@@ -636,7 +636,7 @@ void BindCreditCardToStatement(const CreditCard& credit_card,
 void BindIBANToStatement(const IBAN& iban,
                          sql::Statement* s,
                          const AutofillTableEncryptor& encryptor) {
-  DCHECK(base::IsValidGUID(iban.guid()));
+  DCHECK(base::IsValidUuid(iban.guid()));
   int index = 0;
   s->BindString(index++, iban.guid());
 
@@ -690,7 +690,7 @@ std::unique_ptr<CreditCard> CreditCardFromStatement(
 
   int index = 0;
   credit_card->set_guid(s.ColumnString(index++));
-  DCHECK(base::IsValidGUID(credit_card->guid()));
+  DCHECK(base::IsValidUuid(credit_card->guid()));
 
   for (ServerFieldType type : {CREDIT_CARD_NAME_FULL, CREDIT_CARD_EXP_MONTH,
                                CREDIT_CARD_EXP_4_DIGIT_YEAR}) {
@@ -715,7 +715,7 @@ std::unique_ptr<IBAN> IBANFromStatement(
 
   int index = 0;
   iban->set_guid(s.ColumnString(index++));
-  DCHECK(base::IsValidGUID(iban->guid()));
+  DCHECK(base::IsValidUuid(iban->guid()));
   iban->set_use_count(s.ColumnInt64(index++));
   iban->set_use_date(base::Time::FromTimeT(s.ColumnInt64(index++)));
 
@@ -1651,7 +1651,7 @@ bool AutofillTable::AddAutofillProfile(const AutofillProfile& profile) {
 }
 
 bool AutofillTable::UpdateAutofillProfile(const AutofillProfile& profile) {
-  DCHECK(base::IsValidGUID(profile.guid()));
+  DCHECK(base::IsValidUuid(profile.guid()));
 
   std::unique_ptr<AutofillProfile> old_profile =
       GetAutofillProfile(profile.guid(), profile.source());
@@ -1702,7 +1702,7 @@ bool AutofillTable::UpdateAutofillProfile(const AutofillProfile& profile) {
 bool AutofillTable::RemoveAutofillProfile(
     const std::string& guid,
     AutofillProfile::Source profile_source) {
-  DCHECK(base::IsValidGUID(guid));
+  DCHECK(base::IsValidUuid(guid));
   if (profile_source == AutofillProfile::Source::kAccount) {
     sql::Transaction transaction(db_);
     return transaction.Begin() &&
@@ -1726,7 +1726,7 @@ bool AutofillTable::RemoveAllAutofillProfiles(
 std::unique_ptr<AutofillProfile> AutofillTable::GetAutofillProfile(
     const std::string& guid,
     AutofillProfile::Source profile_source) {
-  DCHECK(base::IsValidGUID(guid));
+  DCHECK(base::IsValidUuid(guid));
   if (profile_source == AutofillProfile::Source::kAccount)
     return GetAutofillProfileFromContactInfoTable(db_, guid);
 
@@ -1744,7 +1744,7 @@ std::unique_ptr<AutofillProfile> AutofillTable::GetAutofillProfile(
   auto profile = std::make_unique<AutofillProfile>(
       guid, /*origin=*/s.ColumnString(0),
       AutofillProfile::Source::kLocalOrSyncable);
-  DCHECK(base::IsValidGUID(profile->guid()));
+  DCHECK(base::IsValidUuid(profile->guid()));
 
   // Get associated name info using guid.
   AddAutofillProfileNamesToProfile(db_, profile.get());
@@ -1938,7 +1938,7 @@ bool AutofillTable::AddIBAN(const IBAN& iban) {
 }
 
 bool AutofillTable::UpdateIBAN(const IBAN& iban) {
-  DCHECK(base::IsValidGUID(iban.guid()));
+  DCHECK(base::IsValidUuid(iban.guid()));
 
   std::unique_ptr<IBAN> old_iban = GetIBAN(iban.guid());
   if (!old_iban) {
@@ -1960,12 +1960,12 @@ bool AutofillTable::UpdateIBAN(const IBAN& iban) {
 }
 
 bool AutofillTable::RemoveIBAN(const std::string& guid) {
-  DCHECK(base::IsValidGUID(guid));
+  DCHECK(base::IsValidUuid(guid));
   return DeleteWhereColumnEq(db_, kIBANsTable, kGuid, guid);
 }
 
 std::unique_ptr<IBAN> AutofillTable::GetIBAN(const std::string& guid) {
-  DCHECK(base::IsValidGUID(guid));
+  DCHECK(base::IsValidUuid(guid));
   sql::Statement s;
   SelectBuilder(db_, s, kIBANsTable,
                 {kGuid, kUseCount, kUseDate, kValue, kNickname},
@@ -2013,7 +2013,7 @@ bool AutofillTable::AddCreditCard(const CreditCard& credit_card) {
 }
 
 bool AutofillTable::UpdateCreditCard(const CreditCard& credit_card) {
-  DCHECK(base::IsValidGUID(credit_card.guid()));
+  DCHECK(base::IsValidUuid(credit_card.guid()));
 
   std::unique_ptr<CreditCard> old_credit_card =
       GetCreditCard(credit_card.guid());
@@ -2040,7 +2040,7 @@ bool AutofillTable::UpdateCreditCard(const CreditCard& credit_card) {
 }
 
 bool AutofillTable::RemoveCreditCard(const std::string& guid) {
-  DCHECK(base::IsValidGUID(guid));
+  DCHECK(base::IsValidUuid(guid));
   return DeleteWhereColumnEq(db_, kCreditCardsTable, kGuid, guid);
 }
 
@@ -2073,7 +2073,7 @@ bool AutofillTable::AddFullServerCreditCard(const CreditCard& credit_card) {
 
 std::unique_ptr<CreditCard> AutofillTable::GetCreditCard(
     const std::string& guid) {
-  DCHECK(base::IsValidGUID(guid));
+  DCHECK(base::IsValidUuid(guid));
   sql::Statement s;
   SelectBuilder(db_, s, kCreditCardsTable,
                 {kGuid, kNameOnCard, kExpirationMonth, kExpirationYear,
--- components/autofill/core/browser/webdata/contact_info_sync_bridge.cc
+++ components/autofill/core/browser/webdata/contact_info_sync_bridge.cc
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/contact_info_sync_bridge.h"
 
 #include "base/check.h"
-#include "base/guid.h"
 #include "base/ranges/algorithm.h"
+#include "base/uuid.h"
 #include "components/autofill/core/browser/contact_info_sync_util.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/sync/base/features.h"
