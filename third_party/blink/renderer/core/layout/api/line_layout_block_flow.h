f548bccf08a6f448efab328e276f8d621d8268a0
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-04-04 05:12:38 +0000

    [RemoveLegacy] Remove LayoutBLock::has_markup_truncation_
    Bug: 1229581
    Change-Id: Ib015e18f1c8644e1efe6020d52a8f644c85fa467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396462
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125815}

--- third_party/blink/renderer/core/layout/api/line_layout_block_flow.h
+++ third_party/blink/renderer/core/layout/api/line_layout_block_flow.h
@@ -127,10 +127,6 @@ class LineLayoutBlockFlow : public LineLayoutBox {
                                                    logical_height);
   }
 
-  void SetHasMarkupTruncation(bool b) {
-    ToBlockFlow()->SetHasMarkupTruncation(b);
-  }
-
   LayoutUnit LogicalWidth() { return ToBlockFlow()->LogicalWidth(); }
 
   LineBoxList* LineBoxes() { return ToBlockFlow()->LineBoxes(); }
