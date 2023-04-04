d31a5fc14bf45d9a40546a20510b4baf8d46e7a9
Claudio DeSouza <cdesouza@igalia.com>
2023-04-04 06:57:54 +0000

    Replace base::GUID uses with base::Uuid (47/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/web_package
    
    This CL was uploaded by git cl split.
    
    R=cmfcmf@chromium.org
    
    Bug: 1428566
    Change-Id: I671acf996f98dd43262d004624839769f381b5df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394534
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125835}

--- components/web_package/web_bundle_utils.cc
+++ components/web_package/web_bundle_utils.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/web_package/web_bundle_utils.h"
 
-#include "base/guid.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "base/uuid.h"
 #include "components/web_package/mojom/web_bundle_parser.mojom.h"
 #include "net/http/http_status_code.h"
 #include "net/http/http_util.h"
@@ -71,7 +71,7 @@ bool IsValidUuidInPackageURL(const GURL& url) {
   std::string spec = url.spec();
   return base::StartsWith(
              spec, "uuid-in-package:", base::CompareCase::INSENSITIVE_ASCII) &&
-         base::GUID::ParseCaseInsensitive(base::StringPiece(spec).substr(16))
+         base::Uuid::ParseCaseInsensitive(base::StringPiece(spec).substr(16))
              .is_valid();
 }
 
