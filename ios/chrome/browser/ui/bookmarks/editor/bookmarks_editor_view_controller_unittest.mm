ba0633cdb2234e63a454faf3a422cd8f1b25ac92
Arthur Milchior <arthurmilchior@chromium.org>
2023-04-04 14:48:58 +0000

    [iOS] Bookmark View Controller can sync before being loaded
    This change ensure that, if the model is not yet loaded, sync message
    is just ignored, and does not lead to a crash.
    
    The test crashes without this code change.
    
    Fixed: 1429435
    Change-Id: Ia199a7b80cd4541972c68a9c1ca14b7fb3821fe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396354
    Reviewed-by: Petro Akzhygitov <pakzhygitov@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125973}

--- ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_view_controller_unittest.mm
+++ ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_view_controller_unittest.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_view_controller.h"
+#import "ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_mediator.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -46,6 +47,24 @@ TEST_F(BookmarksEditorViewControllerTest, Metrics) {
   [controller keyCommand_close];
 
   EXPECT_EQ(user_action_tester.GetActionCount(user_action), 1);
+}
+
+// Regression test for See crbug.com/1429435
+// Checks sync can safely occurs before the view is loaded.
+TEST_F(BookmarksEditorViewControllerTest, CanSyncBeforeLoad) {
+  BookmarksEditorViewController* controller =
+      [[BookmarksEditorViewController alloc] initWithBrowser:browser_.get()];
+
+  const bookmarks::BookmarkNode* mobile_node = bookmark_model_->mobile_node();
+  const bookmarks::BookmarkNode* bookmark = AddBookmark(mobile_node, @"foo");
+  BookmarksEditorMediator* mediator =
+      [[BookmarksEditorMediator alloc] initWithBookmarkModel:bookmark_model_
+                                                    bookmark:bookmark
+                                                       prefs:nullptr
+                                            syncSetupService:nullptr
+                                                 syncService:nullptr];
+  controller.mutator = mediator;
+  [controller updateSync];
 }
 
 }  // namespace
