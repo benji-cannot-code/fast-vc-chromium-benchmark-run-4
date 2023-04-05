41dc3b561f35cfaea098141cddb15a3f63f2b33e
Claudio DeSouza <cdesouza@igalia.com>
2023-04-05 17:50:14 +0000

    Replace base::GUID uses with base::Uuid (37/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/services/quarantine
    
    This CL was uploaded by git cl split.
    
    R=wfh@chromium.org
    
    Bug: 1428566
    Change-Id: I6540d76f03e791a38b85a58e3e409f831cb788e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395789
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126682}

--- components/services/quarantine/quarantine_win.cc
+++ components/services/quarantine/quarantine_win.cc
@@ -15,16 +15,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <shobjidl.h>
 #include <wininet.h>
 
-
 #include "base/check_op.h"
 #include "base/feature_list.h"
 #include "base/files/file_util.h"
-#include "base/guid.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/scoped_blocking_call.h"
+#include "base/uuid.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/win_util.h"
 #include "components/services/quarantine/common.h"
@@ -239,7 +238,7 @@ void QuarantineFile(const base::FilePath& file,
 
   std::string braces_guid = "{" + client_guid + "}";
   GUID guid = GUID_NULL;
-  if (base::IsValidGUID(client_guid)) {
+  if (base::Uuid::ParseCaseInsensitive(client_guid).is_valid()) {
     HRESULT hr = CLSIDFromString(base::UTF8ToWide(braces_guid).c_str(), &guid);
     if (FAILED(hr))
       guid = GUID_NULL;
