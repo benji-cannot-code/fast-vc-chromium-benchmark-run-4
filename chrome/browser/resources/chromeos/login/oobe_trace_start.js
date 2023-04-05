75c744362f140eb1eca9e057c294343111fdad39
Renato Silva <rrsilva@google.com>
2023-04-05 00:20:44 +0000

    [OOBE] Use the Performance API timestamps
    Use the Performance API in oobe_trace.js. Using this API removes the
    requirement of always needing to have an initial timestamp stored in
    the global object.
    
    Fixed: b:274395122
    Bug: b:266939445
    
    Change-Id: Iaf009c1406ba206dcba2e776b52b6221b4cab528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373952
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126339}

--- chrome/browser/resources/chromeos/login/oobe_trace_start.js
+++ chrome/browser/resources/chromeos/login/oobe_trace_start.js
@@ -10,5 +10,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * that gets executed when OOBE starts.
  */
 
-const oobeInitializationBeginTimestamp = new Date();
+const oobeInitializationBeginTimestamp = performance.now();
 window.oobeInitializationBeginTimestamp = oobeInitializationBeginTimestamp;
