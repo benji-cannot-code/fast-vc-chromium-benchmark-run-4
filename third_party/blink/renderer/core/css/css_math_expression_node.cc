3bd0cc6fb0ca4672f07d434c4d7bf1240b0409c2
Xiaocheng Hu <xiaochengh@chromium.org>
2023-04-05 22:26:23 +0000

    Revert "Nested calc() expressions need to consume all their tokens"
    This reverts commit 5a426f9babceea8ffa93d870690634371a79457b.
    
    Reason for revert: Caused crbug.com/1430953
    
    Original change's description:
    > Nested calc() expressions need to consume all their tokens
    >
    > When parsing a nested calc() expression that had a valid first part
    > (term) that was followed by an invalid operator the parsed part was
    > returned to the caller - which had already consumed the tokens within
    > the sub-expression. The caller only checked that an expression node was
    > returned, which meant that the sub-expression was considered valid.
    >
    > Require that all tokens that are part of the sub-expression have been
    > consumed after parsing has completed to fail the parsing in such a case.
    >
    > Fixed: 1430413
    > Change-Id: I3072e22e2a7b81296510173266814a18fb6bfc37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400956
    > Auto-Submit: Fredrik Söderquist <fs@opera.com>
    > Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1126635}
    
    Bug: 1430953,1430413
    Change-Id: I866b130615c4328bd4b99716e74e3a045e0f348c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404759
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1126878}

--- third_party/blink/renderer/core/css/css_math_expression_node.cc
+++ third_party/blink/renderer/core/css/css_math_expression_node.cc
@@ -1794,7 +1794,7 @@ class CSSMathExpressionNodeParser {
       tokens.ConsumeWhitespace();
       inner_range.ConsumeWhitespace();
       CSSMathExpressionNode* result = ParseValueExpression(inner_range, depth);
-      if (!result || !inner_range.AtEnd()) {
+      if (!result) {
         return nullptr;
       }
       result->SetIsNestedCalc();
