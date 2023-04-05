52e30a7d71a2ee5c127a2320925629899f4b5515
Florian Leimgruber <fleimgruber@google.com>
2023-04-05 08:38:05 +0000

    Add AddressProfileSaveManagerTest for profile migration.
    Adapts the AddressProfileSaveManagerTest, so migration can be tested.
    Right now the expected_final_profiles are compared by GUID + content.
    This doesn't work for migrations, since profile changes their GUID after
    migration. This makes it difficult to specify the correct target GUID
    beforehand.
    
    This CL changes to test logic, so profiles are only compared by content
    in the migration case.
    
    Bug: 1423319
    Change-Id: I0f1b5ea3efa11ca3a66ad86c0e51b350e710e9db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373987
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126443}

--- components/autofill/core/browser/autofill_profile_import_process.cc
+++ components/autofill/core/browser/autofill_profile_import_process.cc
@@ -500,9 +500,7 @@ void ProfileImportProcess::CollectMetrics(ukm::UkmRecorder* ukm_recorder,
         import_metadata_.num_autocomplete_unrecognized_fields);
 
     LogUkmMetrics(num_edited_fields);
-  } else if (import_type_ == AutofillProfileImportType::kConfirmableMerge ||
-             import_type_ ==
-                 AutofillProfileImportType::kConfirmableMergeAndSilentUpdate) {
+  } else if (is_confirmable_update()) {
     autofill_metrics::LogProfileUpdateImportDecision(user_decision_);
     autofill_metrics::LogProfileUpdateNumberOfAutocompleteUnrecognizedFields(
         import_metadata_.num_autocomplete_unrecognized_fields);
@@ -525,6 +523,9 @@ void ProfileImportProcess::CollectMetrics(ukm::UkmRecorder* ukm_recorder,
     LogUkmMetrics(num_edited_fields);
   } else if (import_type_ == AutofillProfileImportType::kSilentUpdate) {
     LogUkmMetrics();
+  } else if (is_migration()) {
+    // TODO(crbug.com/1423319): Log migration metrics.
+    LogUkmMetrics(num_edited_fields);
   }
 }
 
