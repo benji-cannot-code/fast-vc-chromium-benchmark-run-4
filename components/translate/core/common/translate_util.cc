b1ea904fa6721840d8448f2348d7b2d566356dd1
Olivier ROBIN <olivierrobin@google.com>
2023-04-05 13:06:45 +0000

    Enable IOSForceTranslate by default
    Launch has been approved 100%
    
    Change-Id: I504a198ca20ff278a3d7d9b056a5972a9cc8ee4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396356
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126510}

--- components/translate/core/common/translate_util.cc
+++ components/translate/core/common/translate_util.cc
@@ -47,7 +47,7 @@ BASE_FEATURE(kTFLiteLanguageDetectionIgnoreEnabled,
 
 BASE_FEATURE(kIOSForceTranslateEnabled,
              "IOSForceTranslateEnabled",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDesktopPartialTranslate,
              "DesktopPartialTranslate",
