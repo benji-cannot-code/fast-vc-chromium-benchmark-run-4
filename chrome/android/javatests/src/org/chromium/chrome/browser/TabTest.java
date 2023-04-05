a49f101e02bf4091aa0e54dbd151183cc2decc18
Calder Kitagawa <ckitagawa@chromium.org>
2023-04-05 21:18:54 +0000

    Test Reviver: TabTest#testTabRestoredIfKilledWhileActivityStopped
    The old bug is from 2020 and long since closed. There is no stack-
    trace or evidence from the previous flakes. This appears to be
    passing on test-reviver reliably. This may have flaked on older
    Android versions or before improvements to our test harnesses.
    
    If it flakes again at least there will be an up-to-date stacktrace to
    try to understand and fix.
    
    Fixed: 1331627
    Change-Id: I63f289790dbfdc7fc8b17e54c157e750a0f7b91b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404436
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126830}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabTest.java
@@ -19,7 +19,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
@@ -103,7 +102,6 @@ public class TabTest {
     @Test
     @SmallTest
     @Feature({"Tab"})
-    @DisabledTest(message = "https://crbug.com/1090378")
     public void testTabRestoredIfKilledWhileActivityStopped() throws Exception {
         // Ensure the tab is showing before stopping the activity.
         TestThreadUtils.runOnUiThreadBlocking(
