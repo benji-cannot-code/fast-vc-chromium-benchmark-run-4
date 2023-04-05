b73578cd18467fa95b5a84c43b459afcae4f952e
Marco Monaco <marco.monaco@ocado.com>
2023-04-05 22:05:34 +0000

    gamepad: Fix devices with multiple input reports on Windows
    When handling input reports, the current code sets axis values and
    button states to zero when those axes or buttons are not present in that
    report (so their value is not known within that report's handling)
    This change stops zeroing axis and button state when the report doesn't
    contain the axis or button(s) in question
    
    R=mattreynolds@chromium.org
    
    Fixed: 1253821
    Change-Id: Ib8fad8f417526cfc0eb84f49743cd55ad35609c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395542
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126866}

--- device/gamepad/raw_input_gamepad_device_win.h
+++ device/gamepad/raw_input_gamepad_device_win.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "device/gamepad/abstract_haptic_gamepad.h"
 #include "device/gamepad/public/cpp/gamepad.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace device {
 
@@ -128,8 +129,8 @@ class RawInputGamepadDeviceWin final : public AbstractHapticGamepad {
   size_t buttons_length_ = 0;
   bool buttons_[Gamepad::kButtonsLengthCap];
 
-  // Keep track of which button indices are in use.
-  std::vector<bool> button_indices_used_;
+  // The report ID for each button index, or nullopt if the button is not used.
+  std::vector<absl::optional<uint8_t>> button_report_id_;
 
   // Bitfield to keep track of which axes indices are in use.
   uint32_t axes_used_ = 0;
