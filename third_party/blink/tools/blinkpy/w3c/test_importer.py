2309eb54b0aa6f734c0f39b0a2d70e296d601ea0
Jonathan Lee <jonathanjlee@google.com>
2023-04-05 20:02:16 +0000

    [blinkpy] Remove remaining references to WebLayer
    Also, remove `ImportNotifier.create_bugs_for_product(...)` and
    associated plumbing, which are unused. Without WebLayer, this method has
    no valid callers.
    
    Fixed: 1376617
    Change-Id: I8d5f630f9c840455279e0a016bf935beed5d3a39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404317
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126778}

--- third_party/blink/tools/blinkpy/w3c/test_importer.py
+++ third_party/blink/tools/blinkpy/w3c/test_importer.py
@@ -68,9 +68,6 @@ class TestImporter(object):
         # wpt_expectations_updater.SimpleTestResult.
         self.rebaselined_tests = set()
         self.new_test_expectations = {}
-        # New override expectations for Android targets. A dictionary mapping
-        # products to dictionary that maps test names to test expectation lines
-        self.new_override_expectations = {}
         self.verbose = False
 
         args = ['--clean-up-affected-tests-only',
@@ -715,7 +712,6 @@ class TestImporter(object):
             self.wpt_revision,
             self.rebaselined_tests,
             self.new_test_expectations,
-            self.new_override_expectations,
             issue,
             patchset,
             dry_run=not auto_file_bugs,
