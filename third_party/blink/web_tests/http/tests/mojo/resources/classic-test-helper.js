daf8048f1af25334d6a7c53d0f816e7444a8b927
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2023-04-05 01:18:43 +0000

    mojo-js: Share tests across all bindings versions
    We generate multiple versions of JS bindings, e.g. lite, modules, etc.,
    that need to work in various execution contexts, e.g. modules,
    service workers, as script tags, etc.
    
    bindings-lite.js, where most of our encoding/decoding tests
    are, is duplicated in bindings.html to test the `.m.js` version of the
    bindings. This sometimes causes us to add tests to bindings-lite.js
    but forget to add them to bindings.html.
    
    This change adds helper scripts that add the relevant
    classes/structs/enums to globalThis and rewrites the tests
    to use the classes/structs/enums in globalThis. This allows us to
    share the same code across all tests.
    
    Also:
     - Adds a README.md to explain all of this
     - Moves tests for lite bindings to lite/ folder.
     - Inlines versioning.js into versioning.html
    
    Bug: 657632
    Change-Id: I53ba7366045a83b1d64f94946c1d1a61f9ae41b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4393827
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126368}

---
+++ third_party/blink/web_tests/http/tests/mojo/resources/classic-test-helper.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Adds all used Mojo classes/structs/enums to globalThis. This allows the
+// bindings-tests.js script to be used for both various types of tests. See
+// README.md for more information.
+
+globalThis.TestMessageTargetReceiver = liteJsTest.mojom.TestMessageTargetReceiver;
+globalThis.TestMessageTarget_NestedEnum = liteJsTest.mojom.TestMessageTarget_NestedEnum;
+globalThis.TestMessageTarget = liteJsTest.mojom.TestMessageTarget;
+globalThis.TestMessageTargetCallbackRouter = liteJsTest.mojom.TestMessageTargetCallbackRouter;
+globalThis.SubinterfaceCallbackRouter = liteJsTest.mojom.SubinterfaceCallbackRouter;
+globalThis.SubinterfaceRemote = liteJsTest.mojom.SubinterfaceRemote;
+globalThis.SubinterfaceClientCallbackRouter = liteJsTest.mojom.SubinterfaceClientCallbackRouter;
+globalThis.MojoEchoRemote = content.mojom.MojoEchoRemote;
