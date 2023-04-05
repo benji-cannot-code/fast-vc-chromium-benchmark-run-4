04b21954fae9b62e53a7252aeacc8e5d36601882
Claudio DeSouza <cdesouza@igalia.com>
2023-04-05 14:53:39 +0000

    Replace base::GUID uses with base::Uuid (28/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/performance_manager
    
    This CL was uploaded by git cl split.
    
    R=fdoray@chromium.org
    
    Bug: 1428566
    Change-Id: Ibd651e9371a55c3d126047fa9c1adb9eab3d0b5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394848
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126572}

--- components/performance_manager/worker_watcher_unittest.cc
+++ components/performance_manager/worker_watcher_unittest.cc
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/observer_list.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/uuid.h"
 #include "components/performance_manager/frame_node_source.h"
 #include "components/performance_manager/graph/frame_node_impl.h"
 #include "components/performance_manager/graph/page_node_impl.h"
@@ -440,7 +440,9 @@ void TestServiceWorkerContext::StopServiceWorker(int64_t version_id) {
 std::string TestServiceWorkerContext::AddClient(
     int64_t version_id,
     const content::ServiceWorkerClientInfo& client_info) {
-  return AddClientWithClientID(version_id, base::GenerateGUID(), client_info);
+  return AddClientWithClientID(
+      version_id, base::Uuid::GenerateRandomV4().AsLowercaseString(),
+      client_info);
 }
 
 std::string TestServiceWorkerContext::AddClientWithClientID(
