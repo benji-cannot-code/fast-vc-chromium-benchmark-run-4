1e754eedb4b5d91632194535e8664c62bc62edb5
Sky Malice <skym@chromium.org>
2023-04-05 15:59:43 +0000

    CCT capture token includes width.
    Bug: 1428978
    Change-Id: I46d6e39f6fa54d8b7a1e154e8b70b3445617fbbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400925
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126606}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
@@ -752,7 +752,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
         String title = mLocationBar.mTitleBar.getText().toString();
         return new CustomTabCaptureStateToken(url, title, getBackground().getColor(),
                 mLocationBar.mAnimDelegate.getSecurityIconRes(),
-                mLocationBar.mAnimDelegate.isInAnimation());
+                mLocationBar.mAnimDelegate.isInAnimation(), getWidth());
     }
 
     /**
