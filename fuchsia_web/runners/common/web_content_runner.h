e548bb74c233e93d3efc6c85689f96aa80bb575c
Wez <wez@chromium.org>
2023-04-05 17:23:38 +0000

    [fuchsia] Remove fuchsia.sys.Runner implementation for web
    Remove the unused `fuchsia.sys.Runner` implementation from the
    `WebContentRunner`. This is a simple cleanup to expedite removal
    of the fuchsia.sys FIDL dependency; further cleanup/simplification
    is left as a follow-up.
    
    Bug: 1423889
    Change-Id: I01c2dc8d71bbcf00da6a9ef2f205cbb09524f3e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395797
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126658}

--- fuchsia_web/runners/common/web_content_runner.h
+++ fuchsia_web/runners/common/web_content_runner.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FUCHSIA_WEB_RUNNERS_COMMON_WEB_CONTENT_RUNNER_H_
 
 #include <fuchsia/io/cpp/fidl.h>
-#include <fuchsia/sys/cpp/fidl.h>
 #include <fuchsia/web/cpp/fidl.h>
 #include <lib/fidl/cpp/interface_ptr_set.h>
 
@@ -22,8 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class WebComponent;
 
-// sys::Runner that instantiates components hosting standard web content.
-class WebContentRunner : public fuchsia::sys::Runner {
+// Manages creation of a `fuchsia.web.Context` used to host content for one or
+// more `WebComponent`s.
+class WebContentRunner {
  public:
   struct WebInstanceConfig {
     WebInstanceConfig();
@@ -61,7 +61,7 @@ class WebContentRunner : public fuchsia::sys::Runner {
       CreateWebInstanceAndContextCallback create_web_instance_callback,
       WebInstanceConfig web_instance_config);
 
-  ~WebContentRunner() override;
+  ~WebContentRunner();
 
   WebContentRunner(const WebContentRunner&) = delete;
   WebContentRunner& operator=(const WebContentRunner&) = delete;
@@ -85,12 +85,6 @@ class WebContentRunner : public fuchsia::sys::Runner {
   // Used by WebComponent instances to signal that the ComponentController
   // channel was dropped, and therefore the component should be destroyed.
   void DestroyComponent(WebComponent* component);
-
-  // fuchsia::sys::Runner implementation.
-  void StartComponent(fuchsia::sys::Package package,
-                      fuchsia::sys::StartupInfo startup_info,
-                      fidl::InterfaceRequest<fuchsia::sys::ComponentController>
-                          controller_request) override;
 
   // Registers a WebComponent, or specialization, with this Runner.
   void RegisterComponent(std::unique_ptr<WebComponent> component);
