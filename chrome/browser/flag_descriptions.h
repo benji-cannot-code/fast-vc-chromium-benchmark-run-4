192aa14dd99cde2722304a7d9447e330f389fcfb
Maxim Kolosovskiy <kolos@chromium.org>
2023-04-05 10:30:19 +0000

    Revert "Second flag for diacritics"
    This reverts commit cf394a69de696f32663b16833c53939d51f6648f.
    
    Reason for revert: The CL likely caused test failure of inputs.PhysicalKeyboardLongpressDiacritics on the chromeos-betty-pi-arc-cfi-thin-lto-chrome builder.
    First failure https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-cfi-thin-lto-chrome/45506/overview
    
    Failure Reason 
    left_click: Subtest "left_click" failed: failed to PK longpress and left click to insert diacritics on step 4: context deadline exceeded during a poll with timeout 15s; last error follows: Uncaught (in promise): "failed to find node with properties: {className: /\\bSuggestionWindowView\\b/, role: window}"
    
    Original change's description:
    > Second flag for diacritics
    >
    > Add a new flag which allows us to separate the rollout of the feature into 2 phases
    >
    > 1) Feature is available for users
    > 2) Feature is on by default.
    >
    > This may simplify any future rollbacks etc.
    >
    > Fixed: b:274690585
    > Change-Id: I1bc8e4e9fd2e15acec83478062f8f34c7ad192a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369018
    > Commit-Queue: John Palmer <jopalmer@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1126301}
    
    Change-Id: I4184bd78c54655983316c9a375ad4b3f7691dfb7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4403055
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Owners-Override: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126472}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3341,9 +3341,6 @@ extern const char kHotspotDescription[];
 extern const char kDiacriticsOnPhysicalKeyboardLongpressName[];
 extern const char kDiacriticsOnPhysicalKeyboardLongpressDescription[];
 
-extern const char kDiacriticsOnPhysicalKeyboardLongpressDefaultOnName[];
-extern const char kDiacriticsOnPhysicalKeyboardLongpressDefaultOnDescription[];
-
 extern const char kHoldingSpacePredictabilityName[];
 extern const char kHoldingSpacePredictabilityDescription[];
 
