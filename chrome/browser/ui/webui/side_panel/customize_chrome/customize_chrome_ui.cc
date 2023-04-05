94131bc6d6a701a4f83a735cf0947d3835bb5099
John Lee <johntlee@chromium.org>
2023-04-05 23:04:12 +0000

    CR2023: Add chrome-refresh-2023 attribute to Customize Chrome side panel
    This will allow Customize Chrome side panel to get the new Chrome
    Refresh 2023 styles for the shared elements.
    
    Bug: 1430283
    Change-Id: I8ecc237a2ad9e17a8db6d83aff802bc5d7e501f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402404
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126895}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/base/webui/web_ui_util.h"
 
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(CustomizeChromeUI,
@@ -96,6 +97,9 @@ CustomizeChromeUI::CustomizeChromeUI(content::WebUI* web_ui)
       "modulesEnabled",
       ntp::HasModulesEnabled(module_id_names_,
                              IdentityManagerFactory::GetForProfile(profile_)));
+  source->AddString(
+      "chromeRefresh2023Attribute",
+      features::IsChromeRefresh2023() ? "chrome-refresh-2023" : "");
 
   webui::SetupWebUIDataSource(
       source,
