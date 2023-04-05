70426d20d9f6f6b89f2c5532a6a8f0b8af070183
Xinghui Lu <xinghuilu@chromium.org>
2023-04-05 16:20:23 +0000

    [SBv5] Implement async workflow in OHTTP key service.
    Async workflow will be useful to avoid fetching the key synchronously
    during hash-prefix real time lookup. It will start at profile
    creation, and be checked repeatedly every hour. If the key is found
    close to expiration, the workflow will fetch a new key.
    
    The workflow is disabled if Safe Browsing state is in no protection or
    enhanced protection, since HPRT is disabled in those states. The
    public API GetOhttpKey will also return an empty key if the service is
    disabled.
    
    Also reduce key expiration duration from 30 days to 7 days based on
    feedback.
    
    Bug: 1407283
    Change-Id: Ifbfbfcab8277ba319364b84a47c59f6f75720772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396092
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126621}

--- chrome/browser/safe_browsing/ohttp_key_service_factory.h
+++ chrome/browser/safe_browsing/ohttp_key_service_factory.h
@@ -45,6 +45,7 @@ class OhttpKeyServiceFactory : public ProfileKeyedServiceFactory {
   // BrowserContextKeyedServiceFactory:
   KeyedService* BuildServiceInstanceFor(
       content::BrowserContext* context) const override;
+  bool ServiceIsCreatedWithBrowserContext() const override;
 };
 
 }  // namespace safe_browsing
