864a804d2f1ab186d70828494e53a0a306f5ba1a
Maxim Kolosovskiy <kolos@google.com>
2023-04-05 14:17:56 +0000

    Disables failing ScreensaverImageDownloader.DownloadImages
    Bug: 1430802
    
    Change-Id: Idf915ead1ea864d2839b43459502e1aa054d1c19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401586
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Owners-Override: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Auto-Submit: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126545}

--- chrome/browser/ash/policy/handlers/screensaver_image_downloader_unittest.cc
+++ chrome/browser/ash/policy/handlers/screensaver_image_downloader_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ash/policy/handlers/screensaver_image_downloader.h"
+#include "build/build_config.h"
 
 #include <memory>
 
@@ -91,7 +92,13 @@ class ScreensaverImageDownloaderTest : public testing::Test {
   std::unique_ptr<ScreensaverImageDownloader> screensaver_image_downloader_;
 };
 
-TEST_F(ScreensaverImageDownloaderTest, DownloadImagesTest) {
+// crbug.com/1430802: flaky on Linux,
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_DownloadImagesTest DISABLED_DownloadImagesTest
+#else
+#define MAYBE_DownloadImagesTest DownloadImagesTest
+#endif
+TEST_F(ScreensaverImageDownloaderTest, MAYBE_DownloadImagesTest) {
   // Test successful download.
   {
     url_loader_factory()->AddResponse(kImageUrl1, kFileContents);
