3f997084fa6a750c1878cf3fc7a138d1d7b40030
Calder Kitagawa <ckitagawa@chromium.org>
2023-04-05 23:41:14 +0000

    Reland "[Tab Switcher] Remove DFM Code"
    This reverts commit 3e5825d0e49d062d4d7f7aa6b6e77348fbbf3a00.
    
    Reason for revert: was reverted for a resource missing bug
    which seems to have been remedied by
    https://chromium-review.googlesource.com/c/chromium/src/+/4385474
    
    I've tried building the failing gn args config from
    https://crbug.com/1427465 locally and didn't encounter any issues.
    
    Also added android-official build to the CQ to double check.
    
    See the delta between PS1 and Latest for changes.
    
    Original change's description:
    > Revert "[Tab Switcher] Remove DFM Code"
    >
    > This reverts commit d04167d58f9710d0d3ac1d50c0ce40fe18cb3c38.
    >
    > Reason for revert: https://crbug.com/1427465 causing internal
    > build breakage.
    >
    > Original change's description:
    > > [Tab Switcher] Remove DFM Code
    > >
    > > The Tab Management DFM was never fully implemented and is always
    > > installed. We don't anticipate this behavior to change and are removing
    > > the framework for installing the feature as a module.
    > >
    > > Note that we cannot modularize this code as it depends on code in
    > > chrome_java. As such it needs to be included in the monolithic
    > > chrome_java target until such time as it can be modularized.
    > >
    > > There was a bug where isGridTabSwitcherEnabled was returning true always
    > > this should only be true if one of StartSurface or TabGroups is enabled
    > > as on low-end devices and for accessibility enabled we don't actually
    > > show the grid unless TAB_GROUPS_CONTINUATION is enabled.
    > >
    > > Bug: 1422324
    > > Change-Id: I0bdc78fc89f4987ab359d46b27cf723295a84152
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316344
    > > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > > Reviewed-by: Mei Liang <meiliang@chromium.org>
    > > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > > Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1121651}
    >
    > Bug: 1422324
    > Change-Id: I412def87c4c114f427f1f6197f792600e9e6288c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368680
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Ted Choc <tedchoc@chromium.org>
    > Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1121732}
    
    Bug: 1422324, 1427465
    Change-Id: If345524b20ef596173dae2821f127b811fafbee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402393
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126915}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiFeatureUtilities.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiFeatureUtilities.java
@@ -10,8 +10,6 @@ import static org.chromium.chrome.browser.device.DeviceClassManager.GTS_LOW_END_
 
 import android.content.Context;
 
-import androidx.annotation.Nullable;
-
 import org.chromium.base.Log;
 import org.chromium.base.SysUtils;
 import org.chromium.chrome.browser.device.DeviceClassManager;
@@ -92,24 +90,6 @@ public class TabUiFeatureUtilities {
     private static Boolean sTabManagementModuleSupportedForTesting;
 
     /**
-     * Set whether the tab management module is supported for testing.
-     */
-    public static void setTabManagementModuleSupportedForTesting(@Nullable Boolean enabled) {
-        sTabManagementModuleSupportedForTesting = enabled;
-    }
-
-    /**
-     * @return Whether the tab management module is supported.
-     */
-    private static boolean isTabManagementModuleSupported() {
-        if (sTabManagementModuleSupportedForTesting != null) {
-            return sTabManagementModuleSupportedForTesting;
-        }
-
-        return TabManagementModuleProvider.isTabManagementModuleSupported();
-    }
-
-    /**
      * @return Whether the Grid Tab Switcher UI is enabled and available for use.
      * @param context The activity context.
      */
@@ -119,7 +99,7 @@ public class TabUiFeatureUtilities {
         }
 
         // Having Tab Groups or Start implies Grid Tab Switcher.
-        return isTabManagementModuleSupported() || isTabGroupsAndroidEnabled(context)
+        return isTabGroupsAndroidEnabled(context)
                 || ReturnToChromeUtil.isStartSurfaceEnabled(context);
     }
 
@@ -163,8 +143,7 @@ public class TabUiFeatureUtilities {
         }
 
         return !DeviceClassManager.enableAccessibilityLayout(context)
-                && ChromeFeatureList.sTabGroupsAndroid.isEnabled()
-                && isTabManagementModuleSupported();
+                && ChromeFeatureList.sTabGroupsAndroid.isEnabled();
     }
 
     /**
