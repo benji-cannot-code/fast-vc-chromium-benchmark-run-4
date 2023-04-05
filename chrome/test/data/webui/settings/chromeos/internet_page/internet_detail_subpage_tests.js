3bbf59b199ca77e6af881efaf357d449d0d183e9
Gordon Seto <gordonseto@google.com>
2023-04-05 19:03:22 +0000

    [CrOS Cellular] Hide APN row when non-active SIM
    Update <settings-internet-detail-subpage> to hide the APN row item if
    the cellular network is not in the active SIM slot.
    
    Bug: b:162365553
    Test: *InternetDetailSubpage*
    Change-Id: Id889f43a7a594657d768c3c0d5bfe520a4bf88df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396748
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Auto-Submit: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126736}

--- chrome/test/data/webui/settings/chromeos/internet_page/internet_detail_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/internet_page/internet_detail_subpage_tests.js
@@ -1113,6 +1113,7 @@ suite('InternetDetailPage', function() {
     test(
         'Cellular network on active sim slot, show config sections',
         async () => {
+          loadTimeData.overrideValues({isApnRevampEnabled: true});
           init();
           const test_iccid = '11111111111111111';
 
@@ -1142,11 +1143,14 @@ suite('InternetDetailPage', function() {
           assertTrue(!!internetDetailPage.shadowRoot
                            .querySelector('cellular-roaming-toggle-button')
                            .getCellularRoamingToggle());
+          assertTrue(!!internetDetailPage.shadowRoot.querySelector(
+              '#apnSubpageButton'));
         });
 
     test(
         'Cellular network on non-active sim slot, hide config sections',
         async () => {
+          loadTimeData.overrideValues({isApnRevampEnabled: true});
           init();
           const test_iccid = '11111111111111111';
 
@@ -1176,6 +1180,8 @@ suite('InternetDetailPage', function() {
           // showing.
           assertTrue(!!internetDetailPage.shadowRoot.querySelector(
               '#connectDisconnect'));
+          assertFalse(!!internetDetailPage.shadowRoot.querySelector(
+              '#apnSubpageButton'));
         });
 
     test(
@@ -1478,13 +1484,27 @@ suite('InternetDetailPage', function() {
         init();
         mojoApi_.setNetworkTypeEnabledState(NetworkType.kCellular, true);
         const apnName = 'test';
+        const testIccid = '11111';
         const cellularNetwork =
             getManagedProperties(NetworkType.kCellular, 'cellular');
         cellularNetwork.typeProperties.cellular.connectedApn = {};
         cellularNetwork.typeProperties.cellular.connectedApn.accessPointName =
             apnName;
+        cellularNetwork.typeProperties.cellular.iccid = testIccid;
         mojoApi_.setManagedPropertiesForTest(cellularNetwork);
         internetDetailPage.init('cellular_guid', 'Cellular', 'cellular');
+
+        // Set cellular network as active SIM so that APN row should show up if
+        // the flag is enabled.
+        mojoApi_.setDeviceStateForTest({
+          type: NetworkType.kCellular,
+          deviceState: DeviceStateType.kEnabled,
+          inhibitReason: InhibitReason.kNotInhibited,
+          simInfos: [{
+            iccid: testIccid,
+            isPrimary: true,
+          }],
+        });
         await flushAsync();
         const crLink =
             internetDetailPage.shadowRoot.querySelector('#apnSubpageButton');
