ea1f648fe521fc00d932c1973dc0bd9a6bbcc527
ginnnnnnny <ginnyhuang@chromium.org>
2023-04-05 17:26:33 +0000

    [iOS][byot] Bottom message prompt UI for Bring Android Tabs
    This is the SwiftUI view for the bottom message prompt. A follow up CL
    would implement display/remove the view from the regular tab grid.
    
    Screenshot: https://screenshot.googleplex.com/4C4NdRdYwdddPzX
    (The screenshot is captured for UI demonstration purpose only. In real
    life the prompt would NEVER show when the number of tabs is 0.),
    
    Bug: 1418117
    Change-Id: Ice5ec7e11d4b7874c8c28b881759a134bb7b15eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4393672
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126662}

--- ios/chrome/browser/ui/bring_android_tabs/bring_android_tabs_prompt_coordinator.mm
+++ ios/chrome/browser/ui/bring_android_tabs/bring_android_tabs_prompt_coordinator.mm
@@ -50,6 +50,8 @@ void SetModalPresentationStyle(UIViewController* view_controller) {
   // Mediator that updates Chromium model objects; serves as a delegate to the
   // view controller.
   BringAndroidTabsPromptMediator* _mediator;
+  // View provider for the bottom message variant of the prompt.
+  BringAndroidTabsPromptBottomMessageProvider* _provider;
 }
 
 - (void)start {
@@ -74,7 +76,11 @@ void SetModalPresentationStyle(UIViewController* view_controller) {
       break;
     }
     case BringYourOwnTabsPromptType::kBottomMessage: {
-      // TODO(crbug.com/1418117): Create bottom message prompt.
+      _provider = [[BringAndroidTabsPromptBottomMessageProvider alloc]
+          initWithTabCount:static_cast<int>(service->GetNumberOfAndroidTabs())];
+      _provider.delegate = _mediator;
+      _provider.commandHandler = self.commandHandler;
+      _viewController = _provider.viewController;
       break;
     }
     case BringYourOwnTabsPromptType::kDisabled: {
@@ -94,6 +100,7 @@ void SetModalPresentationStyle(UIViewController* view_controller) {
   // Remove the mediator.
   DCHECK(_mediator);
   _mediator = nil;
+  _provider = nil;
 }
 
 @end
