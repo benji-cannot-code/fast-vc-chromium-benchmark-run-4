1fbc4b1c7c13de6c69fd24b703bddcdcb671fb60
Benjamin Zielinski <bzielinski@google.com>
2023-04-05 22:23:58 +0000

    [FreezeCast] Close trays before freezing
    Depending if QsRevamp is enabled, the cast notification may be clicked
    on while the UnifiedSystemTray is open or the NotificationCenterTray is
    open. It may also be click on if it is visible while neither is open.
    In the cases where a tray is open, tell that tray to close and wait for
    its widget to be destroyed before calling freeze. This helps so that a
    tray is not captured on the frozen cast screen.
    
    Also, update the notification icon when casting is frozen. Add the new
    icon to components/vector_icons since it will also be used in the
    Chrome browser.
    
    Bug: b:271443217
    Change-Id: Ife6e7f470ddd3c4bac8dd25dcdace1c56b6810ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387042
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126876}

--- ash/system/cast/cast_notification_controller.h
+++ ash/system/cast/cast_notification_controller.h
@@ -11,11 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "base/memory/weak_ptr.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/views/widget/widget_observer.h"
+
+namespace views {
+class Widget;
+}  // namespace views
 
 namespace ash {
 
 class ASH_EXPORT CastNotificationController
-    : public CastConfigController::Observer {
+    : public CastConfigController::Observer,
+      public views::WidgetObserver {
  public:
   CastNotificationController();
 
@@ -27,6 +33,9 @@ class ASH_EXPORT CastNotificationController
 
   // CastConfigControllerObserver:
   void OnDevicesUpdated(const std::vector<SinkAndRoute>& devices) override;
+
+  // views::WidgetObserver overrides:
+  void OnWidgetDestroyed(views::Widget* widget) override;
 
  private:
   // The callback that is triggered when the cast notification is pressed,
@@ -49,6 +58,7 @@ class ASH_EXPORT CastNotificationController
   // interacts with a cast, we use these values.
   absl::optional<int> freeze_button_index_;
   bool displayed_route_is_frozen_ = false;
+  bool freeze_on_tray_widget_destroyed_ = false;
 
   base::WeakPtrFactory<CastNotificationController> weak_ptr_factory_{this};
 };
