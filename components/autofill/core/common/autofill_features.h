fc052ebdce87f86ccfd99d91d932180b474ddd35
Jihad Hanna <jihadghanna@google.com>
2023-04-05 16:02:56 +0000

    Set cached is_autofilled only on filling (not preview).
    Autofill caches FormStructure's for retention of state purposes.
    `is_autofilled` is maintained after filling to detect that a user
    modified an autofilled field (For metrics such as FillingCorrectness).
    
    This CL:
    -Makes cached `is_autofilled` state set only when filling a for and not
    previewing, since after preview a user typing into a form doesn't count
    as editing an autofilled field.
    -Fixes some typos in touched file.
    
    Fixed: 1430704
    
    Change-Id: I6caa639ead0804cc3fd9a7cfb2ae657ebd1f1897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402254
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126609}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -113,6 +113,8 @@ BASE_DECLARE_FEATURE(kAutofillEnableSupportForPhoneNumberTrunkTypes);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillFeedback);
 COMPONENT_EXPORT(AUTOFILL)
+BASE_DECLARE_FEATURE(kAutofillOnlyCacheIsAutofilledOnFill);
+COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(
     kAutofillStreetNameOrHouseNumberPrecedenceOverAutocomplete);
 // Records the values of HtmlFieldType that the server or heuristic predictions
