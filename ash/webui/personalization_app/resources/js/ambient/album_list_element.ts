e1ec948fbe82e7871d8354a8c5f9e4c4c4ddea63
Jason Thai <jasontt@chromium.org>
2023-04-05 19:46:03 +0000

    personalization: do not allow de-selecting video album
    Exactly one video must be selected at all times for
    |TopicSource::kVideo|. This means once a video album is selected, it
    cannot be de-selected. A user can select a different album, which
    will un-select all the other video albums.
    
    BUG=b/275561700
    TEST=browser_tests --gtest_filter="*PersonalizationAppAmbientSubpage*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Icf55f188e8c4fdba373a0a2e59b3f4e6fbb410a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400921
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126766}

--- ash/webui/personalization_app/resources/js/ambient/album_list_element.ts
+++ ash/webui/personalization_app/resources/js/ambient/album_list_element.ts
@@ -61,6 +61,11 @@ export class AlbumList extends WithPersonalizationStore {
 
   /** Invoked on selection of an album. */
   private onAlbumSelected_(e: Event&{model: {album: AmbientModeAlbum}}) {
+    if (this.topicSource === TopicSource.kVideo && e.model.album.checked) {
+      // De-selecting a selected video album is a no-op. Selecting a different
+      // video album will unselect the other video albums in the client.
+      return;
+    }
     e.model.album.checked = !e.model.album.checked;
     this.dispatchEvent(new CustomEvent(
         'album_selected_changed',
@@ -109,6 +114,10 @@ export class AlbumList extends WithPersonalizationStore {
 
   private isGooglePhotos_(topicSource: TopicSource): boolean {
     return topicSource === TopicSource.kGooglePhotos;
+  }
+
+  private isVideo_(topicSource: TopicSource): boolean {
+    return topicSource === TopicSource.kVideo;
   }
 }
 
