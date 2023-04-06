bf589da0be6c150beb89cf9042b7abd0b9e5712f
Stephan Hartmann <stha09@googlemail.com>
2023-04-06 07:15:03 +0000

    [unbundle] do not use ZipAnalyzer traits header
    It is enough to include chrome/common/safe_browsing/archive_analyzer_results.h.
    
    Bug: None
    Change-Id: Ia63a66b77c073fa4fdb8470db34b4c24722293cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373706
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1127041}

--- chrome/services/file_util/safe_archive_analyzer.h
+++ chrome/services/file_util/safe_archive_analyzer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/safe_browsing/archive_analyzer_results.h"
 #include "chrome/common/safe_browsing/rar_analyzer.h"
+#include "chrome/common/safe_browsing/zip_analyzer.h"
 #include "chrome/services/file_util/public/mojom/safe_archive_analyzer.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
