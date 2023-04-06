5a06abf37e9828e762ed9fc3e2b524a66b90ff49
Hidehiko Abe <hidehiko@chromium.org>
2023-04-06 06:58:16 +0000

    Remove user_manager::RemoveUserDelegate from UserRequiredScreen
    Currently UserRequiredScreen checks the condition every time when
    a user is removed.
    However, it is redundant, and we can just check once after all
    trials of removing users is completed.
    
    Note: this class is the only user of RemoveUserDelegate on
    non-testing code. In a following CL, we can remove the delegate itself.
    
    BUG=b/267685577, b/277159583
    TEST=Ran on DUT.
    
    Change-Id: If1c5c50fcd977a55b5b14b216fd230308529dbd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394403
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127037}

--- chrome/browser/ash/login/screens/update_required_screen.h
+++ chrome/browser/ash/login/screens/update_required_screen.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/screens/error_screen.h"
 #include "chrome/browser/ash/login/version_updater/version_updater.h"
 #include "chromeos/ash/components/network/network_state_handler_observer.h"
-#include "components/user_manager/remove_user_delegate.h"
 
 namespace base {
 class Clock;
@@ -33,8 +32,7 @@ class UpdateRequiredView;
 // Controller for the update required screen.
 class UpdateRequiredScreen : public BaseScreen,
                              public VersionUpdater::Delegate,
-                             public NetworkStateHandlerObserver,
-                             public user_manager::RemoveUserDelegate {
+                             public NetworkStateHandlerObserver {
  public:
   using TView = UpdateRequiredView;
 
@@ -85,10 +83,6 @@ class UpdateRequiredScreen : public BaseScreen,
 
   // NetworkStateHandlerObserver:
   void DefaultNetworkChanged(const NetworkState* network) override;
-
-  // user_manager::RemoveUserDelegate:
-  void OnBeforeUserRemoved(const AccountId& account_id) override;
-  void OnUserRemoved(const AccountId& account_id) override;
 
   void RefreshNetworkState();
   void RefreshView(const VersionUpdater::UpdateInfo& update_info);
