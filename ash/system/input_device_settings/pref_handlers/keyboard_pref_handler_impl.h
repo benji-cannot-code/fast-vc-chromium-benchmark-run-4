da0c3fac4b86d79110283ced98f4777ed8dcb1b8
Michael Checo <michaelcheco@google.com>
2023-04-06 04:16:00 +0000

    Settings Split: Support keyboard login screen settings in pref handler
    - Add methods to support storing internal/external keyboard
      settings
    - A follow-up CL will call these methods when the login screen
      user changes
    
    Bug: b/241965700
    Change-Id: I42d2d1abb308bd51549555b3a7f05781a0146371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395022
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127006}

--- ash/system/input_device_settings/pref_handlers/keyboard_pref_handler_impl.h
+++ ash/system/input_device_settings/pref_handlers/keyboard_pref_handler_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/input_device_settings/pref_handlers/keyboard_pref_handler.h"
 #include "base/values.h"
 
+class AccountId;
 class PrefService;
 
 namespace ash {
@@ -27,9 +28,24 @@ class ASH_EXPORT KeyboardPrefHandlerImpl : public KeyboardPrefHandler {
       PrefService* pref_service,
       const mojom::KeyboardPolicies& keyboard_policies,
       mojom::Keyboard* keyboard) override;
+
+  void InitializeLoginScreenKeyboardSettings(
+      PrefService* local_state,
+      const AccountId& account_id,
+      const mojom::KeyboardPolicies& keyboard_policies,
+      mojom::Keyboard* keyboard) override;
+
+  // Updates device settings stored in prefs to match the values in
+  // `keyboard.settings`.
   void UpdateKeyboardSettings(PrefService* pref_service,
                               const mojom::KeyboardPolicies& keyboard_policies,
                               const mojom::Keyboard& keyboard) override;
+
+  void UpdateLoginScreenKeyboardSettings(
+      PrefService* local_state,
+      const AccountId& account_id,
+      const mojom::KeyboardPolicies& keyboard_policies,
+      const mojom::Keyboard& keyboard) override;
 };
 
 }  // namespace ash
