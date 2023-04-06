58b03eaeb55cdcf5701e32d0420f7bbb88a97257
Daniel Murphy <dmurph@google.com>
2023-04-06 19:50:58 +0000

    [dPWA] Fix WebAppDataRetriever missed callbacks on errors
    Change-Id: Ie1eee5930cc3ec8f669465762cb3104269bde89c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4406356
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127348}

--- chrome/browser/web_applications/web_contents/web_app_data_retriever.cc
+++ chrome/browser/web_applications/web_contents/web_app_data_retriever.cc
@@ -168,6 +168,7 @@ void WebAppDataRetriever::OnGetWebPageMetadata(
     int last_committed_nav_entry_unique_id,
     webapps::mojom::WebPageMetadataPtr web_page_metadata) {
   if (ShouldStopRetrieval()) {
+    CallCallbackOnError(webapps::InstallableStatusCode::RENDERER_CANCELLED);
     return;
   }
 
@@ -206,6 +207,7 @@ void WebAppDataRetriever::OnGetWebPageMetadata(
 void WebAppDataRetriever::OnDidPerformInstallableCheck(
     const webapps::InstallableData& data) {
   if (ShouldStopRetrieval()) {
+    CallCallbackOnError(webapps::InstallableStatusCode::RENDERER_CANCELLED);
     return;
   }
 
@@ -230,6 +232,7 @@ void WebAppDataRetriever::OnIconsDownloaded(
     IconsMap icons_map,
     DownloadedIconsHttpResults icons_http_results) {
   if (ShouldStopRetrieval()) {
+    CallCallbackOnError(webapps::InstallableStatusCode::RENDERER_CANCELLED);
     return;
   }
 
