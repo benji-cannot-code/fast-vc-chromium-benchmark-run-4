34b90884333ad052760fb5c6d82eb3cfa1f9e3df
Ilkin Safarli <safarli@google.com>
2023-04-06 20:07:35 +0000

    CrOS Settings: Convert os_a11y_page browser test to TS
    Bug: b:270728282
    Test: browser_tests --gtest_filter="OSSettingsOsA11yPageV3*"
    Change-Id: Icc7214c70be332d44dc69219ad98ceb1da02a960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401810
    Commit-Queue: Ilkin Safarli <safarli@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127360}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/os_a11y_page.ts
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/os_a11y_page.ts
@@ -39,7 +39,7 @@ import {Route, Router} from '../router.js';
 import {getTemplate} from './os_a11y_page.html.js';
 import {OsA11yPageBrowserProxy, OsA11yPageBrowserProxyImpl} from './os_a11y_page_browser_proxy.js';
 
-interface OsSettingsA11yPageElement {
+export interface OsSettingsA11yPageElement {
   $: {
     a11yImageLabels: SettingsToggleButtonElement,
   };
@@ -48,7 +48,7 @@ interface OsSettingsA11yPageElement {
 const OsSettingsA11yPageElementBase = DeepLinkingMixin(
     RouteOriginMixin(PrefsMixin(WebUiListenerMixin(PolymerElement))));
 
-class OsSettingsA11yPageElement extends OsSettingsA11yPageElementBase {
+export class OsSettingsA11yPageElement extends OsSettingsA11yPageElementBase {
   static get is() {
     return 'os-settings-a11y-page';
   }
--- chrome/test/data/webui/settings/chromeos/os_a11y_page/test_os_a11y_page_browser_proxy.ts
+++ chrome/test/data/webui/settings/chromeos/os_a11y_page/test_os_a11y_page_browser_proxy.ts
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {OsA11yPageBrowserProxy} from 'chrome://os-settings/chromeos/os_settings.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
-/** @implements {OsA11yPageBrowserProxy} */
-export class TestOsA11yPageBrowserProxy extends TestBrowserProxy {
+export class TestOsA11yPageBrowserProxy extends TestBrowserProxy implements
+    OsA11yPageBrowserProxy {
   constructor() {
     super([
       'a11yPageReady',
