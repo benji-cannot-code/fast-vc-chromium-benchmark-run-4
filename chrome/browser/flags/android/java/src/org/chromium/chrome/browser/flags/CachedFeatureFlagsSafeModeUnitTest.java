42a21f4783b29dfc2605a5e1114b7db38d9b979a
Henrique Nakashima <hnakashima@chromium.org>
2023-04-06 19:53:45 +0000

    [Android] Migrate CachedFlags safe mode test to @Looper(PAUSED)
    CachedFeatureFlagsSafeModeUnitTest was using @Looper(LEGACY) as it was
    written prior to the Robolectric update that made PAUSED the default.
    
    Bug: 1210371
    Change-Id: I682ddf5ce1cc1ef7e64a863bf137a94b0147e578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4406852
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127350}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFeatureFlagsSafeModeUnitTest.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFeatureFlagsSafeModeUnitTest.java
@@ -11,14 +11,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
-import org.robolectric.annotation.LooperMode;
 
 import org.chromium.base.FeatureList;
 import org.chromium.base.FeatureList.TestValues;
-import org.chromium.base.task.test.ShadowPostTask;
+import org.chromium.base.task.test.PausedExecutorTestRule;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.flags.CachedFlagsSafeMode.Behavior;
 
@@ -31,9 +30,10 @@ import java.util.Arrays;
  * {@link CachedFlagsSafeMode}.
  */
 @RunWith(BaseRobolectricTestRunner.class)
-@Config(shadows = {ShadowPostTask.class})
-@LooperMode(LooperMode.Mode.LEGACY)
 public class CachedFeatureFlagsSafeModeUnitTest {
+    @Rule
+    public PausedExecutorTestRule mExecutorRule = new PausedExecutorTestRule();
+
     private static final String CRASHY_FEATURE = "CrashyFeature";
     private static final String OK_FEATURE = "OkFeature";
     private static final boolean CRASHY_FEATURE_DEFAULT = false;
@@ -589,7 +589,8 @@ public class CachedFeatureFlagsSafeModeUnitTest {
                 Arrays.asList(BOOL_PARAM, INT_PARAM, DOUBLE_PARAM, STRING_PARAM));
 
         CachedFeatureFlags.onEndCheckpoint();
-        // Async task writing values should have run synchronously because of ShadowPostTask.
+        mExecutorRule.runAllBackgroundAndUi();
+
         assertTrue(CachedFlagsSafeMode.getSafeValuePreferences().contains(
                 "Chrome.Flags.CachedFlag.CrashyFeature"));
 
