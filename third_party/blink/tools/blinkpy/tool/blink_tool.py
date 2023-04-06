98401ed1688117c035687b40c847f0fc771b499b
Jonathan Lee <jonathanjlee@google.com>
2023-04-06 00:35:12 +0000

    [rebaseline-cl] Parallelize ResultDB/Logdog calls
    This change introduces asynchronous network calls for trivially
    parallelizable tasks. The asynchronous work is executed in a persistent
    thread pool and backed by a shared `Web` that has been made thread-safe.
    This avoids large backwards-incompatible changes to the networking API.
    
    Because crbug.com/1428727 requires `rebaseline-cl` to make many more
    network calls, we see a substantial speedup when manually testing this
    change with many failed builders (see below). ResultDB handles the
    parallel traffic well because the number of HTTP connections remains
    bounded.
    
    Fixed: 1411891
    Test: git cl issue 4386057
    Test: ./blink_tool.py rebaseline-cl --patchset=1 --verbose
    Test: Wall clock runtime reduced from 4:00 without patch -> 1:40 with
    Change-Id: I15e919de86633a98b7ef82bcb5200bc7dd479c98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404941
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126932}

--- third_party/blink/tools/blinkpy/tool/blink_tool.py
+++ third_party/blink/tools/blinkpy/tool/blink_tool.py
@@ -36,6 +36,7 @@ These commands don't necessarily have anything to do with each other.
 import logging
 import optparse
 import sys
+from concurrent.futures import ThreadPoolExecutor
 
 # pylint: disable=cyclic-import; `rebaseline_cl -> rebaseline` false positive
 from blinkpy.common.host import Host
@@ -72,6 +73,7 @@ class BlinkTool(Host):
     def __init__(self, path):
         super(BlinkTool, self).__init__()
         self._path = path
+        io_pool = ThreadPoolExecutor(max_workers=RebaselineCL.MAX_WORKERS)
         self.commands = [
             AnalyzeBaselines(),
             CrashLog(),
@@ -81,7 +83,7 @@ class BlinkTool(Host):
             PrintBaselines(),
             PrintExpectations(),
             Rebaseline(),
-            RebaselineCL(self),
+            RebaselineCL(self, io_pool),
             UpdateMetadata(self),
         ]
         self.help_command = HelpCommand(tool=self)
