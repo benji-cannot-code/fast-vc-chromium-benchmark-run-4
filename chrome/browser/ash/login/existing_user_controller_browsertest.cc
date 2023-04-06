8ad0ff619ac09a4fb46c5fa03af4a6b7d492d925
Roman Sorokin <rsorokin@chromium.org>
2023-04-06 07:58:44 +0000

    [OOBE CodeHealth] Remove `show_guest` arg from the init function
    Call LoginScreen directly instead
    
    BUG=b:275061367
    
    Change-Id: I9e59756a4398571f284d8abd6fd43fd723b61ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401397
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127056}

--- chrome/browser/ash/login/existing_user_controller_browsertest.cc
+++ chrome/browser/ash/login/existing_user_controller_browsertest.cc
@@ -231,7 +231,7 @@ class ExistingUserControllerTest : public policy::DevicePolicyCrosBrowserTest {
     EXPECT_CALL(*mock_login_display_host_, GetSigninUI())
         .Times(AnyNumber())
         .WillRepeatedly(Return(mock_signin_ui_.get()));
-    EXPECT_CALL(*mock_login_display_, Init(_, true)).Times(1);
+    EXPECT_CALL(*mock_login_display_, Init(_)).Times(1);
   }
 
   void SetUpOnMainThread() override {
@@ -461,7 +461,7 @@ class ExistingUserControllerPublicSessionTest
     EXPECT_CALL(*mock_login_display_host_, GetSigninUI())
         .Times(AnyNumber())
         .WillRepeatedly(Return(mock_signin_ui_.get()));
-    EXPECT_CALL(*mock_login_display_, Init(_, _)).Times(AnyNumber());
+    EXPECT_CALL(*mock_login_display_, Init(_)).Times(AnyNumber());
   }
 
   void TearDownOnMainThread() override {
@@ -969,7 +969,7 @@ class ExistingUserControllerActiveDirectoryUserAllowlistTest
     EXPECT_CALL(*mock_login_display_host_, GetSigninUI())
         .Times(AnyNumber())
         .WillRepeatedly(Return(mock_signin_ui_.get()));
-    EXPECT_CALL(*mock_login_display_, Init(_, true)).Times(1);
+    EXPECT_CALL(*mock_login_display_, Init(_)).Times(1);
   }
 };
 
