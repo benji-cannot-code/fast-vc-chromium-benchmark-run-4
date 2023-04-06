be008c05245b089e2bb207d8e7aa390e075d750b
Tanmoy Mollik <triploblastic@chromium.org>
2023-04-06 11:42:53 +0000

    [IOS][Bkmrks] Cleanup of BookmarksFolderEditor* - Part 2
    This cl removes the two class creator methods in
    BookmarksFolderEditorViewController and makes a single initializer
    method public.
    The client of the two class methods was the
    BookmarksFolderEditorCoordinator. These class methods are not needed
    and can be replaced by the public initializer.
    
    Change-Id: I767d068c63b70dcb50155feb98e6c1597edc1fca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394427
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Petro Akzhygitov <pakzhygitov@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127115}

--- ios/chrome/browser/ui/bookmarks/folder_editor/bookmarks_folder_editor_coordinator.mm
+++ ios/chrome/browser/ui/bookmarks/folder_editor/bookmarks_folder_editor_coordinator.mm
@@ -93,31 +93,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       SyncSetupServiceFactory::GetForBrowserState(browserState);
   syncer::SyncService* syncService =
       SyncServiceFactory::GetForBrowserState(browserState);
+  _viewController = [[BookmarksFolderEditorViewController alloc]
+      initWithBookmarkModel:model
+                 folderNode:_folderNode
+           parentFolderNode:_parentFolderNode
+           syncSetupService:syncSetupService
+                syncService:syncService
+                    browser:self.browser];
+  _viewController.delegate = self;
+  _viewController.snackbarCommandsHandler = HandlerForProtocol(
+      self.browser->GetCommandDispatcher(), SnackbarCommands);
+
   if (_baseNavigationController) {
     DCHECK(_parentFolderNode);
-    _viewController = [BookmarksFolderEditorViewController
-        folderCreatorWithBookmarkModel:model
-                          parentFolder:_parentFolderNode
-                               browser:self.browser
-                      syncSetupService:syncSetupService
-                           syncService:syncService];
-    _viewController.delegate = self;
-    _viewController.snackbarCommandsHandler = HandlerForProtocol(
-        self.browser->GetCommandDispatcher(), SnackbarCommands);
+    DCHECK(!_folderNode);
     [_baseNavigationController pushViewController:_viewController animated:YES];
   } else {
     DCHECK(!_navigationController);
-    DCHECK(_folderNode);
     DCHECK(!_parentFolderNode);
-    _viewController = [BookmarksFolderEditorViewController
-        folderEditorWithBookmarkModel:model
-                               folder:_folderNode
-                              browser:self.browser
-                     syncSetupService:syncSetupService
-                          syncService:syncService];
-    _viewController.delegate = self;
-    _viewController.snackbarCommandsHandler = HandlerForProtocol(
-        self.browser->GetCommandDispatcher(), SnackbarCommands);
+    DCHECK(_folderNode);
     _navigationController = [[BookmarkNavigationController alloc]
         initWithRootViewController:_viewController];
     _navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
