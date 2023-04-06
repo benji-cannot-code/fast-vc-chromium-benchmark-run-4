1672b307c3d28860759f42432e5a4c67e8f40039
Luc Nguyen <lucnguyen@google.com>
2023-04-06 18:17:19 +0000

    Add new groups to Early Process Singleton experiment
    Some data oddities appeared in the experiment. It was pointed out that
    because the mechanism to report the metrics differ between the
    experimental groups, there may be biases specific to the groups:
    https://chromium-review.googlesource.com/c/chromium/src/+/4245577/comment/be857e5a_b7d3283c/
    
    This CL introduces a new "Disabled_Merge3" group that reports metrics
    the same way as the "Enabled" group in order to see if the oddities
    were caused by the way the metrics were reported.
    
    This CL also changes some leftover !IS_ANDROID buildflags to
    ENABLE_PROCESS_SINGLETON buildflags (the latter buildflag is 0 for
    Android and CrOS).
    
    Bug: 1408239
    Change-Id: Iff03fd345f8421d2b28b6e2a753aa83c8b739e90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395185
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127292}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event_impl.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/buildflags.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/chrome_resource_bundle_helper.h"
 #include "chrome/browser/defaults.h"
@@ -209,10 +210,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/child/pdf_child_init.h"
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(ENABLE_PROCESS_SINGLETON)
 #include "chrome/browser/chrome_process_singleton.h"
 #include "chrome/browser/process_singleton.h"
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(ENABLE_PROCESS_SINGLETON)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "chrome/common/chrome_paths_lacros.h"
@@ -659,7 +660,7 @@ absl::optional<int> ChromeMainDelegate::PostEarlyInitialization(
     return absl::nullopt;
   }
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(ENABLE_PROCESS_SINGLETON)
   // Configure the early process singleton experiment.
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
@@ -1614,10 +1615,10 @@ void ChromeMainDelegate::ProcessExiting(const std::string& process_type) {
         browser_shutdown::ShutdownType::kOtherExit);
   }
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(ENABLE_PROCESS_SINGLETON)
   if (ChromeProcessSingleton::IsEarlySingletonFeatureEnabled())
     ChromeProcessSingleton::DeleteInstance();
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(ENABLE_PROCESS_SINGLETON)
 
   if (SubprocessNeedsResourceBundle(process_type))
     ui::ResourceBundle::CleanupSharedInstance();
