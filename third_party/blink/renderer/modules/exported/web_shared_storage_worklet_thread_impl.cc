12227bd1f9ea812e157e51a1c377a465164ad55a
Michael Lippautz <mlippautz@chromium.org>
2023-04-06 11:45:25 +0000

    Fix cppgc_enable_object_names build
    The build requires that destructors are fully defined for each object
    at construction time, even though on a regular build we would dispatch
    through the base class.
    
    Change-Id: Ie3f968ccf707f5bf5779ef37c4206a84fda94666
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4405738
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127116}

--- third_party/blink/renderer/modules/exported/web_shared_storage_worklet_thread_impl.cc
+++ third_party/blink/renderer/modules/exported/web_shared_storage_worklet_thread_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/exported/web_shared_storage_worklet_thread_impl.h"
 
 #include "third_party/blink/public/mojom/shared_storage/shared_storage_worklet_service.mojom.h"
+#include "third_party/blink/renderer/core/workers/threaded_worklet_object_proxy.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
