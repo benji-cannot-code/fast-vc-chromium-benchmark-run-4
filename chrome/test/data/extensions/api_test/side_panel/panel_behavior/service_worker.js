f9f8e10774ba021ec275a69f2ce37119da967f6e
Kelvin Jiang <kelvinjiang@chromium.org>
2023-04-06 16:42:51 +0000

    [Extensions] Add API methods to open side panel on icon click (pref)
    Add chrome.sidePanel.getPanelBehavior and setPanelBehavior, which
    gets/sets a pref that will allow an extension to open its side panel
    entry when its icon in the toolbar is clicked.
    
    This CL only adds the pref setting and API method definition, behavior
    will be implemented in a follow up.
    
    Bug: 1430549
    Change-Id: I1f5626a5ec75dee6aee0564309001bf7b6ee492e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400076
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127239}

---
+++ chrome/test/data/extensions/api_test/side_panel/panel_behavior/service_worker.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  // Test the default values of fields for chrome.sidePanel.getPanelBehavior().
+  async function defaultPanelBehavior() {
+    const behavior = await chrome.sidePanel.getPanelBehavior();
+    chrome.test.assertFalse(behavior.openPanelOnActionClick);
+    chrome.test.succeed();
+  },
+
+  // Test that chrome.sidePanel.setPanelBehavior is an upsert operation.
+  async function getAndSetPanelBehavior() {
+    await chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true});
+    let behavior = await chrome.sidePanel.getPanelBehavior();
+    chrome.test.assertTrue(behavior.openPanelOnActionClick);
+
+    await chrome.sidePanel.setPanelBehavior({});
+    behavior = await chrome.sidePanel.getPanelBehavior();
+    chrome.test.assertTrue(behavior.openPanelOnActionClick);
+
+    chrome.test.succeed();
+  },
+]);
