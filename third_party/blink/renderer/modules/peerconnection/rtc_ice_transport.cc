a5c6e2622bd58a06a453a51d42efd8b3813ad26c
Philipp Hancke <phancke@microsoft.com>
2023-04-06 08:41:09 +0000

    Re-add local candidates to RTCIceTransport
    BUG=chromium:1385446
    
    Change-Id: I47f5dd171da0529b59d1a376ceb0861dc1731bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394584
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Philipp Hancke <phancke@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1127067}

--- third_party/blink/renderer/modules/peerconnection/rtc_ice_transport.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_ice_transport.cc
@@ -42,6 +42,7 @@ const char* kIceRoleControllingStr = "controlling";
 const char* kIceRoleControlledStr = "controlled";
 
 RTCIceCandidate* ConvertToRtcIceCandidate(const cricket::Candidate& candidate) {
+  // The "" mid and sdpMLineIndex 0 are wrong, see https://crbug.com/1385446
   return RTCIceCandidate::Create(MakeGarbageCollected<RTCIceCandidatePlatform>(
       String::FromUTF8(webrtc::SdpSerializeCandidate(candidate)), "", 0));
 }
@@ -192,6 +193,11 @@ void RTCIceTransport::OnGatheringStateChanged(
   }
   gathering_state_ = new_state;
   DispatchEvent(*Event::Create(event_type_names::kGatheringstatechange));
+}
+void RTCIceTransport::OnCandidateGathered(
+    const cricket::Candidate& parsed_candidate) {
+  RTCIceCandidate* candidate = ConvertToRtcIceCandidate(parsed_candidate);
+  local_candidates_.push_back(candidate);
 }
 
 void RTCIceTransport::OnStateChanged(webrtc::IceTransportState new_state) {
