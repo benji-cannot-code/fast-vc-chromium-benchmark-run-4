bea6b027ab91b22260c2c4d8679de3e642c035ec
David Black <dmblack@google.com>
2023-04-06 03:47:19 +0000

    Add observer to WelcomeTourController.
    The new WelcomeTourControllerObserver will receive events:
    * OnWelcomeTourStarted()
    * OnWelcomeTourEnded()
    
    TODOs added in this CL hint at the various contexts that will need to
    be updated to react to these events.
    
    Bug: b:275616974
    Change-Id: I7668433e5757e54ac0e1608550a916ecf528b17b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404318
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127001}

---
+++ ash/user_education/welcome_tour/mock_welcome_tour_controller_observer.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/user_education/welcome_tour/mock_welcome_tour_controller_observer.h"
+
+namespace ash {
+
+MockWelcomeTourControllerObserver::MockWelcomeTourControllerObserver() =
+    default;
+
+MockWelcomeTourControllerObserver::~MockWelcomeTourControllerObserver() =
+    default;
+
+}  // namespace ash
