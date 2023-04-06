f8fedad19f65847537c2b9b6f10ee46be7fa350d
Stefan Zager <szager@chromium.org>
2023-04-06 01:09:19 +0000

    Check whether a <table> is an AX data table after children are parsed
    Previously, the full heuristic for detecting a data table was only
    applied when the AXNode for the <table> was created. if the table's
    contents are not fully parsed when the heuristic runs, it may
    incorrectly mark the table as a display table.
    
    Change-Id: I14aa940251621e98b1410e7d199537966cf38191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366366
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126950}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
@@ -202,6 +202,7 @@ class MODULES_EXPORT AXObjectCacheImpl
 
   void HandleAttributeChanged(const QualifiedName& attr_name,
                               Element*) override;
+  void FinishedParsingTable(HTMLTableElement*) override;
   void HandleValidationMessageVisibilityChanged(
       const Node* form_control) override;
   void HandleEventListenerAdded(const Node& node,
@@ -354,6 +355,7 @@ class MODULES_EXPORT AXObjectCacheImpl
   void HandleValidationMessageVisibilityChangedWithCleanLayout(const Node*);
   void HandleUpdateActiveMenuOptionWithCleanLayout(Node*);
   void HandleEditableTextContentChangedWithCleanLayout(Node*);
+  void UpdateTableRoleWithCleanLayout(Node*);
 
   bool InlineTextBoxAccessibilityEnabled();
 
