aba52f78e7528c1825b5d7080f1b98928c8eff7e
Dustin Mitchell <djmitche@chromium.org>
2023-04-06 21:00:36 +0000

    Revert "Pass a StorageKey to CodeCacheHostImpl"
    This reverts commit e02ff967486c2e6c001da68c12e3e2d2892a7416.
    
    Reason for revert: crashes - https://crbug.com/1431027, https://crbug.com/1431200
    
    Original change's description:
    > Pass a StorageKey to CodeCacheHostImpl
    >
    > Supply CodeCacheHostImpl with a storage key at every point where one is
    > created, and use that to key the cache storage.
    >
    > The effect is that CodeCache is now covered by third-party storage partitioning.
    >
    > Bug: 1402861, 779444
    > Change-Id: Iac89bec207f7b0bba0e8df4d58c4ff91753c8168
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4362561
    > Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    > Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    > Reviewed-by: Steven Bingler <bingler@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1126554}
    
    Bug: 1402861, 779444
    Change-Id: I62c39ac57521390be0c633a90f2dbf6f6be99534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404107
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Owners-Override: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127387}

--- content/browser/renderer_host/code_cache_host_impl.h
+++ content/browser/renderer_host/code_cache_host_impl.h
@@ -18,11 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/unique_receiver_set.h"
-#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/cache_storage/cache_storage.mojom.h"
 #include "third_party/blink/public/mojom/loader/code_cache.mojom.h"
 
 class GURL;
+
+namespace url {
+class Origin;
+}
 
 namespace content {
 
@@ -50,12 +53,10 @@ class CONTENT_EXPORT CodeCacheHostImpl : public blink::mojom::CodeCacheHost {
         mojo::UniqueReceiverSet<blink::mojom::CodeCacheHost>&)>;
     void Add(int render_process_id,
              const net::NetworkIsolationKey& nik,
-             const blink::StorageKey& storage_key,
              mojo::PendingReceiver<blink::mojom::CodeCacheHost> receiver,
              CodeCacheHostReceiverHandler handler);
     void Add(int render_process_id,
              const net::NetworkIsolationKey& nik,
-             const blink::StorageKey& storage_key,
              mojo::PendingReceiver<blink::mojom::CodeCacheHost> receiver);
     void Clear();
 
@@ -72,8 +73,7 @@ class CONTENT_EXPORT CodeCacheHostImpl : public blink::mojom::CodeCacheHost {
   CodeCacheHostImpl(
       int render_process_id,
       scoped_refptr<GeneratedCodeCacheContext> generated_code_cache_context,
-      const net::NetworkIsolationKey& nik,
-      const blink::StorageKey& storage_key);
+      const net::NetworkIsolationKey& nik);
 
   CodeCacheHostImpl(const CodeCacheHostImpl&) = delete;
   CodeCacheHostImpl& operator=(const CodeCacheHostImpl&) = delete;
@@ -98,6 +98,7 @@ class CONTENT_EXPORT CodeCacheHostImpl : public blink::mojom::CodeCacheHost {
       const GURL& url,
       base::Time expected_response_time,
       mojo_base::BigBuffer data,
+      const url::Origin& cache_storage_origin,
       const std::string& cache_storage_cache_name) override;
 
   // Helpers.
@@ -118,9 +119,6 @@ class CONTENT_EXPORT CodeCacheHostImpl : public blink::mojom::CodeCacheHost {
   scoped_refptr<GeneratedCodeCacheContext> generated_code_cache_context_;
 
   const net::NetworkIsolationKey network_isolation_key_;
-
-  // The storage key for the document that created this CodeCache.
-  const blink::StorageKey storage_key_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
--- content/test/fuzzer/code_cache_host_mojolpm_fuzzer.cc
+++ content/test/fuzzer/code_cache_host_mojolpm_fuzzer.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/quota/quota_manager.h"
 #include "storage/browser/quota/special_storage_policy.h"
 #include "storage/browser/test/mock_special_storage_policy.h"
-#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/loader/code_cache.mojom-mojolpm.h"
 #include "third_party/libprotobuf-mutator/src/src/libfuzzer/libfuzzer_macro.h"
 #include "url/origin.h"
@@ -87,8 +86,7 @@ class CodeCacheHostTestcase
       uint32_t id,
       int renderer_id,
       const Origin& origin,
-      const net::NetworkIsolationKey& nik,
-      const blink::StorageKey& storage_key,
+      const net::NetworkIsolationKey& key,
       mojo::PendingReceiver<::blink::mojom::CodeCacheHost>&& receiver);
 
   // Create and bind a new instance for fuzzing. This ensures that the new
@@ -252,10 +250,9 @@ void CodeCacheHostTestcase::AddCodeCacheHostImpl(
     int renderer_id,
     const Origin& origin,
     const net::NetworkIsolationKey& nik,
-    const blink::StorageKey& storage_key,
     mojo::PendingReceiver<::blink::mojom::CodeCacheHost>&& receiver) {
   auto code_cache_host = std::make_unique<content::CodeCacheHostImpl>(
-      renderer_id, generated_code_cache_context_, nik, storage_key);
+      renderer_id, generated_code_cache_context_, nik);
   code_cache_host->SetCacheStorageControlForTesting(
       cache_storage_control_wrapper_.get());
   UniqueCodeCacheReceiverSet receivers(
@@ -309,8 +306,7 @@ void CodeCacheHostTestcase::AddCodeCacheHost(
           FROM_HERE,
           base::BindOnce(&CodeCacheHostTestcase::AddCodeCacheHostImpl,
                          base::Unretained(this), id, renderer_id, *origin,
-                         net::NetworkIsolationKey(), blink::StorageKey(),
-                         std::move(receiver)),
+                         net::NetworkIsolationKey(), std::move(receiver)),
           base::BindOnce(AddCodeCacheHostInstance, id, std::move(remote),
                          std::move(run_closure)));
 }
