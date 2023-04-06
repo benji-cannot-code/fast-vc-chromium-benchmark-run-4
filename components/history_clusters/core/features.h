a274299b20b27a98995b6d7010ea3ee5c57b793c
Sophie Chang <sophiechang@chromium.org>
2023-04-06 18:10:25 +0000

    Add a cache in front of UrlForDisplay behind a feature flag
    Bug: b/276320968
    Change-Id: Ie7a677f97c5ce7e9eb216e46730465ed7d50d3cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404977
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127288}

--- components/history_clusters/core/features.h
+++ components/history_clusters/core/features.h
@@ -80,6 +80,10 @@ BASE_DECLARE_FEATURE(kHistoryClustersNavigationContextClustering);
 // of persisted clusters.
 BASE_DECLARE_FEATURE(kHideVisits);
 
+// Enables use of cache to store the URL for display to avoid calls to url
+// formatter.
+BASE_DECLARE_FEATURE(kUseUrlForDisplayCache);
+
 // Order consistently with config.h.
 
 }  // namespace internal
