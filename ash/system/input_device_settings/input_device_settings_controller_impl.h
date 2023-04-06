941254ac508487571a8ca6e0e7290cfc65730bfc
David Padlipsky <dpad@google.com>
2023-04-06 20:17:54 +0000

    Settings Split: Generate MousePolicies mojom objects to track policy
    Generates MousePolicies mojom structs which will be used to keep track
    of mouse enterprise policies on settings. These mojom objects will be
    sent to the Settings SWA so the Settings SWA can update the UI.
    
    Bug: b/241965700
    Change-Id: Ie59a45e979a3f07eee6aeceb48b4f1d76aad629a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382687
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127370}

--- ash/system/input_device_settings/input_device_settings_controller_impl.h
+++ ash/system/input_device_settings/input_device_settings_controller_impl.h
@@ -107,6 +107,7 @@ class ASH_EXPORT InputDeviceSettingsControllerImpl
 
   void InitializePolicyHandler();
   void OnKeyboardPoliciesChanged();
+  void OnMousePoliciesChanged();
 
   base::ObserverList<InputDeviceSettingsController::Observer> observers_;
 
--- ash/system/input_device_settings/input_device_settings_policy_handler.h
+++ ash/system/input_device_settings/input_device_settings_policy_handler.h
@@ -20,7 +20,8 @@ class ASH_EXPORT InputDeviceSettingsPolicyHandler {
   using EnterprisePolicyCallback = base::RepeatingClosure;
 
   explicit InputDeviceSettingsPolicyHandler(
-      EnterprisePolicyCallback keyboard_policy_callback);
+      EnterprisePolicyCallback keyboard_policy_callback,
+      EnterprisePolicyCallback mouse_policy_callback);
   InputDeviceSettingsPolicyHandler(const InputDeviceSettingsPolicyHandler&) =
       delete;
   InputDeviceSettingsPolicyHandler& operator=(
@@ -33,6 +34,8 @@ class ASH_EXPORT InputDeviceSettingsPolicyHandler {
     return keyboard_policies_;
   }
 
+  const mojom::MousePolicies& mouse_policies() const { return mouse_policies_; }
+
  private:
   void RefreshKeyboardPolicies(bool notify);
   void RefreshMousePolicies(bool notify);
@@ -41,10 +44,13 @@ class ASH_EXPORT InputDeviceSettingsPolicyHandler {
   void OnMousePoliciesChanged(const std::string& pref_name);
 
   EnterprisePolicyCallback keyboard_policy_callback_;
+  EnterprisePolicyCallback mouse_policy_callback_;
 
   mojom::KeyboardPolicies keyboard_policies_;
+  mojom::MousePolicies mouse_policies_;
 
-  // Used to track preferences which may be controlled by enterprise policies.
+  // Used to track preferences which may be controlled by enterprise
+  // policies.
   PrefChangeRegistrar pref_change_registrar_;
 };
 
