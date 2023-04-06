c4f83b897c378739f787bf3cbc650d531a143052
Michael Checo <michaelcheco@google.com>
2023-04-06 02:22:53 +0000

    Settings Split: Remove unused PrefService param from Retrieve*Settings
    Bug: b/241965700
    Change-Id: I760d1c8a4a2f3f70ea6ed36013030dd8ae7e7ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400649
    Commit-Queue: David Padlipsky <dpad@google.com>
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126982}

--- ash/system/input_device_settings/pref_handlers/mouse_pref_handler_impl.cc
+++ ash/system/input_device_settings/pref_handlers/mouse_pref_handler_impl.cc
@@ -99,7 +99,6 @@ mojom::MouseSettingsPtr GetMouseSettingsFromPrefs(
 }
 
 mojom::MouseSettingsPtr RetrieveMouseSettings(
-    PrefService* pref_service,
     const mojom::Mouse& mouse,
     const base::Value::Dict& settings_dict) {
   mojom::MouseSettingsPtr settings = mojom::MouseSettings::New();
@@ -212,8 +211,7 @@ void MousePrefHandlerImpl::InitializeMouseSettings(PrefService* pref_service,
   ForceMouseSettingPersistence force_persistence;
 
   if (settings_dict) {
-    mouse->settings =
-        RetrieveMouseSettings(pref_service, *mouse, *settings_dict);
+    mouse->settings = RetrieveMouseSettings(*mouse, *settings_dict);
   } else if (Shell::Get()->input_device_tracker()->WasDevicePreviouslyConnected(
                  InputDeviceTracker::InputDeviceCategory::kMouse,
                  mouse->device_key)) {
