efa37c9a0f2f99d9289d482491ae89528030757f
Viktor Semeniuk <vsemeniuk@google.com>
2023-04-06 20:12:57 +0000

    [Password Manager] Display "Install app" button in the omnibox
    Fixed: 1429579
    Change-Id: I82dd2f5e48f3c376bbc3c800ff21e29d405e5975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4406051
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127363}

--- chrome/browser/banners/app_banner_manager_desktop_browsertest.cc
+++ chrome/browser/banners/app_banner_manager_desktop_browsertest.cc
@@ -491,7 +491,7 @@ IN_PROC_BROWSER_TEST_F(AppBannerManagerDesktopBrowserTestForPasswordManagerPage,
     run_loop.Run();
   }
 
-  EXPECT_EQ(AppBannerManager::InstallableWebAppCheckResult::kYes_ByUserRequest,
+  EXPECT_EQ(AppBannerManager::InstallableWebAppCheckResult::kYes_Promotable,
             manager->GetInstallableWebAppCheckResultForTesting());
 }
 
