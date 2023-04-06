6a966988622ab2d2a9fa7b127dd521ccc30047f1
Jon Mann <jonmann@chromium.org>
2023-04-06 16:44:00 +0000

    Eche: Fix crash on loading animation ended.
    This change uses a WeakPtr for the loading icon in the animation
    to avoid a crash if the icon is already destroyed when the animation
    ended callback is run.
    
    TEST=Verified that the crash was no longer reproducible on DUT.
    
    Change-Id: I421a8436ea8e578d6ba89585eb1e78cc2804808f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404329
    Auto-Submit: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127241}

--- ash/system/phonehub/phone_hub_recent_apps_view.cc
+++ ash/system/phonehub/phone_hub_recent_apps_view.cc
@@ -335,6 +335,11 @@ const char* PhoneHubRecentAppsView::LoadingView::GetClassName() const {
   return "RecentAppLoadingView";
 }
 
+base::WeakPtr<PhoneHubRecentAppsView::LoadingView>
+PhoneHubRecentAppsView::LoadingView::GetWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
+}
+
 void PhoneHubRecentAppsView::LoadingView::StartLoadingAnimation() {
   for (size_t i = 0; i < app_loading_icons_.size(); i++) {
     app_loading_icons_[i]->StartLoadingAnimation(
@@ -461,7 +466,7 @@ void PhoneHubRecentAppsView::FadeOutLoadingView() {
 
     views::AnimationBuilder()
         .OnEnded(base::BindOnce(&LoadingView::SetVisible,
-                                base::Unretained(loading_view_), false))
+                                loading_view_->GetWeakPtr(), false))
         .Once()
         .SetOpacity(loading_view_, 1.0f)
         .SetOpacity(recent_app_buttons_view_, 0.0f)
