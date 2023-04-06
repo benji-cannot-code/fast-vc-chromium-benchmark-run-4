b90052d59d1830c18f07c812a3a09747f883f247
Andrew Grieve <agrieve@chromium.org>
2023-04-06 14:39:12 +0000

    Optimize binary size of semantic_tree.cc
    * Use 2 bytes per parent rather than sizeof(int).
    * Do not store version since it's always 1 for now.
    * Do not store message id (use index to derive it)
    
    This new format is not ideal for being able to handle changes
    to the data going forward. However, I think it's better to
    start with a more optimized structure.
    
    Bug: 1430527
    Change-Id: I3ea9dccf7495c04825143c9d226bb810231e3198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396064
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127186}

--- components/browsing_topics/common/semantic_tree.h
+++ components/browsing_topics/common/semantic_tree.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_BROWSING_TOPICS_COMMON_SEMANTIC_TREE_H_
 #define COMPONENTS_BROWSING_TOPICS_COMMON_SEMANTIC_TREE_H_
 
-#include <map>
-#include <set>
 #include <vector>
 
 #include "base/component_export.h"
@@ -24,18 +22,10 @@ class COMPONENT_EXPORT(BROWSING_TOPICS_COMMON) SemanticTree {
   SemanticTree();
   SemanticTree(const SemanticTree& other) = delete;
   ~SemanticTree();
-  std::set<Topic> GetDescendantTopics(const Topic& topic);
+  std::vector<Topic> GetDescendantTopics(const Topic& topic);
   std::vector<Topic> GetAncestorTopics(const Topic& topic);
   absl::optional<int> GetLocalizedNameMessageId(const Topic& topic,
                                                 int taxonomy_version);
-
- private:
-  void InitializeParentToChildTopicMap();
-
-  // `parent_to_child_topic_map_` is lazy initialized when GetDescendantTopics
-  // is first used
-  absl::optional<std::map<Topic, std::vector<Topic>>>
-      parent_to_child_topic_map_;
 };
 }  // namespace browsing_topics
 
