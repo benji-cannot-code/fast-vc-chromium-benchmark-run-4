13b6516c596fb1bd01a2f1e1afb61f184e57227d
Bastian Kersting <bkersting@google.com>
2023-04-06 14:23:30 +0000

    [Telemetry] Add event service to Lacros remotes
    Adds the TelemetryEventService to the list of constructed
    remotes in LacrosService to make it available in Lacros.
    
    Bug: b/268125453
    Change-Id: I03364c41283d0577d2e5697bcfa9e6e2ad61dee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394672
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127168}

--- chromeos/lacros/lacros_service.cc
+++ chromeos/lacros/lacros_service.cc
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/speech_recognition.mojom.h"
 #include "chromeos/crosapi/mojom/sync.mojom.h"
 #include "chromeos/crosapi/mojom/task_manager.mojom.h"
+#include "chromeos/crosapi/mojom/telemetry_event_service.mojom.h"
 #include "chromeos/crosapi/mojom/test_controller.mojom.h"
 #include "chromeos/crosapi/mojom/timezone.mojom.h"
 #include "chromeos/crosapi/mojom/tts.mojom.h"
@@ -491,6 +492,10 @@ LacrosService::LacrosService()
   ConstructRemote<crosapi::mojom::TaskManager,
                   &crosapi::mojom::Crosapi::BindTaskManager,
                   Crosapi::MethodMinVersions::kBindTaskManagerMinVersion>();
+  ConstructRemote<
+      crosapi::mojom::TelemetryEventService,
+      &Crosapi::BindTelemetryEventService,
+      Crosapi::MethodMinVersions::kBindTelemetryEventServiceMinVersion>();
   ConstructRemote<
       crosapi::mojom::TelemetryProbeService,
       &crosapi::mojom::Crosapi::BindTelemetryProbeService,
