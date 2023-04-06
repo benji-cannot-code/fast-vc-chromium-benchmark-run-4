51cf8c0420b033166dad74798f3cc199fd8d8122
danakj <danakj@chromium.org>
2023-04-06 21:56:46 +0000

    Remove Bindgen and its dependencies from third_party/rust.
    Bindgen is now built on the rust toolchain builders. It is not part of
    chromium and does not need to be in the chromium src repo anymore.
    
    R=lukasza@chromium.org
    
    Bug: 1426886
    Change-Id: I812bfba1b2a94b0d0dcde7a56d56c1e0770c80ba
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408004
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127418}

--- third_party/rust/cexpr/v0_6/crate/tests/input/chars.h
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#define CharChar_65 'A'
-#define CharChar_127849 '\U0001f369' // 🍩
-#define CharRaw_255 U'\xff'
--- third_party/rust/nom/v7/crate/build.rs
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-extern crate version_check;
-
-fn main() {
-  if version_check::is_min_version("1.44.0").unwrap_or(true) {
-    println!("cargo:rustc-cfg=stable_i128");
-  }
-}
