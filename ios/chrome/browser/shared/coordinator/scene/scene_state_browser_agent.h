eeda45cc999190cf5916b8843caf2a23605e0e4d
Gauthier Ambard <gambard@chromium.org>
2023-04-06 14:49:34 +0000

    [iOS] Prepare to move scene and layout_guide
    This CL prepares to moves all scene-related files out of
    ios/c/b/ui/main and in the shared folder instead.
    It also moves all the layout_guide files together.
    
    Bug: 1319852
    Change-Id: Ida08782333df9cae6d21858ec95d6634a9cba83f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402263
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127189}

---
+++ ios/chrome/browser/shared/coordinator/scene/scene_state_browser_agent.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_COORDINATOR_SCENE_SCENE_STATE_BROWSER_AGENT_H_
+#define IOS_CHROME_BROWSER_SHARED_COORDINATOR_SCENE_SCENE_STATE_BROWSER_AGENT_H_
+
+// Temporary include.
+#import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_COORDINATOR_SCENE_SCENE_STATE_BROWSER_AGENT_H_
