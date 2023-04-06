b16f55c4aacdbce1b64a3244c95e6fa90fb0e89c
Sylvain Defresne <sdefresne@chromium.org>
2023-04-06 11:40:05 +0000

    [ios] Remove snapshot storage migration
    This was introduced in https://crrev.com/c/2354838 to migrate the
    storage from a single location to a per-Browser location. As this
    has been active since M-87 which is far enough in the past that
    the migration is no longer necessary.
    
    Keep SnapshotBrowserAgent::MigrateStorageIfNecessary() even though
    it is empty as a new migration will be added as a followup (to use
    an identifier derived from GetUniqueIdentifier).
    
    Bug: 1430399
    Change-Id: I7dce79704fa0e01ba9b4e18e3c95a107e169569f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396060
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127113}

--- ios/chrome/browser/snapshots/snapshot_browser_agent.mm
+++ ios/chrome/browser/snapshots/snapshot_browser_agent.mm
@@ -20,13 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-namespace {
-
-const base::FilePath::CharType kLegacyBaseDirectory[] =
-    FILE_PATH_LITERAL("Chromium");
-
-}  // namespace
-
 BROWSER_USER_DATA_KEY_IMPL(SnapshotBrowserAgent)
 
 SnapshotBrowserAgent::SnapshotBrowserAgent(Browser* browser)
@@ -104,14 +97,6 @@ void SnapshotBrowserAgent::RemoveAllSnapshots() {
 
 void SnapshotBrowserAgent::MigrateStorageIfNecessary() {
   DCHECK(snapshot_cache_);
-  base::FilePath legacy_directory;
-  DCHECK(base::PathService::Get(base::DIR_CACHE, &legacy_directory));
-  legacy_directory = legacy_directory.Append(kLegacyBaseDirectory)
-                         .Append(kSnapshotsDirectoryName);
-  // The legacy directory is deleted in migration, and migration is NO-OP if
-  // directory does not exist.
-  [snapshot_cache_ migrateSnapshotsWithIDs:GetTabIDs()
-                            fromSourcePath:legacy_directory];
 }
 
 void SnapshotBrowserAgent::PurgeUnusedSnapshots() {
