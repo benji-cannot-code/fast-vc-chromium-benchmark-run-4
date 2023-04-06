495638bf4c37ba32d8eb0c4e20e632f7131eabf4
Will Harris <wfh@chromium.org>
2023-04-06 21:53:45 +0000

    Add crash key for dll name when a delay load fails
    Bug: b:277234803
    Change-Id: I5c3b6152f4547903cd39e135bb7439b59fca0cef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4406876
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127415}

--- chrome/common/win/delay_load_failure_support.cc
+++ chrome/common/win/delay_load_failure_support.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/debug/alias.h"
+#include "base/debug/crash_logging.h"
 #include "base/process/memory.h"
 
 namespace chrome {
@@ -22,7 +23,8 @@ FARPROC WINAPI HandleDelayLoadFailureCommon(unsigned reason,
     base::TerminateBecauseOutOfMemory(0);
   }
 
-  DEBUG_ALIAS_FOR_CSTR(dll_name, dll_info->szDll, MAX_PATH);
+  DEBUG_ALIAS_FOR_CSTR(dll_name, dll_info->szDll, 256);
+  SCOPED_CRASH_KEY_STRING256("DelayLoad", "ModuleName", dll_name);
 
   // Deterministically crash here. Returning 0 from the hook would likely result
   // in the process crashing anyway, but in a form that might trigger undefined
