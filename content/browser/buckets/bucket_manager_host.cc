2d875585ce6140527851e2d630d4e479628b4d82
Evan Stade <estade@chromium.org>
2023-04-06 00:57:31 +0000

    Storage buckets: add histograms for open() parameter values.
    Bug: 1425563
    Change-Id: I2a1846dd984f6af1a9a37d81d0ceda9af8944308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368206
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126945}

--- content/browser/buckets/bucket_manager_host.cc
+++ content/browser/buckets/bucket_manager_host.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/buckets/bucket_manager_host.h"
 
 #include "base/containers/contains.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/types/pass_key.h"
 #include "components/services/storage/public/cpp/buckets/bucket_info.h"
@@ -17,6 +19,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 
 namespace content {
+
+// These enums are used in metrics. Do not reorder or change their values.
+// Append new values to the end.
+enum class DurabilityMetric {
+  kNoneProvided = 0,
+  kRelaxed = 1,
+  kStrict = 2,
+  kMaxValue = kStrict,
+};
+enum class PersistenceMetric {
+  kNoneProvided = 0,
+  kNotPersisted = 1,
+  kPersisted = 2,
+  kMaxValue = kPersisted,
+};
 
 BucketManagerHost::BucketManagerHost(BucketManager* manager,
                                      const blink::StorageKey& storage_key)
@@ -75,6 +92,34 @@ void BucketManagerHost::OpenBucket(const std::string& name,
         params.persistent = policies->persisted;
       }
     }
+
+    // Count the number of minutes until expiration. This doesn't use the TIMES
+    // histogram variant because that counts in milliseconds and caps the max at
+    // ~24 days. Note that negative counts are logged as zero, so all
+    // expirations less than one minute in the future (including none specified)
+    // will be logged in the underflow bucket. Max duration we care about is 500
+    // days.
+    base::UmaHistogramCustomCounts(
+        "Storage.Buckets.Parameters.Expiration",
+        (params.expiration - base::Time::Now()).InMinutes(), 1,
+        base::Days(500).InMinutes(), 50);
+    // Convert quota to kB before logging.
+    base::UmaHistogramCustomCounts("Storage.Buckets.Parameters.QuotaKb",
+                                   params.quota / 1024, 1,
+                                   /* 20 GB */ 20L * 1024 * 1024, 50);
+    base::UmaHistogramEnumeration(
+        "Storage.Buckets.Parameters.Durability",
+        policies->has_durability
+            ? policies->durability == blink::mojom::BucketDurability::kStrict
+                  ? DurabilityMetric::kStrict
+                  : DurabilityMetric::kRelaxed
+            : DurabilityMetric::kNoneProvided);
+    base::UmaHistogramEnumeration("Storage.Buckets.Parameters.Persisted",
+                                  policies->has_persisted
+                                      ? policies->persisted
+                                            ? PersistenceMetric::kPersisted
+                                            : PersistenceMetric::kNotPersisted
+                                      : PersistenceMetric::kNoneProvided);
   }
 
   GetQuotaManagerProxy()->UpdateOrCreateBucket(
