b2bf85caeefd43a8e47f7fa0b1f7103cceba51d8
Aaron Leventhal <aleventhal@google.com>
2023-04-06 20:30:21 +0000

    Remove unused overload RequestAXTreeSnapshot()
    Another signature of RequestAXTreeSnapshot() that takes different parameters is used, and is kept.
    
    Bug: none
    Change-Id: I48f7c2313916bd303fac8696c97bcd96f654e3e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4403627
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127377}

--- content/public/browser/render_frame_host.h
+++ content/public/browser/render_frame_host.h
@@ -51,7 +51,6 @@ class JavaRef;
 }  // namespace android
 #endif
 
-class TimeDelta;
 class UnguessableToken;
 }  // namespace base
 
@@ -98,7 +97,6 @@ class InterfaceProvider;
 namespace ui {
 struct AXActionData;
 struct AXTreeUpdate;
-class AXMode;
 class AXTreeID;
 }  // namespace ui
 
@@ -200,13 +198,6 @@ class CONTENT_EXPORT RenderFrameHost : public IPC::Listener,
 
   using AXTreeSnapshotCallback =
       base::OnceCallback<void(const ui::AXTreeUpdate&)>;
-  // Request a one-time snapshot of the accessibility tree without changing
-  // the accessibility mode.
-  virtual void RequestAXTreeSnapshot(AXTreeSnapshotCallback callback,
-                                     const ui::AXMode& ax_mode,
-                                     bool exclude_offscreen,
-                                     size_t max_nodes,
-                                     const base::TimeDelta& timeout) = 0;
 
   // Returns the SiteInstance grouping all RenderFrameHosts that have script
   // access to this RenderFrameHost, and must therefore live in the same
