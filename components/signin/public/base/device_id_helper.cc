493c699da46d5aa512fe7184b738671d9c4382f9
Claudio DeSouza <cdesouza@igalia.com>
2023-04-06 08:57:46 +0000

    Replace base::GUID uses with base::Uuid (40/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Changes for path: /components/signin
    
    This CL was uploaded by git cl split.
    
    R=msarda@chromium.org
    
    Bug: 1428566
    Change-Id: I3ebada64df2972471b6e7ad29bcc0eef44784bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4392578
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127072}

--- components/signin/public/base/device_id_helper.cc
+++ components/signin/public/base/device_id_helper.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/command_line.h"
-#include "base/guid.h"
+#include "base/uuid.h"
 #include "build/chromeos_buildflags.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
@@ -35,7 +35,7 @@ std::string RecreateSigninScopedDeviceId(PrefService* prefs) {
 }
 
 std::string GenerateSigninScopedDeviceId() {
-  return base::GenerateGUID();
+  return base::Uuid::GenerateRandomV4().AsLowercaseString();
 }
 
 std::string GetOrCreateScopedDeviceId(PrefService* prefs) {
