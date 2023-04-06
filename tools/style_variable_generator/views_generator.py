2dbf398e521105ccded27edf3c533ec91de9710b
Zain Afzal <zafzal@google.com>
2023-04-06 06:08:24 +0000

    [Jellybean] Allow for json5 color files to force preblending.
    https://chromium-review.googlesource.com/c/chromium/src/+/4349545 made
    the CSSGenerator output blended colors as color-mix calls. However this
    assumes that the target browser supports color-mix which as of writing
    is untrue for non chrome browsers and chrome pre M111.
    
    This CL adds the option for a json5 file to force preblending for all
    of it's colors, allowing our legacy colors to remain preblended and
    usable on older and none chrome browsers. Note that newer cros tokens
    do not specify preblending as a dynamic palette has a hard requirement
    on color-mix calls.
    
    Change-Id: I7c396db0793c902c2198f3a78ff920c154b1e6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402061
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127028}

--- tools/style_variable_generator/views_generator.py
+++ tools/style_variable_generator/views_generator.py
@@ -58,7 +58,7 @@ class ViewsStyleGenerator(CSSStyleGenerator):
 
         return globals
 
-    def ShouldResolveBlendedColors(self):
+    def DefaultPreblend(self):
         # CSSGenerator sets this to false, we set it back to true since
         # views does not do runtime blends.
         return True
