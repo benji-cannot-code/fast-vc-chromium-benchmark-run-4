39bd59eabf1034b642bd2d84e5a7da13a3909b6d
Javier Contreras Tenorio <javiercon@microsoft.com>
2023-04-07 17:13:08 +0000

    [a11y] Removing TODO about AOM
    This CL removes two TODO comments that are no longer relevant.
    
    Change-Id: If04488d2bb1e741f779cefa95b35625cefa10cd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4406877
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1127621}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -6398,7 +6398,6 @@ bool AXObject::RequestShowContextMenuAction() {
 }
 
 bool AXObject::RequestExpandAction() {
-  // TODO(accessibility): Handle AOM event dispatching if needed.
   if (ui::SupportsArrowKeysForExpandCollapse(RoleValue())) {
     return OnNativeKeyboardAction(ax::mojom::blink::Action::kExpand);
   }
@@ -6406,7 +6405,6 @@ bool AXObject::RequestExpandAction() {
 }
 
 bool AXObject::RequestCollapseAction() {
-  // TODO(accessibility): Handle AOM event dispatching if needed.
   if (ui::SupportsArrowKeysForExpandCollapse(RoleValue())) {
     return OnNativeKeyboardAction(ax::mojom::blink::Action::kCollapse);
   }
