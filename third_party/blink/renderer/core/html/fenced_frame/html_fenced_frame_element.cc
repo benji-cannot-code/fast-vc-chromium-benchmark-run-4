23ca7f42f178ab0ba91c1719fba59838f54b87c5
Liam Brady <lbrady@google.com>
2023-04-07 17:17:05 +0000

    Remove src attribute from fenced frames. [6/N]
    Fenced frames are planned to only be allowed to load using a config
    object. Because of that, loading using a `src` will no longer be
    supported. This CL removes the ability to load with the `src` attribute.
    Prior CLs handled updating tests to account for this change, which
    allows this change to be small.
    
    Bug: 1428960
    Change-Id: I07fce91ab4e8d9d514e02d7c1a50ed8afd23f81b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404944
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127628}

--- third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element.cc
+++ third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element.cc
@@ -416,20 +416,7 @@ void HTMLFencedFrameElement::RemovedFrom(ContainerNode& node) {
 
 void HTMLFencedFrameElement::ParseAttribute(
     const AttributeModificationParams& params) {
-  if (params.name == html_names::kSrcAttr) {
-    if (config_) {
-      DCHECK(config_->url());
-      GetDocument().AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
-          mojom::blink::ConsoleMessageSource::kJavaScript,
-          mojom::blink::ConsoleMessageLevel::kWarning,
-          "Changing the `src` attribute on a fenced frame has no effect after "
-          "it has already been installed a config with a specified url."));
-      return;
-    }
-
-    KURL url = GetNonEmptyURLAttribute(html_names::kSrcAttr);
-    Navigate(url);
-  } else if (params.name == html_names::kSandboxAttr) {
+  if (params.name == html_names::kSandboxAttr) {
     sandbox_->DidUpdateAttributeValue(params.old_value, params.new_value);
 
     network::mojom::blink::WebSandboxFlags current_flags =
@@ -468,10 +455,6 @@ void HTMLFencedFrameElement::ParseAttribute(
   }
 }
 
-bool HTMLFencedFrameElement::IsURLAttribute(const Attribute& attribute) const {
-  return attribute.GetName() == html_names::kSrcAttr;
-}
-
 bool HTMLFencedFrameElement::IsPresentationAttribute(
     const QualifiedName& name) const {
   if (name == html_names::kWidthAttr || name == html_names::kHeightAttr)
@@ -507,7 +490,7 @@ void HTMLFencedFrameElement::Navigate(
   // prerendering. If this function is called while prerendering we won't have a
   // delegate and will bail early, but this should still be correct since,
   // post-activation, CreateDelegateAndNavigate will be run which will navigate
-  // to the most current src.
+  // to the most current config.
   if (!frame_delegate_)
     return;
 
@@ -647,7 +630,7 @@ void HTMLFencedFrameElement::CreateDelegateAndNavigate() {
   // prerendering, but we should only actually create the delegate once. Note,
   // this will also mean that we skip calling Navigate() again, but the result
   // should still be correct since the first Navigate call will use the
-  // up-to-date src.
+  // up-to-date config.
   if (frame_delegate_)
     return;
   if (GetDocument().IsPrerendering()) {
@@ -661,8 +644,6 @@ void HTMLFencedFrameElement::CreateDelegateAndNavigate() {
 
   if (config_) {
     NavigateToConfig();
-  } else {
-    Navigate(GetNonEmptyURLAttribute(html_names::kSrcAttr));
   }
 }
 
