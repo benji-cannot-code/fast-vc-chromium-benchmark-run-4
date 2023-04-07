ab35575cb6173478d435b36979c198277baf7834
Nikhil Nayunigari <nikhilcn@google.com>
2023-04-07 20:15:08 +0000

    [CrOS Hotspot] Notify when WiFi is turned on by the user
    Enabling WiFi will disable active hotspot and these changes will notify
    the user. Added a unit test for the case where the hotspot is disabled
    by the admin and hotspot config test helper to support unit tests.
    
    Screenshot: https://screenshot.googleplex.com/7yQnBH3VTtD74a7
    
    Bug: b/269354043
    Change-Id: Id41d9f63ae6b9ddb9d6ed2c8ff54c6dcbf9d5feb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4332976
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127740}

---
+++ chromeos/ash/services/hotspot_config/public/cpp/cros_hotspot_config_test_helper.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_SERVICES_HOTSPOT_CONFIG_PUBLIC_CPP_CROS_HOTSPOT_CONFIG_TEST_HELPER_H_
+#define CHROMEOS_ASH_SERVICES_HOTSPOT_CONFIG_PUBLIC_CPP_CROS_HOTSPOT_CONFIG_TEST_HELPER_H_
+
+#include <memory>
+#include "chromeos/ash/services/hotspot_config/cros_hotspot_config.h"
+
+namespace ash {
+
+namespace hotspot_config {
+
+class CrosHotspotConfig;
+
+// Helper for tests which need a CrosHotspotConfig service interface.
+class CrosHotspotConfigTestHelper {
+ public:
+  // Default constructor for unit tests.
+  CrosHotspotConfigTestHelper();
+
+  CrosHotspotConfigTestHelper(const CrosHotspotConfigTestHelper&) = delete;
+  CrosHotspotConfigTestHelper& operator=(const CrosHotspotConfigTestHelper&) =
+      delete;
+
+  ~CrosHotspotConfigTestHelper();
+
+  void EnableHotspot();
+
+ protected:
+  // Called in |~CrosHotspotConfigTestHelper()| to destroy
+  // cros_hotspot_config_impl_.
+  void Shutdown();
+
+  std::unique_ptr<CrosHotspotConfig> cros_hotspot_config_impl_;
+};
+
+}  // namespace hotspot_config
+
+}  // namespace ash
+
+#endif  // CHROMEOS_ASH_SERVICES_HOTSPOT_CONFIG_PUBLIC_CPP_CROS_HOTSPOT_CONFIG_TEST_HELPER_H_
