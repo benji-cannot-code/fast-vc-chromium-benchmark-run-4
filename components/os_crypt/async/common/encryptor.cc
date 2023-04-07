71b568c88a742a36571d17c52d438b9305ec5f1b
Will Harris <wfh@chromium.org>
2023-04-07 03:25:43 +0000

    Add mojo traits for transferring a os_crypt_async::Encryptor.
    The mojo Algorithm enum is used instead of defining the same
    one in C++, and for consistency, the `algo_` field is renamed
    uniformly between the C++ and mojo implementations.
    
    Tests are added to verify that serialization works for both an
    Encryptor loaded with keys, and an empty one.
    
    BUG=1373092
    
    Change-Id: Idf041529677cc3711cc61c883e6a35520757b588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401064
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127510}

--- components/os_crypt/async/common/encryptor.cc
+++ components/os_crypt/async/common/encryptor.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "base/ranges/algorithm.h"
+#include "components/os_crypt/async/common/algorithm.mojom.h"
 #include "components/os_crypt/sync/os_crypt.h"
 #include "crypto/aead.h"
 #include "crypto/random.h"
@@ -24,14 +26,21 @@ constexpr size_t kNonceLength = 96 / 8;  // AES_GCM_NONCE_LENGTH
 
 }  // namespace
 
-Encryptor::Key::Key(base::span<const uint8_t> key, const Algorithm& algo)
-    : algo_(algo), key_(key.begin(), key.end()) {
-  switch (algo_) {
-    case Key::Algorithm::kAES256GCM:
+Encryptor::Key::Key(base::span<const uint8_t> key,
+                    const mojom::Algorithm& algorithm)
+    : algorithm_(algorithm), key_(key.begin(), key.end()) {
+  if (!algorithm_.has_value()) {
+    NOTREACHED_NORETURN();
+  }
+
+  switch (*algorithm_) {
+    case mojom::Algorithm::kAES256GCM:
       CHECK_EQ(key.size(), Key::kAES256GCMKeySize);
       break;
   }
 }
+
+Encryptor::Key::Key() = default;
 
 Encryptor::Key::Key(Key&& other) = default;
 Encryptor::Key& Encryptor::Key::operator=(Key&& other) = default;
@@ -39,7 +48,7 @@ Encryptor::Key& Encryptor::Key::operator=(Key&& other) = default;
 Encryptor::Key::~Key() = default;
 
 Encryptor::Key Encryptor::Key::Clone() const {
-  return Key(key_, algo_);
+  return Key(key_, *algorithm_);
 }
 
 Encryptor::Encryptor() = default;
@@ -54,8 +63,12 @@ Encryptor::~Encryptor() = default;
 
 std::vector<uint8_t> Encryptor::Key::Encrypt(
     base::span<const uint8_t> plaintext) const {
-  switch (algo_) {
-    case Key::Algorithm::kAES256GCM: {
+  if (!algorithm_.has_value()) {
+    NOTREACHED_NORETURN();
+  }
+
+  switch (*algorithm_) {
+    case mojom::Algorithm::kAES256GCM: {
       crypto::Aead aead(crypto::Aead::AES_256_GCM);
       aead.Init(key_);
 
@@ -72,13 +85,16 @@ std::vector<uint8_t> Encryptor::Key::Encrypt(
       return ciphertext;
     }
   }
-  LOG(FATAL) << "Unsupported algorithm" << static_cast<int>(algo_);
+  LOG(FATAL) << "Unsupported algorithm" << static_cast<int>(*algorithm_);
 }
 
 absl::optional<std::vector<uint8_t>> Encryptor::Key::Decrypt(
     base::span<const uint8_t> ciphertext) const {
-  switch (algo_) {
-    case Key::Algorithm::kAES256GCM: {
+  if (!algorithm_.has_value()) {
+    NOTREACHED_NORETURN();
+  }
+  switch (*algorithm_) {
+    case mojom::Algorithm::kAES256GCM: {
       if (ciphertext.size() < kNonceLength) {
         return absl::nullopt;
       }
@@ -92,7 +108,7 @@ absl::optional<std::vector<uint8_t>> Encryptor::Key::Decrypt(
       return aead.Open(data, nonce, /*additional_data=*/{});
     }
   }
-  LOG(FATAL) << "Unsupported algorithm" << static_cast<int>(algo_);
+  LOG(FATAL) << "Unsupported algorithm" << static_cast<int>(*algorithm_);
 }
 
 bool Encryptor::EncryptString(const std::string& plaintext,
