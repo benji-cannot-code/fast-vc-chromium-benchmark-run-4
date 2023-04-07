8e76aa43bf0794a6251b555600dd1bec335f7722
dpapad <dpapad@chromium.org>
2023-04-07 18:39:47 +0000

    [Code Health Rotation] Remove expired History histograms, part 1.
    History.InMemoryDBKeywordTermsCount
    History.InMemoryDBKeywordURLItemCount
    
    Fixed: 1384854
    Change-Id: I8afea9c507e8ad13fb42a166f3991e87ec2784e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4409494
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127693}

--- components/history/core/browser/in_memory_database.cc
+++ components/history/core/browser/in_memory_database.cc
@@ -104,8 +104,6 @@ bool InMemoryDatabase::InitFromDisk(const base::FilePath& history_name) {
     // Unable to get data from the history database. This is OK, the file may
     // just not exist yet.
   }
-  UMA_HISTOGRAM_COUNTS_1M("History.InMemoryDBKeywordURLItemCount",
-                          db_.GetLastChangeCount());
 
   // Copy search terms to memory.
   if (!db_.Execute(
@@ -114,8 +112,6 @@ bool InMemoryDatabase::InitFromDisk(const base::FilePath& history_name) {
     // Unable to get data from the history database. This is OK, the file may
     // just not exist yet.
   }
-  UMA_HISTOGRAM_COUNTS_1M("History.InMemoryDBKeywordTermsCount",
-                          db_.GetLastChangeCount());
 
   // Detach from the history database on disk.
   if (!db_.Execute("DETACH history")) {
