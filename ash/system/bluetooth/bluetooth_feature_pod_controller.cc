cf74e0ffa79dfde3db830aedf127cad6bbbb2d9a
James Cook <jamescook@chromium.org>
2023-04-07 17:34:52 +0000

    chromeos: Fix transient incorrect bluetooth state in quick settings
    Querying the Bluetooth adapter state is asynchronous, but quick
    settings needs to know the state synchronously when constructing its
    bubble. Create a cache of the adapter state, initially populated at
    system start, so quick settings can query it synchronously.
    
    Bug: b:266996235
    Test: added to ash_unittests
    Change-Id: I2f0efb6fb73530a4541466473556f1cf912ce0cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4390922
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127659}

--- ash/system/bluetooth/bluetooth_feature_pod_controller.cc
+++ ash/system/bluetooth/bluetooth_feature_pod_controller.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/bluetooth_config_service.h"
 #include "ash/public/cpp/hats_bluetooth_revamp_trigger.h"
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/system/bluetooth/bluetooth_state_cache.h"
 #include "ash/system/unified/feature_pod_button.h"
 #include "ash/system/unified/feature_tile.h"
 #include "ash/system/unified/quick_settings_metrics_util.h"
@@ -36,9 +38,9 @@ using bluetooth_config::mojom::DeviceConnectionState;
 
 BluetoothSystemState GetInitialSystemState() {
   if (features::IsQsRevampEnabled()) {
-    // Ensure the feature tile is visible while waiting for the async mojo query
-    // in the constructor. This simplifies the initial QS layout.
-    return BluetoothSystemState::kEnabled;
+    // Synchronously query the initial state so the feature tile doesn't flash
+    // with the wrong state. See b/266996235
+    return Shell::Get()->bluetooth_state_cache()->system_state();
   } else {
     return BluetoothSystemState::kUnavailable;
   }
