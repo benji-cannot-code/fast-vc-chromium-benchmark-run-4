a80225f7c3293f65002a80f44a0769bd5a297f4b
Yue Zhang <yuezhanggg@google.com>
2023-04-07 17:07:55 +0000

    [ChromeCart] Introduce pending deletion tracking and restoring
    This CL adds capability to restore pending deletions using old proto
    when needed. This will fix discount information being wrongly deleted
    due to ephemeral cart deletions. This CL also makes the deletion pending time shorter as long pending time leaves more room of error for
    consecutive deletions.
    
    Bug: 1429339, b/275753158
    Change-Id: Id27116a472990fe90299b84aab5044c37b251ff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402336
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127612}

--- chrome/browser/cart/cart_service.h
+++ chrome/browser/cart/cart_service.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CART_CART_SERVICE_H_
 #define CHROME_BROWSER_CART_CART_SERVICE_H_
 
+#include "base/containers/flat_map.h"
 #include "base/functional/callback_helpers.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
@@ -256,8 +257,7 @@ class CartService : public history::HistoryServiceObserver,
       base::OnceCallback<void(bool)> callback,
       bool success,
       std::vector<CartDB::KeyAndValue> proto_pairs);
-  void CheckCartExistenceAfterDeletion(GURL url);
-  void MaybeDeleteCoupons(GURL url, bool has_cart);
+  void MaybeCommitDeletion(GURL url);
 
   raw_ptr<Profile> profile_;
   std::unique_ptr<CartDB> cart_db_;
@@ -275,6 +275,8 @@ class CartService : public history::HistoryServiceObserver,
   raw_ptr<CouponService> coupon_service_;
   PrefChangeRegistrar pref_change_registrar_;
   raw_ptr<commerce::ShoppingService, DanglingUntriaged> shopping_service_;
+  base::flat_map<std::string, cart_db::ChromeCartContentProto>
+      pending_deletion_map_;
   base::WeakPtrFactory<CartService> weak_ptr_factory_{this};
 };
 
