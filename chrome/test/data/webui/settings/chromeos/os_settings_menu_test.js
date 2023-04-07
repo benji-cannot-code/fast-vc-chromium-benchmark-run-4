36d2c901c4d1e5b537f16f6627833f0bef62f440
Wes Okuhara <wesokuhara@google.com>
2023-04-07 12:00:29 +0000

    CrOS Settings: Use dom-repeat to render menu items
    Utilizes dom-repeat to loop and render menu items to adhere to DRY.
    Done in preparation for easily adding HTML attributes to the elements.
    
    Also removes some attributes that were not actively used.
    
    Bug: b/263413626
    Test: Manual UI test
    Change-Id: I07cc2eafce85d1da7687eca857d9c0e86bde0484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375516
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127587}

--- chrome/test/data/webui/settings/chromeos/os_settings_menu_test.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_menu_test.js
@@ -26,7 +26,7 @@ function setupRouter() {
   routes.BASIC = testRoutes.BASIC;
 }
 
-suite('OSSettingsMenu', function() {
+suite('OsSettingsMenu', function() {
   let settingsMenu = null;
 
   setup(function() {
@@ -45,11 +45,11 @@ suite('OSSettingsMenu', function() {
     assertFalse(settingsMenu.advancedOpened);
     settingsMenu.advancedOpened = true;
     flush();
-    assertTrue(settingsMenu.isAdvancedSubmenuOpenedForTest());
+    assertTrue(settingsMenu.$.advancedSubmenu.opened);
 
     settingsMenu.advancedOpened = false;
     flush();
-    assertFalse(settingsMenu.isAdvancedSubmenuOpenedForTest());
+    assertFalse(settingsMenu.$.advancedSubmenu.opened);
   });
 
   test('tapAdvanced', function() {
@@ -61,11 +61,11 @@ suite('OSSettingsMenu', function() {
 
     advancedToggle.click();
     flush();
-    assertTrue(settingsMenu.isAdvancedSubmenuOpenedForTest());
+    assertTrue(settingsMenu.$.advancedSubmenu.opened);
 
     advancedToggle.click();
     flush();
-    assertFalse(settingsMenu.isAdvancedSubmenuOpenedForTest());
+    assertFalse(settingsMenu.$.advancedSubmenu.opened);
   });
 
   test('upAndDownIcons', function() {
