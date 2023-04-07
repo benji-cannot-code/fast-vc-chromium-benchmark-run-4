051072f10669366eb77734358b69a1dbcbc04583
Austin Sullivan <asully@chromium.org>
2023-04-07 17:12:03 +0000

    fsa: Add WPT confirming the name of the OPFS root is the empty string
    See step 3.2 of this algorithm in the spec:
    https://fs.spec.whatwg.org/#dom-storagemanager-getdirectory
    
    No Chromium bug for this, but I filed a bug against Firefox since they
    currently return a handle whose name is "root"
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1826886
    
    Change-Id: Ie160ac6571cd287629893239e1df1d97c35b5d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408007
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127619}

---
+++ third_party/blink/web_tests/external/wpt/fs/root-name.https.any.js
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+promise_test(async test => {
+  let root = await navigator.storage.getDirectory();
+  assert_equals(root.name, '');
+}, 'getDirectory returns a directory whose name is the empty string');
