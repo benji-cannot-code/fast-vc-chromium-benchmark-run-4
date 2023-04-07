5f2551711627d1bebb8e5fbd30fa5f1787a25fa3
Solomon Kinard <solomonkinard@chromium.org>
2023-04-07 02:10:17 +0000

    Extensions: FileHandlers: LaunchQueue: Enqueue with LaunchParams
    Place launchQueue in JavaScript to allow access to FileSystemFileHandle.
    
    Bridge between browser `WebContents` and JavaScript `launchParams`. A
    new file was created even though it's only called once for separation of
    concerns and not adding more lines of code and #include lines to its
    callsite.
    
    Bug: 1179530
    Change-Id: Ib965a850d262d0647d9e1d16e8660afa0530ea73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290373
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127487}

--- chrome/browser/ui/extensions/application_launch.cc
+++ chrome/browser/ui/extensions/application_launch.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/platform_apps/platform_app_launch.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_util.h"
+#include "chrome/browser/extensions/file_handlers/file_handling_launch_utils.h"
 #include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
@@ -42,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
 #include "chrome/common/url_constants.h"
+#include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
@@ -330,12 +332,14 @@ WebContents* OpenEnabledApplication(Profile* profile,
                             params.container);
 
   GURL url;
-  if (extensions::WebFileHandlers::SupportsWebFileHandlers(
-          extension->manifest_version()) &&
-      params.intent->activity_name.has_value()) {
+  bool supports_web_file_handlers =
+      extensions::WebFileHandlers::SupportsWebFileHandlers(
+          extension->manifest_version());
+  if (supports_web_file_handlers && params.intent->activity_name.has_value()) {
     // `params.intent->activity_name` is actually the `action` url set in the
     // manifest of the extension.
     url = extension->GetResourceURL(params.intent->activity_name.value());
+    params.container = apps::LaunchContainer::kLaunchContainerWindow;
   } else {
     url = UrlForExtension(extension, profile, params);
   }
@@ -361,6 +365,11 @@ WebContents* OpenEnabledApplication(Profile* profile,
     default:
       NOTREACHED();
       break;
+  }
+
+  if (supports_web_file_handlers) {
+    extensions::EnqueueLaunchParamsInWebContents(tab, *extension, url,
+                                                 params.launch_files);
   }
 
   return tab;
@@ -433,8 +442,9 @@ WebContents* NavigateApplicationWindow(Browser* browser,
 
   WebContents* const web_contents = nav_params.navigated_or_inserted_contents;
 
-  if (extension) {
-    DCHECK(extension->is_app());
+  // Before MV3, an extension reaching this point must have been an app. MV3
+  // added support for Web File Handlers, which don't use extension TabHelper.
+  if (extension && extension->is_app()) {
     extensions::TabHelper::FromWebContents(web_contents)
         ->SetExtensionApp(extension);
   }
