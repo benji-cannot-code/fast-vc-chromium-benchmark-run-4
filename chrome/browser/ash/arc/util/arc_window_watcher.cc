9fd78faaedb9242a9d64d61b418f8756475784ed
Alex Gimenez <raging@google.com>
2023-04-07 02:06:08 +0000

    Add support for window launch doze unblocking
    During the time between a launch request and a window popping up on the
    screen, we must not allow either of:
    
    a) doze mode (which would prevent window from showing
    b) CPU restriction (which would increase visual lag)
    
    TEST=manual feature test (doze mode enter/exit based on window behavior)
    
    Low-Coverage-Reason:Unit tests coming in later CLs (one in this chain)
    
    Bug: b:258072954
    Change-Id: Ifcdba0cb70e594cf373ccb87bb77d85860205e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4376007
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Auto-Submit: Alexandre Marciano Gimenez <raging@google.com>
    Quick-Run: Alexandre Marciano Gimenez <raging@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Alexandre Marciano Gimenez <raging@google.com>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127486}

--- chrome/browser/ash/arc/util/arc_window_watcher.cc
+++ chrome/browser/ash/arc/util/arc_window_watcher.cc
@@ -45,6 +45,19 @@ class Tracker : public aura::WindowObserver {
 
   ~Tracker() override { window_->RemoveObserver(this); }
 
+  void OnPackageNameChanged() {
+    if (display_reported_) {
+      // Must not do this more than once
+      return;
+    }
+    const auto* pkg_name = arc_window_->GetProperty(ash::kArcPackageNameKey);
+    if (!pkg_name || pkg_name->empty()) {
+      return;
+    }
+    display_reported_ = true;
+    ash::ArcWindowWatcher::instance()->BroadcastArcWindowDisplay(*pkg_name);
+  }
+
   void MaybeTagArcWindow() {
     if (!ash::IsArcWindow(window_)) {
       return;
@@ -64,6 +77,9 @@ class Tracker : public aura::WindowObserver {
                                const void* key,
                                intptr_t old) override {
     if (arc_window_) {
+      if (key == ash::kArcPackageNameKey) {
+        OnPackageNameChanged();
+      }
       return;
     }
 
@@ -83,6 +99,7 @@ class Tracker : public aura::WindowObserver {
   aura::Window* window_;
   aura::Window* arc_window_ =
       nullptr;  // set to window_ when we know it is ARC.
+  bool display_reported_ = false;
 };
 
 }  // namespace
@@ -170,13 +187,24 @@ void ArcWindowWatcher::RemoveObserver(ArcWindowCountObserver* observer) {
   arc_window_count_observers_.RemoveObserver(observer);
 }
 
-bool ArcWindowWatcher::HasObservers() const {
-  return !arc_window_count_observers_.empty();
-}
-
 void ArcWindowWatcher::BroadcastArcWindowCount(uint32_t count) {
   for (auto& observer : arc_window_count_observers_) {
     observer.OnArcWindowCountChanged(count);
+  }
+}
+
+// Manage the list of arc-window-display observers.
+void ArcWindowWatcher::AddObserver(ArcWindowDisplayObserver* observer) {
+  arc_window_display_observers_.AddObserver(observer);
+}
+
+void ArcWindowWatcher::RemoveObserver(ArcWindowDisplayObserver* observer) {
+  arc_window_display_observers_.RemoveObserver(observer);
+}
+
+void ArcWindowWatcher::BroadcastArcWindowDisplay(const std::string& pkg_name) {
+  for (auto& observer : arc_window_display_observers_) {
+    observer.OnArcWindowDisplayed(pkg_name);
   }
 }
 
