6ccbb060600a891a8628a1f6ff91117222ac5a9e
dpapad <dpapad@chromium.org>
2023-04-07 17:57:30 +0000

    WebUI: Turn on no-var ESLint check on c/b/r/chromeos by default.
     - Removing unnecessary overrides (inherited by parent configs).
     - Adding 'no-var: off' in accessibility/ and login/, which are the
       only subfolders with remaining no-var violations.
    
    All other violations have been fixed in previous CLs, as revealed by
    running the following command.
    
    git cl presubmit --files "chrome/browser/resources/chromeos/*.js;chrome/browser/resources/chromeos/*.ts"
    
    Bug: 792774
    Change-Id: I6404606e35c657c5f7e96b75a88b911b6bdc2d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408205
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127675}

--- chrome/browser/resources/chromeos/accessibility/.eslintrc.js
+++ chrome/browser/resources/chromeos/accessibility/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-confusing-arrow' : ['error'],
     'no-console' : 'off',
     'no-throw-literal' : 'off',
+    'no-var' : 'off',
     'object-shorthand' : ['error', 'always'],
     'prefer-const' : ['error', {'destructuring' : 'all'}],
   },
