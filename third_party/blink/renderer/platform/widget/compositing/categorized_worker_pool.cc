e711e98495c3dfd400c60364fbdc11d26d3bae39
Etienne Pierre-doray <etiennep@chromium.org>
2023-04-07 00:53:29 +0000

    [task] Enable UseCompositorJob by default
    Stable results https://docs.google.com/document/d/1GPaWfCVj9ToqB3UJBvD1yIzILF0fYvXWBMdmx8YB6-c/edit?resourcekey=0-A97kzDTaP_7udm5FDrxWpg#bookmark=id.m2rulg3oheq7
    
    Bug: 1364153
    Change-Id: I5a78fa24431c9cc38f0f49814b894a5150d0e8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404440
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127474}

--- third_party/blink/renderer/platform/widget/compositing/categorized_worker_pool.cc
+++ third_party/blink/renderer/platform/widget/compositing/categorized_worker_pool.cc
@@ -36,7 +36,7 @@ namespace {
 
 BASE_FEATURE(kUseCompositorJob,
              "UseCompositorJob",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Task categories running at normal thread priority.
 constexpr cc::TaskCategory kNormalThreadPriorityCategories[] = {
