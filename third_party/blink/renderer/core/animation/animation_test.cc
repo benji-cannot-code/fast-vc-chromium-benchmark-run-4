1f1c33526695a40aa7429420a6400caba63e0438
Xianzhu Wang <wangxianzhu@chromium.org>
2023-04-07 17:43:44 +0000

    [CompositeScrollAfterPaint] Always use composited scroll-driven animations
    This CL skips code that prevented composited scroll-linked animation
    for non-composited scrollers, when CompositeScrollAfterPaint is
    enabled. This is required by CompositeScrollAfterPaint which defers
    the decision of composited scrolling after paint.
    
    For now this will expose non-composited scrollers to
    crbug.com/1416223. The changed code has a TODO to block
    CompositeScrollAfterPaint on ScrollUnification and crbug.com/1416223.
    
    Bug: 1414885
    Change-Id: Ic87b61bf014dc73b8b296e84778372eacbaee9ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401555
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127671}

--- third_party/blink/renderer/core/animation/animation_test.cc
+++ third_party/blink/renderer/core/animation/animation_test.cc
@@ -2315,8 +2315,11 @@ TEST_P(AnimationAnimationTestCompositing,
 
   UpdateAllLifecyclePhasesForTest();
   scroll_animation->play();
-  EXPECT_EQ(scroll_animation->CheckCanStartAnimationOnCompositor(nullptr),
-            CompositorAnimations::kTimelineSourceHasInvalidCompositingState);
+  EXPECT_EQ(
+      scroll_animation->CheckCanStartAnimationOnCompositor(nullptr),
+      RuntimeEnabledFeatures::CompositeScrollAfterPaintEnabled()
+          ? CompositorAnimations::kNoFailure
+          : CompositorAnimations::kTimelineSourceHasInvalidCompositingState);
 }
 
 #if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
