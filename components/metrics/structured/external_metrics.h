c4c7393d0f83609bf0cd5eb6e631b7d1ce6871c5
Andrew Bregger <andrewbregger@google.com>
2023-04-07 00:04:28 +0000

    Adding disallowed list support for external metrics
    NO_BUG
    
    Change-Id: I7f60b074687757d0f95d6ad5f5831545c33b5bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4403148
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127462}

--- components/metrics/structured/external_metrics.h
+++ components/metrics/structured/external_metrics.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_METRICS_STRUCTURED_EXTERNAL_METRICS_H_
 #define COMPONENTS_METRICS_STRUCTURED_EXTERNAL_METRICS_H_
 
+#include "base/containers/flat_set.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
@@ -39,6 +40,9 @@ class ExternalMetrics {
   ExternalMetrics(const ExternalMetrics&) = delete;
   ExternalMetrics& operator=(const ExternalMetrics&) = delete;
 
+  // Adds a project to the disallowed list for testing.
+  void AddDisallowedProjectForTest(uint64_t project_name_hash);
+
  private:
   friend class ExternalMetricsTest;
 
@@ -46,9 +50,16 @@ class ExternalMetrics {
   void CollectEventsAndReschedule();
   void CollectEvents();
 
+  // Builds a cache of disallow projects from the Finch controlled variable.
+  void CacheDisallowedProjectsSet();
+
   const base::FilePath events_directory_;
   const base::TimeDelta collection_interval_;
   MetricsCollectedCallback callback_;
+
+  // A set of projects that are not allowed to be recorded. This is a cache of
+  // GetDisabledProjects().
+  base::flat_set<uint64_t> disallowed_projects_;
 
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
   base::WeakPtrFactory<ExternalMetrics> weak_factory_{this};
