33ea7154754062d51691765fa599c3630c02a996
Chad Duffin <chadduffin@chromium.org>
2023-04-07 18:01:41 +0000

    cellular: Add profiles to HermesEuiccClient::Properties
    This change adds profiles to HermesEuiccClient::Properties. This
    property will contain all eSIM profiles known to Hermes. This property
    will be a near drop-in replacement for the the
    installed_carrier_profiles_ and pending_carrier_profiles_ properties
    since Chrome only differentiates profiles based on their state
    (installation, pending) and not where the DBus object was stored (with
    the installed profiles, or with the pending profiles).
    
    For more information see go/cros-smds-dbus.
    
    Bug: b/271854446
    Change-Id: I7e70c4deb82ae10e56a6760b1870b0151d16bccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400933
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127678}

--- chromeos/ash/components/dbus/hermes/hermes_euicc_client.cc
+++ chromeos/ash/components/dbus/hermes/hermes_euicc_client.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/dbus/hermes/hermes_euicc_client.h"
 
+#include "ash/constants/ash_features.h"
+#include "base/check.h"
 #include "base/functional/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -36,10 +38,18 @@ HermesEuiccClient::Properties::Properties(
                    &installed_carrier_profiles_);
   RegisterProperty(hermes::euicc::kPendingProfilesProperty,
                    &pending_carrier_profiles_);
+  RegisterProperty(hermes::euicc::kProfilesProperty, &profiles_);
   RegisterProperty(hermes::euicc::kPhysicalSlotProperty, &physical_slot_);
 }
 
 HermesEuiccClient::Properties::~Properties() = default;
+
+// TODO(b/271854446): Inline this accessor once the feature is launched.
+dbus::Property<std::vector<dbus::ObjectPath>>&
+HermesEuiccClient::Properties::profiles() {
+  DCHECK(features::IsSmdsDbusMigrationEnabled());
+  return profiles_;
+}
 
 class HermesEuiccClientImpl : public HermesEuiccClient {
  public:
