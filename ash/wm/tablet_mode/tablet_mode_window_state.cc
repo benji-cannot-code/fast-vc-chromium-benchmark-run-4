054c9b6ae33de8ad827f8522628ba664019260f1
Benjamin Zielinski <bzielinski@google.com>
2023-04-08 03:23:24 +0000

    Fix tablet mode + locked fullscreen
    https://crrev.com/c/3218654 refactored how locked fullscreen is set in
    default_state.cc, but that change did not update the implementation for
    tablet_mode_window_state.cc. Since tablet mode + locked fullscreen is a
    rare use case, this was not caught at first.
    
    Bug: b:248499774, b:277040434
    Change-Id: Icf1c09f3f764b9b931664aa3a18f93ff88fc4b87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404279
    Auto-Submit: Benjamin Zielinski <bzielinski@google.com>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1127878}

--- ash/wm/tablet_mode/tablet_mode_window_state.cc
+++ ash/wm/tablet_mode/tablet_mode_window_state.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_positioning_utils.h"
 #include "ash/wm/window_properties.h"
 #include "ash/wm/window_state.h"
+#include "ash/wm/window_state_delegate.h"
 #include "ash/wm/window_state_util.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
@@ -526,6 +527,9 @@ void TabletModeWindowState::UpdateWindow(WindowState* window_state,
   if (chromeos::IsPinnedWindowStateType(old_state_type) ||
       chromeos::IsPinnedWindowStateType(target_state)) {
     Shell::Get()->screen_pinning_controller()->SetPinnedWindow(window);
+    if (window_state->delegate()) {
+      window_state->delegate()->ToggleLockedFullscreen(window_state);
+    }
   }
 }
 
