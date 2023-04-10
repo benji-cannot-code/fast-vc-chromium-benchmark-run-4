32fd3019fcad06dd9e0cfbe8dc576df3cfadb761
Thomas Lukaszewicz <tluk@chromium.org>
2023-04-10 17:31:54 +0000

    [favicon] Add a default favicon getter that accounts for bg color
    Currently favicon::GetDefaultFavicon() leverages the global native
    theme instance to determine whether to serve the light or dark
    favicon.
    
    This CL adds a GetDefaultFaviconModel() that takes into account the
    color of the background. This enables the correct favicon to be
    served for a given context (background colors may be light/dark
    regardless of the global color scheme setting).
    
    Bug: 1431468
    Change-Id: Iac39f9a81ad5fa8fe1e78885662843e7cf459b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402157
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128205}

--- chrome/browser/favicon/favicon_utils.h
+++ chrome/browser/favicon/favicon_utils.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/favicon/content/content_favicon_driver.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/base/models/image_model.h"
+#include "ui/color/color_id.h"
 
 namespace content {
 class WebContents;
@@ -38,6 +40,12 @@ gfx::Image TabFaviconFromWebContents(content::WebContents* contents);
 // Returns the image to use when no favicon is available, taking dark mode
 // into account if necessary.
 gfx::Image GetDefaultFavicon();
+
+// Returns the image to use when no favicon is available, taking the background
+// color into account. If no background color is provided the window background
+// color will be used (which is appropriate for most use cases).
+ui::ImageModel GetDefaultFaviconModel(
+    ui::ColorId bg_color = ui::kColorWindowBackground);
 
 // Saves the favicon for the last committed navigation entry to the favicon
 // database.
