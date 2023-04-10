c44cf74003d7898cc33631fffbefe791f1b99306
Yuki Awano <yawano@google.com>
2023-04-10 17:32:51 +0000

    Move a link to a next line
    - Use views::Link instead of views::StyledLabel as we can apply a style
      to the link.
    - This CL increments revision number of the ash pixeltests as this CL is
      a UI change.
    
    Bug: b:272370530
    Test: browser_tests --gtest_filter=*AppListIphBrowserTest*
    Test: ash_pixeltests --gtest_filter=*AppListViewLauncherSearchIphTest*
    Change-Id: I880b2df202f0c529a5fc8cb94a5a9ab95ce373c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408898
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128206}

--- ash/app_list/views/launcher_search_iph_view.h
+++ ash/app_list/views/launcher_search_iph_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/app_list/app_list_client.h"
 #include "ui/events/event.h"
+#include "ui/views/controls/link.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/view.h"
 
@@ -39,7 +40,7 @@ class LauncherSearchIphView : public views::View {
 
   enum ViewId {
     kSelf = 1,
-    kDescriptionLabel,
+    kDescriptionLinkLabel,
     kAssistant,
     // Do not put a new id after `kChipStart`. Numbers after `kChipStart`
     // will be used for chips.
@@ -63,11 +64,7 @@ class LauncherSearchIphView : public views::View {
 
   std::unique_ptr<ScopedIphSession> scoped_iph_session_;
   raw_ptr<Delegate> delegate_;
-  raw_ptr<views::StyledLabel> description_label_;
-
-  // Ranges for text part and link part in the text of `description_label_`.
-  gfx::Range text_range_;
-  gfx::Range link_range_;
+  raw_ptr<views::Link> link_label_;
 
   base::WeakPtrFactory<LauncherSearchIphView> weak_ptr_factory_{this};
 };
