793c9a2cfbdbaa9a9069a19dbc307c9b2a18627e
Damien Dejean <damiendejean@google.com>
2023-04-10 08:39:30 +0000

    Create Passpoint settings flag using base::Feature
    Adds a disabled by default ash feature flag for Passpoint settings UI.
    The setting is used by the settings UI to know when to display the
    Passpoint information.
    
    BUG=b:276404119
    
    Change-Id: I6d6dedaee24543412a666e2e9930c44ef03c2813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395931
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Damien Dejean <damiendejean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128114}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1629,6 +1629,11 @@ BASE_FEATURE(kPasspointARCSupport,
              "PasspointARCSupport",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enables user to display Passpoint credentials in the UI.
+BASE_FEATURE(kPasspointSettings,
+             "PasspointSettings",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables a notification warning users that their Thunderbolt device is not
 // supported on their CrOS device.
 // TODO(crbug/1254930): Revisit this flag when there is a way to query billboard
@@ -3109,6 +3114,11 @@ bool IsOverviewDeskNavigationEnabled() {
 
 bool IsPasspointARCSupportEnabled() {
   return base::FeatureList::IsEnabled(kPasspointARCSupport);
+}
+
+bool IsPasspointSettingsEnabled() {
+  return base::FeatureList::IsEnabled(kPasspointSettings) &&
+         base::FeatureList::IsEnabled(kPasspointARCSupport);
 }
 
 bool IsPcieBillboardNotificationEnabled() {
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -147,6 +147,9 @@ extern const char kMainThreadCompositingPriorityDescription[];
 extern const char kPasspointARCSupportName[];
 extern const char kPasspointARCSupportDescription[];
 
+extern const char kPasspointSettingsName[];
+extern const char kPasspointSettingsDescription[];
+
 extern const char kPasswordNotesWithBackupName[];
 extern const char kPasswordNotesWithBackupDescription[];
 
