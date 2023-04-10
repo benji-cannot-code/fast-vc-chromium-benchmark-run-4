70bb3d85ddfe67e3ffb806f7b4486460743a7617
Hidehiko Abe <hidehiko@chromium.org>
2023-04-10 05:38:14 +0000

    Align log level for Lacros with ash's.
    Currently, lacros log level is 0 (default).
    This CL passes ash's log level. In production, we are using 1.
    
    BUG=1429617
    TEST=Ran on DUT.
    
    Change-Id: I2c7d912e80169aacc7a77700cd1a481ac56391eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408689
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128088}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager.h"
 #include "components/user_manager/user_type.h"
 #include "components/version_info/version_info.h"
+#include "content/public/common/content_switches.h"
 #include "media/capture/capture_switches.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
@@ -1095,13 +1096,19 @@ void BrowserManager::StartWithLogFile(LaunchParamsFromBackground params) {
     // DetermineLoggingDestination in logging_chrome.cc.
     argv.push_back("--enable-logging=stderr");
 
+    auto* command_line = base::CommandLine::ForCurrentProcess();
+    if (command_line->HasSwitch(switches::kLoggingLevel)) {
+      argv.push_back(base::StringPrintf(
+          "--%s=%s", switches::kLoggingLevel,
+          command_line->GetSwitchValueASCII(switches::kLoggingLevel).c_str()));
+    }
+
     // TODO(crbug.com/1423163): Remove after root causing the issue.
     argv.push_back(
         "--vmodule=command_storage_backend=1,session_service_commands=1");
 
     if (launch_at_login_screen_ &&
-        !base::CommandLine::ForCurrentProcess()->HasSwitch(
-            switches::kDisableLoggingRedirect)) {
+        !command_line->HasSwitch(switches::kDisableLoggingRedirect)) {
       // Redirect logs to cryptohome after login on non-test images.
       argv.push_back(base::StringPrintf(
           "--%s=%s", chromeos::switches::kCrosPostLoginLogFile,
