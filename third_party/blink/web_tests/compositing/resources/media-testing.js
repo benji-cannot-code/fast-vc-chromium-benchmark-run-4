38a886134cdf3580186526439cb4be1501aba42e
Dale Curtis <dalecurtis@chromium.org>
2023-04-10 20:16:57 +0000

    Switch to video.rVFC() for video compositing tests.
    These tests want a video frame, which isn't always
    guaranteed by canplaythrough.
    
    Fixed: 1381153
    Change-Id: I4d13d7d1d3d71369318b32573cdef7de8da552f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411718
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128296}

--- third_party/blink/web_tests/compositing/resources/media-testing.js
+++ third_party/blink/web_tests/compositing/resources/media-testing.js
@@ -1,11 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (window.testRunner) {
+  testRunner.waitUntilDone();
+}
 
-if (window.testRunner)
-    testRunner.waitUntilDone();
-
-function setupVideo(videoElement, videoPath, canPlayThroughCallback)
-{
-    videoElement.addEventListener("canplaythrough", canPlayThroughCallback);
-    videoElement.preload = "auto";
-    videoElement.src = videoPath;
+function setupVideo(videoElement, videoPath, firstFrameCallback) {
+  videoElement.requestVideoFrameCallback(firstFrameCallback);
+  videoElement.src = videoPath;
 }
