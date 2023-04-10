b5d505ecb98a7476e84915e979340ad877ded358
Ovidio Ruiz-Henríquez <odejesush@chromium.org>
2023-04-10 17:25:46 +0000

    controlledframe: Fix ChromeWebViewImpl require statement
    This change fixes the ChromeWebViewImpl import in the JavaScript source
    file for ControlledFrame. The module is registered as "chromeWebView"
    instead of "chromeWebViewImpl" in ChromeExtensionsDispatcherDelegate.
    
    Bug: 1233993, b/274664383
    Change-Id: I8fc209800381c80f523c8df0f79ba5a42d3c926a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412063
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128203}

--- chrome/renderer/resources/controlled_frame/controlled_frame.js
+++ chrome/renderer/resources/controlled_frame/controlled_frame.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This module implements chrome-specific <controlledframe> Element.
 
-var ChromeWebViewImpl = require('chromeWebViewImpl').ChromeWebViewImpl;
+var ChromeWebViewImpl = require('chromeWebView').ChromeWebViewImpl;
 var registerElement = require('guestViewContainerElement').registerElement;
 var WebViewElement = require('webViewElement').WebViewElement;
 var WebViewAttributeNames = require('webViewConstants').WebViewAttributeNames;
