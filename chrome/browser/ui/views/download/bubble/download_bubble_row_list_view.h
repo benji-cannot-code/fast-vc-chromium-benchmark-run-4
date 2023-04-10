759339c4c190a8dff593e31fcb780cbe715ac4e1
Daniel Rubery <drubery@chromium.org>
2023-04-10 17:48:59 +0000

    Expose RowListView construction
    In future CLs we need to construct the RowListView for the
    partial view and main view in different files. This CL preparse for that
    by moving RowListView construction to a static method on RowListView.
    
    Bug: 1406274
    Change-Id: I44e92451c1bdebc5fc0bdd3435bc1c34b7951ad8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408208
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128217}

--- chrome/browser/ui/views/download/bubble/download_bubble_row_list_view.h
+++ chrome/browser/ui/views/download/bubble/download_bubble_row_list_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/functional/callback_helpers.h"
+#include "chrome/browser/download/download_ui_model.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/layout/flex_layout_view.h"
 
@@ -16,9 +17,21 @@ namespace views {
 class ImageView;
 }  // namespace views
 
+class DownloadBubbleUIController;
+class DownloadBubbleNavigationHandler;
+
 class DownloadBubbleRowListView : public views::FlexLayoutView {
  public:
   METADATA_HEADER(DownloadBubbleRowListView);
+
+  static std::unique_ptr<views::View> CreateWithScroll(
+      bool is_partial_view,
+      Browser* browser,
+      DownloadBubbleUIController* bubble_controller,
+      DownloadBubbleNavigationHandler* navigation_handler,
+      std::vector<DownloadUIModel::DownloadUIModelPtr> rows,
+      int fixed_width,
+      base::OnceClosure on_mouse_entered_closure = base::DoNothing());
 
   DownloadBubbleRowListView(
       bool is_partial_view,
