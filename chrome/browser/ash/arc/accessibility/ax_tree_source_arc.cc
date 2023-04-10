9c790f29d36562ed53e0b92232acd0adb35c4bc4
Aaron Leventhal <aleventhal@google.com>
2023-04-10 20:07:41 +0000

    Assert that serializer is not fed invalid nodes
    We should remove the idea that the serializer works around invalid nodes, because it is not used and only adds complexity and uncertainty.
    
    Bug: none
    Change-Id: I5e807024ad3855505e2d63c6eac41d2421051bda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400867
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128291}

--- chrome/browser/ash/arc/accessibility/ax_tree_source_arc.cc
+++ chrome/browser/ash/arc/accessibility/ax_tree_source_arc.cc
@@ -320,8 +320,9 @@ void AXTreeSourceArc::ComputeEnclosingBoundsInternal(
 AccessibilityInfoDataWrapper*
 AXTreeSourceArc::FindFirstFocusableNodeInFullFocusMode(
     AccessibilityInfoDataWrapper* info_data) const {
-  if (!IsValid(info_data))
+  if (!info_data) {
     return nullptr;
+  }
 
   if (info_data->IsVisibleToUser() && info_data->IsFocusableInFullFocusMode())
     return info_data;
@@ -358,8 +359,9 @@ bool AXTreeSourceArc::UpdateAndroidFocusedId(const AXEventData& event_data) {
           UseFullFocusMode()
               ? FindFirstFocusableNodeInFullFocusMode(source_node)
               : source_node;
-      if (IsValid(adjusted_node))
+      if (adjusted_node) {
         android_focused_id_ = adjusted_node->GetId();
+      }
     }
   } else if (event_data.event_type == AXEventType::VIEW_ACCESSIBILITY_FOCUSED &&
              UseFullFocusMode()) {
@@ -419,15 +421,16 @@ bool AXTreeSourceArc::UpdateAndroidFocusedId(const AXEventData& event_data) {
           GetFromId(event_data.source_id));
     }
 
-    if (IsValid(new_focus))
+    if (new_focus) {
       android_focused_id_ = new_focus->GetId();
+    }
   }
 
   if (!android_focused_id_ || !GetFromId(*android_focused_id_)) {
     // Because we only handle events from the focused window, let's reset the
     // focus to the root window.
     AccessibilityInfoDataWrapper* root = GetRoot();
-    DCHECK(IsValid(root));
+    CHECK(root);
     android_focused_id_ = root_id_;
   }
 
@@ -581,10 +584,6 @@ void AXTreeSourceArc::ClearChildCache(AccessibilityInfoDataWrapper* info_data) {
 
 bool AXTreeSourceArc::IsIgnored(AccessibilityInfoDataWrapper* info_data) const {
   return false;
-}
-
-bool AXTreeSourceArc::IsValid(AccessibilityInfoDataWrapper* info_data) const {
-  return info_data;
 }
 
 bool AXTreeSourceArc::IsEqual(AccessibilityInfoDataWrapper* info_data1,
--- ui/accessibility/ax_tree_source_checker_unittest.cc
+++ ui/accessibility/ax_tree_source_checker_unittest.cc
@@ -70,10 +70,6 @@ class FakeAXTreeSource : public AXTreeSource<const FakeAXNode*> {
 
   bool IsIgnored(const FakeAXNode* node) const override { return false; }
 
-  bool IsValid(const FakeAXNode* node) const override {
-    return node != nullptr;
-  }
-
   bool IsEqual(const FakeAXNode* node1,
                const FakeAXNode* node2) const override {
     return node1 == node2;
@@ -118,7 +114,7 @@ TEST(AXTreeSourceCheckerTest, BadRoot) {
   std::string error_string;
   EXPECT_FALSE(checker.CheckAndGetErrorString(&error_string));
   CleanAXNodeDataString(&error_string);
-  EXPECT_EQ("Root is not valid.", error_string);
+  EXPECT_EQ("Root is not present.", error_string);
 }
 
 TEST(AXTreeSourceCheckerTest, BadNodeIdOfRoot) {
