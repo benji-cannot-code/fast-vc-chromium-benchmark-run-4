3431e95e75fea635ab15e3bdfc5aedc2ea6faf20
Daniel Rubery <drubery@chromium.org>
2023-04-10 18:35:06 +0000

    Add view for partial bubble
    At the moment this only contains a RowListView and handles tracking
    whether it has been interacted with. Future CLs will add additional
    functionality for the setting to not show partial bubble on download
    completion.
    
    Bug: 1406274
    Change-Id: Id22738fd7ba2d2448020db9cd1eda32661b07f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410170
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128253}

--- chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
+++ chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/accessibility/non_accessible_image_view.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/download/bubble/download_bubble_partial_view.h"
 #include "chrome/browser/ui/views/download/bubble/download_bubble_row_list_view.h"
 #include "chrome/browser/ui/views/download/bubble/download_bubble_row_view.h"
 #include "chrome/browser/ui/views/download/bubble/download_bubble_security_view.h"
@@ -381,11 +382,9 @@ bool DownloadToolbarButtonView::ShouldShowInkdropAfterIphInteraction() {
 
 std::unique_ptr<views::View> DownloadToolbarButtonView::GetPrimaryView() {
   if (is_primary_partial_view_) {
-    return DownloadBubbleRowListView::CreateWithScroll(
-        /*is_partial_view=*/true, browser_, bubble_controller_.get(), this,
+    return DownloadBubblePartialView::Create(
+        browser_, bubble_controller_.get(), this,
         bubble_controller_->GetPartialView(),
-        ChromeLayoutProvider::Get()->GetDistanceMetric(
-            views::DISTANCE_BUBBLE_PREFERRED_WIDTH),
         base::BindOnce(&DownloadToolbarButtonView::DeactivateAutoClose,
                        base::Unretained(this)));
   }
@@ -468,9 +467,6 @@ void DownloadToolbarButtonView::CreateBubbleDialogDelegate(
       switcher_view->AddChildView(std::make_unique<DownloadBubbleSecurityView>(
           bubble_controller_.get(), this, bubble_delegate.get()));
   security_view_->SetVisible(false);
-  bubble_delegate->set_fixed_width(
-      ChromeLayoutProvider::Get()->GetDistanceMetric(
-          views::DISTANCE_BUBBLE_PREFERRED_WIDTH));
   bubble_delegate->set_margins(GetPrimaryViewMargin());
   bubble_delegate->SetEnableArrowKeyTraversal(true);
   bubble_delegate_ = bubble_delegate.get();
