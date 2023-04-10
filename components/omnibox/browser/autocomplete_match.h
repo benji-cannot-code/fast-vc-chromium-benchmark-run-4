7ab6a687069dd1d05bb2529643987800dd21c1dc
Tomasz Wiszkowski <ender@google.com>
2023-04-10 18:12:15 +0000

    Restrict OmniboxActions offerings based on AutocompleteMatch position
    This change ensures that only specific type of an OmniboxAction is
    offered for AutocompleteMatches placed at specific positions on the
    list:
    - Position 0 and 1: all OmniboxActionIds
    - Position 2: HISTORY_CLUSTERS and PEDALs,
    - Position 3 and beyond: HISTORY_CLUSTERS only.
    
    The change guarantees that the different kinds of OmniboxAction are
    not mixed together - only one type of OmniboxAction can remain
    attached to the suggestion. The allowed types are supplied in a form
    of a preference list; if no allowed types are found, all Actions are
    removed.
    
    Bug: 1418077
    Change-Id: Ie56bbc697be0f97361ee081e3001af1a38514a3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402397
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128230}

--- components/omnibox/browser/autocomplete_match.h
+++ components/omnibox/browser/autocomplete_match.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/utf_offset_string_conversions.h"
 #include "build/build_config.h"
+#include "components/omnibox/browser/actions/omnibox_action_concepts.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/omnibox/browser/buildflags.h"
@@ -504,6 +505,11 @@ struct AutocompleteMatch {
   // providers.
   bool IsOnDeviceSearchSuggestion() const;
 
+  // Filter OmniboxActions based on the supplied qualifiers.
+  // The order of the supplied qualifiers determines the preference.
+  void FilterOmniboxActions(
+      const std::vector<OmniboxActionId>& allowed_action_ids);
+
   // Returns whether the autocompletion is trivial enough that we consider it
   // an autocompletion for which the omnibox autocompletion code did not add
   // any value.
@@ -584,11 +590,8 @@ struct AutocompleteMatch {
   // need to be selected. If no such action is found, returns nullptr.
   template <typename Predicate>
   OmniboxAction* GetActionWhere(Predicate predicate) const {
-    auto it = std::find_if(actions.begin(), actions.end(), predicate);
-    if (it == actions.end()) {
-      return nullptr;
-    }
-    return it->get();
+    auto it = base::ranges::find_if(actions, std::move(predicate));
+    return it != actions.end() ? it->get() : nullptr;
   }
 
   // The provider of this match, used to remember which provider the user had
