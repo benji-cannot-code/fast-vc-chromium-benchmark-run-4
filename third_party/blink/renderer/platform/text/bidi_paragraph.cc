c8b6286ac28064881ff9fde8a22823133ca90708
Koji Ishii <kojii@chromium.org>
2023-04-10 21:41:54 +0000

    [RemoveLegacy] Add `stop_at` to `BaseDirectionForString`
    When determining directionality from text content, CSS uses
    strong characters before a segment break[1], but HTML doesn't
    stop searching for strong characters by a segment break[2].
    This patch adds the `stop_at` argument to handle this
    difference.
    
    Also adds `BaseDirectionForStringOrLtr` because defaulting to
    LTR when there are no strong characters is a common practice
    found in HTML and CSS, and it can run an optimized code path
    for 8 bits strings.
    
    This patch has no behavior changes.
    
    [1] https://w3c.github.io/csswg-drafts/css-writing-modes-3/#valdef-unicode-bidi-plaintext
    [2] https://html.spec.whatwg.org/multipage/dom.html#attr-dir-auto-state
    
    Bug: 1229581
    Change-Id: Ia6fc8be30383be54b73de0d4cc3fc5dfb661a359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412215
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128342}

--- third_party/blink/renderer/platform/text/bidi_paragraph.cc
+++ third_party/blink/renderer/platform/text/bidi_paragraph.cc
@@ -45,13 +45,14 @@ bool BidiParagraph::SetParagraph(const String& text,
 // static
 template <>
 absl::optional<TextDirection> BidiParagraph::BaseDirectionForString(
-    base::span<const LChar> text) {
+    base::span<const LChar> text,
+    bool (*stop_at)(UChar)) {
   for (const LChar ch : text) {
     if (u_charDirection(ch) == U_LEFT_TO_RIGHT) {
       return TextDirection::kLtr;
     }
 
-    if (ch == kNewlineCharacter) {
+    if (stop_at && stop_at(ch)) {
       break;
     }
   }
@@ -61,7 +62,8 @@ absl::optional<TextDirection> BidiParagraph::BaseDirectionForString(
 // static
 template <>
 absl::optional<TextDirection> BidiParagraph::BaseDirectionForString(
-    base::span<const UChar> text) {
+    base::span<const UChar> text,
+    bool (*stop_at)(UChar)) {
   const UChar* data = text.data();
   const size_t len = text.size();
   for (size_t i = 0; i < len;) {
@@ -77,7 +79,7 @@ absl::optional<TextDirection> BidiParagraph::BaseDirectionForString(
         break;
     }
 
-    if (ch == kNewlineCharacter) {
+    if (stop_at && stop_at(ch)) {
       break;
     }
   }
@@ -86,9 +88,10 @@ absl::optional<TextDirection> BidiParagraph::BaseDirectionForString(
 
 // static
 absl::optional<TextDirection> BidiParagraph::BaseDirectionForString(
-    const StringView& text) {
-  return text.Is8Bit() ? BaseDirectionForString(text.Span8())
-                       : BaseDirectionForString(text.Span16());
+    const StringView& text,
+    bool (*stop_at)(UChar)) {
+  return text.Is8Bit() ? BaseDirectionForString(text.Span8(), stop_at)
+                       : BaseDirectionForString(text.Span16(), stop_at);
 }
 
 // static
