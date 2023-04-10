72214f430b65a782d5c0ffa5902acc7fb4397332
Ilkin Safarli <safarli@google.com>
2023-04-10 21:08:00 +0000

    CrOS Settings: Convert cursor_and_touchpad browser test to TS
    Bug: b:270728282
    Test: browser_tests --gtest_filter="OSSettingsOsA11yPageCursorAndTouchpadPage*"
    Change-Id: I602ef4929133ba518fa180300d904a4ba73bacc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401198
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Ilkin Safarli <safarli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128321}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/cursor_and_touchpad_page.ts
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/cursor_and_touchpad_page.ts
@@ -43,7 +43,7 @@ interface Option {
   value: number;
 }
 
-interface SettingsCursorAndTouchpadPageElement {
+export interface SettingsCursorAndTouchpadPageElement {
   $: {
     pointerSubpageButton: CrLinkRowElement,
   };
@@ -53,7 +53,7 @@ const SettingsCursorAndTouchpadPageElementBase =
     DeepLinkingMixin(RouteOriginMixin(
         PrefsMixin(WebUiListenerMixin(I18nMixin(PolymerElement)))));
 
-class SettingsCursorAndTouchpadPageElement extends
+export class SettingsCursorAndTouchpadPageElement extends
     SettingsCursorAndTouchpadPageElementBase {
   static get is() {
     return 'settings-cursor-and-touchpad-page';
