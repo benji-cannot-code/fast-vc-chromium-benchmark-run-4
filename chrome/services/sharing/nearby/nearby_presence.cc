540a006154edbfa3c7df256766fb47d0426486fa
Alex Kingsborough <akingsb@google.com>
2023-04-10 18:38:17 +0000

    [NP] Create skeleton Nearby Presence mojom
    Create the skeleton files for a Nearby Presence mojom, these files will
    be built out in subsequent cls.
    
    Project design and security considerations are included in go/cros-nearby-presence-service. The bug for security review for the project is b/265362835.
    
    Test: Compiled successfully and passed skeleton unit test.
    Change-Id: I205514267ccdff6aae282a25f6312de1dcf9a399
    Bug: b/276642472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4403133
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128256}

---
+++ chrome/services/sharing/nearby/nearby_presence.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/nearby_presence.h"
+
+namespace ash::nearby::presence {
+
+NearbyPresence::NearbyPresence(
+    mojo::PendingReceiver<mojom::NearbyPresence> nearby_presence,
+    base::OnceClosure on_disconnect)
+    : nearby_presence_(this, std::move(nearby_presence)) {
+  nearby_presence_.set_disconnect_handler(std::move(on_disconnect));
+}
+
+NearbyPresence::~NearbyPresence() = default;
+
+}  // namespace ash::nearby::presence
