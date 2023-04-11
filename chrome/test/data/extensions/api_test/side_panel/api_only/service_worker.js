06fce24d14cfccab868f4242c87432e2d9080f90
Solomon Kinard <solomonkinard@chromium.org>
2023-04-11 04:23:08 +0000

    Extensions: Side Panel: Set optional option to a default value: enabled
    chrome.sidePanel.setOptions({path: “the.html”}) no longer works. It used
    to. It was enabled by default two months ago, but incidentally changed
    last month in a way that didn't appear intentional.
    
    This aims to fix a regression whereby the menu item for the extension in
    the side panel used to appear before a change [1] when not statically
    declaring the side panel path in the manifest.
    
    [1] d0a5fa8d489c024c0736af2b2ba2e59628ecaf9e
    
    Doc:
    https://docs.google.com/document/d/1v58G20SDYON9Q2CZz7B7b030SDz8DHe8ZPcs3to1fX4
    
    Fixed: 1432012
    Change-Id: If5a1d2d6b550c4d2326683053780c6c8caaa06fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412432
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128482}

---
+++ chrome/test/data/extensions/api_test/side_panel/api_only/service_worker.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  // Setting a panel option without explicitly setting the optional enabled bool
+  // value defaults to enabled being true. Bug fix for crbug.com/1432012.
+  async function setAndGetPanelOptionAndEnsureEnabled() {
+    await chrome.sidePanel.setOptions({path: 'path.html'});
+    let result = await chrome.sidePanel.getOptions({});
+    const expected = {path: 'path.html', enabled: true};
+    chrome.test.assertEq(expected, result);
+    chrome.test.succeed();
+  },
+]);
