bef2809f05114dc1142967efc15256c0b702a9cc
Claudio DeSouza <cdesouza@igalia.com>
2023-04-11 08:28:33 +0000

    Enum modernisation for storage.json
    This CL updates the codegen attribute for the enums described in
    storage.json, so it generates `enum class` types, with updated naming
    conventions.
    
    Bug: 1421546
    Change-Id: Id12cc798414e14b6e2e9a56041642630a30366bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411426
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1128532}

--- extensions/browser/api/storage/storage_api.cc
+++ extensions/browser/api/storage/storage_api.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/trace_event/trace_event.h"
+#include "base/types/cxx23_to_underlying.h"
 #include "base/values.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -227,16 +228,20 @@ void SettingsFunction::OnSessionSettingsChanged(
 bool SettingsFunction::IsAccessToStorageAllowed() {
   ExtensionPrefs* prefs = ExtensionPrefs::Get(browser_context());
   // Default access level is only secure contexts.
-  int access_level = api::storage::ACCESS_LEVEL_TRUSTED_CONTEXTS;
+  int access_level =
+      base::to_underlying(api::storage::AccessLevel::kTrustedContexts);
   prefs->ReadPrefAsInteger(extension()->id(), kPrefSessionStorageAccessLevel,
                            &access_level);
 
   // Only a blessed extension context is considered trusted.
-  if (access_level == api::storage::ACCESS_LEVEL_TRUSTED_CONTEXTS)
+  if (access_level ==
+      base::to_underlying(api::storage::AccessLevel::kTrustedContexts)) {
     return source_context_type() == Feature::BLESSED_EXTENSION_CONTEXT;
+  }
 
   // All contexts are allowed.
-  DCHECK_EQ(api::storage::ACCESS_LEVEL_TRUSTED_AND_UNTRUSTED_CONTEXTS,
+  DCHECK_EQ(base::to_underlying(
+                api::storage::AccessLevel::kTrustedAndUntrustedContexts),
             access_level);
   return true;
 }
@@ -522,13 +527,14 @@ StorageStorageAreaSetAccessLevelFunction::RunInSession() {
 
   // The parsing code ensures `access_level` is sane.
   DCHECK(params->access_options.access_level ==
-             api::storage::ACCESS_LEVEL_TRUSTED_CONTEXTS ||
+             api::storage::AccessLevel::kTrustedContexts ||
          params->access_options.access_level ==
-             api::storage::ACCESS_LEVEL_TRUSTED_AND_UNTRUSTED_CONTEXTS);
+             api::storage::AccessLevel::kTrustedAndUntrustedContexts);
 
   ExtensionPrefs* prefs = ExtensionPrefs::Get(browser_context());
-  prefs->SetIntegerPref(extension_id(), kPrefSessionStorageAccessLevel,
-                        params->access_options.access_level);
+  prefs->SetIntegerPref(
+      extension_id(), kPrefSessionStorageAccessLevel,
+      base::to_underlying(params->access_options.access_level));
 
   return NoArguments();
 }
