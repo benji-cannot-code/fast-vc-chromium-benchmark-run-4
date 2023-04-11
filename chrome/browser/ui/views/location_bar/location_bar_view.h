cdc755c7822ff68751981861d3cd2fcd5a0cceb0
Khalid Peer <khalidpeer@chromium.org>
2023-04-11 20:34:24 +0000

    [omnibox][CR2023] Update Omnibox "expanded state" colors per CR2023 spec
    In order to align with the latest GM3 designs, this CL updates various
    Omnibox colors in the "expanded state" for "Chrome Classic" in both
    Light and Dark modes.
    
    More specifically, these changes will impact the following UI elements:
    
      - Focus bar (width + color)
      - Suggestion hover fill (color)
      - Omnibox popup background (color)
      - In-suggestion URL links (color)
      - Keyword mode text (color)
      - Keyword mode icon (color)
      - Keyword mode divider (color)
      - Suggestion secondary text (color)
      - Suggestion text divider (color)
      - Suggestion vector icon (color) (exc. `chrome://` URLs)
      - Omnibox vector icon (color) (exc. `chrome://` URLs)
    
    In order to facilitate experimentation, this CL also defines the
    necessary feature flag (accessible via `chrome://flags`).
    
    Bug: 1431337
    Change-Id: I1dcabec05ea89a3ba630fad6872b177808408a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408066
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128871}

--- chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -203,6 +203,7 @@ class LocationBarView : public LocationBar,
 
   // IconLabelBubbleView::Delegate:
   SkColor GetIconLabelBubbleSurroundingForegroundColor() const override;
+  SkAlpha GetIconLabelBubbleSeparatorAlpha() const override;
   SkColor GetIconLabelBubbleBackgroundColor() const override;
 
   // ContentSettingImageView::Delegate:
