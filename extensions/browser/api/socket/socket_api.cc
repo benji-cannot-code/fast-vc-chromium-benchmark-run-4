995349b75bb4e66fa545a9b92c932abf7d66b161
Claudio DeSouza <cdesouza@igalia.com>
2023-04-11 08:09:01 +0000

    Enum modernisation for socket.idl
    This CL updates the codegen attribute for the enums described in
    socket.idl, so it generates `enum class` types, with updated naming
    conventions.
    
    Bug: 1421546
    Change-Id: I930cf576dfc38c9b852c4e38bbc8b7ef1316dac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411423
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128523}

--- extensions/browser/api/socket/socket_api.cc
+++ extensions/browser/api/socket/socket_api.cc
@@ -265,11 +265,11 @@ ExtensionFunction::ResponseAction SocketCreateFunction::Work() {
 
   Socket* socket = nullptr;
   switch (params->type) {
-    case extensions::api::socket::SOCKET_TYPE_TCP:
+    case extensions::api::socket::SocketType::kTcp:
       socket = new TCPSocket(browser_context(), GetOriginId());
       break;
 
-    case extensions::api::socket::SOCKET_TYPE_UDP: {
+    case extensions::api::socket::SocketType::kUdp: {
       mojo::PendingRemote<network::mojom::UDPSocketListener> listener_remote;
       mojo::PendingReceiver<network::mojom::UDPSocketListener>
           socket_listener_receiver =
@@ -285,7 +285,7 @@ ExtensionFunction::ResponseAction SocketCreateFunction::Work() {
                         std::move(socket_listener_receiver), GetOriginId());
       break;
     }
-    case extensions::api::socket::SOCKET_TYPE_NONE:
+    case extensions::api::socket::SocketType::kNone:
       NOTREACHED();
       return RespondNow(NoArguments());
   }
@@ -792,9 +792,9 @@ ExtensionFunction::ResponseAction SocketGetInfoFunction::Work() {
   // This represents what we know about the socket, and does not call through
   // to the system.
   if (socket->GetSocketType() == Socket::TYPE_TCP)
-    info.socket_type = extensions::api::socket::SOCKET_TYPE_TCP;
+    info.socket_type = extensions::api::socket::SocketType::kTcp;
   else
-    info.socket_type = extensions::api::socket::SOCKET_TYPE_UDP;
+    info.socket_type = extensions::api::socket::SocketType::kUdp;
   info.connected = socket->IsConnected();
 
   // Grab the peer address as known by the OS. This and the call below will
