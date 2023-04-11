b329ea4f66fa640227a63b9dcf6c599e6dd5fca2
dpapad <dpapad@chromium.org>
2023-04-11 16:55:13 +0000

    WebUI: Disallow usages of obsolete Polymer() syntax via ESLint checks.
    The class based "extends PolymerElement" syntax has been the preferred
    way for a very long time and the vast majority of the codebase is
    already using it.
    
    Added a couple exceptions for a few CrOS folders that still use the old
    syntax, and otherwise disallowing any new usages via ESLint's
    no-restricted-imports rule.
    
    All violations discovered with  the following command should have been
    fixed or suppressed already.
    git cl presubmit --files "*.js;*.ts"
    
    Bug: 720034
    Change-Id: I630dfbcfdbd1cab066e2faf145e6685ecb4693ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412401
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Cole Horvitz <colehorvitz@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128705}

--- ui/file_manager/file_manager/foreground/elements/.eslintrc.js
+++ ui/file_manager/file_manager/foreground/elements/.eslintrc.js
@@ -1,13 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2017 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 module.exports = {
   'rules' : {
-    'eqeqeq' : 'off',
-
-    // Turn off since there are too many imports of 'Polymer'. Remove if/when
+    // Turn off since there are many imports of 'Polymer'. Remove if/when
     // everything under this folder is migrated to PolymerElement.
     'no-restricted-imports': 'off',
   },
