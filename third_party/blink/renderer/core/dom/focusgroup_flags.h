bb61b79653639f5c9f73f01d78b211facb6c95fe
L. David Baron <dbaron@chromium.org>
2023-04-11 13:07:39 +0000

    Use base::to_underlying in FocusgroupFlags.
    This was an optional review suggestion from dcheng@ in
    https://crrev.com/c/4319052, and it improves the code.  I'm putting it
    into its own CL because it requires a DEPS change, and I didn't want to
    add the reviews for that change into the existing CL.
    
    Bug: 1250716
    Change-Id: I54ed14299f2ad12cd43f1f91baca19637b5257ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411370
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128600}

--- third_party/blink/renderer/core/dom/focusgroup_flags.h
+++ third_party/blink/renderer/core/dom/focusgroup_flags.h
@@ -6,8 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_DOM_FOCUSGROUP_FLAGS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_FOCUSGROUP_FLAGS_H_
 
-#include <type_traits>
-
+#include "base/types/cxx23_to_underlying.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
@@ -38,16 +37,14 @@ enum FocusgroupFlags : uint16_t {
 
 inline constexpr FocusgroupFlags operator&(FocusgroupFlags a,
                                            FocusgroupFlags b) {
-  return static_cast<FocusgroupFlags>(
-      static_cast<std::underlying_type_t<FocusgroupFlags>>(a) &
-      static_cast<std::underlying_type_t<FocusgroupFlags>>(b));
+  return static_cast<FocusgroupFlags>(base::to_underlying(a) &
+                                      base::to_underlying(b));
 }
 
 inline constexpr FocusgroupFlags operator|(FocusgroupFlags a,
                                            FocusgroupFlags b) {
-  return static_cast<FocusgroupFlags>(
-      static_cast<std::underlying_type_t<FocusgroupFlags>>(a) |
-      static_cast<std::underlying_type_t<FocusgroupFlags>>(b));
+  return static_cast<FocusgroupFlags>(base::to_underlying(a) |
+                                      base::to_underlying(b));
 }
 
 inline FocusgroupFlags& operator|=(FocusgroupFlags& a, FocusgroupFlags b) {
@@ -59,8 +56,7 @@ inline FocusgroupFlags& operator&=(FocusgroupFlags& a, FocusgroupFlags b) {
 }
 
 inline constexpr FocusgroupFlags operator~(FocusgroupFlags flags) {
-  return static_cast<FocusgroupFlags>(
-      ~static_cast<std::underlying_type_t<FocusgroupFlags>>(flags));
+  return static_cast<FocusgroupFlags>(~base::to_underlying(flags));
 }
 
 FocusgroupFlags FindNearestFocusgroupAncestorFlags(const Element* element);
