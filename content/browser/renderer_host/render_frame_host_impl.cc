6a5ead014c7efe2f43d4db396451b8a44564db3f
David Tseng <dtseng@google.com>
2023-04-11 16:16:17 +0000

    Check tree updates unserialize on the expected AXTree
    This change enforces that any AXTreeUpdate with a known tree id gets
    applied onto an AXTree with the same tree id (if also known).
    
    REad Anything and other managers of multiple trees motivated this
    change. It resulted in at least one substantitie real issue found.
    
    Change-Id: Iee11289a2c25f7e234d9ff774a39d27114a4036a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307953
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128685}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -14832,6 +14832,13 @@ void RenderFrameHostImpl::SetEmbeddingToken(
       lifecycle_state_ != LifecycleStateImpl::kPrerendering) {
     outermost->UpdateAXTreeData();
   }
+
+  // Finally, since the AXTreeID changed, we have to ensure the
+  // BrowserAccessibilityManager gets a new tree as well.
+  if (browser_accessibility_manager_) {
+    browser_accessibility_manager_->DetachFromParentManager();
+    browser_accessibility_manager_.reset();
+  }
 }
 
 bool RenderFrameHostImpl::DocumentUsedWebOTP() {
