427e8e0af6a3be9e785153aa4f35c824f28f7b18
Maciek Swiech <drmasquatch@google.com>
2023-04-11 17:04:20 +0000

    Always start SyncAppsyncService
    previous CL registered the SyncAppsyncService as a normal KeyedService,
    which is instantiated lazily - so we never saw it perform actions. This
    patch registers the factory and marks it as always being started with
    the BrowserContext.
    
    TEST=deployed to DUT, saw histograms and file system changes
    
    Bug: chromium:1193485
    Change-Id: Ica15c5fb6211508efb2b603bf0b62e0d2d93235f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4397881
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maciek Swiech <drmasquatch@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128710}

--- chrome/browser/ash/sync/sync_appsync_service_factory.h
+++ chrome/browser/ash/sync/sync_appsync_service_factory.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_SYNC_SYNC_APPSYNC_SERVICE_FACTORY_H_
 #define CHROME_BROWSER_ASH_SYNC_SYNC_APPSYNC_SERVICE_FACTORY_H_
 
+#include <memory>
+
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
+#include "components/keyed_service/core/keyed_service.h"
 
 class Profile;
 
@@ -14,6 +17,10 @@ namespace base {
 template <typename T>
 struct DefaultSingletonTraits;
 }  // namespace base
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace ash {
 
@@ -34,9 +41,10 @@ class SyncAppsyncServiceFactory : public ProfileKeyedServiceFactory {
   SyncAppsyncServiceFactory();
   ~SyncAppsyncServiceFactory() override;
 
-  // BrowserContextKeyedServiceFactory implementation.
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
+  bool ServiceIsCreatedWithBrowserContext() const override;
+  bool ServiceIsNULLWhileTesting() const override;
 };
 
 }  // namespace ash
