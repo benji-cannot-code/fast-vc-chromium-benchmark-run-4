d257812cae47fcb993c9223c5a10a96e6598b545
Louis Romero <lpromero@google.com>
2023-04-11 15:30:18 +0000

    Support having Inactive Tabs settings be presented from Inactive Tabs
    Prior to this CL, when opening the Inactive Tabs settings from the
    Inactive Tabs grid, the InactiveTabsSettingsMediator got the inactive
    browser, which broke the migration code.
    With this CL, we support launching the settings with any of the active
    or inactive browser. The mediator then retrieves the associated pair of
    browsers and migrate accordingly.
    
    Bug: 1429294
    Change-Id: Iaf160d982c985f65dcfbd500957dbf051df96825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395998
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128657}

--- ios/chrome/browser/ui/settings/tabs/inactive_tabs/inactive_tabs_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/tabs/inactive_tabs/inactive_tabs_settings_mediator.mm
@@ -24,32 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-namespace {
-
-// Find the associated inactive browser to the current regular browser.
-Browser* FindInactiveBrowserWithRegularBrowser(Browser* regular_browser) {
-  ChromeBrowserState* browserState = regular_browser->GetBrowserState();
-  SceneState* current_scene_state =
-      SceneStateBrowserAgent::FromBrowser(regular_browser)->GetSceneState();
-  std::set<Browser*> regular_browsers =
-      BrowserListFactory::GetForBrowserState(browserState)
-          ->AllRegularBrowsers();
-
-  std::set<Browser*>::iterator inactive_browser_iterator =
-      base::ranges::find_if(regular_browsers, [current_scene_state](
-                                                  Browser* browser) {
-        return browser->IsInactive() &&
-               SceneStateBrowserAgent::FromBrowser(browser)->GetSceneState() ==
-                   current_scene_state;
-      });
-
-  DCHECK(inactive_browser_iterator != regular_browsers.end());
-
-  return *inactive_browser_iterator;
-}
-
-}  // namespace
-
 @interface InactiveTabsSettingsMediator () <PrefObserverDelegate>
 @end
 
@@ -62,7 +36,7 @@ Browser* FindInactiveBrowserWithRegularBrowser(Browser* regular_browser) {
   std::unique_ptr<PrefObserverBridge> _prefObserverBridge;
   // Registrar for pref changes notifications.
   PrefChangeRegistrar _prefChangeRegistrar;
-  // Regular browser.
+  // The current browser.
   Browser* _browser;
 }
 
@@ -115,15 +89,18 @@ Browser* FindInactiveBrowserWithRegularBrowser(Browser* regular_browser) {
   }
   _prefs->SetInteger(prefs::kInactiveTabsTimeThreshold, threshold);
 
-  Browser* inactiveBrowser = FindInactiveBrowserWithRegularBrowser(_browser);
+  Browser* active_browser = _browser->GetActiveBrowser();
+  Browser* inactive_browser = _browser->GetInactiveBrowser();
+  CHECK(active_browser);
+  CHECK(inactive_browser);
 
   if (threshold == kInactiveTabsDisabledByUser) {
-    RestoreAllInactiveTabs(inactiveBrowser, _browser);
+    RestoreAllInactiveTabs(inactive_browser, active_browser);
   } else if (previousThreshold == kInactiveTabsDisabledByUser ||
              previousThreshold > threshold) {
-    MoveTabsFromActiveToInactive(_browser, inactiveBrowser);
+    MoveTabsFromActiveToInactive(active_browser, inactive_browser);
   } else {
-    MoveTabsFromInactiveToActive(inactiveBrowser, _browser);
+    MoveTabsFromInactiveToActive(inactive_browser, active_browser);
   }
 }
 
