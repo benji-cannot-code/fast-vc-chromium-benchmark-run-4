2764a8444ea362f0d31e6b3ed8d281ecf9eb29a1
Joel Hockey <joelhockey@chromium.org>
2023-04-11 01:34:23 +0000

    Log when using finch safe seed or null seed on ChromeOS
    Log detailed info at startup on ChromeOS when safe seed or null seed
    is used so that listnr feedback logs can capture this information.
    
    Bug: b/277650823
    Change-Id: I3809da26229705db2030ea7b070c8dc8a6eaff69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387215
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128443}

--- components/variations/service/safe_seed_manager.cc
+++ components/variations/service/safe_seed_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/cxx17_backports.h"
+#include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "build/chromeos_buildflags.h"
@@ -98,10 +99,19 @@ SeedType SafeSeedManager::GetSeedType() const {
       local_state_->GetInteger(prefs::kVariationsFailedToFetchSeedStreak);
   if (num_crashes >= kCrashStreakNullSeedThreshold ||
       num_failed_fetches >= kFetchFailureStreakNullSeedThreshold) {
+#if BUILDFLAG(IS_CHROMEOS)
+    // Logging is useful in listnr reports for ChromeOS (http://b/277650823).
+    LOG(ERROR) << "Using finch safe mode null seed: num_crashes=" << num_crashes
+               << ", num_failed_fetches=" << num_failed_fetches;
+#endif  // BUILDFLAG(IS_CHROMEOS)
     return SeedType::kNullSeed;
   }
   if (num_crashes >= kCrashStreakSafeSeedThreshold ||
       num_failed_fetches >= kFetchFailureStreakSafeSeedThreshold) {
+#if BUILDFLAG(IS_CHROMEOS)
+    LOG(ERROR) << "Using finch safe mode safe seed: num_crashes=" << num_crashes
+               << ", num_failed_fetches=" << num_failed_fetches;
+#endif  // BUILDFLAG(IS_CHROMEOS)
     return SeedType::kSafeSeed;
   }
   return SeedType::kRegularSeed;
