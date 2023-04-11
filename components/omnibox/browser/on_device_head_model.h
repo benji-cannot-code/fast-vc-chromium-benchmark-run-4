b4f99c81fbdb140a607fd2d7eb9eb3a61eea0cd8
Stephan Hartmann <stha09@googlemail.com>
2023-04-11 13:30:43 +0000

    IWYU: add stdint.h for various integer types in //components
    Bug: 957519
    Change-Id: I920fc7c442b05c3522a6533c4b0ec83b403fd0f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4406547
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1128605}

--- components/omnibox/browser/on_device_head_model.h
+++ components/omnibox/browser/on_device_head_model.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OMNIBOX_BROWSER_ON_DEVICE_HEAD_MODEL_H_
 #define COMPONENTS_OMNIBOX_BROWSER_ON_DEVICE_HEAD_MODEL_H_
 
+#include <stdint.h>
+
 #include <string>
 #include <utility>
 #include <vector>
--- components/safe_browsing/content/browser/web_contents_key.h
+++ components/safe_browsing/content/browser/web_contents_key.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SAFE_BROWSING_CONTENT_BROWSER_WEB_CONTENTS_KEY_H_
 #define COMPONENTS_SAFE_BROWSING_CONTENT_BROWSER_WEB_CONTENTS_KEY_H_
 
+#include <cstdint>
+
 #include "base/types/strong_alias.h"
 
 namespace content {
