cd9224c0466722828aad7048e969efbcd2513598
Koji Ishii <kojii@chromium.org>
2023-04-11 03:34:06 +0000

    [RemoveLegacy] Remove `TextRunWithDirectionality`
    `DragImage` is the only caller of `TextRunWithDirectionality`.
    This patch changes it to use `BidiParagraph` instead, and
    remove `TextRunWithDirectionality`.
    
    Bug: 1229581
    Change-Id: If2f6c8a3d6ef4583fafab4db0285be1b2d987568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4409465
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128474}

--- third_party/blink/renderer/platform/text/text_run.cc
+++ third_party/blink/renderer/platform/text/text_run.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/text/text_run.h"
 
+#include "third_party/blink/renderer/platform/text/bidi_paragraph.h"
 #include "third_party/blink/renderer/platform/text/character.h"
 #include "third_party/blink/renderer/platform/wtf/size_assertions.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_buffer.h"
@@ -103,6 +104,10 @@ unsigned TextRun::IndexOfSubRun(const TextRun& sub_run) const {
       return static_cast<unsigned>(start_index);
   }
   return std::numeric_limits<unsigned>::max();
+}
+
+void TextRun::SetDirectionFromText() {
+  SetDirection(BidiParagraph::BaseDirectionForStringOrLtr(ToStringView()));
 }
 
 }  // namespace blink
