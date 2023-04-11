71d79273689d2ce48d14fc2b012724759aaa510f
Jérôme Lebel <jlebel@chromium.org>
2023-04-11 10:49:01 +0000

    [iOS] Disconnect observers for ClearBrowsingDataTableViewController
    ClearBrowsingDataCoordinator needs to be stopped from
    SettingsNavigationController.
    Need to call -[ClearBrowsingDataTableViewController
    prepareForDismissal], from ClearBrowsingDataCoordinator.
    
    Fixed: 1429513
    Change-Id: I68c6334f2736344ede1f4f969c9cf980ab826ea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410800
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128571}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_coordinator.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_coordinator.mm
@@ -60,6 +60,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
+  [self.viewController prepareForDismissal];
+  self.viewController.delegate = nil;
+  self.viewController.dispatcher = nil;
   self.viewController = nil;
 }
 
--- ios/chrome/browser/ui/settings/settings_navigation_controller.mm
+++ ios/chrome/browser/ui/settings/settings_navigation_controller.mm
@@ -540,6 +540,7 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
   [self stopGoogleServicesSettingsCoordinator];
   [self stopPasswordsCoordinator];
   [self stopSafetyCheckCoordinator];
+  [self stopClearBrowsingDataCoordinator];
 
   // Reset the delegate to prevent any queued transitions from attempting to
   // close the settings.
