9e3655a9f81f1f4566067191795d0a765f37523a
Anders Hartvoll Ruud <andruud@chromium.org>
2023-04-11 10:00:48 +0000

    [scroll-animations] Make scroll() accept arguments in any order
    This CL also reorders some code/members so it matches the canonical
    order in the spec (purely for aesthetic reasons):
    
     - <scroller> || <axis>
     - root | nearest | self
    
    Fixed: 1428549
    Change-Id: I7b3ff3a15416aaa51d117136f927c23f56ff79d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4398110
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128562}

--- third_party/blink/renderer/core/css/css_scroll_value.cc
+++ third_party/blink/renderer/core/css/css_scroll_value.cc
@@ -9,34 +9,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace cssvalue {
 
-CSSScrollValue::CSSScrollValue(const CSSValue* axis, const CSSValue* scroller)
-    : CSSValue(kScrollClass), axis_(axis), scroller_(scroller) {}
+CSSScrollValue::CSSScrollValue(const CSSValue* scroller, const CSSValue* axis)
+    : CSSValue(kScrollClass), scroller_(scroller), axis_(axis) {}
 
 String CSSScrollValue::CustomCSSText() const {
   StringBuilder result;
   result.Append("scroll(");
-  if (axis_) {
-    result.Append(axis_->CssText());
-  }
   if (scroller_) {
-    if (axis_) {
+    result.Append(scroller_->CssText());
+  }
+  if (axis_) {
+    if (scroller_) {
       result.Append(' ');
     }
-    result.Append(scroller_->CssText());
+    result.Append(axis_->CssText());
   }
   result.Append(")");
   return result.ReleaseString();
 }
 
 bool CSSScrollValue::Equals(const CSSScrollValue& other) const {
-  return base::ValuesEquivalent(axis_, other.axis_) &&
-         base::ValuesEquivalent(scroller_, other.scroller_);
+  return base::ValuesEquivalent(scroller_, other.scroller_) &&
+         base::ValuesEquivalent(axis_, other.axis_);
 }
 
 void CSSScrollValue::TraceAfterDispatch(blink::Visitor* visitor) const {
   CSSValue::TraceAfterDispatch(visitor);
-  visitor->Trace(axis_);
   visitor->Trace(scroller_);
+  visitor->Trace(axis_);
 }
 
 }  // namespace cssvalue
