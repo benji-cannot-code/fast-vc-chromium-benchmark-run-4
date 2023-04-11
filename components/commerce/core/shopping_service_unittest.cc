dd1dd2d89dc51b95557979971178ee014a7b13f4
Matt Jones <mdjones@chromium.org>
2023-04-11 18:49:32 +0000

    [Shopping service] Relax restrictions on product and merchant apis
    This patch changes the CHECKs in the product info and merchant trust
    apis to early returns that execute the callback with a nullopt. This
    makes the system more consistent (e.g. always getting a callback) and
    less error-prone as other features can check for product or merchant information without fear of crashing the browser. The shopping
    service tests have been updated to reflect these changes.
    
    Bug: b:266449217
    Change-Id: I37cc6e77036665b27323549ff45b97a09918035a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414284
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128793}

--- components/commerce/core/shopping_service_unittest.cc
+++ components/commerce/core/shopping_service_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/power_bookmarks/core/proto/power_bookmark_meta.pb.h"
 #include "components/power_bookmarks/core/proto/shopping_specifics.pb.h"
 #include "components/prefs/testing_pref_service.h"
+#include "components/search/ntp_features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using optimization_guide::OptimizationGuideDecision;
@@ -112,6 +113,27 @@ TEST_F(ShoppingServiceTest, TestProductInfoResponse) {
             run_loop->Quit();
           },
           &run_loop));
+  run_loop.Run();
+}
+
+// Test that the product info api fails gracefully (callback run with nullopt)
+// if it is disabled.
+TEST_F(ShoppingServiceTest, TestProductInfoResponse_ApiDisabled) {
+  // Ensure a feature that uses product info is disabled.
+  test_features_.InitWithFeatures({},
+                                  {kShoppingList, kShoppingListRegionLaunched,
+                                   ntp_features::kNtpChromeCartModule});
+
+  base::RunLoop run_loop;
+  shopping_service_->GetProductInfoForUrl(
+      GURL(kProductUrl), base::BindOnce(
+                             [](base::RunLoop* run_loop, const GURL& url,
+                                const absl::optional<ProductInfo>& info) {
+                               ASSERT_EQ(kProductUrl, url.spec());
+                               ASSERT_FALSE(info.has_value());
+                               run_loop->Quit();
+                             },
+                             &run_loop));
   run_loop.Run();
 }
 
@@ -380,6 +402,24 @@ TEST_F(ShoppingServiceTest, TestMerchantInfoResponse) {
             run_loop->Quit();
           },
           &run_loop));
+  run_loop.Run();
+}
+
+// Test that the merchant info fails gracefully when the api is disabled.
+TEST_F(ShoppingServiceTest, TestMerchantInfoResponse_ApiDisabled) {
+  // Ensure a feature that uses merchant info is disabled.
+  test_features_.InitAndDisableFeature(kCommerceMerchantViewer);
+
+  base::RunLoop run_loop;
+  shopping_service_->GetMerchantInfoForUrl(
+      GURL(kMerchantUrl), base::BindOnce(
+                              [](base::RunLoop* run_loop, const GURL& url,
+                                 absl::optional<MerchantInfo> info) {
+                                ASSERT_EQ(kMerchantUrl, url.spec());
+                                ASSERT_FALSE(info.has_value());
+                                run_loop->Quit();
+                              },
+                              &run_loop));
   run_loop.Run();
 }
 
