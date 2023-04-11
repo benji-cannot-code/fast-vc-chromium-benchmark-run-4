761e80d1ccba284c2041693e1b26efad9d6f91a0
Emilia Paz <emiliapaz@chromium.org>
2023-04-11 21:03:39 +0000

    [Extensions c2s] Change context menu icon when extension is pinned
    Change context menu icon to pin icon only for button normal state when
    the action is pinned in the toolbar. All other button states should
    maintain the same icon, as well as the callback.
    
    Screencast: https://drive.google.com/file/d/1iPi3f3UCTqELNlMJBZcszWcIHW4ccOa5/view?usp=sharing
    
    Bug: 1390952
    Change-Id: Ifb56a7af75f0ff4d0565c4b70ddca069318e61e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4366864
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128886}

--- chrome/browser/ui/views/extensions/extensions_menu_main_page_view_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view_unittest.cc
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/extensions/chrome_test_extension_loader.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
+#include "chrome/browser/ui/views/controls/hover_button.h"
 #include "chrome/browser/ui/views/extensions/extensions_menu_button.h"
 #include "chrome/browser/ui/views/extensions/extensions_menu_coordinator.h"
 #include "chrome/browser/ui/views/extensions/extensions_menu_item_view.h"
@@ -24,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/test/permissions_manager_waiter.h"
 #include "extensions/test/test_extension_dir.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "ui/gfx/image/image_unittest_util.h"
+#include "ui/views/vector_icons.h"
 #include "ui/views/view_utils.h"
 
 namespace {
@@ -341,6 +346,72 @@ TEST_F(ExtensionsMenuMainPageViewUnitTest,
   }
 }
 
+// Verifies the pin button appears on the menu item, in place of context menu
+// button when state is normal, when an extension is pinned.
+TEST_F(ExtensionsMenuMainPageViewUnitTest, PinnedExtensions) {
+  auto extension = InstallExtension("Test Extension");
+
+  ShowMenu();
+  EXPECT_EQ(menu_items().size(), 1u);
+  HoverButton* context_menu_button =
+      GetOnlyMenuItem()->context_menu_button_for_testing();
+
+  const ui::ColorProvider* color_provider =
+      context_menu_button->GetColorProvider();
+  auto pin_icon = gfx::Image(gfx::CreateVectorIcon(
+      views::kPinIcon,
+      color_provider->GetColor(kColorExtensionMenuPinButtonIcon)));
+  auto three_dot_icon = gfx::Image(gfx::CreateVectorIcon(
+      kBrowserToolsIcon, color_provider->GetColor(kColorExtensionMenuIcon)));
+
+  // Verify context menu button has three dot icon for all button states.
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_NORMAL)),
+      three_dot_icon));
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_HOVERED)),
+      three_dot_icon));
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_PRESSED)),
+      three_dot_icon));
+
+  // Pin extension.
+  auto* toolbar_model = ToolbarActionsModel::Get(profile());
+  ASSERT_TRUE(toolbar_model);
+  toolbar_model->SetActionVisibility(extension->id(), true);
+  LayoutMenuIfNecessary();
+
+  // Verify context menu button changes to pin icon only in normal state.
+  // Pin icon is tested by checking it is NOT the three dot icon because of a
+  // weird reason when retrieving the pin icon. We can do this because a) no
+  // other icon is expected, and b) pin icon correctly appears when manually
+  // testing.
+  EXPECT_FALSE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_NORMAL)),
+      three_dot_icon));
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_HOVERED)),
+      three_dot_icon));
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_PRESSED)),
+      three_dot_icon));
+
+  // Unpin extension.
+  toolbar_model->SetActionVisibility(extension->id(), false);
+  LayoutMenuIfNecessary();
+
+  // Verify context menu button has three dot icon for all button states.
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_NORMAL)),
+      three_dot_icon));
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_HOVERED)),
+      three_dot_icon));
+  EXPECT_TRUE(gfx::test::AreImagesEqual(
+      gfx::Image(context_menu_button->GetImage(views::Button::STATE_PRESSED)),
+      three_dot_icon));
+}
+
 TEST_F(ExtensionsMenuMainPageViewUnitTest, DisableAndEnableExtension) {
   constexpr char kName[] = "Test Extension";
   auto extension_id = InstallExtension(kName)->id();
@@ -423,8 +494,7 @@ TEST_F(ExtensionsMenuMainPageViewUnitTest, ReloadExtensionFailed) {
   base::RunLoop().RunUntilIdle();
   LayoutMenuIfNecessary();
 
-  // Verify the extension is no longer visible in the menu or on the toolbar
-  // since it was removed.
+  // Verify the extension is no longer visible in the menu.
   EXPECT_EQ(menu_items().size(), 0u);
 }
 
