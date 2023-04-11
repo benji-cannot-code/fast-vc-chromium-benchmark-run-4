56b63c1b0cb238be54f26c7dfe426f29dc9f618a
David Dorwin <ddorwin@chromium.org>
2023-04-11 14:42:52 +0000

    [base] Improve file_path_watcher_stub.cc for use on Fuchsia.
    With the removal of inotify (https://crbug.com/1430566), Fuchsia must
    once again use a stub. Since there are no near-term plans to
    reimplement this functionality, use the existing stub implementation.
    
    This change makes the stub more complete by incorporating aspects of `file_path_watcher_fuchsia.cc` as it was when it was removed in
    https://crrev.com/c/3540915.
    
    Bug: 1430566
    Change-Id: I362706044804fbac8940a1408ee81a8d66f47365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400931
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128634}

--- base/files/file_path_watcher_stub.cc
+++ base/files/file_path_watcher_stub.cc
@@ -3,12 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file exists for Unix systems which don't have the inotify headers, and
-// thus cannot build file_watcher_inotify.cc
+// This file exists for systems for which Chromium does not support watching
+// file paths. This includes Unix systems that don't have the inotify headers
+// and thus cannot build file_watcher_inotify.cc.
 
 #include "base/files/file_path_watcher.h"
 
-#include "base/memory/ptr_util.h"
+#include <memory>
+
+#include "base/notreached.h"
+#include "base/task/sequenced_task_runner.h"
 
 namespace base {
 
@@ -21,13 +25,16 @@ class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate {
   FilePathWatcherImpl& operator=(const FilePathWatcherImpl&) = delete;
   ~FilePathWatcherImpl() override = default;
 
+  // FilePathWatcher::PlatformDelegate:
   bool Watch(const FilePath& path,
              Type type,
              const FilePathWatcher::Callback& callback) override {
+    DCHECK(!callback.is_null());
+
+    NOTIMPLEMENTED_LOG_ONCE();
     return false;
   }
-
-  void Cancel() override {}
+  void Cancel() override { set_cancelled(); }
 };
 
 }  // namespace
