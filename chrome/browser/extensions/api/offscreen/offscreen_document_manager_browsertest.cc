854b24da692c1861c5f356383ae0615c042466c7
Claudio DeSouza <cdesouza@igalia.com>
2023-04-11 18:07:25 +0000

    Enum modernisation for offscreen.idl
    This CL updates the codegen attribute for the enums described in
    offscreen.idl, so it generates `enum class` types, with updated naming
    conventions.
    
    Bug: 1421546
    Change-Id: I55eb1c00a196394825d605cf18b4ce05380faa26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411418
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1128766}

--- chrome/browser/extensions/api/offscreen/offscreen_document_manager_browsertest.cc
+++ chrome/browser/extensions/api/offscreen/offscreen_document_manager_browsertest.cc
@@ -92,7 +92,7 @@ class OffscreenDocumentManagerBrowserTest : public ExtensionApiTest {
     host_waiter.RestrictToType(mojom::ViewType::kOffscreenDocument);
     OffscreenDocumentHost* offscreen_document =
         OffscreenDocumentManager::Get(&profile)->CreateOffscreenDocument(
-            extension, url, api::offscreen::REASON_TESTING);
+            extension, url, api::offscreen::Reason::kTesting);
     host_waiter.WaitForHostCompletedFirstLoad();
 
     return offscreen_document;
@@ -150,7 +150,7 @@ IN_PROC_BROWSER_TEST_F(OffscreenDocumentManagerBrowserTest,
     host_waiter.RestrictToType(mojom::ViewType::kOffscreenDocument);
     offscreen_document = offscreen_document_manager()->CreateOffscreenDocument(
         *extension, extension->GetResourceURL("offscreen.html"),
-        api::offscreen::REASON_TESTING);
+        api::offscreen::Reason::kTesting);
     ASSERT_TRUE(offscreen_document);
     host_waiter.WaitForHostCompletedFirstLoad();
   }
@@ -370,7 +370,7 @@ IN_PROC_BROWSER_TEST_F(OffscreenDocumentManagerBrowserTest,
   TestLifetimeEnforcer* lifetime_enforcer = nullptr;
   LifetimeEnforcerFactories::TestingOverride factory_override;
   factory_override.map().emplace(
-      api::offscreen::REASON_TESTING,
+      api::offscreen::Reason::kTesting,
       base::BindRepeating(&CreateTestLifetimeEnforcer, &lifetime_enforcer));
 
   // Load an extension and create an offscreen document.
@@ -417,7 +417,7 @@ IN_PROC_BROWSER_TEST_F(OffscreenDocumentManagerBrowserTest,
   TestLifetimeEnforcer* lifetime_enforcer = nullptr;
   LifetimeEnforcerFactories::TestingOverride factory_override;
   factory_override.map().emplace(
-      api::offscreen::REASON_TESTING,
+      api::offscreen::Reason::kTesting,
       base::BindRepeating(&CreateTestLifetimeEnforcer, &lifetime_enforcer));
 
   // Load an extension an create an offscreen document.
