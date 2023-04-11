2eb5822db6cd6a7dd590561334a46abfb73c3dbc
Lauren Winston <lwinston@google.com>
2023-04-11 00:54:39 +0000

    [Read Anything] Ensure ReadAnythingAppModel has proper test coverage.
    From code coverage analysis, this should be the final CL in breaking
    ReadAnythingModel tests out from the controller.
    
    Bug: 1266555
    Change-Id: I3b7285b82ae284156e4462ac7acc614450bab509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400434
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128429}

--- chrome/renderer/accessibility/read_anything_app_model.h
+++ chrome/renderer/accessibility/read_anything_app_model.h
@@ -74,7 +74,6 @@ class ReadAnythingAppModel {
   bool NodeIsContentNode(ui::AXNodeID ax_node_id) const;
   void OnThemeChanged(read_anything::mojom::ReadAnythingThemePtr new_theme);
 
-  void InsertDisplayNode(ui::AXNodeID node);
   void Reset(const std::vector<ui::AXNodeID>& content_node_ids);
   bool PostProcessSelection();
   // Helper functions for the rendering algorithm. Post-process the AXTree and
@@ -124,6 +123,7 @@ class ReadAnythingAppModel {
   double GetLineSpacingValue(
       read_anything::mojom::LineSpacing line_spacing) const;
 
+  void InsertDisplayNode(ui::AXNodeID node);
   void ResetSelection();
   void InsertSelectionNode(ui::AXNodeID node);
   void UpdateSelection();
