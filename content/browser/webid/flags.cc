5053ec3139cdb12e5ecde67fb9b47b8bfb6f77c1
Sam Goto <goto@chromium.org>
2023-04-11 19:43:55 +0000

    [fedcm] Pass the AuthZ API parameters to the id assertion endpoint
    Change-Id: If6dd7266f0ff50f172f91ce16ed8069fffe3683b
    Bug: 1429083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394512
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128829}

--- content/browser/webid/flags.cc
+++ content/browser/webid/flags.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+bool IsFedCmAuthzEnabled() {
+  return base::FeatureList::IsEnabled(features::kFedCmAuthz);
+}
+
 bool IsFedCmAutoReauthnEnabled() {
   return base::FeatureList::IsEnabled(features::kFedCmAutoReauthn);
 }
