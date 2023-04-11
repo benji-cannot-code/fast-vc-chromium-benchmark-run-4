f60069ef557ed8e4bd29bc6938faa2a3cdcba695
François Beaufort <beaufort.francois@gmail.com>
2023-04-11 17:08:55 +0000

    Web NFC: Delay disabling reader mode
    This CL adds a 500ms delay when disabling reader mode so that subsequent
    NFC operations don't require re-connecting to NFC devices, thus fasten
    things up.
    
    Change-Id: I007378b9a67153e97547c074fd92388ea99bf995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411557
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1128714}

--- services/device/nfc/android/junit/src/org/chromium/device/nfc/NFCTest.java
+++ services/device/nfc/android/junit/src/org/chromium/device/nfc/NFCTest.java
@@ -45,6 +45,9 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.task.TaskTraits;
+import org.chromium.base.task.test.ShadowPostTask;
+import org.chromium.base.task.test.ShadowPostTask.TestImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.device.mojom.NdefError;
@@ -71,7 +74,7 @@ import java.util.List;
  * Unit tests for NfcImpl and NdefMessageUtils classes.
  */
 @RunWith(BaseRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
+@Config(manifest = Config.NONE, shadows = {ShadowPostTask.class})
 public class NFCTest {
     private TestNfcDelegate mDelegate;
     private int mNextWatchId;
@@ -140,6 +143,12 @@ public class NFCTest {
 
     @Before
     public void setUp() {
+        ShadowPostTask.setTestImpl(new TestImpl() {
+            @Override
+            public void postDelayedTask(@TaskTraits int taskTraits, Runnable task, long delay) {
+                task.run();
+            }
+        });
         MockitoAnnotations.initMocks(this);
         mDelegate = new TestNfcDelegate(mActivity);
         doReturn(mNfcManager).when(mContext).getSystemService(Context.NFC_SERVICE);
