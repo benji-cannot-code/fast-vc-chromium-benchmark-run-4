0b94b4f592d8a5fbb34e01a32cb8a8f52941e8aa
Ilkin Safarli <safarli@google.com>
2023-04-11 01:49:02 +0000

    CrOS Settings: Convert multidevice_wifi_sync_item browser test to TS
    Bug: b:270728282
    Test: browser_tests --gtest_filter="OSSettingsMultidevicePageMultideviceWifiSyncItem*"
    Change-Id: I9907dc576d992a95ca80550dc27c0acb14a46f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408269
    Commit-Queue: Ilkin Safarli <safarli@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128447}

--- chrome/test/data/webui/settings/chromeos/multidevice_page/multidevice_wifi_sync_item_tests.ts
+++ chrome/test/data/webui/settings/chromeos/multidevice_page/multidevice_wifi_sync_item_tests.ts
@@ -5,74 +5,80 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://os-settings/chromeos/os_settings.js';
 
-import {OsSyncBrowserProxyImpl} from 'chrome://os-settings/chromeos/lazy_load.js';
-import {webUIListenerCallback} from 'chrome://resources/ash/common/cr.m.js';
+import {OsSyncBrowserProxy, OsSyncBrowserProxyImpl, OsSyncPrefs} from 'chrome://os-settings/chromeos/lazy_load.js';
+import {assert} from 'chrome://resources/js/assert_ts.js';
+import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
-// Prefs used by settings-multidevice-wifi-sync-item.
-function getOsPrefs() {
-  return {
-    osWifiConfigurationsRegistered: true,
-    osWifiConfigurationsSynced: true,
-  };
-}
-
-class TestOsSyncBrowserProxy extends TestBrowserProxy {
+class TestOsSyncBrowserProxy extends TestBrowserProxy implements
+    OsSyncBrowserProxy {
   constructor() {
     super([
+      'setOsSyncDatatypes',
       'sendOsSyncPrefsChanged',
     ]);
   }
 
-  /** @override */
-  sendOsSyncPrefsChanged() {
+  didNavigateToOsSyncPage(): void {}
+  didNavigateAwayFromOsSyncPage(): void {}
+
+  setOsSyncDatatypes(osSyncPrefs: OsSyncPrefs): void {
+    this.methodCalled('setOsSyncDatatypes', osSyncPrefs);
+  }
+
+  sendOsSyncPrefsChanged(): void {
     this.methodCalled('sendOsSyncPrefsChanged');
   }
 }
 
-suite('Multidevice_WifiSyncItem_CategorizationEnabled', function() {
-  let wifiSyncItem;
+suite('<settings-multidevice-wifi-sync-item>', () => {
+  // Prefs used by settings-multidevice-wifi-sync-item.
+  const osSyncPrefs = {
+    osWifiConfigurationsRegistered: true,
+    osWifiConfigurationsSynced: true,
+  };
 
-  setup(function() {
+  let wifiSyncItem: HTMLElement;
+
+  setup(() => {
     const browserProxy = new TestOsSyncBrowserProxy();
     OsSyncBrowserProxyImpl.setInstanceForTesting(browserProxy);
-
-    PolymerTest.clearBody();
-
     wifiSyncItem =
         document.createElement('settings-multidevice-wifi-sync-item');
     document.body.appendChild(wifiSyncItem);
     flush();
   });
 
-  teardown(function() {
+  teardown(() => {
     wifiSyncItem.remove();
   });
 
   test('Wifi Sync off', async () => {
-    const prefs = getOsPrefs();
+    const prefs = osSyncPrefs;
     prefs.osWifiConfigurationsSynced = false;
     webUIListenerCallback('os-sync-prefs-changed', prefs);
     flush();
 
-    assertTrue(!!wifiSyncItem.shadowRoot.querySelector(
+    assert(wifiSyncItem.shadowRoot!.querySelector(
         'settings-multidevice-wifi-sync-disabled-link'));
 
-    const toggle = wifiSyncItem.shadowRoot.querySelector('cr-toggle');
-    assertTrue(!!toggle);
+    const toggle = wifiSyncItem.shadowRoot!.querySelector('cr-toggle');
+    assert(toggle);
     assertTrue(toggle.disabled);
     assertFalse(toggle.checked);
   });
 
   test('Wifi Sync on', async () => {
-    const prefs = getOsPrefs();
+    const prefs = osSyncPrefs;
     prefs.osWifiConfigurationsSynced = true;
     webUIListenerCallback('os-sync-prefs-changed', prefs);
     flush();
 
-    assertFalse(!!wifiSyncItem.shadowRoot.querySelector(
-        'settings-multidevice-wifi-sync-disabled-link'));
+    assertEquals(
+        null,
+        wifiSyncItem.shadowRoot!.querySelector(
+            'settings-multidevice-wifi-sync-disabled-link'));
   });
 });
