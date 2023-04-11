c85bbae3c411ba4905d0a5edb5ef96a3f5fbf0f6
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-04-11 21:20:52 +0000

    Record PasswordManager.AccountStorageUserStateDuration.* metrics on iOS
    This metric was already recorded on desktop, record it on iOS too now.
    
    Bug: 1392747
    Change-Id: Ic3fa35e1c46f24516cdea26636a6bb1707f39535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404117
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128907}

--- ios/chrome/app/app_metrics_app_state_agent_unittest.mm
+++ ios/chrome/app/app_metrics_app_state_agent_unittest.mm
@@ -25,8 +25,7 @@ namespace {
 class FakeProfileSessionDurationsService
     : public IOSProfileSessionDurationsService {
  public:
-  FakeProfileSessionDurationsService()
-      : IOSProfileSessionDurationsService(nullptr, nullptr) {}
+  FakeProfileSessionDurationsService() = default;
 
   FakeProfileSessionDurationsService(
       const FakeProfileSessionDurationsService&) = delete;
--- ios/chrome/browser/metrics/ios_profile_session_durations_service.h
+++ ios/chrome/browser/metrics/ios_profile_session_durations_service.h
@@ -8,13 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/sync/driver/sync_session_durations_metrics_recorder.h"
+
+class PrefService;
+
+namespace password_manager {
+class PasswordSessionDurationsMetricsRecorder;
+}
 
 namespace signin {
 class IdentityManager;
 }
+
 namespace syncer {
 class SyncService;
+class SyncSessionDurationsMetricsRecorder;
 }
 
 // Tracks the active browsing time that the user spends signed in and/or syncing
@@ -22,9 +29,9 @@ class SyncService;
 class IOSProfileSessionDurationsService : public KeyedService {
  public:
   // Callers must ensure that the parameters outlive this object.
-  // If `sync_service` and `identity_manager` are null, then this object does
-  // not monitor profile session durations.
+  // `sync_service`, `pref_service` and `identity_manager` must be non-null.
   IOSProfileSessionDurationsService(syncer::SyncService* sync_service,
+                                    PrefService* pref_service,
                                     signin::IdentityManager* identity_manager);
 
   IOSProfileSessionDurationsService(const IOSProfileSessionDurationsService&) =
@@ -47,9 +54,16 @@ class IOSProfileSessionDurationsService : public KeyedService {
   bool IsSignedIn() const;
   bool IsSyncing() const;
 
+ protected:
+  // Test-only constructor that doesn't do anything and fake implementations
+  // can invoke.
+  IOSProfileSessionDurationsService();
+
  private:
   std::unique_ptr<syncer::SyncSessionDurationsMetricsRecorder>
-      metrics_recorder_;
+      sync_metrics_recorder_;
+  std::unique_ptr<password_manager::PasswordSessionDurationsMetricsRecorder>
+      password_metrics_recorder_;
 
   bool is_session_active_ = false;
 };
