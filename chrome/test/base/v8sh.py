47cac7c68fcf658ec1c073606a465b0744b79d79
Nico Weber <thakis@chromium.org>
2023-04-11 17:35:01 +0000

    v8sh.py: Minor cleanups
    * Remove no-op --external= option
    * Embrace py3 a bit more: Remove __future__ import, update run line,
      use subprocess.run()
    
    No behavior change.
    
    Bug: none
    Change-Id: I7c15ddf5624acfa86349628fbdb4fa83c8a08c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415218
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128740}

--- chrome/test/base/v8sh.py
+++ chrome/test/base/v8sh.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2012 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Wrapper for using the V8 shell in js2gtest.gni's js2gtest template
 to maintain the argument lists and to generate inlinable tests.
 """
-
-from __future__ import print_function
 
 import json
 import argparse
@@ -47,10 +45,6 @@ def main ():
                       action="store",
                       help=("Path to deps.js for dependency resolution, " +
                             "optional."))
-  parser.add_argument('--external',
-                      action='store',
-                      help="Load V8's initial snapshot from " +
-                      "external files (y/n)")
   args = parser.parse_args()
 
   cmd = [args.v8_shell]
@@ -66,15 +60,13 @@ def main ():
     print(cmd)
   if not args.impotent:
     try:
-      p = subprocess.Popen(
-          cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
-      out, err = p.communicate()
+      p = subprocess.run(cmd, capture_output=True)
       if p.returncode != 0:
-        sys.stderr.write((out + err).decode('utf-8'))
+        sys.stderr.write((p.stdout + p.stderr).decode('utf-8'))
         return 1
-      if not HasSameContent(args.cxxoutfile, out):
+      if not HasSameContent(args.cxxoutfile, p.stdout):
         with open(args.cxxoutfile, 'wb') as f:
-          f.write(out)
+          f.write(p.stdout)
       shutil.copyfile(args.inputfile, args.jsoutfile)
     except Exception as ex:
       if os.path.exists(args.cxxoutfile):
