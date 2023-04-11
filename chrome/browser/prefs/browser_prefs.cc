05d82dd21194f0f9d11ccf0df7706c0f784e1d1f
Dan Harrington <harringtond@chromium.org>
2023-04-11 18:23:57 +0000

    Remove components/ntp_snippets and references
    For prefs deprecation, I noticed we removed calls to the
    code which registered these prefs a while back without
    following the deprecation procedure. This change will
    register the prefs for both android and ios,
    and clear them, so they can be correctly cleaned up.
    
    Bug: 1424924
    Change-Id: I27caf9da45e22cd77765f54369f515a93b596bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395467
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128777}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -251,6 +251,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/video_tutorials/prefs.h"
 #include "components/cdm/browser/media_drm_storage_impl.h"  // nogncheck crbug.com/1125897
 #include "components/content_creation/notes/core/note_prefs.h"
+#include "components/ntp_snippets/register_prefs.h"
 #include "components/ntp_tiles/popular_sites_impl.h"
 #include "components/permissions/contexts/geolocation_permission_context_android.h"
 #include "components/query_tiles/tile_service_prefs.h"
@@ -1157,6 +1158,11 @@ void RegisterProfilePrefsForMigration(
   registry->RegisterDictionaryPref(kTypeSubscribedForInvalidations);
   registry->RegisterStringPref(kActiveRegistrationToken, std::string());
   registry->RegisterStringPref(kFCMInvalidationClientIDCache, std::string());
+
+  // Deprecated 04/2023.
+#if BUILDFLAG(IS_ANDROID)
+  ntp_snippets::prefs::RegisterProfilePrefsForMigrationApril2023(registry);
+#endif
 }
 
 }  // namespace
@@ -2220,6 +2226,11 @@ void MigrateObsoleteProfilePrefs(Profile* profile) {
   profile_prefs->ClearPref(kTypeSubscribedForInvalidations);
   profile_prefs->ClearPref(kActiveRegistrationToken);
   profile_prefs->ClearPref(kFCMInvalidationClientIDCache);
+
+  // Added 04/2023.
+#if BUILDFLAG(IS_ANDROID)
+  ntp_snippets::prefs::MigrateObsoleteProfilePrefsApril2023(profile_prefs);
+#endif
 
   // Please don't delete the following line. It is used by PRESUBMIT.py.
   // END_MIGRATE_OBSOLETE_PROFILE_PREFS
