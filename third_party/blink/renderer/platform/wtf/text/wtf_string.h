13d89db8f76b77900ff841770911b55295bd0cc8
Avi Drissman <avi@chromium.org>
2023-04-12 07:33:54 +0000

    Remove unneeded <objc/objc.h> includes
    We're looking to ban <objc/objc.h> includes, so remove them in cases
    where they're not needed and not used.
    
    Bug: 1429730
    Change-Id: Ib6c34ffd50965132302cf6b1bcac21a4b0c6c01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416175
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129119}

--- third_party/blink/renderer/platform/wtf/text/wtf_string.h
+++ third_party/blink/renderer/platform/wtf/text/wtf_string.h
@@ -40,10 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 
-#ifdef __OBJC__
-#include <objc/objc.h>
-#endif
-
 namespace WTF {
 
 #define DISPATCH_CASE_OP(caseSensitivity, op, args)     \
