ed3778564cb05b7269de65fbe7e8aed16b9c5043
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-04-12 19:58:37 +0000

    [RemoveLegacy] Remove vertical_position_cache.h & glyph_overflow.h
    As above - there should be no behaviour change.
    
    Bug: 1229581
    Change-Id: I94d5fd60980f9da5552f792ce70d8218cad95b74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4418758
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129437}

--- third_party/blink/renderer/core/layout/layout_text.cc
+++ third_party/blink/renderer/core/layout/layout_text.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_object_inlines.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/line/abstract_inline_text_box.h"
-#include "third_party/blink/renderer/core/layout/line/glyph_overflow.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/layout_ng_text_combine.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_abstract_inline_text_box.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h"
