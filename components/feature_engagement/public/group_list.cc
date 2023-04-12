11848a68bf32c1270da055ab0af6c6ef6b1c7f1e
Robbie Gibson <rkgibson@google.com>
2023-04-12 05:30:16 +0000

    Add iOS Promo group to list of Feature Engagment Groups
    The promo group was missing from the list of groups, so that group's
    GroupConfig wasn't being parsed.
    
    Fixed: 1432221, 1432218
    Change-Id: I61192efe76834adc331aca2742d6f2acc3e4e611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415780
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129094}

--- components/feature_engagement/public/group_list.cc
+++ components/feature_engagement/public/group_list.cc
@@ -12,7 +12,10 @@ namespace feature_engagement {
 
 namespace {
 const base::Feature* const kAllGroups[] = {
-    &kIPHDummyGroup,
+    &kIPHDummyGroup,  // Ensures non-empty array for all platforms.
+#if BUILDFLAG(IS_IOS)
+    &kiOSFullscreenPromosGroup,
+#endif  // BUILDFLAG(IS_IOS)
 };
 }  // namespace
 
