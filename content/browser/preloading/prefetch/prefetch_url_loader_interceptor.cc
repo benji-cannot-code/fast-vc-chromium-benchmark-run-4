d78011a3cfedd8e8d96e4162440cc08b6a86667d
Max Curran <curranmax@chromium.org>
2023-04-12 19:19:28 +0000

    Added metrics for handling redirects when prefetching.
    This CL adds the following histograms:
    - PrefetchProxy.Prefetch.RedirectChainSize --> The redirect chain size
      of a prefetch. Recorded for every prefetch.
    - PrefetchProxy.AfterClick.RedirectChainSize --> The redirect chain size
      of a served prefetch. Recorded only for prefetches that were served.
    - PrefetchProxy.Redirect.Result --> The result of handling a redirect
      when prefetching. Recorded for every redirect.
    - PrefetchProxy.Redirect.NetworkContextStateTransition --> The
      transition of network contexts required for a redirect. Recorded for
      every redirect.
    - PrefetchProxy.AfterClick.WasFullRedirectChainServed --> Whether the
      full redirect chain of a prefetch was served, or just part of the
      redirect chain was served. Recorded only for prefetches with at least
      one redirect.
    
    Bug: 1430581
    Change-Id: I9a4741f31e8c2f676e35b99293f2618f2d27060e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400084
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Commit-Queue: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129413}

--- content/browser/preloading/prefetch/prefetch_url_loader_interceptor.cc
+++ content/browser/preloading/prefetch/prefetch_url_loader_interceptor.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_macros.h"
 #include "content/browser/browser_context_impl.h"
 #include "content/browser/loader/navigation_loader_interceptor.h"
 #include "content/browser/preloading/prefetch/prefetch_container.h"
@@ -31,6 +32,12 @@ BrowserContext* BrowserContextFromFrameTreeNodeId(int frame_tree_node_id) {
   if (!web_content)
     return nullptr;
   return web_content->GetBrowserContext();
+}
+
+void RecordWasFullRedirectChainServedHistogram(
+    bool was_full_redirect_chain_served) {
+  UMA_HISTOGRAM_BOOLEAN("PrefetchProxy.AfterClick.WasFullRedirectChainServed",
+                        was_full_redirect_chain_served);
 }
 
 }  // namespace
@@ -69,6 +76,11 @@ void PrefetchURLLoaderInterceptor::MaybeCreateLoader(
                        weak_factory_.GetWeakPtr()),
         redirect_prefetch_container_);
     return;
+  }
+
+  if (redirect_prefetch_container_) {
+    RecordWasFullRedirectChainServedHistogram(false);
+    redirect_prefetch_container_ = nullptr;
   }
 
   GetPrefetch(
@@ -121,6 +133,9 @@ void PrefetchURLLoaderInterceptor::OnGetPrefetchComplete(
         base::MakeRefCounted<network::SingleRequestURLLoaderFactory>(
             raw_prefetch_streaming_url_loader->ServingFinalResponseHandler(
                 std::move(prefetch_streaming_url_loader)));
+    if (redirect_prefetch_container_) {
+      RecordWasFullRedirectChainServedHistogram(true);
+    }
     redirect_prefetch_container_ = nullptr;
   } else {
     single_request_url_loader_factory =
