af8f79b50bbe6d6b31d8175ce3b82c515dd7fdbd
Charles Hager <clhager@google.com>
2023-04-12 20:21:26 +0000

    Add coordinator/mediator for device lock UI
    Bug: 1426859
    Change-Id: If771145efc8cd9043a1823b259ae7554ef81bd01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412714
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129455}

---
+++ chrome/browser/ui/android/device_lock/javatests/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockCoordinatorTest.java
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.device_lock;
+
+import static org.junit.Assert.assertNotEquals;
+
+import android.content.Context;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.base.test.util.Batch;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+
+/**
+ * Tests for {@link DeviceLockCoordinator}.
+ */
+@RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.UNIT_TESTS)
+public class DeviceLockCoordinatorTest {
+    private Context mContext;
+
+    @Before
+    public void setUpTest() {
+        mContext = ContextUtils.getApplicationContext();
+    }
+
+    // TODO(crbug.com/1432028): Add more tests, particularly render tests, once flow is finalized
+    @Test
+    @SmallTest
+    public void testDeviceLockCoordinator_simpleTest() {
+        DeviceLockCoordinator deviceLockCoordinator =
+                new DeviceLockCoordinator(true, new MockDelegate(), mContext);
+        assertNotEquals(deviceLockCoordinator, null);
+
+        deviceLockCoordinator.destroy();
+    }
+
+    private class MockDelegate implements DeviceLockCoordinator.Delegate {
+        @Override
+        public void onDeviceLockReady() {}
+
+        @Override
+        public void onDeviceLockRefused() {}
+    }
+}
