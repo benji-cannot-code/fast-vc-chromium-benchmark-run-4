edd889216bb7da109de04fbb2987bd5cfd1f3006
Morten Stenshorne <mstensho@chromium.org>
2023-04-12 18:13:07 +0000

    [RemoveLegacy] Remove BlockPainter.
    Bug: 1229581
    Change-Id: I94e05795f47e9dbe71b40b1cd02cff72c75fc76e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414448
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129367}

--- third_party/blink/renderer/core/paint/svg_foreign_object_painter.cc
+++ third_party/blink/renderer/core/paint/svg_foreign_object_painter.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/layout/ng/svg/layout_ng_svg_foreign_object.h"
-#include "third_party/blink/renderer/core/paint/block_painter.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_painter.h"
