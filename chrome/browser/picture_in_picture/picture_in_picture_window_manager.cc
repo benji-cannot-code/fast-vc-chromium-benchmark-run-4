08dab483082042d55aa7e6b2bc247a94a44957f7
Benjamin Keen <bkeen@google.com>
2023-04-12 00:38:17 +0000

    Fix Document PiP window inner bounds.
    This change ensures that the inner bounds, of the Document Picture in Picture window, respect their minimum size.
    
    Bug: b/275772381
    Change-Id: I3adab63b6f51be9f5f328eace26dbd6d05c786be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401174
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129018}

--- chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
@@ -193,7 +193,7 @@ PictureInPictureWindowManager::CalculateInitialPictureInPictureWindowBounds(
     gfx::Size window_size(base::saturated_cast<int>(pip_options.width),
                           base::saturated_cast<int>(pip_options.height));
     window_size.SetToMin(GetMaximumWindowSize(display));
-    window_size.SetToMax(GetMinimumWindowSize());
+    window_size.SetToMax(GetMinimumInnerWindowSize());
     window_bounds = gfx::Rect(window_size);
   } else {
     // Otherwise, fall back to the aspect ratio.
@@ -202,10 +202,10 @@ PictureInPictureWindowManager::CalculateInitialPictureInPictureWindowBounds(
                                       : 1.0;
     gfx::Size window_size(work_area.width() / 5, work_area.height() / 5);
     window_size.SetToMin(GetMaximumWindowSize(display));
-    window_size.SetToMax(GetMinimumWindowSize());
+    window_size.SetToMax(GetMinimumInnerWindowSize());
     window_bounds = gfx::Rect(window_size);
     gfx::SizeRectToAspectRatio(gfx::ResizeEdge::kTopLeft, initial_aspect_ratio,
-                               GetMinimumWindowSize(),
+                               GetMinimumInnerWindowSize(),
                                GetMaximumWindowSize(display), &window_bounds);
   }
 
@@ -224,7 +224,7 @@ PictureInPictureWindowManager::CalculateInitialPictureInPictureWindowBounds(
 }
 
 // static
-gfx::Size PictureInPictureWindowManager::GetMinimumWindowSize() {
+gfx::Size PictureInPictureWindowManager::GetMinimumInnerWindowSize() {
   return kMinWindowSize;
 }
 
