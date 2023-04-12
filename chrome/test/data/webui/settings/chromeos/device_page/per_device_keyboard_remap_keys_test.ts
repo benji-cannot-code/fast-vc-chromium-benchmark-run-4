658882f1babdd0891e72bf58d0fa4d82ec734f25
Michael Checo <michaelcheco@google.com>
2023-04-12 22:17:49 +0000

    Settings Split: Move restore defaults button to subpage header
    - Note: Refresh icon needs to be updated.
    
    Screenshot: http://screen/45MXWtTyQ6GngGW
    
    Bug: b/241965700
    Change-Id: I047a6b31313a25a799f9c8976122e6793d92d20c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4419506
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129544}

--- chrome/test/data/webui/settings/chromeos/device_page/per_device_keyboard_remap_keys_test.ts
+++ chrome/test/data/webui/settings/chromeos/device_page/per_device_keyboard_remap_keys_test.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CrButtonElement, FakeInputDeviceSettingsProvider, fakeKeyboards, Keyboard, KeyboardRemapModifierKeyRowElement, MetaKey, ModifierKey, Router, routes, setInputDeviceSettingsProviderForTesting, SettingsPerDeviceKeyboardRemapKeysElement} from 'chrome://os-settings/chromeos/os_settings.js';
+import {FakeInputDeviceSettingsProvider, fakeKeyboards, Keyboard, KeyboardRemapModifierKeyRowElement, MetaKey, ModifierKey, Router, routes, setInputDeviceSettingsProviderForTesting, SettingsPerDeviceKeyboardRemapKeysElement} from 'chrome://os-settings/chromeos/os_settings.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
@@ -172,11 +172,7 @@ suite('<settings-per-device-keyboard-remap-keys>', () => {
    * Verify that the restore defaults button will restore the remapping keys.
    */
   test('keyboard remap subpage restore defaults', async () => {
-    // Click the restore defaults button.
-    const restoreButton = page.shadowRoot!.querySelector<CrButtonElement>(
-        '#restoreDefaultsButton');
-    assert(restoreButton);
-    restoreButton.click();
+    page.restoreDefaults();
     await flushTasks();
 
     // The keyboard has "Command" as metaKey, so ctrl key should be restored to
@@ -216,7 +212,7 @@ suite('<settings-per-device-keyboard-remap-keys>', () => {
     assert(page.get('keyboard'));
     await flushTasks();
 
-    restoreButton.click();
+    page.restoreDefaults();
     await flushTasks();
     // The keyboard has "Launcher" as metaKey, meta key should be restored to
     // default metaKey mappings.
@@ -258,10 +254,7 @@ suite('<settings-per-device-keyboard-remap-keys>', () => {
   test('Update keyboard settings', async () => {
     assertTrue(page.get('isInitialized'));
     // Set the modifier remappings to default stage.
-    const restoreButton = page.shadowRoot!.querySelector<CrButtonElement>(
-        '#restoreDefaultsButton');
-    assert(restoreButton);
-    restoreButton.click();
+    page.restoreDefaults();
     checkPrefsSetToDefault();
 
     // Change several key remappings in the page.
