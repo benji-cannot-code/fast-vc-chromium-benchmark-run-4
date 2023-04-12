b13fcc54e810695d7da3831e0b67168d8194e9b5
Ben Reich <benreich@chromium.org>
2023-04-12 03:40:11 +0000

    Reland "[FilesBulkPinning] Extract Stage to a mojom type to share with Settings"
    This reverts commit d9934901020471d25c09a42d118f4e4a96fb5035.
    
    Reason for revert: Fixed up the missing dependency for DriveIntegrationService
    
    Original change's description:
    > Revert "[FilesBulkPinning] Extract Stage to a mojom type to share with Settings"
    >
    > This reverts commit 3ad864d50c4ed6627edbc96bf263593c6f284b49.
    >
    > Reason for revert:
    > Causing a compilation failure:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20Full/241486/overview
    >
    > Original change's description:
    > > [FilesBulkPinning] Extract Stage to a mojom type to share with Settings
    > >
    > > OS Settings uses Mojo to talk with the browser process and this will
    > > result in sharing the Stage across the C++ <-> JS boundary. To avoid
    > > deduplicating the Stage enum in multiple places (once in OS Settings and
    > > once in the PinManager) extract out the enum to a mojo file and use that
    > > in the PinManager. An upcoming CL will integrate it with OS settings
    > > (you can see the full vision at CL:4402135)
    > >
    > > Bug: b:262647284
    > > Change-Id: I858c39acd9dcf0cec394dec87644a553d4c0ea21
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402082
    > > Reviewed-by: Sam McNally <sammc@chromium.org>
    > > Commit-Queue: Ben Reich <benreich@chromium.org>
    > > Reviewed-by: François Degros <fdegros@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1129047}
    >
    > Bug: b:262647284
    > Change-Id: If3033e43eeb7dfdd9a40274f234563064098bc4e
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416938
    > Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    > Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1129049}
    
    Bug: b:262647284
    Change-Id: I438c65e1795a32725040ea4a12c5356ed878a9c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4417117
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129070}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.h
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/elapsed_timer.h"
 #include "chromeos/ash/components/drivefs/drivefs_host_observer.h"
 #include "chromeos/ash/components/drivefs/mojom/drivefs.mojom.h"
+#include "chromeos/ash/components/drivefs/mojom/pin_manager_types.mojom.h"
 #include "components/drive/file_errors.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -44,33 +45,9 @@ enum HumanReadableSize : int64_t;
 COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DRIVEFS)
 std::ostream& operator<<(std::ostream& out, HumanReadableSize size);
 
-// The PinManager first undergoes a setup phase, where it audits the current
-// disk space, pins all available files (disk space willing) then moves to
-// monitoring. This enum represents the various stages the setup goes through.
-enum class Stage {
-  // Initial stage.
-  kStopped,
-
-  // Paused because of unfavorable network conditions.
-  kPaused,
-
-  // In-progress stages.
-  kGettingFreeSpace,
-  kListingFiles,
-  kSyncing,
-
-  // Final success stage.
-  kSuccess,
-
-  // Final error stages.
-  kCannotGetFreeSpace,
-  kCannotListFiles,
-  kNotEnoughSpace,
-  kCannotEnableDocsOffline,
-};
-
+using pin_manager_types::mojom::Stage;
 COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DRIVEFS)
-std::ostream& operator<<(std::ostream& out, Stage stage);
+std::string ToString(Stage stage);
 
 // When the manager is setting up, this struct maintains all the information
 // gathered.
