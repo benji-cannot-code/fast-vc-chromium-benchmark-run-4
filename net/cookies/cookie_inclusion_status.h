17f706c8585310f51d7a12ddad7727fd472efb31
Stephan Hartmann <stha09@googlemail.com>
2023-04-12 08:42:20 +0000

    IWYU: add stdint.h for various integer types in //net
    Bug: 957519
    Change-Id: I7161bc8f5974a4d241c12d618d954db0787ab84c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394856
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129139}

--- net/cookies/cookie_inclusion_status.h
+++ net/cookies/cookie_inclusion_status.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_COOKIES_COOKIE_INCLUSION_STATUS_H_
 #define NET_COOKIES_COOKIE_INCLUSION_STATUS_H_
 
+#include <stdint.h>
+
 #include <bitset>
 #include <cstdint>
 #include <ostream>
