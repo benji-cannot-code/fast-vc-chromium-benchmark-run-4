930d988a783f8bec1666be4d8c54d5daaf96f211
Luciano Pacheco <lucmult@chromium.org>
2023-04-12 08:05:57 +0000

    Files app: Fix selection to ignore undefined
    Change the FileSelectionHandler to ignore undefined value when getting
    the Entry from the file list.
    
    It's unclear how this can get an undefined, so this is a defense in
    depth.
    
    This fixes an error as reported by the crash report in b/277232289.
    
    Bug: b:277232289
    Change-Id: Ia569ad5f7a56738095439a46591516536a87f610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4413947
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129129}

--- ui/file_manager/file_manager/foreground/js/file_selection.js
+++ ui/file_manager/file_manager/foreground/js/file_selection.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {dispatchSimpleEvent} from 'chrome://resources/ash/common/cr_deprecated.js';
 import {assert} from 'chrome://resources/ash/common/assert.js';
+import {dispatchSimpleEvent} from 'chrome://resources/ash/common/cr_deprecated.js';
 import {NativeEventTarget as EventTarget} from 'chrome://resources/ash/common/event_target.js';
 
 import {FileType} from '../../common/js/file_type.js';
@@ -27,7 +27,7 @@ import {ListContainer} from './ui/list_container.js';
 export class FileSelection {
   /**
    * @param {!Array<number>} indexes
-   * @param {!Array<Entry>} entries
+   * @param {!Array<!Entry>} entries
    * @param {!VolumeManager} volumeManager
    */
   constructor(indexes, entries, volumeManager) {
@@ -87,6 +87,9 @@ export class FileSelection {
     this.hasReadOnlyEntry_ = false;
 
     entries.forEach(entry => {
+      if (!entry) {
+        return;
+      }
       if (entry.isFile) {
         this.fileCount += 1;
       } else {
@@ -221,10 +224,14 @@ export class FileSelectionHandler extends EventTarget {
    */
   onFileSelectionChanged() {
     const indexes = this.listContainer_.selectionModel.selectedIndexes;
-    const entries = indexes.map(index => {
-      return /** @type {!Entry} */ (
-          this.directoryModel_.getFileList().item(index));
-    });
+    const entries =
+        indexes
+            .map(
+                index =>
+                    /** @type {!Entry} */ (
+                        this.directoryModel_.getFileList().item(index)))
+            // Filter out undefined for invalid index b/277232289.
+            .filter(entry => !!entry);
     this.selection = new FileSelection(indexes, entries, this.volumeManager_);
 
     if (this.selectionUpdateTimer_) {
