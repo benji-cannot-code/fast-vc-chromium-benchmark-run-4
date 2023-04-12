0bbc47461ec1b19b29d1391cd59d548903ef90de
Daniel White <danieltwhite@google.com>
2023-04-12 14:43:58 +0000

    [iOS] Polish Price Tracking Animations
    In this CL, the animations for the tracking and untracking products in
    the Price Tracking UI was improved. When the user tracks a new product,
    the product slides to the bottom when removed from the 'Current Site'
    section and slides in from the top into the 'Tracked' section. For
    untracking an item, the animation process will be reversed.
    
    Bug: 1429639
    Change-Id: I6ef512c7f50f602dbe868d1d2465548cca5d8607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4389063
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129253}

--- ios/chrome/browser/ui/price_notifications/price_notifications_table_view_controller.mm
+++ ios/chrome/browser/ui/price_notifications/price_notifications_table_view_controller.mm
@@ -188,8 +188,9 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
 
   if (trackableItem && !currentlyTracking) {
     [self addItem:trackableItem
-        toBeginning:YES
-          ofSection:SectionIdentifierTrackableItemsOnCurrentSite];
+             toBeginning:YES
+               ofSection:SectionIdentifierTrackableItemsOnCurrentSite
+        withRowAnimation:UITableViewRowAnimationAutomatic];
   }
 
   if (!self.viewIfLoaded.window) {
@@ -207,32 +208,38 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
   _shouldHideLoadingState = YES;
   [self initializeTableViewModelIfNeeded];
   [self removeLoadingState];
-  BOOL shouldReloadSection = NO;
   TableViewModel* model = self.tableViewModel;
-
   if (!_hasTrackedItems) {
     [model setHeader:
                [self createHeaderForSectionIndex:SectionIdentifierTrackedItems
                                          isEmpty:NO]
         forSectionWithIdentifier:SectionIdentifierTrackedItems];
-    [model setHeader:[self createHeaderForSectionIndex:
-                               SectionIdentifierTableViewHeader
-                                               isEmpty:NO]
-        forSectionWithIdentifier:SectionIdentifierTableViewHeader];
-    shouldReloadSection = YES;
-  }
-  _hasTrackedItems = YES;
-  [self addItem:trackedItem
-      toBeginning:beginning
-        ofSection:SectionIdentifierTrackedItems];
 
-  if (!self.viewIfLoaded.window || !shouldReloadSection) {
+    trackedItem.type = ItemTypeListItem;
+    trackedItem.delegate = self;
+    if (beginning) {
+      [self.tableViewModel insertItem:trackedItem
+              inSectionWithIdentifier:SectionIdentifierTrackedItems
+                              atIndex:0];
+    } else {
+      [self.tableViewModel addItem:trackedItem
+           toSectionWithIdentifier:SectionIdentifierTrackedItems];
+    }
+
+    if (self.viewIfLoaded.window) {
+      [self.tableView reloadSections:[self createIndexSetForSectionIdentifiers:
+                                               {SectionIdentifierTrackedItems}]
+                    withRowAnimation:UITableViewRowAnimationFade];
+    }
+    _hasTrackedItems = YES;
     return;
   }
-  [self.tableView reloadSections:[self createIndexSetForSectionIdentifiers:
-                                           {SectionIdentifierTrackedItems,
-                                            SectionIdentifierTableViewHeader}]
-                withRowAnimation:UITableViewRowAnimationAutomatic];
+
+  _hasTrackedItems = YES;
+  [self addItem:trackedItem
+           toBeginning:beginning
+             ofSection:SectionIdentifierTrackedItems
+      withRowAnimation:UITableViewRowAnimationTop];
 }
 
 - (void)didStopPriceTrackingItem:(PriceNotificationsTableViewItem*)trackedItem
@@ -241,7 +248,6 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
   SectionIdentifier trackedSection = SectionIdentifierTrackedItems;
   SectionIdentifier trackableSection =
       SectionIdentifierTrackableItemsOnCurrentSite;
-  std::vector<SectionIdentifier> sectionsToReload;
   BOOL addItemToTrackableSection =
       isViewingProductSite && ![model hasItemForItemType:ItemTypeListItem
                                        sectionIdentifier:trackableSection];
@@ -251,15 +257,15 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
   [model removeItemWithType:ItemTypeListItem
       fromSectionWithIdentifier:trackedSection
                         atIndex:index.item];
+  BOOL reloadTrackedSection = ![model hasItemForItemType:ItemTypeListItem
+                                       sectionIdentifier:trackedSection];
 
-  if (![model hasItemForItemType:ItemTypeListItem
-               sectionIdentifier:trackedSection]) {
+  if (reloadTrackedSection) {
     _hasTrackedItems = NO;
     [model setHeader:
                [self createHeaderForSectionIndex:SectionIdentifierTrackedItems
                                          isEmpty:YES]
         forSectionWithIdentifier:trackedSection];
-    sectionsToReload.push_back(trackedSection);
   }
 
   if (addItemToTrackableSection) {
@@ -270,7 +276,6 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
     [model insertItem:trackedItem
         inSectionWithIdentifier:trackableSection
                         atIndex:0];
-    sectionsToReload.push_back(trackableSection);
   }
 
   NSString* messageText = l10n_util::GetNSString(
@@ -285,22 +290,30 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
     return;
   }
 
-  if (addItemToTrackableSection) {
-    NSIndexPath* trackableSectionIndex =
-        [model indexPathForItemType:ItemTypeListItem
-                  sectionIdentifier:trackableSection];
-    [self.tableView moveRowAtIndexPath:index toIndexPath:trackableSectionIndex];
-  }
+  [self.tableView
+      performBatchUpdates:^{
+        UITableViewRowAnimation animation = UITableViewRowAnimationMiddle;
+        if (addItemToTrackableSection) {
+          [self.tableView
+                reloadSections:[self createIndexSetForSectionIdentifiers:
+                                         {trackableSection}]
+              withRowAnimation:UITableViewRowAnimationFade];
+          animation = UITableViewRowAnimationFade;
+        }
 
-  if (sectionsToReload.size()) {
-    [self.tableView reloadSections:[self createIndexSetForSectionIdentifiers:
-                                             sectionsToReload]
-                  withRowAnimation:UITableViewRowAnimationAutomatic];
-    return;
-  }
+        if (reloadTrackedSection) {
+          [self.tableView
+                reloadSections:
+                    [self createIndexSetForSectionIdentifiers:{trackedSection}]
+              withRowAnimation:UITableViewRowAnimationFade];
+          return;
+        }
 
-  [self.tableView deleteRowsAtIndexPaths:@[ index ]
-                        withRowAnimation:UITableViewRowAnimationAutomatic];
+        [self.tableView deleteRowsAtIndexPaths:@[ index ]
+                              withRowAnimation:animation];
+      }
+               completion:^(BOOL completion){
+               }];
 }
 
 - (void)didStartPriceTrackingForItem:
@@ -323,9 +336,8 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
 
   [self.tableView
         reloadSections:[self createIndexSetForSectionIdentifiers:
-                                 {SectionIdentifierTableViewHeader,
-                                  SectionIdentifierTrackableItemsOnCurrentSite}]
-      withRowAnimation:UITableViewRowAnimationAutomatic];
+                                 {SectionIdentifierTrackableItemsOnCurrentSite}]
+      withRowAnimation:UITableViewRowAnimationFade];
 
   [self addTrackedItem:trackableItem toBeginning:YES];
 }
@@ -358,8 +370,9 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
 
 // Adds an item to `sectionID` and displays it to the UI.
 - (void)addItem:(PriceNotificationsTableViewItem*)item
-    toBeginning:(BOOL)toBeginning
-      ofSection:(SectionIdentifier)sectionID {
+         toBeginning:(BOOL)toBeginning
+           ofSection:(SectionIdentifier)sectionID
+    withRowAnimation:(UITableViewRowAnimation)animation {
   if (sectionID == SectionIdentifierTrackableItemsOnCurrentSite &&
       [self.tableViewModel
           hasItemForItemType:ItemTypeListItem
@@ -384,7 +397,7 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
 
   [self.tableView
       insertRowsAtIndexPaths:@[ [self.tableViewModel indexPathForItem:item] ]
-            withRowAnimation:UITableViewRowAnimationAutomatic];
+            withRowAnimation:animation];
 }
 
 // Returns a TableViewHeaderFooterItem that displays the title for the section
@@ -457,14 +470,17 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
   emptyTrackedItem.loading = YES;
   emptyTrackedItem.tracking = YES;
   [self addItem:emptyTrackableItem
-      toBeginning:YES
-        ofSection:SectionIdentifierTrackableItemsOnCurrentSite];
+           toBeginning:YES
+             ofSection:SectionIdentifierTrackableItemsOnCurrentSite
+      withRowAnimation:UITableViewRowAnimationAutomatic];
   [self addItem:emptyTrackedItem
-      toBeginning:YES
-        ofSection:SectionIdentifierTrackedItems];
+           toBeginning:YES
+             ofSection:SectionIdentifierTrackedItems
+      withRowAnimation:UITableViewRowAnimationAutomatic];
   [self addItem:emptyTrackedItem
-      toBeginning:YES
-        ofSection:SectionIdentifierTrackedItems];
+           toBeginning:YES
+             ofSection:SectionIdentifierTrackedItems
+      withRowAnimation:UITableViewRowAnimationAutomatic];
   _displayedLoadingState = YES;
 }
 
