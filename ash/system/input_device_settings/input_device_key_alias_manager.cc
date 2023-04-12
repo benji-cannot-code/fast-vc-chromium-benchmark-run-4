7429a46330709d27c0e40109068ecc504df4a042
Michael Checo <michaelcheco@google.com>
2023-04-12 17:17:38 +0000

    Settings Split: Initial commit for DeviceKeyAliasManager
    - DeviceKeyAliasManager will maintain a list containing devices that
      report different PID/VIDs depending on the connection method
      used (USB/Bluetooth). DeviceKeyAliasManager can be used to find the
      "primary" device key for a given device or to retrieve a list
      of aliases associated with a "primary" device key.
    
    Bug: b/241965700
    Change-Id: Ia3d3509a447ed13ab6aac5aaf11ea150ed6c4474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415005
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129330}

---
+++ ash/system/input_device_settings/input_device_key_alias_manager.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/input_device_settings/input_device_key_alias_manager.h"
+
+namespace ash {
+
+InputDeviceKeyAliasManager::InputDeviceKeyAliasManager() = default;
+InputDeviceKeyAliasManager::~InputDeviceKeyAliasManager() = default;
+
+}  // namespace ash
