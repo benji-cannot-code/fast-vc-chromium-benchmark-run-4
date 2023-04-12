5d5ab8c7cfe214d1c485526a6bf03013c11d9159
Tim Sergeant <tsergeant@chromium.org>
2023-04-12 05:19:40 +0000

    Intents: Remove obsolete IntentPickerAction histograms
    Auto-popup behavior has been removed from the Intent Picker, so
    recording IntentPickerAction split by launch source is no longer needed.
    
    Bug: 1431922
    Change-Id: I46011060f707b908a22912c2e24b4678c2904bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4417118
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129091}

--- chrome/browser/apps/intent_helper/metrics/intent_handling_metrics.cc
+++ chrome/browser/apps/intent_helper/metrics/intent_handling_metrics.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
-#include "chrome/browser/apps/intent_helper/chromeos_intent_picker_helpers.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/components/arc/metrics/arc_metrics_service.h"
@@ -153,24 +152,12 @@ IntentHandlingMetrics::IntentHandlingMetrics() = default;
 void IntentHandlingMetrics::RecordIntentPickerMetrics(
     PickerEntryType entry_type,
     IntentPickerCloseReason close_reason,
-    bool should_persist,
-    PickerShowState show_state) {
+    bool should_persist) {
   IntentPickerAction action =
       GetIntentPickerAction(entry_type, close_reason, should_persist);
-  Platform platform = GetIntentPickerDestinationPlatform(action);
-
   UMA_HISTOGRAM_ENUMERATION("ChromeOS.Intents.IntentPickerAction", action);
-  switch (show_state) {
-    case PickerShowState::kOmnibox:
-      UMA_HISTOGRAM_ENUMERATION(
-          "ChromeOS.Intents.IntentPickerAction.FromOmniboxIcon", action);
-      break;
-    case PickerShowState::kPopOut:
-      UMA_HISTOGRAM_ENUMERATION(
-          "ChromeOS.Intents.IntentPickerAction.FromAutoPopOut", action);
-      break;
-  }
 
+  Platform platform = GetIntentPickerDestinationPlatform(action);
   RecordDestinationPlatformMetric(platform);
 }
 
