df246b2fae4826f07fab5fdca5d1bc45296bbb76
Takumi Fujimoto <takumif@chromium.org>
2023-04-12 04:03:09 +0000

    Revert "accelerators: Improve top row key aliasing"
    This reverts commit 7a7a51c2b5c7a18c9329e4e2a9f2ab719fe6fb61.
    
    Reason for revert:
    Depends on a CL that's suspected to be causing test failures:
    https://chromium-review.googlesource.com/c/chromium/src/+/4414341
    
    Original change's description:
    > accelerators: Improve top row key aliasing
    >
    > Bug: b/266085562
    > Change-Id: If233172183dfd10afd63b79058a0a2639bd30460
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411840
    > Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    > Commit-Queue: David Padlipsky <dpad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1128983}
    
    Bug: b/266085562
    Change-Id: Ifde2e58d663bd9a6248e4ea863c46a91880e73b7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4413740
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1129074}

--- ui/events/ash/keyboard_capability.h
+++ ui/events/ash/keyboard_capability.h
@@ -45,8 +45,7 @@ enum class TopRowActionKey {
   kNextTrack,
   kPreviousTrack,
   kPlayPause,
-  kLauncher,
-  kMaxValue = kLauncher,
+  kMaxValue = kPlayPause,
 };
 
 inline constexpr auto kLayout1TopRowActionKeys =
@@ -242,11 +241,6 @@ class KeyboardCapability : public InputDeviceEventObserver {
   static std::unique_ptr<EventDeviceInfo> CreateEventDeviceInfoFromInputDevice(
       const InputDevice& keyboard);
 
-  // Converts from the given `key_code` to the corresponding meaning in
-  // `TopRowActionKey` enum.
-  static absl::optional<TopRowActionKey> ConvertToTopRowActionKey(
-      ui::KeyboardCode key_code);
-
   void AddObserver(Observer* observer);
 
   void RemoveObserver(Observer* observer);
@@ -332,12 +326,6 @@ class KeyboardCapability : public InputDeviceEventObserver {
   bool HasTopRowActionKey(const InputDevice& keyboard,
                           TopRowActionKey action_key) const;
   bool HasTopRowActionKeyOnAnyKeyboard(TopRowActionKey action_key) const;
-
-  // Gets the corresponding function key for the given `action_key` on the given
-  // `keyboard`.
-  absl::optional<KeyboardCode> GetCorrespondingFunctionKey(
-      const InputDevice& keyboard,
-      TopRowActionKey action_key) const;
 
   const base::flat_map<int, KeyboardInfo>& keyboard_info_map() const {
     return keyboard_info_map_;
