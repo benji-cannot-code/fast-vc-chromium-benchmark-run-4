9c6ec921c360bed0005eb61de9a941440ac2f39e
François Degros <fdegros@chromium.org>
2023-04-12 05:29:00 +0000

    PinManager: Use Mojo enum output operators
    When logging Mojo enum values, use the output operators provided by the
    Mojo-generated code, instead of rewriting them.
    
    Bug: None
    Test: chromeos_unittests --gtest_filter=DriveFsPinManagerTest.*
    Change-Id: I3ae5a09b9d4cc45e857636ed8af5b4140c9de605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416782
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129092}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.cc
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iomanip>
 #include <locale>
+#include <sstream>
 #include <type_traits>
 
 #include "base/files/file_path.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
+#include "base/strings/string_piece.h"
 #include "base/task/sequenced_task_runner.h"
 #include "chromeos/ash/components/dbus/spaced/spaced_client.h"
 #include "chromeos/ash/components/drivefs/mojom/drivefs.mojom.h"
@@ -77,6 +79,22 @@ Quoter<T> Quote(const T& value) {
   return {value};
 }
 
+template <typename T>
+  requires std::is_enum_v<T>
+ostream& operator<<(ostream& out, Quoter<T> q) {
+  // Convert enum value to string.
+  const std::string s = (std::ostringstream() << q.value).str();
+
+  // Does the string start with 'k'?
+  if (!s.empty() && s.front() == 'k') {
+    // Skip the 'k' prefix.
+    return out << base::StringPiece(s).substr(1);
+  }
+
+  // No 'k' prefix. Print between parentheses.
+  return out << '(' << s << ')';
+}
+
 ostream& operator<<(ostream& out, Quoter<TimeDelta> q) {
   const int64_t ms = q.value.InMilliseconds();
   if (ms < 1000) {
@@ -114,72 +132,6 @@ ostream& operator<<(ostream& out, Quoter<absl::optional<T>> q) {
   return out << Quote(*q.value);
 }
 
-ostream& operator<<(ostream& out, Quoter<FileMetadata::Type> q) {
-  using Type = FileMetadata::Type;
-  switch (q.value) {
-#define PRINT(s)   \
-  case Type::k##s: \
-    return out << #s;
-    PRINT(File)
-    PRINT(Hosted)
-    PRINT(Directory)
-#undef PRINT
-  }
-
-  return out << "FileMetadata::Type("
-             << static_cast<std::underlying_type_t<Type>>(q.value) << ")";
-}
-
-ostream& operator<<(ostream& out, Quoter<mojom::ItemEvent::State> q) {
-  using State = mojom::ItemEvent::State;
-  switch (q.value) {
-#define PRINT(s)    \
-  case State::k##s: \
-    return out << #s;
-    PRINT(Queued)
-    PRINT(InProgress)
-    PRINT(Completed)
-    PRINT(Failed)
-#undef PRINT
-  }
-
-  return out << "ItemEvent::State("
-             << static_cast<std::underlying_type_t<State>>(q.value) << ")";
-}
-
-ostream& operator<<(ostream& out, Quoter<mojom::FileChange::Type> q) {
-  using Type = mojom::FileChange::Type;
-  switch (q.value) {
-#define PRINT(s)   \
-  case Type::k##s: \
-    return out << #s;
-    PRINT(Create)
-    PRINT(Delete)
-    PRINT(Modify)
-#undef PRINT
-  }
-
-  return out << "FileChange::Type("
-             << static_cast<std::underlying_type_t<Type>>(q.value) << ")";
-}
-
-ostream& operator<<(ostream& out, Quoter<LookupStatus> q) {
-  switch (q.value) {
-#define PRINT(s)           \
-  case LookupStatus::k##s: \
-    return out << #s;
-    PRINT(Ok)
-    PRINT(NotFound)
-    PRINT(PermissionDenied)
-    PRINT(Unknown)
-#undef PRINT
-  }
-
-  return out << "LookupStatus("
-             << static_cast<std::underlying_type_t<LookupStatus>>(q.value)
-             << ")";
-}
-
 ostream& operator<<(ostream& out, Quoter<ShortcutDetails> q) {
   return out << "{id: " << PinManager::Id(q.value.target_stable_id)
              << ", status: " << Quote(q.value.target_lookup_status) << "}";
@@ -214,49 +166,10 @@ ostream& operator<<(ostream& out, Quoter<mojom::FileChange> q) {
              << "}";
 }
 
-ostream& operator<<(ostream& out, Quoter<mojom::DriveError::Type> q) {
-  using Type = mojom::DriveError::Type;
-  switch (q.value) {
-#define PRINT(s)   \
-  case Type::k##s: \
-    return out << #s;
-    PRINT(CantUploadStorageFull)
-    PRINT(PinningFailedDiskFull)
-    PRINT(CantUploadStorageFullOrganization)
-    PRINT(CantUploadSharedDriveStorageFull)
-#undef PRINT
-  }
-
-  return out << "DriveError::Type("
-             << static_cast<std::underlying_type_t<Type>>(q.value) << ")";
-}
-
 ostream& operator<<(ostream& out, Quoter<mojom::DriveError> q) {
   const mojom::DriveError& e = q.value;
   return out << "{" << Quote(e.type) << " " << PinManager::Id(e.stable_id)
              << " " << Quote(e.path) << "}";
-}
-
-ostream& operator<<(ostream& out, Quoter<Stage> q) {
-  switch (q.value) {
-#define PRINT(s)    \
-  case Stage::k##s: \
-    return out << #s;
-    PRINT(Stopped)
-    PRINT(Paused)
-    PRINT(GettingFreeSpace)
-    PRINT(ListingFiles)
-    PRINT(Syncing)
-    PRINT(Success)
-    PRINT(CannotGetFreeSpace)
-    PRINT(CannotListFiles)
-    PRINT(NotEnoughSpace)
-    PRINT(CannotEnableDocsOffline)
-#undef PRINT
-  }
-
-  return out << "Stage(" << static_cast<std::underlying_type_t<Stage>>(q.value)
-             << ")";
 }
 
 // Rounds the given size to the next multiple of 4-KB.
