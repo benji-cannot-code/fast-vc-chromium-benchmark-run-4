f1b6ff9e3f24a75c25be24831c0f09b3d9211fb4
Xinyi Ji <xinyiji@google.com>
2023-04-12 23:10:14 +0000

    [Start] Polish the single tab card code when used on the tablet
    In this CL, we added the code for updating the most recent tab information of the single tab card. We also make sure the single tab card won't show if the current most recent tab is a new tab page.
    
    Bug: 1416536
    Change-Id: I5858ae69293268ddadd3ad34a79980e657d629d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4413114
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129578}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -872,8 +872,8 @@ public class NewTabPage implements NativePage, InvalidationAwareThumbnailProvide
     @Override
     public void destroy() {
         assert !mIsDestroyed;
-        assert !ViewCompat
-                .isAttachedToWindow(getView()) : "Destroy called before removed from window";
+        assert !ViewCompat.isAttachedToWindow(getView())
+            : "Destroy called before removed from window";
         if (mIsLoaded && !mTab.isHidden()) recordNTPHidden();
 
         mNewTabPageManager.onDestroy();
@@ -1057,6 +1057,11 @@ public class NewTabPage implements NativePage, InvalidationAwareThumbnailProvide
     public void showHomeSurfaceUi() {
         if (mSingleTabSwitcherCoordinator == null) {
             initializeSingleTabCard();
+        } else {
+            Tab mostRecentTab =
+                    TabModelUtils.getMostRecentTab(mTabModelSelector.getModel(false), mTab.getId());
+            mSingleTabSwitcherCoordinator.updateTrackingTab(mostRecentTab);
+            setSingleTabCardVisibility(true);
         }
     }
 
@@ -1084,7 +1089,7 @@ public class NewTabPage implements NativePage, InvalidationAwareThumbnailProvide
     private void initializeSingleTabCardImpl() {
         Tab mostRecentTab =
                 TabModelUtils.getMostRecentTab(mTabModelSelector.getModel(false), mTab.getId());
-        if (mostRecentTab == null) {
+        if (mostRecentTab == null || UrlUtilities.isNTPUrl(mostRecentTab.getUrl())) {
             return;
         }
         mSingleTabCardContainer = (FrameLayout) ((ViewStub) mNewTabPageLayout.findViewById(
