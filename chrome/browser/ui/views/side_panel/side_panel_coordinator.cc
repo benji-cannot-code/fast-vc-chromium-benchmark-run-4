57133f320aefff74a6589aab6186d27a952e0106
Caroline Rising <corising@chromium.org>
2023-04-12 15:09:36 +0000

    Add metrics for csc side panel entry shows, shown duration, pins, and unpins.
    Bug: b:277075386
    Change-Id: I63a88657612452daba94acaceb2d67fc156fa357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401195
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129272}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
+#include "base/metrics/user_metrics.h"
+#include "base/strings/strcat.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -346,6 +348,9 @@ void SidePanelCoordinator::UpdatePinState() {
         prefs::kSidePanelCompanionEntryPinnedToToolbar);
     pref_service->SetBoolean(prefs::kSidePanelCompanionEntryPinnedToToolbar,
                              !current_state);
+    base::RecordComputedAction(base::StrCat(
+        {"SidePanel.Companion.", !current_state ? "Pinned" : "Unpinned",
+         ".BySidePanelHeaderButton"}));
   }
 }
 
