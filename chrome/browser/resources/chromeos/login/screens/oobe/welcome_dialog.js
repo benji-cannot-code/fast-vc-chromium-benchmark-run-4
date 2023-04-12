bd76ccd3934cc81d684312a821d596604051572d
Simon Ziegltrum <ziegltrum@google.com>
2023-04-12 13:58:05 +0000

    [OOBE] Conditionally load lottie animation for welcome
    Bug: b:266939445 b:269231504
    Change-Id: I08876954e94db36dff2656c3aa5a3940862cf12c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415289
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129230}

--- chrome/browser/resources/chromeos/login/screens/oobe/welcome_dialog.js
+++ chrome/browser/resources/chromeos/login/screens/oobe/welcome_dialog.js
@@ -229,10 +229,9 @@ export class OobeWelcomeDialog extends OobeWelcomeDialogBase {
    * @suppress {missingProperties}
    */
   setVideoPlay_(play) {
-    if (this.isMeet_) {
-      return;
+    if (this.$$('#welcomeAnimation')) {
+      this.$$('#welcomeAnimation').playing = play;
     }
-    this.$.welcomeAnimation.playing = play;
   }
 
   /**
