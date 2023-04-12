b0d56b58f1fc9d1ae7f304387b3d2685dad8bfa7
David Padlipsky <dpad@google.com>
2023-04-12 23:38:16 +0000

    accelerators: Hide Globe Key accelerator if device has no globe key
    Bug: b/266085562
    Change-Id: I44addbf9c38cdf22f5d92f318811ad8f52d56e20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415493
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129593}

--- ash/accelerators/accelerator_alias_converter.cc
+++ ash/accelerators/accelerator_alias_converter.cc
@@ -252,6 +252,13 @@ AcceleratorAliasConverter::FilterAliasBySupportedKeys(
       continue;
     }
 
+    if (accelerator.key_code() == ui::VKEY_MODECHANGE) {
+      if (Shell::Get()->keyboard_capability()->HasGlobeKeyOnAnyKeyboard()) {
+        filtered_accelerators.push_back(accelerator);
+      }
+      continue;
+    }
+
     // Otherwise, always copy the accelerator.
     filtered_accelerators.push_back(accelerator);
   }
