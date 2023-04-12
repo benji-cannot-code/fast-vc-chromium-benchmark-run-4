11e6a9d1de19ee6fc3a74f74fe78dfa004a5aebd
Jood Hajeer <jood@google.com>
2023-04-12 14:07:17 +0000

    [iOS/B4P/PasswordGrouping] Fix the anchor of the actionSheets in PasswordDetails view.
    This CL contains one behaviour change behind the kPasswordsGrouping
    flag and another behind the kEnablePasswordsAccountStorage flag,
    both disabled by default.
    
    Using bounds which describes the view’s location and size in its own
    coordinate system seems to fix the misplaced actionSheets.
    Instead of the frame which describes the view’s location and size in its
    superview’s coordinate system.
    
    Also in similar uses in the Settings UI the bounds is used to set the
    sourceRect not the frame.
    
    Screenshots in https://crbug.com/1430742#c3.
    
    Fixed: 1430742
    Change-Id: If83fc4c4f308dc29076653619e70c4f76d8a2ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404802
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129238}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
@@ -280,7 +280,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                    browser:self.browser
                                      title:title
                                    message:message
-                                      rect:anchorView.frame
+                                      rect:anchorView.bounds
                                       view:anchorView]
           : [[ActionSheetCoordinator alloc]
                 initWithBaseViewController:self.viewController
@@ -319,7 +319,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                          browser:self.browser
                            title:actionSheetTitle
                          message:actionSheetMessage
-                            rect:anchorView.frame
+                            rect:anchorView.bounds
                             view:anchorView];
 
   __weak __typeof(self) weakSelf = self;
