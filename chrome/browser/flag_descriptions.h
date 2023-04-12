0bb14d0ef4f080f6300a4c542974d726dbb6c7ba
Donn Denman <donnd@chromium.org>
2023-04-12 17:28:08 +0000

    [E2E] Initial implementation of basic E2E Nav Bar
    Adds a new Flag and Module for the Edge To Edge Feature.
    
    Flag: draw-edge-to-edge that allows drawing below the Android
    Navigation Bar.
    Module: chrome/browser/ui/android/edge_to_edge
    We followed this guide when building the module:
    go/how-to-modularize-clank.
    
    This basic implementation just applies the behavior to any page
    when enabled, as a demo-level implementation. This is not
    a launchable implementation but fine for a Dev demo.
    
    BUG=1430288
    
    Change-Id: I15f368c658cb99300b144dd322c6100a872a8814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367532
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129336}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2157,6 +2157,9 @@ extern const char kCormorantDescription[];
 extern const char kDefaultViewportIsDeviceWidthName[];
 extern const char kDefaultViewportIsDeviceWidthDescription[];
 
+extern const char kDrawEdgeToEdgeName[];
+extern const char kDrawEdgeToEdgeDescription[];
+
 extern const char kAutofillManualFallbackAndroidName[];
 extern const char kAutofillManualFallbackAndroidDescription[];
 
---
+++ chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeControllerFactory.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.edge_to_edge;
+
+import android.os.Build.VERSION_CODES;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+/**
+ * Creates an {@link EdgeToEdgeController} used to control drawing using the Android Edge to Edge
+ * Feature. This allows drawing under Android System Bars.
+ */
+public class EdgeToEdgeControllerFactory {
+    /** Creates an {@link EdgeToEdgeController} instance using the given activity. */
+    public static EdgeToEdgeController create(AppCompatActivity activity) {
+        return new EdgeToEdgeControllerImpl(activity);
+    }
+
+    /** @Return whether the feature is enabled or not. */
+    public static boolean isEnabled() {
+        // Make sure we test SDK version before checking the Feature so Field Trials only collect
+        // from qualifying devices.
+        if (android.os.Build.VERSION.SDK_INT < VERSION_CODES.R) return false;
+        return ChromeFeatureList.isEnabled(ChromeFeatureList.DRAW_EDGE_TO_EDGE);
+    }
+}
