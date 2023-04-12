4a7d5347f1387871b67c6943639cc7a328b2ca54
Tomasz Wiszkowski <ender@google.com>
2023-04-12 00:28:36 +0000

    Ignore intermediate Autocomplete Results.
    This change introduces a flag that allows Android to suppress
    intermediate Autocomplete results until the final update is ready.
    
    The change is expected to help us reduce the resource utilization
    during the Active Omnibox input session, volume of JNI jumps, view
    modification, drawable and string instantiation - and overall time
    needed to build the suggestion list, especially on low end devices.
    
    Bug: 1430911
    Change-Id: I5a9b41a8d6cc483d075fdf52d6f959e5a140a3b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416086
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129014}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1194,6 +1194,9 @@ extern const char kOmniboxCR23SteadyStateIconsDescription[];
 extern const char kOmniboxDisableCGIParamMatchingName[];
 extern const char kOmniboxDisableCGIParamMatchingDescription[];
 
+extern const char kOmniboxIgnoreIntermediateResultsName[];
+extern const char kOmniboxIgnoreIntermediateResultsDescription[];
+
 extern const char kOmniboxDiscardTemporaryInputOnTabSwitchName[];
 extern const char kOmniboxDiscardTemporaryInputOnTabSwitchDescription[];
 
