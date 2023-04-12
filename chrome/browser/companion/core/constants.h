5e9db3ad1e67e2194f029f9364a9bd3ccc3ae337
Shakti Sahu <shaktisahu@chromium.org>
2023-04-12 21:18:27 +0000

    [CSC] Some variable renames
    Bug: b/277850422
    Change-Id: Id574c340b004da3ad1651811f7eafe990eb74e54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416692
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129492}

--- chrome/browser/companion/core/constants.h
+++ chrome/browser/companion/core/constants.h
@@ -13,8 +13,8 @@ const char kMsbbPromoDeclinedCountPref[] =
     "Companion.Promo.MSBB.Declined.Count";
 const char kSigninPromoDeclinedCountPref[] =
     "Companion.Promo.Signin.Declined.Count";
-const char kLabsPromoDeclinedCountPref[] =
-    "Companion.Promo.Labs.Declined.Count";
+const char kExpsPromoDeclinedCountPref[] =
+    "Companion.Promo.Exps.Declined.Count";
 
 }  // namespace companion
 
