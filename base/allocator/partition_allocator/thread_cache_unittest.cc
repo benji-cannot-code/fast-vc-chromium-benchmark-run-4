47f9191a6baa36ade3c1ce9f0ba73f3ce31d9d0d
Thiabaud Engelbrecht <thiabaud@google.com>
2023-04-12 21:57:03 +0000

    [partition_alloc] Remove 'coarse' bucket distribution
    This CL removes the 'coarse' bucket distribution, since
    finch experiments have determined it regresses memory.
    
    Bug: 1238858
    Change-Id: Id5d62531cc933f48137c6f46817fd7d04ce9ce5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373791
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129523}

--- base/allocator/partition_allocator/thread_cache_unittest.cc
+++ base/allocator/partition_allocator/thread_cache_unittest.cc
@@ -103,9 +103,6 @@ class PartitionAllocThreadCacheTest
     ThreadSafePartitionRoot* root = allocator_->root();
     switch (GetParam()) {
       case BucketDistribution::kDefault:
-        root->SwitchToDefaultBucketDistribution();
-        break;
-      case BucketDistribution::kCoarser:
         root->ResetBucketDistributionForTesting();
         break;
       case BucketDistribution::kDenser:
@@ -147,7 +144,7 @@ class PartitionAllocThreadCacheTest
   size_t GetBucketSizeForThreadCache() {
     size_t tc_bucket_index = root()->SizeToBucketIndex(
         sizeof(ThreadCache),
-        ThreadSafePartitionRoot::BucketDistribution::kCoarser);
+        ThreadSafePartitionRoot::BucketDistribution::kDefault);
     auto* tc_bucket = &root()->buckets[tc_bucket_index];
     return tc_bucket->slot_size;
   }
@@ -194,7 +191,6 @@ class PartitionAllocThreadCacheTest
 INSTANTIATE_TEST_SUITE_P(AlternateBucketDistribution,
                          PartitionAllocThreadCacheTest,
                          ::testing::Values(BucketDistribution::kDefault,
-                                           BucketDistribution::kCoarser,
                                            BucketDistribution::kDenser));
 
 TEST_P(PartitionAllocThreadCacheTest, Simple) {
@@ -1154,7 +1150,7 @@ TEST_P(PartitionAllocThreadCacheTest, MAYBE_Bookkeeping) {
   // always based on the sparser distribution.
   size_t tc_bucket_index = root()->SizeToBucketIndex(
       sizeof(ThreadCache),
-      ThreadSafePartitionRoot::BucketDistribution::kCoarser);
+      ThreadSafePartitionRoot::BucketDistribution::kDefault);
   auto* tc_bucket = &root()->buckets[tc_bucket_index];
   size_t expected_allocated_size =
       tc_bucket->slot_size;  // For the ThreadCache itself.
@@ -1240,7 +1236,7 @@ TEST(AlternateBucketDistributionTest, SizeToIndex) {
     for (size_t offset = 0; offset < 4; offset++) {
       size_t n = i * (4 + offset) / 4;
       EXPECT_EQ(BucketIndexLookup::GetIndex(n),
-                BucketIndexLookup::GetIndexForDenserBuckets(n));
+                BucketIndexLookup::GetIndexForDefaultBuckets(n));
     }
   }
 
@@ -1260,7 +1256,7 @@ TEST(AlternateBucketDistributionTest, SizeToIndex) {
     for (size_t offset = 0; offset < 2; offset++) {
       size_t n = i * (4 + offset) / 4;
       EXPECT_EQ(BucketIndexLookup::GetIndex(n),
-                BucketIndexLookup::GetIndexForDenserBuckets(n));
+                BucketIndexLookup::GetIndexForDefaultBuckets(n));
       EXPECT_EQ(BucketIndexLookup::GetIndex(n), expected_index);
       expected_index += 2;
     }
@@ -1273,7 +1269,7 @@ TEST(AlternateBucketDistributionTest, SizeToIndex) {
       // the bucket index to be larger than the bucket index for the same
       // allocation under the default distribution.
       EXPECT_GT(BucketIndexLookup::GetIndex(n),
-                BucketIndexLookup::GetIndexForDenserBuckets(n));
+                BucketIndexLookup::GetIndexForDefaultBuckets(n));
       // We expect both allocations in this loop to be rounded up to the next
       // power of two bucket.
       EXPECT_EQ(BucketIndexLookup::GetIndex(n), expected_index);
@@ -1287,7 +1283,7 @@ TEST(AlternateBucketDistributionTest, SizeToIndex) {
     for (size_t offset = 0; offset < 4; offset++) {
       size_t n = i * (4 + offset) / 4;
       EXPECT_EQ(BucketIndexLookup::GetIndex(n),
-                BucketIndexLookup::GetIndexForDenserBuckets(n));
+                BucketIndexLookup::GetIndexForDefaultBuckets(n));
     }
   }
 }
@@ -1445,7 +1441,7 @@ TEST(AlternateBucketDistributionTest, SwitchBeforeAlloc) {
   root->SwitchToDenserBucketDistribution();
   constexpr size_t n = (1 << 12) * 3 / 2;
   EXPECT_NE(internal::BucketIndexLookup::GetIndex(n),
-            internal::BucketIndexLookup::GetIndexForDenserBuckets(n));
+            internal::BucketIndexLookup::GetIndexForDefaultBuckets(n));
 
   void* ptr = root->Alloc(n, "");
 
@@ -1461,7 +1457,7 @@ TEST(AlternateBucketDistributionTest, SwitchAfterAlloc) {
   std::unique_ptr<PartitionAllocatorForTesting> allocator(CreateAllocator());
   constexpr size_t n = (1 << 12) * 3 / 2;
   EXPECT_NE(internal::BucketIndexLookup::GetIndex(n),
-            internal::BucketIndexLookup::GetIndexForDenserBuckets(n));
+            internal::BucketIndexLookup::GetIndexForDefaultBuckets(n));
 
   ThreadSafePartitionRoot* root = allocator->root();
   void* ptr = root->Alloc(n, "");
