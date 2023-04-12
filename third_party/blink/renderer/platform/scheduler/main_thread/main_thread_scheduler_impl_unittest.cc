819b7f254defecafd7baf52aff32c2655ad80ccd
Scott Haseley <shaseley@chromium.org>
2023-04-12 16:18:44 +0000

    [scheduler] Remove PrioritizeCompositingAfterInput flag
    Removing this flag since this feature shipped in M109.
    
    Follow-up work:
     - Per-frame prioritization types can be represented as an enum since
       they are all mutually exclusive now.
     - The compositor prioritization logic should probably be in its own
       class.
    
    Fixes: 853771
    Change-Id: Ifefc6826b5cdbbb03212709ff1358b21a06ff4d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416406
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129304}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl_unittest.cc
@@ -3334,15 +3334,7 @@ TEST_F(MainThreadSchedulerImplWithInitalVirtualTimeTest, VirtualTimeOverride) {
   EXPECT_EQ(base::Time::Now(), base::Time::FromJsTime(1000000.0));
 }
 
-class MainThreadSchedulerImplWithCompositingAfterInputPrioritizationTest
-    : public MainThreadSchedulerImplTest {
- public:
-  MainThreadSchedulerImplWithCompositingAfterInputPrioritizationTest()
-      : MainThreadSchedulerImplTest({kPrioritizeCompositingAfterInput}, {}) {}
-};
-
-TEST_F(MainThreadSchedulerImplWithCompositingAfterInputPrioritizationTest,
-       CompositingAfterInput) {
+TEST_F(MainThreadSchedulerImplTest, CompositingAfterInput) {
   Vector<String> run_order;
 
   // Input tasks don't cause compositor tasks to be prioritized unless an input
@@ -3368,7 +3360,7 @@ TEST_F(MainThreadSchedulerImplWithCompositingAfterInputPrioritizationTest,
   run_order.clear();
 }
 
-TEST_F(MainThreadSchedulerImplWithCompositingAfterInputPrioritizationTest,
+TEST_F(MainThreadSchedulerImplTest,
        CompositorNotPrioritizedAfterContinuousInput) {
   Vector<String> run_order;
 
