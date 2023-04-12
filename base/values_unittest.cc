b30f364fa316f55e62c9c51319c4f5dd79d40377
Georg Neis <neis@chromium.org>
2023-04-12 06:12:26 +0000

    base: Remove comment reference to deleted function
    The function Value::FindDoubleKey was deleted in
    https://chromium-review.googlesource.com/c/chromium/src/+/4347916.
    
    Bug: 1187001
    Change-Id: Id04d121d4f1cfc15314509ece89dd7ca9f08eba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411582
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129101}

--- base/values_unittest.cc
+++ base/values_unittest.cc
@@ -1028,8 +1028,6 @@ TEST(ValuesTest, SetDoubleKey) {
   dict.Set("minus_one_key", -1.0);
   dict.Set("pi_key", 3.1415);
 
-  // NOTE: Use Find() instead of FindDoubleKey() because the latter will
-  // auto-convert integers to doubles as well.
   const Value* value;
 
   value = dict.Find("one_key");
