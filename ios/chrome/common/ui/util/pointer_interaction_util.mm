2a8ff0fee6a7e11a02b1c8c3369be419ee2ffde1
Mark Cogan <marq@google.com>
2023-04-12 08:10:08 +0000

    [iOS] Downgrade assertions in pointer interaction utils.
    Bug: 1428001
    Change-Id: I2814ff226056d71b494b5e68d1c528d641dafc4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414880
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129130}

--- ios/chrome/common/ui/util/pointer_interaction_util.mm
+++ ios/chrome/common/ui/util/pointer_interaction_util.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <ostream>
 
-#import "base/check_op.h"
+#import "base/logging.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -40,11 +40,13 @@ UIPointerStyle* CreateHighlightEffectRectShapePointerStyle(UIButton* button) {
 UIButtonPointerStyleProvider CreateDefaultEffectCirclePointerStyleProvider() {
   return ^UIPointerStyle*(UIButton* button, UIPointerEffect* proposedEffect,
                           UIPointerShape* proposedShape) {
-    DCHECK_EQ(button.frame.size.width, button.frame.size.height)
-        << "Pointer shape cannot be a circle since button is not square";
-    UIPointerShape* shape =
-        [UIPointerShape shapeWithRoundedRect:button.frame
-                                cornerRadius:button.frame.size.width / 2];
+    CGRect frame = button.frame;
+    if (abs(frame.size.width - frame.size.height) > 0.5) {
+      LOG(ERROR) << "Button frame for circular pointer isn't square: ("
+                 << frame.size.width << " x " << frame.size.height << ")";
+    }
+    UIBezierPath* path = [UIBezierPath bezierPathWithOvalInRect:frame];
+    UIPointerShape* shape = [UIPointerShape shapeWithPath:path];
     return [UIPointerStyle styleWithEffect:proposedEffect shape:shape];
   };
 }
@@ -52,15 +54,17 @@ UIButtonPointerStyleProvider CreateDefaultEffectCirclePointerStyleProvider() {
 UIButtonPointerStyleProvider CreateLiftEffectCirclePointerStyleProvider() {
   return ^UIPointerStyle*(UIButton* button, UIPointerEffect* proposedEffect,
                           UIPointerShape* proposedShape) {
-    DCHECK_EQ(button.frame.size.width, button.frame.size.height)
-        << "Pointer shape cannot be a circle since button is not square";
+    CGRect frame = button.frame;
+    if (abs(frame.size.width - frame.size.height) > 0.5) {
+      LOG(ERROR) << "Button frame for circular pointer isn't square: ("
+                 << frame.size.width << " x " << frame.size.height << ")";
+    }
     UITargetedPreview* preview =
         [[UITargetedPreview alloc] initWithView:button];
     UIPointerLiftEffect* effect =
         [UIPointerLiftEffect effectWithPreview:preview];
-    UIPointerShape* shape =
-        [UIPointerShape shapeWithRoundedRect:button.frame
-                                cornerRadius:button.frame.size.width / 2];
+    UIBezierPath* path = [UIBezierPath bezierPathWithOvalInRect:frame];
+    UIPointerShape* shape = [UIPointerShape shapeWithPath:path];
     return [UIPointerStyle styleWithEffect:effect shape:shape];
   };
 }
@@ -68,9 +72,10 @@ UIButtonPointerStyleProvider CreateLiftEffectCirclePointerStyleProvider() {
 UIButtonPointerStyleProvider CreateOpaqueButtonPointerStyleProvider() {
   return ^UIPointerStyle*(UIButton* button, UIPointerEffect* proposedEffect,
                           UIPointerShape* proposedShape) {
-    DCHECK(button.backgroundColor &&
-           button.backgroundColor != [UIColor clearColor])
-        << "Expected an opaque background for button.";
+    if (!button.backgroundColor ||
+        button.backgroundColor == [UIColor clearColor]) {
+      LOG(ERROR) << "Expected an opaque background for button.";
+    }
     return CreateHoverEffectNoShapePointerStyle(button);
   };
 }
@@ -78,9 +83,10 @@ UIButtonPointerStyleProvider CreateOpaqueButtonPointerStyleProvider() {
 UIButtonPointerStyleProvider CreateTransparentButtonPointerStyleProvider() {
   return ^UIPointerStyle*(UIButton* button, UIPointerEffect* proposedEffect,
                           UIPointerShape* proposedShape) {
-    DCHECK(!button.backgroundColor ||
-           button.backgroundColor == [UIColor clearColor])
-        << "Expected a transparent background for button.";
+    if (button.backgroundColor &&
+        button.backgroundColor != [UIColor clearColor]) {
+      LOG(ERROR) << "Expected a transparent background for button.";
+    }
     return CreateHighlightEffectRectShapePointerStyle(button);
   };
 }
