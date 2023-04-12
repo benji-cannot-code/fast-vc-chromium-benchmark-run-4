806fbf1cc629f84153c3abb4fface50f37e2f377
Jimmy <jimmyxgong@chromium.org>
2023-04-12 22:14:34 +0000

    shortcuts: Fix bug where text accelerators would repopulate incorrectly
    Fixes a bug in which a call to `onAcceleratorsUpdated` would update the
    non-configurable accelerators without clearing the existing mapping.
    This results in multiple non-configurable text entries for one action.
    An existing `assert()` checks that only one entry exists, which resulted
    in no text entries to appear.
    
    Adds a test to enforce the correctness of `onAcceleratorsUpdated`.
    
    Bug: b:216049298
    Test: browser_tests
    Change-Id: I7f4f1a96bbdf378200c9d2674ec4097844d42260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408360
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129540}

--- ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
+++ ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
@@ -9,7 +9,7 @@ import {assert} from 'chrome://resources/js/assert_ts.js';
 
 import {AcceleratorResultData, AcceleratorsUpdatedObserverRemote} from '../mojom-webui/ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom-webui.js';
 
-import {Accelerator, AcceleratorConfigResult, AcceleratorSource, MojoAcceleratorConfig, MojoLayoutInfo, ShortcutProviderInterface} from './shortcut_types.js';
+import {Accelerator, AcceleratorConfig, AcceleratorConfigResult, AcceleratorSource, MojoAcceleratorConfig, MojoLayoutInfo, ShortcutProviderInterface} from './shortcut_types.js';
 
 
 /**
@@ -60,6 +60,10 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
     this.registerObservables();
   }
 
+  triggerOnAcceleratorUpdated(): void {
+    this.observables.trigger(ON_ACCELERATORS_UPDATED_METHOD_NAME);
+  }
+
   getAcceleratorLayoutInfos(): Promise<{layoutInfos: MojoLayoutInfo[]}> {
     return this.methods.resolveMethod('getAcceleratorLayoutInfos');
   }
@@ -80,8 +84,7 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
 
   addObserver(observer: AcceleratorsUpdatedObserverRemote): void {
     this.acceleratorsUpdatedPromise = this.observe(
-        ON_ACCELERATORS_UPDATED_METHOD_NAME,
-        (config: MojoAcceleratorConfig) => {
+        ON_ACCELERATORS_UPDATED_METHOD_NAME, (config: AcceleratorConfig) => {
           observer.onAcceleratorsUpdated(config);
         });
   }
@@ -93,7 +96,7 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
 
   // Set the value that will be retuned when `onAcceleratorsUpdated()` is
   // called.
-  setFakeAcceleratorsUpdated(config: MojoAcceleratorConfig[]): void {
+  setFakeAcceleratorsUpdated(config: AcceleratorConfig[]): void {
     this.observables.setObservableData(
         ON_ACCELERATORS_UPDATED_METHOD_NAME, config);
   }
