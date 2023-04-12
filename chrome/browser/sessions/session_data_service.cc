1c1c3f413e4e43c0079f3153ee1ea2413f21215d
Sergii Bykov <sbykov@google.com>
2023-04-12 09:28:00 +0000

    [Code Health] Remove expired Session.SessionData histograms
    Removed Session.SessionData histograms that expired as of M95:
    CleanupTime
    StartupCleanupTime
    StatusFromLastSession
    
    Bug: 1384774
    Change-Id: I7be80d4d19df419037c72fd70779aa2d6c5d36b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414903
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129148}

--- chrome/browser/sessions/session_data_service.cc
+++ chrome/browser/sessions/session_data_service.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
-#include "base/metrics/histogram_functions.h"
-#include "base/time/time.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/lifetime/browser_shutdown.h"
 #include "chrome/browser/profiles/profile.h"
@@ -50,10 +48,9 @@ SessionDataService::SessionDataService(
 
   SetStatusPref(Status::kInitialized);
   auto* policy = profile_->GetSpecialStoragePolicy();
-  if (policy && policy->HasSessionOnlyOrigins()) {
-    RecordHistogramForLastSession(last_status);
-    if (base::FeatureList::IsEnabled(kDeleteSessionOnlyDataOnStartup))
-      MaybeContinueDeletionFromLastSesssion(last_status);
+  if (policy && policy->HasSessionOnlyOrigins() &&
+      base::FeatureList::IsEnabled(kDeleteSessionOnlyDataOnStartup)) {
+    MaybeContinueDeletionFromLastSesssion(last_status);
   }
 
   for (Browser* browser : *BrowserList::GetInstance())
@@ -64,13 +61,6 @@ SessionDataService::SessionDataService(
 
 SessionDataService::~SessionDataService() {
   BrowserList::RemoveObserver(this);
-}
-
-void SessionDataService::RecordHistogramForLastSession(Status last_status) {
-  if (last_status == Status::kUninitialized)
-    return;
-  base::UmaHistogramEnumeration("Session.SessionData.StatusFromLastSession",
-                                last_status);
 }
 
 void SessionDataService::MaybeContinueDeletionFromLastSesssion(
@@ -105,14 +95,10 @@ void SessionDataService::MaybeContinueDeletionFromLastSesssion(
   deleter_->DeleteSessionOnlyData(
       /*skip_session_cookies=*/true,
       base::BindOnce(&SessionDataService::OnCleanupAtStartupFinished,
-                     base::Unretained(this), base::TimeTicks::Now()));
+                     base::Unretained(this)));
 }
 
-void SessionDataService::OnCleanupAtStartupFinished(
-    base::TimeTicks time_started) {
-  base::UmaHistogramMediumTimes("Session.SessionData.StartupCleanupTime",
-                                base::TimeTicks::Now() - time_started);
-}
+void SessionDataService::OnCleanupAtStartupFinished() {}
 
 void SessionDataService::SetStatusPref(Status status) {
   profile_->GetPrefs()->SetInteger(kSessionDataStatusPref,
@@ -171,17 +157,11 @@ void SessionDataService::StartCleanupInternal(bool skip_session_cookies) {
   deleter_->DeleteSessionOnlyData(
       skip_session_cookies,
       base::BindOnce(&SessionDataService::OnCleanupAtSessionEndFinished,
-                     base::Unretained(this), base::TimeTicks::Now()));
+                     base::Unretained(this)));
 }
 
-void SessionDataService::OnCleanupAtSessionEndFinished(
-    base::TimeTicks time_started) {
+void SessionDataService::OnCleanupAtSessionEndFinished() {
   SetStatusPref(Status::kDeletionFinished);
-  auto* policy = profile_->GetSpecialStoragePolicy();
-  if (policy && policy->HasSessionOnlyOrigins()) {
-    base::UmaHistogramMediumTimes("Session.SessionData.CleanupTime",
-                                  base::TimeTicks::Now() - time_started);
-  }
 }
 
 void SessionDataService::SetForceKeepSessionState() {
--- chrome/browser/sessions/session_data_service.h
+++ chrome/browser/sessions/session_data_service.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
-#include "base/time/time.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 
@@ -62,15 +61,13 @@ class SessionDataService : public BrowserListObserver, public KeyedService {
   // content setting are removed.
   void StartCleanupInternal(bool skip_session_cookies);
 
-  // Records the Status of the last session.
-  void RecordHistogramForLastSession(Status last_status);
   // Starts another data deletion if the deletion at the end of the last session
   // did not finish.
   void MaybeContinueDeletionFromLastSesssion(Status last_status);
 
   void SetStatusPref(Status status);
-  void OnCleanupAtStartupFinished(base::TimeTicks time_started);
-  void OnCleanupAtSessionEndFinished(base::TimeTicks time_started);
+  void OnCleanupAtStartupFinished();
+  void OnCleanupAtSessionEndFinished();
 
   raw_ptr<Profile> profile_;
   std::unique_ptr<SessionDataDeleter> deleter_;
