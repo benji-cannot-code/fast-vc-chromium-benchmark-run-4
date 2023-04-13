138ac7a04ef3c101552d6834b97ef5587c439c5e
Dave Tapuska <dtapuska@chromium.org>
2023-04-13 16:10:29 +0000

    Fix flaky PrefetchURLLoaderInterceptorTest
    Seems we need to parse the config after setting the scoped feature list.
    
    Bug: 1431812
    Change-Id: I691cf846d0f83e693455e659638f66f3c17d9738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4423749
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129889}

--- content/browser/preloading/prefetch/prefetch_url_loader_interceptor_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_url_loader_interceptor_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/prefetch/prefetch_type.h"
 #include "content/browser/preloading/prefetch/prefetch_url_loader_helper.h"
 #include "content/browser/preloading/preloading.h"
+#include "content/browser/preloading/preloading_config.h"
 #include "content/browser/preloading/preloading_data_impl.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/renderer_host/navigation_request.h"
@@ -255,10 +256,13 @@ class PrefetchURLLoaderInterceptorTest : public RenderViewHostTestHarness {
         std::make_unique<base::ScopedMockElapsedTimersForTest>();
 
     scoped_feature_list_.InitAndDisableFeature(::features::kPreloadingConfig);
+    PreloadingConfig::GetInstance().ParseConfig();
   }
 
   void TearDown() override {
     interceptor_.release();
+    scoped_feature_list_.Reset();
+    PreloadingConfig::GetInstance().ParseConfig();
 
     RenderViewHostTestHarness::TearDown();
   }
