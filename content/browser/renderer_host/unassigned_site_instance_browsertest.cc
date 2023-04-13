ec14e54a373ab8db798a05348fc69befae7ad692
Sharon Yang <yangsharon@chromium.org>
2023-04-13 17:23:47 +0000

    Refactor NavigationController tests
    navigation_controller_impl_browsertest is very large. Move the
    history intervention tests out to a separate file.
    
    Change-Id: I5d2d1c8f8d30d833c5c96db92662cd63b2aa2eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902969
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1129936}

--- content/browser/renderer_host/unassigned_site_instance_browsertest.cc
+++ content/browser/renderer_host/unassigned_site_instance_browsertest.cc
@@ -61,22 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
-
 const std::string kEmptySchemeForTesting = "empty-scheme";
-
-void InitBackForwardCacheFeature(base::test::ScopedFeatureList* feature_list,
-                                 bool enable_back_forward_cache) {
-  if (enable_back_forward_cache) {
-    feature_list->InitWithFeaturesAndParameters(
-        GetBasicBackForwardCacheFeatureForTesting(
-            {{kBackForwardCacheNoTimeEviction, {}},
-             {features::kBackForwardCacheMemoryControls, {}}}),
-        {});
-  } else {
-    feature_list->InitAndDisableFeature(features::kBackForwardCache);
-  }
-}
-
 }  // namespace
 
 // Note that this test suite is parametrized for RenderDocument and
