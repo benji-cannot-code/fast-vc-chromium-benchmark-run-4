693d3068106d2b8976fe8f9f8df837d9a546a3d5
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-04-13 03:37:36 +0000

    [RemoveLegacy] Remove dead methods in text_run_constructor.h
    There is a unsafe type thing happening, but we can likely remove
    this whole file.
    
    This patch has no behaviour change.
    
    Bug: 1229581
    Change-Id: I081524f837325bccfca62a5d4ca7b5d7760ce7af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420107
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129670}

--- third_party/blink/renderer/core/layout/text_run_constructor.h
+++ third_party/blink/renderer/core/layout/text_run_constructor.h
@@ -44,9 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class Font;
 class ComputedStyle;
-class LayoutText;
 
 enum TextRunFlag {
   kDefaultTextRunFlags = 0,
@@ -56,34 +54,12 @@ enum TextRunFlag {
 
 typedef unsigned TextRunFlags;
 
-// Direction resolved from string value.
-TextRun ConstructTextRun(const Font&,
-                         const String&,
+// TODO(1229581): All the calls to this function are bad. They are passing in
+// TextRun::ExpansionBehaviour to flags instead of TextRunFlag above.
+// Remove this function.
+TextRun ConstructTextRun(const String&,
                          const ComputedStyle&,
                          TextRunFlags = kDefaultTextRunFlags);
-
-// Explicit direction.
-TextRun ConstructTextRun(const Font&,
-                         const String&,
-                         const ComputedStyle&,
-                         TextDirection,
-                         TextRunFlags = kDefaultTextRunFlags);
-TextRun ConstructTextRun(const Font&,
-                         const LayoutText*,
-                         unsigned offset,
-                         unsigned length,
-                         const ComputedStyle&,
-                         TextDirection);
-TextRun ConstructTextRun(const Font&,
-                         const LChar*,
-                         int length,
-                         const ComputedStyle&,
-                         TextDirection);
-TextRun ConstructTextRun(const Font&,
-                         const UChar*,
-                         int length,
-                         const ComputedStyle&,
-                         TextDirection);
 
 }  // namespace blink
 
