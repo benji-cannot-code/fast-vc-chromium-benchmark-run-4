2e50337a16c5917004928b4a5feec72b1c44915d
Pavol Marko <pmarko@google.com>
2023-04-13 18:54:23 +0000

    Lacros certs: Remove declared dependency on IdentityManager
    The lacros CertDbInitializer doesn't have a dependency on
    IdentityManager since CL:3060375, so also stop stating that.
    
    Bug: 1148300
    Change-Id: I567c3e652ed4cb04c8353c065d60b751483fa4c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420786
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1130003}

--- chrome/browser/lacros/cert/cert_db_initializer_factory.cc
+++ chrome/browser/lacros/cert/cert_db_initializer_factory.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "chrome/browser/lacros/cert/cert_db_initializer_impl.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chromeos/lacros/lacros_service.h"
 
 class CertDbInitializer;
@@ -30,9 +29,7 @@ CertDbInitializer* CertDbInitializerFactory::GetForBrowserContext(
 CertDbInitializerFactory::CertDbInitializerFactory()
     : ProfileKeyedServiceFactory(
           "CertDbInitializerFactory",
-          ProfileSelections::BuildRedirectedInIncognito()) {
-  DependsOn(IdentityManagerFactory::GetInstance());
-}
+          ProfileSelections::BuildRedirectedInIncognito()) {}
 
 bool CertDbInitializerFactory::ServiceIsCreatedWithBrowserContext() const {
   return should_create_with_browser_context_;
