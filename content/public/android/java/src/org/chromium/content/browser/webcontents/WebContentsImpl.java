99f74457a9ed67af5a4112bdcb4e4a8d77776d6e
Wayne Jackson Jr <wbjacksonjr@google.com>
2023-04-13 17:02:06 +0000

    [webview][clank] Trackpad Click Gesture Handling
    Previously we were handling trackpad click events as touch events
    which resulted in trackpad not supporting the following gestures.
    
    1. Double-click (Select Word)
    2. Triple-click (Select paragraph)
    3. Two-finger click (Show Context Menu)
    4. Click + Drag to select text
    
    This also resulted in the following UI elements being unexpectedly
    shown during trackpad text selection.
    
    1. Magnifier View
    2. Selection Handles
    
    This CL now properly ports trackpad click gestures &
    click + drag events as mouse events which fixes all
    of the issues above, and doesn't break any touch gestures (i.e pinch to zoom, two-finger scroll).
    
    Bug: 1428015, 1428017, 1428020
    
    Low-Coverage-Reason: Existing class that were made public with no logic changes
    
    Change-Id: I107e8bae8fd8067984ff4183f44f62b4c5a68705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355011
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129918}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -224,8 +224,8 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
     }
 
     @CalledByNative
-    @VisibleForTesting
-    static WebContentsImpl create(
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public static WebContentsImpl create(
             long nativeWebContentsAndroid, NavigationController navigationController) {
         return new WebContentsImpl(nativeWebContentsAndroid, navigationController);
     }
@@ -1011,6 +1011,35 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
         return key.cast(data);
     }
 
+    /**
+     * Convenience method to initialize test state. Only use for testing.
+     */
+    @VisibleForTesting
+    public void initializeForTesting() {
+        if (mInternalsHolder == null) {
+            mInternalsHolder = WebContents.createDefaultInternalsHolder();
+        }
+        WebContentsInternalsImpl internals = (WebContentsInternalsImpl) mInternalsHolder.get();
+        if (internals == null) {
+            internals = new WebContentsInternalsImpl();
+            internals.userDataHost = new UserDataHost();
+        }
+        mInternalsHolder.set(internals);
+        mInitialized = true;
+    }
+
+    /**
+     * Convenience method to set user data. Only use for testing.
+     */
+    @VisibleForTesting
+    public <T extends UserData> void setUserDataForTesting(Class<T> key, T userData) {
+        // Be sure to call initializeForTesting() first.
+        assert mInitialized;
+
+        WebContentsInternalsImpl internals = (WebContentsInternalsImpl) mInternalsHolder.get();
+        internals.userDataHost.setUserData(key, userData);
+    }
+
     public <T extends UserData> void removeUserData(Class<T> key) {
         UserDataHost userDataHost = getUserDataHost();
         if (userDataHost == null) return;
@@ -1126,8 +1155,9 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
                 "Native WebContents already destroyed", mNativeDestroyThrowable);
     }
 
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
     @NativeMethods
-    interface Natives {
+    public interface Natives {
         // This is static to avoid exposing a public destroy method on the native side of this
         // class.
         void destroyWebContents(long webContentsAndroidPtr);
