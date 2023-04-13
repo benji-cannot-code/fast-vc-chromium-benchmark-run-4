c5c5fbe0a3aa5f42530dbeb1eb9212beec317e16
Juliet Levesque <julietlevesque@google.com>
2023-04-13 15:59:17 +0000

    [Nearby Presence] Add LocalDeviceDataProvider skeleton class
    Adds the LocalDeviceDataProvider class as part of
    go/cros-nearby-presence-credential-web-integration. This skeleton
    class imports the protos from third_party/nearby to be
    eventually uploaded to Nearby PResence web service.
    
    Test: Unit tests only, manually testing N/A since not hooked up
    Bug: b/276307539
    Change-Id: I98558d521827c115f5f4708d7cc259550de283fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395943
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1129884}

---
+++ chromeos/ash/components/nearby/presence/credentials/proto_conversions.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/nearby/internal/proto/metadata.pb.h"
+
+#ifndef CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
+#define CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
+
+namespace ash::nearby::presence {
+
+::nearby::internal::Metadata BuildMetadata(
+    ::nearby::internal::DeviceType device_type,
+    const std::string& account_name,
+    const std::string& device_name,
+    const std::string& user_name,
+    const std::string& profile_url,
+    const std::string& mac_address);
+
+}  // namespace ash::nearby::presence
+
+#endif  // CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
