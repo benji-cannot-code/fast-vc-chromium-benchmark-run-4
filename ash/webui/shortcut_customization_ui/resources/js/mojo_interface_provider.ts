c53b74c185eeb819c664b42c73f53eea7c2f21a4
Jimmy <jimmyxgong@chromium.org>
2023-04-13 23:56:59 +0000

    Shortcuts: Add ReplaceAccelerator mojo API
    ReplaceAccelerator is the final customization API to be added
    to support full accelerator customization. This API is the
    atomic version of Remove + Add Accelerator.
    
    Bug: b:216049298
    Test: ash_webui_unittest
    Change-Id: If7ac3d09125f415d766d60456c1f6bdc2a07b485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385797
    Reviewed-by: David Padlipsky <dpad@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130206}

--- ash/webui/shortcut_customization_ui/resources/js/mojo_interface_provider.ts
+++ ash/webui/shortcut_customization_ui/resources/js/mojo_interface_provider.ts
@@ -9,7 +9,7 @@ import {AcceleratorConfigurationProvider, AcceleratorConfigurationProviderRemote
 
 import {fakeAcceleratorConfig, fakeLayoutInfo} from './fake_data.js';
 import {FakeShortcutProvider} from './fake_shortcut_provider.js';
-import {Accelerator, AcceleratorConfigResult, AcceleratorSource, MojoAcceleratorConfig, MojoLayoutInfo, ShortcutProviderInterface} from './shortcut_types.js';
+import {Accelerator, AcceleratorSource, MojoAcceleratorConfig, MojoLayoutInfo, ShortcutProviderInterface} from './shortcut_types.js';
 
 
 
@@ -101,7 +101,7 @@ export class ShortcutProviderWrapper implements ShortcutProviderInterface {
 
   replaceAccelerator(
       source: AcceleratorSource, action: number, oldAccelerator: Accelerator,
-      newAccelerator: Accelerator): Promise<AcceleratorConfigResult> {
+      newAccelerator: Accelerator): Promise<{result: AcceleratorResultData}> {
     // TODO(cambickel) Replace with real mojo method.
     return this.fakeProvider.replaceAccelerator(
         source, action, oldAccelerator, newAccelerator);
