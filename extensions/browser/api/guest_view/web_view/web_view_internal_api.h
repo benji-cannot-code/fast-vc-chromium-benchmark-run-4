bf17a642be114da69ec6401e246244a321bec40b
Pâris <pmeuleman@chromium.org>
2023-04-13 13:48:24 +0000

    mark flaky CQ's dangling pointers 2023‑W14
    This marks all the flaky dangling pointers detected on the CQ
    during week 14 (2023-04-03 to 2023-04-09 inclusive).
    Note that a few dangling pointers detected were also actual failures,
    those are not marked here, most are fixed by follow up patch sets.
    Detail of the dangling pointers here:
    https://docs.google.com/spreadsheets/d/1k12PQOG4y1-UEV9xDfP1F8FSk4cVFywafEYHmzFubJ8/edit?pli=1#gid=0
    
    Bug: 1432570
    Change-Id: I9187602a946846f23e576cbcb4e4e870b499c02f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4417689
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129807}

--- extensions/browser/api/guest_view/web_view/web_view_internal_api.h
+++ extensions/browser/api/guest_view/web_view/web_view_internal_api.h
@@ -36,7 +36,7 @@ class WebViewInternalExtensionFunction : public ExtensionFunction {
   ~WebViewInternalExtensionFunction() override {}
   bool PreRunValidation(std::string* error) override;
 
-  raw_ptr<WebViewGuest> guest_ = nullptr;
+  raw_ptr<WebViewGuest, DanglingUntriaged> guest_ = nullptr;
 };
 
 class WebViewInternalCaptureVisibleRegionFunction
