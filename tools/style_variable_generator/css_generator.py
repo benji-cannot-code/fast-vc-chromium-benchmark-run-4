8283d72350ad6e16f758a2071ccba358a9214691
Christos Froussios <cfroussios@google.com>
2023-04-13 05:08:33 +0000

    [Jellybean] Add variable aliases to json config
    Change-Id: Icb7d451567d7b5028caa6c71373aad5ae3ebae1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4405517
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129683}

--- tools/style_variable_generator/css_generator.py
+++ tools/style_variable_generator/css_generator.py
@@ -40,11 +40,13 @@ class CSSStyleGenerator(BaseGenerator):
             'typefaces': self.model.typefaces,
             'font_families': self.model.font_families,
             'untyped_css': self.model.untyped_css,
+            'legacy_mappings': self.model.legacy_mappings,
         }
 
     def GetFilters(self):
         return {
             'to_css_var_name': self.ToCSSVarName,
+            'to_css_var_name_unscoped': self.ToCSSVarNameUnscoped,
             'css_opacity': self._CSSOpacity,
             'css_color_rgb': self.CSSColorRGB,
             'process_simple_ref': self.ProcessSimpleRef,
@@ -127,6 +129,9 @@ class CSSStyleGenerator(BaseGenerator):
         var_name = name.translate(str.maketrans('-_.', '_--'))
 
         return '--%s%s' % (self._GetCSSVarPrefix(name), var_name)
+
+    def ToCSSVarNameUnscoped(self, name):
+        return f'--{name}'
 
     def _CSSOpacity(self, opacity):
         if opacity.var:
