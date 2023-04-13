06a7f2948ebc63814ec233758bebbb9bfd7d9e07
Angus L. M. McLean IV <angusmclean@chromium.org>
2023-04-13 02:21:56 +0000

    holding space: Remove AddScan one-off method
    No logical change. `AddScan()` was already calling `AddItemOfType()`.
    
    Bug: b:274477308
    Change-Id: Ide2c4a1c18e006f66cc7506825cf294b3ab0ced0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420275
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129649}

--- chrome/browser/ui/ash/holding_space/holding_space_keyed_service_unittest.cc
+++ chrome/browser/ui/ash/holding_space/holding_space_keyed_service_unittest.cc
@@ -2628,8 +2628,6 @@ class HoldingSpaceKeyedServiceAddAndRemoveItemTest
                                              /*from_incognito_profile=*/false);
         break;
       case HoldingSpaceItem::Type::kScan:
-        holding_space_service->AddScan(file_path);
-        break;
       case HoldingSpaceItem::Type::kScreenRecording:
       case HoldingSpaceItem::Type::kScreenRecordingGif:
       case HoldingSpaceItem::Type::kScreenshot:
