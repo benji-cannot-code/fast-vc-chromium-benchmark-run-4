d44aa95a4ebf828abaae6318d41e8c173dc3be8e
Shakti Sahu <shaktisahu@chromium.org>
2023-04-13 01:04:52 +0000

    [CSC] Plumbing for sending and persisting exps optin status in chrome
    This CL
    1. Adds postmessage calls to pipe the exps optin status from iframe
    to browser.
    2. Stores the status in a pref.
    
    Bug: b/277928503
    Change-Id: If7d3c6f362cf72fd9a65a9af39087eed614f65ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4418635
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129631}

--- chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
+++ chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/companion/core/constants.h"
 #include "chrome/browser/companion/core/mojom/companion.mojom.h"
 #include "chrome/browser/companion/core/msbb_delegate.h"
 #include "components/lens/buildflags.h"
@@ -47,6 +48,7 @@ class CompanionPageHandler : public side_panel::mojom::CompanionPageHandler,
   void OnPromoAction(side_panel::mojom::PromoType promo_type,
                      side_panel::mojom::PromoAction promo_action) override;
   void OnRegionSearchClicked() override;
+  void OnExpsOptInStatusAvailable(bool is_exps_opted_in) override;
 
   // content::WebContentsObserver:
   void PrimaryPageChanged(content::Page& page) override;
