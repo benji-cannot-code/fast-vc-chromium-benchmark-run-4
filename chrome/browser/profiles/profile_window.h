b4b6e8d110f74c7382d4e9fe0d40b9d7b9610b37
David Roger <droger@chromium.org>
2023-04-13 17:36:52 +0000

    [profiles] OpenBrowserWindowForProfile() fails when profile is deleted
    OpenBrowserWindowForProfile() is supposed to not call its callback in
    case of failure.
    Howevere, if the profile or the browser is deleted, the callback is
    still called, with no indication to the user that the operation has
    failed.
    
    This function shows up in the task trace of bug 1431342, and it may be
    caused by this edge case.
    
    This CL observes browser deletion, and handles profile deletion
    gracefully as well. The callback is not called in this case.
    
    It would be better for the function to call its callback every time,
    with an explicit value for failures, but this will be done in a future
    CL (see https://crbug.com/1374315).
    
    Fixed: 1431342
    Change-Id: I0e600dd23183c74927de418207627a9688d2c5ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4423429
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1129949}

--- chrome/browser/profiles/profile_window.h
+++ chrome/browser/profiles/profile_window.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "Not used on Android"
 #endif
 
+class BrowserList;
 class Profile;
 
 namespace base { class FilePath; }
@@ -90,10 +91,16 @@ class BrowserAddedForProfileObserver : public BrowserListObserver {
  private:
   // Overridden from BrowserListObserver:
   void OnBrowserAdded(Browser* browser) override;
+  void OnBrowserRemoved(Browser* browser) override;
+
+  void NotifyBrowserCreatedAnDie();
 
   // Profile for which the browser should be opened.
-  raw_ptr<Profile> profile_;
+  base::WeakPtr<Profile> profile_;
+  raw_ptr<Browser> browser_ = nullptr;
   base::OnceClosure callback_;
+  base::ScopedObservation<BrowserList, BrowserListObserver>
+      browser_list_observation_{this};
 };
 
 }  // namespace profiles
