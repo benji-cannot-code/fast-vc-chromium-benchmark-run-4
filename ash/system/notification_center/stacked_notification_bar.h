ec61968f2f0c0820af062defcfe1b473e5b0eb47
James Cook <jamescook@chromium.org>
2023-04-13 20:35:44 +0000

    chromeos: Clean up NotificationsRefresh flag, part 8
    NotificationsRefresh has been on-by-default since M-104. This CL cleans
    up StackedNotificationBar.
    
    AFAICT NotificationsRefresh makes StackedNotificationBar always be
    visible. This allows some cleanup in NotificationCenterView.
    
    This CL may allow the PillButton constructor to take fewer arguments.
    I'll investigate in a later CL, as this may affect many PillButtons
    in //ash.
    
    Bug: b:257291597
    Test: ash_unittests
    Change-Id: I738dd689144035c90bbcbc98c2a6a3c556611eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420222
    Commit-Queue: James Cook <jamescook@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130074}

--- ash/system/notification_center/stacked_notification_bar.h
+++ ash/system/notification_center/stacked_notification_bar.h
@@ -53,7 +53,6 @@ class StackedNotificationBar : public views::View,
   void SetExpanded();
 
   // views::View:
-  void OnPaint(gfx::Canvas* canvas) override;
   const char* GetClassName() const override;
 
   // message_center::MessageCenterObserver:
@@ -77,9 +76,6 @@ class StackedNotificationBar : public views::View,
   // Search for a icon view in the stacked notification bar based on a provided
   // notification id.
   const StackedNotificationBarIcon* GetIconFromId(const std::string& id) const;
-
-  // Set visibility based on number of stacked notifications or animation state.
-  void UpdateVisibility();
 
   // Add a stacked notification icon to the front or back of the row.
   void AddNotificationIcon(message_center::Notification* notification,
