6eb43168318aef81e58fdec47e6f985251adcdfd
Ben Reich <benreich@chromium.org>
2023-04-13 08:51:52 +0000

    [FilesBulkPinning] Update storage page to use new mojom method
    The storage page previously calculated the offline files space by
    iterating over all the files. This was cumbersome and prone to errors
    and performance issues. CL:4402513 introduced a more performant method
    on the DriveFs mojo, prefer this method.
    
    Fixed: b:277987212
    Test: browser_tests --gtest_filter=*DriveIntegrationBrowserTest*
    Change-Id: Iee59f9710aa58385acc639ae6a13f4e1aa0f7d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4421367
    Reviewed-by: Marcello Salomao <msalomao@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129744}

--- chromeos/ash/components/drivefs/fake_drivefs.h
+++ chromeos/ash/components/drivefs/fake_drivefs.h
@@ -87,6 +87,12 @@ class FakeDriveFs : public drivefs::mojom::DriveFs,
                drivefs::mojom::QueryParametersPtr query_params),
               (override));
 
+  MOCK_METHOD(
+      void,
+      GetOfflineFilesSpaceUsage,
+      (drivefs::mojom::DriveFs::GetOfflineFilesSpaceUsageCallback callback),
+      (override));
+
   const base::FilePath& mount_path() { return mount_path_; }
 
   absl::optional<bool> IsItemPinned(const std::string& path);
@@ -204,10 +210,6 @@ class FakeDriveFs : public drivefs::mojom::DriveFs,
   void SetDocsOfflineEnabled(
       bool enabled,
       drivefs::mojom::DriveFs::SetDocsOfflineEnabledCallback callback) override;
-
-  void GetOfflineFilesSpaceUsage(
-      drivefs::mojom::DriveFs::GetOfflineFilesSpaceUsageCallback callback)
-      override;
 
   const base::FilePath mount_path_;
   int64_t next_stable_id_ = 1;
