80b8c80612623a07d6660b3d63e3c0906c43b4bf
Juliet Levesque <julietlevesque@google.com>
2023-04-13 18:12:57 +0000

    [Nearby Presence] Generate and persist device id
    Implements LocalDeviceDataProvider::GetDeviceId() to fetch the device
    id from prefs, or generate it and persist it for first time
    flow, based on
    go/cros-nearby-presence-web-credential-integration.
    
    Test: Unit test only, no manual testing (code not hooked up).
    Bug: b/276307539
    Change-Id: I35684bfc148f30d8afcdca47bc129e8b49fd4eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400879
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1129980}

---
+++ chromeos/ash/components/nearby/presence/credentials/prefs.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/nearby/presence/credentials/prefs.h"
+
+#include "components/prefs/pref_registry.h"
+#include "components/prefs/pref_registry_simple.h"
+
+namespace ash::nearby::presence {
+
+namespace prefs {
+
+const char kNearbyPresenceDeviceIdPrefName[] =
+    "nearby_presence.local_device_id";
+
+}  // namespace prefs
+
+void RegisterNearbyPresenceCredentialPrefs(PrefRegistrySimple* registry) {
+  // These prefs are not synced across devices on purpose.
+  registry->RegisterStringPref(prefs::kNearbyPresenceDeviceIdPrefName,
+                               /*default_value=*/std::string());
+}
+
+}  // namespace ash::nearby::presence
---
+++ chromeos/ash/components/nearby/presence/credentials/prefs.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PREFS_H_
+#define CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PREFS_H_
+
+class PrefRegistrySimple;
+
+namespace ash::nearby::presence {
+
+namespace prefs {
+
+extern const char kNearbyPresenceDeviceIdPrefName[];
+
+}  // namespace prefs
+
+void RegisterNearbyPresenceCredentialPrefs(PrefRegistrySimple* registry);
+
+}  // namespace ash::nearby::presence
+
+#endif  // CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PREFS_H_
