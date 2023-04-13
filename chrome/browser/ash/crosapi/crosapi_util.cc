64b8b0bf7857663f55271dc6229e34a58ff11234
Sam McNally <sammc@chromium.org>
2023-04-13 00:51:11 +0000

    Support feature flag control over enabling HEVC on ChromeOS ash.
    Support for HEVC on ChromeOS may need to be determined by a runtime
    feature. Reuse the existing PlatformHEVCDecoderSupport feature in the
    places already influenced by kLacrosEnablePlatformHevc, plus the part
    responsible for populating what determines the presence of
    kLacrosEnablePlatformHevc.
    
    Generalise DeriveEnabledFeatures to forward any allowlisted features
    with an overridden value so enabling or disabling HEVC is correctly
    forwarded for browser restarts.
    
    Bug: 272659994
    Change-Id: I602c6f4804debf4b2687c5a0db2ad24f4a51ea9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4402170
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129623}

--- chrome/browser/ash/crosapi/crosapi_util.cc
+++ chrome/browser/ash/crosapi/crosapi_util.cc
@@ -148,6 +148,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/version_info/version_info.h"
 #include "content/public/common/content_switches.h"
 #include "device/bluetooth/floss/floss_features.h"
+#include "media/base/media_switches.h"
 #include "media/capture/mojom/video_capture.mojom.h"
 #include "media/mojo/mojom/stable/stable_video_decoder.mojom.h"
 #include "printing/buildflags/buildflags.h"
@@ -531,7 +532,9 @@ void InjectBrowserInitParams(
   // lacros for runtime handling instead.
   std::vector<crosapi::mojom::BuildFlag> build_flags;
 #if BUILDFLAG(ENABLE_PLATFORM_HEVC)
-  build_flags.emplace_back(crosapi::mojom::BuildFlag::kEnablePlatformHevc);
+  if (base::FeatureList::IsEnabled(media::kPlatformHEVCDecoderSupport)) {
+    build_flags.emplace_back(crosapi::mojom::BuildFlag::kEnablePlatformHevc);
+  }
 #endif  // BUILDFLAG(ENABLE_PLATFORM_HEVC)
 #if BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
   build_flags.emplace_back(
