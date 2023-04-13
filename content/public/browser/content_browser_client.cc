e028d6cefaeac1470cab93de29c62d613a911727
Andrew Rayskiy <greengrape@google.com>
2023-04-13 16:22:58 +0000

    Remove FirewallHoleProxy from content/public/browser/
    The last remaining piece after crrev.com/c/4395937.
    
    Bug: 1429625
    Change-Id: I11b27f6f67bdbac6570c27b77c08b46974a951c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401108
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129895}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -77,10 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/tts_environment_android.h"
 #endif
 
-#if BUILDFLAG(IS_CHROMEOS)
-#include "content/public/browser/firewall_hole_proxy.h"
-#endif
-
 namespace content {
 
 std::unique_ptr<BrowserMainParts> ContentBrowserClient::CreateBrowserMainParts(
