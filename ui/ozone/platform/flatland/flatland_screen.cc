09c58089f832d8283c0bc7aeb1c6cf8a62c6eb0e
Sergey Ulanov <sergeyu@chromium.org>
2023-04-13 21:02:19 +0000

    [Fuchsia] Set display resolution for cast_streaming receiver
    Now ReceiverSessionClient sets VideoLimits and Display in
    ReceiverConfig to limit resolution of the stream to the size of the
    display. Also updated FlatlandScreen to default to the 1280x720
    resolutio as a workaround until bug 1242052 is resolved.
    
    Bug: 1429666, 1242052
    Change-Id: Ib214acc46c880da97ae5be9a2fc4801825ebf32d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414035
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1130091}

--- ui/ozone/platform/flatland/flatland_screen.cc
+++ ui/ozone/platform/flatland/flatland_screen.cc
@@ -12,11 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+namespace {
+constexpr gfx::Size kDefaultDisplaySize = gfx::Size(1280, 720);
+}  // namespace
+
 // TODO(crbug.com/1242052): Integrate with platform APIs for screen enumeration
 // and management, when available.
 
 FlatlandScreen::FlatlandScreen()
-    : displays_({display::Display::GetDefaultDisplay()}) {}
+    : displays_({display::Display(display::kDefaultDisplayId,
+                                  gfx::Rect(kDefaultDisplaySize))}) {}
 
 FlatlandScreen::~FlatlandScreen() = default;
 
