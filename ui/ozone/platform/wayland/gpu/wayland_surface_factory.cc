2f2d86fb8108201c1f45352351be5cbefb64677d
Antonio Gomes <tonikitoo@igalia.com>
2023-04-14 05:56:35 +0000

    fixup! ozone: Implement GetSupportedFormatsForTexturing for Ozone/Wayland.
    In [1], header //ui/gfx/linux/gbm_device.h, that belongs to
    target //ui/gfx/linux:gbm, was included from wayland_surface_factory.cc.
    
    Although the inclusion is guarded by `#if defined(WAYLAND_GBM)` guards,
    wayland_surface_factory.cc itself only depends on //ui/gfx/linux:gbm
    if `use_wayland_gbm` GN arg resolves to TRUE.
    
    Hence, when `use_wayland_gbm = false`, build failts with
    
    ERROR at //ui/o/p/wl/gpu/wayland_surface_factory.cc:30:11:
                                        Include not allowed.
              ^------------------------
    It is not in any dependency of
      //ui/ozone/platform/wayland:wayland
    The include file is in the target(s):
      //ui/gfx/linux:gbm
    which should somehow be reachable.
    tonikitoo@tonikitoo-desktop:~/Devel/chrom
    
    [1] https://crrev.com/c/4410380
    
    R=msisov@igalia.com
    
    b: 277387200
    Change-Id: Ic281b9b898c0e0c941903924b00ac92cac0216dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4422198
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1130292}

--- ui/ozone/platform/wayland/gpu/wayland_surface_factory.cc
+++ ui/ozone/platform/wayland/gpu/wayland_surface_factory.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(WAYLAND_GBM)
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/linux/drm_util_linux.h"
-#include "ui/gfx/linux/gbm_device.h"
+#include "ui/gfx/linux/gbm_device.h"  // nogncheck
 #include "ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.h"
 #include "ui/ozone/platform/wayland/gpu/gbm_surfaceless_wayland.h"
 #include "ui/ozone/public/ozone_platform.h"
