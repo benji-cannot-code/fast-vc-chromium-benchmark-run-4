cb7e0782c7923a580834528e9326a51f8a141155
anthie@google.com <anthie@google.com>
2023-04-14 16:49:40 +0000

    Fix supervised user navigation for Desktop
    This change implements the Web Content Handler interface
    for Desktop.
    More methods will be added as we migrate the content-dependent
    parts of the interstitial into platform-specific objects.
    
    Bug: b/277337029
    Change-Id: I9bedbf36e2265bb109fc161b4761e1481bf1073b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4422711
    Reviewed-by: James Lee <ljjlee@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1130523}

--- chrome/browser/supervised_user/android/supervised_user_web_content_handler_impl.cc
+++ chrome/browser/supervised_user/android/supervised_user_web_content_handler_impl.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/supervised_user/android/website_parent_approval.h"
 #include "chrome/browser/supervised_user/child_accounts/child_account_feedback_reporter_android.h"
-#include "chrome/browser/supervised_user/supervised_user_browser_utils.h"
 #include "chrome/browser/supervised_user/supervised_user_settings_service_factory.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/supervised_user/core/browser/supervised_user_settings_service.h"
---
+++ chrome/browser/supervised_user/linux_mac_windows/supervised_user_web_content_handler_impl.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/supervised_user/linux_mac_windows/supervised_user_web_content_handler_impl.h"
+
+#include "base/notreached.h"
+#include "url/gurl.h"
+
+SupervisedUserWebContentHandlerImpl::SupervisedUserWebContentHandlerImpl(
+    content::WebContents* web_contents,
+    int frame_id)
+    : ChromeSupervisedUserWebContentHandlerBase(web_contents, frame_id) {}
+
+SupervisedUserWebContentHandlerImpl::~SupervisedUserWebContentHandlerImpl() =
+    default;
+
+void SupervisedUserWebContentHandlerImpl::RequestLocalApproval(
+    const GURL& url,
+    const std::u16string& child_display_name,
+    ApprovalRequestInitiatedCallback callback) {
+  // Method unsupposted on Desktop.
+  NOTREACHED_NORETURN();
+}
+
+void SupervisedUserWebContentHandlerImpl::ShowFeedback(GURL url,
+                                                       std::u16string reason) {
+  // This method is never called when kWebFilterInterstitialRefresh is enabled
+  // (Supervised User Interstitial V2 is used).
+  NOTREACHED_NORETURN();
+}
