700616a57c633726e52f6cbed088579ba8d6398f
Louise Brett <loubrett@google.com>
2023-04-14 00:25:00 +0000

    Tabbed mode home tab scope
    Previously navigations within a tabbed web app would only open in
    the home tab if the URL was the same as the start_url.
    This change means any navigations within the scope set in the manifest
    will be opened in the home tab.
    
    Bug: 1381374
    Change-Id: I468929d706cb8ba02748d05a6180a17d41ce6c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227315
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Louise Brett <loubrett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1130220}

--- chrome/browser/ui/web_applications/web_app_browser_controller.h
+++ chrome/browser/ui/web_applications/web_app_browser_controller.h
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "components/services/app_service/public/cpp/icon_types.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/liburlpattern/options.h"
+#include "third_party/liburlpattern/pattern.h"
+#include "third_party/re2/src/re2/set.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/models/image_model.h"
 
@@ -88,6 +91,7 @@ class WebAppBrowserController : public AppBrowserController,
   std::u16string GetFormattedUrlOrigin() const override;
   GURL GetAppStartUrl() const override;
   GURL GetAppNewTabUrl() const override;
+  bool IsUrlInHomeTabScope(const GURL& url) const override;
   bool IsUrlInAppScope(const GURL& url) const override;
   WebAppBrowserController* AsWebAppBrowserController() override;
   bool CanUserUninstall() const override;
@@ -172,6 +176,10 @@ class WebAppBrowserController : public AppBrowserController,
   // given the current state of dark/light mode.
   absl::optional<SkColor> GetResolvedManifestBackgroundColor() const;
 
+  // Returns the set of scope patterns for the home tab scope of tabbed web
+  // apps.
+  absl::optional<RE2::Set> GetTabbedHomeTabScope() const;
+
   const raw_ref<WebAppProvider> provider_;
 
   // Save the display mode at time of launch. The web app display mode may
@@ -186,6 +194,8 @@ class WebAppBrowserController : public AppBrowserController,
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   mutable absl::optional<ui::ImageModel> app_icon_;
   mutable absl::optional<gfx::ImageSkia> home_tab_icon_;
+
+  mutable absl::optional<RE2::Set> home_tab_scope_;
 
 #if BUILDFLAG(IS_CHROMEOS)
   // The result of digital asset link verification of the web app.
