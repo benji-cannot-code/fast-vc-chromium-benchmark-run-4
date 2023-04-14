47bf088045158f868f6e28300a9adf6db7326918
Nina Satragno <nsatragno@chromium.org>
2023-04-14 19:21:07 +0000

    [fido] Remove NOTREACHED on GetAssertionOperation
    The TouchID Mac authenticator hits a NOTREACHED() when it receives a get
    assertion request for which there are no credentials. It is possible to
    get into this situation by deleting a credential after it is presented
    in an account picker, and then selecting it.
    
    Remove this NOTREACHED(). The error code handling results in a message
    telling the user to try a different security key, which is not strictly
    accurate but should be so rare that this is not a big deal.
    
    Fixed: 1432482
    Change-Id: I52e44abb370e76e7935c8ac0cafe7776eda508e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4429050
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1130616}

--- device/fido/mac/authenticator_unittest.mm
+++ device/fido/mac/authenticator_unittest.mm
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/task_environment.h"
 #include "base/test/with_feature_override.h"
+#include "device/fido/authenticator_get_assertion_response.h"
 #include "device/fido/ctap_get_assertion_request.h"
 #include "device/fido/discoverable_credential_metadata.h"
 #include "device/fido/features.h"
+#include "device/fido/fido_constants.h"
 #include "device/fido/fido_request_handler_base.h"
 #include "device/fido/fido_types.h"
 #include "device/fido/mac/authenticator_config.h"
@@ -26,6 +28,9 @@ namespace {
 using GetInfoCallback =
     test::TestCallbackReceiver<std::vector<DiscoverableCredentialMetadata>,
                                FidoRequestHandlerBase::RecognizedCredential>;
+using GetAssertionCallback = test::StatusAndValueCallbackReceiver<
+    CtapDeviceResponseCode,
+    std::vector<AuthenticatorGetAssertionResponse>>;
 
 constexpr char kRp1[] = "one.com";
 constexpr char kRp2[] = "two.com";
@@ -155,6 +160,15 @@ TEST_P(TouchIdAuthenticatorTest, GetPlatformCredentialInfoForRequest_NonRK) {
         std::get<1>(*callback.result()),
         FidoRequestHandlerBase::RecognizedCredential::kNoRecognizedCredential);
   }
+}
+
+TEST_P(TouchIdAuthenticatorTest, GetAssertionEmpty) {
+  GetAssertionCallback callback;
+  CtapGetAssertionRequest request(kRp1, "{json: true}");
+  authenticator_->GetAssertion(std::move(request), CtapGetAssertionOptions(),
+                               callback.callback());
+  callback.WaitForCallback();
+  EXPECT_EQ(callback.status(), CtapDeviceResponseCode::kCtap2ErrNoCredentials);
 }
 
 INSTANTIATE_FEATURE_OVERRIDE_TEST_SUITE(TouchIdAuthenticatorTest);
