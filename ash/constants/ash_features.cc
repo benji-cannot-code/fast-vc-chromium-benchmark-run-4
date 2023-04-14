76576264f2a7ed1744a7e97e6bc3f28cb6b1e4c5
James Cook <jamescook@chromium.org>
2023-04-14 03:22:58 +0000

    chromeos: Clean up NotificationsRefresh flag, part 9
    NotificationsRefresh has been on-by-default since M-104. This CL cleans
    up the ARC notification code in chrome and removes the flag.
    
    I filed a separate bug for the ARC team to clean up the legacy
    pre-NotificationsRefresh code in ARC.
    
    Bug: b:257291597
    Test: ash_unittests, manual on device with "Notify Me" app
    Low-Coverage-Reason: removing dead code from low-coverage files
    Change-Id: Ieac7090accef0866d9c83a942aedc0dcbe00e8e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4424833
    Commit-Queue: James Cook <jamescook@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130267}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1522,11 +1522,6 @@ BASE_FEATURE(kNotificationsInContextMenu,
              "NotificationsInContextMenu",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables new notifications UI and grouped notifications.
-BASE_FEATURE(kNotificationsRefresh,
-             "NotificationsRefresh",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Controls whether to enable on-device grammar check service.
 BASE_FEATURE(kOnDeviceGrammarCheck,
              "OnDeviceGrammarCheck",
@@ -3047,10 +3042,6 @@ bool IsNotificationScrollBarEnabled() {
 
 bool IsNotificationsInContextMenuEnabled() {
   return base::FeatureList::IsEnabled(kNotificationsInContextMenu);
-}
-
-bool IsNotificationsRefreshEnabled() {
-  return base::FeatureList::IsEnabled(kNotificationsRefresh);
 }
 
 bool IsOobeChromeVoxHintEnabled() {
--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -443,7 +443,6 @@ BASE_DECLARE_FEATURE(kNotificationImageDrag);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kNotificationScrollBar);
 COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kNotificationsInContextMenu);
-COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kNotificationsRefresh);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kOnDeviceGrammarCheck);
 COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kOnDeviceSpeechRecognition);
@@ -820,7 +819,6 @@ bool IsNotificationExperimentalShortTimeoutsEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNotificationImageDragEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNotificationScrollBarEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNotificationsInContextMenuEnabled();
-COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNotificationsRefreshEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOAuthIppEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobeChoobeEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobeDriveOfflineEnabled();
