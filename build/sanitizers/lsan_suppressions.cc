c6d1b441b787484a1837e0a7714a5b260ee1f3e7
Yuly Novikov <ynovikov@chromium.org>
2023-04-14 18:02:42 +0000

    Suppress LSAN leaks in libdbus-1.so
    Reported on Linux NVIDIA in gl_tests_passthrough
    
    Bug: 1433299
    Change-Id: I5b221184dd4dd142fb272ebad4a8919e331e19b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4427776
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130555}

--- build/sanitizers/lsan_suppressions.cc
+++ build/sanitizers/lsan_suppressions.cc
@@ -31,7 +31,7 @@ char kLSanDefaultSuppressions[] =
     // suppression works. http://crbug.com/605286
     "leak:__strdup\n"
 
-    // Leaks in Nvidia's GL and Vulkan drivers.
+    // Leaks in GL and Vulkan drivers and system libraries on Linux NVIDIA
     "leak:libGL.so\n"
     "leak:libGLX_nvidia.so\n"
     "leak:libnvidia-cbl.so\n"
@@ -40,6 +40,7 @@ char kLSanDefaultSuppressions[] =
     "leak:libnvidia-rtcore.so\n"
     "leak:nvidia0\n"
     "leak:nvidiactl\n"
+    "leak:libdbus-1.so\n"
 
     // XRandR has several one time leaks.
     "leak:libxrandr\n"
