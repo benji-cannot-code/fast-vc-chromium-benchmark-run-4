a37d8615774c587ced8b54343416d7e107e52269
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2023-04-14 19:06:14 +0000

    Add kHighEfficiencyModeEnabled migration code to an enum pref
    This CL introduces kHighEfficiencyModeState, a replacement pref for
    kHighEfficiencyModeEnabled that can represent more than 2 modes. This is
    to prepare for an eventual new mode of operation for Memory Saver.
    
    The pref isn't actually migrated in this patch. The migration function
    will start being called in the same CL as the settings UI and
    enterprise policy are switched to the enum pref.
    
    Bug: 1430068
    Change-Id: I559d0dd72f9b0ca86e15d3ab1c30e4e2fdb40e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296339
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130607}

--- components/performance_manager/user_tuning/prefs.cc
+++ components/performance_manager/user_tuning/prefs.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/performance_manager/public/user_tuning/prefs.h"
 
+#include "components/performance_manager/public/features.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
@@ -13,6 +14,9 @@ namespace performance_manager::user_tuning::prefs {
 
 const char kHighEfficiencyModeEnabled[] =
     "performance_tuning.high_efficiency_mode.enabled";
+
+const char kHighEfficiencyModeState[] =
+    "performance_tuning.high_efficiency_mode.state";
 
 const char kBatterySaverModeState[] =
     "performance_tuning.battery_saver_mode.state";
@@ -29,6 +33,9 @@ const char kManagedTabDiscardingExceptions[] =
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kHighEfficiencyModeEnabled, false);
   registry->RegisterIntegerPref(
+      kHighEfficiencyModeState,
+      static_cast<int>(HighEfficiencyModeState::kDisabled));
+  registry->RegisterIntegerPref(
       kBatterySaverModeState,
       static_cast<int>(BatterySaverModeState::kEnabledBelowThreshold));
   registry->RegisterTimePref(kLastBatteryUseTimestamp, base::Time());
@@ -38,6 +45,19 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterListPref(kTabDiscardingExceptions,
                              user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterListPref(kManagedTabDiscardingExceptions);
+}
+
+HighEfficiencyModeState GetCurrentHighEfficiencyModeState(
+    PrefService* pref_service) {
+  int state = pref_service->GetInteger(kHighEfficiencyModeState);
+  if (state < static_cast<int>(HighEfficiencyModeState::kDisabled) ||
+      state > static_cast<int>(HighEfficiencyModeState::kEnabledOnTimer)) {
+    int disabled_state = static_cast<int>(HighEfficiencyModeState::kDisabled);
+    pref_service->SetInteger(kHighEfficiencyModeState, disabled_state);
+    state = disabled_state;
+  }
+
+  return static_cast<HighEfficiencyModeState>(state);
 }
 
 BatterySaverModeState GetCurrentBatterySaverModeState(
@@ -51,6 +71,33 @@ BatterySaverModeState GetCurrentBatterySaverModeState(
   }
 
   return static_cast<BatterySaverModeState>(state);
+}
+
+void MigrateHighEfficiencyModePref(PrefService* pref_service) {
+  const PrefService::Preference* state_pref =
+      pref_service->FindPreference(kHighEfficiencyModeState);
+  if (!state_pref->IsDefaultValue()) {
+    // The user has changed the new pref, no migration needed. Clear the old
+    // pref because it won't be used anymore.
+    pref_service->ClearPref(kHighEfficiencyModeEnabled);
+    return;
+  }
+
+  const PrefService::Preference* bool_pref =
+      pref_service->FindPreference(kHighEfficiencyModeEnabled);
+
+  bool enabled = bool_pref->GetValue()->GetBool();
+  int equivalent_int_pref =
+      enabled ? static_cast<int>(HighEfficiencyModeState::kEnabledOnTimer)
+              : static_cast<int>(HighEfficiencyModeState::kDisabled);
+  if (!bool_pref->IsDefaultValue()) {
+    // The user has changed the old pref, but the new pref is still set to the
+    // default value. This means the old pref's state needs to be migrated into
+    // the new pref.
+    pref_service->SetInteger(kHighEfficiencyModeState, equivalent_int_pref);
+    // Clear the old pref because it won't be used anymore.
+    pref_service->ClearPref(kHighEfficiencyModeEnabled);
+  }
 }
 
 }  // namespace performance_manager::user_tuning::prefs
