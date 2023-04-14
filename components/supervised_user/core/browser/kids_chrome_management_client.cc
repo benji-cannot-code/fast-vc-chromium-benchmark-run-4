652767b06dcd2375bc346c1adfdc95d413925e07
Tomasz Jurkiewicz <tju@google.com>
2023-04-14 15:55:27 +0000

    Rename metric to have documentation-compliant name.
    The metric was only introduced recently and changing name is not
    affecting any prod values.
    
    Bug: b:272209467
    Change-Id: I03a2cac1b29efb1f34ea9f5638d606d67f914983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420361
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130478}

--- components/supervised_user/core/browser/kids_chrome_management_client.cc
+++ components/supervised_user/core/browser/kids_chrome_management_client.cc
@@ -43,7 +43,7 @@ constexpr char kClassifyUrlDataContentType[] =
     "application/x-www-form-urlencoded";
 
 constexpr char kClassifyUrlNetOrHttpStatusMetric[] =
-    "ManagedUsers.ClassifyUrlRequest.NetOrHttpStatus";
+    "FamilyLinkUser.ClassifyUrlRequest.NetOrHttpStatus";
 
 // Constants for ClassifyURL.
 constexpr char kClassifyUrlOauthConsumerName[] = "kids_url_classifier";
