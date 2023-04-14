428e19838b9f05a86021aa6c95e5c2b79460734b
Ernesto Izquierdo Clua <eic@google.com>
2023-04-14 15:15:19 +0000

    [ios] Disconnect CVC prompt from c++ dependencies.
    This patch adds logic to disconnect the CardUnmaskPromptViewController
    from its c++ dependencies when these are freed, in order to prevent
    a UAF crash.
    
    Fixed: 1428838
    Change-Id: I4adc4f5a0a8c46c3a2471342fdc2301a82ba67a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415245
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130459}

--- ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
+++ ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
@@ -67,7 +67,8 @@ const char kFooterDummyLinkTarget[] = "about:blank";
   // Button displayed on the right side of the navigation bar.
   // Tapping it sends the data in the prompt for verification.
   UIBarButtonItem* _confirmButton;
-  // Owns `self`.
+  // Owns `self`. A value of nullptr means the view controller is dismissed or
+  // about to be dismissed.
   autofill::CardUnmaskPromptViewBridge* _bridge;  // weak
   // Model of the CVC input cell.
   TableViewTextEditItem* _CVCInputItem;
@@ -103,6 +104,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
+  if (_bridge == nullptr) {
+    return;
+  }
 
   // Default inset inherited from `super` is for cells that display icons.
   // Using smaller inset.
@@ -143,6 +148,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 
 - (void)loadModel {
   [super loadModel];
+
+  if (_bridge == nullptr) {
+    return;
+  }
 
   TableViewModel* model = self.tableViewModel;
   [model addSectionWithIdentifier:SectionIdentifierHeader];
@@ -198,22 +207,34 @@ const char kFooterDummyLinkTarget[] = "about:blank";
   [self presentViewController:errorAlert animated:YES completion:nil];
 }
 
+- (void)disconnectFromBridge {
+  _bridge = nullptr;
+}
+
 #pragma mark - UIAdaptivePresentationControllerDelegate
 
 - (void)presentationControllerDidDismiss:
     (UIPresentationController*)presentationController {
+  if (_bridge == nullptr) {
+    return;
+  }
   // Notify bridge that UI was dismissed.
   _bridge->NavigationControllerDismissed();
-  _bridge = nullptr;
 }
 
 #pragma mark - Actions
 
 - (void)onCancelTapped {
-  _bridge->PerformClose();
+  if (_bridge) {
+    _bridge->PerformClose();
+  }
 }
 
 - (void)onVerifyTapped {
+  if (_bridge == nullptr) {
+    return;
+  }
+
   autofill::CardUnmaskPromptController* controller = _bridge->GetController();
 
   NSString* CVC = _CVCInputItem.textFieldValue;
@@ -314,6 +335,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 
 // Returns a new confirm button for the navigation bar.
 - (UIBarButtonItem*)createConfirmButton {
+  if (_bridge == nullptr) {
+    return nil;
+  }
+
   NSString* confirmButtonText =
       base::SysUTF16ToNSString(_bridge->GetController()->GetOkButtonLabel());
   UIBarButtonItem* confirmButton =
@@ -335,6 +360,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 
 // Returns the model for the CVC input cell.
 - (TableViewTextEditItem*)createCVCInputItem {
+  if (_bridge == nullptr) {
+    return nil;
+  }
+
   autofill::CardUnmaskPromptController* controller = _bridge->GetController();
 
   TableViewTextEditItem* CVCInputItem =
@@ -409,6 +438,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 // The actual correctness of the CVC is verified by the server when the form is
 // submitted.
 - (BOOL)isCVCInputValid {
+  if (_bridge == nullptr) {
+    return NO;
+  }
+
   return _bridge->GetController()->InputCvcIsValid(
       base::SysNSStringToUTF16(_CVCInputItem.textFieldValue));
 }
@@ -417,6 +450,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 // the expiration date input is not being displayed. The actual correctness of
 // the expiration date is verified by the server when the form is submitted.
 - (BOOL)isExpirationInputValid {
+  if (_bridge == nullptr) {
+    return NO;
+  }
+
   if (!_expirationDateInputItem) {
     return YES;
   }
@@ -436,6 +473,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 // Updates `self` after the error alert was dismissed.
 // When `closeOnDismiss` is YES, `self` is dismissed.
 - (void)onErrorAlertDismissedAndShouldCloseOnDismiss:(BOOL)closeOnDismiss {
+  if (_bridge == nullptr) {
+    return;
+  }
+
   if (closeOnDismiss) {
     _bridge->PerformClose();
     return;
@@ -454,6 +495,9 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 
 // Helper method that fetches the instructions from the Controller.
 - (NSString*)instructions {
+  if (_bridge == nullptr) {
+    return nil;
+  }
   autofill::CardUnmaskPromptController* controller = _bridge->GetController();
   return base::SysUTF16ToNSString(controller->GetInstructionsMessage());
 }
@@ -569,6 +613,9 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 #pragma mark - TableViewLinkHeaderFooterDelegate
 
 - (void)view:(TableViewLinkHeaderFooterView*)view didTapLinkURL:(CrURL*)URL {
+  if (_bridge == nullptr) {
+    return;
+  }
   // Notify Controller about the Expiration Date Form being shown so it updates
   // its state accordingly.
   _bridge->GetController()->NewCardLinkClicked();
