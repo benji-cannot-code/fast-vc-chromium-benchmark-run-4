629c0ce932489c5e344261cd6f9f7e42d2b9e8b6
Gauthier Ambard <gambard@chromium.org>
2023-04-14 14:18:23 +0000

    [iOS] Follow up cleanup for ImageSaver
    Refactoring a block in a separate method.
    
    Bug: None
    Change-Id: Ia60e00edfac263a073fe440e66d13d7e6340a7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4422693
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130429}

--- ios/chrome/browser/shared/ui/util/image/image_saver.mm
+++ ios/chrome/browser/shared/ui/util/image/image_saver.mm
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
-- (void)saveImageAtURL:(const GURL&)url
+- (void)saveImageAtURL:(const GURL&)URL
               referrer:(const web::Referrer&)referrer
               webState:(web::WebState*)webState
     baseViewController:(UIViewController*)baseViewController {
@@ -57,46 +57,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   DCHECK(tabHelper);
 
   __weak ImageSaver* weakSelf = self;
-  tabHelper->GetImageData(url, referrer, ^(NSData* data) {
-    ImageSaver* strongSelf = weakSelf;
-    if (!strongSelf) {
-      return;
-    }
-
-    if (data.length == 0) {
-      [strongSelf displayPrivacyErrorAlertOnMainQueue:
-                      l10n_util::GetNSString(
-                          IDS_IOS_SAVE_IMAGE_NO_INTERNET_CONNECTION)];
-      return;
-    }
-
-    // Use -imageWithData to validate `data`, but continue to pass the raw
-    // `data` to -savePhoto to ensure no data loss occurs.
-    UIImage* savedImage = [UIImage imageWithData:data];
-    if (!savedImage) {
-      [strongSelf
-          displayPrivacyErrorAlertOnMainQueue:l10n_util::GetNSString(
-                                                  IDS_IOS_SAVE_IMAGE_ERROR)];
-      return;
-    }
-
-    // Dump `data` into the photo library. Requires the usage of
-    // NSPhotoLibraryAddUsageDescription.
-    [[PHPhotoLibrary sharedPhotoLibrary]
-        performChanges:^{
-          PHAssetResourceCreationOptions* options =
-              [[PHAssetResourceCreationOptions alloc] init];
-          [[PHAssetCreationRequest creationRequestForAsset]
-              addResourceWithType:PHAssetResourceTypePhoto
-                             data:data
-                          options:options];
-        }
-        completionHandler:^(BOOL success, NSError* error) {
-          [weakSelf image:savedImage
-              didFinishSavingWithError:error
-                           contextInfo:nil];
-        }];
+  tabHelper->GetImageData(URL, referrer, ^(NSData* data) {
+    [weakSelf didGetImageData:data];
   });
+}
+
+// Callback when the image `data` got retrieved from the tab.
+- (void)didGetImageData:(NSData*)data {
+  if (data.length == 0) {
+    [self
+        displayPrivacyErrorAlertOnMainQueue:
+            l10n_util::GetNSString(IDS_IOS_SAVE_IMAGE_NO_INTERNET_CONNECTION)];
+    return;
+  }
+
+  // Use -imageWithData to validate `data`, but continue to pass the raw
+  // `data` to -savePhoto to ensure no data loss occurs.
+  UIImage* savedImage = [UIImage imageWithData:data];
+  if (!savedImage) {
+    [self displayPrivacyErrorAlertOnMainQueue:l10n_util::GetNSString(
+                                                  IDS_IOS_SAVE_IMAGE_ERROR)];
+    return;
+  }
+
+  // Dump `data` into the photo library. Requires the usage of
+  // NSPhotoLibraryAddUsageDescription.
+  __weak ImageSaver* weakSelf = self;
+  [[PHPhotoLibrary sharedPhotoLibrary]
+      performChanges:^{
+        PHAssetResourceCreationOptions* options =
+            [[PHAssetResourceCreationOptions alloc] init];
+        [[PHAssetCreationRequest creationRequestForAsset]
+            addResourceWithType:PHAssetResourceTypePhoto
+                           data:data
+                        options:options];
+      }
+      completionHandler:^(BOOL success, NSError* error) {
+        [weakSelf image:savedImage
+            didFinishSavingWithError:error
+                         contextInfo:nil];
+      }];
 }
 
 // Called when Chrome has been denied access to add photos or videos and the
