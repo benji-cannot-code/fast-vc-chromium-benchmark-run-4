fd14b63e6748feddab9304c41a2da61e397ac109
Ed Chin <edchin@google.com>
2023-04-14 19:22:09 +0000

    [ios][feed] Flag for disable hot start refresh
    Change-Id: I0b33f7eaa2846f62fa4baaa03a2bb00d81f4f613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4425882
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1130617}

--- ios/chrome/browser/ntp/features.h
+++ ios/chrome/browser/ntp/features.h
@@ -54,6 +54,9 @@ BASE_DECLARE_FEATURE(kEnableFeedExperimentTagging);
 // Feature flag to enable the Set Up List.
 BASE_DECLARE_FEATURE(kIOSSetUpList);
 
+// Feature flag to disable Discover-controlled foregrounding refreshes.
+BASE_DECLARE_FEATURE(kFeedDisableHotStartRefresh);
+
 // Feature param under `kEnableFeedBackgroundRefresh` to also enable background
 // refresh for the Following feed.
 extern const char kEnableFollowingFeedBackgroundRefresh[];
@@ -235,5 +238,8 @@ bool IsFeedExperimentTaggingEnabled();
 
 // Whether the Set Up List feature is enabled.
 bool IsIOSSetUpListEnabled();
+
+// Whether Discover-controlled foregrounding refreshes are disabled.
+bool IsFeedHotStartRefreshDisabled();
 
 #endif  // IOS_CHROME_BROWSER_NTP_FEATURES_H_
