479852ae93e3c7058676b6ec7a9d43e8ddb59528
Austin Tankiang <austinct@chromium.org>
2023-04-14 04:09:25 +0000

    Add GetBulkPinProgress file manager private api
    Bug: b/275635867
    Change-Id: I3def3fed94e7be7911ac68deceb3192ae345a760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4421950
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130273}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1844,6 +1844,7 @@ enum HistogramValue {
   OS_EVENTS_STOPCAPTURINGEVENTS = 1780,
   SIDEPANEL_SETPANELBEHAVIOR = 1781,
   SIDEPANEL_GETPANELBEHAVIOR = 1782,
+  FILEMANAGERPRIVATE_GETBULKPINPROGRESS = 1783,
   // Last entry: Add new entries above, then run:
   // tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
