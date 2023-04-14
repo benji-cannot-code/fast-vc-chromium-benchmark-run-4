27a5e5bb2657be2f21ef3464314b21baf6776ecc
Matthew Denton <mpdenton@chromium.org>
2023-04-14 10:48:25 +0000

    Add sequence checkers/annotations to AddressTrackerLinux
    Add locking annotations to AddressTrackerLinux to ensure variables are
    only accessed when holding the appropriate lock.
    
    Also convert the ThreadChecker to a SequenceChecker and call it in
    various methods.
    
    Bug: 1312226
    Change-Id: Iffc5895243126e57d2fda3e14a28eb9034423a28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4425201
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130367}

--- net/base/address_tracker_linux.cc
+++ net/base/address_tracker_linux.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/sequence_checker.h"
 #include "base/task/current_thread.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "base/threading/thread_restrictions.h"
@@ -173,11 +174,13 @@ AddressTrackerLinux::AddressTrackerLinux(
       tracking_(true) {
   DCHECK(!address_callback.is_null());
   DCHECK(!link_callback.is_null());
+  DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
 AddressTrackerLinux::~AddressTrackerLinux() = default;
 
 void AddressTrackerLinux::Init() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 #if BUILDFLAG(IS_ANDROID)
   // RTM_GETLINK stopped working in Android 11 (see
   // https://developer.android.com/preview/privacy/mac-address),
@@ -275,11 +278,13 @@ void AddressTrackerLinux::Init() {
 }
 
 bool AddressTrackerLinux::DidTrackingInitSucceedForTesting() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   CHECK(tracking_);
   return watcher_ != nullptr;
 }
 
 void AddressTrackerLinux::AbortAndForceOnline() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   watcher_.reset();
   netlink_fd_.reset();
   AddressTrackerAutoLock lock(*this, connection_type_lock_);
@@ -299,6 +304,7 @@ std::unordered_set<int> AddressTrackerLinux::GetOnlineLinks() const {
 }
 
 void AddressTrackerLinux::SetDiffCallback(DiffCallback diff_callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   diff_callback_ = std::move(diff_callback);
 
   // Send the initial configuration to the diff callback.
@@ -318,6 +324,7 @@ void AddressTrackerLinux::SetDiffCallback(DiffCallback diff_callback) {
 }
 
 bool AddressTrackerLinux::IsInterfaceIgnored(int interface_index) const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (ignored_interfaces_.empty())
     return false;
 
@@ -345,6 +352,7 @@ void AddressTrackerLinux::ReadMessages(bool* address_changed,
                                        bool* tunnel_changed,
                                        AddressMapDiff* address_map_diff,
                                        OnlineLinksDiff* online_links_diff) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   *address_changed = false;
   *link_changed = false;
   *tunnel_changed = false;
@@ -388,6 +396,7 @@ void AddressTrackerLinux::HandleMessage(const char* buffer,
                                         bool* tunnel_changed,
                                         AddressMapDiff* address_map_diff,
                                         OnlineLinksDiff* online_links_diff) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(buffer);
   // Note that NLMSG_NEXT decrements |length| to reflect the number of bytes
   // remaining in |buffer|.
@@ -519,6 +528,7 @@ void AddressTrackerLinux::HandleMessage(const char* buffer,
 }
 
 void AddressTrackerLinux::OnFileCanReadWithoutBlocking() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   bool address_changed;
   bool link_changed;
   bool tunnel_changed;
@@ -552,6 +562,7 @@ bool AddressTrackerLinux::IsTunnelInterfaceName(const char* name) {
 }
 
 void AddressTrackerLinux::UpdateCurrentConnectionType() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   AddressTrackerLinux::AddressMap address_map = GetAddressMap();
   std::unordered_set<int> online_links = GetOnlineLinks();
 
@@ -591,7 +602,7 @@ AddressTrackerLinux::AddressTrackerAutoLock::AddressTrackerAutoLock(
   if (tracker_->tracking_) {
     lock_->Acquire();
   } else {
-    DCHECK(tracker_->thread_checker_.CalledOnValidThread());
+    DCHECK_CALLED_ON_VALID_SEQUENCE(tracker_->sequence_checker_);
   }
 }
 
@@ -599,6 +610,8 @@ AddressTrackerLinux::AddressTrackerAutoLock::~AddressTrackerAutoLock() {
   if (tracker_->tracking_) {
     lock_->AssertAcquired();
     lock_->Release();
+  } else {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(tracker_->sequence_checker_);
   }
 }
 
