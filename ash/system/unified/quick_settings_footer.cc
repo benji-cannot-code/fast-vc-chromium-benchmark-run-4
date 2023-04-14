bc16492bc8004fc0598ac9ff94864b7d8918371f
James Cook <jamescook@chromium.org>
2023-04-14 17:08:38 +0000

    chromeos: Clean up PillButton now that NotificationsRefresh is launched
    PillButton took two extra constructor arguments to support the legacy
    (pre-NotificationsRefresh) message center. NotificationsRefresh shipped
    in M-104 and the flag is being removed.
    
    All call sites now pass use_light_colors = false and
    rounded_highlight_path = true. Remove the constructor arguments added in
    https://chromium-review.googlesource.com/c/chromium/src/+/3239026
    
    Bug: b:257291597
    Test: ash_unittests, visually checked style viewer
    Change-Id: Ia0b507cdcca061f8babfc35ff8bd9fa9fb534a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4424101
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130525}

--- ash/system/unified/quick_settings_footer.cc
+++ ash/system/unified/quick_settings_footer.cc
@@ -63,8 +63,6 @@ QsBatteryInfoViewBase::QsBatteryInfoViewBase(
                  type,
                  icon,
                  kHorizontalSpacing,
-                 /*use_light_colors=*/false,
-                 /*rounded_highlight_path=*/true,
                  kPaddingReduction) {
   PowerStatus::Get()->AddObserver(this);
   SetImageLabelSpacing(kImageLabelSpacing);
