4b528a0c81134e8c83f026f32919b13f7a889b9d
Colin Kincaid <ckincaid@chromium.org>
2023-04-14 00:04:50 +0000

    multipaste: Tie clipboard history refresh launch to Jelly launch
    This CL makes the enablement of the clipboard history refresh flag
    require the enablement of the Jelly colors flag. The enforcement of this
    assumption, which is also made in the clipboard history refresh spec,
    allows future UI changes not to account for the possibility of being
    launched without Jelly.
    
    Bug: b/262443689
    Change-Id: I39d96f9d09986b8e9189dfa501235b40fbee6223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4425169
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1130211}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -347,7 +347,7 @@ BASE_FEATURE(kClipboardHistoryLongpress,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Enables updated UI for the clipboard history menu and new system behavior
-// related to clipboard history.
+// related to clipboard history. Requires jelly-colors flag to also be enabled.
 BASE_FEATURE(kClipboardHistoryRefresh,
              "ClipboardHistoryRefresh",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -2539,7 +2539,8 @@ bool IsClipboardHistoryLongpressEnabled() {
 }
 
 bool IsClipboardHistoryRefreshEnabled() {
-  return base::FeatureList::IsEnabled(kClipboardHistoryRefresh);
+  return chromeos::features::IsJellyEnabled() &&
+         base::FeatureList::IsEnabled(kClipboardHistoryRefresh);
 }
 
 bool IsClipboardHistoryReorderEnabled() {
