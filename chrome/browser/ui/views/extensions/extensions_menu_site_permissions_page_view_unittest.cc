a94dfb260c94a74d3592e64ce233f3a0e4846139
Emilia Paz <emiliapaz@chromium.org>
2023-04-14 23:48:40 +0000

    [Extensions cleanup] Use helper method to navigate in various tests
    Test class adds web contents at construction, and has a method to
    navigate. Thus, we can use such method in test instead of creating new
    web contents (which is done similarly in the rest of the tests).
    
    Bug: n/a
    Change-Id: I78fbc29283cabbbcb48677bd2c12539002d80b1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4424095
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130743}

--- chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view_unittest.cc
@@ -277,15 +277,10 @@ TEST_F(ExtensionsSitePermissionsPageViewUnitTest,
 // permissions controls (e.g restricted site) closes the site permissions page.
 TEST_F(ExtensionsSitePermissionsPageViewUnitTest,
        PageNavigationWithMenuOpen_UserLosesRuntimeHostPermissionsControls) {
-  content::WebContentsTester* web_contents_tester =
-      AddWebContentsAndGetTester();
-
   auto extension =
       InstallExtensionWithHostPermissions("Extension", {"<all_urls>"});
 
-  const GURL url("http://www.non-restricted.com");
-  web_contents_tester->NavigateAndCommit(url);
-  WaitForAnimation();
+  NavigateAndCommit("http://www.non-restricted.com");
 
   ShowSitePermissionsPage(extension->id());
   EXPECT_FALSE(IsMainPageOpened());
@@ -293,9 +288,7 @@ TEST_F(ExtensionsSitePermissionsPageViewUnitTest,
 
   // While the menu is open, navigate to an url where extension should not have
   // a site permissions page.
-  const GURL restricted_url("chrome://extensions");
-  web_contents_tester->NavigateAndCommit(restricted_url);
-  WaitForAnimation();
+  NavigateAndCommit("chrome://extensions");
 
   // Menu should navigate back to main page since site permissions page should
   // not be visible for the new url.
@@ -307,15 +300,10 @@ TEST_F(ExtensionsSitePermissionsPageViewUnitTest,
 // permissions controls updates the page contents.
 TEST_F(ExtensionsSitePermissionsPageViewUnitTest,
        PageNavigationWithMenuOpen_UserMaintainsRuntimeHostPermissionsControls) {
-  content::WebContentsTester* web_contents_tester =
-      AddWebContentsAndGetTester();
-
   auto extension =
       InstallExtensionWithHostPermissions("Extension", {"<all_urls>"});
 
-  const GURL url_a("http://www.a.com");
-  web_contents_tester->NavigateAndCommit(url_a);
-  WaitForAnimation();
+  NavigateAndCommit("http://www.a.com");
 
   ShowSitePermissionsPage(extension->id());
   EXPECT_FALSE(IsMainPageOpened());
@@ -323,9 +311,7 @@ TEST_F(ExtensionsSitePermissionsPageViewUnitTest,
 
   // While the menu is open, navigate to an url where extension also should have
   // a site permissions page.
-  const GURL url_b("http://www.b.com");
-  web_contents_tester->NavigateAndCommit(url_b);
-  WaitForAnimation();
+  NavigateAndCommit("http://www.b.com");
 
   // Menu should stay open in site permissions page for `extension`.
   EXPECT_FALSE(IsMainPageOpened());
