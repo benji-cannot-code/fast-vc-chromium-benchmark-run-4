ef69db123a9d9625c77d4f289b4f133befce6fae
Shiyi Zou <shiyi.zou@intel.com>
2023-04-14 14:50:36 +0000

    Implement EnergyMetricsProvider in //components
    EnergyMetricsProvider provides system-wide energy consumption metrics.
    It currently only works on Windows 11, and we will add implementations
    for other supported operating systems.
    
    Bug: 1385251
    Change-Id: I8474ea042535f42a92bece364264c68e88e2c9fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317885
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130446}

---
+++ components/power_metrics/energy_metrics_provider.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/power_metrics/energy_metrics_provider.h"
+
+#include "build/build_config.h"
+#if BUILDFLAG(IS_WIN)
+#include "components/power_metrics/energy_metrics_provider_win.h"
+#endif  // BUILDFLAG(IS_WIN)
+
+namespace power_metrics {
+
+EnergyMetricsProvider::EnergyMetricsProvider() = default;
+EnergyMetricsProvider::~EnergyMetricsProvider() = default;
+
+// static
+std::unique_ptr<EnergyMetricsProvider> EnergyMetricsProvider::Create() {
+#if BUILDFLAG(IS_WIN)
+  return EnergyMetricsProviderWin::Create();
+#else
+  return nullptr;
+#endif  // BUILDFLAG(IS_WIN)
+}
+
+}  // namespace power_metrics
