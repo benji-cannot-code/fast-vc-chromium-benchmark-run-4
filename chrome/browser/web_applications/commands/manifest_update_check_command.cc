92d719c37807f083fabc12ec064cf6cda00fdeaf
Daniel Murphy <dmurph@google.com>
2023-04-14 15:29:31 +0000

    [dPWA] Removing waiting for favicon urls.
    By waiting for favicons, we actually never resolve the callback if
    the page doesn't have favicons at all. This turns the settings into
    a "best effort", which means we only fetch the favicons that actually
    exist.
    
    This change also does a little code cleanup to make the options better.
    
    Bug: 1328977,1420627
    Change-Id: I9e00cd2515b38a783336b1f1fd7f6d04a5ce0dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4404976
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130465}

--- chrome/browser/web_applications/commands/manifest_update_check_command.cc
+++ chrome/browser/web_applications/commands/manifest_update_check_command.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/browser/web_applications/web_app_ui_manager.h"
+#include "chrome/browser/web_applications/web_contents/web_app_icon_downloader.h"
 #include "chrome/common/chrome_features.h"
 #include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
@@ -162,10 +163,10 @@ void ManifestUpdateCheckCommand::DownloadNewIconBitmaps(
   base::flat_set<GURL> icon_urls =
       GetValidIconUrlsToDownload(new_install_info_);
 
+  IconDownloaderOptions options = {.skip_page_favicons = true,
+                                   .fail_all_if_any_fail = true};
   icon_downloader_.emplace(web_contents_.get(), std::move(icon_urls),
-                           std::move(next_step_callback));
-  icon_downloader_->SkipPageFavicons();
-  icon_downloader_->FailAllIfAnyFail();
+                           std::move(next_step_callback), options);
   icon_downloader_->Start();
 }
 
