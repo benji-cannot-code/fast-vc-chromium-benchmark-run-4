4075d6120be36542d97304a780fdbeaee0eea56e
Sean Kau <skau@chromium.org>
2023-04-15 02:11:34 +0000

    Add SchemeVariant to the CPM::Key and NativeTheme
    Plumb scheme_variant through the color system so that it can be
    used to manipulate the colors that are generated.
    
    Change-Id: I7eedee5782b4fde154b4b8b36ffaf7eab2c8a50d
    Bug: b:258719005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416440
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130773}

--- ui/color/color_provider_manager.cc
+++ ui/color/color_provider_manager.cc
@@ -60,6 +60,7 @@ ColorProviderManager::Key::Key()
           SystemTheme::kDefault,
           FrameType::kChromium,
           absl::nullopt,
+          absl::nullopt,
           nullptr) {}
 
 ColorProviderManager::Key::Key(
@@ -68,6 +69,7 @@ ColorProviderManager::Key::Key(
     SystemTheme system_theme,
     FrameType frame_type,
     absl::optional<SkColor> user_color,
+    absl::optional<SchemeVariant> scheme_variant,
     scoped_refptr<ThemeInitializerSupplier> custom_theme)
     : color_mode(color_mode),
       contrast_mode(contrast_mode),
@@ -75,6 +77,7 @@ ColorProviderManager::Key::Key(
       system_theme(system_theme),
       frame_type(frame_type),
       user_color(user_color),
+      scheme_variant(scheme_variant),
       custom_theme(std::move(custom_theme)) {}
 
 ColorProviderManager::Key::Key(const Key&) = default;
