ca2ec44eb742f73ea9cbe9d794a879b121eb472c
Gavin Williams <gavinwill@chromium.org>
2023-04-15 00:11:00 +0000

    Printing: Add metric for counting nearby printers
    This metric will help clarify the ratio of Automatic to Discovered
    printers seen by users on the Printer Settings page.
    
    Change-Id: I2aa5075163a02abd0a167fb9232161057eeff7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416083
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1130749}

--- chrome/browser/ui/webui/settings/ash/cups_printers_handler.cc
+++ chrome/browser/ui/webui/settings/ash/cups_printers_handler.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_string_value_serializer.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/ranges/algorithm.h"
@@ -76,6 +77,11 @@ using ::chromeos::Uri;
 using ::printing::PrinterQueryResult;
 
 constexpr int kPpdMaxLineLength = 255;
+
+constexpr char kNearbyAutomaticPrintersHistogramName[] =
+    "Printing.CUPS.NearbyNetworkAutomaticPrintersCount";
+constexpr char kNearbyDiscoveredPrintersHistogramName[] =
+    "Printing.CUPS.NearbyNetworkDiscoveredPrintersCount";
 
 void OnRemovedPrinter(const Printer::PrinterProtocol& protocol, bool success) {
   if (success) {
@@ -1138,6 +1144,11 @@ void CupsPrintersHandler::HandleStartDiscovery(const base::Value::List& args) {
                     printers_manager_->GetPrinters(PrinterClass::kAutomatic));
   OnPrintersChanged(PrinterClass::kDiscovered,
                     printers_manager_->GetPrinters(PrinterClass::kDiscovered));
+
+  base::UmaHistogramCounts100(kNearbyAutomaticPrintersHistogramName,
+                              automatic_printers_.size());
+  base::UmaHistogramCounts100(kNearbyDiscoveredPrintersHistogramName,
+                              discovered_printers_.size());
   UMA_HISTOGRAM_COUNTS_100(
       "Printing.CUPS.PrintersDiscovered",
       discovered_printers_.size() + automatic_printers_.size());
