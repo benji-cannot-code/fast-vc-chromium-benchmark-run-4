396d3c0bf829c82d7d4fa8dfc409a1c2043ce383
Sina Firoozabadi <sinafirooz@google.com>
2023-04-16 02:33:33 +0000

    Add finch flag BlockMidiByDefault
    Change-Id: I2b61f0e97019d43e0bfdca5b0ac24f0dc9c65701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4409990
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1130914}

---
+++ services/audio/public/cpp/audio_features.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file defines all the public base::FeatureList features for the
+// services/audio module.
+
+#ifndef SERVICES_AUDIO_PUBLIC_CPP_AUDIO_FEATURES_H_
+#define SERVICES_AUDIO_PUBLIC_CPP_AUDIO_FEATURES_H_
+
+#include "base/feature_list.h"
+#include "build/build_config.h"
+#include "services/audio/public/cpp/audio_features_export.h"
+
+namespace features {
+
+// The features should be documented alongside the definition of their values
+// in the .cc file.
+AUDIO_FEATURES_EXPORT BASE_DECLARE_FEATURE(kBlockMidiByDefault);
+
+}  // namespace features
+
+#endif  // SERVICES_AUDIO_PUBLIC_CPP_AUDIO_FEATURES_H_
