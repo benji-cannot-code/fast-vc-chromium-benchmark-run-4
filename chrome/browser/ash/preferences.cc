743b5086927fbd5821585b4d952888eb7516554e
Osama Fathy <osamafathy@google.com>
2023-04-17 17:31:12 +0000

    [OOBE] Remove Consolidated Consent field trial
    - Remove the consolidated consent field trial as part of cleaning
    up the consolidated consent feature flag.
    - Deprecate `per_user_metrics.trial_group` local state pref.
    
    Bug: b:264170220
    
    Change-Id: I17d07b3c536d0c1834d145b044ab5d37e36adfde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4428874
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131329}

--- chrome/browser/ash/preferences.cc
+++ chrome/browser/ash/preferences.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/drive/file_system_util.h"
 #include "chrome/browser/ash/input_method/input_method_persistence.h"
 #include "chrome/browser/ash/input_method/input_method_syncer.h"
-#include "chrome/browser/ash/login/consolidated_consent_field_trial.h"
 #include "chrome/browser/ash/login/hid_detection_revamp_field_trial.h"
 #include "chrome/browser/ash/login/login_pref_names.h"
 #include "chrome/browser/ash/login/session/user_session_manager.h"
@@ -166,7 +165,6 @@ void Preferences::RegisterPrefs(PrefRegistrySimple* registry) {
                                 false);
 
   RegisterLocalStatePrefs(registry);
-  ash::consolidated_consent_field_trial::RegisterLocalStatePrefs(registry);
   ash::hid_detection_revamp_field_trial::RegisterLocalStatePrefs(registry);
 }
 
--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/ash/login/consolidated_consent_field_trial.h"
 #include "chrome/common/channel_info.h"
 #include "chromeos/ash/components/login/auth/recovery/recovery_utils.h"
 #include "chromeos/ash/services/multidevice_setup/public/cpp/first_run_field_trial.h"
@@ -71,11 +70,6 @@ void ChromeBrowserFieldTrials::SetUpClientSideFieldTrials(
     bool has_seed,
     const variations::EntropyProviders& entropy_providers,
     base::FeatureList* feature_list) {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  ash::consolidated_consent_field_trial::Create(
-      entropy_providers.default_entropy(), feature_list, local_state_);
-#endif
-
   // Only create the fallback trials if there isn't already a variations seed
   // being applied. This should occur during first run when first-run variations
   // isn't supported. It's assumed that, if there is a seed, then it either
