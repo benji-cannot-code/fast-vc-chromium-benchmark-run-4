0fea522c5b3177c8d68ea756f8e4fae18f7be98f
Zelin Liu <zelin@chromium.org>
2023-04-17 22:59:33 +0000

    iwa: Introduce IsolatedWebAppTask in task manager
    With this change, web apps are tagged by WebAppTag. For Isolated Web
    Apps, an IsolatedWebAppTask will be created inside task manager. For
    other web apps, current logic will be retained.
    
    Bug: 1426652
    Change-Id: Ie6575045fdd1c1d9868f9e9c9ba48a1b26c9de68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410265
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131545}

--- chrome/browser/task_manager/providers/web_contents/tab_contents_tag.cc
+++ chrome/browser/task_manager/providers/web_contents/tab_contents_tag.cc
@@ -18,7 +18,6 @@ TabContentsTag::TabContentsTag(content::WebContents* web_contents)
     : WebContentsTag(web_contents) {
 }
 
-TabContentsTag::~TabContentsTag() {
-}
+TabContentsTag::~TabContentsTag() = default;
 
 }  // namespace task_manager
--- chrome/browser/task_manager/web_contents_tags.h
+++ chrome/browser/task_manager/web_contents_tags.h
@@ -6,11 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_TASK_MANAGER_WEB_CONTENTS_TAGS_H_
 #define CHROME_BROWSER_TASK_MANAGER_WEB_CONTENTS_TAGS_H_
 
+#include "build/build_config.h"
 #include "extensions/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "extensions/common/mojom/view_type.mojom.h"
 #endif
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/web_applications/web_app_id.h"
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 class BackgroundContents;
 
@@ -32,7 +37,7 @@ class WebContentsTags {
   WebContentsTags(const WebContentsTags&) = delete;
   WebContentsTags& operator=(const WebContentsTags&) = delete;
 
-  // Tag a BackgroundContents so that it shows up in the task manager. Calling
+  // Tags a BackgroundContents so that it shows up in the task manager. Calling
   // this function creates a BackgroundContentsTag, and attaches it to
   // |web_contents|. If an instance is already attached, this does nothing. The
   // resulting tag does not have to be cleaned up by the caller, as it is owned
@@ -41,13 +46,14 @@ class WebContentsTags {
       content::WebContents* web_contents,
       BackgroundContents* background_contents);
 
-  // Tag a DevTools WebContents so that it shows up in the task manager. Calling
-  // this function creates a DevToolsTag, and attaches it to |web_contents|. If
-  // an instance is already attached, this does nothing. The resulting tag does
-  // not have to be cleaned up by the caller, as it is owned by |web_contents|.
+  // Tags a DevTools WebContents so that it shows up in the task manager.
+  // Calling this function creates a DevToolsTag, and attaches it to
+  // |web_contents|. If an instance is already attached, this does nothing. The
+  // resulting tag does not have to be cleaned up by the caller, as it is owned
+  // by |web_contents|.
   static void CreateForDevToolsContents(content::WebContents* web_contents);
 
-  // Tag a WebContents owned by the NoStatePrefetchManager so that it shows up
+  // Tags a WebContents owned by the NoStatePrefetchManager so that it shows up
   // in the task manager. Calling this function creates a PrerenderTag, and
   // attaches it to |web_contents|. If an instance is already attached, this
   // does nothing. The resulting tag does not have to be cleaned up by the
@@ -55,21 +61,21 @@ class WebContentsTags {
   static void CreateForNoStatePrefetchContents(
       content::WebContents* web_contents);
 
-  // Tag a WebContents owned by the TabStripModel so that it shows up in the
+  // Tags a WebContents owned by the TabStripModel so that it shows up in the
   // task manager. Calling this function creates a TabContentsTag, and attaches
   // it to |web_contents|. If an instance is already attached, this does
   // nothing. The resulting tag does not have to be cleaned up by the caller, as
   // it is owned by |web_contents|.
   static void CreateForTabContents(content::WebContents* web_contents);
 
-  // Tag a WebContents created for a print preview or background printing so
+  // Tags a WebContents created for a print preview or background printing so
   // that it shows up in the task manager. Calling this function creates a
   // PrintingTag, and attaches it to |web_contents|. If an instance is already
   // attached, this does nothing. The resulting tag does not have to be cleaned
   // up by the caller, as it is owned by |web_contents|.
   static void CreateForPrintingContents(content::WebContents* web_contents);
 
-  // Tag a WebContents owned by a GuestViewBase so that it shows up in the
+  // Tags a WebContents owned by a GuestViewBase so that it shows up in the
   // task manager. Calling this function creates a GuestTag, and attaches it to
   // |web_contents|. If an instance is already attached, this does nothing. The
   // resulting tag does not have to be cleaned up by the caller, as it is owned
@@ -77,7 +83,7 @@ class WebContentsTags {
   static void CreateForGuestContents(content::WebContents* web_contents);
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-  // Tag a WebContents that belongs to |extension| so that it shows up in the
+  // Tags a WebContents that belongs to |extension| so that it shows up in the
   // task manager. Calling this function creates a ExtensionTag, and attaches
   // it to |web_contents|. If an instance is already attached, this does
   // nothing. The resulting tag does not have to be cleaned up by the caller,
@@ -88,20 +94,32 @@ class WebContentsTags {
                                  extensions::mojom::ViewType view_type);
 #endif
 
-  // Tag a WebContents created for a Portal so that it shows up in the task
+  // Tags a WebContents created for a Portal so that it shows up in the task
   // manager. Calling this function creates a PortalTag, and attaches it to
   // |web_contents|. If an instance is already attached, this does nothing.
   // The resulting tag does not have to be cleaned up by the caller, as it is
   // owned by |web_contents|.
   static void CreateForPortal(content::WebContents* web_contents);
 
-  // Tag a WebContents created for a tool so that it shows up in the task
+  // Tags a WebContents created for a tool so that it shows up in the task
   // manager. Calling this function creates a ToolTag, and attaches it to
   // |web_contents|. If an instance is already attached, this does nothing. The
   // resulting tag does not have to be cleaned up by the caller, as it is owned
   // by |web_contents|. |tool_name| is the string ID of the name of the tool.
   static void CreateForToolContents(content::WebContents* web_contents,
                                     int tool_name);
+
+#if !BUILDFLAG(IS_ANDROID)
+  // Tags a WebContents created for a web app so that it shows up in the task
+  // manager. Calling this function creates a WebAppTag, and attaches it to
+  // |web_contents|. If an instance is already attached, this does nothing (but
+  // caller may choose to overwrite the existing tag). The resulting tag does
+  // not have to be cleaned up by the caller, as it is owned by |web_contents|.
+  // |app_id| is the string ID of the web app.
+  static void CreateForWebApp(content::WebContents* web_contents,
+                              const web_app::AppId& app_id,
+                              const bool is_isolated_web_app);
+#endif  // !BUILDFLAG(IS_ANDROID)
 
   // Clears the task-manager tag, created by any of the above functions, from
   // the given |web_contents| if any.
