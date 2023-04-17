7e28ab96f1456bdd11ca223afee1432e13b33b00
David Padlipsky <dpad@google.com>
2023-04-17 18:46:41 +0000

    accelerators: Only show 1 capslock accelerator
    Bug: b/266085562
    Change-Id: I72aef42c80a39149aae5c6b79f247dcaa31da07d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4421626
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131396}

--- ash/accelerators/accelerator_alias_converter.cc
+++ ash/accelerators/accelerator_alias_converter.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ash/keyboard_layout_util.h"
 #include "ui/events/devices/device_data_manager.h"
 #include "ui/events/devices/input_device.h"
+#include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
 
 namespace ash {
@@ -267,6 +268,13 @@ AcceleratorAliasConverter::FilterAliasBySupportedKeys(
               ->HasCalculatorKeyOnAnyKeyboard()) {
         filtered_accelerators.push_back(accelerator);
       }
+      continue;
+    }
+
+    // If the accelerator is pressing Search + Alt to do capslock, only Alt +
+    // Search should be shown in the shortcuts app.
+    if (accelerator.key_code() == ui::VKEY_MENU &&
+        accelerator.modifiers() == ui::EF_COMMAND_DOWN) {
       continue;
     }
 
