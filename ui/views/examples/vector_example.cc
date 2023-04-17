13dfd59a58bd56866193c9304033b8a2397f0bad
David Yeung <dayeung@chromium.org>
2023-04-17 17:18:06 +0000

    [views] Fixed tooltip and icons being cut off in vector_example.
    Tooltip was off by one and rows/cols were not based on size.
    
    Change-Id: I2abd7bfe91722448c884b4962ca977fcc73eadc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4428786
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1131313}

--- ui/views/examples/vector_example.cc
+++ ui/views/examples/vector_example.cc
@@ -188,7 +188,7 @@ class VectorIconGallery : public View, public TextfieldController {
   void GenerateAllIconInFolder(const base::FilePath& path) {
     image_view_container_->RemoveAllChildViews();
 
-    int nCols = 18;
+    int nCols = image_view_container_->width() / size_;
     int kColumnWidth = size_;
     views::TableLayout* layout = image_view_container_->SetLayoutManager(
         std::make_unique<views::TableLayout>());
@@ -199,7 +199,7 @@ class VectorIconGallery : public View, public TextfieldController {
                         kColumnWidth);
     }
 
-    int nRows = 10;
+    int nRows = image_view_container_->height() / size_;
     for (int i = 0; i < nRows; ++i) {
       layout->AddRows(1, TableLayout::kFixedSize);
     }
@@ -216,7 +216,6 @@ class VectorIconGallery : public View, public TextfieldController {
         count++;
         std::string file_content;
         base::ReadFileToString(file, &file_content);
-        file = file_iter.Next();
 
         // Skip over comments.
         // This handles very basic cases of // and /*. More complicated edge
@@ -238,6 +237,7 @@ class VectorIconGallery : public View, public TextfieldController {
         icon_view->SetImage(
             gfx::CreateVectorIconFromSource(file_content, size_, color_));
         icon_view->SetTooltipText(file.BaseName().AsUTF16Unsafe());
+        file = file_iter.Next();
       }
     }
     InvalidateLayout();
