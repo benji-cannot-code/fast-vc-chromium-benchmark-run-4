2a74c6c3f655ead5a47e2526ac7d1c312773a935
Kent Tamura <tkent@chromium.org>
2023-04-17 06:32:40 +0000

    css-multiple-display: Fix layout tree building
    New list-item types should attach ::marker.
    
    * IsListItemIncludingNG() should return true for LayoutNGInlineListItem
      PseudoElement::AttachLayoutTree() has a parent type check by
      IsListItemIncludingNG().
    
    * LayoutObject::CreateBlockFlowOrListItem() should create
      LayoutNGListItem for kFlowRootListItem and kInlineFlowRootListItem.
    
    * LayoutObject::CreateObject() for ::marker in kInlineListItem should
      create LayoutNGInsideListMarker.
    
    Bug: 995106
    Change-Id: I523c632004346b694028730565cef69c12e66ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431772
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131065}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -2795,7 +2795,7 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
   // TODO(1229581): Rename this function.
   bool IsListItemIncludingNG() const {
     NOT_DESTROYED();
-    return IsLayoutNGListItem();
+    return IsLayoutNGListItem() || IsInlineListItem();
   }
 
   // There 2 different types of list markers:
