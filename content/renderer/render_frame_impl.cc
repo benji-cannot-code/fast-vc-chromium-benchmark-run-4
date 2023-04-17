69c03ab71555b042c94ff0f5a718573e422ce382
Claudio DeSouza <cdesouza@igalia.com>
2023-04-17 14:21:31 +0000

    Replace base::GUID uses with base::Uuid (105/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /content/renderer
    
    This CL was uploaded by git cl split.
    
    R=danakj@chromium.org
    
    Bug: 1428566
    Change-Id: Id854dd7204a52886a953a97b855fd6ee8cee0290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430411
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1131206}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
