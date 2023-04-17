a46ff458574bd8149a8d36c115ca5893866cd4f1
Ahmed Nasr <anasr@google.com>
2023-04-17 19:43:42 +0000

    Use DeviceReportNetworkEvents policy to control network events.
    Bug: b:276461232
    Change-Id: I023811510f164b68880b01f6e83adbb76a7ada48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408390
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131439}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer_browsertest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer_browsertest.cc
@@ -118,12 +118,7 @@ class NetworkEventsBrowserTest : public ::policy::DevicePolicyCrosBrowserTest {
 
   void EnablePolicy() {
     scoped_testing_cros_settings_.device_settings()->SetBoolean(
-        ash::kReportDeviceNetworkStatus, true);
-  }
-
-  void DisablePolicy() {
-    scoped_testing_cros_settings_.device_settings()->SetBoolean(
-        ash::kReportDeviceNetworkStatus, false);
+        ash::kDeviceReportNetworkEvents, true);
   }
 
   void SetWifiSignalEventDrivenPolicy() {
