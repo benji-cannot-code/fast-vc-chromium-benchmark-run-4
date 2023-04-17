c601ca9d17cb8333d06f72d4b174c47b38d4b625
adamta <adamta@google.com>
2023-04-17 20:43:16 +0000

    [iOS] Rename tile ablation groups for M113
    Changes suffix for tile ablation groups so that they can be filtered in UMA after the release of M113. Leaves the popular site experiment names
    as they previously were to not affect the experiment.
    
    Bug: 1434083
    Change-Id: I7df913639fb558be114261302688e057d6478eda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4436129
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131483}

--- ios/chrome/browser/ui/ntp/new_tab_page_retention_field_trial_constants.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_retention_field_trial_constants.h
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace field_trial_constants {
 
 // Version suffix for group names.
-const char kNewTabPageRetentionVersionSuffix[] = "_20230308";
+const char kNewTabPageRetentionVersionSuffixImprovedTiles[] = "_20230308";
+const char kNewTabPageRetentionVersionSuffixTileAblation[] = "_20230504";
 
 // Variation IDs for the improved popular sites default suggestions experiment
 // arms.
