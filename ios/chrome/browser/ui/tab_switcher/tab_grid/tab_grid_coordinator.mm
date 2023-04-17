73b82d3a7d092a944371faa545175b05ea7a55d6
Louis Romero <lpromero@google.com>
2023-04-17 12:25:37 +0000

    Dismiss Inactive Tabs UI on initiating New Search
    This CL lets TabGridCoordinator tell Inactive Tabs when to dismiss
    itself when the app has been backgrounded and reopened for example via
    New Search on the app icon.
    
    Fixed: 1432597, 1432545, 1432541
    Change-Id: I399c2283c9efbe560f427bf4f8446e7408fd107f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4427980
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131168}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -327,6 +327,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self dismissPopovers];
 
+  [self.inactiveTabsCoordinator hide];
+
   if (_bookmarksCoordinator) {
     [_bookmarksCoordinator dismissBookmarkModalControllerAnimated:YES];
   }
