2a919e6dd3b0aa82fa09a269dacb10ffd8463e48
James Cook <jamescook@chromium.org>
2023-04-17 17:35:41 +0000

    chromeos: Rename RoundedMessageCenterScrollBar
    This name was used because there used to be two styles of scroll bar,
    based on the NotificationsRefresh flag. That flag no longer exists, so
    there is only one kind of scroll bar. Rename the class to just
    MessageCenterScrollBar.
    
    This also makes the filename match the class name.
    
    Bug: b:257291597
    Test: compiles
    Change-Id: I08c1f25bc796cff8d12783d4dbba1e944e995d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4425278
    Reviewed-by: Andre Le <leandre@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131334}

--- ash/system/message_center/message_center_scroll_bar.cc
+++ ash/system/message_center/message_center_scroll_bar.cc
@@ -43,18 +43,18 @@ void CollectScrollActionReason(ScrollActionReason reason) {
 
 namespace ash {
 
-BEGIN_METADATA(RoundedMessageCenterScrollBar, RoundedScrollBar)
+BEGIN_METADATA(MessageCenterScrollBar, RoundedScrollBar)
 END_METADATA
 
-RoundedMessageCenterScrollBar::RoundedMessageCenterScrollBar(Observer* observer)
+MessageCenterScrollBar::MessageCenterScrollBar(Observer* observer)
     : RoundedScrollBar(false), observer_(observer) {
   GetThumb()->layer()->SetVisible(features::IsNotificationScrollBarEnabled());
   GetThumb()->layer()->CompleteAllAnimations();
 }
 
-RoundedMessageCenterScrollBar::~RoundedMessageCenterScrollBar() = default;
+MessageCenterScrollBar::~MessageCenterScrollBar() = default;
 
-bool RoundedMessageCenterScrollBar::OnKeyPressed(const ui::KeyEvent& event) {
+bool MessageCenterScrollBar::OnKeyPressed(const ui::KeyEvent& event) {
   if (!stats_recorded_ &&
       (event.key_code() == ui::VKEY_UP || event.key_code() == ui::VKEY_DOWN)) {
     CollectScrollActionReason(ScrollActionReason::kByArrowKey);
@@ -63,8 +63,7 @@ bool RoundedMessageCenterScrollBar::OnKeyPressed(const ui::KeyEvent& event) {
   return RoundedScrollBar::OnKeyPressed(event);
 }
 
-bool RoundedMessageCenterScrollBar::OnMouseWheel(
-    const ui::MouseWheelEvent& event) {
+bool MessageCenterScrollBar::OnMouseWheel(const ui::MouseWheelEvent& event) {
   if (!stats_recorded_) {
     CollectScrollActionReason(ScrollActionReason::kByMouseWheel);
     stats_recorded_ = true;
@@ -78,7 +77,7 @@ bool RoundedMessageCenterScrollBar::OnMouseWheel(
   return result;
 }
 
-void RoundedMessageCenterScrollBar::OnGestureEvent(ui::GestureEvent* event) {
+void MessageCenterScrollBar::OnGestureEvent(ui::GestureEvent* event) {
   if (event->type() == ui::ET_GESTURE_SCROLL_BEGIN) {
     if (!presentation_time_recorder_ && GetWidget()) {
       presentation_time_recorder_ = CreatePresentationTimeHistogramRecorder(
@@ -105,7 +104,7 @@ void RoundedMessageCenterScrollBar::OnGestureEvent(ui::GestureEvent* event) {
     observer_->OnMessageCenterScrolled();
 }
 
-bool RoundedMessageCenterScrollBar::OnScroll(float dx, float dy) {
+bool MessageCenterScrollBar::OnScroll(float dx, float dy) {
   const bool result = RoundedScrollBar::OnScroll(dx, dy);
   if (observer_)
     observer_->OnMessageCenterScrolled();
