90f85767c94dc5aa3f922fa06ce8490f803c1bae
Claudio DeSouza <cdesouza@igalia.com>
2023-04-17 17:58:02 +0000

    Replace base::GUID uses with base::Uuid (108/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /services/device/geolocation
    
    This CL was uploaded by git cl split.
    
    R=mattreynolds@chromium.org
    
    Bug: 1428566
    Change-Id: Ib7e25b2d0be15d5357235ebc6e608928420dcf9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430397
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1131357}

--- services/device/geolocation/position_cache_test_util.cc
+++ services/device/geolocation/position_cache_test_util.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 
 #include "base/check_op.h"
-#include "base/guid.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/uuid.h"
 
 namespace device {
 namespace testing {
@@ -19,10 +19,12 @@ WifiData CreateUniqueWifiData(int number_of_access_points) {
   for (int i = 0; i < number_of_access_points; ++i) {
     AccessPointData single_access_point;
     single_access_point.channel = 2;
-    single_access_point.mac_address = base::ASCIIToUTF16(base::GenerateGUID());
+    single_access_point.mac_address =
+        base::ASCIIToUTF16(base::Uuid::GenerateRandomV4().AsLowercaseString());
     single_access_point.radio_signal_strength = 4;
     single_access_point.signal_to_noise = 5;
-    single_access_point.ssid = base::ASCIIToUTF16(base::GenerateGUID());
+    single_access_point.ssid =
+        base::ASCIIToUTF16(base::Uuid::GenerateRandomV4().AsLowercaseString());
     wifi_data.access_point_data.insert(single_access_point);
   }
   return wifi_data;
