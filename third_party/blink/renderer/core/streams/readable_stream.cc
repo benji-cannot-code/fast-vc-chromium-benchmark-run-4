217f347692e2fa59e11889973721f81fe657acac
Nidhi Jaju <nidhijaju@chromium.org>
2023-04-17 09:19:09 +0000

    Support cloneForBranch2 when Teeing default readable streams
    This CL implements support for the cloneForBranch2 flag in the Streams
    API standard. Previously, when teeing default readable streams, the
    data was not cloned for the second branch, and hence the same chunk
    object was used on both branches. However, this does not match the
    spec[1].
    
    This change updates this behavior to match the spec. This is also
    useful for scenarios where both branches are to be consumed in a way
    that they might interfere with each other i.e. transferring their
    chunks.
    
    [1] https://streams.spec.whatwg.org/#readable-stream-tee
    
    Bug: 1243329
    Change-Id: Ia4f1d22441aff0ca080ec04385aba566c2b80d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4422024
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131114}

--- third_party/blink/renderer/core/streams/readable_stream.cc
+++ third_party/blink/renderer/core/streams/readable_stream.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/event_loop.h"
 #include "third_party/blink/renderer/platform/wtf/deque.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
@@ -540,7 +541,8 @@ ScriptPromise ReadableStream::pipeTo(ScriptState* script_state,
 HeapVector<Member<ReadableStream>> ReadableStream::tee(
     ScriptState* script_state,
     ExceptionState& exception_state) {
-  return CallTeeAndReturnBranchArray(script_state, this, exception_state);
+  return CallTeeAndReturnBranchArray(script_state, this, false,
+                                     exception_state);
 }
 
 // Unlike in the standard, this is defined as a separate method from the
@@ -709,13 +711,17 @@ void ReadableStream::Initialize(ReadableStream* stream) {
   DCHECK(!stream->is_disturbed_);
 }
 
-// TODO(domenic): cloneForBranch2 argument from spec not supported yet
 void ReadableStream::Tee(ScriptState* script_state,
                          ReadableStream** branch1,
                          ReadableStream** branch2,
+                         bool clone_for_branch2,
                          ExceptionState& exception_state) {
   auto* engine = MakeGarbageCollected<TeeEngine>();
-  engine->Start(script_state, this, exception_state);
+  engine->Start(
+      script_state, this,
+      clone_for_branch2 &&
+          RuntimeEnabledFeatures::ReadableStreamTeeCloneForBranch2Enabled(),
+      exception_state);
   if (exception_state.HadException()) {
     return;
   }
@@ -1182,13 +1188,10 @@ bool ReadableStream::HasDefaultReader(const ReadableStream* stream) {
   return reader->IsDefaultReader();
 }
 
-//
-// TODO(ricea): Functions for transferable streams.
-//
-
 HeapVector<Member<ReadableStream>> ReadableStream::CallTeeAndReturnBranchArray(
     ScriptState* script_state,
     ReadableStream* readable,
+    bool clone_for_branch2,
     ExceptionState& exception_state) {
   // https://streams.spec.whatwg.org/#rs-tee
   ReadableStream* branch1 = nullptr;
@@ -1199,7 +1202,8 @@ HeapVector<Member<ReadableStream>> ReadableStream::CallTeeAndReturnBranchArray(
     readable->ByteStreamTee(script_state, &branch1, &branch2, exception_state);
   } else {
     DCHECK(readable->readable_stream_controller_->IsDefaultController());
-    readable->Tee(script_state, &branch1, &branch2, exception_state);
+    readable->Tee(script_state, &branch1, &branch2, clone_for_branch2,
+                  exception_state);
   }
 
   if (!branch1 || !branch2)
