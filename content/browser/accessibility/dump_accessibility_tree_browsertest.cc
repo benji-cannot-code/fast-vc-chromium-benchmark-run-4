2ecc0e81e9e701b2f18cd935c6ce55c290d4cb88
Kuan Huang <kuanhuang@chromium.org>
2023-04-17 16:52:27 +0000

    Disable flaky DumpAccessibilityTreeTest on all platform
    All/DumpAccessibilityTreeTest.AccessibilityPortalNameFromText/blink is
    found flaky on multiple CI sheriff builders and caused 68 build
    failures last week. i.e.
    https://ci.chromium.org/ui/b/8784071464096804385/test-results
    https://ci.chromium.org/ui/b/8784071029948752625/test-results
    https://ci.chromium.org/ui/b/8784041736092929441/test-results
    
    Bug: 1367886
    Change-Id: Iabc9d138fab3a698fd2ea23aa67f2d7e16ee178d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420609
    Auto-Submit: Kuan Huang <kuanhuang@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131292}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2840,8 +2840,9 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, DISABLED_AccessibilityPortal) 
   RunHtmlTest(FILE_PATH_LITERAL("portal.html"));
 }
 
+// TODO(crbug.com/1367886): Flaky on multiple platforms
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       AccessibilityPortalNameFromText) {
+                       DISABLED_AccessibilityPortalNameFromText) {
   RunHtmlTest(FILE_PATH_LITERAL("portal-name-from-text.html"));
 }
 
