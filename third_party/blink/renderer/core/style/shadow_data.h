d337c1671a47d98dce24bef34e0f39c19df9dc8b
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-04-17 19:19:52 +0000

    Add support for different X&Y blurs for dropShadow CanvasFilter.
    This aligns the CanvasFilter dropShadow implementation with SVG,
    which supports separable X & Y blurs:
    https://drafts.fxtf.org/filter-effects/#feDropShadowElement
    
    Note that SVG's `color-interpolation-filters` attribute defaults to
    `linearRBG`, whereas all other interpolations, as configured by
    `color-interpolation`, default to `sRGB`:
    https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/color-interpolation-filters
    
    It's unclear at this point which default CanvasFilter should use.
    In the mean time, we must manually set `color-interpolation-filters`
    to `sRGB` in the WPT test or else, the CanvasFilter and SVG filters
    won't produce the same results.
    
    Change-Id: I3ed1546a901fe3ab2752bc9feff04f9f67018390
    Bug: 1428652
    Fixed: 1430524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415135
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131422}

--- third_party/blink/renderer/core/style/shadow_data.h
+++ third_party/blink/renderer/core/style/shadow_data.h
@@ -49,6 +49,19 @@ class CORE_EXPORT ShadowData {
              StyleColor color,
              float opacity = 1.0f)
       : location_(location),
+        blur_(blur, blur),
+        spread_(spread),
+        color_(color),
+        style_(style),
+        opacity_(opacity) {}
+
+  ShadowData(gfx::PointF location,
+             gfx::PointF blur,
+             float spread,
+             ShadowStyle style,
+             StyleColor color,
+             float opacity = 1.0f)
+      : location_(location),
         blur_(blur),
         spread_(spread),
         color_(color),
@@ -63,7 +76,8 @@ class CORE_EXPORT ShadowData {
   float X() const { return location_.x(); }
   float Y() const { return location_.y(); }
   gfx::PointF Location() const { return location_; }
-  float Blur() const { return blur_; }
+  float Blur() const { return blur_.x(); }
+  gfx::PointF BlurXY() const { return blur_; }
   float Spread() const { return spread_; }
   ShadowStyle Style() const { return style_; }
   StyleColor GetColor() const { return color_; }
@@ -77,7 +91,7 @@ class CORE_EXPORT ShadowData {
 
  private:
   gfx::PointF location_;
-  float blur_;
+  gfx::PointF blur_;
   float spread_;
   StyleColor color_;
   ShadowStyle style_;
