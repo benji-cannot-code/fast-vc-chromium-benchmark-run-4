67ec32463a787e34f36b437aff00acfd100ccc34
Hidehiko Abe <hidehiko@chromium.org>
2023-04-17 23:30:11 +0000

    Fix GrammarFragment offsets.
    The current code sends GrammarFragment and SetSurroundingText
    independently, so offsets are not handled properly, specifically
    if surrounding text is huge as it is trimmed.
    
    This CL merges LinuxInputMethodContext methods and so fixes
    of the offset handling.
    
    BUG=1402906
    TEST=Ran tryjob
    
    Change-Id: I20e809ee32813f3b527195ae18d1b066d0b72b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4419306
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131563}

--- ui/base/ime/linux/linux_input_method_context.h
+++ ui/base/ime/linux/linux_input_method_context.h
@@ -12,10 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/component_export.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/ime/grammar_fragment.h"
 #include "ui/base/ime/text_input_client.h"
 #include "ui/base/ime/text_input_mode.h"
 #include "ui/base/ime/text_input_type.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/range/range.h"
 
 namespace gfx {
 class Rect;
@@ -33,6 +36,11 @@ class VirtualKeyboardController;
 // GNU/Linux and likes.
 class COMPONENT_EXPORT(UI_BASE_IME_LINUX) LinuxInputMethodContext {
  public:
+  struct AutocorrectInfo {
+    gfx::Range range;
+    gfx::Rect bounds;
+  };
+
   virtual ~LinuxInputMethodContext() = default;
 
   // Dispatches the key event to an underlying IME.  Returns true if the key
@@ -47,9 +55,12 @@ class COMPONENT_EXPORT(UI_BASE_IME_LINUX) LinuxInputMethodContext {
   virtual void SetCursorLocation(const gfx::Rect& rect) = 0;
 
   // Tells the system IME the surrounding text around the cursor location.
-  virtual void SetSurroundingText(const std::u16string& text,
-                                  const gfx::Range& text_range,
-                                  const gfx::Range& selection_range) = 0;
+  virtual void SetSurroundingText(
+      const std::u16string& text,
+      const gfx::Range& text_range,
+      const gfx::Range& selection_range,
+      const absl::optional<ui::GrammarFragment>& fragment,
+      const absl::optional<AutocorrectInfo>& autocorrect) = 0;
 
   // Tells the system IME the content type of the text input client is changed.
   virtual void SetContentType(TextInputType type,
@@ -57,15 +68,6 @@ class COMPONENT_EXPORT(UI_BASE_IME_LINUX) LinuxInputMethodContext {
                               uint32_t flags,
                               bool should_do_learning,
                               bool can_compose_inline) = 0;
-
-  // Sets grammar fragment at the cursor position. If not exists, sends a
-  // fragment with empty range.
-  virtual void SetGrammarFragmentAtCursor(
-      const ui::GrammarFragment& fragment) = 0;
-
-  // Tells Ash about the current autocorrect information.
-  virtual void SetAutocorrectInfo(const gfx::Range& autocorrect_range,
-                                  const gfx::Rect& autocorrect_bounds) = 0;
 
   // Resets the context.  A client needs to call OnTextInputTypeChanged() again
   // before calling DispatchKeyEvent().
