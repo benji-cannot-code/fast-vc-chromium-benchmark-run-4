8534d7c4be3744b27a92595f81e3caf04e6462f2
Rakina Zata Amni <rakina@chromium.org>
2023-04-17 05:09:03 +0000

    Revert "[iOS] Consolidate user session time"
    This reverts commit bcef66b8273a7735992ab93c04ec337dd9a49269.
    
    Reason for revert: Potentially causing check_static_initializers failures (see crbug.com/1433376) 
    
    Original change's description:
    > [iOS] Consolidate user session time
    >
    > Use the NTP's defined user session time to have a single definition of
    > user session in case it changes in the future.
    >
    > Bug: 1433042
    > Change-Id: I96bc28bfa3d0897c7d93bd4ab90185bede29ea68
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4424789
    > Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1130560}
    
    Bug: 1433042
    Change-Id: I4df8b125aaad95ede9e7b12de1965f8c8a8e70f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431572
    Owners-Override: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1131048}

--- ios/chrome/browser/default_browser/utils.mm
+++ ios/chrome/browser/default_browser/utils.mm
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/driver/sync_service.h"
 #import "ios/chrome/browser/application_context/application_context.h"
 #import "ios/chrome/browser/feature_engagement/tracker_factory.h"
-#import "ios/chrome/browser/ntp/features.h"
 #import "ios/chrome/browser/settings/sync/utils/identity_error_util.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 
@@ -134,8 +133,7 @@ constexpr base::TimeDelta kMaximumTimeBetweenFirstPartyAppLaunches =
     base::Days(7);
 
 // Maximum time representing one user session.
-const base::TimeDelta kMaximumTimeOneUserSession =
-    base::Seconds(GetFeedUnseenRefreshThresholdInSeconds());
+constexpr base::TimeDelta kMaximumTimeOneUserSession = base::Hours(6);
 
 // Maximum time range between valid user URL pastes to notify the FET.
 constexpr base::TimeDelta kMaximumTimeBetweenValidURLPastes = base::Days(7);
