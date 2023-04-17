51be2b2bc57d29f1fa355cec26e3a9ab24d1bf36
Alexander Alekseev <alemate@chromium.org>
2023-04-17 23:14:43 +0000

    ARC: Fix ArcAppInitialAppsInstallDuration
    Ash.ArcAppInitialAppsInstallDuration was only emitted on apps list refresh.
    But it was not chedked on apps removal which led to some tests waiting forever.
    
    This CL fixes this issue and now it could be also emitted on app removal.
    
    Bug: b/278611452
    Change-Id: I35288f25ac442a8f0d80cd187751eeec486e422d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437651
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131555}

--- chrome/browser/ash/app_list/arc/arc_app_list_prefs.cc
+++ chrome/browser/ash/app_list/arc/arc_app_list_prefs.cc
@@ -1827,6 +1827,7 @@ void ArcAppListPrefs::AddOrUpdatePackagePrefs(
 
 void ArcAppListPrefs::RemovePackageFromPrefs(const std::string& package_name) {
   ScopedDictPrefUpdate(prefs_, arc::prefs::kArcPackages)->Remove(package_name);
+  OnArcAppListRefreshed(profile_);
 }
 
 void ArcAppListPrefs::OnAppListRefreshed(
