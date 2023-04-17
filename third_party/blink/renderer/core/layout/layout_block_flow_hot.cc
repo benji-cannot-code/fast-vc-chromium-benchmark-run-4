7fa00877ab74c183d8ca15280994b3dce7dacb89
Kent Tamura <tkent@chromium.org>
2023-04-17 07:36:51 +0000

    css-multiple-display: Support 'flow-root list-item' behavior
    It should create new formatting context.
    
    Bug: 995106
    Change-Id: Icc679e685b4780134990cd0dd853b81338ed3eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431777
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131080}

--- third_party/blink/renderer/core/layout/layout_block_flow_hot.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow_hot.cc
@@ -64,6 +64,7 @@ bool LayoutBlockFlow::CreatesNewFormattingContext() const {
       IsFlexItemIncludingNG() || IsCustomItem() || IsDocumentElement() ||
       IsGridItemIncludingNG() || IsWritingModeRoot() || IsMathItem() ||
       StyleRef().Display() == EDisplay::kFlowRoot ||
+      StyleRef().Display() == EDisplay::kFlowRootListItem ||
       ShouldApplyPaintContainment() || ShouldApplyLayoutContainment() ||
       StyleRef().IsDeprecatedWebkitBoxWithVerticalLineClamp() ||
       StyleRef().SpecifiesColumns() ||
