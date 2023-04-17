301f6df0b519a7f9356023b3a97116d714a3b3e7
Tim Judkins <tjudkins@chromium.org>
2023-04-17 20:28:49 +0000

    [Extensions] Add component extensions to DevTools blocking policy
    This CL adjusts the logic behind the DeveloperToolsAvailability Chrome
    policy that disallows DevTools for force installed extensions to also
    include component extensions which use the management permission. See:
    https://chromeenterprise.google/policies/?policy=DeveloperToolsAvailability
    and the associated bug for further details.
    
    Also adds a test case for this.
    
    Bug: b/274599835
    Change-Id: I46fe6366c1a136845c15b95cb0ffd0cf20f2c092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412168
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131471}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.cc
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/view_type_utils.h"
 #include "extensions/common/manifest.h"
 #include "extensions/common/mojom/view_type.mojom.h"
+#include "extensions/common/permissions/permissions_data.h"
 #include "ui/base/resource/resource_bundle.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -248,8 +249,18 @@ bool ChromeDevToolsManagerDelegate::AllowInspection(
     case Availability::kAllowed:
       return true;
     case Availability::kDisallowedForForceInstalledExtensions:
-      return !extension ||
-             !extensions::Manifest::IsPolicyLocation(extension->location());
+      if (!extension) {
+        return true;
+      }
+      if (extensions::Manifest::IsPolicyLocation(extension->location())) {
+        return false;
+      }
+      if (extensions::Manifest::IsComponentLocation(extension->location()) &&
+          extension->permissions_data()->HasAPIPermission(
+              extensions::mojom::APIPermissionID::kManagement)) {
+        return false;
+      }
+      return true;
     default:
       NOTREACHED() << "Unknown developer tools policy";
       return true;
