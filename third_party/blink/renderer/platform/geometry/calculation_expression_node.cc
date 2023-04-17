f547372d02cd87cf5590fa772b852bc7d90f0e05
Daniil Sakhapov <sakhapov@chromium.org>
2023-04-17 12:26:00 +0000

    Implement CSS stepped math functions
    round(), mod(), rem() as per
    https://drafts.csswg.org/css-values/#round-func
    
    Bug: 1407473
    Change-Id: Ibfd9c3ae423350896ef86512b3e9be982c173272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350350
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131169}

--- third_party/blink/renderer/platform/geometry/calculation_expression_node.cc
+++ third_party/blink/renderer/platform/geometry/calculation_expression_node.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
 #include "third_party/blink/renderer/platform/geometry/length_functions.h"
+#include "third_party/blink/renderer/platform/geometry/math_functions.h"
 
 namespace blink {
 
@@ -178,6 +179,28 @@ CalculationExpressionOperationNode::CreateSimplified(Children&& children,
       return base::MakeRefCounted<CalculationExpressionOperationNode>(
           std::move(children), op);
     }
+    case CalculationOperator::kRoundNearest:
+    case CalculationOperator::kRoundUp:
+    case CalculationOperator::kRoundDown:
+    case CalculationOperator::kRoundToZero:
+    case CalculationOperator::kMod:
+    case CalculationOperator::kRem: {
+      DCHECK_EQ(children.size(), 2u);
+      const auto* a =
+          DynamicTo<CalculationExpressionPixelsAndPercentNode>(*children[0]);
+      const auto* b =
+          DynamicTo<CalculationExpressionPixelsAndPercentNode>(*children[1]);
+      bool can_simplify = a && !a->Percent() && b && !b->Percent();
+      if (can_simplify) {
+        float value =
+            EvaluateSteppedValueFunction(op, a->Pixels(), b->Pixels());
+        return base::MakeRefCounted<CalculationExpressionPixelsAndPercentNode>(
+            PixelsAndPercent(value, 0));
+      } else {
+        return base::MakeRefCounted<CalculationExpressionOperationNode>(
+            std::move(children), op);
+      }
+    }
     case CalculationOperator::kInvalid:
       NOTREACHED();
       return nullptr;
@@ -251,6 +274,17 @@ float CalculationExpressionOperationNode::Evaluate(
       // clamp(MIN, VAL, MAX) is identical to max(MIN, min(VAL, MAX))
       return std::max(min, std::min(val, max));
     }
+    case CalculationOperator::kRoundNearest:
+    case CalculationOperator::kRoundUp:
+    case CalculationOperator::kRoundDown:
+    case CalculationOperator::kRoundToZero:
+    case CalculationOperator::kMod:
+    case CalculationOperator::kRem: {
+      DCHECK_EQ(children_.size(), 2u);
+      float a = children_[0]->Evaluate(max_value, anchor_evaluator);
+      float b = children_[1]->Evaluate(max_value, anchor_evaluator);
+      return EvaluateSteppedValueFunction(operator_, a, b);
+    }
     case CalculationOperator::kInvalid:
       break;
       // TODO(crbug.com/1284199): Support other math functions.
@@ -291,7 +325,13 @@ CalculationExpressionOperationNode::Zoom(double factor) const {
     }
     case CalculationOperator::kMin:
     case CalculationOperator::kMax:
-    case CalculationOperator::kClamp: {
+    case CalculationOperator::kClamp:
+    case CalculationOperator::kRoundNearest:
+    case CalculationOperator::kRoundUp:
+    case CalculationOperator::kRoundDown:
+    case CalculationOperator::kRoundToZero:
+    case CalculationOperator::kMod:
+    case CalculationOperator::kRem: {
       DCHECK(children_.size());
       Vector<scoped_refptr<const CalculationExpressionNode>> cloned_operands;
       cloned_operands.reserve(children_.size());
@@ -340,7 +380,13 @@ CalculationExpressionOperationNode::ResolvedResultType() const {
     }
     case CalculationOperator::kMin:
     case CalculationOperator::kMax:
-    case CalculationOperator::kClamp: {
+    case CalculationOperator::kClamp:
+    case CalculationOperator::kRoundNearest:
+    case CalculationOperator::kRoundUp:
+    case CalculationOperator::kRoundDown:
+    case CalculationOperator::kRoundToZero:
+    case CalculationOperator::kMod:
+    case CalculationOperator::kRem: {
       DCHECK(children_.size());
       auto first_child_type = children_.front()->ResolvedResultType();
       for (const auto& child : children_) {
--- third_party/blink/renderer/platform/geometry/calculation_expression_node.h
+++ third_party/blink/renderer/platform/geometry/calculation_expression_node.h
@@ -23,6 +23,12 @@ enum class CalculationOperator {
   kMin,
   kMax,
   kClamp,
+  kRoundNearest,
+  kRoundUp,
+  kRoundDown,
+  kRoundToZero,
+  kMod,
+  kRem,
   kInvalid
 };
 
