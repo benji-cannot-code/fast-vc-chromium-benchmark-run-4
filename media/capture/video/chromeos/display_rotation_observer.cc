85d2576e7b79d7507b289896a4371d2aa793f610
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-04-17 07:39:45 +0000

    ChromeOS rewrite: Preliminary fixes [2/N]
    This is a preliminary patch for the MiraclePtr rewrite for ChromeOS:
    https://chromium-review.googlesource.com/c/chromium/src/+/4406541
    
    This patches improves/tweak code to avoid the rewriter to cause compile
    errors. Most of them are about:using nullptr instead of NULL.
    
    Bug: 1430482
    Change-Id: I175d51346e2ec679a0e8bd1bcdd4d9d92d65d97a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4427837
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131083}

--- media/capture/video/chromeos/display_rotation_observer.cc
+++ media/capture/video/chromeos/display_rotation_observer.cc
@@ -35,7 +35,7 @@ ScreenObserverDelegate::ScreenObserverDelegate(
 
 void ScreenObserverDelegate::RemoveObserver() {
   DCHECK(delegate_task_runner_->BelongsToCurrentThread());
-  observer_ = NULL;
+  observer_ = nullptr;
   display_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&ScreenObserverDelegate::RemoveObserverOnDisplayThread,
