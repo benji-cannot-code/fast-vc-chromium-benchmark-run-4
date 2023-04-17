759ff0781f07e4c6dde1c5ef86eee8dd749b145a
Sunny Sachanandani <sunnyps@chromium.org>
2023-04-17 22:44:10 +0000

    Revert "gpu: DumpWithoutCrashing for debugging D3D overlay crashes"
    This reverts commit 43c3a3f4cff8699e548e1acd1a96371457fd7002.
    
    Reason for revert: Got enough crash reports to debug original issue
    
    Original change's description:
    > gpu: DumpWithoutCrashing for debugging D3D overlay crashes
    >
    > Will remove these once we know the failing code path.
    >
    > Bug: 1430941
    > Change-Id: I869a955a0d4606615682594da98f54575b0e8254
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420184
    > Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1130005}
    
    Bug: 1430941, 1433393, 1433445
    Change-Id: Iaca9de313f79c72229d404632868e0f524f9152e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437269
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1131532}

--- gpu/command_buffer/service/shared_image/d3d_image_backing.cc
+++ gpu/command_buffer/service/shared_image/d3d_image_backing.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <d3d11_3.h>
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ptr_util.h"
@@ -967,11 +966,8 @@ void D3DImageBacking::EndAccessDawn(WGPUDevice device, WGPUTexture texture) {
 #endif
 
 bool D3DImageBacking::BeginAccessD3D11(bool write_access) {
-  if (!ValidateBeginAccess(write_access)) {
-    // TODO(crbug.com/1430941): Remove after fixing overlay access crash.
-    base::debug::DumpWithoutCrashing();
+  if (!ValidateBeginAccess(write_access))
     return false;
-  }
 
   // If read fences or write fence are present, shared handle should be too.
   DCHECK((read_fences_.empty() && !write_fence_) || dxgi_shared_handle_state_);
@@ -981,8 +977,6 @@ bool D3DImageBacking::BeginAccessD3D11(bool write_access) {
   // no dependency between concurrent reads and instead wait for the last write.
   if (write_fence_ && !write_fence_->WaitD3D11(d3d11_device_)) {
     DLOG(ERROR) << "Failed to wait for write fence";
-    // TODO(crbug.com/1430941): Remove after fixing overlay access crash.
-    base::debug::DumpWithoutCrashing();
     return false;
   }
   if (write_access) {
@@ -990,8 +984,6 @@ bool D3DImageBacking::BeginAccessD3D11(bool write_access) {
     for (const auto& fence : read_fences_) {
       if (!fence->WaitD3D11(d3d11_device_)) {
         DLOG(ERROR) << "Failed to wait for read fence";
-        // TODO(crbug.com/1430941): Remove after fixing overlay access crash.
-        base::debug::DumpWithoutCrashing();
         return false;
       }
     }
