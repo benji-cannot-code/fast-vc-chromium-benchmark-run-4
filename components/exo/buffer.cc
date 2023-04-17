6362a93e5ec1b8d2ff9bc43b656db53476579e52
Colin Blundell <blundell@chromium.org>
2023-04-17 14:32:28 +0000

    [ChromeOS] Remove ResourceFormat usage from //components/exo
    Part of the ongoing conversion of the codebase away from using
    ResourceFormat.
    
    Bug: 1378708
    Change-Id: I31142bdee08f86bb619d5b9fd7fb489e9b2a7329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4432434
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131213}

--- components/exo/buffer.cc
+++ components/exo/buffer.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/frame_sink_resource_manager.h"
 #include "components/viz/common/gpu/context_lost_observer.h"
 #include "components/viz/common/gpu/context_provider.h"
-#include "components/viz/common/resources/resource_format.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "components/viz/common/resources/resource_id.h"
 #include "components/viz/common/resources/returned_resource.h"
@@ -472,7 +471,7 @@ bool Buffer::ProduceTransferableResource(
   }
 
   resource->id = resource_manager->AllocateResourceId();
-  resource->format = viz::SharedImageFormat::SinglePlane(viz::RGBA_8888);
+  resource->format = viz::SinglePlaneFormat::kRGBA_8888;
   resource->filter = GL_LINEAR;
   resource->size = gpu_memory_buffer_->GetSize();
 
