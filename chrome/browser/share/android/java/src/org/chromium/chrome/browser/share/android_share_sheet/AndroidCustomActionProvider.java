8364cf5d9a12cc198aeede74f2bdea3ec412fd97
Wenyu Fu <wenyufu@google.com>
2023-04-17 16:32:25 +0000

    [Sharesheet] Bypass long screenshot preview
    Remove the long screenshot preview and directly share it using
    ChromeOptionShareCallback.
    
    In trade, Android share sheet will add the save image action when sharing the screenshot to compensate the fact "Save" was the most used action on the long screenshot preview. Besides, the page URL will be used.
    
    Post change demo:
    https://drive.google.com/file/d/1ki5XQqqMFDAoWP9Vikrh12SkUVNR5L5r/view?usp=share_link&resourcekey=0-1KfF_kG-EQGYrlHD9Up52g
    
    Bug: 1433326, 1410201
    Change-Id: I27651dc16a312e4cc424a2f36a3f47b1a1d22fee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4427686
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131274}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/android_share_sheet/AndroidCustomActionProvider.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/android_share_sheet/AndroidCustomActionProvider.java
@@ -105,9 +105,15 @@ class AndroidCustomActionProvider extends ChromeProvidedSharingOptionsProviderBa
             return;
         }
 
-        List<FirstPartyOption> options = getFirstPartyOptions(
-                ShareContentTypeHelper.getContentTypes(params, chromeShareExtras),
-                chromeShareExtras.getDetailedContentType(), isMultiWindow);
+        List<FirstPartyOption> options;
+        if (chromeShareExtras.getDetailedContentType() == DetailedContentType.SCREENSHOT) {
+            options = new ArrayList<>();
+            options.add(createSaveImageFirstPartyOption());
+        } else {
+            options = getFirstPartyOptions(
+                    ShareContentTypeHelper.getContentTypes(params, chromeShareExtras),
+                    chromeShareExtras.getDetailedContentType(), isMultiWindow);
+        }
         assert options.size() <= MAX_ACTION_SUPPORTED;
         for (var option : options) {
             mCustomActions.add(shareActionFromFirstPartyOption(option));
@@ -153,9 +159,12 @@ class AndroidCustomActionProvider extends ChromeProvidedSharingOptionsProviderBa
                 .setDisableForMultiWindow(true)
                 .setOnClickCallback((view) -> {
                     mFeatureEngagementTracker.notifyEvent(EventConstants.SHARE_SCREENSHOT_SELECTED);
-                    LongScreenshotsCoordinator coordinator =
-                            LongScreenshotsCoordinator.create(mActivity, mTabProvider.get(), mUrl,
-                                    mChromeOptionShareCallback, mBottomSheetController, null);
+                    // Share the page URL for long screenshot.
+                    String pageUrl =
+                            mTabProvider.get() == null ? "" : mTabProvider.get().getUrl().getSpec();
+                    LongScreenshotsCoordinator coordinator = LongScreenshotsCoordinator.create(
+                            mActivity, mTabProvider.get(), pageUrl, mChromeOptionShareCallback,
+                            mBottomSheetController, null, /*bypassPreviewDialog*/ true);
                     coordinator.captureScreenshot();
                 })
                 .build();
