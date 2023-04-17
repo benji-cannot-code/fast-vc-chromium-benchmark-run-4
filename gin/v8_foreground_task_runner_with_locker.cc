6a5448f41f6b34be2bb2d8510b85d71776419189
Andreas Haas <ahaas@chromium.org>
2023-04-17 15:35:15 +0000

    [gin] Open Isolate::Scope in V8ForegroundTaskRunnerWithLocker
    Before accessing the V8 isolate, the isolate has to be "entered". This
    can be done conveniently by opening an `Isolate::Scope`. In [1] it is
    mentioned that an `Isolate::Scope` should be entered after a V8::Locker
    is entered, as this is a point where the isolate could change and a new
    isolate has to be entered. This CL adds additional `Isolate::Scope`s
    where they were missing before.
    
    
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:v8/include/v8-locker.h;l=37;drc=65034fdf545d2f937dd05f1aa535c39ba21ccac5
    
    R=mlippautz@chromium.org
    
    Change-Id: Ice30a9b9a3fc6482ddc218df70c45c51f8a3c0b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4433429
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131249}

--- gin/v8_foreground_task_runner_with_locker.cc
+++ gin/v8_foreground_task_runner_with_locker.cc
@@ -28,6 +28,7 @@ namespace {
 
 void RunWithLocker(v8::Isolate* isolate, std::unique_ptr<v8::Task> task) {
   v8::Locker lock(isolate);
+  v8::Isolate::Scope isolate_scope(isolate);
   task->Run();
 }
 
@@ -42,6 +43,7 @@ class IdleTaskWithLocker : public v8::IdleTask {
   // v8::IdleTask implementation.
   void Run(double deadline_in_seconds) override {
     v8::Locker lock(isolate_);
+    v8::Isolate::Scope isolate_scope(isolate_);
     task_->Run(deadline_in_seconds);
   }
 
