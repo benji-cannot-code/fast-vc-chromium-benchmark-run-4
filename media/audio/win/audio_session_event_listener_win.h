e72195e7b77b251665adbdcf742a99576d03aa95
Steve Becker <stevebe@microsoft.com>
2023-04-17 18:51:40 +0000

    Fix AudioSessionEventListener Crashes
    Updates AudioSessionEventListener to implement IUnknown to prevent the
    Windows OS Audio service from dispatching an event to a destructed
    AudioSessionEventListener.  From the bug:
    
    "AudioSessionEventListener inherits a COM event listener interface, but
    it does not implement IUnknown correctly.  The code comment in
    AudioSessionEventListener::AddRef() says: "Class is owned in Chromium
    code and should have no outside refs.".  However, this is not accurate.
    Calling IAudioSessionControl::RegisterAudioSessionNotification() with
    AudioSessionEventListener gives an external reference to the Windows OS
    Audio service.  Before dispatching an event on a worker thread, the
    Windows OS Audio service adds a reference to AudioSessionEventListener.
    The added reference ensures AudioSessionEventListener remains alive
    during the event handler callback.
    
    The crash happens when AudioSessionEventListener destructs on the main
    thread while pending audio session events exist.  On another worker
    thread, the pending audio session event will dispatch to the
    destructed AudioSessionEventListener, which then crashes."
    
    The fix updates AudioSessionEventListener to inherit
    Microsoft::WRL::RuntimeClass, which provides a correct implementation of
    IUnknown with a multi-threaded reference count.  This enables both the
    main audio thread and all Windows OS Audio service event callback
    threads to hold a reference to AudioSessionEventListener.
    
    Since AudioSessionEventListener is now a threadsafe object accessed by
    multiple threads, the change updates WASAPIAudioOutputStream to
    manage Windows audio session events instead of the
    AudioSessionEventListener.  This way, WASAPIAudioOutputStream can ensure
    that subscribing, unsubscribing and event callbacks all occur on the
    same thread, which is the `creating_thread_id_` WASAPIAudioOutputStream
    member.  WASAPIAudioOutputStream can also break circular references
    between IAudioSessionControl and AudioSessionEventListener by
    unsubscribing from events when closing/destructing.
    
    Bug: 1433425
    Change-Id: I40cf450549a5676de05c3a0fb294321cf9eb34ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4428613
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131400}

--- media/audio/win/audio_session_event_listener_win.h
+++ media/audio/win/audio_session_event_listener_win.h
@@ -8,29 +8,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <audiopolicy.h>
 #include <wrl/client.h>
+#include <wrl/implements.h>
 
 #include "base/functional/callback.h"
 #include "media/base/media_export.h"
 
 namespace media {
 
-class MEDIA_EXPORT AudioSessionEventListener : public IAudioSessionEvents {
+class MEDIA_EXPORT AudioSessionEventListener
+    : public Microsoft::WRL::RuntimeClass<
+          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+          IAudioSessionEvents> {
  public:
-  // Calls RegisterAudioSessionNotification() on |client| and calls
+  // Handles event callbacks from
+  // IAudioSessionControl::RegisterAudioSessionNotification().  Runs
   // |device_change_cb| when OnSessionDisconnected() is called.
   //
   // Since the IAudioClient session is dead after the disconnection, we use a
   // OnceCallback. The delivery of this notification is fatal to the |client|.
-  AudioSessionEventListener(IAudioClient* client,
-                            base::OnceClosure device_change_cb);
-  virtual ~AudioSessionEventListener();
+  AudioSessionEventListener(base::OnceClosure device_change_cb);
+  ~AudioSessionEventListener() override;
 
  private:
   friend class AudioSessionEventListenerTest;
-
-  IFACEMETHODIMP_(ULONG) AddRef() override;
-  IFACEMETHODIMP_(ULONG) Release() override;
-  IFACEMETHODIMP QueryInterface(REFIID iid, void** object) override;
 
   // IAudioSessionEvents implementation.
   IFACEMETHODIMP OnChannelVolumeChanged(DWORD channel_count,
@@ -51,7 +51,6 @@ class MEDIA_EXPORT AudioSessionEventListener : public IAudioSessionEvents {
   IFACEMETHODIMP OnStateChanged(AudioSessionState new_state) override;
 
   base::OnceClosure device_change_cb_;
-  Microsoft::WRL::ComPtr<IAudioSessionControl> audio_session_control_;
 };
 
 }  // namespace media
