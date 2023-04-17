9c29b037869469cbd7f7a7b8c01625c3e337096f
Salma Elmahallawy <esalma@google.com>
2023-04-17 22:48:53 +0000

    [PolicyLogs] Enabling Android Policy Logs By Default
    This CL enables the PolicyLogsPageAndroid feature by default to fully
    launch chrome://policy/logs to M112 stable.
    
    Bug: b/251799119
    Change-Id: I3a78219a29fcf62b997f4236832888493cbd9bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4433410
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131538}

--- components/policy/core/browser/configuration_policy_handler_list_unittest.cc
+++ components/policy/core/browser/configuration_policy_handler_list_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/functional/bind.h"
+#include "base/test/task_environment.h"
 #include "base/values.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
 #include "components/policy/core/browser/policy_conversions_client.h"
@@ -183,6 +184,7 @@ TEST_F(ConfigurationPolicyHandlerListTest,
 // Device platform policy will be fitered out.
 TEST_F(ConfigurationPolicyHandlerListTest,
        ApplySettingsWithPlatformDevicePolicy) {
+  base::test::SingleThreadTaskEnvironment task_environment;
   AddSimplePolicy();
   details()->is_device_policy = true;
 
