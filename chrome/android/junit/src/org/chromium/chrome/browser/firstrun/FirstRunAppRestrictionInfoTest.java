9238b238179d67be234f29a01accadf9a646d465
Michael Thiessen <mthiesse@chromium.org>
2023-04-18 01:01:51 +0000

    Fix FirstRunAppRestrictionInfoTest#testQueuedCallback
    I have no idea how this test passed before this, but it stopped working
    with https://chromium-review.googlesource.com/c/chromium/src/+/4355453
    
    Bug: 1424788
    Change-Id: I8996f13e8cdc9d5ba3ec3465beacb619a827c763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437489
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131612}

--- chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunAppRestrictionInfoTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunAppRestrictionInfoTest.java
@@ -158,6 +158,7 @@ public class FirstRunAppRestrictionInfoTest {
         Assert.assertEquals("CallbackHelper should not triggered yet.", 0,
                 completionCallbackHelper3.getCallCount());
 
+        mPauseDuringPostTask = false;
         // Initialized the AppRestrictionInfo and wait until initialized.
         TestThreadUtils.runOnUiThreadBlocking(() -> mPendingPostTask.run());
 
