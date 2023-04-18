bed296fd17a8368c086a8dfdefa28591813fdd0e
Ritika Gupta <ritikagup@google.com>
2023-04-18 11:48:27 +0000

    Adding client side config and model for tablet productivity.
    Added inputs and logic for segmenting users into various tablet
    productivity groups. Users are divided into various groups based on
    device-tier and other UMA metrics criteria.
    
    Change-Id: I49512892068a76134f973a5ba08dc40706f144e1
    Bug: 1424539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4337240
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131827}

--- chrome/browser/segmentation_platform/segmentation_platform_config.cc
+++ chrome/browser/segmentation_platform/segmentation_platform_config.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/embedder/default_model/resume_heavy_user_model.h"
 #include "components/segmentation_platform/embedder/default_model/search_user_model.h"
 #include "components/segmentation_platform/embedder/default_model/shopping_user_model.h"
+#include "components/segmentation_platform/embedder/default_model/tablet_productivity_user_model.h"
 #include "components/segmentation_platform/internal/config_parser.h"
 #include "components/segmentation_platform/public/config.h"
 #include "components/segmentation_platform/public/constants.h"
@@ -133,6 +134,7 @@ std::vector<std::unique_ptr<Config>> GetSegmentationPlatformConfig(
   configs.emplace_back(PowerUserSegment::GetConfig());
   configs.emplace_back(FrequentFeatureUserModel::GetConfig());
   configs.emplace_back(DeviceTierSegment::GetConfig());
+  configs.emplace_back(TabletProductivityUserModel::GetConfig());
 #endif
 
   configs.emplace_back(LowUserEngagementModel::GetConfig());
