cde5decf672626a50137b5cb31a13c7de7f80a02
David Bokan <bokan@chromium.org>
2023-04-18 20:27:14 +0000

    Reland "Add flag to make device-width default on Clank"
    This relands https://crrev.com/e0ed8280aeb66 combined with the bug fix
    in https://crrev.com/0d3aeffa9e9cd4a (which was also reverted).
    
    This reland also puts the changes in viewport_style_resolver behind the
    flag to make it safer to potentially merge.
    
    Originally landed CLs in Patchset 2, diff for changes.
    
    Change-Id: I5ff5de1322edbea0dc07dc86e5f3488c96c892d0
    Bug: 1434390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442526
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1132120}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2169,6 +2169,9 @@ extern const char kContextualSearchSuppressShortViewDescription[];
 extern const char kCormorantName[];
 extern const char kCormorantDescription[];
 
+extern const char kDefaultViewportIsDeviceWidthName[];
+extern const char kDefaultViewportIsDeviceWidthDescription[];
+
 extern const char kDrawEdgeToEdgeName[];
 extern const char kDrawEdgeToEdgeDescription[];
 
