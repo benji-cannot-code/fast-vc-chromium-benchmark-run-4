2de852bc9424564b44debb363657465ba0291106
Xiaohan Wang <xhwang@chromium.org>
2023-04-18 16:15:18 +0000

    media: MediaFoundationWidevineCdm supports ComponentUpdatesEnabled policy
    This CL fixes MediaFoundationWidevineCdmComponentInstallerPolicy to
    support the `ComponentUpdatesEnabled` enterprise policy. See
    https://chromeenterprise.google/policies/?policy=ComponentUpdatesEnabled
    
    Change-Id: Ib56e46bbacaa3ca4128947fa13b985c13c351ede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4440736
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131955}

--- chrome/browser/component_updater/media_foundation_widevine_cdm_component_installer.cc
+++ chrome/browser/component_updater/media_foundation_widevine_cdm_component_installer.cc
@@ -66,9 +66,11 @@ const char kWidevineCdmArch[] =
 
 namespace component_updater {
 
+// Allows this component to be disabled via `ComponentUpdatesEnabled` policy.
+// See https://chromeenterprise.google/policies/?policy=ComponentUpdatesEnabled
 bool MediaFoundationWidevineCdmComponentInstallerPolicy::
     SupportsGroupPolicyEnabledComponentUpdates() const {
-  return false;
+  return true;
 }
 
 bool MediaFoundationWidevineCdmComponentInstallerPolicy::
