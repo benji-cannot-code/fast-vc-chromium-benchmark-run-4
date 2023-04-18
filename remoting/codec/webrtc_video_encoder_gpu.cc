d6c2144864a54478c7af1e2674d678ed6b11857c
Hirokazu Honda <hiroh@chromium.org>
2023-04-18 05:56:39 +0000

    remoting: Replace NotifyError with NotifyErrorStatus()
    This replaces NotifyError() implementation of WebRtcVideoEncoderGpu
    with NotifyErrorStatus().
    
    Bug: b:276005687
    Test: Build
    Change-Id: I6f3fc76445019b931864bd4ab98a80d62298a56a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4417540
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131688}

--- remoting/codec/webrtc_video_encoder_gpu.cc
+++ remoting/codec/webrtc_video_encoder_gpu.cc
@@ -125,7 +125,7 @@ class WebrtcVideoEncoderGpu::Core
   void BitstreamBufferReady(
       int32_t bitstream_buffer_id,
       const media::BitstreamBufferMetadata& metadata) override;
-  void NotifyError(media::VideoEncodeAccelerator::Error error) override;
+  void NotifyErrorStatus(const media::EncoderStatus& status) override;
 
  private:
   enum State { UNINITIALIZED, INITIALIZING, INITIALIZED, INITIALIZATION_ERROR };
@@ -341,10 +341,12 @@ void WebrtcVideoEncoderGpu::Core::BitstreamBufferReady(
   callbacks_.erase(metadata.timestamp);
 }
 
-void WebrtcVideoEncoderGpu::Core::NotifyError(
-    media::VideoEncodeAccelerator::Error error) {
+void WebrtcVideoEncoderGpu::Core::NotifyErrorStatus(
+    const media::EncoderStatus& status) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  LOG(ERROR) << __func__ << " error: " << error;
+  LOG(ERROR) << "NotifyErrorStatus() is called, code="
+             << static_cast<int32_t>(status.code())
+             << ", message=" << status.message();
 }
 
 void WebrtcVideoEncoderGpu::Core::BeginInitialization() {
