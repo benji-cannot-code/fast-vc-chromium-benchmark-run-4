ba4074e038e2389602f72660cc17d7a387075e50
Mohamed Amir Yosef <mamir@chromium.org>
2023-04-18 18:45:30 +0000

    Revert "Fix Regression: Size of text caret is longer then expected on bookmarks overlay"
    This reverts commit a0b867b844d0e05d5b5699016468870581a12ad4.
    
    Reason for revert: This CL is fixing a Windows 7 specific bug
    The fix is actually breaking the cursor for multiline text area.
    The reviewer asked about that in the unresolved comment thread,
    but the comment was never addressed.
    Since Chrome does not support Window 7 anymore, this CL can be
    reverted to fix the cursor behaviour in multiline textareas.
    
    For mutliline textfield
      location.set_height(
          std::min(location.height(),
                   GetLocalBounds().height() - location.y() - location.y()));
    
    location.y() can go arbitrarily large, and as a result, the height will become negative and hence the size will become zero and the cursor disappears.
    
    
    Original change's description:
    > Fix Regression: Size of text caret is longer then expected on bookmarks overlay
    >
    > When the system language is set to Telegu, the text cursor is displayed to be
    > longer than the textfield in the bookmark overlay. The problem is that different
    > languages are displayed at different font, so the text cursor can be longer for
    > some font, ex: Telegu. The Text cursor should stop using the fixed default height
    > and be able to adjust its height based on the height of the textfield and the
    > location of the origin of the cursor.
    >
    > TEXT=TextfieldTest.CursorViewHeight
    >
    > Bug: 739686
    > Change-Id: If57578c7fc2f8342bd81707f45db5d278d8b9822
    > Reviewed-on: https://chromium-review.googlesource.com/608628
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Commit-Queue: Yi Xu <yiyix@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494150}
    
    Bug: 739686
    Change-Id: Ia822be520d17aa8335a086eb23d8e6730800629e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442547
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1132057}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -2566,9 +2566,6 @@ void Textfield::UpdateCursorVisibility() {
 gfx::Rect Textfield::CalculateCursorViewBounds() const {
   gfx::Rect location(GetRenderText()->GetUpdatedCursorBounds());
   location.set_x(GetMirroredXForRect(location));
-  location.set_height(
-      std::min(location.height(),
-               GetLocalBounds().height() - location.y() - location.y()));
   return location;
 }
 
