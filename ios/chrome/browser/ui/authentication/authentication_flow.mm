88cbbef9b7462f5a5288b5147a5c76ebe5d63e27
Menghan YANG <myuu@google.com>
2023-04-18 08:46:03 +0000

    [iOS] Fix crash on sign-in with account storage promo in incognito
    The authentification flow doesn't use the original browser state in
    incognito mode, which causes the app to crash when trying to retrieve
    and use auth related services using the browser state.
    
    This CL makes it use the original browser state in all situations.
    
    Bug: 1432747
    Change-Id: Ib36662a39ed1765c2dc87d9e6541e17be476255f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4428673
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131764}

--- ios/chrome/browser/ui/authentication/authentication_flow.mm
+++ ios/chrome/browser/ui/authentication/authentication_flow.mm
@@ -289,7 +289,7 @@ enum AuthenticationState {
 }
 
 - (void)continueSignin {
-  ChromeBrowserState* browserState = _browser->GetBrowserState();
+  ChromeBrowserState* browserState = [self originalBrowserState];
   if (self.handlingError) {
     // The flow should not continue while the error is being handled, e.g. while
     // the user is being informed of an issue.
@@ -425,7 +425,7 @@ enum AuthenticationState {
 // is not subject to parental controls and then continues sign-in.
 - (void)checkMergeCaseForUnsupervisedAccounts {
   if (([_performer shouldHandleMergeCaseForIdentity:_identityToSignIn
-                                  browserStatePrefs:_browser->GetBrowserState()
+                                  browserStatePrefs:[self originalBrowserState]
                                                         ->GetPrefs()])) {
     [_performer promptMergeCaseForIdentity:_identityToSignIn
                                    browser:_browser
@@ -441,7 +441,7 @@ enum AuthenticationState {
 - (void)checkSigninSteps {
   id<SystemIdentity> currentIdentity =
       AuthenticationServiceFactory::GetForBrowserState(
-          _browser->GetBrowserState())
+          [self originalBrowserState])
           ->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
   if (currentIdentity && ![currentIdentity isEqual:_identityToSignIn]) {
     // If the identity to sign-in is different than the current identity,
@@ -453,7 +453,7 @@ enum AuthenticationState {
 }
 
 - (void)signInIdentity:(id<SystemIdentity>)identity {
-  ChromeBrowserState* browserState = _browser->GetBrowserState();
+  ChromeBrowserState* browserState = [self originalBrowserState];
   ChromeAccountManagerService* accountManagerService =
       ChromeAccountManagerServiceFactory::GetForBrowserState(browserState);
 
@@ -640,6 +640,14 @@ enum AuthenticationState {
                                             completion();
                                           }
                                         }];
+}
+
+#pragma mark - Private methods
+
+// The original chrome browser state used for services that don't exist in
+// incognito mode.
+- (ChromeBrowserState*)originalBrowserState {
+  return _browser->GetBrowserState()->GetOriginalChromeBrowserState();
 }
 
 #pragma mark - Used for testing
