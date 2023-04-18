7bbee006e696dfb9f747edc85c7f6a082b01000b
Jenny Zhang <jennyz@chromium.org>
2023-04-18 03:46:33 +0000

    Fix a bug in StartUniqueAshChrome.
    This cl fixes a bug in nProcessBrowserTest::StartUniqueAshChrome,
    and added the test coverage for verifying the enabled features
    and disabled features configured for unique ash, and also verify
    that a ash feature flag configured to take a non-default value with
    unique ash is passed to Lacros with the expected value.
    
    Bug: 1368284
    Change-Id: I22e078509065909c4fdb4457d801c45acbfc7987
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437429
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131665}

--- chrome/test/base/in_process_browser_test_browsertest_lacros.cc
+++ chrome/test/base/in_process_browser_test_browsertest_lacros.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/base/in_process_browser_test.h"
 
+#include "chrome/browser/speech/tts_crosapi_util.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -14,12 +15,20 @@ class StartUniqueAshBrowserTest : public InProcessBrowserTest {
  public:
   void SetUp() override {
     // Need to put this before starting lacros.
-    StartUniqueAshChrome({}, {}, {"random-unused-example-cmdline"},
-                         "Reason:Test");
+    StartUniqueAshChrome({"ClipboardHistoryRefresh", "Jelly"},
+                         {"DisableLacrosTtsSupport"},
+                         {"random-unused-example-cmdline"}, "Reason:Test");
     InProcessBrowserTest::SetUp();
   }
 
-  void CheckExpectations() { EXPECT_TRUE(ash_process_.IsValid()); }
+  void CheckExpectations() {
+    EXPECT_TRUE(ash_process_.IsValid());
+
+    // Verify that Lacros Tts support is enabled in Lacros. This feature is
+    // disabled by default in Ash, and is enabled by the unique ash in this
+    // test, and passed to Lacros via mojom::crosapi::BrowserInitParams.
+    EXPECT_TRUE(tts_crosapi_util::ShouldEnableLacrosTtsSupport());
+  }
 };
 
 IN_PROC_BROWSER_TEST_F(StartUniqueAshBrowserTest, StartAshChrome) {
