2a4623b33a44ef338b469ac9bc855b580b9bc003
David Benjamin <davidben@chromium.org>
2023-04-18 16:42:44 +0000

    Make absl::string_view a typedef for std::string_view
    Now we only have two string_view implementations in Chromium, instead of
    three!
    
    This only changes the types around. It doesn't rewrite existing uses to
    the STL variants, which we can do incrementally. It also does not
    change PRESUBMITs or programmer expectations yet. It only makes
    absl::string_view internally the same type as std::string_view.
    https://crrev.com/c/4294483 is a follow-up that proposes a new
    policy and corresponding PRESUBMITs.
    
    Hardening comparison:
    https://docs.google.com/document/d/1d4RnD1uAE2t4iANR0nXy82ASIPGsPuw2mpO6v6T7JKs/edit?usp=sharing
    
    Bug: 691162
    Change-Id: Ia8d148a79a2685d0c4300b66bebf14cad8203503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295063
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131979}

--- third_party/abseil-cpp/absl/base/options.h
+++ third_party/abseil-cpp/absl/base/options.h
@@ -149,7 +149,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // absl::string_view is a typedef of std::string_view, use the feature macro
 // ABSL_USES_STD_STRING_VIEW.
 
-#define ABSL_OPTION_USE_STD_STRING_VIEW 0
+#define ABSL_OPTION_USE_STD_STRING_VIEW 2
 
 // ABSL_OPTION_USE_STD_VARIANT
 //
