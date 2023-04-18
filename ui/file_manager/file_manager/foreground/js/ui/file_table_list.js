ce15a01ce4534e7023f38b8e537877cbeefa2aa4
Oleg Davydov <burunduk@chromium.org>
2023-04-18 06:36:39 +0000

    [file manager] Add missing type annotation
    This is a follow-up to https://crrev.com/c/4406475. One method was
    missing a type annotation for the `doc` argument, let's add it.
    
    Bug: None
    Change-Id: I1277b8099e8fafa237370663e412ca9fd2f2010f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4433395
    Auto-Submit: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131696}

--- ui/file_manager/file_manager/foreground/js/ui/file_table_list.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_table_list.js
@@ -493,6 +493,7 @@ filelist.renderEncryptionStatus = (doc) => {
 
 /**
  * Renders the drive inline status in the detail table.
+ * @param {!Document} doc Owner document.
  * @return {!HTMLDivElement} Created element.
  */
 filelist.renderInlineStatus = (doc) => {
