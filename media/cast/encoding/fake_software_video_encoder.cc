3f6150f77574f1bb15908a9bb0a1d14374f0b3c7
Sergii Bykov <sbykov@google.com>
2023-04-18 13:10:38 +0000

    [Code Health] Use base value v2 api in fake_software_video_encoder
    Bug: 1303949
    Change-Id: Iad196af62e723046f2c7608ba79791ce535b7602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420360
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131860}

--- media/cast/encoding/fake_software_video_encoder.cc
+++ media/cast/encoding/fake_software_video_encoder.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/values.h"
 #include "media/base/video_frame.h"
-#include "media/cast/common/encoded_frame.h"
 #include "media/cast/common/frame_id.h"
 #include "media/cast/common/openscreen_conversion_helpers.h"
 #include "media/cast/common/rtp_time.h"
@@ -20,8 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using Dependency = openscreen::cast::EncodedFrame::Dependency;
 
-namespace media {
-namespace cast {
+namespace media::cast {
 
 FakeSoftwareVideoEncoder::FakeSoftwareVideoEncoder(
     const FrameSenderConfig& video_config)
@@ -60,11 +58,13 @@ void FakeSoftwareVideoEncoder::Encode(
       ToRtpTimeTicks(video_frame->timestamp(), kVideoFrequency);
   encoded_frame->reference_time = reference_time;
 
-  base::Value values(base::Value::Type::DICT);
-  values.SetBoolKey("key", encoded_frame->dependency == Dependency::kKeyFrame);
-  values.SetIntKey("ref", encoded_frame->referenced_frame_id.lower_32_bits());
-  values.SetIntKey("id", encoded_frame->frame_id.lower_32_bits());
-  values.SetIntKey("size", frame_size_);
+  const auto values =
+      base::Value::Dict()
+          .Set("key", encoded_frame->dependency == Dependency::kKeyFrame)
+          .Set("ref", static_cast<int>(
+                          encoded_frame->referenced_frame_id.lower_32_bits()))
+          .Set("id", static_cast<int>(encoded_frame->frame_id.lower_32_bits()))
+          .Set("size", frame_size_);
   base::JSONWriter::Write(values, &encoded_frame->data);
   encoded_frame->data.resize(
       std::max<size_t>(encoded_frame->data.size(), frame_size_), ' ');
@@ -86,5 +86,4 @@ void FakeSoftwareVideoEncoder::GenerateKeyFrame() {
   next_frame_is_key_ = true;
 }
 
-}  // namespace cast
-}  // namespace media
+}  // namespace media::cast
