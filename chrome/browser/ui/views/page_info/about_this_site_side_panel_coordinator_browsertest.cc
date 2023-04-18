8dae9f3ac60bf82f77663653264090270009872c
Filipa Senra <fsenra@google.com>
2023-04-18 10:18:31 +0000

    [AboutThisSite] Field trial for AboutThisSiteKeepSidePanelOnSameTabNav
    Bug: 1425145
    Change-Id: I28bf5936c431b0a7c8b140e24d56046f1a61eb90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4422853
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131807}

--- chrome/browser/ui/views/page_info/about_this_site_side_panel_coordinator_browsertest.cc
+++ chrome/browser/ui/views/page_info/about_this_site_side_panel_coordinator_browsertest.cc
@@ -69,8 +69,9 @@ class AboutThisSiteSidePanelCoordinatorBrowserTest
 
  private:
   virtual void SetUpFeatureList() {
-    feature_list_.InitAndEnableFeature(
-        page_info::kPageInfoAboutThisSiteMoreInfo);
+    feature_list_.InitWithFeatures(
+        {page_info::kPageInfoAboutThisSiteMoreInfo},
+        {page_info::kPageInfoAboutThisSiteKeepSidePanelOnSameTabNavs});
   }
 
   net::EmbeddedTestServer https_server_{net::EmbeddedTestServer::TYPE_HTTPS};
