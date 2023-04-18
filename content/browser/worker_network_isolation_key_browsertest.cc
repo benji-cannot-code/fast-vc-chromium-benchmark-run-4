a7836b0927e9c89179363732098cf474e735954e
sbingler <bingler@chromium.org>
2023-04-18 22:22:33 +0000

    Disable storage partitioning for ServiceWorkerMainScriptRequest test
    ServiceWorkerMainScriptRequestNetworkIsolationKeyBrowserTest.
    ServiceWorkerMainScriptRequest depends on unpartitioned storage in order
    to set up and confirm its test expectations. As
    kThirdPartyStoragePartitioning will be enabled by default "soon" this
    test has been modified to force disable kThirdPartyStoragePartitioning.
    
    As follow up work this test should be refactored to remove its
    dependency on kThirdPartyStoragePartitioning's state.
    
    Bug: 1147281, 1403043
    Change-Id: If3282d7fbd96a33adce37b59b5065c1cacf0c531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442326
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132194}

--- content/browser/worker_network_isolation_key_browsertest.cc
+++ content/browser/worker_network_isolation_key_browsertest.cc
@@ -228,8 +228,19 @@ INSTANTIATE_TEST_SUITE_P(
                        ::testing::Values(WorkerType::kServiceWorker,
                                          WorkerType::kSharedWorker)));
 
-using ServiceWorkerMainScriptRequestNetworkIsolationKeyBrowserTest =
-    WorkerNetworkIsolationKeyBrowserTest;
+class ServiceWorkerMainScriptRequestNetworkIsolationKeyBrowserTest
+    : public WorkerNetworkIsolationKeyBrowserTest {
+ public:
+  ServiceWorkerMainScriptRequestNetworkIsolationKeyBrowserTest() {
+    // TODO(crbug.com/1147281): Tests under this class fail when
+    // kThirdPartyStoragePartitioning is enabled.
+    feature_list_.InitAndDisableFeature(
+        net::features::kThirdPartyStoragePartitioning);
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
 
 // Test that network isolation key is filled in correctly for service worker's
 // main script request. The test navigates to "a.test" and creates an iframe
@@ -246,6 +257,9 @@ using ServiceWorkerMainScriptRequestNetworkIsolationKeyBrowserTest =
 // Note that it's sufficient not to test the cache miss when subframe origins
 // are different as in that case the two script urls must be different and it
 // also won't trigger an update.
+//
+// TODO(crbug.com/1147281): Update test to not depend on
+// kThirdPartyStoragePartitioning being disabled.
 IN_PROC_BROWSER_TEST_F(
     ServiceWorkerMainScriptRequestNetworkIsolationKeyBrowserTest,
     ServiceWorkerMainScriptRequest) {
@@ -300,7 +314,7 @@ IN_PROC_BROWSER_TEST_F(
 }
 
 using SharedWorkerMainScriptRequestNetworkIsolationKeyBrowserTest =
-    ServiceWorkerMainScriptRequestNetworkIsolationKeyBrowserTest;
+    WorkerNetworkIsolationKeyBrowserTest;
 
 // Test that network isolation key is filled in correctly for shared worker's
 // main script request. The test navigates to "a.test" and creates an iframe
