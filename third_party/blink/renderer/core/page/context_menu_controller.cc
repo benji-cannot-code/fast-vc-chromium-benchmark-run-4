7e4e1f3ab50d026d56cb47b5b862a4a4bc201c38
François Beaufort <beaufort.francois@gmail.com>
2023-04-18 14:42:55 +0000

    Disable Copy Image context menu for WebGPU canvas
    The Chromium compositor and the Blink WebGPU code can't access the same
    underlying image for reading at the same time. This cause issues when
    user click "Copy Image" context menu as they may get an empty image.
    This CL makes sure this context menu is disabled for WebGPU canvas
    while the underlying issue is being fixed.
    
    Bug: 1267243, 1431714
    Change-Id: I71de085f2b0ebe5f6f4dc16cb839091ba85110bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437202
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131901}

--- third_party/blink/renderer/core/page/context_menu_controller.cc
+++ third_party/blink/renderer/core/page/context_menu_controller.cc
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/frame/web_frame_widget_impl.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
+#include "third_party/blink/renderer/core/html/canvas/html_canvas_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
 #include "third_party/blink/renderer/core/html/html_anchor_element.h"
@@ -599,7 +600,9 @@ bool ContextMenuController::ShowContextMenu(LocalFrame* frame,
     if (potential_image_node != nullptr &&
         IsA<HTMLCanvasElement>(potential_image_node)) {
       data.media_type = mojom::blink::ContextMenuDataMediaType::kCanvas;
-      data.has_image_contents = true;
+      // TODO(crbug.com/1267243): Support WebGPU canvas.
+      data.has_image_contents =
+          !To<HTMLCanvasElement>(potential_image_node)->IsWebGPU();
     } else if (potential_image_node != nullptr &&
                !HitTestResult::AbsoluteImageURL(potential_image_node)
                     .IsEmpty()) {
