f506759ee83d4e7db9539fbe4772fda3e64e21df
Chad Duffin <chadduffin@chromium.org>
2023-04-18 22:19:44 +0000

    jelly: Jellify the internet config dialog
    This change jellifies the internet config dialog, enabling it to
    dynamically update the CSS used for the dialog so that it matches the
    theme chosen by the user.
    
    Video: https://drive.google.com/file/d/1oWw70XdvEwIuLP_67CYntddw4t6Is0TQ/view?usp=sharing
    
    Please note that the video was taken prior to the .error color change
    to var(--cros-text-color-alert). See the screenshot below for what this
    change updated the color to look like.
    
    https://screenshot.googleplex.com/7XeDdX2jNZL4Dao.png
    
    Bug: b/274649233
    Test: Unit tests and manually on device.
    Change-Id: I38be0546de390a246f538f2774c34ea4f534b849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4428833
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132192}

--- chrome/browser/ui/webui/ash/internet_config_dialog.cc
+++ chrome/browser/ui/webui/ash/internet_config_dialog.cc
@@ -21,12 +21,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
 #include "chromeos/ash/components/network/network_util.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"  // nogncheck
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "ui/chromeos/strings/grit/ui_chromeos_strings.h"
 #include "ui/chromeos/strings/network/network_element_localized_strings_provider.h"
+#include "ui/webui/color_change_listener/color_change_handler.h"
 #include "ui/wm/core/shadow_types.h"
 
 namespace ash {
@@ -169,6 +171,7 @@ InternetConfigDialogUI::InternetConfigDialogUI(content::WebUI* web_ui)
 
   source->DisableTrustedTypesCSP();
 
+  source->AddBoolean("isJellyEnabled", ::chromeos::features::IsJellyEnabled());
   AddInternetStrings(source);
   source->AddLocalizedString("title", IDS_SETTINGS_INTERNET_CONFIG);
 
@@ -185,6 +188,12 @@ void InternetConfigDialogUI::BindInterface(
     mojo::PendingReceiver<chromeos::network_config::mojom::CrosNetworkConfig>
         receiver) {
   GetNetworkConfigService(std::move(receiver));
+}
+
+void InternetConfigDialogUI::BindInterface(
+    mojo::PendingReceiver<color_change_listener::mojom::PageHandler> receiver) {
+  color_change_handler_ = std::make_unique<ui::ColorChangeHandler>(
+      web_ui()->GetWebContents(), std::move(receiver));
 }
 
 WEB_UI_CONTROLLER_TYPE_IMPL(InternetConfigDialogUI)
