ea93365c22d7f7efbc22181a7d60bd054751f724
Dylan Cutler <dylancutler@google.com>
2023-04-18 00:13:19 +0000

    Add WPT for partitioned cookies/service worker interaction
    This CL adds a WPT which exercises the interaction between partitioned service workers and partitioned cookies.
    
    When 3P storage partitioning is enabled, partitioned cookies should
    only be visible to partitioned workers whose StorageKey's equivalent CookiePartitionKey matches their own.
    
    The CookieStoreManager API was partitioned in a separate CL.
    
    This does NOT test that workers in nonced partitions do not have access to unpartitioned cookies.
    
    Bug: 1225444,1246549,1427879
    Change-Id: Id101b4fa48bed46605fe95f7ef2b1d2463f9301a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674179
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131591}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/partitioned-cookies-3p-sw.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener('message', ev => ev.waitUntil(onMessage(ev)));
+
+async function onMessage(event) {
+  if (!event.data)
+    return;
+  switch (event.data.type) {
+    case 'test_message':
+      return onTestMessage(event);
+    case 'echo_cookies':
+      return onEchoCookies(event);
+    default:
+      return;
+  }
+}
+
+// test_message just verifies that the message passing is working.
+async function onTestMessage(event) {
+  event.source.postMessage({ok: true});
+}
+
+// echo_cookies returns the names of all of the cookies available to the worker.
+async function onEchoCookies(event) {
+  try {
+    const cookie_objects = await self.cookieStore.getAll();
+    const cookies = cookie_objects.map(c => c.name);
+    event.source.postMessage({ok: true, cookies});
+  } catch (err) {
+    event.source.postMessage({ok: false});
+  }
+}
