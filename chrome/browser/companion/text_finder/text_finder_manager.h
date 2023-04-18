0bfeea46c6cdaacd7771c033eac854b3bf32aed8
Jun Zou <junzou@chromium.org>
2023-04-18 21:00:20 +0000

    Add a method for creating multiple text finders.
    Bug: b/273307525
    Change-Id: Ia86c39022981de7dd6dbd5511664cb5a79651dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437617
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Jun Zou <junzou@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132147}

--- chrome/browser/companion/text_finder/text_finder_manager.h
+++ chrome/browser/companion/text_finder/text_finder_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <unordered_map>
+#include <vector>
 
 #include "base/memory/weak_ptr.h"
 #include "base/unguessable_token.h"
@@ -20,6 +21,9 @@ namespace companion {
 
 class TextFinderManager : public content::PageUserData<TextFinderManager> {
  public:
+  using AllDoneCallback = base::OnceCallback<void(
+      const std::vector<std::pair<std::string, bool>>&)>;
+
   ~TextFinderManager() override;
 
   TextFinderManager(const TextFinderManager&) = delete;
@@ -34,13 +38,20 @@ class TextFinderManager : public content::PageUserData<TextFinderManager> {
       const std::string& text_directive,
       TextFinder::FinishedCallback callback);
 
+  // Creates multiple text finders for a vector of text directives. Calls
+  // `all_done_callback` when all text finders finish searching (via
+  // `base::BarrierCallback`).
+  void CreateTextFinders(const std::vector<std::string>& text_directives,
+                         AllDoneCallback all_done_callback);
+
   // Returns the number of text finder instances.
   size_t Size() const;
 
  private:
   friend class content::PageUserData<TextFinderManager>;
   friend class TextFinderManagerBaseTest;
-  FRIEND_TEST_ALL_PREFIXES(TextFinderManagerTest, TextFinderTest);
+  FRIEND_TEST_ALL_PREFIXES(TextFinderManagerTest, SingleTextFinderTest);
+  FRIEND_TEST_ALL_PREFIXES(TextFinderManagerTest, MultiTextFindersTest);
 
   explicit TextFinderManager(content::Page& page);
 
