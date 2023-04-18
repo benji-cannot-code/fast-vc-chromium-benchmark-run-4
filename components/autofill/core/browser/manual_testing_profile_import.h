d9784deed9389a43200bd17e646fda9e5d6ca0c5
Dmitry Vykochko <vykochko@google.com>
2023-04-18 12:15:21 +0000

    Manual profile import: cl flag for JSON content.
    `--autofill-profiles-content-for-manual-testing`
    
    In addition to the path flag(`--autofill-profiles-for-manual-testing`).
    
    Bug: 1421688
    Low-Coverage-Reason: changed code is trivial and there are more broad tests being added in another CL
    Change-Id: I93ab4985e599e2a29a18a1b4db68499925d72507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4438573
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131835}

--- components/autofill/core/browser/manual_testing_profile_import.h
+++ components/autofill/core/browser/manual_testing_profile_import.h
@@ -17,9 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-// Command line flag that enables importing AutofillProfiles for manual testing.
-// It expects the path to a file as a value, which contains profile descriptions
-// in JSON format. The following format is expected:
+// Command line flags that enable importing AutofillProfiles for manual testing:
+//   --autofill-profiles-content-for-manual-testing
+//       Expects profile descriptions as a string in JSON format.
+//   --autofill-profiles-for-manual-testing
+//       The same as above, but instead of the JSON content, expects the path
+//       to a file with profile descriptions in JSON format.
+// The following format is expected:
 // {
 //   "profiles" : [
 //     {
@@ -42,6 +46,8 @@ namespace autofill {
 // The profiles are expected to be fully structured
 constexpr char kManualProfileImportForTestingFlag[] =
     "autofill-profiles-for-manual-testing";
+constexpr char kManualProfileContentImportForTestingFlag[] =
+    "autofill-profiles-content-for-manual-testing";
 
 // Given a description of fully structured profiles in the aforementioned JSON
 // format, converts it to a vector of AutofillProfiles.
