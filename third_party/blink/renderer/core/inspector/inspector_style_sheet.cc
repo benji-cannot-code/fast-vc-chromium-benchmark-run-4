85020fe46e14258f42233d305ab35476ed04312a
Philip Pfaffe <pfaffe@chromium.org>
2023-04-18 12:45:11 +0000

    Do not make loadingFailed the default for non-network stylesheets
    Bug: 1386059
    Change-Id: I3f72a7e494d5cda9ff1347a1ca68e343deff0e0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4439755
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131847}

--- third_party/blink/renderer/core/inspector/inspector_style_sheet.cc
+++ third_party/blink/renderer/core/inspector/inspector_style_sheet.cc
@@ -2287,7 +2287,7 @@ void InspectorStyleSheet::UpdateText() {
   if (!success)
     success = InlineStyleSheetText(&text);
   if (!success) {
-    bool loadingFailed = true;
+    bool loadingFailed = false;
     success = ResourceStyleSheetText(&text, &loadingFailed);
     request_failed_to_load_ = loadingFailed;
   }
