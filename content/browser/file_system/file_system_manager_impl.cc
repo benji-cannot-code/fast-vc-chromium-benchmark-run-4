0478245513bf47cea673a1c64103ed486d82cea4
Claudio DeSouza <cdesouza@igalia.com>
2023-04-18 13:55:54 +0000

    Replace base::GUID uses with base::Uuid (88/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /content/browser/file_system
    
    This CL was uploaded by git cl split.
    
    R=ayui@chromium.org
    
    Bug: 1428566
    Change-Id: Iff8db3326ca620d404e2a6372f1e1996b68d0b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4429685
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1131872}

--- content/browser/file_system/file_system_manager_impl.cc
+++ content/browser/file_system/file_system_manager_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
-#include "base/guid.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/user_metrics.h"
 #include "base/notreached.h"
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/threading/thread.h"
 #include "base/time/time.h"
+#include "base/uuid.h"
 #include "build/build_config.h"
 #include "components/services/filesystem/public/mojom/types.mojom.h"
 #include "content/browser/bad_message.h"
@@ -1016,7 +1016,7 @@ void FileSystemManagerImpl::ContinueRegisterBlob(
     storage::FileSystemURL crack_url,
     bool security_check_success) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  std::string uuid = base::GenerateGUID();
+  std::string uuid = base::Uuid::GenerateRandomV4().AsLowercaseString();
   mojo::PendingRemote<blink::mojom::Blob> blob_remote;
   mojo::PendingReceiver<blink::mojom::Blob> blob_receiver =
       blob_remote.InitWithNewPipeAndPassReceiver();
