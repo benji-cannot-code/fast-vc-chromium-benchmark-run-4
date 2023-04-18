8f2ad38ace5fe9e9128a343573fbb45fe5d52578
Mike Dougherty <michaeldo@chromium.org>
2023-04-18 17:13:23 +0000

    Remove web_frame_util usage in //components/translate
    This util only supports the page content world which is less widely used
    now than it was when the utility was created. Additionally, it can hide
    this fact from the caller and lead to bugs where the expected frames are
    not returned (because the associated features live in an isolated
    world).
    
    Bug: 1427327
    Change-Id: I231b3f122aa92a350ee9d174eebabbcf8bc242bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437732
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1132008}

--- components/translate/ios/browser/js_translate_web_frame_manager_unittest.mm
+++ components/translate/ios/browser/js_translate_web_frame_manager_unittest.mm
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/grit/components_resources.h"
 #import "components/translate/ios/browser/js_translate_web_frame_manager_factory.h"
+#import "components/translate/ios/browser/translate_java_script_feature.h"
 #import "ios/web/public/js_messaging/web_frame.h"
-#import "ios/web/public/js_messaging/web_frame_util.h"
+#import "ios/web/public/js_messaging/web_frames_manager.h"
 #import "ios/web/public/test/web_test_with_web_state.h"
 #import "testing/gtest_mac.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -30,7 +31,10 @@ TEST_F(JsTranslateWebFrameManagerTest, Inject) {
   id result = ExecuteJavaScript(@"typeof cr == 'undefined'");
   ASSERT_NSEQ(@YES, result);
 
-  web::WebFrame* frame = web::GetMainFrame(web_state());
+  TranslateJavaScriptFeature* feature =
+      TranslateJavaScriptFeature::GetInstance();
+  web::WebFrame* frame =
+      feature->GetWebFramesManager(web_state())->GetMainWebFrame();
   JSTranslateWebFrameManager* manager =
       JSTranslateWebFrameManagerFactory::GetInstance()->FromWebFrame(frame);
   ASSERT_FALSE(manager);
@@ -56,7 +60,10 @@ TEST_F(JsTranslateWebFrameManagerTest, Inject) {
 // lib ready.
 TEST_F(JsTranslateWebFrameManagerTest, Reinject) {
   LoadHtml(@"<html></html>");
-  web::WebFrame* frame = web::GetMainFrame(web_state());
+  TranslateJavaScriptFeature* feature =
+      TranslateJavaScriptFeature::GetInstance();
+  web::WebFrame* frame =
+      feature->GetWebFramesManager(web_state())->GetMainWebFrame();
   JSTranslateWebFrameManagerFactory::GetInstance()->CreateForWebFrame(frame);
   JSTranslateWebFrameManager* manager =
       JSTranslateWebFrameManagerFactory::GetInstance()->FromWebFrame(frame);
@@ -76,7 +83,10 @@ TEST_F(JsTranslateWebFrameManagerTest, Reinject) {
 // Tests that starting translation calls the appropriate API.
 TEST_F(JsTranslateWebFrameManagerTest, Translate) {
   LoadHtml(@"<html></html>");
-  web::WebFrame* frame = web::GetMainFrame(web_state());
+  TranslateJavaScriptFeature* feature =
+      TranslateJavaScriptFeature::GetInstance();
+  web::WebFrame* frame =
+      feature->GetWebFramesManager(web_state())->GetMainWebFrame();
   JSTranslateWebFrameManagerFactory::GetInstance()->CreateForWebFrame(frame);
   JSTranslateWebFrameManager* manager =
       JSTranslateWebFrameManagerFactory::GetInstance()->FromWebFrame(frame);
@@ -107,7 +117,10 @@ TEST_F(JsTranslateWebFrameManagerTest, Translate) {
 // Tests that reverting translation calls the appropriate API.
 TEST_F(JsTranslateWebFrameManagerTest, Revert) {
   LoadHtml(@"<html></html>");
-  web::WebFrame* frame = web::GetMainFrame(web_state());
+  TranslateJavaScriptFeature* feature =
+      TranslateJavaScriptFeature::GetInstance();
+  web::WebFrame* frame =
+      feature->GetWebFramesManager(web_state())->GetMainWebFrame();
   JSTranslateWebFrameManagerFactory::GetInstance()->CreateForWebFrame(frame);
   JSTranslateWebFrameManager* manager =
       JSTranslateWebFrameManagerFactory::GetInstance()->FromWebFrame(frame);
