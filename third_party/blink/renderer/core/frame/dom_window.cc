8bdf72d004573d70b84a4d3cf95f123d10a1ba77
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-04-18 09:27:01 +0000

    COOP-reporting: Console error on reporting.
    Chrome-Security-Fixit-Week-9
    
    It is difficult for developer to verify the effect of adding
    `Cross-Origin-Opener-Policy-Report-Only`.
    
    This patch adds a console error on accesses that would be blocked by
    COOP.
    
    Before this patch, developers would have to setup a reporting server or
    install some ReportingObserver. This is not as immediate and easy as a
    console error message.
    
    Fixed: 1124251
    Bug: 1124251
    Change-Id: I48722350a0d8a5e191036342cdb476937e686633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374000
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1131789}

--- third_party/blink/renderer/core/frame/dom_window.cc
+++ third_party/blink/renderer/core/frame/dom_window.cc
@@ -47,6 +47,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+String CoopReportOnlyErrorMessage(const String& property_name) {
+  String call;
+  if (property_name == "named") {
+    call = "window[\"name\"]";
+  } else if (property_name == "indexed") {
+    call = "window[i]";
+  } else {
+    call = "window." + property_name;
+  }
+  return "Cross-Origin-Opener-Policy policy would block the " + call + " call.";
+}
+
+}  // namespace
+
 DOMWindow::DOMWindow(Frame& frame)
     : frame_(frame),
       window_proxy_manager_(frame.GetWindowProxyManager()),
@@ -628,7 +644,11 @@ void DOMWindow::ReportCoopAccess(const char* property_name) {
         location->Url() ? location->Url() : "", location->LineNumber(),
         location->ColumnNumber());
 
-    // TODO(https://crbug.com/1124251): Notify Devtool about the access attempt.
+    accessing_window->GetFrameConsole()->AddMessage(
+        MakeGarbageCollected<ConsoleMessage>(
+            mojom::blink::ConsoleMessageSource::kJavaScript,
+            mojom::blink::ConsoleMessageLevel::kError,
+            CoopReportOnlyErrorMessage(property_name), location->Clone()));
 
     // If the reporting document hasn't specified any network report
     // endpoint(s), then it is likely not interested in receiving
