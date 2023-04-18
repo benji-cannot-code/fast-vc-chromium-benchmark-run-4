6a2f60b83da57a642a0724fdff2a52333f3f4e7a
Hira Mahmood <hiramahmood@google.com>
2023-04-18 20:48:07 +0000

    [iOS] Create Credential Provider Promo EGtest cases
    This CL adds EGTest cases for the CPE promo to test:
     * Promo shown on password copied
     * Promo shown on password saved
    
    Bug: 1422993
    Change-Id: Ife324c59b6e13d599552d179e546b87a489cec85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4383030
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132139}

--- ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h
+++ ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h
@@ -14,6 +14,7 @@ extern NSString* const kConfirmationAlertTitleAccessibilityIdentifier;
 extern NSString* const kConfirmationAlertSubtitleAccessibilityIdentifier;
 extern NSString* const kConfirmationAlertPrimaryActionAccessibilityIdentifier;
 extern NSString* const kConfirmationAlertSecondaryActionAccessibilityIdentifier;
+extern NSString* const kConfirmationAlertTertiaryActionAccessibilityIdentifier;
 
 @protocol ConfirmationAlertActionHandler;
 
