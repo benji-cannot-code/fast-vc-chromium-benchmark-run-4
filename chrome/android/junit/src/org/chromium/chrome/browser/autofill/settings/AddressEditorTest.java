74c4f86dd9e3ae98a6f4daded5baf5a2206162ef
Timofey Chudakov <tchudakov@google.com>
2023-04-18 13:37:29 +0000

    [Leipzig][Android] Check country eligibility via PersonalDataManager.
    Check if country is eligible for address account storage using
    PersonalDataManager instead of using a hard coded list of
    countries in the plaftform specific code.
    
    Bug: 1421688
    Change-Id: Id55c8bfcac10e058992a8c5fa19adf753e38649c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430990
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1131865}

--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AddressEditorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AddressEditorTest.java
@@ -126,11 +126,7 @@ public class AddressEditorTest {
     // initializes Android library dependencies.
     private final List<DropdownKeyValue> mSupportedCountries =
             List.of(new DropdownKeyValue("US", "United States"),
-                    new DropdownKeyValue("DE", "Germany"), new DropdownKeyValue("CU", "Cuba"),
-                    new DropdownKeyValue("IR", "Iran"), new DropdownKeyValue("KP", "North Korea"),
-                    new DropdownKeyValue("SD", "Sudan"), new DropdownKeyValue("SY", "Syria"));
-    private final List<DropdownKeyValue> mNonSactionedCountries = List.of(
-            new DropdownKeyValue("US", "United States"), new DropdownKeyValue("DE", "Germany"));
+                    new DropdownKeyValue("DE", "Germany"), new DropdownKeyValue("CU", "Cuba"));
 
     private Callback<AutofillAddress> mDoneCallback;
     @Nullable
@@ -168,6 +164,7 @@ public class AddressEditorTest {
         when(mSyncService.getSelectedTypes()).thenReturn(new HashSet());
         SyncService.overrideForTests(mSyncService);
 
+        when(mPersonalDataManager.isCountryEligibleForAccountStorage(anyString())).thenReturn(true);
         PersonalDataManager.setInstanceForTesting(mPersonalDataManager);
 
         setUpSupportedCountries(mSupportedCountries);
@@ -822,13 +819,14 @@ public class AddressEditorTest {
 
     @Test
     @SmallTest
-    public void accountSavingDisallowedForSanctionedCountry() {
+    public void accountSavingDisallowedForUnsupportedCountry() {
         when(mPersonalDataManager.isEligibleForAddressAccountStorage()).thenReturn(true);
+        when(mPersonalDataManager.isCountryEligibleForAccountStorage(eq("CU"))).thenReturn(false);
         mAddressEditor = new AddressEditor(
                 /*saveToDisk=*/false, /*isUpdate=*/false, /*isMigrationToAccount=*/false);
         mAddressEditor.setEditorDialog(mEditorDialog);
         setUpAddressUiComponents(sSupportedAddressFields, "US");
-        setUpAddressUiComponents(sSupportedAddressFields, "SY");
+        setUpAddressUiComponents(sSupportedAddressFields, "CU");
         mAddressEditor.edit(null, mDoneCallback);
 
         EditorModel editorModel = mEditorModelCapture.getValue();
@@ -837,7 +835,7 @@ public class AddressEditorTest {
         Assert.assertEquals(13, editorFields.size());
 
         EditorFieldModel countryDropdown = editorFields.get(0);
-        countryDropdown.setDropdownKey("SY", () -> {});
+        countryDropdown.setDropdownKey("CU", () -> {});
 
         // Set values of the required fields.
         editorFields.get(2).setValue("New Name");
@@ -854,7 +852,8 @@ public class AddressEditorTest {
 
     @Test
     @SmallTest
-    public void countryDropDownExcludesSanctionedCountries_saveInAccountFlow() {
+    public void countryDropDownExcludesUnsupportedCountries_saveInAccountFlow() {
+        when(mPersonalDataManager.isCountryEligibleForAccountStorage(eq("CU"))).thenReturn(false);
         mAddressEditor = new AddressEditor(
                 /*saveToDisk=*/false, /*isUpdate=*/false, /*isMigrationToAccount=*/false);
         mAddressEditor.setEditorDialog(mEditorDialog);
@@ -872,7 +871,8 @@ public class AddressEditorTest {
 
     @Test
     @SmallTest
-    public void countryDropDownExcludesSanctionedCountries_MigrationFlow() {
+    public void countryDropDownExcludesUnsupportedCountries_MigrationFlow() {
+        when(mPersonalDataManager.isCountryEligibleForAccountStorage(eq("CU"))).thenReturn(false);
         mAddressEditor = new AddressEditor(
                 /*saveToDisk=*/false, /*isUpdate=*/false, /*isMigrationToAccount=*/true);
         mAddressEditor.setEditorDialog(mEditorDialog);
@@ -890,7 +890,8 @@ public class AddressEditorTest {
 
     @Test
     @SmallTest
-    public void countryDropDownExcludesSanctionedCountries_editExistingAccountProfile() {
+    public void countryDropDownExcludesUnsupportedCountries_editExistingAccountProfile() {
+        when(mPersonalDataManager.isCountryEligibleForAccountStorage(eq("CU"))).thenReturn(false);
         mAddressEditor = new AddressEditor(
                 /*saveToDisk=*/false, /*isUpdate=*/true, /*isMigrationToAccount=*/false);
         mAddressEditor.setEditorDialog(mEditorDialog);
