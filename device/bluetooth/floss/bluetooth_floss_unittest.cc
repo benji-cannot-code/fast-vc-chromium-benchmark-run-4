52795df0e9a2f0c161dadd4c0e56aab59fda9564
Katherine Lai <laikatherine@google.com>
2023-04-18 15:23:01 +0000

    [floss] Fix inconsistent connection state
    |OnConnectAllEnabledProfiles| only indicates the dbus
    successfully dispatched the command. Delay the connect success
    callback until we have actually connected to the device and create
    a timeout to run the error callback.
    
    Also refactors connecting logic to stay within Floss device
    class and creates connecting state tracking.
    
    BUG=b:260620015
    TEST=manual and device_unittests
    
    Change-Id: Ide5c6ee98acb1d7e718768fe00dcf7c322b7f234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415800
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Commit-Queue: Katherine Lai <laikatherine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131928}

--- device/bluetooth/floss/bluetooth_floss_unittest.cc
+++ device/bluetooth/floss/bluetooth_floss_unittest.cc
@@ -243,6 +243,8 @@ TEST_F(BluetoothFlossTest, PairJustWorks) {
             run_loop.Quit();
           }));
   ASSERT_TRUE(device->IsConnecting());
+
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(true);
   run_loop.Run();
 
   ASSERT_FALSE(device->IsConnecting());
@@ -272,6 +274,7 @@ TEST_F(BluetoothFlossTest, PairConfirmPasskey) {
             EXPECT_FALSE(error.has_value());
             run_loop.Quit();
           }));
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(true);
   run_loop.Run();
 
   EXPECT_TRUE(device->IsPaired());
@@ -306,6 +309,7 @@ TEST_F(BluetoothFlossTest, PairDisplayPasskeySucceeded) {
             EXPECT_FALSE(error.has_value());
             run_loop.Quit();
           }));
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(true);
   run_loop.Run();
 
   EXPECT_TRUE(device->IsPaired());
@@ -342,6 +346,7 @@ TEST_F(BluetoothFlossTest, PairDisplayPasskeyFailed) {
             EXPECT_TRUE(error.has_value());
             run_loop.Quit();
           }));
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(true);
   run_loop.Run();
 
   EXPECT_FALSE(device->IsPaired());
@@ -367,6 +372,7 @@ TEST_F(BluetoothFlossTest, PairPasskeyEntry) {
             EXPECT_FALSE(error.has_value());
             run_loop.Quit();
           }));
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(true);
   run_loop.Run();
 
   EXPECT_TRUE(device->IsPaired());
@@ -393,9 +399,14 @@ TEST_F(BluetoothFlossTest, RemoveBonding) {
             EXPECT_FALSE(error.has_value());
             run_loop.Quit();
           }));
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(true);
   run_loop.Run();
 
   EXPECT_TRUE(device->IsPaired());
+  ASSERT_TRUE(device->IsConnected());
+
+  // Simulate device disconnecting
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(false);
 
   base::RunLoop run_loop2;
   device->Forget(base::BindLambdaForTesting([&run_loop2]() {
@@ -448,6 +459,7 @@ TEST_F(BluetoothFlossTest, Disconnect) {
             EXPECT_FALSE(error.has_value());
             run_loop.Quit();
           }));
+  static_cast<BluetoothDeviceFloss*>(device)->SetIsConnected(true);
   run_loop.Run();
 
   EXPECT_TRUE(device->IsPaired());
