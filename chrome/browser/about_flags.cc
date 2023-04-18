0c2d0f425d35673620b2c28162749a663128337d
Christopher Cameron <ccameron@chromium.org>
2023-04-18 23:21:05 +0000

    Gainmap HDR Images: Fix chrome://flags
    This was erroneously inserted into an Android-only block.
    
    Bug: 1404000
    Change-Id: Ib2da40c38ee793771827e1356e44ee8a60c457f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442247
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Quick-Run: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132223}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6929,10 +6929,6 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAndroid,
      FEATURE_VALUE_TYPE(switches::kGaiaIdCacheInAccountManagerFacade)},
 
-    {"gainmap-hdr-images", flag_descriptions::kGainmapHdrImagesName,
-     flag_descriptions::kGainmapHdrImagesDescription, kOsAll,
-     FEATURE_VALUE_TYPE(blink::features::kGainmapHdrImages)},
-
     {"tangible-sync", flag_descriptions::kTangibleSyncName,
      flag_descriptions::kTangibleSyncDescription, kOsAndroid,
      FEATURE_WITH_PARAMS_VALUE_TYPE(switches::kTangibleSync,
@@ -6944,6 +6940,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kIdentityStatusConsistencyDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(switches::kIdentityStatusConsistency)},
 #endif  // BUILDFLAG(IS_ANDROID)
+
+    {"gainmap-hdr-images", flag_descriptions::kGainmapHdrImagesName,
+     flag_descriptions::kGainmapHdrImagesDescription, kOsAll,
+     FEATURE_VALUE_TYPE(blink::features::kGainmapHdrImages)},
 
     {"autofill-use-improved-label-disambiguation",
      flag_descriptions::kAutofillUseImprovedLabelDisambiguationName,
