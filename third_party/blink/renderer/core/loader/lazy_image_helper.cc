3d080b1f6c02ca331790cd1a5be8201d106c1570
Philip Rogers <pdr@chromium.org>
2023-04-18 17:35:41 +0000

    Add lazy loaded image size metrics
    This patch adds the following histograms:
      Blink.LazyLoadedImage.Size
      Blink.LazyLoadedImageBeforeDocumentOnLoad.Size
    
    These histograms measure the network response size of each lazy loaded
    image. This can be used to directly measure the effect of the
    DelayOutOfViewportLazyImages experiment. These metrics are also useful
    for measuring an in-progress project to select smaller lazy loaded
    images (https://crbug.com/1359051).
    
    Bug: 1394025
    Change-Id: I7aad17f86a38e1e4cae288d3eff28c8655a78289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4440746
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132018}

--- third_party/blink/renderer/core/loader/lazy_image_helper.cc
+++ third_party/blink/renderer/core/loader/lazy_image_helper.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/loader/lazy_image_helper.h"
 
+#include "base/metrics/histogram_macros.h"
+#include "base/numerics/safe_conversions.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -19,6 +21,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace {
+
+// Records |bytes| to |histogram_name| in kilobytes (i.e., bytes / 1024).
+// https://almanac.httparchive.org/en/2022/page-weight#fig-12 reports the 90th
+// percentile of jpeg images is 213KB with a max of ~64MB. The max bucket size
+// has been set at 64MB to capture this range with as much granularity as
+// possible.
+#define IMAGE_BYTES_HISTOGRAM(histogram_name, bytes)                        \
+  UMA_HISTOGRAM_CUSTOM_COUNTS(histogram_name,                               \
+                              base::saturated_cast<int>((bytes) / 1024), 1, \
+                              64 * 1024, 50)
 
 // Returns true if absolute dimension is specified in the width and height
 // attributes or in the inline style.
@@ -150,6 +162,19 @@ LazyImageHelper::DetermineEligibilityAndTrackVisibilityMetrics(
 
 void LazyImageHelper::RecordMetricsOnLoadFinished(
     HTMLImageElement* image_element) {
+  if (image_element->is_lazy_loaded()) {
+    if (ImageResourceContent* content = image_element->CachedImage()) {
+      int64_t response_size = content->GetResponse().EncodedDataLength();
+      IMAGE_BYTES_HISTOGRAM("Blink.LazyLoadedImage.Size", response_size);
+      if (Document* document = GetRootDocumentOrNull(image_element)) {
+        if (!document->LoadEventFinished()) {
+          IMAGE_BYTES_HISTOGRAM(
+              "Blink.LazyLoadedImageBeforeDocumentOnLoad.Size", response_size);
+        }
+      }
+    }
+  }
+
   if (!RuntimeEnabledFeatures::LazyImageVisibleLoadTimeMetricsEnabled())
     return;
   if (Document* document = GetRootDocumentOrNull(image_element)) {
