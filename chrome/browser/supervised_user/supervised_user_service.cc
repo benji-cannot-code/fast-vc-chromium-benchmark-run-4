a72f6b8ba49799f3f7f235bd604d25a2afb90405
anthie@google.com <anthie@google.com>
2023-04-18 18:26:34 +0000

    Change child check in Supervised User Service
    This change replaces usages of Profile::IsChild before
    migrating the source to components.
    The new public method exposed by the SUS should be used
    instead of Profile::IsChild to guard child-specific
    behaviour. The description of both methods is updated with
    these instructions.
    
    In the passing: removed unused Chrome OS dependencies (they should
    have been removed already - dead code now).
    
    Bug: b/270535171
    Change-Id: Ib310cb177e97fdd368b0ccdaa3712c8d9da40db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414710
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132043}

--- chrome/browser/supervised_user/supervised_user_service.cc
+++ chrome/browser/supervised_user/supervised_user_service.cc
@@ -122,7 +122,7 @@ void SupervisedUserService::Init() {
       base::BindRepeating(&SupervisedUserService::OnSupervisedUserIdChanged,
                           base::Unretained(this)));
 
-  SetActive(IsChild());
+  SetActive(IsSubjectToParentalControls());
 }
 
 void SupervisedUserService::SetDelegate(Delegate* delegate) {
@@ -188,9 +188,9 @@ std::u16string SupervisedUserService::GetExtensionsLockedMessage() const {
 bool SupervisedUserService::IsURLFilteringEnabled() const {
 // TODO(b/271413641): Use capabilities to verify if filtering is enabled on iOS.
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
-  return profile_->IsChild();
+  return IsSubjectToParentalControls();
 #else
-  return profile_->IsChild() &&
+  return IsSubjectToParentalControls() &&
          base::FeatureList::IsEnabled(
              supervised_user::kFilterWebsitesForSupervisedUsersOnDesktopAndIOS);
 #endif
@@ -199,9 +199,9 @@ bool SupervisedUserService::IsURLFilteringEnabled() const {
 bool SupervisedUserService::AreExtensionsPermissionsEnabled() const {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
-  return profile_->IsChild();
+  return IsSubjectToParentalControls();
 #else
-  return profile_->IsChild() &&
+  return IsSubjectToParentalControls() &&
          base::FeatureList::IsEnabled(
              supervised_user::
                  kEnableExtensionsPermissionsForSupervisedUsersOnDesktop);
@@ -279,7 +279,7 @@ void SupervisedUserService::RemoveExtensionApproval(
 
 bool SupervisedUserService::
     GetSupervisedUserExtensionsMayRequestPermissionsPref() const {
-  DCHECK(IsChild())
+  DCHECK(IsSubjectToParentalControls())
       << "Calling GetSupervisedUserExtensionsMayRequestPermissionsPref() only "
          "makes sense for supervised users";
   return user_prefs_->GetBoolean(
@@ -408,8 +408,9 @@ void SupervisedUserService::SetActive(bool active) {
   }
 }
 
-bool SupervisedUserService::IsChild() const {
-  return profile_->IsChild();
+bool SupervisedUserService::IsSubjectToParentalControls() const {
+  return user_prefs_->GetString(prefs::kSupervisedUserId) ==
+         supervised_user::kChildAccountSUID;
 }
 
 void SupervisedUserService::OnCustodianInfoChanged() {
@@ -418,7 +419,7 @@ void SupervisedUserService::OnCustodianInfoChanged() {
 }
 
 void SupervisedUserService::OnSupervisedUserIdChanged() {
-  SetActive(IsChild());
+  SetActive(IsSubjectToParentalControls());
 }
 
 void SupervisedUserService::OnDefaultFilteringBehaviorChanged() {
@@ -442,7 +443,7 @@ void SupervisedUserService::OnDefaultFilteringBehaviorChanged() {
 }
 
 bool SupervisedUserService::IsSafeSitesEnabled() const {
-  return profile_->IsChild() &&
+  return IsSubjectToParentalControls() &&
          user_prefs_->GetBoolean(prefs::kSupervisedUserSafeSites);
 }
 
@@ -519,7 +520,7 @@ void SupervisedUserService::Shutdown() {
     return;
   DCHECK(!did_shutdown_);
   did_shutdown_ = true;
-  if (IsChild()) {
+  if (IsSubjectToParentalControls()) {
     base::RecordAction(UserMetricsAction("ManagedUsers_QuitBrowser"));
   }
   SetActive(false);
@@ -597,7 +598,7 @@ std::string SupervisedUserService::GetDebugPolicyProviderName() const {
 
 bool SupervisedUserService::UserMayLoad(const Extension* extension,
                                         std::u16string* error) const {
-  DCHECK(IsChild());
+  DCHECK(IsSubjectToParentalControls());
   ExtensionState result = GetExtensionState(*extension);
   bool may_load = result != ExtensionState::BLOCKED;
   if (!may_load && error)
@@ -609,7 +610,7 @@ bool SupervisedUserService::MustRemainDisabled(
     const Extension* extension,
     extensions::disable_reason::DisableReason* reason,
     std::u16string* error) const {
-  DCHECK(IsChild());
+  DCHECK(IsSubjectToParentalControls());
   ExtensionState state = GetExtensionState(*extension);
   // Only extensions that require approval should be disabled.
   // Blocked extensions should be not loaded at all, and are taken care of
@@ -653,7 +654,7 @@ void SupervisedUserService::ChangeExtensionStateIfNecessary(
   // shouldn't be needed.
   if (!active_)
     return;
-  DCHECK(IsChild());
+  DCHECK(IsSubjectToParentalControls());
   ExtensionRegistry* registry = ExtensionRegistry::Get(profile_);
   const Extension* extension = registry->GetInstalledExtension(extension_id);
   // If the extension is not installed (yet), do nothing.
