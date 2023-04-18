254a69c4c135b4cb21ab90d8e6029fcc57fa91a0
Claudio DeSouza <cdesouza@igalia.com>
2023-04-18 13:59:00 +0000

    Replace base::GUID uses with base::Uuid (93/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /content/browser/media/capture
    
    This CL was uploaded by git cl split.
    
    R=sergeyu@chromium.org
    
    Bug: 1428566
    Change-Id: Ib120b0587d3ce4aad1c2b161c63f6e234a31c9bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430393
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131876}

--- content/browser/media/capture/crop_id_web_contents_helper.cc
+++ content/browser/media/capture/crop_id_web_contents_helper.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/functional/callback.h"
-#include "base/guid.h"
 #include "base/token.h"
+#include "base/uuid.h"
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/browser_thread.h"
@@ -25,7 +25,7 @@ namespace content {
 
 // TODO(crbug.com/1264849): Remove this protected static function.
 // See header for more details.
-base::Token CropIdWebContentsHelper::GUIDToToken(const base::GUID& guid) {
+base::Token CropIdWebContentsHelper::GUIDToToken(const base::Uuid& guid) {
   std::string lowercase = guid.AsLowercaseString();
 
   // |lowercase| is either empty, or follows the expected pattern.
@@ -74,10 +74,10 @@ std::string CropIdWebContentsHelper::ProduceCropId() {
   // enough to perform, given that `kMaxCropIdsPerWebContents` is so small
   // compared to the space of GUIDs, and it guarantees we never silently fail
   // the application by cropping to the wrong, duplicate target.
-  base::GUID guid;
+  base::Uuid guid;
   base::Token crop_id;
   do {
-    guid = base::GUID::GenerateRandomV4();
+    guid = base::Uuid::GenerateRandomV4();
     crop_id = GUIDToToken(guid);
   } while (IsAssociatedWithCropId(crop_id));
   crop_ids_.push_back(crop_id);
--- content/browser/media/capture/crop_id_web_contents_helper.h
+++ content/browser/media/capture/crop_id_web_contents_helper.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback.h"
-#include "base/guid.h"
 #include "base/token.h"
+#include "base/uuid.h"
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/navigation_handle.h"
@@ -61,7 +61,7 @@ class CONTENT_EXPORT CropIdWebContentsHelper final
   // process. It should be made visible to the browser process and reused
   // rather than redefined. It is defined as protected so that unit tests
   // can use it, too.
-  static base::Token GUIDToToken(const base::GUID& guid);
+  static base::Token GUIDToToken(const base::Uuid& guid);
 
  private:
   friend class WebContentsUserData<CropIdWebContentsHelper>;
