5dd5584ce8fd041994d84b5bd4dbafa3354cd774
Dmitry Vykochko <vykochko@google.com>
2023-04-18 08:47:33 +0000

    Manual profile import: support for initial_creator_id.
    Bug: 1421688
    Change-Id: If8b14c8aedbf5cd5c979008472144ccf1b4e8764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4432881
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1131766}

--- components/autofill/core/browser/manual_testing_profile_import.cc
+++ components/autofill/core/browser/manual_testing_profile_import.cc
@@ -35,6 +35,7 @@ constexpr auto kSourceMapping =
     base::MakeFixedFlatMap<base::StringPiece, AutofillProfile::Source>(
         {{"account", AutofillProfile::Source::kAccount},
          {"localOrSyncable", AutofillProfile::Source::kLocalOrSyncable}});
+constexpr base::StringPiece kKeyInitialCreatorId = "initial_creator_id";
 
 using FieldTypeLookupTable = base::flat_map<std::string, ServerFieldType>;
 
@@ -103,6 +104,15 @@ absl::optional<AutofillProfile> MakeProfile(
   for (const auto [key, value] : dict) {
     if (key == kKeySource) {
       continue;
+    }
+    if (key == kKeyInitialCreatorId) {
+      if (const absl::optional<int> creator_id = dict.FindInt(key)) {
+        profile.set_initial_creator_id(*creator_id);
+        continue;
+      } else {
+        LOG(ERROR) << "Incorrect value for " << key << ".";
+        return absl::nullopt;
+      }
     }
     if (!lookup_table.contains(key)) {
       LOG(ERROR) << "Unknown type " << key << ".";
