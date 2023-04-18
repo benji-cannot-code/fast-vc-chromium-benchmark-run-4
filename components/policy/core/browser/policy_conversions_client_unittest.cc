44b7c06677f370c00da6736128b71d07bd14d7c4
Rubin Deliallisi <rubindl@chromium.org>
2023-04-18 13:10:07 +0000

    Revert "[PolicyLogs] Enabling Android Policy Logs By Default"
    This reverts commit c8a426a55d61aa4610848780c06c00b794842f5f.
    
    Reason for revert: https://crbug.com/1434241
    
    Original change's description:
    > [PolicyLogs] Enabling Android Policy Logs By Default
    >
    > This CL enables the PolicyLogsPageAndroid feature by default to fully
    > launch chrome://policy/logs to M112 stable.
    >
    > Bug: b/251799119
    > Change-Id: I3a78219a29fcf62b997f4236832888493cbd9bd5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4433410
    > Commit-Queue: Salma Elmahallawy <esalma@google.com>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1131538}
    
    Bug: 1434241, b/251799119
    Change-Id: I8a9fa8f25b1a911f05eb229797ad0273aecb8467
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4438473
    Owners-Override: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131859}

--- components/policy/core/browser/policy_conversions_client_unittest.cc
+++ components/policy/core/browser/policy_conversions_client_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/policy/core/browser/policy_conversions_client.h"
 
-#include "base/test/task_environment.h"
 #include "components/policy/core/common/policy_map.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -67,7 +66,6 @@ class PolicyConversionsClientTest : public ::testing::Test {
 
 // Verify dropping default values option is working.
 TEST_F(PolicyConversionsClientTest, SetDropDefaultValues) {
-  base::test::SingleThreadTaskEnvironment task_environment;
   policy::PolicyMap policy_map;
   policy_map.Set(kPolicyName1, CreateEntry(false /* set_is_default */));
   policy_map.Set(kPolicyName2, CreateEntry(true /* set_is_default */));
