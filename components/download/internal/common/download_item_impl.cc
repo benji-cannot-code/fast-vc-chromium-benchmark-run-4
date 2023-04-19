478fe079e48dd5f77d411bb0d33a652d886cdb0b
Claudio DeSouza <cdesouza@igalia.com>
2023-04-19 12:51:22 +0000

    Replace base::GUID uses with base::Uuid (63/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /components/download/internal/common
    
    This CL was uploaded by git cl split.
    
    R=xinghuilu@chromium.org
    
    Bug: 1428566
    Change-Id: Ic59ed3c101065786daee67b06d9fbefa933f5324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430098
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132440}

--- components/download/internal/common/download_item_impl.cc
+++ components/download/internal/common/download_item_impl.cc
@@ -380,7 +380,7 @@ DownloadItemImpl::DownloadItemImpl(
   delegate_->Attach();
   DCHECK(state_ == COMPLETE_INTERNAL || state_ == INTERRUPTED_INTERNAL ||
          state_ == CANCELLED_INTERNAL);
-  DCHECK(base::IsValidUuid(guid_));
+  DCHECK(base::Uuid::ParseCaseInsensitive(guid_).is_valid());
 
   if (download_entry) {
     download_source_ = download_entry->download_source;
@@ -415,7 +415,9 @@ DownloadItemImpl::DownloadItemImpl(DownloadItemImplDelegate* delegate,
                     info.isolation_info,
                     info.save_info->range_request_from,
                     info.save_info->range_request_to),
-      guid_(info.guid.empty() ? base::GenerateUuid() : info.guid),
+      guid_(info.guid.empty()
+                ? base::Uuid::GenerateRandomV4().AsLowercaseString()
+                : info.guid),
       download_id_(download_id),
       response_headers_(info.response_headers),
       content_disposition_(info.content_disposition),
@@ -454,7 +456,7 @@ DownloadItemImpl::DownloadItemImpl(
     const std::string& mime_type,
     DownloadJob::CancelRequestCallback cancel_request_callback)
     : request_info_(url),
-      guid_(base::GenerateUuid()),
+      guid_(base::Uuid::GenerateRandomV4().AsLowercaseString()),
       download_id_(download_id),
       mime_type_(mime_type),
       original_mime_type_(mime_type),
