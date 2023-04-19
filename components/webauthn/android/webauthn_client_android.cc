d067ef24cee845eab07dd9340b30885b5988e7be
Adem Derinel <derinel@google.com>
2023-04-19 07:22:49 +0000

    [webauthn] Introduce credMan delegate for conditional request caching
    This CL aims to cache the response from prepareGetCredential call.
    Instead of piping the response all the way up to
    ChromeWebAuthnCredentialsDelegate,
    it introduces a new class, WebAuthnCredManDelegate for caching and
    consuming. This will become handy if WebView wants to use CredMan too.
    
    Follow-up CLs will consume the WebAuthnCredMan delegate by:
    - hooking into PasswordAutofillAgent::TryToShowTouchToFill
    - hooking the prefetch response in
    Fido2CredentialRequest.prefetchCredentialsViaCredMan
    
    This CL currently has no impact.
    
    Bug: 1434278
    Change-Id: I9c760f9f9c83e6b02da64722b1d516637782474d
    Low-Coverage-Reason: No Android 14 bots yet + tests will be added separately.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4432194
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132360}

--- components/webauthn/android/webauthn_client_android.cc
+++ components/webauthn/android/webauthn_client_android.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/check.h"
+#include "components/webauthn/android/webauthn_cred_man_delegate.h"
+#include "content/public/browser/web_contents.h"
 
 namespace components {
 
@@ -28,6 +30,17 @@ void WebAuthnClientAndroid::SetClient(
 WebAuthnClientAndroid* WebAuthnClientAndroid::GetClient() {
   DCHECK(g_webauthn_client);
   return g_webauthn_client;
+}
+
+void WebAuthnClientAndroid::OnCredManConditionalRequestPending(
+    content::RenderFrameHost* render_frame_host,
+    bool has_results,
+    base::RepeatingClosure full_assertion_request) {
+  auto* cred_man_delegate = WebAuthnCredManDelegate::GetRequestDelegate(
+      content::WebContents::FromRenderFrameHost(render_frame_host));
+  cred_man_delegate->OnCredManConditionalRequestPending(
+      render_frame_host, has_results, std::move(full_assertion_request));
+  return;
 }
 
 }  // namespace components
