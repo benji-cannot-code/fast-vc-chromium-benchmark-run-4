13ab14ec5dcbf81d52fe69c67fce8b9b4f7d6a4b
Thomas Nguyen <tungnh@chromium.org>
2023-04-19 08:34:47 +0000

    Rename metrics from pageload to navigation
    Bug: 1431108
    Change-Id: I3c230f45ec80b3e27664e3eb3673ffbdc2ab5203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4438813
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132380}

--- components/permissions/permission_uma_util.cc
+++ components/permissions/permission_uma_util.cc
@@ -1544,7 +1544,7 @@ void PermissionUmaUtil::RecordCrossOriginFrameActionAndPolicyConfiguration(
 }
 
 // static
-void PermissionUmaUtil::RecordTopLevelPermissionsHeaderPolicyOnPageLoad(
+void PermissionUmaUtil::RecordTopLevelPermissionsHeaderPolicyOnNavigation(
     content::RenderFrameHost* render_frame_host) {
   DCHECK(render_frame_host);
   static constexpr ContentSettingsType kContentSettingsTypesForMetrics[] = {
@@ -1557,7 +1557,7 @@ void PermissionUmaUtil::RecordTopLevelPermissionsHeaderPolicyOnPageLoad(
     DCHECK(feature.has_value());
     base::UmaHistogramEnumeration(
         base::StrCat(
-            {"Permissions.Experimental.PageLoad.",
+            {"Permissions.Experimental.PrimaryMainNavigationFinished.",
              PermissionUtil::GetPermissionString(content_settings_type),
              ".TopLevelHeaderPolicy"}),
         GetTopLevelPermissionHeaderPolicyForUMA(render_frame_host,
