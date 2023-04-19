c32b8c6aaf8f447eb87f74d17ebd876dd8f1c59e
Yuheng Huang <yuhengh@chromium.org>
2023-04-19 18:47:21 +0000

    WebUI tab strip: update tab round corner radius for GM3
    Update tab round corner radius from 8px to 12px for GM3.
    
    Screenshot with GM3 enabled:
    https://screenshot.googleplex.com/AJqWLrxRuc8VZxi
    
    Screenshot without GM3 enabled:
    https://screenshot.googleplex.com/BpcKpRB46UHVt8z
    
    Change-Id: I065881cb7468368290e28d724d84866cbdfe0e5f
    Bug: 1426587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442408
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132610}

--- chrome/browser/ui/webui/tab_strip/tab_strip_ui.cc
+++ chrome/browser/ui/webui/tab_strip/tab_strip_ui.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_message_handler.h"
 #include "content/public/common/url_constants.h"
 #include "ui/base/theme_provider.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/webui/color_change_listener/color_change_handler.h"
@@ -58,6 +59,9 @@ TabStripUI::TabStripUI(content::WebUI* web_ui)
 
   html_source->AddString("tabIdDataType", kWebUITabIdDataType);
   html_source->AddString("tabGroupIdDataType", kWebUITabGroupIdDataType);
+  html_source->AddString(
+      "chromeRefresh2023Attribute",
+      features::IsChromeRefresh2023() ? "chrome-refresh-2023" : "");
 
   static constexpr webui::LocalizedString kStrings[] = {
       {"tabListTitle", IDS_ACCNAME_TAB_LIST},
