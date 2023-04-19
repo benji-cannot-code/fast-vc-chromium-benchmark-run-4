e3b34e82a3f06b51a1dfec2ffdcb79d7154ff501
Irem Uguz <iremuguz@google.com>
2023-04-19 14:20:00 +0000

    Set upload_parameters of LogUploadEvent as JSON instead of XML
    Update the format of upload_parameters for LogUploadEvent as JSON
    string. File Storage Server will accept the metadata as JSON after
    cl/524837637, so the client also needs to send metadata as JSON.
    
    Bug: b:277017666
    Change-Id: I12b8eedad2e2b27108424d011238b92c0b391eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430621
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Miriam Polzer <mpolzer@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132483}

--- chrome/browser/ash/policy/remote_commands/device_command_fetch_support_packet_job_unittest.cc
+++ chrome/browser/ash/policy/remote_commands/device_command_fetch_support_packet_job_unittest.cc
@@ -54,6 +54,10 @@ constexpr char kCommandPayload[] =
       }
     })";
 
+constexpr char kExpectedUploadParametersFormatter[] =
+    R"({"Command-ID":"%ld","File-Type":"support_file","Filename":"%s"}
+application/json)";
+
 em::RemoteCommand GenerateCommandProto(RemoteCommandJob::UniqueIDType unique_id,
                                        base::TimeTicks command_start_time,
                                        std::string payload) {
@@ -150,7 +154,7 @@ TEST_F(DeviceCommandFetchSupportPacketTest, Success) {
 
   // Check the contents of LogUploadEvent that the job enqueued.
   std::string expected_upload_parameters =
-      base::StringPrintf(kUploadParametersFormatter, kUniqueID,
+      base::StringPrintf(kExpectedUploadParametersFormatter, kUniqueID,
                          exported_file.BaseName().value().c_str());
   EXPECT_EQ(
       expected_upload_parameters,
