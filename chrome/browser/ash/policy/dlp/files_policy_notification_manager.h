95a3dcf1e336d6d6a1648b3298c710ef699a84a8
Aya ElAttar <ayaelattar@chromium.org>
2023-04-19 14:03:07 +0000

    Instantiate FilesPolicyNotificationManager
    Instantiate FilesPolicyNotificationManager class
    which will be responsible for showing warning/block notifications
    for files
    
    Bug: b/277035082
    Change-Id: Ic8356de17bd4552a8dd4b49f651b2107b23377e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400962
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132475}

---
+++ chrome/browser/ash/policy/dlp/files_policy_notification_manager.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_DLP_FILES_POLICY_NOTIFICATION_MANAGER_H_
+#define CHROME_BROWSER_ASH_POLICY_DLP_FILES_POLICY_NOTIFICATION_MANAGER_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
+
+namespace policy {
+
+// FilesPolicyNotificationManager is responsible for showing block and warning
+// notifications/dialogs for files because of DLP and enterprise connectors
+// policies.
+class FilesPolicyNotificationManager : public KeyedService {
+ public:
+  explicit FilesPolicyNotificationManager(content::BrowserContext* context);
+
+  FilesPolicyNotificationManager(const FilesPolicyNotificationManager&) =
+      delete;
+  FilesPolicyNotificationManager& operator=(
+      const FilesPolicyNotificationManager&) = delete;
+
+  ~FilesPolicyNotificationManager() override;
+};
+
+}  // namespace policy
+
+#endif  // CHROME_BROWSER_ASH_POLICY_DLP_FILES_POLICY_NOTIFICATION_MANAGER_H_
