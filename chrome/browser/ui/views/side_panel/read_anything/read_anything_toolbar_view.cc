cfd0ed6350beb79d44654f869ebea6f9569775a9
Kristi Saney <kristislee@google.com>
2023-04-19 01:41:24 +0000

    Make the toolbar navigable by arrow keys.
    Behavior before: full toolbar reachable with tab key only.
    Behavior now: first item in toolbar reachable with tab key only, remaining items reachable with arrow keys only.
    
    Demo: https://drive.google.com/file/d/1biuKWXUCIeqioJ04APphux4RlnjtGmlh/view?usp=sharing&resourcekey=0-z4NMUDebcXWc08H-AFOMnA
    Bug: 1266555
    Change-Id: I16050ae28dd9d317133145df86b84f44af1c194c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4429233
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132283}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_toolbar_view.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_toolbar_view.cc
@@ -52,6 +52,7 @@ ReadAnythingToolbarView::ReadAnythingToolbarView(
   combobox->SetProperty(
       views::kFlexBehaviorKey,
       views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToMinimum));
+  combobox->SetGroup(kToolbarGroupId);
 
   // Create the decrease/increase text size buttons.
   auto decrease_size_button = std::make_unique<ReadAnythingButtonView>(
@@ -62,6 +63,7 @@ ReadAnythingToolbarView::ReadAnythingToolbarView(
           IDS_READING_MODE_DECREASE_FONT_SIZE_BUTTON_LABEL));
   decrease_size_button->SetProperty(views::kCrossAxisAlignmentKey,
                                     views::LayoutAlignment::kCenter);
+  decrease_size_button->SetGroup(kToolbarGroupId);
 
   auto increase_size_button = std::make_unique<ReadAnythingButtonView>(
       base::BindRepeating(&ReadAnythingToolbarView::IncreaseFontSizeCallback,
@@ -71,6 +73,7 @@ ReadAnythingToolbarView::ReadAnythingToolbarView(
           IDS_READING_MODE_INCREASE_FONT_SIZE_BUTTON_LABEL));
   increase_size_button->SetProperty(views::kCrossAxisAlignmentKey,
                                     views::LayoutAlignment::kCenter);
+  increase_size_button->SetGroup(kToolbarGroupId);
 
   // Create theme selection menubutton.
   auto colors_button = std::make_unique<ReadAnythingMenuButton>(
@@ -79,6 +82,7 @@ ReadAnythingToolbarView::ReadAnythingToolbarView(
       kPaletteIcon,
       l10n_util::GetStringUTF16(IDS_READING_MODE_COLORS_COMBOBOX_LABEL),
       delegate_->GetColorsModel());
+  colors_button->SetGroup(kToolbarGroupId);
 
   // Create line spacing menubutton.
   auto line_spacing_button = std::make_unique<ReadAnythingMenuButton>(
@@ -87,6 +91,7 @@ ReadAnythingToolbarView::ReadAnythingToolbarView(
       kReadAnythingLineSpacingIcon,
       l10n_util::GetStringUTF16(IDS_READING_MODE_LINE_SPACING_COMBOBOX_LABEL),
       delegate_->GetLineSpacingModel());
+  line_spacing_button->SetGroup(kToolbarGroupId);
 
   // Create letter spacing menubutton.
   auto letter_spacing_button = std::make_unique<ReadAnythingMenuButton>(
@@ -95,6 +100,7 @@ ReadAnythingToolbarView::ReadAnythingToolbarView(
       kReadAnythingLetterSpacingIcon,
       l10n_util::GetStringUTF16(IDS_READING_MODE_LETTER_SPACING_COMBOBOX_LABEL),
       delegate_->GetLetterSpacingModel());
+  letter_spacing_button->SetGroup(kToolbarGroupId);
 
   // Add all views as children.
   font_combobox_ = AddChildView(std::move(combobox));
