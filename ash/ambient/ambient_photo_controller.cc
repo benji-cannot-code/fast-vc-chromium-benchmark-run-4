5ceeb274cbabbd030d1db5a00703b70fe3a73b64
Claudio DeSouza <cdesouza@igalia.com>
2023-04-19 17:53:29 +0000

    Replace base::GUID uses with base::Uuid (49/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /ash/ambient
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: Iec62b3303963ec93081b601a6e4f567862e5035f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430092
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132584}

--- ash/ambient/ambient_photo_controller.cc
+++ ash/ambient/ambient_photo_controller.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/guid.h"
 #include "base/hash/sha1.h"
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
--- ash/ambient/backdrop/ambient_backend_controller_impl.cc
+++ ash/ambient/backdrop/ambient_backend_controller_impl.cc
@@ -27,9 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
-#include "base/guid.h"
 #include "base/logging.h"
 #include "base/time/time.h"
+#include "base/uuid.h"
 #include "chromeos/assistant/internal/ambient/backdrop_client_config.h"
 #include "chromeos/assistant/internal/ambient/utils.h"
 #include "chromeos/assistant/internal/proto/backdrop/backdrop.pb.h"
@@ -99,7 +99,7 @@ std::string GetClientId() {
   std::string client_id =
       prefs->GetString(ambient::prefs::kAmbientBackdropClientId);
   if (client_id.empty()) {
-    client_id = base::GenerateGUID();
+    client_id = base::Uuid::GenerateRandomV4().AsLowercaseString();
     prefs->SetString(ambient::prefs::kAmbientBackdropClientId, client_id);
   }
 
