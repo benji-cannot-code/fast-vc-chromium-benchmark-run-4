2860c02d0c64a59901a29c99f00479762baf94d4
Fergus Dall <sidereal@google.com>
2023-04-19 05:10:08 +0000

    bruschetta: Use edk2-ovmf-dlc when installing
    This removes the last remaining need to store anything in user
    directories.
    
    Bug: b:264495396
    Change-Id: I420bc4fc2222577abae7f8b2e9390c67f52a1775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431958
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Emil Mikulic <easy@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132328}

--- chrome/browser/ash/bruschetta/bruschetta_util.h
+++ chrome/browser/ash/bruschetta/bruschetta_util.h
@@ -16,6 +16,7 @@ class Profile;
 namespace bruschetta {
 
 extern const char kToolsDlc[];
+extern const char kUefiDlc[];
 
 extern const char kBruschettaVmName[];
 extern const char kBruschettaDisplayName[];
