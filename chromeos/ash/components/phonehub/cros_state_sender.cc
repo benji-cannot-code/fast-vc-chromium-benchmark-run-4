a36747ab1e115aad55714f4257713118e24afd45
Pu Shi <pushi@google.com>
2023-04-19 20:47:07 +0000

    [PhoneHub] Add metric to record time for generating attestation
    This metric is to keep record of the time spent for generating attestation before sending message. We will use this metric to decide whether we need to cache the attestation certificate.
    
    Tested: manually verified on chrome://histogram that the metric is emitted as expected and average time is ~4 seconds on test device.
    Bug: b/278785681
    Change-Id: I58fca512eed9ce28b38f3cf4bdd02de64cf7d3c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443471
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132668}

--- chromeos/ash/components/phonehub/cros_state_sender.cc
+++ chromeos/ash/components/phonehub/cros_state_sender.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/phonehub/cros_state_sender.h"
 #include "ash/constants/ash_features.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "chromeos/ash/components/multidevice/logging/logging.h"
 #include "chromeos/ash/components/phonehub/message_sender.h"
 #include "chromeos/ash/components/phonehub/phone_model.h"
@@ -104,6 +105,7 @@ void CrosStateSender::PerformUpdateCrosState() {
     return;
   }
 
+  attestation_generating_start_time_ = base::Time::Now();
   attestation_certificate_generator_->GenerateCertificate(
       base::BindRepeating(&CrosStateSender::OnAttestationCertificateGenerated,
                           weak_ptr_factory_.GetWeakPtr()));
@@ -113,10 +115,16 @@ void CrosStateSender::OnAttestationCertificateGenerated(
     const std::vector<std::string>& attestation_certs,
     bool is_valid) {
   if (!is_valid) {
+    base::UmaHistogramLongTimes(
+        "PhoneHub.Attestation.GeneratingTime.Invalid",
+        base::Time::Now() - attestation_generating_start_time_);
     SendCrosStateMessage(/*attestation_certs=*/nullptr);
     return;
   }
 
+  base::UmaHistogramLongTimes(
+      "PhoneHub.Attestation.GeneratingTime",
+      base::Time::Now() - attestation_generating_start_time_);
   SendCrosStateMessage(std::move(&attestation_certs));
 }
 
