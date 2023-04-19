5009d505f3e65632ca57d4e71814e8dd4ca4e03f
Claudio DeSouza <cdesouza@igalia.com>
2023-04-19 19:01:14 +0000

    Replace base::GUID uses with base::Uuid (96/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /content/browser/preloading/prefetch
    
    This CL was uploaded by git cl split.
    
    R=curranmax@chromium.org
    
    Bug: 1428566
    Change-Id: I74dd489f9b47354bf6d53c5035bd8f4a9a355747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430395
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132616}

--- content/browser/preloading/prefetch/prefetch_canary_checker.cc
+++ content/browser/preloading/prefetch/prefetch_canary_checker.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
