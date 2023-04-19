eaac7409b022df114602f487fb3b9d9d284b2b15
Louis Romero <lpromero@google.com>
2023-04-19 23:11:44 +0000

    Fix layouts of Inactive Tabs UIs
    This CL fixes:
    - AutoLayout conflicts with both the button header and the preamble
      header in GridViewController.
    - AutoLayout conflict with the Inactive Tabs bottom toolbar before it is
      first laid out.
    
    Video: https://drive.google.com/file/d/1m7S_rD7oElOrYq62d_MQU6JVQkzBmJZe
    
    Fixed: 1434822
    Change-Id: I8611a556ff71f00c1c30c8dccbf1c52e98f8bce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4439617
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1132761}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_view_controller.mm
@@ -68,29 +68,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.view addSubview:_navigationBar];
 
   // Add the bottom toolbar with the Close All Inactive button.
-  NSString* buttonTitle =
-      l10n_util::GetNSString(IDS_IOS_INACTIVE_TABS_CLOSE_ALL_BUTTON);
-  __weak __typeof(self) weakSelf = self;
-  UIAction* closeAllInactiveAction =
-      [UIAction actionWithTitle:buttonTitle
-                          image:nil
-                     identifier:nil
-                        handler:^(UIAction* action) {
-                          [weakSelf didTapCloseAllInactive];
-                        }];
-  _closeAllInactiveButton =
-      [[UIBarButtonItem alloc] initWithPrimaryAction:closeAllInactiveAction];
-  _closeAllInactiveButton.accessibilityIdentifier = kInactiveTabGridIdentifier;
-
   _bottomBar = [[UIToolbar alloc] init];
   _bottomBar.barStyle = UIBarStyleBlack;
   _bottomBar.translucent = YES;
   _bottomBar.tintColor = [UIColor colorNamed:kRed500Color];
-  UIBarButtonItem* flexibleSpace = [[UIBarButtonItem alloc]
-      initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
-                           target:nil
-                           action:nil];
-  _bottomBar.items = @[ flexibleSpace, _closeAllInactiveButton, flexibleSpace ];
   _bottomBar.translatesAutoresizingMaskIntoConstraints = NO;
   [self.view addSubview:_bottomBar];
 
@@ -111,6 +92,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_bottomBar.bottomAnchor
         constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor],
   ]];
+
+  // Let the bottom bar lay itself out before setting the items, as otherwise it
+  // spits out AutoLayout constraints conflicts.
+  [_bottomBar layoutIfNeeded];
+  NSString* buttonTitle =
+      l10n_util::GetNSString(IDS_IOS_INACTIVE_TABS_CLOSE_ALL_BUTTON);
+  __weak __typeof(self) weakSelf = self;
+  UIAction* closeAllInactiveAction =
+      [UIAction actionWithTitle:buttonTitle
+                          image:nil
+                     identifier:nil
+                        handler:^(UIAction* action) {
+                          [weakSelf didTapCloseAllInactive];
+                        }];
+  _closeAllInactiveButton =
+      [[UIBarButtonItem alloc] initWithPrimaryAction:closeAllInactiveAction];
+  _closeAllInactiveButton.accessibilityIdentifier = kInactiveTabGridIdentifier;
+  UIBarButtonItem* flexibleSpace = [[UIBarButtonItem alloc]
+      initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                           target:nil
+                           action:nil];
+  _bottomBar.items = @[ flexibleSpace, _closeAllInactiveButton, flexibleSpace ];
 }
 
 - (void)viewDidLayoutSubviews {
