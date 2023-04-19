fe40beaac6054987a6fb5a5bf1e4201b96538e5f
David Bienvenu <davidbienvenu@chromium.org>
2023-04-19 23:21:49 +0000

    win: skip pixel tests if in dark mode
    The gold files for pixel tests are all light mode. Skip pixel tests
    if host is in dark mode, on Windows.
    
    Bug: 1429080, 1429079
    Change-Id: I48281ffc7a888190b2db7b6efae8c44d74079ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4449387
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1132768}

--- chrome/browser/ui/test/test_browser_ui.cc
+++ chrome/browser/ui/test/test_browser_ui.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "ui/aura/test/ui_controls_factory_aura.h"
 #include "ui/base/test/ui_controls.h"
+#include "ui/base/ui_base_switches.h"
 
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
@@ -120,6 +121,15 @@ void TestBrowserUi::SetPixelMatchAlgorithm(
 
 void TestBrowserUi::ShowAndVerifyUi() {
   PreShow();
+#if BUILDFLAG(IS_WIN)
+  // Gold files for pixel tests are for light mode, so if dark mode is not
+  // forced, and host is in dark mode, skip test.
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kForceDarkMode) &&
+      ui::NativeTheme::GetInstanceForNativeUi()->ShouldUseDarkColors()) {
+    GTEST_SKIP() << "Host is in dark mode; skipping test";
+  }
+#endif  // BUILDFLAG(IS_WIN)
   ShowUi(NameFromTestCase());
   ASSERT_TRUE(VerifyUi());
   if (IsInteractiveUi())
