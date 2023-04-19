a3ef99b460be1ccfd5d1b115c75618faac00c75e
Alexander Hartl <alexanderhartl@google.com>
2023-04-19 12:39:46 +0000

    Add screenplay id to PlatformKeys browser tests
    BUG=None
    
    Change-Id: I8890ad79512e5252ffb3255da1314982de6aa67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4444630
    Commit-Queue: Alexander Hartl <alexanderhartl@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132436}

--- chrome/browser/extensions/api/platform_keys/platform_keys_apitest_nss.cc
+++ chrome/browser/extensions/api/platform_keys/platform_keys_apitest_nss.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/gtest_tags.h"
 #include "chrome/browser/ash/platform_keys/key_permissions/key_permissions_service.h"
 #include "chrome/browser/ash/platform_keys/key_permissions/key_permissions_service_factory.h"
 #include "chrome/browser/chromeos/platform_keys/extension_key_permissions_service.h"
@@ -74,6 +75,8 @@ class PlatformKeysTest : public PlatformKeysTestBase {
   PlatformKeysTest& operator=(const PlatformKeysTest&) = delete;
 
   void SetUpOnMainThread() override {
+    base::AddTagToTestResult("feature_id",
+                             "screenplay-63f95a00-bff8-4d81-9cf9-ccf5fdacbef0");
     if (!IsPreTest()) {
       // Set up the private slot before
       // |PlatformKeysTestBase::SetUpOnMainThread| triggers the user sign-in.
