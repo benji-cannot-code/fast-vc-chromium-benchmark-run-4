302f71d0e40d5aa25bbdfe4fa0452695aa229499
Gavin Williams <gavinwill@chromium.org>
2023-04-19 22:23:37 +0000

    Printer settings: Show printer icon for saved printers
    - Add the set of printer status icons to the OS settings icon repo
    - Show the default gray printer status icon for saved printers
    - Later updates will choose the correct icon based on the printer state
    
    Bug: b:278621575
    Test: browser_tests --gtest_filter=*CupsPrinterEntry*
    Change-Id: I3f667e51a99b34a7643ff7c6dae6fb03ec3c3e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443253
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132733}

--- chrome/browser/resources/settings/chromeos/os_printing_page/cups_printers_entry.ts
+++ chrome/browser/resources/settings/chromeos/os_printing_page/cups_printers_entry.ts
@@ -55,6 +55,17 @@ export class SettingsCupsPrintersEntryElement extends
         type: Boolean,
         value: false,
       },
+
+      /**
+       * True when the "printer-settings-revamp" feature flag is enabled.
+       */
+      isPrinterSettingsRevampEnabled_: {
+        type: Boolean,
+        value: () => {
+          return loadTimeData.getBoolean('isPrinterSettingsRevampEnabled');
+        },
+        readOnly: true,
+      },
     };
   }
 
@@ -62,6 +73,7 @@ export class SettingsCupsPrintersEntryElement extends
   savingPrinter: boolean;
   subtext: string;
   userPrintersAllowed: boolean;
+  private isPrinterSettingsRevampEnabled_: boolean;
 
   /**
    * Fires a custom event when the menu button is clicked. Sends the details of
@@ -125,6 +137,10 @@ export class SettingsCupsPrintersEntryElement extends
     return this.printerEntry.printerType === PrinterType.PRINTSERVER;
   }
 
+  private isSavedPrinter_(): boolean {
+    return this.printerEntry.printerType === PrinterType.SAVED;
+  }
+
   private isConfigureDisabled_(): boolean {
     return !this.userPrintersAllowed || this.savingPrinter;
   }
@@ -137,6 +153,10 @@ export class SettingsCupsPrintersEntryElement extends
   private getSetupButtonAria_(): string {
     return loadTimeData.getStringF(
         'setupPrinterAria', this.printerEntry.printerInfo.printerName);
+  }
+
+  private showPrinterStatusIcon_(): boolean {
+    return this.isSavedPrinter_() && this.isPrinterSettingsRevampEnabled_;
   }
 }
 
