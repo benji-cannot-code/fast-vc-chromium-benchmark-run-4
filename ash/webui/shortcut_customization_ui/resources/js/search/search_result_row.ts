b0463c440536259ab3a9c00d13c0183e2f821f64
Ashley Prasad <ashleydp@google.com>
2023-04-19 20:35:37 +0000

    shortcut-customization: add jelly overrides
    - Add 'jelly-enabled' to allow overrides to identify when jelly is
      enabled in CSS.
    - Override input_key colors for jelly.
    - Override search result colors for jelly.
    
    Screenshot w/ jelly: http://screen/9PLcTqTZmuKG5i4.png
    Screenshot w/o jelly: http://screen/Aq4boNLapVkjAGL.png
    
    Bug: b:275718921
    Test: Manual verified UI.
    Change-Id: Ia0f7433e52d436af018b9ff9380fe32bb9b9836d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4447580
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132662}

--- ash/webui/shortcut_customization_ui/resources/js/search/search_result_row.ts
+++ ash/webui/shortcut_customization_ui/resources/js/search/search_result_row.ts
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+import 'chrome://resources/cr_elements/chromeos/cros_color_overrides.css.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import '../text_accelerator.js';
 
