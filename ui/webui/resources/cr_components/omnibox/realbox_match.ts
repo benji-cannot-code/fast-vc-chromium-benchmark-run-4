ec04edbb93d3f58df776db0f9b47c3e87fedec0f
Orin Jaworski <orinj@google.com>
2023-04-19 23:12:50 +0000

    [omnibox][realbox] Prevent possible match mis-indexing by checking URL
    This CL improves realbox UI & native interop by checking match URL
    when taking it by index from the result. For more details, see
    discussion here:
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=1432777
    
    Bug: 1408506, 1431513
    Change-Id: I51e6336225e6d1ccf8303fdbbe5ae2766cc9f0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443020
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132763}

--- ui/webui/resources/cr_components/omnibox/realbox_match.ts
+++ ui/webui/resources/cr_components/omnibox/realbox_match.ts
@@ -204,7 +204,7 @@ export class RealboxMatchElement extends PolymerElement {
    */
   private executeAction_(e: MouseEvent|KeyboardEvent) {
     this.pageHandler_.executeAction(
-        this.matchIndex, mojoTimeTicks(Date.now()),
+        this.matchIndex, this.match.destinationUrl, mojoTimeTicks(Date.now()),
         (e as MouseEvent).button || 0, e.altKey, e.ctrlKey, e.metaKey,
         e.shiftKey);
   }
@@ -248,7 +248,8 @@ export class RealboxMatchElement extends PolymerElement {
 
   private onMatchMouseDown_() {
     this.pageHandler_.onNavigationLikely(
-        this.matchIndex, NavigationPredictor.kMouseDown);
+        this.matchIndex, this.match.destinationUrl,
+        NavigationPredictor.kMouseDown);
   }
 
   private onRemoveButtonClick_(e: MouseEvent) {
