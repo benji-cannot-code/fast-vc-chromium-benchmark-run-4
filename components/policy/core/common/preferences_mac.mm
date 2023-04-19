4703318a771ab7393e802bd23018d69dfcc79f1d
Avi Drissman <avi@chromium.org>
2023-04-19 13:45:50 +0000

    Re-add support for ARC to scoped_nsobject
    scoped_nsobject was given ARC support when Chrome iOS was converted to
    ARC. When Chrome Mac didn't follow, scoped_nsobject's ARC support was
    removed in https://crrev.com/c/2082385 to simplify the code.
    
    Now that Chrome Mac is being converted to use ARC, that support is now
    needed again. Therefore, this CL effectively reverts that CL to regain
    that ARC support.
    
    The differences here:
    - ScopedBlock is not changed and remains incompatible with ARC.
      ScopedBlock is not used in any header file so it is not in the way
      of converting files by groups.
    - Direct calls to the ObjC runtime are used to help with the eventual
      ARCification of base/.
    
    Bug: 1280317
    Change-Id: I32fa20db27e0aafd9f43d522afe463ba8d93aeeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387456
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132468}

--- components/policy/core/common/preferences_mac.mm
+++ components/policy/core/common/preferences_mac.mm
@@ -36,7 +36,7 @@ namespace {
 base::scoped_nsobject<_CFXPreferences> CreateCFXPrefs() {
   Class prefs_class = NSClassFromString(@"_CFXPreferences");
   if (!prefs_class) {
-    return {};
+    return base::scoped_nsobject<_CFXPreferences>{};
   }
 
   return base::scoped_nsobject<_CFXPreferences>([[prefs_class alloc] init]);
@@ -45,7 +45,7 @@ base::scoped_nsobject<_CFXPreferences> CreateCFXPrefs() {
 base::scoped_nsobject<CFPrefsManagedSource>
 CreateCFPrefsManagedSourceForMachine(CFStringRef application_id, id cfx_prefs) {
   if (!cfx_prefs) {
-    return {};
+    return base::scoped_nsobject<CFPrefsManagedSource>{};
   }
 
   Class source_class = NSClassFromString(@"CFPrefsManagedSource");
@@ -54,7 +54,7 @@ CreateCFPrefsManagedSourceForMachine(CFStringRef application_id, id cfx_prefs) {
           instancesRespondToSelector:@selector
           (initWithDomain:user:byHost:containerPath:containingPreferences:)] ||
       ![source_class instancesRespondToSelector:@selector(copyValueForKey:)]) {
-    return {};
+    return base::scoped_nsobject<CFPrefsManagedSource>{};
   }
 
   return base::scoped_nsobject<CFPrefsManagedSource>([[source_class alloc]
