23325baa36e018056699346da29451a50641e432
Claudio DeSouza <cdesouza@igalia.com>
2023-04-20 23:37:24 +0000

    Replace base::GUID uses with base::Uuid (186/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ui/views/tabs
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1428566
    Change-Id: Id903bd1cd6715ea8f1f5d978374b00472199e04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454518
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1133463}

--- chrome/browser/ui/views/tabs/tab_drag_controller.h
+++ chrome/browser/ui/views/tabs/tab_drag_controller.h
@@ -12,11 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback.h"
-#include "base/guid.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/timer/timer.h"
+#include "base/uuid.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/tabs/tab_drag_context.h"
@@ -665,7 +665,7 @@ class TabDragController : public views::WidgetObserver,
 
   // The GUID of the saved tab group whose tracking is paused between paired
   // Detach() and Attach() calls, if dragging a saved tab group between windows.
-  absl::optional<base::GUID> paused_saved_group_id_;
+  absl::optional<base::Uuid> paused_saved_group_id_;
 
   // True until MoveAttached() is first invoked.
   bool initial_move_;
