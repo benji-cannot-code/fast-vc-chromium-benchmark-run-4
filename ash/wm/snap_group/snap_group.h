d30e3dc68db0d4c17ef2325cacfa14db8a327bea
Michele Fan <michelefan@chromium.org>
2023-04-20 21:15:30 +0000

    snap-group: Revamp split view divider in tablet and clamshell
    This CL revamps the split view divider Z order in tablet mode which
    is `kFloatingUIElement` and `kShellWindowId_AlwaysonTopContainer` to
    be on the same parent container of the top window. For the parent
    container, it will be set to the same parent container of the top window.
    
    The stacking logic now is to always keep the split view divider right
    on top of the observed windows, both of which are under the split view
    divider.
    
    This CL also centralizes the stacking order update that happens in
    the split view to be in `SplitViewDivider`.
    
    Fixed: b/272365643
    Test: Manually + added + updated existing unit tests
    Change-Id: I11f50216e64813b72dcfc80acb471faa1162ef97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408268
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133373}

--- ash/wm/snap_group/snap_group.h
+++ ash/wm/snap_group/snap_group.h
@@ -25,7 +25,6 @@ class SnapGroup : public aura::WindowObserver {
 
   // aura::WindowObserver:
   // TODO: Implement `OnWindowParentChanged`.
-  void OnWindowStackingChanged(aura::Window* window) override;
   void OnWindowDestroying(aura::Window* window) override;
 
   aura::Window* window1() const { return window1_; }
