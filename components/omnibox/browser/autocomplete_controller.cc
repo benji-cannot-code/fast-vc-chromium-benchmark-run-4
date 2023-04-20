c2be799ad63f6690e80a2253863d28636d337075
manukh <manukh@chromium.org>
2023-04-20 17:15:33 +0000

    [omnibox][post-ac][ml] Cancel pending requests in `StopHelper()`.
    Currently:
    - `StopHelper()` does not cancel debounce requests.
    - `StopHelper()` cancels ULR scoring requests ONLY if the popup is
      closed.
    
    This CL:
    - `StopHelper()` cancel both requests always.
    
    This fixes this issues:
    1) User types 'x'.
    2) An async autocomplete pass kicks off a debounce request. OR the last
       async pass kicks off a URL scoring request.
    3) Autocompletion is stopped, because e.g.: the stop timer expired, the
       user pressed the down/up/left/right arrow, the user pessed
       tab/shift+tab, the user pressed ctrl+a, etc.
    4) The pending debounce or URL scoring request completes, and the
       results are updated.
    
    Bug 1427127, 1340291, 1405555
    
    Change-Id: I441c1030c23a3c7e473befaffdd89ad63458004b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450694
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133183}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -1335,8 +1335,7 @@ void AutocompleteController::NotifyChanged() {
 
   for (Observer& obs : observers_)
     obs.OnResultChanged(this, notify_changed_default_match_);
-  notify_changed_debouncer_.CancelRequest();
-  notify_changed_default_match_ = false;
+  CancelDelayedNotifyChanged();
 }
 
 void AutocompleteController::DelayedNotifyChanged(bool notify_default_match) {
@@ -1349,6 +1348,11 @@ void AutocompleteController::DelayedNotifyChanged(bool notify_default_match) {
     notify_changed_debouncer_.RequestRun(base::BindOnce(
         &AutocompleteController::NotifyChanged, base::Unretained(this)));
   }
+}
+
+void AutocompleteController::CancelDelayedNotifyChanged() {
+  notify_changed_debouncer_.CancelRequest();
+  notify_changed_default_match_ = false;
 }
 
 void AutocompleteController::CheckIfDone() {
@@ -1403,10 +1407,13 @@ void AutocompleteController::StopHelper(bool clear_result,
   expire_timer_.Stop();
   stop_timer_.Stop();
   done_ = true;
-  if (clear_result && !result_.empty()) {
-    // Cancel the scoring model when updating `result_`.
-    CancelUrlScoringModel();
 
+  // Cancel any pending requests that may update the results. Otherwise, e.g.,
+  // the user's suggestion selection may be reset.
+  CancelDelayedNotifyChanged();
+  CancelUrlScoringModel();
+
+  if (clear_result && !result_.empty()) {
     result_.Reset();
 
     // Pass false to clear only the popup and not the edit. Passing true would,
