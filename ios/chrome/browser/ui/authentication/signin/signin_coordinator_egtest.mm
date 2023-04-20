86a1c39c54be60873c22c9685698f0b5b53ad6fb
Jérôme Lebel <jlebel@chromium.org>
2023-04-20 17:29:21 +0000

    [iOS] Add EGTests fo swipe down sign-in without an identity
    This is related to http://crbug.com/1434238.
    The fix was landed in http://crrev.com/c/4441226.
    
    Bug: 1434238
    Change-Id: If45f50617b29c7453dbb1dfed0cc6e04464533d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442267
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133201}

--- ios/chrome/browser/ui/authentication/signin/signin_coordinator_egtest.mm
+++ ios/chrome/browser/ui/authentication/signin/signin_coordinator_egtest.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_app_interface.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/ui/authentication/signin_matchers.h"
+#import "ios/chrome/browser/ui/authentication/unified_consent/unified_consent_constants.h"
 #import "ios/chrome/browser/ui/authentication/views/views_constants.h"
 #import "ios/chrome/browser/ui/bookmarks/bookmark_earl_grey.h"
 #import "ios/chrome/browser/ui/bookmarks/bookmark_earl_grey_ui.h"
@@ -1289,6 +1290,16 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
                          kRecentTabsTableViewControllerAccessibilityIdentifier),
                      grey_sufficientlyVisible(), nil)]
       assertWithMatcher:grey_notNil()];
+}
+
+// Tests to dismiss the sign-in view by swipe down without an identity.
+// See http://crbug.com/1434238.
+- (void)testSwipeDownSignInViewWithoutAnIdentity {
+  [self openSigninFromView:OpenSigninMethodFromSettings tapSettingsLink:NO];
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                          kUnifiedConsentScrollViewIdentifier)]
+      performAction:grey_swipeFastInDirection(kGREYDirectionDown)];
+  // Test no crash.
 }
 
 @end
