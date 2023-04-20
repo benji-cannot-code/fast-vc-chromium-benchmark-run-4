03f458e81168821358f8540282e41d92350f6b3b
Minoru Chikamune <chikamune@chromium.org>
2023-04-20 10:14:39 +0000

    Introduce WarmUpServiceWorker
    This CL introduces WarmUpServiceWorker function which warms-up Service
    Workers. WarmUpServiceWorker prepares the renderer process, initializes
    mojo pipes, and loads top-level scripts without evaluating the Service
    Worker's top-level script.
    
    [How warmed-up Service Worker behaves]
    
    After the service worker is warmed-up, the running_status() will be kept
    as `STARTING` status.
    
    There are the following scenarios after Service Worker is warmed-up.
    
    A) Time out (after kWarmUpDuration) => Service Worker will stop.
    B) Be requested to warm-up again => Extend the time out.
    C) Be requested to start => Call `InitializeGlobalScope`.
    
    [How WarmUpServiceWorker works]
    
    WarmUpServiceWorker postpones initializing the global scope. This is
    controlled by pause_initializing_global_scope_ flag and
    SetPauseInitializingGlobalScope() and ResumeInitializingGlobalScope()
    functions.
    
    The following trace logs demonstrate how WarmUpServiceWorker behaves.
    
    <Trace sample of WarmUpAndStartServiceWorker test>
    https://ui.perfetto.dev/#!/?s=360cc921e9f9563a26de4e7a627bc9fe8ce3bdccf7798f4d372ba71581b16313
    
    <Trace sample of WarmUpWorkerAndTimeout test>
    https://ui.perfetto.dev/#!/?s=76d3599d825e37c66797380c2696f34d9166e513e998d59d47c4cd99314cb55a
    
    [History of InitializeGlobalScope (delayed global scope initialization)]
    
    In the past, ServiceWorker::InitializeGlobalScope mojo API was used to
    prevent from evaluating a script until the byte-for-byte comparison or
    COEP check is completed (https://crrev.com/c/2060446). However, this is
    not needed after off-the-main-thread service worker script
    fetch. PlzServiceWorker CL (https://crrev.com/c/3382976) also cleaned up
    the delayed global scope initialization code. Now this CL utilizes the
    delayed global scope initialization mechanism to implement
    WarmUpServiceWorker.
    
    Bug: 1431792
    Change-Id: I5af0d1efa3e297cdaa363dcd1a5399cc73126c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411619
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133024}

--- content/browser/service_worker/service_worker_metrics.cc
+++ content/browser/service_worker/service_worker_metrics.cc
@@ -132,6 +132,8 @@ const char* EventTypeToSuffix(ServiceWorkerMetrics::EventType event_type) {
     case ServiceWorkerMetrics::EventType::
         BYPASS_ONLY_IF_SERVICE_WORKER_NOT_STARTED:
       return "_BYPASS_ONLY_IF_SERVICE_WORKER_NOT_STARTED";
+    case ServiceWorkerMetrics::EventType::WARM_UP:
+      return "_WARM_UP";
   }
   return "_UNKNOWN";
 }
@@ -201,6 +203,8 @@ const char* ServiceWorkerMetrics::EventTypeToString(EventType event_type) {
     case ServiceWorkerMetrics::EventType::
         BYPASS_ONLY_IF_SERVICE_WORKER_NOT_STARTED:
       return "Bypass Only If ServiceWorker Is Not Started";
+    case ServiceWorkerMetrics::EventType::WARM_UP:
+      return "Warm Up";
   }
   NOTREACHED() << "Got unexpected event type: " << static_cast<int>(event_type);
   return "error";
@@ -405,6 +409,9 @@ void ServiceWorkerMetrics::RecordEventDuration(EventType event,
       break;
     case EventType::PUSH_SUBSCRIPTION_CHANGE:
       // Do nothing: the histogram has been removed.
+      break;
+    case EventType::WARM_UP:
+      // Do nothing: the warm up should not be sent as an event.
       break;
     case EventType::BYPASS_MAIN_RESOURCE:
     // The bypass main resource should not be sent as an event.
