62c8599a2f9c048b34cbc5daeb5ef957057ae878
Gyuyoung Kim <gyuyoung@igalia.com>
2023-04-20 15:08:41 +0000

    [iOS] Share OSMetrics Mac implementations with iOS
    This CL shares the Mac implementations of OSMetrics with iOS port.
    Some Mac specific codes are replaced by the codes supported
    in iOS SDK to share it with iOS port.
    
    Bug: 1412528
    Change-Id: I0c32b21e3101c52ffbc802d0a20d5d5ea953fdc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431780
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133126}

--- base/ios/sim_header_shims.h
+++ base/ios/sim_header_shims.h
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/param.h>
 
 // This file includes the necessary headers that are not part of the
-// iOS public SDK in order to support multiprocess support on iOS.
+// iOS public SDK in order to support multiprocess and memory instrumentations
+// on iOS.
 
 __BEGIN_DECLS
 
@@ -41,7 +42,16 @@ const char* bootstrap_strerror(kern_return_t r);
 #define BOOTSTRAP_NO_MEMORY 1105
 #define BOOTSTRAP_NO_CHILDREN 1106
 
+// These values are copied from darwin-xnu/osfmk/mach/shared_region.h.
+// https://github.com/apple/darwin-xnu/blob/8f02f2a044b9bb1ad951987ef5bab20ec9486310/osfmk/mach/shared_region.h#L86-L87
+#define SHARED_REGION_BASE_ARM64 0x180000000ULL
+#define SHARED_REGION_SIZE_ARM64 0x100000000ULL
+
 int proc_pidpath(int pid, void* buffer, uint32_t buffersize);
+int proc_regionfilename(int pid,
+                        uint64_t address,
+                        void* buffer,
+                        uint32_t buffersize);
 #define PROC_PIDPATHINFO_MAXSIZE (4 * MAXPATHLEN)
 
 __END_DECLS
