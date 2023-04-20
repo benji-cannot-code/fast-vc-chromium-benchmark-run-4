e0786dd7fdbc66708659ffcecf9a4e99c3f85d28
Elly Fong-Jones <ellyjones@chromium.org>
2023-04-20 00:23:14 +0000

    sharing: remove the preview experiment
    This change removes all of the variants except for "enabled 16px", which
    was the launched arm. It does not remove the code to fetch high-quality
    images for the other arms - that will happen in a followup CL.
    
    Bug: 1427551
    Change-Id: I6c0123bf0a30696958e6c0d92c73c47c417103f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450690
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132824}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2689,9 +2689,6 @@ extern const char kSettingsEnableGetTheMostOutOfChromeName[];
 extern const char kSettingsEnableGetTheMostOutOfChromeDescription[];
 #endif
 
-extern const char kSharingDesktopSharePreviewName[];
-extern const char kSharingDesktopSharePreviewDescription[];
-
 extern const char kSharingDesktopShareToGoogleCollectionsName[];
 extern const char kSharingDesktopShareToGoogleCollectionsDescription[];
 
