96911cd820c402c95f7fa2e27cff8baae818c434
David Benjamin <davidben@chromium.org>
2023-04-20 18:51:37 +0000

    Set ABSL_OPTION_USE_STD_ANY to 2
    This removes our patch that forces absl::any to be its own type rather
    than, as upstream expects, a typedef of std::any. This does *not* change
    our guidance around absl::any or std::any. Both are still banned due to
    incompatibility with our components build.
    
    But since both work the same way, that's not a reason to prefer
    absl::any over std::any. It was part of the hardening patch, from the
    days when we had hardening in abseil but not libc++, but I don't see any
    asserts in absl::any. Actually, skimming std::any's API, I'm not sure
    that type has UB we'd want to harden in the first place.
    
    Thus, remove this so we carry one fewer patch to Abseil. This should
    have no impact to Chromium because neither absl::any nor std::any should
    be used in the first place.
    
    Bug: 1096380
    Change-Id: I96a88621754a3c8924ec9cf40b5d3f784d5589e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442570
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133272}

--- third_party/abseil-cpp/absl/base/options.h
+++ third_party/abseil-cpp/absl/base/options.h
@@ -95,7 +95,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // User code should not inspect this macro.  To check in the preprocessor if
 // absl::any is a typedef of std::any, use the feature macro ABSL_USES_STD_ANY.
 
-#define ABSL_OPTION_USE_STD_ANY 0
+#define ABSL_OPTION_USE_STD_ANY 2
 
 
 // ABSL_OPTION_USE_STD_OPTIONAL
