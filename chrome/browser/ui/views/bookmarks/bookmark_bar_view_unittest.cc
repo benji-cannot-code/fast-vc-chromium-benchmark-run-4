937e038c22f2b0867643fa4058e30505d6781501
Claudio DeSouza <cdesouza@igalia.com>
2023-04-20 23:35:48 +0000

    Replace base::GUID uses with base::Uuid (181/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ui/views/bookmarks
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1428566
    Change-Id: I8978adb238b18d9f22c7b5748895d231a0c7c4b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454722
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133461}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view_unittest.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view_unittest.cc
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/functional/bind.h"
-#include "base/guid.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -546,7 +546,7 @@ TEST_F(BookmarkBarViewTest, OnSavedTabGroupUpdateBookmarkBarCallsLayout) {
   keyed_service->model()->Add(SavedTabGroup(
       std::u16string(u"tab group 1"), tab_groups::TabGroupColorId::kGrey, {}));
 
-  base::GUID button_2_id = base::GUID::GenerateRandomV4();
+  base::Uuid button_2_id = base::Uuid::GenerateRandomV4();
   keyed_service->model()->Add(SavedTabGroup(std::u16string(u"tab group 2"),
                                             tab_groups::TabGroupColorId::kGrey,
                                             {}, button_2_id));
