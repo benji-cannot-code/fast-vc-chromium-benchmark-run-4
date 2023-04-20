d249636d999d10fa78f46a8ff94d9d60d541ee84
Dustin J. Mitchell <djmitche@chromium.org>
2023-04-20 17:57:42 +0000

    Add brief docs of `ServiceWorkerContainerHost::UpdateUrls`
    This also fixes some typos and old names in this class's unit tests.
    
    Bug: 1402965
    Change-Id: I7472c22cbc439cdd42818fe1a1dc4b0cf4cb9b1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4451447
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133231}

--- content/browser/service_worker/service_worker_container_host_unittest.cc
+++ content/browser/service_worker/service_worker_container_host_unittest.cc
@@ -394,7 +394,7 @@ TEST_F(ServiceWorkerContainerHostTest, MatchRegistration) {
   container_host->RemoveMatchingRegistration(registration1_.get());
   ASSERT_EQ(nullptr, container_host->MatchRegistration());
 
-  // SetDocumentUrl sets all of matching registrations
+  // UpdateUrls sets all of matching registrations
   container_host->UpdateUrls(
       GURL("https://www.example.com/example1"),
       url::Origin::Create(GURL("https://www.example.com/example1")),
@@ -404,7 +404,7 @@ TEST_F(ServiceWorkerContainerHostTest, MatchRegistration) {
   container_host->RemoveMatchingRegistration(registration2_.get());
   ASSERT_EQ(registration1_, container_host->MatchRegistration());
 
-  // SetDocumentUrl with another origin also updates matching registrations
+  // UpdateUrls with another origin also updates matching registrations
   container_host->UpdateUrls(
       GURL("https://other.example.com/example"),
       url::Origin::Create(GURL("https://other.example.com/example")),
@@ -737,7 +737,7 @@ TEST_F(ServiceWorkerContainerHostTest,
   SetBrowserClientForTesting(old_browser_client);
 }
 
-TEST_F(ServiceWorkerContainerHostTest, AllowsServiceWorker) {
+TEST_F(ServiceWorkerContainerHostTest, AllowServiceWorker) {
   // Create an active version.
   scoped_refptr<ServiceWorkerVersion> version =
       base::MakeRefCounted<ServiceWorkerVersion>(
