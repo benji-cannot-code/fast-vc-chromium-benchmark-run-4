95758ca51e60392727644a8399b1248fdc076dc2
Rob Schonberger <robsc@chromium.org>
2023-04-20 08:00:19 +0000

    [Live Caption] Clean 2 simple little changes from codebase, no impact.
    Firstly, remove a TODO for work on language changes from code that
    handles language identification.
    
    Secondly, as a cleanup, remove an explicit tag on a constructor that
    has 2 arguments, since it has no impact/meaning.
    
    Bug: b:260372471
    Change-Id: I3f15a19ee9940b4f7eaa9b2487508ee61b37c410
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4452354
    Commit-Queue: Evan Liu <evliu@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Auto-Submit: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132967}

--- chrome/browser/accessibility/live_caption/live_caption_speech_recognition_host.h
+++ chrome/browser/accessibility/live_caption/live_caption_speech_recognition_host.h
@@ -69,7 +69,7 @@ class LiveCaptionSpeechRecognitionHost
 #endif
 
  private:
-  explicit LiveCaptionSpeechRecognitionHost(
+  LiveCaptionSpeechRecognitionHost(
       content::RenderFrameHost& frame_host,
       mojo::PendingReceiver<media::mojom::SpeechRecognitionRecognizerClient>
           pending_receiver);
