26001396e873713f1f4cdd0b747793e4676c52d6
Mikel Astiz <mastiz@chromium.org>
2023-04-20 19:30:54 +0000

    ModelLoadManager: Reintroduce logic for sync-mode switches
    The logic removed in https://crrev.com/c/4396361 is now reintroduced in
    a different way and motivated by a different need: when the sync mode
    changes, it avoids problems by always clearing metadata in storage.
    
    This is the simplest to reason about since it guarantees that there is
    at most one copy of sync metadata per datatype. Otherwise, it is
    possible that one storage (account storage or full-sync storage) keeps
    a non-authoritative copy of sync metadata (including tombstones,
    progress markers, etc.) that can become arbitrarily stale.
    
    Bug: 1430450, 1431189
    Change-Id: I163741108644c38d893bc2f63af6ed1da8efaf4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4418286
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Quick-Run: Mikel Astiz <mastiz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133297}

--- components/sync/driver/model_load_manager.h
+++ components/sync/driver/model_load_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/sync_stop_metadata_fate.h"
 #include "components/sync/driver/configure_context.h"
 #include "components/sync/driver/data_type_controller.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace base {
 class ElapsedTimer;
@@ -107,7 +108,7 @@ class ModelLoadManager {
   // The delegate in charge of handling model load results.
   const raw_ptr<ModelLoadManagerDelegate> delegate_;
 
-  ConfigureContext configure_context_;
+  absl::optional<ConfigureContext> configure_context_;
 
   // Data types that are enabled.
   ModelTypeSet preferred_types_without_errors_;
