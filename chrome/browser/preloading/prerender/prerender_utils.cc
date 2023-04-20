0305c8b05289cc2f3495d3efa80dffeb45b49d90
lingqi <lingqi@chromium.org>
2023-04-20 02:38:51 +0000

    Prerender: move feature flags from chrome/common to chrome/browser
    Thanks to I7565b5db89758450ab618609518e0f778d9139e2, prerender
    feature flags in the chrome layer are no longer used by renderer
    processes, and we can move them to the browser dir to make things
    simpler.
    
    Bug: n/a
    Change-Id: I57602146ba5ee69dae3c9dd5c091e9fd3cf7867c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4438952
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132883}

--- chrome/browser/preloading/prerender/prerender_utils.cc
+++ chrome/browser/preloading/prerender/prerender_utils.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/preloading/prerender/prerender_utils.h"
 
-#include "chrome/common/chrome_features.h"
+#include "chrome/browser/browser_features.h"
 #include "third_party/blink/public/common/features.h"
 #include "ui/base/page_transition_types.h"
 
