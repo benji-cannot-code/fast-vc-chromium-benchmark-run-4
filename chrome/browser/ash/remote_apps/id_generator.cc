94102a3fe9cae6a8b47dbc8f76c4b2032a287ec4
Claudio DeSouza <cdesouza@igalia.com>
2023-04-20 22:32:04 +0000

    Replace base::GUID uses with base::Uuid (136/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ash
    
    This CL was uploaded by git cl split.
    
    R=jamescook@chromium.org
    
    Bug: 1428566
    Change-Id: If01af1ffacceb7fd6360cfd8b59ff9b203f8c74a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455074
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133422}

--- chrome/browser/ash/remote_apps/id_generator.cc
+++ chrome/browser/ash/remote_apps/id_generator.cc
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/remote_apps/id_generator.h"
 
-#include "base/guid.h"
+#include "base/uuid.h"
 
 namespace ash {
 
 std::string GuidIdGenerator::GenerateId() {
-  return base::GenerateGUID();
+  return base::Uuid::GenerateRandomV4().AsLowercaseString();
 }
 
 FakeIdGenerator::FakeIdGenerator(const std::vector<std::string>& ids)
