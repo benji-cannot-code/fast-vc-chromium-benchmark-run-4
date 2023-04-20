0033cde5ec74778a08590dad6f4b26bcef21070e
Vishwas Uppoor <vishwasuppoor@google.com>
2023-04-20 23:20:07 +0000

    [Metadata][TTFCC][Android] Always show last 4 digits in suggestions
    Card name + obfuscated last 4 digits are shown on the first line of the
    suggestion. When the nickname or the card product name is too long, it
    might overflow the view, and the last 4 digits might get hidden.
    
    Always show the last 4 digits by truncating the card name if necessary.
    
    Mock: https://www.figma.com/file/N6F3DN9prdEzQiylOtVBCW/X-PA-share?node-id=12-91635&t=7spgP5B9Mri1Unpv-0
    Current UI: https://screenshot.googleplex.com/ANkbdoogT9jCKk7
    Implementation UI: https://screenshot.googleplex.com/6VdVmgwjxefwkmg
    
    Bug: 1433540
    Change-Id: I7ac37c68e449a9ded968010c4951d786fb06fdde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4429117
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133448}

--- chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardViewTest.java
+++ chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardViewTest.java
@@ -69,6 +69,10 @@ public class TouchToFillCreditCardViewTest {
             /* number= */ "4111111111111111", /* month= */ "4", /* year= */ "2090",
             /* network= */ "Visa", /* iconId= */ 0, /* cardNameForAutofillDisplay= */ "Visa",
             /* obfuscatedLastFourDigits= */ "1111");
+    private static final CreditCard LONG_CARD_NAME_CARD =
+            createCreditCard("MJ", "4111111111111111", "5", "2050", false,
+                    "How much wood would a woodchuck chuck if a woodchuck could chuck wood",
+                    "• • • • 1111", 0, "visa");
 
     @Rule
     public final MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
@@ -233,8 +237,8 @@ public class TouchToFillCreditCardViewTest {
         });
         BottomSheetTestSupport.waitForOpen(mBottomSheetController);
 
-        TextView cardName = mTouchToFillCreditCardView.getContentView().findViewById(
-                R.id.touch_to_fill_card_name);
+        TextView cardName =
+                mTouchToFillCreditCardView.getContentView().findViewById(R.id.card_name);
         assertTrue(cardName.getContentDescription().toString().equals("Best Card visa"));
     }
 
@@ -248,9 +252,29 @@ public class TouchToFillCreditCardViewTest {
         });
         BottomSheetTestSupport.waitForOpen(mBottomSheetController);
 
-        TextView cardName = mTouchToFillCreditCardView.getContentView().findViewById(
-                R.id.touch_to_fill_card_name);
+        TextView cardName =
+                mTouchToFillCreditCardView.getContentView().findViewById(R.id.card_name);
         assertEquals(cardName.getContentDescription(), null);
+    }
+
+    @Test
+    @MediumTest
+    public void testCardNameTooLong_cardNameTruncated_lastFourDigitsAlwaysShown() {
+        runOnUiThreadBlocking(() -> {
+            mTouchToFillCreditCardModel.get(SHEET_ITEMS)
+                    .add(new ListItem(CREDIT_CARD, createCardModel(LONG_CARD_NAME_CARD)));
+            mTouchToFillCreditCardModel.set(VISIBLE, true);
+        });
+        BottomSheetTestSupport.waitForOpen(mBottomSheetController);
+
+        TextView cardName =
+                mTouchToFillCreditCardView.getContentView().findViewById(R.id.card_name);
+        TextView cardNumber =
+                mTouchToFillCreditCardView.getContentView().findViewById(R.id.card_number);
+        assertTrue(
+                cardName.getLayout().getEllipsisCount(cardName.getLayout().getLineCount() - 1) > 0);
+        assertThat(cardNumber.getLayout().getText().toString(),
+                is(LONG_CARD_NAME_CARD.getObfuscatedLastFourDigits()));
     }
 
     private RecyclerView getCreditCards() {
@@ -258,11 +282,11 @@ public class TouchToFillCreditCardViewTest {
     }
 
     private TextView getCreditCardNameAt(int index) {
-        return getCreditCards().getChildAt(index).findViewById(R.id.touch_to_fill_card_name);
+        return getCreditCards().getChildAt(index).findViewById(R.id.card_name);
     }
 
     private TextView getCreditCardNumberAt(int index) {
-        return getCreditCards().getChildAt(index).findViewById(R.id.touch_to_fill_card_number);
+        return getCreditCards().getChildAt(index).findViewById(R.id.card_number);
     }
 
     private TextView getCreditCardExpirationAt(int index) {
