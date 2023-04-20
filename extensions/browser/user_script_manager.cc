724450f1c18cd681f877429edc10251dcbcc1a62
Tim Judkins <tjudkins@chromium.org>
2023-04-20 19:51:30 +0000

    [Extensions] Code Health: upgrade DCHECKs to CHECKs in UserScriptManager
    As per new guidance these can just be CHECKs now, which also avoids
    potential null dereferences in non-DCHECK builds and instead makes them
    fail in a more controlled way with a CHECK.
    
    Bug: 1433451
    Change-Id: I2d419da86003b9f81baac3954d0ab6618acd1cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4449051
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133308}

--- extensions/browser/user_script_manager.cc
+++ extensions/browser/user_script_manager.cc
@@ -51,7 +51,7 @@ ExtensionUserScriptLoader* UserScriptManager::GetUserScriptLoaderForExtension(
   const Extension* extension = ExtensionRegistry::Get(browser_context_)
                                    ->enabled_extensions()
                                    .GetByID(extension_id);
-  DCHECK(extension);
+  CHECK(extension);
 
   auto it = extension_script_loaders_.find(extension->id());
   return (it == extension_script_loaders_.end())
@@ -117,7 +117,7 @@ void UserScriptManager::RemovePendingExtensionLoadAndSignal(
 
 ExtensionUserScriptLoader* UserScriptManager::CreateExtensionUserScriptLoader(
     const Extension* extension) {
-  DCHECK(!base::Contains(extension_script_loaders_, extension->id()));
+  CHECK(!base::Contains(extension_script_loaders_, extension->id()));
   // Inserts a new ExtensionUserScriptLoader and returns a ptr to it.
   ExtensionUserScriptLoader* loader =
       extension_script_loaders_
@@ -134,7 +134,7 @@ ExtensionUserScriptLoader* UserScriptManager::CreateExtensionUserScriptLoader(
 
 WebUIUserScriptLoader* UserScriptManager::CreateWebUIUserScriptLoader(
     const GURL& url) {
-  DCHECK(!base::Contains(webui_script_loaders_, url));
+  CHECK(!base::Contains(webui_script_loaders_, url));
   // Inserts a new WebUIUserScriptLoader and returns a ptr to it.
   WebUIUserScriptLoader* loader =
       webui_script_loaders_
