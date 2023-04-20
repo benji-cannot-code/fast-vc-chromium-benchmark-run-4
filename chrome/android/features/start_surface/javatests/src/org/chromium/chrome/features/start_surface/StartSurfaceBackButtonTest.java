a55f25235e274f0e7e3da82cafca0926ac987c71
Victor Vianna <victorvianna@google.com>
2023-04-20 15:45:45 +0000

    Revert "[Start] Enable StartSurfaceBackButtonTest#testSwipeBackOnStartSurfaceHomePage__* and StartSurfaceBackButtonTest#testSwipeBackOnStartSurfaceHomePage_BackGestureRefactor__*"
    This reverts commit 10698d1bc22cd2cbf2048b9cbb1cfc1ca7b01c49.
    
    Reason for revert: Tests still failing
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-official-tests/5164/overview
    
    Original change's description:
    > [Start] Enable StartSurfaceBackButtonTest#testSwipeBackOnStartSurfaceHomePage__* and StartSurfaceBackButtonTest#testSwipeBackOnStartSurfaceHomePage_BackGestureRefactor__*
    >
    > After adding the statement "AsyncInitializationActivity.interceptMoveTaskToBackForTesting();". We have run the tests with android-12-x64-rel, and android-nougat-x86-rel. Since we didn't find out any error with these tests, we enabled it.
    >
    > Bug: 1246457, 1371614, 1371613, 1371612
    > Change-Id: I9fcf705b898670a6adf90ce747d300856d2fada8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4432130
    > Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    > Reviewed-by: Xi Han <hanxi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1132172}
    
    Bug: 1246457, 1371614, 1371613, 1371612
    Fixed: 1434978
    Change-Id: I0a0d8cab2af35868d06221e109f55c1f6a8d7c6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454971
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133141}

--- chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceBackButtonTest.java
+++ chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceBackButtonTest.java
@@ -58,6 +58,7 @@ import org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
+import org.chromium.chrome.test.util.ChromeApplicationTestUtils;
 import org.chromium.chrome.test.util.MenuUtils;
 import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
@@ -351,6 +352,7 @@ public class StartSurfaceBackButtonTest {
     @Feature({"StartSurface"})
     // clang-format off
     @CommandLineFlags.Add({START_SURFACE_TEST_SINGLE_ENABLED_PARAMS})
+    @DisabledTest(message = "https://crbug.com/1246457")
     @DisableFeatures({ChromeFeatureList.BACK_GESTURE_REFACTOR})
     public void testSwipeBackOnStartSurfaceHomePage() throws ExecutionException {
         // clang-format on
@@ -361,6 +363,7 @@ public class StartSurfaceBackButtonTest {
     @MediumTest
     @Feature({"StartSurface"})
     @CommandLineFlags.Add({START_SURFACE_TEST_SINGLE_ENABLED_PARAMS})
+    @DisabledTest(message = "https://crbug.com/1246457")
     @EnableFeatures({ChromeFeatureList.BACK_GESTURE_REFACTOR})
     public void testSwipeBackOnStartSurfaceHomePage_BackGestureRefactor()
             throws ExecutionException {
@@ -449,7 +452,10 @@ public class StartSurfaceBackButtonTest {
         StartSurfaceTestUtils.waitForStartSurfaceVisible(mLayoutChangedCallbackHelper,
                 mCurrentlyActiveLayout, mActivityTestRule.getActivity());
 
-        StartSurfaceTestUtils.gestureNavigateBackToBringChromeBackground(mActivityTestRule);
+        StartSurfaceTestUtils.gestureNavigateBack(mActivityTestRule);
+
+        // Back gesture on the start surface puts Chrome background.
+        ChromeApplicationTestUtils.waitUntilChromeInBackground();
     }
 
     /**
