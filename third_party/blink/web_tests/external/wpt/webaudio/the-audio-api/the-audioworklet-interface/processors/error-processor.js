f2d5152505cf2061babdeca56a171bc0eca925a2
Hongchan Choi <hongchan@chromium.org>
2023-04-20 19:24:03 +0000

    Add checks before deserializing an object in AudioWorkletProcessor
    This CL adds two more steps before deserializing a transferred object
    from AudioWorkletNode to AudioWorkletProcessor:
    
    1. Use SerializedScriptValue->CanSerializeIn() check to before
       unpacking.
    2. Then use UnpackedSerializedScriptValue() to unpack.
    3. Rewrite the related WPT to simplify and remove redundant dependency.
    
    Bug: 1429681
    Change-Id: I0f24b7e92c23e2abc8402248aa5e410eb4da3786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4406357
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133289}

--- third_party/blink/web_tests/external/wpt/webaudio/the-audio-api/the-audioworklet-interface/processors/error-processor.js
+++ third_party/blink/web_tests/external/wpt/webaudio/the-audio-api/the-audioworklet-interface/processors/error-processor.js
@@ -30,5 +30,12 @@ class ProcessErrorProcessor extends AudioWorkletProcessor {
 }
 
 
+/**
+ * @class EmptyErrorProcessor
+ * @extends AudioWorkletProcessor
+ */
+class EmptyErrorProcessor extends AudioWorkletProcessor { process() {} }
+
 registerProcessor('constructor-error', ConstructorErrorProcessor);
 registerProcessor('process-error', ProcessErrorProcessor);
+registerProcessor('empty-error', EmptyErrorProcessor);
