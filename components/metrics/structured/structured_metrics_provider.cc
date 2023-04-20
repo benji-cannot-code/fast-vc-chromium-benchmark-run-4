0d8fe349be6658de487311bce810dacd21c4e829
Jong Ahn <jongahn@google.com>
2023-04-20 17:41:28 +0000

    Cleanup DelayHwidUpload experiment now that experiment is over.
    NO_BUG
    
    Change-Id: I14b112319cdbef0c74b17b72a208213193a14cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443254
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133210}

--- components/metrics/structured/structured_metrics_provider.cc
+++ components/metrics/structured/structured_metrics_provider.cc
@@ -295,11 +295,6 @@ void StructuredMetricsProvider::ProvideCurrentSessionData(
     return;
   }
 
-  if (base::FeatureList::IsEnabled(kDelayUploadUntilHwid) &&
-      !system_profile_initialized_) {
-    return;
-  }
-
   LogNumEventsInUpload(events_.get()->get()->uma_events_size());
 
   auto* structured_data = uma_proto->mutable_structured_data();
@@ -325,11 +320,6 @@ bool StructuredMetricsProvider::HasIndependentMetrics() {
     return false;
   }
 
-  if (base::FeatureList::IsEnabled(kDelayUploadUntilHwid) &&
-      !system_profile_initialized_) {
-    return false;
-  }
-
   return events_.get()->get()->non_uma_events_size() != 0;
 }
 
@@ -339,12 +329,6 @@ void StructuredMetricsProvider::ProvideIndependentMetrics(
     base::HistogramSnapshotManager*) {
   DCHECK(base::CurrentUIThread::IsSet());
   if (!recording_enabled_ || init_state_ != InitState::kInitialized) {
-    std::move(done_callback).Run(false);
-    return;
-  }
-
-  if (base::FeatureList::IsEnabled(kDelayUploadUntilHwid) &&
-      !system_profile_initialized_) {
     std::move(done_callback).Run(false);
     return;
   }
@@ -382,9 +366,9 @@ void StructuredMetricsProvider::ProvideIndependentMetrics(
 
 void StructuredMetricsProvider::ProvideSystemProfile(
     SystemProfileProto* system_profile) {
-  // Populate the proto if the system profile has been intiailzed and
-  // have a system profile provider.
-  // The field may be populated if ChromeOSMetricsProvider has already run.
+  // Populate the proto if the system profile has been initialized and
+  // has a system profile provider. The field may be populated if
+  // ChromeOSMetricsProvider has already run.
   if (system_profile_initialized_) {
     system_profile_provider_->ProvideSystemProfileMetrics(system_profile);
   }
