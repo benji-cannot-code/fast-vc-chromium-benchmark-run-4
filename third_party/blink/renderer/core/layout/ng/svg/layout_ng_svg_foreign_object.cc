ae6e6f13018ce5c535a1dd765ded3037a7a67d67
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-04-20 23:28:38 +0000

    [RemoveLegacy] Make SVG Foreign Objects create their own space.
    Behind a kill-switch LayoutNewSVGForeignObjectEntry.
    
    This will allow us to delete a bunch of mixin logic.
    There should be no behaviour change.
    
    Bug: 1229581
    Change-Id: I533035353439aa1343f9adc6a5022c3926ca7458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456338
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133451}

--- third_party/blink/renderer/core/layout/ng/svg/layout_ng_svg_foreign_object.cc
+++ third_party/blink/renderer/core/layout/ng/svg/layout_ng_svg_foreign_object.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/svg/layout_ng_svg_foreign_object.h"
 
+#include "third_party/blink/renderer/core/layout/ng/ng_block_node.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.h"
 #include "third_party/blink/renderer/core/layout/svg/svg_resources.h"
 #include "third_party/blink/renderer/core/layout/svg/transformed_hit_test_location.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
@@ -121,14 +123,13 @@ void LayoutNGSVGForeignObject::UpdateBlockLayout(bool relayout_children) {
   // This is necessary for external/wpt/inert/inert-on-non-html.html.
   // See FullyClipsContents() in fully_clipped_state_stack.cc.
   const float zoom = style.EffectiveZoom();
-  const LayoutUnit zoomed_width = LayoutUnit(viewport_.width() * zoom);
-  const LayoutUnit zoomed_height = LayoutUnit(viewport_.height() * zoom);
-  if (style.IsHorizontalWritingMode()) {
-    SetOverrideLogicalWidth(zoomed_width);
-    SetOverrideLogicalHeight(zoomed_height);
-  } else {
-    SetOverrideLogicalWidth(zoomed_height);
-    SetOverrideLogicalHeight(zoomed_width);
+  LogicalSize zoomed_size = PhysicalSize(LayoutUnit(viewport_.width() * zoom),
+                                         LayoutUnit(viewport_.height() * zoom))
+                                .ConvertToLogical(style.GetWritingMode());
+
+  if (!RuntimeEnabledFeatures::LayoutNewSVGForeignObjectEntryEnabled()) {
+    SetOverrideLogicalWidth(zoomed_size.inline_size);
+    SetOverrideLogicalHeight(zoomed_size.block_size);
   }
 
   // Use the zoomed version of the viewport as the location, because we will
@@ -144,7 +145,18 @@ void LayoutNGSVGForeignObject::UpdateBlockLayout(bool relayout_children) {
   // specifying them through CSS.
   overridden_location_ = LayoutPoint(zoomed_location);
 
-  UpdateNGBlockLayout();
+  if (RuntimeEnabledFeatures::LayoutNewSVGForeignObjectEntryEnabled()) {
+    NGConstraintSpaceBuilder builder(
+        style.GetWritingMode(), style.GetWritingDirection(),
+        /* is_new_fc */ true, /* adjust_inline_size_if_needed */ false);
+    builder.SetAvailableSize(zoomed_size);
+    builder.SetIsFixedInlineSize(true);
+    builder.SetIsFixedBlockSize(true);
+    NGBlockNode(this).Layout(builder.ToConstraintSpace());
+  } else {
+    UpdateNGBlockLayout();
+  }
+
   DCHECK(!NeedsLayout() || ChildLayoutBlockedByDisplayLock());
   const bool bounds_changed = old_frame_rect != FrameRect();
 
