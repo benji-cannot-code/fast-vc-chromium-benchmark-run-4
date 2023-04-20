26c9b975d4da51d69f0613e881c5d45262186adf
Claudio DeSouza <cdesouza@igalia.com>
2023-04-20 20:24:56 +0000

    Replace base::GUID uses with base::Uuid (203/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/services/sharing/nearby
    
    This CL was uploaded by git cl split.
    
    R=pushi@google.com
    
    Bug: 1428566
    Change-Id: Ic186a70d59ff2fdb2fe5797d4ea76af002c94f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456570
    Commit-Queue: Pu Shi <pushi@google.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133339}

--- chrome/services/sharing/nearby/platform.cc
+++ chrome/services/sharing/nearby/platform.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/nearby/src/internal/platform/implementation/platform.h"
 
-#include "base/guid.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/thread_pool.h"
 #include "chrome/services/sharing/nearby/nearby_connections.h"
