4e8ba64c11c1a20d847a259c0efc15fd92e51210
Michael Thiessen <mthiesse@chromium.org>
2023-04-20 20:23:50 +0000

    Kill switch flip for BlockFrameRenavigations
    Caused a regression with new tab external navigation on Stable, used
    commonly by Firebase Dynamic Links.
    
    Bug: 1433137
    Change-Id: Ic4f6e98cc65c986292266df622f11b1b8e222eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456289
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133337}

--- components/external_intents/android/external_intents_features.cc
+++ components/external_intents/android/external_intents_features.cc
@@ -32,7 +32,7 @@ BASE_FEATURE(kExternalNavigationDebugLogs,
 
 BASE_FEATURE(kBlockFrameRenavigations,
              "BlockFrameRenavigations",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDoNotRequireSpecializedCCTHandler,
              "DoNotRequireSpecializedCCTHandler",
