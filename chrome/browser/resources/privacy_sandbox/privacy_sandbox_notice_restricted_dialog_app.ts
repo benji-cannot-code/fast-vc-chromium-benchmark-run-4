8d1358f1dd9b6754ef02687400dfd972aa666162
Matt Reichhoff <mreichhoff@chromium.org>
2023-04-20 16:39:43 +0000

    [GARU] Add more overlay to restricted dialog
    This ensures the full text is seen, and matches the
    other privacy sandbox dialogs.
    
    Screenshot: https://screenshot.googleplex.com/8Tq8zoYnyk4Q6bg
    
    Bug: 1428506, b/277180533
    Change-Id: I52cfff35de47d70d0d868c0b5909da9a47e0dc8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442569
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133164}

--- chrome/browser/resources/privacy_sandbox/privacy_sandbox_notice_restricted_dialog_app.ts
+++ chrome/browser/resources/privacy_sandbox/privacy_sandbox_notice_restricted_dialog_app.ts
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/icons.html.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import './shared_style.css.js';
 
 import {CrScrollableMixin} from 'chrome://resources/cr_elements/cr_scrollable_mixin.js';
@@ -33,7 +35,9 @@ export class PrivacySandboxNoticeRestrictedDialogAppElement extends
 
     this.resizeAndShowNativeDialog().then(() => {
       this.updateScrollableContents();
-      this.promptActionOccurred(PrivacySandboxPromptAction.NOTICE_SHOWN);
+      this.maybeShowMoreButton().then(
+          () => this.promptActionOccurred(
+              PrivacySandboxPromptAction.NOTICE_SHOWN));
     });
   }
 
