155eb3b10eb0a0509ece6312e5f76663f39586e1
Christian Dullweber <dullweber@chromium.org>
2023-04-20 12:52:17 +0000

    SAA: Add helper to convert pattern to URL
    In order to handle SAA exceptions in PageInfo, we need to conver the
    SAA pattern to a GURL. This is complicated by SAA using a domain
    wildcard, so a simple GURL(pattern.ToString()) doesn't work. This adds
    a general purpose helper to convert a pattern to a representative
    URL that matches the pattern.
    
    Bug: 1433644
    Change-Id: Ic3db8df64d9527c541eafa37bf2e2ee63dbdc950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4446358
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133068}

--- components/content_settings/core/common/content_settings_pattern_parser.h
+++ components/content_settings/core/common/content_settings_pattern_parser.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece_forward.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 
+class GURL;
+
 namespace content_settings {
 
 class PatternParser {
@@ -23,6 +25,8 @@ class PatternParser {
                     ContentSettingsPattern::BuilderInterface* builder);
 
   static std::string ToString(
+      const ContentSettingsPattern::PatternParts& parts);
+  static GURL ToRepresentativeUrl(
       const ContentSettingsPattern::PatternParts& parts);
 };
 
