3d5c4f6a0c2ec2bda525444c175ece5a8696114f
Robert Kaplow <rkaplow@google.com>
2023-04-20 20:15:44 +0000

    Remove unused OpenFileSystem actions.
    Bug: 1435045
    Change-Id: I5f43ce62752048bf2e41f1aa3d99304cc39ed1d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455034
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133331}

--- content/browser/file_system/file_system_manager_impl.cc
+++ content/browser/file_system/file_system_manager_impl.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
-#include "base/metrics/user_metrics.h"
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
 #include "base/task/sequenced_task_runner.h"
@@ -251,11 +250,6 @@ void FileSystemManagerImpl::ContinueOpen(
     return;
   }
 
-  if (file_system_type == blink::mojom::FileSystemType::kTemporary) {
-    RecordAction(base::UserMetricsAction("OpenFileSystemTemporary"));
-  } else if (file_system_type == blink::mojom::FileSystemType::kPersistent) {
-    RecordAction(base::UserMetricsAction("OpenFileSystemPersistent"));
-  }
   context_->OpenFileSystem(
       storage_key, /*bucket=*/absl::nullopt,
       ToStorageFileSystemType(file_system_type),
