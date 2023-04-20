e7db10f9092cc4f254c3850c2ccc184d2074a496
Donn Denman <donnd@chromium.org>
2023-04-20 00:53:08 +0000

    [Sharesheet] Update custom actions criteria
    * Remove download image option from Android share sheet
    * Add copy options back to Chrome's share sheet with action polish
    
    Bug: 1410201, 1434465
    Change-Id: Idaa371a8625794f658d84536e77c7027e23e2333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4425886
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132849}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/ChromeProvidedSharingOptionsProviderBase.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/ChromeProvidedSharingOptionsProviderBase.java
@@ -254,6 +254,7 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
         }
         if (ChromeFeatureList.isEnabled(ChromeFeatureList.SHARE_SHEET_CUSTOM_ACTIONS_POLISH)) {
             mOrderedFirstPartyOptions.add(createCopyLinkFirstPartyOption());
+            maybeAddCopyFirstPartyOption();
             maybeAddLongScreenshotFirstPartyOption();
             maybeAddPrintFirstPartyOption();
             maybeAddSendTabToSelfFirstPartyOption();
@@ -269,7 +270,7 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
             maybeAddSendTabToSelfFirstPartyOption();
             maybeAddQrCodeFirstPartyOption();
             maybeAddPrintFirstPartyOption();
-            mOrderedFirstPartyOptions.add(createSaveImageFirstPartyOption());
+            maybeAddDownloadImageFirstPartyOption();
         }
     }
 
@@ -322,6 +323,10 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
         mOrderedFirstPartyOptions.add(createCopyImageFirstPartyOption());
         mOrderedFirstPartyOptions.add(createCopyFirstPartyOption());
         mOrderedFirstPartyOptions.add(createCopyTextFirstPartyOption());
+    }
+
+    protected void maybeAddDownloadImageFirstPartyOption() {
+        mOrderedFirstPartyOptions.add(createSaveImageFirstPartyOption());
     }
 
     private FirstPartyOption createCopyLinkFirstPartyOption() {
