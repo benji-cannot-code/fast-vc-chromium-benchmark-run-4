3f185a89d3c2a7416fc6538542f7cd62baa0c099
Noémie St-Onge <noemies@google.com>
2023-04-20 22:07:47 +0000

    [iOS] Handle case where there is only 1 password left in a reused group
    When passwords are identified as reused, all the credentials with the
    same password are grouped together (reused group). If all the
    passwords in that group are deleted or modified except for one, that
    one password left is still marked as reused (which is not accurate
    anymore). This CL brings a temporary solution to filter out a reused
    password if it's the only one left in its group. A better solution
    will be implemented in the future (see crbug.com/1434343).
    
    Fixed: 1434073
    Change-Id: I58959fe7e173d3a00f4c5e6f65d88e808986e9a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4436611
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133406}

--- ios/chrome/browser/ui/settings/password/password_issues/password_issues_mediator_unittest.mm
+++ ios/chrome/browser/ui/settings/password/password_issues/password_issues_mediator_unittest.mm
@@ -282,6 +282,8 @@ TEST_F(PasswordIssuesMediatorTest, TestPasswordIssuesFilteredByWarningType) {
   // Reused.
   MakeTestPasswordIssue(kExampleCom2, kUsername, kPassword,
                         InsecureType::kReused);
+  MakeTestPasswordIssue(kExampleCom3, kUsername2, kPassword,
+                        InsecureType::kReused);
   // Dismissed Compromised
   MakeTestPasswordIssue(kExampleCom3, kUsername, kPassword,
                         InsecureType::kLeaked, /*muted=*/true);
@@ -310,6 +312,8 @@ TEST_F(PasswordIssuesMediatorTest, TestPasswordIssuesFilteredByWarningType) {
   CreateMediator(WarningType::kReusedPasswordsWarning);
 
   CheckIssue(/*group=*/0, /*index=*/0, /*expected_website=*/kExample2String);
+  CheckIssue(/*group=*/0, /*index=*/1, /*expected_website=*/kExample3String,
+             /*expected_username=*/GetUsername2());
 
   EXPECT_FALSE(consumer().dismissedWarningsButtonText);
 
