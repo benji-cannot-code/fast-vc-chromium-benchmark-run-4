9665d2b3a0edc4e9a90d47be54fc279808d773c4
Nic Hollingum <hollingum@google.com>
2023-04-20 03:54:30 +0000

    Revert "[Extensions] Add component extensions to DevTools blocking policy"
    This reverts commit f13f412eba229f15e0642ecb87659325ed0d706a.
    
    Reason for revert: https://b.corp.google.com/issues/278782724
    
    Original change's description:
    > [Extensions] Add component extensions to DevTools blocking policy
    >
    > This CL adjusts the logic behind the DeveloperToolsAvailability Chrome
    > policy that disallows DevTools for force installed extensions to also
    > include component extensions which use the management permission. See:
    > https://chromeenterprise.google/policies/?policy=DeveloperToolsAvailability
    > and the associated bug for further details.
    >
    > Also adds a test case for this.
    >
    > Bug: b/274599835
    > Change-Id: I46fe6366c1a136845c15b95cb0ffd0cf20f2c092
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412168
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Tim <tjudkins@chromium.org>
    > Reviewed-by: David Bertoni <dbertoni@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1131471}
    
    Bug: b/274599835
    Change-Id: Ica510b4f209dfa3d70dc813217fd783fc065abe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4451707
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Owners-Override: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132908}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.cc
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/view_type_utils.h"
 #include "extensions/common/manifest.h"
 #include "extensions/common/mojom/view_type.mojom.h"
-#include "extensions/common/permissions/permissions_data.h"
 #include "ui/base/resource/resource_bundle.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -249,18 +248,8 @@ bool ChromeDevToolsManagerDelegate::AllowInspection(
     case Availability::kAllowed:
       return true;
     case Availability::kDisallowedForForceInstalledExtensions:
-      if (!extension) {
-        return true;
-      }
-      if (extensions::Manifest::IsPolicyLocation(extension->location())) {
-        return false;
-      }
-      if (extensions::Manifest::IsComponentLocation(extension->location()) &&
-          extension->permissions_data()->HasAPIPermission(
-              extensions::mojom::APIPermissionID::kManagement)) {
-        return false;
-      }
-      return true;
+      return !extension ||
+             !extensions::Manifest::IsPolicyLocation(extension->location());
     default:
       NOTREACHED() << "Unknown developer tools policy";
       return true;
