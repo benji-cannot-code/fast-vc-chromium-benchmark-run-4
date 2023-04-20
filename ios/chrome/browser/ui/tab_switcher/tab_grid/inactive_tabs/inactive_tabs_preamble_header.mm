149716fd8a542c02950066fce6ebd1a87d30bde4
Louis Romero <lpromero@google.com>
2023-04-20 18:47:33 +0000

    Fix Inactive Tabs UI details
    This CL:
    - adds spacing between the title and subtitle of the Inactive Tabs
      button;
    - updates its subtitle font to footnote;
    - adds more vertical padding around the preamble;
    - updates the spacings between the elements of the user education
      screen;
    - updates the font of the title of the user education screen;
    - aligns all elements to the top of the user education screen.
    
    Tab Grid:
    - https://screenshot.googleplex.com/8jRcXL2LJ2J7x7v
    Inactive Tabs Grid:
    - https://screenshot.googleplex.com/3SYcc2odNnJfmkb
    User Education:
    - https://screenshot.googleplex.com/6ZNqTrVUvP3JFay
    - https://screenshot.googleplex.com/3wgTVFz8AxpkeGP
    
    Fixed: 1434977
    Change-Id: I93bf406245b38d7da16a30b8b605b1b81ba32c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454830
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133269}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_preamble_header.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_preamble_header.mm
@@ -22,7 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Layout constants.
-const CGFloat kVerticalPadding = 8;
+const CGFloat kTopPadding = 14;
+const CGFloat kBottomPadding = 10;
 const CGFloat kHorizontalPadding = 16;
 
 }  // namespace
@@ -48,9 +49,8 @@ const CGFloat kHorizontalPadding = 16;
     _textView.adjustsFontForContentSizeCategory = YES;
     _textView.backgroundColor = [UIColor colorNamed:kGridBackgroundColor];
     _textView.textContainer.lineFragmentPadding = 0;
-    _textView.textContainerInset =
-        UIEdgeInsets(kVerticalPadding, kHorizontalPadding, kVerticalPadding,
-                     kHorizontalPadding);
+    _textView.textContainerInset = UIEdgeInsets(
+        kTopPadding, kHorizontalPadding, kBottomPadding, kHorizontalPadding);
     _textView.translatesAutoresizingMaskIntoConstraints = NO;
     [self addSubview:_textView];
     AddSameConstraints(_textView, self);
