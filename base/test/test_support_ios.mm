1df813b5abd1f66224de26ac738427f8fe9e6cd9
Dave Tapuska <dtapuska@chromium.org>
2023-04-20 17:43:36 +0000

    [ios] Speed up process termination of child test processes
    The 2s sleep is not necessary for the spawning test
    launcher and just slows down our test execution
    
    Bug: 1427365
    Change-Id: I81cbf1e7965944414579c898b90f2bc32e1ba483
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455467
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133212}

--- base/test/test_support_ios.mm
+++ base/test/test_support_ios.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/google_test_runner_delegate.h"
 #include "base/test/test_suite.h"
 #include "base/test/test_switches.h"
+#include "build/blink_buildflags.h"
 #include "testing/coverage_util_ios.h"
 
 // Springboard will kill any iOS app that fails to check in after launch within
@@ -227,9 +228,13 @@ bool IsSceneStartupEnabled() {
 
   int exitStatus = [self runGoogleTests];
 
+  // The blink code path uses a spawning test launcher and this wait isn't
+  // really necessary for that code path.
+#if !BUILDFLAG(USE_BLINK)
   // If a test app is too fast, it will exit before Instruments has has a
   // a chance to initialize and no test results will be seen.
   [NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:2.0]];
+#endif
   _window.reset();
 
   // Use the hidden selector to try and cleanly take down the app (otherwise
