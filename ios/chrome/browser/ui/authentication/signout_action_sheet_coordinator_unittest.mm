47318f54f527a1d283a835fe41835c6d343aa1a9
Sylvain Defresne <sdefresne@chromium.org>
2023-04-20 11:06:57 +0000

    Reland "[sync] Remove SyncSetupService::IsInitialSetupOngoing()"
    This is a reland of commit cdc9b7baafd88de3a956dcc3ec941d8db4f2e093
    
    Restore the IsInitialSetupOngoing() method (marking it as deprecated)
    as it is still used in the internal repository.
    
    Original change's description:
    > [sync] Remove SyncSetupService::IsInitialSetupOngoing()
    >
    > No behavioral changes outside tests: the API is redundant and the one
    > calling site can lean on SyncSetupService's public API to determine the
    > very same logic.
    >
    > This helps in tests, where pre-existing issues are surfaced: before this
    > patch, some tests exercised a case where IsInitialSetupOngoing()
    > returned true while IsSyncRequested() returns false, which isn't
    > realistic.
    >
    > Bug: 1291953
    > Change-Id: I3c4fdbab6b8b4eb91e000d89847c047c8c6da62f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442109
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1132499}
    
    This CL is part of a multi-CL change:
     => https://crrev.com/c/4453544
     -  https://crrev.com/i/5780229
     -  https://crrev.com/c/4453507
    
    Bug: 1291953, 1434741
    Change-Id: Ia74b46007b9af405795e5f8f13e3ef474849de84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4453544
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133038}

--- ios/chrome/browser/ui/authentication/signout_action_sheet_coordinator_unittest.mm
+++ ios/chrome/browser/ui/authentication/signout_action_sheet_coordinator_unittest.mm
@@ -67,8 +67,8 @@ class SignoutActionSheetCoordinatorTest : public PlatformTest {
         std::make_unique<FakeAuthenticationServiceDelegate>());
     browser_ = std::make_unique<TestBrowser>(browser_state_.get());
 
-    sync_service_mock_ = static_cast<syncer::MockSyncService*>(
-        SyncServiceFactory::GetForBrowserState(browser_state_.get()));
+    sync_setup_service_mock_ = static_cast<SyncSetupServiceMock*>(
+        SyncSetupServiceFactory::GetForBrowserState(browser_state_.get()));
   }
 
   // Identity services.
@@ -106,14 +106,14 @@ class SignoutActionSheetCoordinatorTest : public PlatformTest {
   std::unique_ptr<TestChromeBrowserState> browser_state_;
   id<SystemIdentity> identity_ = nil;
 
-  syncer::MockSyncService* sync_service_mock_ = nullptr;
+  SyncSetupServiceMock* sync_setup_service_mock_ = nullptr;
 };
 
 // Tests that a signed-in user with Sync enabled will have an action sheet with
 // a sign-out title.
 TEST_F(SignoutActionSheetCoordinatorTest, SignedInUserWithSync) {
   authentication_service()->SignIn(identity_);
-  ON_CALL(*sync_service_mock_->GetMockUserSettings(), IsFirstSetupComplete())
+  ON_CALL(*sync_setup_service_mock_, IsFirstSetupComplete())
       .WillByDefault(testing::Return(true));
 
   SignoutActionSheetCoordinator* signout_coordinator = CreateCoordinator();
@@ -126,7 +126,7 @@ TEST_F(SignoutActionSheetCoordinatorTest, SignedInUserWithSync) {
 // no title.
 TEST_F(SignoutActionSheetCoordinatorTest, SignedInUserWithoutSync) {
   authentication_service()->SignIn(identity_);
-  ON_CALL(*sync_service_mock_->GetMockUserSettings(), IsFirstSetupComplete())
+  ON_CALL(*sync_setup_service_mock_, IsFirstSetupComplete())
       .WillByDefault(testing::Return(false));
 
   SignoutActionSheetCoordinator* signout_coordinator = CreateCoordinator();
@@ -143,7 +143,7 @@ TEST_F(SignoutActionSheetCoordinatorTest, SignedInUserWithForcedSignin) {
                               static_cast<int>(BrowserSigninMode::kForced));
 
   authentication_service()->SignIn(identity_);
-  ON_CALL(*sync_service_mock_->GetMockUserSettings(), IsFirstSetupComplete())
+  ON_CALL(*sync_setup_service_mock_, IsFirstSetupComplete())
       .WillByDefault(testing::Return(false));
 
   SignoutActionSheetCoordinator* signout_coordinator = CreateCoordinator();
@@ -157,7 +157,7 @@ TEST_F(SignoutActionSheetCoordinatorTest, SignedInUserWithForcedSignin) {
 // sheet with a sign-out title.
 TEST_F(SignoutActionSheetCoordinatorTest, SignedInManagedUserWithSync) {
   authentication_service()->SignIn(identity_);
-  ON_CALL(*sync_service_mock_->GetMockUserSettings(), IsFirstSetupComplete())
+  ON_CALL(*sync_setup_service_mock_, IsFirstSetupComplete())
       .WillByDefault(testing::Return(true));
 
   SignoutActionSheetCoordinator* signout_coordinator = CreateCoordinator();
@@ -170,7 +170,7 @@ TEST_F(SignoutActionSheetCoordinatorTest, SignedInManagedUserWithSync) {
 // sheet with no title.
 TEST_F(SignoutActionSheetCoordinatorTest, SignedInManagedUserWithoutSync) {
   authentication_service()->SignIn(identity_);
-  ON_CALL(*sync_service_mock_->GetMockUserSettings(), IsFirstSetupComplete())
+  ON_CALL(*sync_setup_service_mock_, IsFirstSetupComplete())
       .WillByDefault(testing::Return(false));
 
   SignoutActionSheetCoordinator* signout_coordinator = CreateCoordinator();
