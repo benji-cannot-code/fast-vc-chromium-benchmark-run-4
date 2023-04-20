11915a8cf3dddc4b689cea72ca752f2cc676d83c
Vishwas Uppoor <vishwasuppoor@google.com>
2023-04-20 04:17:18 +0000

    [Metadata][Android] Make dropdown sublabel more visible
    The Autofill dropdown's sublabels (second and third line) do not show
    the specified colors on the layout file. Programmatically set the color
    in the view-binder. This affects both credit card and address
    suggestions.
    
    Before
    Light: https://screenshot.googleplex.com/9vQeaSkM3YirBJY
    Dark: https://screenshot.googleplex.com/4yjYGyyKZq4gzis
    
    After
    Light: https://screenshot.googleplex.com/8rj3ZXBXzrH7caB
    Dark: https://screenshot.googleplex.com/3rPfcQoyKDNm3EE
    
    require tests.
    
    Low-Coverage-Reason: This change only updates text color, and doesn't
    Bug: 1434340
    Change-Id: I4cea1e906caa3601834396f7f4b7aeee034ab906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4441526
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132916}

--- components/autofill/android/java/src/org/chromium/components/autofill/AutofillDropdownAdapter.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/AutofillDropdownAdapter.java
@@ -103,6 +103,7 @@ public class AutofillDropdownAdapter extends ArrayAdapter<DropdownItem> {
         if (itemTagView != null) {
             itemTagView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     mContext.getResources().getDimension(item.getSublabelFontSizeResId()));
+            itemTagView.setTextColor(mContext.getColor(item.getSublabelFontColorResId()));
             height += mContext.getResources().getDimensionPixelSize(
                     R.dimen.autofill_dropdown_item_tag_height);
         }
@@ -143,14 +144,14 @@ public class AutofillDropdownAdapter extends ArrayAdapter<DropdownItem> {
             }
         }
 
-        labelView.setTextColor(mContext.getColor(item.getLabelFontColorResId()));
         labelView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                mContext.getResources().getDimension(R.dimen.text_size_large));
+                mContext.getResources().getDimension(item.getLabelFontSizeResId()));
+        labelView.setTextColor(mContext.getColor(item.getLabelFontColorResId()));
 
         if (secondaryLabelView != null) {
-            secondaryLabelView.setTextColor(mContext.getColor(item.getLabelFontColorResId()));
             secondaryLabelView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                    mContext.getResources().getDimension(R.dimen.text_size_large));
+                    mContext.getResources().getDimension(item.getLabelFontSizeResId()));
+            secondaryLabelView.setTextColor(mContext.getColor(item.getLabelFontColorResId()));
         }
 
         // Layout of the sublabel view, which has a smaller font and usually sits below the main
@@ -160,6 +161,7 @@ public class AutofillDropdownAdapter extends ArrayAdapter<DropdownItem> {
         if (sublabelView != null) {
             sublabelView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     mContext.getResources().getDimension(item.getSublabelFontSizeResId()));
+            sublabelView.setTextColor(mContext.getColor(item.getSublabelFontColorResId()));
         }
 
         TextView secondarySublabelView = populateLabelView(
@@ -167,6 +169,7 @@ public class AutofillDropdownAdapter extends ArrayAdapter<DropdownItem> {
         if (secondarySublabelView != null) {
             secondarySublabelView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     mContext.getResources().getDimension(item.getSublabelFontSizeResId()));
+            secondarySublabelView.setTextColor(mContext.getColor(item.getSublabelFontColorResId()));
         }
 
         ImageView iconViewStart = (ImageView) layout.findViewById(R.id.start_dropdown_icon);
