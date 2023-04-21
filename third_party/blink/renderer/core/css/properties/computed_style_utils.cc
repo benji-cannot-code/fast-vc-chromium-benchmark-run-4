03f18354026ff9db06237cebcb921541ea7cbc93
Daniil Sakhapov <sakhapov@chromium.org>
2023-04-21 09:05:14 +0000

    [scroll-animations] Support animation-range-start/end <length-percentage>
    As per
    https://drafts.csswg.org/scroll-animations-1/#propdef-animation-range-start
    these properties now support a single <length-percentage> argument.
    
    Fixed: 1427062
    Change-Id: Ic5309139712da71f8ebeb94d50706b6470ba6862
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437201
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133680}

--- third_party/blink/renderer/core/css/properties/computed_style_utils.cc
+++ third_party/blink/renderer/core/css/properties/computed_style_utils.cc
@@ -2289,7 +2289,9 @@ CSSValue* ValueForAnimationRange(const absl::optional<TimelineOffset>& offset,
     return MakeGarbageCollected<CSSIdentifierValue>(CSSValueID::kNormal);
   }
   CSSValueList* list = CSSValueList::CreateSpaceSeparated();
-  list->Append(*MakeGarbageCollected<CSSIdentifierValue>(offset->name));
+  if (offset->name != TimelineOffset::NamedRange::kNone) {
+    list->Append(*MakeGarbageCollected<CSSIdentifierValue>(offset->name));
+  }
   if (offset->offset != default_offset) {
     list->Append(*ComputedStyleUtils::ZoomAdjustedPixelValueForLength(
         offset->offset, style));
