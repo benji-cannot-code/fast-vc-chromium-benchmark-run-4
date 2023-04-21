a4eb487c8fa8b9005ed5b0c8a40b310e88fb6ebf
Yue Zhang <yuezhanggg@google.com>
2023-04-21 16:12:16 +0000

    [ChromeCart][Quest] Use correct URL for Cart<=>Quest matching
    https://crrev.com/c/4429263 used url_for_display in visits for
    Cart <=> Quest matching. This is wrong because url_for_display is the
    stripped version of URL and therefore cannot be converted to GURL and
    then extract domain. This CL fixes this issue by using normalized_url
    instead.
    
    Bug: 1431951
    Change-Id: I710322fe4e97b736d156e84972531b62bcb83f90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456601
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133852}

--- chrome/browser/new_tab_page/modules/history_clusters/cart/cart_processor.cc
+++ chrome/browser/new_tab_page/modules/history_clusters/cart/cart_processor.cc
@@ -91,7 +91,7 @@ void CartProcessor::OnLoadCart(
   }
   for (auto cart : carts) {
     for (auto& visit : cluster->visits) {
-      if (cart.first == eTLDPlusOne(GURL(visit->url_for_display))) {
+      if (cart.first == eTLDPlusOne(visit->normalized_url)) {
         std::move(callback).Run(CartToMojom(cart));
         return;
       }
--- chrome/browser/new_tab_page/modules/history_clusters/cart/cart_processor_unittest.cc
+++ chrome/browser/new_tab_page/modules/history_clusters/cart/cart_processor_unittest.cc
@@ -72,7 +72,7 @@ TEST_F(CartProcessorTest, TestFindCartForCluster) {
   // Create a fake cluster with one visit.
   auto cluster_mojom = history_clusters::mojom::Cluster::New();
   auto visit_mojom = history_clusters::mojom::URLVisit::New();
-  visit_mojom->url_for_display = kMockMerchantPageURL;
+  visit_mojom->normalized_url = GURL(kMockMerchantPageURL);
   cluster_mojom->visits.push_back(std::move(visit_mojom));
 
   // Mock a fake cart that belongs to the same domain as the visit.
@@ -109,7 +109,7 @@ TEST_F(CartProcessorTest, TestNoCartForCluster) {
   // Create a fake cluster with one visit.
   auto cluster_mojom = history_clusters::mojom::Cluster::New();
   auto visit_mojom = history_clusters::mojom::URLVisit::New();
-  visit_mojom->url_for_display = kMockMerchantPageURL;
+  visit_mojom->normalized_url = GURL(kMockMerchantPageURL);
   cluster_mojom->visits.push_back(std::move(visit_mojom));
 
   // Mock a fake cart that belongs to a different domain as the visit.
@@ -146,7 +146,7 @@ TEST_F(CartProcessorTest, TestNoCartForFailedLoad) {
   // Create a fake cluster with one visit.
   auto cluster_mojom = history_clusters::mojom::Cluster::New();
   auto visit_mojom = history_clusters::mojom::URLVisit::New();
-  visit_mojom->url_for_display = kMockMerchantPageURL;
+  visit_mojom->normalized_url = GURL(kMockMerchantPageURL);
   cluster_mojom->visits.push_back(std::move(visit_mojom));
 
   // Mock the DB load fails.
@@ -182,7 +182,7 @@ TEST_F(CartProcessorTest, TestCartToMojom) {
   // Create a fake cluster with one visit.
   auto cluster_mojom = history_clusters::mojom::Cluster::New();
   auto visit_mojom = history_clusters::mojom::URLVisit::New();
-  visit_mojom->url_for_display = kMockMerchantPageURL;
+  visit_mojom->normalized_url = GURL(kMockMerchantPageURL);
   cluster_mojom->visits.push_back(std::move(visit_mojom));
 
   // Mock a fake cart that belongs to the same domain as the visit, and add full
