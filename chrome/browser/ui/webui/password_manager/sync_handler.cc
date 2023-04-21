2ad7ee29ea1023a7e9a77fca7fbb4afb529ffdb5
Viktor Semeniuk <vsemeniuk@google.com>
2023-04-21 08:25:48 +0000

    [Password Manager] Check sync_service before using it
    Fixed: 1434834
    Change-Id: Ibe21933cc26976178248e2591ac0482ea3df66d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455072
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133665}

--- chrome/browser/ui/webui/password_manager/sync_handler.cc
+++ chrome/browser/ui/webui/password_manager/sync_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/password_manager/sync_handler.h"
 
+#include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
@@ -85,6 +86,12 @@ base::Value::Dict SyncHandler::GetSyncInfo() const {
   base::Value::Dict dict;
 
   syncer::SyncService* sync_service = GetSyncService();
+  // sync_service might be nullptr if SyncServiceFactory::IsSyncAllowed is
+  // false.
+  if (!sync_service) {
+    return dict;
+  }
+
   PrefService* pref_service = profile_->GetPrefs();
   syncer::UserSelectableTypeSet types =
       sync_service->GetUserSettings()->GetSelectedTypes();
