647ad0d2a4d184b4c2dc34c4a10fedc35143b67c
Christian Flach <cmfcmf@chromium.org>
2023-04-21 08:33:23 +0000

    iwa: Derive Profile from WebContents in IWAInstallCommand
    Instead of separately storing `Profile` and `WebContents`, only store
    the `WebContents`, since the `Profile` can be derived from it. Storing
    both is unnecessary and confusing, since it suggests that the `Profile`
    could differ from the `WebContents`' underlying `Profile`.
    
    Bug: 1426677
    Change-Id: I9e29e03879dff3bdb87a1f5d9a6988be5cb4d3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4453545
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133672}

--- chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.h
+++ chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.h
@@ -29,9 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/manifest/manifest.mojom-forward.h"
 
 class GURL;
+class Profile;
+class PrefService;
 
 namespace content {
-class BrowserContext;
 class WebContents;
 }  // namespace content
 
@@ -65,35 +66,28 @@ struct InstallIsolatedWebAppCommandError {
 // re-using web contents.
 class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
  public:
-  //
-  // |url_info| holds the origin information of the app. It is
-  // randomly generated for dev-proxy and the public key of signed bundle. It is
+  static std::unique_ptr<IsolatedWebAppResponseReaderFactory>
+  CreateDefaultResponseReaderFactory(const PrefService& prefs);
+
+  // |url_info| holds the origin information of the app. It is randomly
+  // generated for dev-proxy and the public key of signed bundle. It is
   // guarantee to be valid.
   //
-  // |location| holds information about the
-  // mode(dev-mod-proxy/signed-bundle) and the source.
+  // |location| holds information about the mode(dev-mod-proxy/signed-bundle)
+  // and the source.
   //
   // |callback| must be not null.
   //
   // The `id` in the application's manifest must equal "/".
+  //
+  // `response_reader_factory` should be created via
+  // `CreateDefaultResponseReaderFactory` and is used to create the
+  // `IsolatedWebAppResponseReader` for the Web Bundle.
   explicit InstallIsolatedWebAppCommand(
       const IsolatedWebAppUrlInfo& url_info,
       const IsolatedWebAppLocation& location,
       std::unique_ptr<content::WebContents> web_contents,
       std::unique_ptr<WebAppUrlLoader> url_loader,
-      content::BrowserContext& browser_context,
-      base::OnceCallback<
-          void(base::expected<InstallIsolatedWebAppCommandSuccess,
-                              InstallIsolatedWebAppCommandError>)> callback);
-
-  // Same constructor as above, but additionally exposes the
-  // `response_reader_factory` for providing a mock factory in testing.
-  explicit InstallIsolatedWebAppCommand(
-      const IsolatedWebAppUrlInfo& url_info,
-      const IsolatedWebAppLocation& location,
-      std::unique_ptr<content::WebContents> web_contents,
-      std::unique_ptr<WebAppUrlLoader> url_loader,
-      content::BrowserContext& browser_context,
       base::OnceCallback<
           void(base::expected<InstallIsolatedWebAppCommandSuccess,
                               InstallIsolatedWebAppCommandError>)> callback,
@@ -123,6 +117,9 @@ class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
  private:
   void ReportFailure(base::StringPiece message);
   void ReportSuccess();
+
+  Profile& profile();
+  const PrefService& prefs();
 
   void DownloadIcons(WebAppInstallInfo install_info);
   void OnGetIcons(WebAppInstallInfo install_info,
@@ -166,8 +163,6 @@ class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
   std::unique_ptr<content::WebContents> web_contents_;
 
   std::unique_ptr<WebAppUrlLoader> url_loader_;
-
-  base::raw_ref<content::BrowserContext> browser_context_;
 
   std::unique_ptr<WebAppDataRetriever> data_retriever_;
 
