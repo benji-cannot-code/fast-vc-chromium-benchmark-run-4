7e4baf6550bd2b8652063f85a7771fb09c91ecb9
Andrii Natiahlyi <natiahlyi@google.com>
2023-04-21 15:02:11 +0000

    [UPM][Import] Add import link from empty passwords list
    Impl: https://screenshot.googleplex.com/9zLNAKY4ykjeoe6.png
    Mocks: https://screenshot.googleplex.com/36u7hqGH7Kvp3Kj.png
    
    Bug: 1432962
    Change-Id: Ifd6d21fca3257fcda689def0728e287fdc902192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454508
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133815}

--- chrome/browser/ui/webui/password_manager/password_manager_ui.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui.cc
@@ -57,6 +57,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+std::u16string InsertBrandedPasswordManager(int message_id) {
+  return l10n_util::GetStringFUTF16(
+      message_id,
+      l10n_util::GetStringUTF16(
+          IDS_PASSWORD_BUBBLES_PASSWORD_MANAGER_LINK_TEXT_SAVING_ON_DEVICE));
+}
+
 content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
     Profile* profile,
     content::WebUI* web_ui) {
@@ -146,7 +153,8 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
     {"editPasswordFootnote", IDS_PASSWORD_MANAGER_UI_PASSWORD_EDIT_FOOTNOTE},
     {"editPasswordTitle", IDS_PASSWORD_MANAGER_UI_EDIT_PASSWORD},
     {"emptyNote", IDS_PASSWORD_MANAGER_UI_NO_NOTE_SAVED},
-    {"emptyState", IDS_PASSWORD_MANAGER_UI_EMPTY_STATE},
+    {"emptyStateImportSyncing",
+     IDS_PASSWORD_MANAGER_UI_EMPTY_STATE_SYNCING_USERS},
     {"exportPasswords", IDS_PASSWORD_MANAGER_UI_EXPORT_TITLE},
     {"exportPasswordsDescription",
      IDS_PASSWORD_MANAGER_UI_EXPORT_BANNER_DESCRIPTION},
@@ -363,31 +371,30 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
   source->AddString("trustedVaultLearnMoreUrl",
                     chrome::kSyncTrustedVaultLearnMoreURL);
 
+  source->AddString("addShortcutDescription",
+                    InsertBrandedPasswordManager(
+                        IDS_PASSWORD_MANAGER_UI_ADD_SHORTCUT_DESCRIPTION));
+
   source->AddString(
-      "addShortcutDescription",
-      l10n_util::GetStringFUTF16(
-          IDS_PASSWORD_MANAGER_UI_ADD_SHORTCUT_DESCRIPTION,
-          l10n_util::GetStringUTF16(
-              IDS_PASSWORD_BUBBLES_PASSWORD_MANAGER_LINK_TEXT_SAVING_ON_DEVICE)));
+      "emptyStateImportAccountStore",
+      InsertBrandedPasswordManager(
+          IDS_PASSWORD_MANAGER_UI_EMPTY_STATE_ACCOUNT_STORE_USERS));
+  source->AddString("emptyStateImportDevice",
+                    InsertBrandedPasswordManager(
+                        IDS_PASSWORD_MANAGER_UI_EMPTY_STATE_SIGNEDOUT_USERS));
 
   source->AddString(
       "importPasswordsGenericDescription",
-      l10n_util::GetStringFUTF16(
-          IDS_PASSWORD_MANAGER_UI_IMPORT_DESCRIPTION_ACCOUNT_STORE_USERS,
-          l10n_util::GetStringUTF16(
-              IDS_PASSWORD_BUBBLES_PASSWORD_MANAGER_LINK_TEXT_SAVING_ON_DEVICE)));
+      InsertBrandedPasswordManager(
+          IDS_PASSWORD_MANAGER_UI_IMPORT_DESCRIPTION_ACCOUNT_STORE_USERS));
   source->AddString(
       "importPasswordsDescriptionDevice",
-      l10n_util::GetStringFUTF16(
-          IDS_PASSWORD_MANAGER_UI_IMPORT_DESCRIPTION_SIGNEDOUT_USERS,
-          l10n_util::GetStringUTF16(
-              IDS_PASSWORD_BUBBLES_PASSWORD_MANAGER_LINK_TEXT_SAVING_ON_DEVICE)));
-  source->AddString(
-      "passwordsStoreOptionDevice",
-      l10n_util::GetStringFUTF16(
-          IDS_PASSWORD_MANAGER_UI_STORE_PICKER_OPTION_DEVICE,
-          l10n_util::GetStringUTF16(
-              IDS_PASSWORD_BUBBLES_PASSWORD_MANAGER_LINK_TEXT_SAVING_ON_DEVICE)));
+      InsertBrandedPasswordManager(
+          IDS_PASSWORD_MANAGER_UI_IMPORT_DESCRIPTION_SIGNEDOUT_USERS));
+  source->AddString("passwordsStoreOptionDevice",
+                    InsertBrandedPasswordManager(
+                        IDS_PASSWORD_MANAGER_UI_STORE_PICKER_OPTION_DEVICE));
+
   source->AddString(
       "importPasswordsLimitExceeded",
       l10n_util::GetStringFUTF16(
