28fd894b5a0ca6808017135479d98d6dfb9b50b5
Sven Zheng <svenzheng@chromium.org>
2023-04-21 22:57:59 +0000

    Fix AshBrowserTestStarterTest unit tests
    base::ToString doesn't work well for int. Change to use
    base::NumberToString.
    
    Bug: 1435555
    Test: Locally tested with linux-chromeos-dbg gn args.
    Change-Id: If447e7812c7a67fc7926d34678176e8536a22818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4460992
    Auto-Submit: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134103}

--- chrome/test/base/chromeos/ash_browser_test_starter.cc
+++ chrome/test/base/chromeos/ash_browser_test_starter.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/environment.h"
 #include "base/files/file_util.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/task/thread_pool.h"
 #include "base/test/bind.h"
 #include "base/test/test_switches.h"
@@ -61,7 +62,8 @@ bool AshBrowserTestStarter::PrepareEnvironmentForLacros() {
     int retry_count = 1;
     while (base::PathExists(test_output_folder) && retry_count < 5) {
       test_output_folder = output_file_path.DirName().Append(
-          test_name + base::ToString(".retry_") + base::ToString(retry_count));
+          test_name + base::ToString(".retry_") +
+          base::NumberToString(retry_count));
       ++retry_count;
     }
     // Unlikely the path still exist. But in case it happens, we would let
