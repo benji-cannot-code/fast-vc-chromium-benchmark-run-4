866b1b9eaec0374bb283a1354f9aa2d87b4009b1
Claudio DeSouza <cdesouza@igalia.com>
2023-04-21 06:37:07 +0000

    Replace base::GUID uses with base::Uuid (170/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/support_tool
    
    This CL was uploaded by git cl split.
    
    R=pmarko@chromium.org
    
    Bug: 1428566
    Change-Id: I33ad4f0335f751398a12ccdd4a989460ff5dd984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456136
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133628}

--- chrome/browser/support_tool/support_packet_metadata.cc
+++ chrome/browser/support_tool/support_packet_metadata.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
-#include "base/guid.h"
 #include "base/i18n/time_formatting.h"
 #include "base/location.h"
 #include "base/strings/string_util.h"
@@ -24,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "chrome/browser/policy/policy_ui_utils.h"
 #include "chrome/browser/support_tool/data_collector.h"
@@ -189,7 +189,7 @@ std::string SupportPacketMetadata::GetDataCollectorsListString(
 }
 
 std::string SupportPacketMetadata::GetGUIDForSupportPacket() {
-  return base::GUID::GenerateRandomV4().AsLowercaseString();
+  return base::Uuid::GenerateRandomV4().AsLowercaseString();
 }
 
 void SupportPacketMetadata::AddErrorMessagesToMetadata() {
