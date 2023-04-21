97e3e08c5629cbdbfad78861ad68c19c070716ae
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-04-21 13:48:36 +0000

    [RemoveLegacy] Simplify layout from subtree roots.
    Previously we'd perform lots of complex logic to layout a subtree
    root. This isn't required anymore. Instead just use the previous
    constraint space.
    
    This new logic is behind a kill-switch:
    LayoutNewSubtreeRoot
    
    Bug: 1229581
    Change-Id: I83a73833b8d076cd69b5c15a9982263776a8fe63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456347
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133774}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -682,7 +682,11 @@ bool LocalFrameView::LayoutFromRootObject(LayoutObject& root) {
     }
   }
 
-  To<LayoutBox>(root).LayoutSubtreeRoot();
+  if (RuntimeEnabledFeatures::LayoutNewSubtreeRootEnabled()) {
+    To<LayoutBox>(root).LayoutSubtreeRoot();
+  } else {
+    To<LayoutBox>(root).LayoutSubtreeRootOld();
+  }
   return true;
 }
 
