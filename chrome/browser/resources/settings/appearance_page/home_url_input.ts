995db7153d908a425bf3430c9295954931f1efc5
dpapad <dpapad@chromium.org>
2023-04-21 22:24:11 +0000

    Settings: Move c/b/r/settings/control/ Mixins to "limited sharing".
    This is in preparation of moving settings-toggle-button and
    settings-checkbox to limited sharing.
    
    Bug: 1431477
    Change-Id: Icaf9310c204c14ac28d12ffe69eb02eec47d93ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4458539
    Reviewed-by: Cole Horvitz <colehorvitz@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134085}

--- chrome/browser/resources/settings/appearance_page/home_url_input.ts
+++ chrome/browser/resources/settings/appearance_page/home_url_input.ts
@@ -11,12 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import 'chrome://resources/cr_elements/policy/cr_policy_pref_indicator.js';
 
+import {CrPolicyPrefMixin, CrPolicyPrefMixinInterface} from '/shared/settings/controls/cr_policy_pref_mixin.js';
+import {PrefControlMixin} from '/shared/settings/controls/pref_control_mixin.js';
 import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-
-import {CrPolicyPrefMixin, CrPolicyPrefMixinInterface} from '../controls/cr_policy_pref_mixin.js';
-import {PrefControlMixin} from '../controls/pref_control_mixin.js';
 
 import {AppearanceBrowserProxy, AppearanceBrowserProxyImpl} from './appearance_browser_proxy.js';
 import {getTemplate} from './home_url_input.html.js';
