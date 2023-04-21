dae8d4c7f106ac140b87f833ec548fe7974fb77b
Peter Kvitek <kvitekp@chromium.org>
2023-04-21 17:53:11 +0000

    [headless] Removed headless specific PRODUCT_VERSION declaration
    Headless Chrome version always matches the Chrome version, so there is no reason to have a separate declaration for it.
    
    Bug: 1434165
    Change-Id: Idb19c46c575d52ae7d4c31c7094fdbaa810f4e40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4458299
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133908}

--- headless/lib/browser/headless_browser_impl.cc
+++ headless/lib/browser/headless_browser_impl.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_browser_context_impl.h"
 #include "headless/lib/browser/headless_browser_main_parts.h"
 #include "headless/lib/browser/headless_web_contents_impl.h"
-#include "headless/public/version.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 
 namespace headless {
--- headless/lib/headless_crash_reporter_client.cc
+++ headless/lib/headless_crash_reporter_client.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "components/version_info/version_info.h"
 #include "content/public/common/content_switches.h"
-#include "headless/public/version.h"
 
 namespace headless {
 
