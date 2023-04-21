6e05fc3f4c3918bc65f5938f10d5b8d19fb6168e
Zach Katz <katzz@google.com>
2023-04-21 19:33:45 +0000

    [PCCT-SideSheet] Max width landscape bottom sheet keeps side shadows
    Bug: 1435113
    Change-Id: Ic28485b790af3562f1e3c65bfb50b247828cd6b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457277
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Zach Katz <katzz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133974}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBottomSheetStrategy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBottomSheetStrategy.java
@@ -496,9 +496,6 @@ public class PartialCustomTabBottomSheetStrategy extends PartialCustomTabBaseStr
     protected void setTopMargins(int shadowOffset, int handleOffset) {
         View handleView = mActivity.findViewById(R.id.custom_tabs_handle_view);
         boolean isMaxWidthLandscapeBottomSheet = isMaxWidthLandscapeBottomSheet();
-        int sideOffset = shouldHaveNoShadowOffset()
-                ? 0
-                : mActivity.getResources().getDimensionPixelSize(R.dimen.custom_tabs_shadow_offset);
 
         if (ChromeFeatureList.sCctResizableSideSheet.isEnabled()) {
             float maxWidthBottomSheetEv =
@@ -515,6 +512,9 @@ public class PartialCustomTabBottomSheetStrategy extends PartialCustomTabBaseStr
             }
         }
 
+        int sideOffset = shouldDrawDividerLine()
+                ? 0
+                : mActivity.getResources().getDimensionPixelSize(R.dimen.custom_tabs_shadow_offset);
         int sideMargin = isMaxWidthLandscapeBottomSheet ? sideOffset : 0;
         if (handleView != null) {
             ViewGroup.MarginLayoutParams lp =
