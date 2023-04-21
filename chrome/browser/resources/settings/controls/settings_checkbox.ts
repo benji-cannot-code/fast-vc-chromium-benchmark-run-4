c30bab469369829e05533343a958bcda13af8adc
dpapad <dpapad@chromium.org>
2023-04-21 19:32:36 +0000

    Settings: Restore cr-link-color in settings-{checkbox,toggle-button}.
    The link styling regressed when those elements stopped using
    settings_shared.css
    
    Fixed: 1435337
    Change-Id: Ie52742d271473b7a3d373043862ff35ccb14e9bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4459305
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133972}

--- chrome/browser/resources/settings/controls/settings_checkbox.ts
+++ chrome/browser/resources/settings/controls/settings_checkbox.ts
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/policy/cr_policy_pref_indicator.js';
 // <if expr='chromeos_ash'>
 import '//resources/cr_elements/chromeos/cros_color_overrides.css.js';
