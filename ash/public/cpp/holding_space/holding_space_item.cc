732566db5c8df83e2bd2b7f865c0765fc3bdaf08
Angus L. M. McLean IV <angusmclean@chromium.org>
2023-04-21 21:15:40 +0000

    holding space: Update HoldingSpaceItem::Type checking method names
    Updating static utility functions' names to more accurately describe
    their functionality and be in line with other similar methods.
    
    Bug: b:278147195
    Change-Id: I891ad91b4f24834d9183bdb1dc0509b0965e814f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4460032
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134050}

--- ash/public/cpp/holding_space/holding_space_item.cc
+++ ash/public/cpp/holding_space/holding_space_item.cc
@@ -137,7 +137,7 @@ bool HoldingSpaceItem::IsCameraAppType(HoldingSpaceItem::Type type) {
 }
 
 // static
-bool HoldingSpaceItem::IsDownload(HoldingSpaceItem::Type type) {
+bool HoldingSpaceItem::IsDownloadType(HoldingSpaceItem::Type type) {
   switch (type) {
     case Type::kArcDownload:
     case Type::kDownload:
@@ -164,7 +164,7 @@ bool HoldingSpaceItem::IsDownload(HoldingSpaceItem::Type type) {
 }
 
 // static
-bool HoldingSpaceItem::IsScreenCapture(HoldingSpaceItem::Type type) {
+bool HoldingSpaceItem::IsScreenCaptureType(HoldingSpaceItem::Type type) {
   switch (type) {
     case Type::kScreenRecording:
     case Type::kScreenRecordingGif:
@@ -191,7 +191,7 @@ bool HoldingSpaceItem::IsScreenCapture(HoldingSpaceItem::Type type) {
 }
 
 // static
-bool HoldingSpaceItem::IsSuggestion(HoldingSpaceItem::Type type) {
+bool HoldingSpaceItem::IsSuggestionType(HoldingSpaceItem::Type type) {
   switch (type) {
     case Type::kDriveSuggestion:
     case Type::kLocalSuggestion:
