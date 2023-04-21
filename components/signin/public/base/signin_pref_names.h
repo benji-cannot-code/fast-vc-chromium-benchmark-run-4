4e42a0726e6bca32568d4bee0faef0042e03e65d
Liza Bipin <mlbipin@google.com>
2023-04-21 09:18:24 +0000

    Move pref::kSigninAllowedOnNextStartup to signin_pref_names.h
    Must be moved so that it can be added to supervised user pref store in crrev.com/c/4432611
    
    Bug: b:276437386
    Change-Id: I3a0b305d994e12b17b3b06b0d6e321f108391f36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4439856
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133683}

--- components/signin/public/base/signin_pref_names.h
+++ components/signin/public/base/signin_pref_names.h
@@ -33,6 +33,7 @@ extern const char kReverseAutologinRejectedEmailList[];
 extern const char kSignedInWithCredentialProvider[];
 extern const char kSigninAllowed[];
 extern const char kGaiaCookieLastListAccountsData[];
+extern const char kSigninAllowedOnNextStartup[];
 
 }  // namespace prefs
 
