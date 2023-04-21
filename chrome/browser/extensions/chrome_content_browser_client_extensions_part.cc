e589995ed389744121a3b270d256c8c957912c18
Kevin McNee <mcnee@chromium.org>
2023-04-21 15:59:19 +0000

    Don't apply extension WebPreferences to webviews which load accessible resources
    Extensions code only checks for the extension scheme when deciding
    whether to apply extension WebPreferences to a navigation. This also
    affects webview accessible resources. Notably these settings prevent
    cookie access, so if a webview navigates to a webview accessible
    resource, then navigates to a regular web page, the web page can't
    access cookies.
    
    We now check that a navigation with an extension scheme is not happening
    in a webview before applying extension WebPreferences.
    
    Bug: 1417528
    Change-Id: I04b12ab91c00c340591f7f1933e6d0687bff5408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450353
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133845}

--- chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
+++ chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_service_worker_message_filter.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/guest_view/extensions_guest_view.h"
+#include "extensions/browser/guest_view/web_view/web_view_guest.h"
 #include "extensions/browser/guest_view/web_view/web_view_renderer_state.h"
 #include "extensions/browser/process_map.h"
 #include "extensions/browser/renderer_startup_helper.h"
@@ -764,10 +765,24 @@ bool ChromeContentBrowserClientExtensionsPart::
   // Ensure that we are only granting extension preferences to URLs with
   // the correct scheme. Without this check, hosts that happen to match the id
   // of an installed extension would get the wrong preferences.
+  // TODO(crbug.com/1435121): Once the `web_prefs` have been set based on
+  // `extension` below, they are not unset when navigating a tab from an
+  // extension page to a regular web page. We should clear extension settings in
+  // this case.
   const GURL& site_url =
       web_contents->GetPrimaryMainFrame()->GetSiteInstance()->GetSiteURL();
   if (!site_url.SchemeIs(kExtensionScheme))
     return false;
+
+  // If a webview navigates to a webview accessible resource, extension
+  // preferences should not be applied to the webview.
+  // TODO(crbug.com/1435121): Once it is possible to clear extension settings
+  // after a navigation, we can remove this case so that extension settings can
+  // apply to webview accessible resources without impacting web pages
+  // subsequently loaded in the webview.
+  if (WebViewGuest::FromWebContents(web_contents)) {
+    return false;
+  }
 
   const Extension* extension =
       registry->enabled_extensions().GetByID(site_url.host());
