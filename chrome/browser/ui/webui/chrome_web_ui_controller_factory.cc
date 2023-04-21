cb99765f6a4700be41f59a9794553ed48cb969b8
Jun W <jungoog@google.com>
2023-04-21 05:33:35 +0000

    Migrate RemoteMaintenanceCurtainUI to WebUIConfig
    Context:
    
    Currently WebUI properties, e.g. CSPs, requesting schemes, host, mojo,
    etc. are stored in WebUIControllers themselves or in separate lists
    like IsWebUIAllowedToMakeNetworkRequests. The lifetime of a
    WebUIController is bound to the frame, which makes it hard to use for
    some use cases where the WebUIController hasn't been created yet. A
    non-dynamically allocated class where clients could query a WebUI's
    properties would be easier to use and audit.
    
    WebUIConfig is a class that stores properties of WebUIs. For now the
    properties are 1. the WebUI's origin, 2. if the WebUI is enabled,
    and 3. GetURLDataSource() method for service worker initialization.
    In the future this class could include information like CSPs, if we
    should enable Mojo, if we should enable chrome.send(), if we should
    allow network requests, URLDataSource, etc.
    
    Bug: 1317510
    Change-Id: Iab5bea54d23952bccb4e6d5f070e8e5e012d878b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4452210
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Junguo (Jun) W. <jungoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133613}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -291,7 +291,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/notification_tester/notification_tester_ui.h"
 #include "chrome/browser/ui/webui/ash/parent_access/parent_access_ui.h"
 #include "chrome/browser/ui/webui/ash/power_ui.h"
-#include "chrome/browser/ui/webui/ash/remote_maintenance_curtain_ui.h"
 #include "chrome/browser/ui/webui/ash/set_time_ui.h"
 #include "chrome/browser/ui/webui/ash/slow_trace_ui.h"
 #include "chrome/browser/ui/webui/ash/slow_ui.h"
@@ -977,9 +976,6 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
       return &NewWebUI<ash::OobeUI>;
     }
     return nullptr;
-  }
-  if (url.host_piece() == chrome::kChromeUIRemoteManagementCurtainHost) {
-    return &NewWebUI<ash::RemoteMaintenanceCurtainUI>;
   }
   if (url.host_piece() == ash::kChromeUIDiagnosticsAppHost) {
     return &NewWebUI<ash::DiagnosticsDialogUI>;
--- chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/office_fallback/office_fallback_ui.h"
 #include "chrome/browser/ui/webui/ash/parent_access/parent_access_ui.h"
 #include "chrome/browser/ui/webui/ash/power_ui.h"
+#include "chrome/browser/ui/webui/ash/remote_maintenance_curtain_ui.h"
 #include "chrome/browser/ui/webui/ash/set_time_ui.h"
 #include "chrome/browser/ui/webui/ash/slow_trace_ui.h"
 #include "chrome/browser/ui/webui/ash/slow_ui.h"
@@ -197,6 +198,7 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(
       std::make_unique<ash::reporting::EnterpriseReportingUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::PowerUIConfig>());
+  map.AddWebUIConfig(std::make_unique<ash::RemoteMaintenanceCurtainUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::SetTimeUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::ShortcutCustomizationAppUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::SlowTraceControllerConfig>());
