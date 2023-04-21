aafa8c940140e5f05f1b2967ae31e1293b06ed08
Andrew Paseltiner <apaseltiner@chromium.org>
2023-04-21 17:04:49 +0000

    Reland "Support multiple background attributionsrc requests"
    This is a reland of commit a8cbb3c314d868a35309095f103d5bd572385b58.
    We've actually encountered a similar bug before related to navigations
    (http://crbug.com/1405318) so for now I've simply removed the flaky
    test and will investigate separately.
    
    Original change's description:
    > Support multiple background attributionsrc requests
    >
    > https://github.com/WICG/attribution-reporting-api/pull/744
    > https://github.com/WICG/attribution-reporting-api/pull/752
    >
    > The attributionsrc attribute on <a>, <img>, and <script> now supports
    > a space-separated list of URLs to which to make background requests in
    > the same contexts in which a single such request was previously
    > supported. The strings are resolved to full URLs as previously: relative
    > URLs are supported.
    >
    > Likewise, the attributionsrc window.open feature can be specified
    > multiple times.
    >
    > URLs that are not allowed to register are logged to DevTools but
    > otherwise ignored, and do not affect the valid URLs in the list, if any.
    >
    > Registrations from multiple requests are multiplexed onto a single
    > AttributionDataHost Mojo remote because the registration order *between*
    > requests is unspecified. This reduces resource consumption in both the
    > renderer and the browser and makes it unnecessary to associate a single
    > AttributionSrcToken with multiple data hosts, which would require
    > significant browser-side refactoring.
    >
    > As a result of the multiplexing change, we move the
    > Conversions.Registered(Sources|Triggers)PerDataHost metrics to the
    > renderer, where it is easier to track per redirect chain, and combine
    > them into a single metric for both types of registrations.
    >
    > Bug: 1430194, 1430496, 1434306
    > Change-Id: Id5dbc42a20a78394560f11aa7fc57bfa2d92ae78
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4393405
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    > Reviewed-by: Nan Lin <linnan@chromium.org>
    > Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1133249}
    
    Bug: 1430194, 1430496, 1434306
    Change-Id: I09257bc5711a08c3312c1bd846b57bd86c139ebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454297
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133882}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
@@ -190,8 +190,6 @@ TEST_F(AttributionDataHostManagerImplTest, SourceDataHost_SourceRegistered) {
 
 TEST_F(AttributionDataHostManagerImplTest,
        SourceDataHost_ReceiverDestinationsMayDiffer) {
-  base::HistogramTester histograms;
-
   Checkpoint checkpoint;
   {
     InSequence seq;
@@ -243,14 +241,9 @@ TEST_F(AttributionDataHostManagerImplTest,
                                                     std::move(source_data));
     data_host_remote.data_host.FlushForTesting();
   }
-
-  histograms.ExpectUniqueSample("Conversions.RegisteredSourcesPerDataHost", 4,
-                                1);
 }
 
 TEST_F(AttributionDataHostManagerImplTest, TriggerDataHost_TriggerRegistered) {
-  base::HistogramTester histograms;
-
   auto destination_origin =
       *SuitableOrigin::Deserialize("https://trigger.example");
   auto reporting_origin =
@@ -315,15 +308,10 @@ TEST_F(AttributionDataHostManagerImplTest, TriggerDataHost_TriggerRegistered) {
         /*attestation=*/absl::nullopt);
     data_host_remote.data_host.FlushForTesting();
   }
-
-  histograms.ExpectBucketCount("Conversions.RegisteredTriggersPerDataHost", 1,
-                               1);
 }
 
 TEST_F(AttributionDataHostManagerImplTest,
        TriggerDataHost_ReceiverModeCheckPerformed) {
-  base::HistogramTester histograms;
-
   Checkpoint checkpoint;
   {
     InSequence seq;
@@ -386,16 +374,10 @@ TEST_F(AttributionDataHostManagerImplTest,
         /*attestation=*/absl::nullopt);
     data_host_remote.data_host.FlushForTesting();
   }
-
-  histograms.ExpectTotalCount("Conversions.RegisteredSourcesPerDataHost", 0);
-  histograms.ExpectUniqueSample("Conversions.RegisteredTriggersPerDataHost", 3,
-                                1);
 }
 
 TEST_F(AttributionDataHostManagerImplTest,
        MixedDataHost_AllowsSourcesAndTriggers) {
-  base::HistogramTester histograms;
-
   Checkpoint checkpoint;
   {
     InSequence seq;
@@ -449,17 +431,10 @@ TEST_F(AttributionDataHostManagerImplTest,
                                                     std::move(source_data));
     data_host_remote.data_host.FlushForTesting();
   }
-
-  histograms.ExpectUniqueSample("Conversions.RegisteredSourcesPerDataHost", 3,
-                                1);
-  histograms.ExpectUniqueSample("Conversions.RegisteredTriggersPerDataHost", 1,
-                                1);
 }
 
 TEST_F(AttributionDataHostManagerImplTest,
        SourceDataHost_ReceiverModeCheckPerformed) {
-  base::HistogramTester histograms;
-
   Checkpoint checkpoint;
   {
     InSequence seq;
@@ -519,10 +494,6 @@ TEST_F(AttributionDataHostManagerImplTest,
                                                     std::move(source_data));
     data_host_remote.data_host.FlushForTesting();
   }
-
-  histograms.ExpectUniqueSample("Conversions.RegisteredSourcesPerDataHost", 3,
-                                1);
-  histograms.ExpectTotalCount("Conversions.RegisteredTriggersPerDataHost", 0);
 }
 
 TEST_F(AttributionDataHostManagerImplTest,
@@ -682,8 +653,6 @@ TEST_F(AttributionDataHostManagerImplTest, NoSourceOrTrigger) {
         kFrameId, /*last_navigation_id=*/kNavigationId);
   }
 
-  histograms.ExpectTotalCount("Conversions.RegisteredSourcesPerDataHost", 0);
-  histograms.ExpectTotalCount("Conversions.RegisteredTriggersPerDataHost", 0);
   // kImmediately = 0
   histograms.ExpectUniqueSample(kRegisterDataHostOutcomeHistogram, 0, 1);
 }
