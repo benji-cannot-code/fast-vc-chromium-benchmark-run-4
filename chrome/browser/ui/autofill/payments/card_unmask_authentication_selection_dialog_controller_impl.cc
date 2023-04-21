0c1a8cea7b821594cf02e430425bcddf163e7b85
Junhui He <junhuihe@google.com>
2023-04-21 19:01:27 +0000

    Replace the email icon for email OTP dialog
    This CL is to create a new icon named email_light and use it for
    challenge option selection dialog when only email OTP option returned.
    
    SVG link: https://fonts.gstatic.com/s/i/googlematerialicons/email/v13/24px.svg
    
    Mock:
    old: https://screenshot.googleplex.com/9CwrpSBXQLrRHrm.png
    new: https://screenshot.googleplex.com/5feU6oFADcUi9jA.png
    
    Bug: 1427665
    Change-Id: I43ba46465559300fffd95771b1a2b542f7e8810b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457110
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Junhui He <junhuihe@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133950}

--- chrome/browser/ui/autofill/payments/card_unmask_authentication_selection_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/card_unmask_authentication_selection_dialog_controller_impl.cc
@@ -208,7 +208,7 @@ ui::ImageModel CardUnmaskAuthenticationSelectionDialogControllerImpl::
     case CardUnmaskChallengeOptionType::kSmsOtp:
       return ui::ImageModel::FromVectorIcon(vector_icons::kSmsIcon);
     case CardUnmaskChallengeOptionType::kEmailOtp:
-      return ui::ImageModel::FromVectorIcon(vector_icons::kEmailIcon);
+      return ui::ImageModel::FromVectorIcon(vector_icons::kEmailOutlineIcon);
     case CardUnmaskChallengeOptionType::kCvc:
     case CardUnmaskChallengeOptionType::kUnknownType:
       NOTREACHED();
