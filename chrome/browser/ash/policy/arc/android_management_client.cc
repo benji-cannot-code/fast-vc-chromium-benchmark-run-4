92c1f91b1eb594f31b8a17359799d1c5c909069c
Claudio DeSouza <cdesouza@igalia.com>
2023-04-21 09:55:45 +0000

    Replace base::GUID uses with base::Uuid (131/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ash/policy
    
    This CL was uploaded by git cl split.
    
    R=igorcov@chromium.org
    
    Bug: 1428566
    Change-Id: I4cc5730df7adb9425f8eca13a11ab6f9accb7941
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454359
    Commit-Queue: Igor <igorcov@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133697}

--- chrome/browser/ash/policy/arc/android_management_client.cc
+++ chrome/browser/ash/policy/arc/android_management_client.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/logging.h"
+#include "base/uuid.h"
 #include "components/policy/core/common/cloud/device_management_service.h"
 #include "components/policy/core/common/cloud/dm_auth.h"
 #include "components/policy/core/common/cloud/dmserver_job_configurations.h"
@@ -87,7 +87,7 @@ void AndroidManagementClientImpl::CheckAndroidManagement(
       DMServerJobConfiguration>(
       device_management_service_,
       DeviceManagementService::JobConfiguration::TYPE_ANDROID_MANAGEMENT_CHECK,
-      /*client_id=*/base::GenerateGUID(),
+      /*client_id=*/base::Uuid::GenerateRandomV4().AsLowercaseString(),
       /*critical=*/false, DMAuth::NoAuth(), access_token, url_loader_factory_,
       base::BindOnce(&AndroidManagementClientImpl::OnAndroidManagementChecked,
                      weak_ptr_factory_.GetWeakPtr()));
