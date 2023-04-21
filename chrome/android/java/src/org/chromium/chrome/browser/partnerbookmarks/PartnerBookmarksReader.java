357f4b713e2e20c77af093f4d6722c00e1183178
Daniel White <danieltwhite@google.com>
2023-04-21 15:17:05 +0000

    Removed Expired PartnerBookmarks Histograms
    In this CL, the expired histograms PartnerBookmark.Count2 and
    PartnerBookmark.FaviconThrottleFetchResult and their invocations were
    removed.
    
    Bug: 1384766
    Change-Id: I4e362adbefe8539648b83e5003eff82ea1fd79a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455028
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133826}

--- chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksReader.java
+++ chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksReader.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
@@ -158,9 +157,6 @@ public class PartnerBookmarksReader {
         FetchFaviconCallback callback = new FetchFaviconCallback() {
             @Override
             public void onFaviconFetched(@FaviconFetchResult int result) {
-                RecordHistogram.recordEnumeratedHistogram(
-                        "PartnerBookmark.FaviconThrottleFetchResult", result,
-                        FaviconFetchResult.UMA_BOUNDARY);
                 synchronized (mProgressLock) {
                     if (result == FaviconFetchResult.SUCCESS_FROM_SERVER) {
                         // If we've fetched a new favicon from a server, store a flag to indicate
@@ -239,10 +235,6 @@ public class PartnerBookmarksReader {
         }
     }
 
-    void recordPartnerBookmarkCount(int count) {
-        RecordHistogram.recordCount100Histogram("PartnerBookmark.Count2", count);
-    }
-
     /** Handles fetching partner bookmarks in a background thread. */
     private class ReadBookmarksTask extends AsyncTask<Void> {
         private final Object mRootSync = new Object();
@@ -287,8 +279,6 @@ public class PartnerBookmarksReader {
                 urlSet.add(bookmark.mUrl);
             }
             bookmarkIterator.close();
-            int count = urlSet.size();
-            recordPartnerBookmarkCount(count);
 
             // Recreate the folder hierarchy and read it.
             recreateFolderHierarchy(idMap);
