417b4fbfe4bf0b431707e4a88aec8b85483b8a25
Ginny Huang <ginnyhuang@chromium.org>
2023-04-21 17:20:32 +0000

    Remove accidentally created comments.
    This is a follow up of crrev/c/4456688, where commented-out code is
    accidentally committed.
    
    Bug: N/A
    Change-Id: I961d8669f507bc3045e34606ea5d52ed8ecbe25b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457170
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133889}

--- ios/chrome/browser/ui/bring_android_tabs/bring_android_tabs_app_interface.h
+++ ios/chrome/browser/ui/bring_android_tabs/bring_android_tabs_app_interface.h
@@ -8,11 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-/*typedef NS_ENUM(NSUInteger, BringAndroidTabsAppInterfaceForeignSession) {
-  BringAndroidTabsAppInterfaceRecentSessionFromAndroidPhone = 0,
-  BringAndroidTabsAppInterfaceExpiredSessionFromAndroidPhone,
-  BringAndroidTabsAppInterfaceRecentSessionFromDesktop,
-};*/
 // Enum specifying different foreign sessions used for Bring Android Tabs
 // testing.
 enum class BringAndroidTabsAppInterfaceForeignSession {
