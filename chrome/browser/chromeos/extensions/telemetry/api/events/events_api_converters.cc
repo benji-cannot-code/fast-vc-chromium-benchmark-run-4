6b5f67eb9900a86d61a5d603afc1479ec65fbe81
Ethan Cheng <yycheng@google.com>
2023-04-21 07:54:04 +0000

    [Telemetry] Add Lid Event Implementation
    This adds an implementation for detecting the lid event using Telemetry
    Extension.
    
    This is part of a bigger effort to add an event interface
    to the Telemetry Extension. For more context visit
    go/design-sketch-event-api.
    
    Bug: b/278191784, b/268125453
    Test: 1. unit_tests --gtest_filter="*TelemetryEvent*"
          2. extensions_unittests --gtest_filter="*TelemetryExtension*"
          3. browser_tests --gtest_filter="*TelemetryExtensionEventsApi*"
    
    Change-Id: Iead327cec84049f8dd2f1492bd9e50c9e1cc9059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4422499
    Auto-Submit: Ethan Cheng <yycheng@google.com>
    Commit-Queue: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Bastian Kersting <bkersting@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133646}

--- chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters.cc
@@ -23,6 +23,15 @@ api::os_events::AudioJackEventInfo UncheckedConvertPtr(
   return result;
 }
 
+api::os_events::LidEventInfo UncheckedConvertPtr(
+    crosapi::mojom::TelemetryLidEventInfoPtr ptr) {
+  api::os_events::LidEventInfo result;
+
+  result.event = Convert(ptr->state);
+
+  return result;
+}
+
 }  // namespace unchecked
 
 api::os_events::AudioJackEvent Convert(
@@ -52,6 +61,19 @@ api::os_events::AudioJackDeviceType Convert(
   NOTREACHED();
 }
 
+api::os_events::LidEvent Convert(
+    crosapi::mojom::TelemetryLidEventInfo::State state) {
+  switch (state) {
+    case crosapi::mojom::TelemetryLidEventInfo_State::kUnmappedEnumField:
+      return api::os_events::LidEvent::kNone;
+    case crosapi::mojom::TelemetryLidEventInfo_State::kClosed:
+      return api::os_events::LidEvent::kClosed;
+    case crosapi::mojom::TelemetryLidEventInfo_State::kOpened:
+      return api::os_events::LidEvent::kOpened;
+  }
+  NOTREACHED();
+}
+
 crosapi::mojom::TelemetryEventCategoryEnum Convert(
     api::os_events::EventCategory input) {
   switch (input) {
@@ -59,6 +81,8 @@ crosapi::mojom::TelemetryEventCategoryEnum Convert(
       return crosapi::mojom::TelemetryEventCategoryEnum::kUnmappedEnumField;
     case api::os_events::EventCategory::kAudioJack:
       return crosapi::mojom::TelemetryEventCategoryEnum::kAudioJack;
+    case api::os_events::EventCategory::kLid:
+      return crosapi::mojom::TelemetryEventCategoryEnum::kLid;
   }
   NOTREACHED();
 }
--- chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters.h
+++ chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters.h
@@ -18,6 +18,9 @@ namespace unchecked {
 api::os_events::AudioJackEventInfo UncheckedConvertPtr(
     crosapi::mojom::TelemetryAudioJackEventInfoPtr ptr);
 
+api::os_events::LidEventInfo UncheckedConvertPtr(
+    crosapi::mojom::TelemetryLidEventInfoPtr ptr);
+
 }  // namespace unchecked
 
 api::os_events::AudioJackEvent Convert(
@@ -25,6 +28,9 @@ api::os_events::AudioJackEvent Convert(
 
 api::os_events::AudioJackDeviceType Convert(
     crosapi::mojom::TelemetryAudioJackEventInfo::DeviceType device_type);
+
+api::os_events::LidEvent Convert(
+    crosapi::mojom::TelemetryLidEventInfo::State state);
 
 crosapi::mojom::TelemetryEventCategoryEnum Convert(
     api::os_events::EventCategory input);
