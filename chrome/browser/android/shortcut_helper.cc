50d58cf217995f55fb54287bbf20047ea596e4ba
Claudio DeSouza <cdesouza@igalia.com>
2023-04-21 02:17:35 +0000

    Replace base::GUID uses with base::Uuid (116/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/android
    
    This CL was uploaded by git cl split.
    
    R=dominickn@chromium.org
    
    Bug: 1428566
    Change-Id: I9434dda50dd03c97f885b0cf52fe1b01893cf7d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456448
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133558}

--- chrome/browser/android/shortcut_helper.cc
+++ chrome/browser/android/shortcut_helper.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/functional/bind.h"
-#include "base/guid.h"
+#include "base/uuid.h"
 #include "chrome/android/chrome_jni_headers/ShortcutHelper_jni.h"
 #include "components/webapps/browser/android/shortcut_info.h"
 #include "content/public/browser/manifest_icon_downloader.h"
@@ -124,7 +124,7 @@ void ShortcutHelper::AddToLauncherWithSkBitmap(
     webapps::InstallableStatusCode installable_status) {
   RecordAddToHomeScreenUKM(web_contents, info, installable_status);
 
-  std::string webapp_id = base::GenerateGUID();
+  std::string webapp_id = base::Uuid::GenerateRandomV4().AsLowercaseString();
   if (info.display == blink::mojom::DisplayMode::kStandalone ||
       info.display == blink::mojom::DisplayMode::kFullscreen ||
       info.display == blink::mojom::DisplayMode::kMinimalUi) {
