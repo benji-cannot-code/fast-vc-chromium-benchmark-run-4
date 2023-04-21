ac982f2ec4986745013e9b1d8a401aa2909bf1db
Viktor Semeniuk <vsemeniuk@google.com>
2023-04-21 20:18:53 +0000

    [Password Manager] Using extension-controlled-indicator
    This CL adds extension-controlled-indicator from settings limited
    sharing. It makes pref toggle in the new Password Manager UI look as
    it used to in the old UI. This CL also adds missing
    cr-policy-pref-indicator just like settings-toggle-button has.
    
    Old UI: https://screenshot.googleplex.com/BgzChAzruUc3EmT
    New UI: https://screenshot.googleplex.com/9rip3d3eHLLWAXm
    
    Bug: 1350947
    Change-Id: I6a5f6dc569e3cb19c62a64c0fa9b32cfcc5ee4d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454328
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134002}

--- chrome/test/data/webui/password_manager/settings_section_test.ts
+++ chrome/test/data/webui/password_manager/settings_section_test.ts
@@ -114,8 +114,8 @@ suite('SettingsSectionTest', function() {
     await flushTasks();
 
     assertTrue(settings.$.passwordToggle.checked);
-    assertTrue(!!settings.$.passwordToggle.shadowRoot!.querySelector(
-        '#extensionIcon'));
+    assertTrue(
+        !!settings.shadowRoot!.querySelector('extension-controlled-indicator'));
   });
 
   test('no extension control icon by default', async function() {
@@ -127,7 +127,7 @@ suite('SettingsSectionTest', function() {
     assertTrue(settings.$.passwordToggle.checked);
     settings.$.passwordToggle.click();
     assertFalse(!!settings.$.passwordToggle.shadowRoot!.querySelector(
-        '#extensionIcon'));
+        'extension-controlled-indicator'));
   });
 
   test('pref updated externally', async function() {
