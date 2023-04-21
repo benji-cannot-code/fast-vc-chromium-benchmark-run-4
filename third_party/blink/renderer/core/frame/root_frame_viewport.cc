924e0cb6aa36c3946a53beb20b67cc0e769e2804
Kent Tamura <tkent@chromium.org>
2023-04-21 11:24:51 +0000

    [RemoveLegacy] Remove LayoutRectOutsets usages in core/
    This CL should have no behavior changes.
    
    Bug: 1229581
    Change-Id: I6d92435b624ad42fc56418de26f0930c73f838e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4458568
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133729}

--- third_party/blink/renderer/core/frame/root_frame_viewport.cc
+++ third_party/blink/renderer/core/frame/root_frame_viewport.cc
@@ -234,7 +234,7 @@ PhysicalRect RootFrameViewport::VisibleScrollSnapportRect(
     return visible_scroll_snapport;
 
   const ComputedStyle* style = LayoutViewport().GetLayoutBox()->Style();
-  LayoutRectOutsets padding(
+  visible_scroll_snapport.ContractEdges(
       MinimumValueForLength(style->ScrollPaddingTop(),
                             visible_scroll_snapport.Height()),
       MinimumValueForLength(style->ScrollPaddingRight(),
@@ -243,7 +243,6 @@ PhysicalRect RootFrameViewport::VisibleScrollSnapportRect(
                             visible_scroll_snapport.Height()),
       MinimumValueForLength(style->ScrollPaddingLeft(),
                             visible_scroll_snapport.Width()));
-  visible_scroll_snapport.Contract(padding);
 
   return visible_scroll_snapport;
 }
