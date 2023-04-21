1bdb03c41c52dda2b9081d722b773f4a41a155ab
Claudio DeSouza <cdesouza@igalia.com>
2023-04-21 00:58:37 +0000

    Replace base::GUID uses with base::Uuid (188/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ui/webui/ash/login
    
    This CL was uploaded by git cl split.
    
    R=xiyuan@chromium.org
    
    Bug: 1428566
    Change-Id: I708583b9712c055b5136792aa587447fd599ce01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454365
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133513}

--- chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/i18n/message_formatter.h"
 #include "base/i18n/number_formatting.h"
 #include "base/json/json_reader.h"
