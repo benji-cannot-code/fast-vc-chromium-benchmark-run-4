86eac81c20d770534cc456850e7fdc4b4ac93aa4
Nic Hollingum <hollingum@google.com>
2023-04-21 06:10:13 +0000

    Revert "Reland "[Read Anything] Add ReadAnything field trial.""
    This reverts commit ec002985747b820605f86f53c6716adce0d4f1fb.
    
    Reason for revert: breaks chromium uprev https://b.corp.google.com/issues/279088859
    
    Original change's description:
    > Reland "[Read Anything] Add ReadAnything field trial."
    >
    > This is a reland of commit 78b3f60cc19db8b30b88782e6c39ee758d24ac88
    > Enables kReadAnything feature on the ExtensionWebUITest to ensure that
    > sanity_check_available_apis passes.
    >
    > Original change's description:
    > > [Read Anything] Add ReadAnything field trial.
    > >
    > > Bug: 1266555
    > > Change-Id: I4073ed437c42367eb967f9f1dea8d90448fea08e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4378497
    > > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > > Commit-Queue: Abigail Klein <abigailbklein@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1128254}
    >
    > Bug: 1266555
    > Change-Id: I141cb46330ba8f051c4d030fdd86cc29d379e289
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4436092
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Abigail Klein <abigailbklein@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1132135}
    
    Bug: 1266555
    Change-Id: I4c7844092c6d2c7e098defa3ac8a506495b29b02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457731
    Owners-Override: Alexander Alekseev <alemate@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1133620}

--- chrome/browser/extensions/extension_webui_apitest.cc
+++ chrome/browser/extensions/extension_webui_apitest.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/accessibility/accessibility_features.h"
 
 namespace extensions {
 
@@ -42,11 +41,6 @@ namespace {
 
 // Tests running extension APIs on WebUI.
 class ExtensionWebUITest : public ExtensionApiTest {
- public:
-  ExtensionWebUITest() {
-    scoped_feature_list_.InitWithFeatures({features::kReadAnything}, {});
-  }
-
  protected:
   testing::AssertionResult RunTest(const char* name,
                                    const GURL& page_url,
@@ -91,9 +85,6 @@ class ExtensionWebUITest : public ExtensionApiTest {
     // Tests should fail.
     return RunTest(name, GURL("chrome://about"), false);
   }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Tests running within an <extensionoptions>.
