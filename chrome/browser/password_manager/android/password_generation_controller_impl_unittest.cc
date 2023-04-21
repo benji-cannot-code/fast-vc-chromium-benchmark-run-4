9542b7a79059606932512f6262a9de1352d6be24
Anna Tsvirchkova <atsvirchkova@google.com>
2023-04-21 11:05:41 +0000

    Reland "[TTFPassGen] Move PasswordGenerationAgent::FocusedElementChanged call earlier"
    This is a reland of commit 3c1a89031d0a3c5ef1c89a89c637ca80b1bcc629
    
    The original CL was reverted because of the failing PasswordManagerUIUtilAndroid::ShouldNotAcceptFocusEventAfterLoosingFocus test (it's renamed to ShouldNotAcceptFocusEventAfterFrameLostFocus currently) on the android-bf-cache bot. The test was re-writted and now passes.
    
    Original change's description:
    > [TTFPassGen] Move PasswordGenerationAgent::FocusedElementChanged call earlier
    >
    > Now PasswordGenerationAgent::FocusedNodeHasChanged is called from AutofillAgent::DidCompleteFocusChangeInFrame, which happens later then the virtual keyboard is shown (when a text input element is navigated).
    > It feels more natural to move the FocusedNodeHasChanged call to AutofillAgent::HandleFocusChangeComplete, which happens before triggering the keyboard. This is needed for the password generation bottom sheet (the bottom sheet should be able to suppress the keyboard before it's triggered).
    >
    > Bug: 1421753
    > Change-Id: Icacfce86a448dc5ea3ab82a205bccb237328df60
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4304062
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1130362}
    
    Bug: 1421753, 1433296
    Change-Id: Ief1372d9849342096370becd3bd89e8c2fe2d81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431191
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133719}

--- chrome/browser/password_manager/android/password_generation_controller_impl_unittest.cc
+++ chrome/browser/password_manager/android/password_generation_controller_impl_unittest.cc
@@ -41,6 +41,7 @@ using base::ASCIIToUTF16;
 using password_manager::MockPasswordStoreInterface;
 using password_manager::PasswordForm;
 using testing::_;
+using testing::AtMost;
 using testing::ByMove;
 using testing::Eq;
 using testing::Mock;
@@ -172,6 +173,8 @@ class PasswordGenerationControllerTest
         test_pwd_manager_client_.get());
     mock_password_manager_driver_ =
         std::make_unique<NiceMock<MockPasswordManagerDriver>>();
+    mock_another_password_manager_driver_ =
+        std::make_unique<NiceMock<MockPasswordManagerDriver>>();
 
     // TODO(crbug.com/969051): Remove once kAutofillKeyboardAccessory is
     // enabled.
@@ -183,6 +186,11 @@ class PasswordGenerationControllerTest
     ON_CALL(*mock_password_manager_driver_, GetPasswordManager())
         .WillByDefault(Return(password_manager_.get()));
     ON_CALL(*mock_password_manager_driver_, GetPasswordAutofillManager())
+        .WillByDefault(Return(password_autofill_manager_.get()));
+    ON_CALL(*mock_another_password_manager_driver_, GetPasswordManager())
+        .WillByDefault(Return(password_manager_.get()));
+    ON_CALL(*mock_another_password_manager_driver_,
+            GetPasswordAutofillManager())
         .WillByDefault(Return(password_autofill_manager_.get()));
 
     mock_generation_helper_ =
@@ -220,6 +228,8 @@ class PasswordGenerationControllerTest
 
   std::unique_ptr<NiceMock<MockPasswordManagerDriver>>
       mock_password_manager_driver_;
+  std::unique_ptr<NiceMock<MockPasswordManagerDriver>>
+      mock_another_password_manager_driver_;
   std::unique_ptr<NiceMock<MockPasswordGenerationHelper>>
       mock_generation_helper_;
   std::unique_ptr<NiceMock<MockPasswordGenerationDialogView>> mock_dialog_;
@@ -244,7 +254,7 @@ void PasswordGenerationControllerTest::InitializeAutomaticGeneration(
               OnAutomaticGenerationStatusChanged(true));
 
   controller()->OnAutomaticGenerationAvailable(
-      mock_password_manager_driver_.get(), GetTestGenerationUIData1(),
+      mock_password_manager_driver_->AsWeakPtr(), GetTestGenerationUIData1(),
       gfx::RectF(100, 20));
 
   ON_CALL(*mock_generation_helper_, GeneratePassword(_, _, _, _))
@@ -273,7 +283,7 @@ TEST_F(PasswordGenerationControllerTest, RelaysAutomaticGenerationAvailable) {
   EXPECT_CALL(mock_manual_filling_controller_,
               OnAutomaticGenerationStatusChanged(true));
   controller()->OnAutomaticGenerationAvailable(
-      mock_password_manager_driver_.get(), GetTestGenerationUIData1(),
+      mock_password_manager_driver_->AsWeakPtr(), GetTestGenerationUIData1(),
       gfx::RectF(100, 20));
 }
 
@@ -287,11 +297,12 @@ TEST_F(PasswordGenerationControllerTest,
               OnAutomaticGenerationStatusChanged(true))
       .Times(2);
   controller()->OnAutomaticGenerationAvailable(
-      mock_password_manager_driver_.get(), GetTestGenerationUIData1(),
+      mock_password_manager_driver_->AsWeakPtr(), GetTestGenerationUIData1(),
       gfx::RectF(100, 20));
   PasswordGenerationUIData new_ui_data = GetTestGenerationUIData2();
   controller()->OnAutomaticGenerationAvailable(
-      mock_password_manager_driver_.get(), new_ui_data, gfx::RectF(100, 20));
+      mock_password_manager_driver_->AsWeakPtr(), new_ui_data,
+      gfx::RectF(100, 20));
 
   autofill::FormSignature form_signature =
       autofill::CalculateFormSignature(new_ui_data.form_data);
@@ -324,7 +335,7 @@ TEST_F(PasswordGenerationControllerTest,
   EXPECT_CALL(mock_manual_filling_controller_,
               OnAutomaticGenerationStatusChanged(true));
   controller()->OnAutomaticGenerationAvailable(
-      mock_password_manager_driver_.get(), GetTestGenerationUIData1(),
+      mock_password_manager_driver_->AsWeakPtr(), GetTestGenerationUIData1(),
       gfx::RectF(100, 20));
 
   EXPECT_CALL(mock_manual_filling_controller_,
@@ -388,13 +399,18 @@ TEST_F(PasswordGenerationControllerTest,
 }
 
 TEST_F(PasswordGenerationControllerTest,
-       RejectAutomaticAvailableForNonActiveFrame) {
+       SetActiveFrameOnAutomaticGenerationAvailable) {
+  // TODO(crbug.com/1421753): Refactor PasswordGenerationController so that
+  // OnAutomaticGenerationStatusChanged would be called only once. Right now
+  // it's called twice: the first call resets the manual filling controller
+  // status and the second one sets it according to the focused input.
   EXPECT_CALL(mock_manual_filling_controller_,
               OnAutomaticGenerationStatusChanged(_))
-      .Times(0);
-  MockPasswordManagerDriver wrong_driver;
+      .Times(AtMost(2));
+
   controller()->OnAutomaticGenerationAvailable(
-      &wrong_driver, GetTestGenerationUIData2(), gfx::RectF(100, 20));
+      mock_another_password_manager_driver_->AsWeakPtr(),
+      GetTestGenerationUIData2(), gfx::RectF(100, 20));
 }
 
 TEST_F(PasswordGenerationControllerTest,
@@ -439,7 +455,7 @@ TEST_F(PasswordGenerationControllerTest, HidesDialogWhenFocusChanges) {
   EXPECT_CALL(*raw_dialog_view, Destroy());
   controller()->FocusedInputChanged(
       FocusedFieldType::kFillableUsernameField,
-      base::AsWeakPtr(mock_password_manager_driver_.get()));
+      mock_another_password_manager_driver_->AsWeakPtr());
   Mock::VerifyAndClearExpectations(raw_dialog_view);
 }
 
@@ -498,7 +514,7 @@ TEST_F(PasswordGenerationControllerTest, DontShowManualDialogIfFocusChanged) {
               OnAutomaticGenerationStatusChanged(false));
   controller()->FocusedInputChanged(
       FocusedFieldType::kFillablePasswordField,
-      base::AsWeakPtr(mock_password_manager_driver_.get()));
+      mock_another_password_manager_driver_->AsWeakPtr());
   EXPECT_CALL(mock_dialog_factory(), Run).Times(0);
   controller()->ShowManualGenerationDialog(mock_password_manager_driver_.get(),
                                            GetTestGenerationUIData1());
