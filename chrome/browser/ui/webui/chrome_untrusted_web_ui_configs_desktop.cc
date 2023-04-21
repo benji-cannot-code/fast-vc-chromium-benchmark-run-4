e7e2b89f1719b677601d57e0b8ec703491c54d50
Abigail Klein <abigailbklein@google.com>
2023-04-21 16:31:57 +0000

    [Read Anything] Migrate Read Anything WebUI to chrome-untrusted.
    The Read Anything WebUI has the architecture:
    [renderer] app.ts <-- v8 --> read_anything_app_controller.cc
    <-- mojo --> [browser] read_anything_page_handler.cc.
    
    Setting up this communication happens as follows:
    1. In browser process: ReadAnythingUI serves app.html and app.ts
    sources.
    2. In renderer process: ChromeRenderFrameObserver constructs
    ReadAnythingAppController which creates a v8 context.
    3. When app.ts connects, calls ReadAnythingAppController::OnConnected.
    4. ReadAnythingAppController passes a factory containing a
    bi-directional mojo pipe (remote and receiver) to ReadAnythingUI in
    the browser which constructs ReadAnythingPageHandler and transfers
    pipe ownership there.
    
    For more details, see go/read-anything-v8.
    
    This CL does the following:
    1. Make ReadAnythingUI a standalone untrusted according to
    https://chromium.googlesource.com/chromium/src/+/HEAD/docs/chrome_untrusted.md#How-do-I-use-chrome_untrusted
    2. Override content security policies.
    3. ChromeRenderFrameObserver only installs ReadAnythingAppController
    when the document url matches kChromeUIReadAnythingSidePanelURL. Untrusted WebUIs do not have enabled bindings, so the former method is
    no longer valid (also, the new way is more specific).
    4. Disables read_anything_app_test.ts. Test is migrated and re-enabled
    in https://crrev.com/c/4343530.
    
    Related CLs: https://crrev.com/c/4412525, https://crrev.com/c/4436076
    
    
    Bug: 1266555
    Change-Id: Id3d5cf545f2c293f38dd541a16d5a1ec93d059d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250940
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133866}

--- chrome/browser/ui/webui/chrome_untrusted_web_ui_configs_desktop.cc
+++ chrome/browser/ui/webui/chrome_untrusted_web_ui_configs_desktop.cc
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/chrome_untrusted_web_ui_configs_desktop.h"
 
+#include <memory>
+
 #include "build/branding_buildflags.h"
 #include "chrome/browser/companion/core/features.h"
 #include "chrome/browser/ui/webui/feed/feed_ui_config.h"
 #include "chrome/browser/ui/webui/side_panel/companion/companion_side_panel_untrusted_ui.h"
+#include "chrome/browser/ui/webui/side_panel/read_anything/read_anything_ui.h"
 #include "components/lens/buildflags.h"
 #include "content/public/browser/webui_config_map.h"
+#include "ui/accessibility/accessibility_features.h"
 
 #if BUILDFLAG(ENABLE_LENS_DESKTOP_GOOGLE_BRANDED_FEATURES)
 #include "chrome/browser/ui/webui/lens/lens_untrusted_ui_config.h"
@@ -26,6 +30,9 @@ void RegisterDesktopChromeUntrustedWebUIConfigs() {
     map.AddUntrustedWebUIConfig(
         std::make_unique<CompanionSidePanelUntrustedUIConfig>());
   }
+  map.AddUntrustedWebUIConfig(
+      std::make_unique<ReadAnythingUIUntrustedConfig>());
+
 #if BUILDFLAG(ENABLE_LENS_DESKTOP_GOOGLE_BRANDED_FEATURES)
   map.AddUntrustedWebUIConfig(std::make_unique<lens::LensUntrustedUIConfig>());
 #endif
--- chrome/test/data/webui/side_panel/side_panel_browsertest.js
+++ chrome/test/data/webui/side_panel/side_panel_browsertest.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
 
 GEN('#include "content/public/test/browser_test.h"');
-GEN('#include "ui/accessibility/accessibility_features.h"');
 GEN('#include "components/user_notes/user_notes_features.h"');
 GEN('#include "components/power_bookmarks/core/power_bookmark_features.h"');
 
@@ -124,22 +123,6 @@ var ReadingListAppTest = class extends SidePanelBrowserTest {
 };
 
 TEST_F('ReadingListAppTest', 'All', function() {
-  mocha.run();
-});
-
-var ReadAnythingAppTest = class extends SidePanelBrowserTest {
-  /** @override */
-  get browsePreload() {
-    return 'chrome://read-anything-side-panel.top-chrome/test_loader.html?module=side_panel/read_anything/read_anything_app_test.js';
-  }
-
-  /** @override */
-  get featureList() {
-    return {enabled: ['features::kReadAnything']};
-  }
-};
-
-TEST_F('ReadAnythingAppTest', 'All', function() {
   mocha.run();
 });
 
