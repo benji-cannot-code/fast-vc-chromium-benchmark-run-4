d3e5f125dde7d560655b4c1559c9821363213a8a
Ankush Singh <ankushkush@google.com>
2023-04-21 18:15:21 +0000

    [sync] Add metric to measure syncable service start time
    Bug: 1416480
    Change-Id: I3e272bd18eb0d77704b359a9ae1e14ecf0068e60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4444534
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1133922}

--- components/sync/model/syncable_service_based_bridge.cc
+++ components/sync/model/syncable_service_based_bridge.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/strings/stringprintf.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "components/sync/base/client_tag_hash.h"
 #include "components/sync/model/conflict_resolution.h"
@@ -221,6 +223,8 @@ SyncableServiceBasedBridge::SyncableServiceBasedBridge(
       syncable_service_(syncable_service),
       syncable_service_started_(false) {
   DCHECK(syncable_service_);
+
+  init_start_time_ = base::Time::Now();
 
   std::move(store_factory)
       .Run(type_, base::BindOnce(&SyncableServiceBasedBridge::OnStoreCreated,
@@ -452,7 +456,17 @@ void SyncableServiceBasedBridge::OnSyncableServiceReady(
   // possible (regardless of whether sync actually starts or not). Otherwise,
   // the SyncableService will be started from MergeFullSyncData().
   if (change_processor()->IsTrackingMetadata()) {
-    ReportErrorIfSet(StartSyncableService());
+    if (auto error = StartSyncableService()) {
+      change_processor()->ReportError(*error);
+    } else {
+      // Using the same range as Sync.ModelTypeConfigurationTime.* metric.
+      base::UmaHistogramCustomTimes(
+          base::StringPrintf("Sync.SyncableServiceStartTime.%s",
+                             ModelTypeToHistogramSuffix(type_)),
+          base::Time::Now() - init_start_time_,
+          /*min=*/base::Milliseconds(1),
+          /*max=*/base::Seconds(60), /*buckets=*/50);
+    }
   }
 }
 
