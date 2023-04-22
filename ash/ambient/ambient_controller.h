8d0907abbf8e8c29c197b5efaafbe147ecbff47b
Eric Sum <esum@google.com>
2023-04-22 04:17:50 +0000

    Migrate metrics to AmbientSessionMetricsRecorder.
    First, this fixes the crash in the linked bug. The crash can be
    reproduced if you launch ambient mode and then shutdown chrome
    while ambient mode is still running. It crashes while trying to
    access the user's pref service (which doesn't exist on shutdown
    to get the ambient theme that was rendering for metrics purposes.
    This fixes the crash by basically using the cached ambient theme
    in AmbientSessionMetricsRecorder to record metrics on shutdown
    rather than retrieving it from pref service.
    
    Second, this takes more logic out of AmbientController, which is
    the goal given that it's taken on a lot of business logic
    recently.
    
    Bug: 1428481
    
    Change-Id: Ifc59f96946a9096329c7a27a7c34a97742a643f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456527
    Commit-Queue: Eric Sum <esum@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134181}

--- ash/ambient/ambient_controller.h
+++ ash/ambient/ambient_controller.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/device/public/mojom/fingerprint.mojom.h"
 #include "services/device/public/mojom/wake_lock.mojom.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/user_activity/user_activity_detector.h"
 #include "ui/base/user_activity/user_activity_observer.h"
 #include "ui/events/event_handler.h"
@@ -307,9 +306,6 @@ class ASH_EXPORT AmbientController
 
   // Observes user profile prefs for ambient.
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
-
-  // Used to record Ambient mode engagement metrics.
-  absl::optional<base::Time> start_time_ = absl::nullopt;
 
   // Records the time when preview widgets are created.
   base::Time preview_widget_created_at_;
