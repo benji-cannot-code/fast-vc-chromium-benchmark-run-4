d9cc0ae6efbd5f27a60f3b3ce51095a409400c0a
Alex Moshchuk <alexmos@chromium.org>
2023-04-22 08:31:34 +0000

    Remove expired RoutingIDSpaceUsed histogram.
    This metric has been around for a while, and it seems we've learned
    everything we wanted from it, namely, that we're nowhere close to
    using the full routing ID space, and virtually all usage is less than
    1%.  The metric has recently expired, so remove the code for logging
    it.
    
    Bug: 1421808
    Change-Id: I259384012cbad4554f92c0c61c2148dfb932fdd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456514
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134197}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/writable_shared_memory_region.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/metrics/histogram_base.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/metrics/persistent_memory_allocator.h"
@@ -1658,10 +1657,6 @@ RenderProcessHostImpl::~RenderProcessHostImpl() {
   // "Browser.RenderProcessHostImpl"
   TRACE_EVENT_END("shutdown", perfetto::Track::FromPointer(this),
                   ChromeTrackEvent::kRenderProcessHost, *this);
-
-  base::UmaHistogramPercentage(
-      "BrowserRenderProcessHost.RoutingIDSpaceUsed",
-      100. * GetNextRoutingID() / std::numeric_limits<int32_t>::max());
 }
 
 bool RenderProcessHostImpl::Init() {
