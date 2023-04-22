1f0deba94836cba89945bc37b5b45b9f9e490e88
Ruma Kesh <rkesh@google.com>
2023-04-22 01:30:12 +0000

    CrOS Settings: Convert change dictation locale dialog browser test to TS
    Additionally, moved the test under the `os_a11y_page/` folder to mirror
    the app folder structure.
    
    gtest_filter="*OsA11yPageChangeDictationLocaleDialog*"
    
    Bug: b/270728282
    Test: browser_tests --
    Change-Id: Ibe423ec112f219cecef72ba7f4bc13978e411512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457493
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134164}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/change_dictation_locale_dialog.ts
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/change_dictation_locale_dialog.ts
@@ -17,6 +17,7 @@ import 'chrome://resources/polymer/v3_0/paper-ripple/paper-ripple.js';
 import '../../settings_shared.css.js';
 import '../os_languages_page/shared_style.css.js';
 
+import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import {CrSearchFieldElement} from 'chrome://resources/cr_elements/cr_search_field/cr_search_field.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
@@ -31,7 +32,7 @@ import {getTemplate} from './change_dictation_locale_dialog.html.js';
  * pack for the locale is installed, and whether it should be highlighted as
  * recommended to the user.
  */
-interface DictationLocaleOption {
+export interface DictationLocaleOption {
   name: string;
   value: string;
   worksOffline: boolean;
@@ -45,6 +46,8 @@ export interface ChangeDictationLocaleDialog {
     changeDictationLocaleDialog: CrDialogElement,
     recommendedLocalesList: IronListElement,
     search: CrSearchFieldElement,
+    cancel: CrButtonElement,
+    update: CrButtonElement,
   };
 }
 
@@ -53,7 +56,7 @@ const ChangeDictationLocaleDialogBase = I18nMixin(PolymerElement);
 export class ChangeDictationLocaleDialog extends
     ChangeDictationLocaleDialogBase {
   static get is() {
-    return 'os-settings-change-dictation-locale-dialog';
+    return 'os-settings-change-dictation-locale-dialog' as const;
   }
 
   static get template() {
@@ -305,6 +308,12 @@ export class ChangeDictationLocaleDialog extends
       this.set('pref.value', this.selectedLocale_.value);
     }
     this.$.changeDictationLocaleDialog.close();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ChangeDictationLocaleDialog.is]: ChangeDictationLocaleDialog;
   }
 }
 
