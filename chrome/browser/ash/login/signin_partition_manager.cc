7b8948caadf9625012004bd14270223e8d7a7c0a
Claudio DeSouza <cdesouza@igalia.com>
2023-04-22 00:46:12 +0000

    Replace base::GUID uses with base::Uuid (128/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ash/login
    
    This CL was uploaded by git cl split.
    
    R=tbarzic@chromium.org
    
    Bug: 1428566
    Change-Id: I54a00a53296ad015bd64917cf9726027f9811d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454358
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1134131}

--- chrome/browser/ash/login/signin_partition_manager.cc
+++ chrome/browser/ash/login/signin_partition_manager.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
+#include "base/uuid.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
@@ -27,7 +27,7 @@ namespace {
 
 // Generates a new unique StoragePartition name.
 std::string GeneratePartitionName() {
-  return base::GenerateGUID();
+  return base::Uuid::GenerateRandomV4().AsLowercaseString();
 }
 
 // Clears data from the passed storage partition. `partition_data_cleared`
