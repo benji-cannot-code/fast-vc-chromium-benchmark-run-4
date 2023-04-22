48592bc8eb99b2520146959fe05a17591ef437db
Jennifer Serrano <jennserrano@google.com>
2023-04-22 01:02:37 +0000

    [QuickStart] Message sending for forced update
    In this CL, before the target device performs a forced update, message is sent to source device containing secondary shared secret and session ID.
    
    TEST=Modified unit test to ensure values being sent are found in message.
    
    bug: b/276458936
    
    Change-Id: I4900d82e5301a61db38c7cb2a6569c66648dbe3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420229
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134156}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/connection.cc
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/connection.cc
@@ -25,12 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::quick_start {
 
-namespace {
-
-const char kNotifySourceOfUpdateMessageKey[] = "isForcedUpdateRequired";
-
-}  // namespace
-
 Connection::Factory::~Factory() = default;
 
 std::unique_ptr<Connection> Connection::Factory::Create(
@@ -94,10 +88,14 @@ void Connection::RequestWifiCredentials(
                      weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 }
 
-void Connection::NotifySourceOfUpdate() {
-  base::Value::Dict message_payload;
-  message_payload.Set(kNotifySourceOfUpdateMessageKey, true);
-  SendPayload(message_payload);
+void Connection::NotifySourceOfUpdate(int32_t session_id) {
+  // Send message to source that target device will perform an update.
+  std::string shared_secret_str(secondary_shared_secret_.begin(),
+                                secondary_shared_secret_.end());
+
+  SendMessage(
+      requests::BuildNotifySourceOfUpdateMessage(session_id, shared_secret_str),
+      absl::nullopt);
 }
 
 void Connection::RequestAccountTransferAssertion(
@@ -193,10 +191,15 @@ void Connection::GenerateFidoAssertionInfo(
   std::move(callback).Run(assertion_info);
 }
 
-void Connection::SendMessage(std::unique_ptr<QuickStartMessage> message,
-                             ConnectionResponseCallback callback) {
+void Connection::SendMessage(
+    std::unique_ptr<QuickStartMessage> message,
+    absl::optional<ConnectionResponseCallback> callback) {
   SendPayload(*message->GenerateEncodedMessage());
-  nearby_connection_->Read(std::move(callback));
+  if (!callback.has_value()) {
+    return;
+  }
+
+  nearby_connection_->Read(std::move(callback.value()));
 }
 
 void Connection::InitiateHandshake(const std::string& authentication_token,
