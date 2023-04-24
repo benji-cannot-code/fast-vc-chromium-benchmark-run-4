e0319143dd25e07c87dc6d87832a04352871156b
Toni Barzic <tbarzic@chromium.org>
2023-04-24 19:13:01 +0000

    Fix how PagedAppsGridView a11y node data is set
    PagedAppsGridView adds kClipsChildren attribute in its accessible node
    data before calling GetAccessibleNodeData on the base class. The default
    view implementation of the method will set its own node data,
    effectively overriding the property set by PagedAppsGridView. To fix
    this, switch the order in which properties are set.
    
    Change-Id: I11c7ba8d114df76d2b64507c6d9fd9e5c6a0aabe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4468327
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134755}

--- ash/app_list/views/paged_apps_grid_view.cc
+++ ash/app_list/views/paged_apps_grid_view.cc
@@ -329,8 +329,8 @@ void PagedAppsGridView::Layout() {
 }
 
 void PagedAppsGridView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  node_data->AddBoolAttribute(ax::mojom::BoolAttribute::kClipsChildren, true);
   AppsGridView::GetAccessibleNodeData(node_data);
+  node_data->AddBoolAttribute(ax::mojom::BoolAttribute::kClipsChildren, true);
 }
 
 void PagedAppsGridView::OnThemeChanged() {
