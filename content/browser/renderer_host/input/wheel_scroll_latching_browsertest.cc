432241e63f6946e38b6a7fac8625fc5759ae0aaa
Florian Jacky <fjacky@chromium.org>
2023-04-24 09:54:42 +0000

    Revert "Remove ExecuteScriptAndExtractString under content/"
    This reverts commit 241506b6bce6801ba38c16bc5b101e51d6529b5e.
    
    Reason for revert: Potential culprit for failures of RenderFrameHostManagerUnloadBrowserTest.PostMessageToParentWhenSubframeNavigates
    
    Bug: 1438424
    
    Original change's description:
    > Remove ExecuteScriptAndExtractString under content/
    >
    > Bug: 1157718
    > Change-Id: Ib894045f0d69c109fb29f7f1c9b4b73123d2a9fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450569
    > Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1132808}
    
    Bug: 1157718
    Change-Id: Ib78e571e81912494217cddb770fc1b66c9578e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4463101
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Owners-Override: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134469}

--- content/browser/renderer_host/input/wheel_scroll_latching_browsertest.cc
+++ content/browser/renderer_host/input/wheel_scroll_latching_browsertest.cc
@@ -121,6 +121,9 @@ class WheelScrollLatchingBrowserTest : public ContentBrowserTest {
   double ExecuteScriptAndExtractDouble(const std::string& script) {
     return EvalJs(shell(), script).ExtractDouble();
   }
+  std::string ExecuteScriptAndExtractString(const std::string& script) {
+    return EvalJs(shell(), script).ExtractString();
+  }
 };
 
 // Start scrolling by mouse wheel on the document: the wheel event will be sent
@@ -370,7 +373,7 @@ IN_PROC_BROWSER_TEST_F(WheelScrollLatchingBrowserTest,
   // Run until we get the callback, then check the target.
   EXPECT_EQ(blink::mojom::InputEventResultState::kConsumed,
             wheel_msg_watcher->WaitForAck());
-  EXPECT_EQ("blueDiv", EvalJs(shell(), "domTarget"));
+  EXPECT_EQ("blueDiv", ExecuteScriptAndExtractString("domTarget"));
 
   // Send the second wheel event.
   wheel_msg_watcher = std::make_unique<InputMsgWatcher>(
@@ -382,7 +385,7 @@ IN_PROC_BROWSER_TEST_F(WheelScrollLatchingBrowserTest,
   // Run until we get the callback, then check the target.
   EXPECT_EQ(blink::mojom::InputEventResultState::kNotConsumed,
             wheel_msg_watcher->WaitForAck());
-  EXPECT_EQ("redDiv", EvalJs(shell(), "domTarget"));
+  EXPECT_EQ("redDiv", ExecuteScriptAndExtractString("domTarget"));
 }
 
 }  // namespace content
