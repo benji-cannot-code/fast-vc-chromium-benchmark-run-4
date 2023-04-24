3e9fe71e37be085a61c7925d81d3298ceeb70728
Dmitry Vykochko <vykochko@google.com>
2023-04-24 11:37:37 +0000

    [Leipzig][Desktop] Source notice in migration address editor.
    Added profile target source notice to the address editor:
    https://screenshot.googleplex.com/6Gyg7MQVuaCtC2q
    
    Bug: 1421688
    Change-Id: Ia70ab9a2723ca260ac3920ef3ab4a3a6e1c0f7c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4463122
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1134509}

--- chrome/browser/ui/autofill/save_update_address_profile_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/save_update_address_profile_bubble_controller_impl.cc
@@ -301,7 +301,6 @@ std::u16string SaveUpdateAddressProfileBubbleControllerImpl::GetFooterMessage()
     absl::optional<AccountInfo> account =
         GetPrimaryAccountInfoFromBrowserContext(
             web_contents()->GetBrowserContext());
-    CHECK(account);
 
     int string_id =
         IsSaveBubble()
@@ -337,7 +336,7 @@ void SaveUpdateAddressProfileBubbleControllerImpl::OnEditButtonClicked() {
   EditAddressProfileDialogControllerImpl* controller =
       EditAddressProfileDialogControllerImpl::FromWebContents(web_contents());
   controller->OfferEdit(address_profile_, GetOriginalProfile(),
-                        GetFooterMessage(),
+                        GetEditorFooterMessage(),
                         std::move(address_profile_save_prompt_callback_),
                         is_migration_to_account_);
   HideBubble();
@@ -401,6 +400,20 @@ void SaveUpdateAddressProfileBubbleControllerImpl::DoShowBubble() {
                             web_contents(), this, shown_by_user_gesture_));
   }
   DCHECK(bubble_view());
+}
+
+std::u16string
+SaveUpdateAddressProfileBubbleControllerImpl::GetEditorFooterMessage() const {
+  if (is_migration_to_account_) {
+    absl::optional<AccountInfo> account =
+        GetPrimaryAccountInfoFromBrowserContext(
+            web_contents()->GetBrowserContext());
+    return l10n_util::GetStringFUTF16(
+        IDS_AUTOFILL_SAVE_IN_ACCOUNT_PROMPT_ADDRESS_SOURCE_NOTICE,
+        base::UTF8ToUTF16(account->email));
+  }
+
+  return GetFooterMessage();
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(SaveUpdateAddressProfileBubbleControllerImpl);
