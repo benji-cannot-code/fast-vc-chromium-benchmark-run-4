3ab98810b87f4781f4281f967d07bf28a974555b
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-04-24 19:11:51 +0000

    [Extensions] Enable runtime.getContexts() on dev channel
    Enable runtime.getContexts() on dev channel and remove the need for
    a commandline flag. The API is in a good spot to open it up for
    developers to test it out.
    
    Bug: 1426192
    Change-Id: I2d028e907e2706b298fc8f3522da742416cce157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4461851
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134753}

--- chrome/browser/extensions/api/runtime/runtime_apitest.cc
+++ chrome/browser/extensions/api/runtime/runtime_apitest.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/offscreen_document_host.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/test_extension_registry_observer.h"
-#include "extensions/common/extension_features.h"
 #include "extensions/common/features/feature_channel.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
@@ -613,10 +612,7 @@ IN_PROC_BROWSER_TEST_P(BackgroundPageOnlyRuntimeApiTest,
 
 class RuntimeGetContextsApiTest : public ExtensionApiTest {
  public:
-  RuntimeGetContextsApiTest() {
-    feature_list_.InitAndEnableFeature(
-        extensions_features::kApiRuntimeGetContexts);
-  }
+  RuntimeGetContextsApiTest() = default;
   RuntimeGetContextsApiTest(const RuntimeGetContextsApiTest&) = delete;
   RuntimeGetContextsApiTest& operator=(const RuntimeGetContextsApiTest&) =
       delete;
@@ -728,7 +724,6 @@ class RuntimeGetContextsApiTest : public ExtensionApiTest {
   raw_ptr<const Extension, DanglingUntriaged> extension_ = nullptr;
   TestExtensionDir test_dir_;
   ScopedCurrentChannel channel_override_{version_info::Channel::UNKNOWN};
-  base::test::ScopedFeatureList feature_list_;
 };
 
 // Tests retrieving the background service worker context using
