9400f2762973b46624078834f1135046f2e91fab
Taylor Bergquist <tbergquist@chromium.org>
2023-04-24 22:11:58 +0000

    Keep saved tab groups closed by Close Tabs To The Right / Close Other Tabs.
    Bug: 1439628
    Change-Id: If5a2d1d5ac498b2c4cb7d8da5057c01a9caa802f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4435949
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134867}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_model_listener.h
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_model_listener.h
@@ -20,13 +20,6 @@ class SavedTabGroupModel;
 class TabStripModel;
 class Profile;
 
-struct TabGroupIdHash {
- public:
-  size_t operator()(const tab_groups::TabGroupId& group_id) const {
-    return base::TokenHash()(group_id.token());
-  }
-};
-
 // Serves to maintain and listen to browsers who contain saved tab groups and
 // update the model if a saved tab group was changed.
 class SavedTabGroupModelListener : public BrowserListObserver,
@@ -71,7 +64,7 @@ class SavedTabGroupModelListener : public BrowserListObserver,
   // Testing Accessors.
   std::unordered_map<tab_groups::TabGroupId,
                      LocalTabGroupListener,
-                     TabGroupIdHash>&
+                     tab_groups::TabGroupIdHash>&
   GetLocalTabGroupListenerMapForTesting() {
     return local_tab_group_listeners_;
   }
@@ -80,7 +73,7 @@ class SavedTabGroupModelListener : public BrowserListObserver,
   // The LocalTabGroupListeners for each saved tab group that's currently open.
   std::unordered_map<tab_groups::TabGroupId,
                      LocalTabGroupListener,
-                     TabGroupIdHash>
+                     tab_groups::TabGroupIdHash>
       local_tab_group_listeners_;
   raw_ptr<SavedTabGroupModel> model_ = nullptr;
   raw_ptr<Profile> profile_;
