0e45545f3b3a13d759a1ef66675596ff6b62b9db
Noémie St-Onge <noemies@google.com>
2023-04-24 23:43:33 +0000

    [iOS] Add methods to save reused, weak and muted passwords for EG tests
    This CL adds methods to PasswordSettingsAppInterface to save reused,
    weak and muted compromised passwords. These new methods are going to be
    used in incoming EG tests for the new Password Checkup feature.
    
    Bug: 1406540
    Change-Id: Icf14a8f5c220e27d5cda29a94e2873f20c0f0cd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454928
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1134921}

--- ios/chrome/browser/ui/settings/password/password_settings_app_interface.h
+++ ios/chrome/browser/ui/settings/password/password_settings_app_interface.h
@@ -49,10 +49,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                userName:(NSString*)userName
                  origin:(NSString*)origin;
 
-// Creates password form which is leaked.
-+ (BOOL)saveInsecurePassword:(NSString*)password
-                    userName:(NSString*)userName
-                      origin:(NSString*)origin;
+// Creates a compromised password form.
++ (BOOL)saveCompromisedPassword:(NSString*)password
+                       userName:(NSString*)userName
+                         origin:(NSString*)origin;
+
+// Creates a muted compromised password form.
++ (BOOL)saveMutedCompromisedPassword:(NSString*)password
+                            userName:(NSString*)userName
+                              origin:(NSString*)origin;
+
+// Creates a reused password form.
++ (BOOL)saveReusedPassword:(NSString*)password
+                  userName:(NSString*)userName
+                    origin:(NSString*)origin;
+
+// Creates a weak password form.
++ (BOOL)saveWeakPassword:(NSString*)password
+                userName:(NSString*)userName
+                  origin:(NSString*)origin;
 
 // Creates a blocked password form for given origin.
 + (BOOL)saveExampleBlockedOrigin:(NSString*)origin;
