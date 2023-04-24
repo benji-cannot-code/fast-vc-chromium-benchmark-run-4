c088eab0c377030c5da4850f3fc9dab974faa015
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2023-04-24 14:59:57 +0000

    Add an absolute threshold to heuristic memory saver
    This finch-controllable threshold must be exceeded as well as the %
    threshold for discarding to start happening. This is to avoid cases
    the threshold is set to e.g. 5%, and we want to avoid discarding on a
    machine with 128Gb of installed RAM that has 6Gb free.
    
    In this case, the absolute threshold could be set to e.g. 1Gb, which
    would result in an 8Gb machine starting to discard tabs at 5% remaining
    but the 128Gb machine would start discarding when it has 1Gb available.
    
    Bug: 1420344
    Change-Id: I19615048065c2004d788d9327fd257eb4e756775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4447541
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134597}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -178,6 +178,8 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
                                             HeuristicMemorySaverPolicy>(
         performance_manager::features::
             kHeuristicMemorySaverAvailableMemoryThresholdPercent.Get(),
+        performance_manager::features::
+            kHeuristicMemorySaverAvailableMemoryThresholdMb.Get(),
         base::Seconds(
             performance_manager::features::
                 kHeuristicMemorySaverThresholdReachedHeartbeatSeconds.Get()),
