c93751d6356f9fe2cbb5e86f393af5e73e616efa
Federica Germinario <fedegermi@google.com>
2023-04-24 09:39:54 +0000

    Remove expired histograms
    This CL removes the following expired histograms:
     - Android.ThumbnailDiskStorage.CachedBitmap.Found
     - Android.ThumbnailDiskStorage.Size
    
    Fixed: 1384797
    Change-Id: I3e418e47ee71514d25dd6a5996eed98cff7808d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454312
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134459}

--- chrome/browser/thumbnail/generator/android/java/src/org/chromium/chrome/browser/thumbnail/generator/ThumbnailDiskStorage.java
+++ chrome/browser/thumbnail/generator/android/java/src/org/chromium/chrome/browser/thumbnail/generator/ThumbnailDiskStorage.java
@@ -22,7 +22,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.StreamUtil;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.base.task.BackgroundOnlyAsyncTask;
 import org.chromium.chrome.browser.thumbnail.generator.ThumbnailCacheEntry.ContentId;
@@ -160,9 +159,6 @@ public class ThumbnailDiskStorage implements ThumbnailGeneratorCallback {
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
-            RecordHistogram.recordBooleanHistogram(
-                    "Android.ThumbnailDiskStorage.CachedBitmap.Found", bitmap != null);
-
             if (bitmap != null) {
                 onThumbnailRetrieved(mRequest.getContentId(), bitmap, mRequest.getIconSize());
                 return;
@@ -314,9 +310,6 @@ public class ThumbnailDiskStorage implements ThumbnailGeneratorCallback {
                 Log.e(TAG, "Error while reading from disk.", e);
             }
         }
-
-        RecordHistogram.recordMemoryKBHistogram("Android.ThumbnailDiskStorage.Size",
-                (int) (mSizeBytes / ConversionUtils.BYTES_PER_KILOBYTE));
     }
 
     /**
