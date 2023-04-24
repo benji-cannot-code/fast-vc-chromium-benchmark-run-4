26dfbd27c658f686c97aad9d243988d78a8b7454
Ashik K <ashikk@opera.com>
2023-04-24 18:01:27 +0000

    Destroy the old menu on "Select All"
    We do not want the old menu to be drawn again at a new place
    when "Select All" is clicked.
    
    So immediately destroy it in the selection change handler
    when it is called for "Select All".
    
    Without this, the old menu is redrawn and it is especially noticeable
    when the old menu is a of different type (like the expanded vertical
    menu that gets populated when clicking on the 3 dots).
    
    Bug: 1433585
    Change-Id: I1d68906abf5467ed5f352520d9a835c6bdd29017
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4432610
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Ashik Kizhakkepallathu <ashikk@opera.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134717}

--- content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
@@ -167,6 +167,10 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
     // Tracks whether a touch selection is currently active.
     private boolean mHasSelection;
 
+    // If we are currently processing a Select All request from the menu. Used to
+    // dismiss the old menu so that it won't be preserved and redrawn at a new anchor.
+    private boolean mIsProcessingSelectAll;
+
     // Lazily created paste popup menu, triggered either via long press in an
     // editable region or from tapping the insertion handle.
     private PastePopupMenu mPastePopupMenu;
@@ -1057,6 +1061,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
      */
     @VisibleForTesting
     public void selectAll() {
+        mIsProcessingSelectAll = true;
         mWebContents.selectAll();
         mClassificationResult = null;
         // Even though the above statement logged a SelectAll user action, we want to
@@ -1441,7 +1446,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
     @CalledByNative
     /* package */ void onSelectionChanged(String text) {
         final boolean unSelected = TextUtils.isEmpty(text) && hasSelection();
-        if (unSelected) {
+        if (unSelected || mIsProcessingSelectAll) {
             if (mSmartSelectionEventProcessor != null) {
                 mSmartSelectionEventProcessor.onSelectionAction(mLastSelectedText,
                         mLastSelectionOffset, SelectionEvent.ACTION_ABANDON,
@@ -1453,6 +1458,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
         if (mSelectionClient != null) {
             mSelectionClient.onSelectionChanged(text);
         }
+        mIsProcessingSelectAll = false;
     }
 
     /**
