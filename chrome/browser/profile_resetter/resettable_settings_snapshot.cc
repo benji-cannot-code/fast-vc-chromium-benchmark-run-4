a53c0bbf38cc683feea1f96db03378496ed48ec6
Claudio DeSouza <cdesouza@igalia.com>
2023-04-24 11:37:19 +0000

    Replace base::GUID uses with base::Uuid (160/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/profile_resetter
    
    This CL was uploaded by git cl split.
    
    R=vasilii@chromium.org
    
    Bug: 1428566
    Change-Id: Ia531c0480950cfc09e7c14d65af469c4e67d253f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454514
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134508}

--- chrome/browser/profile_resetter/resettable_settings_snapshot.cc
+++ chrome/browser/profile_resetter/resettable_settings_snapshot.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/bind.h"
-#include "base/guid.h"
 #include "base/hash/md5.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
@@ -86,7 +86,7 @@ ResettableSettingsSnapshot::ResettableSettingsSnapshot(Profile* profile)
 
   // Calculate the MD5 sum of the GUID to make sure that no part of the GUID
   // contains information identifying the sender of the report.
-  guid_ = base::MD5String(base::GenerateGUID());
+  guid_ = base::MD5String(base::Uuid::GenerateRandomV4().AsLowercaseString());
 }
 
 ResettableSettingsSnapshot::~ResettableSettingsSnapshot() {
