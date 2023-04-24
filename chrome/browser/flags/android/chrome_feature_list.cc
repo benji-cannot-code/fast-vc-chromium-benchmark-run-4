2623d52f2d015ba74da81de1588e7c2d5ee8f86c
Peter E Conn <peconn@chromium.org>
2023-04-24 15:53:45 +0000

    ✉️  Add flag for TWA PostMessage.
    Also removes CCTPostMessageOrigin as that will be launched alongside
    TrustedWebActivityPostMessage so this simplifies things.
    
    Bug: 1418044
    Change-Id: Iedff32a12817c5d62c224251c3e6da27ec2cd00a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4439695
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134625}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -203,7 +203,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTIntentFeatureOverrides,
     &kCCTNewDownloadTab,
     &kCCTPostMessageAPI,
-    &kCCTPostMessageOrigin,
     &kCCTPrefetchDelayShowOnStart,
     &kCCTRealTimeEngagementSignals,
     &kCCTRealTimeEngagementSignalsAlternativeImpl,
@@ -593,10 +592,6 @@ BASE_FEATURE(kCCTIntentFeatureOverrides,
 BASE_FEATURE(kCCTPostMessageAPI,
              "CCTPostMessageAPI",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kCCTPostMessageOrigin,
-             "CCTPostMessageOrigin",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTPrefetchDelayShowOnStart,
              "CCTPrefetchDelayShowOnStart",
