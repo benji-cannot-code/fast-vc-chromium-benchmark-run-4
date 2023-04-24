404e7ee772453766b7071de6de090d47c0e35d1c
Claudio DeSouza <cdesouza@igalia.com>
2023-04-24 10:31:25 +0000

    Replace base::GUID uses with base::Uuid (162/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/push_messaging
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: 1428566
    Change-Id: I8e0b865a213cca42bb3b8cc76150b6803528c8d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455796
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134480}

--- chrome/browser/push_messaging/push_messaging_app_identifier.cc
+++ chrome/browser/push_messaging/push_messaging_app_identifier.cc
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "base/check_op.h"
-#include "base/guid.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
@@ -130,7 +130,8 @@ PushMessagingAppIdentifier PushMessagingAppIdentifier::GenerateInternal(
     const absl::optional<base::Time>& expiration_time) {
   // Use uppercase GUID for consistency with GUIDs Push has already sent to GCM.
   // Also allows detecting case mangling; see code commented "crbug.com/461867".
-  std::string guid = base::ToUpperASCII(base::GenerateGUID());
+  std::string guid =
+      base::ToUpperASCII(base::Uuid::GenerateRandomV4().AsLowercaseString());
   if (use_instance_id) {
     guid.replace(guid.size() - kGuidSuffixLength, kGuidSuffixLength,
                  kInstanceIDGuidSuffix);
@@ -309,12 +310,12 @@ void PushMessagingAppIdentifier::DCheckValid() const {
   // kInstanceIDGuidSuffix (which contains non-hex characters invalid in GUIDs).
   std::string guid = app_id_.substr(app_id_.size() - kGuidLength);
   if (UseInstanceID(app_id_)) {
-    DCHECK(!base::IsValidGUID(guid));
+    DCHECK(!base::Uuid::ParseCaseInsensitive(guid).is_valid());
 
     // Replace suffix with valid hex so we can validate the rest of the string.
     guid = guid.replace(guid.size() - kGuidSuffixLength, kGuidSuffixLength,
                         kGuidSuffixLength, 'C');
   }
-  DCHECK(base::IsValidGUID(guid));
+  DCHECK(base::Uuid::ParseCaseInsensitive(guid).is_valid());
 #endif  // DCHECK_IS_ON()
 }
