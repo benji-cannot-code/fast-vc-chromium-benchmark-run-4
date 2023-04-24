cfd5e837fe2676c835eff4b100747ab45ad65185
Joseph Koshy <jkoshy@chromium.org>
2023-04-24 14:32:17 +0000

    Include 'build/build_config.h'.
    This change list addresses a review comment in https://crrev.com/c/4447538.
    
    Bug: 1434246, 1428068
    Change-Id: I7e3dc4d2b2c3a18953c1c31a9a29cae8b94c268d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4466047
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Auto-Submit: Joseph Koshy <jkoshy@chromium.org>
    Quick-Run: Joseph Koshy <jkoshy@chromium.org>
    Commit-Queue: Joseph Koshy <jkoshy@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1134576}

--- base/allocator/dispatcher/tls.cc
+++ base/allocator/dispatcher/tls.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/dcheck_is_on.h"
 #include "base/immediate_crash.h"
+#include "build/build_config.h"
 
 #include <sys/mman.h>
 
