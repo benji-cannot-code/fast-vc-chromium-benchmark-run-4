4b9d64614c1b844064cfe4c993712922352552bb
Charles Hager <clhager@google.com>
2023-04-24 18:10:14 +0000

    Added device lock to FRE sign in on automotive
    Bug: 1432388
    Change-Id: If9cd9188bd78174a2d3976c0352db691f61704f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443441
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134723}

--- chrome/browser/ui/android/device_lock/javatests/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockCoordinatorTest.java
+++ chrome/browser/ui/android/device_lock/javatests/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockCoordinatorTest.java
@@ -6,14 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.device_lock;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import android.content.Context;
+import android.view.View;
 
 import androidx.test.filters.SmallTest;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.Batch;
@@ -25,10 +34,17 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @Batch(Batch.UNIT_TESTS)
 public class DeviceLockCoordinatorTest {
+    @Rule
+    public final MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private MockDelegate mMockDelegate;
+
     private Context mContext;
 
     @Before
     public void setUpTest() {
+        mMockDelegate = Mockito.mock(MockDelegate.class);
         mContext = ContextUtils.getApplicationContext();
     }
 
@@ -37,13 +53,17 @@ public class DeviceLockCoordinatorTest {
     @SmallTest
     public void testDeviceLockCoordinator_simpleTest() {
         DeviceLockCoordinator deviceLockCoordinator =
-                new DeviceLockCoordinator(true, new MockDelegate(), null, mContext, null);
+                new DeviceLockCoordinator(true, mMockDelegate, null, mContext, null);
         assertNotEquals(deviceLockCoordinator, null);
+        verify(mMockDelegate, times(1)).setView(any());
 
         deviceLockCoordinator.destroy();
     }
 
     private class MockDelegate implements DeviceLockCoordinator.Delegate {
+        @Override
+        public void setView(View view) {}
+
         @Override
         public void onDeviceLockReady() {}
 
--- chrome/browser/ui/android/device_lock/junit/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockMediatorUnitTest.java
+++ chrome/browser/ui/android/device_lock/junit/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockMediatorUnitTest.java
@@ -30,6 +30,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.ResolveInfo;
+import android.view.View;
 
 import androidx.constraintlayout.utils.widget.MockView;
 
@@ -305,6 +306,9 @@ public class DeviceLockMediatorUnitTest {
     }
 
     private class MockDelegate implements DeviceLockCoordinator.Delegate {
+        @Override
+        public void setView(View view) {}
+
         @Override
         public void onDeviceLockReady() {}
 
