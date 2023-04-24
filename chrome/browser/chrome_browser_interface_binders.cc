f219302b5b4d1824a1dbd1e0c732e4d941ca638f
Daniel Classon <dclasson@google.com>
2023-04-24 23:30:55 +0000

    [Nearby Share] Add jelly support for dynamic colors
    This adds support for jelly in the Nearby Share WebUI.
    
    Before: https://screenshot.googleplex.com/43eSkt5Mp3AyqPC
    After: https://screenshot.googleplex.com/Aginxyv9qY6DdTe
    Dark mode: https://screenshot.googleplex.com/7XyFM7gEB8b4dxV
    
    Change-Id: I9f4bfea37537bba4e47eb05a6ea52d383255e0f0
    Bug: b/278738323
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455611
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134911}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -991,7 +991,7 @@ void PopulateChromeWebUIFrameBinders(
       ash::ShortcutCustomizationAppUI,
       ash::printing::printing_manager::PrintManagementUI,
       ash::InternetConfigDialogUI, ash::InternetDetailDialogUI, ash::SetTimeUI,
-      ash::BluetoothPairingDialogUI,
+      ash::BluetoothPairingDialogUI, nearby_share::NearbyShareDialogUI,
 #endif
       NewTabPageUI, OmniboxPopupUI, BookmarksSidePanelUI, CustomizeChromeUI>(
       map);
