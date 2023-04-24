2eaa738b029df00b6cb7f9bf06271fd1d9f68ce1
Tsuyoshi Horo <horo@chromium.org>
2023-04-24 03:59:24 +0000

    Revert "[Password Manager] Using extension-controlled-indicator"
    This reverts commit 3d01b20755bcdecd71103f04b03620d90aed3eb5.
    
    Bug: 1436858
    Reason for revert: Introduced PasswordManagerUI tests failure.
    
    Original change's description:
    > [Password Manager] Using extension-controlled-indicator
    >
    > This CL adds extension-controlled-indicator from settings limited
    > sharing. It makes pref toggle in the new Password Manager UI look as
    > it used to in the old UI. This CL also adds missing
    > cr-policy-pref-indicator just like settings-toggle-button has.
    >
    > Old UI: https://screenshot.googleplex.com/BgzChAzruUc3EmT
    > New UI: https://screenshot.googleplex.com/9rip3d3eHLLWAXm
    >
    > Bug: 1350947
    > Change-Id: I6a5f6dc569e3cb19c62a64c0fa9b32cfcc5ee4d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454328
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1134002}
    
    Bug: 1350947
    Change-Id: I0d3ed0c9a70337c811092c0f9963d7713cb66898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4464438
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134385}

--- chrome/test/data/webui/password_manager/settings_section_test.ts
+++ chrome/test/data/webui/password_manager/settings_section_test.ts
@@ -114,8 +114,8 @@ suite('SettingsSectionTest', function() {
     await flushTasks();
 
     assertTrue(settings.$.passwordToggle.checked);
-    assertTrue(
-        !!settings.shadowRoot!.querySelector('extension-controlled-indicator'));
+    assertTrue(!!settings.$.passwordToggle.shadowRoot!.querySelector(
+        '#extensionIcon'));
   });
 
   test('no extension control icon by default', async function() {
@@ -127,7 +127,7 @@ suite('SettingsSectionTest', function() {
     assertTrue(settings.$.passwordToggle.checked);
     settings.$.passwordToggle.click();
     assertFalse(!!settings.$.passwordToggle.shadowRoot!.querySelector(
-        'extension-controlled-indicator'));
+        '#extensionIcon'));
   });
 
   test('pref updated externally', async function() {
