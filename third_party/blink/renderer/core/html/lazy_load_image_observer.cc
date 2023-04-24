54b8dbce3fbbdd4f4f7de7303db313bbab08e726
Philip Rogers <pdr@chromium.org>
2023-04-24 20:09:14 +0000

    Disconnect existing observer when recreating lazy load observer
    The DelayOutOfViewportLazyImages experiment (see:
    https://crrev.com/1129319) starts with lazy load intersection observers
    with zero margin and then switches to a non-zero margin. Because
    intersection observer does not have a mechanism for changing margins
    dynamically, a new observer is created. This logic had a bug where the
    zero-margin intersection observer member of LazyLoadImageObserver would
    be cleared, but this would not synchronously disconnect the observer,
    and it could still exist with high cost.
    
    Bug: 1394025
    Change-Id: Ib78233cde78aabc4ca1d2177689cff28e36562b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4468408
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1134796}

--- third_party/blink/renderer/core/html/lazy_load_image_observer.cc
+++ third_party/blink/renderer/core/html/lazy_load_image_observer.cc
@@ -345,9 +345,10 @@ void LazyLoadImageObserver::DocumentOnLoadFinished(Document* root_document) {
 void LazyLoadImageObserver::CreateLazyLoadIntersectionObserver(
     Document* root_document) {
   HeapVector<Member<Element>> existing_targets;
-  if (lazy_load_intersection_observer_) {
+  IntersectionObserver* existing_observer = lazy_load_intersection_observer_;
+  if (existing_observer) {
     for (const IntersectionObservation* observation :
-         lazy_load_intersection_observer_->Observations()) {
+         existing_observer->Observations()) {
       existing_targets.push_back(observation->Target());
     }
   }
@@ -364,6 +365,9 @@ void LazyLoadImageObserver::CreateLazyLoadIntersectionObserver(
       LocalFrameUkmAggregator::kLazyLoadIntersectionObserver);
 
   for (Element* element : existing_targets) {
+    if (existing_observer) {
+      existing_observer->unobserve(element);
+    }
     lazy_load_intersection_observer_->observe(element);
   }
 }
