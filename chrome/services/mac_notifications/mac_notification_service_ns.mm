487e7da6b1918621cb3b96ae406d352de6e5f621
Mike Dougherty <michaeldo@chromium.org>
2023-04-24 18:19:00 +0000

    Remove expired Notifications.macOS.Delivered.{Style} histogram
    Fixed: 1384904
    Change-Id: I63deaf8f200b8b3d3d9316b25387b3d1ebbe342c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437497
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134731}

--- chrome/services/mac_notifications/mac_notification_service_ns.mm
+++ chrome/services/mac_notifications/mac_notification_service_ns.mm
@@ -200,8 +200,6 @@ void MacNotificationServiceNS::DisplayNotification(
       base::SysUTF8ToNSString(DeriveMacNotificationId(notification->meta->id));
   [toast setIdentifier:notification_id];
 
-  LogMacNotificationDelivered(IsAppBundleAlertStyle(), /*success=*/true);
-
   [notification_center_ deliverNotification:toast.get()];
 }
 
