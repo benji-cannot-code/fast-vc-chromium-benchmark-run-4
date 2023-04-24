7c8c4bf2264ae4c2884dddf851c4d3706b7997b7
Hongyu Long <hongyulong@chromium.org>
2023-04-24 19:58:00 +0000

    audio: Call functions when the `stream_` is bounded
    According the stack trace, the `receiver_` in
    AudioOutputStreamProxy::SetVolume may be cleaned when call the
    OutputDevice::SetVolume. So we added one condition only call the
    functions when the `stream_` is bounded.
    
    BUG=chromium:1433191
    
    Change-Id: I4da2f67c4eb5732938529acaf2238490ce28d819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4451226
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134788}

--- services/audio/public/cpp/output_device.cc
+++ services/audio/public/cpp/output_device.cc
@@ -43,17 +43,23 @@ OutputDevice::~OutputDevice() {
 
 void OutputDevice::Play() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  stream_->Play();
+  if (stream_.is_bound()) {
+    stream_->Play();
+  }
 }
 
 void OutputDevice::Pause() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  stream_->Pause();
+  if (stream_.is_bound()) {
+    stream_->Pause();
+  }
 }
 
 void OutputDevice::SetVolume(double volume) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  stream_->SetVolume(volume);
+  if (stream_.is_bound()) {
+    stream_->SetVolume(volume);
+  }
 }
 
 void OutputDevice::StreamCreated(
@@ -82,6 +88,8 @@ void OutputDevice::OnConnectionError() {
   // simpler.
   base::ScopedAllowBaseSyncPrimitivesOutsideBlockingScope allow_thread_join;
   CleanUp();
+
+  render_callback_->OnRenderError();
 }
 
 void OutputDevice::CleanUp() {
