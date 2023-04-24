bce717227b1b85bc1bb762ccec20e1dabd7117c0
Dibyajyoti Pal <dibyapal@google.com>
2023-04-24 15:14:19 +0000

    [dPWA] Add AppHome to field trial configs
    Bug: b/279020860
    Change-Id: Iad85c79d165c5d2fcebfc499fbe1fe98b33b9652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4447939
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1134603}

--- chrome/browser/policy/test/hide_webstore_icon_policy_browsertest.cc
+++ chrome/browser/policy/test/hide_webstore_icon_policy_browsertest.cc
@@ -4,10 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/locale_settings.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -42,7 +45,14 @@ bool ContainsVisibleElement(content::WebContents* contents,
 // before the browser start.
 class PolicyWebStoreIconTest : public PolicyTest {
  public:
-  PolicyWebStoreIconTest() = default;
+  PolicyWebStoreIconTest() {
+#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_ANDROID)
+    // The new chrome://apps (App Home) page does not have the web store app, so
+    // the test only works for the old implementation. In the future, this test
+    // can be entirely removed.
+    feature_list_.InitAndDisableFeature(features::kDesktopPWAsAppHomePage);
+#endif  // !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_ANDROID)
+  }
   ~PolicyWebStoreIconTest() override = default;
 
   void SetUpInProcessBrowserTestFixture() override {
@@ -52,6 +62,9 @@ class PolicyWebStoreIconTest : public PolicyTest {
   void SetUpCommandLine(base::CommandLine* command_line) override {
     PolicyTest::SetUpCommandLine(command_line);
   }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(PolicyWebStoreIconTest, AppsWebStoreIconHidden) {
