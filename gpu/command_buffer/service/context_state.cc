3a9f78c300b50bd4fc3baaad32377bb2fb2b5adc
Ho Cheung <uioptt24@gmail.com>
2023-04-24 09:42:59 +0000

    Convert base::clamp() to std::clamp() in gpu/
    According to the instructions at crbug.com/1373621,
    we should convert base::clamp() to std::clamp() in the code.
    
    This is a continuation of the previous CLs
    https://chromium-review.googlesource.com/c/chromium/src/+/4410943
    https://chromium-review.googlesource.com/c/chromium/src/+/4461000
    https://chromium-review.googlesource.com/c/chromium/src/+/4462333
    https://chromium-review.googlesource.com/c/chromium/src/+/4462797
    https://chromium-review.googlesource.com/c/chromium/src/+/4460969
    https://chromium-review.googlesource.com/c/chromium/src/+/4462837
    https://chromium-review.googlesource.com/c/chromium/src/+/4461080
    
    Bug: 1373621
    Change-Id: Ibf30268600d045ad730dbfd1b75ff49dc95b7416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4462838
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134463}

--- gpu/command_buffer/service/context_state.cc
+++ gpu/command_buffer/service/context_state.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cmath>
 
-#include "base/cxx17_backports.h"
 #include "gpu/command_buffer/common/gles2_cmd_utils.h"
 #include "gpu/command_buffer/service/buffer_manager.h"
 #include "gpu/command_buffer/service/framebuffer_manager.h"
@@ -375,7 +374,7 @@ void ContextState::RestoreUnpackState() const {
 }
 
 void ContextState::DoLineWidth(GLfloat width) const {
-  api()->glLineWidthFn(base::clamp(width, line_width_min_, line_width_max_));
+  api()->glLineWidthFn(std::clamp(width, line_width_min_, line_width_max_));
 }
 
 void ContextState::RestoreBufferBindings() const {
--- gpu/command_buffer/tests/gl_ext_blend_func_extended_unittest.cc
+++ gpu/command_buffer/tests/gl_ext_blend_func_extended_unittest.cc
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <GLES3/gl3.h>
 #include <stdint.h>
 
-#include "base/cxx17_backports.h"
+#include <algorithm>
+
 #include "gpu/command_buffer/tests/gl_manager.h"
 #include "gpu/command_buffer/tests/gl_test_utils.h"
 #include "gpu/config/gpu_test_config.h"
@@ -55,8 +56,8 @@ void BlendEquationFuncAdd(float dst[4],
   r[3] = src[3] * Weight<As, 3>(dst, src, src1) +
          dst[3] * Weight<Ad, 3>(dst, src, src1);
   for (int i = 0; i < 4; ++i) {
-    result[i] = static_cast<uint8_t>(
-        std::floor(base::clamp(r[i], 0.0f, 1.0f) * 255.0f));
+    result[i] =
+        static_cast<uint8_t>(std::floor(std::clamp(r[i], 0.0f, 1.0f) * 255.0f));
   }
 }
 
