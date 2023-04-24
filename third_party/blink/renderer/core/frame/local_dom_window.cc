290c40aeca7e49ea6d29086bef25d91dd09326ca
Nick Burris <nburris@chromium.org>
2023-04-24 19:21:21 +0000

    [SPC] Allow activationless show() for SPC
    This implements behind a flag the functionality to allow one show() call
    per page load without a user activation for a Payment Request with the
    Secure Payment Confirmation method.
    
    Design doc:
    https://docs.google.com/document/d/1DW4hGyuVzcN8sE8TC3YOkg6xO4XpUGZQNxcHgfMXVwA/edit?usp=sharing
    
    Bug: 1420304
    Change-Id: I53b88353785608a52746d2d591f3855da430a806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4436149
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1134769}

--- third_party/blink/renderer/core/frame/local_dom_window.cc
+++ third_party/blink/renderer/core/frame/local_dom_window.cc
@@ -2522,4 +2522,12 @@ void LocalDOMWindow::SetHasStorageAccess() {
   has_storage_access_ = true;
 }
 
+bool LocalDOMWindow::HadActivationlessPaymentRequest() const {
+  return had_activationless_payment_request_;
+}
+
+void LocalDOMWindow::SetHadActivationlessPaymentRequest() {
+  had_activationless_payment_request_ = true;
+}
+
 }  // namespace blink
