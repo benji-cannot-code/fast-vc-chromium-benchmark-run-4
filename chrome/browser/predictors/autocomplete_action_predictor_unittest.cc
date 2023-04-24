efa8fd6cb4e622fc9cbfb86c673acb1865fad49b
Claudio DeSouza <cdesouza@igalia.com>
2023-04-24 12:50:03 +0000

    Replace base::GUID uses with base::Uuid (159/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/predictors
    
    This CL was uploaded by git cl split.
    
    R=lizeb@chromium.org
    
    Bug: 1428566
    Change-Id: Ic1eb7e7423edc277831ff1af87a03406d26d61c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455078
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1134532}

--- chrome/browser/predictors/autocomplete_action_predictor_unittest.cc
+++ chrome/browser/predictors/autocomplete_action_predictor_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
-#include "base/guid.h"
 #include "base/memory/ref_counted.h"
 #include "base/no_destructor.h"
 #include "base/ranges/algorithm.h"
@@ -22,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#include "base/uuid.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/preloading/chrome_preloading.h"
 #include "chrome/common/chrome_switches.h"
@@ -174,7 +174,7 @@ class AutocompleteActionPredictorTest : public testing::Test {
   AutocompleteActionPredictorTable::Row CreateRowFromTestUrlInfo(
       const TestUrlInfo& test_row) const {
     AutocompleteActionPredictorTable::Row row;
-    row.id = base::GenerateGUID();
+    row.id = base::Uuid::GenerateRandomV4().AsLowercaseString();
     row.user_text = test_row.user_text;
     row.url = test_row.url;
     row.number_of_hits = test_row.number_of_hits;
