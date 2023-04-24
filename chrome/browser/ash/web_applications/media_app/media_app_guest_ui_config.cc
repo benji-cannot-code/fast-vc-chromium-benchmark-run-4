0bea2078df0b791d4cc83f7caa22e284eb35a2df
Joseph Kim <josephkimsh@google.com>
2023-04-24 04:42:34 +0000

    Media App: Enable Photos integration flags for dev builds of Photos
    When testing with a development build of the Photos app, the version
    string is not in the form "a.b.c" and therefore cannot be parsed into a
    version string. Consequently, the Photos integration hooks in the Media
    App are also not shown. Enable the hooks when the version string is
    "DEVELOPMENT".
    
    Bug: b:278788321
    Change-Id: I906cbc39503b3a9aa589bc2f7e0f322d8b342b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4464343
    Commit-Queue: Joseph Kim <josephkimsh@google.com>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134392}

--- chrome/browser/ash/web_applications/media_app/media_app_guest_ui_config.cc
+++ chrome/browser/ash/web_applications/media_app/media_app_guest_ui_config.cc
@@ -27,6 +27,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_data_source.h"
 
+namespace {
+
+bool PhotosIntegrationSupported(const apps::AppUpdate& update) {
+  constexpr char kMinPhotosVersion[] = "6.12";
+  if (!apps_util::IsInstalled(update.Readiness())) {
+    return false;
+  }
+
+  if (update.Version() == "DEVELOPMENT") {
+    return true;
+  }
+
+  auto photos_version = base::Version(update.Version());
+  return photos_version.IsValid() &&
+         photos_version >= base::Version(kMinPhotosVersion);
+}
+
+}  // namespace
+
 ChromeMediaAppGuestUIDelegate::ChromeMediaAppGuestUIDelegate() = default;
 
 void ChromeMediaAppGuestUIDelegate::PopulateLoadTimeData(
@@ -37,13 +56,11 @@ void ChromeMediaAppGuestUIDelegate::PopulateLoadTimeData(
   apps::AppRegistryCache& app_registry_cache =
       apps::AppServiceProxyFactory::GetForProfile(profile)->AppRegistryCache();
 
-  bool photos_installed = false;
-  auto photos_version = base::Version();
+  bool photos_integration_supported = false;
   app_registry_cache.ForOneApp(
       arc::kGooglePhotosAppId,
-      [&photos_installed, &photos_version](const apps::AppUpdate& update) {
-        photos_installed = apps_util::IsInstalled(update.Readiness());
-        photos_version = base::Version(update.Version());
+      [&photos_integration_supported](const apps::AppUpdate& update) {
+        photos_integration_supported = PhotosIntegrationSupported(update);
       });
 
   source->AddString("appLocale", g_browser_process->GetApplicationLocale());
@@ -52,11 +69,8 @@ void ChromeMediaAppGuestUIDelegate::PopulateLoadTimeData(
   version_info::Channel channel = chrome::GetChannel();
   source->AddBoolean("colorThemes", true);
   source->AddBoolean("jelly", chromeos::features::IsJellyEnabled());
-  base::Version min_photos_version("6.12");
-  bool photos_available = photos_installed && photos_version.IsValid() &&
-                          photos_version >= min_photos_version;
-  source->AddBoolean("photosAvailableForImage", photos_available);
-  source->AddBoolean("photosAvailableForVideo", photos_available);
+  source->AddBoolean("photosAvailableForImage", photos_integration_supported);
+  source->AddBoolean("photosAvailableForVideo", photos_integration_supported);
   source->AddBoolean("pdfSignature", base::FeatureList::IsEnabled(
                                          ash::features::kMediaAppPdfSignature));
   source->AddBoolean("flagsMenu", channel != version_info::Channel::BETA &&
