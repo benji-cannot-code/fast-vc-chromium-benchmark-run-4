c782f3c238e4cfe0a40ccbc2d0cba39526026d56
Michael Tang <tangm@microsoft.com>
2023-04-25 23:07:27 +0000

    ui: test: Allow subclasses of SkiaGoldPixelDiff to specify optional keys
    Subclasses of SkiaGoldPixelDiff can now specify their system environment
    by overriding FillInSystemEnvironment.
    
    This CL is purely refactoring and should have no behavioral changes.
    
    Change-Id: I0d36d8d2993d1abecefc3e75f93cfc662e4179f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4471914
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1135615}

--- ui/base/test/skia_gold_pixel_diff.h
+++ ui/base/test/skia_gold_pixel_diff.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 
 namespace base {
@@ -18,6 +19,22 @@ class SkBitmap;
 
 namespace ui {
 namespace test {
+
+// Keys used to identify test environment information that can possibly affect
+// pixel output. If your test depends on some new environment information, add
+// it here and update |TestEnvironmentKeyToString|.
+enum class TestEnvironmentKey {
+  // Being default-provided keys. These should not by used by subtypes.
+
+  // The operating system name.
+  kSystem,
+  // The processor architecture.
+  kProcessor,
+
+  // Begin subtype-provided keys.
+};
+
+using TestEnvironmentMap = base::flat_map<TestEnvironmentKey, std::string>;
 
 class SkiaGoldMatchingAlgorithm;
 // This is the utility class for Skia Gold pixeltest.
@@ -47,8 +64,13 @@ class SkiaGoldPixelDiff {
   // corpus The corpus (i.e. result group) that will be used to store the
   //   result in Gold. If omitted, will default to the generic corpus for
   //   results from gtest-based tests.
+  // test_environment A map containing any test-specific environment information
+  //   to determine whether a new screenshot is good or not. All the information
+  //   that can affect the output of pixels should be filled in. Eg: operating
+  //   system, graphics card, processor architecture, screen resolution, etc.
   void Init(const std::string& screenshot_prefix,
-            const std::string& corpus = std::string());
+            const std::string& corpus = std::string(),
+            TestEnvironmentMap test_environment = TestEnvironmentMap());
 
   bool CompareScreenshot(
       const std::string& screenshot_name,
@@ -67,7 +89,7 @@ class SkiaGoldPixelDiff {
   bool Initialized() const { return initialized_; }
 
  private:
-  void InitSkiaGold();
+  void InitSkiaGold(TestEnvironmentMap test_environment);
   // Prefix for every golden images.
   std::string prefix_;
   bool initialized_ = false;
