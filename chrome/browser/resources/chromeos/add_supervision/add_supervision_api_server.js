945472197315fb96b84c533a35d1b707f8f982fe
Amber Haynes <amberhaynes@chromium.org>
2023-04-25 17:48:43 +0000

    Add Supervision: Utilize Shared Error and Offline Screens
    + This change unifies the error/offline screens of the Gellerization
    flow with other Supervised User features (Parent Access, EDU
    Coexistence).
    + Error logic has been added to the Gellerization UI to trigger the new
    error screen in common error cases.
    + The pre-existing offline screen has been removed.
    + The browser test has been modified to use an embedded test server and
    a local setup URL to avoid attempting to access URLs in the google.com
    domain.
    + AddSupervisionAPIServer was modified to avoid initialization during
    testing.
    + Test cases for the new screens have been added.
    
    Original Offline Screen:
    https://screenshot.googleplex.com/7zsBFXkW48zoiGd.png
    New Offline Screen:
    https://screenshot.googleplex.com/9LNYiL9HCcTNAt6.png
    New Error Screen:
    https://screenshot.googleplex.com/7gCVQfBkHoL3Vzo.png
    
    Bug: b:265995792
    Change-Id: Ibebb5e8221de730cdb46281368b54da024bb5341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4385282
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Amber Haynes <amberhaynes@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135362}

--- chrome/browser/resources/chromeos/add_supervision/add_supervision_api_server.js
+++ chrome/browser/resources/chromeos/add_supervision/add_supervision_api_server.js
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {PostMessageAPIServer} from 'chrome://resources/ash/common/post_message_api/post_message_api_server.js';
 
+import {isLocalHostForTesting} from './add_supervision_ui.js';
+
 /**
  * Class that implements the server side of the AddSupervision postMessage
  * API.  In the case of this API, the Add Supervision WebUI is the server, and
@@ -14,6 +16,7 @@ import {PostMessageAPIServer} from 'chrome://resources/ash/common/post_message_a
 export class AddSupervisionAPIServer extends PostMessageAPIServer {
   /*
    * @constructor
+   * @param {!Element} ui  Polymer object add-supervision-ui
    * @param {!Element} webviewElement  The <webview> element to listen to as a
    *     client.
    * @param {string} targetURL  The target URL to use for outgoing messages.
@@ -21,8 +24,10 @@ export class AddSupervisionAPIServer extends PostMessageAPIServer {
    * @param {string} originURLPrefix  The URL prefix to use to filter incoming
    *     messages via the postMessage API.
    */
-  constructor(webviewElement, targetURL, originURLPrefix) {
+  constructor(ui, webviewElement, targetURL, originURLPrefix) {
     super(webviewElement, targetURL, originURLPrefix);
+
+    this.ui_ = ui;
 
     this.addSupervisionHandler_ =
         addSupervision.mojom.AddSupervisionHandler.getRemote();
@@ -37,8 +42,18 @@ export class AddSupervisionAPIServer extends PostMessageAPIServer {
   }
 
   /** @override */
+  initialize() {
+    // The server cannot communicate with the mock webview used
+    // in the browser test, so skip initialization during tests.
+    if (isLocalHostForTesting(this.targetURL())) {
+      return;
+    }
+    super.initialize();
+  }
+
+  /** @override */
   onInitializationError(origin) {
-    // TODO(): Trigger an error page to be shown in this case.
+    this.ui_.showErrorPage();
   }
 
   /**
