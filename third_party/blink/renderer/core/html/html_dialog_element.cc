3916a3d21badffcb36eb85d6986de7ff495b32d9
Joey Arhar <jarhar@chromium.org>
2023-04-25 05:56:44 +0000

    Use dialog focus algorithm for dialog popovers
    This patch makes it so that when a <dialog popover> gets opened as a
    popover, it uses the dialog initial focus algorithm instead of the
    popover initial focus algorithm.
    
    The popover initial focus algorithm only focuses descendants with the
    autofocus attribute, whereas the dialog initial focus algorithm focuses
    anything that's focusable.
    
    HTML spec PR: https://github.com/whatwg/html/pull/9121
    
    Fixed: 1430405
    Change-Id: I627d8ab9045ecdad8d8265fab95b68e571227685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400070
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135058}

--- third_party/blink/renderer/core/html/html_dialog_element.cc
+++ third_party/blink/renderer/core/html/html_dialog_element.cc
@@ -56,16 +56,7 @@ void HTMLDialogElement::SetFocusForDialogLegacy(HTMLDialogElement* dialog) {
   if (!dialog->isConnected())
     return;
 
-  // Showing a <dialog> should hide all open popovers.
   auto& document = dialog->GetDocument();
-  if (RuntimeEnabledFeatures::HTMLPopoverAttributeEnabled(
-          document.GetExecutionContext())) {
-    HTMLElement::HideAllPopoversUntil(
-        nullptr, document, HidePopoverFocusBehavior::kNone,
-        HidePopoverTransitionBehavior::kFireEventsAndWaitForTransitions,
-        HidePopoverIndependence::kHideUnrelated);
-  }
-
   dialog->previously_focused_element_ = document.FocusedElement();
 
   // TODO(kochi): How to find focusable element inside Shadow DOM is not
@@ -216,8 +207,18 @@ void HTMLDialogElement::show(ExceptionState& exception_state) {
   // Element::isFocusable, which requires an up-to-date layout.
   GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kJavaScript);
 
+  // Showing a <dialog> should hide all open popovers.
+  auto& document = GetDocument();
+  if (RuntimeEnabledFeatures::HTMLPopoverAttributeEnabled(
+          document.GetExecutionContext())) {
+    HTMLElement::HideAllPopoversUntil(
+        nullptr, document, HidePopoverFocusBehavior::kNone,
+        HidePopoverTransitionBehavior::kFireEventsAndWaitForTransitions,
+        HidePopoverIndependence::kHideUnrelated);
+  }
+
   if (RuntimeEnabledFeatures::DialogNewFocusBehaviorEnabled()) {
-    SetFocusForDialog();
+    SetFocusForDialog(is_modal_);
   } else {
     SetFocusForDialogLegacy(this);
   }
@@ -298,8 +299,17 @@ void HTMLDialogElement::showModal(ExceptionState& exception_state) {
     }
   }
 
+  // Showing a <dialog> should hide all open popovers.
+  if (RuntimeEnabledFeatures::HTMLPopoverAttributeEnabled(
+          document.GetExecutionContext())) {
+    HTMLElement::HideAllPopoversUntil(
+        nullptr, document, HidePopoverFocusBehavior::kNone,
+        HidePopoverTransitionBehavior::kFireEventsAndWaitForTransitions,
+        HidePopoverIndependence::kHideUnrelated);
+  }
+
   if (RuntimeEnabledFeatures::DialogNewFocusBehaviorEnabled()) {
-    SetFocusForDialog();
+    SetFocusForDialog(is_modal_);
   } else {
     SetFocusForDialogLegacy(this);
   }
@@ -349,20 +359,11 @@ void HTMLDialogElement::CloseWatcherFiredClose() {
 }
 
 // https://html.spec.whatwg.org#dialog-focusing-steps
-void HTMLDialogElement::SetFocusForDialog() {
+void HTMLDialogElement::SetFocusForDialog(bool is_modal) {
   previously_focused_element_ = GetDocument().FocusedElement();
 
-  // Showing a <dialog> should hide all open popovers.
-  if (RuntimeEnabledFeatures::HTMLPopoverAttributeEnabled(
-          GetDocument().GetExecutionContext())) {
-    HTMLElement::HideAllPopoversUntil(
-        nullptr, GetDocument(), HidePopoverFocusBehavior::kNone,
-        HidePopoverTransitionBehavior::kFireEventsAndWaitForTransitions,
-        HidePopoverIndependence::kHideUnrelated);
-  }
-
   Element* control = GetFocusDelegate(/*autofocus_only=*/false);
-  if (is_modal_ && IsAutofocusable()) {
+  if (is_modal && IsAutofocusable()) {
     control = this;
   }
   if (!control)
@@ -370,8 +371,9 @@ void HTMLDialogElement::SetFocusForDialog() {
 
   if (control->IsFocusable())
     control->Focus();
-  else if (is_modal_)
+  else if (is_modal) {
     control->GetDocument().ClearFocusedElement();
+  }
 
   // 4. Let topDocument be the active document of control's node document's
   // browsing context's top-level browsing context.
