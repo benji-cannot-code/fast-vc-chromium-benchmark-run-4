51673497765214aff7be7bfa79e9b37799208aee
Bryant Chandler <bryantchandler@chromium.org>
2023-04-25 15:29:47 +0000

    [media] Add protection for readonly video frame data
    1) Change layer storage to be const
    2) Change all Wrap* methods to take const data pointers
    3) Protect writable* methods with a CHECK for unowned memory
    4) Remove const_casts from code that uses media::VideoFrame
       and only const_cast after the CHECK in the writable* functions.
       This CL only removes one const_cast, further casts will be
       removed in subsequent CLs.
    
    Bug: 1161304, 843117, 1316810
    Change-Id: If3b4b0c2f9709a582c15d037462cad861a2ad7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4449186
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135200}

--- media/base/video_frame.h
+++ media/base/video_frame.h
@@ -236,7 +236,7 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       const gfx::Size& coded_size,
       const gfx::Rect& visible_rect,
       const gfx::Size& natural_size,
-      uint8_t* data,
+      const uint8_t* data,
       size_t data_size,
       base::TimeDelta timestamp);
 
@@ -244,7 +244,7 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       const VideoFrameLayout& layout,
       const gfx::Rect& visible_rect,
       const gfx::Size& natural_size,
-      uint8_t* data,
+      const uint8_t* data,
       size_t data_size,
       base::TimeDelta timestamp);
 
@@ -258,9 +258,9 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       int32_t y_stride,
       int32_t u_stride,
       int32_t v_stride,
-      uint8_t* y_data,
-      uint8_t* u_data,
-      uint8_t* v_data,
+      const uint8_t* y_data,
+      const uint8_t* u_data,
+      const uint8_t* v_data,
       base::TimeDelta timestamp);
 
   // Wraps external YUV data with VideoFrameLayout. The returned VideoFrame does
@@ -269,9 +269,9 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       const VideoFrameLayout& layout,
       const gfx::Rect& visible_rect,
       const gfx::Size& natural_size,
-      uint8_t* y_data,
-      uint8_t* u_data,
-      uint8_t* v_data,
+      const uint8_t* y_data,
+      const uint8_t* u_data,
+      const uint8_t* v_data,
       base::TimeDelta timestamp);
 
   // Wraps external YUVA data of the given parameters with a VideoFrame.
@@ -285,10 +285,10 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       int32_t u_stride,
       int32_t v_stride,
       int32_t a_stride,
-      uint8_t* y_data,
-      uint8_t* u_data,
-      uint8_t* v_data,
-      uint8_t* a_data,
+      const uint8_t* y_data,
+      const uint8_t* u_data,
+      const uint8_t* v_data,
+      const uint8_t* a_data,
       base::TimeDelta timestamp);
 
   // Wraps external NV12 data of the given parameters with a VideoFrame.
@@ -300,8 +300,8 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       const gfx::Size& natural_size,
       int32_t y_stride,
       int32_t uv_stride,
-      uint8_t* y_data,
-      uint8_t* uv_data,
+      const uint8_t* y_data,
+      const uint8_t* uv_data,
       base::TimeDelta timestamp);
 
   // Wraps |gpu_memory_buffer| along with the mailboxes created from
@@ -571,22 +571,17 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   // IsMappable() frame type. The memory is owned by VideoFrame object and must
   // not be freed by the caller.
   const uint8_t* data(size_t plane) const {
-    DCHECK(IsValidPlane(format(), plane));
-    DCHECK(IsMappable());
-    if (UNLIKELY(plane >= kMaxPlanes)) {
-      NOTREACHED();
-      return nullptr;
-    }
+    CHECK(IsValidPlane(format(), plane));
+    CHECK(IsMappable());
     return data_[plane];
   }
   uint8_t* writable_data(size_t plane) {
-    DCHECK(IsValidPlane(format(), plane));
-    DCHECK(IsMappable());
-    if (UNLIKELY(plane >= kMaxPlanes)) {
-      NOTREACHED();
-      return nullptr;
-    }
-    return data_[plane];
+    // TODO(crbug.com/1435549): Also CHECK that the storage type isn't
+    // STORAGE_UNOWNED_MEMORY once non-compliant usages are fixed.
+    CHECK_NE(storage_type_, STORAGE_SHMEM);
+    CHECK(IsValidPlane(format(), plane));
+    CHECK(IsMappable());
+    return const_cast<uint8_t*>(data_[plane]);
   }
 
   const absl::optional<gpu::VulkanYCbCrInfo>& ycbcr_info() const {
@@ -729,7 +724,7 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
                                     const gfx::Rect& visible_rect,
                                     const gfx::Size& natural_size);
 
-  void set_data(size_t plane, uint8_t* ptr) {
+  void set_data(size_t plane, const uint8_t* ptr) {
     DCHECK(IsValidPlane(format(), plane));
     DCHECK(ptr);
     data_[plane] = ptr;
@@ -805,7 +800,7 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   // TODO(mcasas): we don't know on ctor if we own |data_| or not. Change
   // to std::unique_ptr<uint8_t, AlignedFreeDeleter> after refactoring
   // VideoFrame.
-  uint8_t* data_[kMaxPlanes];
+  const uint8_t* data_[kMaxPlanes];
 
   // Native texture mailboxes, if this is a IsTexture() frame.
   gpu::MailboxHolder mailbox_holders_[kMaxPlanes];
