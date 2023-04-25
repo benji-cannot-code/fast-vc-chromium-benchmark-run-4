0493eb02ea40267f40305665f8fa985f569271d4
Peter Kasting <pkasting@chromium.org>
2023-04-25 02:39:31 +0000

    Misc. cleanup. No functional change.
    * Shorten code.
    * Use `expected` monadic ops.
    * `+` is shorter than `base::StrCat` for simple cases.
    * Explicit `base::ok` not needed to return an `expected` of non-void.
    * Add a few missing #includes.
    * Use local lambdas to avoid code duplication on exit paths.
    * Remove dead code.
    
    Bug: none
    Low-Coverage-Reason: cleanup
    Change-Id: I1f07a7bef062955f2a603d24e2f9124d74fe9d70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4464199
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134997}

--- chrome/browser/ui/webui/net_internals/net_internals_ui.cc
+++ chrome/browser/ui/webui/net_internals/net_internals_ui.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/types/expected.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/webui_util.h"
@@ -118,8 +117,6 @@ base::Value::List HostResolverEndpointResultsToBaseList(
   }
   return endpoint_results_list;
 }
-
-using ResolveHostResult = base::expected<base::Value, std::string>;
 
 // This class implements network::mojom::ResolveHostClient.
 class NetInternalsResolveHostClient : public network::mojom::ResolveHostClient {
