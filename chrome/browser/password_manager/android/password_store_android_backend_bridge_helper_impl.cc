9187bfb24d0daa10cb4bba0819f5bf4fef9870f4
Ioana Pandele <ioanap@chromium.org>
2023-04-25 16:19:02 +0000

    [UPMCleanup] Remove addressed todos related to the GMS version
    The check is now using the cached version in BuildInfo, so there is
    no need to run it on the background thread.
    
    Bug: 1394715
    Change-Id: I805948c1650f6ef4c2de1bc5f7347e50b34e1aa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475170
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135240}

--- chrome/browser/password_manager/android/password_store_android_backend_bridge_helper_impl.cc
+++ chrome/browser/password_manager/android/password_store_android_backend_bridge_helper_impl.cc
@@ -31,8 +31,6 @@ PasswordStoreAndroidBackendBridgeHelper::Create() {
 }
 
 bool PasswordStoreAndroidBackendBridgeHelper::CanCreateBackend() {
-  // TODO(crbug.com/1394715): Either move this call to the background thread or
-  // use cached GMS Core version from `BuildInfo.gmsVersionCode`.
   return PasswordStoreAndroidBackendDispatcherBridge::CanCreateBackend();
 }
 
