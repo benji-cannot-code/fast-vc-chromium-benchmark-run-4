e5d0192e263c8ab9f3711336511bdd190dcc4ce3
Josiah Krutz <josiahk@google.com>
2023-04-25 17:52:17 +0000

    [ChromeVox] Enable ChromeVoxEditingTest.GiantTextAreaPerformance on Ash
    (Still disabled for browser_test_require_lacros)
    https://source.chromium.org/chromium/chromium/src/+/f993270764fc373cf960332531737f8cfea7b50d:testing/buildbot/filters/linux-chromeos.browser_tests.require_lacros.filter;l=69;bpv=1;bpt=0
    
    Bug: 1430387
    Change-Id: If534ab3d4b248970b0de890db9048e37511bdcb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4461971
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135367}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editing_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editing_test.js
@@ -2362,9 +2362,8 @@ AX_TEST_F(
     });
 
 // Regression test that large text areas produce output.
-// Flaky test: https://crbug.com/1430387
 AX_TEST_F(
-    'ChromeVoxEditingTest', 'DISABLED_GiantTextAreaPerformance',
+    'ChromeVoxEditingTest', 'GiantTextAreaPerformance',
     async function() {
       const mockFeedback = this.createMockFeedback();
       const site = `
