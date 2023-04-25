2f3c9c43b8cd9bb4e5dcd43d5c5960c9b091f43c
Takumi Fujimoto <takumif@chromium.org>
2023-04-25 23:49:11 +0000

    Make MediaDialogUiForTest observe MediaItemManager
    Previously MediaDialogUiForTest was listening only for the dialog getting opened via the toolbar button. This makes it listen for all the entry points.
    
    Bug: 1432004
    Change-Id: I1bd2a0dccbec2e009dfd8f5c17957c26f4e1bf81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4473329
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135627}

--- chrome/browser/ui/views/global_media_controls/media_dialog_ui_for_test.h
+++ chrome/browser/ui/views/global_media_controls/media_dialog_ui_for_test.h
@@ -2,11 +2,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2021 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #ifndef CHROME_BROWSER_UI_VIEWS_GLOBAL_MEDIA_CONTROLS_MEDIA_DIALOG_UI_FOR_TEST_H_
 #define CHROME_BROWSER_UI_VIEWS_GLOBAL_MEDIA_CONTROLS_MEDIA_DIALOG_UI_FOR_TEST_H_
 
 #include <string>
 #include "base/functional/callback.h"
+
+namespace global_media_controls {
+class MediaItemManager;
+}  // namespace global_media_controls
 
 class Browser;
 class MediaToolbarButtonView;
@@ -58,6 +63,8 @@ class MediaDialogUiForTest {
   void WaitForPictureInPictureButtonVisibility(bool visible);
 
  private:
+  global_media_controls::MediaItemManager* GetItemManager() const;
+
   base::RepeatingCallback<Browser*()> browser_callback_;
 };
 
