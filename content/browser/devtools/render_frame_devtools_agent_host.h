210af89d337a664ecc85b90f914567ca6cd79284
Andrey Kosyakov <caseq@chromium.org>
2023-04-25 20:39:47 +0000

    Fix ...PolicyDisallowedForForceInstalledExtensionsAfterNavigation
    with --enable-feature=DevToolsTabTarget.
    
    This fixes test by implementing tab target session access control checks
    similar to those on the frame target. This may be redundant, but helps
    to preserve existent front-end behavior and shouldn't hurt.
    
    Bug: 1348385
    Change-Id: I897c3530b9534e955a5a6fdf6735707081794919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4476364
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135513}

--- content/browser/devtools/render_frame_devtools_agent_host.h
+++ content/browser/devtools/render_frame_devtools_agent_host.h
@@ -71,6 +71,8 @@ class CONTENT_EXPORT RenderFrameDevToolsAgentHost
       delete;
 
   static void AttachToWebContents(WebContents* web_contents);
+  static bool ShouldAllowSession(RenderFrameHost* frame_host,
+                                 DevToolsSession* session);
 
   FrameTreeNode* frame_tree_node() { return frame_tree_node_; }
 
@@ -151,8 +153,6 @@ class CONTENT_EXPORT RenderFrameDevToolsAgentHost
   void UpdateFrameHost(RenderFrameHostImpl* frame_host);
   void ChangeFrameHostAndObservedProcess(RenderFrameHostImpl* frame_host);
   void UpdateFrameAlive();
-
-  bool ShouldAllowSession(DevToolsSession* session);
 
 #if BUILDFLAG(IS_ANDROID)
   device::mojom::WakeLock* GetWakeLock();
