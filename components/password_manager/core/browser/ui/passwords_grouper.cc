255d4f73044dd816e495137ea3b4d1ab37d95cd6
Viktor Semeniuk <vsemeniuk@google.com>
2023-04-25 18:02:41 +0000

    [Password Manager] Show favicon from gstatic as a fallback
    This change updates the fallback logic to include icon from gstatic.
    This is similar to what happens inside BrandingInfoFallbackHelper.java
    which is used by passwords.google.com
    
    It should fix absence of favicons in Password Manager UI.
    
    Bug: 1355956
    Change-Id: I75e447574e072c15971aaa016418fa4a64996488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475166
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135382}

--- components/password_manager/core/browser/ui/passwords_grouper.cc
+++ components/password_manager/core/browser/ui/passwords_grouper.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/containers/cxx20_erase.h"
 #include "base/containers/flat_set.h"
+#include "base/strings/escape.h"
 #include "base/strings/string_util.h"
 #include "components/password_manager/core/browser/affiliation/affiliation_service.h"
 #include "components/password_manager/core/browser/affiliation/affiliation_utils.h"
@@ -21,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace password_manager {
 
 namespace {
+
+constexpr char kDefaultFallbackIconUrl[] = "https://t1.gstatic.com/faviconV2";
+constexpr char kFallbackIconQueryParams[] =
+    "client=PASSWORD_MANAGER&type=FAVICON&size=32&url=";
 
 // Converts signon_realm (url for federated forms) into GURL and strips path. If
 // form is valid Android credential or conversion fails signon_realm is returned
@@ -201,7 +206,14 @@ FacetBrandingInfo CreateBrandingInfoFromFacetURI(
             : facet_uri.potentially_invalid_spec();
   }
   branding_info.name = group_name;
-  // TODO(crbug.com/1355956): Handle default icon URL.
+
+  GURL::Replacements replacements;
+  std::string query = kFallbackIconQueryParams +
+                      base::EscapeQueryParamValue(credential.GetURL().spec(),
+                                                  /*use_plus=*/false);
+  replacements.SetQueryStr(query);
+  branding_info.icon_url =
+      GURL(kDefaultFallbackIconUrl).ReplaceComponents(replacements);
   return branding_info;
 }
 
--- components/password_manager/core/browser/ui/passwords_grouper_unittest.cc
+++ components/password_manager/core/browser/ui/passwords_grouper_unittest.cc
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback_helpers.h"
+#include "base/strings/escape.h"
 #include "base/test/gmock_callback_support.h"
 #include "components/password_manager/core/browser/affiliation/affiliation_utils.h"
 #include "components/password_manager/core/browser/affiliation/mock_affiliation_service.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_list_sorter.h"
 #include "components/password_manager/core/browser/password_ui_utils.h"
+#include "components/password_manager/core/browser/ui/credential_ui_entry.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
@@ -42,6 +44,20 @@ GroupedFacets GetSingleGroupForForm(PasswordForm form) {
   group.facets = {
       Facet(FacetURI::FromPotentiallyInvalidSpec(form.signon_realm))};
   return group;
+}
+
+GURL GetIconUrl(const std::string& site) {
+  GURL::Replacements replacements;
+  std::string query = "client=PASSWORD_MANAGER&type=FAVICON&size=32&url=" +
+                      base::EscapeQueryParamValue(site,
+                                                  /*use_plus=*/false);
+  replacements.SetQueryStr(query);
+  return GURL("https://t1.gstatic.com/faviconV2")
+      .ReplaceComponents(replacements);
+}
+
+FacetBrandingInfo GetDefaultBrandingInfo(const CredentialUIEntry& credential) {
+  return {GetShownOrigin(credential), GetIconUrl(credential.GetURL().spec())};
 }
 
 }  // namespace
@@ -88,8 +104,8 @@ TEST_F(PasswordsGrouperTest, GetAffiliatedGroupsWithGroupingInfo) {
   EXPECT_THAT(
       grouper().GetAffiliatedGroupsWithGroupingInfo(),
       UnorderedElementsAre(
-          AffiliatedGroup({credential1}, {GetShownOrigin(credential1)}),
-          AffiliatedGroup({credential2}, {GetShownOrigin(credential2)})));
+          AffiliatedGroup({credential1}, GetDefaultBrandingInfo(credential1)),
+          AffiliatedGroup({credential2}, GetDefaultBrandingInfo(credential2))));
   EXPECT_THAT(grouper().GetPasswordFormsFor(credential1), ElementsAre(form));
   EXPECT_THAT(grouper().GetPasswordFormsFor(credential2),
               ElementsAre(federated_form));
@@ -136,8 +152,8 @@ TEST_F(PasswordsGrouperTest, GroupPasswords) {
       grouper().GetAffiliatedGroupsWithGroupingInfo(),
       UnorderedElementsAre(
           AffiliatedGroup({credential1, credential2},
-                          {GetShownOrigin(credential1)}),
-          AffiliatedGroup({credential3}, {GetShownOrigin(credential3)})));
+                          GetDefaultBrandingInfo(credential1)),
+          AffiliatedGroup({credential3}, GetDefaultBrandingInfo(credential3))));
 
   EXPECT_THAT(grouper().GetBlockedSites(),
               ElementsAre(CredentialUIEntry(blocked_form)));
@@ -176,8 +192,8 @@ TEST_F(PasswordsGrouperTest, GroupPasswordsWithoutAffiliation) {
       grouper().GetAffiliatedGroupsWithGroupingInfo(),
       UnorderedElementsAre(
           AffiliatedGroup({credential1, credential2},
-                          {GetShownOrigin(credential1)}),
-          AffiliatedGroup({credential3}, {GetShownOrigin(credential3)})));
+                          GetDefaultBrandingInfo(credential1)),
+          AffiliatedGroup({credential3}, GetDefaultBrandingInfo(credential3))));
 
   EXPECT_THAT(grouper().GetBlockedSites(),
               ElementsAre(CredentialUIEntry(blocked_form)));
@@ -196,9 +212,9 @@ TEST_F(PasswordsGrouperTest, HttpCredentialsSupported) {
   grouper().GroupPasswords({form}, base::DoNothing());
 
   CredentialUIEntry credential(form);
-  EXPECT_THAT(
-      grouper().GetAffiliatedGroupsWithGroupingInfo(),
-      ElementsAre(AffiliatedGroup({credential}, {GetShownOrigin(credential)})));
+  EXPECT_THAT(grouper().GetAffiliatedGroupsWithGroupingInfo(),
+              ElementsAre(AffiliatedGroup({credential},
+                                          GetDefaultBrandingInfo(credential))));
   EXPECT_THAT(grouper().GetPasswordFormsFor(credential), ElementsAre(form));
 }
 
@@ -221,7 +237,7 @@ TEST_F(PasswordsGrouperTest, FederatedCredentialsGroupedWithRegular) {
   EXPECT_THAT(grouper().GetAffiliatedGroupsWithGroupingInfo(),
               ElementsAre(AffiliatedGroup(
                   {credential, CredentialUIEntry(federated_form)},
-                  {GetShownOrigin(credential)})));
+                  GetDefaultBrandingInfo(credential))));
 }
 
 TEST_F(PasswordsGrouperTest, GroupsWithMatchingMainDomainsMerged) {
@@ -259,8 +275,8 @@ TEST_F(PasswordsGrouperTest, GroupsWithMatchingMainDomainsMerged) {
       grouper().GetAffiliatedGroupsWithGroupingInfo(),
       UnorderedElementsAre(
           AffiliatedGroup({credential1, credential2, credential3},
-                          {GetShownOrigin(credential1)}),
-          AffiliatedGroup({credential4}, {GetShownOrigin(credential4)})));
+                          GetDefaultBrandingInfo(credential1)),
+          AffiliatedGroup({credential4}, GetDefaultBrandingInfo(credential4))));
 }
 
 TEST_F(PasswordsGrouperTest, MainDomainComputationUsesPSLExtensions) {
@@ -293,11 +309,15 @@ TEST_F(PasswordsGrouperTest, MainDomainComputationUsesPSLExtensions) {
   // a.com is considered eTLD+1 but since a.com is present in PSL Extension List
   // main domains for |forms| would be m.a.com, b.a.com, b.a.com and a.com, thus
   // only forms for b.a.com are grouped.
-  EXPECT_THAT(grouper.GetAffiliatedGroupsWithGroupingInfo(),
-              UnorderedElementsAre(
-                  AffiliatedGroup({credential1}, {"m.a.com"}),
-                  AffiliatedGroup({credential2, credential3}, {"b.a.com"}),
-                  AffiliatedGroup({credential4}, {"a.com"})));
+  EXPECT_THAT(
+      grouper.GetAffiliatedGroupsWithGroupingInfo(),
+      UnorderedElementsAre(
+          AffiliatedGroup({credential1},
+                          {"m.a.com", GetIconUrl(credential1.GetURL().spec())}),
+          AffiliatedGroup({credential2, credential3},
+                          {"b.a.com", GetIconUrl(credential2.GetURL().spec())}),
+          AffiliatedGroup({credential4},
+                          {"a.com", GetIconUrl(credential4.GetURL().spec())})));
 }
 
 TEST_F(PasswordsGrouperTest, HttpAndHttpsGroupedTogether) {
@@ -314,9 +334,9 @@ TEST_F(PasswordsGrouperTest, HttpAndHttpsGroupedTogether) {
   grouper().GroupPasswords({form1, form2}, base::DoNothing());
 
   CredentialUIEntry credential({form1, form2});
-  EXPECT_THAT(
-      grouper().GetAffiliatedGroupsWithGroupingInfo(),
-      ElementsAre(AffiliatedGroup({credential}, {GetShownOrigin(credential)})));
+  EXPECT_THAT(grouper().GetAffiliatedGroupsWithGroupingInfo(),
+              ElementsAre(AffiliatedGroup({credential},
+                                          GetDefaultBrandingInfo(credential))));
   EXPECT_THAT(grouper().GetPasswordFormsFor(credential),
               UnorderedElementsAre(form1, form2));
 }
@@ -379,9 +399,10 @@ TEST_F(PasswordsGrouperTest, EncodedCharactersInSignonRealm) {
   grouper().GroupPasswords({form, federated_form}, base::DoNothing());
 
   CredentialUIEntry credential1(form), credential2(federated_form);
-  EXPECT_THAT(grouper().GetAffiliatedGroupsWithGroupingInfo(),
-              UnorderedElementsAre(AffiliatedGroup(
-                  {credential1, credential2}, {GetShownOrigin(credential1)})));
+  EXPECT_THAT(
+      grouper().GetAffiliatedGroupsWithGroupingInfo(),
+      UnorderedElementsAre(AffiliatedGroup(
+          {credential1, credential2}, GetDefaultBrandingInfo(credential1))));
 }
 
 TEST_F(PasswordsGrouperTest, OrderIsCaseInsensitive) {
@@ -433,10 +454,10 @@ TEST_F(PasswordsGrouperTest, IpAddressesGroupedTogether) {
 
   CredentialUIEntry credential1({form1, form2}), credential2(form3),
       credential3(form4);
-  EXPECT_THAT(
-      grouper().GetAffiliatedGroupsWithGroupingInfo(),
-      UnorderedElementsAre(AffiliatedGroup(
-          {credential1, credential2, credential3}, {"https://192.168.1.1"})));
+  EXPECT_THAT(grouper().GetAffiliatedGroupsWithGroupingInfo(),
+              UnorderedElementsAre(AffiliatedGroup(
+                  {credential1, credential2, credential3},
+                  {"https://192.168.1.1", GetIconUrl(form1.signon_realm)})));
 }
 
 TEST_F(PasswordsGrouperTest, SchemeOmittedDuringOrdering) {
@@ -454,9 +475,11 @@ TEST_F(PasswordsGrouperTest, SchemeOmittedDuringOrdering) {
       credential3(ip_form);
   EXPECT_THAT(
       grouper().GetAffiliatedGroupsWithGroupingInfo(),
-      ElementsAre(AffiliatedGroup({credential3}, {"https://192.168.1.1"}),
-                  AffiliatedGroup({credential1}, {"a.com"}),
-                  AffiliatedGroup({credential2}, {"b.com"})));
+      ElementsAre(
+          AffiliatedGroup({credential3}, {"https://192.168.1.1",
+                                          GetIconUrl(ip_form.signon_realm)}),
+          AffiliatedGroup({credential1}, GetDefaultBrandingInfo(credential1)),
+          AffiliatedGroup({credential2}, GetDefaultBrandingInfo(credential2))));
 }
 
 }  // namespace password_manager
