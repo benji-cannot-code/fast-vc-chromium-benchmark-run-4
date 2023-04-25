9ddaecef2795a2628bd49f48aed3c61320b60fc3
Wangyang Dai <jludwy@gmail.com>
2023-04-25 15:17:35 +0000

    Exclude unimplemented function definitions on the Android platform
    The implementation of three functions(NavigateToGooglePasswordManager, NavigateToManagePasswordsPage, NavigateToPasswordCheckupPage) is excluded from Android platform, their definition should also be excluded from Android platform.
    
    Change-Id: I4da92b40b231f9d15c863069f5485152888e4269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4473691
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: 戴汪洋 <jludwy@gmail.com>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135194}

--- chrome/browser/ui/passwords/ui_utils.h
+++ chrome/browser/ui/passwords/ui_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "build/build_config.h"
 #include "components/password_manager/core/browser/manage_passwords_referrer.h"
 #include "components/password_manager/core/browser/origin_credential_store.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -102,6 +103,7 @@ bool IsSyncingAutosignSetting(Profile* profile);
 GURL GetGooglePasswordManagerURL(
     password_manager::ManagePasswordsReferrer referrer);
 
+#if !BUILDFLAG(IS_ANDROID)
 // Navigates to the Google Password Manager, i.e. passwords.google.com.
 void NavigateToGooglePasswordManager(
     Profile* profile,
@@ -116,6 +118,7 @@ void NavigateToManagePasswordsPage(
 
 // Navigates to Passwords Checkup page.
 void NavigateToPasswordCheckupPage(Profile* profile);
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 mojo::Remote<network::mojom::URLLoaderFactory> GetURLLoaderForMainFrame(
     content::WebContents* web_contents);
