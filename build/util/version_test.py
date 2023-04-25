e4192db9a698d68fe92f5624d567a01eab0e166e
Andrew Grieve <agrieve@chromium.org>
2023-04-25 18:06:17 +0000

    Android: Delete chrome_modern_version_code from GN templates
    "Chrome Modern" SKU no longer exists.
    The logic for the version code remains in android_chrome_version.py for
    the purposes of decoding old version codes.
    
    Bug: 1363002
    Change-Id: If89acb5ccd86ae49d8b73630ceb30f3ee8b0c338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4428732
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135388}

--- build/util/version_test.py
+++ build/util/version_test.py
@@ -41,7 +41,6 @@ class _VersionTest(unittest.TestCase):
 
   _ANDROID_CHROME_VARS = [
       'chrome_version_code',
-      'chrome_modern_version_code',
       'monochrome_version_code',
       'trichrome_version_code',
       'webview_stable_version_code',
@@ -107,7 +106,6 @@ class _VersionTest(unittest.TestCase):
     contents = output['contents']
 
     self.assertRegex(contents, r'\bchrome_version_code = "\d+"\s')
-    self.assertRegex(contents, r'\bchrome_modern_version_code = "\d+"\s')
     self.assertRegex(contents, r'\bmonochrome_version_code = "\d+"\s')
     self.assertRegex(contents, r'\btrichrome_version_code = "\d+"\s')
     self.assertRegex(contents, r'\bwebview_stable_version_code = "\d+"\s')
