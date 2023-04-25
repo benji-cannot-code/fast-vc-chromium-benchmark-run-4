faba3c9b7c4aaa8782209cd64d48974d4f9e0e3c
Zelin Liu <zelin@chromium.org>
2023-04-25 21:07:00 +0000

    iwa: Override Error Page for Isolated Web Apps
    This CL enables Isolated Web Apps to have their own special override for
    error page.
    
    With this change, we rename web_app_default_offline to
    web_app_error_page. PWA will continue to use web_app_error_page for
    offline error only (i.e. ERR_INTERNET_DISCONNECTED). IWA will use this
    for all error pages.
    
    Also added |ConstructWebAppErrorPage| method which take in an additional
    message parameter. For web apps, the only message will be offline error
    message. For IWAs, we plan to add localized message for different error
    codes.
    
    Bug: 1434818
    Change-Id: I5a70c26008ec8bba78ede04bc9a3bf7007d8b6ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4421549
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135541}

--- chrome/browser/web_applications/web_app_utils.h
+++ chrome/browser/web_applications/web_app_utils.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/services/app_service/public/cpp/file_handler.h"
 #include "components/services/app_service/public/cpp/run_on_os_login_types.h"
+#include "content/public/common/alternative_error_page_override_info.mojom.h"
 
 class GURL;
 class Profile;
@@ -32,6 +33,7 @@ class FilePath;
 }
 
 namespace content {
+class RenderFrameHost;
 class BrowserContext;
 }
 
@@ -39,13 +41,13 @@ namespace web_app {
 
 class WebAppProvider;
 
-namespace default_offline {
+namespace error_page {
 // |alternative_error_page_params| dictionary key values in the
 // |AlternativeErrorPageOverrideInfo| mojom struct.
-const char kMessage[] = "web_app_default_offline_message";
+const char kMessage[] = "web_app_error_page_message";
 const char kAppShortName[] = "app_short_name";
 const char kIconUrl[] = "icon_url";
-}  // namespace default_offline
+}  // namespace error_page
 
 // These functions return true if the WebApp System or its subset is allowed
 // for a given profile.
@@ -193,6 +195,12 @@ std::string RunOnOsLoginModeToString(RunOnOsLoginMode mode);
 apps::RunOnOsLoginMode ConvertOsLoginMode(RunOnOsLoginMode login_mode);
 
 const char* IconsDownloadedResultToString(IconsDownloadedResult result);
+
+content::mojom::AlternativeErrorPageOverrideInfoPtr ConstructWebAppErrorPage(
+    const GURL& url,
+    content::RenderFrameHost* render_frame_host,
+    content::BrowserContext* browser_context,
+    std::u16string message);
 
 }  // namespace web_app
 
