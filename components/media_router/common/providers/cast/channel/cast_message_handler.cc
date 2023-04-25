30ceaf367435873cf066c516b7135d91dcddc912
Peter Kasting <pkasting@chromium.org>
2023-04-25 20:46:22 +0000

    Add missing #include.
    Bug: none
    Change-Id: Ie9195abfb7b126aa4d53ff3eeca0b6af37e141d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4476091
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135519}

--- components/media_router/common/providers/cast/channel/cast_message_handler.cc
+++ components/media_router/common/providers/cast/channel/cast_message_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/media_router/common/providers/cast/channel/cast_message_handler.h"
 
+#include <string>
 #include <tuple>
 #include <utility>
 #include <vector>
