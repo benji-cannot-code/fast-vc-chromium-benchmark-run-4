0470048d6efd806d0f536db29a69e011c12c3801
Pu Shi <pushi@google.com>
2023-04-25 05:15:05 +0000

    [PhoneHub] Add success metric to record if received PhoneStatusSnapshot
    Add a metric to record if PhoneStatusSnapshot is ever received at disconnection and a CrosState message has been sent to phone. This can help us to under stand if there is an error when initialized UI.
    
    Tested: manually verified in chromeos://histograms that data points are logged to correct buckets.
    Bug: b/279236231
    Change-Id: If48aea96fbf61ad8c5f159107c037467d2e69082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4460889
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135039}

--- chromeos/ash/components/phonehub/phone_status_processor.h
+++ chromeos/ash/components/phonehub/phone_status_processor.h
@@ -62,7 +62,7 @@ class PhoneStatusProcessor
       PrefService* pref_service,
       AppStreamManager* app_stream_manager,
       AppStreamLauncherDataModel* app_stream_launcher_data_model,
-      IconDecoder* icon_decoder,
+      IconDecoder* icon_decoder_,
       CrosStateMessageRecorder* cros_state_message_recorder);
   ~PhoneStatusProcessor() override;
 
