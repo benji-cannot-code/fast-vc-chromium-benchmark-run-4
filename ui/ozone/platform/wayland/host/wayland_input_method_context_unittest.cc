d89dab136bb62d3006248a5f432ad5ece5261703
Alex Yang <aycyang@chromium.org>
2023-04-25 15:16:28 +0000

    ozone: Construct TextInputExtension GlobalObject upfront in tests
    Since the version is now declared at TestWaylandServerThread
    construction time, we no longer need to delay the construction of the
    TextInputExtension GlobalObject, so we can avoid the heap allocation.
    
    Bug: 1315587
    Change-Id: Iedb3e5e1421ad853284bf7517c3bffb5cad3fd48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4467512
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135192}

--- ui/ozone/platform/wayland/host/wayland_input_method_context_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_input_method_context_unittest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/test/mock_zwp_text_input.h"
 #include "ui/ozone/platform/wayland/test/test_util.h"
 #include "ui/ozone/platform/wayland/test/test_wayland_server_thread.h"
+#include "ui/ozone/platform/wayland/test/test_zcr_text_input_extension.h"
 #include "ui/ozone/platform/wayland/test/wayland_test.h"
 
 using ::testing::_;
@@ -293,7 +294,8 @@ INSTANTIATE_TEST_SUITE_P(
     TextInputExtensionV7,
     WaylandInputMethodContextOldServerTest,
     ::testing::Values(wl::ServerConfig{
-        .text_input_extension_version = wl::TextInputExtensionVersion::kV7}));
+        .text_input_extension_version =
+            wl::TestZcrTextInputExtensionV1::Version::kV7}));
 
 TEST_P(WaylandInputMethodContextOldServerTest, SetContentType) {
   PostToServerAndWait([](wl::TestWaylandServerThread* server) {
