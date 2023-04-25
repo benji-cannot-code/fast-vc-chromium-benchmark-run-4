5acd35d3ba285b361ceaf60ad901c94a2c3d935e
Avi Drissman <avi@chromium.org>
2023-04-25 16:04:05 +0000

    Make the RWHVMac delegate creator return an autoreleased object
    In ARC, when a C++ function returns an Objective-C object, the default
    is that it's an autoreleased object, so change the creator function to
    do that.
    
    /!\ Embedders: If your code breaks at this CL, be sure to update
    *both* the ownership in the newly-renamed GetDelegateForHost()
    callback as well as the RenderWidgetHostViewMacDelegateCallback passed
    to CreateRenderWidgetHostNSView().
    
    This re-lands https://crrev.com/c/4458149 which crashed with app shims
    because it failed to address the codepath involving
    RenderWidgetHostViewMacDelegateCallback.
    
    Bug: 1280317
    Change-Id: I7e96e27ed7bda20f0ce8885e323977b8d613dff3
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4469710
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135220}

--- content/public/browser/remote_cocoa.h
+++ content/public/browser/remote_cocoa.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remote_cocoa {
 
+// This must return an autoreleased object.
 using RenderWidgetHostViewMacDelegateCallback =
     base::OnceCallback<NSObject<RenderWidgetHostViewMacDelegate>*()>;
 
