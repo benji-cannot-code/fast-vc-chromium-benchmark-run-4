ab556c02151bd42b8c4f5d6ed215f3c6ebdc9f19
aliaaborhamy <aliaaborhamy@google.com>
2023-04-25 17:09:54 +0000

    [iOS] Add Shortcuts widget's flags
    - Added compile time flags for the new Shortcuts widget in WidgetKit Extension.
    - Added Empty widget's placeholder for the Shortcuts widget in WidgetKit Extension.
    - Renamed the Lockscreen launcher widget's flags in the WidgetKit Extension (this widget is not a new extension) :
       - ios_enable_lockscreen_extension -> ios_enable_lockscreen_widget.
       - ios_available_lockscreen_extension -> ios_available_lockscreen_widget.
    
    
    Bug: 1373407
    Change-Id: Id4bc5e97c93911a792cda317ee97530000189d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4465956
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1135306}

--- ios/chrome/widget_kit_extension/lockscreen_launcher_widget.swift
+++ ios/chrome/widget_kit_extension/lockscreen_launcher_widget.swift
@@ -7,8 +7,8 @@ import Foundation
 import SwiftUI
 import WidgetKit
 
-#if IOS_ENABLE_LOCKSCREEN_EXTENSION
-  #if IOS_AVAILABLE_LOCKSCREEN_EXTENSION
+#if IOS_ENABLE_LOCKSCREEN_WIDGET
+  #if IOS_AVAILABLE_LOCKSCREEN_WIDGET
 
     enum LockscreenLauncherWidgetType {
       case search, incognito, voiceSearch, dinoGame
@@ -138,5 +138,5 @@ import WidgetKit
       }
     }
 
-  #endif  // IOS_AVAILABLE_LOCKSCREEN_EXTENSION
-#endif  // IOS_ENABLE_LOCKSCREEN_EXTENSION
+  #endif  // IOS_AVAILABLE_LOCKSCREEN_WIDGET
+#endif  // IOS_ENABLE_LOCKSCREEN_WIDGET
