7ff4cb908c37ba003a09f23a1f6b89f70eba4084
Mike Dougherty <michaeldo@chromium.org>
2023-04-25 14:38:26 +0000

    Update CommonJsTest and MessageJsTest to use JavascriptTest helper
    Using JavascriptTest prevents the creation of many other web classes
    within the test fixture base class. Additionally, this prevents the
    need to create a WebFramesManager for these tests in
    crrev.com/c/4442734.
    
    Change-Id: I9d979e46d03cb4b5fb52d2a7f805fce5cd52d5f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4469792
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135184}

--- ios/web/web_state/js/common_js_unittest.mm
+++ ios/web/web_state/js/common_js_unittest.mm
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <stddef.h>
 
 #import "base/strings/sys_string_conversions.h"
-#import "ios/web/public/test/web_test_with_web_state.h"
+#import "ios/web/public/test/javascript_test.h"
+#import "ios/web/public/test/js_test_util.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 
@@ -38,7 +39,18 @@ struct TestScriptAndExpectedValue {
 namespace web {
 
 // Test fixture to test common.js.
-typedef web::WebTestWithWebState CommonJsTest;
+class CommonJsTest : public web::JavascriptTest {
+ protected:
+  CommonJsTest() {}
+  ~CommonJsTest() override {}
+
+  void SetUp() override {
+    web::JavascriptTest::SetUp();
+
+    AddGCrWebScript();
+    AddCommonScript();
+  }
+};
 
 // Tests __gCrWeb.common.isTextField JavaScript API.
 TEST_F(CommonJsTest, IsTestField) {
@@ -88,10 +100,12 @@ TEST_F(CommonJsTest, IsTestField) {
       {"submit", 0, false}};
   for (size_t i = 0; i < std::size(testElements); ++i) {
     TextFieldTestElement element = testElements[i];
-    id result = ExecuteJavaScript([NSString
-        stringWithFormat:@"__gCrWeb.common.isTextField("
-                          "window.document.getElementsByName('%s')[%d])",
-                         element.element_name, element.element_index]);
+    id result = web::test::ExecuteJavaScript(
+        web_view(),
+        [NSString
+            stringWithFormat:@"__gCrWeb.common.isTextField("
+                              "window.document.getElementsByName('%s')[%d])",
+                             element.element_name, element.element_index]);
     EXPECT_NSEQ(element.expected_is_text_field ? @YES : @NO, result)
         << element.element_name << " with index " << element.element_index
         << " isTextField(): " << element.expected_is_text_field;
@@ -135,7 +149,7 @@ TEST_F(CommonJsTest, Stringify) {
     // Load a sample HTML page. As a side-effect, loading HTML via
     // `webController_` will also inject web_bundle.js.
     LoadHtml(@"<p>");
-    id result = ExecuteJavaScript(data.test_script);
+    id result = web::test::ExecuteJavaScript(web_view(), data.test_script);
     EXPECT_NSEQ(data.expected_value, result)
         << " in test " << i << ": "
         << base::SysNSStringToUTF8(data.test_script);
@@ -164,7 +178,8 @@ TEST_F(CommonJsTest, RemoveQueryAndReferenceFromURL) {
   for (size_t i = 0; i < std::size(test_data); i++) {
     LoadHtml(@"<p>");
     TestData& data = test_data[i];
-    id result = ExecuteJavaScript(
+    id result = web::test::ExecuteJavaScript(
+        web_view(),
         [NSString stringWithFormat:
                       @"__gCrWeb.common.removeQueryAndReferenceFromURL('%@')",
                       data.input_url]);
