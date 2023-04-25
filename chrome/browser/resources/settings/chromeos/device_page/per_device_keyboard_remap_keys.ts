d9f38e595d752863ea41ccfc713ce0efe02982d9
dpapad <dpapad@chromium.org>
2023-04-25 17:31:02 +0000

    Settings: Move settings-dropdown-menu to "limited sharing".
    Bug: 1431477
    Change-Id: I091a956f69c65160effeb8629e26e1b4e3bcc678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4471209
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Cole Horvitz <colehorvitz@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135335}

--- chrome/browser/resources/settings/chromeos/device_page/per_device_keyboard_remap_keys.ts
+++ chrome/browser/resources/settings/chromeos/device_page/per_device_keyboard_remap_keys.ts
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_components/settings_prefs/prefs.js';
 import '../../icons.html.js';
 import '../../settings_shared.css.js';
-import '../../controls/settings_dropdown_menu.js';
+import '/shared/settings/controls/settings_dropdown_menu.js';
 import './input_device_settings_shared.css.js';
 import './keyboard_remap_modifier_key_row.js';
 
