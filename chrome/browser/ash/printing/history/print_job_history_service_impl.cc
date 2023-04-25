5c29ed4a4293bef2a159419d3b451fd464bf4eca
Claudio DeSouza <cdesouza@igalia.com>
2023-04-25 00:27:56 +0000

    Replace base::GUID uses with base::Uuid (134/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ash/printing/history
    
    This CL was uploaded by git cl split.
    
    R=pawliczek@chromium.org
    
    Bug: 1428566
    Change-Id: If06090ae6b1e3a8d6b23829490b3b586b91f20c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454978
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134946}

--- chrome/browser/ash/printing/history/print_job_history_service_impl.cc
+++ chrome/browser/ash/printing/history/print_job_history_service_impl.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/printing/history/print_job_history_service_impl.h"
 
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/uuid.h"
 #include "chrome/browser/ash/printing/cups_print_job.h"
 #include "chrome/browser/ash/printing/history/print_job_info_proto_conversions.h"
 #include "chrome/browser/printing/print_job.h"
@@ -81,8 +81,9 @@ void PrintJobHistoryServiceImpl::SavePrintJob(base::WeakPtr<CupsPrintJob> job) {
     return;
   }
 
-  printing::proto::PrintJobInfo print_job_info =
-      CupsPrintJobToProto(*job, /*id=*/base::GenerateGUID(), base::Time::Now());
+  printing::proto::PrintJobInfo print_job_info = CupsPrintJobToProto(
+      *job, /*id=*/base::Uuid::GenerateRandomV4().AsLowercaseString(),
+      base::Time::Now());
   print_job_database_->SavePrintJob(
       print_job_info,
       base::BindOnce(&PrintJobHistoryServiceImpl::OnPrintJobSaved,
