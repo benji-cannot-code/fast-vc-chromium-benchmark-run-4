ac0b5d661c4722fcb948eb52edbaf93963f4fc55
Kent Tamura <tkent@chromium.org>
2023-04-25 05:08:43 +0000

    [RemoveLegacy] Remove layout/api/ directory
    Move the remaining files of layout/api/ to layout/.
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: I1d21ea896cd213d131e2b1baa0a47840f0cb1e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4472788
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135038}

--- third_party/blink/renderer/core/paint/selection_bounds_recorder.h
+++ third_party/blink/renderer/core/paint/selection_bounds_recorder.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_SELECTION_BOUNDS_RECORDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_SELECTION_BOUNDS_RECORDER_H_
 
-#include "third_party/blink/renderer/core/layout/api/selection_state.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_rect.h"
+#include "third_party/blink/renderer/core/layout/selection_state.h"
 #include "third_party/blink/renderer/platform/text/text_direction.h"
 #include "third_party/blink/renderer/platform/text/writing_mode.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
