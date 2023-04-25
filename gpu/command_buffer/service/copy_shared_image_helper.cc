11a31b8ccfa711f29778fd6d4a2db45296726b42
Saifuddin Hitawala <hitawala@chromium.org>
2023-04-25 22:23:12 +0000

    Graphite: Implement CreateSkImage for SkiaGraphiteRep
    This change implements CreateSkImage for SkiaGraphiteImageRepresentation
    and makes it pure virtual in the base class. It uses the SkImage::
    MakeGraphiteFromBackendTexture and MakeGraphiteFromYUVABackendTextures
    apis for creating SkImage using recorder and backend textures.
    
    Bug: 1430206
    Change-Id: Ic565d2c45dd727bc6a3674f656c49d2b3457989f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4476224
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135586}

--- gpu/command_buffer/service/copy_shared_image_helper.cc
+++ gpu/command_buffer/service/copy_shared_image_helper.cc
@@ -336,8 +336,7 @@ base::expected<void, GLError> CopySharedImageHelper::ConvertRGBAToYUVAMailboxes(
                                     "glConvertYUVAMailboxesToRGB",
                                     "RGBA shared image is not readable"));
   }
-  auto rgba_sk_image =
-      rgba_scoped_access->CreateSkImage(shared_context_state_->gr_context());
+  auto rgba_sk_image = rgba_scoped_access->CreateSkImage(shared_context_state_);
   if (!rgba_sk_image) {
     return base::unexpected(GLError(GL_INVALID_OPERATION,
                                     "glReadbackImagePixels",
@@ -635,7 +634,7 @@ base::expected<void, GLError> CopySharedImageHelper::CopySharedImage(
 
   base::expected<void, GLError> result = base::ok();
   auto source_image =
-      source_scoped_access->CreateSkImage(shared_context_state_->gr_context());
+      source_scoped_access->CreateSkImage(shared_context_state_);
   if (!source_image) {
     result = base::unexpected(
         GLError(GL_INVALID_VALUE, "glCopySubTexture",
@@ -796,7 +795,7 @@ base::expected<void, GLError> CopySharedImageHelper::CopySharedImageToGLTexture(
 
   base::expected<void, GLError> result = base::ok();
   auto source_image =
-      source_scoped_access->CreateSkImage(shared_context_state_->gr_context());
+      source_scoped_access->CreateSkImage(shared_context_state_);
   if (!source_image) {
     result = base::unexpected<GLError>(
         GLError(GL_INVALID_VALUE, "glCopySharedImageToTexture",
@@ -855,12 +854,11 @@ base::expected<void, GLError> CopySharedImageHelper::ReadPixels(
   if (source_shared_image->format().is_single_plane()) {
     // Create SkImage without plane index for single planar formats or legacy
     // multiplanar formats with external sampler.
-    sk_image = source_scoped_access->CreateSkImage(
-        shared_context_state_->gr_context());
+    sk_image = source_scoped_access->CreateSkImage(shared_context_state_);
   } else {
     // Pass plane index for creating an SkImage for multiplanar formats.
     sk_image = source_scoped_access->CreateSkImageForPlane(
-        plane_index, shared_context_state_->gr_context());
+        plane_index, shared_context_state_);
   }
 
   base::expected<void, GLError> result = base::ok();
