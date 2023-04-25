421e6647947e2ff838e293cba76546c730ab1bfb
Sreeja Kamishetty <sreejakshetty@chromium.org>
2023-04-25 20:05:34 +0000

    [Clank] Create SpareTab when Start Surface is Shown
    As the first use case of SpareTab - we use it to speed up navigations
    from start surface. The creation of spare tabs is only initiated after native is complete and the start surface has been displayed. A spare tab is created in a PostTask so that it won't interfere with high priority tasks. We destroy the spare tab when the start surface is hidden to cleanup the resources.
    
    In the previous CLs we have introduced spareTab concept in Clank
    crrev.com/c/4409587 (creation of spare tab) and crrev.com/c/4415678 (loading url in spare tab).
    
    New feature ChromeFeatureList.StartSurfaceSpareTab is added to create spare tab. All this implementation is behind this flag.
    
    Testing
    Tests are added to StartSurfaceTest to verify the functionality of this new feature, such as spare tab creation, deletion, usage when start is shown, hidden and navigated respectively.
    
    Detailed design doc
    https://docs.google.com/document/d/1blqCc7W8oGNtKVn90yLJDYUWSqzoO03aE5tyk1CrWLM/edit?usp=sharing&resourcekey=0-qj1ZVcaqm2s3vOR2eEQqJA
    
    Mock with SpareTab Optimization on Start Surface
    https://drive.google.com/file/d/1nv3jO1uJXw49HGNVUq5SYP01465Se_2Z/view?usp=sharing&resourcekey=0-Pos1rEF1t88zNfnbw4r_DA
    
    BUG=1412572
    
    Validate-Test-Flakiness: skip
    Change-Id: I447d0f84543f179c5df4b9c8aa0a55e92e749dae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348048
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135489}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -332,6 +332,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kStartSurfaceOnTablet,
     &kStartSurfaceReturnTime,
     &kStartSurfaceRefactor,
+    &kStartSurfaceSpareTab,
     &kStartSurfaceDisabledFeedImprovement,
     &kStartSurfaceWithAccessibility,
     &kUmaBackgroundSessions,
@@ -1118,6 +1119,10 @@ BASE_FEATURE(kShouldIgnoreIntentSkipInternalCheck,
 
 BASE_FEATURE(kStartSurfaceOnTablet,
              "StartSurfaceOnTablet",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kStartSurfaceSpareTab,
+             "StartSurfaceSpareTab",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kStartSurfaceReturnTime,
