bf9eb741d2c6b316b6616023b54d92a5a0b42935
Sergey Ulanov <sergeyu@chromium.org>
2023-04-25 20:56:38 +0000

    [cast_streaming] Ensure stream continuity in FrameInjectingDemuxer
    Previously FrameInjectingDemuxer was producing streams with gaps, while
    AudioRenderer implementations expect contiguous streams. Updated the
    implementation to ensure continuity. The demuxer now detects large gaps
    between audio packets and adjusts timestamps in both audio and video
    streams to remove these gaps.
    
    Bug: 1426979
    Bug: b/261641119
    Change-Id: Ia7e11f819ec3f370f2170095e6418c5e51348a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379097
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135528}

--- components/cast_streaming/renderer/frame/frame_injecting_demuxer.h
+++ components/cast_streaming/renderer/frame/frame_injecting_demuxer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cast_streaming {
 
+class StreamTimestampOffsetTracker;
 class FrameInjectingAudioDemuxerStream;
 class FrameInjectingVideoDemuxerStream;
 class DemuxerConnector;
@@ -79,6 +80,8 @@ class FrameInjectingDemuxer final : public media::Demuxer {
   scoped_refptr<base::SequencedTaskRunner> media_task_runner_;
   scoped_refptr<base::SequencedTaskRunner> original_task_runner_;
   media::DemuxerHost* host_ = nullptr;
+
+  scoped_refptr<StreamTimestampOffsetTracker> timestamp_tracker_;
   std::unique_ptr<FrameInjectingAudioDemuxerStream> audio_stream_;
   std::unique_ptr<FrameInjectingVideoDemuxerStream> video_stream_;
 
