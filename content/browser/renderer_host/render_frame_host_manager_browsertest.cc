b364c1ce4b191ac463a59b6515b6ddd722bf9ef5
Keishi Hattori <keishi@chromium.org>
2023-04-25 06:58:30 +0000

    [Sheriff] Disable flaky PostMessageToParentWhenSubframeNavigates test
    Disable PostMessageToParentWhenSubframeNavigates test for Linux CFI
    because it is flaky.
    
    Bug: 1439244
    Change-Id: I71e0fd4b090bbd997bbec306ebfe7c8046bce5b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4473687
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135072}

--- content/browser/renderer_host/render_frame_host_manager_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_manager_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 
+#include "base/cfi_buildflags.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
@@ -5759,9 +5760,11 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
 // its unload handler is able to send a postMessage to the parent frame.
 // See https://crbug.com/857274.
 // TODO(https://crbug.com/989704): Fix flake on Linux TSAN and ASAN.
+// TODO(https://crbug.com/1439710): Fix flake on Linux CFI.
 #if BUILDFLAG(IS_MAC) || (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)) || \
     (BUILDFLAG(IS_LINUX) &&                                                   \
-     (defined(ADDRESS_SANITIZER) || defined(THREAD_SANITIZER)))
+     (defined(ADDRESS_SANITIZER) || defined(THREAD_SANITIZER) ||              \
+      BUILDFLAG(CFI_ICALL_CHECK)))
 #define MAYBE_PostMessageToParentWhenSubframeNavigates \
   DISABLED_PostMessageToParentWhenSubframeNavigates
 #else
