b5115bfd3190cda36081dd26a092e10e207c85ca
Claudio DeSouza <cdesouza@igalia.com>
2023-04-25 17:17:16 +0000

    Replace base::GUID uses with base::Uuid (197/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/credential_provider
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: I4b2879aea3d8b8fa2736f4b10b675a4fdbf6fa3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456567
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135317}

--- chrome/credential_provider/extension/task_manager_unittests.cc
+++ chrome/credential_provider/extension/task_manager_unittests.cc
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "base/functional/bind.h"
-#include "base/guid.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "base/uuid.h"
 #include "chrome/credential_provider/extension/extension_utils.h"
 #include "chrome/credential_provider/extension/task.h"
 #include "chrome/credential_provider/extension/task_manager.h"
@@ -185,7 +185,8 @@ TEST_F(TaskManagerTest, TaskExecuted) {
 
   FakeTokenGenerator fake_token_generator;
   fake_token_generator.SetTokensForTesting(
-      {base::GenerateGUID(), base::GenerateGUID()});
+      {base::Uuid::GenerateRandomV4().AsLowercaseString(),
+       base::Uuid::GenerateRandomV4().AsLowercaseString()});
 
   ASSERT_EQ(S_OK, GenerateGCPWDmToken((BSTR)sid1));
 
@@ -285,7 +286,8 @@ TEST_F(TaskManagerTest, BackOff) {
 
   FakeTokenGenerator fake_token_generator;
   fake_token_generator.SetTokensForTesting(
-      {base::GenerateGUID(), base::GenerateGUID()});
+      {base::Uuid::GenerateRandomV4().AsLowercaseString(),
+       base::Uuid::GenerateRandomV4().AsLowercaseString()});
 
   ASSERT_EQ(S_OK, GenerateGCPWDmToken((BSTR)sid1));
 
