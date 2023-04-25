5a119f8a93e1f72656ef8063a8c4ed5f0e3a8428
Guillem Perez <guiperez@google.com>
2023-04-25 22:41:10 +0000

    [iOS] Refactor new tab page metrics constants to a constants file
    Moving all the histograms and actions to a constants file.
    
    Bug: 1431193
    Change-Id: I5c152f042b1aea2c3328eee98f054faff0e34e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475949
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1135600}

---
+++ ios/chrome/browser/ui/ntp/metrics/new_tab_page_metrics_constants.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_NTP_METRICS_NEW_TAB_PAGE_METRICS_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_UI_NTP_METRICS_NEW_TAB_PAGE_METRICS_CONSTANTS_H_
+
+#pragma mark - Actions
+
+extern const char kFakeboxNTPTappedAction[];
+extern const char kFakeViewNTPTappedAction[];
+extern const char kMostVisitedVoiceSearchAction[];
+extern const char kNTPEntrypointTappedAction[];
+extern const char kNTPIdentityDiscTappedAction[];
+
+#pragma mark - Histograms
+
+extern const char kHomeActionOnNTPHistogram[];
+extern const char kHomeActionOnStartSurfaceHistogram[];
+extern const char kNTPTimeSpentHistogram[];
+extern const char kNTPImpressionHistogram[];
+extern const char kNTPOverscrollActionHistogram[];
+
+#endif  // IOS_CHROME_BROWSER_UI_NTP_METRICS_NEW_TAB_PAGE_METRICS_CONSTANTS_H_
