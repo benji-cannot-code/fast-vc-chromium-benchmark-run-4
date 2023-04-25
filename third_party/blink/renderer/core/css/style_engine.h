041f1a55076397bf8ddee2c4741583a70a9ef4af
Daniil Sakhapov <sakhapov@chromium.org>
2023-04-25 09:23:24 +0000

    Revert "Implement contain style scope manager"
    This reverts commit bf700377ff5911705a81a410be2fad9018aae9d1.
    
    Reason for revert: Performance regression
    
    Original change's description:
    > Implement contain style scope manager
    >
    > In order to be able to handle quotes (and later counters)
    > with respect to the "contain: style" subtrees, a contain style scope
    > manager has been introduced.
    >
    > It manages quotes by putting them in appropriate scope, creating and
    > deleting scopes when necessary.
    >
    > Design doc: https://docs.google.com/document/d/1zDanh4b4TXraq1b0JikhDdgZOIzZZxE0M55oKzDxtNg/edit?usp=sharing
    >
    > Bug: 882385
    > Change-Id: I59bd3f2a9876b39a4b5fe2a804acd4a9d6d3b0a9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4334605
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Fredrik Söderquist <fs@opera.com>
    > Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1134542}
    
    Bug: 882385
    Change-Id: I181288e63061cd513b3d05bc9de4b7e46897d397
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4474627
    Auto-Submit: Daniil Sakhapov <sakhapov@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1135100}

--- third_party/blink/renderer/core/css/style_engine.h
+++ third_party/blink/renderer/core/css/style_engine.h
@@ -75,7 +75,6 @@ namespace blink {
 class ComputedStyleBuilder;
 class CounterStyle;
 class CounterStyleMap;
-class StyleContainmentScopeTree;
 class CSSFontSelector;
 class CSSPropertyValueSet;
 class CSSStyleSheet;
@@ -344,10 +343,6 @@ class CORE_EXPORT StyleEngine final : public GarbageCollected<StyleEngine>,
   StyleResolver& GetStyleResolver() const {
     DCHECK(resolver_);
     return *resolver_;
-  }
-
-  StyleContainmentScopeTree& GetStyleContainmentScopeTree() const {
-    return *style_containment_scope_tree_;
   }
 
   void SetRuleUsageTracker(StyleRuleUsageTracker*);
@@ -827,9 +822,6 @@ class CORE_EXPORT StyleEngine final : public GarbageCollected<StyleEngine>,
   bool AllowSkipStyleRecalcForScope() const;
 
   Member<Document> document_;
-
-  // Tree of style containment scopes. Is in charge of the document's quotes.
-  Member<StyleContainmentScopeTree> style_containment_scope_tree_;
 
   // Tracks the number of currently loading top-level stylesheets. Sheets loaded
   // using the @import directive are not included in this count. We use this
--- third_party/blink/renderer/core/dom/pseudo_element.cc
+++ third_party/blink/renderer/core/dom/pseudo_element.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
-#include "third_party/blink/renderer/core/css/style_containment_scope_tree.h"
 #include "third_party/blink/renderer/core/dom/element_rare_data_vector.h"
 #include "third_party/blink/renderer/core/dom/first_letter_pseudo_element.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
@@ -308,14 +307,8 @@ void PseudoElement::AttachLayoutTree(AttachContext& context) {
       LayoutObject* child = content->CreateLayoutObject(*this, style);
       if (layout_object->IsChildAllowed(child, style)) {
         layout_object->AddChild(child);
-        if (child->IsQuote()) {
-          StyleContainmentScopeTree& tree =
-              GetDocument().GetStyleEngine().GetStyleContainmentScopeTree();
-          StyleContainmentScope* scope =
-              tree.FindOrCreateEnclosingScopeForElement(*this);
-          scope->AttachQuote(*To<LayoutQuote>(child));
-          tree.UpdateOutermostDirtyScope(scope);
-        }
+        if (child->IsQuote())
+          To<LayoutQuote>(child)->AttachQuote();
       } else {
         child->Destroy();
       }
