5ac13aff6bf76aee1490bc535780fcf3ecb3ee41
Eric Lawrence <ericlaw@microsoft.com>
2023-04-25 22:08:03 +0000

    Extensions: Don't pass INVALID_HANDLE_VALUE to NativeHost App
    Chrome 113 on Windows began directly invoking Native Host applications
    rather than using cmd.exe as a proxy. Whenever Chrome is started without
    redirecting STD_ERROR, the original CL would pass INVALID_HANDLE_VALUE
    as the value of |hStdError|, whereas prior to that CL, the cmd.exe used
    as a proxy would launch the process with 0 as the value.
    
    This behavioral difference resulted in application compatibility
    problems with host applications that can accommodate a handle of 0 but
    which fail when receiving INVALID_HANDLE_VALUE.
    
    This change updates Chrome to always pass along its own |hStdError|
    handle, even if it is 0.
    
    Bug: 1433711
    Change-Id: I449fe5a4fbacf38de6a59efc4e020122b9f77af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437512
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Eric Lawrence <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1135573}

--- chrome/browser/extensions/api/messaging/native_messaging_test_echo_host.cc
+++ chrome/browser/extensions/api/messaging/native_messaging_test_echo_host.cc
@@ -11,6 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 
 int main(int argc, char* argv[]) {
+  // Ensure stderr isn't pointed at INVALID_HANDLE_VALUE. crbug.com/1433711
+  {
+    HANDLE hStdError = GetStdHandle(STD_ERROR_HANDLE);
+    if (INVALID_HANDLE_VALUE == hStdError) {
+      return -1;
+    }
+  }
+
   base::File read_stream = base::File(GetStdHandle(STD_INPUT_HANDLE));
   base::File write_stream = base::File(GetStdHandle(STD_OUTPUT_HANDLE));
 
