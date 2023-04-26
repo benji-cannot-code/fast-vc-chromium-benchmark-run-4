81d3f9eaea1b2fbedeb8a0751979b5f23e8bc70a
George Benz <gbj@google.com>
2023-04-26 19:15:19 +0000

    Fixes saved device expiration timer on reset.
    This patch checks that devices are manually added devices before they are stored/there expiration timers are reset.
    
    Bug: b/279070821
    Change-Id: If23d2ec44933555d97461dcf0d71cd2f4eb35075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4461491
    Commit-Queue: george benz <gbj@google.com>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136084}

--- chrome/browser/media/router/discovery/access_code/access_code_cast_sink_service.h
+++ chrome/browser/media/router/discovery/access_code/access_code_cast_sink_service.h
@@ -209,6 +209,8 @@ class AccessCodeCastSinkService : public KeyedService,
   FRIEND_TEST_ALL_PREFIXES(AccessCodeCastSinkServiceTest, RecordRouteDuration);
   FRIEND_TEST_ALL_PREFIXES(AccessCodeCastSinkServiceTest,
                            RecordRouteDurationNonAccessCodeDevice);
+  FRIEND_TEST_ALL_PREFIXES(AccessCodeCastSinkServiceTest,
+                           RestartExpirationTimerDoesntResetTimer);
 
   // Use |AccessCodeCastSinkServiceFactory::GetForProfile(..)| to get
   // an instance of this service.
@@ -227,7 +229,7 @@ class AccessCodeCastSinkService : public KeyedService,
                              AddSinkResultCode result_code);
 
   void OnChannelOpenedResult(AddSinkResultCallback add_sink_callback,
-                             MediaSink::Id sink_id,
+                             const MediaSinkInternal& sink,
                              bool channel_opened);
 
   bool IsSinkValidAccessCodeSink(const MediaSinkInternal* sink);
