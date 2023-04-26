631f93bc860e532844262f0a0f5d59ccc9b6a567
Ho Cheung <uioptt24@gmail.com>
2023-04-26 07:36:08 +0000

    Remove `base/cxx17_backports.h` from the code in extensions/
    Remove the reference to `base/cxx17_backports.h` from the code.
    
    Bug: 1373621
    Change-Id: I6086b934fbb324f8f2a298a35c109338cc00c5d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4478310
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Quick-Run: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ho Cheung <uioptt24@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1135771}

--- extensions/common/image_util.cc
+++ extensions/common/image_util.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/check.h"
-#include "base/cxx17_backports.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
