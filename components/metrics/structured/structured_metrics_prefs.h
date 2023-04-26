25e5062f0de4c4cb20cfd32dd594f65305d1a52d
Andrew Bregger <andrewbregger@google.com>
2023-04-26 21:01:40 +0000

    [SM Service] Adding reporting service for Structure Metrics.
    BUG: crbug.com/1434483
    
    Change-Id: I5ed0617c33cd206da2fb135af183011838403e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456901
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136146}

---
+++ components/metrics/structured/structured_metrics_prefs.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef COMPONENTS_METRICS_STRUCTURED_STRUCTURED_METRICS_PREFS_H_
+#define COMPONENTS_METRICS_STRUCTURED_STRUCTURED_METRICS_PREFS_H_
+
+namespace metrics::structured::prefs {
+
+// Preference which stores serialized StructuredMetrics logs to be uploaded.
+extern const char kLogStoreName[];
+
+}  // namespace metrics::structured::prefs
+
+#endif  // COMPONENTS_METRICS_STRUCTURED_STRUCTURED_METRICS_PREFS_H_
