445e40bfc4fcfdf45a27d9ced8421633938a93ed
Tarun Bansal <tbansal@google.com>
2023-04-26 15:19:03 +0000

    Remove unused login_detection code
    Remove unused login_detection code that integrates with chrome
    browser password manager or optimization guide.
    
    Logic related to OAuth is not deleted because it's used
    by site isolation. The persistence logic that keeps track of
    whether the user logged-in to a site is tracked by the
    SiteIsolation layer.
    
    Change-Id: I9d9a1311a932d8897c02acf29076251f62fe7c5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410784
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135912}

--- chrome/browser/login_detection/login_detection_keyed_service_factory.cc
+++ chrome/browser/login_detection/login_detection_keyed_service_factory.cc
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "chrome/browser/login_detection/login_detection_keyed_service.h"
 #include "chrome/browser/login_detection/login_detection_util.h"
-#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
-#include "chrome/browser/password_manager/account_password_store_factory.h"
-#include "chrome/browser/password_manager/password_store_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/browser_context.h"
 
@@ -47,11 +44,7 @@ LoginDetectionKeyedServiceFactory::GetInstance() {
 
 LoginDetectionKeyedServiceFactory::LoginDetectionKeyedServiceFactory()
     : ProfileKeyedServiceFactory("LoginDetectionKeyedService",
-                                 BuildLoginDetectionProfileSelection()) {
-  DependsOn(AccountPasswordStoreFactory::GetInstance());
-  DependsOn(PasswordStoreFactory::GetInstance());
-  DependsOn(OptimizationGuideKeyedServiceFactory::GetInstance());
-}
+                                 BuildLoginDetectionProfileSelection()) {}
 
 LoginDetectionKeyedServiceFactory::~LoginDetectionKeyedServiceFactory() =
     default;
