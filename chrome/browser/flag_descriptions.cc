e2c7ab83583fa9803436d8e25f21af21a8b1a37d
Sophie Chang <sophiechang@chromium.org>
2023-04-26 22:09:34 +0000

    Move flags to 120 for Journeys and Optimization Guide and deprecate some old ones
    Bug: 1440410,1440403,1440402,1440400,1440399,1440397,1440395,1440394,1440348,1440344,1440340
    Change-Id: I9d64d8f39a4a6549abe7c83598d4cfdbb13e5a20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481769
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1136187}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1927,11 +1927,6 @@ const char kJourneysLabelsName[] = "History Journeys Labels";
 const char kJourneysLabelsDescription[] =
     "Enables labels for Journeys within the History Journeys UI.";
 
-const char kJourneysNavigationContextClusteringName[] =
-    "History Journeys Navigation Context Clustering";
-const char kJourneysNavigationContextClusteringDescription[] =
-    "Enables context clustering to be done at navigation time";
-
 const char kJourneysOmniboxActionName[] = "History Journeys Omnibox Action";
 const char kJourneysOmniboxActionDescription[] =
     "Enables the History Journeys Omnibox Action.";
@@ -1946,11 +1941,6 @@ const char kJourneysPersistCachesToPrefsName[] =
     "History Journeys Persist Caches to Prefs";
 const char kJourneysPersistCachesToPrefsDescription[] =
     "Enables the persistence of Journeys keyword caches via the pref service.";
-
-const char kJourneysPersistedClustersName[] =
-    "History Journeys Persisted Clusters";
-const char kJourneysPersistedClustersDescription[] =
-    "Enables the persistence of clusters into the history database.";
 
 const char kJourneysShowAllClustersName[] =
     "History Journeys Show All Clusters";
