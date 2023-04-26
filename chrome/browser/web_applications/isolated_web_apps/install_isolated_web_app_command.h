727d765c276412ec4c70ed4b630df062532598c5
Christian Flach <cmfcmf@chromium.org>
2023-04-26 17:14:08 +0000

    iwa: Add IsolatedWebAppCommandLineInstallManager for CLI installation
    There are two command line switches to install an IWA for development
    purposes: `--install-isolated-web-app-from-url` and `...-from-file`.
    
    `MaybeInstallAppFromCommandLine` parsed these switches, and, if present,
    installed an IWA based on these switches.
    
    This CL refactors the function to instead forward the installation
    request to a newly introduced `IsolatedWebAppCommandLineInstallManager`.
    `MaybeInstallAppFromCommandLine` used blocking file I/O on the UI thread
    (`base::MakeAbsoluteFilePath`, `base::PathExists`, and
    `base::DirectoryExists`). The refactored code instead runs these
    functions on a separate thread that allows blocking. The new code also
    creates `Scoped(Profile)KeepAlive`s so that browser and profile are kept
    alive during installation.
    
    Now that the `IsolatedWebAppCommandLineInstallManager` is owned by the
    `WebAppProvider`, we also no longer need to worry about profile
    shutdowns, since the `WebAppProvider` and hence the
    `IsolatedWebAppCommandLineInstallManager` will be deleted before the
    profile shuts down.
    
    Low-Coverage-Reason: Debug logging code in profile_keep_alive_types and
                         keep_alive_types has low coverage, which seems
    		     fine for that code and also matches existing
                         coverage in these files.
    Bug: 1426677
    
    Change-Id: I0f2bf58cd911eabbbbb2eb78dc206e2617259023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370305
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135993}

--- chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.h
+++ chrome/browser/web_applications/isolated_web_apps/install_isolated_web_app_command.h
@@ -90,8 +90,8 @@ class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
       const IsolatedWebAppLocation& location,
       std::unique_ptr<content::WebContents> web_contents,
       std::unique_ptr<WebAppUrlLoader> url_loader,
-      std::unique_ptr<ScopedKeepAlive> keep_alive,
-      std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive,
+      std::unique_ptr<ScopedKeepAlive> optional_keep_alive,
+      std::unique_ptr<ScopedProfileKeepAlive> optional_profile_keep_alive,
       base::OnceCallback<
           void(base::expected<InstallIsolatedWebAppCommandSuccess,
                               InstallIsolatedWebAppCommandError>)> callback,
@@ -168,8 +168,8 @@ class InstallIsolatedWebAppCommand : public WebAppCommandTemplate<AppLock> {
 
   std::unique_ptr<WebAppUrlLoader> url_loader_;
 
-  std::unique_ptr<ScopedKeepAlive> keep_alive_;
-  std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive_;
+  std::unique_ptr<ScopedKeepAlive> optional_keep_alive_;
+  std::unique_ptr<ScopedProfileKeepAlive> optional_profile_keep_alive_;
 
   std::unique_ptr<WebAppDataRetriever> data_retriever_;
 
--- chrome/browser/web_applications/web_app_command_scheduler.h
+++ chrome/browser/web_applications/web_app_command_scheduler.h
@@ -64,7 +64,7 @@ class WebAppCommandScheduler {
                      InstallIsolatedWebAppCommandError>)>;
 
   WebAppCommandScheduler(Profile& profile, WebAppProvider* provider);
-  ~WebAppCommandScheduler();
+  virtual ~WebAppCommandScheduler();
 
   void Shutdown();
 
@@ -163,11 +163,11 @@ class WebAppCommandScheduler {
 
   // Schedules a command that installs the Isolated Web App described by the
   // given IsolatedWebAppUrlInfo and IsolationData.
-  void InstallIsolatedWebApp(
+  virtual void InstallIsolatedWebApp(
       const IsolatedWebAppUrlInfo& url_info,
       const IsolatedWebAppLocation& location,
-      std::unique_ptr<ScopedKeepAlive> keep_alive,
-      std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive,
+      std::unique_ptr<ScopedKeepAlive> optional_keep_alive,
+      std::unique_ptr<ScopedProfileKeepAlive> optional_profile_keep_alive,
       InstallIsolatedWebAppCallback callback,
       const base::Location& call_location = FROM_HERE);
 
