8bcecc94d371aef1dc1f7c58aca94bcbae5909e7
Richard Chen <richche@chromium.org>
2023-04-26 22:19:49 +0000

    CRX Telemetry: Cookies Signal 100% Stable launch.
    - Enable kExtensionTelemetryCookiesGetAllSignal by default
    - Remove entry in fieldtrial_testing_config.json
    
    Bug: 1350204
    Change-Id: Ifc8ec832438003589fd8053ac75a96fb3e847094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482414
    Commit-Queue: Richard Chen <richche@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136198}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -101,7 +101,7 @@ BASE_FEATURE(kExtensionTelemetryConfiguration,
 
 BASE_FEATURE(kExtensionTelemetryCookiesGetAllSignal,
              "SafeBrowsingExtensionTelemetryCookiesGetAllSignal",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionTelemetryPersistence,
              "SafeBrowsingExtensionTelemetryPersistence",
@@ -146,7 +146,7 @@ BASE_FEATURE(kExtensionTelemetryTabsExecuteScriptSignal,
 
 BASE_FEATURE(kExtensionTelemetryCookiesGetSignal,
              "SafeBrowsingExtensionTelemetryCookiesGetSignal",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionTelemetryDeclarativeNetRequestSignal,
              "SafeBrowsingExtensionTelemetryDeclarativeNetRequestSignal",
