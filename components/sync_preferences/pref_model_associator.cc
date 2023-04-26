6015d88c2bf62ab6fed0bcc8a832eaaca0709ea1
Peter Kasting <pkasting@chromium.org>
2023-04-26 16:57:22 +0000

    Trivial: DCHECK_EQ gives slightly nicer error output
    Bug: none
    Change-Id: I9469ee1cc14ed214f7386dd92c03b48ecf3c95a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4476891
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135980}

--- components/sync_preferences/pref_model_associator.cc
+++ components/sync_preferences/pref_model_associator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/auto_reset.h"
+#include "base/check_op.h"
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/json/json_reader.h"
@@ -141,7 +142,7 @@ void PrefModelAssociator::InitPrefAndAssociate(
 
   if (sync_pref.IsValid()) {
     const sync_pb::PreferenceSpecifics& preference = GetSpecifics(sync_pref);
-    DCHECK(pref_name == preference.name());
+    CHECK_EQ(pref_name, preference.name());
     base::JSONReader::Result parsed_json =
         base::JSONReader::ReadAndReturnValueWithError(preference.value());
     if (!parsed_json.has_value()) {
