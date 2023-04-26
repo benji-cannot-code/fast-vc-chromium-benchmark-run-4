accedc7e5e917d35fd9b79be91de0395d910f3ac
Ho Cheung <uioptt24@gmail.com>
2023-04-26 11:56:24 +0000

    Remove `base/cxx17_backports.h` from the code in ios/
    Remove the reference to `base/cxx17_backports.h` from the code.
    
    Bug: 1373621
    Change-Id: Id983a24f9aeea57b2286492ca87b36c573a1138a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4477771
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135842}

--- ios/chrome/browser/web_state_list/web_state_list_order_controller.mm
+++ ios/chrome/browser/web_state_list/web_state_list_order_controller.mm
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/web_state_list/web_state_list_order_controller.h"
 
+#import <algorithm>
 #import <cstdint>
 #import <set>
 
 #import "base/check_op.h"
-#import "base/cxx17_backports.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_removing_indexes.h"
 #import "ios/chrome/browser/web_state_list/web_state_opener.h"
