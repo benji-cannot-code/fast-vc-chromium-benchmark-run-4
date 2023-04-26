ff180aede113b60626288ba0b042960c1961bd25
Claudio DeSouza <cdesouza@igalia.com>
2023-04-26 15:40:01 +0000

    Replace base::GUID uses with base::Uuid (118/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ash/app_list
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: I54ae7924db9764bfe0032f6ff2a39df18164da2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454716
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1135927}

--- chrome/browser/ash/app_list/app_list_sync_model_sanitizer.cc
+++ chrome/browser/ash/app_list/app_list_sync_model_sanitizer.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_config.h"
 #include "base/check.h"
 #include "base/containers/contains.h"
-#include "base/guid.h"
 #include "base/strings/string_util.h"
+#include "base/uuid.h"
 #include "chrome/browser/ash/app_list/app_list_model_updater.h"
 #include "chrome/browser/ash/app_list/app_list_syncable_service.h"
 #include "components/sync/model/string_ordinal.h"
@@ -147,7 +147,8 @@ void AppListSyncModelSanitizer::SanitizePageBreaks(
 
   for (const auto& position : page_breaks_to_add) {
     syncable_service_->AddPageBreakItem(
-        std::string(kImplicitPageBreakIdPrefix) + base::GenerateGUID(),
+        std::string(kImplicitPageBreakIdPrefix) +
+            base::Uuid::GenerateRandomV4().AsLowercaseString(),
         position);
   }
 
