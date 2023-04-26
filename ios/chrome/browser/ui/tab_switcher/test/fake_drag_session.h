32c49774db44ad1ab5edf83b1a7cfcfe8ec2c198
Ewann Pelle <ewannpv@chromium.org>
2023-04-26 12:34:54 +0000

    [iOS][PT] Adds PinnedTabsMediatorTest
    This CL unit tests the PinnedTabsMediator.
    It also creates some fake objects used in the tests.
    
    Bug: 1431949
    Change-Id: I3753360416734f967fdae0a2530b66aa0ca8d2d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4466297
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135850}

---
+++ ios/chrome/browser/ui/tab_switcher/test/fake_drag_session.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TEST_FAKE_DRAG_SESSION_H_
+#define IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TEST_FAKE_DRAG_SESSION_H_
+
+#import <UIKit/UIKit.h>
+
+// Test object that conforms to UIDragSession.
+@interface FakeDragSession : NSObject <UIDragSession>
+
+@property(nonatomic, readonly) NSArray<UIDragItem*>* items;
+@property(nonatomic, readonly) BOOL allowsMoveOperation;
+@property(nonatomic, readonly, getter=isRestrictedToDraggingApplication)
+    BOOL restrictedToDraggingApplication;
+@property(nonatomic, strong) id localContext;
+
+- (instancetype)initWithItems:(NSArray<UIDragItem*>*)items;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TEST_FAKE_DRAG_SESSION_H_
