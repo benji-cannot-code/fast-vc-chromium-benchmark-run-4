e0ee458aaa2390d29359d76332919b06e79c10ca
Ho Cheung <uioptt24@gmail.com>
2023-04-26 07:47:05 +0000

    Remove `base/cxx17_backports.h` from the code in services/
    Remove the reference to `base/cxx17_backports.h` from the code.
    
    Bug: 1373621
    Change-Id: I35036563af0397d8ecc51ebcca9365ad176412fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4477571
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135774}

--- services/network/public/cpp/client_hints.cc
+++ services/network/public/cpp/client_hints.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/cpp/client_hints.h"
 
+#include <algorithm>
 #include <utility>
 #include <vector>
 
-#include "base/cxx17_backports.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
