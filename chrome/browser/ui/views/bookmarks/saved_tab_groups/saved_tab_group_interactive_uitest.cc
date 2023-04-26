42bfab7b2a9518cd9d75a0c9dd40580389b8b866
Tim Judkins <tjudkins@chromium.org>
2023-04-26 20:14:36 +0000

    [Sheriff] Disable failing SavedTabGroupInteractiveTest on WIN
    Test has been failing on the Win10 Tests x64 builder e.g.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/b8782739564466847073/overview
    
    Bug: 1440199
    Change-Id: Icea108a4d4a3824487b45d726fd2842e0a39ce71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4477594
    Reviewed-by: Iman Saboori <isaboori@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Owners-Override: Tim Judkins <tjudkins@google.com>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136123}

--- chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_interactive_uitest.cc
+++ chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_interactive_uitest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/gtest_util.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
@@ -127,8 +128,15 @@ IN_PROC_BROWSER_TEST_F(SavedTabGroupInteractiveTest, CreateGroupAndSave) {
                   WaitForShow(kSavedTabGroupButtonElementId, true));
 }
 
+// TODO(crbug.com/1440199): Re-enable this test
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_UnsaveGroupFromTabGroupHeader \
+  DISABLED_UnsaveGroupFromTabGroupHeader
+#else
+#define MAYBE_UnsaveGroupFromTabGroupHeader UnsaveGroupFromTabGroupHeader
+#endif
 IN_PROC_BROWSER_TEST_F(SavedTabGroupInteractiveTest,
-                       UnsaveGroupFromTabGroupHeader) {
+                       MAYBE_UnsaveGroupFromTabGroupHeader) {
   const tab_groups::TabGroupId group_id =
       browser()->tab_strip_model()->AddToNewGroup({0});
 
