9bc79090d1143804529c291e7ba0b467f6e237be
Brian Osman <brianosman@google.com>
2023-04-26 13:37:12 +0000

    [pdf] Remove staging code for Skia analytic antialiasing
    I previously added code to migrate individual test suites to Skia's
    higher quality analytic antialiasing. At this point, analytic AA is
    always enabled (via #define), so this staging code is unnecessary.
    
    Bug: chromium:1421297
    Change-Id: I6328185a6ce4a474ce1b379146a22484c3b6dba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4468567
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1135871}

--- pdf/test/run_all_unittests.cc
+++ pdf/test/run_all_unittests.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
 #include "third_party/blink/public/web/blink.h"
-#include "third_party/skia/include/core/SkGraphics.h"
 #include "tools/v8_context_snapshot/buildflags.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
@@ -82,8 +81,6 @@ class PdfTestSuite final : public base::TestSuite {
                       platform_->GetMainThreadScheduler());
 
     InitializeResourceBundle();
-
-    SkGraphics::SetPathAnalyticAADecider([](const SkPath&) { return true; });
   }
 
   void Shutdown() override {
