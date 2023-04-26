1862200b5950f90460fb58d4810b506d093194f2
Ho Cheung <uioptt24@gmail.com>
2023-04-26 06:08:48 +0000

    Remove `base/cxx17_backports.h` from the code in storage/
    Remove the reference to `base/cxx17_backports.h` from the code.
    
    Bug: 1373621
    Change-Id: I3bc5c73294a62afda1f64d6fe78403f1bfc93b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4477772
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135747}

--- storage/browser/file_system/file_system_quota_client_unittest.cc
+++ storage/browser/file_system/file_system_quota_client_unittest.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <algorithm>
 #include <memory>
 #include <vector>
 
-#include "base/cxx17_backports.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
