115691f3b2762fe37c4d4d2a5293eb624febae87
Vignesh Shenvi <vshenvi@google.com>
2023-04-26 22:14:40 +0000

    Fix possible dangling references in app metric browser tests
    This change fixes issues involving possible dangling references in app
    metric browser tests due to NRVO.
    
    Bug: b:260803048
    Change-Id: Iec068401326b1cd6b22441fa6c05e6ed8f6ce67c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481332
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136191}

--- chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_telemetry_sampler_browsertest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_telemetry_sampler_browsertest.cc
@@ -322,7 +322,7 @@ IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest, ReportUsageData) {
   MockClock::Get().Advance(
       metrics::kDefaultDeviceActivityHeartbeatCollectionRate);
   const auto [priority, record] = missive_observer.GetNextEnqueuedRecord();
-  const auto& metric_data = AssertAppUsageTelemetryData(priority, record);
+  const auto metric_data = AssertAppUsageTelemetryData(priority, record);
 
   // Data reported includes usage from the web app + the native Chrome
   // component application since these leverage the browser.
@@ -375,7 +375,7 @@ IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
   MockClock::Get().Advance(
       metrics::kDefaultDeviceActivityHeartbeatCollectionRate);
   const auto [priority, record] = missive_observer.GetNextEnqueuedRecord();
-  const auto& metric_data = AssertAppUsageTelemetryData(priority, record);
+  const auto metric_data = AssertAppUsageTelemetryData(priority, record);
 
   // Data reported includes usage from the web app + the native Chrome
   // component application since these leverage the browser.
