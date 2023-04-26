2ab0ba3ff8ee83225504e4e52af1c23e1b1ae95d
Bijan Choobineh <bchoobineh@chromium.org>
2023-04-26 16:23:17 +0000

    media/gpu/v4l2/test: Update H.264 Decoder logging messages
    Previously the H.264 decoder was setup with logging messages which used
    DVLOGF. Since a debug build is not usually used these would not work.
    
    This update replaces the logging calls to DVLOGF to VLOGF with high
    verbosity.
    
    This has no effect on the decoder's decoding ability but instead on the
    ability to see logging on a non debug build.
    
    BUG=b:234752983
    TEST=v4l2_stateless_decoder --video=BA1_Sony_D.h264 --md5=checksums.log
    TEST=v4l2_stateless_decoder --video=CVSE2_Sony_B.h264 --md5=checksums.log
    
    Change-Id: I10c693931980bbfc1f63a3c780747a48492a1443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475676
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135960}

--- media/gpu/v4l2/test/h264_decoder.cc
+++ media/gpu/v4l2/test/h264_decoder.cc
@@ -49,7 +49,7 @@ struct H264PicOrderCompare {
 bool ParseBitDepth(const H264SPS& sps, uint8_t& bit_depth) {
   // Spec 7.4.2.1.1
   if (sps.bit_depth_luma_minus8 != sps.bit_depth_chroma_minus8) {
-    DVLOG(1) << "H264Decoder doesn't support different bit depths between luma"
+    VLOGF(4) << "H264Decoder doesn't support different bit depths between luma"
              << "and chroma, bit_depth_luma_minus8="
              << sps.bit_depth_luma_minus8
              << ", bit_depth_chroma_minus8=" << sps.bit_depth_chroma_minus8;
@@ -71,7 +71,7 @@ bool ParseBitDepth(const H264SPS& sps, uint8_t& bit_depth) {
       bit_depth = 14u;
       break;
     default:
-      DVLOG(1) << "Invalid bit depth: "
+      VLOGF(4) << "Invalid bit depth: "
                << base::checked_cast<int>(sps.bit_depth_luma_minus8 + 8);
       return false;
   }
@@ -572,7 +572,7 @@ VideoDecoder::Result H264Decoder::InitializeSliceMetadata(
       break;
     }
     default: {
-      DVLOG(1) << "Invalid pic_order_cnt_type: " << sps->pic_order_cnt_type;
+      VLOGF(4) << "Invalid pic_order_cnt_type: " << sps->pic_order_cnt_type;
       return VideoDecoder::kError;
     }
   }
@@ -735,7 +735,7 @@ void H264Decoder::FinishPicture(H264SliceMetadata picture, const int sps_id) {
 
   for (const auto reusable_buffer_slot : reusable_buffer_slots) {
     if (!v4l2_ioctl_->QBuf(CAPTURE_queue_, reusable_buffer_slot)) {
-      VLOG(1) << "VIDIOC_QBUF failed for CAPTURE queue.";
+      VLOGF(4) << "VIDIOC_QBUF failed for CAPTURE queue.";
     }
     // Keeps track of which indices are currently queued in the
     // CAPTURE queue. This will be used to determine which indices
