9d424d137586f92dbabaf048337d0daaa589285a
Ho Cheung <uioptt24@gmail.com>
2023-04-26 07:56:32 +0000

    Remove `base/cxx17_backports.h` from the code in gpu/
    Remove the reference to `base/cxx17_backports.h` from the code.
    
    Bug: 1373621
    Change-Id: I0bcda359d874b360c60bcf89e6ced653cfba9557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4477499
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135775}

--- gpu/command_buffer/common/raster_cmd_format_test.cc
+++ gpu/command_buffer/common/raster_cmd_format_test.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <algorithm>
 #include <limits>
 
-#include "base/cxx17_backports.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/synchronization/waitable_event.h"
