4f487b0d197f5a4b63f3061ff44f4f9dab83e079
Bastian Kersting <bkersting@google.com>
2023-04-26 14:06:50 +0000

    [Telemetry] Create folder for diagnostics in ash
    This change creates a folder for diagnostics in
    //chrome/browser/ash/telemetry_extension which contains all code
    related to the DiagnosticsService.
    
    Bug: b/279409934
    Change-Id: Ic9ae2c3e7fc485fc0e8c3a64a7d1242794894dab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4466308
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135879}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -103,7 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/remote_apps/remote_apps_manager_factory.h"
 #include "chrome/browser/ash/sync/sync_mojo_service_ash.h"
 #include "chrome/browser/ash/sync/sync_mojo_service_factory_ash.h"
-#include "chrome/browser/ash/telemetry_extension/diagnostics_service_ash.h"
+#include "chrome/browser/ash/telemetry_extension/diagnostics/diagnostics_service_ash.h"
 #include "chrome/browser/ash/telemetry_extension/probe_service_ash.h"
 #include "chrome/browser/ash/telemetry_extension/telemetry_event_service_ash.h"
 #include "chrome/browser/ash/video_conference/video_conference_manager_ash.h"
