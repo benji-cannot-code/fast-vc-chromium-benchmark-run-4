ff2d86a2e8e1e45c8778dcc3875d766795b3f7ee
Peter Marshall <petermarshall@chromium.org>
2023-04-26 07:39:54 +0000

    Reland "[Office upload] Use BrowserListObserver to wait for created Files app"
    This is a reland of commit 9089c92681121f77bda5c622d902dd94cc0aa80d
    
    This is a clean reland. The failure was caused by a flaky test,
    fixed in crrev.com/c/4463978, crrev.com/c/4478591
    
    Original change's description:
    > [Office upload] Use BrowserListObserver to wait for created Files app
    >
    > When we didn't have a Files app window to use as a modal parent for
    > our dialog, we created a new Files app window and used it as the
    > parent. But this didn't work because the callback we provide to
    > ShowItemInFolder gets called early in the launch process, before
    > the actual Files app is launched.
    >
    > Instead use BrowserListObserver to catch when a new Browser is
    > added, check that it's the Files app window we were expecting,
    > and launch our dialog with this window as the modal parent.
    >
    > Several tests didn't use "real" files, they just created
    > FileSystemURLs to plausible locations. This meant that
    > ShowItemInFolder() failed to launch Files app in tests, which
    > now causes CloudOpenTask to never make progress. Use the default
    > browser() in several tests to avoid this problem.
    >
    > Update NewModalParentCreatedWithExisting to more directly
    > capture and check the Browser that was created by CloudOpenTask.
    >
    > Bug: b:275950760
    > Change-Id: I4f3b9f1891e0b32a2c426333eebcd5c742359833
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431739
    > Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    > Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1133586}
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: b:275950760
    Change-Id: I00ad795ce62ad7e21b37507600ea42ae7b98ae61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4464338
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135772}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog.h
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_system_provider/mount_path_util.h"
 #include "chrome/browser/ash/file_system_provider/provided_file_system_interface.h"
 #include "chrome/browser/platform_util.h"
+#include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/webui/ash/cloud_upload/cloud_upload.mojom.h"
 #include "chrome/browser/ui/webui/ash/system_web_dialog_delegate.h"
 #include "storage/browser/file_system/file_system_url.h"
@@ -69,7 +70,8 @@ class CloudUploadDialog;
 // The business logic for running setup, moving files to a cloud provider, and
 // opening files on cloud providers. Spawns instances of `CloudUploadDialog` if
 // necessary to run setup or get confirmation from the user.
-class CloudOpenTask : public base::RefCounted<CloudOpenTask> {
+class CloudOpenTask : public BrowserListObserver,
+                      public base::RefCounted<CloudOpenTask> {
  public:
   CloudOpenTask(const CloudOpenTask&) = delete;
   CloudOpenTask& operator=(const CloudOpenTask&) = delete;
@@ -92,6 +94,12 @@ class CloudOpenTask : public base::RefCounted<CloudOpenTask> {
   // logic.
   void SetTasksForTest(
       const std::vector<::file_manager::file_tasks::TaskDescriptor>& tasks);
+
+  // BrowserListObserver implementation.
+  // This is called when we are waiting for a new Files app window to be
+  // launched, to be used as the modal parent. This is triggered by
+  // ShowDialog().
+  void OnBrowserAdded(Browser* browser) override;
 
   FRIEND_TEST_ALL_PREFIXES(FileHandlerDialogBrowserTest,
                            OnDialogCompleteOpensFileTasks);
@@ -126,7 +134,7 @@ class CloudOpenTask : public base::RefCounted<CloudOpenTask> {
                 const CloudProvider cloud_provider,
                 gfx::NativeWindow modal_parent);
 
-  ~CloudOpenTask();
+  ~CloudOpenTask() override;
 
   // See the .cc implementation for comments on private methods.
   bool ExecuteInternal();
@@ -154,8 +162,6 @@ class CloudOpenTask : public base::RefCounted<CloudOpenTask> {
   void SetTaskArgs(mojom::DialogArgsPtr& args,
                    std::unique_ptr<::file_manager::file_tasks::ResultingTasks>
                        resulting_tasks);
-  void FilesAppWindowCreated(CloudUploadDialog* dialog,
-                             platform_util::OpenOperationResult result);
 
   void OnDialogComplete(const std::string& user_response);
   void LaunchLocalFileTask(const std::string& string_task_position);
@@ -182,6 +188,7 @@ class CloudOpenTask : public base::RefCounted<CloudOpenTask> {
   gfx::NativeWindow modal_parent_;
   std::vector<::file_manager::file_tasks::TaskDescriptor> local_tasks_;
   size_t pending_uploads_ = 0;
+  CloudUploadDialog* pending_dialog_ = nullptr;
 };
 
 // Return True if feature `kUploadOfficeToCloud` is enabled and is eligible for
@@ -240,8 +247,6 @@ class CloudUploadDialog : public SystemWebDialogDelegate {
   void OnDialogShown(content::WebUI* webui) override;
   void OnDialogClosed(const std::string& json_retval) override;
 
-  void set_modal_type(ui::ModalType modal_type) { modal_type_ = modal_type; }
-
  protected:
   ~CloudUploadDialog() override;
   ui::ModalType GetDialogModalType() const override;
@@ -261,9 +266,6 @@ class CloudUploadDialog : public SystemWebDialogDelegate {
   mojom::DialogPage dialog_page_;
   size_t num_local_tasks_;
   bool office_move_confirmation_shown_;
-  // Modal by default, although if we don't have a parent window, we will
-  // make it non-modal so that the dialog stays on top.
-  ui::ModalType modal_type_ = ui::MODAL_TYPE_WINDOW;
 };
 
 }  // namespace ash::cloud_upload
