10ef66095e1c482997c72591b5baea485cafab65
dpapad <dpapad@chromium.org>
2023-04-26 21:55:24 +0000

    iOS: Use JS modules in chrome://omaha WebUI.
    This is part of an effort to remove the last few usages of the
    util_deprecated.js on iOS.
    
    Bug: None
    Change-Id: I3f43f7c7ec2a1aa46c2cd8d2e89c62b7d8831bac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4468548
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136177}

--- ios/chrome/app/resources/omaha/omaha.js
+++ ios/chrome/app/resources/omaha/omaha.js
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Requests the debug information from the backend.
- */
-function requestOmahaDebugInformation() {
-  chrome.send('requestOmahaDebugInformation');
-}
+import 'chrome://resources/js/ios/web_ui.js';
+
+import {sendWithPromise} from 'chrome://resources/js/cr.js';
+import {$} from 'chrome://resources/js/util_ts.js';
+
 
 /**
  * Update the visibility state of the given element. Using the hidden attribute
@@ -21,14 +20,17 @@ function setVisible(element, visible) {
 }
 
 /**
- * Callback from backend with the debug informations. Construct the UI.
+ * Callback with the debug information. Construct the UI.
  * @param {Object} information The debug information.
  */
 function updateOmahaDebugInformation(information) {
-  for (key in information) {
+  for (const key in information) {
     $(key).textContent = information[key];
     setVisible($(key + '-tr'), true);
   }
 }
 
-document.addEventListener('DOMContentLoaded', requestOmahaDebugInformation);
+document.addEventListener('DOMContentLoaded', async () => {
+  const information = await sendWithPromise('requestOmahaDebugInformation');
+  updateOmahaDebugInformation(information);
+});
