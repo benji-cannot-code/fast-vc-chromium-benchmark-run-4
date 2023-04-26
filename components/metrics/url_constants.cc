5bc6974287e0a2dad3b50db15db0cefff7f14c37
Luc Nguyen <lucnguyen@google.com>
2023-04-26 22:49:02 +0000

    Remove metrics URLs from non-Chrome branded builds
    This also makes the following cmd line flags available for all builds:
      - uma-server-url
      - uma-insecure-server-url
      - ukm-server-url
    Reason being is that now, Chromium builds won't include the metrics
    URLs anymore, and if someone wants to test the uploading of logs
    for some reason on a Chromium build, they should be able to do so.
    But FYI those logs won't make it through our pipelines since they
    won't be signed properly.
    
    FYI, components/metrics/internal/url_constants.cc is pulled from here:
    http://shortn/_4lbmIEgNNQ
    
    Bug: 1428677
    Change-Id: I4541f44538a22eca76aba53bb367542a5553d59f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4469667
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136215}

--- components/metrics/url_constants.cc
+++ components/metrics/url_constants.cc
@@ -7,18 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace metrics {
 
-const char kNewMetricsServerUrl[] =
-    "https://clientservices.googleapis.com/uma/v2";
-
-const char kNewMetricsServerUrlInsecure[] =
-    "http://clientservices.googleapis.com/uma/v2";
-
-const char kOldMetricsServerUrl[] = "https://clients4.google.com/uma/v2";
-
-const char kDefaultMetricsMimeType[] = "application/vnd.chrome.uma";
-
-const char kDefaultUkmServerUrl[] = "https://clients4.google.com/ukm";
-
-const char kUkmMimeType[] = "application/vnd.chrome.ukm";
+// Chrome metrics URLs are stored internally to prevent Chromium forks from
+// accidentally sending metrics to Google servers. The URLs can be found here:
+// https://chrome-internal.googlesource.com/chrome/components/metrics/internal/
+const char kNewMetricsServerUrl[] = "";
+const char kNewMetricsServerUrlInsecure[] = "";
+const char kOldMetricsServerUrl[] = "";
+const char kDefaultMetricsMimeType[] = "";
+const char kDefaultUkmServerUrl[] = "";
+const char kUkmMimeType[] = "";
 
 } // namespace metrics
