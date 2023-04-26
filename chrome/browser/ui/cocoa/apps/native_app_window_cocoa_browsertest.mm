8763adf96bab5ac050b3d9ce0a19dc4a621dafa6
Jayson Adams <shrike@chromium.org>
2023-04-26 18:11:59 +0000

    [Mac Ventura Tests] Fix NativeAppWindowCocoaBrowserTest.Minimize*
    Window minimization timing has changed with macOS 13, such that
    -isMiniaturized returns NO immediately after calling -miniaturize:.
    This CL modifies the tests to include the proper state change waits.
    
    Test: out/Default/browser_tests \
        --gtest_filter=NativeAppWindowCocoaBrowserTest.Minimize*
    
    Bug: 1439879
    Change-Id: I0a59fb9ce3171f0659ec9025c1ffaa4aca89f6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475747
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136046}

--- chrome/browser/ui/cocoa/apps/native_app_window_cocoa_browsertest.mm
+++ chrome/browser/ui/cocoa/apps/native_app_window_cocoa_browsertest.mm
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/test/scoped_fake_nswindow_focus.h"
 #include "ui/base/test/scoped_fake_nswindow_fullscreen.h"
 #import "ui/base/test/windowed_nsnotification_observer.h"
+#include "ui/views/widget/widget_interactive_uitest_utils.h"
 
 using extensions::AppWindow;
 using extensions::PlatformAppBrowserTest;
@@ -244,23 +245,44 @@ IN_PROC_BROWSER_TEST_F(NativeAppWindowCocoaBrowserTest, Minimize) {
   EXPECT_FALSE([ns_window isMiniaturized]);
 
   // Native minimize, Restore.
+  base::scoped_nsobject<WindowedNSNotificationObserver> miniaturizationObserver(
+      [[WindowedNSNotificationObserver alloc]
+          initForNotification:NSWindowDidMiniaturizeNotification
+                       object:ns_window]);
   [ns_window miniaturize:nil];
+  [miniaturizationObserver wait];
   EXPECT_NSEQ(initial_frame, [ns_window frame]);
   EXPECT_TRUE(window->IsMinimized());
   EXPECT_TRUE([ns_window isMiniaturized]);
 
+  views::test::PropertyWaiter deminimize_waiter(
+      base::BindRepeating(&extensions::NativeAppWindow::IsMinimized,
+                          base::Unretained(window)),
+      false);
   app_window->Restore();
+  EXPECT_TRUE(deminimize_waiter.Wait());
+
   EXPECT_NSEQ(initial_frame, [ns_window frame]);
   EXPECT_FALSE(window->IsMinimized());
   EXPECT_FALSE([ns_window isMiniaturized]);
 
   // Minimize, native restore.
+  views::test::PropertyWaiter minimize_waiter(
+      base::BindRepeating(&extensions::NativeAppWindow::IsMinimized,
+                          base::Unretained(window)),
+      true);
   app_window->Minimize();
+  EXPECT_TRUE(minimize_waiter.Wait());
   EXPECT_NSEQ(initial_frame, [ns_window frame]);
   EXPECT_TRUE(window->IsMinimized());
   EXPECT_TRUE([ns_window isMiniaturized]);
 
+  base::scoped_nsobject<WindowedNSNotificationObserver>
+      deminiaturizationObserver([[WindowedNSNotificationObserver alloc]
+          initForNotification:NSWindowDidDeminiaturizeNotification
+                       object:ns_window]);
   [ns_window deminiaturize:nil];
+  [deminiaturizationObserver wait];
   EXPECT_NSEQ(initial_frame, [ns_window frame]);
   EXPECT_FALSE(window->IsMinimized());
   EXPECT_FALSE([ns_window isMiniaturized]);
@@ -380,25 +402,49 @@ IN_PROC_BROWSER_TEST_F(NativeAppWindowCocoaBrowserTest, MinimizeMaximize) {
   EXPECT_NSEQ(maximized_frame, [ns_window frame]);
   EXPECT_TRUE(window->IsMaximized());
 
-  app_window->Minimize();
+  {
+    views::test::PropertyWaiter minimize_waiter(
+        base::BindRepeating(&extensions::NativeAppWindow::IsMinimized,
+                            base::Unretained(window)),
+        true);
+    app_window->Minimize();
+    EXPECT_TRUE(minimize_waiter.Wait());
+  }
   EXPECT_NSEQ(maximized_frame, [ns_window frame]);
   EXPECT_FALSE(window->IsMaximized());
   EXPECT_TRUE(window->IsMinimized());
   EXPECT_TRUE([ns_window isMiniaturized]);
 
+  views::test::PropertyWaiter deminimize_waiter(
+      base::BindRepeating(&extensions::NativeAppWindow::IsMinimized,
+                          base::Unretained(window)),
+      false);
   app_window->Restore();
+  EXPECT_TRUE(deminimize_waiter.Wait());
   EXPECT_NSEQ(initial_frame, [ns_window frame]);
   EXPECT_FALSE(window->IsMaximized());
   EXPECT_FALSE(window->IsMinimized());
   EXPECT_FALSE([ns_window isMiniaturized]);
 
   // Minimize, Maximize.
-  app_window->Minimize();
+  {
+    views::test::PropertyWaiter minimize_waiter(
+        base::BindRepeating(&extensions::NativeAppWindow::IsMinimized,
+                            base::Unretained(window)),
+        true);
+    app_window->Minimize();
+    EXPECT_TRUE(minimize_waiter.Wait());
+  }
   EXPECT_NSEQ(initial_frame, [ns_window frame]);
   EXPECT_TRUE(window->IsMinimized());
   EXPECT_TRUE([ns_window isMiniaturized]);
 
+  base::scoped_nsobject<WindowedNSNotificationObserver>
+      deminiaturizationObserver([[WindowedNSNotificationObserver alloc]
+          initForNotification:NSWindowDidDeminiaturizeNotification
+                       object:ns_window]);
   app_window->Maximize();
+  [deminiaturizationObserver wait];
   EXPECT_TRUE([ns_window isVisible]);
   EXPECT_NSEQ(maximized_frame, [ns_window frame]);
   EXPECT_TRUE(window->IsMaximized());
