3a6a75097572f8a30bd0997474abe7b429388c2a
sisidovski <sisidovski@chromium.org>
2023-04-26 23:24:35 +0000

    [BestEffortServiceWorker] Enable the feature via hash based allowlist
    This CL adds the allowlist support to RaceNetworkRequest. If the sha256
    hash in ServiceWorkerVersion matches the list, the feature is enabled.
    This mechanism will be used upcoming a/b experiment.
    
    `FetchHandlerBypassedHashStrings` in
    service_worker_controllee_request_handler.cc is moved to
    `service_worker_loader_helpers` to avoid the code duplication between
    ServiceWorkerControlleeRequestHandler and
    ServiceWorkerMainResourceLoader.
    
    Bug: 1420517
    Change-Id: I6aecfda4d8740aad4c0b534ba272b2c2fad58840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4479069
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136229}

--- content/browser/service_worker/service_worker_controllee_request_handler.cc
+++ content/browser/service_worker/service_worker_controllee_request_handler.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_container_host.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
+#include "content/browser/service_worker/service_worker_loader_helpers.h"
 #include "content/browser/service_worker/service_worker_main_resource_loader.h"
 #include "content/browser/service_worker/service_worker_metrics.h"
 #include "content/browser/service_worker/service_worker_registration.h"
@@ -87,16 +88,6 @@ const char* FetchHandlerTypeToString(
   }
 }
 
-// Returns the set of hash strings of fetch handlers which can be bypassed.
-const base::flat_set<std::string> FetchHandlerBypassedHashStrings() {
-  const static base::NoDestructor<base::flat_set<std::string>> result(
-      base::SplitString(
-          features::kServiceWorkerBypassFetchHandlerBypassedHashStrings.Get(),
-          ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY));
-
-  return *result;
-}
-
 bool ShouldBypassFetchHandlerForMainResource(ServiceWorkerVersion& version) {
   if (!base::FeatureList::IsEnabled(
           features::kServiceWorkerBypassFetchHandler)) {
@@ -123,8 +114,9 @@ bool ShouldBypassFetchHandlerForMainResource(ServiceWorkerVersion& version) {
     // resource fetch handlers are bypassed only when the sha256 checksum of the
     // script is in the allowlist.
     case features::ServiceWorkerBypassFetchHandlerStrategy::kAllowList:
-      if (FetchHandlerBypassedHashStrings().contains(
-              version.sha256_script_checksum())) {
+      if (content::service_worker_loader_helpers::
+              FetchHandlerBypassedHashStrings()
+                  .contains(version.sha256_script_checksum())) {
         version.CountFeature(
             blink::mojom::WebFeature::
                 kServiceWorkerBypassFetchHandlerForMainResource);
--- content/browser/service_worker/service_worker_loader_helpers.cc
+++ content/browser/service_worker/service_worker_loader_helpers.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_loader_helpers.h"
 
 #include "base/command_line.h"
+#include "base/no_destructor.h"
+#include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "components/network_session_configurator/common/network_switches.h"
@@ -14,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/referrer.h"
 #include "services/network/public/cpp/constants.h"
 #include "third_party/blink/public/common/features.h"
@@ -322,6 +325,15 @@ bool IsPathRestrictionSatisfiedWithoutHeader(const GURL& scope,
                                              std::string* error_message) {
   return IsPathRestrictionSatisfiedInternal(scope, script_url, false, nullptr,
                                             error_message);
+}
+
+const base::flat_set<std::string> FetchHandlerBypassedHashStrings() {
+  const static base::NoDestructor<base::flat_set<std::string>> result(
+      base::SplitString(
+          features::kServiceWorkerBypassFetchHandlerBypassedHashStrings.Get(),
+          ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY));
+
+  return *result;
 }
 
 }  // namespace service_worker_loader_helpers
