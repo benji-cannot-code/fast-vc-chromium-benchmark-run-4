7eb66c73a9f145ad672cc2afa11954bf49891625
Claudio DeSouza <cdesouza@igalia.com>
2023-04-26 17:02:02 +0000

    Replace base::GUID uses with base::Uuid (120/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/ash/arc/bluetooth
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: I0ac46aa55d08294c78116ace5a227cca424ade6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454717
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135985}

--- chrome/browser/ash/arc/bluetooth/arc_floss_bridge.cc
+++ chrome/browser/ash/arc/bluetooth/arc_floss_bridge.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/bluetooth/bluetooth_type_converters.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/ash/arc/bluetooth/arc_floss_bridge.h"
