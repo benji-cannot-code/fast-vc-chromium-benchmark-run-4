805566d6c98917f9be00f648cfb0e4a5d29b46b8
Ovidio Ruiz-Henríquez <odejesush@chromium.org>
2023-04-27 14:14:59 +0000

    controlledframe: Add WPT for API outside of IWA
    This change adds a WPT for the Controlled Frame API that verifies that
    the API and related types are not defined outside of the Isolated Web App context.
    
    Bug: 1233993, b/264596740
    Change-Id: I468e469e9b03de9c95b96e61d8431849523e1cfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450887
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136495}

---
+++ third_party/blink/web_tests/wpt_internal/controlled-frame/controlledframe-undefined.https.window.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+
+const test_desc =
+    'ControlledFrame and related types are not defined outside of an ' +
+    'Isolated Web App';
+
+test(() => {
+  assert_true(typeof ControlledFrame === 'undefined');
+  // 'WebView' is not defined when 'ControlledFrame' is defined, but 'WebView'
+  // is defined for some non-web contexts. We test it here out of caution to
+  // ensure the element isn't accidentally exposed.
+  assert_true(typeof WebView == 'undefined');
+}, test_desc);
