35dcd39b1d786fdb4a0ee250d4827c0e34d08404
Liza Bipin <mlbipin@google.com>
2023-04-27 13:53:30 +0000

    Unify supervisedUser and childUser in Sync status
    Bug: b/278842413
    Change-Id: I398e7a7e25d1071e375ac5eca8cb948963c5fe57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4479414
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136485}

--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_availability_mixin.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_availability_mixin.ts
@@ -50,7 +50,7 @@ export const PrivacyGuideAvailabilityMixin = dedupingMixin(
               'sync-status-changed',
               (syncStatus: SyncStatus) =>
                   this.onPrivacyGuideAvailabilityChanged_(
-                      !syncStatus.childUser));
+                      !syncStatus.supervisedUser));
         }
 
         private onPrivacyGuideAvailabilityChanged_(isAvailable: boolean) {
