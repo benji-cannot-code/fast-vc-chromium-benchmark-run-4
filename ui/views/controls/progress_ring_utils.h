415feb4bb6cc2c53db8e8013b5281d61e1db530f
Lily Chen <chlily@chromium.org>
2023-04-27 19:17:42 +0000

    Refactor progress_ring_utils to fix graininess
    Previously, we were drawing a full circle with the background color,
    underneath a partial ring with the foreground color. This led to
    overlapping segments of both colors, which looks bad due to the
    partially transparent pixels along the edges being darker than they
    should be.
    
    Refactor this function to only draw arcs that don't overlap.
    
    Screenshots:
    https://drive.google.com/drive/folders/1fG6EzA-0AXa7jgsPjMr2Mk5gNxQ3LzCF
    
    Bug: 1435576
    Change-Id: Ifa9b49ea2b96ee53dbd69360d09a052ad10e5927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480471
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136716}

--- ui/views/controls/progress_ring_utils.h
+++ ui/views/controls/progress_ring_utils.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 
 // Helper function that draws a progress ring on `canvas`. The progress ring
-// consists a background full ring and a partial ring that indicates the
-// progress. `start_angle` and `sweep_angle` are used to indicate the current
-// progress of the ring.
+// consists of a partial ring that indicates the progress, and the rest of the
+// ring is stroked with a background color. `start_angle` and `sweep_angle` are
+// used to indicate the current progress of the ring.
 VIEWS_EXPORT void DrawProgressRing(gfx::Canvas* canvas,
                                    const SkRect& bounds,
                                    SkColor background_color,
@@ -24,8 +24,9 @@ VIEWS_EXPORT void DrawProgressRing(gfx::Canvas* canvas,
                                    SkScalar sweep_angle);
 
 // Helper function that draws a spinning ring on `canvas`. The spinning ring
-// consists a background full ring and three arches distributed evenly on the
-// ring. `start_angle` is used to indicate the start angle of the first arch.
+// consists of three arches distributed evenly on the ring, with spaces in
+// between stroked with a background color. `start_angle` is used to indicate
+// the start angle of the first arch.
 VIEWS_EXPORT void DrawSpinningRing(gfx::Canvas* canvas,
                                    const SkRect& bounds,
                                    SkColor background_color,
