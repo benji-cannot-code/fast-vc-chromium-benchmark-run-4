1c70dc4041b559e17840839bb7718f78f74d52b9
Gauthier Ambard <gambard@chromium.org>
2023-04-27 12:27:31 +0000

    [iOS] Prepare to move BrowserState
    Bug: 1319852
    Change-Id: I5c920acc972141df1278f75d8f56549b0ce1792e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483936
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136438}

---
+++ ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_STATE_CHROME_BROWSER_STATE_MANAGER_H_
+#define IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_STATE_CHROME_BROWSER_STATE_MANAGER_H_
+
+// Temporary include.
+#import "ios/chrome/browser/browser_state/chrome_browser_state_manager.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_STATE_CHROME_BROWSER_STATE_MANAGER_H_
