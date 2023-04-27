8e1a8ea4d6b5c33974d5e160e1d79d77fa2f9ddb
Elliot Tuck <etuck@chromium.org>
2023-04-27 19:44:01 +0000

    Fix cut-off notification list in tablet mode
    Take into account extra bottom insets that get used on the launcher
    homescreen in tablet mode so that the list of notifications does not get
    cut off.
    
    Bug: b:278471988
    Change-Id: Iaa15520d1451544446124b5e948b23d2ae16fc0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481840
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Elliot Tuck <etuck@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136729}

--- ash/system/notification_center/notification_center_bubble.h
+++ ash/system/notification_center/notification_center_bubble.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/ash_export.h"
 #include "ash/system/screen_layout_observer.h"
 #include "base/memory/raw_ptr.h"
 
@@ -24,7 +25,7 @@ class TrayBubbleWrapper;
 
 // Manages the bubble that contains NotificationCenterView.
 // Shows the bubble on `ShowBubble()`, and closes the bubble on the destructor.
-class NotificationCenterBubble : public ScreenLayoutObserver {
+class ASH_EXPORT NotificationCenterBubble : public ScreenLayoutObserver {
  public:
   explicit NotificationCenterBubble(
       NotificationCenterTray* notification_center_tray);
