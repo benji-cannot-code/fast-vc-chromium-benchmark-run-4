410f4017886e313a54d0620135e6f0e8dc902dbf
Emilia Paz <emiliapaz@chromium.org>
2023-04-27 05:11:56 +0000

    [Extensions c2s] Display access in extensions site permissions buttons
    Display the extensions site access in its menu item site permission button. This includes showing site access of enterprise extensions.
    Therefore, now ExtensionSiteAccess and UserSiteAccess also compute the
    site access for enterprise extensions.
    
    Screencast: https://drive.google.com/file/d/1Xhhn6MjgoYzPgOLLTAnJ4eg4bmHAx5Av/view?usp=sharing
    
    Bug: 1390952
    Change-Id: Idfd33713533e3dff43159264f5108c2010db3246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4378164
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136325}

--- extensions/browser/permissions_manager.cc
+++ extensions/browser/permissions_manager.cc
@@ -145,6 +145,12 @@ std::unique_ptr<PermissionSet> GetAllowedPermissionsAfterWithholding(
       URLPatternSet::IntersectionBehavior::kDetailed);
 }
 
+// Returns whether the extension requests host permissions.
+bool ExtensionRequestsHostPermissions(const Extension& extension) {
+  return !PermissionsParser::GetRequiredPermissions(&extension).IsEmpty() ||
+         !PermissionsParser::GetOptionalPermissions(&extension).IsEmpty();
+}
+
 class PermissionsManagerFactory : public BrowserContextKeyedServiceFactory {
  public:
   PermissionsManagerFactory();
@@ -355,12 +361,10 @@ PermissionsManager::UserSiteAccess PermissionsManager::GetUserSiteAccess(
 
   // Extension with no host permissions but with active tab permission has "on
   // click" access.
-  if (!CanAffectExtension(extension) &&
+  if (!ExtensionRequestsHostPermissions(extension) &&
       HasActiveTabAndCanAccess(extension, gurl)) {
     return UserSiteAccess::kOnClick;
   }
-
-  DCHECK(CanAffectExtension(extension));
 
   ExtensionSiteAccess site_access = GetSiteAccess(extension, gurl);
   if (site_access.has_all_sites_access) {
@@ -377,8 +381,8 @@ PermissionsManager::ExtensionSiteAccess PermissionsManager::GetSiteAccess(
     const GURL& url) const {
   PermissionsManager::ExtensionSiteAccess extension_access;
 
-  // Extension that cannot be affected by host permissions has no access.
-  if (!CanAffectExtension(extension)) {
+  // Extension that doesn't request host permission has no access.
+  if (!ExtensionRequestsHostPermissions(extension)) {
     return extension_access;
   }
 
@@ -452,8 +456,7 @@ bool PermissionsManager::CanAffectExtension(const Extension& extension) const {
 
   // The extension can be affected by runtime host permissions if it requests
   // host permissions.
-  return !PermissionsParser::GetRequiredPermissions(&extension).IsEmpty() ||
-         !PermissionsParser::GetOptionalPermissions(&extension).IsEmpty();
+  return ExtensionRequestsHostPermissions(extension);
 }
 
 bool PermissionsManager::CanUserSelectSiteAccess(
@@ -518,7 +521,6 @@ bool PermissionsManager::HasBroadGrantedHostPermissions(
 
 bool PermissionsManager::HasWithheldHostPermissions(
     const Extension& extension) const {
-  DCHECK(CanAffectExtension(extension));
   return extension_prefs_->GetWithholdingPermissions(extension.id());
 }
 
