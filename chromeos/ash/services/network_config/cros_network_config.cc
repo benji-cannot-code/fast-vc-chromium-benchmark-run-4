b0cd7842fe626b8c2946900ff820d1e7f058a846
Claudio DeSouza <cdesouza@igalia.com>
2023-04-27 21:50:41 +0000

    Replace base::GUID uses with base::Uuid (216/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chromeos/ash/services/network_config
    
    This CL was uploaded by git cl split.
    
    R=jiajunz@google.com
    
    Bug: 1428566
    Change-Id: I170140a40d405762c992eee8233eedefa03e0ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481485
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136815}

--- chromeos/ash/services/network_config/cros_network_config.cc
+++ chromeos/ash/services/network_config/cros_network_config.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/containers/contains.h"
 #include "base/containers/flat_map.h"
-#include "base/guid.h"
 #include "base/i18n/time_formatting.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
