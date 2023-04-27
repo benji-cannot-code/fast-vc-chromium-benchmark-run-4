340e012bfeff20b59b8e1ea534fdc0d29ad734c1
Claudio DeSouza <cdesouza@igalia.com>
2023-04-27 18:02:06 +0000

    Replace base::GUID uses with base::Uuid (234/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /ios/chrome/browser/autofill
    
    This CL was uploaded by git cl split.
    
    R=sebsg@chromium.org
    
    Bug: 1428566
    Change-Id: I1503ad878f0b45d961dfb4929b69455c9861aa87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482448
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136657}

--- ios/chrome/browser/autofill/autofill_controller_unittest.mm
+++ ios/chrome/browser/autofill/autofill_controller_unittest.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#import "base/guid.h"
 #import "base/ios/ios_util.h"
 #import "base/mac/foundation_util.h"
 #import "base/memory/ptr_util.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/task/thread_pool/thread_pool_instance.h"
 #import "base/test/ios/wait_util.h"
 #import "base/test/metrics/histogram_tester.h"
+#import "base/uuid.h"
 #import "components/autofill/core/browser/browser_autofill_manager.h"
 #import "components/autofill/core/browser/form_structure.h"
 #import "components/autofill/core/browser/metrics/autofill_metrics.h"
@@ -507,7 +507,8 @@ void AutofillControllerTest::SetUpForSuggestions(
   PersonalDataManager* personal_data_manager =
       PersonalDataManagerFactory::GetForBrowserState(
           ChromeBrowserState::FromBrowserState(browser_state_.get()));
-  AutofillProfile profile(base::GenerateGUID(), "https://www.example.com/");
+  AutofillProfile profile(base::Uuid::GenerateRandomV4().AsLowercaseString(),
+                          "https://www.example.com/");
   profile.SetRawInfo(NAME_FULL, u"Homer Simpson");
   profile.SetRawInfo(ADDRESS_HOME_LINE1, u"123 Main Street");
   profile.SetRawInfo(ADDRESS_HOME_CITY, u"Springfield");
@@ -578,7 +579,8 @@ TEST_F(AutofillControllerTest, MultipleProfileSuggestions) {
   personal_data_manager->SetSyncServiceForTest(nullptr);
   PersonalDataManagerFinishedProfileTasksWaiter waiter(personal_data_manager);
 
-  AutofillProfile profile(base::GenerateGUID(), "https://www.example.com/");
+  AutofillProfile profile(base::Uuid::GenerateRandomV4().AsLowercaseString(),
+                          "https://www.example.com/");
   profile.SetRawInfo(NAME_FULL, u"Homer Simpson");
   profile.SetRawInfo(ADDRESS_HOME_LINE1, u"123 Main Street");
   profile.SetRawInfo(ADDRESS_HOME_CITY, u"Springfield");
@@ -590,7 +592,8 @@ TEST_F(AutofillControllerTest, MultipleProfileSuggestions) {
   waiter.Wait();
 
   EXPECT_EQ(1U, personal_data_manager->GetProfiles().size());
-  AutofillProfile profile2(base::GenerateGUID(), "https://www.example.com/");
+  AutofillProfile profile2(base::Uuid::GenerateRandomV4().AsLowercaseString(),
+                           "https://www.example.com/");
   profile2.SetRawInfo(NAME_FULL, u"Larry Page");
   profile2.SetRawInfo(ADDRESS_HOME_LINE1, u"1600 Amphitheatre Parkway");
   profile2.SetRawInfo(ADDRESS_HOME_CITY, u"Mountain View");
--- ios/chrome/browser/autofill/automation/automation_app_interface.mm
+++ ios/chrome/browser/autofill/automation/automation_app_interface.mm
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/autofill/automation/automation_app_interface.h"
 
-#import "base/guid.h"
 #import "base/json/json_reader.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
+#import "base/uuid.h"
 #import "base/values.h"
 #import "components/autofill/core/browser/personal_data_manager.h"
 #import "ios/chrome/browser/autofill/personal_data_manager_factory.h"
@@ -84,10 +84,12 @@ NSError* PrepareAutofillProfileWithValues(
         @"Unable to find autofill profile in parsed JSON value.");
   }
 
-  autofill::AutofillProfile profile(base::GenerateGUID(),
-                                    "https://www.example.com/");
-  autofill::CreditCard credit_card(base::GenerateGUID(),
-                                   "https://www.example.com/");
+  autofill::AutofillProfile profile(
+      base::Uuid::GenerateRandomV4().AsLowercaseString(),
+      "https://www.example.com/");
+  autofill::CreditCard credit_card(
+      base::Uuid::GenerateRandomV4().AsLowercaseString(),
+      "https://www.example.com/");
 
   // For each type-value dictionary in the autofill profile list, validate it,
   // then add it to the appropriate profile.
