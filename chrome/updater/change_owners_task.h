eb31062bec4950cb98f5048fb0a43c593be9df77
Joshua Pawlicki <waffles@chromium.org>
2023-04-27 01:21:34 +0000

    Updater: allow cross-user updates on macOS.
    Fixed: 1439829
    Change-Id: I8fd6aa25c8f4b6a59c7cd8a5b2e4c384a6fdc33c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475904
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136272}

---
+++ chrome/updater/change_owners_task.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_UPDATER_CHANGE_OWNERS_TASK_H_
+#define CHROME_UPDATER_CHANGE_OWNERS_TASK_H_
+
+#include "base/functional/callback.h"
+#include "base/memory/scoped_refptr.h"
+#include "chrome/updater/updater_scope.h"
+
+namespace updater {
+
+class PersistedData;
+
+base::OnceCallback<void(base::OnceClosure)> MakeChangeOwnersTask(
+    scoped_refptr<PersistedData> config,
+    UpdaterScope scope);
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_CHANGE_OWNERS_TASK_H_
