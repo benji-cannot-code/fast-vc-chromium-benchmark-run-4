95a866154de670c4e663720c03afedc052982763
Tom Sepez <tsepez@chromium.org>
2023-04-27 17:53:10 +0000

    Replace base::CategoryRegistry::Range with std::span<>.
    It looks like span, walks like span, quacks like span ...
    
    Range() was only ever made from a global static array, so
    the raw_ptr protections didn't matter, and they had dubious
    dangling suppressions as well. The use of first(), however,
    gets us a bounds check "for free".
    
    Bug: 1291138
    Change-Id: I6808e2a1c7c88007c9ddff6f65486b7cb7b9739e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482048
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136645}

--- base/trace_event/trace_category_unittest.cc
+++ base/trace_event/trace_category_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/lazy_instance.h"
 #include "base/synchronization/lock.h"
@@ -48,7 +49,7 @@ class TraceCategoryTest : public testing::Test {
     return is_new_cat;
   }
 
-  static CategoryRegistry::Range GetAllCategories() {
+  static base::span<TraceCategory> GetAllCategories() {
     return CategoryRegistry::GetAllCategories();
   }
 
