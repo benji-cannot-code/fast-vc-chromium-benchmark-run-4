51eb6b37fd6d9bd27fd668ed7c9bb06b925f7c70
Lukasz Anforowicz <lukasza@chromium.org>
2023-04-27 20:38:19 +0000

    Remove `..QRCodeRequest::should_render` and `...QRCodeResponse::data`.
    Before this CL all callers of `GenerateQRCode` would set `should_render`
    to true and would ignore the raw QR pixels returned in the `data` part
    of the response.  Based on this, this CL removes the
    `GenerateQRCodeRequest::should_render` and
    `GenerateQRCodeResponse::data` fields.
    
    (Before and after this CL all callers of `GenerateQRCode` are only
    reading the `bitmap` part of the response.)
    
    Bug: 1431991
    Change-Id: I4786e7b69f8d911a21d27e40a12c55a0113cd7ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4477690
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136770}

--- chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble_unittest.cc
+++ chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble_unittest.cc
@@ -226,7 +226,6 @@ TEST_F(QRCodeGeneratorBubbleUITest, ImageShowsAfterErrorState) {
   auto ok_response = mojom::GenerateQRCodeResponse::New();
   ok_response->error_code = mojom::QRCodeGeneratorError::NONE;
   ok_response->bitmap.allocN32Pixels(16, 16);
-  ok_response->data.resize(16 * 16);
   ok_response->data_size = gfx::Size(16, 16);
 
   {
@@ -263,7 +262,6 @@ TEST_F(QRCodeGeneratorBubbleUITest,
   ok_response->error_code = mojom::QRCodeGeneratorError::NONE;
   ok_response->bitmap.allocN32Pixels(16, 16);
   ok_response->bitmap.eraseColor(SK_ColorRED);
-  ok_response->data.resize(16 * 16);
   ok_response->data_size = gfx::Size(16, 16);
 
   // The UI regenerates the QR code when the user types new text, so synthesize
