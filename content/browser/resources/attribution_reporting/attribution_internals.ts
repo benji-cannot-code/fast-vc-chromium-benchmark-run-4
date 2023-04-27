7d9679cdfa065d3402935f59440c090ab8b1b0e1
Nan Lin <linnan@chromium.org>
2023-04-27 21:37:39 +0000

    Display null reports in internals UI
    The screenshot was attached in the associated crbug. Null reports will
    be shown in aggregatable reports tab as well. A new column "Null
    Report" is added to indicate whether it's a null report.
    
    Bug: 1432558
    Change-Id: I38a7b751e19ed898a3b3d54488213240a1ac8647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480796
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136807}

--- content/browser/resources/attribution_reporting/attribution_internals.ts
+++ content/browser/resources/attribution_reporting/attribution_internals.ts
@@ -518,6 +518,7 @@ class AggregatableAttributionReport extends Report {
   contributions: string;
   attestationToken: string;
   aggregationCoordinator: string;
+  isNullReport: boolean;
 
   constructor(mojo: WebUIReport) {
     super(mojo);
@@ -531,6 +532,7 @@ class AggregatableAttributionReport extends Report {
 
     this.aggregationCoordinator =
         mojo.data.aggregatableAttributionData!.aggregationCoordinator;
+    this.isNullReport = mojo.data.aggregatableAttributionData!.isNullReport;
   }
 }
 
@@ -670,9 +672,8 @@ class EventLevelReportTableModel extends ReportTableModel<EventLevelReport> {
         [
           new ValueColumn<EventLevelReport, bigint>(
               'Report Priority', (e) => e.reportPriority),
-          new ValueColumn<EventLevelReport, string>(
-              'Randomized Report',
-              (e) => e.attributedTruthfully ? 'no' : 'yes'),
+          new ValueColumn<EventLevelReport, boolean>(
+              'Randomized Report', (e) => !e.attributedTruthfully),
         ],
         showDebugReportsContainer,
         sendReportsButton,
@@ -693,7 +694,9 @@ class AggregatableAttributionReportTableModel extends
           new ValueColumn<AggregatableAttributionReport, string>(
               'Attestation Token', (e) => e.attestationToken),
           new ValueColumn<AggregatableAttributionReport, string>(
-            'Aggregation Coordinator', (e) => e.aggregationCoordinator),
+              'Aggregation Coordinator', (e) => e.aggregationCoordinator),
+          new ValueColumn<AggregatableAttributionReport, boolean>(
+              'Null Report', (e) => e.isNullReport),
         ],
         showDebugReportsContainer,
         sendReportsButton,
