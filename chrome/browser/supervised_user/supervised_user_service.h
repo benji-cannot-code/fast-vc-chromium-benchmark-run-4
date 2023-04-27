3574807c7dd01df701cebf16f5719b3b3e9a2b70
anthie@google.com <anthie@google.com>
2023-04-27 15:38:35 +0000

    Remove unused dependencies from Supervised User Service
    This chagne removes dead code found in preparation for
    migrating the source to components. It can be already
    removed.
    
    Bug: b/270535171
    Change-Id: I590dfeabcc1e24eb80a379304a54896fa76e475b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481482
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136544}

--- chrome/browser/supervised_user/supervised_user_service.h
+++ chrome/browser/supervised_user/supervised_user_service.h
@@ -50,10 +50,6 @@ class Extension;
 }
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
-namespace signin {
-class IdentityManager;
-}  // namespace signin
-
 namespace supervised_user {
 class SupervisedUserSettingsService;
 }  // namespace supervised_user
@@ -218,7 +214,6 @@ class SupervisedUserService
   // an instance of this service.
   SupervisedUserService(
       Profile* profile,
-      signin::IdentityManager* identity_manager,
       KidsChromeManagementClient* kids_chrome_management_client,
       PrefService& user_prefs,
       supervised_user::SupervisedUserSettingsService& settings_service,
@@ -322,8 +317,6 @@ class SupervisedUserService
 
   // Owns us via the KeyedService mechanism.
   raw_ptr<Profile> profile_;
-
-  raw_ptr<signin::IdentityManager> identity_manager_;
 
   raw_ptr<KidsChromeManagementClient> kids_chrome_management_client_;
 
--- chrome/browser/supervised_user/supervised_user_service_factory.cc
+++ chrome/browser/supervised_user/supervised_user_service_factory.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_key.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/supervised_user/kids_chrome_management/kids_chrome_management_client_factory.h"
 #include "chrome/browser/supervised_user/supervised_user_browser_utils.h"
 #include "chrome/browser/supervised_user/supervised_user_service.h"
@@ -70,7 +69,7 @@ SupervisedUserServiceFactory* SupervisedUserServiceFactory::GetInstance() {
 // static
 KeyedService* SupervisedUserServiceFactory::BuildInstanceFor(Profile* profile) {
   return new SupervisedUserService(
-      profile, IdentityManagerFactory::GetInstance()->GetForProfile(profile),
+      profile,
       KidsChromeManagementClientFactory::GetInstance()->GetForProfile(profile),
       *profile->GetPrefs(),
       *SupervisedUserSettingsServiceFactory::GetInstance()->GetForKey(
@@ -88,7 +87,6 @@ SupervisedUserServiceFactory::SupervisedUserServiceFactory()
   DependsOn(
       extensions::ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
 #endif
-  DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(KidsChromeManagementClientFactory::GetInstance());
   DependsOn(SyncServiceFactory::GetInstance());
   DependsOn(SupervisedUserSettingsServiceFactory::GetInstance());
