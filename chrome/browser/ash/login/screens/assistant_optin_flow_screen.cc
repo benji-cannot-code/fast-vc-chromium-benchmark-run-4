4a4fb758dd091c01ff856b366b6946d45d88e0d3
Osama Fathy <osamafathy@google.com>
2023-04-27 14:45:54 +0000

    [OOBE] Skip Assistant Setup screen for CHOOBE
    Bug: b:279886931
    Change-Id: I2c9cd3ea0334adaf96e90a0e155854ee772c9ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414723
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136514}

--- chrome/browser/ash/login/screens/assistant_optin_flow_screen.cc
+++ chrome/browser/ash/login/screens/assistant_optin_flow_screen.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/constants/ash_features.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/assistant/assistant_util.h"
 #include "chrome/browser/ash/login/users/chrome_user_manager_util.h"
@@ -52,6 +53,11 @@ AssistantOptInFlowScreen::AssistantOptInFlowScreen(
 AssistantOptInFlowScreen::~AssistantOptInFlowScreen() = default;
 
 bool AssistantOptInFlowScreen::MaybeSkip(WizardContext& context) {
+  if (features::IsOobeSkipAssistantEnabled()) {
+    exit_callback_.Run(Result::NOT_APPLICABLE);
+    return true;
+  }
+
   if (context.skip_post_login_screens_for_tests || !g_libassistant_enabled ||
       chrome_user_manager_util::IsPublicSessionOrEphemeralLogin()) {
     exit_callback_.Run(Result::NOT_APPLICABLE);
