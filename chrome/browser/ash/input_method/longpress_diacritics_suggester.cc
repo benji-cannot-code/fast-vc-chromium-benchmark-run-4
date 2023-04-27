99bc934654251f1d54571f057a4815463f0612f2
John Palmer <jopalmer@chromium.org>
2023-04-27 01:34:54 +0000

    Update nudge with correct string from UX
    BUG: b:278464189
    Change-Id: I7c2e7b69982e1793222223ce30084ae7f3bb5f64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455912
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136279}

--- chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
+++ chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/ash/ime_bridge.h"
 #include "ui/base/ime/ash/text_input_target.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/chromeos/strings/grit/ui_chromeos_strings.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 
 namespace content {
@@ -66,10 +67,8 @@ class DiacriticsNudge : public ash::SystemNudge {
  protected:
   // SystemNudge:
   std::unique_ptr<SystemNudgeLabel> CreateLabelView() const override {
-    // TODO(b/274349603): convert to final copy + internationalise.
     std::u16string label_text =
-        u"Looking for key-repeat? Turn off \"accent mark\" in Keyboard "
-        u"Settings.";
+        l10n_util::GetStringUTF16(IDS_CHROMEOS_DIACRITIC_NUDGE_TEXT);
     // Set the label's text.
     auto label = std::make_unique<SystemNudgeLabel>(label_text, kMinLabelWidth);
     label->set_font_size_delta(2);
