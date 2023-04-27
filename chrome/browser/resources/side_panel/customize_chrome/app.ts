c5f4207ad00d4da03e925d058660c3c0778f73e7
John Lee <johntlee@chromium.org>
2023-04-27 20:27:32 +0000

    CR2023: Update Customize Chrome top-level view
    - Add dependency on shared side panel library for shared styles and
    shared elements
    - Add background color from the color pipeline
    - Migrate headings to <sp-heading> to automatically get CR2023 styles
    and add CSS to make them look the same as before for non-CR2023
    - Add .sp-card to sections to give them the new cards look
    
    http://screen/Jv5cd55mBi5AZkp
    
    Bug: 1430915
    Change-Id: Ic730f544575915b0e264474f6881c1b6dc3801ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420553
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136755}

--- chrome/browser/resources/side_panel/customize_chrome/app.ts
+++ chrome/browser/resources/side_panel/customize_chrome/app.ts
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
+import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_shared_style.css.js';
 import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
 import './appearance.js';
 import './cards.js';
--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/side_panel_customize_chrome_resources.h"
 #include "chrome/grit/side_panel_customize_chrome_resources_map.h"
+#include "chrome/grit/side_panel_shared_resources.h"
+#include "chrome/grit/side_panel_shared_resources_map.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -105,6 +107,8 @@ CustomizeChromeUI::CustomizeChromeUI(content::WebUI* web_ui)
       base::make_span(kSidePanelCustomizeChromeResources,
                       kSidePanelCustomizeChromeResourcesSize),
       IDR_SIDE_PANEL_CUSTOMIZE_CHROME_CUSTOMIZE_CHROME_HTML);
+  source->AddResourcePaths(base::make_span(kSidePanelSharedResources,
+                                           kSidePanelSharedResourcesSize));
 
   content::URLDataSource::Add(profile_,
                               std::make_unique<SanitizedImageSource>(profile_));
