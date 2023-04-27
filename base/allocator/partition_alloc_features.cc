f9caa6544319b94dae0118121295e94856d24ed5
Keishi Hattori <keishi@chromium.org>
2023-04-27 08:09:23 +0000

    [BRP] Enable BRP by default on Linux/Mac/ChromeOS
    Enable BRP by default on Linux/Mac/ChromeOS for the browser process only.
    
    Launch approval:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1181199
    
    Bug: 1181199, 1073933
    Change-Id: I421667de7519c32485d485e3ff7726720a1b0546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4478673
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136369}

--- base/allocator/partition_alloc_features.cc
+++ base/allocator/partition_alloc_features.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_export.h"
 #include "base/feature_list.h"
 #include "build/build_config.h"
+#include "build/chromecast_buildflags.h"
+#include "build/chromeos_buildflags.h"
 
 namespace base {
 namespace features {
@@ -105,8 +107,10 @@ BASE_FEATURE(kPartitionAllocLargeEmptySlotSpanRing,
 
 BASE_FEATURE(kPartitionAllocBackupRefPtr,
              "PartitionAllocBackupRefPtr",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) ||    \
-    BUILDFLAG(ENABLE_BACKUP_REF_PTR_FEATURE_FLAG) || \
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || \
+    BUILDFLAG(IS_CHROMEOS_ASH) ||                                      \
+    (BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CASTOS)) ||                  \
+    BUILDFLAG(ENABLE_BACKUP_REF_PTR_FEATURE_FLAG) ||                   \
     (BUILDFLAG(USE_ASAN_BACKUP_REF_PTR) && BUILDFLAG(IS_LINUX))
              FEATURE_ENABLED_BY_DEFAULT
 #else
