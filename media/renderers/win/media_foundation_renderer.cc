86302d9ca1c01290d58cad56df2f1c81e58a272c
Claudio DeSouza <cdesouza@igalia.com>
2023-04-27 19:42:50 +0000

    Replace base::GUID uses with base::Uuid (241/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /media
    
    This CL was uploaded by git cl split.
    
    R=tmathmeyer@chromium.org
    
    Bug: 1428566
    Change-Id: I78ac2550f4f42e5f28e34124eb690ba6c24ff98e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4484521
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136728}

--- media/renderers/win/media_foundation_renderer.cc
+++ media/renderers/win/media_foundation_renderer.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/numerics/clamped_math.h"
 #include "base/numerics/safe_conversions.h"
