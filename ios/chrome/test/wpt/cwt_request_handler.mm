7d1e82487439d61a5bcb7f1bcccaad4f99395d97
Claudio DeSouza <cdesouza@igalia.com>
2023-04-27 17:34:35 +0000

    Replace base::GUID uses with base::Uuid (238/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /ios/chrome/test/wpt
    
    This CL was uploaded by git cl split.
    
    R=ajuma@chromium.org
    
    Bug: 1428566
    Change-Id: I80e04bf9ea54b3d5deea34fd6733067754f7ec88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483807
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136629}

--- ios/chrome/test/wpt/cwt_request_handler.mm
+++ ios/chrome/test/wpt/cwt_request_handler.mm
@@ -11,11 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/debug/stack_trace.h"
 #import "base/files/file_path.h"
 #import "base/files/file_util.h"
-#import "base/guid.h"
 #import "base/json/json_reader.h"
 #import "base/json/json_writer.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
+#import "base/uuid.h"
 #import "base/values.h"
 #import "components/version_info/version_info.h"
 #import "ios/chrome/test/wpt/cwt_constants.h"
@@ -315,7 +315,7 @@ base::Value CWTRequestHandler::InitializeSession() {
       base::SysNSStringToUTF8([CWTWebDriverAppInterface currentTabID]);
 
   base::Value::Dict result;
-  session_id_ = base::GenerateGUID();
+  session_id_ = base::Uuid::GenerateRandomV4().AsLowercaseString();
   result.Set(kWebDriverSessionIdValueField, session_id_);
 
   base::Value::Dict capabilities;
