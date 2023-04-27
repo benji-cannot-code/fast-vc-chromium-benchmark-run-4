1731a4a5d183ae17e03d23740c6b516c91bec47b
Viktor Semeniuk <vsemeniuk@google.com>
2023-04-27 15:52:52 +0000

    [Password Manager] Hide "Password Manager" item in the menu
    This CL hides "Password Manager" menu item when opening the menu from
    password manager page.
    
    Bug: 1440465
    Change-Id: I784a92e5acdf29e7b808f84d5e1b831a427efc80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481594
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136560}

--- chrome/browser/ui/toolbar/app_menu_model_interactive_uitest.cc
+++ chrome/browser/ui/toolbar/app_menu_model_interactive_uitest.cc
@@ -250,3 +250,12 @@ IN_PROC_BROWSER_TEST_P(PasswordManagerMenuItemInteractiveTest,
                     EnsureNotPresent(AppMenuModel::kPasswordManagerMenuItem));
   }
 }
+
+IN_PROC_BROWSER_TEST_P(PasswordManagerMenuItemInteractiveTest,
+                       NoMenuItemOnPasswordManagerPage) {
+  RunTestSequence(
+      AddInstrumentedTab(kPrimaryTabPageElementId,
+                         GURL("chrome://password-manager/passwords")),
+      PressButton(kAppMenuButtonElementId),
+      EnsureNotPresent(AppMenuModel::kPasswordManagerMenuItem));
+}
