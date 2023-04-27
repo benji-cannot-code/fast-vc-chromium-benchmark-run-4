51c15f4adef4865cf333ae606609102c13c29c23
Claudio DeSouza <cdesouza@igalia.com>
2023-04-27 18:00:52 +0000

    Replace base::GUID uses with base::Uuid (229/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /extensions/renderer
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: I3893f389f48840e1e363722fc7305c541a2e4c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483707
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136655}

--- extensions/renderer/ipc_message_sender.cc
+++ extensions/renderer/ipc_message_sender.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <utility>
 
-#include "base/guid.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/uuid.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/worker_thread.h"
@@ -312,7 +312,7 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     params->worker_thread_id = worker_thread_id;
     params->service_worker_version_id = service_worker_version_id_;
 
-    std::string guid = base::GenerateGUID();
+    std::string guid = base::Uuid::GenerateRandomV4().AsLowercaseString();
     request_id_to_guid_[params->request_id] = guid;
 
     // Keeps the worker alive during extension function call. Balanced in
