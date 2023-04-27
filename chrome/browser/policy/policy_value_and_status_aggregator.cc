619c560f8ffeba8e111baa4101c0e5123bba5ae5
Roland Bock <rbock@google.com>
2023-04-27 09:33:48 +0000

    Remove active directory state uploader and status provider
    Since ChromeAD is fully deprecated, it is safe to remove active
    directory code in ash.
    
    This CL removes
    - ActiveDirectoryDeviceStateUploader
    - UserActiveDirectoryPolicyStatusProvider
    - DeviceActiveDirectoryPolicyStatusProvider
    - ActiveDirectoryMigrationManager
    
    Bug: b/274762979
    Change-Id: I084fd9b11fc1442267f34b2ed2f36c094c23b1a9
    Tests: Existing tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4469547
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1136403}

--- chrome/browser/policy/policy_value_and_status_aggregator.cc
+++ chrome/browser/policy/policy_value_and_status_aggregator.cc
@@ -26,10 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/core/user_cloud_policy_manager_ash.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/browser_process_platform_part_ash.h"
-#include "chrome/browser/policy/status_provider/device_active_directory_policy_status_provider.h"
 #include "chrome/browser/policy/status_provider/device_cloud_policy_status_provider_chromeos.h"
 #include "chrome/browser/policy/status_provider/device_local_account_policy_status_provider.h"
-#include "chrome/browser/policy/status_provider/user_active_directory_policy_status_provider.h"
 #include "chrome/browser/policy/status_provider/user_cloud_policy_status_provider_chromeos.h"
 #include "components/user_manager/user_manager.h"
 #else
@@ -91,8 +89,6 @@ std::unique_ptr<policy::PolicyStatusProvider> GetUserPolicyStatusProvider(
           : nullptr;
   policy::UserCloudPolicyManagerAsh* user_cloud_policy =
       profile->GetUserCloudPolicyManagerAsh();
-  policy::ActiveDirectoryPolicyManager* active_directory_policy =
-      profile->GetActiveDirectoryPolicyManager();
   if (local_account_service) {
     return std::make_unique<DeviceLocalAccountPolicyStatusProvider>(
         user_manager->GetActiveUser()->GetAccountId().GetUserEmail(),
@@ -100,9 +96,6 @@ std::unique_ptr<policy::PolicyStatusProvider> GetUserPolicyStatusProvider(
   } else if (user_cloud_policy) {
     return std::make_unique<UserCloudPolicyStatusProviderChromeOS>(
         user_cloud_policy->core(), profile);
-  } else if (active_directory_policy) {
-    return std::make_unique<UserActiveDirectoryPolicyStatusProvider>(
-        active_directory_policy, profile);
   }
 #else  // BUILDFLAG(IS_CHROMEOS_ASH)
   policy::CloudPolicyManager* cloud_policy_manager =
@@ -135,11 +128,6 @@ std::unique_ptr<policy::PolicyStatusProvider>
 GetChromeOSDevicePolicyStatusProvider(
     Profile* profile,
     policy::BrowserPolicyConnectorAsh* connector) {
-  if (connector->GetDeviceActiveDirectoryPolicyManager()) {
-    return std::make_unique<DeviceActiveDirectoryPolicyStatusProvider>(
-        connector->GetDeviceActiveDirectoryPolicyManager(),
-        connector->GetEnterpriseDomainManager());
-  }
   return std::make_unique<DeviceCloudPolicyStatusProviderChromeOS>(connector);
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
