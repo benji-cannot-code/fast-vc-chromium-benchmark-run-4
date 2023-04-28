2babbf901557e05d1020847fa9411a6b3323eea5
Peilin Wang <peilinwang@google.com>
2023-04-28 21:54:38 +0000

    [EnableIPH] Cleanup reintroduced usages
    This flag was already cleaned up, but related code was reintroduced
    (probably accidentally) in:
    https://chromium-review.googlesource.com/c/chromium/src/+/3866373
    https://chromium-review.googlesource.com/c/chromium/src/+/4195032
    https://chromium-review.googlesource.com/c/chromium/src/+/4116600
    
    Bug: 1440298
    Change-Id: I2fae766881b183aa9b38df4dffdaf790cebe88e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480933
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1137413}

--- chrome/android/junit/src/org/chromium/chrome/browser/desktop_site/DesktopSiteSettingsIPHControllerUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/desktop_site/DesktopSiteSettingsIPHControllerUnitTest.java
@@ -123,7 +123,6 @@ public class DesktopSiteSettingsIPHControllerUnitTest {
         MockitoAnnotations.initMocks(this);
         mJniMocker.mock(WebsitePreferenceBridgeJni.TEST_HOOKS, mWebsitePreferenceBridgeJniMock);
 
-        enableFeatureWithParams(ChromeFeatureList.ENABLE_IPH, null);
         enableFeatureWithParams(ChromeFeatureList.ANDROID_SCROLL_OPTIMIZATIONS, null);
         enableFeatureWithParams(ChromeFeatureList.REQUEST_DESKTOP_SITE_PER_SITE_IPH, null);
 
