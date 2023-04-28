efe2567eef80d0b7c417815952224f19d444256d
Judy Wang <shidi@chromium.org>
2023-04-28 17:06:47 +0000

    Cros_next: Add blur to desk items.
    Add blur to the save desk buttons and desk items.
    
    Text: Manual
    Fixed: b/279520269
    Change-Id: Ia910fb7e011cccdaa1d4dbcf50964b5cef547f8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4478309
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137233}

--- ash/wm/desks/templates/saved_desk_save_desk_button.h
+++ ash/wm/desks/templates/saved_desk_save_desk_button.h
@@ -47,7 +47,6 @@ class ASH_EXPORT SavedDeskSaveDeskButton : public PillButton,
   void OnViewUnhighlighted() override;
 
   // PillButton:
-  void OnThemeChanged() override;
   void OnFocus() override;
   void OnBlur() override;
 
