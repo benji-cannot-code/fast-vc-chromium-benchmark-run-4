c3292da7dd67bd7cadb4688ee8d11b75ee352596
Emilia Paz <emiliapaz@chromium.org>
2023-04-28 17:46:28 +0000

    [Extensions c2s] Add button that opens the chrome webstore
    Adds a button in the extensions menu header that opens the chrome
    webstore.
    
    Screenshot: https://drive.google.com/file/d/1Lwq1-WMD5WS0cc4S2-awQRPmUPJv6XAG/view?usp=sharing
    Screencast: https://drive.google.com/file/d/188_sNbX3jaULYC516ewk9hj0bVzdPxHE/view?usp=sharing
    
    Bug: 1390952
    Change-Id: I158c7cb61ef05ab86e2c77fbee36ddd814c17204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4463237
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137260}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/profiling.h"
 #include "content/public/common/url_constants.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/common/extension_urls.h"
 #include "printing/buildflags/buildflags.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/events/keycodes/keyboard_codes.h"
@@ -822,7 +823,7 @@ bool BrowserCommandController::ExecuteCommandWithDisposition(
     case IDC_EXTENSIONS_SUBMENU_VISIT_CHROME_WEB_STORE:
       CHECK(base::FeatureList::IsEnabled(features::kExtensionsMenuInAppMenu) ||
             features::IsChromeRefresh2023());
-      ShowWebStoreFromAppMenu(browser_);
+      ShowWebStore(browser_, extension_urls::kAppMenuUtmSource);
       break;
     case IDC_PERFORMANCE:
       ShowSettingsSubPage(browser_->GetBrowserForOpeningWebUi(),
