a5d5072313a46503e2ca76e385348f4ff54fc1a0
Kam Kwankajornkiet <kamchonlathorn@google.com>
2023-04-28 05:26:44 +0000

    CCA: Expose time-lapse initial speed value to Tast
    Expose the initial speed value from CCA to Tast, so that the test won't
    be affected when the constant value changes. The initial speed will be
    used to test on video duration.
    
    Bug: b:236800499
    Test: Manually by calling the function from Tast
    Change-Id: I7748cb756cd8c244e13660d24f45a93bf553de2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482481
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136962}

--- ash/webui/camera_app_ui/resources/js/test/cca_test.ts
+++ ash/webui/camera_app_ui/resources/js/test/cca_test.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {assert, assertExists, assertInstanceof} from '../assert.js';
+import {TIME_LAPSE_INITIAL_SPEED} from '../device/mode/video.js';
 import * as dom from '../dom.js';
 import * as localStorage from '../models/local_storage.js';
 import {ChromeHelper} from '../mojo/chrome_helper.js';
@@ -342,6 +343,13 @@ export class CCATest {
     const element = resolveVisibleElement(component, index);
     const {width, height} = element.getBoundingClientRect();
     return new Resolution(Math.round(width), Math.round(height));
+  }
+
+  /**
+   * Returns the initial speed of time-lapse recording.
+   */
+  static getTimeLapseInitialSpeed(): number {
+    return TIME_LAPSE_INITIAL_SPEED;
   }
 
   /**
