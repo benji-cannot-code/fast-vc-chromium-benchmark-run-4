fdf68cd6d1f9ce17b9f214d15166ddf91e98a4d5
Claudio DeSouza <cdesouza@igalia.com>
2023-04-28 19:51:17 +0000

    Replace base::GUID uses with base::Uuid (252/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /ui/webui/examples
    
    This CL was uploaded by git cl split.
    
    R=robliao@chromium.org
    
    Bug: 1428566
    Change-Id: I041e743a336ff77cc3441688ebce6b4ed62d5519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482843
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137332}

--- ui/webui/examples/browser/devtools/devtools_frontend.cc
+++ ui/webui/examples/browser/devtools/devtools_frontend.cc
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 
-#include "base/guid.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/devtools_frontend_host.h"
@@ -134,8 +134,9 @@ class DevToolsFrontend::AgentHostClient
     auto it = extensions_api_.find(origin);
     if (it == extensions_api_.end())
       return;
-    std::string script = base::StringPrintf("%s(\"%s\")", it->second.c_str(),
-                                            base::GenerateGUID().c_str());
+    std::string script = base::StringPrintf(
+        "%s(\"%s\")", it->second.c_str(),
+        base::Uuid::GenerateRandomV4().AsLowercaseString().c_str());
     content::DevToolsFrontendHost::SetupExtensionsAPI(frame, script);
   }
 
