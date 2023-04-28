a9920db0bfc6be9c0f3d22b1efaa8f5433b4c47c
Ahmed Fakhry <afakhry@chromium.org>
2023-04-28 17:20:24 +0000

    capture_audio_mixing: Establish skeleton
    This CL adds a feature flag for the new audio settings,
    and adds a skeleton for testing.
    
    Fixed: b/279774661
    Change-Id: Id570bb210c12467856c9c81abae3e7d8f24234b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4490264
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137239}

---
+++ ash/capture_mode/capture_audio_mixing_unittests.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/constants/ash_features.h"
+#include "ash/test/ash_test_base.h"
+#include "base/test/scoped_feature_list.h"
+
+namespace ash {
+
+class CaptureAudioMixingTest : public AshTestBase {
+ public:
+  CaptureAudioMixingTest()
+      : scoped_feature_list_(features::kCaptureModeAudioMixing) {}
+  CaptureAudioMixingTest(const CaptureAudioMixingTest&) = delete;
+  CaptureAudioMixingTest& operator=(const CaptureAudioMixingTest&) = delete;
+  ~CaptureAudioMixingTest() override = default;
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+TEST_F(CaptureAudioMixingTest, Basic) {
+  EXPECT_TRUE(features::IsCaptureModeAudioMixingEnabled());
+}
+
+}  // namespace ash
--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -313,6 +313,11 @@ BASE_FEATURE(kCameraPrivacySwitchNotifications,
              "CameraPrivacySwitchNotifications",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Controls whether the capture mode advanced audio settings are enabled.
+BASE_FEATURE(kCaptureModeAudioMixing,
+             "CaptureModeAudioMixing",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Controls whether the capture mode demo tools feature is enabled for Capture
 // Mode.
 BASE_FEATURE(kCaptureModeDemoTools,
@@ -2517,6 +2522,10 @@ bool IsBluetoothQualityReportEnabled() {
 
 bool IsCalendarJellyEnabled() {
   return base::FeatureList::IsEnabled(kCalendarJelly);
+}
+
+bool IsCaptureModeAudioMixingEnabled() {
+  return base::FeatureList::IsEnabled(kCaptureModeAudioMixing);
 }
 
 bool IsCaptivePortalErrorPageEnabled() {
