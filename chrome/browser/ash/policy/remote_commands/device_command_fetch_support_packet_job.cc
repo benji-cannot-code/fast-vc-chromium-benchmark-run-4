bb7a3f9ee98a1ff3b930805cb2584f9d8e3f796c
Irem Uguz <iremuguz@google.com>
2023-04-28 12:05:11 +0000

    Add link to File Storage Server documentation about upload metadata
    Remove the TODO and include the link to File Storage Server
    documentation. The documentation contains the details about upload
    metadata that's attached to LogUploadEvents on FETCH_SUPPORT_PACKET
    remote command.
    
    Bug: b:278856041
    Change-Id: I838f438f1997e8beb7359b35ce2b4c7227d6baa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4488192
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137075}

--- chrome/browser/ash/policy/remote_commands/device_command_fetch_support_packet_job.cc
+++ chrome/browser/ash/policy/remote_commands/device_command_fetch_support_packet_job.cc
@@ -56,9 +56,7 @@ constexpr char kRequestedPiiTypesKey[] = "requestedPiiTypes";
 constexpr char kRequesterId[] = "requesterMetadata";
 
 // JSON keys and values used for creating the upload metadata to File Storage
-// Server (go/crosman-file-storage-server).
-// TODO(b/278856041): Add link to File Storage Server documentation about the
-// expected metadata format.
+// Server (go/crosman_fss_action#scotty-upload-agent).
 constexpr char kFileTypeKey[] = "File-Type";
 constexpr char kSupportFileType[] = "support_file";
 constexpr char kCommandIdKey[] = "Command-ID";
@@ -139,6 +137,8 @@ std::string ErrorsToString(const std::set<SupportToolError>& errors) {
 // Returns the upload_parameters string for LogUploadEvent. This will be used as
 // request metadata for the log upload request to the File Storage Server.
 // Contains File-Type, Command-ID and Filename fields.
+// The details of metadata format can be found in
+// go/crosman_fss_action#scotty-upload-agent.
 std::string GetUploadParameters(
     const base::FilePath& filename,
     policy::RemoteCommandJob::UniqueIDType command_id) {
