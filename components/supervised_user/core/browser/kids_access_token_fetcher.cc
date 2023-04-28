bb21b8e0303c8242491511541c14133b8f099b9e
Peter Kasting <pkasting@chromium.org>
2023-04-28 16:58:02 +0000

    Remove unnecessary using directives.
    Bug: none
    Change-Id: I9b5c84a4e078a378c4a8986320344d31dcb486b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4471674
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1137227}

--- components/supervised_user/core/browser/kids_access_token_fetcher.cc
+++ components/supervised_user/core/browser/kids_access_token_fetcher.cc
@@ -21,41 +21,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-using ::base::BindOnce;
-using ::base::expected;
-using ::base::NoDestructor;
-using ::base::unexpected;
-using ::base::Unretained;
-using ::signin::AccessTokenFetcher;
-using ::signin::AccessTokenInfo;
-using ::signin::ConsentLevel;
-using ::signin::IdentityManager;
-using ::signin::PrimaryAccountAccessTokenFetcher;
-
-expected<AccessTokenInfo, GoogleServiceAuthError> ToSingleReturnValue(
-    GoogleServiceAuthError error,
-    AccessTokenInfo access_token_info) {
+base::expected<signin::AccessTokenInfo, GoogleServiceAuthError>
+ToSingleReturnValue(GoogleServiceAuthError error,
+                    signin::AccessTokenInfo access_token_info) {
   if (error.state() == GoogleServiceAuthError::NONE) {
     return access_token_info;
   }
-  return unexpected(error);
+  return base::unexpected(error);
 }
 
 }  // namespace
 
 KidsAccessTokenFetcher::KidsAccessTokenFetcher(
-    IdentityManager& identity_manager,
+    signin::IdentityManager& identity_manager,
     Consumer consumer)
     : consumer_(std::move(consumer)) {
   // base::Unretained(.) is safe, because no extra on-destroyed semantics are
   // needed and this instance must outlive the callback execution.
   primary_account_access_token_fetcher_ =
-      std::make_unique<PrimaryAccountAccessTokenFetcher>(
+      std::make_unique<signin::PrimaryAccountAccessTokenFetcher>(
           "family_info_fetcher", &identity_manager, Scopes(),
-          BindOnce(&KidsAccessTokenFetcher::OnAccessTokenFetchComplete,
-                   Unretained(this)),
-          PrimaryAccountAccessTokenFetcher::Mode::kWaitUntilAvailable,
-          ConsentLevel::kSignin);
+          base::BindOnce(&KidsAccessTokenFetcher::OnAccessTokenFetchComplete,
+                         base::Unretained(this)),
+          signin::PrimaryAccountAccessTokenFetcher::Mode::kWaitUntilAvailable,
+          signin::ConsentLevel::kSignin);
 }
 KidsAccessTokenFetcher::~KidsAccessTokenFetcher() = default;
 
@@ -66,7 +55,7 @@ void KidsAccessTokenFetcher::OnAccessTokenFetchComplete(
 }
 
 const OAuth2AccessTokenManager::ScopeSet& KidsAccessTokenFetcher::Scopes() {
-  static auto nonce = NoDestructor<OAuth2AccessTokenManager::ScopeSet>{
+  static auto nonce = base::NoDestructor<OAuth2AccessTokenManager::ScopeSet>{
       {GaiaConstants::kKidFamilyReadonlyOAuth2Scope}};
   return *nonce;
 }
