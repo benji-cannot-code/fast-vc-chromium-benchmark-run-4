ee5fa3898b51f4fe3ed4949ae47dd399fed955a3
Kenichi Ishibashi <bashi@chromium.org>
2023-04-28 08:35:38 +0000

    Disable process reuse in ChromeRenderProcessHostBackgroundingTestWithAudio
    The test used switches::kProcessPerTab. It is currently no-op so it
    doesn't actually prevent tabs from sharing processes. Use the
    DisableProcessReuse feature flag to disable process sharing.
    
    This is a preparation to start experiment of process-per-site with
    main frame threshold.
    
    Bug: 1434900
    Change-Id: I2b8592c92d3fde0c2526b6712ffe2acaae4d665c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482342
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137022}

--- chrome/browser/renderer_host/render_process_host_chrome_browsertest.cc
+++ chrome/browser/renderer_host/render_process_host_chrome_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/process/process.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/test_timeouts.h"
 #include "build/build_config.h"
 #include "chrome/browser/devtools/devtools_window.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host_iterator.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -640,7 +642,10 @@ IN_PROC_BROWSER_TEST_F(ChromeRenderProcessHostTest,
 class ChromeRenderProcessHostBackgroundingTestWithAudio
     : public ChromeRenderProcessHostTest {
  public:
-  ChromeRenderProcessHostBackgroundingTestWithAudio() {}
+  ChromeRenderProcessHostBackgroundingTestWithAudio() {
+    // Tests require that each tab has a different process.
+    feature_list_.InitAndEnableFeature(features::kDisableProcessReuse);
+  }
 
   ChromeRenderProcessHostBackgroundingTestWithAudio(
       const ChromeRenderProcessHostBackgroundingTestWithAudio&) = delete;
@@ -649,7 +654,6 @@ class ChromeRenderProcessHostBackgroundingTestWithAudio
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     ChromeRenderProcessHostTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(switches::kProcessPerTab);
 
     command_line->AppendSwitchASCII(
         switches::kAutoplayPolicy,
@@ -721,6 +725,8 @@ class ChromeRenderProcessHostBackgroundingTestWithAudio
     return process.IsProcessBackgrounded();
 #endif
   }
+
+  base::test::ScopedFeatureList feature_list_;
 
 #if BUILDFLAG(IS_MAC)
   raw_ptr<base::PortProvider> port_provider_;
