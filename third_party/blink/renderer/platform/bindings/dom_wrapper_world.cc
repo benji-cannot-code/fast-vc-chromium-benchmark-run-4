852d9ceb41b21cf0e26aecd3396ffa7259ee5ca6
Anton Bikineev <bikineev@chromium.org>
2023-04-28 12:00:29 +0000

    bindings: Implement BlinkRootsHandler::TryResetRoot()
    This is the thread-safe counterpart of ResetRoot which only checks the
    hot path - the main world handle.
    
    Bug: 1432840
    Change-Id: I8e481e8861aa66a993733f3c3fc116d64e305263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4474509
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137074}

--- third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
+++ third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
@@ -34,9 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/no_destructor.h"
-#include "base/synchronization/lock.h"
-
 #include "third_party/abseil-cpp/absl/base/attributes.h"
 #include "third_party/blink/public/platform/web_isolated_world_info.h"
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
@@ -310,10 +307,6 @@ bool DOMWrapperWorld::HasWrapperInAnyWorldInMainThread(
 bool DOMWrapperWorld::UnsetNonMainWorldWrapperIfSet(
     ScriptWrappable* object,
     const v8::TracedReference<v8::Object>& handle) {
-  // This function can be called in parallel.
-  static base::NoDestructor<base::Lock> g_lock;
-  base::AutoLock lock(*g_lock);
-
   for (DOMWrapperWorld* world : GetWorldMap().Values()) {
     DOMDataStore& data_store = world->DomDataStore();
     if (data_store.UnsetSpecificWrapperIfSet(object, handle))
