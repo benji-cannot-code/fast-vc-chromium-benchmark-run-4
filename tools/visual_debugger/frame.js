4e61e77857e6ba3c6feb006f39d3889b794be041
Peter McNeeley <petermcneeley@chromium.org>
2023-04-28 01:20:50 +0000

    Fix live button in visual debugger
    Bug: 1423059
    Change-Id: Id20dd65d534d7ec6d1570dad6f51d2114f1ca506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482912
    Reviewed-by: Kevin Haslett <khaslett@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136910}

--- tools/visual_debugger/frame.js
+++ tools/visual_debugger/frame.js
@@ -376,6 +376,7 @@ class Viewer {
     }
   }
 
+
   drawPreviousFrame() {
     // When we switch to a different frame, we need to unfreeze the current
     // frame (to make sure the frame draws completely the next time it is drawn
@@ -456,7 +457,7 @@ class Player {
     this.viewer_ = viewer;
     this.paused_ = false;
     this.nextFrameScheduled_ = false;
-
+    this.live_ = true;
     this.drawCb_ = draw_cb;
 
     Player.instances[0] = this;
@@ -467,6 +468,10 @@ class Player {
     if (this.nextFrameScheduled_) return;
 
     const drawn = this.viewer_.drawNextFrame();
+    if(this.live_){
+      while(this.viewer_.drawNextFrame());
+    }
+
     this.didDrawNewFrame_();
     if (!drawn) return;
 
@@ -478,8 +483,15 @@ class Player {
     });
   }
 
+  live()
+  {
+    this.live_ = true;
+    this.play();
+  }
+
   pause() {
     this.paused_ = true;
+    this.live_ = false;
   }
 
   rewind() {
