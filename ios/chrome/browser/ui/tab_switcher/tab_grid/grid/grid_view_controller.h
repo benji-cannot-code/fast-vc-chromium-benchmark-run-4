5c2beeb39ae25a79163b4598da9fb84fd73974f6
Aliona DANGLA <alionadangla@google.com>
2023-04-28 15:19:59 +0000

    [iOS] Enable edit button when no active but inactive tabs
    When a user is in the regular tab grid, the "Close All" option should be
    available if there is inactive tab to close even if there is no actives.
    This CL add dynamic update to the Edit button in the following scenario:
    * When the regular tab grid is empty but there is inactive tabs then
    "Edit" button is available and only have "Close All" options
    See video:
    https://drive.google.com/file/d/1yDpOQjaMNaqyLbCOTlFTT68ctFWZHpMl/view?usp=share_link
    * When there is no active nor inactive then "Edit" is not available
    See screenshot:
    https://drive.google.com/file/d/1U_6s-UQBYjYZgte_6GbN0rukRz29qAGC/view?usp=share_link
    * For incognito, the "Edit" button should behave as before
    See video:
    https://drive.google.com/file/d/1Z7EJnpHT4X9G8KcrtrYursPPcfj0MGDC/view?usp=share_link
    
    Fixed: 1434217
    Change-Id: I8e14eff68554f1252647f67f685339fc6e46b467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481598
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137170}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.h
@@ -111,6 +111,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) UIView<GridEmptyView>* emptyStateView;
 // Returns YES if the grid has no items.
 @property(nonatomic, readonly, getter=isGridEmpty) BOOL gridEmpty;
+// Returns YES if the inactive grid has no items.
+@property(nonatomic, readonly, getter=isInactiveGridEmpty)
+    BOOL inactiveGridEmpty;
 // The visual look of the grid.
 @property(nonatomic, assign) GridTheme theme;
 // The current mode for the grid.
