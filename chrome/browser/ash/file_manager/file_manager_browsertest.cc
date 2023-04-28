6c20f5c9e0ce94ea0c5aa12453177316551d5b6f
Ben Reich <benreich@chromium.org>
2023-04-28 12:38:54 +0000

    [FilesBulkPinning] Only show cloud icon when pref is enabled
    For certain states (in progress and paused) the cloud icon should show
    only when the preference is enabled. When other consumers call
    CalculateRequiredSpace this moves the PinManager into an in progress
    state, however, the preference is not enabled. To avoid showing the
    cloud icon for the time it is calculating, only show when the preference
    is enabled.
    
    Bug: b:275635808
    Test: browser_tests --gtest_filter=*toolbar*
    Change-Id: Id870cd4458695ae447a5de13b2f29ec835f12a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482287
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137091}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1345,6 +1345,8 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("toolbarCloudIconShouldNotShowWhenCannotGetFreeSpace")
             .EnableBulkPinning(),
         TestCase("toolbarCloudIconWhenPressedShouldOpenCloudPanel")
+            .EnableBulkPinning(),
+        TestCase("toolbarCloudIconShouldNotShowWhenPrefDisabled")
             .EnableBulkPinning()));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
