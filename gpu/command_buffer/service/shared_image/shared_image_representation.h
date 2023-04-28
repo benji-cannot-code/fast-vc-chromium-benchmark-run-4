597f7a63e36928f188908d7fbd880a3e4d3cce7d
Sunny Sachanandani <sunnyps@chromium.org>
2023-04-28 18:49:49 +0000

    Reland "graphite: Build Skia Graphite core by default"
    Add //gpu/config:workaround_list to skia_source_set deps to fix build.
    
    > Original change's description:
    > graphite: Build Skia Graphite core by default
    >
    > Using ENABLE_SKIA_GRAPHITE build flag is proving to be burdensome in
    > practice due to the many places where Viz/OOP-R would have to interact
    > with Graphite. Just limiting build flag checks to Dawn/Metal backend
    > specific code is more tractable. Binary cost increase for just building
    > Skia Graphite core should be modest.
    >
    > Enabling Graphite build by default caused gn check to fail because:
    > 1) Backend specific public includes were not split from private sources.
    >    Fixed by https://skia-review.googlesource.com/c/skia/+/677136
    > 2) Shared GPU sources need to depend on private Graphite sources which
    >    requires splitting up the skia_graphite source set into separate
    >    public and private source sets - sources in the skia_graphite_private
    >    can be included only in the skia component whereas public files in
    >    skia_graphite_public are exported as skia component public_deps.
    >
    > Binary Size: Looking at the SuperSize report, most of the increase is
    > Graphite code (~104kB), and some large SkSL shader strings (~18kB).
    > Graphite is Skia's new GPU backend that's intended to replace
    > the current GPU backend (Ganesh) completely. This CL makes Graphite
    > core (i.e. not specific to any OS/native API) build by default in
    > Chromium. See above for why we want to do this instead of using
    > conditional build flags. Graphite will be rolled out in phases
    > targeting Mac and Windows first, followed by Android and ChromeOS.
    > Eventually, it is intended that Ganesh will be removed completely from
    > Chromium which will significantly reduce binary size.
    >
    > Binary-Size: See commit description.
    > Fuchsia-Binary-Size: See commit description.
    > Bug: 1429361
    > Change-Id: Icd8f3343cde6c3b1e63f7e317b60b8a122e159d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456509
    > Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    > Reviewed-by: Kevin Lubick <kjlubick@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1136876}
    
    Binary-Size: See commit description.
    Fuchsia-Binary-Size: See commit description.
    Bug: 1429361
    Change-Id: Ice1b366d3c370ffe36deee24bacdf0944ed673ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4485538
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137303}

--- gpu/command_buffer/service/shared_image/shared_image_representation.h
+++ gpu/command_buffer/service/shared_image/shared_image_representation.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrDirectContext.h"
+#include "third_party/skia/include/gpu/graphite/BackendTexture.h"
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
@@ -44,10 +45,6 @@ extern "C" typedef struct AHardwareBuffer AHardwareBuffer;
 #if BUILDFLAG(IS_WIN)
 #include <d3d11.h>
 #include <wrl/client.h>
-#endif
-
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
-#include "third_party/skia/include/gpu/graphite/BackendTexture.h"
 #endif
 
 typedef unsigned int GLenum;
@@ -325,7 +322,6 @@ class GPU_GLES2_EXPORT SkiaImageRepresentation
       return promise_image_textures_[plane_index].get();
     }
 
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
     skgpu::graphite::BackendTexture graphite_texture() const {
       DCHECK(representation()->format().is_single_plane());
       return graphite_texture(0);
@@ -333,7 +329,6 @@ class GPU_GLES2_EXPORT SkiaImageRepresentation
     skgpu::graphite::BackendTexture graphite_texture(int plane_index) const {
       return graphite_textures_[plane_index];
     }
-#endif
 
     // NOTE: Implemented only for Ganesh.
     // Applies the GrBackendSurfaceMutableState for Vulkan layout and external
@@ -346,21 +341,17 @@ class GPU_GLES2_EXPORT SkiaImageRepresentation
     ScopedWriteAccess(
         SkiaImageRepresentation* representation,
         std::vector<sk_sp<SkPromiseImageTexture>> promise_image_textures);
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
     ScopedWriteAccess(
         SkiaImageRepresentation* representation,
         std::vector<skgpu::graphite::BackendTexture> graphite_textures);
-#endif
 
     // A vector of surfaces, promise textures and graphite backend textures
     // corresponding to the number of planes in SharedImageFormat.
     std::vector<sk_sp<SkSurface>> surfaces_;
     // NOTE: Used only for Ganesh.
     std::vector<sk_sp<SkPromiseImageTexture>> promise_image_textures_;
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
     // NOTE: Used only for Graphite.
     std::vector<skgpu::graphite::BackendTexture> graphite_textures_;
-#endif
   };
 
   class GPU_GLES2_EXPORT ScopedReadAccess
@@ -376,7 +367,6 @@ class GPU_GLES2_EXPORT SkiaImageRepresentation
       return promise_image_textures_[plane_index].get();
     }
 
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
     skgpu::graphite::BackendTexture graphite_texture() const {
       DCHECK(representation()->format().is_single_plane());
       return graphite_texture(0);
@@ -384,7 +374,6 @@ class GPU_GLES2_EXPORT SkiaImageRepresentation
     skgpu::graphite::BackendTexture graphite_texture(int plane_index) const {
       return graphite_textures_[plane_index];
     }
-#endif
 
     // Creates an SkImage from BackendTexture for single planar formats or if
     // format prefers external sampler. Creates an SkImage from
@@ -410,19 +399,15 @@ class GPU_GLES2_EXPORT SkiaImageRepresentation
     ScopedReadAccess(
         SkiaImageRepresentation* representation,
         std::vector<sk_sp<SkPromiseImageTexture>> promise_image_textures);
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
     ScopedReadAccess(
         SkiaImageRepresentation* representation,
         std::vector<skgpu::graphite::BackendTexture> graphite_textures);
-#endif
 
     // A vector of promise textures and graphite backend textures corresponding
     // to the number of planes in SharedImageFormat. NOTE: Used only for Ganesh.
     std::vector<sk_sp<SkPromiseImageTexture>> promise_image_textures_;
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
     // NOTE: Used only for Graphite.
     std::vector<skgpu::graphite::BackendTexture> graphite_textures_;
-#endif
   };
 
   SkiaImageRepresentation(SharedImageManager* manager,
@@ -627,7 +612,6 @@ class GPU_GLES2_EXPORT SkiaGaneshImageRepresentation
 ///////////////////////////////////////////////////////////////////////////////
 // SkiaGraphiteImageRepresentation
 
-#if BUILDFLAG(ENABLE_SKIA_GRAPHITE)
 class GPU_GLES2_EXPORT SkiaGraphiteImageRepresentation
     : public SkiaImageRepresentation {
  public:
@@ -724,7 +708,6 @@ class GPU_GLES2_EXPORT SkiaGraphiteImageRepresentation
   // Returns an empty vector on failure.
   virtual std::vector<skgpu::graphite::BackendTexture> BeginReadAccess() = 0;
 };
-#endif
 
 ///////////////////////////////////////////////////////////////////////////////
 // DawnImageRepresentation
