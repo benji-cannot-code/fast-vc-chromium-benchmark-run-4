1c078be2303179112e765e58f1a9afb41b5d570f
Victor Tan <victortan@chromium.org>
2023-04-28 15:13:00 +0000

    Remove full user agent about flags in UI
    As the UA reduction deprecation origin trial will expire soon, remove
    th expiring about flags in UI. Once we completed all phases for UA
    reduction, we can consider clean up the actual feature flags.
    
    Bug: 1440334
    Change-Id: I865ba152eec479fe8aaa57e47de4bae6a7afd2eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4479911
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1137167}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -913,9 +913,6 @@ extern const char kForceDirectionRtl[];
 extern const char kForceUiDirectionName[];
 extern const char kForceUiDirectionDescription[];
 
-extern const char kFullUserAgentName[];
-extern const char kFullUserAgentDescription[];
-
 extern const char kFullscreenPopupWindowsName[];
 extern const char kFullscreenPopupWindowsDescription[];
 
