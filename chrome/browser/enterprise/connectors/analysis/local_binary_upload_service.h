ff119d771a724f4fcc8263df1d14b41e6a92a297
Roger Tawa <rogerta@chromium.org>
2023-04-28 13:50:31 +0000

    LocalBinaryUploadService should send cancel requests after all active requests.
    Bug: b:279184585
    Change-Id: I4ec5e5e5dbc61b864e9b4bdb2fc6db94e0e6cebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482225
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137125}

--- chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.h
+++ chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <set>
 #include <vector>
 
 #include "base/functional/callback_forward.h"
@@ -162,9 +163,18 @@ class LocalBinaryUploadService : public safe_browsing::BinaryUploadService {
           cancel);
 
   // Handles a response from the agent for a given ask or cancel.
-  void HandleAckOrCancelResponse(
+  void HandleAckResponse(
       scoped_refptr<ContentAnalysisSdkManager::WrappedClient> wrapped,
       int status);
+  void HandleCancelResponse(
+      scoped_refptr<ContentAnalysisSdkManager::WrappedClient> wrapped,
+      std::unique_ptr<safe_browsing::BinaryUploadService::CancelRequests>
+          cancel,
+      int status);
+
+  // In tests, this method can be overridden to know when a cancel request
+  // has been sent to the agent.
+  virtual void OnCancelRequestSent(std::unique_ptr<CancelRequests> cancel) {}
 
   // Find the request that corresponds to the given response.
   RequestKey FindRequestByToken(
@@ -184,6 +194,10 @@ class LocalBinaryUploadService : public safe_browsing::BinaryUploadService {
   void FinishRequest(RequestKey key,
                      Result result,
                      ContentAnalysisResponse response);
+
+  // Send a cancel request to the agent if there are no more active requests
+  // for the given action.
+  void SendCancelRequestsIfNeeded();
 
   // Handles a timeout for the request given by `key`.  The request could
   // be in either the active or pending lists.
@@ -223,6 +237,12 @@ class LocalBinaryUploadService : public safe_browsing::BinaryUploadService {
 
   // Keeps track of pending requests not yet sent.
   std::vector<RequestInfo> pending_requests_;
+
+  // Pending cancel requests.  Once all requests associated with a given
+  // action are sent, a cancel request can be sent.  This is to ensure that
+  // chrome does not send requests for analysis for a given action after a
+  // cancel request has been sent.
+  std::set<std::unique_ptr<CancelRequests>> pending_cancel_requests_;
 
   // Timer used to retry connection to agent.
   base::OneShotTimer connection_retry_timer_;
