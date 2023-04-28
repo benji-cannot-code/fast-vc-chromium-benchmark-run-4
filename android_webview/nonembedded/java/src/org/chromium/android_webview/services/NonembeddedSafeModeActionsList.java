740716779cd72562b7638408f55b3b03774db576
avvall <avvall@google.com>
2023-04-28 16:29:56 +0000

    WebView: Deregistering FastVariationsSeedSafeModeAction
    Since this feature will not be complete until the Fast Mode bug in
    AwVariationsSeedFetcher is addressed, the FastVariationsSeedSafeModeAction
    is being de-registered until the issues are properly addressed.
    
    Bug: 1439906
    Test: out/Default/bin/run_webview_instrumentation_test_apk -f SafeModeTest.* -repeat 1 --break-on-failure --num_retries=0 large_output=true
    Change-Id: I14d7f51604e18677c71dd43d9e34c1e44bcc98bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455645
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Adam Walls <avvall@google.com>
    Cr-Commit-Position: refs/heads/main@{#1137206}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/NonembeddedSafeModeActionsList.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/NonembeddedSafeModeActionsList.java
@@ -21,6 +21,6 @@ public final class NonembeddedSafeModeActionsList {
      */
     public static final SafeModeAction[] sList = {
             new ComponentUpdaterResetSafeModeAction(),
-            new NonEmbeddedFastVariationsSeedSafeModeAction(),
+            // TODO(avvall): Re-add NonEmbeddedFastVariationsSeedSafeModeAction
     };
 }
