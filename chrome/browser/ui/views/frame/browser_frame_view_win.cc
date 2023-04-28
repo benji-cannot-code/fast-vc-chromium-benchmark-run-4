9ae27f69b2f5ef27f38281d5d8d1fdf114ca6219
Jackson Loeffler <j@jloeffler.com>
2023-04-28 18:08:31 +0000

    [win] Clean up unnecessary caption_button_container_ null checks
    caption_button_container_ can never be null in BrowserFrameViewWin since
    it no longer transfers ownership to any other View. This CL removes its
    null checks and related documentation.
    
    Change-Id: Ibb387bd74982074243d08bd8da3b567712a508b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4489384
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137277}

--- chrome/browser/ui/views/frame/browser_frame_view_win.cc
+++ chrome/browser/ui/views/frame/browser_frame_view_win.cc
@@ -255,7 +255,7 @@ void BrowserFrameViewWin::PaintAsActiveChanged() {
   // painted to a layer and is not repainted by
   // BrowserNonClientFrameView::PaintAsActiveChanged. Schedule a re-paint here
   // to update the caption button colors.
-  if (caption_button_container_ && caption_button_container_->layer()) {
+  if (caption_button_container_->layer()) {
     caption_button_container_->SchedulePaint();
   }
 }
@@ -324,15 +324,13 @@ int BrowserFrameViewWin::NonClientHitTest(const gfx::Point& point) {
   }
 
   // Then see if the point is within any of the window controls.
-  if (caption_button_container_) {
-    gfx::Point local_point = point;
-    ConvertPointToTarget(parent(), caption_button_container_, &local_point);
-    if (caption_button_container_->HitTestPoint(local_point)) {
-      const int hit_test_result =
-          caption_button_container_->NonClientHitTest(local_point);
-      if (hit_test_result != HTNOWHERE) {
-        return hit_test_result;
-      }
+  gfx::Point local_point = point;
+  ConvertPointToTarget(parent(), caption_button_container_, &local_point);
+  if (caption_button_container_->HitTestPoint(local_point)) {
+    const int hit_test_result =
+        caption_button_container_->NonClientHitTest(local_point);
+    if (hit_test_result != HTNOWHERE) {
+      return hit_test_result;
     }
   }
 
@@ -399,9 +397,7 @@ void BrowserFrameViewWin::UpdateWindowTitle() {
 
 void BrowserFrameViewWin::ResetWindowControls() {
   BrowserNonClientFrameView::ResetWindowControls();
-  if (caption_button_container_) {
-    caption_button_container_->ResetWindowControls();
-  }
+  caption_button_container_->ResetWindowControls();
 }
 
 bool BrowserFrameViewWin::ShouldTabIconViewAnimate() const {
@@ -745,9 +741,6 @@ void BrowserFrameViewWin::LayoutTitleBar() {
 
 void BrowserFrameViewWin::LayoutCaptionButtons() {
   TRACE_EVENT0("views.frame", "BrowserFrameViewWin::LayoutCaptionButtons");
-  if (!caption_button_container_) {
-    return;
-  }
 
   caption_button_container_->SetVisible(!frame()->IsFullscreen());
 
