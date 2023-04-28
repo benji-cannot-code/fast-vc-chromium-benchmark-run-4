d9a46e8462be4cc6c6d95f8d6b55fa6a56cf3fe4
Pi-Hsun Shih <pihsun@chromium.org>
2023-04-28 08:10:28 +0000

    CCA: fix getAudioTrack() can be undefined
    CL:4479310 accidentally broke CCA since the getAudioTrack() can actually
    be undefined when there's no audio track. Fix it to return null instead.
    
    Bug: None
    Test: cca.py lint
    Test: manually tested on betty
    Change-Id: I3581e4bcc956ee1f1662895ccae837e2957d9a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4486548
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137015}

--- ash/webui/camera_app_ui/resources/js/device/camera_manager.ts
+++ ash/webui/camera_app_ui/resources/js/device/camera_manager.ts
@@ -165,8 +165,8 @@ export class CameraManager implements EventListener {
     return this.preview.getVideo();
   }
 
-  getAudioTrack(): MediaStreamTrack {
-    return this.getPreviewVideo().getStream().getAudioTracks()[0];
+  getAudioTrack(): MediaStreamTrack|null {
+    return this.getPreviewVideo().getStream().getAudioTracks()[0] ?? null;
   }
 
   /**
