9a0fee2ece2dc133b4f3853e409204b55304b25e
Zelin Liu <zelin@chromium.org>
2023-04-28 21:29:43 +0000

    iwa: Updating Page Info IWA-logic with UrlIdentity API
    Updating IWA-logic inside Page Info with UrlIdentity API.
    
    - Adds `IsIsolatedWebApp()` and `GetSubjectName()` to PageInfoDelegate.
    
    - Removes `GetWebAppShortName()` from PageInfoDelegate.
    
    - Adds enum option |kOmitSchemePathAndTrivialSubdomains| to UrlIdentity.
    
    - Removes `GetSiteNameOrAppNameToDisplay()`,
    `SetIsolatedWebAppNameForTesting()`, `GetSimpleSiteName()`,
    `isolated_web_app_name_`, `is_isolated_web_app_for_testing_` from
    PageInfo.
    
    - Removes |PageInfoTest.IsolatedWebAppStatus| unit test. Moving the
    logic to |PageInfoBubbleViewIsolatedWebAppBrowserTest.
    InvokeUi_AppNameIsDisplayedInsteadOfOriginForIsolatedWebApps_REV2|.
    
    Bug: 1311065
    Change-Id: Ib4147e8a86d1bbf2e9ec609708fc7cfc5e05991d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4470197
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1137391}

--- chrome/browser/ui/page_info/chrome_page_info_delegate.h
+++ chrome/browser/ui/page_info/chrome_page_info_delegate.h
@@ -58,7 +58,7 @@ class ChromePageInfoDelegate : public PageInfoDelegate {
   bool CreateInfoBarDelegate() override;
   std::unique_ptr<content_settings::CookieControlsController>
   CreateCookieControlsController() override;
-  std::u16string GetWebAppShortName() override;
+  bool IsIsolatedWebApp() override;
   // In Chrome's case, this may show the site settings page or an app settings
   // page, depending on context.
   void ShowSiteSettings(const GURL& site_url) override;
@@ -75,6 +75,7 @@ class ChromePageInfoDelegate : public PageInfoDelegate {
   void OnUIClosing() override;
 #endif
 
+  std::u16string GetSubjectName(const GURL& url) override;
   permissions::PermissionDecisionAutoBlocker* GetPermissionDecisionAutoblocker()
       override;
   StatefulSSLHostStateDelegate* GetStatefulSSLHostStateDelegate() override;
