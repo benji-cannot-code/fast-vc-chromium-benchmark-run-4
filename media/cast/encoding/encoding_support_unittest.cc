5904df755a4c853eb7eba51844743c533ac0d101
Hirokazu Honda <hiroh@chromium.org>
2023-04-28 00:51:22 +0000

    media/cast/encoding: Clean up includes
    Bug: b:178660032
    Test: Build
    Change-Id: I22553f80eb17295d457a957c43e05f1c1a1a60a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483720
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Auto-Submit: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136892}

--- media/cast/encoding/encoding_support_unittest.cc
+++ media/cast/encoding/encoding_support_unittest.cc
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
-#include "base/cpu.h"  // nogncheck
-#endif
-
 namespace media::cast::encoding_support {
 namespace {
 
