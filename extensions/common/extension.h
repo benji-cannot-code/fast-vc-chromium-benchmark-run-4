25daa4a3b58b8e7b74153eaca82f2a2cbb2c1fb9
Claudio DeSouza <cdesouza@igalia.com>
2023-04-28 15:37:14 +0000

    Replace base::GUID uses with base::Uuid (228/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /extensions/common
    
    This CL was uploaded by git cl split.
    
    R=finnur@chromium.org
    
    Bug: 1428566
    Change-Id: I713ab5d7622bd34423d070ceb8b6b91b3aa7e280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483706
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137180}

--- extensions/common/extension.h
+++ extensions/common/extension.h
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_path.h"
-#include "base/guid.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/threading/thread_checker.h"
+#include "base/uuid.h"
 #include "base/values.h"
 #include "base/version.h"
 #include "extensions/buildflags/buildflags.h"
@@ -455,7 +455,7 @@ class Extension final : public base::RefCountedThreadSafe<Extension> {
 
   // A dynamic ID that can be used when referencing extension resources via URL
   // instead of an extension ID.
-  base::GUID guid_;
+  base::Uuid guid_;
 };
 
 typedef std::vector<scoped_refptr<const Extension>> ExtensionList;
