5dcb4111375f890678b3a518386b9ee620f20a00
Jeffrey Young <cowmoo@google.com>
2023-04-28 13:42:26 +0000

    events: rm PlatformEventSourceTestApi
    Unused and no longer needed.
    
    Bug: none
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I0a8122eef1210668c2aebd65c1ea513dd37dc889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4484389
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1137122}

--- ui/events/platform/platform_event_source.h
+++ ui/events/platform/platform_event_source.h
@@ -22,10 +22,6 @@ class PlatformEventDispatcher;
 class PlatformEventObserver;
 class ScopedEventDispatcher;
 
-namespace test {
-class PlatformEventSourceTestAPI;
-}
-
 // PlatformEventSource receives events from a source and dispatches the events
 // to the appropriate dispatchers.
 class EVENTS_EXPORT PlatformEventSource {
@@ -92,7 +88,6 @@ class EVENTS_EXPORT PlatformEventSource {
 
  private:
   friend class ScopedEventDispatcher;
-  friend class test::PlatformEventSourceTestAPI;
 
   // Use a base::ObserverList<> instead of an std::vector<> to store the list of
   // dispatchers, so that adding/removing dispatchers during an event dispatch
