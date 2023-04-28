f4c531ea12dca185c636aed1e1108fe7c2c1d738
Ben Reich <benreich@chromium.org>
2023-04-28 06:46:31 +0000

    [FilesBulkPinning] Introduce fully synced state
    When the bulk pinning feature has fully synced all files, it enter into
    the "All files synced" state. This should show when the progress="100"
    as an attribute.
    
    Light: http://screen/mpJnbHQ6QY28snd
    Dark: http://screen/To2c4qjP7RikaQd
    
    Bug: b:275635808, b:276126188
    Test: browser_tests --gtest_filter=FileManagerJsTest.XfCloudPanel
    Test: browser_tests --gtest_filter=*CloudPanelContainer
    Change-Id: I14fc3bdbe16f0e6c0881f7e5ee3f119132290f51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480557
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136992}

--- ui/file_manager/file_manager/widgets/xf_cloud_panel.ts
+++ ui/file_manager/file_manager/widgets/xf_cloud_panel.ts
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import {str, strf, util} from '../common/js/util.js';
+import {constants} from '../foreground/js/constants.js';
 
 import {css, customElement, html, property, query, XfBase} from './xf_base.js';
 
@@ -45,7 +46,8 @@ export class XfCloudPanel extends XfBase {
             } catch (e) {
               return null;
             }
-            if (util.isNullOrUndefined(percentage) || percentage < 0 ||
+            if (util.isNullOrUndefined(percentage) ||
+                Number.isNaN(percentage) || percentage < 0 ||
                 percentage > 100) {
               return null;
             }
@@ -122,7 +124,13 @@ export class XfCloudPanel extends XfBase {
           </progress>
           <div class="progress-description">3 minutes remaining</div>
         </div>
-        <xf-icon class="status-icon" type="error" size="large"></xf-icon>
+        <div id="progress-finished">
+          <xf-icon type="${
+        constants.ICON_TYPES.BULK_PINNING_DONE}" size="large"></xf-icon>
+          <div class="status-description">
+            ${str('DRIVE_ALL_FILES_SYNCED')}
+          </div>
+        </div>
         <div class="divider"></div>
         <div class="menu">
           <button class="action" @click=${this.onSettingsClicked_}>${
@@ -142,7 +150,12 @@ function getCSS() {
       z-index: 600;
     }
 
-    :host(:not([items][percentage])) #progress-state {
+    :host(:not([items][percentage])) #progress-state,
+    :host([percentage="100"]) #progress-state {
+      display: none;
+    }
+
+    :host(:not([items][percentage="100"])) #progress-finished {
       display: none;
     }
 
@@ -153,9 +166,14 @@ function getCSS() {
       width: 320px;
     }
 
-    .status-icon {
-      --xf-icon-color: transparent;
-      align-self: center;
+    #progress-finished {
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+    }
+
+    xf-icon[type="bulk_pinning_done"] {
+      --xf-icon-color: var(--cros-sys-positive);
       padding: 27px 0px 20px;
     }
 
