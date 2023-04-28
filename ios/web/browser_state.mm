57cd44c47f6dd4b1fd0d309ac7427fcb6d59b868
Claudio DeSouza <cdesouza@igalia.com>
2023-04-28 10:17:02 +0000

    Replace base::GUID uses with base::Uuid (239/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /ios/web
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Bug: 1428566
    Change-Id: Ic07fb7c4c4f4c821730f03fbf33dd513053ed9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4484520
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137046}

--- ios/web/browser_state.mm
+++ ios/web/browser_state.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/functional/bind.h"
 #import "base/functional/callback_helpers.h"
-#import "base/guid.h"
 #import "base/location.h"
 #import "base/memory/ref_counted.h"
 #import "base/metrics/histogram_functions.h"
