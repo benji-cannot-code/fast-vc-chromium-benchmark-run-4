e1a3144a137c66efac211f3ed4ae980deba32dd1
Elaine Chien <elainechien@chromium.org>
2023-04-28 16:51:34 +0000

    [ChromeRefresh2023]Fix autofill submenu icon colors, cleanup FindAndEdit
    Bug: 1427667
    Change-Id: Id82fd2473ebe0b08a4ca6318b6a810d33508ff40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481099
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137221}

--- chrome/browser/ui/toolbar/app_menu_model.h
+++ chrome/browser/ui/toolbar/app_menu_model.h
@@ -142,8 +142,7 @@ class ExtensionsMenuModel : public ui::SimpleMenuModel {
 
 class AutofillSubMenuModel : public ui::SimpleMenuModel {
  public:
-  AutofillSubMenuModel(ui::SimpleMenuModel::Delegate* delegate,
-                       AppMenuIconController* app_menu_icon_controller);
+  explicit AutofillSubMenuModel(ui::SimpleMenuModel::Delegate* delegate);
 
   AutofillSubMenuModel(const AutofillSubMenuModel&) = delete;
   AutofillSubMenuModel& operator=(const AutofillSubMenuModel&) = delete;
@@ -153,16 +152,12 @@ class AutofillSubMenuModel : public ui::SimpleMenuModel {
 
 class FindAndEditSubMenuModel : public ui::SimpleMenuModel {
  public:
-  FindAndEditSubMenuModel(ui::SimpleMenuModel::Delegate* delegate,
-                          Browser* browser);
+  explicit FindAndEditSubMenuModel(ui::SimpleMenuModel::Delegate* delegate);
 
   FindAndEditSubMenuModel(const FindAndEditSubMenuModel&) = delete;
   FindAndEditSubMenuModel& operator=(const FindAndEditSubMenuModel&) = delete;
 
   ~FindAndEditSubMenuModel() override;
-
- private:
-  void Build(Browser* browser);
 };
 
 // A menu model that builds the contents of the app menu.
