e6d904a36663bc8c07e244ac2e84fc814956171b
Minoru Chikamune <chikamune@chromium.org>
2023-04-28 00:01:24 +0000

    Bugfix: avoid failure on about:srcdoc
    If the user navigates to about:srcdoc or invalid URLs, the chrome
    browser will crash when the SpeculativeServiceWorkerStartup feature is
    enabled. This CL fixes by checking these URLs before calling
    GetTentativeOriginAtRequestTime() and
    StartServiceWorkerForNavigationHint().
    
    Bug: 1440062
    Change-Id: I670f24c0c6847467ea370a22b2efebdf355bd4aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4478693
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136874}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -121,6 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/network_service_util.h"
+#include "content/public/common/origin_util.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/common/url_utils.h"
 #include "mojo/public/cpp/system/data_pipe.h"
@@ -2001,7 +2002,14 @@ NavigationRequest::NavigationRequest(
   // service worker (e.g, Prerendering or the previous navigation already
   // started the service worker), but this call does nothing if the service
   // worker already started for the URL.
-  if (reload_type_ != ReloadType::BYPASSING_CACHE &&
+  //
+  // Checking OriginCanAccessServiceWorkers() is needed before calling
+  // GetTentativeOriginAtRequestTime() since loading an about:srcdoc URL
+  // on the main frame will cause a failure while processing
+  // GetTentativeOriginAtRequestTime(). OriginCanAccessServiceWorkers()
+  // can also skip unnecessary computation.
+  if (GetURL().is_valid() && OriginCanAccessServiceWorkers(GetURL()) &&
+      reload_type_ != ReloadType::BYPASSING_CACHE &&
       base::FeatureList::IsEnabled(kSpeculativeServiceWorkerStartup)) {
     if (ServiceWorkerContext* context =
             frame_tree_node_->navigator()
