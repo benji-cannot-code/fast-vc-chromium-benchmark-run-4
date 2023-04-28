60b055dfb811c47cc863400738e3db85b98500d6
Mei Liang <meiliang@chromium.org>
2023-04-28 00:34:34 +0000

    Breakdown required entry for commerce in the internals page
    Screenshot: https://screenshot.googleplex.com/Aa23BhEpjYt2QGd
    
    Bug: b/274621159
    Change-Id: I2639f70332908a18e46511f5a47257e1a895e77a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4460963
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136883}

--- components/commerce/core/internals/resources/commerce_internals_api_proxy.ts
+++ components/commerce/core/internals/resources/commerce_internals_api_proxy.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CommerceInternalsHandlerFactory, CommerceInternalsHandlerRemote, CommerceInternalsPageCallbackRouter} from './commerce_internals.mojom-webui.js';
+import {CommerceInternalsHandlerFactory, CommerceInternalsHandlerRemote, CommerceInternalsPageCallbackRouter, ShoppingListEligibleDetail} from './commerce_internals.mojom-webui.js';
 
 export class CommerceInternalsApiProxy {
   private callbackRouter: CommerceInternalsPageCallbackRouter;
@@ -25,6 +25,11 @@ export class CommerceInternalsApiProxy {
 
   getIsShoppingListEligible(): Promise<{eligible: boolean}> {
     return this.handler.getIsShoppingListEligible();
+  }
+
+  getShoppingListEligibleDetails():
+      Promise<{detail: ShoppingListEligibleDetail}> {
+    return this.handler.getShoppingListEligibleDetails();
   }
 
   getCallbackRouter(): CommerceInternalsPageCallbackRouter {
