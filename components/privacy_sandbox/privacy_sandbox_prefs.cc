80f3e12f6e696b4ce0d0b5a60dcea4ddcc393198
Nicola Tommasi <tommasin@chromium.org>
2023-04-28 11:32:26 +0000

    [GARU] Set unrestricted pref when account capability can run PS API
    This CL introduces a new kPrivacySandboxM1Unrestricted pref which tells
    whether the user was ever detected as unrestricted in relation to the
    Privacy Sandbox APIs run capability. The pref will be used in
    combination with the its "kPrivacySandboxM1Restricted" counterpart
    to record whether the user is waiting for graduation.
    
    
    Bug: 1428506, b/279714352
    Change-Id: I36aeac363cc81a9d1dd9ffdf5c1448b490e8ea9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4484203
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137066}

--- components/privacy_sandbox/privacy_sandbox_prefs.cc
+++ components/privacy_sandbox/privacy_sandbox_prefs.cc
@@ -36,6 +36,8 @@ const char kPrivacySandboxM1AdMeasurementEnabled[] =
 
 const char kPrivacySandboxM1Restricted[] = "privacy_sandbox.m1.restricted";
 
+const char kPrivacySandboxM1Unrestricted[] = "privacy_sandbox.m1.unrestricted";
+
 const char kPrivacySandboxApisEnabled[] = "privacy_sandbox.apis_enabled";
 
 const char kPrivacySandboxApisEnabledV2[] = "privacy_sandbox.apis_enabled_v2";
@@ -124,6 +126,7 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kPrivacySandboxM1AdMeasurementEnabled,
                                 false);
   registry->RegisterBooleanPref(prefs::kPrivacySandboxM1Restricted, false);
+  registry->RegisterBooleanPref(prefs::kPrivacySandboxM1Unrestricted, false);
 
   registry->RegisterBooleanPref(
       prefs::kPrivacySandboxManuallyControlled, false,
