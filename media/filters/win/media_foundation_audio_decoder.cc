d507c25c9b55eaa3800096b5f893fc939c0aa50b
Vikram Pasupathy <vpasupathy@chromium.org>
2023-04-29 05:56:43 +0000

    media: Remove DoSubsamplesMatch from Audio Decoders
    Since these audio decoders do not handle encrypted content, this CL
    removes all DoSubsamplesMatch calls from these decoders. Instead, we
    have a check to enforce in these decoders, we do not decode encrypted
    buffers.
    
    Bug: b/261450791
    Change-Id: Ibee42ef1f4b1c3123860494dd1d3077d2ddc7299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4448808
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137513}

--- media/filters/win/media_foundation_audio_decoder.cc
+++ media/filters/win/media_foundation_audio_decoder.cc
@@ -181,10 +181,8 @@ void MediaFoundationAudioDecoder::Initialize(const AudioDecoderConfig& config,
 
 void MediaFoundationAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
                                          DecodeCB decode_cb) {
-  if (!DecoderBuffer::DoSubsamplesMatch(*buffer)) {
-    std::move(decode_cb).Run(DecoderStatus::Codes::kFailed);
-    return;
-  }
+  DecodeCB decode_cb_bound =
+      base::BindPostTaskToCurrentDefault(std::move(decode_cb));
 
   if (buffer->end_of_stream()) {
     switch (decoder_->ProcessMessage(MFT_MESSAGE_COMMAND_DRAIN, 0)) {
@@ -194,26 +192,33 @@ void MediaFoundationAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
           rc = PumpOutput(PumpState::kNormal);
         } while (rc == OutputStatus::kSuccess);
         // Return kOk if more input is needed since this is end of stream
-        base::BindPostTaskToCurrentDefault(std::move(decode_cb))
+        std::move(decode_cb_bound)
             .Run(rc == OutputStatus::kFailed ? DecoderStatus::Codes::kFailed
                                              : DecoderStatus::Codes::kOk);
         return;
       }
       case MF_E_TRANSFORM_TYPE_NOT_SET:
-        base::BindPostTaskToCurrentDefault(std::move(decode_cb))
+        std::move(decode_cb_bound)
             .Run(DecoderStatus::Codes::kPlatformDecodeFailure);
         return;
       default:
-        base::BindPostTaskToCurrentDefault(std::move(decode_cb))
-            .Run(DecoderStatus::Codes::kFailed);
+        std::move(decode_cb_bound).Run(DecoderStatus::Codes::kFailed);
         return;
     }
   }
 
+  if (buffer->decrypt_config() &&
+      buffer->decrypt_config()->encryption_scheme() !=
+          EncryptionScheme::kUnencrypted) {
+    DLOG(ERROR) << "Encrypted buffer not supported";
+    std::move(decode_cb_bound)
+        .Run(DecoderStatus::Codes::kUnsupportedEncryptionMode);
+    return;
+  }
+
   if (buffer->timestamp() == kNoTimestamp) {
     DLOG(ERROR) << "Received a buffer without timestamps!";
-    base::BindPostTaskToCurrentDefault(std::move(decode_cb))
-        .Run(DecoderStatus::Codes::kMissingTimestamp);
+    std::move(decode_cb_bound).Run(DecoderStatus::Codes::kMissingTimestamp);
     return;
   }
 
@@ -224,14 +229,12 @@ void MediaFoundationAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
 
   auto sample = CreateEmptySampleWithBuffer(buffer->data_size(), 0);
   if (!sample) {
-    base::BindPostTaskToCurrentDefault(std::move(decode_cb))
-        .Run(DecoderStatus::Codes::kFailed);
+    std::move(decode_cb_bound).Run(DecoderStatus::Codes::kFailed);
     return;
   }
 
   if (!PopulateInputSample(sample.Get(), *buffer)) {
-    base::BindPostTaskToCurrentDefault(std::move(decode_cb))
-        .Run(DecoderStatus::Codes::kFailed);
+    std::move(decode_cb_bound).Run(DecoderStatus::Codes::kFailed);
     return;
   }
 
@@ -253,7 +256,7 @@ void MediaFoundationAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
         break;
     }
     // Drop remaining samples on error, no need to call PumpOutput
-    base::BindPostTaskToCurrentDefault(std::move(decode_cb)).Run(rc);
+    std::move(decode_cb_bound).Run(rc);
     return;
   }
 
@@ -266,8 +269,7 @@ void MediaFoundationAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
     if (rc == OutputStatus::kNeedMoreInput)
       break;
     if (rc == OutputStatus::kFailed) {
-      base::BindPostTaskToCurrentDefault(std::move(decode_cb))
-          .Run(DecoderStatus::Codes::kFailed);
+      std::move(decode_cb_bound).Run(DecoderStatus::Codes::kFailed);
       return;
     }
     decoded_frame_this_loop = true;
@@ -281,7 +283,7 @@ void MediaFoundationAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
     DCHECK(!result);
   }
 
-  base::BindPostTaskToCurrentDefault(std::move(decode_cb)).Run(OkStatus());
+  std::move(decode_cb_bound).Run(OkStatus());
 }
 
 void MediaFoundationAudioDecoder::Reset(base::OnceClosure reset_cb) {
