65358aec081daf7f9e1a405f416ea9a45b4cd4d8
Chris Fredrickson <cfredric@chromium.org>
2023-04-30 00:17:09 +0000

    Stop stringifying ResultQueue's data
    Whichever consumers require strings can supply strings; otherwise, the
    ResultQueue should not care what its contents are. This makes it
    easier to reuse in different contexts.
    
    Change-Id: I63b5d83b0934a1d7bae2f09f88e7a27dec63c953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4493683
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137606}

--- content/browser/payments/payment_app_browsertest.cc
+++ content/browser/payments/payment_app_browsertest.cc
@@ -108,7 +108,9 @@ class PaymentAppBrowserTest : public ContentBrowserTest {
     return EvalJs(shell()->web_contents(), script).ExtractString();
   }
 
-  std::string PopConsoleString() { return RunScript("resultQueue.pop()"); }
+  std::string PopConsoleString() {
+    return RunScript("resultQueue.pop().then(result => String(result))");
+  }
 
   void RegisterPaymentApp() {
     SkBitmap app_icon;
