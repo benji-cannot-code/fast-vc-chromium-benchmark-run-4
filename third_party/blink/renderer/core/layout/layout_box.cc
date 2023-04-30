3a2b753fddd4d4a074ef41eaffed8f4e031b2c2c
Alice Boxhall <alice@igalia.com>
2023-04-30 10:10:23 +0000

    [cleanup] Remove PaintLayer::size_
    Taking over https://crrev.com/c/4327287 from Ian Kilpatrick.
    
    Change-Id: Ie3bde5d340e7c4df7e72868f87040ebb45d58503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437810
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Alice Boxhall <alice@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1137642}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -1255,7 +1255,7 @@ void LayoutBox::UpdateAfterLayout() {
   // transform after layout.
   if (HasLayer()) {
     Layer()->UpdateTransform();
-    Layer()->UpdateSizeAndScrollingAfterLayout();
+    Layer()->UpdateScrollingAfterLayout();
   }
 
   GetFrame()->GetInputMethodController().DidUpdateLayout(*this);
@@ -1514,7 +1514,7 @@ void LayoutBox::SetLocationAndUpdateOverflowControlsIfNeeded(
       old_pixel_snapped_border_rect_size) {
     bool needed_layout = NeedsLayout();
     PaintLayerScrollableArea::FreezeScrollbarsScope freeze_scrollbar;
-    Layer()->UpdateSizeAndScrollingAfterLayout();
+    Layer()->UpdateScrollingAfterLayout();
     // The above call should not schedule new NeedsLayout.
     DCHECK(needed_layout || !NeedsLayout());
   }
@@ -2713,6 +2713,7 @@ void LayoutBox::LocationChanged() {
 
 void LayoutBox::SizeChanged() {
   NOT_DESTROYED();
+  SetScrollableAreaSizeChanged(true);
   // The size may change because of layout of other objects. Should check this
   // object for paint invalidation.
   if (!NeedsLayout())
