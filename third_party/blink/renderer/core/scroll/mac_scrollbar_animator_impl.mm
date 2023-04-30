17b55c1266bc3e33499724308777ac0bd61a64d0
Avi Drissman <avi@chromium.org>
2023-04-30 18:57:00 +0000

    Remove obsolete "block exceptions" code from Blink
    This is not used any more.
    
    Bug: none
    Change-Id: I83d8e1bba60b288d8aa599eb108a5b135a767796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4493108
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137676}

--- third_party/blink/renderer/core/scroll/mac_scrollbar_animator_impl.mm
+++ third_party/blink/renderer/core/scroll/mac_scrollbar_animator_impl.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/animation/timing_function.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
-#include "third_party/blink/renderer/platform/mac/block_exceptions.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
 
 namespace blink {
