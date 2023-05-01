b27392fbc18dc792d6dcfc38ed3c4e2599071752
Claudio DeSouza <cdesouza@igalia.com>
2023-05-01 14:09:56 +0000

    Replace base::GUID uses with base::Uuid (232/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /fuchsia_web/webinstance_host
    
    This CL was uploaded by git cl split.
    
    R=wez@chromium.org
    
    Bug: 1428566
    Change-Id: I6137e20cc20e1f2274464640e69bb5e2fa2e6149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481327
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137815}

--- fuchsia_web/webinstance_host/web_instance_host.cc
+++ fuchsia_web/webinstance_host/web_instance_host.cc
@@ -65,7 +65,7 @@ vfs::PseudoDir* GetWebInstancesCollectionDir(
 }
 
 // Returns an instance's name given its unique id.
-std::string InstanceNameFromId(const base::GUID& id) {
+std::string InstanceNameFromId(const base::Uuid& id) {
   return base::StrCat({kCollectionName, "_", id.AsLowercaseString()});
 }
 
@@ -86,10 +86,10 @@ void DestroyInstanceDirectory(vfs::PseudoDir* instances_dir,
 }
 
 struct Instance {
-  base::GUID id;
+  base::Uuid id;
   fuchsia::component::BinderPtr binder_ptr;
 
-  Instance(base::GUID id, fuchsia::component::BinderPtr binder_ptr)
+  Instance(base::Uuid id, fuchsia::component::BinderPtr binder_ptr)
       : id(std::move(id)), binder_ptr(std::move(binder_ptr)) {}
 };
 
@@ -148,7 +148,7 @@ class InstanceBuilder {
  private:
   InstanceBuilder(sys::OutgoingDirectory& outgoing_directory,
                   fuchsia::component::Realm& realm,
-                  base::GUID id,
+                  base::Uuid id,
                   std::string name,
                   vfs::PseudoDir* instance_dir,
                   const base::CommandLine& launch_args);
@@ -214,7 +214,7 @@ class InstanceBuilder {
 
   const raw_ref<sys::OutgoingDirectory> outgoing_directory_;
   const raw_ref<fuchsia::component::Realm> realm_;
-  const base::GUID id_;
+  const base::Uuid id_;
   const std::string name_;
   raw_ptr<vfs::PseudoDir> instance_dir_;
   base::CommandLine args_;
@@ -232,7 +232,7 @@ InstanceBuilder::Create(sys::OutgoingDirectory& outgoing_directory,
                         fuchsia::component::Realm& realm,
                         const base::CommandLine& launch_args) {
   // Pick a unique identifier for the new instance.
-  base::GUID instance_id = base::GUID::GenerateRandomV4();
+  base::Uuid instance_id = base::Uuid::GenerateRandomV4();
   auto instance_name = InstanceNameFromId(instance_id);
 
   // Create a pseudo-directory to contain the various directory capabilities
@@ -257,7 +257,7 @@ InstanceBuilder::Create(sys::OutgoingDirectory& outgoing_directory,
 
 InstanceBuilder::InstanceBuilder(sys::OutgoingDirectory& outgoing_directory,
                                  fuchsia::component::Realm& realm,
-                                 base::GUID id,
+                                 base::Uuid id,
                                  std::string name,
                                  vfs::PseudoDir* instance_dir,
                                  const base::CommandLine& launch_args)
@@ -695,7 +695,7 @@ void WebInstanceHost::OnRealmError(zx_status_t status) {
   Uninitialize();
 }
 
-void WebInstanceHost::OnComponentBinderClosed(const base::GUID& id,
+void WebInstanceHost::OnComponentBinderClosed(const base::Uuid& id,
                                               zx_status_t status) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
