2afaab220f865664f40247b8eddcadb3db65fa7d
Hirokazu Honda <hiroh@chromium.org>
2023-05-01 05:34:33 +0000

    GpuArcVEA: Deprecate VideoEncodeAccelerator::Error
    Since VideoEncodeAccelerator::Error is replaced with EncoderStatus,
    this CL changes GpuArcVideoEncodeAccelerator and mojom files so that
    they don't use it.
    
    Bug: b:276005687
    Test: Build
    Change-Id: I2b1e0fa967c4005a80f50e5285d2b97ef6b34984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4478771
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137746}

--- ash/components/arc/mojom/video_encode_accelerator_mojom_traits.cc
+++ ash/components/arc/mojom/video_encode_accelerator_mojom_traits.cc
@@ -10,21 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
-// Make sure values in arc::mojom::VideoEncodeAccelerator::Error and
-// media::VideoEncodeAccelerator::Error match.
-#define CHECK_ERROR_ENUM(value)                                            \
-  static_assert(                                                           \
-      static_cast<int>(arc::mojom::VideoEncodeAccelerator_Error::value) == \
-          media::VideoEncodeAccelerator::Error::value,                     \
-      "enum ##value mismatch")
-
-CHECK_ERROR_ENUM(kIllegalStateError);
-CHECK_ERROR_ENUM(kInvalidArgumentError);
-CHECK_ERROR_ENUM(kPlatformFailureError);
-CHECK_ERROR_ENUM(kErrorMax);
-
-#undef CHECK_ERROR_ENUM
-
 // static
 arc::mojom::VideoFrameStorageType
 EnumTraits<arc::mojom::VideoFrameStorageType,
@@ -47,23 +32,6 @@ bool EnumTraits<arc::mojom::VideoFrameStorageType,
           media::VideoEncodeAccelerator::Config::StorageType::kGpuMemoryBuffer;
       return true;
   }
-  return false;
-}
-
-// static
-arc::mojom::VideoEncodeAccelerator_Error
-EnumTraits<arc::mojom::VideoEncodeAccelerator_Error,
-           media::VideoEncodeAccelerator::Error>::
-    ToMojom(media::VideoEncodeAccelerator::Error input) {
-  return static_cast<arc::mojom::VideoEncodeAccelerator_Error>(input);
-}
-
-// static
-bool EnumTraits<arc::mojom::VideoEncodeAccelerator_Error,
-                media::VideoEncodeAccelerator::Error>::
-    FromMojom(arc::mojom::VideoEncodeAccelerator_Error input,
-              media::VideoEncodeAccelerator::Error* output) {
-  NOTIMPLEMENTED();
   return false;
 }
 
