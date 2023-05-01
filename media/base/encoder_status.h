0b7f0f1345d20cacb7c504d1c909b52061abef8f
Hirokazu Honda <hiroh@chromium.org>
2023-05-01 12:24:38 +0000

    RTCVideoEncoder: Replace the shutdown reason UMA
    RTCVideoEncoder collects the UMA about the hardware encoder status
    with their own error code, RTCVideoEncoderShutdownReason. It is
    basically correspond to VideoEncodeAccelerator::Error.
    VEA reports more detailed error code with EncoderStatus. This replaces
    the UMA with the new UMA, RTCVideoEncoderStatus, who records the
    EncoderStatus.
    
    Bug: b:276005687
    Test: webrtc.RTCPeerConnection* on brya
    Test: Monitor chrome://histograms/Media.RTCVideoEncoderStatus in VC
    
    Change-Id: I1e40ecb7414cb334337b267cf6752ac9eee812fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4478770
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137800}

--- media/base/encoder_status.h
+++ media/base/encoder_status.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 struct EncoderStatusTraits {
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused. Please keep the consistency with
+  // EncoderStatus in tools/metrics/histograms/enums.xml.
   enum class Codes : StatusCodeType {
     kOk = 0,
     kEncoderInitializeNeverCompleted = 1,
@@ -30,6 +33,7 @@ struct EncoderStatusTraits {
     kSystemAPICallError = 15,
     kInvalidInputFrame = 16,
     kInvalidOutputBuffer = 17,
+    kMaxValue = kInvalidOutputBuffer,
   };
   static constexpr StatusGroupType Group() { return "EncoderStatus"; }
 };
