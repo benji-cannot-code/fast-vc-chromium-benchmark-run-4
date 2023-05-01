ab32737d1e5191bbe3f964bf7c9a99088f1a9fd9
Sunggook Chue <sunggch@microsoft.com>
2023-05-01 23:39:23 +0000

    Support MP4Muxer of mvex and trex boxes.
    It continue to add MP4 boxes with 'mvex' and 'trex' boxes.
    'mvex' box is a child of 'moov', and 'trex' is child of
    'mvex'.
    
    The change refactor the header to use macro of DECLARE_MP4_WRITER_CLASS_WITH_BOX
    for box writer declaratioin.
    
    It also updates BoxByteStream to support multiple parallel children
    
    Bug: 1072056
    Change-Id: If08b6f702a28d2fc37163bf992d84d7205d59978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4485406
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1138092}

--- media/muxers/mp4_box_writer.cc
+++ media/muxers/mp4_box_writer.cc
@@ -24,7 +24,7 @@ void Mp4BoxWriter::WriteAndFlush() {
   Write(writer);
 
   // Update the total size on respective boxes.
-  std::vector<uint8_t> buffer = writer.EndWrite();
+  std::vector<uint8_t> buffer = writer.Flush();
 
   // Write the entire boxes to the blob.
   context().GetOutputPositionTracker().WriteString(
@@ -45,7 +45,6 @@ void Mp4BoxWriter::AddChildBox(std::unique_ptr<Mp4BoxWriter> box_writer) {
 
 void Mp4BoxWriter::WriteBox(BoxByteStream& writer, mp4::FourCC fourcc) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  writer.WritePlaceholderSizeU32();
   writer.WriteU32(fourcc);
 }
 
@@ -55,7 +54,6 @@ void Mp4BoxWriter::WriteFullBox(BoxByteStream& writer,
                                 uint8_t version) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.WritePlaceholderSizeU32();
   writer.WriteU32(fourcc);
 
   writer.WriteU8(version);
