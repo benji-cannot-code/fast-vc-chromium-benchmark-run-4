d5fc14e446dbf65dcb29d83e7248f2f2816ab1c1
Claudio DeSouza <cdesouza@igalia.com>
2023-05-01 14:07:48 +0000

    Replace base::GUID uses with base::Uuid (231/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /fuchsia_web/runners/cast
    
    This CL was uploaded by git cl split.
    
    R=wez@chromium.org
    
    Bug: 1428566
    Change-Id: I4472d11472c2f3cb16b9536c17d722a00ed444cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483805
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137814}

--- fuchsia_web/runners/cast/cast_runner_integration_test.cc
+++ fuchsia_web/runners/cast/cast_runner_integration_test.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/mem_buffer_util.h"
 #include "base/fuchsia/scoped_service_binding.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/strings/strcat.h"
@@ -34,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "base/test/test_timeouts.h"
+#include "base/uuid.h"
 #include "build/build_config.h"
 #include "build/chromecast_buildflags.h"
 #include "components/fuchsia_component_support/dynamic_component_host.h"
@@ -240,7 +240,7 @@ class TestCastComponent {
     component_.emplace(
         test_realm_services_.Connect<fuchsia::component::Realm>(),
         test::CastRunnerLauncher::kTestCollectionName,
-        base::GUID::GenerateRandomV4().AsLowercaseString(), component_url,
+        base::Uuid::GenerateRandomV4().AsLowercaseString(), component_url,
         base::BindOnce(&TestCastComponent::OnComponentTeardown,
                        base::Unretained(this)),
         std::move(services));
