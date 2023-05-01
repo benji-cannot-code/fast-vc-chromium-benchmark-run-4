59a8d2d9f7d9073509da14e888b2dd4e2cc48b8b
Sammie Quon <sammiequon@chromium.org>
2023-05-01 21:02:20 +0000

    overview: Separate some overview animation smoothness tests
    These tests are used to make sure the animation smoothness histograms
    recorded for performance tests are working properly. Due to a
    combination of needing to wait for animations and compositor frames,
    they have been flaky and disabled for quite some time.
    
    The tests also test some non histogram related logic. This CL
    separates that logic out into separate tests. The histogram related
    tests will be re-enabled in a follow up CL.
    
    Test: existing tests
    Bug: b/252523767
    Change-Id: I47e2b03c5fbbeb027f566a7a35a433894444444e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496875
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138007}

--- ash/wm/splitview/split_view_controller_unittest.cc
+++ ash/wm/splitview/split_view_controller_unittest.cc
@@ -263,7 +263,7 @@ class SplitViewControllerTest : public AshTestBase {
 
   void EndSplitView() { split_view_controller()->EndSplitView(); }
 
-  void LongPressOnOverivewButtonTray() {
+  void LongPressOnOverviewButtonTray() {
     ui::GestureEvent event(0, 0, 0, base::TimeTicks(),
                            ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
     StatusAreaWidgetTestHelper::GetStatusAreaWidget()
@@ -693,44 +693,6 @@ TEST_F(SplitViewControllerTest, SnapWindowWithUnresizableSnapProperty) {
   EXPECT_EQ(window->GetBoundsInScreen().width(), 300);
 }
 
-// Tests that if split view mode and overview mode are active at the same time,
-// i.e., half of the screen is occupied by a snapped window and half of the
-// screen is occupied by the overview windows grid, the next activatable window
-// will be picked to snap when exiting the overview mode.
-// TODO(crbug.com/1312252): Re-enable this test
-TEST_F(SplitViewControllerTest, DISABLED_ExitOverviewTest) {
-  ui::ScopedAnimationDurationScaleMode anmatin_scale(
-      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
-
-  const gfx::Rect bounds(0, 0, 400, 400);
-  std::unique_ptr<aura::Window> window1(CreateWindow(bounds));
-  std::unique_ptr<aura::Window> window2(CreateWindow(bounds));
-  std::unique_ptr<aura::Window> window3(CreateWindow(bounds));
-  EXPECT_EQ(split_view_controller()->InSplitViewMode(), false);
-
-  ToggleOverview();
-  WaitForOverviewEnterAnimation();
-  CheckOverviewEnterExitHistogram("EnterInTablet", {1, 0}, {0, 0});
-
-  split_view_controller()->SnapWindow(
-      window1.get(), SplitViewController::SnapPosition::kPrimary);
-  EXPECT_EQ(split_view_controller()->InSplitViewMode(), true);
-  EXPECT_EQ(split_view_controller()->state(),
-            SplitViewController::State::kPrimarySnapped);
-  EXPECT_EQ(split_view_controller()->primary_window(), window1.get());
-  // Activate |window1| in preparation to verify that it stays active when
-  // overview mode is ended.
-  wm::ActivateWindow(window1.get());
-
-  ToggleOverview();
-  WaitForOverviewExitAnimation();
-  EXPECT_EQ(split_view_controller()->state(),
-            SplitViewController::State::kBothSnapped);
-  EXPECT_EQ(split_view_controller()->secondary_window(), window3.get());
-  EXPECT_TRUE(wm::IsActiveWindow(window1.get()));
-  CheckOverviewEnterExitHistogram("ExitInSplitView", {1, 0}, {0, 1});
-}
-
 // Tests that in split view with a single overview window, when overview is
 // ended, the wallpaper stays blurred until the window finishes animating.
 TEST_F(SplitViewControllerTest,
@@ -763,11 +725,7 @@ TEST_F(SplitViewControllerTest,
 // Tests that if split view mode is active when entering overview, the overview
 // windows grid should show in the non-default side of the screen, and the
 // default snapped window should not be shown in the overview window grid.
-// TODO(crbug.com/1335854): Deflake this test.
-TEST_F(SplitViewControllerTest, DISABLED_EnterOverviewModeTest) {
-  ui::ScopedAnimationDurationScaleMode anmatin_scale(
-      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
-
+TEST_F(SplitViewControllerTest, EnterOverviewMode) {
   const gfx::Rect bounds(0, 0, 400, 400);
   std::unique_ptr<aura::Window> window1(CreateWindow(bounds));
   std::unique_ptr<aura::Window> window2(CreateWindow(bounds));
@@ -782,13 +740,67 @@ TEST_F(SplitViewControllerTest, DISABLED_EnterOverviewModeTest) {
   EXPECT_EQ(split_view_controller()->GetDefaultSnappedWindow(), window1.get());
 
   ToggleOverview();
-  WaitForOverviewEnterAnimation();
-  CheckOverviewEnterExitHistogram("EnterInSplitView", {0, 1}, {0, 0});
   EXPECT_EQ(split_view_controller()->state(),
             SplitViewController::State::kPrimarySnapped);
   EXPECT_FALSE(
       base::Contains(GetWindowsInOverviewGrids(),
                      split_view_controller()->GetDefaultSnappedWindow()));
+}
+
+// Tests that if split view mode and overview mode are active at the same time,
+// i.e., half of the screen is occupied by a snapped window and half of the
+// screen is occupied by the overview windows grid, the next activatable window
+// will be picked to snap when exiting the overview mode.
+TEST_F(SplitViewControllerTest, ExitOverviewMode) {
+  const gfx::Rect bounds(0, 0, 400, 400);
+  std::unique_ptr<aura::Window> window1(CreateWindow(bounds));
+  std::unique_ptr<aura::Window> window2(CreateWindow(bounds));
+  std::unique_ptr<aura::Window> window3(CreateWindow(bounds));
+  ASSERT_FALSE(split_view_controller()->InSplitViewMode());
+
+  ToggleOverview();
+  split_view_controller()->SnapWindow(
+      window1.get(), SplitViewController::SnapPosition::kPrimary);
+  EXPECT_TRUE(split_view_controller()->InSplitViewMode());
+  EXPECT_EQ(split_view_controller()->state(),
+            SplitViewController::State::kPrimarySnapped);
+  EXPECT_EQ(split_view_controller()->primary_window(), window1.get());
+
+  // Activate `window1` in preparation to verify that it stays active when
+  // overview mode is ended.
+  wm::ActivateWindow(window1.get());
+
+  ToggleOverview();
+  EXPECT_EQ(split_view_controller()->state(),
+            SplitViewController::State::kBothSnapped);
+  EXPECT_EQ(split_view_controller()->secondary_window(), window3.get());
+  EXPECT_TRUE(wm::IsActiveWindow(window1.get()));
+}
+
+// Tests that the overview mode enter exit smoothness histograms are recorded
+// properly when one window is snapped.
+// TODO(b/252523767): Reenable this test.
+TEST_F(SplitViewControllerTest, DISABLED_EnterExitOverviewModeHistograms) {
+  const gfx::Rect bounds(0, 0, 400, 400);
+  std::unique_ptr<aura::Window> window1(CreateWindow(bounds));
+  std::unique_ptr<aura::Window> window2(CreateWindow(bounds));
+  std::unique_ptr<aura::Window> window3(CreateWindow(bounds));
+
+  // Snap `window1` to the left. This will auto trigger entering overview.
+  wm::ActivateWindow(window1.get());
+  split_view_controller()->SnapWindow(
+      window1.get(), SplitViewController::SnapPosition::kPrimary);
+  split_view_controller()->SnapWindow(
+      window2.get(), SplitViewController::SnapPosition::kSecondary);
+  ASSERT_EQ(SplitViewController::State::kBothSnapped,
+            split_view_controller()->state());
+
+  ui::ScopedAnimationDurationScaleMode animation_scale(
+      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
+
+  ToggleOverview();
+  WaitForOverviewEnterAnimation();
+  CheckOverviewEnterExitHistogram("EnterInSplitView", {0, 1}, {0, 0});
 
   ToggleOverview();
   WaitForOverviewExitAnimation();
@@ -1578,7 +1590,7 @@ TEST_F(SplitViewControllerTest, StartDraggingDividerDuringSnapAnimation) {
 
 TEST_F(SplitViewControllerTest, LongPressEntersSplitView) {
   // Tests that with no active windows, split view does not get activated.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
 
   const gfx::Rect bounds(0, 0, 400, 400);
@@ -1586,7 +1598,7 @@ TEST_F(SplitViewControllerTest, LongPressEntersSplitView) {
   wm::ActivateWindow(window1.get());
 
   // Tests that with split view gets activated with an active window.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_TRUE(split_view_controller()->InSplitViewMode());
 }
 
@@ -1609,7 +1621,7 @@ TEST_F(SplitViewControllerTest, LongPressExitsSplitView) {
   // Verify that by long pressing on the overview button tray with left snapped
   // window, split view mode gets exited and the left window (|window1|) is the
   // current active window.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
   EXPECT_FALSE(Shell::Get()->overview_controller()->InOverviewSession());
   EXPECT_EQ(window1.get(), window_util::GetActiveWindow());
@@ -1623,7 +1635,7 @@ TEST_F(SplitViewControllerTest, LongPressExitsSplitView) {
   // Verify that by long pressing on the overview button tray with right snapped
   // window, split view mode gets exited and the right window (|window1|) is the
   // current active window.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
   EXPECT_FALSE(Shell::Get()->overview_controller()->InOverviewSession());
   EXPECT_EQ(window1.get(), window_util::GetActiveWindow());
@@ -1638,7 +1650,7 @@ TEST_F(SplitViewControllerTest, LongPressExitsSplitView) {
 
   // Verify that by long pressing on the overview button tray with two snapped
   // windows, split view mode gets exited.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
   EXPECT_EQ(window1.get(), window_util::GetActiveWindow());
 
@@ -1653,7 +1665,7 @@ TEST_F(SplitViewControllerTest, LongPressExitsSplitView) {
   // Verify that by long pressing on the overview button tray with two snapped
   // windows, split view mode gets exited, and the activated window in splitview
   // is the current active window.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
   EXPECT_EQ(window2.get(), window_util::GetActiveWindow());
 }
@@ -1672,7 +1684,7 @@ TEST_F(SplitViewControllerTest, LongPressEntersSplitViewWithTransientChild) {
   wm::ActivateWindow(child.get());
 
   // Verify that long press will snap the focused transient child's parent.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_TRUE(split_view_controller()->InSplitViewMode());
   EXPECT_EQ(split_view_controller()->GetDefaultSnappedWindow(), parent.get());
 }
@@ -1700,39 +1712,56 @@ TEST_F(SplitViewControllerTest, LongPressExitsSplitViewWithTransientChild) {
   // Verify that by long pressing on the overview button tray, split view mode
   // gets exited and the window which contained |transient_child| is the
   // current active window.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
   EXPECT_FALSE(Shell::Get()->overview_controller()->InOverviewSession());
   EXPECT_EQ(right_window.get(), window_util::GetActiveWindow());
 }
 
-#if defined(NDEBUG)
-#define MAYBE_LongPressInOverviewMode LongPressInOverviewMode
-#else
-// TODO(b/264848385): Flaky on dbg configuration
-#define MAYBE_LongPressInOverviewMode DISABLED_LongPressInOverviewMode
-#endif
 // Verify that split view mode get activated when long pressing on the overview
-// button while in overview mode iff we have at least one window.
-TEST_F(SplitViewControllerTest, MAYBE_LongPressInOverviewMode) {
-  ui::ScopedAnimationDurationScaleMode anmatin_scale(
+// button while in overview mode if we have at least one window.
+TEST_F(SplitViewControllerTest, LongPressInOverviewMode) {
+  ToggleOverview();
+  ASSERT_TRUE(Shell::Get()->overview_controller()->InOverviewSession());
+  ASSERT_FALSE(split_view_controller()->InSplitViewMode());
+
+  // Nothing happens if there are no windows.
+  LongPressOnOverviewButtonTray();
+  EXPECT_FALSE(split_view_controller()->InSplitViewMode());
+  EXPECT_TRUE(Shell::Get()->overview_controller()->InOverviewSession());
+
+  std::unique_ptr<aura::Window> window = CreateAppWindow();
+  ASSERT_FALSE(Shell::Get()->overview_controller()->InOverviewSession());
+
+  ToggleOverview();
+  ASSERT_TRUE(Shell::Get()->overview_controller()->InOverviewSession());
+  ASSERT_FALSE(split_view_controller()->InSplitViewMode());
+
+  // Verify that with a window, a long press on the overview button tray will
+  // enter splitview.
+  LongPressOnOverviewButtonTray();
+  EXPECT_TRUE(split_view_controller()->InSplitViewMode());
+  EXPECT_EQ(window.get(), split_view_controller()->primary_window());
+}
+
+// Tests the overview animation smoothness histograms when using long pressing
+// the overview button.
+// TODO(b/252523767): Reenable this test.
+TEST_F(SplitViewControllerTest, DISABLED_LongPressInOverviewModeHistograms) {
+  ui::ScopedAnimationDurationScaleMode animation_scale(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
 
   ToggleOverview();
   WaitForOverviewEnterAnimation();
   ASSERT_TRUE(Shell::Get()->overview_controller()->InOverviewSession());
-  ASSERT_FALSE(split_view_controller()->InSplitViewMode());
   CheckOverviewEnterExitHistogram("EnterInTablet", {0, 0}, {0, 0});
 
   // Nothing happens if there are no windows.
-  LongPressOnOverivewButtonTray();
-  EXPECT_FALSE(split_view_controller()->InSplitViewMode());
+  LongPressOnOverviewButtonTray();
   EXPECT_TRUE(Shell::Get()->overview_controller()->InOverviewSession());
 
-  // Verify that with a window, a long press on the overview button tray will
-  // enter splitview.
-  std::unique_ptr<aura::Window> window(CreateWindow(gfx::Rect(0, 0, 400, 400)));
-  wm::ActivateWindow(window.get());
+  // Activating a window will exit overview.
+  std::unique_ptr<aura::Window> window = CreateAppWindow();
   CheckOverviewEnterExitHistogram("ExitByActivation", {0, 0}, {0, 0});
 
   ToggleOverview();
@@ -1741,10 +1770,11 @@ TEST_F(SplitViewControllerTest, MAYBE_LongPressInOverviewMode) {
   ASSERT_TRUE(Shell::Get()->overview_controller()->InOverviewSession());
   ASSERT_FALSE(split_view_controller()->InSplitViewMode());
 
-  LongPressOnOverivewButtonTray();
+  // Verify that with a window, a long press on the overview button tray will
+  // enter splitview, but with no animation.
+  LongPressOnOverviewButtonTray();
   EXPECT_TRUE(split_view_controller()->InSplitViewMode());
   EXPECT_EQ(window.get(), split_view_controller()->primary_window());
-  // This scenario should not trigger animation.
   CheckOverviewEnterExitHistogram("NoTransition", {1, 0}, {0, 0});
 }
 
@@ -1761,7 +1791,7 @@ TEST_F(SplitViewControllerTest, LongPressWithUnsnappableWindow) {
 
   // Verify split view is not activated when long press occurs when active
   // window is unsnappable.
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
 
   // Verify split view is not activated when long press occurs in overview mode
@@ -1774,7 +1804,7 @@ TEST_F(SplitViewControllerTest, LongPressWithUnsnappableWindow) {
   ASSERT_EQ(unsnappable_window.get(),
             Shell::Get()->mru_window_tracker()->BuildWindowForCycleList(
                 kActiveDesk)[0]);
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_FALSE(split_view_controller()->InSplitViewMode());
 }
 
@@ -1783,7 +1813,7 @@ TEST_F(SplitViewControllerTest, LongPressWithMinimizedWindow) {
   std::unique_ptr<aura::Window> window(CreateWindow(gfx::Rect(400, 400)));
   WindowState::Get(window.get())->Minimize();
 
-  LongPressOnOverivewButtonTray();
+  LongPressOnOverviewButtonTray();
   EXPECT_TRUE(split_view_controller()->InSplitViewMode());
 }
 
