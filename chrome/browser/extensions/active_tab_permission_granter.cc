4b378856efa8bfeb9e34d36dd933545d60cb469c
Justin Lulejian <jlulejian@chromium.org>
2023-05-01 16:30:53 +0000

    [Extensions] Allow clearing of single extension active tab.
    Refactors ActiveTabPermissionGranter::ClearActiveExtensionsAndNotify()
    to allow for passing in a single extension to have its active tab
    permission cleared in a public method.
    
    This will allow for SitePermissionsHelper to revoke this access for an
    extension in a future CL to fix this bug.
    
    Adds a unit test for the new method and also provides an explicit
    unit test for
    ActiveTabPermissionGranter::ClearActiveExtensionsAndNotify() since there
    wasn't one before.
    
    Pair: emiliapaz@chromium.org
    Bug: 1433399
    Change-Id: I23d84c1af05bbd1d6e6b315811f3bb3f9ae01ade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4462416
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137861}

--- chrome/browser/extensions/active_tab_permission_granter.cc
+++ chrome/browser/extensions/active_tab_permission_granter.cc
@@ -193,7 +193,7 @@ void ActiveTabPermissionGranter::GrantIfRequested(const Extension* extension) {
 }
 
 void ActiveTabPermissionGranter::RevokeForTesting() {
-  ClearActiveExtensionsAndNotify();
+  ClearGrantedExtensionsAndNotify();
 }
 
 void ActiveTabPermissionGranter::DidFinishNavigation(
@@ -209,11 +209,11 @@ void ActiveTabPermissionGranter::DidFinishNavigation(
   if (navigation_handle->IsSameOrigin())
     return;
 
-  ClearActiveExtensionsAndNotify();
+  ClearGrantedExtensionsAndNotify();
 }
 
 void ActiveTabPermissionGranter::WebContentsDestroyed() {
-  ClearActiveExtensionsAndNotify();
+  ClearGrantedExtensionsAndNotify();
 }
 
 void ActiveTabPermissionGranter::OnExtensionUnloaded(
@@ -225,8 +225,24 @@ void ActiveTabPermissionGranter::OnExtensionUnloaded(
   granted_extensions_.Remove(extension->id());
 }
 
-void ActiveTabPermissionGranter::ClearActiveExtensionsAndNotify() {
-  if (granted_extensions_.empty()) {
+void ActiveTabPermissionGranter::ClearGrantedExtensionsAndNotify() {
+  ClearGrantedExtensionsAndNotify(granted_extensions_);
+}
+
+void ActiveTabPermissionGranter::ClearActiveExtensionAndNotify(
+    const ExtensionId& id) {
+  if (!granted_extensions_.Contains(id)) {
+    return;
+  }
+
+  ExtensionSet granted_to_remove{};
+  granted_to_remove.Insert(granted_extensions_.GetByID(id));
+  ClearGrantedExtensionsAndNotify(granted_to_remove);
+}
+
+void ActiveTabPermissionGranter::ClearGrantedExtensionsAndNotify(
+    const ExtensionSet& granted_extensions_to_remove) {
+  if (granted_extensions_to_remove.empty()) {
     return;
   }
 
@@ -235,7 +251,8 @@ void ActiveTabPermissionGranter::ClearActiveExtensionsAndNotify() {
   content::BrowserContext* browser_context =
       web_contents()->GetBrowserContext();
   ProcessManager* process_manager = ProcessManager::Get(browser_context);
-  for (const scoped_refptr<const Extension>& extension : granted_extensions_) {
+  for (const scoped_refptr<const Extension>& extension :
+       granted_extensions_to_remove) {
     extension->permissions_data()->ClearTabSpecificPermissions(tab_id_);
     SetCorsOriginAccessList(browser_context, *extension, base::DoNothing());
 
@@ -252,7 +269,9 @@ void ActiveTabPermissionGranter::ClearActiveExtensionsAndNotify() {
       frame_hosts, web_contents()->GetPrimaryMainFrame()->GetProcess(),
       clear_message);
 
-  granted_extensions_.Clear();
+  for (const auto& id : extension_ids) {
+    granted_extensions_.Remove(id);
+  }
 }
 
 }  // namespace extensions
