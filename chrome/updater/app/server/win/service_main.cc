2becde30014f009906c8fdb709cc8375ef375d4f
Sorin Jianu <sorin@chromium.org>
2023-05-01 23:21:14 +0000

    updater: COM service needs to log the error code before exiting.
    Fixed: 1441686
    Change-Id: Idf96f14ca1b3272a5c1f6619de4ad38e86e44774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496517
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138076}

--- chrome/updater/app/server/win/service_main.cc
+++ chrome/updater/app/server/win/service_main.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <atlsecurity.h>
 #include <sddl.h>
 
+#include <ios>
 #include <string>
 #include <type_traits>
 
@@ -125,9 +126,10 @@ void ServiceMain::ServiceMainImpl(const base::CommandLine& command_line) {
   SetServiceStatus(SERVICE_RUNNING);
 
   // When the Run function returns, the service has stopped.
-  // `hr` can be either a HRESULT or a Windows error code.
+  // `hr` can be either an HRESULT or a Windows error code.
   const HRESULT hr = Run(command_line);
   if (hr != S_OK) {
+    VLOG(2) << "Run returned: " << std::hex << hr;
     service_status_.dwWin32ExitCode = ERROR_SERVICE_SPECIFIC_ERROR;
     service_status_.dwServiceSpecificExitCode = hr;
   }
