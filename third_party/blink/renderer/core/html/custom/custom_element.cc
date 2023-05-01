95fb85ad53359d3e77c8c1494fadaaf3e7081d0b
Xiaocheng Hu <xiaochengh@chromium.org>
2023-05-01 18:57:43 +0000

    [scoped-registry] Implement scoped custom element upgrade
    This patch:
    
    1. Changes CustomElement::Registry() to return tree-scoped registries
       instead of the global registry. As this function is used by a lot
       of callers (including upgrade), this allows these callers to use
       the scoped registry associated with the tree scope.
    
    2. Custom element construction stack entries are augmented with the
       definition being used, so that later when calling `super()`, we
       can still know which custom element definition is being used.
       See https://github.com/WICG/webcomponents/issues/969 for details.
    
    Bug: 1304439
    Change-Id: Id510ecea0f4c5cf6386f77a39d346918c9592e76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144367
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137942}

--- third_party/blink/renderer/core/html/custom/custom_element.cc
+++ third_party/blink/renderer/core/html/custom/custom_element.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/qualified_name.h"
+#include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/html/custom/ce_reactions_scope.h"
 #include "third_party/blink/renderer/core/html/custom/custom_element_definition.h"
@@ -23,12 +24,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 CustomElementRegistry* CustomElement::Registry(const Element& element) {
-  return Registry(element.GetDocument());
+  return Registry(element.GetTreeScope());
 }
 
-CustomElementRegistry* CustomElement::Registry(const Document& document) {
-  if (LocalDOMWindow* window = document.domWindow())
+CustomElementRegistry* CustomElement::Registry(const TreeScope& tree_scope) {
+  if (RuntimeEnabledFeatures::ScopedCustomElementRegistryEnabled()) {
+    if (const ShadowRoot* shadow = DynamicTo<ShadowRoot>(tree_scope)) {
+      if (CustomElementRegistry* registry = shadow->registry()) {
+        return registry;
+      }
+    }
+  }
+  if (LocalDOMWindow* window = tree_scope.GetDocument().domWindow()) {
     return window->customElements();
+  }
   return nullptr;
 }
 
