ad04d8930d5fc09546a90269eeea869a762d152e
Henrique Nakashima <hnakashima@chromium.org>
2023-05-01 18:36:19 +0000

    [Android] Remove ShadowChromeFeatureList from OptionalNewTabButtonControllerActivityTest
    This shadow doesn't do anything - it's not included in @Config.
    
    Bug: 1199025
    Change-Id: I755afbd8b15653fe082b15f7166a29eb5ba8e1ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496410
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137929}

--- chrome/android/junit/src/org/chromium/chrome/browser/toolbar/adaptive/OptionalNewTabButtonControllerActivityTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/toolbar/adaptive/OptionalNewTabButtonControllerActivityTest.java
@@ -19,7 +19,6 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.filters.MediumTest;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -52,9 +51,7 @@ import org.chromium.ui.display.DisplayAndroidManager;
 import org.chromium.url.JUnitTestGURLs;
 import org.chromium.url.ShadowGURL;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 /**
@@ -100,28 +97,6 @@ public class OptionalNewTabButtonControllerActivityTest {
         }
     }
 
-    // TODO(crbug.com/1199025): Remove this shadow.
-    @Implements(ChromeFeatureList.class)
-    static class ShadowChromeFeatureList {
-        private static final Map<String, String> sParamValues = new HashMap<>();
-
-        @Implementation
-        public static String getFieldTrialParamByFeature(String feature, String paramKey) {
-            Assert.assertTrue(ChromeFeatureList.isEnabled(feature));
-            return sParamValues.getOrDefault(paramKey, "");
-        }
-
-        @Implementation
-        public static boolean isEnabled(String featureName) {
-            return featureName.equals(
-                    ChromeFeatureList.ADAPTIVE_BUTTON_IN_TOP_TOOLBAR_CUSTOMIZATION_V2);
-        }
-
-        public static void reset() {
-            sParamValues.clear();
-        }
-    }
-
     private ActivityScenario<ChromeTabbedActivity> mActivityScenario;
     private AdaptiveToolbarButtonController mAdaptiveButtonController;
     private MockTab mTab;
@@ -164,7 +139,6 @@ public class OptionalNewTabButtonControllerActivityTest {
 
     private static void resetStaticState() {
         ShadowDelegate.reset();
-        ShadowChromeFeatureList.reset();
         // DisplayAndroidManager will reuse the Display between tests. This can cause
         // AsyncInitializationActivity#applyOverrides to set incorrect smallestWidth.
         DisplayAndroidManager.resetInstanceForTesting();
