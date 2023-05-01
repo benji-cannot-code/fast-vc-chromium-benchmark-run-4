d114af8b6d85a96dd3f742675b1fa92b84f164c5
Clement Yan <clamclamyan@google.com>
2023-05-01 02:45:38 +0000

    [WebUI] Enable trusted types in smb dialogs
    TEST = verified manually at chrome://smb=credentials-dialog & chrome://smb-share-dialog
    
    Bug: 1400799
    Change-Id: I6e2d4c17c43facfe2b6793a39305d42a98529d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4486019
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1137723}

--- chrome/browser/ui/webui/ash/smb_shares/smb_credentials_dialog.cc
+++ chrome/browser/ui/webui/ash/smb_shares/smb_credentials_dialog.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/values.h"
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_handler.h"
+#include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
@@ -116,8 +117,7 @@ SmbCredentialsDialogUI::SmbCredentialsDialogUI(content::WebUI* web_ui)
     : ui::WebDialogUI(web_ui) {
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       Profile::FromWebUI(web_ui), chrome::kChromeUISmbCredentialsHost);
-
-  source->DisableTrustedTypesCSP();
+  webui::EnableTrustedTypesCSP(source);
 
   AddSmbCredentialsDialogStrings(source);
 
--- chrome/browser/ui/webui/ash/smb_shares/smb_share_dialog.cc
+++ chrome/browser/ui/webui/ash/smb_shares/smb_share_dialog.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_handler.h"
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_shares_localized_strings_provider.h"
+#include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
@@ -71,8 +72,7 @@ SmbShareDialogUI::SmbShareDialogUI(content::WebUI* web_ui)
     : ui::WebDialogUI(web_ui) {
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       Profile::FromWebUI(web_ui), chrome::kChromeUISmbShareHost);
-
-  source->DisableTrustedTypesCSP();
+  webui::EnableTrustedTypesCSP(source);
 
   AddSmbSharesStrings(source);
 
