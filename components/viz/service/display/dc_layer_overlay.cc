77e23ebe33041681c7fea048776d4c5ed6e53fb5
Saifuddin Hitawala <hitawala@chromium.org>
2023-05-01 18:37:32 +0000

    Multiplanar: Update YUV overlay params in TextureDrawQuad if multiplanar
    For multiplanar shared images, we now create a TextureDrawQuad instead
    of YUVDrawQuad as we have a single shared image and multiple textures
    with one for each plane inside the shared image. As part of this,
    we need to update DCLayerOverlay to update YUV overlay params for
    multiplanar formats for TextureDrawQuads similar to how its done
    for YUVDrawQuads. Once the feature is shipped we can probably get
    rid of YUVDrawQuads if all its usages are removed.
    
    Bug: 1366495
    Change-Id: I37b3ffd73591ab55ed95e9bdc0f7eebe74ef49e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482223
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137930}

--- components/viz/service/display/dc_layer_overlay.cc
+++ components/viz/service/display/dc_layer_overlay.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/overlay_processor_interface.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/config/gpu_finch_features.h"
+#include "media/base/media_switches.h"
 #include "media/base/win/mf_feature_checks.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
@@ -73,6 +74,45 @@ gfx::RectF GetExpandedRectWithPixelMovingFilter(
   // expanded_rect in the target space
   return cc::MathUtil::MapClippedRect(
       shared_quad_state->quad_to_target_transform, expanded_rect);
+}
+
+DCLayerResult ValidateYUVOverlay(
+    const gfx::ProtectedVideoType& protected_video_type,
+    const gfx::ColorSpace& video_color_space,
+    const absl::optional<gfx::HDRMetadata>& hdr_metadata,
+    bool has_overlay_support,
+    int allowed_yuv_overlay_count,
+    int processed_yuv_overlay_count) {
+  // Note: Do not override this value based on base::Feature values. It is the
+  // result after the GPU blocklist has been consulted.
+  if (!has_overlay_support) {
+    return DC_LAYER_FAILED_UNSUPPORTED_QUAD;
+  }
+
+  // Hardware protected video must use Direct Composition Overlay
+  if (protected_video_type == gfx::ProtectedVideoType::kHardwareProtected) {
+    return DC_LAYER_SUCCESS;
+  }
+
+  if (processed_yuv_overlay_count >= allowed_yuv_overlay_count) {
+    return DC_LAYER_FAILED_TOO_MANY_OVERLAYS;
+  }
+
+  // HLG shouldn't have the hdr metadata, but we don't want to promote it to
+  // overlay, as VideoProcessor doesn't support HLG tone mapping well between
+  // different gpu vendors, see: https://crbug.com/1144260#c6.
+  // Some HLG streams may carry hdr metadata, see: https://crbug.com/1429172.
+  if (video_color_space.GetTransferID() == gfx::ColorSpace::TransferID::HLG) {
+    return DC_LAYER_FAILED_YUV_VIDEO_QUAD_HLG;
+  }
+
+  // Otherwise, it could be a parser bug like https://crbug.com/1362288 if the
+  // hdr metadata is still missing. We shouldn't promote too for that case.
+  if (video_color_space.IsHDR() && !hdr_metadata.has_value()) {
+    return DC_LAYER_FAILED_YUV_VIDEO_QUAD_NO_HDR_METADATA;
+  }
+
+  return DC_LAYER_SUCCESS;
 }
 
 DCLayerResult ValidateYUVQuad(
@@ -173,6 +213,9 @@ void FromYUVQuad(const YUVVideoDrawQuad* quad,
 DCLayerResult ValidateTextureQuad(
     const TextureDrawQuad* quad,
     const std::vector<gfx::Rect>& backdrop_filter_rects,
+    bool has_overlay_support,
+    int allowed_yuv_overlay_count,
+    int processed_yuv_overlay_count,
     DisplayResourceProvider* resource_provider) {
   // Check that resources are overlay compatible first so that subsequent
   // assumptions are valid.
@@ -195,11 +238,24 @@ DCLayerResult ValidateTextureQuad(
       return DC_LAYER_FAILED_BACKDROP_FILTERS;
   }
 
+  auto si_format = resource_provider->GetSharedImageFormat(quad->resource_id());
+  if (media::IsMultiPlaneFormatForHardwareVideoEnabled() &&
+      si_format.is_multi_plane()) {
+    auto color_space =
+        resource_provider->GetOverlayColorSpace(quad->resource_id());
+    auto result = ValidateYUVOverlay(quad->protected_video_type, color_space,
+                                     quad->hdr_metadata, has_overlay_support,
+                                     allowed_yuv_overlay_count,
+                                     processed_yuv_overlay_count);
+    return result;
+  }
+
   return DC_LAYER_SUCCESS;
 }
 
 void FromTextureQuad(const TextureDrawQuad* quad,
                      const gfx::Transform& transform_to_root_target,
+                     DisplayResourceProvider* resource_provider,
                      OverlayCandidate* dc_layer) {
   dc_layer->resource_id = quad->resource_id();
   dc_layer->plane_z_order = 1;
@@ -230,8 +286,8 @@ void FromTextureQuad(const TextureDrawQuad* quad,
         quad->shared_quad_state->clip_rect.value_or(gfx::Rect()));
   }
 
-  dc_layer->color_space = gfx::ColorSpace::CreateSRGB();
-
+  dc_layer->color_space =
+      resource_provider->GetOverlayColorSpace(quad->resource_id());
   // Both color space and protected_video_type are hard-coded for stream video.
   // TODO(crbug.com/1384544): Consider using quad->protected_video_type.
   if (quad->is_stream_video) {
@@ -721,28 +777,14 @@ void DCLayerOverlayProcessor::Process(
 
     gpu::Mailbox promotion_hint_mailbox;
     DCLayerResult result;
+    bool is_yuv_overlay = false;
     switch (it->material) {
       case DrawQuad::Material::kYuvVideoContent:
         result = ValidateYUVQuad(
             YUVVideoDrawQuad::MaterialCast(*it), backdrop_filter_rects,
             has_overlay_support_, allowed_yuv_overlay_count_,
             processed_yuv_overlay_count_, resource_provider);
-        yuv_quads_in_quad_list++;
-
-        if (no_undamaged_overlay_promotion_) {
-          if (it->shared_quad_state->overlay_damage_index.has_value() &&
-              !surface_damage_rect_list_[it->shared_quad_state
-                                             ->overlay_damage_index.value()]
-                   .IsEmpty()) {
-            damaged_yuv_quads_in_quad_list++;
-            if (result == DC_LAYER_SUCCESS)
-              processed_yuv_overlay_count_++;
-          }
-        } else {
-          if (result == DC_LAYER_SUCCESS)
-            processed_yuv_overlay_count_++;
-        }
-
+        is_yuv_overlay = true;
         break;
       case DrawQuad::Material::kTextureContent: {
         const TextureDrawQuad* tex_quad = TextureDrawQuad::MaterialCast(*it);
@@ -752,8 +794,16 @@ void DCLayerOverlayProcessor::Process(
           // always presented as overlay.
           result = DC_LAYER_SUCCESS;
         } else {
-          result = ValidateTextureQuad(tex_quad, backdrop_filter_rects,
-                                       resource_provider);
+          result = ValidateTextureQuad(
+              tex_quad, backdrop_filter_rects, has_overlay_support_,
+              allowed_yuv_overlay_count_, processed_yuv_overlay_count_,
+              resource_provider);
+        }
+        auto si_format =
+            resource_provider->GetSharedImageFormat(tex_quad->resource_id());
+        if (media::IsMultiPlaneFormatForHardwareVideoEnabled() &&
+            si_format.is_multi_plane()) {
+          is_yuv_overlay = true;
         }
 
         if (allow_promotion_hinting_) {
@@ -767,6 +817,25 @@ void DCLayerOverlayProcessor::Process(
       } break;
       default:
         result = DC_LAYER_FAILED_UNSUPPORTED_QUAD;
+    }
+
+    if (is_yuv_overlay) {
+      yuv_quads_in_quad_list++;
+      if (no_undamaged_overlay_promotion_) {
+        if (it->shared_quad_state->overlay_damage_index.has_value() &&
+            !surface_damage_rect_list_[it->shared_quad_state
+                                           ->overlay_damage_index.value()]
+                 .IsEmpty()) {
+          damaged_yuv_quads_in_quad_list++;
+          if (result == DC_LAYER_SUCCESS) {
+            processed_yuv_overlay_count_++;
+          }
+        }
+      } else {
+        if (result == DC_LAYER_SUCCESS) {
+          processed_yuv_overlay_count_++;
+        }
+      }
     }
 
     if (!promotion_hint_mailbox.IsZero()) {
@@ -898,7 +967,7 @@ void DCLayerOverlayProcessor::Process(
         HasOccludingDamageRect(it->shared_quad_state, surface_damage_rect_list_,
                                quad_rectangle_in_root_space);
 
-    UpdateDCLayerOverlays(display_rect, render_pass, it,
+    UpdateDCLayerOverlays(resource_provider, display_rect, render_pass, it,
                           quad_rectangle_in_root_space, is_overlay, &prev_it,
                           &prev_index, damage_rect, dc_layer_overlays,
                           is_page_fullscreen_mode);
@@ -979,6 +1048,7 @@ bool DCLayerOverlayProcessor::ShouldSkipOverlay(
 }
 
 void DCLayerOverlayProcessor::UpdateDCLayerOverlays(
+    DisplayResourceProvider* resource_provider,
     const gfx::RectF& display_rect,
     AggregatedRenderPass* render_pass,
     const QuadList::Iterator& it,
@@ -1004,10 +1074,17 @@ void DCLayerOverlayProcessor::UpdateDCLayerOverlays(
                   render_pass->transform_to_root_target, &dc_layer);
       processed_yuv_overlay_count_++;
       break;
-    case DrawQuad::Material::kTextureContent:
-      FromTextureQuad(TextureDrawQuad::MaterialCast(*it),
-                      render_pass->transform_to_root_target, &dc_layer);
-      break;
+    case DrawQuad::Material::kTextureContent: {
+      const TextureDrawQuad* tex_quad = TextureDrawQuad::MaterialCast(*it);
+      FromTextureQuad(tex_quad, render_pass->transform_to_root_target,
+                      resource_provider, &dc_layer);
+      auto si_format =
+          resource_provider->GetSharedImageFormat(tex_quad->resource_id());
+      if (media::IsMultiPlaneFormatForHardwareVideoEnabled() &&
+          si_format.is_multi_plane()) {
+        processed_yuv_overlay_count_++;
+      }
+    } break;
     default:
       NOTREACHED();
   }
