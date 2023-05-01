6403f9be669363b9224c03b4d87cd618d2ee6590
Philip Rogers <pdr@chromium.org>
2023-05-01 19:29:21 +0000

    Reland "Remove LazyImageLoading feature/REF"
    This is a reland of https://crrev.com/1137356 which was reverted in
    https://crrev.com/1137373 due to a compile failure. The compile failure
    was due to a conflict with another patch that landed at the same time.
    
    Original change description is below:
    This patch removes the feature flags (both a content feature and a
    blink runtime enabled feature) for lazy image loading, which has been
    enabled by default for many years. There's some non-trivial test
    cleanup because we used to have a feature that automatically applied
    lazy loading to images, which required testing both regular (developer
    controlled lazy loading) and automatic lazy loading. This patch is
    cleanup with no change in behavior.
    
    Bug: 1441062
    Change-Id: Ie85b1c1ccd5fffcc96ec8a2a0ca3c223a5c436c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492090
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1137952}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -2030,8 +2030,7 @@ FrameNavigationDisabler::~FrameNavigationDisabler() {
 
 LocalFrame::LazyLoadImageSetting LocalFrame::GetLazyLoadImageSetting() const {
   DCHECK(GetSettings());
-  if (!RuntimeEnabledFeatures::LazyImageLoadingEnabled() ||
-      !GetSettings()->GetLazyLoadEnabled()) {
+  if (!GetSettings()->GetLazyLoadEnabled()) {
     return LocalFrame::LazyLoadImageSetting::kDisabled;
   }
 
