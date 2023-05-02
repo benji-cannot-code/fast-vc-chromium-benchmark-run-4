16385722e9af12f44160f456665fea6044025c61
Adam Langley <agl@chromium.org>
2023-05-02 19:38:22 +0000

    Add TLS Kyber support in chrome://flags
    This change adds the ability to enable post-quantum TLS 1.3 via
    chrome://flags/#enable-tls13-kyber. There are not currently any known
    TLS servers that support this with the code point that Chromium is
    using, but hopefully soon.
    
    Change-Id: I28244a24f3fb59c1ea72bc3482ba7cc2cc2dbefd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4499393
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138575}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -597,6 +597,9 @@ extern const char kEnablePreinstalledWebAppDuplicationFixerDescription[];
 extern const char kEnableTLS13EarlyDataName[];
 extern const char kEnableTLS13EarlyDataDescription[];
 
+extern const char kEnableTLS13KyberName[];
+extern const char kEnableTLS13KyberDescription[];
+
 extern const char kAccessibilityAcceleratorNotificationsTimeoutName[];
 extern const char kAccessibilityAcceleratorNotificationsTimeoutDescription[];
 
