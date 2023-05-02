521c3311160e791548ceb7351d4c9c143dca1563
Tibor Goldschwendt <tiborg@chromium.org>
2023-05-02 16:03:40 +0000

    [ntp] Add shadow to OGB when scrim removed
    When a background image is shown the OGB will have white text and this
    CL adds a shadow, assuring contrast in all background image scenarios.
    
    NtpRemoveScrim enabled: http://screenshot/AzUcqUDvPCH7Hkz
    NtpRemoveScrim disabled: http://screenshot/8hGrNkhwFDQwyYc
    
    Bug: 1349994
    Change-Id: Iec18b9214c5a9e16e15dbccf1914d17225aea867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496730
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138398}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -308,6 +308,12 @@ export class AppElement extends AppElementBase {
         reflectToAttribute: true,
       },
 
+      showOneGoogleBarScrim_: {
+        type: Boolean,
+        computed:
+            'computeShowOneGoogleBarScrim_(removeScrim_, showBackgroundImage_)',
+      },
+
       showLensUploadDialog_: Boolean,
 
       /**
@@ -320,7 +326,7 @@ export class AppElement extends AppElementBase {
 
   static get observers() {
     return [
-      'udpateOneGoogleBarAppearance_(oneGoogleBarLoaded_, removeScrim_, showBackgroundImage_, theme_)',
+      'udpateOneGoogleBarAppearance_(oneGoogleBarLoaded_, theme_)',
     ];
   }
 
@@ -488,10 +494,6 @@ export class AppElement extends AppElementBase {
         type: 'updateAppearance',
         // We should be using a light OGB for dark themes and vice versa.
         applyLightTheme: isNtpDarkTheme,
-        // Only apply background protection if using a custom background in
-        // combination with a light OGB theme.
-        applyBackgroundProtection:
-            this.removeScrim_ && this.showBackgroundImage_ && isNtpDarkTheme,
       });
     }
   }
@@ -524,6 +526,10 @@ export class AppElement extends AppElementBase {
     return (!loadTimeData.getBoolean('middleSlotPromoEnabled') ||
             this.middleSlotPromoLoaded_) &&
         (!loadTimeData.getBoolean('modulesEnabled') || this.modulesLoaded_);
+  }
+
+  private computeShowOneGoogleBarScrim_(): boolean {
+    return this.removeScrim_ && this.showBackgroundImage_;
   }
 
   private async onLazyRendered_() {
