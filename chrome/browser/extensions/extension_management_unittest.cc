66a572fa4cf9b884a15cdbcc6f36713a31e85d56
Owen Min <zmin@chromium.org>
2023-05-02 01:42:05 +0000

    Update ExtensionManifestV2Availability policy last option
    The last option ENABLE_FOR_FORCED_EXTENSIONS is used to only working
    when manifest v2->3 migration is rolled out by default.
    
    With this change, the option will take effort when manifest v2 is
    still enabled by default.
    
    This is a behavior change that will be mentioned in the release notes.
    It's acceptable because v2->3 migration has not started yet and the
    option is currently no-op.
    
    Bug: b/280328085
    Change-Id: Ie068261875ecc172b67316ee7a6aca22906786e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4493713
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138132}

--- chrome/browser/extensions/extension_management_unittest.cc
+++ chrome/browser/extensions/extension_management_unittest.cc
@@ -1218,9 +1218,6 @@ TEST_F(ExtensionManagementServiceTest, ManifestV2Enabled) {
 }
 
 TEST_F(ExtensionManagementServiceTest, ManifestV2EnabledForForceInstalled) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      extensions_features::kExtensionsManifestV3Only);
   SetPref(
       true, pref_names::kManifestV2Availability,
       base::Value(static_cast<int>(internal::GlobalSettings::ManifestV2Setting::
