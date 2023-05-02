705b1612c734e04d022f0ff1d708b1d055301de2
Kent Tamura <tkent@chromium.org>
2023-05-02 04:19:46 +0000

    Remove two LayoutMedia runtime flags
    * LayoutMediaNGContainer
      It has been enabled by default since M111.
    
    * LayoutMediaNoInlineChildren
      It has been enabled by default since M112.
    
    This CL has no behavior changes.
    
    Bug: 1353190, 1379779
    Change-Id: I5ebb1b18f24112fed6162f883baa4c9d49bd97e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496129
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138158}

--- third_party/blink/renderer/core/layout/layout_media.h
+++ third_party/blink/renderer/core/layout/layout_media.h
@@ -74,8 +74,6 @@ class LayoutMedia : public LayoutImage {
   LayoutUnit ComputePanelWidth(const LayoutRect& media_width) const;
 
  protected:
-  void UpdateLayout() override;
-
   bool IsOfType(LayoutObjectType type) const override {
     NOT_DESTROYED();
     return type == kLayoutObjectMedia || LayoutImage::IsOfType(type);
