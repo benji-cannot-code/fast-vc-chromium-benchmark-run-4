3e97b28f92ce9ef0e795ea04057fd033bdcf9b34
Austin Sullivan <asully@chromium.org>
2023-05-02 00:57:02 +0000

    FSA: Allow the getUniqueId() method to raise an exception
    Previously, we returned an empty string if the mojo pipe was
    disconnected. This is not an ideal behavior. Instead, we'll reject
    with an invalid state error. This also opens the door to adding other
    logic that may throw exceptions, such as access checks.
    
    Also adds a bunch of TODOs pointing to https://crbug.com/1327821 in
    case mojo ever adds something like base::expected *fingers crossed*
    
    Bug: 1342961, 1413551
    Change-Id: Id628144b3131234b4d35698acd7cf4d68408e5a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492812
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138120}

--- third_party/blink/renderer/modules/file_system_access/file_system_handle.h
+++ third_party/blink/renderer/modules/file_system_access/file_system_handle.h
@@ -62,7 +62,7 @@ class FileSystemHandle : public ScriptWrappable, public ExecutionContextClient {
   ScriptPromise isSameEntry(ScriptState*,
                             FileSystemHandle* other,
                             ExceptionState&);
-  ScriptPromise getUniqueId(ScriptState*);
+  ScriptPromise getUniqueId(ScriptState*, ExceptionState&);
 
   // Grab a handle to a transfer token. This may return an invalid PendingRemote
   // if the context is already destroyed.
@@ -91,7 +91,8 @@ class FileSystemHandle : public ScriptWrappable, public ExecutionContextClient {
       base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr,
                               bool)>) = 0;
   virtual void GetUniqueIdImpl(
-      base::OnceCallback<void(const WTF::String&)>) = 0;
+      base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr,
+                              const WTF::String&)>) = 0;
 
   String name_;
 };
