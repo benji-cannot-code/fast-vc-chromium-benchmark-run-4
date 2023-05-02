32d56ba2c7ac2d2c652f97e44bbf0cc82e8029e9
Sammie Quon <sammiequon@chromium.org>
2023-05-02 00:40:32 +0000

    overview: Minor cleanup to OverviewItem
    Remove some dead code and move some testing only functions to
    OverviewTestBase, and update some comments.
    
    Test: existing
    Bug: none
    Change-Id: I39868ea1b395e01dbcbd39a65caa6dd33ceaea38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492091
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138115}

--- ash/wm/overview/overview_test_base.cc
+++ ash/wm/overview/overview_test_base.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
 #include "ash/style/close_button.h"
+#include "ash/style/system_shadow.h"
 #include "ash/test_shell_delegate.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_grid.h"
@@ -127,6 +128,23 @@ views::View* OverviewTestBase::GetBackdropView(OverviewItem* item) {
 
 WindowPreviewView* OverviewTestBase::GetPreviewView(OverviewItem* item) {
   return item->overview_item_view_->preview_view();
+}
+
+gfx::Rect OverviewTestBase::GetShadowBounds(OverviewItem* item) const {
+  SystemShadow* shadow = item->shadow_.get();
+  if (!shadow || !shadow->GetLayer()->visible()) {
+    return gfx::Rect();
+  }
+
+  return shadow->GetContentBounds();
+}
+
+views::Widget* OverviewTestBase::GetCannotSnapWidget(OverviewItem* item) {
+  return item->cannot_snap_widget_.get();
+}
+
+void OverviewTestBase::SetAnimatingToClose(OverviewItem* item, bool val) {
+  item->animating_to_close_ = val;
 }
 
 float OverviewTestBase::GetCloseButtonOpacity(OverviewItem* item) {
