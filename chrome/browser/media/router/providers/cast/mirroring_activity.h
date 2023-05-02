3d3af21db43b5a5b48bf321899b4818fdf17b12d
George Benz <gbj@google.com>
2023-05-02 21:21:53 +0000

    Adds command line switch for cast mirroring target playout delay.
    Adds a command line switch that, if enabled, will override any target playout delay for a mirroring session. This switch is used right now for debugging purposes but will eventually be turned into a multi-option chrome://flags option that will be exposed to the general public.
    
    Bug: b/280320180
    Change-Id: Iad2973f313cf87c98276ab625fb959b40a5b6584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496118
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: george benz <gbj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138645}

--- chrome/browser/media/router/providers/cast/mirroring_activity.h
+++ chrome/browser/media/router/providers/cast/mirroring_activity.h
@@ -129,6 +129,7 @@ class MirroringActivity : public CastActivity,
   FRIEND_TEST_ALL_PREFIXES(MirroringActivityTest, Pause);
   FRIEND_TEST_ALL_PREFIXES(MirroringActivityTest, Play);
   FRIEND_TEST_ALL_PREFIXES(MirroringActivityTest, OnRemotingStateChanged);
+  FRIEND_TEST_ALL_PREFIXES(MirroringActivityTest, GetTargetPlayoutDelay);
 
   void HandleParseJsonResult(const std::string& route_id,
                              data_decoder::DataDecoder::ValueOrError result);
@@ -162,6 +163,11 @@ class MirroringActivity : public CastActivity,
   void ScheduleFetchMirroringStats();
   void FetchMirroringStats();
   void OnMirroringStats(base::Value json_stats);
+
+  // Checks if we should override the target playout delay if the
+  // kCastMirroringTargetPlayoutDelay switch has a value.
+  absl::optional<base::TimeDelta> GetTargetPlayoutDelay(
+      const absl::optional<base::TimeDelta>& source_playout_delay);
 
   std::unique_ptr<mirroring::MirroringServiceHost> host_;
 
