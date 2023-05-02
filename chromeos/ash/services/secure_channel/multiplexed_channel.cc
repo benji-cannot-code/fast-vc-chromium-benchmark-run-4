41e1c821557551dc268d18baf22fbeabf4ef5f8d
Claudio DeSouza <cdesouza@igalia.com>
2023-05-02 20:31:01 +0000

    Replace base::GUID uses with base::Uuid (217/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chromeos/ash/services/secure_channel
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: Icb3bfc2ab8bedf80385cc785096967bd31c438e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482446
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138610}

--- chromeos/ash/services/secure_channel/multiplexed_channel.cc
+++ chromeos/ash/services/secure_channel/multiplexed_channel.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/secure_channel/multiplexed_channel.h"
 
 #include "base/check.h"
-#include "base/guid.h"
 
 namespace ash::secure_channel {
 
