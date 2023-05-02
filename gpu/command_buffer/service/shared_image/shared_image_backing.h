ea51a0763dd02827c6c2e32e5aa97165cb731747
Saifuddin Hitawala <hitawala@chromium.org>
2023-05-02 16:45:40 +0000

    Graphite: Add ProduceSkiaGraphite on SharedImageBacking
    This change adds ProduceSkiaGraphite method on SharedImageBacking
    that will later be implemented for backings needing Graphite support.
    
    Bug: 1430206
    Change-Id: Id2de085f733b3bb3b966605d03d164f1eff12c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480449
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138436}

--- gpu/command_buffer/service/shared_image/shared_image_backing.h
+++ gpu/command_buffer/service/shared_image/shared_image_backing.h
@@ -53,6 +53,7 @@ class SharedImageRepresentation;
 class GLTextureImageRepresentation;
 class GLTexturePassthroughImageRepresentation;
 class SkiaGaneshImageRepresentation;
+class SkiaGraphiteImageRepresentation;
 class SkiaImageRepresentation;
 class DawnImageRepresentation;
 class LegacyOverlayImageRepresentation;
@@ -253,6 +254,12 @@ class GPU_GLES2_EXPORT SharedImageBacking {
   // Returns a SkiaGaneshImageRepresentation created using the Skia Ganesh
   // backend.
   virtual std::unique_ptr<SkiaGaneshImageRepresentation> ProduceSkiaGanesh(
+      SharedImageManager* manager,
+      MemoryTypeTracker* tracker,
+      scoped_refptr<SharedContextState> context_state);
+  // Returns a SkiaGraphiteImageRepresentation created using the Skia Graphite
+  // backend.
+  virtual std::unique_ptr<SkiaGraphiteImageRepresentation> ProduceSkiaGraphite(
       SharedImageManager* manager,
       MemoryTypeTracker* tracker,
       scoped_refptr<SharedContextState> context_state);
