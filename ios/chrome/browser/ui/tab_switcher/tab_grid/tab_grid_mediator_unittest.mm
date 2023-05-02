fc3193232248d9f57146668277e0ab3de3c1a047
Samar <samarchehade@google.com>
2023-05-02 16:10:26 +0000

    [Signin][iOS] Add AccessPoint to signin method in tab_switcher unit tests.
    The goal is to update all affected unit tests in order to eventually remove the default parameter in the sign-in method.
    
    Bug: 1261772
    Change-Id: I0f9afd0bfdbb9b7e5e6f795665db97d6111a54e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4498436
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138402}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator_unittest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator_unittest.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sessions/core/session_id.h"
 #import "components/sessions/core/tab_restore_service.h"
 #import "components/sessions/core/tab_restore_service_helper.h"
+#import "components/signin/public/base/signin_metrics.h"
 #import "components/sync_preferences/testing_pref_service_syncable.h"
 #import "components/unified_consent/pref_names.h"
 #import "ios/chrome/browser/application_context/application_context.h"
@@ -148,7 +149,8 @@ class TabGridMediatorTest : public PlatformTest {
     auth_service_ = static_cast<AuthenticationService*>(
         AuthenticationServiceFactory::GetInstance()->GetForBrowserState(
             browser_state_.get()));
-    auth_service_->SignIn(identity);
+    auth_service_->SignIn(identity,
+                          signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN);
 
     tab_restore_service_ =
         IOSChromeTabRestoreServiceFactory::GetForBrowserState(
