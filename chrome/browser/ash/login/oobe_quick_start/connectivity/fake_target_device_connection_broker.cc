9a94ed9f8bf611240d3218319eebb88b56d60c7f
Joe Antonetti <joeantonetti@google.com>
2023-05-02 17:51:19 +0000

    [QuickStart][2/n] Move FakeQuickStartDecoder to shared directory
    This is part of a series of changes to thread QuickStartDecoder down to
    Connection's constructor.
    
    To resolve a future dependency cycle with TargetDeviceBootstrapController,
    move FakeQuickStartDecoder down to a more accessible directory.
    
    Test: Updated existing tests
    Bug: b/280330703
    Change-Id: I3e1112d4945cf15ea964f010f7fd809695957d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4494322
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138512}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/fake_target_device_connection_broker.cc
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/fake_target_device_connection_broker.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/ash/login/oobe_quick_start/connectivity/fake_quick_start_decoder.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/random_session_id.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/target_device_connection_broker.h"
 #include "chrome/browser/nearby_sharing/fake_nearby_connection.h"
 #include "chrome/browser/nearby_sharing/public/cpp/nearby_connection.h"
+#include "chromeos/ash/components/quick_start/fake_quick_start_decoder.h"
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder.mojom.h"
 
 namespace ash::quick_start {
--- chromeos/ash/components/quick_start/fake_quick_start_decoder.h
+++ chromeos/ash/components/quick_start/fake_quick_start_decoder.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_LOGIN_OOBE_QUICK_START_CONNECTIVITY_FAKE_QUICK_START_DECODER_H_
-#define CHROME_BROWSER_ASH_LOGIN_OOBE_QUICK_START_CONNECTIVITY_FAKE_QUICK_START_DECODER_H_
+#ifndef CHROMEOS_ASH_COMPONENTS_QUICK_START_FAKE_QUICK_START_DECODER_H_
+#define CHROMEOS_ASH_COMPONENTS_QUICK_START_FAKE_QUICK_START_DECODER_H_
 
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder.mojom.h"
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom.h"
@@ -61,4 +61,4 @@ class FakeQuickStartDecoder : public mojom::QuickStartDecoder {
 
 }  // namespace ash::quick_start
 
-#endif  // CHROME_BROWSER_ASH_LOGIN_OOBE_QUICK_START_CONNECTIVITY_FAKE_QUICK_START_DECODER_H_
+#endif  // CHROMEOS_ASH_COMPONENTS_QUICK_START_FAKE_QUICK_START_DECODER_H_
