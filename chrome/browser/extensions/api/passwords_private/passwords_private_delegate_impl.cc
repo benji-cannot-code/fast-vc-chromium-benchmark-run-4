6b25f43d149980072abd2e1e85e106148349aa9a
Elias Khsheibun <eliaskh@chromium.org>
2023-05-02 07:56:39 +0000

    [Passwords] Cleanup kPasswordManagerSystemAuthentication feature flag.
    The feature (Using native ChromeOS auth in PWM) is enabled by default and thus can be cleaned up.
    
    Bug: b:259526857
    Change-Id: Ia51ae7a77cd498656a9f9bcb4df7a8090008b251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4476811
    Commit-Queue: Elias Khsheibun <eliaskh@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138203}

--- chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl.cc
+++ chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl.cc
@@ -313,10 +313,11 @@ PasswordsPrivateDelegateImpl::~PasswordsPrivateDelegateImpl() {
 
 void PasswordsPrivateDelegateImpl::GetSavedPasswordsList(
     UiEntriesCallback callback) {
-  if (current_entries_initialized_)
+  if (current_entries_initialized_) {
     std::move(callback).Run(current_entries_);
-  else
+  } else {
     get_saved_passwords_list_callbacks_.push_back(std::move(callback));
+  }
 }
 
 PasswordsPrivateDelegate::CredentialsGroups
@@ -344,10 +345,11 @@ PasswordsPrivateDelegateImpl::GetCredentialGroups() {
 
 void PasswordsPrivateDelegateImpl::GetPasswordExceptionsList(
     ExceptionEntriesCallback callback) {
-  if (current_entries_initialized_)
+  if (current_entries_initialized_) {
     std::move(callback).Run(current_exceptions_);
-  else
+  } else {
     get_password_exception_list_callbacks_.push_back(std::move(callback));
+  }
 }
 
 absl::optional<api::passwords_private::UrlCollection>
@@ -408,8 +410,9 @@ absl::optional<int> PasswordsPrivateDelegateImpl::ChangeSavedPassword(
     const api::passwords_private::ChangeSavedPasswordParams& params) {
   const CredentialUIEntry* original_credential =
       credential_id_generator_.TryGetKey(id);
-  if (!original_credential)
+  if (!original_credential) {
     return absl::nullopt;
+  }
 
   CredentialUIEntry updated_credential = *original_credential;
   updated_credential.username = base::UTF8ToUTF16(params.username);
@@ -524,20 +527,8 @@ void PasswordsPrivateDelegateImpl::OsReauthCall(
 #elif BUILDFLAG(IS_MAC)
   AuthenticateUser(password_manager_util_mac::GetMessageForLoginPrompt(purpose),
                    std::move(callback));
-#elif BUILDFLAG(IS_CHROMEOS_ASH)
-  if (chromeos::features::IsPasswordManagerSystemAuthenticationEnabled()) {
-    AuthenticateUser(std::u16string(), std::move(callback));
-  } else {
-    bool result =
-        IsOsReauthAllowedAsh(profile_, GetAuthTokenLifetimeForPurpose(purpose));
-    std::move(callback).Run(result);
-  }
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-  if (chromeos::features::IsPasswordManagerSystemAuthenticationEnabled()) {
-    AuthenticateUser(std::u16string(), std::move(callback));
-  } else {
-    IsOsReauthAllowedLacrosAsync(purpose, std::move(callback));
-  }
+#elif BUILDFLAG(IS_CHROMEOS)
+  AuthenticateUser(std::u16string(), std::move(callback));
 #else
   std::move(callback).Run(true);
 #endif
@@ -547,8 +538,9 @@ void PasswordsPrivateDelegateImpl::OsReauthTimeoutCall() {
 #if !BUILDFLAG(IS_LINUX)
   PasswordsPrivateEventRouter* router =
       PasswordsPrivateEventRouterFactory::GetForProfile(profile_);
-  if (router)
+  if (router) {
     router->OnPasswordManagerAuthTimeout();
+  }
 #endif
 }
 
@@ -600,11 +592,13 @@ void PasswordsPrivateDelegateImpl::SetCredentials(
   current_entries_initialized_ = true;
   InitializeIfNecessary();
 
-  for (auto& callback : get_saved_passwords_list_callbacks_)
+  for (auto& callback : get_saved_passwords_list_callbacks_) {
     std::move(callback).Run(current_entries_);
+  }
   get_saved_passwords_list_callbacks_.clear();
-  for (auto& callback : get_password_exception_list_callbacks_)
+  for (auto& callback : get_password_exception_list_callbacks_) {
     std::move(callback).Run(current_exceptions_);
+  }
   get_password_exception_list_callbacks_.clear();
 }
 
@@ -979,13 +973,15 @@ void PasswordsPrivateDelegateImpl::OnWebAppInstallManagerDestroyed() {
 }
 
 void PasswordsPrivateDelegateImpl::InitializeIfNecessary() {
-  if (is_initialized_ || !current_entries_initialized_)
+  if (is_initialized_ || !current_entries_initialized_) {
     return;
+  }
 
   is_initialized_ = true;
 
-  for (base::OnceClosure& callback : pre_initialization_callbacks_)
+  for (base::OnceClosure& callback : pre_initialization_callbacks_) {
     std::move(callback).Run();
+  }
   pre_initialization_callbacks_.clear();
 }
 
--- chrome/browser/extensions/api/passwords_private/passwords_private_utils_chromeos.h
+++ chrome/browser/extensions/api/passwords_private/passwords_private_utils_chromeos.h
@@ -10,18 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "components/password_manager/core/browser/reauth_purpose.h"
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chromeos/crosapi/mojom/authentication.mojom.h"
-#include "components/password_manager/core/browser/password_access_authenticator.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
-
 class Profile;
 
 namespace extensions {
-
-// Returns the lifetime of an auth token for a given |purpose|.
-base::TimeDelta GetAuthTokenLifetimeForPurpose(
-    password_manager::ReauthPurpose purpose);
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 // Returns whether |profile| has been authorized for password access, and
@@ -30,16 +21,6 @@ base::TimeDelta GetAuthTokenLifetimeForPurpose(
 bool IsOsReauthAllowedAsh(Profile* profile,
                           base::TimeDelta auth_token_lifetime);
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-// Makes a crosapi call to determine whether the active user profile has been
-// authorized for password access, and whether the auth token is no older than a
-// |purpose|-dependent lifetime. The results are asynchronously passed to
-// |callback|.
-void IsOsReauthAllowedLacrosAsync(
-    password_manager::ReauthPurpose purpose,
-    password_manager::PasswordAccessAuthenticator::AuthResultCallback callback);
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
 }  // namespace extensions
 
