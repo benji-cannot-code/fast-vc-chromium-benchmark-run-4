7ac4801eca2059821759082859e1e04b365f1b64
Carlos Acosta <cyacosta@google.com>
2023-05-02 14:46:37 +0000

    Exposed Weak Check Utility from ChromeWebView
    Expose an API to allow weak checks of CWVPassword from the CWVWebViewConfiguration.
    
    Bug: 1440478
    
    Change-Id: If760feff0c9326aad557297e2edddfe3856831ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480797
    Commit-Queue: Carlos Acosta <cyacosta@google.com>
    Reviewed-by: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138348}

---
+++ ios/web_view/internal/passwords/cwv_weak_check_utils.mm
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "base/strings/sys_string_conversions.h"
+#import "components/password_manager/core/browser/ui/weak_check_utility.h"
+#import "ios/web_view/internal/passwords/cwv_password_internal.h"
+#import "ios/web_view/internal/passwords/cwv_weak_check_utils_internal.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation CWVWeakCheckUtils
+
+- (instancetype)init {
+  self = [super init];
+  return self;
+}
+
++ (BOOL)isPasswordWeak:(NSString*)password {
+  std::u16string passwordToCheck = base::SysNSStringToUTF16(password);
+  return password_manager::IsWeak(passwordToCheck).value();
+}
+
+@end
