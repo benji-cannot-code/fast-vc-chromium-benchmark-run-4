ff0fd3a161f16ba7b7e2dcc6c341361e97b87fef
Ella Ge <eirage@chromium.org>
2023-05-02 16:25:12 +0000

    Reenable test launchesWebApkSiteSettings
    Test Reviver is happy with the test
    
    Bug: 1333222,1283285
    Change-Id: Ia02f644a95788965a44fd6837e9120a6036cb46b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4494418
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138417}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/ManageTrustedWebActivityDataActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/ManageTrustedWebActivityDataActivityTest.java
@@ -18,10 +18,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Matchers;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -36,6 +36,7 @@ import org.chromium.webapk.lib.common.WebApkConstants;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@Batch(Batch.PER_CLASS)
 public class ManageTrustedWebActivityDataActivityTest {
     private static final String SETTINGS_ACTIVITY_NAME =
             "org.chromium.chrome.browser.settings.SettingsActivity";
@@ -44,7 +45,6 @@ public class ManageTrustedWebActivityDataActivityTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "http://crbug.com/1283285")
     public void launchesWebApkSiteSettings() throws Exception {
         WebApkValidator.setDisableValidationForTesting(true);
         ManageTrustedWebActivityDataActivity.setCallingPackageForTesting(TEST_PACKAGE_NAME);
