d58a8035241585467c464689d7ebc46e742976a5
Ruma Kesh <rkesh@google.com>
2023-05-02 19:00:31 +0000

    CrOS Settings: Convert switch access action assignment dialog browser test to TS
    Additionally, moved the test under the `os_a11y_page/` folder to
    mirror the app folder structure.
    
    gtest_filter="*OsA11yPageSwitchAccessActionAssignmentDialog*"
    
    Bug: b/270728282
    Test: browser_tests --
    Change-Id: I98e717691dcf14b0a193e8390b0fd9ed81670a3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496874
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138538}

---
+++ chrome/test/data/webui/settings/chromeos/os_a11y_page/switch_access_action_assignment_dialog_test.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://os-settings/chromeos/lazy_load.js';
+
+import {SettingsSwitchAccessActionAssignmentDialogElement, SwitchAccessCommand} from 'chrome://os-settings/chromeos/lazy_load.js';
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+suite('<settings-switch-access-action-assignment-dialog>', () => {
+  let dialog: SettingsSwitchAccessActionAssignmentDialogElement;
+
+  setup(() => {
+    dialog = document.createElement(
+        'settings-switch-access-action-assignment-dialog');
+    dialog.action = SwitchAccessCommand.SELECT;
+    document.body.appendChild(dialog);
+    flush();
+  });
+
+  test('Exit button closes dialog', () => {
+    assertTrue(dialog.$.switchAccessActionAssignmentDialog.open);
+
+    const exitBtn = dialog.$.switchAccessActionAssignmentDialog.$.close;
+    assertTrue(!!exitBtn);
+
+    exitBtn.click();
+    assertFalse(dialog.$.switchAccessActionAssignmentDialog.open);
+  });
+});
