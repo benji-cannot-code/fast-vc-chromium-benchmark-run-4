6a5fa8183b0c9e1c1361f870dbf19b75c6dea4d1
Dave Tapuska <dtapuska@chromium.org>
2023-05-02 23:36:54 +0000

    [extensions] Ensure RenderProcessHost is always observed
    We fail to observe a RenderProcessHost if it isn't added via
    AddListener. This could lead to us not detecting the process
    exited and then using a stale mojo connection. The stale mojo
    connection would then fail to send.
    
    Bug: 1316588, 1441325
    Change-Id: I0ceb268dca17b68f4e592a1ebe353c6230e9c0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496586
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138661}

--- extensions/browser/event_router.cc
+++ extensions/browser/event_router.cc
@@ -151,7 +151,6 @@ const char EventRouter::kRegisteredLazyEvents[] = "events";
 const char EventRouter::kRegisteredServiceWorkerEvents[] =
     "serviceworkerevents";
 
-// static
 void EventRouter::DispatchExtensionMessage(
     content::RenderProcessHost* rph,
     int worker_thread_id,
@@ -170,14 +169,13 @@ void EventRouter::DispatchExtensionMessage(
   params->event_id = event_id;
   params->is_user_gesture = user_gesture == USER_GESTURE_ENABLED;
   params->filtering_info = std::move(info);
-
-  Get(browser_context)
-      ->RouteDispatchEvent(rph, std::move(params), std::move(event_args));
+  RouteDispatchEvent(rph, std::move(params), std::move(event_args));
 }
 
 void EventRouter::RouteDispatchEvent(content::RenderProcessHost* rph,
                                      mojom::DispatchEventParamsPtr params,
                                      base::Value::List event_args) {
+  CHECK(base::Contains(observed_process_set_, rph));
   int worker_thread_id = params->worker_thread_id;
   mojo::AssociatedRemote<mojom::EventDispatcher>& dispatcher =
       rph_dispatcher_map_[rph][worker_thread_id];
@@ -221,7 +219,6 @@ void EventRouter::DispatchEventToSender(
     const std::string& extension_id,
     events::HistogramValue histogram_value,
     const std::string& event_name,
-    int render_process_id,
     int worker_thread_id,
     int64_t service_worker_version_id,
     base::Value::List event_args,
@@ -229,14 +226,28 @@ void EventRouter::DispatchEventToSender(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   int event_id = g_extension_event_id.GetNext();
 
-  DoDispatchEventToSenderBookkeeping(
-      browser_context, extension_id, event_id, render_process_id,
-      service_worker_version_id, histogram_value, event_name);
+  if (!ExtensionsBrowserClient::Get()->IsValidContext(browser_context)) {
+    return;
+  }
 
-  DispatchExtensionMessage(rph, worker_thread_id, browser_context, extension_id,
-                           event_id, event_name, std::move(event_args),
-                           UserGestureState::USER_GESTURE_UNKNOWN,
-                           std::move(info));
+  EventRouter* event_router = EventRouter::Get(browser_context);
+  CHECK(event_router);
+  auto* registry = ExtensionRegistry::Get(browser_context);
+  CHECK(registry);
+  const Extension* extension =
+      registry->enabled_extensions().GetByID(extension_id);
+  if (extension) {
+    event_router->IncrementInFlightEvents(browser_context, rph, extension,
+                                          event_id, event_name,
+                                          service_worker_version_id);
+    event_router->ReportEvent(histogram_value, extension,
+                              /*did_enqueue=*/false);
+  }
+  event_router->ObserveProcess(rph);
+  event_router->DispatchExtensionMessage(
+      rph, worker_thread_id, browser_context, extension_id, event_id,
+      event_name, std::move(event_args), UserGestureState::USER_GESTURE_UNKNOWN,
+      std::move(info));
 }
 
 // static.
@@ -527,6 +538,7 @@ void EventRouter::AddEventListener(const std::string& event_name,
                                    const std::string& extension_id) {
   listeners_.AddListener(EventListener::ForExtension(event_name, extension_id,
                                                      process, absl::nullopt));
+  CHECK(base::Contains(observed_process_set_, process));
 }
 
 void EventRouter::AddServiceWorkerEventListener(
@@ -540,6 +552,7 @@ void EventRouter::AddServiceWorkerEventListener(
       event_name, extension_id, process, process->GetBrowserContext(),
       service_worker_scope, service_worker_version_id, worker_thread_id,
       absl::nullopt));
+  CHECK(base::Contains(observed_process_set_, process));
 }
 
 void EventRouter::RemoveEventListener(const std::string& event_name,
@@ -570,6 +583,7 @@ void EventRouter::AddEventListenerForURL(const std::string& event_name,
                                          const GURL& listener_url) {
   listeners_.AddListener(
       EventListener::ForURL(event_name, listener_url, process, absl::nullopt));
+  CHECK(base::Contains(observed_process_set_, process));
 }
 
 void EventRouter::RemoveEventListenerForURL(const std::string& event_name,
@@ -607,6 +621,11 @@ void EventRouter::RemoveObserverForTesting(TestObserver* observer) {
 }
 
 void EventRouter::OnListenerAdded(const EventListener* listener) {
+  RenderProcessHost* process = listener->process();
+  if (process) {
+    ObserveProcess(process);
+  }
+
   const EventListenerInfo details(
       listener->event_name(), listener->extension_id(),
       listener->listener_url(), listener->browser_context(),
@@ -618,13 +637,6 @@ void EventRouter::OnListenerAdded(const EventListener* listener) {
     for (auto& observer : *it->second) {
       observer.OnListenerAdded(details);
     }
-  }
-
-  RenderProcessHost* process = listener->process();
-  if (process) {
-    bool inserted = observed_process_set_.insert(process).second;
-    if (inserted)
-      process->AddObserver(this);
   }
 }
 
@@ -643,6 +655,14 @@ void EventRouter::OnListenerRemoved(const EventListener* listener) {
   }
 }
 
+void EventRouter::ObserveProcess(RenderProcessHost* process) {
+  CHECK(process);
+  bool inserted = observed_process_set_.insert(process).second;
+  if (inserted) {
+    process->AddObserver(this);
+  }
+}
+
 void EventRouter::RenderProcessExited(
     RenderProcessHost* host,
     const content::ChildProcessTerminationInfo& info) {
@@ -655,6 +675,7 @@ void EventRouter::RenderProcessExited(
 void EventRouter::RenderProcessHostDestroyed(RenderProcessHost* host) {
   listeners_.RemoveListenersForProcess(host);
   observed_process_set_.erase(host);
+  rph_dispatcher_map_.erase(host);
   host->RemoveObserver(this);
 }
 
@@ -701,6 +722,7 @@ void EventRouter::AddFilteredEventListener(
     return;
   }
   listeners_.AddListener(std::move(regular_listener));
+  CHECK(base::Contains(observed_process_set_, process));
 
   DCHECK_EQ(add_lazy_listener, !!lazy_listener);
   if (lazy_listener) {
@@ -1100,34 +1122,6 @@ void EventRouter::DispatchEventToProcess(
   }
 }
 
-// static
-void EventRouter::DoDispatchEventToSenderBookkeeping(
-    content::BrowserContext* browser_context,
-    const std::string& extension_id,
-    int event_id,
-    int render_process_id,
-    int64_t service_worker_version_id,
-    events::HistogramValue histogram_value,
-    const std::string& event_name) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (!ExtensionsBrowserClient::Get()->IsValidContext(browser_context))
-    return;
-
-  auto* registry = ExtensionRegistry::Get(browser_context);
-  DCHECK(registry);
-  const Extension* extension =
-      registry->enabled_extensions().GetByID(extension_id);
-  if (!extension)
-    return;
-
-  EventRouter* event_router = EventRouter::Get(browser_context);
-  DCHECK(event_router);
-  event_router->IncrementInFlightEvents(
-      browser_context, RenderProcessHost::FromID(render_process_id), extension,
-      event_id, event_name, service_worker_version_id);
-  event_router->ReportEvent(histogram_value, extension, /*did_enqueue=*/false);
-}
-
 void EventRouter::IncrementInFlightEvents(BrowserContext* context,
                                           RenderProcessHost* process,
                                           const Extension* extension,
@@ -1368,6 +1362,7 @@ void EventRouter::BindServiceWorkerEventDispatcher(
   if (!process) {
     return;
   }
+  ObserveProcess(process);
   mojo::AssociatedRemote<mojom::EventDispatcher>& worker_dispatcher =
       rph_dispatcher_map_[process][worker_thread_id];
   CHECK(!worker_dispatcher);
