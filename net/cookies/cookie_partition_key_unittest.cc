96fe99f4d23c368bba062ac295580fc25dbffbe4
Dylan Cutler <dylancutler@google.com>
2023-05-02 15:49:34 +0000

    Swap frame site for top-level frame site in nonced cookie partitions
    This is to align CookiePartitionKey's implementation of nonced
    partitions with StorageKey, which uses the storage origin for the
    top-level site. This mismatch prevents nonced partitioned workers from
    accessing partitioned cookies in the frame that registered them's
    partition, since partitioned workers derive their CookiePartitionKey
    from their StorageKey.
    
    Even with this change, the nonced partitions are still unique and ephemeral.
    
    The WPT I added checks that workers registered by credentialless frames can access partitioned cookies set by the frame that registered them.
    Before this change, this test would fail.
    
    Bug: 1440765
    Change-Id: Iaa288365723386c02cbd4a58fcbfabb69186e3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4491704
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138389}

--- net/cookies/cookie_partition_key_unittest.cc
+++ net/cookies/cookie_partition_key_unittest.cc
@@ -145,7 +145,7 @@ TEST_P(CookiePartitionKeyTest, FromNetworkIsolationKey) {
           "WithNonce",
           NetworkIsolationKey(kTopLevelSite, kCookieSite, kNonce),
           /*allow_nonced_partition_keys=*/false,
-          CookiePartitionKey::FromURLForTesting(kTopLevelSite.GetURL(), kNonce),
+          CookiePartitionKey::FromURLForTesting(kCookieSite.GetURL(), kNonce),
       },
       {
           "NoncedAllowed_KeyWithoutNonce",
@@ -157,7 +157,7 @@ TEST_P(CookiePartitionKeyTest, FromNetworkIsolationKey) {
           "NoncedAllowed_KeyWithoutNonce",
           NetworkIsolationKey(kTopLevelSite, kCookieSite, kNonce),
           /*allow_nonced_partition_keys=*/true,
-          CookiePartitionKey::FromURLForTesting(kTopLevelSite.GetURL(), kNonce),
+          CookiePartitionKey::FromURLForTesting(kCookieSite.GetURL(), kNonce),
       },
   };
 
