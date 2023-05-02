8b1134869f1aa85f0773738fd2fca36e90972925
Alex Ilin <alexilin@chromium.org>
2023-05-02 12:53:39 +0000

    [sheriff] Disable flaky geolocation test
    This CL disables the test
    GeolocationTest#testGeolocationPlumbingAllowedDialogNoGesture
    
    Bug: 1441822
    Change-Id: Ie5192e1d62f2ec01b8e73f4403508ea0237f231c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492772
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Owners-Override: Alex Ilin <alexilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138298}

--- chrome/android/javatests/src/org/chromium/chrome/browser/permissions/GeolocationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/permissions/GeolocationTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.permissions.PermissionTestRule.PermissionUpdateWaiter;
@@ -76,6 +77,7 @@ public class GeolocationTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1441822")
     @Feature({"Location", "Main"})
     public void testGeolocationPlumbingAllowedDialogNoGesture() throws Exception {
         runTest("initiate_getCurrentPosition()", 1, false, true);
