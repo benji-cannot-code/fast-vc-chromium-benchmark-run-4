242afed46d7412937e9c318c137c50ead273d308
Saifuddin Hitawala <hitawala@chromium.org>
2023-05-02 20:38:18 +0000

    Graphite: Implement SkiaGraphiteDawnImageRepresentation
    This change implements SkiaGraphiteDawnImageRepresentation where
    the SkiaGraphite is a wrapper around the Dawn representation used
    for Graphite-Dawn backends. Currently, it is only implemented for
    single planar formats and added TODOs for multiplanar. Also implement
    ProduceSkiaGraphite calling SkiaDawn from IOSurfaceBacking.
    
    Bug: 1430206
    Change-Id: I731d703a54839fe08a6399d8b9a1110dfb0f068f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480798
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138613}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_cftyperef.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/trace_event/memory_dump_manager.h"
+#include "components/viz/common/gpu/dawn_context_provider.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "components/viz/common/resources/resource_sizes.h"
 #include "gpu/command_buffer/common/shared_image_trace_utils.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/shared_context_state.h"
 #include "gpu/command_buffer/service/shared_image/iosurface_image_backing_factory.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_format_utils.h"
+#include "gpu/command_buffer/service/shared_image/skia_graphite_dawn_image_representation.h"
 #include "gpu/command_buffer/service/skia_utils.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/include/core/SkPromiseImageTexture.h"
@@ -434,7 +436,11 @@ DawnIOSurfaceRepresentation::~DawnIOSurfaceRepresentation() {
 }
 
 WGPUTexture DawnIOSurfaceRepresentation::BeginAccess(WGPUTextureUsage usage) {
+  const std::string debug_label =
+      "IOSurface(" + CreateLabelForSharedImageUsage(usage) + ")";
+
   WGPUTextureDescriptor texture_descriptor = {};
+  texture_descriptor.label = debug_label.c_str();
   texture_descriptor.format = wgpu_format_;
   texture_descriptor.usage = usage;
   texture_descriptor.dimension = WGPUTextureDimension_2D;
@@ -868,6 +874,39 @@ IOSurfaceImageBacking::ProduceSkiaGanesh(
   return std::make_unique<SkiaIOSurfaceRepresentation>(
       manager, this, egl_state, std::move(context_state), promise_textures,
       tracker);
+}
+
+std::unique_ptr<SkiaGraphiteImageRepresentation>
+IOSurfaceImageBacking::ProduceSkiaGraphite(
+    SharedImageManager* manager,
+    MemoryTypeTracker* tracker,
+    scoped_refptr<SharedContextState> context_state) {
+  CHECK(context_state);
+  CHECK(context_state->graphite_context());
+  if (context_state->gr_context_type() == GrContextType::kGraphiteDawn) {
+#if BUILDFLAG(SKIA_USE_DAWN)
+    WGPUDevice device =
+        context_state->dawn_context_provider()->GetDevice().Get();
+    auto backend_type = WGPUBackendType::WGPUBackendType_Metal;
+    auto dawn_representation = ProduceDawn(manager, tracker, device,
+                                           backend_type, /*view_formats=*/{});
+    if (!dawn_representation) {
+      LOG(ERROR) << "Could not create Dawn Representation";
+      return nullptr;
+    }
+    // Use GPU main recorder since this should only be called for
+    // fulfilling Graphite promise images on GPU main thread.
+    return SkiaGraphiteDawnImageRepresentation::Create(
+        std::move(dawn_representation), context_state,
+        context_state->gpu_main_graphite_recorder(), manager, this, tracker);
+#else
+    NOTREACHED();
+    return nullptr;
+#endif
+  }
+  // TODO(hitawala): Implement this for GrContextType::kGraphiteMetal.
+  NOTREACHED();
+  return nullptr;
 }
 
 void IOSurfaceImageBacking::SetPurgeable(bool purgeable) {
