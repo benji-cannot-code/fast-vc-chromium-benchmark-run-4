eb84ec57c889b0e084661aab28b2d450329f3e82
dpapad <dpapad@chromium.org>
2023-05-02 00:20:18 +0000

    CrOS CellularSetup: Remove obsolete big_buffer.mojom-lite.js import.
    This is a leftover from http://crrev.com/c/3905706 which migrated
    CellularSetup to Mojo JS modules.
    
    Bug: 1179821
    Change-Id: I00e6de2ec1ba6cce2a768ec1a0c255f1fb281571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4497251
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138109}

--- ash/webui/common/resources/cellular_setup/mojo_interface_provider.js
+++ ash/webui/common/resources/cellular_setup/mojo_interface_provider.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/mojo/mojo/public/mojom/base/big_buffer.mojom-lite.js';
-
 import {CellularSetup, CellularSetupRemote} from 'chrome://resources/mojo/chromeos/ash/services/cellular_setup/public/mojom/cellular_setup.mojom-webui.js';
 import {ESimManager, ESimManagerObserverInterface, ESimManagerObserverReceiver, ESimManagerObserverRemote, ESimManagerRemote} from 'chrome://resources/mojo/chromeos/ash/services/cellular_setup/public/mojom/esim_manager.mojom-webui.js';
 
