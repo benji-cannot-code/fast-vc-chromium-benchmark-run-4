761c5a6e7878a26058a5927c94d48be5747ab72b
Boris Sazonov <bsazonov@google.com>
2023-05-02 20:01:04 +0000

    [Bookmarks] Add BookmarkModel::MoveToOtherModelWithNewNodeIdsAndUuids
    1. Implements BookmarkModel::MoveToOtherModelWithNewNodeIdsAndUuids
       that is needed to move bookmarks nodes from model to another.
    2. Marks BookmarkModel as final as a safety precaution - it might be
       hard to implement robust cross-model moves if dest_model passed to
       MoveToOtherModelWithNewNodeIdsAndUuids can refer to some subclass
       of BookmarkModel.
    3. To correctly test observer notifications triggered from
       MoveToOtherModelWithNewNodeIdsAndUuids - MockBookmarkModelObserver
       now mocks OnWillRemoveBookmarks.
    
    Bug: 1416567
    Change-Id: Ie0efb2b82edfa8e03e6f3ccc91bfb4c544d44318
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4479456
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138585}

--- components/bookmarks/test/mock_bookmark_model_observer.h
+++ components/bookmarks/test/mock_bookmark_model_observer.h
@@ -29,6 +29,10 @@ class MockBookmarkModelObserver : public BookmarkModelObserver {
   MOCK_METHOD4(BookmarkNodeAdded,
                void(BookmarkModel*, const BookmarkNode*, size_t, bool));
 
+  MOCK_METHOD4(
+      OnWillRemoveBookmarks,
+      void(BookmarkModel*, const BookmarkNode*, size_t, const BookmarkNode*));
+
   MOCK_METHOD5(BookmarkNodeRemoved,
                void(BookmarkModel*,
                     const BookmarkNode*,
