f9c4ac8114c4ae4aafd84da036a80d74598babfe
Iman Saboori <isaboori@google.com>
2023-05-02 20:02:35 +0000

    Record `PreloadOnHover` in `PageAnchorsMetricsObserver`
    To be able to properly handle BFCache, instead of directly recording
    anchor element data to UKM in `NavigationPredictor`, we should delegate
    it to `PageAnchorsMetricsObserver`.
    
    Bug: 1423508
    Change-Id: I9138087e5c8483ae682b971208c96248265803fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4447858
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138588}

--- chrome/browser/navigation_predictor/navigation_predictor_metrics_document_data.h
+++ chrome/browser/navigation_predictor/navigation_predictor_metrics_document_data.h
@@ -91,6 +91,12 @@ class NavigationPredictorMetricsDocumentData
     absl::optional<base::TimeDelta> max_hover_dwell_time;
   };
 
+  struct PreloadOnHoverData {
+    bool taken = false;
+    absl::optional<base::TimeDelta> hover_dwell_time;
+    absl::optional<base::TimeDelta> pointer_down_duration;
+  };
+
   NavigationPredictorMetricsDocumentData(
       const NavigationPredictorMetricsDocumentData&) = delete;
   NavigationPredictorMetricsDocumentData& operator=(
@@ -127,6 +133,11 @@ class NavigationPredictorMetricsDocumentData
     return user_interactions_;
   }
 
+  void AddPreloadOnHoverData(PreloadOnHoverData data);
+  void RecordPreloadOnHoverData(ukm::SourceId ukm_source_id);
+
+  void RecordDataToUkm(ukm::SourceId ukm_source_id);
+
  private:
   friend class content::DocumentUserData<
       NavigationPredictorMetricsDocumentData>;
@@ -146,6 +157,7 @@ class NavigationPredictorMetricsDocumentData
   std::vector<PageLinkClickData> page_link_clicks_;
   std::map<int, AnchorElementMetricsData> anchor_element_metrics_;
   std::unordered_map<int, UserInteractionsData> user_interactions_;
+  std::vector<PreloadOnHoverData> preload_on_hover_;
   // The time between navigation start and the last time user clicked on a
   // link.
   absl::optional<base::TimeDelta> navigation_start_to_click_;
--- chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.cc
@@ -11,41 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/metrics_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 
-void PageAnchorsMetricsObserver::RecordUserInteractionDataToUkm() {
-  if (!render_frame_host_) {
-    return;
-  }
-  NavigationPredictorMetricsDocumentData* data =
-      NavigationPredictorMetricsDocumentData::GetOrCreateForCurrentDocument(
-          render_frame_host_);
-  CHECK(data);
-  data->SetNavigationStartTime(GetDelegate().GetNavigationStart());
-  data->RecordUserInteractionsData(ukm_source_id_);
-}
-
-void PageAnchorsMetricsObserver::RecordAnchorDataToUkm() {
-  if (!render_frame_host_) {
-    return;
-  }
-
-  NavigationPredictorMetricsDocumentData* data =
-      NavigationPredictorMetricsDocumentData::GetOrCreateForCurrentDocument(
-          render_frame_host_);
-  CHECK(data);
-  data->RecordAnchorData(ukm_source_id_);
-}
-
-void PageAnchorsMetricsObserver::RecordPageLinkClickDataToUkm() {
-  if (!render_frame_host_) {
-    return;
-  }
-  NavigationPredictorMetricsDocumentData* data =
-      NavigationPredictorMetricsDocumentData::GetOrCreateForCurrentDocument(
-          render_frame_host_);
-  CHECK(data);
-  data->RecordPageLinkClickData(ukm_source_id_);
-}
-
 void PageAnchorsMetricsObserver::RecordAnchorElementMetricsDataToUkm() {
   if (!render_frame_host_) {
     return;
@@ -61,9 +26,14 @@ void PageAnchorsMetricsObserver::RecordDataToUkm() {
   // `AnchorElementMetricsData` are already recorded to UKM as we receive them,
   // and we don't need to record them again here. The edge case scenario is
   // handled separately in `OnRestoreFromBackForwardCache`.
-  RecordPageLinkClickDataToUkm();
-  RecordAnchorDataToUkm();
-  RecordUserInteractionDataToUkm();
+  if (!render_frame_host_) {
+    return;
+  }
+  NavigationPredictorMetricsDocumentData* data =
+      NavigationPredictorMetricsDocumentData::GetOrCreateForCurrentDocument(
+          render_frame_host_);
+  CHECK(data);
+  data->RecordDataToUkm(ukm_source_id_);
 }
 
 page_load_metrics::PageLoadMetricsObserver::ObservePolicy
@@ -88,7 +58,6 @@ void PageAnchorsMetricsObserver::OnComplete(
   // Do not report Ukm while prerendering.
   if (is_in_prerendered_page_)
     return;
-
   RecordDataToUkm();
 }
 
@@ -114,6 +83,7 @@ void PageAnchorsMetricsObserver::UpdateRenderFrameHostAndSourceId(
           render_frame_host_);
   CHECK(data);
   data->SetUkmSourceId(ukm_source_id_);
+  data->SetNavigationStartTime(GetDelegate().GetNavigationStart());
 }
 
 void PageAnchorsMetricsObserver::DidActivatePrerenderedPage(
