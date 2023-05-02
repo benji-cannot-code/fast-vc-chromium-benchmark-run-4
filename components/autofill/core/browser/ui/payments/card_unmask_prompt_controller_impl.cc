bc7c344f7d18400ad6aed3cfbf3ec4a63b737a6f
Vishwas Uppoor <vishwasuppoor@google.com>
2023-05-02 17:01:03 +0000

    [Metadata][CVC Dialog][Android] Format expiration as mm/yy
    Update expiration date shown on the Card Unmask prompt mm/yyyy -> mm/yy.
    
    Mock: https://www.figma.com/file/N6F3DN9prdEzQiylOtVBCW/X-PA-share?node-id=13697-102346&t=coq9qLbhZbosrHHU-4
    Current UI: https://screenshot.googleplex.com/XgM43p7RvEUSysW
    Implementation UI: https://screenshot.googleplex.com/6PGnLcB7cfFEicx
    
    Bug: 1434340
    Change-Id: Ifd999f3753b0268016191d2b84ad7caf5825802f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4463357
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1138456}

--- components/autofill/core/browser/ui/payments/card_unmask_prompt_controller_impl.cc
+++ components/autofill/core/browser/ui/payments/card_unmask_prompt_controller_impl.cc
@@ -314,7 +314,7 @@ std::u16string CardUnmaskPromptControllerImpl::GetCardLastFourDigits() const {
 }
 
 std::u16string CardUnmaskPromptControllerImpl::GetCardExpiration() const {
-  return card_.ExpirationDateForDisplay();
+  return card_.AbbreviatedExpirationDateForDisplay(false);
 }
 
 const GURL& CardUnmaskPromptControllerImpl::GetCardArtUrl() const {
