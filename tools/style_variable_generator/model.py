3600d407e8a030b9fe747d0cb469f326e46ff6da
Zain Afzal <zafzal@google.com>
2023-05-02 11:48:26 +0000

    [Jelly] Allow legacy mapping to color formula
    Previously any legacy mappings had to be specified as css var to css var
    mappings. This cl makes it so that legacy mappings can be specified as
    css var to any valid color formula i.e rgba(), blend, etc.
    
    This allows for us to better represent mappings and get automatic
    generation of rgb variable mappings.
    
    Bug: b/279094501
    Change-Id: Idcde651ea2b61d1c875478b41714fec8514afd6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4495886
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138274}

--- tools/style_variable_generator/model.py
+++ tools/style_variable_generator/model.py
@@ -170,7 +170,6 @@ class ColorModel(ModeKeyedModel):
         self.variable_type = VariableType.COLOR
 
     def Add(self, name, value_obj, context):
-        name = full_token_name(name, context)
         added = []
         # If a color has generate_per_mode set, a separate variable will be
         # created for each mode, suffixed by mode name.
@@ -307,6 +306,23 @@ class NamespacedModel(SimpleModel):
         return super().Add(name, value_obj, context)
 
 
+# A color model where all variables are prefixed with the current namespace.
+class NamespacedColorModel(ColorModel):
+    def Add(self, name, value_obj, context):
+        name = full_token_name(name, context)
+        return super().Add(name, value_obj, context)
+
+
+# A color model specifically for storing mappings of arbitrary css vars to some
+# known StyleVariable.
+class LegacyMappingsModel(ColorModel):
+    def Add(self, name, value_obj, context):
+        if isinstance(value_obj, dict):
+            raise ValueError(
+                'Legacy mappings can only be singular references.')
+        return super().Add(name, value_obj, context)
+
+
 class Model(object):
     def __init__(self):
         # A map of all variables to their |StyleVariable| object.
@@ -318,13 +334,13 @@ class Model(object):
         self.opacities = OpacityModel()
         self.submodels[VariableType.OPACITY] = self.opacities
 
-        self.colors = ColorModel(self.opacities)
+        self.colors = NamespacedColorModel(self.opacities)
         self.submodels[VariableType.COLOR] = self.colors
 
         self.untyped_css = NamespacedModel(VariableType.UNTYPED_CSS)
         self.submodels[VariableType.UNTYPED_CSS] = self.untyped_css
 
-        self.legacy_mappings = SimpleModel(VariableType.LEGACY_MAPPING)
+        self.legacy_mappings = LegacyMappingsModel(self.opacities)
         self.submodels[VariableType.LEGACY_MAPPING] = self.legacy_mappings
 
         def CheckTypeFace(name, value_obj, context):
