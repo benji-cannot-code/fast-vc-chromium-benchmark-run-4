ff82ee3f5d9c1cd52c46d407c627bc97bbcf12de
Joe Antonetti <joeantonetti@google.com>
2023-05-02 18:16:13 +0000

    [QuickStart][3/n] Refactor TargetDeviceBootstrapController to no longer call factory directly
    This is part of a series of changes to pass QuickStartDecoder down to
    the Connection object directly.
    
    This change refactors TargetDeviceBootstrapController to accept a
    TargetDeviceConnectionBroker, rather than constructing it directly using
    a factory. This change was made for several reasons:
    
    1) Since TargetDeviceBootstrapController only uses one instance of the
    connection broker created in it's constructor, it has no need to have
    a reference to a factory (it never creates arbitrary instances)
    2) As we increase the number of dependencies needed to construct
    TargetDeviceConnectionBroker, threading all of these dependencies through
    TargetDeviceBootstrapController will become difficult to manage
    
    Test: Updated unit tests
    Bug: b/280330703
    Change-Id: Ib232c0ee290f00a3af64384908f7aa75a2ba983c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4495511
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138525}

--- chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller.h
+++ chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller.h
@@ -15,16 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/target_device_connection_broker.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 
-class NearbyConnectionsManager;
-
 namespace ash::quick_start {
 
 class TargetDeviceBootstrapController
     : public TargetDeviceConnectionBroker::ConnectionLifecycleListener {
  public:
   explicit TargetDeviceBootstrapController(
-      base::WeakPtr<NearbyConnectionsManager> nearby_connections_manager,
-      bool is_resume_after_update = false);
+      std::unique_ptr<TargetDeviceConnectionBroker>
+          target_device_connection_broker);
   TargetDeviceBootstrapController(TargetDeviceBootstrapController&) = delete;
   TargetDeviceBootstrapController& operator=(TargetDeviceBootstrapController&) =
       delete;
