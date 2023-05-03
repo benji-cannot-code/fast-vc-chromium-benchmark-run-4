028dc498b0ebfb78e0c99653fd5c9333cde8d41b
François Beaufort <beaufort.francois@gmail.com>
2023-05-03 21:14:14 +0000

    WebGPU: Warn developers when GPUBlendComponent is partially specified.
    This CL adds a DevTools console warning when a GPUBlendComponent is
    partially specified. Even though those have good default values, it
    is still useful to warn developers about it.
    
    I've filed WebIDL issue at https://github.com/whatwg/webidl/issues/1300
    to see if we can get WebIDL to change their behavior to not alter our
    IDL files.
    
    Bug: dawn:1785
    Change-Id: I234419108cb92854d886e64f7b30c0fce173c5e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481266
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139124}

--- third_party/blink/renderer/modules/webgpu/gpu_device.h
+++ third_party/blink/renderer/modules/webgpu/gpu_device.h
@@ -152,6 +152,7 @@ class GPUDevice final : public EventTargetWithInlineData,
   ExecutionContext* GetExecutionContext() const override;
 
   void InjectError(WGPUErrorType type, const char* message);
+  void AddConsoleWarning(const String& message);
   void AddConsoleWarning(const char* message);
   void AddSingletonWarning(GPUSingletonWarning type);
 
