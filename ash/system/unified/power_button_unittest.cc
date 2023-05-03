bc080a1cf6dfe4bee8b29ad4f51f921c38e6c6fb
Sam Cackett <samcackett@google.com>
2023-05-03 08:32:16 +0000

    Hide power off button from QS menu
    If DeviceRebootOnShutdownPolicy is enabled, then hide the power off button
    
    Bug: b/252862242
    Change-Id: I0a38cd1135373797500f55c0415a93eaa776fd01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4498437
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138837}

--- ash/system/unified/power_button_unittest.cc
+++ ash/system/unified/power_button_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/constants/quick_settings_catalogs.h"
 #include "ash/public/cpp/ash_view_ids.h"
+#include "ash/shell.h"
+#include "ash/shutdown_controller_impl.h"
 #include "ash/style/icon_button.h"
 #include "ash/system/unified/quick_settings_footer.h"
 #include "ash/system/unified/quick_settings_view.h"
@@ -407,6 +409,17 @@ TEST_F(PowerButtonTest, ButtonRoundedRadii) {
 
   EXPECT_EQ(gfx::RoundedCornersF(16, 4, 16, 16),
             GetBackgroundLayer()->rounded_corner_radii());
+}
+
+TEST_F(PowerButtonTest, DeviceRebootOnShutdownPolicyHidesPowerOffButton) {
+  CreateUserSessions(1);
+  // Simulate DeviceRebootOnShutdownPolicy is enabled.
+  Shell::Get()->shutdown_controller()->SetRebootOnShutdown(true);
+
+  SimulatePowerButtonPress();
+
+  EXPECT_FALSE(GetPowerOffButton());
+  EXPECT_TRUE(GetRestartButton());
 }
 
 }  // namespace ash
