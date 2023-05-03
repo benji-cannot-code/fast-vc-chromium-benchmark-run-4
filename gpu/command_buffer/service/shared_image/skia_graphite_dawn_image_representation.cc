4269c0c3a488fcdcb5839cfcd0d0d61112afc3f9
Saifuddin Hitawala <hitawala@chromium.org>
2023-05-03 17:07:29 +0000

    Graphite: Fix WGPUTextureUsage in SkiaDawnRep
    This change fixes WGPUTextureUsages in SkiaGraphiteDawnRepresentation
    to be similar to those set in ImageContextImpl as Skia does not
    handle usage differences for now. It also addresses some left over
    nits in crrev.com/c/4480798
    
    Bug: 1430206
    Change-Id: I075ea0253abde5b112cfc0b338e86ce146677609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503528
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138964}

--- gpu/command_buffer/service/shared_image/skia_graphite_dawn_image_representation.cc
+++ gpu/command_buffer/service/shared_image/skia_graphite_dawn_image_representation.cc
@@ -18,10 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// TODO(sunnyps): Revisit this when implementing wrapped graphite backings
+// for render passes - do we also need CopySrc and/or CopyDst?
 constexpr WGPUTextureUsage kDefaultTextureUsage = static_cast<WGPUTextureUsage>(
-    WGPUTextureUsage_CopySrc | WGPUTextureUsage_CopyDst |
     WGPUTextureUsage_RenderAttachment | WGPUTextureUsage_TextureBinding);
-
 }
 
 namespace gpu {
@@ -72,12 +72,17 @@ SkiaGraphiteDawnImageRepresentation::BeginWriteAccess(
   dawn_scoped_access_ = dawn_representation_->BeginScopedAccess(
       kDefaultTextureUsage, AllowUnclearedAccess::kYes);
   if (!dawn_scoped_access_) {
-    LOG(ERROR) << "Could not create DawnImageRepresentation::ScopedAccess";
+    DLOG(ERROR) << "Could not create DawnImageRepresentation::ScopedAccess";
     return {};
   }
 
   // TODO(crbug.com/1430206): Add multiplanar format support.
-  CHECK(format().is_single_plane());
+  if (!format().is_single_plane()) {
+    DLOG(ERROR) << "BeginWriteAccess called for unsupported format = "
+                << format().ToString();
+    return {};
+  }
+
   viz::SharedImageFormat actual_format = format();
 #if BUILDFLAG(IS_MAC)
   // IOSurfaces are allocated as BGRA_8888 if the requested format is RGBA_8888,
@@ -102,7 +107,7 @@ SkiaGraphiteDawnImageRepresentation::BeginWriteAccess(
       backing()->color_space().GetAsFullRangeRGB().ToSkColorSpace(),
       &surface_props);
   if (!surface) {
-    LOG(ERROR) << "Could not create SkSurface";
+    DLOG(ERROR) << "Could not create SkSurface";
     dawn_scoped_access_.reset();
     return {};
   }
@@ -115,11 +120,16 @@ SkiaGraphiteDawnImageRepresentation::BeginWriteAccess() {
   CHECK_EQ(mode_, RepresentationAccessMode::kNone);
   CHECK(!dawn_scoped_access_);
   // TODO(crbug.com/1430206): Add multiplanar format support.
-  CHECK(format().is_single_plane());
+  if (!format().is_single_plane()) {
+    DLOG(ERROR) << "BeginWriteAccess called for unsupported format = "
+                << format().ToString();
+    return {};
+  }
+
   dawn_scoped_access_ = dawn_representation_->BeginScopedAccess(
       kDefaultTextureUsage, AllowUnclearedAccess::kYes);
   if (!dawn_scoped_access_) {
-    LOG(ERROR) << "Could not create DawnImageRepresentation::ScopedAccess";
+    DLOG(ERROR) << "Could not create DawnImageRepresentation::ScopedAccess";
     return {};
   }
   mode_ = RepresentationAccessMode::kWrite;
@@ -137,11 +147,16 @@ SkiaGraphiteDawnImageRepresentation::BeginReadAccess() {
   CHECK_EQ(mode_, RepresentationAccessMode::kNone);
   CHECK(!dawn_scoped_access_);
   // TODO(crbug.com/1430206): Add multiplanar format support.
-  CHECK(format().is_single_plane());
+  if (!format().is_single_plane()) {
+    DLOG(ERROR) << "BeginReadAccess called for unsupported format = "
+                << format().ToString();
+    return {};
+  }
+
   dawn_scoped_access_ = dawn_representation_->BeginScopedAccess(
       kDefaultTextureUsage, AllowUnclearedAccess::kNo);
   if (!dawn_scoped_access_) {
-    LOG(ERROR) << "Could not create DawnImageRepresentation::ScopedAccess";
+    DLOG(ERROR) << "Could not create DawnImageRepresentation::ScopedAccess";
     return {};
   }
   mode_ = RepresentationAccessMode::kRead;
