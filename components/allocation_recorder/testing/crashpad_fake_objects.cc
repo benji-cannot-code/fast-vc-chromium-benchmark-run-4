7b27c4b42e84a8edcc971525581a093fc199977e
André Kempe <andre.kempe@arm.com>
2023-05-03 19:01:44 +0000

    Include AllocationStackTraceRecorder into Crashpad report - 2/N
    With Arm's Memory Tagging Extension enabled, a process shall receive a
    SIG_SEGV if it encounters a mismatch in memory tags. To help developers
    analyse problems like this, we want to include a history of the most
    recent allocations and frees in the crashpad report.
    
    This CL introduces a rudimentary integration into the crashpad_handler.
    The content of report only has simple messages, the transformation of
    the recorder into a real report will be addressed in following CLs.
    
    Bug: 1137393
    Change-Id: I13ab7cf8fbf6132c570f8e29bba7fb820b9be8ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394798
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Andre Kempe <andre.kempe@arm.com>
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Cr-Commit-Position: refs/heads/main@{#1139050}

---
+++ components/allocation_recorder/testing/crashpad_fake_objects.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/allocation_recorder/testing/crashpad_fake_objects.h"
+
+namespace crashpad::test {
+
+bool BufferExtensionStreamDataSourceDelegate::ExtensionStreamDataSourceRead(
+    const void* data,
+    size_t size) {
+  const uint8_t* const typed_data = reinterpret_cast<const uint8_t*>(data);
+  data_.insert(data_.end(), typed_data, typed_data + size);
+
+  return true;
+}
+
+std::vector<uint8_t> BufferExtensionStreamDataSourceDelegate::GetMessage()
+    const {
+  return data_;
+}
+}  // namespace crashpad::test
