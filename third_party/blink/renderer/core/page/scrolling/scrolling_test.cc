fd9400c6aee2f9b110d31626a0d149040833baa1
Claudio DeSouza <cdesouza@igalia.com>
2023-05-03 11:24:17 +0000

    Replace base::GUID uses with base::Uuid (248/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /third_party/blink/renderer/core
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: I1bc968c8aae3789095c4aca15dfc0f51d07afa77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483708
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138862}

--- third_party/blink/renderer/core/page/scrolling/scrolling_test.cc
+++ third_party/blink/renderer/core/page/scrolling/scrolling_test.cc
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "base/guid.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/uuid.h"
 #include "build/build_config.h"
 #include "cc/base/features.h"
 #include "cc/input/main_thread_scrolling_reason.h"
@@ -1431,9 +1431,9 @@ TEST_P(ScrollingTest, ElementRegionCaptureData) {
       GetFrame()->GetDocument()->getElementById("content");
 
   const RegionCaptureCropId scrollable_id(
-      GUIDToToken(base::GUID::GenerateRandomV4()));
+      GUIDToToken(base::Uuid::GenerateRandomV4()));
   const RegionCaptureCropId content_id(
-      GUIDToToken(base::GUID::GenerateRandomV4()));
+      GUIDToToken(base::Uuid::GenerateRandomV4()));
 
   scrollable_element->SetRegionCaptureCropId(
       std::make_unique<RegionCaptureCropId>(scrollable_id));
