b4d4ebed190db246bdf31acaa54e22d6e65822d9
Stephan Hartmann <stha09@googlemail.com>
2023-05-03 21:20:10 +0000

    IWYU: add limits for std::numeric_limits in base::IDMap
    Bug: 957519
    Change-Id: I99362c0b8d7e30b57a853e31941ec0c884036971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502609
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1139135}

--- base/containers/id_map.h
+++ base/containers/id_map.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <iterator>
+#include <limits>
 #include <memory>
 #include <ostream>
 #include <type_traits>
