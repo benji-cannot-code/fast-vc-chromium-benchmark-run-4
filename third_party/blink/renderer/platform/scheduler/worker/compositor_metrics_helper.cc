fd45c405178304962dc8c4c128e1b30ba0ffba64
Ari Chivukula <arichiv@chromium.org>
2023-05-03 13:40:34 +0000

    Remove expired histograms RendererScheduler.TaskDurationPerThreadType2
    This CL removes the code that records these histogram:
      - RendererScheduler.TaskDurationPerThreadType2 (expired as of M85)
    
    Bug: 1384769
    Change-Id: I7f33ef2d038aa1106ec55443f89fde62b3cae3ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4498846
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138895}

--- third_party/blink/renderer/platform/scheduler/worker/compositor_metrics_helper.cc
+++ third_party/blink/renderer/platform/scheduler/worker/compositor_metrics_helper.cc
@@ -21,7 +21,7 @@ void CompositorMetricsHelper::RecordTaskMetrics(
   if (ShouldDiscardTask(task, task_timing))
     return;
 
-  MetricsHelper::RecordCommonTaskMetrics(task, task_timing);
+  // Any needed metrics should be recorded here.
 }
 
 }  // namespace scheduler
