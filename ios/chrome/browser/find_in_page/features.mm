dc526fd6174f30a741e5bd75cd36538b0692906b
Quentin Pubert <qpubert@google.com>
2023-05-03 08:10:08 +0000

    Revert "[iOS] Enable Native Find in Page by default"
    This reverts commit 360d56f954a67b40cadbc7d55444ee750bd55176.
    
    Reason for revert: More metrics are needed before full launch
    
    Original change's description:
    > [iOS] Enable Native Find in Page by default
    >
    > As per Finch best practices, this CL enables the Native Find in Page
    > feature flag by default and removes the associated fieldtrial testing
    > config entry so we can proceed to launch to 100%.
    >
    > Fixed: 1440630
    > Change-Id: I23af3b76c7a4b8c5bc368b1382e5087f3e147834
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481595
    > Commit-Queue: Quentin Pubert <qpubert@google.com>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1136414}
    
    Change-Id: I74cfc8c73003255f1b436978bdc3ba8a7fb8e031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4498748
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138831}

--- ios/chrome/browser/find_in_page/features.mm
+++ ios/chrome/browser/find_in_page/features.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 BASE_FEATURE(kNativeFindInPage,
              "NativeFindInPage",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 const char kNativeFindInPageParameterName[] = "variant";
 
