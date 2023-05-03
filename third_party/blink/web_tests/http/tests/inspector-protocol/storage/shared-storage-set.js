64f27524f217d577e92cc1b70973f530b700c45a
Danil Somsikov <dsv@chromium.org>
2023-05-03 10:28:57 +0000

    Clear shared storage entries after test so that they don't leak into another test.
    Bug: 1407335
    Change-Id: I17a6a3f5bdc25ed6a1baa07a8c44e55fb74abc83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502783
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138854}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/shared-storage-set.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/shared-storage-set.js
@@ -82,4 +82,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await getSharedStorageEntries(dp, testRunner, baseOrigin);
 
   testRunner.completeTest();
+
+  await dp.Storage.clearSharedStorageEntries({
+    ownerOrigin: baseOrigin
+  });
 })
