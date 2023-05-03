7583fe15e439f0fed5332a1d90c60985b0ff89b5
Teresa Mao <temao@chromium.org>
2023-05-03 21:29:01 +0000

    Add histograms for better measuring feature adoption
    - WebUICodeCache.FeatureEnabled increments when generated code cache for WebUI JS is instantiated.
    - SiteIsolatedCodeCache.JS.WebUI.Behaviour logs caching behavior (clear/create/hit/miss/update) for WebUI JS scripts only.
    
    Bug: None
    Change-Id: Iba1e0fed76c686ced917345a3b7a8131c20132e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420273
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139149}

--- content/browser/code_cache/generated_code_cache_context.cc
+++ content/browser/code_cache/generated_code_cache_context.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "content/browser/code_cache/generated_code_cache.h"
@@ -88,6 +89,8 @@ void GeneratedCodeCacheContext::InitializeOnThread(const base::FilePath& path,
             path.AppendASCII("webui_js"), max_bytes_webui_js,
             GeneratedCodeCache::CodeCacheType::kWebUIJavaScript),
         base::OnTaskRunnerDeleter(task_runner_)};
+
+    UMA_HISTOGRAM_BOOLEAN("WebUICodeCache.FeatureEnabled", true);
   }
 
   generated_js_code_cache_ = {
