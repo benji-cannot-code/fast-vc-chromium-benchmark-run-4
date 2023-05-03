7d7d81926d86b9750a18e8325003e053df719739
David Bertoni <dbertoni@chromium.org>
2023-05-03 00:24:22 +0000

    [Extensions] Check that a Native Messaging host returns value JSON.
    Currently, if a native message host returns an invalid response,
    the renderer will crash. This CL adds code to check the response in
    the browser before it's sent to the renderer.
    
    Bug: 1439827
    Change-Id: If73e43d497f43a5c944695588be509b51a12ca12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496720
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138720}

--- chrome/browser/extensions/api/messaging/native_message_port.h
+++ chrome/browser/extensions/api/messaging/native_message_port.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "extensions/browser/api/messaging/message_port.h"
 #include "extensions/common/api/messaging/port_id.h"
+#include "services/data_decoder/public/cpp/data_decoder.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -35,6 +36,9 @@ class NativeMessagePort : public MessagePort {
  private:
   class Core;
   void PostMessageFromNativeHost(const std::string& message);
+  void PostMessageFromNativeHostCallback(
+      const std::string& message,
+      data_decoder::DataDecoder::ValueOrError);
   void CloseChannel(const std::string& error_message);
 
   base::ThreadChecker thread_checker_;
