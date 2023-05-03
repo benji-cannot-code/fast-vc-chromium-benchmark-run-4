8fa77f8378631ba59082260d269e39fcc4024369
Katie Dektar <katie@chromium.org>
2023-05-03 00:41:59 +0000

    [ChromeVox] Remove content scripts not used by gdocs
    ChromeVox on Chrome OS injects cvox.api into Google Docs
    (and sheets, slides, etc) pages. This API is used by
    Docs to provide speech / interface for ChromeVox classic
    extension, but is no longer used for ChromeVox on Chrome OS
    (see google3/javascript/apps/docsshared/a11y/screenreaderwrapper.js
    line 42).
    
    We can stop injecting this API into Google Docs and remove
    it from ChromeVox entirely, leaving only the event to inform
    Docs that ChromeVox is on (see cvox.Api.internalEnable, now
    moved to cvox_gdocs_script.js)
    
    Tested this change manually and:
    ChromeVox still turns on "accessibility" mode in Google Docs
    pages when ChromeVox is loaded (via injected_script_loader.js)
    and when a new Google Docs page is loaded (via manifest).
    Added 'console.error' statements to cvox API `send_` method and
    content_script_bridge methods and found no console errors after
    running ChromeVox on Google Docs, Sheets, Slides, Forms, and
    Drawings.
    
    Bug: b:280303318
    Test: Manual
    Change-Id: I60ecbe4114f2f86dbfd5b1c099d6128e3dff8bb3
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496789
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138732}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/classic_background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/classic_background.js
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @fileoverview Script that runs on the background page.
  */
-import {ContentScriptBridge} from '../common/content_script_bridge.js';
 import {QueueMode, TtsSpeechProperties} from '../common/tts_types.js';
 
 import {ChromeVox} from './chromevox.js';
@@ -17,8 +16,6 @@ import {InjectedScriptLoader} from './injected_script_loader.js';
  */
 export class ChromeVoxBackground {
   constructor() {
-    this.addBridgeListener();
-
     this.injectContentScriptForGoogleDocs_();
   }
 
@@ -67,27 +64,8 @@ export class ChromeVoxBackground {
         new TtsSpeechProperties(msg['properties']));
   }
 
-  /**
-   * Listen for connections from our content script bridges, and dispatch the
-   * messages to the proper destination.
-   */
-  addBridgeListener() {
-    ContentScriptBridge.addMessageListener((msg, port) => {
-      if (msg['target'] !== 'TTS') {
-        return;
-      }
-
-      try {
-        this.onTtsMessage(msg);
-      } catch (err) {
-        console.log(err);
-      }
-    });
-  }
-
   /** Initializes classic background object. */
   static init() {
-    ContentScriptBridge.init();
     const background = new ChromeVoxBackground();
   }
 }
