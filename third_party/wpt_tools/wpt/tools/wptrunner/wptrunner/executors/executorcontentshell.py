1d6059834b02b42707b7e273c3f80d10f833dadf
Jonathan Lee <jonathanjlee@google.com>
2023-05-03 22:09:32 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 3cc3f5123a8a092697490c66e6c69cf614523d87
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I56f34dbfd9f5ef7a25f8d766aca3ca1ee55d8925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4500506
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1139221}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executorcontentshell.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executorcontentshell.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # mypy: allow-untyped-defs
 
 from .base import RefTestExecutor, RefTestImplementation, CrashtestExecutor, TestharnessExecutor
+from .executorchrome import make_sanitizer_mixin
 from .protocol import Protocol, ProtocolPart
 from time import time
 from queue import Empty
@@ -189,7 +190,25 @@ def _convert_exception(test, exception, errors):
     raise exception
 
 
-class ContentShellRefTestExecutor(RefTestExecutor):
+class ContentShellCrashtestExecutor(CrashtestExecutor):
+    def __init__(self, logger, browser, server_config, timeout_multiplier=1, debug_info=None,
+            **kwargs):
+        super().__init__(logger, browser, server_config, timeout_multiplier, debug_info, **kwargs)
+        self.protocol = ContentShellProtocol(self, browser)
+
+    def do_test(self, test):
+        try:
+            _ = self.protocol.content_shell_test.do_test(self.test_url(test), test.timeout * self.timeout_multiplier)
+            self.protocol.content_shell_errors.read_errors()
+            return self.convert_result(test, {"status": "PASS", "message": None})
+        except BaseException as exception:
+            return _convert_exception(test, exception, self.protocol.content_shell_errors.read_errors())
+
+
+_SanitizerMixin = make_sanitizer_mixin(ContentShellCrashtestExecutor)
+
+
+class ContentShellRefTestExecutor(RefTestExecutor, _SanitizerMixin):  # type: ignore
     def __init__(self, logger, browser, server_config, timeout_multiplier=1, screenshot_cache=None,
             debug_info=None, reftest_screenshot="unexpected", **kwargs):
         super().__init__(logger, browser, server_config, timeout_multiplier, screenshot_cache,
@@ -235,22 +254,7 @@ class ContentShellPrintRefTestExecutor(ContentShellRefTestExecutor):
     is_print = True
 
 
-class ContentShellCrashtestExecutor(CrashtestExecutor):
-    def __init__(self, logger, browser, server_config, timeout_multiplier=1, debug_info=None,
-            **kwargs):
-        super().__init__(logger, browser, server_config, timeout_multiplier, debug_info, **kwargs)
-        self.protocol = ContentShellProtocol(self, browser)
-
-    def do_test(self, test):
-        try:
-            _ = self.protocol.content_shell_test.do_test(self.test_url(test), test.timeout * self.timeout_multiplier)
-            self.protocol.content_shell_errors.read_errors()
-            return self.convert_result(test, {"status": "PASS", "message": None})
-        except BaseException as exception:
-            return _convert_exception(test, exception, self.protocol.content_shell_errors.read_errors())
-
-
-class ContentShellTestharnessExecutor(TestharnessExecutor):
+class ContentShellTestharnessExecutor(TestharnessExecutor, _SanitizerMixin):  # type: ignore
     def __init__(self, logger, browser, server_config, timeout_multiplier=1, debug_info=None,
             **kwargs):
         super().__init__(logger, browser, server_config, timeout_multiplier, debug_info, **kwargs)
