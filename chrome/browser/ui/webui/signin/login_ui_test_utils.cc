86206e413de8ea77b134f9d2e7cbbac0a194c796
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-05-03 19:23:40 +0000

    [fyfre] Remove legacy style support from chrome://sync-confirmation
    Bug: 1395809
    Change-Id: Ic64968bf93829567a1ccad2f4b01c42cafadf787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481323
    Commit-Queue: Nicolas Dossou-gbete <dgn@chromium.org>
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139065}

--- chrome/browser/ui/webui/signin/login_ui_test_utils.cc
+++ chrome/browser/ui/webui/signin/login_ui_test_utils.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/signin_features.h"
 #include "chrome/browser/signin/signin_promo.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/chrome_pages.h"
@@ -215,9 +214,7 @@ std::string GetButtonIdForSyncConfirmationDialogAction(
     case SyncConfirmationDialogAction::kConfirm:
       return "confirmButton";
     case SyncConfirmationDialogAction::kCancel:
-      return base::FeatureList::IsEnabled(switches::kTangibleSync)
-                 ? "notNowButton"
-                 : "cancelButton";
+      return "notNowButton";
   }
 }
 
