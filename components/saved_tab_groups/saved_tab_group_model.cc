5156017d3cc197cd9fb425c83ae718c8c32f5ee9
dljames <dljames@google.com>
2023-05-03 00:34:53 +0000

    [ SavedTabGroup ] Fix wrong ids passed in tab observer functions
    While investigating the reason why some synced tab updates are dropped,
    I noticed that some tab function in SavedTabGroupModel were not sending
    the appropriate updates in the observer functions.
    
    Updated SavedTabGroupModel Functions:
    - UpdateTabInGroup
    - RemoveTabFromGroup
    - ReplaceTabInGroupAt
    
    Change-Id: I9d23146fbb26da9bd7a322a201ecc48b54aae91f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4501241
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138730}

--- components/saved_tab_groups/saved_tab_group_model.cc
+++ components/saved_tab_groups/saved_tab_group_model.cc
@@ -241,7 +241,7 @@ void SavedTabGroupModel::UpdateTabInGroup(const base::Uuid& group_id,
   saved_tab_groups_[group_index.value()].UpdateTab(tab);
 
   for (auto& observer : observers_) {
-    observer.SavedTabGroupUpdatedLocally(group_id);
+    observer.SavedTabGroupUpdatedLocally(group_id, tab.saved_tab_guid());
   }
 }
 
@@ -272,7 +272,7 @@ void SavedTabGroupModel::RemoveTabFromGroup(const base::Uuid& group_id,
   // to pass a group_id and an optional tab_id.
   if (!update_tab_positions) {
     for (auto& observer : observers_) {
-      observer.SavedTabGroupUpdatedFromSync(group_id);
+      observer.SavedTabGroupUpdatedFromSync(group_id, copy_tab_id);
     }
   } else {
     for (auto& observer : observers_) {
@@ -352,7 +352,7 @@ std::unique_ptr<sync_pb::SavedTabGroupSpecifics> SavedTabGroupModel::MergeTab(
 
   for (auto& observer : observers_) {
     observer.SavedTabGroupUpdatedFromSync(group_guid,
-                                          merged_tab.saved_group_guid());
+                                          merged_tab.saved_tab_guid());
   }
 
   return merged_tab.ToSpecifics();
