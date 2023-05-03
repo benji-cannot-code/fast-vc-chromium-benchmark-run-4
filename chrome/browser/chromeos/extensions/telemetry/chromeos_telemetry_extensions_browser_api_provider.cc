de05866401d3b2b4b40aa65ca6caf712e0a4d309
Bastian Kersting <bkersting@google.com>
2023-05-03 12:47:12 +0000

    [Telemetry] Fix target for generating bindings
    Currently chrome/browser/chromeos/extensions/telemetry/api/{diagnostics,
    events, telemetry}/generated_api_registration.h has the same content
    because we build the target in a duplicated way. This CL instead
    generates the bindings in one single place.
    
    Bug: b/275360296
    Change-Id: Ic6450d02ba4f8de88a2c62ca9324de0fa7b90070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4498827
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138883}

--- chrome/browser/chromeos/extensions/telemetry/chromeos_telemetry_extensions_browser_api_provider.cc
+++ chrome/browser/chromeos/extensions/telemetry/chromeos_telemetry_extensions_browser_api_provider.cc
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/extensions/telemetry/chromeos_telemetry_extensions_browser_api_provider.h"
 
-#include "chrome/browser/chromeos/extensions/telemetry/api/diagnostics/generated_api_registration.h"
-#include "chrome/browser/chromeos/extensions/telemetry/api/events/generated_api_registration.h"
-#include "chrome/browser/chromeos/extensions/telemetry/api/telemetry/generated_api_registration.h"
+#include "chrome/browser/chromeos/extensions/telemetry/api/generated_api_registration.h"
 #include "extensions/browser/extension_function_registry.h"
 
 namespace chromeos {
