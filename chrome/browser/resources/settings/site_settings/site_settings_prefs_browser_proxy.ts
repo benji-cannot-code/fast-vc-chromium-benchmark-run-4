73af144b99546a7daf93bfa396fdc8fbe8674ace
Robbie McElrath <rmcelrath@chromium.org>
2023-05-03 21:27:50 +0000

    iwa: Append app id in Site Details title
    This updates the Site Details page to display the IWA app id
    in parenthesis after the app name "App Name (ID: <id>)". This matches
    the formatting used by extensions.
    
    Bug: 1311065
    Change-Id: I5a5506b2821a55b12109a5e0542e5b393ef65698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482810
    Reviewed-by: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139147}

--- chrome/browser/resources/settings/site_settings/site_settings_prefs_browser_proxy.ts
+++ chrome/browser/resources/settings/site_settings/site_settings_prefs_browser_proxy.ts
@@ -82,7 +82,6 @@ export interface RawSiteException {
   isEmbargoed: boolean;
   origin: string;
   displayName: string;
-  extensionNameWithId?: string;
   type: string;
   setting: ContentSetting;
   source: SiteSettingSource;
--- chrome/browser/ui/webui/settings/site_settings_helper.h
+++ chrome/browser/ui/webui/settings/site_settings_helper.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class HostContentSettingsMap;
 class Profile;
+struct UrlIdentity;
 
 namespace content {
 class WebUI;
@@ -70,7 +71,6 @@ constexpr char kFileReadGrants[] = "fileReadGrants";
 constexpr char kFileWriteGrants[] = "fileWriteGrants";
 constexpr char kNotificationInfoString[] = "notificationInfoString";
 constexpr char kPermissions[] = "permissions";
-constexpr char kExtensionNameWithId[] = "extensionNameWithId";
 
 enum class SiteSettingSource {
   kAllowlist,
@@ -151,8 +151,7 @@ ContentSetting GetContentSettingForOrigin(Profile* profile,
                                           const HostContentSettingsMap* map,
                                           const GURL& origin,
                                           ContentSettingsType content_type,
-                                          std::string* source_string,
-                                          std::string* display_name);
+                                          std::string* source_string);
 
 // Returns URLs with granted entries from the File System Access API.
 void GetFileSystemGrantedEntries(std::vector<base::Value::Dict>* exceptions,
@@ -199,16 +198,14 @@ base::Value::List GetChooserExceptionListFromProfile(
     Profile* profile,
     const ChooserTypeNameEntry& chooser_type);
 
-// Returns the short name of a browser extension, or nullopt if `origin` is not
-// an extension URL.
-absl::optional<std::string> GetExtensionDisplayName(Profile* profile,
-                                                    GURL origin);
-
 // Takes |url| and converts it into an individual origin string or retrieves
 // name of the extension or Isolated Web App it belongs to. If |hostname_only|
 // is true, returns |url|'s hostname for HTTP/HTTPS pages or unknown
 // extension/IWA URLs, otherwise an origin string will be returned that
 // includes the scheme if it's non-cryptographic.
+UrlIdentity GetUrlIdentityForGURL(Profile* profile,
+                                  const GURL& url,
+                                  bool hostname_only);
 std::string GetDisplayNameForGURL(Profile* profile,
                                   const GURL& url,
                                   bool hostname_only);
