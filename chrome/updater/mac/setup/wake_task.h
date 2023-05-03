879f46d519b10bfa115ecdaa4ff41133eebbdf2a
Joshua Pawlicki <waffles@chromium.org>
2023-05-03 13:53:26 +0000

    Updater: Only reload wake tasks if they are changed.
    Also refactors the reload to use bootout/bootstrap and avoid the shell.
    Also cleans up some unnecessary functions.
    
    Bug: 1440706
    Change-Id: I87c0a96efdb8d1da35a9af2811249f79551d9d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4493717
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138902}

---
+++ chrome/updater/mac/setup/wake_task.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_UPDATER_MAC_SETUP_WAKE_TASK_H_
+#define CHROME_UPDATER_MAC_SETUP_WAKE_TASK_H_
+
+#include <CoreFoundation/CoreFoundation.h>
+
+#include "base/mac/scoped_cftyperef.h"
+#include "chrome/updater/updater_scope.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace updater {
+
+absl::optional<base::ScopedCFTypeRef<CFDictionaryRef>> CreateWakeLaunchdPlist(
+    UpdaterScope scope);
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_MAC_SETUP_WAKE_TASK_H_
