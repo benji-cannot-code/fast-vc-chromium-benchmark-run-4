1d4e3575047054f9ca9be9c85af55608372cdcac
Avi Drissman <avi@chromium.org>
2023-05-03 21:08:42 +0000

    Convert /url to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: I62784e759a938efdbd5436d4f98b84c3487e6f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504015
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139114}

--- url/url_idna_icu_alternatives_ios.mm
+++ url/url_idna_icu_alternatives_ios.mm
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "url/url_canon_internal.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace url {
 
 // Only allow ASCII to avoid ICU dependency. Use NSString+IDN
