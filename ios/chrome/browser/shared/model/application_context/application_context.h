882c6d0fde8b16fea9f4b88955e8f02e6c2d9fac
Gauthier Ambard <gambard@chromium.org>
2023-05-03 10:55:26 +0000

    [iOS] Prepare Browser, WSL and ApplicationContext to be moved
    This CL creates files that will be used to move the Browser,
    WebStateList and ApplicationContext later.
    
    Bug: 1319852
    Change-Id: I76c4ba8f2e639d978ea7df986afb685a8ad368b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496152
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138858}

---
+++ ios/chrome/browser/shared/model/application_context/application_context.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_APPLICATION_CONTEXT_APPLICATION_CONTEXT_H_
+#define IOS_CHROME_BROWSER_SHARED_MODEL_APPLICATION_CONTEXT_APPLICATION_CONTEXT_H_
+
+// Temporary include.
+#import "ios/chrome/browser/application_context/application_context.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_APPLICATION_CONTEXT_APPLICATION_CONTEXT_H_
---
+++ ios/chrome/browser/shared/model/web_state_list/web_state_list.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_WEB_STATE_LIST_WEB_STATE_LIST_H_
+#define IOS_CHROME_BROWSER_SHARED_MODEL_WEB_STATE_LIST_WEB_STATE_LIST_H_
+
+// Temporary include.
+#import "ios/chrome/browser/web_state_list/web_state_list.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_WEB_STATE_LIST_WEB_STATE_LIST_H_
