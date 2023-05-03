5b42c240ffbc2228e73d18fb43749fd44e833164
David Bertoni <dbertoni@chromium.org>
2023-05-03 22:39:54 +0000

    Revert "Implement `ExperimentalAsh` raw_ptr trait"
    This reverts commit 07d0df3de8a7ea743a695dfd08c97cf7e2f27dd4.
    
    Reason for revert: Causing numerous test failures:
    
    https://luci-analysis.appspot.com/p/chromium/clusters?selectedMetrics=human-cls-failed-presubmit%2Ccritical-failures-exonerated%2Cfailures&orderBy=critical-failures-exonerated&orderDir=desc&interval=24h&q=test_id%3A%22BackupRefPtrTest.ExperimentalAsh%22
    
    Original change's description:
    > Implement `ExperimentalAsh` raw_ptr trait
    >
    > See the new ChromeOS ash launch plan:
    > https://docs.google.com/document/d/105OVhNl-2lrfWElQSk5BXYv-nLynfxUrbC4l8cZ0CoU/edit
    >
    > These pointers will be evaluated in a separate finch experiment from
    > PartitionAllocBackupRefPtr, and the trait is needed to differentiate
    > the new raw_ptr from the existing ones.
    >
    > Bug: 1435441,1430482
    > Change-Id: I9414a9638c8821814c4b9e0b05d5194dd12587ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456018
    > Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    > Reviewed-by: Keishi Hattori <keishi@chromium.org>
    > Reviewed-by: Sergei Glazunov <glazunov@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1138393}
    
    Bug: 1435441,1430482
    Change-Id: Ib11323e350b0141bd0717d2cdb2a5ce7832d7ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503786
    Owners-Override: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139242}

--- base/allocator/partition_allocator/pointers/raw_ptr_backup_ref_impl.cc
+++ base/allocator/partition_allocator/pointers/raw_ptr_backup_ref_impl.cc
@@ -17,11 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base::internal {
 
-template <bool AllowDangling, bool ExperimentalAsh>
-void RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::AcquireInternal(
-    uintptr_t address) {
+template <bool AllowDangling>
+void RawPtrBackupRefImpl<AllowDangling>::AcquireInternal(uintptr_t address) {
 #if BUILDFLAG(PA_DCHECK_IS_ON) || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
-  PA_BASE_CHECK(UseBrp(address));
+  PA_BASE_CHECK(partition_alloc::IsManagedByPartitionAllocBRPPool(address));
 #endif
   uintptr_t slot_start =
       partition_alloc::PartitionAllocGetSlotStartInBRPPool(address);
@@ -33,11 +32,10 @@ void RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::AcquireInternal(
   }
 }
 
-template <bool AllowDangling, bool ExperimentalAsh>
-void RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::ReleaseInternal(
-    uintptr_t address) {
+template <bool AllowDangling>
+void RawPtrBackupRefImpl<AllowDangling>::ReleaseInternal(uintptr_t address) {
 #if BUILDFLAG(PA_DCHECK_IS_ON) || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
-  PA_BASE_CHECK(UseBrp(address));
+  PA_BASE_CHECK(partition_alloc::IsManagedByPartitionAllocBRPPool(address));
 #endif
   uintptr_t slot_start =
       partition_alloc::PartitionAllocGetSlotStartInBRPPool(address);
@@ -54,9 +52,9 @@ void RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::ReleaseInternal(
   }
 }
 
-template <bool AllowDangling, bool ExperimentalAsh>
-void RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::
-    ReportIfDanglingInternal(uintptr_t address) {
+template <bool AllowDangling>
+void RawPtrBackupRefImpl<AllowDangling>::ReportIfDanglingInternal(
+    uintptr_t address) {
   if (partition_alloc::internal::IsUnretainedDanglingRawPtrCheckEnabled()) {
     if (IsSupportedAndNotNull(address)) {
       uintptr_t slot_start =
@@ -68,11 +66,11 @@ void RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::
 }
 
 // static
-template <bool AllowDangling, bool ExperimentalAsh>
-bool RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::
-    CheckPointerWithinSameAlloc(uintptr_t before_addr,
-                                uintptr_t after_addr,
-                                size_t type_size) {
+template <bool AllowDangling>
+bool RawPtrBackupRefImpl<AllowDangling>::CheckPointerWithinSameAlloc(
+    uintptr_t before_addr,
+    uintptr_t after_addr,
+    size_t type_size) {
   partition_alloc::internal::PtrPosWithinAlloc ptr_pos_within_alloc =
       partition_alloc::internal::IsPtrWithinSameAlloc(before_addr, after_addr,
                                                       type_size);
@@ -90,11 +88,10 @@ bool RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::
 #endif
 }
 
-template <bool AllowDangling, bool ExperimentalAsh>
-bool RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::IsPointeeAlive(
-    uintptr_t address) {
+template <bool AllowDangling>
+bool RawPtrBackupRefImpl<AllowDangling>::IsPointeeAlive(uintptr_t address) {
 #if BUILDFLAG(PA_DCHECK_IS_ON) || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
-  PA_BASE_CHECK(UseBrp(address));
+  PA_BASE_CHECK(partition_alloc::IsManagedByPartitionAllocBRPPool(address));
 #endif
   uintptr_t slot_start =
       partition_alloc::PartitionAllocGetSlotStartInBRPPool(address);
@@ -104,14 +101,8 @@ bool RawPtrBackupRefImpl<AllowDangling, ExperimentalAsh>::IsPointeeAlive(
 
 // Explicitly instantiates the two BackupRefPtr variants in the .cc. This
 // ensures the definitions not visible from the .h are available in the binary.
-template struct RawPtrBackupRefImpl</*AllowDangling=*/false,
-                                    /*ExperimentalAsh=*/false>;
-template struct RawPtrBackupRefImpl</*AllowDangling=*/false,
-                                    /*ExperimentalAsh=*/true>;
-template struct RawPtrBackupRefImpl</*AllowDangling=*/true,
-                                    /*ExperimentalAsh=*/false>;
-template struct RawPtrBackupRefImpl</*AllowDangling=*/true,
-                                    /*ExperimentalAsh=*/true>;
+template struct RawPtrBackupRefImpl</*AllowDangling=*/false>;
+template struct RawPtrBackupRefImpl</*AllowDangling=*/true>;
 
 #if BUILDFLAG(PA_DCHECK_IS_ON) || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
 void CheckThatAddressIsntWithinFirstPartitionPage(uintptr_t address) {
