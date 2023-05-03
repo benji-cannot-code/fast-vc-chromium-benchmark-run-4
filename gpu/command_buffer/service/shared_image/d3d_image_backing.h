b445e646ef69c24d492da320d740be7300fbe5e2
Peng Huang <penghuang@chromium.org>
2023-05-03 17:02:31 +0000

    webgpu: switch to use ExternalImage related API in namespace d3d
    Those methods will support both d3d11 and d3d12, and have been moved
    into d3d namespace.
    
    Bug: dawn:1705
    Bug: dawn:1724
    Change-Id: I93e12bb056fe2b7ebc7ce1c7744b341a34415731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502506
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138957}

--- gpu/command_buffer/service/shared_image/d3d_image_backing.h
+++ gpu/command_buffer/service/shared_image/d3d_image_backing.h
@@ -29,11 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Usage of BUILDFLAG(USE_DAWN) needs to be after the include for
 // ui/gl/buildflags.h
 #if BUILDFLAG(USE_DAWN)
-#include <dawn/native/D3D12Backend.h>
-using dawn::native::d3d12::ExternalImageDescriptorDXGISharedHandle;
-using dawn::native::d3d12::ExternalImageDXGI;
-using dawn::native::d3d12::ExternalImageDXGIBeginAccessDescriptor;
-using dawn::native::d3d12::ExternalImageDXGIFenceDescriptor;
+#include <dawn/native/D3DBackend.h>
+using dawn::native::d3d::ExternalImageDescriptorDXGISharedHandle;
+using dawn::native::d3d::ExternalImageDXGI;
+using dawn::native::d3d::ExternalImageDXGIBeginAccessDescriptor;
+using dawn::native::d3d::ExternalImageDXGIFenceDescriptor;
 #endif  // BUILDFLAG(USE_DAWN)
 
 namespace gfx {
