cd867f786b2dfd7317ad41f280a2a1220c1081d7
Peter Kvitek <kvitekp@chromium.org>
2023-05-03 22:32:54 +0000

    [browser_tests] Fixed dangling pointer use in InProcessBrowserTest
    InProcessBrowserTest::PreRunTestOnMainThread() retrieves an
    active web contents pointer and stores it in a local variable,
    then calls content::WaitForLoadStop(). After that it dereferences
    the stored pointer which may be invalid because the WebContents
    it refers to could have been deleted during the wait loop.
    
    This CL replaces the raw pointer with a base::WeakPtr which is
    checked before use.
    
    Related fix: https://crrev.com/c/4500659
    
    Change-Id: I5107594f5baf029364049f33d0f344c5bf0c96b4
    Bug: 1442328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504292
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139240}

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
+#include "base/memory/weak_ptr.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
 #include "base/sampling_heap_profiler/poisson_allocation_sampler.h"
@@ -784,9 +785,12 @@ void InProcessBrowserTest::PreRunTestOnMainThread() {
 
   SelectFirstBrowser();
   if (browser_) {
-    auto* tab = browser_->tab_strip_model()->GetActiveWebContents();
-    content::WaitForLoadStop(tab);
-    SetInitialWebContents(tab);
+    base::WeakPtr<content::WebContents> tab =
+        browser_->tab_strip_model()->GetActiveWebContents()->GetWeakPtr();
+    content::WaitForLoadStop(tab.get());
+    if (tab) {
+      SetInitialWebContents(tab.get());
+    }
   }
 
 #if !BUILDFLAG(IS_ANDROID)
