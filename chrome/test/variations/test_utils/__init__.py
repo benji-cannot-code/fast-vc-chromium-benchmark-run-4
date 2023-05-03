b999a6298940e67b4633b455335d4fe3f425123c
Hao Wu <haowoo@google.com>
2023-05-03 21:44:22 +0000

    Add the seed locator fixture.
    The locator can be requested via seed_locator. It takes one argument, seed name, and returns the path. Multiple seed files are defined by names so the test don't need to worry about seed locations and file names for different platforms/channels and experiments groups. The returned path can be used directly to the driver factory.
    
    Bug: b/279912663
    Change-Id: I327a732c92a1528e6cddb7e8b9e348810ba05110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4497166
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Hao Wu <haowoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1139176}

--- chrome/test/variations/test_utils/__init__.py
+++ chrome/test/variations/test_utils/__init__.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from .downloader import *
-from .helper import *
-from .http import *
+from chrome.test.variations.test_utils.downloader import *
+from chrome.test.variations.test_utils.helper import *
+from chrome.test.variations.test_utils.http import *
+from chrome.test.variations.test_utils.defines import *
--- chrome/test/variations/test_utils/defines.py
+++ chrome/test/variations/test_utils/defines.py
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from chrome.test.variations.fixtures.seed_locator import SeedLocator
-from chrome.test.variations.fixtures.seed_locator import SeedName
-from chrome.test.variations.fixtures.skia_gold import VariationsSkiaGoldUtil
+import os
+
+SRC_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), *([os.pardir] * 4)))
+
+TEST_DATA_DIR = os.path.join(
+    SRC_DIR, 'chrome', 'test', 'data', 'variations')
