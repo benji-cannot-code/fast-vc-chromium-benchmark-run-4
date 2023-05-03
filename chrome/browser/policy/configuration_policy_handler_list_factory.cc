9362d3895d65c976b37dd30967da997e396e854a
Brahim Chikhaoui <bchikhaoui@google.com>
2023-05-03 03:58:16 +0000

    Add a user policy to skip displaySize screen in oobe
    The policy will skip by default the displaySize screen
    in oobe for the end users and allow admins to enable the
    feature if they want.
    
    Bug: b:280402231
    Change-Id: I27b90588164a466515ffe0b15d16116caa2f3fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492434
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138797}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1422,6 +1422,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kUserAvatarCustomizationSelectorsEnabled,
     ash::user_image::prefs::kUserAvatarCustomizationSelectorsEnabled,
     base::Value::Type::BOOLEAN },
+  { key::kShowDisplaySizeScreenEnabled,
+    ash::prefs::kShowDisplaySizeScreenEnabled,
+    base::Value::Type::BOOLEAN },
 #endif // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_LINUX)
