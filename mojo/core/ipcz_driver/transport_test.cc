c1e4b9f21394f2128910f6098a2b41eb52393152
Ken Rockot <rockot@google.com>
2023-05-03 06:52:33 +0000

    mojo: Introduce DirectReceiver
    Experimental type which enables select use cases to establish Mojo
    interface receiver that receives its IPCs directly on its binding thread
    without a hop through the process's IO thread first.
    
    This is not ever meant to be used widely and a more robust approach
    could be to have Mojo manage thread-local node overrides, pending a
    bunch of other changes that would be needed first.
    
    For now this is defined so that Chrome developers can experiment with
    specific use cases, like the renderer's input handler on its compositor
    thread.
    
    Bug: None
    Change-Id: I11c7d6bc41bd2855541a98752f87e2420f1096fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4429594
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138821}

--- mojo/core/ipcz_driver/transport_test.cc
+++ mojo/core/ipcz_driver/transport_test.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/test/gtest_util.h"
+#include "base/test/task_environment.h"
 #include "build/build_config.h"
 #include "mojo/core/ipcz_driver/driver.h"
 #include "mojo/core/ipcz_driver/shared_buffer.h"
