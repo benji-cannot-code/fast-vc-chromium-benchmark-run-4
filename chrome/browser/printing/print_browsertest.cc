ba4feead7a6abc88e8f6a1a9c650ce9ed1a7c2fc
Roland Bock <rbock@google.com>
2023-05-03 17:49:09 +0000

    [DMToken] Migrate away from Create*ForTesting methods
    Migrate browser and unit tests.
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: b/280046033
    Test: Refactored unit tests
    Change-Id: I764be9c90c157d6fbf9dadd409c4ec5312bfd7a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4497350
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Roland Bock <rbock@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138988}

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -1960,7 +1960,7 @@ class ContentAnalysisPrintBrowserTest
  public:
   ContentAnalysisPrintBrowserTest() {
     policy::SetDMTokenForTesting(
-        policy::DMToken::CreateValidTokenForTesting(kFakeDmToken));
+        policy::DMToken::CreateValidToken(kFakeDmToken));
     enterprise_connectors::ContentAnalysisDelegate::SetFactoryForTesting(
         base::BindRepeating(
             &enterprise_connectors::FakeContentAnalysisDelegate::Create,
