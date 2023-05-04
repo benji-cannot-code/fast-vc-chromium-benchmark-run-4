11580031b8faa35aa7668bbab73c4c59333efe19
François Doray <fdoray@chromium.org>
2023-05-04 22:05:37 +0000

    [ash] Remove ProcessDataCollector.
    It has been disabled since M110 via the ReduceCpuUtilization feature
    and is considered no longer useful.
    
    Bug: 1295441
    Change-Id: I7c471c7e8b4a73258f19a2f6c45b39df2609b0c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4501805
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139799}

--- chrome/browser/ash/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/chrome_browser_main_parts_ash.cc
@@ -143,7 +143,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/power/ml/adaptive_screen_brightness_manager.h"
 #include "chrome/browser/ash/power/power_data_collector.h"
 #include "chrome/browser/ash/power/power_metrics_reporter.h"
-#include "chrome/browser/ash/power/process_data_collector.h"
 #include "chrome/browser/ash/power/renderer_freezer.h"
 #include "chrome/browser/ash/power/smart_charging/smart_charging_manager.h"
 #include "chrome/browser/ash/printing/bulk_printers_calculator_factory.h"
@@ -526,7 +525,6 @@ class DBusServices {
 
     // Initialize PowerDataCollector after DBusThreadManager is initialized.
     PowerDataCollector::Initialize();
-    ProcessDataCollector::Initialize();
 
     LoginState::Initialize();
     TPMTokenLoader::Initialize();
@@ -590,7 +588,6 @@ class DBusServices {
     lock_to_single_user_service_.reset();
     fusebox_service_.reset();
     mojo_connection_service_.reset();
-    ProcessDataCollector::Shutdown();
     PowerDataCollector::Shutdown();
     chromeos::PowerPolicyController::Shutdown();
     device::BluetoothAdapterFactory::Shutdown();
--- chrome/browser/resources/chromeos/power.js
+++ chrome/browser/resources/chromeos/power.js
@@ -9,20 +9,6 @@ import {sendWithPromise} from 'chrome://resources/ash/common/cr.m.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 import {$} from 'chrome://resources/ash/common/util.js';
 
-/**
- * The different types of power consumer types. Should be kept in sync with the
- * PowerConsumerType in ProcessDataCollector.
- * @enum {number}
- */
-const PowerConsumerType = {
-  SCREEN: 0,
-  KEYBOARD: 1,
-  CROSTINI: 2,
-  ARC: 3,
-  CHROME: 4,
-  SYSTEM: 5,
-};
-
 const devicePixelRatio = window.devicePixelRatio;
 
 /**
@@ -824,11 +810,6 @@ function showCpuFreqData({freqStateData, systemResumedData}) {
       'frequencyStateOccupancyPercentageHeader', 'MHz', 'cpu-freq-plots-div');
 }
 
-function showProcessUsageData(processUsageData) {
-  // TODO(crbug.com/851767): Add the code to create a suitable UI for this
-  // information.
-}
-
 function requestBatteryChargeData() {
   sendWithPromise('requestBatteryChargeData').then(showBatteryChargeData);
 }
@@ -839,10 +820,6 @@ function requestCpuIdleData() {
 
 function requestCpuFreqData() {
   sendWithPromise('requestCpuFreqData').then(showCpuFreqData);
-}
-
-function requestProcessUsageData() {
-  sendWithPromise('requestProcessUsageData').then(showProcessUsageData);
 }
 
 /**
