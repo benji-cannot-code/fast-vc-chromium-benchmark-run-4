6483159194f36d8e0a640b86424ef3eaab35f799
Dan Clark <daniec@microsoft.com>
2023-05-04 19:36:01 +0000

    EditContext: Handle WebLocalFrameImpl::FirstRectForCharacterRange
    On Mac, when there is an active composition for an EditContext and
    the author has not called updateCharacterBounds before the next call
    to RenderWidgetHostViewCocoa firstRectForCharacterRange, the browser
    process will fall back to calling
    WebLocalFrameImpl::FirstRectForCharacterRange in the render process.
    
    This function does not take EditContext into account, and will try
    to find the right rect by positioning a range in the DOM, which will
    often produce an entirely wrong result if the composition is
    happening in an EditContext.
    
    Fix this by implementing FirstRectForCharacterRange for EditContext.
    First we attempt to use the character bounds provided by the author,
    but fallback to using the control bounds if characterBounds are
    not available.
    
    Bug: 999184
    Change-Id: I7d1b2e44ba88e743cbcb9c27bc0894d7c9001f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4497408
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139714}

--- third_party/blink/renderer/core/exported/web_input_method_controller_impl.h
+++ third_party/blink/renderer/core/exported/web_input_method_controller_impl.h
@@ -51,7 +51,7 @@ class CORE_EXPORT WebInputMethodControllerImpl
   WebTextInputInfo TextInputInfo() override;
   int ComputeWebTextInputNextPreviousFlags() override;
   WebTextInputType TextInputType() override;
-  WebRange CompositionRange() override;
+  WebRange CompositionRange() const override;
   bool GetCompositionCharacterBounds(WebVector<gfx::Rect>& bounds) override;
 
   WebRange GetSelectionOffsets() const override;
