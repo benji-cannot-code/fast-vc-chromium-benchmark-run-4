e00a14a3b662d2df5490b8232eaa0dc5735640de
Melissa Zhang <melzhang@google.com>
2023-05-04 04:21:54 +0000

    [Preloads,Dedupe] Reduce invalid traffic to server.
    The server is getting high traffic of queries with invalid API
    keys. To fix this, we verify that the build is using the official
    Google-internal file containing the official API keys for preloads
    and dedupe to run. Otherwise there is no point to instantiate
    the services, as any queries to the server with an invalid API
    key will fail.
    
    Bug: b/280374997
    Change-Id: Ib994bc87218bf61fde3ea05602983338257b15fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4495756
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139344}

--- chrome/browser/apps/app_preload_service/app_preload_service_factory.cc
+++ chrome/browser/apps/app_preload_service/app_preload_service_factory.cc
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/chrome_features.h"
+#include "google_apis/google_api_keys.h"
+
+namespace {
+bool g_skip_api_key_check = false;
+}  // namespace
 
 namespace apps {
 
@@ -49,6 +54,12 @@ bool AppPreloadServiceFactory::IsAvailable(Profile* profile) {
     return false;
   }
 
+  // Ensure that the build uses the Google-internal file containing the
+  // official API keys, which are required to make queries to the Almanac.
+  if (!google_apis::IsGoogleChromeAPIKeyUsed() && !g_skip_api_key_check) {
+    return false;
+  }
+
   if (!apps::AppServiceProxyFactory::IsAppServiceAvailableForProfile(profile)) {
     return false;
   }
@@ -65,6 +76,12 @@ bool AppPreloadServiceFactory::IsAvailable(Profile* profile) {
   }
 
   return true;
+}
+
+// static
+void AppPreloadServiceFactory::SkipApiKeyCheckForTesting(
+    bool skip_api_key_check) {
+  g_skip_api_key_check = skip_api_key_check;
 }
 
 KeyedService* AppPreloadServiceFactory::BuildServiceInstanceFor(
--- chrome/browser/apps/app_preload_service/app_preload_service_unittest.cc
+++ chrome/browser/apps/app_preload_service/app_preload_service_unittest.cc
@@ -52,6 +52,7 @@ class AppPreloadServiceTest : public testing::Test {
   AppPreloadServiceTest()
       : scoped_user_manager_(std::make_unique<ash::FakeChromeUserManager>()) {
     scoped_feature_list_.InitAndEnableFeature(features::kAppPreloadService);
+    AppPreloadServiceFactory::SkipApiKeyCheckForTesting(true);
   }
 
   void SetUp() override {
@@ -68,6 +69,10 @@ class AppPreloadServiceTest : public testing::Test {
     web_app::test::AwaitStartWebAppProviderAndSubsystems(GetProfile());
   }
 
+  void TearDown() override {
+    AppPreloadServiceFactory::SkipApiKeyCheckForTesting(false);
+  }
+
   Profile* GetProfile() { return profile_.get(); }
 
   ash::FakeChromeUserManager* GetFakeUserManager() const {
@@ -78,6 +83,7 @@ class AppPreloadServiceTest : public testing::Test {
   network::TestURLLoaderFactory url_loader_factory_;
 
  private:
+  // BrowserTaskEnvironment has to be the first member or test will break.
   content::BrowserTaskEnvironment task_environment_;
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<TestingProfile> profile_;
@@ -227,6 +233,11 @@ TEST_F(AppPreloadServiceTest, FirstLoginStartedNotCompletedAfterServerError) {
   // not "completed".
   EXPECT_EQ(flow_started, true);
   EXPECT_EQ(flow_completed, absl::nullopt);
+}
+
+TEST_F(AppPreloadServiceTest, ServiceDisabledInTestsWhenTestModeIsOff) {
+  AppPreloadServiceFactory::SkipApiKeyCheckForTesting(false);
+  ASSERT_FALSE(AppPreloadServiceFactory::IsAvailable(GetProfile()));
 }
 
 }  // namespace apps
