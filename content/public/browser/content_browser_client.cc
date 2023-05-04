386b8daf685381200609316cd5764374d0149aeb
Stephan Hartmann <stha09@googlemail.com>
2023-05-04 15:33:33 +0000

    libstdc++: fix incomplete type of ResponsivenessCalculatorDelegate
    Destructor of std::unique_ptr in libstdc++ uses sizeof() which
    requires full definition of class ResponsivenessCalculatorDelegate
    in return value of content::ContentBrowserClient::
    CreateResponsivenessCalculatorDelegate().
    
    Bug: 957519
    Change-Id: Iaae2428f890dff18418bbf97788435ee80c599f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506772
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1139550}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/prefetch_service_delegate.h"
 #include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/responsiveness_calculator_delegate.h"
 #include "content/public/browser/sms_fetcher.h"
 #include "content/public/browser/speculation_host_delegate.h"
 #include "content/public/browser/url_loader_request_interceptor.h"
