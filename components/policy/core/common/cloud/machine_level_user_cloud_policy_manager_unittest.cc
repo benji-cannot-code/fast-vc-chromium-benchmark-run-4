2beda4cca53cadb99aecc4d196c18fbe969bdc42
Roland Bock <rbock@google.com>
2023-05-04 14:25:11 +0000

    [DMToken] Migrate away from Create*ForTesting methods
    Migrate remaining browser and unit tests.
    
    This CL was uploaded by git cl split.
    
    R=zmin@chromium.org
    
    Bug: b/280046033
    Test: Refactored unit tests
    Change-Id: I55ca75274850ea151f04f496235a67aaa5b66a87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506691
    Commit-Queue: Owen Min <zmin@chromium.org>
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139502}

--- components/policy/core/common/cloud/machine_level_user_cloud_policy_manager_unittest.cc
+++ components/policy/core/common/cloud/machine_level_user_cloud_policy_manager_unittest.cc
@@ -24,7 +24,7 @@ class MockMachineLevelUserCloudPolicyStore
  public:
   MockMachineLevelUserCloudPolicyStore()
       : MachineLevelUserCloudPolicyStore(
-            DMToken::CreateEmptyTokenForTesting(),
+            DMToken::CreateEmptyToken(),
             std::string(),
             base::FilePath(),
             base::FilePath(),
