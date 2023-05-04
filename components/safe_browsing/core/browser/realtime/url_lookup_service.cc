9223804a775a3e617308e0f846dbfbd48cb79f3b
Xinghui Lu <xinghuilu@chromium.org>
2023-05-04 15:59:10 +0000

    Check URLLookup is enabled in CanCheckSubresourceURL
    CanPerformFullURLLookup is a prerequisite for subresource check.
    
    Bug: 1442096
    Change-Id: I1eceb486ad93aa3a95a16e30e93c0813a59e1612
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505011
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139568}

--- components/safe_browsing/core/browser/realtime/url_lookup_service.cc
+++ components/safe_browsing/core/browser/realtime/url_lookup_service.cc
@@ -140,7 +140,8 @@ bool RealTimeUrlLookupService::CanSendPageLoadToken() const {
 }
 
 bool RealTimeUrlLookupService::CanCheckSubresourceURL() const {
-  return IsEnhancedProtectionEnabled(*pref_service_);
+  return IsEnhancedProtectionEnabled(*pref_service_) &&
+         CanPerformFullURLLookup();
 }
 
 bool RealTimeUrlLookupService::CanCheckSafeBrowsingDb() const {
