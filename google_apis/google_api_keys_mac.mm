b482a84a8c5ebc46692801c19f8410bd31adc38f
Avi Drissman <avi@chromium.org>
2023-05-04 09:16:41 +0000

    Convert /google_apis to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: I7123a3ae786fbb868fbc0677202681eba7b10726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504690
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139404}

--- google_apis/google_api_keys_mac.mm
+++ google_apis/google_api_keys_mac.mm
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace google_apis {
 
 std::string GetAPIKeyFromInfoPlist(const std::string& key_name) {
