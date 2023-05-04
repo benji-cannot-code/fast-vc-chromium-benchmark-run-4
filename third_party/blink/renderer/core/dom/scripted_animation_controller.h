d789b41551b7125528e89166c096c53b71a45184
Yan,Shaobo <shaobo.yan@intel.com>
2023-05-04 02:08:53 +0000

    Simplify GPUExternalTexture expiry mechanism on HTMLVideoElement case
    GPUExternalTexture has two cache refresh entry point for
    HTMLVideoElement.
    
    One is a repeated callback registered in ScriptedAnimationController,
    the other is triggered by importExternalTexture() call.
    
    This CL removes the repeated callback to unify the cache refresh
    entry point to avoid unexpected multiple destroy situation.
    
    The CL also do some clean ups.
    
    Bug: 1430497
    Change-Id: Id3dca190cc43383a4f6ca68bd9d83b09f394d3a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456425
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139315}

--- third_party/blink/renderer/core/dom/scripted_animation_controller.h
+++ third_party/blink/renderer/core/dom/scripted_animation_controller.h
@@ -68,14 +68,6 @@ class CORE_EXPORT ScriptedAnimationController
   // when running the callbacks.
   using ExecuteVfcCallback = base::OnceCallback<void(double)>;
 
-  // Check all VideoFrames held by GPUExternalTextures are still latest.
-  // Callback returns true means VideoFrame held by the GPUExternalTexture is
-  // still the latest and keep this callback for next check.
-  // Callback returns false means VideoFrame held by GPUExternalTexture is
-  // outdated and GPUExternalTexture is expired and no need to keep this
-  // callback.
-  using WebGPUVideoFrameStateCallback = base::RepeatingCallback<bool()>;
-
   // Animation frame callbacks are used for requestAnimationFrame().
   typedef int CallbackId;
   CallbackId RegisterFrameCallback(FrameCallback*);
@@ -103,12 +95,6 @@ class CORE_EXPORT ScriptedAnimationController
 
   void DispatchEventsAndCallbacksForPrinting();
 
-  // GPUExternalTexture generated with HTMLVideoElement source needs to check
-  // new presented video frame before "update rendering" step. Listen to the
-  // scheduler to check the states.
-  void WebGPURegisterVideoFrameStateCallback(
-      WebGPUVideoFrameStateCallback webgpu_video_frame_state_callback);
-
   LocalDOMWindow* GetWindow() const;
   void ScheduleAnimationIfNeeded();
 
@@ -128,9 +114,6 @@ class CORE_EXPORT ScriptedAnimationController
   // A helper function that is called by more than one callsite.
   PageAnimator* GetPageAnimator();
   bool HasScheduledFrameTasks() const;
-  // The step to check whether related GPUExternalTexture should be expired.
-  // WebGPU Spec requires this step to happen before any update rendering steps.
-  void WebGPUCheckStateToExpireVideoFrame();
 
  private:
   ALWAYS_INLINE bool InsertToPerFrameEventsMap(const Event* event);
@@ -139,7 +122,6 @@ class CORE_EXPORT ScriptedAnimationController
   FrameRequestCallbackCollection callback_collection_;
   Vector<base::OnceClosure> task_queue_;
   Vector<ExecuteVfcCallback> vfc_execution_queue_;
-  Vector<WebGPUVideoFrameStateCallback> webgpu_video_frame_state_callbacks_;
   HeapVector<Member<Event>> event_queue_;
   using PerFrameEventsMap =
       HeapHashMap<Member<const EventTarget>, HashSet<const StringImpl*>>;
