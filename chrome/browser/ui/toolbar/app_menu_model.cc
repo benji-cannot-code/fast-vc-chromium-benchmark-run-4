a265cd90c9013d7fa9766d541543e32162ec6679
Joseph Park <josephjoopark@chromium.org>
2023-05-04 19:11:14 +0000

    Put CSC app menu item behind CSC flag
    Bug: 1427667
    Change-Id: I81951e101729b1f8b2b3d8232e208e3b7b476b9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505135
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139702}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/global_error/global_error_service.h"
 #include "chrome/browser/ui/global_error/global_error_service_factory.h"
 #include "chrome/browser/ui/managed_ui.h"
+#include "chrome/browser/ui/side_panel/companion/companion_utils.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/app_menu_icon_controller.h"
 #include "chrome/browser/ui/toolbar/bookmark_sub_menu_model.h"
@@ -1101,9 +1102,11 @@ void AppMenuModel::Build() {
     AddItemWithStringId(IDC_ROUTE_MEDIA, IDS_MEDIA_ROUTER_MENU_ITEM_TITLE);
 
   if (features::IsChromeRefresh2023()) {
-    // TODO(josephjoopark): Update CSC string with StringId when finalized.
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-    AddItem(IDC_SHOW_SEARCH_COMPANION, u"Chrome Search Companion");
+    if (companion::IsCompanionFeatureEnabled()) {
+      // TODO(josephjoopark): Update CSC string with StringId when finalized.
+      AddItem(IDC_SHOW_SEARCH_COMPANION, u"Chrome Search Companion");
+    }
 #endif
 
     // TODO(josephjoopark): Update translate string with StringId when
