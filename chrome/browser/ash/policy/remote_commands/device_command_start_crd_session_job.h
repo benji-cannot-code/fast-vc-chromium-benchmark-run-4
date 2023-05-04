7b8cf5b81d08c8337fd9911a30b20351ba7d8aa9
Jeroen Dhollander <jeroendh@google.com>
2023-05-04 14:00:00 +0000

    Block CRD session if user is different from remote command admin
    Previously there was nothing enforcing that the user who establishes
    the CRD connection is the same admin user that issued the
    'start enterprise CRD session' remote command.
    
    Bug: b/201259731
    Change-Id: I22f1ab48e95769c9fc6ad58f68ef78b76ae95a15
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4459434
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1139491}

--- chrome/browser/ash/policy/remote_commands/device_command_start_crd_session_job.h
+++ chrome/browser/ash/policy/remote_commands/device_command_start_crd_session_job.h
@@ -35,8 +35,17 @@ class DeviceCommandStartCrdSessionJob : public RemoteCommandJob {
    public:
     // Session parameters used to start the CRD host.
     struct SessionParameters {
+      SessionParameters();
+      ~SessionParameters();
+
+      SessionParameters(const SessionParameters&);
+      SessionParameters& operator=(const SessionParameters&);
+      SessionParameters(SessionParameters&&);
+      SessionParameters& operator=(SessionParameters&&);
+
       std::string oauth_token = "";
       std::string user_name = "";
+      absl::optional<std::string> admin_email;
       bool terminate_upon_input = false;
       bool show_confirmation_dialog = false;
       bool curtain_local_user_session = false;
@@ -130,6 +139,9 @@ class DeviceCommandStartCrdSessionJob : public RemoteCommandJob {
 
   // True if the admin requested a curtained remote access session.
   bool curtain_local_user_session_ = false;
+
+  // The email address of the admin user who issued the remote command.
+  absl::optional<std::string> admin_email_;
 
   // -- End of command parameters --
 
