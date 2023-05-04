c95ba8b8bac317247a4f3f462e355ee9279dabd4
Damien Dejean <damiendejean@google.com>
2023-05-04 13:19:38 +0000

    [CrOS settings] enable Passpoint flags during tests
    Enables Passpoint feature flags by default when running "known networks"
    and "network details" pages tests.
    
    Bug: b:266151248
    Test: browser_tests
    Change-Id: I298c0a458b3973049c3ebcfb0f46dc33c110fc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492769
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Damien Dejean <damiendejean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1139479}

--- chrome/test/data/webui/settings/chromeos/internet_page/internet_detail_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/internet_page/internet_detail_subpage_tests.js
@@ -181,7 +181,6 @@ suite('InternetDetailPage', function() {
       internetKnownNetworksPageTitle: 'internetKnownNetworksPageTitle',
       showMeteredToggle: true,
       isApnRevampEnabled: false,
-      isPasspointEnabled: true,
     });
 
     PolymerTest.clearBody();
@@ -656,6 +655,7 @@ suite('InternetDetailPage', function() {
           async () => {
             loadTimeData.overrideValues({
               isPasspointEnabled: isPasspointEnabled,
+              isPasspointSettingsEnabled: false,
             });
             init();
             mojoApi_.resetForTest();
