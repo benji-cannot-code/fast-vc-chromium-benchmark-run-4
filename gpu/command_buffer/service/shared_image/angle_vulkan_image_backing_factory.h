e0a14626de80b3c9f4dcce7df430d3753148c2c6
kylechar <kylechar@chromium.org>
2023-05-04 20:45:40 +0000

    Reland "Add AngleVulkanImageBacking tests"
    This is a reland of commit f85957c67183f36a5aac86f14a37a931b99cc995.
    Changed to only reset Vulkan function pointers before/after the tests
    for AngleVulkanImageBacking that need them reset. Some WebView tests
    were using Vulkan pointers after VulkanInstance destruction.
    
    Original change's description:
    > Add AngleVulkanImageBacking tests
    >
    > Implement tests for AngleVulkanImageBacking. This requires having the
    > DefaultANGLEVulkan feature enabled before the tests fixture starts so
    > the fixture has static initializer to enable all the required flags.
    >
    > This also requires resetting VulkanFunctionPointers when VulkanInstance
    > is destroyed. Modify autogen to initialize a Reset() function that
    > resets all pointers.
    >
    > Bug: none
    > Change-Id: Id7f1cea0758660099d309a6a37e2c2b24ff838cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4473349
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Reviewed-by: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1136077}
    
    Bug: none
    Change-Id: I8a3b5aa80d3c97966bce61577df3e3a305417824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4501202
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139757}

--- gpu/command_buffer/service/shared_image/angle_vulkan_image_backing_factory.h
+++ gpu/command_buffer/service/shared_image/angle_vulkan_image_backing_factory.h
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_ANGLE_VULKAN_IMAGE_BACKING_FACTORY_H_
 
 #include "gpu/command_buffer/service/shared_image/gl_common_image_backing_factory.h"
+#include "gpu/gpu_gles2_export.h"
 
 namespace gpu {
 
 class SharedContextState;
 
-class AngleVulkanImageBackingFactory : public GLCommonImageBackingFactory {
+class GPU_GLES2_EXPORT AngleVulkanImageBackingFactory
+    : public GLCommonImageBackingFactory {
  public:
   AngleVulkanImageBackingFactory(const GpuPreferences& gpu_preferences,
                                  const GpuDriverBugWorkarounds& workarounds,
