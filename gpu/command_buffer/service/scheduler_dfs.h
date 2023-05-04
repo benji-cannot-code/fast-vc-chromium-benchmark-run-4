b92b05d9c52bba4f62e28ee60779dd5cb22bb692
François Doray <fdoray@chromium.org>
2023-05-04 23:46:36 +0000

    Cleanup CPU reduction experiment in gpu::SchedulerDfs.
    Subsampling metrics in this class contributed to reducing Chrome's
    power consumption (go/chrome-all-day-battery-perfys).
    
    This CL replaces the experiment code with a permanent subsampling
    mechanism.
    
    Bug: 1295441
    Change-Id: Ia3e36ba66b0e1df023c7adc59b7c7ab62982bc73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503046
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139840}

--- gpu/command_buffer/service/scheduler_dfs.h
+++ gpu/command_buffer/service/scheduler_dfs.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/rand_util.h"
 #include "base/synchronization/lock.h"
 #include "base/thread_annotations.h"
 #include "base/time/time.h"
@@ -342,6 +343,8 @@ class GPU_EXPORT SchedulerDfs {
 
   base::flat_map<SequenceId, std::unique_ptr<Sequence>> sequence_map_
       GUARDED_BY(lock_);
+
+  base::MetricsSubSampler metrics_subsampler_ GUARDED_BY(lock_);
 
   // Each thread will have its own priority queue to schedule sequences
   // created on that thread.
