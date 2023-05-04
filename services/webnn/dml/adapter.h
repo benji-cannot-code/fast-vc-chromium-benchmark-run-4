0ddd3f3a7eb15e80e6bf1a2b02a8919ecca33965
mingmingtasd <mingming1.xu@intel.com>
2023-05-04 09:47:26 +0000

    WebNN: Define the CommandQueue for DirectML
    The Direct Machine Learning (DirectML) is a low-level API for hardware
    accelerated machine learning on Windows platform.
    
    This CL defines the CommandQueue class to manage a D3D12 command queue
    and provide a waitable fence which is signaled with a monotonically
    increasing value once each execute completes on the GPU.
    
    Bug: 1273291
    Change-Id: I61c7992124adc443143ba414dcae91a6df420e4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363233
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1139414}

--- services/webnn/dml/adapter.h
+++ services/webnn/dml/adapter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wrl.h>
 
 #include "base/memory/ref_counted.h"
+#include "services/webnn/dml/command_queue.h"
 
 namespace webnn::dml {
 
@@ -30,20 +31,20 @@ class Adapter final : public base::RefCounted<Adapter> {
 
   IDMLDevice* dml_device() const { return dml_device_.Get(); }
 
-  ID3D12CommandQueue* command_queue() const { return command_queue_.Get(); }
+  CommandQueue* command_queue() const { return command_queue_.get(); }
 
  private:
   friend class base::RefCounted<Adapter>;
-  explicit Adapter(ComPtr<IDXGIAdapter> dxgi_adapter,
-                   ComPtr<ID3D12Device> d3d12_device,
-                   ComPtr<IDMLDevice> dml_device,
-                   ComPtr<ID3D12CommandQueue> command_queue);
+  Adapter(ComPtr<IDXGIAdapter> dxgi_adapter,
+          ComPtr<ID3D12Device> d3d12_device,
+          ComPtr<IDMLDevice> dml_device,
+          std::unique_ptr<CommandQueue> command_queue);
   ~Adapter();
 
   ComPtr<IDXGIAdapter> dxgi_adapter_;
   ComPtr<ID3D12Device> d3d12_device_;
   ComPtr<IDMLDevice> dml_device_;
-  ComPtr<ID3D12CommandQueue> command_queue_;
+  std::unique_ptr<CommandQueue> command_queue_;
 };
 
 }  // namespace webnn::dml
