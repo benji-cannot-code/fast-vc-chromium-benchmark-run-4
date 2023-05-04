84f9a009aaca521377f2856530256eb26300b27b
Ernesto Izquierdo Clua <eic@google.com>
2023-05-04 16:52:23 +0000

    [ios] Move password_checkup_utils to i/c/b/passwords.
    This is a multipart CL. In this patch password_checkup_utils is moved
    to i/c/b/passwords. In a subsequent CL files in i/c/b/passwords will
    reference password_checkup_utils for metrics recording.
    
    Bug: 1441345
    Change-Id: Id5457f0ac19713bd130e367d3fa78581057fad88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503792
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1139607}

--- ios/chrome/browser/passwords/password_checkup_utils.h
+++ ios/chrome/browser/passwords/password_checkup_utils.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORD_CHECKUP_PASSWORD_CHECKUP_UTILS_H_
-#define IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORD_CHECKUP_PASSWORD_CHECKUP_UTILS_H_
+#ifndef IOS_CHROME_BROWSER_PASSWORDS_PASSWORD_CHECKUP_UTILS_H_
+#define IOS_CHROME_BROWSER_PASSWORDS_PASSWORD_CHECKUP_UTILS_H_
 
 #import <Foundation/Foundation.h>
 
@@ -20,12 +20,16 @@ namespace password_manager {
 struct CredentialUIEntry;
 
 // Enum which represents possible warnings of Password Checkup on UI.
+//
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
 enum class WarningType {
-  kCompromisedPasswordsWarning,
-  kReusedPasswordsWarning,
-  kWeakPasswordsWarning,
-  kDismissedWarningsWarning,
-  kNoInsecurePasswordsWarning,
+  kCompromisedPasswordsWarning = 0,
+  kReusedPasswordsWarning = 1,
+  kWeakPasswordsWarning = 2,
+  kDismissedWarningsWarning = 3,
+  kNoInsecurePasswordsWarning = 4,
+  kMaxValue = kNoInsecurePasswordsWarning,
 };
 
 // Struct used to obtain the password counts associated with the different
@@ -85,4 +89,4 @@ NSString* FormatElapsedTimeSinceLastCheck(
 
 }  // namespace password_manager
 
-#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORD_CHECKUP_PASSWORD_CHECKUP_UTILS_H_
+#endif  // IOS_CHROME_BROWSER_PASSWORDS_PASSWORD_CHECKUP_UTILS_H_
--- ios/chrome/browser/ui/settings/password/password_checkup/password_checkup_mediator.mm
+++ ios/chrome/browser/ui/settings/password/password_checkup/password_checkup_mediator.mm
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #import "ios/chrome/browser/passwords/ios_chrome_password_check_manager.h"
 #import "ios/chrome/browser/passwords/password_check_observer_bridge.h"
+#import "ios/chrome/browser/passwords/password_checkup_utils.h"
 #import "ios/chrome/browser/ui/settings/password/password_checkup/password_checkup_consumer.h"
-#import "ios/chrome/browser/ui/settings/password/password_checkup/password_checkup_utils.h"
 #import "ios/chrome/grit/ios_chromium_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 
