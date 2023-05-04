caaf69054d6e31ad3ec08e43a36f4a5d9acedd77
Claudio DeSouza <cdesouza@igalia.com>
2023-05-04 19:40:08 +0000

    Replace base::GUID uses with base::Uuid (224/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /device/bluetooth/floss
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: I5ea6634d4e83e7a6f005e0ee4e8c3148ae21f6f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481996
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139718}

--- device/bluetooth/floss/bluetooth_advertisement_floss.cc
+++ device/bluetooth/floss/bluetooth_advertisement_floss.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/logging.h"
 #include "base/observer_list.h"
 #include "base/strings/string_number_conversions.h"
--- device/bluetooth/floss/bluetooth_local_gatt_service_floss.cc
+++ device/bluetooth/floss/bluetooth_local_gatt_service_floss.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/floss/bluetooth_local_gatt_service_floss.h"
 
-#include "base/guid.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/rand_util.h"
