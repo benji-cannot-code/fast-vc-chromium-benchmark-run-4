8c954c521ea8bca69964bf915b8e95cd42c0c761
Maggie Chen <magchen@chromium.org>
2023-05-04 20:39:40 +0000

    Revert "Add Mac GpuVSyncThread and use CVDisplayLink as the source of begin frame."
    This reverts commit 881d7cfdecaf624068b4fcea861880741f54dbee.
    
    Reason for revert: <Break the iOS build>
    
    Original change's description:
    > Add Mac GpuVSyncThread and use CVDisplayLink as the source of begin frame.
    >
    > The feature flag "GpuVsync" is disabled by default for Mac.
    >
    > Bug: 1404797
    >
    > Change-Id: I4a752da421f2785d7ebe7a705fd0bb1707cee34f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4247199
    > Reviewed-by: vikas soni <vikassoni@chromium.org>
    > Commit-Queue: Maggie Chen <magchen@chromium.org>
    > Reviewed-by: ccameron chromium <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1139664}
    
    Bug: 1404797
    Change-Id: I6073effc973e427ba8c46c45201e0cdc55ac4c0f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508054
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Owners-Override: Dave Tapuska <dtapuska@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139750}

--- gpu/ipc/service/image_transport_surface_overlay_mac.h
+++ gpu/ipc/service/image_transport_surface_overlay_mac.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/scoped_nsobject.h"
 #include "base/memory/weak_ptr.h"
-#include "components/viz/common/gpu/gpu_vsync_callback.h"
 #include "gpu/ipc/service/command_buffer_stub.h"
 #include "gpu/ipc/service/image_transport_surface.h"
 #include "ui/gfx/ca_layer_result.h"
@@ -18,11 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_surface.h"
 #include "ui/gl/presenter.h"
-
-// Put gpu_vsync_mac.h (which includes ui/display/mac/display_link_mac.h)
-// after ui/gl/gl_xxx.h. There is a conflict between MacOSX sdk gltypes.h and
-// third_party/mesa_headers/GL/glext.h
-#include "gpu/ipc/service/gpu_vsync_mac.h"
 
 @class CAContext;
 @class CALayer;
@@ -40,6 +34,9 @@ namespace gpu {
 
 class ImageTransportSurfaceOverlayMacEGL : public gl::Presenter {
  public:
+  using VSyncCallback =
+      base::RepeatingCallback<void(base::TimeTicks, base::TimeDelta)>;
+
   ImageTransportSurfaceOverlayMacEGL(
       base::WeakPtr<ImageTransportSurfaceDelegate> delegate);
 
@@ -62,8 +59,8 @@ class ImageTransportSurfaceOverlayMacEGL : public gl::Presenter {
 
   // GLSurface override
   bool SupportsGpuVSync() const override;
-  void SetVSyncDisplayID(int64_t display_id) override;
   void SetGpuVSyncEnabled(bool enabled) override;
+  void SetVSyncDisplayID(int64_t display_id) override;
 
  private:
   ~ImageTransportSurfaceOverlayMacEGL() override;
@@ -89,7 +86,8 @@ class ImageTransportSurfaceOverlayMacEGL : public gl::Presenter {
   // backpressure.
   uint64_t previous_frame_fence_ = 0;
 
-  std::unique_ptr<GpuVSyncMac> gpu_vsync_mac_;
+  const VSyncCallback vsync_callback_;
+  bool gpu_vsync_enabled_ = false;
 
   base::WeakPtrFactory<ImageTransportSurfaceOverlayMacEGL> weak_ptr_factory_;
 };
