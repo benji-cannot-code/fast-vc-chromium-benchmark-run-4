f3d0498bbafaf07a10477870d612deb41f3370c8
Sammie Quon <sammiequon@chromium.org>
2023-05-04 21:49:22 +0000

    splitview: Make *StackingOrderSplitViewWindow* test more readable
    It follows a user CUJ more closely now. Also while fixing this test, it
    was noticed that if the previous active overview window is not the
    default snapped window, it would not regain activation on exiting
    overview. Fixed that bit as well to make the test pass.
    
    Test: ash_unittests *TabletModeOverviewSessionTest.StackingOrderSplitViewWindow
    Change-Id: Id6a7f96a64fca997fec2f6dbfdd6ab2705d9ff6e
    Fixed: b/278952025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4490803
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139790}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -2019,6 +2019,8 @@ void SplitViewController::OnOverviewModeEnding(
     if (window != GetDefaultSnappedWindow()) {
       absl::optional<float> snap_ratio = ComputeSnapRatio(window);
       if (snap_ratio) {
+        const bool was_active =
+            overview_session->IsWindowActiveWindowBeforeOverview(window);
         // Remove the overview item before snapping because the overview session
         // is unavailable to retrieve outside this function after
         // OnOverviewEnding is notified.
@@ -2031,6 +2033,10 @@ void SplitViewController::OnOverviewModeEnding(
                        : SnapPosition::kPrimary,
                    WindowSnapActionSource::kAutoSnapInSplitView,
                    /*activate_window=*/false, *snap_ratio);
+        if (was_active) {
+          wm::ActivateWindow(window);
+        }
+
         // If ending overview causes a window to snap, also do not do exiting
         // overview animation.
         overview_session->SetWindowListNotAnimatedWhenExiting(root_window_);
