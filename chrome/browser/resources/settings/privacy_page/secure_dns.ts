d3b874e954e86adb6ca69e2906e4ca9cca1b7de9
dpapad <dpapad@chromium.org>
2023-05-04 01:19:22 +0000

    Settings: Make settings-secure-dns not depend on settings-shared style.
     - Change 'secondary' CSS class to 'cr-secondary-text'
     - Remove 'list-item' and 'list-frame' CSS classes, and replace with
       targeted styles for the elements that used them.
    
    This also fixes a bug where the settings-secure-dns element would
    invisibly extend outside the boundaries of the enclosing card,
    noticeable only when hovering the cursor over that area.
    
    This is in preparation of moving it to the "limited sharing" mechanism
    so that it can be properly shared between OS and Browser Settings.
    
    Bug: 1431477
    Change-Id: I5cc9dde16dba82d35296657f9552e23ced8394ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504415
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139295}

--- chrome/browser/resources/settings/privacy_page/secure_dns.ts
+++ chrome/browser/resources/settings/privacy_page/secure_dns.ts
@@ -17,10 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
 import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/md_select.css.js';
 import 'chrome://resources/cr_components/settings_prefs/prefs.js';
 import '/shared/settings/controls/settings_toggle_button.js';
-import '../settings_shared.css.js';
 import '../strings.m.js';
 import './secure_dns_input.js';
 
