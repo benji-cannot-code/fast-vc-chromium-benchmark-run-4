ddaa196611743b4d1c0726f0c54654b2ff054ed9
David Padlipsky <dpad@google.com>
2023-05-04 23:55:25 +0000

    Settings Split: Add is_haptic field to touchpads mojom struct
    BUG: b/241965700
    Change-Id: Ic926e67cd35fbbe08faaa4dc7514681bd495bc42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492515
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1139842}

--- ash/system/input_device_settings/input_device_settings_controller_impl.cc
+++ ash/system/input_device_settings/input_device_settings_controller_impl.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ash/keyboard_capability.h"
 #include "ui/events/devices/input_device.h"
 #include "ui/events/devices/keyboard_device.h"
+#include "ui/events/devices/touchpad_device.h"
 
 namespace ash {
 
@@ -90,7 +91,7 @@ mojom::MousePtr BuildMojomMouse(const ui::InputDevice& mouse) {
   return mojom_mouse;
 }
 
-mojom::TouchpadPtr BuildMojomTouchpad(const ui::InputDevice& touchpad) {
+mojom::TouchpadPtr BuildMojomTouchpad(const ui::TouchpadDevice& touchpad) {
   mojom::TouchpadPtr mojom_touchpad = mojom::Touchpad::New();
   mojom_touchpad->id = touchpad.id;
   mojom_touchpad->name = touchpad.name;
@@ -99,6 +100,7 @@ mojom::TouchpadPtr BuildMojomTouchpad(const ui::InputDevice& touchpad) {
           touchpad);
   mojom_touchpad->is_external =
       touchpad.type != ui::InputDeviceType::INPUT_DEVICE_INTERNAL;
+  mojom_touchpad->is_haptic = touchpad.is_haptic;
   return mojom_touchpad;
 }
 
