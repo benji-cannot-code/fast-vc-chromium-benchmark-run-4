917ebb40764fa6d4ba383b6ec39ab209285ce63f
Ed Chin <edchin@google.com>
2023-05-04 21:11:24 +0000

    [ios][feed] Enable image caching by default
    Change-Id: I1804c3ba183025d0c4dcd54340e169450279ecc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507754
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: edchin <edchin@google.com>
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139774}

--- ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
@@ -58,7 +58,7 @@ BASE_FEATURE(kOverrideFeedSettings,
 
 BASE_FEATURE(kEnableFeedImageCaching,
              "EnableFeedImageCaching",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableFeedSyntheticCapabilities,
              "EnableFeedSyntheticCapabilities",
