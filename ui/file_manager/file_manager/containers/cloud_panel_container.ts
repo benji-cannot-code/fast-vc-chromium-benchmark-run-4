4ffba2755f5a044d295261708e59ab66eb6b2ec0
Ben Reich <benreich@chromium.org>
2023-05-04 03:48:05 +0000

    [FilesBulkPinning] Wire up not enough space state
    NOT_ENOUGH_SPACE is returned when the local disk space is insufficient
    to store all the users My drive files. When this happens the cloud panel
    enters into a not enough space state indicating as such to the user.
    
    Fixed: b:275635747
    Bug: b:262647382
    Test: browser_test --gtest_filter=FileManagerJsTest.CloudPanelContainer
    Validate-Test-Flakiness: skip
    Change-Id: I4f9dca81c6c3a44a88cb79014edf82d6e9fabe2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4495750
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139338}

--- ui/file_manager/file_manager/containers/cloud_panel_container.ts
+++ ui/file_manager/file_manager/containers/cloud_panel_container.ts
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {util} from '../common/js/util.js';
 import {State} from '../externs/ts/state.js';
 import {getStore, Store} from '../state/store.js';
-import {CloudPanelSettingsClickEvent, XfCloudPanel} from '../widgets/xf_cloud_panel.js';
+import {CloudPanelSettingsClickEvent, CloudPanelType, XfCloudPanel} from '../widgets/xf_cloud_panel.js';
 
 export type BulkPinProgress = chrome.fileManagerPrivate.BulkPinProgress;
 export const BulkPinStage = chrome.fileManagerPrivate.BulkPinStage;
@@ -101,8 +101,14 @@ export class CloudPanelContainer {
     // offline. This could be from either the network not being connected or
     // cellular being disabled for syncing.
     if (bulkPinProgress.stage === BulkPinStage.PAUSED) {
-      this.panel_.setAttribute('type', 'offline');
-      this.increaseUpdates_();
+      this.updatePanelType_(CloudPanelType.OFFLINE);
+      return;
+    }
+
+    // Not enough space indicates the available local storage is insufficient to
+    // store all the files required by the users My drive.
+    if (bulkPinProgress.stage === BulkPinStage.NOT_ENOUGH_SPACE) {
+      this.updatePanelType_(CloudPanelType.NOT_ENOUGH_SPACE);
       return;
     }
     this.panel_.removeAttribute('type');
@@ -120,6 +126,17 @@ export class CloudPanelContainer {
         (bulkPinProgress.pinnedBytes / bulkPinProgress.bytesToPin * 100)
             .toFixed(0);
     this.panel_.setAttribute('percentage', String(percentage));
+    this.increaseUpdates_();
+  }
+
+  /**
+   * Updates the underlying panel to the `type` and removes the in progress
+   * attributes.
+   */
+  private updatePanelType_(type: CloudPanelType) {
+    this.panel_.setAttribute('type', type);
+    this.panel_.removeAttribute('items');
+    this.panel_.removeAttribute('percentage');
     this.increaseUpdates_();
   }
 
