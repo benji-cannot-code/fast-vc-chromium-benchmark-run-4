a507808e9ecda1bb50565cb205bf237c446c6d8b
Erica Lee <ericamlee@google.com>
2023-05-05 19:53:04 +0000

    personalization: removed fake theme provider
    Removed the fake personalization app theme provider so that our browser
    tests are more robust.
    
    BUG=b:277811185
    TEST=browser_tests PersonalizationApp*
    
    Change-Id: I017f4889def014c692cb6c9171e5ed16ee555f09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4499394
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140295}

--- chrome/browser/ash/web_applications/personalization_app/personalization_app_browsertest_fixture.cc
+++ chrome/browser/ash/web_applications/personalization_app/personalization_app_browsertest_fixture.cc
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/personalization_app/personalization_app_url_constants.h"
 #include "ash/webui/personalization_app/test/fake_personalization_app_ambient_provider.h"
 #include "ash/webui/personalization_app/test/fake_personalization_app_keyboard_backlight_provider.h"
-#include "ash/webui/personalization_app/test/fake_personalization_app_theme_provider.h"
 #include "ash/webui/personalization_app/test/fake_personalization_app_user_provider.h"
 #include "chrome/browser/ash/wallpaper_handlers/test_wallpaper_fetcher_delegate.h"
+#include "chrome/browser/ash/web_applications/personalization_app/personalization_app_theme_provider_impl.h"
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_utils.h"
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_wallpaper_provider_impl.h"
 #include "chrome/browser/profiles/profile.h"
@@ -37,7 +37,7 @@ TestPersonalizationAppWebUIProvider::NewWebUI(content::WebUI* web_ui,
   auto keyboard_backlight_provider =
       std::make_unique<FakePersonalizationAppKeyboardBacklightProvider>(web_ui);
   auto theme_provider =
-      std::make_unique<FakePersonalizationAppThemeProvider>(web_ui);
+      std::make_unique<PersonalizationAppThemeProviderImpl>(web_ui);
   auto wallpaper_provider =
       std::make_unique<PersonalizationAppWallpaperProviderImpl>(
           web_ui,
