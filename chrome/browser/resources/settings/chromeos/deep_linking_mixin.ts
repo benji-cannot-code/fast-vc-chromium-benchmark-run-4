7d98764d76de18e3d1d19574d5d989c65d0d3a65
Wes Okuhara <wesokuhara@google.com>
2023-05-05 17:28:06 +0000

    CrOS Settings: Move setting_id_param_util to common folder
    In effort to better organize the root dir of CrOS Settings SWA.
    
    Bug: None
    Change-Id: I7bda1f83ce2357cf19a6d058ed08bc0c7302deb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505601
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Auto-Submit: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140185}

--- chrome/browser/resources/settings/chromeos/deep_linking_mixin.ts
+++ chrome/browser/resources/settings/chromeos/deep_linking_mixin.ts
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {afterNextRender, dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {getSettingIdParameter} from './common/setting_id_param_util.js';
 import {Constructor} from './common/types.js';
 import {Setting} from './mojom-webui/setting.mojom-webui.js';
-import {getSettingIdParameter} from './setting_id_param_util.js';
 
 const DEEP_LINK_FOCUS_ID = 'deep-link-focus-id';
 
