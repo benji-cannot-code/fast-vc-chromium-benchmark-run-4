216e0703055d488430b11870b605da198fd07be8
Adithya Srinivasan <adithyas@chromium.org>
2023-05-05 14:14:48 +0000

    PSCS: Change when WebContentsDestroyed is called
    In a scenario where a SiteDataObserver is also a WebContentsUserData
    [1], we could run into problems while cleaning up on WebContents
    destruction. In this situation, ~SiteDataObserver is called during
    WebContentsUserData destruction before ~PSCS::WebContentsHandler. We
    try looking up PSCS from the WebContents in ~SiteDataObserver, but at
    this point, the WCUD map is empty [2], and the lookup will fail, even
    though the PSCS::WebContentsHandler object itself is still alive. This
    could mean that the observer can't deregister itself from
    PSCS::WebContentsHolder and we have a dangling pointer.
    
    This CL moves some of this clean up to
    WebContentsObserver::WebContentsDestroyed, which is called before
    WebContentsUserData is cleared and SiteDataObserver can now lookup
    PSCS::WebContentsHandler.
    
    [1] https://crrev.com/c/4489564/9 attempted to make a WCUD a SiteDataObserver
    
    [2] https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:base/supports_user_data.cc;l=123;drc=e0e0d24aaa54727dc0a8bc4b159ccdf80d3f5d8d
    
    Change-Id: I8b332ee7a3f1ae9a1635bfe3ca96e3c88b787d61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4501346
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140102}

--- components/content_settings/browser/page_specific_content_settings.cc
+++ components/content_settings/browser/page_specific_content_settings.cc
@@ -170,6 +170,7 @@ class WebContentsHandler
       content::RenderFrameHost* frame,
       const GURL& scope,
       content::AllowServiceWorkerResult allowed) override;
+  void WebContentsDestroyed() override;
 
   std::unique_ptr<Delegate> delegate_;
 
@@ -230,10 +231,7 @@ WebContentsHandler::WebContentsHandler(content::WebContents* web_contents,
       web_contents->GetPrimaryPage(), delegate_.get());
 }
 
-WebContentsHandler::~WebContentsHandler() {
-  for (SiteDataObserver& observer : observer_list_)
-    observer.WebContentsDestroyed();
-}
+WebContentsHandler::~WebContentsHandler() = default;
 
 void WebContentsHandler::TransferNavigationContentSettingsToCommittedDocument(
     const InflightNavigationContentSettings& navigation_settings,
@@ -423,6 +421,12 @@ void WebContentsHandler::AddPendingCommitUpdate(
   pending_commit_updates_[id].push_back(std::move(update));
 }
 
+void WebContentsHandler::WebContentsDestroyed() {
+  for (SiteDataObserver& observer : observer_list_) {
+    observer.WebContentsDestroyed();
+  }
+}
+
 WEB_CONTENTS_USER_DATA_KEY_IMPL(WebContentsHandler);
 
 AccessDetails::AccessDetails() = default;
@@ -458,6 +462,10 @@ PageSpecificContentSettings::SiteDataObserver::~SiteDataObserver() {
 }
 
 void PageSpecificContentSettings::SiteDataObserver::WebContentsDestroyed() {
+  auto* handler = WebContentsHandler::FromWebContents(web_contents_);
+  if (handler) {
+    handler->RemoveSiteDataObserver(this);
+  }
   web_contents_ = nullptr;
 }
 
