57503203a8664998cff702da214f153de342e658
Liza Bipin <mlbipin@google.com>
2023-05-05 18:15:07 +0000

    Set pref to show banner for supervised users on Desktop
    * Adds integer pref that for banner to be shown to supervised users that are already been signed in to desktop when parental controls are applied
    
    * Introduces a new enum type to track the state of banner.
    
    Bug: b/277042699
    Change-Id: I3c68459785e9be227041aad2b1543d103b522be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481448
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140209}

--- chrome/browser/supervised_user/supervised_user_service.cc
+++ chrome/browser/supervised_user/supervised_user_service.cc
@@ -66,6 +66,10 @@ void SupervisedUserService::RegisterProfilePrefs(
   for (const char* pref : supervised_user::kCustodianInfoPrefs) {
     registry->RegisterStringPref(pref, std::string());
   }
+  registry->RegisterIntegerPref(
+      prefs::kFirstTimeInterstitialBannerState,
+      static_cast<int>(
+          supervised_user::FirstTimeInterstitialBannerState::kUnknown));
 }
 
 void SupervisedUserService::Init() {
@@ -78,7 +82,26 @@ void SupervisedUserService::Init() {
       prefs::kSupervisedUserId,
       base::BindRepeating(&SupervisedUserService::OnSupervisedUserIdChanged,
                           base::Unretained(this)));
+  supervised_user::FirstTimeInterstitialBannerState banner_state =
+      static_cast<supervised_user::FirstTimeInterstitialBannerState>(
+          user_prefs_->GetInteger(prefs::kFirstTimeInterstitialBannerState));
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+  if (banner_state ==
+          supervised_user::FirstTimeInterstitialBannerState::kUnknown &&
+      !profile_->IsNewProfile()) {
+    banner_state =
+        supervised_user::FirstTimeInterstitialBannerState::kNeedToShow;
+  } else {
+    banner_state =
+        supervised_user::FirstTimeInterstitialBannerState::kSetupComplete;
+  }
+#else
+  banner_state =
+      supervised_user::FirstTimeInterstitialBannerState::kSetupComplete;
+#endif
 
+  user_prefs_->SetInteger(prefs::kFirstTimeInterstitialBannerState,
+                          static_cast<int>(banner_state));
   SetActive(IsSubjectToParentalControls());
 }
 
@@ -404,4 +427,18 @@ void SupervisedUserService::Shutdown() {
 void SupervisedUserService::OnSiteListUpdated() {
   for (SupervisedUserServiceObserver& observer : observer_list_)
     observer.OnURLFilterChanged();
+}
+
+void SupervisedUserService::MarkFirstTimeInterstitialBannerShown() const {
+  supervised_user::FirstTimeInterstitialBannerState banner_state =
+      static_cast<supervised_user::FirstTimeInterstitialBannerState>(
+          user_prefs_->GetInteger(prefs::kFirstTimeInterstitialBannerState));
+
+  if (banner_state ==
+      supervised_user::FirstTimeInterstitialBannerState::kNeedToShow) {
+    user_prefs_->SetInteger(
+        prefs::kFirstTimeInterstitialBannerState,
+        static_cast<int>(
+            supervised_user::FirstTimeInterstitialBannerState::kSetupComplete));
+  }
 }
