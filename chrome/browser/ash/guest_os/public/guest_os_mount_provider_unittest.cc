ead6c6d0e6fb9ccb201d41e52731788e7f18ab08
David Munro <davidmunro@google.com>
2023-05-05 01:39:03 +0000

    guest_os: Avoid double-volume creation
    There can be multiple active profiles (e.g. main profile and an
    incognito profile in Chrome when the user is saving a file to disk),
    each of which has its own VolumeManager, so we need:
    1. Each profile has its own volume
    2. When unmount is called all volumes get removed
    3. Deal with profiles having different lifetimes
    
    Bug: b:277677336
    Test: Unit, CQ, deploy and manually mess around with incognito
    
    Change-Id: I9173fc1433be702725b92967fcb39f8d74948d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506017
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1139892}

--- chrome/browser/ash/guest_os/public/guest_os_mount_provider_unittest.cc
+++ chrome/browser/ash/guest_os/public/guest_os_mount_provider_unittest.cc
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
-#include "base/test/task_environment.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "chrome/browser/ash/file_manager/volume_manager_factory.h"
+#include "chrome/browser/ash/file_manager/volume_manager_observer.h"
 #include "chrome/browser/ash/guest_os/guest_id.h"
 #include "chrome/browser/ash/guest_os/guest_os_test_helpers.h"
 #include "chrome/test/base/testing_profile.h"
@@ -45,6 +45,18 @@ std::unique_ptr<KeyedService> BuildVolumeManager(
       nullptr /* file_system_provider_service */,
       file_manager::VolumeManager::GetMtpStorageInfoCallback());
 }
+
+class MockVolumeManagerObserver : public file_manager::VolumeManagerObserver {
+ public:
+  MockVolumeManagerObserver() = default;
+  ~MockVolumeManagerObserver() override = default;
+
+  MOCK_METHOD(void,
+              OnVolumeMounted,
+              (ash::MountError error_code, const file_manager::Volume& volume),
+              (override));
+};
+
 }  // namespace
 
 namespace guest_os {
@@ -62,6 +74,8 @@ class GuestOsMountProviderTest : public testing::Test {
 
     DiskMountManager::InitializeForTesting(disk_manager_);
     volume_manager_ = file_manager::VolumeManagerFactory::Get(profile_.get());
+    volume_manager_observer_ = std::make_unique<MockVolumeManagerObserver>();
+    volume_manager_->AddObserver(volume_manager_observer_.get());
   }
 
   GuestOsMountProviderTest(const GuestOsMountProviderTest&) = delete;
@@ -106,6 +120,8 @@ class GuestOsMountProviderTest : public testing::Test {
         .Times(n)
         .WillRepeatedly(
             Invoke(this, &GuestOsMountProviderTest::NotifyMountEvent));
+
+    EXPECT_CALL(*volume_manager_observer_, OnVolumeMounted).Times(n);
   }
 
   // guestos_${UserHash}_${encode(kGuestId.ToString())}. Note that UserHash
@@ -118,6 +134,7 @@ class GuestOsMountProviderTest : public testing::Test {
   raw_ptr<ash::disks::MockDiskMountManager, ExperimentalAsh> disk_manager_;
   std::unique_ptr<TestingProfile> profile_;
   raw_ptr<file_manager::VolumeManager, ExperimentalAsh> volume_manager_;
+  std::unique_ptr<MockVolumeManagerObserver> volume_manager_observer_;
   std::unique_ptr<MockMountProvider> provider_;
   int cid_ = 41;     // Default set in MockMountProvider
   int port_ = 1234;  // Default set in MockMountProvider
@@ -222,8 +239,11 @@ TEST_F(GuestOsMountProviderTest, VolumesMountedOnChildProfiles) {
       otr_profile, base::BindRepeating(&BuildVolumeManager));
   auto* otr_volume_manager =
       file_manager::VolumeManagerFactory::Get(otr_profile);
+  MockVolumeManagerObserver otr_volume_manager_observer;
+  otr_volume_manager->AddObserver(&otr_volume_manager_observer);
 
   ExpectMountCalls(1);
+  EXPECT_CALL(otr_volume_manager_observer, OnVolumeMounted).Times(1);
   bool result = false;
 
   provider_->Mount(otr_profile, base::BindLambdaForTesting(
@@ -246,4 +266,5 @@ TEST_F(GuestOsMountProviderTest, VolumesMountedOnChildProfiles) {
   // wrong order.
   profile_->DestroyOffTheRecordProfile(otr_profile);
 }
+
 }  // namespace guest_os
