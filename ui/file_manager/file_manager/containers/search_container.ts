f6ca7eec186bbd489cef4d3eba201606bf3289b9
Bo Majewski <majewski@chromium.org>
2023-05-05 04:05:39 +0000

    [Files]: Further fixes to search clear button.
    Updates clear button state on close and open. Otherwise, it was possible
    to create the search widget, close it and re-open it and the clear
    button was still visible. Also adds clearing of the input value on
    close, so that re-opening always starts in a clean slate.
    
    Bug: b:280546205
    Change-Id: I73dd94432237a47bc2fe47421de1df021013e1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506039
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139929}

--- ui/file_manager/file_manager/containers/search_container.ts
+++ ui/file_manager/file_manager/containers/search_container.ts
@@ -228,6 +228,7 @@ export class SearchContainer extends EventTarget {
 
     // The button that allows the user to clear the query.
     this.clearButton_ = this.searchBox_.querySelector('.clear') as HTMLElement;
+    // Hide clear button when created.
     this.updateClearButton_('');
 
     // The list showing possible matches to the current query.
@@ -639,6 +640,7 @@ export class SearchContainer extends EventTarget {
       this.searchWrapper_.classList.add('has-cursor', 'has-text');
       this.searchBox_.classList.add('has-cursor', 'has-text');
       this.searchButton_.tabIndex = -1;
+      this.updateClearButton_(this.getQuery());
     }
   }
 
@@ -662,6 +664,7 @@ export class SearchContainer extends EventTarget {
         this.inputState_ = SearchInputState.CLOSED;
         this.searchWrapper_.setAttribute('collapsed', '');
       }, {once: true, passive: true, capture: true});
+      this.inputElement_.value = '';
       this.searchWrapper_.classList.remove('has-cursor', 'has-text');
       this.searchBox_.classList.remove('has-cursor', 'has-text');
       this.searchButton_.tabIndex = 0;
