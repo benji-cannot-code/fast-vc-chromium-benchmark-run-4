1cb983f41606a554f1354856a725c5bbe8f9fe53
Xiaohui Chen <xiaohuic@google.com>
2023-05-05 17:23:42 +0000

    os settings: remove "v3" from file names
    Bug: b/280685158
    Change-Id: Ibb70ac232855b78688da4ba19071a04da2bc8547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505431
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140184}

--- chrome/test/data/webui/settings/chromeos/a11y/os_a11y_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/a11y/os_a11y_browsertest.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // OSSettingsAccessibilityV3Test fixture.
 GEN_INCLUDE([
-  'os_settings_accessibility_v3_test.js',
+  'os_settings_accessibility_test.js',
 ]);
 
 GEN('#include "build/branding_buildflags.h"');
@@ -81,52 +81,52 @@ const crostiniSubpageConfig = {
 
 [[
   'Basic',
-  'basic_a11y_v3_test.js',
+  'basic_a11y_test.js',
   {options: axeOptionsExcludeLinkInTextBlock},
 ],
  [
    'GoogleAssistant',
-   'google_assistant_a11y_v3_test.js',
+   'google_assistant_a11y_test.js',
    {options: axeOptionsDocumentTitle},
  ],
  [
    'ManageAccessibility',
-   'manage_accessibility_a11y_v3_test.js',
+   'manage_accessibility_a11y_test.js',
    {options: axeOptionsDocumentTitle},
  ],
  [
    'Multidevice',
-   'multidevice_a11y_v3_test.js',
+   'multidevice_a11y_test.js',
    {options: axeOptionsDocumentTitle},
  ],
  [
    'CrostiniDetails',
-   'crostini_settings_details_a11y_v3_test.js',
+   'crostini_settings_details_a11y_test.js',
    crostiniConfig,
  ],
  [
    'CrostiniExportImport',
-   'crostini_settings_export_import_a11y_v3_test.js',
+   'crostini_settings_export_import_a11y_test.js',
    crostiniConfig,
  ],
  [
    'CrostiniSharedPaths',
-   'crostini_settings_shared_paths_a11y_v3_test.js',
+   'crostini_settings_shared_paths_a11y_test.js',
    crostiniConfig,
  ],
  [
    'CrostiniSharedUsbDevices',
-   'crostini_settings_shared_usb_devices_a11y_v3_test.js',
+   'crostini_settings_shared_usb_devices_a11y_test.js',
    crostiniConfig,
  ],
  [
    'CrostiniSubpage',
-   'crostini_settings_subpage_a11y_v3_test.js',
+   'crostini_settings_subpage_a11y_test.js',
    crostiniSubpageConfig,
  ],
  [
    'MultideviceFeatures',
-   'multidevice_features_a11y_v3_test.js',
+   'multidevice_features_a11y_test.js',
    {
      filter: multideviceFeatureViolationFilter,
      options: axeOptionsDocumentTitle,
@@ -134,7 +134,7 @@ const crostiniSubpageConfig = {
  ],
  [
    'Tts',
-   'tts_subpage_a11y_v3_test.js',
+   'tts_subpage_a11y_test.js',
    {
      switches: ['enable-experimental-a11y-features'],
      options: axeOptionsDocumentTitle,
