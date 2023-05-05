4b96d78b113517ea803d97d8d57a4a8112adbed4
Justin Lulejian <jlulejian@chromium.org>
2023-05-05 21:31:25 +0000

    [Extensions] Prevent active tab from persisting unexpectedly.
    When granting user site permissions we also grant active tab. Before
    this change we never revoked active tab when we revoked user site
    permissions. This allowed an extension to retain access in the tab where
    the user site permissions were allowed.
    
    After this change the active tab is removed when we revoke user site
    permissions.
    
    For testing I've written test cases for two scenarios.
    revoke_execute_on_click is for an extension that uses
    chrome.scripting.executeScript() to inject which closely mimics the
    extension that exposed the bug. content_script_at_end is for a content
    script injection. I additionally added a test case for
    revoke_execute_on_click where grant active tab is set independently of
    user site permissions so that we confirm it is removed then too.
    
    
    Pair: emiliapaz@chromium.org, rdevlin.cronin@chromium.org
    Fixed: 1433399
    Change-Id: I609979819029664c3f19966c33355913b9b41ce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492270
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140347}

--- chrome/browser/extensions/site_permissions_helper.cc
+++ chrome/browser/extensions/site_permissions_helper.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_action_runner.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/scripting_permissions_modifier.h"
+#include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/web_contents.h"
@@ -153,6 +154,14 @@ void SitePermissionsHelper::UpdateSiteAccess(
     runner->ShowReloadPageBubbleWithReloadPageCallback(extension.id());
   } else if (blocked_actions != BLOCKED_ACTION_NONE) {
     runner->RunBlockedActions(&extension);
+  }
+
+  // Clear extension's active tab permission since it is set when granting user
+  // site permissions.
+  if (revoking_current_site_permissions) {
+    TabHelper::FromWebContents(web_contents)
+        ->active_tab_permission_granter()
+        ->ClearActiveExtensionAndNotify(extension.id());
   }
 }
 
---
+++ chrome/test/data/extensions/blocked_actions/revoke_execute_script_on_click/background.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function changeDocumentTitle() {
+  document.title = 'success';
+  chrome.test.sendMessage("injection succeeded");
+}
+
+chrome.webNavigation.onCommitted.addListener(async function(activeInfo) {
+  let tab = await getCurrentTab();
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: changeDocumentTitle
+  });
+});
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true};
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
