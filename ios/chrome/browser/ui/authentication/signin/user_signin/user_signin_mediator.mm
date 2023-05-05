a9058983a69f237cbdfbb982b0a8d865fb876c04
Jood Hajeer <jood@google.com>
2023-05-05 12:29:56 +0000

    [iOS/sync] Fix passphrase is reset when dismissing sync opt-in for a signed-in user.
    StopAndClear() is called to clear the encryption passphrase if a
    signed-in user entered it before canceling the sync opt-in flow. Which
    is not the expected behavior to have no impact on the passphrase state.
    
    Fixed: 1434691
    Change-Id: Ifc96ed403c1ef66f42bf05c17873f2966b830a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502645
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140058}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
@@ -153,9 +153,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if ([self.authenticationService->GetPrimaryIdentity(
               signin::ConsentLevel::kSignin)
               isEqual:self.delegate.signinIdentityOnStart]) {
-        // Call StopAndClear() to clear the encryption passphrase, in case the
-        // user entered it before canceling the sync opt-in flow.
-        _syncService->StopAndClear();
         if (completion)
           completion();
       } else {
