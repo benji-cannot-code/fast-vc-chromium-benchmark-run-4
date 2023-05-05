8160cea19cadbb68bdb70bd639092ebc14a6d7a5
Nasser Al-shawwa <alshawwa@chromium.org>
2023-05-05 18:00:50 +0000

    Add CrowdstrikeSignalReporting Finch flag
    Bug: b/280364768
    Change-Id: If92039541f0e47b5132f4951f1ac016c48930838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503804
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140202}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -11,6 +11,10 @@ namespace policy {
 
 namespace features {
 
+BASE_FEATURE(kCrowdstrikeSignalReporting,
+             "CrowdstrikeSignalReporting",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kEnableUserCloudSigninRestrictionPolicyFetcher,
              "UserCloudSigninRestrictionPolicyFetcher",
              base::FEATURE_ENABLED_BY_DEFAULT);
