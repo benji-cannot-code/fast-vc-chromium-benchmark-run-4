f4c373ad2d9925d25ffa0204fe0d6994c736e22f
Ivana Žužić <izuzic@google.com>
2023-05-05 19:06:05 +0000

    Reland "[UPMLocalPwd] Trigger the sheet from save password message"
    This is a reland of commit d56d9550092ff736991a3bf30bcf5b4cd57af31a
    
    The problem with the original CL was that the path to the newly
    introduced files was too long and it exceeded the 260 characters limit
    for Windows. The check did not run during the dry run so it was only
    discovered on the failing CI bot.
    
    In the reland CL, a new build file is created for the new files to it
    shortens the paths.
    
    Original change's description:
    > [UPMLocalPwd] Trigger the sheet from save password message
    >
    > The PasswordMigrationWarningBridge triggers the password migration
    > warning from the cpp side.
    > Currently it only creates the coordinator and calls its showWarning
    > method which will contain the logic of displaying the sheet in an
    > upcoming CL.
    >
    > Bug: 1439761
    > Change-Id: I5b0f1fe32704aa679e9bc666ac41e0cffd0599ce
    > Low-Coverage-Reason: The logic is split between Java and C++.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4474554
    > Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    > Reviewed-by: Peter Wen <wnwen@chromium.org>
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1138445}
    
    Bug: 1439761
    Change-Id: I3c9e09da427fd3cac5a653917c04125d2aca27d7
    Low-Coverage-Reason: The logic is split between Java and C++.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506778
    Auto-Submit: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140248}

---
+++ chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PasswordMigrationWarningCoordinator.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.pwd_migration;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+
+/** The coordinator of the password migration warning. */
+class PasswordMigrationWarningCoordinator {
+    public PasswordMigrationWarningCoordinator(
+            @Nullable Context context, BottomSheetController sheetController) {}
+
+    public void showWarning() {
+        // TODO(crbug.com/1439761): Implement.
+    }
+}
--- chrome/browser/password_manager/android/save_update_password_message_delegate.h
+++ chrome/browser/password_manager/android/save_update_password_message_delegate.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/password_edit_dialog/android/password_edit_dialog_bridge.h"
+#include "chrome/browser/password_manager/android/local_passwords_migration_warning_util.h"
 #include "chrome/browser/ui/passwords/manage_passwords_state.h"
 #include "components/messages/android/message_enums.h"
 #include "components/messages/android/message_wrapper.h"
@@ -78,7 +79,9 @@ class SaveUpdatePasswordMessageDelegate {
   enum class SavePasswordDialogMenuItem { kNeverSave = 0, kEditPassword = 1 };
 
   SaveUpdatePasswordMessageDelegate(
-      PasswordEditDialogFactory password_edit_dialog_factory);
+      PasswordEditDialogFactory password_edit_dialog_factory,
+      base::RepeatingCallback<void(gfx::NativeWindow)>
+          password_migration_warning_bridge_callback);
 
   void DismissSaveUpdatePasswordMessage(messages::DismissReason dismiss_reason);
 
@@ -165,6 +168,8 @@ class SaveUpdatePasswordMessageDelegate {
 
   std::unique_ptr<messages::MessageWrapper> message_;
   std::unique_ptr<PasswordEditDialog> password_edit_dialog_;
+  base::RepeatingCallback<void(gfx::NativeWindow)>
+      create_migration_warning_callback_;
 };
 
 #endif  // CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_SAVE_UPDATE_PASSWORD_MESSAGE_DELEGATE_H_
