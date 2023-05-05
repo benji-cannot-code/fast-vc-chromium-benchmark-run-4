8f9ea0f374c65a2e3d60b5cf80166cc54e1af974
Dibyajyoti Pal <dibyapal@google.com>
2023-05-05 18:31:56 +0000

    [dPWA/AppHome] Override display of cr-checkbox for hidden elements
    The display:block property of cr-checkbox overrides the hidden
    functionality of cr-checkbox elements on the app home page. This CL fixes
    that by ensuring that the hidden behavior sticks.
    
    Change-Id: I9d7940dd2d44755a4b8fd9116a82c702b0ab8b5f
    Fixed: 1442720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509572
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140218}

--- chrome/browser/resources/app_home/app_item.ts
+++ chrome/browser/resources/app_home/app_item.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '//resources/cr_elements/cr_hidden_style.css.js';
 import '//resources/cr_elements/cr_checkbox/cr_checkbox.js';
 
 import {CrActionMenuElement} from '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
