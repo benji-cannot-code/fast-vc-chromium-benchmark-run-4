eceae46e851c3c6fa28536f3710bb6990f350e30
Charlie Reis <creis@chromium.org>
2023-05-05 00:30:07 +0000

    Fix CanGoBack consistency during NavigationStateChanged.
    WebContentsDelegates were seeing the value of CanGoBack change
    unexpectedly, because clearing the pending entry was notifying the
    delegate before the last committed entry index had been updated. This
    made it look like the user could go back again when it wasn't possible.
    
    Bug: 1439948
    Change-Id: I6aed795ba2c71894e8f5976446d0ebd302804395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504295
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139859}

--- content/browser/renderer_host/navigation_controller_impl.cc
+++ content/browser/renderer_host/navigation_controller_impl.cc
@@ -120,6 +120,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
+// TODO(https://crbug.com/1439948): Remove this base::Feature kill switch once
+// the feature safely rolls out.
+BASE_FEATURE(kUpdateSessionHistoryIndexBeforeNavigationStateChanged,
+             "UpdateSessionHistoryIndexBeforeNavigationStateChanged",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Invoked when entries have been pruned, or removed. For example, if the
 // current entries are [google, digg, yahoo], with the current entry google,
 // and the user types in cnet, then digg and yahoo are pruned.
@@ -2184,6 +2190,16 @@ void NavigationControllerImpl::RendererDidNavigateToExistingEntry(
   if (ui::PageTransitionIsRedirect(params.transition) && !is_same_document)
     entry->GetFavicon() = FaviconStatus();
 
+  // Update the last committed index to reflect the committed entry. Do this
+  // before calling DiscardNonCommittedEntriesWithCommitDetails, so that the
+  // delegate sees the correct committed index when notified of navigation
+  // state changes. (Otherwise CanGoBack may incorrectly return true, as in
+  // https://crbug.com/1439948.)
+  if (base::FeatureList::IsEnabled(
+          kUpdateSessionHistoryIndexBeforeNavigationStateChanged)) {
+    last_committed_entry_index_ = GetIndexOfEntry(entry);
+  }
+
   // We should also usually discard the pending entry if it corresponds to a
   // different navigation, since that one is now likely canceled.  In rare
   // cases, we leave the pending entry for another navigation in place when we
@@ -2195,8 +2211,12 @@ void NavigationControllerImpl::RendererDidNavigateToExistingEntry(
   if (!keep_pending_entry)
     DiscardNonCommittedEntriesWithCommitDetails(commit_details);
 
-  // Update the last committed index to reflect the committed entry.
-  last_committed_entry_index_ = GetIndexOfEntry(entry);
+  if (!base::FeatureList::IsEnabled(
+          kUpdateSessionHistoryIndexBeforeNavigationStateChanged)) {
+    // Update the last committed index to reflect the committed entry.
+    // (This is legacy behavior, in case the kill-switch needs to be used.)
+    last_committed_entry_index_ = GetIndexOfEntry(entry);
+  }
 }
 
 void NavigationControllerImpl::RendererDidNavigateNewSubframe(
