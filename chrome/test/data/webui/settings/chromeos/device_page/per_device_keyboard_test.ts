5b835aa3b78f56a7b6e2892fd9a01cfe2aac0986
YuhanYang <yyhyyh@google.com>
2023-05-05 23:20:07 +0000

    Settings Split: Update empty keyboard state to match the spec
    Updated two strings as the screenshot highlighted.
    
    Screenshot: https://screenshot.googleplex.com/B8EjibKDcuAmm9u
    Spec doc: go/input-customization-ux-page-14
    
    Test: verified visual effect and browser_tests
    Bug: b/241965700
    Change-Id: Ib1ac045028e778363a353c5dbfbbb06a9dbaf431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455618
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140407}

--- chrome/test/data/webui/settings/chromeos/device_page/per_device_keyboard_test.ts
+++ chrome/test/data/webui/settings/chromeos/device_page/per_device_keyboard_test.ts
@@ -183,7 +183,7 @@ suite('<settings-per-device-keyboard>', () => {
     assertTrue(isVisible(perDeviceKeyboardPage.shadowRoot!.querySelector(
         '#noKeyboardsConnectedContainer')));
     assertEquals(
-        'No keyboard detected',
+        'No keyboard connected',
         perDeviceKeyboardPage.shadowRoot!
             .querySelector<HTMLElement>(
                 '#noKeyboardsConnectedMessage')!.innerText.trim());
