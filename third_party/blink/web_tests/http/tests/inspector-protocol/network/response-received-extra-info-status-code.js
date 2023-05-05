d3bc4ae0620638759b3dde2d4534e6e74c3132df
Jaroslav Sevcik <jarin@chromium.org>
2023-05-05 11:49:06 +0000

    [test] Disable cache in inspector test to always get extra info [part 2]
    Bug: 1442680
    Change-Id: I3e8b266339ac4b013d566ae93bb5d0dd90d20ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508854
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140050}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/response-received-extra-info-status-code.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/response-received-extra-info-status-code.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       `Tests that the IP address space is reported on responseReceivedExtraInfo.`);
 
   await dp.Network.enable();
+  await dp.Network.setCacheDisabled({cacheDisabled: true});
   testRunner.log('Network Enabled');
 
   session.evaluateAsync(`fetch('index.html');`);
