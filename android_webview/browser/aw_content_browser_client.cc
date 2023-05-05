5545483ad35ce5e58a64de313302a15a0ec6609d
Xi Han <hanxi@chromium.org>
2023-05-05 16:17:15 +0000

    Revert "Implement user-agent client hints on Android WebView"
    This reverts commit 2a57a2590d23509cc2d4332e34bd63ef86c84dc4.
    
    Reason for revert: https://crbug.com/1442932
    
    Original change's description:
    > Implement user-agent client hints on Android WebView
    >
    > Implementation to provide the user-agent metadata client hints when the the user-agent contains default user-agent. For the totally overwrote user-agent, we won't generate the default user-agent client hints.
    >
    > Also, add a switch for enabling user-agent client hints, currently, we can't distinguish the Android and Android WebView from the complier flag, also the chromium feature flag has no option to disable feature on Android WebView but enable feature on Android.
    >
    > Bug: 1430051
    > Change-Id: Icc60c8004855dfea0512f32832a4e83fcf2bfbb7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450588
    > Commit-Queue: Victor Tan <victortan@chromium.org>
    > Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1139862}
    
    Bug: 1430051, 1442932
    Change-Id: I93d4f32b1198ef616eb812e11283d95293ad7aff
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508196
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Auto-Submit: Xi Han <hanxi@chromium.org>
    Owners-Override: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140158}

--- android_webview/browser/aw_content_browser_client.cc
+++ android_webview/browser/aw_content_browser_client.cc
@@ -1026,11 +1026,6 @@ std::string AwContentBrowserClient::GetUserAgent() {
   return android_webview::GetUserAgent();
 }
 
-blink::UserAgentMetadata AwContentBrowserClient::GetUserAgentMetadata() {
-  return embedder_support::GetUserAgentMetadata(
-      browser_context_->GetPrefService());
-}
-
 content::ContentBrowserClient::WideColorGamutHeuristic
 AwContentBrowserClient::GetWideColorGamutHeuristic() {
   if (base::FeatureList::IsEnabled(features::kWebViewWideColorGamutSupport)) {
