833f89fbdef36705bb5030977569c0956cbbaa36
Veronique Nguyen <veronguyen@google.com>
2023-05-05 15:37:32 +0000

    [iOS] Fix Password Bottom Sheet Title
    Show "Google Password Manager" instead of "Password Manager"
    
    Bug: 1442540
    Change-Id: Ia323cddfd890a84d13ab63ecd2b44094725e5e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505031
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140141}

--- ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_view_controller.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/favicon/favicon_attributes.h"
 #import "ios/chrome/common/ui/favicon/favicon_view.h"
 #import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
+#import "ios/chrome/grit/ios_google_chrome_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
 #import "url/gurl.h"
@@ -323,7 +324,7 @@ CGFloat const kLandscapeTableViewWidthMultiplier = 0.65;
 
 // Configures the title view of this ViewController.
 - (UIView*)setUpTitleView {
-  NSString* title = l10n_util::GetNSString(IDS_IOS_PASSWORD_MANAGER);
+  NSString* title = l10n_util::GetNSString(IDS_IOS_PASSWORD_BOTTOM_SHEET_TITLE);
   return password_manager::CreatePasswordManagerTitleView(title);
 }
 
