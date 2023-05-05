1260b699baaedff534a1676df082182104dffa51
Lukasz Anforowicz <lukasza@chromium.org>
2023-05-05 21:09:49 +0000

    Refactoring: RenderBitmap: Return value directly, not via out param.
    Bug: 1431991
    Change-Id: Ifb182f4e57ae2c3dcf979f36fc716065a2abe4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508065
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140341}

--- chrome/services/qrcode_generator/qrcode_generator_service_impl.cc
+++ chrome/services/qrcode_generator/qrcode_generator_service_impl.cc
@@ -197,11 +197,10 @@ static void DrawLocators(SkCanvas* canvas,
   // No locator on bottom-right.
 }
 
-void QRCodeGeneratorServiceImpl::RenderBitmap(
+SkBitmap QRCodeGeneratorServiceImpl::RenderBitmap(
     base::span<const uint8_t> data,
     const gfx::Size data_size,
-    const mojom::GenerateQRCodeRequestPtr& request,
-    mojom::GenerateQRCodeResponsePtr* response) {
+    const mojom::GenerateQRCodeRequest& request) {
   // Setup: create colors and clear canvas.
   SkBitmap bitmap;
   bitmap.allocN32Pixels(data_size.width() * kModuleSizePixels,
@@ -229,7 +228,7 @@ void QRCodeGeneratorServiceImpl::RenderBitmap(
           continue;
         }
 
-        if (request->render_module_style == mojom::ModuleStyle::CIRCLES) {
+        if (request.render_module_style == mojom::ModuleStyle::CIRCLES) {
           float xc = (x + 0.5) * kModuleSizePixels;
           float yc = (y + 0.5) * kModuleSizePixels;
           SkScalar radius = kModuleSizePixels / 2 - 1;
@@ -245,12 +244,12 @@ void QRCodeGeneratorServiceImpl::RenderBitmap(
   }
 
   DrawLocators(&canvas, data_size, paint_black, paint_white,
-               request->render_locator_style);
+               request.render_locator_style);
 
   SkRect bitmap_bounds;
   bitmap.getBounds(&bitmap_bounds);
 
-  switch (request->center_image) {
+  switch (request.center_image) {
     case mojom::CenterImage::DEFAULT_NONE:
       break;
     case mojom::CenterImage::CHROME_DINO:
@@ -262,7 +261,7 @@ void QRCodeGeneratorServiceImpl::RenderBitmap(
       break;
   }
 
-  (*response)->bitmap = bitmap;
+  return bitmap;
 }
 
 void QRCodeGeneratorServiceImpl::GenerateQRCode(
@@ -319,8 +318,8 @@ void QRCodeGeneratorServiceImpl::GenerateQRCode(
 
   {
     base::TimeTicks start_time = base::TimeTicks::Now();
-    RenderBitmap(base::make_span(qr_data->data), response->data_size, request,
-                 &response);
+    response->bitmap = RenderBitmap(base::make_span(qr_data->data),
+                                    response->data_size, *request);
     base::UmaHistogramTimes(
         "Sharing.QRCodeGeneration.Duration.QrPixelsToQrImage",
         base::TimeTicks::Now() - start_time);
