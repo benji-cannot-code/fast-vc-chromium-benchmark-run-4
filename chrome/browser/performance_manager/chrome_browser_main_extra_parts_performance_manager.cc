1cdefd8ca72ecaeffe31fac9c05f7e14c83b162c
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2023-05-05 19:37:38 +0000

    Reland "Factor out Battery Saver functionality from UserPerformanceTuningManager"
    This is a reland of commit bd50a773e01b98dc0599aeac38a4ac9ff8767184
    
    Original change's description:
    > Factor out Battery Saver functionality from UserPerformanceTuningManager
    >
    > This is in preparation to implement a ChromeOS-specific Battery Saver
    > that is controlled by the operating system.
    >
    > Bug: 1435413
    > Change-Id: Icb54e368e63d4e955faaf3ba7c3239612369ed1f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505371
    > Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1140204}
    
    Bug: 1435413
    Change-Id: I2713b7e7b6067df176c62f6df6f21f958440ba91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509972
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140280}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "base/power_monitor/battery_state_sampler.h"
 #include "base/power_monitor/power_monitor_buildflags.h"
 #include "base/system/sys_info.h"
 #include "base/time/default_tick_clock.h"
@@ -66,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if !BUILDFLAG(IS_ANDROID)
-#include "base/power_monitor/battery_state_sampler.h"
 #include "chrome/browser/performance_manager/mechanisms/page_freezer.h"
 #include "chrome/browser/performance_manager/policies/heuristic_memory_saver_policy.h"
 #include "chrome/browser/performance_manager/policies/high_efficiency_mode_policy.h"
