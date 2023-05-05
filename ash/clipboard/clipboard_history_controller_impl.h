30e631496e560af1e0afd3a9adf8cd19b892a032
andrewxu <andrewxu@chromium.org>
2023-05-05 00:39:23 +0000

    [Multipaste] Notify of item updates when availability changes
    Currently, ClipboardHistoryController notifies observers of clipboard
    history item update when item(s) are added to, removed from, or
    updated in the clipboard history. In fact, ClipboardHistoryController
    should also notify observers when clipboard history availability
    changes. For example, clipboard history becomes disabled due to
    the change in session state.
    
    In this CL, ClipboardHistoryController becomes a session observer so
    that the controller can notify observers when the clipboard history
    enabling state is toggled.
    
    In addition, in this CL, ClipboardHistoryController posts a task to
    notify observers. In this way, observers are only notified of
    clipboard history update once if there are multiple changes in a
    short duration.
    
    The earlier patch set (before and including patch set 15), an
    independent class called ClipboardHistoryDescriptorManager is
    created to achieve the same functionality. In the latest patch set,
    ClipboardHistoryDescriptorManager is removed and its code logic is
    incorporated into ClipboardHistoryControllerImpl.
    
    Bug: b/278916298
    Change-Id: I842a9b3fb826bd12ed328ce5201e1de0142414c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482295
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139867}

--- ash/clipboard/clipboard_history_controller_impl.h
+++ ash/clipboard/clipboard_history_controller_impl.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/clipboard/clipboard_history.h"
 #include "ash/clipboard/clipboard_history_resource_manager.h"
 #include "ash/public/cpp/clipboard_history_controller.h"
+#include "ash/public/cpp/session/session_observer.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
@@ -43,6 +44,7 @@ class ClipboardHistoryMenuModelAdapter;
 class ClipboardHistoryResourceManager;
 class ClipboardManagerBubbleView;
 class ClipboardNudgeController;
+enum class LoginStatus;
 class ScopedClipboardHistoryPause;
 
 constexpr char kClipboardCopyToastId[] = "CopiedToClipboard";
@@ -53,7 +55,8 @@ class ASH_EXPORT ClipboardHistoryControllerImpl
     : public ClipboardHistoryController,
       public ClipboardHistory::Observer,
       public ClipboardHistoryResourceManager::Observer,
-      public views::WidgetObserver {
+      public views::WidgetObserver,
+      public SessionObserver {
  public:
   // Source and plain vs. rich text info for each paste. These values are used
   // in the Ash.ClipboardHistory.PasteType histogram and therefore cannot be
@@ -107,6 +110,12 @@ class ASH_EXPORT ClipboardHistoryControllerImpl
       OnMenuClosingCallback callback) override;
   void GetHistoryValues(GetHistoryValuesCallback callback) const override;
 
+  // Whether the clipboard history has items.
+  bool IsEmpty() const;
+
+  // Fires the timer to notify observers of item updates immediately.
+  void FireItemUpdateNotificationTimerForTest();
+
   // Returns bounds for the contextual menu in screen coordinates.
   gfx::Rect GetMenuBoundsInScreenForTest() const;
 
@@ -115,9 +124,6 @@ class ASH_EXPORT ClipboardHistoryControllerImpl
   // requested and before the values are returned.
   void BlockGetHistoryValuesForTest();
   void ResumeGetHistoryValuesForTest();
-
-  // Whether the ClipboardHistory has items.
-  bool IsEmpty() const;
 
   // Returns the history which tracks what is being copied to the clipboard.
   const ClipboardHistory* history() const { return clipboard_history_.get(); }
@@ -160,7 +166,7 @@ class ASH_EXPORT ClipboardHistoryControllerImpl
   class MenuDelegate;
 
   // ClipboardHistoryController:
-  bool CanShowMenu() const override;
+  bool HasAvailableHistoryItems() const override;
   void OnScreenshotNotificationCreated() override;
   std::unique_ptr<ScopedClipboardHistoryPause> CreateScopedPause() override;
   std::vector<std::string> GetHistoryItemIds() const override;
@@ -184,6 +190,18 @@ class ASH_EXPORT ClipboardHistoryControllerImpl
 
   // views::WidgetObserver:
   void OnWidgetClosing(views::Widget* widget) override;
+
+  // SessionObserver:
+  void OnSessionStateChanged(session_manager::SessionState state) override;
+  void OnLoginStatusChanged(LoginStatus login_status) override;
+
+  // Posts a task to notify `observers_` of updates to clipboard history items.
+  void PostItemUpdateNotificationTask();
+
+  // Notifies `observers_` of updates to clipboard history items. No-op if
+  // there are no available clipboard history items and there were no available
+  // history items in the last notification.
+  void MaybeNotifyObserversOfItemUpdate();
 
   // Invoked by `GetHistoryValues()` once all clipboard instances with images
   // have been encoded into PNGs. Calls `callback` with the clipboard history
@@ -285,6 +303,14 @@ class ASH_EXPORT ClipboardHistoryControllerImpl
   // Indicates the count of pastes which are triggered through the clipboard
   // history menu and are waiting for the confirmations from `ClipboardHistory`.
   int pastes_to_be_confirmed_ = 0;
+
+  // Used to post a task to notify `observers_` of updates to clipboard history
+  // items.
+  base::OneShotTimer item_update_notification_timer_;
+
+  // True if there were available items in the last clipboard history update
+  // notification.
+  bool has_available_items_in_last_update_ = false;
 
   // Created when a test requests that `GetHistoryValues()` wait for some work
   // to be done before encoding finishes. Reset and recreated if the same test
