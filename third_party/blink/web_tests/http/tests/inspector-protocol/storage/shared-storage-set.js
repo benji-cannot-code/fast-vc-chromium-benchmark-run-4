7e4788649623eef2a29b7805ac9aa95616896faf
Jaroslav Sevcik <jarin@chromium.org>
2023-05-05 05:37:00 +0000

    [test] Remove storage entries before completing the test
    Bug: 1407335
    Change-Id: I3c4fd715584db12d527cc042134f5b16847617da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506158
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139941}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/shared-storage-set.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/shared-storage-set.js
@@ -81,9 +81,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await getSharedStorageMetadata(dp, testRunner, baseOrigin);
   await getSharedStorageEntries(dp, testRunner, baseOrigin);
 
-  testRunner.completeTest();
-
   await dp.Storage.clearSharedStorageEntries({
     ownerOrigin: baseOrigin
   });
+
+  testRunner.completeTest();
 })
