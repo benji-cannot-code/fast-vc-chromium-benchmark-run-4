cbc9f2433233ac64d68e3b3a1dd0af18497f6338
Alex Moshchuk <alexmos@chromium.org>
2023-05-05 18:49:18 +0000

    Fix SitePerProcessMemoryThresholdBrowserTestIsolation test flakiness.
    The flakiness was because these tests ended up using a cached value in
    SiteIsolationPolicy::ShouldDisableSiteIsolationDueToMemoryThreshold(),
    which could be wrong if it was previously set by other tests in this
    file.  This CL ensures that all tests in this file uniformly turn off
    this caching in their base test class, since they all use different
    site isolation modes and thresholds and the caching could conflict
    across tests.  Previously, this was already done by some, but not all
    of these tests.
    
    Bug: 1406968
    Change-Id: Ib82b45a10e5d3b8e346b05ea7f211270bc3ad947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508538
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140234}

--- components/site_isolation/site_isolation_policy_unittest.cc
+++ components/site_isolation/site_isolation_policy_unittest.cc
@@ -76,6 +76,14 @@ class BaseSiteIsolationTest : public testing::Test {
     content::SetBrowserClientForTesting(original_client_);
   }
 
+  void SetUp() override {
+    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(true);
+  }
+
+  void TearDown() override {
+    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(false);
+  }
+
  protected:
   void SetEnableStrictSiteIsolation(bool enable) {
     browser_client_.strict_isolation_enabled_ = enable;
@@ -525,6 +533,7 @@ class SitePerProcessMemoryThresholdBrowserTest
     // On Android official builds, we expect to isolate an additional set of
     // built-in origins.
     expected_embedder_origins_ = GetBrowserSpecificBuiltInIsolatedOrigins();
+    BaseSiteIsolationTest::SetUp();
   }
 
  protected:
@@ -728,11 +737,7 @@ class PasswordSiteIsolationFieldTrialTest : public BaseSiteIsolationTest {
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kEnableLowEndDeviceMode);
     EXPECT_EQ(512, base::SysInfo::AmountOfPhysicalMemoryMB());
-    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(true);
-  }
-
-  void TearDown() override {
-    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(false);
+    BaseSiteIsolationTest::SetUp();
   }
 
  protected:
@@ -945,11 +950,7 @@ class StrictOriginIsolationFieldTrialTest : public BaseSiteIsolationTest {
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kEnableLowEndDeviceMode);
     EXPECT_EQ(512, base::SysInfo::AmountOfPhysicalMemoryMB());
-    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(true);
-  }
-
-  void TearDown() override {
-    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(false);
+    BaseSiteIsolationTest::SetUp();
   }
 
  protected:
@@ -1075,6 +1076,7 @@ class BuiltInIsolatedOriginsTest : public SiteIsolationPolicyTest {
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kEnableLowEndDeviceMode);
     EXPECT_EQ(512, base::SysInfo::AmountOfPhysicalMemoryMB());
+    SiteIsolationPolicyTest::SetUp();
   }
 };
 
@@ -1206,13 +1208,7 @@ class OptInOriginIsolationPolicyTest : public BaseSiteIsolationTest {
     SetEnableStrictSiteIsolation(false);
     // Enable Origin-Agent-Cluster header.
     feature_list_.InitAndEnableFeature(::features::kOriginIsolationHeader);
-    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(true);
     BaseSiteIsolationTest::SetUp();
-  }
-
-  void TearDown() override {
-    SiteIsolationPolicy::SetDisallowMemoryThresholdCachingForTesting(false);
-    BaseSiteIsolationTest::TearDown();
   }
 
   content::BrowserContext* browser_context() { return &browser_context_; }
