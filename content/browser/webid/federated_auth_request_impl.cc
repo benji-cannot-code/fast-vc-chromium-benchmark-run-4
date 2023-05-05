e60604f9c02078b7297d110eb5b9a195c8283d98
Zachary Tan <tanzachary@chromium.org>
2023-05-05 04:00:39 +0000

    [FedCM] Fix interactions between accounts view and IDP sign-in dialog
    1. Clicking "Continue" button on failure dialog closes the failure
    dialog and opens the IDP sign-in dialog
    2. Completing the sign-in flow closes the IDP sign-in dialog and opens
    the accounts dialog
    3. Refreshing the page closes the IDP sign-in dialog.
    
    Bug: 1430830
    Change-Id: I02c19acb7acd1daa51e2afae7953f9b9e45e11d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4501400
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139927}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -1278,8 +1278,8 @@ void FederatedAuthRequestImpl::HandleAccountsFetchFailure(
                      FederatedAuthRequestResult::kError,
                      TokenStatus::kNotSignedInWithIdp,
                      /*should_delay_callback=*/true),
-      base::BindOnce(&FederatedAuthRequestImpl::CreateIdentityRegistry,
-                     weak_ptr_factory_.GetWeakPtr(), idp_origin));
+      base::BindRepeating(&FederatedAuthRequestImpl::CreateIdentityRegistry,
+                          weak_ptr_factory_.GetWeakPtr(), idp_origin));
 }
 
 void FederatedAuthRequestImpl::CreateIdentityRegistry(
