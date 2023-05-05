63575f35095a4882f3aa608bebf8c4bb057089ed
Ramin Halavati <rhalavati@chromium.org>
2023-05-05 05:08:34 +0000

    Install/Uninstall ScreenAI DLC only after OOBE.
    ScreenAI DLC installation is managed only after OOBE.
    
    AX-Relnotes: n/a.
    Bug: 1278249
    Change-Id: I95a00063681272ef48bce169b31b63d9244062c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502944
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139937}

--- chrome/browser/ash/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/chrome_browser_main_parts_ash.cc
@@ -243,7 +243,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/quirks/quirks_manager.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
-#include "components/services/screen_ai/public/cpp/screen_ai_chromeos_installer.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/sync/base/command_line_switches.h"
 #include "components/user_manager/known_user.h"
@@ -1264,9 +1263,6 @@ void ChromeBrowserMainPartsAsh::PostProfileInit(Profile* profile,
 
     login_screen_extensions_storage_cleaner_ =
         std::make_unique<LoginScreenExtensionsStorageCleaner>();
-
-    screen_ai::chrome_os_installer::ManageInstallation(
-        g_browser_process->local_state());
 
     ash::ShillManagerClient::Get()->SetProperty(
         shill::kEnableRFC8925Property,
