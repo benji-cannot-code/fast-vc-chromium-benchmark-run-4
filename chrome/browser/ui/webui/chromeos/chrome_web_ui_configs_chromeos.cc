c5860db105a9322b09c4550583ec0d0cab1cf06c
Jun W <jungoog@google.com>
2023-05-06 07:17:54 +0000

    [WebUI] Migrate ProximityAuthUI to WebUIConfig
    Context:
    
    Currently WebUI properties, e.g. CSPs, requesting schemes, host, mojo,
    etc. are stored in WebUIControllers themselves or in separate lists
    like IsWebUIAllowedToMakeNetworkRequests. The lifetime of a
    WebUIController is bound to the frame, which makes it hard to use for
    some use cases where the WebUIController hasn't been created yet. A
    non-dynamically allocated class where clients could query a WebUI's
    properties would be easier to use and audit.
    
    WebUIConfig is a class that stores properties of WebUIs. For now the
    properties are 1. the WebUI's origin, 2. if the WebUI is enabled,
    and 3. GetURLDataSource() method for service worker initialization.
    In the future this class could include information like CSPs, if we
    should enable Mojo, if we should enable chrome.send(), if we should
    allow network requests, URLDataSource, etc.
    
    Bug: 1317510
    Change-Id: I6b924082e4e912b3d02f3296ee1f5f81a0402ea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4465668
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Junguo (Jun) W. <jungoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140482}

--- chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/shortcut_customization_ui/shortcut_customization_app_ui.h"
 #include "ash/webui/system_extensions_internals_ui/system_extensions_internals_ui.h"
 #include "chrome/browser/ash/guest_os/public/installer_delegate_factory.h"
+#include "chrome/browser/ash/multidevice_debug/proximity_auth_ui_config.h"
 #include "chrome/browser/ash/net/network_health/network_health_manager.h"
 #include "chrome/browser/ash/os_feedback/chrome_os_feedback_delegate.h"
 #include "chrome/browser/ash/scanning/chrome_scanning_app_delegate.h"
@@ -245,6 +246,8 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(
       std::make_unique<ash::reporting::EnterpriseReportingUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::PowerUIConfig>());
+  map.AddWebUIConfig(
+      std::make_unique<ash::multidevice::ProximityAuthUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::RemoteMaintenanceCurtainUIConfig>());
   map.AddWebUIConfig(
       MakeComponentConfigWithDelegate<ash::ScanningUIConfig, ash::ScanningUI,
