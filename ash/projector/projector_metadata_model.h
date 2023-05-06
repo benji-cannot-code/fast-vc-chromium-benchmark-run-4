ab8ee13d0fc0ffcc3fdc7a17efcf869a919337de
Sergii Bykov <sbykov@google.com>
2023-05-06 10:17:00 +0000

    [Code Health] Replace remaining base.Value.SetIntKey with dict Set
    Bug: 1303949
    Change-Id: I80555fcda6c17d01fb69d3cff4aa126b78a60f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508954
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140496}

--- ash/projector/projector_metadata_model.h
+++ ash/projector/projector_metadata_model.h
@@ -12,11 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/time/time.h"
-#include "media/mojo/mojom/speech_recognition.mojom.h"
-
-namespace base {
-class Value;
-}  // namespace base
+#include "base/values.h"
+#include "media/mojo/mojom/speech_recognition_result.h"
 
 namespace ash {
 
@@ -47,7 +44,7 @@ class MetadataItem {
   base::TimeDelta& end_time() { return end_time_; }
 
   // Return the serialized metadata item. This is used for storage.
-  virtual base::Value ToJson() = 0;
+  virtual base::Value::Dict ToJson() = 0;
 
  protected:
   // The start time of the metadata item from the start of the recording
@@ -69,7 +66,7 @@ class ASH_EXPORT ProjectorKeyIdea : public MetadataItem {
   ProjectorKeyIdea& operator=(const ProjectorKeyIdea&) = delete;
   ~ProjectorKeyIdea() override;
 
-  base::Value ToJson() override;
+  base::Value::Dict ToJson() override;
 };
 
 // Class to describe a transcription.
@@ -84,7 +81,7 @@ class ASH_EXPORT ProjectorTranscript : public MetadataItem {
   ProjectorTranscript& operator=(const ProjectorTranscript&) = delete;
   ~ProjectorTranscript() override;
 
-  base::Value ToJson() override;
+  base::Value::Dict ToJson() override;
 
  private:
   std::vector<media::HypothesisParts> hypothesis_parts_;
@@ -115,7 +112,7 @@ class ASH_EXPORT ProjectorMetadata {
   size_t GetTranscriptsCount() const { return transcripts_.size(); }
 
  private:
-  base::Value ToJson();
+  base::Value::Dict ToJson();
 
   std::vector<std::unique_ptr<ProjectorTranscript>> transcripts_;
   std::vector<std::unique_ptr<ProjectorKeyIdea>> key_ideas_;
--- chrome/browser/ash/system_web_apps/types/system_web_app_data.cc
+++ chrome/browser/ash/system_web_apps/types/system_web_app_data.cc
@@ -10,10 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-base::Value SystemWebAppData::AsDebugValue() const {
-  base::Value root(base::Value::Type::DICT);
-  root.SetIntKey("system_app_type", static_cast<int>(system_app_type));
-  return root;
+base::Value::Dict SystemWebAppData::AsDebugValue() const {
+  return base::Value::Dict().Set("system_app_type",
+                                 static_cast<int>(system_app_type));
 }
 
 bool operator==(const SystemWebAppData& chromeos_data1,
