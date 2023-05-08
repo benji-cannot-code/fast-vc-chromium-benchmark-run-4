16b12167b21ba456b090121e1a5ecd1c09ca4307
Kevin Radtke <kradtke@chromium.org>
2023-05-08 18:08:49 +0000

    Anchored Nudge Data struct
    Add AnchoredNudgeData struct to pass and store AnchoredNudge parameters
    between the feature code, manager and the view.
    
    Also add a base catalog enum for AnchoredNudge objects which will be
    used to record general metrics for all implemented nudges.
    
    Bug: b:279653685, b:279817749
    Change-Id: I1a786cfaa184efa571d5b773ee2c3b0aacfe33ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496879
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140958}

--- ash/system/toast/anchored_nudge.h
+++ ash/system/toast/anchored_nudge.h
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/shelf_types.h"
+#include "ash/public/cpp/system/anchored_nudge_data.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
 namespace views {
-class View;
 class Widget;
 }  // namespace views
 
@@ -27,7 +27,7 @@ class ASH_EXPORT AnchoredNudge : public views::BubbleDialogDelegateView {
  public:
   METADATA_HEADER(AnchoredNudge);
 
-  explicit AnchoredNudge(views::View* anchor);
+  explicit AnchoredNudge(AnchoredNudgeData nudge_data);
   AnchoredNudge(const AnchoredNudge&) = delete;
   AnchoredNudge& operator=(const AnchoredNudge&) = delete;
   ~AnchoredNudge() override;
