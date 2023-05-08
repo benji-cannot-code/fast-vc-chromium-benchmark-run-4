c2b498fe640ad826a916c3cbe83c89189cb437dc
Joe Downing <joedow@google.com>
2023-05-08 23:28:16 +0000

    Add breakpad impl for generating minidumps on Linux
    This CL implements the Linux breakpad handler, this is
    similar to the Windows breakpad handler except that
    since the Linux host is a single process, crash dump
    and metadata generation will be done within the host
    process. In a follow-up CL I will add a crash dump
    upload utility that will be owned by the
    chrome-remote-desktop service.
    
    Change-Id: I884d395c0e06699769779669b93fb3369e903693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504300
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141118}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -396,6 +396,7 @@ class SelectFileDialogBridge;
 
 namespace remoting {
 class AutoThread;
+class ScopedAllowBlockingForCrashReporting;
 class ScopedBypassIOThreadRestrictions;
 namespace protocol {
 class ScopedAllowSyncPrimitivesForWebRtcDataStreamAdapter;
@@ -641,6 +642,7 @@ class BASE_EXPORT [[maybe_unused, nodiscard]] ScopedAllowBlocking {
   friend class remote_cocoa::SelectFileDialogBridge;
   friend class remoting::
       ScopedBypassIOThreadRestrictions;  // http://crbug.com/1144161
+  friend class remoting::ScopedAllowBlockingForCrashReporting;
   friend class ui::DrmDisplayHostManager;
   friend class ui::ScopedAllowBlockingForGbmSurface;
   friend class ui::SelectFileDialogLinux;
