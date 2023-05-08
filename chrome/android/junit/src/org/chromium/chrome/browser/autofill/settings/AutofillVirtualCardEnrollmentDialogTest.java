2c14b66ebc5c81684f39f1d6fa39f7648ddc0e00
Vishwas Uppoor <vishwasuppoor@google.com>
2023-05-08 15:57:43 +0000

    [Metadata][VCN][Dialog] Show larger icons in enrollment prompts
    Show larger (40x24)dp icons for credit cards in VCN enrollment dialog
    and VCN enrollment infobar.
    
    Infobar (upstream/downstream enrollment):
    Before: https://screenshot.googleplex.com/6nSzHvjR8JwVQeW
    After: https://screenshot.googleplex.com/58J65uRap3rHFM3
    
    Dialog (settings page enrollment):
    Before: https://screenshot.googleplex.com/A5zPZ7Y8RcJZ8hJ
    After: https://screenshot.googleplex.com/6VuC9txV9usDQnr
    
    All changes are behind AutofillEnableNewCardArtAndNetworkImages flag.
    
    Bug: 1434340
    Change-Id: Iea6af0f3641fe320f750c4aedaf7547e09e7364d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507567
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1140875}

--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AutofillVirtualCardEnrollmentDialogTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AutofillVirtualCardEnrollmentDialogTest.java
@@ -22,6 +22,7 @@ import androidx.test.filters.SmallTest;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
@@ -33,6 +34,8 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeStringConstants;
 import org.chromium.chrome.browser.autofill.LegalMessageLine;
+import org.chromium.chrome.test.util.browser.Features;
+import org.chromium.components.autofill.AutofillFeatures;
 import org.chromium.components.autofill.VirtualCardEnrollmentLinkType;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager.ModalDialogType;
@@ -46,6 +49,7 @@ import java.util.List;
 
 /** Unit tests for {@link AutofillVirtualCardEnrollmentDialog}. */
 @RunWith(BaseRobolectricTestRunner.class)
+@Features.EnableFeatures({AutofillFeatures.AUTOFILL_ENABLE_NEW_CARD_ART_AND_NETWORK_IMAGES})
 public class AutofillVirtualCardEnrollmentDialogTest {
     private static final String LEGAL_MESSAGE_URL = "http://www.google.com";
     private static final String ACCEPT_BUTTON_TEXT = "Yes";
@@ -53,6 +57,9 @@ public class AutofillVirtualCardEnrollmentDialogTest {
 
     @Rule
     public MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public TestRule mProcessor = new Features.JUnitProcessor();
 
     @Mock
     private Callback<Integer> mResultHandlerMock;
