eee5a10b7ca7620a6ef37bd1248469ff7b4e2e11
Kent Tamura <tkent@chromium.org>
2023-05-08 07:18:16 +0000

    [RemoveLegacy] Remove 'expansion' feature of TextRun
    TextRun::expansion_ is always 0, and expansion_behavior_ is not
    referred.
    
    Bug: 1229581
    Change-Id: Ief402e7304dda1d3506e0fa4054dae9bef983cd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4511253
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140726}

--- third_party/blink/renderer/platform/exported/web_text_run.cc
+++ third_party/blink/renderer/platform/exported/web_text_run.cc
@@ -36,8 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 WebTextRun::operator TextRun() const {
-  return TextRun(text, 0, 0, TextRun::kAllowTrailingExpansion,
-                 rtl ? TextDirection::kRtl : TextDirection::kLtr,
+  return TextRun(text, 0, rtl ? TextDirection::kRtl : TextDirection::kLtr,
                  directional_override);
 }
 
