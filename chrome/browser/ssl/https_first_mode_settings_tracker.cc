3170bf3a55f07a4df0124bdbd6af97a605330f04
Mustafa Emre Acer <meacer@chromium.org>
2023-05-08 21:51:31 +0000

    HTTPS-First Mode: Add feature flag for Site Engagement heuristic
    This CL adds a feature flag for HTTPS-First Mode Site Engagement
    heuristic. The flag will control whether to show an HTTPS-First Mode
    interstitial based on the site-engagement scores of https and http URLs
    of a hostname.
    
    This CL also converts the hardcoded thresholds used to enable/disable
    HFM on a site to feature params.
    
    Bug: 1435222
    Change-Id: I0e7a106a520fa01603096713215b3c04af82a3a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503311
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141074}

--- chrome/browser/ssl/https_first_mode_settings_tracker.cc
+++ chrome/browser/ssl/https_first_mode_settings_tracker.cc
@@ -26,6 +26,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+// Minimum score of an HTTPS origin to enable HFM on its hostname.
+const base::FeatureParam<int> kHttpsAddThreshold{
+    &features::kHttpsFirstModeV2ForEngagedSites, "https-add-threshold", 40};
+
+// Maximum score of an HTTP origin to enable HFM on its hostname.
+const base::FeatureParam<int> kHttpsRemoveThreshold{
+    &features::kHttpsFirstModeV2ForEngagedSites, "https-remove-threshold", 30};
+
+// If HTTPS score goes below kHttpsRemoveThreshold or HTTP score goes above
+// kHttpRemoveThreshold, disable HFM on this hostname.
+const base::FeatureParam<int> kHttpAddThreshold{
+    &features::kHttpsFirstModeV2ForEngagedSites, "http-add-threshold", 5};
+const base::FeatureParam<int> kHttpRemoveThreshold{
+    &features::kHttpsFirstModeV2ForEngagedSites, "http-remove-threshold", 10};
+
 namespace {
 
 using security_interstitials::https_only_mode::SiteEngagementHeuristicState;
@@ -35,18 +50,6 @@ const char kHttpsFirstModeSyntheticFieldTrialName[] =
     "HttpsFirstModeClientSetting";
 const char kHttpsFirstModeSyntheticFieldTrialEnabledGroup[] = "Enabled";
 const char kHttpsFirstModeSyntheticFieldTrialDisabledGroup[] = "Disabled";
-
-// Minimum score of an HTTPS origin to enable HFM on its hostname.
-// TODO(crbug.com/1435222): Convert these into feature params.
-constexpr double kHttpsAddThreshold = 40;
-
-// Maximum score of an HTTP origin to enable HFM on its hostname.
-constexpr double kHttpAddThreshold = 5;
-
-// If HTTPS score goes below kHttpsRemoveThreshold or HTTP score goes above
-// kHttpRemoveThreshold, disable HFM on this hostname.
-constexpr double kHttpsRemoveThreshold = 30;
-constexpr double kHttpRemoveThreshold = 10;
 
 // Returns the HTTP URL from `http_url` using the test port numbers, if any.
 // TODO(crbug.com/1435222): Refactor and merge with UpgradeUrlToHttps().
@@ -178,11 +181,17 @@ void HttpsFirstModeService::OnAdvancedProtectionStatusChanged(bool enabled) {
 
 void HttpsFirstModeService::MaybeEnableHttpsFirstModeForUrl(Profile* profile,
                                                             const GURL& url) {
+  // Ideal parameter order is kHttpsAddThreshold > kHttpsRemoveThreshold >
+  // kHttpRemoveThreshold > kHttpAddThreshold.
+  if (!(kHttpsAddThreshold.Get() > kHttpsRemoveThreshold.Get() &&
+        kHttpsRemoveThreshold.Get() > kHttpRemoveThreshold.Get() &&
+        kHttpRemoveThreshold.Get() > kHttpAddThreshold.Get())) {
+    return;
+  }
+
   StatefulSSLHostStateDelegate* state =
       static_cast<StatefulSSLHostStateDelegate*>(
           profile_->GetSSLHostStateDelegate());
-  static_assert(kHttpsAddThreshold > kHttpsRemoveThreshold);
-  static_assert(kHttpAddThreshold < kHttpRemoveThreshold);
 
   // StatefulSSLHostStateDelegate can be null during tests. In that case, we
   // can't save the site setting.
@@ -199,8 +208,8 @@ void HttpsFirstModeService::MaybeEnableHttpsFirstModeForUrl(Profile* profile,
 
   double https_score = engagement_svc->GetScore(https_url);
   double http_score = engagement_svc->GetScore(http_url);
-  bool should_enable =
-      https_score >= kHttpsAddThreshold && http_score <= kHttpAddThreshold;
+  bool should_enable = https_score >= kHttpsAddThreshold.Get() &&
+                       http_score <= kHttpAddThreshold.Get();
   if (!enforced && should_enable) {
     state->SetHttpsEnforcementForHost(url.host(),
                                       /*enforced=*/true,
@@ -208,8 +217,8 @@ void HttpsFirstModeService::MaybeEnableHttpsFirstModeForUrl(Profile* profile,
     return;
   }
 
-  bool should_disable = https_score <= kHttpsRemoveThreshold ||
-                        http_score >= kHttpRemoveThreshold;
+  bool should_disable = https_score <= kHttpsRemoveThreshold.Get() ||
+                        http_score >= kHttpRemoveThreshold.Get();
   if (enforced && should_disable) {
     state->SetHttpsEnforcementForHost(url.host(),
                                       /*enforced=*/false,
