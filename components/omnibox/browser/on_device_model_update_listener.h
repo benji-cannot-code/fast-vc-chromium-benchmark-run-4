dbcdea406e9291e15b98c4b19f0bb620d02f8bdf
Ce Chen <cch@chromium.org>
2023-05-08 14:45:34 +0000

    [omnibox][ml] add tail model service to on device provider.
    After this change, the provider will no longer own the model executor
    but call it via client->OnDeviceTailModelService. Also, clean up
    executor related code accordingly.
    
    Bug: 1372112
    Change-Id: If0262e6feebf57ba2442512ea8ce789ce8fbf9ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503628
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: C Ch <cch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140844}

--- components/omnibox/browser/on_device_model_update_listener.h
+++ components/omnibox/browser/on_device_model_update_listener.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/no_destructor.h"
 #include "base/threading/thread_checker.h"
-#include "components/optimization_guide/proto/on_device_tail_suggest_model_metadata.pb.h"
 
 // This class is used by OnDeviceHeadSuggestComponentInstaller or
 // OnDeviceTailModelObserver to hold the filenames for the on device models
@@ -26,20 +25,7 @@ class OnDeviceModelUpdateListener {
   // |head_model_dir_| and |head_model_filename_|.
   void OnHeadModelUpdate(const base::FilePath& model_dir);
 
-  // Called by on device tail model observer when tail model update is completed
-  // to update |tail_model_filename_|, |vocab_filename_| and
-  // |tail_model_metadata_|.
-  void OnTailModelUpdate(
-      const base::FilePath& model_file,
-      const base::flat_set<base::FilePath>& additional_files,
-      const optimization_guide::proto::OnDeviceTailSuggestModelMetadata&
-          metadata);
-
   std::string head_model_filename() const;
-  base::FilePath tail_model_filepath() const;
-  base::FilePath vocab_filepath() const;
-  optimization_guide::proto::OnDeviceTailSuggestModelMetadata
-  tail_model_metadata() const;
 
  private:
   friend class base::NoDestructor<OnDeviceModelUpdateListener>;
@@ -59,16 +45,6 @@ class OnDeviceModelUpdateListener {
 
   // The filename of the head model.
   std::string head_model_filename_;
-
-  // The file path of the tail model.
-  base::FilePath tail_model_filepath_;
-
-  // The file path of the vocabulary file for the tail model.
-  base::FilePath vocab_filepath_;
-
-  // The metadata for the tail model.
-  optimization_guide::proto::OnDeviceTailSuggestModelMetadata
-      tail_model_metadata_;
 
   THREAD_CHECKER(thread_checker_);
 };
