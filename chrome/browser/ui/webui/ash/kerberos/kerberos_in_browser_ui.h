d6a45ee017d74035d24b5bb779cf703403c4ed62
Jun W <jungoog@google.com>
2023-05-08 04:45:58 +0000

    [WebUI] Migrate KerberosInBrowserUI to WebUIConfig
    Context:
    Currently WebUI properties, e.g. CSPs, requesting schemes, host, mojo,
    etc. are stored in WebUIControllers themselves or in separate lists
    like IsWebUIAllowedToMakeNetworkRequests. The lifetime of a
    WebUIController is bound to the frame, which makes it hard to use for
    some use cases where the WebUIController hasn't been created yet. A
    non-dynamically allocated class where clients could query a WebUI's
    properties would be easier to use and audit.
    
    WebUIConfig is a class that stores properties of WebUIs. For now the
    properties are 1. the WebUI's origin, 2. if the WebUI is enabled,
    and 3. GetURLDataSource() method for service worker initialization.
    In the future this class could include information like CSPs, if we
    should enable Mojo, if we should enable chrome.send(), if we should
    allow network requests, URLDataSource, etc.
    
    Bug: 1317510
    Change-Id: I7dd205e286f824d436298de56c8408ad3c05f306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4498447
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Junguo (Jun) W. <jungoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140696}

--- chrome/browser/ui/webui/ash/kerberos/kerberos_in_browser_ui.h
+++ chrome/browser/ui/webui/ash/kerberos/kerberos_in_browser_ui.h
@@ -6,10 +6,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_ASH_KERBEROS_KERBEROS_IN_BROWSER_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_ASH_KERBEROS_KERBEROS_IN_BROWSER_UI_H_
 
+#include "ash/webui/common/chrome_os_webui_config.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/common/webui_url_constants.h"
+#include "content/public/common/url_constants.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
 
+namespace content {
+class BrowserContext;
+}  // namespace content
+
 namespace ash {
+
+class KerberosInBrowserUI;
+
+// The WebUIConfig for the Kerberos UI class.
+class KerberosInBrowserUIConfig
+    : public ChromeOSWebUIConfig<KerberosInBrowserUI> {
+ public:
+  KerberosInBrowserUIConfig();
+
+  bool IsWebUIEnabled(content::BrowserContext* browser_context) override;
+};
+
 // Kerberos UI class. This UI is invoked, when Kerberos authentication
 // fails in the browser in ChromeOS.
 class KerberosInBrowserUI : public ui::WebDialogUI {
--- chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/in_session_password_change/password_change_ui.h"
 #include "chrome/browser/ui/webui/ash/internet_config_dialog.h"
 #include "chrome/browser/ui/webui/ash/internet_detail_dialog.h"
+#include "chrome/browser/ui/webui/ash/kerberos/kerberos_in_browser_ui.h"
 #include "chrome/browser/ui/webui/ash/launcher_internals/launcher_internals_ui.h"
 #include "chrome/browser/ui/webui/ash/lock_screen_reauth/lock_screen_network_ui.h"
 #include "chrome/browser/ui/webui/ash/lock_screen_reauth/lock_screen_start_reauth_ui.h"
@@ -241,6 +242,7 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(std::make_unique<ash::HumanPresenceInternalsUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::InternetConfigDialogUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::InternetDetailDialogUIConfig>());
+  map.AddWebUIConfig(std::make_unique<ash::KerberosInBrowserUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::LauncherInternalsUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::LockScreenNetworkUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::LockScreenStartReauthUIConfig>());
