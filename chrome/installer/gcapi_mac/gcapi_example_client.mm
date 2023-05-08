df9396a4a6c3bd68a642b26cc1acc510b46829aa
Avi Drissman <avi@chromium.org>
2023-05-08 20:11:24 +0000

    Convert /chrome/installer/gcapi_mac to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This is a more raw rewrite into ARC-compliant Objective-C. Note that
    full ARC compliance showed up in Mac OS X 10.7. I bumped the minimum
    OS for GCAPI to 10.12 as I was getting link errors about other libs
    we require bottoming out at 10.12. I think that’s not unreasonable.
    
    Bug: 1280317
    Change-Id: Id8236424fad90a81ecd8918d3c59ecb792422c2f
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505494
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141027}

--- chrome/installer/gcapi_mac/gcapi_example_client.mm
+++ chrome/installer/gcapi_mac/gcapi_example_client.mm
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 void Usage() {
   fprintf(stderr,
           "usage: gcapi_example [options]\n"
@@ -65,7 +69,7 @@ int main(int argc, char* argv[]) {
   }
 
   if (reinstall) {
-    [[NSFileManager defaultManager]
+    [NSFileManager.defaultManager
         removeItemAtPath:@"/Applications/Google Chrome.app"
                    error:nil];
   }
@@ -73,8 +77,9 @@ int main(int argc, char* argv[]) {
   unsigned reasons;
   int can_install = GoogleChromeCompatibilityCheck(&reasons);
   NSLog(@"can_install: %d, reasons %x", can_install, reasons);
-  if (check_only)
+  if (check_only) {
     return 0;
+  }
 
   if (can_install && !source_path.empty()) {
     int install_result = InstallGoogleChrome(
@@ -83,6 +88,7 @@ int main(int argc, char* argv[]) {
     NSLog(@"install result: %d", install_result);
   }
 
-  if (launch)
+  if (launch) {
     LaunchGoogleChrome();
+  }
 }
