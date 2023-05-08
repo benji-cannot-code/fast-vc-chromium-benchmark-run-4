640523556b5fe456162b2355397286a42d5c1269
Brian Sheedy <bsheedy@chromium.org>
2023-05-08 20:04:00 +0000

    Reduce duplicate SkiaGoldProperties code
    Refactors the implementation of
    SkiaGoldProperties._GetGitOriginMainHeadSha1 so that most uses do not
    have to override the implementation anymore.
    
    Uses in third party repos will be updated after this is rolled into
    the relevant repo.
    
    Bug: 1443021
    Change-Id: Ib543332424eb79b8d54edd7cc123410e2afadd44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4510334
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Bot-Commit: Hao Wu <haowoo@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141021}

--- testing/scripts/skia_gold_infra/finch_skia_gold_utils.py
+++ testing/scripts/skia_gold_infra/finch_skia_gold_utils.py
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import logging
 import sys
-from .finch_skia_gold_properties import FinchSkiaGoldProperties
 from .finch_skia_gold_session_manager import FinchSkiaGoldSessionManager
+
+from skia_gold_common.skia_gold_properties import SkiaGoldProperties
 
 # This is the corpus used by skia gold to identify the data set.
 # We are not using the same corpus as the rest of the skia gold chromium tests.
@@ -16,7 +17,7 @@ CORPUS = 'finch-smoke-tests'
 
 class FinchSkiaGoldUtil:
   def __init__(self, temp_dir, args):
-    self._skia_gold_properties = FinchSkiaGoldProperties(args)
+    self._skia_gold_properties = SkiaGoldProperties(args)
     self._skia_gold_session_manager = FinchSkiaGoldSessionManager(
         temp_dir, self._skia_gold_properties)
     self._skia_gold_session = self._GetSkiaGoldSession()
