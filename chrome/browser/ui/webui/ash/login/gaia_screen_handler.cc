54940cf94db19caa43173866fa8216d1239b0a1b
Roland Bock <rbock@google.com>
2023-05-08 18:10:31 +0000

    Remove Realm from GaiaScreenHandler
    As ChromeAD is being removed, we can remove
    BrowserPolicyConnectorAsh::GetRealm().
    
    This CL is removing a call site in GaiaScreenHandler.
    
    Bug: b/281482019
    Test: Remaining tests
    Change-Id: I39ff1f2ae061229696706d42125b5fe07b190979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509340
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140959}

--- chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
@@ -230,12 +230,6 @@ std::string GetSSOProfile() {
   return connector->GetSSOProfile();
 }
 
-std::string GetRealm() {
-  policy::BrowserPolicyConnectorAsh* connector =
-      g_browser_process->platform_part()->browser_policy_connector_ash();
-  return connector->GetRealm();
-}
-
 std::string GetChromeType() {
   switch (chromeos::GetDeviceType()) {
     case chromeos::DeviceType::kChromebox:
@@ -435,11 +429,6 @@ void GaiaScreenHandler::LoadGaiaWithPartitionAndVersionAndConsent(
   const std::string app_locale = g_browser_process->GetApplicationLocale();
   if (!app_locale.empty())
     params.Set("hl", app_locale);
-
-  std::string realm(GetRealm());
-  if (!realm.empty()) {
-    params.Set("realm", realm);
-  }
 
   const std::string enterprise_enrollment_domain(
       GetEnterpriseEnrollmentDomain());
