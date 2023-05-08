64901f425ad8627b352d9d53244bbd4c8cda4c0d
Avi Drissman <avi@chromium.org>
2023-05-08 18:57:17 +0000

    Convert /chrome/services to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: Ib4275bff441e1d8e2914d1e87ea06eadfeba3066
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505042
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140992}

--- chrome/services/system_signals/mac/mac_system_signals_service.mm
+++ chrome/services/system_signals/mac/mac_system_signals_service.mm
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/device_signals/core/system_signals/mac/mac_platform_delegate.h"
 #include "components/device_signals/core/system_signals/platform_delegate.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace system_signals {
 
 MacSystemSignalsService::MacSystemSignalsService(
