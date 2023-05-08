ebd683b771a2f06b15609d648edf3cbaeae76e87
Avi Drissman <avi@chromium.org>
2023-05-08 23:15:42 +0000

    Convert /components/handoff to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Bug: 1280317
    Change-Id: I9510cfc8da2130e139587312ae5e9f99a2f895a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504023
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141113}

--- components/handoff/handoff_manager.mm
+++ components/handoff/handoff_manager.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/handoff/handoff_manager.h"
 
 #include "base/check.h"
-#include "base/mac/scoped_nsobject.h"
 #include "base/notreached.h"
 #include "base/strings/sys_string_conversions.h"
 #include "build/build_config.h"
@@ -21,10 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/mac_util.h"
 #endif
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @interface HandoffManager ()
 
 // The active user activity.
-@property(nonatomic, retain) NSUserActivity* userActivity;
+@property(nonatomic, strong) NSUserActivity* userActivity;
 
 // Whether the URL of the current tab should be exposed for Handoff.
 - (BOOL)shouldUseActiveURL;
@@ -36,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation HandoffManager {
   GURL _activeURL;
-  NSUserActivity* _userActivity;
   handoff::Origin _origin;
 }
 
@@ -64,11 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
-- (void)dealloc {
-  [_userActivity release];
-  [super dealloc];
-}
-
 - (void)updateActiveURL:(const GURL&)url {
   _activeURL = url;
   [self updateUserActivity];
@@ -94,20 +91,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   // No change to the user activity.
-  const GURL userActivityURL(net::GURLWithNSURL(self.userActivity.webpageURL));
-  if (userActivityURL == _activeURL)
+  const GURL userActivityURL = net::GURLWithNSURL(self.userActivity.webpageURL);
+  if (userActivityURL == _activeURL) {
     return;
+  }
 
   // Invalidate the old user activity and make a new one.
   [self.userActivity invalidate];
 
-  base::scoped_nsobject<NSUserActivity> userActivity([[NSUserActivity alloc]
-      initWithActivityType:NSUserActivityTypeBrowsingWeb]);
-  self.userActivity = userActivity;
+  self.userActivity = [[NSUserActivity alloc]
+      initWithActivityType:NSUserActivityTypeBrowsingWeb];
   self.userActivity.webpageURL = net::NSURLWithGURL(_activeURL);
   NSString* origin = handoff::StringFromOrigin(_origin);
   DCHECK(origin);
-  self.userActivity.userInfo = @{ handoff::kOriginKey : origin };
+  self.userActivity.userInfo = @{handoff::kOriginKey : origin};
   [self.userActivity becomeCurrent];
 }
 
--- components/handoff/handoff_utility.mm
+++ components/handoff/handoff_utility.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/handoff/handoff_utility.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace handoff {
 
 NSString* const kChromeHandoffActivityType = @"com.google.chrome.handoff";
