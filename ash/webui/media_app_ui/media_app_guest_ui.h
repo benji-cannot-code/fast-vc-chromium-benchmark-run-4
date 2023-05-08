f3ba02eb035282d5ffb99e6186c753772cfa3da3
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-05-08 05:51:16 +0000

    Revert "[Jellybean] Expose color change events to media app untrusted ui."
    This reverts commit 339362565984a89ab7dccb4e7a38bef5c719fa0a.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8781692582978898785
    
    Sample failed build: https://ci.chromium.org/b/8781692582978898785
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8781692582978898785&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4485923
    
    Original change's description:
    > [Jellybean] Expose color change events to media app untrusted ui.
    >
    > Implements the chromium side of go/cros-jellybean-google3-colors.
    >
    > Media app doesn't need to do anything further, the exposed functions
    > are automatically picked up by the google3 side installColors() call
    > and used where applicable.
    >
    > Bug: b/264329813
    > Change-Id: I00f0121d4ef78f6e21fce6951e71a6871762baeb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4485923
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Commit-Queue: Zain Afzal <zafzal@google.com>
    > Reviewed-by: Patti Lor <patricialor@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1140703}
    >
    
    Bug: b/264329813
    Change-Id: I816f686b70c839cf3953796e4c38cc7e90f52875
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4511385
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1140712}

--- ash/webui/media_app_ui/media_app_guest_ui.h
+++ ash/webui/media_app_ui/media_app_guest_ui.h
@@ -12,13 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_ui_data_source.h"
-#include "ui/webui/color_change_listener/color_change_handler.h"
-#include "ui/webui/resources/cr_components/color_change_listener/color_change_listener.mojom.h"
 #include "ui/webui/untrusted_web_ui_controller.h"
-
-namespace ui {
-class ColorChangeHandler;
-}
 
 namespace ash {
 
@@ -43,16 +37,7 @@ class MediaAppGuestUI : public ui::UntrustedWebUIController,
   // content::WebContentsObserver:
   void ReadyToCommitNavigation(content::NavigationHandle* handle) override;
 
-  // Binds a PageHandler to MediaAppGuestUI. This handler grabs a reference to
-  // the page and pushes a colorChangeEvent to the untrusted JS running there
-  // when the OS color scheme has changed.
-  void BindInterface(
-      mojo::PendingReceiver<color_change_listener::mojom::PageHandler>
-          receiver);
-
  private:
-  WEB_UI_CONTROLLER_TYPE_DECL();
-
   void StartFontDataRequest(
       const std::string& path,
       content::WebUIDataSource::GotDataCallback got_data_callback);
@@ -66,8 +51,6 @@ class MediaAppGuestUI : public ui::UntrustedWebUIController,
 
   // Whether ReadyToCommitNavigation has occurred for the main `app.html`.
   bool app_navigation_committed_ = false;
-
-  std::unique_ptr<ui::ColorChangeHandler> color_provider_handler_;
 
   base::WeakPtrFactory<MediaAppGuestUI> weak_factory_{this};
 };
