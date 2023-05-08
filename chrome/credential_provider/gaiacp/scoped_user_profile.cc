fe01642c4b37c16d56cc885c8cd3995dd06babe9
Andrew Rayskiy <greengrape@google.com>
2023-05-08 22:17:24 +0000

    Eliminate calls to FindStringKey() in chrome/credential_provider/gaiacp
    This CL replaces selected calls to deprecated FindStringKey(key) with
    GetDict().FindString(key) and refactors necessary call sites to deal
    with base::Value::Dict instead of base::Value.
    
    Bug: 1439196
    Change-Id: I4302094d80a7417dbcbad1883a7001ce84d59e31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506954
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1141082}

--- chrome/credential_provider/gaiacp/scoped_user_profile.cc
+++ chrome/credential_provider/gaiacp/scoped_user_profile.cc
@@ -51,7 +51,7 @@ constexpr int kProfilePictureSizes[] = {32, 40, 48, 96, 192, 240, 448};
 
 std::string GetEncryptedRefreshToken(
     base::win::ScopedHandle::Handle logon_handle,
-    const base::Value& properties) {
+    const base::Value::Dict& properties) {
   std::string refresh_token = GetDictStringUTF8(properties, kKeyRefreshToken);
   if (refresh_token.empty()) {
     LOGFN(ERROR) << "Refresh token is empty";
@@ -434,7 +434,7 @@ bool ScopedUserProfile::IsValid() {
 }
 
 HRESULT ScopedUserProfile::ExtractAssociationInformation(
-    const base::Value& properties,
+    const base::Value::Dict& properties,
     std::wstring* sid,
     std::wstring* id,
     std::wstring* email,
@@ -508,7 +508,8 @@ HRESULT ScopedUserProfile::RegisterAssociation(
   return S_OK;
 }
 
-HRESULT ScopedUserProfile::SaveAccountInfo(const base::Value& properties) {
+HRESULT ScopedUserProfile::SaveAccountInfo(
+    const base::Value::Dict& properties) {
   LOGFN(VERBOSE);
 
   std::wstring sid;
