6586292e3817002f6a4d342f2ecf5b8768050b26
Ming-Ying Chung <mych@chromium.org>
2023-05-08 01:53:34 +0000

    Revert "Do not exit fullscreen for a Borealis window"
    This reverts commit 7c3d96e24fb8a248f0ff0631a1bf4b1ae4e0cef2.
    
    Reason for revert: failing on linux-chromeos-chrome 
    
    Original change's description:
    > Do not exit fullscreen for a Borealis window
    >
    > - Add kNoExitFulscreenOnLock to avoid exiting fullscreen for a Borealis
    >   window.
    > - Changes UiLockController as it shows exit notifier again on unlock.
    >
    > Bug: b:261909101
    > Change-Id: Ib6b34e3c1382226720c26f0c51781840be8a8706
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4497034
    > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > Reviewed-by: Nic Hollingum <hollingum@google.com>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Chloe Pelling <cpelling@google.com>
    > Quick-Run: Yuki Awano <yawano@google.com>
    > Commit-Queue: Yuki Awano <yawano@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1140206}
    
    Bug: b:261909101, 1443269
    Change-Id: I30864280e5491a65f354c6361a06e70543fff41b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506724
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140670}

--- components/exo/ui_lock_controller_unittest.cc
+++ components/exo/ui_lock_controller_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/test/power_monitor_test.h"
 #include "base/test/scoped_feature_list.h"
-#include "chromeos/ash/components/login/auth/auth_events_recorder.h"
 #include "chromeos/dbus/power/fake_power_manager_client.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/dbus/power_manager/backlight.pb.h"
@@ -140,14 +139,6 @@ class UILockControllerTest : public test::ExoTestBase {
   void SetUp() override {
     test::ExoTestBase::SetUp();
     seat_ = std::make_unique<Seat>();
-
-    // Order of window activations and observer callbacks is not trivial, e.g.
-    // lock screen widget is active when `OnLockStateChanged(locked=false)`
-    // callback is called. It's better to test them with views.
-    // `AuthEventsRecorder` is required for `set_show_lock_screen_views=true`.
-    auth_events_recorder_ = ash::AuthEventsRecorder::CreateForTesting();
-    GetSessionControllerClient()->set_show_lock_screen_views(true);
-
     WMHelper::GetInstance()->RegisterAppPropertyResolver(
         std::make_unique<TestPropertyResolver>());
   }
@@ -193,7 +184,6 @@ class UILockControllerTest : public test::ExoTestBase {
 
   std::unique_ptr<Seat> seat_;
   base::test::ScopedFeatureList scoped_feature_list_;
-  std::unique_ptr<ash::AuthEventsRecorder> auth_events_recorder_;
 };
 
 TEST_F(UILockControllerTest, HoldingEscapeExitsFullscreen) {
@@ -588,9 +578,6 @@ TEST_F(UILockControllerTest, PointerLockNotificationReshownWhenScreenTurnedOn) {
 }
 
 TEST_F(UILockControllerTest, PointerLockNotificationReshownOnUnlock) {
-  // Lock screen takes focus and it disables pointer capture.
-  GetSessionControllerClient()->set_show_lock_screen_views(false);
-
   // Arrange: Set up a pointer capture notification, then let it expire.
   std::unique_ptr<ShellSurface> test_surface = BuildSurface(1024, 768);
   test_surface->SetApplicationId(kOverviewToExitAppId);
