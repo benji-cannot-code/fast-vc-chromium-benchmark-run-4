e62b6d1600b496c32dc86b7cdacb7c23b57ee608
Wei4 Wang <wei4.wang@intel.com>
2023-05-08 02:17:55 +0000

    [ComputePressure] Integrate with Origin Trial Framework
    This CL integrates the implementation with the Origin Trial Framework
    and adds origin trial-related tests. This CL also adds a kill switch
    for the Compute Pressure API origin trial. Some of the files being
    added were originally written in https://crrev.com/c/2903275.
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=1351314#c2 is
    also a good reference for this CL, as all the feature-related changes
    that have landed over the years are quite confusing to follow.
    
    Intent to Experiment: https://groups.google.com/a/chromium.org/g/blink-dev/c/QfJ4pngu3gc/m/oLL5nhBeAgAJ
    
    Bug: 1196419
    Change-Id: Ie10d6df3b551c5274d74822e89cffce5224d986d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483020
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1140678}

--- third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
+++ third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
@@ -539,6 +539,10 @@ bool OriginTrialContext::CanEnableTrialFromName(const StringView& trial_name) {
                network::features::kAttributionReportingCrossAppWeb);
   }
 
+  if (trial_name == "ComputePressure") {
+    return base::FeatureList::IsEnabled(features::kComputePressure);
+  }
+
   return true;
 }
 
