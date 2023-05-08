020d7cdf313a222c0dd3cc1167e4903ec11348fc
Andrew Rayskiy <greengrape@google.com>
2023-05-08 12:52:54 +0000

    Eliminate selected calls to FindStringKey()
    This CL replaces selected calls to deprecated FindStringKey(key) with
    GetDict().FindString(key) and refactors necessary call sites to deal
    with base::Value::Dict instead of base::Value.
    
    Bug: 1439196
    
    Change-Id: Ib3d8b9bfa63603ea0c7a30475658c2d79cc27702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4465949
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140810}

--- android_webview/browser/enterprise_authentication_app_link_policy_handler.cc
+++ android_webview/browser/enterprise_authentication_app_link_policy_handler.cc
@@ -44,7 +44,7 @@ bool EnterpriseAuthenticationAppLinkPolicyHandler::CheckPolicySettings(
 
   std::vector<std::string> invalid_policies;
   for (const auto& entry : value->GetList()) {
-    const std::string* url = entry.FindStringKey("url");
+    const std::string* url = entry.GetDict().FindString("url");
     if (!url) {
       invalid_policies.push_back(
           "Invalid policy: Required key 'url' does not exists");
@@ -71,7 +71,7 @@ void EnterpriseAuthenticationAppLinkPolicyHandler::ApplyPolicySettings(
 
   base::Value::List filtered_values;
   for (const auto& entry : value->GetList()) {
-    const std::string* url = entry.FindStringKey("url");
+    const std::string* url = entry.GetDict().FindString("url");
     if (ValidatePolicyEntry(url))
       filtered_values.Append(*url);
   }
--- android_webview/browser/metrics/aw_metrics_service_client_unittest.cc
+++ android_webview/browser/metrics/aw_metrics_service_client_unittest.cc
@@ -124,8 +124,8 @@ TEST_F(AwMetricsServiceClientTest, TestShouldRecordPackageName_WithCache) {
   base::TimeDelta expiry_time = base::Days(1);
   AppPackageNameLoggingRule expected_record(
       base::Version(kTestAllowlistVersion), base::Time::Now() + expiry_time);
-  prefs->Set(prefs::kMetricsAppPackageNameLoggingRule,
-             expected_record.ToDictionary());
+  prefs->SetDict(prefs::kMetricsAppPackageNameLoggingRule,
+                 expected_record.ToDictionary());
 
   absl::optional<AppPackageNameLoggingRule> cached_record =
       client->GetCachedAppPackageNameLoggingRule();
@@ -279,7 +279,8 @@ TEST_F(AwMetricsServiceClientTest, TestShouldRecordPackageName_SameAsCache) {
   base::TimeDelta expiry_time = base::Days(1);
   AppPackageNameLoggingRule record(base::Version(kTestAllowlistVersion),
                                    base::Time::Now() + expiry_time);
-  prefs->Set(prefs::kMetricsAppPackageNameLoggingRule, record.ToDictionary());
+  prefs->SetDict(prefs::kMetricsAppPackageNameLoggingRule,
+                 record.ToDictionary());
   client->SetAppPackageNameLoggingRule(record);
 
   EXPECT_TRUE(client->ShouldRecordPackageName());
