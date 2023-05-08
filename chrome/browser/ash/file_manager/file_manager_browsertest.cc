e1e6749c38912ba668532d3959e639b34d93fa70
Ben Reich <benreich@chromium.org>
2023-05-08 02:10:16 +0000

    [FilesBulkPinning] Introduce suffix icon to the cloud button
    The cloud button has a suffix icon (using cr-button nomenclature) to
    show the current state of the bulk pinning process. This introduces 3
    possible states:
    
      - Syncing: There are items to download and it is currently progressing
      - Offline: The device is offline and is paused syncing.
      - Error: There is not enough space on the disk to sync.
      - Done: The sync process has finished and is awaiting new files.
    
    Bug: b:280377758
    Test: browser_tests --gtest_filter=*toolbarCloudIcon*
    Change-Id: I4cca912c097fa5e7649119db93a1a2ed756500ae
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505901
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140675}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1340,6 +1340,8 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("toolbarCloudIconShouldNotShowWhenPrefDisabled")
             .EnableBulkPinning(),
         TestCase("toolbarCloudIconShouldShowOnStartupEvenIfSyncing")
+            .EnableBulkPinning(),
+        TestCase("toolbarCloudIconShouldShowWhenPausedState")
             .EnableBulkPinning()));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
