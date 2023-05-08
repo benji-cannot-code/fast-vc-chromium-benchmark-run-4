f0e5d495dc9538ce90141560a513a22661ff5208
Kevin Lubick <kjlubick@google.com>
2023-05-08 21:40:46 +0000

    Fix include of GrTypes.h
    In https://skia-review.googlesource.com/c/skia/+/687639, we
    remove GrTypes.h from SkSurface.h and this would otherwise
    break.
    
    
    Bug: skia:13983
    Change-Id: Iac65504dcf76bca88ad696d3dd3a84b1a9ebfb6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514119
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141068}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/thread_specific.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "third_party/skia/include/core/SkSurface.h"
+#include "third_party/skia/include/gpu/GrTypes.h"
 
 class GrDirectContext;
 
