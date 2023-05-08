9c0659f1ef5354575a1ee7486ae0fcaada5697c8
Nan Lin <linnan@chromium.org>
2023-05-08 16:19:58 +0000

    Fix typo in tools/origin_trials/generate_token.py comment
    Change-Id: If970f617c408bf6d8fa930fdab5461610391cd79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4512676
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140886}

--- tools/origin_trials/generate_token.py
+++ tools/origin_trials/generate_token.py
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 usage: generate_token.py [-h] [--key-file KEY_FILE]
                          [--expire-days EXPIRE_DAYS |
                           --expire-timestamp EXPIRE_TIMESTAMP]
-                         [--is_subdomain | --no-subdomain]
-                         [--is_third-party | --no-third-party]
+                         [--is-subdomain | --no-subdomain]
+                         [--is-third-party | --no-third-party]
                          [--usage-restriction USAGE_RESTRICTION]
                          --version=VERSION
                          origin trial_name
