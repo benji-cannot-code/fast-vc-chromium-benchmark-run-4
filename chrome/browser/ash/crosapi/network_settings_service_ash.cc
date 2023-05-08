422c81909d300e19cbdd7328b521e192498bb492
Andreea Costinas <acostinas@google.com>
2023-05-08 17:45:03 +0000

    NetworkSettingsServiceAsh: Replace GetPrimaryUserProfile() call
    This CL replaces the call to `GetPrimaryUserProfile()`, which can
    return an unexpected result if the primary profile is not
    available.
    
    Bug: 1411949
    Change-Id: I2303df9243a13d1ed0c9337b1ce300f83cb84166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507514
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140940}

--- chrome/browser/ash/crosapi/network_settings_service_ash.cc
+++ chrome/browser/ash/crosapi/network_settings_service_ash.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/components/network/network_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
@@ -36,11 +37,21 @@ const char kPrefExtensionCanDisabledKey[] = "can_be_disabled_key";
 PrefService* GetPrimaryLoggedInUserProfilePrefs() {
   // Check login state first.
   if (!user_manager::UserManager::IsInitialized() ||
-      !user_manager::UserManager::Get()->IsUserLoggedIn() ||
-      ProfileManager::GetPrimaryUserProfile() == nullptr) {
+      !user_manager::UserManager::Get()->IsUserLoggedIn()) {
     return nullptr;
   }
-  return ProfileManager::GetPrimaryUserProfile()->GetPrefs();
+
+  auto* primary_user = user_manager::UserManager::Get()->GetPrimaryUser();
+  if (!primary_user) {
+    return nullptr;
+  }
+
+  auto* profile = Profile::FromBrowserContext(
+      ash::BrowserContextHelper::Get()->GetBrowserContextByUser(primary_user));
+  if (!profile) {
+    return nullptr;
+  }
+  return profile->GetPrefs();
 }
 
 }  // namespace
