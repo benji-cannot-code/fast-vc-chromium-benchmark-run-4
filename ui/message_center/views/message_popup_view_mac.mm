12054722c0856585d1b846f1f6c5c4bb5706939b
Avi Drissman <avi@chromium.org>
2023-05-08 19:06:03 +0000

    Convert /ui/message_center to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: I9afe66be787ae34643b794f62889e8c6c2b8df03
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504800
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140994}

--- ui/message_center/views/message_popup_view_mac.mm
+++ ui/message_center/views/message_popup_view_mac.mm
@@ -9,12 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/widget/widget.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace message_center {
 
 float MessagePopupView::GetOpacity() const {
-  if (!IsWidgetValid())
+  if (!IsWidgetValid()) {
     return 0.f;
-  return [GetWidget()->GetNativeWindow().GetNativeNSWindow() alphaValue];
+  }
+  return GetWidget()->GetNativeWindow().GetNativeNSWindow().alphaValue;
 }
 
 }  // namespace message_center
