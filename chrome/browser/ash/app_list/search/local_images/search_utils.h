69c9f661bb3e34a82cae6fa627aeacd1b6a49c65
Dmitry Grebenyuk <dgrebenyuk@google.com>
2023-05-08 07:34:44 +0000

    Reduce annotations for OCR
    This CL reduces the number of annotations received from OCR.
    We filter stop words, the word size, and check that
    the first symbol is a letter.
    
    Test: "on DUT with enabled image search"
    Test: "passes unittests"
    Bug: b:279852430
    Change-Id: I6d9a787a0523fef9843372cd79701316a4ac73e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506038
    Reviewed-by: CJ Huang <chenjih@google.com>
    Commit-Queue: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140735}

---
+++ chrome/browser/ash/app_list/search/local_images/search_utils.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_APP_LIST_SEARCH_LOCAL_IMAGES_SEARCH_UTILS_H_
+#define CHROME_BROWSER_ASH_APP_LIST_SEARCH_LOCAL_IMAGES_SEARCH_UTILS_H_
+
+#include <string>
+
+namespace app_list {
+
+// Checks for the `word` in the current list of stop words.
+bool IsStopWord(const std::string& word);
+
+}  // namespace app_list
+
+#endif  // CHROME_BROWSER_ASH_APP_LIST_SEARCH_LOCAL_IMAGES_SEARCH_UTILS_H_
