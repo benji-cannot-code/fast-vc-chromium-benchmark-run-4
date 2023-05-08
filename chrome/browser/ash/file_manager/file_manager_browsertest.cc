e916049a61cdfcd5400fe924eefe8d4ff6d25bd1
Oleg Davydov <burunduk@chromium.org>
2023-05-08 11:29:55 +0000

    [file manager] Disable quick preview for CSE files
    Currently CSE files (encrypted ones from Google Drive) are not locally
    readable, so no possible preview could be available for them. For the
    Files App to not to try to show one, let's explicitly disable preview
    of CSE files.
    
    Bug: b:280621979
    Change-Id: If18a68cb65f8bcd507e5238c66fc0a6ab468fa20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507071
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140795}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1427,6 +1427,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("openQuickViewUmaForCheckSelectViaContextMenu"),
         TestCase("openQuickViewUmaViaSelectionMenu"),
         TestCase("openQuickViewUmaViaSelectionMenuKeyboard"),
+        TestCase("openQuickViewEncryptedFile"),
         TestCase("closeQuickView")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
