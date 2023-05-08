4b531a0a7b6d12582ddaee4ecf9a85567e1e9aae
Avi Drissman <avi@chromium.org>
2023-05-08 15:36:40 +0000

    Convert /ui/snapshot to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Bug: 1280317
    Change-Id: I0ce554a384877b0ce6f5335016ced57ef8fc7fcd
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4511634
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140864}

--- ui/snapshot/snapshot_mac_unittest.mm
+++ ui/snapshot/snapshot_mac_unittest.mm
@@ -10,13 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/mac/mac_util.h"
-#include "base/mac/scoped_nsobject.h"
 #include "base/test/task_environment.h"
 #include "testing/platform_test.h"
 #import "ui/base/test/cocoa_helper.h"
 #import "ui/base/test/windowed_nsnotification_observer.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/image/image.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace ui {
 namespace {
@@ -44,12 +47,12 @@ TEST_F(GrabWindowSnapshotTest, TestGrabWindowSnapshot) {
   const NSUInteger window_size = 400;
   NSRect frame = NSMakeRect(0, 0, window_size, window_size);
   NSWindow* window = test_window();
-  base::scoped_nsobject<WindowedNSNotificationObserver> waiter(
+  WindowedNSNotificationObserver* waiter =
       [[WindowedNSNotificationObserver alloc]
           initForNotification:NSWindowDidUpdateNotification
-                       object:window]);
+                       object:window];
   [window setFrame:frame display:false];
-  [window setBackgroundColor:NSColor.blueColor];
+  window.backgroundColor = NSColor.blueColor;
   [window makeKeyAndOrderFront:nil];
   [window display];
   EXPECT_TRUE([waiter wait]);
