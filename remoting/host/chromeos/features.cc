3c9bfa03e7b26377189519d8df54ff72d7830243
Ashutosh Singhal <macinashutosh@google.com>
2023-05-08 17:28:38 +0000

    Enable EnableFrameSinkDesktopCapturerInCrd by default.
    Enabling feature for 100% launch. This is required by go/finch-best-practices.
    
    Bug: b/280415717
    Change-Id: Ic35e251738c3b4109bc2d52303226f693b97fe5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514695
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140928}

--- remoting/host/chromeos/features.cc
+++ remoting/host/chromeos/features.cc
@@ -18,6 +18,6 @@ BASE_FEATURE(kForceCrdAdminRemoteAccess,
 
 BASE_FEATURE(kEnableFrameSinkDesktopCapturerInCrd,
              "EnableFrameSinkDesktopCapturerInCrd",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace remoting::features
