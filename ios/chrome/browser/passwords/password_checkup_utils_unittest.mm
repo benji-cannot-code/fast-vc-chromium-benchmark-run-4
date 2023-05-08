2845d75bb478fefd6bd79c63847da02f12b05ebc
Veronique Nguyen <veronguyen@google.com>
2023-05-08 18:01:33 +0000

    [iOS] Fix PasswordCheckupUtilsTest with PasswordsGrouping enabled
    Bug: 1345753
    Change-Id: Ia85d58d1ad2bce5c83ba999982f5063d65ab90e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509977
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140949}

--- ios/chrome/browser/passwords/password_checkup_utils_unittest.mm
+++ ios/chrome/browser/passwords/password_checkup_utils_unittest.mm
@@ -6,14 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/passwords/password_checkup_utils.h"
 
 #import "base/strings/string_piece.h"
+#import "base/test/bind.h"
 #import "base/test/scoped_feature_list.h"
 #import "components/keyed_service/core/service_access_type.h"
+#import "components/password_manager/core/browser/affiliation/fake_affiliation_service.h"
 #import "components/password_manager/core/browser/password_form.h"
 #import "components/password_manager/core/browser/password_manager_test_utils.h"
 #import "components/password_manager/core/browser/test_password_store.h"
 #import "components/password_manager/core/common/password_manager_features.h"
 #import "components/password_manager/core/common/password_manager_pref_names.h"
 #import "components/prefs/testing_pref_service.h"
+#import "ios/chrome/browser/passwords/ios_chrome_affiliation_service_factory.h"
 #import "ios/chrome/browser/passwords/ios_chrome_password_check_manager.h"
 #import "ios/chrome/browser/passwords/ios_chrome_password_check_manager_factory.h"
 #import "ios/chrome/browser/passwords/ios_chrome_password_store_factory.h"
@@ -89,12 +92,20 @@ void AddIssueToForm(PasswordForm* form,
 class PasswordCheckupUtilsTest : public PlatformTest {
  protected:
   PasswordCheckupUtilsTest() {
+    feature_list_.InitAndEnableFeature(
+        password_manager::features::kPasswordsGrouping);
     TestChromeBrowserState::Builder builder;
     builder.AddTestingFactory(
         IOSChromePasswordStoreFactory::GetInstance(),
         base::BindRepeating(
             &password_manager::BuildPasswordStore<web::BrowserState,
                                                   TestPasswordStore>));
+    builder.AddTestingFactory(
+        IOSChromeAffiliationServiceFactory::GetInstance(),
+        base::BindRepeating(base::BindLambdaForTesting([](web::BrowserState*) {
+          return std::unique_ptr<KeyedService>(
+              std::make_unique<password_manager::FakeAffiliationService>());
+        })));
     browser_state_ = builder.Build();
     store_ =
         base::WrapRefCounted(static_cast<password_manager::TestPasswordStore*>(
@@ -112,6 +123,7 @@ class PasswordCheckupUtilsTest : public PlatformTest {
   IOSChromePasswordCheckManager& manager() { return *manager_; }
 
  private:
+  base::test::ScopedFeatureList feature_list_;
   web::WebTaskEnvironment task_env_;
   std::unique_ptr<ChromeBrowserState> browser_state_;
   scoped_refptr<TestPasswordStore> store_;
