b10d4313bc5d53e3860e7ef9ce5bba54dbf1f26d
David Grogan <dgrogan@chromium.org>
2023-05-08 18:50:35 +0000

    [css-flex] Fix broken spine
    Flex was laying out a node in a MinMaxSizes pass after a layout had
    already been done. This requires a NGDisableSideEffectsScope to prevent
    LayoutObjects and fragments from getting out of sync.
    
    This patch adds the NGDisableSideEffectsScope.
    
    Fixed: 1440908
    Change-Id: I34dfcc93f99d100c3bec5d03c2b90944ea37a8f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4510548
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140980}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_disable_side_effects_scope.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_input_node.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_length_utils.h"
@@ -836,6 +837,10 @@ void NGFlexLayoutAlgorithm::ConstructAndAppendFlexItems(
       if (!layout_result) {
         NGConstraintSpace child_space = BuildSpaceForIntrinsicBlockSize(
             child, max_content_contribution, phase);
+        absl::optional<NGDisableSideEffectsScope> disable_side_effects;
+        if (phase != Phase::kLayout && !Node().GetLayoutBox()->NeedsLayout()) {
+          disable_side_effects.emplace();
+        }
         layout_result = child.Layout(child_space, /* break_token */ nullptr);
         DCHECK(layout_result);
       }
