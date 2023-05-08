6bf968d8422467fd147b9a3675eacc51e68519a5
Kam Kwankajornkiet <kamchonlathorn@google.com>
2023-05-08 09:42:21 +0000

    CCA: Expose getTimeLapseDuration function to Tast
    To let Tast side verify the duration of time-lapse video file, we expose
    getTimeLapseDuration function to return the expected duration given the
    recorded time. By using this function, we don't have to provide initial
    speed, max duration, and the criteria for updating speed.
    
    Bug: b:236800499
    Test: tast run $DUT camera.CCAUITimeLapse
    Change-Id: Ibf2b53b8eabee9c409b5ffd3769cdf11fcbddd33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506048
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140765}

--- ash/webui/camera_app_ui/resources/js/models/video_saver.ts
+++ ash/webui/camera_app_ui/resources/js/models/video_saver.ts
@@ -216,7 +216,7 @@ class TimeLapseFixedSpeedSaver {
 /**
  * Maximum duration for the time-lapse video in seconds.
  */
-const TIME_LAPSE_MAX_DURATION = 30;
+export const TIME_LAPSE_MAX_DURATION = 30;
 
 /**
  * Default number of fps in case it's not defined from the original video.
@@ -321,7 +321,8 @@ export class TimeLapseSaver {
   private async init(speed: number): Promise<void> {
     this.initialSpeed = speed;
     this.currSpeedSaver = await this.createSaver(speed);
-    this.nextSpeedSaver = await this.createSaver(this.getNextSpeed(speed));
+    this.nextSpeedSaver =
+        await this.createSaver(TimeLapseSaver.getNextSpeed(speed));
     this.speedCheckpoint = speed * TIME_LAPSE_MAX_DURATION * this.fps;
     setTimeout(() => this.manageSavers(), SAVER_MANAGER_TIMEOUT_MS);
   }
@@ -415,7 +416,8 @@ export class TimeLapseSaver {
     this.currSpeedSaver = this.nextSpeedSaver;
 
     const speed = this.currSpeedSaver.speed;
-    this.nextSpeedSaver = await this.createSaver(this.getNextSpeed(speed));
+    this.nextSpeedSaver =
+        await this.createSaver(TimeLapseSaver.getNextSpeed(speed));
     this.speedCheckpoint = speed * TIME_LAPSE_MAX_DURATION * this.fps;
 
     // Drops unused frames.
@@ -472,7 +474,7 @@ export class TimeLapseSaver {
   /**
    * Returns the time-lapse speed after the given speed.
    */
-  getNextSpeed(speed: number): number {
+  static getNextSpeed(speed: number): number {
     return speed * 2;
   }
 
