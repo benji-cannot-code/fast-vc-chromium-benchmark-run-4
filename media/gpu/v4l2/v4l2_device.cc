5c25c547fe827a59339f62a1d8f0efab4d99640b
Fritz Koenig <frkoenig@chromium.org>
2023-05-09 20:54:54 +0000

    media/gpu/v4l2: Remove amlogic support
    Amlogic support is no longer needed for cast.
    
    Bug: b:281675565
    
    Change-Id: Iffbd170183ed81f723fe8575015017ea2f8b4d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518000
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141624}

--- media/gpu/v4l2/v4l2_device.cc
+++ media/gpu/v4l2/v4l2_device.cc
@@ -40,10 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/generic_shared_memory_id.h"
 #include "ui/gfx/native_pixmap_handle.h"
 
-#if defined(AML_V4L2)
-#include "media/gpu/v4l2/aml_v4l2_device.h"
-#endif
-
 namespace media {
 
 namespace {
@@ -1532,12 +1528,6 @@ scoped_refptr<V4L2Device> V4L2Device::Create() {
   DVLOGF(3);
 
   scoped_refptr<V4L2Device> device;
-
-#if defined(AML_V4L2)
-  device = new AmlV4L2Device();
-  if (device->Initialize())
-    return device;
-#endif
 
   device = new GenericV4L2Device();
   if (device->Initialize())
