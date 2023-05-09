2724bc47c544ce63b09b6aaafd89c7f1f34f7fe5
Gauthier Ambard <gambard@chromium.org>
2023-05-09 11:06:36 +0000

    [iOS] Prepare to move URLs and Prefs
    Bug: 1319852
    Change-Id: If9605af0dd29513ceba52a4ae16228655b554d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508961
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141268}

---
+++ ios/chrome/browser/shared/model/url/chrome_url_constants.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_URL_CHROME_URL_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_SHARED_MODEL_URL_CHROME_URL_CONSTANTS_H_
+
+// Temporary include.
+#import "ios/chrome/browser/url/chrome_url_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_URL_CHROME_URL_CONSTANTS_H_
