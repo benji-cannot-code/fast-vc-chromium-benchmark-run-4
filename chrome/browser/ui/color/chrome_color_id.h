e3bee07d2f702412f21fea293f85d203f8969505
Tove Petersson <tovep@chromium.org>
2023-05-09 11:59:04 +0000

    Delay the activation of the share-this-tab dialog.
    The delay is controlled by the ShareThisTabDialog:activation_delay_ms
    parameter (default 1500ms). Until the dialog has been activated, a
    throbber is shown and the "Allow" button is disabled.
    
    Bug: 1428878
    Change-Id: I4ae06ac913ae6e305c103f94f9f9e47030448645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506167
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141296}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -331,6 +331,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   /* Screenshot captured bubble colors. */ \
   E_CPONLY(kColorScreenshotCapturedImageBackground) \
   E_CPONLY(kColorScreenshotCapturedImageBorder) \
+  /* Share-this-tab dialg colors. */ \
+  E_CPONLY(kColorShareThisTabSourceViewBorder) \
   /* Side panel colors. */ \
   E_CPONLY(kColorSidePanelBackground) \
   E_CPONLY(kColorSidePanelBadgeBackground) \
---
+++ chrome/browser/ui/views/desktop_capture/share_this_tab_source_view.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/desktop_capture/share_this_tab_source_view.h"
+
+#include "ui/views/layout/box_layout.h"
+
+namespace {
+
+constexpr gfx::Size kPreviewSize(320, 240);
+constexpr int kPadding = 8;
+
+}  // namespace
+
+ShareThisTabSourceView::ShareThisTabSourceView() {
+  View* throbber_container = AddChildView(std::make_unique<views::View>());
+  views::BoxLayout* throbber_layout =
+      throbber_container->SetLayoutManager(std::make_unique<views::BoxLayout>(
+          views::BoxLayout::Orientation::kHorizontal));
+  throbber_layout->set_main_axis_alignment(
+      views::BoxLayout::MainAxisAlignment::kCenter);
+  throbber_layout->set_cross_axis_alignment(
+      views::BoxLayout::CrossAxisAlignment::kCenter);
+  // TODO(crbug.com/1428878): Use distances from LayoutProvider
+  throbber_container->SetBoundsRect(
+      gfx::Rect(gfx::Point(kPadding, kPadding), kPreviewSize));
+  throbber_container->SetCanProcessEventsWithinSubtree(false);
+  throbber_ =
+      throbber_container->AddChildView(std::make_unique<views::Throbber>());
+  throbber_->Start();
+}
+
+ShareThisTabSourceView::~ShareThisTabSourceView() = default;
+
+void ShareThisTabSourceView::Activate() {
+  throbber_->Stop();
+  throbber_->SetVisible(false);
+}
+
+gfx::Size ShareThisTabSourceView::CalculatePreferredSize() const {
+  // TODO(crbug.com/1428878): Use distances from LayoutProvider
+  return kPreviewSize + gfx::Size(2 * kPadding, 2 * kPadding);
+}
