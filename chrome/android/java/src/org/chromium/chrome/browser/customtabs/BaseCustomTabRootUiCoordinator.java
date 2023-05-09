c5ac2e1cf9f9704149a8b1f56ad65eab268ecf4f
Zach Katz <katzz@google.com>
2023-05-09 19:57:49 +0000

    [PCCT-SideSheet] SideSheet/FullSize integration tests
    Bug: 1413913
    Change-Id: I6cf1450a1fdf43bc25402a97dac96d4dbd9b6ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4484837
    Commit-Queue: Zach Katz <katzz@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1141581}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
@@ -12,6 +12,7 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.chromium.base.Callback;
@@ -392,5 +393,10 @@ public class BaseCustomTabRootUiCoordinator extends RootUiCoordinator {
                     getToolbarManager().getMenuButtonView(),
                     mAppMenuCoordinator.getAppMenuHandler(), getPrimaryDisplaySizeInInches());
         }
+    }
+
+    @VisibleForTesting
+    CustomTabHeightStrategy getCustomTabSizeStrategyForTesting() {
+        return mCustomTabHeightStrategy;
     }
 }
