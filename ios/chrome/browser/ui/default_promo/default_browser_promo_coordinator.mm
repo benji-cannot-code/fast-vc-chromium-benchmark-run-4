f41a9eaf0671e0c643258bf9fadd1f87ef270999
Robbie Gibson <rkgibson@google.com>
2023-05-09 22:17:20 +0000

    [iOS] Properly clean up default browser promo coordinator after dismiss
    The root cause here is that none of these coordianators are correctly
    stopped when dismissed via user swipe.
    
    Bug: 1443982
    Change-Id: I58135a828d907786c2e9a620ccc2306790a16596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517765
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1141691}

--- ios/chrome/browser/ui/default_promo/default_browser_promo_coordinator.mm
+++ ios/chrome/browser/ui/default_promo/default_browser_promo_coordinator.mm
@@ -78,6 +78,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // This ensures that a modal swipe dismiss will also be logged.
   LogUserInteractionWithFullscreenPromo();
   [self recordDefaultBrowserPromoShown];
+
+  [self.handler hidePromo];
 }
 
 #pragma mark - ConfirmationAlertActionHandler
