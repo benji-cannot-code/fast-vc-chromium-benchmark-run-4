5536e09563e8172216f2ffa8926f02fd28e1293b
Cici Ruan <cuicuiruan@google.com>
2023-05-09 06:19:28 +0000

    arc/gio: Implement EditingList of non-zero state
    This CL adds list items when there are input mappings. The action
    content is hardcoded to users now. Next CL will make the content
    dynamic.
    
    Bug: b/270969479
    Test: Manual test to see if the view layout is correct.
    Change-Id: Ie5b2e77f19c6060869fe2a866c0e1d3c2413cce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4510227
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141207}

--- chrome/browser/ash/arc/input_overlay/actions/action_tap.h
+++ chrome/browser/ash/arc/input_overlay/actions/action_tap.h
@@ -32,6 +32,7 @@ class ActionTap : public Action {
       DisplayOverlayController* display_overlay_controller) override;
   void UnbindInput(const InputElement& input_element) override;
   std::unique_ptr<ActionProto> ConvertToProtoIfCustomized() const override;
+  ActionType GetType() override;
 
  private:
   class ActionTapView;
---
+++ chrome/browser/ash/arc/input_overlay/ui/ui_utils.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_UI_UTILS_H_
+#define CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_UI_UTILS_H_
+
+#include <memory>
+#include <string>
+
+namespace views {
+class View;
+}
+
+namespace arc::input_overlay {
+
+class Action;
+
+// Create name tag with title and sub-title as:
+// -----------
+// |Title    |
+// |Sub-title|
+// -----------
+std::unique_ptr<views::View> CreateNameTag(const std::u16string& title,
+                                           const std::u16string& sub_title);
+
+// Create key layout view for ActionMove.
+// -------------
+// |   | w |   |
+// |-----------|
+// | a | s | d |
+// -------------
+std::unique_ptr<views::View> CreateActionMoveEditForKeyboard(Action* action);
+}  // namespace arc::input_overlay
+
+#endif  // CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_UI_UTILS_H_
