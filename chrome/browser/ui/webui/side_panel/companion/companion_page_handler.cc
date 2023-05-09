b94a5b9339ff127da989d24d33a624ddc78cb94a
Shakti Sahu <shaktisahu@chromium.org>
2023-05-09 21:33:17 +0000

    Reland "[CSC] Added metric for position of the clicked UI element"
    This is a reland of commit 5a0dee00a73c1c40ad34a6631896f8057b38da23
    
    Reland reason: The original CL had a compilation error because of a
    simulataneous change in another CL. Resolved the compilation issue.
    
    Original change's description:
    > [CSC] Added metric for position of the clicked UI element
    >
    > This CL adds the position of the clicked UI element in its parent list.
    >
    > Bug: b/272954072
    > Change-Id: Ib3d3f450f055255f7f6fade08b8a29d61e513951
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4501075
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1141571}
    
    Bug: b/272954072
    Change-Id: I04ecb9bd24c59d0e5927ca02592a8637faf12825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518752
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141660}

--- chrome/browser/ui/webui/side_panel/companion/companion_page_handler.cc
+++ chrome/browser/ui/webui/side_panel/companion/companion_page_handler.cc
@@ -148,7 +148,7 @@ void CompanionPageHandler::OnRegionSearchClicked() {
   CHECK(helper);
   helper->StartRegionSearch(web_contents(), /*use_fullscreen_capture=*/false);
   metrics_logger_->RecordUiSurfaceClicked(
-      side_panel::mojom::UiSurface::kRegionSearch);
+      side_panel::mojom::UiSurface::kRegionSearch, kInvalidPosition);
 }
 
 void CompanionPageHandler::OnExpsOptInStatusAvailable(bool is_exps_opted_in) {
@@ -174,8 +174,9 @@ void CompanionPageHandler::RecordUiSurfaceShown(
 }
 
 void CompanionPageHandler::RecordUiSurfaceClicked(
-    side_panel::mojom::UiSurface ui_surface) {
-  metrics_logger_->RecordUiSurfaceClicked(ui_surface);
+    side_panel::mojom::UiSurface ui_surface,
+    int32_t click_position) {
+  metrics_logger_->RecordUiSurfaceClicked(ui_surface, click_position);
 }
 
 void CompanionPageHandler::OnCqCandidatesAvailable(
