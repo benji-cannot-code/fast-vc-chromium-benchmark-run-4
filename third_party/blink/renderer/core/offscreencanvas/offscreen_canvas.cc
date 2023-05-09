0ac10dfce773f870fef6f0e57fa0505c4c1cfd9b
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-05-09 14:13:07 +0000

    Remove dead code in offscreen_canvas.cc/h.
    `needs_matrix_clip_restore_` is never set to `true`.
    
    Change-Id: I3b14a5589d08076f796db16358a49381cec80abb
    Bug: 1237275, 1396372, 1403977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514531
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141351}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -491,11 +491,6 @@ CanvasResourceProvider* OffscreenCanvas::GetOrCreateResourceProvider() {
     base::UmaHistogramEnumeration("Blink.Canvas.ResourceProviderType",
                                   ResourceProvider()->GetType());
     DidDraw();
-
-    if (needs_matrix_clip_restore_) {
-      needs_matrix_clip_restore_ = false;
-      context_->RestoreCanvasMatrixClipStack(ResourceProvider()->Canvas());
-    }
   }
   return ResourceProvider();
 }
