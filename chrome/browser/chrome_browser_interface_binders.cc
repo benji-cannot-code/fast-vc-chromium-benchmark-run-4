23d452e7ba270c4aa82d3141175335a8bf0ce366
Zain Afzal <zafzal@google.com>
2023-05-09 02:55:16 +0000

    Reland "[Jellybean] Expose color change events to media app untrusted ui."
    This reverts commit 8d1c2199edc54e419a2e1027e9264af9baaae847.
    
    Reason for revert: Fixed build error
    
    Original change's description:
    > Revert "[Jellybean] Expose color change events to media app untrusted ui."
    >
    > This reverts commit 339362565984a89ab7dccb4e7a38bef5c719fa0a.
    >
    > Reason for revert:
    > LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8781692582978898785
    >
    > Sample failed build: https://ci.chromium.org/b/8781692582978898785
    >
    > If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8781692582978898785&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4485923
    >
    > Original change's description:
    > > [Jellybean] Expose color change events to media app untrusted ui.
    > >
    > > Implements the chromium side of go/cros-jellybean-google3-colors.
    > >
    > > Media app doesn't need to do anything further, the exposed functions
    > > are automatically picked up by the google3 side installColors() call
    > > and used where applicable.
    > >
    > > Bug: b/264329813
    > > Change-Id: I00f0121d4ef78f6e21fce6951e71a6871762baeb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4485923
    > > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > > Commit-Queue: Zain Afzal <zafzal@google.com>
    > > Reviewed-by: Patti Lor <patricialor@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1140703}
    > >
    >
    > Bug: b/264329813
    > Change-Id: I816f686b70c839cf3953796e4c38cc7e90f52875
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4511385
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1140712}
    
    Bug: b/264329813
    Change-Id: I60122bd1857582f3f4404bae426a12acd35f527c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505781
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1141155}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -245,6 +245,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/help_app_ui/help_app_ui.h"
 #include "ash/webui/help_app_ui/help_app_ui.mojom.h"
 #include "ash/webui/help_app_ui/search/search.mojom.h"
+#include "ash/webui/media_app_ui/media_app_guest_ui.h"
 #include "ash/webui/media_app_ui/media_app_ui.h"
 #include "ash/webui/media_app_ui/media_app_ui.mojom.h"
 #include "ash/webui/multidevice_debug/proximity_auth_ui.h"
@@ -1536,6 +1537,9 @@ void PopulateChromeWebUIFrameInterfaceBrokers(
       .Add<ash::projector::mojom::UntrustedProjectorPageHandlerFactory>();
 
   registry.ForWebUI<ash::feedback::OsFeedbackUntrustedUI>()
+      .Add<color_change_listener::mojom::PageHandler>();
+
+  registry.ForWebUI<ash::MediaAppGuestUI>()
       .Add<color_change_listener::mojom::PageHandler>();
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
