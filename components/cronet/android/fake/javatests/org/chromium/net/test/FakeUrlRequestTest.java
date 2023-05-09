5fc55c13e1bbe4729a9f679c2cb65e01a2aa213a
Stefano Duo <stefanoduo@google.com>
2023-05-09 17:16:15 +0000

    Reorder parameters in assertEqual calls
    The first parameter is supposed to be the expected value.
    
    Bug: 994722
    Change-Id: I67042559b9b5ace461f3a64ce7847a7aa00d36c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507137
    Reviewed-by: Dan Stahr <danstahr@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1141489}

--- components/cronet/android/fake/javatests/org/chromium/net/test/FakeUrlRequestTest.java
+++ components/cronet/android/fake/javatests/org/chromium/net/test/FakeUrlRequestTest.java
@@ -476,7 +476,7 @@ public class FakeUrlRequestTest {
 
         request.start();
         callback.waitForNextStep();
-        assertEquals(callback.mResponseStep, ResponseStep.ON_RECEIVED_REDIRECT);
+        assertEquals(ResponseStep.ON_RECEIVED_REDIRECT, callback.mResponseStep);
         checkStatus(request, Status.IDLE);
         callback.setAutoAdvance(true);
         request.followRedirect();
@@ -507,7 +507,7 @@ public class FakeUrlRequestTest {
         request.start();
         callback.blockForDone();
 
-        assertEquals(callback.mResponseStep, ResponseStep.ON_SUCCEEDED);
+        assertEquals(ResponseStep.ON_SUCCEEDED, callback.mResponseStep);
         assertTrue(request.isDone());
     }
 
@@ -687,7 +687,7 @@ public class FakeUrlRequestTest {
         request.start();
         callback.blockForDone();
 
-        assertEquals(callback.mResponseStep, ResponseStep.ON_FAILED);
+        assertEquals(ResponseStep.ON_FAILED, callback.mResponseStep);
         // Checks that the exception from {@link DirectPreventingExecutor} was successfully returned
         // to the callabck in the onFailed method.
         assertTrue(callback.mError.getCause() instanceof InlineExecutionProhibitedException);
@@ -808,7 +808,7 @@ public class FakeUrlRequestTest {
             assertEquals("Invalid state transition - expected 4 but was 3", e.getMessage());
         }
         callback.waitForNextStep();
-        assertEquals(callback.mResponseStep, ResponseStep.ON_RECEIVED_REDIRECT);
+        assertEquals(ResponseStep.ON_RECEIVED_REDIRECT, callback.mResponseStep);
         callback.setAutoAdvance(true);
         request.followRedirect();
         callback.blockForDone();
