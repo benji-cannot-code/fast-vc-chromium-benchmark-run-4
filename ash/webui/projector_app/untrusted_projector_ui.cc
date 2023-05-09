a7d44f33f4f9ec3197188a819d48e713616e54eb
Xiqi Ruan <xiqiruan@chromium.org>
2023-05-09 21:39:25 +0000

    projector: Register auto grandted common permission
    Register it for chrome-untrusted://projector to fix the local
    storage is not accessible when users disable 3rd party cookies.
    Also add JS and image permisions.
    
    Bug: b/280071983
    Change-Id: I9c9a4ce90d628dd034196682a8e34b5954308afb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4510336
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141663}

--- ash/webui/projector_app/untrusted_projector_ui.cc
+++ ash/webui/projector_app/untrusted_projector_ui.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/common/url_constants.h"
+#include "ui/webui/webui_allowlist.h"
 #include "url/gurl.h"
 
 namespace ash {
@@ -27,9 +28,9 @@ namespace {
 
 void CreateAndAddProjectorHTMLSource(content::WebUI* web_ui,
                                      UntrustedProjectorUIDelegate* delegate) {
+  auto* browser_context = web_ui->GetWebContents()->GetBrowserContext();
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
-      web_ui->GetWebContents()->GetBrowserContext(),
-      kChromeUIUntrustedProjectorUrl);
+      browser_context, kChromeUIUntrustedProjectorUrl);
 
   source->AddResourcePaths(
       base::make_span(kAshProjectorAppUntrustedResources,
@@ -83,6 +84,16 @@ void CreateAndAddProjectorHTMLSource(content::WebUI* web_ui,
 
   delegate->PopulateLoadTimeData(source);
   source->UseStringsJs();
+
+  auto* webui_allowlist = WebUIAllowlist::GetOrCreate(browser_context);
+  const url::Origin untrusted_origin =
+      url::Origin::Create(GURL(kChromeUIUntrustedProjectorUrl));
+  webui_allowlist->RegisterAutoGrantedPermission(untrusted_origin,
+                                                 ContentSettingsType::COOKIES);
+  webui_allowlist->RegisterAutoGrantedPermission(
+      untrusted_origin, ContentSettingsType::JAVASCRIPT);
+  webui_allowlist->RegisterAutoGrantedPermission(untrusted_origin,
+                                                 ContentSettingsType::IMAGES);
 }
 
 }  // namespace
