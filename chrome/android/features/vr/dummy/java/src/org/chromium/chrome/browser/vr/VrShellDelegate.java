1e113bc3fd07561d138a78c220b70e0bf5a49a8d
Viatcheslav Ostapenko <sl.ostapenko@samsung.com>
2023-05-09 21:38:28 +0000

    Replace VrShell sources with stub in vr module.
    Still were showing up in tests.
    
    Bug: none
    Change-Id: I13a82b93e7fd83093dc4c15353e5846e18f6c17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4508307
    Commit-Queue: Viatcheslav Ostapenko <sl.ostapenko@samsung.com>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141662}

---
+++ chrome/android/features/vr/dummy/java/src/org/chromium/chrome/browser/vr/VrShellDelegate.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.vr;
+
+import org.chromium.base.annotations.JNINamespace;
+
+/**
+ * Stub implementation of the VR Shell.
+ * Used when enable_gvr_services = false
+ */
+@JNINamespace("vr")
+public class VrShellDelegate {
+     /**
+     * Called when the native library is first available.
+     */
+    public static void onNativeLibraryAvailable() {
+    }
+}
