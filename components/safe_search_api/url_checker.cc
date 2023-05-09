1d068b82c3e91a9de48c508f88b7ef3afa94e815
Tomasz Jurkiewicz <tju@google.com>
2023-05-09 11:05:46 +0000

    Do not cache uncertain (aka unknown) Url classifications when the experiment is enabled.
    When unknown classifications, the users might get wrong classification even if the problem was caused by some auth or infrastructure errors. This might be elevating the UNKNOWN error rate (as investigated).
    
    Register the experiment.
    
    Bug: b:272209467
    Change-Id: Ifd3cdeedf613ce0d07ad028a61ea272bd77b9650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503063
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1141267}

--- components/safe_search_api/url_checker.cc
+++ components/safe_search_api/url_checker.cc
@@ -5,24 +5,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/safe_search_api/url_checker.h"
 
-#include <string>
 #include <utility>
 #include <vector>
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/json/json_reader.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/strings/string_piece.h"
-#include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "base/values.h"
 
 namespace safe_search_api {
 
+BASE_FEATURE(kCacheOnlyCertainUrlClassifications,
+             "CacheOnlyCertainUrlClassifications",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 namespace {
+bool ShouldCacheUrlClassification(bool is_uncertain) {
+  if (!is_uncertain) {
+    return true;
+  }
+
+  return !base::FeatureList::IsEnabled(kCacheOnlyCertainUrlClassifications);
+}
 
 const size_t kDefaultCacheSize = 1000;
 const size_t kDefaultCacheTimeoutSeconds = 3600;
@@ -110,10 +117,13 @@ void URLChecker::OnAsyncCheckComplete(CheckList::iterator it,
   std::vector<CheckCallback> callbacks = std::move(it->get()->callbacks);
   checks_in_progress_.erase(it);
 
-  cache_.Put(url, CheckResult(classification, uncertain));
+  if (ShouldCacheUrlClassification(uncertain)) {
+    cache_.Put(url, CheckResult(classification, uncertain));
+  }
 
-  for (size_t i = 0; i < callbacks.size(); i++)
-    std::move(callbacks[i]).Run(url, classification, uncertain);
+  for (CheckCallback& callback : callbacks) {
+    std::move(callback).Run(url, classification, uncertain);
+  }
 }
 
 }  // namespace safe_search_api
