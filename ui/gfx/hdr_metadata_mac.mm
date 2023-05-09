d68406d6a1964459458cf9397ff92a9c182cb553
Avi Drissman <avi@chromium.org>
2023-05-09 15:39:39 +0000

    Convert /ui/gfx:color_space to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: Ia9ff50c46d1219aa94c38017feebbc033c812cdb
    Include-Ci-Only-Tests: true
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517222
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141401}

--- ui/gfx/hdr_metadata_mac.mm
+++ ui/gfx/hdr_metadata_mac.mm
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <simd/simd.h>
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace gfx {
 
 base::ScopedCFTypeRef<CFDataRef> GenerateContentLightLevelInfo(
