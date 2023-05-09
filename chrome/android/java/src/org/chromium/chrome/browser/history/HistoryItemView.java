bdb080825369517913a1b5b43f8a6b85fca9947e
Sky Malice <skym@chromium.org>
2023-05-09 22:10:19 +0000

    Switch FaviconHelper to getColor from Context.
    Bug: 1302803
    Change-Id: I410cc3ff4489ecd217055020c7c0cfbee7ab0417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519382
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141686}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryItemView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryItemView.java
@@ -102,8 +102,8 @@ public class HistoryItemView extends SelectableItemView<HistoryItem> {
             setStartIconDrawable(mBlockedVisitDrawable);
             mTitleView.setTextColor(getContext().getColor(R.color.default_red));
         } else {
-            setStartIconDrawable(mFaviconHelper.getDefaultFaviconDrawable(
-                    getContext().getResources(), item.getUrl(), true));
+            setStartIconDrawable(
+                    mFaviconHelper.getDefaultFaviconDrawable(getContext(), item.getUrl(), true));
             requestIcon();
 
             mTitleView.setTextColor(AppCompatResources.getColorStateList(
--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
@@ -787,8 +787,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
         Drawable image;
         if (url == null) {
             // URL is null for print jobs, for example.
-            image = mDefaultFaviconHelper.getDefaultFaviconDrawable(
-                    mActivity.getResources(), url, true);
+            image = mDefaultFaviconHelper.getDefaultFaviconDrawable(mActivity, url, true);
         } else {
             image = mFaviconCaches.get(locality).getFaviconImage(url);
             if (image == null) {
@@ -796,9 +795,9 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                     @Override
                     public void onFaviconAvailable(Bitmap bitmap, GURL iconUrl) {
                         if (this != viewHolder.imageCallback) return;
-                        Drawable faviconDrawable = FaviconUtils.getIconDrawableWithFilter(bitmap,
-                                url, mIconGenerator, mDefaultFaviconHelper,
-                                mActivity.getResources(), mFaviconSize);
+                        Drawable faviconDrawable =
+                                FaviconUtils.getIconDrawableWithFilter(bitmap, url, mIconGenerator,
+                                        mDefaultFaviconHelper, mActivity, mFaviconSize);
                         mFaviconCaches.get(locality).putFaviconImage(url, faviconDrawable);
                         viewHolder.imageView.setImageDrawable(faviconDrawable);
                     }
@@ -814,8 +813,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                         break;
                 }
 
-                image = mDefaultFaviconHelper.getDefaultFaviconDrawable(
-                        mActivity.getResources(), url, true);
+                image = mDefaultFaviconHelper.getDefaultFaviconDrawable(mActivity, url, true);
             }
         }
         viewHolder.imageView.setImageDrawable(image);
