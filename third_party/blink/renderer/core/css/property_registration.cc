38439506329b2eba5836af0694531b1657876592
Rune Lillesveen <futhark@chromium.org>
2023-05-09 11:26:52 +0000

    Allow 'default' as an initial-value value for registered properties
    'default' is reserved and not allowed as <custom-ident>, but is
    currently computationally independent, so it should be allowed as
    initial-value per spec.
    
    Bug: 1344170
    Change-Id: Ic8563e225f0e7378f2123b906b52e743715bf1c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507079
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141278}

--- third_party/blink/renderer/core/css/property_registration.cc
+++ third_party/blink/renderer/core/css/property_registration.cc
@@ -104,7 +104,7 @@ static absl::optional<CSSSyntaxDefinition> ConvertSyntax(
   return CSSSyntaxStringParser(To<CSSStringValue>(value).Value()).Parse();
 }
 
-static bool ConvertInherts(const CSSValue& value) {
+static bool ConvertInherits(const CSSValue& value) {
   CSSValueID inherits_id = To<CSSIdentifierValue>(value).GetValueID();
   DCHECK(inherits_id == CSSValueID::kTrue || inherits_id == CSSValueID::kFalse);
   return inherits_id == CSSValueID::kTrue;
@@ -137,7 +137,7 @@ PropertyRegistration* PropertyRegistration::MaybeCreateForDeclaredProperty(
   if (!inherits_value) {
     return nullptr;
   }
-  bool inherits = ConvertInherts(*inherits_value);
+  bool inherits = ConvertInherits(*inherits_value);
 
   // https://drafts.css-houdini.org/css-properties-values-api-1/#initial-value-descriptor
   const CSSValue* initial_value = rule.GetInitialValue();
