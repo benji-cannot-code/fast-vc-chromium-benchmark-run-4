cb88a9a8ff034ef3beebe31b862dfd9feeb0eaa4
Ben Reich <benreich@chromium.org>
2023-05-09 06:24:21 +0000

    [FilesBulkPinning] Expose calculateBulkPinRequiredSpace
    Exposes the method required to kick off calculating the required space
    to pin all the users My drive items. This returns nothing as the results
    are expected to be received via the onBulkPinProgress method.
    
    Bug: b:280892739
    Change-Id: Id8ca7ae8c33333177a148b94a31968895a362392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505997
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141209}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -1749,6 +1749,12 @@ chrome.fileManagerPrivate.parseTrashInfoFiles = function(entries, callback) {};
 chrome.fileManagerPrivate.getBulkPinProgress = function(callback) {};
 
 /**
+ * Starts calculating the space required to pin all the items in a users My
+ * drive.
+ */
+chrome.fileManagerPrivate.calculateBulkPinRequiredSpace = function() {};
+
+/**
  * @type {!ChromeEvent}
  */
 chrome.fileManagerPrivate.onMountCompleted;
