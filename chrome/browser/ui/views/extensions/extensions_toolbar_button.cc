eb5566e28ff0a0df4d0132f0080551e5e76f2140
Emilia Paz <emiliapaz@chromium.org>
2023-05-09 16:59:42 +0000

    [Extensions c2s] Update extensions icon when extensions have access
    Display the extensions on icon in the toolbar when at least one
    extension has access to the current site.
    
    Screencast:
    https://drive.google.com/file/d/1NQuTsD98k3F84e3lTsM5GXbYCRve_55W/view?usp=sharing
    
    Bug: 1239772
    Change-Id: Id79647a1783d06d3a594848f0f459b5e70d05580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492170
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141475}

--- chrome/browser/ui/views/extensions/extensions_toolbar_button.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_button.cc
@@ -35,6 +35,8 @@ const gfx::VectorIcon& GetIcon(ExtensionsToolbarButton::State state) {
                  : vector_icons::kExtensionIcon;
     case ExtensionsToolbarButton::State::kAllExtensionsBlocked:
       return vector_icons::kExtensionOffIcon;
+    case ExtensionsToolbarButton::State::kAnyExtensionHasAccess:
+      return vector_icons::kExtensionOnIcon;
   }
 }
 
