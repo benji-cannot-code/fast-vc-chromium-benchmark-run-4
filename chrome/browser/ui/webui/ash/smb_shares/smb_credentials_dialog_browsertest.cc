0077dbd732e26858bf613312fe1a1407852c9d31
Chris Fredrickson <cfredric@chromium.org>
2023-05-10 06:07:40 +0000

    Replace ExecuteScript with ExecJs
    This CL was uploaded by git cl split.
    
    R=alexbn@chromium.org
    
    Bug: 1157718
    Change-Id: Ib1f30057a280c9b8c1e656df8557d00899adc2e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519148
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141879}

--- chrome/browser/ui/webui/ash/smb_shares/smb_credentials_dialog_browsertest.cc
+++ chrome/browser/ui/webui/ash/smb_shares/smb_credentials_dialog_browsertest.cc
@@ -36,8 +36,7 @@ IN_PROC_BROWSER_TEST_F(SmbCredentialsDialogTest, CloseDialog) {
   ASSERT_TRUE(content::WaitForLoadStop(dialog_contents));
   EXPECT_EQ(dialog_contents->GetLastCommittedURL().host(),
             chrome::kChromeUISmbCredentialsHost);
-  ASSERT_TRUE(
-      content::ExecuteScript(dialog_contents, "chrome.send('dialogClose');"));
+  ASSERT_TRUE(content::ExecJs(dialog_contents, "chrome.send('dialogClose');"));
 
   run_loop.Run();
 }
@@ -65,8 +64,7 @@ IN_PROC_BROWSER_TEST_F(SmbCredentialsDialogTest, ShowSameMountId) {
   ASSERT_TRUE(content::WaitForLoadStop(dialog_contents));
   EXPECT_EQ(dialog_contents->GetLastCommittedURL().host(),
             chrome::kChromeUISmbCredentialsHost);
-  ASSERT_TRUE(
-      content::ExecuteScript(dialog_contents, "chrome.send('dialogClose');"));
+  ASSERT_TRUE(content::ExecJs(dialog_contents, "chrome.send('dialogClose');"));
 
   run_loop.Run();
 }
@@ -90,8 +88,8 @@ IN_PROC_BROWSER_TEST_F(SmbCredentialsDialogTest, SubmitCredentials) {
   ASSERT_TRUE(content::WaitForLoadStop(dialog_contents));
   EXPECT_EQ(dialog_contents->GetLastCommittedURL().host(),
             chrome::kChromeUISmbCredentialsHost);
-  ASSERT_TRUE(content::ExecuteScript(dialog_contents,
-                                     R"xxx(
+  ASSERT_TRUE(content::ExecJs(dialog_contents,
+                              R"xxx(
 const dialog = document.querySelector('smb-credentials-dialog');
 dialog.username_ = 'my-username';
 dialog.password_ = 'my-password';
