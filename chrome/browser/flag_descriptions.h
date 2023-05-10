6e3f155513c4f93029d59222a8c8d9f08d819fbb
Cam Bickel <cambickel@google.com>
2023-05-10 23:29:59 +0000

    shortcuts: Add flag to enable deprecation of old shortcut
    Bug: b:266085562
    Change-Id: I4e81bd1de9603209609536cf73c7005022c077f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517354
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142472}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -664,6 +664,9 @@ extern const char kDeprecateAltClickDescription[];
 extern const char kDeprecateAltBasedSixPackName[];
 extern const char kDeprecateAltBasedSixPackDescription[];
 
+extern const char kDeprecateOldKeyboardShortcutsAcceleratorName[];
+extern const char kDeprecateOldKeyboardShortcutsAcceleratorDescription[];
+
 extern const char kMemlogName[];
 extern const char kMemlogDescription[];
 extern const char kMemlogModeMinimal[];
