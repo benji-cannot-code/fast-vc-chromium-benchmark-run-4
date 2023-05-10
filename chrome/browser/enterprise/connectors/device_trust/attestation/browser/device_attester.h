ff15ce1aae9c991c1c971960dc9e707cb14b325a
hmare <hmare@google.com>
2023-05-10 20:43:59 +0000

    [DTC-BYOD] Implement device and profile attesters
    Finish the implementation of the attesters that will be used in the
    browser attestation service.
    
    Bug: b/279063303
    Change-Id: I0d0151d9cb28387d1b335958af3058ccd84d1518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517242
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1142342}

--- chrome/browser/enterprise/connectors/device_trust/attestation/browser/device_attester.h
+++ chrome/browser/enterprise/connectors/device_trust/attestation/browser/device_attester.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_ATTESTATION_BROWSER_DEVICE_ATTESTER_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/enterprise/connectors/device_trust/attestation/browser/attester.h"
 
 namespace policy {
@@ -37,6 +38,18 @@ class DeviceAttester : public Attester {
                     base::OnceClosure done_closure) override;
 
  private:
+  // Adds the `exported_key` details to the `key_info` and invokes the
+  // `done_closure` to indicate completion.
+  void OnPublicKeyExported(KeyInfo& key_info,
+                           base::OnceClosure done_closure,
+                           absl::optional<std::string> exported_key);
+
+  // Adds the `signed_response` to the `signed_data` and invokes the
+  // `done_closure` to indicate completion.
+  void OnResponseSigned(SignedData& signed_data,
+                        base::OnceClosure done_closure,
+                        absl::optional<std::vector<uint8_t>> signed_response);
+
   // Owned by the CBCMController, which is eventually owned by the browser
   // process. Since the current service is owned at the profile level, this
   // respects the browser shutdown sequence.
@@ -47,6 +60,8 @@ class DeviceAttester : public Attester {
 
   // Used for retrieving a managed devices customer ID.
   const raw_ptr<policy::CloudPolicyStore> browser_cloud_policy_store_;
+
+  base::WeakPtrFactory<DeviceAttester> weak_factory_{this};
 };
 
 }  // namespace enterprise_connectors
