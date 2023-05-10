468715b4a918f7a82d19db2365aa9bc75739595e
Brian Sheedy <bsheedy@chromium.org>
2023-05-10 22:38:51 +0000

    Support different UPF repos
    Adds support for running the GPU unexpected pass finder on different
    repos. The primary use for this is to support WebGPU, as the
    expectations are in the Dawn repo, but a positive side effect is that
    the script should now work properly regardless of where the script is
    run from instead of requiring that it be run from chromium/src.
    
    Bug: 1426512
    Change-Id: Ica96fa8dc24a602970750fe3b4a920508f7abc2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509673
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142428}

--- content/test/gpu/gpu_tests/webgpu_cts_integration_test.py
+++ content/test/gpu/gpu_tests/webgpu_cts_integration_test.py
@@ -537,6 +537,10 @@ class WebGpuCtsIntegrationTest(gpu_integration_test.GpuIntegrationTest):
   def ExpectationsFiles(cls) -> List[str]:
     return [EXPECTATIONS_FILE]
 
+  @classmethod
+  def GetExpectationsFilesRepoPath(cls) -> str:
+    return os.path.join(gpu_path_util.CHROMIUM_SRC_DIR, 'third_party', 'dawn')
+
 
 class WebGpuMessageProtocolError(RuntimeError):
   pass
