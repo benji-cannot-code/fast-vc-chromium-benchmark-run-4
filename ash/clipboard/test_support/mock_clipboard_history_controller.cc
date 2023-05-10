5c95fa4c3487ec42369df37260162f21f49ce00b
andrewxu <andrewxu@chromium.org>
2023-05-10 01:59:48 +0000

    [Multipaste Growth] Create a mojo API to paste item by id
    This CL provides an API to the Lacros side to paste a clipboard
    history item specified by the id.
    
    This CL should not bring any noticeable changes.
    
    Bug: b/279451360
    Change-Id: I99204662b14e5d38edbdd67e2be89cb9f86fadd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4480918
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141799}

---
+++ ash/clipboard/test_support/mock_clipboard_history_controller.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/clipboard/test_support/mock_clipboard_history_controller.h"
+
+namespace ash {
+
+MockClipboardHistoryController::MockClipboardHistoryController() = default;
+
+MockClipboardHistoryController::~MockClipboardHistoryController() = default;
+
+}  // namespace ash
--- chrome/browser/ash/crosapi/clipboard_history_ash.h
+++ chrome/browser/ash/crosapi/clipboard_history_ash.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 
+namespace base {
+class UnguessableToken;
+}  // namespace base
+
 namespace gfx {
 class Rect;
 }  // namespace gfx
@@ -30,6 +34,10 @@ class ClipboardHistoryAsh : public mojom::ClipboardHistory {
       const gfx::Rect& anchor_point,
       ui::MenuSourceType menu_source_type,
       mojom::ClipboardHistoryControllerShowSource show_source) override;
+  void PasteClipboardItemById(
+      const base::UnguessableToken& item_id,
+      int event_flags,
+      mojom::ClipboardHistoryControllerShowSource paste_source) override;
 
  private:
   // This class supports any number of connections. This allows the client to
