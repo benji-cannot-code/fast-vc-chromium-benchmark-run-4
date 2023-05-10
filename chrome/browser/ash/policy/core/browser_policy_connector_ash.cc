522528a3eea838c12b0f3c6b6c2f8aadfe02a026
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-05-10 08:32:53 +0000

    Revert "BrowserPolicyConnectorAsh: Remove unused ChromeAD functions"
    This reverts commit ea176bd1844f2b6e0305c1d2f40837eb02b23d60.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8781499618756944177
    
    Sample failed build: https://ci.chromium.org/b/8781499618756944177
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8781499618756944177&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4519120
    
    Original change's description:
    > BrowserPolicyConnectorAsh: Remove unused ChromeAD functions
    >
    > IsActiveDirectoryManaged and GetRealm are unused.
    > They get removed by this CL.
    >
    > Bug: b/281482019
    > Test: Remaining tests
    > Change-Id: I35441d94366675e9e127845c0e4852f2103e5281
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519120
    > Reviewed-by: Igor <igorcov@chromium.org>
    > Commit-Queue: Roland Bock <rbock@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1141933}
    >
    
    Bug: b/281482019
    Change-Id: I818de11fe560262d75f55dcc825cd58934410756
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519963
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1141942}

--- chrome/browser/ash/policy/core/browser_policy_connector_ash.cc
+++ chrome/browser/ash/policy/core/browser_policy_connector_ash.cc
@@ -358,6 +358,10 @@ bool BrowserPolicyConnectorAsh::IsCloudManaged() const {
   return ash::InstallAttributes::Get()->IsCloudManaged();
 }
 
+bool BrowserPolicyConnectorAsh::IsActiveDirectoryManaged() const {
+  return ash::InstallAttributes::Get()->IsActiveDirectoryManaged();
+}
+
 std::string BrowserPolicyConnectorAsh::GetEnterpriseEnrollmentDomain() const {
   return ash::InstallAttributes::Get()->GetDomain();
 }
@@ -376,6 +380,10 @@ std::string BrowserPolicyConnectorAsh::GetSSOProfile() const {
   if (policy && policy->has_sso_profile())
     return policy->sso_profile();
   return std::string();
+}
+
+std::string BrowserPolicyConnectorAsh::GetRealm() const {
+  return ash::InstallAttributes::Get()->GetRealm();
 }
 
 std::string BrowserPolicyConnectorAsh::GetDeviceAssetID() const {
