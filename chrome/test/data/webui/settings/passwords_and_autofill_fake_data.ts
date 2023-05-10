a6c1103be9dca63a129932dc763a3d28d0c2ad0e
Vipul Vinod Koul <koulvipul@google.com>
2023-05-10 21:17:01 +0000

    Settings: Hook up manadtory reauthentication in the payments page.
    Whenever a user tries to change the mandatory auth toggle to on or off, it would ask for authentication from the user.
    
    This CL only deals with the UI components of the settings page.
    
    Design doc- go/pay-autofill-mandatory-reauth-engineering-design-chrome
    
    Screenshot- go/strpic/806044854b5e53130c7152ec21dae790de989851
    
    Bug: 1434413
    Change-Id: Ied9f9cfa7cee7a9f27b76fa31e4d991f8567b35e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443474
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1142367}

--- chrome/test/data/webui/settings/passwords_and_autofill_fake_data.ts
+++ chrome/test/data/webui/settings/passwords_and_autofill_fake_data.ts
@@ -515,6 +515,7 @@ export class PaymentsManagerExpectations {
   requestedIbans: number = 0;
   removedIbans: number = 0;
   isValidIban: number = 0;
+  authenticateUserAndFlipMandatoryAuthToggle: number = 0;
 }
 
 /**
@@ -545,6 +546,7 @@ export class TestPaymentsManager extends TestBrowserProxy implements
       'removeIban',
       'addVirtualCard',
       'isValidIban',
+      'authenticateUserAndFlipMandatoryAuthToggle',
     ]);
 
     // Set these to have non-empty data.
@@ -626,6 +628,10 @@ export class TestPaymentsManager extends TestBrowserProxy implements
     return Promise.resolve(this.isUserVerifyingPlatformAuthenticatorAvailable_);
   }
 
+  authenticateUserAndFlipMandatoryAuthToggle() {
+    this.methodCalled('authenticateUserAndFlipMandatoryAuthToggle');
+  }
+
   /**
    * Verifies expectations.
    */
@@ -654,5 +660,9 @@ export class TestPaymentsManager extends TestBrowserProxy implements
     assertEquals(
         expected.removedIbans, this.getCallCount('removeIban'),
         'removedIbans mismatch');
+    assertEquals(
+        expected.authenticateUserAndFlipMandatoryAuthToggle,
+        this.getCallCount('authenticateUserAndFlipMandatoryAuthToggle'),
+        'authenticateUserAndFlipMandatoryAuthToggle mismatch');
   }
 }
