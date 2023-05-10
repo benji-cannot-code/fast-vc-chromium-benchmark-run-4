ed76500495489f92c71cec25091ae8d0483c4975
Koji Ishii <kojii@chromium.org>
2023-05-10 04:18:28 +0000

    [text-wrap-pretty] Extract `HyphenResult`
    So that it can be used without `NGInlineItemResult`.
    
    This patch has no behavior changes.
    
    Bug: 1432798
    Change-Id: Ib8a44e314ac6661db015a5da7b7d5d86f230d910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4513665
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141853}

---
+++ third_party/blink/renderer/core/layout/ng/inline/hyphen_result.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/layout/ng/inline/hyphen_result.h"
+
+#include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
+
+namespace blink {
+
+void HyphenResult::Shape(const ComputedStyle& style) {
+  text_ = style.HyphenString();
+  HarfBuzzShaper shaper(text_);
+  shape_result_ = shaper.Shape(&style.GetFont(), style.Direction());
+}
+
+}  // namespace blink
--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_result.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_result.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_box_strut.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/hyphen_result.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_text_index.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_text_offset_range.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_positioned_float.h"
@@ -53,16 +54,8 @@ struct CORE_EXPORT NGInlineItemResult {
   NGInlineItemTextIndex Start() const { return {item_index, StartOffset()}; }
   NGInlineItemTextIndex End() const { return {item_index, EndOffset()}; }
 
-  LayoutUnit HyphenInlineSize() const {
-    return hyphen_shape_result->SnappedWidth().ClampNegativeToZero();
-  }
-
   // Compute/clear |hyphen_string| and |hyphen_shape_result|.
   void ShapeHyphen();
-  void ClearHyphen() {
-    hyphen_string = String();
-    hyphen_shape_result = nullptr;
-  }
 
   void Trace(Visitor* visitor) const;
 #if DCHECK_IS_ON()
@@ -93,9 +86,8 @@ struct CORE_EXPORT NGInlineItemResult {
 
   // Hyphen character and its |ShapeResult|.
   // Use |is_hyphenated| to determine whether this item is hyphenated or not.
-  // These fields may be set even when this item is not hyphenated.
-  String hyphen_string;
-  scoped_refptr<const ShapeResult> hyphen_shape_result;
+  // This field may be set even when this item is not hyphenated.
+  HyphenResult hyphen;
 
   // NGLayoutResult for atomic inline items.
   Member<const NGLayoutResult> layout_result;
