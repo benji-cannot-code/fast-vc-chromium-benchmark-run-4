4240a6837428686f8b843824a15b75645caf5c66
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-05-10 16:06:01 +0000

    Kombucha-fy other flaky tests from ProfilePickerInteractiveUiTest
    Bug: 1223291
    Change-Id: Ic339e0b7e4c5ccc380a9cae884ad368ddbc6017c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4506832
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Nicolas Dossou-gbete <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142103}

--- chrome/browser/ui/views/profiles/profile_picker_interactive_uitest_base.h
+++ chrome/browser/ui/views/profiles/profile_picker_interactive_uitest_base.h
@@ -7,16 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_PICKER_INTERACTIVE_UITEST_BASE_H_
 
 #include "chrome/browser/ui/views/profiles/profile_picker_test_base.h"
-
-namespace ui {
-enum KeyboardCode;
-}
+#include "ui/base/accelerators/accelerator.h"
 
 // Mixin adding helpers to write interactive ui tests focused on
 // `ProfilePickerView`.
 class WithProfilePickerInteractiveUiTestHelpers
     : public WithProfilePickerTestHelpers {
  public:
+  ui::Accelerator GetAccelerator(int command_id);
+
   void SendCloseWindowKeyboardCommand();
 
   void SendBackKeyboardCommand();
@@ -27,6 +26,8 @@ class WithProfilePickerInteractiveUiTestHelpers
   void SendQuitAppKeyboardCommand();
 #endif
 
+  // Sends the provided keyboard command to the profile picker window.
+  void SendKeyPress(ui::Accelerator accelerator);
   void SendKeyPress(ui::KeyboardCode key,
                     bool control,
                     bool shift,
