ed172bea60b1ee4a55973548f4d427e86aac08bb
Owen Min <zmin@chromium.org>
2023-05-10 13:15:37 +0000

    Create legacy tech report URL matcher
    Bug: 1441215
    Change-Id: Ibf39007a1317b6fe25f87f5d674241ece60ff1aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514684
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Quick-Run: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1142045}

---
+++ chrome/browser/enterprise/reporting/legacy_tech/legacy_tech_url_matcher.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_REPORTING_LEGACY_TECH_LEGACY_TECH_URL_MATCHER_H_
+#define CHROME_BROWSER_ENTERPRISE_REPORTING_LEGACY_TECH_LEGACY_TECH_URL_MATCHER_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/memory/raw_ptr.h"
+#include "components/prefs/pref_change_registrar.h"
+#include "components/url_matcher/url_matcher.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+class GURL;
+class Profile;
+
+namespace enterprise_reporting {
+
+class LegacyTechURLMatcher {
+ public:
+  explicit LegacyTechURLMatcher(Profile* profile);
+  LegacyTechURLMatcher(const LegacyTechURLMatcher&) = delete;
+  LegacyTechURLMatcher& operator=(const LegacyTechURLMatcher&) = delete;
+  ~LegacyTechURLMatcher();
+
+  void OnPrefUpdated();
+
+  absl::optional<std::string> GetMatchedURL(const GURL& url) const;
+
+ private:
+  raw_ptr<Profile> profile_;
+  PrefChangeRegistrar pref_change_;
+
+  std::unique_ptr<url_matcher::URLMatcher> url_matcher_;
+  std::vector<size_t> path_length_;
+};
+
+}  // namespace enterprise_reporting
+
+#endif  // CHROME_BROWSER_ENTERPRISE_REPORTING_LEGACY_TECH_LEGACY_TECH_URL_MATCHER_H_
