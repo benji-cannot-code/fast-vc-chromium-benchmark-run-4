5c640f7d2ef7d25b057585a169a270dffa9107ad
Richard Chen <richche@google.com>
2023-05-10 22:58:52 +0000

    Revert "CRX Telemetry: Off-store file data 100% Stable launch."
    This reverts commit 0740618f03d5f38d24e921018a799e7ef51446ce.
    
    Reason for revert: Rollback to 50% Stable
    
    Original change's description:
    > CRX Telemetry: Off-store file data 100% Stable launch.
    >
    > - Enable kExtensionTelemetryFileData by default
    > - Remove entry in fieldtrial_testing_config.json
    >
    > Bug: 1384755
    > Change-Id: Ifecf290c0f7e6ca9dd3239726e877294c4376905
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505365
    > Commit-Queue: Richard Chen <richche@google.com>
    > Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1139648}
    
    Bug: 1384755
    Change-Id: I5a89fa84108f4c382f974f9e63d5692bf3f89b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517772
    Commit-Queue: Richard Chen <richche@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142446}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -109,7 +109,7 @@ BASE_FEATURE(kExtensionTelemetryPersistence,
 
 BASE_FEATURE(kExtensionTelemetryFileData,
              "SafeBrowsingExtensionTelemetryFileData",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 const base::FeatureParam<int> kExtensionTelemetryFileDataMaxFilesToProcess{
     &kExtensionTelemetryFileData, "MaxFilesToProcess",
@@ -309,7 +309,6 @@ constexpr struct {
     {&kExtensionTelemetryCookiesGetAllSignal, true},
     {&kExtensionTelemetryCookiesGetSignal, true},
     {&kExtensionTelemetryDeclarativeNetRequestSignal, true},
-    {&kExtensionTelemetryFileData, true},
     {&kExtensionTelemetryPersistence, true},
     {&kExtensionTelemetryPotentialPasswordTheft, true},
     {&kExtensionTelemetryReportContactedHosts, true},
