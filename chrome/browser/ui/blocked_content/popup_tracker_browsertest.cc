d15b25d1838f8bb8e348e7c92addc2bf16210fd2
Chris Fredrickson <cfredric@chromium.org>
2023-05-10 03:14:11 +0000

    Replace ExecuteScript with ExecJs
    This CL was uploaded by git cl split.
    
    R=csharrison@chromium.org
    
    Bug: 1157718
    Change-Id: I53adac9c215fc9f51cac2c11433529f90e510a18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517749
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141823}

--- chrome/browser/ui/blocked_content/popup_tracker_browsertest.cc
+++ chrome/browser/ui/blocked_content/popup_tracker_browsertest.cc
@@ -116,9 +116,9 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest,
   content::TestNavigationObserver navigation_observer(nullptr, 1);
   navigation_observer.StartWatchingNewWebContents();
 
-  EXPECT_TRUE(content::ExecuteScript(
-      browser()->tab_strip_model()->GetActiveWebContents(),
-      "window.open('/title1.html')"));
+  EXPECT_TRUE(
+      content::ExecJs(browser()->tab_strip_model()->GetActiveWebContents(),
+                      "window.open('/title1.html')"));
   navigation_observer.Wait();
 
   EXPECT_EQ(2, browser()->tab_strip_model()->count());
@@ -152,9 +152,9 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest,
   content::TestNavigationObserver navigation_observer(nullptr, 1);
   navigation_observer.StartWatchingNewWebContents();
 
-  EXPECT_TRUE(content::ExecuteScript(
-      browser()->tab_strip_model()->GetActiveWebContents(),
-      "window.open('/title1.html')"));
+  EXPECT_TRUE(
+      content::ExecJs(browser()->tab_strip_model()->GetActiveWebContents(),
+                      "window.open('/title1.html')"));
   navigation_observer.Wait();
 
   EXPECT_EQ(2, browser()->tab_strip_model()->count());
@@ -734,7 +734,7 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerPrerenderBrowserTest,
 
   // Load a prerender url in the popup window.
   const GURL prerender_url = embedded_test_server()->GetURL("/empty.html");
-  EXPECT_TRUE(content::ExecuteScript(
+  EXPECT_TRUE(content::ExecJs(
       browser()->tab_strip_model()->GetActiveWebContents(),
       "window.open('/popup_blocker/popup-simple-prerender.html')"));
   prerender_helper()->WaitForPrerenderLoadCompletion(prerender_url);
