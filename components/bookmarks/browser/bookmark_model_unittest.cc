cac329abe24fb32f5b66a65fcb8aa460b9e76988
Boris Sazonov <bsazonov@google.com>
2023-05-10 16:21:52 +0000

    [Bookmarks] Send notifications for descendants during cross-model move
    Changes notifications sent by MoveToOtherModelWithNewNodeIdsAndUuids
    when a bookmark hierarchy is moved. Prior to this CL,
    BookmarkNodeAdded was sent for the topmost folder only, which was
    causing issues in the sync engine. After this CL, BookmarkNodeAdded is
    sent for all nodes in the hierarchy.
    
    Bug: 1440384
    Change-Id: I4bc46899f6cbbdde20075bef9aa65e3a338ebed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507297
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142118}

--- components/bookmarks/browser/bookmark_model_unittest.cc
+++ components/bookmarks/browser/bookmark_model_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/uuid.h"
 #include "build/build_config.h"
 #include "components/bookmarks/browser/bookmark_model_observer.h"
+#include "components/bookmarks/browser/bookmark_node.h"
 #include "components/bookmarks/browser/bookmark_undo_delegate.h"
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/bookmarks/browser/titled_url_match.h"
@@ -53,7 +54,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::ASCIIToUTF16;
 using base::Time;
+using testing::Invoke;
 using testing::Mock;
+using testing::WithArg;
 
 namespace bookmarks {
 namespace {
@@ -1998,9 +2001,21 @@ TEST_F(BookmarkDualModelTest, MoveToOtherModel) {
               BookmarkNodeRemoved(local_or_syncable_model_.get(), root, 0,
                                   folder, removed_urls))
       .InSequence(local_or_syncable_sequence);
-  // Not associated with the sequence and allowed to be invoked at any point.
+
+  testing::Sequence account_sequence;
   EXPECT_CALL(account_observer_,
-              BookmarkNodeAdded(account_model_.get(), dest_folder, 0, true));
+              BookmarkNodeAdded(account_model_.get(), dest_folder, 0, true))
+      .InSequence(account_sequence);
+  const BookmarkNode* captured_foo_parent = nullptr;
+  EXPECT_CALL(account_observer_,
+              BookmarkNodeAdded(account_model_.get(), testing::_, 0, true))
+      .InSequence(account_sequence)
+      .WillOnce(testing::SaveArg<1>(&captured_foo_parent));
+  const BookmarkNode* captured_bar_parent = nullptr;
+  EXPECT_CALL(account_observer_,
+              BookmarkNodeAdded(account_model_.get(), testing::_, 1, true))
+      .InSequence(account_sequence)
+      .WillOnce(testing::SaveArg<1>(&captured_bar_parent));
 
   local_or_syncable_model_->MoveToOtherModelWithNewNodeIdsAndUuids(
       folder, account_model_.get(), dest_folder);
@@ -2009,6 +2024,8 @@ TEST_F(BookmarkDualModelTest, MoveToOtherModel) {
   const BookmarkNode* moved_folder = dest_folder->children()[0].get();
   EXPECT_EQ(moved_folder->GetTitle(), u"folder");
   ASSERT_EQ(moved_folder->children().size(), 2u);
+  EXPECT_EQ(captured_foo_parent, moved_folder);
+  EXPECT_EQ(captured_bar_parent, moved_folder);
   const BookmarkNode* moved_foo = moved_folder->children()[0].get();
   EXPECT_EQ(moved_foo->GetTitle(), u"foo");
   EXPECT_EQ(moved_foo->GetTitledUrlNodeUrl(), GURL("http://foo.com"));
