0e4947c7a4ff0ba6faed16dd94e83a40422b0ccb
Federica Germinario <fedegermi@google.com>
2023-05-10 09:02:18 +0000

    [ios] Remove unused property from grid_view_controller+private.h
    Property viewAppeared defined in grid_view_controller+private.h is
    never used.
    
    Change-Id: Ic3cc7a929f7f8d0e04e86575db478760c3adc1b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4516835
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141945}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller+private.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller+private.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, readonly) NSMutableArray<TabSwitcherItem*>* items;
 @property(nonatomic, readonly) NSUInteger selectedIndex;
 @property(nonatomic, readonly) UICollectionView* collectionView;
-@property(nonatomic, assign, getter=isViewAppeared) BOOL viewAppeared;
 
 @end
 
