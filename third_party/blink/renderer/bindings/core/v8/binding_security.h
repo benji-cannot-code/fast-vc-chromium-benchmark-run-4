117435ca14f49c386260b511d89188aaa4c5d74c
Nate Chapin <japhet@chromium.org>
2023-05-10 16:37:46 +0000

    Don't throw in LocalDOMWindow when BindingSecurity fails
    These are secondary security checks that are only for catching bugs, and
    we DCHECK when we fail them. This is the only usage of the
    ShouldAllowAccessTo() variant that takes a DOMWindow* and an
    ExceptionState&.
    
    Change-Id: I6332e5e5cb5373cc85801ce01072b4dfb4670b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519388
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142133}

--- third_party/blink/renderer/bindings/core/v8/binding_security.h
+++ third_party/blink/renderer/bindings/core/v8/binding_security.h
@@ -68,9 +68,6 @@ class CORE_EXPORT BindingSecurity {
   // DOMWindow
   static bool ShouldAllowAccessTo(const LocalDOMWindow* accessing_window,
                                   const DOMWindow* target,
-                                  ExceptionState&);
-  static bool ShouldAllowAccessTo(const LocalDOMWindow* accessing_window,
-                                  const DOMWindow* target,
                                   ErrorReportOption);
 
   // Location
