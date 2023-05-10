e862af0e0ba47d942d6f3b9580d24795b8695ad6
Tove Petersson <tovep@chromium.org>
2023-05-10 07:36:58 +0000

    Added a tab-preview to the share-this-tab dialog.
    Bug: 1428878
    Change-Id: I95de4cfea71ec56c9c0f84785d76b3d6a812bd33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509396
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141913}

--- chrome/browser/ui/views/desktop_capture/share_this_tab_dialog_views.cc
+++ chrome/browser/ui/views/desktop_capture/share_this_tab_dialog_views.cc
@@ -72,7 +72,7 @@ ShareThisTabDialogView::ShareThisTabDialogView(
       views::BoxLayout::CrossAxisAlignment::kCenter);
 
   source_view_ = source_container->AddChildView(
-      std::make_unique<ShareThisTabSourceView>());
+      std::make_unique<ShareThisTabSourceView>(web_contents_));
 
   activation_timer_.Start(
       FROM_HERE,
@@ -134,6 +134,7 @@ bool ShareThisTabDialogView::Accept() {
   CHECK(!activation_timer_.IsRunning());
   CHECK(IsDialogButtonEnabled(ui::DIALOG_BUTTON_OK));
 
+  source_view_->StopRefreshing();
   if (parent_ && web_contents_) {
     parent_->NotifyDialogResult(content::DesktopMediaID(
         content::DesktopMediaID::TYPE_WEB_CONTENTS,
@@ -149,6 +150,7 @@ bool ShareThisTabDialogView::Accept() {
 
 bool ShareThisTabDialogView::Cancel() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  source_view_->StopRefreshing();
   activation_timer_.Stop();
   return views::DialogDelegateView::Cancel();
 }
