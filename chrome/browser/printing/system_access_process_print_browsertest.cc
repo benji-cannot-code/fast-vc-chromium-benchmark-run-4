b8e6ca02343c2d46fec90f28689a8b3b97f9611f
Chris Fredrickson <cfredric@chromium.org>
2023-05-10 22:07:38 +0000

    Replace ExecuteScript with ExecJs
    ExecuteScript is deprecated in favor of ExecJs. ExecJs is more
    flexible and has better error-handling. If the script's completion
    value is *not* a Promise, then ExecJs is a drop-in replacement for
    ExecuteScript. Otherwise, the new equivalent is to use ExecJs with the
    EXECUTE_SCRIPT_NO_RESOLVE_PROMISES option.
    
    This CL was uploaded by git cl split.
    
    R=awscreen@chromium.org
    
    Bug: 1157718
    Change-Id: I22bd61216ce6ee2ace429c1ef6f28032e10f0302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518283
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142403}

--- chrome/browser/printing/system_access_process_print_browsertest.cc
+++ chrome/browser/printing/system_access_process_print_browsertest.cc
@@ -454,7 +454,7 @@ class SystemAccessProcessPrintBrowserTestBase
                        .shadowRoot.querySelector('print-preview-button-strip')
                        .shadowRoot.querySelector('.action-button');
       button.click();)";
-    ASSERT_TRUE(content::ExecuteScript(preview_dialog, kScript));
+    ASSERT_TRUE(content::ExecJs(preview_dialog, kScript));
     WaitUntilCallbackReceived();
   }
 
