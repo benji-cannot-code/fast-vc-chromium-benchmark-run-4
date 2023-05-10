232226b6d9c373027397cb46259dc4d33d99157e
Avi Drissman <avi@chromium.org>
2023-05-10 16:26:50 +0000

    Convert /content/app to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: I6caf1447947d32b6f01586e883d80d05dabcb4de
    Include-Ci-Only-Tests: true
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517747
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142122}

--- content/app/mac_init.mm
+++ content/app/mac_init.mm
@@ -7,10 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace content {
 
 void InitializeMac() {
-  [[NSUserDefaults standardUserDefaults] registerDefaults:@{
+  [NSUserDefaults.standardUserDefaults registerDefaults:@{
     // Exceptions routed to -[NSApplication reportException:] should crash
     // immediately, as opposed being swallowed or presenting UI that gives the
     // user a choice in the matter.
