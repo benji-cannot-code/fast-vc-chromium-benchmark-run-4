feaab8c699c560af74e0ca16e758e5f58fd1fd8d
Violetta Fedotova <violettfaid@google.com>
2023-05-10 18:35:08 +0000

    ADPF: Continuous boost hint during scrolls.
    ADPF hints are reported in the end of the frame, so if we are missing a frame the hint will be send in the very end when it is too late.
    
    So here we introduce a change where we send hints which emulate a "miss frame" behaviour for each scroll frame to prevent the actual frame missing.
    
    Bug: b/276748520
    Change-Id: If3cab38bc62bf6e4fb992f9fc51c85b72b2595b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4444644
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Violetta Fedotova <violettfaid@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1142210}

---
+++ components/viz/service/display/display_utils.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_DISPLAY_UTILS_H_
+#define COMPONENTS_VIZ_SERVICE_DISPLAY_DISPLAY_UTILS_H_
+
+#include <vector>
+
+#include "components/viz/service/viz_service_export.h"
+#include "ui/latency/latency_info.h"
+
+namespace viz {
+
+VIZ_SERVICE_EXPORT bool IsScroll(
+    const std::vector<ui::LatencyInfo>& latency_infos);
+
+}  // namespace viz
+
+#endif  // COMPONENTS_VIZ_SERVICE_DISPLAY_DISPLAY_UTILS_H_
