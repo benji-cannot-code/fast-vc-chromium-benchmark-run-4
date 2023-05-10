77973f6a9e9d513291708777cdf29277aa6bf51d
Markus Handell <handellm@google.com>
2023-05-10 10:51:35 +0000

    SocketClientImpl: add a unit test.
    This CL is a NOP but adds unit test support for SocketClientImpl. It
    also contains a small refactoring - the socket creation was moved from
    the SocketClientImpl into the IpcSocketFactory which simplifies the
    test.
    
    The tests are needed for more complex batching logic in forthcoming
    CLs.
    
    Bug: chromium:1439830
    Change-Id: Ib0600c9aa277c454e0ebad46a2fed25b12565c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519082
    Commit-Queue: Markus Handell <handellm@google.com>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Auto-Submit: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1141989}

--- third_party/blink/renderer/platform/p2p/socket_client_delegate.h
+++ third_party/blink/renderer/platform/p2p/socket_client_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_P2P_SOCKET_CLIENT_DELEGATE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_P2P_SOCKET_CLIENT_DELEGATE_H_
 
+#include "base/time/time.h"
 #include "net/base/ip_endpoint.h"
 #include "services/network/public/cpp/p2p_socket_type.h"
 
