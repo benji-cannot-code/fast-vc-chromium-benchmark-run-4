55b38e17b3caaff6d6c441f09a52979d4a9a471b
Xi Han <hanxi@google.com>
2023-05-10 02:29:33 +0000

    [StartTablet] Polish single tab card UI.
    In this CL, we change the width of the single Tab card to be 16dp
    shorter than the fake search box on NTP.
    Demo: http://shortn/_hvpnbnUacA.
    
    Bug: 1416536
    Change-Id: If2a53475c78c5ee89b8ea756bf2752014ffea6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518524
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141810}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/tasks/SingleTabSwitcherCoordinator.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/tasks/SingleTabSwitcherCoordinator.java
@@ -19,7 +19,9 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.feed.FeedFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
@@ -45,7 +47,9 @@ public class SingleTabSwitcherCoordinator implements TabSwitcher {
     private Tab mLastActiveTab;
 
     public SingleTabSwitcherCoordinator(@NonNull Activity activity, @NonNull ViewGroup container,
-            @NonNull TabModelSelector tabModelSelector, boolean isTablet, Tab mostRecentTab) {
+            ActivityLifecycleDispatcher activityLifecycleDispatcher,
+            @NonNull TabModelSelector tabModelSelector, boolean isTablet,
+            boolean isScrollableMvtEnabled, Tab mostRecentTab) {
         mTabModelSelector = tabModelSelector;
         mIsTablet = isTablet;
         mLastActiveTab = mostRecentTab;
@@ -62,8 +66,10 @@ public class SingleTabSwitcherCoordinator implements TabSwitcher {
                     activity, propertyModel, tabModelSelector, mTabListFaviconProvider);
             mMediatorOnTablet = null;
         } else {
-            mMediatorOnTablet = new SingleTabSwitcherOnTabletMediator(
-                    propertyModel, tabModelSelector, mTabListFaviconProvider, mostRecentTab);
+            mMediatorOnTablet = new SingleTabSwitcherOnTabletMediator(propertyModel,
+                    activity.getResources(), activityLifecycleDispatcher, tabModelSelector,
+                    mTabListFaviconProvider, mostRecentTab,
+                    FeedFeatures.isMultiColumnFeedEnabled(activity), isScrollableMvtEnabled);
             mMediator = null;
         }
         if (ChromeFeatureList.sInstantStart.isEnabled()) {
@@ -224,6 +230,9 @@ public class SingleTabSwitcherCoordinator implements TabSwitcher {
             mLastActiveTab.removeObserver(mLastActiveTabObserver);
             mLastActiveTab = null;
             mLastActiveTabObserver = null;
+        }
+        if (mMediatorOnTablet != null) {
+            mMediatorOnTablet.destroy();
         }
     }
 
