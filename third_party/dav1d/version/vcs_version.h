e66c1b6de246a0a2d019b39c7742903de69c0672
Wan-Teh Chang <wtc@google.com>
2023-05-10 23:04:36 +0000

    Roll src/third_party/dav1d/libdav1d/ d426d1c91..676a864a1 (42 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/d426d1c91075..676a864a11af
    
    $ git log d426d1c91..676a864a1 --date=short --no-merges --format='%ad %ae %s'
    2023-04-28 jb News for 1.2.0
    2023-04-28 gramner x86: Add high bit-depth ipred z3 SSSE3 asm
    2023-04-25 vigneshv crossfiles: Add android x86 and x86_64 crossfiles
    2023-04-17 gramner x86: Add high bit-depth ipred z1 SSSE3 asm
    2023-04-12 code crossfiles: Streamline and simplify crossfiles
    2023-04-16 rsbultje x86: add AVX512-IceLake implementation of HBD 64x64 DCT^2
    2023-04-18 jamrial picture: allow storing an array of Dav1dITUTT35 entries
    2023-04-14 rsbultje x86: add AVX512-IceLake implementation of HBD 64x32 DCT^2
    2023-04-12 rsbultje x86: add AVX512-IceLake implementation of HBD 64x16 DCT^2
    2023-04-08 rsbultje x86: add AVX512-IceLake implementation of HBD 32x64 DCT^2
    (...)
    2023-03-08 martin arm: ipred: Fix a misindented line in the C wrapper
    2023-03-13 victorien x86: Add refmvs.save_tmvs AVX-512 (Ice Lake) asm
    2023-02-26 victorien x86: Add refmvs.save_tmvs SSSE3 asm
    2023-03-13 victorien x86: Optimize refmvs.save_tmvs AVX2 asm
    2023-03-13 victorien threading: Ensure passing the correct retval to decode_frame_exit
    2023-02-27 gramner x86: Add 8-bit ipred z3 SSSE3 asm
    2023-02-17 victorien x86: Add refmvs.save_tmvs AVX2 asm
    2023-01-30 victorien refmvs: Add refmvs_load/save_tmvs to dsp interface
    2023-02-26 victorien refmvs: Pack refmvs_temporal_block struct
    2023-03-03 tmatth meson: fix clock_gettime check on wasm32
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org
    
    Change-Id: Icfb2559d7a6de660703cf82e345c1ed1fecd5633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521630
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142450}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.1.0-11-gd426d1c"
+#define DAV1D_VERSION "1.2.0-0-g676a864"
