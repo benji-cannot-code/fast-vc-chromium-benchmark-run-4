5b9f392f868abd78829ae5ffc9daea6b2144c8b3
Marc Treib <treib@chromium.org>
2023-05-10 12:04:43 +0000

    PasswordModelTypeController: Remove account_password_store_for_cleanup
    After recent improvements, this parameter isn't needed anymore and was
    unused.
    
    Bug: 1425033
    Change-Id: Iaa43344fa70a6613430d8504135c3ac0ae7367b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517528
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142017}

--- components/browser_sync/sync_api_component_factory_impl.cc
+++ components/browser_sync/sync_api_component_factory_impl.cc
@@ -385,7 +385,7 @@ SyncApiComponentFactoryImpl::CreateCommonDataTypeControllers(
               account_password_store_
                   ? account_password_store_->CreateSyncControllerDelegate()
                   : nullptr,
-              account_password_store_, sync_client_->GetPrefService(),
+              sync_client_->GetPrefService(),
               sync_client_->GetIdentityManager(), sync_service));
     }
   }
