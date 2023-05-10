92131b8e613aa6c5ad54298682ffde4b1ea78d46
Luciano Pacheco <lucmult@chromium.org>
2023-05-10 10:32:56 +0000

    Files Office: Change XfNudge to accept nudge with dismiss button
    The internal specs says that nudges with a dismiss button should only
    be dismissed via its button. This CL implements this.
    
    Configure the Office related nudges to have an "ok" button to dismiss.
    
    Bug: b:276387552
    Change-Id: Ia927c034849307540b793857518812740f6f41a6
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496126
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141983}

--- ui/file_manager/file_manager/widgets/xf_nudge.ts
+++ ui/file_manager/file_manager/widgets/xf_nudge.ts
@@ -43,6 +43,11 @@ export class XfNudge extends HTMLElement {
   private contentSlot_: HTMLElement;
 
   /**
+   * The dismiss button element.
+   */
+  private dismissButton_: HTMLElement;
+
+  /**
    * The direction of the nudge relative to the anchor.
    */
   private direction_: NudgeDirection = NudgeDirection.TOP_STARTWARD;
@@ -51,6 +56,11 @@ export class XfNudge extends HTMLElement {
    * The content of the nudge.
    */
   private content_: string = '';
+
+  /**
+   * Text used in the dismiss button. When empty the button is hidden.
+   */
+  private dismissText_: string = '';
 
   /**
    * How many times the nudge has been repositioned, this is reset when the
@@ -68,7 +78,21 @@ export class XfNudge extends HTMLElement {
 
     this.bubble_ = this.shadowRoot!.getElementById('bubble')!;
     this.contentSlot_ = this.shadowRoot!.getElementById('text')!;
+    this.dismissButton_ = this.shadowRoot!.getElementById('dismiss')!;
+    this.dismissButton_.addEventListener(
+        'click', this.dismissClicked_.bind(this));
     this.dot_ = this.shadowRoot!.getElementById('dot')!;
+  }
+
+  static get events() {
+    return {
+      DISMISS: 'dismiss',
+    } as const;
+  }
+
+  private dismissClicked_() {
+    this.dispatchEvent(new CustomEvent(
+        XfNudge.events.DISMISS, {bubbles: true, composed: true}));
   }
 
   /**
@@ -84,6 +108,8 @@ export class XfNudge extends HTMLElement {
       throw new Error('Attempted to show <xf-nudge> without an anchor');
     }
 
+    this.dismissButton_.innerText = this.dismissText_;
+    this.dismissButton_.toggleAttribute('hidden', this.dismissText_ === '');
     this.contentSlot_.innerText = this.content_;
     this.reposition();
   }
@@ -157,6 +183,17 @@ export class XfNudge extends HTMLElement {
    */
   get content() {
     return this.content_;
+  }
+
+  /**
+   * Sets the text for the dismiss button, when empty hides the button.
+   */
+  set dismissText(text: string) {
+    this.dismissText_ = text;
+  }
+
+  get dismissText() {
+    return this.dismissText_;
   }
 
   /**
