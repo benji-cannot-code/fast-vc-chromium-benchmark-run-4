e14b792ca618c47bcc569570451b64a99118fb03
Victor Vianna <victorvianna@google.com>
2023-05-10 12:50:05 +0000

    Remove obsolete TODO in chrome_backup_agent
    Fixed: 1305213
    Change-Id: I6719d8cfddd8519b5c772474bd6d4d6323cef3e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521119
    Commit-Queue: Marc Treib <treib@chromium.org>
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1142034}

--- chrome/browser/android/chrome_backup_agent.cc
+++ chrome/browser/android/chrome_backup_agent.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// TODO(crbug.com/1305213): The data type toggles shouldn't be individually
-// listed here.
 static_assert(46 == syncer::GetNumModelTypes(),
               "If the new type has a corresponding pref, add it here");
 const char* backed_up_preferences_[] = {
