cac9860bccc809bafabfa6f41e8a0035a63d3b37
Veronique Nguyen <veronguyen@google.com>
2023-05-10 13:32:56 +0000

    [iOS] Fix missing domain in the password bottom sheet
    Show the page's domain in the description label of cells in the
    password bottom sheet.
    
    Bug: 1440733
    Change-Id: Ia069aeee9ffba2d89574e26ea911bfc552a10749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517685
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142049}

--- ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_consumer.h
+++ ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_consumer.h
@@ -13,9 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Suggestions consumer for the passwords bottom sheet.
 @protocol PasswordSuggestionBottomSheetConsumer
 
-// Sends the list of suggestions to be presented to the user on the bottom
-// sheet.
-- (void)setSuggestions:(NSArray<FormSuggestion*>*)suggestions;
+// Sends the list of suggestions to be presented to the user on the bottom sheet
+// and the current page's domain.
+- (void)setSuggestions:(NSArray<FormSuggestion*>*)suggestions
+             andDomain:(NSString*)domain;
 
 // Request to dismiss the bottom sheet.
 - (void)dismiss;
