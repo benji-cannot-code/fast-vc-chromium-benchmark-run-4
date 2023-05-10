29401d6e4f186c0ed780d152d6c0ebc23fe14fb7
David Awogbemila <awogbemila@chromium.org>
2023-05-10 01:31:21 +0000

    [scroll-start] Implement parsing for scroll-start shorthand
    scroll-start is currently part of the scroll-snap-2 spec[1].
    
    Per the spec, the shorthand takes 2 values. The first value specifies
    the start position along the block axis and the second value specifies
    the start position along the inline axis.
    
    [1] https://drafts.csswg.org/css-scroll-snap-2/#scroll-start
    
    Bug: 1439807
    Change-Id: Ia349147e5859aecda82ba36668219253016b8cbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509442
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141787}

--- third_party/blink/renderer/core/css/css_property_equality.cc
+++ third_party/blink/renderer/core/css/css_property_equality.cc
@@ -1192,6 +1192,7 @@ bool CSSPropertyEquality::PropertiesEqual(const PropertyHandle& property,
     case CSSPropertyID::kPlaceSelf:
     case CSSPropertyID::kScrollMargin:
     case CSSPropertyID::kScrollPadding:
+    case CSSPropertyID::kScrollStart:
     case CSSPropertyID::kScrollTimeline:
     case CSSPropertyID::kTextDecoration:
     case CSSPropertyID::kTextEmphasis:
