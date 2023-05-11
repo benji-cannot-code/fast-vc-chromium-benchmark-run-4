18210505d44619b1b66dd77446ad8f3664786f8f
wenyu zhang <zhangwenyu@google.com>
2023-05-11 02:46:45 +0000

    hats: add Peripherals survey config
    Add battery life hats survey config and it will
    be triggered in user_session_manager.cc.
    
    This CL follows the example of the battery
    life survey (https://crrev.com/c/4450266).
    
    Bug: b/281683659
    Test: Build chrome with autoninja
    Change-Id: I9fea02a155d855e5b87fa1135aa77ca1a631b0f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4515297
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142552}

--- chrome/browser/ash/login/session/user_session_manager.cc
+++ chrome/browser/ash/login/session/user_session_manager.cc
@@ -2287,6 +2287,10 @@ void UserSessionManager::DoBrowserLaunchInternal(Profile* profile,
                  profile, kHatsBatteryLifeSurvey)) {
     hats_notification_controller_ =
         new HatsNotificationController(profile, kHatsBatteryLifeSurvey);
+  } else if (HatsNotificationController::ShouldShowSurveyToProfile(
+                 profile, kHatsPeripheralsSurvey)) {
+    hats_notification_controller_ =
+        new HatsNotificationController(profile, kHatsPeripheralsSurvey);
   }
 
   base::OnceClosure login_host_finalized_callback = base::BindOnce(
