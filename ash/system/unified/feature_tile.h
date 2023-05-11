944a101a31dae192f424fd1a0aa2692f8b1022ab
James Cook <jamescook@chromium.org>
2023-05-11 17:45:54 +0000

    Add ink drop ripple effect to quick settings FeatureTile buttons
    This requires a workaround with views::InkDropContainerView to ensure
    the ink drop paints above the button background.
    
    See go/cros-quick-settings-spec > Components > Tiles for the spec.
    
    Screenshot showing focus ring (unchanged):
    https://screenshot.googleplex.com/BpTgYnavk5zYipu
    
    Bug: b:266996218, b:277086033
    Change-Id: If20219840a0271218f6df307253c3c25adfb97e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521720
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142756}

--- ash/system/unified/feature_tile.h
+++ ash/system/unified/feature_tile.h
@@ -21,6 +21,7 @@ namespace views {
 class FlexLayoutView;
 class ImageButton;
 class ImageView;
+class InkDropContainerView;
 class Label;
 }  // namespace views
 
@@ -127,6 +128,10 @@ class ASH_EXPORT FeatureTile : public views::Button {
   friend class BluetoothFeaturePodControllerTest;
   friend class NotificationCounterViewTest;
 
+  // views::View:
+  void AddLayerToRegion(ui::Layer* layer, views::LayerRegion region) override;
+  void RemoveLayerFromRegions(ui::Layer* layer) override;
+
   // Returns the color id to use for the `icon_button_` and `drill_in_arrow_`
   // based on the tile's enabled and toggled state.
   ui::ColorId GetIconColorId() const;
@@ -139,6 +144,10 @@ class ASH_EXPORT FeatureTile : public views::Button {
 
   // Updates the color of `drill_in_arrow_` for better visibility.
   void UpdateDrillInArrowColor();
+
+  // Ensures the ink drop is painted above the button's background.
+  raw_ptr<views::InkDropContainerView, ExperimentalAsh> ink_drop_container_ =
+      nullptr;
 
   // The vector icon for the tile, if one is set.
   raw_ptr<const gfx::VectorIcon, ExperimentalAsh> vector_icon_ = nullptr;
