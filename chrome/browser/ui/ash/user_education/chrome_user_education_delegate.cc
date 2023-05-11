5860c31dfef24943d1e948f8bcbdc4abd3bd7999
David Black <dmblack@google.com>
2023-05-11 19:59:27 +0000

    Implement delegated help bubble creation.
    This CL fulfills help bubble creation through the user education
    delegate in the browser. The browser utilizes the help bubble factory
    registry provided by user education services in order to fulfill the
    request.
    
    Note that this CL also removes test parameterization that provided
    little value.
    
    Bug: b:279040829
    Change-Id: I83286f0f8ab89992fe8f4cf651f3cfd7d424d497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521947
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1142862}

--- chrome/browser/ui/ash/user_education/chrome_user_education_delegate.cc
+++ chrome/browser/ui/ash/user_education/chrome_user_education_delegate.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/user_education/chrome_user_education_delegate.h"
 
+#include "ash/user_education/user_education_types.h"
 #include "ash/user_education/user_education_util.h"
-#include "base/notreached.h"
+#include "base/check.h"
+#include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -15,9 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/user_education/browser_user_education_service.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "components/account_id/account_id.h"
+#include "components/user_education/common/help_bubble_factory_registry.h"
+#include "components/user_education/common/help_bubble_params.h"
 #include "components/user_education/common/tutorial_registry.h"
 #include "components/user_education/common/tutorial_service.h"
 #include "components/user_manager/user_manager.h"
+#include "ui/base/interaction/element_tracker.h"
 
 namespace {
 
@@ -42,7 +47,6 @@ ChromeUserEducationDelegate::ChromeUserEducationDelegate() {
 
 ChromeUserEducationDelegate::~ChromeUserEducationDelegate() = default;
 
-// TODO(http://b/279040829): Implement.
 std::unique_ptr<user_education::HelpBubble>
 ChromeUserEducationDelegate::CreateHelpBubble(
     const AccountId& account_id,
@@ -50,8 +54,32 @@ ChromeUserEducationDelegate::CreateHelpBubble(
     user_education::HelpBubbleParams help_bubble_params,
     ui::ElementIdentifier element_id,
     ui::ElementContext element_context) {
-  NOTIMPLEMENTED();
-  return nullptr;
+  Profile* profile = Profile::FromBrowserContext(
+      ash::BrowserContextHelper::Get()->GetBrowserContextByAccountId(
+          account_id));
+
+  // NOTE: User education in Ash is currently only supported for the primary
+  // user profile. This is a self-imposed restriction.
+  CHECK(IsPrimaryProfile(profile));
+
+  // If a tracked `element` cannot be found for the specified `element_id` and
+  // `element_context` pair, there's nothing to anchor a help bubble to.
+  ui::TrackedElement* const element =
+      ui::ElementTracker::GetElementTracker()->GetFirstMatchingElement(
+          element_id, element_context);
+  if (!element) {
+    return nullptr;
+  }
+
+  // Help bubble factories expect `help_bubble_id` to be provided via extended
+  // properties being as it is a ChromeOS specific platform construct.
+  help_bubble_params.extended_properties.values().Merge(std::move(
+      ash::user_education_util::CreateExtendedProperties(help_bubble_id)
+          .values()));
+
+  return UserEducationServiceFactory::GetForProfile(profile)
+      ->help_bubble_factory_registry()
+      .CreateHelpBubble(element, std::move(help_bubble_params));
 }
 
 void ChromeUserEducationDelegate::RegisterTutorial(
@@ -64,7 +92,7 @@ void ChromeUserEducationDelegate::RegisterTutorial(
 
   // NOTE: User education in Ash is currently only supported for the primary
   // user profile. This is a self-imposed restriction.
-  DCHECK(IsPrimaryProfile(profile));
+  CHECK(IsPrimaryProfile(profile));
 
   UserEducationServiceFactory::GetForProfile(profile)
       ->tutorial_registry()
@@ -84,7 +112,7 @@ void ChromeUserEducationDelegate::StartTutorial(
 
   // NOTE: User education in Ash is currently only supported for the primary
   // user profile. This is a self-imposed restriction.
-  DCHECK(IsPrimaryProfile(profile));
+  CHECK(IsPrimaryProfile(profile));
 
   UserEducationServiceFactory::GetForProfile(profile)
       ->tutorial_service()
