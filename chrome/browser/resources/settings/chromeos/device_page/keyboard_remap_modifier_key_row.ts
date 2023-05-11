030c7341c41be0d2fbe8a82b7f9b656403efdd95
Michael Checo <michaelcheco@google.com>
2023-05-11 17:51:39 +0000

    Settings Split: Fix meta string
    - external meta -> meta
    
    Bug: b/241965700
    Change-Id: I32103067eddd09640617568053690579bdf27943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521963
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1142763}

--- chrome/browser/resources/settings/chromeos/device_page/keyboard_remap_modifier_key_row.ts
+++ chrome/browser/resources/settings/chromeos/device_page/keyboard_remap_modifier_key_row.ts
@@ -135,7 +135,7 @@ export class KeyboardRemapModifierKeyRowElement extends
         return this.i18n('perDeviceKeyboardKeyCommand');
       }
       case MetaKey.kExternalMeta: {
-        return this.i18n('keyboardKeyMeta');
+        return this.i18n('perDeviceKeyboardKeyMeta');
       }
       // Launcher and Search key will display icon instead of text.
       case MetaKey.kLauncher:
