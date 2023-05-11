cf05eb746bf2993599505800fb210bf520035c34
François Degros <fdegros@chromium.org>
2023-05-11 07:39:54 +0000

    XfBulkPinningDialog: Tweak some spacing
    Bug: b:281927877
    Change-Id: Ic885f3fd2daa3b210c8cb73e9a9c53f8e0ea2baf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520151
    Reviewed-by: Marcello Salomao <msalomao@google.com>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142600}

--- ui/file_manager/file_manager/widgets/xf_bulk_pinning_dialog.ts
+++ ui/file_manager/file_manager/widgets/xf_bulk_pinning_dialog.ts
@@ -119,15 +119,15 @@ export class XfBulkPinningDialog extends XfBase {
   // Enables or disables the 'Continue' button according to the given state.
   set state(s: DialogState) {
     this.$offlineFooter_.style.display =
-        s === DialogState.OFFLINE ? 'flex' : 'none';
+        s === DialogState.OFFLINE ? 'initial' : 'none';
     this.$listingFooter_.style.display =
         s === DialogState.LISTING ? 'flex' : 'none';
     this.$errorFooter_.style.display =
-        s === DialogState.ERROR ? 'flex' : 'none';
+        s === DialogState.ERROR ? 'initial' : 'none';
     this.$notEnoughSpaceFooter_.style.display =
-        s === DialogState.NOT_ENOUGH_SPACE ? 'flex' : 'none';
+        s === DialogState.NOT_ENOUGH_SPACE ? 'initial' : 'none';
     this.$readyFooter_.style.display =
-        s === DialogState.READY ? 'flex' : 'none';
+        s === DialogState.READY ? 'initial' : 'none';
 
     this.$button_.disabled = s !== DialogState.READY;
   }
@@ -184,7 +184,6 @@ export class XfBulkPinningDialog extends XfBase {
         <div slot="body">
           <div class="description">
             ${str('BULK_PINNING_EXPLANATION')}
-            &ensp;
             <a href="_blank" @click="${this.onLearnMore}">
               ${str('LEARN_MORE_LABEL')}
             </a>
@@ -207,7 +206,6 @@ export class XfBulkPinningDialog extends XfBase {
           </div>
           <div id="not-enough-space-footer" class="error-footer">
             ${str('BULK_PINNING_NOT_ENOUGH_SPACE')}
-            &ensp;
             <a href="_blank" @click="${this.onViewStorage}">
               ${str('BULK_PINNING_VIEW_STORAGE')}
             </a>
@@ -295,7 +293,6 @@ export class XfBulkPinningDialog extends XfBase {
         border-radius: 0 0 12px 12px;
         border-top-style: none;
         color: var(--cros-sys-on_surface);
-        display: flex;
         padding: 16px;
       }
 
@@ -305,7 +302,6 @@ export class XfBulkPinningDialog extends XfBase {
         border-radius: 0 0 12px 12px;
         border-top-style: none;
         color: var(--cros-sys-on_error_container);
-        display: flex;
         padding: 16px;
       }
 
@@ -315,7 +311,6 @@ export class XfBulkPinningDialog extends XfBase {
         border-radius: 0 0 12px 12px;
         border-top-style: none;
         color: var(--cros-sys-on_surface_variant);
-        display: flex;
         padding: 16px;
       }
 
