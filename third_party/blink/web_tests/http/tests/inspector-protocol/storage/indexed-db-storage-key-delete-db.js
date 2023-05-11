f40e063e2fa877de0681f4ae1be31d75e233348b
Jaroslav Sevcik <jarin@chromium.org>
2023-05-11 16:49:35 +0000

    [test] Clean up after inspector database delete tests
    This should deflake the indexed-db-storage-key-request-database-names.js
    test.
    
    Bug: 1406017
    Change-Id: I217510dc03390ea46a0b70f293dff58d01f7df1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518431
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142685}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-delete-db.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-delete-db.js
@@ -59,6 +59,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     `);
 
     testRunner.log(`${accessAfterDelete}\n`);
+
+    // Clean up
+    await dp.IndexedDB.deleteDatabase(
+        {storageBucket, databaseName: 'test-database'});
   }
 
   const { dp, session } = await testRunner.startBlank(
