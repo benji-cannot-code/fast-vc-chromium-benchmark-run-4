ae928cb2516bae5547f9a21e4e99744e62d8fd98
François Degros <fdegros@chromium.org>
2023-05-11 03:09:22 +0000

    XfBulkPinningDialog: Tweak color of links
    Bug: b:281922054
    Change-Id: I82efa54800395c02d027ef612f7386db5a2f7171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520524
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142560}

--- ui/file_manager/file_manager/widgets/xf_bulk_pinning_dialog.ts
+++ ui/file_manager/file_manager/widgets/xf_bulk_pinning_dialog.ts
@@ -316,7 +316,7 @@ export class XfBulkPinningDialog extends XfBase {
       }
 
       a {
-        color: var(--cros-sys-on_error_container);
+        color: currentcolor;
       }
 
       files-spinner {
