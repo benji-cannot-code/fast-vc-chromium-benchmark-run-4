440a1bb88a3c5e8d1595c88aadc8011db8fdc51f
Benjamin Zielinski <bzielinski@google.com>
2023-05-11 19:15:42 +0000

    [FreezeCast] Add logging for mirroring pauses
    Add two new histograms, one to measure the duration of pauses, and
    another to measure the total count of pauses during a session.
    
    Bug: b:271442221
    Change-Id: I473792b704e0b676aa3c987c8194e210f77cd84f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4500505
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1142829}

--- chrome/browser/media/router/providers/cast/mirroring_activity.cc
+++ chrome/browser/media/router/providers/cast/mirroring_activity.cc
@@ -26,10 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/media/cast_mirroring_service_host_factory.h"
 #include "chrome/browser/media/router/data_decoder_util.h"
+#include "chrome/browser/media/router/discovery/access_code/access_code_cast_feature.h"
 #include "chrome/browser/media/router/media_router_feature.h"
 #include "chrome/browser/media/router/providers/cast/cast_activity_manager.h"
 #include "chrome/browser/media/router/providers/cast/cast_internal_message_util.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/access_code_cast/common/access_code_cast_metrics.h"
 #include "components/media_router/browser/media_router_debugger.h"
 #include "components/media_router/browser/mirroring_to_flinging_switcher.h"
 #include "components/media_router/common/discovery/media_sink_internal.h"
@@ -181,6 +184,26 @@ MirroringActivity::~MirroringActivity() {
   if (!did_start_mirroring_timestamp_) {
     return;
   }
+
+  // Record mirroring pause metrics.
+  if (mirroring_pause_timestamp_) {  // The session is ending while paused.
+    AccessCodeCastMetrics::RecordMirroringPauseDuration(
+        base::Time::Now() - mirroring_pause_timestamp_.value());
+  }
+  // We can only get a profile on the UI thread, so we must post a task to check
+  // if we should log certain metrics.
+  content::GetUIThreadTaskRunner({})->PostTask(
+      FROM_HERE,
+      base::BindOnce(
+          [](int pause_count) {
+            // Don't record pause count if the cast session cannot be paused.
+            if (pause_count > 0 ||
+                media_router::IsAccessCodeCastFreezeUiEnabled(
+                    ProfileManager::GetLastUsedProfile())) {
+              AccessCodeCastMetrics::RecordMirroringPauseCount(pause_count);
+            }
+          },
+          mirroring_pause_count_));
 
   auto cast_duration = base::Time::Now() - *did_start_mirroring_timestamp_;
   base::UmaHistogramLongTimes(kHistogramSessionLength, cast_duration);
@@ -360,6 +383,7 @@ void MirroringActivity::OnSourceChanged() {
   // The source changed, which means that a new capturer was created that is
   // now sending frames. Ensure the state is now PLAYING.
   media_status_->play_state = mojom::MediaStatus::PlayState::PLAYING;
+  OnMirroringResumed();
   NotifyMediaStatusObserver();
 
   content::GetUIThreadTaskRunner({})->PostTask(
@@ -372,6 +396,7 @@ void MirroringActivity::OnRemotingStateChanged(bool is_remoting) {
   // Transitions to/from remoting restart the capturer. Set the state to
   // playing.
   media_status_->play_state = mojom::MediaStatus::PlayState::PLAYING;
+  OnMirroringResumed();
   NotifyMediaStatusObserver();
 }
 
@@ -730,6 +755,11 @@ void MirroringActivity::Pause() {
 
 void MirroringActivity::SetPlayState(mojom::MediaStatus::PlayState play_state) {
   media_status_->play_state = play_state;
+  if (play_state == mojom::MediaStatus::PlayState::PLAYING) {
+    OnMirroringResumed();
+  } else if (play_state == mojom::MediaStatus::PlayState::PAUSED) {
+    OnMirroringPaused();
+  }
   NotifyMediaStatusObserver();
 }
 
@@ -737,6 +767,24 @@ void MirroringActivity::NotifyMediaStatusObserver() {
   if (media_status_observer_) {
     media_status_observer_->OnMediaStatusUpdated(media_status_.Clone());
   }
+}
+
+void MirroringActivity::OnMirroringPaused() {
+  // Do nothing if we are already paused.
+  if (mirroring_pause_timestamp_) {
+    return;
+  }
+  mirroring_pause_timestamp_ = base::Time::Now();
+  mirroring_pause_count_++;
+}
+
+void MirroringActivity::OnMirroringResumed() {
+  if (!mirroring_pause_timestamp_) {
+    return;
+  }
+  AccessCodeCastMetrics::RecordMirroringPauseDuration(
+      base::Time::Now() - mirroring_pause_timestamp_.value());
+  mirroring_pause_timestamp_.reset();
 }
 
 }  // namespace media_router
