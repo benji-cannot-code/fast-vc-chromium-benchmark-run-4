15b0f52fab10016b4f35f900644d8fded2ea89a9
Michael Thiessen <mthiesse@chromium.org>
2023-05-11 18:32:01 +0000

    Fix loop when selecting self with browser chooser dialog
    When an external browser had a verified app link for a URL that Chrome
    could handle, and you selected Chrome in the browser chooser dialog, we
    would re-navigate to that URL and show the chooser dialog again. Also,
    Chrome would show up twice in this case, because the Activity chooser
    Activity also shows non-default Activities in the dialog, so we would
    get both the fake Chrome entry and the real Chrome entry in the dialog.
    
    We now ensure when we load the URL in Chrome after the dialog we no
    longer allow external navigation on the redirect chain, and we avoid
    adding the fake Chrome entry if Chrome is already going to be shown in
    the chooser dialog.
    
    Bug: 1440679
    Change-Id: Id2d22ff92ed5f83044f21c457c5c9fc7272e5275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523229
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1142799}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -57,6 +57,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.PackageManagerUtils;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
@@ -1052,6 +1053,12 @@ public class UrlOverridingTest {
         CriteriaHelper.pollUiThread(() -> {
             Criteria.checkThat(mActivityTestRule.getActivity().getActivityTab().getUrl().getSpec(),
                     Matchers.is(mTestServer.getURL(HELLO_PAGE)));
+        });
+        ThreadUtils.runOnUiThreadBlocking(() -> {
+            Assert.assertTrue(
+                    RedirectHandlerTabHelper
+                            .getOrCreateHandlerFor(mActivityTestRule.getActivity().getActivityTab())
+                            .shouldNotOverrideUrlLoading());
         });
     }
 
