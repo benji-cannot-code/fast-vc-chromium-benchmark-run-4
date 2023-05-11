64b2e392abe3388ad7399d0b570d78494a48044b
Noam Rosenthal <nrosenthal@chromium.org>
2023-05-11 17:48:17 +0000

    Add UKM for framework versions
    This currently detects Next, Vue & Angular.
    
    See UKM privacy review
    https://docs.google.com/document/d/1eatNgsHthqcSaocbevAslky0kb7WqGP4_DT9HorGAQc/edit
    
    Bug: 1006274
    Change-Id: Ib5d574e888e7fa005c9296d967bc1cbe644d5d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507034
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142759}

---
+++ third_party/blink/web_tests/wpt_internal/framework-detection/resources/test-framework-version.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+async function test_framework_version(framework, expected_decimal, variant = "default") {
+  promise_test(async t => {
+    const [major, minor] = expected_decimal.split('.').map(n => +n);
+    const framework_version = `${framework}Version`;
+    const recorder = internals.initializeUKMRecorder();
+    await new Promise(resolve => window.addEventListener("load", resolve));
+    await new Promise(resolve => t.step_timeout(resolve));
+    const entries = recorder.getMetrics(
+      "Blink.JavaScriptFramework.Versions", [framework_version]);
+    console.log(JSON.stringify(entries));
+    assert_equals(entries.length, 1);
+    const metrics = entries[0];
+    assert_equals(metrics[framework_version], (major << 8) | minor);
+  }, `Test framework version for ${framework} (${variant})`);
+}
