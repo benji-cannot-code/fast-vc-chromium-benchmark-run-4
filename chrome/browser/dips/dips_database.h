ad6d71176f260b0c7daf536e192e54a0f08b57ea
Chris Fredrickson <cfredric@chromium.org>
2023-05-11 16:56:21 +0000

    Remove stale TODO
    This crbug has already been marked fixed.
    
    Bug: 1342228
    Change-Id: I7049ee0a08210fabd42ea4d92ea450885171e467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517778
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Joshua Hood <jdh@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142697}

--- chrome/browser/dips/dips_database.h
+++ chrome/browser/dips/dips_database.h
@@ -19,9 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/meta_table.h"
 #include "sql/statement.h"
 
-// TODO(crbug.com/1342228): This is currently in-memory only. Add support for a
-// persistent SQLite database to be used for non-OTR profiles.
-//
 // Encapsulates an SQL database that holds DIPS info.
 class DIPSDatabase {
  public:
