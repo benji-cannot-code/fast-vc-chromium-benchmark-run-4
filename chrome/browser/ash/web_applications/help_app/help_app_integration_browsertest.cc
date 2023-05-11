286dd7fd3831b1d33af3369061fd804eb97e9ab9
Chris Fredrickson <cfredric@chromium.org>
2023-05-11 04:50:42 +0000

    Replace ExecuteScript with ExecJs
    ExecuteScript is deprecated in favor of ExecJs. ExecJs is more
    flexible and has better error-handling. If the script's completion
    value is *not* a Promise, then ExecJs is a drop-in replacement for
    ExecuteScript. Otherwise, the new equivalent is to use ExecJs with the
    EXECUTE_SCRIPT_NO_RESOLVE_PROMISES option.
    
    This CL was uploaded by git cl split.
    
    R=callistus@google.com
    
    Bug: 1157718
    Change-Id: If7f0da4b10aea3ebf8d12d7d05d310d37484c829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518661
    Reviewed-by: Callistus Tan <callistus@google.com>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142574}

--- chrome/browser/ash/web_applications/help_app/help_app_integration_browsertest.cc
+++ chrome/browser/ash/web_applications/help_app/help_app_integration_browsertest.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -346,7 +347,7 @@ IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest,
       return true;
     })();
   )";
-  // Use ExecuteScript instead of EvalJsInAppFrame because the script needs to
+  // Use EvalJs instead of EvalJsInAppFrame because the script needs to
   // run in the same world as the page's code.
   EXPECT_EQ(true,
             content::EvalJs(
@@ -404,7 +405,7 @@ IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest,
       return true;
     })();
   )";
-  // Use ExecuteScript instead of EvalJsInAppFrame because the script needs to
+  // Use EvalJs instead of EvalJsInAppFrame because the script needs to
   // run in the same world as the page's code.
   EXPECT_EQ(true,
             content::EvalJs(
@@ -522,10 +523,10 @@ IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest, HelpAppV2ShowParentalControls) {
       await window.customLaunchData.delegate.showParentalControls();
     })();
   )";
-  // Trigger the script, then wait for settings to open. Use ExecuteScript
+  // Trigger the script, then wait for settings to open. Use ExecJs
   // instead of EvalJsInAppFrame because the script needs to run in the same
   // world as the page's code.
-  EXPECT_TRUE(content::ExecuteScript(
+  EXPECT_TRUE(content::ExecJs(
       SandboxedWebUiAppTestBase::GetAppFrame(web_contents), kScript));
   navigation_observer.Wait();
 
@@ -564,11 +565,12 @@ IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest,
     })();
   )";
   // Trigger the script, then wait for the URL to open in a new tab. Use
-  // ExecuteScript instead of EvalJsInAppFrame because the script needs to run
+  // ExecJs instead of EvalJsInAppFrame because the script needs to run
   // in the same world as the page's code.
-  EXPECT_TRUE(content::ExecuteScript(
-      SandboxedWebUiAppTestBase::GetAppFrame(web_contents),
-      content::JsReplace(kScript, test_url)));
+  EXPECT_TRUE(
+      content::ExecJs(SandboxedWebUiAppTestBase::GetAppFrame(web_contents),
+                      content::JsReplace(kScript, test_url),
+                      content::EXECUTE_SCRIPT_NO_RESOLVE_PROMISES));
   navigation_observer.Wait();
 
   // There should still be two browser windows.
