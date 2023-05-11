1ed3bbcb427c752bc1e95ee3dc813395b9fe9a4d
Daniel Cheng <dcheng@chromium.org>
2023-05-11 22:25:22 +0000

    Remove gtest_prod_util.h from //base/memory/ref_counted.h.
    Change-Id: I222595f1aac5646c7764e5e9732b2c15d791a255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517895
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142943}

--- base/memory/ref_counted.h
+++ base/memory/ref_counted.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/compiler_specific.h"
 #include "base/dcheck_is_on.h"
-// TODO(dcheng): Remove this separately.
-#include "base/gtest_prod_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
 #include "base/template_util.h"
