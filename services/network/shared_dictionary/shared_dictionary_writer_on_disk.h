d93122b96a1d16e402a45f362c166fe3e3457663
Tsuyoshi Horo <horo@chromium.org>
2023-05-11 22:10:28 +0000

    Fix potential issue in SharedDictionaryWriterOnDisk
    There is a potential issue in SharedDictionaryWriterOnDisk that
    when Append() was called multiple times, and then OnEntry() is called,
    and if Entry::WriteData() synchronously succeeds, MaybeFinish() may
    finishes the operation when the first buffer is written.
    
    This is because MaybeFinish() checks `pending_write_buffers_.empty()`,
    but `pending_write_buffers_` is cleared in OnEntry() by being moved to
    `buffers`.
    
    This CL solves this issue by using deque for `pending_write_buffers_`.
    
    Note: Entry::WriteData() does not succeed synchronously when
    CACHE_BACKEND_SIMPLE is used, so this issue does not occur in
    production.
    
    Bug: 1413922
    Change-Id: I424b4f324b8c20d6f5074a3b62321fc2d97abbf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520035
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142934}

--- services/network/shared_dictionary/shared_dictionary_writer_on_disk.h
+++ services/network/shared_dictionary/shared_dictionary_writer_on_disk.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_NETWORK_SHARED_DICTIONARY_SHARED_DICTIONARY_WRITER_ON_DISK_H_
 #define SERVICES_NETWORK_SHARED_DICTIONARY_SHARED_DICTIONARY_WRITER_ON_DISK_H_
 
+#include <deque>
 #include <set>
 #include <string>
-#include <vector>
 
 #include "base/component_export.h"
 #include "base/functional/callback.h"
@@ -81,7 +81,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) SharedDictionaryWriterOnDisk
 
   State state_ = State::kBeforeInitialize;
   disk_cache::ScopedEntryPtr entry_;
-  std::vector<scoped_refptr<net::StringIOBuffer>> pending_write_buffers_;
+  std::deque<scoped_refptr<net::StringIOBuffer>> pending_write_buffers_;
 
   int offset_ = 0;
   bool finish_called_ = false;
