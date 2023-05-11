ebfafd138eec8cacaea0f49783eef1cde99d6d96
Mark Schillaci <mschillaci@google.com>
2023-05-11 17:41:07 +0000

    [Auto-disable Accessibility] Use AccessibilityState in views
    This CL continues work on refactoring the accessibility state management
    in Clank as part of the work to enable further performance improvements.
    
    With this CL we update some views in Clank UI to use the new
    AccessibilityState, which should be the source-of-truth going forward,
    rather than fetching the information from system services directly.
    
    
    AX-Relnotes: N/A
    Bug: 1430202, b/265493191
    Change-Id: I4b70b755b9a29ac3277c97986428939a707d8a46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4515239
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142749}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSaveFlowCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSaveFlowCoordinator.java
@@ -8,7 +8,6 @@ package org.chromium.chrome.browser.bookmarks;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.accessibility.AccessibilityManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,6 +30,7 @@ import org.chromium.components.browser_ui.bottomsheet.EmptyBottomSheetObserver;
 import org.chromium.components.commerce.core.ShoppingService;
 import org.chromium.components.feature_engagement.FeatureConstants;
 import org.chromium.components.power_bookmarks.PowerBookmarkMeta;
+import org.chromium.ui.accessibility.AccessibilityState;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
@@ -119,9 +119,7 @@ public class BookmarkSaveFlowCoordinator {
         boolean shown =
                 mBottomSheetController.requestShowContent(mBottomSheetContent, /* animate= */ true);
 
-        AccessibilityManager am =
-                (AccessibilityManager) mContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
-        if (!am.isTouchExplorationEnabled()) {
+        if (!AccessibilityState.isTouchExplorationEnabled()) {
             setupAutodismiss();
         }
 
