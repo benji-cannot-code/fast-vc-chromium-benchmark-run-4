43ea387ef8510e58ec488b44febf10857588f2bc
David Awogbemila <awogbemila@chromium.org>
2023-05-11 22:53:17 +0000

    [scroll-start] Parse scroll-start-target longhands
    scroll-start-target is included in the scroll-snap-2 spec[1].
    
    scroll-start-target, when set on the child of a scroller, is one of the
    factors that determines the starting scroll position of that scroller.
    
    [1] https://drafts.csswg.org/css-scroll-snap-2/#scroll-start
    
    Bug: 1439807
    Change-Id: I5cf33610d36cd73b3c48fc1e206790f941a0ba8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514676
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142961}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -6561,6 +6561,10 @@ CSSValue* ConsumeScrollStart(CSSParserTokenRange& range,
                                 CSSPrimitiveValue::ValueRange::kNonNegative);
 }
 
+CSSValue* ConsumeScrollStartTarget(CSSParserTokenRange& range) {
+  return ConsumeIdent<CSSValueID::kAuto, CSSValueID::kNone>(range);
+}
+
 CSSValue* ConsumeOffsetPath(CSSParserTokenRange& range,
                             const CSSParserContext& context) {
   CSSValue* offset_path = nullptr;
