2d86483f6da1fe94eb8aaa9294115eff2d8b238a
Shakti Sahu <shaktisahu@chromium.org>
2023-05-12 13:38:57 +0000

    [CSC] Refresh companion for same page navigations
    Bug: b/280442180
    Change-Id: Ie2b810356ce600743ca1f8b8ca40eb693dcc7623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4510545
    Auto-Submit: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143228}

--- chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
+++ chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/companion/core/msbb_delegate.h"
 #include "chrome/browser/ui/side_panel/side_panel_enums.h"
 #include "components/lens/buildflags.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -60,7 +61,8 @@ class CompanionPageHandler : public side_panel::mojom::CompanionPageHandler,
   void OnCqJumptagClicked(const std::string& text_directive) override;
 
   // content::WebContentsObserver:
-  void PrimaryPageChanged(content::Page& page) override;
+  void DidFinishNavigation(
+      content::NavigationHandle* navigation_handle) override;
 
   // Informs the page handler that a new text query to initialize / reload the
   // page with was sent from client.
@@ -97,6 +99,9 @@ class CompanionPageHandler : public side_panel::mojom::CompanionPageHandler,
 
   // Logs metrics for companion page. Reset when there is a new navigation.
   std::unique_ptr<CompanionMetricsLogger> metrics_logger_;
+
+  // The current URL of the main frame.
+  GURL page_url_;
 
   base::WeakPtrFactory<CompanionPageHandler> weak_ptr_factory_{this};
 };
