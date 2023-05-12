5a2fbd0dfd502adf0426a666b9ec2790380be8d6
Federica Germinario <fedegermi@google.com>
2023-05-12 14:40:05 +0000

    [ios] Refactor resetContentView inside sideSwipeController
    resetContentView method is only called during swipe, this check doesn't
    need to be performed inside BVC but it can be moved inside
    SideSwipeController.
    
    Change-Id: Idcb55150e85c2f2b01eacc836405ada320fb3615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523345
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143263}

--- ios/chrome/browser/ui/side_swipe/side_swipe_controller.mm
+++ ios/chrome/browser/ui/side_swipe/side_swipe_controller.mm
@@ -647,6 +647,9 @@ class SideSwipeControllerBrowserRemover : public BrowserObserver {
 }
 
 - (void)resetContentView {
+  if (!_inSwipe) {
+    return;
+  }
   CGRect frame = [_swipeDelegate sideSwipeContentView].frame;
   frame.origin.x = 0;
   [_swipeDelegate sideSwipeContentView].frame = frame;
