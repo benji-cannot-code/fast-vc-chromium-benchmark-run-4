f9418fb97aa4933a90cdf0e296cba9d06459a3cb
Daniil Sakhapov <sakhapov@chromium.org>
2023-05-12 10:16:57 +0000

    Implement support for motion path basic-shape animation
    Change-Id: Icb5bf1640d1b49f9af2def97a51ac65e783e0718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517531
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1143172}

--- third_party/blink/renderer/core/animation/css_basic_shape_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_basic_shape_interpolation_type.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/basic_shapes.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style/shape_clip_path_operation.h"
+#include "third_party/blink/renderer/core/style/shape_offset_path_operation.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
@@ -35,6 +36,23 @@ const BasicShape* GetBasicShape(const CSSProperty& property,
       if (style.ShapeOutside()->CssBox() != CSSBoxType::kMissing)
         return nullptr;
       return style.ShapeOutside()->Shape();
+    case CSSPropertyID::kOffsetPath: {
+      auto* offset_path_operation =
+          DynamicTo<ShapeOffsetPathOperation>(style.OffsetPath());
+      if (!offset_path_operation) {
+        return nullptr;
+      }
+      const auto& shape = offset_path_operation->GetBasicShape();
+
+      // Path and Ray shapes are handled by PathInterpolationType and
+      // RayInterpolationType.
+      if (shape.GetType() == BasicShape::kStylePathType ||
+          shape.GetType() == BasicShape::kStyleRayType) {
+        return nullptr;
+      }
+
+      return &shape;
+    }
     case CSSPropertyID::kClipPath: {
       auto* clip_path_operation =
           DynamicTo<ShapeClipPathOperation>(style.ClipPath());
@@ -137,8 +155,12 @@ InterpolationValue CSSBasicShapeInterpolationType::MaybeConvertValue(
     return basic_shape_interpolation_functions::MaybeConvertCSSValue(value);
 
   const auto& list = To<CSSValueList>(value);
-  if (list.length() != 1)
+  // Path and Ray shapes are handled by PathInterpolationType and
+  // RayInterpolationType.
+  if (!list.First().IsBasicShapeValue() || list.First().IsRayValue() ||
+      list.First().IsPathValue()) {
     return nullptr;
+  }
   return basic_shape_interpolation_functions::MaybeConvertCSSValue(
       list.Item(0));
 }
@@ -189,6 +211,11 @@ void CSSBasicShapeInterpolationType::ApplyStandardPropertyValue(
     case CSSPropertyID::kShapeOutside:
       state.StyleBuilder().SetShapeOutside(MakeGarbageCollected<ShapeValue>(
           std::move(shape), CSSBoxType::kMissing));
+      break;
+    case CSSPropertyID::kOffsetPath:
+      // TODO(sakhapov): handle coord box.
+      state.StyleBuilder().SetOffsetPath(ShapeOffsetPathOperation::Create(
+          std::move(shape), CoordBox::kBorderBox));
       break;
     case CSSPropertyID::kClipPath:
       state.StyleBuilder().SetClipPath(
