29583ca2a72a3dff89dd0dff98c8a174833968ad
Bastian Kersting <bkersting@google.com>
2023-05-12 08:31:41 +0000

    Revert "FSA: Remove disabling of BFCache on use of any local file"
    This reverts commit 1b4ea1775e0b1a4439e2a13b907d0e173403d7bd.
    
    Reason for revert: This change introduced an error on linux-bfcache-rel: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/46927/overview
    
    Original change's description:
    > FSA: Remove disabling of BFCache on use of any local file
    >
    > BFCache is currently disabled when using the File System Access API to
    > interact with _any_ local files in any way. This was added back in
    > https://crrev.com/c/1837813 due to its use of
    > RenderFrameHost::IsCurrent, which at the time returned true
    > for iframes
    >
    > However, the behavior of RenderFrameHost::IsCurrent was changed in
    > https://crrev.com/c/2060374 such that it's no longer necessary to
    > disable BFCache in this specific case.
    > See https://crbug.com/1259861 for more details.
    >
    > Note that BFCache is still disabled when permission to a file is
    > requested from the File System Access API.
    > This behavior was added in https://crrev.com/c/3217485 and requires
    > further work to address
    >
    > Bug: 1241174, 1259861
    > Change-Id: Id0f00c9a801cb38331a4584682fd26bc18beab6f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169116
    > Commit-Queue: Austin Sullivan <asully@chromium.org>
    > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1143092}
    
    Bug: 1241174, 1259861
    Change-Id: I12df0effd5436a369f634f06c6c6137472f84a45
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517538
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Owners-Override: Bastian Kersting <bkersting@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1143135}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -1622,6 +1622,9 @@ DisableForRenderFrameHostReasonToProtocol(
               ContentFileChooser;
         case BackForwardCacheDisable::DisabledReasonId::kSerial:
           return Page::BackForwardCacheNotRestoredReasonEnum::ContentSerial;
+        case BackForwardCacheDisable::DisabledReasonId::kFileSystemAccess:
+          return Page::BackForwardCacheNotRestoredReasonEnum::
+              ContentFileSystemAccess;
         case BackForwardCacheDisable::DisabledReasonId::
             kMediaDevicesDispatcherHost:
           return Page::BackForwardCacheNotRestoredReasonEnum::
