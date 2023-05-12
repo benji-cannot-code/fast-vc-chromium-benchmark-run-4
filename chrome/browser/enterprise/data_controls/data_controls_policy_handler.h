be962e4d19dfe260f1fcb7485676e0d43deb74c1
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-05-12 16:12:37 +0000

    Add DataControlsPolicyHandler
    This refactors out the cloud-only bits into a separate class to reuse
    them for Data Controls.
    
    Bug: b/281575892
    Change-Id: I3da1c1132f563817b804026272659d7166044cdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4516898
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143302}

---
+++ chrome/browser/enterprise/data_controls/data_controls_policy_handler.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_POLICY_HANDLER_H_
+#define CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_POLICY_HANDLER_H_
+
+#include "components/policy/core/browser/configuration_policy_handler.h"
+
+class PrefValueMap;
+
+namespace data_controls {
+
+class DataControlsPolicyHandler : public policy::CloudOnlyPolicyHandler {
+ public:
+  DataControlsPolicyHandler(const char* policy_name,
+                            const char* pref_path,
+                            policy::Schema schema);
+  ~DataControlsPolicyHandler() override;
+
+  void ApplyPolicySettings(const policy::PolicyMap& policies,
+                           PrefValueMap* prefs) override;
+
+ private:
+  const char* pref_path_;
+};
+
+}  // namespace data_controls
+
+#endif  // CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_POLICY_HANDLER_H_
