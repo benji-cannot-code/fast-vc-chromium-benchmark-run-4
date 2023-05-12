0b968830181c6d20e06b1ef07e89481a598e7a39
Mikel Astiz <mastiz@chromium.org>
2023-05-12 15:55:19 +0000

    [sync] Ifdef Ash-specific error case
    The reported UI string, IDS_SIGNED_IN_WITH_SYNC_STOPPED_VIA_DASHBOARD,
    is only relevant in ChromeOS Ash. The code and the string itself can
    be restricted to this platform.
    
    Change-Id: I936d82e53b835a337d96d1fdd114144ee531a889
    Bug: 1443446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4525902
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143292}

--- chrome/browser/sync/sync_ui_util_unittest.cc
+++ chrome/browser/sync/sync_ui_util_unittest.cc
@@ -63,7 +63,9 @@ enum DistinctState {
   STATUS_CASE_TRUSTED_VAULT_RECOVERABILITY_ERROR,
   STATUS_CASE_SYNCED,
   STATUS_CASE_SYNC_DISABLED_BY_POLICY,
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   STATUS_CASE_SYNC_RESET_FROM_DASHBOARD,
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   NUMBER_OF_STATUS_CASES
 };
 
@@ -195,6 +197,7 @@ SyncStatusLabels SetUpDistinctCase(
               IDS_SIGNED_IN_WITH_SYNC_DISABLED_BY_POLICY,
               IDS_SETTINGS_EMPTY_STRING, SyncStatusActionType::kNoAction};
     }
+#if BUILDFLAG(IS_CHROMEOS_ASH)
     case STATUS_CASE_SYNC_RESET_FROM_DASHBOARD: {
       // Note: On desktop, if there is a primary account, then
       // DISABLE_REASON_USER_CHOICE can only occur if Sync was reset from the
@@ -210,6 +213,7 @@ SyncStatusLabels SetUpDistinctCase(
               IDS_SIGNED_IN_WITH_SYNC_STOPPED_VIA_DASHBOARD,
               IDS_SETTINGS_EMPTY_STRING, SyncStatusActionType::kNoAction};
     }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
     case NUMBER_OF_STATUS_CASES:
       NOTREACHED();
   }
