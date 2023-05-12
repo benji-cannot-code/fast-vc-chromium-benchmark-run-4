1f00f72d1a712de4eb774c6f18adcfa7c792145e
Sida Zhu <zhusida@bytedance.com>
2023-05-12 21:04:48 +0000

    GPU: Fix undefined value in chrome://gpu for skia graphite
    Bug: none
    Change-Id: I3839d418c2a10c3098ab8f627e57007611692aba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4525777
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143492}

--- content/browser/resources/gpu/info_view.js
+++ content/browser/resources/gpu/info_view.js
@@ -293,6 +293,7 @@ export class InfoViewElement extends CustomElement {
       'direct_rendering_display_compositor':
           'Direct Rendering Display Compositor',
       'webgpu': 'WebGPU',
+      'skia_graphite': 'Skia Graphite',
     };
 
     const statusMap = {
