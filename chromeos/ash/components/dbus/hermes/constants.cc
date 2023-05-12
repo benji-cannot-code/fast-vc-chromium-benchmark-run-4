20191160302a003bf105ae68fa5bf3660d30f4fb
Nikhil Nayunigari <nikhilcn@google.com>
2023-05-12 17:44:33 +0000

    [CrOS Cellular] Handle the case where carrier call timesout
    When the call to the carrier to download the profile timesout, we get
    the response "org.freedesktop.DBus.Error.NoReply" and that translates to
    ErrorUnknown.
    
    After interacting with the platform team, I've learned that their
    timeout is four minutes where as we timeout after two minutes. Updated
    the timeout to be inline with the platform configuration and also mapped
    no reply to Hermes No response code.
    
    Bug: b/244447732
    Change-Id: I1978e07e50990f923680c9166b16408c931ead32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514977
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143373}

--- chromeos/ash/components/dbus/hermes/constants.cc
+++ chromeos/ash/components/dbus/hermes/constants.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::hermes_constants {
 
-// 2min * 60s/min * 1000ms/s.
-int kHermesNetworkOperationTimeoutMs = 2 * 60 * 1000;
+// 4min * 60s/min * 1000ms/s.
+int kHermesNetworkOperationTimeoutMs = 4 * 60 * 1000;
+
+// 2min * 60s/min * 1000ms/s
+int kHermesOperationTimeoutMs = 2 * 60 * 1000;
 
 }  // namespace ash::hermes_constants
