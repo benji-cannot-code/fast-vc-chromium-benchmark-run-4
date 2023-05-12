4ba93853ff09c70c91a68f845a8af48def4d5018
David Yeung <dayeung@chromium.org>
2023-05-12 01:11:10 +0000

    [CR2023] Update the textfield border color only with the default border.
    For ChromeRefresh2023, Textfields should not have a border outline when disabled or in read only state.
    
    Originally, this was implemented by calling UpdateBorder when disabled or in read only
    See: https://chromium-review.googlesource.com/c/chromium/src/+/4406814
    
    This introduced a bug where we were creating a border for Textfields that did not want a border. Some UIs would call SetBorder and pass an empty border.
    
    This is because Textfield::UpdateBorder() will override any existing borders originally called by Textfield::SetBorder.
    
    We rolled back the implementation and this CL introduces a new fix.
    
    Keep a flag that tracks if we're still using the default border. If so, update the border color for Disabled and ReadOnly states.
    
    Bug: 1410804
    Change-Id: I6e073e445729134b5fbe5929682e5bc02d80ff80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518133
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143017}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -236,7 +236,7 @@ Textfield::Textfield()
   cursor_view->GetViewAccessibility().OverrideIsIgnored(true);
   cursor_view_ = AddChildView(std::move(cursor_view));
   GetRenderText()->SetFontList(GetDefaultFontList());
-  UpdateBorder();
+  UpdateDefaultBorder();
   SetFocusBehavior(FocusBehavior::ALWAYS);
   views::InstallRoundRectHighlightPathGenerator(this, gfx::Insets(),
                                                 GetCornerRadius());
@@ -295,6 +295,8 @@ void Textfield::SetReadOnly(bool read_only) {
     SetColor(GetTextColor());
     UpdateBackgroundColor();
   }
+
+  UpdateDefaultBorder();
   OnPropertyChanged(&read_only_, kPropertyEffectsPaint);
 }
 
@@ -559,7 +561,7 @@ void Textfield::SetInvalid(bool invalid) {
   if (invalid == invalid_)
     return;
   invalid_ = invalid;
-  UpdateBorder();
+  UpdateDefaultBorder();
   if (FocusRing::Get(this))
     FocusRing::Get(this)->SetInvalid(invalid);
   OnPropertyChanged(&invalid_, kPropertyEffectsNone);
@@ -575,7 +577,7 @@ void Textfield::SetObscuredGlyphSpacing(int spacing) {
 
 void Textfield::SetExtraInsets(const gfx::Insets& insets) {
   extra_insets_ = insets;
-  UpdateBorder();
+  UpdateDefaultBorder();
 }
 
 void Textfield::FitToLocalBounds() {
@@ -598,6 +600,13 @@ void Textfield::FitToLocalBounds() {
   bounds.set_x(GetMirroredXForRect(bounds));
   GetRenderText()->SetDisplayRect(bounds);
   UpdateAfterChange(TextChangeType::kNone, true);
+}
+
+bool Textfield::GetUseDefaultBorder() const {
+  return use_default_border_;
+}
+void Textfield::SetUseDefaultBorder(bool use_default_border) {
+  use_default_border_ = use_default_border;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -626,6 +635,7 @@ gfx::Size Textfield::GetMinimumSize() const {
 void Textfield::SetBorder(std::unique_ptr<Border> b) {
   FocusRing::Remove(this);
   View::SetBorder(std::move(b));
+  use_default_border_ = false;
 }
 
 ui::Cursor Textfield::GetCursor(const ui::MouseEvent& event) {
@@ -2508,7 +2518,12 @@ void Textfield::UpdateBackgroundColor() {
   OnPropertyChanged(&model_ + kTextfieldBackgroundColor, kPropertyEffectsPaint);
 }
 
-void Textfield::UpdateBorder() {
+void Textfield::UpdateDefaultBorder() {
+  // Only update the border if SetBorder() has not been called. This is to avoid
+  // overriding any custom borders.
+  if (!use_default_border_) {
+    return;
+  }
   auto border = std::make_unique<views::FocusableBorder>();
   const LayoutProvider* provider = LayoutProvider::Get();
   border->SetColorId(ui::kColorTextfieldOutline);
@@ -2831,6 +2846,7 @@ void Textfield::OnCursorBlinkTimerFired() {
 void Textfield::OnEnabledChanged() {
   if (GetInputMethod())
     GetInputMethod()->OnTextInputTypeChanged(this);
+  UpdateDefaultBorder();
 }
 
 void Textfield::DropDraggedText(
