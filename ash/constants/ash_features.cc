c6e4381a4acc046f2c38b1f7342e08cdbee44bf1
Guoxing Zhao <charleszhao@chromium.org>
2023-05-12 04:47:03 +0000

    [VC] add kFeatureManagementVideoConference
    This feature will be passed from chromeos.
    
    Bug=b:277140069
    TEST="N/A"
    
    Change-Id: I99821eceb5400abb1602467d9c3fb292190cb105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4515319
    Commit-Queue: Charles Zhao <charleszhao@chromium.org>
    Reviewed-by: CJ Huang <chenjih@google.com>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143078}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2252,6 +2252,11 @@ BASE_FEATURE(kUserActivityPrediction,
 BASE_FEATURE(kVideoConference,
              "VideoConference",
              base::FEATURE_DISABLED_BY_DEFAULT);
+// Restricts the video conference feature to the intended
+// target population,
+BASE_FEATURE(kFeatureManagementVideoConference,
+             "FeatureManagementVideoConference",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Controls whether the vc background replace is enabled.
 BASE_FEATURE(kVcBackgroundReplace,
@@ -3494,7 +3499,8 @@ bool IsUserEducationEnabled() {
 }
 
 bool IsVideoConferenceEnabled() {
-  return base::FeatureList::IsEnabled(kVideoConference) &&
+  return (base::FeatureList::IsEnabled(kVideoConference) ||
+          base::FeatureList::IsEnabled(kFeatureManagementVideoConference)) &&
          switches::IsCameraEffectsSupportedByHardware();
 }
 
