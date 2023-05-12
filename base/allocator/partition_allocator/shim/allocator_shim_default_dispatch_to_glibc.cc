13a56996929d9fb3fd841c809741a7a25903f157
Bartek Nowierski <bartekn@chromium.org>
2023-05-12 06:53:24 +0000

    [PA] Improve formatting (7 of N)
    Change-Id: I3f301eb6d0db40bd2add873dd79fa5fbc95d0186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4525815
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143107}

--- base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_glibc.cc
+++ base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_glibc.cc
@@ -37,8 +37,9 @@ constexpr size_t kMaxAllowedSize = std::numeric_limits<int>::max() - (1 << 12);
 void* GlibcMalloc(const AllocatorDispatch*, size_t size, void* context) {
   // Cannot force glibc's malloc() to crash when a large size is requested, do
   // it in the shim instead.
-  if (PA_UNLIKELY(size >= kMaxAllowedSize))
+  if (PA_UNLIKELY(size >= kMaxAllowedSize)) {
     base::TerminateBecauseOutOfMemory(size);
+  }
 
   return __libc_malloc(size);
 }
@@ -46,8 +47,9 @@ void* GlibcMalloc(const AllocatorDispatch*, size_t size, void* context) {
 void* GlibcUncheckedMalloc(const AllocatorDispatch*,
                            size_t size,
                            void* context) {
-  if (PA_UNLIKELY(size >= kMaxAllowedSize))
+  if (PA_UNLIKELY(size >= kMaxAllowedSize)) {
     return nullptr;
+  }
 
   return __libc_malloc(size);
 }
@@ -57,8 +59,9 @@ void* GlibcCalloc(const AllocatorDispatch*,
                   size_t size,
                   void* context) {
   const auto total = partition_alloc::internal::base::CheckMul(n, size);
-  if (PA_UNLIKELY(!total.IsValid() || total.ValueOrDie() >= kMaxAllowedSize))
+  if (PA_UNLIKELY(!total.IsValid() || total.ValueOrDie() >= kMaxAllowedSize)) {
     base::TerminateBecauseOutOfMemory(size * n);
+  }
 
   return __libc_calloc(n, size);
 }
@@ -67,8 +70,9 @@ void* GlibcRealloc(const AllocatorDispatch*,
                    void* address,
                    size_t size,
                    void* context) {
-  if (PA_UNLIKELY(size >= kMaxAllowedSize))
+  if (PA_UNLIKELY(size >= kMaxAllowedSize)) {
     base::TerminateBecauseOutOfMemory(size);
+  }
 
   return __libc_realloc(address, size);
 }
@@ -77,8 +81,9 @@ void* GlibcMemalign(const AllocatorDispatch*,
                     size_t alignment,
                     size_t size,
                     void* context) {
-  if (PA_UNLIKELY(size >= kMaxAllowedSize))
+  if (PA_UNLIKELY(size >= kMaxAllowedSize)) {
     base::TerminateBecauseOutOfMemory(size);
+  }
 
   return __libc_memalign(alignment, size);
 }
--- base/allocator/partition_allocator/shim/allocator_shim_unittest.cc
+++ base/allocator/partition_allocator/shim/allocator_shim_unittest.cc
@@ -66,16 +66,18 @@ class AllocatorShimTest : public testing::Test {
   static void* MockAlloc(const AllocatorDispatch* self,
                          size_t size,
                          void* context) {
-    if (instance_ && size < MaxSizeTracked())
+    if (instance_ && size < MaxSizeTracked()) {
       ++(instance_->allocs_intercepted_by_size[size]);
+    }
     return self->next->alloc_function(self->next, size, context);
   }
 
   static void* MockAllocUnchecked(const AllocatorDispatch* self,
                                   size_t size,
                                   void* context) {
-    if (instance_ && size < MaxSizeTracked())
+    if (instance_ && size < MaxSizeTracked()) {
       ++(instance_->allocs_intercepted_by_size[size]);
+    }
     return self->next->alloc_unchecked_function(self->next, size, context);
   }
 
@@ -84,8 +86,9 @@ class AllocatorShimTest : public testing::Test {
                                  size_t size,
                                  void* context) {
     const size_t real_size = n * size;
-    if (instance_ && real_size < MaxSizeTracked())
+    if (instance_ && real_size < MaxSizeTracked()) {
       ++(instance_->zero_allocs_intercepted_by_size[real_size]);
+    }
     return self->next->alloc_zero_initialized_function(self->next, n, size,
                                                        context);
   }
@@ -95,10 +98,12 @@ class AllocatorShimTest : public testing::Test {
                                 size_t size,
                                 void* context) {
     if (instance_) {
-      if (size < MaxSizeTracked())
+      if (size < MaxSizeTracked()) {
         ++(instance_->aligned_allocs_intercepted_by_size[size]);
-      if (alignment < MaxSizeTracked())
+      }
+      if (alignment < MaxSizeTracked()) {
         ++(instance_->aligned_allocs_intercepted_by_alignment[alignment]);
+      }
     }
     return self->next->alloc_aligned_function(self->next, alignment, size,
                                               context);
@@ -121,8 +126,9 @@ class AllocatorShimTest : public testing::Test {
         return address;
       }
 
-      if (size < MaxSizeTracked())
+      if (size < MaxSizeTracked()) {
         ++(instance_->reallocs_intercepted_by_size[size]);
+      }
       ++instance_->reallocs_intercepted_by_addr[Hash(address)];
     }
     return self->next->realloc_function(self->next, address, size, context);
@@ -141,8 +147,9 @@ class AllocatorShimTest : public testing::Test {
                                     void* address,
                                     void* context) {
     // Special testing values for GetSizeEstimate() interception.
-    if (address == kTestSizeEstimateAddress)
+    if (address == kTestSizeEstimateAddress) {
       return kTestSizeEstimate;
+    }
     return self->next->get_size_estimate_function(self->next, address, context);
   }
 
@@ -150,8 +157,9 @@ class AllocatorShimTest : public testing::Test {
                                  void* address,
                                  void* context) {
     // The same as MockGetSizeEstimate.
-    if (address == kTestSizeEstimateAddress)
+    if (address == kTestSizeEstimateAddress) {
       return true;
+    }
     return self->next->claimed_address_function(self->next, address, context);
   }
 
@@ -218,8 +226,9 @@ class AllocatorShimTest : public testing::Test {
                                   size_t alignment,
                                   void* context) {
     if (instance_) {
-      if (size < MaxSizeTracked())
+      if (size < MaxSizeTracked()) {
         ++instance_->aligned_reallocs_intercepted_by_size[size];
+      }
       ++instance_->aligned_reallocs_intercepted_by_addr[Hash(address)];
     }
     return self->next->aligned_realloc_function(self->next, address, size,
@@ -236,8 +245,9 @@ class AllocatorShimTest : public testing::Test {
   }
 
   static void NewHandler() {
-    if (!instance_)
+    if (!instance_) {
       return;
+    }
     instance_->num_new_handler_calls.fetch_add(1, std::memory_order_relaxed);
   }
 
@@ -604,15 +614,17 @@ TEST_F(AllocatorShimTest, NewHandlerConcurrency) {
 
   ThreadDelegateForNewHandlerTest mock_thread_main(&event);
 
-  for (auto& thread : threads)
+  for (auto& thread : threads) {
     base::PlatformThread::Create(0, &mock_thread_main, &thread);
+  }
 
   std::set_new_handler(&AllocatorShimTest::NewHandler);
   SetCallNewHandlerOnMallocFailure(true);  // It's going to fail on realloc().
   InsertAllocatorDispatch(&g_mock_dispatch);
   event.Signal();
-  for (auto& thread : threads)
+  for (auto& thread : threads) {
     base::PlatformThread::Join(thread);
+  }
   RemoveAllocatorDispatchForTesting(&g_mock_dispatch);
   ASSERT_EQ(kNumThreads, GetNumberOfNewHandlerCalls());
 }
@@ -663,8 +675,9 @@ TEST_F(AllocatorShimTest, ShimDoesntChangeMallocSizeWhenEnabled) {
 TEST_F(AllocatorShimTest, InterceptCLibraryFunctions) {
   auto total_counts = [](const std::vector<size_t>& counts) {
     size_t total = 0;
-    for (const auto count : counts)
+    for (const auto count : counts) {
       total += count;
+    }
     return total;
   };
   size_t counts_before;
