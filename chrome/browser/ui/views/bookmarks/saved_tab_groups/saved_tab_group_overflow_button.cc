c3233d482def164bc916a48dd371fb36e1a9eb6e
dljames <dljames@google.com>
2023-05-12 00:08:13 +0000

    [ SavedTabGroup ] Focus traversable overflow menu
    Implements focus traversal in the saved tab group overflow menu.
    
    Updates accessible properties on the button, bar, and overflow button.
    
    Implements functionality to open a group and its context menu with the
    keyboard.
    
    Change-Id: I891e4dbf7f30711e06185d6ac9f11c1defe615c3
    Bug: 1396552
    Low-Coverage-Reason: Waiting on updates / bug fixes to the interactive_ui_test framework "Kombucha" to be less flaky with mouse events. When ready these tests will cover the missing coverage lines, most of which are on callback functions we are unable to test easily. File the tests will be added to: chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_interactive_uitest.cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518636
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142993}

--- chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_overflow_button.cc
+++ chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_overflow_button.cc
@@ -24,30 +24,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 SavedTabGroupOverflowButton::SavedTabGroupOverflowButton(
     PressedCallback callback)
-    : views::MenuButton(std::move(callback), u"") {
-  SetAccessibleName(
+    : views::MenuButton(std::move(callback)) {
+  SetAccessibilityProperties(
+      ax::mojom::Role::kMenu,
       l10n_util::GetStringUTF16(IDS_ACCNAME_SAVED_TAB_GROUPS_CHEVRON));
   SetTooltipText(
       l10n_util::GetStringUTF16(IDS_SAVED_TAB_GROUPS_OVERFLOW_BUTTON_TOOLTIP));
   ConfigureInkDropForToolbar(this);
   SetImageLabelSpacing(ChromeLayoutProvider::Get()->GetDistanceMetric(
       DISTANCE_RELATED_LABEL_HORIZONTAL_LIST));
-  views::InstallPillHighlightPathGenerator(this);
 }
 
 SavedTabGroupOverflowButton::~SavedTabGroupOverflowButton() = default;
 
 void SavedTabGroupOverflowButton::GetAccessibleNodeData(
     ui::AXNodeData* node_data) {
-  // If the button would have no name, avoid crashing by setting the name
-  // explicitly empty.
-  if (GetAccessibleName().empty()) {
-    node_data->SetNameExplicitlyEmpty();
-  }
-
   views::MenuButton::GetAccessibleNodeData(node_data);
-  node_data->AddStringAttribute(
-      ax::mojom::StringAttribute::kRoleDescription,
+  node_data->role = ax::mojom::Role::kMenu;
+  node_data->SetNameChecked(
       l10n_util::GetStringUTF8(IDS_ACCNAME_SAVED_TAB_GROUPS_CHEVRON));
 }
 
