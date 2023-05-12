da6532c70d4fd95e98d83346f1aae8794d9781ea
Igor Ruvinov <igorruvinov@chromium.org>
2023-05-12 16:19:20 +0000

    Add feature for third-party profile management.
    Bug: 1445072
    Change-Id: Ib2d71c863677ca0eacd5c41c0fb3eae508863c50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4525403
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143306}

---
+++ chrome/browser/enterprise/profile_management/profile_management_features.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/profile_management/profile_management_features.h"
+
+#include "build/build_config.h"
+
+namespace profile_management {
+
+BASE_FEATURE(kThirdPartyProfileManagement,
+             "ThirdPartyProfileManagement",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace profile_management
