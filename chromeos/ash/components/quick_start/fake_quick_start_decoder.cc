39c87baa9cd6c5c4f3c02fceb67d639113b40bec
Joe Antonetti <joeantonetti@google.com>
2023-05-12 16:50:43 +0000

    [QuickStart][1/n] Added support for DecodeUserVerificationResult
    This is part of a series of changes to parse the verification result.
    
    This change handles the verification result. The next change will listen
    for a verification started.
    
    Test: Added Tests
    Bug: b\280453645
    Change-Id: Ia22b36d1f763577e0d3c2dcfe2a911794217c79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4512639
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143328}

--- chromeos/ash/components/quick_start/fake_quick_start_decoder.cc
+++ chromeos/ash/components/quick_start/fake_quick_start_decoder.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fake_quick_start_decoder.h"
 
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom-forward.h"
+#include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom-shared.h"
+#include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash::quick_start {
@@ -32,6 +34,17 @@ void FakeQuickStartDecoder::DecodeWifiCredentialsResponse(
   std::move(callback).Run(std::move(credentials_), error_);
 }
 
+void FakeQuickStartDecoder::DecodeUserVerificationResult(
+    const std::vector<uint8_t>& data,
+    DecodeUserVerificationResultCallback callback) {
+  if (error_ != absl::nullopt) {
+    std::move(callback).Run(nullptr, error_);
+  } else {
+    std::move(callback).Run(std::move(user_verification_response_),
+                            absl::nullopt);
+  }
+}
+
 void FakeQuickStartDecoder::DecodeGetAssertionResponse(
     const std::vector<uint8_t>& data,
     DecodeGetAssertionResponseCallback callback) {
@@ -51,6 +64,18 @@ void FakeQuickStartDecoder::DecodeNotifySourceOfUpdateResponse(
 void FakeQuickStartDecoder::SetExpectedData(
     std::vector<uint8_t> expected_data) {
   expected_data_ = expected_data;
+}
+
+void FakeQuickStartDecoder::SetDecoderError(
+    mojom::QuickStartDecoderError error) {
+  error_ = error;
+}
+
+void FakeQuickStartDecoder::SetUserVerificationResponse(
+    mojom::UserVerificationResult result,
+    bool is_first_user_verification) {
+  user_verification_response_ =
+      mojom::UserVerificationResponse::New(result, is_first_user_verification);
 }
 
 void FakeQuickStartDecoder::SetAssertionResponse(
--- chromeos/ash/components/quick_start/fake_quick_start_decoder.h
+++ chromeos/ash/components/quick_start/fake_quick_start_decoder.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom-shared.h"
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
-#include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace ash::quick_start {
@@ -38,6 +37,9 @@ class FakeQuickStartDecoder : public mojom::QuickStartDecoder {
   void DecodeNotifySourceOfUpdateResponse(
       const std::vector<uint8_t>& data,
       DecodeNotifySourceOfUpdateResponseCallback callback) override;
+  void DecodeUserVerificationResult(
+      const std::vector<uint8_t>& data,
+      DecodeUserVerificationResultCallback callback) override;
 
   void SetExpectedData(std::vector<uint8_t> expected_data);
   void SetAssertionResponse(
@@ -48,6 +50,11 @@ class FakeQuickStartDecoder : public mojom::QuickStartDecoder {
       const std::string& credential_id,
       const std::vector<uint8_t>& signature,
       const std::vector<uint8_t>& data);
+
+  void SetUserVerificationResponse(mojom::UserVerificationResult result,
+                                   bool is_first_user_verification);
+
+  void SetDecoderError(mojom::QuickStartDecoderError error);
 
   void SetWifiCredentialsResponse(
       mojom::WifiCredentialsPtr credentials,
@@ -67,6 +74,7 @@ class FakeQuickStartDecoder : public mojom::QuickStartDecoder {
   mojo::ReceiverSet<ash::quick_start::mojom::QuickStartDecoder> receiver_set_;
   absl::optional<bool> notify_source_of_update_response_;
   mojom::WifiCredentialsPtr credentials_;
+  mojom::UserVerificationResponsePtr user_verification_response_;
   absl::optional<mojom::QuickStartDecoderError> error_;
 };
 
