05d90ef51c80e5aec0db76da1a0ab787f4cc81df
Liviu Tinta <liviutinta@chromium.org>
2023-05-12 13:33:11 +0000

    [no-vary-search-hint] Add a use counter for No-Vary-Search hint
    Bug: 1416659
    Change-Id: I83a2a0f008a8c1330cf1983f4e302037c2319ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4522709
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143226}

--- third_party/blink/renderer/core/speculation_rules/speculation_rule_set.cc
+++ third_party/blink/renderer/core/speculation_rules/speculation_rule_set.cc
@@ -349,6 +349,7 @@ SpeculationRule* ParseSpeculationRule(JSONObject* input,
         return nullptr;
       }
     } else {
+      UseCounter::Count(context, WebFeature::kSpeculationRulesNoVarySearchHint);
       no_vary_search = std::move(no_vary_search_hint->get_no_vary_search());
     }
   }
