66d0cede8964d833805dbbd825897f4591063086
Emmanuel Arias Soto <eariassoto@google.com>
2023-05-12 08:19:59 +0000

    ambient: Track list of image files in ScreensaverImageDownloader
    This CL is the first effort to decouple responsibilities from the
    ScreensaverImagesPolicyHandler class. This class should not have any
    download/caching logic, but rather send policy updates to
    ScreensaverImageDownloader.
    
    Following changes will move the rest of the image processing logic, so
    that the cleanup logic can be implemented completely in the downloader
    class.
    
    Bug: b:280810255
    Test: ./ash_unittests --gtest_filter=*AmbientController*
    Test: ./ash_unittests --gtest_filter=*ScreensaverImage*
    Change-Id: I65f4069f4dabe04f8c5f2319f060724c8e55cd64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507329
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143130}

--- ash/ambient/ambient_controller_unittest.cc
+++ ash/ambient/ambient_controller_unittest.cc
@@ -1777,7 +1777,9 @@ TEST_F(AmbientControllerForManagedScreensaverTest,
       GetContainerView()->GetViewByID(AmbientViewID::kAmbientPhotoView));
 }
 
-class AmbientControllerForManagedScreensaverLoginScreenTest
+// TODO(b/271093537): Enable this tests once `ScreensaverImagesPolicyHandler`
+// supports the sign-in screen
+class DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest
     : public AmbientControllerForManagedScreensaverTest {
  public:
   void SetUp() override {
@@ -1799,7 +1801,7 @@ class AmbientControllerForManagedScreensaverLoginScreenTest
   }
 };
 
-TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
+TEST_F(DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest,
        ShownOnLoginScreen) {
   TriggerLoginScreen();
 
@@ -1813,7 +1815,7 @@ TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
   EXPECT_TRUE(ambient_controller()->ShouldShowAmbientUi());
 }
 
-TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
+TEST_F(DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest,
        ShownOnLoginWhenPrefUpdatedLater) {
   SetAmbientModeManagedScreensaverEnabled(/*enabled=*/false);
   EXPECT_FALSE(ambient_controller()->ShouldShowAmbientUi());
@@ -1826,14 +1828,14 @@ TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
   ASSERT_TRUE(GetContainerView());
 }
 
-TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
+TEST_F(DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest,
        NotShownOnLoginScreenWhenDisabled) {
   SetAmbientModeManagedScreensaverEnabled(/*enabled=*/false);
   FastForwardByLockScreenInactivityTimeout();
   EXPECT_FALSE(ambient_controller()->ShouldShowAmbientUi());
 }
 
-TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
+TEST_F(DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest,
        UserLogsInAmbientModeDisabledAndManagedAmbientModeEnabled) {
   TriggerLoginScreen();
   EXPECT_TRUE(ambient_controller()->ShouldShowAmbientUi());
@@ -1859,7 +1861,7 @@ TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
   ASSERT_TRUE(GetContainerView());
 }
 
-TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
+TEST_F(DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest,
        UserLogsInAmbientModeEnabled) {
   TriggerLoginScreen();
   EXPECT_TRUE(ambient_controller()->ShouldShowAmbientUi());
@@ -1878,7 +1880,7 @@ TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
   ASSERT_TRUE(GetContainerView());
 }
 
-TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
+TEST_F(DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest,
        ManagedScreensaverClosedWhenImagesCleared) {
   TriggerLoginScreen();
   EXPECT_TRUE(ambient_controller()->ShouldShowAmbientUi());
@@ -1907,7 +1909,7 @@ TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
   EXPECT_FALSE(ambient_controller()->ShouldShowAmbientUi());
 }
 
-TEST_F(AmbientControllerForManagedScreensaverLoginScreenTest,
+TEST_F(DISABLED_AmbientControllerForManagedScreensaverLoginScreenTest,
        ManagedScreensaverClosedWhenImageLoadingFails) {
   TriggerLoginScreen();
   EXPECT_TRUE(ambient_controller()->ShouldShowAmbientUi());
--- ash/ambient/managed/screensaver_images_policy_handler.h
+++ ash/ambient/managed/screensaver_images_policy_handler.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ambient/managed/screensaver_image_downloader.h"
 #include "ash/ash_export.h"
 #include "ash/public/cpp/ambient/ambient_managed_photo_source.h"
-#include "base/containers/flat_set.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
@@ -47,10 +46,7 @@ class ASH_EXPORT ScreensaverImagesPolicyHandler
   void OnAmbientModeManagedScreensaverImagesPrefChanged();
 
   // Download completion handler.
-  void OnDownloadJobCompleted(ScreensaverImageDownloadResult result,
-                              absl::optional<base::FilePath> path);
-
-  base::flat_set<base::FilePath> downloaded_images_;
+  void OnDownloadJobCompleted(const std::vector<base::FilePath>& image_list);
 
   raw_ptr<PrefService> user_pref_service_ = nullptr;
 
