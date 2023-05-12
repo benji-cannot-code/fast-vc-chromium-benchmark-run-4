82ea4558f3b5c4cfee3e0b33d71d06c2bbcd04b6
Nina Satragno <nsatragno@chromium.org>
2023-05-12 17:37:30 +0000

    [webui] Presubmit lint for third_party/lottie
    Add a presubmit lint check for third_party/lottie
    
    Fixed: 1110474
    Change-Id: I75b39a54de4dfee5470a1b07578b9628b056150a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518221
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143365}

---
+++ third_party/lottie/PRESUBMIT.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+USE_PYTHON3 = True
+PRESUBMIT_VERSION = '2.0.0'
+
+def CheckChange(input_api, output_api):
+  results = []
+  try:
+    import sys
+    old_sys_path = sys.path[:]
+    cwd = input_api.PresubmitLocalPath()
+    sys.path += [input_api.os_path.join(cwd, '..', '..', 'tools')]
+    import web_dev_style.presubmit_support
+    results += web_dev_style.presubmit_support.CheckStyleESLint(
+        input_api, output_api)
+  finally:
+    sys.path = old_sys_path
+  results += input_api.canned_checks.CheckPatchFormatted(input_api, output_api,
+                                                         check_js=True)
+  return results
