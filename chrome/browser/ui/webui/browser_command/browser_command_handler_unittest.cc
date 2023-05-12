7186200dc6d8b8d7f1099ef7d8fb09478d36ebae
Simon Hangl <simonha@google.com>
2023-05-12 19:56:35 +0000

    [CodeHealth] Fixed mock method and formatting
    Fix for /chrome/browser/ui/webui/browser_command
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: b/280051002
    Change-Id: Iae1b3cb362396953fef882fc36a65e0e8717960c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4488916
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143465}

--- chrome/browser/ui/webui/browser_command/browser_command_handler_unittest.cc
+++ chrome/browser/ui/webui/browser_command/browser_command_handler_unittest.cc
@@ -82,8 +82,9 @@ class TestCommandHandler : public BrowserCommandHandler {
   ui::ElementContext GetUiElementContext() override { return kTestContext1; }
 
   CommandUpdater* GetCommandUpdater() override {
-    if (command_updater_)
+    if (command_updater_) {
       return command_updater_.get();
+    }
     return BrowserCommandHandler::GetCommandUpdater();
   }
 
@@ -172,13 +173,15 @@ class MockTutorialService : public TestTutorialService {
       : TestTutorialService(tutorial_registry, help_bubble_factory_registry) {}
   ~MockTutorialService() override = default;
 
-  MOCK_METHOD4(StartTutorial,
-               void(user_education::TutorialIdentifier,
-                    ui::ElementContext,
-                    base::OnceClosure,
-                    base::OnceClosure));
-  MOCK_METHOD2(LogStartedFromWhatsNewPage,
-               void(user_education::TutorialIdentifier, bool));
+  MOCK_METHOD(void,
+              StartTutorial,
+              (user_education::TutorialIdentifier,
+               ui::ElementContext,
+               base::OnceClosure,
+               base::OnceClosure));
+  MOCK_METHOD(void,
+              LogStartedFromWhatsNewPage,
+              (user_education::TutorialIdentifier, bool));
   MOCK_CONST_METHOD0(IsRunningTutorial, bool());
 };
 
@@ -187,9 +190,9 @@ class MockCommandHandler : public TestCommandHandler {
   explicit MockCommandHandler(Profile* profile) : TestCommandHandler(profile) {}
   ~MockCommandHandler() override = default;
 
-  MOCK_METHOD2(NavigateToURL, void(const GURL&, WindowOpenDisposition));
+  MOCK_METHOD(void, NavigateToURL, (const GURL&, WindowOpenDisposition));
 
-  MOCK_METHOD0(OpenFeedbackForm, void());
+  MOCK_METHOD(void, OpenFeedbackForm, ());
 };
 
 class MockCommandUpdater : public CommandUpdaterImpl {
