0e5b6b150c53cd47ef6ccc0556366e76adfa4895
Sylvie Liu <sylvieliu@chromium.org>
2023-05-12 16:34:15 +0000

    Replaced text secondary color with on-surface-variant for Jelly
    Texts that use kTextColorSecondary before should be using
    cros.sys.on-surface-variant instead.
    
    Screenshots:
    - Notification message label and timestamps:
      https://screenshot.googleplex.com/i73nLyNpNeBV6bk
    - Continue view label:
      https://screenshot.googleplex.com/8skorYnAN8vJuzZ
    - Calendar month view inactive date labels:
      https://screenshot.googleplex.com/4DajrkC3RFeRtig
    - User chooser view email address:
      https://screenshot.googleplex.com/42gupFWYi3iJ5yz
    
    Bug: b:278298618
    Change-Id: Ic302d7b8e3b7a78b8c7a7a4e0c9a562c37acea05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523587
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143316}

--- ash/system/message_center/ash_notification_view.cc
+++ ash/system/message_center/ash_notification_view.cc
@@ -384,7 +384,7 @@ AshNotificationView::NotificationTitleRow::NotificationTitleRow(
     title_view_->SetEnabledColorId(cros_tokens::kCrosSysOnSurface);
 
     timestamp_in_collapsed_view_->SetEnabledColorId(
-        cros_tokens::kCrosSysSecondary);
+        cros_tokens::kCrosSysOnSurfaceVariant);
     ash::TypographyProvider::Get()->StyleLabel(
         ash::TypographyToken::kCrosAnnotation1, *timestamp_in_collapsed_view_);
   }
@@ -469,7 +469,7 @@ void AshNotificationView::NotificationTitleRow::OnThemeChanged() {
   if (chromeos::features::IsJellyEnabled()) {
     title_view_->SetEnabledColorId(cros_tokens::kCrosSysOnSurface);
     timestamp_in_collapsed_view_->SetEnabledColorId(
-        cros_tokens::kCrosSysSecondary);
+        cros_tokens::kCrosSysOnSurfaceVariant);
   }
 }
 
@@ -593,7 +593,7 @@ AshNotificationView::AshNotificationView(
 
   if (chromeos::features::IsJellyEnabled()) {
     message_label_in_expanded_state_->SetEnabledColorId(
-        cros_tokens::kCrosSysSecondary);
+        cros_tokens::kCrosSysOnSurfaceVariant);
     ash::TypographyProvider::Get()->StyleLabel(
         ash::TypographyToken::kCrosAnnotation1,
         *message_label_in_expanded_state_);
@@ -1223,7 +1223,7 @@ void AshNotificationView::UpdateWithNotification(
     ConfigureLabelStyle(message_label(), kMessageLabelSize,
                         /*is_color_primary=*/false);
     if (chromeos::features::IsJellyEnabled()) {
-      message_label()->SetEnabledColorId(cros_tokens::kCrosSysSecondary);
+      message_label()->SetEnabledColorId(cros_tokens::kCrosSysOnSurfaceVariant);
       ash::TypographyProvider::Get()->StyleLabel(
           ash::TypographyToken::kCrosAnnotation1, *message_label());
     }
@@ -1419,7 +1419,7 @@ void AshNotificationView::OnThemeChanged() {
   if (message_label()) {
     message_label()->SetEnabledColor(secondary_text_color);
     if (chromeos::features::IsJellyEnabled()) {
-      message_label()->SetEnabledColorId(cros_tokens::kCrosSysSecondary);
+      message_label()->SetEnabledColorId(cros_tokens::kCrosSysOnSurfaceVariant);
     }
   }
 
@@ -1433,7 +1433,7 @@ void AshNotificationView::OnThemeChanged() {
     message_label_in_expanded_state_->SetEnabledColor(secondary_text_color);
     if (chromeos::features::IsJellyEnabled()) {
       message_label_in_expanded_state_->SetEnabledColorId(
-          cros_tokens::kCrosSysSecondary);
+          cros_tokens::kCrosSysOnSurfaceVariant);
     }
   }
 
