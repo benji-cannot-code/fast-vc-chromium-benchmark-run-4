fa5ac23ec1bdfdee8cd9addec537ba572d626f3b
Alex Ilin <alexilin@chromium.org>
2023-05-12 11:00:46 +0000

    [BSC]: Implement BoundSessionRefreshCookieFetcherImpl
    This CL implements `BoundSessionRefreshCookieFetcherImpl` that does the
    network request to the rotation endpoint. Proper error handling will be
    added in a follow-up CL.
    
    Bug: b/273920907
    Change-Id: I9ddaab0c640d3547ee7b81de93a7c06876bd0a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4509334
    Auto-Submit: Monica Basta <msalama@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143182}

--- chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher.h
+++ chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SIGNIN_BOUND_SESSION_CREDENTIALS_BOUND_SESSION_REFRESH_COOKIE_FETCHER_H_
 #define CHROME_BROWSER_SIGNIN_BOUND_SESSION_CREDENTIALS_BOUND_SESSION_REFRESH_COOKIE_FETCHER_H_
 
+#include "base/functional/callback_forward.h"
 #include "net/base/net_errors.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
