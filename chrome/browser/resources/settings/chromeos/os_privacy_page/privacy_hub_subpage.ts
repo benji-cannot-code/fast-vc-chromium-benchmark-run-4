c5b5a1867aacd93aacba379375aa779aa1c1f03c
CJ Huang <chenjih@google.com>
2023-05-12 02:03:25 +0000

    [SPEAK-ON-MUTE] Add mute nudge to the privacy hub.
    Add mute nudge to the privacy hub in os settings with associated pref.
    
    This mute nudge will be used to turn on/off the speak-on-mute detection
    in later CL. This is developed under a Chrome flag.
    
    We use a temporary learn moreURL for development purpose, and we
    will replace it when the new URL is ready.
    
    Follow https://crrev.com/c/3851126 as an example.
    
    Screenshot: https://screenshot.googleplex.com/XP2Ss4jnGdgFEdK
    
    Bug: b/280925839
    Test: browser_tests --gtest_filter="OSSettingsOsPrivacyPagePrivacyHubSubpage*"
    Change-Id: I2bda0baf2d6fa5af69a9ab2bd238bad7e3434f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520279
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143038}

--- chrome/browser/resources/settings/chromeos/os_privacy_page/privacy_hub_subpage.ts
+++ chrome/browser/resources/settings/chromeos/os_privacy_page/privacy_hub_subpage.ts
@@ -106,6 +106,17 @@ export class SettingsPrivacyHubSubpage extends SettingsPrivacyHubSubpageBase {
       },
 
       /**
+       * Whether the part of speak-on-mute detection should be displayed.
+       */
+      showSpeakOnMuteDetectionPage_: {
+        type: Boolean,
+        readOnly: true,
+        value: () => {
+          return loadTimeData.getBoolean('showSpeakOnMuteDetectionPage');
+        },
+      },
+
+      /**
        * Used by DeepLinkingMixin to focus this page's deep links.
        */
       supportedSettingIds: {
@@ -113,6 +124,7 @@ export class SettingsPrivacyHubSubpage extends SettingsPrivacyHubSubpageBase {
         value: () => new Set<Setting>([
           Setting.kCameraOnOff,
           Setting.kMicrophoneOnOff,
+          Setting.kSpeakOnMuteDetectionOnOff,
           Setting.kGeolocationOnOff,
           Setting.kUsageStatsAndCrashReports,
         ]),
@@ -128,6 +140,7 @@ export class SettingsPrivacyHubSubpage extends SettingsPrivacyHubSubpageBase {
   private microphoneHardwareToggleActive_: boolean;
   private shouldDisableMicrophoneToggle_: boolean;
   private showPrivacyHubMVPPage_: boolean;
+  private showSpeakOnMuteDetectionPage_: boolean;
 
   constructor() {
     super();
