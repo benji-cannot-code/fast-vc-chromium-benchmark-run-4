114597dbf9ae172333efb9e0bffdebc1a228c391
Jun Kokatsu <jkokatsu@google.com>
2023-05-12 01:05:36 +0000

    Add origin information to chrome.tabs.sendMessage
    This change sets MessageSender.origin to the sender's origin. In
    practice, this is always the extension's origin, because
    `chrome.tabs.sendMessage` API can be called only from the extension
    page (and not from content scripts).
    
    Bug: 1442980
    Change-Id: I2d8184e67e89f0e8c755b03703f58705246bca97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514239
    Commit-Queue: Jun Kokatsu <jkokatsu@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Jun Kokatsu <jkokatsu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143007}

--- chrome/test/data/extensions/api_test/tabs/send_message/echo.js
+++ chrome/test/data/extensions/api_test/tabs/send_message/echo.js
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // A simple onMessage listener that responds to "ping" messages with a "pong"
 // message.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const response =
-      request == 'ping' ? 'pong' : `Unexpected message: ${request}`;
+  const extensionOrigin = new URL(chrome.runtime.getURL('')).origin;
+  let response = 'pong';
+  if (request != 'ping' || sender.origin != extensionOrigin)
+    response = `Unexpected message "${request}" from origin "${sender.origin}"`;
   sendResponse(response);
 });
