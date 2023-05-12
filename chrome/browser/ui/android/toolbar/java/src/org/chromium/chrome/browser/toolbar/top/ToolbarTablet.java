9805f0850558dd4a0e3b545346a0a0b533b88ee8
Zhe Li <zheliooo@google.com>
2023-05-12 01:05:44 +0000

    [Tab Strip Redesign] Add experiment param for restore Home button position on Toolbar
    Also changed DisableNTBAnchor param name from 'NTB fix position' to 'NTB unanchored"
    
    Enable Folio: https://screenshot.googleplex.com/4UpY2jNa4FvcBrF
    Enabled Folio Shut off Toolbar Reordering: https://screenshot.googleplex.com/ADwWYo8Rvd74tKE
    
    Bug: 1444583
    Change-Id: I1a10bc3e4821907251e9e5b5d21adb909241d702
    Low-Coverage-Reason: Unit test for enable this experiment param has been added in ToolbarTabletUnitTest, which I think is enough to cover the code in this CL.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519243
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143008}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
@@ -36,6 +36,7 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.omnibox.LocationBar;
 import org.chromium.chrome.browser.omnibox.LocationBarCoordinator;
 import org.chromium.chrome.browser.omnibox.NewTabPageDelegate;
+import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
 import org.chromium.chrome.browser.omnibox.UrlBarData;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
@@ -128,6 +129,11 @@ public class ToolbarTablet
                 getResources().getDimensionPixelOffset(R.dimen.toolbar_edge_padding);
     }
 
+    public boolean isToolbarButtonReorderingEnabled() {
+        return ChromeFeatureList.sTabStripRedesign.isEnabled()
+            && !OmniboxFeatures.isTabStripToolbarReorderingDisabled();
+    }
+
     @Override
     public void onFinishInflate() {
         super.onFinishInflate();
@@ -136,8 +142,9 @@ public class ToolbarTablet
         mForwardButton = findViewById(R.id.forward_button);
         mReloadButton = findViewById(R.id.refresh_button);
 
-        // Reposition home button to align with desktop ordering when TSR enabled.
-        if (ChromeFeatureList.sTabStripRedesign.isEnabled()) {
+        // Reposition home button to align with desktop ordering when TSR enabled and toolbar
+        // reordering not disabled
+        if (isToolbarButtonReorderingEnabled()) {
             // Remove home button view added in XML and adding back with different ordering
             // programmatically.
             ((ViewGroup) mHomeButton.getParent()).removeView(mHomeButton);
@@ -191,7 +198,7 @@ public class ToolbarTablet
         mHomeButton.setOnKeyListener(new KeyboardNavigationListener() {
             @Override
             public View getNextFocusForward() {
-                if (ChromeFeatureList.sTabStripRedesign.isEnabled()) {
+                if (isToolbarButtonReorderingEnabled()) {
                     return findViewById(R.id.url_bar);
                 } else {
                     if (mBackButton.isFocusable()) {
@@ -206,7 +213,7 @@ public class ToolbarTablet
 
             @Override
             public View getNextFocusBackward() {
-                if (ChromeFeatureList.sTabStripRedesign.isEnabled()) {
+                if (isToolbarButtonReorderingEnabled()) {
                     return findViewById(R.id.refresh_button);
                 } else {
                     return findViewById(R.id.menu_button);
@@ -228,7 +235,7 @@ public class ToolbarTablet
 
             @Override
             public View getNextFocusBackward() {
-                if (ChromeFeatureList.sTabStripRedesign.isEnabled()) {
+                if (isToolbarButtonReorderingEnabled()) {
                     return findViewById(R.id.menu_button);
                 } else {
                     if (mHomeButton.getVisibility() == VISIBLE) {
@@ -252,7 +259,7 @@ public class ToolbarTablet
             public View getNextFocusBackward() {
                 if (mBackButton.isFocusable()) {
                     return mBackButton;
-                } else if (!ChromeFeatureList.sTabStripRedesign.isEnabled()
+                } else if (!isToolbarButtonReorderingEnabled()
                         && mHomeButton.getVisibility() == VISIBLE) {
                     return findViewById(R.id.home_button);
                 } else {
@@ -266,7 +273,7 @@ public class ToolbarTablet
         mReloadButton.setOnKeyListener(new KeyboardNavigationListener() {
             @Override
             public View getNextFocusForward() {
-                if (ChromeFeatureList.sTabStripRedesign.isEnabled()
+                if (isToolbarButtonReorderingEnabled()
                         && mHomeButton.getVisibility() == VISIBLE) {
                     return findViewById(R.id.home_button);
                 } else {
@@ -280,7 +287,7 @@ public class ToolbarTablet
                     return mForwardButton;
                 } else if (mBackButton.isFocusable()) {
                     return mBackButton;
-                } else if (!ChromeFeatureList.sTabStripRedesign.isEnabled()
+                } else if (!isToolbarButtonReorderingEnabled()
                         && mHomeButton.getVisibility() == VISIBLE) {
                     return findViewById(R.id.home_button);
                 } else {
