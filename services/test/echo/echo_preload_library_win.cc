87a0b6a350aded5c48ece064329a9093cc9998cd
Alex Gough <ajgo@chromium.org>
2023-05-12 23:00:46 +0000

    Reland "Allow utilities to preload libraries before sandbox lockdown"
    This is a reland of commit fe96e5d8bdf603c73616ff87082352a562de1be7
    
    Now builds correctly for asan.
    
    Additionally, during testing a flaky failure was uncovered where
    the mojo service could start before the preloading happened, this
    is resolved by moving the preloading code ahead of IO thread
    creation in UtilityMain.
    
    Original change's description:
    > Allow utilities to preload libraries before sandbox lockdown
    >
    > Some utilities on Windows wrap dynamic libraries which might be
    > distributed as components. Allowing these to load requires punching
    > holes in the sandbox using file interceptions that are being phased
    > out.
    >
    > This CL introduces a mechanism for service processes to specify a
    > list of dll paths which will be loaded before the sandbox goes into
    > lockdown. They can later be loaded by code using the same path, as
    > Windows keeps them in memory.
    >
    > The mechanism uses a new opaque data transfer mechanism in the
    > sandbox which will extend to different uses later. For
    > now the list of DLLs is the only data transferred so this CL uses
    > base::Pickle to serialize & transfer the DLLs to load.
    >
    > As loading arbitrary DLLs is a powerful capability a passkey is
    > added for the new service process host option, and a file containing
    > valid callers is added with OWNERS from the sandbox to ensure
    > security review.
    >
    > A following CL will enable this behavior for screen_ai, but at this
    > point only tests exercise these APIs
    >
    > The flow is roughly:
    > ---
    > In the browser/broker:-
    >
    > ServiceProcessHost gets a new option:
    >   .WithPreloadedLibraries
    >
    > UtilityProcessHost gets a new member:
    >   preload_libraries_
    >
    > UtilityProcessHost gives this to the UtilitySandboxedProcessLauncherDelegate
    >
    > UtilitySandboxedProcessLauncherDelegate turns a list of DLLs into an
    > opaque blob and attaches it to the sandbox's TargetPolicy during
    > PreSpawnTarget.
    >
    > The sandbox smuggles this blob into the target (previous CL).
    >
    > In the child/target:-
    >
    > The UtilityMain knows it might have a blob, so asks TargetServices.
    >
    > UtilityMain coordinates with its sandbox delegate, so knows that
    > the blob is a pickled list of DLLs.
    >
    > UtilityMain loads these dlls before sandbox lockdown.
    > ---
    >
    > Bug: 1435571
    > Change-Id: Ided63f9d723811b66c183335b5533c84e9783a2a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4471953
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1137415}
    
    Bug: 1435571
    Change-Id: Id0156d4649efd67adc5fe2eadaa73415b9c35503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518747
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143553}

---
+++ services/test/echo/echo_preload_library_win.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is a small file to make a loadable dll for the echo service tests.
+#include <windows.h>
+
+#define SECURITY_WIN32
+#include <security.h>
+
+extern "C" {
+BOOL WINAPI DllMain(PVOID h, DWORD reason, PVOID reserved) {
+  return true;
+}
+
+BOOL FnCallsDelayloadFn() {
+  // Calls xyz which is delayloaded but in a library already in
+  // utility processes.
+  ULONG sz = 0;
+  wchar_t buf[1];
+  // This call should happen, we don't actually care about the return.
+  if (::GetUserNameExW(NameSamCompatible, buf, &sz)) {
+    return false;
+  }
+  return true;
+}
+}  // extern "C"
--- services/test/echo/echo_service.cc
+++ services/test/echo/echo_service.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
 #include <winevt.h>
+
+#include "base/native_library.h"
 #endif
 
 #include <string>
@@ -50,6 +52,41 @@ void EchoService::DelayLoad() {
   EVT_HANDLE handle = ::EvtCreateRenderContext(0, nullptr, 0);
   ::EvtClose(handle);
 }
+
+void EchoService::LoadNativeLibrary(const ::base::FilePath& library,
+                                    bool call_sec32_delayload,
+                                    LoadNativeLibraryCallback callback) {
+  // This attempts to load a library inside the sandbox - it should fail unless
+  // the library was in `ServiceProcessHostOptions::WithPreloadedLibraries()`.
+  base::NativeLibraryLoadError error;
+  // We leak the module as preloading already leaked it.
+  HMODULE hmod = base::LoadNativeLibrary(library, &error);
+  if (!hmod) {
+    std::move(callback).Run(LoadStatus::kFailedLoadLibrary, error.code);
+    return;
+  }
+
+  // Calls an exported function that calls a delayloaded function that should
+  // be loaded in the utility (as secur32.dll is imported by chrome.dll).
+  if (call_sec32_delayload) {
+    BOOL(WINAPI * fn)() = nullptr;
+    fn = reinterpret_cast<decltype(fn)>(
+        GetProcAddress(hmod, "FnCallsDelayloadFn"));
+    if (!fn) {
+      std::move(callback).Run(LoadStatus::kFailedGetProcAddress,
+                              GetLastError());
+      return;
+    }
+    BOOL ret = fn();
+    if (!ret) {
+      std::move(callback).Run(LoadStatus::kFailedCallingDelayLoad,
+                              GetLastError());
+      return;
+    }
+  }
+  std::move(callback).Run(LoadStatus::kSuccess, ERROR_SUCCESS);
+}
+
 #endif  // BUILDFLAG(IS_WIN)
 
 }  // namespace echo
