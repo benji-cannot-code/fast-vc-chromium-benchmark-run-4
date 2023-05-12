5976a250cd5bb97d7a01d791925159e5f0f68524
Zhiyuan Cai <zhiyuancai@chromium.org>
2023-05-12 17:51:48 +0000

    [PriceInsights] Add the feature flag
    This CL adds a feature flag for PriceInsights and also a region launched
    flag since this feature will roll out country by country.
    
    Bug: b:282070088
    Change-Id: Idfe5030ccae296d1fa42ac242d6ef74d7122a9ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519447
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143376}

--- components/commerce/core/commerce_feature_list.cc
+++ components/commerce/core/commerce_feature_list.cc
@@ -48,6 +48,7 @@ const CountryLocaleMap& GetAllowedCountryToLocaleMap() {
     map[&ntp_features::kNtpChromeCartModule] = {{"us", {"en-us"}}};
     map[&kCommerceMerchantViewerRegionLaunched] = {{"us", {"en-us"}}};
     map[&kCommercePriceTrackingRegionLaunched] = {{"us", {"en-us"}}};
+    map[&kPriceInsightsRegionLaunched] = {{"us", {"en-us"}}};
 
     return map;
   }());
@@ -161,6 +162,13 @@ BASE_FEATURE(kCommercePriceTrackingRegionLaunched,
              "CommercePriceTrackingRegionLaunched",
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
+
+BASE_FEATURE(kPriceInsights,
+             "PriceInsights",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPriceInsightsRegionLaunched,
+             "PriceInsightsRegionLaunched",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 const base::FeatureParam<bool> kDeleteAllMerchantsOnClearBrowsingHistory{
     &kCommerceMerchantViewer, "delete_all_merchants_on_clear_history", false};
