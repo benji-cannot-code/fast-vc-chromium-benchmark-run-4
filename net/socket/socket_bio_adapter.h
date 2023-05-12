8e1f07f725f136bdf31e74186a256af2f2a7d319
David Benjamin <davidben@chromium.org>
2023-05-12 01:10:04 +0000

    Add more CHECKs to SocketBIOAdapter
    Also add a SequenceChecker to try to catch if something is violating
    threading invariants, as well as a reentrancy check on socket->Write.
    
    Bug: 1440692
    Change-Id: Ibee174fe1eb5103501572c002de9f4543bd0edbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521843
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143015}

--- net/socket/socket_bio_adapter.h
+++ net/socket/socket_bio_adapter.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "net/base/completion_repeating_callback.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_export.h"
@@ -143,6 +144,7 @@ class NET_EXPORT_PRIVATE SocketBIOAdapter {
 
   raw_ptr<Delegate> delegate_;
 
+  SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<SocketBIOAdapter> weak_factory_{this};
 };
 
