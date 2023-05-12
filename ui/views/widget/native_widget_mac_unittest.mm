9f2c369cd05766785a60cdc1e8b2247adb1fab1b
Jayson Adams <shrike@chromium.org>
2023-05-12 21:00:35 +0000

    [Mac Window State] Add kMiniaturizeWindow to remote cocoa WindowVisibilityState
    This cl creates the WindowVisibilityState::kMiniaturizeWindow state and
    enables session restore requests to minimize windows (previously, a
    request to ui::SHOW_STATE_MINIMIZED ordered the window out).
    
    Bug: 1431602
    
    Test: out/Default/views_unittests \
      --gtest_filter=NativeWidgetMacTest.MinimizeByNativeShow
    
    Change-Id: Iad62d649b5e33f463075ddbacc678b6195360787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4492278
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143491}

--- ui/views/widget/native_widget_mac_unittest.mm
+++ ui/views/widget/native_widget_mac_unittest.mm
@@ -485,14 +485,14 @@ TEST_F(NativeWidgetMacTest, ChildWidgetOnInactiveSpace) {
 // Test minimized states triggered externally, implied visibility and restored
 // bounds whilst minimized.
 TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
-  Widget* widget = new Widget;
+  WidgetAutoclosePtr widget(new Widget);
   Widget::InitParams init_params(Widget::InitParams::TYPE_WINDOW);
   widget->Init(std::move(init_params));
 
-  PaintCountView* view = new PaintCountView();
-  widget->GetContentsView()->AddChildView(view);
+  auto* view = widget->GetContentsView()->AddChildView(
+      std::make_unique<PaintCountView>());
   NSWindow* ns_window = widget->GetNativeWindow().GetNativeNSWindow();
-  WidgetChangeObserver observer(widget);
+  WidgetChangeObserver observer(widget.get());
 
   widget->SetBounds(gfx::Rect(100, 100, 300, 300));
 
@@ -501,7 +501,7 @@ TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
 
   {
     views::test::PropertyWaiter visibility_waiter(
-        base::BindRepeating(&Widget::IsVisible, base::Unretained(widget)),
+        base::BindRepeating(&Widget::IsVisible, base::Unretained(widget.get())),
         true);
     widget->Show();
     EXPECT_TRUE(visibility_waiter.Wait());
@@ -522,7 +522,8 @@ TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
   // anything fancy to wait for it finish.
   {
     views::test::PropertyWaiter minimize_waiter(
-        base::BindRepeating(&Widget::IsMinimized, base::Unretained(widget)),
+        base::BindRepeating(&Widget::IsMinimized,
+                            base::Unretained(widget.get())),
         true);
     [ns_window performMiniaturize:nil];
     EXPECT_TRUE(minimize_waiter.Wait());
@@ -542,7 +543,8 @@ TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
 
   {
     views::test::PropertyWaiter deminimize_waiter(
-        base::BindRepeating(&Widget::IsMinimized, base::Unretained(widget)),
+        base::BindRepeating(&Widget::IsMinimized,
+                            base::Unretained(widget.get())),
         false);
     [ns_window deminiaturize:nil];
     EXPECT_TRUE(deminimize_waiter.Wait());
@@ -559,7 +561,8 @@ TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
 
   {
     views::test::PropertyWaiter minimize_waiter(
-        base::BindRepeating(&Widget::IsMinimized, base::Unretained(widget)),
+        base::BindRepeating(&Widget::IsMinimized,
+                            base::Unretained(widget.get())),
         true);
     widget->Minimize();
     EXPECT_TRUE(minimize_waiter.Wait());
@@ -574,7 +577,8 @@ TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
 
   {
     views::test::PropertyWaiter deminimize_waiter(
-        base::BindRepeating(&Widget::IsMinimized, base::Unretained(widget)),
+        base::BindRepeating(&Widget::IsMinimized,
+                            base::Unretained(widget.get())),
         false);
     widget->Restore();  // If miniaturized, should deminiaturize.
     EXPECT_TRUE(deminimize_waiter.Wait());
@@ -589,7 +593,8 @@ TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
 
   {
     views::test::PropertyWaiter deminimize_waiter(
-        base::BindRepeating(&Widget::IsMinimized, base::Unretained(widget)),
+        base::BindRepeating(&Widget::IsMinimized,
+                            base::Unretained(widget.get())),
         false);
     widget->Restore();  // If not miniaturized, does nothing.
     EXPECT_TRUE(deminimize_waiter.Wait());
@@ -601,8 +606,50 @@ TEST_F(NativeWidgetMacTest, MiniaturizeExternally) {
   EXPECT_EQ(2, observer.lost_visible_count());
   EXPECT_EQ(restored_bounds, widget->GetRestoredBounds());
   EXPECT_EQ(3, view->paint_count());
+}
 
-  widget->CloseNow();
+// Tests that NativeWidgetMac::Show(ui::SHOW_STATE_MINIMIZED) minimizes the
+// widget (previously it ordered its window out).
+TEST_F(NativeWidgetMacTest, MinimizeByNativeShow) {
+  WidgetAutoclosePtr widget(new Widget);
+  Widget::InitParams init_params(Widget::InitParams::TYPE_WINDOW);
+  widget->Init(std::move(init_params));
+
+  auto* view = widget->GetContentsView()->AddChildView(
+      std::make_unique<PaintCountView>());
+  WidgetChangeObserver observer(widget.get());
+
+  widget->SetBounds(gfx::Rect(100, 100, 300, 300));
+
+  EXPECT_TRUE(view->IsDrawn());
+  EXPECT_EQ(0, view->paint_count());
+
+  {
+    views::test::PropertyWaiter visibility_waiter(
+        base::BindRepeating(&Widget::IsVisible, base::Unretained(widget.get())),
+        true);
+    widget->Show();
+    EXPECT_TRUE(visibility_waiter.Wait());
+  }
+
+  EXPECT_FALSE(widget->IsMinimized());
+  EXPECT_TRUE(widget->IsVisible());
+
+  {
+    views::test::PropertyWaiter minimize_waiter(
+        base::BindRepeating(&Widget::IsMinimized,
+                            base::Unretained(widget.get())),
+        true);
+
+    NativeWidgetMac* native_widget =
+        static_cast<views::NativeWidgetMac*>(widget->native_widget());
+    gfx::Rect restore_bounds(100, 100, 300, 300);
+    native_widget->Show(ui::SHOW_STATE_MINIMIZED, restore_bounds);
+
+    EXPECT_TRUE(minimize_waiter.Wait());
+  }
+
+  EXPECT_TRUE(widget->IsMinimized());
 }
 
 TEST_F(NativeWidgetMacTest, MiniaturizeFramelessWindow) {
