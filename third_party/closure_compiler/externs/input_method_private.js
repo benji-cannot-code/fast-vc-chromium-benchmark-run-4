eb5a21ca176c6360e124027d719158d67e9089df
John Palmer <jopalmer@chromium.org>
2023-05-12 00:24:17 +0000

    rm has_been_password from VK API
    Once we land cl/528351947 this field is no longer read on the VK side,
    so delete it. (We can't land this before that has landed and is released
    though)
    
    BUG: b:280234311
    Change-Id: I11b8e23c21c031c1de2a53c8ce55942b9f7d3bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4495765
    Reviewed-by: jhtin <jhtin@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143001}

--- third_party/closure_compiler/externs/input_method_private.js
+++ third_party/closure_compiler/externs/input_method_private.js
@@ -109,7 +109,6 @@ chrome.inputMethodPrivate.AutoCapitalizeType = {
  *   spellCheck: boolean,
  *   shouldDoLearning: boolean,
  *   focusReason: !chrome.inputMethodPrivate.FocusReason,
- *   hasBeenPassword: boolean,
  *   appKey: (string|undefined)
  * }}
  */
