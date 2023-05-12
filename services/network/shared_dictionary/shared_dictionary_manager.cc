b5bec59562c0690126349956e7502e48927cd146
Tsuyoshi Horo <horo@chromium.org>
2023-05-12 04:38:19 +0000

    Implement SharedDictionary{Manager/Storage}OnDisk
    This CL implements SharedDictionaryManagernDisk and
    SharedDictionaryStorageOnDisk. SharedDictionaryManagerOnDisk holds a
    SharedDictionaryDiskCache and a SQLitePersistentSharedDictionaryStore.
    
    A SharedDictionaryStorageOnDisk is created for the given
    net::SharedDictionaryStorageIsolationKey to manage the dictionaries.
    It loads the all dictionary information for the
    net::SharedDictionaryStorageIsolationKey from the
    SQLitePersistentSharedDictionaryStore, and stores the information to
    `dictionary_info_map_`.
    
    When GetDictionary() is called, SharedDictionaryStorageOnDisk returns a
    SharedDictionary which loads the dictionary binary from the
    SharedDictionaryStorageOnDisk.
    
    When CreateWriter() is called, SharedDictionaryStorageOnDisk returns a
    SharedDictionaryWriterOnDisk which writes the dictionary binary into the
    SharedDictionaryStorageOnDisk. When the writer finishes writing the
    binary, SharedDictionaryStorageOnDisk::OnDictionaryWrittenInDiskCache()
    is called. And the SharedDictionaryStorageOnDisk will write the
    dictionary metadata into SQLitePersistentSharedDictionaryStore.
    
    Low-Coverage-Reason: This line only runs on Android.
    Bug: 1413922
    Change-Id: I14058745ffd398bcf58fe1a81c2524f93a43cd9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521142
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143077}

--- services/network/shared_dictionary/shared_dictionary_manager.cc
+++ services/network/shared_dictionary/shared_dictionary_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/shared_dictionary/shared_dictionary_manager.h"
 
 #include "services/network/shared_dictionary/shared_dictionary_manager_in_memory.h"
+#include "services/network/shared_dictionary/shared_dictionary_manager_on_disk.h"
 #include "services/network/shared_dictionary/shared_dictionary_storage.h"
 
 namespace network {
@@ -14,6 +15,23 @@ namespace network {
 std::unique_ptr<SharedDictionaryManager>
 SharedDictionaryManager::CreateInMemory() {
   return std::make_unique<SharedDictionaryManagerInMemory>();
+}
+
+// static
+std::unique_ptr<SharedDictionaryManager> SharedDictionaryManager::CreateOnDisk(
+    const base::FilePath& database_path,
+    const base::FilePath& cache_directory_path,
+#if BUILDFLAG(IS_ANDROID)
+    base::android::ApplicationStatusListener* app_status_listener,
+#endif  // BUILDFLAG(IS_ANDROID)
+    scoped_refptr<disk_cache::BackendFileOperationsFactory>
+        file_operations_factory) {
+  return std::make_unique<SharedDictionaryManagerOnDisk>(
+      database_path, cache_directory_path,
+#if BUILDFLAG(IS_ANDROID)
+      app_status_listener,
+#endif  // BUILDFLAG(IS_ANDROID)
+      std::move(file_operations_factory));
 }
 
 SharedDictionaryManager::SharedDictionaryManager() = default;
@@ -27,6 +45,7 @@ scoped_refptr<SharedDictionaryStorage> SharedDictionaryManager::GetStorage(
     return it->second.get();
   }
   scoped_refptr<SharedDictionaryStorage> storage = CreateStorage(isolation_key);
+  CHECK(storage);
   storages_.emplace(isolation_key, storage.get());
   return storage;
 }
--- services/network/shared_dictionary/shared_dictionary_manager.h
+++ services/network/shared_dictionary/shared_dictionary_manager.h
@@ -13,6 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "net/extras/shared_dictionary/shared_dictionary_storage_isolation_key.h"
 
+namespace base {
+namespace android {
+class ApplicationStatusListener;
+}  // namespace android
+class FilePath;
+}  //  namespace base
+
+namespace disk_cache {
+class BackendFileOperationsFactory;
+}  // namespace disk_cache
+
 namespace network {
 
 class SharedDictionaryStorage;
@@ -24,6 +35,17 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) SharedDictionaryManager {
   // Returns a SharedDictionaryManager which keeps the whole dictionary
   // information in memory.
   static std::unique_ptr<SharedDictionaryManager> CreateInMemory();
+
+  // Returns a SharedDictionaryManager which keeps the dictionary information
+  // on disk.
+  static std::unique_ptr<SharedDictionaryManager> CreateOnDisk(
+      const base::FilePath& database_path,
+      const base::FilePath& cache_directory_path,
+#if BUILDFLAG(IS_ANDROID)
+      base::android::ApplicationStatusListener* app_status_listener,
+#endif  // BUILDFLAG(IS_ANDROID)
+      scoped_refptr<disk_cache::BackendFileOperationsFactory>
+          file_operations_factory);
 
   // TODO(crbug.com/1413922): Implement a manager which supports persistence
   // and use if for non-incognito mode. Also, if preventing incognito mode
