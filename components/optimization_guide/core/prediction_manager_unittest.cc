49305d2e4c735348bcb38cabea0b4fbe8e0a5f69
rajendrant <rajendrant@chromium.org>
2023-05-12 21:44:51 +0000

    Support relative paths in model store
    Only store relative paths in the model store database w.r.t the base model store dir. This is behind feature and enabled for iOS.
    
    Bug: 1442722
    Change-Id: I831748e595674351005d14d61c22a54c65971a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521914
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143515}

--- components/optimization_guide/core/prediction_manager_unittest.cc
+++ components/optimization_guide/core/prediction_manager_unittest.cc
@@ -296,8 +296,10 @@ class TestOptimizationGuideStore : public OptimizationGuideStore {
  public:
   TestOptimizationGuideStore(
       std::unique_ptr<StoreEntryProtoDatabase> database,
+      const base::FilePath& base_model_store_dir,
       scoped_refptr<base::SequencedTaskRunner> store_task_runner)
       : OptimizationGuideStore(std::move(database),
+                               base_model_store_dir,
                                store_task_runner,
                                nullptr) {}
 
@@ -439,7 +441,7 @@ class PredictionManagerTestBase : public ProtoDatabaseProviderTestBase {
     auto db = std::make_unique<FakeDB<StoreEntry>>(&db_store_);
 
     return std::make_unique<TestOptimizationGuideStore>(
-        std::move(db), task_environment_.GetMainThreadTaskRunner());
+        std::move(db), temp_dir(), task_environment_.GetMainThreadTaskRunner());
   }
 
   TestPredictionManager* prediction_manager() const {
