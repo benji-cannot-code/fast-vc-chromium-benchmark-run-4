b57f3ce5a7efc8cfc9acb4e33fafacf31aad365e
Joe Antonetti <joeantonetti@google.com>
2023-05-12 18:31:59 +0000

    [QuickStart][2/n] Add Support for UserVerificationRequested
    This is part of a series of changes to parse the verification result.
    
    This change waits for a UserVerificationRequested message, and only
    listens for a result if a request was made.
    
    Tests: Added Tests
    Bug: b\280453645
    Change-Id: I53d49cbaec30a09d76983786a9f8bce1afb49cf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514475
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143420}

--- chromeos/ash/components/quick_start/fake_quick_start_decoder.h
+++ chromeos/ash/components/quick_start/fake_quick_start_decoder.h
@@ -37,6 +37,9 @@ class FakeQuickStartDecoder : public mojom::QuickStartDecoder {
   void DecodeNotifySourceOfUpdateResponse(
       const std::vector<uint8_t>& data,
       DecodeNotifySourceOfUpdateResponseCallback callback) override;
+  void DecodeUserVerificationRequested(
+      const std::vector<uint8_t>& data,
+      DecodeUserVerificationRequestedCallback callback) override;
   void DecodeUserVerificationResult(
       const std::vector<uint8_t>& data,
       DecodeUserVerificationResultCallback callback) override;
@@ -53,6 +56,8 @@ class FakeQuickStartDecoder : public mojom::QuickStartDecoder {
 
   void SetUserVerificationResponse(mojom::UserVerificationResult result,
                                    bool is_first_user_verification);
+
+  void SetUserVerificationRequested(bool is_awaiting_user_verification);
 
   void SetDecoderError(mojom::QuickStartDecoderError error);
 
@@ -74,6 +79,7 @@ class FakeQuickStartDecoder : public mojom::QuickStartDecoder {
   mojo::ReceiverSet<ash::quick_start::mojom::QuickStartDecoder> receiver_set_;
   absl::optional<bool> notify_source_of_update_response_;
   mojom::WifiCredentialsPtr credentials_;
+  mojom::UserVerificationRequestedPtr user_verification_request_;
   mojom::UserVerificationResponsePtr user_verification_response_;
   absl::optional<mojom::QuickStartDecoderError> error_;
 };
