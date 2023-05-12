1a7a26aed3c538e04ba5841ce75e8830ca093ed0
Peter Kasting <pkasting@chromium.org>
2023-05-12 02:45:42 +0000

    std::set::contains() does not exist prior to C++20.
    Use base::Contains() instead.
    
    Bug: 1444563
    Change-Id: I16d3a0f11d5bfdeb2e63067568a6eb8627e0e8f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523683
    Commit-Queue: Emily Stark <estark@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143049}

--- chrome/browser/ssl/https_only_mode_tab_helper.h
+++ chrome/browser/ssl/https_only_mode_tab_helper.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SSL_HTTPS_ONLY_MODE_TAB_HELPER_H_
 #define CHROME_BROWSER_SSL_HTTPS_ONLY_MODE_TAB_HELPER_H_
 
+#include "base/containers/contains.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -45,7 +46,7 @@ class HttpsOnlyModeTabHelper
   GURL fallback_url() const { return fallback_url_; }
 
   bool has_failed_upgrade(const GURL& url) {
-    return failed_upgrade_urls_.contains(url);
+    return base::Contains(failed_upgrade_urls_, url);
   }
   void add_failed_upgrade(const GURL& url) { failed_upgrade_urls_.insert(url); }
 
