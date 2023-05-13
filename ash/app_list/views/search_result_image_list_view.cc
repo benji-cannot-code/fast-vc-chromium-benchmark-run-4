3c7ccccde56b8112c4767eaf5344ca2ea6236ba6
Wen-Chien Wang <wcwang@chromium.org>
2023-05-13 01:15:31 +0000

    Add skeleton code for single image search result
    This cl adds the information section that shows beside the image search
    result when there is only one result. Currently the data is hard-coded
    and will be updated when the backend is connected.
    
    Demo: https://screenshot.googleplex.com/BisTx9VtPc6fbgc
    
    Bug: 1352636
    Change-Id: I9d6bf071787bab180b0e6b329fbdad1135116ff1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523594
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143603}

--- ash/app_list/views/search_result_image_list_view.cc
+++ ash/app_list/views/search_result_image_list_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/search_result_image_list_view.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/flex_layout.h"
+#include "ui/views/layout/table_layout_view.h"
 
 namespace ash {
 
@@ -34,6 +36,9 @@ constexpr int kPreferredTitleTopMargins = 12;
 constexpr int kPreferredTitleBottomMargins = 4;
 
 }  // namespace
+
+using views::LayoutAlignment;
+using views::TableLayout;
 
 SearchResultImageListView::SearchResultImageListView(
     AppListViewDelegate* view_delegate)
@@ -67,6 +72,40 @@ SearchResultImageListView::SearchResultImageListView(
     image_views_.back()->layer()->SetFillsBoundsOpaquely(false);
     image_views_.back()->SetVisible(true);
     image_view_container_->AddChildView(image_views_.back());
+  }
+
+  // TODO(crbug.com/1352636): replace mock results with real results.
+  std::vector<std::u16string> info_strings = {
+      u"3.46MB", u"Today 13:28", u"image/png", u"My files/Downloads/abc.png"};
+  std::vector<int> title_string_ids = {
+      IDS_ASH_SEARCH_RESULT_IMAGE_FILE_SIZE,
+      IDS_ASH_SEARCH_RESULT_IMAGE_DATE_MODIFIED,
+      IDS_ASH_SEARCH_RESULT_IMAGE_FILE_TYPE,
+      IDS_ASH_SEARCH_RESULT_IMAGE_FILE_LOCATION};
+  const views::Label::CustomFont title_font = {
+      views::Label::GetDefaultFontList().DeriveWithWeight(
+          gfx::Font::Weight::MEDIUM)};
+
+  auto append_image_info = [&](int idx) {
+    image_info_container_->AddChildView(std::make_unique<views::Label>(
+        l10n_util::GetStringUTF16(title_string_ids[idx]), title_font));
+    image_info_container_->AddChildView(
+        std::make_unique<views::Label>(info_strings[idx]));
+  };
+
+  image_info_container_ = image_view_container_->AddChildView(
+      std::make_unique<views::TableLayoutView>());
+  image_info_container_->SetVisible(false);
+  image_info_container_->AddColumn(
+      LayoutAlignment::kStart, LayoutAlignment::kStretch,
+      TableLayout::kFixedSize, TableLayout::ColumnSize::kUsePreferred, 0, 0);
+  image_info_container_->AddPaddingColumn(TableLayout::kFixedSize, 5);
+  image_info_container_->AddColumn(
+      LayoutAlignment::kStart, LayoutAlignment::kStretch, 1.0f,
+      TableLayout::ColumnSize::kUsePreferred, 0, 0);
+  image_info_container_->AddRows(title_string_ids.size(), 1.0f);
+  for (size_t i = 0; i < title_string_ids.size(); ++i) {
+    append_image_info(i);
   }
 }
 
@@ -150,6 +189,7 @@ int SearchResultImageListView::DoUpdate() {
       result_view->SetResult(nullptr);
     }
   }
+  image_info_container_->SetVisible(num_results == 1);
   SetVisible(num_results > 0);
   return num_results;
 }
--- ash/app_list/views/search_result_image_list_view.h
+++ ash/app_list/views/search_result_image_list_view.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 class BoxLayoutView;
+class TableLayoutView;
 class Label;
 }  // namespace views
 
@@ -48,6 +49,10 @@ class ASH_EXPORT SearchResultImageListView : public SearchResultContainerView {
   // Returns all search result image views children of this view.
   std::vector<SearchResultImageView*> GetSearchResultImageViews();
 
+  const views::TableLayoutView* image_info_container_for_test() const {
+    return image_info_container_.get();
+  }
+
  private:
   // Overridden from views::View:
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
@@ -64,6 +69,7 @@ class ASH_EXPORT SearchResultImageListView : public SearchResultContainerView {
   // Owned by views hierarchy.
   raw_ptr<views::Label> title_label_ = nullptr;
   raw_ptr<views::BoxLayoutView> image_view_container_ = nullptr;
+  raw_ptr<views::TableLayoutView> image_info_container_ = nullptr;
   std::vector<SearchResultImageView*> image_views_;
 };
 
