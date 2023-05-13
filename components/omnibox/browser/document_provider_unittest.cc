3cb17cd03fc2763f97b5b8ec9d8c0ea56d4a62b0
manukh <manukh@chromium.org>
2023-05-13 23:02:41 +0000

    [omnibox][doc] Display shorter URLs.
    https://screenshot.googleplex.com/7ReyxdCrmUns4Dj
    
    The remote doc response contains 2 URLs per document: `originalUrl` and
    `url`.
    - `originalUrl` is much shorter. E.g.
        https://drive.google.com/a/google.com/open?id=<id>&resourcekey=<key>
    
    - `url` is much longer. E.g.,
        https://www.google.com/url?sa=t&rct=j&esrc=s&source=
        appssearch&uact=8&cd=0&cad=rja&q=&sig2=9NB3hPvQWP_8Z-
        OMFJTsog&ved=0ahUKEwiB3e6Z_Ov-AhXxEvkAHZhmB0k4ABABKAAwAA&url
        =https://drive.google.com/a/google.com/open%3Fid%3D<id>
        %26resourcekey%3D<key>&usg=AOvVaw2q5Vrrjd92EfjX5jek6uxD
    
    Currently,
    - navigating (i.e. `destination_url`) uses the longer `url`.
    - omnibox display (i.e. `fill_into_edit`) uses the longer `url`
    - deduping (i.e. `stripped_destination_url`) uses the shorter
      `originalUrl`.
    
    With this CL, display will use the shorter `originalUrl`. Navigation
    will continue to use `url`.
    
    This allows copying doc URLs without having to navigate to them and wait
    for the redirects to complete. Though the redirected final URL still
    differs from the `originalUrl`.
    
    This CL also has the `stripped_destination_url` fallback to use `url`
    when `originalUrl` is empty.
    
    Bug: 841265
    Change-Id: I8cc7c33efd4c0c2fe70e4b12b5b136afa9fec4f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517892
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143722}

--- components/omnibox/browser/document_provider_unittest.cc
+++ components/omnibox/browser/document_provider_unittest.cc
@@ -360,12 +360,17 @@ TEST_F(DocumentProviderTest, ParseDocumentSearchResults) {
             GURL("https://documentprovider.tld/doc?id=1"));
   EXPECT_EQ(matches[0].relevance, 1234);  // Server-specified.
   EXPECT_EQ(matches[0].stripped_destination_url, GURL(kSampleStrippedURL));
+  EXPECT_EQ(matches[0].fill_into_edit,
+            base::UTF8ToUTF16(std::string(kSampleOriginalURL)));
 
   EXPECT_EQ(matches[1].contents, u"Document 2 longer title");
   EXPECT_EQ(matches[1].destination_url,
             GURL("https://documentprovider.tld/doc?id=2"));
   EXPECT_EQ(matches[1].relevance, 0);
-  EXPECT_TRUE(matches[1].stripped_destination_url.is_empty());
+  EXPECT_EQ(matches[1].stripped_destination_url,
+            GURL("http://documentprovider.tld/doc?id=2"));
+  EXPECT_EQ(matches[1].fill_into_edit,
+            u"https://documentprovider.tld/doc?id=2");
 
   EXPECT_EQ(matches[2].contents, u"Document 3 longer title");
   EXPECT_EQ(matches[2].destination_url,
@@ -375,6 +380,8 @@ TEST_F(DocumentProviderTest, ParseDocumentSearchResults) {
   // using |AutocompleteMatch::GURLToStrippedGURL()|.
   EXPECT_EQ(matches[2].stripped_destination_url,
             "http://sites.google.com/google.com/abc/def");
+  EXPECT_EQ(matches[2].fill_into_edit,
+            u"http://sites.google.com/google.com/abc/def");
 
   EXPECT_FALSE(provider_->backoff_for_session_);
 }
@@ -596,13 +603,15 @@ TEST_F(DocumentProviderTest, ParseDocumentSearchResultsBreakTies) {
   EXPECT_EQ(matches[1].destination_url,
             GURL("https://documentprovider.tld/doc?id=2"));
   EXPECT_EQ(matches[1].relevance, 1233);  // Tie demoted
-  EXPECT_TRUE(matches[1].stripped_destination_url.is_empty());
+  EXPECT_EQ(matches[1].stripped_destination_url,
+            GURL("http://documentprovider.tld/doc?id=2"));
 
   EXPECT_EQ(matches[2].contents, u"Document 3");
   EXPECT_EQ(matches[2].destination_url,
             GURL("https://documentprovider.tld/doc?id=3"));
   EXPECT_EQ(matches[2].relevance, 1232);  // Tie demoted, twice.
-  EXPECT_TRUE(matches[2].stripped_destination_url.is_empty());
+  EXPECT_EQ(matches[2].stripped_destination_url,
+            GURL("http://documentprovider.tld/doc?id=3"));
 
   EXPECT_FALSE(provider_->backoff_for_session_);
 }
@@ -657,7 +666,8 @@ TEST_F(DocumentProviderTest, ParseDocumentSearchResultsBreakTiesCascade) {
   EXPECT_EQ(matches[1].destination_url,
             GURL("https://documentprovider.tld/doc?id=2"));
   EXPECT_EQ(matches[1].relevance, 1233);  // Tie demoted
-  EXPECT_TRUE(matches[1].stripped_destination_url.is_empty());
+  EXPECT_EQ(matches[1].stripped_destination_url,
+            GURL("http://documentprovider.tld/doc?id=2"));
 
   EXPECT_EQ(matches[2].contents, u"Document 3");
   EXPECT_EQ(matches[2].destination_url,
@@ -665,7 +675,8 @@ TEST_F(DocumentProviderTest, ParseDocumentSearchResultsBreakTiesCascade) {
   // Document 2's demotion caused an implicit tie.
   // Ensure we demote this one as well.
   EXPECT_EQ(matches[2].relevance, 1232);
-  EXPECT_TRUE(matches[2].stripped_destination_url.is_empty());
+  EXPECT_EQ(matches[2].stripped_destination_url,
+            GURL("http://documentprovider.tld/doc?id=3"));
 
   EXPECT_FALSE(provider_->backoff_for_session_);
 }
@@ -720,14 +731,16 @@ TEST_F(DocumentProviderTest, ParseDocumentSearchResultsBreakTiesZeroLimit) {
   EXPECT_EQ(matches[1].destination_url,
             GURL("https://documentprovider.tld/doc?id=2"));
   EXPECT_EQ(matches[1].relevance, 0);  // Tie demoted
-  EXPECT_TRUE(matches[1].stripped_destination_url.is_empty());
+  EXPECT_EQ(matches[1].stripped_destination_url,
+            GURL("http://documentprovider.tld/doc?id=2"));
 
   EXPECT_EQ(matches[2].contents, u"Document 3");
   EXPECT_EQ(matches[2].destination_url,
             GURL("https://documentprovider.tld/doc?id=3"));
   // Tie is demoted further.
   EXPECT_EQ(matches[2].relevance, 0);
-  EXPECT_TRUE(matches[2].stripped_destination_url.is_empty());
+  EXPECT_EQ(matches[2].stripped_destination_url,
+            GURL("http://documentprovider.tld/doc?id=3"));
 
   EXPECT_FALSE(provider_->backoff_for_session_);
 }
