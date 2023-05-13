fd792dc09c2a8f99ebd8248753c58b380f89383e
Kuan Huang <kuanhuang@chromium.org>
2023-05-13 00:02:45 +0000

    Disable DebuggerExtensionApiTest.Debugger on Linux and Lacros
    Also disable SitePerProcessDebuggerExtensionApiTest.Debugger that seems
    have the same root cause and failing on similar builders. The tests are
    failed 14 times on CI sheriff builders in last 7 days.
    
    Bug: 1434257, 1440919
    Change-Id: I76e88f918309a88d59013750ab8de690192b0ed0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528755
    Auto-Submit: Kuan Huang <kuanhuang@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143582}

--- chrome/browser/extensions/api/debugger/debugger_apitest.cc
+++ chrome/browser/extensions/api/debugger/debugger_apitest.cc
@@ -664,7 +664,13 @@ class DebuggerExtensionApiTest : public ExtensionApiTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(DebuggerExtensionApiTest, Debugger) {
+// TODO(crbug/1434257): Flaky on Lacros and Linux.
+#if BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_LINUX)
+#define MAYBE_Debugger DISABLED_Debugger
+#else
+#define MAYBE_Debugger Debugger
+#endif
+IN_PROC_BROWSER_TEST_F(DebuggerExtensionApiTest, MAYBE_Debugger) {
   ASSERT_TRUE(RunExtensionTest("debugger")) << message_;
 }
 
@@ -743,8 +749,8 @@ class SitePerProcessDebuggerExtensionApiTest : public DebuggerExtensionApiTest {
   }
 };
 
-// TODO(crbug/1444625): Flaky on Lacros.
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
+// TODO(crbug/1440919): Flaky on Lacros and Linux.
+#if BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_LINUX)
 #define MAYBE_Debugger DISABLED_Debugger
 #else
 #define MAYBE_Debugger Debugger
