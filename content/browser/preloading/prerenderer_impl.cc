014c9779f769281e67f5af04a3b6e4338fe98c55
Hiroki Nakagawa <nhiroki@chromium.org>
2023-05-13 01:35:09 +0000

    Prerender: Break down PrerenderFinalStatus::kTriggerDestroyed
    After this change, remaining usages of kTriggerDestroyed are the
    destructor of PrerendererImpl for speculation rules trigger and the
    destructor of PrerenderHandleImpl for embedder triggers.
    
    Bug: 1401246
    Change-Id: Iaea7c36d4ec228fabe58f7eea561bed1dc74f273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4515841
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143612}

--- content/browser/preloading/prerenderer_impl.cc
+++ content/browser/preloading/prerenderer_impl.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/preloading/preloading.h"
 #include "content/browser/preloading/prerender/prerender_attributes.h"
-#include "content/browser/preloading/prerender/prerender_final_status.h"
 #include "content/browser/preloading/prerender/prerender_host_registry.h"
 #include "content/browser/preloading/prerender/prerender_metrics.h"
 #include "content/browser/preloading/prerender/prerender_navigation_utils.h"
@@ -49,7 +48,7 @@ PrerendererImpl::PrerendererImpl(RenderFrameHost& render_frame_host)
 
 PrerendererImpl::~PrerendererImpl() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  CancelStartedPrerenders();
+  CancelStartedPrerenders(PrerenderFinalStatus::kTriggerDestroyed);
 }
 
 void PrerendererImpl::PrimaryPageChanged(Page& page) {
@@ -62,7 +61,7 @@ void PrerendererImpl::PrimaryPageChanged(Page& page) {
   // before the next primary page swaps in so that the next page can trigger a
   // new prerender without hitting the max number of running prerenders.
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  CancelStartedPrerenders();
+  CancelStartedPrerenders(PrerenderFinalStatus::kTriggerPageNavigated);
 }
 
 // TODO(isaboori) Part of the logic in |ProcessCandidatesForPrerender| method is
@@ -149,9 +148,9 @@ void PrerendererImpl::ProcessCandidatesForPrerender(
     started_it = equal_prerender_end;
   }
 
-  registry_->CancelHosts(
-      removed_prerender_rules,
-      PrerenderCancellationReason(PrerenderFinalStatus::kTriggerDestroyed));
+  registry_->CancelHosts(removed_prerender_rules,
+                         PrerenderCancellationReason(
+                             PrerenderFinalStatus::kSpeculationRuleRemoved));
   {
     base::flat_set<int> removed_prerender_rules_set(
         removed_prerender_rules.begin(), removed_prerender_rules.end());
@@ -283,15 +282,15 @@ bool PrerendererImpl::ShouldWaitForPrerenderResult(const GURL& url) {
   return begin != end;
 }
 
-void PrerendererImpl::CancelStartedPrerenders() {
+void PrerendererImpl::CancelStartedPrerenders(
+    PrerenderFinalStatus final_status) {
   if (registry_) {
     std::vector<int> started_prerender_ids;
     for (auto& prerender_info : started_prerenders_) {
       started_prerender_ids.push_back(prerender_info.prerender_host_id);
     }
-    registry_->CancelHosts(
-        started_prerender_ids,
-        PrerenderCancellationReason(PrerenderFinalStatus::kTriggerDestroyed));
+    registry_->CancelHosts(started_prerender_ids,
+                           PrerenderCancellationReason(final_status));
   }
 
   started_prerenders_.clear();
