7dd950ae8374f05b03db598375be2948bacd1375
Avi Drissman <avi@chromium.org>
2023-05-14 17:59:31 +0000

    Convert /components/ui_devtools to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Bug: 1280317
    Change-Id: I6644855b73b47d29f05145f0dbf91fc42e540c96
    Include-Ci-Only-Tests: true
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4530076
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143795}

--- components/ui_devtools/views/dom_agent_mac.mm
+++ components/ui_devtools/views/dom_agent_mac.mm
@@ -11,9 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ui_devtools/views/widget_element.h"
 #include "ui/views/widget/native_widget_mac.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace ui_devtools {
 
-DOMAgentMac::DOMAgentMac() {}
+DOMAgentMac::DOMAgentMac() = default;
 
 DOMAgentMac::~DOMAgentMac() {
   CHECK(!IsInObserverList());
@@ -68,7 +72,7 @@ std::unique_ptr<protocol::DOM::Node> DOMAgentMac::BuildTreeForWindow(
 }
 
 void DOMAgentMac::InitializeRootsFromOpenWindows() {
-  for (NSWindow* window : [NSApp windows]) {
+  for (NSWindow* window in NSApp.windows) {
     if (views::Widget* widget =
             views::Widget::GetWidgetForNativeWindow(window)) {
       widget->AddObserver(this);
