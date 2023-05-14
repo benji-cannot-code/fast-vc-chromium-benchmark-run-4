01331528a054eb8811c41898b713910c31de4120
Julie Jeongeun Kim <jkim@igalia.com>
2023-05-14 10:59:10 +0000

    [iOS] Introduce BluetoothChooserIOS and BluetoothDeviceList
    This CL introduces BluetoothChooserIOS and BluetoothDeviceList
    and supports a basic functionality for choosing a bluetooth device.
    
    BluetoothChooserIOS has BluetoothDeviceList that holds UITableView
    to manage a device list. UITableView is loaded as a popover and a
    device is added to UITableView or updated when
    BluetoothChooserIOS::AddOrUpdateDevice() is called.
    
    Bug: 1431447
    Change-Id: Ie243c97dae0f40549772f9918e12bd6fabb23e25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430271
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143764}

---
+++ content/shell/browser/bluetooth/ios/shell_bluetooth_device_list_consumer.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_SHELL_BROWSER_BLUETOOTH_IOS_SHELL_BLUETOOTH_DEVICE_LIST_CONSUMER_H_
+#define CONTENT_SHELL_BROWSER_BLUETOOTH_IOS_SHELL_BLUETOOTH_DEVICE_LIST_CONSUMER_H_
+
+#import <Foundation/Foundation.h>
+
+// Consumer for model to push configurations to the device list UI.
+@protocol ShellBluetoothDeviceListConsumer <NSObject>
+
+// Adds a cell for a new device to the UITableView or updates the information of
+// an existing cell.
+- (void)addOrUpdateDevice:(NSString*)deviceID
+         shouldUpdateName:(bool)shouldUpdateName
+               deviceName:(NSString*)deviceName
+            gattConnected:(bool)gattConnected
+      signalStrengthLevel:(NSInteger)signalStrengthLevel;
+
+@end
+
+#endif  // CONTENT_SHELL_BROWSER_BLUETOOTH_IOS_SHELL_BLUETOOTH_DEVICE_LIST_CONSUMER_H_
--- content/shell/browser/bluetooth/shell_bluetooth_delegate_impl_client.cc
+++ content/shell/browser/bluetooth/shell_bluetooth_delegate_impl_client.cc
@@ -5,7 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/browser/bluetooth/shell_bluetooth_delegate_impl_client.h"
 
+#include "build/build_config.h"
 #include "content/public/browser/render_frame_host.h"
+
+#if BUILDFLAG(IS_IOS)
+#include "content/shell/browser/bluetooth/ios/shell_bluetooth_chooser_ios.h"
+#endif
 
 namespace content {
 
@@ -24,8 +29,11 @@ std::unique_ptr<content::BluetoothChooser>
 ShellBluetoothDelegateImplClient::RunBluetoothChooser(
     RenderFrameHost* frame,
     const BluetoothChooser::EventHandler& event_handler) {
-  // TODO(crbug.com/1431447): Implement BluetoothChooser for iOS.
+#if BUILDFLAG(IS_IOS)
+  return std::make_unique<ShellBluetoothChooserIOS>(frame, event_handler);
+#else
   return nullptr;
+#endif
 }
 
 std::unique_ptr<BluetoothScanningPrompt>
