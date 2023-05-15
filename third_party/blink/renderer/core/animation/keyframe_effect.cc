0c317458fcefa2b030a90b85237678f4ef04008c
Anders Hartvoll Ruud <andruud@chromium.org>
2023-05-15 23:22:24 +0000

    [scroll-animations] Avoid ViewTimeline in getKeyframes
    As of CL:4519273 we can resolve timeline offsets without
    down-casting to ViewTimeline. This prepares for Deferred Timelines v2,
    where we can't rely on scroll-driven timelines being a specific type.
    
    Bug: 1425939
    Change-Id: Ie778fba0b9ee24c1b4d64a214eea306884fb161f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4534339
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144444}

--- third_party/blink/renderer/core/animation/keyframe_effect.cc
+++ third_party/blink/renderer/core/animation/keyframe_effect.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/sampled_effect.h"
 #include "third_party/blink/renderer/core/animation/timing_calculations.h"
 #include "third_party/blink/renderer/core/animation/timing_input.h"
-#include "third_party/blink/renderer/core/animation/view_timeline.h"
 #include "third_party/blink/renderer/core/css/parser/css_selector_parser.h"
 #include "third_party/blink/renderer/core/css/properties/css_property_ref.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
@@ -304,9 +303,8 @@ HeapVector<ScriptValue> KeyframeEffect::getKeyframes(
     ScriptState* script_state) {
   if (Animation* animation = GetAnimation()) {
     animation->FlushPendingUpdates();
-    if (ViewTimeline* view_timeline =
-            DynamicTo<ViewTimeline>(animation->timeline())) {
-      view_timeline->ResolveTimelineOffsets();
+    if (AnimationTimeline* timeline = animation->timeline()) {
+      animation->ResolveTimelineOffsets(timeline->GetTimelineRange());
     }
   }
 
