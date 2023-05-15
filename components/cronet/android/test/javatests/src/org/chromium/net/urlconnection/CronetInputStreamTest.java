4952976008425768ba2c2b8c069b7a948a811625
Stefano Duo <stefanoduo@google.com>
2023-05-15 10:02:26 +0000

    Batch CronetInputStreamTest
    These are effectively unittests, batch as such.
    See associated bug for more context.
    
    Bug: 1442885
    Cq-Include-Trybots: luci.chromium.try:android-cronet-arm-dbg,android-cronet-arm64-dbg,android-cronet-arm64-rel,android-cronet-x86-dbg,android-cronet-x86-dbg-11-tests,android-cronet-x86-dbg-10-tests,android-cronet-x86-dbg-pie-tests,android-cronet-x86-rel,android-cronet-x86-rel-kitkat-tests,android_cronet,ios-m1-simulator-cronet,ios-simulator-cronet
    Change-Id: I69298e012c63797d98cc1e287b01ebaaa53087cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507324
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Dan Stahr <danstahr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143970}

--- components/cronet/android/test/javatests/src/org/chromium/net/urlconnection/CronetInputStreamTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/urlconnection/CronetInputStreamTest.java
@@ -14,6 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.Batch;
 import org.chromium.net.CronetTestRule;
 
 import java.io.IOException;
@@ -21,6 +22,7 @@ import java.nio.ByteBuffer;
 import java.util.concurrent.Callable;
 
 /** Test for {@link CronetInputStream}. */
+@Batch(Batch.UNIT_TESTS)
 @RunWith(AndroidJUnit4.class)
 public class CronetInputStreamTest {
     @Rule
