aa32ba9196c2450e6a665280e23933f1884bc79c
Zhiyuan Cai <zhiyuancai@chromium.org>
2023-05-15 17:12:58 +0000

    [PriceInsights] Add a new side panel entry
    This CL adds a new side panel entry for Price Insights. The coordinator
    will be added in a followup CL to handle this new panel's behaviors.
    
    Bug: b:282018949
    Change-Id: I4714ff3acb1d92b92aa43abfd5d7d880f7f11422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519482
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144189}

--- chrome/browser/ui/side_panel/side_panel_entry_id.h
+++ chrome/browser/ui/side_panel/side_panel_entry_id.h
@@ -25,6 +25,7 @@ enum class SidePanelEntryId {
   kAboutThisSite,
   kCustomizeChrome,
   kSearchCompanion,
+  kShoppingInsights,
   // Extensions (nothing more should be added below here)
   kExtension
 };
