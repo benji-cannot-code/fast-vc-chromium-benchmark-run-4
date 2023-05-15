0c03c1d698a512a4f50ae7dd57102fa9feac2c2b
Alex Ilin <alexilin@chromium.org>
2023-05-15 10:26:43 +0000

    [BSC] Add UnexportableKeyLoader class
    This CL adds a helper UnexportableKeyLoader class that allows scheduling
    tasks on UnexportableKeyService before a key is loaded.
    
    Bug: b/263249728
    Change-Id: Ib2ca96e9bd13b9cea95be7343b3dc0f705159357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454999
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Kristian Monsen <kristianm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143982}

--- components/unexportable_keys/service_error.h
+++ components/unexportable_keys/service_error.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace unexportable_keys {
 
-// Various errors returned by `UnexportableKeyService`.
+// Various errors returned by this component.
 enum class ServiceError {
   // crypto:: operation returned an error.
   kCryptoApiFailed,
@@ -22,7 +22,9 @@ enum class ServiceError {
   // Unexportable key provider is not available on this platform.
   kNoKeyProvider,
   // None of the requested algorithms are supported by the key provider.
-  kAlgorithmNotSupported
+  kAlgorithmNotSupported,
+  // The key object hasn't been created yet. Try again later.
+  kKeyNotReady,
 };
 
 // Return value for methods which perform unexportable keys operations that may
