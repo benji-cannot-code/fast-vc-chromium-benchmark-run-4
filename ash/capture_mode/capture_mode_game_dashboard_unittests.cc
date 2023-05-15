37e52b1d305a50f0450e0054c13680d68e83b117
minch <minch@google.com>
2023-05-15 17:56:29 +0000

    capture_game: Capture bar position anchored to the window
    Fixed: b/280465981
    Change-Id: I8e55b9f55f8a5cdbf36759fbfb5ae35388b8346b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517887
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144218}

--- ash/capture_mode/capture_mode_game_dashboard_unittests.cc
+++ ash/capture_mode/capture_mode_game_dashboard_unittests.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ash/capture_mode/capture_mode_constants.h"
 #include "ash/capture_mode/capture_mode_controller.h"
 #include "ash/capture_mode/capture_mode_metrics.h"
 #include "ash/capture_mode/capture_mode_session.h"
@@ -23,6 +24,8 @@ class GameDashboardCaptureModeTest : public AshTestBase {
   GameDashboardCaptureModeTest& operator=(const GameDashboardCaptureModeTest&) =
       delete;
   ~GameDashboardCaptureModeTest() override = default;
+
+  aura::Window* window() const { return window_.get(); }
 
   // AshTestBase:
   void SetUp() override {
@@ -97,6 +100,21 @@ TEST_F(GameDashboardCaptureModeTest, CaptureBar) {
   EXPECT_FALSE(GetWindowToggleButton());
   EXPECT_TRUE(GetSettingsButton());
   EXPECT_TRUE(GetCloseButton());
+}
+
+TEST_F(GameDashboardCaptureModeTest, CaptureBarPosition) {
+  StartGameCaptureModeSession();
+  views::Widget* bar_widget = GetCaptureModeBarWidget();
+  ASSERT_TRUE(bar_widget);
+
+  const gfx::Rect window_bounds = window()->GetBoundsInScreen();
+  const gfx::Rect bar_bounds = bar_widget->GetWindowBoundsInScreen();
+  // Checks that the game capture bar is inside the window. And centered above a
+  // constant distance from the bottom of the window.
+  EXPECT_TRUE(window_bounds.Contains(bar_bounds));
+  EXPECT_EQ(bar_bounds.CenterPoint().x(), window_bounds.CenterPoint().x());
+  EXPECT_EQ(bar_bounds.bottom() + capture_mode::kCaptureBarBottomPadding,
+            window_bounds.bottom());
 }
 
 }  // namespace ash
