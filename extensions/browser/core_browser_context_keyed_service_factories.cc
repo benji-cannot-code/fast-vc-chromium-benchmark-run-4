545d082130a7e42c7493822959663d4973b3639e
Ewelina Baran <ebaran@opera.com>
2023-05-15 08:33:15 +0000

    Add missing ExtensionPrefHelper service registration
    Bug: None
    Change-Id: Ib6b89c6e5ce4500301929ce9791de7e1d8bd0cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521492
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Ewelina Barań <ebaran@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1143919}

--- extensions/browser/core_browser_context_keyed_service_factories.cc
+++ extensions/browser/core_browser_context_keyed_service_factories.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_function.h"
 #include "extensions/browser/extension_message_filter.h"
 #include "extensions/browser/extension_prefs_factory.h"
+#include "extensions/browser/extension_prefs_helper_factory.h"
 #include "extensions/browser/extension_protocols.h"
 #include "extensions/browser/extension_service_worker_message_filter.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_stream_manager.h"
@@ -33,6 +34,7 @@ void EnsureCoreBrowserContextKeyedServiceFactoriesBuilt() {
   ExtensionMessageFilter::EnsureShutdownNotifierFactoryBuilt();
   ExtensionServiceWorkerMessageFilter::EnsureShutdownNotifierFactoryBuilt();
   ExtensionPrefsFactory::GetInstance();
+  ExtensionPrefsHelperFactory::GetInstance();
   ImageLoaderFactory::GetInstance();
   MimeHandlerStreamManager::EnsureFactoryBuilt();
   ProcessManagerFactory::GetInstance();
