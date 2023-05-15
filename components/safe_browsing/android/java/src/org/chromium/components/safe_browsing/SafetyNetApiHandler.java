ffb227ece2602f23046e21f39c4f99c0760c4819
Xinghui Lu <xinghuilu@chromium.org>
2023-05-15 21:45:04 +0000

    [1/3] Rename SafeBrowsingApiHandler to SafetyNetApiHandler
    We will migrate to a new Safe Browsing API that is built in a
    dedicated SafeBrowsing module, rename the current handler to
    SafetyNetApiHandler.
    
    This is a three-sided patch:
    1) (this CL) Land new class name with additional compatibility shims.
    2) Rename in downstream.
    3) Rename in upstream and remove the compatibility shims.
    
    Bug: 1444515
    Change-Id: I8e96df3998c2bd083caab2639f4ddff3823a86a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4529004
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144399}

---
+++ components/safe_browsing/android/java/src/org/chromium/components/safe_browsing/SafetyNetApiHandler.java
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.safe_browsing;
+
+/**
+ * Java interface that a SafetyNetApiHandler must implement when used with
+ * {@code SafeBrowsingApiBridge}.
+ * TODO(crbug.com/1444515): Port SafeBrowsingApiHandler to this interface.
+ */
+public interface SafetyNetApiHandler extends SafeBrowsingApiHandler {}
