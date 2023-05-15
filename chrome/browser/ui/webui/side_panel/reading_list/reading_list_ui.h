e4114c0c6e579e6a929affba0cf5ffc79f4554fc
John Lee <johntlee@chromium.org>
2023-05-15 19:52:06 +0000

    CR2023: Add color pipeline to Reading List
    Bug: 1399976
    Change-Id: Ie02cc023f166c9872e3f2b1fe75d2d08643b5e44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4527480
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144308}

--- chrome/browser/ui/webui/side_panel/reading_list/reading_list_ui.h
+++ chrome/browser/ui/webui/side_panel/reading_list/reading_list_ui.h
@@ -15,9 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "ui/webui/mojo_bubble_web_ui_controller.h"
+#include "ui/webui/resources/cr_components/color_change_listener/color_change_listener.mojom.h"
 #include "ui/webui/resources/cr_components/help_bubble/help_bubble.mojom.h"
 
 class ReadingListPageHandler;
+
+namespace ui {
+class ColorChangeHandler;
+}
 
 class ReadingListUI : public ui::MojoBubbleWebUIController,
                       public reading_list::mojom::PageHandlerFactory,
@@ -27,6 +32,10 @@ class ReadingListUI : public ui::MojoBubbleWebUIController,
   ReadingListUI(const ReadingListUI&) = delete;
   ReadingListUI& operator=(const ReadingListUI&) = delete;
   ~ReadingListUI() override;
+
+  void BindInterface(
+      mojo::PendingReceiver<color_change_listener::mojom::PageHandler>
+          pending_receiver);
 
   // Instantiates the implementor of the mojom::PageHandlerFactory mojo
   // interface passing the pending receiver that will be internally bound.
@@ -55,6 +64,7 @@ class ReadingListUI : public ui::MojoBubbleWebUIController,
   mojo::Receiver<reading_list::mojom::PageHandlerFactory>
       page_factory_receiver_{this};
 
+  std::unique_ptr<ui::ColorChangeHandler> color_provider_handler_;
   std::unique_ptr<user_education::HelpBubbleHandler> help_bubble_handler_;
   mojo::Receiver<help_bubble::mojom::HelpBubbleHandlerFactory>
       help_bubble_handler_factory_receiver_{this};
