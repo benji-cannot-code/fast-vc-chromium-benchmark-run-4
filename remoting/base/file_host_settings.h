8b1a8fa041352a9114aa1ca0813037a4ddb07c09
Andrew Rayskiy <greengrape@google.com>
2023-05-15 11:53:52 +0000

    [CodeHealth] Replace SetStringKey() calls in remoting/
    Bug: 1439196
    Change-Id: I07e969d7bc2ce759e38a70128557915fb3802b41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520925
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144019}

--- remoting/base/file_host_settings.h
+++ remoting/base/file_host_settings.h
@@ -10,11 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/values.h"
 #include "remoting/base/host_settings.h"
-
-namespace base {
-class Value;
-}
 
 namespace remoting {
 
@@ -46,7 +43,7 @@ class FileHostSettings final : public HostSettings {
 
   // TODO(yuweih): This needs to be guarded with a lock if we detect changes of
   // the settings file.
-  std::unique_ptr<base::Value> settings_;
+  absl::optional<base::Value::Dict> settings_;
 };
 
 }  // namespace remoting
