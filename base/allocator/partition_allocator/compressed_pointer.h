9ee7476c75434d6b9583fbc6a706839617b414ba
Kalvin Lee <kdlee@chromium.org>
2023-05-15 05:26:48 +0000

    [PA] Make glued pools a buildflag
    This change is trivial.
    
    As gluing core pools is only dependent on the build config and the "has
    64-bit pointers" buildflag, we can also convert
    `PA_CONFIG(GLUE_CORE_POOLS)` into a buildflag. This change does so
    trivially.
    
    Change-Id: Id9140f7ab22faa42d788834bcd458630310fee6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4511936
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143868}

--- base/allocator/partition_allocator/compressed_pointer.h
+++ base/allocator/partition_allocator/compressed_pointer.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PA_CONFIG(POINTER_COMPRESSION)
 
-#if !PA_CONFIG(GLUE_CORE_POOLS)
+#if !BUILDFLAG(GLUE_CORE_POOLS)
 #error "Pointer compression only works with glued pools"
 #endif
 #if PA_CONFIG(DYNAMICALLY_SELECT_POOL_SIZE)
