4642330dc2d44c1572e0f18b9cf9bfd0bb9bb33f
Judy Wang <shidi@chromium.org>
2023-05-15 21:00:59 +0000

    CrosNext: Fix desk textfield background unhide issue.
    The DeskTextField is using OverviewHighlight to trace when to
    show/hide the background. This fix ensure the highlightview is
    always updated.
    
    Test: Manual
    Change-Id: I25a932b6706d3ce118514d5dcd3b2ed1f3a0fd17
    Fixed: b/278916321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4529398
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144366}

--- ash/wm/overview/overview_utils.cc
+++ ash/wm/overview/overview_utils.cc
@@ -347,8 +347,7 @@ void UpdateOverviewHighlightForFocus(OverviewHighlightableView* target_view) {
                                    ->highlight_controller();
   DCHECK(highlight_controller);
 
-  if (highlight_controller->IsFocusHighlightVisible())
-    highlight_controller->MoveHighlightToView(target_view);
+  highlight_controller->MoveHighlightToView(target_view);
 }
 
 void UpdateOverviewHighlightForFocusAndSpokenFeedback(
@@ -361,10 +360,9 @@ void UpdateOverviewHighlightForFocusAndSpokenFeedback(
                                    ->overview_session()
                                    ->highlight_controller();
   DCHECK(highlight_controller);
-
-  if (highlight_controller->IsFocusHighlightVisible() ||
-      a11y_controller->spoken_feedback().enabled()) {
-    highlight_controller->MoveHighlightToView(target_view);
+  DCHECK(a11y_controller);
+  if (a11y_controller->spoken_feedback().enabled()) {
+    UpdateOverviewHighlightForFocus(target_view);
   }
 }
 
