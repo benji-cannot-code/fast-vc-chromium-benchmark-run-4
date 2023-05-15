be186d1f72aa7290a283dbe92c35554c8ddd5068
Peter Marshall <petermarshall@chromium.org>
2023-05-15 01:16:47 +0000

    [Office upload] Update errors when Drive is not mounted
    We can detect when Drive is not mounted, which can be because Drive
    is explicitly disabled in Settings. Prioritise this error over the
    offline error for the fallback dialog, because offline checks
    whether Drive is connected, which can lead to the offline error
    showing when the device is actually online.
    
    Clean up some other categories of fallback errors that are no
    longer used.
    
    Bug: b/281773568
    Change-Id: I6edf42403aee7bdf8f811dc39fc77c26556b197b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520494
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143833}

--- chrome/browser/ui/webui/ash/cloud_upload/drive_upload_handler.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/drive_upload_handler.cc
@@ -121,7 +121,7 @@ void DriveUploadHandler::Run(UploadCallback callback) {
   }
 
   if (!drive_integration_service_) {
-    OnEndUpload(GURL(), "No drive integration service");
+    OnEndUpload(GURL(), "No Drive integration service");
     return;
   }
 
@@ -130,6 +130,11 @@ void DriveUploadHandler::Run(UploadCallback callback) {
 
   // Observe Drive updates.
   drive_integration_service_->GetDriveFsHost()->AddObserver(this);
+
+  if (!drive_integration_service_->IsMounted()) {
+    OnEndUpload(GURL(), "Google Drive is not mounted");
+    return;
+  }
 
   // Destination url.
   base::FilePath destination_folder_path =
