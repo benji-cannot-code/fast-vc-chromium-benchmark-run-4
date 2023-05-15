80a6f45743f74a262ca65ce82409cc59856b1ca4
Gang Wu <gangwu@chromium.org>
2023-05-15 21:06:27 +0000

    Check in the skeleton for the auxiliary search
    This CL checks in the protobuff to carry information for auxiliary
    search. This CL also contains the JNI call to pass the protobuff from
    native to java.
    
    Bug:1445112
    
    Change-Id: If639aa97ef532a2708affc6e6e3fea63b5ccb509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4525736
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144372}

---
+++ chrome/browser/android/auxiliary_search/auxiliary_search_provider.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_AUXILIARY_SEARCH_AUXILIARY_SEARCH_PROVIDER_H_
+#define CHROME_BROWSER_ANDROID_AUXILIARY_SEARCH_AUXILIARY_SEARCH_PROVIDER_H_
+
+#include <jni.h>
+
+#include "base/android/scoped_java_ref.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+class Profile;
+
+// AuxiliarySearchProvider is responsible for providing the necessary
+// information for the auxiliary search..
+class AuxiliarySearchProvider : public KeyedService {
+ public:
+  explicit AuxiliarySearchProvider(Profile* profile);
+
+  base::android::ScopedJavaLocalRef<jbyteArray> GetSearchableData(
+      JNIEnv* env) const;
+
+ private:
+  ~AuxiliarySearchProvider() override;
+
+  raw_ptr<Profile> profile_;
+};
+
+#endif  // CHROME_BROWSER_ANDROID_AUXILIARY_SEARCH_AUXILIARY_SEARCH_PROVIDER_H_
