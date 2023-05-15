00958dbda1e0f19645684d46bb22251445463ba9
Paul Semel <paulsemel@chromium.org>
2023-05-15 15:26:55 +0000

    DanglingPtr: mark dangling ptr in RasterImplementation
    Bug: 1431684,1444166
    Change-Id: I8e298c268c8fdc969d7f72a350f057e9a3236368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517533
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144119}

--- gpu/command_buffer/client/raster_implementation.cc
+++ gpu/command_buffer/client/raster_implementation.cc
@@ -1734,7 +1734,8 @@ void RasterImplementation::ReadbackYUVPixelsAsync(
   yuv_request_queue_.push(std::move(request));
   SignalQuery(query,
               base::BindOnce(&RasterImplementation::OnAsyncYUVReadbackDone,
-                             base::Unretained(this), request_ptr));
+                             base::Unretained(this),
+                             base::UnsafeDanglingUntriaged(request_ptr)));
 }
 
 void RasterImplementation::OnAsyncYUVReadbackDone(
