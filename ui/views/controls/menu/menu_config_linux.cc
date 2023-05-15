da986b57fc89c2020eb0e98c88f909e41eff7251
Allen Bauer <kylixrd@chromium.org>
2023-05-15 20:15:02 +0000

    [CR2023] Update menu metrics and painting for MacOS.
    The menu margin is too wide on MacOS. The highlight painting of the Zoom row buttons are always square and with CR2023 they need to be rounded.
    
    Bug: 1427667
    Change-Id: I65041f78e0ce27ee0aa6945ed24c8016e5473287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4522708
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144330}

--- ui/views/controls/menu/menu_config_linux.cc
+++ ui/views/controls/menu/menu_config_linux.cc
@@ -5,10 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/controls/menu/menu_config.h"
 
+#include "ui/base/ui_base_features.h"
+
 namespace views {
 
 void MenuConfig::Init() {
   arrow_to_edge_padding = 6;
+  // Set Linux specific metrics for CR2023
+  if (features::IsChromeRefresh2023()) {
+    item_horizontal_border_padding = 12;
+  }
 }
 
 }  // namespace views
