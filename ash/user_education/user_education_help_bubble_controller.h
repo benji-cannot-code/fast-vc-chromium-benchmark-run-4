05af2d4a1d37938f0385fb663b152086e066a7a5
David Black <dmblack@google.com>
2023-05-15 19:11:50 +0000

    Expose ability to set close callback when creating help bubbles.
    This allows help bubble creators to react to help bubble close events.
    The immediate use case is for the holding space tour in which we will
    prohibit the shelf from auto-hiding while a help bubble is being shown
    above the holding space tray.
    
    Bug: b:279040829
    Change-Id: I27326843d7b6016ba803ca02afa83566e63559cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528959
    Reviewed-by: Angus McLean <angusmclean@google.com>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144288}

--- ash/user_education/user_education_help_bubble_controller.h
+++ ash/user_education/user_education_help_bubble_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/callback_list.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 
 namespace ui {
@@ -46,12 +47,14 @@ class ASH_EXPORT UserEducationHelpBubbleController {
   // specified `help_bubble_params` for the tracked element associated with the
   // specified `element_id` in the specified `element_context`. A help bubble
   // may not be created under certain circumstances, e.g. if there is already a
-  // help bubble showing or if there is an ongoing tutorial running.
+  // help bubble showing or if there is an ongoing tutorial running. Iff a help
+  // bubble was created, `close_callback` is run when the help bubble is closed.
   // NOTE: Currently only the primary user profile is supported.
   bool CreateHelpBubble(HelpBubbleId help_bubble_id,
                         user_education::HelpBubbleParams help_bubble_params,
                         ui::ElementIdentifier element_id,
-                        ui::ElementContext element_context);
+                        ui::ElementContext element_context,
+                        base::OnceClosure close_callback = base::DoNothing());
 
  private:
   // The delegate owned by the `UserEducationController` which facilitates
--- ash/user_education/user_education_help_bubble_controller_unittest.cc
+++ ash/user_education/user_education_help_bubble_controller_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/user_education/mock_user_education_delegate.h"
 #include "ash/user_education/user_education_ash_test_base.h"
 #include "ash/user_education/user_education_types.h"
+#include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/user_education/common/help_bubble.h"
 #include "components/user_education/common/help_bubble_params.h"
@@ -153,9 +154,15 @@ TEST_F(UserEducationHelpBubbleControllerTest, CreateHelpBubble) {
 
   // The `controller()` should indicate to the caller success when attempting to
   // create a new `help_bubble` since the previous `help_bubble` was closed.
+  // Note that this time a `close_callback` is provided.
+  base::MockOnceClosure close_callback;
   EXPECT_TRUE(controller()->CreateHelpBubble(
-      HelpBubbleId::kTest, HelpBubbleParams(), kElementId, element_context));
+      HelpBubbleId::kTest, HelpBubbleParams(), kElementId, element_context,
+      close_callback.Get()));
   Mock::VerifyAndClearExpectations(user_education_delegate());
+
+  // Expect that closing the `help_bubble` will invoke `close_callback`.
+  EXPECT_CALL(close_callback, Run);
 
   // Close the `help_bubble`.
   ASSERT_TRUE(help_bubble);
