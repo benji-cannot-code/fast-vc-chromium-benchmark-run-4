cbea34f4c51ed948c487e244faf1a59369b77cf9
Roland Bock <rbock@google.com>
2023-05-15 10:23:54 +0000

    Remove AuthPolicyHelper
    This CL removes the remaining call sites of AuthPolicyHelper and also
    AuthPolicyHelper itself.
    
    Bug: b/281994674
    Change-Id: Ic8c163afce55933fc0a5cf0fcfee9260578f4f52
    Tests: Remaining tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518273
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143980}

--- chrome/browser/ash/login/wizard_controller.h
+++ chrome/browser/ash/login/wizard_controller.h
@@ -238,9 +238,6 @@ class WizardController : public OobeUI::Observer {
   void ShowGaiaPasswordChangedScreenLegacy(const AccountId& account_id,
                                            bool has_error);
 
-  // Configure and show active directory password change screen.
-  void ShowActiveDirectoryPasswordChangeScreen(const std::string& username);
-
   // Configure and show the signin fatal error screen.
   void ShowSignInFatalErrorScreen(SignInFatalErrorScreen::Error error,
                                   base::Value::Dict params);
@@ -350,8 +347,7 @@ class WizardController : public OobeUI::Observer {
   // `exit_reason` is the screen specific exit reason reported by the screen.
   void OnScreenExit(OobeScreenId screen, const std::string& exit_reason);
 
-  // Advances either to Gaia screen or Active Directory login screen, depending
-  // on the device state.
+  // Advances to Gaia login screen.
   void AdvanceToSigninScreen();
 
   // Exit handlers:
@@ -391,7 +387,6 @@ class WizardController : public OobeUI::Observer {
   void OnUpdateRequiredScreenExit();
   void OnOobeFlowFinished();
   void OnPackagedLicenseScreenExit(PackagedLicenseScreen::Result result);
-  void OnActiveDirectoryPasswordChangeScreenExit();
   void OnFamilyLinkNoticeScreenExit(FamilyLinkNoticeScreen::Result result);
   void OnUserCreationScreenExit(UserCreationScreen::Result result);
   void OnGaiaScreenExit(GaiaScreen::Result result);
--- chrome/browser/ui/webui/ash/login/oobe_ui.cc
+++ chrome/browser/ui/webui/ash/login/oobe_ui.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/about_ui.h"
-#include "chrome/browser/ui/webui/ash/login/active_directory_password_change_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/app_downloading_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/app_launch_splash_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/arc_vm_data_migration_screen_handler.h"
@@ -437,9 +436,6 @@ void OobeUI::ConfigureOobeDisplay() {
 
   AddScreenHandler(std::make_unique<GaiaPasswordChangedScreenHandler>());
 
-  auto password_change_handler =
-      std::make_unique<ActiveDirectoryPasswordChangeScreenHandler>();
-
   if (features::IsOobeGaiaInfoScreenEnabled()) {
     AddScreenHandler(std::make_unique<GaiaInfoScreenHandler>());
   }
@@ -452,8 +448,6 @@ void OobeUI::ConfigureOobeDisplay() {
   AddScreenHandler(std::make_unique<SignInFatalErrorScreenHandler>());
 
   AddScreenHandler(std::make_unique<OfflineLoginScreenHandler>());
-
-  AddScreenHandler(std::move(password_change_handler));
 
   AddWebUIHandler(std::make_unique<SshConfiguredHandler>());
 
