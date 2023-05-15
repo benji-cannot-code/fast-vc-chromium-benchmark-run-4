196460c1f8b61ff87bacf8a8ba9120c7f9196acf
Ashutosh Singhal <macinashutosh@google.com>
2023-05-15 10:33:38 +0000

    Replace RunLoop with TestFuture in c/b/ash/crosapi/
    Bug: b/255713365.
    Change-Id: If6f2bb48d858bec51ab55eeabdfe49a1c0d3957d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496386
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143987}

--- chrome/browser/ash/crosapi/ash_requires_lacros_extension_apitest.cc
+++ chrome/browser/ash/crosapi/ash_requires_lacros_extension_apitest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/location.h"
 #include "base/one_shot_event.h"
-#include "base/run_loop.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/ash/crosapi/crosapi_ash.h"
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -45,10 +45,10 @@ void AshRequiresLacrosExtensionApiTest::SetUpOnMainThread() {
   ash_starter_.StartLacros(this);
 
   // Wait until StandaloneBrowserTestController binds with test_controller_ash_.
-  base::RunLoop run_loop;
+  base::test::TestFuture<void> waiter;
   test_controller_ash_->on_standalone_browser_test_controller_bound().Post(
-      FROM_HERE, run_loop.QuitClosure());
-  run_loop.Run();
+      FROM_HERE, waiter.GetCallback());
+  EXPECT_TRUE(waiter.Wait());
 }
 
 mojom::StandaloneBrowserTestController*
--- chrome/browser/ash/crosapi/device_oauth2_token_service_ash_unittest.cc
+++ chrome/browser/ash/crosapi/device_oauth2_token_service_ash_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/device_oauth2_token_service_ash.h"
 
 #include "base/test/mock_callback.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/device_identity/device_oauth2_token_service_factory.h"
@@ -50,20 +51,17 @@ class DeviceOAuth2TokenServiceAshTest : public testing::Test {
 };
 
 TEST_F(DeviceOAuth2TokenServiceAshTest, SingleRequest) {
-  base::RunLoop run_loop;
+  base::test::TestFuture<crosapi::mojom::AccessTokenResultPtr> waiter;
   std::unique_ptr<crosapi::DeviceOAuth2TokenServiceAsh> service =
       std::make_unique<crosapi::DeviceOAuth2TokenServiceAsh>();
-  base::MockCallback<FetchAccessTokenForDeviceAccountCallback> callback;
-  EXPECT_CALL(callback, Run(testing::_)).WillOnce(testing::Invoke([&run_loop] {
-    run_loop.Quit();
-  }));
-  service->FetchAccessTokenForDeviceAccount(/*scopes=*/{}, callback.Get());
-  run_loop.Run();
+
+  service->FetchAccessTokenForDeviceAccount(/*scopes=*/{},
+                                            waiter.GetCallback());
+  EXPECT_TRUE(waiter.Wait());
 }
 
 // Tests that passing an empty callback does not crash.
 TEST_F(DeviceOAuth2TokenServiceAshTest, EmptyCallback) {
-  base::RunLoop run_loop;
   std::unique_ptr<crosapi::DeviceOAuth2TokenServiceAsh> service =
       std::make_unique<crosapi::DeviceOAuth2TokenServiceAsh>();
   service->FetchAccessTokenForDeviceAccount(
@@ -73,24 +71,15 @@ TEST_F(DeviceOAuth2TokenServiceAshTest, EmptyCallback) {
 
 TEST_F(DeviceOAuth2TokenServiceAshTest, MultipleRequests) {
   constexpr int kRequestCount = 4;
-  base::RunLoop run_loop;
   std::unique_ptr<crosapi::DeviceOAuth2TokenServiceAsh> service =
       std::make_unique<crosapi::DeviceOAuth2TokenServiceAsh>();
-  std::array<base::MockCallback<FetchAccessTokenForDeviceAccountCallback>,
-             kRequestCount>
-      callbacks;
-  int call_count = 0;
-  for (auto& callback : callbacks) {
-    EXPECT_CALL(callback, Run(testing::_))
-        .WillOnce(testing::Invoke([&run_loop, &call_count] {
-          if (++call_count == kRequestCount)
-            run_loop.Quit();
-        }));
+
+  for (int call_count = 0; call_count < kRequestCount; call_count++) {
+    base::test::TestFuture<crosapi::mojom::AccessTokenResultPtr> waiter;
+    service->FetchAccessTokenForDeviceAccount(/*scopes=*/{},
+                                              waiter.GetCallback());
+    EXPECT_TRUE(waiter.Wait());
   }
-  for (auto& callback : callbacks)
-    service->FetchAccessTokenForDeviceAccount(/*scopes=*/{}, callback.Get());
-  run_loop.Run();
-  EXPECT_EQ(call_count, kRequestCount);
 }
 
 TEST_F(DeviceOAuth2TokenServiceAshTest, Cancel) {
