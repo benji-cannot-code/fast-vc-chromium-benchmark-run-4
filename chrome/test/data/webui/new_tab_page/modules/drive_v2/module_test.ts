fc0a93c97e929e1c65ce330450617fc8dfd330d9
Marlon Facey <mfacey@chromium.org>
2023-05-15 23:43:05 +0000

    [NTP Quests] Move again to end of toast message
    The word "again" was moved for clarity.
    
    screenshot: http://screenshot/3h8pQCF5eyWsvqE
    
    Bug: 1434151
    Change-Id: I9132cb09d8f73a2b0fd6449dee73b96fb50fae2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455845
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144460}

--- chrome/test/data/webui/new_tab_page/modules/drive_v2/module_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/drive_v2/module_test.ts
@@ -185,7 +185,7 @@ suite('NewTabPageModulesDriveModuleTest', () => {
         // Assert.
         const event: DisableModuleEvent = await whenFired;
         assertEquals(
-            'You won\'t see Drive files again on this page',
+            'You won\'t see Drive files on this page again',
             event.detail.message);
       });
 });
