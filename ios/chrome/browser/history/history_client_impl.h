dd68fe159120a128d57e58cb3877dbecdb7ec7b1
David Roger <droger@chromium.org>
2023-05-15 12:17:19 +0000

    [ios] Use ScopedObservation in HistoryClientImpl
    No behavior change. Cleanup in preparation for supporting account
    bookmarks.
    
    Bug: 1425458
    Change-Id: Ia85f1b008d9fb18d8afce9188f05ce866e2a33fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4531865
    Auto-Submit: David Roger <droger@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144027}

--- ios/chrome/browser/history/history_client_impl.h
+++ ios/chrome/browser/history/history_client_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/functional/callback_forward.h"
+#include "base/scoped_observation.h"
 #include "components/bookmarks/browser/base_bookmark_model_observer.h"
 #include "components/history/core/browser/history_client.h"
 #include "components/history/core/browser/history_service.h"
@@ -68,6 +69,10 @@ class HistoryClientImpl : public history::HistoryClient,
 
   // Subscription for notifications of changes to favicons.
   base::CallbackListSubscription favicons_changed_subscription_;
+
+  base::ScopedObservation<bookmarks::BookmarkModel,
+                          bookmarks::BaseBookmarkModelObserver>
+      local_or_syncable_bookmark_model_observation_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_HISTORY_HISTORY_CLIENT_IMPL_H_
