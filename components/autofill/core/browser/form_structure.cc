4de3ca3fabcaf250119a5a58501c735799eddd4d
Dominic Battre <battre@chromium.org>
2023-05-15 11:52:46 +0000

    Add HtmlFieldType rationalization to FormStructureRationalizer
    This CL is feature preserving. It replicates the rationalization of
    HtmlFieldTypes from autocomplete_parsing_util.cc to
    FormStructureRationalizer.
    
    A first followup CL will then remove the rationalization from
    autocomplete_parsing_util.cc.
    
    A second followup CL will improve the rationalization to also consider
    hints from label and placeholder fields.
    
    Bug: 1441057
    Change-Id: I5394a549200ab7411623dfbe63fc351fee21e5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4493243
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144017}

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -378,6 +378,7 @@ void FormStructure::DetermineHeuristicTypes(
   IdentifySections(/*ignore_autocomplete=*/false);
 
   FormStructureRationalizer rationalizer(&fields_, form_signature_);
+  rationalizer.RationalizeAutocompleteAttributes(log_manager);
   if (base::FeatureList::IsEnabled(features::kAutofillPageLanguageDetection)) {
     rationalizer.RationalizeRepeatedFields(form_interactions_ukm_logger,
                                            log_manager);
@@ -701,6 +702,7 @@ void FormStructure::ProcessQueryResponse(
     form->UpdateAutofillCount();
     FormStructureRationalizer rationalizer(&(form->fields_),
                                            form->form_signature_);
+    rationalizer.RationalizeAutocompleteAttributes(log_manager);
     rationalizer.RationalizeRepeatedFields(form_interactions_ukm_logger,
                                            log_manager);
     rationalizer.RationalizeFieldTypePredictions(log_manager);
