fa0f5965625f2feba7f9b7fb1f1e93361a488599
Federica Germinario <fedegermi@google.com>
2023-05-15 14:48:48 +0000

    [ios] Refactor browser-coordinator
    This CL adds some instance variables inside BrowserCoordinator to
    avoid redundancy of some operations. The new attributes are:
     - _layoutGuideCenter
     - _webNavigationBrowserAgent
     - _urlLoadingBrowserAgent
    
    Change-Id: I572d6bd1dc6f9e8e7aa668d2476843f5a35b329a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518427
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144092}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -465,6 +465,9 @@ enum class ToolbarKind {
   UrlLoadingNotifierBrowserAgent* _urlLoadingNotifierBrowserAgent;
   id<LoadQueryCommands> _loadQueryCommandsHandler;
   id<OmniboxCommands> _omniboxCommandsHandler;
+  LayoutGuideCenter* _layoutGuideCenter;
+  WebNavigationBrowserAgent* _webNavigationBrowserAgent;
+  UrlLoadingBrowserAgent* _urlLoadingBrowserAgent;
 }
 
 #pragma mark - ChromeCoordinator
@@ -676,7 +679,7 @@ enum class ToolbarKind {
   self.tabLifecycleMediator.baseViewController = self.viewController;
   self.tabLifecycleMediator.delegate = self.viewController;
 
-  WebNavigationBrowserAgent::FromBrowser(self.browser)->SetDelegate(self);
+  _webNavigationBrowserAgent->SetDelegate(self);
 
   self.contextMenuProvider = [[ContextMenuConfigurationProvider alloc]
          initWithBrowser:self.browser
@@ -755,6 +758,10 @@ enum class ToolbarKind {
   }
 
   _fullscreenController = FullscreenController::FromBrowser(self.browser);
+  _layoutGuideCenter = LayoutGuideCenterForBrowser(self.browser);
+  _webNavigationBrowserAgent =
+      WebNavigationBrowserAgent::FromBrowser(self.browser);
+  _urlLoadingBrowserAgent = UrlLoadingBrowserAgent::FromBrowser(self.browser);
 
   self.locationBarCoordinator =
       [[LocationBarCoordinator alloc] initWithBrowser:self.browser];
@@ -778,17 +785,14 @@ enum class ToolbarKind {
       [[BubblePresenter alloc] initWithBrowserState:browserState];
   _bubblePresenter.toolbarHandler =
       HandlerForProtocol(_dispatcher, ToolbarCommands);
-  _bubblePresenter.layoutGuideCenter =
-      LayoutGuideCenterForBrowser(self.browser);
+  _bubblePresenter.layoutGuideCenter = _layoutGuideCenter;
   [_dispatcher startDispatchingToTarget:_bubblePresenter
                             forProtocol:@protocol(HelpCommands)];
 
   _toolbarAccessoryPresenter = [[ToolbarAccessoryPresenter alloc]
-      initWithIsIncognito:self.browser->GetBrowserState()->IsOffTheRecord()];
-  LayoutGuideCenter* layoutGuideCenter =
-      LayoutGuideCenterForBrowser(self.browser);
+      initWithIsIncognito:browserState->IsOffTheRecord()];
   _toolbarAccessoryPresenter.toolbarLayoutGuide =
-      [layoutGuideCenter makeLayoutGuideNamed:kPrimaryToolbarGuide];
+      [_layoutGuideCenter makeLayoutGuideNamed:kPrimaryToolbarGuide];
 
   _sideSwipeController =
       [[SideSwipeController alloc] initWithBrowser:self.browser];
@@ -882,18 +886,15 @@ enum class ToolbarKind {
       HandlerForProtocol(_dispatcher, BrowserCoordinatorCommands);
   _viewControllerDependencies.findInPageCommandsHandler =
       HandlerForProtocol(_dispatcher, FindInPageCommands);
-  _viewControllerDependencies.isOffTheRecord =
-      self.browser->GetBrowserState()->IsOffTheRecord();
-  _viewControllerDependencies.urlLoadingBrowserAgent =
-      UrlLoadingBrowserAgent::FromBrowser(self.browser);
+  _viewControllerDependencies.isOffTheRecord = browserState->IsOffTheRecord();
+  _viewControllerDependencies.urlLoadingBrowserAgent = _urlLoadingBrowserAgent;
   _viewControllerDependencies.urlLoadingNotifierBrowserAgent =
       _urlLoadingNotifierBrowserAgent;
   _viewControllerDependencies.tabUsageRecorderBrowserAgent =
       TabUsageRecorderBrowserAgent::FromBrowser(self.browser);
   _viewControllerDependencies.webNavigationBrowserAgent =
-      WebNavigationBrowserAgent::FromBrowser(self.browser);
-  _viewControllerDependencies.layoutGuideCenter =
-      LayoutGuideCenterForBrowser(self.browser);
+      _webNavigationBrowserAgent;
+  _viewControllerDependencies.layoutGuideCenter = _layoutGuideCenter;
   _viewControllerDependencies.webStateList =
       self.browser->GetWebStateList()->AsWeakPtr();
   _viewControllerDependencies.voiceSearchController = _voiceSearchController;
@@ -1008,6 +1009,9 @@ enum class ToolbarKind {
 
   _keyCommandsProvider = nil;
   _dispatcher = nil;
+  _layoutGuideCenter = nil;
+  _webNavigationBrowserAgent = nil;
+  _urlLoadingBrowserAgent = nil;
 }
 
 // Starts child coordinators.
@@ -1391,12 +1395,10 @@ enum class ToolbarKind {
                                 scenario:SharingScenario::ShareChrome];
 
   // Exit fullscreen if needed to make sure that share button is visible.
-  FullscreenController::FromBrowser(self.browser)->ExitFullscreen();
+  _fullscreenController->ExitFullscreen();
 
-  LayoutGuideCenter* layoutGuideCenter =
-      LayoutGuideCenterForBrowser(self.browser);
   UIView* originView =
-      [layoutGuideCenter referencedViewUnderName:kToolsMenuGuide];
+      [_layoutGuideCenter referencedViewUnderName:kToolsMenuGuide];
   self.sharingCoordinator =
       [[SharingCoordinator alloc] initWithBaseViewController:self.viewController
                                                      browser:self.browser
@@ -1546,7 +1548,7 @@ enum class ToolbarKind {
   params.append_to = OpenPosition::kCurrentTab;
   params.user_initiated = NO;
   params.in_incognito = self.browser->GetBrowserState()->IsOffTheRecord();
-  UrlLoadingBrowserAgent::FromBrowser(self.browser)->Load(params);
+  _urlLoadingBrowserAgent->Load(params);
 }
 
 - (void)showAddCreditCard {
@@ -1929,7 +1931,7 @@ enum class ToolbarKind {
 - (void)showSecurityHelpPage {
   UrlLoadParams params = UrlLoadParams::InNewTab(GURL(kPageInfoHelpCenterURL));
   params.in_incognito = self.browser->GetBrowserState()->IsOffTheRecord();
-  UrlLoadingBrowserAgent::FromBrowser(self.browser)->Load(params);
+  _urlLoadingBrowserAgent->Load(params);
   [self hidePageInfo];
 }
 
@@ -2113,10 +2115,8 @@ enum class ToolbarKind {
       ->SetUIProviders(self.contextMenuProvider,
                        self.formInputAccessoryCoordinator, self.viewController);
 
-  UrlLoadingBrowserAgent* loadingAgent =
-      UrlLoadingBrowserAgent::FromBrowser(self.browser);
-  if (loadingAgent) {
-    loadingAgent->SetDelegate(self);
+  if (_urlLoadingBrowserAgent) {
+    _urlLoadingBrowserAgent->SetDelegate(self);
   }
 
   id<ApplicationCommands> applicationCommandHandler = HandlerForProtocol(
@@ -2156,10 +2156,8 @@ enum class ToolbarKind {
 
 // Uninstalls delegates for self.browser.
 - (void)uninstallDelegatesForBrowser {
-  UrlLoadingBrowserAgent* loadingAgent =
-      UrlLoadingBrowserAgent::FromBrowser(self.browser);
-  if (loadingAgent) {
-    loadingAgent->SetDelegate(nil);
+  if (_urlLoadingBrowserAgent) {
+    _urlLoadingBrowserAgent->SetDelegate(nil);
   }
 
   WebStateDelegateBrowserAgent::FromBrowser(self.browser)->ClearUIProviders();
@@ -2618,11 +2616,9 @@ enum class ToolbarKind {
   }
 
   // Navigate to NTP in same tab.
-  UrlLoadingBrowserAgent* urlLoadingBrowserAgent =
-      UrlLoadingBrowserAgent::FromBrowser(self.browser);
   UrlLoadParams urlLoadParams =
       UrlLoadParams::InCurrentTab(GURL(kChromeUINewTabURL));
-  urlLoadingBrowserAgent->Load(urlLoadParams);
+  _urlLoadingBrowserAgent->Load(urlLoadParams);
 }
 
 #pragma mark - WebNavigationNTPDelegate
