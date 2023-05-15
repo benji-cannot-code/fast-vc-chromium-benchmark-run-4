894410357fba223f816adeb29cc4321ff9edf11c
Peter Marshall <petermarshall@chromium.org>
2023-05-15 01:16:38 +0000

    [Office upload] Remove 'setup complete' pref and update debug page
    We use the file handler default prefs to determine whether setup
    should be run or not now. This means we can remove the 'setup complete' pref.
    
    Update files-internals to show the file handlers set in prefs for
    office file types, and add the ability to clear them for testing
    purposes.
    
    Low-Coverage-Reason: Low coverage refers to debug code only
    Bug: b/275912658
    Change-Id: I68764cc2fe26d3b547f515544e6604e27aaa0e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520460
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143832}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_page_handler.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_page_handler.cc
@@ -130,7 +130,6 @@ void CloudUploadPageHandler::SetOfficeAsDefaultHandler() {
       profile_, kActionIdOpenInOffice);
   file_manager::file_tasks::SetPowerPointFileHandlerToFilesSWA(
       profile_, kActionIdOpenInOffice);
-  file_manager::file_tasks::SetOfficeSetupComplete(profile_);
 }
 
 void CloudUploadPageHandler::GetAlwaysMoveOfficeFilesToDrive(
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -606,7 +606,6 @@ extern const char kDefaultTasksBySuffix[];
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 extern const char kDefaultHandlersForFileExtensions[];
-extern const char kOfficeSetupComplete[];
 extern const char kOfficeFilesAlwaysMoveToDrive[];
 extern const char kOfficeFilesAlwaysMoveToOneDrive[];
 extern const char kOfficeMoveConfirmationShownForDrive[];
