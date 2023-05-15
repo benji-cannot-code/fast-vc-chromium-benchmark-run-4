196d988165b3cd08f5b9a58ceccb9652ce82b732
Jose Dapena Paz <jdapena@igalia.com>
2023-05-15 17:58:25 +0000

    IWYU: include stdint.h for uint64_t usage in view_demotion.h
    Bug: 957519
    Change-Id: I09ff5fce640db80e86ef91400eb02253bc87384f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4532396
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1144220}

--- components/feed/core/v2/view_demotion.h
+++ components/feed/core/v2/view_demotion.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_FEED_CORE_V2_VIEW_DEMOTION_H_
 #define COMPONENTS_FEED_CORE_V2_VIEW_DEMOTION_H_
 
+#include <stdint.h>
+
 #include <iosfwd>
 #include <vector>
 #include "base/functional/callback_forward.h"
