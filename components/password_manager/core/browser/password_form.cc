f98adb8230ecf16cdf259683e43cd506eb2317f5
Peter Kasting <pkasting@chromium.org>
2023-05-15 10:56:08 +0000

    Avoid operator<=>(), which is C++20-specific.
    Bug: 1444563
    Change-Id: If62c20da059d620a7e9fe1f2817eff3217861214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518991
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143998}

--- components/password_manager/core/browser/password_form.cc
+++ components/password_manager/core/browser/password_form.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/password_form.h"
 
-#include <compare>
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <tuple>
 
 #include "base/json/json_writer.h"
 #include "base/json/values_util.h"
@@ -220,15 +220,23 @@ AlternativeElement::AlternativeElement(const AlternativeElement::Value& value,
     : value(value), field_renderer_id(field_renderer_id), name(name) {}
 
 AlternativeElement::AlternativeElement(const AlternativeElement& rhs) = default;
+
 AlternativeElement::AlternativeElement(AlternativeElement&& rhs) = default;
+
 AlternativeElement& AlternativeElement::operator=(
     const AlternativeElement& rhs) = default;
+
 AlternativeElement& AlternativeElement::operator=(AlternativeElement&& rhs) =
     default;
+
 AlternativeElement::~AlternativeElement() = default;
+
 bool AlternativeElement::operator==(const AlternativeElement&) const = default;
-std::strong_ordering AlternativeElement::operator<=>(
-    const AlternativeElement&) const = default;
+
+bool AlternativeElement::operator<(const AlternativeElement& other) const {
+  return std::tie(value, field_renderer_id, name) <
+         std::tie(other.value, other.field_renderer_id, other.name);
+}
 
 std::ostream& operator<<(std::ostream& os, const AlternativeElement& element) {
   base::Value::Dict element_json;
