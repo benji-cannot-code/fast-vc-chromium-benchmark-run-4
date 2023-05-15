63b387cdba463ad0e1fc9f3081190466a3495967
Ali Juma <ajuma@chromium.org>
2023-05-15 20:36:26 +0000

    [iOS] Reroute calls to webkit.messageHandlers to webkitMessageHandler
    In ios/content, messages to the browser must be sent using
    webkitMessageHandler. This CL injects a proxy object to intercept calls
    to webkit.messageHandlers and reroutes them.
    
    Bug: 1423527
    Change-Id: Ie18103bad0ad9805e5398b21920fcb7595631bec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528375
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144344}

--- ios/web/content/js_messaging/content_web_frames_manager.mm
+++ ios/web/content/js_messaging/content_web_frames_manager.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <set>
 
+#import "base/no_destructor.h"
 #import "base/strings/utf_string_conversions.h"
 #import "base/unguessable_token.h"
 #import "components/js_injection/browser/js_communication_host.h"
@@ -33,6 +34,23 @@ namespace {
 const char kHandlerNamePropertyName[] = "handler_name";
 const char kMessagePropertyName[] = "message";
 
+const char kSendWebKitMessageScriptName[] = "send_webkit_message";
+
+// This feature intercepts calls to window.webkit.messageHandlers and reroutes
+// them to window.webkitMessageHandler.
+JavaScriptFeature* GetSendWebKitMessageJavaScriptFeature() {
+  // Static storage is ok for `send_webkit_message_feature` as it holds no
+  // state.
+  static base::NoDestructor<JavaScriptFeature> send_webkit_message_feature(
+      ContentWorld::kPageContentWorld,
+      std::vector<const JavaScriptFeature::FeatureScript>(
+          {JavaScriptFeature::FeatureScript::CreateWithFilename(
+              kSendWebKitMessageScriptName,
+              JavaScriptFeature::FeatureScript::InjectionTime::kDocumentStart,
+              JavaScriptFeature::FeatureScript::TargetFrames::kAllFrames)}));
+  return send_webkit_message_feature.get();
+}
+
 }  // namespace
 
 ContentWebFramesManager::ContentWebFramesManager(
@@ -53,10 +71,7 @@ ContentWebFramesManager::ContentWebFramesManager(
   std::vector<JavaScriptFeature*> java_script_features;
   java_script_features.push_back(
       java_script_features::GetBaseJavaScriptFeature());
-
-  // TODO(crbug.com/1423527): Insert another feature that overrides the
-  // definition of sendWebKitMessage from common.js, to use
-  // webkitMessageHandler.postMessage.
+  java_script_features.push_back(GetSendWebKitMessageJavaScriptFeature());
   java_script_features.push_back(
       java_script_features::GetCommonJavaScriptFeature());
   java_script_features.push_back(
---
+++ ios/web/content/js_messaging/resources/send_webkit_message.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+interface Window {
+  __proto__: any;
+  webkitMessageHandler: any;
+}
+
+/**
+ * Injects a proxy object that captures calls to window.webkit.messageHandlers
+ * and forwards them to window.webkitMessageHandler.
+ */
+window.__proto__.webkit = {};
+window.__proto__.webkit.messageHandlers =
+    new Proxy(window.webkitMessageHandler, {
+      get(target, property) {
+        return {
+          'postMessage': function(message: object|string) {
+            var payload = {'handler_name': property, 'message': message};
+            target.postMessage(JSON.stringify(payload));
+          }
+        }
+      }
+    });
