828f5a18c921ccb39b2d5c15cd58059487b87aa0
Stephen Chenney <schenney@chromium.org>
2023-05-15 16:09:20 +0000

    Break out a method for invalidating ink overflow
    The code in LayoutObject for invalidating ink overflow was
    contained within an if block in the style change code. Here
    we separate it into a standalone method so that it can be
    called from other places to invalidate ink overflow.
    
    Also rename the existing method used for DCHECKs to avoid
    name conflicts.
    
    This change is needed for ink overflow invalidation of CSS
    pseudo highlights. No functional change.
    
    Bug: 1443428
    Change-Id: Ifc7614f82a7ab07c5d59693d59dae30a69b55939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4532735
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144143}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -699,7 +699,7 @@ void PaintLayer::ScrollContainerStatusChanged() {
 void PaintLayer::SetNeedsVisualOverflowRecalc() {
   DCHECK(IsSelfPaintingLayer());
 #if DCHECK_IS_ON()
-  GetLayoutObject().InvalidateVisualOverflow();
+  GetLayoutObject().InvalidateVisualOverflowForDCheck();
 #endif
   needs_visual_overflow_recalc_ = true;
   // |MarkAncestorChainForFlagsUpdate| will cause a paint property update which
