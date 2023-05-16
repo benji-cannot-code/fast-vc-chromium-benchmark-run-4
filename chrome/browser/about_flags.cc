72d5e26ce7c3668a8a9ab9bde98c1f61eced1944
Eshwar Stalin <estalin@chromium.org>
2023-05-16 17:12:42 +0000

    Adding flags for hover card image previews
    This CL reintroduces the flags for hover card image previews. While we
    finish the settings implementation in the next milestone, this is an
    intermediate change to allow users to be configure them through
    chrome://flags
    
    Bug: 1442482
    Change-Id: I00227fe457ddb5a1d8bc5f44de68eb336aeebec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4534188
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144792}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6553,6 +6553,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kDownloadRangeDescription, kOsAll,
      FEATURE_VALUE_TYPE(download::features::kDownloadRange)},
 
+    {"tab-hover-card-images", flag_descriptions::kTabHoverCardImagesName,
+     flag_descriptions::kTabHoverCardImagesDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(features::kTabHoverCardImages)},
+
     {"enable-storage-pressure-event",
      flag_descriptions::kStoragePressureEventName,
      flag_descriptions::kStoragePressureEventDescription, kOsAll,
