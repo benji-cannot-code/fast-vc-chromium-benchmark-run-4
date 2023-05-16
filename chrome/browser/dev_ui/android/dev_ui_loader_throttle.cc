2db4550c0724dfb03cbafb69c5e731dc06056e99
Matt Jones <mdjones@chromium.org>
2023-05-16 18:14:41 +0000

    Add missing internals URLs to dev UI list for android
    This patch adds the internals URLs for commerce, history clusters,
    and optimization guide which should fix the flaky behavior of
    these internals pages on official android builds. These internals
    pages would start working only after a DFM had been loaded (or at
    least this is the behavior that I was able to discern).
    
    Bug: b:274621159
    Change-Id: I56ee426a958662152409d139c2eee05a082fbdcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4539244
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144827}

--- chrome/browser/dev_ui/android/dev_ui_loader_throttle.cc
+++ chrome/browser/dev_ui/android/dev_ui_loader_throttle.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/android/modules/dev_ui/provider/dev_ui_module_provider.h"
 #include "chrome/browser/dev_ui/android/dev_ui_loader_error_page.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/commerce/core/commerce_constants.h"
+#include "components/history_clusters/history_clusters_internals/webui/url_constants.h"
+#include "components/optimization_guide/optimization_guide_internals/webui/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/common/url_constants.h"
@@ -63,6 +66,7 @@ bool IsWebUiHostInDevUiDfm(const std::string& host) {
          host == chrome::kChromeUIUserActionsHost ||
          host == chrome::kChromeUIWebApksHost ||
          host == chrome::kChromeUIWebRtcLogsHost ||
+         host == commerce::kChromeUICommerceInternalsHost ||
          host == content::kChromeUIPrivateAggregationInternalsHost ||
          host == content::kChromeUIAttributionInternalsHost ||
          host == content::kChromeUIBlobInternalsHost ||
@@ -75,7 +79,11 @@ bool IsWebUiHostInDevUiDfm(const std::string& host) {
          host == content::kChromeUIQuotaInternalsHost ||
          host == content::kChromeUIServiceWorkerInternalsHost ||
          host == content::kChromeUIUkmHost ||
-         host == content::kChromeUIWebRTCInternalsHost;
+         host == content::kChromeUIWebRTCInternalsHost ||
+         host == history_clusters_internals::
+                     kChromeUIHistoryClustersInternalsHost ||
+         host == optimization_guide_internals::
+                     kChromeUIOptimizationGuideInternalsHost;
 }
 
 }  // namespace
