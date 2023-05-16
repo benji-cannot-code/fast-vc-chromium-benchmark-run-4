5cb94565b94ec895b1baa18cdf97e7f9b350b115
Nate Chapin <japhet@chromium.org>
2023-05-16 22:55:10 +0000

    Revert "Improve hover triggering functionality to support popover=manual"
    This reverts commit e4cde2ad20aac0ace162db28090bc421df2e7e76.
    
    Reason for revert: Compile break: https://ci.chromium.org/ui/p/chromium/builders/ci/lacros64-archive-rel/53380/overview
    
    Original change's description:
    > Improve hover triggering functionality to support popover=manual
    >
    > This CL primarily adds a much larger hover hiding test, which tests
    > all popover types, two invoker types, and all popovertargetaction
    > values. In building that, I found a number of cases that weren't
    > properly handled, mostly because hover detection was using a version
    > of IsNodePopoverDescendant that was fairly manual and old. This
    > new version makes better use of a newly-expanded
    > FindTopmostRelatedPopover which is able to optionally look at manual
    > popovers. In doing this, I also refactored the `inclusive` argument
    > into an EnumSet to allow controlling the manual popover behavior
    > at the same time.
    >
    > Bug: 1416284
    > Fixed: 1443372
    > Fixed: 1445580
    > Change-Id: I2370610b4c7517fa635c353f0cfaf6de7b16b3af
    > Cq-Do-Not-Cancel-Tryjobs: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505324
    > Commit-Queue: Mason Freed <masonf@chromium.org>
    > Auto-Submit: Mason Freed <masonf@chromium.org>
    > Reviewed-by: Joey Arhar <jarhar@chromium.org>
    > Commit-Queue: Joey Arhar <jarhar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1144968}
    
    Bug: 1416284
    Change-Id: I6ed5ce2f3b7ace980838cf8e0106ecc3a5fb9cc1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4540744
    Owners-Override: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1144983}

--- third_party/blink/renderer/core/dom/popover_data.h
+++ third_party/blink/renderer/core/dom/popover_data.h
@@ -21,9 +21,6 @@ enum class PopoverVisibilityState {
   kShowing,
 };
 
-using PopoverHoverShowMap =
-    HeapHashMap<WeakMember<const HTMLFormControlElement>, TaskHandle>;
-
 class PopoverData final : public GarbageCollected<PopoverData>,
                           public ElementRareDataField {
  public:
@@ -102,7 +99,10 @@ class PopoverData final : public GarbageCollected<PopoverData>,
     bool was_set_;
   };
 
-  PopoverHoverShowMap& hoverShowTasks() { return hover_show_tasks_; }
+  HeapHashMap<WeakMember<const HTMLFormControlElement>, TaskHandle>&
+  hoverShowTasks() {
+    return hover_show_tasks_;
+  }
 
   void setHoverHideTask(TaskHandle&& task) {
     if (hover_hide_task_.IsActive()) {
@@ -143,7 +143,8 @@ class PopoverData final : public GarbageCollected<PopoverData>,
   // Map from elements with the 'popovertarget' attribute and
   // `popovertargetaction=hover` to a task that will show the popover after a
   // delay.
-  PopoverHoverShowMap hover_show_tasks_;
+  HeapHashMap<WeakMember<const HTMLFormControlElement>, TaskHandle>
+      hover_show_tasks_;
   // A task that hides the popover after a delay.
   TaskHandle hover_hide_task_;
 
--- third_party/blink/renderer/core/html/forms/html_form_control_element.h
+++ third_party/blink/renderer/core/html/forms/html_form_control_element.h
@@ -39,11 +39,6 @@ namespace blink {
 
 class HTMLFormElement;
 
-constexpr const char* kPopoverTargetActionToggle = "toggle";
-constexpr const char* kPopoverTargetActionShow = "show";
-constexpr const char* kPopoverTargetActionHide = "hide";
-constexpr const char* kPopoverTargetActionHover = "hover";
-
 // HTMLFormControlElement is the default implementation of
 // ListedElement, and listed element implementations should use
 // HTMLFormControlElement unless there is a special reason.
@@ -112,9 +107,6 @@ class CORE_EXPORT HTMLFormControlElement : public HTMLElement,
   virtual PopoverTriggerSupport SupportsPopoverTriggering() const {
     return PopoverTriggerSupport::kNone;
   }
-  // The IDL reflections:
-  AtomicString popoverTargetAction() const;
-  void setPopoverTargetAction(const AtomicString& value);
 
   void DefaultEventHandler(Event&) override;
 
