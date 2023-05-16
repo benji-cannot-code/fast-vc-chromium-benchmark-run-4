db52de7df2c7631637a3899f66c8d47515b48c9f
Samar Chehade-Lepleux <samarchehade@google.com>
2023-05-16 14:54:59 +0000

    Revert "[A11y] Small simplification to ignored computation for block flows"
    This reverts commit 4c8a3029c4699ffc19ee0909d4f11352ef7fb4d6.
    
    Reason for revert: https://crbug.com/1445940
    
    Original change's description:
    > [A11y] Small simplification to ignored computation for block flows
    >
    > This continues the work from CL:4194697 to simplify our code that decides whether or not a block flow is ignored. By simplifying, we improve performance and predictability, and have fewer cases where we miss an invalidation when one of the dependent conditions changes.
    >
    > In this case, we remove code that checks to see if a cursor can be set inside of the block flow. As a result, a few more harmless generic containers are introduced to the hierarchy. It is not necessary to remove all uninteresting generic containers, and it is only an optimization to do so. In this case, the extra "magic" regarding NGCursor is not worth having a few extra generic containers in the tree.
    >
    > The code is now clearer and less likely to run into a state where there is a stale tree structure containing a node with the wrong ignored state, confusing the serializer.
    >
    > Bug: none
    > Change-Id: Id19f911547c56e4518d43e71e04fc25a04fc36b7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261382
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1143545}
    
    Bug: 1445940
    Change-Id: I86ced0c603a1be7341f6b1b67f5e9dde779c458d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4538541
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144736}

--- content/browser/accessibility/ax_platform_node_win_browsertest.cc
+++ content/browser/accessibility/ax_platform_node_win_browsertest.cc
@@ -272,7 +272,7 @@ IN_PROC_BROWSER_TEST_F(AXPlatformNodeWinUIABrowserTest,
   LoadInitialAccessibilityTreeFromHtml(std::string(R"HTML(
       <!DOCTYPE html>
       <html>
-        <body role="none">
+        <body>
           <dialog open>Example Text</dialog>
         </body>
       </html>
@@ -288,7 +288,7 @@ IN_PROC_BROWSER_TEST_F(AXPlatformNodeWinUIABrowserTest,
   LoadInitialAccessibilityTreeFromHtml(std::string(R"HTML(
       <!DOCTYPE html>
       <html>
-        <body role="none">
+        <body>
           <dialog open aria-modal="false">Example Text</dialog>
         </body>
       </html>
@@ -304,7 +304,7 @@ IN_PROC_BROWSER_TEST_F(AXPlatformNodeWinUIABrowserTest,
   LoadInitialAccessibilityTreeFromHtml(std::string(R"HTML(
       <!DOCTYPE html>
       <html>
-        <body role="none">
+        <body>
           <dialog open aria-modal="true">Example Text</dialog>
         </body>
       </html>
