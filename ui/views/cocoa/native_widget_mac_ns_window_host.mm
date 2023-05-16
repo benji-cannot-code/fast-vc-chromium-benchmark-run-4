0b4e97ebd93a7a682c6febe9965a4eba3855d907
Jayson Adams <shrike@chromium.org>
2023-05-16 21:32:42 +0000

    [Mac Ventura Views] Track contentView when attaching a NativeView
    When attaching Widget A to Widget B using NativeViewHost, the NativeViewHost machinery makes Widget A's native_view_ a subview of
    Widget B's native_view_. A widget's native_view_ is also the
    contentView of its native NSWindow, and this reparenting didn't remove
    the native_view_ from its originating window (it simply added the
    native_view_ to this new view hierarchy). In previous versions of
    macOS, it appears the AppKit wouldn't notice that Widget A's window
    contentView was now also a subview in a second window's NSView tree.
    As of macOS Ventura, it appears the AppKit does take notice, setting
    Widget A's window contentView to nil.
    
    Widget focus traversal depends on a Widget's contentView's
    connection to its original window, so reparenting a widget breaks
    several tests on Ventura. This cl addresses the problem by storing a
    reference to a widget's contentView before removing it from its original window.
    
    Test: out/Default/views_unittests \
      --gtest_filter=FocusTraversalTest*:NativeViewHostMacTest.*NativeViewReferenceOn*
    out/Default/browser_tests \
      --gtest_filter=ImmersiveModeControllerMacBrowserTest.ToggleFullscreen
    
    Bug: 1414941
    Change-Id: I36dbb30b48105b4d3d54d336fdb5b465ecc62b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420534
    Reviewed-by: Tom Burgin <bur@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144936}

--- ui/views/cocoa/native_widget_mac_ns_window_host.mm
+++ ui/views/cocoa/native_widget_mac_ns_window_host.mm
@@ -274,8 +274,8 @@ NativeWidgetMacNSWindowHost* NativeWidgetMacNSWindowHost::GetFromNativeView(
 }
 
 // static
-const char NativeWidgetMacNSWindowHost::kImmersiveContentNSView[] =
-    "kImmersiveContentNSView";
+const char NativeWidgetMacNSWindowHost::kMovedContentNSView[] =
+    "kMovedContentNSView";
 
 // static
 NativeWidgetMacNSWindowHost* NativeWidgetMacNSWindowHost::GetFromId(
