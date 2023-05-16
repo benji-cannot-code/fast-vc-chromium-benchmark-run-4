4e13f6ff470d49d4af57c198d7bc79dfb0248b7d
Sorin Jianu <sorin@chromium.org>
2023-05-16 21:51:44 +0000

    updater: remove aggressive CHECK from task scheduler code.
    CHECKs on runtime conditions are not desirable since the value of such
    runtime condition is not an invariant source of truth that can be reasoned about in our program.
    
    Further CLs will change the function declaration to avoid passing pointer parameters where possible to reduce the number of CHECKs even more.
    
    Bug: 1445583
    Change-Id: I3060e3d78e3c2c3cb705f47548768271d24e0f8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4537689
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144954}

--- chrome/updater/win/task_scheduler.cc
+++ chrome/updater/win/task_scheduler.cc
@@ -82,9 +82,9 @@ bool UTCFileTimeToLocalSystemTime(const FILETIME& file_time_utc,
 }
 
 bool GetCurrentUser(base::win::ScopedBstr* user_name) {
+  static_assert(sizeof(OLECHAR) == sizeof(WCHAR));
   CHECK(user_name);
   ULONG user_name_size = 256;
-  CHECK_EQ(sizeof(OLECHAR), sizeof(WCHAR));
   if (!::GetUserNameExW(
           NameSamCompatible,
           user_name->AllocateBytes(user_name_size * sizeof(OLECHAR)),
@@ -97,7 +97,6 @@ bool GetCurrentUser(base::win::ScopedBstr* user_name) {
             NameSamCompatible,
             user_name->AllocateBytes(user_name_size * sizeof(OLECHAR)),
             &user_name_size)) {
-      CHECK_NE(DWORD{ERROR_MORE_DATA}, ::GetLastError());
       PLOG(ERROR) << "GetUserNameEx failed.";
       return false;
     }
@@ -149,7 +148,6 @@ class TaskSchedulerV2 final : public TaskScheduler {
     if (!task_folder_) {
       return false;
     }
-
     return GetTask(task_name, nullptr);
   }
 
@@ -418,18 +416,14 @@ class TaskSchedulerV2 final : public TaskScheduler {
       return false;
     }
 
-    CHECK(!IsTaskRegistered(task_name));
-
-    if (!use_task_subfolders_) {
-      return true;
+    if (use_task_subfolders_) {
+      // Try to delete \\Company\Product first and \\Company second.
+      if (DeleteFolderIfEmpty(GetTaskSubfolderName())) {
+        DeleteFolderIfEmpty(GetTaskCompanyFolder());
+      }
     }
 
-    // Try to delete \\Company\Product first and \\Company second.
-    if (DeleteFolderIfEmpty(GetTaskSubfolderName())) {
-      DeleteFolderIfEmpty(GetTaskCompanyFolder());
-    }
-
-    return true;
+    return !IsTaskRegistered(task_name);
   }
 
   bool RegisterTask(const wchar_t* task_name,
@@ -442,7 +436,6 @@ class TaskSchedulerV2 final : public TaskScheduler {
 
     // Create the task definition object to create the task.
     Microsoft::WRL::ComPtr<ITaskDefinition> task;
-    CHECK(task_service_);
     HRESULT hr = task_service_->NewTask(0, &task);
     if (FAILED(hr)) {
       PLOG(ERROR) << "Can't create new task. " << std::hex << hr;
@@ -723,11 +716,9 @@ class TaskSchedulerV2 final : public TaskScheduler {
       }
     }
 
-    CHECK(IsTaskRegistered(task_name));
-
     VLOG(1) << __func__ << ":" << task_name << ": "
             << run_command.GetCommandLineString();
-    return true;
+    return IsTaskRegistered(task_name);
   }
 
   bool StartTask(const wchar_t* task_name) override {
@@ -758,10 +749,9 @@ class TaskSchedulerV2 final : public TaskScheduler {
   }
 
   std::wstring GetTaskSubfolderName() override {
-    CHECK(use_task_subfolders_);
-
-    return base::StrCat(
-        {GetTaskCompanyFolder(), L"\\" PRODUCT_FULLNAME_STRING});
+    return use_task_subfolders_ ? base::StrCat({GetTaskCompanyFolder(),
+                                                L"\\" PRODUCT_FULLNAME_STRING})
+                                : std::wstring();
   }
 
   void ForEachTaskWithPrefix(
@@ -889,8 +879,6 @@ class TaskSchedulerV2 final : public TaskScheduler {
   // task folders have a "System" suffix, and User task folders have a "User"
   // suffix.
   std::wstring GetTaskCompanyFolder() const {
-    CHECK(use_task_subfolders_);
-
     return base::StrCat({L"\\" COMPANY_SHORTNAME_STRING,
                          IsSystemInstall(scope_) ? L"System" : L"User"});
   }
@@ -1107,10 +1095,6 @@ class TaskSchedulerV2 final : public TaskScheduler {
 
   // Return the branded task folder (e.g. \\Google\Updater).
   Microsoft::WRL::ComPtr<ITaskFolder> GetUpdaterTaskFolder() {
-    if (!task_service_) {
-      return nullptr;
-    }
-
     Microsoft::WRL::ComPtr<ITaskFolder> root_task_folder;
     HRESULT hr = task_service_->GetFolder(base::win::ScopedBstr(L"\\").Get(),
                                           &root_task_folder);
@@ -1344,17 +1328,12 @@ class TaskSchedulerV2 final : public TaskScheduler {
 }  // namespace
 
 TaskScheduler::TaskInfo::TaskInfo() = default;
-
 TaskScheduler::TaskInfo::TaskInfo(const TaskScheduler::TaskInfo&) = default;
-
 TaskScheduler::TaskInfo::TaskInfo(TaskScheduler::TaskInfo&&) = default;
-
 TaskScheduler::TaskInfo& TaskScheduler::TaskInfo::operator=(
     const TaskScheduler::TaskInfo&) = default;
-
 TaskScheduler::TaskInfo& TaskScheduler::TaskInfo::operator=(
     TaskScheduler::TaskInfo&&) = default;
-
 TaskScheduler::TaskInfo::~TaskInfo() = default;
 
 // static.
