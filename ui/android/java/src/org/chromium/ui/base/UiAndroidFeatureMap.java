30c4ad782a4bdaf2ae8f0a8bbabf275cc9dcc0a8
Henrique Nakashima <hnakashima@chromium.org>
2023-05-16 21:46:31 +0000

    [Android] Use FeatureMap in ui/
    Replace UiAndroidFeatureList with boilerplate UiAndroidFeatureMap.
    
    This allows using @EnableFeatures and @DisableFeatures annotations to
    set test values, query field trial parameters, and soon use CachedFlags.
    
    Disable-Rts: True
    Bug: 1445758
    Change-Id: I70905ebc049f2295891a5323dce67dedf7ca2ece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4534587
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144947}

---
+++ ui/android/java/src/org/chromium/ui/base/UiAndroidFeatureMap.java
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.base;
+
+import org.chromium.base.FeatureMap;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
+
+/**
+ * Java accessor for ui/android/ui_android_feature_map.cc state
+ */
+@JNINamespace("ui")
+public class UiAndroidFeatureMap extends FeatureMap {
+    private static UiAndroidFeatureMap sInstance;
+
+    // Do not instantiate this class
+    private UiAndroidFeatureMap() {}
+
+    /**
+     * @return the singleton UiAndroidFeatureMap.
+     */
+    public static UiAndroidFeatureMap getInstance() {
+        if (sInstance == null) sInstance = new UiAndroidFeatureMap();
+        return sInstance;
+    }
+
+    @Override
+    protected long getNativeMap() {
+        return UiAndroidFeatureMapJni.get().getNativeMap();
+    }
+
+    @NativeMethods
+    public interface Natives {
+        long getNativeMap();
+    }
+}
