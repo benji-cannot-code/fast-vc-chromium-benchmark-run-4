7ebdc0c8957b4c427d0afb347f806bd057a7897b
Miguel Casas <mcasas@chromium.org>
2023-05-16 20:30:46 +0000

    RELAND: media/gpu/v4l2: Remove BufferAffinityTracker
    Original CL broke lacros-arm-archive-rel bot, probably because
    I wrote GUARDED_BY() when I should have used GUARDED_BY_CONTEXT
    instead. Concrete error was:
    
    ../../media/gpu/v4l2/v4l2_device.h:538:18: error: use of undeclared identifier 'sequence_checker_'
    GUARDED_BY(sequence_checker_);
    
    PS0 contains the reverted CL, subsequent patches the fixes.
    
    Original CL description -----------------------------------------------
    
    BufferAffinityTracker is supposed to be a wrapper around a map
    (from id to a correlative index 0... max size).  There're
    multiple strangenesses in that class: Method naming and comments
    don't follow Chromium convention, uses an unnecessary lock, and
    more importantly it carries out a simple map operation behind a
    complicated class (which only has a use site).
    
    This CL removes said class and moves the logic directly to its only user. Note how mutual exclusion condition is enforced at compile
    time via GUARDED_BY(sequence_checker_).
    
    Bug: b:279980150
    Change-Id: Ie378eb1377f935b70643a70b76178758c844917d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519943
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1144788}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4537783
    Cr-Commit-Position: refs/heads/main@{#1144887}

--- media/gpu/v4l2/v4l2_device.h
+++ media/gpu/v4l2/v4l2_device.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_frame_layout.h"
 #include "media/gpu/chromeos/fourcc.h"
 #include "media/gpu/media_gpu_export.h"
-#include "media/gpu/v4l2/buffer_affinity_tracker.h"
 #include "media/gpu/v4l2/v4l2_device_poller.h"
 #include "media/video/video_decode_accelerator.h"
 #include "media/video/video_encode_accelerator.h"
@@ -533,9 +532,11 @@ class MEDIA_GPU_EXPORT V4L2Queue
   // by the v4l2_buffer queue ID. The value will be set to the VideoFrame that
   // has been passed when we queued the buffer, if any.
   base::small_map<std::map<size_t, scoped_refptr<VideoFrame>>> queued_buffers_;
-  // Keep track of which buffer was assigned to which frame by
-  // |GetFreeBufferForFrame()| so we reuse the same buffer in subsequent calls.
-  BufferAffinityTracker affinity_tracker_;
+
+  // Dictionary of queue buffers (indexed 0... |buffers_| size), indexed by the
+  // unique VideoFrame id (be that a GpuMemoryBuffer ID or a DmaBuf ID).
+  std::map<gfx::GenericSharedMemoryId, size_t> free_buffers_indexes_
+      GUARDED_BY_CONTEXT(sequence_checker_);
 
   scoped_refptr<V4L2Device> device_;
   // Callback to call in this queue's destructor.
