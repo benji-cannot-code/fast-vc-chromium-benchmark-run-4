cd258524640b2cf32103bf55cf10e9b64bcd4333
Artem Sumaneev <asumaneev@google.com>
2023-05-16 13:17:42 +0000

    [Remote commands] Use RemoteCommandsServiceMixin for reboot command.
    Use RemoteCommandsServiceMixin and RemoteCommandBuilder to simplify
    tests and use shared code.
    
    Add DeviceCommandRebootBaseTest for the common setup of reboot command
    test cases.
    
    Move browser test for SET_VOLUME command into its own file. Remove
    several calls which do not affect the test.
    
    Bug: b:225913691
    Test: browser_tests --gtest_filter="DeviceCommand*"
    Change-Id: If50c0b805a859c1f24ed39530f59f6721ae379b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4529142
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144676}

--- chrome/browser/ash/policy/remote_commands/device_command_set_volume_job_browsertest.cc
+++ chrome/browser/ash/policy/remote_commands/device_command_set_volume_job_browsertest.cc
@@ -3,18 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <string>
-
-#include "ash/constants/ash_switches.h"
-#include "base/command_line.h"
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
 #include "chrome/browser/ash/policy/test_support/embedded_policy_test_server_mixin.h"
 #include "chrome/browser/ash/policy/test_support/remote_commands_service_mixin.h"
-#include "chrome/test/base/in_process_browser_test.h"
 #include "components/policy/core/common/remote_commands/test_support/remote_command_builders.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "content/public/test/browser_test.h"
-#include "testing/gtest/include/gtest/gtest.h"
 
 namespace em = enterprise_management;
 
@@ -23,25 +17,8 @@ namespace policy {
 namespace {
 
 // Tests sending remote commands and verifying the results.
-class RemoteCommandsTest : public DevicePolicyCrosBrowserTest {
- public:
-  RemoteCommandsTest() = default;
-
-  RemoteCommandsTest(const RemoteCommandsTest&) = delete;
-  RemoteCommandsTest& operator=(const RemoteCommandsTest&) = delete;
-
-  ~RemoteCommandsTest() override = default;
-
-  void SetUp() override {
-    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
-    DevicePolicyCrosBrowserTest::SetUp();
-  }
-
-  void SetUpInProcessBrowserTestFixture() override {
-    DevicePolicyCrosBrowserTest::SetUpInProcessBrowserTestFixture();
-    embedded_test_server()->StartAcceptingConnections();
-  }
-
+class DeviceCommandSetVolumeBrowserTest : public DevicePolicyCrosBrowserTest {
+ protected:
   em::RemoteCommandResult SendRemoteCommand(
       const enterprise_management::RemoteCommand& command) {
     return remote_commands_service_mixin_.SendRemoteCommand(command);
@@ -55,7 +32,7 @@ class RemoteCommandsTest : public DevicePolicyCrosBrowserTest {
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_F(RemoteCommandsTest, DeviceSetVolume) {
+IN_PROC_BROWSER_TEST_F(DeviceCommandSetVolumeBrowserTest, DeviceSetVolume) {
   em::RemoteCommandResult result =
       SendRemoteCommand(RemoteCommandBuilder()
                             .SetType(em::RemoteCommand_Type_DEVICE_SET_VOLUME)
