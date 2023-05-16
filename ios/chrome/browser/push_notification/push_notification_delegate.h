7f464baa51a56c20e6ec64f474f62708e9e13c94
David Maunder <davidjm@google.com>
2023-05-16 22:05:05 +0000

    Revert "Price Drop Notification URL Handler for backgrounded multi window iPad"
    This reverts commit 0782f29aaf8f929ac063aeccabbf3f157faacc73.
    
    Reason for revert: crbug.com/1445798
    
    Original change's description:
    > Price Drop Notification URL Handler for backgrounded multi window iPad
    >
    > The price drop notification URL handler does not work for multi window
    > iPad when the app is backgrounded. This CL updates the hooks used
    > and has been verified works for all use cases (iPhone, iPad, single
    > window, multi window, foreground, background, re-open after closed).
    > It uses UIApplicationDidBecomeActiveNotification as a signal for
    > the browser objects being available instead of InitStageFinal in
    > AppStateObserver. It also uses a browser with its scene activation
    > level SceneActivationLevelForegroundInactive instead of just the
    > first active browser seen as a fallback in the event a browser
    > with scene state activation level of
    > SceneActivationLevelForegroundActive is not found.
    >
    > Bug: b:267630688
    > Change-Id: Id462553c3188b4d5dd9ea263cef5f73a9e403c50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523582
    > Commit-Queue: David Maunder <davidjm@chromium.org>
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1143470}
    
    Bug: b:267630688
    Change-Id: I6a17f5649fcd06e18cf918602228209077a340c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4539103
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144961}

--- ios/chrome/browser/push_notification/push_notification_delegate.h
+++ ios/chrome/browser/push_notification/push_notification_delegate.h
@@ -10,10 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UserNotifications.h>
 
-// Passes information from the application level to the push notification
-// infrastructure.
+#import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/app/application_delegate/app_state_observer.h"
+
 @interface PushNotificationDelegate
-    : NSObject <UNUserNotificationCenterDelegate>
+    : NSObject <UNUserNotificationCenterDelegate, AppStateObserver>
+
+- (instancetype)initWithAppState:(AppState*)appState NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)init NS_UNAVAILABLE;
 
 // Passes the contents of an incoming push notification to the appropriate
 // `PushNotificationClient` for processing and logs the time it takes for the
@@ -25,9 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // token this function aggregates all the necessary information and registers
 // the device to the Push Notification server.
 - (void)applicationDidRegisterWithAPNS:(NSData*)deviceToken;
-
-// Called when the Browser objects are ready to use.
-- (void)browserDidBecomeReady;
 @end
 
 #endif  // IOS_CHROME_BROWSER_PUSH_NOTIFICATION_PUSH_NOTIFICATION_DELEGATE_H_
