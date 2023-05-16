791e92c74271787071d801c374bcb5023c681974
Andrew Rayskiy <greengrape@google.com>
2023-05-16 10:14:06 +0000

    [CodeHealth] Remove RemoveKey() from base/values.{h,cc}
    Bug: 1439196
    Change-Id: Ib94daa01e70611c535ba4fd04fdbcdef1b849cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4532676
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144623}

--- base/values.cc
+++ base/values.cc
@@ -1157,10 +1157,6 @@ Value* Value::SetKey(StringPiece key, Value&& value) {
   return GetDict().Set(key, std::move(value));
 }
 
-bool Value::RemoveKey(StringPiece key) {
-  return GetDict().Remove(key);
-}
-
 bool operator==(const Value& lhs, const Value& rhs) {
   return lhs.data_ == rhs.data_;
 }
