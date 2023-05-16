1ddfe863780c3228278ec89c6abcd6227ee63f2c
Scott Haseley <shaseley@chromium.org>
2023-05-16 21:51:19 +0000

    Don't use AbortSignal::Follow in fetch and cache-storage
    AbortSignal's "follow" algorithm is replaced with a new algorithm for
    creating a dependent abort signal ([1], [2]), which is implemented as
    a new AbortSignal constructor. This CL replaces Follow with the new
    ctor, behind the AbortSignalAny REF.
    
    There is a slight compat risk with the change (when the feature ships):
     1. fetch: abort is propagated to dependent signals after abort event
        dispatch for a signal, whereas "follow" propagated abort before (as
        abort algorithms), which is observable. We could alternatively use
        weak abort algorithms in AbortSignal.any(), but that would be more
        complicated and partially limit GC optimizations.
    
     2. cache: The use of "follow" in cache.addAll was not specced and
        could lead to abort events firing when they shouldn't have. The new
        behavior preserves the specced behavior (aborting outstanding
        requests on failure) without dispatching events when abort is
        triggered internally.
    
    Note: the virtual test expectations match the current expectations
    except for the test modified in fetch/api/abort/general.any.js.
    
    [1] https://github.com/whatwg/dom/pull/1152
    [2] https://github.com/whatwg/fetch/pull/1646
    
    Bug: 1323391
    Low-Coverage-Reason: the uncovered lines in request.cc are covered by WPT tests
    Change-Id: I5c25048fcf9f8db759e2f0181ac338c8b603b451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4515276
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144953}

--- third_party/blink/renderer/core/fetch/request.h
+++ third_party/blink/renderer/core/fetch/request.h
@@ -55,13 +55,13 @@ class CORE_EXPORT Request final : public ScriptWrappable,
                          Request*,
                          const RequestInit*,
                          ExceptionState&);
-  static Request* Create(ScriptState*, FetchRequestData*);
+  static Request* Create(ScriptState*, FetchRequestData*, AbortSignal*);
   static Request* Create(ScriptState*,
                          mojom::blink::FetchAPIRequestPtr,
                          ForServiceWorkerFetchEvent);
 
   Request(ScriptState*, FetchRequestData*, Headers*, AbortSignal*);
-  Request(ScriptState*, FetchRequestData*);
+  Request(ScriptState*, FetchRequestData*, AbortSignal*);
   Request(const Request&) = delete;
   Request& operator=(const Request&) = delete;
 
