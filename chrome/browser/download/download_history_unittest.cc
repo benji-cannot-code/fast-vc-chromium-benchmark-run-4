e35c97fe25fa52427498561a0fb357ae04f202ce
Lily Chen <chlily@chromium.org>
2023-05-16 13:54:22 +0000

    Revert "[Downloads] Record web app that initiated a download in history DB"
    This reverts commit 2a8929798c3aefc4d6dc06c9a39cfff25b377a55.
    
    Reason for revert: mistakenly assumed that extension downloads and
    web app downloads are mutually exclusive. Reverting before it gets
    released in Canary.
    
    Original change's description:
    > [Downloads] Record web app that initiated a download in history DB
    >
    > This changes the existing by_ext_id column in the downloads table, which records the ID of the extension that downloaded the file, renaming it
    > to by_ext_or_web_app_id, so that it may now record the ID of the
    > web app that initiated the download. These are mutually exclusive, so
    > the field can be shared between them. A new boolean column is added to
    > record whether the column is for a web app or an extension.
    >
    > Increments DB version to 64.
    >
    > Also updates the README to document the current process for generating
    > test sql schema files.
    >
    > Bug: 1420454
    > Change-Id: I3cc8f134af96521f32263881dfac89e32ec5d25f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517767
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Lily Chen <chlily@chromium.org>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1144424}
    
    Bug: 1420454
    Change-Id: I73a1819925f36f8aff146f6310ab7a116919ed8a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4538362
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1144697}

--- chrome/browser/download/download_history_unittest.cc
+++ chrome/browser/download/download_history_unittest.cc
@@ -42,10 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/downloads/downloads_api.h"
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/download/download_item_web_app_data.h"
-#endif
-
 using testing::_;
 using testing::DoAll;
 using testing::Invoke;
@@ -471,15 +467,8 @@ class DownloadHistoryTest : public testing::Test {
         .WillRepeatedly(Return(&item(index)));
     EXPECT_CALL(item(index), IsTemporary()).WillRepeatedly(Return(false));
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-    if (!row->is_by_web_app) {
-      new extensions::DownloadedByExtension(
-          &item(index), row->by_ext_or_web_app_id, row->by_ext_name);
-    }
-#endif
-#if !BUILDFLAG(IS_ANDROID)
-    if (!row->by_ext_or_web_app_id.empty() && row->is_by_web_app) {
-      new DownloadItemWebAppData(&item(index), row->by_ext_or_web_app_id);
-    }
+    new extensions::DownloadedByExtension(&item(index), row->by_ext_id,
+                                          row->by_ext_name);
 #endif
 
     std::vector<download::DownloadItem*> items;
@@ -1000,27 +989,5 @@ TEST_F(DownloadHistoryTest,
   EXPECT_TRUE(DownloadHistory::IsPersisted(&item(0)));
   EXPECT_TRUE(DownloadHistory::IsPersisted(&item(1)));
 }
-
-#if !BUILDFLAG(IS_ANDROID)
-// Test that web app id is inserted into history.
-TEST_F(DownloadHistoryTest, ByWebAppId) {
-  // Create a fresh item not from download DB
-  CreateDownloadHistory({});
-
-  history::DownloadRow row;
-  row.by_ext_or_web_app_id = "by_web_app_id";
-  row.is_by_web_app = true;
-  InitBasicItem(FILE_PATH_LITERAL("/foo/bar.pdf"), "http://example.com/bar.pdf",
-                "http://example.com/referrer.html",
-                download::DownloadItem::COMPLETE, &row);
-
-  EXPECT_CALL(item(0), IsDone()).WillRepeatedly(Return(true));
-
-  CallOnDownloadCreated(0);
-  ExpectDownloadCreated(row);
-  EXPECT_TRUE(DownloadHistory::IsPersisted(&item(0)));
-  EXPECT_NE(DownloadItemWebAppData::Get(&item(0)), nullptr);
-}
-#endif
 
 }  // anonymous namespace
