e4ffb5933ca9810c7f729c619cc137b578d79c6d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-05-16 22:06:45 +0000

    Import wpt@0d3e62f9d3317828aae9bc8b34e0e52c3fc0bc9d
    Using wpt-import in Chromium dde3ca8fcfe5ea6c0042ec05332dc3c6125146fc.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    pcupp@microsoft.com:
      external/wpt/editing
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I80bef6f73651b27715c7af70c9bbdcac2824c114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4537945
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144962}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/element_click/navigate.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/element_click/navigate.py
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import pytest
-from webdriver import error
+
+from webdriver.error import NoSuchElementException
 
 from tests.support.asserts import assert_success
 from tests.support.helpers import wait_for_new_handle
@@ -121,7 +122,7 @@ def test_link_from_toplevel_context_with_target(session, inline, target):
     wait = Poll(
         session,
         timeout=5,
-        ignored_exceptions=error.NoSuchElementException,
+        ignored_exceptions=NoSuchElementException,
         message="Expected element has not been found")
     wait.until(lambda s: s.find.css("#foo"))
 
@@ -160,7 +161,7 @@ def test_link_from_nested_context_with_target(session, inline, iframe, target):
     wait = Poll(
         session,
         timeout=5,
-        ignored_exceptions=error.NoSuchElementException,
+        ignored_exceptions=NoSuchElementException,
         message="Expected element has not been found")
     wait.until(lambda s: s.find.css("#foo"))
 
@@ -180,8 +181,6 @@ def test_link_cross_origin(session, inline, url):
     assert_success(response)
 
     assert session.url == target_page
-    with pytest.raises(error.StaleElementReferenceException):
-        link.click()
 
     session.find.css("#delete", all=False)
 
