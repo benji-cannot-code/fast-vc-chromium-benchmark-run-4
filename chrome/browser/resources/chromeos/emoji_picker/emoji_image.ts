c6339ab14f95eac819771736d93f986a00f0059c
Grey Wang <greywang@google.com>
2023-05-16 07:15:41 +0000

    Make GIFs clearable
    Add a context menu for <emoji-image /> (under history group), so users can remove GIFs individually.
    
    Bug: b:282083514
    Change-Id: I7d0411c091a3a812839b8430ced5093ad3a3d860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4533818
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Commit-Queue: Grey Wang <greywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144564}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_image.ts
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_image.ts
@@ -7,7 +7,8 @@ import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bu
 
 import {VISUAL_CONTENT_WIDTH} from './constants.js';
 import {getTemplate} from './emoji_image.html.js';
-import {EmojiVariants} from './types.js';
+import { createCustomEvent, EMOJI_CLEAR_RECENTS_CLICK } from './events.js';
+import {CategoryEnum, EmojiVariants} from './types.js';
 
 export class EmojiImageComponent extends PolymerElement {
   static get is() {
@@ -24,6 +25,9 @@ export class EmojiImageComponent extends PolymerElement {
       item: Object,
       showTooltip: Object,
       emojiClick: Object,
+
+      clearable: {type: Boolean, value: false},
+      showClearButton: {type: Boolean, value: false},
     };
   }
 
@@ -33,6 +37,9 @@ export class EmojiImageComponent extends PolymerElement {
 
   showTooltip: (e: MouseEvent|FocusEvent) => void;
   emojiClick: (e: MouseEvent) => void;
+
+  clearable: boolean;
+  showClearButton: boolean;
 
   private handleMouseEnter(event: MouseEvent): void {
     this.showTooltip(event);
@@ -50,6 +57,32 @@ export class EmojiImageComponent extends PolymerElement {
     this.loading = false;
   }
 
+  private handleContextMenu(evt: Event): void {
+    if (this.clearable) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.showClearButton = true;
+    }
+  }
+
+  private handleMouseLeave(): void {
+    if (this.showClearButton) {
+      this.showClearButton = false;
+    }
+  }
+
+  private handleClear(evt: Event): void {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.showClearButton = false;
+    this.dispatchEvent(createCustomEvent(
+      EMOJI_CLEAR_RECENTS_CLICK, {
+        category: CategoryEnum.GIF,
+        item: this.item,
+      },
+    ));
+  }
+
   private getImageClassName(loading: boolean) {
     return loading ? 'emoji-image loading' : 'emoji-image';
   }
@@ -57,12 +90,12 @@ export class EmojiImageComponent extends PolymerElement {
   /**
    * Returns visual content preview url.
    */
-  private getUrl() {
-    return this.item.base.visualContent?.url.preview.url;
+  private getUrl(item: EmojiVariants) {
+    return item.base.visualContent?.url.preview.url;
   }
 
-  private setHeight() {
-    const {visualContent} = this.item.base;
+  private getStyles(item: EmojiVariants) {
+    const {visualContent} = item.base;
 
     if (visualContent === undefined) {
       return;
@@ -70,14 +103,8 @@ export class EmojiImageComponent extends PolymerElement {
 
     const {height, width} = visualContent.previewSize;
     const visualContentHeight = height / width * VISUAL_CONTENT_WIDTH;
-    this.updateStyles({
-      '--visual-content-height': `${visualContentHeight}px`,
-    });
-  }
 
-  override ready() {
-    super.ready();
-    this.setHeight();
+    return `--visual-content-height: ${visualContentHeight}px`;
   }
 }
 
