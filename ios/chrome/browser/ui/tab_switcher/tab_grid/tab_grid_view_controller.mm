3fa79e0bf634412aaeedc58b14f46ccbfa465030
Louis Romero <lpromero@google.com>
2023-05-16 13:23:27 +0000

    Don't crash when the Tab Grid enter time was not recorded
    This CL changes a CHECK (crashing when the timer was not started) into
    just bailing out (and dumping without crashing).
    We have reports that indicate that the CHECK was triggered, but don't
    know yet how. This CL keeps the app running even in that case, and just
    doesn't log the timing.
    
    Fixed: 1444540
    Change-Id: I3c38e56929cda3821e3c695282dfb3597afdd301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4537661
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144678}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <objc/runtime.h>
 
+#import "base/debug/dump_without_crashing.h"
 #import "base/functional/bind.h"
 #import "base/ios/ios_util.h"
 #import "base/logging.h"
@@ -2218,7 +2219,11 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
 }
 
 - (void)reportTabSelectionTime {
-  CHECK(!self.tabGridEnterTime.is_null());
+  if (self.tabGridEnterTime.is_null()) {
+    // The enter time was not recorded. Bail out.
+    base::debug::DumpWithoutCrashing();
+    return;
+  }
   base::TimeDelta duration = base::TimeTicks::Now() - self.tabGridEnterTime;
   base::UmaHistogramLongTimes("IOS.TabSwitcher.TimeSpentOpeningExistingTab",
                               duration);
