dbb929c3ee45037f13cca0178437d438ae046535
Ashley Prasad <ashleydp@google.com>
2023-05-16 23:43:06 +0000

    shortcut-customization: enable by default dynamic colors
    Enables dynamic color for Shortcut Customization app by default. Allows
    control for launch to be based on CrOS Jelly colors flag/finch
    experiment.
    
    Bug: b:275718921
    Test: Manual verified UI.
    Test: Ran Shortcut Customization browser and tast tests.
    Change-Id: Icf1cae43042e77bc5549dae38d165e2e780de8f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4533915
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144992}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2050,7 +2050,7 @@ BASE_FEATURE(kShimlessRMADiagnosticPage,
 // Requires jelly-colors flag to also be enabled.
 BASE_FEATURE(kShortcutCustomizationJelly,
              "ShortcutCustomizationJelly",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables or disables a toggle to enable Bluetooth debug logs.
 BASE_FEATURE(kShowBluetoothDebugLogToggle,
