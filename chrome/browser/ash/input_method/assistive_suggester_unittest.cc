ec19d5859d975e12d37565591c1fe6bd2a49b6ba
Colin Kincaid <ckincaid@chromium.org>
2023-05-16 01:41:35 +0000

    multipaste: Fix menu anchoring for Ctrl+V longpress menu in Lacros
    The previous logic for getting the clipboard history menu's anchor rect
    relied on a function that was not implemented in Lacros, so the menu
    would always appear at (0,0). This CL changes the logic to use the text
    input client's caret bounds, which works in both Ash and Lacros.
    
    Note that the testing added in this CL only applies to Ash; Tast tests
    will be added for Lacros pending the results of this feature's dogfood.
    
    Bug: b/282200723
    Change-Id: I0629d6ac13f29823c89a84e9ca0c2b107c827ec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4533050
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144513}

--- chrome/browser/ash/input_method/assistive_suggester_unittest.cc
+++ chrome/browser/ash/input_method/assistive_suggester_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "base/functional/bind.h"
+#include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/repeating_test_future.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
+#include "ui/views/controls/textfield/textfield.h"
 
 namespace ash::input_method {
 namespace {
@@ -1405,10 +1407,19 @@ class AssistiveSuggesterControlVLongpressTest : public AshTestBase {
     SetClipboardText("B");
     SetClipboardText("A");
 
-    // Create a test window for the clipboard history controller to recognize as
-    // a paste target.
-    gfx::Rect test_window_rect(100, 100, 100, 100);
-    std::unique_ptr<aura::Window> window(CreateTestWindow(test_window_rect));
+    // Create a textfield for the clipboard history controller to recognize as a
+    // paste target.
+    textfield_widget_ = CreateFramelessTestWidget();
+    textfield_widget_->SetBounds(gfx::Rect(100, 100, 100, 100));
+    textfield_ = textfield_widget_->SetContentsView(
+        std::make_unique<views::Textfield>());
+
+    // Set the textfield as the text input client so that its caret position can
+    // be queried.
+    IMEBridge::Get()
+        ->GetInputContextHandler()
+        ->GetInputMethod()
+        ->SetFocusedTextInputClient(textfield_);
   }
 
   void SetClipboardText(const std::string& text) {
@@ -1431,6 +1442,8 @@ class AssistiveSuggesterControlVLongpressTest : public AshTestBase {
   FakeSuggestionHandler suggestion_handler_;
   AssistiveSuggester assistive_suggester_;
   base::test::RepeatingTestFuture<bool> operation_confirmed_future_;
+  std::unique_ptr<views::Widget> textfield_widget_;
+  raw_ptr<views::Textfield> textfield_;
   base::HistogramTester histogram_tester_;
 };
 
@@ -1442,7 +1455,13 @@ TEST_F(AssistiveSuggesterControlVLongpressTest,
   assistive_suggester_.OnSurroundingTextChanged(u"A", gfx::Range(1));
   task_environment()->FastForwardBy(base::Seconds(1));
 
-  EXPECT_TRUE(Shell::Get()->clipboard_history_controller()->IsMenuShowing());
+  auto* const controller = Shell::Get()->clipboard_history_controller();
+  EXPECT_TRUE(controller->IsMenuShowing());
+  // Precise anchoring logic may change as time goes on, so this test only
+  // assumes that the clipboard history menu should be left-aligned with the
+  // input field's caret.
+  EXPECT_EQ(controller->GetMenuBoundsInScreenForTest().x(),
+            textfield_->GetCaretBounds().x());
   histogram_tester_.ExpectUniqueSample(
       "Ash.ClipboardHistory.ContextMenu.ShowMenu",
       crosapi::mojom::ClipboardHistoryControllerShowSource::kControlVLongpress,
