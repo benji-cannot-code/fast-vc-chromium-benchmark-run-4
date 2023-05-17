8b8d13d001282959fdc20c56f3e0dfaa54cc11bd
ChangSeok Oh <changseok.oh@bytedance.com>
2023-05-17 22:20:57 +0000

    Fix a build failure caused by the incomplete type blink::ResourceClient
    This CL fixes a build failure of blink unit tests on Mac. The compiler
    is not happy with the incomplete type of blink::ResourceClient
    when building blink_unittests.
    
    Bug: 1446438
    Change-Id: If8ecfefb8db5e8673ad30d89bb525ae3b7c1b0ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544259
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: ChangSeok Oh <changseok.oh@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1145650}

--- third_party/blink/renderer/platform/loader/fetch/resource.h
+++ third_party/blink/renderer/platform/loader/fetch/resource.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/instrumentation/memory_pressure_listener.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/web_process_memory_dump.h"
 #include "third_party/blink/renderer/platform/loader/fetch/integrity_metadata.h"
+#include "third_party/blink/renderer/platform/loader/fetch/resource_client.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_error.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_load_priority.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_loader_options.h"
@@ -69,7 +70,6 @@ namespace blink {
 
 class BlobDataHandle;
 class FetchParameters;
-class ResourceClient;
 class ResourceFinishObserver;
 class ResourceLoader;
 class ResponseBodyLoaderDrainableInterface;
