0909edfb38844959546669da9bf4abc4b2e75551
Henrique Nakashima <hnakashima@chromium.org>
2023-05-17 16:15:03 +0000

    [Android] Move lists of CachedFlags from ChromeCachedFlags
    ChromeFeatureList now contains the lists of CachedFlags (and the
    CachedFlags themselves).
    
    It is necessary to have a list of all CachedFlags in
    ChromeFeatureList, since we need to map the feature name to the
    legacy shared preference key in the CachedFlag when:
    1. Setting up test values via @EnableFeatures/@DisableFeatures
        * Currently done through CachedFeatureFlags#setForTesting, but
          requires CachedFeatureFlags to know legacy SharedPrefs key
          overrides, which blocks its migration to //base.
    2. Called via Java_CachedFeatureFlags_IsEnabled()
        * Currently uses the default from native passed as a param through
          JNI.
    
    This also adds a test to ensure flags aren't declared and not cached,
    which is a common pitfall.
    
    Bug: 1442347
    Change-Id: I24092289f438fcc367d9c51a8cb1d7d85e54c5a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4540276
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145394}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureListWithProcessorUnitTest.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureListWithProcessorUnitTest.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.flags;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.Sets;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,6 +22,12 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Tests the behavior of {@link ChromeFeatureList} in Robolectric unit tests when the rule
@@ -82,5 +91,30 @@ public class ChromeFeatureListWithProcessorUnitTest {
     @DisableFeatures(ChromeFeatureList.TEST_DEFAULT_ENABLED)
     public void testSetTestFeaturesDisabled_returnsDisabled() {
         assertFalse(ChromeFeatureList.isEnabled(ChromeFeatureList.TEST_DEFAULT_ENABLED));
+    }
+
+    @Test
+    public void testAllCachedFlagsMap_matchesCachedFlagsDeclared() throws IllegalAccessException {
+        HashSet<String> cachedFlagsDeclared = new HashSet<>();
+        for (Field field : ChromeFeatureList.class.getDeclaredFields()) {
+            int modifiers = field.getModifiers();
+            if (CachedFlag.class.isAssignableFrom(field.getType()) && Modifier.isPublic(modifiers)
+                    && Modifier.isStatic(modifiers) && Modifier.isFinal(modifiers)) {
+                CachedFlag flag = (CachedFlag) field.get(null);
+                cachedFlagsDeclared.add(flag.getFeatureName());
+            }
+        }
+
+        Set<String> cachedFlagsListed = ChromeFeatureList.sAllCachedFlags.keySet();
+
+        Set<String> declaredButNotListed = Sets.difference(cachedFlagsDeclared, cachedFlagsListed);
+        assertEquals(
+                "Cached flags declared in ChromeFeatureList, but not added to |sAllCachedFlags|",
+                Collections.emptySet(), declaredButNotListed);
+
+        Set<String> listedButNotDeclared = Sets.difference(cachedFlagsListed, cachedFlagsDeclared);
+        assertEquals("Cached flags listed in |sAllCachedFlags|, but not declared as public static "
+                        + "final in ChromeFeatureList",
+                Collections.emptySet(), listedButNotDeclared);
     }
 }
