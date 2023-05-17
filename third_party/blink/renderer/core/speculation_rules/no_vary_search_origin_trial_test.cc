311b79bcc245cfef8c633cad39f521a1793ff8b2
Liviu Tinta <liviutinta@chromium.org>
2023-05-17 01:21:01 +0000

    [no-vary-search-hint] Enable No-Vary-Search hint for origin trial
    We would like to experiment with No-Vary-Search hint as part of the
    existing origin trial for No-Vary-Search.
    
    We would also like to use "eagerness" functionality to support
    No-Vary-Search hint matching between URLs the browser predicts
    are good candidates for the next navigation (at pointerdown or
    on-hover) and prefetch candidates.
    
    Bug: 1416659
    Change-Id: I23d8c8effcf53d966cdb14e07ce004cdcdc28ce2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520273
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145058}

--- third_party/blink/renderer/core/speculation_rules/no_vary_search_origin_trial_test.cc
+++ third_party/blink/renderer/core/speculation_rules/no_vary_search_origin_trial_test.cc
@@ -83,6 +83,10 @@ TEST(PrefetchNoVarySearchOriginTrialTest, CanEnableFromToken) {
       RuntimeEnabledFeatures::NoVarySearchPrefetchEnabled(frame.DomWindow()));
   EXPECT_TRUE(RuntimeEnabledFeatures::SpeculationRulesPrefetchProxyEnabled(
       frame.DomWindow()));
+  EXPECT_TRUE(RuntimeEnabledFeatures::SpeculationRulesNoVarySearchHintEnabled(
+      frame.DomWindow()));
+  EXPECT_TRUE(RuntimeEnabledFeatures::SpeculationRulesEagernessEnabled(
+      frame.DomWindow()));
 }
 
 TEST(PrefetchNoVarySearchOriginTrialTest, DoesNotEnableWithoutToken) {
