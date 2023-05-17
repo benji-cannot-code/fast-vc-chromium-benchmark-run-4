f3683900c1237e55722f0241878a1f9e36dae79a
Ian Clelland <iclelland@chromium.org>
2023-05-17 19:18:26 +0000

    Revert "Reconnect with local agent when a new tab is open."
    This reverts commit 8495ff734660b4d7b7cc229921b8da0117910930.
    
    Reason for revert: Breaks android build with a failed assertion.
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20Release%20(Nexus%205X)/66205
    
    Original change's description:
    > Reconnect with local agent when a new tab is open.
    >
    > Example CL I followed: https://crrev.com/c/3456543.
    > Tracking bug for adding browser tests: b/283111162, which will be tackled during DPR fix-it week.
    >
    > Bug: b/268477311, b/283111162
    > Change-Id: I9499f0f3c061e3b44b2602ad7dd0585d887568ac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520619
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1145508}
    
    Bug: b/268477311, b/283111162
    Change-Id: I14aa7df9caf36ee1afdb9f24268bffcb83ba6197
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544942
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ian Clelland <iclelland@chromium.org>
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145517}

--- chrome/browser/enterprise/connectors/connectors_manager.cc
+++ chrome/browser/enterprise/connectors/connectors_manager.cc
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/analysis/content_analysis_features.h"
 #include "chrome/browser/enterprise/connectors/reporting/browser_crash_event_router.h"
 #include "chrome/browser/enterprise/connectors/reporting/extension_install_event_router.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "components/prefs/pref_service.h"
 #include "url/gurl.h"
 
@@ -25,19 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace enterprise_connectors {
-
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-namespace {
-
-static constexpr enterprise_connectors::AnalysisConnector
-    kLocalAnalysisConnectors[] = {
-        AnalysisConnector::BULK_DATA_ENTRY,
-        AnalysisConnector::FILE_ATTACHED,
-        AnalysisConnector::PRINT,
-};
-
-}  // namespace
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 ConnectorsManager::ConnectorsManager(
     std::unique_ptr<BrowserCrashEventRouter> browser_crash_event_router,
@@ -51,14 +35,6 @@ ConnectorsManager::ConnectorsManager(
           std::move(extension_install_event_router)) {
   DCHECK(browser_crash_event_router_) << "Crash event router is null";
   DCHECK(extension_install_event_router_) << "Extension event router is null";
-
-  // Start observing tab strip models for all browsers.
-  BrowserList* browser_list = BrowserList::GetInstance();
-  for (Browser* browser : *browser_list) {
-    OnBrowserAdded(browser);
-  }
-  browser_list->AddObserver(this);
-
   if (observe_prefs) {
     StartObservingPrefs(pref_service);
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
@@ -68,13 +44,7 @@ ConnectorsManager::ConnectorsManager(
   extension_install_event_router_->StartObserving();
 }
 
-ConnectorsManager::~ConnectorsManager() {
-  BrowserList* browser_list = BrowserList::GetInstance();
-  browser_list->RemoveObserver(this);
-  for (Browser* browser : *browser_list) {
-    OnBrowserRemoved(browser);
-  }
-}
+ConnectorsManager::~ConnectorsManager() = default;
 
 bool ConnectorsManager::IsConnectorEnabled(AnalysisConnector connector) const {
   if (analysis_connector_settings_.count(connector) == 0 &&
@@ -173,42 +143,6 @@ absl::optional<AnalysisSettings> ConnectorsManager::GetAnalysisSettings(
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
-void ConnectorsManager::OnBrowserAdded(Browser* browser) {
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-  browser->tab_strip_model()->AddObserver(this);
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-}
-
-void ConnectorsManager::OnBrowserRemoved(Browser* browser) {
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-  browser->tab_strip_model()->RemoveObserver(this);
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-}
-
-void ConnectorsManager::OnTabStripModelChanged(
-    TabStripModel* tab_strip_model,
-    const TabStripModelChange& change,
-    const TabStripSelectionChange& selection) {
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-  // Checking only when new tab is open.
-  if (change.type() != TabStripModelChange::kInserted) {
-    return;
-  }
-
-  for (auto connector : kLocalAnalysisConnectors) {
-    if (!IsConnectorEnabledForLocalAgent(connector)) {
-      continue;
-    }
-
-    // Send a connection event to the local agent. If all the enabled connectors
-    // are configured to use the same agent, the same connection is reused here.
-    auto configs = GetAnalysisServiceConfigs(connector);
-    enterprise_connectors::ContentAnalysisSdkManager::Get()->GetClient(
-        {configs[0]->local_path, configs[0]->user_specific});
-  }
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-}
-
 absl::optional<AnalysisSettings>
 ConnectorsManager::GetAnalysisSettingsFromConnectorPolicy(
     const GURL& url,
@@ -242,7 +176,13 @@ void ConnectorsManager::CacheAnalysisConnectorPolicy(
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 void ConnectorsManager::MaybeCloseLocalContentAnalysisAgentConnection() {
-  for (auto connector : kLocalAnalysisConnectors) {
+  constexpr enterprise_connectors::AnalysisConnector kConnectors[] = {
+      AnalysisConnector::BULK_DATA_ENTRY,
+      AnalysisConnector::FILE_ATTACHED,
+      AnalysisConnector::PRINT,
+  };
+
+  for (auto connector : kConnectors) {
     if (IsConnectorEnabledForLocalAgent(connector)) {
       // Return early because at lease one access point is enabled for local
       // agent.
