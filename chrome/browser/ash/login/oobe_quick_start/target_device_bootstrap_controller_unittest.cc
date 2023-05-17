f607341b817fbb74c123b1f5bae5665fa209847d
Brittany Hartmire <bhartmire@google.com>
2023-05-17 20:02:27 +0000

    [QuickStart] Refactor shared code in Bootstrap Controller test
    Change-Id: I605c744dd4ce8ff85bb9dc089abbd1d20859b771
    Bug: b/279612088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520770
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145563}

--- chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller_unittest.cc
+++ chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller_unittest.cc
@@ -85,6 +85,16 @@ class TargetDeviceBootstrapControllerTest : public testing::Test {
     bootstrap_controller_->AddObserver(fake_observer_.get());
   }
 
+  void BootstrapConnection() {
+    bootstrap_controller_->StartAdvertising();
+    fake_target_device_connection_broker_->on_start_advertising_callback().Run(
+        /*success=*/true);
+    fake_target_device_connection_broker_->InitiateConnection(kSourceDeviceId);
+    fake_target_device_connection_broker_->AuthenticateConnection(
+        kSourceDeviceId);
+    ASSERT_EQ(fake_observer_->last_status.step, Step::CONNECTED);
+  }
+
   void NotifySourceOfUpdateResponse(bool ack_successful) {
     bootstrap_controller_->OnNotifySourceOfUpdateResponse(ack_successful);
   }
@@ -171,14 +181,7 @@ TEST_F(TargetDeviceBootstrapControllerTest, InitiateConnection_Pin) {
 }
 
 TEST_F(TargetDeviceBootstrapControllerTest, AuthenticateConnection) {
-  bootstrap_controller_->StartAdvertising();
-  fake_target_device_connection_broker_->on_start_advertising_callback().Run(
-      /*success=*/true);
-  fake_target_device_connection_broker_->InitiateConnection(kSourceDeviceId);
-  fake_target_device_connection_broker_->AuthenticateConnection(
-      kSourceDeviceId);
-
-  EXPECT_EQ(fake_observer_->last_status.step, Step::CONNECTED);
+  BootstrapConnection();
   EXPECT_TRUE(absl::holds_alternative<absl::monostate>(
       fake_observer_->last_status.payload));
 }
@@ -249,14 +252,7 @@ TEST_F(TargetDeviceBootstrapControllerTest,
   ASSERT_TRUE(GetLocalState()
                   ->GetDict(prefs::kResumeQuickStartAfterRebootInfo)
                   .empty());
-
-  bootstrap_controller_->StartAdvertising();
-  fake_target_device_connection_broker_->on_start_advertising_callback().Run(
-      /*success=*/true);
-  fake_target_device_connection_broker_->InitiateConnection(kSourceDeviceId);
-  fake_target_device_connection_broker_->AuthenticateConnection(
-      kSourceDeviceId);
-  ASSERT_EQ(fake_observer_->last_status.step, Step::CONNECTED);
+  BootstrapConnection();
 
   NotifySourceOfUpdateResponse(/*ack_successful=*/true);
 
@@ -278,14 +274,7 @@ TEST_F(TargetDeviceBootstrapControllerTest,
   ASSERT_TRUE(GetLocalState()
                   ->GetDict(prefs::kResumeQuickStartAfterRebootInfo)
                   .empty());
-
-  bootstrap_controller_->StartAdvertising();
-  fake_target_device_connection_broker_->on_start_advertising_callback().Run(
-      /*success=*/true);
-  fake_target_device_connection_broker_->InitiateConnection(kSourceDeviceId);
-  fake_target_device_connection_broker_->AuthenticateConnection(
-      kSourceDeviceId);
-  ASSERT_EQ(fake_observer_->last_status.step, Step::CONNECTED);
+  BootstrapConnection();
 
   NotifySourceOfUpdateResponse(/*ack_successful=*/false);
 
