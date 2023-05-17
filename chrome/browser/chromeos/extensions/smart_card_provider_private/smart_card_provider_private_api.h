5b6a9a3adfbd14e762e25d27fb12e3a8b7c2df7f
Daniel d'Andrada <dandrader@google.com>
2023-05-17 07:29:35 +0000

    SmartCard: Implement Control in SmartCardProviderAPI
    Bug: 1386175
    Change-Id: I3dc573de37c9e9b437edc3b210b2290a39922149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481324
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145180}

--- chrome/browser/chromeos/extensions/smart_card_provider_private/smart_card_provider_private_api.h
+++ chrome/browser/chromeos/extensions/smart_card_provider_private/smart_card_provider_private_api.h
@@ -100,6 +100,9 @@ class SmartCardProviderPrivateAPI
   void Transmit(device::mojom::SmartCardProtocol protocol,
                 const std::vector<uint8_t>& data,
                 TransmitCallback callback) override;
+  void Control(uint32_t control_code,
+               const std::vector<uint8_t>& data,
+               ControlCallback callback) override;
 
   // Called by extension functions:
   void ReportResult(RequestId request_id,
@@ -174,6 +177,11 @@ class SmartCardProviderPrivateAPI
                     device::mojom::SmartCardProtocol protocol,
                     const std::vector<uint8_t>& data,
                     TransmitCallback callback);
+  void SendControl(ContextId scard_context,
+                   Handle handle,
+                   uint32_t control_code,
+                   const std::vector<uint8_t>& data,
+                   ControlCallback callback);
 
   // Called when a device::mojom::SmartCardContext loses its mojo connection.
   // eg: because its mojo Remote was destroyed.
@@ -205,6 +213,8 @@ class SmartCardProviderPrivateAPI
                            RequestId request_id);
   void OnTransmitTimeout(const std::string& provider_extension_id,
                          RequestId request_id);
+  void OnControlTimeout(const std::string& provider_extension_id,
+                        RequestId request_id);
 
   template <typename ResultPtr>
   void DispatchEventWithTimeout(
