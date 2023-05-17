24b26e3a7a8f8cd169ad75a1ee53cd6a577af64f
Nate Chapin <japhet@chromium.org>
2023-05-17 22:55:57 +0000

    Don't crash in structuredClone() or reportError() on a detached window
    Fixed: 1445375
    Change-Id: I943d5a45c4914498afe96064b82c2b79532bd533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4539525
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145672}

--- third_party/blink/renderer/core/frame/window_or_worker_global_scope.cc
+++ third_party/blink/renderer/core/frame/window_or_worker_global_scope.cc
@@ -98,6 +98,9 @@ static bool IsAllowed(ExecutionContext* execution_context,
 void WindowOrWorkerGlobalScope::reportError(ScriptState* script_state,
                                             EventTarget& event_target,
                                             const ScriptValue& e) {
+  if (!script_state->ContextIsValid()) {
+    return;
+  }
   ScriptState::Scope scope(script_state);
   V8ScriptRunner::ReportException(script_state->GetIsolate(), e.V8Value());
 }
@@ -250,6 +253,9 @@ ScriptValue WindowOrWorkerGlobalScope::structuredClone(
     const ScriptValue& message,
     const StructuredSerializeOptions* options,
     ExceptionState& exception_state) {
+  if (!script_state->ContextIsValid()) {
+    return ScriptValue();
+  }
   ScriptState::Scope scope(script_state);
   v8::Isolate* isolate = script_state->GetIsolate();
 
