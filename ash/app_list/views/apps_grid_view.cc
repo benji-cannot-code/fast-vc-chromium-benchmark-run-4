545042e80f28be1886a033b3b0675ece1c8f0253
Ana Salazar <anasalazar@chromium.org>
2023-05-17 01:51:48 +0000

    Track when apps grid has a pending cardified state animation
    Add a flag to keep track when the paged apps grid view is running a
    cardified state animation.
    
    Item layers are deleted usually after animations end if no other pending
    animations are running.  AppsGridView provides a method to detect
    whether items need layers, but it failed to consider cardified state
    animation running, which also may modifies item bounds and thus, need
    layers.
    
    The CL is an speculative fix for a crash during AppsGrid
    EndCardifiedState, that may interrupt an item animation while setting
    the newer ones.
    
    Bug: b:282238658
    Change-Id: I41f803f2d9e5b2c7db41d0e00c63fb7b04c1b345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4538106
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145083}

--- ash/app_list/views/apps_grid_view.cc
+++ ash/app_list/views/apps_grid_view.cc
@@ -1487,6 +1487,10 @@ absl::optional<int> AppsGridView::TilesPerPage(int page) const {
   return *max_rows * cols();
 }
 
+bool AppsGridView::IsAnimatingCardifiedState() const {
+  return false;
+}
+
 bool AppsGridView::MaybeStartPageFlip() {
   return false;
 }
@@ -2962,6 +2966,10 @@ bool AppsGridView::ItemViewsRequireLayers() const {
 
   if (setting_up_ideal_bounds_animation_)
     return true;
+
+  if (IsAnimatingCardifiedState()) {
+    return true;
+  }
 
   return false;
 }
