070343f8161fb7a65a9c9234444df05ffd4f297b
Chloe Pelling <cpelling@google.com>
2023-05-17 22:50:21 +0000

    Refactor GuestOS protocol handler interface.
    First step towards implementing
    go/borealis-installer-entry-point#heading=h.gjmxdv9g632b.
    
    * Abstracts "app registrations" from "URL handlers". In this model,
      a "URL handler" is just a name and a callback, so can be constructed
      without necessarily needing a Registration. This will be needed in
      the next step.
    
    * Most logic moves into GuestOsRegistryService as a side-effect.
      Since it houses the app list, it also becomes responsible for checking
      which apps support which protocols, and creating the handler callback.
      This change is not strictly necessary, but it feels more natural,
      and gives us a place to register non-app URL handlers in the next CL.
    
    BUG=b:242947809
    
    Change-Id: Ib6622b6348babb065eaf52a3ef3de0d828151abb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517817
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145669}

--- chrome/browser/ash/guest_os/guest_os_registry_service.h
+++ chrome/browser/ash/guest_os/guest_os_registry_service.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_icon/app_icon_factory.h"
 #include "chrome/browser/ash/crostini/crostini_simple_types.h"
 #include "chrome/browser/ash/guest_os/guest_id.h"
+#include "chrome/browser/ash/guest_os/guest_os_external_protocol_handler.h"
 #include "chrome/browser/ash/guest_os/public/types.h"
 #include "chromeos/ash/components/dbus/vm_applications/apps.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -166,6 +167,9 @@ class GuestOsRegistryService : public KeyedService {
   // Return null if |app_id| is not found in the registry.
   absl::optional<GuestOsRegistryService::Registration> GetRegistration(
       const std::string& app_id) const;
+
+  // Return the preferred handler for the given URL, if any.
+  absl::optional<GuestOsUrlHandler> GetHandler(const GURL& url) const;
 
   // Constructs path to app icon for specific scale factor.
   base::FilePath GetIconPath(const std::string& app_id,
