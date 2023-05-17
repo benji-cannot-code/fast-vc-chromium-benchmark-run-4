6d5522d644e2bd7b062b4581edcb70e50f3d7473
Alexander Hendrich <hendrich@chromium.org>
2023-05-17 13:29:35 +0000

    Revert "Ash: Extend test coverage for Web Content Handler"
    This reverts commit d3fcdc6b7d506c843b40767c21b1b1a3e479e630.
    
    Reason for revert: MSAN failures (see http://b/278079069#comment3)
    
    Original change's description:
    > Ash: Extend test coverage for Web Content Handler
    >
    > This change adds test coverage for the public API
    > method `RequestLocalApproval` for Ash.
    >
    > It is a dependency of the change crrev.com/c/4484597,
    > and it will help refactor the internal methods of
    > the Web Content Handler that don't need access to
    > the object's attributes.
    >
    > Bug: b/278079069
    > Change-Id: Iec9cd0d7b7905d138113f126858d74b2db7c1f71
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502790
    > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > Reviewed-by: Duong Dac <ddac@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1145200}
    
    Bug: b/278079069
    Change-Id: I4f5d21a1ebbe82a8fffeddb7d392b2c34bc39376
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542246
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145292}

--- chrome/browser/ash/crosapi/parent_access_ash_unittest.cc
+++ chrome/browser/ash/crosapi/parent_access_ash_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
-#include "chrome/browser/ash/crosapi/fake_ash_parent_access_dialog_provider.h"
 #include "chrome/browser/ui/webui/ash/parent_access/parent_access_dialog.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -23,6 +22,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_unittest_util.h"
 #include "url/gurl.h"
+
+class FakeParentAccessDialogProvider : public ash::ParentAccessDialogProvider {
+ public:
+  ParentAccessDialogProvider::ShowError Show(
+      parent_access_ui::mojom::ParentAccessParamsPtr params,
+      ash::ParentAccessDialog::Callback callback) override {
+    callback_ = std::move(callback);
+    last_params_received_ = std::move(params);
+    return next_show_error_;
+  }
+
+  void SetNextShowError(ash::ParentAccessDialogProvider::ShowError error) {
+    next_show_error_ = error;
+  }
+
+  parent_access_ui::mojom::ParentAccessParamsPtr GetLastParamsReceived() {
+    return std::move(last_params_received_);
+  }
+
+  void TriggerCallbackWithResult(
+      std::unique_ptr<ash::ParentAccessDialog::Result> result) {
+    std::move(callback_).Run(std::move(result));
+  }
+
+ private:
+  ash::ParentAccessDialog::Callback callback_;
+  parent_access_ui::mojom::ParentAccessParamsPtr last_params_received_;
+  ash::ParentAccessDialogProvider::ShowError next_show_error_;
+};
 
 namespace {
 constexpr char test_url[] = "http://example.com";
@@ -41,9 +69,9 @@ class ParentAccessAshTest : public testing::Test {
     parent_access_ash_ = std::make_unique<crosapi::ParentAccessAsh>();
     parent_access_ash_->BindReceiver(
         parent_access_remote_.BindNewPipeAndPassReceiver());
-    dialog_provider_ = static_cast<crosapi::FakeAshParentAccessDialogProvider*>(
+    dialog_provider_ = static_cast<FakeParentAccessDialogProvider*>(
         parent_access_ash_->SetDialogProviderForTest(
-            std::make_unique<crosapi::FakeAshParentAccessDialogProvider>()));
+            std::make_unique<FakeParentAccessDialogProvider>()));
   }
 
   void TearDown() override { parent_access_ash_.reset(); }
@@ -52,8 +80,7 @@ class ParentAccessAshTest : public testing::Test {
   base::test::TaskEnvironment task_environment_;
   mojo::Remote<crosapi::mojom::ParentAccess> parent_access_remote_;
   std::unique_ptr<crosapi::ParentAccessAsh> parent_access_ash_;
-  raw_ptr<crosapi::FakeAshParentAccessDialogProvider, ExperimentalAsh>
-      dialog_provider_;
+  raw_ptr<FakeParentAccessDialogProvider, ExperimentalAsh> dialog_provider_;
 };
 
 // Tests that the correct parameters were passed through to the dialog for
