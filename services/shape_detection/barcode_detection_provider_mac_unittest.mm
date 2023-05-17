5892204ff53413499a5f1c90ed096e04e42e856b
Avi Drissman <avi@chromium.org>
2023-05-17 15:42:31 +0000

    Convert /services to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Bug: 1280317
    Change-Id: I0b3f0091c51bfd350eac7c1711cc725ef347da56
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4540462
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145369}

--- services/shape_detection/barcode_detection_provider_mac_unittest.mm
+++ services/shape_detection/barcode_detection_provider_mac_unittest.mm
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/shape_detection/public/mojom/barcodedetection_provider.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using base::test::RunClosure;
 using ::testing::NiceMock;
 using ::testing::Return;
