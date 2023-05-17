5e7063fb76ed22864a009f6727c91e31d61923e6
Gaston Rodriguez Lopez <gastonr@microsoft.com>
2023-05-17 23:09:07 +0000

    Add high contrast mode for fluent scrollbars.
    Adding high contrast color definition for fluent scrollbars.
    
    Added a high contrast color providers in blink's web theme
    engine default to comply with the dev tool's emulated forced mode.
    
    When system colors are removed from native themes, the definition for emulation colors in WebThemeEngineDefault::OverrideForcedColorsTheme will be moved to ui::CreateEmulatedForcedColorsColorProvider to live in the emulated forced colors color provider.
    
    Bug: 1378337
    Change-Id: I384a2ce0bc51104ff3adfccc8d3c86ff1f30fe9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457467
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1145683}

--- ui/color/color_provider_utils.h
+++ ui/color/color_provider_utils.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_id.mojom.h"
+#include "ui/color/color_mixer.h"
 #include "ui/color/color_provider_manager.h"
 
 namespace ui {
@@ -71,6 +72,17 @@ RendererColorMap COMPONENT_EXPORT(COLOR)
 // in the renderer process.
 ColorProvider COMPONENT_EXPORT(COLOR) CreateColorProviderFromRendererColorMap(
     const RendererColorMap& renderer_color_map);
+
+// Creates a color provider emulating Windows 10 default high contrast color
+// themes. Currently only defines colors for scrollbar parts.
+ColorProvider COMPONENT_EXPORT(COLOR)
+    CreateEmulatedForcedColorsColorProvider(bool dark_mode);
+
+// Fluent scrollbars have three main colors. This function completes the
+// definition of colors for all scrollbar parts in relation to the three main
+// ones.
+void COMPONENT_EXPORT(COLOR)
+    CompleteFluentScrollbarColorsDefinition(ui::ColorMixer& mixer);
 
 // Returns true if `color_provider` and `renderer_color_map` map renderer
 // color ids to the same SkColor.
