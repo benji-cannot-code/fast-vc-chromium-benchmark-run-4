18e8b5778798eba5b4886d629f81415a26a74909
David Yeung <dayeung@chromium.org>
2023-05-17 22:06:24 +0000

    [CR2023] Remove toggle button horizontal offset in CR2023.
    In the Pageinfo dialog, the toggle button is not right-aligned with the other rows in the view. For example, the toggle button and the launch icon seem mismatched in the page info cookie view. This is caused by a horizontal offset in the Toggle button.
    
    With the updated UI for the toggle button in CR2023, there does not seem to be a need for this horizontal offset anymore. Fix is to just set the offset to 0 so the toggle button is right aligned with the rest of the view.
    
    https://screenshot.googleplex.com/AGEmH2GCQRvYob3
    
    Bug: 1443661, 1422123
    Change-Id: I9ae05570aefc6c2aefd54f11b560e73855a95344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544086
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145643}

--- ui/views/controls/button/toggle_button.cc
+++ ui/views/controls/button/toggle_button.cc
@@ -56,6 +56,7 @@ constexpr int kRefreshThumbInset = -4;
 constexpr int kRefreshThumbInsetSelected = -2;
 constexpr int kRefreshThumbPressedOutset = 1;
 constexpr int kRefreshHoverDiameter = 20;
+constexpr int kTrackHorizontalMarginChromeRefresh = 0;
 
 const gfx::Size GetTrackSize() {
   return features::IsChromeRefresh2023() ? kRefreshTrackSize : kTrackSize;
@@ -382,7 +383,10 @@ void ToggleButton::RemoveLayerFromRegions(ui::Layer* layer) {
 
 gfx::Size ToggleButton::CalculatePreferredSize() const {
   gfx::Rect rect(GetTrackSize());
-  rect.Inset(gfx::Insets::VH(-kTrackVerticalMargin, -kTrackHorizontalMargin));
+  rect.Inset(gfx::Insets::VH(-kTrackVerticalMargin,
+                             features::IsChromeRefresh2023()
+                                 ? kTrackHorizontalMarginChromeRefresh
+                                 : -kTrackHorizontalMargin));
   rect.Inset(-GetInsets());
   return rect.size();
 }
