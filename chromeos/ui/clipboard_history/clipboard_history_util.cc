194b93c2e03806c9b765e512af6486d865cec6f3
andrewxu <andrewxu@chromium.org>
2023-05-17 02:03:21 +0000

    [Multipaste Growth] The nested menu supports file data
    With this CL, the clipboard history nested menu support clipboard
    file data. See the screenshot attached to the issue.
    
    Bug: b/281568172
    Change-Id: I9a5e7e106a3b57dcc422fc89302e715dcf764058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4461914
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145084}

--- chromeos/ui/clipboard_history/clipboard_history_util.cc
+++ chromeos/ui/clipboard_history/clipboard_history_util.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "base/notreached.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chromeos/crosapi/mojom/clipboard_history.mojom.h"
+#include "chromeos/ui/base/file_icon_util.h"
 #include "chromeos/ui/vector_icons/vector_icons.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/models/image_model.h"
@@ -59,10 +61,10 @@ void PasteClipboardItemById(
   GetPasteClipboardItemByIdImpl().Run(id, event_flags, show_source);
 }
 
-ui::ImageModel GetIconForDisplayFormat(
-    crosapi::mojom::ClipboardHistoryDisplayFormat display_format) {
+ui::ImageModel GetIconForDescriptor(
+    const crosapi::mojom::ClipboardHistoryItemDescriptor& descriptor) {
   const gfx::VectorIcon* icon = nullptr;
-  switch (display_format) {
+  switch (descriptor.display_format) {
     case crosapi::mojom::ClipboardHistoryDisplayFormat::kText:
       icon = &kTextIcon;
       break;
@@ -72,9 +74,16 @@ ui::ImageModel GetIconForDisplayFormat(
     case crosapi::mojom::ClipboardHistoryDisplayFormat::kHtml:
       icon = &vector_icons::kCodeIcon;
       break;
-    case crosapi::mojom::ClipboardHistoryDisplayFormat::kFile:
-      // TODO(b/281568172): Add menu item icons for other display formats.
+    case crosapi::mojom::ClipboardHistoryDisplayFormat::kFile: {
+      // If `display_text` is the name of a single file, use the icon
+      // corresponding to the file type, if any; otherwise, use a generic
+      // multi-file icon.
+      icon = descriptor.file_count == 1
+                 ? &chromeos::GetIconForPath(base::FilePath(
+                       base::UTF16ToASCII(descriptor.display_text)))
+                 : &vector_icons::kContentCopyIcon;
       break;
+    }
     case crosapi::mojom::ClipboardHistoryDisplayFormat::kUnknown:
       NOTREACHED_NORETURN();
   }
@@ -86,7 +95,7 @@ ui::ImageModel GetIconForDisplayFormat(
                                           kIconSize);
   }
 
-  return ui::ImageModel();
+  NOTREACHED_NORETURN();
 }
 
 }  // namespace chromeos::clipboard_history
