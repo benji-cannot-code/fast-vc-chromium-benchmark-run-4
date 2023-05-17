72b8e2a5fb200f2bea0ac1a418e82279e0e93144
Martin Kreichgauer <martinkr@google.com>
2023-05-17 22:22:03 +0000

    webauthn: minor tweaks to request JSON serialization
    Some small improvements to the code serializing WebAuthn requests, which is used by the chrome.webAuthenticationProxy extensions API
    and for talking to the Android CredMan API:
    
    - Treat PublicKeyCredentialUserEntityJSON.user_display_name as required,
      like the spec does.
    - Omit `extensions` if it is empty on both
      PublicKeyCredentialRequestOptionsJSON and
      PublicKeyCredentialRegistrationOptionsJSON, since it is optional.
      Previously, we emitted an empty dict, which isn't wrong, but is
      superfluous.
    
    Also resolve TODOs in the header to reference the JSON IDL, which
    had not yet been spec'd when the code was originally written.
    
    Change-Id: I84aa5c1d270b1cbcd8685ba91b4896a2d57928b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544920
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145652}

--- components/webauthn/json/value_conversions.cc
+++ components/webauthn/json/value_conversions.cc
@@ -103,9 +103,9 @@ base::Value ToValue(const device::PublicKeyCredentialUserEntity& user) {
   // `PublicKeyCredentialEntity.name` is required in the IDL but optional on the
   // mojo struct.
   value.Set("name", user.name.value_or(""));
-  if (user.display_name) {
-    value.Set("displayName", *user.display_name);
-  }
+  // `PublicKeyCredentialUserEntity.displayName` is required in the IDL but
+  // optional on the mojo struct.
+  value.Set("displayName", user.display_name.value_or(""));
   return base::Value(std::move(value));
 }
 
@@ -331,8 +331,7 @@ base::Value ToValue(
   }
   value.Set("attestation", ToValue(options->attestation));
 
-  base::Value::Dict& extensions =
-      value.Set("extensions", base::Value::Dict())->GetDict();
+  base::Value::Dict extensions;
 
   if (options->hmac_create_secret) {
     extensions.Set("hmacCreateSecret", true);
@@ -377,6 +376,10 @@ base::Value ToValue(
 
   DCHECK(!options->prf_enable);
 
+  if (!extensions.empty()) {
+    value.Set("extensions", std::move(extensions));
+  }
+
   return base::Value(std::move(value));
 }
 
@@ -395,8 +398,7 @@ base::Value ToValue(
 
   value.Set("userVerification", ToValue(options->user_verification));
 
-  base::Value::Dict& extensions =
-      value.Set("extensions", base::Value::Dict())->GetDict();
+  base::Value::Dict extensions;
 
   if (options->appid) {
     extensions.Set("appid", *options->appid);
@@ -433,6 +435,10 @@ base::Value ToValue(
   }
 
   DCHECK(!options->prf);
+
+  if (!extensions.empty()) {
+    value.Set("extensions", std::move(extensions));
+  }
 
   return base::Value(std::move(value));
 }
