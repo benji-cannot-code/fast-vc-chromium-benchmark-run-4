61dcee502ce5c3c8e4b39b8dd12765246b3c63d3
Ted Meyer <tmathmeyer@chromium.org>
2023-05-17 01:09:56 +0000

    Implement HlsManifestDemuxerEngine
    This CL still needs better codec support checking before it is ready
    to submit.
    
    Bug: 1266991
    Change-Id: Ia613c8504bebe05f11a6d71d8700e3222729e6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357199
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145048}

---
+++ media/filters/hls_rendition.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/filters/hls_rendition.h"
+
+#include "media/filters/manifest_demuxer.h"
+
+namespace media {
+
+// Static
+HlsDemuxerStatus::Or<std::unique_ptr<HlsRendition>>
+HlsRendition::CreateRendition(ManifestDemuxerEngineHost* engine_host,
+                              HlsRenditionHost* rendition_host,
+                              std::string role,
+                              scoped_refptr<hls::MediaPlaylist> playlist,
+                              GURL uri) {
+  return HlsDemuxerStatus::Codes::kInvalidManifest;
+}
+
+}  // namespace media
