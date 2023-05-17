adef14098acc96ed00a6bfcf1ffa6627ca78d281
Liam Brady <lbrady@google.com>
2023-05-17 17:09:51 +0000

    Add automatic beacon click handling WPT.
    This CL adds a new WPT test to test calling
    setReportEventDataForAutomaticBeacons in a click handler. This test
    ensures that the data set is indeed what is sent in the outgoing
    automatic beacon, ensuring there is no raciness with the beacon
    setting/sending calls. This CL also adds support for automatic beacon
    testing in WPTs, which will allow us to migrate our automatic beacon
    browser tests to WPTs.
    
    Change-Id: Ic6e921f8ef854e67ab016407343c88397fd01e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4522304
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145438}

--- third_party/blink/web_tests/external/wpt/fenced-frame/resources/fledge-bidding-logic.js
+++ third_party/blink/web_tests/external/wpt/fenced-frame/resources/fledge-bidding-logic.js
@@ -26,5 +26,10 @@ function generateBid(interestGroup, auctionSignals, perBuyerSignals,
 
 function reportWin(
     auctionSignals, perBuyerSignals, sellerSignals, browserSignals) {
+  registerAdBeacon({
+    'reserved.top_navigation':
+        browserSignals.interestGroupOwner +
+        '/fenced-frame/resources/automatic-beacon-store.py'
+  });
   return;
 }
