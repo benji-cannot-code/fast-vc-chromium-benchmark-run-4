d61e2b7799af8fc7ba2bad6c483a513019dddb87
Frank Liberato <liberato@chromium.org>
2023-05-17 21:13:37 +0000

    Replace blank media notification titles with the package name.
    Some versions of Android don't like blank titles on media
    notifications.  Replace them with the package name.
    
    Bug: 1445673
    Change-Id: I5fda2be2112915696336b407d4571231c25650ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544468
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145602}

--- components/browser_ui/media/android/java/src/org/chromium/components/browser_ui/media/MediaNotificationController.java
+++ components/browser_ui/media/android/java/src/org/chromium/components/browser_ui/media/MediaNotificationController.java
@@ -540,7 +540,7 @@ public class MediaNotificationController {
         if (mMediaNotificationInfo.isPrivate) return metadataBuilder.build();
 
         metadataBuilder.putString(
-                MediaMetadataCompat.METADATA_KEY_TITLE, mMediaNotificationInfo.metadata.getTitle());
+                MediaMetadataCompat.METADATA_KEY_TITLE, getSafeNotificationTitle());
         metadataBuilder.putString(
                 MediaMetadataCompat.METADATA_KEY_ARTIST, mMediaNotificationInfo.origin);
 
@@ -799,7 +799,7 @@ public class MediaNotificationController {
             return;
         }
 
-        builder.setContentTitle(mMediaNotificationInfo.metadata.getTitle());
+        builder.setContentTitle(getSafeNotificationTitle());
         String artistAndAlbumText = getArtistAndAlbumText(mMediaNotificationInfo.metadata);
         builder.setContentText(artistAndAlbumText);
         builder.setSubText(mMediaNotificationInfo.origin);
@@ -917,5 +917,15 @@ public class MediaNotificationController {
 
     private static Context getContext() {
         return ContextUtils.getApplicationContext();
+    }
+
+    // Return a non-blank string for use as the notification title, to avoid issues on some
+    // versions of Android.
+    private String getSafeNotificationTitle() {
+        String title = mMediaNotificationInfo.metadata.getTitle();
+        if (title != null && title.toString().trim().length() > 0) {
+            return title;
+        }
+        return getContext().getPackageName();
     }
 }
