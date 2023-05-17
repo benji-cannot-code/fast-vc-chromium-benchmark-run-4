662b27d5a05271a9bfbc5eb33dd08fc790b388a3
Koji Ishii <kojii@chromium.org>
2023-05-17 01:08:43 +0000

    [text-wrap-pretty] Add `NGLineBreaker::SetBreakAt`
    This patch adds the capability to break at the specified point
    to `NGLineBreaker`.
    
    The score-based line breaker computes the line breaking
    points. Specifying the offset is more error-prone and
    performant than specifying the available width.
    
    This patch should have no behavior changes as the `SetBreakAt`
    is called only from unit tests in this patch. Following
    patches will call this from the real code.
    
    Bug: 1432798
    Change-Id: I2fffc0aed0bca17de63a75ccde233792c27568ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4540560
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145046}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_result.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_text_index.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_line_break_point.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_spacing.h"
 #include "third_party/blink/renderer/platform/text/text_break_iterator.h"
@@ -27,6 +28,7 @@ class NGInlineItem;
 class NGLineBreakCandidateContext;
 class NGLineInfo;
 class ResolvedTextLayoutAttributesIterator;
+class ShapingLineBreaker;
 
 // The line breaker needs to know which mode its in to properly handle floats.
 enum class NGLineBreakerMode { kContent, kMinContent, kMaxContent };
@@ -65,6 +67,8 @@ class CORE_EXPORT NGLineBreaker {
   // Override the available width to compute line breaks. This is reset after
   // each `NextLine`.
   void OverrideAvailableWidth(LayoutUnit available_width);
+  // Specify to break at the `offset` rather than the available width.
+  void SetBreakAt(const NGLineBreakPoint& offset);
 
   // Computing |NGLineBreakerMode::kMinContent| with |MaxSizeCache| caches
   // information that can help computing |kMaxContent|. It is recommended to set
@@ -147,13 +151,18 @@ class CORE_EXPORT NGLineBreaker {
   // Returns true if we should split NGInlineItem before
   // svg_addressable_offset_.
   bool ShouldCreateNewSvgSegment() const;
-  enum BreakResult { kSuccess, kOverflow };
+  enum BreakResult { kSuccess, kOverflow, kBreakAt };
   BreakResult BreakText(NGInlineItemResult*,
                         const NGInlineItem&,
                         const ShapeResult&,
                         LayoutUnit available_width,
                         LayoutUnit available_width_with_hyphens,
                         NGLineInfo*);
+  bool BreakTextAt(NGInlineItemResult*,
+                   const NGInlineItem&,
+                   ShapingLineBreaker& breaker,
+                   unsigned options,
+                   NGLineInfo*);
   bool BreakTextAtPreviousBreakOpportunity(NGInlineItemResult* item_result);
   bool HandleTextForFastMinContent(NGInlineItemResult*,
                                    const NGInlineItem&,
@@ -251,6 +260,7 @@ class CORE_EXPORT NGLineBreaker {
   LineBreakState state_;
   NGInlineItemTextIndex current_;
   unsigned svg_addressable_offset_ = 0;
+  NGLineBreakPoint break_at_;
 
   // |WhitespaceState| of the current end. When a line is broken, this indicates
   // the state of trailing whitespaces.
