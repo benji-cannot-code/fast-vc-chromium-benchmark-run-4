1811a1088aaeb57afe88792e6f835bebc78f6434
Brittany Hartmire <bhartmire@google.com>
2023-05-18 21:19:22 +0000

    [SmartLock] Remove deprecated challenge decrypt methods
    Requesting/Handling decrypt messages was only necessary when using
    Smart Lock on the Sign In screen. Now that Sign in with Smart Lock is
    deprecated, this cl removes those methods. Unlock with Smart Lock is
    unchanged.
    
    TEST=manually tested Smart Lock with strongbad and pixel4a
    
    Change-Id: I267aa246f98caeefe422edbe44b79239f42b12db
    Bug: b/227674947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544094
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1146180}

--- chromeos/ash/components/proximity_auth/messenger_observer.h
+++ chromeos/ash/components/proximity_auth/messenger_observer.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_ASH_COMPONENTS_PROXIMITY_AUTH_MESSENGER_OBSERVER_H_
 #define CHROMEOS_ASH_COMPONENTS_PROXIMITY_AUTH_MESSENGER_OBSERVER_H_
 
-#include <memory>
-#include <string>
-
 namespace proximity_auth {
 
 struct RemoteStatusUpdate;
@@ -22,13 +19,6 @@ class MessengerObserver {
 
   // Called when a RemoteStatusUpdate is received.
   virtual void OnRemoteStatusUpdate(const RemoteStatusUpdate& status_update) {}
-
-  // Called when a response to a 'decrypt_request' is received, with the
-  // |decrypted_bytes| that were returned by the remote device. An empty string
-  // indicates failure.
-  // TODO(b/227674947): Delete this method since it is only used for deprecated
-  // sign in with Smart Lock.
-  virtual void OnDecryptResponse(const std::string& decrypted_bytes) {}
 
   // Called when a response to a 'unlock_request' is received.
   // |success| is true iff the request was made successfully.
