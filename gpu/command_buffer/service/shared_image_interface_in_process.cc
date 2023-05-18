9e3407f2cef36384cf473a8c66144e89a431557a
Vikas Soni <vikassoni@chromium.org>
2023-05-18 21:22:10 +0000

    Add ScopedCache during GrDirectContext::freeGpuResources().
    Add ScopedCache during GrDirectContext::freeGpuResources() since it
    could result in shader cache access.
    
    Bug: 1444865
    Change-Id: I512f7f6a89006715809279da65d8a31e892edb58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544143
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146184}

--- gpu/command_buffer/service/shared_image_interface_in_process.cc
+++ gpu/command_buffer/service/shared_image_interface_in_process.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "gpu/config/gpu_feature_info.h"
 #include "gpu/config/gpu_preferences.h"
+#include "gpu/ipc/common/gpu_client_ids.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gl/gl_context.h"
 
@@ -280,7 +281,7 @@ void SharedImageInterfaceInProcess::CreateSharedImageWithDataOnGpuThread(
   // Creating a si with data can result in raster work. This will be a textureop
   // that requires a program. See crbug.com/1442725.
   absl::optional<gpu::raster::GrShaderCache::ScopedCacheUse> cache_use;
-  context_state_->UseShaderCache(cache_use);
+  context_state_->UseShaderCache(cache_use, gpu::kDisplayCompositorClientId);
 
   DCHECK(shared_image_factory_);
   if (!shared_image_factory_->CreateSharedImage(
