a8382e70a6221a6c00474985939cf681884ca4aa
Takumi Fujimoto <takumif@chromium.org>
2023-05-18 23:55:08 +0000

    Revert "Convert /chrome/browser/web_applications to use ARC"
    This reverts commit 93e51b8f93ab090c9d36fff9ba493eded3a0311b.
    
    Reason for revert: 
    Suspected to be the culprit of WebApp* test failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/18256/overview
    
    Original change's description:
    > Convert /chrome/browser/web_applications to use ARC
    >
    > See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    > for information about this conversion.
    >
    > This was committed as https://crrev.com/c/4541181 but then reverted.
    > The issue causing the revert is fixed by using a block rather than a
    > lambda, which will retain as needed.
    >
    > Bug: 1280317
    > Change-Id: Ife19fefd81a21fd3583b9670b88e242a8c7274e4
    > Include-Ci-Only-Tests: true
    > Cq-Include-Trybots: luci.chrome.try:mac-chrome
    > Validate-Test-Flakiness: skip
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546402
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Auto-Submit: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1146209}
    
    Bug: 1280317
    Change-Id: Ic889633865bf35b993cc455e2456559c267f07ab
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547027
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Quick-Run: Takumi Fujimoto <takumif@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1146271}

--- chrome/browser/web_applications/os_integration/web_app_run_on_os_login_mac.mm
+++ chrome/browser/web_applications/os_integration/web_app_run_on_os_login_mac.mm
@@ -8,11 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/web_applications/os_integration/web_app_shortcut_mac.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
+namespace web_app {
 
-namespace web_app::internals {
+namespace internals {
 
 bool RegisterRunOnOsLogin(const ShortcutInfo& shortcut_info) {
   base::FilePath shortcut_data_dir = GetShortcutDataDir(shortcut_info);
@@ -31,4 +29,6 @@ Result UnregisterRunOnOsLogin(const std::string& app_id,
   return Result::kOk;
 }
 
-}  // namespace web_app::internals
+}  // namespace internals
+
+}  // namespace web_app
