50a18d81c8d90b8d8f358fc8afa4f6cc64579433
Daniel Murphy <dmurph@google.com>
2023-05-18 21:57:44 +0000

    [dPWA] Adding metrics collection task for ML promotion.
    This change creates the skeleton task class to be used to accumulate
    metrics that are eventually emitted in a UKM event. It also includes
    a disabled browser test that will be used later to test for metrics
    emitted.
    
    Bug: b/280455340
    Change-Id: I33755f6795b522967c975623a9605ece73ad965b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4538050
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146212}

--- components/webapps/browser/installable/ml_installability_promoter.cc
+++ components/webapps/browser/installable/ml_installability_promoter.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/webapps/browser/installable/ml_installability_promoter.h"
 
+#include "base/feature_list.h"
+#include "base/time/time.h"
+#include "components/webapps/browser/features.h"
+#include "components/webapps/browser/installable/site_metrics_collection_task.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -18,13 +22,25 @@ void MLInstallabilityPromoter::StartGatheringMetricsForFrameUrl(
     const GURL& url) {
   // TODO(b/279521783): Start gathering UKMs from here. Assign the input url to
   // frame_url if a ML model is not already running.
+
+  CHECK(web_contents());
+  if (base::FeatureList::IsEnabled(features::kWebAppsMlUkmCollection)) {
+    // Note: the destruct of any previous task is intended here.
+    current_collection_task_ = SiteMetricsCollectionTask::CreateAndStart(
+        *web_contents(), /*maximum_wait_time=*/base::Seconds(3),
+        base::BindOnce(&MLInstallabilityPromoter::OnMetricsTaskFinished,
+                       weak_factory_.GetWeakPtr()));
+  }
+}
+
+void MLInstallabilityPromoter::OnMetricsTaskFinished() {
+  current_collection_task_.reset();
 }
 
 MLInstallabilityPromoter::MLInstallabilityPromoter(
     content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
-      content::WebContentsUserData<MLInstallabilityPromoter>(*web_contents),
-      frame_url_(web_contents->GetLastCommittedURL()) {}
+      content::WebContentsUserData<MLInstallabilityPromoter>(*web_contents) {}
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(MLInstallabilityPromoter);
 
--- storage/browser/quota/quota_manager_proxy.h
+++ storage/browser/quota/quota_manager_proxy.h
@@ -51,6 +51,8 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManagerProxy
     : public base::RefCountedThreadSafe<QuotaManagerProxy> {
  public:
   using UsageAndQuotaCallback = QuotaManagerImpl::UsageAndQuotaCallback;
+  using UsageAndQuotaWithBreakdownCallback =
+      QuotaManagerImpl::UsageAndQuotaWithBreakdownCallback;
 
   // The caller is responsible for calling InvalidateQuotaManagerImpl() before
   // `quota_manager_impl` is destroyed. `quota_manager_impl_task_runner` must be
@@ -214,6 +216,12 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManagerProxy
       blink::mojom::StorageType type,
       scoped_refptr<base::SequencedTaskRunner> callback_task_runner,
       UsageAndQuotaCallback callback);
+
+  void GetUsageAndQuotaWithBreakdown(
+      const blink::StorageKey& storage_key,
+      blink::mojom::StorageType type,
+      scoped_refptr<base::SequencedTaskRunner> callback_task_runner,
+      UsageAndQuotaWithBreakdownCallback callback);
 
   void GetBucketUsageAndQuota(
       BucketId bucket,
