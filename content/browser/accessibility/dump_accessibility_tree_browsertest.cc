08687a144b7f863b2b8022c06607f208f381f12b
Luciano Pacheco <lucmult@chromium.org>
2023-05-18 05:03:46 +0000

    Sheriff: Disable faling tests
    Bug: 1446550
    Change-Id: Ic4ac0e40914e4456eed2c3b384b52f7f2c4e0b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547018
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145818}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2646,12 +2646,14 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       AccessibilityModalDialogClosed) {
+                       // TODO(crbug.com/1446550): Re-enable this test
+                       DISABLED_AccessibilityModalDialogClosed) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-closed.html"));
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       AccessibilityModalDialogOpened) {
+                       // TODO(crbug.com/1446550): Re-enable this test
+                       DISABLED_AccessibilityModalDialogOpened) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-opened.html"));
 }
 
@@ -2675,7 +2677,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTestWithIgnoredNodes,
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       AccessibilityModalDialogStack) {
+                       // TODO(crbug.com/1446550): Re-enable this test
+                       DISABLED_AccessibilityModalDialogStack) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-stack.html"));
 }
 
