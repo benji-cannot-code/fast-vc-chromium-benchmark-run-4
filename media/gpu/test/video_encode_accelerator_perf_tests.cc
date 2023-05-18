e20a9cb55ee033e3df4507d24a153c03dcfedf51
Justin Green <greenjustin@google.com>
2023-05-18 16:03:41 +0000

    media/gpu: Add output bitstream saving to VAE perf tests
    Add a "--output_bitstream" flag to the
    video_encode_accelerator_perf_tests binary that will output the
    bitstream produced in the MeasureProducedBitstreamQuality test. Having
    the ability to dump the output bitstream is helpful for debugging
    quality issues in new hardware encoders.
    
    Bug: b:274756117
    Test: Tested by running video_encode_accelerator_perf_tests on Rex both
    with and without the flag.
    
    Change-Id: Iabb292a9e27d903a2d3659b7bcade35b4f467572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545399
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Justin Green <greenjustin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145963}

--- media/gpu/test/video_encode_accelerator_perf_tests.cc
+++ media/gpu/test/video_encode_accelerator_perf_tests.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/test_data_util.h"
 #include "media/base/video_decoder_config.h"
 #include "media/gpu/test/video.h"
+#include "media/gpu/test/video_encoder/bitstream_file_writer.h"
 #include "media/gpu/test/video_encoder/bitstream_validator.h"
 #include "media/gpu/test/video_encoder/decoder_buffer_validator.h"
 #include "media/gpu/test/video_encoder/video_encoder.h"
@@ -47,6 +48,7 @@ constexpr const char* usage_msg =
            [--codec=<codec>] [--svc_mode=<svc scalability mode>]
            [--bitrate_mode=(cbr|vbr)] [--reverse] [--bitrate=<bitrate>]
            [-v=<level>] [--vmodule=<config>] [--output_folder]
+           [--output_bitstream]
            [--disable_vaapi_lock]
            [--gtest_help] [--help]
            [<video path>] [<video metadata path>]
@@ -89,6 +91,9 @@ The following arguments are supported:
   --output_folder       overwrite the output folder used to store
                         performance metrics, if not specified results
                         will be stored in the current working directory.
+  --output_bitstream    save the output bitstream in either H264 AnnexB
+                        format (for H264) or IVF format (for vp8 and
+                        vp9) to <output_folder>/<testname>.
   --disable_vaapi_lock  disable the global VA-API lock if applicable,
                         i.e., only on devices that use the VA-API with a libva
                         backend that's known to be thread-safe and only in
@@ -734,6 +739,13 @@ class VideoEncoderTest : public ::testing::Test {
           /*spatial_layer_index_to_decode=*/absl::nullopt,
           /*temporal_layer_index_to_decode=*/absl::nullopt,
           /*spatial_layer_resolutions=*/{}, decoder_buffer_validator_));
+      if (g_env->SaveOutputBitstream()) {
+        bitstream_processors.emplace_back(BitstreamFileWriter::Create(
+            g_env->OutputFilePath(VideoCodecProfileToVideoCodec(profile),
+                                  video->FilePath().BaseName()),
+            VideoCodecProfileToVideoCodec(profile), video->Resolution(),
+            video->FrameRate(), video->NumFrames()));
+      }
     } else {
       // Temporal/Spatial layer encoding.
       std::vector<gfx::Size> spatial_layer_resolutions;
@@ -746,6 +758,15 @@ class VideoEncoderTest : public ::testing::Test {
           bitstream_processors.push_back(CreateBitstreamValidator(
               profile, gfx::Rect(spatial_layer_resolutions[sid]), sid, tid,
               spatial_layer_resolutions, decoder_buffer_validator_));
+          if (g_env->SaveOutputBitstream()) {
+            bitstream_processors.emplace_back(BitstreamFileWriter::Create(
+                g_env->OutputFilePath(VideoCodecProfileToVideoCodec(profile),
+                                      video->FilePath().BaseName(), true, sid,
+                                      tid),
+                VideoCodecProfileToVideoCodec(profile),
+                spatial_layer_resolutions[sid], video->FrameRate(),
+                video->NumFrames(), sid, tid, spatial_layer_resolutions));
+          }
         }
       }
     }
@@ -910,6 +931,7 @@ int main(int argc, char** argv) {
   std::string codec = "h264";
   media::Bitrate::Mode bitrate_mode = media::Bitrate::Mode::kConstant;
   bool reverse = false;
+  bool output_bitstream = false;
   absl::optional<uint32_t> encode_bitrate;
   std::vector<base::test::FeatureRef> disabled_features;
   std::string svc_mode = "L1T1";
@@ -933,6 +955,8 @@ int main(int argc, char** argv) {
 
     if (it->first == "output_folder") {
       output_folder = it->second;
+    } else if (it->first == "output_bitstream") {
+      output_bitstream = true;
     } else if (it->first == "codec") {
       codec = it->second;
     } else if (it->first == "svc_mode") {
@@ -970,8 +994,8 @@ int main(int argc, char** argv) {
   media::test::VideoEncoderTestEnvironment* test_environment =
       media::test::VideoEncoderTestEnvironment::Create(
           video_path, video_metadata_path, false, base::FilePath(output_folder),
-          codec, svc_mode, false /* output_bitstream */, encode_bitrate,
-          bitrate_mode, reverse, media::test::FrameOutputConfig(),
+          codec, svc_mode, output_bitstream, encode_bitrate, bitrate_mode,
+          reverse, media::test::FrameOutputConfig(),
           /*enabled_features=*/{}, disabled_features);
   if (!test_environment)
     return EXIT_FAILURE;
