dd9f5bf8f553c1526f876e84d93078f7bcaea8cb
Liam Brady <lbrady@google.com>
2023-05-18 19:07:24 +0000

    Enrollment: Allow devtools to add per-site overrides.
    This CL allows using the devtools procotol to allow a site to use
    privacy sandbox features, even if the site is not officially enrolled.
    
    Developers can trigger this by calling
    `SystemInfo.addPrivacySandboxEnrollmentOverride. The list is stored in
    the `PrivacySandboxAttestations` class, and will cause the
    `IsSiteAttested()` to always return true for any sites added to the
    override list.
    
    See design doc: https://docs.google.com/document/d/1dzj2nImlqJnxKDvcO6AUU8r7BzfuJGBjF_4VCKQn50I/edit?resourcekey=0-UiSJxD7BfO7xhb9DlAYohw#heading=h.ebofd1me8h28
    Note that we are skipping adding a chrome://flag and instead just going
    the devtools route.
    
    Change-Id: If79c7c922c3b92850b7840206b1f105768e18817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4534426
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146087}

--- chrome/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ chrome/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/unpacked_installer.h"
 #include "chrome/browser/prefetch/prefetch_prefs.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_paths.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_delegate.h"
+#include "components/privacy_sandbox/privacy_sandbox_settings.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/ssl_status.h"
 #include "content/public/browser/web_contents.h"
@@ -917,6 +919,42 @@ IN_PROC_BROWSER_TEST_F(PrerenderDataSaverProtocolTest,
   histogram_tester.ExpectUniqueSample(
       "Prerender.Experimental.PrerenderHostFinalStatus.SpeculationRule",
       /*PrerenderFinalStatus::kDataSaverEnabled=*/38, 1);
+}
+
+IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, PrivacySandboxEnrollmentOverride) {
+  Attach();
+
+  base::Value::Dict paramsDIPS;
+  const std::string attestation_url = "https://google.com";
+  paramsDIPS.Set("url", attestation_url);
+  const net::SchemefulSite expected_site =
+      net::SchemefulSite(GURL(attestation_url));
+
+  SendCommand("Browser.addPrivacySandboxEnrollmentOverride",
+              std::move(paramsDIPS));
+  privacy_sandbox::PrivacySandboxSettings* settings =
+      PrivacySandboxSettingsFactory::GetForProfile(browser()->profile());
+
+  EXPECT_THAT(settings->GetAttestationOverridesForTesting(),
+              testing::ElementsAre(expected_site));
+}
+
+IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest,
+                       PrivacySandboxEnrollmentOverrideInvalidUrl) {
+  Attach();
+
+  base::Value::Dict paramsDIPS;
+  const std::string attestation_url = "this is a bad url";
+  paramsDIPS.Set("url", attestation_url);
+
+  SendCommand("Browser.addPrivacySandboxEnrollmentOverride",
+              std::move(paramsDIPS));
+  privacy_sandbox::PrivacySandboxSettings* settings =
+      PrivacySandboxSettingsFactory::GetForProfile(browser()->profile());
+
+  EXPECT_TRUE(error());
+  EXPECT_THAT(settings->GetAttestationOverridesForTesting(),
+              testing::IsEmpty());
 }
 
 }  // namespace
--- components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.cc
+++ components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.h"
+#include "base/containers/contains.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 
 namespace privacy_sandbox {
@@ -19,6 +20,11 @@ bool PrivacySandboxAttestations::IsSiteAttested(
   // If attestations aren't enabled, pass the check trivially.
   if (!base::FeatureList::IsEnabled(
           privacy_sandbox::kEnforcePrivacySandboxAttestations)) {
+    return true;
+  }
+
+  // Pass the check if the site is in the list of devtools overrides.
+  if (base::Contains(overrides_, site)) {
     return true;
   }
 
