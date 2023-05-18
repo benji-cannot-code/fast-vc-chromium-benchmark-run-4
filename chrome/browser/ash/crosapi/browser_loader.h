00cac0381ae8455c18b266db3485a317184b1448
Eriko Kurimoto <elkurin@chromium.org>
2023-05-18 11:55:17 +0000

    BrowserLoader: Call GetVersion for Rootfs and Stateful in parallel
    This CL calls GetVersion in parallel instead of calling them in sequence
    to make it readable and slightly improve the performance.
    
    Bug: 1429138
    Change-Id: I69ee2717bedac825fa354fca704fcc298e5cea25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4530260
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145892}

--- chrome/browser/ash/crosapi/browser_loader.h
+++ chrome/browser/ash/crosapi/browser_loader.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_CROSAPI_BROWSER_LOADER_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
@@ -15,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
+#include "base/version.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace component_updater {
 class CrOSComponentManager;
@@ -50,6 +53,16 @@ class BrowserLoader {
   // If this returns false subsequent loads of lacros-chrome will never load
   // a newer lacros-chrome version and update checking can be skipped.
   static bool WillLoadStatefulComponentBuilds();
+
+  struct LacrosSelectionVersion {
+    LacrosSelectionVersion(LacrosSelection selection, base::Version version);
+
+    // LacrosSelection::kRootfs or kStateful should be set here, not
+    // kDeployedLocally.
+    LacrosSelection selection;
+
+    base::Version version;
+  };
 
   // Starts to load lacros-chrome binary or the rootfs lacros-chrome binary.
   // |callback| is called on completion with the path to the lacros-chrome on
@@ -93,17 +106,18 @@ class BrowserLoader {
                           bool load_stateful_lacros);
   void SelectStatefulLacros(LoadCompletionCallback callback);
 
-  // Called when stateful lacros version is calculated.
-  // TODO(crbug.com/1429138): Make it pararell to load stateful and rootfs
-  // lacros.
-  void OnLoadStatefulLacros(LoadCompletionCallback callback,
-                            base::Version stateful_lacros_version);
+  // Called on GetVersion for each rootfs and stateful lacros loader.
+  void OnGetVersion(
+      LacrosSelection selection,
+      base::OnceCallback<void(LacrosSelectionVersion)> barrier_callback,
+      const base::Version& version);
 
   // Called to determine which lacros to load based on version (rootfs vs
   // stateful).
-  void OnLoadVersionSelection(LoadCompletionCallback callback,
-                              base::Version stateful_lacros_version,
-                              base::Version rootfs_lacros_version);
+  // `versions` consists of 2 elements, one for rootfs lacros, one for stateful
+  // lacros. The order is not specified.
+  void OnLoadVersions(LoadCompletionCallback callback,
+                      std::vector<LacrosSelectionVersion> versions);
 
   // Called on the completion of loading.
   void OnLoadComplete(LoadCompletionCallback callback,
