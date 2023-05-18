f05b78230d38e7cabd73cf03770a291e2de8e7de
Jan Lanik <janlanik@google.com>
2023-05-18 20:14:06 +0000

    [Privacy Hub] Throttle geolocation notifications
    Introducing throttling logic to Privacy Hub notifications.
    In particular the geolocation notification will not be displayed in case
    it was dismissed during the last hour or three times during the last 24
    hours.
    A known limitation is that the throttling state is not persisted between sessions - this will be addressed in b/283046754.
    
    BUG=b:282742896
    
    Change-Id: I4b58ee6d3af8ef973b7a3deef841ce06bb34dd1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4533618
    Commit-Queue: Jan Láník <janlanik@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146125}

--- ash/system/privacy_hub/privacy_hub_notification.h
+++ ash/system/privacy_hub/privacy_hub_notification.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/sensor_disabled_notification_delegate.h"
 #include "ash/public/cpp/system_notification_builder.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/message_center/message_center_observer.h"
 #include "ui/message_center/public/cpp/notification_delegate.h"
 
 namespace ash {
@@ -94,8 +95,24 @@ class ASH_EXPORT PrivacyHubNotificationDescriptor {
 
 // This class wraps `SystemNotificationBuilder` and adds additional constraints
 // and shared behavior that applies to all Privacy Hub notifications.
-class ASH_EXPORT PrivacyHubNotification {
+class ASH_EXPORT PrivacyHubNotification
+    : public message_center::MessageCenterObserver {
  public:
+  // Class used to encapsulate the logic whether a notification should be
+  // throttled (suppressed because it has been manually dismissed recently);
+  class Throttler {
+   public:
+    Throttler() = default;
+    Throttler(const Throttler&) = delete;
+    Throttler& operator=(const Throttler&) = delete;
+    virtual ~Throttler() = default;
+
+    // Returns `true` if the notification should be suppressed.
+    virtual bool ShouldThrottle() = 0;
+    // To be called when a notification is dismissed by the user.
+    virtual void RecordDismissalByUser() = 0;
+  };
+
   // Create a new notification.
   // When calling `Show() or `Update()`:
   // If `sensors_` is empty, the generic notification message from `descriptor`
@@ -123,7 +140,11 @@ class ASH_EXPORT PrivacyHubNotification {
   PrivacyHubNotification(PrivacyHubNotification&&) = delete;
   PrivacyHubNotification& operator=(PrivacyHubNotification&&) = delete;
 
-  ~PrivacyHubNotification();
+  ~PrivacyHubNotification() override;
+
+  // message_center::MessageCenterObserver:
+  void OnNotificationRemoved(const std::string& notification_id,
+                             bool by_user) override;
 
   // Show the notification to the user for at least `kMinShowTime`. Every time
   // `Show()` is called, the notification will pop up. For silent updates, use
@@ -153,7 +174,16 @@ class ASH_EXPORT PrivacyHubNotification {
   // `Show()` and `Hide()` are not going to work reliably.
   SystemNotificationBuilder& builder() { return builder_; }
 
+  // Sets a custom Throttler - the object that decides whether to suppress a
+  // notification due too too many repetitions.
+  void SetThrottler(std::unique_ptr<Throttler> throttler);
+
  private:
+  // Starts observation of dismissed messages
+  void StartDismissalObservation();
+  // Stops observation of dismissed messages
+  void StopDismissalObservation();
+
   // Get names of apps accessing sensors in `sensors_`. At most `number_of_apps`
   // elements will be returned.
   std::vector<std::u16string> GetAppsAccessingSensors(
@@ -193,6 +223,9 @@ class ASH_EXPORT PrivacyHubNotification {
   // determine if the notification is for the camera hardware switch to handle
   // it specially.
   NotificationCatalogName catalog_name_;
+
+  // Encapsulates the throttling logic for this notification.
+  std::unique_ptr<Throttler> throttler_;
 };
 
 }  // namespace ash
--- ash/system/privacy_hub/privacy_hub_notification_unittest.cc
+++ ash/system/privacy_hub/privacy_hub_notification_unittest.cc
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/privacy_hub/privacy_hub_notification.h"
 
+#include <memory>
+
 #include "ash/public/cpp/sensor_disabled_notification_delegate.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/test/ash_test_base.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/test/bind.h"
 #include "base/test/gtest_util.h"
+#include "privacy_hub_notification.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/message_center.h"
@@ -85,28 +88,40 @@ message_center::Notification* GetPopupNotification() {
 class PrivacyHubNotificationTest : public AshTestBase {
  public:
   PrivacyHubNotificationTest()
-      : AshTestBase(base::test::TaskEnvironment::TimeSource::MOCK_TIME),
-        notification_(
-            kNotificationId,
-            ash::NotificationCatalogName::kTestCatalogName,
-            PrivacyHubNotificationDescriptor{
-                SensorDisabledNotificationDelegate::SensorSet{
-                    SensorDisabledNotificationDelegate::Sensor::kMicrophone},
-                IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_TITLE,
-                std::vector<int>{
-                    IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_BUTTON},
-                std::vector<int>{
-                    IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_MESSAGE,
-                    IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_MESSAGE_WITH_ONE_APP_NAME,
-                    IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_MESSAGE_WITH_TWO_APP_NAMES},
-                base::MakeRefCounted<PrivacyHubNotificationClickDelegate>(
-                    base::DoNothing())}) {}
+      : AshTestBase(base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
   ~PrivacyHubNotificationTest() override = default;
 
-  PrivacyHubNotification& notification() { return notification_; }
+  PrivacyHubNotification& notification() { return *notification_; }
 
   FakeSensorDisabledNotificationDelegate& sensor_delegate() {
     return sensor_delegate_;
+  }
+
+  // testing::Test
+  void SetUp() override {
+    AshTestBase::SetUp();
+    // We need initialize the notification after `AshTestBase::SetUp` has been
+    // called, as the constructor depends on the message center, which is not
+    // available earlier.
+    notification_ = std::make_unique<PrivacyHubNotification>(
+        kNotificationId, ash::NotificationCatalogName::kTestCatalogName,
+        PrivacyHubNotificationDescriptor{
+            SensorDisabledNotificationDelegate::SensorSet{
+                SensorDisabledNotificationDelegate::Sensor::kMicrophone},
+            IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_TITLE,
+            std::vector<int>{
+                IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_BUTTON},
+            std::vector<int>{
+                IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_MESSAGE,
+                IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_MESSAGE_WITH_ONE_APP_NAME,
+                IDS_PRIVACY_HUB_MICROPHONE_AND_CAMERA_OFF_NOTIFICATION_MESSAGE_WITH_TWO_APP_NAMES},
+            base::MakeRefCounted<PrivacyHubNotificationClickDelegate>(
+                base::DoNothing())});
+  }
+  // testing::Test
+  void TearDown() override {
+    notification_.reset();
+    AshTestBase::TearDown();
   }
 
   void WaitUntilPopupCloses() {
@@ -116,7 +131,7 @@ class PrivacyHubNotificationTest : public AshTestBase {
 
  private:
   FakeSensorDisabledNotificationDelegate sensor_delegate_;
-  PrivacyHubNotification notification_;
+  std::unique_ptr<PrivacyHubNotification> notification_;
 };
 
 using PrivacyHubNotificationClickDelegateTest = AshTestBase;
