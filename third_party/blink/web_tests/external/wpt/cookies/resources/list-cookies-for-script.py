1ab69f52e709e2a330265c25510b22cc0985b244
Dylan Cutler <dylancutler@google.com>
2023-05-18 21:31:34 +0000

    Add WPT for cookies/service worker partitioning in importScripts
    Bug: 1427879
    Change-Id: I20d5a74daee8f59d537472cb7836c7e576b24b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543752
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146188}

---
+++ third_party/blink/web_tests/external/wpt/cookies/resources/list-cookies-for-script.py
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import json
+from cookies.resources import helpers
+
+from wptserve.utils import isomorphic_decode
+
+def main(request, response):
+    headers = helpers.setNoCacheAndCORSHeaders(request, response)
+    headers[0] = (b"Content-Type", b"text/javascript")
+    cookies = helpers.readCookies(request)
+    decoded_cookies = {isomorphic_decode(key): isomorphic_decode(val) for key, val in cookies.items()}
+    return headers, 'self._cookies = [{}];\n'.format(
+        ', '.join(['"{}"'.format(name) for name in decoded_cookies.keys()]))
