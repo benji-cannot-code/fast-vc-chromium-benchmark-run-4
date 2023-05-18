70520d651c41d7022f9df6f4349ca6bdfc4645c0
Ashley Prasad <ashleydp@google.com>
2023-05-18 16:27:44 +0000

    print-management: add flag for setup assistance
    Add flag for changes to print management to direct users to Printer
    settings page to configure printers.
    
    Bug: b:283119650
    Test:  unit_tests --gtest_filter=*AboutFlagsHistogramTest.*:*AboutFlagsTest.*
    
    Change-Id: Ia08efb9779a2fd80efa565d4a4f88453e5cb74ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4540962
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145974}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3590,6 +3590,9 @@ extern const char kPrintingPpdChannelDescription[];
 extern const char kPrintManagementJellyName[];
 extern const char kPrintManagementJellyDescription[];
 
+extern const char kPrintManagementSetupAssistanceName[];
+extern const char kPrintManagementSetupAssistanceDescription[];
+
 extern const char kProductivityLauncherName[];
 extern const char kProductivityLauncherDescription[];
 
