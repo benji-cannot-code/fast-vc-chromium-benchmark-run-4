01fe247f1eb82536e94e4aeba03cb33944f2e3fa
James Decker <deckerjames@google.com>
2023-05-18 16:13:04 +0000

    Enabling puffin patches
    Bug: 1320481
    Change-Id: I96eb3a0cc92f85b85ac59469ccd1ae4aaf80bfdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547483
    Commit-Queue: James Decker <deckerjames@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145965}

--- components/update_client/features.cc
+++ components/update_client/features.cc
@@ -8,7 +8,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 
 namespace update_client::features {
-BASE_FEATURE(kPuffinPatches,
-             "PuffinPatches",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPuffinPatches, "PuffinPatches", base::FEATURE_ENABLED_BY_DEFAULT);
 }
