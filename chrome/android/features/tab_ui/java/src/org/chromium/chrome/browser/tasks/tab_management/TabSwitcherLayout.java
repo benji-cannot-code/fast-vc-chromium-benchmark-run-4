f0a484ef6275c0ad94f48eccb3d8abff19224b29
Calder Kitagawa <ckitagawa@chromium.org>
2023-05-18 15:06:41 +0000

    [Tab Switcher] A small handful of improvements for framerate
    Some small optimizations that affect every frame of the Tab Switcher
    shrink and expand animations.
    
    A lot of these checks are done every frame or for every tab which is
    wasteful if they are somewhat expensive or can be cached. flat_map and
    flat_set are sufficiently performant for very small cases like this as well.
    
    Bug: https://crbug.com/1444782
    Change-Id: I2cdd0c493caee87feee2fd04bde8fac16fac8954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517775
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1145947}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayout.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayout.java
@@ -99,6 +99,9 @@ public class TabSwitcherLayout extends Layout {
     private final LayoutTab mDummyLayoutTab;
     private boolean mIsInitialized;
 
+    // Only access this value via isTabGtsAnimationEnabled. Caches the value to avoid repeated
+    // calculations during animations.
+    private Boolean mCachedIsTabGtsAnimationEnabled;
     private float mBackgroundAlpha;
     private int mTabListTopOffset;
 
@@ -150,7 +153,7 @@ public class TabSwitcherLayout extends Layout {
                 // causing janky frames. When animation is off or not used, the thumbnail is already
                 // updated when showing the GTS. Tab-to-GTS animation is not invoked for tablet tab
                 // switcher polish.
-                if (isTabGtsAnimationEnabled()) {
+                if (isTabGtsAnimationEnabled(false)) {
                     // Delay thumbnail taking a bit more to make it less likely to happen before the
                     // thumbnail taking triggered by ThumbnailFetcher. See crbug.com/996385 for
                     // details.
@@ -175,7 +178,7 @@ public class TabSwitcherLayout extends Layout {
                 // The Android View version of GTS overview is hidden.
                 // If not doing GTS-to-Tab transition animation, we show the fade-out instead, which
                 // was already done.
-                if (!isTabGtsAnimationEnabled()) {
+                if (!isTabGtsAnimationEnabled(false)) {
                     postHiding();
                     return;
                 }
@@ -334,7 +337,7 @@ public class TabSwitcherLayout extends Layout {
             if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(getContext())) {
                 translateDown();
             } else {
-                mController.hideTabSwitcherView(!isTabGtsAnimationEnabled());
+                mController.hideTabSwitcherView(!isTabGtsAnimationEnabled(true));
             }
         }
     }
@@ -409,7 +412,7 @@ public class TabSwitcherLayout extends Layout {
         // Skip shrinking animation when there is no tab in current tab model.
         boolean isCurrentTabModelEmpty = mTabModelSelector.getCurrentModel().getCount() == 0;
         boolean showShrinkingAnimation =
-                animate && isTabGtsAnimationEnabled() && !isCurrentTabModelEmpty;
+                animate && isTabGtsAnimationEnabled(true) && !isCurrentTabModelEmpty;
 
         boolean skipSlowZooming = TabUiFeatureUtilities.SKIP_SLOW_ZOOMING.getValue();
         Log.d(TAG, "SkipSlowZooming = " + skipSlowZooming);
@@ -695,7 +698,7 @@ public class TabSwitcherLayout extends Layout {
         // The content viewport is intentionally sent as both params below.
         mSceneLayer.pushLayers(getContext(), contentViewport, contentViewport, this,
                 tabContentManager, resourceManager, browserControls,
-                isTabGtsAnimationEnabled() ? mGridTabListDelegate.getResourceId() : 0,
+                isTabGtsAnimationEnabled(false) ? mGridTabListDelegate.getResourceId() : 0,
                 mBackgroundAlpha, mTabListTopOffset);
         mFrameCount++;
         if (mLastFrameTime != 0) {
@@ -740,8 +743,15 @@ public class TabSwitcherLayout extends Layout {
      * Shrink/Expand animation is disabled for Tablet TabSwitcher launch polish.
      * @return Whether shrink/expand animation is enabled.
      */
-    private boolean isTabGtsAnimationEnabled() {
-        if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(getContext())) return false;
-        return TabUiFeatureUtilities.isTabToGtsAnimationEnabled(getContext());
+    private boolean isTabGtsAnimationEnabled(boolean updateCachedValue) {
+        if (updateCachedValue || mCachedIsTabGtsAnimationEnabled == null) {
+            if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(getContext())) {
+                mCachedIsTabGtsAnimationEnabled = false;
+            } else {
+                mCachedIsTabGtsAnimationEnabled =
+                        TabUiFeatureUtilities.isTabToGtsAnimationEnabled(getContext());
+            }
+        }
+        return mCachedIsTabGtsAnimationEnabled;
     }
 }
