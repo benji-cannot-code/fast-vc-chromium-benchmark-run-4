7d5f4f8a1285bdff0572b0709509a453bb20511d
Mustaq Ahmed <mustaq@google.com>
2023-05-18 17:40:47 +0000

    [Interop 2023] Make PE/ME enter/leave events non-composed
    Chrome always had mouse/pointer enter/leave events as
    "composed", which was matching the specs in the past.
    However, the specs were updated in the last few years to
    make these events non-composed:
      https://github.com/w3c/uievents/pull/210
      https://github.com/w3c/pointerevents/pull/461
    Firefox was update too:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1484371
    
    This CL makes Chrome spec compliant.
    
    Fixed: 876994, 874082, 1136584
    Change-Id: I861b15cd71bc9c06e28d5d325f8b72c72d7cde16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4536900
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146033}

--- third_party/blink/renderer/core/events/pointer_event_factory.cc
+++ third_party/blink/renderer/core/events/pointer_event_factory.cc
@@ -274,8 +274,9 @@ PointerEventInit* PointerEventFactory::ConvertIdTypeButtonsEvent(
 void PointerEventFactory::SetEventSpecificFields(
     PointerEventInit* pointer_event_init,
     const AtomicString& type) {
-  pointer_event_init->setBubbles(type != event_type_names::kPointerenter &&
-                                 type != event_type_names::kPointerleave);
+  bool is_pointer_enter_or_leave = type == event_type_names::kPointerenter ||
+                                   type == event_type_names::kPointerleave;
+  pointer_event_init->setBubbles(!is_pointer_enter_or_leave);
   pointer_event_init->setCancelable(
       type != event_type_names::kPointerenter &&
       type != event_type_names::kPointerleave &&
@@ -283,8 +284,10 @@ void PointerEventFactory::SetEventSpecificFields(
       type != event_type_names::kPointerrawupdate &&
       type != event_type_names::kGotpointercapture &&
       type != event_type_names::kLostpointercapture);
-
-  pointer_event_init->setComposed(true);
+  pointer_event_init->setComposed(
+      RuntimeEnabledFeatures::NonComposedEnterLeaveEventsEnabled()
+          ? !is_pointer_enter_or_leave
+          : true);
   pointer_event_init->setDetail(0);
 }
 
