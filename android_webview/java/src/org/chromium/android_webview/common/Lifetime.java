4c871196d0586fcfa18c37b247425fda049809ec
Peter E Conn <peconn@chromium.org>
2023-05-18 14:44:27 +0000

    👪 Add Lifetime annotation and docs.
    Change-Id: Iea0e5059dd5705956789e57954c316f890dea1f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547379
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145932}

---
+++ android_webview/java/src/org/chromium/android_webview/common/Lifetime.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.common;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotations specifying the lifetimes of classes in embedded WebView code.
+ *
+ * See: //android_webview/docs/lifetime-annotations.md.
+ */
+public class Lifetime {
+    private Lifetime() {}
+
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Singleton {}
+
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Profile {}
+
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface WebView {}
+
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Temporary {}
+}
