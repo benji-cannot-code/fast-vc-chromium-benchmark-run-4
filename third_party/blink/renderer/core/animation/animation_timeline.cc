8453ea280c379f728726a58ed846781dbccf8eb4
Anders Hartvoll Ruud <andruud@chromium.org>
2023-05-18 17:47:42 +0000

    [scroll-animations] Introduce ScrollSnapshotTimeline
    This CL splits out parts of ScrollTimeline into a new class
    ScrollSnapshotTimeline. A ScrollSnapshotTimeline has a pure virtual
    ComputeTimelineSnapshot function, and the idea is that different
    types of ScrollSnapshotTimelines can get their state from various
    sources. In particular, this will be useful for implementing
    Deferred Timelines v2, where we need a new "DeferredTimeline" that
    grabs its state from another timeline.
    
    There should be no behavior change.
    
    Bug: 1425939
    Change-Id: Ia71a338bec0e8ec31c4870b53c430b63a6f23aae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547637
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146039}

--- third_party/blink/renderer/core/animation/animation_timeline.cc
+++ third_party/blink/renderer/core/animation/animation_timeline.cc
@@ -83,7 +83,7 @@ wtf_size_t AnimationTimeline::AnimationsNeedingUpdateCount() const {
   for (const auto& animation : animations_needing_update_) {
     // Exclude animations which are not actively generating frames.
     if ((!animation->CompositorPending() && !animation->Playing() &&
-         !IsScrollTimeline()) ||
+         !IsProgressBased()) ||
         animation->AnimationHasNoEffect()) {
       continue;
     }
@@ -111,7 +111,7 @@ void AnimationTimeline::ServiceAnimations(TimingUpdateReason reason) {
 
   auto current_phase_and_time = CurrentPhaseAndTime();
 
-  if (IsScrollTimeline() &&
+  if (IsProgressBased() &&
       last_current_phase_and_time_ != current_phase_and_time) {
     UpdateCompositorTimeline();
   }
--- third_party/blink/renderer/core/animation/document_animations.cc
+++ third_party/blink/renderer/core/animation/document_animations.cc
@@ -135,9 +135,11 @@ void DocumentAnimations::UpdateAnimations(
   document_->GetWorkletAnimationController().UpdateAnimationStates();
   document_->GetFrame()->ScheduleNextServiceForScrollSnapshotClients();
   for (auto& timeline : timelines_) {
-    // ScrollTimelines are already handled as ScrollSnapshotClients above.
-    if (!timeline->IsScrollTimeline())
+    // ScrollSnapshotTimelines are already handled as ScrollSnapshotClients
+    // above.
+    if (!timeline->IsScrollSnapshotTimeline()) {
       timeline->ScheduleNextService();
+    }
   }
 }
 
