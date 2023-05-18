c5314f5d03047ff945d46b1c3162ad5566e80f60
CJ Huang <chenjih@google.com>
2023-05-18 00:48:56 +0000

    [SPEAK-ON-MUTE] Add response to "settings" button and toast on-click
    Add a response to the VC tray, which opens the privacy hub page with the
    mute nudge focused. The added response is not actually used until the
    setting button or the toast on-click action is implemented.
    
    Bug: b/280925635
    TEST: /ash_unittests --gtest_filter='*VideoConferenceTrayTest*'
    Change-Id: I44849a06840f9837a58a688a5bdf0f6094efb438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4529159
    Commit-Queue: CJ Huang <chenjih@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145740}

--- chrome/browser/ui/ash/system_tray_client_impl.cc
+++ chrome/browser/ui/ash/system_tray_client_impl.cc
@@ -449,6 +449,14 @@ void SystemTrayClientImpl::ShowPrivacyHubSettings() {
       chromeos::settings::mojom::kPrivacyHubSubpagePath);
 }
 
+void SystemTrayClientImpl::ShowSpeakOnMuteDetectionSettings() {
+  ShowSettingsSubPageForActiveUser(
+      std::string(chromeos::settings::mojom::kPrivacyHubSubpagePath) +
+      "?settingId=" +
+      base::NumberToString(static_cast<int32_t>(
+          chromeos::settings::mojom::Setting::kSpeakOnMuteDetectionOnOff)));
+}
+
 void SystemTrayClientImpl::ShowSmartPrivacySettings() {
   ShowSettingsSubPageForActiveUser(
       chromeos::settings::mojom::kSmartPrivacySubpagePath);
--- chrome/browser/ui/ash/system_tray_client_impl.h
+++ chrome/browser/ui/ash/system_tray_client_impl.h
@@ -73,6 +73,7 @@ class SystemTrayClientImpl : public ash::SystemTrayClient,
   void ShowPowerSettings() override;
   void ShowPrivacyAndSecuritySettings() override;
   void ShowPrivacyHubSettings() override;
+  void ShowSpeakOnMuteDetectionSettings() override;
   void ShowSmartPrivacySettings() override;
   void ShowChromeSlow() override;
   void ShowIMESettings() override;
