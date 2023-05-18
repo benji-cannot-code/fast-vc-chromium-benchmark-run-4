8ca902b214628edc1145235719e2f66541e76029
Ramin Halavati <rhalavati@chromium.org>
2023-05-18 13:22:47 +0000

    Revert "Run mouse callbacks for emulated touch events"
    This reverts commit 185201f9ba9351055c87d5da1c4dee0b281538ac.
    
    Reason for revert: Added test is failing on many builders, e.g.:
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2F%3Ablink_web_tests%2Finspector-protocol%2Finput%2FdispatchMouseEvent-emulation.js?q=VHASH%3A97e272e92adc1980
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2F%3Ablink_web_tests%2Finspector-protocol%2Finput%2FdispatchMouseEvent-emulation.js?q=VHASH%3A4571b7f9614f3aab
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2F%3Ablink_web_tests%2Finspector-protocol%2Finput%2FdispatchMouseEvent-emulation.js?q=VHASH%3A329768773e20e824
    
    Original change's description:
    > Run mouse callbacks for emulated touch events
    >
    > This CL fixes an issue where mouse callbacks do not get triggered when touch emulation from mouse is enabled.
    >
    > Fixed: 1415994
    > Change-Id: Ifb66c74182cdddad12b4f88c34f6e5c3b51c212b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4534375
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Randolf Jung <jrandolf@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1145297}
    
    Change-Id: I854446e09941981d8f29116abdbca16a491d7cbd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547380
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Owners-Override: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145903}

--- content/browser/renderer_host/input/touch_emulator.h
+++ content/browser/renderer_host/input/touch_emulator.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/queue.h"
 #include "base/functional/callback.h"
-#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
 #include "content/browser/renderer_host/input/touch_emulator_client.h"
@@ -68,8 +67,7 @@ class CONTENT_EXPORT TouchEmulator : public ui::GestureProviderClient {
   // propagate any further.
   // TODO(dgozman): maybe pass latency info together with events.
   bool HandleMouseEvent(const blink::WebMouseEvent& event,
-                        RenderWidgetHostViewBase* target_view,
-                        base::OnceClosure callback);
+                        RenderWidgetHostViewBase* target_view);
   bool HandleMouseWheelEvent(const blink::WebMouseWheelEvent& event);
   bool HandleKeyboardEvent(const blink::WebKeyboardEvent& event);
   bool HandleTouchEvent(const blink::WebTouchEvent& event);
@@ -135,7 +133,7 @@ class CONTENT_EXPORT TouchEmulator : public ui::GestureProviderClient {
                                 RenderWidgetHostViewBase* target_view);
 
   // Called when ack for injected touch has been received.
-  void OnTouchCompleted();
+  void OnInjectedTouchCompleted();
 
   const raw_ptr<TouchEmulatorClient> client_;
 
@@ -183,7 +181,7 @@ class CONTENT_EXPORT TouchEmulator : public ui::GestureProviderClient {
   float pinch_scale_;
   bool pinch_gesture_active_;
 
-  base::queue<base::OnceClosure> touch_event_completion_callbacks_;
+  base::queue<base::OnceClosure> injected_touch_completion_callbacks_;
 };
 
 }  // namespace content
