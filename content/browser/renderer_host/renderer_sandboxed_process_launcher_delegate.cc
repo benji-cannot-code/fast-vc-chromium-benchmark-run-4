544096f23b06754d4ae4e62c7ec937c84939e64b
Will Harris <wfh@chromium.org>
2023-05-18 17:20:12 +0000

    Remove renderer environment filtering feature and enable by default.
    Also, add some tests, as these were missing from the previous CL.
    
    BUG=1316641,1403087
    
    Change-Id: Iab5803962504bcd53b6474c1730258f60eb4068b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545784
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146014}

--- content/browser/renderer_host/renderer_sandboxed_process_launcher_delegate.cc
+++ content/browser/renderer_host/renderer_sandboxed_process_launcher_delegate.cc
@@ -140,8 +140,7 @@ bool RendererSandboxedProcessLauncherDelegateWin::InitializeConfig(
     }
   }
 
-  config->SetFilterEnvironment(base::FeatureList::IsEnabled(
-      sandbox::policy::features::kRendererFilterEnvironment));
+  config->SetFilterEnvironment(/*filter=*/true);
 
   ContentBrowserClient::ChildSpawnFlags flags(
       ContentBrowserClient::ChildSpawnFlags::kChildSpawnFlagNone);
--- sandbox/win/src/policy_target_test.cc
+++ sandbox/win/src/policy_target_test.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <ntstatus.h>
 
+#include "base/environment.h"
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/writable_shared_memory_region.h"
+#include "base/scoped_environment_variable_override.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
@@ -172,6 +174,20 @@ SBOX_TESTS_COMMAND int PolicyTargetTest_process(int argc, wchar_t** argv) {
     return SBOX_TEST_SUCCEEDED;
   base::win::ScopedProcessInformation process_info(temp_process_info);
   return SBOX_TEST_FAILED;
+}
+
+// Tests that environment is filtered correctly.
+SBOX_TESTS_COMMAND int PolicyTargetTest_filterEnvironment(int argc,
+                                                          wchar_t** argv) {
+  auto env = base::Environment::Create();
+  // "TMP" should never be filtered. See `TargetProcess::Create`.
+  if (!env->HasVar("TMP")) {
+    return SBOX_TEST_FIRST_ERROR;
+  }
+  if (env->HasVar("SBOX_TEST_ENV")) {
+    return SBOX_TEST_SECOND_ERROR;
+  }
+  return SBOX_TEST_SUCCEEDED;
 }
 
 TEST(PolicyTargetTest, SetInformationThread) {
@@ -557,6 +573,23 @@ TEST(SharedTargetConfig, BrokerConfigManagement) {
   // But should be the second time.
   policy_two = broker->CreatePolicy("key-two");
   EXPECT_TRUE(policy_two->GetConfig()->IsConfigured());
+}
+
+// Test that environment for a sandboxed process is filtered correctly.
+TEST(PolicyTargetTest, FilterEnvironment) {
+  base::ScopedEnvironmentVariableOverride scoped_env("SBOX_TEST_ENV", "FOO");
+  {
+    TestRunner runner;
+    runner.GetPolicy()->GetConfig()->SetFilterEnvironment(/*filter=*/true);
+    EXPECT_EQ(SBOX_TEST_SUCCEEDED,
+              runner.RunTest(L"PolicyTargetTest_filterEnvironment"));
+  }
+  {
+    TestRunner runner;
+    runner.GetPolicy()->GetConfig()->SetFilterEnvironment(/*filter=*/false);
+    EXPECT_EQ(SBOX_TEST_SECOND_ERROR,
+              runner.RunTest(L"PolicyTargetTest_filterEnvironment"));
+  }
 }
 
 }  // namespace sandbox
