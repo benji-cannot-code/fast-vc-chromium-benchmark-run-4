26cd5567ca839c6a2b971b011d0d50dc2a11539b
Dan Harrington <harringtond@chromium.org>
2023-05-18 21:42:19 +0000

    enable ON_DEVICE_VIEW_HISTORY behind flag
    Bug: 279196406
    Change-Id: I4927a38abcdae412f663ab5644c649734f2cf832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547279
    Commit-Queue: Jonathan Freed <freedjm@google.com>
    Reviewed-by: Jonathan Freed <freedjm@google.com>
    Auto-Submit: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146199}

--- components/feed/core/v2/proto_util.cc
+++ components/feed/core/v2/proto_util.cc
@@ -174,6 +174,10 @@ feedwire::Request CreateFeedQueryRequest(
     feed_request.add_client_capability(Capability::ON_DEVICE_USER_PROFILE);
   }
 
+  if (base::FeatureList::IsEnabled(kFeedSignedOutViewDemotion)) {
+    feed_request.add_client_capability(Capability::ON_DEVICE_VIEW_HISTORY);
+  }
+
   if (base::FeatureList::IsEnabled(kInfoCardAcknowledgementTracking)) {
     feed_request.add_client_capability(
         Capability::INFO_CARD_ACKNOWLEDGEMENT_TRACKING);
