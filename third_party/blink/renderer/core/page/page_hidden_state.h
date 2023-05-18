8305cfae6b35eb8990f5ebb35d3d3a7de9ad11c4
Mason Freed <masonf@chromium.org>
2023-05-18 00:02:06 +0000

    Inline the PageHiddenStateString() function
    See [1] for the first part of this cleanup. This just inlines
    the function for better perf and one less file.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4517962
    
    Bug: 1444094
    Change-Id: I6b4fe8edcdcd2d455ae7fb02eec3ef7f948556fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546061
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145715}

--- third_party/blink/renderer/core/page/page_hidden_state.h
+++ third_party/blink/renderer/core/page/page_hidden_state.h
@@ -32,12 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_PAGE_HIDDEN_STATE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_PAGE_HIDDEN_STATE_H_
 
-#include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+#include "third_party/blink/renderer/core/keywords.h"
 
 namespace blink {
 
-CORE_EXPORT AtomicString PageHiddenStateString(bool hidden);
+ALWAYS_INLINE AtomicString PageHiddenStateString(bool hidden) {
+  return hidden ? keywords::kHidden : keywords::kVisible;
+}
 
 }  // namespace blink
 
