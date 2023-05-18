8deaedc76c4666c281826308e5b1260e092c172d
Thuong Phan <thuongphan@chromium.org>
2023-05-18 18:43:40 +0000

    personalization: update promo banner for enterprise users
    As enterprise users cannot access screen saver subpage, we need to
    update the promo banner not to mention screen saver for those users.
    https://screenshot.googleplex.com/KkqLrcSnYZ4i47u
    
    BUG=b:279957849
    TEST=Manually
    
    Change-Id: I9aa6d08c97d7aa76a80f7f692689689c2b58b46a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545314
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146069}

--- ash/webui/personalization_app/resources/js/time_of_day_banner_element.ts
+++ ash/webui/personalization_app/resources/js/time_of_day_banner_element.ts
@@ -13,6 +13,7 @@ import '../css/cros_button_style.css.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 
 import {dismissTimeOfDayBanner} from './ambient/ambient_controller.js';
+import {isAmbientModeAllowed} from './load_time_booleans.js';
 import {WithPersonalizationStore} from './personalization_store.js';
 import {getTemplate} from './time_of_day_banner_element.html.js';
 
@@ -27,6 +28,12 @@ export class TimeOfDayBannerElement extends WithPersonalizationStore {
 
   private onDismissClick_() {
     dismissTimeOfDayBanner(this.getStore());
+  }
+
+  private getTimeOfDayBannerDescription_() {
+    return this.i18nAdvanced(
+        isAmbientModeAllowed() ? 'timeOfDayBannerDescription' :
+                                 'timeOfDayBannerDescriptionNoScreensaver');
   }
 }
 
