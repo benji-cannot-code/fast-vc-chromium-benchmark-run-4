15dba3e2a08d5c82bee6c1511ebb4c4acb430987
Mikhail Khokhlov <khokhlov@google.com>
2023-05-18 21:33:38 +0000

    Remove the feature flag for direct memory allocations on Android
    The flag has gone into M114, this should be enough to run the
    experiment in Stable and estimate performance gains.
    
    Now we remove it and instead make Android builds always allocate SMB
    directly. This will allow to start early tracing at any time and unblock
    the migration to Perfetto SDK on Android.
    
    Bug: 1432615, b/277751515
    Change-Id: I8e8e5c9e7be1d022d07c99ae33978b040403bc86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547658
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1146191}

--- content/app/initialize_mojo_core.cc
+++ content/app/initialize_mojo_core.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "mojo/core/embedder/configuration.h"
 #include "mojo/core/embedder/embedder.h"
-#include "mojo/core/embedder/features.h"
 #include "mojo/public/c/system/functions.h"
 #include "mojo/public/c/system/types.h"
 #include "mojo/public/cpp/base/shared_memory_utils.h"
@@ -57,10 +56,6 @@ void InitializeMojoCore() {
     // On Windows it's not necessary to broker shared memory allocation, as
     // even sandboxed processes can allocate their own without trouble.
     config.force_direct_shared_memory_allocation = true;
-#elif BUILDFLAG(IS_ANDROID)
-    // On Android we run a Finch experiment testing direct memory allocation.
-    config.force_direct_shared_memory_allocation = base::FeatureList::IsEnabled(
-        mojo::core::kMojoDirectSharedMemoryAndroid);
 #endif
   }
 
@@ -87,7 +82,8 @@ void InitializeMojoCore() {
   // Note #2: some platforms can directly allocated shared memory in a
   // sandboxed process. The defines below must be in sync with the
   // implementation of mojo::NodeController::CreateSharedBuffer().
-#if !BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_NACL) && !BUILDFLAG(IS_FUCHSIA)
+#if !BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_NACL) && !BUILDFLAG(IS_FUCHSIA) && \
+    !BUILDFLAG(IS_ANDROID)
   if (sandbox::policy::IsUnsandboxedSandboxType(
           sandbox::policy::SandboxTypeFromCommandLine(
               *base::CommandLine::ForCurrentProcess()))) {
