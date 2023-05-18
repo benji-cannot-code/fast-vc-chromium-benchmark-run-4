e5638f277e94db51ee114d0c0e79a2e0c379eae9
Shengsong Tan <sstan@chromium.org>
2023-05-18 02:31:45 +0000

    arc: Enable ARC app connection observer in vmm manager
    In previous CL, the observer initialized but forget added to
    observer list. https://crrev.com/c/4530262
    
    BUG=b:281929987
    TEST=ArcVmmManagerTest
    
    Change-Id: I68503757d825c02fadaeddf2021bb342e5c1187b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542064
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145784}

--- chrome/browser/ash/arc/vmm/arc_vmm_manager.h
+++ chrome/browser/ash/arc/vmm/arc_vmm_manager.h
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/components/arc/mojom/app.mojom.h"
+#include "ash/components/arc/session/connection_holder.h"
 #include "ash/components/arc/session/connection_observer.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ash/arc/vmm/arc_system_state_observation.h"
 #include "chrome/browser/ash/arc/vmm/arc_vmm_swap_scheduler.h"
 #include "chrome/browser/ash/arc/vmm/arcvm_working_set_trim_executor.h"
@@ -110,6 +112,12 @@ class ArcVmmManager : public KeyedService,
       trim_call_;
 
   raw_ptr<content::BrowserContext> context_ = nullptr;
+  raw_ptr<ArcBridgeService> bridge_service_ = nullptr;
+
+  base::ScopedObservation<
+      ConnectionHolder<mojom::AppInstance, mojom::AppHost>,
+      ConnectionHolder<mojom::AppInstance, mojom::AppHost>::Observer>
+      app_instance_observation_{this};
 
   base::WeakPtrFactory<ArcVmmManager> weak_ptr_factory_{this};
 };
