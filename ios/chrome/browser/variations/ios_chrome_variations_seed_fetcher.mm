17f10c3d59a18fa598781fa9cdae35a0c76f3223
ginnnnnnny <ginnyhuang@chromium.org>
2023-05-18 03:25:20 +0000

    [iOS] Change timeout value of seed fetch on first run in iOS & bug fix
    This CL updates the FRE seed fetch timeout value and fixes the following
    bug: when a user leaves the app without completing first run experience
    and launch the app again, variations app agent would still consider the
    session a "first run" and create an experiment group assignment. However
    when the session is logged on UMA, the group assignment would not be
    updated; instead it would still use the group assigned in the actual
    "first run."
    
    This CL fixes the issue so that the group assignment would be updated
    to the one assigned in the latest "first run."
    
    Fixed: 1446430
    Change-Id: If54caffb31131122a1490640fa2f4dea028fbad8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543746
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145799}

--- ios/chrome/browser/variations/ios_chrome_variations_seed_fetcher.mm
+++ ios/chrome/browser/variations/ios_chrome_variations_seed_fetcher.mm
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Maximum time allowed to fetch the seed before the request is cancelled.
-const base::TimeDelta kRequestTimeout = base::Seconds(2);
+const base::TimeDelta kRequestTimeout = base::Seconds(1.5);
 // Histogram names for seed fetch time and result.
 const char kSeedFetchResultHistogram[] =
     "IOS.Variations.FirstRun.SeedFetchResult";
