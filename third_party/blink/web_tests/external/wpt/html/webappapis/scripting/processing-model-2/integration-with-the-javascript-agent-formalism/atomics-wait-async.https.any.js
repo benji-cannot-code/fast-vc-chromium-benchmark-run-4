9f6936d17edd9f0c1ab7f0761f2c3d7f639bed14
Shu-yu Guo <syg@chromium.org>
2023-05-18 20:21:37 +0000

    [atomics] Add WPT for Atomics.waitAsync
    Bug: v8:10239
    Change-Id: Ie478aed6e069d23964331fa07bfe190c55798ab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545947
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146132}

---
+++ third_party/blink/web_tests/external/wpt/html/webappapis/scripting/processing-model-2/integration-with-the-javascript-agent-formalism/atomics-wait-async.https.any.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker
+
+promise_test(async () => {
+  const sab = new SharedArrayBuffer(64);
+  const ta = new Int32Array(sab);
+
+  const waitAsyncObj = Atomics.waitAsync(ta, 0, 0, 10);
+  assert_equals(waitAsyncObj.async, true);
+  const v = await waitAsyncObj.value;
+  assert_equals(v, "timed-out");
+}, `Atomics.waitAsync timeout in a ${self.constructor.name}`);
+
+promise_test(async () => {
+  const sab = new SharedArrayBuffer(64);
+  const ta = new Int32Array(sab);
+
+  const waitAsyncObj = Atomics.waitAsync(ta, 0, 0);
+  assert_equals(waitAsyncObj.async, true);
+
+  const worker = new Worker("resources/notify-worker.js");
+  worker.postMessage(sab);
+
+  const v = await waitAsyncObj.value;
+  assert_equals(v, "ok");
+}, `Atomics.waitAsync notification in a ${self.constructor.name}`);
