dad66357c88c425f2492b5700acb1fee35023003
James Cook <jamescook@chromium.org>
2023-05-18 19:41:00 +0000

    Fix a11y labels / tooltips in quick settings bluetooth detailed view
    The accessibility team says there is too much repetition of spoken
    text when Search-Arrow navigating through the row at the top of the
    view. Ignore all the child views for accessibility use the main
    toggle row for ChromeVox users.
    
    Update the tooltip text for the row, which also fixes the a11y label.
    
    Bug: b:280673293
    Test: updated ash_unittests
    Change-Id: I94cb30044d3436b42118f34a4bd9fc461c57a68a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544390
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146104}

--- ash/system/bluetooth/bluetooth_detailed_view_impl_unittest.cc
+++ ash/system/bluetooth/bluetooth_detailed_view_impl_unittest.cc
@@ -144,6 +144,8 @@ TEST_F(BluetoothDetailedViewImplTest,
   bluetooth_detailed_view_->UpdateBluetoothEnabledState(true);
 
   EXPECT_EQ(u"On", toggle_row->text_label()->GetText());
+  EXPECT_EQ(u"Toggle Bluetooth. Bluetooth is on.",
+            toggle_row->GetTooltipText());
   EXPECT_TRUE(toggle_button->GetIsOn());
   EXPECT_EQ(u"Toggle Bluetooth. Bluetooth is on.",
             toggle_button->GetTooltipText());
@@ -152,6 +154,8 @@ TEST_F(BluetoothDetailedViewImplTest,
   bluetooth_detailed_view_->UpdateBluetoothEnabledState(false);
 
   EXPECT_EQ(u"Off", toggle_row->text_label()->GetText());
+  EXPECT_EQ(u"Toggle Bluetooth. Bluetooth is off.",
+            toggle_row->GetTooltipText());
   EXPECT_FALSE(toggle_button->GetIsOn());
   EXPECT_EQ(u"Toggle Bluetooth. Bluetooth is off.",
             toggle_button->GetTooltipText());
