b3368e89c7bef5095555eaae48f23801aa28667b
Tom Anderson <thomasanderson@chromium.org>
2023-05-18 01:58:59 +0000

    Refactor sysroot-creator.sh
    * Use consistent architecture naming
    * Change usage to sysroot-creator.sh {build,upload} <arch>
    * Merge sysroot-creator-bullseye.sh into sysroot-creator.sh
      since we've only had 1 sysroot for >5 years.
    * Remove some unneeded commands like PrintDistro/PrintRelease
    * Update references from docs and other scripts
    
    Bug: None
    
    Change-Id: I4809de50a03557df533d1f502f15e5acf793c6a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4535715
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145773}

--- build/linux/sysroot_scripts/install-sysroot.py
+++ build/linux/sysroot_scripts/install-sysroot.py
@@ -26,31 +26,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import hashlib
 import json
-import platform
 import optparse
 import os
-import re
 import shutil
 import subprocess
 import sys
-try:
-  # For Python 3.0 and later
-  from urllib.request import urlopen
-except ImportError:
-  # Fall back to Python 2's urllib2
-  from urllib2 import urlopen
+from urllib.request import urlopen
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 URL_PREFIX = 'https://commondatastorage.googleapis.com'
 URL_PATH = 'chrome-linux-sysroot/toolchain'
 
-VALID_ARCHS = ('arm', 'arm64', 'i386', 'amd64', 'mips', 'mips64el')
+VALID_ARCHS = ('amd64', 'i386', 'armhf', 'arm64', 'armel', 'mipsel', 'mips64el')
 
 ARCH_TRANSLATIONS = {
     'x64': 'amd64',
     'x86': 'i386',
-    'mipsel': 'mips',
+    'arm': 'armhf',
+    'mips': 'mipsel',
     'mips64': 'mips64el',
 }
 
