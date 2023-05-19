2f6ac0555ab453c747c25c37bde17bacca7384a3
Pu Shi <pushi@google.com>
2023-05-19 23:29:29 +0000

    [Eche] Fix the issue that streaming success is recorded to wrong bucket
    This change records connection status in AppsLaunchInfoProvider only
    when starting the app streaming and prevent the status being updated
    during an app streaming session. After the change, when app streaming is
    initialized from notification while connection status is failed and
    successfully started, the event will not be incorrectly logged to
    Eche.StreamEvent bucket.
    
    and click notification on chromebook to start app stream, meanwhile
    set both devices on same network. Verified that both initialization
    and start events are logged to
    Eche.StreamEvent.FromNotification.PreviousNetworkCheckFailed.Result
    
    Tested: Set both devices on different network, send message to phone
    Change-Id: I63a2fb4ace0c1ab4a976640313e3fdc7def3c682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548334
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146774}

--- ash/webui/eche_app_ui/apps_launch_info_provider.cc
+++ ash/webui/eche_app_ui/apps_launch_info_provider.cc
@@ -7,28 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/eche_app_ui/mojom/eche_app.mojom-shared.h"
 
-namespace ash {
-namespace eche_app {
+namespace ash::eche_app {
 
 AppsLaunchInfoProvider::AppsLaunchInfoProvider(
     EcheConnectionStatusHandler* connection_handler)
-    : eche_connection_status_handler_(connection_handler) {
-  eche_connection_status_handler_->AddObserver(this);
-}
+    : eche_connection_status_handler_(connection_handler) {}
 
-AppsLaunchInfoProvider::~AppsLaunchInfoProvider() {
-  eche_connection_status_handler_->RemoveObserver(this);
-}
-
-void AppsLaunchInfoProvider::OnConnectionStatusForUiChanged(
-    mojom::ConnectionStatus connection_status) {
-  last_connection_ = connection_status;
-}
-
-void AppsLaunchInfoProvider::SetEntryPoint(
+void AppsLaunchInfoProvider::SetAppLaunchInfo(
     mojom::AppStreamLaunchEntryPoint entry_point) {
   entry_point_ = entry_point;
+  last_connection_ =
+      eche_connection_status_handler_->connection_status_for_ui();
 }
 
-}  // namespace eche_app
-}  // namespace ash
+}  // namespace ash::eche_app
--- ash/webui/eche_app_ui/eche_notification_click_handler.cc
+++ ash/webui/eche_app_ui/eche_notification_click_handler.cc
@@ -53,7 +53,7 @@ void EcheNotificationClickHandler::HandleNotificationClick(
       base::UmaHistogramEnumeration(
           "Eche.AppStream.LaunchAttempt",
           mojom::AppStreamLaunchEntryPoint::NOTIFICATION);
-      apps_launch_info_provider_->SetEntryPoint(
+      apps_launch_info_provider_->SetAppLaunchInfo(
           mojom::AppStreamLaunchEntryPoint::NOTIFICATION);
       launch_app_helper_->LaunchEcheApp(
           notification_id, app_metadata.package_name,
