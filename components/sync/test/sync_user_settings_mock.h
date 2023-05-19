4d503e3ad30e7ce194523348bdc00c96ab4a9be3
Jood Hajeer <jood@google.com>
2023-05-19 11:12:54 +0000

    [sync] Introduce SyncUserSettings::SetSelectedType API for UNO.
    Introduce the new setter to set/un-set a single type which will be used
    in the new unified settings page.
    
    No expected behavior change.
    
    Bug: 1429254
    Change-Id: I67078b667178d3dd46bb568d4185cfbd0c126f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542985
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146446}

--- components/sync/test/sync_user_settings_mock.h
+++ components/sync/test/sync_user_settings_mock.h
@@ -35,6 +35,7 @@ class SyncUserSettingsMock : public SyncUserSettings {
               SetSelectedTypes,
               (bool, UserSelectableTypeSet),
               (override));
+  MOCK_METHOD(void, SetSelectedType, (UserSelectableType, bool), (override));
 #if BUILDFLAG(IS_IOS)
   MOCK_METHOD(void,
               SetBookmarksAndReadingListAccountStorageOptIn,
