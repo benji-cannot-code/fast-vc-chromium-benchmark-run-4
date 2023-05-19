63a5956cbb9cd6ac3e9feede4e8d98bfb01dd289
Christoph Schwering <schwering@google.com>
2023-05-19 09:42:23 +0000

    [Autofill] Move content dependencies from FormForest to AutofillDriver
    This CL abstracts from the //content dependencies in FormForest and
    moves the respective functions to AutofillDriver.
    
    This CL is a no-op.
    
    Bug: 1187842, 1443645
    Low-Coverage-Reason: autofill_driver_ios.mm only contains stubs.
    Change-Id: I46ac3575e3660bd2f050c8695373d0f7bb34b807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4499288
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146434}

--- components/autofill/ios/browser/autofill_driver_ios.h
+++ components/autofill/ios/browser/autofill_driver_ios.h
@@ -44,9 +44,14 @@ class AutofillDriverIOS : public AutofillDriver,
   ~AutofillDriverIOS() override;
 
   // AutofillDriver:
+  LocalFrameToken GetFrameToken() const override;
+  AutofillDriverIOS* GetParent() override;
+  absl::optional<LocalFrameToken> Resolve(FrameToken query) override;
   bool IsInActiveFrame() const override;
   bool IsInAnyMainFrame() const override;
+  bool IsInFencedFrameRoot() const override;
   bool IsPrerendering() const override;
+  bool HasSharedAutofillPermission() const override;
   bool CanShowAutofillUi() const override;
   ui::AXTreeID GetAxTreeId() const override;
   bool RendererIsAvailable() override;
@@ -67,6 +72,7 @@ class AutofillDriverIOS : public AutofillDriver,
   void SendFieldsEligibleForManualFillingToRenderer(
       const std::vector<FieldGlobalId>& fields) override;
   void SetShouldSuppressKeyboard(bool suppress) override;
+  void TriggerReparse() override;
   void TriggerReparseInAllFrames(
       base::OnceCallback<void(bool)> trigger_reparse_finished_callback)
       override;
