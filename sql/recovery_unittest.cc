d809946129a71810ce868f6690fbea797a742077
Austin Sullivan <asully@chromium.org>
2023-05-19 00:10:19 +0000

    sql: Add BuiltInRecovery::IsSupported method
    BuiltInRecovery is not yet supported on Fuchsia. Consumers can call this
    static helper method rather than manually checking buildflags themselves
    
    Bug: 1385500
    Change-Id: I1cc4eb3b95fb827bc16ab8ee89619c99d0068427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544906
    Reviewed-by: Evan Stade <estade@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146280}

--- sql/recovery_unittest.cc
+++ sql/recovery_unittest.cc
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
 #include "base/test/gtest_util.h"
-#include "base/types/pass_key.h"
-#include "build/build_config.h"
 #include "sql/database.h"
 #include "sql/meta_table.h"
 #include "sql/sqlite_result_code_values.h"
@@ -83,14 +81,7 @@ class SqlRecoveryTest : public testing::TestWithParam<bool> {
     return file.Write(0, kText, kTextBytes) == kTextBytes;
   }
 
-  bool UseBuiltIn() {
-#if BUILDFLAG(IS_FUCHSIA)
-    // TODO(https://crbug.com/1385500): Make built-in recovery work on Fuchsia.
-    return false;
-#else
-    return GetParam();
-#endif  // BUILDFLAG(IS_FUCHSIA)
-  }
+  bool UseBuiltIn() { return GetParam() && BuiltInRecovery::IsSupported(); }
 
  protected:
   base::ScopedTempDir temp_dir_;
