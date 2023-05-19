7c2decc818e58646a2afe38a9946dda9b8cfe405
Tom Anderson <thomasanderson@chromium.org>
2023-05-19 23:10:16 +0000

    Revert "Remove QT feature flag"
    This reverts commit bfc2cc87f3b7e24617d36ee98deed1f2209c0de3.
    
    Reason for revert: Scale factor not updated on change (https://crbug.com/1439149)
    
    Original change's description:
    > Remove QT feature flag
    >
    > The feature has received launch approval.
    >
    > R=sky
    >
    > Change-Id: Icc9bff102bcdfabd6ce548fa306efa1183bb2246
    > Bug: 1317782
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408764
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1128358}
    
    Bug: 1317782, 1439149
    Change-Id: I722f47816d0f88da31e58978cec78ea64135de0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549942
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146766}

--- ui/linux/linux_ui_factory.cc
+++ ui/linux/linux_ui_factory.cc
@@ -56,6 +56,9 @@ LinuxUiAndTheme* GetGtkUi() {
 }
 
 std::unique_ptr<LinuxUiAndTheme> CreateQtUi() {
+  if (!base::FeatureList::IsEnabled(kAllowQt)) {
+    return nullptr;
+  }
 #if BUILDFLAG(USE_QT)
   auto qt_ui = qt::CreateQtUi(GetGtkUi());
   if (qt_ui->Initialize()) {
@@ -124,6 +127,8 @@ LinuxUiAndTheme* GetDefaultLinuxUiAndTheme() {
 }
 
 }  // namespace
+
+BASE_FEATURE(kAllowQt, "AllowQt", base::FEATURE_DISABLED_BY_DEFAULT);
 
 LinuxUi* GetDefaultLinuxUi() {
   auto* linux_ui = GetDefaultLinuxUiAndTheme();
