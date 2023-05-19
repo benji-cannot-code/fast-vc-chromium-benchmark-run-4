2a6d1366636937f31206e63610af4624831658d8
Bartek Nowierski <bartekn@chromium.org>
2023-05-19 05:45:09 +0000

    [BRP] Make ref-count size feature play nicely w/ Mac11 usable size hack
    Also re-enable GetUsableSizeWithMac11MallocSizeHack test that was
    partially disabled in crrev.com/c/4542666.
    
    Bug: 1446870, 1349955
    Change-Id: I55ded9ef028b6fe6dc37d8122d26c02d242a5172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546520
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146375}

--- base/allocator/partition_allocator/partition_root.cc
+++ base/allocator/partition_allocator/partition_root.cc
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/thread_isolation/thread_isolation.h"
 #include "build/build_config.h"
 
-#if PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK) && BUILDFLAG(IS_APPLE)
+#if PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK)
 #include "base/allocator/partition_allocator/partition_alloc_base/mac/mac_util.h"
-#endif  // PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK) && BUILDFLAG(IS_APPLE)
+#endif
 
 #if BUILDFLAG(USE_STARSCAN)
 #include "base/allocator/partition_allocator/starscan/pcscan.h"
@@ -804,8 +804,34 @@ void PartitionRoot<thread_safe>::DestructForTesting() {
 
 #if PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK)
 template <bool thread_safe>
-void PartitionRoot<thread_safe>::EnableMac11MallocSizeHackForTesting() {
+void PartitionRoot<thread_safe>::InitMac11MallocSizeHackUsableSize(
+    size_t ref_count_size) {
   flags.mac11_malloc_size_hack_enabled_ = true;
+
+  // 0 means reserve just enough extras to fit PartitionRefCount.
+  if (!ref_count_size) {
+    ref_count_size = sizeof(internal::PartitionRefCount);
+  }
+  // Request of 32B will fall into a 48B bucket in the presence of BRP
+  // ref-count, yielding |48 - ref_count_size| of actual usable space.
+  flags.mac11_malloc_size_hack_usable_size_ = 48 - ref_count_size;
+}
+
+template <bool thread_safe>
+void PartitionRoot<thread_safe>::EnableMac11MallocSizeHackForTesting(
+    size_t ref_count_size) {
+  flags.mac11_malloc_size_hack_enabled_ = true;
+  InitMac11MallocSizeHackUsableSize(ref_count_size);
+}
+
+template <bool thread_safe>
+void PartitionRoot<thread_safe>::EnableMac11MallocSizeHackIfNeeded(
+    size_t ref_count_size) {
+  flags.mac11_malloc_size_hack_enabled_ =
+      flags.brp_enabled_ && internal::base::mac::IsOS11();
+  if (flags.mac11_malloc_size_hack_enabled_) {
+    InitMac11MallocSizeHackUsableSize(ref_count_size);
+  }
 }
 #endif  // PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK)
 
@@ -885,10 +911,9 @@ void PartitionRoot<thread_safe>::Init(PartitionOptions opts) {
 #if BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
     flags.brp_enabled_ =
         opts.backup_ref_ptr == PartitionOptions::BackupRefPtr::kEnabled;
-#if PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK) && BUILDFLAG(IS_APPLE)
-    flags.mac11_malloc_size_hack_enabled_ =
-        flags.brp_enabled_ && internal::base::mac::IsOS11();
-#endif  // PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK) && BUILDFLAG(IS_APPLE)
+#if PA_CONFIG(ENABLE_MAC11_MALLOC_SIZE_HACK)
+    EnableMac11MallocSizeHackIfNeeded(opts.ref_count_size);
+#endif
 #else   // BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
     PA_CHECK(opts.backup_ref_ptr == PartitionOptions::BackupRefPtr::kDisabled);
 #endif  // BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
