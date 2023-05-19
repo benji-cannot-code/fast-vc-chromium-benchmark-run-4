29e45bf683e174d479ff8a35b9caba0f9d0d942d
Yuheng Huang <yuhengh@chromium.org>
2023-05-19 18:02:40 +0000

    Remove views DEPS for NTP feed
    Change the side panel API usage from c/b/ui/views to c/b/ui so
    the DEPS rules is not longer needed.
    
    Change-Id: I517c3e0ee47cedb7062e0c64abd59dddb6bdceed
    Bug: 1341399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518706
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1146586}

--- chrome/browser/new_tab_page/modules/feed/feed_handler.cc
+++ chrome/browser/new_tab_page/modules/feed/feed_handler.cc
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
-#include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
+#include "chrome/browser/ui/side_panel/side_panel_ui.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_version.h"
 #include "components/feed/core/v2/public/ntp_feed_content_fetcher.h"
@@ -89,16 +87,9 @@ void FeedHandler::ArticleOpened() {
   if (!browser)
     return;
 
-  BrowserView* const browser_view =
-      BrowserView::GetBrowserViewForBrowser(browser);
-  if (!browser_view)
-    return;
-
-  // TODO(https://crbug.com/1341399): When possible, show the side panel feed in
-  // a way that doesn't depend on Views and remove the DEPS rule.
-  if (browser_view->side_panel_coordinator()) {
-    browser_view->side_panel_coordinator()->Show(SidePanelEntry::Id::kFeed);
-  }
+  SidePanelUI* side_panel_ui = SidePanelUI::GetSidePanelUIForBrowser(browser);
+  DCHECK(side_panel_ui);
+  side_panel_ui->Show(SidePanelEntryId::kFeed);
 }
 
 }  // namespace ntp
