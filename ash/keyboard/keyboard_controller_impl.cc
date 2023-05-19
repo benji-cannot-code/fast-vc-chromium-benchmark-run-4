c60d321f317c42cdc6db22401d9443abd4ad88f2
YuhanYang <yyhyyh@google.com>
2023-05-19 03:52:11 +0000

    Settings Split: Call GetGeneralizedTopRowAreFKeys in controller
    Call GetGeneralizedTopRowAreFKeys in controller when the flag is on.
    
    Test: ash_unittests
    Bug: b/241965700
    Change-Id: Ic2b58c95d4a4d79c044f52357d4a8d32b4de19b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548042
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146353}

--- ash/keyboard/keyboard_controller_impl.cc
+++ ash/keyboard/keyboard_controller_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
 #include "ash/shell_delegate.h"
+#include "ash/system/input_device_settings/input_device_settings_controller_impl.h"
 #include "ash/wm/window_util.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
@@ -288,6 +289,11 @@ KeyboardControllerImpl::GetKeyRepeatSettings() {
 }
 
 bool KeyboardControllerImpl::AreTopRowKeysFunctionKeys() {
+  if (ash::features::IsInputDeviceSettingsSplitEnabled()) {
+    return Shell::Get()
+        ->input_device_settings_controller()
+        ->GetGeneralizedTopRowAreFKeys();
+  }
   PrefService* prefs = pref_change_registrar_->prefs();
   return prefs->GetBoolean(ash::prefs::kSendFunctionKeys);
 }
