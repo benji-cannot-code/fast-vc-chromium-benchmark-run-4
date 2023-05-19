c2edc0f348d6414980e29034bccc4aff46137e13
Xinghui Lu <xinghuilu@chromium.org>
2023-05-19 23:20:57 +0000

    [3/3] Rename SafeBrowsingApiHandler to SafetyNetApiHandler
    We will migrate to a new Safe Browsing API that is built in a
    dedicated SafeBrowsing module, rename the current handler to
    SafetyNetApiHandler.
    
    This is a three-sided patch:
    1) Land new class name with additional compatibility shims.
    2) Rename in downstream.
    3) (this CL) Rename in upstream and remove the compatibility shims.
    
    Bug: 1444515
    Change-Id: Ia9afc7e99cea474a038050bd722b5a1460c78800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528489
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146769}

--- chrome/android/javatests/src/org/chromium/chrome/browser/subresource_filter/SubresourceFilterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/subresource_filter/SubresourceFilterTest.java
@@ -24,7 +24,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Restriction;
-import org.chromium.chrome.browser.MockSafeBrowsingApiHandler;
+import org.chromium.chrome.browser.MockSafetyNetApiHandler;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
@@ -101,7 +101,7 @@ public final class SubresourceFilterTest {
     @Before
     public void setUp() throws Exception {
         mTestServer = mTestServerRule.getServer();
-        SafeBrowsingApiBridge.setHandler(new MockSafeBrowsingApiHandler());
+        SafeBrowsingApiBridge.setHandler(new MockSafetyNetApiHandler());
         mActivityTestRule.startMainActivityOnBlankPage();
 
         // Disallow all jpgs.
@@ -110,7 +110,7 @@ public final class SubresourceFilterTest {
 
     @After
     public void tearDown() {
-        MockSafeBrowsingApiHandler.clearMockResponses();
+        MockSafetyNetApiHandler.clearMockResponses();
     }
 
     @Test
@@ -323,7 +323,7 @@ public final class SubresourceFilterTest {
 
     private boolean loadPageWithBlockableContentAndTestIfBlocked(String url, String metadata)
             throws TimeoutException {
-        MockSafeBrowsingApiHandler.addMockResponse(url, metadata);
+        MockSafetyNetApiHandler.addMockResponse(url, metadata);
         mActivityTestRule.loadUrl(url);
         return Boolean.parseBoolean(mActivityTestRule.runJavaScriptCodeInCurrentTab("imgLoaded"));
     }
