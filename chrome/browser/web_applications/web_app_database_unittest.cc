ac3f98ec9ed76fce177007fd811053ab0a3620db
Alan Cutter <alancutter@chromium.org>
2023-05-19 07:02:06 +0000

    [dPWA] Use many more random web apps in WebAppDatabaseTest
    This CL increases our proto writing test coverage 10x with little effort
    or cost. The test suite runs in under 2 seconds still.
    
    Change-Id: I616c187a22181c652fa9936d988419f304ecd0b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546767
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146393}

--- chrome/browser/web_applications/web_app_database_unittest.cc
+++ chrome/browser/web_applications/web_app_database_unittest.cc
@@ -171,7 +171,7 @@ TEST_F(WebAppDatabaseTest, WriteAndReadRegistry) {
   InitSyncBridge();
   EXPECT_TRUE(registrar().is_empty());
 
-  const uint32_t num_apps = 20;
+  const uint32_t num_apps = 100;
   const GURL base_url("https://example.com/path");
 
   std::unique_ptr<WebApp> app =
@@ -198,7 +198,7 @@ TEST_F(WebAppDatabaseTest, WriteAndDeleteAppsWithCallbacks) {
   InitSyncBridge();
   EXPECT_TRUE(registrar().is_empty());
 
-  const uint32_t num_apps = 10;
+  const uint32_t num_apps = 100;
   const GURL base_url("https://example.com/path");
 
   RegistryUpdateData::Apps apps_to_create;
@@ -267,7 +267,7 @@ TEST_F(WebAppDatabaseTest, WriteAndDeleteAppsWithCallbacks) {
 }
 
 TEST_F(WebAppDatabaseTest, OpenDatabaseAndReadRegistry) {
-  Registry registry = WriteWebApps(GURL("https://example.com/path"), 20);
+  Registry registry = WriteWebApps(GURL("https://example.com/path"), 100);
 
   InitSyncBridge();
   EXPECT_TRUE(IsRegistryEqual(mutable_registrar().registry(), registry));
