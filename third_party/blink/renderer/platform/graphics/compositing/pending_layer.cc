683df8feb2be4072ebdfa28ae24a5f8b20eb5c2d
Xianzhu Wang <wangxianzhu@chromium.org>
2023-05-19 01:18:20 +0000

    Use color_utils::GetResultingPaintColor instead of SkColorFilter
    This removes 2 references to SkColorFilter from blink. The new
    function is more suitable for the usage.
    
    Bug: 1434335
    Change-Id: If148f540cbb9da54e0be88339c41e1290ae7e1bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546278
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146302}

--- third_party/blink/renderer/platform/graphics/compositing/pending_layer.cc
+++ third_party/blink/renderer/platform/graphics/compositing/pending_layer.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
-#include "third_party/skia/include/core/SkColorFilter.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/vector2d_conversions.h"
@@ -695,12 +695,10 @@ SkColor4f PendingLayer::ComputeBackgroundColor() const {
   }
   SkColor4f background_color = background_colors.back();
   background_colors.pop_back();
+
   for (const SkColor4f& color : base::Reversed(background_colors)) {
-    if (auto color_filter =
-            SkColorFilters::Blend(color, nullptr, SkBlendMode::kSrcOver)) {
-      background_color =
-          color_filter->filterColor4f(background_color, nullptr, nullptr);
-    }
+    background_color = SkColor4f::FromColor(color_utils::GetResultingPaintColor(
+        color.toSkColor(), background_color.toSkColor()));
   }
   return background_color;
 }
--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -1038,6 +1038,15 @@ _CONFIG = [
     },
     {
         'paths': [
+            'third_party/blink/renderer/platform/graphics/compositing/pending_layer.cc',
+            'third_party/blink/renderer/platform/graphics/paint/paint_chunker.cc',
+        ],
+        'allowed': [
+            'color_utils::GetResultingPaintColor',
+        ],
+    },
+    {
+        'paths': [
             'third_party/blink/renderer/core/inspector/inspector_contrast.cc',
             'third_party/blink/renderer/core/inspector/inspector_contrast.h'
         ],
