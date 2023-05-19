d3c68cb51c5608af14b17bdcee82077ed1ffb2e7
Michael Lippautz <mlippautz@chromium.org>
2023-05-19 13:23:40 +0000

    Fix cppgc_enable_object_names build
    This is in anticipation of enabling it for Chrome for Testing
    channels which would then build and test these builds.
    
    Bug: 1446931
    Change-Id: Ib084765c2756df9407caeca1aeffe346c72eef4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545538
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146474}

--- third_party/blink/renderer/core/workers/threaded_worklet_messaging_proxy.h
+++ third_party/blink/renderer/core/workers/threaded_worklet_messaging_proxy.h
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/workers/threaded_messaging_proxy_base.h"
+#include "third_party/blink/renderer/core/workers/threaded_worklet_object_proxy.h"
 #include "third_party/blink/renderer/core/workers/worklet_global_scope_proxy.h"
 
 namespace blink {
 
-class ThreadedWorkletObjectProxy;
 class WorkerClients;
 class WorkletModuleResponsesMap;
 
