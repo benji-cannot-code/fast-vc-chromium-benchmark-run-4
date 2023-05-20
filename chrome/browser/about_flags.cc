69e3c5c28b83f5983cf144f56e330eabd89c3ac7
Jun Zou <junzou@chromium.org>
2023-05-20 00:45:38 +0000

    [Omnibox][ML] Add batch scoring for autocomplete urls.
    - Guarded by a feature param `ml_batch_url_scoring`.
    
    Bug: b/282173802
    Change-Id: I5cab1fa2df295514486517a754b0c5e519a7e5f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548341
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Jun Zou <junzou@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146808}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1528,6 +1528,9 @@ const FeatureEntry::FeatureParam kOmniboxMlUrlScoringPreserveDefault[] = {
     {"MlUrlScoringRerankFinalMatchesOnly", "true"},
     {"MlUrlScoringPreserveDefault", "true"},
 };
+const FeatureEntry::FeatureParam kOmniboxMlBatchUrlScoring[] = {
+    {"MlBatchUrlScoring", "true"},
+};
 
 const FeatureEntry::FeatureVariation kOmniboxMlUrlScoringVariations[] = {
     {"Run the model but do not rescore or rerank the matches (counterfactual)",
@@ -1541,6 +1544,8 @@ const FeatureEntry::FeatureVariation kOmniboxMlUrlScoringVariations[] = {
      "match",
      kOmniboxMlUrlScoringPreserveDefault,
      std::size(kOmniboxMlUrlScoringPreserveDefault), nullptr},
+    {"Run the model on a batch of matches", kOmniboxMlBatchUrlScoring,
+     std::size(kOmniboxMlBatchUrlScoring), nullptr},
 };
 const FeatureEntry::FeatureParam kRealboxTwoPreviousSearchRelatedSuggestions[] =
     {
