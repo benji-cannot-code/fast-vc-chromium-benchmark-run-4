ed213d8355012f06f4cc4856164655af16462e09
Philip Rogers <pdr@chromium.org>
2023-05-20 00:54:17 +0000

    Fix cached lazy images always reporting below-fold metrics
    The intersection observer spec requires at least one intersection be
    reported after observe() is called, even for disconnected targets. This
    was problematic for the lazy load visibility metrics observer
    (`LazyLoadImageObserver::visibility_metrics_observer_`) which could be
    created during parsing if the image was already cached. Parsing can
    occur before attaching the element, resulting in an initial empty
    observation that caused the visibility metrics to report the image as
    initially below-the-fold.
    
    This patch adds a new flag to the intersection observer delegate that
    prevents the detached-target behavior for the visibility metrics
    observer.
    
    Bug: 1446753
    Change-Id: I427aa4fbe288eae60c7eaee8c1cc5696aea2f94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548765
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146811}

--- third_party/blink/renderer/core/testing/intersection_observer_test_helper.h
+++ third_party/blink/renderer/core/testing/intersection_observer_test_helper.h
@@ -34,6 +34,9 @@ class TestIntersectionObserverDelegate : public IntersectionObserverDelegate {
     call_count_++;
     entries_.AppendVector(entries);
   }
+  bool NeedsInitialObservationWithDetachedTarget() const override {
+    return true;
+  }
   ExecutionContext* GetExecutionContext() const override {
     return document_->GetExecutionContext();
   }
