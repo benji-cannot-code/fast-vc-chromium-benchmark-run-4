f5dc9c5f2cd7a19c6c6561623f270c4603ee4b15
Joshua Pawlicki <waffles@chromium.org>
2023-05-22 19:26:57 +0000

    Updater: Fix or remove recovery component TODOs.
    1. Suppress the install UI during recovery.
    2. Fix an issue with ksadmin install not passing the scope.
    3. Remove TODO related to ksadmin install UI suppression: there is no
       mac UI.
    4. Remove TODO related to synthesizing an ECP for the browser during
       recovery. We support apps without ECPs on macOS now, and the browser
       can update the ECP later.
    5. Remove unused session ID in recovery flow.
    
    Fixed: 1281971
    Change-Id: I1c17734b512cf51d5251685090a58e59a98e61b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4552357
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147398}

--- chrome/updater/app/app_recover.cc
+++ chrome/updater/app/app_recover.cc
@@ -37,11 +37,8 @@ namespace {
 class AppRecover : public App {
  public:
   AppRecover(const base::Version& browser_version,
-             const std::string& session_id,
              const std::string& browser_app_id)
-      : browser_version_(browser_version),
-        session_id_(session_id),
-        browser_app_id_(browser_app_id) {}
+      : browser_version_(browser_version), browser_app_id_(browser_app_id) {}
 
  private:
   ~AppRecover() override = default;
@@ -56,7 +53,6 @@ class AppRecover : public App {
                     int reinstall_result);
 
   const base::Version browser_version_;
-  const std::string session_id_;  // TODO(crbug.com/1281971): Unused.
   const std::string browser_app_id_;
   scoped_refptr<GlobalPrefs> global_prefs_;
 };
@@ -117,7 +113,6 @@ std::vector<RegistrationRequest> AppRecover::RecordRegisteredApps() const {
     RegistrationRequest registration;
     registration.app_id = browser_app_id_;
     registration.version = browser_version_;
-    // TODO(crbug.com/1281971): registration.existence_checker_path must be set.
     apps.emplace_back(registration);
   }
   return apps;
@@ -148,13 +143,13 @@ int AppRecover::ReinstallUpdater() const {
   }
   base::CommandLine install_command(setup_path);
   install_command.AppendSwitch(kInstallSwitch);
+  install_command.AppendSwitch(kSilentSwitch);
   install_command.AppendSwitch(kEnableLoggingSwitch);
   install_command.AppendSwitchASCII(kLoggingModuleSwitch,
                                     kLoggingModuleSwitchValue);
   if (IsSystemInstall(updater_scope())) {
     install_command.AppendSwitch(kSystemSwitch);
   }
-  // TODO(crbug.com/1281971): suppress the installer's UI.
   if (!base::LaunchProcess(install_command, {}).WaitForExit(&exit_code)) {
     VLOG(0) << "Failed to wait for the installer to exit.";
     return kErrorWaitFailedInstall;
@@ -199,7 +194,6 @@ scoped_refptr<App> MakeAppRecover() {
       base::CommandLine::ForCurrentProcess();
   return base::MakeRefCounted<AppRecover>(
       base::Version(command_line->GetSwitchValueASCII(kBrowserVersionSwitch)),
-      command_line->GetSwitchValueASCII(kSessionIdSwitch),
       command_line->GetSwitchValueASCII(kAppGuidSwitch));
 }
 
