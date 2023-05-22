1917dc32b94c9a23d77bd293ce53e5aa71b203d1
Wenyu Fu <wenyufu@google.com>
2023-05-22 21:49:09 +0000

    [Sharesheet] Long screenshot only supports link of current page
    Only supports long screenshot when sharing the link of the current
    page.
    
    Bug: 1432021, 1410201
    Change-Id: Ib6b47e67324dc55e1bcd3d10d4f96fa505109dd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548584
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147489}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/ChromeProvidedSharingOptionsProviderBase.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/ChromeProvidedSharingOptionsProviderBase.java
@@ -240,6 +240,10 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
         return availableOptions;
     }
 
+    protected boolean usePolishedActionOrderedList() {
+        return ChromeFeatureList.isEnabled(ChromeFeatureList.SHARE_SHEET_CUSTOM_ACTIONS_POLISH);
+    }
+
     /**
      * Creates all enabled {@link FirstPartyOption}s and adds them to {@code
      * mOrderedFirstPartyOptions} in the order they should appear.
@@ -252,7 +256,7 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
             maybeAddQrCodeFirstPartyOption();
             return;
         }
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.SHARE_SHEET_CUSTOM_ACTIONS_POLISH)) {
+        if (usePolishedActionOrderedList()) {
             mOrderedFirstPartyOptions.add(createCopyLinkFirstPartyOption());
             maybeAddCopyFirstPartyOption();
             maybeAddLongScreenshotFirstPartyOption();
@@ -332,7 +336,7 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
     private FirstPartyOption createCopyLinkFirstPartyOption() {
         FirstPartyOptionBuilder builder = new FirstPartyOptionBuilder(
                 ContentType.LINK_PAGE_VISIBLE, ContentType.LINK_PAGE_NOT_VISIBLE);
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.SHARE_SHEET_CUSTOM_ACTIONS_POLISH)) {
+        if (usePolishedActionOrderedList()) {
             builder.setContentTypesToDisableFor(
                     ContentType.LINK_AND_TEXT, ContentType.IMAGE_AND_LINK);
         } else {
