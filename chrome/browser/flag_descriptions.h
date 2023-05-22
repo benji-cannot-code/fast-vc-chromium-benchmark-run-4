7a64a210d765d219ca7f52f3bc86c11604020566
Timothy Loh <timloh@chromium.org>
2023-05-22 06:26:53 +0000

    Crostini: Add flag to turn on the Qt IM module and plumb to garcon
    Similar to the VK flag, this depends on the main (GTK) Crostini IME
    flag to be enabled, as that is required to set up exo appropriately.
    
    Also update description of #crostini-ime-support flag to mention it
    is only for GTK3(/Electron) apps.
    
    Bug: b/232048947
    Change-Id: Ib9a34c9b026dcad3223fe6823fc241fff47a8735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546452
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Sophia Lin <sophialin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147059}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3107,6 +3107,9 @@ extern const char kCrostiniMultiContainerDescription[];
 extern const char kCrostiniImeSupportName[];
 extern const char kCrostiniImeSupportDescription[];
 
+extern const char kCrostiniQtImeSupportName[];
+extern const char kCrostiniQtImeSupportDescription[];
+
 extern const char kCrostiniVirtualKeyboardSupportName[];
 extern const char kCrostiniVirtualKeyboardSupportDescription[];
 
