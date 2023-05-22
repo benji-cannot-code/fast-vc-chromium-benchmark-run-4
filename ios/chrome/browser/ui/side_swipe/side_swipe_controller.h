3e4762857acf47f2d4d9a78d33e4759c4341d4ed
Federica Germinario <fedegermi@google.com>
2023-05-22 15:21:21 +0000

    [ios] Minor cleanup inside SideSwipeController
    This CL removes some unused code inside SideSwipeController and moves
    all UIGestureRecognizerDelegate methods together under the same pragma
    mark.
    
    Change-Id: I9684bc94e39e1f0403d78e8231f675a026a96ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4539301
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147236}

--- ios/chrome/browser/ui/side_swipe/side_swipe_controller.h
+++ ios/chrome/browser/ui/side_swipe/side_swipe_controller.h
@@ -10,12 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/snapshots/snapshot_generator_delegate.h"
 
-namespace web {
-class WebState;
-}
-
-@class CardSideSwipeView;
-@class SideSwipeGestureRecognizer;
 @protocol SideSwipeToolbarInteracting;
 @protocol SideSwipeToolbarSnapshotProviding;
 @protocol TabStripHighlighting;
