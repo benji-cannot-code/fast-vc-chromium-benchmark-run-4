c49ff83914777d394517a1e3c4ff99786abf2ee1
Shengsong Tan <sstan@chromium.org>
2023-05-22 02:27:17 +0000

    arc: Maintain swap request state in vmm manager
    This CL save swap state request in vmm manager and remove the saved
    swap state in vmm swap scheduler.
    
    BUG=b:281948925
    TEST=ArcVmmManagerTest
    
    Change-Id: I47cfd1c058da090d4c83026c35132894e2943bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549038
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147006}

--- chrome/browser/ash/arc/vmm/arc_vmm_manager_unittest.cc
+++ chrome/browser/ash/arc/vmm/arc_vmm_manager_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
+#include "base/time/time.h"
 #include "chrome/browser/ash/arc/vmm/arcvm_working_set_trim_executor.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
@@ -288,6 +289,44 @@ TEST_F(ArcVmmManagerTest, EnableSwapRequestWillEnableHeartbeat) {
   task_environment_.FastForwardBy(kEnabledStateHeartbeatInterval);
   EXPECT_EQ(3, client()->force_enable_count());
   EXPECT_EQ(3, client()->enable_count());
+  EXPECT_EQ(0, client()->swap_out_count());
+  EXPECT_EQ(0, client()->disable_count());
+  task_environment_.RunUntilIdle();
+}
+
+TEST_F(ArcVmmManagerTest, NotResendSameStateRequestButHeartbeat) {
+  InitVmmManager();
+  EnableAndConnectArcVm();
+  SetTrimCall(true);
+  InitAggressiveBallonResponse();
+
+  manager()->SetSwapState(SwapState::ENABLE);
+  task_environment_.RunUntilIdle();
+  EXPECT_EQ(0, client()->force_enable_count());
+  EXPECT_EQ(1, client()->enable_count());
+  EXPECT_EQ(0, client()->swap_out_count());
+  EXPECT_EQ(0, client()->disable_count());
+
+  // Not resend same request, but leave to next heart beat.
+  manager()->SetSwapState(SwapState::ENABLE);
+  task_environment_.RunUntilIdle();
+  EXPECT_EQ(0, client()->force_enable_count());
+  EXPECT_EQ(1, client()->enable_count());
+  EXPECT_EQ(0, client()->swap_out_count());
+  EXPECT_EQ(0, client()->disable_count());
+
+  task_environment_.FastForwardBy(base::Seconds(1));
+  // Not resend same request, but leave to next heart beat.
+  manager()->SetSwapState(SwapState::ENABLE);
+  task_environment_.RunUntilIdle();
+  EXPECT_EQ(0, client()->force_enable_count());
+  EXPECT_EQ(1, client()->enable_count());
+  EXPECT_EQ(0, client()->swap_out_count());
+  EXPECT_EQ(0, client()->disable_count());
+
+  task_environment_.FastForwardBy(kEnabledStateHeartbeatInterval);
+  EXPECT_EQ(0, client()->force_enable_count());
+  EXPECT_EQ(2, client()->enable_count());
   EXPECT_EQ(0, client()->swap_out_count());
   EXPECT_EQ(0, client()->disable_count());
   task_environment_.RunUntilIdle();
