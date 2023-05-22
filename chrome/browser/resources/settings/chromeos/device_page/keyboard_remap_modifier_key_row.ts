db886b22a090e2d5096baa1e2d36e9a5f2b53091
Danny Wang <wangdanny@google.com>
2023-05-22 18:29:43 +0000

    Settings Split: Fix the meta key label
    Fix the label when icon shows up.
    
    Bug: b/241965700
    Test: test in vm
    Change-Id: Ica7a069f030e4b6e2f6c88933af2f49c9c596b7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548340
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147361}

--- chrome/browser/resources/settings/chromeos/device_page/keyboard_remap_modifier_key_row.ts
+++ chrome/browser/resources/settings/chromeos/device_page/keyboard_remap_modifier_key_row.ts
@@ -139,7 +139,7 @@ export class KeyboardRemapModifierKeyRowElement extends
       // Launcher and Search key will display icon instead of text.
       case MetaKey.kLauncher:
       case MetaKey.kSearch:
-        return '';
+        return this.i18n('perDeviceKeyboardKeySearch');
     }
   }
 
