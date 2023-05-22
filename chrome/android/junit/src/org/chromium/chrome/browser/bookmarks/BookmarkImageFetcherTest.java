aa587463047a3413fce252eae6043fafeed5fe96
Brandon Wylie <wylieb@chromium.org>
2023-05-22 18:01:51 +0000

    [bookmarks] Skip null children when fetching images for folders
    It's possible for children to be removed while folder images are being
    fetched. It's possible to display folders and their children
    side-by-side in the root bookmarks screen. When this happens and one
    of the children being queried returns null, the image fetcher crashes.
    Adding code to check for null when querying folder children and
    skipping them.
    
    Bug: 1446932
    Change-Id: I2aeb8e93274e2f02c215fd24047adfa216e67eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4552418
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1147338}

--- chrome/android/junit/src/org/chromium/chrome/browser/bookmarks/BookmarkImageFetcherTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/bookmarks/BookmarkImageFetcherTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.bookmarks;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doAnswer;
@@ -170,6 +171,18 @@ public class BookmarkImageFetcherTest {
         Pair<Drawable, Drawable> drawables = mFolderDrawablesCaptor.getValue();
         assertNotNull(drawables.first);
         assertNotNull(drawables.second);
+    }
+
+    @Test
+    public void testFetchFirstTwoImagesForFolder_nullChild() {
+        doReturn(null).when(mBookmarkModel).getBookmarkById(mBookmarkId2);
+
+        mBookmarkImageFetcher.fetchFirstTwoImagesForFolder(mFolderItem, mFolderDrawablesCallback);
+        verify(mFolderDrawablesCallback).onResult(mFolderDrawablesCaptor.capture());
+
+        Pair<Drawable, Drawable> drawables = mFolderDrawablesCaptor.getValue();
+        assertNotNull(drawables.first);
+        assertNull(drawables.second);
     }
 
     @Test
