69a4ecf04ca6fccf2a2d3b7e824c7f167032ef8e
Jerry Liu <pzliu@google.com>
2023-05-22 21:27:15 +0000

    personalization: policy managed wallpaper card
    1. Replace the managed icon.
    2. Change opacity to match with the ambient policy managed state.
    
    https://screenshot.googleplex.com/6bip2GUh2XCqDFh Jelly off
    https://screenshot.googleplex.com/8BZ5MGFFQR3iUnf Jelly on
    
    BUG=b:283547562
    TEST=Manually
    
    Change-Id: I3be38e923083921fc55fccb97791d114dc49369a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4552124
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Commit-Queue: Jerry Liu <pzliu@google.com>
    Auto-Submit: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147475}

--- ash/webui/personalization_app/resources/js/wallpaper/wallpaper_preview_element.ts
+++ ash/webui/personalization_app/resources/js/wallpaper/wallpaper_preview_element.ts
@@ -18,6 +18,7 @@ import '../../css/cros_button_style.css.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 
 import {CurrentWallpaper, WallpaperType} from '../../personalization_app.mojom-webui.js';
+import {isPersonalizationJellyEnabled} from '../load_time_booleans.js';
 import {Paths, PersonalizationRouter} from '../personalization_router_element.js';
 import {WithPersonalizationStore} from '../personalization_store.js';
 import {isNonEmptyArray} from '../utils.js';
@@ -50,6 +51,12 @@ export class WallpaperPreview extends WithPersonalizationStore {
         type: Boolean,
         computed: 'isPolicyControlled_(image_)',
       },
+      isPersonalizationJellyEnabled_: {
+        type: Boolean,
+        value() {
+          return isPersonalizationJellyEnabled();
+        },
+      },
     };
   }
 
@@ -57,6 +64,7 @@ export class WallpaperPreview extends WithPersonalizationStore {
   private imageLoading_: boolean;
   private loading_: boolean;
   private policyControlled_: boolean;
+  private isPersonalizationJellyEnabled_: boolean;
 
   override connectedCallback() {
     super.connectedCallback();
