c7a5ab20c6af80cf9bbc17f4ef68f825e3e1f54f
Osama Fathy <osamafathy@google.com>
2023-05-22 20:05:30 +0000

    [OOBE][CHOOBE] Add zoom slider to display size screen
    - Add the zoom slider to the display size screen.
    - This change intoduces a slider with a tick for each valid zoom
    factor (retrieved from `display::GetDisplayZoomFactors()`).
    - The slider will be shown with the current zoom factor selected
    (retrieved from
    `display_manager()->GetDisplayInfo(display_id).zoom_factor()`).
    - All factors will be shown in percentage, the JS side will receive
    and return the values in percentage, the conversions from the
    float factors to the integer percentages and back are done in the
    browser side.
    - Applying the selected zoom will be implemented in a follow-up
    CL.
    
    Bug: b:275555658
    Change-Id: I6bfe035aef80aca302567f1978d3eda1d2ded910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503071
    Reviewed-by: Brahim Chikhaoui <bchikhaoui@google.com>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147430}

--- chrome/browser/resources/chromeos/login/screens/common/display_size.js
+++ chrome/browser/resources/chromeos/login/screens/common/display_size.js
@@ -6,13 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for touchpad scroll screen.
  */
 
+import '//resources/cr_elements/cr_slider/cr_slider.js';
 import '//resources/polymer/v3_0/iron-iconset-svg/iron-iconset-svg.js';
 import '../../components/buttons/oobe_next_button.js';
 import '../../components/common_styles/oobe_common_styles.css.js';
 import '../../components/common_styles/oobe_dialog_host_styles.css.js';
 import '../../components/dialogs/oobe_adaptive_dialog.js';
+import '../../components/oobe_display_size_selector.js';
 import '../../components/oobe_icons.html.js';
 
+import {CrSliderElement} from '//resources/cr_elements/cr_slider/cr_slider.js';
 import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.js';
@@ -64,13 +67,16 @@ class DisplaySizeScreen extends DisplaySizeScreenElementBase {
     this.initializeLoginScreen('DisplaySizeScreen');
   }
 
+  onBeforeShow(data) {
+    this.$.sizeSelector.init(data['availableSizes'], data['currentSize']);
+  }
 
   getOobeUIInitialState() {
     return OOBE_UI_STATE.ONBOARDING;
   }
 
   onNextClicked_() {
-    this.userActed(UserAction.NEXT);
+    this.userActed([UserAction.NEXT, this.$.sizeSelector.getSelectedSize()]);
   }
 }
 
