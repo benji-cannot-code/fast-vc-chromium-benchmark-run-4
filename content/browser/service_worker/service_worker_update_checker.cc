5c6d731f58cec4270b31567b3fd89a80aa3c8563
Dylan Cutler <dylancutler@google.com>
2023-05-22 21:37:00 +0000

    Use StorageKey to build IsolationInfo for service worker script requests
    Bug: 1147281
    Change-Id: Ic36cb6f8b785ba2bf1c1cf43078244f4d5ba53e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496784
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147481}

--- content/browser/service_worker/service_worker_update_checker.cc
+++ content/browser/service_worker/service_worker_update_checker.cc
@@ -257,6 +257,7 @@ void ServiceWorkerUpdateChecker::OnResourceIdAssignedForOneScriptCheck(
                 kDefault
           : ServiceWorkerSingleScriptUpdateChecker::ScriptChecksumUpdateOption::
                 kForceUpdate,
+      version_to_update_->key(),
       base::BindOnce(&ServiceWorkerUpdateChecker::OnOneUpdateCheckFinished,
                      weak_factory_.GetWeakPtr(), resource_id));
 }
