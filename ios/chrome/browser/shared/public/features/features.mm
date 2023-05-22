e36fe905b9756d7385c1a9f04c0b0f0854ad30ca
Louis Romero <lpromero@google.com>
2023-05-22 09:54:36 +0000

    Disable Recency Sort on iPad
    Recency Sort is tried as part of Inactive Tabs, which is only targetting
    iPhone for now.
    
    Fixed: 1446282
    Change-Id: Ibdc6cb5ba994fefce9b1e668c5578d3d88e4dc16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543268
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147117}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -172,6 +172,10 @@ BASE_FEATURE(kTabGridRecencySort,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsTabGridSortedByRecency() {
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+    return false;
+  }
+
   return base::FeatureList::IsEnabled(kTabGridRecencySort);
 }
 
