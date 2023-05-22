8bca15634425104345af2165434ff42634ed587c
Simon Zünd <szuend@chromium.org>
2023-05-22 10:17:31 +0000

    [devtools] Import test runners in http/tests/devtools/overrides
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    Drive-by: Fix, rebaseline and re-enable one of the overrides tests.
    
    R=kprokopenko@chromium.org
    
    Bug: 1442410,1094436
    Change-Id: I1614f0a43bdbc314e045cac76d0edd8415649d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543043
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147126}

--- third_party/blink/web_tests/http/tests/devtools/overrides/project-added-with-existing-files-bind.js
+++ third_party/blink/web_tests/http/tests/devtools/overrides/project-added-with-existing-files-bind.js
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestRunner} from 'test_runner';
+import {BindingsTestRunner} from 'bindings_test_runner';
+
 (async function() {
   TestRunner.addResult(`Ensures that when a project is added with already existing files they bind.\n`);
-  await TestRunner.loadTestModule('bindings_test_runner');
 
   await TestRunner.navigatePromise('http://127.0.0.1:8000/devtools/network/resources/empty.html');
 
@@ -18,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   });
 
   const { testFileSystem } = await BindingsTestRunner.createOverrideProject('file:///tmp');
-  testFileSystem.addFile('127.0.0.1%3a8000/devtools/network/resources/empty.html', 'New Content');
+  testFileSystem.addFile('127.0.0.1:8000/devtools/network/resources/empty.html', 'New Content');
 
   BindingsTestRunner.setOverridesEnabled(true);
 })();
