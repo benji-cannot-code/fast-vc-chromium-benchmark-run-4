b7a1d58f4cb9b321826cb0e230f03ed178d210cf
Noah Rose Ledesma <noahrose@google.com>
2023-05-22 19:45:56 +0000

    Remove Linux CHECK and TODO from uninstall pinging
    Revert the changes of crrev.com/c/4409640. Remove CHECK statements added
    as a temporary debugging measure.
    
    Bug: 1431487
    Change-Id: Icdb963be9ddda98715f69cb279ade35b6aea59f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4550364
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147411}

--- chrome/updater/app/app_uninstall.cc
+++ chrome/updater/app/app_uninstall.cc
@@ -40,12 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/posix/setup.h"
 #endif
 
-#if BUILDFLAG(IS_LINUX)
-// TODO(crbug.com/1431487): Remove these includes after investigation.
-#include "base/ranges/algorithm.h"
-#include "url/gurl.h"
-#endif
-
 namespace updater {
 namespace {
 
@@ -141,12 +135,6 @@ void AppUninstall::UninstallAll(int reason) {
     // currently-running version of the updater.
     uninstall_data.version = base::Version(kUpdaterVersion);
   }
-// TODO(crbug.com/1431487): Remove this code after investigation.
-#if BUILDFLAG(IS_LINUX)
-  CHECK(base::ranges::none_of(config_->PingUrl(), [](const GURL& url) {
-    return url.DomainIs("update.googleapis.com");
-  })) << "Attempted to send an uninstall ping to non-local server";
-#endif
   update_client::UpdateClientFactory(config_)->SendUninstallPing(
       uninstall_data, reason,
       base::BindOnce(
