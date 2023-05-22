617dc6d8d3cdc9efa039ba77536f3407fc0467de
Andre Le <leandre@chromium.org>
2023-05-22 21:35:25 +0000

    VC UI: Fix a crash when clicking on the stop screen share button.
    The crash in the bug attached might be caught because of
    ScreenCaptureNotificationUI. In OnStarted, it passes the a stop_callback
    to SystemTrayNotifier using base::Unretained. However, since we start to
    use this function within the VC tray, the instance of
    ScreenCaptureNotificationUI can be deleted before this callback is
    triggered (i.e. in ArcScreenCaptureSession the notification can be
    deleted, which invalidate the callback). Thus, it is not safe to use
    base::Unretained here and we should use a weak pointer.
    
    This is only a quick fix to prevent the crash, a proper fix for stop
    screen sharing within VC tray is implemented in b/274956433.
    
    Bug: b:281908723
    Change-Id: I405b8f37ef32dad9c936220052e4ad0462617b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4551085
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147478}

--- chrome/browser/ash/notifications/screen_capture_notification_ui_ash.h
+++ chrome/browser/ash/notifications/screen_capture_notification_ui_ash.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_NOTIFICATIONS_SCREEN_CAPTURE_NOTIFICATION_UI_ASH_H_
 #define CHROME_BROWSER_ASH_NOTIFICATIONS_SCREEN_CAPTURE_NOTIFICATION_UI_ASH_H_
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/screen_capture_notification_ui.h"
 
 namespace ash {
@@ -34,6 +35,8 @@ class ScreenCaptureNotificationUIAsh : public ScreenCaptureNotificationUI {
 
   const std::u16string text_;
   base::OnceClosure stop_callback_;
+
+  base::WeakPtrFactory<ScreenCaptureNotificationUIAsh> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
