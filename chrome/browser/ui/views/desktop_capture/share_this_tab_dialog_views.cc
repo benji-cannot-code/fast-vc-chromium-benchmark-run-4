bdbbe864ff5a4dbeec865b8fe3cfafa783990745
Tove Petersson <tovep@chromium.org>
2023-05-22 06:57:29 +0000

    Adjusted spacing between elements in the ShareThisTabDialog
    Adjusted the scaling between some elements in the ShareThisTabDialog
    to align with mocks:
    - Added 16px between description and preview frame
    - Added 8px between preview frame and audio toggle
    - Reduce spacing between the audio toggle and the buttons to 16 px
    
    Bug: 1428878
    Change-Id: Ifad62e70660a01bb84414d9d36132c43b0d452b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549479
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147068}

--- chrome/browser/ui/views/desktop_capture/share_this_tab_dialog_views.cc
+++ chrome/browser/ui/views/desktop_capture/share_this_tab_dialog_views.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/border.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/style/typography.h"
+#include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 
 #if defined(USE_AURA)
@@ -96,7 +97,7 @@ ShareThisTabDialogView::ShareThisTabDialogView(
 
   const ChromeLayoutProvider* const provider = ChromeLayoutProvider::Get();
   gfx::Insets dialog_insets = provider->GetDialogInsetsForContentType(
-      views::DialogContentType::kText, views::DialogContentType::kControl);
+      views::DialogContentType::kText, views::DialogContentType::kText);
   dialog_insets.set_top(kTitleTopMargin);
   SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical, dialog_insets,
@@ -220,6 +221,9 @@ bool ShareThisTabDialogView::ShouldShowCloseButton() const {
 
 void ShareThisTabDialogView::SetupSourceView() {
   View* source_container = AddChildView(std::make_unique<views::View>());
+  source_container->SetProperty(views::kMarginsKey,
+                                gfx::Insets::TLBR(16, 0, 0, 0));
+
   views::BoxLayout* source_layout =
       source_container->SetLayoutManager(std::make_unique<views::BoxLayout>(
           views::BoxLayout::Orientation::kHorizontal));
@@ -233,6 +237,8 @@ void ShareThisTabDialogView::SetupSourceView() {
 
 void ShareThisTabDialogView::SetupAudioToggle() {
   View* audio_toggle_container = AddChildView(std::make_unique<views::View>());
+  audio_toggle_container->SetProperty(views::kMarginsKey,
+                                      gfx::Insets::TLBR(8, 0, 0, 0));
   audio_toggle_container->SetBackground(
       views::CreateThemedRoundedRectBackground(
           kColorShareThisTabAudioToggleBackground, 4));
