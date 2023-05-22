748b8fe5119efaaef31ad4d7cc380f7bc2c948fd
Rushan Suleymanov <rushans@google.com>
2023-05-22 08:02:21 +0000

    [Sync] Wait for sync engine initialization for download status
    While Sync data types are not initialized, it's unknown whether all the
    recent changes were downloaded. After this CL the status will be
    "Waiting for updates" until the data type is active.
    
    Bug: 1425026
    Change-Id: I249c7a8e93f1e85b4959d19e3ac7f314ebb273af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521032
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147079}

--- components/sync/test/data_type_manager_mock.h
+++ components/sync/test/data_type_manager_mock.h
@@ -31,6 +31,10 @@ class DataTypeManagerMock : public DataTypeManager {
               GetTypesWithPendingDownloadForInitialSync,
               (),
               (const override));
+  MOCK_METHOD(ModelTypeSet,
+              GetDataTypesWithPermanentErrors,
+              (),
+              (const override));
   MOCK_METHOD(State, state, (), (const override));
 
  private:
