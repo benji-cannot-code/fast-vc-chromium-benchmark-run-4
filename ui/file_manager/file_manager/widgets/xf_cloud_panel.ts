cc27d69e97fa18c44844d448623f24adcc4488b6
Ben Reich <benreich@chromium.org>
2023-05-22 08:03:58 +0000

    [FilesBulkPinning] Add preparing to sync state for progress panel
    The strings document (http://shortn/_qSFNkOjwk7) provides a preparing to
    sync state for the progress panel.
    
    Bug: b:276391980
    Change-Id: Iefae327fff12119758a6f4953c0611a6e5c6d709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4551431
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147080}

--- ui/file_manager/file_manager/widgets/xf_cloud_panel.ts
+++ ui/file_manager/file_manager/widgets/xf_cloud_panel.ts
@@ -170,6 +170,9 @@ export class XfCloudPanel extends XfBase {
   override render() {
     return html`<cr-action-menu>
       <div class="body">
+        <div class="static progress" id="progress-preparing">
+          ${str('DRIVE_PREPARING_TO_SYNC')}
+        </div>
         <div id="progress-state">
           <div class="progress">${
         this.items && this.items > 1 ?
@@ -225,12 +228,18 @@ function getCSS() {
 
     :host(:not([items][percentage])) #progress-state,
     :host([percentage="100"]) #progress-state,
+    :host([percentage="0"]) #progress-state,
     :host([type]) #progress-state {
       display: none;
     }
 
     :host(:not([items][percentage="100"])) #progress-finished,
     :host([type]) #progress-finished {
+      display: none;
+    }
+
+    :host(:not([items][percentage="0"])) #progress-preparing,
+    :host([type]) #progress-preparing {
       display: none;
     }
 
@@ -301,6 +310,11 @@ function getCSS() {
       margin: 8px 0 8px;
       margin-inline: 16px;
       width: calc(100% - 32px);
+    }
+
+    #progress-preparing {
+      align-self: start;
+      padding-bottom: 20px;
     }
 
     progress::-webkit-progress-bar {
