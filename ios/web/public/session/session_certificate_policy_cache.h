9da52d648c05738a5f71e4f465735d504a3925ab
Sylvain Defresne <sdefresne@chromium.org>
2023-05-22 14:32:58 +0000

    [ios][web] Cleanup SessionCertificatePolicyCache API
    Remove unused import of Foundation framework and remove obsolete
    TODO about UpdateCertificatePolicyCache() as the method is needed
    to implement the session restoration.
    
    Fixed: 1040566
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Change-Id: Id431c1acf05540f24e986d26b9181565f8e92bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543264
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147215}

--- ios/web/public/session/session_certificate_policy_cache.h
+++ ios/web/public/session/session_certificate_policy_cache.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_PUBLIC_SESSION_SESSION_CERTIFICATE_POLICY_CACHE_H_
 #define IOS_WEB_PUBLIC_SESSION_SESSION_CERTIFICATE_POLICY_CACHE_H_
 
-#import <Foundation/Foundation.h>
-
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "net/cert/cert_status_flags.h"
@@ -35,10 +33,8 @@ class SessionCertificatePolicyCache {
   SessionCertificatePolicyCache& operator=(
       const SessionCertificatePolicyCache&) = delete;
 
-  // Transfers the allowed certificate information from this session to `cache`.
-  //
-  // TODO(crbug.com/1040566): Delete this method since
-  // RegisterAllowedCertificate already updates the CertificatePolicyCache.
+  // Transfers the allowed certificate information from this session to the
+  // web::CertificatePolicyCache. Used as part of session restoration.
   virtual void UpdateCertificatePolicyCache() const = 0;
 
   // Stores certificate information that a user has indicated should be allowed
