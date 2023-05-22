52c6877deb13738f7b62c045865e836d70b7d3f6
Abigail Klein <abigailbklein@google.com>
2023-05-22 17:46:45 +0000

    [ReadAnything] ReadAnythingAppController is no longer an AXTreeObserver.
    A small cleanup.
    
    Bug: 1266555
    Change-Id: I125b2086c6040548d187942f5a2fdebf6f2c6d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544675
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147326}

--- chrome/renderer/accessibility/read_anything_app_controller.h
+++ chrome/renderer/accessibility/read_anything_app_controller.h
@@ -59,8 +59,7 @@ class ReadAnythingAppControllerTest;
 //
 class ReadAnythingAppController
     : public gin::Wrappable<ReadAnythingAppController>,
-      public read_anything::mojom::UntrustedPage,
-      public ui::AXTreeObserver {
+      public read_anything::mojom::UntrustedPage {
  public:
   static gin::WrapperInfo kWrapperInfo;
 
