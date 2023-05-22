2a2863547bd4c74e045567bf4073e1bc9aed5a06
Sorin Jianu <sorin@chromium.org>
2023-05-22 13:33:21 +0000

    updater: remove TODO(crbug.com/1376713).
    This bug is marked as a closed duplicate and merged into a different
    bug, which describes the scope of the problem better.
    
    Also, did some mechanical refactoring of nearby code.
    
    Bug: 1376713
    Change-Id: I0bbb7d1dd45b0dcd1d297df4e7991b5e86510acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4550217
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1147190}

--- chrome/updater/installer.cc
+++ chrome/updater/installer.cc
@@ -33,14 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace updater {
 namespace {
 
-// This task joins a process, hence .WithBaseSyncPrimitives().
-// TODO(crbug.com/1376713) - implement a way to express priority for
-// foreground/background installs.
-static constexpr base::TaskTraits kTaskTraitsBlockWithSyncPrimitives = {
-    base::MayBlock(), base::WithBaseSyncPrimitives(),
-    base::TaskPriority::USER_VISIBLE,
-    base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN};
-
 // Returns the full path to the installation directory for the application
 // identified by the |app_id|.
 absl::optional<base::FilePath> GetAppInstallDir(UpdaterScope scope,
@@ -49,7 +41,6 @@ absl::optional<base::FilePath> GetAppInstallDir(UpdaterScope scope,
   if (!app_install_dir) {
     return absl::nullopt;
   }
-
   return app_install_dir->AppendASCII(kAppsDir).AppendASCII(app_id);
 }
 
@@ -61,7 +52,6 @@ AppInfo::AppInfo(const UpdaterScope scope,
                  const base::Version& app_version,
                  const base::FilePath& ecp)
     : scope(scope), app_id(app_id), ap(ap), version(app_version), ecp(ecp) {}
-
 AppInfo::AppInfo(const AppInfo&) = default;
 AppInfo& AppInfo::operator=(const AppInfo&) = default;
 AppInfo::~AppInfo() = default;
@@ -90,9 +80,7 @@ Installer::Installer(
       crx_verifier_format_(crx_verifier_format),
       usage_stats_enabled_(persisted_data->GetUsageStatsEnabled()) {}
 
-Installer::~Installer() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-}
+Installer::~Installer() = default;
 
 update_client::CrxComponent Installer::MakeCrxComponent() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -223,7 +211,10 @@ void Installer::Install(const base::FilePath& unpack_path,
                         ProgressCallback progress_callback,
                         Callback callback) {
   base::ThreadPool::PostTask(
-      FROM_HERE, kTaskTraitsBlockWithSyncPrimitives,
+      FROM_HERE,
+      {base::MayBlock(), base::WithBaseSyncPrimitives(),
+       base::TaskPriority::USER_VISIBLE,
+       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
       base::BindOnce(&Installer::InstallWithSyncPrimitives, this, unpack_path,
                      std::move(install_params), std::move(progress_callback),
                      std::move(callback)));
