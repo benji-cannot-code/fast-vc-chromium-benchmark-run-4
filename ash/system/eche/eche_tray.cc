25c6d4b78b939550e00e7ead6ed589508283f49b
Pu Shi <pushi@google.com>
2023-05-22 21:51:01 +0000

    [Eche] Break up Eche.StreamEvent histogram based on entry points
    This CL breaks up Eche.StreamEvent histogram into smaller histograms
    based on different entry points (notification, launcher and recent apps)
    so that we can have better understanding of success rate of each one.
    
    b/283842969.
    they are recorded into correct buckets.
    
    Low-Coverage-Reason: Created followup bug to improve coverage.
    Tested: initialized app streaming from all entry points and verified
    Change-Id: I74885a53b2294e82a5f04888f84f4f19a51fbbcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549946
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147492}

--- ash/system/eche/eche_tray.cc
+++ ash/system/eche/eche_tray.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/notreached.h"
 #include "base/time/default_tick_clock.h"
 #include "base/time/time.h"
 #include "chromeos/ash/components/multidevice/logging/logging.h"
@@ -622,6 +623,26 @@ void EcheTray::InitBubble(
     base::UmaHistogramEnumeration(
         "Eche.StreamEvent",
         eche_app::mojom::StreamStatus::kStreamStatusInitializing);
+    switch (entry_point) {
+      case eche_app::mojom::AppStreamLaunchEntryPoint::APPS_LIST:
+        base::UmaHistogramEnumeration(
+            "Eche.StreamEvent.FromLauncher",
+            eche_app::mojom::StreamStatus::kStreamStatusInitializing);
+        break;
+      case eche_app::mojom::AppStreamLaunchEntryPoint::NOTIFICATION:
+        base::UmaHistogramEnumeration(
+            "Eche.StreamEvent.FromNotification",
+            eche_app::mojom::StreamStatus::kStreamStatusInitializing);
+        break;
+      case eche_app::mojom::AppStreamLaunchEntryPoint::RECENT_APPS:
+        base::UmaHistogramEnumeration(
+            "Eche.StreamEvent.FromRecentApps",
+            eche_app::mojom::StreamStatus::kStreamStatusInitializing);
+        break;
+      case eche_app::mojom::AppStreamLaunchEntryPoint::UNKNOWN:
+        NOTREACHED();
+        break;
+    }
   }
   init_stream_timestamp_ = base::TimeTicks::Now();
   TrayBubbleView::InitParams init_params;
--- chrome/browser/ash/eche_app/eche_app_manager_factory_unittest.cc
+++ chrome/browser/ash/eche_app/eche_app_manager_factory_unittest.cc
@@ -53,6 +53,8 @@ class EcheAppManagerFactoryTest : public ChromeAshTestBase {
     connection_handler_ = std::make_unique<EcheConnectionStatusHandler>();
     apps_launch_info_provider_ =
         std::make_unique<AppsLaunchInfoProvider>(connection_handler_.get());
+    apps_launch_info_provider_->SetAppLaunchInfo(
+        mojom::AppStreamLaunchEntryPoint::APPS_LIST);
     eche_tray_ = StatusAreaWidgetTestHelper::GetStatusAreaWidget()->eche_tray();
     phone_hub_tray_ =
         StatusAreaWidgetTestHelper::GetStatusAreaWidget()->phone_hub_tray();
@@ -153,6 +155,8 @@ class EcheAppManagerFactoryWithBackgroundTest : public ChromeAshTestBase {
     connection_handler_ = std::make_unique<EcheConnectionStatusHandler>();
     apps_launch_info_provider_ =
         std::make_unique<AppsLaunchInfoProvider>(connection_handler_.get());
+    apps_launch_info_provider_->SetAppLaunchInfo(
+        mojom::AppStreamLaunchEntryPoint::APPS_LIST);
     eche_tray_ = StatusAreaWidgetTestHelper::GetStatusAreaWidget()->eche_tray();
   }
 
