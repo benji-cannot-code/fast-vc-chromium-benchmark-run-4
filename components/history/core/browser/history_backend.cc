be391125a43dc593cd8b803de90bba10ba008be6
Marc Treib <treib@chromium.org>
2023-05-22 10:06:30 +0000

    HistoryBackendObserver::OnVisitUpdated(): Provide update reason
    This CL adds a "reason" parameter to
    HistoryBackendObserver::OnVisitUpdated(). This allows HistorySyncBridge
    to ignore some updates that aren't relevant for it, and thus save some
    unnecessary traffic to the server.
    
    Bug: 1446297
    Change-Id: Ia3156d3645ba7794e368517f82d8eb278e4e1faf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543383
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147122}

--- components/history/core/browser/history_backend.cc
+++ components/history/core/browser/history_backend.cc
@@ -715,7 +715,7 @@ void HistoryBackend::SetPageLanguageForVisitByVisitID(
       annotations.page_language = page_language;
       db_->AddContentAnnotationsForVisit(visit_id, annotations);
     }
-    NotifyVisitUpdated(visit_row);
+    NotifyVisitUpdated(visit_row, VisitUpdateReason::kSetPageLanguage);
     ScheduleCommit();
   }
 }
@@ -752,7 +752,7 @@ void HistoryBackend::SetPasswordStateForVisitByVisitID(
       annotations.password_state = password_state;
       db_->AddContentAnnotationsForVisit(visit_id, annotations);
     }
-    NotifyVisitUpdated(visit_row);
+    NotifyVisitUpdated(visit_row, VisitUpdateReason::kSetPasswordState);
     ScheduleCommit();
   }
 }
@@ -891,7 +891,7 @@ void HistoryBackend::UpdateVisitDuration(VisitID visit_id, const Time end_ts) {
                                    ? end_ts - visit_row.visit_time
                                    : base::Microseconds(0);
     db_->UpdateVisitRow(visit_row);
-    NotifyVisitUpdated(visit_row);
+    NotifyVisitUpdated(visit_row, VisitUpdateReason::kUpdateVisitDuration);
   }
 }
 
@@ -1079,7 +1079,7 @@ void HistoryBackend::AddPage(const HistoryAddPageArgs& request) {
             visit_row.transition = ui::PageTransitionFromInt(
                 visit_row.transition & ~ui::PAGE_TRANSITION_CHAIN_END);
             db_->UpdateVisitRow(visit_row);
-            NotifyVisitUpdated(visit_row);
+            NotifyVisitUpdated(visit_row, VisitUpdateReason::kUpdateTransition);
           }
 
           extended_redirect_chain = GetCachedRecentRedirects(request.referrer);
@@ -1808,7 +1808,7 @@ VisitID HistoryBackend::UpdateSyncedVisit(
                                       content_annotations->password_state);
   }
 
-  NotifyVisitUpdated(updated_row);
+  NotifyVisitUpdated(updated_row, VisitUpdateReason::kUpdateSyncedVisit);
   ScheduleCommit();
   return updated_row.visit_id;
 }
@@ -2148,7 +2148,7 @@ void HistoryBackend::AddContextAnnotationsForVisit(
   if (!db_ || !db_->GetRowForVisit(visit_id, &visit_row))
     return;
   db_->AddContextAnnotationsForVisit(visit_id, visit_context_annotations);
-  NotifyVisitUpdated(visit_row);
+  NotifyVisitUpdated(visit_row, VisitUpdateReason::kAddContextAnnotations);
   ScheduleCommit();
 }
 
@@ -2170,7 +2170,8 @@ void HistoryBackend::SetOnCloseContextAnnotationsForVisit(
   } else {
     db_->AddContextAnnotationsForVisit(visit_id, visit_context_annotations);
   }
-  NotifyVisitUpdated(visit_row);
+  NotifyVisitUpdated(visit_row,
+                     VisitUpdateReason::kSetOnCloseContextAnnotations);
   ScheduleCommit();
 }
 
@@ -3533,9 +3534,10 @@ void HistoryBackend::NotifyURLsDeleted(DeletionInfo deletion_info) {
   delegate_->NotifyURLsDeleted(std::move(deletion_info));
 }
 
-void HistoryBackend::NotifyVisitUpdated(const VisitRow& visit) {
+void HistoryBackend::NotifyVisitUpdated(const VisitRow& visit,
+                                        VisitUpdateReason reason) {
   for (HistoryBackendObserver& observer : observers_) {
-    observer.OnVisitUpdated(visit);
+    observer.OnVisitUpdated(visit, reason);
   }
 }
 
