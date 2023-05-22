b525ff811ad585db660460eec1f659c61409868c
Dana Fried <dfried@chromium.org>
2023-05-22 20:28:38 +0000

    Disable flaky test on Mac
    See attached bug for description and likely cause.
    
    Bug: 1447298
    Change-Id: I9ba63144d0531388de129e97258623d387f0ab5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4550167
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1147445}

--- chrome/test/interaction/webcontents_interaction_test_util_browsertest.cc
+++ chrome/test/interaction/webcontents_interaction_test_util_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/gtest_util.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/resource_coordinator/lifecycle_unit_state.mojom-shared.h"
 #include "chrome/browser/resource_coordinator/tab_manager.h"
@@ -2008,8 +2009,16 @@ class WebContentsInteractionTestUtilInteractiveTest
   }
 };
 
+// TODO(crbug.com/1447298): flaky on Mac - see comments below for the likely
+// culprit line.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_TrackWebContentsAcrossReplace \
+  DISABLED_TrackWebContentsAcrossReplace
+#else
+#define MAYBE_TrackWebContentsAcrossReplace TrackWebContentsAcrossReplace
+#endif
 IN_PROC_BROWSER_TEST_F(WebContentsInteractionTestUtilInteractiveTest,
-                       TrackWebContentsAcrossReplace) {
+                       MAYBE_TrackWebContentsAcrossReplace) {
   const GURL url1 = embedded_test_server()->GetURL(kDocumentWithLinksURL);
   const GURL url2 = embedded_test_server()->GetURL(kEmptyDocumentURL);
   RunTestSequence(InstrumentTab(kWebContentsElementId),
@@ -2027,6 +2036,8 @@ IN_PROC_BROWSER_TEST_F(WebContentsInteractionTestUtilInteractiveTest,
                   })),
                   WaitForHide(kWebContentsElementId), FlushEvents(),
                   // This has to be done on a fresh message loop.
+                  // For some reason, this does not reliably trigger page
+                  // reload on Mac (see crbug.com/1447298).
                   SelectTab(kTabStripElementId, 0),
                   WaitForShow(kWebContentsElementId));
 }
