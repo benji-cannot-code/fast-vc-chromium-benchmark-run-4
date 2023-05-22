c35386f2f74c570ea3b1ea13911890b5d6f80c8e
Florian Leimgruber <fleimgruber@google.com>
2023-05-22 13:30:27 +0000

    Add WebDatabase.SucceededMigrationToVersion metric.
    This complements the existing WebDatabase.FailedMigrationToVersion
    metric and gives us a way to put failures into perspective (e.g. x% of
    migrations to version y failed).
    For every migrated version, either FailedMigrationToVersion or
    SucceededMigrationToVersion is logged with the target database version.
    
    Change-Id: Id1b5ff63a9d87ee3a502a44f28263aaf652140df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545070
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147187}

--- components/webdata/common/web_database.cc
+++ components/webdata/common/web_database.cc
@@ -226,6 +226,9 @@ sql::InitStatus WebDatabase::MigrateOldVersionsAsNeeded() {
         return FailedMigrationTo(next_version);
       }
     }
+    base::UmaHistogramExactLinear("WebDatabase.SucceededMigrationToVersion",
+                                  next_version,
+                                  WebDatabase::kCurrentVersionNumber + 1);
   }
   return sql::INIT_OK;
 }
