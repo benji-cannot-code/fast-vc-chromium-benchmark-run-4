931e35e16c3ff2c60c9ef65438f7a29121085ee4
Markus Handell <handellm@google.com>
2023-05-22 15:35:55 +0000

    Moves VSyncDecoding and WebRtcSendPacketBatch to blink features to prepare for experimentation.
    Also adds field testing configs for them.
    
    Bug: chromium:1439830, chromium:1393053
    Change-Id: Ie5361280684c7c36755fe1d5934350033d7a1868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543407
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147246}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "build/chromecast_buildflags.h"
 #include "media/base/media_switches.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_url.h"
@@ -1095,7 +1096,7 @@ bool RTCPeerConnectionHandler::Initialize(
                                       &configuration_);
 
   configuration_.media_config.video.enable_send_packet_batching =
-      base::FeatureList::IsEnabled(kWebRtcSendPacketBatch);
+      base::FeatureList::IsEnabled(features::kWebRtcSendPacketBatch);
 
   peer_connection_observer_ =
       MakeGarbageCollected<Observer>(weak_factory_.GetWeakPtr(), task_runner_);
