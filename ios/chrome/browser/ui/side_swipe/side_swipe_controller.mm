5b1788f921b973c2c2515ea97d69265f62a55166
Federica Germinario <fedegermi@google.com>
2023-05-22 16:16:13 +0000

    [ios] Avoid exposing internals of SideSwipeController
    Exposing internals of a class in test file is error prone as it is
    invisible to refactoring. This CL creates a +private.h file to fix this
    problem.
    
    Fixed: 1379023
    Change-Id: I478dd44a010aa3a8b0c87fb6a01cc4236b6f5bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542247
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147267}

--- ios/chrome/browser/ui/side_swipe/side_swipe_controller.mm
+++ ios/chrome/browser/ui/side_swipe/side_swipe_controller.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/side_swipe/side_swipe_controller.h"
+#import "ios/chrome/browser/ui/side_swipe/side_swipe_controller+private.h"
 
 #import <memory>
 
@@ -111,10 +112,6 @@ const NSUInteger kIpadGreySwipeTabCount = 8;
 
 // Browser passed on the initializer.
 @property(nonatomic, assign) Browser* browser;
-// Whether to allow navigating from the leading edge.
-@property(nonatomic, assign) BOOL leadingEdgeNavigationEnabled;
-// Whether to allow navigating from the trailing edge.
-@property(nonatomic, assign) BOOL trailingEdgeNavigationEnabled;
 // The current active WebState.
 @property(nonatomic, readonly) web::WebState* activeWebState;
 // The browser state owning the current browser.
--- ios/chrome/browser/ui/side_swipe/side_swipe_controller_unittest.mm
+++ ios/chrome/browser/ui/side_swipe/side_swipe_controller_unittest.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/side_swipe/side_swipe_controller.h"
+#import "ios/chrome/browser/ui/side_swipe/side_swipe_controller+private.h"
 
 #import <WebKit/WebKit.h>
 
@@ -30,12 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-@interface SideSwipeController (ExposedForTesting)
-@property(nonatomic, assign) BOOL leadingEdgeNavigationEnabled;
-@property(nonatomic, assign) BOOL trailingEdgeNavigationEnabled;
-- (void)updateNavigationEdgeSwipeForWebState:(web::WebState*)webState;
-@end
 
 namespace {
 
