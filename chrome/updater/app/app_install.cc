4a31317ef5117e3c27bb508d1b16abc87b95bda9
Sorin Jianu <sorin@chromium.org>
2023-05-22 12:06:59 +0000

    updater: remove TODO related to supporting bundles of apps
    Bug: 1128631
    Change-Id: I369acff19b10c736ae4cc5f5df6ce06e28d8cc96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4550106
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147161}

--- chrome/updater/app/app_install.cc
+++ chrome/updater/app/app_install.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check.h"
-#include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -132,8 +131,7 @@ void AppInstall::FirstTaskRun() {
   const tagging::TagArgs tag_args =
       tag_parsing_result.tag_args.value_or(tagging::TagArgs());
   if (!tag_args.apps.empty()) {
-    // TODO(crbug.com/1128631): support bundles. For now, assume one app.
-    CHECK_EQ(tag_args.apps.size(), size_t{1});
+    // Assume only one app is present since bundles are not supported.
     const tagging::AppArgs& app_args = tag_args.apps.front();
     app_id_ = app_args.app_id;
     app_name_ = app_args.app_name;
