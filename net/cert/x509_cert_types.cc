9fd802121bdcd0a30caf26d221d52e7ddeb3a1b9
Matt Mueller <mattm@chromium.org>
2023-05-22 19:52:31 +0000

    CertPrincipal: remove unused street_addresses and domain_components members
    Change-Id: I7a842122262d07af939e8d60aae22e1f46e24edb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4550197
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147420}

--- net/cert/x509_cert_types.cc
+++ net/cert/x509_cert_types.cc
@@ -63,11 +63,6 @@ bool CertPrincipal::ParseDistinguishedName(
                                                            &country_name)) {
           return false;
         }
-      } else if (name_attribute.type == der::Input(kTypeStreetAddressOid)) {
-        std::string s;
-        if (!name_attribute.ValueAsStringWithUnsafeOptions(string_handling, &s))
-          return false;
-        street_addresses.push_back(s);
       } else if (name_attribute.type == der::Input(kTypeOrganizationNameOid)) {
         std::string s;
         if (!name_attribute.ValueAsStringWithUnsafeOptions(string_handling, &s))
@@ -79,11 +74,6 @@ bool CertPrincipal::ParseDistinguishedName(
         if (!name_attribute.ValueAsStringWithUnsafeOptions(string_handling, &s))
           return false;
         organization_unit_names.push_back(s);
-      } else if (name_attribute.type == der::Input(kTypeDomainComponentOid)) {
-        std::string s;
-        if (!name_attribute.ValueAsStringWithUnsafeOptions(string_handling, &s))
-          return false;
-        domain_components.push_back(s);
       }
     }
   }
