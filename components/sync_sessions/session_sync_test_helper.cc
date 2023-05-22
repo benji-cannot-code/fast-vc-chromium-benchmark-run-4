012a3b91963716f36f4b2a1237ad14b3904fedeb
ginnnnnnny <ginnyhuang@chromium.org>
2023-05-22 17:13:03 +0000

    [iOS] Fixes issue so that SessionSpecifics form factor gets converted
    There used to be an issue that when device type of a foreign session
    is not available, the form_factor gets disregarded when converting to
    SyncedSession. This CL fixed the synced_session_tracker so that form
    factor gets considered even without a device type.
    
    Fixed: 1434959
    Change-Id: I65e51238f1cf6df1f99fe01d68eb13ba1932cf6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454767
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147300}

--- components/sync_sessions/session_sync_test_helper.cc
+++ components/sync_sessions/session_sync_test_helper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "components/sync/protocol/session_specifics.pb.h"
+#include "components/sync_device_info/device_info_proto_enum_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace sync_sessions {
@@ -23,12 +24,11 @@ static const char* kTitle = "title";
 sync_pb::SessionSpecifics
 SessionSyncTestHelper::BuildHeaderSpecificsWithoutWindows(
     const std::string& tag,
-    sync_pb::SyncEnums_DeviceType device_type) {
+    const syncer::DeviceInfo::FormFactor& device_form_factor) {
   sync_pb::SessionSpecifics specifics;
   specifics.set_session_tag(tag);
   sync_pb::SessionHeader* header = specifics.mutable_header();
-  // TODO(crbug.com/1434959): Set device form factor instead.
-  header->set_device_type(device_type);
+  header->set_device_form_factor(ToDeviceFormFactorProto(device_form_factor));
   header->set_client_name(kClientName);
   return specifics;
 }
