c4f0671a3187f008bf385664a06d17b9984abbbd
Osama Fathy <osamafathy@google.com>
2023-05-22 20:14:29 +0000

    [OOBE][CHOOBE] Introduce OobeDisplaySizeFactorDeferred pref
    `OobeDisplaySizeFactorDeferred` preference will be used to store
    the user's selection in the display zoom screen to be applied
    later upon session start.
    
    Bug: b:275555658
    Change-Id: I6fbe3a1d3225d0792af26d2761dd7f507bebd17c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502616
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147436}

--- chrome/browser/ash/login/login_pref_names.h
+++ chrome/browser/ash/login/login_pref_names.h
@@ -38,6 +38,7 @@ extern const char kUrlParameterToAutofillSAMLUsername[];
 extern const char kChoobeSelectedScreens[];
 extern const char kChoobeCompletedScreens[];
 extern const char kOobeDrivePinningEnabledDeferred[];
+extern const char kOobeDisplaySizeFactorDeferred[];
 
 }  // namespace prefs
 }  // namespace ash
--- chrome/browser/ash/login/screens/display_size_screen.cc
+++ chrome/browser/ash/login/screens/display_size_screen.cc
@@ -7,8 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/shell.h"
+#include "chrome/browser/ash/login/login_pref_names.h"
 #include "chrome/browser/ash/login/wizard_context.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/ash/login/display_size_screen_handler.h"
 #include "ui/display/display.h"
 #include "ui/display/manager/display_manager.h"
@@ -104,6 +107,12 @@ void DisplaySizeScreen::HideImpl() {}
 void DisplaySizeScreen::OnUserAction(const base::Value::List& args) {
   const std::string& action_id = args[0].GetString();
   if (action_id == kUserActionNext) {
+    CHECK_EQ(args.size(), 2u);
+    double selected_factor = args[1].GetDouble();
+
+    Profile* profile = ProfileManager::GetActiveUserProfile();
+    profile->GetPrefs()->SetDouble(prefs::kOobeDisplaySizeFactorDeferred,
+                                   selected_factor);
     exit_callback_.Run(Result::kNext);
     return;
   }
