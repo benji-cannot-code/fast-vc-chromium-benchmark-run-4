162eea64e4f56e498433d7395e6fe937bbbebcf5
Tove Petersson <tovep@chromium.org>
2023-05-23 18:37:22 +0000

    Move the ShareThisTabDialog feature to the implementation directory
    Bug: 1428878
    Change-Id: I54b75f86528063a3f19068785433dcad79c118b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4555609
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Auto-Submit: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148055}

--- chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h
+++ chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
 #include "chrome/browser/media/webrtc/desktop_media_picker.h"
@@ -25,6 +26,8 @@ class MdTextButton;
 }  // namespace views
 
 class DesktopMediaPickerViews;
+
+BASE_DECLARE_FEATURE(kShareThisTabDialog);
 
 // Dialog view used for DesktopMediaPickerViews.
 //
