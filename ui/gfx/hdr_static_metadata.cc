b1ef66abdcd08c7772f4b92e9ac7781f90ce8435
Sasha McIntosh <sashamcintosh@chromium.org>
2023-05-23 17:28:53 +0000

    display,gfx,ozone: Add eotfs to HDR output metadata
    Support HDR output metadata for HDR10 and other formats.
    
    BUG=b:217589372
    
    Change-Id: Ic0a51c8a27bfe477d6fdf8cb9fbc02e560bdd988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544550
    Commit-Queue: Sasha McIntosh <sashamcintosh@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148005}

--- ui/gfx/hdr_static_metadata.cc
+++ ui/gfx/hdr_static_metadata.cc
@@ -8,8 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 HDRStaticMetadata::HDRStaticMetadata() = default;
-HDRStaticMetadata::HDRStaticMetadata(double max, double max_avg, double min)
-    : max(max), max_avg(max_avg), min(min) {}
+HDRStaticMetadata::HDRStaticMetadata(double max,
+                                     double max_avg,
+                                     double min,
+                                     uint8_t supported_eotf_mask)
+    : max(max),
+      max_avg(max_avg),
+      min(min),
+      supported_eotf_mask(supported_eotf_mask) {}
 HDRStaticMetadata::HDRStaticMetadata(const HDRStaticMetadata& rhs) = default;
 HDRStaticMetadata& HDRStaticMetadata::operator=(const HDRStaticMetadata& rhs) =
     default;
