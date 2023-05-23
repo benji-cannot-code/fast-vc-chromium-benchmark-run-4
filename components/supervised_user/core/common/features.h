5c545a5f548ba5c21d6531e732db9fa17884933d
James Lee <ljjlee@google.com>
2023-05-23 08:35:44 +0000

    Inform the user if a profile is managed by a parent or guardian
    This implements the first part of the change, on Linux, Mac and Windows:
    * For supervised users without enterprise management, include text
    saying that the browser is managed by the parent (in the app menu, and
    in the settings screen)
    * Include a clickable link to the Family Link page which shows the
    supervised user what settings currently apply to them
    
    A subsequent CL will update the icon used on these surfaces.
    
    See mocks and screenshots in the linked bug.
    
    Bug: b/281641773
    Change-Id: Ib93d26db19c1771dd60394074af0ce14674ccebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518205
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147723}

--- components/supervised_user/core/common/features.h
+++ components/supervised_user/core/common/features.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SUPERVISED_USER_CORE_COMMON_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 
 namespace supervised_user {
 
@@ -23,6 +24,8 @@ BASE_DECLARE_FEATURE(kSynchronousSignInChecking);
 BASE_DECLARE_FEATURE(kEnableSupervisionOnDesktopAndIOS);
 BASE_DECLARE_FEATURE(kFilterWebsitesForSupervisedUsersOnDesktopAndIOS);
 BASE_DECLARE_FEATURE(kEnableExtensionsPermissionsForSupervisedUsersOnDesktop);
+BASE_DECLARE_FEATURE(kEnableManagedByParentUi);
+extern const base::FeatureParam<std::string> kManagedByParentUiMoreInfoUrl;
 
 BASE_DECLARE_FEATURE(kLocalExtensionApprovalsV2);
 
