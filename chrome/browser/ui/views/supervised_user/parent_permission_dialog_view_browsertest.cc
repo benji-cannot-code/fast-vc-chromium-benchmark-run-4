f2630d1eb77e2b532ef4d09dbf15efe27692621b
Aga Wronska <agawronska@chromium.org>
2023-05-23 13:48:03 +0000

    ext approvals: Enable extensions approval v2 on ChromeOS
    Enable the new extensions approval experience for supervised users on
    ChromeOS by default.
    
    Bug: b:283479500
    Change-Id: I32b7e1779b0eaad265869fd104a5d13817074f8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549390
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147837}

--- chrome/browser/ui/views/supervised_user/parent_permission_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/supervised_user/parent_permission_dialog_view_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/metrics/user_action_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ash/login/test/device_state_mixin.h"
 #include "chrome/browser/ash/login/test/logged_in_user_mixin.h"
 #include "chrome/browser/extensions/chrome_test_extension_loader.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/supervised_user/core/browser/supervised_user_service.h"
+#include "components/supervised_user/core/common/features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_launcher.h"
 #include "content/public/test/test_utils.h"
@@ -65,7 +67,12 @@ class ParentPermissionDialogViewTest
   };
 
   ParentPermissionDialogViewTest()
-      : TestParentPermissionDialogViewObserver(this) {}
+      : TestParentPermissionDialogViewObserver(this) {
+    // This UI is only used in V1 extensions approvals flow, so to test it V2
+    // flow needs to be disabled.
+    feature_list_.InitAndDisableFeature(
+        supervised_user::kLocalExtensionApprovalsV2);
+  }
 
   ParentPermissionDialogViewTest(const ParentPermissionDialogViewTest&) =
       delete;
@@ -261,6 +268,8 @@ class ParentPermissionDialogViewTest
       supervised_user_extensions_delegate_;
 
  private:
+  base::test::ScopedFeatureList feature_list_;
+
   raw_ptr<ParentPermissionDialogView, ExperimentalAsh> view_ = nullptr;
   std::unique_ptr<ParentPermissionDialog> parent_permission_dialog_;
   ParentPermissionDialog::Result result_;
