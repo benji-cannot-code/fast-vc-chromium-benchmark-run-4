39551fe2fbe26b0e341ddd3a79fa315d5559c013
Danny Wang <wangdanny@google.com>
2023-05-23 05:48:54 +0000

    Settings Split: Fix the route when navigate to the old page with FF on
    Bug: b/241965700
    Test: test in dut
    Change-Id: Id1284665b34db380fbfc30d2b97adff548bf20a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4553272
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147679}

--- chrome/browser/resources/settings/chromeos/device_page/keyboard.ts
+++ chrome/browser/resources/settings/chromeos/device_page/keyboard.ts
@@ -139,6 +139,17 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
           Setting.kShowDiacritic,
         ]),
       },
+
+      /**
+       * Whether settings should be split per device.
+       */
+      isDeviceSettingsSplitEnabled_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('enableInputDeviceSettingsSplit');
+        },
+        readOnly: true,
+      },
     };
   }
 
@@ -152,6 +163,7 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
   private showExternalMetaKey_: boolean;
   private shouldShowDiacriticSetting_ =
       loadTimeData.getBoolean('allowDiacriticsOnPhysicalKeyboardLongpress');
+  private isDeviceSettingsSplitEnabled_: boolean;
 
   constructor() {
     super();
@@ -173,7 +185,16 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
     if (route !== routes.KEYBOARD) {
       return;
     }
-
+    if (Router.getInstance().currentRoute === routes.KEYBOARD &&
+        this.isDeviceSettingsSplitEnabled_) {
+      // Call setCurrentRoute function to go to the per device keyboard subpage
+      // when the feature flag is turned on. We don't use navigateTo function
+      // since we don't want to navigate back to the previous keyboard subpage.
+      setTimeout(() => {
+        Router.getInstance().setCurrentRoute(
+            routes.PER_DEVICE_KEYBOARD, new URLSearchParams(), false);
+      });
+    }
     this.attemptDeepLink();
   }
 
