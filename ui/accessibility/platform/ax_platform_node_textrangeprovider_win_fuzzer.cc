c18511d7ad096b7cad49f1ce30e222d15d924f53
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2023-05-23 19:39:34 +0000

    [UIA] Fix ITextRangeProvider memory leak
    When SafeArrayPutElement is called with the VARTYPE VT_UNKOWN, it
    calls AddRef() on the item being added to the SafeArray. However,
    because we called AddRef() manually in
    AXPlatformNodeTextRangeProviderWin's ctor, we end up having two
    references to the same object. This leads to a memory leak.
    
    To fix this, we now call Release() on the item right after it got
    added to the SafeArray.
    
    Note: GetVisibleRanges cannot be tested via unit tests for now
    because it relies on the TestAXNodeWrapper which doesn't implement
    the GetBoundsRect function. That's why we added a browser test for
    this function specifically.
    
    Fixed: 1429061
    Change-Id: I25cedbfe8e18d9b420951a0d39c9df511b4b23a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4554261
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1148115}

--- ui/accessibility/platform/ax_platform_node_textrangeprovider_win_fuzzer.cc
+++ ui/accessibility/platform/ax_platform_node_textrangeprovider_win_fuzzer.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/win/atl.h"
 #include "base/win/scoped_bstr.h"
-#include "base/win/scoped_safearray.h"
 #include "base/win/scoped_variant.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node.h"
