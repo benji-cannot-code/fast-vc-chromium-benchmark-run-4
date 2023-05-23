b3a482e178f69ae0afb73d00b52a3c2213bd8f5c
Justin Wells <justinleewells@google.com>
2023-05-23 17:32:22 +0000

    Implement Mandatory Reauth confirmation bubble.
    This bubble is displayed after the user has agreed to enable Mandatory
    Reauth.
    
    Screenshot:
    https://screenshot.googleplex.com/A4Qvsi5r3s3THyD.png
    
    Mockup:
    https://screenshot.googleplex.com/4kPoTf8owipR3vL.png
    
    Bug: 1427216
    Change-Id: I5e2d32596ba08a2cc66e2f6bd1a594fbac91b90c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4499402
    Commit-Queue: Justin Wells <justinleewells@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148009}

--- chrome/browser/ui/views/autofill/autofill_bubble_handler_impl.cc
+++ chrome/browser/ui/views/autofill/autofill_bubble_handler_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/autofill/payments/local_card_migration_bubble_views.h"
 #include "chrome/browser/ui/views/autofill/payments/local_card_migration_icon_view.h"
 #include "chrome/browser/ui/views/autofill/payments/manage_saved_iban_bubble_view.h"
+#include "chrome/browser/ui/views/autofill/payments/mandatory_reauth_confirmation_bubble_view.h"
 #include "chrome/browser/ui/views/autofill/payments/mandatory_reauth_opt_in_bubble_view.h"
 #include "chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views.h"
 #include "chrome/browser/ui/views/autofill/payments/offer_notification_icon_view.h"
@@ -312,7 +313,8 @@ AutofillBubbleBase* AutofillBubbleHandlerImpl::ShowVirtualCardEnrollBubble(
 AutofillBubbleBase* AutofillBubbleHandlerImpl::ShowMandatoryReauthBubble(
     content::WebContents* web_contents,
     MandatoryReauthBubbleController* controller,
-    bool is_user_gesture) {
+    bool is_user_gesture,
+    MandatoryReauthBubbleType bubble_type) {
   PageActionIconView* icon_view =
       toolbar_button_provider_->GetPageActionIconView(
           PageActionIconType::kMandatoryReauth);
@@ -320,16 +322,30 @@ AutofillBubbleBase* AutofillBubbleHandlerImpl::ShowMandatoryReauthBubble(
   views::View* anchor_view = toolbar_button_provider_->GetAnchorView(
       PageActionIconType::kMandatoryReauth);
 
-  MandatoryReauthOptInBubbleView* bubble =
-      new MandatoryReauthOptInBubbleView(anchor_view, web_contents, controller);
-
-  DCHECK(bubble);
-  bubble->SetHighlightedButton(icon_view);
-
-  views::BubbleDialogDelegateView::CreateBubble(bubble);
-  bubble->Show(is_user_gesture ? LocationBarBubbleDelegateView::USER_GESTURE
-                               : LocationBarBubbleDelegateView::AUTOMATIC);
-  return bubble;
+  switch (bubble_type) {
+    case MandatoryReauthBubbleType::kOptIn: {
+      MandatoryReauthOptInBubbleView* bubble =
+          new MandatoryReauthOptInBubbleView(anchor_view, web_contents,
+                                             controller);
+      bubble->SetHighlightedButton(icon_view);
+      views::BubbleDialogDelegateView::CreateBubble(bubble);
+      bubble->Show(is_user_gesture ? LocationBarBubbleDelegateView::USER_GESTURE
+                                   : LocationBarBubbleDelegateView::AUTOMATIC);
+      return bubble;
+    }
+    case MandatoryReauthBubbleType::kConfirmation: {
+      MandatoryReauthConfirmationBubbleView* bubble =
+          new MandatoryReauthConfirmationBubbleView(anchor_view, web_contents,
+                                                    controller);
+      bubble->SetHighlightedButton(icon_view);
+      views::BubbleDialogDelegateView::CreateBubble(bubble);
+      bubble->Show(is_user_gesture ? LocationBarBubbleDelegateView::USER_GESTURE
+                                   : LocationBarBubbleDelegateView::AUTOMATIC);
+      return bubble;
+    }
+    case MandatoryReauthBubbleType::kInactive:
+      NOTREACHED_NORETURN();
+  }
 }
 
 void AutofillBubbleHandlerImpl::OnPasswordSaved() {}
