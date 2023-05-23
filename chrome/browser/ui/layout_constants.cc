6000cc3a07312ece0635ce2ad2cb63ac2ee2438f
conniekxu <conniekxu@chromium.org>
2023-05-23 21:08:30 +0000

    cros_next: Revert window caption bar size changes
    Since the rounded window corner radius updates are not able to meet the
    M116 timeline, revert the window caption bar size changes which were
    originally made for the rounded window. Here's the link for the
    original CL: https://chromium-review.googlesource.com/c/chromium/src/+/4222272
    
    Bug: b/267192501
    Change-Id: Ied6eecda0c71f98f21cb53638ee85a8e4ea7218a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557260
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148166}

--- chrome/browser/ui/layout_constants.cc
+++ chrome/browser/ui/layout_constants.cc
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/insets.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
-#include "chromeos/constants/chromeos_features.h"
-#endif  // IS_CHROMEOS
-
 int GetLayoutConstant(LayoutConstant constant) {
   const bool touch_ui = ui::TouchUiController::Get()->touch_ui();
   switch (constant) {
@@ -78,11 +74,6 @@ int GetLayoutConstant(LayoutConstant constant) {
     case TAB_ALERT_INDICATOR_ICON_WIDTH:
       return touch_ui ? 12 : 16;
     case TAB_HEIGHT:
-#if BUILDFLAG(IS_CHROMEOS)
-      if (chromeos::features::IsJellyrollEnabled()) {
-        return 34 + GetLayoutConstant(TABSTRIP_TOOLBAR_OVERLAP);
-      }
-#endif  // IS_CHROMEOS
       return (touch_ui ? 41 : 34) + GetLayoutConstant(TABSTRIP_TOOLBAR_OVERLAP);
     case TAB_PRE_TITLE_PADDING:
       return 8;
