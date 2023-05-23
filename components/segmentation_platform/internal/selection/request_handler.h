729dba25c7b8648ef00dc78beffc69a21b7234cb
ssid <ssid@chromium.org>
2023-05-23 02:25:40 +0000

    [segmentation] Add support for cached annotated numeric result
    The cached result provider now supports prediction result, so the
    annotated numeric result API can support cached results now.
    
    Refactor code so that request handler does not need to know about post
    processing, and change templates in request dispatcher to use the
    handler. All post processing is done in dispatcher.
    The raw result api is used for all cases and gets converted to
    classification result at a later step.
    
    Change-Id: I0041a5d1253d8a8960885dcccda3a386fb7f4579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4522530
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147621}

--- components/segmentation_platform/internal/selection/request_handler.h
+++ components/segmentation_platform/internal/selection/request_handler.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/scheduler/execution_service.h"
 #include "components/segmentation_platform/internal/selection/segment_result_provider.h"
 #include "components/segmentation_platform/public/input_context.h"
+#include "components/segmentation_platform/public/proto/prediction_result.pb.h"
 #include "components/segmentation_platform/public/result.h"
 
 namespace segmentation_platform {
@@ -33,17 +34,10 @@ class RequestHandler {
       std::unique_ptr<SegmentResultProvider> result_provider,
       ExecutionService* execution_service);
 
-  // Client API. See `SegmentationPlatformService::GetClassificationResult`.
-  virtual void GetClassificationResult(
-      const PredictionOptions& options,
-      scoped_refptr<InputContext> input_context,
-      ClassificationResultCallback callback) = 0;
-
-  // Client API. See SegmentationPlatformService::GetAnnotatedNumericResult`.
-  virtual void GetAnnotatedNumericResult(
-      const PredictionOptions& options,
-      scoped_refptr<InputContext> input_context,
-      AnnotatedNumericResultCallback callback) = 0;
+  // Fetches raw result for on demand executions.
+  virtual void GetPredictionResult(const PredictionOptions& options,
+                                   scoped_refptr<InputContext> input_context,
+                                   RawResultCallback callback) = 0;
 };
 
 }  // namespace segmentation_platform
