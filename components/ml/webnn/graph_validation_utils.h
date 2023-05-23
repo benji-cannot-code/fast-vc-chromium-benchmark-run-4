aea98341087a2779fb70700ec0af816c5f1c4b74
junwei <junwei.fu@intel.com>
2023-05-23 02:51:22 +0000

    WebNN: Move graph validation functions to a component library.
    The WebNN graph is verified in blink side for a computational graph,
    it will throw error messages to developer if the graph is invalid, but
    the content of the graph over mojo is untrustworthy and may come from a
    compromised renderer for the privileged gpu process, so the validation
    layer need to be added before using the graph in WebNNService.
    
    Bug: 1273291
    Change-Id: I1eff4c8cbf78f1e90a7489298c20a49872986383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528431
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147633}

---
+++ components/ml/webnn/graph_validation_utils.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ML_WEBNN_GRAPH_VALIDATION_UTILS_H_
+#define COMPONENTS_ML_WEBNN_GRAPH_VALIDATION_UTILS_H_
+
+#include <vector>
+
+#include "base/containers/span.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace webnn {
+
+absl::optional<size_t> ValidateAndCalculateElementsNumber(
+    base::span<const uint32_t> dimensions,
+    std::string& error_message);
+
+absl::optional<size_t> ValidateAndCalculateByteLength(
+    size_t type_bytes,
+    base::span<const uint32_t> dimensions,
+    std::string& error_message);
+
+// Broadcast the input shapes and return the output shape.
+// If bidirectional is true, its behavior follows the numpy-broadcasting-rule:
+// https://numpy.org/doc/stable/user/basics.broadcasting.html#general-broadcasting-rules.
+// Otherwise, it unidirectionally broadcasts the lhs to the rhs.
+absl::optional<std::vector<uint32_t>> BroadcastShapes(
+    base::span<const uint32_t> dims_lhs,
+    base::span<const uint32_t> dims_rhs,
+    bool bidirectional = true);
+
+}  // namespace webnn
+
+#endif  // COMPONENTS_ML_WEBNN_GRAPH_VALIDATION_UTILS_H_
