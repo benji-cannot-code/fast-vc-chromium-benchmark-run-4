97e1ee4fdefb07581d95e6e92bec30859a9be422
Neil Coronado <nemco@google.com>
2023-05-23 20:02:48 +0000

    Revert "[Empty States] Add empty state illustrations to tab switcher UI"
    This reverts commit e108b3cfa2b15b94d0f968281a34249484bb9123.
    
    Reason for revert: Caused issue where illustration would remain on screen on start surface. See https://crbug.com/1448283.
    
    Original change's description:
    > [Empty States] Add empty state illustrations to tab switcher UI
    >
    > - Add TabListEmptyCoordinator class to initialize empty state resources and use TabModelObserver and TabModelSelectorObserver to decide when to add /remove empty view.
    >     Add empty state view when:
    >     - a. On closing tab and normal tab count is going to be 0.
    >     - b. On toggle tab model selector to normal tab model and normal tab count is 0.
    >     - c On register tab switcher layout, add empty view when recycler is empty in TabListCoordinator.
    >     Remove empty state view when:
    >     - a. Adding a new tab.
    >     - b. Undo a closed tab.
    >     - c. On toggle tab model selector to go from normal tab model with 0 tab to incognito (We never apply empty view to incognito model).
    >     - d. On unregister tab switcher layout.
    >
    > - Apply phone_tab_switcher_empty_state_illustration.xml to icon image view when form factor is phone.
    > - Apply tablet_tab_switcher_empty_state_illustration.xml to icon image view when form factor is tablet.
    >
    > Phone demo: https://drive.google.com/file/d/17deVSRomQUQ1wj5Ml04FTM3tJ0ZXXKZo/view?usp=sharing
    > Tablet demo: https://drive.google.com/file/d/1012QQcwxmBoqx7TUjOun7BlO6z2sP4o6/view?usp=sharing
    >
    > Change-Id: I43b89866ae612a05912b0256790cd538b38417f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4531843
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1147331}
    
    Change-Id: I3e7f2bdbd89951c7e5b849fc41dd36d3b122ef30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557147
    Commit-Queue: Neil Coronado <nemco@google.com>
    Quick-Run: Neil Coronado <nemco@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Neil Coronado <nemco@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1148134}

--- chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceBackButtonTest.java
+++ chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceBackButtonTest.java
@@ -79,8 +79,7 @@ import java.util.concurrent.ExecutionException;
 @UseRunnerDelegate(ChromeJUnit4RunnerDelegate.class)
 @Restriction(
         {UiRestriction.RESTRICTION_TYPE_PHONE, Restriction.RESTRICTION_TYPE_NON_LOW_END_DEVICE})
-@EnableFeatures(
-        {ChromeFeatureList.START_SURFACE_ANDROID + "<Study", ChromeFeatureList.EMPTY_STATES})
+@EnableFeatures({ChromeFeatureList.START_SURFACE_ANDROID + "<Study"})
 @DoNotBatch(reason = "StartSurface*Test tests startup behaviours and thus can't be batched.")
 @CommandLineFlags.
 Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE, "force-fieldtrials=Study/Group"})
@@ -123,6 +122,7 @@ public class StartSurfaceBackButtonTest {
     @Before
     public void setUp() throws IOException {
         StartSurfaceTestUtils.setUpStartSurfaceTests(mImmediateReturn, mActivityTestRule);
+
         mLayoutChangedCallbackHelper = new CallbackHelper();
 
         if (isInstantReturn()) {
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
@@ -31,7 +31,6 @@ import androidx.recyclerview.widget.RecyclerView.OnItemTouchListener;
 
 import org.chromium.base.Callback;
 import org.chromium.base.TraceEvent;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lifecycle.DestroyObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabUtils;
@@ -83,7 +82,6 @@ public class TabListCoordinator
     static final int GRID_LAYOUT_SPAN_COUNT_LARGE = 4;
     static final int MAX_SCREEN_WIDTH_COMPACT_DP = 600;
     static final int MAX_SCREEN_WIDTH_MEDIUM_DP = 800;
-
     private final TabListMediator mMediator;
     private final TabListRecyclerView mRecyclerView;
     private final SimpleRecyclerViewAdapter mAdapter;
@@ -100,7 +98,6 @@ public class TabListCoordinator
     private @Nullable TabStripSnapshotter mTabStripSnapshotter;
     private ItemTouchHelper mItemTouchHelper;
     private OnItemTouchListener mOnItemTouchListener;
-    private TabListEmptyCoordinator mTabListEmptyCoordinator;
 
     /**
      * Construct a coordinator for UI that shows a list of tabs.
@@ -145,7 +142,6 @@ public class TabListCoordinator
         mModel = new TabListModel();
         mAdapter = new SimpleRecyclerViewAdapter(mModel);
         mRootView = rootView;
-
         RecyclerView.RecyclerListener recyclerListener = null;
         if (mMode == TabListMode.GRID || mMode == TabListMode.CAROUSEL) {
             mAdapter.registerType(UiType.SELECTABLE, parent -> {
@@ -223,10 +219,6 @@ public class TabListCoordinator
                 titleProvider, tabListFaviconProvider, actionOnRelatedTabs,
                 selectionDelegateProvider, gridCardOnClickListenerProvider, dialogHandler,
                 priceWelcomeMessageController, componentName, itemType);
-
-        if (ChromeFeatureList.sEmptyStates.isEnabled()) {
-            mTabListEmptyCoordinator = new TabListEmptyCoordinator(rootView, mModel);
-        }
 
         try (TraceEvent e = TraceEvent.scoped("TabListCoordinator.setupRecyclerView")) {
             if (!attachToParent) {
@@ -539,9 +531,6 @@ public class TabListCoordinator
     @Override
     public void onDestroy() {
         mMediator.destroy();
-        if (mTabListEmptyCoordinator != null) {
-            mTabListEmptyCoordinator.destroy();
-        }
         if (mListLayoutListener != null) {
             mRecyclerView.removeOnLayoutChangeListener(mListLayoutListener);
             mLayoutListenerRegistered = false;
