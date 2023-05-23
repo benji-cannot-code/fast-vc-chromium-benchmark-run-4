8a0541a5b1ed7878b90acb14b759aae999a3de2a
Sam Cackett <samcackett@google.com>
2023-05-23 16:33:59 +0000

    Add inset to button SkPaths
    The managed state and channel indicator button paths were artefacting as they weren't drawn with enough space. Adding a small inset allows the path enough room to draw correctly
    
    Before and after image: http://shortn/_smro6msrVX
    
    Bug: b/277268908
    Change-Id: I879083498696ee0d36b53066447939d9920400fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557285
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147968}

--- ash/system/channel_indicator/channel_indicator_quick_settings_view_pixeltest.cc
+++ ash/system/channel_indicator/channel_indicator_quick_settings_view_pixeltest.cc
@@ -114,7 +114,7 @@ TEST_P(ChannelIndicatorQuickSettingsViewPixelTest, FeedbackButtonVisible) {
   // `ChannelIndicatorQuickSettingsView`.
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "feedback_button_visible",
-      /*revision_number=*/2, view()));
+      /*revision_number=*/3, view()));
 }
 
 }  // namespace ash
