53caa3f8a123d4752f897b3a8d6f27f7b6a75bd0
Mike West <mkwst@chromium.org>
2023-05-23 10:42:21 +0000

    Add a flag to make NetworkInformation.{type,downlinkMax} constants.
    This seems like something reasonable to experiment with, given that
    we've shipped Effective Connection Type.
    
    Bug: 1431702
    Change-Id: Iafc065c04db714cc7c80d50da10b62d87a64b082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523100
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147760}

--- third_party/blink/renderer/modules/netinfo/network_information.cc
+++ third_party/blink/renderer/modules/netinfo/network_information.cc
@@ -66,6 +66,10 @@ bool NetworkInformation::IsObserving() const {
 }
 
 String NetworkInformation::type() const {
+  if (RuntimeEnabledFeatures::NetInfoConstantTypeEnabled()) {
+    return ConnectionTypeToString(kWebConnectionTypeUnknown);
+  }
+
   // type_ is only updated when listening for events, so ask
   // networkStateNotifier if not listening (crbug.com/379841).
   if (!IsObserving())
@@ -76,6 +80,10 @@ String NetworkInformation::type() const {
 }
 
 double NetworkInformation::downlinkMax() const {
+  if (RuntimeEnabledFeatures::NetInfoConstantTypeEnabled()) {
+    return std::numeric_limits<double>::infinity();
+  }
+
   if (!IsObserving())
     return GetNetworkStateNotifier().MaxBandwidth();
 
