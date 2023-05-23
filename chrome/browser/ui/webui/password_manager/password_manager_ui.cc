11b06058d3c5fdfd7fff2276d24529faa4d30d31
Yi Gu <yigu@chromium.org>
2023-05-23 15:45:45 +0000

    [FedCM] Update strings in PasswordManager for auto re-authn
    PasswordManager Auto sign-in and FedCM auto re-authn have quite similar
    UX. While we should respect the existing control for auto sign-in in
    PasswordManager, the string needs to be updated to be more generic.
    
    Bug: 1439231
    Change-Id: Id8dfc794b026645fadc7899d95de173ebce6e1c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557084
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147913}

--- chrome/browser/ui/webui/password_manager/password_manager_ui.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui.cc
@@ -104,7 +104,6 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
      IDS_PASSWORD_MANAGER_UI_ALREADY_CHANGED_PASSWORD},
     {"appsLabel", IDS_PASSWORD_MANAGER_UI_APPS_LABEL},
     {"authTimedOut", IDS_PASSWORD_MANAGER_UI_AUTH_TIMED_OUT},
-    {"autosigninDescription", IDS_PASSWORD_MANAGER_UI_AUTOSIGNIN_TOGGLE_DESC},
     {"autosigninLabel", IDS_PASSWORD_MANAGER_UI_AUTOSIGNIN_TOGGLE_LABEL},
     {"backToCheckup", IDS_PASSWORD_MANAGER_UI_BACK_TO_CHECKUP_ARIA_DESCRIPTION},
     {"backToPasswords",
@@ -390,6 +389,10 @@ content::WebUIDataSource* CreateAndAddPasswordsUIHTMLSource(
   source->AddString("addShortcutDescription",
                     InsertBrandedPasswordManager(
                         IDS_PASSWORD_MANAGER_UI_ADD_SHORTCUT_DESCRIPTION));
+
+  source->AddString("autosigninDescription",
+                    InsertBrandedPasswordManager(
+                        IDS_PASSWORD_MANAGER_UI_AUTOSIGNIN_TOGGLE_DESC));
 
   source->AddString(
       "emptyStateImportAccountStore",
