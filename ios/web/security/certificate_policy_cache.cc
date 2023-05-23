a2d5db289fcd63f6b3e597f486d212e2b4491638
Sylvain Defresne <sdefresne@chromium.org>
2023-05-23 10:11:42 +0000

    [ios][web] Cleanup CertificatePolicyCache & CertPolicy API
    Change the API of CertificatePolicyCache & CertPolicy to take the
    X509 certificate as a const pointer (since no mutable method is
    called on the value).
    
    Make all method of CertificatePolicyCache non-virtual since the
    class is never sub-classed (not even in tests).
    
    Bug: 1383087
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Change-Id: Id1e536f0362079a53cd5a4cda74081db14e51f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4539801
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147752}

--- ios/web/security/certificate_policy_cache.cc
+++ ios/web/security/certificate_policy_cache.cc
@@ -14,7 +14,7 @@ CertificatePolicyCache::CertificatePolicyCache() {}
 
 CertificatePolicyCache::~CertificatePolicyCache() {}
 
-void CertificatePolicyCache::AllowCertForHost(net::X509Certificate* cert,
+void CertificatePolicyCache::AllowCertForHost(const net::X509Certificate* cert,
                                               const std::string& host,
                                               net::CertStatus error) {
   DCHECK_CURRENTLY_ON(WebThread::IO);
@@ -22,7 +22,7 @@ void CertificatePolicyCache::AllowCertForHost(net::X509Certificate* cert,
 }
 
 CertPolicy::Judgment CertificatePolicyCache::QueryPolicy(
-    net::X509Certificate* cert,
+    const net::X509Certificate* cert,
     const std::string& host,
     net::CertStatus error) {
   DCHECK_CURRENTLY_ON(WebThread::IO);
