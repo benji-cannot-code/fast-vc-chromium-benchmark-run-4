29f621647d39163f71b192c01b2c11689382963a
Dana Fried <dfried@chromium.org>
2023-05-23 21:27:47 +0000

    Remove deprecated histogram.
    Bug: 1448115
    Change-Id: I29cd5b79c828c34ddd11865134e7f76b5a40dc6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557039
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1148177}

--- components/user_education/common/help_bubble_factory_registry.h
+++ components/user_education/common/help_bubble_factory_registry.h
@@ -26,8 +26,6 @@ namespace user_education {
 // that instances can be created multiple times in a test environment.
 class HelpBubbleFactoryRegistry {
  public:
-  using ToggleFocusCallback = base::RepeatingCallback<void(HelpBubble*)>;
-
   HelpBubbleFactoryRegistry();
   ~HelpBubbleFactoryRegistry();
   HelpBubbleFactoryRegistry(const HelpBubbleFactoryRegistry&) = delete;
@@ -52,10 +50,6 @@ class HelpBubbleFactoryRegistry {
   // bubble or nothing can be focused.
   bool ToggleFocusForAccessibility(ui::ElementContext context);
 
-  // Listens for ToggleFocusForAccessibility() calls for metrics purposes.
-  base::CallbackListSubscription AddToggleFocusCallback(
-      ToggleFocusCallback callback);
-
   // Gets the first visible help bubble in the given context, or null if none
   // exists.
   HelpBubble* GetHelpBubble(ui::ElementContext context);
@@ -75,10 +69,6 @@ class HelpBubbleFactoryRegistry {
 
   // The list of known help bubbles.
   std::map<HelpBubble*, base::CallbackListSubscription> help_bubbles_;
-
-  // For listening
-  base::RepeatingCallbackList<typename ToggleFocusCallback::RunType>
-      toggle_focus_callbacks_;
 };
 
 }  // namespace user_education
