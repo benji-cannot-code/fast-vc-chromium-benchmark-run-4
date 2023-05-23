7917dc6e182c54edc36d8f3ef1839e32e088e82b
Adithya Srinivasan <adithyas@chromium.org>
2023-05-23 15:50:56 +0000

    Prefetch: Use base::circular_deque instead of std::deque
    base/containers/README.md [1] recommends using base::circular_deque
    over std::deque because it has more predictable cross-platform
    behaviour and generates less code.
    
    [1] https://chromium.googlesource.com/chromium/src/+/main/base/containers/README.md#Deque-Usage-advice
    
    Bug: 1445086
    Change-Id: Ic0048a46a6d20e15ded2b86599e06c3a84da9d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546264
    Reviewed-by: Iman Saboori <isaboori@google.com>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147922}

--- content/browser/preloading/prefetch/prefetch_document_manager.h
+++ content/browser/preloading/prefetch/prefetch_document_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/containers/circular_deque.h"
 #include "base/memory/weak_ptr.h"
 #include "content/browser/preloading/prefetch/no_vary_search_helper.h"
 #include "content/browser/preloading/prefetch/prefetch_type.h"
@@ -143,7 +144,8 @@ class CONTENT_EXPORT PrefetchDocumentManager
   size_t number_eager_prefetches_completed_{0};
   // A list of non-eager prefetch requests (from this page) that have completed
   // (oldest to newest).
-  std::deque<base::WeakPtr<PrefetchContainer>> completed_non_eager_prefetches_;
+  base::circular_deque<base::WeakPtr<PrefetchContainer>>
+      completed_non_eager_prefetches_;
 
   // Metrics related to the prefetches requested by this page load.
   PrefetchReferringPageMetrics referring_page_metrics_;
