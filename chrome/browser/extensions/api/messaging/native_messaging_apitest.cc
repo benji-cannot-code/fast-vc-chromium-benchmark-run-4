30fd5427f1996a4ba933cd307534e6687ea19bd3
Eric Lawrence <ericlaw@microsoft.com>
2023-05-23 04:12:03 +0000

    Directly launch Native Host executables on Windows
    Previously, launching an extension's native host executable on Windows
    would launch cmd.exe, setting the input and output pipes to Named Pipes
    used to share JSON messages between the extension and the Host app.
    
    While this worked in most cases, it would fail in cases where:
    
      1) the system disabled access to cmd.exe,
      2) cmd.exe was configured to always elevate,
      3) COMSPEC was pointed at something other than cmd.exe, or
      4) a special character (e.g. &) was in the path to the Native
         Host program.
    
    This CL changes the calling pattern for the common case where the Native
    Host manifest points at an executable. In such cases, Chrome will now
    directly launch the executable and pass file streams to its stdin and
    stdout, avoiding the invocation of cmd.exe altogether.
    
    For legacy compatibility (e.g. a Native Host can be implemented via a
    python script launched by a batch file), the old path using cmd.exe
    remains for use when the Native Host target is not an executable.
    
    This change was previously landed and backed out due to compatibility
    issues with some Native Hosts. It is now behind an off-by-default
    feature flag to enable experimentation and evangelism.
    
    BUG=1445763,335558,387233,387228
    
    Change-Id: I6906f708caeb47a3c3c3248b155b9610426aadb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4522048
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Eric Lawrence <ericlaw@microsoft.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147656}

--- chrome/browser/extensions/api/messaging/native_messaging_apitest.cc
+++ chrome/browser/extensions/api/messaging/native_messaging_apitest.cc
@@ -26,8 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/process_manager.h"
+#include "extensions/common/extension_features.h"
 #include "extensions/test/extension_background_page_waiter.h"
 #include "extensions/test/result_catcher.h"
+
+#if BUILDFLAG(IS_WIN)
+#include <windows.h>
+#endif
 
 namespace extensions {
 namespace {
@@ -61,6 +66,50 @@ IN_PROC_BROWSER_TEST_F(NativeMessagingApiTestBase, UserLevelSendNativeMessage) {
   ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestHost(kUserLevel));
   ASSERT_TRUE(RunExtensionTest("native_messaging_send_native_message"));
 }
+
+#if BUILDFLAG(IS_WIN)
+// On Windows, a new codepath is used to directly launch .EXE-based Native
+// Hosts. This codepath allows launching of Native Hosts even when cmd.exe is
+// disabled, or if the path to the host contains a character that prevents
+// cmd.exe from successfully launching it (e.g. "&" in this test).
+class NativeMessagingLaunchExeTest : public NativeMessagingApiTestBase,
+                                     public testing::WithParamInterface<bool> {
+ public:
+  NativeMessagingLaunchExeTest() {
+    feature_list_.InitWithFeatureState(
+        extensions_features::kLaunchWindowsNativeHostsDirectly,
+        IsDirectLaunchEnabled());
+  }
+
+  bool IsDirectLaunchEnabled() const { return GetParam(); }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+INSTANTIATE_TEST_SUITE_P(NativeMessagingLaunchExe,
+                         NativeMessagingLaunchExeTest,
+                         testing::Bool());
+
+IN_PROC_BROWSER_TEST_P(NativeMessagingLaunchExeTest, SendNativeMessageWinExe) {
+  ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestExeHost(/*user_level=*/false));
+
+  // The extension works properly only if the host launches successfully, which
+  // requires the kLaunchWindowsNativeHostsDirectly feature to be enabled.
+  ASSERT_EQ(IsDirectLaunchEnabled(),
+            RunExtensionTest("native_messaging_send_native_message_exe"));
+}
+
+IN_PROC_BROWSER_TEST_P(NativeMessagingLaunchExeTest,
+                       UserLevelSendNativeMessageWinExe) {
+  ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestExeHost(/*user_level=*/true));
+
+  // The extension works properly only if the host launches successfully, which
+  // requires the kLaunchWindowsNativeHostsDirectly feature to be enabled.
+  ASSERT_EQ(IsDirectLaunchEnabled(),
+            RunExtensionTest("native_messaging_send_native_message_exe"));
+}
+#endif
 
 class NativeMessagingApiTest : public NativeMessagingApiTestBase,
                                public testing::WithParamInterface<ContextType> {
