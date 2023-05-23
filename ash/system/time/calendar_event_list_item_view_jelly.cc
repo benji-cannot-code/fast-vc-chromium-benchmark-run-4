31f170acd7aed2696fcb03f76725f2152e1f64d5
Sam Cackett <samcackett@google.com>
2023-05-23 15:55:30 +0000

    Parameterize test for CalendarJelly
    Bug: b/274455212
    Change-Id: Iea91a452c1617124161cd902ef38b0f01c26ed6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548239
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147928}

--- ash/system/time/calendar_event_list_item_view_jelly.cc
+++ ash/system/time/calendar_event_list_item_view_jelly.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/style_util.h"
 #include "ash/style/typography.h"
 #include "ash/system/model/system_tray_model.h"
-#include "ash/system/time/calendar_metrics.h"
 #include "ash/system/time/calendar_utils.h"
 #include "ash/system/time/calendar_view_controller.h"
 #include "ash/system/time/event_date_formatter_util.h"
