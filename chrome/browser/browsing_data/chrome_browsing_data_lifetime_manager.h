2779751607bdb8622b4b658d15ccf6b29738070e
Salma Elmahallawy <esalma@google.com>
2023-05-23 19:14:19 +0000

    [DataRetentionPolicies] Changing BrowsingDataLifetimeManager Condition
    The BrowsingDataLifetimeManager checks were only checking that
    SyncDisabled policy is set. Following the change of the requirements
    for data retention policies, the condition now checks if browser sign-in
    is disabled or if the sync types that need to be disabled have been
    disabled automatically.
    
    The task for deletion is also scheduled every 30 minutes now instead of 1 hour now to have data live for a less longer time beyond its time to live. Example: data created at t=0.25 will now be deleted at t=1.5 instead of t=2. It used to not be deleted at t=1 (less than its lifetime) and it had to wait till t=2 for the next scheduled task to execute. Now it only lives until t=1.5.
    
    Bug: b/276759837
    Change-Id: I8fc0af7cc1f4936eaefc71d5e43650405784fdec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503675
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148089}

--- chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager.h
+++ chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
+#include "components/sync/base/user_selectable_type.h"
 #include "content/public/browser/browsing_data_remover.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -95,6 +96,12 @@ class ChromeBrowsingDataLifetimeManager : public KeyedService {
   absl::optional<base::Time> end_time_for_testing_;
   base::WeakPtrFactory<ChromeBrowsingDataLifetimeManager> weak_ptr_factory_{
       this};
+
+  // Checks that the conditions needed to clear the browsing data types are
+  // satisfied. 'sync_types' are checked if neither sync nor
+  // browser sign in are disabled.
+  bool IsConditionSatisfiedForBrowsingDataRemoval(
+      const syncer::UserSelectableTypeSet sync_types);
 };
 
 #endif  // CHROME_BROWSER_BROWSING_DATA_CHROME_BROWSING_DATA_LIFETIME_MANAGER_H_
--- chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager_factory.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager_factory.cc
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "components/browsing_data/core/features.h"
 #include "content/public/browser/browser_context.h"
 #include "extensions/buildflags/buildflags.h"
-
 // static
 ChromeBrowsingDataLifetimeManagerFactory*
 ChromeBrowsingDataLifetimeManagerFactory::GetInstance() {
@@ -36,6 +36,7 @@ ChromeBrowsingDataLifetimeManagerFactory::
               .WithGuest(ProfileSelection::kOffTheRecordOnly)
               .Build()) {
   DependsOn(ChromeBrowsingDataRemoverDelegateFactory::GetInstance());
+  DependsOn(SyncServiceFactory::GetInstance());
 }
 
 ChromeBrowsingDataLifetimeManagerFactory::
