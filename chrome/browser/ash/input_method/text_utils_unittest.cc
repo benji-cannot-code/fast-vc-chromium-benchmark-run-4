1a20e2e8b63419cbde26745848b1ad2565aaf94b
Owen Min <zmin@chromium.org>
2023-05-23 15:46:50 +0000

    Revert "multiword: remove invalid utf-8 bytes of the preceding text"
    This reverts commit c38ba384446d058bbf9c541902d2e1563b093f07.
    
    Reason for revert: Test failure after the CL:
    inputs.PhysicalKeyboardMultiwordSuggestion
    
    First failure:
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-chrome/27507/test-results?sortby=&groupby=
    
    
    Original change's description:
    > multiword: remove invalid utf-8 bytes of the preceding text
    >
    > Currently we are using TrimText to get the most recent 100 "characters" (more correctly it is 100 bytes). This will often cause invalid utf-8 characters, for example, if the cutoff is at the middle of an emoji. This causes suggestion problem for Multi-word prediction.
    >
    > This CL ensures that the invalid utf-8 chars are removed at the cutoff position.
    >
    > Change-Id: I12eb93619bd82a1250c742573ce3653a93a79012
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546450
    > Commit-Queue: Chuong Ho <hdchuong@google.com>
    > Reviewed-by: Mehrab N <mehrab@chromium.org>
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1147744}
    
    Change-Id: If638665b59d7802391593e5ae98b2a52a78631e4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557564
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Owen Min <zmin@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147915}

--- chrome/browser/ash/input_method/text_utils_unittest.cc
+++ chrome/browser/ash/input_method/text_utils_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ash/input_method/text_utils.h"
+
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
@@ -104,17 +105,6 @@ TEST(TextUtilsTest, FindCurrentSentence) {
             Sentence(gfx::Range(14, 27), u"That is a cat"));
   EXPECT_EQ(FindCurrentSentence(u"This is a dog\n", 13u),
             Sentence(gfx::Range(0, 13), u"This is a dog"));
-}
-
-TEST(TextUtilsTest, GetCodepointsWithEmoji) {
-  EXPECT_EQ(GetLastNCodepoints("😀😀😀", 2), "😀😀");
-  EXPECT_EQ(GetLastNCodepoints("😁😁Hey", 3), "Hey");
-  EXPECT_EQ(GetLastNCodepoints("😁😁Hey", 4), "😁Hey");
-  EXPECT_EQ(GetLastNCodepoints("Nice job! 😀😀😀", 8), "job! 😀😀😀");
-}
-
-TEST(TextUtilsTest, GetCodepointsWithChineseCharacters) {
-  EXPECT_EQ(GetLastNCodepoints("你好 Rob!", 6), "好 Rob!");
 }
 
 }  // namespace
