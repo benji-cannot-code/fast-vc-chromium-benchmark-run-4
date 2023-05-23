b3c36f9b0731725d29c87624d8502d3a7f51d9d7
Chris Lu <thegreenfrog@chromium.org>
2023-05-23 15:31:24 +0000

    [ios] Remove locationBarBecomesFirstResponder signal to NTPHeader
    Now that NTPViewController drives all fake omnibox focus animations,
    -locationBarBecomesFirstResponder doesn't serve a purpose for the
    NTPHeader. All omnibox focuses not from a fakebox tap (i.e. widget New
    Search) will still go through the NTPViewController.
    
    This change ensures that for every fakebox tap doesn't log
    IOS.Start/NTP.Click twice in -fakeboxTapped.
    
    Change-Id: I81ad1dc08cdacae52c9675779ac9576832dd17ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548769
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147904}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_consumer.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_consumer.h
@@ -19,9 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Exposes view and methods to drive the doodle.
 - (void)setLogoVendor:(id<LogoVendor>)logoVendor;
 
-// Tell location bar has taken focus.
-- (void)locationBarBecomesFirstResponder;
-
 // Sets whether voice search is currently enabled.
 - (void)setVoiceSearchIsEnabled:(BOOL)voiceSearchIsEnabled;
 
