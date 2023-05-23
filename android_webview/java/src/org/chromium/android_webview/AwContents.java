6929c091d109ff8326d1cc6509ed778682b36602
Rakina Zata Amni <rakina@chromium.org>
2023-05-23 21:27:54 +0000

    Add bounds check to AwContents.goBackOrForward()
    Out-of-bounds call this function might cause failing DCHECKs in the
    content/ navigation code, and we want to prevent those. This CL adds
    bounds check for that.
    
    Bug: 1411462
    Change-Id: Ie8fa3fed8dafe2998c9207f698b96c5c9f7f3901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4554558
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148178}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -2551,6 +2551,9 @@ public class AwContents implements SmartClipProvider {
      */
     public void goBackOrForward(int steps) {
         if (TRACE) Log.i(TAG, "%s goBackOrForward=%d", this, steps);
+        if (!canGoBackOrForward(steps)) {
+            return;
+        }
         if (!isDestroyed(WARN)) mNavigationController.goToOffset(steps);
     }
 
