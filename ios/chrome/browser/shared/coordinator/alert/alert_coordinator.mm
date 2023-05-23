2aca8ba23efba7120add24dde894f5885c3c5d5e
Louis Romero <lpromero@google.com>
2023-05-23 21:56:32 +0000

    Add matcher for AlertCoordinator actions
    This CL makes an explicit matcher for actions of an AlertCoordinator.
    
    Bug: 1439184
    Change-Id: I84d8e3238ee77b3999dd877f2828525d2ec66d46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557826
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148201}

--- ios/chrome/browser/shared/coordinator/alert/alert_coordinator.mm
+++ ios/chrome/browser/shared/coordinator/alert/alert_coordinator.mm
@@ -104,7 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                              }];
 
   alertAction.accessibilityIdentifier =
-      [NSString stringWithFormat:@"%@%@", title, @"AlertAction"];
+      [title stringByAppendingString:@"AlertAction"];
   alertAction.enabled = enabled;
 
   [self.alertController addAction:alertAction];
