e01673611f5ce3ffd00e00f008f60389ba1a29dd
Jason Thai <jasontt@chromium.org>
2023-05-23 20:45:05 +0000

    wallpaper: fetch time of day wallpaper variants
    As part of setting time of day wallpapers to be default, this CL adds
    the codes to fetch these wallpapers so they can be called in
    WallpaperController.
    
    BUG=b/283013683
    TEST=ash_unittests
    --gtest_filter="OnlineWallpaperVariantInfoFetcherTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ibf602a72e46ca5ca752f826e0ab5649ae4a2063d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546558
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148150}

--- ash/wallpaper/online_wallpaper_variant_info_fetcher.h
+++ ash/wallpaper/online_wallpaper_variant_info_fetcher.h
@@ -27,7 +27,8 @@ namespace ash {
 
 class WallpaperControllerClient;
 
-// Resolves wallpaper variants from WallpaperInfo for WallpaperController.
+// Resolves wallpaper variants for WallpaperController. These variants can exist
+// from WallpaperInfo or can be fetched from the backdrop server.
 class ASH_EXPORT OnlineWallpaperVariantInfoFetcher {
  public:
   OnlineWallpaperVariantInfoFetcher();
@@ -60,6 +61,13 @@ class ASH_EXPORT OnlineWallpaperVariantInfoFetcher {
                            const WallpaperInfo& info,
                            ScheduleCheckpoint checkpoint,
                            FetchParamsCallback callback);
+
+  // Fetches the time of day wallpaper that has `unit_id` for the user with
+  // `account_id`. Callback is run after the operation completes.
+  void FetchTimeOfDayWallpaper(const AccountId& account_id,
+                               uint64_t unit_id,
+                               ScheduleCheckpoint checkpoint,
+                               FetchParamsCallback callback);
 
  private:
   // An internal representation of the partial information required to construct
@@ -96,6 +104,14 @@ class ASH_EXPORT OnlineWallpaperVariantInfoFetcher {
   void FindAndSetOnlineWallpaperVariants(
       std::unique_ptr<OnlineWallpaperRequest> request,
       const std::string& location,
+      FetchParamsCallback callback,
+      bool success,
+      const std::vector<backdrop::Image>& images);
+
+  // Used as callback when the time of day wallpapers are fetched.
+  void OnTimeOfDayWallpapersFetched(
+      std::unique_ptr<OnlineWallpaperRequest> request,
+      uint64_t unit_id,
       FetchParamsCallback callback,
       bool success,
       const std::vector<backdrop::Image>& images);
