f8878241e8b40abb54843b1a3d6a44a79b417754
Felipe Andrade <fsandrade@chromium.org>
2023-05-23 18:44:07 +0000

    Remember Kerberos password by default
    Change the default value of the "Remember password" checkbox in the
    Kerberos "Add account" dialog. This change doesn't apply to managed
    guest sessions, or to users with "Kerberos remember password" disabled
    by policy.
    
    For more details about this planned change, see the "Additional Change"
    session of go/kerberos-preconfig-dd.
    
    Bug: b:260522530
    Change-Id: Ia9dc11434617751a1af72a3962d23b4fe05a97e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4551086
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148062}

--- chrome/browser/resources/settings/chromeos/kerberos_page/kerberos_add_account_dialog.ts
+++ chrome/browser/resources/settings/chromeos/kerberos_page/kerberos_add_account_dialog.ts
@@ -43,8 +43,8 @@ interface KerberosAddAccountDialogElement {
 }
 
 /**
- * The default placeholder that is shown in the username field
- * of authentication dialog.
+ * The default placeholder that is shown in the username field of the
+ * authentication dialog.
  */
 const DEFAULT_USERNAME_PLACEHOLDER: string = 'user@example.com';
 
@@ -96,11 +96,6 @@ class KerberosAddAccountDialogElement extends
         value: '',
       },
 
-      rememberPassword_: {
-        type: Boolean,
-        value: false,
-      },
-
       generalErrorText_: {
         type: String,
         value: '',
@@ -137,7 +132,17 @@ class KerberosAddAccountDialogElement extends
       },
 
       /**
-       * Whether the remember password options is allowed by policy.
+       * Whether the password should be remembered by default.
+       */
+      rememberPasswordByDefault_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('kerberosRememberPasswordByDefault');
+        },
+      },
+
+      /**
+       * Whether the remember password option is allowed by policy.
        */
       rememberPasswordEnabled_: {
         type: Boolean,
@@ -183,6 +188,7 @@ class KerberosAddAccountDialogElement extends
   private passwordErrorText_: string;
   private password_: string;
   private prefillDomain_: string;
+  private rememberPasswordByDefault_: boolean;
   private rememberPasswordEnabled_: boolean;
   private rememberPassword_: boolean;
   private showAdvancedConfig_: boolean;
@@ -195,6 +201,8 @@ class KerberosAddAccountDialogElement extends
     super();
 
     this.useRememberedPassword_ = false;
+    this.rememberPassword_ = this.rememberPasswordByDefault_ &&
+        this.rememberPasswordEnabled_ && !this.isGuestMode_;
     this.config_ = '';
     this.title_ = '';
     this.actionButtonLabel_ = '';
