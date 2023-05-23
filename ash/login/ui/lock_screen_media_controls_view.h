2d45238204506a26bf277b6ad84e78e017e4844a
Istvan Nagy <iscsi@google.com>
2023-05-23 10:28:03 +0000

    Use ColorId's instead of OnThemeChanged in LockScreenMediaControlsView.
    Bug: b:263227123
    Change-Id: I9454bb9d52c78cd6b5fbb7c207c0f42d65df9f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547646
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147757}

--- ash/login/ui/lock_screen_media_controls_view.h
+++ ash/login/ui/lock_screen_media_controls_view.h
@@ -115,7 +115,6 @@ class ASH_EXPORT LockScreenMediaControlsView
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
   void OnMouseEntered(const ui::MouseEvent& event) override;
   void OnMouseExited(const ui::MouseEvent& event) override;
-  void OnThemeChanged() override;
 
   // media_session::mojom::MediaControllerObserver:
   void MediaSessionInfoChanged(
@@ -206,8 +205,6 @@ class ASH_EXPORT LockScreenMediaControlsView
 
   // Animates |contents_view_| to its original position.
   void RunResetControlsAnimation();
-
-  void UpdateColors();
 
   // Used to control the active session.
   mojo::Remote<media_session::mojom::MediaController> media_controller_remote_;
