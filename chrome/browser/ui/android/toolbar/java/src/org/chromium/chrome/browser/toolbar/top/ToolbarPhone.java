aaae6b4f6632254ebe35996746c172a6848ad0cd
Patrick Noland <pnoland@google.com>
2023-05-23 18:19:42 +0000

    [Omnibox] Fix status bg being shown persistently post scroll
    The call that sets this to true on dropdown scroll isn't guarded, but
    the call to reset on focus loss is guarded. They should all be guarded.
    
    Bug: none
    Change-Id: I5b892f7181d6cc35c38f5e983de23d6f061b9e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557385
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1148046}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2761,8 +2761,11 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
         if (!mShouldShowModernizeVisualUpdate) {
             return;
         }
+
         mDropdownListScrolled = true;
-        mLocationBar.setStatusIconBackgroundVisibility(true);
+        if (!OmniboxFeatures.shouldShowActiveColorOnOmnibox()) {
+            mLocationBar.setStatusIconBackgroundVisibility(true);
+        }
         updateToolbarAndLocationBarColor();
     }
 
@@ -2771,8 +2774,11 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
         if (!mShouldShowModernizeVisualUpdate) {
             return;
         }
+
         mDropdownListScrolled = false;
-        mLocationBar.setStatusIconBackgroundVisibility(false);
+        if (!OmniboxFeatures.shouldShowActiveColorOnOmnibox()) {
+            mLocationBar.setStatusIconBackgroundVisibility(false);
+        }
         updateToolbarAndLocationBarColor();
     }
 
