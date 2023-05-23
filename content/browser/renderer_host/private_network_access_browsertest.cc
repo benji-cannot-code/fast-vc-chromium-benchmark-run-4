e4e035b575431f44bb22939584db796199500b70
Titouan Rigoudy <titouan@chromium.org>
2023-05-23 14:32:34 +0000

    [Private Network Access] Fix TODOs to point to the right bug.
    Bug: chromium:1291252
    Change-Id: I11d22fa74a2229103d020f7097580079074862b5
    Fixed: chromium:1170335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4556387
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147864}

--- content/browser/renderer_host/private_network_access_browsertest.cc
+++ content/browser/renderer_host/private_network_access_browsertest.cc
@@ -2944,7 +2944,7 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessBrowserTest,
       child_frame->BuildClientSecurityState();
   ASSERT_FALSE(security_state.is_null());
 
-  // TODO(https://crbug.com/1170335): Expect `kAllow` here once inheritance is
+  // TODO(https://crbug.com/1291252): Expect `kAllow` here once inheritance is
   // properly implemented.
   EXPECT_EQ(security_state->local_network_request_policy,
             network::mojom::LocalNetworkRequestPolicy::kBlock);
@@ -2962,7 +2962,7 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessBrowserTest,
       child_frame->BuildClientSecurityState();
   ASSERT_FALSE(security_state.is_null());
 
-  // TODO(https://crbug.com/1170335): Expect `kAllow` here once inheritance is
+  // TODO(https://crbug.com/1291252): Expect `kAllow` here once inheritance is
   // properly implemented.
   EXPECT_EQ(security_state->local_network_request_policy,
             network::mojom::LocalNetworkRequestPolicy::kBlock);
@@ -3950,7 +3950,7 @@ IN_PROC_BROWSER_TEST_F(
 // against the parent document's address space. This is incorrect, as the
 // initiator of the navigation is not always the parent document.
 //
-// TODO(https://crbug.com/1170335): Revisit this when the initiator's address
+// TODO(https://crbug.com/1291252): Revisit this when the initiator's address
 // space is used instead.
 //
 // Top-level navigations are never blocked.
