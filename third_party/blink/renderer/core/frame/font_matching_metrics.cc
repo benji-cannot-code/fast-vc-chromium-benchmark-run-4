fdb83d02099d8fedc8b266d9f23332aab14d6e13
Antonio Sartori <antoniosartori@chromium.org>
2023-05-23 04:29:34 +0000

    [Identifiability Study] Report font existence by unique or family name
    We have several ways of reporting fonts as Identifiable Surfaces.
    However, we do not currently have an easy way of reporting which fonts
    are loaded by the page via CSS font-family
    (kLocalFontLookupByUniqueOrFamilyName is the closest thing we have,
    but it has the problem that the surface key is a hash of the whole
    font description, so loading the same font in several sizes causes
    different keys to be reported).
    
    Let's add a simple surface for this.
    
    Change-Id: If9a9b1cb423e8cca4b3f7dc6d57aa4d425ed1cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542965
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147664}

--- third_party/blink/renderer/core/frame/font_matching_metrics.cc
+++ third_party/blink/renderer/core/frame/font_matching_metrics.cc
@@ -51,6 +51,7 @@ bool IdentifiabilityStudyShouldSampleFonts() {
       IdentifiableSurface::Type::kGenericFontLookup,
       IdentifiableSurface::Type::kLocalFontLoadPostScriptName,
       IdentifiableSurface::Type::kLocalFontExistenceByUniqueNameOnly,
+      IdentifiableSurface::Type::kLocalFontExistenceByUniqueOrFamilyName,
   });
 }
 
@@ -98,6 +99,8 @@ void FontMatchingMetrics::ReportSuccessfulFontFamilyMatch(
     return;
   }
   successful_font_families_.insert(font_family_name);
+  ReportLocalFontExistenceByUniqueOrFamilyName(font_family_name,
+                                               /*font_exists=*/true);
 }
 
 void FontMatchingMetrics::ReportFailedFontFamilyMatch(
@@ -106,6 +109,8 @@ void FontMatchingMetrics::ReportFailedFontFamilyMatch(
     return;
   }
   failed_font_families_.insert(font_family_name);
+  ReportLocalFontExistenceByUniqueOrFamilyName(font_family_name,
+                                               /*font_exists=*/false);
 }
 
 void FontMatchingMetrics::ReportSystemFontFamily(
@@ -140,6 +145,21 @@ void FontMatchingMetrics::ReportFailedLocalFontMatch(
   }
   local_fonts_failed_.insert(font_name);
   ReportLocalFontExistenceByUniqueNameOnly(font_name, /*font_exists=*/false);
+}
+
+void FontMatchingMetrics::ReportLocalFontExistenceByUniqueOrFamilyName(
+    const AtomicString& font_name,
+    bool font_exists) {
+  if (font_name.IsNull()) {
+    return;
+  }
+  if (!IdentifiabilityStudySettings::Get()->ShouldSampleType(
+          IdentifiableSurface::Type::kLocalFontExistenceByUniqueOrFamilyName)) {
+    return;
+  }
+  IdentifiableTokenKey input_key(
+      IdentifiabilityBenignCaseFoldingStringToken(font_name));
+  local_font_existence_by_unique_or_family_name_.insert(input_key, font_exists);
 }
 
 void FontMatchingMetrics::ReportLocalFontExistenceByUniqueNameOnly(
@@ -344,6 +364,8 @@ void FontMatchingMetrics::PublishIdentifiabilityMetrics() {
            IdentifiableSurface::Type::kGenericFontLookup},
           {&font_load_postscript_name_,
            IdentifiableSurface::Type::kLocalFontLoadPostScriptName},
+          {&local_font_existence_by_unique_or_family_name_,
+           IdentifiableSurface::Type::kLocalFontExistenceByUniqueOrFamilyName},
           {&local_font_existence_by_unique_name_only_,
            IdentifiableSurface::Type::kLocalFontExistenceByUniqueNameOnly},
       };
