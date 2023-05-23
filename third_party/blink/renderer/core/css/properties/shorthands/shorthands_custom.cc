e8de61123fb478bbecdaf89c348536c0f833ae85
Daniil Sakhapov <sakhapov@chromium.org>
2023-05-23 18:45:13 +0000

    Revert "Fix offset shorthand serialization"
    This reverts commit 2c56e64661bfc4d13ae677f6a927525e9c13b614.
    
    Reason for revert: Crash
    
    Original change's description:
    > Fix offset shorthand serialization
    >
    > Change-Id: I0567385dec274d01c0e9ca6c19823485e5fe129c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545287
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1147191}
    
    Change-Id: Ic4c568715acb44c2366d14f2d81ba3a4b2454093
    Bug: 1448040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4556426
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148064}

--- third_party/blink/renderer/core/css/properties/shorthands/shorthands_custom.cc
+++ third_party/blink/renderer/core/css/properties/shorthands/shorthands_custom.cc
@@ -2656,7 +2656,7 @@ bool Offset::ParseShorthand(
   } else if (RuntimeEnabledFeatures::CSSOffsetPositionAnchorEnabled()) {
     css_parsing_utils::AddProperty(
         CSSPropertyID::kOffsetPosition, CSSPropertyID::kOffset,
-        *CSSIdentifierValue::Create(CSSValueID::kAuto), important,
+        *CSSInitialValue::Create(), important,
         css_parsing_utils::IsImplicitProperty::kNotImplicit, properties);
   }
 
@@ -2668,7 +2668,7 @@ bool Offset::ParseShorthand(
   } else {
     css_parsing_utils::AddProperty(
         CSSPropertyID::kOffsetPath, CSSPropertyID::kOffset,
-        *CSSIdentifierValue::Create(CSSValueID::kNone), important,
+        *CSSInitialValue::Create(), important,
         css_parsing_utils::IsImplicitProperty::kNotImplicit, properties);
   }
 
@@ -2680,10 +2680,8 @@ bool Offset::ParseShorthand(
   } else {
     css_parsing_utils::AddProperty(
         CSSPropertyID::kOffsetDistance, CSSPropertyID::kOffset,
-        *CSSNumericLiteralValue::Create(0,
-                                        CSSPrimitiveValue::UnitType::kPixels),
-        important, css_parsing_utils::IsImplicitProperty::kNotImplicit,
-        properties);
+        *CSSInitialValue::Create(), important,
+        css_parsing_utils::IsImplicitProperty::kNotImplicit, properties);
   }
 
   if (offset_rotate) {
@@ -2694,7 +2692,7 @@ bool Offset::ParseShorthand(
   } else {
     css_parsing_utils::AddProperty(
         CSSPropertyID::kOffsetRotate, CSSPropertyID::kOffset,
-        *CSSIdentifierValue::Create(CSSValueID::kAuto), important,
+        *CSSInitialValue::Create(), important,
         css_parsing_utils::IsImplicitProperty::kNotImplicit, properties);
   }
 
@@ -2706,7 +2704,7 @@ bool Offset::ParseShorthand(
   } else if (RuntimeEnabledFeatures::CSSOffsetPositionAnchorEnabled()) {
     css_parsing_utils::AddProperty(
         CSSPropertyID::kOffsetAnchor, CSSPropertyID::kOffset,
-        *CSSIdentifierValue::Create(CSSValueID::kAuto), important,
+        *CSSInitialValue::Create(), important,
         css_parsing_utils::IsImplicitProperty::kNotImplicit, properties);
   }
 
