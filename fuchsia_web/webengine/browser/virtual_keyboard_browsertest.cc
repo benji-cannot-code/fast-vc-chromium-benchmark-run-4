5e3f1f2f604046897158d4fe250c1b805c8ecb5e
David Song <wintermelons@google.com>
2023-05-23 22:01:53 +0000

    [fuchsia] Switch browsertests to Flatland
    Switch Browsertests to run with the Flatland TestUiStack, and remove
    scenic hardcoding in WebEngineTestLauncher. WebEngineBrowserMainParts
    picks the appropriate Ozone platform based on
    fuchsia.ui.scenic/Scenic.UseFlatland. Test configurations that cannot
    use Scenic or Flatland should use Headless instead with
    --ozone-platform=headless. See crrev.com/c/4001000 for example.
    
    * web_engine_browsertests now runs with the Flatland TestUiStack.
    * cast_runner_browsertests now runs with the same Flatland TestUiStack
      to detect Scenic.
    * FrameImplTest.VisibilityState is migrated to CreateView2 because
      GraphicalPresenter no longer accepts GFX parameters.
    * VirtualKeyboardTest and KeyboardInputTest are migrated to CreateView2
      because they assert the KOID of the ViewRef.
    * VirtualKeyboardTest and KeyboardInputTest are skipped when ran with
      headless Ozone because keyboard events are ignored.
    
    Tests can continue to use Frame.CreateView and
    Frame.CreateViewWithViewRef for now, since GFX APIs are still present.
    Future CLs will migrate these to CreateView2.
    
    
    Bug: 1300993
    Change-Id: I9655bf1ec5185cf6949e21b89db54490ef28e58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4371128
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Song <wintermelons@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148204}

--- fuchsia_web/webengine/browser/virtual_keyboard_browsertest.cc
+++ fuchsia_web/webengine/browser/virtual_keyboard_browsertest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia_web/webengine/test/test_data.h"
 #include "fuchsia_web/webengine/test/web_engine_browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/ozone/public/ozone_platform.h"
 
 namespace virtualkeyboard = fuchsia_input_virtualkeyboard;
 
@@ -54,6 +55,10 @@ class VirtualKeyboardTest : public WebEngineBrowserTest {
   ~VirtualKeyboardTest() override = default;
 
   void SetUp() override {
+    if (ui::OzonePlatform::GetPlatformNameForTest() == "headless") {
+      GTEST_SKIP() << "Keyboard inputs are ignored in headless mode.";
+    }
+
     scoped_feature_list_.InitWithFeatures(
         {features::kVirtualKeyboard, features::kKeyboardInput}, {});
     WebEngineBrowserTest::SetUp();
