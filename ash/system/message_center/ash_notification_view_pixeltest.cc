25c28ee5b58673bee74cc23b5163bb0008eb79f2
Sylvie Liu <sylvieliu@chromium.org>
2023-05-24 23:37:32 +0000

    Fixed failed unittest and pixel test for kQsRevamp
    This CL fixed SnoopingProtectionNotificationBlockerTest by setting
    kQsRevamp to disabled since the MessageCenterBubble doesn't exist for
    QsRevamp. Also fixed ScreenCaptureNotificationPixelTest by parameterized
    it with QsRevamp.
    
    Bug: b:283845801
    Change-Id: Id86f687f470cec2d45dc7e655c0d457e17e239cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4559768
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148834}

--- ash/system/message_center/ash_notification_view_pixeltest.cc
+++ ash/system/message_center/ash_notification_view_pixeltest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "ui/base/models/image_model.h"
 #include "ui/message_center/views/message_popup_view.h"
 #include "ui/message_center/views/message_view.h"
@@ -212,14 +213,23 @@ TEST_P(AshNotificationViewTitlePixelTest, NotificationTitleTest) {
 
 class ScreenCaptureNotificationPixelTest
     : public AshNotificationViewPixelTestBase,
-      public testing::WithParamInterface<DisplayType> {
+      public testing::WithParamInterface<
+          std::tuple<bool /*IsQsRevampEnabled()*/, DisplayType>> {
  public:
   // AshNotificationViewPixelTestBase:
   void SetUp() override {
+    std::vector<base::test::FeatureRef> features = {features::kQsRevamp,
+                                                    chromeos::features::kJelly};
+    std::vector<base::test::FeatureRef> enabled_features;
+    std::vector<base::test::FeatureRef> disabled_features;
+    (std::get<0>(GetParam()) ? enabled_features : disabled_features)
+        .swap(features);
+    scoped_feature_list_.InitWithFeatures(enabled_features, disabled_features);
+
     AshNotificationViewPixelTestBase::SetUp();
 
     // Change the display size depending on the test param.
-    switch (GetParam()) {
+    switch (GetDisplayType()) {
       case DisplayType::kNormal:
         break;
       case DisplayType::kUltraWidth:
@@ -244,16 +254,23 @@ class ScreenCaptureNotificationPixelTest
     AshNotificationViewPixelTestBase::TearDown();
   }
 
+  bool IsQsRevampEnabled() const { return std::get<0>(GetParam()); }
+
+  const DisplayType& GetDisplayType() const { return std::get<1>(GetParam()); }
+
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<aura::Window> window1_;
   std::unique_ptr<aura::Window> window2_;
 };
 
-INSTANTIATE_TEST_SUITE_P(DisplaySize,
-                         ScreenCaptureNotificationPixelTest,
-                         testing::ValuesIn({DisplayType::kNormal,
-                                            DisplayType::kUltraWidth,
-                                            DisplayType::kUltraHeight}));
+INSTANTIATE_TEST_SUITE_P(
+    DisplaySize,
+    ScreenCaptureNotificationPixelTest,
+    testing::Combine(/*IsQsRevampEnabled()=*/testing::Bool(),
+                     testing::ValuesIn({DisplayType::kNormal,
+                                        DisplayType::kUltraWidth,
+                                        DisplayType::kUltraHeight})));
 
 // Verifies the notification popup of a full screenshot.
 TEST_P(ScreenCaptureNotificationPixelTest, VerifyPopup) {
@@ -271,7 +288,7 @@ TEST_P(ScreenCaptureNotificationPixelTest, VerifyPopup) {
   // Get the notification view.
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       base::StrCat({"screen_capture_popup_notification_",
-                    GetDisplayTypeName(GetParam())}),
+                    GetDisplayTypeName(GetDisplayType())}),
       /*revision_number=*/1,
       test_api()->GetPopupViewForId(kScreenCaptureNotificationId)));
 }
