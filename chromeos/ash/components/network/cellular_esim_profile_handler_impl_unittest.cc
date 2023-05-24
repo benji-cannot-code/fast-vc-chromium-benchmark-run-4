dc9cbd629b9d5ab56b13f291f041008c40340865
Chad Duffin <chadduffin@chromium.org>
2023-05-24 21:58:28 +0000

    cellular: Update SM-DS flag dependency order
    This change updates the flag dependency order for the SmdsSupport
    feature flag.
    
    Bug: N/A
    Test: N/A
    Change-Id: I298ce573666fa5ef6d4a122ea3787212dad23f66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549502
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148781}

--- chromeos/ash/components/network/cellular_esim_profile_handler_impl_unittest.cc
+++ chromeos/ash/components/network/cellular_esim_profile_handler_impl_unittest.cc
@@ -319,7 +319,8 @@ class CellularESimProfileHandlerImplTest_SmdsSupportEnabled
   CellularESimProfileHandlerImplTest_SmdsSupportEnabled()
       : CellularESimProfileHandlerImplTest(
             /*enabled_features=*/{ash::features::kSmdsDbusMigration,
-                                  ash::features::kSmdsSupport},
+                                  ash::features::kSmdsSupport,
+                                  ash::features::kSmdsSupportEuiccUpload},
             /*disabled_features=*/{}) {}
   ~CellularESimProfileHandlerImplTest_SmdsSupportEnabled() override = default;
 };
@@ -339,6 +340,7 @@ class CellularESimProfileHandlerImplTest_SmdsSupportAndStorkEnabled
       : CellularESimProfileHandlerImplTest(
             /*enabled_features=*/{ash::features::kSmdsDbusMigration,
                                   ash::features::kSmdsSupport,
+                                  ash::features::kSmdsSupportEuiccUpload,
                                   ash::features::kUseStorkSmdsServerAddress},
             /*disabled_features=*/{}) {}
   ~CellularESimProfileHandlerImplTest_SmdsSupportAndStorkEnabled() override =
