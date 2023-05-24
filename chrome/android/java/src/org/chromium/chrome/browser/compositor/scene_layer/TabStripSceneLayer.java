9f8deb34edb15504db52bb51e6109c2974e7c98d
Zhe Li <zheliooo@google.com>
2023-05-24 20:29:13 +0000

    [Tab Strip Redesign] Add param to disable button style
    Param(enable remove button style) currently includes remove button bg color and disable btn anchor. Alternatively we can make disable btn anchor as a new feature flag if we want to experiment with these two things individually.
    
    Enabled Detached Remove Button Style: https://drive.google.com/file/d/1-buSoWLsYeWZSG7VW6xhUgqGrrXQ78GW/view?usp=sharing
    Enabled Folio Remove Button Style: https://drive.google.com/file/d/1-_-o-HyhsxYdevuKeLZmIvlm8JZ438hf/view?usp=sharing
    
    Bug: 1447109
    Change-Id: I062beec1486c2f6b29086beabe78b7180d4f15ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545166
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148744}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
@@ -20,6 +20,7 @@ import org.chromium.chrome.browser.compositor.overlays.strip.StripLayoutTab;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.layouts.scene_layer.SceneLayer;
 import org.chromium.chrome.browser.layouts.scene_layer.SceneOverlayLayer;
+import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
 import org.chromium.ui.base.LocalizationUtils;
 import org.chromium.ui.resources.ResourceManager;
 
@@ -45,8 +46,9 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
     @Override
     protected void initializeNative() {
         if (mNativePtr == 0) {
-            mNativePtr = TabStripSceneLayerJni.get().init(
-                    TabStripSceneLayer.this, ChromeFeatureList.sTabStripRedesign.isEnabled());
+            mNativePtr = TabStripSceneLayerJni.get().init(TabStripSceneLayer.this,
+                    ChromeFeatureList.sTabStripRedesign.isEnabled(),
+                    TabUiFeatureUtilities.isTabStripButtonStyleDisabled());
         }
         // Set flag for testing
         if (!sTestFlag) {
@@ -178,7 +180,8 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
 
     @NativeMethods
     public interface Natives {
-        long init(TabStripSceneLayer caller, boolean isTabStripRedesignEnabled);
+        long init(TabStripSceneLayer caller, boolean isTabStripRedesignEnabled,
+                boolean isTsrButtonStyleDisabled);
         void beginBuildingFrame(
                 long nativeTabStripSceneLayer, TabStripSceneLayer caller, boolean visible);
         void finishBuildingFrame(long nativeTabStripSceneLayer, TabStripSceneLayer caller);
