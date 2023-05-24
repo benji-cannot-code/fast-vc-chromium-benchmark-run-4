86ac6d74c38ea1b00674498f9cb0d4b2948dc7f8
sohanjg <sohanjg@chromium.org>
2023-05-24 03:06:06 +0000

    cc: Plumb raster capabilities
    This introduces new raster struct for easier maintaining and access of the
    raster settings across cc/workers and gpu.
    
    Bug: 1442970
    Change-Id: I607436be625e61ec94bc62858832cffd22ce2675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547502
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Sohan Jyoti Ghosh <sohanjg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148324}

---
+++ cc/trees/raster_capabilities.h
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CC_TREES_RASTER_CAPABILITIES_H_
+#define CC_TREES_RASTER_CAPABILITIES_H_
+
+#include "cc/cc_export.h"
+
+namespace cc {
+
+enum class GpuRasterizationStatus {
+  ON,
+  OFF_FORCED,
+  OFF_DEVICE,
+};
+
+struct CC_EXPORT RasterCapabilities {
+  RasterCapabilities() = default;
+  RasterCapabilities(const RasterCapabilities& other) = delete;
+  RasterCapabilities& operator=(const RasterCapabilities& other) = delete;
+  ~RasterCapabilities() = default;
+
+  bool can_use_msaa = false;
+  bool supports_disable_msaa = false;
+
+  bool need_update_gpu_rasterization_status = false;
+  bool use_gpu_rasterization = false;
+  GpuRasterizationStatus gpu_rasterization_status =
+      GpuRasterizationStatus::OFF_DEVICE;
+  // The maximum size (either width or height) that any texture can be. Also
+  // holds a reasonable value for software compositing bitmaps.
+  int max_texture_size = 0;
+  bool use_dmsaa_for_tiles = false;
+};
+}  // namespace cc
+
+#endif  // CC_TREES_RASTER_CAPABILITIES_H_
