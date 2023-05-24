cddd61a9c1ccf6e3c54766e8fb1c3060cd8b5405
Keren Zhu <kerenzhu@chromium.org>
2023-05-24 16:41:52 +0000

    [CR2023] Use Tonal button for the manage button in Page Action bubble
    Bug: 1422128
    Change-Id: Ide36eea5c2a8d801cc14c8e73f2ce2d269e9e626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4559324
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1148580}

--- chrome/browser/ui/views/content_setting_bubble_contents.cc
+++ chrome/browser/ui/views/content_setting_bubble_contents.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/combobox_model.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
 #include "ui/gfx/color_utils.h"
@@ -644,6 +645,9 @@ ContentSettingBubbleContents::CreateHelpAndManageView() {
     manage_button->SetMinSize(gfx::Size(
         layout->GetDistanceMetric(views::DISTANCE_DIALOG_BUTTON_MINIMUM_WIDTH),
         0));
+    if (features::IsChromeRefresh2023()) {
+      manage_button->SetStyle(ui::ButtonStyle::kTonal);
+    }
     manage_button_ = manage_button.get();
     extra_views.push_back(std::move(manage_button));
   }
