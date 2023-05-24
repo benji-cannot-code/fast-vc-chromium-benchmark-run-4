f4350bdbe124f400abf604b30329a8fc7e75019e
Vipul Vinod Koul <koulvipul@google.com>
2023-05-24 00:49:55 +0000

    Update the memory fix solution for payments DeviceAuthenticator.
    This updating the changes implemented in this CL based on
    Dominic's suggestion- https://chromium-review.googlesource.com/c/chromium/src/+/4535366
    
    Bug: 1445789
    Change-Id: I9ad6f7cf872cdee6bf79b10bbd6a136f4c3ae444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4553517
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148288}

--- chrome/browser/extensions/api/autofill_private/autofill_private_api.h
+++ chrome/browser/extensions/api/autofill_private/autofill_private_api.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_function.h"
 #include "extensions/browser/extension_function_histogram_value.h"
 
-namespace device_reauth {
-class DeviceAuthenticator;
-}
-
 namespace extensions {
 class AutofillPrivateGetAccountInfoFunction : public ExtensionFunction {
  public:
@@ -345,7 +341,7 @@ class AutofillPrivateRemoveVirtualCardFunction : public ExtensionFunction {
 class AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction
     : public ExtensionFunction {
  public:
-  AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction();
+  AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction() = default;
   AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction(
       const AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction&) =
       delete;
@@ -357,22 +353,14 @@ class AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction
       AUTOFILLPRIVATE_AUTHENTICATEUSERANDFLIPMANDATORYAUTHTOGGLE)
 
  protected:
-  ~AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction() override;
+  ~AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction()
+      override = default;
 
   // ExtensionFunction overrides.
   ResponseAction Run() override;
 
  private:
   void UpdateMandatoryAuthTogglePref(bool reauth_succeeded);
-
-  // Callback to reset `device_authenticator_` after auth is completed.
-  void OnReauthCompleted();
-
-  // Reference pointer to prevent `device_authenticator` from getting
-  // dereferenced before the auth is completed.
-  // `device_authenticator_` is used to access the OS level biometric auth. If
-  // not available, it uses device password/pin auth as the fallback.
-  scoped_refptr<device_reauth::DeviceAuthenticator> device_authenticator_;
 };
 
 class AutofillPrivateAuthenticateUserToEditLocalCardFunction
