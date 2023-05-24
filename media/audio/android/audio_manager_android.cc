7f833f6266309246fbdc25ddf8159a1cf1ce1670
Thomas Guilbert <tguilbert@chromium.org>
2023-05-24 01:49:04 +0000

    Add AAudio input support
    This CL adds an input driver for Android built on top of the AAudio
    library.
    
    It is very similar to AAudioOutputStream, with the exception that
    device changes are handled differently: AAudioInputStream automatically
    recreates a new AAudioStreamWrapper under the hood, whilst the
    AAudioOutputStream surfaces a device change error which destroys and
    recreates the whole output stream.
    
    The feature is gated behind the "UseAAudioInput" flag.
    
    Bug: 1439668
    Change-Id: Ic06d8d8cad8868ed9f52886d00c2052b16e4b670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547495
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148310}

--- media/audio/android/audio_manager_android.cc
+++ media/audio/android/audio_manager_android.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
+#include "media/audio/android/aaudio_input.h"
 #include "media/audio/android/aaudio_output.h"
 #include "media/audio/android/aaudio_stubs.h"
 #include "media/audio/android/audio_track_output_stream.h"
@@ -257,8 +258,9 @@ AudioOutputStream* AudioManagerAndroid::MakeLinearOutputStream(
   DCHECK_EQ(AudioParameters::AUDIO_PCM_LINEAR, params.format());
   DCHECK(GetTaskRunner()->BelongsToCurrentThread());
 
-  if (UseAAudio())
+  if (UseAAudioOutput()) {
     return new AAudioOutputStream(this, params, AAUDIO_USAGE_MEDIA);
+  }
 
   return new OpenSLESOutputStream(this, params, SL_ANDROID_STREAM_MEDIA);
 }
@@ -269,7 +271,7 @@ AudioOutputStream* AudioManagerAndroid::MakeLowLatencyOutputStream(
     const LogCallback& log_callback) {
   DCHECK_EQ(AudioParameters::AUDIO_PCM_LOW_LATENCY, params.format());
 
-  if (UseAAudio()) {
+  if (UseAAudioOutput()) {
     const aaudio_usage_t usage = communication_mode_is_on_
                                      ? AAUDIO_USAGE_VOICE_COMMUNICATION
                                      : AAUDIO_USAGE_MEDIA;
@@ -300,6 +302,11 @@ AudioInputStream* AudioManagerAndroid::MakeLinearInputStream(
   // needs it.
   DLOG_IF(ERROR, !device_id.empty()) << "Not implemented!";
   DCHECK_EQ(AudioParameters::AUDIO_PCM_LINEAR, params.format());
+
+  if (UseAAudioInput()) {
+    return new AAudioInputStream(this, params);
+  }
+
   return new OpenSLESInputStream(this, params);
 }
 
@@ -319,6 +326,10 @@ AudioInputStream* AudioManagerAndroid::MakeLowLatencyInputStream(
   if (!SetAudioDevice(device_id)) {
     LOG(ERROR) << "Unable to select audio device!";
     return NULL;
+  }
+
+  if (UseAAudioInput()) {
+    return new AAudioInputStream(this, params);
   }
 
   // Create a new audio input stream and enable or disable all audio effects
@@ -518,9 +529,6 @@ void AudioManagerAndroid::DoSetVolumeOnAudioThread(double volume) {
 }
 
 bool AudioManagerAndroid::UseAAudio() {
-  if (!base::FeatureList::IsEnabled(features::kUseAAudioDriver))
-    return false;
-
   if (base::android::BuildInfo::GetInstance()->sdk_int() <
       base::android::SDK_VERSION_Q) {
     // We need APIs that weren't added until API Level 28. Also, AAudio crashes
@@ -528,10 +536,27 @@ bool AudioManagerAndroid::UseAAudio() {
     return false;
   }
 
-  if (!is_aaudio_available_.has_value())
+  if (!is_aaudio_available_.has_value()) {
     is_aaudio_available_ = InitAAudio();
+  }
 
   return is_aaudio_available_.value();
+}
+
+bool AudioManagerAndroid::UseAAudioOutput() {
+  if (!base::FeatureList::IsEnabled(features::kUseAAudioDriver)) {
+    return false;
+  }
+
+  return UseAAudio();
+}
+
+bool AudioManagerAndroid::UseAAudioInput() {
+  if (!base::FeatureList::IsEnabled(features::kUseAAudioInput)) {
+    return false;
+  }
+
+  return UseAAudio();
 }
 
 }  // namespace media
