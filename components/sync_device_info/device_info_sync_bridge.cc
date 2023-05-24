94ad7a63b9b0b8d301ee19bb855179c6c621f526
Rushan Suleymanov <rushans@google.com>
2023-05-24 07:53:40 +0000

    [Sync] Remove LocalDeviceInfoDeletionReuploaded histogram
    Fixed: 1447623
    Change-Id: Ieef65bc0f49ac8c75f1c2c4c5f2bd5b85ecd953d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4558424
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Quick-Run: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148388}

--- components/sync_device_info/device_info_sync_bridge.cc
+++ components/sync_device_info/device_info_sync_bridge.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
 #include "components/sync/base/model_type.h"
@@ -485,14 +484,9 @@ absl::optional<ModelError> DeviceInfoSyncBridge::ApplyIncrementalSyncChanges(
     device_info_synced_callback_list_.clear();
   }
 
-  if (has_tombstone_for_local_device) {
-    const bool should_reupload_device_info = !reuploaded_on_tombstone_;
-    base::UmaHistogramBoolean("Sync.LocalDeviceInfoDeletionReuploaded",
-                              should_reupload_device_info);
-    if (should_reupload_device_info) {
-      SendLocalData();
-      reuploaded_on_tombstone_ = true;
-    }
+  if (has_tombstone_for_local_device && !reuploaded_on_tombstone_) {
+    SendLocalData();
+    reuploaded_on_tombstone_ = true;
   }
 
   return absl::nullopt;
