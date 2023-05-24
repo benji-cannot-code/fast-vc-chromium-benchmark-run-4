9a8febdf2589532b25069b7e45984e7b8900d3bf
Munira Tursunova <moonira@google.com>
2023-05-24 13:14:47 +0000

    Retrieve color records from font
    Implement color records data retrieval from CPAL table using Harfbuzz.
    Will be used for font-palette animation.
    
    Explainer: https://docs.google.com/document/d/1XMTrKH003KBOes6hxzI-3E7LTwp5YwFC-rnzoFpFrfw/edit?usp=sharing
    
    Bug: 1400620
    Change-Id: I5a1e210ae6c18a4a9ae3bf1d126e8a7e6bf27899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4486477
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148476}

--- third_party/blink/renderer/platform/fonts/opentype/open_type_cpal_lookup.h
+++ third_party/blink/renderer/platform/fonts/opentype/open_type_cpal_lookup.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 
 namespace blink {
@@ -27,6 +29,12 @@ class PLATFORM_EXPORT OpenTypeCpalLookup {
    */
   static absl::optional<uint16_t> FirstThemedPalette(sk_sp<SkTypeface> typeface,
                                                      PaletteUse palette_use);
+
+  /* Returns a sorted Vector of color records from the specified font palette.
+   * The position in the returned vector matches the palette index in the font.
+   */
+  static Vector<SkColor> RetrieveColorRecords(sk_sp<SkTypeface> typeface,
+                                              unsigned int palette_index);
 };
 
 }  // namespace blink
