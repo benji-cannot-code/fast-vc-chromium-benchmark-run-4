1eab00ed475c475128e83432ec0f29522df043d9
Louis Romero <lpromero@google.com>
2023-05-24 10:06:58 +0000

    Rename ScrollingTimeLogger in ScopedScrollingTimeLogger
    This conveys the meaning that it follows the RAII pattern.
    
    Change-Id: I66c80c2d6c5e35e5c8021dc9bbe14efbe335b819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4486120
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148440}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -59,10 +59,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-class ScrollingTimeLogger {
+class ScopedScrollingTimeLogger {
  public:
-  ScrollingTimeLogger() : start_(base::TimeTicks::Now()) {}
-  ~ScrollingTimeLogger() {
+  ScopedScrollingTimeLogger() : start_(base::TimeTicks::Now()) {}
+  ~ScopedScrollingTimeLogger() {
     base::TimeDelta duration = base::TimeTicks::Now() - start_;
     base::UmaHistogramTimes("IOS.TabSwitcher.TimeSpentScrolling", duration);
   }
@@ -198,7 +198,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 @implementation GridViewController {
   // Tracks when the grid view is scrolling. Create a new instance to start
   // timing and reset to stop and log the associated time histogram.
-  std::unique_ptr<ScrollingTimeLogger> _scrollingTimeLogger;
+  std::unique_ptr<ScopedScrollingTimeLogger> _scopedScrollingTimeLogger;
 }
 
 @synthesize thumbStripEnabled = _thumbStripEnabled;
@@ -1148,18 +1148,18 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 - (void)scrollViewWillBeginDragging:(UIScrollView*)scrollView {
   [self.delegate gridViewControllerWillBeginDragging:self];
   base::RecordAction(base::UserMetricsAction("MobileTabGridUserScrolled"));
-  _scrollingTimeLogger = std::make_unique<ScrollingTimeLogger>();
+  _scopedScrollingTimeLogger = std::make_unique<ScopedScrollingTimeLogger>();
 }
 
 - (void)scrollViewDidEndDragging:(UIScrollView*)scrollView
                   willDecelerate:(BOOL)decelerate {
   if (!decelerate) {
-    _scrollingTimeLogger = nullptr;
+    _scopedScrollingTimeLogger = nullptr;
   }
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView*)scrollView {
-  _scrollingTimeLogger = nullptr;
+  _scopedScrollingTimeLogger = nullptr;
 }
 
 - (void)scrollViewDidScrollToTop:(UIScrollView*)scrollView {
