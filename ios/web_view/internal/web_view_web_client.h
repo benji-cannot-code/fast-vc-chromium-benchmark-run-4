ab619cb8b5a8417d4700e15d06a74a31760d9056
Daniel White <danieltwhite@google.com>
2023-05-24 16:48:54 +0000

    [iOS] Remove share_workaround & plugin_placeholder
    share_workaround.js and plugin_placeholder.js were removed because
    Chrome for iOS' minimum iOS version has been increased to iOS 15.
    
    Fixed: 1123689, 1218221
    Change-Id: Ic2abb43573b3c32702246e7c8150a67b42110be2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544643
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148583}

--- ios/web_view/internal/web_view_web_client.h
+++ ios/web_view/internal/web_view_web_client.h
@@ -35,7 +35,6 @@ class WebViewWebClient : public web::WebClient {
       web::BrowserState* browser_state) const override;
   NSString* GetDocumentStartScriptForMainFrame(
       web::BrowserState* browser_state) const override;
-  std::u16string GetPluginNotSupportedText() const override;
   void PrepareErrorPage(web::WebState* web_state,
                         const GURL& url,
                         NSError* error,
