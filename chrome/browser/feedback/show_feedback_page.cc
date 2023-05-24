1c60b13b6bd039529de7595339f0fddd7554bea4
Polina Bondarenko <pbond@google.com>
2023-05-24 18:57:30 +0000

    feedback: disable a new feedback tool for kiosk.
    SWAs are disabled in kiosk mode and a new feedback tools should be
    disabled.
    
    BUG=b/283942805
    TEST=kiosk device with troubleshooting tools
    
    Change-Id: Ia2c6a293d3fe079b43aab4744c23f3ab10df2143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4561424
    Commit-Queue: Polina Bondarenko <pbond@chromium.org>
    Auto-Submit: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148685}

--- chrome/browser/feedback/show_feedback_page.cc
+++ chrome/browser/feedback/show_feedback_page.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/browser_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
+#include "chromeos/components/kiosk/kiosk_utils.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #endif
@@ -222,9 +223,11 @@ void RequestFeedbackFlow(const GURL& page_url,
     include_bluetooth_logs = IsFromUserInteraction(source);
     show_questionnaire = IsFromUserInteraction(source);
   }
-  if (base::FeatureList::IsEnabled(ash::features::kOsFeedback)) {
-    // TODO(crbug.com/1407646): Include autofill metadata into CrOS new feedback
-    // tool.
+  // Disable a new feedback tool for kiosk, because SWAs are disabled there.
+  if (!chromeos::IsKioskSession() &&
+      base::FeatureList::IsEnabled(ash::features::kOsFeedback)) {
+    // TODO(crbug.com/1407646): Include autofill metadata into CrOS new
+    // feedback tool.
     ash::SystemAppLaunchParams params{};
     params.url = BuildFeedbackUrl(
         extra_diagnostics, description_template, description_placeholder_text,
