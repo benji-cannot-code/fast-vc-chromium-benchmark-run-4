48eb54435688fbbbb2cffa9295490ac59a88550c
Zhanbang He <hezhanbang@gmail.com>
2023-05-24 18:14:52 +0000

    Copy QR code image to system clipboard.
    This feature is convenient for copying QR code image to multiple software.
    
    Bug: 1444950
    Change-Id: Iaeebc5bea017eea3a94dd5b5a09156bbc82736c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520697
    Commit-Queue: Wissem Gamra <wiss@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Wissem Gamra <wiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148651}

--- chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble.cc
+++ chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/download_request_utils.h"
 #include "content/public/browser/web_contents.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "ui/base/clipboard/scoped_clipboard_writer.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/theme_provider.h"
@@ -182,6 +183,7 @@ void QRCodeGeneratorBubble::DisplayPlaceholderImage() {
 }
 
 void QRCodeGeneratorBubble::DisplayError(mojom::QRCodeGeneratorError error) {
+  copy_button_->SetEnabled(false);
   download_button_->SetEnabled(false);
   if (error == mojom::QRCodeGeneratorError::INPUT_TOO_LONG) {
     ShrinkAndHideDisplay(center_error_label_);
@@ -197,11 +199,12 @@ void QRCodeGeneratorBubble::DisplayError(mojom::QRCodeGeneratorError error) {
   center_error_label_->SetVisible(true);
 }
 
-void QRCodeGeneratorBubble::HideErrors(bool enable_download_button) {
+void QRCodeGeneratorBubble::HideErrors(bool enable_button) {
   ShrinkAndHideDisplay(center_error_label_);
   bottom_error_label_->SetVisible(false);
   bottom_error_label_->GetViewAccessibility().OverrideIsIgnored(true);
-  download_button_->SetEnabled(enable_download_button);
+  copy_button_->SetEnabled(enable_button);
+  download_button_->SetEnabled(enable_button);
 }
 
 void QRCodeGeneratorBubble::ShrinkAndHideDisplay(views::View* view) {
@@ -306,6 +309,9 @@ void QRCodeGeneratorBubble::Init() {
       AddChildView(std::make_unique<views::BoxLayoutView>());
   button_container->SetCrossAxisAlignment(
       views::BoxLayout::CrossAxisAlignment::kCenter);
+  button_container->SetBetweenChildSpacing(
+      ChromeLayoutProvider::Get()->GetDistanceMetric(
+          views::DISTANCE_RELATED_BUTTON_HORIZONTAL));
 
   // "More info" tooltip; looks like (i).
   auto tooltip_icon = std::make_unique<views::TooltipIcon>(
@@ -320,6 +326,15 @@ void QRCodeGeneratorBubble::Init() {
 
   auto* flex = button_container->AddChildView(std::make_unique<views::View>());
   button_container->SetFlexForView(flex, 1);
+
+  // Copy button.
+  copy_button_ =
+      button_container->AddChildView(std::make_unique<views::MdTextButton>(
+          base::BindRepeating(&QRCodeGeneratorBubble::CopyButtonPressed,
+                              base::Unretained(this)),
+          l10n_util::GetStringUTF16(
+              IDS_BROWSER_SHARING_QR_CODE_DIALOG_COPY_BUTTON_LABEL)));
+  copy_button_->SetHorizontalAlignment(gfx::ALIGN_CENTER);
 
   // Download button.
   download_button_ =
@@ -420,12 +435,22 @@ void QRCodeGeneratorBubble::SetQRCodeServiceForTesting(
   qrcode_service_override_ = std::move(qrcode_service_override);
 }
 
-void QRCodeGeneratorBubble::DownloadButtonPressed() {
+const SkBitmap QRCodeGeneratorBubble::GetBitmap() {
   const gfx::ImageSkia& image_ref = qr_code_image_->GetImage();
   // Returns closest scaling to parameter (1.0).
   // Should be exact since we generated the bitmap.
   const gfx::ImageSkiaRep& image_rep = image_ref.GetRepresentation(1.0f);
-  const SkBitmap& bitmap = image_rep.GetBitmap();
+  return image_rep.GetBitmap();
+}
+
+// Copy image to system clipboard.
+void QRCodeGeneratorBubble::CopyButtonPressed() {
+  const SkBitmap& bitmap = GetBitmap();
+  ui::ScopedClipboardWriter(ui::ClipboardBuffer::kCopyPaste).WriteImage(bitmap);
+}
+
+void QRCodeGeneratorBubble::DownloadButtonPressed() {
+  const SkBitmap& bitmap = GetBitmap();
   const GURL data_url = GURL(webui::GetBitmapDataUrl(bitmap));
 
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents_);
