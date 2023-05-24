8e9adb1a6b24881221a95ae833af33d99eb3d748
Ben Pastene <bpastene@chromium.org>
2023-05-24 17:31:49 +0000

    chromeos: Add the skipReason into Tast results uploaded to rdb
    This should display the reason a test is skipped in milo's results
    UI rather than requiring someone to dig into the task's stdout log
    to find it.
    
    eg:
    https://luci-milo.appspot.com/ui/inv/task-chromium-swarm.appspot.com-6262f208fe15c911/test-results?sortby=&groupby=
    
    Bug: b/283525092
    Change-Id: Ifd309f5d5a3f0855932a03f6f9c32f5238e501c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557733
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148618}

--- build/chromeos/test_runner.py
+++ build/chromeos/test_runner.py
@@ -400,6 +400,9 @@ class TastTest(RemoteTest):
         # inside as an RDB 'artifact'. (This could include system logs, screen
         # shots, etc.)
         artifacts = self.get_artifacts(test['outDir'])
+        html_artifact = debug_link
+        if result == base_test_result.ResultType.SKIP:
+          html_artifact = 'Test was skipped because: ' + test['skipReason']
         self._rdb_client.Post(
             test['name'],
             result,
@@ -408,7 +411,7 @@ class TastTest(RemoteTest):
             None,
             artifacts=artifacts,
             failure_reason=primary_error_message,
-            html_artifact=debug_link)
+            html_artifact=html_artifact)
 
     if self._rdb_client and self._logs_dir:
       # Attach artifacts from the device that don't apply to a single test.
