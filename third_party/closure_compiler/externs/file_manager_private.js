25ed49f7ad9edb8c4da1c1d9968c9de089a7b1b4
Aida Zolic <aidazolic@chromium.org>
2023-05-24 14:47:39 +0000

    dlp: Add scanning IOTask progress values
    Adds number of scanned items to the ProgressStatus structs, in order to
    show progress for scanning in progress notifications. This is roughly
    the percentage of the number of scanned items out of the total items.
    In some cases this won't be accurate, like in case of uploading entire
    folders, or because some items are not scanned at all. The main purpose
    is to show the user that some progress is happening.
    
    R=ayaelattar@chromium.org
    
    Fixed: b:282130948
    Change-Id: I2e7d9bdcf3c2ba2028ee6b69a3f683baf592c019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4556507
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148511}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -535,8 +535,8 @@ chrome.fileManagerPrivate.ResultingTasks;
  *   isArbitrarySyncFolder: (boolean|undefined),
  *   syncStatus: (!chrome.fileManagerPrivate.SyncStatus|undefined),
  *   progress: (number|undefined),
- *   shortcut: (boolean|undefined),
- *   syncCompletedTime: (number|undefined)
+ *   syncCompletedTime: (number|undefined),
+ *   shortcut: (boolean|undefined)
  * }}
  */
 chrome.fileManagerPrivate.EntryProperties;
@@ -934,6 +934,7 @@ chrome.fileManagerPrivate.ResumeParams;
  *   totalBytes: number,
  *   taskId: number,
  *   remainingSeconds: number,
+ *   sourcesScanned: number,
  *   showNotification: boolean,
  *   errorName: string,
  *   pauseParams: (!chrome.fileManagerPrivate.PauseParams|undefined),
@@ -1019,7 +1020,7 @@ chrome.fileManagerPrivate.executeTask = function(descriptor, entries, callback) 
  * of MIME types and URLs may contain duplicates. Additionally, the list of MIME
  * types can be empty. |descriptor| The unique identifier of task to mark as
  * default. |entries| Array of selected entries to extract path extensions from.
-\ * |mimeTypes| Array of selected file MIME types. |callback|
+ * |mimeTypes| Array of selected file MIME types. |callback|
  * @param {!chrome.fileManagerPrivate.FileTaskDescriptor} descriptor
  * @param {!Array<Entry>} entries
  * @param {!Array<string>} mimeTypes
