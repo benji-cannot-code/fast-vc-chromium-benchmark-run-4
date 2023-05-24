cc54656ffbb7d11354bc1bda850c9242be66e8b8
dpapad <dpapad@chromium.org>
2023-05-24 23:31:34 +0000

    WebUI: Replace "Tap_" suffix with "Click_" in click handlers, part 7.
    In this part updating: chrome/browser/resources/settings/chromeos/
    
    "Tap_" is an old terminology from Polymer1 where "on-tap=" instead of
    "on-click=" was used. Even though on-tap is no longer used anywhere
    (crbug.com/812035), a lot of places still use the old "Tap_" suffix
    which is inconsistent with the majority of cases.
    
    This is in preparation of disallowing the "Tap_" suffix via presubmit
    ESLint checks.
    
    Bug: 720034
    Change-Id: If36df2984154c57c06930fae0ca4bc70d2c70a26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564154
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148828}

--- chrome/browser/resources/settings/chromeos/internet_page/internet_known_networks_subpage.ts
+++ chrome/browser/resources/settings/chromeos/internet_page/internet_known_networks_subpage.ts
@@ -379,7 +379,7 @@ class SettingsInternetKnownNetworksPageElement extends
     event.stopPropagation();
   }
 
-  private onSubscriptionListItemTap_(
+  private onSubscriptionListItemClick_(
       event: DomRepeatEvent<PasspointSubscription>): void {
     const showPasspointEvent = new CustomEvent(
         'show-passpoint-detail',
@@ -395,7 +395,7 @@ class SettingsInternetKnownNetworksPageElement extends
     event.stopPropagation();
   }
 
-  private onSubscriptionMenuButtonTap_(
+  private onSubscriptionMenuButtonClick_(
       event: DomRepeatEvent<PasspointSubscription>): void {
     const button = event.target as HTMLButtonElement;
     this.selectedSubscriptionId_ = event.model.item.id;
@@ -410,7 +410,7 @@ class SettingsInternetKnownNetworksPageElement extends
         this.getSubscriptionDisplayName_(subscription));
   }
 
-  private async onSubscriptionForgetTap_(): Promise<void> {
+  private async onSubscriptionForgetClick_(): Promise<void> {
     this.$.subscriptionDotsMenu.close();
     this.selectedSubscriptionId_ = '';
     const response = await this.passpointService_.deletePasspointSubscription(
