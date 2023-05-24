8251c54d652720ce5bbd8e7da182711c1362659b
Oksana Zhuravlova <oksamyt@chromium.org>
2023-05-24 15:40:57 +0000

    [Code Health] Remove the WebFont.HttpCacheStatus histogram
    OBSOLETE_HISTOGRAM[WebFont.HttpCacheStatus]=Expired in 2021
    
    This change removes the code that records WebFont.HttpCacheStatus*
    histograms, as well as relevant tests and the histogram itself.
    
    Bug: 1384976
    Change-Id: Ib7a9535794f2896cabe649c5402a75f0986155c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549343
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148544}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
 #include "net/http/http_util.h"
-#include "net/http/webfonts_histogram.h"
 #include "net/log/net_log_event_type.h"
 #include "net/ssl/ssl_cert_request_info.h"
 #include "net/ssl/ssl_config_service.h"
@@ -3857,10 +3856,6 @@ void HttpCache::Transaction::SyncCacheEntryStatusToResponse() {
 void HttpCache::Transaction::RecordHistograms() {
   DCHECK(!recorded_histograms_);
   recorded_histograms_ = true;
-
-  web_fonts_histogram::MaybeRecordCacheStatus(
-      cache_entry_status_,
-      HttpCache::GetResourceURLFromHttpCacheKey(cache_key_));
 
   if (CacheEntryStatus::ENTRY_UNDEFINED == cache_entry_status_)
     return;
--- net/http/http_cache_unittest.cc
+++ net/http/http_cache_unittest.cc
@@ -1254,9 +1254,6 @@ class HttpCacheTest_SplitCacheFeature
 };
 
 TEST_P(HttpCacheTest_SplitCacheFeature, SimpleGETVerifyGoogleFontMetrics) {
-  base::HistogramTester histograms;
-  const std::string histogram_name = "WebFont.HttpCacheStatus_roboto";
-
   SchemefulSite site_a(GURL("http://www.a.com"));
 
   MockHttpCache cache;
@@ -1273,15 +1270,8 @@ TEST_P(HttpCacheTest_SplitCacheFeature, SimpleGETVerifyGoogleFontMetrics) {
   RunTransactionTestWithRequest(cache.http_cache(), transaction, request,
                                 nullptr);
 
-  histograms.ExpectUniqueSample(
-      histogram_name, static_cast<int>(CacheEntryStatus::ENTRY_NOT_IN_CACHE),
-      1);
-
   RunTransactionTestWithRequest(cache.http_cache(), transaction, request,
                                 nullptr);
-
-  histograms.ExpectBucketCount(
-      histogram_name, static_cast<int>(CacheEntryStatus::ENTRY_USED), 1);
 }
 
 INSTANTIATE_TEST_SUITE_P(
