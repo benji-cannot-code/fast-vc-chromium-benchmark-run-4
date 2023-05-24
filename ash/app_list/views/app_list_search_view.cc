f1a0ecb583bfe0a50741a7e817db5020f5190c8c
Wen-Chien Wang <wcwang@chromium.org>
2023-05-24 18:20:05 +0000

    Animates image search result section when updated.
    This cl adds animations to the image search result section when the
    result is updated. The whole list of results is considered as a single
    row and will animate upward as other text search result. Note that the
    animation reuses the search result list one so the animation function
    is changed to a utility function.
    
    Animation demo: crbug.com/1352636#c12
    
    Bug: 1352636
    Change-Id: Iba772e824699711de22e5ad58fc68e624829600d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4529042
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148656}

--- ash/app_list/views/app_list_search_view.cc
+++ ash/app_list/views/app_list_search_view.cc
@@ -109,8 +109,7 @@ AppListSearchView::AppListSearchView(
   auto* answer_card_container =
       scroll_contents->AddChildView(std::make_unique<SearchResultListView>(
           view_delegate, dialog_controller_,
-          SearchResultView::SearchResultViewType::kAnswerCard,
-          /*animates_result_updates=*/true, absl::nullopt));
+          SearchResultView::SearchResultViewType::kAnswerCard, absl::nullopt));
   answer_card_container->SetListType(
       SearchResultListView::SearchResultListType::kAnswerCard);
   add_result_container(answer_card_container);
@@ -119,8 +118,7 @@ AppListSearchView::AppListSearchView(
   auto* best_match_container =
       scroll_contents->AddChildView(std::make_unique<SearchResultListView>(
           view_delegate, dialog_controller_,
-          SearchResultView::SearchResultViewType::kDefault,
-          /*animated_result_updates=*/true, absl::nullopt));
+          SearchResultView::SearchResultViewType::kDefault, absl::nullopt));
   best_match_container->SetListType(
       SearchResultListView::SearchResultListType::kBestMatch);
   add_result_container(best_match_container);
@@ -144,8 +142,7 @@ AppListSearchView::AppListSearchView(
     auto* result_container =
         scroll_contents->AddChildView(std::make_unique<SearchResultListView>(
             view_delegate, dialog_controller_,
-            SearchResultView::SearchResultViewType::kDefault,
-            /*animates_result_updates=*/true, i));
+            SearchResultView::SearchResultViewType::kDefault, i));
     add_result_container(result_container);
   }
 
