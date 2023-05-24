ec04e70572e20409b4fa6b5cb1b663937fc2d3fe
Yi Xu <yiyix@google.com>
2023-05-24 19:11:09 +0000

    Add MotionMark to third_party library
    Since the MotionMark repository is in WebKit itself, which makes it
    hard for chrome to do add, modify or remove tests. We added
    MotionMark to third_party/blink/perf_tests directory. In this way,
    we can continue contribute to MotionMark project.
    
    Note that this is a temporary solution, this directory will be removed
    when MotionMark becomes a public project itself. This adds around
    1.3MB to the project.
    
    Github URL: https://github.com/WebKit/webkit/tree/master/PerformanceTests
    Revision: 4b28b98d37306eb5ae9ab8bca504590295f64210
    
    Bug: 1446401
    
    Change-Id: I040a8497bdedffab0cb7b0f369c0a389ff41b92c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4541141
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148691}

---
+++ third_party/blink/perf_tests/MotionMark/README.md
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This folder is copy from PerformanceTests folder in webkit's repo
+(https://github.com/WebKit/webkit/tree/master/PerformanceTests).
+
+Revision: 4b28b98d37306eb5ae9ab8bca504590295f64210
+This is a temporary solution for Chrome to contribute to MotionMark tests
+before Webkit moves it to a separate project. "no-check" is added to bypass
+Chromium PRESUBMIT.
---
+++ third_party/blink/perf_tests/MotionMark/resources/runner/tests.js
@@ -0,0 +1,78 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var Headers = {
+    testName: [
+        {
+            title: "<span onclick='benchmarkController.showDebugInfo()'>" + Strings.text.testName + "</span>",
+            text: Strings.text.testName
+        }
+    ],
+    score: [
+        {
+            title: Strings.text.score,
+            text: Strings.json.score
+        }
+    ],
+    details: [
+        {
+            title: "&nbsp;",
+            text: function(data) {
+                var bootstrap = data[Strings.json.complexity][Strings.json.bootstrap];
+                return "<span>±" + (Statistics.largestDeviationPercentage(bootstrap.confidenceLow, bootstrap.median, bootstrap.confidenceHigh) * 100).toFixed(2) + "%</span>";
+            }
+        }
+    ]
+};
+
+var Suite = function(name, tests) {
+    this.name = name;
+    this.tests = tests;
+};
+
+var Suites = [];
+
+Suites.push(new Suite("MotionMark",
+    [
+        {
+            url: "master/multiply.html", // nocheck
+            name: "Multiply"
+        },
+        {
+            url: "master/canvas-stage.html?pathType=arcs", // nocheck
+            name: "Canvas Arcs"
+        },
+        {
+            url: "master/leaves.html", // nocheck
+            name: "Leaves"
+        },
+        {
+            url: "master/canvas-stage.html?pathType=linePath", // nocheck
+            name: "Paths"
+        },
+        {
+            url: "master/canvas-stage.html?pathType=line&lineCap=square", // nocheck
+            name: "Canvas Lines"
+        },
+        {
+            url: "master/image-data.html", // nocheck
+            name: "Images"
+        },
+        {
+            url: "master/design.html", // nocheck
+            name: "Design"
+        },
+        {
+            url: "master/suits.html", // nocheck
+            name: "Suits"
+        },
+    ]
+));
+
+function suiteFromName(name)
+{
+    return Suites.find(function(suite) { return suite.name == name; });
+}
+
+function testFromName(suite, name)
+{
+    return suite.tests.find(function(test) { return test.name == name; });
+}
