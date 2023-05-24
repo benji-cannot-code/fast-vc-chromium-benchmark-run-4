954f0e1ec4fba12b808ae332fcd2eb50fdb8c2a1
Yiren Wang <yrw@chromium.org>
2023-05-24 00:06:43 +0000

    CrOS GMC V2: Add a media start casting button
    Add the UI for the cast button only and its functionality will be
    implemented in later CLs.
    
    Since Button::set_tag() is being deprecated, we switch to use
    View::SetID() to record the media action enums for media action buttons.
    We will reference the vector icon names and tooltip text IDs directly
    when creating the buttons, so that the cast button can be created in the
    same way as other media action buttons, but the casting functionality is
    not a media action while these buttons look similar.
    
    Setting tooltip texts in buttons will set the accessibility texts too.
    Screenshot: https://storage.cloud.google.com/chromium-translation-screenshots/8ca6c40c6f671182a73c4bc4c34405c7dce46156
    
    Bug: 1442056
    Change-Id: I6465e1124e5a638ea891a2d6152f88f8ab4db345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4553921
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148275}

--- components/media_message_center/media_notification_view_ash_impl.h
+++ components/media_message_center/media_notification_view_ash_impl.h
@@ -83,9 +83,10 @@ class COMPONENT_EXPORT(MEDIA_MESSAGE_CENTER) MediaNotificationViewAshImpl
  private:
   friend class MediaNotificationViewAshImplTest;
 
-  MediaButton* CreateMediaButton(
-      views::View* parent,
-      media_session::mojom::MediaSessionAction action);
+  MediaButton* CreateMediaButton(views::View* parent,
+                                 int button_id,
+                                 const gfx::VectorIcon& vector_icon,
+                                 int tooltip_text_id);
 
   void UpdateActionButtonsVisibility();
 
@@ -125,6 +126,7 @@ class COMPONENT_EXPORT(MEDIA_MESSAGE_CENTER) MediaNotificationViewAshImpl
 
   raw_ptr<MediaSquigglyProgressView> squiggly_progress_view_ = nullptr;
   raw_ptr<MediaButton> play_pause_button_ = nullptr;
+  raw_ptr<MediaButton> start_casting_button_ = nullptr;
   raw_ptr<MediaButton> picture_in_picture_button_ = nullptr;
 };
 
