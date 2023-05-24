6c6f73cb4b7ed84ea67de0151d71b0a62be774cf
Timofey Chudakov <tchudakov@google.com>
2023-05-24 13:39:25 +0000

    [Android][Autofill] Enable cloud_off icon for signed in users.
    Bug: 1448428
    Change-Id: I5534e8910909b32d6975002b12d1ca8bd3ae7cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4561264
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148484}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
@@ -36,10 +36,12 @@ import org.chromium.chrome.browser.payments.SettingsAutofillAndPaymentsObserver;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.ChromeManagedPreferenceDelegate;
 import org.chromium.chrome.browser.settings.ProfileDependentSetting;
+import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.sync.SyncService;
 import org.chromium.components.autofill.prefeditor.EditorObserverForTest;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
+import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.sync.UserSelectableType;
 
 /**
@@ -150,7 +152,6 @@ public class AutofillProfilesFragment extends PreferenceFragmentCompat
         });
         getPreferenceScreen().addPreference(autofillSwitch);
 
-        final boolean addressesNotSyncedWithSyncOn = addressesNotSyncedWithSyncOn();
         for (AutofillProfile profile : PersonalDataManager.getInstance().getProfilesForSettings()) {
             assert profile.getIsLocal();
             // Add a preference for the profile.
@@ -158,7 +159,7 @@ public class AutofillProfilesFragment extends PreferenceFragmentCompat
             pref.setTitle(profile.getFullName());
             pref.setSummary(profile.getLabel());
             pref.setKey(pref.getTitle().toString()); // For testing.
-            if (addressesNotSyncedWithSyncOn && profile.getSource() != Source.ACCOUNT) {
+            if (shouldShowLocalProfileIcon(profile)) {
                 // Conditionally set local profile icon for address profiles that are neither
                 // synced, nor saved in the account.
                 pref.setWidgetLayoutResource(R.layout.autofill_local_profile_icon);
@@ -263,10 +264,17 @@ public class AutofillProfilesFragment extends PreferenceFragmentCompat
         return new AutofillAddress(getActivity(), profile);
     }
 
-    private boolean addressesNotSyncedWithSyncOn() {
+    private boolean shouldShowLocalProfileIcon(AutofillProfile profile) {
+        if (!IdentityServicesProvider.get().getIdentityManager(mProfile).hasPrimaryAccount(
+                    ConsentLevel.SIGNIN)) {
+            return false;
+        }
+        if (profile.getSource() == Source.ACCOUNT) {
+            return false;
+        }
         SyncService syncService = SyncService.get();
-        return syncService != null && syncService.isSyncFeatureEnabled()
-                && !syncService.getSelectedTypes().contains(UserSelectableType.AUTOFILL);
+        return syncService == null || !syncService.isSyncFeatureEnabled()
+                || !syncService.getSelectedTypes().contains(UserSelectableType.AUTOFILL);
     }
 
     private Context getStyledContext() {
