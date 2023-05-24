9126d6f05b256d4b701cff4ae898969d9b29230d
seannli <seannli@google.com>
2023-05-24 05:31:08 +0000

    camera: Move must-do cleanup to VCD_delegate destructor.
    If VCD_delegate doesn't Allocate, DeAllocate or ShutDown normally, it
    will cause some component didn't be reset before it was freed. This CL
    moves some must-do cleanup functions to destructor to make sure them
    will be called.
    
    Bug: b:281837116
    Test: Manually test by opening CCA, taking videos and photos.
    Test by tast -verbose run camera.CCAUIStress*.
    
    Change-Id: I159ddb90fca791b59feabfa214093ca68a2c4448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4552757
    Commit-Queue: Sean Li <seannli@google.com>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148358}

--- media/capture/video/chromeos/display_rotation_observer.h
+++ media/capture/video/chromeos/display_rotation_observer.h
@@ -28,7 +28,7 @@ class ScreenObserverDelegate
       public base::RefCountedThreadSafe<ScreenObserverDelegate> {
  public:
   static scoped_refptr<ScreenObserverDelegate> Create(
-      DisplayRotationObserver* observer,
+      base::WeakPtr<DisplayRotationObserver> observer,
       scoped_refptr<base::SingleThreadTaskRunner> display_task_runner);
 
   ScreenObserverDelegate() = delete;
@@ -43,7 +43,7 @@ class ScreenObserverDelegate
   friend class base::RefCountedThreadSafe<ScreenObserverDelegate>;
 
   ScreenObserverDelegate(
-      DisplayRotationObserver* observer,
+      base::WeakPtr<DisplayRotationObserver> observer,
       scoped_refptr<base::SingleThreadTaskRunner> display_task_runner);
   ~ScreenObserverDelegate() override;
 
@@ -58,7 +58,7 @@ class ScreenObserverDelegate
   void SendDisplayRotation(const display::Display& display);
   void SendDisplayRotationOnCaptureThread(const display::Display& display);
 
-  raw_ptr<DisplayRotationObserver, ExperimentalAsh> observer_;
+  base::WeakPtr<DisplayRotationObserver> observer_;
 
   absl::optional<display::ScopedDisplayObserver> display_observer_;
 
