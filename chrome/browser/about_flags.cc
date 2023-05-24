b4445a0086cd8da91389aaa18f76d10935cf7e96
Vikas Soni <vikassoni@chromium.org>
2023-05-24 08:59:46 +0000

    Add new feature flag UseClientGmbInterface.
    1. Add a new finch flag UseClientGmbInterface which will allow
     renderers to do direct IPC to GPU process in order to create
     GMBs. Currently Renderers IPCs are routed via Browser process.
    
    2. Add about gpu flag to control the feature.
    
    Note that Feature related changes will be added in follow up
    CLs.
    
    Bug: 1445706
    Change-Id: I35402de1b90a5f6f05a5156dc8e84d693c1dedb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557384
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148414}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -7604,6 +7604,10 @@ const FeatureEntry kFeatureEntries[] = {
 
 #endif  // BUILDFLAG(IS_ANDROID)
 
+    {"use-client-gmb-interface", flag_descriptions::kUseClientGmbInterfaceName,
+     flag_descriptions::kUseClientGmbInterfaceDescription, kOsAll,
+     FEATURE_VALUE_TYPE(features::kUseClientGmbInterface)},
+
 #if BUILDFLAG(IS_ANDROID)
     {"use-toast-manager", flag_descriptions::kUseToastManagerName,
      flag_descriptions::kUseToastManagerDescription, kOsAndroid,
