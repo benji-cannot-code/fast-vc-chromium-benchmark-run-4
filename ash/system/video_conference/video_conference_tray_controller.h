c2f2d88e1b3f035e3f42360dd47fcd058fe1f84a
Andre Le <leandre@chromium.org>
2023-05-24 23:02:30 +0000

    VC UI: Add metric to detect flickering in VC tray
    Fixed: b:278937272
    Change-Id: Ib29b759d0b01680e4f02e897e75e2efdd2d094f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4558844
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148809}

--- ash/system/video_conference/video_conference_tray_controller.h
+++ ash/system/video_conference/video_conference_tray_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/video_conference/video_conference_common.h"
 #include "base/observer_list_types.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chromeos/ash/components/audio/cras_audio_handler.h"
 #include "chromeos/crosapi/mojom/video_conference.mojom-forward.h"
 #include "media/capture/video/chromeos/camera_hal_dispatcher_impl.h"
@@ -168,6 +169,9 @@ class ASH_EXPORT VideoConferenceTrayController
   // of active `MediaApp`'s to force the shelf to show or hide.
   void UpdateShelfAutoHide(MediaApps apps);
 
+  // Records repeated shows metric when the timer is stop.
+  void RecordRepeatedShows();
+
   // The number of capturing apps, fetched from `VideoConferenceManagerAsh`.
   int capturing_apps_ = 0;
 
@@ -210,6 +214,10 @@ class ASH_EXPORT VideoConferenceTrayController
   // which is after VideoConferenceTrayController.
   raw_ptr<VideoConferenceManagerBase> video_conference_manager_ = nullptr;
   bool initialized_ = false;
+
+  // Used to record metrics of repeated shows per 100 ms.
+  int count_repeated_shows_ = 0;
+  base::DelayTimer repeated_shows_timer_;
 
   base::WeakPtrFactory<VideoConferenceTrayController> weak_ptr_factory_{this};
 };
