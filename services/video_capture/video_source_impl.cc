2dbfae9a9538a96220347aed8e5969dd696b4395
Bryant Chandler <bryantchandler@chromium.org>
2023-05-24 15:11:34 +0000

    [video_capture] Only StopDevice if all subscribers are disconnected
    OnClientDisconnected currently calls StopDevice if any subscriber
    is disconnected, but it should only stop the device if all subscribers
    are disconnected. This CL makes that adjustment.
    
    Change-Id: I3de382a4bb9639a3b7da4000d6b189418893995c
    Bug: 1446105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4532859
    Reviewed-by: Markus Handell <handellm@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148521}

--- services/video_capture/video_source_impl.cc
+++ services/video_capture/video_source_impl.cc
@@ -83,6 +83,9 @@ void VideoSourceImpl::CreatePushSubscription(
 
 void VideoSourceImpl::OnClientDisconnected() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (!receivers_.empty()) {
+    return;
+  }
 
   if (device_status_ != DeviceStatus::kStoppingAsynchronously) {
     // We need to stop devices when VideoSource remote discarded with active
@@ -90,11 +93,9 @@ void VideoSourceImpl::OnClientDisconnected() {
     device_factory_->StopDevice(device_id_);
   }
 
-  if (receivers_.empty()) {
-    // Note: Invoking this callback may synchronously trigger the destruction of
-    // |this|, so no more member access should be done after it.
-    on_last_binding_closed_cb_.Run();
-  }
+  // Note: Invoking this callback may synchronously trigger the destruction of
+  // |this|, so no more member access should be done after it.
+  on_last_binding_closed_cb_.Run();
 }
 
 void VideoSourceImpl::StartDeviceWithSettings(
