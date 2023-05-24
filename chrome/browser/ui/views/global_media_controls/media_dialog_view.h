b3a6036b036112da2793fdbeb02a49764b04a99a
Evan Liu <evliu@google.com>
2023-05-24 19:35:05 +0000

    Live Translate Global Media Control
    This CL adds Live Translate controls to the Global Media Control.
    
    Screenshot with Live Caption disabled: https://screenshot.googleplex.com/9aXFdqfaX7r3DMF
    
    Screenshot with Live Caption enabled: https://screenshot.googleplex.com/62yHNU7EafKPFz8
    
    Screenshot with Live Translate enabled: https://screenshot.googleplex.com/93eU7W2pY8kVsfT
    
    UX Deck: go/live-translate-directional-review
    
    Change-Id: I1d62fc155a9db206c317605171fea35b179b2625
    Bug: 1426899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4389333
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148708}

--- chrome/browser/ui/views/global_media_controls/media_dialog_view.h
+++ chrome/browser/ui/views/global_media_controls/media_dialog_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
+#include "chrome/browser/ui/global_media_controls/live_translate_combobox_model.h"
 #include "components/global_media_controls/public/constants.h"
 #include "components/global_media_controls/public/media_dialog_delegate.h"
 #include "components/global_media_controls/public/media_item_ui_observer.h"
@@ -19,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/soda/soda_installer.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
+
+class PrefChangeRegistrar;
+class RichHoverButton;
 
 namespace content {
 class WebContents;
@@ -31,7 +35,9 @@ class MediaItemUIFooter;
 }  // namespace global_media_controls
 
 namespace views {
+class Combobox;
 class Label;
+class Separator;
 class ToggleButton;
 }  // namespace views
 
@@ -99,6 +105,8 @@ class MediaDialogView : public views::BubbleDialogDelegateView,
   void AddObserver(MediaDialogViewObserver* observer);
   void RemoveObserver(MediaDialogViewObserver* observer);
 
+  void TargetLanguageChanged();
+
   const std::map<const std::string, global_media_controls::MediaItemUIView*>&
   GetItemsForTesting() const;
 
@@ -130,9 +138,13 @@ class MediaDialogView : public views::BubbleDialogDelegateView,
 
   // views::Button::PressedCallback
   void OnLiveCaptionButtonPressed();
+  void OnLiveTranslateButtonPressed();
+  void OnSettingsButtonPressed();
 
-  void ToggleLiveCaption(bool enabled);
   void UpdateBubbleSize();
+
+  void OnLiveCaptionEnabledChanged();
+  void OnLiveTranslateEnabledChanged();
 
   // SodaInstaller::Observer overrides:
   void OnSodaInstalled(speech::LanguageCode language_code) override;
@@ -141,6 +153,9 @@ class MediaDialogView : public views::BubbleDialogDelegateView,
   void OnSodaProgress(speech::LanguageCode language_code,
                       int progress) override;
 
+  void InitializeLiveCaptionSection();
+  void InitializeLiveTranslateSection();
+  void InitializeCaptionSettingsSection();
   void SetLiveCaptionTitle(const std::u16string& new_text);
 
   std::unique_ptr<global_media_controls::MediaItemUIFooter> BuildFooterView(
@@ -154,6 +169,7 @@ class MediaDialogView : public views::BubbleDialogDelegateView,
   const raw_ptr<MediaNotificationService> service_;
 
   const raw_ptr<Profile> profile_;
+  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
 
   const raw_ptr<global_media_controls::MediaItemUIListView>
       active_sessions_view_;
@@ -167,6 +183,20 @@ class MediaDialogView : public views::BubbleDialogDelegateView,
   raw_ptr<views::View> live_caption_container_ = nullptr;
   raw_ptr<views::Label> live_caption_title_ = nullptr;
   raw_ptr<views::ToggleButton> live_caption_button_ = nullptr;
+
+  raw_ptr<views::Separator> separator_ = nullptr;
+  raw_ptr<views::View> live_translate_container_ = nullptr;
+  raw_ptr<views::View> live_translate_label_wrapper_ = nullptr;
+  raw_ptr<views::Label> live_translate_title_ = nullptr;
+  raw_ptr<views::Label> live_translate_subtitle_ = nullptr;
+  raw_ptr<views::ToggleButton> live_translate_button_ = nullptr;
+  raw_ptr<views::View> live_translate_settings_container_ = nullptr;
+
+  raw_ptr<views::View> target_language_container_ = nullptr;
+  raw_ptr<views::Combobox> target_language_combobox_ = nullptr;
+
+  raw_ptr<RichHoverButton> caption_settings_button_ = nullptr;
+  raw_ptr<views::View> caption_settings_container_ = nullptr;
 
   // It stores the WebContents* from which a MediaRouterDialogControllerViews
   // opened the dialog for a presentation request. It is nullptr if the dialog
