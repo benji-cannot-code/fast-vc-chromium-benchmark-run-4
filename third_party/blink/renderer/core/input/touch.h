a07bde7d55d853bf543e58379cd2a56386d62c88
Fredrik Söderquist <fs@opera.com>
2023-05-24 16:30:24 +0000

    Make Touch::absolute_location_ a PhysicalOffset
    Functionally identical[*], but makes the physical nature of the location
    more obvious.
    
    Will be propagated to SliderContainerElement's state in a follow-up CL.
    
    [*] The PageToAbsolute() helper in touch.cc now uses the more explicit
        PhysicalOffset::FromPointFFloor() - and thus floors the components
        of the point - rather than (implicitly) truncating when converting
        to LayoutUnits.
    
    Change-Id: Ie0170903312cc9ae7e265cce5876387d70b74bd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545248
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148571}

--- third_party/blink/renderer/core/input/touch.h
+++ third_party/blink/renderer/core/input/touch.h
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
+#include "third_party/blink/renderer/core/layout/geometry/physical_offset.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
-#include "third_party/blink/renderer/platform/geometry/layout_point.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/size_f.h"
@@ -78,7 +78,7 @@ class CORE_EXPORT Touch final : public ScriptWrappable {
         const gfx::SizeF& radius,
         float rotation_angle,
         float force,
-        LayoutPoint absolute_location);
+        PhysicalOffset absolute_location);
 
   Touch(LocalFrame*, const TouchInit*);
 
@@ -97,7 +97,7 @@ class CORE_EXPORT Touch final : public ScriptWrappable {
   float force() const { return force_; }
 
   // Blink-internal methods
-  const LayoutPoint& AbsoluteLocation() const { return absolute_location_; }
+  const PhysicalOffset& AbsoluteLocation() const { return absolute_location_; }
   const gfx::PointF& ScreenLocation() const { return screen_pos_; }
   Touch* CloneWithNewTarget(EventTarget*) const;
 
@@ -119,7 +119,7 @@ class CORE_EXPORT Touch final : public ScriptWrappable {
   // FIXME(rbyers): Shouldn't we be able to migrate callers to relying on
   // screenPos, pagePos or clientPos? absoluteLocation appears to be the same as
   // pagePos but without browser scale applied.
-  LayoutPoint absolute_location_;
+  PhysicalOffset absolute_location_;
 };
 
 }  // namespace blink
