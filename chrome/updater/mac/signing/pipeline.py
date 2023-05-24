d77c646e0aaf846afce0365791da695a193fc21b
Robert Sesek <rsesek@chromium.org>
2023-05-24 17:42:01 +0000

    updater: Stop using relative module imports
    Bug: 1442256
    Change-Id: If7fdf66fe8630e3b17d21aa3074cad221bfb5383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563114
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148627}

--- chrome/updater/mac/signing/pipeline.py
+++ chrome/updater/mac/signing/pipeline.py
@@ -11,7 +11,7 @@ The pipeline module orchestrates the entire signing process, which includes:
 
 import os.path
 
-from . import commands, model, notarize, parts, signing
+from signing import commands, model, notarize, parts, signing
 
 
 def _sign_app(paths, config, dest_dir):
