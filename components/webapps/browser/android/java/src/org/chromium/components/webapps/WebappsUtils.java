1438733b549ca581db497336b0d9f73e3fff5697
Kevin Grosu <kgrosu@google.com>
2023-05-24 14:28:42 +0000

    [CCT] "Add to Home screen" button should not be displayed if home screen capability is not available
    Adding a check to not display the button when request to pin shortcut functionality is not available on the device. Also, the button should not be visible if the URL has a file or content scheme.
    
    Bug: 1442390
    Change-Id: Iee400ce5ab646f02fd0467bd021279d70a31ec77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4554098
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Kevin Grosu <kgrosu@google.com>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148502}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/WebappsUtils.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/WebappsUtils.java
@@ -175,7 +175,8 @@ public class WebappsUtils {
         ShortcutManager shortcutManager =
                 ContextUtils.getApplicationContext().getSystemService(ShortcutManager.class);
         try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
-            sIsRequestPinShortcutSupported = shortcutManager.isRequestPinShortcutSupported();
+            sIsRequestPinShortcutSupported =
+                    shortcutManager != null && shortcutManager.isRequestPinShortcutSupported();
         }
     }
 }
