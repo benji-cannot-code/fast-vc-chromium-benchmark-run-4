e8a6c9fdf0e8d1a2f5a2127482ac13e9b757879d
Lei Zhang <thestig@chromium.org>
2023-05-24 17:43:01 +0000

    Forward declare blink::Resource in document.h
    Forward declare to reduce the build cost, as document.h is included in
    6% of all translation units in the chrome target. This is a follow-up to
    crrev.com/1144530.
    
    Bug: 1413922
    Change-Id: I9166c870591c0a265739e8c2026a968c627057db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4559408
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148628}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -343,6 +343,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/language.h"
 #include "third_party/blink/renderer/platform/loader/fetch/null_resource_fetcher_properties.h"
+#include "third_party/blink/renderer/platform/loader/fetch/resource.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h"
 #include "third_party/blink/renderer/platform/network/content_security_policy_parsers.h"
 #include "third_party/blink/renderer/platform/network/http_parsers.h"
