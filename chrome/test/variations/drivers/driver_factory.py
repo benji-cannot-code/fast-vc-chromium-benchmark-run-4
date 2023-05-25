6ab4293377931938bc8d998615f0d50465ca0c9d
Hao Wu <haowoo@google.com>
2023-05-25 19:00:14 +0000

    Move DriverFactory to a separate module.
    This prepares to add Android and CrOS driver factories.
    
    Change-Id: Iacecf8e1ee3b2f257b30a1071ae2b8d8db2a8bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567067
    Commit-Queue: Hao Wu <haowoo@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149273}

---
+++ chrome/test/variations/drivers/driver_factory.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from contextlib import contextmanager
+from typing import Optional
+
+class DriverFactory:
+  """The factory to create webdriver for the pre-defined environment"""
+
+  @contextmanager
+  def create_driver(self,
+                    seed_file: Optional[str] = None,
+                    options: Optional['webdriver.ChromeOptions'] = None
+    ) -> 'webdriver.Remote':
+    """Creates a webdriver."""
+    raise NotImplemented
+
+  def close(self):
+    """Cleans up anything that is created during the session."""
+    pass
