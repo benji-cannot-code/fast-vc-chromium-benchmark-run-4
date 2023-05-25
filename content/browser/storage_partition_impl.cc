a23f10a86da322e5a57d22d45085b265e3fc77b3
Mingyu Lei <leimy@chromium.org>
2023-05-25 12:43:00 +0000

    CCNS BFCache: evict CCNS BFCache when HTTP auth is required
    If `StoragePartitionImpl::OnAuthRequired()` is called, it means the
    URLLoader receives 401/407 for some page even after attempting to
    authenticate with auth cache. If there is any BFCache entries with
    CCNS header from the same origin, we should evict them since they may
    contain some sensitive information from the previous authenticated
    session.
    
    Bug: 1444299
    Change-Id: I67ac53bbe7da8624ca23a3d1f485394a38cc254e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546088
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149033}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -144,6 +144,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/devtools/inspector_issue.mojom-shared.h"
 #include "third_party/blink/public/mojom/quota/quota_types.mojom.h"
+#include "url/scheme_host_port.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "content/public/browser/android/java_interfaces.h"
@@ -1997,6 +1998,37 @@ void StoragePartitionImpl::OnAuthRequired(
       }
     }
   }
+
+  WebContents* current_web_contents = GetWebContents(context);
+  if (current_web_contents) {
+    // Evict all the BFCache entries that
+    // 1): are stored in the same BrowserContext
+    // 2): contain CCNS header
+    // 3): match the challenger information of the page that requires HTTP
+    // authentication.
+    for (WebContentsImpl* web_contents : WebContentsImpl::GetAllWebContents()) {
+      if (web_contents->GetBrowserContext()->UniqueId() ==
+          current_web_contents->GetBrowserContext()->UniqueId()) {
+        for (const std::unique_ptr<BackForwardCacheImpl::Entry>& entry :
+             web_contents->GetController().GetBackForwardCache().GetEntries()) {
+          RenderFrameHostImpl* rfh = entry->render_frame_host();
+          const GURL& last_committed_url = rfh->GetLastCommittedURL();
+          if (rfh->GetBackForwardCacheDisablingFeatures().Has(
+                  blink::scheduler::WebSchedulerTrackedFeature::
+                      kMainResourceHasCacheControlNoStore) &&
+              auth_info.challenger ==
+                  url::SchemeHostPort(last_committed_url.scheme(),
+                                      last_committed_url.host(),
+                                      last_committed_url.IntPort())) {
+            rfh->EvictFromBackForwardCacheWithReason(
+                BackForwardCacheMetrics::NotRestoredReason::
+                    kCacheControlNoStore);
+          }
+        }
+      }
+    }
+  }
+
   OnAuthRequiredContinuation(
       process_id, request_id, url, *is_primary_main_frame, first_auth_attempt,
       auth_info, head_headers, std::move(auth_challenge_responder),
