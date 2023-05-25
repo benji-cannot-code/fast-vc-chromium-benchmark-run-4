e11b3587502298c95d26e9cea5c185b46e271d42
Ho Cheung <uioptt24@gmail.com>
2023-05-25 18:08:57 +0000

    Manual raw_ptr rewrite: //media
    Bug: 1446392
    Change-Id: Iad0ced003db243c554a27fece0b3917fcf3280e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4565093
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149238}

--- media/gpu/test/video_test_helpers.h
+++ media/gpu/test/video_test_helpers.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/synchronization/condition_variable.h"
 #include "base/synchronization/lock.h"
@@ -80,9 +79,7 @@ StateEnum ClientStateNotification<StateEnum>::Wait() {
 
 struct IvfFrame {
   IvfFrameHeader header;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #union
-  RAW_PTR_EXCLUSION uint8_t* data = nullptr;
+  raw_ptr<uint8_t> data = nullptr;
 };
 
 // Read functions to fill IVF file header and IVF frame header from |data|.
