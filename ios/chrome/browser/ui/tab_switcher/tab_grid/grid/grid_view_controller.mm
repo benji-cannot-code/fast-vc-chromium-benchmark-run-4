27e3e38201612eafb7dfe625d09103d2a7069ead
Louis Romero <lpromero@google.com>
2023-05-25 11:18:02 +0000

    Add the sizing header to the view hierarchy
    Adding that sizing header itself to the hierarchy is apparently
    necessary for it to get the proper environment settings (Dynamic Type
    size for example).
    
    That line was removed in crrev.com/c/4466229 , but actually it didn’t
    fix it entirely.
    
    Bug: 1439200
    Fixed: 1439955
    Change-Id: I1763791f0bb7c572dadf0ae4d890207568f9288f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4559865
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149009}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -2059,6 +2059,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
   // collection. This might be due a UIKit/SwiftUI interaction bug, as this is
   // not necessary for `InactiveTabsPreambleHeader` below for example.
   gHeader.parent = self;
+  [self.view addSubview:gHeader];
 
   CGSize size =
       [gHeader systemLayoutSizeFittingSize:targetSize
