56a2f8c72d3f4c1363c912a603e53665e3ff8bf9
Kenichi Ishibashi <bashi@chromium.org>
2023-05-25 05:33:20 +0000

    Remove expired histograms in BlockFiles
    These are no longer used.
    
    Bug: 1448182
    Change-Id: I535e19224ce886ff3fa1296bb9137da03dc7125b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564175
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148917}

--- net/disk_cache/blockfile/block_files.cc
+++ net/disk_cache/blockfile/block_files.cc
@@ -70,7 +70,6 @@ bool BlockHeader::CreateMapBlock(int size, int* index) {
     return false;
   }
 
-  TimeTicks start = TimeTicks::Now();
   // We are going to process the map on 32-block chunks (32 bits), and on every
   // chunk, iterate through the 8 nibbles where the new block can be located.
   int current = header_->hints[target - 1];
@@ -103,7 +102,6 @@ bool BlockHeader::CreateMapBlock(int size, int* index) {
       if (target != size) {
         header_->empty[target - size - 1]++;
       }
-      LOCAL_HISTOGRAM_TIMES("DiskCache.CreateBlock", TimeTicks::Now() - start);
       return true;
     }
   }
@@ -120,7 +118,6 @@ void BlockHeader::DeleteMapBlock(int index, int size) {
     NOTREACHED();
     return;
   }
-  TimeTicks start = TimeTicks::Now();
   int byte_index = index / 8;
   uint8_t* byte_map = reinterpret_cast<uint8_t*>(header_->allocation_map);
   uint8_t map_block = byte_map[byte_index];
@@ -150,7 +147,6 @@ void BlockHeader::DeleteMapBlock(int index, int size) {
   std::atomic_thread_fence(std::memory_order_seq_cst);
   header_->num_entries--;
   STRESS_DCHECK(header_->num_entries >= 0);
-  LOCAL_HISTOGRAM_TIMES("DiskCache.DeleteBlock", TimeTicks::Now() - start);
 }
 
 // Note that this is a simplified version of DeleteMapBlock().
@@ -524,7 +520,6 @@ MappedFile* BlockFiles::FileForNewBlock(FileType block_type, int block_count) {
   MappedFile* file = block_files_[block_type - 1].get();
   BlockHeader file_header(file);
 
-  TimeTicks start = TimeTicks::Now();
   while (file_header.NeedToGrowBlockFile(block_count)) {
     if (kMaxBlocks == file_header.Header()->max_entries) {
       file = NextFile(file);
@@ -538,8 +533,6 @@ MappedFile* BlockFiles::FileForNewBlock(FileType block_type, int block_count) {
       return nullptr;
     break;
   }
-  LOCAL_HISTOGRAM_TIMES("DiskCache.GetFileForNewBlock",
-                        TimeTicks::Now() - start);
   return file;
 }
 
@@ -606,7 +599,6 @@ bool BlockFiles::RemoveEmptyFile(FileType block_type) {
       block_files_[file_index] = nullptr;
 
       int failure = base::DeleteFile(name) ? 0 : 1;
-      UMA_HISTOGRAM_COUNTS_1M("DiskCache.DeleteFailed2", failure);
       if (failure)
         LOG(ERROR) << "Failed to delete " << name.value() << " from the cache.";
       continue;
