e92ad3e52fbe69ac800482f0531bf00a2ac7949b
Mathijs Affourtit <maffourtit@google.com>
2023-05-25 14:51:10 +0000

    Reland "Remove Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite histogram."
    This reverts commit 990ed74ad85a608e9b5c39df5e1e08d908bf553e.
    
    Reason for revert: Following further investigation this is likely not the cause of: https://bugs.chromium.org/p/chromium/issues/detail?id=1440580
    
    Original change's description:
    > Revert "Remove Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite histogram."
    >
    > This reverts commit 804d774de51f646e4377a9ce89f37b6dcc873b01.
    >
    > Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=1440580 Issue 1440580: Chrome: Crash Report - blink::ChildURLLoaderFactoryBundle::CreateLoaderAndStart
    >
    > Original change's description:
    > > Remove Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite histogram.
    > >
    > > Remove the entry from histograms.xml, the metric logging in resource_fetcher.cc and associated tests in resource_fetcher_test.cc.
    > >
    > >
    > > Bug: 1385063
    > > Change-Id: Ic94e11465b22d923a4d44289cb71fb60b888019b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4460611
    > > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > > Commit-Queue: Mathijs Affourtit <maffourtit@google.com>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/main@{#1135168}
    >
    > Bug: 1385063
    > Change-Id: Ib4b0fea722ea6ca253fc4078266e29acdffd3ec6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483522
    > Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    > Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1136637}
    
    Bug: 1385063
    Change-Id: Ia6a88ae79d5cdcd1b1d1ef46b2ca3f7b49e8c6e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4490209
    Commit-Queue: Mathijs Affourtit <maffourtit@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149096}

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher_test.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher_test.cc
@@ -387,9 +387,6 @@ TEST_F(ResourceFetcherTest, MetricsPerTopFrameSite) {
   EXPECT_EQ(resource_1, resource_2);
 
   // Test histograms.
-  histogram_tester.ExpectTotalCount(
-      "Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite.Mock", 0);
-
   histogram_tester.ExpectTotalCount("Blink.MemoryCache.RevalidationPolicy.Mock",
                                     2);
 
@@ -401,7 +398,7 @@ TEST_F(ResourceFetcherTest, MetricsPerTopFrameSite) {
       0 /* RevalidationPolicy::kUse */, 1);
 
   // Now load the same resource with origin_b as top-frame site. The
-  // PerTopFrameSite histogram should be incremented.
+  // histograms should be incremented.
   auto* fetcher_3 = CreateFetcher();
   ResourceRequestHead request_head_3(url);
   scoped_refptr<const SecurityOrigin> foo_origin_b =
@@ -413,11 +410,6 @@ TEST_F(ResourceFetcherTest, MetricsPerTopFrameSite) {
   Resource* resource_3 =
       MockResource::Fetch(fetch_params_2, fetcher_3, nullptr);
   EXPECT_EQ(resource_1, resource_3);
-  histogram_tester.ExpectTotalCount(
-      "Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite.Mock", 1);
-  histogram_tester.ExpectBucketCount(
-      "Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite.Mock",
-      0 /* RevalidationPolicy::kUse */, 1);
   histogram_tester.ExpectTotalCount("Blink.MemoryCache.RevalidationPolicy.Mock",
                                     3);
   histogram_tester.ExpectBucketCount(
@@ -466,9 +458,6 @@ TEST_F(ResourceFetcherTest, MetricsPerTopFrameSiteOpaqueOrigins) {
   EXPECT_EQ(resource_1, resource_2);
 
   // Test histograms.
-  histogram_tester.ExpectTotalCount(
-      "Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite.Mock", 0);
-
   histogram_tester.ExpectTotalCount("Blink.MemoryCache.RevalidationPolicy.Mock",
                                     2);
 
@@ -480,7 +469,7 @@ TEST_F(ResourceFetcherTest, MetricsPerTopFrameSiteOpaqueOrigins) {
       0 /* RevalidationPolicy::kUse */, 1);
 
   // Now load the same resource with opaque_origin1 as top-frame site. The
-  // PerTopFrameSite histogram should be incremented.
+  // histograms should be incremented.
   auto* fetcher_3 = CreateFetcher();
   ResourceRequestHead request_head_3(url);
   request_head_3.SetTopFrameOrigin(opaque_origin2);
@@ -490,11 +479,6 @@ TEST_F(ResourceFetcherTest, MetricsPerTopFrameSiteOpaqueOrigins) {
   Resource* resource_3 =
       MockResource::Fetch(fetch_params_2, fetcher_3, nullptr);
   EXPECT_EQ(resource_1, resource_3);
-  histogram_tester.ExpectTotalCount(
-      "Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite.Mock", 1);
-  histogram_tester.ExpectBucketCount(
-      "Blink.MemoryCache.RevalidationPolicy.PerTopFrameSite.Mock",
-      0 /* RevalidationPolicy::kUse */, 1);
   histogram_tester.ExpectTotalCount("Blink.MemoryCache.RevalidationPolicy.Mock",
                                     3);
   histogram_tester.ExpectBucketCount(
