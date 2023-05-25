5f638f904bb515927482e072849a79767fae5bf4
Ewann Pelle <ewannpv@chromium.org>
2023-05-25 16:34:04 +0000

    [iOS][Message] Add banner default flow
    This CL adds some logic to retrieve the right coordinator and mediator
    corresponding to the banner overlay that use the new default flow.
    
    Bug: 1447483
    Change-Id: I2a4fb1e8021bb1cf89791951e1f39404d15931cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4562276
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149156}

--- ios/chrome/browser/ui/overlays/overlay_coordinator_factory.mm
+++ ios/chrome/browser/ui/overlays/overlay_coordinator_factory.mm
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/notreached.h"
+#import "ios/chrome/browser/infobars/infobar_ios.h"
+#import "ios/chrome/browser/infobars/infobar_type.h"
+#import "ios/chrome/browser/overlays/public/default/default_infobar_overlay_request_config.h"
 #import "ios/chrome/browser/overlays/public/overlay_request_support.h"
 #import "ios/chrome/browser/ui/overlays/infobar_banner/infobar_banner_overlay_coordinator.h"
 #import "ios/chrome/browser/ui/overlays/infobar_banner/translate/translate_infobar_placeholder_overlay_coordinator.h"
@@ -62,14 +65,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // `request`'s overlay UI.
 // TODO(crbug.com/1447483): Clean the switch when the default flow is added.
 - (Class)coordinatorClassForRequest:(OverlayRequest*)request {
+  if (DefaultInfobarOverlayRequestConfig::RequestSupport()->IsRequestSupported(
+          request)) {
+    DefaultInfobarOverlayRequestConfig* config =
+        request->GetConfig<DefaultInfobarOverlayRequestConfig>();
+    return [self coordinatorClassForInfobarType:config->infobar_type()];
+  }
+
   switch (_modality) {
     case OverlayModality::kTesting:
       // Use TestOverlayRequestCoordinatorFactory to create factories for
       // OverlayModality::kTesting.
       // TODO(crbug.com/1056837): Remove requirement once modalities are
       // converted to no longer use enums.
-      NOTREACHED() << "Received unsupported modality.";
-      break;
+      NOTREACHED_NORETURN() << "Received unsupported modality.";
       return nil;
     case OverlayModality::kWebContentArea:
       return [AlertOverlayCoordinator class];
@@ -96,17 +105,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
               ->IsRequestSupported(request)) {
         return [TranslateInfobarModalOverlayCoordinator class];
       }
-      if (@available(iOS 15.0, *)) {
-        if ([PermissionsInfobarModalOverlayCoordinator requestSupport]
-                ->IsRequestSupported(request)) {
-          return [PermissionsInfobarModalOverlayCoordinator class];
-        }
-      }
       break;
   }
+  NOTREACHED_NORETURN() << "Received unsupported request type.";
+}
 
-  return nil;
-  NOTREACHED() << "Received unsupported request type.";
+// Returns the coordinator class corresponding to the given `infobarType`.
+- (Class)coordinatorClassForInfobarType:(InfobarType)infobarType {
+  switch (_modality) {
+    case OverlayModality::kTesting:
+      // Use TestOverlayRequestCoordinatorFactory to create factories for
+      // OverlayModality::kTesting.
+      // TODO(crbug.com/1056837): Remove requirement once modalities are
+      // converted to no longer use enums.
+      NOTREACHED_NORETURN() << "Received unsupported modality.";
+    case OverlayModality::kWebContentArea:
+      NOTREACHED_NORETURN()
+          << "None implemented yet. Received unsupported modality.";
+    case OverlayModality::kInfobarBanner:
+      return [InfobarBannerOverlayCoordinator class];
+    case OverlayModality::kInfobarModal:
+      switch (infobarType) {
+        case InfobarType::kInfobarTypePermissions:
+          return [PermissionsInfobarModalOverlayCoordinator class];
+        default:
+          break;
+      }
+  }
+  NOTREACHED_NORETURN() << "Received unsupported infobar type.";
 }
 
 @end
