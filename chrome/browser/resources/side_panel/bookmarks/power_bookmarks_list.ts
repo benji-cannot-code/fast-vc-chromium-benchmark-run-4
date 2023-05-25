9516b9b8f998f77cfb063ffcf475ec45c9b94411
Emily Shack <emshack@chromium.org>
2023-05-25 17:49:20 +0000

    Bookmarks++: Remove tree role
    Addresses several a11y issues on NVDA screen reader
    
    Bug: 1448690
    Change-Id: I9b8592798f3659046a72fb9f72ce60ac0bbbce18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563565
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149221}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
@@ -238,7 +238,6 @@ export class PowerBookmarksListElement extends PolymerElement {
 
   override connectedCallback() {
     super.connectedCallback();
-    this.setAttribute('role', 'tree');
     listenOnce(this.$.powerBookmarksContainer, 'dom-change', () => {
       setTimeout(() => this.bookmarksApi_.showUi(), 0);
     });
