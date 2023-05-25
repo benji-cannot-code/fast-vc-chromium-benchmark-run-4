41639dc6380af0c51b695abf444a82419b6cb0d4
Jason Thai <jasontt@chromium.org>
2023-05-25 16:39:19 +0000

    wallpaper: add `SetTimeOfDayWallpaper`
    This method allows the callers to download and set a time of day
    wallpaper as the active wallpaper.
    
    BUG=b/283013683
    TEST=ash_unittests --gtest_filter="WallpaperControllerTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I9c60f5e175336c930a0d1a24a481d806f03dda14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547689
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149162}

--- ash/wallpaper/wallpaper_controller_impl.cc
+++ ash/wallpaper/wallpaper_controller_impl.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/dark_light_mode_controller_impl.h"
 #include "ash/system/scheduled_feature/scheduled_feature.h"
 #include "ash/wallpaper/wallpaper_blur_manager.h"
+#include "ash/wallpaper/wallpaper_constants.h"
 #include "ash/wallpaper/wallpaper_drag_drop_delegate.h"
 #include "ash/wallpaper/wallpaper_image_downloader.h"
 #include "ash/wallpaper/wallpaper_metrics_manager.h"
@@ -1089,6 +1090,20 @@ bool WallpaperControllerImpl::GetDailyGooglePhotosWallpaperIdCache(
     DailyGooglePhotosIdCache& ids_out) const {
   return pref_manager_->GetDailyGooglePhotosWallpaperIdCache(account_id,
                                                              ids_out);
+}
+
+void WallpaperControllerImpl::SetTimeOfDayWallpaper(
+    const AccountId& account_id,
+    SetWallpaperCallback callback) {
+  OnlineWallpaperVariantInfoFetcher::FetchParamsCallback on_fetch =
+      base::BindOnce(&WallpaperControllerImpl::OnWallpaperVariantsFetched,
+                     set_wallpaper_weak_factory_.GetWeakPtr(),
+                     WallpaperType::kOnline,
+                     /*start_daily_refresh_timer=*/false, std::move(callback));
+  variant_info_fetcher_->FetchTimeOfDayWallpaper(
+      account_id, wallpaper_constants::kDefaultTimeOfDayWallpaperUnitId,
+      Shell::Get()->dark_light_mode_controller()->current_checkpoint(),
+      std::move(on_fetch));
 }
 
 void WallpaperControllerImpl::SetDefaultWallpaper(
