70ef0fc0e6044096fdc4243e79e3b01b55f2b421
Jonathan Lee <jonathanjlee@google.com>
2023-05-25 02:22:55 +0000

    Import wpt@b22c9bb52f359e46d30f8812d6cc83c2742816e4
    Using wpt-import in Chromium 913c8e5917aa3aeb4ed00fe567f08c15c7d3f973.
    With Chromium commits locally applied on WPT:
    3b9ea3f51e "Migrate gentestutilsunion.py to Jinja2."
    3db6b7bd0e "Fix color-valid-color-mix-function test"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kojii@chromium.org:
      external/wpt/css/CSS2/text
    rego@igalia.com:
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I6f0136b7d1808deb49064a3b0a6aca35df318e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4561977
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148880}

---
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/classic/perform_actions/sequence.py
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# META: timeout=long
+
+from tests.classic.perform_actions.support.refine import get_events, get_keys
+
+
+def test_perform_no_actions_send_no_events(session, key_reporter, key_chain):
+    key_chain.perform()
+    assert len(get_keys(key_reporter)) == 0
+    assert len(get_events(session)) == 0
