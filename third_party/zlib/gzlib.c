ff5e2632139182e34e722ece79a1914c5908c820
Ramin Halavati <rhalavati@chromium.org>
2023-05-25 14:43:59 +0000

    Add open64 for Large File System support to gzlib.
    The convention of large file support is passing 3 build flags -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE. Then we can expect open(), lseek(), fopen(), etc. calls to be defined as the LFS-enabled counterparts like open64(), fopen64().
    
    zlib doesn't follow the convention of large file support on Linux and undefines the build flag -D_FILE_OFFSET_BITS=64 in gzguts.h included by gzlib.c. We decide to have a local fix in gzlib.c without an intrusive fix to gzguts.h. This looks to be fine because lseek64 is handled similarly.
    
    Bug: 1445975
    Change-Id: I5ebed3a51ca106a5d0355d5b882fc2a2d48e7368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543405
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149090}

--- third_party/zlib/gzlib.c
+++ third_party/zlib/gzlib.c
@@ -8,11 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(_WIN32) && !defined(__BORLANDC__)
 #  define LSEEK _lseeki64
+#  define OPEN  open
 #else
 #if defined(_LARGEFILE64_SOURCE) && _LFS64_LARGEFILE-0
 #  define LSEEK lseek64
+#  define OPEN  open64
 #else
 #  define LSEEK lseek
+#  define OPEN  open
 #endif
 #endif
 
@@ -245,7 +248,7 @@ local gzFile gz_open(path, fd, mode)
 #ifdef WIDECHAR
         fd == -2 ? _wopen(path, oflag, 0666) :
 #endif
-        open((const char *)path, oflag, 0666));
+        OPEN((const char *)path, oflag, 0666));
     if (state->fd == -1) {
         free(state->path);
         free(state);
