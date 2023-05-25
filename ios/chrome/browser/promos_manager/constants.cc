e13b3d5e3e3757ace4c1611ba90a409a828e757f
Samar <samarchehade@google.com>
2023-05-25 18:42:38 +0000

    [Choice][iOS] Add a new promo type for the choice screen.
    Bug: b/280753739
    Change-Id: Id8ec18d07c3780a3230b6c0b8f637d8be05d1300
    Low-Coverage-Reason: No new logic added.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543163
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1149262}

--- ios/chrome/browser/promos_manager/constants.cc
+++ ios/chrome/browser/promos_manager/constants.cc
@@ -40,6 +40,10 @@ absl::optional<Promo> PromoForName(base::StringPiece promo) {
   if (promo == "promos_manager::Promo::WhatsNew")
     return promos_manager::Promo::WhatsNew;
 
+  if (promo == "promos_manager::Promo::Choice") {
+    return promos_manager::Promo::Choice;
+  }
+
   return absl::nullopt;
 }
 
@@ -59,6 +63,8 @@ base::StringPiece NameForPromo(Promo promo) {
       return "promos_manager::Promo::PostRestoreSignInAlert";
     case promos_manager::Promo::WhatsNew:
       return "promos_manager::Promo::WhatsNew";
+    case promos_manager::Promo::Choice:
+      return "promos_manager::Promo::Choice";
   }
 }
 
