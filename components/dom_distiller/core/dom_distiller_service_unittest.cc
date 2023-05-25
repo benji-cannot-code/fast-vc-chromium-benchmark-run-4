4a2bcf6d8f1e080188f5b6e3663f6a9f44892671
Tom Sepez <tsepez@chromium.org>
2023-05-25 20:26:32 +0000

    Avoid dangling ptr warning in ~DomDistillerServiceTest()
    Drop unowned reference to MockDistillerPageFactory before destroying
    the DomDistillerService which owns it as part of TearDown().
    
    Bug: 1401495
    Change-Id: I1e8946a51a8685da6d13e8cf3677079c71f5cd8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564115
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149331}

--- components/dom_distiller/core/dom_distiller_service_unittest.cc
+++ components/dom_distiller/core/dom_distiller_service_unittest.cc
@@ -72,6 +72,7 @@ class DomDistillerServiceTest : public testing::Test {
 
   void TearDown() override {
     base::RunLoop().RunUntilIdle();
+    distiller_page_factory_ = nullptr;
     distiller_factory_ = nullptr;
     service_.reset();
   }
