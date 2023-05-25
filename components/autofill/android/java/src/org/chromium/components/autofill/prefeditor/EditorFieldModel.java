f29f53d284449023bb7b224e267c46e4773006cc
Timofey Chudakov <tchudakov@google.com>
2023-05-25 16:17:12 +0000

    [Android][Autofill] Remove icon generator from EditorTextField.
    Remove unused code from EditorTextField as part of converting
    EditorFieldModel into PropertyModel.
    
    Bug: 1435314
    Change-Id: I2d034c7561c7247c061501fa8997cd1260e2e00f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547541
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1149142}

--- components/autofill/android/java/src/org/chromium/components/autofill/prefeditor/EditorFieldModel.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/prefeditor/EditorFieldModel.java
@@ -47,18 +47,6 @@ public class EditorFieldModel {
     }
 
     /**
-     * The interface to be implemented by the field value icon generator.
-     */
-    public interface EditorValueIconGenerator {
-        /**
-         * Called to get the field value icon resource Id.
-         * @param value The value of the field.
-         * @return The resouce Id of the value icon, 0 indicates no icon.
-         */
-        int getIconResourceId(@Nullable CharSequence value);
-    }
-
-    /**
      * A convenience class for displaying keyed values in a dropdown.
      */
     public static class DropdownKeyValue extends Pair<String, CharSequence> {
@@ -143,8 +131,6 @@ public class EditorFieldModel {
     private TextWatcher mFormatter;
     @Nullable
     private EditorFieldValidator mValidator;
-    @Nullable
-    private EditorValueIconGenerator mValueIconGenerator;
     @Nullable
     private CharSequence mRequiredErrorMessage;
     @Nullable
@@ -241,17 +227,15 @@ public class EditorFieldModel {
      */
     public static EditorFieldModel createTextInput(int inputTypeHint, CharSequence label,
             @Nullable Set<CharSequence> suggestions, @Nullable TextWatcher formatter,
-            @Nullable EditorFieldValidator validator,
-            @Nullable EditorValueIconGenerator valueIconGenerator,
-            @Nullable CharSequence requiredErrorMessage, @Nullable CharSequence invalidErrorMessage,
-            int lengthCounterLimit, @Nullable CharSequence value) {
+            @Nullable EditorFieldValidator validator, @Nullable CharSequence requiredErrorMessage,
+            @Nullable CharSequence invalidErrorMessage, int lengthCounterLimit,
+            @Nullable CharSequence value) {
         assert label != null;
         EditorFieldModel result = new EditorFieldModel(inputTypeHint);
         assert result.isTextField();
         result.mSuggestions = suggestions == null ? null : new ArrayList<CharSequence>(suggestions);
         result.mFormatter = formatter;
         result.mValidator = validator;
-        result.mValueIconGenerator = valueIconGenerator;
         result.mInvalidErrorMessage = invalidErrorMessage;
         result.mRequiredErrorMessage = requiredErrorMessage;
         result.mLabel = label;
@@ -270,12 +254,6 @@ public class EditorFieldModel {
     public TextWatcher getFormatter() {
         assert isTextField();
         return mFormatter;
-    }
-
-    /** @return The value icon generator or null if not exist. */
-    public EditorValueIconGenerator getValueIconGenerator() {
-        assert isTextField();
-        return mValueIconGenerator;
     }
 
     /** @return Whether the input is a text field. */
