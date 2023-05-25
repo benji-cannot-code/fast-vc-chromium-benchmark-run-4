f383361a5fcda5224037f3f2aff80817c62fd13d
Ivana Žužić <izuzic@google.com>
2023-05-25 15:07:19 +0000

    Reland "[UPMLocalPwd] Add the second fragment"
    This is a reland of commit 1c80ecc640b4a65905a9117e5457b09e0fce2196
    
    Introduced changes:
    - Updates to the model in the render test guarantee that the current
    fragment will only be added after the main layout is present
    be shown once the sheet is visible and has the layout to place the
    - The render test has animations disabled to show the checked radio
    button more reliably. It's still flaky so it's disabled for now.
    
    Original change's description:
    > [UPMLocalPwd] Add the second fragment
    >
    > The second fragment contains radio buttons for more options:
    > password export or syncing/signing in.
    > At the moment the first offered option is always looking like the
    > user is not signed in.
    >
    > Bug: 1439761
    > Change-Id: I890f8686bcff931cb966dbce93b7e35463e1dc1d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4527856
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1147148}
    
    Bug: 1439761
    Change-Id: I83867a38ba0fd404696fd924ecfb690d46a26b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545253
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1149105}

--- chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PasswordMigrationWarningMediator.java
+++ chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PasswordMigrationWarningMediator.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.pwd_migration;
 
+import static org.chromium.chrome.browser.pwd_migration.PasswordMigrationWarningProperties.CURRENT_SCREEN;
 import static org.chromium.chrome.browser.pwd_migration.PasswordMigrationWarningProperties.VISIBLE;
 
+import org.chromium.chrome.browser.pwd_migration.PasswordMigrationWarningProperties.ScreenType;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.StateChangeReason;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -14,15 +17,18 @@ import org.chromium.ui.modelutil.PropertyModel;
  * Contains the logic for the local passwords migration warning. It sets the state of the model and
  * reacts to events.
  */
-class PasswordMigrationWarningMediator {
+class PasswordMigrationWarningMediator implements PasswordMigrationWarningOnClickHandler {
     private PropertyModel mModel;
 
     void initialize(PropertyModel model) {
         mModel = model;
     }
 
-    void showWarning() {
+    void showWarning(int screenType) {
+        // TODO(crbug.com/1448503): Ensure that the sheet is shown before adding the current
+        // fragment.
         mModel.set(VISIBLE, true);
+        mModel.set(CURRENT_SCREEN, screenType);
     }
 
     void onDismissed(@StateChangeReason int reason) {
@@ -30,7 +36,24 @@ class PasswordMigrationWarningMediator {
         mModel.set(VISIBLE, false);
     }
 
-    PropertyModel getModel() {
-        return mModel;
+    @Override
+    public void onAcknowledge(BottomSheetController bottomSheetController) {
+        bottomSheetController.collapseSheet(true);
+    }
+
+    @Override
+    public void onMoreOptions() {
+        assert mModel.get(VISIBLE);
+        mModel.set(CURRENT_SCREEN, ScreenType.OPTIONS_SCREEN);
+    }
+
+    @Override
+    public void onNext() {
+        // TODO(crbug.com/1445065): Launch the password Export flow.
+    }
+
+    @Override
+    public void onCancel(BottomSheetController bottomSheetController) {
+        bottomSheetController.collapseSheet(true);
     }
 }
