92abfe44d648987e46e5defc2d43c3c0bc94a4bb
Yuki Shiino <yukishiino@chromium.org>
2023-05-25 15:48:57 +0000

    PA: Remove deps to base/process/memory.h from allocator shims
    Replaces base::TerminateBecauseOutOfMemory with
    partition_alloc::TerminateBecauseOutOfMemory, and removes the deps
    to base/process/memory.h.
    
    Bug: 1337681
    Change-Id: Ie341c261c3ef5be3793d8cef6f6f7b87ba659a0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564834
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1149126}

--- base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_glibc.cc
+++ base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_glibc.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/allocator/partition_allocator/oom.h"
 #include "base/allocator/partition_allocator/partition_alloc_base/compiler_specific.h"
 #include "base/allocator/partition_allocator/partition_alloc_base/numerics/checked_math.h"
 #include "base/allocator/partition_allocator/shim/allocator_shim.h"
-#include "base/process/memory.h"
 
 #include <dlfcn.h>
 #include <malloc.h>
@@ -38,7 +38,7 @@ void* GlibcMalloc(const AllocatorDispatch*, size_t size, void* context) {
   // Cannot force glibc's malloc() to crash when a large size is requested, do
   // it in the shim instead.
   if (PA_UNLIKELY(size >= kMaxAllowedSize)) {
-    base::TerminateBecauseOutOfMemory(size);
+    partition_alloc::TerminateBecauseOutOfMemory(size);
   }
 
   return __libc_malloc(size);
@@ -60,7 +60,7 @@ void* GlibcCalloc(const AllocatorDispatch*,
                   void* context) {
   const auto total = partition_alloc::internal::base::CheckMul(n, size);
   if (PA_UNLIKELY(!total.IsValid() || total.ValueOrDie() >= kMaxAllowedSize)) {
-    base::TerminateBecauseOutOfMemory(size * n);
+    partition_alloc::TerminateBecauseOutOfMemory(size * n);
   }
 
   return __libc_calloc(n, size);
@@ -71,7 +71,7 @@ void* GlibcRealloc(const AllocatorDispatch*,
                    size_t size,
                    void* context) {
   if (PA_UNLIKELY(size >= kMaxAllowedSize)) {
-    base::TerminateBecauseOutOfMemory(size);
+    partition_alloc::TerminateBecauseOutOfMemory(size);
   }
 
   return __libc_realloc(address, size);
@@ -82,7 +82,7 @@ void* GlibcMemalign(const AllocatorDispatch*,
                     size_t size,
                     void* context) {
   if (PA_UNLIKELY(size >= kMaxAllowedSize)) {
-    base::TerminateBecauseOutOfMemory(size);
+    partition_alloc::TerminateBecauseOutOfMemory(size);
   }
 
   return __libc_memalign(alignment, size);
