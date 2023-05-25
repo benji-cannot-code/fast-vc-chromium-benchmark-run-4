2d5d09b6137acdf7c05ad64fa8c4f30ec2a1e5ed
Melanie Lobo <melanie.lobo@intel.com>
2023-05-25 11:31:10 +0000

    Add new Tile 4 modifier in vaapi_unittest
    This modifies vaapi_unittest for a new tiled format,
    I915_FORMAT_MOD_4_TILED, which is supported by MTL and
    used by default for hardware video decoding.
    
    Bug: b:272653468
    Test: grahpics.VaapiUnittest.common
    
    Change-Id: Ifb80f4a5671005839a90f579a0c558c61a6aa0af
    Signed-off-by: Melanie Lobo <melanie.lobo@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4536655
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149014}

--- media/gpu/vaapi/vaapi_unittest.cc
+++ media/gpu/vaapi/vaapi_unittest.cc
@@ -49,6 +49,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/ozone_platform.h"  // nogncheck
 #endif
 
+#ifndef I915_FORMAT_MOD_4_TILED
+#define I915_FORMAT_MOD_4_TILED 0x100000000000009
+#endif
+
 namespace media {
 namespace {
 
@@ -224,6 +228,18 @@ const char* VAProfileToString(VAProfile profile) {
   }
   // clang-format on
   return "<unknown profile>";
+}
+
+// Returns true if the Display version is 14. CPU model ID's are referenced from
+// the following file in the kernel source: arch/x86/include/asm/intel-family.h.
+bool IsDisplayVer14() {
+  constexpr int kMeteorLakeModelId = 0xAC;
+  constexpr int kMeteorLake_LModelId = 0xAA;
+  constexpr int kPentiumAndLaterFamily = 0x06;
+  const base::CPU cpuid;
+  return cpuid.family() == kPentiumAndLaterFamily &&
+         (cpuid.model() == kMeteorLakeModelId ||
+          cpuid.model() == kMeteorLake_LModelId);
 }
 
 }  // namespace
@@ -816,7 +832,8 @@ TEST_P(VaapiMinigbmTest, AllocateAndCompareWithMinigbm) {
   uint64_t expected_drm_modifier = DRM_FORMAT_MOD_LINEAR;
 
   if (backend == VAImplementation::kIntelIHD) {
-    expected_drm_modifier = I915_FORMAT_MOD_Y_TILED;
+    expected_drm_modifier =
+        IsDisplayVer14() ? I915_FORMAT_MOD_4_TILED : I915_FORMAT_MOD_Y_TILED;
   } else if (backend == VAImplementation::kMesaGallium) {
     if (va_vendor_string.find("STONEY") != std::string::npos) {
       expected_drm_modifier = DRM_FORMAT_MOD_INVALID;
