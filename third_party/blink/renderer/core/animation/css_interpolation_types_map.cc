75db39615984cf59c1ca6bdbdcd3057d260a77ab
Munira Tursunova <moonira@google.com>
2023-05-25 12:15:32 +0000

    Implement interpolation of font-palette property
    Explainer: https://docs.google.com/document/d/1XMTrKH003KBOes6hxzI-3E7LTwp5YwFC-rnzoFpFrfw/edit?usp=sharing
    Design doc:
    https://docs.google.com/document/d/1S9JJfjmBtmOVYyP9AmdTFsKfWetU7drsc2v7VSDQXoI/edit?usp=sharing&resourcekey=0-O21aXZY1z6MoClTB-cKmlw
    
    Bug: 1400620
    Low-Coverage-Reason: Coverage is incorrectly identified likely due to test being executed in virtual test suite.
    Change-Id: Ia6b31b321fcf4284996511a8cbf303d11fc94f61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4487257
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149025}

--- third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
+++ third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/css_default_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_display_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_filter_list_interpolation_type.h"
+#include "third_party/blink/renderer/core/animation/css_font_palette_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_size_adjust_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_size_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_stretch_interpolation_type.h"
@@ -312,6 +313,12 @@ const InterpolationTypes& CSSInterpolationTypesMap::Get(
         applicable_types->push_back(
             std::make_unique<CSSFontVariationSettingsInterpolationType>(
                 used_property));
+        break;
+      case blink::CSSPropertyID::kFontPalette:
+        if (RuntimeEnabledFeatures::FontPaletteAnimationEnabled()) {
+          applicable_types->push_back(
+              std::make_unique<CSSFontPaletteInterpolationType>(used_property));
+        }
         break;
       case CSSPropertyID::kVisibility:
         applicable_types->push_back(
