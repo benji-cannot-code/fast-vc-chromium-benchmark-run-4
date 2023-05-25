537350a66c635e200b8f3be4098732f9ace6bef9
Joshua Pawlicki <waffles@chromium.org>
2023-05-25 16:32:52 +0000

    Updater: Wake everything from the backup scheduler.
    Previously the code would only wake the active version, which is
    insufficient to complete self-updates.
    
    Bug: 1447050
    Change-Id: I39440418e72e0d0219bca54da802bc037314ef54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567706
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149154}

--- chrome/updater/mac/keystone/agent_main.cc
+++ chrome/updater/mac/keystone/agent_main.cc
@@ -194,7 +194,7 @@ void KSAgentApp::Wake() {
       continue;
     }
     base::CommandLine command(*path);
-    command.AppendSwitch(kWakeSwitch);
+    command.AppendSwitch(kWakeAllSwitch);
     if (scope == UpdaterScope::kSystem) {
       command.AppendSwitch(kSystemSwitch);
     }
