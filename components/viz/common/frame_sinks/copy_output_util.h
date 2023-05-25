6de2a9a50a2465d9c752c26577aafb170e0de6ef
Colin Blundell <blundell@chromium.org>
2023-05-25 16:00:02 +0000

    [//viz] Remove copy_output_util dead code
    This code has no callers in the codebase, and removing it clarifies
    the usage of CopyOutputResult::Format::NV12_PLANES within the codebase.
    
    Bug: 1429004
    Change-Id: I57967d68f34345f71d35d7c17bf0fb98fdf96a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4565294
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149130}

--- components/viz/common/frame_sinks/copy_output_util.h
+++ components/viz/common/frame_sinks/copy_output_util.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace viz {
 
-class CopyOutputResult;
-
 namespace copy_output {
 
 // Returns the pixels in the scaled result coordinate space that are affected by
@@ -51,30 +49,6 @@ struct VIZ_COMMON_EXPORT RenderPassGeometry {
 
   std::string ToString() const;
 };
-
-// Returns size (in bytes) required to fit luma plane of the |result|. The
-// |result| must not be an empty `CopyOutputResponse`. The pixel format of the
-// |result| must be either I420 or NV12.
-int VIZ_COMMON_EXPORT GetLumaPlaneSize(const CopyOutputResult& result);
-
-// Returns stride (in bytes) of the luma plane of the |result|. The |result|
-// must not be an empty `CopyOutputResponse`. The pixel format of the |result|
-// must be either I420 or NV12.
-int VIZ_COMMON_EXPORT GetLumaPlaneStride(const CopyOutputResult& result);
-
-// Returns size (in bytes) required to fit chroma plane(s) of the |result|. The
-// |result| must not be an empty `CopyOutputResponse`. The pixel format of the
-// |result| must be either I420 or NV12. For NV12, the return value will be the
-// byte size required for an interleaved UV plane. For I420, the return value
-// will be the byte size required for each of the U & V planes.
-int VIZ_COMMON_EXPORT GetChromaPlaneSize(const CopyOutputResult& result);
-
-// Returns stride (in bytes) of the chroma plane(s) of the |result|. The
-// |result| must not be an empty `CopyOutputResponse`. The pixel format of the
-// |result| must be either I420 or NV12. For NV12, the return value will be the
-// stride of an interleaved UV plane. For I420, the return value will be the
-// stride of each of the U & V planes.
-int VIZ_COMMON_EXPORT GetChromaPlaneStride(const CopyOutputResult& result);
 
 }  // namespace copy_output
 }  // namespace viz
