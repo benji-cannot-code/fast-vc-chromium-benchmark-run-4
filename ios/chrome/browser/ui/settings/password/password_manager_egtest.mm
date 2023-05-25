c70755e3be2458bf5d4dac2db09be3d23e5c953c
Hira Mahmood <hiramahmood@google.com>
2023-05-25 13:55:20 +0000

    [iOS] Update field trial config, about_flags for CPE promo
    This change:
    * Updates the testing_field_trial_config.json and about_flags
      to add a new experiment/feature flag param.
    * Disables the CPE promo feature in the password manager tests.
    
    In a follow up CL, the password manager tests will be updated to
    reenable and check for the CPE promo.
    
    Bug: None
    Change-Id: I64d1d719ca3654dbcec35cbcdf7d228486766233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4562934
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149063}

--- ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/base/features.h"
 #import "components/sync/base/sync_prefs.h"
 #import "components/sync/base/user_selectable_type.h"
+#import "ios/chrome/browser/credential_provider_promo/features.h"
 #import "ios/chrome/browser/metrics/metrics_app_interface.h"
 #import "ios/chrome/browser/policy/policy_earl_grey_utils.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
@@ -580,6 +581,10 @@ id<GREYMatcher> EditDoneButton() {
     config.features_disabled.push_back(
         password_manager::features::kIOSPasswordCheckup);
   }
+
+  // TODO(crbug.com/1448574): Re-enable CPE promo and update
+  // testCopyPasswordToast and testCopyPasswordMenuItem to check for the promo.
+  config.features_disabled.push_back(kCredentialProviderExtensionPromo);
 
   return config;
 }
