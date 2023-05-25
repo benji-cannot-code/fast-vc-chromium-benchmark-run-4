ea9e3d302fe5ef5f270159a479e909077775d1cf
Xiaodan Zhu <zxdan@chromium.org>
2023-05-25 22:33:06 +0000

    Remove logout button background color
    This cl removes the background color of the logout button.
    
    TrayBackgroundView sets a color to the layer on theme changed which
    forces the layer to have the color. This should be removed because
    the layer color should be set in UpdateBackground.
    
    Bug: b:283319985
    Change-Id: I7ca145fae0a09912ecf43c3bcac84caca41918b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569264
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149412}

--- ash/system/tray/tray_background_view.cc
+++ ash/system/tray/tray_background_view.cc
@@ -580,8 +580,6 @@ std::unique_ptr<ui::Layer> TrayBackgroundView::RecreateLayer() {
 void TrayBackgroundView::OnThemeChanged() {
   ActionableView::OnThemeChanged();
   UpdateBackground();
-  layer()->SetColor(
-      ShelfConfig::Get()->GetShelfControlButtonColor(GetWidget()));
   StyleUtil::ConfigureInkDropAttributes(this, StyleUtil::kBaseColor |
                                                   StyleUtil::kInkDropOpacity |
                                                   StyleUtil::kHighlightOpacity);
