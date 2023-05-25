9f8ee71f7b39c91174ba7abe0a7ed6b686d70408
Dylan Cutler <dylancutler@google.com>
2023-05-25 18:43:44 +0000

    Enable FedCM when --block-third-party-cookies switch is used
    I verified this change enables FedCM manually using the demo on fedcm-[ip|rp]-demo.glitch.me.
    
    Bug: 1441039
    Change-Id: I7f3670ea486454ff9412bd10fa26b6687f1648ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567704
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149264}

--- chrome/browser/metrics/chrome_feature_list_creator.cc
+++ chrome/browser/metrics/chrome_feature_list_creator.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "cc/base/switches.h"
 #include "chrome/browser/about_flags.h"
+#include "chrome/browser/browser_features.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/first_run/first_run.h"
@@ -54,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_switches.h"
 #include "content/public/common/content_switch_dependent_feature_overrides.h"
 #include "content/public/common/content_switches.h"
+#include "services/network/public/cpp/network_switches.h"
 #include "ui/base/resource/resource_bundle.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -86,6 +88,10 @@ GetSwitchDependentFeatureOverrides(const base::CommandLine& command_line) {
       // overrides.
       {switches::kEnablePrivacySandboxAdsApis,
        std::cref(privacy_sandbox::kOverridePrivacySandboxSettingsLocalTesting),
+       base::FeatureList::OVERRIDE_ENABLE_FEATURE},
+      // Enabled FedCM when the --block-third-party-cookies switch is used.
+      {network::switches::kBlockThirdPartyCookies,
+       std::cref(features::kFedCmWithoutThirdPartyCookies),
        base::FeatureList::OVERRIDE_ENABLE_FEATURE},
   };
 
