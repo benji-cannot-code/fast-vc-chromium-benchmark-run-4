b87dff78d572a116babdc1410b5c12bfec2a1da7
Sky Malice <skym@chromium.org>
2023-05-26 17:59:42 +0000

    Wire up bookmark search box row to model.
    Bug: 1439583
    Change-Id: I6a1ba6c605c1744ef44cd2f415390cf1e214acde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567820
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149806}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSearchBoxRow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSearchBoxRow.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import org.chromium.base.Callback;
 import org.chromium.chrome.R;
 import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.text.EmptyTextWatcher;
@@ -24,6 +25,8 @@ import org.chromium.ui.text.EmptyTextWatcher;
  * bookmarks.
  */
 public class BookmarkSearchBoxRow extends LinearLayout {
+    private Callback<String> mQueryCallback;
+
     public BookmarkSearchBoxRow(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
@@ -36,10 +39,14 @@ public class BookmarkSearchBoxRow extends LinearLayout {
         searchText.addTextChangedListener(new EmptyTextWatcher() {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // TODO(https://crbug.com/1439583): Propagate query string upward.
+                mQueryCallback.onResult(charSequence.toString());
             }
         });
         searchText.setOnEditorActionListener(this::onEditorAction);
+    }
+
+    void setQueryCallback(Callback<String> queryCallback) {
+        mQueryCallback = queryCallback;
     }
 
     private boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSearchBoxRowProperties.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSearchBoxRowProperties.java
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.bookmarks;
 
+import org.chromium.base.Callback;
 import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.ReadableObjectPropertyKey;
 
 /** Responsible for hosting properties for {@link BookmarkSearchBoxRow}. */
 class BookmarkSearchBoxRowProperties {
-    static final PropertyKey[] ALL_KEYS = {BookmarkManagerProperties.BOOKMARK_LIST_ENTRY};
+    public static final ReadableObjectPropertyKey<Callback<String>> QUERY_CALLBACK =
+            new ReadableObjectPropertyKey<>();
+
+    static final PropertyKey[] ALL_KEYS = {QUERY_CALLBACK};
 }
