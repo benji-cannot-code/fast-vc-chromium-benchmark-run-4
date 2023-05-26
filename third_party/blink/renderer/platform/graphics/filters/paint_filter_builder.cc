2766b7f5579f464172e22345818532addb73248f
Kevin Lubick <kjlubick@google.com>
2023-05-26 17:55:22 +0000

    Remove unused skia/effects headers
    These are no longer used and Skia hopes to delete these files.
    
    Change-Id: Ibc94a443a917ba8a04b6c204be5f0ceea6a45544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4566410
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149802}

--- third_party/blink/renderer/platform/graphics/filters/paint_filter_builder.cc
+++ third_party/blink/renderer/platform/graphics/filters/paint_filter_builder.cc
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/paint_record.h"
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/skia/include/core/SkImage.h"
-#include "third_party/skia/include/effects/SkColorMatrixFilter.h"
-#include "third_party/skia/include/effects/SkTableColorFilter.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 
 namespace blink {
