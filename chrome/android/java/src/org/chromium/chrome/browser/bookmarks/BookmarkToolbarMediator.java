065d28bea98ef1a437818ae2093e7d8d1ba8f32e
Brandon Wylie <wylieb@chromium.org>
2023-05-26 01:25:11 +0000

    [bookmarks] Add bookmark folder select row
    Add new folder select row to show in the edit screen. This will also be
    used for the folder selection activity.
    * Add new UI component, properties and coordinator.
    * Use it in BookmarkEditActivity.
    * Reuse BookamrkEditActivity for editing folders.
    
    Bug: 1448675
    Change-Id: Icd90dd0a8f69f1282cfd134c676021cddd560545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4562481
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149487}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarMediator.java
@@ -32,7 +32,6 @@ import org.chromium.components.browser_ui.widget.selectable_list.SelectionDelega
 import org.chromium.ui.modelutil.PropertyModel;
 
 import java.util.List;
-import java.util.Objects;
 
 /** Responsible for the business logic for the BookmarkManagerToolbar. */
 class BookmarkToolbarMediator implements BookmarkUiObserver, DragListener,
@@ -121,7 +120,11 @@ class BookmarkToolbarMediator implements BookmarkUiObserver, DragListener,
             mModel.set(BookmarkToolbarProperties.CHECKED_VIEW_MENU_ID, id);
             return true;
         } else if (id == R.id.edit_menu_id) {
-            BookmarkAddEditFolderActivity.startEditFolderActivity(mContext, mCurrentFolder);
+            if (BookmarkFeatures.isAndroidImprovedBookmarksEnabled()) {
+                BookmarkUtils.startEditActivity(mContext, mCurrentFolder);
+            } else {
+                BookmarkAddEditFolderActivity.startEditFolderActivity(mContext, mCurrentFolder);
+            }
             return true;
         } else if (id == R.id.close_menu_id) {
             BookmarkUtils.finishActivityOnPhone(mContext);
@@ -268,7 +271,7 @@ class BookmarkToolbarMediator implements BookmarkUiObserver, DragListener,
         // New folder button.
         if (BookmarkFeatures.isAndroidImprovedBookmarksEnabled()) {
             mModel.set(BookmarkToolbarProperties.NEW_FOLDER_BUTTON_VISIBLE,
-                    isAddNewFolderButtonVisible());
+                    BookmarkUtils.canAddSubfolder(mBookmarkModel, mCurrentFolder));
         }
     }
 
@@ -306,10 +309,5 @@ class BookmarkToolbarMediator implements BookmarkUiObserver, DragListener,
                 return R.id.sort_by_reverse_alpha;
         }
         return ResourcesCompat.ID_NULL;
-    }
-
-    private boolean isAddNewFolderButtonVisible() {
-        return !Objects.equals(mCurrentFolder, mBookmarkModel.getReadingListFolder())
-                && !Objects.equals(mCurrentFolder, mBookmarkModel.getPartnerFolderId());
     }
 }
