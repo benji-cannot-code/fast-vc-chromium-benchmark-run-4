614009319c659e5d493810c95157f500bfbf32d0
Tomasz Wiszkowski <ender@google.com>
2023-05-26 22:18:21 +0000

    Actions in Suggest: Simplify background rounding logic.
    Now that Actions row is integrated into the SuggestionLayout, this
    change simplifies the suggestion rounding management.
    
    Bug: 1418077
    Change-Id: Id5a18d5a3d36dd46e314447d6cf34105abb81cc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570918
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1149950}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/SuggestionLayout.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/SuggestionLayout.java
@@ -17,6 +17,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
 import org.chromium.chrome.browser.omnibox.R;
+import org.chromium.components.browser_ui.widget.RoundedCornerOutlineProvider;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -36,6 +37,7 @@ class SuggestionLayout extends ViewGroup {
     public final @Px int mCompactContentHeightPx;
     private final @Px int mActionButtonWidthPx;
     private final @Px int mContentPaddingPx;
+    private final @NonNull RoundedCornerOutlineProvider mOutlineProvider;
 
     /**
      * SuggestionLayout's LayoutParams.
@@ -156,6 +158,15 @@ class SuggestionLayout extends ViewGroup {
 
         mContentPaddingPx =
                 getResources().getDimensionPixelSize(R.dimen.omnibox_suggestion_content_padding);
+
+        mOutlineProvider = new RoundedCornerOutlineProvider(getResources().getDimensionPixelSize(
+                R.dimen.omnibox_suggestion_bg_round_corner_radius));
+        setOutlineProvider(mOutlineProvider);
+    }
+
+    public void setRoundingEdges(boolean roundTopEdge, boolean roundBottomEdge) {
+        mOutlineProvider.setRoundingEdges(true, roundTopEdge, true, roundBottomEdge);
+        setClipToOutline(roundTopEdge || roundBottomEdge);
     }
 
     @Override
