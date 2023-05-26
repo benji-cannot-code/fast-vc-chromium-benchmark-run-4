36aabe5ad7064f2deee5c09495cf1e67d9dcd676
Zonghan Xu <xzonghan@google.com>
2023-05-26 18:49:50 +0000

    [DTC] Add Device Signals Disclosure to chrome://management
    Add a disclosure when device signals are being shared in the chrome://management page
    
    Bug:b:277901759
    
    Change-Id: I7bab126b3fbc79702f8088501452d2c153888bdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549539
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149839}

--- chrome/browser/ui/webui/management/management_ui_handler.cc
+++ chrome/browser/ui/webui/management/management_ui_handler.cc
@@ -87,6 +87,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/user_cloud_policy_manager.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#include "chrome/browser/enterprise/signals/user_permission_service_factory.h"
+#include "components/device_signals/core/browser/user_permission_service.h"  // nogncheck
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/common/extensions/permissions/chrome_permission_message_provider.h"
@@ -178,6 +183,11 @@ enum class ReportingType {
 const char kManagementScreenCaptureEvent[] = "managementScreenCaptureEvent";
 const char kManagementScreenCaptureData[] = "managementScreenCaptureData";
 #endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+const char kManagementDeviceSignalsDisclosure[] =
+    "managementDeviceSignalsDisclosure";
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 #if BUILDFLAG(IS_CHROMEOS)
 const char kManagementLogUploadEnabled[] = "managementLogUploadEnabled";
@@ -729,6 +739,18 @@ void ManagementUIHandler::AddReportingInfo(base::Value::List* report_sources) {
              GetReportingTypeValue(report_definition.reporting_type));
     report_sources->Append(std::move(data));
   }
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+  // Insert the device signals consent disclosure at the end of browser
+  // reporting section.
+  auto* user_permission_service = GetUserPermissionService();
+  if (user_permission_service && user_permission_service->CanCollectSignals() ==
+                                     device_signals::UserPermission::kGranted) {
+    base::Value::Dict data;
+    data.Set("messageId", kManagementDeviceSignalsDisclosure);
+    data.Set("reportingType", GetReportingTypeValue(ReportingType::kDevice));
+    report_sources->Append(std::move(data));
+  }
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -1025,6 +1047,14 @@ policy::PolicyService* ManagementUIHandler::GetPolicyService() {
       ->GetProfilePolicyConnector()
       ->policy_service();
 }
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+device_signals::UserPermissionService*
+ManagementUIHandler::GetUserPermissionService() {
+  return enterprise_signals::UserPermissionServiceFactory::GetForProfile(
+      Profile::FromWebUI(web_ui()));
+}
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 void ManagementUIHandler::AsyncUpdateLogo() {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
