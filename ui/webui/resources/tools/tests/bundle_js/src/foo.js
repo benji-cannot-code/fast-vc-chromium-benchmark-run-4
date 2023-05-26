2ea91bb0f3c6e5f01ab06f8a3c3db68f0b5d082e
Cole Horvitz <colehorvitz@chromium.org>
2023-05-26 21:01:38 +0000

    WebUI: Refactor bundle_js_test.py.
    This CL updates the test to use checked in files rather than generated
    tmp files. This is cleaner as it results in greater code clarity and
    allows for code simplification. It also conforms bundle_js_test.py to
    behave similarly to sibling tests such as minify_js_test.py and
    generate_grd_test.py.
    
    This also modifies the test by allowing it to be run from any directory,
    not just u/w/r/tools.
    
    Bug: 1431253
    Change-Id: Ica055976f5f5952d8c0f4ddaf5a89dd09c815b97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571401
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149908}

---
+++ ui/webui/resources/tools/tests/bundle_js/src/foo.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/foo_resource.js';
+import 'some-fake-scheme://foo/foo.js';
+
+alert('Hello from src/foo.js');
---
+++ ui/webui/resources/tools/tests/bundle_js/src/subdir/baz.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome-untrusted://resources/foo_untrusted.js';
+
+import {bar} from '//resources/bar_resource.js';
+
+alert('Hello from src/subdir/baz.js');
