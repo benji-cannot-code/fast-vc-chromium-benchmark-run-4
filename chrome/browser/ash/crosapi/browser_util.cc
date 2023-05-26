594cc40157b4d4fd5524e54ec02197339a8a5d42
Igor Ruvinov <igorruvinov@chromium.org>
2023-05-26 17:07:53 +0000

    Revert "Refactor: Introduce IsLacrosAllowedInternal."
    This reverts commit d14cc3cd1ba928fed61e42a8d101ff4436f10c7c.
    
    Reason for revert: Dependent CL (crrev.com/c/4565993) is causing Linux ChromeOS test failures: https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/33127/overview
    
    Original change's description:
    > Refactor: Introduce IsLacrosAllowedInternal.
    >
    > "Allow" check for each SxS, Primary, Only mode is conceptually same.
    > This CL introduced the shared function, and they are just use
    > it.
    > SxS, Primary will be going to be removed in future CLs, so this CL
    > does not clean up the callers yet. That'll be done in follow up CLs.
    >
    > BUG=1448575
    > TEST=Tryjob.
    >
    > Change-Id: I3af8bd6fdb0b158baec7c8f67a2347264f805555
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4566764
    > Reviewed-by: Yuta Hijikata <ythjkt@chromium.org>
    > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1149624}
    
    Bug: 1448575
    Change-Id: Ia21c6fa277dc718f338ba70c2e1e3e598f4826e9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4566414
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Owners-Override: Igor Ruvinov <igorruvinov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1149775}

--- chrome/browser/ash/crosapi/browser_util.cc
+++ chrome/browser/ash/crosapi/browser_util.cc
@@ -171,10 +171,12 @@ LacrosAvailability GetLacrosAvailability(const user_manager::User* user,
   }
 }
 
-// Returns whether or not lacros is allowed for the Primary user,
-// with given LacrosAvailability policy.
-bool IsLacrosAllowedInternal(const User* user,
-                             LacrosAvailability lacros_availability) {
+// Gets called from IsLacrosAllowedToBeEnabled with primary user or from
+// IsLacrosEnabledForMigration with the user that the
+// IsLacrosEnabledForMigration was passed.
+bool IsLacrosAllowedToBeEnabledWithUser(
+    const User* user,
+    LacrosAvailability launch_availability) {
   if (!user) {
     // User is not available. Practically, this is accidentally happening
     // if related function is called before session, or in testing.
@@ -187,15 +189,18 @@ bool IsLacrosAllowedInternal(const User* user,
     return false;
   }
 
-  switch (lacros_availability) {
+  switch (launch_availability) {
+    case LacrosAvailability::kUserChoice:
+      break;
     case LacrosAvailability::kLacrosDisallowed:
       return false;
-    case LacrosAvailability::kUserChoice:
     case LacrosAvailability::kSideBySide:
     case LacrosAvailability::kLacrosPrimary:
     case LacrosAvailability::kLacrosOnly:
       return true;
   }
+
+  return true;
 }
 
 // Called from `IsDataWipeRequired()` or `IsDataWipeRequiredForTesting()`.
@@ -356,15 +361,13 @@ base::FilePath GetUserDataDir() {
 }
 
 bool IsLacrosAllowedToBeEnabled() {
-  return IsLacrosAllowedInternal(GetPrimaryUser(),
-                                 GetCachedLacrosAvailability());
+  return IsLacrosAllowedToBeEnabledWithUser(GetPrimaryUser(),
+                                            GetCachedLacrosAvailability());
 }
 
 bool IsLacrosEnabled() {
-  if (!IsLacrosAllowedInternal(GetPrimaryUser(),
-                               GetCachedLacrosAvailability())) {
+  if (!IsLacrosAllowedToBeEnabled())
     return false;
-  }
 
   // If profile migration is enabled, the completion of it is necessary for
   // Lacros to be enabled.
@@ -399,9 +402,8 @@ bool IsLacrosEnabledForMigration(const User* user,
   LacrosAvailability lacros_availability =
       GetLacrosAvailability(user, policy_init_state);
 
-  if (!IsLacrosAllowedInternal(user, lacros_availability)) {
+  if (!IsLacrosAllowedToBeEnabledWithUser(user, lacros_availability))
     return false;
-  }
 
   switch (lacros_availability) {
     case LacrosAvailability::kUserChoice:
@@ -558,9 +560,8 @@ bool IsLacrosPrimaryBrowserForMigration(const user_manager::User* user,
   LacrosAvailability lacros_availability =
       GetLacrosAvailability(user, policy_init_state);
 
-  if (!IsLacrosAllowedInternal(user, lacros_availability)) {
+  if (!IsLacrosPrimaryBrowserAllowedForMigration(user, lacros_availability))
     return false;
-  }
 
   switch (lacros_availability) {
     case LacrosAvailability::kUserChoice:
@@ -589,14 +590,45 @@ LacrosMode GetLacrosMode() {
 }
 
 bool IsLacrosPrimaryBrowserAllowed() {
-  return IsLacrosAllowedInternal(GetPrimaryUser(),
-                                 GetCachedLacrosAvailability());
+  // Note that if you are updating this function, please also update the
+  // *ForMigration variant to keep the logics consistent.
+  if (!IsLacrosAllowedToBeEnabled())
+    return false;
+
+  switch (GetCachedLacrosAvailability()) {
+    case LacrosAvailability::kLacrosDisallowed:
+      return false;
+    case LacrosAvailability::kLacrosPrimary:
+    case LacrosAvailability::kLacrosOnly:
+      // Forcibly allow to use Lacros as a Primary respecting the policy.
+      return true;
+    default:
+      // Fallback others.
+      break;
+  }
+
+  return true;
 }
 
 bool IsLacrosPrimaryBrowserAllowedForMigration(
     const user_manager::User* user,
     LacrosAvailability lacros_availability) {
-  return IsLacrosAllowedInternal(user, lacros_availability);
+  if (!IsLacrosAllowedToBeEnabledWithUser(user, lacros_availability))
+    return false;
+
+  switch (lacros_availability) {
+    case LacrosAvailability::kLacrosDisallowed:
+      return false;
+    case LacrosAvailability::kLacrosPrimary:
+    case LacrosAvailability::kLacrosOnly:
+      // Forcibly allow to use Lacros as a Primary respecting the policy.
+      return true;
+    default:
+      // Fallback others.
+      break;
+  }
+
+  return true;
 }
 
 bool IsLacrosPrimaryFlagAllowed() {
@@ -605,8 +637,23 @@ bool IsLacrosPrimaryFlagAllowed() {
 }
 
 bool IsLacrosOnlyBrowserAllowed() {
-  return IsLacrosAllowedInternal(GetPrimaryUser(),
-                                 GetCachedLacrosAvailability());
+  if (!IsLacrosAllowedToBeEnabled())
+    return false;
+
+  switch (GetCachedLacrosAvailability()) {
+    case LacrosAvailability::kLacrosDisallowed:
+      return false;
+    case LacrosAvailability::kLacrosOnly:
+      // Forcibly allow to use Lacros as a Primary respecting the policy.
+      return true;
+    case LacrosAvailability::kUserChoice:
+    case LacrosAvailability::kSideBySide:
+    case LacrosAvailability::kLacrosPrimary:
+      // Fallback others.
+      break;
+  }
+
+  return true;
 }
 
 bool IsLacrosOnlyFlagAllowed() {
