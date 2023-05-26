f546fb326965b5def8f698ddbb21efc68bc83807
Sebastien Lalancette <seblalancette@chromium.org>
2023-05-26 14:38:54 +0000

    Fix Consent Flow Flag Name
    The actual flag name should not start with a `k`.
    
    Change-Id: Idcd7371420b7df6da64611ffe07de606b013cca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570821
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Auto-Submit: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1149717}

--- components/device_signals/core/common/signals_features.cc
+++ components/device_signals/core/common/signals_features.cc
@@ -52,7 +52,7 @@ bool IsNewFunctionEnabled(NewEvFunction new_ev_function) {
 // This feature also requires UnmanagedDeviceSignalsConsentFlowEnabled policy to
 // be enabled
 BASE_FEATURE(kDeviceSignalsConsentDialog,
-             "kDeviceSignalsConsentDialog",
+             "DeviceSignalsConsentDialog",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsConsentDialogEnabled() {
