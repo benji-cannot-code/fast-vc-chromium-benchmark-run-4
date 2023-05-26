7931ff02c5df3aa3e6230a9d0d0042ae8719a77f
Peter Kvitek <kvitekp@chromium.org>
2023-05-26 06:43:21 +0000

    [headless] Move HeadlessSelectFileDialog to //components/headless
    HeadlessSelectFileDialog has to be shared between old and new headless,
    so this CL moves its existing implementation from //headless/lib to
    //component/headless.
    
    New headless will re-use shared implementation of HeadlessSelectFileDialog in a follow up CL.
    
    Bug: 1447911
    Change-Id: I5ed4eb3dd5387a243b032ab1a7ab31a233d8f4f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563343
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149567}

--- components/headless/select_file_dialog/headless_select_file_dialog.h
+++ components/headless/select_file_dialog/headless_select_file_dialog.h
@@ -3,14 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef HEADLESS_LIB_BROWSER_HEADLESS_SELECT_FILE_DIALOG_FACTORY_H_
-#define HEADLESS_LIB_BROWSER_HEADLESS_SELECT_FILE_DIALOG_FACTORY_H_
+#ifndef COMPONENTS_HEADLESS_SELECT_FILE_DIALOG_HEADLESS_SELECT_FILE_DIALOG_H_
+#define COMPONENTS_HEADLESS_SELECT_FILE_DIALOG_HEADLESS_SELECT_FILE_DIALOG_H_
 
 #include <memory>
 
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
-#include "headless/public/headless_export.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
 #include "ui/shell_dialogs/select_file_dialog_factory.h"
 #include "ui/shell_dialogs/select_file_policy.h"
@@ -20,8 +19,7 @@ namespace headless {
 using SelectFileDialogCallback =
     base::OnceCallback<void(ui::SelectFileDialog::Type type)>;
 
-class HEADLESS_EXPORT HeadlessSelectFileDialogFactory
-    : public ui::SelectFileDialogFactory {
+class HeadlessSelectFileDialogFactory : public ui::SelectFileDialogFactory {
  public:
   HeadlessSelectFileDialogFactory(const HeadlessSelectFileDialogFactory&) =
       delete;
@@ -52,4 +50,4 @@ class HEADLESS_EXPORT HeadlessSelectFileDialogFactory
 
 }  // namespace headless
 
-#endif  // HEADLESS_LIB_BROWSER_HEADLESS_SELECT_FILE_DIALOG_FACTORY_H_
+#endif  // COMPONENTS_HEADLESS_SELECT_FILE_DIALOG_HEADLESS_SELECT_FILE_DIALOG_H_
