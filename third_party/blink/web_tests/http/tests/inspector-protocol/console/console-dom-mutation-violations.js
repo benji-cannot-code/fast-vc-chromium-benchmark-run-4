f14570baa7de6b2261733039addf169e2540acf1
Mason Freed <masonf@chromium.org>
2023-05-26 18:46:52 +0000

    Organize web_tests that use Mutation Events
    The vast majority of these tests are tests *of* Mutation Events, and
    the vast majority of *those* are crash tests. I guess that proves that
    these events were a bad idea.
    
    This CL moves all tests that are purely testing things related directly
    to Mutation Events into a sub-folder (dom/mutation-event-tests) together. The idea is that eventually, that folder can just be deleted
    without having to parse through it. There is a short list of other
    tests that seem to use Mutation Events but not to test anything
    directly related to Mutation Events. I need to look further into
    those. For now, they're listed in the dom/mutation-event-
    tests/README.txt file for reference. I will be updating these tests in
    subsequent CLs.
    
    This CL should not change the functionality of any of the tests. A
    few were slightly modified, to keep them functional in their new
    location or to get around some presubmit errors I get when touching
    them. But mostly, this is just a file-move CL.
    
    Bug: 1446498
    Change-Id: Ia021f8bec848df819d5a4a451fb3f7f6cc586f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4549951
    Reviewed-by: David Baron <dbaron@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149836}

--- third_party/blink/web_tests/http/tests/inspector-protocol/console/console-dom-mutation-violations.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/console/console-dom-mutation-violations.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
+  // TODO(crbug.com/1446498) This test can be deleted once Mutation Events are removed.
   var {page, session, dp} = await testRunner.startBlank('Tests that dom modification event listener produces a violation.');
   dp.Log.onEntryAdded(testRunner.log.bind(testRunner));
   dp.Log.enable();
--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/mutation-events-issue.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/mutation-events-issue.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function (testRunner) {
+  // TODO(crbug.com/1446498) This test can be deleted once Mutation Events are removed.
   const { session, dp } = await testRunner.startBlank(
   'Verifies that adding Mutation Event listeners triggers a deprecation issue.');
   await dp.Audits.enable();
