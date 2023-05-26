bd655b1ca8ef538371d9c4f4eb071ae6a5cff759
Adem Derinel <derinel@google.com>
2023-05-26 08:49:57 +0000

    [Android][CredMan] Update ConditionalUiState usage in CredMan
    The CL also adds some test cases for conditional requests which go
    through CredMan.
    
    Bug: 1434278
    Low-Coverage-Reason: Not intending to test if reflection fails
    Change-Id: I9afacf84f3881ed8a66ec5368a697e590827e6c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546220
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1149609}

---
+++ chrome/android/junit/src/org/chromium/chrome/browser/webauth/FakeAndroidCredManPrepareGetCredentialResponse.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webauth;
+
+/**
+ * Fake implementation of the Android Credential Manager PrepareGetCredentialResponse object.
+ */
+public final class FakeAndroidCredManPrepareGetCredentialResponse {
+    public boolean hasAuthenticationResults() {
+        return false;
+    }
+
+    public boolean hasCredentialResults(String credentialType) {
+        return true;
+    }
+
+    public boolean hasRemoteResults() {
+        return true;
+    }
+
+    public Object getPendingGetCredentialHandle() {
+        return new Object();
+    }
+}
