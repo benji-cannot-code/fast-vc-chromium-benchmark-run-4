30c6a1f9afada662a7efff514739a9627c0455ab
Brando <brandosocarras@google.com>
2023-05-26 19:02:44 +0000

    [Nearby Share][Self Share] Consolidate Self Share Flags
    Replaced flags nearby-sharing-self-share-auto-accept and
    nearby-sharing-self-share-ui with nearby-sharing-self-share.
    This includes replacing all uses of the former two flags with the
    latter.
    
    Test:
    - Self Share WAI when flag enabled/disabled on DUT
    - DiscoveryPageTests utests and all utests in target ash_unittests pass
    
    Bug: b/283283967
    Change-Id: I15916801e94277a57a673d2b2bfe7e410b086222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563356
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1149843}

--- chrome/browser/nearby_sharing/nearby_sharing_service_impl_unittest.cc
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl_unittest.cc
@@ -352,8 +352,7 @@ constexpr base::TimeDelta kCertificateDownloadDuringDiscoveryPeriod =
 // We will run tests with the following feature flags enabled and disabled in
 // all permutations. To add or a remove a feature you can just update this list.
 const std::vector<base::test::FeatureRef> kTestFeatures = {
-    features::kNearbySharingSelfShareAutoAccept,
-    features::kNearbySharingSelfShareUI};
+    features::kNearbySharingSelfShare};
 
 bool FileExists(const base::FilePath& file_path) {
   base::ScopedAllowBlockingForTesting allow_blocking;
@@ -2325,7 +2324,7 @@ TEST_P(NearbySharingServiceImplTest,
   NearbySharingService::StatusCodes result = service_->RegisterReceiveSurface(
       &callback, NearbySharingService::ReceiveSurfaceState::kForeground);
   EXPECT_EQ(result, NearbySharingService::StatusCodes::kOk);
-  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShareUI)) {
+  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     EXPECT_TRUE(fake_nearby_connections_manager_->IsAdvertising());
   } else {
     EXPECT_FALSE(fake_nearby_connections_manager_->IsAdvertising());
@@ -2343,7 +2342,7 @@ TEST_P(NearbySharingServiceImplTest, ScreenLocksDuringAdvertising) {
   EXPECT_FALSE(fake_nearby_connections_manager_->is_shutdown());
 
   session_controller_->SetScreenLocked(true);
-  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShareUI)) {
+  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     EXPECT_TRUE(fake_nearby_connections_manager_->IsAdvertising());
   } else {
     EXPECT_FALSE(fake_nearby_connections_manager_->IsAdvertising());
@@ -5256,8 +5255,7 @@ TEST_P(NearbySharingServiceImplTest, CreateShareTarget) {
   ASSERT_TRUE(share_target.has_value());
   EXPECT_EQ(kDeviceName, share_target->device_name);
   EXPECT_EQ(kDeviceType, share_target->type);
-  if (base::FeatureList::IsEnabled(
-          features::kNearbySharingSelfShareAutoAccept)) {
+  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     EXPECT_EQ(certificate_proto.for_self_share(), share_target->for_self_share);
   }
 
@@ -5287,8 +5285,7 @@ TEST_P(NearbySharingServiceImplTest, SelfShareAutoAccept) {
       SetUpIncomingConnection(callback, /*for_self_share=*/true);
 
   // If Self Share is not enabled, we should just time out.
-  if (!base::FeatureList::IsEnabled(
-          features::kNearbySharingSelfShareAutoAccept)) {
+  if (!base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     base::RunLoop run_loop;
     EXPECT_CALL(callback, OnTransferUpdate(testing::_, testing::_))
         .WillOnce(testing::Invoke(
