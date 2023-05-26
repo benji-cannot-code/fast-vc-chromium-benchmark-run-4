203ca126134a3aabe3bfb87bba335052c9bdc02f
dljames <dljames@google.com>
2023-05-26 16:51:56 +0000

    [ SavedTabGroup ] Enable group / tab ordering integration tests
    Enables checks for tests that verify the ordering of groups and tabs after a reorder message is sent from the sync server. These were commented out before since that functionality was not implemented yet.
    
    These tests are now possible due to the following CLs:
     - Notify observers of tab reorders - crrev.com/c/4547229
     - Split Reorder()/MoveTab() into Locally/FromSync - crrev.com/c/4548688
    
    Bug: 1445146
    Change-Id: I382bac67edb87a17c487e6872e6fc9245fa6bdef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4562399
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1149763}

--- chrome/browser/sync/test/integration/single_client_saved_tab_groups_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_saved_tab_groups_sync_test.cc
@@ -155,7 +155,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientSavedTabGroupsSyncTest,
   SavedTabGroupKeyedService* const service =
       SavedTabGroupServiceFactory::GetForProfile(GetProfile(0));
 
-  // TODO(crbug/1445672): Verify that the orphaned tab was exists but isn't
+  // TODO(crbug/1445672): Verify that the orphaned tab exists but isn't
   // linked to any group.
 
   // Verify adding the corresponding group adds the orphaned tab to the model.
@@ -410,10 +410,9 @@ IN_PROC_BROWSER_TEST_F(SingleClientSavedTabGroupsSyncTest, ReorderGroups) {
   AddDataToFakeServer(*group2.ToSpecifics());
 
   // Verify the group positions are updated in the local model as well.
-  // TODO(tbergquist/dljames): Enable this check once ordering syncing is fixed.
-  // EXPECT_TRUE(saved_tab_groups_helper::GroupOrderChecker(
-  //                 service, {group2.saved_guid(), group1.saved_guid()})
-  //                 .Wait());
+  EXPECT_TRUE(saved_tab_groups_helper::GroupOrderChecker(
+                  service, {group2.saved_guid(), group1.saved_guid()})
+                  .Wait());
 }
 
 // Reorder tabs in a group.
@@ -454,11 +453,10 @@ IN_PROC_BROWSER_TEST_F(SingleClientSavedTabGroupsSyncTest, ReorderTabs) {
   AddDataToFakeServer(*tab2.ToSpecifics());
 
   // Verify the tab order was updated in the model.
-  // TODO(tbergquist/dljames): Enable this check once ordering syncing is fixed.
-  // EXPECT_TRUE(saved_tab_groups_helper::TabOrderChecker(
-  //                 service, group1.saved_guid(),
-  //                 {tab2.saved_tab_guid(), tab1.saved_tab_guid()})
-  //                 .Wait());
+  EXPECT_TRUE(saved_tab_groups_helper::TabOrderChecker(
+                  service, group1.saved_guid(),
+                  {tab2.saved_tab_guid(), tab1.saved_tab_guid()})
+                  .Wait());
 }
 
 }  // namespace
