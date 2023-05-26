ddf2cc94af5abb58056f8367628bc815981c96ea
Ramin Halavati <rhalavati@chromium.org>
2023-05-26 05:05:49 +0000

    Add UMA metrics for Screen AI component install/uninstall.
    Adds UMA metrics to record success and failure of Screen AI component
    install or uninstall. The metric is currently only added for DLC (for
    ChromeOS), but will be later added for component updater (for other
    desktop platforms) as well.
    
    AX-Relnotes: n/a.
    Bug: 1278249
    Change-Id: Ib47a52934eca086e4d427463be119858143f5e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4565656
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1149541}

--- chrome/browser/screen_ai/screen_ai_chromeos_installer.cc
+++ chrome/browser/screen_ai/screen_ai_chromeos_installer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/screen_ai/screen_ai_install_state.h"
 #include "chromeos/ash/components/dbus/dlcservice/dlcservice.pb.h"
@@ -16,9 +17,20 @@ namespace {
 
 constexpr char kScreenAIDlcName[] = "screen-ai";
 
+void RecordDlcStateChange(bool install, bool successful) {
+  if (install) {
+    base::UmaHistogramBoolean("Accessibility.ScreenAI.Component.Install",
+                              successful);
+  } else {
+    base::UmaHistogramBoolean("Accessibility.ScreenAI.Component.Uninstall",
+                              successful);
+  }
+}
 void OnInstallCompleted(
     const ash::DlcserviceClient::InstallResult& install_result) {
-  // TODO(https://crbug.com/1278249): Add mertics for install result.
+  RecordDlcStateChange(/*install=*/true, /*successful=*/install_result.error ==
+                                             dlcservice::kErrorNone);
+
   if (install_result.error != dlcservice::kErrorNone) {
     VLOG(0) << "ScreenAI installation failed: " << install_result.error;
     screen_ai::ScreenAIInstallState::GetInstance()->SetState(
@@ -35,6 +47,9 @@ void OnInstallCompleted(
 }
 
 void OnUninstallCompleted(const std::string& err) {
+  RecordDlcStateChange(/*install=*/false,
+                       /*successful=*/err == dlcservice::kErrorNone);
+
   if (err != dlcservice::kErrorNone) {
     VLOG(0) << "Unistall failed: " << err;
   }
