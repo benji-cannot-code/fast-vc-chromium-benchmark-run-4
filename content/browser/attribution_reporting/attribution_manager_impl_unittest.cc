003a0dfef17c25b4991a250bb02ae8daf9a3a14d
Andrew Paseltiner <apaseltiner@chromium.org>
2023-05-26 14:22:49 +0000

    Notify observers when Attribution Reporting data is deleted by key
    Otherwise, the Attribution Internals UI will not be updated promptly,
    causing misleading data to be shown indefinitely.
    
    Bug: 1449219
    Change-Id: I61159ba12d8f6f4bec7b88e1de90452bda2420dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568989
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149708}

--- content/browser/attribution_reporting/attribution_manager_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_manager_impl_unittest.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/attribution_reporting/test/mock_content_browser_client.h"
 #include "content/browser/browsing_data/browsing_data_filter_builder_impl.h"
 #include "content/browser/storage_partition_impl.h"
+#include "content/public/browser/attribution_data_model.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browsing_data_filter_builder.h"
 #include "content/public/browser/global_routing_id.h"
@@ -2838,6 +2839,24 @@ TEST_F(AttributionManagerImplTest, PendingReportsMetrics_OverLimits) {
   EXPECT_EQ(
       histograms.GetTotalSum(kPendingAndBrowserWentOfflineTimeSinceCreation),
       (base::Seconds(10) * kMaxPendingReportsTimings).InMilliseconds());
+}
+
+TEST_F(AttributionManagerImplTest,
+       RemoveAttributionDataByDataKey_NotifiesObservers) {
+  MockAttributionObserver observer;
+  base::ScopedObservation<AttributionManager, AttributionObserver> observation(
+      &observer);
+  observation.Observe(attribution_manager_.get());
+
+  EXPECT_CALL(observer, OnSourcesChanged);
+  EXPECT_CALL(observer, OnReportsChanged);
+
+  base::RunLoop run_loop;
+  attribution_manager_->RemoveAttributionDataByDataKey(
+      AttributionDataModel::DataKey(
+          url::Origin::Create(GURL("https://x.test"))),
+      run_loop.QuitClosure());
+  run_loop.Run();
 }
 
 class AttributionManagerImplDebugReportTest
