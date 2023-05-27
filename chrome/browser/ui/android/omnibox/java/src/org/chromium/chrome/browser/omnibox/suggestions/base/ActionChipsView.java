ea92824562d806d63b49d8e9677a21a88509dfcb
Tomasz Wiszkowski <ender@google.com>
2023-05-27 00:07:51 +0000

    Remove placeholder spacing view from ActionChipsView.
    The change replaces placeholder view with a (poorly documented, but
    established) mechanism supplied by RecyclerView, where the padding
    area can specify lead-in space and can be used to present content
    upon scroll.
    
    Bug: 1418077
    Change-Id: I74169eb7c83fc916f76c16a9b82f04af83ee3227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571666
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149994}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/ActionChipsView.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/ActionChipsView.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration;
 
+import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
 import org.chromium.chrome.browser.omnibox.R;
 import org.chromium.chrome.browser.util.KeyNavigationUtil;
 import org.chromium.components.browser_ui.widget.chips.ChipView;
@@ -41,6 +42,16 @@ public class ActionChipsView extends RecyclerView {
         setMinimumHeight(getResources().getDimensionPixelSize(
                 R.dimen.omnibox_action_chips_container_height));
 
+        boolean showModernizedSuggestionsList =
+                OmniboxFeatures.shouldShowModernizeVisualUpdate(context);
+        setPaddingRelative(getResources().getDimensionPixelSize(showModernizedSuggestionsList
+                                           ? R.dimen.omnibox_suggestion_icon_area_size_modern
+                                           : R.dimen.omnibox_suggestion_icon_area_size),
+                0, 0,
+                getResources().getDimensionPixelSize(R.dimen.omnibox_suggestion_content_padding));
+        // Permit Actions to scroll through the padding area (I know, right?).
+        setClipToPadding(false);
+
         final @Px int actionChipSpacing =
                 getResources().getDimensionPixelSize(R.dimen.omnibox_action_chip_spacing);
         addItemDecoration(new ItemDecoration() {
@@ -51,9 +62,6 @@ public class ActionChipsView extends RecyclerView {
                 outRect.left = actionChipSpacing / 2;
             }
         });
-
-        setPaddingRelative(0, 0, 0,
-                getResources().getDimensionPixelSize(R.dimen.omnibox_suggestion_content_padding));
     }
 
     @Override
