f526919afa9466179a63a043516f942e7a493b28
Brandon Jones <bajones@chromium.org>
2023-05-27 03:22:22 +0000

    Handle Cardboard trigger events
    Processes screen touches and surfaces them as gaze input events
    to WebXR.
    
    Bug: 1429087
    Change-Id: Ibebf2ef59c63666db1518e2244c72ed79bd8b2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570742
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150037}

--- device/vr/android/cardboard/cardboard_render_loop.h
+++ device/vr/android/cardboard/cardboard_render_loop.h
@@ -87,6 +87,9 @@ class CardboardRenderLoop : public base::android::JavaHandlerThread,
   // mojom::XRSessionController
   void SetFrameDataRestricted(bool restricted) override;
 
+  void OnTriggerEvent(bool pressed);
+  device::mojom::XRInputSourceStatePtr GetInputSourceState();
+
   base::WeakPtr<CardboardRenderLoop> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
@@ -135,6 +138,10 @@ class CardboardRenderLoop : public base::android::JavaHandlerThread,
   scoped_refptr<gl::GLContext> context_;
   gfx::RectF left_bounds_;
   gfx::RectF right_bounds_;
+
+  // Input Parameters
+  bool trigger_pressed_ = false;
+  bool trigger_clicked_ = false;
 
   // Owned by our parent (cardboard_device)
   raw_ptr<CardboardSdk> cardboard_sdk_;
