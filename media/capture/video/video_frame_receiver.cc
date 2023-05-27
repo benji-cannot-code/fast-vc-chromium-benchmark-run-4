b6067b82697600fb14a169e474efc6dc4ea83880
Shuhai Peng <shuhai.peng@intel.com>
2023-05-27 07:00:49 +0000

    media: win camera capture zero copy
    This CL is designed to allow Chromium to use shared MediaEngineCapture
    passed textures on windows, thus reducing one time texture copy which
    will benefit video conference use case.
    
    Specifically, the camera capture feature in Chromium is by far
    "1-copy capture". What we get from Frameserver will be firstly copied
    to the GMB allocated by Chromium(internal allocation) and then it's
    consumed for renderer/encoder. This CL uses the shared texture from
    MFCaptureEngine, thus removing the only 1-copy we currently have in
    Chromium, and reuse the share_handle and token for same texture by
    SetPrivateData/GetPrivateData.
    
    This CL also changed the buffer pool size from 30 to 10 since it's
    confirmed that MFCaptureEngine may terminate the capture with
    MF_E_SAMPLEALLOCATOR_EMPTY error if more than 10 buffers are held by
    the client, and usually there are 3-5 frames in flight, unless
    there's some micro-freeze in the renderer or the gpu service.
    
    Tested this CL on mac, no regression in behavior.
    
    Another CL will manage the lifetime of IMFBuffer to correctly reuse
    texture.
    
    Feature command:
    --enable
    features=MediaFoundationD3D11VideoCapture,
    MediaFoundationD3D11VideoCaptureZeroCopy
    
    Test command:
    1. autoninja -C out\Default content_unittests
    2. content_unittests.exe --
    gtest_filter=All/VideoCaptureBufferPoolTest.BufferPoolExternalWin/*
    
    Bug: 1120900, 1430048
    Change-Id: I16f917ae914a5537ade15e16b2b16298e21c94e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4323337
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Shuhai Peng <shuhai.peng@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1150048}

--- media/capture/video/video_frame_receiver.cc
+++ media/capture/video/video_frame_receiver.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+ReadyFrameInBuffer::ReadyFrameInBuffer() = default;
+
 ReadyFrameInBuffer::ReadyFrameInBuffer(
     int buffer_id,
     int frame_feedback_id,
--- media/capture/video/video_frame_receiver.h
+++ media/capture/video/video_frame_receiver.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 struct CAPTURE_EXPORT ReadyFrameInBuffer {
+  ReadyFrameInBuffer();
   ReadyFrameInBuffer(
       int buffer_id,
       int frame_feedback_id,
