8a93b0ca219f3ff03d9d40a7b6ea5e157a792b76
Yi Xu <yiyix@google.com>
2023-05-29 04:51:19 +0000

    Revert some changes in Motionmark folder
    When I move motionmark to the third_party library,
    https://chromium-review.googlesource.com/c/chromium/src/+/4541141,
    I accidenntly included some work in progress changes. Revert those
    changes.
    
    Bug: 1446401
    
    Change-Id: I9ad1689190d28cd7b5e5989241cadbb80106b2e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564973
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Yi Xu <yiyix@google.com>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150179}

--- third_party/blink/perf_tests/MotionMark/resources/debug-runner/tests.js
+++ third_party/blink/perf_tests/MotionMark/resources/debug-runner/tests.js
@@ -428,10 +428,6 @@ Suites.push(new Suite("Basic canvas path suite",
             name: "Canvas ellipses"
         },
         {
-            url: "simple/simple-canvas-paths.html?pathType=spreadSheets",
-            name: "Canvas Spreadsheets"
-        },
-        {
             url: "simple/simple-canvas-paths.html?pathType=lineFill",
             name: "Canvas line path, fill"
         },
