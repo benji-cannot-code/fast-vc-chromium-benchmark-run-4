af1421e0ecb519486c65a65bb9f431af05ac5f0c
Vivian Pao <vpao@google.com>
2023-05-29 06:05:33 +0000

    [Promise Icons] Overload << output for PromiseAppUpdate for debug logs
    Bug: b/261907233
    Change-Id: Id7c77f95d57e2e0f1e528e05cd8637b61a6ce82f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567766
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150195}

--- chrome/browser/apps/app_service/promise_apps/promise_app_update.cc
+++ chrome/browser/apps/app_service/promise_apps/promise_app_update.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/promise_apps/promise_app_update.h"
 
 #include "base/logging.h"
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/apps/app_service/package_id.h"
 #include "chrome/browser/apps/app_service/promise_apps/promise_app.h"
 #include "components/services/app_service/public/cpp/macros.h"
@@ -95,4 +96,20 @@ bool PromiseAppUpdate::ShouldShowChanged() const {
   RETURN_OPTIONAL_VALUE_CHANGED(should_show);
 }
 
+std::ostream& operator<<(std::ostream& out, const PromiseAppUpdate& update) {
+  out << "Package_id: " << update.PackageId().ToString() << std::endl;
+  out << "- Name Changed: " << update.NameChanged() << std::endl;
+  out << "- Name: " << update.Name().value_or("N/A") << std::endl;
+  out << "- Progress Changed: " << update.ProgressChanged() << std::endl;
+  out << "- Progress: "
+      << (update.Progress().has_value()
+              ? base::NumberToString(update.Progress().value())
+              : "N/A")
+      << std::endl;
+  out << "- Status Changed: " << update.StatusChanged() << std::endl;
+  out << "- Status: " << EnumToString(update.Status()) << std::endl;
+  out << "- Should Show Changed: " << update.ShouldShowChanged() << std::endl;
+  out << "- Should Show: " << update.ShouldShow() << std::endl;
+  return out;
+}
 }  // namespace apps
