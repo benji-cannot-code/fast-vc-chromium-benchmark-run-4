fa4ec4c97530acde3a424b4796d40c9e843db3e2
Kevin Ellis <kevers@google.com>
2023-05-29 17:22:44 +0000

    Handle style dependent animation ranges
    Bug: 1448638
    
    Change-Id: Ifbe2ce74a65dc6dadfed841c00e000c6e499797f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567310
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150297}

--- third_party/blink/renderer/core/animation/timeline_offset.h
+++ third_party/blink/renderer/core/animation/timeline_offset.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class Document;
 class Element;
 class CSSValue;
 
@@ -19,13 +20,22 @@ struct TimelineOffset {
   using NamedRange = V8TimelineRange::Enum;
 
   TimelineOffset() = default;
-  TimelineOffset(NamedRange name, Length offset) : name(name), offset(offset) {}
+  TimelineOffset(NamedRange name,
+                 Length offset,
+                 absl::optional<String> style_dependent_offset = absl::nullopt)
+      : name(name),
+        offset(offset),
+        style_dependent_offset(style_dependent_offset) {}
+
+  bool UpdateOffset(Element* element, CSSValue* value);
 
   NamedRange name = NamedRange::kNone;
   Length offset = Length::Fixed();
+  absl::optional<String> style_dependent_offset;
 
   bool operator==(const TimelineOffset& other) const {
-    return name == other.name && offset == other.offset;
+    return name == other.name && offset == other.offset &&
+           style_dependent_offset == other.style_dependent_offset;
   }
 
   bool operator!=(const TimelineOffset& other) const {
@@ -44,6 +54,13 @@ struct TimelineOffset {
       const V8UnionStringOrTimelineRangeOffset* range_offset,
       double default_percent,
       ExceptionState& exception_state);
+
+  // A length is style dependent if using a font relative or viewport relative
+  // unit. We also classify all styles involving calc or var as style dependent.
+  // A style-dependent value needs to be re-resolved after a style change.
+  static bool IsStyleDependent(const CSSValue* value);
+
+  static CSSValue* ParseOffset(Document* document, String css_text);
 
   static Length ResolveLength(Element* element, const CSSValue* value);
 
