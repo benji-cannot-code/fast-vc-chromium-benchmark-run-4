ef7a92c3e8bf3ef8410a4b28911f32514b85363c
Rakina Zata Amni <rakina@chromium.org>
2023-05-29 05:47:55 +0000

    Make tests pass with navigation queuing
    Update or skip tests to handle navigation queueing, which will cause
    newer navigations to wait for a pre-existing pending commit navigation
    to finish committing, before getting into the pending commit state
    itself. For most tests, it just changes the navigations to not get
    into the pending commit stage if it's not necessary for the test.
    
    Bug: 1220337
    Change-Id: I227ebcc35a7f7046592f3cde023bb6c5584dff63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4310980
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150190}

--- content/public/test/test_utils.cc
+++ content/public/test/test_utils.cc
@@ -233,6 +233,10 @@ bool CanSameSiteMainFrameNavigationsChangeSiteInstances() {
          IsBackForwardCacheEnabled();
 }
 
+bool IsNavigationQueueingEnabled() {
+  return ShouldQueueNavigationsWhenPendingCommitRFHExists();
+}
+
 void DisableProactiveBrowsingInstanceSwapFor(RenderFrameHost* rfh) {
   if (!CanSameSiteMainFrameNavigationsChangeSiteInstances())
     return;
@@ -487,8 +491,9 @@ bool RenderFrameDeletedObserver::WaitUntilDeleted() {
 }
 
 RenderFrameHostWrapper::RenderFrameHostWrapper(RenderFrameHost* rfh)
-    : rfh_id_(rfh->GetGlobalId()),
-      deleted_observer_(std::make_unique<RenderFrameDeletedObserver>(rfh)) {}
+    : rfh_id_(rfh ? rfh->GetGlobalId() : GlobalRenderFrameHostId()),
+      deleted_observer_(rfh ? std::make_unique<RenderFrameDeletedObserver>(rfh)
+                            : nullptr) {}
 
 RenderFrameHostWrapper::RenderFrameHostWrapper(RenderFrameHostWrapper&& rfhft) =
     default;
@@ -505,10 +510,12 @@ bool RenderFrameHostWrapper::IsDestroyed() const {
 // See RenderFrameDeletedObserver for notes on the difference between
 // RenderFrame being deleted and RenderFrameHost being destroyed.
 bool RenderFrameHostWrapper::WaitUntilRenderFrameDeleted() const {
+  CHECK(deleted_observer_);
   return deleted_observer_->WaitUntilDeleted();
 }
 
 bool RenderFrameHostWrapper::IsRenderFrameDeleted() const {
+  CHECK(deleted_observer_);
   return deleted_observer_->deleted();
 }
 
