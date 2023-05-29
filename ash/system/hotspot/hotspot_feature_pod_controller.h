f3da74fff8ab5fa901ccd600011fe48d1e6fd022
Tsuyoshi Horo <horo@chromium.org>
2023-05-29 00:16:24 +0000

    Revert "[CrOS Hotspot] Add hotspot icon animation in quick settings"
    This reverts commit 823887121ea1b493691cbbb919e7ef3afeb234da.
    
    Reason for revert: HotspotDetailedViewTest failing on linux-chromeos-dbg bots
    
    Bug:1449561
    
    Original change's description:
    > [CrOS Hotspot] Add hotspot icon animation in quick settings
    >
    > This CL adds the hotspot icon animation when hotspot is enabling
    >
    > Video: https://drive.google.com/file/d/1ZHHg47-ymOGjw61P7YLQuSYCpSy-7pBB/view?usp=drive_link&resourcekey=0-cTP2CVMuH6lbKhbeDm1IfQ
    >
    > Bug: b/280693408
    > Change-Id: I67fed0eb67556abddbc35a0ed6db894817c99e8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546862
    > Commit-Queue: Jason Zhang <jiajunz@google.com>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1149869}
    
    Bug: b/280693408
    Change-Id: Id26428ebd66f5381fe112af0d749aa7ecd12c973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568050
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150158}

--- ash/system/hotspot/hotspot_feature_pod_controller.h
+++ ash/system/hotspot/hotspot_feature_pod_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/constants/quick_settings_catalogs.h"
-#include "ash/system/hotspot/hotspot_icon_animation_observer.h"
 #include "ash/system/unified/feature_pod_controller_base.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -31,7 +30,6 @@ class UnifiedSystemTrayController;
 // detailed page with a Hotspot info.
 class ASH_EXPORT HotspotFeaturePodController
     : public FeaturePodControllerBase,
-      public HotspotIconAnimationObserver,
       public hotspot_config::mojom::CrosHotspotConfigObserver {
  public:
   explicit HotspotFeaturePodController(
@@ -47,9 +45,6 @@ class ASH_EXPORT HotspotFeaturePodController
   QsFeatureCatalogName GetCatalogName() override;
   void OnIconPressed() override;
   void OnLabelPressed() override;
-
-  // HotspotIconAnimationObserver:
-  void HotspotIconChanged() override;
 
  private:
   // mojom::CrosHotspotConfigObserver:
