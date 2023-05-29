b818f03ff33d2268b3b2160ebe6d8d33e7181929
Curtis McMullan <curtismcmullan@chromium.org>
2023-05-29 22:48:23 +0000

    jinx: handle stale suggestions
    Previously in crrev/c/4511752 we added SuggestionsTextContext to the
    InputMethodHost::DisplaySuggestions call. This passed the surrounding
    text information used to generate suggestions.
    
    This CL follows on from crrev/c/4511752 and adds stale suggestion
    handling in MultiWordSuggester using the surrounding text info passed in
    SuggestionsTextContext. Stale suggestions are always dropped even if
    they match the current surrounding text.
    
    BUG=b:278800045
    
    Change-Id: Id29fe05e3641fd54525c89f69d5bee2202687961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520736
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150344}

--- chrome/browser/ash/input_method/assistive_suggester_unittest.cc
+++ chrome/browser/ash/input_method/assistive_suggester_unittest.cc
@@ -1139,13 +1139,13 @@ TEST_F(AssistiveSuggesterMultiWordTest,
                                                      TextContext(""));
   assistive_suggester_->OnSurroundingTextChanged(u"h", gfx::Range(1));
   assistive_suggester_->OnExternalSuggestionsUpdated(suggestions,
-                                                     TextContext(""));
+                                                     TextContext("h"));
   assistive_suggester_->OnSurroundingTextChanged(u"he", gfx::Range(2));
   assistive_suggester_->OnExternalSuggestionsUpdated(suggestions,
-                                                     TextContext(""));
+                                                     TextContext("he"));
   assistive_suggester_->OnSurroundingTextChanged(u"hel", gfx::Range(3));
   assistive_suggester_->OnExternalSuggestionsUpdated(suggestions,
-                                                     TextContext(""));
+                                                     TextContext("hel"));
 
   histogram_tester_.ExpectTotalCount("InputMethod.Assistive.Coverage", 1);
   histogram_tester_.ExpectUniqueSample("InputMethod.Assistive.Coverage",
@@ -1170,13 +1170,13 @@ TEST_F(AssistiveSuggesterMultiWordTest,
                                                      TextContext(""));
   assistive_suggester_->OnSurroundingTextChanged(u"h", gfx::Range(1));
   assistive_suggester_->OnExternalSuggestionsUpdated(first_suggestions,
-                                                     TextContext(""));
+                                                     TextContext("h"));
   assistive_suggester_->OnSurroundingTextChanged(u"he", gfx::Range(2));
   assistive_suggester_->OnExternalSuggestionsUpdated(first_suggestions,
-                                                     TextContext(""));
+                                                     TextContext("he"));
   assistive_suggester_->OnSurroundingTextChanged(u"he ", gfx::Range(3));
   assistive_suggester_->OnExternalSuggestionsUpdated(second_suggestions,
-                                                     TextContext(""));
+                                                     TextContext("he "));
 
   histogram_tester_.ExpectTotalCount("InputMethod.Assistive.Coverage", 2);
   histogram_tester_.ExpectUniqueSample("InputMethod.Assistive.Coverage",
@@ -1193,7 +1193,7 @@ TEST_F(AssistiveSuggesterMultiWordTest, PressingTabShouldAcceptSuggestion) {
   assistive_suggester_->OnFocus(5, empty_context);
   assistive_suggester_->OnSurroundingTextChanged(u"why ar", gfx::Range(6));
   assistive_suggester_->OnExternalSuggestionsUpdated(suggestions,
-                                                     TextContext(""));
+                                                     TextContext("why ar"));
 
   EXPECT_EQ(assistive_suggester_->OnKeyEvent(PressKey(ui::DomCode::TAB)),
             AssistiveSuggesterKeyResult::kHandled);
@@ -1209,7 +1209,7 @@ TEST_F(AssistiveSuggesterMultiWordTest, AltPlusTabShouldNotAcceptSuggestion) {
   assistive_suggester_->OnFocus(5, empty_context);
   assistive_suggester_->OnSurroundingTextChanged(u"why ar", gfx::Range(6));
   assistive_suggester_->OnExternalSuggestionsUpdated(suggestions,
-                                                     TextContext(""));
+                                                     TextContext("why ar"));
 
   EXPECT_EQ(assistive_suggester_->OnKeyEvent(PressKeyWithAlt(ui::DomCode::TAB)),
             AssistiveSuggesterKeyResult::kNotHandled);
@@ -1225,7 +1225,7 @@ TEST_F(AssistiveSuggesterMultiWordTest, CtrlPlusTabShouldNotAcceptSuggestion) {
   assistive_suggester_->OnFocus(5, empty_context);
   assistive_suggester_->OnSurroundingTextChanged(u"why ar", gfx::Range(6));
   assistive_suggester_->OnExternalSuggestionsUpdated(suggestions,
-                                                     TextContext(""));
+                                                     TextContext("why ar"));
 
   EXPECT_EQ(
       assistive_suggester_->OnKeyEvent(PressKeyWithCtrl(ui::DomCode::TAB)),
@@ -1242,7 +1242,7 @@ TEST_F(AssistiveSuggesterMultiWordTest, ShiftPlusTabShouldNotAcceptSuggestion) {
   assistive_suggester_->OnFocus(5, empty_context);
   assistive_suggester_->OnSurroundingTextChanged(u"why ar", gfx::Range(6));
   assistive_suggester_->OnExternalSuggestionsUpdated(suggestions,
-                                                     TextContext(""));
+                                                     TextContext("why ar"));
 
   EXPECT_EQ(
       assistive_suggester_->OnKeyEvent(PressKeyWithShift(ui::DomCode::TAB)),
