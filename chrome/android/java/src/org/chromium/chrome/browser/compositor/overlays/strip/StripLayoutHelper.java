0e9dc51d46c2dc9e0ac2ba587a3a171a5af313a4
Neil Coronado <nemco@google.com>
2023-05-30 23:16:07 +0000

    [TabStripRedesign] Fix new tab button anchoring on tab close
    Fix new tab button anchoring on tab close.
    -Only affects TSR enabled & anchoring disabled.
    
    Before: https://drive.google.com/file/d/1MjVqrNRUKabfGDsiN5HTsm5Wg9b636-k/view?usp=drive_link
    After: https://drive.google.com/file/d/1uyQOzLifCQgEpApocat3Qxq2ZQzNBb8b/view?usp=drive_link
    
    Bug: 1447109
    Change-Id: I2cb5ac164dec1ac87c1c0b8e2b4554319f0ef991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573732
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150940}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -1412,7 +1412,7 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
         }
 
         // 4. Add new tab button offset animation.
-        if (!ChromeFeatureList.sTabStripRedesign.isEnabled()) {
+        if (isNewTabButtonAnchorDisabled()) {
             CompositorAnimator newTabButtonOffsetAnimator = updateNewTabButtonState(true);
             if (newTabButtonOffsetAnimator != null) {
                 tabStripAnimators.add(newTabButtonOffsetAnimator);
