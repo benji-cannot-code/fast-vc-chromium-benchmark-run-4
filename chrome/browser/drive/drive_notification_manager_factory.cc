a906898550c78a2e224e2bf279280639186323ba
Mikel Astiz <mastiz@chromium.org>
2023-05-30 12:13:27 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/drive
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=benreich@chromium.org
    
    Bug: 925323
    Change-Id: Ia0dc33b508e5403ae70f2b863433b81e1904d481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572803
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150496}

--- chrome/browser/drive/drive_notification_manager_factory.cc
+++ chrome/browser/drive/drive_notification_manager_factory.cc
@@ -56,7 +56,8 @@ DriveNotificationManagerFactory::GetForBrowserContext(
 // static
 DriveNotificationManagerFactory*
 DriveNotificationManagerFactory::GetInstance() {
-  return base::Singleton<DriveNotificationManagerFactory>::get();
+  static base::NoDestructor<DriveNotificationManagerFactory> instance;
+  return instance.get();
 }
 
 DriveNotificationManagerFactory::DriveNotificationManagerFactory()
@@ -72,7 +73,7 @@ DriveNotificationManagerFactory::DriveNotificationManagerFactory()
   DependsOn(invalidation::ProfileInvalidationProviderFactory::GetInstance());
 }
 
-DriveNotificationManagerFactory::~DriveNotificationManagerFactory() {}
+DriveNotificationManagerFactory::~DriveNotificationManagerFactory() = default;
 
 KeyedService* DriveNotificationManagerFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
