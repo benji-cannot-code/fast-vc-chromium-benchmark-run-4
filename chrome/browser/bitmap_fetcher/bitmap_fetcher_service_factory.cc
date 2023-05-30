8eb5dc54c1a0b2ca350edad61c0585ba990e6ffb
Mikel Astiz <mastiz@chromium.org>
2023-05-30 18:17:00 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/bitmap_fetcher
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=tommycli@chromium.org
    
    Bug: 925323
    Change-Id: I1f55c77ccdb73b034cc9648479c95563ceddd26b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572887
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150698}

--- chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.cc
+++ chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.cc
@@ -16,7 +16,8 @@ BitmapFetcherService* BitmapFetcherServiceFactory::GetForBrowserContext(
 
 // static
 BitmapFetcherServiceFactory* BitmapFetcherServiceFactory::GetInstance() {
-  return base::Singleton<BitmapFetcherServiceFactory>::get();
+  static base::NoDestructor<BitmapFetcherServiceFactory> instance;
+  return instance.get();
 }
 
 BitmapFetcherServiceFactory::BitmapFetcherServiceFactory()
@@ -29,8 +30,7 @@ BitmapFetcherServiceFactory::BitmapFetcherServiceFactory()
               .WithGuest(ProfileSelection::kOriginalOnly)
               .Build()) {}
 
-BitmapFetcherServiceFactory::~BitmapFetcherServiceFactory() {
-}
+BitmapFetcherServiceFactory::~BitmapFetcherServiceFactory() = default;
 
 KeyedService* BitmapFetcherServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
--- chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.h
+++ chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_BITMAP_FETCHER_BITMAP_FETCHER_SERVICE_FACTORY_H_
 #define CHROME_BROWSER_BITMAP_FETCHER_BITMAP_FETCHER_SERVICE_FACTORY_H_
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 
 class BitmapFetcherService;
@@ -23,7 +23,7 @@ class BitmapFetcherServiceFactory : ProfileKeyedServiceFactory {
       delete;
 
  private:
-  friend struct base::DefaultSingletonTraits<BitmapFetcherServiceFactory>;
+  friend base::NoDestructor<BitmapFetcherServiceFactory>;
 
   BitmapFetcherServiceFactory();
   ~BitmapFetcherServiceFactory() override;
