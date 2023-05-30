154817cff0b6f65058b29dc932de30ae2e389c1c
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2023-05-30 20:11:04 +0000

    [Idle Timeout] Add a kill-switch feature flag
    Change-Id: I100aa80457a20052623552e6aad4105d1e573249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573887
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150805}

---
+++ chrome/browser/enterprise/idle/idle_features.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/idle/idle_features.h"
+
+namespace enterprise_idle {
+
+BASE_FEATURE(kIdleTimeout, "IdleTimeout", base::FEATURE_ENABLED_BY_DEFAULT);
+
+}  // namespace enterprise_idle
--- chrome/browser/enterprise/idle/idle_service.cc
+++ chrome/browser/enterprise/idle/idle_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/json/values_util.h"
+#include "chrome/browser/enterprise/idle/idle_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -88,6 +89,10 @@ IdleService::IdleService(Profile* profile)
       action_runner_(
           std::make_unique<ActionRunner>(profile_,
                                          ActionFactory::GetInstance())) {
+  if (!base::FeatureList::IsEnabled(kIdleTimeout)) {
+    // Policy disabled by kill-switch.
+    return;
+  }
   browser_observer_ = std::make_unique<BrowserObserver>(profile);
   DCHECK_EQ(profile_->GetOriginalProfile(), profile_);
   pref_change_registrar_.Init(profile->GetPrefs());
