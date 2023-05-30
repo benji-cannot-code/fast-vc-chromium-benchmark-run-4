afb97dfbdb8412a5e2cd3c4fd8189cf85d3b328e
Mikel Astiz <mastiz@chromium.org>
2023-05-30 16:53:42 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/ssl
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=cthomp@chromium.org
    
    Bug: 925323
    Change-Id: Ibb4331aabefa2498b399c16fb2714773389d39e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572891
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150642}

--- chrome/browser/ssl/sct_reporting_service_factory.cc
+++ chrome/browser/ssl/sct_reporting_service_factory.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // static
 SCTReportingServiceFactory* SCTReportingServiceFactory::GetInstance() {
-  return base::Singleton<SCTReportingServiceFactory>::get();
+  static base::NoDestructor<SCTReportingServiceFactory> instance;
+  return instance.get();
 }
 
 // static
