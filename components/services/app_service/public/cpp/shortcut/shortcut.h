95715fa328ec1cbf6d4343ad2e906ed57f218f78
Maggie Cai <mxcai@google.com>
2023-05-30 06:39:24 +0000

    [Shortcut] Add observer for shortcut registry cache.
    This CL adds an observer for shortcut registry cache.
    
    BUG=1412708
    
    Change-Id: If2ae1d0e80ef1ec38bca3bca27575342141ad6e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4566014
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150402}

--- components/services/app_service/public/cpp/shortcut/shortcut.h
+++ components/services/app_service/public/cpp/shortcut/shortcut.h
@@ -36,6 +36,8 @@ struct COMPONENT_EXPORT(SHORTCUT) Shortcut {
   Shortcut(Shortcut&&) = delete;
   Shortcut& operator=(Shortcut&&) = delete;
 
+  bool operator==(const Shortcut&) const;
+
   ~Shortcut();
 
   std::unique_ptr<Shortcut> Clone() const;
