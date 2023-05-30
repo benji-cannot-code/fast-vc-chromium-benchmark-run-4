189d741a031d3b0f645dc29458a62a43abb39a44
Nicholas Hollingum <hollingum@google.com>
2023-05-30 02:01:01 +0000

    borealis: Add a "beta" label to the borealis installer
    This label indicates to the user that "Steam for Chromebook" is still in
    beta.
    
    Bug: b:276392572
    Change-Id: If0456fe9f21641f4f5fac066879e8437113070ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564895
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150364}

--- chrome/browser/ui/views/borealis/borealis_installer_view.cc
+++ chrome/browser/ui/views/borealis/borealis_installer_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/borealis/borealis_util.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
+#include "chrome/browser/ui/views/borealis/borealis_beta_badge.h"
 #include "chrome/browser/ui/views/borealis/borealis_installer_disallowed_dialog.h"
 #include "chrome/browser/ui/views/borealis/borealis_installer_error_dialog.h"
 #include "chrome/browser/ui/views/borealis/borealis_splash_screen_view.h"
@@ -161,6 +162,10 @@ BorealisInstallerView::BorealisInstallerView(Profile* profile)
   primary_message_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   primary_message_label_->SetMaximumWidth(264);
   left_container_view->AddChildView(primary_message_label_.get());
+
+  beta_badge_ = left_container_view->AddChildView(
+      std::make_unique<views::BorealisBetaBadge>());
+  beta_badge_->SetProperty(views::kMarginsKey, gfx::Insets::TLBR(16, 0, 0, 0));
 
   secondary_message_label_ = new views::Label(
       GetSecondaryMessage(), views::style::CONTEXT_DIALOG_BODY_TEXT,
@@ -416,6 +421,9 @@ void BorealisInstallerView::OnStateUpdated() {
 
   const bool progress_bar_visible = state_ == State::kInstalling;
   progress_bar_->SetVisible(progress_bar_visible);
+
+  const bool beta_badge_visible = state_ != State::kCompleted;
+  beta_badge_->SetVisible(beta_badge_visible);
 
   DialogModelChanged();
   primary_message_label_->NotifyAccessibilityEvent(
--- chrome/browser/ui/views/borealis/borealis_installer_view.h
+++ chrome/browser/ui/views/borealis/borealis_installer_view.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/dialog_delegate.h"
 
 namespace views {
+class BorealisBetaBadge;
 class BoxLayout;
 class ImageView;
 class Label;
@@ -109,6 +110,7 @@ class BorealisInstallerView : public views::DialogDelegateView,
   raw_ptr<Profile, ExperimentalAsh> profile_ = nullptr;
   raw_ptr<views::Label, ExperimentalAsh> primary_message_label_ = nullptr;
   raw_ptr<views::Label, ExperimentalAsh> secondary_message_label_ = nullptr;
+  raw_ptr<views::BorealisBetaBadge, ExperimentalAsh> beta_badge_ = nullptr;
   raw_ptr<views::ProgressBar, ExperimentalAsh> progress_bar_ = nullptr;
   raw_ptr<views::Label, ExperimentalAsh>
       installation_progress_percentage_label_ = nullptr;
