8687eca4f63232de5b95e672e3688271ab89226b
Mikel Astiz <mastiz@chromium.org>
2023-05-30 15:30:11 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/feature_engagement
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=collinbaker@chromium.org
    
    Bug: 925323
    Change-Id: I0b62004c918f7b7e327cbe2fcead17c63b3d86cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571324
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150584}

--- chrome/browser/feature_engagement/tracker_factory.h
+++ chrome/browser/feature_engagement/tracker_factory.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 template <typename T>
-struct DefaultSingletonTraits;
+class NoDestructor;
 }  // namespace base
 
 namespace content {
@@ -35,7 +35,7 @@ class TrackerFactory : public ProfileKeyedServiceFactory {
   TrackerFactory& operator=(const TrackerFactory&) = delete;
 
  private:
-  friend struct base::DefaultSingletonTraits<TrackerFactory>;
+  friend base::NoDestructor<TrackerFactory>;
 
   TrackerFactory();
   ~TrackerFactory() override;
