1b62b7957266b7ddb1d574043e187664e346be16
Mikel Astiz <mastiz@chromium.org>
2023-05-30 21:06:11 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/accessibility
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=dtseng@chromium.org
    
    Bug: 925323
    Change-Id: If6e03e96da20ac3b19252a15b21a2acfaf236214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568011
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150852}

--- chrome/browser/accessibility/pdf_ocr_controller_factory.cc
+++ chrome/browser/accessibility/pdf_ocr_controller_factory.cc
@@ -17,7 +17,8 @@ PdfOcrController* PdfOcrControllerFactory::GetForProfile(Profile* profile) {
 
 // static
 PdfOcrControllerFactory* PdfOcrControllerFactory::GetInstance() {
-  return base::Singleton<PdfOcrControllerFactory>::get();
+  static base::NoDestructor<PdfOcrControllerFactory> instance;
+  return instance.get();
 }
 
 PdfOcrControllerFactory::PdfOcrControllerFactory()
