8c41099460c81437caffd7247d3560ce6ccda45c
Mikel Astiz <mastiz@chromium.org>
2023-05-30 22:36:57 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/cart
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=tiborg@chromium.org
    
    Bug: 925323
    Change-Id: I484bbaafbf5e863847cb16330a5c69d0f7299492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568342
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150914}

--- chrome/browser/cart/cart_service_factory.cc
+++ chrome/browser/cart/cart_service_factory.cc
@@ -22,7 +22,8 @@ std::unique_ptr<KeyedService> BuildCartService(
 
 // static
 CartServiceFactory* CartServiceFactory::GetInstance() {
-  return base::Singleton<CartServiceFactory>::get();
+  static base::NoDestructor<CartServiceFactory> instance;
+  return instance.get();
 }
 
 // static
--- chrome/browser/cart/cart_service_factory.h
+++ chrome/browser/cart/cart_service_factory.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CART_CART_SERVICE_FACTORY_H_
 #define CHROME_BROWSER_CART_CART_SERVICE_FACTORY_H_
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 
 class Profile;
@@ -26,7 +26,7 @@ class CartServiceFactory : public ProfileKeyedServiceFactory {
   static TestingFactory GetDefaultFactory();
 
  private:
-  friend struct base::DefaultSingletonTraits<CartServiceFactory>;
+  friend base::NoDestructor<CartServiceFactory>;
 
   CartServiceFactory();
   ~CartServiceFactory() override;
