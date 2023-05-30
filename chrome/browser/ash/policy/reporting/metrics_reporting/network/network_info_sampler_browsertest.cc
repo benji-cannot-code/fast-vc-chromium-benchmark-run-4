ec577e1a6e9fbf5b040507e1428d78f008abf26e
Ahmed Nasr <anasr@google.com>
2023-05-30 17:31:14 +0000

    Use MockClock for time control in NetworkInfoSamplerBrowserTest.
    Bug: b:284459886
    Change-Id: I2bc82ac99bac69a71a4075592b995033201534be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568550
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150669}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_info_sampler_browsertest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_info_sampler_browsertest.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/ash/login/test/session_manager_state_waiter.h"
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
-#include "chrome/browser/ash/policy/reporting/metrics_reporting/metric_browsertest_utils.h"
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/ash/settings/stub_cros_settings_provider.h"
+#include "chrome/browser/chromeos/reporting/metric_default_utils.h"
 #include "chromeos/ash/components/dbus/hermes/hermes_manager_client.h"
 #include "chromeos/ash/components/dbus/shill/shill_device_client.h"
 #include "chromeos/ash/components/settings/cros_settings_names.h"
@@ -21,11 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reporting/proto/synced/metric_data.pb.h"
 #include "components/reporting/proto/synced/record.pb.h"
 #include "components/reporting/proto/synced/record_constants.pb.h"
+#include "components/reporting/util/mock_clock.h"
 #include "content/public/test/browser_test.h"
 #include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
 
-namespace ash::reporting {
-
+namespace reporting {
 namespace {
 
 using ::chromeos::MissiveClientTestObserver;
@@ -111,10 +111,10 @@ class NetworkDevice {
 class NetworkInfoSamplerBrowserTest
     : public policy::DevicePolicyCrosBrowserTest {
  protected:
-  NetworkInfoSamplerBrowserTest() = default;
+  NetworkInfoSamplerBrowserTest() { test::MockClock::Get(); }
   ~NetworkInfoSamplerBrowserTest() override = default;
   void SetUpOnMainThread() override {
-    device_client_ = ash::ShillDeviceClient::Get()->GetTestInterface();
+    device_client_ = ::ash::ShillDeviceClient::Get()->GetTestInterface();
     device_client_->ClearDevices();
     ::ash::HermesManagerClient::Get()->GetTestInterface()->AddEuicc(
         dbus::ObjectPath("path0"), kEid0, true, 1);
@@ -124,7 +124,7 @@ class NetworkInfoSamplerBrowserTest
 
   void EnableReportingNetworkInterfaces() {
     scoped_testing_cros_settings_.device_settings()->SetBoolean(
-        kReportDeviceNetworkConfiguration, true);
+        ::ash::kReportDeviceNetworkConfiguration, true);
   }
 
   void AddDevice(const NetworkDevice& device) {
@@ -223,13 +223,10 @@ class NetworkInfoSamplerBrowserTest
     }
   }
 
-  MetricTestInitializationHelper metric_test_initialization_helper_{
-      &device_state_};
-
  private:
   raw_ptr<::ash::ShillDeviceClient::TestInterface, ExperimentalAsh>
       device_client_;
-  ScopedTestingCrosSettings scoped_testing_cros_settings_;
+  ::ash::ScopedTestingCrosSettings scoped_testing_cros_settings_;
 };
 
 IN_PROC_BROWSER_TEST_F(NetworkInfoSamplerBrowserTest,
@@ -241,9 +238,8 @@ IN_PROC_BROWSER_TEST_F(NetworkInfoSamplerBrowserTest,
   EnableReportingNetworkInterfaces();
   MissiveClientTestObserver observer(
       base::BindRepeating(&IsRecordNetworkInterface));
-  // Start initialization after the observer is initialized.
-  metric_test_initialization_helper_.SetUpDelayedInitialization();
 
+  test::MockClock::Get().Advance(metrics::InitDelayParam::Get());
   AssertNetworkInterfaces(devices, &observer);
 }
 
@@ -260,12 +256,11 @@ IN_PROC_BROWSER_TEST_F(NetworkInfoSamplerBrowserTest,
   EnableReportingNetworkInterfaces();
   MissiveClientTestObserver observer(
       base::BindRepeating(&IsRecordNetworkInterface));
-  // Start initialization after the observer is initialized.
-  metric_test_initialization_helper_.SetUpDelayedInitialization();
 
+  test::MockClock::Get().Advance(metrics::InitDelayParam::Get());
   AssertNetworkInterfaces(devices, &observer);
 }
 
 }  // namespace
 
-}  // namespace ash::reporting
+}  // namespace reporting
