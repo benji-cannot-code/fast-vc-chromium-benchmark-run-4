1e1e2cfbc75b61d38cc257dcd387eadae6752056
Kevin McNee <mcnee@chromium.org>
2023-05-30 21:08:28 +0000

    Restore unconditional use of SSL interstitials in <webview>
    We've reconsidered this change given the risk of breakage for enterprise
    workflows involving self-signed certs. We now restore the unconditional
    usage of SSL interstitials in <webview> regardless of the state of
    WebviewTagMPArchBehavior or its enterprise policy escape hatch.
    
    We also update the description of the enterprise policy to no longer
    mention SSL interstitials as being part of the change and update the
    intended timeline for the policy given the postponement of the launch of
    the remaining part of the change.
    
    Bug: 1261928, 1338009, 1442153
    Change-Id: I623034e847d19737c91162aa76eecc31434ebde1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563695
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150856}

--- extensions/browser/guest_view/web_view/web_view_guest.cc
+++ extensions/browser/guest_view/web_view/web_view_guest.cc
@@ -1113,7 +1113,8 @@ void WebViewGuest::WillAttachToEmbedder() {
 }
 
 bool WebViewGuest::RequiresSslInterstitials() const {
-  return !AreWebviewMPArchBehaviorsEnabled(browser_context());
+  // Some enterprise workflows rely on clicking through self-signed cert errors.
+  return true;
 }
 
 content::JavaScriptDialogManager* WebViewGuest::GetJavaScriptDialogManager(
