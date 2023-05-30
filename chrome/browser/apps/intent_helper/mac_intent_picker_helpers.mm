12a1313d17802b5335ad1ea7d6da44971957148e
Avi Drissman <avi@chromium.org>
2023-05-30 22:14:11 +0000

    Convert base::mac::LaunchApplication and all callers to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Because base::mac::LaunchApplication uses base::OnceCallback for
    Objective-C types, all users need to be converted at the same time to
    avoid ODR crashes.
    
    Bug: 1280317
    Change-Id: I065ff3fa136372fa85e71d6763ecf18b8d857885
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Low-Coverage-Reason: Doing core refactoring across all of Chromium; I do not own this code
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571700
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150907}

--- chrome/browser/apps/intent_helper/mac_intent_picker_helpers.mm
+++ chrome/browser/apps/intent_helper/mac_intent_picker_helpers.mm
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/mac/url_conversions.h"
 #include "ui/base/models/image_model.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace apps {
 
 namespace {
@@ -37,7 +41,7 @@ IntentPickerAppInfo AppInfoForAppUrl(NSURL* app_url) {
                           forKey:NSURLLocalizedNameKey
                            error:nil]) {
     // This shouldn't happen but just in case.
-    app_name = [app_url lastPathComponent];
+    app_name = app_url.lastPathComponent;
   }
   NSImage* app_icon = nil;
   if (![app_url getResourceValue:&app_icon
@@ -76,8 +80,7 @@ absl::optional<IntentPickerAppInfo> FindMacAppForUrl(const GURL& url) {
     if (!nsurl)
       return absl::nullopt;
 
-    SFUniversalLink* link =
-        [[[SFUniversalLink alloc] initWithWebpageURL:nsurl] autorelease];
+    SFUniversalLink* link = [[SFUniversalLink alloc] initWithWebpageURL:nsurl];
 
     if (link)
       return AppInfoForAppUrl(link.applicationURL);
--- chrome/browser/web_applications/os_integration/web_app_run_on_os_login_mac.mm
+++ chrome/browser/web_applications/os_integration/web_app_run_on_os_login_mac.mm
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/web_applications/os_integration/web_app_shortcut_mac.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 
-namespace web_app {
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
-namespace internals {
+namespace web_app::internals {
 
 bool RegisterRunOnOsLogin(const ShortcutInfo& shortcut_info) {
   base::FilePath shortcut_data_dir = GetShortcutDataDir(shortcut_info);
@@ -29,6 +31,4 @@ Result UnregisterRunOnOsLogin(const std::string& app_id,
   return Result::kOk;
 }
 
-}  // namespace internals
-
-}  // namespace web_app
+}  // namespace web_app::internals
