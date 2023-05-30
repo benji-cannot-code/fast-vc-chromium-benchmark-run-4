0d55da70cc712607687c1a72a03218412c7c2bbd
Mikel Astiz <mastiz@chromium.org>
2023-05-30 15:19:19 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/subresource_filter
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=johnidel@chromium.org
    
    Bug: 925323
    Change-Id: I9ad03e50fa49d28ba41fcd3c30e70acfd7eadd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567875
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150577}

--- chrome/browser/subresource_filter/subresource_filter_profile_context_factory.cc
+++ chrome/browser/subresource_filter/subresource_filter_profile_context_factory.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/subresource_filter/subresource_filter_profile_context_factory.h"
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -23,7 +23,8 @@ SubresourceFilterProfileContextFactory::GetForProfile(Profile* profile) {
 // static
 SubresourceFilterProfileContextFactory*
 SubresourceFilterProfileContextFactory::GetInstance() {
-  return base::Singleton<SubresourceFilterProfileContextFactory>::get();
+  static base::NoDestructor<SubresourceFilterProfileContextFactory> instance;
+  return instance.get();
 }
 
 SubresourceFilterProfileContextFactory::SubresourceFilterProfileContextFactory()
