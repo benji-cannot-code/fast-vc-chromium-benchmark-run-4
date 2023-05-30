43ff51a7a9d448390e56532cfc9b82d1ea36d333
Cole Horvitz <colehorvitz@chromium.org>
2023-05-30 20:29:50 +0000

    WebUI: Check-in expected bundle_js_test.py bundle outputs.
    Bug: 1431253
    Change-Id: I85d7cdd2b1425b0ba7a7165a9674f5b9c3b54ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571425
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150820}

---
+++ ui/webui/resources/tools/tests/bundle_js/expected/foo_ui_multi_bundle.rollup.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import './shared.rollup.js';
+
+// Copyright 2023 The Chromium Authors
+alert('Hello from resources/foo_resource.js');
+
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+alert('Hello from external/bar/bar.js');
+
+// Copyright 2023 The Chromium Authors
+alert('Hello from external/foo/foo.js');
+
+// Copyright 2023 The Chromium Authors
+
+alert('Hello from src/foo.js');
+//# sourceMappingURL=foo_ui.rollup.js.map
