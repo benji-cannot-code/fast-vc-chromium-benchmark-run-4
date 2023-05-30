792d75c99723afdb9c39b93bf8ad6e79a9deef1d
Steinar H. Gunderson <sesse@chromium.org>
2023-05-30 16:19:37 +0000

    Speed up SinCosDegrees() by manual range reduction.
    SinCosDegrees() is fortunate enough to take in its arguments in degrees
    (360 degrees = 1 turn) instead of radians (2pi radians = 1 turn).
    Accurate range reduction in degrees is much simpler than in radians;
    45 is a whole number and exactly representable as a double, unlike pi/2.
    This means that we can get greater accuracy _and_ higher speed than the
    current code of fmod() + internal sincos() range reduction, by doing it
    ourselves, mostly branch-free. (In particular, fmod() appears to be very
    slow on macOS.) This gives a small boost in MotionMark for the Leaves
    and/or Multiply subtests, which both make heavy use of rotate() in CSS.
    
    MotionMark (mac-m1_mini_2020-perf, 95% CI, only significant results):
    motionmark_ramp_leaves                    [ +0.7%,  +1.0%]
    
    Same with win-10-perf:
    motionmark_ramp_multiply                  [ +0.6%,  +1.9%]
    
    Change-Id: Iedf1822d2995408531a623d0768373703941e3e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4537360
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150624}

--- ui/gfx/geometry/angle_conversions.h
+++ ui/gfx/geometry/angle_conversions.h
@@ -12,17 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 GEOMETRY_EXPORT constexpr double DegToRad(double deg) {
-  return deg * base::kPiDouble / 180.0;
+  return deg * base::kDegToRadDouble;
 }
 GEOMETRY_EXPORT constexpr float DegToRad(float deg) {
-  return deg * base::kPiFloat / 180.0f;
+  return deg * base::kDegToRadFloat;
 }
 
 GEOMETRY_EXPORT constexpr double RadToDeg(double rad) {
-  return rad * 180.0 / base::kPiDouble;
+  return rad * base::kRadToDegDouble;
 }
 GEOMETRY_EXPORT constexpr float RadToDeg(float rad) {
-  return rad * 180.0f / base::kPiFloat;
+  return rad * base::kRadToDegFloat;
 }
 
 }  // namespace gfx
--- ui/gfx/geometry/transform.cc
+++ ui/gfx/geometry/transform.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/quaternion.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/geometry/sin_cos_degrees.h"
 #include "ui/gfx/geometry/transform_util.h"
 #include "ui/gfx/geometry/vector3d_f.h"
 
@@ -33,27 +34,6 @@ const double kEpsilon = std::numeric_limits<float>::epsilon();
 
 double TanDegrees(double degrees) {
   return std::tan(DegToRad(degrees));
-}
-
-struct SinCos {
-  double sin;
-  double cos;
-  bool IsZeroAngle() const { return sin == 0 && cos == 1; }
-};
-
-SinCos SinCosDegrees(double degrees) {
-  double n90degrees = degrees / 90.0;
-  int n = static_cast<int>(n90degrees);
-  if (n == n90degrees) {
-    n %= 4;
-    if (n < 0)
-      n += 4;
-    constexpr SinCos kSinCosN90[] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    return kSinCosN90[n];
-  }
-  // fmod is to reduce errors of DegToRad() with large |degrees|.
-  double rad = DegToRad(std::fmod(degrees, 360.0));
-  return SinCos{std::sin(rad), std::cos(rad)};
 }
 
 inline bool ApproximatelyZero(double x, double tolerance) {
