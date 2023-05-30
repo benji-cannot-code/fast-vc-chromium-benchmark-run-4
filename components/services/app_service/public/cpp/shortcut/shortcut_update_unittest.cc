1868da7fedfce89e3ed47cf6da9d59e234d40e26
Sébastien Lalancette <seblalancette@chromium.org>
2023-05-30 17:45:32 +0000

    Revert "[Shortcut] Add observer for shortcut registry cache."
    This reverts commit b23400ccec9500a3acacafc5796cd6ee27f343c9.
    
    Reason for revert: New tests are failing on builders, e.g.:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/36163/overview
    
    Original change's description:
    > [Shortcut] Add observer for shortcut registry cache.
    >
    > This CL adds an observer for shortcut registry cache.
    >
    > BUG=1412708
    >
    > Change-Id: If2ae1d0e80ef1ec38bca3bca27575342141ad6e9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4566014
    > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > Commit-Queue: Maggie Cai <mxcai@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1150402}
    
    Bug: 1412708
    Change-Id: Ia77b43b1b7ae9ffdf4a7f5b586a2b70bfc36ca25
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574307
    Owners-Override: Sébastien Lalancette <seblalancette@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150684}

--- components/services/app_service/public/cpp/shortcut/shortcut_update_unittest.cc
+++ components/services/app_service/public/cpp/shortcut/shortcut_update_unittest.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/services/app_service/public/cpp/shortcut/shortcut_update.h"
 
-#include <memory>
-
 #include "components/services/app_service/public/cpp/shortcut/shortcut.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -92,56 +90,6 @@ TEST_F(ShortcutUpdateTest, Merge) {
   EXPECT_EQ(shortcut_state.shortcut_source, ShortcutSource::kUser);
   EXPECT_EQ(shortcut_state.host_app_id, host_app_id_);
   EXPECT_EQ(shortcut_state.local_id, local_id_);
-}
-
-TEST_F(ShortcutUpdateTest, Equal) {
-  std::unique_ptr<Shortcut> state_1 = std::make_unique<Shortcut>("a", "b");
-  state_1->name = "name";
-  std::unique_ptr<Shortcut> state_same_as_1 =
-      std::make_unique<Shortcut>("a", "b");
-  state_same_as_1->name = "name";
-  std::unique_ptr<Shortcut> state_2 = std::make_unique<Shortcut>("a", "b");
-  state_2->name = "different name";
-
-  std::unique_ptr<Shortcut> delta_1 = std::make_unique<Shortcut>("a", "b");
-  delta_1->name = "new name";
-  std::unique_ptr<Shortcut> delta_same_as_1 =
-      std::make_unique<Shortcut>("a", "b");
-  delta_same_as_1->name = "new name";
-  std::unique_ptr<Shortcut> delta_2 = std::make_unique<Shortcut>("a", "b");
-  delta_2->name = "new new name";
-
-  // Test nullptr handling.
-  EXPECT_EQ(ShortcutUpdate(nullptr, delta_1.get()),
-            ShortcutUpdate(nullptr, delta_1.get()));
-  EXPECT_NE(ShortcutUpdate(nullptr, delta_1.get()),
-            ShortcutUpdate(state_1.get(), nullptr));
-  EXPECT_NE(ShortcutUpdate(nullptr, delta_1.get()),
-            ShortcutUpdate(state_1.get(), delta_1.get()));
-  EXPECT_NE(ShortcutUpdate(state_1.get(), nullptr),
-            ShortcutUpdate(nullptr, delta_1.get()));
-  EXPECT_EQ(ShortcutUpdate(state_1.get(), nullptr),
-            ShortcutUpdate(state_1.get(), nullptr));
-  EXPECT_NE(ShortcutUpdate(state_1.get(), nullptr),
-            ShortcutUpdate(state_1.get(), delta_1.get()));
-  EXPECT_NE(ShortcutUpdate(state_1.get(), delta_1.get()),
-            ShortcutUpdate(nullptr, delta_1.get()));
-  EXPECT_NE(ShortcutUpdate(state_1.get(), delta_1.get()),
-            ShortcutUpdate(state_1.get(), nullptr));
-  EXPECT_EQ(ShortcutUpdate(state_1.get(), delta_1.get()),
-            ShortcutUpdate(state_1.get(), delta_1.get()));
-
-  // Test deep equal.
-  EXPECT_EQ(ShortcutUpdate(state_1.get(), delta_1.get()),
-            ShortcutUpdate(state_same_as_1.get(), delta_same_as_1.get()));
-
-  // Test not equal.
-  EXPECT_NE(ShortcutUpdate(state_1.get(), delta_1.get()),
-            ShortcutUpdate(state_2.get(), delta_1.get()));
-  EXPECT_NE(ShortcutUpdate(state_1.get(), delta_1.get()),
-            ShortcutUpdate(state_1.get(), delta_2.get()));
-  EXPECT_NE(ShortcutUpdate(state_1.get(), delta_1.get()),
-            ShortcutUpdate(state_2.get(), delta_2.get()));
 }
 
 }  // namespace apps
