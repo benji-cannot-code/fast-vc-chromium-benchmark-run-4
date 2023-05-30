4228a909993c47971bcc55ad865bb1050a34c48e
Clement Yan <clamclamyan@google.com>
2023-05-30 02:28:47 +0000

    [WebUI] Enable trusted types CSP in sys-internals
    TEST = Verified manually at chrome://sys-internals
    
    Bug: 1400799
    Change-Id: I1500b0054ad798f9b71a0336bd957ba1b1e670fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408835
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150370}

--- chrome/browser/ui/webui/ash/sys_internals/sys_internals_ui.cc
+++ chrome/browser/ui/webui/ash/sys_internals/sys_internals_ui.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/sys_internals/sys_internals_message_handler.h"
+#include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "content/public/browser/web_ui.h"
@@ -26,7 +27,8 @@ SysInternalsUI::SysInternalsUI(content::WebUI* web_ui)
   content::WebUIDataSource* html_source =
       content::WebUIDataSource::CreateAndAdd(Profile::FromWebUI(web_ui),
                                              chrome::kChromeUISysInternalsHost);
-  html_source->DisableTrustedTypesCSP();
+  webui::EnableTrustedTypesCSP(html_source);
+
   html_source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::ScriptSrc,
       "script-src chrome://resources chrome://test 'self';");
