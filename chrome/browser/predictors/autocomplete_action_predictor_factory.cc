a117f67dcbb60abf2be66101ba9e37222534803c
Mikel Astiz <mastiz@chromium.org>
2023-05-30 19:17:22 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/predictors
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=ryansturm@chromium.org
    
    Bug: 925323
    Change-Id: I5cadeaca5e5db9b1188a8353e461c7bcafe89cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568016
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150750}

--- chrome/browser/predictors/autocomplete_action_predictor_factory.cc
+++ chrome/browser/predictors/autocomplete_action_predictor_factory.cc
@@ -22,7 +22,8 @@ AutocompleteActionPredictor* AutocompleteActionPredictorFactory::GetForProfile(
 // static
 AutocompleteActionPredictorFactory*
     AutocompleteActionPredictorFactory::GetInstance() {
-  return base::Singleton<AutocompleteActionPredictorFactory>::get();
+  static base::NoDestructor<AutocompleteActionPredictorFactory> instance;
+  return instance.get();
 }
 
 AutocompleteActionPredictorFactory::AutocompleteActionPredictorFactory()
@@ -38,7 +39,8 @@ AutocompleteActionPredictorFactory::AutocompleteActionPredictorFactory()
   DependsOn(PredictorDatabaseFactory::GetInstance());
 }
 
-AutocompleteActionPredictorFactory::~AutocompleteActionPredictorFactory() {}
+AutocompleteActionPredictorFactory::~AutocompleteActionPredictorFactory() =
+    default;
 
 KeyedService* AutocompleteActionPredictorFactory::BuildServiceInstanceFor(
     content::BrowserContext* profile) const {
--- chrome/browser/predictors/predictor_database_factory.cc
+++ chrome/browser/predictors/predictor_database_factory.cc
@@ -23,7 +23,8 @@ PredictorDatabase* PredictorDatabaseFactory::GetForProfile(Profile* profile) {
 
 // static
 PredictorDatabaseFactory* PredictorDatabaseFactory::GetInstance() {
-  return base::Singleton<PredictorDatabaseFactory>::get();
+  static base::NoDestructor<PredictorDatabaseFactory> instance;
+  return instance.get();
 }
 
 PredictorDatabaseFactory::PredictorDatabaseFactory()
@@ -36,8 +37,7 @@ PredictorDatabaseFactory::PredictorDatabaseFactory()
               .WithGuest(ProfileSelection::kOriginalOnly)
               .Build()) {}
 
-PredictorDatabaseFactory::~PredictorDatabaseFactory() {
-}
+PredictorDatabaseFactory::~PredictorDatabaseFactory() = default;
 
 KeyedService* PredictorDatabaseFactory::BuildServiceInstanceFor(
     content::BrowserContext* profile) const {
