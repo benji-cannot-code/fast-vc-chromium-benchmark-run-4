30432fe6b33d36e633e12177a99d22b25533c396
Wenbo Jie <wenbojie@chromium.org>
2023-05-30 06:08:35 +0000

    [Files F2] Add a rename slot for xf-tree-item
    * Support passing a input as a slot for tree item rename
    * Update refresh23 styles for xf-tree-item
    * Update styles for trailing icons
    * Add rename/trailing icon examples for tree items in Labs page
    
    Demo: http://screen/5wEZwCrvmYsJ8Wo.png
    
    Bug: b:279092120
    Change-Id: Idd2fa98ed0cd84142296dd4c9dcdc8402e028f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573948
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150397}

--- ash/webui/file_manager/resources/labs/main.ts
+++ ash/webui/file_manager/resources/labs/main.ts
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://file-manager/foreground/elements/icons.js';
 import 'chrome://file-manager/widgets/xf_breadcrumb.js';
 import 'chrome://file-manager/widgets/xf_icon.js';
 import 'chrome://file-manager/widgets/xf_tree.js';
 import 'chrome://file-manager/widgets/xf_tree_item.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 
 import {FocusOutlineManager} from 'chrome://resources/js/focus_outline_manager.js';
 
