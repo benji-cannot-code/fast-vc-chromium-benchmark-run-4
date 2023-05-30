448eb93cdf9d7f86ad47b20d7ee4c3bcc619ec5a
Ewann Pelle <ewannpv@chromium.org>
2023-05-30 15:46:57 +0000

    [iOS] remove not reached return
    Change-Id: I90341dc9e1b19699edbe0cb572896635854ef983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567356
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150599}

--- ios/chrome/browser/ui/overlays/overlay_coordinator_factory.mm
+++ ios/chrome/browser/ui/overlays/overlay_coordinator_factory.mm
@@ -79,7 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // TODO(crbug.com/1056837): Remove requirement once modalities are
       // converted to no longer use enums.
       NOTREACHED_NORETURN() << "Received unsupported modality.";
-      return nil;
     case OverlayModality::kWebContentArea:
       return [AlertOverlayCoordinator class];
     case OverlayModality::kInfobarBanner:
