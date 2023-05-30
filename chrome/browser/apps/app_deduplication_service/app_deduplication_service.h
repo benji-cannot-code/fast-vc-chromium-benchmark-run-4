71dbbbf3faa2976bac585f0dea5fd8b8b124d343
Sharmin Zaman <sharminzaman@google.com>
2023-05-30 01:14:58 +0000

    [Deduplication] Removed outdated code
    Deduplication code no longer utilises component updater. This CL removes any code corresponding to that.
    
    Bug: b/283900876
    Change-Id: I4bd8129c2a36d461f63a173db82e8b08b5374b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564933
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Sharmin Zaman <sharminzaman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150357}

--- chrome/browser/apps/app_deduplication_service/app_deduplication_service.h
+++ chrome/browser/apps/app_deduplication_service/app_deduplication_service.h
@@ -52,14 +52,6 @@ class AppDeduplicationService : public KeyedService,
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
  private:
-  friend class AppDeduplicationServiceTest;
-  FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceTest,
-                           OnDuplicatedGroupListUpdated);
-  FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceTest,
-                           ExactDuplicateAllInstalled);
-  FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceTest, Installation);
-  FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceTest, Websites);
-
   friend class AppDeduplicationServiceAlmanacTest;
   FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceAlmanacTest,
                            DeduplicateDataToEntries);
@@ -87,10 +79,6 @@ class AppDeduplicationService : public KeyedService,
   // A call is only made to the server if there is a difference of over 24 hours
   // between now and the time stored in the server pref.
   void StartLoginFlow();
-
-  // AppProvisioningDataManager::Observer:
-  void OnDuplicatedGroupListUpdated(
-      const proto::DuplicatedGroupList& duplicated_apps_map) override;
 
   // apps::AppRegistryCache::Observer:
   void OnAppUpdate(const apps::AppUpdate& update) override;
--- chrome/browser/apps/app_provisioning_service/app_provisioning_data_manager.h
+++ chrome/browser/apps/app_provisioning_service/app_provisioning_data_manager.h
@@ -18,7 +18,6 @@ namespace apps {
 
 struct ComponentFileContents {
   std::string app_with_locale_pb;
-  std::string deduplication_pb;
 };
 
 // The AppProvisioningDataManager parses the updates received from the Component
@@ -32,8 +31,6 @@ class AppProvisioningDataManager {
    public:
     virtual void OnAppWithLocaleListUpdated(
         const proto::AppWithLocaleList& app_with_locale_list) {}
-    virtual void OnDuplicatedGroupListUpdated(
-        const proto::DuplicatedGroupList& duplicated_group_list) {}
   };
 
   static AppProvisioningDataManager* Get();
@@ -71,7 +68,6 @@ class AppProvisioningDataManager {
 
   // The latest app data. Starts out as null.
   std::unique_ptr<proto::AppWithLocaleList> app_with_locale_list_;
-  std::unique_ptr<proto::DuplicatedGroupList> duplicated_group_list_;
 
   base::ObserverList<Observer> observers_;
 
