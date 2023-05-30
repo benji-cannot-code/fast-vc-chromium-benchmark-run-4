bd36bb6f92dcd39eee05b87843a57becdc8e8a35
Avi Drissman <avi@chromium.org>
2023-05-30 17:15:46 +0000

    Ensure the AppController singleton is created when expected
    With https://crrev.com/c/4566530, it would have been possible for
    AppController to be created earlier than expected with no feedback.
    Add some CHECKs to ChromeBrowserMainPartsMac::PreCreateMainMessageLoop()
    so that if that changes, it will be noticed.
    
    Bug: 1280317
    Change-Id: I073cc623ed2736714c132e77952039ac859e4f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570975
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150656}

--- chrome/browser/chrome_browser_main_mac.mm
+++ chrome/browser/chrome_browser_main_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/apple/bundle_locations.h"
+#include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
@@ -115,7 +116,9 @@ void ChromeBrowserMainPartsMac::PreCreateMainMessageLoop() {
 #endif  // !BUILDFLAG(CHROME_FOR_TESTING)
 
   // Create the app delegate by requesting the shared AppController.
+  CHECK_EQ(nil, NSApp.delegate);
   AppController* app_controller = AppController.sharedController;
+  CHECK_NE(nil, NSApp.delegate);
 
   chrome::BuildMainMenu(NSApp, app_controller,
                         l10n_util::GetStringUTF16(IDS_PRODUCT_NAME), false);
