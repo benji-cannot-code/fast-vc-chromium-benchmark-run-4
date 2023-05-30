c5b9d9292d07ae0500812c37b46c5b86fdc674a0
John Lee <johntlee@chromium.org>
2023-05-30 20:45:56 +0000

    Read Later: Hide list if there are no entries
    This prevents some extra padding from showing up when the list is empty.
    
    Bug: 1399976
    Change-Id: I981006b990b4570cdc139022a48d89a9337a1817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572637
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150834}

--- chrome/browser/resources/side_panel/reading_list/app.ts
+++ chrome/browser/resources/side_panel/reading_list/app.ts
@@ -316,6 +316,10 @@ export class ReadingListAppElement extends ReadingListAppElementBase {
   private shouldShowHr_(): boolean {
     return this.unreadItems_.length > 0 && this.readItems_.length > 0;
   }
+
+  private shouldShowList_(): boolean {
+    return this.unreadItems_.length > 0 || this.readItems_.length > 0;
+  }
 }
 
 declare global {
