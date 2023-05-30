a04c4c3ef0cdbd5f61a77589ab95ac441486fe09
Lei Zhang <thestig@chromium.org>
2023-05-30 05:59:36 +0000

    Fix double PrintPreviewUI creation
    Currently, GetWebUIFactoryFunction() allows PrintPreviewUI construction
    for all chrome://print paths except for "/pdf/index.html". However, this
    is out of date, as the URL path that needs to be filtered out is now
    "/pdf/index_print.html". Fix this issue by following the suggested
    approach in the code comments. Now GetWebUIFactoryFunction() only allows
    PrintPreviewUI construction for "/" and "/test_loader.html".
    
    Bug: 1448984
    Change-Id: Id865f9a5a91321c1bee6ddd553c80d00cdc2d856
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569063
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150394}

--- chrome/browser/ui/webui/print_preview/print_preview_ui_browsertest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/app/chrome_command_ids.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/webui/print_preview/print_preview_metrics.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -42,7 +44,8 @@ namespace {
 
 class PrintPreviewBrowserTest : public InProcessBrowserTest {
  public:
-  PrintPreviewBrowserTest() {}
+  PrintPreviewBrowserTest() = default;
+  ~PrintPreviewBrowserTest() override = default;
 
   void Print() {
     content::TestNavigationObserver nav_observer(nullptr);
@@ -50,6 +53,7 @@ class PrintPreviewBrowserTest : public InProcessBrowserTest {
     chrome::ExecuteCommand(browser(), IDC_PRINT);
     nav_observer.Wait();
     nav_observer.StopWatchingNewWebContents();
+    EXPECT_EQ(GURL("chrome://print/"), nav_observer.last_navigation_url());
   }
 };
 
@@ -144,5 +148,28 @@ IN_PROC_BROWSER_TEST_F(PrintPreviewBrowserTest,
              TabStripUserGestureDetails::GestureType::kOther));
 }
 #endif  // BUILDFLAG(IS_WIN)
+
+IN_PROC_BROWSER_TEST_F(PrintPreviewBrowserTest, PreviewStartedMetric) {
+  base::HistogramTester histogram_tester;
+  histogram_tester.ExpectBucketCount(
+      "PrintPreview.UserAction", printing::UserActionBuckets::kPreviewStarted,
+      /*expected_count=*/0);
+
+  Print();
+  histogram_tester.ExpectBucketCount(
+      "PrintPreview.UserAction", printing::UserActionBuckets::kPreviewStarted,
+      /*expected_count=*/1);
+
+  // Watch for the next navigation in the print preview dialog. The metric
+  // shouldn't change. See crbug.com/1075795 and crbug.com/1448984.
+  content::TestNavigationObserver nav_observer(nullptr);
+  nav_observer.WatchExistingWebContents();
+  nav_observer.Wait();
+  EXPECT_EQ(GURL("chrome-untrusted://print/1/0/print.pdf"),
+            nav_observer.last_navigation_url());
+  histogram_tester.ExpectBucketCount(
+      "PrintPreview.UserAction", printing::UserActionBuckets::kPreviewStarted,
+      /*expected_count=*/1);
+}
 
 }  // namespace
