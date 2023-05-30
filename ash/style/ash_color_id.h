ab1329fdb3c2068227471d8475766aaf51a31b90
Xiaodan Zhu <zxdan@chromium.org>
2023-05-30 23:14:29 +0000

    cros_next: add opacity to secondary button
    Add 30% opacity to the secondary pill button.
    
    Bug: b:284187073
    Change-Id: I3fc842dd187bfe8ff52d8545b77df55c074ef8cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575269
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150938}

--- ash/style/ash_color_id.h
+++ ash/style/ash_color_id.h
@@ -31,6 +31,7 @@ namespace ash {
   E_CPONLY(kColorAshControlBackgroundColorWarning) \
   E_CPONLY(kColorAshControlBackgroundColorPositive) \
   E_CPONLY(kColorAshFocusAuraColor) \
+  E_CPONLY(kColorAshSecondaryButtonBackgroundColor)\
   /* Content layer colors. */ \
   E_CPONLY(kColorAshScrollBarColor) \
   E_CPONLY(kColorAshSeparatorColor) \
