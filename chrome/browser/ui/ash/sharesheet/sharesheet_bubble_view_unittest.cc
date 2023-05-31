f376d1653ff6c08f8b9af597f887e99edd01d711
Nigel Tao <nigeltao@chromium.org>
2023-05-31 04:45:14 +0000

    [Copy To Clipboard] substitute fusebox filenames
    When LaCros is enabled, this fixes CTC for non-real (virtual) volumes
    the same way that crrev.com/c/4546339 fixed "Open With..." and
    crrev.com/c/4422480 fixed drag-and-drop for non-real volumes.
    
    BUG=1274983
    TEST=manual
    
    Change-Id: Ifdaff7fabc31866197676d2ecfddd9f0c61f2623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568465
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Auto-Submit: Nigel Tao <nigeltao@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151069}

--- chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view_unittest.cc
+++ chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view_unittest.cc
@@ -224,7 +224,7 @@ TEST_F(SharesheetBubbleViewTest, RecordShareActionCount) {
       ::sharesheet::kSharesheetShareActionResultHistogram,
       ::sharesheet::SharesheetMetrics::UserAction::kCopyAction, 1);
 
-  // Drive intent should show only drive action.
+  // Drive intent should show drive and copy actions.
   ShowAndVerifyBubble(::sharesheet::CreateDriveIntent(),
                       ::sharesheet::LaunchSource::kUnknown);
   CloseBubble();
@@ -233,7 +233,7 @@ TEST_F(SharesheetBubbleViewTest, RecordShareActionCount) {
       ::sharesheet::SharesheetMetrics::UserAction::kDriveAction, 1);
   histograms.ExpectBucketCount(
       ::sharesheet::kSharesheetShareActionResultHistogram,
-      ::sharesheet::SharesheetMetrics::UserAction::kCopyAction, 1);
+      ::sharesheet::SharesheetMetrics::UserAction::kCopyAction, 2);
 
   // Invalid intent should not show any actions.
   ShowAndVerifyBubble(::sharesheet::CreateInvalidIntent(),
@@ -244,7 +244,7 @@ TEST_F(SharesheetBubbleViewTest, RecordShareActionCount) {
       ::sharesheet::SharesheetMetrics::UserAction::kDriveAction, 1);
   histograms.ExpectBucketCount(
       ::sharesheet::kSharesheetShareActionResultHistogram,
-      ::sharesheet::SharesheetMetrics::UserAction::kCopyAction, 1);
+      ::sharesheet::SharesheetMetrics::UserAction::kCopyAction, 2);
 }
 
 TEST_F(SharesheetBubbleViewTest, ClickCopyToClipboard) {
