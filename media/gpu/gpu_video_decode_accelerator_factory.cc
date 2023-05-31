e75a31df210722f740f6527918aa5c3fb994b116
Abhijeet Kandalkar <abhijeet@igalia.com>
2023-05-31 06:15:35 +0000

    [ios] Enable hardware encoding/decoding for media
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    
    Bug: 1439606
    Change-Id: I8a693eeefb02d2a73c80a74d8b192e7abf6d61ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570939
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1151083}

--- media/gpu/gpu_video_decode_accelerator_factory.cc
+++ media/gpu/gpu_video_decode_accelerator_factory.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "media/gpu/windows/dxva_video_decode_accelerator_win.h"
 #endif
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
 #include "media/gpu/mac/vt_video_decode_accelerator_mac.h"
 #endif
 #if BUILDFLAG(USE_VAAPI)
@@ -70,7 +70,7 @@ gpu::VideoDecodeAcceleratorCapabilities GetDecoderCapabilitiesInternal(
       V4L2SliceVideoDecodeAccelerator::GetSupportedProfiles(),
       &capabilities.supported_profiles);
 #endif
-#elif BUILDFLAG(IS_MAC)
+#elif BUILDFLAG(IS_APPLE)
   capabilities.supported_profiles =
       VTVideoDecodeAccelerator::GetSupportedProfiles(workarounds);
 #endif
@@ -153,7 +153,7 @@ GpuVideoDecodeAcceleratorFactory::CreateVDA(
     &GpuVideoDecodeAcceleratorFactory::CreateV4L2SliceVDA,
 #endif
 
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
     &GpuVideoDecodeAcceleratorFactory::CreateVTVDA,
 #endif
   };
@@ -228,7 +228,7 @@ GpuVideoDecodeAcceleratorFactory::CreateV4L2SliceVDA(
 }
 #endif
 
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
 std::unique_ptr<VideoDecodeAccelerator>
 GpuVideoDecodeAcceleratorFactory::CreateVTVDA(
     const gpu::GpuDriverBugWorkarounds& workarounds,
