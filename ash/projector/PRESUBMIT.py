b53277583ad0a00d7fef4d059e94c95386725221
Takuto Ikuta <tikuta@chromium.org>
2023-05-31 16:12:33 +0000

    ash,chrome,content: remove `USE_PYTHON3 = True`
    presubmit now uses python3 only.
    
    Bug: 1207012
    Change-Id: Idcbbccd59cb69442e06b3058084c237db5208204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568251
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Owners-Override: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151282}

--- ash/projector/PRESUBMIT.py
+++ ash/projector/PRESUBMIT.py
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Optional but recommended
 PRESUBMIT_VERSION = '2.0.0'
 
-# Mandatory: run under Python 3
-USE_PYTHON3 = True
-
 
 def CheckMetricsChangeHasTrackerUpdatedMessage(input_api, output_api):
     """Reminder to update metrics tracker and verify in CL description."""
--- chrome/browser/PRESUBMIT.py
+++ chrome/browser/PRESUBMIT.py
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Presubmit script for Chromium browser code."""
 
-USE_PYTHON3 = True
 
 import re
 
