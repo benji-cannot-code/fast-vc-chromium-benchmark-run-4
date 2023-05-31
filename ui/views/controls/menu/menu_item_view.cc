2599e42b5ec2726b445b1e6bd055b9ccbfadcba1
Peter Kasting <pkasting@chromium.org>
2023-05-31 15:50:50 +0000

    Icons on parents of submenus should be included in the icon max width.
    For example, bookmark folders are a submenu, so none of their icons were
    included in this calculation.
    
    The effect of this was severe, as the overall menu width and the text
    start locations are computed based on this value, so wrongly setting it
    to zero would result in very glitchy-looking menus. This is more visible
    than it used to be since r1148577 stopped reserving the check width even
    for menus without checks/radios.
    
    This also does a little more cleanup -- be consistent about whether to
    use has_icons_ or has_icons(), and don't null-check GetRootMenuItem()
    (which can't return null).
    
    Bug: none
    Change-Id: Ic98874b5167e4fcacd58dfe3897e8fbb5f00a8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4576162
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151269}

--- ui/views/controls/menu/menu_item_view.cc
+++ ui/views/controls/menu/menu_item_view.cc
@@ -157,8 +157,7 @@ std::u16string MenuItemView::GetTooltipText(const gfx::Point& p) const {
     return std::u16string();
   }
 
-  const MenuItemView* root_menu_item = GetRootMenuItem();
-  if (root_menu_item->canceled_) {
+  if (GetRootMenuItem()->canceled_) {
     // TODO(sky): if |canceled_| is true, controller->exit_type() should be
     // something other than ExitType::kNone, but crash reports seem to indicate
     // otherwise. Figure out why this is needed.
@@ -580,8 +579,9 @@ int MenuItemView::GetHeightForWidth(int width) const {
 
   const gfx::Insets margins = GetContainerMargins();
   int height = children().front()->GetHeightForWidth(width - margins.width());
-  if (!icon_view_ && GetRootMenuItem()->has_icons())
+  if (!icon_view_ && GetRootMenuItem()->has_icons_) {
     height = std::max(height, MenuConfig::instance().check_height);
+  }
 
   height += margins.height();
 
@@ -1223,8 +1223,7 @@ void MenuItemView::DestroyAllMenuHosts() {
 int MenuItemView::GetTopMargin() const {
   int margin = top_margin_;
   if (margin < 0) {
-    const MenuItemView* root = GetRootMenuItem();
-    margin = root && root->has_icons_
+    margin = GetRootMenuItem()->has_icons_
                  ? MenuConfig::instance().item_top_margin
                  : MenuConfig::instance().item_no_icon_top_margin;
   }
@@ -1235,8 +1234,7 @@ int MenuItemView::GetTopMargin() const {
 int MenuItemView::GetBottomMargin() const {
   int margin = bottom_margin_;
   if (margin < 0) {
-    const MenuItemView* root = GetRootMenuItem();
-    margin = root && root->has_icons_
+    margin = GetRootMenuItem()->has_icons_
                  ? MenuConfig::instance().item_bottom_margin
                  : MenuConfig::instance().item_no_icon_bottom_margin;
   }
@@ -1309,7 +1307,7 @@ MenuItemView::MenuItemDimensions MenuItemView::CalculateDimensions() const {
   dimensions.height = child_size.height();
   // Adjust item content height if menu has both items with and without icons.
   // This way all menu items will have the same height.
-  if (!icon_view_ && GetRootMenuItem()->has_icons()) {
+  if (!icon_view_ && GetRootMenuItem()->has_icons_) {
     dimensions.height =
         std::max(dimensions.height, MenuConfig::instance().check_height);
   }
@@ -1465,10 +1463,9 @@ int MenuItemView::GetMaxIconViewWidth() const {
       // alignment of other items.
       return 0;
     }
-    if (item->HasSubmenu()) {
-      return item->GetMaxIconViewWidth();
-    }
-    return item->icon_view_ ? item->icon_view_->GetPreferredSize().width() : 0;
+    return std::max(
+        item->icon_view_ ? item->icon_view_->GetPreferredSize().width() : 0,
+        item->HasSubmenu() ? item->GetMaxIconViewWidth() : 0);
   });
   return base::ranges::max(widths);
 }
