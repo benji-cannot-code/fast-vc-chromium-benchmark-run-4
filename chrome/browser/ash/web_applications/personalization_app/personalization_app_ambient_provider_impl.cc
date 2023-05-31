399e9ca250751e3961422668ab4c9c144344a583
Thuong Phan <thuongphan@chromium.org>
2023-05-31 06:00:33 +0000

    personalization: hide tod banner for users with managed wallpapers
    Hide promo banner for the users whose wallpapers are policy controlled
    as they cannot change the wallpapers by themselves.
    
    BUG=b:283826179
    TEST=unit_tests --gtest_filter='*PersonalizationAppAmbientProviderImpl*'
    
    Change-Id: I58c7ffc4d871e9f48d28b3e34d838fddc6b11529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4559202
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151081}

--- chrome/browser/ash/web_applications/personalization_app/personalization_app_ambient_provider_impl.cc
+++ chrome/browser/ash/web_applications/personalization_app/personalization_app_ambient_provider_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ambient/ambient_ui_model.h"
 #include "ash/public/cpp/ambient/common/ambient_settings.h"
 #include "ash/public/cpp/image_downloader.h"
+#include "ash/public/cpp/wallpaper/wallpaper_controller.h"
 #include "ash/shell.h"
 #include "ash/webui/personalization_app/mojom/personalization_app.mojom.h"
 #include "ash/webui/personalization_app/mojom/personalization_app_mojom_traits.h"
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_manager.h"
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_manager_factory.h"
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_metrics.h"
+#include "chrome/browser/ash/web_applications/personalization_app/personalization_app_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/prefs/pref_service.h"
 #include "mojo/public/cpp/bindings/message.h"
@@ -704,7 +706,13 @@ void PersonalizationAppAmbientProviderImpl::StartScreenSaverPreview() {
 
 void PersonalizationAppAmbientProviderImpl::ShouldShowTimeOfDayBanner(
     ShouldShowTimeOfDayBannerCallback callback) {
+  // Time of day banner should not display for the users with policy managed
+  // wallpapers who cannot change their wallpapers. Note that although
+  // enterprise users are not able to access screen saver, some of them are able
+  // to access wallpaper subpage and change wallpapers.
   std::move(callback).Run(
+      !WallpaperController::Get()->IsWallpaperControlledByPolicy(
+          GetAccountId(profile_)) &&
       features::IsTimeOfDayScreenSaverEnabled() &&
       contextual_tooltip::ShouldShowNudge(
           profile_->GetPrefs(),
