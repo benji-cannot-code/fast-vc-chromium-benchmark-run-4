4df220d62c2f77cbbe72f25a049f27708782ec46
Eriko Kurimoto <elkurin@chromium.org>
2023-05-31 14:17:18 +0000

    BrowserLoader: Refine background stateful loading log
    This CL updates the log on loading stateful lacros in the background.
    In case where stateful lacros loading failed in the background, it
    looked strange with the existing code:
    "stateful lacros-chrome installation completed in the background in ,
    version is invalid"
    Refined it to look better.
    
    This CL does not affect the behavior.
    
    Bug: b:270164115
    Change-Id: Ic9441f62eb1817214742dbbe6f19304c476c4264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570315
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151210}

--- chrome/browser/ash/crosapi/stateful_lacros_loader.cc
+++ chrome/browser/ash/crosapi/stateful_lacros_loader.cc
@@ -228,8 +228,9 @@ void StatefulLacrosLoader::OnLoad(
       error == component_updater::CrOSComponentManager::Error::NONE &&
       !path.empty();
   LOG_IF(WARNING, !is_stateful_lacros_available)
-      << "Error loading lacros component image: " << static_cast<int>(error)
-      << ", " << path;
+      << "Error loading lacros component image in the "
+      << (callback ? "foreground" : "background") << ": "
+      << static_cast<int>(error) << ", " << path;
 
   version_ = is_stateful_lacros_available
                  ? browser_util::GetInstalledLacrosComponentVersion(
@@ -240,9 +241,11 @@ void StatefulLacrosLoader::OnLoad(
   if (callback) {
     std::move(callback).Run(version_.value(), path_.value());
   } else {
-    LOG(WARNING)
-        << "stateful lacros-chrome installation completed in the background in "
-        << path << ", version is " << version_.value();
+    if (is_stateful_lacros_available) {
+      LOG(WARNING) << "stateful lacros-chrome installation completed in the "
+                   << "background in " << path << ", version is "
+                   << version_.value();
+    }
   }
 }
 
