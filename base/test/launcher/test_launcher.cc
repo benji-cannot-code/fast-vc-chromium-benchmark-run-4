10a8e259edd8b592d5b6536f5416dc08df025cad
Ian Vollick <vollick@chromium.org>
2023-05-31 19:19:58 +0000

    [ios] Enable content_browsertests
    Based on a previous run [1] we still have 240 tests that need to be
    skipped so we will add a filter file for these to enable coverage while
    these failures are investigated.
    
    Also, following vulkan_swiftshader_content_browsertests, we will force
    bot mode to ensure that we can still run parallel jobs (since this
    suite can take a long time when run in serial). We also set up
    sharding for this suite for more parallelism.
    
    credit: thanks to rohitrao@ for pointing out that BUILD.gn needed to
    be updated for the filters to be included.
    
    [1] https://ci.chromium.org/ui/p/chromium/builders/try/ios-blink-dbg-fyi/547/overview
    
    Bug: 1413706
    Change-Id: I21785b7e234fabf595d979042d1049174a6c0fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568919
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Lindsay Pasricha <lindsayw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151409}

--- base/test/launcher/test_launcher.cc
+++ base/test/launcher/test_launcher.cc
@@ -98,6 +98,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/fuchsia_logging.h"
 #endif
 
+#if BUILDFLAG(IS_IOS)
+#include "base/path_service.h"
+#endif
+
 namespace base {
 
 // See
@@ -1616,8 +1620,16 @@ bool TestLauncher::Init(CommandLine* command_line) {
     for (auto filter_file :
          SplitStringPiece(filter, FILE_PATH_LITERAL(";"), base::TRIM_WHITESPACE,
                           base::SPLIT_WANT_ALL)) {
+#if BUILDFLAG(IS_IOS)
+      // On iOS, the filter files are bundled with the test application.
+      base::FilePath data_dir;
+      PathService::Get(DIR_SRC_TEST_DATA_ROOT, &data_dir);
+      base::FilePath filter_file_path = data_dir.Append(FilePath(filter_file));
+#else
       base::FilePath filter_file_path =
           base::MakeAbsoluteFilePath(FilePath(filter_file));
+#endif  // BUILDFLAG(IS_IOS)
+
       if (!LoadFilterFile(filter_file_path, &positive_file_filter,
                           &negative_test_filter_))
         return false;
