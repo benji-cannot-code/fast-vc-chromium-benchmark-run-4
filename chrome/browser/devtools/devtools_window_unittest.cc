49f1110a47bbcee92990e2d2b09e00cc3a919218
Robert Lin <robertlin@chromium.org>
2023-05-31 12:44:01 +0000

    Preload: Enable DevToolsTabTarget by default.
    This CL enables DevToolsTabTarget by default, because it is expected to
    be launched in M116.
    
    Bug: 1348385
    Change-Id: Iabd93308168bf97fe1970baf4efbe89342118e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567767
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151179}

--- chrome/browser/devtools/devtools_window_unittest.cc
+++ chrome/browser/devtools/devtools_window_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/branding_buildflags.h"
 #include "build/buildflag.h"
+#include "chrome/browser/browser_features.h"
 #include "chrome/browser/ui/webui/devtools_ui.h"
 #include "components/version_info/channel.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -33,6 +34,10 @@ TEST_F(DevToolsWindowTest, GetDevtoolsURL) {
   std::string expected =
       "devtools://devtools/bundled/devtools_app.html?remoteBase=" +
       DevToolsUI::GetRemoteBaseURL().spec();
+  if (base::FeatureList::IsEnabled(::features::kDevToolsTabTarget)) {
+    expected += "&targetType=tab";
+  }
+
   EXPECT_EQ(expected, GetURL(version_info::Channel::UNKNOWN));
   EXPECT_EQ(expected, GetURL(version_info::Channel::CANARY));
 #if BUILDFLAG(IS_CHROMEOS_ASH)
