8b602ee3edb5ef4e888a6b2a62b93d74ea297259
Elad Alon <eladalon@chromium.org>
2023-05-31 06:41:21 +0000

    Fix auto-accept for the share-this-tab dialog
    Fix the problem where the dialog is not dismissed after being accepted,
    by calling AcceptDialog() rather than Accept().
    
    Bug: None
    Change-Id: I82bee2d4849e7dd7f10c4b6b7a511bd5c171b542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574327
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151091}

--- chrome/browser/ui/views/desktop_capture/share_this_tab_dialog_views.cc
+++ chrome/browser/ui/views/desktop_capture/share_this_tab_dialog_views.cc
@@ -293,7 +293,7 @@ void ShareThisTabDialogView::Activate() {
 
   // In tests.
   if (ShouldAutoAccept()) {
-    Accept();
+    AcceptDialog();
   } else if (ShouldAutoReject()) {
     CancelDialog();
   }
