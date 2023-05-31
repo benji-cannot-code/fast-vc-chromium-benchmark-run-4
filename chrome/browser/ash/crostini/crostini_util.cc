55181cdd43b5f37fd3d57bcae7ba0b84b228fc86
Joel Hockey <joelhockey@chromium.org>
2023-05-31 04:53:41 +0000

    Put spinner on Terminal for terminal apps
    When opening an app like vim, it is better to have the spinner on the
    Terminal app which is the window which will eventually open when VM
    starts, rather than spinner with vim icon, which then goes away when
    Terminal launches.
    
    Bug: 853560
    Change-Id: I49c493420972aab5c5fd7cd294839f2109633dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570317
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151072}

--- chrome/browser/ash/crostini/crostini_util.cc
+++ chrome/browser/ash/crostini/crostini_util.cc
@@ -132,11 +132,6 @@ void OnSharePathForLaunchApplication(
     OnApplicationLaunched(app_id, std::move(callback),
                           crostini::CrostiniResult::SUCCESS, true,
                           std::string());
-    // The spinner for the app may start, but since terminal will launch rather
-    // that the GUI app with `app_id`, we must close the spinner.
-    if (auto* chrome_controller = ChromeShelfController::instance()) {
-      chrome_controller->GetShelfSpinnerController()->CloseSpinner(app_id);
-    }
     return;
   }
 
@@ -279,6 +274,8 @@ void LaunchCrostiniAppImpl(
   registry_service->AppLaunched(app_id);
   crostini_manager->UpdateLaunchMetricsForEnterpriseReporting();
 
+  auto spinner_app_id =
+      registration.Terminal() ? guest_os::kTerminalSystemAppId : app_id;
   auto restart_id = crostini_manager->RestartCrostini(
       container_id,
       base::BindOnce(
@@ -305,7 +302,8 @@ void LaunchCrostiniAppImpl(
           args, std::move(callback)));
 
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
-      FROM_HERE, base::BindOnce(&AddSpinner, restart_id, app_id, profile),
+      FROM_HERE,
+      base::BindOnce(&AddSpinner, restart_id, spinner_app_id, profile),
       base::Milliseconds(kDelayBeforeSpinnerMs));
 }
 
