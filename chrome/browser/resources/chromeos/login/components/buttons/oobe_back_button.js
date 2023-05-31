d16e94ca7050bb21658b6fe7b8c748b341606950
Simon Ziegltrum <ziegltrum@google.com>
2023-05-31 20:33:13 +0000

    [OOBE] Apply cros_color_overrides to buttons
    Bug: b:269226083 b:269226380
    Change-Id: I1aebf970e39a452afa6e75fc0754b0c339bb01e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572478
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151448}

--- chrome/browser/resources/chromeos/login/components/buttons/oobe_back_button.js
+++ chrome/browser/resources/chromeos/login/components/buttons/oobe_back_button.js
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * label-for-aria value.
  */
 
+import '//resources/cr_elements/chromeos/cros_color_overrides.css.js';
 import '//resources/cr_elements/cr_button/cr_button.js';
 import '//resources/cr_elements/cr_icons.css.js';
 import '//resources/cr_elements/cr_shared_style.css.js';
