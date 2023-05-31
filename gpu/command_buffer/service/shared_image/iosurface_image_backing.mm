d685e1f6dc29d07559a9624777683aa255342d6d
Sunny Sachanandani <sunnyps@chromium.org>
2023-05-31 21:03:09 +0000

    gpu: Allow Graphite Metal backend for developer builds
    Graphite Metal backend is not intended to be launched, but is still
    useful during development and debugging to figure out if an issue is in
    Dawn or Graphite. Enable it by default on non-official developer builds
    on Mac. Also fix minor issues discovered while building and running with
    --skia-graphite-backend=metal.
    
    Bug: 1423574
    Change-Id: I6db8e2be04d91f0fc5e8455440608894de0f10ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570696
    Reviewed-by: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151468}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsobject.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "components/viz/common/gpu/dawn_context_provider.h"
+#include "components/viz/common/gpu/metal_context_provider.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "components/viz/common/resources/resource_sizes.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
@@ -125,8 +126,7 @@ std::vector<skgpu::graphite::BackendTexture> CreateGraphiteMetalTextures(
   graphite_textures.reserve(num_planes);
   for (int plane = 0; plane < num_planes; plane++) {
     SkISize sk_size = gfx::SizeToSkISize(format.GetPlaneSize(plane, size));
-    skgpu::graphite::BackendTexture backend_texture(sk_size,
-                                                    mtl_textures[plane].get());
+    graphite_textures.emplace_back(sk_size, mtl_textures[plane].get());
   }
   return graphite_textures;
 }
@@ -439,10 +439,10 @@ class IOSurfaceImageBacking::SkiaGraphiteIOSurfaceRepresentation
   }
 
   void EndWriteAccess() override {
-    backing_impl()->HandleEndAccessSync(/*readonly=*/false);
     for (auto& surface : write_surfaces_) {
-      surface->flush();
+      DCHECK(surface->unique());
     }
+    backing_impl()->HandleEndAccessSync(/*readonly=*/false);
     write_surfaces_.clear();
   }
 
@@ -1047,7 +1047,7 @@ IOSurfaceImageBacking::ProduceSkiaGraphite(
     // Use GPU main recorder since this should only be called for
     // fulfilling Graphite promise images on GPU main thread.
     return std::make_unique<SkiaGraphiteIOSurfaceRepresentation>(
-        manager, this, tracker, context_state->gpu_graphite_main_recorder(),
+        manager, this, tracker, context_state->gpu_main_graphite_recorder(),
         std::move(mtl_textures));
 #endif
   }
--- gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h
+++ gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/config/gpu_preferences.h"
 #include "gpu/gpu_gles2_export.h"
 #include "gpu/vulkan/buildflags.h"
+#include "skia/buildflags.h"
 #include "third_party/dawn/include/dawn/webgpu.h"
 #include "third_party/dawn/include/dawn/webgpu_cpp.h"
 #include "third_party/skia/include/core/SkYUVAInfo.h"
+#include "third_party/skia/include/gpu/graphite/TextureInfo.h"
 #include "ui/gfx/buffer_types.h"
 
 #if BUILDFLAG(ENABLE_VULKAN)
@@ -122,6 +124,13 @@ GPU_GLES2_EXPORT skgpu::graphite::TextureInfo GetGraphiteTextureInfo(
     viz::SharedImageFormat format,
     int plane_index = 0,
     bool mipmapped = false);
+
+#if BUILDFLAG(SKIA_USE_METAL)
+GPU_GLES2_EXPORT skgpu::graphite::MtlTextureInfo GetGraphiteMetalTextureInfo(
+    viz::SharedImageFormat format,
+    int plane_index = 0,
+    bool mipmapped = false);
+#endif
 
 }  // namespace gpu
 
