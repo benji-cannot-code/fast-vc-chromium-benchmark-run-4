8dff9511b63c4d155fe092e8dc43d829f29cf51c
Tommy C. Li <tommycli@chromium.org>
2023-05-31 16:08:10 +0000

    [page_image_service] Add an end-to-end test for Suggest backend
    Adds an end-to-end test for the Suggest backend. Also refactors the
    class to remove the odd dependency on AutocompleteProviderClient.
    
    Bug: b:244507194, 1367485, b:248367751, 1449377
    Change-Id: Ic9465205ddf22b47ed63d48daf37d95f9d059f44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568620
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151280}

--- chrome/browser/image_service/image_service_factory.cc
+++ chrome/browser/image_service/image_service_factory.cc
@@ -3,13 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "chrome/browser/image_service/image_service_factory.h"
 
-#include "base/no_destructor.h"
-#include "chrome/browser/autocomplete/chrome_autocomplete_provider_client.h"
+#include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
+#include "chrome/browser/autocomplete/remote_suggestions_service_factory.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "components/page_image_service/image_service.h"
 
@@ -37,6 +40,8 @@ ImageServiceFactory::ImageServiceFactory()
               // Guest mode.
               .WithGuest(ProfileSelection::kOriginalOnly)
               .Build()) {
+  DependsOn(TemplateURLServiceFactory::GetInstance());
+  DependsOn(RemoteSuggestionsServiceFactory::GetInstance());
   DependsOn(OptimizationGuideKeyedServiceFactory::GetInstance());
   DependsOn(SyncServiceFactory::GetInstance());
 }
@@ -47,9 +52,12 @@ KeyedService* ImageServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   auto* profile = Profile::FromBrowserContext(context);
   return new ImageService(
-      std::make_unique<ChromeAutocompleteProviderClient>(profile),
+      TemplateURLServiceFactory::GetForProfile(profile),
+      RemoteSuggestionsServiceFactory::GetForProfile(
+          profile, /*create_if_necessary=*/true),
       OptimizationGuideKeyedServiceFactory::GetForProfile(profile),
-      SyncServiceFactory::GetForProfile(profile));
+      SyncServiceFactory::GetForProfile(profile),
+      std::make_unique<ChromeAutocompleteSchemeClassifier>(profile));
 }
 
 // static
