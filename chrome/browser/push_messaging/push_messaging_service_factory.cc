4d168d3d5891d3c736ac5b4440bbade11a27828b
Mikel Astiz <mastiz@chromium.org>
2023-05-31 15:09:38 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/push_messaging
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: 925323
    Change-Id: I22729410b417ab7cfdfce00fb7d650fbb01f9aa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573625
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151239}

--- chrome/browser/push_messaging/push_messaging_service_factory.cc
+++ chrome/browser/push_messaging/push_messaging_service_factory.cc
@@ -38,7 +38,8 @@ PushMessagingServiceImpl* PushMessagingServiceFactory::GetForProfile(
 
 // static
 PushMessagingServiceFactory* PushMessagingServiceFactory::GetInstance() {
-  return base::Singleton<PushMessagingServiceFactory>::get();
+  static base::NoDestructor<PushMessagingServiceFactory> instance;
+  return instance.get();
 }
 
 PushMessagingServiceFactory::PushMessagingServiceFactory()
@@ -62,7 +63,7 @@ PushMessagingServiceFactory::PushMessagingServiceFactory()
 #endif
 }
 
-PushMessagingServiceFactory::~PushMessagingServiceFactory() {}
+PushMessagingServiceFactory::~PushMessagingServiceFactory() = default;
 
 void PushMessagingServiceFactory::RestoreFactoryForTests(
     content::BrowserContext* context) {
