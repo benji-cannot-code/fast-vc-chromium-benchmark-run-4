e8c05555f3cfb2f0a3be78ac22c2605c35e9da87
Solomon Kinard <solomonkinard@chromium.org>
2023-05-31 21:49:23 +0000

    Revert "gpu: Allow Graphite Metal backend for developer builds"
    This reverts commit d5c2d5950f66418d339805caa3eecb0cd5f9190a.
    
    Reason for revert: Reason for revert: Suspected cause of tree closure. Error: https://ci.chromium.org/ui/p/chromium/builders/ci/mac-arm64-archive-rel/20494/overview.
    
    Original change's description:
    > gpu: Allow Graphite Metal backend for developer builds
    >
    > Graphite Metal backend is not intended to be launched, but is still
    > useful during development and debugging to figure out if an issue is in
    > Dawn or Graphite. Enable it by default on non-official developer builds
    > on Mac. Also fix minor issues discovered while building and running with
    > --skia-graphite-backend=metal.
    >
    > Bug: 1423574
    > Change-Id: I6db8e2be04d91f0fc5e8455440608894de0f10ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570696
    > Reviewed-by: Kevin Lubick <kjlubick@chromium.org>
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Peng Huang <penghuang@chromium.org>
    > Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    > Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1151468}
    
    Bug: 1423574
    Change-Id: I07840688b4f4509f5945ad1ba4c8e87ebe55274c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577641
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Solomon Kinard <solomonkinard@google.com>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Owners-Override: Solomon Kinard <solomonkinard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151484}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -264,7 +264,6 @@ static const char* const kSwitchNames[] = {
     switches::kProfilingFlush,
     switches::kRunAllCompositorStagesBeforeDraw,
     switches::kSkiaFontCacheLimitMb,
-    switches::kSkiaGraphiteBackend,
     switches::kSkiaResourceCacheLimitMb,
     switches::kTestGLLib,
     switches::kTraceToConsole,
--- gpu/command_buffer/service/shared_image/shared_image_format_service_utils.cc
+++ gpu/command_buffer/service/shared_image/shared_image_format_service_utils.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/resource_format.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
+#include "third_party/skia/include/gpu/graphite/TextureInfo.h"
 
 namespace gpu {
 
@@ -289,7 +290,25 @@ skgpu::graphite::TextureInfo GetGraphiteTextureInfo(
     bool mipmapped) {
   if (gr_context_type == GrContextType::kGraphiteMetal) {
 #if BUILDFLAG(SKIA_USE_METAL)
-    return GetGraphiteMetalTextureInfo(format, plane_index, mipmapped);
+    MTLPixelFormat mtl_pixel_format =
+        static_cast<MTLPixelFormat>(ToMTLPixelFormat(format, plane_index));
+    if (mtl_pixel_format != MTLPixelFormatInvalid) {
+      // Must match CreateMetalTexture in iosurface_image_backing.mm.
+      // TODO(sunnyps): Move constants to a common utility header.
+      skgpu::graphite::MtlTextureInfo mtl_texture_info;
+      mtl_texture_info.fSampleCount = 1;
+      mtl_texture_info.fFormat = mtl_pixel_format;
+      mtl_texture_info.fUsage =
+          MTLTextureUsageRenderTarget | MTLTextureUsageShaderRead;
+#if BUILDFLAG(IS_IOS)
+      mtl_texture_info.fStorageMode = MTLStorageModeShared;
+#else
+      mtl_texture_info.fStorageMode = MTLStorageModePrivate;
+#endif
+      mtl_texture_info.fMipmapped =
+          mipmapped ? skgpu::Mipmapped::kYes : skgpu::Mipmapped::kNo;
+      return mtl_texture_info;
+    }
 #endif
   } else {
     CHECK_EQ(gr_context_type, GrContextType::kGraphiteDawn);
