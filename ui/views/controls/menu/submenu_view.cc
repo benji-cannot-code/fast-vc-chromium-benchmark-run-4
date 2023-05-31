f0c5c34b6cb0a4ef5bbd17284e49e3c1b5164aa8
Peter Kasting <pkasting@chromium.org>
2023-05-31 16:05:01 +0000

    Misc. cleanup for menu code.
    * Make a couple class-scope functions file-scope (or inline). This
      required changing how MenuPart and its Type enum were declared.
    * Factor out some common expressions into helper functions.
    * Make fewer places assume that the scroll container is the root view.
      (It is, but assuming this equivalence makes the code less clear.)
    * Use gfx::Rect::Contains() instead of manual bounds checking.
    * Use the ConvertPointTo/FromScreen() variants that return by value
      instead of the ones that use an outparam.
    * More const.
    * Pass coords as gfx::Point, not int+int.
    
    Bug: none
    Change-Id: I8c75a6d064e4071ba0aacf720cc8abaa97da4927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575743
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151277}

--- ui/views/controls/menu/submenu_view.cc
+++ ui/views/controls/menu/submenu_view.cc
@@ -460,7 +460,7 @@ bool SubmenuView::SkipDefaultKeyEventProcessing(const ui::KeyEvent& e) {
   return views::FocusManager::IsTabTraversalKeyEvent(e);
 }
 
-MenuItemView* SubmenuView::GetMenuItem() {
+const MenuItemView* SubmenuView::GetMenuItem() const {
   return parent_menu_item_;
 }
 
--- ui/views/controls/menu/submenu_view.h
+++ ui/views/controls/menu/submenu_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
@@ -144,7 +145,10 @@ class VIEWS_EXPORT SubmenuView : public View,
   bool SkipDefaultKeyEventProcessing(const ui::KeyEvent& e) override;
 
   // Returns the parent menu item we're showing children for.
-  MenuItemView* GetMenuItem();
+  const MenuItemView* GetMenuItem() const;
+  MenuItemView* GetMenuItem() {
+    return const_cast<MenuItemView*>(std::as_const(*this).GetMenuItem());
+  }
 
   // Set the drop item and position.
   void SetDropMenuItem(MenuItemView* item, MenuDelegate::DropPosition position);
