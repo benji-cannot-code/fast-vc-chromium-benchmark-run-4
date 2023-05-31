2b8af00aab7ab234280b066afbaa5585deef733f
Jun Kokatsu <jkokatsu@google.com>
2023-05-31 14:20:30 +0000

    Add kDanglingMarkupInWindowName to UKM
    Usage of dangling markup in iframe's name is somewhat high.
    Let's add a UKM to figure out how it's been used and where.
    
    [1] https://chromestatus.com/metrics/feature/timeline/popularity/4490
    
    Bug: 1450092
    Change-Id: Ief3c2b8858b4322e06d64c23bdf05df2d8c3dddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575452
    Auto-Submit: Jun Kokatsu <jkokatsu@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151212}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -300,6 +300,7 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
                kServiceWorkerBypassFetchHandlerForMainResourceByOriginTrial,
            WebFeature::kCascadedCSSZoomNotEqualToOne,
            WebFeature::kV8Window_QueryLocalFonts_Method,
-           WebFeature::kHiddenUntilFoundAttribute}));
+           WebFeature::kHiddenUntilFoundAttribute,
+           WebFeature::kDanglingMarkupInWindowName}));
   return *opt_in_features;
 }
