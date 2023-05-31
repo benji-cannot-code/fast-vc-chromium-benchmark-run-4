26feeb45608dfceb9a87b786437ec398165d7532
Tom Sepez <tsepez@chromium.org>
2023-05-31 15:33:45 +0000

    Re-order member destruction in ~ComponentCloudPolicyUpdaterTest()
    Avoid a spurious dangling ptr warning by ensuring the ResourceCache
    outlives the ComponentCloudPolicyStore which is using it.
    
    Bug: 1401495
    Change-Id: I398ca805028cfbefcf550a467d395947eade4310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575415
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151256}

--- components/policy/core/common/cloud/component_cloud_policy_updater_unittest.cc
+++ components/policy/core/common/cloud/component_cloud_policy_updater_unittest.cc
@@ -82,6 +82,7 @@ class ComponentCloudPolicyUpdaterTest : public testing::Test {
 
   const PolicyNamespace kTestPolicyNS{POLICY_DOMAIN_EXTENSIONS, kTestExtension};
   base::test::TaskEnvironment task_env_;
+  std::unique_ptr<ResourceCache> cache_;
   std::unique_ptr<ComponentCloudPolicyStore> store_;
   MockComponentCloudPolicyStoreDelegate store_delegate_;
   network::TestURLLoaderFactory loader_factory_;
@@ -91,7 +92,6 @@ class ComponentCloudPolicyUpdaterTest : public testing::Test {
 
  private:
   base::ScopedTempDir temp_dir_;
-  std::unique_ptr<ResourceCache> cache_;
   std::string public_key_;
 };
 
