26d033f42a92bc4166548660840e2ac4a4ddab07
Tim Sergeant <tsergeant@chromium.org>
2023-05-31 11:02:16 +0000

    Revert "Add WebAppIntegration.IncompatibleAppShim test."
    This reverts commit 90c1ec7d9a9c64ffa363b7565178142932e51b97.
    
    Reason for revert: The new test is failing on Mac 11 tests: https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/18559/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/18560/overview
    
    Original change's description:
    > Add WebAppIntegration.IncompatibleAppShim test.
    >
    > This test aims to cover the same code that the (long disabled)
    > AppShimInteractiveTest.RebuildShim test did. One differences being
    > that that test relied on differences in architecture between the app shim
    > and chrome, while this test relies on differences in the expected entry
    > point exposed by chrome. Both should result in the same app shim
    > behavior.
    >
    > Another difference is that the old test uses (deprecated) chrome apps
    > while this test uses PWAs.
    >
    > Since this is the only AppShimInteractiveTest that doesn't already
    > have equivalent tests elsewhere, this should pave the way for deleting
    > the entire test suite.
    >
    > Bug: 509774
    > Include-Ci-Only-Tests: true
    > Cq-Include-Trybots: luci.chrome.try:mac-chrome
    > Validate-Test-Flakiness: skip
    > Change-Id: Ifd4160f30d5c10b46ac8ba48b722212a3680d61a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569285
    > Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1150996}
    
    Bug: 509774
    Change-Id: I8b33cb004a3e9f7a890115492597bd39e9ad265b
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573270
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151161}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -170,8 +170,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/app_shim_registry_mac.h"
 #include "chrome/browser/web_applications/os_integration/web_app_shortcut_mac.h"
 #include "chrome/browser/web_applications/web_app_ui_manager.h"
-#include "chrome/common/mac/app_mode_common.h"
-#include "chrome/test/base/launchservices_utils_mac.h"
 #include "net/base/filename_util.h"
 #include "skia/ext/skia_utils_mac.h"
 #endif
@@ -2390,8 +2388,7 @@ void WebAppIntegrationTestDriver::UninstallFromOs(Site site) {
 }
 
 #if BUILDFLAG(IS_MAC)
-void WebAppIntegrationTestDriver::CorruptAppShim(Site site,
-                                                 AppShimCorruption corruption) {
+void WebAppIntegrationTestDriver::CorruptAppShim(Site site) {
   if (!BeforeStateChangeAction(__FUNCTION__)) {
     return;
   }
@@ -2404,33 +2401,7 @@ void WebAppIntegrationTestDriver::CorruptAppShim(Site site,
   base::FilePath bin_path = app_path.AppendASCII("Contents")
                                 .AppendASCII("MacOS")
                                 .AppendASCII("app_mode_loader");
-
-  switch (corruption) {
-    case AppShimCorruption::kNoExecutable:
-      EXPECT_TRUE(base::DeleteFile(bin_path));
-      break;
-    case AppShimCorruption::kIncompatibleVersion: {
-      // Find and replace the entry point symbol in the app shim executable with
-      // something that definitely doesn't exist in the Chrome framework.
-      std::string bin_contents;
-      EXPECT_TRUE(base::ReadFileToString(bin_path, &bin_contents));
-      auto pos = bin_contents.find(APP_SHIM_ENTRY_POINT_NAME_STRING);
-      ASSERT_NE(pos, std::string::npos);
-      bin_contents[pos] = 'D';
-      EXPECT_TRUE(base::WriteFile(bin_path, bin_contents));
-
-      // Since we modified the binary, we need to re-sign it.
-      std::string codesign_output;
-      std::vector<std::string> codesign_argv = {"codesign", "--force", "--sign",
-                                                "-", bin_path.value()};
-      EXPECT_TRUE(base::GetAppOutputAndError(base::CommandLine(codesign_argv),
-                                             &codesign_output))
-          << "Failed to sign executable at " << bin_path << ": "
-          << codesign_output;
-      break;
-    }
-  }
-
+  EXPECT_TRUE(base::DeleteFile(bin_path));
   AfterStateChangeAction();
 }
 
@@ -4214,20 +4185,14 @@ bool WebAppIntegrationTestDriver::LaunchFromAppShim(
       override_registration_->test_override->chrome_apps_folder(), app_name,
       app_id);
 
-  base::FilePath chrome_path = ::test::GuessAppBundlePath();
-  chrome_path =
-      chrome_path.Append("Contents")
-          .Append("MacOS")
-          .Append(chrome_path.BaseName().RemoveFinalExtension().value());
-
   AppShimLaunchWaiter launch_waiter(wait_for_complete_launch);
   apps::AppShimManager::Get()->SetAppShimObserverForTesting(&launch_waiter);
+
   LaunchShimForTesting(app_path, urls,
                        base::BindOnce(&AppShimLaunchWaiter::OnLaunchStarted,
                                       launch_waiter.AsWeakPtr()),
                        base::BindOnce(&AppShimLaunchWaiter::OnShimTerminated,
-                                      launch_waiter.AsWeakPtr()),
-                       chrome_path);
+                                      launch_waiter.AsWeakPtr()));
   launch_waiter.Wait();
 
   apps::AppShimManager::Get()->SetAppShimObserverForTesting(nullptr);
--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
@@ -140,8 +140,6 @@ enum class SubAppInstallDialogOptions {
   kPolicyOverride
 };
 
-enum class AppShimCorruption { kNoExecutable, kIncompatibleVersion };
-
 // These structs are used to store the current state of the world before & after
 // each state-change action.
 
@@ -328,7 +326,7 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   void UninstallPolicyApp(Site site);
   void UninstallFromOs(Site site);
 #if BUILDFLAG(IS_MAC)
-  void CorruptAppShim(Site site, AppShimCorruption corruption);
+  void CorruptAppShim(Site site);
   void QuitAppShim(Site site);
 #endif
 
