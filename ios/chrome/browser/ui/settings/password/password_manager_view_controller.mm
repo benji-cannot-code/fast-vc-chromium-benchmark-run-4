8a3c987b02513d5ea5cba32044515d18c527bc6a
Noémie St-Onge <noemies@google.com>
2023-05-31 18:02:42 +0000

    [iOS] Add "No issues found" to Password Checkup cell accessibility label
    This CL adds clarity to the accessibility label of the Password
    Manager's Password Checkup cell whenever the current password check
    state is safe.
    
    Fixed: 1450002
    Fixed: 1448987
    Fixed: 1448977
    Change-Id: Ia6da55c393eea2de54e90f08a85c5fc170582760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573891
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151355}

--- ios/chrome/browser/ui/settings/password/password_manager_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_view_controller.mm
@@ -444,7 +444,7 @@ bool AreIssuesEqual(const std::vector<password_manager::AffiliatedGroup>& lhs,
   [self setSearchBarEnabled:self.shouldEnableSearchBar];
   [self updatePasswordCheckButtonWithState:self.passwordCheckState];
   [self updatePasswordCheckStatusLabelWithState:self.passwordCheckState];
-  [self updatePasswordCheckSection];
+  [self updatePasswordCheckSectionWithState:self.passwordCheckState];
   [self updateUIForEditState];
 }
 
@@ -837,7 +837,7 @@ bool AreIssuesEqual(const std::vector<password_manager::AffiliatedGroup>& lhs,
     return;
   }
 
-  [self updatePasswordCheckSection];
+  [self updatePasswordCheckSectionWithState:state];
 
   // When the Password Checkup feature is enabled, this timestamp only appears
   // in the detail text of the Password Checkup status cell. It is therefore
@@ -1703,6 +1703,19 @@ bool AreIssuesEqual(const std::vector<password_manager::AffiliatedGroup>& lhs,
   }
 }
 
+- (void)setPasswordProblemsItemAccessibilityLabelForSafeState {
+  NSIndexPath* indexPath =
+      [self.tableViewModel indexPathForItemType:ItemTypePasswordCheckStatus
+                              sectionIdentifier:SectionIdentifierPasswordCheck];
+  UITableViewCell* passwordProblemsCell =
+      [self.tableView cellForRowAtIndexPath:indexPath];
+  passwordProblemsCell.accessibilityLabel = [NSString
+      stringWithFormat:
+          @"%@. %@", passwordProblemsCell.accessibilityLabel,
+          l10n_util::GetNSString(
+              IDS_IOS_PASSWORD_CHECKUP_SAFE_STATE_ACCESSIBILITY_LABEL)];
+}
+
 // Logs metrics related to favicons for the Password Manager.
 - (void)logMetricsForFavicons {
   DCHECK(!_faviconMetricLogged);
@@ -1840,7 +1853,7 @@ bool AreIssuesEqual(const std::vector<password_manager::AffiliatedGroup>& lhs,
   [self deleteItemAtIndexPaths:indexPaths];
 }
 
-- (void)updatePasswordCheckSection {
+- (void)updatePasswordCheckSectionWithState:(PasswordCheckUIState)state {
   if (![self.tableViewModel
           hasSectionForSectionIdentifier:SectionIdentifierPasswordCheck]) {
     return;
@@ -1850,6 +1863,11 @@ bool AreIssuesEqual(const std::vector<password_manager::AffiliatedGroup>& lhs,
       performBatchUpdates:^{
         if (_passwordProblemsItem) {
           [self reconfigureCellsForItems:@[ _passwordProblemsItem ]];
+          // When in safe state, a custom accessibility label needs to be set
+          // for the Password Checkup cell.
+          if (state == PasswordCheckStateSafe) {
+            [self setPasswordProblemsItemAccessibilityLabelForSafeState];
+          }
         }
         if (_checkForProblemsItem) {
           // If kIOSPasswordCheckup feature is disabled, only reconfigure the
