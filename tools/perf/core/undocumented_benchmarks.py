47b3a1474d161094019fc9d8b40ec7aabc15de04
Omer Katz <omerkatz@chromium.org>
2023-05-31 13:16:27 +0000

    [heap] Add and enable MinorMC variant for Octane and Jetstream2
    Add a MinorMC variant for Octane and Jetstream2 and enable on the
    linux-perf and mac-m1_mini_2020 (only Jetstream2) bots.
    
    Bug: v8:12612, chromium:1448815
    Change-Id: I6a3016b4211dc685b5d4c20b7c83e5dadd184cd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577057
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151191}

--- tools/perf/core/undocumented_benchmarks.py
+++ tools/perf/core/undocumented_benchmarks.py
@@ -13,6 +13,7 @@ UNDOCUMENTED_BENCHMARKS = {
     'load_library_perf_tests',
     'memory.desktop',
     'octane',
+    'octane-minormc',
     'performance_browser_tests',
     'startup.mobile',
     'system_health.webview_startup',
