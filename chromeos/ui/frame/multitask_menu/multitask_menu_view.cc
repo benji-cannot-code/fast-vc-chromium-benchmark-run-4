7bc8fe71c4892886ab826dbe8b1db41d0ab9d9ef
Phillip Vanderlaat <pvanderlaat@google.com>
2023-05-31 18:42:21 +0000

    Fix multitask menu colors on lacros
    This is a reland of https://crrev.com/c/4553644
    
    Test: Manual
    Change-Id: Ied29421b35089c2f1f183e7d1dc3a34703e4c14f
    Fixed: b/281107973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563669
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Phillip Vanderlaat <pvanderlaat@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151391}

--- chromeos/ui/frame/multitask_menu/multitask_menu_view.cc
+++ chromeos/ui/frame/multitask_menu/multitask_menu_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/user_metrics.h"
 #include "base/timer/timer.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "chromeos/ui/base/display_util.h"
 #include "chromeos/ui/base/window_properties.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/default_style.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/compositor/layer.h"
 #include "ui/display/screen.h"
 #include "ui/events/types/event_type.h"
@@ -182,6 +184,9 @@ MultitaskMenuView::MultitaskMenuView(aura::Window* window,
       close_callback_(std::move(close_callback)) {
   DCHECK(window);
   DCHECK(close_callback_);
+  if (features::IsJellyEnabled()) {
+    SetBackground(views::CreateThemedSolidBackground(ui::kColorSysSurface3));
+  }
   SetUseDefaultFillLayout(true);
 
   window_observation_.Observe(window);
