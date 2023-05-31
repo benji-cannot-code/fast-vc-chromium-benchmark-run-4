6a94529cda1a7a0ba4fca00a511c9d4a5ef55602
Anthony Cui <cuianthony@google.com>
2023-05-31 22:02:41 +0000

    [Translate metrics cleanup] Remove Translate.UserActionDuration metrics code
    OBSOLETE_HISTOGRAM[Translate.UserActionDuration]=No longer used
    
    Bug: 1266978
    Change-Id: I276093c56f71bce51593e84a623fe9e274b557cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571224
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151491}

--- components/translate/core/common/translate_metrics.cc
+++ components/translate/core/common/translate_metrics.cc
@@ -24,7 +24,6 @@ const char kTranslateTimeToBeReady[] = "Translate.Translation.TimeToBeReady";
 const char kTranslateTimeToLoad[] = "Translate.Translation.TimeToLoad";
 const char kTranslateTimeToTranslate[] =
     "Translate.Translation.TimeToTranslate";
-const char kTranslateUserActionDuration[] = "Translate.UserActionDuration";
 const char kTranslateLanguageDeterminedDuration[] =
     "Translate.LanguageDeterminedDuration";
 const char kTranslatedLanguageDetectionContentLength[] =
@@ -54,11 +53,6 @@ void ReportTimeToTranslate(double time_in_msec) {
   UMA_HISTOGRAM_MEDIUM_TIMES(
       metrics_internal::kTranslateTimeToTranslate,
       base::Microseconds(static_cast<int64_t>(time_in_msec * 1000.0)));
-}
-
-void ReportUserActionDuration(base::TimeTicks begin, base::TimeTicks end) {
-  UMA_HISTOGRAM_LONG_TIMES(metrics_internal::kTranslateUserActionDuration,
-                           end - begin);
 }
 
 void ReportLanguageDeterminedDuration(base::TimeTicks begin,
