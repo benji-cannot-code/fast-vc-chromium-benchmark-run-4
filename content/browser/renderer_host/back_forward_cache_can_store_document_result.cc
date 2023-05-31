36cf250308d2d05a69fb032824fce03999c05018
Mingyu Lei <leimy@chromium.org>
2023-05-31 02:31:18 +0000

    CCNS BFCache: add new not restored reason for cookie disabled
    Bug: 1444297
    Change-Id: I5ad0ef3cefb2504a68a5c00028b36c59feb441b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572748
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151035}

--- content/browser/renderer_host/back_forward_cache_can_store_document_result.cc
+++ content/browser/renderer_host/back_forward_cache_can_store_document_result.cc
@@ -188,6 +188,8 @@ ProtoEnum::BackForwardCacheNotRestoredReason NotRestoredReasonToTraceEnum(
       return ProtoEnum::ERROR_DOCUMENT;
     case Reason::kFencedFramesEmbedder:
       return ProtoEnum::FENCED_FRAMES_EMBEDDER;
+    case Reason::kCookieDisabled:
+      return ProtoEnum::COOKIE_DISABLED;
     case Reason::kBlocklistedFeatures:
       return ProtoEnum::BLOCKLISTED_FEATURES;
     case Reason::kUnknown:
@@ -445,6 +447,8 @@ std::string BackForwardCacheCanStoreDocumentResult::NotRestoredReasonToString(
       return "Error documents cannot be stored in bfcache";
     case Reason::kFencedFramesEmbedder:
       return "Pages using FencedFrames cannot be stored in bfcache.";
+    case Reason::kCookieDisabled:
+      return "Cookie is disabled for the page.";
   }
 }
 
@@ -527,6 +531,8 @@ BackForwardCacheCanStoreDocumentResult::NotRestoredReasonToReportString(
     case Reason::kCacheControlNoStoreCookieModified:
     case Reason::kCacheControlNoStoreHTTPOnlyCookieModified:
       return "Cache-control:no-store";
+    case Reason::kCookieDisabled:
+      return "Cookie is disabled";
     case Reason::kDisableForRenderFrameHostCalled:
       return DisabledReasonsToString(disabled_reasons_,
                                      /*for_not_restored_reasons=*/true);
