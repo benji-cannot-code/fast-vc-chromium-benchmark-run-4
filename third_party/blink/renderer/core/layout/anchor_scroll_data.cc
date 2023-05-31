74b26a9f8eb3838d575d3c46582bfc43ff85dc09
Xiaocheng Hu <xiaochengh@chromium.org>
2023-05-31 22:06:04 +0000

    [anchor-position] Port position fallback result getters to LayoutBox
    So that these getters are easier to use, and callers don't need to
    worry about block fragmentation.
    
    Bug: 1309178
    Change-Id: If26e0dc10a5518c8c0999a9ab00d6942ee7d5ad4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563164
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151497}

--- third_party/blink/renderer/core/layout/anchor_scroll_data.cc
+++ third_party/blink/renderer/core/layout/anchor_scroll_data.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
-#include "third_party/blink/renderer/core/layout/ng/ng_anchor_query.h"
-#include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 
@@ -75,13 +73,8 @@ const Vector<PhysicalScrollRange>* GetNonOverflowingScrollRanges(
   if (!layout_object || !layout_object->IsOutOfFlowPositioned()) {
     return nullptr;
   }
-  DCHECK(layout_object->IsBox());
-  const auto& layout_results = To<LayoutBox>(layout_object)->GetLayoutResults();
-  if (layout_results.empty()) {
-    return nullptr;
-  }
-  // TODO(crbug.com/1309178): Make sure it works when the box is fragmented.
-  return layout_results.front()->PositionFallbackNonOverflowingRanges();
+  CHECK(layout_object->IsBox());
+  return To<LayoutBox>(layout_object)->PositionFallbackNonOverflowingRanges();
 }
 
 }  // namespace
--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -60,6 +60,7 @@ struct BoxLayoutExtraInput;
 struct NGFragmentGeometry;
 struct NGPhysicalBoxStrut;
 struct PaintInfo;
+struct PhysicalScrollRange;
 
 enum SizeType { kMainOrPreferredSize, kMinSize, kMaxSize };
 enum AvailableLogicalHeightType {
@@ -1744,6 +1745,11 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   // acceptable anchor element.
   // https://drafts.csswg.org/css-anchor-position-1/#ref-for-valdef-anchor-implicit
   const LayoutObject* AcceptableImplicitAnchor() const;
+
+  // Returns position fallback results for anchor positioned element.
+  absl::optional<wtf_size_t> PositionFallbackIndex() const;
+  const Vector<PhysicalScrollRange>* PositionFallbackNonOverflowingRanges()
+      const;
 
  protected:
   ~LayoutBox() override;
