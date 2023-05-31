0da763673e8b0a47c9d5d098fccae8aaf92f76bc
Katie Dektar <katie@chromium.org>
2023-05-31 17:12:25 +0000

    [A11y] Prepare to share some extension code with Lacros
    Select to Speak and Switch Access both inject a content script into
    Google Docs that informs Docs to annotate the canvas for a11y. This
    script was not injected in Lacros browsers because STS/Switch run as
    component extension in Ash.
    
    This change prepares some code in Chrome OS extensions to share with
    Lacros in an upcoming patch: specifically manifest generation template
    and the gdocs_script.
    
    Test:Manual from crrev.com/c/4569470, compile.
    
    Ax-Relnotes: N/A
    Bug: b:272150598
    Change-Id: I03de3e9a1920e0d04ce4193422197bd85b7893d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575567
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151313}

--- chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
+++ chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
@@ -4,8 +4,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 function triggerDocsCanvasAnnotationMode() {
+  let extensionId = '';
+  if (!chrome.runtime) {
+    // Manifest v3: Not running in extension's runtime.
+    // Parse this script's URL to determine extension ID.
+    // The URL will look like
+    // `chrome-extension://extensionId/common/gdocs_script.js.
+    extensionId = document.currentScript.src.split('/')[2];
+  } else {
+    // Manifest v2.
+    extensionId = chrome.runtime.id;
+  }
   const scriptContents = `
-    window['_docs_annotate_canvas_by_ext'] = "${chrome.runtime.id}";
+    window['_docs_annotate_canvas_by_ext'] = "${extensionId}";
   `;
   const script = document.createElement('script');
   script.innerHTML = scriptContents;
