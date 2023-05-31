652f014f592f1a357e5a9725fcf8cb9bd88372ac
Femi Adegunloye <mrfemi@google.com>
2023-05-31 00:53:01 +0000

    Revert "chrome-flags/lacros: enable LacrosColorManagement by default"
    This reverts commit a465b4fb99e1b25edc56e7f616cd8e7d0fbe87b7.
    
    Reason for revert: There are crash reports from Lacros version 114.0.5735.xx. This CL will disable the flag until a fix can be applied.
    
    Original change's description:
    > Reland "chrome-flags/lacros: enable LacrosColorManagement by default"
    >
    > This is a reland of commit cf7ec069a9da56a9be8156e484e98ac666af2c48
    >
    > The two crashes that were being seen before the revert have been handled:
    >  "[Wayland error] zcr_color_manager_vN: error N: Unable to find a matrix matching N"
    >  -> fix: don't send unknown MatrixIDs/RangeIDs over the protocol
    >  "libwayland: listener function for opcode 5 of zcr_color_space_v1 is NULL"
    >  -> fix: improved version handling so running lacros with older
    >  protocols doesn't crash.
    >
    > Original change's description:
    > > chrome-flags/lacros: enable LacrosColorManagement by default
    > >
    > > Set LacrosColorManagement flag to be enabled by default.
    > >
    > > BUG=b:192562912
    > >
    > > Change-Id: I0b53454939f9b327c4567798092d074868982177
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938357
    > > Reviewed-by: ccameron chromium <ccameron@chromium.org>
    > > Commit-Queue: Femi Adegunloye <mrfemi@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1105828}
    >
    > Bug: b:192562912
    > Change-Id: I3310c7c0d8ba8e0e907fd1e859caec631dd350e5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303525
    > Reviewed-by: ccameron chromium <ccameron@chromium.org>
    > Commit-Queue: Femi Adegunloye <mrfemi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1112465}
    
    Bug: 1444397
    Change-Id: I0f3269775b3ac5050e448529896c162263be1e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575785
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Quick-Run: Femi Adegunloye <mrfemi@google.com>
    Commit-Queue: Femi Adegunloye <mrfemi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150995}

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -449,7 +449,7 @@ BASE_FEATURE(kWaylandCancelComposition,
 // Enables chrome color management wayland protocol for lacros.
 BASE_FEATURE(kLacrosColorManagement,
              "LacrosColorManagement",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsLacrosColorManagementEnabled() {
   return base::FeatureList::IsEnabled(kLacrosColorManagement);
