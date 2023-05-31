638453fca579f24b2d2e858af16303e58b71c3f6
Simon Zünd <szuend@chromium.org>
2023-05-31 10:32:13 +0000

    Import test runners in http/tests/devtools/sources/debugger-breakpoints
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    R=wolfi@chromium.org
    
    Bug: 1442410
    Change-Id: I2ef76ae3dbb9a2c05ef46b683d7605c6f1f9c16a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572479
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151153}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger-breakpoints/dynamic-scripts-breakpoints.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger-breakpoints/dynamic-scripts-breakpoints.js
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestRunner} from 'test_runner';
+import {SourcesTestRunner} from 'sources_test_runner';
+
 (async function() {
   TestRunner.addResult(
       `Tests that there is no exception in front-end on page reload when breakpoint is set in HTML document and some dynamic scripts are loaded before the script with the breakpoint is loaded.`);
-  await TestRunner.loadLegacyModule('sources'); await TestRunner.loadTestModule('sources_test_runner');
+  await TestRunner.loadLegacyModule('sources');
   await TestRunner.showPanel('sources');
   await TestRunner.navigatePromise(
       'resources/dynamic-scripts-breakpoints.html');
