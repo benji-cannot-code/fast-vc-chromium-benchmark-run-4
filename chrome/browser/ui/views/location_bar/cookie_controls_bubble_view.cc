ebede2a01f43610c107a800942a09b2fbe58428e
Olesia Marukhno <olesiamarukhno@google.com>
2023-05-31 16:13:25 +0000

    [user bypass] Record a pref on cookies controls bubble open
    Following crrev.com/c/4542960, record the pref when the cookie controls
    bubble is opened.
    
    Bug: 1450189
    Change-Id: I264dcb37978fa8d894cceba21b04ceade186b7de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574695
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151283}

--- chrome/browser/ui/views/location_bar/cookie_controls_bubble_view.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls_bubble_view.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
+#include "components/content_settings/core/common/pref_names.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_types.h"
@@ -63,6 +66,10 @@ void CookieControlsBubbleView::ShowBubble(
   }
 
   base::RecordAction(UserMetricsAction("CookieControls.Bubble.Opened"));
+  Profile* profile =
+      Profile::FromBrowserContext(web_contents->GetBrowserContext());
+  profile->GetPrefs()->SetBoolean(prefs::kInContextCookieControlsOpened, true);
+
   g_instance =
       new CookieControlsBubbleView(anchor_view, web_contents, controller);
   g_instance->SetHighlightedButton(highlighted_button);
