faf24c10006df6ec98a5531d187066fe46538cd3
Marijn Kruisselbrink <mek@chromium.org>
2023-05-31 19:39:57 +0000

    Reland "Add WebAppIntegration.IncompatibleAppShim test."
    This is a reland of commit 90c1ec7d9a9c64ffa363b7565178142932e51b97
    
    This relands skips the code-signing step on Mac OS 11 and older,
    as the codesign tool is not installed there, and also not needed.
    
    Original change's description:
    > Add WebAppIntegration.IncompatibleAppShim test.
    >
    > This test aims to cover the same code that the (long disabled)
    > AppShimInteractiveTest.RebuildShim test did. One differences being
    > that that test relied on differences in architecture between the app shim
    > and chrome, while this test relies on differences in the expected entry
    > point exposed by chrome. Both should result in the same app shim
    > behavior.
    >
    > Another difference is that the old test uses (deprecated) chrome apps
    > while this test uses PWAs.
    >
    > Since this is the only AppShimInteractiveTest that doesn't already
    > have equivalent tests elsewhere, this should pave the way for deleting
    > the entire test suite.
    >
    > Bug: 509774
    > Include-Ci-Only-Tests: true
    > Cq-Include-Trybots: luci.chrome.try:mac-chrome
    > Validate-Test-Flakiness: skip
    > Change-Id: Ifd4160f30d5c10b46ac8ba48b722212a3680d61a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569285
    > Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1150996}
    
    Bug: 509774
    Change-Id: I2250067204a4520ef496e853d9a27069fb5ce145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575580
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151425}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
@@ -140,6 +140,8 @@ enum class SubAppInstallDialogOptions {
   kPolicyOverride
 };
 
+enum class AppShimCorruption { kNoExecutable, kIncompatibleVersion };
+
 // These structs are used to store the current state of the world before & after
 // each state-change action.
 
@@ -326,7 +328,7 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   void UninstallPolicyApp(Site site);
   void UninstallFromOs(Site site);
 #if BUILDFLAG(IS_MAC)
-  void CorruptAppShim(Site site);
+  void CorruptAppShim(Site site, AppShimCorruption corruption);
   void QuitAppShim(Site site);
 #endif
 
