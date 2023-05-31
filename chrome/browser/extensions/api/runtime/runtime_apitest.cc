48a1eedbef42876511642fd72d6320ac1b37baae
David Bertoni <dbertoni@chromium.org>
2023-05-31 17:47:42 +0000

    [Extensions] Disable more tests that are flaky for SWs.
    These tests are very flaky in the service worker configuration.
    This CL only disables that configuration. One test was disabled for
    both configurations in a prior CL, and this CL reenables the
    background page configuration.
    
    ExtensionManagementApiTestWithBackgroundType.SelfUninstallNoPermissions
    ExtensionPreferenceApiTest.ThirdPartyCookiesAllowed
    RuntimeApiTest.ChromeRuntimeUninstallURL
    RuntimeApiTest.DoNotOpenUninstallUrlForBlocklistedExtensions
    
    Bug: 1446968
    Change-Id: I921c82a225c9e3d435680f0378230878b5581ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575416
    Reviewed-by: Tim <tjudkins@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151343}

--- chrome/browser/extensions/api/runtime/runtime_apitest.cc
+++ chrome/browser/extensions/api/runtime/runtime_apitest.cc
@@ -83,7 +83,15 @@ IN_PROC_BROWSER_TEST_P(RuntimeApiTest, ChromeRuntimeUnprivileged) {
   EXPECT_TRUE(catcher.GetNextResult()) << message_;
 }
 
-IN_PROC_BROWSER_TEST_P(RuntimeApiTest, ChromeRuntimeUninstallURL) {
+// TODO(crbug.com/1446968): The service worker version is flaky.
+using RuntimeApiBackgroundPageTest = RuntimeApiTest;
+
+INSTANTIATE_TEST_SUITE_P(EventPage,
+                         RuntimeApiBackgroundPageTest,
+                         ::testing::Values(ContextType::kPersistentBackground));
+
+IN_PROC_BROWSER_TEST_P(RuntimeApiBackgroundPageTest,
+                       ChromeRuntimeUninstallURL) {
   // Auto-confirm the uninstall dialog.
   extensions::ScopedTestDialogAutoConfirm auto_confirm(
       extensions::ScopedTestDialogAutoConfirm::ACCEPT);
@@ -493,8 +501,8 @@ IN_PROC_BROWSER_TEST_F(RuntimeAPIUpdateTest,
 // Tests that when a blocklisted extension with a set uninstall url is
 // uninstalled, its uninstall url does not open.
 // TODO(crbug.com/1446468): Flaky on multiple builds.
-IN_PROC_BROWSER_TEST_P(RuntimeApiTest,
-                       DISABLED_DoNotOpenUninstallUrlForBlocklistedExtensions) {
+IN_PROC_BROWSER_TEST_P(RuntimeApiBackgroundPageTest,
+                       DoNotOpenUninstallUrlForBlocklistedExtensions) {
   ExtensionTestMessageListener ready_listener("ready");
   // Load an extension that has set an uninstall url.
   scoped_refptr<const extensions::Extension> extension =
