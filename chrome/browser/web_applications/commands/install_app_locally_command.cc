dcd1f20887b261e65e79fc19987a2420b741fd4a
Nicola Tommasi <tommasin@chromium.org>
2023-05-31 07:51:05 +0000

    [CodeHealth]: Remove Apps.Launcher expired histogram
    As part of the Chrome code health rotation effort this CL removes the
    expired Apps.Launcher.InstallLocallyShortcutsCreated histogram and
    cleans up the code using it.
    
    OBSOLETE_HISTOGRAM[Apps.Launcher.InstallLocallyShortcutsCreated]=
    expired histogram.
    
    Bug: 1448130
    Change-Id: I382010534a7407eea3d7e8c64babb89af31acfa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571019
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151106}

--- chrome/browser/web_applications/commands/install_app_locally_command.cc
+++ chrome/browser/web_applications/commands/install_app_locally_command.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/browser/web_applications/commands/web_app_command.h"
@@ -107,10 +106,6 @@ void InstallAppLocallyCommand::StartWithLock(
 
 void InstallAppLocallyCommand::OnOsHooksInstalled(
     const OsHooksErrors os_hooks_errors) {
-  // TODO(dmurph): Once installation takes the OsHooksErrors bitfield, then
-  // use that to compare with the results, and record if they all were
-  // successful, instead of just shortcuts.
-  bool error = os_hooks_errors[web_app::OsHookType::kShortcuts];
   const base::Time& install_time = base::Time::Now();
   {
     // Updating install time on app.
@@ -121,8 +116,6 @@ void InstallAppLocallyCommand::OnOsHooksInstalled(
     }
   }
 
-  base::UmaHistogramBoolean("Apps.Launcher.InstallLocallyShortcutsCreated",
-                            !error);
   app_lock_->install_manager().NotifyWebAppInstalledWithOsHooks(app_id_);
   app_lock_->registrar().NotifyWebAppInstallTimeChanged(app_id_, install_time);
   debug_log_.Set("command_result", "success");
