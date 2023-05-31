204c1674ef1553d533ed24630fd667c871ee4bbe
Daniel Rubery <drubery@chromium.org>
2023-05-31 00:29:01 +0000

    Flag-guard updated deep scanning prompt
    The updated deep scanning UX was not completed in one milestone, and
    therefore should be behind a flag until it can be completed.
    
    This CL adds flag guarding to https://crrev.com/c/4519953
    
    Bug: 1424527
    Change-Id: Ie99b675b994f73ee394b2e3f5c0b517a926b9713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575174
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150979}

--- chrome/browser/download/download_item_model_unittest.cc
+++ chrome/browser/download/download_item_model_unittest.cc
@@ -608,7 +608,7 @@ TEST_F(DownloadItemModelTest, CompletedBubbleWarningStatusText) {
       {download::DOWNLOAD_DANGER_TYPE_SENSITIVE_CONTENT_BLOCK,
        "Blocked by your organization"},
       {download::DOWNLOAD_DANGER_TYPE_PROMPT_FOR_SCANNING,
-       "Scan for malware \xE2\x80\xA2 Suspicious"},
+       "Scan before opening"},
   };
   for (const auto& test_case : kDangerTypeTestCases) {
     SetupDownloadItemDefaults();
@@ -746,7 +746,7 @@ TEST_F(DownloadItemModelTest, DangerousWarningBubbleUIInfo_V2On) {
        {DownloadCommands::Command::DISCARD, DownloadCommands::Command::KEEP}},
       {download::DOWNLOAD_DANGER_TYPE_PROMPT_FOR_SCANNING,
        false,
-       absl::nullopt,
+       DownloadCommands::Command::DEEP_SCAN,
        {DownloadCommands::Command::DEEP_SCAN,
         DownloadCommands::Command::BYPASS_DEEP_SCANNING}},
       {download::DOWNLOAD_DANGER_TYPE_ASYNC_SCANNING,
@@ -814,7 +814,7 @@ TEST_F(DownloadItemModelTest, DangerousWarningBubbleUIInfo_V2Off) {
        {DownloadCommands::Command::DISCARD, DownloadCommands::Command::KEEP}},
       {download::DOWNLOAD_DANGER_TYPE_PROMPT_FOR_SCANNING,
        false,
-       absl::nullopt,
+       DownloadCommands::Command::DEEP_SCAN,
        {DownloadCommands::Command::DEEP_SCAN,
         DownloadCommands::Command::BYPASS_DEEP_SCANNING}},
       {download::DOWNLOAD_DANGER_TYPE_ASYNC_SCANNING,
