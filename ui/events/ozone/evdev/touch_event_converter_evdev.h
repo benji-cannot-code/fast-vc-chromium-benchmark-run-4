57f6454688efb29cd65773923aae09ef51adb490
Jing Wang <jiwan@chromium.org>
2023-05-31 00:17:32 +0000

    ozone: detect repeated touches and record it
    This metrics serves as an indication of false positves in our palm
    rejection. Although it may not be an accurate estimation, it can be
    used to compare different approaches.
    
    Design doc: go/palm-rejection-metrics
    
    Bug: b:283019262
    Change-Id: I23dfc90cfb94a4252b6916bbb3414c01d496b9d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4551423
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1150972}

--- ui/events/ozone/evdev/touch_event_converter_evdev.h
+++ ui/events/ozone/evdev/touch_event_converter_evdev.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <ostream>
 #include <queue>
+#include <set>
 // See if we compile against new enough headers and add missing definition
 // if the headers are too old.
 #include "base/memory/raw_ptr.h"
@@ -100,12 +101,35 @@ class COMPONENT_EXPORT(EVDEV) TouchEventConverterEvdev
   static const char kHoldCountAtCancelEventName[];
   static const char kPalmFilterTimerEventName[];
   static const char kPalmTouchCountEventName[];
+  static const char kRepeatedTouchCountEventName[];
   static const char kTouchSessionCountEventName[];
   static const char kTouchSessionLengthEventName[];
   static const char kStylusSessionCountEventName[];
   static const char kStylusSessionLengthEventName[];
 
  private:
+  struct CancelledTouch {
+    const base::TimeTicks cancel_timestamp;
+    const float start_x, start_y;
+
+    CancelledTouch(base::TimeTicks cancel_timestamp,
+                   float start_x,
+                   float start_y)
+        : cancel_timestamp(cancel_timestamp),
+          start_x(start_x),
+          start_y(start_y) {}
+
+    bool operator==(const CancelledTouch& other) const {
+      return cancel_timestamp == other.cancel_timestamp &&
+             start_x == other.start_x && start_y == other.start_y;
+    }
+
+    bool operator<(const CancelledTouch& other) const {
+      return std::tie(cancel_timestamp, start_x, start_y) <
+             std::tie(other.cancel_timestamp, other.start_x, other.start_y);
+    }
+  };
+
   friend class MockTouchEventConverterEvdev;
 
   // Overidden from base::MessagePumpLibevent::FdWatcher.
@@ -145,6 +169,8 @@ class COMPONENT_EXPORT(EVDEV) TouchEventConverterEvdev
   void UpdateRadiusFromTouchWithOrientation(InProgressTouchEvdev* event) const;
 
   int NextTrackingId();
+
+  void DetectRepeatedTouch(base::TimeTicks timestamp);
 
   void RecordMetrics(base::TimeTicks timestamp);
 
@@ -260,6 +286,10 @@ class COMPONENT_EXPORT(EVDEV) TouchEventConverterEvdev
 
   // Whether the last touch was detected as palm.
   bool last_touch_is_palm_ = false;
+
+  // Stores recently-canceled touches, which are used to detect repeated
+  // touches.
+  std::set<CancelledTouch> cancelled_touches_;
 
   // A delay timer starts whenever a touch is reported and stops after 5s. If a
   // new touch is reported during the 5s, the timer will be reset and restarted.
