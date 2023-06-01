cd23b11799d14a367b46b7273acabd808b19a786
manukh <manukh@chromium.org>
2023-06-01 18:04:22 +0000

    [memories][journeys] Cleanup hide visits.
    Launched in m114 4/3/23 crrev.com/c/4382731.
    
    m114 reached stable 5/30. So we can clean up the feature code now.
    
    Bug: b/260755715
    Change-Id: I8eb4d49ca1f12099e3361f49392800abd498ebe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4579420
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151981}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1063,9 +1063,6 @@ extern const char kJourneysDescription[];
 extern const char kJourneysContentClusteringName[];
 extern const char kJourneysContentClusteringDescription[];
 
-extern const char kJourneysHideVisitsName[];
-extern const char kJourneysHideVisitsDescription[];
-
 extern const char kJourneysImagesName[];
 extern const char kJourneysImagesDescription[];
 
