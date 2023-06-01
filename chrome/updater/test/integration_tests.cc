f35b66f2d5992927df16c2a551976dc44f4b655d
Xiaoling Bao <xiaolingbao@chromium.org>
2023-06-01 20:10:31 +0000

    No need to install/uninstall test service, it's done on-demand.
    Also removed a TODO in rpc_handler.py since we are already on python3.
    
    Bug: 1233612
    Change-Id: I6d83316a5fdd66c5780490aab4769530cc866950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4581311
    Auto-Submit: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152083}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -101,8 +101,6 @@ class IntegrationTest : public ::testing::Test {
     ASSERT_TRUE(WaitForUpdaterExit());
     ASSERT_NO_FATAL_FAILURE(Clean());
     ASSERT_NO_FATAL_FAILURE(ExpectClean());
-    // TODO(crbug.com/1233612) - reenable the code when system tests pass.
-    // SetUpTestService();
     ASSERT_NO_FATAL_FAILURE(EnterTestMode(GURL("http://localhost:1234"),
                                           GURL("http://localhost:1235"),
                                           GURL("http://localhost:1236")));
@@ -139,9 +137,6 @@ class IntegrationTest : public ::testing::Test {
     CopyLog();
 
     DMCleanup();
-
-    // TODO(crbug.com/1233612) - reenable the code when system tests pass.
-    // TearDownTestService();
 
     // Updater process must not be running for `Clean()` to succeed.
     ASSERT_TRUE(WaitForUpdaterExit());
@@ -338,18 +333,6 @@ class IntegrationTest : public ::testing::Test {
 
   [[nodiscard]] bool WaitForUpdaterExit() {
     return test_commands_->WaitForUpdaterExit();
-  }
-
-  void SetUpTestService() {
-#if BUILDFLAG(IS_WIN)
-    test_commands_->SetUpTestService();
-#endif  // BUILDFLAG(IS_WIN)
-  }
-
-  void TearDownTestService() {
-#if BUILDFLAG(IS_WIN)
-    test_commands_->TearDownTestService();
-#endif  // BUILDFLAG(IS_WIN)
   }
 
   void ExpectUpdateCheckSequence(ScopedServer* test_server,
