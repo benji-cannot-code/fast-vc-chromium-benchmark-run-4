c3b63167b35c13e1d6a871e5974603d0c3322679
thefrog <thefrog@chromium.org>
2023-06-01 13:39:12 +0000

    Explicitly disable hash real-time lookups for guest mode
    They were already disabled by WithGuest having no profile selection for
    OTR, but this CL does so more explicitly. It also adds tests to confirm
    the existing functionality.
    
    Bug: 1441654
    Change-Id: I832578c5737a1ee1f5f477bee9ffdc1bbcf4cda2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577038
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151819}

--- chrome/browser/safe_browsing/ohttp_key_service_factory.cc
+++ chrome/browser/safe_browsing/ohttp_key_service_factory.cc
@@ -34,7 +34,6 @@ OhttpKeyServiceFactory::OhttpKeyServiceFactory()
           "SafeBrowsingOhttpKeyService",
           ProfileSelections::Builder()
               .WithRegular(ProfileSelection::kOriginalOnly)
-              .WithGuest(ProfileSelection::kOriginalOnly)
               .Build()) {
   DependsOn(NetworkContextServiceFactory::GetInstance());
 }
