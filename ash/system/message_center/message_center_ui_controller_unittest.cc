be72ce8a4d174718be4b0ee7fd5a110e30786fef
James Cook <jamescook@chromium.org>
2023-06-01 02:54:33 +0000

    chromeos: Delete MessageCenterUiController::HidePopupBubble()
    It is only called in tests.
    
    Bug: none
    Test: ash_unittests
    Change-Id: I313cf8f5039a8512d47ce5ace9580c37fac727d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575093
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151641}

--- ash/system/message_center/message_center_ui_controller_unittest.cc
+++ ash/system/message_center/message_center_ui_controller_unittest.cc
@@ -159,11 +159,6 @@ TEST_F(MessageCenterUiControllerTest, BasicPopup) {
 
   ASSERT_TRUE(ui_controller_->popups_visible());
   ASSERT_FALSE(ui_controller_->message_center_visible());
-
-  ui_controller_->HidePopupBubble();
-
-  ASSERT_FALSE(ui_controller_->popups_visible());
-  ASSERT_FALSE(ui_controller_->message_center_visible());
 }
 
 TEST_F(MessageCenterUiControllerTest, MessageCenterClosesPopups) {
@@ -228,11 +223,6 @@ TEST_F(MessageCenterUiControllerTest, ShowBubbleFails) {
   ASSERT_FALSE(ui_controller_->message_center_visible());
 
   ui_controller_->ShowMessageCenterBubble();
-
-  ASSERT_FALSE(ui_controller_->popups_visible());
-  ASSERT_FALSE(ui_controller_->message_center_visible());
-
-  ui_controller_->HidePopupBubble();
 
   ASSERT_FALSE(ui_controller_->popups_visible());
   ASSERT_FALSE(ui_controller_->message_center_visible());
