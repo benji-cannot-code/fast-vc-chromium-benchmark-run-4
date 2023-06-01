2735ae65919b4df2797c7162e0d6e8ee07d50c3b
jhtin <jhtin@chromium.org>
2023-06-01 01:57:43 +0000

    vietnamese input method: add default on settings for flexible input
    Bug: b/282866641
    
    
    Change-Id: Ib01e8359102e896724611e1233bc02534e8ccc63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570211
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151619}

--- chrome/browser/ash/input_method/input_method_settings.cc
+++ chrome/browser/ash/input_method/input_method_settings.cc
@@ -167,6 +167,7 @@ bool IsVietnameseVniEngine(base::StringPiece engine_id) {
 mojom::VietnameseVniSettingsPtr CreateVietnameseVniSettings(
     const base::Value::Dict& input_method_specific_pref) {
   auto settings = mojom::VietnameseVniSettings::New();
+  settings->allow_flexible_diacritics = true;
   settings->show_underline_for_composition_text = true;
   return settings;
 }
@@ -174,6 +175,7 @@ mojom::VietnameseVniSettingsPtr CreateVietnameseVniSettings(
 mojom::VietnameseTelexSettingsPtr CreateVietnameseTelexSettings(
     const base::Value::Dict& input_method_specific_pref) {
   auto settings = mojom::VietnameseTelexSettings::New();
+  settings->allow_flexible_diacritics = true;
   settings->show_underline_for_composition_text = true;
   return settings;
 }
