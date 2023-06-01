4fe5e84759b8e00891c4286a7f5bfa00aea37521
Christian Xu <christianxu@google.com>
2023-06-01 16:52:06 +0000

    [iOS][Toolbar] Remove secondaryToolbarCoordinator refs from BVC
    Remove checks of SecondaryToolbarCoordinator as it's always initialized:
    https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/browser_view/browser_coordinator.mm;l=769;drc=4764a9af525eb819410d7aa129021578a8bf799f
    
    and started:
    https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/browser_view/browser_view_controller.mm;l=1421;drc=24de59c47af50ef975186f740c578f5ed39fea97
    
    Bug: 1449960
    Change-Id: I3e25009c69685bf59e16b01f1e55f62d38bc7638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575205
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151931}

--- ios/chrome/browser/ui/toolbar/toolbar_coordinator.h
+++ ios/chrome/browser/ui/toolbar/toolbar_coordinator.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class PrimaryToolbarCoordinator;
 @class PrimaryToolbarViewController;
 @protocol SharingPositioner;
-@class SecondaryToolbarCoordinator;
 @class SecondaryToolbarViewController;
 @protocol SideSwipeToolbarSnapshotProviding;
 @protocol ViewRevealingAnimatee;
@@ -42,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /// Returns `primaryToolbarCoordinator`.
 - (PrimaryToolbarCoordinator*)primaryToolbarCoordinator;
-/// Returns `secondaryToolbarCoordinator`.
-- (SecondaryToolbarCoordinator*)secondaryToolbarCoordinator;
 
 /// Returns `primaryToolbarViewController`.
 - (UIViewController*)primaryToolbarViewController;
