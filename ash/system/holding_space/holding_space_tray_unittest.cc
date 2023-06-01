3e1d819594b9b50c229403e5dd5475620de82891
Jeroen Dhollander <jeroendh@google.com>
2023-06-01 13:59:31 +0000

    [base][test] Add a `RunUntil(<predicate>)` utility
    It's very common that a test has to wait until a certain condition is
    fulfilled. Often, the test knows this is the case by installing an
    observer or callback.
    
    However, quite often the code under test doesn't have an observer or
    callback to report it is done, since it doesn't need one. This makes
    testing this code a challenge.
    
    This CL adds a utility that can be used in these cases:
    
       // Example usage
       ChangeColorFieldAsyncTo(object_under_test, kRed);
    
       ASSERT_TRUE(RunUntil(
             base::BindLambdaForTesting([&](){
               return object_under_test.Color() == kRed;
             })));
    
       // When we come here we are guaranteed that
       // `object_under_test.Color()` is set to `kRed`.
    
    This utility will poll periodically until the predicate returns `true`
    (or until we hit a timeout).
    
    Change-Id: I4f4f49fb06362cd1a5d8969fb0a3d50bb5a58ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370081
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151826}

--- ash/system/holding_space/holding_space_tray_unittest.cc
+++ ash/system/holding_space/holding_space_tray_unittest.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/branding_buildflags.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -78,6 +79,7 @@ namespace ash {
 
 namespace {
 
+using base::test::RunUntil;
 using testing::_;
 using testing::ElementsAre;
 
@@ -254,32 +256,6 @@ const views::MenuItemView* GetMenuItemByCommandId(HoldingSpaceCommandId id) {
   }
   return nullptr;
 }
-
-// PredicateWaiter -------------------------------------------------------------
-
-// A class capable of waiting until a predicate returns true.
-class PredicateWaiter {
- public:
-  PredicateWaiter() = default;
-  PredicateWaiter(const PredicateWaiter&) = delete;
-  PredicateWaiter& operator=(const PredicateWaiter&) = delete;
-  ~PredicateWaiter() = default;
-
-  void WaitUntil(base::RepeatingCallback<bool()> predicate,
-                 base::TimeDelta polling_interval = base::Milliseconds(100)) {
-    DCHECK(polling_interval.is_positive());
-    if (predicate.Run())
-      return;
-    base::RunLoop run_loop;
-    base::RepeatingTimer scheduler;
-    scheduler.Start(FROM_HERE, polling_interval,
-                    base::BindLambdaForTesting([&]() {
-                      if (predicate.Run())
-                        run_loop.Quit();
-                    }));
-    run_loop.Run();
-  }
-};
 
 // ViewVisibilityChangedWaiter -------------------------------------------------
 
@@ -3268,7 +3244,7 @@ TEST_P(HoldingSpaceTrayDownloadsSectionTest,
 
   // Wait until the `progress_indicator` is synced with the model, which happens
   // asynchronously in response to compositor scheduling.
-  PredicateWaiter().WaitUntil(base::BindLambdaForTesting([&]() {
+  ASSERT_TRUE(RunUntil([&]() {
     return progress_indicator->progress() ==
            ProgressIndicator::kProgressComplete;
   }));
@@ -3285,8 +3261,8 @@ TEST_P(HoldingSpaceTrayDownloadsSectionTest,
   // Wait until the `progress_indicator` is synced with the model. Note that
   // this happens asynchronously since the `progress_indicator` does so in
   // response to compositor scheduling.
-  PredicateWaiter().WaitUntil(base::BindLambdaForTesting(
-      [&]() { return progress_indicator->progress() == 0.f; }));
+  ASSERT_TRUE(
+      RunUntil([&]() { return progress_indicator->progress() == 0.f; }));
 
   // The `default_tray_icon` should not be visible so as to avoid overlap with
   // the `progress_indicator`'s inner icon while in progress.
@@ -3298,7 +3274,7 @@ TEST_P(HoldingSpaceTrayDownloadsSectionTest,
 
   // Wait until the `progress_indicator` is synced with the model, which happens
   // asynchronously in response to compositor scheduling.
-  PredicateWaiter().WaitUntil(base::BindLambdaForTesting([&]() {
+  ASSERT_TRUE(RunUntil([&]() {
     return progress_indicator->progress() ==
            ProgressIndicator::kProgressComplete;
   }));
@@ -3340,8 +3316,8 @@ TEST_P(HoldingSpaceTrayDownloadsSectionTest,
 
   // Wait until the `progress_indicator` is synced with the model, which happens
   // asynchronously in response to compositor scheduling.
-  PredicateWaiter().WaitUntil(base::BindLambdaForTesting(
-      [&]() { return progress_indicator->progress() == 0.f; }));
+  ASSERT_TRUE(
+      RunUntil([&]() { return progress_indicator->progress() == 0.f; }));
 
   // Verify image opacity/transform.
   EXPECT_EQ(image->GetTargetOpacity(), 0.f);
@@ -3354,7 +3330,7 @@ TEST_P(HoldingSpaceTrayDownloadsSectionTest,
 
   // Wait until the `progress_indicator` is synced with the model, which happens
   // asynchronously in response to compositor scheduling.
-  PredicateWaiter().WaitUntil(base::BindLambdaForTesting([&]() {
+  ASSERT_TRUE(RunUntil([&]() {
     return progress_indicator->progress() ==
            ProgressIndicator::kProgressComplete;
   }));
@@ -4012,7 +3988,7 @@ TEST_P(HoldingSpaceTrayPrimaryAndSecondaryActionsTest, HasExpectedActions) {
   } else {
     // For screen capture items, the holding space image should always be shown.
     EXPECT_TRUE(IsShowingImage(item_views.front()));
-  };
+  }
 
   // Right click the item view to show the context menu.
   RightClick(item_views.front());
