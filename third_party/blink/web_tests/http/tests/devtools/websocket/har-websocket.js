3463700c5341c153c93f19851ef6ffa44337f5f8
Simon Zünd <szuend@chromium.org>
2023-06-01 09:17:13 +0000

    [devtools] Import test runners in http/tests/devtools/websocket
    Also updates tests in http/tests/devtools/webaudio.
    
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    R=dsv@chromium.org
    
    Bug: 1442410
    Change-Id: If1b6e5a09d104a81fc9b2192037d27b242b8ce92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575309
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151742}

--- third_party/blink/web_tests/http/tests/devtools/websocket/har-websocket.js
+++ third_party/blink/web_tests/http/tests/devtools/websocket/har-websocket.js
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestRunner} from 'test_runner';
+import {NetworkTestRunner} from 'network_test_runner';
+
 (async function() {
   TestRunner.addResult('Verifies that HAR exports contain websocket messages');
-  await TestRunner.loadTestModule('network_test_runner');
   await TestRunner.showPanel('network');
   await TestRunner.NetworkAgent.setCacheDisabled(true);
 
