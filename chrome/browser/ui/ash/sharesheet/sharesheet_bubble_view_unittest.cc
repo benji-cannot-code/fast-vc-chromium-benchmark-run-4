07cc09596ab7703fd25fa6cd0dbb5d0f4078c0da
Melissa Zhang <melzhang@google.com>
2023-06-01 17:20:13 +0000

    [Sharesheet] Remove empty state code.
    This CL removes empty state code because copy to clipboard will
    always be visible now that copy to clipboard works with files from
    MTP.
    
    Bug: 1188938
    Change-Id: Ic884f1495ce5c331611ba2d9c29dc6371e18f822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574544
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151950}

--- chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view_unittest.cc
+++ chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view_unittest.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/events/base_event_utils.h"
-#include "ui/lottie/resource.h"
 #include "ui/views/controls/native/native_view_host.h"
 #include "ui/views/test/widget_test.h"
 #include "ui/views/view.h"
@@ -95,10 +94,6 @@ class SharesheetBubbleViewTest : public ChromeAshTestBase {
     widget->GetContentsView()->AddChildView(content_view);
 
     parent_window_ = widget->GetNativeWindow();
-
-    ui::ResourceBundle::SetLottieParsingFunctions(
-        &lottie::ParseLottieAsStillImage,
-        &lottie::ParseLottieAsThemedStillImage);
   }
 
   void ShowAndVerifyBubble(apps::IntentPtr intent,
@@ -178,23 +173,6 @@ TEST_F(SharesheetBubbleViewTest, BubbleDoesOpenAndClose) {
   CloseBubble();
 }
 
-TEST_F(SharesheetBubbleViewTest, EmptyState) {
-  ShowAndVerifyBubble(::sharesheet::CreateInvalidIntent(),
-                      ::sharesheet::LaunchSource::kUnknown);
-
-  // Header should contain Share label.
-  ASSERT_TRUE(header_view()->GetVisible());
-  ASSERT_EQ(header_view()->children().size(), 1u);
-
-  // Body view should contain 3 children, an image and 2 labels.
-  ASSERT_TRUE(body_view()->GetVisible());
-  ASSERT_EQ(body_view()->children().size(), 3u);
-
-  // Footer should be an empty view that just acts as padding.
-  ASSERT_TRUE(footer_view()->GetVisible());
-  ASSERT_EQ(footer_view()->children().size(), 0u);
-}
-
 TEST_F(SharesheetBubbleViewTest, RecordLaunchSource) {
   base::HistogramTester histograms;
 
@@ -226,17 +204,6 @@ TEST_F(SharesheetBubbleViewTest, RecordShareActionCount) {
 
   // Drive intent should show drive and copy actions.
   ShowAndVerifyBubble(::sharesheet::CreateDriveIntent(),
-                      ::sharesheet::LaunchSource::kUnknown);
-  CloseBubble();
-  histograms.ExpectBucketCount(
-      ::sharesheet::kSharesheetShareActionResultHistogram,
-      ::sharesheet::SharesheetMetrics::UserAction::kDriveAction, 1);
-  histograms.ExpectBucketCount(
-      ::sharesheet::kSharesheetShareActionResultHistogram,
-      ::sharesheet::SharesheetMetrics::UserAction::kCopyAction, 2);
-
-  // Invalid intent should not show any actions.
-  ShowAndVerifyBubble(::sharesheet::CreateInvalidIntent(),
                       ::sharesheet::LaunchSource::kUnknown);
   CloseBubble();
   histograms.ExpectBucketCount(
