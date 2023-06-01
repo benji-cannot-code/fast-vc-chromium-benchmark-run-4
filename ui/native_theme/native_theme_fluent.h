9bfdf3cc583873e64a2cd63fab06e637de82aee2
Gaston Rodriguez Lopez <gastonr@microsoft.com>
2023-06-01 00:13:19 +0000

    Implement high contrast borders for fluent scrollbars.
    Adding an outline for fluent scrollbars in high contrast mode improves
    visibility of the scrollbar's edges.
    
    Bug: 1430477
    Change-Id: I4c9e36d9dbd5e1d1d9a5e29e96b7288e865de238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569655
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1151571}

--- ui/native_theme/native_theme_fluent.h
+++ ui/native_theme/native_theme_fluent.h
@@ -66,6 +66,7 @@ class NATIVE_THEME_EXPORT NativeThemeFluent : public NativeThemeBase {
   void PaintButton(cc::PaintCanvas* canvas,
                    const ColorProvider* color_provider,
                    const gfx::Rect& rect,
+                   Part direction,
                    ColorScheme color_scheme) const;
   void PaintArrow(cc::PaintCanvas* canvas,
                   const ColorProvider* color_provider,
