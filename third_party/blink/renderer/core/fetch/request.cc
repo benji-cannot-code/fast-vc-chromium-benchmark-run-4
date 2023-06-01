0a8219a2901d7f6521bcb749e646ce09f697c63a
Nidhi Jaju <nidhijaju@chromium.org>
2023-06-01 14:04:08 +0000

    Fetch: Cleanup use of HasPendingActivity()
    Body::HasPendingActivity(), Request::HasPendingActivity(), and
    Response::HasPendingActivity() always return false, so they are not
    useful. This CL removes these functions.
    
    Bug: 1243329
    Change-Id: I076e61e1c6eaa25368c4ad41b09759c0e52ecd48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573177
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151828}

--- third_party/blink/renderer/core/fetch/request.cc
+++ third_party/blink/renderer/core/fetch/request.cc
@@ -900,8 +900,7 @@ Request::Request(ScriptState* script_state,
                  FetchRequestData* request,
                  Headers* headers,
                  AbortSignal* signal)
-    : ActiveScriptWrappable<Request>({}),
-      Body(ExecutionContext::From(script_state)),
+    : Body(ExecutionContext::From(script_state)),
       request_(request),
       headers_(headers),
       signal_(signal) {}
@@ -1151,7 +1150,6 @@ network::mojom::RequestMode Request::GetRequestMode() const {
 
 void Request::Trace(Visitor* visitor) const {
   ScriptWrappable::Trace(visitor);
-  ActiveScriptWrappable<Request>::Trace(visitor);
   Body::Trace(visitor);
   visitor->Trace(request_);
   visitor->Trace(headers_);
