ed7762689d769a79ab4242d5d6a05f33f163d3b3
Simon Zünd <szuend@chromium.org>
2023-06-01 07:30:03 +0000

    [devtools] Import test runners in http/tests/devtools/unit
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    R=alexrudenko@chromium.org
    
    Bug: 1442410
    Change-Id: I672629498af2727b1d4847e90d82eeb3470ebe86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577063
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151693}

--- third_party/blink/web_tests/http/tests/devtools/unit/list-control-various-height.js
+++ third_party/blink/web_tests/http/tests/devtools/unit/list-control-various-height.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+import {TestRunner} from 'test_runner';
 (async function() {
   TestRunner.addResult('Test ListControl rendering for various height items case.');
 
--- third_party/blink/web_tests/http/tests/devtools/unit/test-failure.js
+++ third_party/blink/web_tests/http/tests/devtools/unit/test-failure.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+import {TestRunner} from 'test_runner';
 (async function() {
   TestRunner.addResult("Tests that a test will properly exit if it has an asynchronous error.");
   setTimeout(_ => { throw {stack: "This error is expected"} }, 0);
