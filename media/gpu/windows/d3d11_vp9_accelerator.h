ce95447d204ad8ba45460a3a78c10b521bd6e670
Zhibo Wang <zhibo1.wang@intel.com>
2023-06-01 03:59:02 +0000

    Complete migration to media::D3DVideoDecoderWrapper
    Migrate H265 and AV1 accelerator to the new wrapper and remove the old one.
    
    Bug: 1348104
    Change-Id: Idd98e0da4935b995a0222eb365c4a6c33e1426d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4331451
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Zhibo Wang <zhibo1.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1151662}

--- media/gpu/windows/d3d11_vp9_accelerator.h
+++ media/gpu/windows/d3d11_vp9_accelerator.h
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vp9_decoder.h"
 #include "media/gpu/windows/d3d11_com_defs.h"
 #include "media/gpu/windows/d3d11_status.h"
-#include "media/gpu/windows/d3d11_video_context_wrapper.h"
 #include "media/gpu/windows/d3d11_video_decoder_client.h"
+#include "media/gpu/windows/d3d11_video_decoder_wrapper.h"
 #include "media/gpu/windows/d3d11_vp9_picture.h"
 #include "media/gpu/windows/d3d_accelerator.h"
 
@@ -29,8 +29,7 @@ class D3D11VP9Accelerator : public D3DAccelerator,
  public:
   D3D11VP9Accelerator(D3D11VideoDecoderClient* client,
                       MediaLog* media_log,
-                      ComD3D11VideoDevice video_device,
-                      std::unique_ptr<VideoContextWrapper> video_context);
+                      ComD3D11VideoDevice video_device);
 
   D3D11VP9Accelerator(const D3D11VP9Accelerator&) = delete;
   D3D11VP9Accelerator& operator=(const D3D11VP9Accelerator&) = delete;
