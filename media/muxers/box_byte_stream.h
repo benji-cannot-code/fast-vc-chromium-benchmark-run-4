233e37053ae40cdebfc2c6f6f99872b9cb096e43
Sunggook Chue <sunggch@microsoft.com>
2023-06-01 22:34:02 +0000

    MP4 muxer - moof box and its children, and mdat box.
    It adds moof box and its children of mfhd, traf, tfhd,
    hfdt, trun, and mdat box.
    
    1. It uses default-base-is-moof that is a enforced flag from
    web isobmff, which leads to deletion of moof offset set/get
    APIs from the context.
    
    2. It also uses BoxByteStream for mdat offset, which leads
    to deletion of mdat offset set/get APIs from the context.
    
    3. It adds WriteAndFlush API to Mp4BoxWriter that accept
    BoxByteStream as a parameter to support multiple root boxes
    write with single BoxByteStream object. It is required
    for 'trun' box's data_offset refer to another root box
    of 'mdat' box.
    
    4. It remove WriteU24 API from the BoxByteStream because
    it is only used once, and the current implementation does
    not support big endian format. Removing and write U32 with
    bit operations seems more efficient than wrting big endian
    format (WriteBigEndian).
    
    5. This CL is the last of box writer.
    
    Bug: 1072056
    Change-Id: I90dd7c1c3fbcd3a9fa9b5d869bf9550122465e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575451
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1152180}

--- media/muxers/box_byte_stream.h
+++ media/muxers/box_byte_stream.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_MUXERS_BOX_BYTE_STREAM_H_
 #define MEDIA_MUXERS_BOX_BYTE_STREAM_H_
 
+#include <queue>
 #include <vector>
 
 #include "base/big_endian.h"
@@ -41,7 +42,6 @@ class MEDIA_EXPORT BoxByteStream {
   // type.
   void WriteU8(uint8_t value);
   void WriteU16(uint16_t value);
-  void WriteU24(uint32_t value);
   void WriteU32(uint32_t value);
   void WriteU64(uint64_t value);
   void WriteBytes(const void* buf, size_t len);
@@ -55,6 +55,19 @@ class MEDIA_EXPORT BoxByteStream {
   // is a summation of the box itself with its children.
   void EndBox();
 
+  // Write placeholder for the track data offset of the `trun` box. The data
+  // is stored in the `mdat` box so its value will be written during `mdat'
+  // box `Write` time.
+  void WriteOffsetPlaceholder();
+
+  // Populate the placeholder, which was set by `WriteOffsetPlaceholder`
+  // with the current offset. The current offset is a data offset only
+  // when the `BoxByteStream` is created with `moof` box.
+  void FlushCurrentOffset();
+
+  // Validates whether there is a open box or not.
+  bool has_open_boxes() const { return !size_offsets_.empty(); }
+
   // TODO(crbug.com/1072056): Investigate if this is a reasonable starting size.
   static constexpr int kDefaultBufferLimit = 4096;
 
@@ -66,6 +79,8 @@ class MEDIA_EXPORT BoxByteStream {
   void GrowWriter();
 
   std::vector<size_t> size_offsets_;
+  std::queue<size_t> data_offsets_by_track_;
+
   size_t position_ = 0;
   std::vector<uint8_t> buffer_;
   absl::optional<base::BigEndianWriter> writer_;
--- media/muxers/mp4_movie_box_writer.cc
+++ media/muxers/mp4_movie_box_writer.cc
@@ -174,7 +174,7 @@ Mp4MovieTrackExtendsBoxWriter::~Mp4MovieTrackExtendsBoxWriter() = default;
 void Mp4MovieTrackExtendsBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_TREX);
+  writer.StartFullBox(mp4::FOURCC_TREX, /*flags=*/0, /*version=*/0);
 
   writer.WriteU32(box_.track_id);
   writer.WriteU32(box_.default_sample_description_index);
@@ -222,7 +222,7 @@ Mp4MovieTrackHeaderBoxWriter::~Mp4MovieTrackHeaderBoxWriter() = default;
 void Mp4MovieTrackHeaderBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_TKHD, box_.flags, /*version*/ 1);
+  writer.StartFullBox(mp4::FOURCC_TKHD, box_.flags);
 
   WriteIsoTime(writer, box_.creation_time);
   WriteIsoTime(writer, box_.modification_time);
@@ -319,7 +319,7 @@ Mp4MovieMediaHandlerBoxWriter::~Mp4MovieMediaHandlerBoxWriter() = default;
 void Mp4MovieMediaHandlerBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_HDLR);
+  writer.StartFullBox(mp4::FOURCC_HDLR, /*flags=*/0, /*version=*/0);
 
   writer.WriteU32(0);  // predefined = 0;
   writer.WriteU32(box_.handler_type);
@@ -380,7 +380,7 @@ Mp4MovieVideoHeaderBoxWriter::~Mp4MovieVideoHeaderBoxWriter() = default;
 void Mp4MovieVideoHeaderBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_VMHD);
+  writer.StartFullBox(mp4::FOURCC_VMHD, /*flags=*/1, /*version=*/0);
 
   writer.WriteU16(0);  // graphics_mode.
   writer.WriteU16(0);  // op_color[0].
@@ -402,7 +402,7 @@ Mp4MovieSoundHeaderBoxWriter::~Mp4MovieSoundHeaderBoxWriter() = default;
 void Mp4MovieSoundHeaderBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_SMHD);
+  writer.StartFullBox(mp4::FOURCC_SMHD, /*flags=*/0, /*version=*/0);
 
   writer.WriteU16(0);  // balance.
   writer.WriteU16(0);  // reserved.
@@ -448,7 +448,7 @@ Mp4MovieDataReferenceBoxWriter::~Mp4MovieDataReferenceBoxWriter() = default;
 void Mp4MovieDataReferenceBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_DREF);
+  writer.StartFullBox(mp4::FOURCC_DREF, /*flags=*/0, /*version=*/0);
 
   writer.WriteU32(box_.entries.size());
 
@@ -469,7 +469,7 @@ Mp4MovieDataUrlEntryBoxWriter::~Mp4MovieDataUrlEntryBoxWriter() = default;
 void Mp4MovieDataUrlEntryBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_URL);
+  writer.StartFullBox(mp4::FOURCC_URL, /*flags=*/0, /*version=*/0);
 
   // We use empty Url location to prevent accidental PII leak.
   writer.WriteString("");
@@ -515,7 +515,7 @@ Mp4MovieSampleToChunkBoxWriter::~Mp4MovieSampleToChunkBoxWriter() = default;
 void Mp4MovieSampleToChunkBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_STSC);
+  writer.StartFullBox(mp4::FOURCC_STSC, /*flags=*/0, /*version=*/0);
 
   writer.WriteU32(0);  // entry_count.
 
@@ -535,7 +535,7 @@ Mp4MovieDecodingTimeToSampleBoxWriter::
 void Mp4MovieDecodingTimeToSampleBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_STTS);
+  writer.StartFullBox(mp4::FOURCC_STTS, /*flags=*/0, /*version=*/0);
 
   writer.WriteU32(0);  // entry_count.
 
@@ -575,7 +575,7 @@ Mp4MovieSampleChunkOffsetBoxWriter::~Mp4MovieSampleChunkOffsetBoxWriter() =
 void Mp4MovieSampleChunkOffsetBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_STCO);
+  writer.StartFullBox(mp4::FOURCC_STCO, /*flags=*/0, /*version=*/0);
 
   writer.WriteU32(0);  // entry_count.
 
@@ -606,7 +606,7 @@ Mp4MovieSampleDescriptionBoxWriter::~Mp4MovieSampleDescriptionBoxWriter() =
 void Mp4MovieSampleDescriptionBoxWriter::Write(BoxByteStream& writer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  writer.StartFullBox(mp4::FOURCC_STSD);
+  writer.StartFullBox(mp4::FOURCC_STSD, /*flags=*/0, /*version=*/0);
 
   writer.WriteU32(box_.entry_count);
 
