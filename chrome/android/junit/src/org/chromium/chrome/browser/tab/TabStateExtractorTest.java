d5e41800a0557e2480f2bb80fd2c9a529c2ccf61
Andrew Grieve <agrieve@chromium.org>
2023-06-01 16:33:32 +0000

    Android: Use a real ByteBuffer rather than mocking it.
    Mockito has trouble mocking ByteBuffer on JDK20. See:
    https://chromium-review.googlesource.com/c/chromium/src/+/4573174
    
    There's no reason to mock it anyways.
    
    Bug: 1405257
    Change-Id: I09abb1713c09fbd17242168308d666fb700b815f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4579040
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151909}

--- chrome/android/junit/src/org/chromium/chrome/browser/tab/TabStateExtractorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tab/TabStateExtractorTest.java
@@ -50,9 +50,9 @@ public class TabStateExtractorTest {
     @Mock
     private WebContents mWebContentsMock;
     @Mock
-    private ByteBuffer mByteBufferMock;
-    @Mock
     private Origin mMockOrigin;
+
+    private ByteBuffer mByteBuffer = ByteBuffer.allocate(1);
 
     @Before
     public void setUp() {
@@ -67,7 +67,7 @@ public class TabStateExtractorTest {
     public void testGetWebContentsState_notPending() {
         doReturn(null).when(mTabMock).getPendingLoadParams();
         doReturn(mWebContentsMock).when(mTabMock).getWebContents();
-        doReturn(mByteBufferMock)
+        doReturn(mByteBuffer)
                 .when(mWebContentsBridgeJni)
                 .getContentsStateAsByteBuffer(eq(mWebContentsMock));
 
@@ -75,7 +75,7 @@ public class TabStateExtractorTest {
 
         assertNotNull(result);
         assertEquals(WebContentsState.CONTENTS_STATE_CURRENT_VERSION, result.version());
-        assertEquals(mByteBufferMock, result.buffer());
+        assertEquals(mByteBuffer, result.buffer());
     }
 
     @Test
@@ -86,7 +86,7 @@ public class TabStateExtractorTest {
         loadUrlParams.setInitiatorOrigin(mMockOrigin);
         doReturn(loadUrlParams).when(mTabMock).getPendingLoadParams();
         doReturn(true).when(mTabMock).isIncognito();
-        doReturn(mByteBufferMock)
+        doReturn(mByteBuffer)
                 .when(mWebContentsBridgeJni)
                 .createSingleNavigationStateAsByteBuffer(
                         eq(URL), eq(REFERRER_URL), eq(REFERRER_POLICY), eq(mMockOrigin), eq(true));
@@ -95,6 +95,6 @@ public class TabStateExtractorTest {
 
         assertNotNull(result);
         assertEquals(WebContentsState.CONTENTS_STATE_CURRENT_VERSION, result.version());
-        assertEquals(mByteBufferMock, result.buffer());
+        assertEquals(mByteBuffer, result.buffer());
     }
 }
