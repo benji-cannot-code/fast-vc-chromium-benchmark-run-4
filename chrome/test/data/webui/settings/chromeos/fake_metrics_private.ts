7303651abb9b1bdbf7f001d041b896c41bb3c4f6
Gavin Williams <gavinwill@chromium.org>
2023-06-01 18:06:38 +0000

    Update the FakeMetricsPrivate api
    This class mocks the metrics private api which gives access for Settings
    pages to easily record metrics. All functions in metrics private are
    either recording a numeric or enum value (except for sparse values).
    
    Given that recording numeric value is the expectation (and the sparse
    function isn't implemented in fake metrics api) it makes sense to change
    `countMetricValue()` to expect a number or enum value parameter instead
    of a string.
    
    Fixed: b:285354569
    Tests: browser_tests
    Change-Id: Iba5ed330f1d293b95c3fc33193223a486a2dc0f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577985
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151984}

--- chrome/test/data/webui/settings/chromeos/fake_metrics_private.ts
+++ chrome/test/data/webui/settings/chromeos/fake_metrics_private.ts
@@ -3,8 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+const FALSE_COUNT: number = 0;
+const TRUE_COUNT: number = 1;
+
 export class FakeMetricsPrivate {
-  collectedMetrics: Map<string, {[key: string]: number}>;
+  collectedMetrics: Map<string, {[key: number]: number}>;
   constructor() {
     this.collectedMetrics = new Map();
   }
@@ -24,7 +27,7 @@ export class FakeMetricsPrivate {
     this.collectedMetrics.set(metric, metricEntry);
   }
 
-  countMetricValue(metric: string, value: string): number {
+  countMetricValue(metric: string, value: number): number {
     const metricEntry = this.collectedMetrics.get(metric);
 
     if (metricEntry) {
@@ -36,12 +39,12 @@ export class FakeMetricsPrivate {
   }
 
   recordBoolean(metric: string, value: boolean): void {
-    const metricEntry =
-        this.collectedMetrics.get(metric) || {trueCnt: 0, falseCnt: 0};
+    const metricEntry = this.collectedMetrics.get(metric) ||
+        {[TRUE_COUNT]: 0, [FALSE_COUNT]: 0};
     if (value) {
-      metricEntry['trueCnt'] += 1;
+      metricEntry[TRUE_COUNT] += 1;
     } else {
-      metricEntry['falseCnt'] += 1;
+      metricEntry[FALSE_COUNT] += 1;
     }
     this.collectedMetrics.set(metric, metricEntry);
   }
@@ -51,9 +54,9 @@ export class FakeMetricsPrivate {
 
     if (metricEntry) {
       if (value) {
-        return metricEntry['trueCnt'] as number;
+        return metricEntry[TRUE_COUNT] as number;
       } else {
-        return metricEntry['falseCnt'] as number;
+        return metricEntry[FALSE_COUNT] as number;
       }
     } else {
       return 0;
