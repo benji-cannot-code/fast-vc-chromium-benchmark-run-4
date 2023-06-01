69ad2a4cd91ad8eff75cb9cf7c8d5bb9b30d488a
Wenyu Fu <wenyufu@google.com>
2023-06-01 17:02:30 +0000

    [Sharesheet] Remove long screenshot editor when flag enabled
    Remove the image editor for long screenshot when custom action polish flag is enabled.
    
    Bug: 1433326
    Change-Id: Id61a6a61e5a02dc763f0f5e9d120cf562604821d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4576814
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151940}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProvider.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProvider.java
@@ -174,7 +174,8 @@ public class ChromeProvidedSharingOptionsProvider extends ChromeProvidedSharingO
                     mFeatureEngagementTracker.notifyEvent(EventConstants.SHARE_SCREENSHOT_SELECTED);
                     ScreenshotCoordinator coordinator = new ScreenshotCoordinator(mActivity,
                             mShareParams.getWindow(), mUrl, mChromeOptionShareCallback,
-                            mBottomSheetController, mImageEditorModuleProvider);
+                            mBottomSheetController,
+                            usePolishedActionOrderedList() ? null : mImageEditorModuleProvider);
                     mBottomSheetController.addObserver(coordinator);
                     mBottomSheetController.hideContent(mBottomSheetContent, true);
                 })
@@ -193,7 +194,8 @@ public class ChromeProvidedSharingOptionsProvider extends ChromeProvidedSharingO
                     mFeatureEngagementTracker.notifyEvent(EventConstants.SHARE_SCREENSHOT_SELECTED);
                     LongScreenshotsCoordinator coordinator = LongScreenshotsCoordinator.create(
                             mActivity, mTabProvider.get(), mUrl, mChromeOptionShareCallback,
-                            mBottomSheetController, mImageEditorModuleProvider);
+                            mBottomSheetController,
+                            usePolishedActionOrderedList() ? null : mImageEditorModuleProvider);
                     mBottomSheetController.addObserver(coordinator);
                     mBottomSheetController.hideContent(mBottomSheetContent, true);
                 })
