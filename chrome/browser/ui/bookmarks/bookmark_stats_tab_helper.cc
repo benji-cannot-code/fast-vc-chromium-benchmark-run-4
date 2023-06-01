1427cc321eb92796210fc9a9b4502e9b9985223b
Thomas Lukaszewicz <tluk@chromium.org>
2023-06-01 21:32:30 +0000

    [bookmarks] Add BookmarkStatsTabHelper for bookmark metrics - Part 2
    This CL adds a metric that captures the time taken from launching a
    bookmark from the bookmarks bar into the current tab - to when the
    first non-empty paint of the navigation occurs.
    
    Follow up CLs will add metrics to capture similar actions for
    different launch locations (such as the side panel and app menu)
    and different window open dispositions.
    
    Bug: 1449016
    Change-Id: I96ecac75e3f5f18174a57bd92219a5c35b496928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4579251
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152138}

--- chrome/browser/ui/bookmarks/bookmark_stats_tab_helper.cc
+++ chrome/browser/ui/bookmarks/bookmark_stats_tab_helper.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/bookmarks/bookmark_stats_tab_helper.h"
 
+#include "base/metrics/histogram_functions.h"
+#include "base/time/time.h"
 #include "content/public/browser/navigation_handle.h"
 
 BookmarkStatsTabHelper::~BookmarkStatsTabHelper() = default;
@@ -37,6 +39,16 @@ void BookmarkStatsTabHelper::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
   if (navigation_handle->IsInPrimaryMainFrame()) {
     should_reset_launch_data_ = true;
+  }
+}
+
+void BookmarkStatsTabHelper::DidFirstVisuallyNonEmptyPaint() {
+  if (launch_action_ &&
+      tab_disposition_ == WindowOpenDisposition::CURRENT_TAB &&
+      launch_action_->location == BookmarkLaunchLocation::kAttachedBar) {
+    base::UmaHistogramTimes(
+        "Bookmarks.AttachedBar.CurrentTab.TimeToFirstVisuallyNonEmptyPaint",
+        base::TimeTicks::Now() - launch_action_->action_time);
   }
 }
 
