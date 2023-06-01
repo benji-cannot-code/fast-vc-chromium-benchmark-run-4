b20b223e67777015d2b8d911fdaf76128c240d7a
wenyu zhang <zhangwenyu@google.com>
2023-06-01 20:49:24 +0000

    diagnostics: Fix diagnostics input link issue
    Fix a race condition that panel navigation happens before connected
    keyboard is detected. This causes `chrome://diagnostics?input` link
    broken.
    
    Bug: b/282008804
    Test: Test locally and diagnostics browser test
    Change-Id: Id73f7aae1be9a9da544781ba8581c20b72d7cb73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4581669
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152105}

--- ash/webui/diagnostics_ui/resources/diagnostics_app.ts
+++ ash/webui/diagnostics_ui/resources/diagnostics_app.ts
@@ -186,6 +186,7 @@ export class DiagnosticsAppElement extends DiagnosticsAppElementBase {
     ];
 
     if (this.isInputEnabled) {
+      pages.push(this.createInputSelector());
       const devices: ConnectedDevices =
           await this.inputDataProvider.getConnectedDevices();
       // Check the existing value of |numKeyboards| if |GetConnectedDevices|
@@ -195,8 +196,8 @@ export class DiagnosticsAppElement extends DiagnosticsAppElementBase {
       const isTouchPadOrTouchScreenEnabled =
           loadTimeData.getBoolean('isTouchpadEnabled') ||
           loadTimeData.getBoolean('isTouchscreenEnabled');
-      if (this.numKeyboards > 0 || isTouchPadOrTouchScreenEnabled) {
-        pages.push(this.createInputSelector());
+      if (this.numKeyboards === 0 && !isTouchPadOrTouchScreenEnabled) {
+        pages.pop();
       }
     }
 
