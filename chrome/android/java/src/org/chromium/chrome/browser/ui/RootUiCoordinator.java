4c8171705558bdeaa24a188e1aa67090238eab15
Zach Katz <katzz@google.com>
2023-06-01 01:15:15 +0000

    [PCCT-SideSheet] Fix ultrasmall menu sometimes visible on bottom sheet
    - As is, menu height calculation for PCCTs are simply inaccurate. In this change, we measure the Rect for the PCCT activity based on the entire screen, so that we don't run into trouble with our AnchorViewY (now AnchorViewOffset), which started generating false positive values for some PCCT configurations, but did not work in all cases.
    
    Bug: 1423456
    Change-Id: I8e5ce2b7086878f25687c5278dfe0d0588dfb945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4522090
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Zach Katz <katzz@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1151600}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -1336,7 +1336,7 @@ public class RootUiCoordinator
                     mActivityLifecycleDispatcher, mToolbarManager, mAppMenuDelegate,
                     mActivity.getWindow().getDecorView(),
                     mActivity.getWindow().getDecorView().findViewById(R.id.menu_anchor_stub),
-                    this::getAppRectInWindow);
+                    this::getAppRectOnScreen);
             AppMenuCoordinatorFactory.setExceptionReporter(
                     (throwable)
                             -> ChromePureJavaExceptionReporter.reportJavaException(
@@ -1366,7 +1366,7 @@ public class RootUiCoordinator
     /**
      * Returns {@link Rect} that represents the app client area the app menu should fit in.
      */
-    protected Rect getAppRectInWindow() {
+    protected Rect getAppRectOnScreen() {
         Rect appRect = new Rect();
         mActivity.getWindow().getDecorView().getWindowVisibleDisplayFrame(appRect);
         return appRect;
