fbdf907d77f7c5fae1a800e8c0e7e6d53ff1d776
Tom Sepez <tsepez@chromium.org>
2023-06-01 16:57:06 +0000

    Avoid dangling ptr in MetricsWebContentsObserverBackForwardCacheTest
    Explicitly drop unowned reference before destroying the object which
    owns it.
    
    Bug: 1401495
    Change-Id: I102bdb5dc35db61d0824770be47281bcec2eab78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577700
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151935}

--- components/page_load_metrics/browser/metrics_web_contents_observer_unittest.cc
+++ components/page_load_metrics/browser/metrics_web_contents_observer_unittest.cc
@@ -98,6 +98,7 @@ class MetricsWebContentsObserverTest
   }
 
   void TearDown() override {
+    embedder_interface_ = nullptr;
     content::SetBrowserClientForTesting(original_browser_client_);
     RenderViewHostTestHarness::TearDown();
   }
@@ -250,8 +251,7 @@ class MetricsWebContentsObserverTest
   }
 
   base::HistogramTester histogram_tester_;
-  raw_ptr<TestMetricsWebContentsObserverEmbedder, DanglingUntriaged>
-      embedder_interface_;
+  raw_ptr<TestMetricsWebContentsObserverEmbedder> embedder_interface_;
 
  private:
   int num_errors_ = 0;
