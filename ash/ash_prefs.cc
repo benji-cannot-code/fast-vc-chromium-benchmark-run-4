0395a437bb85ea4b4bd3cd4fb1060253e3f3475c
Hongyu Long <hongyulong@chromium.org>
2023-06-01 19:10:56 +0000

    CrOS Settings: Add the toggle button for charging sounds
    This CL adds the charging toggle button under Settings > Device > Audio
    subpage. When the user enables this button, plugging an AC charger, the
    charging sound will be played. Whereas when this button is disabled, no
    charging sound will be played. Notes: This toggle button will be shown
    up only when the `#enable-power-sounds` flag under chrome://flags is
    enabled.
    
    https://screenshot.googleplex.com/8AsRU2w2ZWAbK5u
    https://screenshot.googleplex.com/5cxRhMMBapri62s
    
    Bug: b/281720689
    Test: Manual & unittest
    Change-Id: I3077df9103ed11b3ee384d2861b728ec36dceff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575417
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152041}

--- ash/ash_prefs.cc
+++ ash/ash_prefs.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/palette/palette_welcome_bubble.h"
 #include "ash/system/pcie_peripheral/pcie_peripheral_notification_controller.h"
 #include "ash/system/power/power_prefs.h"
+#include "ash/system/power/power_sounds_controller.h"
 #include "ash/system/privacy_hub/privacy_hub_controller.h"
 #include "ash/system/session/logout_button_tray.h"
 #include "ash/system/session/logout_confirmation_controller.h"
@@ -125,6 +126,7 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry, bool for_test) {
   PaletteTray::RegisterProfilePrefs(registry);
   PaletteWelcomeBubble::RegisterProfilePrefs(registry);
   PciePeripheralNotificationController::RegisterProfilePrefs(registry);
+  PowerSoundsController::RegisterPrefs(registry);
   PrivacyHubController::RegisterProfilePrefs(registry);
   PrivacyScreenController::RegisterProfilePrefs(registry);
   ProjectorControllerImpl::RegisterProfilePrefs(registry);
--- ash/system/power/power_sounds_controller.h
+++ ash/system/power/power_sounds_controller.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/power/power_status.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 
+class PrefRegistrySimple;
+
 namespace ash {
 
 // Controller class to manage power/battery sounds.
@@ -23,6 +25,8 @@ class ASH_EXPORT PowerSoundsController
   PowerSoundsController(const PowerSoundsController&) = delete;
   PowerSoundsController& operator=(const PowerSoundsController&) = delete;
   ~PowerSoundsController() override;
+
+  static void RegisterPrefs(PrefRegistrySimple* registry);
 
   // PowerStatus::Observer:
   void OnPowerStatusChanged() override;
