e8b252b62dd688da371246c8a0bb531f3ba27300
Awad Osman <awado@google.com>
2023-06-01 19:19:44 +0000

    [Desktop] Update icon for the Tailored Security desktop notification
    This change replaces the old blue checkmark icon for the Tailored
    security desktop notification and updates it to the blue google shield
    icon depending on the status of our feature flag.
    
    Old Screenshot: https://screenshot.googleplex.com/5YU6iApKaoYFPL7.png
    New Screenshot: https://screenshot.googleplex.com/AnuUPcDYJZ5j4wb.png
    
    Bug: 1414214
    Change-Id: I6f83423290d0237857a1926c3693efc649c65e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521439
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152046}

--- chrome/browser/safe_browsing/tailored_security/notification_handler_desktop.cc
+++ chrome/browser/safe_browsing/tailored_security/notification_handler_desktop.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/google_chrome_strings.h"
+#include "chrome/grit/theme_resources.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_outcome.h"
 #include "components/safe_browsing/core/common/features.h"
@@ -112,6 +113,16 @@ void TailoredSecurityNotificationHandler::OnClick(
   std::move(completed_closure).Run();
 }
 
+ui::ImageModel GetNotificationIcon() {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  return ui::ImageModel::FromResourceId(
+      IDR_TAILORED_SECURITY_UNCONSENTED_NOTIFICATION);
+#else
+  return ui::ImageModel::FromVectorIcon(kSafetyCheckIcon, ui::kColorAccent,
+                                        message_center::kNotificationIconSize);
+#endif
+}
+
 void DisplayTailoredSecurityUnconsentedPromotionNotification(Profile* profile) {
   std::string notification_id =
       kTailoredSecurityUnconsentedPromotionNotificationId;
@@ -133,12 +144,12 @@ void DisplayTailoredSecurityUnconsentedPromotionNotification(Profile* profile) {
 #else
   const message_center::NotifierId notifier_id = GetNotifierId();
 #endif
-
-  // TODO(crbug/1257622): Confirm with UX that it's appropriate to use the
-  // blue color here.
-  auto icon =
-      ui::ImageModel::FromVectorIcon(kSafetyCheckIcon, ui::kColorAccent,
-                                     message_center::kNotificationIconSize);
+  auto icon = (base::FeatureList::IsEnabled(
+                  safe_browsing::kTailoredSecurityUpdatedMessages))
+                  ? GetNotificationIcon()
+                  : ui::ImageModel::FromVectorIcon(
+                        kSafetyCheckIcon, ui::kColorAccent,
+                        message_center::kNotificationIconSize);
   LogUnconsentedOutcome(TailoredSecurityOutcome::kShown);
   message_center::Notification notification(
       message_center::NOTIFICATION_TYPE_SIMPLE, notification_id, title,
