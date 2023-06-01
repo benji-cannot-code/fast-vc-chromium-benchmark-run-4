a96e3b38ff5bf07d621226febfa1a868edb4a2af
Mark Schillaci <mschillaci@google.com>
2023-06-01 22:53:43 +0000

    [Auto-disable Accessibility] Remove AccessibilityManager dependency from WebContentsAccessibilityImpl
    This CL continues the AccessibilityState consolidation work for Clank.
    
    With this CL we remove the hard dependency on AccessibilityManager that
    the WebContentsAccessibilityImpl class has. We will instead use the new
    AccessibilityState class directly. We also remove the corresponding
    *ForTesting method and make callers of it use AccessibilityState as
    well. This is cleaner as the web contents should not be used to control
    the state of accessibility of the entire app.
    
    
    AX-Relnotes: N/A
    Bug: 1430202, b/265493191
    Change-Id: Ib2c4285fde919fa985902204ead25364d9215bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569293
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152206}

--- content/public/android/java/src/org/chromium/content_public/browser/WebContentsAccessibility.java
+++ content/public/android/java/src/org/chromium/content_public/browser/WebContentsAccessibility.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.ViewStructure;
 import android.view.accessibility.AccessibilityNodeProvider;
 
-import androidx.annotation.VisibleForTesting;
-
 import org.chromium.content.browser.accessibility.WebContentsAccessibilityImpl;
 
 /**
@@ -33,12 +31,6 @@ public interface WebContentsAccessibility {
      *  @return {@code true} if the framework has been initialized.
      */
     boolean isNativeInitialized();
-
-    /**
-     *  Enables a11y for testing.
-     */
-    @VisibleForTesting
-    void setAccessibilityEnabledForTesting();
 
     /**
      * If native accessibility is enabled and no other views are temporarily
