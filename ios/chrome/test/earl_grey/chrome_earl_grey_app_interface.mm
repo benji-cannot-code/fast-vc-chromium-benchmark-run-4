bd7101bb763953b78b5df18de6712aa719354932
Gayane Petrosyan <gayane@google.com>
2023-06-01 14:33:40 +0000

    Revert "[ios] Add EG tests for SetUpList"
    This reverts commit c482d52fa2b80e989d975bbbe2e006f8478001c9.
    
    Reason for revert: Tests failing on downstream bots https://bugs.chromium.org/p/chromium/issues/detail?id=1450595
    
    Original change's description:
    > [ios] Add EG tests for SetUpList
    >
    > Includes the following tests:
    >  * tests that each item brings up the appropriate UI flow and that
    >    dismissing each will mark them as complete. When all are complete,
    >    the "All Set" view appears.
    >  * tests that the signin flow works
    >  * tests that the expand / unexpand button works
    >
    > Bug: 1427439
    > Change-Id: Ic8c348222cfd9db60eb08373c43e82f1fc7eca18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574028
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    > Commit-Queue: Scott Yoder <scottyoder@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1151781}
    
    Bug: 1427439
    Change-Id: Ifdc50cc98047b15a03f50969719c423e94e42969
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4580367
    Owners-Override: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151842}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/command_line.h"
 #import "base/containers/contains.h"
-#import "base/files/file.h"
 #import "base/files/file_util.h"
 #import "base/ios/ios_util.h"
 #import "base/json/json_string_value_serializer.h"
@@ -37,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/content_settings/host_content_settings_map_factory.h"
 #import "ios/chrome/browser/default_browser/utils.h"
 #import "ios/chrome/browser/default_browser/utils_test_support.h"
-#import "ios/chrome/browser/first_run/first_run.h"
 #import "ios/chrome/browser/ntp/features.h"
 #import "ios/chrome/browser/search_engines/search_engines_util.h"
 #import "ios/chrome/browser/search_engines/template_url_service_factory.h"
@@ -1456,23 +1454,6 @@ int watchRunNumber = 0;
   chrome_test_util::GetMainController().appState.shouldShowDefaultBrowserPromo =
       NO;
   LogUserInteractionWithFullscreenPromo();
-}
-
-#pragma mark - First Run Utilities
-
-+ (void)writeFirstRunSentinel {
-  base::ScopedAllowBlockingForTesting allow_blocking;
-  FirstRun::RemoveSentinel();
-  base::File::Error fileError;
-  FirstRun::CreateSentinel(&fileError);
-  FirstRun::LoadSentinelInfo();
-}
-
-+ (void)removeFirstRunSentinel {
-  base::ScopedAllowBlockingForTesting allow_blocking;
-  if (FirstRun::RemoveSentinel()) {
-    FirstRun::LoadSentinelInfo();
-  }
 }
 
 @end
