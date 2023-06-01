73d144c262df0b9be98233417853aa40335c40af
Maggie Chen <magchen@chromium.org>
2023-06-01 18:02:09 +0000

    Track parent_clip_rect across frames in SurfaceAggregator.
    This is for the feature AllowUndamagedNonrootRenderPassToSkip.
    
    Introduce |parent_clip_rect| which is the intersection of ancestors'
    |output_rect|, RenderPassDrawQuad rect, SurfaceDrawQuad rect and |clip
    rect|. If |parent_clip_rect| changes from the previous frame, this
    render pass cannot be skipped.
    
    If a render pass is merged to its parent render pass in the previous
    frame but is not merged in the current frame, it has to redraw with full
    damage because there is no separate output buffer for this render pass
    when it's merged.
    
    Remove in_pixel_moving_filter_pass from
    SurfaceAggregator::RenderPassNeedsFullDamage(). The damage rect of a
    render pass with filters is calculated correctly in Prewalk. It doesn't
    need to full damage.
    
    This implementation of tracking render pass parent_clip_rect across
    frames fixes the regressions in linux WPT tests when a skipped render pass is allow.
    
    Bug: 1446098
    Change-Id: I0300c88921fad20612936a3ece3648782c076d5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4565025
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151977}

--- components/viz/service/display/surface_aggregator_unittest.cc
+++ components/viz/service/display/surface_aggregator_unittest.cc
@@ -19,9 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "cc/base/math_util.h"
 #include "cc/test/render_pass_test_utils.h"
+#include "components/viz/common/features.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/quads/aggregated_render_pass.h"
 #include "components/viz/common/quads/aggregated_render_pass_draw_quad.h"
@@ -5151,6 +5153,12 @@ class SurfaceAggregatorPartialSwapTest
 
 // Tests that quads outside the damage rect are ignored.
 TEST_F(SurfaceAggregatorPartialSwapTest, IgnoreOutside) {
+  // The damage is different for the pixel-moving filter mode with
+  // kAllowUndamagedNonrootRenderPassToSkip enabled.
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      features::kAllowUndamagedNonrootRenderPassToSkip);
+
   TestSurfaceIdAllocator child_surface_id(child_sink_->frame_sink_id());
   constexpr float device_scale_factor = 1.0f;
 
