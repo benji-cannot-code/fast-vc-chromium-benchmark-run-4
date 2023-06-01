4a6239b4b804a5274fc1b6367c5d9abb2ada32a5
Allan Kerr <akerr@dropbox.com>
2023-06-01 19:20:55 +0000

    Add standalone offline install support to the meta-installer
    Offline installers are currently possible but only if the
    `--offlinedir={GUID}` switch is provided. To be able to support
    standlone offline installers, it must be possible to run an offline
    installer with no switches.
    
    To support this, the meta-installer now searches for an offline install
    directory at `bin\Offline\{GUID}` in the directory the embedded
    `updater.7z` is extracted to. If it finds one it appends an
    `--offlinedir={GUID}` switch to the command line used to run
    `updater.exe` indicating an offline install should be performed using
    the directory it found.
    
    Bug: 1281688
    Change-Id: Idd1e085a781ba248321acd4b15bfbcfa7f257f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573758
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152048}

--- chrome/updater/win/installer/installer_unittest.cc
+++ chrome/updater/win/installer/installer_unittest.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
 #include "chrome/updater/constants.h"
 #include "chrome/updater/win/installer/exit_code.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -30,4 +32,26 @@ TEST(InstallerTest, HandleRunElevated) {
       updater::HandleRunElevated(command_line);
   EXPECT_EQ(exit_result.exit_code, updater::UNABLE_TO_ELEVATE_METAINSTALLER);
   EXPECT_EQ(exit_result.windows_error, 0U);
+}
+
+TEST(InstallerTest, FindOfflineDir) {
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+
+  base::FilePath unpack_path = temp_dir.GetPath();
+  base::FilePath metainstall_dir = unpack_path.Append(L"bin");
+  ASSERT_TRUE(base::CreateDirectory(metainstall_dir));
+
+  EXPECT_FALSE(updater::FindOfflineDir(unpack_path).has_value());
+
+  base::FilePath offline_install_dir =
+      metainstall_dir.Append(L"Offline")
+          .Append(L"{8D5D0563-F2A0-40E3-932D-AFEAE261A9D1}");
+  ASSERT_TRUE(base::CreateDirectory(offline_install_dir));
+
+  absl::optional<base::FilePath> offline_dir =
+      updater::FindOfflineDir(unpack_path);
+  EXPECT_TRUE(offline_dir.has_value());
+  EXPECT_EQ(offline_dir->BaseName(),
+            base::FilePath(L"{8D5D0563-F2A0-40E3-932D-AFEAE261A9D1}"));
 }
