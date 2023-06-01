d499b42af8f7089ad60b4b209dedc0350fbdc5f4
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-06-01 14:43:57 +0000

    [RemoveLegacy] Remove LayoutDisableBrokenFloatInvalidation logic.
    In M114 - there should be no behaviour change.
    
    Bug: 1229581
    Change-Id: If78ebe244ba3c4ff088becf774b16240303026ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575573
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151846}

--- third_party/blink/renderer/core/layout/layout_block_flow.h
+++ third_party/blink/renderer/core/layout/layout_block_flow.h
@@ -80,10 +80,6 @@ class CORE_EXPORT LayoutBlockFlow : public LayoutBlock {
 
   bool CanContainFirstFormattedLine() const;
 
-  void MarkAllDescendantsWithFloatsForLayout(
-      LayoutBox* float_to_remove = nullptr,
-      bool in_layout = true);
-
   void AddChild(LayoutObject* new_child,
                 LayoutObject* before_child = nullptr) override;
   void RemoveChild(LayoutObject*) override;
--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -1344,9 +1344,6 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   PositionWithAffinity PositionForPoint(const PhysicalOffset&) const override;
   PositionWithAffinity PositionForPointInFragments(const PhysicalOffset&) const;
 
-  void RemoveFloatingOrPositionedChildFromBlockLists();
-
-  bool ShrinkToAvoidFloats() const;
   virtual bool CreatesNewFormattingContext() const {
     NOT_DESTROYED();
     return true;
