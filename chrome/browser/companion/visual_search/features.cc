72fb2753568181ae31aa7feda1c31688120775f0
Pierre St Juste <pstjuste@google.com>
2023-06-01 22:41:44 +0000

    Adding keyed service for model fetching.
    Update based on review feedback.
    
    Bug: 277769894
    Change-Id: Ief8435affb9d8134990005bd514939afe1aab171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577818
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Pierre St Juste <pstjuste@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152191}

--- chrome/browser/companion/visual_search/features.cc
+++ chrome/browser/companion/visual_search/features.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
-namespace visual_search {
+namespace companion::visual_search {
 namespace features {
 
 BASE_FEATURE(kVisualSearchSuggestions,
@@ -33,4 +33,4 @@ absl::optional<std::string> GetVisualSearchConfigForCompanionOverride() {
 }
 
 }  // namespace switches
-}  // namespace visual_search
+}  // namespace companion::visual_search
--- chrome/browser/companion/visual_search/features.h
+++ chrome/browser/companion/visual_search/features.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
-namespace visual_search {
+namespace companion::visual_search {
 namespace features {
 
 // Enables visual search capabilities for the companion.
@@ -21,9 +21,10 @@ BASE_DECLARE_FEATURE(kVisualSearchSuggestions);
 namespace switches {
 extern const char kVisualSearchConfigForCompanion[];
 
+// Allows us to pass in a base64 proto string in the command line
 absl::optional<std::string> GetVisualSearchConfigForCompanionOverride();
 
 }  // namespace switches
-}  // namespace visual_search
+}  // namespace companion::visual_search
 
 #endif  // CHROME_BROWSER_COMPANION_VISUAL_SEARCH_FEATURES_H_
