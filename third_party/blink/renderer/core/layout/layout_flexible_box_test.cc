8c8068d5b9e521cea6c26d17500a9d62b0c8f362
Kent Tamura <tkent@chromium.org>
2023-06-02 15:23:37 +0000

    Revert "Change the return type of LayoutBox::OriginAdjustmentForScrollbars()"
    This reverts commit edd8ae3b8e078385233e0f9c2e2ba8f2d2867503.
    
    Reason for revert: It turned out that this CL was unnecessary.
    
    Original change's description:
    > Change the return type of LayoutBox::OriginAdjustmentForScrollbars()
    >
    > ... from gfx::Vector2d to PhysicalOffset.
    >
    > This is a preparation to remove pixel-snapping in MaximumScrollOffset().
    > This CL has no behavior changes.
    >
    > Bug: 1450757
    > Change-Id: I66e0f22fe4119efe224cb358bf82df2c89cc4a33
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583307
    > Auto-Submit: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1152310}
    
    Bug: 1450757
    Change-Id: I731c2268d0e6fbe6d2ff0d7fa16a42de0ae34121
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583197
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152545}

--- third_party/blink/renderer/core/layout/layout_flexible_box_test.cc
+++ third_party/blink/renderer/core/layout/layout_flexible_box_test.cc
@@ -80,7 +80,7 @@ void LayoutFlexibleBoxTest::ExpectSameAsRowHTB() {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   // 1040 = child_height (1000) + padding_top (10) + padding_bottom (30)
   EXPECT_EQ(LayoutRect(50, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(1615, 716),
@@ -108,7 +108,7 @@ void LayoutFlexibleBoxTest::ExpectSameAsRowVLR() {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(50, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(1615, 716),
             scrollable_area->MaximumScrollOffsetInt());
@@ -135,7 +135,7 @@ void LayoutFlexibleBoxTest::ExpectSameAsRowVRL() {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(45, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(0, 716), scrollable_area->MaximumScrollOffsetInt());
   EXPECT_EQ(gfx::Vector2d(-1615, 0), scrollable_area->MinimumScrollOffsetInt());
@@ -169,7 +169,7 @@ TEST_F(LayoutFlexibleBoxTest, GeometriesWithScrollbarsRowReverseHTB) {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(-1565, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(0, 716), scrollable_area->MaximumScrollOffsetInt());
   EXPECT_EQ(gfx::Vector2d(-1615, 0), scrollable_area->MinimumScrollOffsetInt());
@@ -186,7 +186,7 @@ void LayoutFlexibleBoxTest::ExpectSameAsRowReverseVLR() {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(50, -696, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(1615, 0), scrollable_area->MaximumScrollOffsetInt());
   EXPECT_EQ(gfx::Vector2d(0, -716), scrollable_area->MinimumScrollOffsetInt());
@@ -212,7 +212,7 @@ void LayoutFlexibleBoxTest::ExpectSameAsRowReverseVRL() {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(45, -696, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(), scrollable_area->MaximumScrollOffsetInt());
   EXPECT_EQ(gfx::Vector2d(-1615, -716),
@@ -278,7 +278,7 @@ TEST_F(LayoutFlexibleBoxTest, GeometriesWithScrollbarsColumnReverseHTB) {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(50, -696, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(1615, 0), scrollable_area->MaximumScrollOffsetInt());
   EXPECT_EQ(gfx::Vector2d(0, -716), scrollable_area->MinimumScrollOffsetInt());
@@ -301,7 +301,7 @@ TEST_F(LayoutFlexibleBoxTest, GeometriesWithScrollbarsColumnReverseVLR) {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(-1565, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(0, 716), scrollable_area->MaximumScrollOffsetInt());
   EXPECT_EQ(gfx::Vector2d(-1615, 0), scrollable_area->MinimumScrollOffsetInt());
@@ -324,7 +324,7 @@ TEST_F(LayoutFlexibleBoxTest, GeometriesWithScrollbarsColumnReverseVRL) {
   const auto* scrollable_area = flex_box->GetScrollableArea();
   CheckFlexBoxPhysicalGeometries(flex_box);
 
-  EXPECT_EQ(PhysicalOffset(), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(-1570, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(1615, 716),
             scrollable_area->MaximumScrollOffsetInt());
@@ -343,7 +343,7 @@ void LayoutFlexibleBoxTest::ExpectSameAsRTLRowHTB() {
   CheckFlexBoxPhysicalGeometries(flex_box);
 
   // Additional origin due to the scrollbar on the left.
-  EXPECT_EQ(PhysicalOffset(15, 0), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(15, 0), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(-1550, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(0, 716), scrollable_area->MaximumScrollOffsetInt());
   EXPECT_EQ(gfx::Vector2d(-1615, 0), scrollable_area->MinimumScrollOffsetInt());
@@ -394,7 +394,7 @@ TEST_F(LayoutFlexibleBoxTest, GeometriesWithScrollbarsRTLRowReverseHTB) {
   CheckFlexBoxPhysicalGeometries(flex_box);
 
   // Additional origin due to the scrollbar on the left.
-  EXPECT_EQ(PhysicalOffset(15, 0), flex_box->OriginAdjustmentForScrollbars());
+  EXPECT_EQ(gfx::Vector2d(15, 0), flex_box->OriginAdjustmentForScrollbars());
   EXPECT_EQ(LayoutRect(65, 20, 2060, 1040), flex_box->LayoutOverflowRect());
   EXPECT_EQ(gfx::Vector2d(1615, 716),
             scrollable_area->MaximumScrollOffsetInt());
