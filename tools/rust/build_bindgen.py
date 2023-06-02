f74432495c21d4734a39fd6201e03ad0040fadb6
danakj <danakj@chromium.org>
2023-06-02 19:06:00 +0000

    Build i686 versions of the Rust stdlib for Linux
    Bug: 1368726
    Change-Id: I70d7fbe48d57efb1b72c7a214ac3e72c3b2d2883
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-rel,linux-rust-x64-dbg,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582127
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152679}

--- tools/rust/build_bindgen.py
+++ tools/rust/build_bindgen.py
@@ -114,8 +114,8 @@ def main():
         # along with the host and target stdlib to a sysroot dir.
         root = InstallRustBetaSysroot(
             RUST_REVISION,
-            [RustTargetTriple(None),
-             RustTargetTriple(args.build_mac_arm)])
+            [RustTargetTriple(),
+             RustTargetTriple(build_mac_arm=True)])
         cargo_bin = os.path.join(root, 'bin', f'cargo{EXE}')
         rustc_bin = os.path.join(root, 'bin', f'rustc{EXE}')
 
@@ -222,7 +222,7 @@ def main():
         'build',
         f'--manifest-path={BINDGEN_SRC_DIR}/Cargo.toml',
         f'--target-dir={build_dir}',
-        f'--target={RustTargetTriple(args.build_mac_arm)}',
+        f'--target={RustTargetTriple(build_mac_arm = args.build_mac_arm)}',
         f'--no-default-features',
         f'--features=logging',
         '--release',
@@ -235,7 +235,8 @@ def main():
     print(f'Installing bindgen to {install_dir} ...')
 
     shutil.copy(
-        os.path.join(build_dir, RustTargetTriple(args.build_mac_arm),
+        os.path.join(build_dir,
+                     RustTargetTriple(build_mac_arm=args.build_mac_arm),
                      'release', f'bindgen{EXE}'),
         os.path.join(install_dir, 'bin'))
     if sys.platform == 'win32':
