0d6676e625e9532b57be26c61f70f55a6c9549eb
朱思达 <zhusida@bytedance.com>
2023-06-02 16:56:55 +0000

    Revert "Complete migration to media::D3DVideoDecoderWrapper"
    This reverts commit 6f295d71c7c7ce7633c319f9b32cbefe92a315bd.
    
    Reason for revert: break H265 video playback on Windows
    
    Original change's description:
    > Complete migration to media::D3DVideoDecoderWrapper
    >
    > Migrate H265 and AV1 accelerator to the new wrapper and remove the old one.
    >
    > Bug: 1348104
    > Change-Id: Idd98e0da4935b995a0222eb365c4a6c33e1426d1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4331451
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Zhibo Wang <zhibo1.wang@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1151662}
    
    Bug: 1348104
    Change-Id: Iaa91b52e815ada772eac2db18b4b5e3c29a2050f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583752
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: 朱思达 <zhusida@bytedance.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152598}

--- media/gpu/windows/d3d11_av1_accelerator.h
+++ media/gpu/windows/d3d11_av1_accelerator.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <d3d11_1.h>
 #include <d3d9.h>
 #include <dxva.h>
+#include <windows.h>
 #include <wrl/client.h>
 
 #include "base/functional/callback_helpers.h"
@@ -17,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/av1_decoder.h"
 #include "media/gpu/windows/d3d11_com_defs.h"
 #include "media/gpu/windows/d3d11_status.h"
+#include "media/gpu/windows/d3d11_video_context_wrapper.h"
 #include "media/gpu/windows/d3d11_video_decoder_client.h"
-#include "media/gpu/windows/d3d11_video_decoder_wrapper.h"
 #include "media/gpu/windows/d3d_accelerator.h"
 
 typedef struct _DXVA_PicParams_AV1 DXVA_PicParams_AV1;
@@ -31,7 +32,8 @@ class D3D11AV1Accelerator : public D3DAccelerator,
  public:
   D3D11AV1Accelerator(D3D11VideoDecoderClient* client,
                       MediaLog* media_log,
-                      ComD3D11VideoDevice video_device);
+                      ComD3D11VideoDevice video_device,
+                      std::unique_ptr<VideoContextWrapper> video_context);
 
   D3D11AV1Accelerator(const D3D11AV1Accelerator&) = delete;
   D3D11AV1Accelerator& operator=(const D3D11AV1Accelerator&) = delete;
@@ -49,6 +51,9 @@ class D3D11AV1Accelerator : public D3DAccelerator,
   bool OutputPicture(const AV1Picture& pic) override;
 
  private:
+  class ScopedDecoderBuffer;
+  ScopedDecoderBuffer GetBuffer(D3D11_VIDEO_DECODER_BUFFER_TYPE type);
+
   bool SubmitDecoderBuffer(
       const DXVA_PicParams_AV1& pic_params,
       const libgav1::Vector<libgav1::TileBuffer>& tile_buffers);
--- media/gpu/windows/d3d11_h265_accelerator.h
+++ media/gpu/windows/d3d11_h265_accelerator.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/h265_dpb.h"
 #include "media/gpu/windows/d3d11_com_defs.h"
 #include "media/gpu/windows/d3d11_status.h"
+#include "media/gpu/windows/d3d11_video_context_wrapper.h"
 #include "media/gpu/windows/d3d11_video_decoder_client.h"
-#include "media/gpu/windows/d3d11_video_decoder_wrapper.h"
 #include "media/gpu/windows/d3d_accelerator.h"
 #include "media/video/picture.h"
 #include "third_party/angle/include/EGL/egl.h"
@@ -33,6 +33,7 @@ namespace media {
 // Maximum of valid DXVA_PicEntry_HEVC entries in RefPicList
 constexpr unsigned kMaxRefPicListSize = 15;
 
+class D3D11H265Accelerator;
 class MediaLog;
 
 // Picture Parameters DXVA buffer struct for Rext/Scc is not specified in DXVA
@@ -111,7 +112,8 @@ class D3D11H265Accelerator : public D3DAccelerator,
  public:
   D3D11H265Accelerator(D3D11VideoDecoderClient* client,
                        MediaLog* media_log,
-                       ComD3D11VideoDevice video_device);
+                       ComD3D11VideoDevice video_device,
+                       std::unique_ptr<VideoContextWrapper> video_context);
 
   D3D11H265Accelerator(const D3D11H265Accelerator&) = delete;
   D3D11H265Accelerator& operator=(const D3D11H265Accelerator&) = delete;
@@ -147,6 +149,9 @@ class D3D11H265Accelerator : public D3DAccelerator,
   bool IsChromaSamplingSupported(VideoChromaSampling chroma_sampling) override;
 
  private:
+  bool SubmitSliceData();
+  bool RetrieveBitstreamBuffer();
+
   // Gets a pic params struct with the constant fields set.
   void FillPicParamsWithConstants(DXVA_PicParams_HEVC_Rext* pic_param);
 
@@ -183,6 +188,12 @@ class D3D11H265Accelerator : public D3DAccelerator,
   bool use_scaling_lists_ = false;
   // If current stream is encoded with range extension profile.
   bool is_rext_ = false;
+
+  // Information that's accumulated during slices and submitted at the end
+  std::vector<DXVA_Slice_HEVC_Short> slice_info_;
+  size_t current_offset_ = 0;
+  size_t bitstream_buffer_size_ = 0;
+  raw_ptr<uint8_t, AllowPtrArithmetic> bitstream_buffer_bytes_ = nullptr;
 
   // For HEVC this number needs to be larger than 1 and different
   // in each call to Execute().
