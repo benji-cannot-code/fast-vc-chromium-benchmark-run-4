b1c397b867cfaeab7869e71bcdfad16c4496c6e0
Markus Handell <handellm@google.com>
2023-06-02 15:31:40 +0000

    Launch WebRtcCombinedNetworkAndWorkerThread.
    This CL enables the feature default-on after successful
    launch review.
    
    Bug: chromium:1373439
    Change-Id: I5aeccdb993192a2fbf94ec69479f891ce3ccaf0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574693
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152550}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1726,7 +1726,7 @@ BASE_FEATURE(
 
 BASE_FEATURE(kWebRtcCombinedNetworkAndWorkerThread,
              "WebRtcCombinedNetworkAndWorkerThread",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kVSyncDecoding,
              "VSyncDecoding",
