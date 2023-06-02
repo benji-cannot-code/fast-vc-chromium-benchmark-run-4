73ae7483ff22d77b7c35ba4591d14b16ffa47944
James Cook <jamescook@chromium.org>
2023-06-02 16:29:39 +0000

    chromeos: Clean up shelf party code, part 4
    Delete the rest of the code and remove the feature.
    
    Bug: b:284500985
    Test: ash_unittests
    Change-Id: Id37e85b71180bd9377fb7cd7b3f1a48ca2f07972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583353
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152585}

--- ash/shelf/shelf_view.h
+++ ash/shelf/shelf_view.h
@@ -566,17 +566,6 @@ class ASH_EXPORT ShelfView : public views::AccessiblePaneView,
   // and on-going bounds animations on |child|.
   gfx::Rect GetChildViewTargetMirroredBounds(const views::View* child) const;
 
-  // Updates the visibility, position, and transform of
-  // `all_pinned_items_are_partying_label_`.
-  void UpdateAllPinnedItemsArePartyingLabel();
-
-  // Returns true if `all_pinned_items_are_partying_label_` should be visible.
-  bool ShouldShowAllPinnedItemsArePartyingLabel() const;
-
-  // Returns the space occupied by `all_pinned_items_are_partying_label_`,
-  // including the gap between that label and the first item, in DIPs.
-  int AllPinnedItemsArePartyingLabelSpace() const;
-
   // Removes and reset |current_ghost_view| and |last_ghost_view|.
   void RemoveGhostView();
 
@@ -719,10 +708,6 @@ class ASH_EXPORT ShelfView : public views::AccessiblePaneView,
   // A view used to make accessibility announcements (changes in the shelf's
   // alignment or auto-hide state).
   raw_ptr<views::View, ExperimentalAsh> announcement_view_ =
-      nullptr;  // Owned by ShelfView
-
-  // A view used to indicate that all pinned items are partying.
-  raw_ptr<views::View, ExperimentalAsh> all_pinned_items_are_partying_label_ =
       nullptr;  // Owned by ShelfView
 
   // For dragging: -1 if scrolling back, 1 if scrolling forward, 0 if neither.
