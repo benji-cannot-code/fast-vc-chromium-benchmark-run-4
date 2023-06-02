373a9022ecbd0502c8a99ba30b1c5fd5ab12cbbc
Peter Kvitek <kvitekp@chromium.org>
2023-06-02 12:52:44 +0000

    [headless] Avoid native focus changes
    There are reports that new headless may steal keyboard focus, so
    this CL prevents platform window focus changes pretending that
    headless browser window always has focus.
    
    Bug: 1449117
    Change-Id: Ib18bc5d05dd2af5e3dd8549a23e1e5f018c43abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582130
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152473}

--- ui/views/win/hwnd_message_handler.cc
+++ ui/views/win/hwnd_message_handler.cc
@@ -1012,7 +1012,12 @@ void HWNDMessageHandler::FlashFrame(bool flash) {
 }
 
 void HWNDMessageHandler::ClearNativeFocus() {
-  ::SetFocus(hwnd());
+  // Headless windows don't get native focus, so just pretend we grabbed one.
+  if (IsHeadless()) {
+    delegate_->HandleNativeFocus(0);
+  } else {
+    ::SetFocus(hwnd());
+  }
 }
 
 void HWNDMessageHandler::SetCapture() {
@@ -1508,7 +1513,12 @@ void HWNDMessageHandler::SetInitialFocus() {
   if (!(GetWindowLong(hwnd(), GWL_EXSTYLE) & WS_EX_TRANSPARENT) &&
       !(GetWindowLong(hwnd(), GWL_EXSTYLE) & WS_EX_NOACTIVATE)) {
     // The window does not get keyboard messages unless we focus it.
-    SetFocus(hwnd());
+    // Headless windows don't get native focus, so just pretend we grabbed one.
+    if (IsHeadless()) {
+      delegate_->HandleNativeFocus(0);
+    } else {
+      ::SetFocus(hwnd());
+    }
   }
 }
 
@@ -2154,7 +2164,11 @@ LRESULT HWNDMessageHandler::OnKeyEvent(UINT message,
 }
 
 void HWNDMessageHandler::OnKillFocus(HWND focused_window) {
-  delegate_->HandleNativeBlur(focused_window);
+  // Headless windows are believed to always have focus, so avoid
+  // reporting native focus changes.
+  if (!IsHeadless()) {
+    delegate_->HandleNativeBlur(focused_window);
+  }
   SetMsgHandled(FALSE);
 }
 
@@ -2742,7 +2756,11 @@ LRESULT HWNDMessageHandler::OnSetCursor(UINT message,
 }
 
 void HWNDMessageHandler::OnSetFocus(HWND last_focused_window) {
-  delegate_->HandleNativeFocus(last_focused_window);
+  // Headless windows are believed to always have focus, so avoid
+  // reporting native focus changes.
+  if (!IsHeadless()) {
+    delegate_->HandleNativeFocus(last_focused_window);
+  }
   SetMsgHandled(FALSE);
 }
 
