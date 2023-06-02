5aff88a434af1a6bd8304d22075485c1eecd3d98
Elijah Hewer <hewer@chromium.org>
2023-06-02 17:17:03 +0000

    Capture Mode: Add shadow to key item view
    Add a shadow to each key item view in the key combo for the capture
    mode demo tools. Also update CaptureModeBarView and CaptureLabelView
    to set the content bounds of their shadows using `OnBoundsChanged`.
    
    Bug: b/280510835
    Test: manual
    Change-Id: Ie2d2ac12c0315f328e7cfb8591fe09b694ec8f86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4524933
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152612}

--- ash/capture_mode/capture_mode_demo_tools_controller.cc
+++ ash/capture_mode/capture_mode_demo_tools_controller.cc
@@ -321,7 +321,6 @@ void CaptureModeDemoToolsController::RefreshKeyComboViewer() {
         views::Widget::ANIMATE_NONE);
     ui::Layer* layer = key_combo_widget_->GetLayer();
     layer->SetFillsBoundsOpaquely(false);
-    layer->SetMasksToBounds(true);
     key_combo_widget_->Show();
   }
 
--- ash/capture_mode/key_item_view.h
+++ ash/capture_mode/key_item_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_CAPTURE_MODE_KEY_ITEM_VIEW_H_
 #define ASH_CAPTURE_MODE_KEY_ITEM_VIEW_H_
 
+#include "ash/style/system_shadow.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/views/view.h"
@@ -29,6 +30,8 @@ class KeyItemView : public views::View {
   ~KeyItemView() override;
 
   // views::View:
+  void AddedToWidget() override;
+  void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
   void OnThemeChanged() override;
   void Layout() override;
   gfx::Size CalculatePreferredSize() const override;
@@ -43,6 +46,11 @@ class KeyItemView : public views::View {
   const ui::KeyboardCode key_code_;
   raw_ptr<views::ImageView, ExperimentalAsh> icon_ = nullptr;
   raw_ptr<views::Label, ExperimentalAsh> label_ = nullptr;
+
+  // The shadow around each key item UI component in the combo. The shadow
+  // should be `SystemShadowOnTextureLayer` as the corners are perfectly
+  // rounded.
+  std::unique_ptr<SystemShadow> shadow_;
 };
 
 }  // namespace ash
