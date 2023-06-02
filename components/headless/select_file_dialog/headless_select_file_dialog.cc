dbb7af6a43f272accbf99ec4236d355750617b45
Peter Kvitek <kvitekp@chromium.org>
2023-06-02 01:36:13 +0000

    Reland "[headless] Move HeadlessSelectFileDialog to //components/headless"
    This is a reland of commit 6a595119b1ca623023e9ac1ef16cdb8117c7c440
    
    The CL exports global factory instance pointer so that component
    builds will not fail.
    
    Original change's description:
    > [headless] Move HeadlessSelectFileDialog to //components/headless
    >
    > HeadlessSelectFileDialog has to be shared between old and new headless,
    > so this CL moves its existing implementation from //headless/lib to
    > //component/headless.
    >
    > New headless will re-use shared implementation of HeadlessSelectFileDialog in a follow up CL.
    >
    > Bug: 1447911
    > Change-Id: I5ed4eb3dd5387a243b032ab1a7ab31a233d8f4f9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563343
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1149567}
    
    Bug: 1447911
    Change-Id: I3d1c8c87cec148cfdcd00fbd2aa0a76a042ec2c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577498
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152293}

--- components/headless/select_file_dialog/headless_select_file_dialog.cc
+++ components/headless/select_file_dialog/headless_select_file_dialog.cc
@@ -3,17 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "headless/lib/browser/headless_select_file_dialog_factory.h"
+#include "components/headless/select_file_dialog/headless_select_file_dialog.h"
 
-#include "url/gurl.h"
+#include "ui/gfx/native_widget_types.h"
 
 namespace headless {
 
-namespace {
-// SelectFileDialogFactory is a destructable singleton, see
-// SelectFileDialog::SetFactory(), so we must make it disposable.
-HeadlessSelectFileDialogFactory* g_factory_instance = nullptr;
-}  // namespace
+// static
+HeadlessSelectFileDialogFactory* HeadlessSelectFileDialogFactory::instance_ =
+    nullptr;
 
 // HeadlessSelectFileDialog implements a stub select file dialog
 // that cancels itself as soon as it gets open, optionally calling
@@ -47,11 +45,13 @@ class HeadlessSelectFileDialog : public ui::SelectFileDialog {
                       gfx::NativeWindow owning_window,
                       void* params,
                       const GURL* caller) override {
-    if (callback_)
+    if (callback_) {
       std::move(callback_).Run(type);
+    }
 
-    if (listener_)
+    if (listener_) {
       listener_->FileSelectionCanceled(/*params=*/nullptr);
+    }
   }
 
  private:
@@ -71,8 +71,8 @@ void HeadlessSelectFileDialogFactory::SetUp() {
 // static
 void HeadlessSelectFileDialogFactory::SetSelectFileDialogOnceCallbackForTests(
     SelectFileDialogCallback callback) {
-  DCHECK(g_factory_instance);
-  g_factory_instance->callback_ = std::move(callback);
+  DCHECK(instance_);
+  instance_->callback_ = std::move(callback);
 }
 
 ui::SelectFileDialog* HeadlessSelectFileDialogFactory::Create(
@@ -83,13 +83,13 @@ ui::SelectFileDialog* HeadlessSelectFileDialogFactory::Create(
 }
 
 HeadlessSelectFileDialogFactory::HeadlessSelectFileDialogFactory() {
-  DCHECK(!g_factory_instance);
-  g_factory_instance = this;
+  DCHECK(!instance_);
+  instance_ = this;
 }
 
 HeadlessSelectFileDialogFactory::~HeadlessSelectFileDialogFactory() {
-  DCHECK_EQ(g_factory_instance, this);
-  g_factory_instance = nullptr;
+  DCHECK_EQ(instance_, this);
+  instance_ = nullptr;
 }
 
 }  // namespace headless
