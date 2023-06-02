4df28b2c1a6406cdd15adf956d8685fba02e2ced
Andrew Paseltiner <apaseltiner@chromium.org>
2023-06-02 13:07:47 +0000

    Grease Attribution-Reporting-Support header
    We reuse the logic from the Attribution-Reporting-Eligible header
    greasing, and avoid generating multiple random integers by realizing
    that the options require only 8 bits each.
    
    Bug: 1450297
    Change-Id: I90d599fb1c76bf281975063c09df3694b2d58008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4580289
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152478}

--- services/network/attribution/request_headers_internal.h
+++ services/network/attribution/request_headers_internal.h
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace network {
 
 // Options controlling greasing during serialization of the
-// Attribution-Reporting-Eligible header, which contains a structured
-// dictionary.
-struct AttributionReportingEligibleGreaseOptions {
+// Attribution-Reporting-Eligible and Attribution-Reportnig-Support headers,
+// which contain structured dictionaries.
+struct AttributionReportingHeaderGreaseOptions {
   // Where to apply a grease.
   enum class GreaseContext : uint8_t {
     // The grease is not applied.
@@ -30,7 +30,9 @@ struct AttributionReportingEligibleGreaseOptions {
     kParamName = 3,
   };
 
-  static AttributionReportingEligibleGreaseOptions FromBits(uint64_t bits);
+  // Note: This function needs exactly 8 bits. If the size of this structure changes,
+  // `uint8_t` should be changed to a larger type.
+  static AttributionReportingHeaderGreaseOptions FromBits(uint8_t bits);
 
   // Whether to reverse the list of dictionary keys.
   bool reverse = false;
@@ -50,11 +52,13 @@ struct AttributionReportingEligibleGreaseOptions {
 // Must not be called with `mojom::AttributionReportingEligibility::kUnset`.
 std::string SerializeAttributionReportingEligibleHeader(
     mojom::AttributionReportingEligibility,
-    const AttributionReportingEligibleGreaseOptions&);
+    const AttributionReportingHeaderGreaseOptions&);
 
 // Returns the value to be set for the `Attribution-Reporting-Support` request
 // header.
-std::string GetAttributionSupportHeader(mojom::AttributionSupport);
+std::string GetAttributionSupportHeader(
+    mojom::AttributionSupport,
+    const AttributionReportingHeaderGreaseOptions&);
 
 }  // namespace network
 
