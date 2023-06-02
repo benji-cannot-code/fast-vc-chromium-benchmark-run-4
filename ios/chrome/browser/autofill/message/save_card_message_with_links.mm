396e2c64cc8d02312fc82599adf6842b05a186db
Ewann Pelle <ewannpv@chromium.org>
2023-06-02 08:59:52 +0000

    [iOS] Move autofill_message source set to i/c/b/autofill/message
    Change-Id: I2d5c4b5269be1e05a87e9b57a73cd107cc933dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571050
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152416}

--- ios/chrome/browser/autofill/message/save_card_message_with_links.mm
+++ ios/chrome/browser/autofill/message/save_card_message_with_links.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/autofill/save_card_message_with_links.h"
+#import "ios/chrome/browser/autofill/message/save_card_message_with_links.h"
 
 #import "url/gurl.h"
 
--- ios/showcase/infobars/sc_infobar_modal_save_card_coordinator.mm
+++ ios/showcase/infobars/sc_infobar_modal_save_card_coordinator.mm
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/showcase/infobars/sc_infobar_modal_save_card_coordinator.h"
 
+#import "ios/chrome/browser/autofill/message/save_card_message_with_links.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
-#import "ios/chrome/browser/ui/autofill/save_card_message_with_links.h"
 #import "ios/chrome/browser/ui/infobars/banners/infobar_banner_delegate.h"
 #import "ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.h"
 #import "ios/chrome/browser/ui/infobars/modals/infobar_save_card_modal_delegate.h"
