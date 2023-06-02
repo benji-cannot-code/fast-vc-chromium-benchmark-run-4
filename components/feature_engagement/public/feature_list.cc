27cfa935dfefbd2840c47fbe087e078d3814fd84
Viktor Semeniuk <vsemeniuk@google.com>
2023-06-02 15:28:29 +0000

    [Password Manager] Add an IPH for Password Manager Shortcut
    This IPH launches a tutorial for Password Manager Shortcut. The trigger
    will be added in a follow up.
    
    Impl: https://screenshot.googleplex.com/7RSyGfxjVAJC8py
    
    Bug: 1445187
    Change-Id: Idb3279c64d6af4e920c0c8908692e3761984a284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4567733
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152546}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -156,6 +156,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHPasswordsManagementBubbleAfterSaveFeature,
     &kIPHPasswordsManagementBubbleDuringSigninFeature,
     &kIPHPasswordsWebAppProfileSwitchFeature,
+    &kIPHPasswordManagerShortcutFeature,
     &kIPHPerformanceNewBadgeFeature,
     &kIPHPowerBookmarksSidePanelFeature,
     &kIPHPriceTrackingChipFeature,
