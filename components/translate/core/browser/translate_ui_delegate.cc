5cec8f11243ddee2b2b877464d91ef544ec5eb1f
Anthony Cui <cuianthony@google.com>
2023-06-02 21:08:38 +0000

    [Translate metrics cleanup] Replace Translate.ShowErrorUI with Translate.Translation.ShowErrorUI in order to match new naming convention
    OBSOLETE_HISTOGRAM[Translate.ShowErrorUI]=Replaced by Translate.Translation.ShowErrorUI in M116
    
    Bug: 1266978
    Change-Id: Id821b06c1b5053b1df852666dbdfa5f2ca23f8f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4581767
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152749}

--- components/translate/core/browser/translate_ui_delegate.cc
+++ components/translate/core/browser/translate_ui_delegate.cc
@@ -24,7 +24,7 @@ namespace {
 const char kNeverTranslateLang[] = "Translate.NeverTranslateLang";
 const char kNeverTranslateSite[] = "Translate.NeverTranslateSite";
 const char kAlwaysTranslateLang[] = "Translate.AlwaysTranslateLang";
-const char kShowErrorUI[] = "Translate.ShowErrorUI";
+const char kShowErrorUI[] = "Translate.Translation.ShowErrorUI";
 
 }  // namespace
 
