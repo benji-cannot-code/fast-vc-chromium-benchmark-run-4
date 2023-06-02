17cd6e8aaea5d3815b70ce0fc31bd870cd55cee2
Tibor Goldschwendt <tiborg@chromium.org>
2023-06-02 20:08:41 +0000

    [ntp][drive] Update fake data to be more representative
    We oftentimes use fake data to demo modules or make screenshots. This CL
    updates the Drive module fake data to better representative a real Drive
    module.
    
    Change-Id: I617b7ba99b4fdb4857dd0725d184bf8e5849fa19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582017
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152718}

--- chrome/browser/new_tab_page/modules/drive/drive_service.cc
+++ chrome/browser/new_tab_page/modules/drive/drive_service.cc
@@ -100,7 +100,7 @@ constexpr char kFakeData[] = R"({
       "itemId": "foo",
       "url": "https://docs.google.com",
       "driveItem": {
-        "title": "foo doc",
+        "title": "Drive Module Design Doc",
         "mimeType": "application/vnd.google-apps.document"
       },
       "justification": {
@@ -113,7 +113,7 @@ constexpr char kFakeData[] = R"({
       "itemId": "bar",
       "url": "https://sheets.google.com",
       "driveItem": {
-        "title": "bar sheet",
+        "title": "Monthly Presentation Schedule",
         "mimeType": "application/vnd.google-apps.spreadsheet"
       },
       "justification": {
@@ -126,7 +126,7 @@ constexpr char kFakeData[] = R"({
       "itemId": "baz",
       "url": "https://slides.google.com",
       "driveItem": {
-        "title": "baz slides",
+        "title": "Cutest Kittens on the Web",
         "mimeType": "application/vnd.google-apps.presentation"
       },
       "justification": {
