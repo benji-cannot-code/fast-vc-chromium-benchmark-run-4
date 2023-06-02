a1ee88c5c1232555f469d212ee55ab4d5adfebb5
Adam Psarouthakis <psarouthakis@google.com>
2023-06-02 18:54:57 +0000

    Settings: Safety Check Extensions Handler
    This CL updates the the safety check extensions handler to
    use the CWSInfo service to get real values and supply them to
    the front end. It updates unit tests to reflect these changes.
    
    Bug: 1432194
    Change-Id: I78dc0f77239471c82039dd93e9df173f4874e6ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523290
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152669}

--- chrome/browser/ui/webui/settings/safety_check_extensions_handler.h
+++ chrome/browser/ui/webui/settings/safety_check_extensions_handler.h
@@ -10,24 +10,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 
+class Profile;
+
+namespace extensions {
+class CWSInfoService;
+class SafetyCheckExtensionsHandlerTest;
+}  // namespace extensions
+
 namespace settings {
 
 // Settings page UI handler that checks for any extensions that trigger
 // a review by the safety check.
 class SafetyCheckExtensionsHandler : public settings::SettingsPageUIHandler {
  public:
-  SafetyCheckExtensionsHandler();
+  explicit SafetyCheckExtensionsHandler(Profile* profile);
   ~SafetyCheckExtensionsHandler() override;
 
- protected:
-  // Return the display string that represents how many extensions
-  // need to be reviewed by the user.
-  int GetNumberOfExtensionsThatNeedReview();
+  void SetCWSInfoServiceForTest(extensions::CWSInfoService* cws_info_service);
 
  private:
+  friend class extensions::SafetyCheckExtensionsHandlerTest;
+
   // Calculate the number of extensions that need to be reviewed by the
   // user.
   void HandleGetNumberOfExtensionsThatNeedReview(const base::Value::List& args);
+
+  // Return the number of extensions that should be reviewed by the user.
+  // There are currently three triggers the `SafetyCheckExtensionsHandler`
+  // tracks:
+  // -- Extension Malware Violation
+  // -- Extension Policy Violation
+  // -- Extension Unpublished by the developer
+  int GetNumberOfExtensionsThatNeedReview();
 
   // SettingsPageUIHandler implementation.
   void OnJavascriptDisallowed() override;
@@ -36,6 +50,8 @@ class SafetyCheckExtensionsHandler : public settings::SettingsPageUIHandler {
   // WebUIMessageHandler implementation.
   void RegisterMessages() override;
 
+  raw_ptr<Profile> profile_ = nullptr;
+  raw_ptr<extensions::CWSInfoService> cws_info_service_ = nullptr;
   base::WeakPtrFactory<SafetyCheckExtensionsHandler> weak_ptr_factory_{this};
 };
 
