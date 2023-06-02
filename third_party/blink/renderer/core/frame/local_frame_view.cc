cd54adbed58d1b6f9ec8923c44f83cbe378ff68a
Aaron Leventhal <aleventhal@google.com>
2023-06-02 18:15:06 +0000

    Handle location changes in about:blank documents
    Instead of suppressing location changes in about:blank, use IsInitialEmptyDocument(), which is smarter and looks for any content.
    
    Fixed: 1434520
    Change-Id: I9b1d0b034fedd9e02225a7d1520adeb466bafe5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583132
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152647}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -1718,9 +1718,7 @@ void LocalFrameView::PerformPostLayoutTasks(bool visual_viewport_size_changed) {
   DCHECK(document);
   if (AXObjectCache* cache = document->ExistingAXObjectCache()) {
     const KURL& url = document->Url();
-    if (url.IsValid() && !url.IsAboutBlankURL()) {
-      // TODO(kschmi) move HandleLayoutComplete to the accessibility lifecycle
-      // stage. crbug.com/1062122
+    if (url.IsValid()) {
       cache->HandleLayoutComplete(document);
     }
   }
