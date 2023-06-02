265cbb9e3bcb13cda9425f84094a06a6b54edb38
Florian Jacky <fjacky@chromium.org>
2023-06-02 16:28:33 +0000

    Flow refactoring to address confirmation chip stickiness
    This CL forwards tab visibility update events from the PRM to the chip_controller instead of using location bar view update events. This simplifies event handling and avoids event order dependencies.
    
    Bug: 1450815
    
    Change-Id: I5a591c4317e3be7bdf7cb5e71e00a1e8f3287393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570985
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Auto-Submit: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152583}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -855,10 +855,6 @@ void LocationBarView::Update(WebContents* contents) {
   RefreshPageActionIconViews();
   location_icon_view_->Update(/*suppress_animations=*/contents);
 
-  if (is_initialized_ && chip_controller_) {
-    chip_controller_->OnWebContentsChanged();
-  }
-
   if (intent_chip_)
     intent_chip_->Update();
 
@@ -1573,7 +1569,7 @@ void LocationBarView::UpdateChipVisibility() {
   if (IsEditingOrEmpty()) {
     // If a user starts typing, a permission request should be ignored and the
     // chip finalized.
-    chip_controller_->ResetChip();
+    chip_controller_->ResetPermissionPromptChip();
   }
 }
 
