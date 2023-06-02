67c269fa24868b320484191bbddb48bb6a319956
Simon Zünd <szuend@chromium.org>
2023-06-02 10:48:21 +0000

    [devtools] Import ConsoleTestRunner in 3 tests that are missing it
    R=jarin@chromium.org
    
    Bug: 1442410
    Change-Id: I8714123199accb96f98a7d27344e70bfad28bd8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584291
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152444}

--- third_party/blink/web_tests/http/tests/devtools/indexeddb/database-data.js
+++ third_party/blink/web_tests/http/tests/devtools/indexeddb/database-data.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {TestRunner} from 'test_runner';
 import {ApplicationTestRunner} from 'application_test_runner';
+import {ConsoleTestRunner} from 'console_test_runner';
 
 (async function() {
   TestRunner.addResult(
