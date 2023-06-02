41fdfae86179553500b1622d73aff0fae63f9b53
Alexis Hétu <sugoi@chromium.org>
2023-06-02 17:33:30 +0000

    [iOS] Fix Password Bottom Sheet related EG tests
    This CL fixes 2 tests:
    1) testUpdatePromptAppearsOnFormSubmission: This should only run
       with kIOSPasswordBottomSheet enabled. Made sure this was done
       in the test's setup.
    2) testOpenPasswordBottomSheetDeletePassword: The test was missing
       a reauth mock for the password deletion action.
    
    Bug: 1448911, 1422341
    Change-Id: I6dd367e963bd929381ae2729f07459b8fed37dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582811
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152624}

--- ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_egtest.mm
+++ ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_egtest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_app_interface.h"
 #import "ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_constants.h"
+#import "ios/chrome/browser/ui/settings/password/password_settings_app_interface.h"
 #import "ios/chrome/browser/ui/settings/password/passwords_table_view_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
@@ -367,6 +368,10 @@ id<GREYMatcher> DeleteConfirmationButton() {
                      grey_interactable(), nullptr)] performAction:grey_tap()];
 
   [ChromeEarlGreyUI waitForAppToIdle];
+
+  [PasswordSettingsAppInterface setUpMockReauthenticationModule];
+  [PasswordSettingsAppInterface mockReauthenticationModuleExpectedResult:
+                                    ReauthenticationResult::kSuccess];
 
   [[EarlGrey selectElementWithMatcher:NavigationBarEditButton()]
       performAction:grey_tap()];
