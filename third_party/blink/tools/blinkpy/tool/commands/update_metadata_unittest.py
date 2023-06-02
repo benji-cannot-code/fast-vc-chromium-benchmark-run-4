2d76751a60033c637aa960f1d88f716fc309d867
Jonathan Lee <jonathanjlee@google.com>
2023-06-02 18:33:07 +0000

    [wpt-metadata] Fix orphan cleanup for multiple tests
    Cleaning up multiple orphaned tests corrupted the `ExpectedManifest`
    tree because that removed nodes from a list of children while it was
    being iterated. As a result, the cleanup could be incomplete:
      https://ci.chromium.org/ui/p/chromium/builders/try/chromium_presubmit/2201880/overview
    
    Iterate over a copy instead.
    
    Bug: 1449378
    Change-Id: Ic6420b599f7dc4f3cd1a91fda077907fd757a7d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583637
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152651}

--- third_party/blink/tools/blinkpy/tool/commands/update_metadata_unittest.py
+++ third_party/blink/tools/blinkpy/tool/commands/update_metadata_unittest.py
@@ -736,6 +736,9 @@ class UpdateMetadataASTSerializationTest(BaseUpdateMetadataTest):
               expected: ERROR
               [subtest]
                 expected: FAIL
+
+            [variant.html?also-does-not-exist]
+              expected: ERROR
             """)
         self.update({
             'results': [{
