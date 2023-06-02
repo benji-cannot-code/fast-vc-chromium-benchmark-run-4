0504ce6b824eeaf6ea2aff91dd58e78fff310095
Luca Accorsi <accorsi@google.com>
2023-06-02 14:01:39 +0000

    dlp: Gray out OneDrive open entry
    With this CL we gray out the OneDrive entry from the Open button in the file app and from the context menu if among the selected files there is at least one for which the upload to OneDrive is restricted.
    
    Bug: b/282131129
    Test: manually tested, right now OneDrive is not enabled for managed users.
    
    Change-Id: I6ea6f37b5480cd7973021db699a823ae35ee488d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4538701
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152497}

--- chrome/browser/ash/file_manager/file_tasks.cc
+++ chrome/browser/ash/file_manager/file_tasks.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_system_provider/provided_file_system_interface.h"
 #include "chrome/browser/ash/file_system_provider/service.h"
 #include "chrome/browser/ash/fileapi/file_system_backend.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_files_utils.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -356,6 +357,7 @@ void ExecuteTaskAfterMimeTypesCollected(
 
 void PostProcessFoundTasks(Profile* profile,
                            const std::vector<extensions::EntryInfo>& entries,
+                           const std::vector<std::string>& dlp_source_urls,
                            FindTasksCallback callback,
                            std::unique_ptr<ResultingTasks> resulting_tasks) {
   AdjustTasksForMediaApp(entries, &resulting_tasks->tasks);
@@ -398,6 +400,11 @@ void PostProcessFoundTasks(Profile* profile,
       FullTaskDescriptor office_task(*it);
       office_task.task_descriptor.action_id =
           base::StrCat({kChromeUIFileManagerURL, "?", kActionIdOpenInOffice});
+      // A transfer to OneDrive is required for the Office PWA to open files, if
+      // transferring files to OneDrive is restricted, we gray out the
+      // corresponding task.
+      office_task.is_dlp_blocked = policy::dlp::IsFilesTransferBlocked(
+          dlp_source_urls, data_controls::Component::kOneDrive);
       resulting_tasks->tasks.push_back(office_task);
     }
   }
@@ -1075,7 +1082,7 @@ void FindExtensionAndAppTasks(Profile* profile,
   FindAppServiceTasks(profile, entries, file_urls, dlp_source_urls, tasks);
 
   // Done. Apply post-filtering and callback.
-  PostProcessFoundTasks(profile, entries, std::move(callback),
+  PostProcessFoundTasks(profile, entries, dlp_source_urls, std::move(callback),
                         std::move(resulting_tasks));
 }
 
--- chrome/browser/chromeos/policy/dlp/dlp_files_utils.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_files_utils.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/policy/dlp/dlp_files_utils.h"
 
+#include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
+
 namespace policy {
 namespace dlp {
 
@@ -26,6 +29,34 @@ namespace dlp {
     case data_controls::Component::kOneDrive:
       return ::dlp::DlpComponent::MICROSOFT_ONEDRIVE;
   }
+}
+
+bool IsFilesTransferBlocked(const std::vector<std::string>& sources,
+                            data_controls::Component component) {
+  // Primary profile restrictions are enforced across all profiles.
+  policy::DlpRulesManager* rules_manager =
+      policy::DlpRulesManagerFactory::GetForPrimaryProfile();
+  if (!rules_manager) {
+    return false;
+  }
+
+  for (const auto& src : sources) {
+    // Non managed files have an empty source URL.
+    if (src.empty()) {
+      continue;
+    }
+
+    std::string out_src_pattern;
+    policy::DlpRulesManager::RuleMetadata out_rule_metadata;
+    if (rules_manager->IsRestrictedComponent(
+            GURL(src), component,
+            policy::DlpRulesManagerBase::Restriction::kFiles, &out_src_pattern,
+            &out_rule_metadata) == policy::DlpRulesManager::Level::kBlock) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 }  // namespace dlp
