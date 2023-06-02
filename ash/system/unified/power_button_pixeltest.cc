d79275fa14ad4298bdd347cebaa4830bc573ce95
Alex Newcomer <newcomer@google.com>
2023-06-02 17:52:21 +0000

    cros: Update 'user icon' to an icon with no hair
    UX prefers the more neutral look
    
    Screenshot: https://screenshot.googleplex.com/3VGcrnkAy773oBg.png
    
    Bug: b:284330163
    Change-Id: I28e4f09048be8760491c4b099fdf45262beb27f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568213
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152631}

--- ash/system/unified/power_button_pixeltest.cc
+++ ash/system/unified/power_button_pixeltest.cc
@@ -81,7 +81,7 @@ TEST_F(PowerButtonPixelTest, LoginSession) {
   SimulatePowerButtonPress();
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "check_menu",
-      /*revision_number=*/0, GetMenuView()));
+      /*revision_number=*/1, GetMenuView()));
 }
 
 TEST_F(PowerButtonPixelTest, LockScreenSession) {
@@ -95,7 +95,7 @@ TEST_F(PowerButtonPixelTest, LockScreenSession) {
   SimulatePowerButtonPress();
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "check_menu",
-      /*revision_number=*/0, GetMenuView()));
+      /*revision_number=*/1, GetMenuView()));
 }
 
 TEST_F(PowerButtonPixelTest, GuestMode) {
