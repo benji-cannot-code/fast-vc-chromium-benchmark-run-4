8e20b3ff59c4ae883089ecddbc31f146aa0b12a8
mikt <mikt@google.com>
2023-06-02 02:51:50 +0000

    [BRP] Automatic rewrite to raw_ptr on Mac (June 1st)
    - This will rewrite one raw pointer usage landed after last week's
      rewrite.
      - crrev.com/c/4557741
    
    Bug: 1273182
    Change-Id: I2113c4b724d6af7c06530ec4adb2a0beb4fcb3d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4578003
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1152331}

--- media/gpu/mac/video_toolbox_frame_converter.h
+++ media/gpu/mac/video_toolbox_frame_converter.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/mac/scoped_cftyperef.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted_delete_on_sequence.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
@@ -68,9 +69,9 @@ class VideoToolboxFrameConverter
   GetCommandBufferStubCB get_stub_cb_;
   bool initialized_ = false;
 
-  gpu::CommandBufferStub* stub_ = nullptr;
+  raw_ptr<gpu::CommandBufferStub> stub_ = nullptr;
   gpu::SequenceId wait_sequence_id_;
-  gpu::SharedImageStub* sis_ = nullptr;
+  raw_ptr<gpu::SharedImageStub> sis_ = nullptr;
   bool texture_rectangle_ = false;
 };
 
