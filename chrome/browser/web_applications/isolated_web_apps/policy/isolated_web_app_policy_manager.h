0c136c4ff913fb09dd38783bfcae3054686086e1
Christian Flach <cmfcmf@chromium.org>
2023-06-02 10:54:05 +0000

    iwa: Extract Web Bundle downloading from policy management
    We will need to download Web Bundles for Isolated Web Apps as part of
    the upcoming support for automatic IWA updates.
    
    This CL is purely a refactoring and does not include any functional
    changes. As part of splitting the network traffic annotation, I've also
    slightly improved its wording.
    
    Bug: 1444407
    Change-Id: Icb7e410bb36f138fb3f3645482621fed3bd5f29b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4579927
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152446}

--- chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_manager.h
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_manager.h
@@ -24,12 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace network {
 class SharedURLLoaderFactory;
-class SimpleURLLoader;
 }  // namespace network
 
 namespace web_app {
 
 class UpdateManifest;
+class IsolatedWebAppDownloader;
 
 // This component is responsible for installing, uninstalling, updating etc.
 // of the policy installed IWAs.
@@ -120,9 +120,7 @@ class IsolatedWebAppPolicyManager {
 
   // Downloading of the Signed Web Bundle.
   void DownloadWebBundle();
-  void OnWebBundleDownloaded(
-      std::unique_ptr<network::SimpleURLLoader> simple_loader,
-      base::FilePath path);
+  void OnWebBundleDownloaded(const base::FilePath& path, int32_t net_error);
 
   // Installing of the IWA using the downloaded Signed Web Bundle.
   void InstallIwa(base::FilePath path);
@@ -144,6 +142,7 @@ class IsolatedWebAppPolicyManager {
       ephemeral_iwa_install_options_;
   std::vector<IsolatedWebAppExternalInstallOptions>::iterator current_app_;
   std::unique_ptr<UpdateManifestFetcher> current_update_manifest_fetcher_;
+  std::unique_ptr<IsolatedWebAppDownloader> current_bundle_downloader_;
 
   const base::FilePath installation_dir_;
 
