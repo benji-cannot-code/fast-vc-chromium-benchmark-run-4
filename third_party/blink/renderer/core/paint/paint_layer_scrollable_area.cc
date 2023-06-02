35bc112b6262ad0197b9c7f3368a7ae4d5a3ffae
Kent Tamura <tkent@chromium.org>
2023-06-02 15:01:13 +0000

    Revert "Rename ScrollOrigin() to ScrollOriginInt()"
    This reverts commit 4fa921dfa13c0679713ecec50852f43c94501457.
    
    Reason for revert: I turned out that this CL was unnecessary.
    
    Original change's description:
    > Rename ScrollOrigin() to ScrollOriginInt()
    >
    > This is a preparation to add a LayoutUnit-version of ScrollOrigin().
    > This CL has no behavior changes.
    >
    > Bug: 1450757
    > Change-Id: Iaae84e75c6c26e07a9533ca674d5fd21ddc8b66b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583467
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1152320}
    
    Bug: 1450757
    Change-Id: I2f3b803d52d03d161f69fed7c011a6796b0ed53a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583196
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1152530}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -601,12 +601,12 @@ void PaintLayerScrollableArea::EnqueueScrollEventIfNeeded() {
 }
 
 gfx::Vector2d PaintLayerScrollableArea::MinimumScrollOffsetInt() const {
-  return -ScrollOriginInt().OffsetFromOrigin();
+  return -ScrollOrigin().OffsetFromOrigin();
 }
 
 gfx::Vector2d PaintLayerScrollableArea::MaximumScrollOffsetInt() const {
   if (!GetLayoutBox() || !GetLayoutBox()->IsScrollContainer())
-    return -ScrollOriginInt().OffsetFromOrigin();
+    return -ScrollOrigin().OffsetFromOrigin();
 
   gfx::Size content_size = ContentsSize();
 
@@ -633,7 +633,7 @@ gfx::Vector2d PaintLayerScrollableArea::MaximumScrollOffsetInt() const {
   // based on stale layout overflow data (http://crbug.com/576933).
   content_size.SetToMax(visible_size);
 
-  return -ScrollOriginInt().OffsetFromOrigin() +
+  return -ScrollOrigin().OffsetFromOrigin() +
          gfx::Vector2d(content_size.width() - visible_size.width(),
                        content_size.height() - visible_size.height());
 }
@@ -673,8 +673,7 @@ PhysicalRect PaintLayerScrollableArea::VisibleScrollSnapportRect(
     IncludeScrollbarsInRect scrollbar_inclusion) const {
   const ComputedStyle* style = GetLayoutBox()->Style();
   PhysicalRect layout_content_rect(LayoutContentRect(scrollbar_inclusion));
-  layout_content_rect.Move(
-      PhysicalOffset(-ScrollOriginInt().OffsetFromOrigin()));
+  layout_content_rect.Move(PhysicalOffset(-ScrollOrigin().OffsetFromOrigin()));
   NGPhysicalBoxStrut padding(
       MinimumValueForLength(style->ScrollPaddingTop(),
                             layout_content_rect.Height()),
@@ -909,12 +908,12 @@ void PaintLayerScrollableArea::UpdateScrollOrigin() {
   gfx::Point new_origin =
       ToFlooredPoint(-scrollable_overflow.offset) +
       ToFlooredVector2d(GetLayoutBox()->OriginAdjustmentForScrollbars());
-  if (new_origin != scroll_origin_int_) {
+  if (new_origin != scroll_origin_) {
     scroll_origin_changed_ = true;
     // ScrollOrigin affects paint offsets of the scrolling contents.
     GetLayoutBox()->SetSubtreeShouldCheckForPaintInvalidation();
   }
-  scroll_origin_int_ = new_origin;
+  scroll_origin_ = new_origin;
 }
 
 void PaintLayerScrollableArea::UpdateScrollDimensions() {
