0aa389e830d18051bbeed6760267dbab62621fce
Shakti Sahu <shaktisahu@chromium.org>
2023-06-02 08:28:24 +0000

    [CSC] Auto-refresh on sign-in state change
    This CL
    1. Disables auto-refresh on tab visibility change.
    2. Auto-refreshes whenever the user's sign-in state change.
    We ignore the the sync state changes as it is already observed
    through unified consent manager observer.
    
    Bug: b/277800496
    Change-Id: I8bfe4ddf182bfb97f009672db37137d15a0fe981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4581841
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152401}

--- chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
+++ chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/companion/core/constants.h"
 #include "chrome/browser/companion/core/mojom/companion.mojom.h"
 #include "chrome/browser/ui/side_panel/side_panel_enums.h"
 #include "components/lens/buildflags.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -33,6 +35,7 @@ class SigninDelegate;
 class CompanionPageHandler
     : public side_panel::mojom::CompanionPageHandler,
       public content::WebContentsObserver,
+      public signin::IdentityManager::Observer,
       public unified_consent::UrlKeyedDataCollectionConsentHelper::Observer {
  public:
   CompanionPageHandler(
@@ -66,7 +69,10 @@ class CompanionPageHandler
   // content::WebContentsObserver overrides.
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
-  void OnVisibilityChanged(content::Visibility visibility) override;
+
+  // IdentityManager::Observer overrides.
+  void OnPrimaryAccountChanged(
+      const signin::PrimaryAccountChangeEvent& event) override;
 
   // UrlKeyedDataCollectionConsentHelper::Observer overrides.
   void OnUrlKeyedDataCollectionConsentStateChanged(
@@ -96,10 +102,6 @@ class CompanionPageHandler
   void DidFinishFindingCqTexts(
       const std::vector<std::pair<std::string, bool>>& text_found_vec);
 
-  // Helper method to determine whether the user has met all the access
-  // requirements, i.e. signed in, msbb enabled, and has exps access.
-  bool MeetsAllAccessRequirements();
-
   mojo::Receiver<side_panel::mojom::CompanionPageHandler> receiver_;
   mojo::Remote<side_panel::mojom::CompanionPage> page_;
   raw_ptr<CompanionSidePanelUntrustedUI> companion_untrusted_ui_ = nullptr;
@@ -114,6 +116,15 @@ class CompanionPageHandler
 
   // The current URL of the main frame.
   GURL page_url_;
+
+  // Observers for sign-in and MSBB status.
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observation_{this};
+  base::ScopedObservation<
+      unified_consent::UrlKeyedDataCollectionConsentHelper,
+      unified_consent::UrlKeyedDataCollectionConsentHelper::Observer>
+      consent_helper_observation_{this};
 
   base::WeakPtrFactory<CompanionPageHandler> weak_ptr_factory_{this};
 };
