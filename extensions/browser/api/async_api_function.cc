d1f1f2862407fb7db76b79549e65e6528c9e6641
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-06-03 00:01:12 +0000

    [Extensions] Cleanup unused fields on AsyncApiFunction
    AsyncApiFunction should go away entirely. Clean up some low-hanging
    fruit in unused fields.
    
    This CL has no behavior change whatsoever.
    
    Bug: 1450946
    Change-Id: Ib4c1b0aff6c369fe5e1603d543a9d9a6eee4b115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583938
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152845}

--- extensions/browser/api/async_api_function.cc
+++ extensions/browser/api/async_api_function.cc
@@ -25,7 +25,7 @@ AsyncApiFunction::~AsyncApiFunction() = default;
 bool AsyncApiFunction::RunAsync() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  if (!PrePrepare() || !Prepare()) {
+  if (!Prepare()) {
     return false;
   }
   bool rv = work_task_runner_->PostTask(
@@ -34,16 +34,7 @@ bool AsyncApiFunction::RunAsync() {
   return true;
 }
 
-bool AsyncApiFunction::PrePrepare() {
-  return true;
-}
-
 void AsyncApiFunction::Work() {}
-
-void AsyncApiFunction::AsyncWorkStart() {
-  Work();
-  AsyncWorkCompleted();
-}
 
 // static
 bool AsyncApiFunction::ValidationFailure(AsyncApiFunction* function) {
@@ -72,10 +63,6 @@ void AsyncApiFunction::SetResult(base::Value result) {
   results_->Append(std::move(result));
 }
 
-void AsyncApiFunction::SetResultList(base::Value::List results) {
-  results_ = std::move(results);
-}
-
 void AsyncApiFunction::SetError(const std::string& error) {
   error_ = error;
 }
@@ -86,7 +73,8 @@ const std::string& AsyncApiFunction::GetError() const {
 
 void AsyncApiFunction::WorkOnWorkThread() {
   DCHECK(work_task_runner_->RunsTasksInCurrentSequence());
-  AsyncWorkStart();
+  Work();
+  AsyncWorkCompleted();
 }
 
 void AsyncApiFunction::RespondOnUIThread() {
