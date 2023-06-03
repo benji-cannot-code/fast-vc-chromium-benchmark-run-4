02aa1025375388ccdd20d19a94584f5e13ed02f2
Moe Ahmadi <mahmadi@chromium.org>
2023-06-03 00:31:43 +0000

    [omnibox][webui] Fixes Action selection + simplifications
    Actions can be selected in the WebUI Omnibox Popup via tab navigation
    in the omnibox input, albeit the focus indicator is not updated
    accordingly. However, Actions cannot be selected via mouse click.
    
    In the former case, OmniboxEditModel keeps track of the popup selection.
    In the latter case the popup selection is synthesized in RealboxHandler
    and communicated to OmniboxEditModel. However RealboxHandler always
    creates its own instance of the ACController which is different from the
    instance owned by the OmniboxEditModel which powers the Omnibox.
    
    This CL makes the RealboxHandler use the same OmniboxController - and
    thus the same OmniboxEditModel and ACController instances - as the
    omnibox when being used in the context of the WebUI omnibox popup.
    
    Additionally, this CL makes a bunch of code simplifications such as
    unifying the mojom Page interface between the realbox and omnibox popup,
    eliminating the use of AutocompleteControllerEmitter in favor of direct
    observation of ACController, and removing unused variables.
    
    Bug: 1442068
    Change-Id: Ia181873bc0bbd370f1356f1689c7c231a3ffbe9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4585009
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1152857}

--- chrome/browser/ui/search/omnibox_utils.cc
+++ chrome/browser/ui/search/omnibox_utils.cc
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace search {
 
 OmniboxView* GetOmniboxView(content::WebContents* web_contents) {
-  Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
+  return GetOmniboxView(chrome::FindBrowserWithWebContents(web_contents));
+}
+
+OmniboxView* GetOmniboxView(Browser* browser) {
   if (!browser)
     return nullptr;
   return browser->window()->GetLocationBar()->GetOmniboxView();
--- chrome/browser/ui/webui/realbox/realbox_handler.h
+++ chrome/browser/ui/webui/realbox/realbox_handler.h
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
-#include "base/time/time.h"
 #include "components/omnibox/browser/autocomplete_controller.h"
-#include "components/omnibox/browser/autocomplete_controller_emitter.h"
 #include "components/omnibox/browser/location_bar_model.h"
 #include "components/omnibox/browser/omnibox.mojom.h"
 #include "components/url_formatter/spoof_checks/idna_metrics.h"
@@ -59,7 +57,8 @@ class RealboxHandler : public omnibox::mojom::PageHandler,
       mojo::PendingReceiver<omnibox::mojom::PageHandler> pending_page_handler,
       Profile* profile,
       content::WebContents* web_contents,
-      MetricsReporter* metrics_reporter);
+      MetricsReporter* metrics_reporter,
+      bool is_omnibox_popup_handler);
 
   RealboxHandler(const RealboxHandler&) = delete;
   RealboxHandler& operator=(const RealboxHandler&) = delete;
@@ -126,12 +125,12 @@ class RealboxHandler : public omnibox::mojom::PageHandler,
 
   raw_ptr<Profile> profile_;
   raw_ptr<content::WebContents> web_contents_;
-  std::unique_ptr<OmniboxController> controller_;
-  base::ScopedObservation<AutocompleteControllerEmitter,
-                          AutocompleteController::Observer>
-      controller_emitter_observation_{this};
-  base::TimeTicks time_user_first_modified_realbox_;
   raw_ptr<MetricsReporter> metrics_reporter_;
+  raw_ptr<OmniboxController> controller_;
+  std::unique_ptr<OmniboxController> owned_controller_;
+  base::ScopedObservation<AutocompleteController,
+                          AutocompleteController::Observer>
+      autocomplete_controller_observation_{this};
 
   mojo::Remote<omnibox::mojom::Page> page_;
   mojo::Receiver<omnibox::mojom::PageHandler> page_handler_;
