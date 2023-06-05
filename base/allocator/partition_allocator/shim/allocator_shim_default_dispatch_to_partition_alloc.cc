52ea90716d852d39f50ab0c004ea0209ea3122c2
Yuki Shiino <yukishiino@chromium.org>
2023-06-05 07:06:45 +0000

    PA: Remove deps to build/chromecast_buildflags.h in allocator shims
    Bug: 1337681
    Change-Id: I3eb0b3783747a7d1c946fccbb6f4d06d474b8493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584502
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153096}

--- base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
+++ base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/partition_alloc_features.h"
 #include "base/allocator/partition_allocator/allocation_guard.h"
+#include "base/allocator/partition_allocator/chromecast_buildflags.h"
 #include "base/allocator/partition_allocator/memory_reclaimer.h"
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/allocator/partition_allocator/partition_alloc_base/bits.h"
@@ -28,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/shim/allocator_shim_internals.h"
 #include "base/memory/nonscannable_memory.h"
 #include "build/build_config.h"
-#include "build/chromecast_buildflags.h"
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include <malloc.h>
@@ -363,11 +363,11 @@ void* PartitionRealloc(const AllocatorDispatch*,
       partition_alloc::AllocFlags::kNoHooks | g_alloc_flags, address, size, "");
 }
 
-#if BUILDFLAG(IS_CAST_ANDROID)
+#if BUILDFLAG(PA_IS_CAST_ANDROID)
 extern "C" {
 void __real_free(void*);
 }  // extern "C"
-#endif  // BUILDFLAG(IS_CAST_ANDROID)
+#endif  // BUILDFLAG(PA_IS_CAST_ANDROID)
 
 void PartitionFree(const AllocatorDispatch*, void* object, void* context) {
   partition_alloc::ScopedDisallowAllocations guard{};
@@ -387,7 +387,7 @@ void PartitionFree(const AllocatorDispatch*, void* object, void* context) {
   // malloc() pointer can be passed to PartitionAlloc's free(). If we don't own
   // the pointer, pass it along. This should not have a runtime cost vs regular
   // Android, since on Android we have a PA_CHECK() rather than the branch here.
-#if BUILDFLAG(IS_CAST_ANDROID)
+#if BUILDFLAG(PA_IS_CAST_ANDROID)
   if (PA_UNLIKELY(!partition_alloc::IsManagedByPartitionAlloc(
                       reinterpret_cast<uintptr_t>(object)) &&
                   object)) {
@@ -396,7 +396,7 @@ void PartitionFree(const AllocatorDispatch*, void* object, void* context) {
     // here.
     return __real_free(object);
   }
-#endif  // BUILDFLAG(IS_CAST_ANDROID)
+#endif  // BUILDFLAG(PA_IS_CAST_ANDROID)
 
   partition_alloc::ThreadSafePartitionRoot::FreeNoHooks(object);
 }
