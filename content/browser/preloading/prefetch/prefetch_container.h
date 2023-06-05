973a638ae8434a1360ab83b7bb95dbc7b3ae6be1
Ali Hijazi <ahijazi@chromium.org>
2023-06-05 18:13:41 +0000

    Rewrite T& field into const raw_ref<T>
    These changes were identified while enabling the raw_ref enforcement
    plugin.
    
    Bug: 1446146
    Change-Id: Ie9ab9c0bb5c6b98569902f891c6ff3c2ab568929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584710
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153351}

--- content/browser/preloading/prefetch/prefetch_container.h
+++ content/browser/preloading/prefetch/prefetch_container.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "content/browser/preloading/prefetch/prefetch_probe_result.h"
@@ -307,7 +308,7 @@ class CONTENT_EXPORT PrefetchContainer {
 
     // Currently the lifetime of `Reader` and `PrefetchContainer` are the same
     // and thus this reference is always valid as long as `Reader` is valid.
-    PrefetchContainer& prefetch_container_;
+    const raw_ref<PrefetchContainer> prefetch_container_;
 
     // The index of the element in |prefetch_container_.redirect_chain_| that
     // can be served.
