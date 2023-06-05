09b7f757a3f7cfd1eaeeb08a6983386b9b3f1609
Jong Ahn <jongahn@google.com>
2023-06-05 22:48:29 +0000

    Apply processors in the recorder.
    The processors were previously applied to the provider, which means that it never triggers in the new service.
    
    Change-Id: I880f0d47a22025217938ea31c8eb249d8fe61181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583527
    Quick-Run: Andrew Bregger <andrewbregger@google.com>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153530}

--- components/metrics/structured/structured_metrics_recorder.cc
+++ components/metrics/structured/structured_metrics_recorder.cc
@@ -128,6 +128,9 @@ void StructuredMetricsRecorder::ProvideEventMetrics(
   LogUploadSizeBytes(structured_data->ByteSizeLong());
   LogExternalMetricsScanInUpload(external_metrics_scans_);
   external_metrics_scans_ = 0;
+
+  // Applies custom metadata providers.
+  Recorder::GetInstance()->OnProvideIndependentMetrics(&uma_proto);
 }
 
 void StructuredMetricsRecorder::OnKeyDataInitialized() {
