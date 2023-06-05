01eb428bf3ffcf993f8c283d62d46e1f9c43779c
François Degros <fdegros@chromium.org>
2023-06-05 06:03:17 +0000

    SpacedClient: Add SpacedClient::Observer
    The `spaced` daemon periodically retrieves the free disk space every 5
    seconds on ChromeOS. This signal is being passed by the DBus client but,
    so far, was not being picked up on the Chrome side. This plumbs it
    through and introduces SpacedClient::Observer to listen to the changes.
    
    Bug: b:283337972
    Change-Id: Ib5ebdcd1915b36668cb023dafe19a48eac33ac9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584496
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153085}

--- chromeos/ash/components/dbus/spaced/spaced_client.cc
+++ chromeos/ash/components/dbus/spaced/spaced_client.cc
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/cros_system_api/dbus/spaced/dbus-constants.h"
 
 namespace ash {
-
 namespace {
+
+using spaced::kSpacedInterface;
 
 SpacedClient* g_instance = nullptr;
 
@@ -32,8 +33,8 @@ class SpacedClientImpl : public SpacedClient {
 
   void GetFreeDiskSpace(const std::string& path,
                         GetSizeCallback callback) override {
-    dbus::MethodCall method_call(::spaced::kSpacedInterface,
-                                 ::spaced::kGetFreeDiskSpaceMethod);
+    dbus::MethodCall method_call(kSpacedInterface,
+                                 spaced::kGetFreeDiskSpaceMethod);
     dbus::MessageWriter writer(&method_call);
     writer.AppendString(path);
 
@@ -45,8 +46,8 @@ class SpacedClientImpl : public SpacedClient {
 
   void GetTotalDiskSpace(const std::string& path,
                          GetSizeCallback callback) override {
-    dbus::MethodCall method_call(::spaced::kSpacedInterface,
-                                 ::spaced::kGetTotalDiskSpaceMethod);
+    dbus::MethodCall method_call(kSpacedInterface,
+                                 spaced::kGetTotalDiskSpaceMethod);
     dbus::MessageWriter writer(&method_call);
     writer.AppendString(path);
 
@@ -57,8 +58,8 @@ class SpacedClientImpl : public SpacedClient {
   }
 
   void GetRootDeviceSize(GetSizeCallback callback) override {
-    dbus::MethodCall method_call(::spaced::kSpacedInterface,
-                                 ::spaced::kGetRootDeviceSizeMethod);
+    dbus::MethodCall method_call(kSpacedInterface,
+                                 spaced::kGetRootDeviceSizeMethod);
     dbus::MessageWriter writer(&method_call);
 
     proxy_->CallMethod(
@@ -68,9 +69,16 @@ class SpacedClientImpl : public SpacedClient {
   }
 
   void Init(dbus::Bus* bus) {
-    proxy_ =
-        bus->GetObjectProxy(::spaced::kSpacedServiceName,
-                            dbus::ObjectPath(::spaced::kSpacedServicePath));
+    proxy_ = bus->GetObjectProxy(spaced::kSpacedServiceName,
+                                 dbus::ObjectPath(spaced::kSpacedServicePath));
+
+    DCHECK(proxy_);
+    proxy_->ConnectToSignal(
+        kSpacedInterface, spaced::kStatefulDiskSpaceUpdate,
+        base::BindRepeating(&SpacedClientImpl::OnSpaceUpdate,
+                            weak_ptr_factory_.GetWeakPtr()),
+        base::BindOnce(&SpacedClientImpl::OnSignalConnected,
+                       weak_ptr_factory_.GetWeakPtr()));
   }
 
  private:
@@ -92,6 +100,28 @@ class SpacedClientImpl : public SpacedClient {
     }
 
     std::move(callback).Run(size);
+  }
+
+  void OnSpaceUpdate(dbus::Signal* const signal) const {
+    Observer::SpaceEvent event;
+    if (!dbus::MessageReader(signal).PopArrayOfBytesAsProto(&event)) {
+      LOG(ERROR) << "Cannot parse StatefulDiskSpaceUpdate proto";
+      return;
+    }
+
+    for (Observer& observer : observers_) {
+      observer.OnSpaceUpdate(event);
+    }
+  }
+
+  void OnSignalConnected(const std::string& interface_name,
+                         const std::string& signal_name,
+                         const bool connected) {
+    connected_ = connected;
+    LOG_IF(ERROR, !connected)
+        << "Cannot connect to StatefulDiskSpaceUpdate signal";
+    DCHECK_EQ(interface_name, kSpacedInterface);
+    DCHECK_EQ(signal_name, spaced::kStatefulDiskSpaceUpdate);
   }
 
   raw_ptr<dbus::ObjectProxy, ExperimentalAsh> proxy_ = nullptr;
