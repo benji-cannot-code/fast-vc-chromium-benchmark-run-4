89165dbf1a24d380c78a0ee46488084c1f0e2573
Daniel Murphy <dmurph@google.com>
2023-06-05 22:04:53 +0000

    [dPWA] Install dialog now closes on navigation
    R=dibyapal@chromium.org
    
    Bug: 1450203
    Change-Id: I219dea13293244176f11a68500e58a50d2d609e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591537
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153505}

--- chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.h
+++ chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.h
@@ -11,9 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "components/prefs/pref_service.h"
-#include "content/public/browser/web_contents.h"
 #include "ui/base/interaction/element_tracker.h"
 #include "ui/views/widget/widget.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
 
 namespace views {
 class Checkbox;
@@ -34,6 +37,7 @@ class PWAConfirmationBubbleView : public LocationBarBubbleDelegateView {
   static PWAConfirmationBubbleView* GetBubble();
 
   PWAConfirmationBubbleView(views::View* anchor_view,
+                            content::WebContents* web_contents,
                             PageActionIconView* highlight_icon_button,
                             std::unique_ptr<WebAppInstallInfo> web_app_info,
                             chrome::AppInstallationAcceptanceCallback callback,
