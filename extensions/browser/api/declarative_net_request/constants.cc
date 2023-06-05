1b0ac46957dec3a263e402c192e60d8fc505eba3
Kelvin Jiang <kelvinjiang@chromium.org>
2023-06-05 23:11:12 +0000

    [DNR] Log compiled regex rule size
    Bug: 1450112
    Change-Id: I698d82fa03f706103964b4446eab3a06d996134a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575764
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153545}

--- extensions/browser/api/declarative_net_request/constants.cc
+++ extensions/browser/api/declarative_net_request/constants.cc
@@ -146,6 +146,8 @@ const char kReadDynamicRulesJSONStatusHistogram[] =
     "Extensions.DeclarativeNetRequest.ReadDynamicRulesJSONStatus";
 const char kIsLargeRegexHistogram[] =
     "Extensions.DeclarativeNetRequest.IsLargeRegexRule";
+const char kRegexRuleSizeHistogram[] =
+    "Extensions.DeclarativeNetRequest.RegexRuleSize";
 const char kLoadRulesetResultHistogram[] =
     "Extensions.DeclarativeNetRequest.LoadRulesetResult";
 
