8248c3ccb89a84eba505ade67477dd5092348322
Bruno Pitrus <brunopitrus@hotmail.com>
2023-06-05 21:22:55 +0000

    IWYU: Add missing standard includes for GCC13
    Bug: 957519
    Change-Id: Ie327653083173b41dfd5a456f82ccb490c305c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547709
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Bruno Pitrus <brunopitrus@hotmail.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153468}

--- base/cpu.h
+++ base/cpu.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_CPU_H_
 #define BASE_CPU_H_
 
+#include <cstdint>
 #include <string>
 
 #include "base/base_export.h"
--- components/services/app_service/public/cpp/intent_filter.h
+++ components/services/app_service/public/cpp/intent_filter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_INTENT_FILTER_H_
 #define COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_INTENT_FILTER_H_
 
+#include <memory>
 #include <set>
 #include <string>
 #include <utility>
