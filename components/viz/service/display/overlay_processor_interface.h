7eed71c4b99f61badec9171a8077d0b5a753a5a0
Eriko Kurimoto <elkurin@chromium.org>
2023-06-05 10:12:23 +0000

    nit: Cleanup overlay_processor_*.h
    Cleanup cl: IWYU, fix typo in comments.
    This CL does not change the behavior.
    
    Change-Id: Ia41be7277f05b20cba6fa9b4b422251004ae6fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583318
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153139}

--- components/viz/service/display/overlay_processor_interface.h
+++ components/viz/service/display/overlay_processor_interface.h
@@ -16,10 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/output_surface.h"
 #include "components/viz/service/display/overlay_candidate.h"
 #include "components/viz/service/viz_service_export.h"
+#include "gpu/command_buffer/common/mailbox.h"
 #include "gpu/command_buffer/service/gpu_task_scheduler_helper.h"
 #include "gpu/ipc/common/surface_handle.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/gfx/buffer_types.h"
 #include "ui/gfx/ca_layer_result.h"
+#include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rrect_f.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/gfx/overlay_priority_hint.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -44,7 +49,7 @@ class OutputSurface;
 class RendererSettings;
 
 // This class is called inside the DirectRenderer to separate the contents that
-// should be send into the overlay system and the contents that requires
+// should be sent into the overlay system and the contents that requires
 // compositing from the DirectRenderer. This class has different subclass
 // implemented by different platforms. This class defines the minimal interface
 // for overlay processing that each platform needs to implement.
@@ -140,7 +145,7 @@ class VIZ_SERVICE_EXPORT OverlayProcessorInterface {
   // processor.
   virtual bool NeedsSurfaceDamageRectList() const = 0;
 
-  // Attempt to replace quads from the specified root render pass with overlays
+  // Attempts to replace quads from the specified root render pass with overlays
   // or CALayers. This must be called every frame.
   virtual void ProcessForOverlays(
       DisplayResourceProvider* resource_provider,
