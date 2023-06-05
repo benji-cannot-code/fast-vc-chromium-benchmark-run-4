b8e5e8514b3227624b78dc2e93a3441d6f3cc033
Maks Orlovich <morlovich@chromium.org>
2023-06-05 15:20:56 +0000

    FLEDGE: Add debug info when rejecting some mojo requests
    We have some reports of AuctionURLLoaderProxy rejecting loading
    requests, but they don't provide anywhere near enough information to
    make it actionable, so add some crash keys to hopefully get a lead.
    
    Bug: 1448458
    Change-Id: I7dd66c463a266a12b719b230be9223251114726d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4561877
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153238}

--- content/browser/interest_group/auction_url_loader_factory_proxy.cc
+++ content/browser/interest_group/auction_url_loader_factory_proxy.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
+#include "base/debug/crash_logging.h"
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/escape.h"
@@ -135,6 +136,20 @@ void AuctionURLLoaderFactoryProxy::CreateLoaderAndStart(
   }
 
   if (!is_request_allowed) {
+    // Debugging for https://crbug.com/1448458
+    SCOPED_CRASH_KEY_STRING32("fledge", "req-accept", accept_header);
+    SCOPED_CRASH_KEY_STRING256("fledge", "req-url",
+                               url_request.url.possibly_invalid_spec());
+    SCOPED_CRASH_KEY_STRING256("fledge", "expect-script-url",
+                               script_url_.possibly_invalid_spec());
+    SCOPED_CRASH_KEY_STRING256(
+        "fledge", "expect-wasm-url",
+        wasm_url_.value_or(GURL()).possibly_invalid_spec());
+    SCOPED_CRASH_KEY_STRING256(
+        "fledge", "expect-trusted",
+        trusted_signals_base_url_.value_or(GURL()).possibly_invalid_spec());
+    SCOPED_CRASH_KEY_STRING256("fledge", "expect-top-frame",
+                               top_frame_origin_.host());
     receiver_.ReportBadMessage("Unexpected request");
     return;
   }
