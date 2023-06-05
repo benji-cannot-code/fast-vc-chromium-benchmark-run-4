f3a25b2d66e27be622b680c53c02402ed216b3f2
Jeroen Dhollander <jeroendh@google.com>
2023-06-05 12:26:13 +0000

    Prevent Use-After-Free of StartupAppLauncher
    When `StartupAppLauncher::BeginInstall` was invoked it notified its
    observers, which indirectly could lead to the `StartupAppLauncher` being
    destroyed, so that when execution of `BeginInstall` continued `this` was
    dangling.
    
    The full code path that leads to this event is:
    
    StartupAppLauncher::BeginInstall
      -> StartupAppLauncher::InstallAppInAsh
      -> KioskAppLauncher::ObserverList::NotifyAppInstalling
      -> KioskLaunchController::OnAppInstalling
      -> AppLaunchSplashScreenHandler::UpdateAppLaunchState
      -> NetworkUiController::OnNetworkStateChanged
      -> KioskLaunchController::OnNetworkLost
      -> NetworkUiController::OnNetworkLostDuringInstallation [*]
      -> NetworkUiController::ShowNetworkConfigureUI
      -> KioskLaunchController::OnNetworkConfigureUiShowing
      -> which destroys StartupAppLauncher.
    
    [*] NetworkUiController::OnNetworkLostDuringInstallation usually
    terminates this call stack here, unless network_required_ is true which
    would be the case only if the network is flaky and if the network
    connection had dropped at least once before this point, which is why
    this 1) evaded detection but also 2) doesn't have a super high crash
    rate.
    
    Since it doesn't make sense that `AppLaunchSplashScreenHandler` would
    send a 'network change' event when it receives a 'app is installing'
    event, this CL removes that code which prevents this UAF.
    
    Fixed: b/283740949
    Tests: kiosk browser_tests, manually launched kiosk
    Change-Id: I36be35bfff8e21b6d1fb8ff420554b95bfc5ab6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583229
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1153176}

--- chrome/browser/ash/app_mode/startup_app_launcher.cc
+++ chrome/browser/ash/app_mode/startup_app_launcher.cc
@@ -64,7 +64,7 @@ StartupAppLauncher::StartupAppLauncher(
       profile_(profile),
       app_id_(app_id),
       should_skip_install_(should_skip_install) {
-  DCHECK(profile_);
+  CHECK(profile_);
   DCHECK(crx_file::id_util::IdIsValid(app_id_));
 
   // Reduce extension downloader retry backoff to avoid waiting on splash screen
@@ -87,9 +87,9 @@ void StartupAppLauncher::RemoveObserver(KioskAppLauncher::Observer* observer) {
 }
 
 void StartupAppLauncher::Initialize() {
-  DCHECK(state_ != LaunchState::kReadyToLaunch &&
-         state_ != LaunchState::kWaitingForWindow &&
-         state_ != LaunchState::kLaunchSucceeded);
+  CHECK(state_ != LaunchState::kReadyToLaunch &&
+        state_ != LaunchState::kWaitingForWindow &&
+        state_ != LaunchState::kLaunchSucceeded);
 
   if (should_skip_install_) {
     OnInstallSuccess();
@@ -151,7 +151,7 @@ void StartupAppLauncher::OnKioskExtensionDownloadFailed(
 
 void StartupAppLauncher::OnKioskAppDataLoadStatusChanged(
     const std::string& app_id) {
-  DCHECK(state_ == LaunchState::kWaitingForCache);
+  CHECK_EQ(state_, LaunchState::kWaitingForCache);
 
   if (app_id != app_id_) {
     return;
@@ -192,7 +192,7 @@ void StartupAppLauncher::InstallAppInLacros() {
 
 void StartupAppLauncher::OnInstallComplete(
     ChromeKioskAppInstaller::InstallResult result) {
-  DCHECK(state_ == LaunchState::kInstallingApp);
+  CHECK_EQ(state_, LaunchState::kInstallingApp);
 
   installer_.reset();
 
@@ -247,7 +247,7 @@ void StartupAppLauncher::LaunchApp() {
 
 void StartupAppLauncher::OnLaunchComplete(
     ChromeKioskAppLauncher::LaunchResult result) {
-  DCHECK(state_ == LaunchState::kReadyToLaunch);
+  CHECK_EQ(state_, LaunchState::kReadyToLaunch);
 
   launcher_.reset();
 
@@ -278,7 +278,7 @@ void StartupAppLauncher::OnLaunchSuccess() {
 
 void StartupAppLauncher::OnLaunchFailure(KioskAppLaunchError::Error error) {
   SYSLOG(ERROR) << "App launch failed, error: " << static_cast<int>(error);
-  DCHECK_NE(KioskAppLaunchError::Error::kNone, error);
+  CHECK_NE(KioskAppLaunchError::Error::kNone, error);
 
   observers_.NotifyLaunchFailed(error);
 }
