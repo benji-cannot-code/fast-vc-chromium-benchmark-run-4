7ad6e82e8e14e2ed8547e6ce699194012fdb3cba
Avi Drissman <avi@chromium.org>
2023-06-05 20:52:00 +0000

    Convert /printing to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Bug: 1280317
    Change-Id: I122a015b23762fca7b1cd2d5493098a9e20a0994
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Low-Coverage-Reason: Doing core refactoring across all of Chromium; I do not own this code
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4585150
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153448}

--- printing/printing_context_mac.h
+++ printing/printing_context_mac.h
@@ -7,14 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PRINTING_PRINTING_CONTEXT_MAC_H_
 
 #include <ApplicationServices/ApplicationServices.h>
+
 #include <string>
 
-#include "base/mac/scoped_nsobject.h"
 #include "base/memory/raw_ptr_exclusion.h"
 #include "base/strings/string_piece.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/print_job_constants.h"
 #include "printing/printing_context.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 @class NSPrintInfo;
 
@@ -108,13 +112,13 @@ class COMPONENT_EXPORT(PRINTING) PrintingContextMac : public PrintingContext {
   mojom::ResultCode PageDone();
 
   // The native print info object.
-  base::scoped_nsobject<NSPrintInfo> print_info_;
+  NSPrintInfo* __strong print_info_;
 
   // The current page's context; only valid between NewPage and PageDone call
   // pairs.
   // This field is not a raw_ptr<> because it was filtered by the rewriter
   // for: #addr-of
-  RAW_PTR_EXCLUSION CGContext* context_;
+  RAW_PTR_EXCLUSION CGContextRef context_ = nullptr;
 };
 
 }  // namespace printing
