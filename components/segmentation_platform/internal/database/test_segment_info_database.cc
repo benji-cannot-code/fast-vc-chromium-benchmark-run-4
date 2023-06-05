56e5e98ddc2c610ba2e4f08f1e33d1685a9f0b2b
Ritika Gupta <ritikagup@google.com>
2023-06-05 20:46:57 +0000

    [CDM] Updating Save API with ModelSource and migrating callers.
    [CDM] = Caching Default Models.
    
    This change is related to segmentation service segment info SAVE API
    update.
    Doc: https://docs.google.com/document/d/1-dpjL73J-GSwQS-Uxr7qUGYUPJpnGxaidi0BZ8WpKmM/edit#heading=h.e9574c8baaho
    This is required for storing default models in database.
    
    The plan is to :
    1. Add model source to database API and migrate callers to call the API
       with server model. (doesn't break anything as this is kind of same
       flow as before).
    2. Implement the logic for database API.
    3. Handle the callers logic to support default models.
    
    This ensures that the compilation works. Also different clients will
    have different handling for default models at different layers. So
    client default models handling could be a part a other set of CLs.
    
    Planned CL:
    - Updating cache and database implementation to support ModelSource.
    - Handling changes required for default models in all the callers.
    
    Change-Id: I948a0328c4ba620ad1adbbc8c84143b65d7744e7
    Bug: b/284380402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569648
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153444}

--- components/segmentation_platform/internal/database/test_segment_info_database.cc
+++ components/segmentation_platform/internal/database/test_segment_info_database.cc
@@ -75,6 +75,7 @@ void TestSegmentInfoDatabase::UpdateSegment(
 
 void TestSegmentInfoDatabase::SaveSegmentResult(
     SegmentId segment_id,
+    ModelSource model_source,
     absl::optional<proto::PredictionResult> result,
     SuccessCallback callback) {
   proto::SegmentInfo* info = FindOrCreateSegment(segment_id);
@@ -87,6 +88,7 @@ void TestSegmentInfoDatabase::SaveSegmentResult(
 }
 
 void TestSegmentInfoDatabase::SaveTrainingData(SegmentId segment_id,
+                                               ModelSource model_source,
                                                const proto::TrainingData& data,
                                                SuccessCallback callback) {
   proto::SegmentInfo* info = FindOrCreateSegment(segment_id);
@@ -223,6 +225,7 @@ void TestSegmentInfoDatabase::SetBucketDuration(SegmentId segment_id,
   info->mutable_model_metadata()->set_time_unit(time_unit);
 }
 
+// TODO(ritikagup@) : Add ModelSource as param to this function.
 proto::SegmentInfo* TestSegmentInfoDatabase::FindOrCreateSegment(
     SegmentId segment_id) {
   proto::SegmentInfo* info = nullptr;
--- components/segmentation_platform/internal/execution/model_execution_manager_impl_unittest.cc
+++ components/segmentation_platform/internal/execution/model_execution_manager_impl_unittest.cc
@@ -72,6 +72,7 @@ class MockSegmentInfoDatabase : public test::TestSegmentInfoDatabase {
   MOCK_METHOD(void,
               SaveSegmentResult,
               (SegmentId segment_id,
+               ModelSource model_source,
                absl::optional<proto::PredictionResult> result,
                SuccessCallback callback),
               (override));
