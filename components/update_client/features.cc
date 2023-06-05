ddc87db734f8aa164c92b05101637b675cdcd35e
James Decker <deckerjames@google.com>
2023-06-05 23:45:06 +0000

    Enabling puffin patches on android.
    Bug: 1446782
    Change-Id: I3b19c2f28b0494c950cc451c882c8bffe022a696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590849
    Commit-Queue: James Decker <deckerjames@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153564}

--- components/update_client/features.cc
+++ components/update_client/features.cc
@@ -9,11 +9,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 namespace update_client::features {
-#if BUILDFLAG(IS_ANDROID)
-BASE_FEATURE(kPuffinPatches,
-             "PuffinPatches",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
 BASE_FEATURE(kPuffinPatches, "PuffinPatches", base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 }
