a66c9ba2948fc97602de96cd2816a1a04fe3821b
Reilly Grant <reillyg@chromium.org>
2023-06-05 20:48:29 +0000

    Revert "geolocation: Add metric for WiFi scanning time"
    This reverts commit d467b6c27960ceb4e44ce958875912e9222d586e.
    
    Reason for revert: Allowing the histogram to expire now that we have the insight we were looking for.
    
    Original change's description:
    > geolocation: Add metric for WiFi scanning time
    >
    > Making a network location request requires first collecting information
    > about nearby access points. On Windows, macOS and Linux this is done
    > with a blocking call to the operating system and this new metric will
    > help us understand what sort of delay this introduces before a
    > location fix is available.
    >
    > Bug: 1344182
    > Change-Id: I9bcb08f82951699495fbae63141c711386aefcc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764307
    > Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    > Auto-Submit: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1024517}
    
    OBSOLETE_HISTOGRAM[Geolocation.WifiDataProviderCommon.WifiScanTaskTime]=Removed in M116
    
    Bug: 1344182,1451440
    Change-Id: Ic546b3fa9fc4f1511ba0b428077e9bffa1f9fdbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590951
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153446}

--- services/device/geolocation/wifi_data_provider_common.cc
+++ services/device/geolocation/wifi_data_provider_common.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/location.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
 #include "components/device_event_log/device_event_log.h"
@@ -68,9 +67,6 @@ void WifiDataProviderCommon::DoWifiScanTask() {
   // Abort the wifi scan if the provider is already being torn down.
   if (!wlan_api_)
     return;
-
-  SCOPED_UMA_HISTOGRAM_TIMER(
-      "Geolocation.WifiDataProviderCommon.WifiScanTaskTime");
 
   bool update_available = false;
   WifiData new_data;
