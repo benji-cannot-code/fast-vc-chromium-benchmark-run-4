5f5aa80035ec704115abc7d8c961e463f2eaddde
Francois Doray <fdoray@chromium.org>
2023-06-05 15:33:21 +0000

    Set CPU security mitigations state before SysInfo::NumberfOfProcessors().
    Today, SysInfo::NumberfOfProcessors() is called by the constructor of
    RenderProcessImpl (via GetThreadPoolInitParams()) before calling
    base::SysInfo::SetIsCpuSecurityMitigationsEnabled(). This means that
    the value returned by SysInfo::NumberfOfProcessors() doesn't take into
    account the fact that CPU security mitigations are enabled.
    
    With this CL, SetCpuSecurityMitigationsEnabled() is called in
    RendererMain, before creating the RenderProcessImpl. Checks are added
    to prevent calls to SysInfo::NumberfOfProcessors() before a call to
    SetCpuSecurityMitigationsEnabled(). Finally, the cache in
    SysInfo::NumberOfProcessors() is removed, to allow different values to
    be returned when the state of CPU security mitigations varies over the
    life of a test process (the cache was added to avoid tripping sandbox
    restrictions which are no longer in place
    https://codereview.chromium.org/67373006).
    
    Bug: 997677
    Change-Id: I59d2807444acad646389663e89f36b913d49ec6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521664
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153242}

--- content/renderer/render_process_impl.cc
+++ content/renderer/render_process_impl.cc
@@ -50,10 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8-wasm-trap-handler-posix.h"
 #endif
 
-#if BUILDFLAG(IS_MAC)
-#include "base/system/sys_info.h"
-#endif
-
 namespace {
 
 void SetV8FlagIfFeature(const base::Feature& feature, const char* v8_flag) {
@@ -105,12 +101,6 @@ namespace content {
 
 RenderProcessImpl::RenderProcessImpl()
     : RenderProcess(GetThreadPoolInitParams()) {
-#if BUILDFLAG(IS_MAC)
-  // Specified when launching the process in
-  // RendererSandboxedProcessLauncherDelegate::EnableCpuSecurityMitigations
-  base::SysInfo::SetIsCpuSecurityMitigationsEnabled(true);
-#endif
-
 #if BUILDFLAG(DCHECK_IS_CONFIGURABLE)
   // Some official builds ship with DCHECKs compiled in. Failing DCHECKs then
   // are either fatal or simply log the error, based on a feature flag.
