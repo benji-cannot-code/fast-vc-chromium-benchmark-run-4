775c2ce1ee06469b72a907658d716a702eb82954
Nohemi Fernandez <fernandex@chromium.org>
2023-06-05 15:42:48 +0000

    [iOS] Add factory class for SupervisedUserMetricsService instance.
    Bug: b:264669957
    Change-Id: I78dc56d8f10077dbfa91a258e54c2b46b4902d0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502857
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153248}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -48,6 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/signin/public/base/signin_pref_names.h"
 #import "components/signin/public/identity_manager/identity_manager.h"
 #import "components/strings/grit/components_locale_settings.h"
+#import "components/supervised_user/core/browser/supervised_user_metrics_service.h"
+#import "components/supervised_user/core/browser/supervised_user_service.h"
 #import "components/sync/base/sync_prefs.h"
 #import "components/sync/service/glue/sync_transport_data_prefs.h"
 #import "components/sync_device_info/device_info_prefs.h"
@@ -275,6 +277,8 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
       registry);
   segmentation_platform::DeviceSwitcherResultDispatcher::RegisterProfilePrefs(
       registry);
+  supervised_user::SupervisedUserService::RegisterProfilePrefs(registry);
+  supervised_user::SupervisedUserMetricsService::RegisterProfilePrefs(registry);
   sync_sessions::SessionSyncPrefs::RegisterProfilePrefs(registry);
   syncer::DeviceInfoPrefs::RegisterProfilePrefs(registry);
   syncer::SyncPrefs::RegisterProfilePrefs(registry);
