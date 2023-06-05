ba2ee6419690b0ea3cd92845e2c949b4bb35f8cb
Ben Reich <benreich@chromium.org>
2023-06-05 04:39:31 +0000

    [FilesBulkPinning] Use drive::util:: flag check for offline calculator
    The offline files calculation uses the overall feature flag, whilst
    we're in the middle of testing this feature it can be either
    FeatureManagement flag or bulk pinning flag, use the global check
    instead.
    
    Fixed: b:285795908
    Change-Id: Icbdea2d7275bcbd85dfc6a85c27d407449480c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584274
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153071}

--- chrome/browser/ui/webui/settings/ash/calculator/size_calculator.cc
+++ chrome/browser/ui/webui/settings/ash/calculator/size_calculator.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crostini/crostini_features.h"
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
 #include "chrome/browser/ash/drive/drive_integration_service.h"
+#include "chrome/browser/ash/drive/file_system_util.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browsing_data/browsing_data_file_system_util.h"
@@ -234,7 +235,7 @@ DriveOfflineSizeCalculator::DriveOfflineSizeCalculator(Profile* profile)
 DriveOfflineSizeCalculator::~DriveOfflineSizeCalculator() = default;
 
 void DriveOfflineSizeCalculator::PerformCalculation() {
-  if (!base::FeatureList::IsEnabled(ash::features::kDriveFsBulkPinning)) {
+  if (!drive::util::IsDriveFsBulkPinningEnabled(profile_)) {
     NotifySizeCalculated(0);
     return;
   }
