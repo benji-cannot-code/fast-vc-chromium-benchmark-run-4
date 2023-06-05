513b5caaa6f2069db496f90996e9e56fc85b9eb3
Ruma Kesh <rkesh@google.com>
2023-06-05 19:32:54 +0000

    CrOS Settings: Convert pin to shelf item browser test to TS
    Additionally, moved the test under the
    `os_apps_page/app_management_page` folder to mirror the app folder
    structure.
    
    gtest_filter="*OsAppsPageAppManagementPagePinToShelfItem*"
    
    Bug: b/270728282
    Test: browser_tests --
    Change-Id: I7542cce4ca40c9e0e7096db0d51bf0dcbb0457f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584422
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1153403}

--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -193,7 +193,6 @@ TEST_F('OSSettingsCrostiniExtraContainerPageTest', 'AllJsTests', () => {
  ['AppManagementFileHandlingItem', 'app_management/file_handling_item_test.js'],
  ['AppManagementManagedApps', 'app_management/managed_apps_test.js'],
  ['AppManagementPage', 'app_management/app_management_page_tests.js'],
- ['AppManagementPinToShelfItem', 'app_management/pin_to_shelf_item_test.js'],
  [
    'AppManagementPluginVmDetailView',
    'app_management/plugin_vm_detail_view_test.js',
@@ -502,6 +501,10 @@ TEST_F('OSSettingsCrostiniExtraContainerPageTest', 'AllJsTests', () => {
  [
    'OsAppsPageAppManagementPageMainView',
    'os_apps_page/app_management_page/main_view_test.js'
+ ],
+ [
+   'OsAppsPageAppManagementPagePinToShelfItem',
+   'os_apps_page/app_management_page/pin_to_shelf_item_test.js'
  ],
  [
    'OsAppsPageAppManagementPagePwaDetailView',
