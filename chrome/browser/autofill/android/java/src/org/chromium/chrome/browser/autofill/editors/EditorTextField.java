7b3f39fb52fee8c27dc1420df85b43fdee98ae68
Timofey Chudakov <tchudakov@google.com>
2023-06-05 11:57:59 +0000

    [Android][Autofill] Move static classes from EditorFieldModel to EditorProperties.
    This is a scaffolding CL before EditorFieldModel will be converted to
    PropertyModel as part of MVCing autofill AddressEditor.
    
    Bug: 1435314
    Change-Id: I26f9c4ac6230389ef6ab7f6253f3f3fc9ad76013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583233
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153171}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/EditorTextField.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/EditorTextField.java
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill.editors;
 
-import static org.chromium.chrome.browser.autofill.editors.EditorFieldModel.TextInputType.ALPHA_NUMERIC_INPUT;
-import static org.chromium.chrome.browser.autofill.editors.EditorFieldModel.TextInputType.EMAIL_ADDRESS_INPUT;
-import static org.chromium.chrome.browser.autofill.editors.EditorFieldModel.TextInputType.PERSON_NAME_INPUT;
-import static org.chromium.chrome.browser.autofill.editors.EditorFieldModel.TextInputType.PHONE_NUMBER_INPUT;
-import static org.chromium.chrome.browser.autofill.editors.EditorFieldModel.TextInputType.REGION_INPUT;
-import static org.chromium.chrome.browser.autofill.editors.EditorFieldModel.TextInputType.STREET_ADDRESS_INPUT;
+import static org.chromium.chrome.browser.autofill.editors.EditorProperties.TextInputType.ALPHA_NUMERIC_INPUT;
+import static org.chromium.chrome.browser.autofill.editors.EditorProperties.TextInputType.EMAIL_ADDRESS_INPUT;
+import static org.chromium.chrome.browser.autofill.editors.EditorProperties.TextInputType.PERSON_NAME_INPUT;
+import static org.chromium.chrome.browser.autofill.editors.EditorProperties.TextInputType.PHONE_NUMBER_INPUT;
+import static org.chromium.chrome.browser.autofill.editors.EditorProperties.TextInputType.REGION_INPUT;
+import static org.chromium.chrome.browser.autofill.editors.EditorProperties.TextInputType.STREET_ADDRESS_INPUT;
 
 import android.content.Context;
 import android.text.Editable;
