5ba033d5422c9e94c133ca2eda457904ff0dc311
Rushan Suleymanov <rushans@google.com>
2023-06-05 09:15:57 +0000

    [Sync] Fix crashes when recording memory usage
    Delegate in ModelTypeController may be empty if the state is either
    NOT_RUNNING or FAILED. Prior to this CL only NOT_RUNNING was checked,
    although it's not clear whether the state can be FAILED for active
    types.
    
    In this CL the check for state was replaced by the presence of delegate.
    
    Fixed: 1450992
    Change-Id: I5ac03d350fea99075c92a3492697c29f7a2fcdc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584931
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1153124}

--- components/sync/service/model_type_controller.cc
+++ components/sync/service/model_type_controller.cc
@@ -268,8 +268,10 @@ void ModelTypeController::GetTypeEntitiesCount(
 }
 
 void ModelTypeController::RecordMemoryUsageAndCountsHistograms() {
-  DCHECK(delegate_);
-  delegate_->RecordMemoryUsageAndCountsHistograms();
+  DCHECK(CalledOnValidThread());
+  if (delegate_) {
+    delegate_->RecordMemoryUsageAndCountsHistograms();
+  }
 }
 
 ModelTypeControllerDelegate* ModelTypeController::GetDelegateForTesting(
