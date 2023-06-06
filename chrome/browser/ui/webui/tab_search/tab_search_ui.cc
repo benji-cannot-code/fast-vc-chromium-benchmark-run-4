29ca34296823dc8c4e24ab62f6a42bb26faf12c3
Yuheng Huang <yuhengh@chromium.org>
2023-06-06 22:36:38 +0000

    Tab Search: GM3 and color pipeline boiler plate setup
    Setup the GM3 flag and ColorChangeHandler. Follow up CL will update
    color and style to match GM3 spec.
    
    Bug: 1422116
    Change-Id: Ic3ff7252ea902943026b8623cb724d59cb1e1aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4592792
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154132}

--- chrome/browser/ui/webui/tab_search/tab_search_ui.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_ui.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/views/style/platform_style.h"
+#include "ui/webui/color_change_listener/color_change_handler.h"
 
 TabSearchUI::TabSearchUI(content::WebUI* web_ui)
     : ui::MojoBubbleWebUIController(web_ui,
@@ -64,6 +65,7 @@ TabSearchUI::TabSearchUI(content::WebUI* web_ui)
       {"collapseRecentlyClosed", IDS_TAB_SEARCH_COLLAPSE_RECENTLY_CLOSED},
 
   };
+  webui::SetupChromeRefresh2023(source);
   source->AddLocalizedStrings(kStrings);
   source->AddBoolean("useRipples", views::PlatformStyle::kUseRipples);
 
@@ -118,6 +120,13 @@ TabSearchUI::TabSearchUI(content::WebUI* web_ui)
 TabSearchUI::~TabSearchUI() = default;
 
 WEB_UI_CONTROLLER_TYPE_IMPL(TabSearchUI)
+
+void TabSearchUI::BindInterface(
+    mojo::PendingReceiver<color_change_listener::mojom::PageHandler>
+        pending_receiver) {
+  color_provider_handler_ = std::make_unique<ui::ColorChangeHandler>(
+      web_ui()->GetWebContents(), std::move(pending_receiver));
+}
 
 void TabSearchUI::BindInterface(
     mojo::PendingReceiver<tab_search::mojom::PageHandlerFactory> receiver) {
--- chrome/browser/ui/webui/tab_search/tab_search_ui.h
+++ chrome/browser/ui/webui/tab_search/tab_search_ui.h
@@ -17,7 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "ui/webui/mojo_bubble_web_ui_controller.h"
+#include "ui/webui/resources/cr_components/color_change_listener/color_change_listener.mojom.h"
 #include "ui/webui/resources/js/metrics_reporter/metrics_reporter.mojom.h"
+
+namespace ui {
+class ColorChangeHandler;
+}
 
 class TabSearchUI : public ui::MojoBubbleWebUIController,
                     public tab_search::mojom::PageHandlerFactory {
@@ -26,6 +31,10 @@ class TabSearchUI : public ui::MojoBubbleWebUIController,
   TabSearchUI(const TabSearchUI&) = delete;
   TabSearchUI& operator=(const TabSearchUI&) = delete;
   ~TabSearchUI() override;
+
+  void BindInterface(
+      mojo::PendingReceiver<color_change_listener::mojom::PageHandler>
+          pending_receiver);
 
   // Instantiates the implementor of the mojom::PageHandlerFactory mojo
   // interface passing the pending receiver that will be internally bound.
@@ -44,6 +53,7 @@ class TabSearchUI : public ui::MojoBubbleWebUIController,
       mojo::PendingRemote<tab_search::mojom::Page> page,
       mojo::PendingReceiver<tab_search::mojom::PageHandler> receiver) override;
 
+  std::unique_ptr<ui::ColorChangeHandler> color_provider_handler_;
   std::unique_ptr<TabSearchPageHandler> page_handler_;
   MetricsReporter metrics_reporter_;
 
