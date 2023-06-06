c150e7f788bd73cf84213a0ac3a9d37d188a3fd3
Alan Cutter <alancutter@chromium.org>
2023-06-06 01:46:05 +0000

    [dPWA] Clean up PreinstalledWebAppDuplicationFixer
    This CL cleans up the PreinstalledWebAppDuplicationFixer start up
    adjustment step.
    This looked for preinstalled web apps that had corresponding old
    Chrome apps still installed and re-migrated them. This was very
    successful when deployed and has tapered off substantially,
    see metrics: go/nejyv
    
    This code is a complexity risk to keep around and can cause other
    unintended issues at start up, see crbug.com/1393284 for an example.
    
    Bug: 1290716
    Change-Id: I3d16034e7a2ea871e1be735aecc9c5903b647207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584075
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153622}

--- chrome/browser/web_applications/adjustments/web_app_adjustments.h
+++ chrome/browser/web_applications/adjustments/web_app_adjustments.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/web_applications/adjustments/preinstalled_web_app_duplication_fixer.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 
@@ -33,15 +32,7 @@ class WebAppAdjustments : public KeyedService {
   explicit WebAppAdjustments(Profile* profile);
   ~WebAppAdjustments() override;
 
-  PreinstalledWebAppDuplicationFixer* preinstalled_web_app_duplication_fixer() {
-    return preinstalled_web_app_duplication_fixer_.get();
-  }
-
  private:
-  // TODO(crbug.com/1290716): This was added in M100, remove in M120.
-  std::unique_ptr<PreinstalledWebAppDuplicationFixer>
-      preinstalled_web_app_duplication_fixer_;
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   // TODO(crbug.com/1290716): This was added in M110, remove in M120.
   std::unique_ptr<CalculatorAppErasureFixer> calculator_app_erasure_fixer_;
