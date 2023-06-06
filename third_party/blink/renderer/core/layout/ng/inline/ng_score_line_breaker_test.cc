5f19c71836ae7d53aad229d204d8d33f6066cc40
Koji Ishii <kojii@chromium.org>
2023-06-06 22:12:00 +0000

    [text-wrap-pretty] Apply the algorithm only to clamped lines
    Similar to what crrev.com/c/4302514 did for `text-wrap:
    balance`, this patch changes the `NGScoreLineBreaker` to apply
    only to clamped lines when `line-clamp` is applied.
    
    In doing this, this patch changes `SuspendUntilConsumed` to
    `SuspendUntilEndParagraph`. They are the same conditions for
    most cases, but when lines are clamped, `NGScoreLineBreaker`
    wants to be suspended until all hidden lines are laid out.
    
    This change fixes `text-wrap-balance-line-clamp-001.html` when
    `NGScoreLineBreaker` is used for `text-wrap: balance`.
    
    Bug: 1432798, 1451205
    Change-Id: I458a89379a20617c83379fb8d580eb3488cc3678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583780
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154119}

--- third_party/blink/renderer/core/layout/ng/inline/ng_score_line_breaker_test.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_score_line_breaker_test.cc
@@ -39,7 +39,7 @@ class NGScoreLineBreakerTest : public RenderingTest {
     line_info_list.Clear();
     NGLineBreakPoints& break_points = context.LineBreakPoints();
     break_points.clear();
-    context.DidCreateLine();
+    context.DidCreateLine(/*is_end_paragraph*/ true);
     for (;;) {
       breaker.OptimalBreakPoints(context);
       if (!context.IsActive() || !breaker.BreakToken() ||
@@ -48,8 +48,10 @@ class NGScoreLineBreakerTest : public RenderingTest {
       }
 
       // Consume the first line in `line_info_list`.
+      const NGLineInfo& line_info = line_info_list.Front();
+      const bool is_end_paragraph = line_info.IsEndParagraph();
       line_info_list.RemoveFront();
-      context.DidCreateLine();
+      context.DidCreateLine(is_end_paragraph);
     }
   }
 
@@ -238,7 +240,7 @@ TEST_F(NGScoreLineBreakerTest, ForcedBreak) {
   // Pretend all the lines are consumed.
   EXPECT_TRUE(optimizer.BreakToken());
   line_info_list.Clear();
-  context.DidCreateLine();
+  context.DidCreateLine(/*is_end_paragraph*/ true);
 
   // Run the optimizer again to continue. This should run up to the end of
   // `target`. It has 4 break candidates so the optimization should apply.
