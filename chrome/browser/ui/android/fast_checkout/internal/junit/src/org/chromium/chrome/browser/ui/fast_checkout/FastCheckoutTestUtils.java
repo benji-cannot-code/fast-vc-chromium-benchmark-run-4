d42947076bcb5c4848fc669213d0674f39cacc3b
Norge Vizcay <vizcay@google.com>
2023-06-06 12:51:18 +0000

    [FastCheckout] Fix UI data update.
    The CL addresses an issue in the Fast Checkout UI, where the address profile list is not updated immediately after a data update. See the bug  for more details.
    
    The CL proposes to use a new ModelList after an update in the data is observed, triggering an update of the RecyclerView adapter and redrawing this way the UI.
    
    Bug: 1451298
    Change-Id: I918a900ad3bb9f0382a730264ff89b41c80ab687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584676
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1153798}

--- chrome/browser/ui/android/fast_checkout/internal/junit/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutTestUtils.java
+++ chrome/browser/ui/android/fast_checkout/internal/junit/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutTestUtils.java
@@ -20,7 +20,7 @@ class FastCheckoutTestUtils {
                 /* companyName= */ "", /* streetAddress= */ streetAddress,
                 /* region= */ "", /* locality= */ "", /* dependentLocality= */ "",
                 /* postalCode= */ postalCode, /* sortingCode= */ "",
-                /* countryCode= */ "", /* countryName= */ "", /* phoneNumber= */ "", email,
+                /* countryCode= */ "", /* countryName= */ "", phoneNumber, email,
                 /* languageCode= */ "en-US");
     }
 
