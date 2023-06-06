6e144b22f945658a230693018cd3d90df5adfe20
Ruma Kesh <rkesh@google.com>
2023-06-06 21:36:53 +0000

    CrOS Settings: Convert plugin vm detail view browser test to TS
    Additionally, moved the test under the
    `os_apps_page/app_management_page` folder to mirror the app folder
    structure.
    
    gtest_filter="*OsAppsPageAppManagementPagePluginVmDetailView*"
    
    Bug: b/270728282
    Test: browser_tests --
    Change-Id: I0678859587ae822772e97beecaf35ef209c031f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591754
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154098}

--- chrome/browser/resources/settings/chromeos/lazy_load.ts
+++ chrome/browser/resources/settings/chromeos/lazy_load.ts
@@ -78,6 +78,7 @@ import './os_apps_page/app_management_page/arc_detail_view.js';
 import './os_apps_page/app_management_page/borealis_page/borealis_detail_view.js';
 import './os_apps_page/app_management_page/chrome_app_detail_view.js';
 import './os_apps_page/app_management_page/pin_to_shelf_item.js';
+import './os_apps_page/app_management_page/plugin_vm_page/plugin_vm_detail_view.js';
 import './os_apps_page/app_management_page/pwa_detail_view.js';
 import './os_apps_page/app_management_page/sub_apps_item.js';
 import './os_files_page/google_drive_subpage.js';
@@ -160,6 +161,7 @@ export {AppManagementBorealisDetailViewElement} from './os_apps_page/app_managem
 export {AppManagementChromeAppDetailViewElement} from './os_apps_page/app_management_page/chrome_app_detail_view.js';
 export {AppManagementMainViewElement} from './os_apps_page/app_management_page/main_view.js';
 export {AppManagementPinToShelfItemElement} from './os_apps_page/app_management_page/pin_to_shelf_item.js';
+export {AppManagementPluginVmDetailViewElement} from './os_apps_page/app_management_page/plugin_vm_page/plugin_vm_detail_view.js';
 export {AppManagementPwaDetailViewElement} from './os_apps_page/app_management_page/pwa_detail_view.js';
 export {AppManagementSubAppsItemElement} from './os_apps_page/app_management_page/sub_apps_item.js';
 export {SettingsGoogleDriveSubpageElement} from './os_files_page/google_drive_subpage.js';
