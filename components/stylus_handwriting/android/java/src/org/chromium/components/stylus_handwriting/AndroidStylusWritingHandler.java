f3694857484acc036671b55a73eef032d9151ece
Alex Mitra <alexmitra@chromium.org>
2023-06-06 09:35:50 +0000

    Treat stylus_handwriting_enabled as enabled by default in Android U
    https://chromium-review.googlesource.com/c/chromium/src/+/4589250/comment/43142db6_8115413c/
    
    Bug: 1451380
    Change-Id: I6d2f6fe4f9da0cd8bb41af8b9ff0ff8230cfb87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590630
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Auto-Submit: Alex Mitra <alexmitra@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153745}

--- components/stylus_handwriting/android/java/src/org/chromium/components/stylus_handwriting/AndroidStylusWritingHandler.java
+++ components/stylus_handwriting/android/java/src/org/chromium/components/stylus_handwriting/AndroidStylusWritingHandler.java
@@ -49,7 +49,7 @@ public class AndroidStylusWritingHandler
         int value = -1;
         if (BuildCompat.isAtLeastU()) {
             value = Settings.Secure.getInt(
-                    context.getContentResolver(), "stylus_handwriting_enabled", -1);
+                    context.getContentResolver(), "stylus_handwriting_enabled", 1);
         } else {
             value = Settings.Global.getInt(
                     context.getContentResolver(), "stylus_handwriting_enabled", -1);
