9c6ff45df1a213fb60918a6a70ad86e1fedeb3a0
Christian Biesinger <cbiesinger@chromium.org>
2023-06-06 07:08:52 +0000

    [FedCM] Use assertTrue with WaitForCondition
    WaitForCondition returns False if it times out; use assertTrue
    here to make it more clear when this happens.
    
    Change-Id: Ib90dfaeffd0ae4e5a6c7c4b89f3081754e9e2d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590211
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153703}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -6410,7 +6410,7 @@ class FedCmSpecificTest(ChromeDriverBaseTestWithWebServer):
 
     self.assertRaises(chromedriver.NoSuchAlert, self._driver.GetAccounts)
     self._driver.ExecuteScript('callFedCm()')
-    self.WaitForCondition(self.FedCmDialogCondition)
+    self.assertTrue(self.WaitForCondition(self.FedCmDialogCondition))
     accounts = self._driver.GetAccounts()
     self.assertEqual(2, len(accounts))
     self.assertEqual({'title': 'Sign in to 127.0.0.1 with localhost'},
@@ -6430,7 +6430,7 @@ class FedCmSpecificTest(ChromeDriverBaseTestWithWebServer):
 
     self.assertRaises(chromedriver.NoSuchAlert, self._driver.GetAccounts)
     self._driver.ExecuteScript('callFedCm()')
-    self.WaitForCondition(self.FedCmDialogCondition)
+    self.assertTrue(self.WaitForCondition(self.FedCmDialogCondition))
 
     self._driver.CancelFedCmDialog()
     self.assertRaises(chromedriver.NoSuchAlert, self._driver.GetAccounts)
