70ad78dcdee0614076fbbbd04200e69645c4b9c8
S. Ganesh <ganesh@chromium.org>
2023-06-06 19:52:11 +0000

    Fix base::win::RegKey OpenKey/CreateKey unexpectedly call `NOTREACHED()`
    This CL sets `wow64access_` unconditionally at the beginning, at
    construction time, and in the calls to `Open` and `Create`.
    
    Fixed: 1450736
    Change-Id: Ie4a914df3cb1819d335877046bfde97c645a9110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582016
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154020}

--- base/win/registry_unittest.cc
+++ base/win/registry_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/run_loop.h"
+#include "base/strings/strcat.h"
 #include "base/test/bind.h"
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
@@ -309,6 +310,24 @@ TYPED_TEST(RegistryTest, OpenSubKey) {
   ASSERT_EQ(ERROR_SUCCESS,
             key.Open(HKEY_CURRENT_USER, this->root_key().c_str(), KEY_WRITE));
   ASSERT_EQ(ERROR_SUCCESS, key.DeleteKey(L"foo"));
+}
+
+TYPED_TEST(RegistryTest, InvalidRelativeKeyCreate) {
+  typename TypeParam::RegType key(TypeParam::Create(
+      HKEY_CURRENT_USER,
+      base::StrCat({this->root_key(), L"_DoesNotExist"}).c_str(),
+      KEY_WOW64_32KEY | KEY_READ));
+  ASSERT_EQ(key.CreateKey(L"SomeSubKey", KEY_WOW64_32KEY | KEY_WRITE),
+            ERROR_INVALID_HANDLE);
+}
+
+TYPED_TEST(RegistryTest, InvalidRelativeKeyOpen) {
+  typename TypeParam::RegType key(TypeParam::Create(
+      HKEY_CURRENT_USER,
+      base::StrCat({this->root_key(), L"_DoesNotExist"}).c_str(),
+      KEY_WOW64_32KEY | KEY_READ));
+  ASSERT_EQ(key.OpenKey(L"SomeSubKey", KEY_WOW64_32KEY | KEY_READ),
+            ERROR_INVALID_HANDLE);
 }
 
 namespace {
