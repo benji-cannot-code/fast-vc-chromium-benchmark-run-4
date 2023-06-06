e97c37dbdfa54a05bea864bf536b62214f3beb09
Morten Stenshorne <mstensho@chromium.org>
2023-06-06 09:56:28 +0000

    Remove LayoutBox::ClearLayoutResults().
    Just be sure to miss the cache afterwards, when we reach any of the
    former call sites of ClearLayoutResults().
    
    Also just remove some code from a web view test, which used to call
    ClearLayoutResults(). The issue wasn't fully analyzed, and the code
    where it crashed (SubtreeLayoutScope) has since been removed.
    
    Clearing the layout results seems problematic. It was responsible for
    the crbug.com/1443633 crash, although the root cause was found to be
    something else.
    
    Change-Id: I3d1e4a83bf882a836670546d54cbac5e3d3e806d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577201
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153754}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -985,8 +985,6 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   // added.
   void FinalizeLayoutResults();
 
-  void ClearLayoutResults();
-
   void RebuildFragmentTreeSpine();
 
   // Call when NG fragment count or size changed. Only call if the fragment
