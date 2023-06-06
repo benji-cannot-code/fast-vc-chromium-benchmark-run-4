1e6d087f8fd244566252ab77399ecd671c6d016a
Shuming Hao <shuminghao@google.com>
2023-06-06 21:48:59 +0000

    Send ARC Notification button click to Android
    For Chrome rendered ARC notifications, send button click event to Android through mojo so it gets called in Android side. This handles inline reply and regular buttons.
    
    ARC side change: ag/23183921
    
    Bug: b:271484624
    Test: manual test in Notify1.apk
    Change-Id: I0107cbf703b16719506c0543f677e6bffc27a9d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564633
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Shuming Hao <shuminghao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154109}

--- ash/public/cpp/external_arc/message_center/arc_notification_manager.h
+++ ash/public/cpp/external_arc/message_center/arc_notification_manager.h
@@ -81,6 +81,9 @@ class ArcNotificationManager
   void SetDoNotDisturbStatusOnAndroid(bool enabled);
   void CancelPress(const std::string& key);
   void SetNotificationConfiguration();
+  void SendNotificationButtonClickedOnChrome(const std::string& key,
+                                             const int button_index,
+                                             const std::string& input);
 
   // Methods called from |visibility_manager_|:
   void OnMessageCenterVisibilityChanged(
--- ash/public/cpp/external_arc/message_center/mock_arc_notification_item.h
+++ ash/public/cpp/external_arc/message_center/mock_arc_notification_item.h
@@ -43,6 +43,7 @@ class MockArcNotificationItem : public ArcNotificationItem {
   // Overriding methods for returning dummy data or doing nothing.
   void OnClosedFromAndroid() override {}
   void Click() override {}
+  void ClickButton(const int button_index, const std::string& input) override {}
   void ToggleExpansion() override {}
   void OnWindowActivated(bool activated) override {}
   void OpenSettings() override {}
