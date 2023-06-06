489800c7ed1c822fe0fd783c247e94117dfb8b93
Elaine Chien <elainechien@chromium.org>
2023-06-06 18:14:06 +0000

    [ChromeRefresh2023] Implement profile chip and profile row background
    This CL adds a MenuItemBackground struct to paint
    the backgrounds on MenuItemViews We can’t use a background object
    because menu labels are painted on the canvas and using a background
    object will cause the background to be painted over the menu label.
    
    This CL also does not show the profile row if we are on an ash build.
    
    Figma mocks: https://www.figma.com/file/NLMJNd5s2g9DEDZCbIngJV/ChroMenu---Desktop-Scalability?type=design&node-id=2560-291355&t=SHKzL2LzsXjxeBJt-0
    
    Ash build mock (no profile row): https://www.figma.com/file/NLMJNd5s2g9DEDZCbIngJV/ChroMenu---Desktop-Scalability?type=design&node-id=4002-331361&t=XaWM3sEsdxy1dAEF-0
    
    Bug: 1427667
    Change-Id: Ia99b2ba01b33db46490b763b86490caa9a22094a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4585960
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153952}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -225,6 +225,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorTextSelectionForeground) \
   \
   /* Further UI element colors */ \
+  E_CPONLY(kColorAppMenuProfileRowBackground) \
+  E_CPONLY(kColorAppMenuProfileRowBackgroundHovered) \
+  E_CPONLY(kColorAppMenuProfileRowChipBackground) \
+  E_CPONLY(kColorAppMenuProfileRowChipHovered) \
   E_CPONLY(kColorAvatarHeaderArt) \
   E_CPONLY(kColorAvatarIconGuest) \
   E_CPONLY(kColorAvatarIconIncognito) \
