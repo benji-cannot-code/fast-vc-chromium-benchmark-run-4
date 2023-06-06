7d68bf9a19595332ce38da087f2c8913ceb3ba7f
Eriko Kurimoto <elkurin@chromium.org>
2023-06-06 13:21:49 +0000

    BrowserLoader: Add selection policy enforce to chrome logging
    This CL adds a log to show whether lacros selection is enforced by user
    policy or not.
    This helps debugging lacros launch issues.
    
    Change-Id: I013d1f0b7f6cade089012839d38d46c3d15256eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583312
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153804}

--- chrome/browser/ash/crosapi/browser_loader.cc
+++ chrome/browser/ash/crosapi/browser_loader.cc
@@ -77,16 +77,18 @@ bool BrowserLoader::WillLoadStatefulComponentBuilds() {
   return true;
 }
 
-void BrowserLoader::SelectRootfsLacros(LoadCompletionCallback callback) {
-  LOG(WARNING) << "rootfs lacros is selected";
+void BrowserLoader::SelectRootfsLacros(LoadCompletionCallback callback,
+                                       LacrosSelectionSource source) {
+  LOG(WARNING) << "rootfs lacros is selected by " << source;
 
   rootfs_lacros_loader_->Load(
       base::BindOnce(&BrowserLoader::OnLoadComplete, weak_factory_.GetWeakPtr(),
                      std::move(callback), LacrosSelection::kRootfs));
 }
 
-void BrowserLoader::SelectStatefulLacros(LoadCompletionCallback callback) {
-  LOG(WARNING) << "stateful lacros is selected";
+void BrowserLoader::SelectStatefulLacros(LoadCompletionCallback callback,
+                                         LacrosSelectionSource source) {
+  LOG(WARNING) << "stateful lacros is selected by " << source;
 
   stateful_lacros_loader_->Load(
       base::BindOnce(&BrowserLoader::OnLoadComplete, weak_factory_.GetWeakPtr(),
@@ -127,10 +129,11 @@ void BrowserLoader::Load(LoadCompletionCallback callback) {
     // too.
     switch (lacros_selection.value()) {
       case browser_util::LacrosSelection::kRootfs:
-        SelectRootfsLacros(std::move(callback));
+        SelectRootfsLacros(std::move(callback), LacrosSelectionSource::kPolicy);
         return;
       case browser_util::LacrosSelection::kStateful:
-        SelectStatefulLacros(std::move(callback));
+        SelectStatefulLacros(std::move(callback),
+                             LacrosSelectionSource::kPolicy);
         return;
       case browser_util::LacrosSelection::kDeployedLocally:
         NOTREACHED();
@@ -213,11 +216,13 @@ void BrowserLoader::OnLoadVersions(
 
   switch (selected->selection) {
     case LacrosSelection::kRootfs: {
-      SelectRootfsLacros(std::move(callback));
+      SelectRootfsLacros(std::move(callback),
+                         LacrosSelectionSource::kCompatibilityCheck);
       break;
     }
     case LacrosSelection::kStateful: {
-      SelectStatefulLacros(std::move(callback));
+      SelectStatefulLacros(std::move(callback),
+                           LacrosSelectionSource::kCompatibilityCheck);
       break;
     }
     case LacrosSelection::kDeployedLocally: {
@@ -283,6 +288,20 @@ void BrowserLoader::FinishOnLoadComplete(LoadCompletionCallback callback,
   // Log the path on success.
   LOG(WARNING) << "Loaded lacros image at " << path;
   std::move(callback).Run(path, selection, std::move(version));
+}
+
+std::ostream& operator<<(std::ostream& ostream,
+                         BrowserLoader::LacrosSelectionSource source) {
+  switch (source) {
+    case BrowserLoader::LacrosSelectionSource::kUnknown:
+      return ostream << "Unknown";
+    case BrowserLoader::LacrosSelectionSource::kCompatibilityCheck:
+      return ostream << "CompatibilityCheck";
+    case BrowserLoader::LacrosSelectionSource::kPolicy:
+      return ostream << "Policy";
+    case BrowserLoader::LacrosSelectionSource::kDeployedPath:
+      return ostream << "DeployedPath";
+  }
 }
 
 }  // namespace crosapi
