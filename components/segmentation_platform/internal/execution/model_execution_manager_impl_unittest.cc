b9752ac34988769f680618367a583177ff24e62b
Ritika Gupta <ritikagup@google.com>
2023-06-06 00:56:08 +0000

    [CDM] Updating Update API with ModelSource and migrating callers.
    [CDM] = Caching Default Models.
    
    Doc: https://docs.google.com/document/d/1-dpjL73J-GSwQS-Uxr7qUGYUPJpnGxaidi0BZ8WpKmM/edit#heading=h.e9574c8baaho
    This is required for storing default models in DB.
    
    The plan is to :
    1. Add model source to database API and migrate callers to call the API
       with server model. (doesn't break anything as this is kind of same
       flow as before).
    2. Implement the logic for database API.
    3. Handle the callers logic to support default models.
    
    This ensures that the compilation works. Also different clients will
    have different handling for default models at different layers. So
    client default models handling could be a part a other set of CLs.
    
    Planned CL:
    - Updating test segment info db implementation to support ModelSource.
    - Handling changes required for default models in all the callers.
    
    Change-Id: I129cc4483fc12f58d9902fcd04475a8383e45f46
    Bug: b/284380402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571426
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153601}

--- components/segmentation_platform/internal/execution/model_execution_manager_impl_unittest.cc
+++ components/segmentation_platform/internal/execution/model_execution_manager_impl_unittest.cc
@@ -66,6 +66,7 @@ class MockSegmentInfoDatabase : public test::TestSegmentInfoDatabase {
   MOCK_METHOD(void,
               UpdateSegment,
               (SegmentId segment_id,
+               ModelSource model_source,
                absl::optional<proto::SegmentInfo> segment_info,
                SuccessCallback callback),
               (override));
