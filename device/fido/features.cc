52115e7bc434fde9052f35742e34d32159848ab7
Adam Langley <agl@chromium.org>
2023-06-06 18:48:09 +0000

    webauthn: support getting caBLE linking information from Play Services
    The caBLE authenticator code is moving from Chrome to Play Services. As
    part of this, Chrome will switch from publishing itself as the caBLE
    endpoint, to publishing Play Service's information.
    
    This change fetches the needed information from Play Services (when
    Chrome has first-party access). It is currently disabled behind a flag.
    
    Low coverage (parser seems broken so putting this here): much of this
    change deals with the lowest-level interface between Chrome and Play
    Services. Bits that can be tested are, but only end-to-end tests are
    going to cover a lot of this and the coverage metrics don't include
    those (for good reason).
    
    Low-Coverage-Reason: see above
    Change-Id: I140b18ec71b05bc4a6181a82de15c157e5106991
    Bug: 1442040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569300
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153975}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -75,4 +75,9 @@ BASE_FEATURE(kWebAuthnNewHybridUI,
              "WebAuthenticationNewHybridUI",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Not yet enabled by default.
+BASE_FEATURE(kWebAuthnPrelinkPlayServices,
+             "WebAuthenticationPrelinkPlayServices",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace device
