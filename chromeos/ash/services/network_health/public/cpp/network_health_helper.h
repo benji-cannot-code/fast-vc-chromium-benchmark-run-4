ff07ffa587a20b16aa5e5267698bd281acb3d25a
Steven Bennetts <stevenjb@chromium.org>
2023-06-06 03:05:54 +0000

    Fix AlternativeErrorPage tests with error_page_enable flag
    This makes NetworkHealthHelper::IsWiFiPortalState() tri-state,
    returning nullopt when no wifi connection exists.
    
    This change fixes non ChromeOS specific tests that expect an empty
    AlternativeErrorPage result.
    
    BUG=247603112
    TEST=/browser_tests --gtest_filter=AlternativeErrorPageOverrideInfoBrowserTest.*
    
    Change-Id: I41b90a9fee86ab8ffaf88825c6422712b71ce5c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590539
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153649}

--- chromeos/ash/services/network_health/public/cpp/network_health_helper.h
+++ chromeos/ash/services/network_health/public/cpp/network_health_helper.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/network_health/public/mojom/network_health_types.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace ash::network_health {
 
@@ -29,9 +30,9 @@ class NetworkHealthHelper
   NetworkHealthHelper& operator=(const NetworkHealthHelper&) = delete;
   ~NetworkHealthHelper() override;
 
-  // Returns the portal state of the default network if the default network is
-  // set and is a WiFi network, otherwise returns false.
-  bool IsWiFiPortalState();
+  // If the default network is set and is a WiFi network, returns the portal
+  // state of the default network, otherwise returns kUnknown.
+  chromeos::network_config::mojom::PortalState WiFiPortalState();
 
   // chromeos::network_health::mojom::NetworkEventsObserver:
   void OnConnectionStateChanged(
