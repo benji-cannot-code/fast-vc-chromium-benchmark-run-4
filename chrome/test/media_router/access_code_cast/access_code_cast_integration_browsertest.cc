fcdf9d473973ab4fc0354ce4800967669ddbb98c
Muyao Xu <muyaoxu@google.com>
2023-06-06 17:27:42 +0000

    [Cast Moderator] Fix browser test data race
    Access code cast browser tests have been flaky because of race
    conditions. CastMediaSinkService is running on its own task runner but
    AccessCodeCastIntegrationBrowserTest is trying to access it on the main
    thread.
    
    This CL uses a explicit exit condition to avoid calling
    VerifyAndClearExpectations() when CastMediaSinkService's task hasn't
    finished.
    
    Bug: 1450324
    Change-Id: I0d4ef44cb31fb5e949be410835b53efcb4172ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591740
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1153916}

--- chrome/test/media_router/access_code_cast/access_code_cast_integration_browsertest.cc
+++ chrome/test/media_router/access_code_cast/access_code_cast_integration_browsertest.cc
@@ -380,8 +380,6 @@ AccessCodeCastIntegrationBrowserTest::GetDeviceAddedTimeFromDict(
 }
 
 void AccessCodeCastIntegrationBrowserTest::TearDownOnMainThread() {
-  ValidateCastMediaSinkServiceImpl();
-
   url_loader_interceptor_.reset();
 
   base::RunLoop().RunUntilIdle();
@@ -389,19 +387,18 @@ void AccessCodeCastIntegrationBrowserTest::TearDownOnMainThread() {
   InProcessBrowserTest::TearDownOnMainThread();
 }
 
-void AccessCodeCastIntegrationBrowserTest::ValidateCastMediaSinkServiceImpl() {
-  EXPECT_TRUE(testing::Mock::VerifyAndClearExpectations(
-      mock_cast_media_sink_service_impl()));
-}
-
 void AccessCodeCastIntegrationBrowserTest::ExpectMediaRouterHasNoSinks(
+    base::OnceClosure callback,
     bool has_sink) {
   EXPECT_FALSE(has_sink);
+  std::move(callback).Run();
 }
 
 void AccessCodeCastIntegrationBrowserTest::ExpectMediaRouterHasSink(
+    base::OnceClosure callback,
     bool has_sink) {
   EXPECT_TRUE(has_sink);
+  std::move(callback).Run();
 }
 
 std::unique_ptr<KeyedService>
