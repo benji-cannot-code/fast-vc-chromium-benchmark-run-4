7f3d588b6b32ceffe3e02d48381360909084f0d5
Ken Buchanan <kenrb@chromium.org>
2023-06-06 12:44:57 +0000

    [WebAuthn] Add hybrid passkeys to password manager keyboard accessory
    This is the third CL in a chain to add Chrome support to activate
    GMSCore's hybrid passkey API on Android.
    
    This change adds an option to the password manager sheet in the
    keyabord accessory that launches the hybrid sign-in flow when
    selected.
    
    The new screen can be seen at:
    go/strpic/53155fc3a03c1b31b2239b4ec4463325948806ce
    
    The previous CL adding the equivalent option to the Touch To Fill
    sheet is:
    https://chromium-review.googlesource.com/c/chromium/src/+/4571663/
    
    Bug: 1449268
    Change-Id: Iba89d3adfde5a434da7e23202f76dec3157b16fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574986
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153794}

--- chrome/browser/password_manager/android/password_accessory_controller_impl.cc
+++ chrome/browser/password_manager/android/password_accessory_controller_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ssl/security_state_tab_helper.h"
 #include "chrome/browser/ui/passwords/ui_utils.h"
+#include "chrome/browser/webauthn/android/webauthn_request_delegate_android.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/autofill/core/browser/ui/accessory_sheet_data.h"
 #include "components/autofill/core/browser/ui/accessory_sheet_enums.h"
@@ -47,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_client.h"
 #include "components/password_manager/core/browser/password_manager_driver.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
+#include "components/password_manager/core/browser/webauthn_credentials_delegate.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/url_formatter/elide_url.h"
@@ -213,6 +215,20 @@ PasswordAccessoryControllerImpl::GetSheetData() const {
   footer_commands_to_add.push_back(FooterCommand(
       manage_passwords_title, autofill::AccessoryAction::MANAGE_PASSWORDS));
 
+  if (password_manager::PasswordManagerDriver* driver =
+          driver_supplier_.Run((&GetWebContents()))) {
+    if (password_manager::WebAuthnCredentialsDelegate* credentials_delegate =
+            password_client_->GetWebAuthnCredentialsDelegateForDriver(driver)) {
+      if (credentials_delegate->IsAndroidHybridAvailable()) {
+        std::u16string passkey_other_device_title = l10n_util::GetStringUTF16(
+            IDS_PASSWORD_MANAGER_ACCESSORY_USE_DEVICE_PASSKEY);
+        footer_commands_to_add.emplace_back(
+            passkey_other_device_title,
+            autofill::AccessoryAction::CROSS_DEVICE_PASSKEY);
+      }
+    }
+  }
+
   bool has_suggestions = !info_to_add.empty();
   AccessorySheetData data = autofill::CreateAccessorySheetData(
       autofill::AccessoryTabType::PASSWORDS, GetTitle(has_suggestions, origin),
@@ -329,6 +345,18 @@ void PasswordAccessoryControllerImpl::OnOptionSelected(
       if (WebAuthnCredManDelegate* delegate =
               WebAuthnCredManDelegate::GetRequestDelegate(&GetWebContents())) {
         delegate->TriggerFullRequest();
+      }
+      return;
+    case autofill::AccessoryAction::CROSS_DEVICE_PASSKEY:
+      if (password_manager::PasswordManagerDriver* driver =
+              driver_supplier_.Run(&GetWebContents())) {
+        if (password_manager::
+                WebAuthnCredentialsDelegate* credentials_delegate =
+                    password_client_->GetWebAuthnCredentialsDelegateForDriver(
+                        driver)) {
+          CHECK(credentials_delegate->IsAndroidHybridAvailable());
+          credentials_delegate->ShowAndroidHybridSignIn();
+        }
       }
       return;
     default:
