bc16c5eed00b9eb602b3f9dea098d12c7d423d9e
Kenichi Ishibashi <bashi@chromium.org>
2023-06-06 10:43:05 +0000

    Disallow DevTools main thread debugger when multiple main frames live in a renderer
    DevToolsMainThreadDebugger indirectly depends on ScopedPagePauser.
    Since ScopedPagePauser works only for one main frame,
    DevToolsMainThreadDebugger doesn't work properly when multiple main frames exist in a renderer. This CL adds a feature flag to enable or
    disble DevTools debugger when multiple frames exist, to mitigate risks
    to pause main frames unexpectedly.
    
    Bug: 1449114
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Change-Id: I3b990cf27beca949bd9d35f6ff77536c13229f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582356
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153761}

--- third_party/blink/renderer/core/inspector/main_thread_debugger.cc
+++ third_party/blink/renderer/core/inspector/main_thread_debugger.cc
@@ -33,8 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/feature_list.h"
 #include "base/synchronization/lock.h"
 #include "build/chromeos_buildflags.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/bindings/core/v8/binding_security.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_controller.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
@@ -66,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/dom_wrapper_world.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/source_location.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
@@ -335,7 +338,35 @@ void MainThreadDebugger::endEnsureAllContextsInGroup(int context_group_id) {
 
 bool MainThreadDebugger::canExecuteScripts(int context_group_id) {
   LocalFrame* frame = WeakIdentifierMap<LocalFrame>::Lookup(context_group_id);
-  return frame->DomWindow()->CanExecuteScripts(kNotAboutToExecuteScript);
+  if (!frame->DomWindow()->CanExecuteScripts(kNotAboutToExecuteScript)) {
+    return false;
+  }
+
+  if (base::FeatureList::IsEnabled(
+          features::kAllowDevToolsMainThreadDebuggerForMutipleMainFrames)) {
+    return true;
+  }
+
+  size_t num_main_frames = 0;
+  for (auto& page : Page::OrdinaryPages()) {
+    if (page->MainFrame() && page->MainFrame()->IsOutermostMainFrame()) {
+      ++num_main_frames;
+    }
+  }
+
+  if (num_main_frames > 1) {
+    String message = String(
+        "DevTools debugger is disabled because it is attached to a process "
+        "that hosts multiple top-level frames, where DevTools debugger doesn't "
+        "work properly. Please enable chrome://flags/#disable-process-reuse "
+        "and restart the browser to enable debugger.");
+    frame->Console().AddMessage(MakeGarbageCollected<ConsoleMessage>(
+        mojom::ConsoleMessageSource::kJavaScript,
+        mojom::ConsoleMessageLevel::kError, message));
+    return false;
+  }
+
+  return true;
 }
 
 void MainThreadDebugger::runIfWaitingForDebugger(int context_group_id) {
--- third_party/blink/renderer/core/inspector/main_thread_debugger.h
+++ third_party/blink/renderer/core/inspector/main_thread_debugger.h
@@ -82,6 +82,8 @@ class CORE_EXPORT MainThreadDebugger final : public ThreadDebuggerCommonImpl {
   void ExceptionThrown(ExecutionContext*, ErrorEvent*);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(MainThreadDebuggerMultipleMainFramesTest, Allow);
+
   void ReportConsoleMessage(ExecutionContext*,
                             mojom::ConsoleMessageSource,
                             mojom::ConsoleMessageLevel,
