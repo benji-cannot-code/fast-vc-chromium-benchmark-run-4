6d8101d962e1c6a6f055235aab96db1a5bd33ba1
Benoit Lize <lizeb@chromium.org>
2023-06-06 09:49:47 +0000

    [aw] Add a feature to reset the draw functor in background.
    When a WebView becomes invisible, the tiles required for the next frame
    are still retained, for as long as the WebView is alive. In particular,
    unlocked compositor frame reclaim doesn't work for WebView, contrary to
    Chrome.
    
    In practice, this means that we retain ~17MiB of tiles for each
    fullscreen WebView which exists on a given device at a time (for a 1440p
    device, here on Pixel 6 Pro). A solution to not keep this memory is to
    release the draw functor after some time in background. This triggers a
    context destroyed notification (DrawFn_OnContextDestroyed), resulting in
    SkiaOutputSurfaceImpl getting destroyed, and then all tiles being
    released.
    
    Bug: 1424346
    Change-Id: I408a040bf1faf4cd6f74bbbcb9d425fa37d87f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570622
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153750}

--- android_webview/browser/aw_feature_map.cc
+++ android_webview/browser/aw_feature_map.cc
@@ -34,6 +34,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewUmaUploadQualityOfServiceSetToDefault,
     &metrics::kAndroidMetricsAsyncMetricLogging,
     &features::kWebViewZoomKeyboardShortcuts,
+    &features::kWebViewClearFunctorInBackground,
 };
 
 // static
