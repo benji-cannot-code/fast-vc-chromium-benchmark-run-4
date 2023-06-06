d1d2bd16d44c913ca8218e6c817cba1075d11f31
Emilia Paz <emiliapaz@chromium.org>
2023-06-06 18:31:20 +0000

    [Extensions cleanup] Remove ButtonVisibility in context menu model
    ButtonVisibility was used to determine whether the extension was pinned
    or not on the toolbar. The enum was in the extension context menu,
    but its computation was made in the extensions menu. This was
    problematic, because it's not a property that belongs to either of them.
    
    Instead we can directly retrieve whether the extension is pinned from
    the toolbar actions model and remove the enum.
    
    Bug: 1416359
    Change-Id: If4c2da83eb36a471fb80e07f78480837bda7bcde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573221
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153967}

--- chrome/browser/ui/views/extensions/extensions_toolbar_container.h
+++ chrome/browser/ui/views/extensions/extensions_toolbar_container.h
@@ -84,7 +84,6 @@ class ExtensionsToolbarContainer
       delete;
   ~ExtensionsToolbarContainer() override;
 
-  DisplayMode display_mode() const { return display_mode_; }
   const ToolbarIcons& icons_for_testing() const { return icons_; }
   ToolbarActionViewController* popup_owner_for_testing() {
     return popup_owner_;
@@ -154,8 +153,6 @@ class ExtensionsToolbarContainer
   void OnContextMenuClosed() override;
   bool CanShowActionsInToolbar() const override;
   bool IsActionVisibleOnToolbar(const std::string& action_id) const override;
-  extensions::ExtensionContextMenuModel::ButtonVisibility GetActionVisibility(
-      const std::string& action_id) const override;
   void UndoPopOut() override;
   void SetPopupOwner(ToolbarActionViewController* popup_owner) override;
   void HideActivePopup() override;
