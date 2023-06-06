9dce8a72faac00e755c0fe8afeb4aba1e221f1db
manukh <manukh@chromium.org>
2023-06-06 06:11:04 +0000

    [omnibox] Sanitize history suggestion titles.
    Page titles can contain unsanitized characters (e.g. new lines or other
    non-vanilla whitespace). We have DCHECKs in place to ensure match
    contents and descriptions do not contain bad characters:
    https://source.chromium.org/chromium/chromium/src/+/main:components/omnibox/browser/autocomplete_result.cc;l=256-261;drc=3956eae144e19077ae5ad5908dee5463cd070a89
    
    Some providers (e.g. document provider) enforce by calling
    `AutocompleteMatch::SanitizeString()`.
    
    Other providers enforce this some other way; e.g. the bookmark provider
    removes bad characters from bookmarks with titles containing them -
    though I didn't investigate how it does so.
    
    But the HQP and HUP don't. E.g., if you 1) create a local html file
    containing `<title>bad&#x2028space</title>`, 2) open the page in
    chromium, then 3) type 'b' into the omnibox, the DCHECKs crash.
    
    This CL fixes those crashes by calling `SanitizeString()` when HQP and
    HUP set their matches' titles.
    
    I considered doing it upstream (i.e. for all history DB reads), but
    there were too many callsites and it didn't seem worthwhile to go
    through all of them and ensure they'd work as expected with the
    sanitized strings.
    
    Bug: 1449517
    Change-Id: I1e76a8defb90a57e8aa572b1ce27b097dbace282
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590218
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153690}

--- components/omnibox/browser/history_url_provider.cc
+++ components/omnibox/browser/history_url_provider.cc
@@ -1151,7 +1151,7 @@ AutocompleteMatch HistoryURLProvider::HistoryMatchToACMatch(
       ACMatchClassification::URL | ACMatchClassification::MATCH,
       ACMatchClassification::URL);
 
-  match.description = info.title();
+  match.description = AutocompleteMatch::SanitizeString(info.title());
   match.description_class =
       ClassifyDescription(params.input.text(), match.description);
 
