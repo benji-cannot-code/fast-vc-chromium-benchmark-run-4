a1d8667cf025cd5bcdb5a61627acecb3c53468ee
Caroline Rising <corising@chromium.org>
2023-06-07 21:13:53 +0000

    Update side panel background for chrome desktop refresh 2023.
    Updates the background to surround the side panel content so the header
    is now over the background. This cl also updates the combobox background
    color and text color.
    
    Bug: 1399966
    Change-Id: I19a6f0d2f0d17c6cee904da0900524aa0022961b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591332
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154612}

--- ui/color/material_ui_color_mixer.cc
+++ ui/color/material_ui_color_mixer.cc
@@ -89,6 +89,7 @@ void AddMaterialUiColorMixer(ColorProvider* provider,
   mixer[kColorSegmentedButtonRipple] = {kColorSysStateRippleNeutralOnSubtle};
   mixer[kColorSegmentedButtonChecked] = {kColorSysTonalContainer};
   mixer[kColorSeparator] = {kColorSysDivider};
+  mixer[kColorSidePanelComboboxBackground] = {SK_ColorTRANSPARENT};
   mixer[kColorSliderThumb] = {kColorSysPrimary};
   mixer[kColorSliderThumbMinimal] = {kColorSysSecondary};
   mixer[kColorSliderTrack] = {kColorSysOnPrimary};
