b4abb4fd43da17c46b92f66ff06da8126503deac
Kalvin Lee <kdlee@chromium.org>
2023-06-07 05:17:17 +0000

    base: Patch MessagePump tests
    The MessagePump unit tests (in their Glib and Libevent variants) both
    report a read of a stack address after its disappearance, centering on
    the stack value `buf`. It appears that the task posted to the
    `io_runner()` can run after control flow leaves the test body, which
    would result in the no-longer-present stack address being dereferenced.
    
    The remedy is to convert the stack-local into a fixture member.
    
    For posterity, my repro was done with the following GN args:
    
    ```
    symbol_level = 2
    is_debug = false
    is_asan = true
    ```
    
    Fixed: 737311
    Change-Id: Ie0da572e1c34b91757ff3789809dda5a16e74ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584284
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154259}

--- base/message_loop/message_pump_libevent_unittest.cc
+++ base/message_loop/message_pump_libevent_unittest.cc
@@ -99,6 +99,7 @@ class MessagePumpLibeventTest : public testing::Test,
   }
 
   int pipefds_[2];
+  static constexpr char null_byte_ = 0;
   std::unique_ptr<test::SingleThreadTaskEnvironment> task_environment_;
 
  private:
@@ -273,9 +274,8 @@ TEST_P(MessagePumpLibeventTest, QuitWatcher) {
                             &controller, &delegate);
 
   // Make the IO thread wait for |event| before writing to pipefds[1].
-  const char buf = 0;
   WaitableEventWatcher::EventCallback write_fd_task =
-      BindOnce(&WriteFDWrapper, pipefds_[1], &buf, 1);
+      BindOnce(&WriteFDWrapper, pipefds_[1], &null_byte_, 1);
   io_runner()->PostTask(
       FROM_HERE, BindOnce(IgnoreResult(&WaitableEventWatcher::StartWatching),
                           Unretained(watcher.get()), &event,
