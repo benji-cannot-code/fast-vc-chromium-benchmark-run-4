7871e148abf86533b1f320d131899a3f08dfcf09
Louis Romero <lpromero@google.com>
2023-06-07 14:10:21 +0000

    Remove DumpWithoutCrashing in reportTabSelectionTime
    This CL removes a DumpWithoutCrashing in a codepath that de facto can
    happen. The theory is that reportTabSelectionTime can be called twice in
    a row: the first call clearing the timer, the second failing to report
    something meaningful.
    
    Fixed: 1449457
    Change-Id: Ib363fd9a3c736a9e2cd4f95d1b259d2b4078da9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595198
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154399}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -2226,7 +2226,6 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
 - (void)reportTabSelectionTime {
   if (self.tabGridEnterTime.is_null()) {
     // The enter time was not recorded. Bail out.
-    base::debug::DumpWithoutCrashing();
     return;
   }
   base::TimeDelta duration = base::TimeTicks::Now() - self.tabGridEnterTime;
