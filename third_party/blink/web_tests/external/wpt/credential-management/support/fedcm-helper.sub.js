aa85fde538630d25862505383d34df5c04497985
Nicolás Peña <npm@chromium.org>
2023-06-07 16:30:19 +0000

    [FedCM] Fix WPT test defaults
    Currently, the mediation parameter is not passed, which means it will be
    set to 'optional'. It's better for most tests to use 'required', and
    instead use 'optional' for the auto re-authn test.
    
    Change-Id: I744a3b486c8edb356510f55a465c8db4b1c58589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595879
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154462}

--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
@@ -30,7 +30,7 @@ export function set_alt_fedcm_cookie() {
 
 // Returns FedCM CredentialRequestOptions for which navigator.credentials.get()
 // succeeds.
-export function default_request_options(manifest_filename) {
+export function request_options_with_mediation_required(manifest_filename) {
   if (manifest_filename === undefined) {
     manifest_filename = "manifest.py";
   }
@@ -41,15 +41,16 @@ credential-management/support/fedcm/${manifest_filename}`;
       providers: [{
         configURL: manifest_path,
         clientId: '1',
-        nonce: '2',
-      }]
+        nonce: '2'
+      }],
+      mediation: 'required'
     }
   };
 }
 
 // Returns alternate FedCM CredentialRequestOptions for which navigator.credentials.get()
 // succeeds.
-export function default_alt_request_options(manifest_filename) {
+export function alt_request_options_with_mediation_required(manifest_filename) {
   if (manifest_filename === undefined) {
     manifest_filename = "manifest.py";
   }
@@ -60,19 +61,20 @@ credential-management/support/fedcm/${manifest_filename}`;
       providers: [{
         configURL: manifest_path,
         clientId: '1',
-        nonce: '2',
-      }]
+        nonce: '2'
+      }],
+      mediation: 'required'
     }
   };
 }
 
 // Returns FedCM CredentialRequestOptions with auto re-authentication.
 // succeeds.
-export function request_options_with_auto_reauthn(manifest_filename) {
-  let options = default_request_options(manifest_filename);
+export function request_options_with_mediation_optional(manifest_filename) {
+  let options = alt_request_options_with_mediation_required(manifest_filename);
   // Approved client
   options.identity.providers[0].clientId = '123';
-  options.identity.autoReauthn = true;
+  options.identity.mediation = 'optional';
 
   return options;
 }
