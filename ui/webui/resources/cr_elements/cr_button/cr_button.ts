e470c8cc1ab61c1a7fa894537677ea4f0457e0d9
Wenbo Jie <wenbojie@chromium.org>
2023-06-07 17:51:05 +0000

    [CrButton] Clear ripple after blur
    Scenario: Click a button via Space key, a keydown event will be
    triggered and `spaceKeyDown_` is set to true. If the click of the
    button triggers a focus change (e.g. opens a popup menu), even the
    keyup event is triggered, the ripple won't be cleared because of
    `spaceKeyDown_ = false`.
    
    This CL clears the ripple in the blur event handler, making sure
    every time we set `spaceKeyDown_` to false, we clear the ripple at
    the same time.
    
    Demo:
    before: http://go/scrcast/NTQ4MTUyNDgyMTQ5MTcxMnxiNTBiNDBiYy1kMQ
    after: http://go/scrcast/NjA2MjU5Mzk2Njg2NjQzMnw2NmNhMWE3Ni01OQ
    
    Bug: b:284909919
    Change-Id: I5511a879075d252736d1a01689dd510def15f5ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574717
    Auto-Submit: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154505}

--- ui/webui/resources/cr_elements/cr_button/cr_button.ts
+++ ui/webui/resources/cr_elements/cr_button/cr_button.ts
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * enter to effectively click the button and fire a 'click' event. It can also
  * style an icon inside of the button with the [has-icon] attribute.
  */
-import '//resources/polymer/v3_0/paper-styles/color.js';
 import '../cr_hidden_style.css.js';
 import '../cr_shared_vars.css.js';
+import '//resources/polymer/v3_0/paper-styles/color.js';
 
 import {FocusOutlineManager} from '//resources/js/focus_outline_manager.js';
 import {PaperRippleBehavior} from '//resources/polymer/v3_0/paper-behaviors/paper-ripple-behavior.js';
@@ -165,6 +165,10 @@ export class CrButtonElement extends CrButtonElementBase {
 
   private onBlur_() {
     this.spaceKeyDown_ = false;
+    // If a keyup event is never fired (e.g. after keydown the focus is moved to
+    // another element), we need to clear the ripple here. 100ms delay was
+    // chosen manually as a good time period for the ripple to be visible.
+    this.setTimeout_(() => this.getRipple().uiUpAction(), 100);
   }
 
   private onClick_(e: Event) {
