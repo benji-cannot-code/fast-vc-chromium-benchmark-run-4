39e678ea757f785b05060f7f53ee3be053f0790a
Ken Rockot <rockot@google.com>
2023-06-07 20:15:42 +0000

    ipcz: Transaction identifiers for BeginPut() and EndPut()
    To support overlapping two-phase puts, EndPut() uses the transaction's
    data address as a stable identifier for the specific transaction to be
    terminated.
    
    This is not ideal since some transactions have no data associated with
    them (and we have to fabricate an address), and two-phase gets would
    have a similar problem. We'll want some semantic consistency between the
    two APIs, so we should stabilize around something cleaner.
    
    This change introduces a new opaque identifier type, IpczTransaction,
    and uses it as a lightweight transaction reference to be output by
    BeginPut() and consumed by EndPut(). No behavioral changes otherwise.
    
    A follow-up change will do something similar for BeginGet() and
    EndGet() while adding support for overlapping transactions.
    
    Bug: 1451625
    Change-Id: Id8a4f4383fa9716731bab4f60df47d91c05eb0e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594838
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154574}

--- third_party/ipcz/src/ipcz/portal.h
+++ third_party/ipcz/src/ipcz/portal.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipcz/api_object.h"
 #include "ipcz/ipcz.h"
 #include "ipcz/parcel.h"
+#include "ipcz/pending_transaction_set.h"
 #include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 #include "third_party/abseil-cpp/absl/synchronization/mutex.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -52,12 +53,13 @@ class Portal : public APIObjectImpl<Portal, APIObject::kPortal> {
   IpczResult Put(absl::Span<const uint8_t> data,
                  absl::Span<const IpczHandle> handles);
   IpczResult BeginPut(IpczBeginPutFlags flags,
-                      size_t& num_data_bytes,
-                      void*& data);
-  IpczResult CommitPut(const void* data,
-                       size_t num_data_bytes_produced,
-                       absl::Span<const IpczHandle> handles);
-  IpczResult AbortPut(const void* data);
+                      void** data,
+                      size_t* num_bytes,
+                      IpczTransaction* transaction);
+  IpczResult EndPut(IpczTransaction transaction,
+                    size_t num_bytes_produced,
+                    absl::Span<const IpczHandle> handles,
+                    IpczEndPutFlags flags);
 
   IpczResult Get(IpczGetFlags flags,
                  void* data,
@@ -79,14 +81,8 @@ class Portal : public APIObjectImpl<Portal, APIObject::kPortal> {
   const Ref<Router> router_;
 
   absl::Mutex mutex_;
-
+  PendingTransactionSet pending_puts_ ABSL_GUARDED_BY(mutex_);
   bool in_two_phase_get_ ABSL_GUARDED_BY(mutex_) = false;
-
-  // Tracks parcels being built for two-phase put operations. The most common
-  // case is a single concurrent put, so this case is optimized to store an
-  // inlined Parcel object with no hash table.
-  using PendingParcelMap = absl::flat_hash_map<const void*, Parcel>;
-  absl::variant<absl::monostate, Parcel, PendingParcelMap> pending_parcels_;
 };
 
 }  // namespace ipcz
