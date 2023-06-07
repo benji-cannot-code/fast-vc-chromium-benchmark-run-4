0fcb52d319b5bb9c1cadcb8ab108119614af38a5
adamta <adamta@google.com>
2023-06-07 20:50:01 +0000

    [iOS] Fix ifndefs for NTP field trials
    Change-Id: I710d75a494ea2a2660e4915a2897cb0c23a35df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598623
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154596}

--- ios/chrome/browser/ui/ntp/new_tab_page_field_trial.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_field_trial.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_NTP_new_tab_page_field_trial_H_
-#define IOS_CHROME_BROWSER_UI_NTP_new_tab_page_field_trial_H_
+#ifndef IOS_CHROME_BROWSER_UI_NTP_NEW_TAB_PAGE_FIELD_TRIAL_H_
+#define IOS_CHROME_BROWSER_UI_NTP_NEW_TAB_PAGE_FIELD_TRIAL_H_
 
 #import "base/metrics/field_trial.h"
 #import "components/variations/variations_associated_data.h"
@@ -34,4 +34,4 @@ void CreateNewTabPageFieldTrialForTesting(
 
 }  // namespace new_tab_page_field_trial
 
-#endif  // IOS_CHROME_BROWSER_UI_NTP_new_tab_page_field_trial_H_
+#endif  // IOS_CHROME_BROWSER_UI_NTP_NEW_TAB_PAGE_FIELD_TRIAL_H_
--- ios/chrome/browser/ui/ntp/new_tab_page_field_trial_constants.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_field_trial_constants.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_NTP_new_tab_page_field_trial_CONSTANTS_H_
-#define IOS_CHROME_BROWSER_UI_NTP_new_tab_page_field_trial_CONSTANTS_H_
+#ifndef IOS_CHROME_BROWSER_UI_NTP_NEW_TAB_PAGE_FIELD_TRIAL_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_UI_NTP_NEW_TAB_PAGE_FIELD_TRIAL_CONSTANTS_H_
 
 #import "components/variations/variations_associated_data.h"
 
@@ -31,4 +31,4 @@ const int kTileAblationPrestableWeight = 0;
 
 }  // namespace field_trial_constants
 
-#endif  // IOS_CHROME_BROWSER_UI_NTP_new_tab_page_field_trial_CONSTANTS_H_
+#endif  // IOS_CHROME_BROWSER_UI_NTP_NEW_TAB_PAGE_FIELD_TRIAL_CONSTANTS_H_
