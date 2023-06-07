4f6ecd7ddd4c53a2987feaab02d0607ee199ce8e
Lily Chen <chlily@chromium.org>
2023-06-07 17:27:24 +0000

    [DownloadBubble] Refactor views pt. 3: Move Incognito row to dialog
    This moves the incognito info row to the dialog rather than the
    scrolling view. The row informs the user that the files they download in
    OTR mode are visible to anyone on the device. This should stay at the
    top of the dialog when the rows scroll, rather than scroll with the
    rows.
    
    Screencasts:
    https://drive.google.com/drive/folders/1L_z6r4PnASzsYV150stQDDp2O2tO3HDR
    
    Bug: 1450660, 1442691
    Change-Id: I3a1eaab6903fff7b152b0bd270af4874ddc3287b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583050
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154485}

--- chrome/browser/ui/views/download/bubble/download_bubble_row_list_view.h
+++ chrome/browser/ui/views/download/bubble/download_bubble_row_list_view.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 namespace views {
-class ImageView;
 class ScrollView;
 }  // namespace views
 
@@ -32,17 +31,14 @@ class DownloadBubbleRowListView : public views::FlexLayoutView {
       std::vector<DownloadUIModel::DownloadUIModelPtr> rows,
       int fixed_width);
 
-  explicit DownloadBubbleRowListView(base::WeakPtr<Browser> browser);
+  DownloadBubbleRowListView();
   ~DownloadBubbleRowListView() override;
   DownloadBubbleRowListView(const DownloadBubbleRowListView&) = delete;
   DownloadBubbleRowListView& operator=(const DownloadBubbleRowListView&) =
       delete;
 
- private:
-  bool IsIncognitoInfoRowEnabled();
-
-  base::WeakPtr<Browser> browser_ = nullptr;
-  raw_ptr<views::ImageView> info_icon_ = nullptr;
+  // TODO(crbug.com/1344515): Add functionality for adding a new download while
+  // this is already open.
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_BUBBLE_ROW_LIST_VIEW_H_
