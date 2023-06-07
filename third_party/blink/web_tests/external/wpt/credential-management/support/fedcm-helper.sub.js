a827e87d41070cbf4a26ad03dc5b10c48e4744d5
Nicolás Peña <npm@chromium.org>
2023-06-07 21:04:46 +0000

    [FedCM] Fix mediation placing in WPTs
    Change-Id: I98c53c6485ea655b3b9a8bff98ea6b6932efcd0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598169
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154606}

--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
@@ -42,9 +42,9 @@ credential-management/support/fedcm/${manifest_filename}`;
         configURL: manifest_path,
         clientId: '1',
         nonce: '2'
-      }],
-      mediation: 'required'
-    }
+      }]
+    },
+    mediation: 'required'
   };
 }
 
@@ -62,9 +62,9 @@ credential-management/support/fedcm/${manifest_filename}`;
         configURL: manifest_path,
         clientId: '1',
         nonce: '2'
-      }],
-      mediation: 'required'
-    }
+      }]
+    },
+    mediation: 'required'
   };
 }
 
@@ -74,7 +74,7 @@ export function request_options_with_mediation_optional(manifest_filename) {
   let options = alt_request_options_with_mediation_required(manifest_filename);
   // Approved client
   options.identity.providers[0].clientId = '123';
-  options.identity.mediation = 'optional';
+  options.mediation = 'optional';
 
   return options;
 }
