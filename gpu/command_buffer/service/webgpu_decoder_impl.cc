fdc55bd683d3516095142d49311d818066159eb5
Loko Kung <lokokung@google.com>
2023-06-07 19:09:37 +0000

    Adds feature flag for WebGPU blob cache and use it.
    Change-Id: I4e64dbbc56cf578e89bb19ae7cf5eb8dfe23f86c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4597096
    Reviewed-by: Gregg Tavares <gman@chromium.org>
    Commit-Queue: Loko Kung <lokokung@google.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154543}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/bits.h"
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/shared_image/shared_image_representation.h"
 #include "gpu/command_buffer/service/skia_utils.h"
 #include "gpu/command_buffer/service/webgpu_decoder.h"
+#include "gpu/config/gpu_finch_features.h"
 #include "gpu/config/gpu_preferences.h"
 #include "gpu/config/webgpu_blocklist.h"
 #include "gpu/webgpu/callback.h"
@@ -1082,9 +1084,10 @@ WebGPUDecoderImpl::WebGPUDecoderImpl(
           std::make_unique<SharedImageRepresentationFactory>(
               shared_image_manager,
               memory_tracker)),
-      dawn_platform_(new DawnPlatform(gpu_preferences.enable_unsafe_webgpu
-                                          ? std::move(dawn_caching_interface)
-                                          : nullptr)),
+      dawn_platform_(new DawnPlatform(
+          base::FeatureList::IsEnabled(features::kWebGPUBlobCache)
+              ? std::move(dawn_caching_interface)
+              : nullptr)),
       dawn_instance_(
           DawnInstance::Create(dawn_platform_.get(), gpu_preferences)),
       memory_transfer_service_(new DawnServiceMemoryTransferService(this)),
