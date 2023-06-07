bc6baf3221dff26795fb3fead7e1924a7b58a242
W. James MacLean <wjmaclean@chromium.org>
2023-06-07 00:45:07 +0000

    Remove diagnostic code for RWHVB alloc/dealloc.
    https://chromium-review.googlesource.com/c/chromium/src/+/2831132 added
    diagnostic code for the associated crbug issue. That has now been
    resolved, so the diagnostic code can be removed.
    
    Bug: 1197154
    Change-Id: Ic280075dee7809a5e7e9592984662681e0aa8828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594057
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154188}

--- content/browser/renderer_host/render_widget_host_view_base.cc
+++ content/browser/renderer_host/render_widget_host_view_base.cc
@@ -46,32 +46,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/size_f.h"
 
-namespace {
-using RenderWidgetHostViewBaseAllocMap = std::unordered_map<const void*, int>;
-base::LazyInstance<RenderWidgetHostViewBaseAllocMap>::DestructorAtExit
-    g_alloc_dealloc_tracker_map = LAZY_INSTANCE_INITIALIZER;
-}  // namespace
-
 namespace content {
-
-// static
-int RenderWidgetHostViewBase::IsValidRWHVBPointer(
-    const RenderWidgetHostViewBase* view) {
-  if (!base::Contains(g_alloc_dealloc_tracker_map.Get(),
-                      static_cast<const void*>(view))) {
-    return -1;
-  }
-  return g_alloc_dealloc_tracker_map.Get()[view];
-}
 
 RenderWidgetHostViewBase::RenderWidgetHostViewBase(RenderWidgetHost* host)
     : host_(RenderWidgetHostImpl::From(host)),
       // `screen_infos_` must be initialized, to permit unconditional access to
       // its current display. A placeholder ScreenInfo is used here, so the
       // first call to UpdateScreenInfo will trigger the expected updates.
-      screen_infos_(display::ScreenInfos(display::ScreenInfo())) {
-  g_alloc_dealloc_tracker_map.Get()[this]++;
-}
+      screen_infos_(display::ScreenInfos(display::ScreenInfo())) {}
 
 RenderWidgetHostViewBase::~RenderWidgetHostViewBase() {
   DCHECK(!keyboard_locked_);
@@ -88,7 +70,6 @@ RenderWidgetHostViewBase::~RenderWidgetHostViewBase() {
   // so that the |text_input_manager_| will free its state.
   if (text_input_manager_)
     text_input_manager_->Unregister(this);
-  g_alloc_dealloc_tracker_map.Get()[this]--;
 }
 
 RenderWidgetHostImpl* RenderWidgetHostViewBase::GetFocusedWidget() const {
