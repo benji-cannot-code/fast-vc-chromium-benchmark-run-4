cd253aee35e3d2093d90683cb28214cdaf5085ad
Wez <wez@chromium.org>
2023-06-07 13:27:57 +0000

    [base] Only allocate executable-mappable pages for V8 JIT
    Respect the V8-supplied no-access-will-JIT-later permissions mode when
    creating new allocations, on platforms which support it:
    
    If an allocation is created with will-JIT-later then executable pages
    are allocated.  Non-JIT allocations will fail if the caller attempts
    to change their permissions to allow code execution.
    
    Bug: 1296146, 1296386
    Change-Id: I7770825cfea5c33b67e96f8482c7123c07e8b761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4427816
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154378}

--- base/allocator/partition_allocator/page_allocator_internals_win.h
+++ base/allocator/partition_allocator/page_allocator_internals_win.h
@@ -95,10 +95,10 @@ int GetAccessFlags(PageAccessibilityConfiguration accessibility) {
     case PageAccessibilityConfiguration::kReadWriteExecute:
       return PAGE_EXECUTE_READWRITE;
     case PageAccessibilityConfiguration::kInaccessible:
+    case PageAccessibilityConfiguration::kInaccessibleWillJitLater:
       return PAGE_NOACCESS;
-    default:
-      PA_NOTREACHED();
   }
+  PA_NOTREACHED();
 }
 
 uintptr_t SystemAllocPagesInternal(
