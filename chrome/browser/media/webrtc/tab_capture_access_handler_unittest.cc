d348af9aaaa7026bab4e1f4a5067ed67142f50b1
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-06-07 23:53:37 +0000

    [Extensions] Enable tabCapture with workers and offscreen documents
    MediaStreams are unavailable in ServiceWorker contexts. This means
    the tabCapture API doesn't work in Manifest V3 background contexts
    (since these are service worker-based). The primary alternative to
    functionality that's tied to a DOM or Window context is to use
    offscreen documents; however, these (by design) do not have access
    to extension APIs, which means they can't access the tabCapture API.
    This effectively means there's no good way to use the tabCapture
    API in a background context in Manifest V3.
    
    Fix this by:
    - Allowing service worker contexts to use
      `tabCapture.getMediaStreamId()`. This returns an ID that can
      later be used to fetch a media stream, rather than returning the
      MediaStream object itself. As such, it's (conceptually)
      compatible with service worker contexts.
    - Allowing offscreen documents to use the media stream ID to
      start capture via the navigator.getUserMedia() API (which is
      what tabCapture.capture() already used).
    
    In practice, this entails allowing extension contexts to use a
    media stream generated by another context *on the same origin*, rather
    than restricting it to the same frame that created the stream ID.
    (This isn't a change in security or privacy, since the extension could
    already pass any of this information between its own contexts.)
    Additionally, update `tabCapture.getMediaStreamId()`'s implementation
    to not rely on the presence of a RenderFrame.
    
    Add an end-to-end test using offscreen documents to capture the
    stream of an active tab.
    
    Bug: 1400269
    Change-Id: I1e0f0a46d098e56357881ed64dfd249b6b692936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4514180
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154676}

--- chrome/browser/media/webrtc/tab_capture_access_handler_unittest.cc
+++ chrome/browser/media/webrtc/tab_capture_access_handler_unittest.cc
@@ -97,6 +97,12 @@ class TabCaptureAccessHandlerTest : public ChromeRenderViewHostTestHarness {
     access_handler_.reset();
   }
 
+  content::RenderFrameHost* main_frame() {
+    return web_contents()->GetPrimaryMainFrame();
+  }
+  int main_frame_id() { return main_frame()->GetRoutingID(); }
+  int process_id() { return main_frame()->GetProcess()->GetID(); }
+
  protected:
   std::unique_ptr<TabCaptureAccessHandler> access_handler_;
 };
@@ -111,7 +117,7 @@ TEST_F(TabCaptureAccessHandlerTest, PermissionGiven) {
 
   extensions::TabCaptureRegistry::Get(profile())->AddRequest(
       web_contents(), /*extension_id=*/"", /*is_anonymous=*/false,
-      GURL(kOrigin), source, web_contents());
+      GURL(kOrigin), source, process_id(), main_frame_id());
 
   blink::mojom::MediaStreamRequestResult result = kInvalidResult;
   blink::mojom::StreamDevices devices;
@@ -146,7 +152,7 @@ TEST_F(TabCaptureAccessHandlerTest, DlpRestricted) {
 
   extensions::TabCaptureRegistry::Get(profile())->AddRequest(
       web_contents(), /*extension_id=*/"", /*is_anonymous=*/false,
-      GURL(kOrigin), source, web_contents());
+      GURL(kOrigin), source, process_id(), main_frame_id());
 
   blink::mojom::MediaStreamRequestResult result = kInvalidResult;
   blink::mojom::StreamDevices devices;
@@ -178,7 +184,7 @@ TEST_F(TabCaptureAccessHandlerTest, DlpNotRestricted) {
 
   extensions::TabCaptureRegistry::Get(profile())->AddRequest(
       web_contents(), /*extension_id=*/"", /*is_anonymous=*/false,
-      GURL(kOrigin), source, web_contents());
+      GURL(kOrigin), source, process_id(), main_frame_id());
 
   blink::mojom::MediaStreamRequestResult result = kInvalidResult;
   blink::mojom::StreamDevices devices;
@@ -211,7 +217,7 @@ TEST_F(TabCaptureAccessHandlerTest, DlpWebContentsDestroyed) {
 
   extensions::TabCaptureRegistry::Get(profile())->AddRequest(
       web_contents(), /*extension_id=*/"", /*is_anonymous=*/false,
-      GURL(kOrigin), source, web_contents());
+      GURL(kOrigin), source, process_id(), main_frame_id());
 
   blink::mojom::MediaStreamRequestResult result = kInvalidResult;
   blink::mojom::StreamDevices devices;
