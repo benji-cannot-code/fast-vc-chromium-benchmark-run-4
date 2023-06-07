165ace45e29a2ecb959cb6b8dd15237e3a942dcc
Avi Drissman <avi@chromium.org>
2023-06-07 05:41:29 +0000

    Convert /components/remote_cocoa/browser to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: I213a14a909791b670abbcb0959cbfd1c4062114f
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591635
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154263}

--- components/remote_cocoa/browser/window.mm
+++ components/remote_cocoa/browser/window.mm
@@ -6,10 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/remote_cocoa/browser/window.h"
 
 #import <Cocoa/Cocoa.h>
+
 #include <map>
 
 #include "base/check.h"
 #include "base/no_destructor.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace remote_cocoa {
 namespace {
