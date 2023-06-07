54bb2962d82e846cefe35fc75228783ef8582070
Chris Fredrickson <cfredric@chromium.org>
2023-06-07 15:26:33 +0000

    Simplify JS helper scripts for Storage Access API browsertests
    There's no need to translate success/failure into bools in JS; we can
    just use ExecJs instead, which does that translation itself.
    
    Additionally, the callers of HasStorageAccessForFrame never expect it
    to reject, so its script does not need an error handler.
    
    Change-Id: I5467a7032c1ffdc34da6fcc5abdb645d4f26f05b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4596139
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154434}

--- chrome/browser/net/storage_test_utils.cc
+++ chrome/browser/net/storage_test_utils.cc
@@ -29,14 +29,9 @@ constexpr char kRequestStorageAccess[] =
     "  .catch(() => false);";
 
 constexpr char kRequestStorageAccessFor[] =
-    "document.requestStorageAccessFor($1).then("
-    "  () => true,"
-    "  () => false,"
-    ");";
+    "document.requestStorageAccessFor($1)";
 
-constexpr char kHasStorageAccess[] =
-    "document.hasStorageAccess()"
-    "  .catch(() => false);";
+constexpr char kHasStorageAccess[] = "document.hasStorageAccess()";
 
 std::vector<std::string> GetStorageTypesForFrame(bool include_cookies) {
   std::vector<std::string> types(kStorageTypesForFrame);
@@ -140,9 +135,8 @@ bool RequestAndCheckStorageAccessForFrame(content::RenderFrameHost* frame) {
 
 bool RequestStorageAccessForOrigin(content::RenderFrameHost* frame,
                                    const std::string& origin) {
-  return content::EvalJs(frame,
-                         content::JsReplace(kRequestStorageAccessFor, origin))
-      .ExtractBool();
+  return content::ExecJs(frame,
+                         content::JsReplace(kRequestStorageAccessFor, origin));
 }
 
 bool HasStorageAccessForFrame(content::RenderFrameHost* frame) {
