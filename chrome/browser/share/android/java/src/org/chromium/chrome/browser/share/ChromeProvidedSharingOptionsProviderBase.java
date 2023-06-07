3b6197ea892ae60e7f267cfe88a643daef0fe2ff
Wenyu Fu <wenyufu@google.com>
2023-06-07 01:24:53 +0000

    Add notifyOnSuccess param for text / image copy through Clipboard
    Consolidate toast showing functionality into Clipboard API, and remove
    Toast references used in most share sheet custom actions.
    
    This CL also rewrites part of the ClipboardTest - in the previous test
    suite, the generated temp Image URI can be null depending on the test
    environment the test being ran, so although the test is passed, it does
    not hit the critical business logic of testing image URI being saved as
    primary clip to the ClipboardManager.
    
    Bug: 1448655
    Change-Id: I4c540b4607fe71a568493f4736908cc36aa6562a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4581171
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1154202}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/ChromeProvidedSharingOptionsProviderBase.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/ChromeProvidedSharingOptionsProviderBase.java
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.share;
 
 import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Build.VERSION;
 import android.text.TextUtils;
 import android.view.View;
 
@@ -326,8 +325,12 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
 
     protected void maybeAddCopyFirstPartyOption() {
         mOrderedFirstPartyOptions.add(createCopyLinkFirstPartyOption());
-        mOrderedFirstPartyOptions.add(createCopyGifFirstPartyOption());
-        mOrderedFirstPartyOptions.add(createCopyImageFirstPartyOption(true));
+        if (usePolishedActionOrderedList()) {
+            mOrderedFirstPartyOptions.add(createCopyImageFirstPartyOption(false));
+        } else {
+            mOrderedFirstPartyOptions.add(createCopyGifFirstPartyOption());
+            mOrderedFirstPartyOptions.add(createCopyImageFirstPartyOption(true));
+        }
         mOrderedFirstPartyOptions.add(createCopyFirstPartyOption());
         mOrderedFirstPartyOptions.add(createCopyTextFirstPartyOption());
     }
@@ -349,12 +352,8 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
         return builder.setIcon(R.drawable.ic_content_copy_black, R.string.sharing_copy_url)
                 .setFeatureNameForMetrics(USER_ACTION_COPY_URL_SELECTED)
                 .setOnClickCallback((view) -> {
-                    ClipboardManager clipboard = (ClipboardManager) mActivity.getSystemService(
-                            Context.CLIPBOARD_SERVICE);
-                    clipboard.setPrimaryClip(
-                            ClipData.newPlainText(mShareParams.getTitle(), mShareParams.getUrl()));
-                    // TODO(crbug/1448655): Remove toast for Android T+
-                    Toast.makeText(mActivity, R.string.link_copied, Toast.LENGTH_SHORT).show();
+                    Clipboard.getInstance().setText(mShareParams.getTitle(), mShareParams.getUrl(),
+                            /*notifyOnSuccess=*/true);
                 })
                 .build();
     }
@@ -370,8 +369,14 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
                     Uri imageUri = mShareParams.getImageUriToShare();
                     if (imageUri != null) {
                         Clipboard.getInstance().setImageUri(imageUri);
-                        // TODO(crbug/1448655): Move toast into Clipboard#setImageUri.
-                        Toast.makeText(mActivity, R.string.gif_copied, Toast.LENGTH_SHORT).show();
+                        // TODO(crbug/1448589): Remove copy GIF action.
+                        // This is separate from regular image copy due to the string used on the
+                        // toast. To avoid growing complexity to customize text on toast in
+                        // Clipboard, this is logic guarded by version code.
+                        if (VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                            Toast.makeText(mActivity, R.string.gif_copied, Toast.LENGTH_SHORT)
+                                    .show();
+                        }
                     }
                 })
                 .build();
@@ -389,10 +394,8 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
                         .setOnClickCallback((view) -> {
                             Uri imageUri = mShareParams.getImageUriToShare();
                             if (imageUri != null) {
-                                Clipboard.getInstance().setImageUri(imageUri);
-                                // TODO(crbug/1448655): Move toast into Clipboard#setImageUri.
-                                Toast.makeText(mActivity, R.string.image_copied, Toast.LENGTH_SHORT)
-                                        .show();
+                                Clipboard.getInstance().setImageUri(
+                                        imageUri, /*notifyOnSuccess=*/true);
                             }
                         });
         if (excludeGif) {
@@ -406,12 +409,8 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
                 .setIcon(R.drawable.ic_content_copy_black, R.string.sharing_copy)
                 .setFeatureNameForMetrics(USER_ACTION_COPY_SELECTED)
                 .setOnClickCallback((view) -> {
-                    // TODO(crbug/1448655): Switch to Clipboard#setText.
-                    ClipboardManager clipboard = (ClipboardManager) mActivity.getSystemService(
-                            Context.CLIPBOARD_SERVICE);
-                    clipboard.setPrimaryClip(ClipData.newPlainText(
-                            mShareParams.getTitle(), mShareParams.getTextAndUrl()));
-                    Toast.makeText(mActivity, R.string.copied, Toast.LENGTH_SHORT).show();
+                    Clipboard.getInstance().setText(mShareParams.getTitle(),
+                            mShareParams.getTextAndUrl(), /*notifyOnSuccess=*/true);
                 })
                 .build();
     }
@@ -422,12 +421,8 @@ public abstract class ChromeProvidedSharingOptionsProviderBase {
                 .setIcon(R.drawable.ic_content_copy_black, R.string.sharing_copy_text)
                 .setFeatureNameForMetrics(USER_ACTION_COPY_TEXT_SELECTED)
                 .setOnClickCallback((view) -> {
-                    // TODO(crbug/1448655): Switch to Clipboard#setText.
-                    ClipboardManager clipboard = (ClipboardManager) mActivity.getSystemService(
-                            Context.CLIPBOARD_SERVICE);
-                    clipboard.setPrimaryClip(
-                            ClipData.newPlainText(mShareParams.getTitle(), mShareParams.getText()));
-                    Toast.makeText(mActivity, R.string.text_copied, Toast.LENGTH_SHORT).show();
+                    Clipboard.getInstance().setText(mShareParams.getTitle(), mShareParams.getText(),
+                            /*notifyOnSuccess=*/true);
                 })
                 .build();
     }
--- ui/android/junit/src/org/chromium/ui/widget/ToastManagerTest.java
+++ ui/android/junit/src/org/chromium/ui/widget/ToastManagerTest.java
@@ -57,7 +57,7 @@ public class ToastManagerTest {
         ToastManager.resetForTesting();
         clearInvocations(mAndroidToastObject);
         clearInvocations(mAndroidToastObjectNext);
-        ToastManager.setEnabledForTesting(false);
+        ToastManager.setEnabledForTesting(null);
     }
 
     private static void waitForIdleUi() {
