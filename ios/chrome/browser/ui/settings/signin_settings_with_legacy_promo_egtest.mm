1a236bd0c7d9a266d36d9902e7e42f7e81e45e90
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-06-07 17:06:54 +0000

    [ios] Add feature flag to remove sync promo from top-level settings
    Fixed: 1450472
    Change-Id: Ia50ec822ef52a30923e7a4ca63a4c833404e7ef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577202
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154475}

--- ios/chrome/browser/ui/settings/signin_settings_with_legacy_promo_egtest.mm
+++ ios/chrome/browser/ui/settings/signin_settings_with_legacy_promo_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/policy/policy_constants.h"
 #import "ios/chrome/browser/policy/policy_app_interface.h"
 #import "ios/chrome/browser/policy/policy_earl_grey_utils.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/cells/signin_promo_view_constants.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_constants.h"
@@ -28,22 +29,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+using chrome_test_util::ButtonWithAccessibilityLabelId;
 using chrome_test_util::IdentityCellMatcherForEmail;
 using chrome_test_util::PrimarySignInButton;
 using chrome_test_util::SecondarySignInButton;
 using chrome_test_util::SettingsAccountButton;
 using chrome_test_util::SettingsDoneButton;
-using chrome_test_util::ButtonWithAccessibilityLabelId;
 
-@interface SigninSettingsTestCase : ChromeTestCase
+@interface SigninSettingsWithLegacyPromoTestCase : ChromeTestCase
 @end
 
-@implementation SigninSettingsTestCase
+@implementation SigninSettingsWithLegacyPromoTestCase
 
 - (void)tearDown {
   [PolicyAppInterface clearPolicies];
 
   [super tearDown];
+}
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  config.features_disabled.push_back(kHideSettingsSyncPromo);
+  return config;
 }
 
 // Tests the primary button with no accounts on the device.
