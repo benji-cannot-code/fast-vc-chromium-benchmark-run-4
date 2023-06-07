c663f08136e7197d63d39c8342a0c00f05968de1
Jack Yammine <jyammine@google.com>
2023-06-07 08:21:36 +0000

    [waffle]Add buttons to the UI
    Screenshot: http://b/280753754#comment11
    Bug: b/280753754
    Change-Id: Id3efc6849ef80724e0c3f301b9cdf5c260b8db28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590494
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154300}

--- chrome/browser/resources/waffle/app.ts
+++ chrome/browser/resources/waffle/app.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './strings.m.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 
 import {afterNextRender, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
