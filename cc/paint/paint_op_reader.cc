cb401b4467d50c812aa30f9ebd2765dacac57102
Michael Ludwig <michaelludwig@google.com>
2023-06-08 22:00:47 +0000

    Reland "Validate SkColor4f in PaintOpReader"
    This reverts commit 8cf4af57ccdb6a794e8d71f4d848a25966a9c2bc.
    
    Reason for revert: check valid_ before checking finite-ness
    
    Original change's description:
    > Revert "Validate SkColor4f in PaintOpReader"
    >
    > This reverts commit a5886ffcb38276edeba64f4be376b3ee0f62fbfc.
    >
    > Reason for revert: Suspected culprit for MSAN test failures
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/36340/overview
    >
    > Original change's description:
    > > Validate SkColor4f in PaintOpReader
    > >
    > > Bug: 1450507
    > > Change-Id: I9c1f8cecd3a1b82f996120bb15f970fae6fe198b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598402
    > > Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    > > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1154879}
    >
    > Bug: 1450507
    > Change-Id: I1fa0f4966da474b32a1ba5ff38c9d1de750600d9
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600675
    > Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Owners-Override: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1154952}
    
    Bug: 1450507
    Change-Id: Ic1c3f7ec5292aec766dc1c6f16732c8d066a5469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600878
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Cr-Commit-Position: refs/heads/main@{#1155119}

--- cc/paint/paint_op_reader.cc
+++ cc/paint/paint_op_reader.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRRect.h"
+#include "third_party/skia/include/core/SkScalar.h"
 #include "third_party/skia/include/core/SkSerialProcs.h"
 #include "third_party/skia/include/effects/SkHighContrastFilter.h"
 #include "third_party/skia/include/private/SkGainmapInfo.h"
@@ -236,6 +237,13 @@ void PaintOpReader::Read(SkRRect* rect) {
 
 void PaintOpReader::Read(SkColor4f* color) {
   ReadSimple(color);
+  // Colors are generally [0, 1], sometimes with a wider gamut, but
+  // infinite and NaN colors don't make sense and shouldn't be produced by a
+  // renderer, so encountering a non-finite color implies the paint op buffer
+  // is invalid.
+  if (valid_ && !SkScalarsAreFinite(color->vec(), 4)) {
+    SetInvalid(DeserializationError::kNonFiniteSkColor4f);
+  }
 }
 
 void PaintOpReader::Read(SkPath* path) {
@@ -292,7 +300,7 @@ void PaintOpReader::Read(SkPath* path) {
 }
 
 void PaintOpReader::Read(PaintFlags* flags) {
-  ReadSimple(&flags->color_);
+  Read(&flags->color_);
   Read(&flags->width_);
   Read(&flags->miter_limit_);
 
