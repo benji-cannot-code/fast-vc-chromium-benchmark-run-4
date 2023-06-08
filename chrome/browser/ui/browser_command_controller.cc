dc45718ad497aea274cd4ee224f10259a1e33a6c
Emily Shack <emshack@chromium.org>
2023-06-08 18:19:01 +0000

    Bookmarks++: Fix menu item crash
    Attempts to fix the crash without introducing a new menu item
    
    Bug: 1452202
    Change-Id: I22788c5e66140e88c47b6407efde63356705d48c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598355
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1155000}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -1339,9 +1339,8 @@ void BrowserCommandController::InitCommandState() {
   }
 #endif
 
-  if (browser_->is_type_normal() &&
-      (features::IsChromeRefresh2023() ||
-       base::FeatureList::IsEnabled(features::kPowerBookmarksSidePanel))) {
+  if ((browser_->is_type_normal() && features::IsChromeRefresh2023()) ||
+      base::FeatureList::IsEnabled(features::kPowerBookmarksSidePanel)) {
     command_updater_.UpdateCommandEnabled(IDC_SHOW_BOOKMARK_SIDE_PANEL, true);
   }
 
