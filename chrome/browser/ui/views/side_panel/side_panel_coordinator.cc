acf8409777099fe94b708e41be4d10e813a616e1
Demetrios Papadopoulos <dpapad@chromium.org>
2023-06-08 23:22:23 +0000

    Revert "Update side panel background for chrome desktop refresh 2023."
    This reverts commit f7e6c016db1d3f91c305ce27ee52cf68407a2fb6.
    
    Reason for revert: SidePanel does not respond to clicks with ChromeRefresh2023 flag on, see https://crbug.com/1453385.
    
    Original change's description:
    > Update side panel background for chrome desktop refresh 2023.
    >
    > Updates the background to surround the side panel content so the header
    > is now over the background. This cl also updates the combobox background
    > color and text color.
    >
    > Bug: 1399966
    > Change-Id: I19a6f0d2f0d17c6cee904da0900524aa0022961b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591332
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Commit-Queue: Caroline Rising <corising@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1154612}
    
    Bug: 1399966
    Fixed: 1453385
    Change-Id: Ia3789daaa4d9a082dfd0f63356238b659e28f3cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602026
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155210}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
@@ -515,14 +515,9 @@ void SidePanelCoordinator::InitializeSidePanel() {
   container->SetCrossAxisAlignment(views::LayoutAlignment::kStretch);
   container->SetID(kSidePanelContentViewId);
 
-  auto header = CreateHeader();
-  if (features::IsChromeRefresh2023()) {
-    browser_view_->unified_side_panel()->AddHeaderView(std::move(header));
-  } else {
-    container->AddChildView(std::move(header));
-    container->AddChildView(std::make_unique<views::Separator>())
-        ->SetColorId(kColorSidePanelContentAreaSeparator);
-  }
+  container->AddChildView(CreateHeader());
+  container->AddChildView(std::make_unique<views::Separator>())
+      ->SetColorId(kColorSidePanelContentAreaSeparator);
 
   auto content_wrapper = std::make_unique<SidePanelContentSwappingContainer>(
       no_delays_for_testing_);
@@ -641,22 +636,16 @@ SidePanelRegistry* SidePanelCoordinator::GetActiveContextualRegistry() const {
 
 std::unique_ptr<views::View> SidePanelCoordinator::CreateHeader() {
   auto header = std::make_unique<views::FlexLayoutView>();
+  // ChromeLayoutProvider for providing margins.
+  ChromeLayoutProvider* const chrome_layout_provider =
+      ChromeLayoutProvider::Get();
 
-  if (!features::IsChromeRefresh2023()) {
-    // ChromeLayoutProvider for providing margins.
-    ChromeLayoutProvider* const chrome_layout_provider =
-        ChromeLayoutProvider::Get();
-
-    // Set the interior margins of the header on the left and right sides.
-    const int horizontal_margin = chrome_layout_provider->GetDistanceMetric(
-        ChromeDistanceMetric::
-            DISTANCE_SIDE_PANEL_HEADER_INTERIOR_MARGIN_HORIZONTAL);
-    header->SetInteriorMargin(
-        gfx::Insets::TLBR(0, horizontal_margin, 0, horizontal_margin * 2));
-    header->SetBackground(
-        views::CreateThemedSolidBackground(ui::kColorWindowBackground));
-  }
-
+  // Set the interior margins of the header on the left and right sides.
+  const int horizontal_margin = chrome_layout_provider->GetDistanceMetric(
+      ChromeDistanceMetric::
+          DISTANCE_SIDE_PANEL_HEADER_INTERIOR_MARGIN_HORIZONTAL);
+  header->SetInteriorMargin(
+      gfx::Insets::TLBR(0, horizontal_margin, 0, horizontal_margin * 2));
   // Set alignments for horizontal (main) and vertical (cross) axes.
   header->SetMainAxisAlignment(views::LayoutAlignment::kStart);
   header->SetCrossAxisAlignment(views::LayoutAlignment::kCenter);
@@ -664,6 +653,8 @@ std::unique_ptr<views::View> SidePanelCoordinator::CreateHeader() {
   // The minimum cross axis size should the expected height of the header.
   constexpr int kDefaultSidePanelHeaderHeight = 40;
   header->SetMinimumCrossAxisSize(kDefaultSidePanelHeaderHeight);
+  header->SetBackground(
+      views::CreateThemedSolidBackground(ui::kColorWindowBackground));
 
   header_combobox_ = header->AddChildView(CreateCombobox());
   header_combobox_->SetFocusBehavior(views::View::FocusBehavior::ALWAYS);
@@ -721,9 +712,6 @@ std::unique_ptr<views::Combobox> SidePanelCoordinator::CreateCombobox() {
           .WithAlignment(views::LayoutAlignment::kStart));
   combobox->SetBorderColorId(ui::kColorSidePanelComboboxBorder);
   combobox->SetBackgroundColorId(ui::kColorSidePanelComboboxBackground);
-  if (features::IsChromeRefresh2023()) {
-    combobox->SetForegroundColorId(ui::kColorSysOnSurface);
-  }
   combobox->SetEventHighlighting(true);
   combobox->SetSizeToLargestLabel(false);
   return combobox;
