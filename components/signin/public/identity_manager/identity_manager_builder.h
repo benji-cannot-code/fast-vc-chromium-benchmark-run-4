035f41f5a630e7d19774c4ba58b86795fdddff70
Alex Ilin <alexilin@chromium.org>
2023-06-08 12:57:51 +0000

    [DBSC] Add TokenBindingHelper class for managing token binding keys
    TokenBindingHelper allows generating binding key assertions from
    wrapped unexportable keys that can be stored on disk.
    
    This CL
    1. Adds a new TokenBindingHelper class.
    2. Adds support for generating assertion JWT to
    components/signin/public/base/session_binding_utils.h
    3. Adds code for registering binding keys to
    MutableProfileOAuth2TokenServiceDelegate
    4. Propagates all necessary dependencies to create TokenBindingHelper
    from identity_manager_factory.cc to
    profile_oauth2_token_service_builder.h
    
    Bug: b/274463812
    Change-Id: I7df5aced994d21ee8bf17c118dc8b43e643dd103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584432
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154869}

--- components/signin/public/identity_manager/identity_manager_builder.h
+++ components/signin/public/identity_manager/identity_manager_builder.h
@@ -28,6 +28,11 @@ class SigninClient;
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 class TokenWebData;
+#if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
+namespace unexportable_keys {
+class UnexportableKeyService;
+}
+#endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
 #endif
 
 #if BUILDFLAG(IS_IOS)
@@ -72,6 +77,9 @@ struct IdentityManagerBuildParams {
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   scoped_refptr<TokenWebData> token_web_data;
+#if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
+  raw_ptr<unexportable_keys::UnexportableKeyService> unexportable_key_service;
+#endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS)
