da353dd8c9531aa9b3cb93604d6f95c58efef91a
S. Ganesh <ganesh@chromium.org>
2023-06-08 16:45:01 +0000

    Add the `ForceInstallApps` GoogleUpdate policy to chrome://policy
    With r1154554, we are now using `IPolicyStatus3` to get the updater's
    policies and state. This CL adds the `ForceInstallApps` property from
    `IPolicyStatus3` to `chrome://policy`.
    
    Fixed: 1452241
    Change-Id: I185f12379e183445bfa56a5bec3e7009099d7f9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598405
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154951}

--- chrome/browser/google/google_update_policy_fetcher_win.cc
+++ chrome/browser/google/google_update_policy_fetcher_win.cc
@@ -29,8 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// TODO(crbug.com/1453285): Add unit tests for these GoogleUpdate policies.
 constexpr char kAutoUpdateCheckPeriodMinutes[] = "AutoUpdateCheckPeriodMinutes";
 constexpr char kDownloadPreference[] = "DownloadPreference";
+constexpr char kForceInstallApps[] = "ForceInstallApps";
 constexpr char kInstallPolicy[] = "InstallPolicy";
 constexpr char kProxyMode[] = "ProxyMode";
 constexpr char kProxyPacUrl[] = "ProxyPacUrl";
@@ -94,6 +96,13 @@ std::unique_ptr<policy::PolicyMap> GetGoogleUpdatePolicies(
   }
   {
     Microsoft::WRL::ComPtr<IPolicyStatusValue> policy;
+    if (SUCCEEDED(policy_status->get_forceInstallApps(
+            install_static::IsSystemInstall(), &policy))) {
+      AddPolicy(kForceInstallApps, policy.Get(), *policies);
+    }
+  }
+  {
+    Microsoft::WRL::ComPtr<IPolicyStatusValue> policy;
     if (SUCCEEDED(policy_status->get_effectivePolicyForAppInstalls(app_id.Get(),
                                                                    &policy))) {
       AddPolicy(kInstallPolicy, policy.Get(), *policies);
@@ -144,23 +153,27 @@ std::unique_ptr<policy::PolicyMap> GetGoogleUpdatePolicies(
   }
   {
     Microsoft::WRL::ComPtr<IPolicyStatusValue> policy;
-    if (SUCCEEDED(policy_status->get_targetChannel(app_id.Get(), &policy)))
+    if (SUCCEEDED(policy_status->get_targetChannel(app_id.Get(), &policy))) {
       AddPolicy(kTargetChannel, policy.Get(), *policies);
+    }
   }
   {
     Microsoft::WRL::ComPtr<IPolicyStatusValue> policy;
-    if (SUCCEEDED(policy_status->get_proxyMode(&policy)))
+    if (SUCCEEDED(policy_status->get_proxyMode(&policy))) {
       AddPolicy(kProxyMode, policy.Get(), *policies);
+    }
   }
   {
     Microsoft::WRL::ComPtr<IPolicyStatusValue> policy;
-    if (SUCCEEDED(policy_status->get_proxyPacUrl(&policy)))
+    if (SUCCEEDED(policy_status->get_proxyPacUrl(&policy))) {
       AddPolicy(kProxyPacUrl, policy.Get(), *policies);
+    }
   }
   {
     Microsoft::WRL::ComPtr<IPolicyStatusValue> policy;
-    if (SUCCEEDED(policy_status->get_proxyServer(&policy)))
+    if (SUCCEEDED(policy_status->get_proxyServer(&policy))) {
       AddPolicy(kProxyServer, policy.Get(), *policies);
+    }
   }
 
   return policies;
@@ -206,6 +219,7 @@ policy::PolicyConversions::PolicyToSchemaMap GetGoogleUpdatePolicySchemas() {
   return policy::PolicyConversions::PolicyToSchemaMap{{
       {kAutoUpdateCheckPeriodMinutes, policy::Schema()},
       {kDownloadPreference, policy::Schema()},
+      {kForceInstallApps, policy::Schema()},
       {kInstallPolicy, policy::Schema()},
       {kProxyMode, policy::Schema()},
       {kProxyPacUrl, policy::Schema()},
