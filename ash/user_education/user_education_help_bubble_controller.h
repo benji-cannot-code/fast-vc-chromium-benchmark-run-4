89e35a084bd61d003c302a7b97db6a4472ca2800
David Black <dmblack@google.com>
2023-06-08 21:13:09 +0000

    Expose `HelpBubbleMetadata` thru `UserEducationHelpBubbleController`.
    This CL tracks and exposes metadata associated with currently showing
    help bubbles through the `UserEducationHelpBubbleController`.
    
    The user education framework intentionally does not expose help bubble
    instances to clients as their implementation is private and direct
    interaction with them is not permitted.
    
    The exposed metadata will be used by the `WelcomeTourScrim` to clip
    scrims around help bubble anchors views.
    
    Bug: b:277091650
    Change-Id: If43ea3488b00c52d6b9bdf7f42cd2ce8eaf23414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599640
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155084}

--- ash/user_education/user_education_help_bubble_controller.h
+++ ash/user_education/user_education_help_bubble_controller.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_USER_EDUCATION_USER_EDUCATION_HELP_BUBBLE_CONTROLLER_H_
 #define ASH_USER_EDUCATION_USER_EDUCATION_HELP_BUBBLE_CONTROLLER_H_
 
+#include <map>
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/user_education/user_education_types.h"
 #include "base/callback_list.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
@@ -83,14 +85,26 @@ class ASH_EXPORT UserEducationHelpBubbleController {
   [[nodiscard]] base::CallbackListSubscription AddHelpBubbleShownCallback(
       base::RepeatingClosure callback);
 
-  // Invoked by `HelpBubbleViewAsh` when a help bubble's anchor bounds change.
-  void NotifyHelpBubbleAnchorBoundsChanged(base::PassKey<HelpBubbleViewAsh>);
+  // Invoked when the specified `help_bubble_view`'s anchor bounds change.
+  void NotifyHelpBubbleAnchorBoundsChanged(
+      base::PassKey<HelpBubbleViewAsh>,
+      const HelpBubbleViewAsh* help_bubble_view);
 
-  // Invoked by `HelpBubbleViewAsh` when a help bubble is closed.
-  void NotifyHelpBubbleClosed(base::PassKey<HelpBubbleViewAsh>);
+  // Invoked when the specified `help_bubble_view` is closed.
+  void NotifyHelpBubbleClosed(base::PassKey<HelpBubbleViewAsh>,
+                              const HelpBubbleViewAsh* help_bubble_view);
 
-  // Invoked by `HelpBubbleViewAsh` when a help bubble is shown.
-  void NotifyHelpBubbleShown(base::PassKey<HelpBubbleViewAsh>);
+  // Invoked when the specified `help_bubble_view`  is shown.
+  void NotifyHelpBubbleShown(base::PassKey<HelpBubbleViewAsh>,
+                             const HelpBubbleViewAsh* help_bubble_view);
+
+  // Returns metadata for all currently showing help bubbles. Note that help
+  // bubbles are closed asynchronously so it is possible for multiple help
+  // bubbles to exist concurrently.
+  const std::map<HelpBubbleKey, HelpBubbleMetadata>&
+  help_bubble_metadata_by_key() const {
+    return help_bubble_metadata_by_key_;
+  }
 
  private:
   // The delegate owned by the `UserEducationController` which facilitates
@@ -101,6 +115,11 @@ class ASH_EXPORT UserEducationHelpBubbleController {
   // notified when it closes. Once closed, help bubble related memory is freed.
   std::unique_ptr<user_education::HelpBubble> help_bubble_;
   base::CallbackListSubscription help_bubble_close_subscription_;
+
+  // Metadata for all currently showing help bubbles. Note that help bubbles
+  // are closed asynchronously so it is possible for multiple help bubbles to
+  // exist concurrently.
+  std::map<HelpBubbleKey, HelpBubbleMetadata> help_bubble_metadata_by_key_;
 
   // Lists of subscribers to notify for the following events:
   // (a) Help bubble anchor bounds changed
---
+++ ash/user_education/user_education_types.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/user_education/user_education_types.h"
+
+namespace ash {
+
+// HelpBubbleMetadata ----------------------------------------------------------
+
+HelpBubbleMetadata::HelpBubbleMetadata(const HelpBubbleKey key,
+                                       const aura::Window* anchor_root_window,
+                                       const gfx::Rect& anchor_bounds_in_screen)
+    : key(key),
+      anchor_root_window(anchor_root_window),
+      anchor_bounds_in_screen(anchor_bounds_in_screen) {}
+
+HelpBubbleMetadata::~HelpBubbleMetadata() = default;
+
+}  // namespace ash
