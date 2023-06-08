77334d3fc98dc3e99bccd03cd214e20b265731f7
Chris Fredrickson <cfredric@chromium.org>
2023-06-08 18:06:59 +0000

    Remove error handling from requestStorageAccess browsertest helper
    This has no effect on the expected "success" cases, but forces the
    expected "failure" cases to be more explicit about what JS they
    expect to fail. This ultimately makes the tests clearer and more
    easily debuggable.
    
    Change-Id: I73b4b1d50182b1143edce13db1aff8781db27527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598448
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154994}

--- chrome/browser/net/storage_test_utils.cc
+++ chrome/browser/net/storage_test_utils.cc
@@ -25,8 +25,7 @@ const std::vector<std::string> kCrossTabCommunicationTypes{
 
 constexpr char kRequestStorageAccess[] =
     "document.requestStorageAccess()"
-    "  .then(() => document.hasStorageAccess())"
-    "  .catch(() => false);";
+    "  .then(() => document.hasStorageAccess())";
 
 constexpr char kRequestStorageAccessFor[] =
     "document.requestStorageAccessFor($1)";
