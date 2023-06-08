19816d661c027e108e85dab3bf6b320f1b61ce0a
Aashna <aashnajena@google.com>
2023-06-08 17:08:52 +0000

    Destroy existing mOnscreenContentProvider in AwContents.setOnscreenContentProvider() to avoid memory leak
    crbug/1445093 points out that LeakCanary shows that we are holding on to instances of WebView and OnscreenContentProvider after they go out of scope. Fixing this bug by ensuring that AwContents.setOnscreenContentProvider() explicitly destroys existing instances of OnscreenContentProvider held by it before assigning a new one.
    
    Verified by @aandrioti from AGSA.
    
    Bug: crbug/1445093
    Change-Id: I0955341b7f90e4badb85b2469abadac382f42287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594797
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Aashna Jena <aashnajena@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154961}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1846,6 +1846,9 @@ public class AwContents implements SmartClipProvider {
     }
 
     public void setOnscreenContentProvider(OnscreenContentProvider onscreenContentProvider) {
+        if (mOnscreenContentProvider != null) {
+            mOnscreenContentProvider.destroy();
+        }
         mOnscreenContentProvider = onscreenContentProvider;
     }
 
