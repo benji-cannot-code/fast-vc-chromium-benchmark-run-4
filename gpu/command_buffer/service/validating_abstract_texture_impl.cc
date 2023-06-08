3437db5325413c10610b0c93e1be898f59704d31
Colin Blundell <blundell@chromium.org>
2023-06-08 07:25:10 +0000

    [GPU][Windows] Eliminate notion of setting unbound GLImage
    This concept is no longer used on Windows now that the legacy VDA is
    not built on that platform. Eliminating it is another step on the road
    to eliminating GLImage altogether.
    
    Bug: 1310018
    Change-Id: I634f7c2b56f193cbdbe2584d1d24644225787643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594260
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154793}

--- gpu/command_buffer/service/validating_abstract_texture_impl.cc
+++ gpu/command_buffer/service/validating_abstract_texture_impl.cc
@@ -52,7 +52,7 @@ void ValidatingAbstractTextureImpl::SetParameteri(GLenum pname, GLint param) {
                                      texture_ref_.get(), pname, param);
 }
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE)
+#if BUILDFLAG(IS_APPLE)
 void ValidatingAbstractTextureImpl::SetUnboundImage(gl::GLImage* image) {
   if (!texture_ref_)
     return;
@@ -71,7 +71,7 @@ void ValidatingAbstractTextureImpl::SetUnboundImage(gl::GLImage* image) {
   GetTextureManager()->SetLevelCleared(texture_ref_.get(), target, level,
                                        image);
 }
-#else
+#elif !BUILDFLAG(IS_WIN)
 void ValidatingAbstractTextureImpl::SetBoundImage(gl::GLImage* image) {
   if (!texture_ref_) {
     return;
