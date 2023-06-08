4ec697d3308633c182702fa0b40a181174c9ec48
Sky Malice <skym@chromium.org>
2023-06-08 18:58:51 +0000

    Empty check bookmark refresh.
    There was a gap between using the back nav button to exit the bookmark manager, and when the mediator was actually destroyed. When the back nav was used, the internal state stack was empty.
    
    During this time before destroy was called, the mediator was still observing the BookmarkModel, and a synced change could cause the mediator to try to refresh the UI. However this refresh didn't verify
    the state of the back stack, and would blow up trying to pop off an empty stack.
    
    This change simply makes sure the stack isn't empty when refreshing, otherwise the refresh is a no-op. In the case that we're in this inconsistent mid-destroy state, a no-op is fine for the refresh call.
    
    Bug: 1450539
    Change-Id: I6ebcf1604992b6c1dbe8c274f7753c279c1efe7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598006
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155024}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
@@ -901,7 +901,9 @@ class BookmarkManagerMediator
 
     /** Refresh the list of bookmarks within the currently visible folder. */
     private void refresh() {
-        notifyUi(mStateStack.peek());
+        if (!mStateStack.isEmpty()) {
+            notifyUi(mStateStack.peek());
+        }
     }
 
     private @ViewType int calculatePromoHeaderType() {
