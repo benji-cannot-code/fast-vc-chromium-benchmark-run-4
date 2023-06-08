74e03fcf1ac20904afe459590c18329027b6e3e2
Yuki Shiino <yukishiino@chromium.org>
2023-06-08 01:53:49 +0000

    PA: Remove hidden deps to CHECK in shim/
    Bug: 1337681
    Change-Id: Ia308cf88183eaff4cea758724874b23aa138148a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4596243
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154719}

--- base/allocator/partition_allocator/shim/allocator_shim_override_mac_default_zone.h
+++ base/allocator/partition_allocator/shim/allocator_shim_override_mac_default_zone.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_SHIM_ALLOCATOR_SHIM_OVERRIDE_MAC_DEFAULT_ZONE_H_
 
 #include "base/allocator/partition_allocator/partition_alloc_buildflags.h"
+#include "base/allocator/partition_allocator/partition_alloc_check.h"
 
 #if !BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 #error This header must be included iff PartitionAlloc-Everywhere is enabled.
@@ -397,7 +398,7 @@ InitializeDefaultMallocZoneWithPartitionAlloc() {
   malloc_zone_register(system_default_zone);
 
   // Confirm that our own zone is now the default zone.
-  CHECK_EQ(GetDefaultMallocZone(), &g_mac_malloc_zone);
+  PA_CHECK(GetDefaultMallocZone() == &g_mac_malloc_zone);
   g_initialization_is_done.store(true, std::memory_order_release);
 }
 
