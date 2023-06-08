6e04bccb69c9979341ad93ae177d7f53de595042
Ernesto Izquierdo Clua <eic@google.com>
2023-06-08 20:06:15 +0000

    [ios] Re-enable testCancelDeletionInDetailView
    This patch re-enables a previously flaky test in Password Manager.
    Tested by running 20 times in a row on different Iphone and Ipad
    simulators.
    
    Fixed the test by changing the mechanism of dismissing the
    confirmation alert. We were tapping on the underlying view controller, at the same position where the alert was, so this wasn't dismissing
    it. I changed the test to tap on the Cancel button on the navigation
    bar to avoid tapping the alert.
    
    Fixed: 1405037
    Change-Id: I1ee8a275f0b12076b3c971d499e65403617794f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598492
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155056}

--- ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
@@ -1071,8 +1071,7 @@ void CopyPasswordDetailWithID(int detail_id) {
 }
 
 // Checks that deleting a password from password details can be cancelled.
-// TODO(crbug.com/1405037): The test is flaky.
-- (void)DISABLED_testCancelDeletionInDetailView {
+- (void)testCancelDeletionInDetailView {
   // Save form to be deleted later.
   SavePasswordForm();
 
@@ -1098,9 +1097,8 @@ void CopyPasswordDetailWithID(int detail_id) {
                                           : DeleteButton()]
       performAction:grey_tap()];
 
-  // Close the dialog by taping on Password Details screen.
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
-                                          kPasswordDetailsTableViewId)]
+  // Close the dialog by tapping on Password Details screen cancel button.
+  [[EarlGrey selectElementWithMatcher:NavigationBarCancelButton()]
       performAction:grey_tap()];
 
   // Check that the current view is still the detail view.
