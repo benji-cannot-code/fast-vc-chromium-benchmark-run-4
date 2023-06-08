414bd01ae91b2509e2e413191b28e944782bfa64
Stephan Hartmann <stha09@googlemail.com>
2023-06-08 20:35:31 +0000

    libstdc++: use math.h instead of cmath in blink::Color
    libstdc++ does not define all non-std functions in cmath.
    
    Bug: 957519
    Change-Id: Ib839793067588e21685725aaccf60febdf5590b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598233
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155065}

--- third_party/blink/renderer/platform/graphics/color.cc
+++ third_party/blink/renderer/platform/graphics/color.cc
@@ -26,7 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/color.h"
 
-#include <cmath>
+#include <math.h>
+
 #include "base/check_op.h"
 #include "base/notreached.h"
 #include "build/build_config.h"
