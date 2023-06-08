2604f35d081832e2197629ce5f059641008dff3b
thefrog <thefrog@chromium.org>
2023-06-08 22:20:50 +0000

    Have OhttpKeyService use util to determine if key fetching is enabled
    Instead of checking STANDARD_PROTECTION directly, this reuses the
    recently introduced hash_realtime_utils helper to see if key fetching is
    enabled.
    
    This CL also changes the IsEnabled logic from checking the feature
    kSafeBrowsingLookupMechanismExperiment to checking either
    kHashRealTimeOverOhttp or kHashPrefixRealTimeLookups (through the util).
    Since we shouldn't have both features on at the same time, this is not a
    functional change. However, it is slightly clearer, because the service
    can (as of crbug.com/4582154) be created even if kHashRealTimeOverOhttp
    is disabled, but key fetching for ESB users is only relevant in the
    experiment if kHashRealTimeOverOhttp is enabled.
    
    For Android, for now we disable the OhttpKeyServiceTest and ensure that
    the OhttpKeyServiceFactory returns null. Once it is finalized whether
    these and associated files are relevant for Android, there will be a
    more elegant solution (e.g. either don't include the files in the binary
    in the first place, or don't return null for Android).
    
    Bug: 1441654
    Change-Id: If53afe3dac0387b40e703e199f75cb87aee517c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595937
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155147}

--- chrome/browser/safe_browsing/ohttp_key_service_factory_unittest.cc
+++ chrome/browser/safe_browsing/ohttp_key_service_factory_unittest.cc
@@ -49,6 +49,12 @@ class OhttpKeyServiceFactoryTest : public testing::Test {
   scoped_refptr<safe_browsing::SafeBrowsingService> sb_service_;
 };
 
+#if BUILDFLAG(IS_ANDROID)
+TEST_F(OhttpKeyServiceFactoryTest, DisabledForRegularProfiles) {
+  TestingProfile* profile = profile_manager_->CreateTestingProfile("profile");
+  EXPECT_EQ(nullptr, OhttpKeyServiceFactory::GetForProfile(profile));
+}
+#else
 TEST_F(OhttpKeyServiceFactoryTest, EnabledForRegularProfiles) {
   TestingProfile* profile = profile_manager_->CreateTestingProfile("profile");
   EXPECT_NE(nullptr, OhttpKeyServiceFactory::GetForProfile(profile));
@@ -95,5 +101,6 @@ TEST_F(OhttpKeyServiceFactoryTest, DisabledForGuestMode) {
           /*create_if_needed=*/true);
   EXPECT_EQ(nullptr, OhttpKeyServiceFactory::GetForProfile(profile));
 }
+#endif
 
 }  // namespace safe_browsing
