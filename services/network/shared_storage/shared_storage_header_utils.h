e5d10006ce1188f9dbca9dd1204ecb5b42b66082
Camillia Smith Barnes <cammie@chromium.org>
2023-06-08 20:39:55 +0000

    Shared Storage: Add SharedStorageRequestHelper for parsing headers
    We add the `SharedStorageRequestHelper` to the network service to
    process requests that are eligible for writing to Shared Storage,
    including both adding the outgoing request header
    "Shared-Storage-Writable: ?1" and parsing the incoming response
    header(s) "Shared-Storage-Write".
    
    Integration tests will be added in the follow-up CL
    https://crrev.com/c/4519162.
    
    This is part of a project to add support to writing to shared storage
    from response headers. See
    https://github.com/WICG/shared-storage/pull/70 for more information
    and https://crrev.com/c/4444068 for a working prototype for `fetch()`.
    
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Bug: 1434529,1218540
    Change-Id: Idc0a7d93d02bda362a2f923a42f83f47d77d283c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519240
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155067}

---
+++ services/network/shared_storage/shared_storage_header_utils.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_SHARED_STORAGE_SHARED_STORAGE_HEADER_UTILS_H_
+#define SERVICES_NETWORK_SHARED_STORAGE_SHARED_STORAGE_HEADER_UTILS_H_
+
+#include "base/strings/string_piece.h"
+#include "services/network/public/mojom/url_loader_network_service_observer.mojom.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace network {
+
+inline constexpr base::StringPiece kSharedStorageWritableHeader =
+    "Shared-Storage-Writable";
+inline constexpr base::StringPiece kSharedStorageWritableValue = "?1";
+inline constexpr base::StringPiece kSharedStorageWriteHeader =
+    "Shared-Storage-Write";
+
+enum class SharedStorageHeaderParamType {
+  kKey,
+  kValue,
+  kIgnoreIfPresent,
+};
+
+absl::optional<network::mojom::SharedStorageOperationType>
+StringToSharedStorageOperationType(base::StringPiece operation_str);
+
+absl::optional<SharedStorageHeaderParamType>
+StringToSharedStorageHeaderParamType(base::StringPiece param_str);
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_SHARED_STORAGE_SHARED_STORAGE_HEADER_UTILS_H_
--- services/network/test/test_url_loader_network_observer.cc
+++ services/network/test/test_url_loader_network_observer.cc
@@ -19,6 +19,10 @@ TestURLLoaderNetworkObserver::Bind() {
   return remote;
 }
 
+void TestURLLoaderNetworkObserver::FlushReceivers() {
+  receivers_.FlushForTesting();
+}
+
 void TestURLLoaderNetworkObserver::OnSSLCertificateError(
     const GURL& url,
     int net_error,
