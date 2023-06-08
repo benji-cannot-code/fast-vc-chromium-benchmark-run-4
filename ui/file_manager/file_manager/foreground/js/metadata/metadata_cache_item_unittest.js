86a04cd4f8d81d990be027b4f4750bb8ee706136
Ben Reich <benreich@chromium.org>
2023-06-08 04:46:57 +0000

    [FilesBulkPinning] Ensure metadata changes invalidate all cached rows
    When metadata is retrieved the cache gets properly invalidated, however,
    only the items that are visible in the viewport get updated by the
    MetadataUpdateController. For items outside the viewport, these are
    cached in the FileListModel and when a user scrolls back the rows
    back into view, they can contain stale information.
    
    Currently many features appear to be invalidating their own individual
    metadata items (e.g. available offline toggle does the pinned icon,
    onDirectoryChanged does the modified time etc.). With the introduction
    of inline sync status this meant the status gets cached and not updated
    if out of view for long enough.
    
    This fix invalidates the indices that have new metadata changes and
    relies on the fact that the MetadataCacheSet only relays information for
    items which actually get changed (instead of blindly updating everything
    item even if nothing changes).
    
    Fixed: b:283204213
    Change-Id: If9e211a12465064b316a42799d1f973ce51e2bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4592153
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154767}

--- ui/file_manager/file_manager/foreground/js/metadata/metadata_cache_item_unittest.js
+++ ui/file_manager/file_manager/foreground/js/metadata/metadata_cache_item_unittest.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assertEquals, assertFalse, assertThrows, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
+import {assertDeepEquals, assertEquals, assertFalse, assertThrows, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 
 import {MetadataCacheItem} from './metadata_cache_item.js';
 import {MetadataItem} from './metadata_item.js';
@@ -14,6 +14,7 @@ import {MetadataItem} from './metadata_item.js';
 const metadataA = new MetadataItem();
 metadataA.contentMimeType = 'value';
 
+const CONTENT_MIME_TYPE_SET = new Set(['contentMimeType']);
 
 export function testMetadataCacheItemBasic() {
   const item = new MetadataCacheItem();
@@ -22,7 +23,7 @@ export function testMetadataCacheItemBasic() {
   assertEquals('contentMimeType', loadRequested[0]);
 
   item.startRequests(1, loadRequested);
-  assertTrue(item.storeProperties(1, metadataA));
+  assertDeepEquals(item.storeProperties(1, metadataA), CONTENT_MIME_TYPE_SET);
 
   const result = item.get(['contentMimeType']);
   assertEquals('value', result.contentMimeType);
@@ -35,7 +36,7 @@ export function testMetadataCacheItemAvoidDoubleLoad() {
   assertEquals(0, loadRequested.length);
 
   item.startRequests(2, loadRequested);
-  assertTrue(item.storeProperties(1, metadataA));
+  assertDeepEquals(item.storeProperties(1, metadataA), CONTENT_MIME_TYPE_SET);
 
   const result = item.get(['contentMimeType']);
   assertEquals('value', result.contentMimeType);
@@ -45,7 +46,7 @@ export function testMetadataCacheItemInvalidate() {
   const item = new MetadataCacheItem();
   item.startRequests(1, item.createRequests(['contentMimeType']));
   item.invalidate(2);
-  assertFalse(item.storeProperties(1, metadataA));
+  assertDeepEquals(item.storeProperties(1, metadataA), new Set());
 
   const loadRequested = item.createRequests(['contentMimeType']);
   assertEquals(1, loadRequested.length);
@@ -59,8 +60,8 @@ export function testMetadataCacheItemStoreInReverseOrder() {
   const metadataB = new MetadataItem();
   metadataB.contentMimeType = 'value2';
 
-  assertTrue(item.storeProperties(2, metadataB));
-  assertFalse(item.storeProperties(1, metadataA));
+  assertDeepEquals(item.storeProperties(2, metadataB), CONTENT_MIME_TYPE_SET);
+  assertDeepEquals(item.storeProperties(1, metadataA), new Set());
 
   const result = item.get(['contentMimeType']);
   assertEquals('value2', result.contentMimeType);
