d952cc82b3052ac73269e40f1b01044ba27f0abc
Gang Wu <gangwu@chromium.org>
2023-06-08 18:44:47 +0000

    Reland "Wire up AuxiliarySearchController"
    This is a reland of commit e14a1253b790e5676d0aaad734d1bca4d4e99dec
    
    Original change's description:
    > Wire up AuxiliarySearchController
    >
    > This is a follow up for the https://crrev.com/c/4581127.
    > The CL(https://crrev.com/c/4576445) got reverted since the internal
    > implementation(https://crrev.com/i/5997452) is not submit yet, now
    > the internal cl is submitted, we can wire up AuxiliarySearchController
    > with ChromeTabbedActivity.
    >
    > Bug: 1445112
    > Change-Id: I5324cea6df1922b4688d6d1ebff76e96de78a693
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4586034
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1153236}
    
    Bug: 1445112
    Change-Id: I3e68de83babf9ee2061bb086731eaa178714c2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4590711
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155017}

---
+++ chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchControllerFactory.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.auxiliary_search;
+
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+
+/**
+ * This Controller for the auxiliary search.
+ */
+public class AuxiliarySearchControllerFactory {
+    public static AuxiliarySearchController createAuxiliarySearchController(
+            Profile profile, TabModelSelector tabModelSelector) {
+        return new AuxiliarySearchControllerImpl(profile, tabModelSelector);
+    }
+}
