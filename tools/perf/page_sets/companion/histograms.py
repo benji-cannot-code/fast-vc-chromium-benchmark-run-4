e58b45ecfeb6be9ea688c87e3831e4788a16652e
Marco Georgaklis <mgeorgaklis@google.com>
2023-06-08 05:13:52 +0000

    Added new histogram checks to companion trigger test.
    Bug: b:273308580
    Change-Id: I282d2961e0bf8f0276e14bf9bd2ee6c8bf7e651c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4592109
    Commit-Queue: Marco Georgaklis <mgeorgaklis@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154773}

---
+++ tools/perf/page_sets/companion/histograms.py
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+_STARTUP = "Startup.BrowserMainRunnerImplInitializeLongTime"
+_ZERO_STATE = "SidePanel.Companion.ShowTriggered"
+_CQ = "Companion.CQ.Shown"
+_USER_OPT_IN = "Companion.IsUserOptedInToExps"
+_PROMO_EVENT = "Companion.PromoEvent"
