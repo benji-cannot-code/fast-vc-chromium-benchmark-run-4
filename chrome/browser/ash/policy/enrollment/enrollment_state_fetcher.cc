25be99dda0b6e2c9e09bab64f8880524cd40a49e
Sergiy Belozorov <sergiyb@chromium.org>
2023-06-08 17:25:46 +0000

    Use sparse histograms for DMServer status code
    This is done instead of converting DeviceManagementStatus to enum class,
    which is rather challenging due to many places in code where it is
    implicitly converted to int, e.g. as part of serialization or logging.
    
    This also adds missing buckets to the enum definition.
    
    Bug: b:265923216
    Change-Id: I632fab25d9b3981c52dde6ac69a461b842cdf734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598769
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Auto-Submit: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154967}

--- chrome/browser/ash/policy/enrollment/enrollment_state_fetcher.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_state_fetcher.cc
@@ -341,9 +341,8 @@ class RlweOprf {
   void OnRequestDone(CompletionCallback completion_callback,
                      DMServerJobResult result) {
     // Handle errors
-    base::UmaHistogramEnumeration(
-        kUMAStateDeterminationPsmRlweOprfRequestDmStatusCode, result.dm_status,
-        static_cast<policy::DeviceManagementStatus>(DM_STATUS_SERVICE_MAX + 1));
+    base::UmaHistogramSparse(
+        kUMAStateDeterminationPsmRlweOprfRequestDmStatusCode, result.dm_status);
     base::UmaHistogramSparse(
         kUMAStateDeterminationPsmRlweOprfRequestNetworkErrorCode,
         -result.net_error);
@@ -441,9 +440,9 @@ class RlweQuery {
       CompletionCallback completion_callback,
       DMServerJobResult result) {
     // Handle errors
-    base::UmaHistogramEnumeration(
-        kUMAStateDeterminationPsmRlweQueryRequestDmStatusCode, result.dm_status,
-        static_cast<policy::DeviceManagementStatus>(DM_STATUS_SERVICE_MAX + 1));
+    base::UmaHistogramSparse(
+        kUMAStateDeterminationPsmRlweQueryRequestDmStatusCode,
+        result.dm_status);
     base::UmaHistogramSparse(
         kUMAStateDeterminationPsmRlweQueryRequestNetworkErrorCode,
         -result.net_error);
@@ -556,9 +555,8 @@ class EnrollmentState {
   void OnRequestDone(CompletionCallback completion_callback,
                      DMServerJobResult result) {
     // Handle errors
-    base::UmaHistogramEnumeration(
-        kUMAStateDeterminationStateRequestDmStatusCode, result.dm_status,
-        static_cast<policy::DeviceManagementStatus>(DM_STATUS_SERVICE_MAX + 1));
+    base::UmaHistogramSparse(kUMAStateDeterminationStateRequestDmStatusCode,
+                             result.dm_status);
     base::UmaHistogramSparse(kUMAStateDeterminationStateRequestNetworkErrorCode,
                              -result.net_error);
     switch (result.dm_status) {
