d07046a0d2afffc8c4fc459cbd6a4bbc465839ad
kylechar <kylechar@chromium.org>
2023-06-08 15:50:05 +0000

    Ensure current GL context when destroying backings
    GLTextureImageBacking requires a current GL context when being
    destroyed. When running with a Vulkan GrContext, SharedImageStub isn't
    necessarily making a GL context current for most operations. For all
    shared image creation and deletion via SharedImageStub, check if usage
    includes GLES2 and if so, force making GL context current. For a GL
    GrContext this makes no difference but for Vulkan or new graphite path
    it should help prevent problems.
    
    Bug: 1443596
    Change-Id: I02c0639e5425f9fa19708c2b88e56aec75e736e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599199
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154931}

--- gpu/ipc/service/shared_image_stub.cc
+++ gpu/ipc/service/shared_image_stub.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "components/viz/common/resources/shared_image_format.h"
+#include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/service/scheduler.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_factory.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_representation.h"
@@ -53,7 +54,8 @@ SharedImageStub::~SharedImageStub() {
   channel_->scheduler()->DestroySequence(sequence_);
   sync_point_client_state_->Destroy();
   if (factory_ && factory_->HasImages()) {
-    bool have_context = MakeContextCurrent();
+    // Some of the backings might require a current GL context to be destroyed.
+    bool have_context = MakeContextCurrent(/*needs_gl=*/true);
     factory_->DestroyAllSharedImages(have_context);
   }
 }
@@ -158,7 +160,9 @@ bool SharedImageStub::CreateSharedImage(const Mailbox& mailbox,
     OnError();
     return false;
   }
-  if (!MakeContextCurrent()) {
+
+  bool needs_gl = usage & SHARED_IMAGE_USAGE_GLES2;
+  if (!MakeContextCurrent(needs_gl)) {
     OnError();
     return false;
   }
@@ -194,7 +198,9 @@ bool SharedImageStub::CreateSharedImage(const Mailbox& mailbox,
     OnError();
     return false;
   }
-  if (!MakeContextCurrent()) {
+
+  bool needs_gl = usage & SHARED_IMAGE_USAGE_GLES2;
+  if (!MakeContextCurrent(needs_gl)) {
     OnError();
     return false;
   }
@@ -243,9 +249,8 @@ void SharedImageStub::OnCreateSharedImage(
     return;
   }
 
-  // Some shared image backing factories will use GL.
-  // TODO(crbug.com/1239365): Only request GL when needed.
-  if (!MakeContextCurrent(/*needs_gl=*/true)) {
+  bool needs_gl = params->usage & SHARED_IMAGE_USAGE_GLES2;
+  if (!MakeContextCurrent(needs_gl)) {
     OnError();
     return;
   }
@@ -272,7 +277,8 @@ void SharedImageStub::OnCreateSharedImageWithData(
     return;
   }
 
-  if (!MakeContextCurrent()) {
+  bool needs_gl = params->usage & SHARED_IMAGE_USAGE_GLES2;
+  if (!MakeContextCurrent(needs_gl)) {
     OnError();
     return;
   }
@@ -385,7 +391,9 @@ void SharedImageStub::OnDestroySharedImage(const Mailbox& mailbox) {
     return;
   }
 
-  if (!MakeContextCurrent()) {
+  bool needs_gl =
+      factory_->GetUsageForMailbox(mailbox) & SHARED_IMAGE_USAGE_GLES2;
+  if (!MakeContextCurrent(needs_gl)) {
     OnError();
     return;
   }
