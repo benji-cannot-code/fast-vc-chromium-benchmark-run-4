26d75886a7ca492c97bed32b909da9dafb476dec
Min Qin <qinmin@chromium.org>
2023-06-08 07:13:46 +0000

    Remove expired recordDownloadDirectoryType() call
    The histogram is expired, so removing this call.
    
    BUG=1452129
    
    Change-Id: I6b78dce0a179e76c6bcd2403edf77302cd69c6a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4597912
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154792}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadController.java
@@ -65,7 +65,6 @@ public class DownloadController {
      */
     @CalledByNative
     private static void onDownloadCompleted(DownloadInfo downloadInfo) {
-        DownloadMetrics.recordDownloadDirectoryType(downloadInfo.getFilePath());
         MediaStoreHelper.addImageToGalleryOnSDCard(
                 downloadInfo.getFilePath(), downloadInfo.getMimeType());
 
