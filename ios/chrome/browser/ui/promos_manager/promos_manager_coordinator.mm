a6bfb947847db44334024feb3fe1c835394cb495
Robbie Gibson <rkgibson@google.com>
2023-06-08 14:48:18 +0000

    [iOS] Convert promos CHECK to a DWC and add necessary data as crash key
    This includes which promo failed to be dismissed as crash data, to help
    fixes.
    
    Bug: 1443982
    Change-Id: Ica32e3154f7fbddaf9c67308ec8e787caabddc41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572516
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154898}

--- ios/chrome/browser/ui/promos_manager/promos_manager_coordinator.mm
+++ ios/chrome/browser/ui/promos_manager/promos_manager_coordinator.mm
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/containers/small_map.h"
+#import "base/debug/dump_without_crashing.h"
 #import "base/metrics/histogram_functions.h"
 #import "base/notreached.h"
 #import "base/strings/sys_string_conversions.h"
+#import "components/crash/core/common/crash_key.h"
 #import "components/feature_engagement/public/tracker.h"
 #import "ios/chrome/app/tests_hook.h"
 #import "ios/chrome/browser/credential_provider_promo/features.h"
@@ -206,6 +208,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)displayPromo:(promos_manager::Promo)promo {
   if (tests_hook::DisablePromoManagerFullScreenPromos()) {
     return;
+  }
+
+  // Trying to display a promo while the previous dismissal was not communicated
+  // back to the promos manager.
+  // TODO(crbug.com/1452233): Remove once all promos dismiss themselves.
+  if (current_promo.has_value()) {
+    static crash_reporter::CrashKeyString<40> key("current-promo");
+    crash_reporter::ScopedCrashKeyString crashKey(
+        &key, ShortNameForPromo(current_promo.value()));
+    base::debug::DumpWithoutCrashing();
   }
 
   current_promo = promo;
