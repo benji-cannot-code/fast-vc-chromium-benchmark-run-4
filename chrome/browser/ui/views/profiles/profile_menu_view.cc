beee73d1af5210659dfa91f2401e1c18d58e2298
Angela Yoeurng <yoangela@chromium.org>
2023-06-09 21:26:35 +0000

    [Sheriff] Revert "[CR2023] Add section 3 of the profile submenu."
    This reverts commit e5bbf20c4acf0f7224a6d3231895eecb5e546c72.
    
    Reason for revert: Added test is failing 
    
    Original change's description:
    > [CR2023] Add section 3 of the profile submenu.
    >
    > Adds 3 menu items with icons.
    > Refactor some functions into a profile_view_utils file
    >
    > Figma:
    > https://www.figma.com/file/NLMJNd5s2g9DEDZCbIngJV/ChroMenu---Desktop-Scalability?type=design&t=0qB5mAXXq8klugJc-0
    >
    > https://screenshot.googleplex.com/97gTgt8gwPGgZTU
    >
    > Change-Id: Ia6c595cbe9ed0885a5f02158f86933c5bf5966b1
    > Bug: 1427667
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577271
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Commit-Queue: David Yeung <dayeung@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1155649}
    
    Bug: 1427667, 1453762
    Change-Id: I91a9babf8311527cfc41a6bd6353ec95119a7fc8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605024
    Owners-Override: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Auto-Submit: Angela Yoeurng <yoangela@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1155763}

--- chrome/browser/ui/views/profiles/profile_menu_view.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/passwords/ui_utils.h"
 #include "chrome/browser/ui/profile_picker.h"
-#include "chrome/browser/ui/profile_view_utils.h"
 #include "chrome/browser/ui/signin/profile_colors_util.h"
 #include "chrome/browser/ui/sync/sync_promo_ui.h"
 #include "chrome/browser/ui/ui_features.h"
@@ -115,6 +114,22 @@ std::u16string GetSyncErrorButtonText(AvatarSyncErrorType error) {
   }
 }
 
+void NavigateToGoogleAccountPage(Profile* profile, const std::string& email) {
+  // Create a URL so that the account chooser is shown if the account with
+  // |email| is not signed into the web. Include a UTM parameter to signal the
+  // source of the navigation.
+  GURL google_account = net::AppendQueryParameter(
+      GURL(chrome::kGoogleAccountURL), "utm_source", "chrome-profile-chooser");
+
+  GURL url(chrome::kGoogleAccountChooserURL);
+  url = net::AppendQueryParameter(url, "Email", email);
+  url = net::AppendQueryParameter(url, "continue", google_account.spec());
+
+  NavigateParams params(profile, url, ui::PAGE_TRANSITION_LINK);
+  params.disposition = WindowOpenDisposition::NEW_FOREGROUND_TAB;
+  Navigate(&params);
+}
+
 // Returns the number of browsers associated with |profile|.
 // Note: For regular profiles this includes incognito sessions.
 int CountBrowsersFor(Profile* profile) {
@@ -123,6 +138,10 @@ int CountBrowsersFor(Profile* profile) {
     browser_count += chrome::GetBrowserCount(
         profile->GetPrimaryOTRProfile(/*create_if_needed=*/true));
   return browser_count;
+}
+
+bool IsSyncPaused(Profile* profile) {
+  return GetAvatarSyncErrorType(profile) == AvatarSyncErrorType::kSyncPaused;
 }
 
 }  // namespace
@@ -641,7 +660,12 @@ void ProfileMenuView::BuildSyncInfo() {
 
 void ProfileMenuView::BuildFeatureButtons() {
   Profile* profile = browser()->profile();
-  bool has_unconsented_account = HasUnconstentedProfile(profile);
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(profile);
+  const bool has_unconsented_account =
+      !profile->IsGuestSession() &&
+      identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin);
+
   if (has_unconsented_account && !IsSyncPaused(profile)) {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
     // The Google G icon needs to be shrunk, so it won't look too big compared
@@ -682,8 +706,6 @@ void ProfileMenuView::BuildFeatureButtons() {
   }
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT) || BUILDFLAG(IS_CHROMEOS_LACROS)
-  signin::IdentityManager* identity_manager =
-      IdentityManagerFactory::GetForProfile(profile);
   const bool has_primary_account =
       !profile->IsGuestSession() &&
       identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSync);
