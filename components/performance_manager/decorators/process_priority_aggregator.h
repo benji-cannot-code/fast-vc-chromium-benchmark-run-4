26d14b3619212a027bf32db100bb5804f67ff93b
Patrick Monette <pmonette@chromium.org>
2023-06-09 00:17:08 +0000

    Move ProcessPriorityAggregator to components/performance_manager/
    Bug: 971272
    Change-Id: I907e380e79892f2b4fb734f3adf0fb152b6691a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598577
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1155253}

--- components/performance_manager/decorators/process_priority_aggregator.h
+++ components/performance_manager/decorators/process_priority_aggregator.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PERFORMANCE_MANAGER_DECORATORS_PROCESS_PRIORITY_AGGREGATOR_H_
-#define CHROME_BROWSER_PERFORMANCE_MANAGER_DECORATORS_PROCESS_PRIORITY_AGGREGATOR_H_
+#ifndef COMPONENTS_PERFORMANCE_MANAGER_DECORATORS_PROCESS_PRIORITY_AGGREGATOR_H_
+#define COMPONENTS_PERFORMANCE_MANAGER_DECORATORS_PROCESS_PRIORITY_AGGREGATOR_H_
 
 #include "components/performance_manager/public/execution_context/execution_context.h"
 #include "components/performance_manager/public/graph/graph.h"
@@ -110,4 +110,4 @@ class ProcessPriorityAggregator::Data {
 
 }  // namespace performance_manager
 
-#endif  // CHROME_BROWSER_PERFORMANCE_MANAGER_DECORATORS_PROCESS_PRIORITY_AGGREGATOR_H_
+#endif  // COMPONENTS_PERFORMANCE_MANAGER_DECORATORS_PROCESS_PRIORITY_AGGREGATOR_H_
