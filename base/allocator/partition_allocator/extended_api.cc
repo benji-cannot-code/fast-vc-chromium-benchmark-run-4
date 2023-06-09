6f12420fb23b70b9331adb1650005575a78e4ebd
Bartek Nowierski <bartekn@chromium.org>
2023-06-09 08:09:56 +0000

    [PA] Rename PartitionRoot::flags -> ::settings
    This avoids confusion with flags passed as parameter. Plus it sounds
    more accurate.
    
    This CL is a no-op.
    
    Change-Id: Iec640e821b5ead5939985fc0f44fb5b0e9ffe0f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546445
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155403}

--- base/allocator/partition_allocator/extended_api.cc
+++ base/allocator/partition_allocator/extended_api.cc
@@ -19,12 +19,12 @@ namespace {
 void DisableThreadCacheForRootIfEnabled(ThreadSafePartitionRoot* root) {
   // Some platforms don't have a thread cache, or it could already have been
   // disabled.
-  if (!root || !root->flags.with_thread_cache) {
+  if (!root || !root->settings.with_thread_cache) {
     return;
   }
 
   ThreadCacheRegistry::Instance().PurgeAll();
-  root->flags.with_thread_cache = false;
+  root->settings.with_thread_cache = false;
   // Doesn't destroy the thread cache object(s). For background threads, they
   // will be collected (and free cached memory) at thread destruction
   // time. For the main thread, we leak it.
@@ -35,7 +35,7 @@ void EnablePartitionAllocThreadCacheForRootIfDisabled(
   if (!root) {
     return;
   }
-  root->flags.with_thread_cache = true;
+  root->settings.with_thread_cache = true;
 }
 
 #if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
@@ -73,7 +73,7 @@ ThreadCacheProcessScopeForTesting::ThreadCacheProcessScopeForTesting(
   auto* regular_allocator =
       allocator_shim::internal::PartitionAllocMalloc::Allocator();
   regular_was_enabled_ =
-      regular_allocator && regular_allocator->flags.with_thread_cache;
+      regular_allocator && regular_allocator->settings.with_thread_cache;
 
   if (root_ != regular_allocator) {
     // Another |root| is ThreadCache's PartitionRoot. Need to disable
@@ -102,7 +102,7 @@ ThreadCacheProcessScopeForTesting::~ThreadCacheProcessScopeForTesting() {
   auto* regular_allocator =
       allocator_shim::internal::PartitionAllocMalloc::Allocator();
   bool regular_enabled =
-      regular_allocator && regular_allocator->flags.with_thread_cache;
+      regular_allocator && regular_allocator->settings.with_thread_cache;
 
   if (regular_was_enabled_) {
     if (!regular_enabled) {
