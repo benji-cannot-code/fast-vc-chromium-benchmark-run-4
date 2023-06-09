a06b977cec4e002975a2d83ae3464b48b5fb18b1
Wes Okuhara <wesokuhara@google.com>
2023-06-09 17:31:18 +0000

    CrOS Settings: Cleanup infinite scroll removal code
    Update comments, improve CSS selectors, and improve readability.
    
    Bug: b/282961146
    Change-Id: Ibbad7453002b4d444965011df4d5dd6dba4da6d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599850
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1155602}

--- chrome/browser/resources/settings/chromeos/main_page_mixin.ts
+++ chrome/browser/resources/settings/chromeos/main_page_mixin.ts
@@ -82,7 +82,6 @@ const FIRST_PAGE_ROUTE: Route = routes.INTERNET;
 
 export interface MainPageMixinInterface extends RouteObserverMixinInterface {
   containsRoute(route: Route|undefined): boolean;
-  querySection(section: string): HTMLElement|null;
   loadAdvancedPage(): Promise<Element>;
 }
 
@@ -170,9 +169,10 @@ export const MainPageMixin = dedupingMixin(
 
           const section = await this.ensureSectionForRoute_(route);
           section.classList.add('expanded');
+          this.dispatchCustomEvent_('show-container');
+
           // Fire event used by a11y tests only.
           this.dispatchCustomEvent_('settings-section-expanded');
-          this.dispatchCustomEvent_('show-container');
         }
 
         private enterMainPage_(oldRoute: Route): Promise<void> {
@@ -429,10 +429,7 @@ export const MainPageMixin = dedupingMixin(
         /**
          * Helper function to get a section from the local DOM.
          */
-        querySection(section: string): HTMLElement|null {
-          if (!section) {
-            return null;
-          }
+        private querySection(section: string): HTMLElement|null {
           return this.shadowRoot!.querySelector(
               `os-settings-section[section="${section}"]`);
         }
