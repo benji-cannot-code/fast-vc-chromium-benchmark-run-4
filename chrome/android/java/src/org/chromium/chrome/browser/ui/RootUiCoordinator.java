01c91bd62d2835db535cc20dfdac30603216733e
Jinsuk Kim <jinsukkim@chromium.org>
2023-06-09 19:35:45 +0000

    [gbb-cct] Coordinate ModalDialogManager/TabObscuringHandler
    This CL takes care of the following cases when the bottom bar goes
    in and out of the expanded mode:
    
    - App/Tab modal dialog should be suppressed
    - TabObscuringHandler needs to be informed so that the obscured tab
      should be hidden from the accessibility tree.
    
    Bug: b/282739536
    Change-Id: I72e0ace764428a1f69f8ad8d321f8a57bf735246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598927
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155695}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -153,6 +153,7 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.Shee
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetControllerFactory;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetObserver;
 import org.chromium.components.browser_ui.bottomsheet.EmptyBottomSheetObserver;
+import org.chromium.components.browser_ui.bottomsheet.ExpandedSheetHelper;
 import org.chromium.components.browser_ui.bottomsheet.ManagedBottomSheetController;
 import org.chromium.components.browser_ui.widget.MenuOrKeyboardActionController;
 import org.chromium.components.browser_ui.widget.gesture.BackPressHandler;
@@ -309,6 +310,7 @@ public class RootUiCoordinator
     @Nullable
     private final BackPressManager mBackPressManager;
     private final @NonNull Supplier<Bundle> mSavedInstanceStateSupplier;
+    protected final ExpandedSheetHelper mExpandedBottomSheetHelper;
     @Nullable
     private PageZoomCoordinator mPageZoomCoordinator;
     private AppMenuObserver mAppMenuObserver;
@@ -503,6 +505,8 @@ public class RootUiCoordinator
         });
         mFoldTransitionController = new FoldTransitionController(mToolbarManagerOneshotSupplier,
                 mLayoutManagerSupplier, mActivityTabProvider, mStartSurfaceSupplier, new Handler());
+        mExpandedBottomSheetHelper =
+                new ExpandedSheetHelperImpl(mModalDialogManagerSupplier, getTabObscuringHandler());
     }
 
     // TODO(pnoland, crbug.com/865801): remove this in favor of wiring it directly.
@@ -1471,10 +1475,9 @@ public class RootUiCoordinator
         BottomSheetControllerFactory.attach(mWindowAndroid, mBottomSheetController);
 
         mBottomSheetManager = new BottomSheetManager(mBottomSheetController, mActivityTabProvider,
-                mBrowserControlsManager, mModalDialogManagerSupplier,
-                this::getBottomSheetSnackbarManager, mTabObscuringHandlerSupplier.get(),
-                mOmniboxFocusStateSupplier, panelManagerSupplier,
-                mLayoutStateProviderOneShotSupplier);
+                mBrowserControlsManager, mExpandedBottomSheetHelper,
+                this::getBottomSheetSnackbarManager, mOmniboxFocusStateSupplier,
+                panelManagerSupplier, mLayoutStateProviderOneShotSupplier);
 
         // TODO(crbug.com/1279941): Consider moving handler registration to feature code.
         if (BackPressManager.isEnabled()) {
---
+++ components/browser_ui/bottomsheet/android/java/src/org/chromium/components/browser_ui/bottomsheet/ExpandedSheetHelper.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.browser_ui.bottomsheet;
+
+/**
+ * Handles interaction with other UI's when a bottom sheet goes in and out of expanded mode.
+ */
+public interface ExpandedSheetHelper {
+    /** Sheet gets expanded. */
+    void onSheetExpanded();
+    /** Sheet gets collapsed. */
+    void onSheetCollapsed();
+}
