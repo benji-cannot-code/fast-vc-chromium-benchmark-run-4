fff8911ad532bc59f4510df7a5b8d26ac4fd2b65
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2023-06-09 00:40:00 +0000

    Don't crash on invalid condition type.
    In https://chromium-review.googlesource.com/c/chromium/src/+/4570020/comment/9531e6c5_56dd9686/,
    I was pointed out that the code may cause the browser process crash.
    Since the ServiceWorkerRouterEvaluator is executed in the browser
    process, it should not be crashed by the renderer's input.
    
    Bug: 1371756
    Change-Id: Id62e9dd7085f23163a59a734b740e1f228d39036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600252
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155269}

--- content/common/service_worker/service_worker_router_evaluator.cc
+++ content/common/service_worker/service_worker_router_evaluator.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/service_worker/service_worker_router_evaluator.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "third_party/liburlpattern/options.h"
 #include "third_party/liburlpattern/pattern.h"
 #include "third_party/re2/src/re2/re2.h"
@@ -20,6 +21,21 @@ std::string ConvertToRegex(const blink::UrlPattern& url_pattern) {
   liburlpattern::Pattern pattern(url_pattern.pathname, options, "[^/]+?");
   VLOG(3) << "regex string:" << pattern.GenerateRegexString();
   return pattern.GenerateRegexString();
+}
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class ServiceWorkerRouterEvaluatorErrorEnums {
+  kNoError = 0,
+  kInvalidType = 1,
+  kParseError = 2,
+  kCompileError = 3,
+
+  kMaxValue = kCompileError,
+};
+
+void RecordSetupError(ServiceWorkerRouterEvaluatorErrorEnums e) {
+  base::UmaHistogramEnumeration("ServiceWorker.RouterEvaluator.Error", e);
 }
 
 }  // namespace
@@ -47,22 +63,29 @@ void ServiceWorkerRouterEvaluator::Compile() {
   for (const auto& r : rules_.rules) {
     std::unique_ptr<RouterRule> rule = absl::make_unique<RouterRule>();
     for (const auto& condition : r.conditions) {
-      CHECK_EQ(condition.type,
-               blink::ServiceWorkerRouterCondition::ConditionType::kUrlPattern);
+      if (condition.type !=
+          blink::ServiceWorkerRouterCondition::ConditionType::kUrlPattern) {
+        // Unexpected condition type.
+        RecordSetupError(ServiceWorkerRouterEvaluatorErrorEnums::kInvalidType);
+        return;
+      }
       if (rule->url_patterns.Add(ConvertToRegex(*condition.url_pattern),
                                  nullptr) == -1) {
         // Failed to parse the regex.
+        RecordSetupError(ServiceWorkerRouterEvaluatorErrorEnums::kParseError);
         return;
       }
     }
     rule->url_pattern_length = r.conditions.size();
     if (!rule->url_patterns.Compile()) {
       // Failed to compile the regex.
+      RecordSetupError(ServiceWorkerRouterEvaluatorErrorEnums::kCompileError);
       return;
     }
     rule->sources = r.sources;
     compiled_rules_.emplace_back(std::move(rule));
   }
+  RecordSetupError(ServiceWorkerRouterEvaluatorErrorEnums::kNoError);
   is_valid_ = true;
 }
 
