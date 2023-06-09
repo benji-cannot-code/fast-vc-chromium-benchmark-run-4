05d6bd02dc6309243871b1cb193cd644c7c1aaf0
Samuel Huang <huangs@chromium.org>
2023-06-09 14:58:36 +0000

    [GRIT] Fix gzip_string.GzipString() comment.
    This is a follow-up to crrev.com/c/4582853. Also update imports.
    
    Bug: 1449810
    Change-Id: I717912caf9e0a9ea434adf72929caaad8cd8ea8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584090
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155526}

--- tools/grit/grit/format/gzip_string.py
+++ tools/grit/grit/format/gzip_string.py
@@ -5,19 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Provides gzip utilities for strings.
 """
 
-
 import gzip
 import io
-import subprocess
 
 
 def GzipString(data):
-  # Gzipping using Python's built in gzip: Windows doesn't ship with gzip, and
-  # OSX's gzip does not have an --rsyncable option built in. Although this is
-  # not preferable to --rsyncable, it is an option for the systems that do
-  # not have --rsyncable. If used over GzipStringRsyncable, the primary
-  # difference of this function's compression will be larger updates every time
-  # a compressed resource is changed.
+  # Gzipping using Python's built in gzip. On Linux, previously there was
+  # alternative that calls system gzip with --rsyncable to reduce delta.
+  # However, this is of dubious value, and creates inconsistencies across. So
+  # now only Python's gzip is used.
   if isinstance(data, str):
     data = data.encode('utf8')
   gzip_output = io.BytesIO()
