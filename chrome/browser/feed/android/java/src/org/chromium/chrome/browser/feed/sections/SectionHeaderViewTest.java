07474a3c66c9a8cd4224ded106c3a9600117ce89
Sky Malice <skym@chromium.org>
2023-06-09 20:20:53 +0000

    Fix Web Feed IPH assert.
    The IPH is being requested with an anchor view that is not fully in
    the view hierarchy yet. This means the IPH does not shown, at least
    initially. TextBubble asserts that it was shown.
    
    Bug: 1448368
    Change-Id: Ic735cd3f65358373ffa60840e3ae35727fbc5923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598412
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155727}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderViewTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderViewTest.java
@@ -11,6 +11,8 @@ import static org.mockito.Mockito.verify;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.test.filters.SmallTest;
 
@@ -59,6 +61,10 @@ public final class SectionHeaderViewTest {
         // Build the class under test, and set up the fake UI.
         mSectionHeaderView = (SectionHeaderView) LayoutInflater.from(mActivity).inflate(
                 R.layout.new_tab_page_multi_feed_header, null, false);
+        ViewGroup contentView = new LinearLayout(mActivity);
+        mActivity.setContentView(contentView);
+        contentView.addView(mSectionHeaderView);
+
         mSectionHeaderView.addTab();
         mSectionHeaderView.addTab();
     }
