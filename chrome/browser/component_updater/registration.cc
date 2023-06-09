f42e9d9f15434395264edf195851dbf46a27ef68
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-06-09 19:51:03 +0000

    [Privacy Sandbox] Create privacy sandbox attestations component installer policy.
    Implement PrivacySandboxAttestationsComponentInstallerPolicy and its
    registration to the component updater.
    
    This component is gated on feature kEnforcePrivacySandboxAttestations.
    If the feature is disabled, this component will not be registered.
    
    This component will receive a proto attestations list. The list will
    be parsed and loaded into an in-memory map, which is used to check if
    the Privacy Sandbox API is enrolled or not.
    
    To be implemented in next few CLs:
    
    1. The loading (opening the file, invoke the parser, maintaining the
    parsing status, and store the result back to the handler) of the
    attestations list will be implemented in class PrivacySandboxAttestationsLoader.
    
    2. The parsing of the attestations list will be implemented in
    class PrivacySandboxAttestationsParser.
    
    3. The class PrivacySandboxAttestationsHandler is a global singleton
    that holds the attestations data. All queries on API enrollment status
    will be directed to the Handler.
    
    Related CL: crrev.com/c/4480525
    Public blog:
    https://developer.chrome.com/blog/announce-enrollment-privacy-sandbox/
    
    Change-Id: I3fba325b51386a0830846d8bde051749bd4d0136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583113
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155703}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/hyphenation_component_installer.h"
 #include "chrome/browser/component_updater/mei_preload_component_installer.h"
 #include "chrome/browser/component_updater/pki_metadata_component_installer.h"
+#include "chrome/browser/component_updater/privacy_sandbox_attestations_component_installer.h"
 #include "chrome/browser/component_updater/ssl_error_assistant_component_installer.h"
 #include "chrome/browser/component_updater/subresource_filter_component_installer.h"
 #include "chrome/browser/component_updater/trust_token_key_commitments_component_installer.h"
@@ -140,6 +141,7 @@ void RegisterComponentsForUpdate() {
   RegisterTrustTokenKeyCommitmentsComponentIfTrustTokensEnabled(cus);
   RegisterFirstPartySetsComponent(cus);
   RegisterMaskedDomainListComponent(cus);
+  RegisterPrivacySandboxAttestationsComponent(cus);
 
   base::FilePath path;
   if (base::PathService::Get(chrome::DIR_USER_DATA, &path)) {
