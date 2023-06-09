a4d2f94fa74cca9aeb36cb00ac745eff769d6328
Maggie Cai <mxcai@google.com>
2023-06-09 08:12:28 +0000

    [Shortcut] Add shortcut info into chrome://app-service-internals.
    This CL adds the shortcut information into
    chrome://app-service-internals page.
    
    BUG=1412708
    
    Change-Id: I473daafbeb8dfe2794e87f1140e0aad5e43332f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600634
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155404}

--- chrome/browser/resources/app_service_internals/app_service_internals.ts
+++ chrome/browser/resources/app_service_internals/app_service_internals.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './app_service_internals.html.js';
-import {AppCapabilityInfo, AppInfo, AppServiceInternalsPageHandler, PreferredAppInfo, PromiseAppInfo} from './app_service_internals.mojom-webui.js';
+import {AppCapabilityInfo, AppInfo, AppServiceInternalsPageHandler, PreferredAppInfo, PromiseAppInfo, ShortcutInfo} from './app_service_internals.mojom-webui.js';
 
 export class AppServiceInternalsElement extends PolymerElement {
   static get is() {
@@ -23,6 +23,7 @@ export class AppServiceInternalsElement extends PolymerElement {
       preferredAppList_: Array,
       promiseAppList_: Array,
       appCapabilityList_: Array,
+      shortcutList_: Array,
     };
   }
 
@@ -35,6 +36,8 @@ export class AppServiceInternalsElement extends PolymerElement {
   private promiseAppList_: PromiseAppInfo[] = [];
   /** List containing app capability access information. */
   private appCapabilityList_: AppCapabilityInfo[] = [];
+  /** List containing debug information for all shortcuts. */
+  private shortcutList_: ShortcutInfo[] = [];
 
   override ready() {
     super.ready();
@@ -47,6 +50,7 @@ export class AppServiceInternalsElement extends PolymerElement {
         this.preferredAppList_ = debugInfo.preferredAppList;
         this.promiseAppList_ = debugInfo.promiseAppList;
         this.appCapabilityList_ = debugInfo.appCapabilityList;
+        this.shortcutList_ = debugInfo.shortcutList;
       }
       window.addEventListener('hashchange', this.hashChangeListener_);
       // setTimeout ensures that we only apply the hash change after all the
@@ -101,6 +105,14 @@ export class AppServiceInternalsElement extends PolymerElement {
       fileParts.push(appCapability.name + '\n');
       fileParts.push('-----\n');
       fileParts.push(appCapability.debugInfo + '\n');
+    }
+
+    fileParts.push('Shortcut List\n');
+    fileParts.push('================\n\n');
+    for (const shortcut of this.shortcutList_) {
+      fileParts.push(shortcut.name + '\n');
+      fileParts.push('-----\n');
+      fileParts.push(shortcut.debugInfo + '\n');
     }
 
     const file = new Blob(fileParts);
