3a4240dfd1523fb289ddb1d238fe18d37ae83f80
Jun Ishiguro <junis@google.com>
2023-06-09 00:12:35 +0000

    Reland "Lacros: Disable DevTools in Ash by default"
    This CL resolves the issues caused by the original CL and relands it.
    
    The original CL added a new dependency of
    //chrome/browser/ash/crosapi:browser_util to //chrome/browser/ui/views,
    and this caused "multiple definitions" errors when component build is
    enabled. The cause of this problem was the dependency from a component
    to a source_set as described in the following document:
    https://chromium.googlesource.com/chromium/src/+/main/docs/component_build.md
    
    This CL moves accelerator_table.{cc,h} from a component
    (//chrome/browser/ui/views) to a static_library (//chrome/browser/ui)
    so as not to introduce the problematic dependency.
    
    Revert commit's description:
    > Revert "Lacros: Disable DevTools in Ash by default"
    >
    > This reverts commit 7af64004607c083afd3066eb69cd13b36fbe5726.
    >
    > Reason for revert: per Elly's suggestion, revert because it causes test issues: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/33054/overview
    >
    > Original change's description:
    > > Lacros: Disable DevTools in Ash by default
    > >
    > > This CL removes the context menu items and disables the shortcuts for
    > > DevTools and the page source viewer in Ash if Lacros is the only
    > > browser. By enabling #allow-devtools-in-system-ui flag in os://flags,
    > > user can access these features even in Ash.
    > >
    > > This CL adds a new feature flag for ChromeOS: #allow-devtools-in-system-ui
    > >
    > > Bug: 1421637, 1444283
    > > Change-Id: Ib4ae69c97ccb7c1c0f1743a0d7281a1fb474ae06
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542584
    > > Reviewed-by: Georg Neis <neis@chromium.org>
    > > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > > Commit-Queue: Jun Ishiguro <junis@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1151669}
    >
    > Bug: 1421637, 1444283
    > Change-Id: I00ce61483cbb4140822ba9b2508ddcc433a701bf
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582688
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Yi Gu <yigu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1152151}
    
    Bug: 1421637, 1444283, 1450589
    Change-Id: Ia04310a332bd9c3ad17ccd0a208b7c4238253a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4592529
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155247}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2882,6 +2882,9 @@ extern const char kAdaptiveChargingDescription[];
 extern const char kAdaptiveChargingForTestingName[];
 extern const char kAdaptiveChargingForTestingDescription[];
 
+extern const char kAllowDevtoolsInSystemUIName[];
+extern const char kAllowDevtoolsInSystemUIDescription[];
+
 extern const char kAllowEapDefaultCasWithoutSubjectVerificationName[];
 extern const char kAllowEapDefaultCasWithoutSubjectVerificationDescription[];
 
