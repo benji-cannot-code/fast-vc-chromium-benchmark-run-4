0f5f1c8437cfbce2ee8b87d8a5cc14e2cad3eb50
Weizhong Xia <weizhong@google.com>
2023-06-09 00:14:51 +0000

    Make Port._build_path a public method
    So that this function can be used by other components.
    
    Bug: 1426296
    Change-Id: Id2fc13cf85733611f0139480a1a5f84a75115ce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595996
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1155250}

--- third_party/blink/tools/blinkpy/wpt_tests/product.py
+++ third_party/blink/tools/blinkpy/wpt_tests/product.py
@@ -105,7 +105,7 @@ class Chrome(Product):
             binary_path = self._host.filesystem.join('Chromium.app',
                                                      'Contents', 'MacOS',
                                                      'Chromium')
-        return self._port._build_path(binary_path)
+        return self._port.build_path(binary_path)
 
     @property
     def default_webdriver_binary(self):
@@ -113,7 +113,7 @@ class Chrome(Product):
             path = 'chromedriver.exe'
         else:
             path = 'chromedriver'  #linux and mac
-        return self._port._build_path(path)
+        return self._port.build_path(path)
 
 
 class ContentShell(Product):
@@ -135,7 +135,7 @@ class ContentShell(Product):
             binary_path = self._host.filesystem.join('Content Shell.app',
                                                      'Contents', 'MacOS',
                                                      'Content Shell')
-        return self._port._build_path(binary_path)
+        return self._port.build_path(binary_path)
 
 
 class ChromeiOS(Product):
@@ -280,7 +280,7 @@ class ChromeAndroidBase(Product):
 
     @property
     def default_webdriver_binary(self):
-        return self._port._build_path('clang_x64', 'chromedriver')
+        return self._port.build_path('clang_x64', 'chromedriver')
 
     def get_browser_package_name(self):
         """Get the name of the package to run tests against.
@@ -349,11 +349,11 @@ class WebView(ChromeAndroidBase):
 
     @property
     def default_browser_apk(self):
-        return self._port._build_path('apks', 'SystemWebViewShell.apk')
+        return self._port.build_path('apks', 'SystemWebViewShell.apk')
 
     @property
     def default_webview_provider(self):
-        return self._port._build_path('apks', 'SystemWebView.apk')
+        return self._port.build_path('apks', 'SystemWebView.apk')
 
     def _install_webview(self, device):
         # Prioritize local builds.
@@ -382,4 +382,4 @@ class ChromeAndroid(ChromeAndroidBase):
 
     @property
     def default_browser_apk(self):
-        return self._port._build_path('apks', 'ChromePublic.apk')
+        return self._port.build_path('apks', 'ChromePublic.apk')
