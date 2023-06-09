4279b8eda785c6f31c9972ab11fd72fb8fd6c408
Jun Ishiguro <junis@google.com>
2023-06-09 02:35:40 +0000

    Add unit tests for the DevTools accelerators
    This CL makes `GetAcceleratorList` testable by adding functions:
    - `GetAcceleratorsPointer` holds a std::vector for accelerator mappings
      and returns a pointer to it.
    - `ClearAcceleratorListForTesting` clears the accelerator mappings.
    
    Also, this CL adds unit tests for DevTools accelerators, which are
    conditionally enabled in Ash.
    
    Bug: 1421637
    Change-Id: Ie160e85445dd8049232adaf8bf7af96fc49577fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569975
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1155312}

--- chrome/browser/ui/views/accelerator_table.cc
+++ chrome/browser/ui/views/accelerator_table.cc
@@ -305,16 +305,20 @@ const int kRepeatableCommandIds[] = {
     IDC_SELECT_NEXT_TAB,     IDC_SELECT_PREVIOUS_TAB,
 };
 
-} // namespace
+std::vector<AcceleratorMapping>* GetAcceleratorsPointer() {
+  static base::NoDestructor<std::vector<AcceleratorMapping>> accelerators;
+  return accelerators.get();
+}
+
+}  // namespace
 
 std::vector<AcceleratorMapping> GetAcceleratorList() {
-  static base::NoDestructor<std::vector<AcceleratorMapping>> accelerators;
+  std::vector<AcceleratorMapping>* accelerators = GetAcceleratorsPointer();
 
   if (accelerators->empty()) {
     accelerators->insert(accelerators->begin(), std::begin(kAcceleratorMap),
                          std::end(kAcceleratorMap));
 
-    // TODO(junis): Add unit tests for DevTools accelerators
     bool enable_devtools = true;
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     // In Ash, DevTools is disabled by default if lacros is the only browser, in
@@ -365,6 +369,11 @@ std::vector<AcceleratorMapping> GetAcceleratorList() {
   }
 
   return *accelerators;
+}
+
+void ClearAcceleratorListForTesting() {
+  std::vector<AcceleratorMapping>* accelerators = GetAcceleratorsPointer();
+  accelerators->clear();
 }
 
 bool GetStandardAcceleratorForCommandId(int command_id,
