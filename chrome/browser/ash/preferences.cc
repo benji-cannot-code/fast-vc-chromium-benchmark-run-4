c3a4b207c0d9aa649511f55c3ed6975af0a0007d
Moteva <moteva@google.com>
2023-06-09 04:00:42 +0000

    CrOS Settings: Separating first and subsequent lifetime metrics.
    Separating "ChromeOS.Settings.NumUniqueSettingsChanged.DeviceLifetime"
    into ".FirstWeek" and ".SubsequentWeeks". Assuming that users will change more Settings in the first week to customize their device to their liking, so to understand the data better, we will separate the data into 2 histograms. This will help us understand the true value of how many Settings users will change on average, without the data from the first week skewing the results.
    
    This CL is a follow-up to b/280480850.
    
    BUG=b/281946097
    TEST=
    Manual:
    A)
      1. Run ChromeOS with OOBE. Take the onboarding steps.
      2. Open Settings.
      2. Change a Setting that we are recording the name of, such as
         switching the toggle for Do Not Disturb.
      3. Close the Settings app, open your browser and search
         "chrome://histograms", then find the histogram
         "ChromeOS.Settings.NumUniqueSettingsChanged.DeviceLifetime.FirstWeek". There
         will be one count added to the respective bucket. The bucket
         represents the number of unique changes made over the device's
         lifetime.
      4. Repeat steps 1-3 with different Settings changed each time. The
         histogram will each time record the correct number of accumulated
         unique Settings changed.
    B)
      1. Repeat 'A' after a week time has passed.
      2. The corresponding histogram will now be called
         "ChromeOS.Settings.NumUniqueSettingsChanged.DeviceLifetime.SunsequentWeeks"
          NOTE: The recorded count has now been reset, meaning that if we repeat `Do not disturb` toggle
          flip after the first week, we will now record it again in
          histogram ".SubsequentWeeks".
      3. No data will be added to ".FirstWeek" after the first week has
         passed.
    
      --gtest_filter="*SettingsUserActionTrackerTest*"
    
    Unittest: DISPLAY=:20 out/Default/unit_tests
    Change-Id: I9c912f7c824e5a33713ed44ca9245e5058197009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568865
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155336}

--- chrome/browser/ash/preferences.cc
+++ chrome/browser/ash/preferences.cc
@@ -596,6 +596,9 @@ void Preferences::RegisterProfilePrefs(
   registry->RegisterBooleanPref(prefs::kShowDisplaySizeScreenEnabled, true);
 
   registry->RegisterDictionaryPref(::prefs::kTotalUniqueOsSettingsChanged);
+
+  registry->RegisterBooleanPref(::prefs::kHasResetFirst7DaysSettingsUsedCount,
+                                false);
 }
 
 void Preferences::InitUserPrefs(sync_preferences::PrefServiceSyncable* prefs) {
