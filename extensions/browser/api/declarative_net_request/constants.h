a23e78ec148919874b54edfb4f5118814587213e
Yannay Hammer <yannayha@gmail.com>
2023-06-09 19:47:12 +0000

    Allowing append to user-agent in declarative net request
    Added user-agent to list of headers allowed to append to in declarative
    net requests for extensions, in order to allow extensions to add data
    to user-agent to identify themselves, without overriding the browser's
    user-agent.
    
    Change-Id: Icc62d5dbe114db103067de73f4a5ee0bb6313198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4536897
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Yannay Hammer <yannayha@gmail.com>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155701}

--- extensions/browser/api/declarative_net_request/constants.h
+++ extensions/browser/api/declarative_net_request/constants.h
@@ -274,6 +274,7 @@ constexpr auto kDNRRequestHeaderAppendAllowList =
          {"trailer", ""},
          {"transfer-encoding", ", "},
          {"upgrade", ", "},
+         {"user-agent", " "},
          {"via", ", "},
          {"want-digest", ", "},
          {"x-forwarded-for", ", "}});
