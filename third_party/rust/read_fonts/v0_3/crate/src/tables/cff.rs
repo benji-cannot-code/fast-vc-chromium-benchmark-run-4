dea085392f4059bd716e312999f1035283addb49
Dominik Röttsches <drott@chromium.org>
2023-06-09 16:38:29 +0000

    Add Fontations libraries as Rust third-party crates
    Needed for prototype integration of fontations font libraries to
    Chromium. Reasoning [1], approved Chrome Third-Party Review [2],
    approved security review [3].
    
    [1] https://docs.google.com/document/d/1gAnqhJC3glyHgvGx1MSFbWFKNcfENw0UvmyS52XUmPk/edit#
    [2] https://groups.google.com/a/google.com/g/chrome-atls-discuss/c/UijPu24OIhM
    [3] https://docs.google.com/document/d/1Whi4ONnDPdeBqXUViRLMaLTtLkFE_Qs2xRXZl0vDfYo/edit#heading=h.lwiipup6ufjp
    
    Bug: 1446293
    Change-Id: I7440cad42d8aaf98d4043f613039afd0678f474b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542966
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155572}

---
+++ third_party/rust/read_fonts/v0_3/crate/src/tables/cff.rs
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! The [CFF](https://learn.microsoft.com/en-us/typography/opentype/spec/cff) table
+
+include!("../../generated/generated_cff.rs");
---
+++ third_party/rust/read_fonts/v0_3/crate/src/tables/vhea.rs
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! the [vhea (Horizontal Header)](https://docs.microsoft.com/en-us/typography/opentype/spec/hhea) table
+
+include!("../../generated/generated_vhea.rs");
