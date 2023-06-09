573f9d98c40dad30525a6e9b321ca6a2b53c5a50
Yuki Awano <yawano@google.com>
2023-06-09 00:36:09 +0000

    Add conditions for ScalableIph availability
    - Add CustomizableTestEnvBrowserTestBase class to:
      - Allow simulation of various session types and device states.
      - Consolidate common logic between ScalableIph and GoogleOneOfferIph.
    - guest_session_mixin has moved in BUILD files to avoid a circular
      dependency.
    
    Bug: b:285158274
    Test: browser_tests --gtest_filter=*ScalableIph*
    Test: browser_tests --gtest_filter=*GoogleOneOfferIph*
    Change-Id: Ie3e113a66fe7e1f45e683799872e475bf575f33d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4596325
    Reviewed-by: Angela Xiao <angelaxiao@chromium.org>
    Quick-Run: Yuki Awano <yawano@google.com>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155267}

--- chrome/browser/ash/scalable_iph/scalable_iph_browser_test_base.cc
+++ chrome/browser/ash/scalable_iph/scalable_iph_browser_test_base.cc
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/functional/bind.h"
+#include "chrome/browser/ash/scalable_iph/customizable_test_env_browser_test_base.h"
 #include "chrome/browser/ash/scalable_iph/mock_scalable_iph_delegate.h"
 #include "chrome/browser/ash/scalable_iph/scalable_iph_factory.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/test/base/in_process_browser_test.h"
 #include "chromeos/ash/components/scalable_iph/scalable_iph.h"
 #include "chromeos/ash/components/scalable_iph/scalable_iph_delegate.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -44,12 +44,18 @@ void ScalableIphBrowserTestBase::SetUp() {
           ->RegisterCreateServicesCallbackForTesting(base::BindRepeating(
               &ScalableIphBrowserTestBase::SetTestingFactories));
 
-  InProcessBrowserTest::SetUp();
+  CustomizableTestEnvBrowserTestBase::SetUp();
 }
 
 // `SetUpOnMainThread` is called just before a test body. Do the mock set up in
 // this function as `browser()` is not available in `SetUp` above.
 void ScalableIphBrowserTestBase::SetUpOnMainThread() {
+  // `CustomizableTestEnvBrowserTestBase::SetUpOnMainThread` must be called
+  // before our `SetUpOnMainThread` as login happens in the method, i.e. profile
+  // is not available before it.
+  CustomizableTestEnvBrowserTestBase::SetUpOnMainThread();
+  CHECK(browser()->profile());
+
   mock_tracker_ = static_cast<feature_engagement::test::MockTracker*>(
       feature_engagement::TrackerFactory::GetForBrowserContext(
           browser()->profile()));
@@ -75,8 +81,6 @@ void ScalableIphBrowserTestBase::SetUpOnMainThread() {
   mock_delegate_ = static_cast<test::MockScalableIphDelegate*>(
       scalable_iph->delegate_for_testing());
   CHECK(mock_delegate_);
-
-  InProcessBrowserTest::SetUpOnMainThread();
 }
 
 void ScalableIphBrowserTestBase::TearDownOnMainThread() {
