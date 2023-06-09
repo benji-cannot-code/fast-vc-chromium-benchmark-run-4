2e0913a020be024eaebc46297ea4152cdcda7596
Lei Zhang <thestig@chromium.org>
2023-06-09 23:41:14 +0000

    Parameterize PDFExtensionPrintingTest for OOPPD
    Run PDFExtensionPrintingTest with both OOPPD turned on and turned off.
    
    Bug: 809738
    Change-Id: I8f58af751501c558a3cb409a65a5b0fee293286e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4606011
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155822}

--- chrome/browser/pdf/pdf_extension_printing_test.cc
+++ chrome/browser/pdf/pdf_extension_printing_test.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "printing/buildflags/buildflags.h"
+#include "printing/printing_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
@@ -100,7 +101,8 @@ class PrintObserver : public printing::PrintViewManagerBase::TestObserver {
 
 }  // namespace
 
-class PDFExtensionPrintingTest : public PDFExtensionTestBase {
+class PDFExtensionPrintingTest : public PDFExtensionTestBase,
+                                 public testing::WithParamInterface<bool> {
  public:
   PDFExtensionPrintingTest() = default;
   ~PDFExtensionPrintingTest() override = default;
@@ -115,10 +117,25 @@ class PDFExtensionPrintingTest : public PDFExtensionTestBase {
     SetShowPrintErrorDialogForTest(base::NullCallback());
     PDFExtensionTestBase::TearDownOnMainThread();
   }
+  std::vector<base::test::FeatureRef> GetEnabledFeatures() const override {
+    if (UseService()) {
+      return {printing::features::kEnableOopPrintDrivers};
+    }
+    return {};
+  }
+  std::vector<base::test::FeatureRef> GetDisabledFeatures() const override {
+    if (UseService()) {
+      return {};
+    }
+    return {printing::features::kEnableOopPrintDrivers};
+  }
+
+ private:
+  bool UseService() const { return GetParam(); }
 };
 
 // Flaky. See http://crbug.com/1415194
-IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest, DISABLED_BasicPrintCommand) {
+IN_PROC_BROWSER_TEST_P(PDFExtensionPrintingTest, DISABLED_BasicPrintCommand) {
   MimeHandlerViewGuest* guest = LoadPdfGetMimeHandlerView(
       embedded_test_server()->GetURL("/pdf/test.pdf"));
   content::RenderFrameHost* frame = GetPluginFrame(guest);
@@ -130,7 +147,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest, DISABLED_BasicPrintCommand) {
 }
 
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
-IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest, PrintCommand) {
+IN_PROC_BROWSER_TEST_P(PDFExtensionPrintingTest, PrintCommand) {
   MimeHandlerViewGuest* guest = LoadPdfGetMimeHandlerView(
       embedded_test_server()->GetURL("/pdf/test.pdf"));
   content::RenderFrameHost* frame = GetPluginFrame(guest);
@@ -141,7 +158,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest, PrintCommand) {
   print_observer.WaitForPrintPreview();
 }
 
-IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest,
+IN_PROC_BROWSER_TEST_P(PDFExtensionPrintingTest,
                        ContextMenuPrintCommandExtensionMainFrame) {
   MimeHandlerViewGuest* guest = LoadPdfGetMimeHandlerView(
       embedded_test_server()->GetURL("/pdf/test.pdf"));
@@ -163,7 +180,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest,
 }
 
 // TODO(crbug.com/1344508): Test is flaky on multiple platforms.
-IN_PROC_BROWSER_TEST_F(
+IN_PROC_BROWSER_TEST_P(
     PDFExtensionPrintingTest,
     DISABLED_ContextMenuPrintCommandEmbeddedExtensionMainFrame) {
   MimeHandlerViewGuest* guest = LoadPdfGetMimeHandlerView(
@@ -187,7 +204,7 @@ IN_PROC_BROWSER_TEST_F(
   menu_interceptor.Wait();
 }
 
-IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest,
+IN_PROC_BROWSER_TEST_P(PDFExtensionPrintingTest,
                        ContextMenuPrintCommandPluginFrame) {
   MimeHandlerViewGuest* guest = LoadPdfGetMimeHandlerView(
       embedded_test_server()->GetURL("/pdf/test.pdf"));
@@ -209,7 +226,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest,
 }
 
 // TODO(crbug.com/1330032): Fix flakiness.
-IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest,
+IN_PROC_BROWSER_TEST_P(PDFExtensionPrintingTest,
                        DISABLED_ContextMenuPrintCommandEmbeddedPluginFrame) {
   MimeHandlerViewGuest* guest = LoadPdfGetMimeHandlerView(
       embedded_test_server()->GetURL("/pdf/pdf_embed.html"));
@@ -230,7 +247,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest,
   menu_interceptor.Wait();
 }
 
-IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest, PrintButton) {
+IN_PROC_BROWSER_TEST_P(PDFExtensionPrintingTest, PrintButton) {
   MimeHandlerViewGuest* guest = LoadPdfGetMimeHandlerView(
       embedded_test_server()->GetURL("/pdf/test.pdf"));
   content::RenderFrameHost* frame = GetPluginFrame(guest);
@@ -246,3 +263,5 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionPrintingTest, PrintButton) {
   print_observer.WaitForPrintPreview();
 }
 #endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
+
+INSTANTIATE_TEST_SUITE_P(All, PDFExtensionPrintingTest, testing::Bool());
