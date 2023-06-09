733ce5de38c15c62255a5e18f60a13f8b9d4e1c4
Juliet Levesque <julietlevesque@google.com>
2023-06-09 23:51:13 +0000

    [Nearby Presence] Generate credentials in CredentialManager through mojo
    The CredentialManager uses the mojo remote passed in its constructor to
    generate credentials in the NP library. In a follow up CL, the
    CredentialManager will upload these credentials to the NP server. See
    go/cros-nearby-presence-credential-web-integration.
    
    Test: Unit tests only
    Bug: b/276307539
    Change-Id: I845cc9bc025d215f48d43dd9c7bf177f40bfcfbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545403
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155827}

--- chromeos/ash/components/nearby/presence/credentials/proto_conversions.h
+++ chromeos/ash/components/nearby/presence/credentials/proto_conversions.h
@@ -3,11 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/nearby/internal/proto/metadata.pb.h"
-
 #ifndef CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
 #define CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
+
+#include "chromeos/ash/services/nearby/public/mojom/nearby_presence.mojom-forward.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/nearby/internal/proto/credential.pb.h"
+#include "third_party/nearby/internal/proto/metadata.pb.h"
 
 namespace ash::nearby::presence {
 
@@ -18,6 +20,15 @@ namespace ash::nearby::presence {
     const std::string& user_name,
     const std::string& profile_url,
     const std::string& mac_address);
+
+mojom::PresenceDeviceType DeviceTypeToMojom(
+    ::nearby::internal::DeviceType device_type);
+mojom::MetadataPtr MetadataToMojom(::nearby::internal::Metadata metadata);
+
+::nearby::internal::IdentityType IdentityTypeFromMojom(
+    mojom::IdentityType identity_type);
+::nearby::internal::SharedCredential SharedCredentialFromMojom(
+    mojom::SharedCredential* shared_credential);
 
 }  // namespace ash::nearby::presence
 
--- chromeos/ash/services/nearby/public/cpp/fake_nearby_presence.cc
+++ chromeos/ash/services/nearby/public/cpp/fake_nearby_presence.cc
@@ -56,6 +56,8 @@ void FakeNearbyPresence::UpdateLocalDeviceMetadata(
 void FakeNearbyPresence::UpdateLocalDeviceMetadataAndGenerateCredentials(
     mojom::MetadataPtr metadata,
     FakeNearbyPresence::UpdateLocalDeviceMetadataAndGenerateCredentialsCallback
-        callback) {}
+        callback) {
+  std::move(callback).Run(std::move(shared_credentials_), status_);
+}
 
 }  // namespace ash::nearby::presence
