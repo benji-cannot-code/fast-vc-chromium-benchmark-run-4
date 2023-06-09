bac623f4d3d8e7964920ef4d734472a387b200dd
Paul Adedeji <pauladedeji@google.com>
2023-06-09 21:25:35 +0000

    [ntp] Close Panorama IPH across navigations
    *Video demo in bug description*
    
    Change-Id: I72b768f69baa76506c48d0cb8b6df0d0a7730867
    Bug: 1440470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599568
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1155762}

--- chrome/browser/ui/search/search_tab_helper.cc
+++ chrome/browser/ui/search/search_tab_helper.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/bookmarks/browser/bookmark_model.h"
+#include "components/feature_engagement/public/feature_constants.h"
 #include "components/google/core/common/google_util.h"
 #include "components/navigation_metrics/navigation_metrics.h"
 #include "components/profile_metrics/browser_profile_type.h"
@@ -190,6 +191,8 @@ void SearchTabHelper::OnTabActivated() {
 
   if (search::IsInstantNTP(web_contents()) && instant_service_)
     instant_service_->OnNewTabPageOpened();
+
+  CloseNTPCustomizeChromeFeaturePromo();
 }
 
 void SearchTabHelper::OnTabDeactivated() {
@@ -219,6 +222,8 @@ void SearchTabHelper::DidStartNavigation(
           entry, l10n_util::GetStringUTF16(IDS_NEW_TAB_TITLE));
     }
   }
+
+  CloseNTPCustomizeChromeFeaturePromo();
 }
 
 void SearchTabHelper::TitleWasSet(content::NavigationEntry* entry) {
@@ -320,6 +325,21 @@ Profile* SearchTabHelper::profile() const {
 
 bool SearchTabHelper::IsInputInProgress() const {
   return search::IsOmniboxInputInProgress(web_contents());
+}
+
+void SearchTabHelper::CloseNTPCustomizeChromeFeaturePromo() {
+  const base::Feature& customize_chrome_feature =
+      feature_engagement::kIPHDesktopCustomizeChromeFeature;
+  if (!base::FeatureList::IsEnabled(customize_chrome_feature) ||
+      web_contents()->GetController().GetVisibleEntry()->GetURL() ==
+          GURL(chrome::kChromeUINewTabPageURL)) {
+    return;
+  }
+  auto* browser_window =
+      BrowserWindow::FindBrowserWindowWithWebContents(web_contents());
+  if (browser_window) {
+    browser_window->CloseFeaturePromo(customize_chrome_feature);
+  }
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(SearchTabHelper);
