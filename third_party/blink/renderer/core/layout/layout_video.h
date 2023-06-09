879d42b42637fe4f4ea6902bdf9cd72f11e9c20a
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-06-09 17:59:30 +0000

    [RemoveLegacy] Remove LayoutVideo::UpdateLayout.
    This moves the invalidation logic that was previously in UpdateLayout
    to UpdateAfterLayout.
    
    There should be no behaviour change from this patch.
    
    Bug: 1229581
    Change-Id: I013a8277e0592b927985accd13dd616339dfbdbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599187
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155621}

--- third_party/blink/renderer/core/layout/layout_video.h
+++ third_party/blink/renderer/core/layout/layout_video.h
@@ -66,10 +66,10 @@ class CORE_EXPORT LayoutVideo final : public LayoutMedia {
                                  : kOverflowClipBothAxis;
   }
 
-  void UpdatePlayer(bool is_in_layout);
-
  private:
-  void UpdateFromElement() override;
+  void UpdateAfterLayout() final;
+  void UpdateFromElement() final;
+  void InvalidateCompositing();
 
   LayoutSize CalculateIntrinsicSize(float scale);
   void UpdateIntrinsicSize();
@@ -83,8 +83,6 @@ class CORE_EXPORT LayoutVideo final : public LayoutMedia {
 
   void PaintReplaced(const PaintInfo&,
                      const PhysicalOffset& paint_offset) const override;
-
-  void UpdateLayout() override;
 
   bool CanHaveAdditionalCompositingReasons() const override {
     NOT_DESTROYED();
