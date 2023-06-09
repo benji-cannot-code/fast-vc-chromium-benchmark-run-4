b732a818bf85c10a0b581fd24cecba91573f9749
Tim Sergeant <tsergeant@chromium.org>
2023-06-09 00:21:43 +0000

    App Service: Update comments on InstanceRegistry
    This updates the comments on InstanceRegistry to:
    
    - Provide information about the user-facing details of the class ahead
      of internal implementation details
    - Mark the extra Observe() methods as deprecated and recommend
      ScopedObservation instead
    
    While I'm here, this also removes the instance_registry() getter on
    InstanceRegistry::Observer, which is only used by tests, and isn't
    compatible with eventually removing the Observe() methods.
    
    Bug: 1453127
    Change-Id: I9fc714104392682d795508b660cf655f5d9b16a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600628
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155258}

--- components/services/app_service/public/cpp/instance_registry_unittest.cc
+++ components/services/app_service/public/cpp/instance_registry_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/contains.h"
+#include "base/memory/raw_ptr.h"
 #include "components/services/app_service/public/cpp/instance.h"
 #include "components/services/app_service/public/cpp/instance_registry.h"
 #include "components/services/app_service/public/cpp/instance_update.h"
@@ -88,7 +89,8 @@ class InstanceRegistryTest : public testing::Test,
 class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
  public:
   explicit InstanceRecursiveObserver(apps::InstanceRegistry* instance_registry)
-      : apps::InstanceRegistry::Observer(instance_registry) {}
+      : apps::InstanceRegistry::Observer(instance_registry),
+        instance_registry_(instance_registry) {}
 
   ~InstanceRecursiveObserver() override = default;
 
@@ -123,7 +125,7 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
   // apps::InstanceRegistry::Observer overrides.
   void OnInstanceUpdate(const apps::InstanceUpdate& outer) override {
     int num_instance = 0;
-    instance_registry()->ForEachInstance(
+    instance_registry_->ForEachInstance(
         [&outer, &num_instance](const apps::InstanceUpdate& inner) {
           if (outer.InstanceId() == inner.InstanceId()) {
             ExpectEq(outer, inner);
@@ -131,12 +133,12 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
           num_instance++;
         });
 
-    EXPECT_TRUE(instance_registry()->ForOneInstance(
+    EXPECT_TRUE(instance_registry_->ForOneInstance(
         outer.InstanceId(), [&outer](const apps::InstanceUpdate& inner) {
           ExpectEq(outer, inner);
         }));
 
-    EXPECT_TRUE(instance_registry()->ForInstancesWithWindow(
+    EXPECT_TRUE(instance_registry_->ForInstancesWithWindow(
         outer.Window(), [&outer](const apps::InstanceUpdate& inner) {
           if (outer.InstanceId() == inner.InstanceId()) {
             ExpectEq(outer, inner);
@@ -155,7 +157,7 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
         super_recursive_instance_params_.pop_back();
         break;
       }
-      instance_registry()->CreateOrUpdateInstance(std::move(*params));
+      instance_registry_->CreateOrUpdateInstance(std::move(*params));
       super_recursive_instance_params_.pop_back();
     }
 
@@ -167,7 +169,7 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
         super_recursive_instances_.pop_back();
         break;
       }
-      instance_registry()->OnInstance(std::move(instance));
+      instance_registry_->OnInstance(std::move(instance));
       super_recursive_instances_.pop_back();
     }
 
@@ -189,6 +191,8 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
     EXPECT_EQ(outer.LastUpdatedTime(), inner.LastUpdatedTime());
     EXPECT_EQ(outer.BrowserContext(), inner.BrowserContext());
   }
+
+  base::raw_ptr<apps::InstanceRegistry> instance_registry_;
 
   int expected_num_instances_ = -1;
   int num_instances_seen_on_instance_update_ = 0;
