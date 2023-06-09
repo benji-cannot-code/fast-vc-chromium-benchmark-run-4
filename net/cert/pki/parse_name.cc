d8e0866d8033dc05098d57241e4c394527a6ab61
Bob Beck <bbe@google.com>
2023-06-09 00:59:45 +0000

    Include cassert when needed.
    Bug: 1322914
    Change-Id: I62343d5a0888f84e58a9d2a908d67be4d2b6b0c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602616
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Bob Beck <bbe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155279}

--- net/cert/pki/parse_name.cc
+++ net/cert/pki/parse_name.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cert/pki/parse_name.h"
 
+#include <cassert>
+
 #include "net/cert/pki/string_util.h"
 #include "net/der/parse_values.h"
 #include "third_party/boringssl/src/include/openssl/bytestring.h"
