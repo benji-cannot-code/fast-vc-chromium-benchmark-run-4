25770743125dbd04c892b8681a7f3b567f3daac3
Colin Blundell <blundell@chromium.org>
2023-06-09 07:27:19 +0000

    [//viz] Convert remaining Width functions in resource_sizes.h to SIF
    This CL converts the following resource_sizes.h functions to take in
    SharedImageFormat rather than ResourceFormat:
    - UncheckedWidthInBytes()
    - CheckedWidthInBytes()
    
    It also eliminates the following unused functions:
    - UncheckedWidthInBytesAligned()
    - MaybeWidthInBytes()
    
    Bug: 1378708
    Change-Id: I86e4d16f25b4676a9dc0bf906c5e4f2e15605d5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595202
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155389}

--- components/viz/common/resources/resource_sizes_unittest.cc
+++ components/viz/common/resources/resource_sizes_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "components/viz/common/resources/resource_sizes.h"
+#include "components/viz/common/resources/shared_image_format.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace viz {
@@ -25,7 +26,7 @@ class ResourceUtilTest : public testing::Test {
   void TestCheckedWidthInBytes(int width, const TestFormat* test_formats) {
     for (int i = 0; i < kTestFormats; ++i) {
       size_t bytes = ResourceSizes::CheckedWidthInBytes<size_t>(
-          width, test_formats[i].format);
+          width, SharedImageFormat::SinglePlane(test_formats[i].format));
       EXPECT_EQ(bytes, test_formats[i].expected_bytes);
     }
   }
@@ -33,17 +34,8 @@ class ResourceUtilTest : public testing::Test {
   void TestUncheckedWidthInBytes(int width, const TestFormat* test_formats) {
     for (int i = 0; i < kTestFormats; ++i) {
       size_t bytes = ResourceSizes::UncheckedWidthInBytes<size_t>(
-          width, test_formats[i].format);
+          width, SharedImageFormat::SinglePlane(test_formats[i].format));
       EXPECT_EQ(bytes, test_formats[i].expected_bytes);
-    }
-  }
-
-  void TestUncheckedWidthInBytesAligned(int width,
-                                        const TestFormat* test_formats) {
-    for (int i = 0; i < kTestFormats; ++i) {
-      size_t bytes = ResourceSizes::UncheckedWidthInBytesAligned<size_t>(
-          width, test_formats[i].format);
-      EXPECT_EQ(bytes, test_formats[i].expected_bytes_aligned);
     }
   }
 
@@ -87,7 +79,6 @@ TEST_F(ResourceUtilTest, WidthInBytes) {
 
   TestCheckedWidthInBytes(width, test_formats);
   TestUncheckedWidthInBytes(width, test_formats);
-  TestUncheckedWidthInBytesAligned(width, test_formats);
 
   // Check bytes for odd width.
   int width_odd = 11;
@@ -100,7 +91,6 @@ TEST_F(ResourceUtilTest, WidthInBytes) {
 
   TestCheckedWidthInBytes(width_odd, test_formats_odd);
   TestUncheckedWidthInBytes(width_odd, test_formats_odd);
-  TestUncheckedWidthInBytesAligned(width_odd, test_formats_odd);
 }
 
 TEST_F(ResourceUtilTest, SizeInBytes) {
@@ -131,17 +121,6 @@ TEST_F(ResourceUtilTest, SizeInBytes) {
   TestUncheckedSizeInBytesAligned(size_odd, test_formats_odd);
 }
 
-TEST_F(ResourceUtilTest, WidthInBytesOverflow) {
-  int width = 10;
-  // 10 * 16 = 160 bits, overflows in char, but fits in unsigned char.
-  signed char ignored_signed;
-  EXPECT_FALSE(ResourceSizes::MaybeWidthInBytes<signed char>(width, RGBA_4444,
-                                                             &ignored_signed));
-  unsigned char ignored_unsigned;
-  EXPECT_TRUE(ResourceSizes::MaybeWidthInBytes<unsigned char>(
-      width, RGBA_4444, &ignored_unsigned));
-}
-
 TEST_F(ResourceUtilTest, SizeInBytesOverflow) {
   gfx::Size size(10, 10);
   // 10 * 16 * 10 = 1600 bits, overflows in char, but fits in int.
@@ -158,8 +137,6 @@ TEST_F(ResourceUtilTest, WidthOverflowDoesNotCrash) {
   // 0x20000000 * 4 = 0x80000000 which overflows int. Should return false, not
   // crash.
   int bytes;
-  EXPECT_FALSE(
-      ResourceSizes::MaybeWidthInBytes<int>(size.width(), BGRA_8888, &bytes));
   EXPECT_FALSE(ResourceSizes::MaybeSizeInBytes<int>(
       size, SinglePlaneFormat::kBGRA_8888, &bytes));
 }
