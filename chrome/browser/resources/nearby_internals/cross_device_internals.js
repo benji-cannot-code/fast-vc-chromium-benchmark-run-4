e1fa2fcdf91def8dc60d18f20c7c298d599f37e8
Alex Kingsborough <akingsb@google.com>
2023-06-10 00:51:51 +0000

    [NP] Add basic UI for Presence Device discovery
    When a presence device is discovered, a basic UI element should pop up
    in chrome://nearby-internals.
    screenshot of changes when devices are added:
    https://screenshot.googleplex.com/4yBgZwBMg2Ybrm7
    
    Test: Manually tested on DUT with Android test app.
    Change-Id: I2aa4df3e791ee52aa233d03008954ed3dc3c6897
    Bug: b/277820435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598003
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1155845}

--- chrome/browser/resources/nearby_internals/cross_device_internals.js
+++ chrome/browser/resources/nearby_internals/cross_device_internals.js
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import './shared_style.css.js';
+import './np_list_object.js';
 
 import {WebUIListenerBehavior} from 'chrome://resources/ash/common/web_ui_listener_behavior.js';
 import {Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './cross_device_internals.html.js';
 import {NearbyPresenceBrowserProxy} from './nearby_presence_browser_proxy.js';
+import {PresenceDevice} from './types.js';
 
 Polymer({
   is: 'cross-device-internals',
@@ -25,6 +27,14 @@ Polymer({
   /** @private {?NearbyPresenceBrowserProxy} */
   browserProxy_: null,
 
+  properties: {
+    /** @private {!Array<!PresenceDevice>} */
+    npDiscoveredDevicesList_: {
+      type: Array,
+      value: [],
+    },
+  },
+
   created() {
     this.browserProxy_ = NearbyPresenceBrowserProxy.getInstance();
   },
@@ -35,6 +45,8 @@ Polymer({
    */
   attached() {
     this.browserProxy_.initialize();
+    this.addWebUIListener(
+        'presence-device-found', device => this.onPresenceDeviceFound_(device));
   },
 
   onStartScanClicked() {
@@ -47,5 +59,21 @@ Polymer({
 
   onFirstTimeFlowClicked() {
     this.browserProxy_.SendFirstTimeFlow();
+  },
+
+  onPresenceDeviceFound_(device) {
+    const type = device['type'];
+    const endpointId = device['endpoint_id'];
+
+    // If there is not a device with this endpoint_id currently in the devices
+    // list, add it.
+    if (!this.npDiscoveredDevicesList_.find(
+            list_device => list_device.endpoint_id === endpointId)) {
+      this.unshift('npDiscoveredDevicesList_', {
+        'connectable': true,
+        'type': type,
+        'endpoint_id': endpointId,
+      });
+    }
   },
 });
