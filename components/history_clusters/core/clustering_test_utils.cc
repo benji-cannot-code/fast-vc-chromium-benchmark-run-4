4483ae1c943f24ecfc1363724bbbc5340a0f2763
Ravi Yanamala <raviyanamala@chromium.org>
2023-06-12 22:03:17 +0000

    Update GetClustersForUI to filter visits in persisted clusters using clusters from command line override.
    Bug: b/265209450
    Change-Id: I80dc0a20e5c1d72bee3b774530473182ea78b855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598330
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Ravi Yanamala <raviyanamala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156489}

--- components/history_clusters/core/clustering_test_utils.cc
+++ components/history_clusters/core/clustering_test_utils.cc
@@ -4,11 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/history_clusters/core/clustering_test_utils.h"
+#include <vector>
 
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/history/core/browser/history_types.h"
 #include "components/history_clusters/core/history_clusters_util.h"
 
 namespace history_clusters::testing {
@@ -114,6 +116,13 @@ history::DuplicateClusterVisit ClusterVisitToDuplicateClusterVisit(
   return {cluster_visit.annotated_visit.visit_row.visit_id,
           cluster_visit.annotated_visit.url_row.url(),
           cluster_visit.annotated_visit.visit_row.visit_time};
+}
+
+history::Cluster CreateCluster(
+    std::vector<history::ClusterVisit>& cluster_visits) {
+  history::Cluster cluster;
+  cluster.visits = cluster_visits;
+  return cluster;
 }
 
 }  // namespace history_clusters::testing
