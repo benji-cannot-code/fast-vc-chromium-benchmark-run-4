536d2f4614b0cf8af8a884ac7a7c60a35d72fb52
Victor Tan <victortan@chromium.org>
2023-06-12 22:04:48 +0000

    Implement WebView client-side feature overrides
    Currently, overriding a feature are implemented as a map of feature name and override states and this map is generated using map emplace. It results in the first override wins when there are a couple of mechanisms to override a feature.
    
    There are three ways to override a feature in order: 1) command-line override -> 2) Finch override -> 3) default override.
    
    Android WebView modifies the command line flags during startup when it needs overrides the feature which is different from Android Chrome. This leads those features can't be overridden by Finch. In other words, it's hard to experiment those feature on Android WebView.
    
    This CL implements a new API in PlatformFieldTrials, which allows platform to add extra overrides between Finch overrides and default override.
    
    This CL also move the first feature user case UserAgentClientHint into the new implemented API. We can migrate all similar features to the new API on Android  WebView once we verify this new mechanism.
    
    Bug: 1453402
    Change-Id: Id055dda3053a82bc94cdbe462de7d66d18f4631f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582427
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156492}

--- android_webview/javatests/src/org/chromium/android_webview/test/ClientHintsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ClientHintsTest.java
@@ -23,7 +23,6 @@ import org.junit.runner.RunWith;
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.AwCookieManager;
 import org.chromium.android_webview.AwSettings;
-import org.chromium.android_webview.common.AwSwitches;
 import org.chromium.android_webview.test.util.CookieUtils;
 import org.chromium.android_webview.test.util.JSUtils;
 import org.chromium.base.test.util.CallbackHelper;
@@ -170,7 +169,7 @@ public class ClientHintsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
-    @CommandLineFlags.Add({AwSwitches.WEBVIEW_ENABLE_USER_AGENT_CLIENT_HINTS,
+    @CommandLineFlags.Add({"enable-features=UserAgentClientHint",
             ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1"})
     public void
     testEnableUserAgentClientHintsNoCustom() throws Throwable {
@@ -188,7 +187,7 @@ public class ClientHintsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
-    @CommandLineFlags.Add({AwSwitches.WEBVIEW_ENABLE_USER_AGENT_CLIENT_HINTS,
+    @CommandLineFlags.Add({"enable-features=UserAgentClientHint",
             ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1"})
     public void
     testEnableUserAgentClientHintsCustomOverride() throws Throwable {
@@ -205,7 +204,7 @@ public class ClientHintsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
-    @CommandLineFlags.Add({AwSwitches.WEBVIEW_ENABLE_USER_AGENT_CLIENT_HINTS,
+    @CommandLineFlags.Add({"enable-features=UserAgentClientHint",
             ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1"})
     public void
     testEnableUserAgentClientHintsModifyDefaultUserAgent() throws Throwable {
@@ -245,7 +244,7 @@ public class ClientHintsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Preferences"})
-    @CommandLineFlags.Add({AwSwitches.WEBVIEW_ENABLE_USER_AGENT_CLIENT_HINTS,
+    @CommandLineFlags.Add({"enable-features=UserAgentClientHint",
             ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1"})
     public void
     testEnableUserAgentClientHintsJavaScript() throws Throwable {
@@ -255,7 +254,7 @@ public class ClientHintsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Preferences"})
-    @CommandLineFlags.Add({AwSwitches.WEBVIEW_ENABLE_USER_AGENT_CLIENT_HINTS,
+    @CommandLineFlags.Add({"enable-features=UserAgentClientHint",
             ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1"})
     public void
     testEnableUserAgentClientHintsOverrideJavaScript() throws Throwable {
