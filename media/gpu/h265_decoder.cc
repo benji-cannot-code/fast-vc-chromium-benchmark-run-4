1efef93e77c94976d74e4b4c995c62558b1f96b2
Saifuddin Hitawala <hitawala@chromium.org>
2023-06-12 19:38:40 +0000

    Multiplanar: Perform Flush for H264/265 decoders when color space change
    H264Decoder on ConfigChange calls a Flush() which outputs remaining
    pictures and clears picture contents. We were missing similar call
    when triggering a ColorSpaceChange which clears all picture buffers in
    D3D11VideoDecoder but since the picture buffer can be stored within the
    D3D11H264Picture it was leading to a use-after-free issue. This Flush()
    call should clear the underlying pictures used by the decoder.
    
    The change also addresses similar issue for H265Decoder.
    
    Bug: 1451087
    Change-Id: I5974971c6c69c2f2a3ff2921285b75f37327b059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604462
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156370}

--- media/gpu/h265_decoder.cc
+++ media/gpu/h265_decoder.cc
@@ -539,6 +539,9 @@ bool H265Decoder::ProcessPPS(int pps_id,
 
   if (new_color_space.IsSpecified() &&
       new_color_space != picture_color_space_) {
+    if (!Flush()) {
+      return false;
+    }
     DVLOG(1) << "Picture color space: " << new_color_space.ToString();
     picture_color_space_ = new_color_space;
     *color_space_changed = true;
