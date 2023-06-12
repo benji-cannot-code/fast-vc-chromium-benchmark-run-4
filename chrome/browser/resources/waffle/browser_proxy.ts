e668d87a808f50c4f67e10757d7e431c10626181
Jack Yammine <jyammine@google.com>
2023-06-12 13:16:38 +0000

    [waffle]Implement the UI for the waffle list
    Implement the Waffle list UI.
    
    Make the waffle list populate dynamically in the UI so that we don't
    need to update the latter every time we add an item to the list.
    
    Screenshot: http://b/280753754#comment15
    Bug: b/280753754
    Change-Id: Icf35def3096dc19165b4eb5eddc39cb48cc6a27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4603790
    Reviewed-by: Jens Mueller <muellerj@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156154}

--- chrome/browser/resources/waffle/browser_proxy.ts
+++ chrome/browser/resources/waffle/browser_proxy.ts
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {PageHandlerFactory, PageHandlerInterface, PageHandlerRemote} from './waffle.mojom-webui.js';
 
+export interface WaffleChoice {
+  name: string;
+}
+
 export class WaffleBrowserProxy {
   handler: PageHandlerInterface;
 
