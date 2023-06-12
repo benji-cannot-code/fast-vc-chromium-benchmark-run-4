671912b333cea6822359b58366ba68d44e5a33fe
Russ Hamilton <behamilton@google.com>
2023-06-12 17:09:46 +0000

    Add check to ensure k-anonymity is not enabled on Child profiles
    This should be guaranteed by other parts of the system, but having a
    check here helps to make sure.
    
    Bug: 1454043
    Change-Id: Ia16f1aed933dd64456c77da961cb95b488f12848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605345
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156259}

--- chrome/browser/k_anonymity_service/k_anonymity_service_factory.cc
+++ chrome/browser/k_anonymity_service/k_anonymity_service_factory.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/k_anonymity_service/k_anonymity_service_factory.h"
+#include <cstddef>
 
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
@@ -60,5 +61,9 @@ KAnonymityServiceFactory::~KAnonymityServiceFactory() = default;
 // BrowserContextKeyedServiceFactory:
 KeyedService* KAnonymityServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
+  Profile* profile = Profile::FromBrowserContext(context);
+  if (!profile || profile->IsChild()) {
+    return nullptr;
+  }
   return new KAnonymityServiceClient(Profile::FromBrowserContext(context));
 }
