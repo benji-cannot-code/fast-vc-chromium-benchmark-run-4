01696b444575e2ffca48757e75deff80514296dc
Greg Thompson <grt@chromium.org>
2023-06-12 12:55:27 +0000

    [a11y] Move test helpers out of the product
    ax_platform_node_test_helper and ax_inspect_test_helper belong in
    //ui/accessibility:test_support rather than in the product's a11y code.
    
    Bug: none
    Change-Id: Ia39bea184ef47615776e31acbc4d947f18dfc192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604753
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156146}

--- ui/accessibility/platform/ax_platform_node_test_helper.h
+++ ui/accessibility/platform/ax_platform_node_test_helper.h
@@ -8,13 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/component_export.h"
 #include "ui/accessibility/platform/ax_platform_node.h"
 #include "ui/accessibility/platform/ax_platform_node_delegate.h"
 
 namespace ui {
 
-class COMPONENT_EXPORT(AX_PLATFORM) AXPlatformNodeTestHelper {
+class AXPlatformNodeTestHelper {
  public:
   static int GetTreeSize(AXPlatformNode* ax_node);
   static AXPlatformNode* FindChildByName(AXPlatformNode* ax_node,
