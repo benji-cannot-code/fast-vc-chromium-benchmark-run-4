40f331cfa5d513ace6c2598bf6d4f433b94662c2
Louis Romero <lpromero@google.com>
2023-06-12 13:28:20 +0000

    Use snapshot ID instead of stable identifier for pinned snapshots
    SnapshotCache is using snapshot IDs since crrev/c/4396359 to identify
    snapshots, so the previous code was not pinning snapshots any more.
    
    Fixed: 1454008
    Change-Id: I4a32df828037df713b323d7de9f921ec13ebac26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4607444
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156157}

--- ios/chrome/browser/snapshots/snapshot_cache_web_state_list_observer.mm
+++ ios/chrome/browser/snapshots/snapshot_cache_web_state_list_observer.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/snapshots/snapshot_cache.h"
+#import "ios/chrome/browser/snapshots/snapshot_tab_helper.h"
 #import "ios/web/public/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -35,12 +36,12 @@ void SnapshotCacheWebStateListObserver::WebStateActivatedAt(
   NSMutableSet<NSString*>* set = [NSMutableSet set];
   if (active_index > 0) {
     web::WebState* web_state = web_state_list->GetWebStateAt(active_index - 1);
-    [set addObject:web_state->GetStableIdentifier()];
+    [set addObject:SnapshotTabHelper::FromWebState(web_state)->GetSnapshotID()];
   }
 
   if (active_index + 1 < web_state_list->count()) {
     web::WebState* web_state = web_state_list->GetWebStateAt(active_index + 1);
-    [set addObject:web_state->GetStableIdentifier()];
+    [set addObject:SnapshotTabHelper::FromWebState(web_state)->GetSnapshotID()];
   }
 
   snapshot_cache_.pinnedSnapshotIDs = [set copy];
