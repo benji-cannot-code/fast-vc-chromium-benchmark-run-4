62d14097e2443a4f7f891d39ee1ceb5eb1bc1f4d
mikt <mikt@google.com>
2023-06-12 03:30:51 +0000

    [BRP/clang-plugin] Remove some raw-ptr-exclude-path args as unneeded
    - Remove `base/no_destructor.h`
      - Now this is excluded in `RawPtrManualPathsToIgnore.h`
    - Remove `base/containers/span.h`
      - Add `RAW_PTR_EXCLUSION` in this CL
    
    Change-Id: Ibbe097b82f22c2e37341217217809bb97d3c2e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600529
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156049}

--- base/containers/span.h
+++ base/containers/span.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/checked_iterators.h"
 #include "base/containers/contiguous_iterator.h"
 #include "base/cxx20_to_address.h"
+#include "base/memory/raw_ptr_exclusion.h"
 #include "base/numerics/safe_math.h"
 
 namespace base {
@@ -453,7 +454,9 @@ class GSL_POINTER span : public internal::ExtentStorage<Extent> {
   }
 
  private:
-  T* data_;
+  // This field is not a raw_ptr<> because it was filtered by the rewriter
+  // for: #constexpr-ctor-field-initializer, #global-scope, #union
+  RAW_PTR_EXCLUSION T* data_;
 };
 
 // span<T, Extent>::extent can not be declared inline prior to C++17, hence this
