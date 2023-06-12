f243404062286eab3fa94faca60585c0958fb764
Maksim Moskvitin <mmoskvitin@google.com>
2023-06-12 14:43:43 +0000

    [TrustedVault] Avoid nullable TrustedVaultClient in SyncServiceCrypto
    The only remaining place where null TrustedVaultClient was passed to
    SyncServiceCrypto was sync_user_settings_impl_unittest.cc.
    This CL makes SyncServiceCrypto assuming TrustedVaultClient is never
    null, sync_user_settings_impl_unittest.cc now passes trivial
    FakeTrustedVaultClient.
    
    Bug: 1434661
    Change-Id: I859ddeaa0d4ea2fa412c6f95a03b21062c6a62d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4603937
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156191}

--- components/sync/service/sync_user_settings_impl_unittest.cc
+++ components/sync/service/sync_user_settings_impl_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/engine/configure_reason.h"
 #include "components/sync/service/sync_service_crypto.h"
+#include "components/sync/test/mock_trusted_vault_client.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -65,9 +66,8 @@ class SyncUserSettingsImplTest : public testing::Test {
     SyncPrefs::RegisterProfilePrefs(pref_service_.registry());
     sync_prefs_ = std::make_unique<SyncPrefs>(&pref_service_);
 
-    sync_service_crypto_ =
-        std::make_unique<SyncServiceCrypto>(&sync_service_crypto_delegate_,
-                                            /*trusted_vault_client=*/nullptr);
+    sync_service_crypto_ = std::make_unique<SyncServiceCrypto>(
+        &sync_service_crypto_delegate_, &trusted_vault_client_);
   }
 
   std::unique_ptr<SyncUserSettingsImpl> MakeSyncUserSettings(
@@ -89,6 +89,7 @@ class SyncUserSettingsImplTest : public testing::Test {
   std::unique_ptr<SyncPrefs> sync_prefs_;
   testing::NiceMock<MockSyncServiceCryptoDelegate>
       sync_service_crypto_delegate_;
+  testing::NiceMock<MockTrustedVaultClient> trusted_vault_client_;
   std::unique_ptr<SyncServiceCrypto> sync_service_crypto_;
 };
 
