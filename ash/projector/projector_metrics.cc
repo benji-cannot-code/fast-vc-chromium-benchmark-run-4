c21512a81e424d842d0d7ef235dc1c17861a218f
Li Lin <llin@chromium.org>
2023-06-12 19:23:31 +0000

    Remove policy change handling error metrics and convert to CHECK.
    PROJECTOR_METRICS_TRACKER_UPDATED
    
    Bug: 240497023
    Change-Id: I47a3bd3fb7f584945bafcd18c7b620babf49ba56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599276
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156356}

--- ash/projector/projector_metrics.cc
+++ ash/projector/projector_metrics.cc
@@ -35,9 +35,6 @@ constexpr char kProjectorPendingScreencastBatchIOTaskDurationHistogramName[] =
 constexpr char kProjectorPendingScreencastChangeIntervalHistogramName[] =
     "Ash.Projector.PendingScreencastChangeInterval";
 
-constexpr char kProjectorPolicyChangeHandlingErrorHistogramName[] =
-    "Ash.Projector.PolicyChangeHandlingError";
-
 constexpr char
     kProjectorOnDeviceToServerSpeechRecognitionFallbackReasonHistogramName[] =
         "Ash.Projector.OnDeviceToServerSpeechRecognitionFallbackReason";
@@ -103,13 +100,6 @@ void RecordCreationFlowError(int message_id) {
   }
   base::UmaHistogramEnumeration(
       GetHistogramName(kProjectorCreationFlowErrorHistogramName), error);
-}
-
-ASH_EXPORT void RecordPolicyChangeHandlingError(
-    ProjectorPolicyChangeHandlingError error) {
-  base::UmaHistogramEnumeration(
-      GetHistogramName(kProjectorPolicyChangeHandlingErrorHistogramName),
-      error);
 }
 
 ASH_EXPORT void RecordPendingScreencastBatchIOTaskDuration(
