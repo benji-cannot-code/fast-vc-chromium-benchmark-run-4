e0580f4b7c6cfd84895a32a249b36e3663783482
Khalid Peer <khalidpeer@chromium.org>
2023-06-12 21:36:10 +0000

    [omnibox][CR23] Update starter pack icons per ChromeRefresh2023 spec.
    In order to align with the latest ChromeRefresh2023 guidance, this CL
    contains the following changes:
    
      1) Update color used by starter pack icons (light/dark theme-reactive)
      2) Replace favicon usage with vector icon usage (for icon recoloring)
      3) Fix inconsistency with @tabs icon in query row vs omnibox popup
      4) Update actual starter pack icons to use latest CR23 icons
    
    BEFORE/AFTER screenshots: https://drive.google.com/drive/folders/1Ea6q1laa8M9jQB1dw-enMpOVnge1pNrL?resourcekey=0-_jb4x7w2aG5inZgiAsoF7A&usp=share_link
    
    Bug: 1431337
    Change-Id: I5019ad7826ca413f7fa97b397784cf9095c2903d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583387
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156464}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/search_engines/search_engine_utils.h"
+#include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
 #include "inline_autocompletion_util.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
@@ -482,7 +483,8 @@ const gfx::VectorIcon& AutocompleteMatch::AnswerTypeToAnswerIcon(int type) {
 }
 
 const gfx::VectorIcon& AutocompleteMatch::GetVectorIcon(
-    bool is_bookmark) const {
+    bool is_bookmark,
+    const TemplateURL* turl) const {
   // TODO(https://crbug.com/1024114): Remove crash logging once fixed.
   SCOPED_CRASH_KEY_NUMBER("AutocompleteMatch", "type", type);
   SCOPED_CRASH_KEY_NUMBER("AutocompleteMatch", "provider_type",
@@ -581,6 +583,23 @@ const gfx::VectorIcon& AutocompleteMatch::GetVectorIcon(
                                       : omnibox::kJourneysIcon;
 
     case Type::STARTER_PACK:
+      if (turl) {
+        switch (turl->GetBuiltinEngineType()) {
+          case KEYWORD_MODE_STARTER_PACK_BOOKMARKS:
+            return use_chrome_refresh_icons
+                       ? omnibox::kStarActiveChromeRefreshIcon
+                       : omnibox::kStarActiveIcon;
+          case KEYWORD_MODE_STARTER_PACK_HISTORY:
+            return use_chrome_refresh_icons
+                       ? vector_icons::kHistoryChromeRefreshIcon
+                       : vector_icons::kHistoryIcon;
+          case KEYWORD_MODE_STARTER_PACK_TABS:
+            return use_chrome_refresh_icons ? omnibox::kProductChromeRefreshIcon
+                                            : omnibox::kProductIcon;
+          default:
+            break;
+        }
+      }
       return use_chrome_refresh_icons ? omnibox::kProductChromeRefreshIcon
                                       : omnibox::kProductIcon;
 
