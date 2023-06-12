fcd81b100b45f86ee466c948b77a9e621ee5c69c
Arthur Milchior <arthurmilchior@chromium.org>
2023-06-12 12:58:34 +0000

    [iOS] BookmarksEditorMediator dealloc checks disconnect was called
    Bug: None
    Change-Id: I2f2f5dae05041e12a6b325ea04337a168d9563dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604614
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156148}

--- ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_mediator.mm
+++ ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_mediator.mm
@@ -98,6 +98,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _browserState = nullptr;
 }
 
+- (void)dealloc {
+  DCHECK(!_profileBookmarkModel);
+}
+
 #pragma mark - Public
 
 - (void)manuallyChangeFolder:(const bookmarks::BookmarkNode*)folder {
--- ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_view_controller_unittest.mm
+++ ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_view_controller_unittest.mm
@@ -67,6 +67,7 @@ TEST_F(BookmarksEditorViewControllerTest, CanSyncBeforeLoad) {
                       browserState:nullptr];
   controller.mutator = mediator;
   [controller updateSync];
+  [mediator disconnect];
 }
 
 }  // namespace
