6ad0f9a62a6ebaf4fd5a0891eaff0a10198b498a
Luca Accorsi <accorsi@google.com>
2023-06-12 15:40:53 +0000

    dlp: Replace all DlpClient::AddFile with DlpClient::AddFiles
    Add multiple files at a time to minimize the number of DBUS calls.
    This is in preparation of subsequent CLs where this feature is actually
    needed.
    
    Bug: b/285868839
    Change-Id: Id3d71e8bd70803f883f89053b2ee7207aec7c94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4601480
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156218}

--- components/services/quarantine/quarantine_chromeos.cc
+++ components/services/quarantine/quarantine_chromeos.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace quarantine {
 
 void OnFileAdded(mojom::Quarantine::QuarantineFileCallback callback,
-                 const dlp::AddFileResponse response) {
+                 const dlp::AddFilesResponse response) {
   if (response.has_error_message()) {
     DVLOG(1) << "Failed to quarantine: " << response.error_message();
     std::move(callback).Run(QuarantineFileResult::ANNOTATION_FAILED);
@@ -34,11 +34,13 @@ void QuarantineFile(const base::FilePath& file,
     std::move(callback).Run(QuarantineFileResult::OK);
     return;
   }
-  dlp::AddFileRequest request;
-  request.set_file_path(file.value());
-  request.set_source_url(source_url_unsafe.spec());
-  request.set_referrer_url(referrer_url_unsafe.spec());
-  chromeos::DlpClient::Get()->AddFile(
+
+  ::dlp::AddFilesRequest request;
+  ::dlp::AddFileRequest* add_request = request.add_add_file_requests();
+  add_request->set_file_path(file.value());
+  add_request->set_source_url(source_url_unsafe.spec());
+  add_request->set_referrer_url(referrer_url_unsafe.spec());
+  chromeos::DlpClient::Get()->AddFiles(
       request, base::BindOnce(&OnFileAdded, std::move(callback)));
 }
 
