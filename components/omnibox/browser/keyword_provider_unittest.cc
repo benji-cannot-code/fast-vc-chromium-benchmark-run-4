35af968d8ace3c7951b07dc47c3bc5d4614b2487
Tom Sepez <tsepez@chromium.org>
2023-06-12 17:00:10 +0000

    Avoid dangling ptr warning in KeywordProviderTest::TearDown()
    Re-order members so that the KeywordProvider does not outlive the
    MockAutocompleteProviderClient to which it has an unowned reference.
    Then remove explicit TearDown() and allow members to be destroyed
    in the inverse order of their appearance. The use of ref-counting
    complicates the matter, but is correct so long as there is no other
    scoped_refptr<> in existence.
    
    Bug: 1401495
    Change-Id: I2e63b96ad188f1846d5d51073510eccff7d4c109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577499
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156250}

--- components/omnibox/browser/keyword_provider_unittest.cc
+++ components/omnibox/browser/keyword_provider_unittest.cc
@@ -75,8 +75,6 @@ class KeywordProviderTest : public testing::Test {
   // related internal variables here.
   void SetUpClientAndKeywordProvider();
 
-  void TearDown() override;
-
   template<class ResultType>
   void RunTest(TestData<ResultType>* keyword_cases,
                int num_cases,
@@ -86,8 +84,8 @@ class KeywordProviderTest : public testing::Test {
   static const TemplateURLService::Initializer kTestData[];
 
   base::test::TaskEnvironment task_environment_;
-  scoped_refptr<KeywordProvider> kw_provider_;
   std::unique_ptr<MockAutocompleteProviderClient> client_;
+  scoped_refptr<KeywordProvider> kw_provider_;
 };
 
 // static
@@ -132,11 +130,6 @@ void KeywordProviderTest::SetUpClientAndKeywordProvider() {
   client_->set_template_url_service(
       std::make_unique<TemplateURLService>(kTestData, std::size(kTestData)));
   kw_provider_ = new KeywordProvider(client_.get(), nullptr);
-}
-
-void KeywordProviderTest::TearDown() {
-  client_.reset();
-  kw_provider_ = nullptr;
 }
 
 template<class ResultType>
