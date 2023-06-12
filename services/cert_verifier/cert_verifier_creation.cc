61abab2237ffc56bf9baa607416c6644058ea57a
Matt Mueller <mattm@chromium.org>
2023-06-12 21:16:47 +0000

    remove unused net/cert_net/cert_net_fetcher_url_request.h include
    services/cert_verifier/cert_verifier_creation.cc only uses
    CertNetFetcher through the abstract interface.
    
    Change-Id: I1a22004776ec0c36fafdc6b7fd6aec2603836506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608818
    Reviewed-by: Hubert Chao <hchao@google.com>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Hubert Chao <hchao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156444}

--- services/cert_verifier/cert_verifier_creation.cc
+++ services/cert_verifier/cert_verifier_creation.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_verify_proc.h"
 #include "net/cert/crl_set.h"
 #include "net/cert/multi_threaded_cert_verifier.h"
-#include "net/cert_net/cert_net_fetcher_url_request.h"
 #include "net/net_buildflags.h"
 
 #if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
