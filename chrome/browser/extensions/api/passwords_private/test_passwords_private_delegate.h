9625ad5bce7a7ca24e2b022e8e0cf752d26316e5
Nina Satragno <nsatragno@chromium.org>
2023-06-12 23:39:12 +0000

    [password manager] Wire editing passkeys
    Update SavedPasswordsPresenter::EditSavedCredentials to support editing
    passkeys. Surface this through a new `changeCredential` method on the
    passwordsPrivate extension API. This generic method can update all of
    password, federated, blocked, and passkey credentials.
    
    A follow-up will remove the old `changeSavedPassword` passwordsPrivate
    method which is now redundant.
    
    Bug: 1432717
    Change-Id: I6bbd4be42fbe9d944ae403c67145c079ca429d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4585682
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156555}

--- chrome/browser/extensions/api/passwords_private/test_passwords_private_delegate.h
+++ chrome/browser/extensions/api/passwords_private/test_passwords_private_delegate.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/extensions/api/passwords_private/passwords_private_delegate.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/extensions/api/passwords_private.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace extensions {
@@ -45,6 +46,8 @@ class TestPasswordsPrivateDelegate : public PasswordsPrivateDelegate {
   absl::optional<int> ChangeSavedPassword(
       const int id,
       const api::passwords_private::ChangeSavedPasswordParams& params) override;
+  bool ChangeCredential(
+      const api::passwords_private::PasswordUiEntry& credential) override;
   void RemoveCredential(
       int id,
       api::passwords_private::PasswordStoreSet from_store) override;
