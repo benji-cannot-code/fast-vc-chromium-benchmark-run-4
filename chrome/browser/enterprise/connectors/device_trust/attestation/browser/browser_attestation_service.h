23da0df0f80ce42214ca027fab4b8588c690bbc8
Sebastien Lalancette <seblalancette@chromium.org>
2023-06-12 13:48:25 +0000

    [DTC] Fix UAF Issue In BrowserAttestationService Using a Unique Ptr
    The root issue is that the KeyInfo variable was passed as std::ref to
    the barrier closure's callback, while in theory it needed to pass over
    ownership. Using a unique_ptr to handle the ownership of that variable
    makes things very clear and will take care of that UAF issue.
    
    Fixed: 1453608
    Change-Id: Icf45313ad0ad7e14135f6359669501e1cf677572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605156
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Leon Masopust <lmasopust@google.com>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Auto-Submit: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156165}

--- chrome/browser/enterprise/connectors/device_trust/attestation/browser/browser_attestation_service.h
+++ chrome/browser/enterprise/connectors/device_trust/attestation/browser/browser_attestation_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_ATTESTATION_BROWSER_BROWSER_ATTESTATION_SERVICE_H_
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_ATTESTATION_BROWSER_BROWSER_ATTESTATION_SERVICE_H_
 
+#include <memory>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
@@ -17,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/device_trust/attestation/browser/google_keys.h"
 #include "chrome/browser/enterprise/connectors/device_trust/attestation/common/attestation_service.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+
+struct KeyInfo;
 
 namespace enterprise_connectors {
 
@@ -49,7 +52,7 @@ class BrowserAttestationService : public AttestationService {
   void OnKeyInfoDecorated(const SignedData& signed_data,
                           const std::set<DTCPolicyLevel>& levels,
                           AttestationCallback callback,
-                          KeyInfo& key_info);
+                          std::unique_ptr<KeyInfo> key_info);
 
   void OnResponseCreated(const std::set<DTCPolicyLevel>& levels,
                          AttestationCallback callback,
