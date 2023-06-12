742a58609d029006df0fb017d6d3151f5bc1813f
Yunke Zhou <yunkez@google.com>
2023-06-12 14:51:12 +0000

    Fix the crash when reaching GaiaPasswordChangedScreen after fatal error
    Bug: b:285923493
    Change-Id: I42b347c09356eff91162557d2b552eb1e24feed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608262
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156193}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -383,7 +383,8 @@ bool IsGaiaPageDefaultsToSAML() {
 
 bool IsContextNeededForScreen(OobeScreenId screen_id) {
   return screen_id == SamlConfirmPasswordView::kScreenId ||
-         screen_id == CryptohomeRecoveryScreenView::kScreenId;
+         screen_id == CryptohomeRecoveryScreenView::kScreenId ||
+         screen_id == GaiaPasswordChangedView::kScreenId;
 }
 
 }  // namespace
