b1cbbffa1ff710acf6e4c90820ca945ac952bcce
Avi Drissman <avi@chromium.org>
2023-06-12 16:43:01 +0000

    Remove scoped_nsobject from files already ARCed.
    Bug: 1280317
    Change-Id: Iefc0f541d5b880bdc63a8316b70b2c5308aff6f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605113
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156246}

--- content/common/mac/attributed_string_type_converters.mm
+++ content/common/mac/attributed_string_type_converters.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/bridging.h"
 #include "base/check.h"
 #include "base/mac/foundation_util.h"
-#include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/public/common/common_param_traits.h"
--- ui/gfx/image/image_ios.mm
+++ ui/gfx/image/image_ios.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 
 #include "base/logging.h"
-#include "base/mac/scoped_nsobject.h"
 #include "ui/base/resource/resource_scale_factor.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image_internal.h"
