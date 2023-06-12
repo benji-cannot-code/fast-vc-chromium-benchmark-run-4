063a744649f01892d7b5767a03732dfa251160c8
Katie Dektar <katie@chromium.org>
2023-06-12 20:51:29 +0000

    [Color correction] Remove settings not planned for launch
    This change removes unused color correction settings that will
    not be part of the final feature and combines greyscale with
    color filter options per mocks.
    
    Color correction is behind a flag while it is being developed,
    so removing these prefs shouldn't cause any issues as they were
    only available with the flag.
    
    Design:go/cros-color-filtering
    
    Test: Existing, manual
    AX-Relnotes: N/A
    Bug: b/259372272
    Change-Id: I1fcffab3d4b6e1bff80bd4f2be5f5c529cf30c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605373
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156418}

--- ash/accessibility/accessibility_controller_unittest.cc
+++ ash/accessibility/accessibility_controller_unittest.cc
@@ -62,7 +62,8 @@ class AccessibilityControllerTest : public AshTestBase {
   void SetUp() override {
     scoped_feature_list_.InitWithFeatures(
         {media::kLiveCaption, media::kLiveCaptionSystemWideOnChromeOS,
-         ash::features::kOnDeviceSpeechRecognition},
+         ash::features::kOnDeviceSpeechRecognition,
+         ::features::kExperimentalAccessibilityColorEnhancementSettings},
         {});
     AshTestBase::SetUp();
   }
@@ -178,9 +179,11 @@ TEST_F(AccessibilityControllerTest, PrefsAreRegistered) {
       prefs::kAccessibilityEnhancedNetworkVoicesInSelectToSpeakAllowed));
   if (::features::
           AreExperimentalAccessibilityColorEnhancementSettingsEnabled()) {
-    EXPECT_TRUE(prefs->FindPreference(prefs::kAccessibilitySepiaAmount));
-    EXPECT_TRUE(prefs->FindPreference(prefs::kAccessibilityHueRotationAmount));
-    EXPECT_TRUE(prefs->FindPreference(prefs::kAccessibilityGreyscaleAmount));
+    EXPECT_TRUE(prefs->FindPreference(prefs::kAccessibilityColorFiltering));
+    EXPECT_TRUE(
+        prefs->FindPreference(prefs::kAccessibilityColorVisionDeficiencyType));
+    EXPECT_TRUE(prefs->FindPreference(
+        prefs::kAccessibilityColorVisionCorrectionAmount));
   }
 }
 
