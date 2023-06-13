7f062ed8c65ba7c600c4cf69dd834ffaadd963c3
Emilia Paz <emiliapaz@chromium.org>
2023-06-13 00:54:49 +0000

    [Extensions] Add extensions to request access button tooltip
    Display the extensions requesting site access in the request access
    button tooltip instead of a hover card. This change is after a study
    where the users were constantly trying to click on the hover card.
    
    Leaving the hover card coordinator and bubble for now, since we are
    testing the tooltip.
    
    Screenshot:
    https://drive.google.com/file/d/1wicsoVb5RkvJQoA51w9y4A9L94c1uDeP/view?usp=sharing
    
    Bug: 1239772
    Change-Id: I24bfb544c9fa9ce01e74191a77ab00edece57847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4607544
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156602}

--- chrome/browser/ui/views/extensions/extensions_toolbar_controls_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_controls_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/extensions/extensions_toolbar_controls.h"
 
+#include "base/strings/strcat.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "chrome/browser/extensions/extension_action_runner.h"
 #include "chrome/browser/extensions/extension_context_menu_model.h"
@@ -231,13 +232,16 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
 
   // Add an extension that only requests access to a.com, and
   // withhold host permissions.
-  auto extension_a =
-      InstallExtensionWithHostPermissions("Extension A", {url_a.spec()});
-  WithholdHostPermissions(extension_a.get());
+  auto extension =
+      InstallExtensionWithHostPermissions("Extension", {url_a.spec()});
+  WithholdHostPermissions(extension.get());
   EXPECT_TRUE(IsRequestAccessButtonVisible());
   EXPECT_EQ(
       request_access_button()->GetText(),
       l10n_util::GetStringFUTF16Int(IDS_EXTENSIONS_REQUEST_ACCESS_BUTTON, 1));
+  std::u16string tooltip = base::UTF8ToUTF16(
+      base::StrCat({"Click to allow on a.com:\n", extension->name()}));
+  EXPECT_EQ(request_access_button()->GetTooltipText(gfx::Point()), tooltip);
 
   // Add an extension with all urls host permissions, and withhold host
   // permissions.
@@ -248,6 +252,10 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
   EXPECT_EQ(
       request_access_button()->GetText(),
       l10n_util::GetStringFUTF16Int(IDS_EXTENSIONS_REQUEST_ACCESS_BUTTON, 2));
+  tooltip = base::UTF8ToUTF16(
+      base::StrCat({"Click to allow on a.com:\n", extension->name(), "\n",
+                    extension_all_urls->name()}));
+  EXPECT_EQ(request_access_button()->GetTooltipText(gfx::Point()), tooltip);
 
   // Navigate to a different url. Only "all_urls" should request access.
   NavigateAndCommit(url_b);
@@ -255,6 +263,9 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
   EXPECT_EQ(
       request_access_button()->GetText(),
       l10n_util::GetStringFUTF16Int(IDS_EXTENSIONS_REQUEST_ACCESS_BUTTON, 1));
+  tooltip = base::UTF8ToUTF16(
+      base::StrCat({"Click to allow on b.com:\n", extension_all_urls->name()}));
+  EXPECT_EQ(request_access_button()->GetTooltipText(gfx::Point()), tooltip);
 
   // Remove the only extension that requests access to the current site.
   UninstallExtension(extension_all_urls->id());
