c41f90bc250eec5d14c285074ada31a68aef658a
Christoph Schwering <schwering@google.com>
2023-06-13 17:49:19 +0000

    [Autofill] Ignore main-origin number, CVC if they're also in sub-frames
    Suppose there are two CREDIT_CARD_NUMBER, one of which has the main
    frame's origin and one is in a cross-origin sub-frame. Then the former
    is likely misclassified and should not be filled. This CL overrides the
    former CREDIT_CARD_NUMBER with UNKNOWN_TYPE.
    
    Analogously for CREDIT_CARD_VERIFICATION_CODE.
    
    The effects of the rationalization has been validated manually:
    go/autofill-multi-origin-numbers
    
    Bug: 1450502
    Change-Id: Ia811014c699e2ef240ce8debcb58cf44c970717b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4597895
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157044}

--- components/autofill/core/browser/form_structure_rationalizer.h
+++ components/autofill/core/browser/form_structure_rationalizer.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/common/form_field_data.h"
+#include "url/origin.h"
 
 namespace autofill {
 
@@ -46,7 +47,8 @@ class FormStructureRationalizer {
 
   // A helper function to review the predictions and do appropriate adjustments
   // when it considers necessary.
-  void RationalizeFieldTypePredictions(LogManager* log_manager);
+  void RationalizeFieldTypePredictions(const url::Origin& main_origin,
+                                       LogManager* log_manager);
 
   // Ensures that only a single phone number (which can be split across multiple
   // fields) is autofilled in the `section`. If the section contains multiple
@@ -67,6 +69,12 @@ class FormStructureRationalizer {
   // card fields in an otherwise non-credit-card related form is unlikely to be
   // correct, the function will override that prediction.
   void RationalizeCreditCardFieldPredictions(LogManager* log_manager);
+
+  // Eradicates the type of credit card number and CVC fields on the main
+  // frame's origin if there are also fields of this type in a child frame.
+  // See crbug.com/1450502 for details.
+  void RationalizeMultiOriginCreditCardFields(const url::Origin& main_origin,
+                                              LogManager* log_manager);
 
   // Sets the offsets of adjacent credit card number fields. For example:
   // four adjacent card fields with `FormFieldData::max_length == 4` should
