449d12e80303694f0a8c6ee00e9548c4587f9432
Jayee <jayee@google.com>
2023-06-13 17:56:23 +0000

    Add LocalTestPolicyProvider
    Added files for LocalTestPolicyProvider.
    
    First CL out of a series of CLs to add a LocalTestPolicyProvider for the chrome://policy/test page.
    
    Bug: b:284460528
    Change-Id: Ia073ee19297eef2c0d612fa601bbc66b9d4c3f8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594100
    Commit-Queue: Jayee Zheng <jayee@google.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157050}

---
+++ components/policy/core/common/policy_loader_local_test.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_LOCAL_TEST_H_
+#define COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_LOCAL_TEST_H_
+
+#include "components/policy/policy_export.h"
+
+namespace policy {
+
+class PolicyBundle;
+
+// Loads policies from policy testing page
+class POLICY_EXPORT PolicyLoaderLocalTest {
+ public:
+  explicit PolicyLoaderLocalTest();
+
+  ~PolicyLoaderLocalTest();
+
+  PolicyBundle Load();
+};
+
+}  // namespace policy
+
+#endif  // COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_LOCAL_TEST_H_
