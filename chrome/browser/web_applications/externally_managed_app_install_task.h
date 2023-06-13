757d8698177127e6c0b530768efddc43dd000b3c
Glen Robertson <glenrob@chromium.org>
2023-06-13 05:50:55 +0000

    CodeHealth: Replace RunLoop with TestFuture in externally_managed_app_install_task_unittest.cc
    Also fixup includes using IWYU.
    No functional changes.
    
    Change-Id: Ibdc1945ff63f993fce5a8df5675d993c48a18424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608223
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156725}

--- chrome/browser/web_applications/externally_managed_app_install_task.h
+++ chrome/browser/web_applications/externally_managed_app_install_task.h
@@ -6,20 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_EXTERNALLY_MANAGED_APP_INSTALL_TASK_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_EXTERNALLY_MANAGED_APP_INSTALL_TASK_H_
 
+#include <memory>
+
 #include "base/functional/callback.h"
-#include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/web_applications/external_install_options.h"
 #include "chrome/browser/web_applications/externally_managed_app_manager.h"
-#include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
+#include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_id.h"
-#include "chrome/browser/web_applications/web_app_install_info.h"
-#include "chrome/browser/web_applications/web_app_install_utils.h"
 #include "chrome/browser/web_applications/web_contents/web_app_url_loader.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 class Profile;
+class GURL;
 
 namespace content {
 class WebContents;
@@ -32,7 +32,6 @@ enum class UninstallResultCode;
 
 namespace web_app {
 
-class WebAppUrlLoader;
 class WebAppInstallFinalizer;
 class WebAppCommandScheduler;
 class WebAppUiManager;
