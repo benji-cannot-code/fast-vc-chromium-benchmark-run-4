00834ad83c7bfddbff428e90b44c611977335904
“Hailey <haileywang@google.com>
2023-06-13 22:37:56 +0000

    Make histogram trigger check enum id
    Bug: b/280493929
    Change-Id: Ida3095053865959c761732afee8e12446d57103f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599024
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157202}

--- components/segmentation_platform/internal/data_collection/training_data_collector_impl.cc
+++ components/segmentation_platform/internal/data_collection/training_data_collector_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/data_collection/training_data_collector_impl.h"
 #include <cstdint>
 
+#include "base/containers/contains.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/metrics/metrics_hashes.h"
@@ -209,8 +210,12 @@ void TrainingDataCollectorImpl::OnGetSegmentsInfoList(
               segment.first);
         } else if (feature.type() == proto::SignalType::HISTOGRAM_VALUE ||
                    feature.type() == proto::SignalType::HISTOGRAM_ENUM) {
+          std::vector<int> enum_ids;
+          for (int j = 0; j < feature.enum_ids_size(); j++) {
+            enum_ids.emplace_back(feature.enum_ids(j));
+          }
           immediate_trigger_histograms_[feature.name_hash()].emplace(
-              segment.first);
+              std::make_pair(segment.first, enum_ids));
         }
       }
     }
@@ -234,12 +239,20 @@ void TrainingDataCollectorImpl::OnHistogramSignalUpdated(
     param->output_metric_hash = hash;
     param->output_value = static_cast<float>(sample);
     for (auto segment : segments) {
-      // TODO (ritikagup@) : Add handling for default models, if required.
-      segment_info_database_->GetSegmentInfo(
-          segment, proto::ModelSource::SERVER_MODEL_SOURCE,
-          base::BindOnce(
-              &TrainingDataCollectorImpl::OnUmaUpdatedReportForSegmentInfo,
-              weak_ptr_factory_.GetWeakPtr(), param));
+      auto segment_id = segment.first;
+      auto accepted_enum_ids = segment.second;
+
+      // Process both enum histograms with their corresponding accepted enum ids
+      // and value histograms with no enum ids.
+      if (accepted_enum_ids.empty() ||
+          base::Contains(accepted_enum_ids, sample)) {
+        // TODO (ritikagup@) : Add handling for default models, if required.
+        segment_info_database_->GetSegmentInfo(
+            segment_id, proto::ModelSource::SERVER_MODEL_SOURCE,
+            base::BindOnce(
+                &TrainingDataCollectorImpl::OnUmaUpdatedReportForSegmentInfo,
+                weak_ptr_factory_.GetWeakPtr(), param));
+      }
     }
   }
 }
