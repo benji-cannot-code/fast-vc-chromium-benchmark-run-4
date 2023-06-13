c704006ee09f9aecc2659c0710a0921ec33388ae
Ritika Gupta <ritikagup@google.com>
2023-06-13 15:38:27 +0000

    [Info Bar] Re-enable testInfoBarForPopUp
    The test is now not showing flakiness and passing on TestReviver.
    
    Change-Id: I374077c945bff9685d99a4fa7e16712b8dad74a2
    Bug: 1269025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609449
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156945}

--- chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarTest.java
@@ -25,7 +25,6 @@ import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.browser.WebContentsFactory;
@@ -228,7 +227,6 @@ public class InfoBarTest {
     @Test
     @MediumTest
     @Feature({"Browser", "Main"})
-    @DisabledTest(message = "https://crbug.com/1269025")
     public void testInfoBarForPopUp() throws TimeoutException, ExecutionException {
         sActivityTestRule.loadUrl(sTestServer.getURL(POPUP_PAGE));
         mListener.addInfoBarAnimationFinished("InfoBar not added");
