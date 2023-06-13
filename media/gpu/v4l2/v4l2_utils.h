1e748adcb0f4fbd7bb57fd03f54b05705d0171a8
Miguel Casas <mcasas@chromium.org>
2023-06-13 18:41:25 +0000

    media/gpu/v4l2: Decouple V4L2Queue from V4L2Device
    This CL decouples V4L2Device from V4L2Queue by teaching V4L2Queue to
    use ioctl callbacks injected upon construction. This clarifies
    that V4L2Device uses (owns and constructs) V4L2Queues, but V4L2Queue
    should not know about V4L2Device (*).
    
    This decoupling allows V4L2StatefulVideoDecoder to use V4L2Queue
    without needing to go via V4L2Device (in a follow up CL).
    
    No new functionality, only moving code around.
    
    (*) Actually there's still two dangling calls to V4L2Device static
    methods from v4l2_queue,  namely GetNumPlanesOfV4L2PixFmt and
    V4L2FormatToVideoFrameLayout, will have to remove in a follow up
    CL). v4l2_queue::V4L2Buffer turned out to be the only caller of
    V4L2Device::GetDmabufsForV4L2Buffer() so moved to file-static.
    
    Bug: b:279980150
    Change-Id: I5aa9d5ff438da542522d0b45def0b5871151c3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568232
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157083}

--- media/gpu/v4l2/v4l2_utils.h
+++ media/gpu/v4l2/v4l2_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include <linux/videodev2.h>
+#include <sys/mman.h>
 
 #include "base/functional/callback.h"
 #include "media/base/video_codecs.h"
@@ -19,6 +20,13 @@ class Size;
 namespace media {
 
 using IoctlAsCallback = base::RepeatingCallback<int(int, void*)>;
+
+// Ideally this should be a decltype(mmap) (void *mmap(void *addr, size_t
+// length, int prot, int flags, int fd, off_t offset)), but the types of e.g.
+// V4L2Device::Mmap are wrong.
+// TODO(b/279980150): correct types and argument order and use decltype.
+using MmapAsCallback =
+    base::RepeatingCallback<void*(void*, unsigned int, int, int, unsigned int)>;
 
 // Returns a human readable description of |memory|.
 const char* V4L2MemoryToString(v4l2_memory memory);
