26b0523af9a1f01af47fec4b7164e239d41b1430
David Maunder <davidjm@google.com>
2023-06-13 16:38:42 +0000

    [Fixit] Fix testOMADownloadInterception
    The condition waiting for the URL to load compares a String with
    a GURL object which will always return false. This CL uses getSpec()
    so the String is being compared with a String.
    
    Bug: 1261941, 1261941
    Change-Id: Ia3fe1584b05bfac5cb8ed67b096d80de79b48d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609706
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156980}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTest.java
@@ -303,7 +303,6 @@ public class DownloadTest implements CustomMainActivityStart {
     @Test
     @MediumTest
     @Feature({"Navigation"})
-    @DisabledTest(message = "crbug.com/1261941")
     public void testOMADownloadInterception() throws Exception {
         TestWebServer webServer = TestWebServer.start();
         try {
@@ -325,7 +324,7 @@ public class DownloadTest implements CustomMainActivityStart {
             CriteriaHelper.pollUiThread(() -> {
                 Criteria.checkThat(interceptor.mDownloadItem, Matchers.notNullValue());
                 Criteria.checkThat(
-                        interceptor.mDownloadItem.getDownloadInfo().getUrl(), Matchers.is(url));
+                        interceptor.mDownloadItem.getDownloadInfo().getUrl().getSpec(), Matchers.is(url));
             });
         } finally {
             webServer.shutdown();
