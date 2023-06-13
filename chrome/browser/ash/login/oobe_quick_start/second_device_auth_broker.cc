1482a5d89785c618704426c54c115414bf84b4d9
Kush Sinha <sinhak@chromium.org>
2023-06-13 04:43:20 +0000

    QuickStart: Fix SecondDeviceAuthBroker::GetChallengeBytes
    http://cl/537437139 changes the Gaia API for getting challenge bytes for
    SecondDeviceAuth. Make the corresponding client side change to identify
    the target device as ChromeOS.
    
    Bug: b/259021973
    Test: unit_tests --gtest_filter="*SecondDeviceAuthBrokerTest*"
    Change-Id: I7b95c70abb561d18540257125004ee326926cf53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594800
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156697}

--- chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker.cc
+++ chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker.cc
@@ -81,6 +81,10 @@ const int64_t kStartSessionTimeoutInSeconds = 60;
 constexpr char kHttpMethod[] = "POST";
 constexpr char kHttpContentType[] = "application/json";
 
+constexpr char kGetChallengeDataRequest[] = R"({
+      "target_device_type": "CHROME_OS"
+    })";
+
 constexpr auto kRejectionReasonErrorMap = base::MakeFixedFlatMap<
     base::StringPiece,
     SecondDeviceAuthBroker::RefreshTokenRejectionResponse::Reason>({
@@ -119,8 +123,8 @@ constexpr net::NetworkTrafficAnnotationTag kChallengeDataAnnotation =
             "Google's authentication server"
           trigger: "When the user starts the Quick Start flow from OOBE"
           data:
-            "Nothing. Authentication to this API is done through Chrome's API "
-            "key"
+            "A JSON dict that identifies the device type as ChromeOS. "
+            "Authentication to this API is done through Chrome's API key"
           destination: GOOGLE_OWNED_SERVICE
         }
         policy {
@@ -597,7 +601,7 @@ void SecondDeviceAuthBroker::GetChallengeBytes(
       /*http_method=*/kHttpMethod,
       /*content_type=*/kHttpContentType,
       /*timeout_ms=*/kGetChallengeDataTimeoutInSeconds * 1000,
-      /*post_data=*/std::string(),
+      /*post_data=*/kGetChallengeDataRequest,
       /*headers=*/std::vector<std::string>(),
       /*annotation_tag=*/kChallengeDataAnnotation,
       /*is_stable_channel=*/chrome::GetChannel() ==
