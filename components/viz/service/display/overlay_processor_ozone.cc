be1c868dc37c6f74b5dede2c26b51a321b18f5f4
Sasha McIntosh <sashamcintosh@chromium.org>
2023-06-13 15:27:48 +0000

    viz: Re-Enable HDR promotion to overlay
    Add P010 to list of buffer formats preventing overlay promotion.
    
    Re-order colorimetry capabilities in accordance with CTA-861-F Table 56.
    This allows HDR10 functionality to correctly identify available formats.
    
    Set max luminance according to provided HDR static metadata.
    
    BUG=1286074,b:249215983
    
    Change-Id: I509079315fc289c6f15a5486f47fd12558686b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582604
    Commit-Queue: Sasha McIntosh <sashamcintosh@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156938}

--- components/viz/service/display/overlay_processor_ozone.cc
+++ components/viz/service/display/overlay_processor_ozone.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/overlay_strategy_underlay.h"
 #include "gpu/command_buffer/client/shared_image_interface.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_manager.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/size_conversions.h"
@@ -100,11 +101,6 @@ bool AllowColorSpaceCombination(
   if (!source_color_space.IsValid())
     return true;
 
-  // TODO(b/249215983): Disable promotion of HDR content to overlay until
-  // decoding to monitor colorspace is supported.
-  if (source_color_space.IsHDR())
-    return false;
-
   // Since https://crrev.com/c/2336347, we force BT.601/narrow for the
   // COLOR_ENCODING and COLOR_RANGE DRM/KMS properties. On the other hand, the
   // compositor is able to handle different YUV encodings and ranges. Therefore,
@@ -129,9 +125,12 @@ bool AllowColorSpaceCombination(
   //
   // TODO(b/243150091): Remove the call to IsYUVColorSpace() or turn it into a
   // DCHECK() once LaCrOS plumbs the correct color space.
+  bool is_yuv_color_space = features::IsLacrosColorManagementEnabled() ||
+                            IsYUVColorSpace(source_color_space);
   if ((source_format == gfx::BufferFormat::YUV_420_BIPLANAR ||
-       source_format == gfx::BufferFormat::YVU_420) &&
-      IsYUVColorSpace(source_color_space) &&
+       source_format == gfx::BufferFormat::YVU_420 ||
+       source_format == gfx::BufferFormat::P010) &&
+      is_yuv_color_space &&
       (source_color_space.GetMatrixID() ==
            gfx::ColorSpace::MatrixID::BT2020_NCL ||
        source_color_space.GetRangeID() == gfx::ColorSpace::RangeID::FULL)) {
