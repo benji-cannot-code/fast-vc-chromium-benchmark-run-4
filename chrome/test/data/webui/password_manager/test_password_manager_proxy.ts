6a1a3f1c3d282696c12424462bef995f19678120
Nina Satragno <nsatragno@chromium.org>
2023-06-13 15:43:32 +0000

    [password manager] Use `changeCredential` instead
    Replace usage of `changeSavedPassword` by `changeCredential` on the new
    password manager. This is a first step in removing
    `changeSavedPassword`. Other uses of the function live in the old
    password manager which is expected to be removed eventually anyway.
    
    Bug: 1432717, 1420597
    Change-Id: If85735fb515cba648ebc84697fdd16b1ac8c76bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4597907
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156949}

--- chrome/test/data/webui/password_manager/test_password_manager_proxy.ts
+++ chrome/test/data/webui/password_manager/test_password_manager_proxy.ts
@@ -51,8 +51,8 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
   constructor() {
     super([
       'addPassword',
+      'changeCredential',
       'cancelExportPasswords',
-      'changeSavedPassword',
       'continueImport',
       'exportPasswords',
       'extendAuthValidity',
@@ -230,10 +230,9 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
     return Promise.resolve();
   }
 
-  changeSavedPassword(
-      id: number, params: chrome.passwordsPrivate.ChangeSavedPasswordParams) {
-    this.methodCalled('changeSavedPassword', {id, params});
-    return Promise.resolve(id);
+  changeCredential(credential: chrome.passwordsPrivate.PasswordUiEntry) {
+    this.methodCalled('changeCredential', credential);
+    return Promise.resolve();
   }
 
   removeCredential(
