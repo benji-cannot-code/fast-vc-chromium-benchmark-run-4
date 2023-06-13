419914544688c6a2fa8d2220046f99d8c2d27318
kylechar <kylechar@chromium.org>
2023-06-13 20:29:45 +0000

    Remove GpuRasterizationStatus enum
    The GpuRasterizationStatus enum used by cc only has two states, on or
    off, so it exactly matches the `use_gpu_rasterization` bool.
    
    Bug: 1442970
    Change-Id: I1c6f50e06b673dc23e0a20965437a427fd5fe315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4610687
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Sohan Jyoti Ghosh <sohanjg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157153}

--- cc/layers/picture_layer_impl_unittest.cc
+++ cc/layers/picture_layer_impl_unittest.cc
@@ -5495,8 +5495,7 @@ TEST_F(LegacySWTileSizeTest, SWTileSizes) {
   gfx::Size result;
 
   host_impl()->CommitComplete();
-  EXPECT_EQ(host_impl()->gpu_rasterization_status(),
-            GpuRasterizationStatus::OFF_DEVICE);
+  EXPECT_FALSE(host_impl()->use_gpu_rasterization());
   host_impl()->NotifyReadyToActivate();
 
   // Default tile-size for large layers.
@@ -5526,8 +5525,7 @@ TEST_F(TileSizeTest, GPUTileSizes) {
 
   // Gpu-rasterization uses 25% viewport-height tiles.
   // The +2's below are for border texels.
-  EXPECT_EQ(host_impl()->gpu_rasterization_status(),
-            GpuRasterizationStatus::ON);
+  EXPECT_TRUE(host_impl()->use_gpu_rasterization());
   host_impl()->active_tree()->SetDeviceViewportRect(gfx::Rect(2000, 2000));
   host_impl()->NotifyReadyToActivate();
 
@@ -5571,8 +5569,7 @@ TEST_F(TileSizeTest, RawDrawTileSizes) {
   constexpr int kMaxTextureSize = 2048;
   // Gpu-RawDraw-rasterization uses 100%
   // std::max({viewport-width, viewport-height, 2280}) tiles.
-  EXPECT_EQ(host_impl()->gpu_rasterization_status(),
-            GpuRasterizationStatus::ON);
+  EXPECT_TRUE(host_impl()->use_gpu_rasterization());
   gfx::Rect viewport_rect(2000, 2000);
   host_impl()->active_tree()->SetDeviceViewportRect(viewport_rect);
   layer->set_gpu_raster_max_texture_size(viewport_rect.size());
@@ -5610,8 +5607,7 @@ TEST_F(HalfWidthTileTest, TileSizes) {
 
   gfx::Size result;
   host_impl()->CommitComplete();
-  EXPECT_EQ(host_impl()->gpu_rasterization_status(),
-            GpuRasterizationStatus::ON);
+  EXPECT_TRUE(host_impl()->use_gpu_rasterization());
   host_impl()->active_tree()->SetDeviceViewportRect(gfx::Rect(2000, 2000));
   host_impl()->NotifyReadyToActivate();
 
