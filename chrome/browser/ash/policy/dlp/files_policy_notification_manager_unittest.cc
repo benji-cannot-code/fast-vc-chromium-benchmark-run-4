56477c5d358ee314a2ec0e6698462c8f152a9287
Aida Zolic <aidazolic@chromium.org>
2023-06-13 12:43:14 +0000

    dlp: Persistent Files desktop notifications
    The desktop notifications for warnings or blocked files should stay
    open until the user dismisses the notification.
    
    R=ayaelattar@chromium.org
    
    Fixed: b:285568350
    Change-Id: Ifcf9e4f8660d1c7eec8a67a148969c7b83b6c40d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595117
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156861}

--- chrome/browser/ash/policy/dlp/files_policy_notification_manager_unittest.cc
+++ chrome/browser/ash/policy/dlp/files_policy_notification_manager_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/dlp/files_policy_notification_manager.h"
 
 #include "ash/webui/file_manager/url_constants.h"
+#include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/scoped_refptr.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/mock_callback.h"
 #include "chrome/browser/ash/file_manager/copy_or_move_io_task.h"
 #include "chrome/browser/ash/file_manager/fake_disk_mount_manager.h"
+#include "chrome/browser/ash/file_manager/io_task.h"
 #include "chrome/browser/ash/file_manager/io_task_controller.h"
 #include "chrome/browser/ash/file_manager/trash_io_task.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
@@ -22,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "content/public/test/browser_task_environment.h"
+#include "storage/browser/file_system/file_system_url.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "storage/browser/test/test_file_system_context.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -101,6 +104,32 @@ class FilesPolicyNotificationManagerTest : public testing::Test {
         base::FilePath::FromUTF8Unsafe(path));
   }
 
+  // Creates and adds a CopyOrMoveIOTask with `task_id` with type
+  // `OperationType::kCopy` if `is_copy` is true, and `OperationType::kMove` if
+  // false.
+  base::FilePath AddCopyOrMoveIOTask(file_manager::io_task::IOTaskId id,
+                                     bool is_copy) {
+    base::FilePath src_file_path = temp_dir_.GetPath().AppendASCII("test1.txt");
+    if (!CreateDummyFile(src_file_path)) {
+      return base::FilePath();
+    }
+    auto src_url = CreateFileSystemURL(src_file_path.value());
+    if (!src_url.is_valid()) {
+      return base::FilePath();
+    }
+    auto dst_url = CreateFileSystemURL(temp_dir_.GetPath().value());
+
+    auto task = std::make_unique<file_manager::io_task::CopyOrMoveIOTask>(
+        is_copy ? file_manager::io_task::OperationType::kCopy
+                : file_manager::io_task::OperationType::kCopy,
+        std::vector<storage::FileSystemURL>({src_url}), dst_url, profile_,
+        file_system_context_);
+
+    io_task_controller_->Add(std::move(task));
+
+    return src_file_path;
+  }
+
  protected:
   std::unique_ptr<FilesPolicyNotificationManager> fpnm_;
   scoped_refptr<storage::FileSystemContext> file_system_context_;
@@ -115,18 +144,8 @@ class FilesPolicyNotificationManagerTest : public testing::Test {
 
 TEST_F(FilesPolicyNotificationManagerTest, AddCopyTask) {
   file_manager::io_task::IOTaskId task_id = 1;
-  base::FilePath src_file_path = temp_dir_.GetPath().AppendASCII("test1.txt");
-  ASSERT_TRUE(CreateDummyFile(src_file_path));
-  auto src_url = CreateFileSystemURL(src_file_path.value());
-  ASSERT_TRUE(src_url.is_valid());
-  auto dst_url = CreateFileSystemURL(temp_dir_.GetPath().value());
+  ASSERT_FALSE(AddCopyOrMoveIOTask(task_id, /*is_copy=*/true).empty());
 
-  auto task = std::make_unique<file_manager::io_task::CopyOrMoveIOTask>(
-      file_manager::io_task::OperationType::kCopy,
-      std::vector<storage::FileSystemURL>({src_url}), dst_url, profile_,
-      file_system_context_);
-
-  io_task_controller_->Add(std::move(task));
   EXPECT_TRUE(fpnm_->HasIOTask(task_id));
 
   // Pause the task. It shouldn't be removed.
@@ -473,6 +492,7 @@ TEST_P(FPNMPausedStatusNotification, PausedShowsWarningNotification_Single) {
   EXPECT_EQ(notification->message(), u"File may contain sensitive content");
   EXPECT_EQ(notification->buttons()[0].title, u"Cancel");
   EXPECT_EQ(notification->buttons()[1].title, ok_button);
+  EXPECT_TRUE(notification->never_timeout());
 }
 
 TEST_P(FPNMPausedStatusNotification, PausedShowsWarningNotification_Multi) {
@@ -503,6 +523,7 @@ TEST_P(FPNMPausedStatusNotification, PausedShowsWarningNotification_Multi) {
   EXPECT_EQ(notification->message(), u"Files may contain sensitive content");
   EXPECT_EQ(notification->buttons()[0].title, u"Cancel");
   EXPECT_EQ(notification->buttons()[1].title, u"Review");
+  EXPECT_TRUE(notification->never_timeout());
 }
 
 INSTANTIATE_TEST_SUITE_P(
@@ -556,6 +577,7 @@ TEST_P(FPNMErrorStatusNotification, ErrorShowsBlockNotification_Single) {
   EXPECT_EQ(notification->message(), u"File was blocked");
   EXPECT_EQ(notification->buttons()[0].title, u"Dismiss");
   EXPECT_EQ(notification->buttons()[1].title, u"Learn more");
+  EXPECT_TRUE(notification->never_timeout());
 }
 
 TEST_P(FPNMErrorStatusNotification, ErrorShowsBlockNotification_Multi) {
@@ -585,6 +607,7 @@ TEST_P(FPNMErrorStatusNotification, ErrorShowsBlockNotification_Multi) {
   EXPECT_EQ(notification->message(), u"Review for further details");
   EXPECT_EQ(notification->buttons()[0].title, u"Dismiss");
   EXPECT_EQ(notification->buttons()[1].title, u"Review");
+  EXPECT_TRUE(notification->never_timeout());
 }
 
 INSTANTIATE_TEST_SUITE_P(
