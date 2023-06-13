4e332abecabdfe3a39983082a229715aed077ef7
Matt Jones <mdjones@chromium.org>
2023-06-13 22:49:47 +0000

    [SFL] Add experiment flag for tracking by default
    This patch adds the feature flag and about:flags entry for the "track
    by default" experiment. The flag has been introduced for all platforms
    but will primarily work on desktop to start with.
    
    Bug: b:286410726
    Change-Id: If4a6627ede899f61b1eaa96cf12dd71a8709b0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605356
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157223}

--- components/commerce/core/commerce_feature_list.h
+++ components/commerce/core/commerce_feature_list.h
@@ -94,6 +94,7 @@ BASE_DECLARE_FEATURE(kPriceInsightsRegionLaunched);
 
 BASE_DECLARE_FEATURE(kShoppingList);
 BASE_DECLARE_FEATURE(kShoppingListRegionLaunched);
+BASE_DECLARE_FEATURE(kShoppingListTrackByDefault);
 BASE_DECLARE_FEATURE(kShoppingPDPMetrics);
 BASE_DECLARE_FEATURE(kShoppingPDPMetricsRegionLaunched);
 BASE_DECLARE_FEATURE(kRetailCoupons);
