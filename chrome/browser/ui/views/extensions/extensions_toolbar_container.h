e6d4c3b195c875d2db30f424ac07a1046ef51bdc
Tom Lukaszewicz <tluk@google.com>
2023-06-13 22:45:19 +0000

    Revert "[Extensions c2s] Display confirmation message on request access button"
    This reverts commit bc1fceb7571569d6db7a8c57738657ea8a94177f.
    
    Reason for revert: Mac bots failing
    
    Example of first failing build
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac13-arm64-rel-tests/612/overview
    
    Original change's description:
    > [Extensions c2s] Display confirmation message on request access button
    >
    > Display a confirmation message for kConfirmationDisplayDuration on the
    > request access button after it has been clicked.
    >
    > This CL doesn't fix the animation issues with the extension toolbar
    > button (puzzle piece)
    >
    > Screencasts:
    > - No reload:
    > https://drive.google.com/file/d/1O3nVNG-Oq8aqGjyhgf9LSxSR_yYyLNy9/view?usp=sharing
    > - Reload accepted:
    > https://drive.google.com/file/d/1LAKk8VUjWVoaX40QGxRa26u3AiqJuC8O/view?usp=sharing
    > - Reload dismissed:
    > https://drive.google.com/file/d/1KUSPYOyX9gICU9Hf6mhAgSnZXwXnHKLi/view?usp=sharing
    >
    > Edge cases:
    > Click on request access button and before button is collapsed:
    > - Navigate to page that requests access
    > https://drive.google.com/file/d/11YNJJFnFCMj3kDfXo5s7nkXArhoH7_cd/view?usp=sharing
    > - Navigate to page that doesn't request access
    > https://drive.google.com/file/d/1zsr8WkzFhmfb1i_LANPBnvo2kRtjvWtp/view?usp=sharing
    > - Open a new tab
    > https://drive.google.com/file/d/1gRv1vIJuDBoyfi7xY6psMTnAGCsCAI4g/view?usp=sharing
    > - Change to an existent tab
    > https://drive.google.com/file/d/1RWbxJdyTPgwYcb-76Uoq2-rhWC5lG8J6/view?usp=sharing
    >
    > Bug: 1390952
    > Change-Id: I42adc2bd4aa125ed4330f964486de89f2a1dcc67
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582732
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1157017}
    
    Bug: 1390952
    Change-Id: I5e0134ee2b36d1bb73cd2e363883afebe9bb2058
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611083
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Tom Lukaszewicz <tluk@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tom Lukaszewicz <tluk@google.com>
    Owners-Override: Tom Lukaszewicz <tluk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157215}

--- chrome/browser/ui/views/extensions/extensions_toolbar_container.h
+++ chrome/browser/ui/views/extensions/extensions_toolbar_container.h
@@ -168,7 +168,6 @@ class ExtensionsToolbarContainer
   void UpdateToolbarActionHoverCard(
       ToolbarActionView* action_view,
       ToolbarActionHoverCardUpdateType update_type) override;
-  void CollapseConfirmation() override;
 
   // ToolbarActionView::Delegate:
   content::WebContents* GetCurrentWebContents() override;
@@ -263,9 +262,6 @@ class ExtensionsToolbarContainer
       TabStripModel* tab_strip_model,
       const TabStripModelChange& change,
       const TabStripSelectionChange& selection) override;
-  void TabChangedAt(content::WebContents* contents,
-                    int index,
-                    TabChangeType change_type) override;
 
   // ToolbarActionsModel::Observer:
   void OnToolbarActionAdded(
