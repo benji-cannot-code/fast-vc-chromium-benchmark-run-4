f4d03249455ee78c2d7ad84daf8112e151eb89c1
Nicolás Peña <npm@chromium.org>
2023-06-13 17:05:29 +0000

    [FedCM] Add WPT for context parameter
    The tests do not yet work because some automation needs to be
    implemented to obtain the title.
    
    Bug: 1440192
    Change-Id: I4f0957e8da02eeeb501a43ed8a06e96883950944
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605035
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157011}

--- third_party/blink/web_tests/resources/testdriver-vendor.js
+++ third_party/blink/web_tests/resources/testdriver-vendor.js
@@ -476,6 +476,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   };
 
+  window.test_driver_internal.get_fedcm_dialog_title = async function() {
+    // TODO(crbug.com/1453691): implement the title getter.
+    return "";
+  }
+
   // Enable automation so we don't wait for user input on unimplemented APIs
   window.test_driver_internal.in_automation = true;
 
