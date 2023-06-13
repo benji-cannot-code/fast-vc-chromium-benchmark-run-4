bc3c269beab34c4bd74a2cc5b822f2021d5f5e18
Hidehiko Abe <hidehiko@chromium.org>
2023-06-13 04:54:02 +0000

    Fix autotestPrivate test to use LacrosOnly mode.
    As a preparation to remove Lacros SxS/Primary.
    
    BUG=1448575
    TEST=Tryjob
    
    Change-Id: I32ead1613dbc020d107b14c154a2977246485cef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608105
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156702}

--- chrome/test/data/extensions/api_test/autotest_private/test.js
+++ chrome/test/data/extensions/api_test/autotest_private/test.js
@@ -1613,9 +1613,9 @@ var systemWebAppsTests = [
         chrome.autotestPrivate.getLacrosInfo(
             chrome.test.callbackPass(function(lacrosInfo) {
               chrome.test.assertEq('Unavailable', lacrosInfo['state']);
-              chrome.test.assertTrue(!lacrosInfo['isKeepAlive']);
+              chrome.test.assertTrue(lacrosInfo['isKeepAlive']);
               chrome.test.assertEq('', lacrosInfo['lacrosPath']);
-              chrome.test.assertEq('SideBySide', lacrosInfo['mode']);
+              chrome.test.assertEq('Only', lacrosInfo['mode']);
             }));
       },
     ]
