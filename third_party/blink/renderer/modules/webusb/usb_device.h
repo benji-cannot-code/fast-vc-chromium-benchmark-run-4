74bf22100d8110278ace3bb2eefd68cde103fe74
Reilly Grant <reillyg@chromium.org>
2023-06-13 19:26:01 +0000

    usb: Combine USBDevice::controlTransferOut implementations
    Rather than having two different implementations of this method we can
    make the `data` parameter optional in one and share the rest of the
    logic.
    
    This makes it easier to get full test coverage. Similarly, an
    unreachable buffer size check has been removed.
    
    Change-Id: I03edc410049dfef2d0293d82d4fe096267fc0cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609979
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Reviewed-by: Sina Firoozabadi <sinafirooz@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157114}

--- third_party/blink/renderer/modules/webusb/usb_device.h
+++ third_party/blink/renderer/modules/webusb/usb_device.h
@@ -92,7 +92,7 @@ class USBDevice : public ScriptWrappable,
                                    ExceptionState&);
   ScriptPromise controlTransferOut(ScriptState*,
                                    const USBControlTransferParameters* setup,
-                                   const DOMArrayPiece& data,
+                                   const DOMArrayPiece& optional_data,
                                    ExceptionState&);
   ScriptPromise clearHalt(ScriptState*,
                           String direction,
