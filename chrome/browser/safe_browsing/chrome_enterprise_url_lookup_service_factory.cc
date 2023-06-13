91dd3cca9ecbbfe83a498d8357fa98be11f3a816
Daniel Rubery <drubery@chromium.org>
2023-06-13 16:41:53 +0000

    Add missing dependency for ChromeEnterpriseRealTimeUrlLookupService
    This class implicitly depends on the IdentityManager because it has the
    regular RealTimeUrlLookupServiceBase as a subclass. Therefore we also
    need it to be shut down before the IdentityManager.
    
    Bug: 1453705
    Change-Id: I2c4a405a8ea7d198fd405c9bbae9546a2608d3d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609499
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156985}

--- chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service_factory.cc
+++ chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service_factory.cc
@@ -57,6 +57,7 @@ ChromeEnterpriseRealTimeUrlLookupServiceFactory::
   DependsOn(VerdictCacheManagerFactory::GetInstance());
   DependsOn(enterprise_connectors::ConnectorsServiceFactory::GetInstance());
   DependsOn(SafeBrowsingNavigationObserverManagerFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
 }
 
 KeyedService*
