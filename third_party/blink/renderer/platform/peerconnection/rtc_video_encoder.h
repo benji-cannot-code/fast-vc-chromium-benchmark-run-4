0e655353738568257ec2c2203d0ebf19f32fd051
Henrik Boström <hbos@chromium.org>
2023-06-13 10:36:14 +0000

    [WebRTC] Add flag so that forcing SW includes 360p.
    We already force SW when resolution is <360p (excluding 360p). This flag
    makes us force SW for <=360p resolutions (including 360p).
    
    Bug: chromium:1454398
    Change-Id: I262214c7e6e88ce879543a7ee0d6cb93acf55cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608072
    Auto-Submit: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156820}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.h
@@ -37,6 +37,7 @@ namespace blink {
 
 namespace features {
 PLATFORM_EXPORT BASE_DECLARE_FEATURE(kWebRtcScreenshareSwEncoding);
+PLATFORM_EXPORT BASE_DECLARE_FEATURE(kForcingSoftwareIncludes360);
 }
 
 // RTCVideoEncoder uses a media::VideoEncodeAccelerator to implement a
