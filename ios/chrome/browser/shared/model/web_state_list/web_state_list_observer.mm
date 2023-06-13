37b44c559cdc901f4a63e8ed584a5b1dc38f63ed
Asami Doi <asamidoi@chromium.org>
2023-06-13 17:01:32 +0000

    [ios] Replace WebStateMoved() with WebStateListChanged()
    This CL replaces WebStateMoved() API with WebStateListChanged().
    
    This CL is a part of refactoring WebStateListObserver API:
    Step 1 [Done]: Introduce WebStateListChanged() w/ kReplace type in WebStateListChange and replace WebStateReplacedAt() with it
    Step 2 [Done]: Introduce kInsert type and replace WebStateInsertedAt()
    Step 3 [This CL]: Replace WebStateMoved() with WebStateListChanged()
    Step 4: Replace WebStateDetachedAt() and WebStateListDestroyed() with WebStateListChanged()
    Step 5: Replace Will*() with WillWebStateListChanged()
    Step 6: Migrate WebStateActivatedAt() to WebStateListChanged()
    
    No behavior changes.
    
    See https://docs.google.com/document/d/1I1QUZAexM1eMI2l2oriZl5GARrtofNI0SxHUPB4h-YE/edit?usp=sharing for more details (internal document).
    
    Bug: 1442546
    Change-Id: I11879cb90b0606916dc369bd89dce70ac674f56e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4556706
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157007}

--- ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.mm
+++ ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.mm
@@ -13,6 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+WebStateListChangeMove::WebStateListChangeMove(
+    raw_ptr<web::WebState> moved_web_state,
+    int moved_from_index)
+    : moved_web_state_(moved_web_state), moved_from_index_(moved_from_index) {}
+
+WebStateListChange::Type WebStateListChangeMove::type() const {
+  return kType;
+}
+
 WebStateListChangeReplace::WebStateListChangeReplace(
     raw_ptr<web::WebState> replaced_web_state,
     raw_ptr<web::WebState> inserted_web_state)
@@ -43,11 +52,6 @@ void WebStateListObserver::WebStateListChanged(
     WebStateList* web_state_list,
     const WebStateListChange& change,
     const WebStateSelection& selection) {}
-
-void WebStateListObserver::WebStateMoved(WebStateList* web_state_list,
-                                         web::WebState* web_state,
-                                         int from_index,
-                                         int to_index) {}
 
 void WebStateListObserver::WillDetachWebStateAt(WebStateList* web_state_list,
                                                 web::WebState* web_state,
