501299c3c070d20a19e4b4acb09bab8fc53ac525
Joseph Park <josephjoopark@chromium.org>
2023-06-13 00:16:04 +0000

    Disable CSC on Chrome pages
    b/282202183
    
    Change-Id: I84873eff887fe847d83399be8237309421c52596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608627
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1156581}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/page_info/page_info_dialog.h"
 #include "chrome/browser/ui/passwords/ui_utils.h"
 #include "chrome/browser/ui/profile_view_utils.h"
+#include "chrome/browser/ui/side_panel/companion/companion_utils.h"
 #include "chrome/browser/ui/side_panel/side_panel_entry_id.h"
 #include "chrome/browser/ui/side_panel/side_panel_enums.h"
 #include "chrome/browser/ui/side_panel/side_panel_ui.h"
@@ -1512,6 +1513,10 @@ void BrowserCommandController::UpdateCommandsForTabState() {
   command_updater_.UpdateCommandEnabled(
       IDC_TOGGLE_REQUEST_TABLET_SITE,
       CanRequestTabletSite(current_web_contents));
+
+  command_updater_.UpdateCommandEnabled(
+      IDC_SHOW_SEARCH_COMPANION,
+      companion::IsCompanionAvailableForCurrentActiveTab(browser_));
 
   UpdateCommandsForContentRestrictionState();
   UpdateCommandsForBookmarkEditing();
