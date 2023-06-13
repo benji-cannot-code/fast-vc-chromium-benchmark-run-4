f01301731401a07a8a444e08741cd2b8dfefbcf0
Taylor Bergquist <tbergquist@chromium.org>
2023-06-13 00:17:18 +0000

    Remove spurious visual data sync updates.
    - Fix a spurious visual data sync update when dragging a group into a new window. We used to call OnGroupClosedInTabStrip followed by OnGroupOpenedInTabStrip, which meant we had to do an update of the visual data after re-opening it to get the header in the right state. Now, we just leave the connection intact the whole time, by going through the listener hierarchy and respecting pause state, so the header starts in the correct visual state in the new window.
    
    - Fix a spurious visual data sync update when opening a saved group in the tabstrip. We used to create the local group, start listening to it, then update the visual data, creating a sync update; now we update the visual data before we start listening.
    
    - Ignore no-op visual data updates across the board to catch anything I missed and/or anything new that gets introduced later.
    
    Change-Id: I08c0df92d5e45f62461138f452ab7213bab8154d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4606435
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156583}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_model_listener.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_model_listener.cc
@@ -57,12 +57,9 @@ void SavedTabGroupModelListener::OnTabGroupChanged(
       return;
     }
 
-    // Called when the last tab in the groups is removed.
-    case TabGroupChange::kClosed: {
-      model_->OnGroupClosedInTabStrip(change.group);
-      return;
-    }
-
+    // Ignored because closing empty groups is handled when the last tab is
+    // removed in TabGroupedStateChanged.
+    case TabGroupChange::kClosed:
     // Ignored because contents changes are handled in TabGroupedStateChanged.
     case TabGroupChange::kContentsChanged:
     // Ignored because we explicitly add the TabGroupId to the saved tab group
@@ -188,6 +185,7 @@ void SavedTabGroupModelListener::ResumeTrackingLocalTabGroup(
 
 void SavedTabGroupModelListener::DisconnectLocalTabGroup(
     tab_groups::TabGroupId tab_group_id) {
+  model_->OnGroupClosedInTabStrip(tab_group_id);
   local_tab_group_listeners_.erase(tab_group_id);
 }
 
