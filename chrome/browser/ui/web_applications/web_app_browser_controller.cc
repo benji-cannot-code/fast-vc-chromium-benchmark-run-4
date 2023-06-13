bdc1beadd0ed327ea8a1af8421a8d1ee0337f6df
Sean Kau <skau@chromium.org>
2023-06-13 23:46:05 +0000

    IWYU ui/color/color_provider_manager.h
    Add an explicit include for ColorProviderManager where it was
    being retrieved through a transitively (which will be broken
    in a future CL).
    
    This CL was uploaded by git cl split.
    
    R=dmurph@chromium.org
    
    Bug: b:286952053
    Change-Id: Ic243af29cdc0de29ddd488deeac9b2f600b166f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611102
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157258}

--- chrome/browser/ui/web_applications/web_app_browser_controller.cc
+++ chrome/browser/ui/web_applications/web_app_browser_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/web_applications/web_app_browser_controller.h"
 
+#include "base/callback_list.h"
 #include "base/check_is_test.h"
 #include "base/containers/flat_set.h"
 #include "base/functional/callback.h"
