eca8f14b21badd659f8a7be41798ecbe8490b39a
Koji Ishii <kojii@chromium.org>
2023-06-13 04:54:50 +0000

    [text-wrap-pretty] Add `NGLineWidths`
    This patch adds a class that computes the line width of each
    line for an `NGInlineNode`. This class is a preparation to
    support simple floating objects for `text-wrap: pretty`.
    
    Currently, this class supports:
    * Up to single exclusion.
    * Inline content is simple; it must be rendered using single
      font, without non-initial `vertical-align`.
    
    This patch has no behavior changes as the changes are called
    only from unit tests. It will be used in following patches.
    
    Bug: 1432798
    Change-Id: I7f7d526d9659fa3cdffad210ccebd4f454e50b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4603613
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156703}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.h
@@ -62,6 +62,8 @@ class CORE_EXPORT NGInlineLayoutAlgorithm final
   }
 
  private:
+  friend class NGLineWidthsTest;
+
   unsigned PositionLeadingFloats(NGExclusionSpace*, NGPositionedFloatVector*);
   NGPositionedFloat PositionFloat(LayoutUnit origin_block_bfc_offset,
                                   LayoutObject* floating_object,
