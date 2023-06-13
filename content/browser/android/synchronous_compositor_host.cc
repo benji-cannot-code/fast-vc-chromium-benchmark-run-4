dbe9647c8e87799268b8a9c8ebdd4c8e73157b61
Bo Liu <boliu@chromium.org>
2023-06-13 19:53:48 +0000

    Fix webview OOP software draw
    It was always being throttled because invalidate after draw count wasn't
    being reset.
    
    Change-Id: Ic691309838776430e08df7cba9a1531f33a73d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605167
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157139}

--- content/browser/android/synchronous_compositor_host.cc
+++ content/browser/android/synchronous_compositor_host.cc
@@ -338,7 +338,6 @@ bool SynchronousCompositorHost::DemandDrawSwInProc(SkCanvas* canvas) {
       blink::mojom::SyncCompositorDemandDrawSwParams::New();  // Unused.
   uint32_t metadata_version = 0u;
   invalidate_needs_draw_ = false;
-  num_invalidates_since_last_draw_ = 0u;
   if (!IsReadyForSynchronousCall() ||
       !GetSynchronousCompositor()->DemandDrawSw(std::move(params),
                                                 &common_renderer_params,
@@ -378,6 +377,7 @@ struct SynchronousCompositorHost::SharedMemoryWithSize {
 
 bool SynchronousCompositorHost::DemandDrawSw(SkCanvas* canvas,
                                              bool software_canvas) {
+  num_invalidates_since_last_draw_ = 0u;
   if (use_in_process_zero_copy_software_draw_)
     return DemandDrawSwInProc(canvas);
 
