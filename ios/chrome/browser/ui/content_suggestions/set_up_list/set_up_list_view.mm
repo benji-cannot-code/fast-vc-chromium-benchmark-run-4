3870c5faa4d3eb136a725915863291569f2da5ea
Scott Yoder <scottyoder@google.com>
2023-06-14 16:25:26 +0000

    [ios] Increase spacing between SetUpList items
    This increases the spacing between Set Up List items from 15px to 18px.
    
    Screenshot with changes:
    https://screenshot.googleplex.com/3RkPTywUkfGBrLY.png
    
    Fixed: 1453262
    Change-Id: I11eaf12d89613715055d52f7f20fa79d52d7fe6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4601649
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157597}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_view.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_view.mm
@@ -39,6 +39,9 @@ constexpr CGFloat kMargin = 14;
 // The padding used inside the list.
 constexpr CGFloat kPadding = 15;
 
+// The spacing between items in the list.
+constexpr CGFloat kItemSpacing = 18;
+
 // The spacing used between title and description in the "All Set" View.
 constexpr CGFloat kAllSetSpacing = 10;
 
@@ -281,7 +284,7 @@ constexpr NSString* const kAllSetRight = @"set_up_list_all_set_right";
       [[UIStackView alloc] initWithArrangedSubviews:initialItems];
   stack.axis = UILayoutConstraintAxisVertical;
   stack.translatesAutoresizingMaskIntoConstraints = NO;
-  stack.spacing = kPadding;
+  stack.spacing = kItemSpacing;
   stack.layer.masksToBounds = YES;
   stack.layer.cornerRadius = kBorderRadius;
   stack.layer.borderWidth = kBorderWidth;
