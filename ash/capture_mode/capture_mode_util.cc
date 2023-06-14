a524c9b92b82d421cd461d148b05e5293dc643d3
Sean Kau <skau@chromium.org>
2023-06-14 01:35:36 +0000

    IWYU ui/color/color_provider_manager.h
    Add an explicit include for ColorProviderManager where it was
    being retrieved through a transitively (which will be broken
    in a future CL).
    
    This CL was uploaded by git cl split.
    
    R=afakhry@chromium.org
    
    Bug: b:286952053
    Change-Id: I2febfd4b3f82eac85ee0becfac2e8b7c8de86bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611731
    Commit-Queue: Sean Kau <skau@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157307}

--- ash/capture_mode/capture_mode_util.cc
+++ ash/capture_mode/capture_mode_util.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/image_model.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/compositor/layer.h"
 #include "ui/events/ash/keyboard_capability.h"
 #include "ui/gfx/geometry/point.h"
