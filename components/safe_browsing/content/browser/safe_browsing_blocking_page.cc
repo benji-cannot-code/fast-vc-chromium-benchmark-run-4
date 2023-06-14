e7891c6d3d9be1a8618b764694f7f16c5c61529f
Xinghui Lu <xinghuilu@chromium.org>
2023-06-14 23:04:30 +0000

    Add more metrics to understand why ClientSafeBrowsingReport is not sent
    Sometimes ClientSafeBrowsingReport can be skipped because there is
    already a threat details job in progress. This can happen if other
    callers have started the job (PasswordProtection or suspicious
    reports) or another subresource URL has already been flagged. We'd
    like to know how often this happens and see if it fills the gap of
    low report QPS.
    
    Histograms added:
    SafeBrowsing.ClientSafeBrowsingReport.HasThreatDetailsAtFinish.{ResourceType}
    SafeBrowsing.ClientSafeBrowsingReport.HasThreatDetailsAtStart.{ResourceType}
    
    Bug: 1315455
    Change-Id: I5a695ef0c17db782236acf8f07099fd9279f3f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611266
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157864}

--- components/safe_browsing/content/browser/safe_browsing_blocking_page.cc
+++ components/safe_browsing/content/browser/safe_browsing_blocking_page.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/lazy_instance.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/content/browser/safe_browsing_navigation_observer_manager.h"
@@ -68,6 +69,7 @@ SafeBrowsingBlockingPage::SafeBrowsingBlockingPage(
                        display_options),
       threat_details_in_progress_(false),
       threat_source_(unsafe_resources[0].threat_source),
+      is_subresource_(unsafe_resources[0].is_subresource),
       history_service_(history_service),
       navigation_observer_manager_(navigation_observer_manager),
       metrics_collector_(metrics_collector),
@@ -142,6 +144,10 @@ void SafeBrowsingBlockingPage::OnInterstitialClosing() {
 void SafeBrowsingBlockingPage::FinishThreatDetails(const base::TimeDelta& delay,
                                                    bool did_proceed,
                                                    int num_visits) {
+  base::UmaHistogramBoolean(
+      "SafeBrowsing.ClientSafeBrowsingReport.HasThreatDetailsAtFinish" +
+          std::string(is_subresource_ ? ".Subresource" : ".Mainframe"),
+      threat_details_in_progress_);
   // Not all interstitials collect threat details (eg., incognito mode).
   if (!threat_details_in_progress_)
     return;
