e51c02aa56b5c26fa17f0583b7ef78faf3b97068
Anthony Garant <anthonygarant@chromium.org>
2023-06-14 19:21:17 +0000

    Refactor AttributionDataHostManager registration context
    Registration context fields are currently duplicated between the
    ReceiverContext and SourceRegistrations. This CL extracts a new
    RegistrationContext class that can be shared by both use cases. It also
    extracts a navigation context class to ease its use.
    
    This is ground work for being able to process additional registrations
    in the browser process (i.e. parse the headers using the DataDecoder).
    Today, only foreground navigation registrations and beacon source
    registrations are processed in the browser.
    
    Bug: 1374121
    Change-Id: I9007abe0f0cbf11d6b27a61fa7ecbc8a03e43794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611290
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157707}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.h
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.h
@@ -111,7 +111,7 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl
       bool is_final_response) override;
 
  private:
-  class ReceiverContext;
+  class RegistrationContext;
 
   struct DeferredReceiverTimeout;
   struct DeferredReceiver;
@@ -135,8 +135,8 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl
   void OsSourceDataAvailable(std::vector<GURL> registration_urls) override;
   void OsTriggerDataAvailable(std::vector<GURL> registration_urls) override;
 
-  const ReceiverContext* GetReceiverContextForSource();
-  const ReceiverContext* GetReceiverContextForTrigger();
+  const RegistrationContext* GetReceiverRegistrationContextForSource();
+  const RegistrationContext* GetReceiverRegistrationContextForTrigger();
 
   void OnReceiverDisconnected();
 
@@ -166,7 +166,7 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl
   // Owns `this`.
   raw_ptr<AttributionManager> attribution_manager_;
 
-  mojo::ReceiverSet<blink::mojom::AttributionDataHost, ReceiverContext>
+  mojo::ReceiverSet<blink::mojom::AttributionDataHost, RegistrationContext>
       receivers_;
 
   // Map which stores pending receivers for data hosts which are going to
