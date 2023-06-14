a37e0dc801ce488b2b1235639307b36e87d58d1e
Sean Kau <skau@chromium.org>
2023-06-14 06:49:44 +0000

    IWYU ui/color/color_provider_manager.h
    Add an explicit include for ColorProviderManager where it was
    being retrieved through a transitively (which will be broken
    in a future CL).
    
    This CL was uploaded by git cl split.
    
    R=zxdan@chromium.org
    
    Bug: b:286952053
    Change-Id: I69aa550d44028f5ff41376500ec5c5196b0d2da5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611159
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157382}

--- ash/style/color_palette_controller.h
+++ ash/style/color_palette_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/containers/span.h"
+#include "base/functional/callback_forward.h"
 #include "base/observer_list_types.h"
 #include "components/account_id/account_id.h"
 #include "components/prefs/pref_registry_simple.h"
