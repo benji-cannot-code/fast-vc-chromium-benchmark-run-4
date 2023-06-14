c5417866262d10eaa1cd1d869cbf7ad235d9bfbf
Bohdan Tyshchenko <bohdanty@google.com>
2023-06-14 22:44:42 +0000

    [OOBE] - Add typography tokens to OS install screen WebView
    Add logic to supply OS install logs WebView with the modal dialog
    typography tokens if OobeJelly enabled.
    
    Some random lines were changed by the git cl format --js command.
    
    Bug: b:270913356
    Change-Id: I4f9b82acd01123bbd7732b9aeba0c4844dd80691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608057
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157856}

--- chrome/browser/resources/chromeos/login/screens/common/os_install.js
+++ chrome/browser/resources/chromeos/login/screens/common/os_install.js
@@ -18,13 +18,13 @@ import '../../components/dialogs/oobe_adaptive_dialog.js';
 import '../../components/dialogs/oobe_loading_dialog.js';
 import '../../components/dialogs/oobe_modal_dialog.js';
 
+import {loadTimeData} from '//resources/ash/common/load_time_data.m.js';
 import {afterNextRender, html, mixinBehaviors, Polymer, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.js';
 import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.js';
 import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.js';
 import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.js';
-
 
 const OsInstallScreenState = {
   INTRO: 'intro',
@@ -79,11 +79,7 @@ class OsInstall extends OsInstallScreenElementBase {
   }
 
   get EXTERNAL_API() {
-    return [
-      'showStep',
-      'setServiceLogs',
-      'updateCountdownString',
-    ];
+    return ['showStep', 'setServiceLogs', 'updateCountdownString'];
   }
 
   defaultUIStep() {
@@ -149,8 +145,9 @@ class OsInstall extends OsInstallScreenElementBase {
    * @private
    */
   getErrorNoDestContentHtml_(locale) {
-    return this.i18nAdvanced(
-        'osInstallDialogErrorNoDestContent', {tags: ['p', 'ul', 'li']});
+    return this.i18nAdvanced('osInstallDialogErrorNoDestContent', {
+      tags: ['p', 'ul', 'li'],
+    });
   }
 
   /**
@@ -159,8 +156,9 @@ class OsInstall extends OsInstallScreenElementBase {
    * @private
    */
   getErrorFailedSubtitleHtml_(locale) {
-    return this.i18nAdvanced(
-        'osInstallDialogErrorFailedSubtitle', {tags: ['p']});
+    return this.i18nAdvanced('osInstallDialogErrorFailedSubtitle', {
+      tags: ['p'],
+    });
   }
 
   /**
@@ -198,10 +196,8 @@ class OsInstall extends OsInstallScreenElementBase {
   setServiceLogs(serviceLogs) {
     this.$.serviceLogsFrame.src = 'data:text/html;charset=utf-8,' +
         encodeURIComponent('<style>' +
-                           'body {' +
-                           '  font-family: Roboto, sans-serif;' +
+                           'body {' + this.getServiceLogsFontsStyling() +
                            '  color: RGBA(0,0,0,.87);' +
-                           '  font-size: 14sp;' +
                            '  margin : 0;' +
                            '  padding : 0;' +
                            '  white-space: pre-wrap;' +
@@ -226,6 +222,26 @@ class OsInstall extends OsInstallScreenElementBase {
    */
   updateCountdownString(timeLeftMessage) {
     this.osInstallDialogSuccessSubtitile_ = timeLeftMessage;
+  }
+
+  /**
+   * Generates fonts styling for the service log WebView based on OobeJelly
+   * flag.
+   * @return {string}
+   * @private
+   */
+  getServiceLogsFontsStyling() {
+    const isOobeJellyEnabled = loadTimeData.getBoolean('isOobeJellyEnabled');
+    if (!isOobeJellyEnabled) {
+      return '  font-family: Roboto, sans-serif;' +
+          '  font-size: 14sp;';
+    }
+    // Those values correspond to the cros-body-1 token.
+    return (
+        '  font-family: Google Sans Text Regular, Google Sans, Roboto, sans-serif;' +
+        '  font-size: 14px;' +
+        '  font-weight: 400;' +
+        '  line-height: 20px;');
   }
 }
 
