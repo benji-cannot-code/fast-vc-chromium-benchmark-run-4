f72570b2dd980fa345d4b5516976c37afd3414c6
Yao Xiao <yaoxia@chromium.org>
2023-06-14 19:18:36 +0000

    [Topics] Let config_version be derived from params directly / not configurable via Finch.
    Why: This is safer, as we won't have inconsistent
    (config --> version) state from Finch misconfiguration.
    
    Right now, we'd only stick to the default configuration. So the
    CurrentConfigVersion() returns a constant.
    
    Also, keep the internals page params up-to-date, as this patch
    already touches the mojom/internals page files.
    
    
    Bug: 1454266
    Change-Id: I71b309e6b860b52daa5275522e128db9fd5d2ca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608744
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157706}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace features {
 
-constexpr int kBrowsingTopicsConfigVersionDefault = 1;
-
 constexpr int kBrowsingTopicsTaxonomyVersionDefault = 1;
 
 BLINK_COMMON_EXPORT
@@ -507,8 +505,6 @@ BLINK_COMMON_EXPORT extern const base::FeatureParam<int>
     kBrowsingTopicsMaxNumberOfApiUsageContextEntriesToLoadPerEpoch;
 BLINK_COMMON_EXPORT extern const base::FeatureParam<int>
     kBrowsingTopicsMaxNumberOfApiUsageContextDomainsToStorePerPageLoad;
-BLINK_COMMON_EXPORT extern const base::FeatureParam<int>
-    kBrowsingTopicsConfigVersion;
 BLINK_COMMON_EXPORT extern const base::FeatureParam<int>
     kBrowsingTopicsTaxonomyVersion;
 BLINK_COMMON_EXPORT extern const base::FeatureParam<std::string>
