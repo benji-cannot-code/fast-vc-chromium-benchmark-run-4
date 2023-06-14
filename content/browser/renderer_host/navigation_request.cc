6a9322d4bf216bb7062f81e67b791fbc5708174c
Jonathan Hao <phao@chromium.org>
2023-06-14 10:07:17 +0000

    Add comment before setting COOP origin in navigation_request.cc
    As pointed out in [1], setting the COOP origin with a mutable reference
    was not very obvious and worth a comment.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4404116/comment/36d1e927_7d23fae3/
    
    Bug: 1385827
    Change-Id: Iebb4cf5e00ac56c548ebf41cd688f4bf2df0e0a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608426
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Quick-Run: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Auto-Submit: Jonathan Hao <phao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157429}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -2513,6 +2513,9 @@ void NavigationRequest::BeginNavigationImpl() {
       // MHTML iframe, before selecting the RenderFrameHost.
       const url::Origin origin = GetOriginForURLLoaderFactoryUnchecked(this);
       const net::SchemefulSite site = net::SchemefulSite(origin);
+
+      // Set the COOP origin in the policy container builder via the mutable
+      // reference before FinalPolicies() is called.
       absl::optional<url::Origin>& coop_origin =
           policy_container_builder_->GetPolicyContainerHost()
               ->cross_origin_opener_policy()
@@ -3086,6 +3089,8 @@ void NavigationRequest::OnRequestRedirected(
     return;
   }
   const url::Origin origin = GetOriginForURLLoaderFactoryUnchecked(this);
+  // Set the COOP origin in the policy container builder via the mutable
+  // reference before coop is sent to EnforceCOOP.
   network::CrossOriginOpenerPolicy& coop =
       response()->parsed_headers->cross_origin_opener_policy;
   coop.origin = origin;
@@ -3916,6 +3921,8 @@ void NavigationRequest::OnResponseStarted(
   // can be determined. This is needed for enforcing COOP below.
 
   {
+    // Set the COOP origin in the policy container builder before
+    // FinalPolicies() is called.
     const url::Origin origin = GetOriginForURLLoaderFactoryBeforeResponse(
         policy_container_builder_->FinalPolicies().sandbox_flags);
     policy_container_builder_->GetPolicyContainerHost()
@@ -4490,6 +4497,8 @@ void NavigationRequest::OnRequestFailedInternal(
   ComputePoliciesToCommitForError();
 
   const auto origin = url::Origin();
+  // Set the COOP origin in the policy container builder before FinalPolicies()
+  // is called.
   policy_container_builder_->GetPolicyContainerHost()
       ->cross_origin_opener_policy()
       .origin = origin;
