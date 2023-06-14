3c667ee76d6260be7efe2809ed11171a1b89b6f8
Osama Fathy <osamafathy@google.com>
2023-06-14 17:49:18 +0000

    [OOBE][CHOOBE] Introduce `skip_choobe_for_tests` to `WizardContext`
    In optional screens tests, when logging in as a new user, there's
    a possibility (based on some build arguments) of hitting CHOOBE screen
    as the first non-skipped screen in the onboarding flow. In this case,
    ChoobeFlowController will be created and will exit any optional screen
    we try to advance to unless it was selected from CHOOBE screen.
    Introducing the flag `skip_choobe_for_tests` will allow tests to
    advance directly to any optional screen.
    
    Bug: b:287023038
    Change-Id: Ieb2a779db46949c1ede24fcf1ef1664af82a0dff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604935
    Reviewed-by: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157659}

--- chrome/browser/ash/login/wizard_controller_browsertest.cc
+++ chrome/browser/ash/login/wizard_controller_browsertest.cc
@@ -3074,6 +3074,10 @@ class WizardControllerThemeSelectionTest : public WizardControllerTest {
 
 IN_PROC_BROWSER_TEST_F(WizardControllerThemeSelectionTest,
                        TransitionToMarketingOptIn) {
+  LoginDisplayHost::default_host()
+      ->GetWizardContextForTesting()
+      ->skip_choobe_for_tests = true;
+
   LoginDisplayHost::default_host()->GetWizardContext()->is_branded_build = true;
   login_mixin_.LoginAsNewRegularUser();
   WizardController::default_controller()->AdvanceToScreen(
@@ -3084,6 +3088,10 @@ IN_PROC_BROWSER_TEST_F(WizardControllerThemeSelectionTest,
 
 IN_PROC_BROWSER_TEST_F(WizardControllerThemeSelectionTest,
                        TransitionToThemeSelection) {
+  LoginDisplayHost::default_host()
+      ->GetWizardContextForTesting()
+      ->skip_choobe_for_tests = true;
+
   login_mixin_.LoginAsNewRegularUser();
   WizardController::default_controller()->AdvanceToScreen(
       GestureNavigationScreenView::kScreenId);
