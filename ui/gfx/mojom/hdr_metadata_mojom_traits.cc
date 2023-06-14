4c0fd7536d582cd339e91462438f91a266a1dfe4
Christopher Cameron <ccameron@chromium.org>
2023-06-14 17:03:34 +0000

    gfx::HDRMetadata: Include extended range in IPC
    Rename gfx::ExtendedRangeBrightness to gfx::HdrMetadataExtendedRange
    to be more consistent with the rest of the HDR metadata.
    
    Include it in mojo.
    
    Add tests for mojo round-tripping.
    
    Bug: 1446302
    Change-Id: I3cc5aacf077986a5102fb8e260d660a0a0277603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599490
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157623}

--- ui/gfx/mojom/hdr_metadata_mojom_traits.cc
+++ ui/gfx/mojom/hdr_metadata_mojom_traits.cc
@@ -27,6 +27,15 @@ bool StructTraits<gfx::mojom::HdrMetadataSmpteSt2086DataView,
   return true;
 }
 
+bool StructTraits<gfx::mojom::HdrMetadataExtendedRangeDataView,
+                  gfx::HdrMetadataExtendedRange>::
+    Read(gfx::mojom::HdrMetadataExtendedRangeDataView data,
+         gfx::HdrMetadataExtendedRange* output) {
+  output->current_headroom = data.current_headroom();
+  output->desired_headroom = data.desired_headroom();
+  return true;
+}
+
 bool StructTraits<gfx::mojom::HDRMetadataDataView, gfx::HDRMetadata>::Read(
     gfx::mojom::HDRMetadataDataView data,
     gfx::HDRMetadata* output) {
@@ -34,6 +43,9 @@ bool StructTraits<gfx::mojom::HDRMetadataDataView, gfx::HDRMetadata>::Read(
     return false;
   }
   if (!data.ReadSmpteSt2086(&output->smpte_st_2086)) {
+    return false;
+  }
+  if (!data.ReadExtendedRange(&output->extended_range)) {
     return false;
   }
   return true;
