79b7899c9b4f88510e62798e3b26ac4e16e4ab28
Sean Kau <skau@chromium.org>
2023-06-14 00:04:13 +0000

    IWYU ui/color/color_provider_manager.h
    Add an explicit include for ColorProviderManager where it was
    being retrieved through a transitively (which will be broken
    in a future CL).
    
    This CL was uploaded by git cl split.
    
    R=chadduffin@chromium.org
    
    Bug: b:286952053
    Change-Id: I8076e6a7ed77c6782cfa3d97f0ce7c221b71001a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611277
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157268}

--- chrome/browser/ash/system_web_apps/color_helpers.cc
+++ chrome/browser/ash/system_web_apps/color_helpers.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/system_web_apps/color_helpers.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/color/color_provider.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/native_theme/native_theme.h"
 
 SkColor ash::GetSystemThemeColor() {
