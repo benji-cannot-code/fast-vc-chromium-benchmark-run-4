8d9277912659019f387b7c9d5d17de2fc539438e
Sylvain Defresne <sdefresne@chromium.org>
2023-06-14 16:11:31 +0000

    [ios] Explicitly list frameworks as target dependencies
    Instead of adding frameworks in default_libs or in ios_app_bundle
    template, let each target explicitly list their frameworks. This
    will allow to link application that e.g. do not link with UIKit.
    
    Fix //base:base_arc target to list explicit dependencies on the
    framework that are used (that were previously coming from the
    default_libs config).
    
    Add missing frameworks and imports that were previously implicitly
    added to the targets.
    
    Bug: none
    Change-Id: I675cd7c499e3c79021a8da3cdb0aa20f2b7d0ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609126
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157582}

--- ios/chrome/intents_extension/chrome_intents_handler.mm
+++ ios/chrome/intents_extension/chrome_intents_handler.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/intents_extension/chrome_intents_handler.h"
 
+#import <Foundation/Foundation.h>
+
 #import "ios/chrome/common/intents/OpenInChromeIncognitoIntent.h"
 #import "ios/chrome/common/intents/OpenInChromeIntent.h"
 #import "ios/chrome/common/intents/SearchInChromeIntent.h"
