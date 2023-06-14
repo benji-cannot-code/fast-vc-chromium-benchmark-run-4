7e65ff070d2b55300f8739c3915fa0bba5108846
François Degros <fdegros@chromium.org>
2023-06-14 05:18:31 +0000

    BulkPinning: Add histogram FileBrowser.GoogleDrive.BulkPinning.Error
    BUG=b:286981785
    
    Change-Id: Ib9b5082a2dfd0c8c3ec121793e46f7647018cdd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4607968
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157364}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.cc
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/logging.h"
 #include "base/memory/raw_ref.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
 #include "base/strings/string_piece.h"
@@ -1001,18 +1002,17 @@ void PinManager::Complete(const Stage stage) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!InProgress(stage));
 
-  progress_.stage = stage;
   switch (stage) {
     case Stage::kSuccess:
       VLOG(1) << "Finished with success";
       break;
 
     case Stage::kPausedOffline:
-      VLOG(1) << "Paused offline";
+      VLOG(1) << "Paused because offline";
       break;
 
     case Stage::kPausedBatterySaver:
-      VLOG(1) << "Paused battery saver";
+      VLOG(1) << "Paused because of battery saver";
       break;
 
     case Stage::kStopped:
@@ -1021,7 +1021,24 @@ void PinManager::Complete(const Stage stage) {
 
     default:
       LOG(ERROR) << "Finished with error: " << Quote(stage);
+
+      switch (progress_.stage) {
+        case Stage::kListingFiles:
+          base::UmaHistogramEnumeration(
+              "FileBrowser.GoogleDrive.BulkPinning.Listing.Error", stage);
+          break;
+
+        case Stage::kSyncing:
+          base::UmaHistogramEnumeration(
+              "FileBrowser.GoogleDrive.BulkPinning.Syncing.Error", stage);
+          break;
+
+        default:
+          break;
+      }
   }
+
+  progress_.stage = stage;
 
   if (progress_.stage == Stage::kNotEnoughSpace) {
     StartMonitoringSpace();
