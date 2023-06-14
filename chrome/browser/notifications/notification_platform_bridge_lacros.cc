c5576eac5c2e1c97791ee5926918c5d214391748
Sean Kau <skau@chromium.org>
2023-06-14 01:28:44 +0000

    IWYU ui/color/color_provider_manager.h
    Add an explicit include for ColorProviderManager where it was
    being retrieved through a transitively (which will be broken
    in a future CL).
    
    This CL was uploaded by git cl split.
    
    R=yoshiki@chromium.org
    
    Bug: b:286952053
    Change-Id: Ied20fede9eac1d0bf11c2582600f385330226e12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4610246
    Commit-Queue: Sean Kau <skau@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157304}

--- chrome/browser/notifications/notification_platform_bridge_lacros.cc
+++ chrome/browser/notifications/notification_platform_bridge_lacros.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/notification.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/message_center/public/cpp/notification.h"
 #include "ui/message_center/public/cpp/notification_types.h"
 #include "ui/native_theme/native_theme.h"
