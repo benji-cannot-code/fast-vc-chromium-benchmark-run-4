331d96166c1fb43f05665ec24546be602a862cdc
Sean Kau <skau@chromium.org>
2023-06-14 21:44:17 +0000

    IWYU ui/color/color_provider_manager.h
    Add an explicit include for ColorProviderManager where it was
    being retrieved through a transitively (which will be broken
    in a future CL).
    
    This CL was uploaded by git cl split.
    
    R=pkasting@chromium.org
    
    Bug: b:286952053
    Change-Id: I10d57ab7ed8d5cd023e82d559f801e53d49cbecc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611805
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157810}

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/layer.h"
 #include "ui/display/screen.h"
