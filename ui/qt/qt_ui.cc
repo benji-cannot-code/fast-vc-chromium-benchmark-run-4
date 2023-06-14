9fb88ca572dbe7f870f127c0d760f16e9dc997d0
Sean Kau <skau@chromium.org>
2023-06-14 21:01:21 +0000

    IWYU ui/color/color_provider_manager.h
    Add an explicit include for ColorProviderManager where it was
    being retrieved through a transitively (which will be broken
    in a future CL).
    
    This CL was uploaded by git cl split.
    
    R=thomasanderson@chromium.org
    
    Bug: b:286952053
    Change-Id: I73853eb50a236a0192239359262bee07c6c8833e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611733
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157776}

--- ui/qt/qt_ui.cc
+++ ui/qt/qt_ui.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/linux/linux_input_method_context.h"
 #include "ui/color/color_mixer.h"
 #include "ui/color/color_provider.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/color/color_recipe.h"
 #include "ui/color/color_transform.h"
 #include "ui/gfx/color_palette.h"
