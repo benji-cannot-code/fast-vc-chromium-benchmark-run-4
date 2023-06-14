203c3b7115f45a09ec0a78c55b691609667621b1
Erica Lee <ericamlee@google.com>
2023-06-14 22:29:00 +0000

    personalization: dynamic color for login screen
    Added local prefs for dynamic color, so that users will see their color
    preferences on the login screen.
    
    With jelly: https://screencast.googleplex.com/cast/NDg3MDI0OTEzNzU2OTc5MnwxZjljOGE5OS04Yg
    Without jelly: https://screencast.googleplex.com/cast/NjAyMTM5NjY0MDU2MzIwMHxiZTYwZGQxZC0zNg
    
    BUG=b:276475812,b:286519064
    TEST=ash_unittests *ColorPalette*
    
    Change-Id: Ibb41635685f2b7e7bb348a1dbea1399f7ac970ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475678
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157846}

--- chrome/browser/ash/login/ui/login_display_host_mojo.cc
+++ chrome/browser/ash/login/ui/login_display_host_mojo.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/login_screen_model.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
+#include "ash/style/color_palette_controller.h"
 #include "ash/system/model/enterprise_domain_model.h"
 #include "ash/system/model/system_tray_model.h"
 #include "base/command_line.h"
@@ -752,6 +753,7 @@ void LoginDisplayHostMojo::HandleAuthenticateUserWithChallengeResponse(
 void LoginDisplayHostMojo::HandleOnFocusPod(const AccountId& account_id) {
   user_selection_screen_->HandleFocusPod(account_id);
   WallpaperControllerClientImpl::Get()->ShowUserWallpaper(account_id);
+  Shell::Get()->color_palette_controller()->SelectLocalAccount(account_id);
   if (features::IsInputDeviceSettingsSplitEnabled()) {
     InputDeviceSettingsController::Get()->OnLoginScreenFocusedPodChanged(
         account_id);
