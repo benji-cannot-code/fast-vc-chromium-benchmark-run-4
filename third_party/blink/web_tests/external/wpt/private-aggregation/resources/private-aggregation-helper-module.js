5d7d349b95ac114197a9c84537bab3abc3ba389f
Yao Xiao <yaoxia@chromium.org>
2023-06-14 16:35:24 +0000

    [PAA] Add basic API surface / error condition tests from shared storage
    Bug: 1452248
    Change-Id: Ie014ea71d7c0d31b3592ab15697bf27bd88ca41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609684
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157604}

---
+++ third_party/blink/web_tests/external/wpt/private-aggregation/resources/private-aggregation-helper-module.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class ContributeToHistogramOperation {
+  async run(urls, data) {
+    if (data.enableDebugMode) {
+      privateAggregation.enableDebugMode(data.enableDebugModeArgs);
+
+      if (data.enableDebugModeExtraTime) {
+        privateAggregation.enableDebugMode(data.enableDebugModeArgs);
+      }
+    }
+    for (const contribution of data.contributions) {
+      privateAggregation.contributeToHistogram(contribution);
+    }
+
+    // If an error occurs, the default URL will be picked instead.
+    return 1;
+  }
+}
+
+register('contribute-to-histogram', ContributeToHistogramOperation);
---
+++ third_party/blink/web_tests/virtual/private-aggregation/README.md
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Private Aggregation
+
+The tests are run with the flag:
+
+```
+--enable-features=SharedStorageAPI,FencedFrames:implementation_type/mparch,PrivacySandboxAdsAPIsOverride,FencedFramesAPIChanges,FencedFramesDefaultMode,PrivateAggregationApi
+```
+
+You can run these tests by targeting the following directory:
+`virtual/private-aggregation/external/wpt/private-aggregation.
