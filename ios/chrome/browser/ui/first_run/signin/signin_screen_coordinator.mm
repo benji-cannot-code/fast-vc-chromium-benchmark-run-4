85046138f692848b027586277a054e2b8ad7b911
Arthur Milchior <arthurmilchior@chromium.org>
2023-06-14 16:01:51 +0000

    [iOS] SigninScreenCoordinator dealloc checks stop was called
    Bug: None
    Change-Id: I4fca449a1c79dddc9bff52dba026e4fac3b00615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609160
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157574}

--- ios/chrome/browser/ui/first_run/signin/signin_screen_coordinator.mm
+++ ios/chrome/browser/ui/first_run/signin/signin_screen_coordinator.mm
@@ -140,6 +140,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.mediator = nil;
   self.accountManagerService = nil;
   self.authenticationService = nil;
+  [super stop];
+}
+
+- (void)dealloc {
+  CHECK(!self.authenticationService);
 }
 
 #pragma mark - Private
