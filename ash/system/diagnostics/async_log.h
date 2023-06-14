7f28956b5c4f745cab4bfd97aa64ef700540e8ba
ChaobinZhang <zhchbin@gmail.com>
2023-06-14 15:44:35 +0000

    diagnostics: avoid race condition in AsyncLog
    - Change AppendImpl and CreateFile to free functions in anonymous namespace, which prevent accessing member variables from the thread pool.
    - Test that log append task does not crash after log destroyed.
    
    Bug: 1451164, b/286210532
    Test: Ran ash_unittests.
    
    
    
    Change-Id: I2bc798a9a8b7de25dc6c2c4f7dd4b609be6b75ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583920
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157551}

--- ash/system/diagnostics/async_log.h
+++ ash/system/diagnostics/async_log.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/memory/weak_ptr.h"
-#include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
 
 namespace ash {
@@ -38,21 +36,11 @@ class ASH_EXPORT AsyncLog {
       const scoped_refptr<base::SequencedTaskRunner>& task_runner);
 
  private:
-  // Appends to the file. Run on the the task runner.
-  void AppendImpl(const std::string& text);
-
-  // Create the log file. Called on the first write to the file.
-  void CreateFile();
-
   // Path of the log file.
   const base::FilePath file_path_;
 
   // Blockable task runner to enable I/O operations.
   scoped_refptr<base::SequencedTaskRunner> sequenced_task_runner_;
-  SEQUENCE_CHECKER(async_log_checker_);
-
-  // WeakPtr should be destroyed first.
-  base::WeakPtrFactory<AsyncLog> weak_factory_{this};
 };
 
 }  // namespace diagnostics
