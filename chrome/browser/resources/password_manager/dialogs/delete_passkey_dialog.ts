30cd3300a793f7ed0f9b7a0353f0fd9d2cd40f12
Nina Satragno <nsatragno@chromium.org>
2023-06-14 11:41:58 +0000

    [password manager] Add passkey delete dialog
    Add a confirmation dialog when deleting a passkey.
    
    UXW review:
    http://go/passkeys-in-chrome-pwm-uxw
    
    Screenshot:
    https://storage.cloud.google.com/chromium-translation-screenshots/a5de9746332c70d1893b771bb244a4ac93911642
    
    Design doc:
    https://docs.google.com/document/d/1Ox_VBGNO-Ff9bcP97FUMcN15Xtfn_sz0_WuDe3ujbwA
    
    Bug: 1432717
    Change-Id: I9dae66ef65b0d4b4fb9c3e0e8d66b3a5a0b249d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4601909
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157454}

---
+++ chrome/browser/resources/password_manager/dialogs/delete_passkey_dialog.ts
@@ -0,0 +1,82 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import '../shared_style.css.js';
+
+import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import {assert} from 'chrome://resources/js/assert_ts.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {PasswordManagerImpl} from '../password_manager_proxy.js';
+
+import {getTemplate} from './delete_passkey_dialog.html.js';
+
+export interface DeletePasskeyDialogElement {
+  $: {
+    cancelButton: CrButtonElement,
+    dialog: CrDialogElement,
+    deleteButton: CrButtonElement,
+  };
+}
+
+const DeletePasskeyDialogElementBase = I18nMixin(PolymerElement);
+
+export class DeletePasskeyDialogElement extends DeletePasskeyDialogElementBase {
+  static get is() {
+    return 'delete-passkey-dialog';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+
+  static get properties() {
+    return {
+      passkey: Object,
+    };
+  }
+
+  passkey: chrome.passwordsPrivate.PasswordUiEntry;
+
+  override ready() {
+    super.ready();
+    assert(this.passkey.isPasskey);
+  }
+
+  private onCancel_() {
+    this.$.dialog.close();
+  }
+
+  private onDelete_() {
+    PasswordManagerImpl.getInstance().removeCredential(
+        this.passkey.id, this.passkey.storedIn);
+    this.$.dialog.close();
+  }
+
+  private getDescriptionHtml_(): TrustedHTML {
+    // Passkeys have a single https affiliated origin corresponding to the
+    // relying party identifier.
+    assert(this.passkey.affiliatedDomains);
+    const domain = this.passkey.affiliatedDomains[0];
+    assert(domain);
+    return this.i18nAdvanced('deletePasskeyConfirmationDescription', {
+      substitutions:
+          [`<a href='${domain.url}' target='_blank'>${domain.name}</a>`],
+    });
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'delete-passkey-dialog': DeletePasskeyDialogElement;
+  }
+}
+
+customElements.define(
+    DeletePasskeyDialogElement.is, DeletePasskeyDialogElement);
