b0912235da40d4b424987bdd063a8a2d92805b5e
Matt Mueller <mattm@chromium.org>
2023-06-14 00:35:57 +0000

    Fix SSLUITestReduceSubresourceNotifications deleted copy/assignment prototypes
    Change-Id: Ib90f2ee283cd7340d60696561cc2f07810ba9ab9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4612165
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157289}

--- chrome/browser/ssl/ssl_browsertest.cc
+++ chrome/browser/ssl/ssl_browsertest.cc
@@ -1029,6 +1029,7 @@ class SSLUITestHSTS : public SSLUITest {
     ssl_test_util::SetHSTSForHostName(browser()->profile(), kHstsTestHostName);
   }
 };
+
 class SSLUITestReduceSubresourceNotifications : public SSLUITestBase {
  public:
   SSLUITestReduceSubresourceNotifications() {
@@ -1037,8 +1038,10 @@ class SSLUITestReduceSubresourceNotifications : public SSLUITestBase {
         /* disabled_features */ {blink::features::kMixedContentAutoupgrade});
   }
 
-  SSLUITestReduceSubresourceNotifications(const SSLUITest&) = delete;
-  SSLUITestReduceSubresourceNotifications& operator=(const SSLUITest&) = delete;
+  SSLUITestReduceSubresourceNotifications(
+      const SSLUITestReduceSubresourceNotifications&) = delete;
+  SSLUITestReduceSubresourceNotifications& operator=(
+      const SSLUITestReduceSubresourceNotifications&) = delete;
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
