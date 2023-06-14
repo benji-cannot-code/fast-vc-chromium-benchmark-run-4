c1f2492bd7996f2840faed8d1ffcaa1891820095
Luca Accorsi <accorsi@google.com>
2023-06-14 15:53:37 +0000

    dlp: Get ScopedFileAccess while unzipping
    Currently it's not possible to unzip restricted archives since we are
    not acquiring a scoped file access object.
    This CL makes it possible to unzip restricted archives by acquiring
    a scoped file access object before the archive is opened.
    A subsequent CL will take care of copying the source of the archive to
    unzipped files.
    
    High level overview: http://docs/document/d/1B_wJgck9woBCHW1e3I4z9CdwtYWcu1UtaL0d_fcFI18?resourcekey=0-qn4RV7da3HaU7uE827VlQg
    
    Bug: b/285532035
    Change-Id: Ib0da20e4f9e06b90265296469142433b4cbd6546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584029
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Daniel Brinkers <brinky@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157568}

--- components/services/unzip/public/cpp/unzip.cc
+++ components/services/unzip/public/cpp/unzip.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
+#include "components/file_access/scoped_file_access_delegate.h"
 #include "components/services/storage/public/cpp/filesystem/filesystem_impl.h"
 #include "components/services/unzip/public/mojom/unzipper.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -373,7 +374,8 @@ void DoUnzipWithParams(mojo::PendingRemote<mojom::Unzipper> unzipper,
                        UnzipFilterCallback filter_callback,
                        UnzipListenerCallback listener_callback,
                        mojom::UnzipOptionsPtr options,
-                       UnzipCallback result_callback) {
+                       UnzipCallback result_callback,
+                       file_access::ScopedFileAccess file_access) {
   base::File zip_file(zip_path, base::File::FLAG_OPEN | base::File::FLAG_READ);
   if (!CheckZipFileValid(zip_path, zip_file)) {
     std::move(result_callback).Run(false);
@@ -407,14 +409,16 @@ void ZipFileUnpacker::Unpack(mojo::PendingRemote<mojom::Unzipper> unzipper,
                              UnzipCallback result_callback) {
   DCHECK(!result_callback.is_null());
 
-  runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(
-          &::unzip::DoUnzipWithParams, std::move(unzipper), std::ref(params_),
-          zip_file, output_dir, UnzipFilterCallback(),
-          base::BindPostTaskToCurrentDefault(std::move(listener_callback)),
-          std::move(options),
-          base::BindPostTaskToCurrentDefault(std::move(result_callback))));
+  file_access::RequestFilesAccessForSystem(
+      {zip_file},
+      base::BindPostTask(
+          runner_,
+          base::BindOnce(
+              &::unzip::DoUnzipWithParams, std::move(unzipper),
+              std::ref(params_), zip_file, output_dir, UnzipFilterCallback(),
+              base::BindPostTaskToCurrentDefault(std::move(listener_callback)),
+              std::move(options),
+              base::BindPostTaskToCurrentDefault(std::move(result_callback)))));
 }
 
 void ReleaseParams(scoped_refptr<UnzipParams>& unzip_params) {
