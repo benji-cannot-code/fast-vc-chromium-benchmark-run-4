11f2dfdf659b7c4ab3156bc463cf1bfa9065e87c
Marijn Kruisselbrink <mek@chromium.org>
2023-06-14 17:33:08 +0000

    Revert "Handle session state changes for anchored nudges"
    This reverts commit dc7339d548121d5729ebef97963e9c5c1b816a4c.
    
    Reason for revert: Compile errors on https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Builder/54405
    
    Original change's description:
    > Handle session state changes for anchored nudges
    >
    > Close all shown nudges whenever the session state changes.
    >
    > Bug: b:281730880
    > Change-Id: I118a4ad19ca6b729ffc46dd15bf5b4e849baad83
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575097
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1157630}
    
    Bug: b:281730880
    Change-Id: I0194e5b5f6d454e9553a64571a1b19bcbd349539
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616403
    Owners-Override: Marijn Kruisselbrink <mek@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157639}

--- ash/system/toast/anchored_nudge_manager_impl.h
+++ ash/system/toast/anchored_nudge_manager_impl.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/ash_export.h"
-#include "ash/public/cpp/session/session_observer.h"
 #include "ash/public/cpp/system/anchored_nudge_data.h"
 #include "ash/public/cpp/system/anchored_nudge_manager.h"
 #include "ash/system/toast/anchored_nudge.h"
@@ -27,8 +26,7 @@ namespace ash {
 struct AnchoredNudgeData;
 
 // Class managing anchored nudge requests.
-class ASH_EXPORT AnchoredNudgeManagerImpl : public AnchoredNudgeManager,
-                                            public SessionObserver {
+class ASH_EXPORT AnchoredNudgeManagerImpl : public AnchoredNudgeManager {
  public:
   AnchoredNudgeManagerImpl();
   AnchoredNudgeManagerImpl(const AnchoredNudgeManagerImpl&) = delete;
@@ -48,9 +46,6 @@ class ASH_EXPORT AnchoredNudgeManagerImpl : public AnchoredNudgeManager,
 
   // AnchoredNudge::Delegate:
   void OnNudgeHoverStateChanged(const std::string& nudge_id, bool is_hovering);
-
-  // SessionObserver:
-  void OnSessionStateChanged(session_manager::SessionState state) override;
 
   // Returns true if `id` is stored in `shown_nudges_`.
   bool IsNudgeShown(const std::string& id);
