bb7afa0efbbc657a2c2285aa1fa52e01d1089dd4
Jocelyn Tran <jocelyntran@google.com>
2023-06-14 17:26:45 +0000

    [Read Anything] Add UKM for select-to-distill
    Record the total number of times the user "selects-to-distill". This
    number will only be recorded after the user sees the empty state page
    in reading mode, which is shown when (automatic) distillation fails.
    This will help us see how often select-to-distill is being used.
    
    UKM Review Doc:
    https://docs.google.com/document/d/1K0IfhTW7qzcqp4HCF3lny3ETT2gY7CPKeiYPept39So/edit#heading=h.bahq24lmowo2
    
    Related select-to-distill CL for UMA:
    crrev.com/c/4558404
    
    Bug: 1266555
    Change-Id: I272010bdac0eb334dce8ab882cf937e95a2d70b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598037
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157636}

--- chrome/renderer/accessibility/read_anything_app_model.cc
+++ chrome/renderer/accessibility/read_anything_app_model.cc
@@ -9,14 +9,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_functions.h"
+#include "content/public/renderer/render_thread.h"
+#include "services/metrics/public/cpp/mojo_ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "ui/accessibility/ax_enums.mojom-shared.h"
 #include "ui/accessibility/ax_node.h"
 #include "ui/accessibility/ax_role_properties.h"
 #include "ui/accessibility/ax_serializable_tree.h"
 #include "ui/accessibility/ax_tree_update_util.h"
 
-ReadAnythingAppModel::ReadAnythingAppModel() = default;
-ReadAnythingAppModel::~ReadAnythingAppModel() = default;
+ReadAnythingAppModel::ReadAnythingAppModel() {
+  // TODO(crbug.com/1450930): Use a global ukm recorder instance instead.
+  mojo::Remote<ukm::mojom::UkmRecorderFactory> factory;
+  content::RenderThread::Get()->BindHostReceiver(
+      factory.BindNewPipeAndPassReceiver());
+  ukm_recorder_ = ukm::MojoUkmRecorder::Create(*factory);
+}
+
+ReadAnythingAppModel::~ReadAnythingAppModel() {
+  SetActiveUkmSourceId(ukm::kInvalidSourceId);
+}
 
 void ReadAnythingAppModel::OnThemeChanged(
     read_anything::mojom::ReadAnythingThemePtr new_theme) {
@@ -82,6 +94,7 @@ bool ReadAnythingAppModel::PostProcessSelection() {
     base::UmaHistogramEnumeration(
         string_constants::kEmptyStateHistogramName,
         ReadAnythingEmptyState::kSelectionAfterEmptyStateShown);
+    num_selections_++;
   }
 
   // If the main panel selection contains content outside of the distilled
@@ -407,6 +420,19 @@ void ReadAnythingAppModel::OnAXTreeDestroyed(const ui::AXTreeID& tree_id) {
     SetActiveUkmSourceId(ukm::kInvalidSourceId);
   }
   EraseTree(tree_id);
+}
+
+void ReadAnythingAppModel::SetActiveUkmSourceId(ukm::SourceId source_id) {
+  // Record the number of selections made on the current page if it was not
+  // distillable.
+  if (active_ukm_source_id_ != ukm::kInvalidSourceId &&
+      content_node_ids_.empty()) {
+    ukm::builders::Accessibility_ReadAnything_EmptyState(active_ukm_source_id_)
+        .SetTotalNumSelections(num_selections_)
+        .Record(ukm_recorder_.get());
+  }
+  num_selections_ = 0;
+  active_ukm_source_id_ = source_id;
 }
 
 ui::AXNode* ReadAnythingAppModel::GetAXNode(ui::AXNodeID ax_node_id) const {
