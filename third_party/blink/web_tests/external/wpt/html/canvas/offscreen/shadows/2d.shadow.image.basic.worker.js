4ad6441ea0cb39c8dc7276f43e3512ee9f54698b
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-06-14 22:15:46 +0000

    Migrate shadows.yaml to new test generator.
    Bug: 1275750
    Change-Id: Ie18d6f6be578b8711c11a7fad987b7b29e832783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4562936
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157833}

--- third_party/blink/web_tests/external/wpt/html/canvas/offscreen/shadows/2d.shadow.image.basic.worker.js
+++ third_party/blink/web_tests/external/wpt/html/canvas/offscreen/shadows/2d.shadow.image.basic.worker.js
@@ -7,12 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts("/resources/testharness.js");
 importScripts("/html/canvas/resources/canvas-tests.js");
 
-var t = async_test("Shadows are drawn for images");
-var t_pass = t.done.bind(t);
-var t_fail = t.step_func(function(reason) {
-    throw reason;
-});
-t.step(function() {
+promise_test(async t => {
 
   var canvas = new OffscreenCanvas(100, 50);
   var ctx = canvas.getContext('2d');
@@ -21,20 +16,12 @@ t.step(function() {
   ctx.fillRect(0, 0, 100, 50);
   ctx.shadowColor = '#0f0';
   ctx.shadowOffsetY = 50;
-  var promise = new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", '/images/red.png');
-      xhr.responseType = 'blob';
-      xhr.send();
-      xhr.onload = function() {
-          resolve(xhr.response);
-      };
-  });
-  promise.then(function(response) {
-      return createImageBitmap(response).then(bitmap => {
-          ctx.drawImage(bitmap, 0, -50);
-          _assertPixel(canvas, 50,25, 0,255,0,255);
-      });
-  }).then(t_pass, t_fail);
-});
+  var response = await fetch('/images/red.png')
+  var blob = await response.blob();
+  var img = await createImageBitmap(blob);
+  ctx.drawImage(img, 0, -50);
+
+  _assertPixel(canvas, 50,25, 0,255,0,255);
+  t.done();
+}, "Shadows are drawn for images");
 done();
