fe355c9462617e3f03cf72d751834be0fcc0b038
Jun W <jungoog@google.com>
2023-06-14 07:57:50 +0000

    [WebUI] Enable trusted types CSP for GuestOSInstallerUI
    TEST= Naviated to chrome://guest-os-installer and verified it worked.
    
    Bug: 1400799
    Change-Id: Iec85e2fbbb611e9c3ba0ad1e2c23d218bf005bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600473
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Junguo (Jun) W. <jungoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157397}

--- ash/webui/guest_os_installer/guest_os_installer_ui.cc
+++ ash/webui/guest_os_installer/guest_os_installer_ui.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/guest_os_installer/guest_os_installer_ui.h"
 
+#include "ash/webui/common/trusted_types_util.h"
 #include "ash/webui/grit/ash_guest_os_installer_resources.h"
 #include "ash/webui/grit/ash_guest_os_installer_resources_map.h"
 #include "ash/webui/guest_os_installer/url_constants.h"
@@ -19,7 +20,7 @@ GuestOSInstallerUI::GuestOSInstallerUI(content::WebUI* web_ui,
       web_ui->GetWebContents()->GetBrowserContext(),
       ash::kChromeUIGuestOSInstallerHost);
 
-  source->DisableTrustedTypesCSP();
+  ash::EnableTrustedTypesCSP(source);
 
   source->AddResourcePaths(base::make_span(kAshGuestOsInstallerResources,
                                            kAshGuestOsInstallerResourcesSize));
