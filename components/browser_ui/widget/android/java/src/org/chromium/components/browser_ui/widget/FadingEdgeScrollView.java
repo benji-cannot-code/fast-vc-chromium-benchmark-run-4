1d89733afc4391010b8a4e1a76a28ddb79374506
Joel Tan-Aristy <jtanaristy@google.com>
2023-06-14 17:52:37 +0000

    [Clank sharing hub][QR code] Fixes top shadow on QR code dialog
    If the QR code sharing or scanning dialog is scrollable, the header shadow will now appear when scrolled.
    
    Before: https://screenshot.googleplex.com/4Qw9nkeVrmpvT8z
    After: https://photos.app.goo.gl/c4JAvuygWAEDTvJ89
    
    Bug: 1143314
    Change-Id: Id766e9ebe1345213f193f65b1e400532514657a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605837
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157665}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/FadingEdgeScrollView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/FadingEdgeScrollView.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -51,6 +52,8 @@ public class FadingEdgeScrollView extends ScrollView {
 
         mSeparatorColor = getContext().getColor(R.color.toolbar_shadow_color);
         mSeparatorHeight = getResources().getDimensionPixelSize(R.dimen.divider_height);
+
+        if (attrs != null) parseAttributes(attrs);
     }
 
     @Override
@@ -113,5 +116,19 @@ public class FadingEdgeScrollView extends ScrollView {
             int top = getScrollY();
             canvas.drawRect(left, top, right, top + mSeparatorHeight, mSeparatorPaint);
         }
+    }
+
+    private void parseAttributes(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FadingEdgeScrollView);
+
+        if (a.hasValue(R.styleable.FadingEdgeScrollView_topEdgeVisibility)) {
+            mDrawTopEdge =
+                    a.getInt(R.styleable.FadingEdgeScrollView_topEdgeVisibility, EdgeType.FADING);
+        }
+        if (a.hasValue(R.styleable.FadingEdgeScrollView_bottomEdgeVisibility)) {
+            mDrawBottomEdge = a.getInt(
+                    R.styleable.FadingEdgeScrollView_bottomEdgeVisibility, EdgeType.FADING);
+        }
+        a.recycle();
     }
 }
