b1ac6d414c5c7aa716a5ee4bc6ab02eca07afd97
hmare <hmare@google.com>
2023-06-14 15:58:00 +0000

    [DTC-BYOD] Enable profile inline flow feature by default
    Bug: b/282143849
    Change-Id: I9ee8aa658f9bc371107747f9fcb30665309f3b54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616003
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1157571}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_features.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_features.cc
@@ -13,7 +13,7 @@ BASE_FEATURE(kDeviceTrustConnectorEnabled,
 
 BASE_FEATURE(kUserDTCInlineFlowEnabled,
              "UserDTCInlineFlowEnabled",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsDeviceTrustConnectorFeatureEnabled() {
   return base::FeatureList::IsEnabled(kDeviceTrustConnectorEnabled);
