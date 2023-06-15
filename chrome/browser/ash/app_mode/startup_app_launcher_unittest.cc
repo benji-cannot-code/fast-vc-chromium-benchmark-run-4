e2c0da7736b9cc23cab80fd5d807afe371797dcb
Hidehiko Abe <hidehiko@chromium.org>
2023-06-15 11:47:42 +0000

    Use LacrosOnly for StartupAppLauncher tests.
    The tests are using LacrosPrimary, which is going to be removed.
    This CL replaces it by LacrosOnly.
    
    BUG=1448575
    TEST=Tryjob
    
    Change-Id: Ieb831af54977559e93ed34d808618486376716cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4610185
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158082}

--- chrome/browser/ash/app_mode/startup_app_launcher_unittest.cc
+++ chrome/browser/ash/app_mode/startup_app_launcher_unittest.cc
@@ -1510,6 +1510,8 @@ class StartupAppLauncherUsingLacrosTest : public testing::Test {
         scoped_user_manager_(base::WrapUnique(fake_user_manager_.get())) {
     scoped_feature_list_.InitWithFeatures(
         {ash::features::kLacrosSupport, ash::features::kLacrosPrimary,
+         ash::features::kLacrosOnly,
+         ash::features::kLacrosProfileMigrationForceOff,
          ::features::kChromeKioskEnableLacros},
         {});
   }
