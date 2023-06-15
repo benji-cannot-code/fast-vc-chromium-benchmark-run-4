bb9853eae8c66deb56c34eb2b02eb60c680fc612
Joey Arhar <jarhar@chromium.org>
2023-06-15 17:30:06 +0000

    Revert "[omnibox] Split WebUI implementation out from OmniboxPopupViewViews"
    This reverts commit e3cd8b9ff33f6c670d4a91f54c81ab0c3a486ab7.
    
    Reason for revert:
    The newly added tests have been failing on several different lsan/msan/asan bots, and I was able to reproduce it locally. Here is an example: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-asan-lsan-rel/5245/overview
    
    Original change's description:
    > [omnibox] Split WebUI implementation out from OmniboxPopupViewViews
    >
    > This CL refactors OmniboxPopupViewViews to make OmniboxPopupViewWebUI
    > a fully separate subclass, eliminating all reference to "webui" except
    > for some safety checks which can be eliminated after the new class
    > is fully settled.
    >
    > In addition, this CL takes a few opportunities for code cleanup and
    > simplification. For example, method `GetSelectedResultView` was removed
    > because it had only one call site so inlining was cleaner than creating
    > a separate implementation to return nullptr in OmniboxPopupViewWebUI.
    >
    > Bug: 1445142
    > Change-Id: I6d37efc8b21ea36e6df3f897a1f732bcbea2d476
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602452
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Commit-Queue: Orin Jaworski <orinj@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1157941}
    
    Bug: 1445142
    Change-Id: I54e8cbad1821c5b7994c858223fed7c4b2149ecd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618013
    Reviewed-by: Joey Arhar <jarhar@google.com>
    Commit-Queue: Joey Arhar <jarhar@google.com>
    Owners-Override: Joey Arhar <jarhar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158275}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_view_views.h"
-#include "chrome/browser/ui/views/omnibox/omnibox_popup_view_webui.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_result_view.h"
 #include "chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_bubble_controller.h"
 #include "chrome/grit/generated_resources.h"
