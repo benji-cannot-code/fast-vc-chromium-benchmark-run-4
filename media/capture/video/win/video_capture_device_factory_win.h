ee32e97bbeb8685f824cf75bd7dd360825b72bd3
Ilya Nikolaevskiy <ilnik@chromium.org>
2023-06-15 09:04:01 +0000

    Add feature to control the D3D11 video capture blocklist for testing
    Bug: 1120900, 1430048
    Change-Id: I557d84fb3cbf0731ea346ac707e1ec4a66e501dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615004
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158036}

--- media/capture/video/win/video_capture_device_factory_win.h
+++ media/capture/video/win/video_capture_device_factory_win.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.devices.enumeration.h>
 #include <wrl.h>
 
+#include "base/feature_list.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/single_thread_task_runner.h"
@@ -24,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/video_capture_device_factory.h"
 
 namespace media {
+
+CAPTURE_EXPORT BASE_DECLARE_FEATURE(kMediaFoundationD3D11VideoCaptureBlocklist);
 
 using ABI::Windows::Foundation::IAsyncOperation;
 using ABI::Windows::Devices::Enumeration::DeviceInformationCollection;
