14da6f5ba1f4ebc0ae3b8a57f75de0b8f9650aae
Rushan Suleymanov <rushans@google.com>
2023-06-15 17:12:25 +0000

    [Sync] Add more details for waiting for updates timeout histogram
    This CL replaces waiting for updates timeout histogram with a new one
    broken down by a reason why the data type is waiting for updates.
    
    Bug: 1425026
    Change-Id: I771b6b6407612cbca9dc5b405d09d24daaceaa98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615796
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Quick-Run: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158255}

--- components/sync/service/sync_service_impl.h
+++ components/sync/service/sync_service_impl.h
@@ -211,6 +211,9 @@ class SyncServiceImpl : public SyncService,
   // once (before this object is destroyed).
   void Shutdown() override;
 
+  // Records the reason if the `type` is waiting for updates to be downloaded.
+  void RecordReasonIfWaitingForUpdates(ModelType type);
+
   // Returns whether or not the underlying sync engine has made any
   // local changes to items that have not yet been synced with the
   // server.
@@ -272,7 +275,7 @@ class SyncServiceImpl : public SyncService,
   // Records UMA histograms related to download status during browser startup.
   class DownloadStatusRecorder : public SyncServiceObserver {
    public:
-    DownloadStatusRecorder(SyncService* sync_service,
+    DownloadStatusRecorder(SyncServiceImpl* sync_service,
                            base::OnceClosure on_finished_callback,
                            ModelTypeSet data_types_to_track);
     DownloadStatusRecorder(const DownloadStatusRecorder&) = delete;
@@ -286,7 +289,7 @@ class SyncServiceImpl : public SyncService,
    private:
     void OnTimeout();
 
-    raw_ptr<SyncService> sync_service_ = nullptr;
+    raw_ptr<SyncServiceImpl> sync_service_ = nullptr;
 
     // Set on browser startup to report metrics related to sync configuration.
     base::OneShotTimer startup_metrics_timer_;
@@ -394,6 +397,13 @@ class SyncServiceImpl : public SyncService,
 
   // Clean up download status recorder.
   void OnDownloadStatusRecorderFinished();
+
+  // Returns current download status for `type`. Records a histogram if the data
+  // type is waiting for updates and `record_waiting_for_updates_metrics` is set
+  // to true.
+  ModelTypeDownloadStatus GetDownloadStatusForImpl(
+      ModelType type,
+      bool record_waiting_for_updates_metrics) const;
 
   // This profile's SyncClient, which abstracts away non-Sync dependencies and
   // the Sync API component factory.
