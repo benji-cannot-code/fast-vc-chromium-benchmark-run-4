b0a5fbb5c93ce90287a8f8a7f5038f1ad058cc28
Alex Kingsborough <akingsb@google.com>
2023-06-15 20:26:48 +0000

    [NP] Add changed/lost functionality in handler
    Add functionality for nearby presence devices being changed and lost in
    the nearby presence handler and the UI.
    
    implementations so I tested this by manually calling the functions
    with fake devices and observing the behavior on my DUT.
    
    Test: ble_v2_medium currently does not have changed/lost
    Change-Id: Ic0c52a51ee58788301f137d1eb165cf776e4feca
    Bug: b/277820435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598594
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158389}

--- chrome/browser/ui/webui/nearby_internals/nearby_internals_ui_presence_handler.cc
+++ chrome/browser/ui/webui/nearby_internals/nearby_internals_ui_presence_handler.cc
@@ -139,11 +139,17 @@ void NearbyInternalsPresenceHandler::OnPresenceDeviceFound(
 
 void NearbyInternalsPresenceHandler::OnPresenceDeviceChanged(
     const ash::nearby::presence::NearbyPresenceService::PresenceDevice&
-        presence_device) {}
+        presence_device) {
+  FireWebUIListener("presence-device-changed",
+                    PresenceDeviceToDictionary(presence_device));
+}
 
 void NearbyInternalsPresenceHandler::OnPresenceDeviceLost(
     const ash::nearby::presence::NearbyPresenceService::PresenceDevice&
-        presence_device) {}
+        presence_device) {
+  FireWebUIListener("presence-device-lost",
+                    PresenceDeviceToDictionary(presence_device));
+}
 
 void NearbyInternalsPresenceHandler::OnScanSessionInvalidated() {
   scan_session_.reset();
