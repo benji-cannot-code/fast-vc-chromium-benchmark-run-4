9bf607e6fe1784dd07b233fd3a336e4e0cb375a3
Damien Dejean <damiendejean@google.com>
2023-06-15 17:51:22 +0000

    [CrOS settings] add messages to Passpoint removal dialog.
    Internet details page shows a dialog to inform the user about the
    removal of a Passpoint subscription. The messages and the behavior are
    different depending on the release phase of the feature. This CL creates
    a dialog element that handles the different messages and allow to
    implement the two behaviors.
    
    Screenshots:
     - https://screenshot.googleplex.com/49bEWcb6d3U6jyk.png
     - https://screenshot.googleplex.com/6AhvEguAAiHJkeU.png
    
    	--gtest_filter="OSSettingsInternetPagePasspointRemoveDialogTest.*"
          browser_test \
          	--gtest_filter="OSSettingsInternetPageInternetDetailSubpageTest.*"
    
    Bug: b:280784916
    Test: browser_test \
    Change-Id: I76930b217b1f2bc4da4304a880ad5b2ce5548e99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4603938
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Damien Dejean <damiendejean@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158299}

--- chrome/browser/resources/settings/chromeos/internet_page/internet_detail_subpage.ts
+++ chrome/browser/resources/settings/chromeos/internet_page/internet_detail_subpage.ts
@@ -34,6 +34,7 @@ import '/shared/settings/controls/settings_toggle_button.js';
 import './cellular_roaming_toggle_button.js';
 import './internet_shared.css.js';
 import './network_proxy_section.js';
+import './passpoint_remove_dialog.js';
 import './settings_traffic_counters.js';
 import './tether_connection_dialog.js';
 
@@ -65,6 +66,7 @@ import {Route, Router, routes} from '../router.js';
 
 import {getTemplate} from './internet_detail_subpage.html.js';
 import {InternetPageBrowserProxy, InternetPageBrowserProxyImpl} from './internet_page_browser_proxy.js';
+import {PasspointRemoveDialogElement} from './passpoint_remove_dialog.js';
 import {TetherConnectionDialogElement} from './tether_connection_dialog.js';
 
 const SettingsInternetDetailPageElementBase =
@@ -1509,9 +1511,10 @@ class SettingsInternetDetailPageElement extends
             '#tetherDialog'));
   }
 
-  private getPasspointRemovalDialog_(): HTMLDialogElement {
-    return castExists(this.shadowRoot!.querySelector<HTMLDialogElement>(
-        '#passpointRemovalDialog'));
+  private getPasspointRemovalDialog_(): PasspointRemoveDialogElement {
+    return castExists(
+        this.shadowRoot!.querySelector<PasspointRemoveDialogElement>(
+            '#passpointRemovalDialog'));
   }
 
   private handleConnectClick_(): void {
@@ -1607,7 +1610,7 @@ class SettingsInternetDetailPageElement extends
     if (this.isPasspointWifi_(this.managedProperties_)) {
       // Ask user confirmation before removing a Passpoint Wi-Fi and the
       // associated subscription.
-      this.getPasspointRemovalDialog_().showModal();
+      this.getPasspointRemovalDialog_().open();
       return;
     }
     return this.forgetNetwork_();
@@ -2191,12 +2194,16 @@ class SettingsInternetDetailPageElement extends
     this.dispatchEvent(showPasspointEvent);
   }
 
-  private onPasspointRemovalDialogCancel_(): void {
-    this.getPasspointRemovalDialog_().close();
-  }
-
   private onPasspointRemovalDialogConfirm_(): void {
     this.getPasspointRemovalDialog_().close();
+
+    if (this.isPasspointSettingsEnabled_) {
+      // When Passpoint settings page is enabled, the removal dialog leads the
+      // user to the subscription page.
+      this.onPasspointRowClicked_();
+      return;
+    }
+
     this.forgetNetwork_();
   }
 
