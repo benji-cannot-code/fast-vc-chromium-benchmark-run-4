4b3fe9b0c87eaf3bb52e0d1cb066fa0710074aa9
James Lee <ljjlee@google.com>
2023-06-15 15:47:11 +0000

    Clean up child history deletion code
    This is no longer needed, now that supervised users are not prevented
    from deleting their history directly within Chrome.
    
    Bug: b/251192695
    Change-Id: I18c615b2130e4cc07c56fa6a90e96c129f14d5fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604310
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158196}

--- chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_dialog.ts
+++ chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_dialog.ts
@@ -136,14 +136,6 @@ export class SettingsClearBrowsingDataDialogElement extends
         value: false,
       },
 
-      isChildAccountWithDisallowedHistoryDeletion_: {
-        type: Boolean,
-        value() {
-          return loadTimeData.getBoolean('isChildAccount') &&
-                 !loadTimeData.getBoolean('allowDeletingBrowserHistory');
-        },
-      },
-
       showHistoryDeletionDialog_: {
         type: Boolean,
         value: false,
@@ -227,7 +219,6 @@ export class SettingsClearBrowsingDataDialogElement extends
   private clearingInProgress_: boolean;
   private clearingDataAlertString_: string;
   private clearButtonDisabled_: boolean;
-  private isChildAccountWithDisallowedHistoryDeletion_: boolean;
   private showHistoryDeletionDialog_: boolean;
   private showPasswordsDeletionDialogLater_: boolean;
   private showPasswordsDeletionDialog_: boolean;
