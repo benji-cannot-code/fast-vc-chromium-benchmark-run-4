393072a9ac7f9758785cbe6cdf8233676b3a69d5
Peter Kvitek <kvitekp@chromium.org>
2023-06-15 15:27:14 +0000

    [headless] Don't show <select> popup on Macs in headless mode
    HTML <select> popup menus on Macs are implemented natively, so
    they show up when running in headless mode. This CL checks if
    the browser window is headless and prevents showing popup
    menus if so.
    
    Bug: 1446277
    Change-Id: I3084a306f3f0377c8c9a58ce530414b5bac989fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617044
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158161}

--- content/browser/renderer_host/popup_menu_helper_mac.mm
+++ content/browser/renderer_host/popup_menu_helper_mac.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/scoped_sending_event.h"
 #import "base/message_loop/message_pump_mac.h"
 #include "base/task/current_thread.h"
+#import "components/remote_cocoa/app_shim/native_widget_mac_nswindow.h"
 #import "content/app_shim_remote_cocoa/render_widget_host_view_cocoa.h"
 #include "content/browser/renderer_host/frame_tree.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
@@ -67,6 +68,14 @@ void PopupMenuHelper::ShowPopupMenu(
   RenderWidgetHostViewMac* rwhvm = GetRenderWidgetHostView();
   base::scoped_nsobject<RenderWidgetHostViewCocoa> cocoa_view(
       [rwhvm->GetInProcessNSView() retain]);
+
+  // Check if the underlying native window is headless and if so, return early
+  // to avoid showing the popup menu.
+  NativeWidgetMacNSWindow* ns_window =
+      base::mac::ObjCCastStrict<NativeWidgetMacNSWindow>([cocoa_view window]);
+  if (ns_window && [ns_window isHeadless]) {
+    return;
+  }
 
   // Display the menu.
   base::scoped_nsobject<WebMenuRunner> runner([[WebMenuRunner alloc]
