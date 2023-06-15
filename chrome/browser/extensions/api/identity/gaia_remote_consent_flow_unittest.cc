ecc8e024dfd8cb0e3c240053b3acd6a8764cd409
Ryan Sultanem <rsult@google.com>
2023-06-15 13:34:48 +0000

    [WebAuthFlow] Propagate user gesture from extension information
    Follow up on https://crrev.com/c/4542942.
    
    Bug: 1445824
    Change-Id: I166f68024efebf88bfbaa42da6f67d4ee8d397bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4613787
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158120}

--- chrome/browser/extensions/api/identity/gaia_remote_consent_flow_unittest.cc
+++ chrome/browser/extensions/api/identity/gaia_remote_consent_flow_unittest.cc
@@ -34,7 +34,8 @@ class FakeWebAuthFlowWithWindowKey : public WebAuthFlow {
                     nullptr,
                     GURL(),
                     WebAuthFlow::INTERACTIVE,
-                    WebAuthFlow::GET_AUTH_TOKEN),
+                    WebAuthFlow::GET_AUTH_TOKEN,
+                    /*user_gesture=*/true),
         fake_window_key_(window_key) {}
 
   ~FakeWebAuthFlowWithWindowKey() override = default;
@@ -55,7 +56,11 @@ class TestGaiaRemoteConsentFlow : public GaiaRemoteConsentFlow {
                             const ExtensionTokenKey& token_key,
                             const RemoteConsentResolutionData& resolution_data,
                             const std::string& window_key)
-      : GaiaRemoteConsentFlow(delegate, nullptr, token_key, resolution_data),
+      : GaiaRemoteConsentFlow(delegate,
+                              nullptr,
+                              token_key,
+                              resolution_data,
+                              /*user_gesture=*/true),
         window_key_(window_key) {
     SetWebAuthFlowForTesting(
         std::make_unique<FakeWebAuthFlowWithWindowKey>(this, window_key_));
