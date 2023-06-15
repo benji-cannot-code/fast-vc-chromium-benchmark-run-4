f8b5583d6455325157229a9f28d7512ca5ffc96f
David Black <dmblack@google.com>
2023-06-15 16:53:19 +0000

    Update progress indicator colors for Jelly.
    When Jelly is enabled, shelf trays have an accent-color background
    when active. The accent-color background causes progress indicators
    to be illegible since they are also accent-colored.
    
    This CL updates colors when Jelly is enabled per spec linked in the
    bug. When shelf trays are active, progress indicator colors are updated
    to ensure contrast.
    
    Before: http://shortn/_TSETDLInV1
    After: http://shortn/_RdQLP5h2js
    
    Bug: b:287151663
    Change-Id: I24c3d79e11562d82fffb992877eb4ba3f7622843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617387
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158238}

--- ash/system/holding_space/holding_space_tray_icon_preview.h
+++ ash/system/holding_space/holding_space_tray_icon_preview.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
+#include "ui/color/color_provider.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/compositor/layer_delegate.h"
 #include "ui/views/view.h"
@@ -125,6 +126,9 @@ class ASH_EXPORT HoldingSpaceTrayIconPreview
   // The given `vector_2df` should specify the desired value for horizontal
   // alignment in LTR and will be adjusted for vertical alignment and/or RTL.
   void AdjustForShelfAlignmentAndTextDirection(gfx::Vector2dF* vector_2df);
+
+  // Returns the color resolved for the specified `color_id`.
+  SkColor GetColor(ui::ColorId color_id) const;
 
   // The shelf whose holding space tray icon this preview belongs.
   const raw_ptr<Shelf, ExperimentalAsh> shelf_;
