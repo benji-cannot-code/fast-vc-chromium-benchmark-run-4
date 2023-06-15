828b659f0ef69eb3591aaf0a38231560f4403301
Gang Wu <gangwu@chromium.org>
2023-06-15 17:25:18 +0000

    Create AuxiliarySearchHooks
    This CL is the first step to split auxiliary search out of chrome.
    After internal repro finished, AuxiliarySearchControllerFactory will
    hook with AuxiliarySearchHooks, and delete
    AuxiliarySearchControllerImpl.
    
    Change-Id: I827b8fc73e7544fa0be3a27fe1ee9acf9071450a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617373
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1158271}

--- chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchController.java
+++ chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchController.java
@@ -23,4 +23,7 @@ public interface AuxiliarySearchController extends PauseResumeWithNativeObserver
 
     @Override
     default void onPauseWithNative() {}
+
+    /** Destroy and unhook objects at destruction. */
+    default void destroy() {}
 }
---
+++ chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchHooks.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.auxiliary_search;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+
+/**
+ * Provides access to internal AuxiliarySearch implementation parts, if they are available.
+ */
+public interface AuxiliarySearchHooks {
+    /** Whether the internal components of the Auxiliary Search are available.*/
+    default boolean isEnabled() {
+        return false;
+    }
+
+    /** Create a AuxiliarySearchController. */
+    default @Nullable AuxiliarySearchController createAuxiliarySearchController(
+            Profile profile, TabModelSelector tabModelSelector) {
+        return null;
+    }
+}
