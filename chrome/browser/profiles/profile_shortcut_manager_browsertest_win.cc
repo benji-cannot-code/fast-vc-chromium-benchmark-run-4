6d9f4e309695ebf45d97ed92378c5005a6c65ae9
David Bienvenu <davidbienvenu@chromium.org>
2023-06-15 21:33:24 +0000

    win: Cleanup desktop icon in UpdateProfileIconOnAvatarLoaded
    ProfileShortcutManagerBrowserTest.UpdateProfileIconOnAvatarLoaded leaves
    a new shortcut on Windows desktop every time it runs. This CL uses
    ScopedPathOverride(base::DIR_USER_DESKTOP) to create the shortcut
    in a temp dir instead.
    
    Bug: 1454297
    Change-Id: I585baaa3ad540908dcef8727f06666bc32aa3d6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611265
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158432}

--- chrome/browser/profiles/profile_shortcut_manager_browsertest_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_browsertest_win.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/base_paths.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_path_override.h"
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
@@ -94,6 +96,8 @@ IN_PROC_BROWSER_TEST_F(ProfileShortcutManagerBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(ProfileShortcutManagerBrowserTest,
                        UpdateProfileIconOnAvatarLoaded) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  base::ScopedPathOverride desktop_override(base::DIR_USER_DESKTOP);
   std::string badged_icon;
   EXPECT_NO_FATAL_FAILURE(badged_icon = ReadProfileIcon());
 
