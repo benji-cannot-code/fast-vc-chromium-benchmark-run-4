6ccc4cef9cf767eaa53877c8cc4a65a171556b87
Vasiliy Telezhnikov <vasilyt@chromium.org>
2023-06-15 20:50:36 +0000

    Tidy-up color buffer handling for default offscreen framebuffer
    The only reachable code was just creating texture and attaching it to
    fbo. This CL cleanup the rest.
    
    Follow-up will stop creating fbo completely, but that requires feature
    flag for safety.
    
    Bug: 1445523
    Change-Id: Ic8adaee90d6e904ea8f0ee945be5a2080b02e518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616405
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158405}

--- gpu/command_buffer/service/gles2_cmd_decoder.h
+++ gpu/command_buffer/service/gles2_cmd_decoder.h
@@ -31,10 +31,6 @@ namespace gl {
 class GLSurface;
 }
 
-namespace gfx {
-class Size;
-}
-
 namespace gpu {
 
 class DecoderClient;
@@ -164,9 +160,6 @@ class GPU_GLES2_EXPORT GLES2Decoder : public CommonDecoder,
                                                 bool preserve,
                                                 bool needs_depth,
                                                 bool needs_stencil) = 0;
-
-  // Resize an offscreen frame buffer.
-  virtual bool ResizeOffscreenFramebuffer(const gfx::Size& size) = 0;
 
   // Gets the GLES2 Util which holds info.
   virtual GLES2Util* GetGLES2Util() = 0;
