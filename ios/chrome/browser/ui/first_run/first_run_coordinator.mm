9c1faf799e8a9c20cde714a551418f08addf4f8f
Arthur Milchior <arthurmilchior@chromium.org>
2023-06-15 13:11:41 +0000

    [iOS] FirstRunCoordinator dealloc checks stop was called
    Bug: None
    Change-Id: I5ef0d6df5a957309d398b1e440c41bfad33ab241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609120
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158114}

--- ios/chrome/browser/ui/first_run/first_run_coordinator.mm
+++ ios/chrome/browser/ui/first_run/first_run_coordinator.mm
@@ -86,6 +86,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self.baseViewController dismissViewControllerAnimated:YES
                                               completion:completion];
+  _navigationController = nil;
+  [super stop];
+}
+
+- (void)dealloc {
+  CHECK(!_navigationController);
 }
 
 #pragma mark - FirstRunScreenDelegate
