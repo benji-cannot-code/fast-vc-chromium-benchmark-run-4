7da39bd6b79751d37d349241983a39b14aa318d1
Geoff Huang <geoffhuang@google.com>
2023-06-15 20:49:47 +0000

    [Fixit] Disable portrait orientation tests for automotive
    Automotive devices currently do not handle  Activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) properly: https://hsv.googleplex.com/5435081880502272
    
    Bug: b/287350212
    Change-Id: Id5914b8bc5b20e7ec315b23ae5f6ca020ea069d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617333
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Geoff Huang <geoffhuang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158404}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentRenderTest.java
@@ -37,6 +37,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.firstrun.FirstRunPageDelegate;
 import org.chromium.chrome.browser.firstrun.PolicyLoadListener;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -56,6 +57,7 @@ import org.chromium.components.externalauth.ExternalAuthUtils;
 import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.test.util.BlankUiTestActivityTestCase;
+import org.chromium.ui.test.util.DeviceRestriction;
 import org.chromium.ui.test.util.RenderTestRule;
 import org.chromium.ui.test.util.ViewUtils;
 
@@ -67,6 +69,7 @@ import java.util.List;
 @RunWith(ParameterizedRunner.class)
 @ParameterAnnotations.UseRunnerDelegate(ChromeJUnit4RunnerDelegate.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO)
 @DoNotBatch(reason = "Relies on global state")
 public class SigninFirstRunFragmentRenderTest extends BlankUiTestActivityTestCase {
     /** Parameter provider for night mode state and device orientation. */
