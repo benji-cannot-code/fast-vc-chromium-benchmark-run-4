874912b258355ab5f77ed7331e3202173fa2f3b0
Henrique Nakashima <hnakashima@chromium.org>
2023-06-15 19:57:00 +0000

    [Android] Re-enable permissions tests
    - GeolocationTest#testGeolocationPlumbingAllowedDialog
    - GeolocationTest#testGeolocationPlumbingAllowedDialogNoGesture
    - GeolocationTest#testGeolocationWatchDialog
    - MediaTest#testCameraPermissionsPlumbingDialog
    - MediaTest#testCombinedPermissionsPlumbingDialog
    - MediaTest#testMicrophoneMediaPermissionsPlumbingDialog
    - NotificationTest#testNotificationDialog
    - RuntimePermissionTest#testAllowRuntimeCameraIncognito
    - RuntimePermissionTest#testAllowRuntimeMicrophoneIncognito
    
    Flakiness fixed by
    https://chromium-review.googlesource.com/c/chromium/src/+/4605840
    and fixes confirmed on Reviver bots.
    
    Bug: 1444083,1441822,1435426,1444217,1444241,1444222,1449975
    Change-Id: I71bf5ad3e3e6d0a5441270e51c8b94155cdd939f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618809
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158372}

--- chrome/android/javatests/src/org/chromium/chrome/browser/permissions/RuntimePermissionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/permissions/RuntimePermissionTest.java
@@ -19,7 +19,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.download.DownloadItem;
 import org.chromium.chrome.browser.download.DownloadManagerService;
@@ -298,7 +297,6 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "MediaPermissions"})
     @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
-    @DisabledTest(message = "crbug.com/1444217")
     public void testAllowRuntimeCameraIncognito() throws Exception {
         mPermissionTestRule.newIncognitoTabFromMenu();
 
@@ -316,7 +314,6 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "MediaPermissions"})
     @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
-    @DisabledTest(message = "crbug.com/1444217")
     public void testAllowRuntimeMicrophoneIncognito() throws Exception {
         mPermissionTestRule.newIncognitoTabFromMenu();
         String[] requestablePermission = new String[] {Manifest.permission.RECORD_AUDIO};
