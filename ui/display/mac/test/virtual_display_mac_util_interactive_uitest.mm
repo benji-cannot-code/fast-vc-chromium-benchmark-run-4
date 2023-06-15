dea8e8cbedeb5085cdb67ff44fb0b2e1d9004187
Avi Drissman <avi@chromium.org>
2023-06-15 18:52:43 +0000

    Convert /ui/display:display_interactive_ui_tests to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: Ie24e0df4f1ee0bbc814e5873577197ad7d1ec243
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609326
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158339}

--- ui/display/mac/test/virtual_display_mac_util_interactive_uitest.mm
+++ ui/display/mac/test/virtual_display_mac_util_interactive_uitest.mm
@@ -10,14 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/types/display_constants.h"
 #include "ui/gfx/geometry/size.h"
 
-class VirtualDisplayMacUtilInteractiveUitest : public testing::Test {
- protected:
-  VirtualDisplayMacUtilInteractiveUitest() = default;
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
+class VirtualDisplayMacUtilInteractiveUitest : public testing::Test {
+ public:
   VirtualDisplayMacUtilInteractiveUitest(
       const VirtualDisplayMacUtilInteractiveUitest&) = delete;
   VirtualDisplayMacUtilInteractiveUitest& operator=(
       const VirtualDisplayMacUtilInteractiveUitest&) = delete;
+
+ protected:
+  VirtualDisplayMacUtilInteractiveUitest() = default;
 
   void SetUp() override {
     if (!display::test::VirtualDisplayMacUtil::IsAPIAvailable()) {
