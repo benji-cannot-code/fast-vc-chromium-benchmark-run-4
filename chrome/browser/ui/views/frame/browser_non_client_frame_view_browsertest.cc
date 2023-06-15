d8c31c069b399c6055d1bbd2df5514603036bd5c
Keren Zhu <kerenzhu@chromium.org>
2023-06-15 21:30:17 +0000

    [CR2023] Fix BrowserNonClientFrameViewBrowserTest.IncognitoIsCorrectColor
    The frame color changes in CR2023 and causes this test to fail because
    it was expecting an hardcoded color. Fix it by expecting a black-ish
    frame color instead.
    
    Bug: 1455104
    Change-Id: I3cf2a5f2a2f31b7ee71de20bc21c4bee8da2507f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617539
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158429}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_browsertest.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/manifest/manifest.mojom.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
+#include "ui/gfx/color_utils.h"
 
 namespace {
 
@@ -228,10 +229,11 @@ IN_PROC_BROWSER_TEST_F(BrowserNonClientFrameViewBrowserTest,
   BrowserFrame* frame = view->frame();
   BrowserNonClientFrameView* frame_view = frame->GetFrameView();
 
-  // Checking the exact color is brittle but there's no better way to ensure
-  // that it's not overridden by accident.
-  EXPECT_EQ(gfx::kGoogleGrey900,
-            frame_view->GetFrameColor(BrowserFrameActiveState::kActive));
+  color_utils::HSL frame_color_hsl;
+  SkColorToHSL(frame_view->GetFrameColor(BrowserFrameActiveState::kActive),
+               &frame_color_hsl);
+  // Ensure that the frame color is very dark in Incognito.
+  EXPECT_LT(frame_color_hsl.l, 0.2);
 
   incognito_browser->window()->Close();
 }
