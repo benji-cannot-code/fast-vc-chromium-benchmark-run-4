ab1964d1036fc0e6428e82ef72dd78b76e9d51cb
Oleg Davydov <burunduk@chromium.org>
2023-06-15 15:43:47 +0000

    [CSE] Make the feature flag accessible in chrome://flags
    The new functionality of showing the Google Drive CSE files is hidden
    under the “DriveFsShowCSEFiles” feature flags. After this change this
    flag is available for the users in chrome://flags.
    Note that changing the flag is not the only requirement for the feature
    to work, it should also be enabled from the DriveFs side, which is
    going to be done later.
    
    Bug: b:277890245
    Change-Id: If8b3b27465add2c3e89c5d59d7c3a158f2803959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615073
    Reviewed-by: Steffen Seckler <sseckler@google.com>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158190}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3254,6 +3254,9 @@ extern const char kQuickSettingsPWANotificationsDescription[];
 extern const char kDriveFsChromeNetworkingName[];
 extern const char kDriveFsChromeNetworkingDescription[];
 
+extern const char kDriveFsShowCSEFilesName[];
+extern const char kDriveFsShowCSEFilesDescription[];
+
 extern const char kEnableAssistantRoutinesName[];
 extern const char kEnableAssistantRoutinesDescription[];
 
