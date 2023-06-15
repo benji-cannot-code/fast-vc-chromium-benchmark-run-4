cf2b7bb02b4fdabae78a12265e15495cc056397f
Tarun Bansal <tbansal@google.com>
2023-06-15 16:17:31 +0000

    [CSC] Show appropriate sign-in promo on auth errors
    Before we were triggering sign-in promo in all cases. This cl changes it to trigger fix-your-auth promo when the auth is failing.
    
    The CL also makes companion page handler listen for events to auth issues to refresh csc.
    
    Bug: b/286401449
    Change-Id: I884fa3e0069a3aa620cb67ede8d8121c1a6499e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618010
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158214}

--- chrome/browser/ui/webui/side_panel/companion/companion_page_handler.cc
+++ chrome/browser/ui/webui/side_panel/companion/companion_page_handler.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/unified_consent/unified_consent_service.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
 #include "content/public/browser/web_contents.h"
+#include "google_apis/gaia/google_service_auth_error.h"
 #include "net/base/url_util.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "url/gurl.h"
@@ -90,6 +91,12 @@ void CompanionPageHandler::OnPrimaryAccountChanged(
     return;
   }
 
+  NotifyURLChanged(/*is_full_reload=*/true);
+}
+
+void CompanionPageHandler::OnErrorStateOfRefreshTokenUpdatedForAccount(
+    const CoreAccountInfo& account_info,
+    const GoogleServiceAuthError& error) {
   NotifyURLChanged(/*is_full_reload=*/true);
 }
 
