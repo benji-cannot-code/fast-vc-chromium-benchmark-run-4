e1d54cd77bd5e9e9f4e599af2da98a26390e743e
Vipul Vinod Koul <koulvipul@google.com>
2023-06-15 07:27:49 +0000

    Adding a syncable profile pref for CVC storage and filling.
    This value will be used to keep track of user preference on storing and autofilling of CVCs.
    
    The default value of the pref is set to true/enabled.
    
    Design doc- go/pay-autofill-cvv-storage-chrome
    
    Documentation used for reference to add prefs- go/chrome-prefs-doc
    
    Bug: 1450363
    Change-Id: I7ed9542821403b3edd5a135580ec552c35f64785
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569476
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Sujie Zhu <sujiezhu@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158002}

--- components/autofill/core/common/autofill_prefs.h
+++ components/autofill/core/common/autofill_prefs.h
@@ -37,6 +37,7 @@ extern const char kAutofillLastVersionDeduped[];
 extern const char kAutofillLastVersionDisusedAddressesDeleted[];
 extern const char kAutofillLastVersionDisusedCreditCardsDeleted[];
 extern const char kAutofillOrphanRowsRemoved[];
+extern const char kAutofillPaymentCvcStorageAndFilling[];
 // Do not get/set the value of this pref directly. Use provided getter/setter.
 extern const char kAutofillProfileEnabled[];
 extern const char kAutofillSyncTransportOptIn[];
@@ -108,6 +109,10 @@ bool ShouldShowPaymentMethodsMandatoryReauthPromo(const PrefService* prefs);
 
 void IncrementPaymentMethodsMandatoryReauthPromoShownCounter(
     PrefService* prefs);
+
+bool IsPaymentCvcStorageAndFillingEnabled(const PrefService* prefs);
+
+void SetPaymentCvcStorageAndFilling(PrefService* prefs, bool value);
 
 void SetUserOptedInWalletSyncTransport(PrefService* prefs,
                                        const CoreAccountId& account_id,
