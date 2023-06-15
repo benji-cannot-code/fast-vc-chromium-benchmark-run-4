89540f71b05097b20e0fd2ca8f79381cb6c476b0
Andrew Grieve <agrieve@chromium.org>
2023-06-15 19:47:01 +0000

    Add wrapper to JDK 3pp script to set -XX:+PerfDisableSharedMem
    This seems to be the only way to inject a default argument.
    
    Bug: 1441023
    Change-Id: Id059db16086ade423d4feb8bc215bb7094c18e7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615626
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158368}

---
+++ third_party/jdk/3pp/install.sh
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+set -x
+
+# An auto-created directory whose content will ultimately be uploaded to CIPD.
+# The commands below should output the built product to this directory.
+PREFIX="$1"
+
+mv current/bin/java current/bin/java.orig
+echo """#!/bin/sh
+
+# https://crbug.com/1441023
+exec "$0.orig" -XX:+PerfDisableSharedMem "$@"
+""" > current/bin/java
+chmod a+x current/bin/java
+
+mv current "$PREFIX"
