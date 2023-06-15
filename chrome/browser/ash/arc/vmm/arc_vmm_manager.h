62629bd1641d05f9f4edad5c4c3244f6861e6412
Shengsong Tan <sstan@chromium.org>
2023-06-15 08:34:35 +0000

    arc: Add ArcVmmManager browser test
    This CL add a simple browser test of ArcVmmManager which only contains a
    Ctor test.
    
    The Ctor test verify no crash happenes during the long dependence chain
    (mostly because there are many system status observers).
    
    The follow up tests will be add soon.
    
    Bug: b:265383671
    TEST=ArcVmmManagerBrowserTest
    
    Change-Id: I8d81f991cf8500f1e8c82623dab153c024aa01f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4614629
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158025}

--- chrome/browser/ash/arc/vmm/arc_vmm_manager.h
+++ chrome/browser/ash/arc/vmm/arc_vmm_manager.h
@@ -72,6 +72,7 @@ class ArcVmmManager : public KeyedService,
 
  private:
   friend class ArcVmmManagerTest;
+  friend class ArcVmmManagerBrowserTest;
   // Accelerator target for experimental usage. Ctrl + Alt + Shift + O / P for
   // enable or disable vmm swap.
   class AcceleratorTarget;
