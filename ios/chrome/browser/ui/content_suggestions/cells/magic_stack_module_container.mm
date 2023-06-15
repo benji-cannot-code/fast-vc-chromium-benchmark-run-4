100e6a93c8601174750d805dd663283853920a34
Chris Lu <thegreenfrog@chromium.org>
2023-06-15 18:16:20 +0000

    [ios] Magic Stack Module Title Resists Vertical Expansion
    This change ensures that the title row (and the separator if shown)
    resists expanding vertically to fill any extra module space. Instead,
    the content view will be the one expanding. This requires any StackView
    within the contents to have vertical constraint flexibility. For
    MultiRowContainerView, this means having that flexibility from the
    bottom for best visual effects.
    
    Screenshot: https://drive.google.com/file/d/1Y3-ILcfAjK5Vl6eUu3d1VqxMQ6wI8nn7/view?usp=sharing
    
    Bug: 1432252
    Change-Id: I6179236e6933ff335c8511d34c597fabbcf793ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617007
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158315}

--- ios/chrome/browser/ui/content_suggestions/cells/magic_stack_module_container.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/magic_stack_module_container.mm
@@ -85,6 +85,10 @@ const CGFloat kTitleStackViewTrailingMargin = 16.0f;
     titleStackView.alignment = UIStackViewAlignmentCenter;
     titleStackView.axis = UILayoutConstraintAxisHorizontal;
     titleStackView.distribution = UIStackViewDistributionFill;
+    // Resist Vertical expansion so all titles are the same height, allowing
+    // content view to fill the rest of the module space.
+    [titleStackView setContentHuggingPriority:UILayoutPriorityDefaultHigh
+                                      forAxis:UILayoutConstraintAxisVertical];
 
     UILabel* title = [[UILabel alloc] init];
     title.text = [MagicStackModuleContainer titleStringForModule:type];
@@ -131,6 +135,8 @@ const CGFloat kTitleStackViewTrailingMargin = 16.0f;
     }
     if ([self shouldShowSeparator]) {
       UIView* separator = [[UIView alloc] init];
+      [separator setContentHuggingPriority:UILayoutPriorityDefaultHigh
+                                   forAxis:UILayoutConstraintAxisVertical];
       separator.backgroundColor = [UIColor colorNamed:kSeparatorColor];
       [stackView addArrangedSubview:separator];
       [NSLayoutConstraint activateConstraints:@[
