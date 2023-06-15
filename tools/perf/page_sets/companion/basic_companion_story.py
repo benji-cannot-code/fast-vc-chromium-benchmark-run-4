5910faf8357ce9552ea714848bccceb3491d6626
Marco Georgaklis <mgeorgaklis@google.com>
2023-06-15 16:02:57 +0000

    Added screenshot companion story.
    This allows for the taking and logging of screenshots of companion with different websites.
    
    Change-Id: I4f7cfd9e361402282a6ecec71c330e80e98125aa
    Bug: b:273308580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4612169
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Marco Georgaklis <mgeorgaklis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158208}

--- tools/perf/page_sets/companion/basic_companion_story.py
+++ tools/perf/page_sets/companion/basic_companion_story.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 import json
+import subprocess
 from page_sets.desktop_ui.multitab_story import MultiTabStory
 from page_sets.desktop_ui.ui_devtools_utils import ClickOn, InputText, \
     PressKey, SHIFT_DOWN
@@ -11,6 +12,9 @@ from page_sets.companion.browser_element_identifiers import \
 from page_sets.companion.histograms import _CQ, _PROMO_EVENT, _SEARCH_BOX, \
     _STARTUP, _ZERO_STATE
 from page_sets.login_helpers import google_login
+from page_sets.data.companion_test_sites import SITES
+
+SCREENSHOT_PATH = "/tmp/"
 
 _GOOGLE_URL = "https://www.google.com/"
 _TEST_SEARCH_URL = "https://www.google.com/search?q=test"
@@ -33,13 +37,14 @@ class CompanionStory(MultiTabStory):
 
   def ToggleSidePanel(self, action_runner):
     ClickOn(self._devtools, element_id=kSidePanelButtonElementId)
-    action_runner.Wait(1)
+    action_runner.Wait(3)
 
   def OpenCompanion(self, action_runner):
     self.ToggleSidePanel(action_runner)
     combobox_node_id = self._devtools.QueryNodes("id:%s" %
                                                  kSidePanelComboboxElementId)[0]
     PressKey(self._devtools, combobox_node_id, " ")
+    action_runner.Wait(1)
     window_node_id = self._devtools.QueryNodes("<Window>")[0]
     PressKey(self._devtools, window_node_id, "ArrowUp")
     PressKey(self._devtools, window_node_id, "Return")
@@ -92,7 +97,7 @@ class CompanionStoryBasicOpen(CompanionStory):
 
     # Open companion and ensure features loaded.
     self.OpenCompanion(action_runner)
-    action_runner.Wait(3)
+    action_runner.Wait(10)
     self.ConductHistogramCheck(action_runner, _ZERO_STATE)
     self.ConductHistogramCheck(action_runner, _CQ)
 
@@ -107,7 +112,7 @@ class CompanionStoryBasicOpenLoggedOut(CompanionStory):
 
     # Open companion and ensure features loaded.
     self.OpenCompanion(action_runner)
-    action_runner.Wait(3)
+    action_runner.Wait(10)
     self.ConductHistogramCheck(action_runner, _ZERO_STATE)
     self.ConductHistogramCheck(action_runner, _PROMO_EVENT)
     self.ConductHistogramCheck(action_runner, _CQ, expected_empty=True)
@@ -140,7 +145,7 @@ class CompanionStorySRP(CompanionStory):
 
     # Open companion and ensure features loaded.
     self.OpenCompanion(action_runner)
-    action_runner.Wait(3)
+    action_runner.Wait(10)
     self.ConductHistogramCheck(action_runner, _ZERO_STATE)
     self.ConductHistogramCheck(action_runner, _PROMO_EVENT)
     self.ConductHistogramCheck(action_runner, _CQ)
@@ -149,3 +154,18 @@ class CompanionStorySRP(CompanionStory):
     self.ConductHistogramCheck(action_runner, _SEARCH_BOX, expected_empty=True)
     self.ConductSideSearch(action_runner)
     self.ConductHistogramCheck(action_runner, _SEARCH_BOX)
+
+
+class CompanionStoryScreenshot(CompanionStory):
+  NAME = 'companion:screenshot'
+
+  def InteractWithPage(self, action_runner):
+    self.SanityHistogramCheck(action_runner)
+    self.LogIn(action_runner)
+    action_runner.Wait(5)
+    self.OpenCompanion(action_runner)
+
+    for count, site in enumerate(SITES):
+      action_runner.tab.Navigate(site)
+      action_runner.Wait(15)
+      subprocess.call(["scrot", SCREENSHOT_PATH + str(count) + ".png"])
