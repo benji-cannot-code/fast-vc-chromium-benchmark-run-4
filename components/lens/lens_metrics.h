f0691f72139d4ae759aee8f99d0609fa16074a10
Jason Hu <hujasonx@google.com>
2023-06-15 19:21:07 +0000

    [Lens]Add more entry points for Bling LVF for logging
    Bug: 1353430
    Change-Id: I6ed742d4077f5eb27ede4f1cbd3272cfe799f005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4611590
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158353}

--- components/lens/lens_metrics.h
+++ components/lens/lens_metrics.h
@@ -68,7 +68,9 @@ enum class AmbientSearchEntryPoint {
   KEYBOARD = 8,
   APP_ICON_LONG_PRESS = 9,
   SPOTLIGHT = 10,
-  kMaxValue = SPOTLIGHT
+  PLUS_BUTTON = 11,
+  WEB_SEARCH_BAR = 12,
+  kMaxValue = WEB_SEARCH_BAR
 };
 
 // This should be kept in sync with the LensRegionSearchAspectRatio enum
