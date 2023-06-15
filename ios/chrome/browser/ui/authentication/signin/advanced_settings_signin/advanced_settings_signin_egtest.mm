e4f9a1136e24f3b39310732ee852e5f212cb1041
Boris Sazonov <bsazonov@google.com>
2023-06-15 12:09:58 +0000

    [iOS] Add EnableBookmarksAccountStorage to fieldtrial_testing_config
    Explicitly disables EnableBookmarksAccountStorage for some tests and
    adds it to fieldtrial_testing_config.json.
    
    Bug: 1455018
    Change-Id: I9f565310da9bd28061bfdd0958e4d06cbd161b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4607398
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158094}

--- ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_egtest.mm
+++ ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/ios/ios_util.h"
 #import "base/test/ios/wait_util.h"
 #import "base/time/time.h"
+#import "components/bookmarks/common/bookmark_features.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_constants.h"
@@ -74,6 +75,17 @@ void WaitForSettingDoneButton() {
 @end
 
 @implementation AdvancedSettingsSigninTestCase
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  if ([self isRunningTest:@selector
+            (testInterruptAdvancedSigninBookmarksFromAdvancedSigninSettings)]) {
+    // TODO(crbug.com/1455018): Re-enable the flag for non-legacy tests.
+    config.features_disabled.push_back(
+        bookmarks::kEnableBookmarksAccountStorage);
+  }
+  return config;
+}
 
 - (void)setUp {
   [super setUp];
