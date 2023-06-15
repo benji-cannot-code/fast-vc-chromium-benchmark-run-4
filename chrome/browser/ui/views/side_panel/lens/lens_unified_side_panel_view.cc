3bdc87f965cb98c95534c8731bb01fddd8b90a82
Juan Mojica <juanmojica@google.com>
2023-06-15 22:41:17 +0000

    Fix screenshots not working on Lens unified side panel feedback.
    Bug: b:281852066
    Change-Id: I94be114dc851e40f9e5b4b0a086f0316876ef754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618993
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158478}

--- chrome/browser/ui/views/side_panel/lens/lens_unified_side_panel_view.cc
+++ chrome/browser/ui/views/side_panel/lens/lens_unified_side_panel_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
+#include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/search/search.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/navigation_entry.h"
@@ -116,6 +118,10 @@ LensUnifiedSidePanelView::LensUnifiedSidePanelView(
       search::DefaultSearchProviderIsGoogle(profile)
           ? lens::features::GetHomepageURLForLens()
           : provider->image_url());
+
+  // Register a modal dialog manager to show permissions dialog like those
+  // requested from the feedback UI.
+  RegisterModalDialogManager(browser_view->browser());
 }
 
 content::WebContents* LensUnifiedSidePanelView::GetWebContents() {
@@ -357,6 +363,23 @@ void LensUnifiedSidePanelView::SetContentAndNewTabButtonVisible(
 
   if (!update_new_tab_button_callback_.is_null())
     update_new_tab_button_callback_.Run();
+}
+
+void LensUnifiedSidePanelView::RequestMediaAccessPermission(
+    content::WebContents* web_contents,
+    const content::MediaStreamRequest& request,
+    content::MediaResponseCallback callback) {
+  // Note: This is needed for taking screenshots via the feedback form.
+  MediaCaptureDevicesDispatcher::GetInstance()->ProcessMediaAccessRequest(
+      web_contents, request, std::move(callback), nullptr /* extension */);
+}
+
+void LensUnifiedSidePanelView::RegisterModalDialogManager(Browser* browser) {
+  CHECK(GetWebContents());
+  web_modal::WebContentsModalDialogManager::CreateForWebContents(
+      GetWebContents());
+  web_modal::WebContentsModalDialogManager::FromWebContents(GetWebContents())
+      ->SetDelegate(browser);
 }
 
 LensUnifiedSidePanelView::~LensUnifiedSidePanelView() = default;
