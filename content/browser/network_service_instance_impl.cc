f5965e79fa0801af15eb269e0783e210315c0913
Yoichi Osato <yoichio@chromium.org>
2023-06-15 05:53:12 +0000

    Reenable empty network service
    This CL reenables empty network service with
    - introducing strict conditions to avoid the found crashes ([1]).
    - add kRegisterEmptyNetworkService command line switch to launch the
    service in the utility process correctly.
    
    [1] https://crbug.com/1446571: Chrome_Android: Crash Report
      ChromeContentBrowserClient::GetApplicationLocale
    
    Bug: 1448414
    Change-Id: If1b340de582c189cad11192d827e1923692e7001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569853
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157972}

--- content/browser/network_service_instance_impl.cc
+++ content/browser/network_service_instance_impl.cc
@@ -90,6 +90,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_interface_change_listener.mojom.h"
 #endif
 
+#if BUILDFLAG(IS_ANDROID)
+#include "content/public/common/content_switches.h"
+#endif
+
 namespace content {
 
 namespace {
@@ -116,6 +120,8 @@ mojo::Remote<network::mojom::EmptyNetworkService>*
     g_empty_network_service_remote = nullptr;
 bool IsEmptyNetworkServiceEnabledForUMA() {
   return IsInProcessNetworkService() &&
+         !base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kSingleProcess) &&
          base::FeatureList::IsEnabled(
              network::features::kNetworkServiceEmptyOutOfProcess);
 }
@@ -391,7 +397,11 @@ void CreateInProcessNetworkService(
       FROM_HERE, base::BindOnce(&CreateInProcessNetworkServiceOnThread,
                                 std::move(receiver)));
 #if BUILDFLAG(IS_ANDROID)
-  if (IsEmptyNetworkServiceEnabledForUMA()) {
+  if (IsEmptyNetworkServiceEnabledForUMA() &&
+      // DownloadManagerService.java calls this in ServiceManagerOnlyMode, where
+      // this is called before the browser threads are initialized and UI thread
+      // is not named Chrome_UIThread at that point. We avoid such rare case.
+      BrowserThread::CurrentlyOn(BrowserThread::UI)) {
     if (!g_empty_network_service_remote) {
       g_empty_network_service_remote =
           new mojo::Remote<network::mojom::EmptyNetworkService>;
@@ -399,10 +409,11 @@ void CreateInProcessNetworkService(
     g_empty_network_service_remote->reset();
     mojo::PendingReceiver<network::mojom::EmptyNetworkService> empty_receiver =
         g_empty_network_service_remote->BindNewPipeAndPassReceiver();
-    ServiceProcessHost::Launch(std::move(empty_receiver),
-                               ServiceProcessHost::Options()
-                                   .WithDisplayName(u"Empty Network Service")
-                                   .Pass());
+    ServiceProcessHost::Options options;
+    options.WithDisplayName(u"Empty Network Service");
+    options.WithExtraCommandLineSwitches(
+        {network::switches::kRegisterEmptyNetworkService});
+    ServiceProcessHost::Launch(std::move(empty_receiver), std::move(options));
   }
 #endif
 }
