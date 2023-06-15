91a226e322ad7d5df6883982e4bba45e8bf30677
Tomasz Jurkiewicz <tju@google.com>
2023-06-15 11:57:39 +0000

    Enable supervised user filter tests for desktop platforms
    Bug: b:276874936
    Change-Id: Ibf1193d57a623bcd499a6bf7b7efc912e638fc20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4572674
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158088}

--- chrome/test/supervised_user/supervision_mixin.cc
+++ chrome/test/supervised_user/supervision_mixin.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace supervised_user {
 
@@ -64,18 +65,27 @@ void SetIsSupervisedProfile(Profile* profile, bool is_supervised_profile) {
 
 SupervisionMixin::SupervisionMixin(
     InProcessBrowserTestMixinHost& test_mixin_host,
-    InProcessBrowserTest* test_base)
-    : SupervisionMixin(test_mixin_host,
-                       test_base,
-                       SupervisionMixin::Options()) {}
-
-SupervisionMixin::SupervisionMixin(
-    InProcessBrowserTestMixinHost& test_mixin_host,
     InProcessBrowserTest* test_base,
     const Options& options)
     : InProcessBrowserTestMixin(&test_mixin_host),
       test_base_(test_base),
       fake_gaia_mixin_(&test_mixin_host),
+      consent_level_(options.consent_level),
+      email_(options.email),
+      account_type_(options.account_type) {}
+
+SupervisionMixin::SupervisionMixin(
+    InProcessBrowserTestMixinHost& test_mixin_host,
+    InProcessBrowserTest* test_base,
+    base::raw_ptr<net::EmbeddedTestServer> embedded_test_server,
+    const Options& options)
+    : InProcessBrowserTestMixin(&test_mixin_host),
+      test_base_(test_base),
+      fake_gaia_mixin_(&test_mixin_host),
+      embedded_test_server_setup_mixin_(absl::in_place,
+                                        test_mixin_host,
+                                        embedded_test_server,
+                                        options.embedded_test_server_options),
       consent_level_(options.consent_level),
       email_(options.email),
       account_type_(options.account_type) {}
@@ -103,8 +113,8 @@ void SupervisionMixin::SetUpTestServer() {
 }
 
 void SupervisionMixin::SetUpIdentityTestEnvironment() {
-  adaptor_ = std::make_unique<IdentityTestEnvironmentProfileAdaptor>(
-      test_base_->browser()->profile());
+  adaptor_ =
+      std::make_unique<IdentityTestEnvironmentProfileAdaptor>(GetProfile());
 }
 
 void SupervisionMixin::LogInUser() {
@@ -123,12 +133,16 @@ void SupervisionMixin::LogInUser() {
   GetIdentityTestEnvironment()->SetAutomaticIssueOfAccessTokens(true);
 
   SetIsSupervisedProfile(
-      test_base_->browser()->profile(),
+      GetProfile(),
       /*is_supervised_profile=*/account_type_ == AccountType::kSupervised);
 }
 
-signin::IdentityTestEnvironment*
-SupervisionMixin::GetIdentityTestEnvironment() {
+Profile* SupervisionMixin::GetProfile() const {
+  return test_base_->browser()->profile();
+}
+
+signin::IdentityTestEnvironment* SupervisionMixin::GetIdentityTestEnvironment()
+    const {
   CHECK(adaptor_->identity_test_env())
       << "Do not use before the environment is set up.";
   return adaptor_->identity_test_env();
