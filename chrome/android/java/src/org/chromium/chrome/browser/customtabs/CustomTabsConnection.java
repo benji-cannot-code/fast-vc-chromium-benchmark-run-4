95f8590a08692ba0f27e7c435c77a6dc3fb0b737
Sinan Sahin <sinansahin@google.com>
2023-06-15 19:09:09 +0000

    [CT] Pass Bundle#EMPTY to EngagementSignalsCallback methods instead of null
    Bug: 1455157, b/287312027
    Change-Id: I7dbf2d63c2c4b571461e334fdba2ebb13e179543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618707
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1158348}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -1270,7 +1270,7 @@ public class CustomTabsConnection {
                 mClientManager.getEngagementSignalsCallbackForSession(session);
         if (callback == null) return;
         try {
-            callback.onVerticalScrollEvent(isDirectionUp, null);
+            callback.onVerticalScrollEvent(isDirectionUp, Bundle.EMPTY);
         } catch (Exception e) {
             // Catching all exceptions is really bad, but we need it here,
             // because Android exposes us to client bugs by throwing a variety
@@ -1299,7 +1299,7 @@ public class CustomTabsConnection {
                 mClientManager.getEngagementSignalsCallbackForSession(session);
         if (callback == null) return;
         try {
-            callback.onGreatestScrollPercentageIncreased(scrollPercentage, null);
+            callback.onGreatestScrollPercentageIncreased(scrollPercentage, Bundle.EMPTY);
         } catch (Exception e) {
             // Catching all exceptions is really bad, but we need it here,
             // because Android exposes us to client bugs by throwing a variety
@@ -1326,7 +1326,7 @@ public class CustomTabsConnection {
                 mClientManager.getEngagementSignalsCallbackForSession(session);
         if (callback == null) return;
         try {
-            callback.onSessionEnded(didGetUserInteraction, null);
+            callback.onSessionEnded(didGetUserInteraction, Bundle.EMPTY);
         } catch (Exception e) {
             // Catching all exceptions is really bad, but we need it here,
             // because Android exposes us to client bugs by throwing a variety
