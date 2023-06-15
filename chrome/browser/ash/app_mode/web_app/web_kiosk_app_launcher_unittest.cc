a4b038dc5c849e8af609659ddc298b8872a03f64
Hidehiko Abe <hidehiko@chromium.org>
2023-06-15 11:47:59 +0000

    Use LacrosOnly for WebKioskAppLauncher tests.
    Currently the tests use LacrosPrimary, which is going to be removed.
    This CL replaces it by LacrosOnly.
    
    BUG=1448575
    TEST=Tryjob
    
    Change-Id: I97b765c67e9113d68c6fc461ac767942cd5b6f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615105
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158083}

--- chrome/browser/ash/app_mode/web_app/web_kiosk_app_launcher_unittest.cc
+++ chrome/browser/ash/app_mode/web_app/web_kiosk_app_launcher_unittest.cc
@@ -317,6 +317,8 @@ class WebKioskAppLauncherUsingLacrosTest : public WebKioskAppLauncherTest {
         wm_helper_(std::make_unique<exo::WMHelper>()) {
     scoped_feature_list_.InitWithFeatures(
         {ash::features::kLacrosSupport, ash::features::kLacrosPrimary,
+         ash::features::kLacrosOnly,
+         ash::features::kLacrosProfileMigrationForceOff,
          ::features::kWebKioskEnableLacros},
         {});
   }
