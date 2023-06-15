a41e8d34510e22e7d16bb428cf563893c8043fc5
Ryan Sultanem <rsult@google.com>
2023-06-15 08:20:16 +0000

    [KeyedServiceRegistration] Ensure Ash keyed service factory registered
    Bug: 1412439
    Change-Id: If68677858162d3cad33d32accc2d005aab88a112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615064
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158023}

--- chrome/browser/ash/browser_context_keyed_service_factories.cc
+++ chrome/browser/ash/browser_context_keyed_service_factories.cc
@@ -102,6 +102,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/tether/tether_service_factory.h"
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_manager_factory.h"
 #include "chrome/browser/browser_process_platform_part_ash.h"
+#include "chrome/browser/scalable_iph/scalable_iph_factory.h"
 #include "chrome/browser/sharesheet/sharesheet_service_factory.h"
 #include "chrome/browser/speech/cros_speech_recognition_service_factory.h"
 #include "chrome/browser/speech/extension_api/tts_engine_extension_observer_chromeos.h"
@@ -227,6 +228,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   policy::UserCloudPolicyTokenForwarderFactory::GetInstance();
   printing::print_management::PrintingManagerFactory::GetInstance();
   PrintJobHistoryServiceFactory::GetInstance();
+  ScalableIphFactory::GetInstance();
   ScanServiceFactory::GetInstance();
   secure_channel::NearbyConnectorFactory::GetInstance();
   sharesheet::SharesheetServiceFactory::GetInstance();
