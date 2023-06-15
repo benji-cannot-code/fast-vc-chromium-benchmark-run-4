ef39b3376f7940ceea14aa852c604119b4c4ee02
Adam Rice <ricea@chromium.org>
2023-06-15 15:31:35 +0000

    WebSocket: add wpt for sending 50 65K messages
    http://crrev/c/3311025 introduced a bug when sending messages a multiple
    of 65536 bytes in size over a connection with backpressure. This was
    fixed in http://crrev/c/3404081 in January 2022, however no repro test
    was added at that time.
    
    Add a wpt to repro the issue and stop it happening again.
    
    BUG=1454078
    
    Change-Id: Icdc1dab66e58bab6eaabec84c0f53d5f17ca28e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4612732
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158168}

---
+++ third_party/blink/web_tests/external/wpt/websockets/handlers/receive-many-with-backpressure_wsh.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Sleep to build backpressure, receive messages, and send back their length.
+# Used by send-many-64K-messages-with-backpressure.any.js.
+
+
+import time
+
+
+def web_socket_do_extra_handshake(request):
+    # Compression will interfere with backpressure, so disable the
+    # permessage-delate extension.
+    request.ws_extension_processors = []
+
+
+def web_socket_transfer_data(request):
+    while True:
+        # Don't read the message immediately, so backpressure can build.
+        time.sleep(0.1)
+        line = request.ws_stream.receive_message()
+        if line is None:
+            return
+        # Send back the size of the message as acknowledgement that it was
+        # received.
+        request.ws_stream.send_message(str(len(line)), binary=False)
