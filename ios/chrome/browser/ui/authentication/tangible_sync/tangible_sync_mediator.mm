afd967b28ab474e4e1f9c6d314d20ff6c999a275
Arthur Milchior <arthurmilchior@chromium.org>
2023-06-15 12:45:54 +0000

    [iOS] TangibleSyncMediator dealloc checks stop was called
    Bug: None
    Change-Id: I1a3c0a0df3cde0b59923817d4ede5979dcecc29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604103
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158105}

--- ios/chrome/browser/ui/authentication/tangible_sync/tangible_sync_mediator.mm
+++ ios/chrome/browser/ui/authentication/tangible_sync/tangible_sync_mediator.mm
@@ -95,6 +95,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _syncSetupService = nil;
 }
 
+- (void)dealloc {
+  CHECK(!_authenticationService);
+}
+
 - (void)startSyncWithConfirmationID:(const int)confirmationID
                            consentIDs:(NSArray<NSNumber*>*)consentIDs
                    authenticationFlow:(AuthenticationFlow*)authenticationFlow
