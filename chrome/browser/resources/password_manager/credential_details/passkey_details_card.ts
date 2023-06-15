32208a2682b261d463269ae417c8051b51207eec
Nina Satragno <nsatragno@chromium.org>
2023-06-15 15:55:48 +0000

    [password manager] Polish passkey card UI
    Polish the passkey card UI so that it matches the mocks.
    * Add passkey card missing strings.
    * Remove `note` as we are dropping it for passkeys.
    * Add passkey icon.
    
    UXW review:
    http://go/passkeys-in-chrome-pwm-uxw
    
    Screnshot:
    https://storage.cloud.google.com/chromium-translation-screenshots/6ecd65cd81c74c71571ffd5537140dbfd329382a
    
    Design doc:
    https://docs.google.com/document/d/1Ox_VBGNO-Ff9bcP97FUMcN15Xtfn_sz0_WuDe3ujbwA
    
    Bug: 1432717
    Change-Id: Ie2d239d831ab0e8636f15a1985d839a942fc080e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4606325
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158204}

--- chrome/browser/resources/password_manager/credential_details/passkey_details_card.ts
+++ chrome/browser/resources/password_manager/credential_details/passkey_details_card.ts
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_input/cr_input_style.css.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
-import 'chrome://resources/cr_elements/cr_icons.css.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import '../shared_style.css.js';
@@ -20,7 +19,6 @@ import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bu
 import {PasswordManagerImpl, PasswordViewPageInteractions} from '../password_manager_proxy.js';
 
 import {CredentialFieldElement} from './credential_field.js';
-import {CredentialNoteElement} from './credential_note.js';
 import {getTemplate} from './passkey_details_card.html.js';
 
 export interface PasskeyDetailsCardElement {
@@ -28,7 +26,6 @@ export interface PasskeyDetailsCardElement {
     deleteButton: CrButtonElement,
     domainLabel: HTMLElement,
     editButton: CrButtonElement,
-    noteValue: CredentialNoteElement,
     showMore: HTMLAnchorElement,
     usernameValue: CredentialFieldElement,
     displayNameValue: CredentialFieldElement,
@@ -62,29 +59,15 @@ export class PasskeyDetailsCardElement extends PasskeyDetailsCardElementBase {
   private showEditPasskeyDialog_: boolean;
   private showDeletePasskeyDialog_: boolean;
 
-  private getPasskeyUsageInfoString_(): string {
-    const website = this.passkey.affiliatedDomains?.[0]?.name;
-    if (website) {
-      // TODO(crbug.com/1432717): i18n. This should be an informative text
-      // telling the user that they'll need their phone to sign in using this
-      // passkey.
-      return '';
-    }
-    return '';
-  }
-
   private getUsernameValue_(): string {
     return !this.passkey.username || this.passkey.username === '' ?
-        // TODO(crbug.com/1432717): i18n. This should say "no username saved".
-        '' :
+        this.i18n('usernamePlaceholder') :
         this.passkey.username!;
   }
 
   private getDisplayNameValue_(): string {
     return !this.passkey.displayName || this.passkey.displayName === '' ?
-        // TODO(crbug.com/1432717): i18n. This should say "no display name
-        // saved".
-        '' :
+        this.i18n('displayNamePlaceholder') :
         this.passkey.displayName!;
   }
 
