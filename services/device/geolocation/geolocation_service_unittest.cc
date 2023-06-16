550975ca89078b5d1b66d21e4068cd6fd87f0ee0
Matt Reynolds <mattreynolds@google.com>
2023-06-16 21:52:10 +0000

    geolocation: Use mock WifiDataProvider in GeolocationServiceUnitTest
    Bug: 1455221
    Change-Id: I69d004036c357f0f84db42511023864b1c9fecaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619369
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159116}

--- services/device/geolocation/geolocation_service_unittest.cc
+++ services/device/geolocation/geolocation_service_unittest.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_change_notifier.h"
 #include "services/device/device_service_test_base.h"
 #include "services/device/geolocation/geolocation_provider_impl.h"
+#include "services/device/geolocation/mock_wifi_data_provider.h"
 #include "services/device/geolocation/network_location_request.h"
+#include "services/device/geolocation/wifi_data_provider_handle.h"
 #include "services/device/public/cpp/device_features.h"
 #include "services/device/public/mojom/geolocation.mojom.h"
 #include "services/device/public/mojom/geolocation_config.mojom.h"
@@ -60,6 +62,10 @@ class GeolocationServiceUnitTest : public DeviceServiceTestBase {
     // the device service.
     DeviceServiceTestBase::SetUp();
 
+    wifi_data_provider_ = MockWifiDataProvider::CreateInstance();
+    WifiDataProviderHandle::SetFactoryForTesting(
+        MockWifiDataProvider::GetInstance);
+
     device_service()->BindGeolocationControl(
         geolocation_control_.BindNewPipeAndPassReceiver());
     geolocation_control_->UserDidOptIntoLocationServices();
@@ -71,6 +77,8 @@ class GeolocationServiceUnitTest : public DeviceServiceTestBase {
   }
 
   void TearDown() override {
+    WifiDataProviderHandle::ResetFactoryForTesting();
+
     DeviceServiceTestBase::TearDown();
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -92,6 +100,7 @@ class GeolocationServiceUnitTest : public DeviceServiceTestBase {
         geolocation_config_.BindNewPipeAndPassReceiver());
   }
 
+  scoped_refptr<MockWifiDataProvider> wifi_data_provider_;
   std::unique_ptr<net::NetworkChangeNotifier> network_change_notifier_;
   mojo::Remote<mojom::GeolocationControl> geolocation_control_;
   mojo::Remote<mojom::GeolocationContext> geolocation_context_;
