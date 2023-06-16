4e43522f7838b3b1979296c89a02e050af970426
Brian Ho <hob@chromium.org>
2023-06-16 02:53:06 +0000

    Reland "drm: Only use modifiers that support Vulkan import"
    Currently when Vulkan is enabled, we do not take into account whether
    a particular DRM format modifier is supported for Vulkan import via
    vkCreateImage. Thus, we can allocate a presentation buffer with a
    modifier that then cannot be imported into Vulkan, and later we fail
    display composition. For example, the Intel Vulkan drivers do not
    support import of Gen 12 CCS buffers, and Chrome crashes on startup
    if we enable Vulkan on a sufficiently modern Intel GPU.
    
    This CL fixes the issue by querying for supported modifiers when
    populating the available modifiers list and only returning those that
    can be imported.
    
    This is a reland of 1c1f725a with a fix for the LSan compilation
    issue.
    
    Change-Id: I03137781dd72eb52cf50dd8b3ebf4dd3e5b8f413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618710
    Commit-Queue: Brian Ho <hob@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158599}

--- ui/ozone/platform/drm/gpu/screen_manager.cc
+++ ui/ozone/platform/drm/gpu/screen_manager.cc
@@ -266,7 +266,8 @@ void ScreenManager::AddDisplayController(const scoped_refptr<DrmDevice>& drm,
   }
 
   controllers_.push_back(std::make_unique<HardwareDisplayController>(
-      std::make_unique<CrtcController>(drm, crtc, connector), gfx::Point()));
+      std::make_unique<CrtcController>(drm, crtc, connector), gfx::Point(),
+      drm_modifiers_filter_.get()));
 }
 
 void ScreenManager::RemoveDisplayControllers(
@@ -654,7 +655,8 @@ void ScreenManager::SetDisplayControllerForEnableAndGetProps(
   // restore mirror mode.
   if (controller->IsMirrored()) {
     controllers_.push_back(std::make_unique<HardwareDisplayController>(
-        controller->RemoveCrtc(drm, crtc), controller->origin()));
+        controller->RemoveCrtc(drm, crtc), controller->origin(),
+        drm_modifiers_filter_.get()));
     it = controllers_.end() - 1;
     controller = it->get();
   }
@@ -672,7 +674,8 @@ bool ScreenManager::SetDisableDisplayControllerForDisableAndGetProps(
     HardwareDisplayController* controller = it->get();
     if (controller->IsMirrored()) {
       controllers_.push_back(std::make_unique<HardwareDisplayController>(
-          controller->RemoveCrtc(drm, crtc), controller->origin()));
+          controller->RemoveCrtc(drm, crtc), controller->origin(),
+          drm_modifiers_filter_.get()));
       controller = controllers_.back().get();
     }
 
@@ -955,6 +958,12 @@ DrmWindow* ScreenManager::FindWindowAt(const gfx::Rect& bounds) const {
   }
 
   return nullptr;
+}
+
+void ScreenManager::SetDrmModifiersFilter(
+    std::unique_ptr<DrmModifiersFilter> filter) {
+  DCHECK(controllers_.empty());
+  drm_modifiers_filter_ = std::move(filter);
 }
 
 }  // namespace ui
