5dc1614af619a6c661d7318003ea88ba6347e429
Arthur Milchior <arthurmilchior@chromium.org>
2023-06-16 14:13:50 +0000

    [iOS] BookmarksEditorCoordinator dealloc checks stop was called
    Bug: None
    Change-Id: Iaa32021dde9bbb8aebb2cb35be67632000889672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604496
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158808}

--- ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_coordinator.mm
+++ ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_coordinator.mm
@@ -134,6 +134,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _navigationController = nil;
 }
 
+- (void)dealloc {
+  DCHECK(!_navigationController);
+}
+
 - (BOOL)canDismiss {
   if (_viewController.edited) {
     return NO;
