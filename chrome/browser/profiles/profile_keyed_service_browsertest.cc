4159b71fe56da10ecad753a32d4b5f3f57b682ff
mark a. foltz <mfoltz@chromium.org>
2023-06-16 22:47:29 +0000

    [Media Router] Enable Media Router OTR by default.
    This enables by default the creation of a separate Media Router
    instance for OTR profiles to enable testing for M116.
    
    Various tests are updated to pass with the feature enabled/disabled.
    
    See http://crrev.com/c/4336996 for more details/background.
    
    Bug: 1291723
    Change-Id: Ic1623fb424c3f4f23c57613a6e75db3be17ab4da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571347
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1159152}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/media/router/media_router_feature.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_selections.h"
 #include "chrome/common/chrome_features.h"
@@ -265,6 +266,10 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceBrowserTest,
     "sct_reporting::Factory"
   };
   // clang-format on
+
+  if (base::FeatureList::IsEnabled(media_router::kMediaRouterOTRInstance)) {
+    guest_otr_active_services.insert("MediaRouter");
+  }
 
   Profile* guest_profile =
       CreateProfileAndWaitForAllTasks(ProfileManager::GetGuestProfilePath());
