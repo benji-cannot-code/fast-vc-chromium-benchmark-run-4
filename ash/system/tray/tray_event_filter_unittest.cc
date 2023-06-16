84d6a9d31a170bb58b12893206ca74a77b4c8e74
Andre Le <leandre@chromium.org>
2023-06-16 18:49:37 +0000

    Status Area: Close calendar bubble when other tray bubble is open
    Fixed: b:265383724
    Change-Id: Ib60c2b24d9b20e5d089d23b7e572736f60d82f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615757
    Reviewed-by: Kevin Radtke <kradtke@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158992}

--- ash/system/tray/tray_event_filter_unittest.cc
+++ ash/system/tray/tray_event_filter_unittest.cc
@@ -6,14 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_event_filter.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/ime/ime_controller_impl.h"
 #include "ash/root_window_controller.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
+#include "ash/system/ime_menu/ime_menu_tray.h"
 #include "ash/system/message_center/ash_message_popup_collection.h"
 #include "ash/system/message_center/ash_notification_expand_button.h"
 #include "ash/system/message_center/ash_notification_view.h"
 #include "ash/system/message_center/unified_message_center_bubble.h"
 #include "ash/system/status_area_widget.h"
+#include "ash/system/unified/date_tray.h"
 #include "ash/system/unified/unified_system_tray.h"
 #include "ash/test/ash_test_base.h"
 #include "base/test/scoped_feature_list.h"
@@ -248,6 +251,23 @@ TEST_P(TrayEventFilterTest, DraggingOnTrayClosesBubble) {
   GetEventGenerator()->GestureScrollSequence(start, end_inside,
                                              base::Milliseconds(100), 4);
   EXPECT_FALSE(IsBubbleShown());
+}
+
+TEST_P(TrayEventFilterTest, ClickOnCalendarBubbleClosesOtherTrays) {
+  Shell::Get()->ime_controller()->ShowImeMenuOnShelf(true);
+  auto* status_area = GetPrimaryShelf()->GetStatusAreaWidget();
+  auto* ime_tray = status_area->ime_menu_tray();
+
+  LeftClickOn(ime_tray);
+  EXPECT_TRUE(ime_tray->GetBubbleWidget());
+
+  auto* date_tray = status_area->date_tray();
+  LeftClickOn(date_tray);
+
+  // When opening the calendar, the unified system tray bubble should be open
+  // with the calendar view, and the IME bubble should be closed.
+  EXPECT_TRUE(IsBubbleShown());
+  EXPECT_FALSE(ime_tray->GetBubbleWidget());
 }
 
 using TrayEventFilterQsRevampDisabledTest = TrayEventFilterTest;
