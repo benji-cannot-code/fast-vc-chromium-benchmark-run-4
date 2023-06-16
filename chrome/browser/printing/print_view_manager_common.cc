fc8700a670f7ea2e559ebd5756c79c6b8ce98555
Lei Zhang <thestig@chromium.org>
2023-06-16 21:44:28 +0000

    Make PrintNodeUnderContextMenu operation go through PrintViewManager
    Currently, RenderViewContextMenu uses Mojo to directly talk to the
    PrintRenderFrame interface when printing the node under the context
    menu. This is inconsistent with other printing operations that go
    through PrintViewManager.
    
    Fix this by adding StartPrintNodeUnderContextMenu() and changing
    RenderViewContextMenu to call it. StartPrintNodeUnderContextMenu() then
    invokes the appropriate version of PrintViewManager. PrintViewManager
    will performs checks, like it does when starting other print operations,
    before triggering printing via the PrintRenderFrame interface.
    
    Bug: 1286556
    Change-Id: I385d167ab93340722ec7e12e44ae73379075f7c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602776
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159107}

--- chrome/browser/printing/print_view_manager_common.cc
+++ chrome/browser/printing/print_view_manager_common.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/printing/print_view_manager_common.h"
 
+#include <utility>
+
 #include "build/chromeos_buildflags.h"
 #include "content/public/browser/render_frame_host.h"
 #include "extensions/buildflags/buildflags.h"
@@ -29,6 +31,12 @@ namespace printing {
 
 namespace {
 
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+using PrintViewManagerImpl = PrintViewManager;
+#else
+using PrintViewManagerImpl = PrintViewManagerBasic;
+#endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
+
 // Pick the right RenderFrameHost based on the WebContents.
 content::RenderFrameHost* GetRenderFrameHostToUse(
     content::WebContents* contents) {
@@ -52,12 +60,6 @@ void StartPrint(
 #endif
     bool print_preview_disabled,
     bool has_selection) {
-#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
-  using PrintViewManagerImpl = PrintViewManager;
-#else
-  using PrintViewManagerImpl = PrintViewManagerBasic;
-#endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
-
   content::RenderFrameHost* rfh_to_use = GetRenderFrameHostToUse(contents);
   if (!rfh_to_use)
     return;
@@ -97,6 +99,24 @@ void StartBasicPrint(content::WebContents* contents) {
 
   print_view_manager->BasicPrint(rfh_to_use);
 #endif  // ENABLE_PRINT_PREVIEW
+}
+
+void StartPrintNodeUnderContextMenu(content::RenderFrameHost* rfh,
+                                    bool print_preview_disabled) {
+  auto* print_view_manager = PrintViewManagerImpl::FromWebContents(
+      content::WebContents::FromRenderFrameHost(rfh));
+  if (!print_view_manager) {
+    return;
+  }
+
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  if (!print_preview_disabled) {
+    print_view_manager->PrintPreviewForNodeUnderContextMenu(rfh);
+    return;
+  }
+#endif
+
+  print_view_manager->PrintNodeUnderContextMenu(rfh);
 }
 
 content::RenderFrameHost* GetFrameToPrint(content::WebContents* contents) {
