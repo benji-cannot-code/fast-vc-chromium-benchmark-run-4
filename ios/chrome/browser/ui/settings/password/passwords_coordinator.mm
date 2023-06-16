7236e8f9ebbf0574877e6e7b5b2bf0fea12d1724
Federica Germinario <fedegermi@google.com>
2023-06-16 11:20:14 +0000

    [ios] Make PasswordManagerViewController independent from Browser
    This CL makes PasswordManagerViewController independent from Browser
    and BrowserState by injecting the specific dependencies instead of
    the entire object.
    
    Bug: 1454018
    Change-Id: I63d9f576e66c7b4a346c737695a1fab4eee86c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4601268
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158755}

--- ios/chrome/browser/ui/settings/password/passwords_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/passwords_coordinator.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/public/commands/open_new_tab_command.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/signin/identity_manager_factory.h"
 #import "ios/chrome/browser/sync/sync_service_factory.h"
 #import "ios/chrome/browser/sync/sync_setup_service_factory.h"
@@ -152,9 +153,11 @@ using password_manager::WarningType;
                                        browserState)];
   self.reauthModule = [[ReauthenticationModule alloc]
       initWithSuccessfulReauthTimeAccessor:self.mediator];
-
-  self.passwordsViewController =
-      [[PasswordManagerViewController alloc] initWithBrowser:self.browser];
+  ChromeAccountManagerService* accountManagerService =
+      ChromeAccountManagerServiceFactory::GetForBrowserState(browserState);
+  self.passwordsViewController = [[PasswordManagerViewController alloc]
+      initWithChromeAccountManagerService:accountManagerService
+                              prefService:browserState->GetPrefs()];
 
   self.passwordsViewController.handler = self;
   self.passwordsViewController.delegate = self.mediator;
