d3c49e783cb646facd231de8754438b1e2a22ca6
Pierre St Juste <pstjuste@google.com>
2023-06-16 22:24:54 +0000

    Adding classifier agent in the renderer.
    The classifier agent serves as the entry point to the visual search feature in the renderer process. It will also ultimately implement the IPC interface to allow communication with the browser process.
    
    Bug: b/284642988
    Change-Id: I58797a0dbd81a87a80398a1fe1707fb473c1dc7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609970
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Pierre St Juste <pstjuste@google.com>
    Cr-Commit-Position: refs/heads/main@{#1159138}

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/open_search_description_document_handler.mojom.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/renderer/chrome_content_settings_agent_delegate.h"
+#include "chrome/renderer/companion/visual_search/visual_search_classifier_agent.h"
 #include "chrome/renderer/media/media_feeds.h"
 #include "components/crash/core/common/crash_key.h"
 #include "components/lens/lens_metadata.mojom.h"
@@ -197,6 +198,10 @@ ChromeRenderFrameObserver::ChromeRenderFrameObserver(
 
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
   SetClientSidePhishingDetection();
+#endif
+
+#if !BUILDFLAG(IS_ANDROID)
+  SetVisualSearchClassifierAgent();
 #endif
 
   if (!translate::IsSubFrameTranslationEnabled()) {
@@ -552,6 +557,14 @@ void ChromeRenderFrameObserver::SetClientSidePhishingDetection() {
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
   phishing_classifier_ = safe_browsing::PhishingClassifierDelegate::Create(
       render_frame(), nullptr);
+#endif
+}
+
+void ChromeRenderFrameObserver::SetVisualSearchClassifierAgent() {
+#if !BUILDFLAG(IS_ANDROID)
+  visual_classifier_ =
+      companion::visual_search::VisualSearchClassifierAgent::Create(
+          render_frame());
 #endif
 }
 
