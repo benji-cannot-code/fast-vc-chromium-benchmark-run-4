dfefe229f3c60582a5c78202ce092d5b176c55d9
Andrew Rayskiy <greengrape@google.com>
2023-06-16 12:49:30 +0000

    Remove SetPrinterConfigurerForTesting() from printer_configurer.{h,cc}
    Unused after crrev.com/c/4568367
    
    Bug: None
    Change-Id: I846cdaeb7b646530db3ee3ceb3f9e7e50443c15f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4620330
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158776}

--- chrome/browser/ash/printing/printer_configurer.cc
+++ chrome/browser/ash/printing/printer_configurer.cc
@@ -40,9 +40,6 @@ namespace {
 using ::chromeos::PpdProvider;
 using ::chromeos::Printer;
 
-// PrinterConfigurer override for testing.
-PrinterConfigurer* g_printer_configurer_for_test = nullptr;
-
 PrinterSetupResult PrinterSetupResultFromDbusResultCode(const Printer& printer,
                                                         int result_code) {
   DCHECK_GE(result_code, 0);
@@ -231,20 +228,7 @@ void PrinterConfigurer::RecordUsbPrinterSetupSource(
 // static
 std::unique_ptr<PrinterConfigurer> PrinterConfigurer::Create(
     scoped_refptr<PpdProvider> ppd_provider) {
-  if (g_printer_configurer_for_test) {
-    auto printer_configurer =
-        base::WrapUnique<PrinterConfigurer>(g_printer_configurer_for_test);
-    g_printer_configurer_for_test = nullptr;
-    return printer_configurer;
-  }
   return std::make_unique<PrinterConfigurerImpl>(ppd_provider);
-}
-
-// static
-void PrinterConfigurer::SetPrinterConfigurerForTesting(
-    std::unique_ptr<PrinterConfigurer> printer_configurer) {
-  DCHECK(!g_printer_configurer_for_test);
-  g_printer_configurer_for_test = printer_configurer.release();
 }
 
 // static
