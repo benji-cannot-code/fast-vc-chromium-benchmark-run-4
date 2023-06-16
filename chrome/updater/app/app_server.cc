ddc84b01a81de67cec524697b36fd6eb4bd42a7f
Sorin Jianu <sorin@chromium.org>
2023-06-16 01:49:35 +0000

    updater: refactor if statement in AppServer::ModeCheck.
    Bug: 1369264
    Change-Id: Iafb21714028b455a7db49838ab0968dacbf5d540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618021
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158570}

--- chrome/updater/app/app_server.cc
+++ chrome/updater/app/app_server.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
@@ -83,7 +84,7 @@ base::OnceClosure AppServer::ModeCheck() {
 #endif
   }
 
-  if (active_version != base::Version("0") && active_version != this_version) {
+  if (active_version != base::Version("0") && this_version > active_version) {
     scoped_refptr<LocalPrefs> local_prefs = CreateLocalPrefs(updater_scope());
     if (!local_prefs->GetQualified()) {
       global_prefs = nullptr;
@@ -110,6 +111,9 @@ base::OnceClosure AppServer::ModeCheck() {
       return base::BindOnce(&AppServer::Shutdown, this, kErrorFailedToSwap);
     }
   }
+
+  CHECK_EQ(base::Version(global_prefs->GetActiveVersion()),
+           base::Version(kUpdaterVersion));
 
   if (IsInternalService()) {
     prefs_ = CreateLocalPrefs(updater_scope());
