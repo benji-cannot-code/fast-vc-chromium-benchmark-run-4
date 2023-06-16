a8c368ab9c86759f6d8a84f06232b71ff85b2a38
YuhanYang <yyhyyh@google.com>
2023-06-16 23:03:31 +0000

    customization: Add fake observer for stylus and tablet
    Bug: b/241965717
    Change-Id: I88638dc2ac8d7702e24255e56d42a94dc2bdd55b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617674
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1159164}

--- chrome/browser/resources/settings/chromeos/device_page/fake_input_device_settings_provider.ts
+++ chrome/browser/resources/settings/chromeos/device_page/fake_input_device_settings_provider.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from 'chrome://resources/js/assert_ts.js';
 
-import {GraphicsTablet, InputDeviceSettingsProviderInterface, Keyboard, KeyboardObserverInterface, KeyboardSettings, MetaKey, ModifierKey, Mouse, MouseObserverInterface, MouseSettings, PointingStick, PointingStickObserverInterface, PointingStickSettings, Stylus, Touchpad, TouchpadObserverInterface, TouchpadSettings} from './input_device_settings_types.js';
+import {GraphicsTablet, GraphicsTabletObserverInterface, InputDeviceSettingsProviderInterface, Keyboard, KeyboardObserverInterface, KeyboardSettings, MetaKey, ModifierKey, Mouse, MouseObserverInterface, MouseSettings, PointingStick, PointingStickObserverInterface, PointingStickSettings, Stylus, StylusObserverInterface, Touchpad, TouchpadObserverInterface, TouchpadSettings} from './input_device_settings_types.js';
 
 /**
  * @fileoverview
@@ -82,6 +82,8 @@ export class FakeInputDeviceSettingsProvider implements
   private pointingStickObservers: PointingStickObserverInterface[] = [];
   private mouseObservers: MouseObserverInterface[] = [];
   private touchpadObservers: TouchpadObserverInterface[] = [];
+  private stylusObservers: StylusObserverInterface[] = [];
+  private graphicsTabletObservers: GraphicsTabletObserverInterface[] = [];
 
   constructor() {
     // Setup method resolvers.
@@ -232,6 +234,20 @@ export class FakeInputDeviceSettingsProvider implements
     }
   }
 
+  notifyStylusListUpdated(): void {
+    const styluses = this.methods.getResult('fakeStyluses');
+    for (const observer of this.stylusObservers) {
+      observer.onStylusListUpdated(styluses);
+    }
+  }
+
+  notifyGraphicsTabletUpdated(): void {
+    const graphicsTablets = this.methods.getResult('fakeGraphicsTablets');
+    for (const observer of this.graphicsTabletObservers) {
+      observer.onGraphicsTabletListUpdated(graphicsTablets);
+    }
+  }
+
   observeKeyboardSettings(observer: KeyboardObserverInterface): void {
     this.keyboardObservers.push(observer);
     this.notifyKeboardListUpdated();
@@ -250,5 +266,16 @@ export class FakeInputDeviceSettingsProvider implements
   observePointingStickSettings(observer: PointingStickObserverInterface): void {
     this.pointingStickObservers.push(observer);
     this.notifyPointingStickListUpdated();
+  }
+
+  observeStylusSettings(observer: StylusObserverInterface): void {
+    this.stylusObservers.push(observer);
+    this.notifyStylusListUpdated();
+  }
+
+  observeGraphicsTabletSettings(observer: GraphicsTabletObserverInterface):
+      void {
+    this.graphicsTabletObservers.push(observer);
+    this.notifyGraphicsTabletUpdated();
   }
 }
