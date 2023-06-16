2e0e3f005a8f8cc6c7c60f0d59b4c6a47d4d1eb9
Hiroshige Hayashizaki <hiroshige@chromium.org>
2023-06-16 18:09:22 +0000

    Remove NoVarySearchHelper::prefetches_with_no_vary_search_
    To centralize the source of truth and align No-Vary-Search
    and No-Vary-Search hint, this CL removes
    `NoVarySearchHelper::prefetches_with_no_vary_search_`
    and instead iterate over
    `PrefetchDocumentManager::all_prefetches_`
    in `IterateCandidates()` to resolve No-Vary-Search.
    
    This is preparation for eventually moving the source of
    truth for No-Vary-Search further into PrefetchService
    (i.e. more like No-Vary-Search hint).
    
    The `net::HttpNoVarySearchData` stored in
    `prefetches_with_no_vary_search_` is moved to
    `PrefetchContainer::no_vary_search_data_` and is checked
    during iteration.
    
    Bug: 1448731, 1449360, 1430331
    Change-Id: Ifb239ab6efe6911eaea8d4637f10f19651ade254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571842
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Reviewed-by: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158966}

--- content/browser/preloading/preloading_data_impl.cc
+++ content/browser/preloading/preloading_data_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/strings/strcat.h"
 #include "content/browser/preloading/prefetch/no_vary_search_helper.h"
+#include "content/browser/preloading/prefetch/prefetch_container.h"
 #include "content/browser/preloading/prefetch/prefetch_document_manager.h"
 #include "content/browser/preloading/preloading.h"
 #include "content/browser/preloading/preloading_attempt_impl.h"
@@ -83,10 +84,10 @@ PreloadingDataImpl::GetSameURLAndNoVarySearchURLMatcher(
           return true;
         }
 
-        const absl::optional<GURL> match_url =
-            prefetch_doc_manager->GetNoVarySearchHelper().MatchUrl(
-                navigated_url);
-        return match_url == predicted_url;
+        base::WeakPtr<PrefetchContainer> prefetch_container =
+            prefetch_doc_manager->MatchUrl(navigated_url);
+        return prefetch_container &&
+               prefetch_container->GetURL() == predicted_url;
       },
       manager, destination_url);
 }
