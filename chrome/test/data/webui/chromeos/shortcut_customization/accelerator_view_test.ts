fa38a5669e77bbf8902522ee712a50b5b0b3cfc7
Longbo Wei <longbowei@google.com>
2023-06-16 01:33:24 +0000

    shortcuts: Add key icons for keys during add/edit
    Handle special cases:
    1. 'space' -> the 'space' key which is displayed as ' ' in the dialog,
       is updated to display as 'space' text.
    2. 'overview' -> the 'overview' key is displayed as 'F4' during
       add/edit. We have updated the keyDisplay to be 'LaunchApplication1'
       then it maps to the 'overview' icon based on keyToIconNameMap.
    3. 'search' ->  the 'search' key now displays as a launcher/search icon
       by updating key='search' to key='meta'.
    
    * Note: Previously, in the getIconIdForKey() method, we set keyState
      to MODIFIER_SELECTED if the key was a META key. This was done in
      order to display the LWIN key as a modifier key and has the same css
      style as a modifier key. However, to avoid setting the keyState for
      the meta key from the view-dialog, we need to separate the LWIN key
      and the META key.
    
    Video:
    http://go/scrcast/NDk5MTEwMjczOTI4Mzk2OHxiNGZhMTE0OC1kMQ
    
    Bug: b/216049298
    Test: browser_tests --gtest_filter=ShortcutCustomizationApp*
    Change-Id: Ia4915ddcad3e1519be2877abc2db76c3d2ee9dfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608892
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158566}

--- chrome/test/data/webui/chromeos/shortcut_customization/accelerator_view_test.ts
+++ chrome/test/data/webui/chromeos/shortcut_customization/accelerator_view_test.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://shortcut-customization/js/accelerator_view.js';
 import 'chrome://webui-test/mojo_webui_test_support.js';
 
+import {IronIconElement} from '//resources/polymer/v3_0/iron-icon/iron-icon.js';
 import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -360,5 +361,70 @@ suite('acceleratorViewTest', function() {
           testCase.expectEditIconVisible,
           !getEditIcon().hasAttribute('hidden'));
     }
+  });
+
+  test('KeyDisplayAndIconDuringEdit', async () => {
+    viewElement = initAcceleratorViewElement();
+    await flushTasks();
+    const acceleratorInfo = createStandardAcceleratorInfo(
+        Modifier.ALT,
+        /*key=*/ 221,
+        /*keyDisplay=*/ ']');
+    viewElement.acceleratorInfo = acceleratorInfo;
+    viewElement.source = AcceleratorSource.kAsh;
+    viewElement.action = 1;
+    await flush();
+
+    // Enable the edit view.
+    viewElement.viewState = ViewState.EDIT;
+    await flush();
+
+    const pendingKey = getInputKey('#pendingKey');
+
+    const fakeResult: AcceleratorResultData = {
+      result: AcceleratorConfigResult.kConflict,
+      shortcutName: {data: [1]},
+    };
+    provider.setFakeReplaceAcceleratorResult(fakeResult);
+
+    // Simulate SHIFT + SPACE, expect the key display to be 'space'.
+    viewElement.dispatchEvent(new KeyboardEvent('keydown', {
+      key: ' ',
+      code: 'Space',
+      shiftKey: true,
+    }));
+
+    await flush();
+    assertEquals('space', pendingKey.key);
+
+    // Simulate SHIFT + OVERVIEW, expect the key display to be
+    // 'LaunchApplication1' and the icon to be 'overview'.
+    viewElement.dispatchEvent(new KeyboardEvent('keydown', {
+      key: 'F4',
+      code: 'ShowAllWindows',
+      shiftKey: true,
+    }));
+    await flush();
+
+    assertEquals('LaunchApplication1', pendingKey.key);
+    const keyIconElement =
+        pendingKey.shadowRoot!.querySelector('#key-icon') as IronIconElement;
+    assertEquals('shortcut-customization-keys:overview', keyIconElement.icon);
+
+    // Simulate SHIFT + BRIGHTNESS_UP, expect the key display to be
+    // 'BrightnessUp' and the icon to be 'display-brightness-up'.
+    viewElement.dispatchEvent(new KeyboardEvent('keydown', {
+      key: 'BrightnessUp',
+      code: 'BrightnessUp',
+      shiftKey: true,
+    }));
+    await flush();
+
+    assertEquals('BrightnessUp', pendingKey.key);
+    const keyIconElement2 =
+        pendingKey.shadowRoot!.querySelector('#key-icon') as IronIconElement;
+    assertEquals(
+        'shortcut-customization-keys:display-brightness-up',
+        keyIconElement2.icon);
   });
 });
