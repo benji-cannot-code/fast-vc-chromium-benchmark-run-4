7f533c6c0d92103b2a99865a6f87f089e9ead7f8
Andrew Grieve <agrieve@chromium.org>
2023-06-16 16:25:02 +0000

    Fix a couple tests in TabListMediatorUnitTest that rely on state from other tests
    Bug: None
    Change-Id: I0bfe629e79b8d8b6a178d778338cf065e483b7a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619101
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158885}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
@@ -3136,6 +3136,7 @@ public class TabListMediatorUnitTest {
 
     @Test
     public void testListObserver_OnItemRangeInserted() {
+        PriceTrackingFeatures.setIsSignedInAndSyncEnabledForTesting(true);
         setPriceTrackingEnabledForTesting(true);
         mMediator = new TabListMediator(mActivity, mModel, TabListMode.GRID, mTabModelSelector,
                 getTabThumbnailCallback(), mTitleProvider, mTabListFaviconProvider, true, null,
@@ -3153,6 +3154,7 @@ public class TabListMediatorUnitTest {
 
     @Test
     public void testListObserver_OnItemRangeRemoved() {
+        PriceTrackingFeatures.setIsSignedInAndSyncEnabledForTesting(true);
         setPriceTrackingEnabledForTesting(true);
         mMediator = new TabListMediator(mActivity, mModel, TabListMode.GRID, mTabModelSelector,
                 getTabThumbnailCallback(), mTitleProvider, mTabListFaviconProvider, true, null,
