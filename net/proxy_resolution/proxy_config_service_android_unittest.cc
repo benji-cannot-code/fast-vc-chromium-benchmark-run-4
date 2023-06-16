eebe800ee53ecfe1f594b76201d64c72ac2e8663
Andrew Grieve <agrieve@chromium.org>
2023-06-16 18:14:58 +0000

    JNI annotation processor -> srcjars (batch 1)
    * android_webview/BUILD.gn
    * android_webview/glue/BUILD.gn
    * android_webview/test/BUILD.gn
    * mojo/public/java/system/BUILD.gn
    * net/android/BUILD.gn
    * services/device/public/java/BUILD.gn
    * testing/android/native_test/BUILD.gn
    * ui/android/BUILD.gn
    * url/BUILD.gn
    * weblayer/browser/java/BUILD.gn
    
    Bug: 1410871
    Change-Id: Ib87fcdc30208552d65f6adbfee16ca6c5f1bd2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616415
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158973}

--- net/proxy_resolution/proxy_config_service_android_unittest.cc
+++ net/proxy_resolution/proxy_config_service_android_unittest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
-#include "net/net_test_jni_headers/AndroidProxyConfigServiceTestUtil_jni.h"
+#include "net/android/net_tests_jni/AndroidProxyConfigServiceTestUtil_jni.h"
 #include "net/proxy_resolution/proxy_config_with_annotation.h"
 #include "net/proxy_resolution/proxy_info.h"
 #include "net/test/test_with_task_environment.h"
