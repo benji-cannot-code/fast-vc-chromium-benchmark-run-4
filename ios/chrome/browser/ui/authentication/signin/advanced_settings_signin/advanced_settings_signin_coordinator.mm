bdade9d91c3f185aec4601381cfae15c869902ed
Jood Hajeer <jood@google.com>
2023-06-16 13:35:53 +0000

    [iOS] Show snackbar confirming sign out from account settings.
    Show a snackbar after signing out from the account settings page.
    
    Bug: 1447017
    Change-Id: I8305cc7d8cda7b7af4c818be1a30e307d243e9d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618034
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158791}

--- ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_coordinator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/foundation_util.h"
 #import "base/metrics/user_metrics.h"
+#import "base/notreached.h"
 #import "components/signin/public/identity_manager/identity_manager.h"
 #import "ios/chrome/browser/shared/coordinator/alert/action_sheet_coordinator.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
@@ -225,6 +226,10 @@ using l10n_util::GetNSString;
 
 - (NSString*)manageSyncSettingsCoordinatorTitle {
   return l10n_util::GetNSString(IDS_IOS_SYNC_SETTINGS_TITLE);
+}
+
+- (void)showSignOutToast {
+  NOTREACHED_NORETURN();
 }
 
 - (void)manageSyncSettingsCoordinatorNeedToOpenChromeSyncWebPage:
