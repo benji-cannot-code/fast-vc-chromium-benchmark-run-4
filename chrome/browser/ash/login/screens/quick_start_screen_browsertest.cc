cb4c769279bfe205fa3d8862bc45196ab5319c0f
Joe Antonetti <joeantonetti@google.com>
2023-06-16 00:24:01 +0000

    [Nearby Share] Revert breaking changes to NearbyProcess creation.
    Effectively revert crrev.com/c/4496546 (key word "effectively"). This CL
    reverts the changes that crrev.com/c/4496546 made to NearbyProcessManagerFactory
    and NearbyDependenciesProviderFactory.
    
    crrev.com/c/4496546 changed how the Nearby utility process and the Nearby Share
    + Phone Hub features are instantiated. Before the breaking CL, just 1 utility
    process and 1 keyed service per feature was instantiated. After the breaking Cl,
    more than 1 of each is created -- causing bugs like b/287061553.
    
    We are unable to actually revert crrev.com/c/4496546 because doing so causes
    regression in OOBE, and we're unable to understand why. Because this fix needs
    to be land ASAP and be merged back to 115, we chose to revert just the files
    that we know to be causing b/287061553.
    
    b/287061553 is a breaking regression in the Nearby Share feature.
    
    Test:
      - Reproduced b/287061553 before applying this CL.
      - Did not reproduce b/287061553 after applying this CL.
      - All unit tests passed; adjusted Quick Start tests to be behind the QS feature flag.
    
    Bug: b/287061553
    Change-Id: If5e11a3c8870b21703fef49b2175e32a22686db6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618867
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158538}

--- chrome/browser/ash/login/screens/quick_start_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/quick_start_screen_browsertest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/constants/ash_features.h"
-#include "ash/public/cpp/login_accelerators.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/fake_target_device_connection_broker.h"
@@ -43,10 +42,12 @@ constexpr test::UIPath kQuickStartPinCode = {QuickStartView::kScreenId.name,
                                              kPinCodeWrapper};
 }  // namespace
 
-class QuickStartBrowserTestBase : public OobeBaseTest {
+class QuickStartBrowserTest : public OobeBaseTest {
  public:
-  QuickStartBrowserTestBase() = default;
-  ~QuickStartBrowserTestBase() override = default;
+  QuickStartBrowserTest() {
+    feature_list_.InitAndEnableFeature(features::kOobeQuickStart);
+  }
+  ~QuickStartBrowserTest() override = default;
 
   void SetUpInProcessBrowserTestFixture() override {
     OobeBaseTest::SetUpInProcessBrowserTestFixture();
@@ -86,13 +87,6 @@ class QuickStartBrowserTestBase : public OobeBaseTest {
  protected:
   quick_start::FakeTargetDeviceConnectionBroker::Factory
       connection_broker_factory_;
-};
-
-class QuickStartBrowserTest : public QuickStartBrowserTestBase {
- public:
-  QuickStartBrowserTest() {
-    feature_list_.InitAndEnableFeature(features::kOobeQuickStart);
-  }
 
  private:
   base::test::ScopedFeatureList feature_list_;
@@ -106,8 +100,6 @@ class QuickStartNotDeterminedBrowserTest : public QuickStartBrowserTest {
             kUndetermined);
   }
 };
-
-class QuickStartAcceleratorBrowserTest : public QuickStartBrowserTestBase {};
 
 IN_PROC_BROWSER_TEST_F(QuickStartNotDeterminedBrowserTest,
                        ButtonVisibleOnWelcomeScreen) {
@@ -170,19 +162,6 @@ IN_PROC_BROWSER_TEST_F(QuickStartBrowserTest, PinCode) {
                       ".textContent === '" + digit + "'")
         ->Wait();
   }
-}
-
-IN_PROC_BROWSER_TEST_F(QuickStartAcceleratorBrowserTest,
-                       ButtonVisibleOnWelcomeScreen) {
-  test::WaitForWelcomeScreen();
-  test::OobeJS().ExpectHiddenPath(kQuickStartButtonPath);
-
-  WizardController::default_controller()->HandleAccelerator(
-      LoginAcceleratorAction::kEnableQuickStart);
-
-  test::OobeJS()
-      .CreateVisibilityWaiter(/*visibility=*/true, kQuickStartButtonPath)
-      ->Wait();
 }
 
 IN_PROC_BROWSER_TEST_F(QuickStartBrowserTest, ClickingOnButtonEntersScreen) {
