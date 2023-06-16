68bf5aa503f3cac4329e5f222853c23cb5b9f1f4
Arthur Milchior <arthurmilchior@chromium.org>
2023-06-16 17:44:08 +0000

    [iOS] HistorySyncMediator dealloc checks stop was called
    Bug: None
    Change-Id: I696f6c68c26b70543fe6d8a3113defa95f92a26a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608128
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158944}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.h"
 
+#import "base/check.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -13,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)disconnect {
   self.consumer = nil;
+}
+
+- (void)dealloc {
+  CHECK(!self.consumer);
 }
 
 @end
