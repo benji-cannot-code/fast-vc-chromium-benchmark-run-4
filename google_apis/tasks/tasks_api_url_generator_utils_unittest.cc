a71d859e542c30df8672e478045bbccf9f4187bf
Artsiom Mitrokhin <amitrokhin@google.com>
2023-06-16 14:11:13 +0000

    Sort tasks as they appear in the companion web app
    For this purpose Task's `position` [1] field is used.
    
    [1] https://developers.google.com/tasks/reference/rest/v1/tasks
    
    Bug: b/287323797
    Change-Id: Ie3ed0d9526c2acc644730ddc3128bffaafa7f329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615204
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158806}

--- google_apis/tasks/tasks_api_url_generator_utils_unittest.cc
+++ google_apis/tasks/tasks_api_url_generator_utils_unittest.cc
@@ -32,8 +32,8 @@ TEST(TasksApiUrlGeneratorUtilsTest, ReturnsListTasksUrl) {
                             /*max_results=*/absl::nullopt,
                             /*page_token=*/""),
             "https://tasks.googleapis.com/tasks/v1/lists/task-list-id/tasks"
-            "?fields=kind%2Citems(id%2Ctitle%2Cstatus%2Cparent%2Cdue%2Clinks("
-            "type))%2CnextPageToken"
+            "?fields=kind%2Citems(id%2Ctitle%2Cstatus%2Cparent%2Cposition"
+            "%2Cdue%2Clinks(type))%2CnextPageToken"
             "&showCompleted=false");
 }
 
@@ -42,8 +42,8 @@ TEST(TasksApiUrlGeneratorUtilsTest, ReturnsListTasksUrlWithOptionalArgs) {
                             /*max_results=*/100,
                             /*page_token=*/"qwerty"),
             "https://tasks.googleapis.com/tasks/v1/lists/task-list-id/tasks"
-            "?fields=kind%2Citems(id%2Ctitle%2Cstatus%2Cparent%2Cdue%2Clinks("
-            "type))%2CnextPageToken"
+            "?fields=kind%2Citems(id%2Ctitle%2Cstatus%2Cparent%2Cposition%"
+            "2Cdue%2Clinks(type))%2CnextPageToken"
             "&showCompleted=true"
             "&maxResults=100"
             "&pageToken=qwerty");
