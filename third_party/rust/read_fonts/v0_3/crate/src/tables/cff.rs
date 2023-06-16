ba7c76fc98b6efa6870019b4b53e9467f272e3f3
Dominik Röttsches <drott@chromium.org>
2023-06-16 14:08:36 +0000

    [Fontations-backend] Roll Fontations libraries
    Includes fixes for variable font metrics when font has no HVAR table.
    
    Bug: 1446251
    Change-Id: I678114c8d922140766a5f28f93ae263964f2e2f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4620529
    Reviewed-by: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158803}

--- third_party/rust/read_fonts/v0_3/crate/src/tables/cff.rs
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! The [CFF](https://learn.microsoft.com/en-us/typography/opentype/spec/cff) table
-
-include!("../../generated/generated_cff.rs");
--- third_party/rust/read_fonts/v0_3/crate/src/tables/cff2.rs
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! The [CFF2](https://learn.microsoft.com/en-us/typography/opentype/spec/cff2) table
-
-include!("../../generated/generated_cff2.rs");
