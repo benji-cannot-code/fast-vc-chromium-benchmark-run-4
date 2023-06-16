4493f84a204bdb200557eac4901b9313b2a3ab04
Brett Wilson <brettw@chromium.org>
2023-06-16 21:04:01 +0000

    [PPAPI] Remove assertions on plugin behavior.
    The browser is not in control of plugin behavior so should not be
    asserting based on it.
    
    This converts the assertions in ppapi/host that are dependent on plugin
    behavior to DLOG(WARNING).
    
    Bug: 1454928
    Change-Id: I93be0e668579acff8a66145fb4047c0021717039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615554
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159068}

--- ppapi/host/ppapi_host.cc
+++ ppapi/host/ppapi_host.cc
@@ -119,7 +119,11 @@ void PpapiHost::SendUnsolicitedReplyWithHandles(
   TRACE_EVENT2("ppapi_proxy", "PpapiHost::SendUnsolicitedReplyWithHandles",
                "Class", IPC_MESSAGE_ID_CLASS(msg.type()), "Line",
                IPC_MESSAGE_ID_LINE(msg.type()));
-  DCHECK(resource);  // If this fails, host is probably pending.
+  if (!resource) {
+    // Host probably still in a pending state, nothing we can do.
+    return;
+  }
+
   proxy::ResourceMessageReplyParams params(resource, 0);
   for (auto& handle : *handles)
     params.AppendHandle(std::move(handle));
@@ -198,7 +202,11 @@ void PpapiHost::OnHostMsgResourceSyncCall(
                IPC_MESSAGE_ID_LINE(nested_msg.type()));
   // Sync messages should always have callback set because they always expect
   // a reply from the host.
-  DCHECK(params.has_callback());
+  if (!params.has_callback()) {
+    DLOG(WARNING) << "Expected callback for sync message.";
+    return;
+  }
+
   // Stash the |reply_msg| in the context so that it can be used to reply
   // to the sync message.
   HostMessageContext context(params, reply_msg);
@@ -240,7 +248,7 @@ void PpapiHost::OnHostMsgResourceCreated(
       CreateResourceHost(params.pp_resource(), instance, nested_msg);
 
   if (!resource_host.get()) {
-    NOTREACHED();
+    DLOG(WARNING) << "Resource host creation failed.";
     return;
   }
 
@@ -256,7 +264,7 @@ void PpapiHost::OnHostMsgAttachToPendingHost(PP_Resource pp_resource,
       pending_resource_hosts_.find(pending_host_id);
   if (found == pending_resource_hosts_.end()) {
     // Plugin sent a bad ID.
-    NOTREACHED();
+    DLOG(WARNING) << "Bad pending host ID = " << pending_host_id;
     return;
   }
   found->second->SetPPResourceForPendingHost(pp_resource);
@@ -267,7 +275,7 @@ void PpapiHost::OnHostMsgAttachToPendingHost(PP_Resource pp_resource,
 void PpapiHost::OnHostMsgResourceDestroyed(PP_Resource resource) {
   ResourceMap::iterator found = resources_.find(resource);
   if (found == resources_.end()) {
-    NOTREACHED();
+    DLOG(WARNING) << "Unknown resource ID " << resource;
     return;
   }
   // Invoking the HostResource destructor might result in looking up the
