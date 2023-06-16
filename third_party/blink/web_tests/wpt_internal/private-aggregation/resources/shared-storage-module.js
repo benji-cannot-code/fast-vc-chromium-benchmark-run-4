59fd076ad0888292eabecb27d4c6d039831c29cd
Alex Turner <alexmt@chromium.org>
2023-06-16 16:32:13 +0000

    Add additional end-to-end success Private Aggregation WPTs
    Renames some files as a drive-by and fixes .ini files to simply disable
    the tests that have exclusive virtual tests instead of having errors
    as expected.
    
    Bug: 1452248
    Change-Id: I7e30e4ce4bf2fa8d2d3f5fb8e56ea21563851e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617351
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158889}

--- third_party/blink/web_tests/wpt_internal/private-aggregation/resources/shared-storage-module.js
+++ third_party/blink/web_tests/wpt_internal/private-aggregation/resources/shared-storage-module.js
@@ -2,10 +2,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ContributeToHistogramOperation {
   async run(data) {
     if (data.enableDebugMode) {
-      privateAggregation.enableDebugMode();
+      privateAggregation.enableDebugMode(data.enableDebugModeArgs);
     }
     for (const contribution of data.contributions) {
       privateAggregation.contributeToHistogram(contribution);
+    }
+    if (data.enableDebugModeAfterOp) {
+      privateAggregation.enableDebugMode(data.enableDebugModeArgs);
     }
   }
 }
