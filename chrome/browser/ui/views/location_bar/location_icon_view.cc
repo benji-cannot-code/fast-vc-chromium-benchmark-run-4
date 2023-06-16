c3a6bbabae0bc81398535e6d45c1c491b84a0ce6
Keren Zhu <kerenzhu@chromium.org>
2023-06-16 08:47:56 +0000

    [CR2023] Fix LocationIconViewBrowserTest.InkDropMode
    This test was crashing because LocationIconView is not in a views tree
    and as a result does not have access to a Color Provider.
    
    Bug: 1443572
    Change-Id: If2782abd0455ae6730dc2e46c9c19140be080100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617848
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1158705}

--- chrome/browser/ui/views/location_bar/location_icon_view.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view.cc
@@ -297,6 +297,7 @@ void LocationIconView::UpdateIcon() {
 
 void LocationIconView::UpdateBackground() {
   if (OmniboxFieldTrial::IsChromeRefreshIconsEnabled()) {
+    CHECK(GetColorProvider());
     SetBackground(views::CreateRoundedRectBackground(
         GetColorProvider()->GetColor(kColorPageInfoBackground), height() / 2));
   } else {
