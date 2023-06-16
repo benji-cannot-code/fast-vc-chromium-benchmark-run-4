13e9a1aff19be3d750ba2144ed504523a6a6309e
Lukasz Anforowicz <lukasza@chromium.org>
2023-06-16 21:03:51 +0000

    Manual raw_ptr rewrite: cc/raster/scoped_grcontext_access.h
    This is a partial / single-file reland of https://crrev.com/c/4548763.
    More granular CLs will hopefully help pinpoint which of the changes in
    the original CL is responsible for the performance regression.
    
    Bug: 1448053, 1446392
    Change-Id: I77d0ae63b433544bafda548ec65d86cc3c58d9ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582609
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159067}

--- cc/raster/scoped_grcontext_access.h
+++ cc/raster/scoped_grcontext_access.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_RASTER_SCOPED_GRCONTEXT_ACCESS_H_
 #define CC_RASTER_SCOPED_GRCONTEXT_ACCESS_H_
 
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "components/viz/common/gpu/raster_context_provider.h"
 #include "gpu/command_buffer/client/raster_interface.h"
 
@@ -25,9 +25,7 @@ class ScopedGrContextAccess {
   }
 
  private:
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #union
-  RAW_PTR_EXCLUSION viz::RasterContextProvider* context_provider_;
+  raw_ptr<viz::RasterContextProvider> context_provider_;
 };
 
 #endif  // CC_RASTER_SCOPED_GRCONTEXT_ACCESS_H_
