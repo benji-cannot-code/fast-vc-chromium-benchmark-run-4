08eb8a990e90b1bc661f73e6e38b20c1326ceb94
Ramin Halavati <rhalavati@chromium.org>
2023-06-16 08:00:21 +0000

    DestroyRegularProfileBeforeOTRs waits for profiles to be destroyed.
    The test expects that the off-the-record profiles would be destroyed
    immediately after calling `DestroyOriginalProfileWhenAppropriate` which
    is wrong as the OTR profiles are sometimes given a bit of time to
    close all their dependencies.
    
    Bug: 1454090
    Change-Id: Ia50c269fbec88cc08844f4cb290078961764ac05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4614511
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158687}

--- chrome/test/base/profile_destruction_waiter.h
+++ chrome/test/base/profile_destruction_waiter.h
@@ -14,9 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
-// Allows to wait until a new profile is created in tests.
-// This waiter works only for normal (on-the-record) profiles, as it observes
-// the ProfileManager that doesn't own OTR profiles.
+// Allows to wait until a profile is destroyed in tests.
 class ProfileDestructionWaiter : public ProfileObserver {
  public:
   explicit ProfileDestructionWaiter(Profile* profile);
