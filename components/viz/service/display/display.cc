fc9abe8657168ce87e5dbc86addffb4121b2ac94
Eriko Kurimoto <elkurin@chromium.org>
2023-06-16 16:59:06 +0000

    viz: Fix dangling ptr DisplayClient in viz::Display
    This CL fixes dangling ptr in viz::Display.
    DisplayClient instance is marked as DanglingUntriaged due to
    SkiaDelegatedInRenderTest and DirectLayerTreeFrameSink.
    
    SkiaDelegatedInRenderTest:
    `display_` instance depends on `client_` while `client_` is destructed
    before `display_` in this class since `display_` is declared in its
    base class `DisplayTest`.
    
    DirectLayerTreeFrameSink:
    Display and DirectLayerTreeFrameSink which inherits DisplayClient
    depends on each other.
    Another way is to make `display_` inside DirectLayerTreeFrameSink
    std::unique_ptr and pass the ownership from InProcessContextFactory,
    but this causes another dangling ptr due to the dependency chain.
    (dependency overview: https://drive.google.com/file/d/140PRvdYK-CDwxZN-exCq8N_t_TOrYNaR/view?usp=sharing&resourcekey=0-8y0QNCy7oTmT8lO4ZIcjxA)
    We rather keep InProcessContextFactory logic as is.
    
    DisplayClient outlives in all other usages.
    
    Bug: 1454709
    Change-Id: Ia3e1f11ba26b3042ed433490cdf912557afdeeef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4612328
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158910}

--- components/viz/service/display/display.cc
+++ components/viz/service/display/display.cc
@@ -1506,4 +1506,9 @@ void Display::InitDelegatedInkPointRendererReceiver(
   }
 }
 
+void Display::ResetDisplayClientForTesting(DisplayClient* old_client) {
+  CHECK_EQ(client_, old_client);
+  client_ = nullptr;
+}
+
 }  // namespace viz
