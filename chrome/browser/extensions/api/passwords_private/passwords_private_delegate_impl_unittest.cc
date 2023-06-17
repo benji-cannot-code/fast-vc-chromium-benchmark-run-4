e4d8319d73b70f4ecb24b1bc1371215bac830efd
Daniel Murphy <dmurph@google.com>
2023-06-17 00:18:05 +0000

    [PasswordManager] Disabling flaky test ShowAddShortcutDialog
    Bug: 1455574
    Change-Id: I6e0005ef97fe60e469e530362169d678c0a19c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622690
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Owners-Override: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159203}

--- chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl_unittest.cc
+++ chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl_unittest.cc
@@ -1593,7 +1593,8 @@ TEST_F(PasswordsPrivateDelegateImplTest,
 
 #endif
 
-TEST_F(PasswordsPrivateDelegateImplTest, ShowAddShortcutDialog) {
+// TODO(http://crbug.com/1455574) Re-enable.
+TEST_F(PasswordsPrivateDelegateImplTest, DISABLED_ShowAddShortcutDialog) {
   base::HistogramTester histogram_tester;
   // Set up a browser instance and simulate a navigation.
   Browser::CreateParams params(profile(), /*user_gesture=*/true);
