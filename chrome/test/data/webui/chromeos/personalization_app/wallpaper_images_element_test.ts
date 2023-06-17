6691343816ce6300958dd5dfa0f56aeec4f02e67
Jeffrey Young <cowmoo@google.com>
2023-06-17 20:12:52 +0000

    personalization: move time of day logic
    Create a new function selectTimeOfDayWallpaper to coordinate
    setting wallpaper and theme.
    
    Bug: b:284023808
    Test: browser_tests --gtest_filter=*WallpaperImages*
    Change-Id: I6a7b9f3b8ed1c27b98e522640b9d98cac8192ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616047
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159330}

--- chrome/test/data/webui/chromeos/personalization_app/wallpaper_images_element_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/wallpaper_images_element_test.ts
@@ -6,22 +6,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://personalization/strings.m.js';
 import 'chrome://webui-test/mojo_webui_test_support.js';
 
-import {ColorScheme, OnlineImageType, PersonalizationRouter, TimeOfDayWallpaperDialog, WallpaperGridItem, WallpaperImages} from 'chrome://personalization/js/personalization_app.js';
+import {ColorScheme, DEFAULT_COLOR_SCHEME, OnlineImageType, PersonalizationRouter, TimeOfDayWallpaperDialog, WallpaperGridItem, WallpaperImages} from 'chrome://personalization/js/personalization_app.js';
 import {assertDeepEquals, assertEquals, assertFalse, assertNotEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 
 import {baseSetup, initElement, teardownElement} from './personalization_app_test_utils.js';
 import {TestPersonalizationStore} from './test_personalization_store.js';
+import {TestThemeProvider} from './test_theme_interface_provider.js';
 import {TestWallpaperProvider} from './test_wallpaper_interface_provider.js';
 
 suite('WallpaperImagesTest', function() {
   let wallpaperImagesElement: WallpaperImages|null;
   let wallpaperProvider: TestWallpaperProvider;
+  let themeProvider: TestThemeProvider;
   let personalizationStore: TestPersonalizationStore;
 
   setup(() => {
     const mocks = baseSetup();
     wallpaperProvider = mocks.wallpaperProvider;
+    themeProvider = mocks.themeProvider;
     personalizationStore = mocks.personalizationStore;
   });
 
@@ -412,6 +415,18 @@ suite('WallpaperImagesTest', function() {
         wallpaperImagesElement.shadowRoot!.querySelector(
             TimeOfDayWallpaperDialog.is),
         'clicking accept dismisses the dialog');
+
+    const scheme = await themeProvider.whenCalled('setColorScheme');
+    assertEquals(
+        DEFAULT_COLOR_SCHEME, scheme,
+        'time of day wallpaper sets default color scheme');
+
+    const autoScheduleEnabled =
+        await themeProvider.whenCalled('setColorModeAutoScheduleEnabled');
+    assertTrue(
+        autoScheduleEnabled,
+        'time of day wallpaper sets auto schedule enabled');
+
     const [assetId, previewMode] =
         await wallpaperProvider.whenCalled('selectWallpaper');
     assertEquals(3n, assetId, 'correct asset id is passed');
