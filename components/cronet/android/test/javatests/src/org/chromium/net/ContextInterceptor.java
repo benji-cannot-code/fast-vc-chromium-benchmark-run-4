8322c156a9e50975a6dae705df519f19970c66bb
Etienne Dechamps <edechamps@google.com>
2023-06-19 17:04:21 +0000

    Support mutating application metadata from CronetTestRule
    This makes it much simpler to inject custom application manifest values
    in the code under test, as evidenced by the improvements to
    CronetManifestTest code.
    
    Change-Id: Ie325b6b33c24c8697c2940533fdc2b77868f082a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608056
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1159669}

---
+++ components/cronet/android/test/javatests/src/org/chromium/net/ContextInterceptor.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+
+/**
+ * An interface for providing a replacement for an Android {@link Context}. Useful for
+ * faking/mocking Context calls in tests.
+ *
+ * @see CronetTestRule.CronetTestFramework#interceptContext
+ */
+public interface ContextInterceptor {
+    /**
+     * Provides a {@link Context} that the current context should be replaced with.
+     *
+     * @param context the original Context to be replaced
+     * @return the new Context. Typically this would forward most calls to the original context, for
+     * example using {@link ContextWrapper}.
+     */
+    public Context interceptContext(Context context);
+}
