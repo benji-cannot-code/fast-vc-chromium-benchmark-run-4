04844402acb2d40964a5ad1aeb30073c4d782f26
Jonathan Hao <phao@chromium.org>
2023-06-19 09:47:21 +0000

    Inherit COOP: restrict-properties and un-suppress opener
    To make the `Cross-Origin-Opener-Policy: restrict-properties` (COOP:RP)
    more useful, we'd like to let cross-origin iframes inherit this policy
    from their parent and keep the opener connection between windows that
    the iframes open. See
    https://github.com/hemeryar/coi-with-popups/blob/main/docs/cross_origin_iframe_popup.MD
    for more explanations.
    
    In https://crrev.com/c/4404116, we've bundled the origin that sets the
    COOP with the policy.  This CL is to use the stored origin in COOP
    algorithms:
    1. In `RenderFrameHostImpl::DidCommitNewDocument`, we let embedded
    documents inherit the COOP:RP bundle when the policy is COOP:RP.
    2. To keep the opener connection, we return false in
    `COOPSuppressOpener` when COOP value is RP.
    3. Since we allow the popup's initial empty document and its subframes
    to stay in the process, we had to restrict its crossOriginIsolated
    capability. Otherwise, a cross-origin iframe can get around permission
    policy that restricts it from using crossOriginIsolated APIs.
      - For the initial empty document, we pass a boolean to the renderer
      via CreateNewWindow.
      - For the subframes of the initial empty document, we pass a boolean
      via CommitNavigation.
    
    This CL also adds new test cases for
    1. Cross-origin iframe opener v.s. same-origin iframe opener.
    2. Checking the popup's subframe's `crossOriginIsolated`.
    3. Checking popup's `crossOriginIsolated` after it navigates away from
    the initial empty document. (This is to make sure that we don't
    restrict the `crossOriginIsolated` when we shouldn't)
    
    Bug: 1385827
    Change-Id: I39cd140f70b628114205640af2220ec2de45ac06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4479858
    Auto-Submit: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159514}

--- services/network/public/cpp/cross_origin_opener_policy.cc
+++ services/network/public/cpp/cross_origin_opener_policy.cc
@@ -110,4 +110,11 @@ void AugmentCoopWithCoep(CrossOriginOpenerPolicy* coop,
   }
 }
 
+bool IsRelatedToCoopRestrictProperties(
+    mojom::CrossOriginOpenerPolicyValue value) {
+  return value == mojom::CrossOriginOpenerPolicyValue::kRestrictProperties ||
+         value ==
+             mojom::CrossOriginOpenerPolicyValue::kRestrictPropertiesPlusCoep;
+}
+
 }  // namespace network
