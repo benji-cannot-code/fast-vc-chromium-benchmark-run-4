9632fad1e5293a910019b8809044cc6b42ba6efe
Luciano Pacheco <lucmult@chromium.org>
2023-06-19 03:48:51 +0000

    Files Office: Record UMA for upload to cloud latency
    Change the OneDrive and Google Drive upload handlers to report back
    the total upload size to be able to record the UMA.
    
    Bug: b:264309906
    Change-Id: I6ffc8207e6473e3629cf7f26298919a9c859e9ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4612728
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159450}

--- chrome/browser/ui/webui/ash/cloud_upload/one_drive_upload_handler.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/one_drive_upload_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/timer/elapsed_timer.h"
 #include "chrome/browser/ash/file_manager/copy_or_move_io_task.h"
 #include "chrome/browser/ash/file_manager/file_tasks.h"
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
@@ -34,8 +35,9 @@ constexpr char kUploadResultMetricName[] =
 // Runs the callback provided to `OneDriveUploadHandler::Upload`.
 void OnUploadDone(scoped_refptr<OneDriveUploadHandler> one_drive_upload_handler,
                   OneDriveUploadHandler::UploadCallback callback,
-                  const FileSystemURL& uploaded_file_url) {
-  std::move(callback).Run(uploaded_file_url);
+                  const FileSystemURL& uploaded_file_url,
+                  int64_t upload_size) {
+  std::move(callback).Run(uploaded_file_url, upload_size);
 }
 
 }  // namespace
@@ -189,7 +191,7 @@ void OneDriveUploadHandler::OnEndUpload(const FileSystemURL& uploaded_file_url,
     }
   }
   if (callback_) {
-    std::move(callback_).Run(uploaded_file_url);
+    std::move(callback_).Run(uploaded_file_url, upload_size_);
   }
 }
 
@@ -205,6 +207,7 @@ void OneDriveUploadHandler::OnIOTaskStatus(
       return;
     case file_manager::io_task::State::kInProgress:
       if (status.total_bytes > 0) {
+        upload_size_ = status.total_bytes;
         notification_manager_->ShowUploadProgress(
             100 * status.bytes_transferred / status.total_bytes);
       }
