b62eadcb0cb4d8ad6f9c8bfe2faa03082cf43036
Boris Sazonov <bsazonov@google.com>
2023-06-19 13:06:50 +0000

    [iOS] Show sync promo in Bookmark Manager if the user is signed in
    Before this CL, users that have EnableBookmarksAccountStorage feature
    enabled didn't have sync promo in the bookmark manager if they were
    signed in (even if IsAccountBookmarkStorageOptedIn returns false).
    This CL changes this behavior - now a sync promo is shown iff
    IsAccountBookmarkStorageOptedIn is false.
    
    This CL also enables EnableBookmarksAccountStorage for
    BookmarksPromoTestCase.testTurnOnSyncPromo that now works correctly.
    
    Bug: 1455031, 1455018
    Change-Id: I85b127b5f2407733bcd5c0379cfd2a1a8d398f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4614877
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159574}

--- ios/chrome/browser/ui/bookmarks/home/bookmarks_promo_egtest.mm
+++ ios/chrome/browser/ui/bookmarks/home/bookmarks_promo_egtest.mm
@@ -64,7 +64,6 @@ using chrome_test_util::SecondarySignInButton;
     config.features_enabled.push_back(
         bookmarks::kEnableBookmarksAccountStorage);
   } else if ([self isRunningTest:@selector(testPromoViewBodyLegacy)] ||
-             [self isRunningTest:@selector(testTurnOnSyncPromo)] ||
              [self isRunningTest:@selector
                    (testSignInPromoWithIdentitiesUsingPrimaryButton)] ||
              [self isRunningTest:@selector
