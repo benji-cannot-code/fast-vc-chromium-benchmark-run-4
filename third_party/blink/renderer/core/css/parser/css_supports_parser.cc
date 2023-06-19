2bcda3489bf7d229dafc138f71e7c11ebdb59d73
Dominik Röttsches <drott@chromium.org>
2023-06-19 10:53:34 +0000

    Remove RuntimeEnabledFeature flags related to font-tech() / tech()
    Remove the two flags that controlled parsing of tech() in the @font-face
    and the @font-tech supports function.
    
    Shipping since M108.
    
    Bug: 1255685, 1216460
    Change-Id: Ic253f0454ab85a66c8f9e95a6a95a122afbd8054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4623549
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159538}

--- third_party/blink/renderer/core/css/parser/css_supports_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_supports_parser.cc
@@ -107,9 +107,8 @@ bool CSSSupportsParser::IsSupportsFeature(const CSSParserToken& first_token,
                                           const CSSParserToken& second_token) {
   return IsSupportsSelectorFn(first_token, second_token) ||
          IsSupportsDecl(first_token, second_token) ||
-         (RuntimeEnabledFeatures::SupportsFontFormatTechEnabled() &&
-          (IsFontFormatFn(first_token, second_token) ||
-           IsFontTechFn(first_token, second_token)));
+         IsFontFormatFn(first_token, second_token) ||
+         IsFontTechFn(first_token, second_token);
 }
 
 bool CSSSupportsParser::IsGeneralEnclosed(const CSSParserToken& first_token) {
@@ -158,14 +157,12 @@ CSSSupportsParser::Result CSSSupportsParser::ConsumeSupportsFeature(
   }
 
   // <supports-font-tech-fn>
-  if (IsFontTechFn(first_token, stream.Peek()) &&
-      RuntimeEnabledFeatures::SupportsFontFormatTechEnabled()) {
+  if (IsFontTechFn(first_token, stream.Peek())) {
     return ConsumeFontTechFn(first_token, stream);
   }
 
   // <supports-font-format-fn>
-  if (IsFontFormatFn(first_token, stream.Peek()) &&
-      RuntimeEnabledFeatures::SupportsFontFormatTechEnabled()) {
+  if (IsFontFormatFn(first_token, stream.Peek())) {
     return ConsumeFontFormatFn(first_token, stream);
   }
 
@@ -189,7 +186,6 @@ CSSSupportsParser::Result CSSSupportsParser::ConsumeFontFormatFn(
     const CSSParserToken& first_token,
     CSSParserTokenStream& stream) {
   DCHECK(IsFontFormatFn(first_token, stream.Peek()));
-  DCHECK(RuntimeEnabledFeatures::SupportsFontFormatTechEnabled());
 
   auto format_block = stream.ConsumeUntilPeekedTypeIs<kRightParenthesisToken>();
 
@@ -224,7 +220,6 @@ CSSSupportsParser::Result CSSSupportsParser::ConsumeFontTechFn(
     const CSSParserToken& first_token,
     CSSParserTokenStream& stream) {
   DCHECK(IsFontTechFn(first_token, stream.Peek()));
-  DCHECK(RuntimeEnabledFeatures::SupportsFontFormatTechEnabled());
   auto technology_block =
       stream.ConsumeUntilPeekedTypeIs<kRightParenthesisToken>();
 
