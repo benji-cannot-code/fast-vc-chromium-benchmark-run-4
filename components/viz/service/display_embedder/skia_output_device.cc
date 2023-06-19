197b6c8c1f7c4853f9da9077bfb10abc99a882eb
François Doray <fdoray@chromium.org>
2023-06-19 21:04:47 +0000

    Disable async reporting of GPU latency.
    GPU latency is reported from a ThreadPool task since
    crrev.com/c/2242634. We measured that the cost of posting the task is
    higher than the cost of reporting the metric. Additionally, we verified
    via a Finch experiment that disabling the async reporting (and instead
    reporting synchronously) does not regress guiding metrics:
    
    go/async-gpu-latency-reporting-win
    go/async-gpu-latency-reporting-mac
    go/async-gpu-latency-reporting-cros
    go/async-gpu-latency-reporting-android
    * Catan team signs off that the small browser PMF and startup time
      movement is insignificant
    
    With this CL, we disable the async reporting by default. The experiment
    code will be cleaned up in the next milestone.
    
    Bug: 1094361, 1327733
    Change-Id: I3501a4ffb361c131ee3b7543956ce7352ef2ba6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4624773
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159728}

--- components/viz/service/display_embedder/skia_output_device.cc
+++ components/viz/service/display_embedder/skia_output_device.cc
@@ -36,9 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace viz {
 namespace {
 
+// TODO(crbug.com/1094361): Clean up the feature in M117.
 BASE_FEATURE(kAsyncGpuLatencyReporting,
              "AsyncGpuLatencyReporting",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 using ::perfetto::protos::pbzero::ChromeLatencyInfo;
 
