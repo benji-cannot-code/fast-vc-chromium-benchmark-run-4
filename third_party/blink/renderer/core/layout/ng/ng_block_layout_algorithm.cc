45baf06235b16bea685e9b4eba981cdc31b6bdd7
Koji Ishii <kojii@chromium.org>
2023-06-19 22:39:24 +0000

    [text-wrap-pretty] Different max lines for balance and pretty
    This patch allows using different max number of lines for
    `balance` (using `NGScoreLineBreaker`) and `pretty`.
    
    To avoid consuming unnecessary stack space, `NGLineInfoList`,
    `NGScoreLineBreakContext`, and `NGOptimalInlineChildLayoutContext`
    are templatized.
    
    By using the capability, this patch sets the max number of
    lines for `balance` when using `NGScoreLineBreaker` to 10.
    When running the `perf_tests/layout/text-wrap-balance`, this
    is slower than 4 by ~13%, but is still faster than bisection
    by ~27%.
    
    This patch has no behavior changes as changes are under a
    runtime flag.
    
    Bug: 1451205
    Change-Id: I223755fbe5fe05aabd02df7d785bfbe3a1918941
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4604253
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159741}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
@@ -209,20 +209,6 @@ LogicalOffset LogicalFromBfcOffsets(const NGBfcOffset& child_bfc_offset,
           child_bfc_offset.block_offset - parent_bfc_offset.block_offset};
 }
 
-// Whether the `node` reuqires `NGLineInfoList` or not.
-inline bool NeedsOptimalInlineChildLayoutContext(const NGInlineNode& node) {
-  const TextWrap wrap = node.Style().GetTextWrap();
-  if (UNLIKELY(wrap == TextWrap::kPretty)) {
-    DCHECK(RuntimeEnabledFeatures::CSSTextWrapPrettyEnabled());
-    return !node.IsScoreLineBreakDisabled();
-  }
-  if (UNLIKELY(wrap == TextWrap::kBalance)) {
-    return RuntimeEnabledFeatures::CSSTextWrapBalanceByScoreEnabled() &&
-           !node.IsScoreLineBreakDisabled();
-  }
-  return false;
-}
-
 }  // namespace
 
 NGBlockLayoutAlgorithm::NGBlockLayoutAlgorithm(
@@ -461,11 +447,7 @@ const NGLayoutResult* NGBlockLayoutAlgorithm::Layout() {
   // only on demand, as it's quite big.
   NGInlineNode inline_child(nullptr);
   if (Node().IsInlineFormattingContextRoot(&inline_child)) {
-    if (UNLIKELY(NeedsOptimalInlineChildLayoutContext(inline_child))) {
-      result = LayoutWithOptimalInlineChildLayoutContext(inline_child);
-    } else {
-      result = LayoutWithSimpleInlineChildLayoutContext(inline_child);
-    }
+    result = LayoutInlineChild(inline_child);
   } else {
     result = Layout(nullptr);
   }
@@ -507,6 +489,25 @@ NGBlockLayoutAlgorithm::HandleNonsuccessfulLayoutResult(
   }
 }
 
+const NGLayoutResult* NGBlockLayoutAlgorithm::LayoutInlineChild(
+    const NGInlineNode& node) {
+  const TextWrap wrap = node.Style().GetTextWrap();
+  if (UNLIKELY(wrap == TextWrap::kPretty)) {
+    DCHECK(RuntimeEnabledFeatures::CSSTextWrapPrettyEnabled());
+    if (!node.IsScoreLineBreakDisabled()) {
+      return LayoutWithOptimalInlineChildLayoutContext<kMaxLinesForOptimal>(
+          node);
+    }
+  } else if (UNLIKELY(wrap == TextWrap::kBalance)) {
+    if (RuntimeEnabledFeatures::CSSTextWrapBalanceByScoreEnabled() &&
+        !node.IsScoreLineBreakDisabled()) {
+      return LayoutWithOptimalInlineChildLayoutContext<kMaxLinesForBalance>(
+          node);
+    }
+  }
+  return LayoutWithSimpleInlineChildLayoutContext(node);
+}
+
 NOINLINE const NGLayoutResult*
 NGBlockLayoutAlgorithm::LayoutWithSimpleInlineChildLayoutContext(
     const NGInlineNode& child) {
@@ -515,10 +516,12 @@ NGBlockLayoutAlgorithm::LayoutWithSimpleInlineChildLayoutContext(
   return result;
 }
 
+template <wtf_size_t capacity>
 NOINLINE const NGLayoutResult*
 NGBlockLayoutAlgorithm::LayoutWithOptimalInlineChildLayoutContext(
     const NGInlineNode& child) {
-  NGOptimalInlineChildLayoutContext context(child, &container_builder_);
+  NGOptimalInlineChildLayoutContext<capacity> context(child,
+                                                      &container_builder_);
   const NGLayoutResult* result = Layout(&context);
   return result;
 }
