471301df14347aaf65bd10a7ae291b0ad51c6711
Zachary Tan <tanzachary@chromium.org>
2023-06-19 18:58:57 +0000

    [FedCM] Add metrics for result of pop-up window
    This metric will be used in the IDP sign-in status API origin trial:
    1. Blink.FedCm.IdpSigninStatus.PopupWindowResult
    
    Bug: 1451536
    Change-Id: I5074f088fe3cba90f275dc2cebdbc24fcb792c40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622107
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159697}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
@@ -50,6 +50,8 @@ FedCmAccountSelectionView::~FedCmAccountSelectionView() {
   should_destroy_bubble_widget_ = false;
   Close();
 
+  // We use this boolean to record metrics in Close(), reset it after Close().
+  is_mismatch_continue_clicked_ = false;
   TabStripModelObserver::StopObservingAll(this);
 }
 
@@ -63,6 +65,8 @@ void FedCmAccountSelectionView::Show(
   // If IDP sign-in modal dialog is open, we delay the showing of the accounts
   // dialog until the modal dialog is destroyed.
   if (idp_signin_modal_dialog_) {
+    popup_window_state_ =
+        PopupWindowResult::kAccountsReceivedAndPopupNotClosedByIdp;
     show_accounts_dialog_callback_ = base::BindOnce(
         &FedCmAccountSelectionView::Show, weak_ptr_factory_.GetWeakPtr(),
         top_frame_etld_plus_one, iframe_etld_plus_one,
@@ -143,6 +147,8 @@ void FedCmAccountSelectionView::Show(
   // WebContents are hidden.
 
   if (!idp_close_popup_time_.is_null()) {
+    popup_window_state_ =
+        PopupWindowResult::kAccountsReceivedAndPopupClosedByIdp;
     UMA_HISTOGRAM_MEDIUM_TIMES(
         "Blink.FedCm.IdpSigninStatus."
         "IdpClosePopupToBrowserShowAccountsDuration",
@@ -383,6 +389,8 @@ void FedCmAccountSelectionView::OnCloseButtonClicked(const ui::Event& event) {
 void FedCmAccountSelectionView::OnSigninToIdP() {
   delegate_->OnSigninToIdP();
   is_mismatch_continue_clicked_ = true;
+  popup_window_state_ =
+      PopupWindowResult::kAccountsNotReceivedAndPopupNotClosedByIdp;
   UMA_HISTOGRAM_ENUMERATION("Blink.FedCm.IdpSigninStatus.MismatchDialogResult",
                             MismatchDialogResult::kContinued);
 }
@@ -406,6 +414,8 @@ void FedCmAccountSelectionView::CloseModalDialog() {
     idp_signin_modal_dialog_.reset();
     should_show_bubble_widget_ = true;
     idp_close_popup_time_ = base::TimeTicks::Now();
+    popup_window_state_ =
+        PopupWindowResult::kAccountsNotReceivedAndPopupClosedByIdp;
   }
 
   if (show_accounts_dialog_callback_) {
@@ -495,5 +505,13 @@ void FedCmAccountSelectionView::OnDismiss(DismissReason dismiss_reason) {
         dismiss_reason == DismissReason::kCloseButton
             ? MismatchDialogResult::kDismissedByCloseIcon
             : MismatchDialogResult::kDismissedForOtherReasons);
+  }
+
+  // Pop-up window can only be opened through clicking the "Continue" button on
+  // the mismatch dialog. Hence, we record the outcome only after the dialog is
+  // closed.
+  if (is_mismatch_continue_clicked_) {
+    UMA_HISTOGRAM_ENUMERATION("Blink.FedCm.IdpSigninStatus.PopupWindowResult",
+                              popup_window_state_);
   }
 }
