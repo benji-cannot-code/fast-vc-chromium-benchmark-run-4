ee7cbeae553e0b71c1cdfa6e5208460cc164b7bb
Anastasiia N <anastasiian@chromium.org>
2023-06-19 15:46:30 +0000

    cros: Remove unused methods of LoginScreenController
    Remove OnNoPodFocused(), LoadWallpaper(), SignOutUser(), LoginAsGuest().
    
    Bug: b:271261286
    Change-Id: Ieefd367790acdaf65ec6ccabb1fa197290e935e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626363
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159648}

--- chrome/browser/ui/ash/login_screen_client_impl.h
+++ chrome/browser/ui/ash/login_screen_client_impl.h
@@ -49,7 +49,6 @@ class LoginScreenClientImpl : public ash::LoginScreenClient,
         const AccountId& account_id,
         base::OnceCallback<void(bool)> callback) = 0;
     virtual void HandleOnFocusPod(const AccountId& account_id) = 0;
-    virtual void HandleOnNoPodFocused() = 0;
     // Handles request to focus a lock screen app window. Returns whether the
     // focus has been handed over to a lock screen app. For example, this might
     // fail if a hander for lock screen apps focus has not been set.
@@ -107,11 +106,7 @@ class LoginScreenClientImpl : public ash::LoginScreenClient,
       const std::string& access_code,
       base::Time validation_time) override;
   void OnFocusPod(const AccountId& account_id) override;
-  void OnNoPodFocused() override;
-  void LoadWallpaper(const AccountId& account_id) override;
-  void SignOutUser() override;
   void CancelAddUser() override;
-  void LoginAsGuest() override;
   void ShowGuestTosScreen() override;
   void OnMaxIncorrectPasswordAttempted(const AccountId& account_id) override;
   void FocusLockScreenApps(bool reverse) override;
@@ -138,6 +133,7 @@ class LoginScreenClientImpl : public ash::LoginScreenClient,
   void OnUserImageChanged(const user_manager::User& user) override;
 
  private:
+  void LoginAsGuest();
   void SetPublicSessionKeyboardLayout(const AccountId& account_id,
                                       const std::string& locale,
                                       base::Value::List keyboard_layouts);
