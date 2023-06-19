5553414f1b6ffd788481835be8364670277b9be4
Marc Treib <treib@chromium.org>
2023-06-19 11:27:35 +0000

    Fix crash when saving an address to the account in transport mode
    Repro:
    * Enable SyncEnableContactInfoDataTypeInTransportMode
      (chrome://flags#replace-sync-promos-with-sign-in-promos does the
      trick).
    * Sign in, but don't enable Sync.
    * Try to save an address.
    
    Result: DCHECK at
    https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:components/autofill/core/browser/autofill_save_update_address_profile_delegate_ios.cc;drc=20911ffd8a0e1636801ddf303c17375fdedf9c83;l=93
    because there is no syncing_user_email.
    
    There were several issues:
    * In transport mode, there is no ConsentLevel::kSync account.
    * syncer::AUTOFILL is the wrong data type to check; addresses are
      AUTOFILL_PROFILE or CONTACT_INFO.
    * IsDataTypeActive() is probably the wrong thing to check? This only
      becomes true once Sync has finished initializing and is actively
      running (hence "active"), but presumably a save-to-account action
      can happen before Sync is fully ready?
    
    The first of these affects transport mode only, but the other two
    affect full-Sync mode too.
    
    This CL fixes the issues by:
    * Changing from ConsentLevel::kSync to kSignin.
    * Removing the data-type-active check.
    
    This is safe, since the logic for deciding whether to upload an
    address to GAS lives elsewhere anyway.
    
    Bug: 1407666
    Change-Id: Id295171bcb872563460b2354eacc77a6fcaf0711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615891
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1159544}

--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -56,8 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/identity_manager_factory.h"
 #import "ios/chrome/browser/sync/sync_service_factory.h"
-#import "ios/chrome/browser/sync/sync_setup_service.h"
-#import "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #import "ios/chrome/browser/translate/chrome_ios_translate_client.h"
 #import "ios/chrome/browser/ui/autofill/card_expiration_date_fix_flow_view_bridge.h"
 #import "ios/chrome/browser/ui/autofill/card_name_fix_flow_view_bridge.h"
@@ -387,7 +385,7 @@ void ChromeAutofillClientIOS::ConfirmSaveAddressProfile(
   }
 
   auto delegate = std::make_unique<AutofillSaveUpdateAddressProfileDelegateIOS>(
-      profile, original_profile, SyncingUserEmail(),
+      profile, original_profile, GetUserEmail(),
       GetApplicationContext()->GetApplicationLocale(), options,
       std::move(callback));
 
@@ -530,18 +528,14 @@ void ChromeAutofillClientIOS::LoadRiskData(
       base::SysNSStringToUTF8(ios::provider::GetRiskData()));
 }
 
-absl::optional<std::u16string> ChromeAutofillClientIOS::SyncingUserEmail() {
+absl::optional<std::u16string> ChromeAutofillClientIOS::GetUserEmail() {
   AuthenticationService* authenticationService =
       AuthenticationServiceFactory::GetForBrowserState(browser_state_);
   DCHECK(authenticationService);
   id<SystemIdentity> identity =
-      authenticationService->GetPrimaryIdentity(signin::ConsentLevel::kSync);
+      authenticationService->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
   if (identity) {
-    SyncSetupService* syncSetupService =
-        SyncSetupServiceFactory::GetForBrowserState(browser_state_);
-    if (syncSetupService->IsDataTypeActive(syncer::AUTOFILL)) {
-      return base::SysNSStringToUTF16(identity.userEmail);
-    }
+    return base::SysNSStringToUTF16(identity.userEmail);
   }
   return absl::nullopt;
 }
