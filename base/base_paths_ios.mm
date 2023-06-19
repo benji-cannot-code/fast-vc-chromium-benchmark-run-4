49f12037cd4de2942fc17b0bab9ae43f0bd8eebc
Sylvain Defresne <sdefresne@chromium.org>
2023-06-19 11:51:42 +0000

    [ios][catalyst] Add missing include
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:ios-catalyst
    Change-Id: Ic67cfe8e78eb1a9da47aa84af1dcd6f39965dd45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626514
    Owners-Override: Christoph Schwering <schwering@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159553}

--- base/base_paths_ios.mm
+++ base/base_paths_ios.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#include "base/apple/bundle_locations.h"
 #include "base/base_paths.h"
 #include "base/base_paths_apple.h"
 #include "base/files/file_path.h"
