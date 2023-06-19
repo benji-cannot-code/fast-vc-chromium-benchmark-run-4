4e79fe1857b1bf5e97b1141d9f3ddc8a3fb0cfcd
Viktor Semeniuk <vsemeniuk@google.com>
2023-06-19 08:30:31 +0000

    Make sure filling is disabled when password store is unavailable
    Bug: 1454584
    Change-Id: I47e3856f68c3d67c290d47664b439473e1e1daa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4620393
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1159493}

--- chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
+++ chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/credential_cache.h"
 #include "components/password_manager/core/browser/credentials_filter.h"
 #include "components/password_manager/core/browser/mock_password_manager_settings_service.h"
+#include "components/password_manager/core/browser/mock_password_store_interface.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_manager.h"
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
@@ -94,7 +95,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/mock_password_accessory_controller.h"
 #include "chrome/browser/password_manager/android/password_accessory_controller_impl.h"
 #include "chrome/browser/password_manager/android/password_generation_controller.h"
-#include "components/password_manager/core/browser/mock_password_store_interface.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
 using autofill::FieldRendererId;
@@ -344,6 +344,12 @@ class ChromePasswordManagerClientTest : public ChromeRenderViewHostTestHarness {
 
 void ChromePasswordManagerClientTest::SetUp() {
   ChromeRenderViewHostTestHarness::SetUp();
+
+  PasswordStoreFactory::GetInstance()->SetTestingFactory(
+      GetBrowserContext(),
+      base::BindRepeating(&password_manager::BuildPasswordStoreInterface<
+                          content::BrowserContext,
+                          password_manager::MockPasswordStoreInterface>));
 
   blink::AssociatedInterfaceProvider* remote_interfaces =
       web_contents()->GetPrimaryMainFrame()->GetRemoteAssociatedInterfaces();
@@ -652,6 +658,15 @@ class ChromePasswordManagerClientSchemeTest
     : public ChromePasswordManagerClientTest,
       public ::testing::WithParamInterface<const char*> {
  public:
+  void SetUp() override {
+    ChromePasswordManagerClientTest::SetUp();
+    PasswordStoreFactory::GetInstance()->SetTestingFactory(
+        GetBrowserContext(),
+        base::BindRepeating(&password_manager::BuildPasswordStoreInterface<
+                            content::BrowserContext,
+                            password_manager::MockPasswordStoreInterface>));
+  }
+
   static std::vector<const char*> GetSchemes() {
     std::vector<const char*> result;
     for (const SchemeTestCase& test_case : kSchemeTestCases) {
