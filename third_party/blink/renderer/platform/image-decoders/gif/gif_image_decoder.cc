9977757ced4733598112c2512f15d089343fb1c7
Kevin Lubick <kjlubick@google.com>
2023-06-20 18:40:26 +0000

    Use moved SkEncodedImageFormat
    Change-Id: I84c22894678394683aebfada15e85a8e37cca0de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4621047
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Commit-Queue: Leon Scroggins <scroggo@google.com>
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160168}

--- third_party/blink/renderer/platform/image-decoders/gif/gif_image_decoder.cc
+++ third_party/blink/renderer/platform/image-decoders/gif/gif_image_decoder.cc
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/image-decoders/segment_stream.h"
 #include "third_party/skia/include/codec/SkCodec.h"
 #include "third_party/skia/include/codec/SkCodecAnimation.h"
+#include "third_party/skia/include/codec/SkEncodedImageFormat.h"
 #include "third_party/skia/include/core/SkAlphaType.h"
 #include "third_party/skia/include/core/SkColorType.h"
-#include "third_party/skia/include/core/SkEncodedImageFormat.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 
 namespace blink {
