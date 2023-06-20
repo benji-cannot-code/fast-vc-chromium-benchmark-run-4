dbbbbeb0e094d1780ca3eb36c3b7dbd93dec61aa
Lily Mihalkova <lilymihal@google.com>
2023-06-20 23:23:32 +0000

    CSC: Simplify the eligibility spec & library
    Per Marzia's suggestion from earlier in https://crrev.com/c/4601988, we get rid of the page level feature name and instead introduce a normalizing op, which can either normalize by viewport size or by max value of the feature. This simplifies the code and makes it less error prone
    
    Bug: b/284645622
    Change-Id: I36e00c466f7689d5f0b8dc6b25299c156ec1e5b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622377
    Commit-Queue: Lily Mihalkova <lilymihal@google.com>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160341}

--- chrome/renderer/companion/visual_search/visual_search_classification_and_eligibility_unittest.cc
+++ chrome/renderer/companion/visual_search/visual_search_classification_and_eligibility_unittest.cc
@@ -43,15 +43,15 @@ TEST_F(VisualClassificationAndEligibilityTest, TestCreateAndRun) {
   EligibilitySpec spec;
   auto* rules = spec.add_cheap_pruning_rules()->add_rules();
   rules->set_feature_name(FeatureLibrary::IMAGE_ONPAGE_AREA);
-  rules->set_op(FeatureLibrary::GT);
+  rules->set_thresholding_op(FeatureLibrary::GT);
   rules->set_threshold(100);
   rules = spec.add_classifier_score_rules()->add_rules();
   rules->set_feature_name(FeatureLibrary::SHOPPING_CLASSIFIER_SCORE);
-  rules->set_op(FeatureLibrary::GT);
+  rules->set_thresholding_op(FeatureLibrary::GT);
   rules->set_threshold(0.33);
   rules = spec.add_classifier_score_rules()->add_rules();
   rules->set_feature_name(FeatureLibrary::SENS_CLASSIFIER_SCORE);
-  rules->set_op(FeatureLibrary::LT);
+  rules->set_thresholding_op(FeatureLibrary::LT);
   rules->set_threshold(0.5);
 
   std::unique_ptr<VisualClassificationAndEligibility> created =
@@ -102,13 +102,13 @@ TEST_F(VisualClassificationAndEligibilityTest, TestShoppyScoreIsCorrect) {
   EligibilitySpec spec;
   auto* rules = spec.add_cheap_pruning_rules()->add_rules();
   rules->set_feature_name(FeatureLibrary::IMAGE_ONPAGE_AREA);
-  rules->set_op(FeatureLibrary::GT);
+  rules->set_thresholding_op(FeatureLibrary::GT);
   rules->set_threshold(100);
   // Counter-intuitive rule designed to check that sensitivity and shoppy
   // scores are not swapped or messed up in some other way.
   rules = spec.add_classifier_score_rules()->add_rules();
   rules->set_feature_name(FeatureLibrary::SHOPPING_CLASSIFIER_SCORE);
-  rules->set_op(FeatureLibrary::LT);
+  rules->set_thresholding_op(FeatureLibrary::LT);
   rules->set_threshold(0.4);
 
   std::unique_ptr<VisualClassificationAndEligibility> created =
@@ -135,13 +135,13 @@ TEST_F(VisualClassificationAndEligibilityTest, TestSensScoreIsCorrect) {
   EligibilitySpec spec;
   auto* rules = spec.add_cheap_pruning_rules()->add_rules();
   rules->set_feature_name(FeatureLibrary::IMAGE_ONPAGE_AREA);
-  rules->set_op(FeatureLibrary::GT);
+  rules->set_thresholding_op(FeatureLibrary::GT);
   rules->set_threshold(100);
   // Counter-intuitive rule designed to check that sensitivity and shoppy
   // scores are not swapped or messed up in some other way.
   rules = spec.add_classifier_score_rules()->add_rules();
   rules->set_feature_name(FeatureLibrary::SENS_CLASSIFIER_SCORE);
-  rules->set_op(FeatureLibrary::GT);
+  rules->set_thresholding_op(FeatureLibrary::GT);
   rules->set_threshold(0.4);
 
   std::unique_ptr<VisualClassificationAndEligibility> created =
@@ -169,7 +169,7 @@ TEST_F(VisualClassificationAndEligibilityTest, TestInvalidCreation) {
     // Spec with missing feature name in cheap pruning rules.
     EligibilitySpec spec;
     auto* rules = spec.add_cheap_pruning_rules()->add_rules();
-    rules->set_op(FeatureLibrary::GT);
+    rules->set_thresholding_op(FeatureLibrary::GT);
     rules->set_threshold(100);
     std::unique_ptr<VisualClassificationAndEligibility> created =
         VisualClassificationAndEligibility::Create(model_bytes_, spec);
@@ -190,7 +190,7 @@ TEST_F(VisualClassificationAndEligibilityTest, TestInvalidCreation) {
     EligibilitySpec spec;
     auto* rules = spec.add_post_renormalization_rules()->add_rules();
     rules->set_feature_name(FeatureLibrary::IMAGE_ONPAGE_AREA);
-    rules->set_normalizing_feature_name(FeatureLibrary::PAGE_LEVEL_UNSPECIFIED);
+    rules->set_normalizing_op(FeatureLibrary::NORMALIZE_UNSPECIFIED);
     rules->set_threshold(100);
     std::unique_ptr<VisualClassificationAndEligibility> created =
         VisualClassificationAndEligibility::Create(model_bytes_, spec);
@@ -201,7 +201,7 @@ TEST_F(VisualClassificationAndEligibilityTest, TestInvalidCreation) {
     EligibilitySpec spec;
     auto* rules = spec.add_cheap_pruning_rules()->add_rules();
     rules->set_feature_name(FeatureLibrary::IMAGE_ONPAGE_AREA);
-    rules->set_op(FeatureLibrary::GT);
+    rules->set_thresholding_op(FeatureLibrary::GT);
     rules->set_threshold(100);
     std::unique_ptr<VisualClassificationAndEligibility> created =
         VisualClassificationAndEligibility::Create(
