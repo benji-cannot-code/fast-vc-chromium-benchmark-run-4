b5ff4a61c2257be8222761e2cef6415babc4804e
Christian Dullweber <dullweber@chromium.org>
2023-06-20 10:12:15 +0000

    ContentSetting: Add SettingInfo parameter to GetContentSetting
    To simplify usage and improve debugging, add the info parameter from
    GetWebsiteSetting() to GetContentSetting(). This allows various clients
    to use the more specific GetContentSetting API.
    
    Bug: 1454216, 1030245
    Change-Id: I8c0d68f679479c11b572bc78605f0c65cb513f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626384
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159921}

--- components/content_settings/core/browser/cookie_settings.cc
+++ components/content_settings/core/browser/cookie_settings.cc
@@ -112,15 +112,13 @@ void CookieSettings::SetCookieSettingForUserBypass(
 bool CookieSettings::IsStoragePartitioningBypassEnabled(
     const GURL& first_party_url) {
   SettingInfo info;
-  const base::Value value = host_content_settings_map_->GetWebsiteSetting(
+  ContentSetting setting = host_content_settings_map_->GetContentSetting(
       GURL(), first_party_url, ContentSettingsType::COOKIES, &info);
 
   bool is_default = info.primary_pattern.MatchesAllHosts() &&
                     info.secondary_pattern.MatchesAllHosts();
 
-  DCHECK(value.is_int());
-
-  return is_default ? false : IsAllowed(ValueToContentSetting(value));
+  return is_default ? false : IsAllowed(setting);
 }
 
 void CookieSettings::ResetCookieSetting(const GURL& primary_url) {
@@ -218,7 +216,7 @@ ContentSetting CookieSettings::GetCookieSettingInternal(
 
   // First get any host-specific settings.
   SettingInfo info;
-  const base::Value value = host_content_settings_map_->GetWebsiteSetting(
+  ContentSetting setting = host_content_settings_map_->GetContentSetting(
       url, first_party_url, ContentSettingsType::COOKIES, &info);
   if (source) {
     *source = info.source;
@@ -231,9 +229,6 @@ ContentSetting CookieSettings::GetCookieSettingInternal(
                      ShouldBlockThirdPartyCookies() &&
                      !first_party_url.SchemeIs(extension_scheme_);
 
-  // We should always have a value, at least from the default provider.
-  DCHECK(value.is_int());
-  ContentSetting setting = ValueToContentSetting(value);
   bool block = block_third && is_third_party_request;
 
   if (!block) {
