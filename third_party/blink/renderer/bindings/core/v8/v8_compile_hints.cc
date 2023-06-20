0dfa7ffa34727963086833fcd70995d340f30217
Marja Hölttä <marja@chromium.org>
2023-06-20 09:13:23 +0000

    [compile hints] Schedule a data deletion task
    The data deletion task ensures that V8CrowdsourcedCompileHintsProvider
    releases ptrs to the v8::Script objects also in the case where the
    main frame of the page never becomes interactive.
    
    Bug: chromium:1406506
    Change-Id: Icee289b2d95ebfee6a99358dc6e52ccb90f1e1f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4620328
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159903}

--- third_party/blink/renderer/bindings/core/v8/v8_compile_hints.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_compile_hints.cc
@@ -38,7 +38,7 @@ V8CrowdsourcedCompileHintsProducer::V8CrowdsourcedCompileHintsProducer(
   static bool compile_hints_enabled =
       base::FeatureList::IsEnabled(features::kProduceCompileHints);
   if (!compile_hints_enabled) {
-    state_ = State::kDisabled;
+    state_ = State::kFinishedOrDisabled;
   }
 }
 
@@ -55,7 +55,6 @@ void V8CrowdsourcedCompileHintsProducer::RecordScript(
   if (data_generated_for_this_process_) {
     // We've already generated data for some other
     // V8CrowdsourcedCompileHintsProducer, so stop collecting data.
-    state_ = State::kDataGenerationFinished;
     ClearData();
     return;
   }
@@ -89,6 +88,10 @@ void V8CrowdsourcedCompileHintsProducer::RecordScript(
 
   scripts_.emplace_back(v8::TracedReference<v8::Script>(isolate, script));
   script_name_hashes_.emplace_back(script_name_hash);
+
+  if (scripts_.size() == 1) {
+    ScheduleDataDeletionTask(execution_context);
+  }
 }
 
 void V8CrowdsourcedCompileHintsProducer::GenerateData() {
@@ -96,9 +99,6 @@ void V8CrowdsourcedCompileHintsProducer::GenerateData() {
   if (state_ != State::kCollectingData) {
     return;
   }
-
-  // Stop logging script executions for this page.
-  state_ = State::kDataGenerationFinished;
 
   if (!data_generated_for_this_process_) {
     data_generated_for_this_process_ = SendDataToUkm();
@@ -113,8 +113,29 @@ void V8CrowdsourcedCompileHintsProducer::Trace(Visitor* visitor) const {
 }
 
 void V8CrowdsourcedCompileHintsProducer::ClearData() {
+  // Stop logging script executions for this page.
+  state_ = State::kFinishedOrDisabled;
   scripts_.clear();
   script_name_hashes_.clear();
+}
+
+namespace {
+
+void ClearDataTask(V8CrowdsourcedCompileHintsProducer* producer) {
+  producer->ClearData();
+}
+
+}  // namespace
+
+void V8CrowdsourcedCompileHintsProducer::ScheduleDataDeletionTask(
+    ExecutionContext* execution_context) {
+  constexpr int kDeletionDelaySeconds = 30;
+  auto delay = base::Seconds(kDeletionDelaySeconds);
+
+  execution_context->GetTaskRunner(TaskType::kIdleTask)
+      ->PostDelayedTask(FROM_HERE,
+                        WTF::BindOnce(&ClearDataTask, WrapWeakPersistent(this)),
+                        delay);
 }
 
 bool V8CrowdsourcedCompileHintsProducer::MightGenerateData() {
@@ -127,7 +148,6 @@ bool V8CrowdsourcedCompileHintsProducer::MightGenerateData() {
   // generate good compile hints, because we cannot retrieve data from other
   // processes.
   if (!main_frame->IsLocalFrame()) {
-    state_ = State::kDisabled;
     ClearData();
     return false;
   }
@@ -138,7 +158,6 @@ bool V8CrowdsourcedCompileHintsProducer::SendDataToUkm() {
   // Re-check the main frame, since it might have changed.
   Frame* main_frame = page_->MainFrame();
   if (!main_frame->IsLocalFrame()) {
-    state_ = State::kDisabled;
     ClearData();
     return false;
   }
--- third_party/blink/renderer/bindings/core/v8/v8_compile_hints.h
+++ third_party/blink/renderer/bindings/core/v8/v8_compile_hints.h
@@ -59,6 +59,7 @@ class V8CrowdsourcedCompileHintsProducer
   void ClearData();
 
  private:
+  void ScheduleDataDeletionTask(ExecutionContext* execution_context);
   bool SendDataToUkm();
   static void AddNoise(unsigned* data);
 
@@ -71,9 +72,7 @@ class V8CrowdsourcedCompileHintsProducer
     // We've tried once to send the data to UKM (but we didn't necessarily send
     // it successfully; e.g., because of throttling or because we didn't have
     // enough data).
-    kDataGenerationFinished,
-
-    kDisabled
+    kFinishedOrDisabled
   };
   State state_ = State::kCollectingData;
 
