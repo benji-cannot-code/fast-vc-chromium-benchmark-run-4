70db7460a6e44e22157a7337754b20ca71f31f20
Daniel Murphy <dmurph@google.com>
2023-06-20 20:09:14 +0000

    [dPWA] Fix external registration task to check callback
    There was a recent CL that caused the callback to be cause async, which
    allows for a possible race condition with some of the other callback
    triggers. This CL makes sure to check the existence of the callback in
    every place we use it, to protect against these race conditions.
    
    R=dibyapal@chromium.org
    
    Bug: 1455593
    Change-Id: I347bddec86f09e06076a10fe6ba55a4d9018db38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629735
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160223}

--- chrome/browser/web_applications/externally_managed_app_registration_task.cc
+++ chrome/browser/web_applications/externally_managed_app_registration_task.cc
@@ -63,8 +63,12 @@ ExternallyManagedAppRegistrationTask::~ExternallyManagedAppRegistrationTask() {
 
 void ExternallyManagedAppRegistrationTask::OnRegistrationCompleted(
     const GURL& scope) {
-  if (!content::ServiceWorkerContext::ScopeMatches(scope, install_url()))
+  if (!callback_) {
     return;
+  }
+  if (!content::ServiceWorkerContext::ScopeMatches(scope, install_url())) {
+    return;
+  }
 
   registration_timer_.Stop();
   std::move(callback_).Run(RegistrationResultCode::kSuccess);
@@ -93,6 +97,9 @@ void ExternallyManagedAppRegistrationTask::CheckHasServiceWorker() {
 
 void ExternallyManagedAppRegistrationTask::OnDidCheckHasServiceWorker(
     content::ServiceWorkerCapability capability) {
+  if (!callback_) {
+    return;
+  }
   if (capability != content::ServiceWorkerCapability::NO_SERVICE_WORKER) {
     registration_timer_.Stop();
     // This is posted as a task because the serviceworker check can be
@@ -122,6 +129,9 @@ void ExternallyManagedAppRegistrationTask::OnWebContentsReady(
 }
 
 void ExternallyManagedAppRegistrationTask::OnRegistrationTimeout() {
+  if (!callback_) {
+    return;
+  }
   std::move(callback_).Run(RegistrationResultCode::kTimeout);
 }
 
