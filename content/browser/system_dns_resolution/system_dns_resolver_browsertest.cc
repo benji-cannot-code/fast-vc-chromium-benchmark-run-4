652412267e5abd1b61cadabb468e5ab38081014d
Matthew Denton <mpdenton@chromium.org>
2023-06-20 09:18:32 +0000

    OutOfProcessSystemDnsResolution won't be enabled on ChromeOS
    ...so we can remove the code that checks if the feature is enabled.
    
    Bug: 1312224
    Change-Id: I04914089d3cdd362fcf2b9fd89b614b10edce659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617985
    Auto-Submit: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159910}

--- content/browser/system_dns_resolution/system_dns_resolver_browsertest.cc
+++ content/browser/system_dns_resolution/system_dns_resolver_browsertest.cc
@@ -113,7 +113,7 @@ IN_PROC_BROWSER_TEST_F(SystemDnsResolverBrowserTest,
 
   const auto& addr_list1 = future.Get<absl::optional<net::AddressList>>();
 
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
   // If system DNS resolution runs in the browser process, check here that the
   // resolver received the correct number of resolves.
   EXPECT_EQ(host_resolver()->NumResolvesForHostPattern(kHostname1), 1u);
@@ -136,7 +136,7 @@ IN_PROC_BROWSER_TEST_F(SystemDnsResolverBrowserTest,
   const auto& addr_list1 = future1.Get<absl::optional<net::AddressList>>();
   const auto& addr_list2 = future2.Get<absl::optional<net::AddressList>>();
 
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
   // If system DNS resolution runs in the browser process, check here that the
   // resolver received the correct number of resolves.
   EXPECT_EQ(host_resolver()->NumResolvesForHostPattern(kHostname1), 1u);
@@ -164,7 +164,7 @@ IN_PROC_BROWSER_TEST_F(SystemDnsResolverBrowserTest,
   auto [result, resolve_error_info, resolved_addresses, endpoints] =
       future.Take();
 
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
   // If system DNS resolution runs in the browser process, check here that
   // the resolver received the correct number of resolves.
   EXPECT_EQ(host_resolver()->NumResolvesForHostPattern(kFailHostname), 1u);
@@ -196,7 +196,7 @@ IN_PROC_BROWSER_TEST_F(SystemDnsResolverBrowserTest,
 
   auto [addr_list, os_error_result, net_error_result] = future.Take();
 
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
   // If system DNS resolution runs in the browser process, check here that the
   // resolver received the correct number of resolves.
   EXPECT_EQ(host_resolver()->NumResolvesForHostPattern(net::GetHostName()), 1u);
