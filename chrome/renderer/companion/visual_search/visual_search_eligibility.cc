799c5f7658c39df0c9a45ec1135a10e2a97d809e
Lily Mihalkova <lilymihal@google.com>
2023-06-20 19:53:18 +0000

    CSC: Change second pass of eligibility library to sort results by shoppy score.
    Change-Id: Ib364a6395c86f1c26a9998e02ae284693e7dfd62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628445
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Lily Mihalkova <lilymihal@google.com>
    Quick-Run: Pierre St Juste <pstjuste@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160211}

--- chrome/renderer/companion/visual_search/visual_search_eligibility.cc
+++ chrome/renderer/companion/visual_search/visual_search_eligibility.cc
@@ -14,8 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 
 namespace companion::visual_search {
+namespace {
 constexpr char kNormalizedPrefix[] = "normalized_";
 constexpr int kMaxNumStored = 200;
+
+// Return true if p1 should be sorted before p2.
+bool SortDesc(const std::pair<std::string, double>& p1,
+              const std::pair<std::string, double>& p2) {
+  return p1.second >= p2.second;
+}
+}  // namespace
 
 EligibilityModule::EligibilityModule(const EligibilitySpec& spec)
     : spec_(spec), have_run_first_pass_(false) {}
@@ -84,11 +92,20 @@ EligibilityModule::RunSecondPassPostClassificationEligibility(
     }
   }
   RenormalizeForThirdPass();
-  std::vector<std::string> eligible_image_ids;
+  std::vector<std::pair<std::string, double>> eligible_image_ids_with_scores;
   for (const std::string& image_id : eligible_after_second_pass_) {
     if (IsEligible(spec_.post_renormalization_rules(), image_id)) {
-      eligible_image_ids.push_back(image_id);
+      eligible_image_ids_with_scores.push_back(
+          std::make_pair(image_id, shopping_classifier_scores.at(image_id)));
     }
+  }
+  std::sort(eligible_image_ids_with_scores.begin(),
+            eligible_image_ids_with_scores.end(), SortDesc);
+
+  std::vector<std::string> eligible_image_ids;
+  eligible_image_ids.reserve(eligible_image_ids_with_scores.size());
+  for (auto& id_score_pair : eligible_image_ids_with_scores) {
+    eligible_image_ids.push_back(std::move(id_score_pair.first));
   }
   return eligible_image_ids;
 }
