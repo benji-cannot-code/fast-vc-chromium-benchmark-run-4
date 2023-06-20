08d2b52a6d7f0bed3f1e9168b27a765bc8ed3bd7
Russ Hamilton <behamilton@google.com>
2023-06-20 18:22:05 +0000

    Set limit of 12 UTF-8 bytes for adRenderId
    Bug: 1442274
    Change-Id: I5fa95be1eb3180e27b186c35f3fdcb70a29dd3f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618194
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160154}

--- third_party/blink/common/interest_group/interest_group.cc
+++ third_party/blink/common/interest_group/interest_group.cc
@@ -28,6 +28,8 @@ namespace blink {
 
 namespace {
 
+const size_t kMaxAdRenderIdSize = 12;
+
 // Check if `url` can be used as an interest group's ad render URL. Ad URLs can
 // be cross origin, unlike other interest group URLs, but are still restricted
 // to HTTPS with no embedded credentials.
@@ -214,6 +216,11 @@ bool InterestGroup::IsValid() const {
           return false;
         }
       }
+      if (ad.ad_render_id) {
+        if (ad.ad_render_id->size() > kMaxAdRenderIdSize) {
+          return false;
+        }
+      }
     }
   }
 
@@ -225,6 +232,11 @@ bool InterestGroup::IsValid() const {
       if (ad.size_group) {
         if (ad.size_group->empty() || !size_groups ||
             !size_groups->contains(ad.size_group.value())) {
+          return false;
+        }
+      }
+      if (ad.ad_render_id) {
+        if (ad.ad_render_id->size() > kMaxAdRenderIdSize) {
           return false;
         }
       }
