4dbdaec2a204088d9f63f184f0a675091d5f0638
Ian Wells <iwells@chromium.org>
2023-06-20 20:53:22 +0000

    Move readability interface outside ReadAloud module
    We'll need to check feature availability and page readability outside of the ReadAloud isolated split.
    
    Bug: b/284355944
    Change-Id: Id23f27daedb85b8b83f169a35adcbd2cca20d031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629746
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160267}

---
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudReadabilityHooks.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.readaloud;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+/** Interface providing access to ReadAloud page readability checking. */
+public interface ReadAloudReadabilityHooks {
+    /** Returns true if ReadAloud feature is available. */
+    boolean isEnabled();
+
+    /**
+     * Checks whether a given page is readable.
+     * @param checkSupportedRequest Serialized CheckSupportedRequest proto message.
+     * @return Future that returns a serialized CheckSupportedResult proto message, or null if the
+     *         check is unavailable.
+     */
+    ListenableFuture<byte[]> isPageReadable(byte[] checkSupportedRequest);
+}
---
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudReadabilityHooksImpl.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.readaloud;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/** Empty implementation of ReadAloudReadabilityHooks. */
+public class ReadAloudReadabilityHooksImpl implements ReadAloudReadabilityHooks {
+    @Override
+    boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    ListenableFuture<byte[]> isPageReadable(byte[] checkSupportedRequest) {
+        return Futures.immediateFuture(null);
+    }
+}
