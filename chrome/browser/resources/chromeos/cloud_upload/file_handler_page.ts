b2030c57facc05db058f1027c2321b1266e4325c
Wenbo Jie <wenbojie@chromium.org>
2023-06-20 06:01:09 +0000

    [Office i18n] i18n strings for File Handler dialog
    Bug: b:254586358
    Change-Id: I4cc977623f6edc1631c6b3efe84c5205da282ca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4624193
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159829}

--- chrome/browser/resources/chromeos/cloud_upload/file_handler_page.ts
+++ chrome/browser/resources/chromeos/cloud_upload/file_handler_page.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import {assert, assertNotReached} from 'chrome://resources/js/assert_ts.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {DialogTask, MetricsRecordedSetupPage, UserAction} from './cloud_upload.mojom-webui.js';
 import {CloudUploadBrowserProxy} from './cloud_upload_browser_proxy.js';
@@ -71,21 +72,23 @@ export class FileHandlerPageElement extends HTMLElement {
       const {name, icon, type} =
           this.getDriveAppInfo(dialogArgs.args.fileNames);
 
-      const fileTypeElement = this.$<HTMLSpanElement>('#file-type');
-      assert(fileTypeElement);
-      fileTypeElement.innerText = type;
+      const titleElement = this.$<HTMLSpanElement>('#title');
+      assert(titleElement);
+      titleElement.innerText =
+          loadTimeData.getStringF('fileHandlerTitle', type);
 
       const driveCard = new CloudProviderCardElement();
       driveCard.setParameters(
-          CloudProviderType.DRIVE, name, 'Uses Google Drive');
+          CloudProviderType.DRIVE, name,
+          loadTimeData.getString('googleDriveStorage'));
       driveCard.setIconClass(icon);
       driveCard.id = 'drive';
       this.addCloudProviderCard(driveCard);
 
       const officeCard = new CloudProviderCardElement();
       officeCard.setParameters(
-          CloudProviderType.ONE_DRIVE, 'Microsoft 365',
-          'Uses Microsoft OneDrive');
+          CloudProviderType.ONE_DRIVE, loadTimeData.getString('microsoft365'),
+          loadTimeData.getString('oneDriveStorage'));
       officeCard.setIconClass('office');
       officeCard.id = 'onedrive';
       this.addCloudProviderCard(officeCard);
@@ -207,14 +210,25 @@ export class FileHandlerPageElement extends HTMLElement {
   // different types, or any error finding the right app, we just default to
   // Docs.
   private getDriveAppInfo(fileNames: string[]) {
-    // TODO(b:254586358): i18n these names.
     const fileName = (fileNames[0] || '').toLowerCase();
     if (/\.xls[m,x]?$/.test(fileName)) {
-      return {name: 'Google Sheets', icon: 'sheets', type: 'Excel'};
+      return {
+        name: loadTimeData.getString('googleSheets'),
+        icon: 'sheets',
+        type: loadTimeData.getString('excel'),
+      };
     } else if (/\.pptx?$/.test(fileName)) {
-      return {name: 'Google Slides', icon: 'slides', type: 'Powerpoint'};
+      return {
+        name: loadTimeData.getString('googleSlides'),
+        icon: 'slides',
+        type: loadTimeData.getString('powerPoint'),
+      };
     } else {
-      return {name: 'Google Docs', icon: 'docs', type: 'Word'};
+      return {
+        name: loadTimeData.getString('googleDocs'),
+        icon: 'docs',
+        type: loadTimeData.getString('word'),
+      };
     }
   }
 
