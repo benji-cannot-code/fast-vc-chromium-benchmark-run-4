fb016f778433da337a289460765341e1cbac578e
Robbie Gibson <rkgibson@google.com>
2023-06-20 20:39:55 +0000

    [iOS] Let OverflowMenuOrderer control the order for menu actions
    For Overflow Menu Customization, the orderer will handle storing
    the current action order, so this change is setting up for that.
    
    Bug: 1445203
    Change-Id: Iee4aee6824fb229ed1a8c6c727b85b2048609095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4621968
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160256}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_orderer.h
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_orderer.h
@@ -12,12 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace overflow_menu {
 enum class Destination;
+enum class ActionType;
 }
+@class OverflowMenuAction;
 @class OverflowMenuDestination;
 class PrefService;
 
 @protocol OverflowMenuDestinationProvider <NSObject>
 
+// The default base ranking of destinations currently supported.
 - (DestinationRanking)baseDestinations;
 
 // Returns the correct `OverflowMenuDestination` for the corresponding
@@ -25,6 +28,20 @@ class PrefService;
 // page does not support the given `destinationType`.
 - (OverflowMenuDestination*)destinationForDestinationType:
     (overflow_menu::Destination)destinationType;
+
+@end
+
+@protocol OverflowMenuActionProvider <NSObject>
+
+// The default base ranking of page actions (those that act on the current page
+// state) currently supported.
+- (ActionRanking)basePageActions;
+
+// Returns the correct `OverflowMenuAction` for the corresponding
+// `overflow_menu::ActionType` on the current page. Returns nil if the current
+// page does not support the given `actionType`.
+- (OverflowMenuAction*)actionForActionType:
+    (overflow_menu::ActionType)actionType;
 
 @end
 
@@ -45,14 +62,19 @@ class PrefService;
 @property(nonatomic, weak) id<OverflowMenuDestinationProvider>
     destinationProvider;
 
+@property(nonatomic, weak) id<OverflowMenuActionProvider> actionProvider;
+
 // Release any C++ objects that can't be reference counted.
 - (void)disconnect;
 
 // Records a `destination` click from the overflow menu carousel.
 - (void)recordClickForDestination:(overflow_menu::Destination)destination;
 
-// Returns a new, sorted list of destinations given the initial list.
+// Returns the current sorted list of active destinations.
 - (NSArray<OverflowMenuDestination*>*)sortedDestinations;
+
+// Returns the current ordering of active page actions.
+- (NSArray<OverflowMenuAction*>*)pageActions;
 
 @end
 
