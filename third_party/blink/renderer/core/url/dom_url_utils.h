117adc298a8266a38e73af57710ee258e51d304c
Nate Chapin <japhet@chromium.org>
2023-06-20 17:20:59 +0000

    Remove deprecated CallWith=ExecutionContext usage in core/html/ and core/url/
    Use CallWith=ScriptState instead. Or in the case of
    HTMLMediaElement::canPlayType(), drop CallWith= entirely.
    
    Change-Id: Ic59db0007cca5dcb2fd32eb38b2b33c76f124d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622378
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160112}

--- third_party/blink/renderer/core/url/dom_url_utils.h
+++ third_party/blink/renderer/core/url/dom_url_utils.h
@@ -29,13 +29,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_URL_DOM_URL_UTILS_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/url/dom_url_utils_read_only.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
 namespace blink {
 
 class KURL;
+class ScriptState;
 
 class CORE_EXPORT DOMURLUtils : public DOMURLUtilsReadOnly {
  public:
@@ -47,7 +47,7 @@ class CORE_EXPORT DOMURLUtils : public DOMURLUtilsReadOnly {
   void setPassword(const String&);
   void setHost(const String&);
   void setHostname(const String&);
-  void setPort(ExecutionContext* execution_context, const String&);
+  void setPort(ScriptState*, const String&);
   void setPathname(const String&);
   void setHash(const String&);
   virtual void setSearch(const String&);
