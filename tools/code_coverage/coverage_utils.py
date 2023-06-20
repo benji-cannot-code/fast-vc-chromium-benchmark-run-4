f66de57e2d5b6392f210c1bbbdaddef1f0ba72ba
Julia Hansbrough <flowerhack@google.com>
2023-06-20 19:17:03 +0000

    [coverage] Fix errors in coverage_test.py.
    coverage_test.py seems to have been outside of presubmits for a while,
    leading to a missed opportunity for ensuring coverage.py remains stablea
    nd reliable.
    
    Before adding it to presubmits again, we need to get rid of some bitrot.
    
    This CL updates a broken call to gn and fixes the import of a necessary
    Python module.
    
    Subsequent CLs will provide further fixes.
    
    Change-Id: I1a8cba125f54cfa6c79563fa8814bdc745dba3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4612110
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Reviewed-by: Prakhar Asthana <pasthana@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160190}

--- tools/code_coverage/coverage_utils.py
+++ tools/code_coverage/coverage_utils.py
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 from collections import defaultdict
 import functools
-import jinja2
 import json
 import logging
 import os
@@ -16,6 +15,13 @@ import re
 import shutil
 import subprocess
 import sys
+
+# Appends third_party/ so that coverage_utils can import jinja2 from
+# third_party/.
+sys.path.append(
+    os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir,
+                 'third_party'))
+import jinja2
 
 # The default name of the html coverage report for a directory.
 DIRECTORY_COVERAGE_HTML_REPORT_NAME = os.extsep.join(['report', 'html'])
