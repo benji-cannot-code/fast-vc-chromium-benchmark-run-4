fdde1e5b6bea2a0777b67906d7dfade3c94f2836
ssid <ssid@chromium.org>
2023-06-20 17:39:19 +0000

    Fix and re-enable MemoryTracingIntegrationTest
    The test suite creates multiple threads which uses the global memory
    dump manager when resetting. Add a reset method in the dump manager and
    use it in tests instead of destroying singleton.
    
    Bug: 1426515
    Change-Id: I1174fca907d18069208faed3dbff7be849439d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4606123
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160125}

--- services/resource_coordinator/public/cpp/memory_instrumentation/tracing_integration_unittest.cc
+++ services/resource_coordinator/public/cpp/memory_instrumentation/tracing_integration_unittest.cc
@@ -146,7 +146,7 @@ class MemoryTracingIntegrationTest : public testing::Test {
   }
 
   void InitializeClientProcess(mojom::ProcessType process_type) {
-    mdm_ = MemoryDumpManager::CreateInstanceForTesting();
+    mdm_ = MemoryDumpManager::GetInstance();
     mdm_->set_dumper_registrations_ignored_for_testing(true);
 
     mojo::PendingRemote<mojom::Coordinator> coordinator;
@@ -161,7 +161,8 @@ class MemoryTracingIntegrationTest : public testing::Test {
 
   void TearDown() override {
     TraceLog::GetInstance()->SetDisabled();
-    mdm_.reset();
+    mdm_->ResetForTesting();
+    mdm_ = nullptr;
     client_process_.reset();
     coordinator_.reset();
     task_environment_.reset();
@@ -247,7 +248,7 @@ class MemoryTracingIntegrationTest : public testing::Test {
     return MemoryDumpScheduler::GetInstance()->is_enabled_for_testing();
   }
 
-  std::unique_ptr<MemoryDumpManager> mdm_;
+  raw_ptr<MemoryDumpManager> mdm_;
 
  private:
   std::unique_ptr<base::test::SingleThreadTaskEnvironment> task_environment_;
@@ -444,8 +445,7 @@ TEST_F(MemoryTracingIntegrationTest, PeriodicDumpingWithMultipleModes) {
   mdm_->UnregisterAndDeleteDumpProviderSoon(std::move(mdp));
 }
 
-// TODO(https://crbug.com/1426515): Re-enable this test
-TEST_F(MemoryTracingIntegrationTest, DISABLED_TestWhitelistingMDP) {
+TEST_F(MemoryTracingIntegrationTest, TestWhitelistingMDP) {
   InitializeClientProcess(mojom::ProcessType::RENDERER);
   base::trace_event::SetDumpProviderAllowlistForTesting(kTestMDPWhitelist);
   std::unique_ptr<MockMemoryDumpProvider> mdp1(new MockMemoryDumpProvider);
@@ -465,9 +465,7 @@ TEST_F(MemoryTracingIntegrationTest, DISABLED_TestWhitelistingMDP) {
 }
 
 // Regression test for https://crbug.com/766274 .
-// TODO(https://crbug.com/1426515): Re-enable this test
-TEST_F(MemoryTracingIntegrationTest,
-       DISABLED_GenerationChangeDoesntReenterMDM) {
+TEST_F(MemoryTracingIntegrationTest, GenerationChangeDoesntReenterMDM) {
   InitializeClientProcess(mojom::ProcessType::RENDERER);
 
   // We want the ThreadLocalEventBuffer MDPs to auto-register to repro this bug.
