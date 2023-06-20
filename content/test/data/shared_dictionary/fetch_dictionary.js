b883f1b0414162ae85b160df65d78a5e6a03494c
Tsuyoshi Horo <horo@chromium.org>
2023-06-20 22:39:31 +0000

    Support writing shared dictionary from workers
    To trigger the shared dictionary writer, we need to set the request's
    shared_dictionary_writer_enabled flag. This flag should be enabled when
    a valid CompressionDictionaryTransport Origin Trial token is set or
    blink::features::kCompressionDictionaryTransport is enabled.
    
    Currently FrameFetchContext is correctly setting this flag.
    But WorkerFetchContext is not setting this flag.
    
    This CL changes WorkerFetchContext::PrepareRequest() to set this flag
    to support writing shared dictionary from workers.
    
    Bug: 1413922
    Change-Id: I904ef3b5a3e182e92b52912ddea6346c9783f7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619955
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160321}

---
+++ content/test/data/shared_dictionary/fetch_dictionary.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// For Dedicatd Worker and Service Worker.
+self.addEventListener('message', (e) => {
+  fetch(e.data);
+});
+
+// For Shared Worker.
+self.addEventListener('connect', (e) => {
+  const port = e.ports[0];
+  port.addEventListener('message', (e) => {
+    fetch(e.data);
+  });
+  port.start();
+});
