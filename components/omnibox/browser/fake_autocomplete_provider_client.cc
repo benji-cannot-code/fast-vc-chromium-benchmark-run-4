e7a163444067b3051524371554d7d999300a9e19
Angela Yoeurng <yoangela@chromium.org>
2023-06-20 20:59:38 +0000

    [omnibox][ml] Remove scoring model from fake provider client includes.
    This CL Removes autocomplete_scoring_model_service.h from inclusion in
    fake_autocomplete_provider_client.cc.  This allows the file to build
    when BUILD_WITH_TFLITE_LIB is false.
    
    Bug: 1405555
    Change-Id: I02a0b398aa64fd32a47a3a0166b4e0f5c2644cfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629745
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160273}

--- components/omnibox/browser/fake_autocomplete_provider_client.cc
+++ components/omnibox/browser/fake_autocomplete_provider_client.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/test/test_bookmark_client.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/test/history_service_test_util.h"
-#include "components/omnibox/browser/autocomplete_scoring_model_service.h"
 #include "components/omnibox/browser/in_memory_url_index.h"
 #include "components/omnibox/browser/shortcuts_backend.h"
 #include "components/prefs/testing_pref_service.h"
