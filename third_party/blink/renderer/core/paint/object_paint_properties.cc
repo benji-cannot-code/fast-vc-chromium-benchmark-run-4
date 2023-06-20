de2ff2d7bdc3399e2b17485d288b143048a5f123
Jordan Bayles <jophba@chromium.org>
2023-06-20 18:13:09 +0000

    [SparseObjectPaintProperties #3] Add ObjectPaintPropertiesSparse and experiment
    This patch is the third in a series for the SparseObjectPaintProperties
    experiment. In this patch, the following closely related changes are
    added:
    
    1. A new SparseObjectPaintProperties runtime feature.
    2. A new SparseVector class, with test coverage.
    3. A new ObjectPaintPropertiesSparse class, that implements the
    ObjectPaintProperties interface backed by SparseVector.
    
    The intent is after this patch lands we should be able to start
    experimenting with the difference in memory usage between
    ObjectPaintPropertiesImpl and ObjectPaintPropertiesSparse.
    
    The Project:
    SparseObjectPaintProperties is a cleanup project with the goal of
    lowering the total memory cost of the ObjectPaintProperties class,
    through the use of a new SparseVector class that allows for zero
    memory overhead for null pointers. If successful, this project should
    significantly the total memory usage of this class and hopefully
    impact Chrome's memory usage in general.
    
    Bug: 1454638
    Change-Id: If03c0c14f673a97c4ffa02c141ceaf739221bc74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4613006
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160148}

--- third_party/blink/renderer/core/paint/object_paint_properties.cc
+++ third_party/blink/renderer/core/paint/object_paint_properties.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/object_paint_properties.h"
 
 #include "third_party/blink/renderer/core/paint/object_paint_properties_impl.h"
+#include "third_party/blink/renderer/core/paint/object_paint_properties_sparse.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -13,6 +15,9 @@ ObjectPaintProperties::~ObjectPaintProperties() = default;
 
 // static
 std::unique_ptr<ObjectPaintProperties> ObjectPaintProperties::Create() {
+  if (RuntimeEnabledFeatures::SparseObjectPaintPropertiesEnabled()) {
+    return std::make_unique<ObjectPaintPropertiesSparse>();
+  }
   return std::make_unique<ObjectPaintPropertiesImpl>();
 }
 
