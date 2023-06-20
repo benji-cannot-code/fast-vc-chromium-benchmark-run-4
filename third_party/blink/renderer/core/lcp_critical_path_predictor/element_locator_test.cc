b54c6e8658b6ecb446d05a3095e74c7b6719bc43
Kouhei Ueno <kouhei@chromium.org>
2023-06-20 04:10:31 +0000

    [LCPP] Serialize ElementLocator to proto3
    Before this CL, ElementLocator was a WTF::String.
    
    After this CL, ElementLocator is a proto3 struct. In the future CL,
    I'm going to introduce a ElementLocator matcher, but it turns out the
    string format is non-trivial to deserialize. It is much easier to
    manage them in a protobuf from the first place.
    
    Change-Id: I946a34cba9df8f738688a9adf7c88d7fb44d8faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4606796
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159815}

--- third_party/blink/renderer/core/lcp_critical_path_predictor/element_locator_test.cc
+++ third_party/blink/renderer/core/lcp_critical_path_predictor/element_locator_test.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/editing/testing/editing_test_base.h"
+#include "third_party/blink/renderer/core/lcp_critical_path_predictor/element_locator.pb.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 
 namespace blink {
@@ -19,7 +21,7 @@ bool HasDataLocateMe(Element& element) {
   return element.hasAttribute(kDataLocateMe);
 }
 
-TEST_F(ElementLocatorTest, ToElementLocatorString) {
+TEST_F(ElementLocatorTest, OfElement) {
   struct TestCase {
     const char* body_html;
     const char* expected_locator_string;
@@ -30,18 +32,18 @@ TEST_F(ElementLocatorTest, ToElementLocatorString) {
 
       // No id on the element, so use relative position.
       {"<div id='container'><div data-locate-me></div></div>",
-       "/DIV[0]/#container"},
+       "/div[0]/#container"},
 
       // No id on the document, so stop at BODY.
-      {"<div data-locate-me></div>", "/DIV[0]/BODY[0]"},
+      {"<div data-locate-me></div>", "/div[0]/body[0]"},
 
       // Siblings
       {"<div id='container'><p><p><p><p data-locate-me><p></div>",
-       "/P[3]/#container"},
+       "/p[3]/#container"},
 
       // Siblings with different tag names
       {"<div id='container'><h1></h1><p><p data-locate-me><p><a></a></div>",
-       "/P[1]/#container"},
+       "/p[1]/#container"},
 
       // Misc complicated cases
       {"<section id='container'>"
@@ -53,7 +55,7 @@ TEST_F(ElementLocatorTest, ToElementLocatorString) {
        "</article>"
        "<article></article>"
        "</section>",
-       "/IMG[1]/ARTICLE[2]/#container"},
+       "/img[1]/article[2]/#container"},
   };
 
   for (const auto& test_case : test_cases) {
@@ -65,8 +67,14 @@ TEST_F(ElementLocatorTest, ToElementLocatorString) {
         Traversal<Element>::FirstWithin(GetDocument(), HasDataLocateMe);
     ASSERT_TRUE(target);
 
-    EXPECT_EQ(String(test_case.expected_locator_string),
-              ToElementLocatorString(target));
+    auto maybe_locator = element_locator::OfElement(target);
+
+    if (test_case.expected_locator_string) {
+      ASSERT_TRUE(maybe_locator.has_value());
+
+      String locator_string = element_locator::ToString(maybe_locator.value());
+      EXPECT_EQ(String(test_case.expected_locator_string), locator_string);
+    }
   }
 }
 
