a203b48fdc2d9649d042e0f3c8f421cbbde424b2
Tomasz Wiszkowski <ender@google.com>
2023-06-20 22:12:37 +0000

    Deflake OmniboxActionsTest.
    This change defers to Espresso and explicit View IDs to uniquely
    identify the Omnibox Actions carousel and specific item on that
    carousel.
    
    Link to flakiness dashboard: http://shortn/_MRcXBD4j5m
    
    Change-Id: I330801ade4463abdce3d99f1d3b190e542f0c361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4621606
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160313}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/ActionChipsProcessor.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/ActionChipsProcessor.java
@@ -62,7 +62,7 @@ public class ActionChipsProcessor {
      * @param position The position of the suggestion with OmniboxAction(s) on the suggestion list.
      */
     public void populateModel(AutocompleteMatch suggestion, PropertyModel model, int position) {
-        if (!doesProcessSuggestion(suggestion, position)) {
+        if (suggestion.getActions().isEmpty()) {
             model.set(ActionChipsProperties.ACTION_CHIPS, null);
             return;
         }
@@ -86,10 +86,6 @@ public class ActionChipsProcessor {
         }
 
         model.set(ActionChipsProperties.ACTION_CHIPS, modelList);
-    }
-
-    private boolean doesProcessSuggestion(AutocompleteMatch suggestion, int position) {
-        return !suggestion.getActions().isEmpty();
     }
 
     /**
--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/OmniboxTestUtils.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/OmniboxTestUtils.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.test.util;
 
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Context;
@@ -17,6 +20,8 @@ import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.hamcrest.Matcher;
@@ -61,6 +66,47 @@ public class OmniboxTestUtils {
     private final @NonNull UrlBar mUrlBar;
     private final @NonNull Instrumentation mInstrumentation;
     private final @Nullable ToolbarLayout mToolbar;
+
+    /**
+     * Invokes a specific ViewAction on an {@link
+     * org.chromium.chrome.browser.omnibox.suggestions.action.OmniboxAction} at specific position.
+     *
+     * This class can be chained with {@link
+     * androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition}.
+     */
+    private static class ActionOnOmniobxActionAtPosition implements ViewAction {
+        private final ViewAction mAction;
+
+        public ActionOnOmniobxActionAtPosition(int position, ViewAction action) {
+            mAction = actionOnItemAtPosition(position, action);
+        }
+
+        @Override
+        public Matcher<View> getConstraints() {
+            return withId(R.id.omnibox_actions_carousel);
+        }
+
+        @Override
+        public String getDescription() {
+            return mAction.getDescription();
+        }
+
+        @Override
+        public void perform(UiController uiController, View view) {
+            mAction.perform(uiController, view.findViewById(R.id.omnibox_actions_carousel));
+        }
+    }
+
+    /**
+     * Create a ViewAction that can be executed on a Suggestion with Action Chips.
+     *
+     * @param position the index of an {@link
+     *         org.chromium.chrome.browser.omnibox.suggestions.action.OmniboxAction},
+     * @param action the action to perform.
+     */
+    public static ViewAction actionOnOmniboxActionAtPosition(int position, ViewAction action) {
+        return new ActionOnOmniobxActionAtPosition(position, action);
+    }
 
     /**
      * Class describing individual suggestion, delivering access to broad range of information.
