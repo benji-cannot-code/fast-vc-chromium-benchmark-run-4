df1c0dfd2dd93556c27d8273d87fecc8353b0fc1
Avi Drissman <avi@chromium.org>
2023-06-20 23:33:06 +0000

    Convert the task manager to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Note that this is a change of ownership. Before, the TaskManagerMac
    object didn't own the TaskManagerWindowController; the window
    controller owned itself. With ARC, Objective-C objects can't really
    "own themselves" any more, so ownership was moved to the C++ object.
    
    Bug: 1280317
    Change-Id: Ic82e71ddbab89ce6067e0c34af6544dbccc609eb
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618607
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160348}

--- chrome/browser/ui/cocoa/task_manager_mac_browsertest.mm
+++ chrome/browser/ui/cocoa/task_manager_mac_browsertest.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import <Cocoa/Cocoa.h>
+#include <Foundation/Foundation.h>
 #include <stddef.h>
 
 #include "base/functional/callback.h"
@@ -47,12 +48,11 @@ using browsertest_util::WaitForTaskManagerRows;
 
 class TaskManagerMacTest : public InProcessBrowserTest {
  public:
-  TaskManagerMacTest() {}
+  TaskManagerMacTest() = default;
+  ~TaskManagerMacTest() override = default;
 
   TaskManagerMacTest(const TaskManagerMacTest&) = delete;
   TaskManagerMacTest& operator=(const TaskManagerMacTest&) = delete;
-
-  ~TaskManagerMacTest() override {}
 
   void SetUpOnMainThread() override {
     ASSERT_FALSE(GetTaskManagerMac());
@@ -71,19 +71,21 @@ class TaskManagerMacTest : public InProcessBrowserTest {
   }
 
   NSTableView* GetTable() const {
-    return GetTaskManagerMac() ? [GetTaskManagerMac()->CocoaControllerForTests()
-                                         tableViewForTesting]
+    return GetTaskManagerMac() ? GetTaskManagerMac()
+                                     ->CocoaControllerForTests()
+                                     .tableViewForTesting
                                : nullptr;
   }
 
   absl::optional<size_t> TableFirstSelectedRow() const {
-    int index = [[GetTable() selectedRowIndexes] firstIndex];
+    int index = GetTable().selectedRowIndexes.firstIndex;
     return (index < 0) ? absl::nullopt
                        : absl::make_optional(static_cast<size_t>(index));
   }
 
   void PressKillButton() {
-    [[GetTaskManagerMac()->CocoaControllerForTests() endProcessButtonForTesting]
+    ASSERT_TRUE(GetTaskManagerMac());
+    [GetTaskManagerMac()->CocoaControllerForTests().endProcessButtonForTesting
         performClick:nil];
   }
 
@@ -141,8 +143,8 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, TableStartsWithDefaultColumns) {
   NSTableView* table = GetTable();
   ASSERT_TRUE(table);
 
-  EXPECT_EQ(0u, [[table sortDescriptors] count]);
-  NSArray* tableColumns = [table tableColumns];
+  EXPECT_EQ(0u, table.sortDescriptors.count);
+  NSArray* tableColumns = table.tableColumns;
   for (size_t i = 0; i < kColumnsSize; ++i) {
     EXPECT_EQ(kColumns[i].id, [[tableColumns[i] identifier] intValue]);
     EXPECT_EQ(kColumns[i].default_visibility, ![tableColumns[i] isHidden]);
@@ -161,7 +163,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, ColumnsSettingsAreRestored) {
   ASSERT_TRUE(table);
 
   // Toggle the visibility of all columns.
-  EXPECT_EQ(0u, [[table sortDescriptors] count]);
+  EXPECT_EQ(0u, table.sortDescriptors.count);
   NSArray* tableColumns = [table tableColumns];
   for (size_t i = 0; i < kColumnsSize; ++i) {
     EXPECT_EQ(kColumns[i].id, [[tableColumns[i] identifier] intValue]);
@@ -174,20 +176,21 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, ColumnsSettingsAreRestored) {
   // http://www.cocoabuilder.com/archive/cocoa/177610-programmatically-click-column-header-in-nstableview.html).
   bool is_sorted = false;
   for (NSTableColumn* column in tableColumns) {
-    if ([column isHidden])
+    if (column.hidden) {
       continue;
-    if ([column sortDescriptorPrototype].ascending) {
+    }
+    if (column.sortDescriptorPrototype.ascending) {
       // Toggle the sort for a descending sort.
       NSSortDescriptor* newSortDescriptor =
-          [[column sortDescriptorPrototype] reversedSortDescriptor];
-      [table setSortDescriptors:@[ newSortDescriptor ]];
+          column.sortDescriptorPrototype.reversedSortDescriptor;
+      table.sortDescriptors = @[ newSortDescriptor ];
       is_sorted = true;
       break;
     }
   }
 
-  NSArray* expectedSortDescriptors = [table sortDescriptors];
-  EXPECT_EQ(is_sorted, [expectedSortDescriptors count] > 0);
+  NSArray* expectedSortDescriptors = table.sortDescriptors;
+  EXPECT_EQ(is_sorted, expectedSortDescriptors.count > 0);
 
   // Close the task manager view and re-open. Expect the inverse of the default
   // visibility, and the last sort order.
@@ -200,7 +203,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, ColumnsSettingsAreRestored) {
   ASSERT_TRUE(table);
 
   if (is_sorted) {
-    EXPECT_NSEQ(expectedSortDescriptors, [table sortDescriptors]);
+    EXPECT_NSEQ(expectedSortDescriptors, table.sortDescriptors);
   }
 }
 
@@ -314,7 +317,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, DISABLED_SelectionConsistency) {
                                indexSetWithIndex:FindRowForTab(tabs[1]).value()]
       byExtendingSelection:NO];
   EXPECT_EQ(TableFirstSelectedRow(), FindRowForTab(tabs[1]));
-  EXPECT_EQ(1, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(1, GetTable().numberOfSelectedRows);
 
   // Add 3 rows above the selection. The selected tab should not change.
   for (int i = 0; i < 3; ++i) {
@@ -323,7 +326,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, DISABLED_SelectionConsistency) {
   }
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows((rows += 3), pattern));
   EXPECT_EQ(TableFirstSelectedRow(), FindRowForTab(tabs[1]));
-  EXPECT_EQ(1, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(1, GetTable().numberOfSelectedRows);
 
   // Add 2 rows below the selection. The selected tab should not change.
   for (int i = 0; i < 2; ++i) {
@@ -332,7 +335,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, DISABLED_SelectionConsistency) {
   }
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows((rows += 2), pattern));
   EXPECT_EQ(TableFirstSelectedRow(), FindRowForTab(tabs[1]));
-  EXPECT_EQ(1, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(1, GetTable().numberOfSelectedRows);
 
   // Add a new row in the same process as the selection. The selected tab should
   // not change.
@@ -340,7 +343,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, DISABLED_SelectionConsistency) {
   EXPECT_EQ(TableFirstSelectedRow(), FindRowForTab(tabs[1]));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows((rows += 1), pattern));
   EXPECT_EQ(TableFirstSelectedRow(), FindRowForTab(tabs[1]));
-  EXPECT_EQ(2, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(2, GetTable().numberOfSelectedRows);
 
   {
     content::ScopedAllowRendererCrashes scoped_allow_renderer_crashes;
@@ -416,7 +419,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, DISABLED_NavigateSelection) {
                                indexSetWithIndex:FindRowForTab(tabs[0]).value()]
       byExtendingSelection:NO];
   EXPECT_EQ(TableFirstSelectedRow(), FindRowForTab(tabs[0]));
-  EXPECT_EQ(1, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(1, GetTable().numberOfSelectedRows);
 
   // Add two new tasks to the same process as a.com
   int num_group_tasks = 1;
@@ -426,7 +429,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, DISABLED_NavigateSelection) {
     ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows((rows += 1), pattern));
     EXPECT_EQ(TableFirstSelectedRow(), FindRowForTab(tabs[0]));
     num_group_tasks++;
-    EXPECT_EQ(num_group_tasks, [GetTable() numberOfSelectedRows]);
+    EXPECT_EQ(num_group_tasks, GetTable().numberOfSelectedRows);
   }
 
   absl::optional<size_t> selected_row = TableFirstSelectedRow();
@@ -440,27 +443,27 @@ IN_PROC_BROWSER_TEST_F(TaskManagerMacTest, DISABLED_NavigateSelection) {
                             false, false, false);
   expected_selected_row = expected_selected_row + 3;
   EXPECT_EQ(expected_selected_row, TableFirstSelectedRow().value());
-  EXPECT_EQ(1, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(1, GetTable().numberOfSelectedRows);
 
   // Navigate into the three grouped tasks
   ui_controls::SendKeyPress(window_controller.window, ui::VKEY_UP, false, false,
                             false, false);
   expected_selected_row -= num_group_tasks;
   EXPECT_EQ(expected_selected_row, TableFirstSelectedRow().value());
-  EXPECT_EQ(num_group_tasks, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(num_group_tasks, GetTable().numberOfSelectedRows);
 
   // Navigate off of grouped tasks
   ui_controls::SendKeyPress(window_controller.window, ui::VKEY_UP, false, false,
                             false, false);
   expected_selected_row--;
   EXPECT_EQ(expected_selected_row, TableFirstSelectedRow().value());
-  EXPECT_EQ(1, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(1, GetTable().numberOfSelectedRows);
 
   // Navigate back into the three grouped tasks
   ui_controls::SendKeyPress(window_controller.window, ui::VKEY_DOWN, false,
                             false, false, false);
   expected_selected_row++;
   EXPECT_EQ(expected_selected_row, TableFirstSelectedRow().value());
-  EXPECT_EQ(num_group_tasks, [GetTable() numberOfSelectedRows]);
+  EXPECT_EQ(num_group_tasks, GetTable().numberOfSelectedRows);
 }
 }  // namespace task_manager
