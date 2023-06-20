d0cb2e5a4f366850070cf75995c56746c74215af
Munira Tursunova <moonira@google.com>
2023-06-20 16:16:04 +0000

    Support parsing of the palette-mix() function
    Accept palette-mix() function as font-palette property's value. Add
    CSSPaletteMixValue class for storing the result of parsing the
    palette-mix() function before resolving it into FontPalette.
    
    Explainer: https://docs.google.com/document/d/1XMTrKH003KBOes6hxzI-3E7LTwp5YwFC-rnzoFpFrfw/edit?usp=sharing
    
    Percentage value serialization will be done in the separate CL.
    
    Bug: 1445039
    Change-Id: I69cb41652deab28ab5a1fdaa3f526e794369dd2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618967
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160070}

--- third_party/blink/renderer/core/css/css_value.cc
+++ third_party/blink/renderer/core/css/css_value.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_math_function_value.h"
 #include "third_party/blink/renderer/core/css/css_numeric_literal_value.h"
 #include "third_party/blink/renderer/core/css/css_paint_value.h"
+#include "third_party/blink/renderer/core/css/css_palette_mix_value.h"
 #include "third_party/blink/renderer/core/css/css_path_value.h"
 #include "third_party/blink/renderer/core/css/css_pending_substitution_value.h"
 #include "third_party/blink/renderer/core/css/css_pending_system_font_value.h"
@@ -324,6 +325,8 @@ bool CSSValue::operator==(const CSSValue& other) const {
         return CompareCSSValues<cssvalue::CSSViewValue>(*this, other);
       case kRatioClass:
         return CompareCSSValues<cssvalue::CSSRatioValue>(*this, other);
+      case kPaletteMixClass:
+        return CompareCSSValues<cssvalue::CSSPaletteMixValue>(*this, other);
     }
     NOTREACHED();
     return false;
@@ -470,6 +473,8 @@ String CSSValue::CssText() const {
       return To<cssvalue::CSSViewValue>(this)->CustomCSSText();
     case kRatioClass:
       return To<cssvalue::CSSRatioValue>(this)->CustomCSSText();
+    case kPaletteMixClass:
+      return To<cssvalue::CSSPaletteMixValue>(this)->CustomCSSText();
   }
   NOTREACHED();
   return String();
@@ -708,6 +713,9 @@ void CSSValue::Trace(Visitor* visitor) const {
       return;
     case kRatioClass:
       To<cssvalue::CSSRatioValue>(this)->TraceAfterDispatch(visitor);
+      return;
+    case kPaletteMixClass:
+      To<cssvalue::CSSPaletteMixValue>(this)->TraceAfterDispatch(visitor);
       return;
   }
   NOTREACHED();
