e97f6ae8c410780d6e0758011020867dda668582
Taylor Bergquist <tbergquist@chromium.org>
2023-06-20 23:14:28 +0000

    Move GetTabBackgroundColor from TabStrip to TabStyle.
    This CL is partial progress towards GM3-ifying tab colors.
    
    - Moving GetTabBackgroundColor from TabStrip to TabStyle allows us to have different behavior in GM3 vs GM2 by swapping out the tab style implementations. It also just doesn't make sense for the tabstrip to be in charge of what color tabs are.
    - TabStyle::GetTabBackgroundColor takes a new enum TabSelectionState instead of just active or inactive. This lets us handle the alpha stuff in selected tabs inside GetTabBackgroundColor, and will let us have specific colors for selected/hovered states in GM3.
    
    Random related cleanup:
    - Tab group highlights were doing weird stuff to paint the selected tab color, now they do less weird stuff.
    
    Remaining work:
    - Refactor TabStyleViews to use the TabSelectionState enum directly.
    - Add a GM3 override with custom colors for selected and hovered states.
    
    Bug: 1399942
    
    Change-Id: I5d5d117debab7a224ae60f3fd77b125696ffe6f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619009
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160336}

--- chrome/browser/ui/tabs/tab_style.h
+++ chrome/browser/ui/tabs/tab_style.h
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
 
+namespace ui {
+class ColorProvider;
+}
+
 // Holds the basic logic for rendering tabs, including preferred sizes, paths,
 // etc.
 class TabStyle {
@@ -57,6 +61,14 @@ class TabStyle {
   enum class HideHoverStyle {
     kGradual,    // The hover should fade out.
     kImmediate,  // The hover should cut off, with no fade out.
+  };
+
+  // The states the tab can be in throughout its selection lifecycle.
+  enum class TabSelectionState {
+    kActive,
+    kSelected,
+    kHovered,
+    kInactive,
   };
 
   // If we want to draw vertical separators between tabs, these are the leading
@@ -143,6 +155,15 @@ class TabStyle {
 
   // Returns the radius of the outer corners of the tab shape.
   virtual int GetBottomCornerRadius() const = 0;
+
+  // Returns the background color of a tab with selection state `state`.
+  // `frame_active` is whether the tab's widget is painted as active or not.
+  // TODO(tbergquist): Non-Tab callers of this should probably be refactored to
+  // use their own color ids.
+  virtual SkColor GetTabBackgroundColor(
+      TabSelectionState state,
+      bool frame_active,
+      const ui::ColorProvider& color_provider) const = 0;
 
   // Opacity of the active tab background painted over inactive selected tabs.
   virtual float GetSelectedTabOpacity() const = 0;
--- chrome/browser/ui/views/tabs/tab_group_highlight.cc
+++ chrome/browser/ui/views/tabs/tab_group_highlight.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/views/background.h"
 #include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
 
 TabGroupHighlight::TabGroupHighlight(TabGroupViews* tab_group_views,
                                      const tab_groups::TabGroupId& group,
@@ -35,15 +36,9 @@ void TabGroupHighlight::OnPaint(gfx::Canvas* canvas) {
   cc::PaintFlags flags;
   flags.setAntiAlias(true);
   flags.setStyle(cc::PaintFlags::kFill_Style);
-
-  // Draw two layers to simulate the color of other non-active selected tabs,
-  // which use a similar drawing strategy (see GM2TabStyle::PaintTab()).
-  // This is needed because the group background color alone would be slightly
-  // transparent, so instead it's drawn over the inactive background color.
-  flags.setColor(tab_group_views_->GetTabBackgroundColor());
-  canvas->DrawPath(path, flags);
-
-  flags.setColor(tab_group_views_->GetGroupBackgroundColor());
+  flags.setColor(TabStyle::Get()->GetTabBackgroundColor(
+      TabStyle::TabSelectionState::kSelected,
+      GetWidget()->ShouldPaintAsActive(), *GetColorProvider()));
   canvas->DrawPath(path, flags);
 }
 
