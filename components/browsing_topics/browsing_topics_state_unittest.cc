e134080cd44de2d33cab23584e207f56a74ca551
Abigail Katcoff <abigailkatcoff@chromium.org>
2023-06-20 19:38:53 +0000

    Topics API: do not use time delay for epoch generated from chrome://topics-internals/
    After hitting the "calculate now" button in  chrome://topics-internals/, let's immediately switch to using the new topics instead of waiting up to 24 hours.
    
    Bug: 1455531
    Change-Id: Ia747bc278563e0421d09bf2cff4f8647ffdf52a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622009
    Commit-Queue: Abigail Katcoff <abigailkatcoff@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160203}

--- components/browsing_topics/browsing_topics_state_unittest.cc
+++ components/browsing_topics/browsing_topics_state_unittest.cc
@@ -43,7 +43,8 @@ constexpr int kTaxonomyVersion = 1;
 constexpr int64_t kModelVersion = 2;
 constexpr size_t kPaddedTopTopicsStartIndex = 3;
 
-EpochTopics CreateTestEpochTopics(base::Time calculation_time) {
+EpochTopics CreateTestEpochTopics(base::Time calculation_time,
+                                  bool from_manually_triggered_calculation) {
   std::vector<TopicAndDomains> top_topics_and_observing_domains;
   top_topics_and_observing_domains.emplace_back(
       TopicAndDomains(Topic(1), {HashedDomain(1)}));
@@ -58,7 +59,8 @@ EpochTopics CreateTestEpochTopics(base::Time calculation_time) {
 
   EpochTopics epoch_topics(std::move(top_topics_and_observing_domains),
                            kPaddedTopTopicsStartIndex, kTaxonomyVersion,
-                           kModelVersion, calculation_time);
+                           kModelVersion, calculation_time,
+                           from_manually_triggered_calculation);
 
   return epoch_topics;
 }
@@ -206,14 +208,16 @@ TEST_F(BrowsingTopicsStateTest, AddEpoch) {
   task_environment_->RunUntilIdle();
 
   // Successful topics calculation at `kTime1`.
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
 
   EXPECT_EQ(state.epochs().size(), 1u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime1);
 
   // Successful topics calculation at `kTime2`.
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
   EXPECT_EQ(state.epochs().size(), 2u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime1);
@@ -231,7 +235,8 @@ TEST_F(BrowsingTopicsStateTest, AddEpoch) {
   EXPECT_EQ(state.epochs()[2].calculation_time(), kTime3);
 
   // Successful topics calculation at `kTime4`.
-  state.AddEpoch(CreateTestEpochTopics(kTime4));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime4, /*from_manually_triggered_calculation=*/false));
   EXPECT_EQ(state.epochs().size(), 4u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime1);
@@ -243,7 +248,8 @@ TEST_F(BrowsingTopicsStateTest, AddEpoch) {
 
   // Successful topics calculation at `kTime5`. When this epoch is added, the
   // first one should be evicted.
-  state.AddEpoch(CreateTestEpochTopics(kTime5));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime5, /*from_manually_triggered_calculation=*/false));
   EXPECT_EQ(state.epochs().size(), 4u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime2);
@@ -269,7 +275,8 @@ TEST_F(BrowsingTopicsStateTest, EpochsForSite_OneEpoch_SwitchTimeNotArrived) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   // The random per-site delay happens to be between (one hour, one day).
@@ -284,7 +291,8 @@ TEST_F(BrowsingTopicsStateTest, EpochsForSite_OneEpoch_SwitchTimeArrived) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   // The random per-site delay happens to be between (one hour, one day).
@@ -299,14 +307,36 @@ TEST_F(BrowsingTopicsStateTest, EpochsForSite_OneEpoch_SwitchTimeArrived) {
   EXPECT_EQ(epochs_for_site[0], &state.epochs()[0]);
 }
 
+TEST_F(BrowsingTopicsStateTest, EpochsForSite_OneEpoch_ManuallyTriggered) {
+  BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
+  task_environment_->RunUntilIdle();
+
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/true));
+  state.UpdateNextScheduledCalculationTime();
+
+  // There shouldn't be a delay when the latest epoch is manually triggered.
+  ASSERT_EQ(state.CalculateSiteStickyTimeDelta("foo.com"),
+            base::Microseconds(0));
+  task_environment_->FastForwardBy(base::Microseconds(10));
+
+  std::vector<const EpochTopics*> epochs_for_site =
+      state.EpochsForSite(/*top_domain=*/"foo.com");
+  EXPECT_EQ(epochs_for_site.size(), 1u);
+  EXPECT_EQ(epochs_for_site[0], &state.epochs()[0]);
+}
+
 TEST_F(BrowsingTopicsStateTest,
        EpochsForSite_ThreeEpochs_SwitchTimeNotArrived) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
-  state.AddEpoch(CreateTestEpochTopics(kTime3));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   task_environment_->FastForwardBy(base::Hours(1));
@@ -322,9 +352,12 @@ TEST_F(BrowsingTopicsStateTest, EpochsForSite_ThreeEpochs_SwitchTimeArrived) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
-  state.AddEpoch(CreateTestEpochTopics(kTime3));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   task_environment_->FastForwardBy(base::Days(1));
@@ -337,14 +370,65 @@ TEST_F(BrowsingTopicsStateTest, EpochsForSite_ThreeEpochs_SwitchTimeArrived) {
   EXPECT_EQ(epochs_for_site[2], &state.epochs()[2]);
 }
 
+TEST_F(BrowsingTopicsStateTest,
+       EpochsForSite_ThreeEpochs_LatestManuallyTriggered) {
+  BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
+  task_environment_->RunUntilIdle();
+
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/true));
+  state.UpdateNextScheduledCalculationTime();
+
+  task_environment_->FastForwardBy(base::Microseconds(10));
+
+  std::vector<const EpochTopics*> epochs_for_site =
+      state.EpochsForSite(/*top_domain=*/"foo.com");
+  EXPECT_EQ(epochs_for_site.size(), 3u);
+  EXPECT_EQ(epochs_for_site[0], &state.epochs()[0]);
+  EXPECT_EQ(epochs_for_site[1], &state.epochs()[1]);
+  EXPECT_EQ(epochs_for_site[2], &state.epochs()[2]);
+}
+
+TEST_F(BrowsingTopicsStateTest,
+       EpochsForSite_ThreeEpochs_EarlierEpochManuallyTriggered) {
+  BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
+  task_environment_->RunUntilIdle();
+
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/true));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/false));
+  state.UpdateNextScheduledCalculationTime();
+
+  task_environment_->FastForwardBy(base::Microseconds(10));
+
+  std::vector<const EpochTopics*> epochs_for_site =
+      state.EpochsForSite(/*top_domain=*/"foo.com");
+  // The latest epoch shouldn't be included because it wasn't manually
+  // triggered.
+  EXPECT_EQ(epochs_for_site.size(), 2u);
+  EXPECT_EQ(epochs_for_site[0], &state.epochs()[0]);
+  EXPECT_EQ(epochs_for_site[1], &state.epochs()[1]);
+}
+
 TEST_F(BrowsingTopicsStateTest, EpochsForSite_FourEpochs_SwitchTimeNotArrived) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
-  state.AddEpoch(CreateTestEpochTopics(kTime3));
-  state.AddEpoch(CreateTestEpochTopics(kTime4));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime4, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   task_environment_->FastForwardBy(base::Hours(1));
@@ -361,10 +445,14 @@ TEST_F(BrowsingTopicsStateTest, EpochsForSite_FourEpochs_SwitchTimeArrived) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
-  state.AddEpoch(CreateTestEpochTopics(kTime3));
-  state.AddEpoch(CreateTestEpochTopics(kTime4));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime4, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   task_environment_->FastForwardBy(base::Days(1));
@@ -377,11 +465,65 @@ TEST_F(BrowsingTopicsStateTest, EpochsForSite_FourEpochs_SwitchTimeArrived) {
   EXPECT_EQ(epochs_for_site[2], &state.epochs()[3]);
 }
 
+TEST_F(BrowsingTopicsStateTest,
+       EpochsForSite_FourEpochs_LatestManuallyTriggered) {
+  BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
+  task_environment_->RunUntilIdle();
+
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime4, /*from_manually_triggered_calculation=*/true));
+
+  state.UpdateNextScheduledCalculationTime();
+
+  task_environment_->FastForwardBy(base::Microseconds(10));
+
+  std::vector<const EpochTopics*> epochs_for_site =
+      state.EpochsForSite(/*top_domain=*/"foo.com");
+  EXPECT_EQ(epochs_for_site.size(), 3u);
+  EXPECT_EQ(epochs_for_site[0], &state.epochs()[1]);
+  EXPECT_EQ(epochs_for_site[1], &state.epochs()[2]);
+  EXPECT_EQ(epochs_for_site[2], &state.epochs()[3]);
+}
+
+TEST_F(BrowsingTopicsStateTest,
+       EpochsForSite_FourEpochs_EarlierEpochManuallyTriggered) {
+  BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
+  task_environment_->RunUntilIdle();
+
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/true));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime3, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime4, /*from_manually_triggered_calculation=*/false));
+  state.UpdateNextScheduledCalculationTime();
+
+  task_environment_->FastForwardBy(base::Microseconds(10));
+
+  std::vector<const EpochTopics*> epochs_for_site =
+      state.EpochsForSite(/*top_domain=*/"foo.com");
+  // The latest epoch shouldn't be included because it wasn't manually
+  // triggered.
+  EXPECT_EQ(epochs_for_site.size(), 3u);
+  EXPECT_EQ(epochs_for_site[0], &state.epochs()[0]);
+  EXPECT_EQ(epochs_for_site[1], &state.epochs()[1]);
+  EXPECT_EQ(epochs_for_site[2], &state.epochs()[2]);
+}
+
 TEST_F(BrowsingTopicsStateTest, InitFromPreexistingFile_CorruptedHmacKey) {
   base::HistogramTester histograms;
 
   std::vector<EpochTopics> epochs;
-  epochs.emplace_back(CreateTestEpochTopics(kTime1));
+  epochs.emplace_back(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
 
   CreateOrOverrideTestFile(std::move(epochs),
                            /*next_scheduled_calculation_time=*/kTime2,
@@ -404,7 +546,8 @@ TEST_F(BrowsingTopicsStateTest, InitFromPreexistingFile_SameConfigVersion) {
   base::HistogramTester histograms;
 
   std::vector<EpochTopics> epochs;
-  epochs.emplace_back(CreateTestEpochTopics(kTime1));
+  epochs.emplace_back(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
 
   CreateOrOverrideTestFile(std::move(epochs),
                            /*next_scheduled_calculation_time=*/kTime2,
@@ -430,7 +573,8 @@ TEST_F(BrowsingTopicsStateTest,
   base::HistogramTester histograms;
 
   std::vector<EpochTopics> epochs;
-  epochs.emplace_back(CreateTestEpochTopics(kTime1));
+  epochs.emplace_back(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
 
   CreateOrOverrideTestFile(std::move(epochs),
                            /*next_scheduled_calculation_time=*/kTime2,
@@ -453,13 +597,15 @@ TEST_F(BrowsingTopicsStateTest, ClearOneEpoch) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
 
   EXPECT_EQ(state.epochs().size(), 1u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime1);
 
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
   EXPECT_EQ(state.epochs().size(), 2u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime1);
@@ -483,13 +629,15 @@ TEST_F(BrowsingTopicsStateTest, ClearAllTopics) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
 
   EXPECT_EQ(state.epochs().size(), 1u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime1);
 
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
   EXPECT_EQ(state.epochs().size(), 2u);
   EXPECT_FALSE(state.epochs()[0].empty());
   EXPECT_EQ(state.epochs()[0].calculation_time(), kTime1);
@@ -510,8 +658,10 @@ TEST_F(BrowsingTopicsStateTest, ClearTopic) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   state.ClearTopic(Topic(3));
@@ -544,8 +694,10 @@ TEST_F(BrowsingTopicsStateTest, ClearContextDomain) {
   BrowsingTopicsState state(temp_dir_.GetPath(), base::DoNothing());
   task_environment_->RunUntilIdle();
 
-  state.AddEpoch(CreateTestEpochTopics(kTime1));
-  state.AddEpoch(CreateTestEpochTopics(kTime2));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime1, /*from_manually_triggered_calculation=*/false));
+  state.AddEpoch(CreateTestEpochTopics(
+      kTime2, /*from_manually_triggered_calculation=*/false));
   state.UpdateNextScheduledCalculationTime();
 
   state.ClearContextDomain(HashedDomain(1));
--- components/browsing_topics/test_util.cc
+++ components/browsing_topics/test_util.cc
@@ -179,6 +179,7 @@ TesterBrowsingTopicsCalculator::TesterBrowsingTopicsCalculator(
                                site_data_manager,
                                annotator,
                                epochs,
+                               /*is_manually_triggered=*/false,
                                std::move(callback)),
       rand_uint64_queue_(std::move(rand_uint64_queue)) {}
 
@@ -195,6 +196,7 @@ TesterBrowsingTopicsCalculator::TesterBrowsingTopicsCalculator(
                                site_data_manager,
                                annotator,
                                base::circular_deque<EpochTopics>(),
+                               /*is_manually_triggered=*/false,
                                base::DoNothing()),
       use_mock_result_(true),
       mock_result_(std::move(mock_result)),
