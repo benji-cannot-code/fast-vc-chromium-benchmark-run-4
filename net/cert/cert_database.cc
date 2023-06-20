70352f25daa686def39a641886949920352bb4b8
Matt Mueller <mattm@chromium.org>
2023-06-20 22:03:52 +0000

    add Net.Certificate.ChangeNotification histogram
    Bug: 915463
    Change-Id: I14f2127cb39f85b0ed69be84d5a170126dac30fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618103
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160308}

--- net/cert/cert_database.cc
+++ net/cert/cert_database.cc
@@ -6,12 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_database.h"
 
 #include "base/memory/singleton.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/observer_list_threadsafe.h"
 #include "build/build_config.h"
 #include "net/log/net_log.h"
 #include "net/log/net_log_values.h"
 
 namespace net {
+
+namespace {
+
+void RecordNotificationHistogram(CertDatabase::HistogramNotificationType type) {
+  base::UmaHistogramEnumeration("Net.Certificate.ChangeNotification", type);
+}
+
+}  // namespace
 
 // static
 CertDatabase* CertDatabase::GetInstance() {
@@ -36,6 +45,8 @@ void CertDatabase::NotifyObserversTrustStoreChanged() {
   net::NetLog::Get()->AddGlobalEntry(
       NetLogEventType::CERTIFICATE_DATABASE_TRUST_STORE_CHANGED);
 
+  RecordNotificationHistogram(HistogramNotificationType::kTrust);
+
   observer_list_->Notify(FROM_HERE, &Observer::OnTrustStoreChanged);
 }
 
@@ -45,6 +56,8 @@ void CertDatabase::NotifyObserversClientCertStoreChanged() {
   // AddGlobalEntry() call without much computation is really cheap.
   net::NetLog::Get()->AddGlobalEntry(
       NetLogEventType::CERTIFICATE_DATABASE_CLIENT_CERT_STORE_CHANGED);
+
+  RecordNotificationHistogram(HistogramNotificationType::kClientCert);
 
   observer_list_->Notify(FROM_HERE, &Observer::OnClientCertStoreChanged);
 }
