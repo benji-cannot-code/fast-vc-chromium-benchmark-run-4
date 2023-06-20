9b76e4a3d64f0955fd8ba1d2bdf5e75b98567ef8
Maggie Cai <mxcai@chromium.org>
2023-06-20 04:41:53 +0000

    Revert "vt:Implement snapshot containing block"
    This reverts commit adc0081414af99f8766b320be2643add71cdc02d.
    
    Reason for revert: This CL is likely the cause of failure of virtual/view-transition-mpa-serialization/external/wpt/css/css-view-transitions/pseudo-rendering-invalidation.html
    virtual/view-transition-wide-gamut/external/wpt/css/css-view-transitions/pseudo-rendering-invalidation.html
    virtual/view-transition/external/wpt/css/css-view-transitions/pseudo-rendering-invalidation.html in Builder Linux Tests (dbg)(1) starting from https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29/114121
    
    Original change's description:
    > vt:Implement snapshot containing block
    >
    > Implements the snapshot containing block by inserting an anonymous
    > layout object between the LayoutView and the ::view-transition.
    >
    > Bug: 1432630
    > Change-Id: Ic100e2cfa333b2369c9ff1682d9cc8586d5ca09d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455568
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1159709}
    
    Bug: 1432630
    Change-Id: I279e8065aca7ccd52ae1dacddc4cd0a9d32f3202
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626398
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159817}

--- third_party/blink/renderer/core/layout/layout_view.cc
+++ third_party/blink/renderer/core/layout/layout_view.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/hit_test_result.h"
 #include "third_party/blink/renderer/core/layout/layout_counter.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
-#include "third_party/blink/renderer/core/layout/layout_view_transition_root.h"
 #include "third_party/blink/renderer/core/layout/ng/list/layout_ng_inline_list_item.h"
 #include "third_party/blink/renderer/core/layout/ng/list/layout_ng_list_item.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_node.h"
@@ -258,30 +257,6 @@ LayoutUnit LayoutView::ComputeMinimumWidth() {
   return min;
 }
 
-void LayoutView::AddChild(LayoutObject* new_child, LayoutObject* before_child) {
-  if (new_child->StyleRef().StyleType() == kPseudoIdViewTransition) {
-    // The view-transition pseudo tree is needs to be laid out within the
-    // "snapshot containing block". This is implemented by inserting an
-    // anonymous LayoutViewTransitionRoot between the ::view-transition and
-    // LayoutView.
-    CHECK(!before_child);
-    CHECK(!GetViewTransitionRoot());
-
-    LayoutViewTransitionRoot* snapshot_containing_block =
-        MakeGarbageCollected<LayoutViewTransitionRoot>(GetDocument());
-    LayoutBlockFlow::AddChild(snapshot_containing_block, /*before_child=*/nullptr);
-    snapshot_containing_block->AddChild(new_child);
-
-    ViewTransition* transition =
-        ViewTransitionUtils::GetActiveTransition(GetDocument());
-    CHECK(transition);
-    transition->UpdateSnapshotContainingBlockStyle();
-    return;
-  }
-
-  LayoutBlockFlow::AddChild(new_child, before_child);
-}
-
 bool LayoutView::IsChildAllowed(LayoutObject* child,
                                 const ComputedStyle&) const {
   NOT_DESTROYED();
@@ -398,11 +373,6 @@ TrackedDescendantsMap& LayoutView::SvgTextDescendantsMap() {
   if (!svg_text_descendants_)
     svg_text_descendants_ = MakeGarbageCollected<TrackedDescendantsMap>();
   return *svg_text_descendants_;
-}
-
-LayoutViewTransitionRoot* LayoutView::GetViewTransitionRoot() const {
-  // Returns nullptr if LastChild isn't a ViewTransitionRoot.
-  return DynamicTo<LayoutViewTransitionRoot>(LastChild());
 }
 
 void LayoutView::Paint(const PaintInfo& paint_info) const {
--- third_party/blink/renderer/core/layout/layout_view.h
+++ third_party/blink/renderer/core/layout/layout_view.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class LayoutQuote;
-class LayoutViewTransitionRoot;
 class LocalFrameView;
 class ViewFragmentationContext;
 
@@ -99,9 +98,6 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
     NOT_DESTROYED();
     return kNormalPaintLayer;
   }
-
-  void AddChild(LayoutObject* new_child,
-                LayoutObject* before_child = nullptr) override;
 
   bool IsChildAllowed(LayoutObject*, const ComputedStyle&) const override;
 
@@ -347,8 +343,6 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
   LogicalSize InitialContainingBlockSize() const;
 
   TrackedDescendantsMap& SvgTextDescendantsMap();
-
-  LayoutViewTransitionRoot* GetViewTransitionRoot() const;
 
  protected:
   void StyleDidChange(StyleDifference, const ComputedStyle* old_style) override;
