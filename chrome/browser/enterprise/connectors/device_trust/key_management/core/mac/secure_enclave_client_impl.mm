43936d387925722241f2b58a342160352ec92d81
Avi Drissman <avi@chromium.org>
2023-06-20 22:16:55 +0000

    Convert /chrome/browser to use ARC
    This is everything left in /chrome/browser that isn’t in
    /chrome/browser/ui (that’s https://crrev.com/c/4615920).
    
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: I1f2e5346c95285f666ba2e1b23cae2c46f0885df
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628310
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160314}

--- chrome/browser/enterprise/connectors/device_trust/key_management/core/mac/secure_enclave_client_impl.mm
+++ chrome/browser/enterprise/connectors/device_trust/key_management/core/mac/secure_enclave_client_impl.mm
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/boringssl/src/include/openssl/evp.h"
 #include "third_party/boringssl/src/include/openssl/ssl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace enterprise_connectors {
 
 namespace {
