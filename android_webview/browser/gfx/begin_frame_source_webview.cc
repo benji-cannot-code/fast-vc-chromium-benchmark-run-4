63dea56bf23bbbcbe2cc0f9ca41e866fe0a3a4be
Eric Seckler <eseckler@chromium.org>
2023-06-20 12:36:43 +0000

    PowerModes: Remove android_webview voters
    Not used anymore, cleaning up.
    
    Bug: 1381645
    Change-Id: I629da34d9cd2a8fb07716b8b65da88322f99b28d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626381
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159965}

--- android_webview/browser/gfx/begin_frame_source_webview.cc
+++ android_webview/browser/gfx/begin_frame_source_webview.cc
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/no_destructor.h"
 #include "base/trace_event/trace_event.h"
-#include "components/power_scheduler/power_mode.h"
-#include "components/power_scheduler/power_mode_arbiter.h"
-#include "components/power_scheduler/power_mode_voter.h"
 
 namespace android_webview {
 
@@ -55,10 +52,7 @@ void BeginFrameSourceWebView::BeginFrameSourceClient::OnNeedsBeginFrames(
 BeginFrameSourceWebView::BeginFrameSourceWebView()
     : ExternalBeginFrameSource(&bfs_client_),
       bfs_client_(this),
-      parent_observer_(std::make_unique<BeginFrameObserver>(this)),
-      animation_power_mode_voter_(
-          power_scheduler::PowerModeArbiter::GetInstance()->NewVoter(
-              "PowerModeVoter.Animation")) {
+      parent_observer_(std::make_unique<BeginFrameObserver>(this)) {
   OnSetBeginFrameSourcePaused(true);
 }
 
@@ -93,14 +87,10 @@ void BeginFrameSourceWebView::ObserveBeginFrameSource(
 void BeginFrameSourceWebView::OnNeedsBeginFrames(bool needs_begin_frames) {
   if (needs_begin_frames) {
     TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("cc,benchmark", "NeedsBeginFrames", this);
-    animation_power_mode_voter_->VoteFor(
-        power_scheduler::PowerMode::kAnimation);
     if (observed_begin_frame_source_)
       observed_begin_frame_source_->AddObserver(parent_observer_.get());
   } else {
     TRACE_EVENT_NESTABLE_ASYNC_END0("cc,benchmark", "NeedsBeginFrames", this);
-    animation_power_mode_voter_->ResetVoteAfterTimeout(
-        power_scheduler::PowerModeVoter::kAnimationTimeout);
     if (observed_begin_frame_source_)
       observed_begin_frame_source_->RemoveObserver(parent_observer_.get());
   }
