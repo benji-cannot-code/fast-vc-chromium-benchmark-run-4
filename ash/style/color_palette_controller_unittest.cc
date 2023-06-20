45c76580496035fe572c994439f383e0d76f18e8
Sean Kau <skau@chromium.org>
2023-06-20 20:19:26 +0000

    Change default SchemeVariant to Neutral for TimeOfDay wallpaper
    The default scheme with TimeOfDay wallpaper is too loud. Switch the
    default for these devices to Neutral from TonalSpot (but leave others
    alone).
    
    Bug: b:287537149
    Change-Id: I7e98863f8e8b5e2434765e1cabec26dee62c59f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4623032
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160230}

--- ash/style/color_palette_controller_unittest.cc
+++ ash/style/color_palette_controller_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <ostream>
 
+#include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/public/cpp/ash_prefs.h"
 #include "ash/shell.h"
@@ -122,6 +123,16 @@ TEST_F(ColorPaletteControllerTest, ExpectedEmptyValues) {
             color_palette_controller()->GetColorScheme(kAccountId));
   EXPECT_EQ(absl::nullopt,
             color_palette_controller()->GetStaticColor(kAccountId));
+}
+
+// Verifies that when the TimeOfDayWallpaper feature is active, the default
+// color scheme is Neutral instead of TonalSpot.
+TEST_F(ColorPaletteControllerTest, ExpectedColorScheme_TimeOfDay) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      {ash::features::kTimeOfDayWallpaper, chromeos::features::kJelly}, {});
+  EXPECT_EQ(ColorScheme::kNeutral,
+            color_palette_controller()->GetColorScheme(kAccountId));
 }
 
 TEST_F(ColorPaletteControllerTest,
@@ -453,6 +464,18 @@ TEST_F(ColorPaletteControllerLocalPrefTest,
       .Times(1);
 
   color_palette_controller()->SelectLocalAccount(kAccountId);
+}
+
+// Verifies that when the TimeOfDayWallpaper feature is active, the default
+// color scheme is Neutral instead of TonalSpot in local_state.
+TEST_F(ColorPaletteControllerLocalPrefTest, NoLocalAccount_TimeOfDayScheme) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      {ash::features::kTimeOfDayWallpaper, chromeos::features::kJelly}, {});
+  // Since `kAccountId` is not logged in, this triggers default local_state
+  // behavior.
+  EXPECT_EQ(ColorScheme::kNeutral,
+            color_palette_controller()->GetColorScheme(kAccountId));
 }
 
 TEST_F(ColorPaletteControllerLocalPrefTest,
