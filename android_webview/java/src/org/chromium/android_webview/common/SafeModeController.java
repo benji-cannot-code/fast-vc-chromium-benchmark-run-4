c871f0685f63a05ae76731fbd1b2885f61b1b561
Andrew Grieve <agrieve@chromium.org>
2023-06-21 17:21:18 +0000

    Android: Add ResettersForTesting.register() to //android_webview
    Used Android Studio's "Structured Replace" to add it to all
    setForTesting methods that contained a single assignment. Then
    went through all spots to reset to "null/false" vs |oldValue|.
    
    Removed calls to modified set*ForTesting(null/false) methods using
    grep/sed/python then removed empty @After methods.
    
    * Also cleans up some calls that had both @VisibleForTesting and
      ForTest suffix (annotation is incorrect in this case).
    * Also renamed a few test-only variables to have "ForTesting" suffix.
    
    Updated tests that broke as a result. Things that broke:
    * When setForTesting() was being called in @BeforeClass
    * When a value was set as a result of normal logic, and then
      setForTesting(null) was called to reset it (which then returned it to
      it's non-null value).
    
    Bug: 1416224
    Change-Id: I3c43b8564db847a5ff8d4b61546f06aa8370aa3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626900
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160725}

--- android_webview/java/src/org/chromium/android_webview/common/SafeModeController.java
+++ android_webview/java/src/org/chromium/android_webview/common/SafeModeController.java
@@ -13,10 +13,10 @@ import android.net.Uri;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
-import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.ResettersForTesting;
 import org.chromium.base.StrictModeContext;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.BuildConfig;
@@ -66,9 +66,9 @@ public class SafeModeController {
      * @param controller The SafeModeController object to return from getInstance(). Passing in a
      * null value resets this.
      */
-    @VisibleForTesting
     public static void setInstanceForTests(SafeModeController controller) {
         sInstanceForTests = controller;
+        ResettersForTesting.register(() -> sInstanceForTests = null);
     }
 
     public static SafeModeController getInstance() {
@@ -99,7 +99,6 @@ public class SafeModeController {
         mRegisteredActions = actions;
     }
 
-    @VisibleForTesting
     public void unregisterActionsForTesting() {
         mRegisteredActions = null;
     }
--- android_webview/junit/src/org/chromium/android_webview/robolectric/nonembedded/AwComponentUpdateServiceSafeModeTest.java
+++ android_webview/junit/src/org/chromium/android_webview/robolectric/nonembedded/AwComponentUpdateServiceSafeModeTest.java
@@ -59,7 +59,6 @@ public class AwComponentUpdateServiceSafeModeTest {
     public void tearDown() {
         Assert.assertTrue("Failed to delete " + mDirectory.getAbsolutePath(),
                 FileUtils.recursivelyDeleteFile(mDirectory, null));
-        SafeModeController.setInstanceForTests(null);
     }
 
     @Test
