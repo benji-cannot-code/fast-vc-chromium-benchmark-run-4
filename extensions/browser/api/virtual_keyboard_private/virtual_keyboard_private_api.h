45ed307c9990461753b017d74479892a5bdc319c
Colin Kincaid <ckincaid@chromium.org>
2023-06-21 01:13:12 +0000

    multipaste: Encode bitmaps in background when serializing items for VK
    This CL changes virtual keyboard clipboard item serialization so that
    getting a data URL for a bitmap happens on a background thread. This is
    a speculative fix for the browser hangs reported in the attached bug.
    In the future, we may also want to downsample images or cache data URLs
    if the URL generation is indeed costly.
    
    Bug: b/286904177
    Change-Id: I61d3077798231f78763f3793f4e51376d6e6233d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4621739
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160411}

--- extensions/browser/api/virtual_keyboard_private/virtual_keyboard_private_api.h
+++ extensions/browser/api/virtual_keyboard_private/virtual_keyboard_private_api.h
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_API_VIRTUAL_KEYBOARD_PRIVATE_VIRTUAL_KEYBOARD_PRIVATE_API_H_
 #define EXTENSIONS_BROWSER_API_VIRTUAL_KEYBOARD_PRIVATE_VIRTUAL_KEYBOARD_PRIVATE_API_H_
 
+#include <memory>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
+#include "extensions/browser/api/virtual_keyboard_private/virtual_keyboard_delegate.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/browser/extension_function.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -267,7 +269,12 @@ class VirtualKeyboardPrivateGetClipboardHistoryFunction
   ResponseAction Run() override;
 
  private:
-  void OnGetClipboardHistory(std::vector<ash::ClipboardHistoryItem> items);
+  void OnGetClipboardHistory(
+      std::vector<ash::ClipboardHistoryItem> history_items);
+
+  using ClipboardItems =
+      std::vector<extensions::api::virtual_keyboard_private::ClipboardItem>;
+  void OnClipboardHistoryItemsConverted(std::unique_ptr<ClipboardItems> items);
 };
 
 class VirtualKeyboardPrivatePasteClipboardItemFunction
