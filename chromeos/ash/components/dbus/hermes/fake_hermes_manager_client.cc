f1074c05db9bcf9981e87b94ce701f6294b4bba2
Chad Duffin <chadduffin@chromium.org>
2023-06-21 15:54:20 +0000

    cellular: Update behavior of Hermes fakes
    This change updates the FakeHermesEuiccClient functionality to update
    the Shill SIM slot info every time that the EUICC list changes or the
    list of eSIM profiles changes.
    
    Without Hermes fakes: https://screenshot.googleplex.com/4QNLbw8ovHf5fNR.png
    With Hermes fakes: https://screenshot.googleplex.com/42ZdQsBEC2m2VoM.png
    
    Bug: b/286927860
    Test: Manually verified on Linux build.
    Change-Id: I3a89f6683aa905f90ba763562dfe3c79a4e0c2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616410
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160673}

--- chromeos/ash/components/dbus/hermes/fake_hermes_manager_client.cc
+++ chromeos/ash/components/dbus/hermes/fake_hermes_manager_client.cc
@@ -96,6 +96,9 @@ void FakeHermesManagerClient::ParseCommandLineSwitch() {
 }
 
 void FakeHermesManagerClient::NotifyAvailableEuiccListChanged() {
+  HermesEuiccClient::TestInterface* euicc_client_test =
+      HermesEuiccClient::Get()->GetTestInterface();
+  euicc_client_test->UpdateShillDeviceSimSlotInfo();
   for (auto& observer : observers()) {
     observer.OnAvailableEuiccListChanged();
   }
