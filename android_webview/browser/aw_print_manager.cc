a271e27d37d0e4efc5bad1ba7457f79490cf6d23
Lei Zhang <thestig@chromium.org>
2023-06-21 19:17:09 +0000

    Remove RealAwPrintManagerCookies feature flag
    This feature flag is a safety switch to ensure a quick fallback to a
    dummy print cookie if using real cookies in AwPrintManager did not work.
    Now that RealAwPrintManagerCookies have rolled out successfully, remove
    the safety switch.
    
    Bug: 1286556
    Change-Id: Ied0223164853661b603595b27c884da5dc8cfc7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602780
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160788}

--- android_webview/browser/aw_print_manager.cc
+++ android_webview/browser/aw_print_manager.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/feature_list.h"
 #include "base/file_descriptor_posix.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
@@ -27,12 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace android_webview {
 
 namespace {
-
-// Enables real document cookie values instead of a dummy value.
-// TODO(crbug.com/1286556): Remove this kill switch after a safe rollout.
-BASE_FEATURE(kRealAwPrintManagerCookies,
-             "RealAwPrintManagerCookies",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 uint32_t SaveDataToFd(int fd,
                       uint32_t page_count,
@@ -104,11 +97,7 @@ void AwPrintManager::UpdateParam(
   settings_ = std::move(settings);
   fd_ = file_descriptor;
   set_pdf_writing_done_callback(std::move(callback));
-  constexpr int kDummyCookie = 1;
-  const int cookie = base::FeatureList::IsEnabled(kRealAwPrintManagerCookies)
-                         ? printing::PrintSettings::NewCookie()
-                         : kDummyCookie;
-  set_cookie(cookie);
+  set_cookie(printing::PrintSettings::NewCookie());
 }
 
 void AwPrintManager::ScriptedPrint(
