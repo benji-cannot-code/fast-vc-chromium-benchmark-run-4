6a56ef333b58baf1a670eb1fc26012fadb2276f1
Dominik Röttsches <drott@chromium.org>
2023-06-21 09:45:58 +0000

    [Fontations-backend] Add a feature flag to enable the backend
    Map from a UI feature flag under chrome://flags to the Blink
    runtime-enabled feature. Only shows when the compile flag is turned on.
    
    Move the Fontations buildflag from blink/renderer/platform/fonts to
    /blink/public/common, where Blink previously maintained two other
    buildflags, so that the buildflag becomes accessible in Chrome side
    code.
    
    Bug: 1446251
    Change-Id: I3f7342f5db56dd8221bf7a8c54b95549716f0ff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618041
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160547}

--- third_party/blink/renderer/platform/fonts/web_font_typeface_factory.h
+++ third_party/blink/renderer/platform/fonts/web_font_typeface_factory.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkFontMgr.h"
 
 #include "build/build_config.h"
-#include "third_party/blink/renderer/platform/fonts/fontations_buildflags.h"
+#include "third_party/blink/public/common/buildflags.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
