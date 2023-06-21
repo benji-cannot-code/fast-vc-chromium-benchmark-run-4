786935f8f5c1b60dc7e742d1f1fddc3b79bfde25
Charlie Reis <creis@chromium.org>
2023-06-21 23:17:53 +0000

    Replace ReloadType::ORIGINAL_REQUEST_URL with LoadOriginalRequestURL.
    When reloading the current NavigationEntry using the original request's
    URL, it is safer to treat it as a new navigation to the URL with
    replacement, rather than using any of the state from the
    NavigationEntry for a different URL.
    
    Bug: 1427288
    Change-Id: Iceae630f17bc8091637d500512f887fa5796070c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370988
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160920}

--- chrome/browser/chromeos/tablet_mode/tablet_mode_page_behavior.cc
+++ chrome/browser/chromeos/tablet_mode/tablet_mode_page_behavior.cc
@@ -103,7 +103,7 @@ void TabletModePageBehavior::SetMobileLikeBehaviorEnabled(bool enabled) {
                 arc::ArcWebContentsData::ArcWebContentsData::
                     kArcTransitionFlag)) {
           entry->SetIsOverridingUserAgent(false);
-          controller.Reload(content::ReloadType::ORIGINAL_REQUEST_URL, true);
+          controller.LoadOriginalRequestURL();
         }
       }
     }
