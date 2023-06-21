c2d40087b7756e552764804c08378b0b2afa9a3b
Jesse Melhuish <melhuishj@google.com>
2023-06-21 20:59:03 +0000

    [floss] Add BtifStatus::Timeout for socket listening
    BUG=b:286684158
    
    Change-Id: Ibf704aef747dc53cfb808a554248ff393b2bb333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4621728
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Commit-Queue: Jesse Melhuish <melhuishj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160851}

--- device/bluetooth/floss/floss_dbus_client.h
+++ device/bluetooth/floss/floss_dbus_client.h
@@ -446,6 +446,7 @@ class DEVICE_BLUETOOTH_EXPORT FlossDBusClient {
     kJniEnvironmentError,
     kJniThreadAttachError,
     kWakelockError,
+    kTimeout,
   };
 
   enum class BluetoothTransport {
