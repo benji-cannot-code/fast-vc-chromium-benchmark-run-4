a6ff715619c44f7d8c9025c3913189f73bfa9353
Zach Katz <katzz@google.com>
2023-06-21 23:20:31 +0000

    [CCT TestApp] Allow URLs that don't start with https://
    Bug: 1456466
    Change-Id: Ic880dcb0a32e4bc29fb033d889a6c99e32b8eb90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4630418
    Commit-Queue: Zach Katz <katzz@google.com>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160922}

--- chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/MainActivity.java
+++ chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/MainActivity.java
@@ -36,6 +36,7 @@ import android.view.WindowInsets;
 import android.view.WindowMetrics;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.URLUtil;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -840,6 +841,7 @@ public class MainActivity
     }
 
     private void launchCct(String url, SharedPreferences.Editor editor) {
+        url = mayPrependUrl(url);
         CustomTabsSession session = getSession();
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(session);
         prepareMenuItems(builder);
@@ -957,6 +959,14 @@ public class MainActivity
                 mSideSheetRoundedCornerCheckbox.isChecked() ? CHECKED : UNCHECKED);
         editor.putInt(SHARED_PREF_DECORATION, decorationType);
         editor.apply();
+    }
+
+    private String mayPrependUrl(String url) {
+        if (!URLUtil.isValidUrl(url)) {
+            url = "https://" + url;
+        }
+
+        return url;
     }
 
     private void prepareAesthetics(CustomTabsIntent.Builder builder, boolean isPcct) {
