818d434d25b0fa78da6b9abbfef5a05a7624f14b
Longbo Wei <longbowei@google.com>
2023-06-21 01:12:05 +0000

    shortcuts: Add maxAccelsReached Hint
    * Show a hint when max number of accelerators reached in the
      edit-dialog and also hide the addButton in this situation.
    
    * Video: http://go/scrcast/NDY0NDk3NzgwMDkwNDcwNHw2ZWQyYjI1Yy1iZA
    
    Bug: b/216049298
    Test: browser_tests --gtest_filter=ShortcutCustomizationApp*
    Change-Id: Ied0282f7907a4a1e2baac7857817b14a64aadbbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616146
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160409}

--- ash/webui/shortcut_customization_ui/resources/js/accelerator_edit_dialog.ts
+++ ash/webui/shortcut_customization_ui/resources/js/accelerator_edit_dialog.ts
@@ -34,6 +34,9 @@ declare global {
   }
 }
 
+// A maximum of 5 accelerators are allowed.
+const MAX_NUM_ACCELERATORS = 5;
+
 /**
  * @fileoverview
  * 'accelerator-edit-dialog' is a dialog that displays the accelerators for
@@ -167,9 +170,20 @@ export class AcceleratorEditDialogElement extends
     this.focusAcceleratorItemContainer();
   }
 
+  protected showNewAccelerator(): boolean {
+    // Show new pending accelerators when ViewState is not VIEW.
+    return this.pendingNewAcceleratorState != ViewState.VIEW &&
+        this.acceleratorLimitNotReached();
+  }
+
   protected showAddButton(): boolean {
     // If the state is VIEW, no new pending accelerators are being added.
-    return this.pendingNewAcceleratorState === ViewState.VIEW;
+    return this.pendingNewAcceleratorState === ViewState.VIEW &&
+        this.acceleratorLimitNotReached();
+  }
+
+  protected acceleratorLimitNotReached(): boolean {
+    return this.acceleratorInfos.length < MAX_NUM_ACCELERATORS;
   }
 
   protected onRestoreDefaultButtonClicked(): void {
