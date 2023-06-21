65d6dd58661b46dea87b61cf0dbfc95a9c40bf22
Riley Tatum <rtatum@google.com>
2023-06-21 20:18:57 +0000

    [GM3][Panorama] Show full color swatch always except for custom color
    * Update to always show the full swatch for Chrome Refresh 2023 for all
      of the chrome colors and default color.
    * Custom color always shows just one color in Chrome Refresh so always
      have backgroundColorHidden be true when the flag is on.
    
    Screenshots:
    
    Upload Image (background image w/ default color)
    Flag Enabled: http://screenshot/ikC3HgdqqZ6suGF
    Flag Disabled: http://screenshot/6hbSgHZHkS7Vitp
    
    Collection Image (background image w/ main color)
    Flag Enabled: http://screenshot/BrESnB33dXFj5HR
    Flag Disabled: http://screenshot/9w7qo8Jc2TLQBQ6
    
    Classic Chrome
    Flag Enabled: http://screenshot/3qWMJqZAgp9u24o
    Flag Disabled: http://screenshot/5tjEiYgLLwJMuRF
    
    Bug: 1430277
    Change-Id: Iee531b2963a5a3ff734983d956fc8675b7a9d372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629501
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160825}

--- chrome/browser/resources/side_panel/customize_chrome/colors.ts
+++ chrome/browser/resources/side_panel/customize_chrome/colors.ts
@@ -74,6 +74,14 @@ export class ColorsElement extends PolymerElement {
         },
       },
       showManagedDialog_: Boolean,
+      showBackgroundColor_: {
+        type: Boolean,
+        computed: 'computeShowBackgroundColor_(theme_)',
+      },
+      showCustomColorBackgroundColor_: {
+        type: Boolean,
+        computed: 'computeShowCustomColorBackgroundColor_(theme_)',
+      },
     };
   }
 
@@ -91,6 +99,14 @@ export class ColorsElement extends PolymerElement {
   private customColor_: Color;
   private setThemeListenerId_: number|null = null;
   private showManagedDialog_: boolean;
+  private isChromeRefresh2023_: boolean;
+
+  constructor() {
+    super();
+    this.isChromeRefresh2023_ =
+        loadTimeData.getString('chromeRefresh2023Attribute') ===
+        'chrome-refresh-2023';
+  }
 
   override connectedCallback() {
     super.connectedCallback();
@@ -109,7 +125,7 @@ export class ColorsElement extends PolymerElement {
   }
 
   private computeDefaultColor_(): Color {
-    if (loadTimeData.getString('chromeRefresh2023Attribute')) {
+    if (this.isChromeRefresh2023_) {
       return this.theme_.isDarkMode ? DARK_BASELINE_BLUE_COLOR :
                                       LIGHT_BASELINE_BLUE_COLOR;
     }
@@ -187,6 +203,14 @@ export class ColorsElement extends PolymerElement {
   private themeHasMainColor_(): boolean {
     return !!this.theme_ && !!this.theme_.backgroundImage &&
         !!this.theme_.backgroundImage.mainColor;
+  }
+
+  private computeShowBackgroundColor_(): boolean {
+    return this.isChromeRefresh2023_ || !this.themeHasBackgroundImage_();
+  }
+
+  private computeShowCustomColorBackgroundColor_(): boolean {
+    return !this.isChromeRefresh2023_ && !this.themeHasBackgroundImage_();
   }
 
   private onDefaultColorClick_() {
