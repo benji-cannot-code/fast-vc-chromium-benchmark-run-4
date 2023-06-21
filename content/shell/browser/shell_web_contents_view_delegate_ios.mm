47691f9543371037f0dc630cace027d4ae86fb6c
Avi Drissman <avi@chromium.org>
2023-06-21 00:35:14 +0000

    Tag that /content/shell uses ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This adds the ARC boilerplate as a marker to indicate that these files
    were audited.
    
    Bug: 1280317
    Change-Id: Iefc516e574e1ccf7518ec8edae53ba8c56f109ad
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628155
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160388}

--- content/shell/browser/shell_web_contents_view_delegate_ios.mm
+++ content/shell/browser/shell_web_contents_view_delegate_ios.mm
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/shell/browser/shell_web_contents_view_delegate_creator.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace content {
 
 std::unique_ptr<WebContentsViewDelegate> CreateShellWebContentsViewDelegate(
