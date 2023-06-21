6b3268232a9d21a4c1f7a4bdc1d2ace7869a15ec
Aya ElAttar <ayaelattar@chromium.org>
2023-06-21 16:53:15 +0000

    Keep IOTasks infos with blocked files
    Don't remove IOTasks with blocked files upon completion because their info are needed to show detailed error dialogs later.
    
    Bug: b/288096592, b/288097070
    Change-Id: Ia28ce122d268dbb778e0ef1d2ed7aa34f22a3247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628931
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Aida Zolic <aidazolic@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Auto-Submit: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160708}

--- chrome/browser/ash/policy/dlp/files_policy_notification_manager.cc
+++ chrome/browser/ash/policy/dlp/files_policy_notification_manager.cc
@@ -665,6 +665,7 @@ void FilesPolicyNotificationManager::HandleFilesPolicyErrorNotificationClick(
     }
   }
   Dismiss(context_, notification_id);
+  io_tasks_.erase(task_id);
 }
 
 void FilesPolicyNotificationManager::ShowDialogForIOTask(
@@ -677,6 +678,9 @@ void FilesPolicyNotificationManager::ShowDialogForIOTask(
   }
 
   ShowFilesPolicyDialog(std::ref(io_tasks_.at(task_id)), type, modal_parent);
+  if (type == FilesDialogType::kError) {
+    io_tasks_.erase(task_id);
+  }
 }
 
 void FilesPolicyNotificationManager::ShowDialogForNonIOTask(
@@ -794,7 +798,10 @@ void FilesPolicyNotificationManager::OnIOTaskStatus(
   if (!HasIOTask(status.task_id) &&
       status.state == file_manager::io_task::State::kQueued) {
     AddIOTask(status.task_id, action);
-  } else if (HasIOTask(status.task_id) && status.IsCompleted()) {
+    return;
+  }
+
+  if (HasIOTask(status.task_id) && status.IsCompleted()) {
     if (status.state == file_manager::io_task::State::kCancelled &&
         HasWarning(status.task_id)) {
       CHECK(!io_tasks_.at(status.task_id)
@@ -803,8 +810,10 @@ void FilesPolicyNotificationManager::OnIOTaskStatus(
           .Run(/*should_proceed=*/false);
       io_tasks_.at(status.task_id).warning_info.reset();
     }
-    // If it's in a terminal state, stop observing.
-    io_tasks_.erase(status.task_id);
+    // Remove only if the IOTask doesn't have any blocked file.
+    if (!HasBlockedFiles(status.task_id)) {
+      io_tasks_.erase(status.task_id);
+    }
   }
 }
 
