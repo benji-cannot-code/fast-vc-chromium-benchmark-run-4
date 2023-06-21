5c66888bb3c2766bc018e6c8041e4f35e4bdb366
Igor Ruvinov <igorruvinov@chromium.org>
2023-06-21 22:22:17 +0000

    Allow NavigationHandle to provide the response body contents.
    The body is read from the data pipe inside NavigationRequest to ensure
    that it is not incorrectly consumed by the client. Response body
    contents are asynchronously passed to the client via callback.
    
    Reference usage of the new API: https://crrev.com/c/4541142
    
    Bug: 1445072
    Change-Id: I2b3ec68f0cc77633de18e0855eeda5014f59e314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4526775
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160898}

--- content/public/test/mock_navigation_handle.h
+++ content/public/test/mock_navigation_handle.h
@@ -218,6 +218,7 @@ class MockNavigationHandle : public NavigationHandle {
   MOCK_METHOD(PrerenderTriggerType, GetPrerenderTriggerType, ());
   MOCK_METHOD(std::string, GetPrerenderEmbedderHistogramSuffix, ());
   MOCK_METHOD(void, SetAllowCookiesFromBrowser, (bool));
+  MOCK_METHOD(void, GetResponseBody, (ResponseBodyCallback));
 
 #if BUILDFLAG(IS_ANDROID)
   MOCK_METHOD(const base::android::JavaRef<jobject>&,
