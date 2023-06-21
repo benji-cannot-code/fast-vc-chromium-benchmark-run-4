b29fb0712a54a4f147f10009b527b9864f6e537c
Adam Rice <ricea@chromium.org>
2023-06-21 06:51:33 +0000

    Optimise URLLoaderFactory::UpdateLoadInfo
    network::URLLoaderFactory::UpdateLoadInfo() is a hot function in
    profiles of the network process. Restructure it a bit to reduce the
    amount of work it does:
    
    1. Call URLLoader::CreateLoadInfo() only for the most interesting url
       loader. A new method, URLLoader::PartialLoadInfo() returns just the
       info needed to choose the most interesting loader.
    2. Call URLLoader::GetLoadState() only once for each url loader. This is
       a hot function.
    
    BUG=1276591
    
    Change-Id: Ice845484def563ae4ef02c6e77410e07687f6175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618315
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160508}

--- services/network/url_loader.h
+++ services/network/url_loader.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_states.h"
 #include "net/base/network_delegate.h"
 #include "net/base/transport_info.h"
+#include "net/base/upload_progress.h"
 #include "net/cookies/cookie_setting_override.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_request.h"
@@ -125,6 +126,24 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
    private:
     mojo::Remote<mojom::URLLoaderClient> mojo_client_;
     base::WeakPtr<mojom::URLLoaderClient> sync_client_;
+  };
+
+  // A subset of the fields in mojom::LoadInfo.
+  struct PartialLoadInfo final {
+    PartialLoadInfo() = default;
+    PartialLoadInfo(net::LoadStateWithParam load_state,
+                    net::UploadProgress upload_progress);
+
+    // Avoid accidentally copying this object as `load_state` contains a string.
+    PartialLoadInfo(const PartialLoadInfo&) = delete;
+    PartialLoadInfo& operator=(const PartialLoadInfo&) = delete;
+
+    // Moving it is good.
+    PartialLoadInfo(PartialLoadInfo&&) = default;
+    PartialLoadInfo& operator=(PartialLoadInfo&&) = default;
+
+    net::LoadStateWithParam load_state;
+    net::UploadProgress upload_progress;
   };
 
   // |delete_callback| tells the URLLoader's owner to destroy the URLLoader.
@@ -270,7 +289,13 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
 
   void SetEnableReportingRawHeaders(bool enable);
 
-  mojom::LoadInfoPtr CreateLoadInfo();
+  // Returns a subset of the info in mojom::LoadInfo. This is sufficient to make
+  // a decision on whether to call CreateLoadInfo() for this loader.
+  PartialLoadInfo GetPartialLoadInfo() const;
+
+  // Returns a mojom::LoadInfo, reusing the data returned by
+  // GetPartialLoadInfo().
+  mojom::LoadInfoPtr CreateLoadInfo(const PartialLoadInfo& partial_load_info);
 
   // Gets the URLLoader associated with this request.
   static URLLoader* ForRequest(const net::URLRequest& request);
