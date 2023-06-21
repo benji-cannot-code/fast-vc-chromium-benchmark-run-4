9ddfd415e573eb433207204afcd0eec401664115
Jens Mueller <muellerj@google.com>
2023-06-21 08:21:20 +0000

    WebUI: Make it possible to display the label first in cr-radio-button
    Adding a new 'label-first' CSS class, which when specified causes the
    label to be shown before the radio button itself. For example
    
    <cr-radio-button class="label-first">hello world</cr-radio-button>
    
    Fixed: 1455391
    Change-Id: I2bf474f88ab3e3a402d7f7f0f09214533fe73f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4614521
    Commit-Queue: Jens Mueller <muellerj@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160529}

--- chrome/test/data/webui/cr_elements/cr_radio_button_test.ts
+++ chrome/test/data/webui/cr_elements/cr_radio_button_test.ts
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
 
 import {CrRadioButtonElement} from 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
-import {assertEquals, assertNotEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertEquals, assertNotEquals, assertFalse, assertTrue, assertLT, assertGT} from 'chrome://webui-test/chai_assert.js';
 // clang-format on
 
 suite('cr-radio-button', function() {
@@ -118,5 +118,20 @@ suite('cr-radio-button', function() {
     assertEquals(
         radioButton.shadowRoot!.querySelector('#label')!.textContent!.trim(),
         'foo');
+  });
+
+  test('Label First', () => {
+    const button = radioButton.$.button;
+    let buttonRect = button.getBoundingClientRect();
+    const labelWrapper = radioButton.shadowRoot!.querySelector('#labelWrapper');
+    assertTrue(!!labelWrapper);
+
+    let labelWrapperRect = labelWrapper.getBoundingClientRect();
+    assertLT(buttonRect.left, labelWrapperRect.left);
+
+    radioButton.classList.add('label-first');
+    buttonRect = button.getBoundingClientRect();
+    labelWrapperRect = labelWrapper.getBoundingClientRect();
+    assertGT(buttonRect.left, labelWrapperRect.left);
   });
 });
