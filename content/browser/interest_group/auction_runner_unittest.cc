4fd6d6d70abdeae64f9eaf506107532d2453ed5e
Orr Bernstein <orrb@google.com>
2023-06-21 22:02:47 +0000

    Add a NumConfigPromises metric to the Auction Latency UKM Entry.
    This will allow us to directly analyze the degree to which config
    promises are being used, and to correlate these to other metrics, e.g.
    e2e latency, to better understand the scale of impact of using config
    promises.
    
    Bug: 1424365
    Change-Id: I8ffdd7e27e3bda97feccb84b7cdb857c627be679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4601771
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Commit-Queue: Orr Bernstein <orrb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160889}

--- content/browser/interest_group/auction_runner_unittest.cc
+++ content/browser/interest_group/auction_runner_unittest.cc
@@ -2085,8 +2085,10 @@ class AuctionRunnerTest : public RenderViewHostTestHarness,
                 Entry::kNumBidsFilteredDuringInterestGroupLoadName,
                 Entry::kNumBidsFilteredDuringReprioritizationName,
                 Entry::kNumBidsFilteredByPerBuyerLimitsName,
-                /* GenerateBid outcome metrics */
+                /* Properties of the auction metrics */
                 Entry::kKAnonymityBidModeName,
+                Entry::kNumConfigPromisesName,
+                /* GenerateBid outcome metrics */
                 Entry::kNumInterestGroupsWithNoBidsName,
                 Entry::kNumInterestGroupsWithOnlyNonKAnonBidName,
                 Entry::kNumInterestGroupsWithSameBidForKAnonAndNonKAnonName,
@@ -2177,6 +2179,11 @@ class AuctionRunnerTest : public RenderViewHostTestHarness,
 
     MetricsExpectations& SetNumBidderWorklets(int64_t value) {
       num_bidder_worklets = value;
+      return *this;
+    }
+
+    MetricsExpectations& SetNumConfigPromises(int64_t value) {
+      num_config_promises = value;
       return *this;
     }
 
@@ -2291,6 +2298,7 @@ class AuctionRunnerTest : public RenderViewHostTestHarness,
     absl::optional<int64_t> num_sellers;
     int64_t num_distinct_owners = 0;
     int64_t num_bidder_worklets = 0;
+    int64_t num_config_promises = 0;
     int64_t num_bids_aborted_by_buyer_cumulative_timeout = 0;
     int64_t num_bids_aborted_by_bidder_worklet_fatal_error = 0;
     int64_t num_bids_filtered_during_interest_group_load = 0;
@@ -2388,6 +2396,9 @@ class AuctionRunnerTest : public RenderViewHostTestHarness,
     EXPECT_THAT(ukm_metrics,
                 HasMetricWithValue(UkmEntry::kKAnonymityBidModeName,
                                    static_cast<int64_t>(kanon_mode_)));
+    EXPECT_THAT(ukm_metrics,
+                HasMetricWithValue(UkmEntry::kNumConfigPromisesName,
+                                   expectations.num_config_promises));
     EXPECT_THAT(
         ukm_metrics,
         HasMetric(UkmEntry::kLoadInterestGroupPhaseLatencyInMillisName));
@@ -6500,6 +6511,14 @@ TEST_F(AuctionRunnerTest, PromiseAuctionSignals) {
   EXPECT_EQ(InterestGroupKey(kBidder2, kBidder2Name), result_.winning_group_id);
   EXPECT_EQ(GURL("https://ad2.com/"), result_.ad_descriptor->url);
   EXPECT_THAT(result_.errors, testing::ElementsAre());
+
+  CheckMetrics(MetricsExpectations(AuctionResult::kSuccess)
+                   .SetNumBidderWorklets(2)
+                   .SetNumConfigPromises(1)
+                   .SetNumOwnersAndDistinctOwners(2)
+                   .SetNumInterestGroups(2)
+                   .SetNumInterestGroupsWithOnlyNonKAnonBid(2)
+                   .SetNumSellers(1));
 }
 
 // Checks case where auction signals promises resolves before the bidder worklet
