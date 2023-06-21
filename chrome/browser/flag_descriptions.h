3e600b152ed8c95e1d334873b5505821e1de4d9c
Crisrael Lucero <crisrael@google.com>
2023-06-21 14:50:57 +0000

    [Phone Hub] Cleanup expired error handling flags
    Feature is already enabled by default. This change does not remove the
    flag in ash_features in order to keep that as a kill switch, this change
    only removes the aspects of the flag that would expose it to the user.
    
    Test: manually verified sub-feature onboarding still works.
    Fixed: 1419224
    Change-Id: Iab7706b6652e81dd8ef05f454d6a5471638bd956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628654
    Reviewed-by: Pu Shi <pushi@google.com>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160632}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3662,9 +3662,6 @@ extern const char kPhoneHubCallNotificationDescription[];
 extern const char kPhoneHubCameraRollName[];
 extern const char kPhoneHubCameraRollDescription[];
 
-extern const char kPhoneHubFeatureSetupErrorHandlingName[];
-extern const char kPhoneHubFeatureSetupErrorHandlingDescription[];
-
 extern const char kPhoneHubNudgeName[];
 extern const char kPhoneHubNudgeDescription[];
 
