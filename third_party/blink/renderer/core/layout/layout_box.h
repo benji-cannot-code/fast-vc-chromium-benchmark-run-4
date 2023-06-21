f24db0cab11561dda48948cf020d215bf31decd1
Kent Tamura <tkent@chromium.org>
2023-06-21 05:36:12 +0000

    [RemoveLegacy] Remove LayoutBox::PositionForPoint()
    It's not used.
    This CL should have no behavior changes.
    
    Bug: 1229581
    Change-Id: I81a1bb16e33165005ca46d6de656e6288480c9b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626397
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160482}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -1274,7 +1274,6 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   void ImageChanged(WrappedImagePtr, CanDeferInvalidation) override;
   ResourcePriority ComputeResourcePriority() const final;
 
-  PositionWithAffinity PositionForPoint(const PhysicalOffset&) const override;
   PositionWithAffinity PositionForPointInFragments(const PhysicalOffset&) const;
 
   virtual bool CreatesNewFormattingContext() const {
