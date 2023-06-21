3463f826cd2f7b2623c51fa43f636dea0ccc145c
Gabriel Oliveira <gabolvr@google.com>
2023-06-21 16:52:03 +0000

    [CR2023][Profiles] Update sync info container style in Profile Menu
    This CL updates the background color, border and padding for the sync
    info container in the Profile Menu for the Chrome Refresh 2023 design.
    
    Preview: https://crbug.com/1422119#c27
    
    Bug: 1422119
    Change-Id: Ied7ee9473f2da3f7c267db4a8a1c2693f7e2b65c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626645
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160707}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -318,8 +318,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorProfileMenuIconButton) \
   E_CPONLY(kColorProfileMenuIconButtonBackground) \
   E_CPONLY(kColorProfileMenuIconButtonBackgroundHovered) \
-  E_CPONLY(kColorProfileMenuSyncIcon) \
   E_CPONLY(kColorProfileMenuSyncErrorIcon) \
+  E_CPONLY(kColorProfileMenuSyncIcon) \
+  E_CPONLY(kColorProfileMenuSyncInfoBackground) \
   E_CPONLY(kColorProfileMenuSyncOffIcon) \
   E_CPONLY(kColorProfileMenuSyncPausedIcon) \
   /* Profiles colors. */ \
