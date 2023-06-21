7cf0ec6920537a331bbaf1402bbeab3ba54c766c
Sina Firoozabadi <sinafirooz@google.com>
2023-06-21 20:15:48 +0000

    Add WPT for closed AudioContext
    Calling either of close, resume, or suspend on an already closed AudioContext
    should reject the promise and throw an InvalidStateError.
    
    spec: www.w3.org/TR/webaudio/#dom-baseaudiocontext-control-thread-state-slot
    
    Change-Id: I89fee4bddaece86d3a3a740fcf761f2b6fb63d14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617045
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160820}

---
+++ third_party/blink/web_tests/external/wpt/webaudio/the-audio-api/the-audiocontext-interface/audiocontext-state-change-after-close.http.window.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+
+'use strict';
+
+promise_test(async t => {
+  let audioContext = new AudioContext();
+  await new Promise((resolve) => (audioContext.onstatechange = resolve));
+  await audioContext.close();
+  return promise_rejects_dom(
+      t, 'InvalidStateError', audioContext.close(),
+      'A closed AudioContext should reject calls to close');
+}, 'Call close on a closed AudioContext');
+
+promise_test(async t => {
+  let audioContext = new AudioContext();
+  await new Promise((resolve) => (audioContext.onstatechange = resolve));
+  await audioContext.close();
+  return promise_rejects_dom(
+      t, 'InvalidStateError', audioContext.resume(),
+      'A closed AudioContext should reject calls to resume');
+}, 'Call resume on a closed AudioContext');
+
+promise_test(async t => {
+  let audioContext = new AudioContext();
+  await new Promise((resolve) => (audioContext.onstatechange = resolve));
+  await audioContext.close();
+  return promise_rejects_dom(
+      t, 'InvalidStateError', audioContext.suspend(),
+      'A closed AudioContext should reject calls to suspend');
+}, 'Call suspend on a closed AudioContext');
