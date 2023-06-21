08b035fb390706b4c419cbb45c9ac921b8cdf46b
Peng Huang <penghuang@chromium.org>
2023-06-21 02:59:10 +0000

    DawnContextProvider: create dawn instance with cache interface
    So compiled shaders from graphite dawn will be stored into disk to
    avoid re-compiling shaders between chrome runs.
    
    Bug: 1454867
    Change-Id: If6a2a7a552b13c29a2e4ea2c9411b98898cf0252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615640
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160452}

--- gpu/ipc/service/gpu_channel_manager.h
+++ gpu/ipc/service/gpu_channel_manager.h
@@ -46,11 +46,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_surface.h"
 #include "url/gurl.h"
 
-namespace base {
-namespace trace_event {
+namespace base::trace_event {
 class TracedValue;
-}  // namespace trace_event
-}  // namespace base
+}  // namespace base::trace_event
 
 namespace gl {
 class GLShareGroup;
@@ -59,15 +57,16 @@ class GLShareGroup;
 namespace gpu {
 
 class BuiltInShaderCacheWriter;
-class SharedImageManager;
+class DawnContextProvider;
+class ImageDecodeAcceleratorWorker;
 struct GpuPreferences;
 class GpuChannel;
 class GpuChannelManagerDelegate;
 class GpuMemoryBufferFactory;
 class GpuWatchdogThread;
-class ImageDecodeAcceleratorWorker;
 class MailboxManager;
 class Scheduler;
+class SharedImageManager;
 class SyncPointManager;
 struct VideoMemoryUsageStats;
 
@@ -108,7 +107,9 @@ class GPU_IPC_SERVICE_EXPORT GpuChannelManager
       ImageDecodeAcceleratorWorker* image_decode_accelerator_worker,
       viz::VulkanContextProvider* vulkan_context_provider = nullptr,
       viz::MetalContextProvider* metal_context_provider = nullptr,
-      DawnContextProvider* dawn_context_provider = nullptr);
+      DawnContextProvider* dawn_context_provider = nullptr,
+      webgpu::DawnCachingInterfaceFactory* dawn_caching_interface_factory =
+          nullptr);
 
   GpuChannelManager(const GpuChannelManager&) = delete;
   GpuChannelManager& operator=(const GpuChannelManager&) = delete;
@@ -227,15 +228,9 @@ class GPU_IPC_SERVICE_EXPORT GpuChannelManager
     return gr_shader_cache_ ? &*gr_shader_cache_ : nullptr;
   }
 
-#if BUILDFLAG(USE_DAWN)
   webgpu::DawnCachingInterfaceFactory* dawn_caching_interface_factory() {
     return dawn_caching_interface_factory_.get();
   }
-#else
-  webgpu::DawnCachingInterfaceFactory* dawn_caching_interface_factory() {
-    return nullptr;
-  }
-#endif
 
   // raster::GrShaderCache::Client implementation.
   void StoreShader(const std::string& key, const std::string& shader) override;
@@ -387,10 +382,7 @@ class GPU_IPC_SERVICE_EXPORT GpuChannelManager
   absl::optional<raster::GrShaderCache> gr_shader_cache_;
   scoped_refptr<SharedContextState> shared_context_state_;
 
-#if BUILDFLAG(USE_DAWN)
-  std::unique_ptr<webgpu::DawnCachingInterfaceFactory>
-      dawn_caching_interface_factory_;
-#endif
+  raw_ptr<webgpu::DawnCachingInterfaceFactory> dawn_caching_interface_factory_;
 
   // With --enable-vulkan, |vulkan_context_provider_| will be set from
   // viz::GpuServiceImpl. The raster decoders will use it for rasterization if
