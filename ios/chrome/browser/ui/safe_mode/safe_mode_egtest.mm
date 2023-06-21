ff9257a2b7009acc0c4cc73fdb3d10b94ebf7d9b
Rohit Rao <rohitrao@chromium.org>
2023-06-21 15:31:11 +0000

    [ios] Fixes SafeModeTestCase.
    https://chromium-review.googlesource.com/c/chromium/src/+/4622316
    moved Localizable.strings from the main bundle to the xctest
    module. As a result, the tests were unable to find the localized
    strings. This CL fixes the tests to look for the strings inside of
    FrameworkBundle() instead.
    
    Bug: None
    Change-Id: Iea1d55539378fe2837074043e513108941f7689c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632837
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160661}

--- ios/chrome/browser/ui/safe_mode/safe_mode_egtest.mm
+++ ios/chrome/browser/ui/safe_mode/safe_mode_egtest.mm
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "base/apple/bundle_locations.h"
 #import "base/feature_list.h"
 #import "base/ios/ios_util.h"
 #import "base/mac/foundation_util.h"
@@ -28,6 +29,14 @@ using chrome_test_util::ButtonWithAccessibilityLabel;
 
 namespace {
 
+// Returns a localized string by looking the given key up in
+// Localizable.strings.
+NSString* LocalizedString(NSString* key) {
+  return [base::apple::FrameworkBundle() localizedStringForKey:key
+                                                         value:@""
+                                                         table:nil];
+}
+
 // Verifies that `message` is displayed.
 void AssertMessageOnPage(NSString* message) {
   id<GREYMatcher> messageMatcher =
@@ -48,7 +57,7 @@ void AssertMessageNotOnPage(NSString* message) {
 // Verifies that the button to reload chrome is displayed.
 void AssertTryAgainButtonOnPage() {
   id<GREYMatcher> tryAgainMatcher = ButtonWithAccessibilityLabel(
-      NSLocalizedString(@"IDS_IOS_SAFE_MODE_RELOAD_CHROME", @""));
+      LocalizedString(@"IDS_IOS_SAFE_MODE_RELOAD_CHROME"));
   [[EarlGrey selectElementWithMatcher:tryAgainMatcher]
       assertWithMatcher:grey_notNil()];
 }
@@ -75,12 +84,11 @@ void AssertTryAgainButtonOnPage() {
   [SafeModeAppInterface presentSafeMode];
 
   // Verifies screen content that shows that crash report is being uploaded.
-  AssertMessageOnPage(NSLocalizedString(@"IDS_IOS_SAFE_MODE_AW_SNAP", @""));
-  AssertMessageOnPage(
-      NSLocalizedString(@"IDS_IOS_SAFE_MODE_UNKNOWN_CAUSE", @""));
+  AssertMessageOnPage(LocalizedString(@"IDS_IOS_SAFE_MODE_AW_SNAP"));
+  AssertMessageOnPage(LocalizedString(@"IDS_IOS_SAFE_MODE_UNKNOWN_CAUSE"));
   AssertTryAgainButtonOnPage();
   AssertMessageOnPage(
-      NSLocalizedString(@"IDS_IOS_SAFE_MODE_SENDING_CRASH_REPORT", @""));
+      LocalizedString(@"IDS_IOS_SAFE_MODE_SENDING_CRASH_REPORT"));
 }
 
 // Tests that Safe Mode screen is displayed with a message that there are
@@ -100,12 +108,11 @@ void AssertTryAgainButtonOnPage() {
   [SafeModeAppInterface presentSafeMode];
   // Verifies screen content that does not show crash report being uploaded.
   // When devices are jailbroken, the crash reports are not very useful.
-  AssertMessageOnPage(NSLocalizedString(@"IDS_IOS_SAFE_MODE_AW_SNAP", @""));
-  AssertMessageOnPage(
-      NSLocalizedString(@"IDS_IOS_SAFE_MODE_TWEAKS_FOUND", @""));
+  AssertMessageOnPage(LocalizedString(@"IDS_IOS_SAFE_MODE_AW_SNAP"));
+  AssertMessageOnPage(LocalizedString(@"IDS_IOS_SAFE_MODE_TWEAKS_FOUND"));
   AssertTryAgainButtonOnPage();
   AssertMessageNotOnPage(
-      NSLocalizedString(@"IDS_IOS_SAFE_MODE_SENDING_CRASH_REPORT", @""));
+      LocalizedString(@"IDS_IOS_SAFE_MODE_SENDING_CRASH_REPORT"));
 }
 
 // Tests that Safe Mode screen is displayed with a message that there are
@@ -131,15 +138,14 @@ void AssertTryAgainButtonOnPage() {
   [SafeModeAppInterface presentSafeMode];
   // Verifies screen content that does not show crash report being uploaded.
   // When devices are jailbroken, the crash reports are not very useful.
-  AssertMessageOnPage(NSLocalizedString(@"IDS_IOS_SAFE_MODE_AW_SNAP", @""));
+  AssertMessageOnPage(LocalizedString(@"IDS_IOS_SAFE_MODE_AW_SNAP"));
   // Constructs the list of bad mods based on `badModulesList` above.
-  NSString* message =
-      [NSLocalizedString(@"IDS_IOS_SAFE_MODE_NAMED_TWEAKS_FOUND", @"")
-          stringByAppendingString:@"\n\n    iAmBad\n    MJackson"];
+  NSString* message = [LocalizedString(@"IDS_IOS_SAFE_MODE_NAMED_TWEAKS_FOUND")
+      stringByAppendingString:@"\n\n    iAmBad\n    MJackson"];
   AssertMessageOnPage(message);
   AssertTryAgainButtonOnPage();
   AssertMessageNotOnPage(
-      NSLocalizedString(@"IDS_IOS_SAFE_MODE_SENDING_CRASH_REPORT", @""));
+      LocalizedString(@"IDS_IOS_SAFE_MODE_SENDING_CRASH_REPORT"));
 }
 
 // Tests that a start NTP is shown after 2 crashes.
