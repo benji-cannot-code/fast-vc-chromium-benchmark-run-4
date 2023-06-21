c9188770c5f57dc7d730a5d13bb2610b12ca6907
Irina Fedorova <irfedorova@google.com>
2023-06-21 16:41:21 +0000

    Build app_session_unittest for Lacros as well
    AppSession is build for both Ash and Lacros, so unit tests for this
    class should also test both Ash and Lacros.
    
    Bug: b/271082600
    Change-Id: I9db1c5d3fdd36381e664b61b6bdab862819d99d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628572
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160700}

--- chrome/browser/chromeos/app_mode/app_session_policies_unittest.cc
+++ chrome/browser/chromeos/app_mode/app_session_policies_unittest.cc
@@ -13,14 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-class AppSessionPolociesTest : public testing::Test {
+class AppSessionPoliciesTest : public testing::Test {
  public:
-  AppSessionPolociesTest() {
+  AppSessionPoliciesTest() {
     app_session_policies_ = std::make_unique<AppSessionPolicies>(GetPrefs());
   }
 
-  AppSessionPolociesTest(const AppSessionPolociesTest&) = delete;
-  AppSessionPolociesTest& operator=(const AppSessionPolociesTest&) = delete;
+  AppSessionPoliciesTest(const AppSessionPoliciesTest&) = delete;
+  AppSessionPoliciesTest& operator=(const AppSessionPoliciesTest&) = delete;
 
   void TearDown() override {
     // Clean up all preferenses that we use in tests.
@@ -40,16 +40,16 @@ class AppSessionPolociesTest : public testing::Test {
   std::unique_ptr<AppSessionPolicies> app_session_policies_;
 };
 
-TEST_F(AppSessionPolociesTest, kNewWindowsInKioskAllowedNotSet) {
+TEST_F(AppSessionPoliciesTest, kNewWindowsInKioskAllowedNotSet) {
   EXPECT_FALSE(IsWindowCreationAllowed());
 }
 
-TEST_F(AppSessionPolociesTest, kNewWindowsInKioskAllowedSetFalse) {
+TEST_F(AppSessionPoliciesTest, kNewWindowsInKioskAllowedSetFalse) {
   GetPrefs()->SetBoolean(prefs::kNewWindowsInKioskAllowed, false);
   EXPECT_FALSE(IsWindowCreationAllowed());
 }
 
-TEST_F(AppSessionPolociesTest, kNewWindowsInKioskAllowedSetTrue) {
+TEST_F(AppSessionPoliciesTest, kNewWindowsInKioskAllowedSetTrue) {
   GetPrefs()->SetBoolean(prefs::kNewWindowsInKioskAllowed, true);
   EXPECT_TRUE(IsWindowCreationAllowed());
 }
