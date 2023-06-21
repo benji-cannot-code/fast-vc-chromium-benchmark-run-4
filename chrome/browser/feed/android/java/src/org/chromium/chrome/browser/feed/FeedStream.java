6e2d218e93d87d2598fc9d13258981716ab83e68
Calder Kitagawa <ckitagawa@chromium.org>
2023-06-21 14:43:22 +0000

    Revert "[Fixit] Fix memory leak in FeedStream"
    This reverts commit 143a46965cd0bdc5118a519ca31f63980f535825.
    
    Reason for revert: Suspect to cause a crash: https://crbug.com/1456626
    
    Original change's description:
    > [Fixit] Fix memory leak in FeedStream
    >
    > As part of deflaking:
    > TabSwitcherAndStartSurfaceLayoutTest#testActivityCanBeGarbageCollectedAfterFinished
    >
    > I've identified one of the causes of the leaked activity is that
    > FeedStream's native counterpart holds a reference to it and is
    > never deleted. This CL fixes that issue. Although others
    > remain.
    >
    > Bug: 1333095
    > Change-Id: I5bc20a47572faf26d87f264e5dffe1609bf03c3f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622327
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1160065}
    
    Bug: 1333095, 1456626
    Change-Id: I2f036c0542e18c3f4b4332d837ca5f9adfafab0f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632657
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160628}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/FeedStream.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/FeedStream.java
@@ -589,7 +589,7 @@ public class FeedStream implements Stream {
     private final int mLoadMoreTriggerScrollDistanceDp;
 
     private final Activity mActivity;
-    private long mNativeFeedStream;
+    private final long mNativeFeedStream;
     private final ObserverList<ContentChangedListener> mContentChangedListeners =
             new ObserverList<>();
     private final int mStreamKind;
@@ -748,10 +748,6 @@ public class FeedStream implements Stream {
             mUnreadContentObserver.destroy();
         }
         mReliabilityLoggingBridge.destroy();
-        if (mNativeFeedStream != 0) {
-            FeedStreamJni.get().destroy(mNativeFeedStream);
-            mNativeFeedStream = 0;
-        }
     }
 
     @Override
@@ -1430,7 +1426,6 @@ public class FeedStream implements Stream {
                 long nativeFeedReliabilityLoggingBridge);
         long initWebFeed(FeedStream caller, byte[] webFeedId,
                 long nativeFeedReliabilityLoggingBridge, int entryPoint);
-        void destroy(long feedStreamPtr);
         void reportFeedViewed(long nativeFeedStream, FeedStream caller);
         void reportSliceViewed(long nativeFeedStream, FeedStream caller, String sliceId);
         void reportPageLoaded(long nativeFeedStream, FeedStream caller, boolean inNewTab);
