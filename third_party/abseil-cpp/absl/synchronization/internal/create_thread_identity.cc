53357acd781d99bd374be8246ad82893dc9c43ec
Danil Chapovalov <danilchap@chromium.org>
2023-06-21 18:35:30 +0000

    Roll abseil_revision 049aa40e7e..166d71d18f
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/049aa40e7e..166d71d18f
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/049aa40e7e..166d71d18f
    
    No .def changes
    
    Bug: None
    Change-Id: Ic5c8c93ce285933bd4c2a0fac40e2e331ecc3565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629756
    Auto-Submit: Danil Chapovalov <danilchap@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160761}

--- third_party/abseil-cpp/absl/synchronization/internal/create_thread_identity.cc
+++ third_party/abseil-cpp/absl/synchronization/internal/create_thread_identity.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // limitations under the License.
 
 #include <stdint.h>
+
 #include <new>
 
 // This file is a no-op if the required LowLevelAlloc support is missing.
