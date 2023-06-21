ae8279f86138a4b46fe8f02cad285688cc767e70
Yuki Awano <yawano@google.com>
2023-06-21 18:36:37 +0000

    Check managed status of pref early
    - Default value of Quick Answers enabled state is false. Currently the
      function returns early if the pref value is the same. With that, we
      cannot observe unmanaged-disabled to managed-disable state transition.
      Move the check of the managed status of the pref early to handle the
      transition.
    
    Bug: b:282233709
    Test: unit_tests --gtest_filter=*QuickAnswersStateAsh*
    Change-Id: I70b781e83beea849564b6c21de2d1fdead8fa0ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4630791
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160763}

--- chrome/browser/ui/quick_answers/quick_answers_state_ash_unittest.cc
+++ chrome/browser/ui/quick_answers/quick_answers_state_ash_unittest.cc
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/quick_answers/test/chrome_quick_answers_test_base.h"
+#include "chromeos/components/quick_answers/public/cpp/quick_answers_prefs.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/language/core/browser/pref_names.h"
+#include "components/prefs/testing_pref_service.h"
 #include "third_party/icu/source/common/unicode/locid.h"
 
 namespace {
@@ -80,20 +82,20 @@ class QuickAnswersStateAshTest : public ChromeQuickAnswersTestBase {
   void SetUp() override {
     ChromeQuickAnswersTestBase::SetUp();
 
-    prefs_ =
-        ash::Shell::Get()->session_controller()->GetPrimaryUserPrefService();
+    prefs_ = static_cast<TestingPrefServiceSimple*>(
+        ash::Shell::Get()->session_controller()->GetPrimaryUserPrefService());
     DCHECK(prefs_);
     DCHECK(QuickAnswersState::Get()->prefs_initialized());
 
     observer_ = std::make_unique<TestQuickAnswersStateObserver>();
   }
 
-  PrefService* prefs() { return prefs_; }
+  TestingPrefServiceSimple* prefs() { return prefs_; }
 
   TestQuickAnswersStateObserver* observer() { return observer_.get(); }
 
  private:
-  raw_ptr<PrefService, ExperimentalAsh> prefs_ = nullptr;
+  raw_ptr<TestingPrefServiceSimple, ExperimentalAsh> prefs_ = nullptr;
   std::unique_ptr<TestQuickAnswersStateObserver> observer_;
 };
 
@@ -273,4 +275,37 @@ TEST_F(QuickAnswersStateAshTest, IneligibleLocales) {
   SimulateUserLogin(kTestUser);
   EXPECT_FALSE(QuickAnswersState::Get()->is_eligible());
   EXPECT_FALSE(observer()->is_eligible());
+}
+
+TEST_F(QuickAnswersStateAshTest, DisabledByPolicy) {
+  QuickAnswersState::Get()->AddObserver(observer());
+
+  ASSERT_FALSE(
+      prefs()->IsManagedPreference(quick_answers::prefs::kQuickAnswersEnabled));
+  ASSERT_FALSE(prefs()->GetBoolean(quick_answers::prefs::kQuickAnswersEnabled));
+  ASSERT_EQ(quick_answers::prefs::ConsentStatus::kUnknown,
+            observer()->consent_status());
+
+  prefs()->SetManagedPref(quick_answers::prefs::kQuickAnswersEnabled,
+                          base::Value(false));
+  EXPECT_EQ(quick_answers::prefs::ConsentStatus::kRejected,
+            observer()->consent_status());
+  EXPECT_FALSE(observer()->settings_enabled());
+}
+
+TEST_F(QuickAnswersStateAshTest, EnabledThenDisabledByPolicy) {
+  QuickAnswersState::Get()->AddObserver(observer());
+
+  prefs()->SetBoolean(quick_answers::prefs::kQuickAnswersEnabled, true);
+  ASSERT_FALSE(
+      prefs()->IsManagedPreference(quick_answers::prefs::kQuickAnswersEnabled));
+  ASSERT_TRUE(prefs()->GetBoolean(quick_answers::prefs::kQuickAnswersEnabled));
+  ASSERT_EQ(quick_answers::prefs::ConsentStatus::kAccepted,
+            observer()->consent_status());
+
+  prefs()->SetManagedPref(quick_answers::prefs::kQuickAnswersEnabled,
+                          base::Value(false));
+  EXPECT_EQ(quick_answers::prefs::ConsentStatus::kRejected,
+            observer()->consent_status());
+  EXPECT_FALSE(observer()->settings_enabled());
 }
