45ea4b42b95268bd678d1196ebc8dbe8239a4bc0
Wes Okuhara <wesokuhara@google.com>
2023-06-21 17:08:48 +0000

    CrOS Settings: Imperatively register main page event listeners
    Imperatively register event listeners for events fired from
    MainPageMixin. This allows better typechecking and maintenance while
    reducing HTML bloat.
    
    Bug: b/285968876
    Test: browser_tests --gtest_filter="OSSettingsMainPageContainer*"
    Test: browser_tests --gtest_filter="OSSettingsOsSettingsUi*"
    Change-Id: I2ad59bbb8448833bf5ac48a46fb95dc525f8694d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618025
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160718}

--- chrome/browser/resources/settings/chromeos/os_settings_main/os_settings_main.ts
+++ chrome/browser/resources/settings/chromeos/os_settings_main/os_settings_main.ts
@@ -33,6 +33,14 @@ interface MainPageVisibility {
   settings: boolean;
 }
 
+declare global {
+  interface HTMLElementEventMap {
+    'showing-main-page': CustomEvent;
+    'showing-subpage': CustomEvent;
+    'showing-section': CustomEvent<HTMLElement>;
+  }
+}
+
 export interface OsSettingsMainElement {
   $: {
     overscroll: HTMLDivElement,
@@ -81,7 +89,7 @@ export class OsSettingsMainElement extends OsSettingsMainElementBase {
         },
       },
 
-      showingSubpage_: Boolean,
+      isShowingSubpage_: Boolean,
 
       toolbarSpinnerActive: {
         type: Boolean,
@@ -102,13 +110,21 @@ export class OsSettingsMainElement extends OsSettingsMainElementBase {
   pageAvailability: OsPageAvailability;
   private overscroll_: number;
   private showPages_: MainPageVisibility;
-  private showingSubpage_: boolean;
+  private isShowingSubpage_: boolean;
   private boundScroll_: (() => void)|null;
 
   constructor() {
     super();
 
     this.boundScroll_ = null;
+  }
+
+  override ready() {
+    super.ready();
+
+    this.addEventListener('showing-main-page', this.onShowingMainPage);
+    this.addEventListener('showing-subpage', this.onShowingSubpage);
+    this.addEventListener('showing-section', this.onShowingSection);
   }
 
   private overscrollChanged_() {
@@ -120,7 +136,7 @@ export class OsSettingsMainElement extends OsSettingsMainElementBase {
       this.boundScroll_ = null;
     } else if (this.overscroll_ && !this.boundScroll_) {
       this.boundScroll_ = () => {
-        if (!this.showingSubpage_) {
+        if (!this.isShowingSubpage_) {
           this.setOverscroll_(0);
         }
       };
@@ -165,12 +181,12 @@ export class OsSettingsMainElement extends OsSettingsMainElementBase {
     }
   }
 
-  private onShowingSubpage_() {
-    this.showingSubpage_ = true;
+  private onShowingMainPage(): void {
+    this.isShowingSubpage_ = false;
   }
 
-  private onShowingMainPage_() {
-    this.showingSubpage_ = false;
+  private onShowingSubpage(): void {
+    this.isShowingSubpage_ = true;
   }
 
   /**
@@ -178,7 +194,7 @@ export class OsSettingsMainElement extends OsSettingsMainElementBase {
    * main-page-container, indicating that a section should be
    * scrolled into view as a result of a navigation.
    */
-  private onShowingSection_(e: CustomEvent<HTMLElement>) {
+  private onShowingSection(e: CustomEvent<HTMLElement>): void {
     const section = e.detail;
     // Calculate the height that the overscroll padding should be set to, so
     // that the given section is displayed at the top of the viewport.
@@ -194,7 +210,7 @@ export class OsSettingsMainElement extends OsSettingsMainElementBase {
   }
 
   private showManagedHeader_(): boolean {
-    return !this.showingSubpage_ && !this.showPages_.about;
+    return !this.isShowingSubpage_ && !this.showPages_.about;
   }
 }
 
