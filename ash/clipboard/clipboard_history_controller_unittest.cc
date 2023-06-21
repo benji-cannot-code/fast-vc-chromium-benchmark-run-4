0e34bc9e6d38ccc03fa5374ebe73893209d49999
Jeroen Dhollander <jeroendh@google.com>
2023-06-21 14:19:56 +0000

    [base][test] Deprecate RepeatingTestFuture (from ash/clipboard)
    `TestFuture` can now handle these cases itself (through the newly
    introduced `GetRepeatingCallback()` functionality).
    
    Bug: b/287939427
    Change-Id: Ib37eaf2fd4d83c6b2fdfd9bcc4490217480447a0
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4624594
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160614}

--- ash/clipboard/clipboard_history_controller_unittest.cc
+++ ash/clipboard/clipboard_history_controller_unittest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/repeating_test_future.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "base/unguessable_token.h"
@@ -188,7 +187,7 @@ class ClipboardHistoryControllerTest : public AshTestBase {
     AshTestBase::SetUp();
     mock_image_factory_ = std::make_unique<MockClipboardImageModelFactory>();
     GetClipboardHistoryController()->set_confirmed_operation_callback_for_test(
-        operation_confirmed_future_.GetCallback());
+        operation_confirmed_future_.GetRepeatingCallback());
   }
 
   ClipboardHistoryControllerImpl* GetClipboardHistoryController() {
@@ -247,7 +246,7 @@ class ClipboardHistoryControllerTest : public AshTestBase {
   }
 
  protected:
-  base::test::RepeatingTestFuture<bool> operation_confirmed_future_;
+  base::test::TestFuture<bool> operation_confirmed_future_;
 
  private:
   std::unique_ptr<MockClipboardImageModelFactory> mock_image_factory_;
@@ -365,7 +364,7 @@ TEST_F(ClipboardHistoryControllerTest, VerifyAvailabilityInUserModes) {
       // disabled actually allows writes to go through, because the operation
       // might not have finished yet in that case. The history verification
       // below mitigates the chance that such a bug would not be caught.
-      EXPECT_TRUE(operation_confirmed_future_.IsEmpty());
+      EXPECT_FALSE(operation_confirmed_future_.IsReady());
     }
 
     const std::list<ClipboardHistoryItem>& items =
@@ -826,7 +825,7 @@ TEST_P(ClipboardHistoryControllerShowSourceTest, ShowMenuReturnsSuccess) {
 // Tests that the client-provided `OnMenuClosingCallback` runs before the menu
 // closes.
 TEST_P(ClipboardHistoryControllerShowSourceTest, OnMenuClosingCallback) {
-  base::test::RepeatingTestFuture<bool> on_menu_closing_future;
+  base::test::TestFuture<bool> on_menu_closing_future;
   base::HistogramTester histogram_tester;
 
   // Copy something to enable the clipboard history menu.
@@ -838,9 +837,9 @@ TEST_P(ClipboardHistoryControllerShowSourceTest, OnMenuClosingCallback) {
   // Show the menu with an `OnMenuClosingCallback`.
   GetClipboardHistoryController()->ShowMenu(
       test_window_rect, ui::MenuSourceType::MENU_SOURCE_NONE, GetSource(),
-      on_menu_closing_future.GetCallback());
+      on_menu_closing_future.GetRepeatingCallback());
   EXPECT_TRUE(GetClipboardHistoryController()->IsMenuShowing());
-  EXPECT_TRUE(on_menu_closing_future.IsEmpty());
+  EXPECT_FALSE(on_menu_closing_future.IsReady());
 
   // Hide the menu. The callback should indicate that nothing will be pasted.
   PressAndReleaseKey(ui::VKEY_ESCAPE);
@@ -856,7 +855,7 @@ TEST_P(ClipboardHistoryControllerShowSourceTest, OnMenuClosingCallback) {
       test_window_rect, ui::MenuSourceType::MENU_SOURCE_NONE, GetSource(),
       on_menu_closing_future.GetCallback());
   EXPECT_TRUE(GetClipboardHistoryController()->IsMenuShowing());
-  EXPECT_TRUE(on_menu_closing_future.IsEmpty());
+  EXPECT_FALSE(on_menu_closing_future.IsReady());
 
   // Toggle the menu closed. The callback should indicate a pending paste.
   PressAndReleaseKey(ui::VKEY_V, ui::EF_COMMAND_DOWN);
--- ash/clipboard/clipboard_history_resource_manager_unittest.cc
+++ ash/clipboard/clipboard_history_resource_manager_unittest.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/task/sequenced_task_runner.h"
-#include "base/test/repeating_test_future.h"
+#include "base/test/test_future.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -322,11 +322,11 @@ TEST_F(ClipboardHistoryResourceManagerTest, PlaceholderDuringRender) {
   EXPECT_CALL(*mock_image_factory(), CancelRequest).Times(0);
   EXPECT_CALL(*mock_image_factory(), Render).Times(1);
 
-  base::test::RepeatingTestFuture<bool> operation_confirmed_future_;
+  base::test::TestFuture<bool> operation_confirmed_future_;
   Shell::Get()
       ->clipboard_history_controller()
       ->set_confirmed_operation_callback_for_test(
-          operation_confirmed_future_.GetCallback());
+          operation_confirmed_future_.GetRepeatingCallback());
 
   {
     ui::ScopedClipboardWriter scw(ui::ClipboardBuffer::kCopyPaste);
