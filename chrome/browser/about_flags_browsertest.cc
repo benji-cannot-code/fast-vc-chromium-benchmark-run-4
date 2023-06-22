a53976b8036778b28fdd6e5cd7d74a6a7c779b7d
Cole Horvitz <colehorvitz@chromium.org>
2023-06-22 18:54:28 +0000

    Flags UI: Continue migration to web components.
    This CL makes several improvements to chrome://flags:
     - Replaces references to flags-experiment's shadowDom from flags.ts
       with explicit API calls on FlagsExperimentElement.
     - Moves style modifications on flags-experiment from inside flags.ts
       to inside FlagsExperimentElement.
     - Refactors and simplifies FlagSearch, moving much of the filtering
       and highlighting logic into FlagsExperimentElement.
     - Refactors and simplifies the logic in the onchange listeners of
       selects and textareas. Moves much of the logic into
       FlagsExperimentElement.
     - Implement FlagsBrowserProxy to add a proxy layer between flags UI and
       API calls to the backend.
    
    Bug: 1376313
    Change-Id: Ib8141d5a9778b827741c36d28273acb463e2a85e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615353
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1161363}

--- chrome/browser/about_flags_browsertest.cc
+++ chrome/browser/about_flags_browsertest.cc
@@ -68,7 +68,7 @@ void SimulateTextType(content::WebContents* contents,
                     "var textarea = parent.getElementsByTagName('textarea')[0];"
                     "textarea.focus();"
                     "textarea.value = `%s`;"
-                    "textarea.onchange();",
+                    "textarea.dispatchEvent(new Event('change'));",
                     experiment_id, text)));
 }
 
@@ -83,7 +83,7 @@ void ToggleEnableDropdown(content::WebContents* contents,
                          "querySelector('.experiment-enable-disable');"
                          "s.focus();"
                          "s.selectedIndex = %d;"
-                         "s.onchange();",
+                         "s.dispatchEvent(new Event('change'));",
                          experiment_id, enable ? 1 : 0)));
 }
 
