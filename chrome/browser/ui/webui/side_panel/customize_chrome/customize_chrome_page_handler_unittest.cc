cd6b746d800026b00efe8d881e89c29e3112c8cc
Sean Kau <skau@chromium.org>
2023-06-22 00:40:09 +0000

    Migrate /chrome/browser/ui/webui/side_panel/customize_chrome to ColorProviderKey
    ColorProviderManager::Key and its associated types are being
    moved into their own class, ColorProviderKey. As a result,
    class names are rewritten and includes are replaced.
    
    This CL was uploaded by git cl split.
    
    R=tiborg@chromium.org
    
    Bug: b:286952053
    Change-Id: I3588b22026b71d2e6f935dcf37d668a07f72897f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629419
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160962}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler_unittest.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler_unittest.cc
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/color/color_provider.h"
-#include "ui/color/color_provider_manager.h"
+#include "ui/color/color_provider_key.h"
 #include "ui/color/dynamic_color/palette_factory.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/shell_dialogs/select_file_dialog_factory.h"
@@ -412,9 +412,9 @@ TEST_F(CustomizeChromePageHandlerTest, GetOverviewChromeColorsGM3) {
 
   ASSERT_EQ(kCustomizeChromeColors.size(), colors.size());
   for (size_t i = 0; i < kCustomizeChromeColors.size(); i++) {
-    auto palette = ui::GeneratePalette(
-        kCustomizeChromeColors[i].color,
-        ui::ColorProviderManager::SchemeVariant::kTonalSpot);
+    auto palette =
+        ui::GeneratePalette(kCustomizeChromeColors[i].color,
+                            ui::ColorProviderKey::SchemeVariant::kTonalSpot);
     EXPECT_EQ(l10n_util::GetStringUTF8(kCustomizeChromeColors[i].label_id),
               colors[i]->name);
     EXPECT_EQ(kCustomizeChromeColors[i].color, colors[i]->seed);
@@ -428,9 +428,9 @@ TEST_F(CustomizeChromePageHandlerTest, GetOverviewChromeColorsGM3) {
 
   ASSERT_EQ(kCustomizeChromeColors.size(), colors.size());
   for (size_t i = 0; i < kCustomizeChromeColors.size(); i++) {
-    auto palette = ui::GeneratePalette(
-        kCustomizeChromeColors[i].color,
-        ui::ColorProviderManager::SchemeVariant::kTonalSpot);
+    auto palette =
+        ui::GeneratePalette(kCustomizeChromeColors[i].color,
+                            ui::ColorProviderKey::SchemeVariant::kTonalSpot);
     EXPECT_EQ(l10n_util::GetStringUTF8(kCustomizeChromeColors[i].label_id),
               colors[i]->name);
     EXPECT_EQ(kCustomizeChromeColors[i].color, colors[i]->seed);
