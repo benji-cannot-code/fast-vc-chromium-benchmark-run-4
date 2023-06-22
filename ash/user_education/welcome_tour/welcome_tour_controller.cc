cc98637ce628f16bd73209902fd4cf3273daadd0
David Black <dmblack@google.com>
2023-06-22 20:34:37 +0000

    Launch Explore app on completion of Welcome Tour.
    On completion of the Welcome Tour, the Explore app should be launched.
    
    Bug: b:288331970
    Change-Id: I3a9e2deaec14362d21c61485e30a657eee6bc183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4636951
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161421}

--- ash/user_education/welcome_tour/welcome_tour_controller.cc
+++ ash/user_education/welcome_tour/welcome_tour_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/user_education/welcome_tour/welcome_tour_controller_observer.h"
 #include "ash/user_education/welcome_tour/welcome_tour_dialog.h"
 #include "ash/user_education/welcome_tour/welcome_tour_scrim.h"
+#include "ash/webui/system_apps/public/system_web_app_type.h"
 #include "base/check_op.h"
 #include "base/functional/bind.h"
 #include "components/user_education/common/help_bubble.h"
@@ -24,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/element_tracker.h"
 #include "ui/base/interaction/interaction_sequence.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/views/interaction/element_tracker_views.h"
 #include "ui/views/view.h"
@@ -219,6 +222,9 @@ WelcomeTourController::GetTutorialDescriptions() {
               HelpBubbleId::kWelcomeTourExploreApp))
           .InSameContext());
 
+  // Step 7: Explore app window.
+  // Implemented in `WelcomeTourController::OnWelcomeTourEnded()`.
+
   return tutorial_descriptions_by_id;
 }
 
@@ -252,10 +258,10 @@ void WelcomeTourController::MaybeShowDialog() {
                                          weak_ptr_factory_.GetWeakPtr()),
       /*cancel_callback=*/
       base::BindOnce(&WelcomeTourController::OnWelcomeTourEnded,
-                     weak_ptr_factory_.GetWeakPtr()),
+                     weak_ptr_factory_.GetWeakPtr(), /*completed=*/false),
       /*close_callback=*/
       base::BindOnce(&WelcomeTourController::OnWelcomeTourEnded,
-                     weak_ptr_factory_.GetWeakPtr()));
+                     weak_ptr_factory_.GetWeakPtr(), /*completed=*/false));
 
   // `WelcomeTourDialog` is part of the Welcome Tour. Therefore, when the dialog
   // shows, the tour has indeed been started.
@@ -270,10 +276,10 @@ void WelcomeTourController::StartTutorial() {
       GetInitialElementContext(),
       /*completed_callback=*/
       base::BindOnce(&WelcomeTourController::OnWelcomeTourEnded,
-                     weak_ptr_factory_.GetWeakPtr()),
+                     weak_ptr_factory_.GetWeakPtr(), /*completed=*/true),
       /*aborted_callback=*/
       base::BindOnce(&WelcomeTourController::OnWelcomeTourEnded,
-                     weak_ptr_factory_.GetWeakPtr()));
+                     weak_ptr_factory_.GetWeakPtr(), /*completed=*/false));
 }
 
 // TODO(http://b/277091006): Stabilize app launches.
@@ -299,7 +305,13 @@ void WelcomeTourController::OnWelcomeTourStarted() {
 // TODO(http://b/277091619): Restore wallpaper.
 // TODO(http://b/277091643): Restore notifications.
 // TODO(http://b/277091624): Restore nudges/toasts.
-void WelcomeTourController::OnWelcomeTourEnded() {
+void WelcomeTourController::OnWelcomeTourEnded(bool completed) {
+  if (completed) {
+    UserEducationController::Get()->LaunchSystemWebAppAsync(
+        UserEducationPrivateApiKey(), ash::SystemWebAppType::HELP,
+        display::Screen::GetScreen()->GetPrimaryDisplay().id());
+  }
+
   scrim_.reset();
 
   for (auto& observer : observer_list_) {
