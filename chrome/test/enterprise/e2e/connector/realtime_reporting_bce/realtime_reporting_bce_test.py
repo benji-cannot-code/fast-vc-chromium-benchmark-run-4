0ef955025da35faf2496afc8be412f006a6589be
Arthur Wang <wuwang@google.com>
2023-06-22 17:00:20 +0000

    Create a reporting connector test that checks all partner services
    Bug: b:280445041
    Change-Id: I39c24ea9b33af7c717e0e8cc685960d34d79310b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4621150
    Reviewed-by: Xiang Ji <jxiang@google.com>
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161275}

--- chrome/test/enterprise/e2e/connector/realtime_reporting_bce/realtime_reporting_bce_test.py
+++ chrome/test/enterprise/e2e/connector/realtime_reporting_bce/realtime_reporting_bce_test.py
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from datetime import datetime
 import logging
 import os
-from datetime import datetime
 
 from chrome_ent_test.infra.core import before_all
 from chrome_ent_test.infra.core import category
@@ -16,18 +16,11 @@ from .. import ChromeReportingConnectorTestCase
 from .. import VerifyContent
 from .reporting_server import RealTimeReportingServer
 
+
 @category("chrome_only")
 @environment(file="../connector_test.asset.textpb")
 class RealTimeBCEReportingPipelineTest(ChromeReportingConnectorTestCase):
   """Test the Realtime Reporting pipeline events"""
-
-  def getServiceAccountKey(self):
-    serviceAccountKey = self.GetFileFromGCSBucket(
-        'secrets/ServiceAccountKey.json')
-    localDir = os.path.dirname(os.path.abspath(__file__))
-    filePath = os.path.join(localDir, 'service_accountkey.json')
-    with open(filePath, 'w', encoding="utf-8") as f:
-      f.write(serviceAccountKey)
 
   @before_all
   def setup(self):
@@ -46,7 +39,7 @@ class RealTimeBCEReportingPipelineTest(ChromeReportingConnectorTestCase):
     logging.info('histogram: %s', histogram)
 
     # read service account private key from gs-bucket & write into local
-    self.getServiceAccountKey()
-    apiService = RealTimeReportingServer()
+    apiService = RealTimeReportingServer(
+        self.GetFileFromGCSBucket('secrets/ServiceAccountKey.json'))
     self.TryVerifyUntilTimeout(
         verifyClass=apiService, content=VerifyContent(deviceId, testStartTime))
--- chrome/test/enterprise/e2e/connector/realtime_reporting_bce/reporting_server.py
+++ chrome/test/enterprise/e2e/connector/realtime_reporting_bce/reporting_server.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
 import os
 
 from googleapiclient.discovery import build
@@ -14,6 +15,15 @@ class RealTimeReportingServer(Verifyable):
   SCOPES = ['https://www.googleapis.com/auth/admin.reports.audit.readonly']
   USER_EMAIL = 'admin@beyondcorp.bigr.name'
 
+  def __init__(self, credentials):
+    self.credentials = None
+    try:
+      serviceAccountInfo = json.loads(credentials)
+      self.credentials = service_account.Credentials.from_service_account_info(
+          serviceAccountInfo, scopes=self.SCOPES)
+    except json.JSONDecodeError:
+      print('Decoding RealTimeReportingServer credentials JSON has failed')
+
   def create_reports_service(self, user_email):
     """Build and returns an Admin SDK Reports service object authorized with
       the service accounts that act on behalf of the given user.
@@ -24,12 +34,7 @@ class RealTimeReportingServer(Verifyable):
       Returns:
         Admin SDK reports service object.
       """
-    localDir = os.path.dirname(os.path.abspath(__file__))
-    filePath = os.path.join(localDir, 'service_accountkey.json')
-    credentials = service_account.Credentials.from_service_account_file(
-        filePath, scopes=self.SCOPES)
-
-    delegatedCreds = credentials.with_subject(user_email)
+    delegatedCreds = self.credentials.with_subject(user_email)
 
     return build('admin', 'reports_v1', credentials=delegatedCreds)
 
