50cf92dc42660651b5ceb30c6610235edc6466b7
Taylor Bergquist <tbergquist@chromium.org>
2023-06-22 01:43:30 +0000

    Refactor paint logic in TabStyleViews to use TabSelectionState enum.
    - Replace TabActive with TabSelectionState. TabSelectionState is broader than TabActive, covering hover and selected states, so passing it around instead of TabActive allows us to simplify some of the maze of methods for painting tabs.
    - Simplified background painting. Now, instead of futzing with opacity and painting the background multiple times, we can just get the color for our current state and paint that color. This makes the GM3 changes we need very easy.
    - Split background painting into two separate approaches for with and without images. I did have to keep the old approach in alongside the new one, just to handle themes with image backgrounds. It is, however, still greatly simplified.
    - Consolidated state calculations into a new method, GetCurrentSelectionState, fixing some inconsistencies and correctly handling precedence of states over one another.
    - Simplified opacity calculations, especially for hover. Rounding that .95 magic constant in GetThrobValue to 1* just let the math collapse down to a lerp.
    
    *: Justification: code archaeology shows that .95 was just the result of faithfully carrying forward an arbitrarily picked value through a decade of refactors. Also it made an imperceptible visual difference - the exact value depends on the theme, but I believe it's at maximum a ~1% difference in absolute brightness.
    
    
    Bug: 1399942
    Change-Id: I3188b9885f291be46cfa85c7846f0b6631169307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629424
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Auto-Submit: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160983}

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -1075,7 +1075,7 @@ int Tab::GetWidthOfLargestSelectableRegion() const {
 }
 
 void Tab::UpdateForegroundColors() {
-  TabStyle::TabColors colors = tab_style_views()->CalculateColors();
+  TabStyle::TabColors colors = tab_style_views()->CalculateTargetColors();
   title_->SetEnabledColor(colors.foreground_color);
   close_button_->SetColors(colors);
   alert_indicator_button_->OnParentTabButtonColorChanged();
--- chrome/browser/ui/views/tabs/tab_group_style.cc
+++ chrome/browser/ui/views/tabs/tab_group_style.cc
@@ -116,10 +116,6 @@ float TabGroupStyle::GetSyncIconWidth() const {
   return kSyncIconWidth;
 }
 
-float TabGroupStyle::GetSelectedTabOpacity() const {
-  return TabStyle::Get()->GetSelectedTabOpacity();
-}
-
 int TabGroupStyle::GetChipCornerRadius() const {
   return TabStyle::Get()->GetBottomCornerRadius() -
          TabGroupUnderline::kStrokeThickness;
