4c98ea98481ec89fc710e6a5f7e6ce4b9d2e88ce
Mariam Ali <alimariam@google.com>
2023-06-22 17:25:48 +0000

    [Browsing Data Model] Merge quota storage types into one
    This change asserts that information about whether the storage is
    partitioned or not should be derived from the data key and not the
    storage type.
    
    Consolidating the Quota storage types helps move in that direction.
    
    Change-Id: Ia8109f47ad7452f68cc1aaf98f5810eb6a2b8924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628508
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161294}

--- components/browsing_data/content/browsing_data_model.cc
+++ components/browsing_data/content/browsing_data_model.cc
@@ -86,10 +86,7 @@ std::string GetDataOwner::GetOwningHost<blink::StorageKey>(
   // implementation of the model, but ultimately these storage types may not
   // coexist.
   switch (storage_type_) {
-    case BrowsingDataModel::StorageType::kPartitionedQuotaStorage:
-      return data_key.top_level_site().GetURL().host();
-
-    case BrowsingDataModel::StorageType::kUnpartitionedQuotaStorage:
+    case BrowsingDataModel::StorageType::kQuotaStorage:
     case BrowsingDataModel::StorageType::kSharedStorage:
     case BrowsingDataModel::StorageType::kLocalStorage:
       return data_key.origin().host();
@@ -219,7 +216,7 @@ void StorageRemoverHelper::Visitor::operator()<blink::StorageKey>(
             helper->GetCompleteCallback()));
   }
 
-  if (types.Has(BrowsingDataModel::StorageType::kUnpartitionedQuotaStorage)) {
+  if (types.Has(BrowsingDataModel::StorageType::kQuotaStorage)) {
     const blink::mojom::StorageType quota_types[] = {
         blink::mojom::StorageType::kTemporary,
         blink::mojom::StorageType::kSyncable};
@@ -339,10 +336,9 @@ void OnQuotaManagedDataLoaded(
     const std::list<BrowsingDataQuotaHelper::QuotaInfo>& quota_info) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   for (const auto& entry : quota_info) {
-    model->AddBrowsingData(
-        entry.storage_key,
-        BrowsingDataModel::StorageType::kUnpartitionedQuotaStorage,
-        entry.syncable_usage + entry.temporary_usage);
+    model->AddBrowsingData(entry.storage_key,
+                           BrowsingDataModel::StorageType::kQuotaStorage,
+                           entry.syncable_usage + entry.temporary_usage);
   }
   std::move(loaded_callback).Run();
 }
