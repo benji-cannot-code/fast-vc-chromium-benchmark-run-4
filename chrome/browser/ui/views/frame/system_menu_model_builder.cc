f57d8c32accda0fb3b1cba78718f8117a1371d49
Jackson Loeffler <j@jloeffler.com>
2023-06-22 20:26:46 +0000

    Simplify DWM transitions on Windows
    This CL removes unnecessary steps when changing themes and performing a
    DWM transition on Windows.
    
    On Windows 10 and above, we never transition between the native DWM
    frame and the custom frame since DWM composition is always on, and all
    themes use the native frame. This removes the need to hide and show the
    window when changing themes, which also fixes related keyboard focus
    issues.
    
    This also removes the obsolete debug-enable-frame-toggle switch and
    skips unit test calls to Widget::DebugToggleFrameType() on Windows
    since they're no longer needed.
    
    Bug: 1446712, 1246181, 1381254
    Change-Id: I5e7ff2ab8d347066236cd061ada0e77e77fd7d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575565
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161417}

--- chrome/browser/ui/views/frame/system_menu_model_builder.cc
+++ chrome/browser/ui/views/frame/system_menu_model_builder.cc
@@ -70,7 +70,6 @@ void SystemMenuModelBuilder::BuildMenu(ui::SimpleMenuModel* model) {
     BuildSystemMenuForBrowserWindow(model);
   else
     BuildSystemMenuForAppOrPopupWindow(model);
-  AddFrameToggleItems(model);
 }
 
 void SystemMenuModelBuilder::BuildSystemMenuForBrowserWindow(
@@ -157,14 +156,6 @@ void SystemMenuModelBuilder::BuildSystemMenuForAppOrPopupWindow(
   AppendMoveToDesksMenu(model);
 #endif
   AppendTeleportMenu(model);
-}
-
-void SystemMenuModelBuilder::AddFrameToggleItems(ui::SimpleMenuModel* model) {
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kDebugEnableFrameToggle)) {
-    model->AddSeparator(ui::NORMAL_SEPARATOR);
-    model->AddItem(IDC_DEBUG_FRAME_TOGGLE, u"Toggle Frame Type");
-  }
 }
 
 #if BUILDFLAG(IS_CHROMEOS)
