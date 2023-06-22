840b18eb938581833174c111cd2667b0b806e560
Francois Marier <francois@brave.com>
2023-06-22 17:31:56 +0000

    Create a Certificate Transparency feature flag
    While this does not change whether or not the feature is enabled in
    Chrome, it allows embedders to override the flag and enable CT using
    the Chrome policy on an opt-in basis.
    
    SCT auditing has been moved behind GOOGLE_CHROME_BRANDING since it
    requires backend infrastructure and a data collection policy.
    Embedders should not use Chrome's infrastructure for this.
    
    Bug: 1451974
    Change-Id: Ief1c06bcb272168537a2b604e1ff87a8c4b8d82b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4596376
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Francois Marier <francois@brave.com>
    Cr-Commit-Position: refs/heads/main@{#1161300}

--- chrome/browser/browser_features.cc
+++ chrome/browser/browser_features.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_features.h"
 
 #include "base/feature_list.h"
+#include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 
@@ -170,8 +171,21 @@ BASE_FEATURE(kWebUsbDeviceDetection,
 // Enables Certificate Transparency on Android.
 BASE_FEATURE(kCertificateTransparencyAndroid,
              "CertificateTransparencyAndroid",
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
              base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#else
+// Enables Certificate Transparency on Desktop.
+BASE_FEATURE(kCertificateTransparencyAskBeforeEnabling,
+             "CertificateTransparencyAskBeforeEnabling",
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#endif  // BUILDFLAG(IS_ANDROID)
 
 BASE_FEATURE(kLargeFaviconFromGoogle,
              "LargeFaviconFromGoogle",
--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/strings/string_split.h"
+#include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/common/chrome_switches.h"
@@ -1031,10 +1032,12 @@ BASE_FEATURE(kSchedulerConfiguration,
 
 // Controls whether SCT audit reports are queued and the rate at which they
 // should be sampled. Default sampling rate is 1/10,000 certificates.
-#if BUILDFLAG(IS_ANDROID)
-BASE_FEATURE(kSCTAuditing, "SCTAuditing", base::FEATURE_DISABLED_BY_DEFAULT);
-#else
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && !BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kSCTAuditing, "SCTAuditing", base::FEATURE_ENABLED_BY_DEFAULT);
+#else
+// This requires backend infrastructure and a data collection policy.
+// Non-Chrome builds should not use Chrome's infrastructure.
+BASE_FEATURE(kSCTAuditing, "SCTAuditing", base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 constexpr base::FeatureParam<double> kSCTAuditingSamplingRate{
     &kSCTAuditing, "sampling_rate", 0.0001};
