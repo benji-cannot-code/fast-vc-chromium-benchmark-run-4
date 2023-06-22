5b87e80541994ed9558577e4a58543a5cf97bc6e
Zhenyao Mo <zmo@chromium.org>
2023-06-22 18:18:48 +0000

    Add trace event to measure driver side VideoProcessorBlt() calls.
    TEST=manual
    R=magchen@chromium.org
    
    Bug: 1457235
    Change-Id: Iecc11b182c77c262da678452a541ec66206d8647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4636925
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161324}

--- ui/gl/swap_chain_presenter.cc
+++ ui/gl/swap_chain_presenter.cc
@@ -1834,8 +1834,11 @@ bool SwapChainPresenter::VideoProcessorBlt(
       use_vp_super_resolution = !is_on_battery_power_ && SUCCEEDED(hr);
     }
 
-    hr = video_context->VideoProcessorBlt(video_processor.Get(),
-                                          output_view_.Get(), 0, 1, &stream);
+    {
+      TRACE_EVENT0("gpu", "ID3D11VideoContext::VideoProcessorBlt");
+      hr = video_context->VideoProcessorBlt(video_processor.Get(),
+                                            output_view_.Get(), 0, 1, &stream);
+    }
     base::UmaHistogramSparse(
         (use_vp_super_resolution
              ? "GPU.VideoProcessorBlt.VpSuperResolution.On"
@@ -1851,8 +1854,11 @@ bool SwapChainPresenter::VideoProcessorBlt(
 
         ToggleVpSuperResolution(gpu_vendor_id_, video_context.Get(),
                                 video_processor.Get(), false);
-        hr = video_context->VideoProcessorBlt(
-            video_processor.Get(), output_view_.Get(), 0, 1, &stream);
+        {
+          TRACE_EVENT0("gpu", "ID3D11VideoContext::VideoProcessorBlt");
+          hr = video_context->VideoProcessorBlt(
+              video_processor.Get(), output_view_.Get(), 0, 1, &stream);
+        }
 
         base::UmaHistogramSparse(
             "GPU.VideoProcessorBlt.VpSuperResolution.RetryOffAfterError", hr);
