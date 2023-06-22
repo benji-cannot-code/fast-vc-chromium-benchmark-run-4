9986c67751ab7ff00602dd0d2c894832e8061bfd
Marc Treib <treib@chromium.org>
2023-06-22 11:47:30 +0000

    SyncPrefs: User migration for kReplaceSyncPromosWithSignInPromos, part 2
    This CL implements the part of the migration that depends on the
    passphrase state of the user. This is a bit tricky because at the point
    in time where the first part of the migration runs (early at browser
    startup), the passphrase type isn't known yet.
    To handle this, the first part of the migration sets a flag that the
    second part should run. The second part gets triggered as soon as the
    passphrase state is known.
    
    Bug: 1447020
    Change-Id: I064a0641d787bf51f506ed6e7f6c4b439cba580d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628488
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161138}

--- components/sync/service/glue/sync_transport_data_prefs.h
+++ components/sync/service/glue/sync_transport_data_prefs.h
@@ -6,14 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SYNC_SERVICE_GLUE_SYNC_TRANSPORT_DATA_PREFS_H_
 #define COMPONENTS_SYNC_SERVICE_GLUE_SYNC_TRANSPORT_DATA_PREFS_H_
 
-#include <map>
 #include <string>
 
 #include "base/memory/raw_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
-#include "components/prefs/pref_member.h"
-#include "components/sync/base/model_type.h"
 
 class PrefRegistrySimple;
 class PrefService;
