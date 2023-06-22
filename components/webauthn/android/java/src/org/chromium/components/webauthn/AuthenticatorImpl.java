3f439a64f9d5ed9571d0a5e167976c022719d94e
Ken Buchanan <kenrb@chromium.org>
2023-06-22 19:44:50 +0000

    [WebAuthn] Separate Android AuthenticatorImpl cleanup from close()
    Currently `close()` is called in AuthenticatorImpl.java when a
    WebAuthn request completes. `close()` is overriden from the mojo
    Interface class, and is also called when the connection terminates.
    
    This change adds `cleanupRequest()` which performs the request state
    cleanup, so that `close()` can be used as a clear signal that the
    connection has closed.
    
    Bug: 1457277
    Change-Id: Ia20115dc15503e77ce1480fd5a568203a8fe70d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4636953
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161391}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/AuthenticatorImpl.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/AuthenticatorImpl.java
@@ -256,7 +256,7 @@ public final class AuthenticatorImpl implements Authenticator {
         assert mMakeCredentialCallback != null;
         assert status == AuthenticatorStatus.SUCCESS;
         mMakeCredentialCallback.call(status, response, null);
-        close();
+        cleanupRequest();
     }
 
     public void onSignResponse(int status, GetAssertionAuthenticatorResponse response) {
@@ -265,7 +265,7 @@ public final class AuthenticatorImpl implements Authenticator {
 
         assert mGetAssertionCallback != null;
         mGetAssertionCallback.call(status, response, null);
-        close();
+        cleanupRequest();
     }
 
     public void onIsUserVerifyingPlatformAuthenticatorAvailableResponse(boolean isUVPAA) {
@@ -285,15 +285,19 @@ public final class AuthenticatorImpl implements Authenticator {
         } else if (mGetAssertionCallback != null) {
             mGetAssertionCallback.call(status, null, null);
         }
-        close();
+        cleanupRequest();
     }
 
-    @Override
-    public void close() {
+    private void cleanupRequest() {
         mIsOperationPending = false;
         mMakeCredentialCallback = null;
         mGetAssertionCallback = null;
         mPendingFido2CredentialRequest = null;
+    }
+
+    @Override
+    public void close() {
+        cleanupRequest();
     }
 
     @Override
