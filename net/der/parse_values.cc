133e868d61be26e73d6a2d039edea168b2322ba2
Bob Beck <bbe@google.com>
2023-06-22 17:10:33 +0000

    Include <vector> in parse_values.cc
    This is used here and is currently obtained via hopes and prayers
    from other dependencies. It it needed when built outside of chrome.
    
    Bug: 1322914
    Change-Id: I6a13eb6ac98cca9eee0889963c16dd947b5cc9f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4635530
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: Bob Beck <bbe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161283}

--- net/der/parse_values.cc
+++ net/der/parse_values.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/der/parse_values.h"
 
 #include <tuple>
+#include <vector>
 
 #include "base/check_op.h"
 #include "base/notreached.h"
