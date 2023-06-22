df2a1a29e5f6c9d820c6daafeddbf1a81037dac7
Sam Thiesen <samicolon@google.com>
2023-06-22 08:36:34 +0000

    [Multi Apps] Split up sub app install into 2 steps
    Reland of https://crrev.com/c/4567844
    
    Change-Id: I4b018bbdaeed8286e3ec85008ff9c9a2c3fce745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598229
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Sam Thiesen <samicolon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161088}

--- chrome/browser/ui/web_applications/sub_apps_service_impl.h
+++ chrome/browser/ui/web_applications/sub_apps_service_impl.h
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEB_APPLICATIONS_SUB_APPS_SERVICE_IMPL_H_
 #define CHROME_BROWSER_UI_WEB_APPLICATIONS_SUB_APPS_SERVICE_IMPL_H_
 
+#include <map>
 #include <string>
 #include <utility>
 #include <vector>
 
 #include "chrome/browser/web_applications/web_app_id.h"
+#include "chrome/browser/web_applications/web_app_install_info.h"
+#include "components/webapps/browser/install_result_code.h"
 #include "content/public/browser/document_service.h"
 #include "third_party/blink/public/mojom/subapps/sub_apps_service.mojom.h"
 
@@ -23,8 +26,6 @@ namespace web_app {
 class SubAppsServiceImpl
     : public content::DocumentService<blink::mojom::SubAppsService> {
  public:
-  using AddResults = std::vector<
-      std::pair<ManifestId, blink::mojom::SubAppsServiceResultCode>>;
   using AddResultsMojo = std::vector<blink::mojom::SubAppsServiceAddResultPtr>;
 
   SubAppsServiceImpl(const SubAppsServiceImpl&) = delete;
@@ -46,6 +47,28 @@ class SubAppsServiceImpl
               RemoveCallback result_callback) override;
 
  private:
+  struct AddCallInfo {
+    AddCallInfo();
+    ~AddCallInfo();
+
+    AddCallback mojo_callback;
+    std::vector<std::unique_ptr<WebAppInstallInfo>> install_infos;
+    AddResultsMojo results;
+  };
+
+  void CollectInstallData(
+      int add_call_id,
+      std::vector<std::pair<ManifestId, GURL>> requested_installs);
+  void ProcessInstallData(
+      int add_call_id,
+      std::vector<std::pair<ManifestId, std::unique_ptr<WebAppInstallInfo>>>
+          install_data);
+  void ScheduleSubAppInstalls(int add_call_id);
+  void FinishAddCall(
+      int add_call_id,
+      std::vector<std::tuple<ManifestId, AppId, webapps::InstallResultCode>>
+          install_results);
+
   void RemoveSubApp(
       const std::string& manifest_id_path,
       base::OnceCallback<void(blink::mojom::SubAppsServiceRemoveResultPtr)>
@@ -54,6 +77,9 @@ class SubAppsServiceImpl
   SubAppsServiceImpl(
       content::RenderFrameHost& render_frame_host,
       mojo::PendingReceiver<blink::mojom::SubAppsService> receiver);
+
+  int next_add_call_id_ = 0;
+  std::map<int, AddCallInfo> add_call_info_;
 
   base::WeakPtrFactory<SubAppsServiceImpl> weak_ptr_factory_{this};
 };
