4ea7122b2497fd047fffda18358468718fb5eaa2
Scott Haseley <shaseley@chromium.org>
2023-06-22 21:57:22 +0000

    AbortSignal: Guard AbortSignal::SignalAbort with a pass key
    Directly aborting signals violates assumptions for composite signals,
    and should be reserved for AbortController and AbortSignal. This CL
    guards SignalAbort with a pass key.
    
    See also https://github.com/whatwg/dom/issues/1194.
    
    Bug: 1448352
    Low-Coverage-Reason: Follow() is covered by virtual tests
    Change-Id: I02cec638d1c4c20c6d85830ea5eb26b5cf4fd251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638358
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161467}

--- third_party/blink/renderer/core/dom/abort_signal.h
+++ third_party/blink/renderer/core/dom/abort_signal.h
@@ -18,9 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class AbortController;
 class AbortSignalCompositionManager;
 class ExceptionState;
 class ExecutionContext;
+class FollowAlgorithm;
 class ScriptState;
 
 // Implementation of https://dom.spec.whatwg.org/#interface-AbortSignal
@@ -123,11 +125,19 @@ class CORE_EXPORT AbortSignal : public EventTargetWithInlineData,
   // needed, e.g. to not rely on GC timing.
   void RemoveAlgorithm(AlgorithmHandle*);
 
+  class SignalAbortPassKey {
+   private:
+    SignalAbortPassKey() = default;
+
+    friend class AbortController;
+    friend class AbortSignal;
+    friend class FollowAlgorithm;
+  };
   // The "To signal abort" algorithm from the standard:
   // https://dom.spec.whatwg.org/#abortsignal-add. Run all algorithms that were
   // added by AddAlgorithm(), in order of addition, then fire an "abort"
   // event. Does nothing if called more than once.
-  void SignalAbort(ScriptState*, ScriptValue reason);
+  void SignalAbort(ScriptState*, ScriptValue reason, SignalAbortPassKey);
 
   // The "follow" algorithm from the standard:
   // https://dom.spec.whatwg.org/#abortsignal-follow
