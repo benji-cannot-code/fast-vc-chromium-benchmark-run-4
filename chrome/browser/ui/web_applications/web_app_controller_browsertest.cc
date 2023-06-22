31f6efe21185a7a99af0755dfb1f97022a436f60
Allen Bauer <kylixrd@chromium.org>
2023-06-22 22:15:22 +0000

    [CR2023] Update PWA app-menu for Chrome Refresh.
    Change-Id: If389cefc313fedb8a91bcc1b9eb965eec6236025
    Bug: 1441147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4630071
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161476}

--- chrome/browser/ui/web_applications/web_app_controller_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_controller_browsertest.cc
@@ -43,6 +43,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web_app {
 
 WebAppControllerBrowserTest::WebAppControllerBrowserTest()
+    : WebAppControllerBrowserTest({}, {}) {}
+
+WebAppControllerBrowserTest::WebAppControllerBrowserTest(
+    const std::vector<base::test::FeatureRef>& enabled_features,
+    const std::vector<base::test::FeatureRef>& disabled_features)
     // TODO(crbug.com/1378355): Fix the manifest update process by ensuring
     // during test installs, an app is installed from the manifest so that the
     // identity update dialog is not triggered after navigation. This will
@@ -51,11 +56,17 @@ WebAppControllerBrowserTest::WebAppControllerBrowserTest()
       update_dialog_scope_(SetIdentityUpdateDialogActionForTesting(
           AppIdentityUpdate::kSkipped)) {
   os_hooks_suppress_.emplace();
-  scoped_feature_list_.InitWithFeatures({}, {
+  std::vector<base::test::FeatureRef> all_disabled_features = {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-    features::kWebAppsCrosapi, ash::features::kLacrosPrimary
+    features::kWebAppsCrosapi,
+    ash::features::kLacrosPrimary
 #endif
-  });
+  };
+  all_disabled_features.insert(all_disabled_features.end(),
+                               disabled_features.begin(),
+                               disabled_features.end());
+  scoped_feature_list_.InitWithFeatures(enabled_features,
+                                        all_disabled_features);
 }
 
 WebAppControllerBrowserTest::~WebAppControllerBrowserTest() = default;
