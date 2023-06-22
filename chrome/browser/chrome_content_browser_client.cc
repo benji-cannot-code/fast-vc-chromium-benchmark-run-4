d58e8387982f1ee49ea5b774f8a8d38da731cc16
Sean Kau <skau@chromium.org>
2023-06-22 00:38:59 +0000

    Migrate /chrome/browser to ColorProviderKey
    ColorProviderManager::Key and its associated types are being
    moved into their own class, ColorProviderKey. As a result,
    class names are rewritten and includes are replaced.
    
    This CL was uploaded by git cl split.
    
    R=mark@chromium.org, sky@chromium.org
    
    Bug: b:286952053
    Change-Id: If81974e3be455037640fe76efdb8de370612862d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629855
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160961}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -359,6 +359,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/color/color_provider_key.h"
 #include "ui/gfx/switches.h"
 #include "ui/native_theme/native_theme.h"
 #include "url/gurl.h"
@@ -3664,8 +3665,7 @@ bool UpdatePreferredColorScheme(WebPreferences* web_prefs,
     // If color scheme is not forced, WebUI should track the color mode of the
     // ColorProvider associated with `web_contents`.
     web_prefs->preferred_color_scheme =
-        web_contents->GetColorMode() ==
-                ui::ColorProviderManager::ColorMode::kLight
+        web_contents->GetColorMode() == ui::ColorProviderKey::ColorMode::kLight
             ? blink::mojom::PreferredColorScheme::kLight
             : blink::mojom::PreferredColorScheme::kDark;
   }
--- chrome/browser/chrome_content_browser_client_browsertest.cc
+++ chrome/browser/chrome_content_browser_client_browsertest.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
 #include "ui/color/color_provider.h"
+#include "ui/color/color_provider_key.h"
 #include "ui/color/color_provider_manager.h"
 #include "ui/color/color_provider_source.h"
 #include "ui/native_theme/native_theme.h"
@@ -407,8 +408,8 @@ class PrefersColorSchemeTest
   class MockColorProviderSource : public ui::ColorProviderSource {
    public:
     explicit MockColorProviderSource(bool is_dark) {
-      key_.color_mode = is_dark ? ui::ColorProviderManager::ColorMode::kDark
-                                : ui::ColorProviderManager::ColorMode::kLight;
+      key_.color_mode = is_dark ? ui::ColorProviderKey::ColorMode::kDark
+                                : ui::ColorProviderKey::ColorMode::kLight;
       provider_.GenerateColorMap();
     }
     MockColorProviderSource(const MockColorProviderSource&) = delete;
@@ -419,13 +420,11 @@ class PrefersColorSchemeTest
     const ui::ColorProvider* GetColorProvider() const override {
       return &provider_;
     }
-    ui::ColorProviderManager::Key GetColorProviderKey() const override {
-      return key_;
-    }
+    ui::ColorProviderKey GetColorProviderKey() const override { return key_; }
 
    private:
     ui::ColorProvider provider_;
-    ui::ColorProviderManager::Key key_;
+    ui::ColorProviderKey key_;
   };
 
   base::test::ScopedFeatureList feature_list_;
