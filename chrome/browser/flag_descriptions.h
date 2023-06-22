03ae5c52e4a52e0c8e83c9ad3bb5837b988b4261
Colin Kincaid <ckincaid@chromium.org>
2023-06-22 02:25:37 +0000

    multipaste: Expose clipboard history refresh flag in chrome://flags
    This CL allows users to enable updated clipboard history functionality:
    a new educational nudge, refreshed menu UI, and a clipboard history
    submenu embedded in context menus.
    
    Bug: b/262443689
    Change-Id: I97c9e9f3af1b55ec61177bcf0d15343d4a6d1779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634825
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161001}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3131,6 +3131,9 @@ extern const char kCellularUseSecondEuiccDescription[];
 extern const char kClipboardHistoryLongpressName[];
 extern const char kClipboardHistoryLongpressDescription[];
 
+extern const char kClipboardHistoryRefreshName[];
+extern const char kClipboardHistoryRefreshDescription[];
+
 extern const char kComponentUpdaterTestRequestName[];
 extern const char kComponentUpdaterTestRequestDescription[];
 
