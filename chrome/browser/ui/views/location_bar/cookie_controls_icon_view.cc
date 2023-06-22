09f664acaac8d77b4ba5e193945f358d8ef0ffda
Nicola Tommasi <tommasin@chromium.org>
2023-06-22 10:13:46 +0000

    [user bypass]: Create scaffolding MVC for bubble view
    This CL just creates the scaffolding Model-View-Controller for the new
    CookieControlsBubbleView. The new view will be used for User Bypass.
    (see more in: go/user-bypass-dd)
    
    Bug: 1446230, b:280779743
    
    Change-Id: Ib5a649ed8a2b67cd8a7ef41ebf93116d07be68c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4620667
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161115}

--- chrome/browser/ui/views/location_bar/cookie_controls_icon_view.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls_icon_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/old_cookie_controls_bubble_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/content_settings/browser/ui/cookie_controls_controller.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/vector_icons.h"
 #include "content/public/browser/browser_context.h"
@@ -36,6 +38,11 @@ CookieControlsIconView::CookieControlsIconView(
   SetAccessibilityProperties(
       /*role*/ absl::nullopt,
       l10n_util::GetStringUTF16(IDS_COOKIE_CONTROLS_TOOLTIP));
+
+  if (base::FeatureList::IsEnabled(content_settings::features::kUserBypassUI)) {
+    bubble_coordinator_ =
+        std::make_unique<CookieControlsBubbleCoordinator>(this);
+  }
 }
 
 CookieControlsIconView::~CookieControlsIconView() = default;
@@ -89,6 +96,23 @@ void CookieControlsIconView::OnStatefulBounceCountChanged(int bounce_count) {
   }
 }
 
+void CookieControlsIconView::OnStatusChanged(
+    CookieControlsStatus status,
+    CookieControlsEnforcement enforcement,
+    absl::optional<base::Time> expiration) {
+  // TODO(1446230): Implement OnStatusChanged.
+}
+
+void CookieControlsIconView::OnSitesCountChanged(int allowed_sites,
+                                                 int blocked_sites) {
+  // TODO(1446230): Implement OnSitesCountChanged.
+}
+
+void CookieControlsIconView::OnBreakageConfidenceLevelChanged(
+    CookieControlsBreakageConfidenceLevel level) {
+  // TODO(1446230): Implement OnBreakageConfidenceLevelChanged.
+}
+
 bool CookieControlsIconView::ShouldBeVisible() const {
   if (delegate()->ShouldHidePageActionIcons()) {
     return false;
@@ -100,6 +124,12 @@ bool CookieControlsIconView::ShouldBeVisible() const {
 
   if (!delegate()->GetWebContentsForPageActionIconView()) {
     return false;
+  }
+
+  if (base::FeatureList::IsEnabled(content_settings::features::kUserBypassUI)) {
+    // TODO(1446230): Remove this once the CookieControlsObserver methods have
+    // been implemented.
+    return true;
   }
 
   switch (status_) {
@@ -122,12 +152,18 @@ bool CookieControlsIconView::GetAssociatedBubble() const {
 
 void CookieControlsIconView::OnExecuting(
     PageActionIconView::ExecuteSource source) {
-  OldCookieControlsBubbleView::ShowBubble(
-      this, this, delegate()->GetWebContentsForPageActionIconView(),
-      controller_.get(), status_);
+  if (base::FeatureList::IsEnabled(content_settings::features::kUserBypassUI)) {
+    bubble_coordinator_->ShowBubble(
+        delegate()->GetWebContentsForPageActionIconView(), controller_.get());
+  } else {
+    OldCookieControlsBubbleView::ShowBubble(
+        this, this, delegate()->GetWebContentsForPageActionIconView(),
+        controller_.get(), status_);
+  }
 }
 
 views::BubbleDialogDelegate* CookieControlsIconView::GetBubble() const {
+  // TODO(crbug.com/1446230): Return new bubble when ready.
   return OldCookieControlsBubbleView::GetCookieBubble();
 }
 
