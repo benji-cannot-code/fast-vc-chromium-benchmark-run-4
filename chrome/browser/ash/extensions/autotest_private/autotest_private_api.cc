52fb527d3e9b0f1d176698a7567dc9f4f4f41c7b
Jason Thai <jasontt@chromium.org>
2023-06-22 18:13:56 +0000

    update autotestPrivate.forceAutoThemeMode
    Disable auto mode to prevent flaky behavior of D/L theme auto switching
    during sun-rise/sun-set. This is the same behavior as the user manually
    setting D/L mode in personalization hub.
    
    BUG=b:288359521
    
    Change-Id: Ib3483d80965163181e1140a6c17401cf27354c73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637521
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161321}

--- chrome/browser/ash/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/ash/extensions/autotest_private/autotest_private_api.cc
@@ -42,12 +42,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/shelf_ui_info.h"
 #include "ash/public/cpp/split_view_test_api.h"
-#include "ash/public/cpp/style/dark_light_mode_controller.h"
 #include "ash/public/cpp/tablet_mode.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/root_window_controller.h"
 #include "ash/rotator/screen_rotation_animator.h"
 #include "ash/shell.h"
+#include "ash/style/dark_light_mode_controller_impl.h"
 #include "ash/wallpaper/wallpaper_widget_controller.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/wm_event.h"
@@ -6315,10 +6315,11 @@ AutotestPrivateForceAutoThemeModeFunction::Run() {
       api::autotest_private::ForceAutoThemeMode::Params::Create(args());
   EXTENSION_FUNCTION_VALIDATE(params);
 
-  ash::DarkLightModeController* dark_light_mode_controller =
-      ash::DarkLightModeController::Get();
+  ash::DarkLightModeControllerImpl* dark_light_mode_controller =
+      ash::Shell::Get()->dark_light_mode_controller();
   DCHECK(dark_light_mode_controller);
 
+  dark_light_mode_controller->SetAutoScheduleEnabled(false);
   dark_light_mode_controller->SetDarkModeEnabledForTest(
       params->dark_mode_enabled);
   return RespondNow(NoArguments());
