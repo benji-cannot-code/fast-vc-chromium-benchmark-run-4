4a263c06e5a080dacf43a37b175d9bdab95388ce
Yue Zhang <yuezhanggg@chromium.org>
2023-06-22 22:23:42 +0000

    [Gardening] Disable test in SyncConsentActivityLauncherImplTest
    Bug: 1457280
    Change-Id: Ie31b4782937fa3cb4d18f9a60c69c689d917bb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638901
    Auto-Submit: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Owners-Override: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161482}

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SyncConsentActivityLauncherImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SyncConsentActivityLauncherImplTest.java
@@ -27,6 +27,7 @@ import org.robolectric.shadows.ShadowToast;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -88,6 +89,7 @@ public class SyncConsentActivityLauncherImplTest {
     }
 
     @Test
+    @DisabledTest(message = "Flaky - crbug/1457280")
     public void testLaunchActivityIfAllowedWhenSigninIsDisabledByPolicy() {
         HistogramWatcher watchSigninDisabledToastShownHistogram =
                 HistogramWatcher.newSingleRecordWatcher("Signin.SyncDisabledNotificationShown",
