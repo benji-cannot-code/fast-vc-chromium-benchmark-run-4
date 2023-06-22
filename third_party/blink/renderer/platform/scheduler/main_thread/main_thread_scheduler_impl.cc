694f16e966210c29b42bf4849b0afa96d813fa65
Noam Rosenthal <nrosenthal@chromium.org>
2023-06-22 18:10:59 +0000

    Connect LoAF origin trial name with feature
    Bug: 1457098
    Change-Id: I1796cdd1e234904807e1a4e9841d61de5f376531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4633460
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161319}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -2397,11 +2397,9 @@ void MainThreadSchedulerImpl::OnTaskCompleted(
   if (queue) {
     queue->OnTaskRunTimeReported(task_timing);
 
-    if (RuntimeEnabledFeatures::LongAnimationFrameMonitoringEnabled()) {
-      if (FrameSchedulerImpl* frame_scheduler = queue->GetFrameScheduler()) {
-        frame_scheduler->OnTaskCompleted(task_timing,
-                                         task.GetDesiredExecutionTime());
-      }
+    if (FrameSchedulerImpl* frame_scheduler = queue->GetFrameScheduler()) {
+      frame_scheduler->OnTaskCompleted(task_timing,
+                                       task.GetDesiredExecutionTime());
     }
   }
 
