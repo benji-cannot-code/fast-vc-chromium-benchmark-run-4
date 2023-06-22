26f9c57bc7d76aaa86ec2d8e075d5dec907fcee2
Hang Nguyen <hnt@google.com>
2023-06-22 23:03:14 +0000

    media/gpu/v4l2: Extract VideoCodecProfileToV4L2PixFmt() out of V4L2Device
    This CL extracts the static function out of V4L2Device since it does not
    use any V4L2 resource.
    
    No new functionality, only moving code around.
    
    Bug: b=279980150
    Change-Id: I962c5b1ae012407554b651cc8d4263fddc741f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634982
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Hang Nguyen <hnt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161503}

--- media/gpu/v4l2/v4l2_utils.h
+++ media/gpu/v4l2/v4l2_utils.h
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   v4l2_fourcc('Q', '0', '8', 'C') /* Qualcomm 8-bit compressed */
 #endif
 
+#ifndef V4L2_PIX_FMT_INVALID
+#define V4L2_PIX_FMT_INVALID v4l2_fourcc('0', '0', '0', '0')
+#endif
+
 namespace gfx {
 class Size;
 }
@@ -84,6 +88,11 @@ void GetSupportedResolution(const IoctlAsCallback& ioctl_cb,
                             gfx::Size* min_resolution,
                             gfx::Size* max_resolution);
 
+// Translates a media::VideoCodecProfile to a supported pixel format
+// (e.g. V4L2_PIX_FMT_VP9) if those are supported by Chrome. It returns
+// V4L2_PIX_FMT_INVALID otherwise.
+uint32_t VideoCodecProfileToV4L2PixFmt(VideoCodecProfile profile,
+                                       bool slice_based);
 }  // namespace media
 
 #endif  // MEDIA_GPU_V4L2_V4L2_UTILS_H_
