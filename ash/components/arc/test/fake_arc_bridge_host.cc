d2621aa8cfa2b48ec7f4513eceb9f607afdba8a3
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-06-23 07:02:20 +0000

    Revert "Remove ArcRotationLockBridge"
    This reverts commit 9bfab2703273b91bb1f0eb7b05c87e74d34e3085.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8777521280598356449
    
    Sample failed build: https://ci.chromium.org/b/8777521280598356449
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8777521280598356449&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4639442
    
    Original change's description:
    > Remove ArcRotationLockBridge
    >
    > It was used for ARC-N, but all boards were migrated to ARC-P or
    > later a long time ago.
    > It should be safe to remove it completely now.
    >
    > Bug: b:288513984
    > Change-Id: Ie74489edfd9da9d84bb1dc929898acdd5c7a68a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4639442
    > Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1161615}
    >
    
    Bug: b:288513984
    Change-Id: Ia3610fc83fb08ed554b492e028361a50d6e2867e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637492
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1161628}

--- ash/components/arc/test/fake_arc_bridge_host.cc
+++ ash/components/arc/test/fake_arc_bridge_host.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/mojom/privacy_items.mojom.h"
 #include "ash/components/arc/mojom/process.mojom.h"
 #include "ash/components/arc/mojom/property.mojom.h"
+#include "ash/components/arc/mojom/rotation_lock.mojom.h"
 #include "ash/components/arc/mojom/screen_capture.mojom.h"
 #include "ash/components/arc/mojom/sharesheet.mojom.h"
 #include "ash/components/arc/mojom/storage_manager.mojom.h"
@@ -212,6 +213,9 @@ void FakeArcBridgeHost::OnProcessInstanceReady(
 
 void FakeArcBridgeHost::OnPropertyInstanceReady(
     mojo::PendingRemote<mojom::PropertyInstance> property_remote) {}
+
+void FakeArcBridgeHost::OnRotationLockInstanceReady(
+    mojo::PendingRemote<mojom::RotationLockInstance> rotation_lock_remote) {}
 
 void FakeArcBridgeHost::OnScreenCaptureInstanceReady(
     mojo::PendingRemote<mojom::ScreenCaptureInstance> screen_capture_remote) {}
