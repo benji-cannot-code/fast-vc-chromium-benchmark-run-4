930665bd8b9e481152c3f829dbf6014ea76a4093
Veronique Nguyen <veronguyen@google.com>
2023-06-23 14:12:49 +0000

    [NOTREACHED] Fix AccountSelectFillData::GetFillData crash
    This happens because we call AccountSelectFillData::Reset() and it
    sets last_requested_form_ = nullptr.
    
    In our case, we just want to clear the credentials_ cache. I made a
    separate method to clear that cache only.
    
    Bug: 1457248
    Change-Id: Ia5305015f520815bc367cc9fea80adcde6889a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638202
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161726}

--- components/password_manager/ios/password_suggestion_helper.mm
+++ components/password_manager/ios/password_suggestion_helper.mm
@@ -215,7 +215,7 @@ typedef void (^PasswordSuggestionsAvailableCompletion)(
         std::make_pair(frame, std::make_unique<AccountSelectFillData>()));
     fillData = it.first->second.get();
   }
-  fillData->Reset();
+  fillData->ResetCache();
 
   if (_suggestionsAvailableCompletion) {
     _suggestionsAvailableCompletion(nullptr);
