54282fe06373c8a1735ae5d6b50318d6daf3dc29
Liam Brady <lbrady@google.com>
2023-06-23 18:03:29 +0000

    [Privacy Sandbox] Support enrollment overrides in chrome://flags
    3rd party developers will need to test how their sites behave with
    Privacy Sandbox APIs. However, sites are only allowed to use the APIs if
    they have been enrolled/have agreed to the Privacy Sandbox attestations.
    In order to unblock the developers if their site hasn't been enrolled,
    this CL will let them to set a list of origins in their Chromium
    instance that can use APIs (such as FLEDGE and Shared Storage) without
    needing to check for enrollment.
    
    Note that this approach is temporary. The long-term goal is to integrate
    this feature into an upcoming Privacy Sandbox devtools extension. Until
    the details of the extension integration have been hashed out, we are
    implementing this using a flag.
    
    Change-Id: If742873024c9ea2e450ac6ca827bf07de1a5fd92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638542
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161847}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -103,4 +103,7 @@ BASE_FEATURE(kEnforcePrivacySandboxAttestations,
              "EnforcePrivacySandboxAttestations",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+const char kPrivacySandboxEnrollmentOverrides[] =
+    "privacy-sandbox-enrollment-overrides";
+
 }  // namespace privacy_sandbox
