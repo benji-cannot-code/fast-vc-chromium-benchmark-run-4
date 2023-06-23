011beaf6537cca80aa85574c549f1e3b4d9f4968
minch <minch@google.com>
2023-06-23 23:29:23 +0000

    multi_display: Update restore bounds on root window changes
    Updates the restore bounds property and restore bounds inside the
    history stack to be inside the new root window while the window
    being moved to another display. Note, do not update them if the
    window is set to be fullscreen-ed in another display on purpose,
    as the window is expected to be restored back to the previous
    display in this case.
    
    This will resolve the issue that windows suddenly jump from one
    display to another. Also the issue that windows are almost offscreen
    while being moved to another display.
    
    Bug: b/283342630, b/284215727
    Tests: Added tests and exist tests
    Change-Id: I968a52d829637aeff7ce706e814b99adb367c0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583357
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162007}

--- ash/wm/window_state.cc
+++ ash/wm/window_state.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/collision_detection/collision_detection_utils.h"
 #include "ash/wm/default_state.h"
 #include "ash/wm/float/float_controller.h"
+#include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/pip/pip_positioner.h"
 #include "ash/wm/splitview/split_view_constants.h"
 #include "ash/wm/splitview/split_view_controller.h"
@@ -55,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_tree_owner.h"
 #include "ui/compositor/paint_recorder.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
+#include "ui/display/display_finder.h"
 #include "ui/display/screen.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/painter.h"
@@ -293,6 +295,29 @@ void ReportAshPipAndroidPipUseTime(base::TimeDelta duration) {
 void SaveWindowForWindowRestore(WindowState* window_state) {
   if (auto* controller = WindowRestoreController::Get())
     controller->SaveWindow(window_state);
+}
+
+// Same as `ScreenAsh::GetDisplayMatching`. But based on current active displays
+// instead of the active only display list. The latter one does not include the
+// displays that will be removed.
+display::Display GetDisplayMatchingOnAllDisplays(const gfx::Rect& match_rect) {
+  display::Screen* screen = display::Screen::GetScreen();
+  if (match_rect.IsEmpty()) {
+    return screen->GetDisplayNearestPoint(match_rect.origin());
+  }
+
+  const display::Display* matching =
+      display::FindDisplayWithBiggestIntersection(screen->GetAllDisplays(),
+                                                  match_rect);
+  return matching ? *matching : screen->GetPrimaryDisplay();
+}
+
+// Checks whether the given `bounds` is inside the bounds of the given `root`.
+bool IsBoundsInsideRoot(aura::Window* root, const gfx::Rect& bounds) {
+  display::Screen* screen = display::Screen::GetScreen();
+  int64_t target_display_id = screen->GetDisplayNearestWindow(root).id();
+  int64_t current_display_id = GetDisplayMatchingOnAllDisplays(bounds).id();
+  return target_display_id == current_display_id;
 }
 
 }  // namespace
@@ -1354,16 +1379,6 @@ void WindowState::OnWindowPropertyChanged(aura::Window* window,
                                           const void* key,
                                           intptr_t old) {
   DCHECK_EQ(window_, window);
-  if (key == aura::client::kRestoreBoundsKey) {
-    // Updates the window bounds restore history after updating the restore
-    // bounds to keep them always aligned. This is necessary to avoid getting
-    // incorrect restore bounds from the outdated restore history stack.
-    if (HasRestoreBounds() && !window_state_restore_history_.empty()) {
-      window_state_restore_history_.back().restore_bounds_in_screen =
-          GetRestoreBoundsInScreen();
-    }
-    return;
-  }
   if (key == aura::client::kShowStateKey) {
     if (!ignore_property_change_) {
       WMEvent event(WMEventTypeFromShowState(GetShowState()));
@@ -1420,6 +1435,43 @@ void WindowState::OnWindowAddedToRootWindow(aura::Window* window) {
   if (::wm::GetTransientParent(window))
     return;
   MoveAllTransientChildrenToNewRoot(window);
+}
+
+void WindowState::OnWindowRemovingFromRootWindow(aura::Window* window,
+                                                 aura::Window* new_root) {
+  // No need o update the restore bounds if the window is excluded from the mru
+  // window list or `new_root` doesn't exist.
+  if (!CanIncludeWindowInMruList(window) || !new_root) {
+    return;
+  }
+
+  // Do not update the restore bounds and history stack if we are moving the
+  // window because of fullscreen it to another display. As in this case, the
+  // window is expected to be restored back to the previous display instead of
+  // the target display set by `kFullscreenTargetDisplayIdKey`.
+  if (GetFullscreenTargetDisplayId() != display::kInvalidDisplayId) {
+    return;
+  }
+
+  // Update restore bounds to the new root window.
+  if (HasRestoreBounds() &&
+      !IsBoundsInsideRoot(new_root, GetRestoreBoundsInScreen())) {
+    gfx::Rect restore_bounds = GetRestoreBoundsInParent();
+    ::wm::ConvertRectToScreen(new_root, &restore_bounds);
+    SetRestoreBoundsInScreen(restore_bounds);
+  }
+
+  // Update the restore bounds inside the history stack to the new root window.
+  for (auto& state : window_state_restore_history_) {
+    gfx::Rect restore_bounds = state.restore_bounds_in_screen.has_value()
+                                   ? state.restore_bounds_in_screen.value()
+                                   : state.actual_bounds_in_screen;
+    if (!IsBoundsInsideRoot(new_root, restore_bounds)) {
+      ::wm::ConvertRectFromScreen(window->GetRootWindow(), &restore_bounds);
+      ::wm::ConvertRectToScreen(new_root, &restore_bounds);
+      state.restore_bounds_in_screen = restore_bounds;
+    }
+  }
 }
 
 void WindowState::OnWindowDestroying(aura::Window* window) {
