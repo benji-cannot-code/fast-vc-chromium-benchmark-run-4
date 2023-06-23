8cbfce2ba4f16b51aa52a2ddce0a9babfd79c125
Emilia Paz <emiliapaz@chromium.org>
2023-06-23 16:43:53 +0000

    [Extensions c2s] Add metric when opening menu while ext. requests access
    Reland of crrev.com/c/4633618. Original CL was missing actions.xml
    entry
    
    Adds a user metrics action when the user opens the extensions menu by
    clicking on the extensions toolbar button and the request access button
    is visible (meaning extensions are requesting access).
    
    Bug: 1456888
    Change-Id: Ib3d3cf908afeb6b9f0282b61a909e989334f88ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637021
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161798}

--- chrome/browser/ui/views/extensions/extensions_menu_main_page_view_interactive_uitest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view_interactive_uitest.cc
@@ -108,7 +108,7 @@ ExtensionsMenuMainPageViewInteractiveUITest::
     GetExtensionsInRequestAccessButton() {
   return GetExtensionsToolbarContainer()
       ->GetExtensionsToolbarControls()
-      ->request_access_button_for_testing()
+      ->request_access_button()
       ->GetExtensionIdsForTesting();
 }
 
--- chrome/browser/ui/views/extensions/extensions_request_access_hover_card_coordinator_browsertest.cc
+++ chrome/browser/ui/views/extensions/extensions_request_access_hover_card_coordinator_browsertest.cc
@@ -18,7 +18,7 @@ class ExtensionsRequestAccessHoverCardCoordinatorBrowserTest
   ExtensionsRequestAccessButton* request_access_button() {
     return extensions_container()
         ->GetExtensionsToolbarControls()
-        ->request_access_button_for_testing();
+        ->request_access_button();
   }
 
   ExtensionsRequestAccessHoverCardCoordinator* hover_card_coordinator() {
