9d5b7dd3246ef168d02f47147c29babccca0c8bf
Anders Hartvoll Ruud <andruud@chromium.org>
2023-06-23 10:22:07 +0000

    [@scope] Avoid invalidation flags based on in_rightmost_compound
    When matching selectors against certain pseudo-classes,
    we set flags to indicate that elements depend on those
    pseudo-classes. To distinguish between elements that depend on
    a pseudo-class directly and elements that are *related* to such
    a dependency, we use the in_rightmost_compound flag.
    
    This does not work well with @scope and the StyleScopeFrame cache,
    because we evaluate the selectors in the @scope prelude *once*,
    when the scope activations are first calculated. At that time,
    :scope will either be in the subject position
    (in_rightmost_compound=true), *or* in the non-subject position
    (in_rightmost_compound=false), and the invalidation flags will be
    marked accordingly. If we're matching another scoped selector later
    with :scope in the opposite position, we'll hit the cache,
    and neglect to set the flags we need.
    
    In preparation of solving this problem, this CL introduces
    "impact" to SelectorCheckingContext, which controls directly how
    flags should be set. The critical difference between this and
    in_rightmost_compound is that it's possible to specify impact=kBoth,
    which will cause *both* subject/non-subject flags to set. This will
    be used by @scope in a future CL.
    
    Bug: 1451091
    Change-Id: I55b5977c452d79c7572447489d8543aa132c18df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629312
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Byungwoo Lee <blee@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1161680}

--- third_party/blink/renderer/core/css/selector_checker.cc
+++ third_party/blink/renderer/core/css/selector_checker.cc
@@ -208,6 +208,21 @@ static bool ShouldMatchHoverOrActive(
   return false;
 }
 
+static bool Impacts(const SelectorChecker::SelectorCheckingContext& context,
+                    SelectorChecker::Impact impact) {
+  return static_cast<int>(context.impact) & static_cast<int>(impact);
+}
+
+static bool ImpactsSubject(
+    const SelectorChecker::SelectorCheckingContext& context) {
+  return Impacts(context, SelectorChecker::Impact::kSubject);
+}
+
+static bool ImpactsNonSubject(
+    const SelectorChecker::SelectorCheckingContext& context) {
+  return Impacts(context, SelectorChecker::Impact::kNonSubject);
+}
+
 static bool IsFirstChild(const Element& element) {
   return !ElementTraversal::PreviousSibling(element);
 }
@@ -377,6 +392,7 @@ SelectorChecker::MatchStatus SelectorChecker::MatchForRelation(
   }
 
   next_context.in_rightmost_compound = false;
+  next_context.impact = Impact::kNonSubject;
   next_context.is_sub_selector = false;
   next_context.previous_element = context.element;
   next_context.pseudo_id = kPseudoIdNone;
@@ -1527,11 +1543,11 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       return element.IsLink() && context.match_visited;
     case CSSSelector::kPseudoDrag:
       if (mode_ == kResolvingStyle) {
-        if (!context.in_rightmost_compound) {
+        if (ImpactsNonSubject(context)) {
           element.SetChildrenOrSiblingsAffectedByDrag();
         }
       }
-      if (context.in_rightmost_compound) {
+      if (ImpactsSubject(context)) {
         result.SetFlag(MatchFlag::kAffectedByDrag);
       }
       return element.IsDragged();
@@ -1540,7 +1556,7 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
         if (UNLIKELY(context.is_inside_has_pseudo_class)) {
           element.SetAncestorsOrSiblingsAffectedByFocusInHas();
         } else {
-          if (!context.in_rightmost_compound) {
+          if (ImpactsNonSubject(context)) {
             element.SetChildrenOrSiblingsAffectedByFocus();
           }
         }
@@ -1551,7 +1567,7 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
         if (UNLIKELY(context.is_inside_has_pseudo_class)) {
           element.SetAncestorsOrSiblingsAffectedByFocusVisibleInHas();
         } else {
-          if (!context.in_rightmost_compound) {
+          if (ImpactsNonSubject(context)) {
             element.SetChildrenOrSiblingsAffectedByFocusVisible();
           }
         }
@@ -1561,11 +1577,11 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       if (mode_ == kResolvingStyle) {
         if (UNLIKELY(context.is_inside_has_pseudo_class)) {
           element.SetAncestorsOrSiblingsAffectedByFocusInHas();
-        } else if (!context.in_rightmost_compound) {
+        } else if (ImpactsNonSubject(context)) {
           element.SetChildrenOrSiblingsAffectedByFocusWithin();
         }
       }
-      if (context.in_rightmost_compound) {
+      if (ImpactsSubject(context)) {
         result.SetFlag(MatchFlag::kAffectedByFocusWithin);
       }
       probe::ForcePseudoState(&element, CSSSelector::kPseudoFocusWithin,
@@ -1578,11 +1594,11 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       if (mode_ == kResolvingStyle) {
         if (UNLIKELY(context.is_inside_has_pseudo_class)) {
           element.SetAncestorsOrSiblingsAffectedByHoverInHas();
-        } else if (!context.in_rightmost_compound) {
+        } else if (ImpactsNonSubject(context)) {
           element.SetChildrenOrSiblingsAffectedByHover();
         }
       }
-      if (context.in_rightmost_compound) {
+      if (ImpactsSubject(context)) {
         result.SetFlag(MatchFlag::kAffectedByHover);
       }
       if (!ShouldMatchHoverOrActive(context)) {
@@ -1598,11 +1614,11 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       if (mode_ == kResolvingStyle) {
         if (UNLIKELY(context.is_inside_has_pseudo_class)) {
           element.SetAncestorsOrSiblingsAffectedByActiveInHas();
-        } else if (!context.in_rightmost_compound) {
+        } else if (ImpactsNonSubject(context)) {
           element.SetChildrenOrSiblingsAffectedByActive();
         }
       }
-      if (context.in_rightmost_compound) {
+      if (ImpactsSubject(context)) {
         result.SetFlag(MatchFlag::kAffectedByActive);
       }
       if (!ShouldMatchHoverOrActive(context)) {
@@ -1860,9 +1876,10 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
         // of a :has() anchor element, we may need to invalidate the subject
         // element of the style rule containing the :has() pseudo class because
         // the mutation can affect the state of the :has().
-        if (context.in_rightmost_compound) {
+        if (ImpactsSubject(context)) {
           element.SetAffectedBySubjectHas();
-        } else {
+        }
+        if (ImpactsNonSubject(context)) {
           element.SetAffectedByNonSubjectHas();
         }
 
@@ -2083,6 +2100,7 @@ bool SelectorChecker::CheckPseudoHost(const SelectorCheckingContext& context,
     }
 
     host_context.in_rightmost_compound = false;
+    host_context.impact = Impact::kNonSubject;
     next_element = FlatTreeTraversal::ParentElement(*next_element);
   } while (next_element);
 
