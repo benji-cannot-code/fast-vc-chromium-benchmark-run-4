503a2942b1ad062c7d80e8ab5f4c88d692846b34
Ritika Gupta <ritikagup@google.com>
2023-06-23 18:41:25 +0000

    [IOS] Adding missing segment in IOS segmentation config.
    Change-Id: I204476de6559d2f78e1e05de902b2ab62f582ccc
    Bug: 1457032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634611
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161871}

--- ios/chrome/browser/segmentation_platform/segmentation_platform_config.mm
+++ ios/chrome/browser/segmentation_platform/segmentation_platform_config.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/segmentation_platform/embedder/default_model/cross_device_user_segment.h"
 #import "components/segmentation_platform/embedder/default_model/device_switcher_model.h"
 #import "components/segmentation_platform/embedder/default_model/feed_user_segment.h"
+#import "components/segmentation_platform/embedder/default_model/low_user_engagement_model.h"
 #import "components/segmentation_platform/embedder/default_model/password_manager_user_segment.h"
 #import "components/segmentation_platform/embedder/default_model/search_user_model.h"
 #import "components/segmentation_platform/embedder/default_model/shopping_user_model.h"
@@ -121,6 +122,7 @@ std::vector<std::unique_ptr<Config>> GetSegmentationPlatformConfig() {
     configs.emplace_back(DeviceSwitcherModel::GetConfig());
   }
 
+  configs.emplace_back(LowUserEngagementModel::GetConfig());
   configs.emplace_back(TabResumptionRanker::GetConfig());
   configs.emplace_back(PasswordManagerUserModel::GetConfig());
   configs.emplace_back(ShoppingUserModel::GetConfig());
