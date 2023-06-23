409b35ec685499a521b5a82ac205b01aa57dde35
Emilia Paz <emiliapaz@chromium.org>
2023-06-23 00:02:44 +0000

    [Extensions c2s] Clear tab permissions when toggling extension off
    Clear tab permissions for extension when extension is toggled off
    and the current site access is "on click".
    
    Additionally, this CL reorganizes
    ExtensionsMenuViewController::OnExtensionToggleSelected and
    SitePermissionsHelper::UpdateSiteAccess so its easier to read.
    
    Screencast: https://drive.google.com/file/d/18LmLCDNF3xRqdPWYm1Sh42YwbTsvrj4W/view?usp=sharing
    
    Bug: 1433399
    Change-Id: Id1680db34f98a98b68d545eea0c5629a94ad17a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619103
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161530}

--- chrome/browser/ui/views/extensions/extensions_toolbar_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_unittest.cc
@@ -42,6 +42,9 @@ base::Value::List ToListValue(const std::vector<std::string>& permissions) {
 
 }  // namespace
 
+ExtensionsToolbarUnitTest::ExtensionsToolbarUnitTest() = default;
+ExtensionsToolbarUnitTest::~ExtensionsToolbarUnitTest() = default;
+
 void ExtensionsToolbarUnitTest::SetUp() {
   TestWithBrowserView::SetUp();
 
@@ -55,9 +58,17 @@ void ExtensionsToolbarUnitTest::SetUp() {
       extensions::ExtensionSystem::Get(profile())->extension_service();
 
   permissions_manager_ = PermissionsManager::Get(profile());
+  permissions_helper_ = std::make_unique<SitePermissionsHelper>(profile());
 
   // Shorten delay on animations so tests run faster.
   views::test::ReduceAnimationDuration(extensions_container());
+}
+
+void ExtensionsToolbarUnitTest::TearDown() {
+  // Avoid dangling pointer to profile.
+  permissions_helper_.reset(nullptr);
+
+  TestWithBrowserView::TearDown();
 }
 
 scoped_refptr<const extensions::Extension>
@@ -166,8 +177,7 @@ void ExtensionsToolbarUnitTest::UpdateUserSiteAccess(
     PermissionsManager::UserSiteAccess site_access) {
   extensions::PermissionsManagerWaiter waiter(
       PermissionsManager::Get(browser()->profile()));
-  SitePermissionsHelper(browser()->profile())
-      .UpdateSiteAccess(extension, web_contents, site_access);
+  permissions_helper_->UpdateSiteAccess(extension, web_contents, site_access);
   waiter.WaitForExtensionPermissionsUpdate();
 }
 
@@ -189,6 +199,13 @@ PermissionsManager::UserSiteAccess ExtensionsToolbarUnitTest::GetUserSiteAccess(
     const extensions::Extension& extension,
     const GURL& url) const {
   return permissions_manager_->GetUserSiteAccess(extension, url);
+}
+
+SitePermissionsHelper::SiteInteraction
+ExtensionsToolbarUnitTest::GetSiteInteraction(
+    const extensions::Extension& extension,
+    content::WebContents* web_contents) const {
+  return permissions_helper_->GetSiteInteraction(extension, web_contents);
 }
 
 std::vector<ToolbarActionView*>
