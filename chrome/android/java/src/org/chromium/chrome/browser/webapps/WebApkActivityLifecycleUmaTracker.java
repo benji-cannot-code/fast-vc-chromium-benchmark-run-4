390a424d0963326430ecdf216ef4dd4f52869ba8
John-Wes Appleton <johnwes@google.com>
2023-06-23 18:57:17 +0000

    Added histograms to track UMA for user themes
    Added histograms to log whether a user is using a light theme or a dark
    theme during WebAPK install and launch.
    
    Change-Id: Ib1293ea6a092a9344f1c597845cea51e02756269
    Fixed: 1456444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637606
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: John-Wes Appleton <johnwes@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161880}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkActivityLifecycleUmaTracker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkActivityLifecycleUmaTracker.java
@@ -22,6 +22,7 @@ import org.chromium.chrome.browser.browserservices.intents.WebApkExtras;
 import org.chromium.chrome.browser.browserservices.intents.WebappIntentUtils;
 import org.chromium.chrome.browser.browserservices.metrics.WebApkUkmRecorder;
 import org.chromium.chrome.browser.browserservices.metrics.WebApkUmaRecorder;
+import org.chromium.chrome.browser.browserservices.metrics.WebApkUmaRecorder.WebApkUserTheme;
 import org.chromium.chrome.browser.browserservices.ui.splashscreen.SplashController;
 import org.chromium.chrome.browser.dependency_injection.ActivityScope;
 import org.chromium.chrome.browser.flags.ActivityType;
@@ -30,6 +31,7 @@ import org.chromium.chrome.browser.lifecycle.InflationObserver;
 import org.chromium.chrome.browser.lifecycle.PauseResumeWithNativeObserver;
 import org.chromium.chrome.browser.metrics.ActivityTabStartupMetricsTracker;
 import org.chromium.chrome.browser.metrics.WebApkSplashscreenMetrics;
+import org.chromium.ui.util.ColorUtils;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -107,7 +109,12 @@ public class WebApkActivityLifecycleUmaTracker
     public void onPostInflationStartup() {}
 
     @Override
-    public void onResumeWithNative() {}
+    public void onResumeWithNative() {
+        @WebApkUserTheme
+        int themeSetting = (ColorUtils.inNightMode(mActivity)) ? WebApkUserTheme.DARK_THEME
+                                                               : WebApkUserTheme.LIGHT_THEME;
+        WebApkUmaRecorder.recordUserThemeWhenLaunch(themeSetting);
+    }
 
     @Override
     public void onPauseWithNative() {
