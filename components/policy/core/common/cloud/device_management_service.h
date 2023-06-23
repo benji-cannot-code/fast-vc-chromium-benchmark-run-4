b13b096d7fcf5cde77869a617ed5ea237efb360d
Felipe Andrade <fsandrade@chromium.org>
2023-06-23 11:24:51 +0000

    Remove AD variants from DMServerRequest UMAs
    Clean up DMServerRequest UMA histograms, removing the
    ".ActiveDirectoryEnrollPlayUser" and the ".ActiveDirectoryPlayActivity"
    variants. This is part of the Chromad removal from ChromeOS, which is
    safe because no device should be running in AD mode any longer.
    
    OBSOLETE_HISTOGRAMS=Multiple histograms are been removed, because of the removal of the Active Directory variants
    
    Bug: b:288280781
    Change-Id: If9e0e404e34ecefc73071aaab87f076ab7983dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634658
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161697}

--- components/policy/core/common/cloud/device_management_service.h
+++ components/policy/core/common/cloud/device_management_service.h
@@ -97,7 +97,7 @@ class POLICY_EXPORT DeviceManagementService {
   // possible because some aren't ready during startup. http://crbug.com/302798
   class POLICY_EXPORT Configuration {
    public:
-    virtual ~Configuration() {}
+    virtual ~Configuration() = default;
 
     // Server at which to contact the service (DMServer).
     virtual std::string GetDMServerUrl() const = 0;
@@ -145,7 +145,7 @@ class POLICY_EXPORT DeviceManagementService {
       RETRY_WITH_DELAY
     };
 
-    virtual ~Job() {}
+    virtual ~Job() = default;
   };
 
   class JobImpl;
@@ -186,7 +186,7 @@ class POLICY_EXPORT DeviceManagementService {
     // facilitate reading of logs.)
     // TYPE_INVALID is used only in tests so that they can EXPECT the correct
     // job type has been used.  Otherwise, tests would need to initially set
-    // the type to somehing like TYPE_AUTO_ENROLLMENT, and then it would not
+    // the type to something like TYPE_AUTO_ENROLLMENT, and then it would not
     // be possible to EXPECT the job type in auto enrollment tests.
     enum JobType {
       TYPE_INVALID = -1,
@@ -231,7 +231,7 @@ class POLICY_EXPORT DeviceManagementService {
     // Convert the job type into a string.
     static std::string GetJobTypeAsString(JobType type);
 
-    virtual ~JobConfiguration() {}
+    virtual ~JobConfiguration() = default;
 
     virtual JobType GetType() = 0;
 
