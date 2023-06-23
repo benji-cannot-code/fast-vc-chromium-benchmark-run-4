4ba00bf79de3f58c8401e468e4b460ee6f8f060c
Dana Fried <dfried@chromium.org>
2023-06-23 16:37:14 +0000

    [Test Automation] Allow overriding default widget context calculation
    This makes sense for e.g. ChromeOS, where all OS widgets (system tray, dialogs, etc.) should be a single context, while apps should retain their individual contexts.
    
    It is done through an installable callback because the code that "knows" what special rules need to apply for a particular app or platform only exist downstream from the core Views library.
    
    This should provide a fairly reasonable solution to the attached bug,
    though we still want to also add the requested functionality.
    
    Bug: 1457375
    Change-Id: Id9ce5e7e5394ea9e014b0a1b218d2fb14fbe9397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638676
    Reviewed-by: James Cook <jamescook@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161795}

--- ui/views/interaction/element_tracker_views.h
+++ ui/views/interaction/element_tracker_views.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/functional/callback_forward.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/no_destructor.h"
@@ -51,17 +52,29 @@ class VIEWS_EXPORT ElementTrackerViews {
  public:
   using ViewList = std::vector<View*>;
 
+  // Returns the context to use for `primary_widget`, which will be a logical
+  // top-level widget, or null to use the default computation. Used e.g. to
+  // ensure that all system widgets on ChromeOS share a context.
+  using ContextOverrideCallback =
+      base::RepeatingCallback<ui::ElementContext(Widget* primary_widget)>;
+
+  // Changes the computation of contexts for some or all widgets. See
+  // `ContextOverrideCallback` for details.
+  static void SetContextOverrideCallback(ContextOverrideCallback callback);
+
   // Gets the global instance of the tracker for Views.
   static ElementTrackerViews* GetInstance();
 
   // Returns the context associated with a particular View. The context will be
   // the same across all Views associated with a root Widget (such as an
-  // application window).
+  // application window), or as specified by the current
+  // `ContextOverrideCallback` if one is set.
   static ui::ElementContext GetContextForView(View* view);
 
   // Returns the context associated with a particular Widget. The context will
   // be the same across all Widgets associated with a root Widget (such as an
-  // application window).
+  // application window), or as specified by the current
+  // `ContextOverrideCallback` if one is set.
   static ui::ElementContext GetContextForWidget(Widget* widget);
 
   // ----------
@@ -156,6 +169,9 @@ class VIEWS_EXPORT ElementTrackerViews {
 
   ElementTrackerViews();
   ~ElementTrackerViews();
+
+  // Returns the current `ContextOverrideCallback`, which may be null.
+  static ContextOverrideCallback& GetContextOverrideCallback();
 
   // We do not get notified at the View level if a view's widget has not yet
   // been shown. We need this notification to know when the view is actually
