42b3a4c547d4e9101bf6c4cdd36ca51eebeb42b3
Colin Blundell <blundell@chromium.org>
2023-06-23 08:10:39 +0000

    [//ui][GL] Fold NativePixmapEGLX11BindingHelper into binding class
    No other class now uses the binding helper, so it can be folded directly
    into NavitePixmapEGLX11Binding.
    
    Change-Id: I150787487c1e25b0fdaa8d35a65cdeda87393f73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629291
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161651}

--- ui/ozone/platform/x11/native_pixmap_egl_x11_binding.h
+++ ui/ozone/platform/x11/native_pixmap_egl_x11_binding.h
@@ -9,11 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ui/gfx/native_pixmap.h"
+#include "ui/gfx/x/connection.h"
+#include "ui/gfx/x/glx.h"
 #include "ui/ozone/public/native_pixmap_gl_binding.h"
 
-namespace gl {
-class NativePixmapEGLX11BindingHelper;
-}
+typedef void* EGLSurface;
+typedef void* EGLDisplay;
 
 namespace ui {
 
@@ -21,9 +22,7 @@ namespace ui {
 // within the context of X11.
 class NativePixmapEGLX11Binding : public NativePixmapGLBinding {
  public:
-  explicit NativePixmapEGLX11Binding(
-      std::unique_ptr<gl::NativePixmapEGLX11BindingHelper> binding_helper,
-      gfx::BufferFormat format);
+  explicit NativePixmapEGLX11Binding(gfx::BufferFormat format);
   ~NativePixmapEGLX11Binding() override;
 
   static std::unique_ptr<NativePixmapGLBinding> Create(
@@ -40,11 +39,14 @@ class NativePixmapEGLX11Binding : public NativePixmapGLBinding {
   GLenum GetDataType() override;
 
  private:
+  bool Initialize(x11::Pixmap pixmap);
+
+  // Binds image to texture currently bound to |target|. Returns true on
+  // success.
   bool BindTexture(GLenum target, GLuint texture_id);
 
-  // TODO(crbug.com/1412693): Fold the helper class into this class once
-  // GLImageEGLPixmap no longer exists.
-  std::unique_ptr<gl::NativePixmapEGLX11BindingHelper> binding_helper_;
+  EGLSurface surface_ = nullptr;
+  EGLDisplay display_;
   gfx::BufferFormat format_;
 };
 
