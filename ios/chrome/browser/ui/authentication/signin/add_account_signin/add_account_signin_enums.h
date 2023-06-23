cddbee1a06c9f5c16a3dfd22620265b1d406d494
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-06-23 17:20:50 +0000

    [ios] Fix crash when adding secondary account
    See details in the linked bug. The kAddSecondaryAccount code path
    is reachable regardless of whether there is a primary account.
    Rename and remove the CHECK.
    
    Fixed: 1457546
    Change-Id: I484c52e5be19c93e9f33bb053c3ce74babf10cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638068
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161818}

--- ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_enums.h
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_enums.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Intent for the add account sign-in flow.
 enum class AddAccountSigninIntent {
-  // Adds a secondary account that will only be used for the web. This intent
-  // can only be used when there is a primary account (sign-in or sync).
-  kAddSecondaryAccount,
+  // Adds a new account to the device. This can happen regardless of whether
+  // there is a primary account set.
+  kAddNewAccount,
   // Reauthenticates with the current primary account, using the SSOAuth add
   // account dialog. The sync consent screen will not be presented to the user.
   // This intent can only be used when there is a primary account with sync
--- ios/chrome/browser/ui/authentication/signin/signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/signin_coordinator.mm
@@ -126,7 +126,7 @@ using signin_metrics::PromoAction;
                          browser:browser
                      accessPoint:accessPoint
                      promoAction:PromoAction::PROMO_ACTION_NO_SIGNIN_PROMO
-                    signinIntent:AddAccountSigninIntent::kAddSecondaryAccount];
+                    signinIntent:AddAccountSigninIntent::kAddNewAccount];
 }
 
 + (instancetype)
