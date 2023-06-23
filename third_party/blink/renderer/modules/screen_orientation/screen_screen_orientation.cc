38a64a74fe918b90e78cb96301131c67a23d9cae
Dave Tapuska <dtapuska@chromium.org>
2023-06-23 19:04:05 +0000

    [screen_orientation] Avoid usage of deprecated constructor
    Pass Screen into ScreenScreenOrientation object so we can avoid
    using the deprecated Supplement constructor.
    
    Bug: None
    Change-Id: I2323117c567eefefe44f67f68ba86925e175e950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641150
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161884}

--- third_party/blink/renderer/modules/screen_orientation/screen_screen_orientation.cc
+++ third_party/blink/renderer/modules/screen_orientation/screen_screen_orientation.cc
@@ -17,7 +17,7 @@ ScreenScreenOrientation& ScreenScreenOrientation::From(Screen& screen) {
   ScreenScreenOrientation* supplement =
       Supplement<Screen>::From<ScreenScreenOrientation>(screen);
   if (!supplement) {
-    supplement = MakeGarbageCollected<ScreenScreenOrientation>();
+    supplement = MakeGarbageCollected<ScreenScreenOrientation>(screen);
     ProvideTo(screen, supplement);
   }
   return *supplement;
@@ -39,7 +39,8 @@ ScreenOrientation* ScreenScreenOrientation::orientation(Screen& screen) {
 const char ScreenScreenOrientation::kSupplementName[] =
     "ScreenScreenOrientation";
 
-ScreenScreenOrientation::ScreenScreenOrientation() : Supplement(nullptr) {}
+ScreenScreenOrientation::ScreenScreenOrientation(Screen& screen)
+    : Supplement(screen) {}
 
 void ScreenScreenOrientation::Trace(Visitor* visitor) const {
   visitor->Trace(orientation_);
