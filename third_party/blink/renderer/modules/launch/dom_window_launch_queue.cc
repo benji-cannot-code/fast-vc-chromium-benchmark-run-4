ea492e7e86c16992b9c4a5cf4df9a83b1dba7d10
Dave Tapuska <dtapuska@chromium.org>
2023-06-23 18:35:43 +0000

    [launch] Pass LocalDOMWindow into DOMWindowLaunchQueue
    Do not use the deprecated constructor but pass the LocalDOMWindow
    along.
    
    Bug: None
    Change-Id: I64be9af39aa5890fbed88a18a200cfcd087ac1f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641148
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161866}

--- third_party/blink/renderer/modules/launch/dom_window_launch_queue.cc
+++ third_party/blink/renderer/modules/launch/dom_window_launch_queue.cc
@@ -15,8 +15,8 @@ namespace blink {
 
 const char DOMWindowLaunchQueue::kSupplementName[] = "DOMWindowLaunchQueue";
 
-DOMWindowLaunchQueue::DOMWindowLaunchQueue()
-    : Supplement(nullptr), launch_queue_(MakeGarbageCollected<LaunchQueue>()) {}
+DOMWindowLaunchQueue::DOMWindowLaunchQueue(LocalDOMWindow& window)
+    : Supplement(window), launch_queue_(MakeGarbageCollected<LaunchQueue>()) {}
 
 Member<LaunchQueue> DOMWindowLaunchQueue::launchQueue(LocalDOMWindow& window) {
   return FromState(&window)->launch_queue_;
@@ -45,7 +45,7 @@ DOMWindowLaunchQueue* DOMWindowLaunchQueue::FromState(LocalDOMWindow* window) {
   DOMWindowLaunchQueue* supplement =
       Supplement<LocalDOMWindow>::From<DOMWindowLaunchQueue>(window);
   if (!supplement) {
-    supplement = MakeGarbageCollected<DOMWindowLaunchQueue>();
+    supplement = MakeGarbageCollected<DOMWindowLaunchQueue>(*window);
     ProvideTo(*window, supplement);
   }
   return supplement;
