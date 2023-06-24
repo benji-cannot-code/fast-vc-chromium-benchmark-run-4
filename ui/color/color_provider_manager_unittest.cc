f7d7b53555687da2b901778ac4592398f84dffb9
Sean Kau <skau@chromium.org>
2023-06-24 02:25:25 +0000

    Migrate /ui/color to ColorProviderKey
    ColorProviderManager::Key and its associated types are being
    moved into their own class, ColorProviderKey. As a result,
    class names are rewritten and includes are replaced.
    
    This CL was uploaded by git cl split.
    
    R=tluk@chromium.org
    
    Bug: b:286952053
    Change-Id: I52722d463a4ad5925f10525ce18ec8b56cb7c620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641567
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162048}

--- ui/color/color_provider_manager_unittest.cc
+++ ui/color/color_provider_manager_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/color/color_mixer.h"
 #include "ui/color/color_provider.h"
+#include "ui/color/color_provider_key.h"
 #include "ui/color/color_recipe.h"
 #include "ui/color/color_test_ids.h"
 #include "ui/gfx/color_palette.h"
@@ -34,9 +35,9 @@ class ColorProviderManagerTest : public testing::Test {
 
 ColorProvider* GetLightNormalColorProvider() {
   return ColorProviderManager::GetForTesting().GetColorProviderFor(
-      {ColorProviderManager::ColorMode::kLight,
-       ColorProviderManager::ContrastMode::kNormal, ui::SystemTheme::kDefault,
-       ColorProviderManager::FrameType::kChromium, /*user_color=*/absl::nullopt,
+      {ColorProviderKey::ColorMode::kLight,
+       ColorProviderKey::ContrastMode::kNormal, ui::SystemTheme::kDefault,
+       ColorProviderKey::FrameType::kChromium, /*user_color=*/absl::nullopt,
        /*scheme_variant=*/absl::nullopt, /*is_grayscale=*/false, nullptr});
 }
 
