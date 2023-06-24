882df425fdc3fa5680ca72d0edb54205abca6c03
Morten Stenshorne <mstensho@chromium.org>
2023-06-24 06:55:34 +0000

    Use the block-in-inline layout result when considering breaks.
    The layout result for the line box that's wrapped around the block-
    in-inline isn't very useful when it comes to considering break points.
    In order to get access to any block break token inside, for instance,
    we need the layout result and fragment of the anonymous block, not the
    line box. We were making an incorrect decision to break before an
    element in MovePastBreakpoint() because of this bug. Since we failed
    to realize that the child had actually broken inside (since we didn't
    see the break tokens that were actually there), we'd instead check if
    the child was taller than the remaining space - which it was. So we
    ended up breaking before at a last-resort break point, instead of
    using the breakpoint that we had already found inside. This is a rather
    bizarre situation, and in this case it's caused by underlying bugs.
    But at least we will no longer freeze / crash, with this CL.
    
    We should also be able to remove code added by CL:4181113 from
    NGInlineLayoutAlgorithm::PlaceBlockInInline() (setting the
    break appeal there manually should no longer be necessary), now that we
    use the "right" layout result for fragmentation considerations. But
    since this CL is likely to be merged to older versions, let's keep it
    as safe and merge-friendly as possible.
    
    NGBoxFragmentBuilder::AddResult() should also call the new function
    LayoutResultForPropagation() instead of doing the work on its own, but
    for the same reason as above, leave it alone for now.
    
    Bug: 1453643
    Change-Id: Iaca8269d46eeaa7dc222298d77873a9c051f71c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638061
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162074}

--- third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.cc
+++ third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.cc
@@ -23,6 +23,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+const NGLayoutResult& NGBoxFragmentBuilder::LayoutResultForPropagation(
+    const NGLayoutResult& layout_result) const {
+  if (layout_result.Status() != NGLayoutResult::kSuccess) {
+    return layout_result;
+  }
+  const auto& fragment = layout_result.PhysicalFragment();
+  if (fragment.IsBox()) {
+    return layout_result;
+  }
+
+  const NGPhysicalLineBoxFragment* line =
+      DynamicTo<NGPhysicalLineBoxFragment>(&fragment);
+  if (!line || !line->IsBlockInInline() || !items_builder_) {
+    return layout_result;
+  }
+
+  const NGLogicalLineItems& line_items =
+      items_builder_->LogicalLineItems(*line);
+  DCHECK(line_items.BlockInInlineLayoutResult());
+  return *line_items.BlockInInlineLayoutResult();
+}
+
 void NGBoxFragmentBuilder::AddBreakBeforeChild(
     NGLayoutInputNode child,
     absl::optional<NGBreakAppeal> appeal,
