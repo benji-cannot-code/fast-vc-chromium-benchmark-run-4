bccd843bda4de346d41cf11b49e674f54f4ab138
Jimmy <jimmyxgong@chromium.org>
2023-06-24 00:06:25 +0000

    Reland "shortcuts: Deprecate Ctrl+Alt+/ for show shortcut viewer"
    This is a reland of commit 3cdd1da9983118499d21133911625daa3e161cb5
    
    Fix addressed: Linus ChromiumOS MSan builds are flaky for spokenfeedback
    browser_tests. Disable the test for MSan builds only.
    
    Original change's description:
    > shortcuts: Deprecate Ctrl+Alt+/ for show shortcut viewer
    >
    > Shift + Search + s is the new shortcut to open the shortcut app.
    > This allows web apps to now consume Ctrl+Alt+/ for their own
    > target handlers.
    > This is part of the process to deprecate all non-search based system
    > accelerators.
    >
    > This CL also updates the deprecation notification message.
    >
    > Screenshots:
    > screenshot.googleplex.com/VeotYoBEF2BjCDX
    > https://screenshot.googleplex.com/6AScLFHqF7CwZ55
    >
    > Bug: b/216049298, b/288313762
    > Change-Id: Ia1225dc8a748348f6a19f24eb7558611840c338f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602855
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: David Padlipsky <dpad@google.com>
    > Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1161493}
    
    Bug: b/216049298, b/288313762
    Change-Id: I9a7bee9e54fe99393ef4e6e16a10ed01269ffbf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641249
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1162019}

--- ash/accelerators/ash_accelerator_configuration_unittest.cc
+++ ash/accelerators/ash_accelerator_configuration_unittest.cc
@@ -187,7 +187,7 @@ TEST_F(AshAcceleratorConfigurationTest, DeprecatedAccelerators) {
   const DeprecatedAcceleratorData deprecated_data[] = {
       {AcceleratorAction::kShowTaskManager,
        /*uma_histogram_name=*/"deprecated.showTaskManager",
-       /*notification_message_id=*/1, /*old_shortcut_id=*/1,
+       /*notification_message_id=*/1,
        /*new_shortcut_id=*/2, /*deprecated_enabled=*/true},
   };
 
@@ -265,7 +265,7 @@ TEST_F(AshAcceleratorConfigurationTest,
   const DeprecatedAcceleratorData deprecated_data[] = {
       {AcceleratorAction::kShowTaskManager,
        /*uma_histogram_name=*/"deprecated.showTaskManager",
-       /*notification_message_id=*/1, /*old_shortcut_id=*/1,
+       /*notification_message_id=*/1,
        /*new_shortcut_id=*/2, /*deprecated_enabled=*/true},
   };
 
@@ -961,7 +961,7 @@ TEST_F(AshAcceleratorConfigurationTest, AddAcceleratorDeprecatedConflict) {
   const DeprecatedAcceleratorData deprecated_data[] = {
       {AcceleratorAction::kShowTaskManager,
        /*uma_histogram_name=*/"deprecated.showTaskManager",
-       /*notification_message_id=*/1, /*old_shortcut_id=*/1,
+       /*notification_message_id=*/1,
        /*new_shortcut_id=*/2, /*deprecated_enabled=*/true},
   };
 
