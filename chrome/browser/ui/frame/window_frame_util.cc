d648d5e38ab1e73a3fbe1820274a68b334b3d569
David Bienvenu <davidbienvenu@chromium.org>
2023-06-24 17:34:29 +0000

    win: Remove 10Glass from Windows10Glass function and var names
    This removes references to Windows10 and Glass, from various
    function names, since we no longer support Win 7/8, and Glass
    is just assumed now.
    
    Change-Id: Ie93505df5122b6fa7eb0df76491664ee094a1b4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641314
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1162115}

--- chrome/browser/ui/frame/window_frame_util.cc
+++ chrome/browser/ui/frame/window_frame_util.cc
@@ -14,25 +14,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_WIN)
 
 // static
-SkAlpha WindowFrameUtil::CalculateWindows10GlassCaptionButtonBackgroundAlpha(
+SkAlpha WindowFrameUtil::CalculateWindowsCaptionButtonBackgroundAlpha(
     SkAlpha theme_alpha) {
   return theme_alpha == SK_AlphaOPAQUE ? 0xCC : theme_alpha;
 }
 
 // static
-gfx::Size WindowFrameUtil::GetWindows10GlassCaptionButtonAreaSize() {
+gfx::Size WindowFrameUtil::GetWindowsCaptionButtonAreaSize() {
   // TODO(crbug.com/1257470): Fix uses of this to dynamically compute the size
-  // of the glass caption button area.
+  // of the caption button area.
   constexpr int kNumButtons = 3;
 
-  return gfx::Size(
-      (kNumButtons * kWindows10GlassCaptionButtonWidth) +
-          ((kNumButtons - 1) * kWindows10GlassCaptionButtonVisualSpacing),
-      kWindows10GlassCaptionButtonHeightRestored);
+  return gfx::Size((kNumButtons * kWindowsCaptionButtonWidth) +
+                       ((kNumButtons - 1) * kWindowsCaptionButtonVisualSpacing),
+                   kWindowsCaptionButtonHeightRestored);
 }
 
 // static
-bool WindowFrameUtil::IsWin10TabSearchCaptionButtonEnabled(
+bool WindowFrameUtil::IsWindowsTabSearchCaptionButtonEnabled(
     const Browser* browser) {
 #if BUILDFLAG(IS_WIN)
   return browser->is_type_normal();
