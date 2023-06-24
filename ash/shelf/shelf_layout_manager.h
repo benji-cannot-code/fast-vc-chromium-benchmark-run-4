9b840b3e2c75755530a19f9f89332744c9763c88
Andre Le <leandre@chromium.org>
2023-06-24 00:09:25 +0000

    QsRevamp: Fix popup baseline when shelf is auto hide
    The shelf auto hide logic in SetBaselineOffset is not needed anymore
    with the recent logic. Thus, we can remove it to avoid the below bug. To
    ensure that everything works correct, this CL also modify
    AshMessagePopupCollectionTest.AutoHide to add some coverage.
    
    Fixed: b:287532325
    Change-Id: Ibab539971d5ef5bf101904f2a32eb075f598f754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641060
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162022}

--- ash/shelf/shelf_layout_manager.h
+++ ash/shelf/shelf_layout_manager.h
@@ -389,6 +389,7 @@ class ASH_EXPORT ShelfLayoutManager : public AppListControllerObserver,
       const gfx::Insets& in_session_shelf_insets);
 
   class UpdateShelfObserver;
+  friend class AshMessagePopupCollectionTest;
   friend class DimShelfLayoutManagerTestBase;
   friend class NotificationTrayTest;
   friend class PanelLayoutManagerTest;
