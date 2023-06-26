14e938c826c6b0742d098278a4746ff6beb6bbd0
Clemens Backes <clemensb@chromium.org>
2023-06-26 10:39:45 +0000

    [wasm] Add crash key for the BytesConsumer debug name
    The |BytesConsumer| seems to return an inconsistent public state. Store
    the debug name to debug the linked issue.
    
    R=yoavweiss@chromium.org
    
    Bug: chromium:1449546
    Change-Id: Id228d59e35dd038222cc92db32f3625d692e9ebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4640242
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162326}

--- third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.h"
 
+#include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_functions.h"
@@ -257,6 +258,12 @@ class FetchDataLoaderForWasmStreaming final : public FetchDataLoader,
               AbortCompilation("Download cancelled");
               break;
             case BytesConsumer::PublicState::kErrored:
+              // Store the debug name of the BytesConsumer to debug
+              // https://crbug.com/1449546.
+              // TODO(clemensb): Remove this after resolving the issue.
+              static crash_reporter::CrashKeyString<128> detail_key(
+                  "v8-wasm-bytes-consumer-debug-name");
+              detail_key.Set(consumer_->DebugName().Utf8().c_str());
               AbortCompilation("Network error: " +
                                consumer_->GetError().Message());
               break;
