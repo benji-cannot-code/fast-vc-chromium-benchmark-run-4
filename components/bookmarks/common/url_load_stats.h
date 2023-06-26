6493db1b8bc4ade0323b05290be982e501356fec
Stephan Hartmann <stha09@googlemail.com>
2023-06-26 08:46:26 +0000

    IWYU: add cstddef for size_t in bookmarks::UrlLoadStats
    Bug: 957519
    Change-Id: Ic58b8b6adc001f0931c2d1d74b7ac1ac3014fbc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4644168
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162286}

--- components/bookmarks/common/url_load_stats.h
+++ components/bookmarks/common/url_load_stats.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_BOOKMARKS_COMMON_URL_LOAD_STATS_H_
 #define COMPONENTS_BOOKMARKS_COMMON_URL_LOAD_STATS_H_
 
+#include <cstddef>
 #include <cstdint>
 #include <vector>
 
