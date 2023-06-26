cf50305d9258c5171596cac294162294c105b1ce
Serena Pascual <srna@google.com>
2023-06-26 23:41:33 +0000

    CSC: Add HandleClassificationTest
    Bug: 1458084
    Change-Id: I5cde78d253f7c5d816e52d946ec1239b34f7072f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4646223
    Commit-Queue: Serena Pascual <srna@google.com>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Auto-Submit: Serena Pascual <srna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1162679}

--- chrome/browser/companion/visual_search/visual_search_classifier_host_unittest.cc
+++ chrome/browser/companion/visual_search/visual_search_classifier_host_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/companion/visual_search/features.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/companion/visual_search.mojom.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/optimization_guide/core/test_model_info_builder.h"
 #include "components/optimization_guide/core/test_optimization_guide_model_provider.h"
@@ -48,6 +49,12 @@ base::FilePath model_file_path() {
       .AppendASCII("test-model-quantized.tflite");
 }
 
+const SkBitmap create_bitmap(int width, int height, int r, int g, int b) {
+  SkBitmap bitmap;
+  bitmap.allocN32Pixels(width, height);
+  bitmap.eraseARGB(255, r, g, b);
+  return bitmap;
+}
 }  // namespace
 
 class VisualSearchClassifierHostTest : public ChromeRenderViewHostTestHarness {
@@ -164,6 +171,24 @@ TEST_F(VisualSearchClassifierHostTest, StartClassification_WithCancellation) {
                                       true, 1);
   histogram_tester_.ExpectBucketCount("Companion.VisualSearch.MismatchURL",
                                       true, 1);
+}
+
+TEST_F(VisualSearchClassifierHostTest, HandleClassification) {
+  SetModelPath();
+  VisualSearchClassifierHost::ResultCallback callback = base::BindOnce(
+      [](std::vector<std::string> results) { EXPECT_EQ(results.size(), 1U); });
+  visual_search_host_->StartClassification(
+      web_contents()->GetPrimaryMainFrame(), url_, std::move(callback));
+  std::vector<mojom::VisualSearchSuggestionPtr> results;
+  SkBitmap result = create_bitmap(1000, 1000, 128, 128, 255);
+  results.emplace_back(mojom::VisualSearchSuggestion::New(result));
+
+  visual_search_host_->HandleClassification(std::move(results));
+  base::RunLoop().RunUntilIdle();
+  histogram_tester_.ExpectBucketCount(
+      "Companion.VisualSearch.ClassificationResultsSize", true, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Companion.VisualSearch.EndClassificationSuccess", 0);
 }
 
 }  // namespace companion::visual_search
