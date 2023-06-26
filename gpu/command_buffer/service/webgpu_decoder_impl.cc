a41c36c180ec939355e5ac27110195337f22ca40
Jiawei Shao <jiawei.shao@intel.com>
2023-06-26 18:37:22 +0000

    WebGPU: Fix the choice of power preference
    This patch uses LowPower as the default power preference and the power
    preference for battery power to match the above comment "if on battery
    power, default to the integrated GPU".
    
    Bug: dawn:1875
    Change-Id: Ia22dc37bfed9530d27b7c97e0618e9c3ce668e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641674
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162524}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -1492,9 +1492,9 @@ WGPUAdapter WebGPUDecoderImpl::CreatePreferredAdapter(
         // If on battery power, default to the integrated GPU.
         if (!base::PowerMonitor::IsInitialized() ||
             base::PowerMonitor::IsOnBatteryPower()) {
-          power_preference = WGPUPowerPreference_HighPerformance;
-        } else {
           power_preference = WGPUPowerPreference_LowPower;
+        } else {
+          power_preference = WGPUPowerPreference_HighPerformance;
         }
       }
       break;
