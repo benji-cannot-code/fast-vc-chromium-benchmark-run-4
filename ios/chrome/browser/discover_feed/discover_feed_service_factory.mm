bf8b3a2bb75063ccd2e63094a13bbf71c9fbe184
Ed Chin <edchin@google.com>
2023-06-26 20:36:22 +0000

    [ios] Do not start DiscoverFeedService on ACR if not already started
    App Close Refresh experiment showed huge increase in CUI#1 AppFlows,
    specifically launch start events. The reason for this is that
    the DiscoverFeedService is often not started (e.g., when the current
    tab on a cold start is non-NTP). If the service was not previously
    started, ACR causes it to be instantiated, thereby triggering a
    Discover launch event when the app is backgrounding. Future work will
    need to change the triggering of launch events. But this CL is enough
    to stop the ACR LE from artificially causing AppFlow count increases.
    
    Change-Id: I699989c5b682fe95040d7933b3c7525c646354fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4633902
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162589}

--- ios/chrome/browser/discover_feed/discover_feed_service_factory.mm
+++ ios/chrome/browser/discover_feed/discover_feed_service_factory.mm
@@ -23,9 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // static
 DiscoverFeedService* DiscoverFeedServiceFactory::GetForBrowserState(
-    ChromeBrowserState* browser_state) {
+    ChromeBrowserState* browser_state,
+    bool create) {
   return static_cast<DiscoverFeedService*>(
-      GetInstance()->GetServiceForBrowserState(browser_state, true));
+      GetInstance()->GetServiceForBrowserState(browser_state, create));
 }
 
 // static
