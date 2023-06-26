2a456a2e365bfcf79c9c66d65bb34aa932d4de3c
Angela Xiao <angelaxiao@chromium.org>
2023-06-26 03:16:42 +0000

    Scalable Notifiers: wire HelpAppWelcomeTips flag to Help app
    Add the boolean value of `AreHelpAppWelcomeTipsEnabled()` to the
    LoadTimeData values used in the Help app.
    
    Bug: b/286126077
    Change-Id: I9a76a1aa7e1b703902d4004c1b0d62f1369406d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642459
    Reviewed-by: Callistus Tan <callistus@google.com>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/main@{#1162237}

--- chrome/browser/ash/web_applications/help_app/help_app_untrusted_ui_config.cc
+++ chrome/browser/ash/web_applications/help_app/help_app_untrusted_ui_config.cc
@@ -93,6 +93,8 @@ void PopulateLoadTimeData(content::WebUI* web_ui,
   source->AddBoolean(
       "HelpAppSearchServiceIntegration",
       base::FeatureList::IsEnabled(features::kEnableLocalSearchService));
+  source->AddBoolean("HelpAppWelcomeTips",
+                     ash::features::AreHelpAppWelcomeTipsEnabled());
   source->AddBoolean("isCloudGamingDevice",
                      chromeos::features::IsCloudGamingDeviceEnabled());
   source->AddBoolean("jelly", chromeos::features::IsJellyEnabled());
