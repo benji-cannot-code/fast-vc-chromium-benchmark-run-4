d8698e0411fd851ec0ce3c10f8d812429e488f5f
Sammie Quon <sammiequon@chromium.org>
2023-06-26 22:43:24 +0000

    float: Do not show tablet multitask menu in kiosk mode
    Test: ash_unittests TabletModeMultitaskMenuTest.NotShownInKioskMode
    Bug: b/288241745
    Change-Id: I278ce2b119264e503e26bf63d266a3872d24cd18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634570
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162646}

--- ash/wm/tablet_mode/tablet_mode_multitask_menu_controller.cc
+++ ash/wm/tablet_mode/tablet_mode_multitask_menu_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/tablet_mode/tablet_mode_multitask_menu_controller.h"
 
 #include "ash/accelerators/debug_commands.h"
+#include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_multitask_cue_controller.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "base/functional/bind.h"
+#include "chromeos/components/kiosk/kiosk_utils.h"
 #include "ui/events/event.h"
 #include "ui/events/event_target.h"
 #include "ui/events/types/event_type.h"
@@ -65,6 +67,12 @@ TabletModeMultitaskMenuController::~TabletModeMultitaskMenuController() {
 
 // static
 bool TabletModeMultitaskMenuController::CanShowMenu(aura::Window* window) {
+  // Cannot show the menu in the lock screen, or in app/kiosk mode.
+  if (Shell::Get()->session_controller()->IsScreenLocked() ||
+      chromeos::IsKioskSession()) {
+    return false;
+  }
+
   auto* window_state = WindowState::Get(window);
   return window_state && window_state->CanMaximize() &&
          window_state->CanResize() && !window_state->IsFloated() &&
