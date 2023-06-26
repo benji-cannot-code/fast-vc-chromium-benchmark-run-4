36179662f2d0b0592ee3ca2602af8e764f2e1f42
Mohannad Farrag <aymanm@google.com>
2023-06-26 12:30:58 +0000

    Clean-up quic_test_server resources on tear-down.
    * In order to batch any test that use QuicTestServer, it was required to clear up the resources during shut-down and set them to null(to pass DCHECK).
    * Make StartQuicTestServer/ShutDownQuicTestServer synchronised.
    * Batch BidirectionalStreamQuicTest.
    * Fix documentation of EngineStartup methods, they were swapped.
    
    Bug: 1432569
    Change-Id: If8bba5c18df103a9e91df9473d8b3118bfc02c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637052
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1162348}

--- components/cronet/android/test/src/org/chromium/net/QuicTestServer.java
+++ components/cronet/android/test/src/org/chromium/net/QuicTestServer.java
@@ -6,11 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.net;
 
 import android.content.Context;
-import android.os.ConditionVariable;
 
 import org.chromium.base.ContextUtils;
-import org.chromium.base.Log;
-import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.test.util.UrlUtils;
@@ -20,7 +17,6 @@ import org.chromium.base.test.util.UrlUtils;
  */
 @JNINamespace("cronet")
 public final class QuicTestServer {
-    private static final ConditionVariable sBlock = new ConditionVariable();
     private static final String TAG = QuicTestServer.class.getSimpleName();
 
     private static final String CERT_USED = "quic-chain.pem";
@@ -39,8 +35,6 @@ public final class QuicTestServer {
         TestFilesInstaller.installIfNeeded(context);
         QuicTestServerJni.get().startQuicTestServer(
                 TestFilesInstaller.getInstalledPath(context), UrlUtils.getIsolatedTestRoot());
-        sBlock.block();
-        sBlock.close();
         sServerRunning = true;
     }
 
@@ -80,15 +74,18 @@ public final class QuicTestServer {
         return MockCertVerifier.createMockCertVerifier(CERTS_USED, true);
     }
 
-    @CalledByNative
-    private static void onServerStarted() {
-        Log.i(TAG, "Quic server started.");
-        sBlock.open();
-    }
-
     @NativeMethods("cronet_tests")
     interface Natives {
+        /*
+         * Runs a quic test server synchronously.
+         */
         void startQuicTestServer(String filePath, String testDataDir);
+        /*
+         * Shutdowns the quic test-server synchronously.
+         *
+         * Calling this without calling startQuicTestServer first will lead to unexpected
+         * behavior if not compiled in debug mode.
+         */
         void shutdownQuicTestServer();
         int getServerPort();
     }
