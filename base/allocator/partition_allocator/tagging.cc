550441e22a7b976d21c2ab3772cd69cfa5ddcdce
Nathan Pratta Teodosio <nathan.teodosio@canonical.com>
2023-06-26 09:43:07 +0000

    Don't assume the existence of HWCAP2_MTE.
    Some ARMv8 machines fail to build with "error: use of undeclared identifier
    'HWCAP2_MTE'"
    
    According to the kernel documentation[1],
    
    > ARMv8.5 based processors introduce the Memory Tagging Extension (MTE) feature.
    >
    > When CONFIG_ARM64_MTE is selected and Memory Tagging Extension is supported by
    > the hardware, the kernel advertises the feature to userspace via HWCAP2_MTE.
    
    [1] https://www.kernel.org/doc/html/latest/arm64/memory-tagging-extension.html#userspace-support
    
    Bug: 1454438
    Change-Id: I2d300a22cb13e7446d8b62428bb8e982eedeb7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4610745
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162311}

--- base/allocator/partition_allocator/tagging.cc
+++ base/allocator/partition_allocator/tagging.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PR_MTE_TCF_MASK (3UL << PR_MTE_TCF_SHIFT)
 #define PR_MTE_TAG_SHIFT 3
 #define PR_MTE_TAG_MASK (0xffffUL << PR_MTE_TAG_SHIFT)
+#define HWCAP2_MTE (1 << 18)
 #endif
 #endif
 
