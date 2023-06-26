e4942113cb9dfbf1a74567d8facbbb9eebe4fe5a
Rakina Zata Amni <rakina@chromium.org>
2023-06-26 05:38:52 +0000

    Update some tests to expect RenderFrameHost swap on same-site subframe navigations
    With RenderDocument, cross-document navigations will use new
    RenderFrameHosts even on same-site subframe navigations. This CL updates some
    tests that didn't expect the RenderFrameHost change.
    
    Bug: 936696
    Change-Id: Ib2c6e598d2afd2492921859007ee008711b3fa17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632737
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162255}

--- content/public/test/test_utils.cc
+++ content/public/test/test_utils.cc
@@ -228,6 +228,10 @@ bool WillSameSiteNavigationsChangeRenderFrameHosts() {
   return ShouldCreateNewHostForAllFrames();
 }
 
+bool WillSameSiteSubframeNavigationsChangeRenderFrameHosts() {
+  return ShouldCreateNewHostForSameSiteSubframe();
+}
+
 bool CanSameSiteMainFrameNavigationsChangeSiteInstances() {
   return IsProactivelySwapBrowsingInstanceOnSameSiteNavigationEnabled() ||
          IsBackForwardCacheEnabled();
