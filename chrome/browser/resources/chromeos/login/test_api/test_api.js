fffe9b97e862f23f41be91e98224ffdc7e7e79d8
Bohdan Tyshchenko <bohdanty@google.com>
2023-06-26 13:45:37 +0000

    [OOBE] - Extend test api with logic to fetch shown screen's name/step
    Add getCurrentScreenName and getCurrentStep functions that could be
    used to debug errors during the OOBE.
    
    Example usage on the Welcome Screen:
    OobeAPI.getCurrentScreenName() -> 'connect'.
    OobeAPI.getCurrentScreenStep() -> 'greeting'.
    
    Bug: b:270549056
    Change-Id: Id77bd548be840a69d9d334236185a9373bc9208b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4631448
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1162371}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -8,6 +8,8 @@ import {loadTimeData} from '//resources/ash/common/load_time_data.m.js';
 import {$} from '//resources/ash/common/util.js';
 import {afterNextRender} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {Oobe} from '../../cr_ui.js';
+
 /**
  * @fileoverview Common testing utils methods used for OOBE tast tests.
  */
@@ -995,6 +997,18 @@ export class OobeApiProvider {
 
     this.getBrowseAsGuestButtonName = function() {
       return loadTimeData.getString('testapi_browseAsGuest');
+    };
+
+    this.getCurrentScreenName = function() {
+      return Oobe.getInstance().currentScreen.id.trim();
+    };
+
+    this.getCurrentScreenStep = function() {
+      const step = Oobe.getInstance().currentScreen.getAttribute('multistep');
+      if (step === null) {
+        return 'default';
+      }
+      return step.trim();
     };
   }
 }
