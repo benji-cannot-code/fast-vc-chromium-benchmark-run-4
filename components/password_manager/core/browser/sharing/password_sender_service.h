63f6993ca6b7644be09ebdc2c1030b3a16cd02bf
markusheintz <markusheintz@chromium.org>
2023-06-26 13:59:45 +0000

    Adds skeleton of the PasswordSenderService impl.
    Bug: 1455407
    Change-Id: Ie50340aeb3d874b512810419197579c8e2e45c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632542
    Commit-Queue: Markus Heintz <markusheintz@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162378}

--- components/password_manager/core/browser/sharing/password_sender_service.h
+++ components/password_manager/core/browser/sharing/password_sender_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_PASSWORD_SENDER_SERVICE_H_
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_PASSWORD_SENDER_SERVICE_H_
 
+#include "components/keyed_service/core/keyed_service.h"
 #include "components/password_manager/core/browser/ui/credential_ui_entry.h"
 
 namespace password_manager {
@@ -20,12 +21,13 @@ struct PasswordRecipient {
 };
 
 // The PasswordSenderService class defines the interface for sending passwords.
-class PasswordSenderService {
+class PasswordSenderService : public KeyedService {
  public:
   PasswordSenderService() = default;
+  ~PasswordSenderService() override = default;
+
   PasswordSenderService(const PasswordSenderService&) = delete;
   PasswordSenderService& operator=(const PasswordSenderService&) = delete;
-  virtual ~PasswordSenderService() = default;
 
   // Sends password entries for the given `credential_ui_entry` to the specified
   // `recipient`.
