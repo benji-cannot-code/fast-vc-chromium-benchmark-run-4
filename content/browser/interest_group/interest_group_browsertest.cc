dca74c625772de4c4026bf951781f045d80f2891
Meredith Lane <meredithl@chromium.org>
2023-06-26 01:53:41 +0000

    Revert "FLEDGE: Make our handling of promises more consistent with the Web"
    This reverts commit 0d88cdad387c9a23efb621084076c6ff0a485d96.
    
    Reason for revert: Suspected culprit of failures on Linux bots.
    
    B=1457670
    
    Original change's description:
    > FLEDGE: Make our handling of promises more consistent with the Web
    >
    > Using Promise<T> in WebIDL will auto-wrap things that are not already promises; and unlike our current code works properly with other then-able.
    >
    > The downside is that auctionSignals/sellerSignals with 'then' fields will break, and it's somewhat less efficient.
    >
    > Change-Id: Ib08fe8fb042c1fe67d302ab5c4108000be87b5c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619514
    > Reviewed-by: Russ Hamilton <behamilton@google.com>
    > Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1161875}
    
    Change-Id: I56fcef3c2a5be6ac7fb74902e37c8bf77c4ad438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4644150
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Meredith Lane <meredithl@chromium.org>
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162225}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -4318,19 +4318,15 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': auctionSignals for AuctionAdConfig with seller "
-      "'https://test.com' must be a JSON-serializable object.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "auctionSignals for AuctionAdConfig with seller 'https://test.com' must "
+      "be a JSON-serializable object.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       auctionSignals: alert
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -4421,19 +4417,15 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': sellerSignals for AuctionAdConfig with seller "
-      "'https://test.com' must be a JSON-serializable object.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "sellerSignals for AuctionAdConfig with seller 'https://test.com' must "
+      "be a JSON-serializable object.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       sellerSignals: function() {}
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -4603,20 +4595,15 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': perBuyerSignals buyer 'https://invalid^&' for "
-      "AuctionAdConfig with seller 'https://test.com' must be a valid https "
-      "origin.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "perBuyerSignals buyer 'https://invalid^&' for AuctionAdConfig with "
+      "seller 'https://test.com' must be a valid https origin.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       perBuyerSignals: {'https://invalid^&': {a:1}}
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -4707,20 +4694,16 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': perBuyerTimeouts buyer 'https://invalid^&' for "
-      "AuctionAdConfig with seller 'https://test.com' must be \"*\" (wildcard) "
-      "or a valid https origin.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "perBuyerTimeouts buyer 'https://invalid^&' for AuctionAdConfig with "
+      "seller 'https://test.com' must be \"*\" (wildcard) or a valid https "
+      "origin.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       perBuyerTimeouts: {'https://invalid^&': 100}
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -4813,19 +4796,17 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': perBuyerCumulativeTimeouts buyer "
-      "'https://invalid^&' for AuctionAdConfig with seller 'https://test.com' "
-      "must be \"*\" (wildcard) or a valid https origin.");
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "perBuyerCumulativeTimeouts buyer 'https://invalid^&' for "
+      "AuctionAdConfig "
+      "with seller 'https://test.com' must be \"*\" (wildcard) or a valid "
+      "https origin.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       perBuyerCumulativeTimeouts: {'https://invalid^&': 100}
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -4834,20 +4815,17 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': perBuyerCurrencies buyer 'https://invalid^&' for "
-      "AuctionAdConfig with seller 'https://test.com' must be \"*\" (wildcard) "
-      "or a valid https origin.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "perBuyerCurrencies buyer 'https://invalid^&' for "
+      "AuctionAdConfig "
+      "with seller 'https://test.com' must be \"*\" (wildcard) or a valid "
+      "https origin.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       perBuyerCurrencies: {'https://invalid^&': 'USD'}
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -4856,20 +4834,15 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': perBuyerCurrencies currency 'usd' for "
-      "AuctionAdConfig with seller 'https://test.com' must be a 3-letter "
-      "uppercase currency code.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator':"
+      " perBuyerCurrencies currency 'usd' for AuctionAdConfig with seller"
+      " 'https://test.com' must be a 3-letter uppercase currency code.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       perBuyerCurrencies: {'*': 'usd'}
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -5052,19 +5025,15 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': perBuyerSignals for AuctionAdConfig with seller "
-      "'https://test.com' must be a JSON-serializable object.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "perBuyerSignals for AuctionAdConfig with seller 'https://test.com' "
+      "must be a JSON-serializable object.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       perBuyerSignals: {'https://test.com': function() {}}
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -5194,20 +5163,15 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': directFromSellerSignals 'https://invalid^&' for "
-      "AuctionAdConfig with seller 'https://test.com' cannot be resolved to a "
-      "valid URL.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'https://invalid^&' for AuctionAdConfig with "
+      "seller 'https://test.com' cannot be resolved to a valid URL.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       directFromSellerSignals: 'https://invalid^&'
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -5216,19 +5180,16 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': directFromSellerSignals 'http://test.com/signals' "
-      "for AuctionAdConfig with seller 'https://test.com' must match seller "
-      "origin; only https scheme is supported.");
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'http://test.com/signals' for AuctionAdConfig "
+      "with seller 'https://test.com' must match seller origin; only https "
+      "scheme is supported.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       directFromSellerSignals: 'http://test.com/signals'
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -5237,20 +5198,16 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': directFromSellerSignals 'https://test2.com/signals' "
-      "for AuctionAdConfig with seller 'https://test.com' must match seller "
-      "origin; only https scheme is supported.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'https://test2.com/signals' for AuctionAdConfig "
+      "with seller 'https://test.com' must match seller origin; only https "
+      "scheme is supported.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       directFromSellerSignals: 'https://test2.com/signals'
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
@@ -5260,20 +5217,16 @@ IN_PROC_BROWSER_TEST_F(
   ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
   AttachInterestGroupObserver();
 
-  WebContentsConsoleObserver console_observer(shell()->web_contents());
-  console_observer.SetPattern(
-      "Uncaught (in promise) TypeError: Failed to execute 'runAdAuction' on "
-      "'NavigatorAuction': directFromSellerSignals "
-      "'https://test.com/signals?shouldntBeHere' for AuctionAdConfig with "
-      "seller 'https://test.com' URL prefix must not have a query string.");
-
-  EXPECT_EQ("Promise argument rejected or resolved to invalid value.",
-            RunAuctionAndWait(R"({
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'https://test.com/signals?shouldntBeHere' for "
+      "AuctionAdConfig with seller 'https://test.com' URL prefix must not have "
+      "a query string.",
+      RunAuctionAndWait(R"({
       seller: 'https://test.com',
       decisionLogicUrl: 'https://test.com',
       directFromSellerSignals: 'https://test.com/signals?shouldntBeHere'
   })"));
-  EXPECT_TRUE(console_observer.Wait());
   WaitForAccessObserved({});
 }
 
