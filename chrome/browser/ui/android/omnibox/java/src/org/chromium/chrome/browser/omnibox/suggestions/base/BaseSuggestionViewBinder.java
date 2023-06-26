ae8e609f38d49a17f80f97c5bfec68b0d27330d7
Patrick Noland <pnoland@google.com>
2023-06-26 18:22:40 +0000

    [Omnibox] Reset cached dimens on Autocomplete startup
    We restart the activity if density changes, so this is a suitable proxy
    and won't waste computation in the common case. This prevents a bug
    where we cache pixel offsets calculated using a stale dpToPX multiplier,
    causing us to use incorrect margins.
    
    Bug: 1455337
    Change-Id: I3ad92c56af795c17aab54096f81ed868164280f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641528
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162514}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinder.java
@@ -355,6 +355,10 @@ public final class BaseSuggestionViewBinder<T extends View>
         view.setLayoutParams(layoutParams);
     }
 
+    public static void resetCachedDimensions() {
+        sDimensionsInitialized = false;
+    }
+
     @VisibleForTesting
     static void initializeDimensions(Context context) {
         boolean showModernizeVisualUpdate =
