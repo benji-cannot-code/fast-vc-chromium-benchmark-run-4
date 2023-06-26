9fd33a1fd0ea562e458b18a0905dc3fb50eb6617
Sean Kau <skau@chromium.org>
2023-06-26 22:39:55 +0000

    Remap header and base colors to new options
    cros.sys.header* and cros.sys.app-base* had undesired values in the
    Expressive palette. Update them to work better.
    
    Introduces 2 new ref colors ref.secondary12 and ref.neutral8.
    
    Bug: b:288333472
    Change-Id: I9d774142bfd6d7b3e4a49ae265b2328f6c1f9a6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632400
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162642}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefPrimary100) \
   E_CPONLY(kColorRefSecondary0) \
   E_CPONLY(kColorRefSecondary10) \
+  E_CPONLY(kColorRefSecondary12) \
   E_CPONLY(kColorRefSecondary15) \
   E_CPONLY(kColorRefSecondary20) \
   E_CPONLY(kColorRefSecondary25) \
@@ -73,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefNeutral0) \
   E_CPONLY(kColorRefNeutral4) \
   E_CPONLY(kColorRefNeutral6) \
+  E_CPONLY(kColorRefNeutral8) \
   E_CPONLY(kColorRefNeutral10) \
   E_CPONLY(kColorRefNeutral12) \
   E_CPONLY(kColorRefNeutral15) \
--- ui/color/ref_color_mixer.cc
+++ ui/color/ref_color_mixer.cc
@@ -43,6 +43,7 @@ void AddBaselinePalette(ColorProvider* provider) {
 
   mixer[kColorRefSecondary0] = {SkColorSetRGB(0x00, 0x00, 0x00)};
   mixer[kColorRefSecondary10] = {SkColorSetRGB(0x00, 0x1D, 0x35)};
+  mixer[kColorRefSecondary12] = {SkColorSetRGB(0x00, 0x22, 0x38)};
   mixer[kColorRefSecondary15] = {SkColorSetRGB(0x00, 0x28, 0x45)};
   mixer[kColorRefSecondary20] = {SkColorSetRGB(0x00, 0x33, 0x55)};
   mixer[kColorRefSecondary25] = {SkColorSetRGB(0x00, 0x3f, 0x66)};
@@ -89,6 +90,7 @@ void AddBaselinePalette(ColorProvider* provider) {
   mixer[kColorRefNeutral0] = {SkColorSetRGB(0x00, 0x00, 0x00)};
   mixer[kColorRefNeutral4] = {SkColorSetRGB(0x0E, 0x0E, 0x0F)};
   mixer[kColorRefNeutral6] = {SkColorSetRGB(0x13, 0x13, 0x14)};
+  mixer[kColorRefNeutral8] = {SkColorSetRGB(0x16, 0x18, 0x18)};
   mixer[kColorRefNeutral10] = {SkColorSetRGB(0x1F, 0x1F, 0x1F)};
   mixer[kColorRefNeutral12] = {SkColorSetRGB(0x1F, 0x20, 0x20)};
   mixer[kColorRefNeutral15] = {SkColorSetRGB(0x28, 0x28, 0x28)};
@@ -154,6 +156,7 @@ void AddGeneratedPalette(ColorProvider* provider,
 
   mixer[kColorRefSecondary0] = {palette->secondary().get(0)};
   mixer[kColorRefSecondary10] = {palette->secondary().get(10)};
+  mixer[kColorRefSecondary12] = {palette->secondary().get(12)};
   mixer[kColorRefSecondary15] = {palette->secondary().get(15)};
   mixer[kColorRefSecondary20] = {palette->secondary().get(20)};
   mixer[kColorRefSecondary25] = {palette->secondary().get(25)};
@@ -200,6 +203,7 @@ void AddGeneratedPalette(ColorProvider* provider,
   mixer[kColorRefNeutral0] = {palette->neutral().get(0)};
   mixer[kColorRefNeutral4] = {palette->neutral().get(4)};
   mixer[kColorRefNeutral6] = {palette->neutral().get(6)};
+  mixer[kColorRefNeutral8] = {palette->neutral().get(8)};
   mixer[kColorRefNeutral10] = {palette->neutral().get(10)};
   mixer[kColorRefNeutral12] = {palette->neutral().get(12)};
   mixer[kColorRefNeutral15] = {palette->neutral().get(15)};
