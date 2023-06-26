520a1be33f31270f8264907fc54db3291f612048
Meredith Lane <meredithl@chromium.org>
2023-06-26 02:07:38 +0000

    Revert "Relayout the launcher image search section."
    This reverts commit 8b8dddadaf795d41c540fdb10a2d322331d624f0.
    
    Reason for revert: Suspected culprit for MSan failures.
    Flakiness dashboard: https://luci-analysis.appspot.com/p/chromium/clusters?selectedMetrics=human-cls-failed-presubmit%2Ccritical-failures-exonerated%2Cfailures&orderBy=critical-failures-exonerated&orderDir=desc&interval=24h&q=SearchResultImageViewTest
    
    Failures started from https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/36654/blamelist
    
    Original change's description:
    > Relayout the launcher image search section.
    >
    > As the new layout spec is out for launcher image search, this cl
    > updates the layout according to the spec.
    > Spec: crbug/1352636#c14
    >
    > Bug: 1352636
    > Change-Id: I2cd2bff62f8cd4441747d82f2cd9e493e2a57e83
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4560244
    > Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1161975}
    
    Bug: 1352636
    Change-Id: I579f1f711428073e6c50c946dacd09346e44c1ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4644868
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Owners-Override: Meredith Lane <meredithl@chromium.org>
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1162226}

--- ash/app_list/views/app_list_search_view_unittest.cc
+++ ash/app_list/views/app_list_search_view_unittest.cc
@@ -376,6 +376,7 @@ TEST_P(SearchResultImageViewTest, ShowContextMenu) {
       views::IsViewClass<SearchResultImageView>(search_result_image_view));
 
   // Perform a long tap on `search_result_image_view`.
+  search_result_image_view->GetWidget()->LayoutRootViewIfNecessary();
   auto image_view_center_point =
       search_result_image_view->GetBoundsInScreen().CenterPoint();
   auto* event_generator = GetEventGenerator();
@@ -421,6 +422,7 @@ TEST_P(SearchResultImageViewTest, ActivateImageResult) {
       views::IsViewClass<SearchResultImageView>(search_result_image_view));
 
   // Click/Tap on `search_result_image_view`.
+  search_result_image_view->GetWidget()->LayoutRootViewIfNecessary();
   if (tablet_mode()) {
     GestureTapOn(search_result_image_view);
   } else {
