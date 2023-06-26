09c12ee73cb81fcb9d5eaf9c67c74c256230def9
Avi Drissman <avi@chromium.org>
2023-06-26 23:04:31 +0000

    Convert content_browsertests to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This CL landed earlier as https://crrev.com/c/4507593. This had to
    be reverted due to issues with gfx::AcceleratedWidget on iOS.
    This is fixed with https://crrev.com/c/4629669 so this can land.
    
    Bug: 1280317
    Change-Id: Ic7431f06d00e9f7f61d90e40544738d4d9f1fd23
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622073
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162659}

--- content/browser/accessibility/hit_testing_mac_browsertest.mm
+++ content/browser/accessibility/hit_testing_mac_browsertest.mm
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell.h"
 #include "ui/gfx/mac/coordinate_conversion.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace content {
 
 #define EXPECT_ACCESSIBILITY_MAC_HIT_TEST_RESULT(css_point, expected_element, \
--- content/browser/site_per_process_mac_browsertest.mm
+++ content/browser/site_per_process_mac_browsertest.mm
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/ocmock_extensions.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace content {
 
 namespace {
