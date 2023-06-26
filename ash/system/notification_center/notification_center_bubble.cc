971a232dab1285925cb2c95bbaa1dd0742dd5f64
Sylvie Liu <sylvieliu@chromium.org>
2023-06-26 23:56:54 +0000

    Changed the NotificationCenterBubble corner radius to 24px for Jelly
    Before the change, both the NotificationListView and the
    NotificationBubbleView has a corner radius of 16px. This CL changed the
    bubble view's corner radius to be 24px for Jelly.
    
    Screenshot:
    https://screenshot.googleplex.com/7xzJaBCExs6pX5q
    
    Bug: b:285024389
    Change-Id: I5893b492e7b62bd44bdf2fd2d92c57525d134931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642262
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162681}

--- ash/system/notification_center/notification_center_bubble.cc
+++ ash/system/notification_center/notification_center_bubble.cc
@@ -14,15 +14,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_bubble_wrapper.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_utils.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
+
+namespace {
+
+constexpr int kNotificationCenterBubbleCornerRadius = 24;
+
+}  // namespace
 
 NotificationCenterBubble::NotificationCenterBubble(
     NotificationCenterTray* notification_center_tray)
     : notification_center_tray_(notification_center_tray) {
   auto init_params = CreateInitParamsForTrayBubble(
       /*tray=*/notification_center_tray_, /*anchor_to_shelf_corner=*/true);
+  // For Jelly: the bubble corner radius should be 24px instead of 16px.
+  if (chromeos::features::IsJellyEnabled()) {
+    init_params.corner_radius = kNotificationCenterBubbleCornerRadius;
+  }
 
   // Create and customize bubble view.
   bubble_view_ = std::make_unique<TrayBubbleView>(init_params);
