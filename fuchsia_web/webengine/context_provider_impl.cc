0cca1d15ad0a88e0a2eb56d6056456870068c6f7
David Dorwin <ddorwin@chromium.org>
2023-06-26 22:18:50 +0000

    [fuchsia] Make Context Provider launch relative web_instance.cm
    `context_provider.cm` now launches the `web_instance.cm` in the same
    package rather than using the absolute component URL. Other users of
    WebInstanceHost continue to use the absolute URL.
    
    Fixed: 1443083
    Bug: 1010222, 1443560
    Change-Id: Iafaec43ef889a39606cac12f417349c2c3c64d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4513901
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162630}

--- fuchsia_web/webengine/context_provider_impl.cc
+++ fuchsia_web/webengine/context_provider_impl.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ContextProviderImpl::ContextProviderImpl(
     sys::OutgoingDirectory& outgoing_directory)
-    : web_instance_host_(outgoing_directory) {}
+    : web_instance_host_(outgoing_directory,
+                         /*is_web_instance_component_in_same_package=*/true) {}
 
 ContextProviderImpl::~ContextProviderImpl() = default;
 
--- fuchsia_web/webinstance_host/web_instance_host.h
+++ fuchsia_web/webinstance_host/web_instance_host.h
@@ -80,7 +80,8 @@ class WebInstanceHost {
   // TODO(crbug.com/1327587): Remove `outgoing_directory` if and when it is
   // possible for tests to serve a test-specific outgoing directory via
   // base::TestComponentContextForProcess on a separate thread.
-  explicit WebInstanceHost(sys::OutgoingDirectory& outgoing_directory);
+  WebInstanceHost(sys::OutgoingDirectory& outgoing_directory,
+                  bool is_web_instance_component_in_same_package);
 
   // Creates a new web_instance Component using `instance_component_url` and
   // connects |services_request| to it. Returns ZX_OK if `params` were valid,
@@ -135,6 +136,11 @@ class WebInstanceHost {
   // directory.
   fidl::InterfaceHandle<fuchsia::io::Directory> tmp_dir_;
 
+  // Whether `web_instance.cm` is in the same Package as this host Component.
+  // TODO(crbug.com/1255292): Determine this based on a static Structured
+  // Configuration value once Structured Configuration is supported.
+  const bool is_web_instance_component_in_same_package_;
+
   SEQUENCE_CHECKER(sequence_checker_);
 };
 
@@ -142,8 +148,9 @@ class WebInstanceHost {
 // provided to each `WebInstance`.
 class WebInstanceHostWithServicesFromThisComponent : public WebInstanceHost {
  public:
-  explicit WebInstanceHostWithServicesFromThisComponent(
-      sys::OutgoingDirectory& outgoing_directory);
+  WebInstanceHostWithServicesFromThisComponent(
+      sys::OutgoingDirectory& outgoing_directory,
+      bool is_web_instance_component_in_same_package);
 
   zx_status_t CreateInstanceForContextWithCopiedArgs(
       fuchsia::web::CreateContextParams params,
@@ -156,8 +163,9 @@ class WebInstanceHostWithServicesFromThisComponent : public WebInstanceHost {
 // `CreateInstanceForContextWithCopiedArgs()`.
 class WebInstanceHostWithoutServices : public WebInstanceHost {
  public:
-  explicit WebInstanceHostWithoutServices(
-      sys::OutgoingDirectory& outgoing_directory);
+  WebInstanceHostWithoutServices(
+      sys::OutgoingDirectory& outgoing_directory,
+      bool is_web_instance_component_in_same_package);
 
   zx_status_t CreateInstanceForContextWithCopiedArgs(
       fuchsia::web::CreateContextParams params,
