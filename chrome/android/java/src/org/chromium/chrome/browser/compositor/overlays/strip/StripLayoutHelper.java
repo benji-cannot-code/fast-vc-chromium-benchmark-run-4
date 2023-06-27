7f12d340aed32f496a957175db3dfd8ef053c35d
Gurmeet Kalra <gurmeetk@google.com>
2023-06-27 17:05:27 +0000

    DnD: Adds support for moving a tab to another instance of Chrome window using drag and drop
    The feature is behind a TAB_DRAG_DROP_ANDROID feature flag which is set to false by default.
    
    The tab is dragged and dropped only on the strip layout area of the toolbar container for the tab being dragged to move. The moved tab will be added to the end of the tabs list.
    
    Add method to MultiInstanceManagerApi31 to move the specified tab to current Chrome window instance.
    
    Bug: b/276804108
    Change-Id: Ic919a497d4c1f8b6240063c08be5bdafe29c2f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617334
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gurmeet Kalra <gurmeetk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163040}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -256,6 +256,7 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
     private MultiInstanceManager mMultiInstanceManager;
     private View mToolbarContainerView;
     private StripLayoutTab mActiveClickedTab;
+    private TabDropTarget mTabDropTarget;
 
     /**
      * Creates an instance of the {@link StripLayoutHelper}.
@@ -437,6 +438,7 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
      */
     public void destroy() {
         mStripTabEventHandler.removeCallbacksAndMessages(null);
+        mTabDropTarget = null;
     }
 
     /**
@@ -3131,12 +3133,18 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
         return mActiveClickedTab;
     }
 
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    TabDropTarget getTabDropTarget() {
+        return mTabDropTarget;
+    }
+
     protected void prepareForDragDrop() {
         if (!MultiWindowUtils.isMultiInstanceApi31Enabled()) return;
         if (!ChromeFeatureList.sTabDragDropAndroid.isEnabled()) return;
 
+        mTabDropTarget = new TabDropTarget();
         TabDragSource.getInstance().prepareForDragDrop(
-                mToolbarContainerView, mMultiInstanceManager);
+                mToolbarContainerView, mMultiInstanceManager, mTabDropTarget);
     }
 
     @VisibleForTesting
