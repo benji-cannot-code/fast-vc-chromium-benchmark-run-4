04106290ef810e5ef1d50bafe8a41b3875ac020a
Zelin Liu <zelin@chromium.org>
2023-06-27 16:52:44 +0000

    [code health] Remove histogram Media.Session.Pause, Media.Session.Stop
    This CL removes the following histograms:
      - Media.Session.Pause (expired as of 2021-12-05)
      - Media.Session.Stop (expired as of 2022-02-06)
    Also removes unreferenced code.
    
    Bug: 1447223
    Change-Id: Iecf7238700f166ace6afc6eb06cc9165fc8b1171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638558
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163027}

--- components/browser_ui/media/android/java/src/org/chromium/components/browser_ui/media/MediaSessionHelper.java
+++ components/browser_ui/media/android/java/src/org/chromium/components/browser_ui/media/MediaSessionHelper.java
@@ -103,9 +103,6 @@ public class MediaSessionHelper implements MediaImageCallback {
         public void onPause(int actionSource) {
             if (isNotificationHidingOrHidden()) return;
 
-            MediaSessionUma.recordPause(
-                    MediaSessionHelper.convertMediaActionSourceToUMA(actionSource));
-
             if (mMediaSessionObserver.getMediaSession() == null) return;
 
             mMediaSessionObserver.getMediaSession().suspend();
@@ -114,9 +111,6 @@ public class MediaSessionHelper implements MediaImageCallback {
         @Override
         public void onStop(int actionSource) {
             if (isNotificationHidingOrHidden()) return;
-
-            MediaSessionUma.recordStop(
-                    MediaSessionHelper.convertMediaActionSourceToUMA(actionSource));
 
             if (mMediaSessionObserver.getMediaSession() != null) {
                 mMediaSessionObserver.getMediaSession().stop();
