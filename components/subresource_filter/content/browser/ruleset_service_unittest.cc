95505bdb6e605380122f6fa71181992340c0d20d
Alan Screen <awscreen@chromium.org>
2023-06-27 00:21:02 +0000

    [CodeHealth] Drop SubresourceFilter.WriteRuleset.ReplaceFileError
    Remove histogram which has long since expired.
    
    Bug: 1448114
    Change-Id: I1fe7426f914b254d8291c043120661e80a85068d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641252
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162692}

--- components/subresource_filter/content/browser/ruleset_service_unittest.cc
+++ components/subresource_filter/content/browser/ruleset_service_unittest.cc
@@ -777,8 +777,6 @@ TEST_F(SubresourceFilteringRulesetServiceTest, NewRuleset_Persisted) {
       "SubresourceFilter.IndexRuleset.WallDuration", 1);
   histogram_tester.ExpectUniqueSample(
       "SubresourceFilter.IndexRuleset.NumUnsupportedRules", 0, 1);
-  histogram_tester.ExpectTotalCount(
-      "SubresourceFilter.WriteRuleset.ReplaceFileError", 0);
   histogram_tester.ExpectUniqueSample(
       "SubresourceFilter.WriteRuleset.Result",
       static_cast<int>(RulesetService::IndexAndWriteRulesetResult::SUCCESS), 1);
@@ -990,9 +988,6 @@ TEST_F(SubresourceFilteringRulesetServiceTest, NewRuleset_WriteFailure) {
       "SubresourceFilter.IndexRuleset.WallDuration", 1);
   histogram_tester.ExpectUniqueSample(
       "SubresourceFilter.IndexRuleset.NumUnsupportedRules", 0, 1);
-  base::File::Error expected_error = base::File::FILE_ERROR_NOT_FOUND;
-  histogram_tester.ExpectUniqueSample(
-      "SubresourceFilter.WriteRuleset.ReplaceFileError", -expected_error, 1);
   histogram_tester.ExpectUniqueSample(
       "SubresourceFilter.WriteRuleset.Result",
       static_cast<int>(IndexAndWriteRulesetResult::FAILED_REPLACE_FILE), 1);
