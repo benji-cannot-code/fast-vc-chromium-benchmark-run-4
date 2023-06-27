9cff24960e8ab5163d200e7244a088adc6fe700e
Andrea Gomez <andreaxg@google.com>
2023-06-27 20:53:35 +0000

    [Settings] Removed detailed desktop language settings flag
    Since the detailed redesign of the language settings has launched, the
    desktop-detailed-language-settings is no longer used. This CL removes
    the flag and references to it in languages.ts.
    
    Bug: 1455177
    Change-Id: I37381659cfbcd6952acf1282e3699c41d566c51a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619089
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163190}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8711,11 +8711,6 @@ const FeatureEntry kFeatureEntries[] = {
         kOsWin | kOsLinux | kOsLacros | kOsMac | kOsFuchsia,
         FEATURE_VALUE_TYPE(features::kUIDebugTools),
     },
-    {"desktop-detailed-language-settings",
-     flag_descriptions::kDesktopDetailedLanguageSettingsName,
-     flag_descriptions::kDesktopDetailedLanguageSettingsDescription,
-     kOsWin | kOsLinux | kOsLacros | kOsMac | kOsFuchsia,
-     FEATURE_VALUE_TYPE(language::kDesktopDetailedLanguageSettings)},
 
     {"sync-poll-immediately-on-every-startup",
      flag_descriptions::kSyncPollImmediatelyOnEveryStartupName,
--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -323,12 +323,6 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
         ->FetchPriceEmailPref();
   }
 
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
-  html_source->AddBoolean(
-      "enableDesktopDetailedLanguageSettings",
-      base::FeatureList::IsEnabled(language::kDesktopDetailedLanguageSettings));
-#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   html_source->AddBoolean(
       "userCannotManuallyEnterPassword",
