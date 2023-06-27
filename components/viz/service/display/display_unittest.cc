073d0553381c8e0da042dddab47fb2bcd443115f
Edgar Arriaga <edgararriaga@google.com>
2023-06-27 18:05:07 +0000

    Fix to avoid non perfect cadences from being throttled
    Throttling non perfect cadences, such as 60 HZ screen refresh
    rate over 24 FPS cause throttling to lower the framerate so
    restrict throttling to only apply only when the refresh rate is
    a multiple of the desired throttle.
    
    Fix for throttling wrongfully throttling when refresh rate matches video fps
    
    When the refresh rate matches the video framerate and in some other
    cases when getting to the actual vsync where it should schedule
    a vsync there is numerical error that happens almost always that
    cause an extra vsync to be throttled. This is particularly bad
    as the refresh rate is lower as the missed vsync represents
    a significant downgrade of framerate.
    
    Bug: b/284356269
    Change-Id: I5185a3d18476a5c8d475a25463c36001d2288c13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4633852
    Auto-Submit: Edgar Arriaga García <edgararriaga@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163093}

--- components/viz/service/display/display_unittest.cc
+++ components/viz/service/display/display_unittest.cc
@@ -236,7 +236,7 @@ class DisplayTest : public testing::Test {
 
   bool ShouldSendBeginFrame(CompositorFrameSinkSupport* support,
                             base::TimeTicks frame_time) {
-    return support->ShouldSendBeginFrame(frame_time);
+    return support->ShouldSendBeginFrame(frame_time, base::Seconds(0));
   }
 
   void UpdateBeginFrameTime(CompositorFrameSinkSupport* support,
