bdd92c07444d5c8f28f3004761dcfc079366316f
Alexander Cooper <alcooper@chromium.org>
2023-06-27 21:17:59 +0000

    Fix Framebuffer scaling on Android OpenXR
    When using Shared Buffer mode OpenXR does not take into account the fact
    that the page may apply a framebuffer scale to the texture that it
    operates on within the page. This can result in a mismatch in sizes
    on the shared buffer, which results in the framebuffer being incomplete
    and operations failing (this is caught by a DCHECK in debug builds).
    The error specifically notes that this is because the textures are of
    different sizes.
    
    This change renames the existing `FrameSize` variables to
    `SwapchainImageSize` to better represent their usage and meaning as the
    size of the textures that we have received from the OpenXR runtime. It
    further adds a set of `TransferSize` variables used for keeping track of
    the texture size expectations from the page, as well as a
    `shared_texture_size` variable on SwapChainInfo to note the current size
    of the buffer.
    
    This change leverages these renames, as well as newly listening to the
    UpdateLayerBounds message from the page, to ensure that we keep an
    appropriately synced SharedBuffer size by checking the size of the
    SharedBuffer before we send it to the page. This allows existing
    SharedBuffers to be unmodified while potential in use and to be modified
    before they are needed again, mimicking the pattern established with
    XrImageTransportBase::TransferFrame.
    
    Note that this change does not address the D3D11 side of things since
    SharedBuffers are more seldomnly used there than in Android where they
    are currently the only path.
    
    There is a little bit of extra plumbing to get the SharedImageInterface
    to the GraphicsBinding in the "BeginFrame" case; but this will hopefully
    be resolved more cleanly after Issue 1454935 is addressed.
    
    Finally, adds a DefaultFramebufferScale value consistent with other
    Android VR runtimes, while leaving the Windows version of OpenXR
    with it's current behavior of not specifying one.
    
    Bug: 1458256
    Change-Id: Ib3f88af93d46e853af79a943f5fafdeb72a5589e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647933
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163211}

--- device/vr/windows/compositor_base.cc
+++ device/vr/windows/compositor_base.cc
@@ -49,7 +49,7 @@ void XRDeviceAbstraction::HandleDeviceLost() {}
 bool XRDeviceAbstraction::HasSessionEnded() {
   return false;
 }
-void XRDeviceAbstraction::OnLayerBoundsChanged() {}
+void XRDeviceAbstraction::OnLayerBoundsChanged(const gfx::Size& frame_size) {}
 device::mojom::XREnvironmentBlendMode
 XRDeviceAbstraction::GetEnvironmentBlendMode(
     device::mojom::XRSessionMode session_mode) {
@@ -223,7 +223,7 @@ void XRCompositorCommon::UpdateLayerBounds(int16_t frame_id,
 
   source_size_ = source_size;
 
-  OnLayerBoundsChanged();
+  OnLayerBoundsChanged(source_size_);
 }
 
 void XRCompositorCommon::RequestSession(
