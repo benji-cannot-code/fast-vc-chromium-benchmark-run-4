f1c2cb33ea01e687d1d36e15e85421d96ce76a78
Justin Huang <justinhuang@google.com>
2023-06-27 03:58:28 +0000

    Revert "Add metrics to confirm traffic difference between CSBRR and hit report"
    This reverts commit d320106256ac5e8f1512f0846c01f0ec0c8f4345.
    
    Reason for revert: Causes msan failures (e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/36726/overview) because hit_report->is_subresource is uninitialized in [here](https://source.chromium.org/chromium/chromium/src/+/main:components/safe_browsing/core/browser/ping_manager_unittest.cc;l=557?q=ping_manager_unittest.cc:557) 
    
    Original change's description:
    > Add metrics to confirm traffic difference between CSBRR and hit report
    >
    > The metrics added last week (https://crrev.com/c/4611266) show some
    > evidence on the traffic difference between ClientSafeBrowsingReport
    > and hit report:
    >   * For mainframe warnings, around 20% of reports are not sent because
    >     there is already a report being collected.
    >   * For subresource warnings, 90% of reports are not sent because
    >     there is already a report being collected.
    > Now we have a good explanation for subresource warnings, but there is
    > still a gap for mainframe warnings. Add more metrics to investigate
    > further.
    >
    > Histograms added:
    > 1 interstitial.CloseReason
    > This is helpful to understand the proportion of reports skipped because
    > users close the entire tab.
    >
    > 2 SafeBrowsing.ClientSafeBrowsingReport.HasThreatDetailsForTab.SecurityInterstitial
    > This is a breakdown of the original histogram because I realize the
    > result is diluted by other types of reports (mainly ads sample).
    >
    > 3 SafeBrowsing.ClientSafeBrowsingReport.HasThreatDetailsInContainer.SecurityInterstitial
    > This is to double check that as long as the collector is found, the
    > threat detail job is not null. In theory, it should always be not null.
    >
    > 4 SafeBrowsing.HitReport.IsSubresource
    > This is to track the total volume of hit reports from the client-side
    > and the break down between mainframe and subresource reports.
    >
    > Bug: 1315455
    > Change-Id: I83b235975d32b86130d5bec58d5b1ff801d19f81
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4639923
    > Reviewed-by: Chris Thompson <cthomp@chromium.org>
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1162671}
    
    Bug: 1315455
    Change-Id: I0d7672d2975908f9d8df33ba0190e7b98b4f596e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4648251
    Commit-Queue: Justin Huang <justinhuang@google.com>
    Owners-Override: Justin Huang <justinhuang@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1162769}

--- components/security_interstitials/content/security_interstitial_tab_helper.cc
+++ components/security_interstitials/content/security_interstitial_tab_helper.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/security_interstitials/content/security_interstitial_tab_helper.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "components/security_interstitials/content/security_interstitial_page.h"
 #include "components/security_interstitials/core/controller_client.h"
@@ -26,8 +25,6 @@ void SecurityInterstitialTabHelper::DidFinishNavigation(
 
   if (navigation_handle->HasCommitted()) {
     if (blocking_page_for_currently_committed_navigation_) {
-      base::UmaHistogramEnumeration("interstitial.CloseReason",
-                                    InterstitialCloseReason::NAVIGATE_AWAY);
       blocking_page_for_currently_committed_navigation_
           ->OnInterstitialClosing();
     }
@@ -36,12 +33,6 @@ void SecurityInterstitialTabHelper::DidFinishNavigation(
       blocking_page_for_currently_committed_navigation_.reset();
     } else {
       blocking_page_for_currently_committed_navigation_ = std::move(it->second);
-      // According to `IsDisplayingInterstitial`,  inserting a value into
-      // `blocking_page_for_currently_committed_navigation_` means an
-      // interstitial is displaying, so log the INTERSTITIAL_SHOWN bucket here.
-      base::UmaHistogramEnumeration(
-          "interstitial.CloseReason",
-          InterstitialCloseReason::INTERSTITIAL_SHOWN);
     }
   }
 
@@ -55,8 +46,6 @@ void SecurityInterstitialTabHelper::DidFinishNavigation(
 
 void SecurityInterstitialTabHelper::WebContentsDestroyed() {
   if (blocking_page_for_currently_committed_navigation_) {
-    base::UmaHistogramEnumeration("interstitial.CloseReason",
-                                  InterstitialCloseReason::CLOSE_TAB);
     blocking_page_for_currently_committed_navigation_->OnInterstitialClosing();
   }
 }
