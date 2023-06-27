ddfaf3f615fea3af86c898021c75c3599d8c0794
Alan Cutter <alancutter@chromium.org>
2023-06-27 06:04:39 +0000

    [dPWA] Use FakeWebAppProvider in WebAppProtocolHandlerManagerTest
    This CL updates WebAppProtocolHandlerManagerTest to use
    FakeWebAppProvider instead of calling SetSubsystems(). This is in
    preparation for replacing SetSubsystems() with SetProvider():
    https://chromium-review.googlesource.com/c/chromium/src/+/4634804
    
    Bug: b/283816014
    Change-Id: I6fdd3fca9cd2158c6942ebd0888d78738c5260cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647509
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162791}

--- chrome/browser/web_applications/os_integration/web_app_protocol_handler_manager_unittest.cc
+++ chrome/browser/web_applications/os_integration/web_app_protocol_handler_manager_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/os_integration/web_app_protocol_handler_manager.h"
 #include "base/strings/escape.h"
+#include "chrome/browser/web_applications/test/fake_os_integration_manager.h"
 #include "chrome/browser/web_applications/test/fake_web_app_provider.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/test/web_app_test.h"
@@ -24,17 +25,19 @@ class WebAppProtocolHandlerManagerTest : public WebAppTest {
     WebAppTest::SetUp();
 
     provider_ = FakeWebAppProvider::Get(profile());
+    provider_->SetOsIntegrationManager(
+        std::make_unique<FakeOsIntegrationManager>(
+            profile(), /*app_shortcut_manager=*/nullptr,
+            /*file_handler_manager=*/nullptr,
+            std::make_unique<WebAppProtocolHandlerManager>(profile()),
+            /*url_handler_manager=*/nullptr));
     test::AwaitStartWebAppProviderAndSubsystems(profile());
-
-    // This is not a WebAppProvider subsystem, so this can be
-    // set after the WebAppProvider has been initialized.
-    protocol_handler_manager_ =
-        std::make_unique<WebAppProtocolHandlerManager>(profile());
-    protocol_handler_manager_->SetSubsystems(&app_registrar());
   }
 
   WebAppProtocolHandlerManager& protocol_handler_manager() {
-    return *protocol_handler_manager_;
+    return provider()
+        .os_integration_manager()
+        .protocol_handler_manager_for_testing();
   }
 
   WebAppProvider& provider() { return *provider_; }
@@ -76,8 +79,6 @@ class WebAppProtocolHandlerManagerTest : public WebAppTest {
 
  private:
   raw_ptr<FakeWebAppProvider, DanglingUntriaged> provider_;
-
-  std::unique_ptr<WebAppProtocolHandlerManager> protocol_handler_manager_;
 };
 
 TEST_F(WebAppProtocolHandlerManagerTest, GetAppProtocolHandlerInfos) {
