57decb651dba581918c3daa28d4b83d9e9825b07
Alan Screen <awscreen@chromium.org>
2023-06-27 00:48:09 +0000

    [CodeHealth] Drop ChromeOS.DeviceSettings.FeatureFlagsMigration
    Remove histogram which has long since expired.  Also remove the
    associated enum FeatureFlagsMigrationStatus which was only used related
    to this histogram.
    
    Fixed: 1448158
    Change-Id: I86a8d00fc42e9e76bb647be05452699eb85a2522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637588
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162705}

--- chrome/browser/ash/ownership/owner_settings_service_ash.cc
+++ chrome/browser/ash/ownership/owner_settings_service_ash.cc
@@ -845,9 +845,6 @@ void OwnerSettingsServiceAsh::MigrateFeatureFlags(
   DCHECK(IsOwner() || IsOwnerInTests(user_id_));
 
   if (settings->feature_flags().switches_size() == 0) {
-    base::UmaHistogramEnumeration(
-        "ChromeOS.DeviceSettings.FeatureFlagsMigration",
-        FeatureFlagsMigrationStatus::kNoFeatureFlags);
     return;
   }
 
@@ -857,9 +854,6 @@ void OwnerSettingsServiceAsh::MigrateFeatureFlags(
     // does the most probable explanation is that we already migrated, so get
     // rid of the raw switches.
     feature_flags->clear_switches();
-    base::UmaHistogramEnumeration(
-        "ChromeOS.DeviceSettings.FeatureFlagsMigration",
-        FeatureFlagsMigrationStatus::kAlreadyMigrated);
     return;
   }
 
@@ -869,9 +863,6 @@ void OwnerSettingsServiceAsh::MigrateFeatureFlags(
     feature_flags->add_feature_flags(flag);
   }
   feature_flags->clear_switches();
-  base::UmaHistogramEnumeration(
-      "ChromeOS.DeviceSettings.FeatureFlagsMigration",
-      FeatureFlagsMigrationStatus::kMigrationPerformed);
 }
 
 }  // namespace ash
