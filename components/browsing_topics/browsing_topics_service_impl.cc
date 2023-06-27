8b46fa2c598fb080368491938f99c24cf0c7596e
Takashi Toyoshima <toyoshim@chromium.org>
2023-06-27 02:11:55 +0000

    Prerender UKM: CHECK to ensure the frame is not under prerendering
    Topics API is gated for prerendered page now, and these code should not
    be reachable while prerendering.
    
    We can revisit this when the API supports prerendering to take
    required UKMs.
    
    Bug: 1245014
    Change-Id: Id3d220c4aeebe884813bd250fd14a44df636483c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4643149
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162729}

--- components/browsing_topics/browsing_topics_service_impl.cc
+++ components/browsing_topics/browsing_topics_service_impl.cc
@@ -158,6 +158,8 @@ void RecordBrowsingTopicsApiResultMetrics(ApiAccessResult result,
     return;
   }
 
+  CHECK(!main_frame->IsInLifecycleState(
+      content::RenderFrameHost::LifecycleState::kPrerendering));
   ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
   ukm::builders::BrowsingTopics_DocumentBrowsingTopicsApiResult2 builder(
       main_frame->GetPageUkmSourceId());
@@ -169,6 +171,8 @@ void RecordBrowsingTopicsApiResultMetrics(ApiAccessResult result,
 void RecordBrowsingTopicsApiResultMetrics(
     const std::vector<CandidateTopic>& valid_candidate_topics,
     content::RenderFrameHost* main_frame) {
+  CHECK(!main_frame->IsInLifecycleState(
+      content::RenderFrameHost::LifecycleState::kPrerendering));
   ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
   ukm::builders::BrowsingTopics_DocumentBrowsingTopicsApiResult2 builder(
       main_frame->GetPageUkmSourceId());
