9ed5d83bc6c26ad8368bc749a3991decd4bccb2a
Yao Li <yaohuali@google.com>
2023-06-27 00:49:18 +0000

    arc: Conditionally start ArcKeyMintBridge or ArcKeymasterBridge
    In any session, only start either of ArcKeyMintBridge or
    ArcKeymasterBridge.
    
    Bug: b:247941366
    Test: With KeyMint features off, Keymaster can start normally
    Change-Id: I13bc2799bf51aaa0cc002271181d706ec2c42307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637765
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Commit-Queue: Yao Li <yaohuali@google.com>
    Cr-Commit-Position: refs/heads/main@{#1162707}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/intent_helper/arc_settings_service.h"
 #include "chrome/browser/ash/arc/intent_helper/chrome_arc_intent_helper_delegate.h"
 #include "chrome/browser/ash/arc/keymaster/arc_keymaster_bridge.h"
+#include "chrome/browser/ash/arc/keymint/arc_keymint_bridge.h"
 #include "chrome/browser/ash/arc/kiosk/arc_kiosk_bridge.h"
 #include "chrome/browser/ash/arc/metrics/arc_metrics_service_proxy.h"
 #include "chrome/browser/ash/arc/nearby_share/arc_nearby_share_bridge.h"
@@ -260,7 +261,11 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
         std::make_unique<ArcIconCacheDelegateProvider>(intent_helper);
   }
   ArcKeyboardShortcutBridge::GetForBrowserContext(profile);
-  ArcKeymasterBridge::GetForBrowserContext(profile);
+  if (ShouldUseArcKeyMint()) {
+    ArcKeyMintBridge::GetForBrowserContext(profile);
+  } else {
+    ArcKeymasterBridge::GetForBrowserContext(profile);
+  }
   ArcKioskBridge::GetForBrowserContext(profile);
   ArcLockScreenBridge::GetForBrowserContext(profile);
   ArcMediaSessionBridge::GetForBrowserContext(profile);
@@ -441,7 +446,11 @@ void ArcServiceLauncher::EnsureFactoriesBuilt() {
   ArcInitialOptInNotifier::EnsureFactoryBuilt();
   ArcInstanceThrottle::EnsureFactoryBuilt();
   ArcKeyboardShortcutBridge::EnsureFactoryBuilt();
-  ArcKeymasterBridge::EnsureFactoryBuilt();
+  if (ShouldUseArcKeyMint()) {
+    ArcKeyMintBridge::EnsureFactoryBuilt();
+  } else {
+    ArcKeymasterBridge::EnsureFactoryBuilt();
+  }
   ArcKioskBridge::EnsureFactoryBuilt();
   ArcLockScreenBridge::EnsureFactoryBuilt();
   ArcMediaSessionBridge::EnsureFactoryBuilt();
