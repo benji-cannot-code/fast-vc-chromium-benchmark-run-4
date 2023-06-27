9b5f1983fa071370438cb5951b4d2f231b90fa7f
Alex Kallam <aakallam@chromium.org>
2023-06-27 13:40:49 +0000

    [MDL] Use installer to send file to Network Service in Chrome
    The Chrome-specific component installer files have been recreated so
    that the file can be more easily sent to Chrome right now. WebView is
    not yet supported.
    
    Bug: 1415482
    Change-Id: I555b0f951f2d058f74cdd09630f380be426e45bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400877
    Commit-Queue: Alex Kallam <aakallam@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162925}

---
+++ chrome/browser/component_updater/masked_domain_list_component_installer.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_COMPONENT_UPDATER_MASKED_DOMAIN_LIST_COMPONENT_INSTALLER_H_
+#define CHROME_BROWSER_COMPONENT_UPDATER_MASKED_DOMAIN_LIST_COMPONENT_INSTALLER_H_
+
+#include "components/component_updater/component_installer.h"
+
+namespace component_updater {
+
+void RegisterMaskedDomainListComponent(ComponentUpdateService* cus);
+
+}
+
+#endif  // CHROME_BROWSER_COMPONENT_UPDATER_MASKED_DOMAIN_LIST_COMPONENT_INSTALLER_H_
--- components/component_updater/installer_policies/masked_domain_list_component_installer_policy.cc
+++ components/component_updater/installer_policies/masked_domain_list_component_installer_policy.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/component_updater/installer_policies/masked_domain_list_component_installer.h"
+#include "components/component_updater/installer_policies/masked_domain_list_component_installer_policy.h"
 
 #include <utility>
 
@@ -14,12 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/logging.h"
-#include "base/no_destructor.h"
 #include "base/task/thread_pool.h"
 #include "base/version.h"
 #include "components/component_updater/component_installer.h"
 #include "services/network/public/cpp/features.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 using component_updater::ComponentUpdateService;
 
@@ -29,7 +27,7 @@ using ListReadyRepeatingCallback = component_updater::
     MaskedDomainListComponentInstallerPolicy::ListReadyRepeatingCallback;
 
 constexpr base::FilePath::CharType kMaskedDomainListFileName[] =
-    FILE_PATH_LITERAL("list.json");
+    FILE_PATH_LITERAL("list.pb");
 
 // The SHA256 of the SubjectPublicKeyInfo used to sign the extension.
 // The extension id is: cffplpkejcbdpfnfabnjikeicbedmifn
@@ -43,18 +41,12 @@ constexpr char kMaskedDomainListManifestName[] = "Masked Domain List";
 constexpr base::FilePath::CharType kMaskedDomainListRelativeInstallDir[] =
     FILE_PATH_LITERAL("MaskedDomainListPreloaded");
 
-base::File OpenFile(const base::FilePath& pb_path) {
-  return base::File(pb_path, base::File::FLAG_OPEN | base::File::FLAG_READ);
-}
-
-bool IsMaskedDomainListEnabled() {
-  // TODO(aakallam): move this to a more accessible location.
-  return base::FeatureList::IsEnabled(network::features::kMaskedDomainList);
-}
-
-base::TaskPriority GetTaskPriority() {
-  return IsMaskedDomainListEnabled() ? base::TaskPriority::USER_BLOCKING
-                                     : base::TaskPriority::BEST_EFFORT;
+std::string ReadFile(const base::FilePath& pb_path) {
+  std::string raw_list;
+  base::ScopedFILE file(FileToFILE(
+      base::File(pb_path, base::File::FLAG_OPEN | base::File::FLAG_READ), "r"));
+  CHECK(base::ReadStreamToString(file.get(), &raw_list));
+  return raw_list;
 }
 
 }  // namespace
@@ -74,6 +66,10 @@ MaskedDomainListComponentInstallerPolicy::
 bool MaskedDomainListComponentInstallerPolicy::
     SupportsGroupPolicyEnabledComponentUpdates() const {
   return true;
+}
+
+bool MaskedDomainListComponentInstallerPolicy::IsEnabled() {
+  return base::FeatureList::IsEnabled(network::features::kMaskedDomainList);
 }
 
 bool MaskedDomainListComponentInstallerPolicy::RequiresNetworkEncryption()
@@ -101,7 +97,7 @@ void MaskedDomainListComponentInstallerPolicy::ComponentReady(
     const base::Version& version,
     const base::FilePath& install_dir,
     base::Value::Dict manifest) {
-  if (install_dir.empty() || !IsMaskedDomainListEnabled()) {
+  if (install_dir.empty() || !IsEnabled()) {
     return;
   }
 
@@ -109,8 +105,8 @@ void MaskedDomainListComponentInstallerPolicy::ComponentReady(
           << version.GetString() << " in " << install_dir.value();
 
   base::ThreadPool::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::MayBlock(), GetTaskPriority()},
-      base::BindOnce(&OpenFile, GetInstalledPath(install_dir)),
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
+      base::BindOnce(&ReadFile, GetInstalledPath(install_dir)),
       base::BindOnce(on_list_ready_, version));
 }
 
@@ -141,24 +137,6 @@ std::string MaskedDomainListComponentInstallerPolicy::GetName() const {
 update_client::InstallerAttributes
 MaskedDomainListComponentInstallerPolicy::GetInstallerAttributes() const {
   return update_client::InstallerAttributes();
-}
-
-void RegisterMaskedDomainListComponent(ComponentUpdateService* cus) {
-  if (!IsMaskedDomainListEnabled()) {
-    return;
-  }
-
-  VLOG(1) << "Registering Masked Domain List component.";
-
-  auto policy = std::make_unique<MaskedDomainListComponentInstallerPolicy>(
-      /*on_list_ready=*/base::BindRepeating(
-          [](base::Version version, base::File list_file) {
-            VLOG(1) << "Received Masked Domain List";
-            // TODO(aakallam): do something with the file
-          }));
-
-  base::MakeRefCounted<ComponentInstaller>(std::move(policy))
-      ->Register(cus, base::OnceClosure(), GetTaskPriority());
 }
 
 }  // namespace component_updater
