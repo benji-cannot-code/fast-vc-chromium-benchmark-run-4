aa215fc975aa8ff0a7cec2046feef4f0788c7040
Min Chen <minch@chromium.org>
2023-06-27 19:22:16 +0000

    Revert "Reland "Add functionality to PolicyLoaderLocalTest""
    This reverts commit d87c44105e4ade4ee00ee144cd6a07cf96dd1eb3.
    
    Reason for revert: Failing on linux-chromeos-chrome https://ci.chromium.org/ui/p/chrome/builders/luci.chrome.ci/linux-chromeos-chrome
    
    More info can be found at https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/33947/overview
    
    
    Original change's description:
    > Reland "Add functionality to PolicyLoaderLocalTest"
    >
    > This is a reland of commit 58676c89605f20192352cff623abd1857dfbac4c
    >
    > Original change's description:
    > > Add functionality to PolicyLoaderLocalTest
    > >
    > > Wrote load function for policy loader. Policies are loaded from a json string in the loader class.
    > >
    > > Bug: b:286422730
    > > Change-Id: Ifcd28a5bd5d1aaabaf4902a9e376d797a7cf7fc9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619664
    > > Commit-Queue: Jayee Zheng <jayee@google.com>
    > > Reviewed-by: Yann Dago <ydago@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1160795}
    >
    > Bug: b:286422730
    > Change-Id: I91297d68804c4e9b5f943478dfb49de306485824
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642032
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Commit-Queue: Jayee Zheng <jayee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1162954}
    
    Bug: b:286422730
    Change-Id: I25ac6096119a9e4bcec4ce24f19b5e43498514c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651995
    Owners-Override: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1163148}

--- components/policy/core/common/policy_loader_local_test.h
+++ components/policy/core/common/policy_loader_local_test.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_LOCAL_TEST_H_
 #define COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_LOCAL_TEST_H_
 
-#include <string>
-
-#include "components/policy/core/common/policy_bundle.h"
 #include "components/policy/policy_export.h"
 
 namespace policy {
@@ -23,18 +20,6 @@ class POLICY_EXPORT PolicyLoaderLocalTest {
   ~PolicyLoaderLocalTest();
 
   PolicyBundle Load();
-
-  // Sets policies from |policy_list_json| to |bundle_|,
-  // |policy_list_json| should be a json string containing a list of
-  // dictionaries. Each dictionary represents a policy and should
-  // contain a key-value pair for the level(int), scope(int), source(int),
-  // name and value of the policy.
-  void SetPolicyListJson(const std::string& policy_list_json);
-
-  void VerifyJsonContents(base::Value::Dict* policy_dict);
-
- private:
-  PolicyBundle bundle_;
 };
 
 }  // namespace policy
