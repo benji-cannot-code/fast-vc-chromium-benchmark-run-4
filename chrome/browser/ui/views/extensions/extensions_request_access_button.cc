956c64a01496887d7ffd0cd9e29a2c361b8f8ed8
Emilia Paz <emiliapaz@chromium.org>
2023-06-27 18:23:31 +0000

    [Extensions c2s] Add histogram to record extensions granted access
    Adds a count histogram to record the number of extensions in the request
    access button that were granted site access after the user clicked on
    the request access button.
    
    Bug: 1456888
    Change-Id: I475ca9999da2cc7c692cb382ad128926c9d5edb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632063
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163107}

--- chrome/browser/ui/views/extensions/extensions_request_access_button.cc
+++ chrome/browser/ui/views/extensions/extensions_request_access_button.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/strings/string_util.h"
@@ -154,6 +155,9 @@ void ExtensionsRequestAccessButton::OnButtonPressed() {
 
   base::RecordAction(base::UserMetricsAction(
       "Extensions.Toolbar.ExtensionsActivatedFromRequestAccessButton"));
+  UMA_HISTOGRAM_COUNTS_100(
+      "Extensions.Toolbar.ExtensionsActivatedFromRequestAccessButton",
+      extension_ids_.size());
   action_runner->GrantTabPermissions(extensions_to_run);
 
   // Show confirmation message, and disable the button, for a specific duration.
