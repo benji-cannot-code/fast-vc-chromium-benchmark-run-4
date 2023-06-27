34fdc093a22fdd1ac2efc050b53f9236cd829df8
Tao Wu <wutao@chromium.org>
2023-06-27 19:42:30 +0000

    scalable_iph: Add wallpaper ash notification view
    Scalable IPH wallpaper notification has four preview images, therefore
    needs to create a custom view to handle it.
    
    Bug: b:284158831
    Test: new unittest
    Change-Id: Ia98d99aef00158b99a08306442631e5397920aca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4646248
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163158}

---
+++ chromeos/ash/components/scalable_iph/scalable_iph_delegate.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/scalable_iph/scalable_iph_delegate.h"
+
+namespace scalable_iph {
+
+ScalableIphDelegate::NotificationParams::NotificationParams() = default;
+
+ScalableIphDelegate::NotificationParams::NotificationParams(
+    const NotificationParams&) = default;
+
+ScalableIphDelegate::NotificationParams&
+ScalableIphDelegate::NotificationParams::NotificationParams::operator=(
+    const NotificationParams&) = default;
+
+ScalableIphDelegate::NotificationParams::~NotificationParams() = default;
+
+}  // namespace scalable_iph
