70228e3cf3155ea0c690a6fd88abcce0a26d766b
Christian Xu <christianxu@google.com>
2023-06-27 14:21:29 +0000

    [iOS][Toolbar] Add progress bar on secondary toolbar
    Add a progress bar in the secondary toolbar.
    This CL:
    - Add a progress bar in SecondaryToolbarView.
    - Only show the progress bar if the primary or secondary toolbar has the
      omnibox.
    
    Bug: 1458327
    Change-Id: I228a7b9f3867af84b32d7f4a453910ae9c7d9b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4649389
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162942}

--- ios/chrome/browser/ui/toolbar/secondary_toolbar_view.mm
+++ ios/chrome/browser/ui/toolbar/secondary_toolbar_view.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_tab_grid_button.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_constants.h"
+#import "ios/chrome/browser/ui/toolbar/toolbar_progress_bar.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ui/gfx/ios/uikit_util.h"
@@ -57,11 +58,11 @@ UIView* SecondaryToolbarLocationBarContainerView(
 
 // Separator above the toolbar, redefined as readwrite.
 @property(nonatomic, strong, readwrite) UIView* separator;
+// Progress bar displayed below the toolbar, redefined as readwrite.
+@property(nonatomic, strong, readwrite) ToolbarProgressBar* progressBar;
 
 // The stack view containing the buttons, redefined as readwrite.
 @property(nonatomic, strong, readwrite) UIStackView* buttonStackView;
-// The stack view containing `locationBarContainer` and `buttonStackView`.
-@property(nonatomic, strong) UIStackView* verticalStackView;
 
 // Button to navigate back, redefined as readwrite.
 @property(nonatomic, strong, readwrite) ToolbarButton* backButton;
@@ -107,6 +108,7 @@ UIView* SecondaryToolbarLocationBarContainerView(
 @synthesize locationBarContainer = _locationBarContainer;
 @synthesize locationBarContainerHeight = _locationBarContainerHeight;
 @synthesize openNewTabButton = _openNewTabButton;
+@synthesize progressBar = _progressBar;
 @synthesize toolsMenuButton = _toolsMenuButton;
 @synthesize tabGridButton = _tabGridButton;
 
@@ -193,6 +195,17 @@ UIView* SecondaryToolbarLocationBarContainerView(
     [contentView bringSubviewToFront:self.collapsedToolbarButton];
     AddSameConstraints(self, self.collapsedToolbarButton);
 
+    // Add progress bar on the top edge.
+    _progressBar = [[ToolbarProgressBar alloc] init];
+    _progressBar.translatesAutoresizingMaskIntoConstraints = NO;
+    _progressBar.hidden = YES;
+    [_progressBar.heightAnchor constraintEqualToConstant:kProgressBarHeight]
+        .active = YES;
+    [contentView addSubview:_progressBar];
+    AddSameConstraintsToSides(
+        self, _progressBar,
+        LayoutSides::kTop | LayoutSides::kLeading | LayoutSides::kTrailing);
+
     // LocationBarView constraints.
     if (self.locationBarView) {
       AddSameConstraints(self.locationBarView, self.locationBarContainer);
@@ -261,10 +274,6 @@ UIView* SecondaryToolbarLocationBarContainerView(
 }
 
 - (ToolbarButton*)shareButton {
-  return nil;
-}
-
-- (MDCProgressView*)progressBar {
   return nil;
 }
 
