78ee9f70ca2c6fdba8435d6ffae35af990208aab
Brandon Wylie <wylieb@chromium.org>
2023-06-27 13:28:38 +0000

    [bookmarks] Add scaffolding for new folder select activity
    Bug: 1448933
    Change-Id: Ic7c42bb63d6901716989049b47e5a79e18818510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4576450
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162920}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/app/bookmarks/BookmarkFolderPickerActivity.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.app.bookmarks;
+
+import org.chromium.chrome.browser.SynchronousInitializationActivity;
+
+/**
+ * The activity that enables the user to pick the parent folder for the given {@link BookmarkId}.
+ * Used for the improved android bookmarks manager.
+ */
+// TODO(crbug.com/1448933): Implement new folder picker activity.
+public class BookmarkFolderPickerActivity extends SynchronousInitializationActivity {}
---
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkFolderSelectActivityMediator.java
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.bookmarks;
+
+/** Mediator for the folder select activity. */
+// TODO(crbug.com/1448933): Implement new folder select activity.
+class BookmarkFolderPickerMediator {}
