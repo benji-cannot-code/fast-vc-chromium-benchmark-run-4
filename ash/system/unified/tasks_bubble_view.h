7bcbc26bc1e3e057a978e0529c64cfcddd8ee38e
Yulun Wu <yulunwu@chromium.org>
2023-06-27 21:31:26 +0000

    Update glanceables action button to use label button.
    Bug: b:277268122
    Change-Id: If4cbf9cf66898f9b708c2c17d26ef136fa0b024c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652630
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163222}

--- ash/system/unified/tasks_bubble_view.h
+++ ash/system/unified/tasks_bubble_view.h
@@ -21,9 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 class Combobox;
-class ImageButton;
 class ImageView;
 class Label;
+class LabelButton;
 }  // namespace views
 
 namespace ash {
@@ -119,8 +119,7 @@ class ASH_EXPORT TasksBubbleView : public GlanceableTrayChildBubble {
   raw_ptr<views::FlexLayoutView, ExperimentalAsh> tasks_footer_view_ = nullptr;
   raw_ptr<views::Label, ExperimentalAsh> tasks_bubble_details_ = nullptr;
   raw_ptr<views::View, ExperimentalAsh> separator_ = nullptr;
-  raw_ptr<views::Label, ExperimentalAsh> action_button_label_ = nullptr;
-  raw_ptr<views::ImageButton, ExperimentalAsh> action_button_ = nullptr;
+  raw_ptr<views::LabelButton, ExperimentalAsh> action_button_ = nullptr;
   raw_ptr<views::FlexLayoutView, ExperimentalAsh> task_items_container_view_ =
       nullptr;
 
