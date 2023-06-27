d020ebb0ff48750cceb7de8870528755d567fa6f
Sharon Yang <yangsharon@chromium.org>
2023-06-27 17:27:54 +0000

    Add data: URLs for SiteInstanceGroup feature and flag
    Add feature and flag to put data: URL for SiteInstanceGroup changes
    behind.
    
    Test: No behaviour change
    Bug: 1447896
    Change-Id: Ie7c59f3de956762edc9be040e24b020432987123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647058
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163059}

--- content/common/content_navigation_policy.cc
+++ content/common/content_navigation_policy.cc
@@ -242,4 +242,9 @@ bool ShouldRestrictCanAccessDataForOriginToUIThread() {
          base::FeatureList::IsEnabled(
              net::features::kSupportPartitionedBlobUrl);
 }
+
+bool ShouldCreateSiteInstanceForDataUrls() {
+  return base::FeatureList::IsEnabled(features::kSiteInstanceGroupsForDataUrls);
+}
+
 }  // namespace content
