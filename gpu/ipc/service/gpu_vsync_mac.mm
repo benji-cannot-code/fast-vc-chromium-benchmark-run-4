d74a611d25e8fc44e700747cf69edbd8d5d2b6d3
Avi Drissman <avi@chromium.org>
2023-06-27 19:11:43 +0000

    Convert /gpu/ipc/service to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Bug: 1280317
    Change-Id: I2687aa7fddb02b6ec45a3abc09aad2ee9db9e9dc
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Low-Coverage-Reason: Doing core refactoring across all of Chromium; I do not own this code
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647191
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163129}

--- gpu/ipc/service/gpu_vsync_mac.mm
+++ gpu/ipc/service/gpu_vsync_mac.mm
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/logging.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace gpu {
 namespace {
 BASE_FEATURE(kForceGpuVSyncTimerForDebugging,
@@ -92,7 +96,7 @@ void GpuVSyncMac::SetGpuVSyncEnabled(bool enabled) {
 void GpuVSyncMac::AddGpuVSyncCallback() {
   if (display_link_mac_) {
     DCHECK(!vsync_callback_mac_);
-    // Request the callback to be called on the hihg-priority system
+    // Request the callback to be called on the high-priority system
     // VCDisplayLink thread.
     vsync_callback_mac_ = display_link_mac_->RegisterCallback(
         base::BindRepeating(&GpuVSyncMac::OnDisplayLinkCallback,
@@ -102,7 +106,7 @@ void GpuVSyncMac::AddGpuVSyncCallback() {
       // RegisterCallback succeeded.
       return;
     } else {
-      // Failed. Destroy DisplaylinkMac and switch to timer.
+      // Failed. Destroy DisplayLinkMac and switch to timer.
       display_link_mac_ = nullptr;
       LOG(ERROR) << "Fail to start CVDisplayLink callback for DisplayID: "
                  << display_id_ << ". Switch to timer for GpuVSync";
