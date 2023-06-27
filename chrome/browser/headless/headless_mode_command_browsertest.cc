e648c9606370021867063bbd602674c9b32b50cb
Peter Kvitek <kvitekp@chromium.org>
2023-06-27 16:22:23 +0000

    [headless] Don't create initial page for headless command target
    When executing a command new headless used to create the target
    page twice: first time during normal browser startup sequence and
    second time from the headless command execution JS harness. This
    CL eliminates the creation of the first page since it's not used.
    
    This allows us to avoid some really ugly workarounds in tests.
    
    Bug: 1458062
    Change-Id: Ia4e4a4d1b6c284515a6202648d8fc66ed61303e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647298
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163003}

--- chrome/browser/headless/headless_mode_command_browsertest.cc
+++ chrome/browser/headless/headless_mode_command_browsertest.cc
@@ -197,17 +197,8 @@ class HeadlessModeDumpDomCommandBrowserTestWithTimeout
   std::unique_ptr<net::test_server::HttpResponse> RequestHandler(
       const net::test_server::HttpRequest& request) {
     if (request.relative_url == "/page.html") {
-      // The target page is opened first from the browser startup sequence and
-      // then again from the command handler. We want to delay only the second
-      // request until the command processing is done.
-      std::unique_ptr<net::test_server::BasicHttpResponse> response;
-      if (++page_request_number_ == 2) {
-        response = std::make_unique<net::test_server::DelayedHttpResponse>(
-            timeout() * 2);
-      } else {
-        response = std::make_unique<net::test_server::BasicHttpResponse>();
-      }
-
+      auto response = std::make_unique<net::test_server::DelayedHttpResponse>(
+          timeout() * 2);
       response->set_code(net::HTTP_OK);
       response->set_content_type("text/html");
       response->set_content(R"(<div>Hi, I'm headless!</div>)");
@@ -217,8 +208,6 @@ class HeadlessModeDumpDomCommandBrowserTestWithTimeout
 
     return nullptr;
   }
-
-  int page_request_number_ = 0;
 };
 
 IN_PROC_BROWSER_TEST_F(HeadlessModeDumpDomCommandBrowserTestWithTimeout,
@@ -278,11 +267,8 @@ class HeadlessModeDumpDomCommandBrowserTestWithSubResourceTimeout
     }
 
     if (request.relative_url == "/script.js") {
-      // The target page is opened first from the browser startup sequence and
-      // then again from the command handler. We want to delay only the second
-      // request until the command processing is done.
       std::unique_ptr<net::test_server::BasicHttpResponse> response;
-      if (++subresource_request_number_ == 2 && delay_response()) {
+      if (delay_response()) {
         response = std::make_unique<net::test_server::DelayedHttpResponse>(
             timeout() * 2);
       } else {
@@ -300,14 +286,12 @@ class HeadlessModeDumpDomCommandBrowserTestWithSubResourceTimeout
 
     return nullptr;
   }
-
-  int subresource_request_number_ = 0;
 };
 
 INSTANTIATE_TEST_SUITE_P(
     All,
     HeadlessModeDumpDomCommandBrowserTestWithSubResourceTimeout,
-    testing::Values(false, true));
+    testing::Bool());
 
 IN_PROC_BROWSER_TEST_P(
     HeadlessModeDumpDomCommandBrowserTestWithSubResourceTimeout,
