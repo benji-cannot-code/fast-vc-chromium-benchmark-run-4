92c50e7758b1d7236c23a0bad8545c899e83d529
Eriko Kurimoto <elkurin@chromium.org>
2023-06-27 13:21:32 +0000

    Revert "Prevent non-closable tabs from being closed"
    This reverts commit 889fff9f35c578e4da34c2a2ae4ef4c7bb49130a.
    
    Reason for revert: Blocking LKGM by breaking multiple crostini tests. See b/288972458 for more details.
    
    Original change's description:
    > Prevent non-closable tabs from being closed
    >
    > This CL implements admin configurable non-closable apps.
    > These apps should neither be closable through closing the only web app
    > tab (which this CL implements) nor by closing the window containing the
    > app (future CL). The check is placed in `TabStripModel::CloseTabs` to
    > ensure the tab cannot be closed, as all paths to close a tab go through
    > this method. The close prevention is achieved by filtering all tabs
    > that cannot be closed from the list of tabs to close.
    >
    > DD: go/preventclose
    > Bug: b/278246367
    > Change-Id: Ib7edb8e03595801b9568c423b6f11e59a48c9a55
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4444147
    > Commit-Queue: Simon Hangl <simonha@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1162323}
    
    Bug: b/278246367, b/288972458
    Change-Id: I5ea445b342964f34dc746cc18ea6d6440da97719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650128
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1162915}

--- chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker_browsertest.cc
+++ chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker_browsertest.cc
@@ -200,10 +200,8 @@ IN_PROC_BROWSER_TEST_F(TabUsageScenarioTrackerBrowserTest, BasicNavigations) {
                                 ->GetActiveWebContents()
                                 ->GetPrimaryMainFrame()
                                 ->GetPageUkmSourceId();
-  int previous_tab_count = browser()->tab_strip_model()->count();
-  browser()->tab_strip_model()->CloseWebContentsAt(
-      0, TabCloseTypes::CLOSE_USER_GESTURE);
-  EXPECT_EQ(previous_tab_count - 1, browser()->tab_strip_model()->count());
+  EXPECT_TRUE(browser()->tab_strip_model()->CloseWebContentsAt(
+      0, TabCloseTypes::CLOSE_USER_GESTURE));
   tick_clock_.Advance(kInterval);
   interval_data = data_store_.ResetIntervalData();
   EXPECT_EQ(2U, interval_data.max_tab_count);
@@ -455,10 +453,8 @@ IN_PROC_BROWSER_TEST_F(TabUsageScenarioTrackerBrowserTest,
   tick_clock_.Advance(kInterval * 2);
   auto expected_source_id =
       contents->GetPrimaryMainFrame()->GetPageUkmSourceId();
-  int previous_tab_count = browser()->tab_strip_model()->count();
-  browser()->tab_strip_model()->CloseWebContentsAt(
-      1, TabCloseTypes::CLOSE_USER_GESTURE);
-  EXPECT_EQ(previous_tab_count - 1, browser()->tab_strip_model()->count());
+  EXPECT_TRUE(browser()->tab_strip_model()->CloseWebContentsAt(
+      1, TabCloseTypes::CLOSE_USER_GESTURE));
 
   auto interval_data = data_store_.ResetIntervalData();
   EXPECT_EQ(2U, interval_data.max_tab_count);
@@ -559,13 +555,8 @@ IN_PROC_BROWSER_TEST_F(TabUsageScenarioTrackerBrowserTest,
       ui_test_utils::BROWSER_TEST_WAIT_FOR_BROWSER);
   Browser* browser2 = BrowserList::GetInstance()->get(1);
 
-  int previous_browser1_tab_count = browser()->tab_strip_model()->count();
-  int previous_browser2_tab_count = browser2->tab_strip_model()->count();
-  browser2->tab_strip_model()->CloseWebContentsAt(
-      0, TabCloseTypes::CLOSE_USER_GESTURE);
-  EXPECT_EQ(previous_browser1_tab_count, browser()->tab_strip_model()->count());
-  EXPECT_EQ(previous_browser2_tab_count - 1,
-            browser2->tab_strip_model()->count());
+  EXPECT_TRUE(browser2->tab_strip_model()->CloseWebContentsAt(
+      0, TabCloseTypes::CLOSE_USER_GESTURE));
 
   tick_clock_.Advance(kInterval);
   auto interval_data = data_store_.ResetIntervalData();
