7633366591efab4776b91731e50eb4b6593dc167
Sean Kau <skau@chromium.org>
2023-06-27 21:07:17 +0000

    Disable test when Jelly is enabled
    Tests were originally added to verify we didn't break any of the
    backward compatibility with old colors. Forward mappings are being
    actively checked and this class is slated for removal.
    
    Change-Id: Ie53ff7f286ccdf512765aace1e2af6d7077162cd
    Bug: b:288978674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4649950
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163205}

--- ash/style/ash_color_provider_unittest.cc
+++ ash/style/ash_color_provider_unittest.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_helper.h"
 #include "base/memory/raw_ptr.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "testing/gtest/include/gtest/gtest-param-test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/color/color_provider_key.h"
@@ -58,7 +60,11 @@ class AshColorProviderBase
     : public testing::TestWithParam<ColorsTestCase<LayerType>> {
  public:
   AshColorProviderBase()
-      : task_environment_(base::test::TaskEnvironment::MainThreadType::UI) {}
+      : task_environment_(base::test::TaskEnvironment::MainThreadType::UI) {
+    // Disable when Jelly is enabled since it changes all the colors (and
+    // this test verifies the old colors).
+    features_.InitAndDisableFeature(chromeos::features::kJelly);
+  }
 
   void SetUp() override {
     ash_test_helper_.SetUp();
@@ -71,6 +77,7 @@ class AshColorProviderBase
   }
 
  protected:
+  base::test::ScopedFeatureList features_;
   base::test::TaskEnvironment task_environment_;
   AshTestHelper ash_test_helper_;
   raw_ptr<AshColorProvider, ExperimentalAsh> color_provider_;
