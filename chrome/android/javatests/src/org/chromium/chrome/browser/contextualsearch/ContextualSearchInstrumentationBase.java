4805bc6cbab1f386a85a168d3999f50bfd4b1252
Samuel Huang <huangs@chromium.org>
2023-06-27 15:41:59 +0000

    [Contextual Search] Revive 4 tests in ContextualSearchCriticalTest.
    The 4 disabled tests in ContextualSearchCriticalTest
      testChainedSearchCreatesNewContent
      testChainedSearchLoadsCorrectSearchTerm
      testChainedSearchContentVisibility
      testChainedTapsRemovedFromHistory
    
    are the tests that make multiple calls to simulate{Non}ResolveSearch(),
    i.e., try to open the panel when it is already open (peeked). For these
    tests, 2 causes for failures were found:
     1. "Space problem": TapFarFromPreviousSuppression suppresses clicks
        that > 75 dp (see RETAP_DISTANCE_SQUARED_DP) away from the previous
        click. For the test case with "Search Term Resolution", increase
        screen resolutions have made "Search" and "Resolution" so distant
        that successive clicking would cause the suppression to kick in. It
        seems this issue was mitigated by retryPanelBarInteractions(), which
        repeats clicks -- but that only masks the problem. Combined with (2)
        below, this issue manifests as error message:
    
    Caused by: org.chromium.base.test.util.CriteriaNotSatisfiedException:
         Expected: is "Resolution"
         but: was null
    
        This issue affects:
          testChainedSearchLoadsCorrectSearchTerm_
          testChainedSearchContentVisibility
    
     2. "Time problem": ContextualSearchInstrumentationBase.clickWordNode()
        simulates click and waits for the panel to peek. However, when the
        panel is already peeked from an earlier click, the wait exits
        quickly. This breaks the callers trigger{Non}Resolve(), which rely
        on the wait to temporarily tweak "decided" to a provided value. In
        particular, the non-resolved flow (tweak to false) on "Resolution"
        would behave as the resolved flow, and lead to assertion errors,
        e.g., mFakeServer.getLoadedUrlCount() returning the wrong value.
    
    This CL fixing (2) and revives the 4 tests. The fix changes
    trigger{Non}Resolve() to wait for a "fresh" panel peek by ignoring an
    existing peeked panel. Ultimately this detects a fresh call to
    OverlayPanel.peekPanel(). Ideally we'd use an observer, but since the
    testing framework uses polling and delays, so a simple time-based
    solution is used:
    * Have OverlayPanel store |mLastPeekInstant|, updated on each
      peekPanel() call, and accessed via getLastPeekInstant().
    * In ContextualSearchInstrumentationBase, add
      waitForPanelToFreshlyPeek(), which is like waitForPanelToPeek() but
      also waits for getLastPeekInstant() to change.
    * Change clickWordNode() to use waitForPanelToFreshlyPeek().
    
    Note that (1) is now benign, with retryPanelBarInteractions() creating
    repeated click to close panel and reopen to elide the suppression
    problem. We'll address it in a follow-up.
    
    Bug: 1192285
    Change-Id: I8de3c71664c4c5ba1b79b43537e370a9d9215a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637353
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162980}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInstrumentationBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInstrumentationBase.java
@@ -960,7 +960,7 @@ public class ContextualSearchInstrumentationBase {
     protected void clickWordNode(String nodeId) throws TimeoutException {
         retryPanelBarInteractions(() -> {
             clickNode(nodeId);
-            waitForPanelToPeek();
+            waitForPanelToFreshlyPeek();
         }, true);
     }
 
@@ -1137,6 +1137,21 @@ public class ContextualSearchInstrumentationBase {
      */
     protected void waitForPanelToPeek() {
         waitForPanelToEnterState(PanelState.PEEKED);
+    }
+
+    /**
+     * Waits for the Search Panel (the Search Bar) to peek up from the bottom, and asserts that it
+     * did peek. Ignores an existing Search Panel that peeked.
+     */
+    protected void waitForPanelToFreshlyPeek() {
+        int lastPeekSequence = mPanel.getLastPeekSequence();
+        final @PanelState int state = PanelState.PEEKED;
+        CriteriaHelper.pollUiThread(() -> {
+            Criteria.checkThat(mPanel, Matchers.notNullValue());
+            Criteria.checkThat(mPanel.getPanelState(), Matchers.is(state));
+            Criteria.checkThat(mPanel.getLastPeekSequence(), Matchers.not(lastPeekSequence));
+            Criteria.checkThat(mPanel.isHeightAnimationRunning(), Matchers.is(false));
+        }, TEST_TIMEOUT, DEFAULT_POLLING_INTERVAL);
     }
 
     /**
