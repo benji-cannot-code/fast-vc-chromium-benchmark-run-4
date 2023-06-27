55521e5659e2b540b6abf8ef607cbae6e40f35e1
Emilia Paz <emiliapaz@chromium.org>
2023-06-27 00:37:27 +0000

    [Extensions c2s] Display confirmation message on request access button
    Reland of crrev.com/c/4582732. Original CL was reverted because
    `mac13-arm64-rel-tests` had consistent failures. However, CQ run was
    successful for `mac13-arm64-rel` (which includes `mac13-arm64-rel
    tests`) when test was run 100 times on crrev.com/c/4637343. Therefore,
    disabling the test on Mac since it seems to be flaky vs consistent.
    
    ----------------------------------------------------------------------
    
    Display a confirmation message for kConfirmationDisplayDuration on the
    request access button after it has been clicked.
    
    This CL doesn't fix the animation issues with the extension toolbar
    button (puzzle piece)
    
    Screencasts:
    - No reload:
    https://drive.google.com/file/d/1O3nVNG-Oq8aqGjyhgf9LSxSR_yYyLNy9/view?usp=sharing
    - Reload accepted:
    https://drive.google.com/file/d/1LAKk8VUjWVoaX40QGxRa26u3AiqJuC8O/view?usp=sharing
    - Reload dismissed:
    https://drive.google.com/file/d/1KUSPYOyX9gICU9Hf6mhAgSnZXwXnHKLi/view?usp=sharing
    
    Edge cases:
    Click on request access button and before button is collapsed:
    - Navigate to page that requests access
    https://drive.google.com/file/d/11YNJJFnFCMj3kDfXo5s7nkXArhoH7_cd/view?usp=sharing
    - Navigate to page that doesn't request access
    https://drive.google.com/file/d/1zsr8WkzFhmfb1i_LANPBnvo2kRtjvWtp/view?usp=sharing
    - Open a new tab
    https://drive.google.com/file/d/1gRv1vIJuDBoyfi7xY6psMTnAGCsCAI4g/view?usp=sharing
    - Change to an existent tab
    https://drive.google.com/file/d/1RWbxJdyTPgwYcb-76Uoq2-rhWC5lG8J6/view?usp=sharing
    
    Bug: 1390952
    Change-Id: Ia487d61f0418db9e9b10134d6df1a314584a4507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4633627
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162699}

--- chrome/browser/ui/views/extensions/extensions_toolbar_container.h
+++ chrome/browser/ui/views/extensions/extensions_toolbar_container.h
@@ -168,6 +168,7 @@ class ExtensionsToolbarContainer
   void UpdateToolbarActionHoverCard(
       ToolbarActionView* action_view,
       ToolbarActionHoverCardUpdateType update_type) override;
+  void CollapseConfirmation() override;
 
   // ToolbarActionView::Delegate:
   content::WebContents* GetCurrentWebContents() override;
@@ -262,6 +263,9 @@ class ExtensionsToolbarContainer
       TabStripModel* tab_strip_model,
       const TabStripModelChange& change,
       const TabStripSelectionChange& selection) override;
+  void TabChangedAt(content::WebContents* contents,
+                    int index,
+                    TabChangeType change_type) override;
 
   // ToolbarActionsModel::Observer:
   void OnToolbarActionAdded(
--- chrome/browser/ui/views/extensions/extensions_toolbar_unittest.h
+++ chrome/browser/ui/views/extensions/extensions_toolbar_unittest.h
@@ -28,6 +28,8 @@ class ExtensionService;
 class ExtensionsToolbarUnitTest : public TestWithBrowserView {
  public:
   ExtensionsToolbarUnitTest();
+  explicit ExtensionsToolbarUnitTest(
+      base::test::TaskEnvironment::TimeSource time_source);
   ~ExtensionsToolbarUnitTest() override;
   ExtensionsToolbarUnitTest(const ExtensionsToolbarUnitTest&) = delete;
   const ExtensionsToolbarUnitTest& operator=(const ExtensionsToolbarUnitTest&) =
