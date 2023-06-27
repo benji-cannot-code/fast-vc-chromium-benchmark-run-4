ed58f84efc8cee286bdaeefae5b3c12878de3276
Aida Zolic <aidazolic@chromium.org>
2023-06-27 04:11:23 +0000

    dlp: Add FPNM browser test for IO warning
    Adds a FPNM browser test for copy or move IO tasks in warning mode. It
    checks that pausing the task notifies FPNM to show the warning, and that
    clicking OK on the shown notification opens a warning dialog. To open
    the dialog, FPNM should use existing Files App window if it exists, as
    this should be the window in which the user started the task (e.g. by
    copying some files in the app). If no windows are found, FPNM should
    first open a new instance of the app, and then show the dialog on top.
    
    R=chromeos-dlp@chromium.org
    
    Bug: b:287458625
    Change-Id: Ie9c195c050ebbd88b42f6ba55180b101ef9b8cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4641628
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162772}

--- chrome/browser/ash/policy/dlp/mock_dlp_files_controller_ash.cc
+++ chrome/browser/ash/policy/dlp/mock_dlp_files_controller_ash.cc
@@ -5,11 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/policy/dlp/mock_dlp_files_controller_ash.h"
 
+#include "testing/gmock/include/gmock/gmock.h"
+
 namespace policy {
 
 MockDlpFilesControllerAsh::MockDlpFilesControllerAsh(
     const DlpRulesManager& rules_manager)
-    : DlpFilesControllerAsh(rules_manager) {}
+    : DlpFilesControllerAsh(rules_manager) {
+  ON_CALL(*this, CheckIfLaunchAllowed)
+      .WillByDefault([](const apps::AppUpdate& app_update,
+                        apps::IntentPtr intent,
+                        CheckIfDlpAllowedCallback result_callback) {
+        std::move(result_callback).Run(true);
+      });
+}
 
 MockDlpFilesControllerAsh::~MockDlpFilesControllerAsh() = default;
 
