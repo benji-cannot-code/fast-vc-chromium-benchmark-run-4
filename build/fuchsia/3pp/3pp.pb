d9709749c17a6c41cd0fff2a64ccc3c6b2da18f3
Zijie He <zijiehe@google.com>
2023-06-27 22:38:39 +0000

    [3pp] Implement 3pp for chromium/build/fuchsia/
    It uploads the content into the chromium/fuchsia/test-scripts cipd
    package, so it can be used to share with other repos.
    
    Unlike other implementations, `latest` returns the sha256 of the files
    in the archive so that it can ignore most of the chromium changes but
    focus only on the changes in the desired folder.
    
    A TODO is to remove the irrelevant files from the cipd package, but it
    is less important.
    
    The 3pp should be automatically picked up by
    https://ci.chromium.org/ui/p/chromium/builders/ci/3pp-linux-amd64-packager.
    
    FYI: 3pp is a system to upload packages to the cipd, since manual
    uploading is disallowed.
    https://chromium.googlesource.com/chromium/src/+/HEAD/docs/cipd_and_3pp.md
    
    Change-Id: I8e61569064fd74fd17d5c6e0c7c3c06b031b7bcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4633913
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163265}

---
+++ build/fuchsia/3pp/3pp.pb
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+create {
+  source {
+    script { name: "fetch.py" }
+    unpack_archive: true
+  }
+  build {}
+}
+
+upload {
+  universal: true
+  pkg_prefix: "chromium/fuchsia/test-scripts"
+}
---
+++ build/fuchsia/3pp/install.sh
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+set -x
+set -o pipefail
+
+PREFIX="$1"
+
+# Use "--parents" to reserve the relative paths, e.g.
+# common/py_utils -> $PREFIX/common/py_utils
+cp -rf --parents * "$PREFIX"/
+
