4b3711577a8d8feef98d20464266fb1b8f51c2f6
Adam Langley <agl@chromium.org>
2023-06-28 16:40:50 +0000

    webauthn: route caBLE requests via `Fido2CredentialRequest`.
    Previously, caBLE (i.e. "hybrid transport") requests were sent directly
    to Play Services. In Android 14, we want to share the logic for
    dispatching to either CredMan or Play Services with requests from the
    renderer. The previous change made that logic generic enough to handle
    caBLE requests and this change finishes the process by connecting the
    caBLE code to `Fido2CredentialRequest`.
    
    Low-Coverage-Reason: trying without punctuation to see if this works
    Bug: 1427843
    Change-Id: I741eb8f81341b3a36acdf8cd8ef3def8386afe2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4644291
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163608}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -124,4 +124,9 @@ BASE_FEATURE(kWebAuthConditionalUIExperimentation,
              "WebAuthenticationConditionalUIExperimentation",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enabled in M117. Remove in or after M120.
+BASE_FEATURE(kWebAuthnCableViaCredMan,
+             "WebAuthenticationCableViaCredMan",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace device
