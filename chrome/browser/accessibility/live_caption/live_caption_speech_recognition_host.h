5ca8f34c7048b0aaaec05bd110e67c25e2b5a0de
Andy Phan <andyphan@google.com>
2023-06-28 00:25:07 +0000

    Revert "Cache Live Translate translation requests"
    This reverts commit 9169a09f44951b5500c051a644e590696bd3034c.
    
    Reason for revert: LiveTranslate tests are failing on MSan and LSan
    bots.
    
    Original change's description:
    > Cache Live Translate translation requests
    >
    > This CL introduces a cache that is used to reduce translating duplicate
    > strings for the Live Translate feature. This cache reduces the number of
    > characters sent to the translation service by ~81% without affecting
    > the user experience.
    >
    > Bug: 1426899
    > Change-Id: I4780aa71e827c0b1c6683d155f46f8ebdbd52d2d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632927
    > Commit-Queue: Evan Liu <evliu@google.com>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1163215}
    
    Bug: 1426899, 1458645
    Change-Id: I083eb4691cb82fe69d3bc9169fb24d4f135c7794
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652995
    Commit-Queue: Andy Phan <andyphan@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Andy Phan <andyphan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163307}

--- chrome/browser/accessibility/live_caption/live_caption_speech_recognition_host.h
+++ chrome/browser/accessibility/live_caption/live_caption_speech_recognition_host.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ACCESSIBILITY_LIVE_CAPTION_LIVE_CAPTION_SPEECH_RECOGNITION_HOST_H_
 
 #include <memory>
-#include <string>
-#include <unordered_map>
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -76,12 +74,7 @@ class LiveCaptionSpeechRecognitionHost
       mojo::PendingReceiver<media::mojom::SpeechRecognitionRecognizerClient>
           pending_receiver);
   ~LiveCaptionSpeechRecognitionHost() override;
-  void OnTranslationCallback(const std::string& cached_translation,
-                             const std::string& original_transcription,
-                             const std::string& source_language,
-                             const std::string& target_language,
-                             bool is_final,
-                             const std::string& result);
+  void OnTranslationCallback(media::SpeechRecognitionResult result);
 
   // Returns the WebContents if it exists. If it does not exist, sets the
   // RenderFrameHost reference to nullptr and returns nullptr.
@@ -100,14 +93,6 @@ class LiveCaptionSpeechRecognitionHost
   // A flag used by the Live Translate feature indicating whether transcriptions
   // should stop.
   bool stop_transcriptions_ = false;
-
-  // Used to cache translations to avoid retranslating the same string. The key
-  // is the source and target language codes followed by a `|` separator
-  // character and the original text. The value is the translated text. This
-  // cache is cleared upon receiving a final recognition event. The size of this
-  // cache depends on the frequency of partial and final recognition events, but
-  // is typically under ~10.
-  std::unordered_map<std::string, std::string> translation_cache_;
 
   // The source language code of the audio stream.
   std::string source_language_;
