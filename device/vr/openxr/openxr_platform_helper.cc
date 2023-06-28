0659aac50be80ddad91a8405a4d8ba2d98c36ed0
Brandon Jones <bajones@chromium.org>
2023-06-28 17:51:49 +0000

    Enable use of OpenXR for AR on Android
    By using the OpenXREnableAR feature flag, this change allows an OpenXR
    Android runtime to take priority over ARCore for "immersive-ar" WebXR
    sessions.
    
    Bug: 1435548
    Change-Id: I060db6451894589da556049a8a74512470755af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651949
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163658}

--- device/vr/openxr/openxr_platform_helper.cc
+++ device/vr/openxr/openxr_platform_helper.cc
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/containers/contains.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/version.h"
 #include "build/build_config.h"
 #include "components/version_info/version_info.h"
+#include "device/vr/openxr/openxr_api_wrapper.h"
 #include "device/vr/openxr/openxr_defs.h"
 #include "device/vr/openxr/openxr_extension_helper.h"
 #include "device/vr/openxr/openxr_graphics_binding.h"
@@ -188,6 +190,22 @@ XrResult OpenXrPlatformHelper::DestroyInstance(XrInstance& instance) {
     xr_instance_ = XR_NULL_HANDLE;
   }
   return result;
+}
+
+bool OpenXrPlatformHelper::IsArBlendModeSupported(XrInstance instance) {
+  XrSystemId system;
+
+  if (XR_FAILED(OpenXrApiWrapper::GetSystem(instance, &system))) {
+    return false;
+  }
+
+  std::vector<XrEnvironmentBlendMode> environment_blend_modes =
+      OpenXrApiWrapper::GetSupportedBlendModes(instance, system);
+
+  return base::Contains(environment_blend_modes,
+                        XR_ENVIRONMENT_BLEND_MODE_ADDITIVE) ||
+         base::Contains(environment_blend_modes,
+                        XR_ENVIRONMENT_BLEND_MODE_ALPHA_BLEND);
 }
 
 }  // namespace device
