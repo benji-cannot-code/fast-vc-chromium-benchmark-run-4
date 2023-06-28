d8b0921360317d035a2f305d2166fa3c85a342b3
Ashley Prasad <ashleydp@google.com>
2023-06-28 00:48:35 +0000

    print-preview: add skeleton setup element
    - Add skeleton `PrintPreviewPrinterSetupInfoElement`.
    - Display element in `PrintPreviewDestinationDialogCrosElement`.
    - Test setup element shows when the flag enabled and there are
      no destinations. Otherwise it is hidden.
    - Test element renders when added to document body.
    
    Screenshot: http://screen/37y39H9ZsmJbAww
    
    Bug: b:283119650
    Test: browser_tests --gtest_filter=*PrintPreview*
    Change-Id: Ifa5e8385ecf616d8874a356bc7ee05fe0ab3715b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4633962
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163315}

---
+++ chrome/browser/resources/print_preview/ui/printer_setup_info_cros.ts
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './print_preview_shared.css.js';
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './printer_setup_info_cros.html.js';
+
+/**
+ * @fileoverview PrintPreviewPrinterSetupInfoCrosElement
+ * This element provides contextual instructions to help users navigate
+ * to printer settings based on the state of printers available in
+ * print-preview. Element will use NativeLayer to open the correct printer
+ * settings interface.
+ */
+
+export class PrintPreviewPrinterSetupInfoCrosElement extends PolymerElement {
+  static get is() {
+    return 'print-preview-printer-setup-info-cros' as const;
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [PrintPreviewPrinterSetupInfoCrosElement.is]:
+        PrintPreviewPrinterSetupInfoCrosElement;
+  }
+}
+
+customElements.define(
+    PrintPreviewPrinterSetupInfoCrosElement.is,
+    PrintPreviewPrinterSetupInfoCrosElement);
---
+++ chrome/test/data/webui/print_preview/printer_setup_info_cros_test.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PrintPreviewPrinterSetupInfoCrosElement} from 'chrome://print/print_preview.js';
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+const printer_setup_info_cros_test = {
+  suiteName: 'PrinterSetupInfoCrosTest',
+  TestNames: {
+    ElementDisplays: 'Element displays',
+  },
+};
+
+Object.assign(
+    window, {printer_setup_info_cros_test: printer_setup_info_cros_test});
+
+suite(printer_setup_info_cros_test.suiteName, function() {
+  teardown(function() {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+  });
+
+  /** Verifies element can be added to UI and display. */
+  test(printer_setup_info_cros_test.TestNames.ElementDisplays, function() {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    const setupInfoElement =
+        document.createElement(PrintPreviewPrinterSetupInfoCrosElement.is);
+    document.body.appendChild(setupInfoElement);
+    flush();
+
+    assertTrue(!!setupInfoElement);
+  });
+});
