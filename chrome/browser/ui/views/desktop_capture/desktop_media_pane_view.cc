6fb345200950debd77cedb05d3b94adab6c0858f
Tove Petersson <tovep@chromium.org>
2023-06-28 14:55:53 +0000

    ChromeRefresh2023 colors and radii for DesktopMediaPickerDialog
    Screenshots of before/after in crbug.com/1458779
    
    Bug: 1422129, 1458779
    Change-Id: I156252d8c61fa7963e83e16c0360447ab90043f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650755
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163553}

--- chrome/browser/ui/views/desktop_capture/desktop_media_pane_view.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_pane_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/desktop_capture/desktop_media_pane_view.h"
 
+#include "ui/base/ui_base_features.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/separator.h"
 #include "ui/views/layout/box_layout.h"
@@ -12,8 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DesktopMediaPaneView::DesktopMediaPaneView(
     std::unique_ptr<views::View> content_view,
     std::unique_ptr<ShareAudioView> share_audio_view) {
-  SetBackground(
-      views::CreateThemedSolidBackground(ui::kColorSubtleEmphasisBackground));
+  SetBackground(views::CreateThemedRoundedRectBackground(
+      features::IsChromeRefresh2023() ? ui::kColorSysSurface4
+                                      : ui::kColorSubtleEmphasisBackground,
+      /*top_radius=*/0,
+      /*bottom_radius=*/features::IsChromeRefresh2023() ? 8 : 4,
+      /*for_border_thickness=*/0));
   views::BoxLayout* layout =
       SetLayoutManager(std::make_unique<views::BoxLayout>(
           views::BoxLayout::Orientation::kVertical, gfx::Insets::VH(0, 16)));
