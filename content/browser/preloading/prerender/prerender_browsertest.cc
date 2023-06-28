89982d6fa5a9a30916f6451a13e750d6ac6a7dac
Taiyo Mizuhashi <taiyo@chromium.org>
2023-06-28 11:58:35 +0000

    Prerender: Grant a grace period to BFCache retriggering tests
    Before this CL, tests of retriggering prerender on BFCache restoration
    used `HasHostForUrl` to confirm that retriggering did not occur when
    it was false (see review comments I attached). However, `HasHostForUrl`
    returns false regardless of whether `UpdateSpeculationRules` has been
    retriggered if it is called immediately after restoration.
    
    This CL inserts `RunLoop().RunUntilIdle()` right after the BFCache
    restoration to allow a time to determine whether retriggering actually
    occurs by `HasHostForUrl`.
    
    Bug: 1449163
    Change-Id: I6d6929cda56d9dc8110e282946fa593b8fd1cf4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618005
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163494}

--- content/browser/preloading/prerender/prerender_browsertest.cc
+++ content/browser/preloading/prerender/prerender_browsertest.cc
@@ -7673,6 +7673,9 @@ IN_PROC_BROWSER_TEST_P(PrerenderBackForwardCacheRestorationBrowserTest,
     ASSERT_EQ(web_contents()->GetLastCommittedURL(), prerendering_url);
     EXPECT_TRUE(prerender_observer.was_activated());
   } else {
+    // Grant a grace period for retriggering.
+    base::RunLoop().RunUntilIdle();
+
     // The next page contains speculation rules for the prerendering page, but
     // it's not processed on BFCache restoration.
     EXPECT_FALSE(HasHostForUrl(prerendering_url));
@@ -7729,6 +7732,9 @@ IN_PROC_BROWSER_TEST_P(PrerenderBackForwardCacheRestorationBrowserTest,
     ASSERT_EQ(web_contents()->GetLastCommittedURL(), prerendering_url_a);
     EXPECT_TRUE(prerender_observer_a_retriggered.was_activated());
   } else {
+    // Grant a grace period for retriggering.
+    base::RunLoop().RunUntilIdle();
+
     // The initial page contains speculation rules for page A/B, but neither of
     // them are processed on BFCache restoration.
     EXPECT_FALSE(HasHostForUrl(prerendering_url_a));
