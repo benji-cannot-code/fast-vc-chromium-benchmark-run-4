249f6471207bba55486d949307840b88e465b7dd
Kent Tamura <tkent@chromium.org>
2023-06-28 03:32:06 +0000

    [RemoveLegacy] Change the return type of PaintLayerScrollableArea::Size
    ... from LayoutSize to PhysicalSize.
    
    This CL should have no behavior changes.
    
    Bug: 1229581
    Change-Id: I1cc5fb0d3d99978a6caf3204cd476881825939fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653108
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163364}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
@@ -394,7 +394,7 @@ class CORE_EXPORT PaintLayerScrollableArea final
     in_resize_mode_ = in_resize_mode;
   }
 
-  LayoutSize Size() const;
+  PhysicalSize Size() const;
   LayoutUnit ScrollWidth() const;
   LayoutUnit ScrollHeight() const;
 
