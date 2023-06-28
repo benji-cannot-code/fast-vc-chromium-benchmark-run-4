f53d8b2ad7e015c1b195254343099d0adb31f8ff
Anastasiia N <anastasiian@chromium.org>
2023-06-28 18:51:27 +0000

    cros: Extract DisabledAuthMessageView to a new file
    Move DisabledAuthMessageView out of ash/login/ui/login_auth_user_view to
    reduce the file size, and to allow reuse of this component when the
    LoginAuthUserView will be refactored.
    
    Bug: b:271261286
    Change-Id: I9ac7acb1a84463e351cb6ff2e8b9170e6a3f0e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650612
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163703}

--- ash/login/ui/login_auth_user_view.h
+++ ash/login/ui/login_auth_user_view.h
@@ -32,6 +32,7 @@ class LabelButton;
 
 namespace ash {
 
+class DisabledAuthMessageView;
 class LoginAuthFactorsView;
 class FingerprintAuthFactorModel;
 class SmartLockAuthFactorModel;
@@ -213,7 +214,6 @@ class ASH_EXPORT LoginAuthUserView : public NonAccessibleView {
  private:
   struct UiState;
   class ChallengeResponseView;
-  class DisabledAuthMessageView;
   class LockedTpmMessageView;
 
   // Called when the user submits an auth method. Runs mojo call.
