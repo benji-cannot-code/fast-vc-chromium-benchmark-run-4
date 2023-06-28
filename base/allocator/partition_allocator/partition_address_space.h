3a76f06d8eabbd8f0144f1606f5f340e9b922808
Lei Zhang <thestig@chromium.org>
2023-06-28 06:51:25 +0000

    Tidy includes in partition_address_space.h
    - Put a thread_isolation header behind the feature's build flag.
    - Remove unused headers.
    - Add missing header for std::pair.
    
    Change-Id: Ibff0917e76d422e408813bc210b408a5e9818328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4649468
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163407}

--- base/allocator/partition_allocator/partition_address_space.h
+++ base/allocator/partition_allocator/partition_address_space.h
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ADDRESS_SPACE_H_
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ADDRESS_SPACE_H_
 
-#include <algorithm>
-#include <array>
 #include <cstddef>
-#include <limits>
+#include <utility>
 
 #include "base/allocator/partition_allocator/address_pool_manager_types.h"
 #include "base/allocator/partition_allocator/page_allocator_constants.h"
@@ -24,8 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc_notreached.h"
 #include "base/allocator/partition_allocator/tagging.h"
 #include "base/allocator/partition_allocator/thread_isolation/alignment.h"
-#include "base/allocator/partition_allocator/thread_isolation/thread_isolation.h"
 #include "build/build_config.h"
+
+#if BUILDFLAG(ENABLE_THREAD_ISOLATION)
+#include "base/allocator/partition_allocator/thread_isolation/thread_isolation.h"
+#endif
 
 // The feature is not applicable to 32-bit address space.
 #if BUILDFLAG(HAS_64_BIT_POINTERS)
