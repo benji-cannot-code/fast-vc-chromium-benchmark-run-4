2fca436266417fc6172b98dcfeeb92d2402c2804
Nikhil Nayunigari <nikhilcn@google.com>
2023-06-28 18:58:21 +0000

    [CrOS Cellular] Capturing the empty messages case in eSIM installation
    With the current code, we emit Unknown error if the error code in dbus
    response is empty or doesn't match any of the known hermes error codes.
    This causes any empty error code cases to be classified as unknown errors.
    
    With this change, we are capturing cases where there is no error code
    returned and use that data to root cause those in the platform layer.
    
    Bug: b/289239432
    Change-Id: I96873eab2977168584c83a25934bf6ce1876d5ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652234
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163708}

--- chromeos/ash/components/dbus/hermes/hermes_response_status.cc
+++ chromeos/ash/components/dbus/hermes/hermes_response_status.cc
@@ -79,8 +79,14 @@ HermesResponseStatus HermesResponseStatusFromErrorName(
   if (error_name == hermes::kErrorModemMessageProcessing) {
     return HermesResponseStatus::kErrorModemMessageProcessing;
   }
+  if (error_name == hermes::kErrorUnknown) {
+    return HermesResponseStatus::kErrorUnknown;
+  }
+  if (error_name.empty()) {
+    return HermesResponseStatus::kErrorEmptyResponse;
+  }
 
-  return HermesResponseStatus::kErrorUnknown;
+  return HermesResponseStatus::kErrorUnknownResponse;
 }
 
 std::ostream& operator<<(std::ostream& stream, HermesResponseStatus status) {
@@ -131,6 +137,10 @@ std::ostream& operator<<(std::ostream& stream, HermesResponseStatus status) {
       return stream << "[kErrorUnexpectedModemManagerState]";
     case HermesResponseStatus::kErrorModemMessageProcessing:
       return stream << "[kErrorModemMessageProcessing]";
+    case HermesResponseStatus::kErrorEmptyResponse:
+      return stream << "[kErrorEmptyResponse]";
+    case HermesResponseStatus::kErrorUnknownResponse:
+      return stream << "[kErrorUnknownResponse]";
   }
   return stream << (static_cast<int>(status));
 }
