4c3bce6d2f9fc3e26f6e5303e2b75c8e84dffb1e
Eugene Zemtsov <eugene@chromium.org>
2023-06-28 21:12:45 +0000

    webcodecs: Per frame QP for H264 on MF VEA
    Add ability to change QP for every frame to MediaFoundationVideoEncodeAccelerator.
    
    Bug: 1424154
    Change-Id: I73b42b406173895df1c46a2d3304ad33a0f8ea36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647155
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163778}

--- media/video/video_encode_accelerator_adapter.cc
+++ media/video/video_encode_accelerator_adapter.cc
@@ -382,14 +382,6 @@ void VideoEncodeAcceleratorAdapter::InitializeOnAcceleratorThread(
     return;
   }
 
-  if (options.bitrate.has_value() &&
-      options.bitrate->mode() == Bitrate::Mode::kExternal) {
-    std::move(done_cb).Run(
-        EncoderStatus(EncoderStatus::Codes::kEncoderUnsupportedConfig,
-                      "Unsupported bitrate mode"));
-    return;
-  }
-
   if (!options.frame_size.GetCheckedArea().IsValid()) {
     std::move(done_cb).Run(
         EncoderStatus(EncoderStatus::Codes::kEncoderUnsupportedConfig,
@@ -512,7 +504,7 @@ void VideoEncodeAcceleratorAdapter::Encode(scoped_refptr<VideoFrame> frame,
 
 void VideoEncodeAcceleratorAdapter::EncodeOnAcceleratorThread(
     scoped_refptr<VideoFrame> frame,
-    const EncodeOptions& encode_options,
+    EncodeOptions encode_options,
     EncoderStatusCB done_cb) {
   TRACE_EVENT1("media",
                "VideoEncodeAcceleratorAdapter::EncodeOnAcceleratorThread",
@@ -572,10 +564,9 @@ void VideoEncodeAcceleratorAdapter::EncodeOnAcceleratorThread(
 
   frame = std::move(result).value();
 
-  bool key_frame = encode_options.key_frame;
   if (last_frame_color_space_ != frame->ColorSpace()) {
     last_frame_color_space_ = frame->ColorSpace();
-    key_frame = true;
+    encode_options.key_frame = true;
   }
 
   auto active_encode = std::make_unique<PendingOp>();
@@ -583,7 +574,7 @@ void VideoEncodeAcceleratorAdapter::EncodeOnAcceleratorThread(
   active_encode->timestamp = frame->timestamp();
   active_encode->color_space = frame->ColorSpace();
   active_encodes_.push_back(std::move(active_encode));
-  accelerator_->Encode(frame, key_frame);
+  accelerator_->Encode(frame, encode_options);
 }
 
 void VideoEncodeAcceleratorAdapter::ChangeOptions(const Options& options,
