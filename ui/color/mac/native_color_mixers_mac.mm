8e52a6692392b683ba5b4f83d217408648e75740
Allen Bauer <kylixrd@chromium.org>
2023-06-28 20:37:35 +0000

    [CR2023] Adjust menu separator color on Mac.
    Normally, kColorMenuSeparator maps to kColorSeparator. However on Mac it was overridden and set to one of two specific light or dark based colors. For CR2023, the colors should already have taken that into account, so it can remain mapped to kColorSeparator.
    
    Bug: 1427667
    Change-Id: Icce48650b8150067993acafa516c1910335fe27e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651684
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163758}

--- ui/color/mac/native_color_mixers_mac.mm
+++ ui/color/mac/native_color_mixers_mac.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/fixed_flat_set.h"
 #import "skia/ext/skia_utils_mac.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_mixer.h"
 #include "ui/color/color_provider.h"
@@ -115,10 +116,12 @@ void AddNativeUiColorMixer(ColorProvider* provider,
     mixer[kColorTableBackgroundAlternate] = {skia::NSSystemColorToSkColor(
         NSColor.alternatingContentBackgroundColors[1])};
 
-    SkColor menu_separator_color = properties.dark
-                                       ? SkColorSetA(gfx::kGoogleGrey800, 0xCC)
-                                       : SkColorSetA(SK_ColorBLACK, 0x26);
-    mixer[kColorMenuSeparator] = {menu_separator_color};
+    if (!features::IsChromeRefresh2023()) {
+      SkColor menu_separator_color =
+          properties.dark ? SkColorSetA(gfx::kGoogleGrey800, 0xCC)
+                          : SkColorSetA(SK_ColorBLACK, 0x26);
+      mixer[kColorMenuSeparator] = {menu_separator_color};
+    }
 
     if (!properties.high_contrast) {
       return;
