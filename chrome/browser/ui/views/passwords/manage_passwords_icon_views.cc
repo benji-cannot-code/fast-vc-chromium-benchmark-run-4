917616cec6377f11cde07fcb4a5146a1cdede777
Allen Bauer <kylixrd@chromium.org>
2023-06-28 20:26:46 +0000

    [CR2023] Password 'key' icon should use the open version most places.
    Added back in the "open" key icon for use in the first level menu
    items and in the profile-chip menu.
    
    The "closed" key icon is only used in the Passwords and Autofill
    sub-menu
    
    Change-Id: I8f82586326b2dc929102e57080bc5ef2cffd8c41
    Bug: 1457545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651966
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163747}

--- chrome/browser/ui/views/passwords/manage_passwords_icon_views.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_icon_views.cc
@@ -84,7 +84,7 @@ bool ManagePasswordsIconViews::OnMousePressed(const ui::MouseEvent& event) {
 
 const gfx::VectorIcon& ManagePasswordsIconViews::GetVectorIcon() const {
   return OmniboxFieldTrial::IsChromeRefreshIconsEnabled()
-             ? kKeyChromeRefreshIcon
+             ? kKeyOpenChromeRefreshIcon
              : kKeyIcon;
 }
 
