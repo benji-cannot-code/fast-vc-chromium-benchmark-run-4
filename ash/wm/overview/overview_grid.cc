3a6e2d8044469af667e5b1f6bf9d8b65246bcf5d
Yongshun Liu <yongshun@chromium.org>
2023-06-28 18:19:00 +0000

    ash: New desk bar slide animation
    The existing desk bar slide animation is causing a lot of problems as it
    may run after the destruction of `Shell`. This implements a new way with
    layer copies.
    
    Bug: b/288629799
    Test: DeskBarTest.*
    Change-Id: Ib66380ec90310c9ac25c47d2f2a509abb2dc49b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4643240
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163674}

--- ash/wm/overview/overview_grid.cc
+++ ash/wm/overview/overview_grid.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <functional>
+#include <memory>
 #include <utility>
 
 #include "ash/accessibility/accessibility_controller_impl.h"
@@ -77,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/presentation_time_recorder.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/compositor/throughput_tracker.h"
+#include "ui/gfx/geometry/transform.h"
 #include "ui/gfx/geometry/transform_util.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 #include "ui/views/animation/animation_builder.h"
@@ -420,18 +422,22 @@ bool ShouldExcludeItemFromGridLayout(
 // bar widget as well.
 class DesksBarSlideAnimation {
  public:
-  DesksBarSlideAnimation(std::unique_ptr<views::Widget> desks_widget,
-                         bool is_zero_state)
-      : desks_widget_(std::move(desks_widget)) {
+  DesksBarSlideAnimation(std::unique_ptr<views::Widget> desk_bar_widget,
+                         bool is_zero_state) {
     gfx::Transform transform;
-    transform.Translate(0, -desks_widget_->GetWindowBoundsInScreen().height());
+    transform.Translate(0,
+                        -desk_bar_widget->GetWindowBoundsInScreen().height());
 
-    const auto duration = is_zero_state ? kZeroDesksBarSlideDuration
-                                        : kExpandedDesksBarSlideDuration;
+    // Create layer copies, get rid of the original desk bar widget, and add the
+    // layer copies to the parent layer.
+    ui::Layer* parent_layer = desk_bar_widget->GetLayer()->parent();
+    layer_tree_ = wm::RecreateLayers(desk_bar_widget->GetContentsView());
+    ui::Layer* layer_tree_root = layer_tree_->root();
+    parent_layer->Add(layer_tree_root);
+    desk_bar_widget.reset();
 
     // Add slide out animation as part of the overview exit animation.
-    ui::ScopedLayerAnimationSettings settings{
-        desks_widget_->GetLayer()->GetAnimator()};
+    ui::ScopedLayerAnimationSettings settings{parent_layer->GetAnimator()};
     auto exit_observer = std::make_unique<ExitAnimationObserver>();
     settings.AddObserver(exit_observer.get());
     Shell::Get()->overview_controller()->AddExitAnimationObserver(
@@ -447,8 +453,9 @@ class DesksBarSlideAnimation {
         .SetPreemptionStrategy(
             ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET)
         .Once()
-        .SetDuration(duration)
-        .SetTransform(desks_widget_->GetLayer(), transform,
+        .SetDuration(is_zero_state ? kZeroDesksBarSlideDuration
+                                   : kExpandedDesksBarSlideDuration)
+        .SetTransform(layer_tree_root, transform,
                       gfx::Tween::ACCEL_20_DECEL_100);
   }
 
@@ -458,7 +465,7 @@ class DesksBarSlideAnimation {
   ~DesksBarSlideAnimation() = default;
 
  private:
-  std::unique_ptr<views::Widget> desks_widget_;
+  std::unique_ptr<ui::LayerTreeOwner> layer_tree_;
 };
 
 OverviewGrid::OverviewGrid(aura::Window* root_window,
