7184d8c3ce17489f344c85ea23f2748937b23e2a
Elizaveta Sheremetyeva <sheremetyeva@google.com>
2023-06-28 13:00:57 +0000

    [DBSC] Add preemptive cookie rotation for DBSC
    If the cookie is missing/expired, requests are blocked until a refresh
    operation is completed. This would cause a significant delay in loading
    network requests, therefore we do preemptive cookie rotation.
    
    Bug: b/263263751
    Change-Id: I97d772e3acfa30600a85beb288dce212e4339827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637042
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Elizaveta Sheremetyeva <sheremetyeva@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163509}

--- chrome/browser/signin/bound_session_credentials/bound_session_cookie_controller_impl.h
+++ chrome/browser/signin/bound_session_credentials/bound_session_cookie_controller_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_cookie_observer.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -59,6 +60,7 @@ class BoundSessionCookieControllerImpl : public BoundSessionCookieController {
   void MaybeRefreshCookie();
   void SetCookieExpirationTimeAndNotify(base::Time expiration_time);
   void OnCookieRefreshFetched(BoundSessionRefreshCookieFetcher::Result result);
+  void MaybeScheduleCookieRotation();
   void ResumeBlockedRequests();
 
   void set_refresh_cookie_fetcher_factory_for_testing(
@@ -72,6 +74,8 @@ class BoundSessionCookieControllerImpl : public BoundSessionCookieController {
   std::unique_ptr<BoundSessionCookieObserver> cookie_observer_;
   std::unique_ptr<BoundSessionRefreshCookieFetcher> refresh_cookie_fetcher_;
   std::vector<base::OnceClosure> resume_blocked_requests_;
+  // Used to schedule preemptive cookie refresh.
+  base::OneShotTimer cookie_refresh_timer_;
 
   RefreshCookieFetcherFactoryForTesting
       refresh_cookie_fetcher_factory_for_testing_;
