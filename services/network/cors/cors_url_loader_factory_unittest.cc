303668e5defdb18b57c8a75544bd14bcc61a38cf
Kenichi Ishibashi <bashi@chromium.org>
2023-06-28 13:29:54 +0000

    Update ResourceScheduler::ClientId
    This is a preparation for renderer state aware resource scheduler in
    the network service. We plan to add additional information to
    ClientId. Current public interfaces, like Increment(), to create new
    ClientId don't work well when we add more information to ClientId
    because additional information may have no notion of "increment."
    
    This CL modifies the public interface to create ClientIds so that it
    can be flexible to have more information.
    
    Bug: 1457817
    Change-Id: I219b91d61a2aa9a2e79cae4d3346c22f420d49b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4644189
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163518}

--- services/network/cors/cors_url_loader_factory_unittest.cc
+++ services/network/cors/cors_url_loader_factory_unittest.cc
@@ -36,7 +36,6 @@ namespace {
 
 constexpr int kProcessId = 123;
 constexpr int kRequestId = 456;
-constexpr ResourceScheduler::ClientId kResourceSchedulerClientId(99);
 
 }  // namespace
 
@@ -81,7 +80,7 @@ class CorsURLLoaderFactoryTest : public testing::Test {
         url::Origin::Create(test_server_.base_url());
     auto resource_scheduler_client =
         base::MakeRefCounted<ResourceSchedulerClient>(
-            kResourceSchedulerClientId, IsBrowserInitiated(false),
+            ResourceScheduler::ClientId::Create(), IsBrowserInitiated(false),
             &resource_scheduler_,
             url_request_context_->network_quality_estimator());
     cors_url_loader_factory_ = std::make_unique<CorsURLLoaderFactory>(
