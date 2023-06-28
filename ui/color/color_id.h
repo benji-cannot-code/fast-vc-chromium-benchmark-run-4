f2f2a5be0ba63adc13d5a378ca29aa63cc5e30dd
John Lee <johntlee@chromium.org>
2023-06-28 18:44:02 +0000

    CR2023: Update toasts in side panel
    http://screen/BJCnczhiB9yc8JU
    
    Fixed: 1457329
    Change-Id: I4044d1cd7cd6ddcd7fe5cec4ce0f954ab5d13984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652428
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163694}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -143,6 +143,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorSysWhite) \
   /* Inverse. */ \
   E_CPONLY(kColorSysInversePrimary) \
+  E_CPONLY(kColorSysInverseSurface) \
   E_CPONLY(kColorSysInverseOnSurface) \
   /* Surfaces. */ \
   E_CPONLY(kColorSysSurface) \
