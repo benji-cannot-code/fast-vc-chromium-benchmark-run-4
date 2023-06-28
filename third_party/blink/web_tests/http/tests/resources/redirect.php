3757b055d62c7cc2021fe32894aa78dc3bb5bb75
Nihar Damarasingu <nihardamar@google.com>
2023-06-28 22:50:49 +0000

    Fix webfont-cors to be compatible with php8
    Some changes from php7->php8 requires some fixes in the php files
    to not negatively impact the php api calls such as "header"
    
    Change-Id: I6b16334523bf9f3538527fc6a97c38c7ff9c8c34
    Bug: 1383647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651759
    Commit-Queue: Nihar Damarasingu <nihardamar@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163829}

--- third_party/blink/web_tests/http/tests/resources/redirect.php
+++ third_party/blink/web_tests/http/tests/resources/redirect.php
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-    $sleep = $_GET['delay'];
+    $sleep = $_GET['delay'] ?? null;
     if (isset($sleep))
         usleep($sleep*1000);
     $url = $_GET['url'];
-    $refresh = $_GET['refresh'];
+    $refresh = $_GET['refresh'] ?? null;
 
     if (isset($refresh)) {
         header("HTTP/1.1 200");
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (isset($_GET['timing_allow_origin']))
         header("Timing-Allow-Origin: " . $_GET['timing_allow_origin']);
 
-    $code = $_GET['code'];
+    $code = $_GET['code'] ?? null;
     if (!isset($code))
         $code = 302;
     header("HTTP/1.1 $code");
