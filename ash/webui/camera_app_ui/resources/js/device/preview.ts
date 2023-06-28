b1b69bb5d6b9b0a9764cfb3d02ba0376851a6c26
seannli <seannli@google.com>
2023-06-28 07:21:58 +0000

    CCA: Add a flag to check if the faceoverlays can be displayed correctly
    If the camera is opened by other apps before CCA, CCA cannot get the
    frame orientation correctly. This CL disables the faceoverlay in this
    case.
    
    Bug: b:283960125
    Test: Manually by Opening Meet and CCA with expert mode
    Change-Id: I2f7c36b94699cb52d0071a1d10af860cce8056d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4649549
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Sean Li <seannli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163419}

--- ash/webui/camera_app_ui/resources/js/device/preview.ts
+++ ash/webui/camera_app_ui/resources/js/device/preview.ts
@@ -103,6 +103,8 @@ export class Preview {
 
   private onPreviewExpired: WaitableEvent|null = null;
 
+  private enableFaceOverlay = false;
+
   /**
    * @param onNewStreamNeeded Callback to request new stream.
    */
@@ -323,9 +325,9 @@ export class Preview {
       }, 100);
       await this.updateFacing();
       this.deviceId = getVideoTrackSettings(this.getVideoTrack()).deviceId;
-      this.updateShowMetadata();
       await this.updatePTZ();
 
+      this.enableFaceOverlay = false;
       const deviceOperator = DeviceOperator.getInstance();
       if (deviceOperator !== null) {
         const {deviceId} = getVideoTrackSettings(this.getVideoTrack());
@@ -338,9 +340,12 @@ export class Preview {
               new Error(
                   'Cannot disable camera frame rotation. ' +
                   'The camera is probably being used by another app.'));
+        } else {
+          this.enableFaceOverlay = true;
         }
         this.vidPid = await deviceOperator.getVidPid(deviceId);
       }
+      this.updateShowMetadata();
 
       assert(
           this.onPreviewExpired === null || this.onPreviewExpired.isSignaled());
@@ -361,6 +366,7 @@ export class Preview {
     // Pause video element to avoid black frames during transition.
     this.video.pause();
     this.disableShowMetadata();
+    this.enableFaceOverlay = false;
     if (this.streamInternal !== null && this.isStreamAlive()) {
       const track = this.getVideoTrack();
       const {deviceId} = getVideoTrackSettings(track);
@@ -546,25 +552,25 @@ export class Preview {
     const activeArraySize = await deviceOperator.getActiveArraySize(deviceId);
     const cameraFrameRotation =
         await deviceOperator.getCameraFrameRotation(deviceId);
-    this.faceOverlay =
-        new FaceOverlay(activeArraySize, cameraFrameRotation, deviceId);
-
+    if (this.enableFaceOverlay) {
+      this.faceOverlay =
+          new FaceOverlay(activeArraySize, cameraFrameRotation, deviceId);
+    }
     const updateFace =
         (mode: AndroidStatisticsFaceDetectMode, rects: number[]) => {
-          assert(this.faceOverlay !== null);
           if (mode ===
               AndroidStatisticsFaceDetectMode
                   .ANDROID_STATISTICS_FACE_DETECT_MODE_OFF) {
             dom.get('#preview-num-faces', HTMLDivElement).style.display =
                 'none';
-            this.faceOverlay.clearRects();
+            this.faceOverlay?.clearRects();
             return;
           }
           assert(rects.length % 4 === 0);
           const numFaces = rects.length / 4;
           const label = numFaces >= 2 ? 'Faces' : 'Face';
           showValue('#preview-num-faces', `${numFaces} ${label}`);
-          this.faceOverlay.show(rects);
+          this.faceOverlay?.show(rects);
         };
 
     const callback = (metadata: CameraMetadata) => {
