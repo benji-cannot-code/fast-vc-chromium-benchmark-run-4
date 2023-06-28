a6becad6841d96f1f6a9707f123ae995be18c3a9
Steve Cho <stevecho@google.com>
2023-06-28 16:48:24 +0000

    media/gpu/v4l2/test: build H.265 platform decoder only with the flag
    This CL changes to build v4l2 stateless decoder (platform decoder) for
    H.265 only with the flag |enable_hevc_parser_and_hw_decoder|.
    
    Bug: b:261127809
    TEST: none
    Change-Id: Idd486bf0d9e061946f31a82f08ad5e992aaeeb48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4646832
    Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163614}

--- media/gpu/v4l2/test/v4l2_stateless_decoder.cc
+++ media/gpu/v4l2/test/v4l2_stateless_decoder.cc
@@ -21,7 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/v4l2/test/av1_decoder.h"
 #endif
 #include "media/gpu/v4l2/test/h264_decoder.h"
+#if BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
 #include "media/gpu/v4l2/test/h265_decoder.h"
+#endif
 #include "media/gpu/v4l2/test/video_decoder.h"
 #include "media/gpu/v4l2/test/vp8_decoder.h"
 #include "media/gpu/v4l2/test/vp9_decoder.h"
@@ -31,7 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using media::v4l2_test::Av1Decoder;
 #endif
 using media::v4l2_test::H264Decoder;
+#if BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
 using media::v4l2_test::H265Decoder;
+#endif
 using media::v4l2_test::VideoDecoder;
 using media::v4l2_test::Vp8Decoder;
 using media::v4l2_test::Vp9Decoder;
@@ -119,9 +123,11 @@ std::unique_ptr<VideoDecoder> CreateVideoDecoder(
   if (!decoder)
     decoder = Vp8Decoder::Create(stream);
 
+#if BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
   if (!decoder) {
     decoder = H265Decoder::Create(stream);
   }
+#endif
 
   return decoder;
 }
