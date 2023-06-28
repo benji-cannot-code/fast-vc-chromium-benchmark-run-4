cf86d5be8f243ae71e3f076fde9d010dcacb3b0b
Dorah <dorahkim@google.com>
2023-06-28 04:50:49 +0000

    CCA: Divide functions to do one thing
    This CL divides start() and stop() into two functions for each. As input
    parameters confuses us, it's better to divide the functions to their own
    job with different naming.
    
    Bug: b:273315158
    Test: Manually.
    Change-Id: If59239147b6ac1bd95d5c3eb215e3926735a0dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4644890
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163385}

--- ash/webui/camera_app_ui/resources/js/device/mode/video.ts
+++ ash/webui/camera_app_ui/resources/js/device/mode/video.ts
@@ -457,11 +457,11 @@ export class Video extends ModeBase {
     this.mediaRecorder.addEventListener(toggledEvent, onToggled);
     if (toBePaused) {
       waitable.wait().then(() => this.playPauseEffect(toBePaused));
-      this.recordTime.stop({pause: true});
+      this.recordTime.pause();
       this.mediaRecorder.pause();
     } else {
       await this.playPauseEffect(toBePaused);
-      this.recordTime.start({resume: true});
+      this.recordTime.resume();
       this.mediaRecorder.resume();
     }
 
@@ -482,11 +482,11 @@ export class Video extends ModeBase {
     // Resume: Sound/Button UI -> Update Timer -> Resume
     if (toBePaused) {
       state.set(state.State.RECORDING_PAUSED, true);
-      this.recordTime.stop({pause: true});
+      this.recordTime.pause();
       await this.playPauseEffect(true);
     } else {
       await this.playPauseEffect(false);
-      this.recordTime.start({resume: true});
+      this.recordTime.resume();
       state.set(state.State.RECORDING_PAUSED, false);
     }
 
@@ -625,7 +625,7 @@ export class Video extends ModeBase {
         this.recordingType === RecordType.GIF);
     if (this.recordingType === RecordType.GIF) {
       state.set(state.State.RECORDING, true);
-      this.gifRecordTime.start({resume: false});
+      this.gifRecordTime.start();
 
       let gifSaver = null;
       try {
@@ -638,7 +638,7 @@ export class Video extends ModeBase {
         throw e;
       } finally {
         state.set(state.State.RECORDING, false);
-        this.gifRecordTime.stop({pause: false});
+        this.gifRecordTime.stop();
       }
 
       const gifName = (new Filenamer()).newVideoName(VideoType.GIF);
@@ -654,14 +654,14 @@ export class Video extends ModeBase {
       // TODO(b/279865370): Don't pause when the confirm dialog is shown.
       window.addEventListener('beforeunload', beforeUnloadListener);
 
-      this.recordTime.start({resume: false});
+      this.recordTime.start();
       let timeLapseSaver: TimeLapseSaver|null = null;
       try {
         assert(param !== null);
         timeLapseSaver = await this.captureTimeLapse(param);
       } finally {
         state.set(state.State.RECORDING, false);
-        this.recordTime.stop({pause: false});
+        this.recordTime.stop();
         window.removeEventListener('beforeunload', beforeUnloadListener);
       }
 
@@ -681,7 +681,7 @@ export class Video extends ModeBase {
         timeLapseSaver,
       })];
     } else {
-      this.recordTime.start({resume: false});
+      this.recordTime.start();
       let videoSaver: VideoSaver|null = null;
 
       const isVideoTooShort = () => this.recordTime.inMilliseconds() <
@@ -691,7 +691,7 @@ export class Video extends ModeBase {
         try {
           videoSaver = await this.captureVideo();
         } finally {
-          this.recordTime.stop({pause: false});
+          this.recordTime.stop();
           sound.play(dom.get('#sound-rec-end', HTMLAudioElement));
           await this.snapshotting;
         }
