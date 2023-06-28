14ffb4b7d80cd7204ff9163ebe7e6f94f467c074
Hyowon Kim <hyowon@igalia.com>
2023-06-28 02:46:03 +0000

    [ios] Hide scrollbars in PaintPreviewRecorderRenderViewTest
    On iOS, the TestCaptureMainFrameAndClipping test fails because an
    overlay scrollbar is drawn at the coordinate (600, 50) used to check
    pixel color. This CL sets the scrollbars to be hidden so that they don't
    affect the tests.
    
    Bug: 1448918
    Change-Id: I5f3a24ea776df52ebc25ad07693df8539337858c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4648668
    Commit-Queue: Hyowon Kim <hyowon@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163354}

--- components/paint_preview/renderer/paint_preview_recorder_browsertest.cc
+++ components/paint_preview/renderer/paint_preview_recorder_browsertest.cc
@@ -20,7 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/web_runtime_features.h"
 #include "third_party/blink/public/web/web_local_frame.h"
+#include "third_party/blink/public/web/web_settings.h"
 #include "third_party/blink/public/web/web_testing_support.h"
+#include "third_party/blink/public/web/web_view.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkPicture.h"
@@ -77,6 +79,8 @@ class PaintPreviewRecorderRenderViewTest
   void SetUp() override {
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
     RenderViewTest::SetUp();
+    // Hide scrollbars so that they don't affect the bitmap color results.
+    web_view_->GetSettings()->SetHideScrollbars(true);
   }
 
   base::FilePath MakeTestFilePath(const std::string& filename) {
@@ -115,16 +119,7 @@ class PaintPreviewRecorderRenderViewTest
   base::test::ScopedFeatureList feature_list_;
 };
 
-// TODO(crbug.com/1448918): The color returned on iOS is incorrect.
-#if BUILDFLAG(IS_IOS)
-#define MAYBE_TestCaptureMainFrameAndClipping \
-  DISABLED_TestCaptureMainFrameAndClipping
-#else
-#define MAYBE_TestCaptureMainFrameAndClipping TestCaptureMainFrameAndClipping
-#endif
-
-TEST_P(PaintPreviewRecorderRenderViewTest,
-       MAYBE_TestCaptureMainFrameAndClipping) {
+TEST_P(PaintPreviewRecorderRenderViewTest, TestCaptureMainFrameAndClipping) {
   LoadHTML(
       "<!doctype html>"
       "<body>"
