4a6f138184b75fad0cbc2f4ec21f0c7e86065854
Avi Drissman <avi@chromium.org>
2023-06-28 20:10:27 +0000

    Convert content_browsertests to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This CL landed earlier as https://crrev.com/c/4622073. This version of
    the CL has a fix, more carefully managing the lifetimes of the test
    windows.
    
    Bug: 1280317
    Change-Id: I6ffe2da3d1c9686f24903c00dcf48d72186b170c
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650276
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163738}

--- content/browser/gpu/browser_child_process_backgrounded_bridge_browsertest.mm
+++ content/browser/gpu/browser_child_process_backgrounded_bridge_browsertest.mm
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test.h"
 #include "gpu/config/gpu_finch_features.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace content {
 
 bool IsProcessBackgrounded(base::ProcessId pid) {
