4be7e230fc9a03d1f54e3bab25c2961bfd662367
Luca Accorsi <accorsi@google.com>
2023-06-28 05:31:57 +0000

    dlp: Extend DlpClient::Observer interface with OnFilesAddedToDlpDaemon
    Currently only the who adds files may be notified of a successful file
    addition. This CL enables the possibility to notify a generic observer.
    As an example use case, we might want to notify an open Files app
    instance that files have been added to the daemon and thus they should
    now show the managed icon.
    
    Bug: b/289031532
    Change-Id: Iea3b662ded4e7db7280e49627912e9eda85efbeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650008
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Daniel Brinkers <brinky@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163392}

--- chromeos/dbus/dlp/dlp_client.cc
+++ chromeos/dbus/dlp/dlp_client.cc
@@ -34,12 +34,14 @@ const char kProtoMessageParsingFailure[] =
 // will return an appropriate error message.
 const char* DeserializeProto(dbus::Response* response,
                              google::protobuf::MessageLite* proto) {
-  if (!response)
+  if (!response) {
     return kDbusCallFailure;
+  }
 
   dbus::MessageReader reader(response);
-  if (!reader.PopArrayOfBytesAsProto(proto))
+  if (!reader.PopArrayOfBytesAsProto(proto)) {
     return kProtoMessageParsingFailure;
+  }
 
   return nullptr;
 }
@@ -96,10 +98,10 @@ class DlpClientImpl : public DlpClient {
       return;
     }
 
-    proxy_->CallMethod(
-        &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-        base::BindOnce(&DlpClientImpl::HandleAddFilesResponse,
-                       weak_factory_.GetWeakPtr(), std::move(callback)));
+    proxy_->CallMethod(&method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+                       base::BindOnce(&DlpClientImpl::HandleAddFilesResponse,
+                                      weak_factory_.GetWeakPtr(),
+                                      std::move(request), std::move(callback)));
   }
 
   void GetFilesSources(const dlp::GetFilesSourcesRequest request,
@@ -196,12 +198,21 @@ class DlpClientImpl : public DlpClient {
     std::move(callback).Run(response_proto);
   }
 
-  void HandleAddFilesResponse(AddFilesCallback callback,
+  void HandleAddFilesResponse(const dlp::AddFilesRequest request,
+                              AddFilesCallback callback,
                               dbus::Response* response) {
     dlp::AddFilesResponse response_proto;
     const char* error_message = DeserializeProto(response, &response_proto);
     if (error_message) {
       response_proto.set_error_message(error_message);
+    } else {
+      std::vector<base::FilePath> added_files;
+      for (const auto& add_file_request : request.add_file_requests()) {
+        added_files.emplace_back(add_file_request.file_path());
+      }
+      for (auto& observer : observers_) {
+        observer.OnFilesAddedToDlpDaemon(added_files);
+      }
     }
     std::move(callback).Run(response_proto);
   }
