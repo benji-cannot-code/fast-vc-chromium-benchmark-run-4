d7c860c3ba672680625958cde8eecc9ee6f5826e
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-06-28 04:16:09 +0000

    Import wpt@1842a951118d489e6a69c8a9ea487a7a187952f0
    Using wpt-import in Chromium b545bd19fcb116c06358aed2109a109968ec5710.
    With Chromium commits locally applied on WPT:
    a8f91f21b8 "Add WebSocket mixed content wpt"
    eddf483759 "RemoteContextHelper: always attach URLs to RemoteContextWrapper"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ericwilligers@chromium.org:
      external/wpt/css/motion
    rego@igalia.com:
      external/wpt/css/css-contain
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Id936365bd3bb7d93d59bb53d170531d8e8969c92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652271
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163378}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/modulepreload-as-worker-cross-origin.h2.window.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/early-hints-helpers.sub.js
+
+// see modulepreload-in-early-hints.h2.window.js for params explanation
+test(() => {
+    const params = new URLSearchParams();
+    params.set("description",
+        'Modulepreload should not load with as="worker" from cross-origin url');
+    params.set("resource-url",
+        CROSS_ORIGIN_RESOURCES_URL + "/empty.js?" + token());
+    params.set("as", "worker");
+    params.set("should-preload", false);
+    const test_url = "resources/modulepreload-in-early-hints.h2.py?" + params.toString();
+    window.location.replace(new URL(test_url, window.location));
+});
