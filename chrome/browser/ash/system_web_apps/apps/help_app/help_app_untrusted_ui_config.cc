2c67a747339609391956ebea9a3eab0cb4582e1f
Zufeng Wang <zufeng@google.com>
2023-06-28 08:05:29 +0000

    help-app: Add eligibility for office to loadTimeData
    Bug: b/286992133
    Change-Id: I24fd23364891134f8a26baa8d8f2dff83f2cc26c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653776
    Reviewed-by: Callistus Tan <callistus@google.com>
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163434}

--- chrome/browser/ash/system_web_apps/apps/help_app/help_app_untrusted_ui_config.cc
+++ chrome/browser/ash/system_web_apps/apps/help_app/help_app_untrusted_ui_config.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/assistant/assistant_util.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_utils.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/upload_office_to_cloud/upload_office_to_cloud.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/channel_info.h"
@@ -130,6 +131,12 @@ void PopulateLoadTimeData(content::WebUI* web_ui,
   source->AddBoolean(
       "hasTouchScreen",
       !ui::DeviceDataManager::GetInstance()->GetTouchscreenDevices().empty());
+
+  // If true, then the user may launch the setup flow for Microsoft 365.
+  source->AddBoolean(
+      "Microsoft365",
+      chromeos::IsEligibleAndEnabledUploadOfficeToCloud(profile));
+
   // Checks if the Google Assistant is allowed on this device by going through
   // policies.
   assistant::AssistantAllowedState assistant_allowed_state =
