3ba0e727a51ee40334c1cb941b154c82c403a268
Ashley Prasad <ashleydp@google.com>
2023-06-29 22:45:03 +0000

    print-preview: add manage printers on-click handler
    - Add on-click handler to manage printers button.
    - Updates NativeLayerStub to enable testing if `managePrinters`
      function is called.
    - Test expected call count when manage printers button clicked.
    
    Screencast: http://go/scrcast/NTAxMjg4OTE2MjIxOTUyMHw5MGI4YzY2Yy1mNA
    
    Bug: b:283119650
    Test: browser_tests --gtest_filter=*PrintPreview*
    Change-Id: I70b9527b0d6851c156fecb0d04eaaa814da72c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651673
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164393}

--- chrome/test/data/webui/print_preview/native_layer_stub.ts
+++ chrome/test/data/webui/print_preview/native_layer_stub.ts
@@ -62,6 +62,7 @@ export class NativeLayerStub extends TestBrowserProxy implements NativeLayer {
       'getPreview',
       'getPrinterCapabilities',
       'hidePreview',
+      'managePrinters',
       'print',
       'saveAppState',
       'showSystemDialog',
@@ -191,7 +192,9 @@ export class NativeLayerStub extends TestBrowserProxy implements NativeLayer {
 
   cancelPendingPrintRequest() {}
 
-  managePrinters() {}
+  managePrinters() {
+    this.methodCalled('managePrinters');
+  }
 
   /**
    * settings The settings to return as a response to |getInitialSettings|.
