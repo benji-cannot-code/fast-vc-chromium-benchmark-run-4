1e69a14be4744ba5529a728d548cdb8d7666b88c
Alexander Bolodurin <alexbn@chromium.org>
2023-06-29 01:10:36 +0000

    [office] localisable welcome screen
    Bug: b/254586358, b/266667162
    Change-Id: Ie625a90df2f90d77593efe84927f8091a33cec2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4624234
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163891}

--- chrome/browser/resources/chromeos/cloud_upload/welcome_page.ts
+++ chrome/browser/resources/chromeos/cloud_upload/welcome_page.ts
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import './strings.m.js';
+
+import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 
 import {BaseSetupPageElement, CANCEL_SETUP_EVENT, NEXT_PAGE_EVENT} from './base_setup_page.js';
 import {getTemplate} from './welcome_page.html.js';
@@ -28,32 +31,28 @@ export class WelcomePageElement extends BaseSetupPageElement {
     const actionButton = this.querySelector('.action-button') as HTMLElement;
     const cancelButton = this.querySelector('.cancel-button') as HTMLElement;
 
-    const installOfficeWebAppDescription = 'Install Microsoft 365';
-    const installOdfsDescription = 'Connect to Microsoft OneDrive';
-    const moveFilesDescription =
-        'Files will move to OneDrive when opening in Microsoft 365';
-    const zeroStepActionButtonText = 'Set up';
-
-
     if (this.isOfficeWebAppInstalled && this.isOdfsMounted) {
-      description.innerText = moveFilesDescription;
-      actionButton.innerText = zeroStepActionButtonText;
+      description.innerText = loadTimeData.getString('welcomeMoveFiles');
+      actionButton.innerText = loadTimeData.getString('welcomeSetUp');
     } else {
       const ul = document.createElement('ul');
       if (!this.isOfficeWebAppInstalled) {
         const installOfficeWebAppElement = document.createElement('li');
-        installOfficeWebAppElement.innerText = installOfficeWebAppDescription;
+        installOfficeWebAppElement.innerText =
+            loadTimeData.getString('welcomeInstallOfficeWebApp');
         ul.appendChild(installOfficeWebAppElement);
       }
       if (!this.isOdfsMounted) {
         const installOdfsElement = document.createElement('li');
-        installOdfsElement.innerText = installOdfsDescription;
+        installOdfsElement.innerText =
+            loadTimeData.getString('welcomeInstallOdfs');
         ul.appendChild(installOdfsElement);
       }
       const moveFilesElement = document.createElement('li');
-      moveFilesElement.innerText = moveFilesDescription;
+      moveFilesElement.innerText = loadTimeData.getString('welcomeMoveFiles');
       ul.appendChild(moveFilesElement);
       description.appendChild(ul);
+      actionButton.innerText = loadTimeData.getString('welcomeGetStarted');
     }
 
     actionButton.addEventListener('click', this.onActionButtonClick);
