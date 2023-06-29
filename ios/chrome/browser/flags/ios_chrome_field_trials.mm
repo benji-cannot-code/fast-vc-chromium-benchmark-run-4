9bd29918057b3a3b1bd25c06ad06a8cdc167e02b
Benjamin Williams <bwwilliams@google.com>
2023-06-29 19:11:05 +0000

    [iOS] Remove client-side fieldtrial registration of SyncSegmentsData.
    This change removes the client-side fieldtrial registration for the
    SyncSegmentsData feature.
    
    Importantly, this change does not remove the existing client-side Finch
    infrastructure for the feature (currently in ios/chrome/browser/ntp/);
    this change merely prevents the fieldtrial from being registered.
    
    Fixed: 1458490
    Change-Id: Id24723632909501bc3a99be00f0b0b51d3d0578a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650491
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164271}

--- ios/chrome/browser/flags/ios_chrome_field_trials.mm
+++ ios/chrome/browser/flags/ios_chrome_field_trials.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/paths/paths.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_field_trial.h"
-#import "ios/chrome/browser/ui/ntp/synced_segments_field_trial.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -42,7 +41,4 @@ void IOSChromeFieldTrials::SetUpClientSideFieldTrials(
   new_tab_page_field_trial::Create(entropy_providers.low_entropy(),
                                    feature_list,
                                    GetApplicationContext()->GetLocalState());
-  synced_segments_field_trial::Create(entropy_providers.low_entropy(),
-                                      feature_list,
-                                      GetApplicationContext()->GetLocalState());
 }
--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -81,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_scene_agent.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_field_trial.h"
-#import "ios/chrome/browser/ui/ntp/synced_segments_field_trial.h"
 #import "ios/chrome/browser/voice/voice_search_prefs_registration.h"
 #import "ios/chrome/browser/web/font_size/font_size_tab_helper.h"
 #import "ios/web/common/features.h"
@@ -153,7 +152,6 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   update_client::RegisterPrefs(registry);
   variations::VariationsService::RegisterPrefs(registry);
   new_tab_page_field_trial::RegisterLocalStatePrefs(registry);
-  synced_segments_field_trial::RegisterLocalStatePrefs(registry);
   component_updater::RegisterComponentUpdateServicePrefs(registry);
   component_updater::AutofillStatesComponentInstallerPolicy::RegisterPrefs(
       registry);
