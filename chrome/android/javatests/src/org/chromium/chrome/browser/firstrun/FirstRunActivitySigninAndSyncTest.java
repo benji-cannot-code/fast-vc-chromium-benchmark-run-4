28edc6dad81271033814221128b3056feb288add
avvall <avvall@google.com>
2023-06-29 00:32:46 +0000

    Disabling due to flakiness until investigation into root cause can be found
    The test bots show flakiness on a couple of different build targets. These tests seem to occasionally fail.
    
    Bug: 1459076
    Change-Id: Ib196d2806f34d5bcdfa2302c280d17f898501549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4655630
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Adam Walls <avvall@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163875}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunActivitySigninAndSyncTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunActivitySigninAndSyncTest.java
@@ -263,6 +263,7 @@ public class FirstRunActivitySigninAndSyncTest {
     @Test
     @MediumTest
     @DisableFeatures({ChromeFeatureList.TANGIBLE_SYNC})
+    @DisabledTest(message = "https://crbug.com/1459076")
     public void acceptingSyncEndsFreAndEnablesSync() {
         when(mExternalAuthUtilsMock.canUseGooglePlayServices(any())).thenReturn(true);
         mAccountManagerTestRule.addAccount(TEST_EMAIL);
@@ -299,6 +300,7 @@ public class FirstRunActivitySigninAndSyncTest {
     @Test
     @MediumTest
     @DisableFeatures({ChromeFeatureList.TANGIBLE_SYNC})
+    @DisabledTest(message = "https://crbug.com/1459076")
     public void refusingSyncEndsFreAndDoesNotEnableSync() {
         mAccountManagerTestRule.addAccount(TEST_EMAIL);
         launchFirstRunActivityAndWaitForNativeInitialization();
@@ -317,6 +319,7 @@ public class FirstRunActivitySigninAndSyncTest {
     @Test
     @MediumTest
     @EnableFeatures({ChromeFeatureList.TANGIBLE_SYNC})
+    @DisabledTest(message = "https://crbug.com/1459076")
     public void refusingSyncEndsFreAndDoesNotEnableSync_tangibleSyncEnabled() {
         mAccountManagerTestRule.addAccount(TEST_EMAIL);
         launchFirstRunActivityAndWaitForNativeInitialization();
