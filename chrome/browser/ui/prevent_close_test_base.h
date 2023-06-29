33093fb2c2725a5b689f41d98ae740d4858289c4
Simon Hangl <simonha@google.com>
2023-06-29 20:28:58 +0000

    Prevent non-closable windows from being closed
    This CL implements admin configurable non-closable apps.
    These apps should neither be closable through closing the only web app
    tab (past CL [1]) nor by closing the window containing the
    app (this CL). The check is placed in the
    `UnloadController::ShouldCloseWindow` oracle.
    
    DD: go/preventclose
    Bug: b/278246367
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4444147
    
    Change-Id: I980d039713704f6d6d2d73153102ff115d9dbc40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594776
    Reviewed-by: Louise Brett <loubrett@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164322}

---
+++ chrome/browser/ui/prevent_close_test_base.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PREVENT_CLOSE_TEST_BASE_H_
+#define CHROME_BROWSER_UI_PREVENT_CLOSE_TEST_BASE_H_
+
+#include "base/strings/string_piece.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/values.h"
+#include "chrome/browser/policy/policy_test_utils.h"
+#include "chrome/browser/web_applications/web_app_id.h"
+#include "components/policy/core/common/mock_configuration_policy_provider.h"
+
+class Browser;
+class GURL;
+class Profile;
+
+class PreventCloseTestBase : public policy::PolicyTest {
+ public:
+  PreventCloseTestBase();
+  PreventCloseTestBase(const PreventCloseTestBase&) = delete;
+  PreventCloseTestBase& operator=(const PreventCloseTestBase&) = delete;
+  ~PreventCloseTestBase() override;
+
+  // policy::PolicyTest:
+  void SetUpInProcessBrowserTestFixture() override;
+  void TearDownInProcessBrowserTestFixture() override;
+
+  void SetWebAppSettings(base::StringPiece config);
+  void ClearWebAppSettings();
+  void InstallPWA(const GURL& app_url, const web_app::AppId& app_id);
+  Browser* LaunchPWA(const web_app::AppId& app_id, bool launch_in_window);
+  base::Value ReturnPolicyValueFromJson(base::StringPiece policy);
+
+  Profile* profile();
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+  testing::NiceMock<policy::MockConfigurationPolicyProvider> provider_;
+};
+
+#endif  // CHROME_BROWSER_UI_PREVENT_CLOSE_TEST_BASE_H_
