8d57dfcc26d034e9a0db4023b012c323439bfb9b
Mei Liang <meiliang@chromium.org>
2023-06-29 20:58:22 +0000

    🌱 Disable FindTest#testBackKeyDismisesFind_BackRefactored test
    Change-Id: I4cc8af85e7addea4e465d87515bb16fe2af40641
    Bug: 1458344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652250
    Owners-Override: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Auto-Submit: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164340}

--- chrome/android/javatests/src/org/chromium/chrome/browser/findinpage/FindTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/findinpage/FindTest.java
@@ -35,6 +35,7 @@ import org.chromium.base.test.util.CloseableOnMainThread;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -479,6 +480,7 @@ public class FindTest {
     @MediumTest
     @Feature({"FindInPage"})
     @EnableFeatures({ChromeFeatureList.BACK_GESTURE_REFACTOR})
+    @DisabledTest(message = "https://crbug.com/1458344")
     public void testBackKeyDismissesFind_BackRefactored() {
         loadTestAndVerifyFindInPage("pitts", "1/7");
         waitForIME(true);
