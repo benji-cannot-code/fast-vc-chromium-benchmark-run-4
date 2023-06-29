7eb37df01da7c7f557bc40f4fd7bb608d923b5bc
Veronique Nguyen <veronguyen@google.com>
2023-06-29 17:52:45 +0000

    [IOSPasswordBottomSheet] Add keyboard icon for the "Use Password" button
    Bug: 1458570
    Change-Id: I8f01c6688082bd4f6bfe1d605580824e3b7acb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650592
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164217}

--- ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_view_controller.mm
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       l10n_util::GetNSString(IDS_IOS_PASSWORD_BOTTOM_SHEET_USE_PASSWORD);
   self.secondaryActionString =
       l10n_util::GetNSString(IDS_IOS_PASSWORD_BOTTOM_SHEET_USE_KEYBOARD);
+  self.secondaryActionImage = DefaultSettingsRootSymbol(@"keyboard");
 
   [super viewDidLoad];
 }
