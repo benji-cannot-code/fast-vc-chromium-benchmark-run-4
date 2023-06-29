c3bfb40780ccc2fcaf1762954a31025061c00e99
Duncan Mercer <mercerd@google.com>
2023-06-29 18:43:15 +0000

    [CSC] Add post communication to server on link open
    This change adds a new post message to our communication with Chrome Search Companion server. This notifies the user of a click that is opening outside of the side panel so the side panel can react accordingly. This first iteration will be used to trigger HATS surveys when the user clicks a link that navigates away from side panel to avoid over triggering on any user interaction.
    
    Screencast: https://screencast.googleplex.com/cast/NjM2MTYyOTM4NzEyODgzMnw5OGQwODJiYi05NA
    
    Bug: b:276809743
    Change-Id: I231c3f391187a13d8c56f1d6b1d70a0dabdff72e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4655073
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164257}

--- chrome/browser/ui/views/side_panel/search_companion/companion_side_panel_controller.cc
+++ chrome/browser/ui/views/side_panel/search_companion/companion_side_panel_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/search_companion/search_companion_side_panel_coordinator.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_registry.h"
+#include "chrome/browser/ui/webui/side_panel/companion/companion_page_handler.h"
 #include "chrome/browser/ui/webui/side_panel/companion/companion_side_panel_untrusted_ui.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/google/core/common/google_util.h"
@@ -196,6 +197,14 @@ void CompanionSidePanelController::DidOpenRequestedURL(
     SidePanelUI::GetSidePanelUIForBrowser(browser)->Show(
         SidePanelEntry::Id::kSearchCompanion,
         SidePanelOpenTrigger::kOpenedInNewTabFromSidePanel);
+  }
+
+  // Notify server that a link was opened in browser.
+  auto* companion_helper =
+      companion::CompanionTabHelper::FromWebContents(tab_web_contents);
+  if (companion_helper && companion_helper->GetCompanionPageHandler()) {
+    companion_helper->GetCompanionPageHandler()->NotifyLinkOpened(
+        url, /**was_handled=*/true);
   }
 }
 
