c38edd75f0b18d6ee92082cfe988cb0f63c6a574
Eriko Kurimoto <elkurin@chromium.org>
2023-06-29 08:22:11 +0000

    Disable SwitchAccessTest.PointScanClickWhenEvents* on ChromeOS
    SwitchAccessTest.PointScaneClickWhenEvent* used to be disabled on
    ChromeOS with memory sanitizer, and this CL expands it to non memory
    sanitizer build as well since it's flaky on linux-chromeos-chrome and
    linux-chromeos-rel for a while.
    
    Bug: 1365773
    Change-Id: I001e68eb966e7a4126aafcd8128eb0d5eaecde98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651163
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163992}

--- chrome/browser/ash/accessibility/switch_access_browsertest.cc
+++ chrome/browser/ash/accessibility/switch_access_browsertest.cc
@@ -243,7 +243,7 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest, TypeIntoVirtualKeyboard) {
   // js-based tests that have the ability to ask the text field for its value.
 }
 
-#if defined(MEMORY_SANITIZER) && BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_PointScanClickWhenMouseEventsEnabled \
   DISABLED_PointScanClickWhenMouseEventsEnabled
 #else
@@ -283,7 +283,7 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest,
   ASSERT_TRUE(IsMouseEventsEnabled(600, 600));
 }
 
-#if defined(MEMORY_SANITIZER) && BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_PointScanClickWhenMouseEventsDisabled \
   DISABLED_PointScanClickWhenMouseEventsDisabled
 #else
