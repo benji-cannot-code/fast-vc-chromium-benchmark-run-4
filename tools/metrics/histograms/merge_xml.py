9e056c27fa4fc16ba7decc432573d883b74345c2
Gabriel Gauthier-Shalom <gabrielgs@google.com>
2023-06-29 01:32:00 +0000

    Create xml_utils.py to sync with the internal version.
    The greater context is that I'm working to sync extract_histograms.py internally and externally.
    
    BUG=1458618
    
    Change-Id: I4c0f3ee779f63e56cc438fb97fcf1eeff4287b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650277
    Commit-Queue: Gabriel Gauthier-Shalom <gabrielgs@google.com>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163897}

--- tools/metrics/histograms/merge_xml.py
+++ tools/metrics/histograms/merge_xml.py
@@ -12,10 +12,10 @@ import sys
 import xml.dom.minidom
 
 import expand_owners
-import extract_histograms
 import histogram_configuration_model
 import histogram_paths
 import populate_enums
+import xml_utils
 
 
 def GetElementsByTagName(trees, tag, depth=2):
@@ -29,7 +29,7 @@ def GetElementsByTagName(trees, tag, depth=2):
   Returns:
     A list of DOM nodes with the specified tag.
   """
-  iterator = extract_histograms.IterElementsWithTag
+  iterator = xml_utils.IterElementsWithTag
   return list(e for t in trees for e in iterator(t, tag, depth))
 
 
@@ -204,7 +204,7 @@ def _AddComponentFromMetadataFile(tree, filename):
   if component:
     histograms = tree.getElementsByTagName('histograms')
     if histograms:
-      iter_matches = extract_histograms.IterElementsWithTag
+      iter_matches = xml_utils.IterElementsWithTag
       for histogram in iter_matches(histograms[0], 'histogram'):
         expand_owners.AddHistogramComponent(histogram, component)
   return tree
