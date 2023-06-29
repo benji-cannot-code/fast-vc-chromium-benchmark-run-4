b33d901189287be092f03a2a474bfa577e8629e4
Michelle <michellegc@google.com>
2023-06-29 00:42:19 +0000

    Use VectorIcons with ColorId for aura touch selection handles.
    Add vector icons for aura touch selection handles and add basic support
    for following the color pipeline. For now, only use these under the CrOS
    touch text editing redesign flag, but add the changes to the general
    aura implementation so that it is easier to update other aura platforms
    as well if it is later needed.
    
    For TouchSelectionControllerImpl (used from views), the handles follow
    the usual views color pipeline. For TouchHandleDrawableAura (used from
    web content), we just make the handles follow the native theme, since it
    seems a bit fiddly to get it to follow the color theme of the context
    window at the moment.
    
    Figma specs: go/next-e14s-touchtext
    (Opacity, padding, CrOS-specific color not to specs yet, will work on
    that in followups.)
    
    Before: https://screenshot.googleplex.com/4bxkuwc9jLKKfqX.png
    After: https://screenshot.googleplex.com/3w9qzk2uP3kGSyY.png
    (i.e. similar, just slightly bigger and supports dynamic color.)
    
    Bug: b:264823752
    Change-Id: Iec05fe4f2a3f2707a91a611626564b8082c27f47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634242
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163881}

--- ui/touch_selection/touch_handle_drawable_aura.h
+++ ui/touch_selection/touch_handle_drawable_aura.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_TOUCH_SELECTION_TOUCH_HANDLE_DRAWABLE_AURA_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
+#include "ui/base/models/image_model.h"
 #include "ui/compositor/layer_delegate.h"
-#include "ui/gfx/image/image.h"
+#include "ui/native_theme/native_theme_observer.h"
 #include "ui/touch_selection/touch_handle.h"
 #include "ui/touch_selection/touch_handle_orientation.h"
 #include "ui/touch_selection/ui_touch_selection_export.h"
@@ -21,7 +23,8 @@ namespace ui {
 
 class UI_TOUCH_SELECTION_EXPORT TouchHandleDrawableAura
     : public TouchHandleDrawable,
-      public ui::LayerDelegate {
+      public ui::LayerDelegate,
+      public ui::NativeThemeObserver {
  public:
   explicit TouchHandleDrawableAura(aura::Window* parent);
 
@@ -50,6 +53,10 @@ class UI_TOUCH_SELECTION_EXPORT TouchHandleDrawableAura
   void OnDeviceScaleFactorChanged(float old_device_scale_factor,
                                   float new_device_scale_factor) override {}
 
+  // ui::NativeThemeObserver:
+  void OnNativeThemeUpdated(ui::NativeTheme* observed_theme) override;
+
+  // Window to draw the handle image.
   std::unique_ptr<aura::Window> window_;
 
   bool enabled_;
@@ -63,7 +70,11 @@ class UI_TOUCH_SELECTION_EXPORT TouchHandleDrawableAura
   // Window bounds relative to the focal position.
   gfx::RectF relative_bounds_;
 
-  gfx::Image handle_image_;
+  // The handle image to draw.
+  ui::ImageModel handle_image_;
+
+  base::ScopedObservation<ui::NativeTheme, ui::NativeThemeObserver>
+      theme_observation_{this};
 };
 
 }  // namespace ui
