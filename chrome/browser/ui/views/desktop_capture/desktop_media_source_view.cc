b1dabdccdab037bbc4c5c83241805975f937e9da
Tove Petersson <tovep@chromium.org>
2023-06-29 10:43:54 +0000

    Rounded preview corners for DesktopMediaPickerDialog ChromeRefresh2023
    Bug: 1422129
    Change-Id: I31a9ca4ee1d1b9c32d981932933649dd78b755bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4656437
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164034}

--- chrome/browser/ui/views/desktop_capture/desktop_media_source_view.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_source_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/desktop_media_list.h"
 #include "chrome/browser/ui/views/desktop_capture/desktop_media_list_view.h"
 #include "chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h"
+#include "chrome/browser/ui/views/desktop_capture/rounded_corner_image_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -51,9 +52,13 @@ DesktopMediaSourceView::DesktopMediaSourceView(
     : parent_(parent),
       source_id_(source_id),
       selected_(false) {
-  AddChildView(icon_view_.get());
-  AddChildView(image_view_.get());
-  AddChildView(label_.get());
+  icon_view_ = AddChildView(std::make_unique<views::ImageView>());
+  image_view_ =
+      AddChildView(base::FeatureList::IsEnabled(kDisplayMediaPickerRedesign) &&
+                           features::IsChromeRefresh2023()
+                       ? std::make_unique<RoundedCornerImageView>()
+                       : std::make_unique<views::ImageView>());
+  label_ = AddChildView(std::make_unique<views::Label>());
   icon_view_->SetCanProcessEventsWithinSubtree(false);
   image_view_->SetCanProcessEventsWithinSubtree(false);
   SetFocusBehavior(FocusBehavior::ALWAYS);
