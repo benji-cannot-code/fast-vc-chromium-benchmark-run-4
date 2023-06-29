989a0a9563eef9383712a8480605e5e594d97d7f
Alan Cutter <alancutter@chromium.org>
2023-06-29 01:43:44 +0000

    [dPWA] Don't use std::format()
    This CL removes a use of std::format() added by:
    https://chromium-review.googlesource.com/c/chromium/src/+/4556505
    We don't have access to C++20 yet.
    
    Change-Id: Ic6ef3baaa8b45e85b28ff15484f6d68b796f39cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654371
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Jason Lin <lxj@google.com>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1163902}

--- chrome/browser/web_applications/commands/uninstall_all_user_installed_web_apps_command.cc
+++ chrome/browser/web_applications/commands/uninstall_all_user_installed_web_apps_command.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/functional/bind.h"
+#include "base/strings/strcat.h"
 #include "chrome/browser/web_applications/locks/all_apps_lock.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
@@ -87,9 +88,9 @@ void UninstallAllUserInstalledWebAppsCommand::JobComplete(
 
   if (code != webapps::UninstallResultCode::kSuccess &&
       code != webapps::UninstallResultCode::kNoAppToUninstall) {
-    errors_.push_back(std::format("{}[{}]: {}", active_job_->app_id(),
-                                  base::ToString(install_source),
-                                  ConvertUninstallResultCodeToString(code)));
+    errors_.push_back(base::StrCat(
+        {active_job_->app_id(), "[", base::ToString(install_source),
+         "]: ", ConvertUninstallResultCodeToString(code)}));
   }
 
   active_job_.reset();
