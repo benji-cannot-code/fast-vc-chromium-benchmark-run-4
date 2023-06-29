f7ab2bf9fc0a7d4b300829d212ff912d9d0f4a73
Bohdan Tyshchenko <bohdanty@google.com>
2023-06-29 10:27:00 +0000

    [OOBE] - Add Jelly style to the pin-keyboard
    Add conditional styling for the pin-keyboard when Jelly is enabled.
    
    Mocks: go/oobe-jelly-components
    
    No jelly: https://screenshot.googleplex.com/7r4N43vhHGuaf7H.png
    With jelly: https://screenshot.googleplex.com/52sbvjBhypVUj3W.png
    
    Bug: b:269226811
    Change-Id: I420b6934ac3b9a9a4ce4e8ec201dd71f2b40b7dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650990
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164033}

--- ash/webui/common/resources/quick_unlock/pin_keyboard.ts
+++ ash/webui/common/resources/quick_unlock/pin_keyboard.ts
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *    </pin-keyboard>
  */
 
+import 'chrome://resources/cr_elements/chromeos/cros_color_overrides.css.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
