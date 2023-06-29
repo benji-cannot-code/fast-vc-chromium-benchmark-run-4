be10297d6212496794c06f738935e4b23bc5b33f
S. Ganesh <ganesh@chromium.org>
2023-06-29 20:03:50 +0000

    Use `FileEnumerator::ForEach`, Remove `ForEachItemInPath`
    r1163741 added `FileEnumerator::ForEach`, so we now use that uniformly
    in the codebase.
    
    Also removed `ForEachItemInPath`.
    
    Low-Coverage-Reason: install_from_archive.mm missing coverage entirely
    Bug: 1447232
    Change-Id: I01e360054f722b47dd8a1aa962d8f468c57ac5b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654940
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164299}

--- chrome/updater/util/unit_test_util.cc
+++ chrome/updater/util/unit_test_util.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
-#include "base/test/bind.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
@@ -528,17 +527,17 @@ void ExpectOnlyMockUpdater(const base::FilePath& mock_updater_path) {
   ASSERT_TRUE(base::PathExists(mock_updater_path));
   int count_mock_updater_path = 0;
 
-  ForEachItemInPath(
+  base::FileEnumerator(
       mock_updater_path.DirName(), false,
-      base::FileEnumerator::FILES | base::FileEnumerator::DIRECTORIES,
-      base::BindLambdaForTesting([&mock_updater_path, &count_mock_updater_path](
-                                     const base::FilePath& item) {
+      base::FileEnumerator::FILES | base::FileEnumerator::DIRECTORIES)
+      .ForEach([&mock_updater_path,
+                &count_mock_updater_path](const base::FilePath& item) {
         if (item == mock_updater_path) {
           ++count_mock_updater_path;
         } else {
           ADD_FAILURE() << "Unexpected file/directory found: " << item;
         }
-      }));
+      });
 
   EXPECT_EQ(count_mock_updater_path, 1);
 }
