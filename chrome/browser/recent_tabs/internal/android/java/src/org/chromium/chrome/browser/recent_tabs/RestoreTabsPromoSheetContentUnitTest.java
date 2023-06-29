ec00185a49284062d4e1d91d7717395e67f6bc7b
Brandon Fong <bjfong@google.com>
2023-06-29 18:10:07 +0000

    [X-Device Restore] Scrolling back up with fling dismisses bottom sheet
    Add a vertical scroll offset to prevent scroll flinging on the restore tabs review screen from dismissing the bottom sheet. Also addressed here is the ability to scroll on the promo screen if the bottom sheet size needs to be shrunken due to landscape/split screen, etc.
    
    Bug: 1458630
    Change-Id: Id0e3ddce9ff2586dc0fcce2b4abb4b7f7366dd35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642269
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164234}

--- chrome/browser/recent_tabs/internal/android/java/src/org/chromium/chrome/browser/recent_tabs/RestoreTabsPromoSheetContentUnitTest.java
+++ chrome/browser/recent_tabs/internal/android/java/src/org/chromium/chrome/browser/recent_tabs/RestoreTabsPromoSheetContentUnitTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.recent_tabs;
 
+import static org.mockito.Mockito.when;
+
 import static org.chromium.chrome.browser.recent_tabs.RestoreTabsProperties.CURRENT_SCREEN;
 import static org.chromium.chrome.browser.recent_tabs.RestoreTabsProperties.ScreenType.DEVICE_SCREEN;
 import static org.chromium.chrome.browser.recent_tabs.RestoreTabsProperties.ScreenType.HOME_SCREEN;
@@ -12,6 +14,9 @@ import static org.chromium.chrome.browser.recent_tabs.RestoreTabsProperties.Scre
 import static org.chromium.chrome.browser.recent_tabs.RestoreTabsProperties.VISIBLE;
 
 import android.view.View;
+import android.widget.ScrollView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -37,6 +42,12 @@ public class RestoreTabsPromoSheetContentUnitTest {
     private View mContentView;
     @Mock
     private BottomSheetController mBottomSheetController;
+    @Mock
+    private RecyclerView mRecyclerView;
+    @Mock
+    private View mChildView;
+    @Mock
+    private ScrollView mScrollView;
 
     private RestoreTabsPromoSheetContent mSheetContent;
     private PropertyModel mModel = RestoreTabsProperties.createDefaultModel();
@@ -64,8 +75,28 @@ public class RestoreTabsPromoSheetContentUnitTest {
     }
 
     @Test
-    public void testSheetContent_getVerticalScrollOffset() {
+    public void testSheetContent_getVerticalScrollOffsetDefault() {
         Assert.assertEquals(0, mSheetContent.getVerticalScrollOffset());
+    }
+
+    @Test
+    public void testSheetContent_getVerticalScrollOffsetRecyclerView() {
+        mSheetContent.setRecyclerViewForTesting(mRecyclerView);
+        mModel.set(CURRENT_SCREEN, DEVICE_SCREEN);
+        when(mRecyclerView.getChildAt(0)).thenReturn(mChildView);
+        when(mChildView.getTop()).thenReturn(0);
+        when(mRecyclerView.getPaddingTop()).thenReturn(1);
+        Assert.assertEquals(1, mSheetContent.getVerticalScrollOffset());
+        mSheetContent.setRecyclerViewForTesting(null);
+    }
+
+    @Test
+    public void testSheetContent_getVerticalScrollOffsetScrollView() {
+        mSheetContent.setScrollViewForTesting(mScrollView);
+        mModel.set(CURRENT_SCREEN, HOME_SCREEN);
+        when(mScrollView.getScrollY()).thenReturn(1);
+        Assert.assertEquals(1, mSheetContent.getVerticalScrollOffset());
+        mSheetContent.setScrollViewForTesting(null);
     }
 
     @Test
