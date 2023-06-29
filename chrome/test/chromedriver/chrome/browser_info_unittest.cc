2d974a53264bde0474756d7001a1f791135f86f8
Vladimir Nechaev <nechaev@chromium.org>
2023-06-29 10:04:44 +0000

    Refactor BrowserInfo in ChromeDriver
    The name of BrowserInfo.is_headless field was misleading because
    it was true only for Headless Shell. In this commit the field is
    renamed to BrowserInfo.is_headless_shell.
    
    Bug: chromedriver:4358
    Change-Id: I37fd295c7497309b4feeb126789f17e4c51a10da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650452
    Commit-Queue: Vladimir Nechaev <nechaev@chromium.org>
    Reviewed-by: Thiago Perrotta <tperrotta@chromium.org>
    Commit-Queue: Thiago Perrotta <tperrotta@chromium.org>
    Auto-Submit: Vladimir Nechaev <nechaev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164028}

--- chrome/test/chromedriver/chrome/browser_info_unittest.cc
+++ chrome/test/chromedriver/chrome/browser_info_unittest.cc
@@ -40,7 +40,7 @@ TEST(ParseBrowserInfo, BlinkVersionContainsSvnRevision) {
   ASSERT_EQ(37, browser_info.major_version);
   ASSERT_EQ(2062, browser_info.build_no);
   ASSERT_EQ(181352, browser_info.blink_revision);
-  ASSERT_FALSE(browser_info.is_headless);
+  ASSERT_FALSE(browser_info.is_headless_shell);
 }
 
 TEST(ParseBrowserInfo, BlinkVersionContainsGitHash) {
@@ -68,7 +68,7 @@ TEST(ParseBrowserString, KitKatWebView) {
   ASSERT_EQ(30, browser_info.major_version);
   ASSERT_EQ(kToTBuildNo, browser_info.build_no);
   ASSERT_TRUE(browser_info.is_android);
-  ASSERT_FALSE(browser_info.is_headless);
+  ASSERT_FALSE(browser_info.is_headless_shell);
 }
 
 TEST(ParseBrowserString, LollipopWebView) {
@@ -81,7 +81,7 @@ TEST(ParseBrowserString, LollipopWebView) {
   ASSERT_EQ(37, browser_info.major_version);
   ASSERT_EQ(kToTBuildNo, browser_info.build_no);
   ASSERT_TRUE(browser_info.is_android);
-  ASSERT_FALSE(browser_info.is_headless);
+  ASSERT_FALSE(browser_info.is_headless_shell);
 }
 
 TEST(ParseBrowserString, AndroidChrome) {
@@ -94,7 +94,7 @@ TEST(ParseBrowserString, AndroidChrome) {
   ASSERT_EQ(39, browser_info.major_version);
   ASSERT_EQ(2171, browser_info.build_no);
   ASSERT_TRUE(browser_info.is_android);
-  ASSERT_FALSE(browser_info.is_headless);
+  ASSERT_FALSE(browser_info.is_headless_shell);
 }
 
 TEST(ParseBrowserString, DesktopChrome) {
@@ -107,7 +107,7 @@ TEST(ParseBrowserString, DesktopChrome) {
   ASSERT_EQ(39, browser_info.major_version);
   ASSERT_EQ(2171, browser_info.build_no);
   ASSERT_FALSE(browser_info.is_android);
-  ASSERT_FALSE(browser_info.is_headless);
+  ASSERT_FALSE(browser_info.is_headless_shell);
 }
 
 TEST(ParseBrowserString, HeadlessChrome) {
@@ -120,7 +120,7 @@ TEST(ParseBrowserString, HeadlessChrome) {
   ASSERT_EQ(39, browser_info.major_version);
   ASSERT_EQ(2171, browser_info.build_no);
   ASSERT_FALSE(browser_info.is_android);
-  ASSERT_TRUE(browser_info.is_headless);
+  ASSERT_TRUE(browser_info.is_headless_shell);
 }
 
 TEST(ParseBlinkVersionString, GitHash) {
@@ -167,7 +167,7 @@ TEST(FillFromBrowserVersionResponse, Chrome) {
   ASSERT_EQ("37.0.2062.124", browser_info.browser_version);
   ASSERT_EQ(37, browser_info.major_version);
   ASSERT_EQ(2062, browser_info.build_no);
-  ASSERT_FALSE(browser_info.is_headless);
+  ASSERT_FALSE(browser_info.is_headless_shell);
 }
 
 TEST(FillFromBrowserVersionResponse, HeadlessChrome) {
@@ -180,5 +180,5 @@ TEST(FillFromBrowserVersionResponse, HeadlessChrome) {
   ASSERT_EQ("39.0.2171.59", browser_info.browser_version);
   ASSERT_EQ(39, browser_info.major_version);
   ASSERT_EQ(2171, browser_info.build_no);
-  ASSERT_TRUE(browser_info.is_headless);
+  ASSERT_TRUE(browser_info.is_headless_shell);
 }
--- chrome/test/chromedriver/session_commands_unittest.cc
+++ chrome/test/chromedriver/session_commands_unittest.cc
@@ -498,13 +498,13 @@ TEST(SessionCommandsTest, ConfigureHeadlessSession_dotNotation) {
 
   Status status = capabilities.Parse(caps);
   BrowserInfo binfo;
-  binfo.is_headless = true;
+  binfo.is_headless_shell = true;
   MockChrome* chrome = new MockChrome(binfo);
   Session session("id", std::unique_ptr<Chrome>(chrome));
 
   status = internal::ConfigureHeadlessSession(&session, capabilities);
   ASSERT_EQ(kOk, status.code()) << status.message();
-  ASSERT_TRUE(session.chrome->GetBrowserInfo()->is_headless);
+  ASSERT_TRUE(session.chrome->GetBrowserInfo()->is_headless_shell);
   ASSERT_STREQ("/examples/python/downloads",
                session.headless_download_directory->c_str());
 }
@@ -520,13 +520,13 @@ TEST(SessionCommandsTest, ConfigureHeadlessSession_nestedMap) {
 
   Status status = capabilities.Parse(caps);
   BrowserInfo binfo;
-  binfo.is_headless = true;
+  binfo.is_headless_shell = true;
   MockChrome* chrome = new MockChrome(binfo);
   Session session("id", std::unique_ptr<Chrome>(chrome));
 
   status = internal::ConfigureHeadlessSession(&session, capabilities);
   ASSERT_EQ(kOk, status.code()) << status.message();
-  ASSERT_TRUE(session.chrome->GetBrowserInfo()->is_headless);
+  ASSERT_TRUE(session.chrome->GetBrowserInfo()->is_headless_shell);
   ASSERT_STREQ("/examples/python/downloads",
                session.headless_download_directory->c_str());
 }
@@ -540,13 +540,13 @@ TEST(SessionCommandsTest, ConfigureHeadlessSession_noDownloadDir) {
 
   Status status = capabilities.Parse(caps);
   BrowserInfo binfo;
-  binfo.is_headless = true;
+  binfo.is_headless_shell = true;
   MockChrome* chrome = new MockChrome(binfo);
   Session session("id", std::unique_ptr<Chrome>(chrome));
 
   status = internal::ConfigureHeadlessSession(&session, capabilities);
   ASSERT_EQ(kOk, status.code()) << status.message();
-  ASSERT_TRUE(session.chrome->GetBrowserInfo()->is_headless);
+  ASSERT_TRUE(session.chrome->GetBrowserInfo()->is_headless_shell);
   ASSERT_STREQ(".", session.headless_download_directory->c_str());
 }
 
@@ -563,7 +563,7 @@ TEST(SessionCommandsTest, ConfigureHeadlessSession_notHeadless) {
 
   status = internal::ConfigureHeadlessSession(&session, capabilities);
   ASSERT_EQ(kOk, status.code()) << status.message();
-  ASSERT_FALSE(session.chrome->GetBrowserInfo()->is_headless);
+  ASSERT_FALSE(session.chrome->GetBrowserInfo()->is_headless_shell);
   ASSERT_FALSE(session.headless_download_directory);
 }
 
