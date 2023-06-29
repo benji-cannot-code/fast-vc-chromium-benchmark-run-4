4a65a8cc663a88bbf79205e484338a5081feeb41
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-06-29 17:35:31 +0000

    [RemoveLegacy] Remove unused column-flex methods from LayoutBox.
    As above - there should be no behaviour change.
    
    Bug: 1229581
    Change-Id: Id80fa3c94b317d34f3db52c581e0f0158c6cdf2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4656798
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164206}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -1740,9 +1740,6 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
 
   void InvalidatePaint(const PaintInvalidatorContext&) const override;
 
-  bool ColumnFlexItemHasStretchAlignment() const;
-  bool IsStretchingColumnFlexItem() const;
-
   void ExcludeScrollbars(
       PhysicalRect&,
       OverlayScrollbarClipBehavior = kIgnoreOverlayScrollbarSize,
