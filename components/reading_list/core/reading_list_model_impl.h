21b5e9e4161dc5b5c93d18b6efccf9c7a1ea3b22
Marc Treib <treib@chromium.org>
2023-06-29 08:31:44 +0000

    Re-introduce improved ReadingList count metrics
    The histograms ReadingList.Read.Count.* and ReadingList.Unread.Count.*
    had expired. This CL re-introduces similar new ones, with an
    additional split by "storage type" (LocalStorage vs LocalStorageSyncing
    vs AccountStorage).
    (Compare similar split for bookmarks metrics: crbug.com/1444386.)
    
    Bug: 1447542, 1448111, 1448112
    Change-Id: I5d8f0074012e7d778058dba68b0b49af273acf5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637270
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163996}

--- components/reading_list/core/reading_list_model_impl.h
+++ components/reading_list/core/reading_list_model_impl.h
@@ -87,6 +87,7 @@ class ReadingListModelImpl : public ReadingListModel {
                                      base::Time distilation_time) override;
   void AddObserver(ReadingListModelObserver* observer) override;
   void RemoveObserver(ReadingListModelObserver* observer) override;
+  void RecordCountMetricsOnUMAUpload() const override;
 
   // Add |entry| to the model, which must not exist before, and notify the sync
   // bridge if |source| is not ADDED_VIA_SYNC.
@@ -142,6 +143,19 @@ class ReadingListModelImpl : public ReadingListModel {
   ReadingListSyncBridge* GetSyncBridgeForTest();
 
  private:
+  // An enum class to add storage state as a suffix to metrics.
+  enum class StorageStateForUma {
+    // Account storage.
+    kAccount,
+    // Local storage that is not being synced at the time the metric is
+    // recorded.
+    kLocalOnly,
+    // Local storage that is being synced at the time the metric is recorded.
+    kSyncEnabled,
+  };
+  StorageStateForUma GetStorageStateForUma() const;
+  std::string GetStorageStateSuffixForUma() const;
+
   ReadingListModelImpl(
       std::unique_ptr<ReadingListModelStorage> storage_layer,
       syncer::StorageType sync_storage_type,
@@ -170,6 +184,8 @@ class ReadingListModelImpl : public ReadingListModel {
   // Update the 3 counts above considering addition/removal of |entry|.
   void UpdateEntryStateCountersOnEntryRemoval(const ReadingListEntry& entry);
   void UpdateEntryStateCountersOnEntryInsertion(const ReadingListEntry& entry);
+
+  void RecordCountMetrics(const std::string& event_suffix) const;
 
   const std::unique_ptr<ReadingListModelStorage> storage_layer_;
   const raw_ptr<base::Clock> clock_;
