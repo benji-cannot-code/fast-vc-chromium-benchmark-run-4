d3371ff73cb42bce8584ca600b6d9142792512ba
Khalid Peer <khalidpeer@chromium.org>
2023-06-29 20:27:08 +0000

    [omnibox][cr23] Update mixer to use tokens for SS omnibox BG colors.
    Given the recent decision to move forward with the "light blue" variant
    for the steady state omnibox background color (and on-hover color), this
    CL updates the relevant color mixer to drop the use of hex string
    feature params in favor of the appropriate sys color tokens.
    
    In addition to setting the "light blue" variant as the default for all
    CR2023 clients moving forward, this change will also ensure that the
    steady state omnibox colors will stay in sync with the rest of the color
    pipeline (instead of using a separate set of hard-coded colors).
    
    Screenshots - BEFORE/AFTER: https://drive.google.com/drive/folders/18WupVMgMvDA0cktw5cW5zKBs6ORrUp7V?resourcekey=0-lKnG2Rz52lLCKkiuZ-I08g&usp=sharing
    
    Bug: 1416351
    Change-Id: If3af06f1497a8b169f92fe24ff8cb827411564f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654778
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164319}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1854,29 +1854,6 @@ const FeatureEntry::FeatureVariation kOmniboxSuggestionHeightVariations[] = {
      std::size(kOmniboxUniformRowHeight40), nullptr},
 };
 
-const FeatureEntry::FeatureParam
-    kOmniboxSteadyStateBackgroundColorBaselineBlue[] = {
-        {"OmniboxLightBackgroundColor", "0xEBEFF7"},
-        {"OmniboxLightBackgroundColorHovered", "0xE3E7F0"}};
-
-const FeatureEntry::FeatureParam kOmniboxSteadyStateBackgroundColorGray[] = {
-    {"OmniboxLightBackgroundColor", "0xF2F2F2"},
-    {"OmniboxLightBackgroundColorHovered", "0xE5E5E5"}};
-
-const FeatureEntry::FeatureParam kOmniboxSteadyStateBackgroundColorDarkBlue[] =
-    {{"OmniboxLightBackgroundColor", "0xE1E9F7"},
-     {"OmniboxLightBackgroundColorHovered", "0xD5DDEA"}};
-
-const FeatureEntry::FeatureVariation
-    kOmniboxSteadyStateBackgroundColorVariations[] = {
-        {"baseline blue", kOmniboxSteadyStateBackgroundColorBaselineBlue,
-         std::size(kOmniboxSteadyStateBackgroundColorBaselineBlue), nullptr},
-        {"gray", kOmniboxSteadyStateBackgroundColorGray,
-         std::size(kOmniboxSteadyStateBackgroundColorGray), nullptr},
-        {"dark blue", kOmniboxSteadyStateBackgroundColorDarkBlue,
-         std::size(kOmniboxSteadyStateBackgroundColorDarkBlue), nullptr},
-};
-
 const FeatureEntry::FeatureParam kOmniboxFontSize12[] = {
     {"OmniboxFontSizeNonTouchUI", "12"}};
 const FeatureEntry::FeatureParam kOmniboxFontSize13[] = {
@@ -6156,11 +6133,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"omnibox-gm3-steady-state-background-color",
      flag_descriptions::kOmniboxGM3SteadyStateBackgroundColorName,
      flag_descriptions::kOmniboxGM3SteadyStateBackgroundColorDescription,
-     kOsAll,
-     FEATURE_WITH_PARAMS_VALUE_TYPE(
-         omnibox::kOmniboxSteadyStateBackgroundColor,
-         kOmniboxSteadyStateBackgroundColorVariations,
-         "OmniboxCR2023m113")},
+     kOsAll, FEATURE_VALUE_TYPE(omnibox::kOmniboxSteadyStateBackgroundColor)},
 
     {"omnibox-gm3-steady-state-height",
      flag_descriptions::kOmniboxGM3SteadyStateHeightName,
