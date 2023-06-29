e6b4327b20d926253ca4cd19bf3d3ee3639c766b
Cheick Cisse <cheickcisse@google.com>
2023-06-29 17:30:15 +0000

    [DefaultBrowser] Extend the impression limit
    This CL increases the impression limit for the default browser promo to 6 times a year.
    
    Bug: 1458512
    Change-Id: I1e27b8d530a2ad6a1b858960cc1f81d995cf8b6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651772
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164199}

--- ios/chrome/browser/ui/default_promo/promo_handler/default_browser_promo_display_handler.mm
+++ ios/chrome/browser/ui/default_promo/promo_handler/default_browser_promo_display_handler.mm
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (PromoConfig)config {
   return PromoConfig(promos_manager::Promo::DefaultBrowser,
                      &feature_engagement::kIPHiOSPromoDefaultBrowserFeature,
-                     @[ [[ImpressionLimit alloc] initWithLimit:1
+                     @[ [[ImpressionLimit alloc] initWithLimit:4
                                                     forNumDays:365] ]);
 }
 
