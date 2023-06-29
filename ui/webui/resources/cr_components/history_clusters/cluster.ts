0d440f7b07854615a454d715d6a0e0d732637bb7
Marlon Facey <mfacey@chromium.org>
2023-06-29 02:41:23 +0000

    [Side Panel Journeys] Add carousel GM3 updates to side panel journeys
    Add carousel for related searches in side panel journeys. Includes
    svgs, functionality for click and resizing and styles. Will add
    a11y labels for buttons in follow up CL.
    
    Screenshot: http://screenshot/6vuPb3MPDSWbYtk
    
    Bug: 1454582
    Change-Id: Ic401c2945fd31e6a3db063367888ca37aa0fa86d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634637
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163918}

--- ui/webui/resources/cr_components/history_clusters/cluster.ts
+++ ui/webui/resources/cr_components/history_clusters/cluster.ts
@@ -4,8 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './cluster_menu.js';
-import './search_query.js';
 import './history_clusters_shared_style.css.js';
+import './horizontal_carousel.js';
+import './search_query.js';
 import './shared_vars.css.js';
 import './url_visit.js';
 import 'chrome://resources/cr_elements/cr_icons.css.js';
@@ -70,7 +71,7 @@ class HistoryClusterElement extends HistoryClusterElementBase {
       /**
        * Whether the cluster is in the side panel.
        */
-      inSidePanel_: {
+      inSidePanel: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('inSidePanel'),
         reflectToAttribute: true,
@@ -119,7 +120,7 @@ class HistoryClusterElement extends HistoryClusterElementBase {
   query: string;
   private callbackRouter_: PageCallbackRouter;
 
-  private inSidePanel_: boolean;
+  inSidePanel: boolean;
   private onVisitsHiddenListenerId_: number|null = null;
   private onVisitsRemovedListenerId_: number|null = null;
   private unusedLabel_: string;
@@ -319,7 +320,7 @@ class HistoryClusterElement extends HistoryClusterElementBase {
   private computeRelatedSearches_(): SearchQuery[] {
     return this.cluster.relatedSearches.filter(
         (query: SearchQuery, index: number) => {
-          return query && !(this.inSidePanel_ && index > 2);
+          return query && !(this.inSidePanel && index > 2);
         });
   }
 
