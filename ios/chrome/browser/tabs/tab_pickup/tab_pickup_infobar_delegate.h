f093c792cdfebc7a2f8f7d7d57ad0fde68f8451c
Ewann Pelle <ewannpv@chromium.org>
2023-06-29 12:29:26 +0000

    [iOS][TabPickup] Update delegate constructor
    This CL updates the TabPickupInfobarDelegate constructor.
    - The Browser is needed to have access to the webState list in order to
    insert (open) the distant tab.
    - The DistantSession is now directly passed by the browser agent.
    
    Bug: 1457175
    Change-Id: I1bd70aae57eb3ee0253737385bf81e87a4a66fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4656580
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164064}

--- ios/chrome/browser/tabs/tab_pickup/tab_pickup_infobar_delegate.h
+++ ios/chrome/browser/tabs/tab_pickup/tab_pickup_infobar_delegate.h
@@ -14,23 +14,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/favicon/favicon_loader.h"
 #import "ios/chrome/browser/synced_sessions/distant_session.h"
 
-class ChromeBrowserState;
+class Browser;
 class FaviconLoader;
 class GURL;
 
 namespace synced_sessions {
-class SyncedSessions;
+struct DistantSession;
 }
 
 // An interface derived from ConfirmInfoBarDelegate for the Tab Pickup InfoBar.
 class TabPickupInfobarDelegate : public ConfirmInfoBarDelegate {
  public:
-  TabPickupInfobarDelegate(ChromeBrowserState* browser_state_);
+  TabPickupInfobarDelegate(Browser* browser,
+                           const synced_sessions::DistantSession* session);
 
   ~TabPickupInfobarDelegate() override;
 
   // Fetches the favicon image and executes the given procedural block.
   void FetchFavIconImage(ProceduralBlock block_handler);
+
+  // Opens the last synced tab from another device.
+  void OpenDistantTab();
 
   // Getters.
   const std::string GetSessionName() const { return session_name_; }
@@ -43,6 +47,8 @@ class TabPickupInfobarDelegate : public ConfirmInfoBarDelegate {
   InfoBarIdentifier GetIdentifier() const override;
 
  private:
+  // Distant session.
+  raw_ptr<const synced_sessions::DistantSession> session_;
   // Session name.
   std::string session_name_;
   // Time the session is last modified.
@@ -52,10 +58,8 @@ class TabPickupInfobarDelegate : public ConfirmInfoBarDelegate {
   // Favicon of the last synced tab.
   UIImage* favicon_image_ = nullptr;
 
-  // The Associated BrowserState.
-  raw_ptr<ChromeBrowserState> browser_state_ = nullptr;
-  // The instance that owns the DistantTabs to display.
-  std::unique_ptr<synced_sessions::SyncedSessions> synced_sessions_;
+  // The owning Browser.
+  raw_ptr<Browser> browser_ = nullptr;
   // Loads favicons.
   raw_ptr<FaviconLoader> favicon_loader_ = nullptr;
 };
