f49092826c37e08b9f8193114c8957b4e9e9951d
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-06-29 03:58:31 +0000

    [RemoveLegacy] Remove LayoutObject::length().
    ???
    
    As above. There should be no behaviour change.
    
    Bug: 1229581
    Change-Id: I7c81fe18d637939cb9b19650b40a5cac81a161fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653313
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163946}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -2729,11 +2729,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     return LayoutSize();
   }
 
-  virtual unsigned length() const {
-    NOT_DESTROYED();
-    return 1;
-  }
-
   bool IsFloatingOrOutOfFlowPositioned() const {
     NOT_DESTROYED();
     return (IsFloating() || IsOutOfFlowPositioned());
