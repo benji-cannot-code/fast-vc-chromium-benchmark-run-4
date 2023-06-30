5b6a0f998cb9f8b97959eb69f34da338ff208d3c
Jimmy <jimmyxgong@chromium.org>
2023-06-30 17:00:32 +0000

    shortcuts: Introduce error state with restoring defaults
    There are 2 cases when restoring defaults of an action:
    
    1. No error, all default accelerators are available.
    2. Error with conflict, a default accelerator is used by another
    accelerator.
    
    This CL addresses case 2, users are can address the conflict by either
    clicking on the trash icon to ignore the conflict or by
    re-adding/editing the default accelerator to address the conflict.
    
    Screenshot: screenshot.googleplex.com/6fw9oiKUNFu8WDE
    
    Bug: b/216049298
    Test: browser_tests
    Change-Id: Ib935d2b0ff9d1682b91f81648293b8cf33b5ba41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634492
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164752}

--- ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
+++ ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
@@ -29,6 +29,7 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
   private acceleratorsUpdatedPromise: Promise<void>|null = null;
   private restoreDefaultCallCount: number = 0;
   private preventProcessingAcceleratorsCallCount: number = 0;
+  private addAcceleratorCallCount: number = 0;
 
   constructor() {
     this.methods = new FakeMethodResolver();
@@ -57,6 +58,7 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
   reset(): void {
     this.restoreDefaultCallCount = 0;
     this.preventProcessingAcceleratorsCallCount = 0;
+    this.addAcceleratorCallCount = 0;
     this.observables = new FakeObservables();
     this.registerObservables();
   }
@@ -106,6 +108,7 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
   addAccelerator(
       _source: AcceleratorSource, _actionId: number,
       _accelerator: Accelerator): Promise<{result: AcceleratorResultData}> {
+    ++this.addAcceleratorCallCount;
     return this.methods.resolveMethod('addAccelerator');
   }
 
@@ -128,11 +131,11 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
   restoreDefault(_source: AcceleratorSource, _actionId: number):
       Promise<{result: AcceleratorResultData}> {
     ++this.restoreDefaultCallCount;
-    // Always return kSuccess in this fake.
-    const result:
-        AcceleratorResultData = {result: AcceleratorConfigResult.kSuccess};
-    this.methods.setResult('restoreDefault', {result});
     return this.methods.resolveMethod('restoreDefault');
+  }
+
+  setRestoreDefault(result: AcceleratorResultData): void {
+    this.methods.setResult('restoreDefault', {result});
   }
 
   restoreAllDefaults(): Promise<{result: AcceleratorResultData}> {
@@ -153,6 +156,14 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
       _source: AcceleratorSource, _actionId: number,
       _accelerator: Accelerator): Promise<{result: AcceleratorResultData}> {
     return this.methods.resolveMethod('getConflictAccelerator');
+  }
+
+  /**
+   * Set the config result that will be returned when calling
+   * `getConflictAccelerator()`.
+   */
+  setFakeGetConflictAccelerator(result: AcceleratorResultData): void {
+    this.methods.setResult('getConflictAccelerator', {result});
   }
 
   /**
@@ -177,6 +188,10 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
 
   getPreventProcessingAcceleratorsCallCount(): number {
     return this.preventProcessingAcceleratorsCallCount;
+  }
+
+  getAddAcceleratorCallCount(): number {
+    return this.addAcceleratorCallCount;
   }
 
   setFakeHasLauncherButton(hasLauncherButton: boolean): void {
