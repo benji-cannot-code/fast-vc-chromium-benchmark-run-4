ad0d97802a7ef3e8866d9b13bf3884e09366178b
Andre Le <leandre@chromium.org>
2023-06-30 20:35:26 +0000

    VC UI: Fix return to app vertical alignment
    An unnecessary padding was added in the collapsed state of the return to
    app button. Fix this and add a pixel test to prevent future regression.
    
    Fixed: b:288915253
    Change-Id: I2978d1f53aae2a1801bc8dea83c87dcba4d1697f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4658176
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164887}

--- ash/system/video_conference/bubble/return_to_app_panel.h
+++ ash/system/video_conference/bubble/return_to_app_panel.h
@@ -151,6 +151,7 @@ class ASH_EXPORT ReturnToAppPanel : public views::View,
  private:
   friend class ReturnToAppPanelTest;
   friend class VideoConferenceIntegrationTest;
+  friend class BubbleViewPixelTest;
 
   // The container that is the parent of all the buttons inside this panel.
   // Mainly used to handle expand/collapse animation.
