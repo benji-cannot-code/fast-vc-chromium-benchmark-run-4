e269de749876a6a7eb6a4b6736b1aed646216651
Kevin Radtke <kradtke@chromium.org>
2023-06-30 16:48:12 +0000

    Update nudges infinite duration to long duration
    Updates nudge data `has_infinite_duration` to `has_long_duration` so
    nudges that are meant to persist expire after 30 minutes if the user
    has not interacted with them to dismiss them.
    
    Add a `PausableTimer` class that pauses when the user hovers over the
    nudge and resumes on hover exit, instead of restarting the timer.
    
    Bug: b:286338731, b:282805060
    Change-Id: I971ee3adbe4d186cce550feb3ceb757c343bb8df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4655261
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164746}

--- ash/system/video_conference/video_conference_tray_controller.cc
+++ ash/system/video_conference/video_conference_tray_controller.cc
@@ -217,7 +217,7 @@ void VideoConferenceTrayController::MaybeShowSpeakOnMuteOptInNudge(
       &VideoConferenceTrayController::OnSpeakOnMuteNudgeOptInAction,
       weak_ptr_factory_.GetWeakPtr(), /*opt_in=*/true);
 
-  nudge_data.has_infinite_duration = true;
+  nudge_data.has_long_duration = true;
   nudge_data.anchored_to_shelf = true;
 
   AnchoredNudgeManager::Get()->Show(nudge_data);
