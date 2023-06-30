f4d1a21510e62b08c15cd04a6305c681ba5fa878
Kent Tamura <tkent@chromium.org>
2023-06-30 09:40:53 +0000

    [RemoveLegacy] Remove LayoutSize usages in core/view_transition/
    This CL should have no behavior changes.
    
    Bug: 1229581
    Change-Id: I4d83aef66a24ad7a0317f0daab5472c7f011fcb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659464
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164585}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.h
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_rect.h"
 #include "third_party/blink/renderer/platform/allow_discouraged_type.h"
-#include "third_party/blink/renderer/platform/geometry/layout_size.h"
 #include "third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.h"
 #include "third_party/blink/renderer/platform/graphics/view_transition_element_id.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
@@ -45,7 +44,7 @@ class ViewTransitionStyleTracker
   // Properties that transition on container elements.
   struct ContainerProperties {
     ContainerProperties() = default;
-    ContainerProperties(const LayoutSize& size, const gfx::Transform& matrix)
+    ContainerProperties(const PhysicalSize& size, const gfx::Transform& matrix)
         : border_box_size_in_css_space(size), snapshot_matrix(matrix) {}
 
     bool operator==(const ContainerProperties& other) const {
@@ -57,7 +56,7 @@ class ViewTransitionStyleTracker
       return !(*this == other);
     }
 
-    LayoutSize border_box_size_in_css_space;
+    PhysicalSize border_box_size_in_css_space;
 
     // Transforms a point from local space into the snapshot viewport. For
     // details of the snapshot viewport, see README.md.
