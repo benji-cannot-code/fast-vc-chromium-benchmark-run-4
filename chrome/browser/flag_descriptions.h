5f28d562174261cef637a6fe1ed5099067286ba7
Norge Vizcay <vizcay@google.com>
2023-06-30 14:08:10 +0000

    Remove landmark flag from chrome:flags.
    Autofill flags are usually enabled from the command line and is not required to have the in chrome:flags.
    
    Change-Id: I5ab615b96e7907fe82440c7d7c899b4429ea5d73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661150
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164658}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -284,9 +284,6 @@ extern const char kAutofillEnableRemadeDownstreamMetricsDescription[];
 extern const char kAutofillEnableStickyManualFallbackForCardsName[];
 extern const char kAutofillEnableStickyManualFallbackForCardsDescription[];
 
-extern const char kAutofillEnableSupportForLandmarkName[];
-extern const char kAutofillEnableSupportForLandmarkDescription[];
-
 extern const char kAutofillEnableUpdateVirtualCardEnrollmentName[];
 extern const char kAutofillEnableUpdateVirtualCardEnrollmentDescription[];
 
