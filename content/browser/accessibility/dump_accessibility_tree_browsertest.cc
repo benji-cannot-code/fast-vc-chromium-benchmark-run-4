ed068520615b3a266ccbca905231184489661aed
Chris Harrelson <chrishtr@chromium.org>
2023-06-30 22:22:27 +0000

    Parameterize AX browser tests for kSerializeAccessibilityPostLifecycle
    Bug: 1342801
    
    Change-Id: I3073bc9e3e9f44091ba5725bbdd265d004d4658d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4553316
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164941}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -49,8 +49,9 @@ using ui::AXTreeFormatter;
 std::vector<ui::AXPropertyFilter> DumpAccessibilityTreeTest::DefaultFilters()
     const {
   std::vector<AXPropertyFilter> property_filters;
-  if (GetParam() == ui::AXApiType::kMac)
+  if (GetParam().first == ui::AXApiType::kMac) {
     return property_filters;
+  }
 
   property_filters.emplace_back("value='*'", AXPropertyFilter::ALLOW);
   // The value attribute on the document object contains the URL of the
@@ -146,8 +147,9 @@ class DumpAccessibilityTreeTestExceptUIA : public DumpAccessibilityTreeTest {};
 // Parameterize the tests so that each test-pass is run independently.
 struct DumpAccessibilityTreeTestPassToString {
   std::string operator()(
-      const ::testing::TestParamInfo<ui::AXApiType::Type>& i) const {
-    return std::string(i.param);
+      const ::testing::TestParamInfo<std::pair<ui::AXApiType::Type, bool>>& i)
+      const {
+    return std::string(i.param.first) + (i.param.second ? "1" : "0");
   }
 };
 
@@ -167,7 +169,7 @@ INSTANTIATE_TEST_SUITE_P(
 INSTANTIATE_TEST_SUITE_P(
     All,
     DumpAccessibilityTreeTestWithIgnoredNodes,
-    ::testing::ValuesIn(ui::AXInspectTestHelper::TreeTestPasses()),
+    ::testing::ValuesIn(DumpAccessibilityTestBase::TreeTestPasses()),
     DumpAccessibilityTreeTestPassToString());
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityCSSAltText) {
@@ -2070,7 +2072,7 @@ class DumpAccessibilityTreeFencedFrameTest : public DumpAccessibilityTreeTest {
 INSTANTIATE_TEST_SUITE_P(
     All,
     DumpAccessibilityTreeFencedFrameTest,
-    ::testing::ValuesIn(ui::AXInspectTestHelper::TreeTestPasses()),
+    ::testing::ValuesIn(DumpAccessibilityTestBase::TreeTestPasses()),
     DumpAccessibilityTreeTestPassToString());
 
 // TODO(crbug.com/1459385): Re-enable this test
@@ -2618,7 +2620,7 @@ class DumpAccessibilityTreeWithSpeakAsDescriptorTest
 INSTANTIATE_TEST_SUITE_P(
     All,
     DumpAccessibilityTreeWithSpeakAsDescriptorTest,
-    ::testing::ValuesIn(ui::AXInspectTestHelper::TreeTestPasses()),
+    ::testing::ValuesIn(DumpAccessibilityTestBase::TreeTestPasses()),
     DumpAccessibilityTreeTestPassToString());
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeWithSpeakAsDescriptorTest,
@@ -3045,7 +3047,7 @@ class YieldingParserDumpAccessibilityTreeTest
 INSTANTIATE_TEST_SUITE_P(
     All,
     YieldingParserDumpAccessibilityTreeTest,
-    ::testing::ValuesIn(ui::AXInspectTestHelper::TreeTestPasses()),
+    ::testing::ValuesIn(DumpAccessibilityTestBase::TreeTestPassesExceptUIA()),
     DumpAccessibilityTreeTestPassToString());
 
 IN_PROC_BROWSER_TEST_P(YieldingParserDumpAccessibilityTreeTest,
@@ -3323,8 +3325,9 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 #if BUILDFLAG(IS_MAC)
   // The /blink test pass is different on macOS than on other platforms. See
   // https://crbug.com/1314896.
-  if (GetParam() == ui::AXApiType::kBlink)
+  if (GetParam().first == ui::AXApiType::kBlink) {
     return;
+  }
 #endif
   RunHtmlTest(FILE_PATH_LITERAL("no-source-video.html"));
 }
@@ -3333,8 +3336,9 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityVideoControls) {
 #if BUILDFLAG(IS_MAC)
   // The /blink test pass is different on macOS than on other platforms. See
   // https://crbug.com/1314896.
-  if (GetParam() == ui::AXApiType::kBlink)
+  if (GetParam().first == ui::AXApiType::kBlink) {
     return;
+  }
 #endif
   RunHtmlTest(FILE_PATH_LITERAL("video-controls.html"));
 }
