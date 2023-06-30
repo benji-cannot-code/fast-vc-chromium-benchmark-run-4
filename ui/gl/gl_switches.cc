def614dd0ccc0cf330bb9b5dd29e542bd320d531
Maggie Chen <magchen@chromium.org>
2023-06-30 18:34:54 +0000

    Add video processor based Auto HDR support on Windows
    (This CL is copied from https://crrev.com/c/4535570.
    Author: Romain Pokrzywka.)
    
    This adds vp-based SDR-to-HDR conversion on Windows for
    supported NVIDIA GPUs via the ID3D11VideoProcessor interface.
    
    The feature is gated by a new disable_vp_auto_hdr flag
    to limit it to supported vendor/gpu/driver combinations.
    
    Currently only enabled for NVIDIA GPUs with a 545+ driver,
    using the vendor-specific extension.
    
    Similar to RTX Super Resolution, the feature will be off
    by default, requiring manual opt-in by users via a toggle
    in the nvidia control panel application.
    
    Bug: 1445741
    Change-Id: Ib0068306168588bcb61b8196c509497083005588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652913
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164814}

--- ui/gl/gl_switches.cc
+++ ui/gl/gl_switches.cc
@@ -233,6 +233,11 @@ BASE_FEATURE(kNvidiaVpSuperResolution,
              "NvidiaVpSuperResolution",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Allow overlay swapchain to use NVIDIA video processor for trueHDR.
+BASE_FEATURE(kNvidiaVpTrueHDR,
+             "NvidiaVpTrueHDR",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Default to using ANGLE's OpenGL backend
 BASE_FEATURE(kDefaultANGLEOpenGL,
              "DefaultANGLEOpenGL",
