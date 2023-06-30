5d678d8a11938de0847459fe1e9cec959b2484c2
Anders Hartvoll Ruud <andruud@chromium.org>
2023-06-30 17:34:21 +0000

    [@scope] Extract selector features from StyleScope::To
    At some point, the spec was changed to make the scope limit inclusive,
    which means it can also affect the subject. In other words, given
    @scope (.a) to (.b) { ... }, removing/adding '.b' from/to an element
    can cause it to go in and out of scope.
    
    Bug: 1451091
    Change-Id: I2d333d13397f1945bb88e7af8bd25d334eb74787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660482
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164775}

--- third_party/blink/renderer/core/css/rule_feature_set_test.cc
+++ third_party/blink/renderer/core/css/rule_feature_set_test.cc
@@ -2042,7 +2042,7 @@ RefTestData ref_scope_equal_test_data[] = {
 
     // Using "to" keyword:
     {"@scope (.a, .b) to (.c, .d) { div {} }",
-     ":is(.a, .b, .c, .d) div, :is(.a, .b):is(div) {}"},
+     ":is(.a, .b, .c, .d) div, :is(.a, .b):is(.c, .d):is(div) {}"},
 
     // TODO(crbug.com/1280240): Many of the following tests current expect
     // whole-subtree invalidation, because we don't extract any features from
@@ -2064,7 +2064,7 @@ RefTestData ref_scope_equal_test_data[] = {
     {"@scope (.a, .b) { @scope (:scope, .c) { :scope {} } }",
      ":is(.a, .b, .c) *, :is(.a, .b, .c) {}"},
     {"@scope (.a) to (.b) { @scope (.c) to (.d) { .e {} } }",
-     ":is(.a, .b, .c, .d) .e, :is(.a, .c):is(.e) {}"},
+     ":is(.a, .b, .c, .d) .e, :is(.a, .b):is(.c, .d):is(.e) {}"},
 };
 
 class RuleFeatureSetScopeRefTest
