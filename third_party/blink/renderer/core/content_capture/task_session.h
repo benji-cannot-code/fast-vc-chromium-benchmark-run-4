4b5528866ca2004fcd05ad0679a0c68baa8a26cc
Nan Lin <linnan@chromium.org>
2023-06-30 16:47:48 +0000

    [Expired Histograms] Remove content capture task histograms
    Bug: 1447149
    Change-Id: I1fd2838cd69d7047a7ca7b3439d158291022732f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661231
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164745}

--- third_party/blink/renderer/core/content_capture/task_session.h
+++ third_party/blink/renderer/core/content_capture/task_session.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CONTENT_CAPTURE_TASK_SESSION_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CONTENT_CAPTURE_TASK_SESSION_H_
 
-#include <utility>
-
-#include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
 #include "cc/paint/node_id.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -47,11 +44,7 @@ class TaskSession final : public GarbageCollected<TaskSession> {
   // document is GC-ed, see TaskSession::to_document_session_.
   class DocumentSession final : public GarbageCollected<DocumentSession> {
    public:
-    // The callback for total_sent_nodes_ metrics.
-    using SentNodeCountCallback = base::RepeatingCallback<void(int)>;
-
-    DocumentSession(const Document& document,
-                    SentNodeCountCallback& call_back);
+    explicit DocumentSession(const Document& document);
     ~DocumentSession();
     // Add the given |node| to changed node set if the node was sent, return
     // true if succeed.
@@ -110,9 +103,6 @@ class TaskSession final : public GarbageCollected<TaskSession> {
     bool first_data_has_sent_ = false;
     // This is for the metrics to record the total node that has been sent.
     int total_sent_nodes_ = 0;
-    // Histogram could be disabed in low time resolution OS, see
-    // base::TimeTicks::IsHighResolution and ContentCaptureTask.
-    absl::optional<SentNodeCountCallback> callback_;
   };
 
   TaskSession();
@@ -130,14 +120,7 @@ class TaskSession final : public GarbageCollected<TaskSession> {
 
   bool HasUnsentData() const { return has_unsent_data_; }
 
-  void SetSentNodeCountCallback(
-      DocumentSession::SentNodeCountCallback call_back) {
-    callback_ = std::move(call_back);
-  }
-
   void Trace(Visitor*) const;
-
-  void ClearDocumentSessionsForTesting();
 
  private:
   void GroupCapturedContentByDocument(
@@ -153,7 +136,6 @@ class TaskSession final : public GarbageCollected<TaskSession> {
   // DocumentSession, this is used to avoid to iterate all document sessions
   // to find out if there is any of them.
   bool has_unsent_data_ = false;
-  DocumentSession::SentNodeCountCallback callback_;
 };
 
 }  // namespace blink
