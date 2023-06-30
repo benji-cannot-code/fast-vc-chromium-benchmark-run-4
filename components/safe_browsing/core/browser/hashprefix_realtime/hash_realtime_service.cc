a0d6d91fe69727293b428f68d031ee17d5b2643f
thefrog <thefrog@chromium.org>
2023-06-30 14:03:26 +0000

    Add browser tests for hash-prefix real-time lookups
    Similarly to the URL real-time lookup browser tests, this does not mock
    network requests. It instead mocks the results by modifying the local
    HPRT cache. The purpose of this is to avoid adding redundant tests,
    since HashRealTimeService already has sufficient unit test coverage.
    
    Specific things covered:
     - A navigation triggers an interstitial if the HashRealTimeService says
       the URL is unsafe and the feature is enabled. (And conversely, it
       doesn't if the URL is safe or if the feature is disabled.)
     - The HPRT-specific interstitial.*.decision metric is triggered
       appropriately (from a navigation resulting in a warning shown).
     - The HPRT-specific SafeBrowsing.BrowserThrottle.TotalDelay2 metric is
       triggered appropriately from a navigation.
     - The SafeBrowsing.HPRT.Ineligible.* logs are triggered on navigation.
     - The HPRT-specific types of hit reports and CSBRRs are logged.
    
    Bug: 1441654
    Change-Id: I02e53fdc47f012b75074b53023fb929e3307f619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652828
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164655}

--- components/safe_browsing/core/browser/hashprefix_realtime/hash_realtime_service.cc
+++ components/safe_browsing/core/browser/hashprefix_realtime/hash_realtime_service.cc
@@ -146,7 +146,7 @@ bool HashRealTimeService::IsEnhancedProtectionEnabled() {
 bool HashRealTimeService::CanCheckUrl(
     const GURL& url,
     network::mojom::RequestDestination request_destination) {
-  if (VerdictCacheManager::has_artificial_unsafe_url()) {
+  if (VerdictCacheManager::has_artificial_cached_url()) {
     return true;
   }
   return request_destination == network::mojom::RequestDestination::kDocument &&
