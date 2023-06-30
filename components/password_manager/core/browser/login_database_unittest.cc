43511a07e332671029e9400efcf79adf14a135fc
Mohamed Amir Yosef <mamir@chromium.org>
2023-06-30 09:50:36 +0000

    [Passwords] Remove incoming sharing sync metadata tables from loginDB
    IncomingSharingInvitations sync metadata will be moved to a dedicated
    database and don't need to be part of the login database.
    
    This CL is basically reverting https://crrev.com/c/4537581
    
    The API changes to PasswordStoreSync::MetadataStore are left unchanged
    since this is more aligned with the syncer::SyncMetadataStore interface.
    
    Bug: 1444700
    Change-Id: I9566c55808535e79b8affbd30f508b6aedd7f534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650459
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164590}

--- components/password_manager/core/browser/login_database_unittest.cc
+++ components/password_manager/core/browser/login_database_unittest.cc
@@ -1626,14 +1626,12 @@ TEST_F(LoginDatabaseTest, ReportAccountStoreMetricsTest) {
       "PasswordManager.AccountStore.InaccessiblePasswords3", 0, 1);
 }
 
-class LoginDatabaseSyncMetadataTest
-    : public LoginDatabaseTest,
-      public testing::WithParamInterface<syncer::ModelType> {
+class LoginDatabaseSyncMetadataTest : public LoginDatabaseTest {
  public:
-  syncer::ModelType SyncModelType() { return GetParam(); }
+  syncer::ModelType SyncModelType() { return syncer::PASSWORDS; }
 };
 
-TEST_P(LoginDatabaseSyncMetadataTest, NoMetadata) {
+TEST_F(LoginDatabaseSyncMetadataTest, NoMetadata) {
   std::unique_ptr<syncer::MetadataBatch> metadata_batch =
       db().password_sync_metadata_store().GetAllSyncMetadata(SyncModelType());
   ASSERT_THAT(metadata_batch, testing::NotNull());
@@ -1642,7 +1640,7 @@ TEST_P(LoginDatabaseSyncMetadataTest, NoMetadata) {
             metadata_batch->GetModelTypeState().SerializeAsString());
 }
 
-TEST_P(LoginDatabaseSyncMetadataTest, GetAllSyncMetadata) {
+TEST_F(LoginDatabaseSyncMetadataTest, GetAllSyncMetadata) {
   sync_pb::EntityMetadata metadata;
   PasswordStoreSync::MetadataStore& password_sync_metadata_store =
       db().password_sync_metadata_store();
@@ -1693,7 +1691,7 @@ TEST_P(LoginDatabaseSyncMetadataTest, GetAllSyncMetadata) {
       sync_pb::ModelTypeState_InitialSyncState_INITIAL_SYNC_STATE_UNSPECIFIED);
 }
 
-TEST_P(LoginDatabaseSyncMetadataTest, DeleteAllSyncMetadata) {
+TEST_F(LoginDatabaseSyncMetadataTest, DeleteAllSyncMetadata) {
   sync_pb::EntityMetadata metadata;
   PasswordStoreSync::MetadataStore& password_sync_metadata_store =
       db().password_sync_metadata_store();
@@ -1729,7 +1727,7 @@ TEST_P(LoginDatabaseSyncMetadataTest, DeleteAllSyncMetadata) {
   EXPECT_EQ(empty_metadata_batch->TakeAllMetadata().size(), 0u);
 }
 
-TEST_P(LoginDatabaseSyncMetadataTest, WriteThenDeleteSyncMetadata) {
+TEST_F(LoginDatabaseSyncMetadataTest, WriteThenDeleteSyncMetadata) {
   sync_pb::EntityMetadata metadata;
   PasswordStoreSync::MetadataStore& password_sync_metadata_store =
       db().password_sync_metadata_store();
@@ -1769,12 +1767,6 @@ TEST_P(LoginDatabaseSyncMetadataTest, WriteThenDeleteSyncMetadata) {
   EXPECT_EQ(sync_pb::ModelTypeState().SerializeAsString(),
             metadata_batch->GetModelTypeState().SerializeAsString());
 }
-
-INSTANTIATE_TEST_SUITE_P(
-    SyncModelTypes,
-    LoginDatabaseSyncMetadataTest,
-    testing::Values(syncer::PASSWORDS,
-                    syncer::INCOMING_PASSWORD_SHARING_INVITATION));
 
 #if BUILDFLAG(IS_POSIX)
 // Only the current user has permission to read the database.
