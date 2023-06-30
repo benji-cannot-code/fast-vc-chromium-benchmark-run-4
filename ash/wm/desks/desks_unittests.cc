ea142b53f37b47ad9b5c04374a26606c9d3d4588
Daniel Andersson <dandersson@chromium.org>
2023-06-30 19:46:59 +0000

    ash: Fix a leak in DesksTest.DragMiniViewWhileRemoving
    This test removes a desk while in overview mode and then attempts to
    drag it. In order to make the drag part meaningful in the test, it
    overrides the animation duration to be `NORMAL_DURATION`.
    
    When a desk is removed, a self deleting `RemovedMiniViewAnimation`
    object is created. It is implicitly owned by the animation and deletes
    itself when the animation completes. When Jelly is enabled, the test
    ends before the animation has completed and the animation object leaks.
    
    The test was fixed by adding an observer that waits until all animations
    in the `DeskMiniView` has completed.
    
    BUG=b:289423356
    
    Change-Id: I5967977b948e2d134763ea35f9a60612dfc12194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661598
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164862}

--- ash/wm/desks/desks_unittests.cc
+++ ash/wm/desks/desks_unittests.cc
@@ -126,6 +126,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/ash/fake_ime_keyboard.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/compositor/layer.h"
+#include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/compositor_extra/shadow.h"
 #include "ui/display/display.h"
@@ -7585,15 +7586,41 @@ TEST_P(DesksTest, RemoveDeskWhileDragging) {
   ExitOverview();
 }
 
+// Used to wait for all animations to complete in a `LayerAnimator`.
+class LayerAnimationWaiter : public ui::LayerAnimationObserver {
+ public:
+  explicit LayerAnimationWaiter(ui::LayerAnimator* animator)
+      : animator_(animator) {
+    animator_->AddObserver(this);
+  }
+
+  void OnLayerAnimationEnded(ui::LayerAnimationSequence* sequence) override {
+    OnAnimationCompleted();
+  }
+
+  void OnLayerAnimationAborted(ui::LayerAnimationSequence* sequence) override {
+    OnAnimationCompleted();
+  }
+
+  void OnLayerAnimationScheduled(
+      ui::LayerAnimationSequence* sequence) override {}
+
+  void Wait() { run_loop_.Run(); }
+
+ private:
+  void OnAnimationCompleted() {
+    if (animator_->is_animating() == false) {
+      animator_->RemoveObserver(this);
+      run_loop_.Quit();
+    }
+  }
+
+  raw_ptr<ui::LayerAnimator, ExperimentalAsh> animator_;
+  base::RunLoop run_loop_;
+};
+
 // Regression test for the asan failure at https://crbug.com/1274641.
-// TODO(crbug.com/1459376): Re-enable this test
-#if BUILDFLAG(IS_LINUX) && \
-    (defined(ADDRESS_SANITIZER) || defined(LEAK_SANITIZER))
-#define MAYBE_DragMiniViewWhileRemoving DISABLED_DragMiniViewWhileRemoving
-#else
-#define MAYBE_DragMiniViewWhileRemoving DragMiniViewWhileRemoving
-#endif
-TEST_P(DesksTest, MAYBE_DragMiniViewWhileRemoving) {
+TEST_P(DesksTest, DragMiniViewWhileRemoving) {
   NewDesk();
   NewDesk();
 
@@ -7610,6 +7637,7 @@ TEST_P(DesksTest, MAYBE_DragMiniViewWhileRemoving) {
   const gfx::Point desk_preview_center =
       mini_view->GetPreviewBoundsInScreen().CenterPoint();
 
+  LayerAnimationWaiter animation_waiter(mini_view->layer()->GetAnimator());
   {
     // This test requires animation to repro the asan failure.
     ui::ScopedAnimationDurationScaleMode animation_scale(
@@ -7626,6 +7654,8 @@ TEST_P(DesksTest, MAYBE_DragMiniViewWhileRemoving) {
     event_generator->MoveMouseBy(0, 50);
     EXPECT_FALSE(desks_bar_view->IsDraggingDesk());
   }
+
+  animation_waiter.Wait();
 }
 
 // Tests that the right desk containers are visible when switching between desks
