f10d8bafa0f71fd2d713c16ece03291cb4ca4494
Colin Blundell <blundell@chromium.org>
2023-06-30 07:20:15 +0000

    [Viz] Remove resource_format.h includes from //ash and //ui
    Bug: 1378708
    Change-Id: I61e462dad9581b8f6dfc814122a25ebe4d1d0d60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654550
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164544}

--- ash/frame_sink/ui_resource_manager.h
+++ ash/frame_sink/ui_resource_manager.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/frame_sink/ui_resource.h"
 #include "base/containers/flat_map.h"
-#include "components/viz/common/resources/resource_format.h"
 #include "components/viz/common/resources/resource_id.h"
 #include "components/viz/common/resources/transferable_resource.h"
 #include "ui/gfx/geometry/size.h"
--- ash/frame_sink/ui_resource_manager_unittest.cc
+++ ash/frame_sink/ui_resource_manager_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/frame_sink/ui_resource.h"
 #include "base/test/gtest_util.h"
-#include "components/viz/common/resources/resource_format.h"
 #include "components/viz/common/resources/resource_id.h"
 #include "components/viz/common/resources/returned_resource.h"
 #include "components/viz/common/resources/transferable_resource.h"
