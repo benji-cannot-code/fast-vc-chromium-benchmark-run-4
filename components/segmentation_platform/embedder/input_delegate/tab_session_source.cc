856f34e213f9e9d2e71549c5f99d87dd23151eb1
Lei Zhang <thestig@chromium.org>
2023-06-30 18:41:03 +0000

    Remove some spurious includes in some segmentation_platform code
    Remove unnecessary includes, like a Windows-only header included in
    cross-platform code.
    
    Change-Id: I33258d772515cd0471d88e98fdee4a32dfb9eb8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4655001
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164816}

--- components/segmentation_platform/embedder/input_delegate/tab_session_source.cc
+++ components/segmentation_platform/embedder/input_delegate/tab_session_source.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/segmentation_platform/embedder/input_delegate/tab_session_source.h"
 
-#include "base/strings/string_number_conversions_win.h"
-#include "base/strings/string_piece.h"
 #include "base/time/time.h"
 #include "components/segmentation_platform/embedder/tab_fetcher.h"
 #include "components/segmentation_platform/internal/execution/processing/feature_processor_state.h"
@@ -15,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/core/session_types.h"
 #include "components/sync_sessions/open_tabs_ui_delegate.h"
 #include "components/sync_sessions/synced_session.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/page_transition_types.h"
 
 namespace segmentation_platform::processing {
@@ -139,4 +136,5 @@ void TabSessionSource::AddLocalTabInfo(
     const TabFetcher::Tab& tab,
     const FeatureProcessorState& feature_processor_state,
     Tensor& inputs) {}
+
 }  // namespace segmentation_platform::processing
