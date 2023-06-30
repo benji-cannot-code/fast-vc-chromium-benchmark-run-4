86586ece2f29e748abcb4b5ebb5b4100aef20b64
Charlie Hu <chenleihu@google.com>
2023-06-30 15:12:23 +0000

    [SSM] Reduce thread level enable rate on Android dev
    This CL makes SSM profiling only happens on 25% of threads for
    Android Dev Channel.
    
    Bug: 1430519
    Change-Id: I0c0c03c03ff37e2deec38b434e5fad59a8b9a1fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4601814
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164688}

--- chrome/common/profiler/thread_profiler_configuration.cc
+++ chrome/common/profiler/thread_profiler_configuration.cc
@@ -86,7 +86,7 @@ bool ThreadProfilerConfiguration::IsProfilerEnabledForCurrentProcessAndThread(
   return IsProfilerEnabledForCurrentProcess() &&
          platform_configuration_->IsEnabledForThread(
              GetProfileParamsProcess(*base::CommandLine::ForCurrentProcess()),
-             thread);
+             thread, GetReleaseChannel());
 }
 
 bool ThreadProfilerConfiguration::IsStartupProfilingEnabled() const {
