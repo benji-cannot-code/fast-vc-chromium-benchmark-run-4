8edae4a74fdba5e51c12ab9f649a3ee46ef2b52d
Anders Hartvoll Ruud <andruud@chromium.org>
2023-06-30 11:23:43 +0000

    [scroll-animations] Handle whitespace after scroll()/view()
    These functions were incorrectly consumed with range.ConsumeBlock()
    when they should have been consumed with ConsumeFunction(range).
    Only the latter cleans up trailing whitespace.
    
    Fixes a DCHECK in CSSPropertyParser::ParseValueStart.
    
    Bug: 1457743
    Change-Id: I5f1fa6998b8ada0fac4383fe17a4ca208d0930c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660399
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164608}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -4230,7 +4230,7 @@ CSSValue* ConsumeScrollFunction(CSSParserTokenRange& range,
   if (range.Peek().FunctionId() != CSSValueID::kScroll) {
     return nullptr;
   }
-  CSSParserTokenRange block = range.ConsumeBlock();
+  CSSParserTokenRange block = ConsumeFunction(range);
 
   CSSValue* scroller = nullptr;
   CSSIdentifierValue* axis = nullptr;
@@ -4274,7 +4274,7 @@ CSSValue* ConsumeViewFunction(CSSParserTokenRange& range,
     return nullptr;
   }
 
-  CSSParserTokenRange block = range.ConsumeBlock();
+  CSSParserTokenRange block = ConsumeFunction(range);
   CSSIdentifierValue* axis = nullptr;
   CSSValue* inset = nullptr;
 
