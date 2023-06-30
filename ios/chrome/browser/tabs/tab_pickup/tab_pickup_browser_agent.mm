a3eda41464c284038b7a405425c8161f67e8884b
Ewann Pelle <ewannpv@chromium.org>
2023-06-30 15:03:25 +0000

    [iOS][TabPickup] Remove observer
    Bug: 1457175
    Change-Id: I6c9aeed4883836349362578faf4621a1e4a2a423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659900
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164682}

--- ios/chrome/browser/tabs/tab_pickup/tab_pickup_browser_agent.mm
+++ ios/chrome/browser/tabs/tab_pickup/tab_pickup_browser_agent.mm
@@ -52,6 +52,9 @@ TabPickupBrowserAgent::TabPickupBrowserAgent(Browser* browser)
 
 TabPickupBrowserAgent::~TabPickupBrowserAgent() {
   DCHECK(!browser_);
+
+  [[NSNotificationCenter defaultCenter]
+      removeObserver:foreground_notification_observer_];
 }
 
 #pragma mark - BrowserObserver
