306408a247b8756de88497465f041fd3ed6fb7ee
Alex Yang <aycyang@chromium.org>
2023-06-30 21:23:34 +0000

    Do not show fullscreen exit button while in pointer lock mode
    This is only necessary because the current implementation of pointer
    lock doesn't constrain the mouse cursor position, so the exit button may
    still appear even though the mouse cursor is invisible and its position
    is technically undefined. This mitigation will become unnecessary when
    pointer lock is re-implemented using relative motion events.
    
    Bug: 1352323
    Change-Id: I1740120e75c5b2b391394e0805fb3346320d6464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654935
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164921}

--- chrome/browser/ui/views/fullscreen_control/fullscreen_control_host.cc
+++ chrome/browser/ui/views/fullscreen_control/fullscreen_control_host.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "components/fullscreen_control/fullscreen_control_view.h"
 #include "components/version_info/channel.h"
+#include "content/public/browser/render_widget_host_view.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 #include "ui/events/event.h"
 #include "ui/events/keycodes/keyboard_codes.h"
@@ -168,6 +170,16 @@ void FullscreenControlHost::OnMouseEvent(const ui::MouseEvent& event) {
     return;
   }
 
+  // TODO(crbug.com/957455) Do not show fullscreen exit button while in pointer
+  // lock mode. This is only necessary because the current implementation of
+  // pointer lock doesn't constrain the mouse cursor position, so the exit
+  // button may still appear even though the mouse cursor is invisible and its
+  // position is technically undefined. This mitigation will become unnecessary
+  // when pointer lock is re-implemented using relative motion events.
+  if (IsMouseLocked()) {
+    return;
+  }
+
   if (IsExitUiNeeded()) {
     if (IsVisible()) {
       if (event.y() >= CalculateCursorBufferHeight())
@@ -293,6 +305,24 @@ bool FullscreenControlHost::IsExitUiNeeded() {
   return browser_view_->IsFullscreen() &&
          browser_view_->CanUserExitFullscreen() &&
          browser_view_->ShouldHideUIForFullscreen();
+}
+
+bool FullscreenControlHost::IsMouseLocked() {
+  if (!browser_view_) {
+    return false;
+  }
+
+  auto* web_contents = browser_view_->GetActiveWebContents();
+  if (!web_contents) {
+    return false;
+  }
+
+  auto* rwhv = web_contents->GetRenderWidgetHostView();
+  if (!rwhv) {
+    return false;
+  }
+
+  return rwhv->IsMouseLocked();
 }
 
 float FullscreenControlHost::CalculateCursorBufferHeight() const {
