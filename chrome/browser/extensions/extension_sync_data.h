0dc50d616d8a28dbdcc919dd0b6177d83872d2f3
Daniel Murphy <dmurph@chromium.org>
2023-06-30 20:26:20 +0000

    [BMO] Prevent & remove bookmark apps from sync.
    This change detects any bookmark app being installed either from sync
    or from the local sync database, and pushes a deletion message to the
    sync server.
    
    Also tested locally using my profile that had these extensions.
    
    Bug: 1423621, b/274012279
    Change-Id: I48f8d4922888ba029a86698ab9c4c54ae6a39230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544907
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164882}

--- chrome/browser/extensions/extension_sync_data.h
+++ chrome/browser/extensions/extension_sync_data.h
@@ -109,6 +109,10 @@ class ExtensionSyncData {
     return linked_icons_;
   }
 
+  bool is_deprecated_bookmark_app() const {
+    return is_deprecated_bookmark_app_;
+  }
+
  private:
   FRIEND_TEST_ALL_PREFIXES(ExtensionSyncDataTest,
                            ExtensionSyncDataForExtension);
@@ -149,6 +153,7 @@ class ExtensionSyncData {
   syncer::StringOrdinal page_ordinal_;
   extensions::LaunchType launch_type_;
   std::vector<LinkedAppIconInfo> linked_icons_;
+  bool is_deprecated_bookmark_app_ = false;
 };
 
 }  // namespace extensions
