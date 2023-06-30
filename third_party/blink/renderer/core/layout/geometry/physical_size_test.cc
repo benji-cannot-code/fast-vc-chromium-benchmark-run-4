2c6dbe74d5dbbe1923203ae1bcfd36ec067813a2
Kent Tamura <tkent@chromium.org>
2023-06-30 05:11:21 +0000

    [RemoveLegacy] Remove LayoutSize for GetHitTestRectForAdjustment()
    This CL replaces LayoutSize of the return type and an argument of
    GetHitTestRectForAdjustment().
    
    * Add operator*, ExpandedTo(), and ShrunkTo() to PHysicalSize for
      LayoutSize compatibility.
    
    This CL should have no behavior changes.
    
    Bug: 1229581
    Change-Id: I356a642d20ef2a3a4dc8a0c1773b8d450f8e70cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651562
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164510}

--- third_party/blink/renderer/core/layout/geometry/physical_size_test.cc
+++ third_party/blink/renderer/core/layout/geometry/physical_size_test.cc
@@ -10,6 +10,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+TEST(PhysicalSizeTest, MultipleFloat) {
+  EXPECT_EQ(PhysicalSize(100, 7), PhysicalSize(200, 14) * 0.5f);
+  EXPECT_EQ(PhysicalSize(-100, -7), PhysicalSize(200, 14) * -0.5f);
+  EXPECT_EQ(PhysicalSize(0, 0),
+            PhysicalSize(200, 14) * std::numeric_limits<float>::quiet_NaN());
+}
+
+TEST(PhysicalSizeTest, ExpandedTo) {
+  EXPECT_EQ(PhysicalSize(13, 7), PhysicalSize(13, 1).ExpandedTo({10, 7}));
+  EXPECT_EQ(PhysicalSize(17, 1), PhysicalSize(13, 1).ExpandedTo({17, 1}));
+}
+
+TEST(PhysicalSizeTest, ShrunkTo) {
+  EXPECT_EQ(PhysicalSize(10, 1), PhysicalSize(13, 1).ShrunkTo({10, 7}));
+  EXPECT_EQ(PhysicalSize(13, -1), PhysicalSize(13, 1).ShrunkTo({14, -1}));
+}
+
 TEST(PhysicalSizeTest, FitToAspectRatioShrink) {
   PhysicalSize aspect_ratio(50000, 40000);
   EXPECT_EQ(PhysicalSize(1250, 1000),
