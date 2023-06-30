e88089d078619cf878053e329d7f730522cb5436
Colin Blundell <blundell@chromium.org>
2023-06-30 07:12:35 +0000

    [//media] Add killswitch to CHECK in ReadbackTexturePlane
    https://chromium-review.googlesource.com/c/chromium/src/+/4632627 added
    a CHECK that video frames requiring external sampling aren't passed to
    ReadbackTexturePlane(). As we don't actually know 100% that this CHECK
    won't fire, this CL adds a killswitch that we can remove once the CHECK
    has gone through stable.
    
    Bug: 1457134, 1459252
    Change-Id: Ibc9b1b00073f02fb5a217c373a0ce1bb4534033e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654628
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164538}

--- media/base/video_util.cc
+++ media/base/video_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bits.h"
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
@@ -46,6 +47,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 namespace {
+
+// Temporary escape hatch in case VideoFrames requiring external sampling are
+// ever passed to ReadbackTexturePlaneToMemorySyncOOP().
+// TODO(crbug.com/1459252): Remove this after 117 has safely gone through
+// stable.
+BASE_FEATURE(kCheckOnExternalSamplerInTextureReadback,
+             "CheckOnExternalSamplerInTextureReadback",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Helper to apply padding to the region outside visible rect up to the coded
 // size with the repeated last column / row of the visible rect.
@@ -197,7 +206,9 @@ bool ReadbackTexturePlaneToMemorySyncOOP(const VideoFrame& src_frame,
                                          gpu::raster::RasterInterface* ri) {
   // It's not possible to read back individual planes when using external
   // sampling, and this codepath should not be entered in that case.
-  CHECK(!src_frame.RequiresExternalSampler());
+  CHECK(
+      !src_frame.RequiresExternalSampler() ||
+      !base::FeatureList::IsEnabled(kCheckOnExternalSamplerInTextureReadback));
   VideoPixelFormat format = ReadbackFormat(src_frame);
   if (format == PIXEL_FORMAT_UNKNOWN) {
     DLOG(ERROR) << "Readback is not possible for this frame: "
