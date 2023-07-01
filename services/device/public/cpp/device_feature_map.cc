9d40fa2d9ff484d40b04ab8fc33728194a56f30b
Adam Langley <agl@chromium.org>
2023-07-01 19:26:07 +0000

    Revert "webauthn: don't prelink in work profiles."
    This reverts commit d0cc56ee248f99be0b6b208287a139019efa8cc5.
    
    Pinpoint thinks this slowed things down: crbug.com/1459794. It's possible, even though this does work on a background thread, as I don't know how expensive this query is under the covers. Trying a revert to see what happens to the benchmark.
    
    BUG=1459794
    
    Original change's description:
    > webauthn: don't prelink in work profiles.
    >
    > When Chrome is running in an Android work profile, it would still
    > publish prelinking information. This can cause Chrome on the desktop to
    > select that instance of Chrome to connect to for hybrid transactions,
    > but from inside of a work profile, only work profile passkeys can be
    > used. Thus this change suppresses publishing any pre-linking information
    > from inside a work profile.
    >
    > Change-Id: I4ae114e5337af9dcab77dcc46e317fc739a986c1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4656022
    > Commit-Queue: Adam Langley <agl@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1165001}
    
    Change-Id: If589c06370868fc5cd14707ed26b41ae3445d14c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4662505
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165070}

--- services/device/public/cpp/device_feature_map.cc
+++ services/device/public/cpp/device_feature_map.cc
@@ -22,7 +22,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &device::kWebAuthnAndroidCredMan,
     &device::kWebAuthnAndroidHybridClientUi,
     &device::kWebAuthnCableViaCredMan,
-    &device::kWebAuthnDontPrelinkInProfiles,
     &device::kWebAuthnHybridLinkWithoutNotifications,
     &kGenericSensorExtraClasses,
 };
