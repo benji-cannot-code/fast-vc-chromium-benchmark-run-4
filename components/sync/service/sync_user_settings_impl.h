f88cfcaf75af1bec83db2b8283684be58baa2431
Jood Hajeer <jood@google.com>
2023-07-03 15:26:55 +0000

    [iOS] Introduce per-account pref for selected types.
    * Introduced a per-gaia-key dict pref kSelectedTypesPerAccount.
    * Introduced SyncPrefs::GetSelectedTypesForAccount which reads
    from the new dict only if not managed by policy.
    * Changed SyncPrefs::SetSelectedType to write only to the new dict and
    renamed it to SetSelectedTypeForAccount.
    
    And added special case handling into
    * SyncUserSettings::GetSelectedTypes; to call the specific SyncPrefs::GetSelectedTypes*.
    * SyncUserSettings::SetSelectedTypes; to call the corresponding SyncPrefs::SetSelectedType* to the account state.
    
    Bug: 1451509, 1447015, 1446965
    Change-Id: Ib56d7514bba067c54d660aabc417e99d38447903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651870
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165338}

--- components/sync/service/sync_user_settings_impl.h
+++ components/sync/service/sync_user_settings_impl.h
@@ -85,6 +85,8 @@ class SyncUserSettingsImpl : public SyncUserSettings {
   bool IsEncryptedDatatypeEnabled() const;
 
  private:
+  bool ShouldUsePerAccountPrefs() const;
+
   const raw_ptr<SyncServiceCrypto> crypto_;
   const raw_ptr<SyncPrefs> prefs_;
   const raw_ptr<const SyncTypePreferenceProvider> preference_provider_;
