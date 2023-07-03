ad19618a457190ed9a105a00ff26fb9d15a03cf7
Wayne Jackson Jr <wbjacksonjr@google.com>
2023-07-03 12:16:49 +0000

    [android][dropdown 4/n] - Chrome Implementation of Selection Drop-down Menu Delegate
    We are adding a drop-down text selection context menu for users on tablets using either a mouse or touchpad.
    
    This CL provides and sets the Chrome implementation of the content/public interface SelectionDropdownMenuDelegate. The list menu item styling has been updated to be inline with GM3 https://m3.material.io/components/menus/specs on Android devices running API 31 or later.
    
    Bug: 1428022
    Change-Id: I4132874df07e5e557d62f83f56f3356f4df91862
    Low-Coverage-Reason: This CL handles the implementation of the SelectionDropdownMenuDelegate. The usage of this delegate was implemented in the previous CL and is covered by unit tests.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507519
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165286}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActionModeHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActionModeHandler.java
@@ -24,6 +24,7 @@ import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.locale.LocaleManager;
+import org.chromium.chrome.browser.selection.ChromeSelectionDropdownMenuDelegate;
 import org.chromium.chrome.browser.share.ChromeShareExtras;
 import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.share.ShareDelegate.ShareOrigin;
@@ -64,9 +65,10 @@ public class ChromeActionModeHandler {
     public ChromeActionModeHandler(ActivityTabProvider activityTabProvider,
             Callback<String> searchCallback, Supplier<ShareDelegate> shareDelegateSupplier) {
         mInitWebContentsObserver = (webContents) -> {
-            SelectionPopupController.fromWebContents(webContents)
-                    .setActionModeCallback(new ActionModeCallback(mActiveTab, webContents,
-                            searchCallback, shareDelegateSupplier));
+            SelectionPopupController spc = SelectionPopupController.fromWebContents(webContents);
+            spc.setActionModeCallback(new ActionModeCallback(
+                    mActiveTab, webContents, searchCallback, shareDelegateSupplier));
+            spc.setDropdownMenuDelegate(new ChromeSelectionDropdownMenuDelegate());
         };
 
         mActivityTabTabObserver =
