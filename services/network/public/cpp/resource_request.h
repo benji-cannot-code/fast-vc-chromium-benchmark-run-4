51ce04d53fb552006f90f754c399e96cbbac2a83
Yoichi Osato <yoichio@chromium.org>
2023-07-03 02:47:38 +0000

    Add location string to ResourceRequest
    This CL adds a location string to ResourceRequest whereby
    CorsURLLoaderFactory::IsValidRequest() warns with ResourceRequest
    created location.
    
    Bug: 1456586
    Change-Id: Id09ba63391d539ee7f0555c1fb4dc1fe407818c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653138
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165172}

--- services/network/public/cpp/resource_request.h
+++ services/network/public/cpp/resource_request.h
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/debug/crash_logging.h"
+#include "base/location.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/unguessable_token.h"
+#include "build/buildflag.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "net/base/isolation_info.h"
 #include "net/base/request_priority.h"
@@ -111,7 +113,11 @@ struct COMPONENT_EXPORT(NETWORK_CPP_BASE) ResourceRequest {
     int32_t render_process_id = -1;
   };
 
+#if BUILDFLAG(IS_ANDROID)
+  explicit ResourceRequest(const base::Location& = base::Location::Current());
+#else
   ResourceRequest();
+#endif
   ResourceRequest(const ResourceRequest& request);
   ~ResourceRequest();
 
@@ -207,6 +213,10 @@ struct COMPONENT_EXPORT(NETWORK_CPP_BASE) ResourceRequest {
   network::AttributionReportingRuntimeFeatures
       attribution_reporting_runtime_features;
   bool shared_dictionary_writer_enabled = false;
+#if BUILDFLAG(IS_ANDROID)
+  // TODO(https://crbug.com/1456586): Remove this once the issue is fixed.
+  std::string created_location;
+#endif
 };
 
 // This does not accept |kDefault| referrer policy.
