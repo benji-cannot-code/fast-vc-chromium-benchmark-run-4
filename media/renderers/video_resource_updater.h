a90ebc6564f29a1c931f4f59a44b73a8856ebdb5
Colin Blundell <blundell@chromium.org>
2023-07-03 09:07:55 +0000

    [Viz] Remove resource_format.h includes from //media
    Bug: 1378708
    Change-Id: I6bcce2ecbdd32729fdc9dea922a1d5a7fbc7140b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660459
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165238}

--- media/renderers/video_resource_updater.h
+++ media/renderers/video_resource_updater.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/memory_dump_provider.h"
 #include "base/unguessable_token.h"
 #include "components/viz/common/resources/release_callback.h"
-#include "components/viz/common/resources/resource_format.h"
 #include "components/viz/common/resources/resource_id.h"
 #include "components/viz/common/resources/transferable_resource.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
