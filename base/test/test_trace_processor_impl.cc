16149486eba527f34ce18b7fe5d3b051f07a1b3c
Mikhail Khokhlov <khokhlov@google.com>
2023-07-03 18:59:56 +0000

    [TestTraceProcessor] A workaround for PartitionAllocator crashes
    Chrome uses a custom memory allocator in non-component builds that
    manages all allocations on the heap. Since test_trace_processor is a
    separate library even in a non-component build, Chrome is not able to
    free memory allocated by the code in this library. This leads to
    crashes when e.g. vectors created here are passed to the code in
    Chrome tests.
    
    This CL introduces a custom class to hold the result of the query
    execution. Since its destructor is defined in this library, it will
    be freed using the same allocator that was used to create it.
    
    Bug: 1453617
    Change-Id: I87f5011b5e3504c1841716f6f5a733f5f51fd909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4645533
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165392}

--- base/test/test_trace_processor_impl.cc
+++ base/test/test_trace_processor_impl.cc
@@ -9,6 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base::test {
 
+QueryResultOrError::QueryResultOrError(const QueryResult& result)
+    : result_(result) {}
+QueryResultOrError::QueryResultOrError(const std::string& error)
+    : error_(error) {}
+QueryResultOrError::~QueryResultOrError() = default;
+
 TestTraceProcessorImpl::TestTraceProcessorImpl() {
   config_ = std::make_unique<perfetto::trace_processor::Config>();
   trace_processor_ =
@@ -22,12 +28,9 @@ TestTraceProcessorImpl::TestTraceProcessorImpl(TestTraceProcessorImpl&& other) =
 TestTraceProcessorImpl& TestTraceProcessorImpl::operator=(
     TestTraceProcessorImpl&& other) = default;
 
-// We use absl::variant instead of base::expected because we can not have a
-// dependency on base in this shared lib due to symbol conflicts between
-// libtrace_processor and libperfetto.
-absl::variant<TestTraceProcessorImpl::QueryResult, std::string>
-TestTraceProcessorImpl::ExecuteQuery(const std::string& sql) const {
-  TestTraceProcessorImpl::QueryResult result;
+QueryResultOrError TestTraceProcessorImpl::ExecuteQuery(
+    const std::string& sql) const {
+  QueryResultOrError::QueryResult result;
   auto it = trace_processor_->ExecuteQuery(sql);
   // Write column names.
   std::vector<std::string> column_names;
@@ -66,9 +69,9 @@ TestTraceProcessorImpl::ExecuteQuery(const std::string& sql) const {
     result.push_back(row);
   }
   if (!it.Status().ok()) {
-    return it.Status().message();
+    return QueryResultOrError(it.Status().message());
   }
-  return result;
+  return QueryResultOrError(result);
 }
 
 absl::Status TestTraceProcessorImpl::ParseTrace(std::unique_ptr<uint8_t[]> buf,
