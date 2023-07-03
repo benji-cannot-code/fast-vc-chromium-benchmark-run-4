6a93a5b5434c47e03a8c2e8d55bf3af2b778a6c2
Camillia Smith Barnes <cammie@chromium.org>
2023-07-03 23:12:59 +0000

    Private Aggregation: Test attestation in Protected Audience
    We add browser tests to verify that enrollment attestation works as
    intended when the Private Aggregation API is called from the Protected
    Audience API.
    
    Bug: 1457659,1448875
    Change-Id: Ic77e53ad9167653bb330db9dad317ebd2b9b7435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642373
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165417}

--- chrome/test/data/interest_group/bidding_logic.js
+++ chrome/test/data/interest_group/bidding_logic.js
@@ -36,7 +36,8 @@ function reportWin(auctionSignals, perBuyerSignals, sellerSignals,
         '/_report_event_server.html'
   });
   if (typeof privateAggregation !== 'undefined') {
-    privateAggregation.reportContributionForEvent(
+    privateAggregation.contributeToHistogram({bucket: 1n, value: 2});
+    privateAggregation.contributeToHistogramOnEvent(
         'auctionwinner', {bucket: 3n, value: 5});
   }
 }
