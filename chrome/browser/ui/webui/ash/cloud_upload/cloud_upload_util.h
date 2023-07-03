a95ddd51019e6f30ee6bdd7d4448360fe452e4c4
Cassy Chun-Crogan <cassycc@google.com>
2023-07-03 05:04:37 +0000

    [Office upload] Find what caused ACCESS_DENIED in the ODFS upload flow
    If move/copy flow to ODFS failed with an ACCESS_DENIED error, this
    could be due to reauthentication to OneDrive being required or another
    access error. If reauthentication is required then a special error
    notification is shown, otherwise a generic access error notification
    is shown.
    
    To determine if reauthentication is required, get the reauthentication
    required state by doing a special getCustomActions requests of the root
    directory (which gets ODFS metadata).
    
    Add two OneDriveUploadHandlerTest tests:
    - Expect upload flow to fail due to reauthentication to OneDrive being required.
    - Expect upload to fail due to some other access error.
    
    Use FakeProvidedFileSystemOneDrive in the tests and update it to:
    - Mock CreateFile to fail with a given error, in this case
    ACCESS_DENIED, so the upload flow fails with ACCESS_DENIED.
    - Mock GetActions to return the given reauthentication state as part of
    the ODFS metadata. Set this to true for the former test and false for
    the latter.
    
    Bug: b/288022200
    Change-Id: Ic9f7e83e3b7deacfea27a7a2d7670b41b0d706a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4635237
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165188}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_util.h
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_util.h
@@ -71,6 +71,7 @@ const char kReauthenticationRequiredId[] =
 const char kGenericErrorMessage[] = "Something went wrong. Try again.";
 const char kReauthenticationRequiredMessage[] =
     "Sign in to your Microsoft account and then try again";
+const char kGenericOneDriveAccessErrorMessage[] = "Access denied";
 
 // Converts an absolute FilePath into a filesystem URL.
 storage::FileSystemURL FilePathToFileSystemURL(
