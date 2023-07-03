128e38db70635e5e98cf9d52c755b152322d4079
Anders Hartvoll Ruud <andruud@chromium.org>
2023-07-03 10:14:03 +0000

    [@scope] Improve sibling invalidation
    ExtractInvalidationSetFeaturesFromCompound is supposed to return
    the last simple selector of the compound, but did not account for
    the kScopeActivation relation, which acts identically to kSubSelector
    in this situation. This would cause unexpected behavior for selectors
    with :scope (and therefore a kScopeActivation) in the subject, since
    we would not be on a "true" combinator boundary after the call to
    ExtractInvalidationSetFeaturesFromCompound.
    
    Bug: 1451091
    Change-Id: I550ccf87c54c0202f2aa46c856f1a53f218b783f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660044
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165257}

--- third_party/blink/renderer/core/css/rule_feature_set.cc
+++ third_party/blink/renderer/core/css/rule_feature_set.cc
@@ -1038,7 +1038,8 @@ const CSSSelector* RuleFeatureSet::ExtractInvalidationSetFeaturesFromCompound(
     }
 
     if (!simple_selector->NextSimpleSelector() ||
-        simple_selector->Relation() != CSSSelector::kSubSelector) {
+        (simple_selector->Relation() != CSSSelector::kSubSelector &&
+         simple_selector->Relation() != CSSSelector::kScopeActivation)) {
       return simple_selector;
     }
   }
