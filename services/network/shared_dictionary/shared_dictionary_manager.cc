1024859e717de6362b182d8234a5ccea74c18a60
Tsuyoshi Horo <horo@chromium.org>
2023-07-03 00:04:12 +0000

    Introduce LRUCache for SharedDictionaryStorage
    Currently, a SharedDictionaryStorage is kept alive as long as there is a
    CorsURLLoaderFactory object that matches its
    SharedDictionaryIsolationKey. This means that once CorsURLLoaderFactory
    is deleted, Chromium needs to load the metadata from the database again.
    For example, if a site has a cross origin iframe in every pages, a page
    navigation in the site always triggers the database read for the cross
    origin iframe. This is not good for performance.
    
    To mitigate this problem, this CL introduces a LRUCache in
    SharedDictionaryManager to keep SharedDictionaryStorages. The cached
    SharedDictionaryStorage will be evicted when MemoryPressure is detected.
    
    Bug: 1413922
    Change-Id: Ibd56e3c0a74266d666ffcce3ea1841b25e924066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659462
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165156}

--- services/network/shared_dictionary/shared_dictionary_manager.cc
+++ services/network/shared_dictionary/shared_dictionary_manager.cc
@@ -5,11 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/shared_dictionary/shared_dictionary_manager.h"
 
+#include "base/location.h"
 #include "services/network/shared_dictionary/shared_dictionary_manager_in_memory.h"
 #include "services/network/shared_dictionary/shared_dictionary_manager_on_disk.h"
 #include "services/network/shared_dictionary/shared_dictionary_storage.h"
 
 namespace network {
+
+namespace {
+
+// SharedDictionaryManager keeps 10 instances in cache until there is memory
+// pressure.
+constexpr size_t kCachedStorageMaxSize = 10;
+
+}  // namespace
 
 // static
 std::unique_ptr<SharedDictionaryManager>
@@ -38,11 +47,21 @@ std::unique_ptr<SharedDictionaryManager> SharedDictionaryManager::CreateOnDisk(
       std::move(file_operations_factory));
 }
 
-SharedDictionaryManager::SharedDictionaryManager() = default;
+SharedDictionaryManager::SharedDictionaryManager()
+    : cached_storages_(kCachedStorageMaxSize) {
+  memory_pressure_listener_ = std::make_unique<base::MemoryPressureListener>(
+      FROM_HERE, base::BindRepeating(&SharedDictionaryManager::OnMemoryPressure,
+                                     weak_factory_.GetWeakPtr()));
+}
 SharedDictionaryManager::~SharedDictionaryManager() = default;
 
 scoped_refptr<SharedDictionaryStorage> SharedDictionaryManager::GetStorage(
     const net::SharedDictionaryIsolationKey& isolation_key) {
+  auto cached_storages_it = cached_storages_.Get(isolation_key);
+  if (cached_storages_it != cached_storages_.end()) {
+    return cached_storages_it->second;
+  }
+
   auto it = storages_.find(isolation_key);
   if (it != storages_.end()) {
     DCHECK(it->second);
@@ -51,6 +70,10 @@ scoped_refptr<SharedDictionaryStorage> SharedDictionaryManager::GetStorage(
   scoped_refptr<SharedDictionaryStorage> storage = CreateStorage(isolation_key);
   CHECK(storage);
   storages_.emplace(isolation_key, storage.get());
+  if (memory_pressure_level_ ==
+      base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_NONE) {
+    cached_storages_.Put(isolation_key, storage);
+  }
   return storage;
 }
 
@@ -62,6 +85,15 @@ void SharedDictionaryManager::OnStorageDeleted(
 
 base::WeakPtr<SharedDictionaryManager> SharedDictionaryManager::GetWeakPtr() {
   return weak_factory_.GetWeakPtr();
+}
+
+void SharedDictionaryManager::OnMemoryPressure(
+    base::MemoryPressureListener::MemoryPressureLevel level) {
+  memory_pressure_level_ = level;
+  if (memory_pressure_level_ !=
+      base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_NONE) {
+    cached_storages_.Clear();
+  }
 }
 
 }  // namespace network
