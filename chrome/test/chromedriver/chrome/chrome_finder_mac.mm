0306d3f16b9da5a60500ac464e9019d1b743b78e
Avi Drissman <avi@chromium.org>
2023-07-04 06:10:07 +0000

    Convert /chrome/test/chromedriver to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: Icd5eb0cd35f81bf822d2a8d4beda7fd3cedfac4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4663602
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165473}

--- chrome/test/chromedriver/chrome/chrome_finder_mac.mm
+++ chrome/test/chromedriver/chrome/chrome_finder_mac.mm
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/mac/foundation_util.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 void GetApplicationDirs(std::vector<base::FilePath>* app_dirs) {
   base::FilePath user_app_dir;
   if (base::mac::GetUserDirectory(NSApplicationDirectory, &user_app_dir))
