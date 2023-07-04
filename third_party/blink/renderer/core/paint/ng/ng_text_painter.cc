81793e5a9d802b517d6085ea5fb85a62a8c959d3
Fredrik Söderquist <fs@opera.com>
2023-07-04 12:14:25 +0000

    Move paint-order handling to a helper class (PaintOrderArray)
    Rather than having a helper on ComputedStyle, do the "unpacking"
    operation (from EPaintOrder -> EPaintOrderType[3]) in the
    PaintOrderArray class, and allow accessing the indices.
    
    Define storage type for EPaintOrderType, and then improve typing a bit.
    
    Change-Id: Ieb3df48ef29b672473ca22788dd338b0de43ebd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660457
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165576}

--- third_party/blink/renderer/core/paint/ng/ng_text_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_text_painter.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/svg_object_painter.h"
 #include "third_party/blink/renderer/core/paint/timing/paint_timing_detector.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/core/style/paint_order_array.h"
 #include "third_party/blink/renderer/core/style/shadow_list.h"
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/platform/fonts/font.h"
@@ -438,10 +439,11 @@ void NGTextPainter::PaintSvgTextFragment(
       GetSvgStyleToPaint(state, SvgPaintMode::kText, selection_style_scope,
                          has_fill, has_visible_stroke);
 
-  for (int i = 0; i < 3; i++) {
+  const PaintOrderArray paint_order(state.Style().PaintOrder());
+  for (unsigned i = 0; i < 3; i++) {
     absl::optional<LayoutSVGResourceMode> resource_mode;
 
-    switch (state.Style().PaintOrderType(i)) {
+    switch (paint_order[i]) {
       case PT_FILL:
         if (has_fill)
           resource_mode = kApplyToFillMode;
@@ -485,10 +487,11 @@ void NGTextPainter::PaintSvgDecorationsExceptLineThrough(
       GetSvgStyleToPaint(state, SvgPaintMode::kTextDecoration,
                          selection_style_scope, has_fill, has_visible_stroke);
 
-  for (int i = 0; i < 3; i++) {
+  const PaintOrderArray paint_order(state.Style().PaintOrder());
+  for (unsigned i = 0; i < 3; i++) {
     absl::optional<LayoutSVGResourceMode> resource_mode;
 
-    switch (state.Style().PaintOrderType(i)) {
+    switch (paint_order[i]) {
       case PT_FILL:
         if (has_fill)
           resource_mode = kApplyToFillMode;
@@ -530,10 +533,11 @@ void NGTextPainter::PaintSvgDecorationsOnlyLineThrough(
       GetSvgStyleToPaint(state, SvgPaintMode::kTextDecoration,
                          selection_style_scope, has_fill, has_visible_stroke);
 
-  for (int i = 0; i < 3; i++) {
+  const PaintOrderArray paint_order(state.Style().PaintOrder());
+  for (unsigned i = 0; i < 3; i++) {
     absl::optional<LayoutSVGResourceMode> resource_mode;
 
-    switch (state.Style().PaintOrderType(i)) {
+    switch (paint_order[i]) {
       case PT_FILL:
         if (has_fill)
           resource_mode = kApplyToFillMode;
--- third_party/blink/renderer/core/paint/svg_shape_painter.cc
+++ third_party/blink/renderer/core/paint/svg_shape_painter.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/svg_model_object_painter.h"
 #include "third_party/blink/renderer/core/paint/svg_object_painter.h"
 #include "third_party/blink/renderer/core/paint/timing/paint_timing.h"
+#include "third_party/blink/renderer/core/style/paint_order_array.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_context_state_saver.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_record.h"
@@ -75,8 +76,9 @@ void SVGShapePainter::Paint(const PaintInfo& paint_info) {
           style.ShapeRendering() != EShapeRendering::kCrispedges &&
           style.ShapeRendering() != EShapeRendering::kOptimizespeed;
 
-      for (int i = 0; i < 3; i++) {
-        switch (style.PaintOrderType(i)) {
+      const PaintOrderArray paint_order(style.PaintOrder());
+      for (unsigned i = 0; i < 3; i++) {
+        switch (paint_order[i]) {
           case PT_FILL: {
             cc::PaintFlags fill_flags;
             if (!SVGObjectPainter(layout_svg_shape_)
