f6b0d69481698f9f71c0a415239a7a6f68a63233
Pi-Hsun Shih <pihsun@chromium.org>
2023-07-04 10:00:33 +0000

    CCA: remove usage of window.loadTimeData
    From https://crbug.com/1395148 we should directly import loadTimeData
    from chrome://resources/js/load_time_data.js instead of relying on the
    window.loadTimeData.
    
    Update CCA to use import instead. This also removes the need of custom
    loadTimeData type.
    
    Bug: chromium:1395148
    Test: manually check CCA works
    Change-Id: I4a0b32d395eeb63ba24773cbe710b24d7a3eec30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664891
    Commit-Queue: Shik Chen <shik@chromium.org>
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165535}

--- ash/webui/camera_app_ui/resources/js/init.ts
+++ ash/webui/camera_app_ui/resources/js/init.ts
@@ -3,13 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/*
- * strings.m.js is generated when we enable it via UseStringsJs() in webUI
- * controller. When loading it, it will populate data such as localized strings
- * into |window.loadTimeData|.
- */
-import '/strings.m.js';
-
 import {AppWindow} from './app_window.js';
 import * as Comlink from './lib/comlink.js';
 import {TestBridge} from './test_bridge.js';
