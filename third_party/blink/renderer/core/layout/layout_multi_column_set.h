101406aa083a26fcbbd67321d5c956da0157a5ed
Kent Tamura <tkent@chromium.org>
2023-07-04 08:21:02 +0000

    [RemoveLegacy] Remove unused functions in multicol-related code
    This CL removes the following unused functions:
     * Layout*FlowThread::FlowThreadPointToVisualPoint()
     * LayoutMultiColumnSet::PageLogicalTopForOffset()
     * LayoutMultiColumnSet::PositionForPoint()
     * LayoutMultiColumnSet::FlowThreadPortionRect()
     * MultiColumnFragmentainerGroup::HeightAdjustedForRowOffset()
    
    Bug: 1229581
    Change-Id: I2002ba4562e88dcf5d4bc0e1c8f03a85d3ba4dfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664348
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165506}

--- third_party/blink/renderer/core/layout/layout_multi_column_set.h
+++ third_party/blink/renderer/core/layout/layout_multi_column_set.h
@@ -170,8 +170,6 @@ class CORE_EXPORT LayoutMultiColumnSet final : public LayoutBlockFlow {
     return group.ColumnLogicalHeight() * UsedColumnCount();
   }
 
-  LayoutRect FlowThreadPortionRect() const;
-
   // The used CSS value of column-count, i.e. how many columns there are room
   // for without overflowing.
   unsigned UsedColumnCount() const {
@@ -209,10 +207,6 @@ class CORE_EXPORT LayoutMultiColumnSet final : public LayoutBlockFlow {
 
   void AttachToFlowThread();
   void DetachFromFlowThread();
-
-  // The top of the page nearest to the specified block offset. All in
-  // flowthread coordinates.
-  LayoutUnit PageLogicalTopForOffset(LayoutUnit offset) const;
 
   LayoutRect FragmentsBoundingBox(
       const LayoutRect& bounding_box_in_flow_thread) const;
@@ -254,7 +248,6 @@ class CORE_EXPORT LayoutMultiColumnSet final : public LayoutBlockFlow {
   void ComputeLogicalHeight(LayoutUnit logical_height,
                             LayoutUnit logical_top,
                             LogicalExtentComputedValues&) const override;
-  PositionWithAffinity PositionForPoint(const PhysicalOffset&) const override;
 
   void ComputeVisualOverflow() final;
 
