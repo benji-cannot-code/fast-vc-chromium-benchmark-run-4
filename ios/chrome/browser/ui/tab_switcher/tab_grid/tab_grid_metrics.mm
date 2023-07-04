8ef3003085b1fa05fe0d0c7e550964785ee993d7
Aliona DANGLA <alionadangla@google.com>
2023-07-04 12:27:06 +0000

    [iOS] Move tab grid scroll interaction metrics outside of the VC
    Move metrics in their own file in order to import and use it from
    multiple area easily.
    
    Bug: 1457146
    Change-Id: Ied87955e0518cf6b2c730220c138b718108d602a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661034
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165577}

---
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_metrics.mm
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_metrics.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Key of the UMA IOS.TabSwitcher.PageChangeInteraction histogram.
+const char kUMATabSwitcherPageChangeInteractionHistogram[] =
+    "IOS.TabSwitcher.PageChangeInteraction";
--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_context_menu/tab_context_menu_provider.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_constants.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_empty_state_view.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_metrics.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_bottom_toolbar.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_new_tab_button.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_page_control.h"
@@ -69,20 +70,6 @@ namespace {
 typedef NS_ENUM(NSUInteger, TabGridConfiguration) {
   TabGridConfigurationBottomToolbar = 1,
   TabGridConfigurationFloatingButton,
-};
-
-// Key of the UMA IOS.TabSwitcher.PageChangeInteraction histogram.
-const char kUMATabSwitcherPageChangeInteractionHistogram[] =
-    "IOS.TabSwitcher.PageChangeInteraction";
-
-// Values of the UMA IOS.TabSwitcher.PageChangeInteraction histogram.
-enum class TabSwitcherPageChangeInteraction {
-  kNone = 0,
-  kScrollDrag = 1,
-  kControlTap = 2,
-  kControlDrag = 3,
-  kItemDrag = 4,
-  kMaxValue = kItemDrag,
 };
 
 // Computes the page from the offset and width of `scrollView`.
