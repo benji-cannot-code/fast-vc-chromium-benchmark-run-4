f3e585bd701cf4b9db01b8d397a761308eed3009
Ella Ge <eirage@chromium.org>
2023-07-04 22:41:39 +0000

    Use weak_ptr in ambient badge GetClassificationResult callback
    Bug: 1458010
    Change-Id: Ibb7b3cd93dd3c066ba893a772e4b40e93a79c774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666080
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165729}

--- components/webapps/browser/android/ambient_badge_manager.cc
+++ components/webapps/browser/android/ambient_badge_manager.cc
@@ -245,7 +245,7 @@ void AmbientBadgeManager::MaybeShowAmbientBadgeSmart(
       segmentation_platform::kWebAppInstallationPromoKey, prediction_options,
       input_context,
       base::BindOnce(&AmbientBadgeManager::OnGotClassificationResult,
-                     base::Unretained(this)));
+                     weak_factory_.GetWeakPtr()));
 }
 
 void AmbientBadgeManager::OnGotClassificationResult(
