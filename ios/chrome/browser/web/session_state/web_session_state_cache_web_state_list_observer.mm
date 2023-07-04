a5d509c2667ce27865c5e735e42aa1e80c5d3bff
Asami Doi <asamidoi@chromium.org>
2023-07-04 14:46:48 +0000

    [ios] Introduce WebStateListWillChange API
    This CL introduces a new WebStateListWillChange() API to notify a WebState will be changed to observers. WillDetachWebStateAt() and WillCloseWebStateAt() APIs are replaced by it.
    
    This CL is a part of refactoring WebStateListObserver API:
    Step 1 [Done]: Introduce WebStateListDidChange() and replace WebStateReplacedAt() with it
    Step 2 [Done]: Replace WebStateInsertedAt() with WebStateListDidChange()
    Step 3 [Done]: Replace WebStateMoved() with WebStateListDidChange()
    Step 4 [Done]: Replace WebStateDetachedAt() with WebStateListDidChange()
    Step 5 [Now]: Introduce WebStateListWillChange() and Replace Will*() with it
      Step 5-1: [This CL]: Introduce WillWebStateListChanged() and replace Will*() in the C++ side.
      Step 5-2: Replace will*() in the Objective-C side. (https://chromium-review.googlesource.com/c/chromium/src/+/4650671)
      Step 5-3: Simplify the API behavior. See the detail below. (https://chromium-review.googlesource.com/c/chromium/src/+/4650366)
    Step 6: Migrate WebStateActivatedAt() and WebStatePinnedStateChanged() to WebStateListChanged()
    
    The series of CLs in Step 5 will simplify the API behavior.
    Currently, 3 APIs are called in total when a WebState is detached from WebStateList and destroyed:
    1. WillDetachWebStateAt() is called.
    2. WebStateListDidChange() with kDetach is called.
    3. WillCloseWebStateAt() is called.
    4. and then a WebState is destroyed.
    
    After Step 5-3, 2 APIs will be called:
    1. WebStateListWillChange() with kDetach is called.
    2. WebStateListDidChange() with kDetach is called.
    3. and then a WebState is destroyed.
    
    See https://docs.google.com/document/d/1I1QUZAexM1eMI2l2oriZl5GARrtofNI0SxHUPB4h-YE/edit?usp=sharing for more details (internal document).
    
    Bug: 1442546
    Change-Id: I7835d81a2d3bbdd73130651839f78fcab0d1f290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4640748
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165618}

--- ios/chrome/browser/web/session_state/web_session_state_cache_web_state_list_observer.mm
+++ ios/chrome/browser/web/session_state/web_session_state_cache_web_state_list_observer.mm
@@ -26,6 +26,18 @@ WebSessionStateCacheWebStateListObserver::
 
 #pragma mark - WebStateListObserver
 
+void WebSessionStateCacheWebStateListObserver::WebStateListWillChange(
+    WebStateList* web_state_list,
+    const WebStateListChangeDetach& detach_change,
+    const WebStateSelection& selection) {
+  if (!detach_change.is_closing()) {
+    return;
+  }
+
+  [web_session_state_cache_
+      removeSessionStateDataForWebState:detach_change.detached_web_state()];
+}
+
 void WebSessionStateCacheWebStateListObserver::WebStateListDidChange(
     WebStateList* web_state_list,
     const WebStateListChange& change,
@@ -52,14 +64,6 @@ void WebSessionStateCacheWebStateListObserver::WebStateListDidChange(
       // Do nothing when a new WebState is inserted.
       break;
   }
-}
-
-void WebSessionStateCacheWebStateListObserver::WillCloseWebStateAt(
-    WebStateList* web_state_list,
-    web::WebState* web_state,
-    int index,
-    bool user_action) {
-  [web_session_state_cache_ removeSessionStateDataForWebState:web_state];
 }
 
 void WebSessionStateCacheWebStateListObserver::WillBeginBatchOperation(
