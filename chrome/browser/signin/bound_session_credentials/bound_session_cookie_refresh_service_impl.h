617547786a5430bc7d85a511e20edb1d1f654264
Elizaveta Sheremetyeva <sheremetyeva@google.com>
2023-07-04 11:32:09 +0000

    [DBSC] Replace cookie with map of cookies in bound_session_controller.h
    The user must be protected in both 1P and 3P context, therefore DBSC
    needs to support multiple cookies (1P + 3P).This is step 1 of adding
    support for multiple DBSC cookies. In this CL, only the first cookie
    is taken into consideration, support for other cookies will be added
    on follow-up CLs.
    
    Bug: b/279720371
    Change-Id: Ia1e0115151fb5b231242de92e9825fa65f5efd0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654611
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Elizaveta Sheremetyeva <sheremetyeva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165558}

--- chrome/browser/signin/bound_session_credentials/bound_session_cookie_refresh_service_impl.h
+++ chrome/browser/signin/bound_session_credentials/bound_session_cookie_refresh_service_impl.h
@@ -78,7 +78,7 @@ class BoundSessionCookieRefreshServiceImpl
   using BoundSessionCookieControllerFactoryForTesting =
       base::RepeatingCallback<std::unique_ptr<BoundSessionCookieController>(
           const GURL& url,
-          const std::string& cookie_name,
+          const std::vector<std::string>& cookie_names,
           Delegate* delegate)>;
 
   // BoundSessionCookieRefreshService:
