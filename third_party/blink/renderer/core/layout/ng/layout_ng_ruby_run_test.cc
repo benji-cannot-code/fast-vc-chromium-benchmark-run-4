75bc7c976c13f031839fed8b47bac84e8897087c
Kent Tamura <tkent@chromium.org>
2023-07-05 06:34:09 +0000

    Layout: Avoid full-layout by background-color changes of <ruby>
    When the style of an existing LayoutNGRubyBase is updated, text-align
    was reset to the initial value, and triggered a full-layout.  We should
    override UpdateAnonymousChildStyle().
    
    Bug: 1461993
    Change-Id: I456bc2b50f272cc101e9dd319b7ac0597607deb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666660
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165796}

---
+++ third_party/blink/renderer/core/layout/ng/layout_ng_ruby_run_test.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/layout/ng/layout_ng_ruby_run.h"
+
+#include "third_party/blink/renderer/core/layout/ng/layout_ng_ruby_base.h"
+#include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
+
+namespace blink {
+
+class LayoutNGRubyRunTest : public RenderingTest {};
+
+// crbug.com/1461993
+TEST_F(LayoutNGRubyRunTest, StylePropagation) {
+  SetBodyInnerHTML(R"HTML(<ruby id="target">Hello<rt>hola</rt></ruby>)HTML");
+  auto* run_box = To<LayoutNGRubyRun>(
+      GetLayoutObjectByElementId("target")->SlowFirstChild());
+
+  GetElementById("target")->setAttribute(html_names::kStyleAttr,
+                                         AtomicString("background-color:red"));
+  GetDocument().UpdateStyleAndLayoutTree();
+  EXPECT_FALSE(run_box->RubyBase()->NeedsLayout());
+}
+
+}  // namespace blink
