1279db93336e5fdf1e2910fe403747e175feb6d3
Olesia Marukhno <olesiamarukhno@google.com>
2023-07-05 10:15:57 +0000

    [user bypass] Check if the site has high site engagement index
    Return a high confidence level for sites with high site engagement index. Having a high site engagement index doesn't mean that a site is
    more likely to be broken but it serves as a signal that user has strong relationship with the site. It is useful when determining when the
    entry point should be additionally promoted and serves as a mitigation
    to avoid overprompting users.
    
    Bug: 1446230, b:280782678
    Change-Id: Ibdc151ecd068a18643f0917d005254f33fc010d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660709
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165858}

--- components/content_settings/browser/ui/cookie_controls_controller.cc
+++ components/content_settings/browser/ui/cookie_controls_controller.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/features.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/prefs/pref_service.h"
+#include "components/site_engagement/content/site_engagement_service.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/reload_type.h"
 #include "content/public/browser/web_contents.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/site_for_cookies.h"
 
 using base::UserMetricsAction;
+using site_engagement::SiteEngagementService;
 
 namespace {
 
@@ -161,7 +163,12 @@ CookieControlsController::GetConfidenceLevel(CookieControlsStatus status,
     return CookieControlsBreakageConfidenceLevel::kHigh;
   }
 
-  // TODO(crbug.com/1446230): Check if the site has high site engagement index.
+  auto score = SiteEngagementService::Get(GetWebContents()->GetBrowserContext())
+                   ->GetScore(GetWebContents()->GetVisibleURL());
+  if (SiteEngagementService::IsEngagementAtLeast(
+          score, blink::mojom::EngagementLevel::HIGH)) {
+    return CookieControlsBreakageConfidenceLevel::kHigh;
+  }
 
   // TODO(crbug.com/1446230): Record if the entry point was already animated for
   // the site. Only animate it once per site.
