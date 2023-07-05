9f3e589f4fc08da29e61e251dbca5f94a5febb5a
Kent Tamura <tkent@chromium.org>
2023-07-05 04:13:38 +0000

    [RemoveLegacy] Update FragmentsBoundingBox() signature
    This CL changes the argument type from LayoutRect to PhysicalRect, and
    changes the return type from LayoutRect to PhysicalRect.
    
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: I264216e0458a83c9a04baecf54a319bff00e8644
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660638
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165777}

--- third_party/blink/renderer/core/layout/layout_flow_thread.cc
+++ third_party/blink/renderer/core/layout/layout_flow_thread.cc
@@ -130,7 +130,10 @@ bool LayoutFlowThread::MapToVisualRectInAncestorSpaceInternal(
   // A flow thread should never be an invalidation container.
   DCHECK_NE(ancestor, this);
   transform_state.Flatten();
-  LayoutRect rect(transform_state.LastPlanarQuad().BoundingBox());
+  gfx::RectF bounding_box = transform_state.LastPlanarQuad().BoundingBox();
+  PhysicalRect rect(LayoutUnit(bounding_box.x()), LayoutUnit(bounding_box.y()),
+                    LayoutUnit(bounding_box.width()),
+                    LayoutUnit(bounding_box.height()));
   rect = FragmentsBoundingBox(rect);
   transform_state.SetQuad(gfx::QuadF(gfx::RectF(rect)));
   return LayoutBlockFlow::MapToVisualRectInAncestorSpaceInternal(
@@ -203,8 +206,7 @@ void LayoutFlowThread::AddOutlineRects(
   // block direction anyway. As far as the inline direction (the column
   // progression direction) is concerned, we'll just include the full height of
   // each column involved. Should be good enough.
-  collector.AddRect(PhysicalRectToBeNoop(
-      FragmentsBoundingBox(flow_collector.Rect().ToLayoutRect())));
+  collector.AddRect(FragmentsBoundingBox(flow_collector.Rect()));
 }
 
 bool LayoutFlowThread::NodeAtPoint(HitTestResult& result,
@@ -231,12 +233,12 @@ void LayoutFlowThread::GenerateColumnSetIntervalTree() {
             column_set->LogicalBottomInFlowThread(), column_set));
 }
 
-LayoutRect LayoutFlowThread::FragmentsBoundingBox(
-    const LayoutRect& layer_bounding_box) const {
+PhysicalRect LayoutFlowThread::FragmentsBoundingBox(
+    const PhysicalRect& layer_bounding_box) const {
   NOT_DESTROYED();
   DCHECK(!column_sets_invalidated_);
 
-  LayoutRect result;
+  PhysicalRect result;
   for (const auto& column_set : multi_column_set_list_)
     result.Unite(column_set->FragmentsBoundingBox(layer_bounding_box));
 
