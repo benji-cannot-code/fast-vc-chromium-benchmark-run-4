3fc570cbb8b46c162c180d75ea3fb51f23d0abd1
Joseph Park <josephjoopark@chromium.org>
2023-07-05 18:43:40 +0000

    Reland "Re-enable test after approving Skia image"
    This is a reland of commit 798e0d1800ee03eb2f5cab57b1d3f6c869d0d18e
    
    Original change's description:
    > Re-enable test after approving Skia image
    >
    > Bug: 1456674
    > Change-Id: I6bddeb2719add92ba38fecc066e634be7d74567e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652957
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1164183}
    
    Bug: 1456674
    Change-Id: I5592945c0886d40fe11623f0ea2c3d343fd67579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666229
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166060}

--- ui/views/examples/examples_unittest.cc
+++ ui/views/examples/examples_unittest.cc
@@ -14,13 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views::examples {
 
-// Fails on Win reliably: crbug.com/1456674
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_TestViewsExamplesLaunches DISABLED_TestViewsExamplesLaunches
-#else
-#define MAYBE_TestViewsExamplesLaunches TestViewsExamplesLaunches
-#endif
-TEST(ExamplesTest, MAYBE_TestViewsExamplesLaunches) {
+TEST(ExamplesTest, TestViewsExamplesLaunches) {
 #if BUILDFLAG(IS_WIN)
   if (ui::NativeTheme::GetInstanceForNativeUi()->ShouldUseDarkColors()) {
     GTEST_SKIP() << "Host is in dark mode; skipping test";
