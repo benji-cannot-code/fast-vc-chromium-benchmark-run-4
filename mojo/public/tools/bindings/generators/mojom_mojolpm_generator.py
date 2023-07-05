04cd3b57b6ab44999c25f890b15f25e773633b5a
Mark Brand <markbrand@google.com>
2023-07-05 14:46:17 +0000

    MojoLPM: Change camel casing to match protobuf everywhere.
    Bug: 1459947
    Change-Id: Ie7e5a548ab09ee3a449630cf9c08345cf8450839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661101
    Commit-Queue: Mark Brand <markbrand@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165938}

--- mojo/public/tools/bindings/generators/mojom_mojolpm_generator.py
+++ mojo/public/tools/bindings/generators/mojom_mojolpm_generator.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Generates C++ source files from a mojom.Module."""
 import os
 import sys
+from functools import partial
 from generators.mojom_cpp_generator import _NameFormatter as CppNameFormatter
 from generators.mojom_cpp_generator import Generator as CppGenerator
 from generators.mojom_cpp_generator import IsNativeOnlyKind, NamespaceToArray
@@ -270,7 +271,7 @@ class Generator(CppGenerator):
         "is_typemapped_kind": self._IsTypemappedKind,
         "is_union_kind": mojom.IsUnionKind,
         "to_unnullable_kind": self._ToUnnullableKind,
-        "under_to_camel": self._UnderToCamel,
+        "under_to_camel": partial(self._UnderToCamel, digits_split=True)
     }
     return cpp_filters
 
