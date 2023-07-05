6b1815771b6e50154635ae25ec1346d2c816ecff
Chad Duffin <chadduffin@chromium.org>
2023-07-05 20:24:56 +0000

    network: Add DisconnectionsWithoutUserAction.ShillError
    This change introduces a new metric to capture the Shill errors
    responsible for network disconnections without user action.
    
    Bug: b/276781196
    Test: Unit tests.
    Change-Id: Ic7e3c8165f0e3d6a451ec7d62ae3b030749e11b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642849
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166120}

--- chromeos/ash/components/network/metrics/connection_info_metrics_logger.cc
+++ chromeos/ash/components/network/metrics/connection_info_metrics_logger.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/containers/flat_set.h"
+#include "chromeos/ash/components/network/metrics/connection_results.h"
 #include "chromeos/ash/components/network/metrics/network_metrics_helper.h"
 #include "chromeos/ash/components/network/network_connection_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
@@ -169,7 +170,8 @@ void ConnectionInfoMetricsLogger::AttemptLogConnectionStateResult(
     const ConnectionInfo& curr_info) const {
   if (curr_info.status == ConnectionInfo::Status::kConnected) {
     NetworkMetricsHelper::LogConnectionStateResult(
-        curr_info.guid, NetworkMetricsHelper::ConnectionState::kConnected);
+        curr_info.guid, NetworkMetricsHelper::ConnectionState::kConnected,
+        /*shill_error=*/absl::nullopt);
     return;
   }
 
@@ -181,7 +183,8 @@ void ConnectionInfoMetricsLogger::AttemptLogConnectionStateResult(
       NetworkState::ErrorIsValid(curr_info.shill_error)) {
     NetworkMetricsHelper::LogConnectionStateResult(
         curr_info.guid,
-        NetworkMetricsHelper::ConnectionState::kDisconnectedWithoutUserAction);
+        NetworkMetricsHelper::ConnectionState::kDisconnectedWithoutUserAction,
+        ShillErrorToConnectResult(curr_info.shill_error));
     return;
   }
 }
