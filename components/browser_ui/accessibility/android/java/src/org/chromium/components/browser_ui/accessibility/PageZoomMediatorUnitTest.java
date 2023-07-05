bc3132c0556c612c7b5e595a12a8a77cdde7425e
Aashna Sheth <aashnas@google.com>
2023-07-05 18:08:22 +0000

    [Page Zoom] Cleanup HostZoomMap code
    This CL makes some small clean-up changes to the HostZoomMap code.
    It (1) changes the SYSTEM_FONT_SCALE variable capitalization and adds
    getters/setters to match other non-final static variables and
    (2) moves the adjustZoomLevel() method to a more reasonable location,
    matching the pattern of other methods.
    
    AX-Relnotes: N/A
    Bug: 1459631, b/265491863
    
    Change-Id: Iab7ca5dd9f9fce34579c54b6fb32cbba592d6efe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4658184
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166030}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomMediatorUnitTest.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomMediatorUnitTest.java
@@ -80,7 +80,7 @@ public class PageZoomMediatorUnitTest {
         mModel.set(PageZoomProperties.DEFAULT_ZOOM_FACTOR, 0.0);
         mMediator = new PageZoomMediator(mModel);
 
-        HostZoomMap.SYSTEM_FONT_SCALE = 1.0f;
+        HostZoomMap.setSystemFontScale(1.0f);
         when(mHostZoomMapMock.getDesktopSiteZoomScale(mWebContentsMock)).thenReturn(1.0);
     }
 
@@ -101,7 +101,7 @@ public class PageZoomMediatorUnitTest {
     @Test
     public void testDecreaseZoom_SmallConfiguration() {
         // Verify that calling decrease zoom method sends expected value to native code.
-        HostZoomMap.SYSTEM_FONT_SCALE = 0.85f;
+        HostZoomMap.setSystemFontScale(0.85f);
         when(mHostZoomMapMock.getZoomLevel(any())).thenReturn(2.22);
         mMediator.setWebContents(mWebContentsMock);
         Assert.assertEquals(
@@ -116,7 +116,7 @@ public class PageZoomMediatorUnitTest {
     @Test
     public void testDecreaseZoom_SmallConfiguration_DesktopUserAgent() {
         // Verify that calling decrease zoom method sends expected value to native code.
-        HostZoomMap.SYSTEM_FONT_SCALE = 0.85f;
+        HostZoomMap.setSystemFontScale(0.85f);
         when(mHostZoomMapMock.getDesktopSiteZoomScale(mWebContentsMock)).thenReturn(1.1);
         when(mHostZoomMapMock.getZoomLevel(any())).thenReturn(2.22);
         mMediator.setWebContents(mWebContentsMock);
@@ -146,7 +146,7 @@ public class PageZoomMediatorUnitTest {
     @Test
     public void testIncreaseZoom_LargeConfiguration() {
         // Verify that calling increase zoom method sends expected value to native code.
-        HostZoomMap.SYSTEM_FONT_SCALE = 1.3f;
+        HostZoomMap.setSystemFontScale(1.3f);
         when(mHostZoomMapMock.getZoomLevel(any())).thenReturn(2.22);
         mMediator.setWebContents(mWebContentsMock);
         Assert.assertEquals(
@@ -161,7 +161,7 @@ public class PageZoomMediatorUnitTest {
     @Test
     public void testIncreaseZoom_LargeConfiguration_DesktopUserAgent() {
         // Verify that calling increase zoom method sends expected value to native code.
-        HostZoomMap.SYSTEM_FONT_SCALE = 1.3f;
+        HostZoomMap.setSystemFontScale(1.3f);
         when(mHostZoomMapMock.getDesktopSiteZoomScale(mWebContentsMock)).thenReturn(1.1);
         when(mHostZoomMapMock.getZoomLevel(any())).thenReturn(2.22);
         mMediator.setWebContents(mWebContentsMock);
@@ -179,7 +179,7 @@ public class PageZoomMediatorUnitTest {
         // Verify that calling increase zoom method sends expected value to native code when the OS
         // font scale and desktop site zoom scale in use could cause the rendered zoom factor value
         // to overflow beyond the maximum displayed zoom factor value.
-        HostZoomMap.SYSTEM_FONT_SCALE = 2.0f;
+        HostZoomMap.setSystemFontScale(2.0f);
         when(mHostZoomMapMock.getDesktopSiteZoomScale(mWebContentsMock)).thenReturn(1.1);
         // Assume that the currently rendered zoom value is maximum, that is 300% or a zoom factor
         // of 6.03. For the zoom scales defined above, this will be equivalent to a display value of
