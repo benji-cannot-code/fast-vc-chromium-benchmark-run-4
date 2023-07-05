7a40aa59f6da6e503bc047a09154eb2966d2d7a8
Ian Wells <iwells@chromium.org>
2023-07-05 18:38:41 +0000

    Update playback hooks
    - remove ChromeProvided since it's unneeded now
    - take interfaces out of .../external since there's no point in the
      separate directory now
    - don't use ZonedDateTime because it isn't available before API level 26
    
    Bug: b/289131818
    Change-Id: I37955d9a3e85cfd83aa5443ac8525ccb7d87b2bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659689
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166057}

--- chrome/android/modules/readaloud/internal/java/src/org/chromium/chrome/modules/readaloud/ReadAloudPlaybackHooksImpl.java
+++ chrome/android/modules/readaloud/internal/java/src/org/chromium/chrome/modules/readaloud/ReadAloudPlaybackHooksImpl.java
@@ -3,11 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.readaloud;
+package org.chromium.chrome.modules.readaloud;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.chrome.browser.android.httpclient.ChromeHttpClient;
 
 /** Empty implementation of ReadAloudPlaybackHooks. */
 public class ReadAloudPlaybackHooksImpl implements ReadAloudPlaybackHooks {
-    public static ReadAloudPlaybackHooks getInstance() {
+    public static ReadAloudPlaybackHooks getInstance(
+            Context context, @Nullable String apiKeyOverride, ChromeHttpClient chromeHttpClient) {
         return new ReadAloudPlaybackHooks() {};
     }
 }
