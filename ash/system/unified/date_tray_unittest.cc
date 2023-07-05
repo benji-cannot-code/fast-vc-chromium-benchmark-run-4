41a92e81b18ca8bf9698658f2e7e48606b58a88e
Artsiom Mitrokhin <amitrokhin@google.com>
2023-07-05 14:41:17 +0000

    Show glanceables for active session state and regular users only
    Bug: b/289063666
    Change-Id: I4b49b4f4b967df01002f0fed04335f68eefe0bd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4657262
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165936}

--- ash/system/unified/date_tray_unittest.cc
+++ ash/system/unified/date_tray_unittest.cc
@@ -23,16 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "base/time/time_override.h"
+#include "components/account_id/account_id.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
-#include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/combobox/combobox.h"
 #include "ui/views/view_utils.h"
 #include "ui/wm/public/activation_change_observer.h"
 #include "ui/wm/public/activation_client.h"
-
-namespace {
-constexpr char kUserEmail[] = "test_user@gmail.com";
-}
 
 namespace ash {
 
@@ -64,7 +60,7 @@ class DateTrayTest
 
     AshTestBase::SetUp();
 
-    SimulateUserLogin(GetAccountId(kUserEmail));
+    SimulateUserLogin(account_id_);
 
     widget_ = CreateFramelessTestWidget();
     widget_->SetContentsView(std::make_unique<views::View>());
@@ -81,7 +77,7 @@ class DateTrayTest
       fake_glanceables_tasks_client_ =
           std::make_unique<FakeGlanceablesTasksClient>();
       Shell::Get()->glanceables_v2_controller()->UpdateClientsRegistration(
-          GetAccountId(kUserEmail),
+          account_id_,
           GlanceablesV2Controller::ClientsRegistration{
               .tasks_client = fake_glanceables_tasks_client_.get()});
     }
@@ -153,16 +149,14 @@ class DateTrayTest
     GetUnifiedSystemTray()->CloseBubble();
   }
 
-  AccountId GetAccountId(base::StringPiece email) {
-    return AccountId::FromUserEmail(std::string(email));
-  }
-
   FakeGlanceablesTasksClient* fake_glanceables_tasks_client() {
     return fake_glanceables_tasks_client_.get();
   }
 
  private:
   std::unique_ptr<views::Widget> widget_;
+  AccountId account_id_ =
+      AccountId::FromUserEmailGaiaId("test_user@gmail.com", "123456");
   std::unique_ptr<FakeGlanceablesTasksClient> fake_glanceables_tasks_client_;
   bool observering_activation_changes_ = false;
 
--- ash/system/unified/glanceable_tray_bubble_view.cc
+++ ash/system/unified/glanceable_tray_bubble_view.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/unified/glanceable_tray_bubble_view.h"
 
+#include "ash/public/cpp/session/user_info.h"
 #include "ash/shelf/shelf.h"
+#include "ash/shell.h"
 #include "ash/system/time/calendar_view.h"
 #include "ash/system/tray/detailed_view_delegate.h"
 #include "ash/system/tray/tray_bubble_view.h"
@@ -13,9 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_utils.h"
 #include "ash/system/unified/classroom_bubble_view.h"
 #include "ash/system/unified/tasks_bubble_view.h"
+#include "base/check.h"
+#include "components/session_manager/session_manager_types.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
-#include "ui/views/controls/label.h"
 #include "ui/views/controls/scroll_view.h"
 
 namespace ash {
@@ -48,15 +51,23 @@ void GlanceableTrayBubbleView::UpdateBubble() {
   child_glanceable_container->SetOrientation(
       views::LayoutOrientation::kVertical);
 
+  const auto* const session_controller = Shell::Get()->session_controller();
+  CHECK(session_controller);
+  const bool should_show_non_calendar_glanceables =
+      session_controller->IsActiveUserSessionStarted() &&
+      session_controller->GetSessionState() ==
+          session_manager::SessionState::ACTIVE &&
+      session_controller->GetUserSession(0)->user_info.has_gaia_account;
+
   // TODO(b:277268122): set real contents for glanceables view.
-  if (!tasks_bubble_view_) {
+  if (should_show_non_calendar_glanceables && !tasks_bubble_view_) {
     tasks_bubble_view_ = child_glanceable_container->AddChildView(
         std::make_unique<TasksBubbleView>(detailed_view_delegate_.get()));
   }
 
   // TODO(b:283370562): only add teacher/student classroom glanceables when
   // the user is enrolled in courses.
-  if (!classroom_bubble_view_) {
+  if (should_show_non_calendar_glanceables && !classroom_bubble_view_) {
     classroom_bubble_view_ = child_glanceable_container->AddChildView(
         std::make_unique<ClassroomBubbleView>(detailed_view_delegate_.get()));
     // Add spacing between the classroom bubble and the previous bubble.
