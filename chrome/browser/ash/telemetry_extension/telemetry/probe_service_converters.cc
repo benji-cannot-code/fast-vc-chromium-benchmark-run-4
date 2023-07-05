316175e055f1b5a75e821ced3fc0ed0b23f35b92
Ethan Cheng <yycheng@google.com>
2023-07-05 08:46:44 +0000

    [Telemetry] Refactor ConvertProbePtr template to detect output type
    Change the function signature of ConvertProbePtr to allow for an
    output type, which will allow for reuse of |ConvertProbePtr| with
    absl::optional types. This output type will be automatically detected.
    
    Rename older converter with Legacy prefix since we want to use
    absl::optional<primitive type> in the future.
    
    Bug: b/283446589
    Test: 1. unit_tests --gtest_filter="*ProbeService*"
    
    Change-Id: I4e0ef14df8954c22123de01800ba87652c137150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650129
    Commit-Queue: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165825}

--- chrome/browser/ash/telemetry_extension/telemetry/probe_service_converters.cc
+++ chrome/browser/ash/telemetry_extension/telemetry/probe_service_converters.cc
@@ -63,7 +63,7 @@ cros_healthd::mojom::ProbeCategoryEnum Convert(
 
 }  // namespace
 
-namespace unchecked {
+namespace unchecked::probe {
 
 crosapi::mojom::ProbeErrorPtr UncheckedConvertPtr(
     cros_healthd::mojom::ProbeErrorPtr input) {
@@ -71,7 +71,7 @@ crosapi::mojom::ProbeErrorPtr UncheckedConvertPtr(
                                          std::move(input->msg));
 }
 
-crosapi::mojom::UInt64ValuePtr UncheckedConvertPtr(
+crosapi::mojom::UInt64ValuePtr LegacyUncheckedConvertPtr(
     cros_healthd::mojom::NullableUint64Ptr input) {
   return crosapi::mojom::UInt64Value::New(input->value);
 }
@@ -191,7 +191,7 @@ crosapi::mojom::ProbeBatteryInfoPtr UncheckedConvertPtr(
       Convert(input->charge_now), Convert(input->current_now),
       std::move(input->technology), std::move(input->status),
       std::move(input->manufacture_date),
-      ConvertProbePtr(std::move(input->temperature)));
+      LegacyConvertProbePtr(std::move(input->temperature)));
 }
 
 crosapi::mojom::ProbeBatteryResultPtr UncheckedConvertPtr(
@@ -217,7 +217,8 @@ crosapi::mojom::ProbeNonRemovableBlockDeviceInfoPtr UncheckedConvertPtr(
       Convert(input->read_time_seconds_since_last_boot),
       Convert(input->write_time_seconds_since_last_boot),
       Convert(input->io_time_seconds_since_last_boot),
-      ConvertProbePtr(std::move(input->discard_time_seconds_since_last_boot)));
+      LegacyConvertProbePtr(
+          std::move(input->discard_time_seconds_since_last_boot)));
 }
 
 crosapi::mojom::ProbeNonRemovableBlockDeviceResultPtr UncheckedConvertPtr(
@@ -545,7 +546,7 @@ crosapi::mojom::ProbeTelemetryInfoPtr UncheckedConvertPtr(
       ConvertProbePtr(std::move(input->bus_result)));
 }
 
-}  // namespace unchecked
+}  // namespace unchecked::probe
 
 crosapi::mojom::ProbeErrorType Convert(cros_healthd::mojom::ErrorType input) {
   switch (input) {
