a06bbe724550b389b375fa6797bd676b10a37fb8
Anton Maliev <amaliev@chromium.org>
2023-07-05 19:28:16 +0000

    [DIPS] Group and clean up 3PC settings checks.
    Earlier we held off on checking for 3P cookie exceptions until the deletion step, so we could collect UKM metrics even for exempted sites. We decided we don't need these metrics anymore, so we can start checking for any cookie exceptions prior to recording a bounce.
    
    This has the following side effects:
    - The global BlockThirdPartyCookies setting is included in this check, so we now avoid recording a bounce when 3PC is enabled by default (unless the user has an override under Block Sites). NOTE that this will  greatly reduce our metrics volume, some weird data effects are expected as this rolls out.
    - Block Sites overrides are now accounted for, even when 3PC are default-allowed (crbug.com/1446350).
    - Fine-grained exceptions (1P+3P pairs) are now accounted for (crbug.com/1458994).
    
    OBSOLETE_HISTOGRAMS=Removing DIPSCookieMode enums with 3PC blocking turned off - we now check for this before recording a bounce, so all data entries will have 3PC blocking on.
    
    Change-Id: I480b410bb47ac634e975366de8069e66dc036102
    Bug: 1458994,1446350
    Fixed: 1458994,1446350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653572
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Joshua Hood <jdh@chromium.org>
    Commit-Queue: Anton Maliev <amaliev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166085}

--- chrome/browser/dips/dips_storage_unittest.cc
+++ chrome/browser/dips/dips_storage_unittest.cc
@@ -856,9 +856,9 @@ TEST_F(DIPSStoragePrepopulateTest, ExistingStorageAndInteractionTimes) {
   // First record interaction and storage for the site, then call Prepopulate().
   storage_.AsyncCall(&DIPSStorage::RecordInteraction)
       .WithArgs(GURL("http://site"), interaction_time,
-                DIPSCookieMode::kStandard);
+                DIPSCookieMode::kBlock3PC);
   storage_.AsyncCall(&DIPSStorage::RecordStorage)
-      .WithArgs(GURL("http://site"), storage_time, DIPSCookieMode::kStandard);
+      .WithArgs(GURL("http://site"), storage_time, DIPSCookieMode::kBlock3PC);
   storage_.AsyncCall(&DIPSStorage::Prepopulate)
       .WithArgs(prepopulate_time, std::vector<std::string>{"site"},
                 base::DoNothing());
@@ -882,7 +882,7 @@ TEST_F(DIPSStoragePrepopulateTest, ExistingStorageTime) {
 
   // Record only storage for the site, then call Prepopulate().
   storage_.AsyncCall(&DIPSStorage::RecordStorage)
-      .WithArgs(GURL("http://site"), storage_time, DIPSCookieMode::kStandard);
+      .WithArgs(GURL("http://site"), storage_time, DIPSCookieMode::kBlock3PC);
   storage_.AsyncCall(&DIPSStorage::Prepopulate)
       .WithArgs(prepopulate_time, std::vector<std::string>{"site"},
                 base::DoNothing());
@@ -906,7 +906,7 @@ TEST_F(DIPSStoragePrepopulateTest, ExistingInteractionTime) {
   // Record only storage for the site, then call Prepopulate().
   storage_.AsyncCall(&DIPSStorage::RecordInteraction)
       .WithArgs(GURL("http://site"), interaction_time,
-                DIPSCookieMode::kStandard);
+                DIPSCookieMode::kBlock3PC);
   storage_.AsyncCall(&DIPSStorage::Prepopulate)
       .WithArgs(prepopulate_time, std::vector<std::string>{"site"},
                 base::DoNothing());
