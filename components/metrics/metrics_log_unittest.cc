370db4a5d810000cdc623ddc4b9f986a40b43176
Luc Nguyen <lucnguyen@google.com>
2023-07-05 19:41:08 +0000

    Record log closed time on main thread
    MetricsLog::CloseLog() may be called on the background. For ongoing
    logs, it will record the current time using some "clock". This "clock"
    is leaky, and all it does is return Time::Now(). So there's no chance
    of a UAF (use-after-free).
    
    However, there is work under way to use a different clock, which is
    *not* leaky (owned by BrowserProcessImpl). See crrev.com/c/3322058
    
    So there is a chance that this new "clock" would be accessed on the
    background thread after it has been released (e.g., the user shuts
    down Chrome while the background task is running).
    
    This CL records the "closed time" on the main thread, before the task
    to finalize the log is posted on the background thread. Though, this
    may mean that the "closed time" is slightly off (I would guess that
    it would be only be by a few milliseconds under normal circumstances).
    
    
    NOTE: as per the current behaviour, this CL will only record the
    "closed" time for ongoing logs. I.e., it is not recorded for stability
    and independent logs.
    
    Bug: 1257449
    Change-Id: I340711e0c2be95cb10a1601fd4647266e7b805ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4665900
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166093}

--- components/metrics/metrics_log_unittest.cc
+++ components/metrics/metrics_log_unittest.cc
@@ -209,7 +209,7 @@ TEST_F(MetricsLogTest, BasicRecord) {
 
   std::string encoded;
   log.FinalizeLog(/*truncate_events=*/false, client_.GetVersionString(),
-                  &encoded);
+                  log.GetCurrentClockTime(/*record_time_zone=*/true), &encoded);
 
   // A couple of fields are hard to mock, so these will be copied over directly
   // for the expected output.
@@ -320,13 +320,14 @@ TEST_F(MetricsLogTest, FinalizeLog) {
   // the one that was used when the log was created.
   std::string encoded;
   log.FinalizeLog(/*truncate_events=*/true, client_.GetVersionString(),
-                  &encoded);
+                  log.GetCurrentClockTime(/*record_time_zone=*/true), &encoded);
 
   // Finalize |log2|. We do not truncate events, and we pass a different version
   // string than the one that was used when the log was created.
   client_.set_version_string(kNewVersionString);
   std::string encoded2;
   log2.FinalizeLog(/*truncate_events=*/false, client_.GetVersionString(),
+                   log.GetCurrentClockTime(/*record_time_zone=*/true),
                    &encoded2);
 
   ChromeUserMetricsExtension parsed;
@@ -362,8 +363,9 @@ TEST_F(MetricsLogTest, Timestamps_InitialStabilityLog) {
                  clock.get(), nullptr, &client_);
   clock->SetNow(base::Time::FromTimeT(2));
   std::string encoded;
+  // Don't set the close_time param since this is an initial stability log.
   log.FinalizeLog(/*truncate_events=*/false, client_.GetVersionString(),
-                  &encoded);
+                  /*close_time=*/absl::nullopt, &encoded);
   ChromeUserMetricsExtension parsed;
   ASSERT_TRUE(parsed.ParseFromString(encoded));
   EXPECT_FALSE(parsed.has_time_log_created());
@@ -380,8 +382,9 @@ TEST_F(MetricsLogTest, Timestamps_IndependentLog) {
                  clock.get(), nullptr, &client_);
   clock->SetNow(base::Time::FromTimeT(2));
   std::string encoded;
+  // Don't set the close_time param since this is an independent log.
   log.FinalizeLog(/*truncate_events=*/false, client_.GetVersionString(),
-                  &encoded);
+                  /*close_time=*/absl::nullopt, &encoded);
   ChromeUserMetricsExtension parsed;
   ASSERT_TRUE(parsed.ParseFromString(encoded));
   EXPECT_FALSE(parsed.has_time_log_created());
@@ -399,7 +402,7 @@ TEST_F(MetricsLogTest, Timestamps_OngoingLog) {
   clock->SetNow(base::Time::FromTimeT(2));
   std::string encoded;
   log.FinalizeLog(/*truncate_events=*/false, client_.GetVersionString(),
-                  &encoded);
+                  log.GetCurrentClockTime(/*record_time_zone=*/true), &encoded);
   ChromeUserMetricsExtension parsed;
   ASSERT_TRUE(parsed.ParseFromString(encoded));
   EXPECT_TRUE(parsed.has_time_log_created());
