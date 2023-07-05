d11b8082e5abf0d121dfd7f8bb1b77bf3727afac
Hira Mahmood <hiramahmood@google.com>
2023-07-05 16:15:34 +0000

    [iOS] Enable feature kAppStoreRating by default
    This CL enables the kAppStoreRating feature by default on TOT and
    deletes its field trial testing config. It will be followed by a
    change to the Finch config to update the max value.
    
    Bug: None
    Change-Id: I34789f5a4b7603b27951ca414ae7addc330e90ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667428
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Auto-Submit: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165968}

--- ios/chrome/browser/ui/app_store_rating/features.mm
+++ ios/chrome/browser/ui/app_store_rating/features.mm
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 BASE_FEATURE(kAppStoreRating,
              "AppStoreRating",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsAppStoreRatingEnabled() {
   return base::FeatureList::IsEnabled(kAppStoreRating);
