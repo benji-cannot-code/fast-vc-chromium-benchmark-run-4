efd520171ffc623ad8a028958ae272fff5afb05e
Adem Derinel <derinel@google.com>
2023-07-05 07:17:06 +0000

    [Android][CredMan] Add histogram for createCredential requests
    Low-Coverage-Reason: Reflection exception is not expected
    Bug: 1427843
    Change-Id: I653b3fb4d4c70cb7018ef33293fa9af3cd9e8a20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659508
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165800}

--- chrome/android/junit/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestRobolectricTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestRobolectricTest.java
@@ -46,6 +46,7 @@ import org.chromium.blink.mojom.PublicKeyCredentialDescriptor;
 import org.chromium.blink.mojom.PublicKeyCredentialRequestOptions;
 import org.chromium.blink.mojom.ResidentKeyRequirement;
 import org.chromium.components.webauthn.CredManMetricsHelper;
+import org.chromium.components.webauthn.CredManMetricsHelper.CredManCreateRequestEnum;
 import org.chromium.components.webauthn.CredManMetricsHelper.CredManGetRequestEnum;
 import org.chromium.components.webauthn.CredManMetricsHelper.CredManPrepareRequestEnum;
 import org.chromium.components.webauthn.Fido2ApiCallHelper;
@@ -175,6 +176,8 @@ public class Fido2CredentialRequestRobolectricTest {
         assertThat(credManRequest.getCandidateQueryData().containsKey("com.android.chrome.CHANNEL"))
                 .isTrue();
         assertThat(mCallback.getStatus()).isEqualTo(Integer.valueOf(AuthenticatorStatus.SUCCESS));
+        verify(mMetricsHelper, times(1))
+                .recordCredManCreateRequestHistogram(CredManCreateRequestEnum.SUCCESS);
     }
 
     @Test
@@ -246,6 +249,7 @@ public class Fido2CredentialRequestRobolectricTest {
                         -> mCallback.onRegisterResponse(responseStatus, response),
                 errorStatus -> mCallback.onError(errorStatus));
         assertThat(mFido2ApiCallHelper.mMakeCredentialCalled).isTrue();
+        verify(mMetricsHelper, times(0)).recordCredManCreateRequestHistogram(anyInt());
     }
 
     @Test
@@ -263,6 +267,8 @@ public class Fido2CredentialRequestRobolectricTest {
                 errorStatus -> mCallback.onError(errorStatus));
         assertThat(mCallback.getStatus())
                 .isEqualTo(Integer.valueOf(AuthenticatorStatus.NOT_ALLOWED_ERROR));
+        verify(mMetricsHelper, times(1))
+                .recordCredManCreateRequestHistogram(CredManCreateRequestEnum.CANCELLED);
     }
 
     @Test
@@ -280,6 +286,8 @@ public class Fido2CredentialRequestRobolectricTest {
                 errorStatus -> mCallback.onError(errorStatus));
         assertThat(mCallback.getStatus())
                 .isEqualTo(Integer.valueOf(AuthenticatorStatus.UNKNOWN_ERROR));
+        verify(mMetricsHelper, times(1))
+                .recordCredManCreateRequestHistogram(CredManCreateRequestEnum.FAILURE);
     }
 
     @Test
