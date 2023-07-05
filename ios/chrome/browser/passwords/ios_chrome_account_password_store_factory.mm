bc482cafbb3f59d4ab371ba832767e0e9714d339
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-07-05 13:53:26 +0000

    Reland "Clean up kEnablePasswordsAccountStorage in ios/chrome/"
    This is a reland of commit eb7a46628f59d0b13fdde14b4ab1f796e8f47b9b
    
    Original change's description:
    > Clean up kEnablePasswordsAccountStorage in ios/chrome/
    >
    > The feature was enabled by default in M116.
    >
    > Fixed: 1459194
    > Change-Id: Id09843c2c55660c14fa0b0b1bd2cb8756a1fc6a0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4656440
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Victor Vianna <victorvianna@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1164102}
    
    Bug: 1459194
    Change-Id: I83bd967788468fd0981f4f219c71ebfaf5487b1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660561
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165909}

--- ios/chrome/browser/passwords/ios_chrome_account_password_store_factory.mm
+++ ios/chrome/browser/passwords/ios_chrome_account_password_store_factory.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 #import <utility>
 
+#import "base/check.h"
 #import "base/feature_list.h"
 #import "base/functional/callback_helpers.h"
 #import "base/no_destructor.h"
@@ -37,10 +38,9 @@ scoped_refptr<password_manager::PasswordStoreInterface>
 IOSChromeAccountPasswordStoreFactory::GetForBrowserState(
     ChromeBrowserState* browser_state,
     ServiceAccessType access_type) {
-  if (!base::FeatureList::IsEnabled(
-          password_manager::features::kEnablePasswordsAccountStorage)) {
-    return nullptr;
-  }
+  CHECK(base::FeatureList::IsEnabled(
+      password_manager::features::kEnablePasswordsAccountStorage));
+
   // `browser_state` gets always redirected to a non-Incognito one below, so
   // Incognito & IMPLICIT_ACCESS means that incognito browsing session would
   // result in traces in the normal BrowserState without the user knowing it.
@@ -72,7 +72,7 @@ IOSChromeAccountPasswordStoreFactory::~IOSChromeAccountPasswordStoreFactory() =
 scoped_refptr<RefcountedKeyedService>
 IOSChromeAccountPasswordStoreFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
-  DCHECK(base::FeatureList::IsEnabled(
+  CHECK(base::FeatureList::IsEnabled(
       password_manager::features::kEnablePasswordsAccountStorage));
 
   ChromeBrowserState* browser_state =
