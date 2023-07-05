297e50fa4010a322fecbd0d053c0cd179993b815
Ben Reich <benreich@chromium.org>
2023-07-05 23:56:55 +0000

    [FilesBulkPinning] Update all OS settings strings
    Bug: b:289736284
    Test: browser_tests --gtest_filter=*GoogleDriveHandler*
    Change-Id: I5ac656ff9c3c5bcaaf43f301f533bad76f291c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4665101
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166226}

--- chrome/test/data/webui/settings/chromeos/os_files_page/google_drive_page_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_files_page/google_drive_page_test.ts
@@ -38,9 +38,9 @@ class GoogleDriveTestBrowserProxy extends TestBrowserProxy implements
  * Generate the expected text for space available.
  */
 function generateRequiredSpaceText(
-    requiredSpace: string, remainingSpace: string): string {
-  return `This will use about ${requiredSpace} leaving ${
-      remainingSpace} available.`;
+    requiredSpace: string, freeSpace: string): string {
+  return `This will use about ${requiredSpace}. You currently have ${
+      freeSpace} available.`;
 }
 
 suite('<settings-google-drive-subpage>', function() {
@@ -201,7 +201,7 @@ suite('<settings-google-drive-subpage>', function() {
 
         // Mock space values and the `kSuccess` stage via the browser proxy.
         testBrowserProxy.observerRemote.onProgress({
-          remainingSpace: '1,024 KB',
+          freeSpace: '1,024 KB',
           requiredSpace: '512 MB',
           stage: Stage.kSuccess,
           isError: false,
@@ -215,7 +215,7 @@ suite('<settings-google-drive-subpage>', function() {
 
         // Mock a failure case via the browser proxy.
         testBrowserProxy.observerRemote.onProgress({
-          remainingSpace: '1,024 KB',
+          freeSpace: '1,024 KB',
           requiredSpace: '512 MB',
           stage: Stage.kCannotGetFreeSpace,
           isError: true,
@@ -268,7 +268,7 @@ suite('<settings-google-drive-subpage>', function() {
         // Mock space values and the `kNotEnoughSpace` stage via the browser
         // proxy.
         testBrowserProxy.observerRemote.onProgress({
-          remainingSpace: '512 MB',
+          freeSpace: '512 MB',
           requiredSpace: '1,024 MB',
           stage: Stage.kNotEnoughSpace,
           isError: true,
@@ -277,7 +277,7 @@ suite('<settings-google-drive-subpage>', function() {
         flush();
 
         // Wait for the page to update the progress information.
-        await assertAsync(() => page.remainingSpace === '512 MB');
+        await assertAsync(() => page.freeSpace === '512 MB');
 
         // Click the bulk pinning toggle.
         bulkPinningToggle.click();
@@ -311,7 +311,7 @@ suite('<settings-google-drive-subpage>', function() {
         // Mock space values and the `kNotEnoughSpace` stage via the browser
         // proxy.
         testBrowserProxy.observerRemote.onProgress({
-          remainingSpace: 'x',
+          freeSpace: 'x',
           requiredSpace: 'y',
           stage: Stage.kCannotGetFreeSpace,
           isError: true,
@@ -320,7 +320,7 @@ suite('<settings-google-drive-subpage>', function() {
         flush();
 
         // Wait for the page to update the progress information.
-        await assertAsync(() => page.remainingSpace === 'x');
+        await assertAsync(() => page.freeSpace === 'x');
 
         // Click the bulk pinning toggle.
         bulkPinningToggle.click();
@@ -351,13 +351,14 @@ suite('<settings-google-drive-subpage>', function() {
     testBrowserProxy.handler.setResultFor(
         'getTotalPinnedSize', {size: '100 MB'});
     page.onNavigated();
-    await assertAsync(() => offlineStorageSubtitle.innerText === '100 MB');
+    await assertAsync(
+        () => offlineStorageSubtitle.innerText === 'Using 100 MB');
 
     // Mock an empty pinned size (size is there but an empty string).
     testBrowserProxy.handler.setResultFor('getTotalPinnedSize', {size: ''});
     page.onNavigated();
-
-    await assertAsync(() => offlineStorageSubtitle.innerText === 'Unknown');
+    await assertAsync(
+        () => offlineStorageSubtitle.innerText === 'Using Unknown');
   });
 
   test(
@@ -365,7 +366,7 @@ suite('<settings-google-drive-subpage>', function() {
       async function() {
         page.setPrefValue('drivefs.bulk_pinning_enabled', false);
         testBrowserProxy.observerRemote.onProgress({
-          remainingSpace: 'x',
+          freeSpace: 'x',
           requiredSpace: 'y',
           stage: Stage.kStopped,
           isError: false,
@@ -375,7 +376,7 @@ suite('<settings-google-drive-subpage>', function() {
 
         page.setPrefValue('drivefs.bulk_pinning_enabled', true);
         testBrowserProxy.observerRemote.onProgress({
-          remainingSpace: 'x',
+          freeSpace: 'x',
           requiredSpace: 'y',
           stage: Stage.kSyncing,
           isError: false,
