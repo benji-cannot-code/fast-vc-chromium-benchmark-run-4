7dcc3244cf5f26a02c30b001b077ef46a766e1ee
Sharmin Zaman <sharminzaman@google.com>
2023-07-05 02:17:50 +0000

    [App Details] Display 'System App' type and tooltip for certain apps.
    Apps with an InstallReason = kSystem will display a 'System App' type
    and its respective tooltip, while other preinstalled apps will display
    its app type. Neither will have an install source displayed. This is to
    distinguish between preinstalled apps.
    
    Bug: 1295875
    Change-Id: I5056bc55cc06369001925e01a28dd2c242e312d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4663464
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Sharmin Zaman <sharminzaman@google.com>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165754}

--- chrome/test/data/webui/settings/chromeos/os_apps_page/app_management_page/app_details_item_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_apps_page/app_management_page/app_details_item_test.ts
@@ -7,7 +7,7 @@ import 'chrome://os-settings/lazy_load.js';
 
 import {AppManagementAppDetailsItem} from 'chrome://os-settings/lazy_load.js';
 import {AppManagementStore, updateSelectedAppId} from 'chrome://os-settings/os_settings.js';
-import {App, AppType, InstallSource} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
+import {App, AppType, InstallReason, InstallSource} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {assertEquals, assertNull, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
@@ -153,9 +153,9 @@ suite('<app-management-app-details-item>', () => {
     assertEquals('System App', typeAndSource.textContent!.trim());
   });
 
-  test('System install source', async function() {
+  test('System install reason', async function() {
     await addApp({
-      installSource: InstallSource.kSystem,
+      installReason: InstallReason.kSystem,
     });
 
     const typeAndSourceText =
@@ -169,7 +169,7 @@ suite('<app-management-app-details-item>', () => {
     const tooltipText = infoIconTooltip.querySelector('#tooltipText');
     assertTrue(!!tooltipText);
     assertEquals(
-        'This system app is preinstalled on your device',
+        'This app is preinstalled on your device',
         tooltipText.textContent!.trim());
   });
 
