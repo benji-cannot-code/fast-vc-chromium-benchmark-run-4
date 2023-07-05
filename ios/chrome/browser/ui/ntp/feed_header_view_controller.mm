e78ab694f11aee1f3ea7822f7e04fcc1485095b1
Scott Yoder <scottyoder@google.com>
2023-07-05 17:57:04 +0000

    [ios] Fix issues caused by changes to dynamic type font sizes
    This fix does several things:
     * In places where the CreateDynamicFont() helper is used, the font will
       not update itself automatically - the view needs to update it when
       the trait collection changes
     * When the font size changes, some views need to be forced to re-layout
     * The MagicStackModuleContainer needed some slight tweaks to handle
       larger text in the title
    
    Fixed: 1454214, 1458368
    Change-Id: Ic28db6573d7272eb8b1b1ef5a49113cfd2aa8901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4658046
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166022}

--- ios/chrome/browser/ui/ntp/feed_header_view_controller.mm
+++ ios/chrome/browser/ui/ntp/feed_header_view_controller.mm
@@ -144,6 +144,21 @@ NSInteger kFeedSymbolPointSize = 17;
   [self applyHeaderConstraints];
 }
 
+#pragma mark - UITraitEnvironment
+
+- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  if (previousTraitCollection.preferredContentSizeCategory !=
+      self.traitCollection.preferredContentSizeCategory) {
+    UIFont* font = [self fontForTitle];
+    self.titleLabel.font = font;
+    NSDictionary* attributes =
+        [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
+    [self.segmentedControl setTitleTextAttributes:attributes
+                                         forState:UIControlStateNormal];
+  }
+}
+
 #pragma mark - Public
 
 - (void)toggleBackgroundBlur:(BOOL)blurred animated:(BOOL)animated {
@@ -376,10 +391,8 @@ NSInteger kFeedSymbolPointSize = 17;
 - (UILabel*)createTitleLabel {
   UILabel* titleLabel = [[UILabel alloc] init];
   titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-  titleLabel.font =
-      CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightMedium);
+  titleLabel.font = [self fontForTitle];
   titleLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
-  titleLabel.adjustsFontForContentSizeCategory = YES;
   titleLabel.accessibilityIdentifier =
       ntp_home::DiscoverHeaderTitleAccessibilityID();
   titleLabel.text = [self feedHeaderTitleText];
@@ -399,8 +412,7 @@ NSInteger kFeedSymbolPointSize = 17;
   [segmentedControl setApportionsSegmentWidthsByContent:NO];
 
   // Set text font and color.
-  UIFont* font =
-      CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightMedium);
+  UIFont* font = [self fontForTitle];
   NSDictionary* attributes =
       [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
   [segmentedControl setTitleTextAttributes:attributes
@@ -417,6 +429,10 @@ NSInteger kFeedSymbolPointSize = 17;
       kNTPFeedHeaderSegmentedControlIdentifier;
 
   return segmentedControl;
+}
+
+- (UIFont*)fontForTitle {
+  return CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightMedium);
 }
 
 // Configures and returns the dot indicating that there is new content in the
