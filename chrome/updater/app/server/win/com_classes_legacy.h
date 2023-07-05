213a8058fa0045191eec8691ae031d0ec9f85ebf
Daniel Cheng <dcheng@chromium.org>
2023-07-05 18:16:19 +0000

    Use a fixed flat map to store typelib info.
    This is more memory efficient than std::unordered_set and requires no
    heap allocations. Several other immutable maps of IIDs to IIDs are also
    migrated to use flat_map, since that's a more compact representation for
    a map that's initialized once and never mutated afterwards.
    
    Since the std::hash specialization for IID is no longer needed, remove
    it.
    
    Finally, this fixes a correctness issue where the static typelib index
    map was constructed using pointers to non-static wchar_t string
    constants. The compiler probably ended up putting these constants in
    .rodata, which is why this worked at all, but this is not something that
    valid C++ should depend on.
    
    Bug: 1459785
    Change-Id: I07467526e9c0abd46560b0c02844004ec1e0c5cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661396
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166038}

--- chrome/updater/app/server/win/com_classes_legacy.h
+++ chrome/updater/app/server/win/com_classes_legacy.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wrl/implements.h>
 
 #include <string>
-#include <unordered_map>
 #include <vector>
 
+#include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
@@ -62,8 +62,8 @@ template <typename TDualInterface, typename... TInterfaces>
 class IDispatchImpl
     : public WrlRuntimeDispatchClass<TDualInterface, TInterfaces...> {
  public:
-  IDispatchImpl(const std::unordered_map<IID, IID>& user_iid_map,
-                const std::unordered_map<IID, IID>& system_iid_map)
+  IDispatchImpl(const base::flat_map<IID, IID, IidComparator>& user_iid_map,
+                const base::flat_map<IID, IID, IidComparator>& system_iid_map)
       : iid_map_(IsSystemInstall() ? system_iid_map : user_iid_map),
         hr_load_typelib_(InitializeTypeInfo()) {}
   IDispatchImpl(const IDispatchImpl&) = default;
@@ -165,7 +165,7 @@ class IDispatchImpl
   }
 
  private:
-  const std::unordered_map<IID, IID> iid_map_;
+  const base::flat_map<IID, IID, IidComparator> iid_map_;
   Microsoft::WRL::ComPtr<ITypeInfo> type_info_;
   const HRESULT hr_load_typelib_;
 };
