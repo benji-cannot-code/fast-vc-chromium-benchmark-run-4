48041e5a83034493a9fe2bdac8abb6e280cb157e
Regan Hsu <hsuregan@google.com>
2023-07-05 20:53:16 +0000

    [CrOS Jelly] Fix NetworkFeaturePodControllerUnittest when Jelly enabled.
    Fixed: b/289459923
    Change-Id: I4bc8fd60025f5ce8b5c7f5991668b108567e1ef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667448
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166134}

--- ash/system/network/network_feature_pod_controller_unittest.cc
+++ ash/system/network/network_feature_pod_controller_unittest.cc
@@ -98,7 +98,8 @@ class NetworkFeaturePodControllerTest
   void SetUp() override {
     feature_list_.InitWithFeatureStates(
         {{features::kQsRevamp, IsQsRevampEnabled()},
-         {chromeos::features::kJellyroll, IsQsRevampEnabled()}});
+         {chromeos::features::kJellyroll, IsQsRevampEnabled()},
+         {chromeos::features::kJelly, IsQsRevampEnabled()}});
 
     AshTestBase::SetUp();
 
