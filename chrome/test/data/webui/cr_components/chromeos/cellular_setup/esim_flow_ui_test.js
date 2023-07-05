a57ef22e15b9e3247b339d173eed9cca69231d96
Saphia Petard <saphiap@google.com>
2023-07-05 20:33:42 +0000

    WebUI: CrOS Settings: Inform no profiles were found by scan
    If a scan is performed, but no profiles are discovered, the activation
    code page now informs the user.
    
    Screenshots of this change will be included in a related, upcoming CL.
    
    Bug: b/278134141
    
    Change-Id: Id12832069d1f27d3fabb99221e4a471e4f5a2540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632072
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Saphia Petard <saphiap@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166125}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.js
@@ -297,7 +297,6 @@ suite('CrComponentsEsimFlowUiTest', function() {
           /*backButtonState*/ ButtonState.HIDDEN);
       // Insert an activation code.
       activationCodePage.$$('#activationCode').value = ACTIVATION_CODE_VALID;
-
       // Forward button should now be enabled.
       assertActivationCodePage(
           /*forwardButtonShouldBeEnabled*/ true,
