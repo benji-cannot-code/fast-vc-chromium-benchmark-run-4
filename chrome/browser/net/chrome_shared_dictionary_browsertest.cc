8747cc9e4e9fe1ccff3ac196be0f9778facc4902
Tsuyoshi Horo <horo@chromium.org>
2023-07-05 07:16:06 +0000

    Use a C style string for a static string
    The presubmit check is complaining about the static std::string in
    chrome/browser/net/chrome_shared_dictionary_browsertest.cc.
    
    Bug: 1413922
    Change-Id: I98d076d100b9058d584a81d3fde438336ba432e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664091
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165799}

--- chrome/browser/net/chrome_shared_dictionary_browsertest.cc
+++ chrome/browser/net/chrome_shared_dictionary_browsertest.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const std::string kTestDictionaryString = "A dictionary";
+constexpr char kTestDictionaryString[] = "A dictionary";
 
 class SharedDictionaryAccessObserver : public content::WebContentsObserver {
  public:
