4ff83f4aea0b45fa24b570e0a623fb082f73ebcc
Chris Fredrickson <cfredric@chromium.org>
2023-07-05 23:18:29 +0000

    Make Storage Access API permission decision lifetimes configurable
    Change-Id: Ie37a5bd2204ddb7416aaad5fc5234292c9ccf0b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666743
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166212}

--- chrome/browser/storage_access_api/storage_access_grant_permission_context.cc
+++ chrome/browser/storage_access_api/storage_access_grant_permission_context.cc
@@ -49,9 +49,6 @@ using content_settings::URLToSchemefulSitePattern;
 
 namespace {
 
-constexpr base::TimeDelta kImplicitGrantDuration = base::Hours(24);
-constexpr base::TimeDelta kExplicitGrantDuration = base::Days(30);
-
 // Returns true if the request wasn't answered by the user explicitly.
 bool IsImplicitOutcome(RequestOutcome outcome) {
   switch (outcome) {
@@ -121,12 +118,14 @@ content_settings::ContentSettingConstraints ComputeConstraints(
   content_settings::ContentSettingConstraints constraints;
   switch (outcome) {
     case RequestOutcome::kGrantedByFirstPartySet:
-      constraints.set_lifetime(kImplicitGrantDuration);
+      constraints.set_lifetime(
+          blink::features::kStorageAccessAPIImplicitPermissionLifetime.Get());
       constraints.set_session_model(
           content_settings::SessionModel::NonRestorableUserSession);
       return constraints;
     case RequestOutcome::kGrantedByAllowance:
-      constraints.set_lifetime(kImplicitGrantDuration);
+      constraints.set_lifetime(
+          blink::features::kStorageAccessAPIImplicitPermissionLifetime.Get());
       constraints.set_session_model(
           content_settings::SessionModel::UserSession);
       return constraints;
@@ -141,7 +140,8 @@ content_settings::ContentSettingConstraints ComputeConstraints(
       NOTREACHED_NORETURN();
     case RequestOutcome::kGrantedByUser:
     case RequestOutcome::kDeniedByUser:
-      constraints.set_lifetime(kExplicitGrantDuration);
+      constraints.set_lifetime(
+          blink::features::kStorageAccessAPIExplicitPermissionLifetime.Get());
       constraints.set_session_model(content_settings::SessionModel::Durable);
       return constraints;
   }
