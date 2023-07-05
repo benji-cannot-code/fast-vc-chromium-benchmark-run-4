5c4c690fe4a481589f7bc8c40935e47a811f74b5
Joey Arhar <jarhar@chromium.org>
2023-07-05 18:42:18 +0000

    Make togglePopover throw more exceptions
    This patch makes sure that togglePopover will throw exceptions when it
    is disconnected from the document or doesn't have a popover attribute.
    
    This is being discussed here: https://github.com/whatwg/html/issues/8999
    
    Change-Id: Iac7a486cd64b09b5657a157dbf3e9e54c1be2a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634324
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166059}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1318,7 +1318,6 @@ bool HTMLElement::IsPopoverReady(PopoverTriggerAction action,
   CHECK(RuntimeEnabledFeatures::HTMLPopoverAttributeEnabled(
       GetDocument().GetExecutionContext()));
   CHECK_NE(action, PopoverTriggerAction::kNone);
-  CHECK_NE(action, PopoverTriggerAction::kToggle);
 
   auto maybe_throw_exception = [&exception_state, &include_event_handler_text](
                                    DOMExceptionCode code, const char* msg) {
@@ -1431,6 +1430,12 @@ bool HTMLElement::togglePopover(bool force, ExceptionState& exception_state) {
     hidePopover(exception_state);
   } else if (force && !popoverOpen()) {
     ShowPopoverInternal(/*invoker*/ nullptr, &exception_state);
+  } else {
+    // Throw an exception if this element is disconnected or doesn't have a
+    // popover attribute.
+    IsPopoverReady(PopoverTriggerAction::kToggle, &exception_state,
+                   /*include_event_handler_text=*/false,
+                   /*document=*/nullptr);
   }
 
   if (GetPopoverData()) {
