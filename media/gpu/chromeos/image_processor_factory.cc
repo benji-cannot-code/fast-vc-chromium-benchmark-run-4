bf867e80428b94ff302811827250bbfaa0abf6bb
Chen-Yu Tsai <wenst@chromium.org>
2023-07-06 10:12:06 +0000

    media/gpu/chromeos: Include build_config.h in image_processor_factory.cc
    To fix presubmit warning:
    
    media/gpu/chromeos/image_processor_factory.cc:200 ARCH_CPU_ARM_FAMILY
    	macro is used without first including build/build_config.h.
    
    Bug: none
    Change-Id: I02b4ec4eb3857338647080d0ff33425b8ef7297b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664330
    Commit-Queue: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166418}

--- media/gpu/chromeos/image_processor_factory.cc
+++ media/gpu/chromeos/image_processor_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
+#include "build/build_config.h"
 #include "media/base/media_switches.h"
 #include "media/base/video_types.h"
 #include "media/gpu/buildflags.h"
