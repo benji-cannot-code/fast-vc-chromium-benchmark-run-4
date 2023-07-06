e7257679e08f13814c2d135f678aace65a9b4475
Rohit Agarwal <roagarwal@chromium.org>
2023-07-06 10:12:33 +0000

    Quick Delete: Add a bare chip view for "More options" in the dialog
    This CL adds a dummy "More options" chip in the quick delete
    dialog and a corresponding style file.
    
    The chip has the visibility forced to GONE, and would be made visible
    in later CLs when the design is polished to correspond
    https://screenshot.googleplex.com/D9SGmJSSAvp7bww
    
    Bug: 1412087
    Change-Id: If732496df34a913f2588e354589352929850b2d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661342
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166419}

--- chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteDialogDelegate.java
+++ chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteDialogDelegate.java
@@ -26,6 +26,7 @@ import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.sync.SyncServiceFactory;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.components.browser_ui.widget.chips.ChipView;
 import org.chromium.components.browser_ui.widget.text.TemplatePreservingTextView;
 import org.chromium.components.browser_ui.widget.text.TextViewWithCompoundDrawables;
 import org.chromium.components.embedder_support.util.UrlConstants;
@@ -209,6 +210,11 @@ class QuickDeleteDialogDelegate {
                 quickDeleteDialogView.findViewById(R.id.search_history_disambiguation);
         addSearchHistoryDisambiguationTextIfRequired(searchHistoryDisambiguation);
 
+        // Update the "More options" chip.
+        ChipView moreOptionsView =
+                quickDeleteDialogView.findViewById(R.id.quick_delete_more_options);
+        updateMoreOptions(moreOptionsView);
+
         return new PropertyModel.Builder(ModalDialogProperties.ALL_KEYS)
                 .with(ModalDialogProperties.CONTROLLER, mModalDialogController)
                 .with(ModalDialogProperties.TITLE,
@@ -362,6 +368,14 @@ class QuickDeleteDialogDelegate {
             text.setMovementMethod(LinkMovementMethod.getInstance());
             text.setVisibility(View.VISIBLE);
         }
+    }
+
+    // TODO(crbug.com/1412087): Add logic here to update the primary text to "More options" and
+    //  open the CBD Advanced tab on click.
+    private void updateMoreOptions(@NonNull ChipView moreOptionsView) {
+        // TODO(crbug.com/1412087): Update the UX of the button to reflect the proposal before
+        //  updating the visibility to visible.
+        moreOptionsView.setVisibility(View.GONE);
     }
 
     /**
