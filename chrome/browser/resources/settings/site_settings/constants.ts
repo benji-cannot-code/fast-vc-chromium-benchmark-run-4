9a3bb360a7d82e2af5dd707e47e765382e5417ff
Ayu Ishii <ayui@chromium.org>
2023-07-06 18:03:17 +0000

    [FSA] Implement C++ Observer to Trigger UI Updates on File System Settings Page
    Adds a C++ observer with the objective of keeping the data
    displayed on the chrome://settings/content/filesystem page
    updated across all active and inactive tabs / windows.
    
    Formatting and further required adjustments will be completed
    in followup changes.
    
    This change is hidden behind a feature flag that will
    not be enabled until Persistent Permissions launches.
    The UI implementation in this CL is NOT in its final state.
    
    Bug: 1354695
    Change-Id: Id837e159f558f5d79d81d02ab87a1cbe093e83fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482696
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166662}

--- chrome/browser/resources/settings/site_settings/constants.ts
+++ chrome/browser/resources/settings/site_settings/constants.ts
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * chrome/browser/resources/settings/site_settings_page/site_settings_page_util
  * for translations.
  */
+
 export enum ContentSettingsTypes {
   ADS = 'ads',
   ANTI_ABUSE = 'anti-abuse',
