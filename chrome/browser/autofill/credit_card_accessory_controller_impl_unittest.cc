6de8ac64af4c88f79b318d16c5d48ae84d87ecf1
Elias Khsheibun <eliaskh@chromium.org>
2023-07-06 13:11:23 +0000

    Delete unused using statement.
    Bug: None
    Change-Id: I1f2f6dde15bfd90d106c08343e21254f31c50a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4668787
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Elias Khsheibun <eliaskh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166484}

--- chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
+++ chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using testing::_;
 using testing::SaveArg;
-using testing::SaveArgPointee;
 using IsFillingSourceAvailable = AccessoryController::IsFillingSourceAvailable;
 
 constexpr char kExampleSite[] = "https://example.com";
