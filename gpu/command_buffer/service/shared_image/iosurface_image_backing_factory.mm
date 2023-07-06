8791c5886ceadb4c81f2ae122d8c450990d40b2a
Saifuddin Hitawala <hitawala@chromium.org>
2023-07-06 15:32:33 +0000

    Multiplanar: Add external sampler invariants for apple and windows
    Update client interfaces and IOSurface, D3DImage backings to check
    for external sampler support on apple, windows and reject if clients
    request it.
    
    Bug: 1462120
    Change-Id: I93530602f5205a3abd32d92f48fc6278d3bf8909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4663172
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166556}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing_factory.mm
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing_factory.mm
@@ -188,6 +188,8 @@ IOSurfaceImageBackingFactory::CreateSharedImage(
     uint32_t usage,
     std::string debug_label,
     gfx::GpuMemoryBufferHandle handle) {
+  // MacOS does not support external sampler.
+  CHECK(!format.PrefersExternalSampler());
   return CreateSharedImageGMBs(
       mailbox, format, size, color_space, surface_origin, alpha_type, usage,
       std::move(handle), /*io_surface_plane=*/0, gfx::BufferPlane::DEFAULT,
@@ -214,6 +216,11 @@ IOSurfaceImageBackingFactory::CreateSharedImage(
 
   const uint32_t io_surface_plane = GetPlaneIndex(plane, buffer_format);
   auto format = viz::GetSharedImageFormat(buffer_format);
+  // Format cannot be using external sampling due to checks in
+  // `IsPlaneValidForGpuMemoryBufferFormat`.
+  if (format.IsLegacyMultiplanar()) {
+    CHECK_NE(plane, gfx::BufferPlane::DEFAULT);
+  }
   return CreateSharedImageGMBs(
       mailbox, format, size, color_space, surface_origin, alpha_type, usage,
       std::move(handle), io_surface_plane, plane, /*is_plane_format=*/true);
