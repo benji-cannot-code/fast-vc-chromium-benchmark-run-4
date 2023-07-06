7967883ec22ce799b9235ba0d2e967728e1d7ae2
Mikel Astiz <mastiz@chromium.org>
2023-07-06 10:54:26 +0000

    [sync] Extend SyncUserSettings::SetSelectedType() to all cases
    Before this patch, the API only supported the transport-mode case, and
    upper layers (namely SyncSetupService) needed to mimic similar behavior
    for full-sync users.
    
    Instead, this patch unifies the logic in the same layer,
    SyncUserSettingsImpl.
    
    Change-Id: I5ddb78dc1daf2444fb5fa0445df8350c654cbe72
    Bug: 1459963, 1446965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660673
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166440}

--- components/sync/service/sync_user_settings_impl.cc
+++ components/sync/service/sync_user_settings_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/check.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -145,11 +146,21 @@ void SyncUserSettingsImpl::SetSelectedType(UserSelectableType type,
                                            bool is_type_on) {
   UserSelectableTypeSet registered_types = GetRegisteredSelectableTypes();
   CHECK(registered_types.Has(type));
-  // To insure this setter is used in transport-mode only.
-  CHECK(ShouldUsePerAccountPrefs());
-  signin::GaiaIdHash gaia_id_hash =
-      signin::GaiaIdHash::FromGaiaId(sync_account_info_callback_.Run().gaia);
-  prefs_->SetSelectedTypeForAccount(type, is_type_on, gaia_id_hash);
+
+  if (ShouldUsePerAccountPrefs()) {
+    signin::GaiaIdHash gaia_id_hash =
+        signin::GaiaIdHash::FromGaiaId(sync_account_info_callback_.Run().gaia);
+    prefs_->SetSelectedTypeForAccount(type, is_type_on, gaia_id_hash);
+  } else {
+    DUMP_WILL_BE_CHECK(!IsSyncEverythingEnabled());
+    syncer::UserSelectableTypeSet selected_types = GetSelectedTypes();
+    if (is_type_on) {
+      selected_types.Put(type);
+    } else {
+      selected_types.Remove(type);
+    }
+    SetSelectedTypes(IsSyncEverythingEnabled(), selected_types);
+  }
 }
 
 #if BUILDFLAG(IS_IOS)
