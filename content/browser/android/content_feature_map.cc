3d87fda7335ee0d4577a418f4a68a0a3d8aa72c3
Mark Schillaci <mschillaci@google.com>
2023-07-06 17:08:08 +0000

    [Page Zoom] Add feature flag for new experimental smart zoom feature
    This CL starts the work for the Smart Zoom experimental feature by
    adding a feature flag. This will be an Android-only feature and
    will be tested using a Finch experiment/rollout.
    
    AX-Relnotes: N/A
    Bug: 1459631, b/265491863
    Change-Id: I3e8c78239ccdf50963e65108e10f4cc1b8f73c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660301
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166608}

--- content/browser/android/content_feature_map.cc
+++ content/browser/android/content_feature_map.cc
@@ -33,6 +33,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kProcessSharingWithStrictSiteInstances,
     &features::kReduceGpuPriorityOnBackground,
     &features::kRequestDesktopSiteAdditions,
+    &features::kSmartZoom,
     &features::kTouchDragAndContextMenu,
     &features::kWebAuthnTouchToFillCredentialSelection,
     &features::kWebBluetoothNewPermissionsBackend,
