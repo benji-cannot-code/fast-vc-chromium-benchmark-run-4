55c0bd7ea6350c7640ce13d81ed26383e719f540
Danila Kuzmin <dkuzmin@google.com>
2023-07-06 10:31:58 +0000

    [OOBE][DebugOverlay]: Fix debug overlay
    Add required params to show enterprise-enrollment and gaia-signin
    screens. Include assistant-optin-flow only when it is enabled.
    
    Bug: b:289788972, b:288254332
    Change-Id: Ic25be9e88fdbd6b08800636b2fcf0228f420fa33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4665820
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166428}

--- chrome/browser/ui/webui/ash/login/oobe_ui.cc
+++ chrome/browser/ui/webui/ash/login/oobe_ui.cc
@@ -132,6 +132,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/oobe_conditional_resources.h"
 #include "chrome/grit/oobe_unconditional_resources.h"
 #include "chrome/grit/oobe_unconditional_resources_map.h"
+#include "chromeos/ash/components/assistant/buildflags.h"
 #include "chromeos/ash/services/auth_factor_config/in_process_instances.h"
 #include "chromeos/ash/services/cellular_setup/public/mojom/esim_manager.mojom.h"
 #include "chromeos/ash/services/multidevice_setup/multidevice_setup_service.h"
@@ -305,6 +306,8 @@ void CreateAndAddOobeUIDataSource(Profile* profile,
       "isChromeVoxHintImprovementsEnabled",
       ::features::
           IsExperimentalAccessibilityChromeVoxOobeDialogImprovementsEnabled());
+  source->AddBoolean("isOobeAssistantEnabled",
+                     !features::IsOobeSkipAssistantEnabled());
   source->AddBoolean("isOobeGaiaInfoScreenEnabled",
                      features::IsOobeGaiaInfoScreenEnabled());
   source->AddBoolean("isChoobeEnabled", features::IsOobeChoobeEnabled());
