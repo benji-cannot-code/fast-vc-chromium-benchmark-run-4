16935e827bc6f1484a55ce54f8a67f0906832c5f
Hiroki Sato <hirokisato@google.com>
2023-07-06 00:48:33 +0000

    Move ArcAccessibilityUtilTest to services/a11y/android directory
    This file was not moved in the previous work in CL:4545786.
    This moved the file and rename it to AndroidAccessibilityUtilTest.
    
    AX-Relnotes: n/a.
    
    Bug: none
    Test: unit_tests
    Change-Id: Ia03d975afccdbc3ff676526ea847f3421a97ba90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664285
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166249}

--- services/accessibility/android/ax_tree_source_android.h
+++ services/accessibility/android/ax_tree_source_android.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_tree_data.h"
 #include "ui/accessibility/ax_tree_serializer.h"
 #include "ui/accessibility/ax_tree_source.h"
-#include "ui/views/view.h"
 
 namespace aura {
 class Window;
