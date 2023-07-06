f5a485f3e621650bdd7c32fbe75ceeb95ff5b3e1
Gordon Seto <gordonseto@google.com>
2023-07-06 16:21:44 +0000

    [CrOS Cellular] Add a11y for APN list item
    Add a11y label to <apn-list-item> handling database APNs, custom APNs,
    connected state and disabled state.
    
    Bug: b:162365553
    Test: *ApnListItem* and DUT
    Change-Id: I018f0afe0c6a26e83289d6bc7cf3bf793f079d72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4663935
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166579}

--- ash/webui/common/resources/network/apn_list.js
+++ ash/webui/common/resources/network/apn_list.js
@@ -63,6 +63,13 @@ export class ApnList extends ApnListBase {
       },
 
       /** @private */
+      apns_: {
+        type: Object,
+        value: [],
+        computed: 'computeApns_(managedCellularProperties)',
+      },
+
+      /** @private */
       shouldShowApnDetailDialog_: {
         type: Boolean,
         value: false,
@@ -146,7 +153,7 @@ export class ApnList extends ApnListBase {
    * @return {Array<!ApnProperties>}
    * @private
    */
-  getApns_() {
+  computeApns_() {
     if (!this.managedCellularProperties) {
       return [];
     }
@@ -155,7 +162,6 @@ export class ApnList extends ApnListBase {
     const customApnList = this.managedCellularProperties.customApnList;
 
     if (!connectedApn) {
-      // TODO(b/162365553): Show error when there is no connected APN.
       return customApnList || [];
     }
 
