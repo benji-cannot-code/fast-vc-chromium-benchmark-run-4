71a46ff57b4efe16f1e81fabc9d399611ba2a7a7
Thomas Quintanilla <tquintanilla@google.com>
2023-07-06 14:00:02 +0000

    Deactivate sources after top level filter matching
    This is to resolve a leak in privacy that may occur with falsely
    attributed reports, see
    https://github.com/WICG/attribution-reporting-api/issues/842
    for context. The change is kept behind a feature flag to verify no
    breakages or loss occurs.
    
    Bug: 1458510
    Change-Id: Ibd22c282343e89086a2ebd71b821847e1f10c539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647270
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166502}

--- content/browser/attribution_reporting/attribution_storage_sql.cc
+++ content/browser/attribution_reporting/attribution_storage_sql.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/numeric/int128.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "url/origin.h"
 
@@ -93,6 +94,10 @@ const base::FilePath::CharType kDatabasePath[] =
     FILE_PATH_LITERAL("Conversions");
 
 constexpr int64_t kUnsetRecordId = -1;
+
+const base::FeatureParam<bool> kSourceDeactivationAfterFiltering{
+    &blink::features::kConversionMeasurement,
+    "source_deactivation_after_filtering", false};
 
 void RecordInitializationStatus(
     const AttributionStorageSql::InitStatus status) {
@@ -1203,6 +1208,23 @@ CreateReportResult AttributionStorageSql::MaybeCreateAndStoreReport(
         AggregatableResult::kNoMatchingSourceFilterData);
   }
 
+  const bool deactivate_after_filtering =
+      kSourceDeactivationAfterFiltering.Get();
+
+  if (deactivate_after_filtering) {
+    // Delete all unattributed sources.
+    if (!DeleteSources(source_ids_to_delete)) {
+      return assemble_report_result(EventLevelResult::kInternalError,
+                                    AggregatableResult::kInternalError);
+    }
+
+    // Deactivate all attributed sources not used.
+    if (!DeactivateSources(source_ids_to_deactivate)) {
+      return assemble_report_result(EventLevelResult::kInternalError,
+                                    AggregatableResult::kInternalError);
+    }
+  }
+
   absl::optional<uint64_t> dedup_key;
   if (!event_level_status.has_value()) {
     if (EventLevelResult create_event_level_status =
@@ -1331,16 +1353,18 @@ CreateReportResult AttributionStorageSql::MaybeCreateAndStoreReport(
                                   store_aggregatable_status);
   }
 
-  // Delete all unattributed sources.
-  if (!DeleteSources(source_ids_to_delete)) {
-    return assemble_report_result(EventLevelResult::kInternalError,
-                                  AggregatableResult::kInternalError);
-  }
+  if (!deactivate_after_filtering) {
+    // Delete all unattributed sources.
+    if (!DeleteSources(source_ids_to_delete)) {
+      return assemble_report_result(EventLevelResult::kInternalError,
+                                    AggregatableResult::kInternalError);
+    }
 
-  // Deactivate all attributed sources.
-  if (!DeactivateSources(source_ids_to_deactivate)) {
-    return assemble_report_result(EventLevelResult::kInternalError,
-                                  AggregatableResult::kInternalError);
+    // Deactivate all attributed sources not used.
+    if (!DeactivateSources(source_ids_to_deactivate)) {
+      return assemble_report_result(EventLevelResult::kInternalError,
+                                    AggregatableResult::kInternalError);
+    }
   }
 
   // Based on the deletion logic here and the fact that we delete sources
