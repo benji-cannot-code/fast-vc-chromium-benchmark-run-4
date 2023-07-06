13fa927fbd362e43c444dd606be48811b8d4b496
Danila Kuzmin <dkuzmin@google.com>
2023-07-06 08:51:44 +0000

    [OOBE][Jelly]: Update oobe-modal-dialog
    Update:
     - paddings;
     - rounded corners;
     - colors.
    
    Jelly on:  https://screenshot.googleplex.com/7eoCkPWzDEHrFeZ
    Jelly off: https://screenshot.googleplex.com/BHmScHExHTPFuFh
    
    Bug: b:269114472
    Change-Id: I0cc96c7f5c8848c6eedc22b57447a0ac39504f22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661090
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Auto-Submit: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166387}

--- chrome/browser/resources/chromeos/login/components/dialogs/oobe_modal_dialog.js
+++ chrome/browser/resources/chromeos/login/components/dialogs/oobe_modal_dialog.js
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *  trigger some action on their side by using 'on-close=myMethod'.
  */
 
-import '//resources/polymer/v3_0/paper-styles/color.js';
+import '//resources/cr_elements/chromeos/cros_color_overrides.css.js';
 import '//resources/cr_elements/cr_dialog/cr_dialog.js';
 import '//resources/cr_elements/cr_shared_style.css.js';
 import '../buttons/oobe_text_button.js';
