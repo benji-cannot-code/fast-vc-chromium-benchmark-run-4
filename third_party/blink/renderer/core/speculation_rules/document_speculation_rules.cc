221363b3a10d5fbdc201b2a79379ba7353b4f976
Taiyo Mizuhashi <taiyo@chromium.org>
2023-07-06 17:57:28 +0000

    Add metrics for BFcache retrigger
    This CL adds the metric that records the number of speculation rules
    preloading updates, classified by the reason for the update, to measure
    the overall impact of BFCache retrigger.
    
    Bug: 1449163
    Change-Id: I2498c7c378a7bbb2624065d408775b95fc321b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667003
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166652}

--- third_party/blink/renderer/core/speculation_rules/document_speculation_rules.cc
+++ third_party/blink/renderer/core/speculation_rules/document_speculation_rules.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/speculation_rules/document_speculation_rules.h"
 
 #include "base/containers/contains.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/ranges/algorithm.h"
 #include "base/state_transitions.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
@@ -138,6 +139,19 @@ absl::optional<Referrer> GetReferrer(SpeculationRule* rule,
 
   return referrer;
 }
+
+// The reason for calling |UpdateSpeculationCandidates| for metrics.
+// Currently, this is designed to measure the impact of
+// |kRetriggerPreloadingOnBFCacheRestoration|(crbug.com/1449163) so that
+// other update reasons (such as ruleset insertion/removal etc...) will be
+// tentatively classified as |kOther|.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class UpdateSpeculationCandidatesReason {
+  kOther = 0,
+  kRestoredFromBFCache = 1,
+  kMaxValue = kRestoredFromBFCache,
+};
 
 }  // namespace
 
@@ -432,6 +446,9 @@ void DocumentSpeculationRules::DisplayLockedElementDisconnected(Element* root) {
 }
 
 void DocumentSpeculationRules::DocumentRestoredFromBFCache() {
+  CHECK(base::FeatureList::IsEnabled(
+      blink::features::kRetriggerPreloadingOnBFCacheRestoration));
+  first_update_after_restored_from_bfcache_ = true;
   QueueUpdateSpeculationCandidates();
 }
 
@@ -613,6 +630,17 @@ void DocumentSpeculationRules::UpdateSpeculationCandidates() {
   if (eagerness_set.Has(SpeculationEagerness::kEager)) {
     UseCounter::Count(GetSupplementable(),
                       WebFeature::kSpeculationRulesEagernessEager);
+  }
+
+  if (base::FeatureList::IsEnabled(
+          blink::features::kRetriggerPreloadingOnBFCacheRestoration)) {
+    base::UmaHistogramEnumeration(
+        "Preloading.Experimental.UpdateSpeculationCandidatesReason",
+        first_update_after_restored_from_bfcache_
+            ? UpdateSpeculationCandidatesReason::kRestoredFromBFCache
+            : UpdateSpeculationCandidatesReason::kOther);
+
+    first_update_after_restored_from_bfcache_ = false;
   }
 }
 
