aaa5e5e6acef0c688534e0609b0c84d0518dccb3
Roman Arora <romanarora@chromium.org>
2023-07-06 05:57:28 +0000

    [ntp-journeys] NTP history_clusters refactor
    Refactors certain history clusters functions and logic so that the V1
    and upcoming V2 Journeys modules can use different cluster query filter
    parameters.
    
    Sets the `CreateFilterParamsFromFeatureFlags` function to filter `done`
    and `hidden` clusters true by default. Should result in no changes to
    the currently returned cluster data by the service. This is because
    at the current time, the history clusters module service filters too
    any `hidden` or `done` clusters from the query results via the
    `OnGetFilteredClusters` function.
    
    Bug: 1443637
    Change-Id: Ia39ecfa495a39a538812d28c7e92ab23e9e5d18b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664012
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1166323}

--- chrome/browser/new_tab_page/modules/history_clusters/history_clusters_module_service_unittest.cc
+++ chrome/browser/new_tab_page/modules/history_clusters/history_clusters_module_service_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cart/cart_service_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/history_clusters/history_clusters_service_factory.h"
+#include "chrome/browser/new_tab_page/modules/history_clusters/history_clusters_module_util.h"
 #include "chrome/browser/new_tab_page/modules/history_clusters/ranking/history_clusters_module_ranking_signals.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/test/base/testing_profile.h"
@@ -48,6 +49,8 @@ class MockCartService : public CartService {
 
 constexpr char kSampleNonSearchUrl[] = "https://www.foo.com/";
 constexpr char kSampleSearchUrl[] = "https://default-engine.com/search?q=foo";
+constexpr int kMinRequiredVisits = 3;
+constexpr int kMinRequiredRelatedSearches = 3;
 
 const TemplateURLService::Initializer kTemplateURLData[] = {
     {"default-engine.com", "http://default-engine.com/search?q={searchTerms}",
@@ -100,15 +103,21 @@ class HistoryClustersModuleServiceTest : public testing::Test {
     std::vector<history::Cluster> clusters;
 
     base::RunLoop run_loop;
-    service().GetClusters(base::BindOnce(
-        [](base::RunLoop* run_loop, std::vector<history::Cluster>* out_clusters,
-           std::vector<history::Cluster> clusters,
-           base::flat_map<int64_t, HistoryClustersModuleRankingSignals>
-               ranking_signals) {
-          *out_clusters = std::move(clusters);
-          run_loop->Quit();
-        },
-        &run_loop, &clusters));
+    const history_clusters::QueryClustersFilterParams filter_params =
+        CreateFilterParamsFromFeatureFlags(kMinRequiredVisits,
+                                           kMinRequiredRelatedSearches);
+    service().GetClusters(
+        filter_params, static_cast<size_t>(kMinRequiredRelatedSearches),
+        base::BindOnce(
+            [](base::RunLoop* run_loop,
+               std::vector<history::Cluster>* out_clusters,
+               std::vector<history::Cluster> clusters,
+               base::flat_map<int64_t, HistoryClustersModuleRankingSignals>
+                   ranking_signals) {
+              *out_clusters = std::move(clusters);
+              run_loop->Quit();
+            },
+            &run_loop, &clusters));
 
     run_loop.Run();
 
--- chrome/browser/new_tab_page/modules/history_clusters/history_clusters_page_handler_unittest.cc
+++ chrome/browser/new_tab_page/modules/history_clusters/history_clusters_page_handler_unittest.cc
@@ -76,11 +76,13 @@ class MockHistoryClustersModuleService : public HistoryClustersModuleService {
   MockHistoryClustersModuleService()
       : HistoryClustersModuleService(nullptr, nullptr, nullptr, nullptr) {}
 
-  MOCK_METHOD1(
+  MOCK_METHOD3(
       GetClusters,
-      void(base::OnceCallback<
-           void(std::vector<history::Cluster>,
-                base::flat_map<int64_t, HistoryClustersModuleRankingSignals>)>
+      void(const history_clusters::QueryClustersFilterParams filter_params,
+           size_t min_required_related_searches,
+           base::OnceCallback<void(
+               std::vector<history::Cluster>,
+               base::flat_map<int64_t, HistoryClustersModuleRankingSignals>)>
                callback));
 };
 
@@ -253,9 +255,12 @@ TEST_F(HistoryClustersPageHandlerTest, GetClusters) {
         SampleCluster(i, /*srp_visits=*/1, /*non_srp_visits=*/2));
     ranking_signals[i] = HistoryClustersModuleRankingSignals();
   }
-  EXPECT_CALL(mock_history_clusters_module_service(), GetClusters(testing::_))
+  EXPECT_CALL(mock_history_clusters_module_service(),
+              GetClusters(testing::_, testing::_, testing::_))
       .WillOnce(testing::Invoke(
           [&sample_clusters, &ranking_signals](
+              const history_clusters::QueryClustersFilterParams filter_params,
+              size_t min_required_related_searches,
               base::OnceCallback<void(
                   std::vector<history::Cluster>,
                   base::flat_map<int64_t, HistoryClustersModuleRankingSignals>)>
@@ -340,7 +345,8 @@ TEST_F(HistoryClustersPageHandlerTest,
 
   const history::Cluster kSampleCluster =
       SampleCluster(/*srp_visits=*/1, /*non_srp_visits=*/2);
-  EXPECT_CALL(mock_history_clusters_module_service(), GetClusters(testing::_))
+  EXPECT_CALL(mock_history_clusters_module_service(),
+              GetClusters(testing::_, testing::_, testing::_))
       .Times(0);
 
   std::vector<history_clusters::mojom::ClusterPtr> clusters_mojom;
@@ -357,9 +363,12 @@ TEST_F(HistoryClustersPageHandlerTest,
 }
 
 TEST_F(HistoryClustersPageHandlerTest, NoClusters) {
-  EXPECT_CALL(mock_history_clusters_module_service(), GetClusters(testing::_))
+  EXPECT_CALL(mock_history_clusters_module_service(),
+              GetClusters(testing::_, testing::_, testing::_))
       .WillOnce(testing::Invoke(
-          [&](base::OnceCallback<void(
+          [&](const history_clusters::QueryClustersFilterParams filter_params,
+              size_t min_required_related_searches,
+              base::OnceCallback<void(
                   std::vector<history::Cluster>,
                   base::flat_map<int64_t, HistoryClustersModuleRankingSignals>)>
                   callback) { std::move(callback).Run({}, {}); }));
@@ -435,9 +444,12 @@ TEST_F(HistoryClustersPageHandlerTest, RecordClick) {
   std::vector<history::Cluster> sample_clusters = {SampleCluster(0, 1, 2)};
   base::flat_map<int64_t, HistoryClustersModuleRankingSignals> ranking_signals =
       {{cluster_id, HistoryClustersModuleRankingSignals()}};
-  EXPECT_CALL(mock_history_clusters_module_service(), GetClusters(testing::_))
+  EXPECT_CALL(mock_history_clusters_module_service(),
+              GetClusters(testing::_, testing::_, testing::_))
       .WillOnce(testing::Invoke(
           [&sample_clusters, &ranking_signals](
+              const history_clusters::QueryClustersFilterParams filter_params,
+              size_t min_required_related_searches,
               base::OnceCallback<void(
                   std::vector<history::Cluster>,
                   base::flat_map<int64_t, HistoryClustersModuleRankingSignals>)>
@@ -473,9 +485,12 @@ TEST_F(HistoryClustersPageHandlerTest, RecordLayoutTypeShown) {
   std::vector<history::Cluster> sample_clusters = {SampleCluster(0, 1, 2)};
   base::flat_map<int64_t, HistoryClustersModuleRankingSignals> ranking_signals =
       {{cluster_id, HistoryClustersModuleRankingSignals()}};
-  EXPECT_CALL(mock_history_clusters_module_service(), GetClusters(testing::_))
+  EXPECT_CALL(mock_history_clusters_module_service(),
+              GetClusters(testing::_, testing::_, testing::_))
       .WillOnce(testing::Invoke(
           [&sample_clusters, &ranking_signals](
+              const history_clusters::QueryClustersFilterParams filter_params,
+              size_t min_required_related_searches,
               base::OnceCallback<void(
                   std::vector<history::Cluster>,
                   base::flat_map<int64_t, HistoryClustersModuleRankingSignals>)>
