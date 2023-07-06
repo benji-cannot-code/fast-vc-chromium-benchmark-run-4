2369a9fac65f39f55d5a776697bfba84631d8e76
Marc Treib <treib@chromium.org>
2023-07-06 09:46:08 +0000

    Enable SyncEnableHistoryDataType by default
    Bug: 1318028
    Change-Id: Iee7539bb25c41279f333558934a3438e51487a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055561
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1166412}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -76,7 +76,7 @@ BASE_FEATURE(kSkipInvalidationOptimizationsWhenDeviceInfoUpdated,
 
 BASE_FEATURE(kSyncEnableHistoryDataType,
              "SyncEnableHistoryDataType",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncEnableContactInfoDataType,
              "SyncEnableContactInfoDataType",
