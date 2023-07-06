fa7a8fdf005af68b2bcc981cd6f7c69c8d97335e
Basia Zimirska <basiaz@google.com>
2023-07-06 23:08:51 +0000

    Adding ReadAloudController (very simplistic for now).
    ReadAloudController will be the "entry point" component for ReadAloud.
    It will check availability of the feature and trigger the playback.
    This CL:
    - Adds a feature and chrome flag (non descriptive on purpose)
    - Plugs ReadAloudController into RootUiCoordinator for use within
    Chrome*Activities
    - Adds MSBB check and a simple unit test in the Controller
    
    Design doc: go/clank-readaloud-dd (Googlers only)
    
    Bug: b/284356963
    Change-Id: I7056d51e277b7e7f6bc3225c26d4c9591bdd2dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664237
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1166810}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -102,6 +102,7 @@ import org.chromium.chrome.browser.password_manager.PasswordManagerLauncher;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.price_tracking.PriceTrackingButtonController;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.readaloud.ReadAloudController;
 import org.chromium.chrome.browser.recent_tabs.RestoreTabsFeatureHelper;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
 import org.chromium.chrome.browser.segmentation_platform.ContextualPageActionController;
@@ -312,6 +313,8 @@ public class RootUiCoordinator
     private final BackPressManager mBackPressManager;
     private final boolean mIsIncognitoReauthPendingOnRestore;
     protected final ExpandedSheetHelper mExpandedBottomSheetHelper;
+    private final ObservableSupplierImpl<ReadAloudController> mReadAloudControllerSupplier =
+            new ObservableSupplierImpl<>();
     @Nullable
     private PageZoomCoordinator mPageZoomCoordinator;
     private AppMenuObserver mAppMenuObserver;
@@ -811,6 +814,11 @@ public class RootUiCoordinator
                 mActivityTabProvider.get().loadIfNeeded(
                         LoadIfNeededCaller.ON_FINISH_NATIVE_INITIALIZATION);
             }
+        }
+
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.READALOUD)) {
+            ReadAloudController controller = new ReadAloudController(mProfileSupplier);
+            mReadAloudControllerSupplier.set(controller);
         }
     }
 
@@ -1635,6 +1643,14 @@ public class RootUiCoordinator
 
     public OneshotSupplier<IncognitoReauthController> getIncognitoReauthControllerSupplier() {
         return mIncognitoReauthControllerOneshotSupplier;
+    }
+
+    /**
+     * Returns the supplier of {@link ReadAloudController}.
+     */
+    @NonNull
+    public Supplier<ReadAloudController> getReadAloudControllerSupplier() {
+        return mReadAloudControllerSupplier;
     }
 
     /**
--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -4072,6 +4072,9 @@ const char kQueryTilesSwapTrendingDescription[] =
     "Swap trending queries if user didn't click on them after several "
     "impressions";
 
+const char kReadAloudName[] = "Read Aloud";
+const char kReadAloudDescription[] = "Controls the Read Aloud feature";
+
 const char kReadLaterFlagId[] = "read-later";
 const char kReadLaterName[] = "Reading List";
 const char kReadLaterDescription[] =
