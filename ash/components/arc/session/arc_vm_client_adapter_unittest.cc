50197a26bdf9961e9d0a317b5eec1f8ecfd8b952
Momoko Hattori <momohatt@google.com>
2023-07-06 11:37:53 +0000

    arc: Destroy and re-initialize FakeUpstartClient in each test case
    In ArcUtilTest and ArcVmClientAdapterTest, the behavior of
    FakeUpstartClient is reset on every test case by passing null callbacks
    to FakeUpstartClient's set_start_job_cb / set_stop_job_cb.
    This CL changes the tests to destroy and re-initialize FakeUpstartClient
    on each test case instead, as it will be simpler and more robust against
    the internal changes of FakeUpstartClient.
    
    BUG=None
    TEST=ash_components_unittest --gtest_filter="ArcUtilTest.*"
    TEST=ash_components_unittest --gtest_filter="ArcVmClientAdapterTest.*"
    
    Change-Id: Ib5bc308af94d2edf8811c5a38a440b7af84f77ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4663900
    Commit-Queue: Momoko Hattori <momohatt@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166451}

--- ash/components/arc/session/arc_vm_client_adapter_unittest.cc
+++ ash/components/arc/session/arc_vm_client_adapter_unittest.cc
@@ -346,6 +346,7 @@ class ArcVmClientAdapterTest : public testing::Test,
   ArcVmClientAdapterTest& operator=(const ArcVmClientAdapterTest&) = delete;
 
   ~ArcVmClientAdapterTest() override {
+    ash::UpstartClient::Shutdown();
     ash::ConciergeClient::Shutdown();
     ash::DebugDaemonClient::SetInstanceForTest(nullptr);
     test_debug_daemon_client_.reset();
@@ -370,7 +371,6 @@ class ArcVmClientAdapterTest : public testing::Test,
     GetTestConciergeClient()->set_start_vm_response(start_vm_response);
 
     // Reset to the original behavior.
-    RemoveUpstartStartStopJobFailures();
     SetArcVmBootNotificationServerFdForTesting(absl::nullopt);
 
     const std::string abstract_addr(GenerateAbstractAddress());
@@ -584,14 +584,6 @@ class ArcVmClientAdapterTest : public testing::Test,
               {job_name, env, UpstartOperationType::STOP});
           return true;
         }));
-  }
-
-  void RemoveUpstartStartStopJobFailures() {
-    auto* upstart_client = ash::FakeUpstartClient::Get();
-    upstart_client->set_start_job_cb(
-        ash::FakeUpstartClient::StartStopJobCallback());
-    upstart_client->set_stop_job_cb(
-        ash::FakeUpstartClient::StartStopJobCallback());
   }
 
   // We expect ConciergeClient::StopVm to have been called two times,
