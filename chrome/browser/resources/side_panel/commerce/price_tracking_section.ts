7b598a46aa078009b7ba10b22bb05aeca609a06c
Yue Zhang <yuezhanggg@chromium.org>
2023-07-06 00:08:38 +0000

    [PriceInsights] Add price tracking error message
    Demo: https://screenshot.googleplex.com/AghxEgfiqsqPbcx
    
    Bug: 1456420
    Change-Id: Ic5c91df5e35de4be8d56a864de2365c4bfdc2772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647296
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166235}

--- chrome/browser/resources/side_panel/commerce/price_tracking_section.ts
+++ chrome/browser/resources/side_panel/commerce/price_tracking_section.ts
@@ -67,6 +67,9 @@ export class PriceTrackingSection extends PolymerElement {
         callbackRouter.priceUntrackedForBookmark.addListener(
             (product: BookmarkProductInfo) =>
                 this.onBookmarkPriceUntracked(product)),
+        callbackRouter.operationFailedForBookmark.addListener(
+            (product: BookmarkProductInfo, attemptedTrack: boolean) =>
+                this.onBookmarkOperationFailed(product, attemptedTrack)),
     );
 
 
@@ -119,6 +122,16 @@ export class PriceTrackingSection extends PolymerElement {
 
   private onFolderClicked_() {
     this.shoppingApi_.showBookmarkEditorForCurrentUrl();
+  }
+
+  private onBookmarkOperationFailed(
+      product: BookmarkProductInfo, attemptedTrack: boolean) {
+    if (product.info.clusterId !== this.productInfo.clusterId) {
+      return;
+    }
+    this.toggleAnnotationText_ = loadTimeData.getString('trackPriceError');
+    this.folderName_ = '';
+    this.isProductTracked_ = !attemptedTrack;
   }
 }
 
