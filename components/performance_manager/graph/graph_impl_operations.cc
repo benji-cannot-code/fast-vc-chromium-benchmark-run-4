3b34dcb00c6509b76ecd204682fdf2d1589e42f8
Eshwar Stalin <estalin@chromium.org>
2023-07-06 21:26:32 +0000

    Hovercard memory usage should only include the main frame PMF estimate
    Currently hovercard memory usage could be misleading since it includes
    memory usage from BF caches and pre-rendered frames. This could also
    lead to high memory value to be triggered more often and possibly on
    the wrong site. This change ensures the estimate only includes the main
    frame of the page node.
    
    Bug: 1462233
    Change-Id: I8f4af0d8e932608c54534dd39d7338b1f96e3a0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4668332
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166771}

--- components/performance_manager/graph/graph_impl_operations.cc
+++ components/performance_manager/graph/graph_impl_operations.cc
@@ -9,40 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace performance_manager {
 
-namespace {
-
-// Implementation details for VisitFrameTree*.
-
-bool VisitFrameAndChildrenPreOrder(
-    FrameNodeImpl* frame,
-    GraphImplOperations::FrameNodeImplVisitor visitor) {
-  if (!visitor(frame)) {
-    return false;
-  }
-  for (auto* child : frame->child_frame_nodes()) {
-    if (!VisitFrameAndChildrenPreOrder(child, visitor)) {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool VisitFrameAndChildrenPostOrder(
-    FrameNodeImpl* frame,
-    GraphImplOperations::FrameNodeImplVisitor visitor) {
-  for (auto* child : frame->child_frame_nodes()) {
-    if (!VisitFrameAndChildrenPostOrder(child, visitor)) {
-      return false;
-    }
-  }
-  if (!visitor(frame)) {
-    return false;
-  }
-  return true;
-}
-
-}  // namespace
-
 // static
 base::flat_set<PageNodeImpl*> GraphImplOperations::GetAssociatedPageNodes(
     const ProcessNodeImpl* process) {
@@ -81,6 +47,36 @@ std::vector<FrameNodeImpl*> GraphImplOperations::GetFrameNodes(
   }
 
   return frame_nodes;
+}
+
+// static
+bool GraphImplOperations::VisitFrameAndChildrenPreOrder(
+    FrameNodeImpl* frame,
+    GraphImplOperations::FrameNodeImplVisitor visitor) {
+  if (!visitor(frame)) {
+    return false;
+  }
+  for (auto* child : frame->child_frame_nodes()) {
+    if (!VisitFrameAndChildrenPreOrder(child, visitor)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// static
+bool GraphImplOperations::VisitFrameAndChildrenPostOrder(
+    FrameNodeImpl* frame,
+    GraphImplOperations::FrameNodeImplVisitor visitor) {
+  for (auto* child : frame->child_frame_nodes()) {
+    if (!VisitFrameAndChildrenPostOrder(child, visitor)) {
+      return false;
+    }
+  }
+  if (!visitor(frame)) {
+    return false;
+  }
+  return true;
 }
 
 // static
