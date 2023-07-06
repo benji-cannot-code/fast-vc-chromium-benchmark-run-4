89228a8b87b3fd50f224b14165355d9f8bb93a98
Gina Domergue <gdomergue@google.com>
2023-07-06 20:18:37 +0000

    Add Screenshot Support to Toolbar
    Adds support when clicking on the "Take screenshot" button within the
    toolbar.
    
    Bug: b:273641151
    Test: Created unittest
    Change-Id: Iddc88161bf3322cc2f1a83b86c72749e882e1f98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619293
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Gina Domergue <gdomergue@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166738}

--- ash/game_dashboard/game_dashboard_toolbar_view.cc
+++ ash/game_dashboard/game_dashboard_toolbar_view.cc
@@ -7,14 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/capture_mode/capture_mode_controller.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/icon_button.h"
+#include "base/types/cxx23_to_underlying.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/views/background.h"
-#include "ui/views/layout/box_layout.h"
 
 namespace ash {
 
@@ -29,15 +30,20 @@ constexpr int kBetweenChildSpacing = 8;
 
 std::unique_ptr<IconButton> CreateIconButton(base::RepeatingClosure callback,
                                              const gfx::VectorIcon* icon,
+                                             int view_id,
                                              const std::u16string& text) {
-  return std::make_unique<IconButton>(
+  auto button = std::make_unique<IconButton>(
       std::move(callback), IconButton::Type::kSmallFloating, icon, text,
       /*is_togglable=*/false, /*has_border=*/true);
+  button->SetID(view_id);
+  return button;
 }
 
 }  // namespace
 
-GameDashboardToolbarView::GameDashboardToolbarView() {
+GameDashboardToolbarView::GameDashboardToolbarView(aura::Window* game_window)
+    : game_window_(game_window) {
+  DCHECK(game_window_);
   SetOrientation(views::BoxLayout::Orientation::kVertical);
   SetBackground(views::CreateThemedSolidBackground(kColorAshShieldAndBase80));
   SetOrientation(views::BoxLayout::Orientation::kVertical);
@@ -59,7 +65,7 @@ void GameDashboardToolbarView::OnRecordButtonPressed() {
 }
 
 void GameDashboardToolbarView::OnScreenshotButtonPressed() {
-  // TODO(b/273641151): Add screenshot support.
+  CaptureModeController::Get()->CaptureScreenshotOfGivenWindow(game_window_);
 }
 
 void GameDashboardToolbarView::AddShortcutTiles() {
@@ -67,6 +73,7 @@ void GameDashboardToolbarView::AddShortcutTiles() {
       base::BindRepeating(&GameDashboardToolbarView::OnGamepadButtonPressed,
                           base::Unretained(this)),
       &vector_icons::kVideogameAssetOutlineIcon,
+      base::to_underlying(ToolbarViewId::kGamepadButton),
       l10n_util::GetStringUTF16(
           IDS_ASH_GAME_DASHBOARD_TOOLBAR_TILE_BUTTON_TITLE)));
 
@@ -77,6 +84,7 @@ void GameDashboardToolbarView::AddShortcutTiles() {
       base::BindRepeating(&GameDashboardToolbarView::OnRecordButtonPressed,
                           base::Unretained(this)),
       &vector_icons::kVideocamIcon,
+      base::to_underlying(ToolbarViewId::kScreenRecordButton),
       l10n_util::GetStringUTF16(
           IDS_ASH_GAME_DASHBOARD_RECORD_GAME_TILE_BUTTON_TITLE)));
 
@@ -84,6 +92,7 @@ void GameDashboardToolbarView::AddShortcutTiles() {
       base::BindRepeating(&GameDashboardToolbarView::OnScreenshotButtonPressed,
                           base::Unretained(this)),
       &vector_icons::kVideocamIcon,
+      base::to_underlying(ToolbarViewId::kScreenshotButton),
       l10n_util::GetStringUTF16(
           IDS_ASH_GAME_DASHBOARD_SCREENSHOT_TILE_BUTTON_TITLE)));
 }
