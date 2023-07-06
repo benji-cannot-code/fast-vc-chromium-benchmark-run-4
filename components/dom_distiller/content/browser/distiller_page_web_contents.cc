ca374a793a023a2ffa1b811bc9b1f7e35e8a3ac6
Alan Screen <awscreen@chromium.org>
2023-07-06 19:20:26 +0000

    [CodeHealth] Drop DomDistiller.Time.RunJavaScript
    Remove histogram which has long since expired.
    
    OBSOLETE_HISTOGRAM[DomDistiller.Time.RunJavaScript]=No longer useful
    
    Fixed: 1448189
    Change-Id: I7deaf6ee9b7dab5983a06e023d49fd0703feaffb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661923
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166712}

--- components/dom_distiller/content/browser/distiller_page_web_contents.cc
+++ components/dom_distiller/content/browser/distiller_page_web_contents.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/dom_distiller/content/browser/distiller_javascript_utils.h"
 #include "components/dom_distiller/core/distiller_page.h"
@@ -185,7 +184,6 @@ void DistillerPageWebContents::OnWebContentsDistillationDone(
 
   if (!javascript_start.is_null()) {
     base::TimeDelta javascript_time = base::TimeTicks::Now() - javascript_start;
-    UMA_HISTOGRAM_TIMES("DomDistiller.Time.RunJavaScript", javascript_time);
     DVLOG(1) << "DomDistiller.Time.RunJavaScript = " << javascript_time;
   }
 
