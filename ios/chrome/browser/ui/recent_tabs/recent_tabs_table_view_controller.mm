1c1d6d88d2f5fd24a2d612391b38f37e2eb84c68
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-07-06 11:12:19 +0000

    Fix some metric recordings in recent tabs
    These were using _browserState->IsOffTheRecord() but _browserState
    is already GetOriginalBrowserState(). _incognito is the thing to check.
    https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm;l=250;drc=8e4dbd6368dae62b250c964bb56ddd6f25b2c67c
    
    That said, IIUC recent tabs is never opened in incognito, so I doubt
    this makes a difference.
    
    Bug: 1462326
    Change-Id: Ib4d8da104aab081f1a754d7f04bf86287de56246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4665194
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166443}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
@@ -1524,8 +1524,7 @@ typedef std::pair<SessionID, TableViewURLItem*> RecentlyClosedTableViewItemPair;
       self.searchTerms = @"";
     }
     new_tab_page_uma::RecordAction(
-        self.browserState->IsOffTheRecord(),
-        self.webStateList->GetActiveWebState(),
+        self.isIncognito, self.webStateList->GetActiveWebState(),
         new_tab_page_uma::ACTION_OPENED_FOREIGN_SESSION);
     std::unique_ptr<web::WebState> web_state =
         session_util::CreateWebStateWithNavigationEntries(
@@ -1570,8 +1569,7 @@ typedef std::pair<SessionID, TableViewURLItem*> RecentlyClosedTableViewItemPair;
         "MobileRecentTabManagerRecentTabOpenedSearchResult"));
   }
   new_tab_page_uma::RecordAction(
-      self.browserState->IsOffTheRecord(),
-      self.webStateList->GetActiveWebState(),
+      self.isIncognito, self.webStateList->GetActiveWebState(),
       new_tab_page_uma::ACTION_OPENED_RECENTLY_CLOSED_ENTRY);
 
   // If RecentTabs is being displayed from incognito, the resulting tab will
