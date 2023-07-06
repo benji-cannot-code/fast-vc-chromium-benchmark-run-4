5ee3b69bba87f7e835522e62df0beba73d83eeb8
Ashley Prasad <ashleydp@google.com>
2023-07-06 20:06:57 +0000

    print-preview: add SVG to setup info
    - Add empty state SVG to icons.
    - Set icon colors to match default colors.
    - Set styling on container to center elements.
    
    Screenshot: http://screen/ADNbZiRf3DKKnTa
    
    Bug: b:283119650
    Test: browser_tests --gtest_filter=*PrintPreview*
    Change-Id: I2a9b36bf1921f6a8ef0b7689bfc96549d32074e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667578
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166729}

--- chrome/browser/resources/print_preview/ui/printer_setup_info_cros.ts
+++ chrome/browser/resources/print_preview/ui/printer_setup_info_cros.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import './print_preview_shared.css.js';
 
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
