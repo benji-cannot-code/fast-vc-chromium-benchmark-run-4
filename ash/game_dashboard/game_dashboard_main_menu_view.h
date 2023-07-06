753f41c78c686faf88cf16b20d1e53620fdd8ab2
Gina Domergue <gdomergue@google.com>
2023-07-06 17:28:22 +0000

    Add Toolbar Support to Menu
    Adds support when clicking on the "Toolbar" tile within the main
    menu view.
    
    Bug: b:273641426
    Test: Created unittests
    Change-Id: Ia3bf744258346a1fafaefb1182a76104fc67b391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619371
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Gina Domergue <gdomergue@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166622}

--- ash/game_dashboard/game_dashboard_main_menu_view.h
+++ ash/game_dashboard/game_dashboard_main_menu_view.h
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_GAME_DASHBOARD_GAME_DASHBOARD_MAIN_MENU_VIEW_H_
 #define ASH_GAME_DASHBOARD_GAME_DASHBOARD_MAIN_MENU_VIEW_H_
 
+#include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
 namespace ash {
+
+class GameDashboardContext;
 
 // GameDashboardMainMenuView is the expanded menu view attached to the game
 // dashboard button.
@@ -17,8 +20,7 @@ class GameDashboardMainMenuView : public views::BubbleDialogDelegateView {
  public:
   METADATA_HEADER(GameDashboardMainMenuView);
 
-  GameDashboardMainMenuView(views::Widget* main_menu_button_widget,
-                            aura::Window* game_window);
+  explicit GameDashboardMainMenuView(GameDashboardContext* context);
   GameDashboardMainMenuView(const GameDashboardMainMenuView&) = delete;
   GameDashboardMainMenuView& operator=(const GameDashboardMainMenuView) =
       delete;
@@ -59,7 +61,8 @@ class GameDashboardMainMenuView : public views::BubbleDialogDelegateView {
   // buttons) to the Game Controls tile view.
   void AddUtilityClusterRow();
 
-  const raw_ptr<aura::Window, ExperimentalAsh> game_window_;
+  // Allows this class to access `GameDashboardContext` owned functions/objects.
+  const raw_ptr<GameDashboardContext, ExperimentalAsh> context_;
 };
 
 }  // namespace ash
