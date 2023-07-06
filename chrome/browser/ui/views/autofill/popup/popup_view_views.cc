10be29c50e6864d68311814e86ac849f09a53660
Christoph Schwering <schwering@google.com>
2023-07-06 16:36:59 +0000

    [Autofill] Launch and clean up AutofillPopupUseThresholdForKeyboardAndMobileAccept
    The experiment does not produce meaningful data, so we're launching
    via waterfall.
    
    Bug: 1418364
    Change-Id: I4c5067daa0450e404245e92c5c336cbf69967e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4613765
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166590}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.cc
@@ -286,12 +286,7 @@ bool PopupViewViews::AcceptSelectedCell(bool tab_key_pressed) {
     }
   }
 
-  if (base::FeatureList::IsEnabled(
-          features::kAutofillPopupUseThresholdForKeyboardAndMobileAccept)) {
-    controller_->AcceptSuggestion(index->first);
-  } else {
-    controller_->AcceptSuggestionWithoutThreshold(index->first);
-  }
+  controller_->AcceptSuggestion(index->first);
   return true;
 }
 
