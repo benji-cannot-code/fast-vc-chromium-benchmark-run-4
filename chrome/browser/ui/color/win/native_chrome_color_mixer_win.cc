dcc5edd1e00f0899ef9327e28b30312dc53ac8e2
Jackson Loeffler <j@jloeffler.com>
2023-07-07 23:20:52 +0000

    [win] Keep the new tab button background transparent when Mica is on
    This CL makes the new tab button transition smoother when its window
    becomes inactive. Keeping the button's background transparent for
    inactive frames allows the titlebar's fade animation to show through.
    
    Bug: 1448376
    Change-Id: I7bf924be53aa9dbe2812d8c7cb7fce7c64f11508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4655842
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167677}

--- chrome/browser/ui/color/win/native_chrome_color_mixer_win.cc
+++ chrome/browser/ui/color/win/native_chrome_color_mixer_win.cc
@@ -126,8 +126,11 @@ void FrameColorHelper::AddNativeChromeColors(
 
   if (ShouldDefaultThemeUseMicaTitlebar() && !key.app_controller) {
     mixer[kColorNewTabButtonBackgroundFrameActive] = {SK_ColorTRANSPARENT};
+    mixer[kColorNewTabButtonBackgroundFrameInactive] = {SK_ColorTRANSPARENT};
     mixer[kColorNewTabButtonInkDropFrameActive] =
         ui::GetColorWithMaxContrast(ui::kColorFrameActive);
+    mixer[kColorNewTabButtonInkDropFrameInactive] =
+        ui::GetColorWithMaxContrast(ui::kColorFrameInactive);
   }
 }
 
