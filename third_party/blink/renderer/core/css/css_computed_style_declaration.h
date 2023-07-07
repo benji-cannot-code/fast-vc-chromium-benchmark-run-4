2d8c235fc26a52fb9577058d7eeb7d1aafb10f2d
Rune Lillesveen <futhark@chromium.org>
2023-07-07 08:00:10 +0000

    Let CSSComputedStyleDeclaration constructor take Element
    There were a couple of instances in EditingStyle where we needed to look
    up a parent element.
    
    Bug: 1459564
    Change-Id: I35e24c2f23aeb5884356ea652b4b97020f3218e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670545
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167277}

--- third_party/blink/renderer/core/css/css_computed_style_declaration.h
+++ third_party/blink/renderer/core/css/css_computed_style_declaration.h
@@ -34,12 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ComputedStyle;
+class Element;
 class ExceptionState;
 class ExecutionContext;
 class LayoutObject;
 class MutableCSSPropertyValueSet;
-class Node;
-class ComputedStyle;
 
 class CORE_EXPORT CSSComputedStyleDeclaration final
     : public CSSStyleDeclaration {
@@ -47,9 +47,9 @@ class CORE_EXPORT CSSComputedStyleDeclaration final
   static const Vector<const CSSProperty*>& ComputableProperties(
       const ExecutionContext*);
 
-  CSSComputedStyleDeclaration(Node*,
-                              bool allow_visited_style = false,
-                              const String& = String());
+  explicit CSSComputedStyleDeclaration(Element*,
+                                       bool allow_visited_style = false,
+                                       const String& = String());
   ~CSSComputedStyleDeclaration() override;
 
   String GetPropertyValue(CSSPropertyID) const;
@@ -76,12 +76,9 @@ class CORE_EXPORT CSSComputedStyleDeclaration final
   void Trace(Visitor*) const override;
 
  private:
-  // The styled node is either the node passed into getComputedStyle, or the
-  // PseudoElement for :before and :after if they exist.
-  // FIXME: This should be styledElement since in JS getComputedStyle only works
-  // on Elements, but right now editing creates these for text nodes. We should
-  // fix that.
-  Node* StyledNode() const;
+  // The styled element is either the element passed into getComputedStyle, or
+  // the PseudoElement for the ::before, ::after, etc if they exist.
+  Element* StyledElement() const;
 
   // The styled layout object is the layout object corresponding to the node
   // being queried, if any.
@@ -132,7 +129,7 @@ class CORE_EXPORT CSSComputedStyleDeclaration final
   bool CssPropertyMatches(CSSPropertyID, const CSSValue&) const override;
 
   AtomicString pseudo_argument_;
-  Member<Node> node_;
+  Member<Element> element_;
   PseudoId pseudo_element_specifier_;
   bool allow_visited_style_;
 };
