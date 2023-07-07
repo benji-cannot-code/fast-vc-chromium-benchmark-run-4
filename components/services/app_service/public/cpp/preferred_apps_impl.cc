040d06976fffcfdfa0ec13b5af05afd2ed611377
Tim Sergeant <tsergeant@chromium.org>
2023-07-07 07:39:58 +0000

    App Service: Use SequenceChecker in PreferredAppsImpl
    This achieves the same result as DCHECK_CURRENTLY_ON, but without the
    dependency on //content.
    
    Bug: 1462788
    Change-Id: Iae0d4ebb28975cb2422d1d20ed1cbefbd9ffbae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672186
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167266}

--- components/services/app_service/public/cpp/preferred_apps_impl.cc
+++ components/services/app_service/public/cpp/preferred_apps_impl.cc
@@ -9,20 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/contains.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/json/json_string_value_serializer.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/ranges/algorithm.h"
+#include "base/sequence_checker.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "components/services/app_service/public/cpp/intent_filter_util.h"
 #include "components/services/app_service/public/cpp/preferred_apps_converter.h"
-#include "content/public/browser/browser_thread.h"
 
 namespace {
 
@@ -92,7 +90,9 @@ PreferredAppsImpl::PreferredAppsImpl(
   InitializePreferredApps();
 }
 
-PreferredAppsImpl::~PreferredAppsImpl() = default;
+PreferredAppsImpl::~PreferredAppsImpl() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
 
 void PreferredAppsImpl::RemovePreferredApp(const std::string& app_id) {
   RunAfterPreferredAppsReady(
@@ -122,7 +122,7 @@ void PreferredAppsImpl::InitializePreferredApps() {
 void PreferredAppsImpl::WriteToJSON(
     const base::FilePath& profile_dir,
     const apps::PreferredAppsList& preferred_apps) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // If currently is writing preferred apps to file, set a flag to write after
   // the current write completed.
   if (writing_preferred_apps_) {
@@ -148,7 +148,7 @@ void PreferredAppsImpl::WriteToJSON(
 }
 
 void PreferredAppsImpl::WriteCompleted() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   writing_preferred_apps_ = false;
   if (!should_write_preferred_apps_to_file_) {
     // Call the testing callback if it is set.
@@ -164,7 +164,7 @@ void PreferredAppsImpl::WriteCompleted() {
 }
 
 void PreferredAppsImpl::ReadFromJSON(const base::FilePath& profile_dir) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   task_runner_->PostTaskAndReplyWithResult(
       FROM_HERE,
       base::BindOnce(&ReadDataBlocking,
@@ -174,7 +174,7 @@ void PreferredAppsImpl::ReadFromJSON(const base::FilePath& profile_dir) {
 }
 
 void PreferredAppsImpl::ReadCompleted(std::string preferred_apps_string) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   bool preferred_apps_upgraded = false;
   if (preferred_apps_string.empty()) {
     preferred_apps_list_.Init();
