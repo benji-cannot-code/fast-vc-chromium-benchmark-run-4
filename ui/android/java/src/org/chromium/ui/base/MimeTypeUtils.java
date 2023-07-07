51f13be3e6e992f50cf8d8b2b255322ad5133186
Finnur Thorarinsson <finnur@chromium.org>
2023-07-07 13:59:14 +0000

    [MediaPicker]: Modernize use of permission const.
    The READ_MEDIA_foo strings weren't available in prior SDKs
    but now are.
    
    no functionality is changing.
    
    Low-Coverage-Reason: This is a purely mechanical change,
    Bug: 1462183
    Change-Id: I528d5972a9a06d662f0a83632c4dec4714a0e7b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4669926
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167414}

--- ui/android/java/src/org/chromium/ui/base/MimeTypeUtils.java
+++ ui/android/java/src/org/chromium/ui/base/MimeTypeUtils.java
@@ -4,14 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.ui.base;
 
-import android.Manifest.permission;
+import android.Manifest;
 import android.webkit.MimeTypeMap;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.BuildInfo;
-import org.chromium.ui.permissions.PermissionConstants;
 import org.chromium.url.GURL;
 
 import java.lang.annotation.Retention;
@@ -77,16 +76,16 @@ public class MimeTypeUtils {
      */
     public @Nullable static String getPermissionNameForMimeType(@MimeTypeUtils.Type int mimeType) {
         if (useExternalStoragePermission()) {
-            return permission.READ_EXTERNAL_STORAGE;
+            return Manifest.permission.READ_EXTERNAL_STORAGE;
         }
 
         switch (mimeType) {
             case MimeTypeUtils.Type.AUDIO:
-                return PermissionConstants.READ_MEDIA_AUDIO;
+                return Manifest.permission.READ_MEDIA_AUDIO;
             case MimeTypeUtils.Type.IMAGE:
-                return PermissionConstants.READ_MEDIA_IMAGES;
+                return Manifest.permission.READ_MEDIA_IMAGES;
             case MimeTypeUtils.Type.VIDEO:
-                return PermissionConstants.READ_MEDIA_VIDEO;
+                return Manifest.permission.READ_MEDIA_VIDEO;
             default:
                 return null;
         }
