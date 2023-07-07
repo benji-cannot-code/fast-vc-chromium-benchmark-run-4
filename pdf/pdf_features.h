18ca75dfc6c5c4619f985e99907639409dc5ad1c
Hui Yingst <nigi@chromium.org>
2023-07-07 22:33:29 +0000

    Reland "Create a feature flag for PDF portfolio"
    This is a reland of commit 747748b35537c1bd952ab266114334354785a125.
    This CL removes the extra line in
    tools/metrics/histograms/enums.xml.
    
    Original change's description:
    > Create a feature flag for PDF portfolio
    >
    > Creates a feature flag `pdf-portfolio` which will be an indicator that
    > portfolio feature is enabled in the PDF viewer. Currently this flag is
    > a dummy flag that has not been linked to the actual feature yet.
    >
    > Bug: 177188
    > Change-Id: Ief14cbdad785bad405bf402d3cb57b22314a07c1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651500
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Nigi <nigi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1164297}
    
    Bug: 177188
    Change-Id: I5c37624ad198ec0a6f7efdfbbeb4d2f4c928fb08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661237
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Nigi <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167665}

--- pdf/pdf_features.h
+++ pdf/pdf_features.h
@@ -17,6 +17,7 @@ namespace features {
 BASE_DECLARE_FEATURE(kAccessiblePDFForm);
 BASE_DECLARE_FEATURE(kPdfIncrementalLoading);
 BASE_DECLARE_FEATURE(kPdfPartialLoading);
+BASE_DECLARE_FEATURE(kPdfPortfolio);
 BASE_DECLARE_FEATURE(kPdfUseSkiaRenderer);
 BASE_DECLARE_FEATURE(kPdfXfaSupport);
 
