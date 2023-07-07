285b4c5118d26ed1556d767e525760dfd0b79738
Wenbo Jie <wenbojie@chromium.org>
2023-07-07 07:03:54 +0000

    [Files F2] Another fix attempt for the tree focus logic
    delegatesFocus can focus on the inner element even with
    tabindex="-1", so in order to make a element non-focusable, we
    need to remove the tabindex attribute. In addition, xf-tree-item
    now has a toggleFocusable public method to make it focusable or
    non-focusable, it will operate on the DOM directly to make sure
    it's synchronous, so calling "focus()" immediately after making
    it focusable will work.
    
    Bug: b:279092120
    Test: browser_tests --gtest_filter="*XfTree*"
    Change-Id: I4399676fe403dafb65debef1c0596eb3452699e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667041
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167243}

--- ui/file_manager/file_manager/widgets/xf_tree.ts
+++ ui/file_manager/file_manager/widgets/xf_tree.ts
@@ -382,11 +382,11 @@ export class XfTree extends XfBase {
       return;
     }
     if (previousFocusedItem) {
-      previousFocusedItem.tabIndex = -1;
+      previousFocusedItem.toggleFocusable(false);
     }
     this.focusedItem_ = itemToFocus;
     if (this.focusedItem_) {
-      this.focusedItem_.tabIndex = 0;
+      this.focusedItem_.toggleFocusable(true);
     }
   }
 }
