5e1c16d3111bea57ffcba26fb9ee654c223ad9f5
Ming-Ying Chung <mych@chromium.org>
2023-07-07 08:09:46 +0000

    Mark flaky CORS keepalive tests possibly caused by insufficient timeout
    Bug: 1455180
    
    Change-Id: I294f2f8bb43edf0ddcd9ef08f669dd57cf789d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672307
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167285}

--- third_party/blink/web_tests/external/wpt/fetch/api/cors/cors-keepalive.any.js
+++ third_party/blink/web_tests/external/wpt/fetch/api/cors/cors-keepalive.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window
+// META: timeout=long
 // META: title=Fetch API: keepalive handling
 // META: script=/resources/testharness.js
 // META: script=/resources/testharnessreport.js
