c9497502c0f9fe95f6a6f29c882c1552daade74b
David Maunder <davidjm@google.com>
2023-07-07 21:00:47 +0000

    Disable PriceAlertsMessageCardTest tests
    testRenderMessageCard_Landscape_AppNotificationsDisabled and
    testRenderMessageCard_Landscape_AppNotificationsEnabled are
    both failing multiple times on android-pie-x86-rel
    
    Bug: 1463032
    Change-Id: Ibedb0ab6c12dbc0fdfc3cbf3019d27f70c08563a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673625
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Owners-Override: David Maunder <davidjm@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167633}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/PriceAlertsMessageCardTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/PriceAlertsMessageCardTest.java
@@ -50,6 +50,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
@@ -356,6 +357,7 @@ public class PriceAlertsMessageCardTest {
     @Feature({"RenderTest"})
     @CommandLineFlags.Add({BASE_PARAMS})
     @MinAndroidSdkLevel(Build.VERSION_CODES.O)
+    @DisabledTest(message = "crbug.com/1463032")
     public void testRenderMessageCard_Landscape_AppNotificationsEnabled() throws IOException {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         mMockNotificationManager.setNotificationsEnabled(true);
@@ -373,6 +375,7 @@ public class PriceAlertsMessageCardTest {
     @MediumTest
     @Feature({"RenderTest"})
     @CommandLineFlags.Add({BASE_PARAMS})
+    @DisabledTest(message = "crbug.com/1463032")
     public void testRenderMessageCard_Landscape_AppNotificationsDisabled() throws IOException {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         mMockNotificationManager.setNotificationsEnabled(false);
