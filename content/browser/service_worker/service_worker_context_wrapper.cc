135647dad9be1f18b042508012beafa906b2ebf8
Minoru Chikamune <chikamune@chromium.org>
2023-07-07 05:27:27 +0000

    Stop warming up service worker when there is no context
    The context_core_ can be null due to system shutdown or restart
    failure. In either case, we should not keep warming up service workers.
    
    Bug: 1462698
    Bug: 1431792
    Change-Id: I9bad5df0b909c98e3391711768b18407f3d2cd41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671927
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167233}

--- content/browser/service_worker/service_worker_context_wrapper.cc
+++ content/browser/service_worker/service_worker_context_wrapper.cc
@@ -1394,6 +1394,10 @@ void ServiceWorkerContextWrapper::FindRegistrationForScopeImpl(
 void ServiceWorkerContextWrapper::MaybeProcessPendingWarmUpRequest() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
+  if (!context_core_) {
+    return;
+  }
+
   context_core_->EndProcessingWarmingUp();
 
   absl::optional<ServiceWorkerContextCore::WarmUpRequest> request =
