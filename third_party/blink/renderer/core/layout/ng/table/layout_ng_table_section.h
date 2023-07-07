4b64937da5fb7050aa4ca40aba38303f2b7f446e
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-07-07 01:11:44 +0000

    [RemoveLegacy] Remove LayoutNGTableSection::SetNeedsCellRecalc
    This was just a duplicate SetNeedsLayout.
    
    There should be no (functional) behaviour change.
    
    Bug: 1229581
    Change-Id: Ia8aa5a7d33b916a1898b9236d6ac0ac2fa0e5652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671505
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166867}

--- third_party/blink/renderer/core/layout/ng/table/layout_ng_table_section.h
+++ third_party/blink/renderer/core/layout/ng/table/layout_ng_table_section.h
@@ -75,8 +75,6 @@ class CORE_EXPORT LayoutNGTableSection : public LayoutNGBlock {
 
   // LayoutBlock methods end.
 
-  void SetNeedsCellRecalc();
-
   unsigned NumRows() const;
 
   unsigned NumCols(unsigned) const;
