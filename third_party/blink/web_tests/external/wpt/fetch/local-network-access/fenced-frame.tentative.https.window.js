2280707523933c7f45dd4512604a5b768150b4fe
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-07-07 14:30:29 +0000

    Fenced frames: Break large local network access test file into smaller ones and await without timeout for positive cases.
    This CL breaks test fenced-frame.tentative.https.window.js
    into two:
    1. New fenced-frame-no-preflight-required.tentative.https.window.js
    contains all test cases that do not require local network access
    preflights.
    2. fenced-frame.tentative.https.window.js contains all other test
    cases that require preflight.
    
    Also changed to await the value from server without timeout if the
    expected test result is success.
    
    The goal is to avoid timeout.
    
    Why tests are slow?
    For fenced frames, we obtain messages by writing value to/reading
    value from a server. This is significantly slower than similar tests
    using iframes, because iframes can simply use `window.postMessage()`.
    
    Does `// META: timeout=long` help?
    In the reland CL (crrev.com/c/4595521) which introduces the tests, I
    have already specified long timeout.
    
    It solved the flakiness for the other test
    fenced-frame-subresource-fetch.tentative.https.window.js, but not this
    test.
    
    History of test results, before it was marked in TestExpectations, it
    was very flaky. Failures are all due to timeouts.
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2F%3Ablink_wpt_tests%2Fvirtual%2Ffenced-frame-mparch%2Fexternal%2Fwpt%2Ffetch%2Flocal-network-access%2Ffenced-frame.tentative.https.window.html?q=V%3Abuilder%3DLinux%2520Tests%2520%28dbg%29%281%29+V%3Aos%3DUbuntu-22.04+V%3Atest_suite%3Dblink_wpt_tests
    
    Bug: 1454910, 1451954
    Change-Id: I445116ea143b50b26b5edd83296a67f21979a287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654659
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167425}

--- third_party/blink/web_tests/external/wpt/fetch/local-network-access/fenced-frame.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/fetch/local-network-access/fenced-frame.tentative.https.window.js
@@ -15,34 +15,6 @@ setup(() => {
   assert_true(window.isSecureContext);
 });
 
-// Source: secure local context.
-//
-// All fetches unaffected by Private Network Access.
-
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {server: Server.HTTPS_LOCAL},
-      target: {server: Server.HTTPS_LOCAL},
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'local to local: no preflight required.');
-
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {server: Server.HTTPS_LOCAL},
-      target: {server: Server.HTTPS_PRIVATE},
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'local to private: no preflight required.');
-
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {server: Server.HTTPS_LOCAL},
-      target: {server: Server.HTTPS_PUBLIC},
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'local to public: no preflight required.');
-
 // Generates tests of preflight behavior for a single (source, target) pair.
 //
 // Scenarios:
@@ -128,22 +100,6 @@ makePreflightTests({
   targetName: 'local',
 });
 
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {server: Server.HTTPS_PRIVATE},
-      target: {server: Server.HTTPS_PRIVATE},
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'private to private: no preflight required.');
-
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {server: Server.HTTPS_PRIVATE},
-      target: {server: Server.HTTPS_PUBLIC},
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'private to public: no preflight required.');
-
 // Source: public secure context.
 //
 // Fetches to the local and private address spaces require a successful
@@ -162,14 +118,6 @@ makePreflightTests({
   targetServer: Server.HTTPS_PRIVATE,
   targetName: 'private',
 });
-
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {server: Server.HTTPS_PUBLIC},
-      target: {server: Server.HTTPS_PUBLIC},
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'public to public: no preflight required.');
 
 // The following tests verify that `CSP: treat-as-public-address` makes
 // documents behave as if they had been served from a public IP address.
@@ -201,28 +149,3 @@ makePreflightTests({
   targetServer: Server.HTTPS_PRIVATE,
   targetName: 'private',
 });
-
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {
-        server: Server.HTTPS_LOCAL,
-        treatAsPublic: true,
-      },
-      target: {server: Server.HTTPS_PUBLIC},
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'treat-as-public-address to public: no preflight required.');
-
-promise_test_parallel(
-    t => fencedFrameTest(t, {
-      source: {
-        server: Server.HTTPS_LOCAL,
-        treatAsPublic: true,
-      },
-      target: {
-        server: Server.HTTPS_PUBLIC,
-        behavior: {preflight: PreflightBehavior.optionalSuccess(token())}
-      },
-      expected: FrameTestResult.SUCCESS,
-    }),
-    'treat-as-public-address to local: optional preflight');
