a6adfbfcf40e30f2da6151c5cd5623829728dc9c
Ben Reich <benreich@chromium.org>
2023-07-07 07:07:11 +0000

    [FilesBulkPinning] Disable bulk pinning when drive is disabled
    Fixed: b:290300238
    Change-Id: I133e69ff18567c2de7cfc4339f3287a079737701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671289
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167248}

--- chrome/browser/resources/ash/settings/os_files_page/google_drive_subpage.ts
+++ chrome/browser/resources/ash/settings/os_files_page/google_drive_subpage.ts
@@ -312,6 +312,7 @@ export class SettingsGoogleDriveSubpageElement extends
     switch (closedDialogType) {
       case ConfirmationDialogType.DISCONNECT:
         this.setPrefValue(GOOGLE_DRIVE_DISABLED_PREF, true);
+        this.setPrefValue(GOOGLE_DRIVE_BULK_PINNING_PREF, false);
         break;
       case ConfirmationDialogType.BULK_PINNING_DISABLE:
         this.setPrefValue(GOOGLE_DRIVE_BULK_PINNING_PREF, false);
