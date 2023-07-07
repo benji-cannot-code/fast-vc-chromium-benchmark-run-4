26d9dfc7b290ca7c4a98a6e327b56918225ddf9e
Andrew Pantera <andp@chromium.org>
2023-07-07 04:21:21 +0000

    Fix WindowCycleControllerTest tests to work with Jelly
    With Jelly enabled some UI elements got larger or more spaced out,
    so the tests needed a bigger window to function the same.
    
    Bug: b/289464296
    Change-Id: Ic4a1e735a27469c271ef5352ca20027b9f3ed5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671122
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Andrew Pantera <andp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167116}

--- ash/wm/window_cycle/window_cycle_controller_unittest.cc
+++ ash/wm/window_cycle/window_cycle_controller_unittest.cc
@@ -1121,6 +1121,7 @@ TEST_F(WindowCycleControllerTest, MouseEventWhenCycleViewDoesNotExist) {
 // If a user clicks on an item, it should complete cycling and activate
 // the hovered item.
 TEST_F(WindowCycleControllerTest, MouseHoverAndSelect) {
+  UpdateDisplay("1200x800");
   std::unique_ptr<Window> w0 = CreateTestWindow();
   std::unique_ptr<Window> w1 = CreateTestWindow();
   std::unique_ptr<Window> w2 = CreateTestWindow();
@@ -1293,6 +1294,7 @@ TEST_F(WindowCycleControllerTest, RapidConfirmSelection) {
 // preventing the mouse from unexpectedly triggering events.
 // See crbug.com/1143275.
 TEST_F(WindowCycleControllerTest, FilterMouseEventsUntilUsed) {
+  UpdateDisplay("1200x800");
   std::unique_ptr<Window> w0 = CreateTestWindow();
   std::unique_ptr<Window> w1 = CreateTestWindow();
   std::unique_ptr<Window> w2 = CreateTestWindow();
@@ -1547,6 +1549,7 @@ TEST_F(WindowCycleControllerTest, VerticalScroll) {
 
 // Tests that touch continuous scrolls for the window cycle list.
 TEST_F(WindowCycleControllerTest, TouchScroll) {
+  UpdateDisplay("900x600");
   const gfx::Rect bounds(0, 0, 200, 200);
   std::unique_ptr<aura::Window> window5 = CreateTestWindow(bounds);
   std::unique_ptr<aura::Window> window4 = CreateTestWindow(bounds);
