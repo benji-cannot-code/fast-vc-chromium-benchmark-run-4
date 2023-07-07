73b26e575eabde91ed8f50f5708f62f0495826b6
Dave Tapuska <dtapuska@chromium.org>
2023-07-07 19:41:27 +0000

    Remove Implicit AtomicString define
    Now with all targets converted remove the define from AtomicString
    which allowed for implicit AtomicStrings.
    
    Bug: 1444094
    Change-Id: Ia08de5385da5d0eadee116c64a796485f0d7fbe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671005
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167591}

--- third_party/blink/renderer/platform/wtf/text/atomic_string.h
+++ third_party/blink/renderer/platform/wtf/text/atomic_string.h
@@ -41,13 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/bridging.h"
 #endif
 
-// TODO(crbug.com/1444094): AtomicString constructors should be explicit.
-#if !defined(ALLOW_IMPLICIT_ATOMIC_STRING_CONVERSIONS)
-#define MAYBE_EXPLICIT explicit
-#else
-#define MAYBE_EXPLICIT
-#endif
-
 namespace WTF {
 
 // An AtomicString instance represents a string, and multiple AtomicString
@@ -62,7 +55,7 @@ class WTF_EXPORT AtomicString {
   static void Init();
 
   AtomicString() = default;
-  MAYBE_EXPLICIT AtomicString(const LChar* chars)
+  explicit AtomicString(const LChar* chars)
       : AtomicString(chars,
                      chars ? strlen(reinterpret_cast<const char*>(chars)) : 0) {
   }
@@ -73,15 +66,14 @@ class WTF_EXPORT AtomicString {
   AtomicString(const LChar* chars, size_t length);
 #endif  // defined(ARCH_CPU_64_BITS)
 
-  MAYBE_EXPLICIT AtomicString(const char* chars)
+  explicit AtomicString(const char* chars)
       : AtomicString(reinterpret_cast<const LChar*>(chars)) {}
   AtomicString(const LChar* chars, unsigned length);
   AtomicString(
       const UChar* chars,
       unsigned length,
       AtomicStringUCharEncoding encoding = AtomicStringUCharEncoding::kUnknown);
-  MAYBE_EXPLICIT AtomicString(const UChar* chars);
-#undef MAYBE_EXPLICIT
+  explicit AtomicString(const UChar* chars);
 
   // Constructing an AtomicString from a String / StringImpl can be expensive if
   // the StringImpl is not already atomic.
