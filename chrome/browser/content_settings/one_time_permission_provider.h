ad74599e00d709d2ee102b1623666bcadd080864
Chris Fredrickson <cfredric@chromium.org>
2023-07-07 14:42:13 +0000

    Avoid renewing Storage Access API permission denials
    Bug: 1450356
    Change-Id: Icd34d0201f69ceacf285267458f8e2605fd51121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4662997
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167430}

--- chrome/browser/content_settings/one_time_permission_provider.h
+++ chrome/browser/content_settings/one_time_permission_provider.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/permissions/one_time_permissions_tracker_observer.h"
 #include "components/content_settings/core/browser/content_settings_origin_identifier_value_map.h"
 #include "components/content_settings/core/browser/user_modifiable_provider.h"
+#include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/permissions/permission_uma_util.h"
@@ -59,9 +60,11 @@ class OneTimePermissionProvider
   bool UpdateLastVisitTime(const ContentSettingsPattern& primary_pattern,
                            const ContentSettingsPattern& secondary_pattern,
                            ContentSettingsType content_type) override;
-  bool RenewContentSetting(const GURL& primary_url,
-                           const GURL& secondary_url,
-                           ContentSettingsType type) override;
+  bool RenewContentSetting(
+      const GURL& primary_url,
+      const GURL& secondary_url,
+      ContentSettingsType type,
+      absl::optional<ContentSetting> setting_to_match) override;
   void SetClockForTesting(base::Clock* clock) override;
 
   // OneTimePermissionsTrackerObserver:
