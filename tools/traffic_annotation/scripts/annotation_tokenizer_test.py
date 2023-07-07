6486a8f6e7b1fcec8524859a0a301ea3bf53d258
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2023-07-07 18:14:05 +0000

    Revert "annotation_tokenizer.py: Add support for Java text blocks"
    This reverts commit aa59adc10a9febca560da69d3f89f6f76dc4a690.
    
    Reason for revert: CQ issues, blocking crrev.com/c/4661549
    
    See e.g. https://ci.chromium.org/ui/p/chromium/builders/try/android-arm64-rel/242104/overview
    
    Original change's description:
    > annotation_tokenizer.py: Add support for Java text blocks
    >
    > And uses Python string decoders rather than regex for processing string
    > escapes.
    >
    > Bug: 1462201
    > Change-Id: I4b36ddcf6279252879e84c7069aaf449716faaea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661435
    > Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1166759}
    
    Bug: 1462201
    Change-Id: Ibb62dfa1e80e39ef5eb41ebfe752ad3c5ec7b3b0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673045
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1167538}

--- tools/traffic_annotation/scripts/annotation_tokenizer_test.py
+++ tools/traffic_annotation/scripts/annotation_tokenizer_test.py
@@ -68,10 +68,6 @@ class AnnotationTokenizerTest(unittest.TestCase):
     self.assertEqual(
         'the quick\nbrown\nfox', tokenizer.advance('string_literal'))
 
-  def testAdvanceTextBlock(self):
-    tokenizer = Tokenizer('\n """\n  the quick\n  red\n  fox"""', 'foo.txt', 2)
-    self.assertEqual('the quick\nred\nfox', tokenizer.advance('string_literal'))
-
   def testAdvanceErrorPaths(self):
     tokenizer = Tokenizer('  hello , ', 'foo.txt', 33)
     tokenizer.advance('symbol')
@@ -104,10 +100,10 @@ class AnnotationTokenizerTest(unittest.TestCase):
   def testEscaping(self):
     tokenizer = Tokenizer(
         '''
-      "\\"ab\\nc \\\\\\" def \\\\\\""
+      "\\"abc \\\\\\" def \\\\\\""
       "string ends here:\\\\" this is not part of the string"
     ''', 'foo.txt', 33)
-    self.assertEqual('"ab\nc \\" def \\"', tokenizer.advance('string_literal'))
+    self.assertEqual('"abc \\" def \\"', tokenizer.advance('string_literal'))
     self.assertEqual('string ends here:\\', tokenizer.advance('string_literal'))
 
 
