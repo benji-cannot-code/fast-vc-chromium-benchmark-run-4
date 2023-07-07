964f9d6f7fb7ba8365eb33ab36e1293b362bea58
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-07-07 18:48:33 +0000

    [Extensions] Use ExtensionFunction::request_uuid() in logging
    Instead of ExtensionFunction::request_id(), use
    ExtensionFunction::request_uuid() in logging, and ensure a
    request_uuid is always set for all extension functions (not just
    service worker-based calls). This is a step towards removing
    ExtensionFunction::request_id().
    
    Bug: 1444561
    Change-Id: Ib7c472d29bd627a7402ab1c51a5dae54005e1489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670314
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167561}

--- chrome/browser/ash/extensions/file_manager/private_api_mount.cc
+++ chrome/browser/ash/extensions/file_manager/private_api_mount.cc
@@ -63,8 +63,8 @@ ExtensionFunction::ResponseAction FileManagerPrivateAddMountFunction::Run() {
 
   Profile* const profile = Profile::FromBrowserContext(browser_context());
   if (drive::EventLogger* logger = file_manager::util::GetLogger(profile)) {
-    logger->Log(logging::LOG_INFO, "%s[%d] called. (source: '%s')", name(),
-                request_id(),
+    logger->Log(logging::LOG_INFO, "%s[%s] called. (source: '%s')", name(),
+                request_uuid().AsLowercaseString().c_str(),
                 params->file_url.empty() ? "(none)" : params->file_url.c_str());
   }
   set_log_on_completion(true);
@@ -148,8 +148,8 @@ FileManagerPrivateCancelMountingFunction::Run() {
   Profile* const profile = Profile::FromBrowserContext(browser_context());
 
   if (drive::EventLogger* logger = file_manager::util::GetLogger(profile)) {
-    logger->Log(logging::LOG_INFO, "%s[%d] called. (source: '%s')", name(),
-                request_id(),
+    logger->Log(logging::LOG_INFO, "%s[%s] called. (source: '%s')", name(),
+                request_uuid().AsLowercaseString().c_str(),
                 params->file_url.empty() ? "(none)" : params->file_url.c_str());
   }
   set_log_on_completion(true);
@@ -186,8 +186,9 @@ ExtensionFunction::ResponseAction FileManagerPrivateRemoveMountFunction::Run() {
 
   Profile* const profile = Profile::FromBrowserContext(browser_context());
   if (drive::EventLogger* logger = file_manager::util::GetLogger(profile)) {
-    logger->Log(logging::LOG_INFO, "%s[%d] called. (volume_id: '%s')", name(),
-                request_id(), params->volume_id.c_str());
+    logger->Log(logging::LOG_INFO, "%s[%s] called. (volume_id: '%s')", name(),
+                request_uuid().AsLowercaseString().c_str(),
+                params->volume_id.c_str());
   }
   set_log_on_completion(true);
 
@@ -304,8 +305,9 @@ FileManagerPrivateGetVolumeMetadataListFunction::Run() {
 
   if (drive::EventLogger* logger = file_manager::util::GetLogger(profile)) {
     logger->Log(logging::LOG_INFO,
-                "%s[%d] succeeded. (results: '[%s]', %" PRIuS " mount points)",
-                name(), request_id(), log_string.c_str(), result.size());
+                "%s[%s] succeeded. (results: '[%s]', %" PRIuS " mount points)",
+                name(), request_uuid().AsLowercaseString().c_str(),
+                log_string.c_str(), result.size());
   }
 
   return RespondNow(ArgumentList(
