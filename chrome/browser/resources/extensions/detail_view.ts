c15ae9315df12cada8c82195adc4bb12887de138
Adam Psarouthakis <psarouthakis@google.com>
2023-07-07 15:00:35 +0000

    SafetyCheckExtensions: Implement The Extensions Safety Check Details Page
    This CL contains the Extensions Detail page components of the
    extensions safety Check. There are the necessary files to add
    new Chrome UI strings to the interface.
    
    Bug: 1432194
    Change-Id: I438b2d29dfe2ff7abe6ea6bbbc5b709d48dbbc9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4579629
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1167438}

--- chrome/browser/resources/extensions/detail_view.ts
+++ chrome/browser/resources/extensions/detail_view.ts
@@ -48,6 +48,7 @@ export interface ExtensionsDetailViewElement {
     extensionsActivityLogLink: HTMLElement,
     extensionsOptions: CrLinkRowElement,
     parentDisabledPermissionsToolTip: CrTooltipIconElement,
+    safetyCheckWarningContainer: HTMLElement,
     source: HTMLElement,
   };
 }
@@ -99,6 +100,19 @@ export class ExtensionsDetailViewElement extends
         type: Array,
         computed: 'computeSortedViews_(data.views)',
       },
+
+      /** Whether the extensions safety check warning is shown. */
+      showSafetyCheck_: {
+        type: Boolean,
+        computed: 'computeShowSafetyCheck_(data.safetyCheckText)',
+        observer: 'onShowSafetyCheckChanged_',
+      },
+
+      /** Whether the extensions blocklist text is shown. */
+      showBlocklistText_: {
+        type: Boolean,
+        computed: 'computeShowBlocklistText_(data.blacklistText)',
+      },
     };
   }
 
@@ -113,8 +127,11 @@ export class ExtensionsDetailViewElement extends
   incognitoAvailable: boolean;
   showActivityLog: boolean;
   fromActivityLog: boolean;
+  private showSafetyCheck_: boolean;
+  private showBlocklistText_: boolean;
   private size_: string;
   private sortedViews_: chrome.developerPrivate.ExtensionView[];
+  private safetyCheckExtensionsEnabled_: boolean;
 
   override ready() {
     super.ready();
@@ -246,7 +263,15 @@ export class ExtensionsDetailViewElement extends
   }
 
   private onRemoveClick_() {
+    if (this.showSafetyCheck_) {
+      chrome.metricsPrivate.recordUserAction('SafetyCheck.DetailRemoveClicked');
+    }
     this.delegate.deleteItem(this.data.id);
+  }
+
+  private onKeepClick_() {
+    // TODO(crbug/1432194): Replace with keep extension API implementation
+    // and enable string variable.
   }
 
   private onRepairClick_() {
@@ -360,6 +385,25 @@ export class ExtensionsDetailViewElement extends
 
   private showReloadButton_(): boolean {
     return getEnableControl(this.data) === EnableControl.RELOAD;
+  }
+
+  private computeShowSafetyCheck_(): boolean {
+    if (!loadTimeData.getBoolean('safetyCheckShowReviewPanel')) {
+      return false;
+    }
+
+    return !!(
+        this.data.safetyCheckText && this.data.safetyCheckText.detailString);
+  }
+
+  private onShowSafetyCheckChanged_() {
+    if (this.showSafetyCheck_) {
+      chrome.metricsPrivate.recordUserAction('SafetyCheck.DetailWarningShown');
+    }
+  }
+
+  private computeShowBlocklistText_(): boolean {
+    return !this.showSafetyCheck_ && !!this.data.blacklistText;
   }
 
   private showRepairButton_(): boolean {
