a2daa9720df4c56e6e21a46a037dbf8884f31ed5
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-07-07 15:44:45 +0000

    [ios/password-settings] Hide B4P switch if SyncToSignin is on
    Behavior changes are behind flag.
    With kReplaceSyncPromosWithSignInPromos, the toggle should move
    from its temporary house in password settings to the definitive one
    in the unified account settings panel. This CL hides the temporary
    toggle.
    
    Fixed: 1462852
    Change-Id: If98a525641fbf4acfbcdeda75145a8b2c5a29716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664658
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167460}

--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_mediator.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/password_manager/core/common/password_manager_pref_names.h"
 #import "components/prefs/pref_service.h"
 #import "components/signin/public/identity_manager/objc/identity_manager_observer_bridge.h"
+#import "components/sync/base/features.h"
 #import "components/sync/base/passphrase_enums.h"
 #import "components/sync/base/user_selectable_type.h"
 #import "components/sync/service/sync_service_utils.h"
@@ -315,7 +316,9 @@ using password_manager::prefs::kCredentialsEnableService;
 
 - (PasswordSettingsAccountStorageState)computeAccountStorageState {
   if (_syncService->GetAccountInfo().IsEmpty() ||
-      _syncService->IsSyncFeatureEnabled()) {
+      _syncService->IsSyncFeatureEnabled() ||
+      base::FeatureList::IsEnabled(
+          syncer::kReplaceSyncPromosWithSignInPromos)) {
     return PasswordSettingsAccountStorageStateNotShown;
   }
 
