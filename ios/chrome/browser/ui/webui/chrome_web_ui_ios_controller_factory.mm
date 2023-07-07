2e53cb95a4abdce0f2e6bda32df7ccf5b805e2d0
Federica Germinario <fedegermi@google.com>
2023-07-07 07:36:54 +0000

    [ios] Implement chrome://userdefaults-internal page
    This page contains all NSUserDefaults keys and associated values.
    The page is not supported on Stable build and in incognito mode.
    To get more info: go/userdefaults-internal
    
    Bug: 1451365
    Change-Id: I2e625f925f2e6fb32753f4f1f070d56bddd5aca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4650397
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1167264}

--- ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/commerce/core/commerce_constants.h"
 #import "components/commerce/ios/browser/commerce_internals_ui.h"
 #import "components/optimization_guide/optimization_guide_internals/webui/url_constants.h"
+#import "components/version_info/channel.h"
 #import "ios/chrome/browser/commerce/shopping_service_factory.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
@@ -39,7 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/webui/translate_internals/translate_internals_ui.h"
 #import "ios/chrome/browser/ui/webui/ukm_internals_ui.h"
 #import "ios/chrome/browser/ui/webui/user_actions_ui.h"
+#import "ios/chrome/browser/ui/webui/userdefaults_internals_ui.h"
 #import "ios/chrome/browser/ui/webui/version_ui.h"
+#import "ios/chrome/common/channel_info.h"
 #import "ios/components/webui/sync_internals/sync_internals_ui.h"
 #import "ios/components/webui/web_ui_url_constants.h"
 #import "url/gurl.h"
@@ -48,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+using ::version_info::Channel;
 using web::WebUIIOS;
 using web::WebUIIOSController;
 
@@ -140,6 +144,10 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(const GURL& url) {
     return &NewWebUIIOS<VersionUI>;
   if (url_host == kChromeUIPolicyHost)
     return &NewWebUIIOS<PolicyUI>;
+  if (url_host == kChromeUIUserDefaultsInternalHost &&
+      GetChannel() != Channel::STABLE) {
+    return &NewWebUIIOS<UserDefaultsInternalsUI>;
+  }
 
   return nullptr;
 }
