ad8bd5b952d579ae46cf2aa39943a47e615d773f
Scott Yoder <scottyoder@google.com>
2023-07-07 17:34:07 +0000

    [ios] Disabling failing eg test
    Failing test:
    ConsistencyPromoSigninCoordinatorTestCase /
    testRemoveLastIdentityWithSigninErrorDialog
    
    Fixed: 1462958
    Change-Id: I04d4aeb0d3ee66bdb27dd9f337c59aeedaa6802c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672387
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Scott Yoder <scottyoder@google.com>
    Quick-Run: Scott Yoder <scottyoder@google.com>
    Owners-Override: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1167513}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator_egtest.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator_egtest.mm
@@ -95,7 +95,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Removes the only identity while the error dialog is opened. Once the identity
 // is removed, the web sign-in dialog is removed.
-- (void)testRemoveLastIdentityWithSigninErrorDialog {
+// TODO(crbug.com/1462976): Reenable this test.
+- (void)DISABLED_testRemoveLastIdentityWithSigninErrorDialog {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
