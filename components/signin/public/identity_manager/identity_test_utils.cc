60b93c38dd980f78019ed365af338afe1f60ff30
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-07-07 17:58:28 +0000

    👤 Avoid profile name resolution with a secondary account's info
    After the migration to Lacros from Ash profiles with multiple accounts,
    it was possible that a secondary account's extended info was available
    first, resulting in the info from that secondary account being used to
    generate the local profile name.
    
    Bug: 1450709
    Change-Id: I2aaa8321d9c1c0102028c945b303f3e3987ab942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664808
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167527}

--- components/signin/public/identity_manager/identity_test_utils.cc
+++ components/signin/public/identity_manager/identity_test_utils.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/run_loop.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
 #include "base/uuid.h"
 #include "build/build_config.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/internal/identity_manager/profile_oauth2_token_service_delegate.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/list_accounts_test_utils.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
 #include "components/signin/public/identity_manager/test_identity_manager_observer.h"
@@ -530,6 +532,26 @@ void SetCookieAccounts(
   cookie_manager->ListAccounts(nullptr, nullptr);
 
   run_loop.Run();
+}
+
+AccountInfo WithGeneratedUserInfo(const AccountInfo& base_account_info,
+                                  base::StringPiece given_name) {
+  CHECK(!given_name.empty())
+      << "A given name is needed to generate the Gaia info.";
+
+  AccountInfo extended_account_info = base_account_info;
+
+  extended_account_info.given_name = given_name;
+  extended_account_info.full_name = base::StrCat({given_name, " FullName"});
+
+  extended_account_info.picture_url =
+      "https://chromium.org/examples/account_picture.jpg";
+  extended_account_info.hosted_domain = kNoHostedDomainFound;
+  extended_account_info.locale = "en";
+
+  CHECK(extended_account_info.IsValid());
+
+  return extended_account_info;
 }
 
 void UpdateAccountInfoForAccount(IdentityManager* identity_manager,
--- components/signin/public/identity_manager/identity_test_utils.h
+++ components/signin/public/identity_manager/identity_test_utils.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/functional/bind.h"
+#include "base/functional/callback_forward.h"
 #include "base/strings/string_piece_forward.h"
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
@@ -297,6 +297,14 @@ void SetFreshnessOfAccountsInGaiaCookie(IdentityManager* identity_manager,
                                         bool accounts_are_fresh);
 
 std::string GetTestGaiaIdForEmail(const std::string& email);
+
+// Returns a new `AccountInfo` object that completes the info from
+// `base_account_info` with generated extended info (the one normally obtained
+// from Gaia's /UserInfo endpoint), based on `given_name`.
+//
+// The returned account info will be valid per `AccountInfo::IsValid()`.
+AccountInfo WithGeneratedUserInfo(const AccountInfo& base_account_info,
+                                  base::StringPiece given_name);
 
 // Updates the persistent auth error set on |account_id| which must be a known
 // account, i.e., an account with a refresh token.
