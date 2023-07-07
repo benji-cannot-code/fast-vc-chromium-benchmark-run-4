409f403f2a2035cfaaf9840f03f4272ee0b6b678
Kent Tamura <tkent@chromium.org>
2023-07-07 11:54:18 +0000

    [RemoveLegacy] Remove LayoutBox::VisualOverflowRectAllowingUnset()
    Use PhysicalVisualOverflowRectAllowingUnset() instead.
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: I230e20ca295ebef43ea5877ad31a0870cc495e97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671886
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167370}

--- third_party/blink/renderer/core/layout/layout_shift_tracker.cc
+++ third_party/blink/renderer/core/layout/layout_shift_tracker.cc
@@ -97,7 +97,7 @@ bool EqualWithinMovementThreshold(const gfx::PointF& a,
          fabs(a.y() - b.y()) < threshold_physical_px;
 }
 
-bool SmallerThanRegionGranularity(const LayoutRect& rect) {
+bool SmallerThanRegionGranularity(const PhysicalRect& rect) {
   // Normally we paint by snapping to whole pixels, so rects smaller than half
   // a pixel may be invisible.
   return rect.Width() < 0.5 || rect.Height() < 0.5;
@@ -189,7 +189,8 @@ bool LayoutShiftTracker::NeedsToTrack(const LayoutObject& object) const {
     return false;
   }
 
-  if (SmallerThanRegionGranularity(box->VisualOverflowRectAllowingUnset())) {
+  if (SmallerThanRegionGranularity(
+          box->PhysicalVisualOverflowRectAllowingUnset())) {
     return false;
   }
 
