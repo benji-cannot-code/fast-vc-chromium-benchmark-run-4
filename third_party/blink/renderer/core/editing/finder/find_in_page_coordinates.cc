52b32cd64660adec962aa59a3739958c68635233
Kent Tamura <tkent@chromium.org>
2023-07-07 11:53:05 +0000

    [RemoveLegacy] Remove LayoutBox::MaxLayoutOverflow()
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: Ie42ca98bf98b4ee7a103d876babaef30042c29b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672046
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167368}

--- third_party/blink/renderer/core/editing/finder/find_in_page_coordinates.cc
+++ third_party/blink/renderer/core/editing/finder/find_in_page_coordinates.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/visible_units.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/layout/geometry/writing_mode_converter.h"
 #include "third_party/blink/renderer/core/layout/layout_block.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
@@ -72,8 +73,12 @@ static gfx::RectF ToNormalizedRect(const gfx::RectF& absolute_rect,
   // We want to normalize by the max layout overflow size instead of only the
   // visible bounding box.  Quads and their enclosing bounding boxes need to be
   // used in order to keep results transform-friendly.
-  PhysicalRect overflow_rect = container->FlipForWritingMode(
-      LayoutRect(LayoutPoint(), container->MaxLayoutOverflow()));
+  auto converter = container->CreateWritingModeConverter();
+  LogicalRect logical_overflow_rect =
+      converter.ToLogical(container->PhysicalLayoutOverflowRect());
+  logical_overflow_rect.ShiftBlockStartEdgeTo(LayoutUnit());
+  logical_overflow_rect.ShiftInlineStartEdgeTo(LayoutUnit());
+  PhysicalRect overflow_rect = converter.ToPhysical(logical_overflow_rect);
 
   // For scrolling we need to get where the actual origin is independently of
   // the scroll.
