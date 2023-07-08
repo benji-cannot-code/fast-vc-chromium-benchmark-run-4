3caf55701f8647874a5c11d0d149376501192252
Hiroki Sato <hirokisato@google.com>
2023-07-08 02:48:11 +0000

    arc-a11y: Clean-up event type conversion
    This is a follow up from CL:4663569.
    
    * We don't need to dispatch kLayoutComplete.
    * Delete 'default:' from switch-statement, and add missing events.
    
    Bug: None
    Test: Tast arc.Accessibility* are passing.
    Test: unit_tests
    Change-Id: I41ba64a89254e8f57392fd51ddd68d5dc0e4419c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672130
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167726}

--- services/accessibility/android/android_accessibility_util.cc
+++ services/accessibility/android/android_accessibility_util.cc
@@ -26,6 +26,8 @@ absl::optional<ax::mojom::Event> ToAXEvent(
     case mojom::AccessibilityEventType::VIEW_FOCUSED:
     case mojom::AccessibilityEventType::VIEW_ACCESSIBILITY_FOCUSED:
       return ax::mojom::Event::kFocus;
+    case mojom::AccessibilityEventType::VIEW_ACCESSIBILITY_FOCUS_CLEARED:
+      return ax::mojom::Event::kBlur;
     case mojom::AccessibilityEventType::VIEW_CLICKED:
     case mojom::AccessibilityEventType::VIEW_LONG_CLICKED:
       return ax::mojom::Event::kClicked;
@@ -37,11 +39,9 @@ absl::optional<ax::mojom::Event> ToAXEvent(
       if (focused_node) {
         return ax::mojom::Event::kFocus;
       } else {
-        return ax::mojom::Event::kLayoutComplete;
+        return absl::nullopt;
       }
     }
-    case mojom::AccessibilityEventType::NOTIFICATION_STATE_CHANGED:
-      return ax::mojom::Event::kLayoutComplete;
     case mojom::AccessibilityEventType::WINDOW_CONTENT_CHANGED:
       int live_region_type_int;
       if (source_node && source_node->GetNode() &&
@@ -66,8 +66,6 @@ absl::optional<ax::mojom::Event> ToAXEvent(
         }
       }
       return absl::nullopt;
-    case mojom::AccessibilityEventType::WINDOWS_CHANGED:
-      return ax::mojom::Event::kLayoutComplete;
     case mojom::AccessibilityEventType::VIEW_HOVER_ENTER:
       return ax::mojom::Event::kHover;
     case mojom::AccessibilityEventType::ANNOUNCEMENT: {
@@ -88,6 +86,11 @@ absl::optional<ax::mojom::Event> ToAXEvent(
         return ax::mojom::Event::kFocus;
       }
     }
+    case mojom::AccessibilityEventType::INVALID_ENUM_VALUE: {
+      NOTREACHED();
+      break;
+    }
+    case mojom::AccessibilityEventType::NOTIFICATION_STATE_CHANGED:
     case mojom::AccessibilityEventType::VIEW_HOVER_EXIT:
     case mojom::AccessibilityEventType::TOUCH_EXPLORATION_GESTURE_START:
     case mojom::AccessibilityEventType::TOUCH_EXPLORATION_GESTURE_END:
@@ -97,10 +100,9 @@ absl::optional<ax::mojom::Event> ToAXEvent(
     case mojom::AccessibilityEventType::GESTURE_DETECTION_END:
     case mojom::AccessibilityEventType::TOUCH_INTERACTION_START:
     case mojom::AccessibilityEventType::TOUCH_INTERACTION_END:
+    case mojom::AccessibilityEventType::WINDOWS_CHANGED:
     case mojom::AccessibilityEventType::VIEW_CONTEXT_CLICKED:
     case mojom::AccessibilityEventType::ASSIST_READING_CONTEXT:
-      return absl::nullopt;
-    default:
       return absl::nullopt;
   }
   return absl::nullopt;
