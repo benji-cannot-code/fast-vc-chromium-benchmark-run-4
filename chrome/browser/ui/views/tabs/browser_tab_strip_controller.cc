c717cfbc1dd94e39207721893339c2d26f054413
Simon Hangl <simonha@google.com>
2023-07-09 18:23:30 +0000

    Moving `IsTabClosable` function out of `TabStripModel`
    This CL moves the pinned home app specific check implemented in
    `IsTabClosable` outside of the `TabStripModel`. This check is not,
    as suggested by the location, a check that should hold for all
    tabs but is specific to a few specific paths of closing a pinned
    home app tab.
    
    This insight comes from two reverts on this CL [1] (due to failing tast
    tests) and the explicit statement in this bug [2] that for example for
    terminal apps the tab must still be closable. Therefore this check
    should not be globally done in the `TabStripModel`.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4650749
    [2] https://buganizer.corp.google.com/issues/289870746
    
    Bug: b/278246367
    Change-Id: Id4b9eaa69c92bd30995da90755d60ccaa203c096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666021
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167857}

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_types.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
+#include "chrome/browser/ui/web_applications/web_app_tabbed_utils.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
@@ -317,7 +318,7 @@ void BrowserTabStripController::AddSelectionFromAnchorTo(int model_index) {
 
 bool BrowserTabStripController::BeforeCloseTab(int model_index,
                                                CloseTabSource source) {
-  if (!model_->IsTabClosable(model_index)) {
+  if (!web_app::IsTabClosable(model_, model_index)) {
     return false;
   }
 
