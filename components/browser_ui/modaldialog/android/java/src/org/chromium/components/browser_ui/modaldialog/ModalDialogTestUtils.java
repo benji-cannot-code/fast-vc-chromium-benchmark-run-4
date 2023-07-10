d22cef0869622a1005dafb41c35c108b27f5fa64
Zaina Al-Mashni <zalmashni@google.com>
2023-07-10 10:03:22 +0000

    [Android] Refactor the modal dialog to include DIALOG_STYLES property
    This CL combines the various existing dialog styles into a DIALOG_STYLE
    property, and it can only be one of the following:
    
    - FULLSCREEN_DIALOG
    - FULLSCREEN_DARK_DIALOG
    - DIALOG_WHEN_LARGE
    - NORMAL (default)
    
    It also removes the EXCEED_MAX_HEIGHT property to reduce the redundancy,
    since this property can be combined with the FULLSCREEN_DIALOG behaviour.
    Currently both properties need to be set to true whenever they are used.
    
    Bug: 1449618
    Change-Id: I7dab9de8c717175659c9e28408c1ddbd0b4e730f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666014
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1167990}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogTestUtils.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogTestUtils.java
@@ -60,6 +60,30 @@ public class ModalDialogTestUtils {
     public static PropertyModel createDialog(Activity activity, ModalDialogManager manager,
             String title, @Nullable TestDialogDismissedObserver observer,
             @ModalDialogProperties.ButtonStyles int buttonStyles) {
+        return createDialog(activity, manager, title, observer, buttonStyles,
+                ModalDialogProperties.DialogStyles.NORMAL);
+    }
+
+    /**
+     * @return A {@link PropertyModel} of a modal dialog that is used for testing with
+     *         dialog style
+     */
+    public static PropertyModel createDialogWithDialogStyle(Activity activity,
+            ModalDialogManager manager, String title,
+            @Nullable TestDialogDismissedObserver observer,
+            @ModalDialogProperties.DialogStyles int dialogStyles) {
+        return createDialog(activity, manager, title, observer,
+                ModalDialogProperties.ButtonStyles.PRIMARY_FILLED_NEGATIVE_OUTLINE, dialogStyles);
+    }
+
+    /**
+     * @return A {@link PropertyModel} of a modal dialog that is used for testing with
+     *         primary or negative button filled and dialog style.
+     */
+    public static PropertyModel createDialog(Activity activity, ModalDialogManager manager,
+            String title, @Nullable TestDialogDismissedObserver observer,
+            @ModalDialogProperties.ButtonStyles int buttonStyles,
+            @ModalDialogProperties.DialogStyles int dialogStyles) {
         return TestThreadUtils.runOnUiThreadBlockingNoException(() -> {
             ModalDialogProperties.Controller controller = new ModalDialogProperties.Controller() {
                 @Override
@@ -87,6 +111,7 @@ public class ModalDialogTestUtils {
                     .with(ModalDialogProperties.POSITIVE_BUTTON_TEXT, resources, R.string.ok)
                     .with(ModalDialogProperties.NEGATIVE_BUTTON_TEXT, resources, R.string.cancel)
                     .with(ModalDialogProperties.BUTTON_STYLES, buttonStyles)
+                    .with(ModalDialogProperties.DIALOG_STYLES, dialogStyles)
                     .build();
         });
     }
