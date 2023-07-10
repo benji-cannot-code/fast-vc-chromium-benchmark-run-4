2322e7115ece8ba6e6faced6ac27115daf95f152
Qiu Jianlin <jianlin.qiu@intel.com>
2023-07-10 18:30:07 +0000

    Use short format for ComPtr in win video accelerators.
    Consolidate all windows video accelerators to use ComXXX format for
    D3D11 and MF types when a ComPtr is needed.
    
    Bug: N/A
    Change-Id: I13ee75c591838e73a686d5a293abebafd927e901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671433
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1168225}

--- media/gpu/windows/d3d11_texture_wrapper.h
+++ media/gpu/windows/d3d11_texture_wrapper.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_GPU_WINDOWS_D3D11_TEXTURE_WRAPPER_H_
 
 #include <d3d11.h>
-#include <wrl/client.h>
 #include <memory>
 #include <vector>
 
