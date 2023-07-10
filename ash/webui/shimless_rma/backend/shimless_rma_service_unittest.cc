d8b3d527c0e72a9dc1593db1336248384b75cfff
Cam Bickel <cambickel@google.com>
2023-07-10 16:48:05 +0000

    shimless: Update SetDeviceInformation to use new fields
    This CL updates the Shimless backend code to read two new fields
    from the UI, `is_chassis_branded` and `hw_compliance_version`, and to
    use those values when calling `SetDeviceInformation` when the Next
    button is pressed.
    
    This CL also prevents the user from continuing on from the page
    if the two compliance questions are not set, since they're required
    fields. Note that the UI only blocks continuing if questions are shown,
    which is when the feature level is unknown + the flag is enabled.
    
    This CL also adds tests for these changes.
    
    Test: browser_tests --gtest_filter=ShimlessRmaApp*
    Test: ash_webui_unittests --gtest_filter=ShimlessRma*
    Bug: b/287317464
    Change-Id: Ia637fbf5e2972f284c8666d305f3ff93e7e89e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642648
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168152}

--- ash/webui/shimless_rma/backend/shimless_rma_service_unittest.cc
+++ ash/webui/shimless_rma/backend/shimless_rma_service_unittest.cc
@@ -2574,6 +2574,8 @@ TEST_F(ShimlessRmaServiceTest, SetDeviceInformation) {
         EXPECT_EQ(state.update_device_info().sku_index(), 2L);
         EXPECT_EQ(state.update_device_info().whitelabel_index(), 3L);
         EXPECT_EQ(state.update_device_info().dram_part_number(), "123-456-789");
+        EXPECT_EQ(state.update_device_info().is_chassis_branded(), true);
+        EXPECT_EQ(state.update_device_info().hw_compliance_version(), 22u);
       });
   base::RunLoop run_loop;
   shimless_rma_provider_->GetCurrentState(
@@ -2585,7 +2587,7 @@ TEST_F(ShimlessRmaServiceTest, SetDeviceInformation) {
   run_loop.RunUntilIdle();
 
   shimless_rma_provider_->SetDeviceInformation(
-      "serial number", 1, 2, 3, "123-456-789",
+      "serial number", 1, 2, 3, "123-456-789", true, 22u,
       base::BindLambdaForTesting([&](mojom::StateResultPtr state_result_ptr) {
         EXPECT_EQ(state_result_ptr->state, mojom::State::kChooseDestination);
         EXPECT_EQ(state_result_ptr->error, rmad::RmadErrorCode::RMAD_ERROR_OK);
@@ -2607,7 +2609,7 @@ TEST_F(ShimlessRmaServiceTest, SetDeviceInformationFromWrongStateFails) {
   run_loop.RunUntilIdle();
 
   shimless_rma_provider_->SetDeviceInformation(
-      "serial number", 1, 2, 3, "123-456-789",
+      "serial number", 1, 2, 3, "123-456-789", false, 0,
       base::BindLambdaForTesting([&](mojom::StateResultPtr state_result_ptr) {
         EXPECT_EQ(state_result_ptr->state, mojom::State::kChooseDestination);
         EXPECT_EQ(state_result_ptr->error,
