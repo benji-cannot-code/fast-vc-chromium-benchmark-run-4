7e39f4ffb975b95ddf0d1e340a7d91badcd6be0f
Denis Kuznetsov <antrim@chromium.org>
2023-07-10 17:05:51 +0000

    Remove unused SetDisplayAndGivenName method
    It seems that this data is now set via UserImageManagerImpl upon
    OnProfileDownloadSuccess.
    
    Bug: None
    Change-Id: I0d858c350cb0a2f1de195200e80e8a6ff2e35d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4669731
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168162}

--- chrome/browser/ash/login/existing_user_controller.h
+++ chrome/browser/ash/login/existing_user_controller.h
@@ -109,8 +109,6 @@ class ExistingUserController : public content::NotificationObserver,
   void CompleteLogin(const UserContext& user_context);
   void OnGaiaScreenReady();
   void SetDisplayEmail(const std::string& email);
-  void SetDisplayAndGivenName(const std::string& display_name,
-                              const std::string& given_name);
   bool IsUserAllowlisted(
       const AccountId& account_id,
       const absl::optional<user_manager::UserType>& user_type);
@@ -348,13 +346,6 @@ class ExistingUserController : public content::NotificationObserver,
 
   // The displayed email for the next login attempt set by `SetDisplayEmail`.
   std::string display_email_;
-
-  // The displayed name for the next login attempt set by
-  // `SetDisplayAndGivenName`.
-  std::u16string display_name_;
-
-  // The given name for the next login attempt set by `SetDisplayAndGivenName`.
-  std::u16string given_name_;
 
   // Whether login attempt is running.
   bool is_login_in_progress_ = false;
