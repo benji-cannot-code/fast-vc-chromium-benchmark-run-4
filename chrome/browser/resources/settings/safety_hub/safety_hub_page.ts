e086e6a44fd147f91af9b8c8a8c83f03d55c7edb
Side Yilmaz <sideyilmaz@chromium.org>
2023-07-10 17:47:21 +0000

    [SHv2] Add SafetyHubModule element
    This CL adds SafetyHubModule polymer element that will be used by
    different safety hub features later.
    
    Bug: 1443466
    Change-Id: I7251d6017f0619a23601916e62c62d5623f3049a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608066
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168193}

--- chrome/browser/resources/settings/safety_hub/safety_hub_page.ts
+++ chrome/browser/resources/settings/safety_hub/safety_hub_page.ts
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
+// TODO(crbug.com/1443466): Clean this dummy import after SafetyHubModuleElement
+// is used.
+import './safety_hub_module.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
