5b37dacfa3296207bcf12487c0b86eb01ab44d23
Thiabaud Engelbrecht <thiabaud@google.com>
2023-07-10 15:28:05 +0000

    [content] Clean up background GPU killing
    Since this always happens in the UI thread, there is no need to post a task.
    
    Also adds a test.
    
    Change-Id: I54329d54511e8b8ffe305b73ac79d2422c1220cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591291
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168117}

--- content/browser/renderer_host/compositor_dependencies_android.h
+++ content/browser/renderer_host/compositor_dependencies_android.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "components/viz/common/surfaces/frame_sink_id_allocator.h"
 #include "components/viz/host/host_frame_sink_manager.h"
+#include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/viz/privileged/mojom/compositing/frame_sink_manager.mojom.h"
@@ -26,7 +27,7 @@ namespace content {
 
 class CompositorImpl;
 
-class CompositorDependenciesAndroid {
+class CONTENT_EXPORT CompositorDependenciesAndroid {
  public:
   static CompositorDependenciesAndroid& Get();
 
@@ -44,6 +45,8 @@ class CompositorDependenciesAndroid {
   void TryEstablishVizConnectionIfNeeded();
   void OnCompositorVisible(CompositorImpl* compositor);
   void OnCompositorHidden(CompositorImpl* compositor);
+
+  void DoLowEndBackgroundCleanupForTesting() { DoLowEndBackgroundCleanup(); }
 
  private:
   friend class base::NoDestructor<CompositorDependenciesAndroid>;
