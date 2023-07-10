ada1f0448d2e0b72a246aaca750d1b78c0d4a064
Patrick Noland <pnoland@google.com>
2023-07-10 19:08:33 +0000

    [Omnibox] Increase min height by 4dp when wrapping lines
    Bug: 1453830
    Change-Id: I92cf18d8163220aad3e111cce1cc525dd530931f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671018
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168246}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/SuggestionViewViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/SuggestionViewViewBinder.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.annotation.ColorInt;
 
+import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
 import org.chromium.chrome.browser.omnibox.R;
 import org.chromium.chrome.browser.omnibox.styles.OmniboxResourceProvider;
 import org.chromium.chrome.browser.omnibox.suggestions.SuggestionCommonProperties;
@@ -27,6 +28,12 @@ public class SuggestionViewViewBinder {
         if (propertyKey == SuggestionViewProperties.TEXT_LINE_1_TEXT) {
             TextView tv = view.findViewById(R.id.line_1);
             tv.setText(model.get(SuggestionViewProperties.TEXT_LINE_1_TEXT));
+            if (OmniboxFeatures.shouldShowModernizeVisualUpdate(view.getContext())) {
+                int minHeight = tv.getResources().getDimensionPixelSize(tv.getLineCount() > 1
+                                ? R.dimen.omnibox_suggestion_minimum_content_height_multiline
+                                : R.dimen.omnibox_suggestion_minimum_content_height);
+                view.setMinimumHeight(minHeight);
+            }
         } else if (propertyKey == SuggestionCommonProperties.COLOR_SCHEME) {
             updateSuggestionTextColor(view, model);
         } else if (propertyKey == SuggestionViewProperties.IS_SEARCH_SUGGESTION) {
