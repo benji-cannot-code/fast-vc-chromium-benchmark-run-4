a204e8b9fa277470776d4d4cff4fc4193db4d043
Michael Ershov <miersh@google.com>
2023-07-10 09:06:41 +0000

    Kcer-over-NSS: Move kcer_nss into the kcer component
    Move the chrome/browser/chromeos/kcer_nss directory
    into the chromeos/components/kcer component.
    This makes all of the kcer code to be in the same place
    and allows to more convenient build a fuzzer for it.
    
    Bug: b:244408716
    Test: Kcer* chromeos_components_unittests
    Change-Id: I65be718bf493f41928c4de761ed620727de44815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542989
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167968}

--- chromeos/components/kcer/kcer_nss/cert_cache_nss.h
+++ chromeos/components/kcer/kcer_nss/cert_cache_nss.h
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_KCER_NSS_CERT_CACHE_NSS_H_
-#define CHROME_BROWSER_CHROMEOS_KCER_NSS_CERT_CACHE_NSS_H_
+#ifndef CHROMEOS_COMPONENTS_KCER_KCER_NSS_CERT_CACHE_NSS_H_
+#define CHROMEOS_COMPONENTS_KCER_KCER_NSS_CERT_CACHE_NSS_H_
 
 #include <set>
 
+#include "base/component_export.h"
 #include "base/containers/span.h"
 #include "base/memory/scoped_refptr.h"
 #include "net/cert/scoped_nss_types.h"
@@ -19,7 +20,8 @@ class Cert;
 namespace internal {
 
 // Cache for a collection of scoped_refptr<const Cert>-s.
-class CertCacheNss {
+// Exported for unit tests only.
+class COMPONENT_EXPORT(KCER) CertCacheNss {
  public:
   // Empty cache.
   CertCacheNss();
@@ -57,4 +59,4 @@ class CertCacheNss {
 }  // namespace internal
 }  // namespace kcer
 
-#endif  // CHROME_BROWSER_CHROMEOS_KCER_NSS_CERT_CACHE_NSS_H_
+#endif  // CHROMEOS_COMPONENTS_KCER_KCER_NSS_CERT_CACHE_NSS_H_
--- chromeos/components/kcer/kcer_nss/cert_cache_nss_unittest.cc
+++ chromeos/components/kcer/kcer_nss/cert_cache_nss_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/kcer_nss/cert_cache_nss.h"
+#include "chromeos/components/kcer/kcer_nss/cert_cache_nss.h"
 
 #include <memory>
 #include <string>
