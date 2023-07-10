37a1ccf7864daa185122d8a02f924a6f1d6c4d12
Jeremy Roman <jbroman@chromium.org>
2023-07-10 16:03:44 +0000

    page_info: Use PrimaryPageChanged instead of DidFinishNavigation.
    This only cares about primary page changes, and is the preferred/simpler
    case after MPArch. This requires changing the optimization_guide
    interface to take GURL rather than NavigationHandle (otherwise, it was
    only used for a DCHECK).
    
    Bug: 1269645
    Change-Id: I9ea2ef759e54ecdb28f998136f5bd700f4ce8bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661443
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168131}

--- chrome/browser/page_info/about_this_site_tab_helper.h
+++ chrome/browser/page_info/about_this_site_tab_helper.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_user_data.h"
 
 namespace optimization_guide {
-class OptimizationGuideDecider;
+class NewOptimizationGuideDecider;
 class OptimizationMetadata;
 }  // namespace optimization_guide
 
@@ -31,13 +31,13 @@ class AboutThisSiteTabHelper
   AboutThisSiteTabHelper& operator=(const AboutThisSiteTabHelper&) = delete;
 
   // content::WebContentsObserver implementation
-  void DidFinishNavigation(
-      content::NavigationHandle* navigation_handle) override;
+  void PrimaryPageChanged(content::Page& page) override;
 
  private:
   explicit AboutThisSiteTabHelper(
       content::WebContents* web_contents,
-      optimization_guide::OptimizationGuideDecider* optimization_guide_decider,
+      optimization_guide::NewOptimizationGuideDecider*
+          optimization_guide_decider,
       page_info::AboutThisSiteService* about_this_site_service);
   friend class content::WebContentsUserData<AboutThisSiteTabHelper>;
 
@@ -46,7 +46,7 @@ class AboutThisSiteTabHelper
       optimization_guide::OptimizationGuideDecision decision,
       const optimization_guide::OptimizationMetadata& metadata);
 
-  raw_ptr<optimization_guide::OptimizationGuideDecider>
+  raw_ptr<optimization_guide::NewOptimizationGuideDecider>
       optimization_guide_decider_ = nullptr;
   raw_ptr<page_info::AboutThisSiteService> about_this_site_service_ = nullptr;
 
