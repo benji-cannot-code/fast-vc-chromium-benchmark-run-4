7b2eb9f2d3f7f53174a3ff4a28715f100a15129d
Emilia Paz <emiliapaz@chromium.org>
2023-07-10 22:19:50 +0000

    [Extensions c2s] Display toolbar IPH in the correct priority order
    Display extensions toolbar IPH with the following priority:
      1. If request button is visible:
         kIPHExtensionsRequestAccessButtonFeature
      2. If extensions toolbar button has "any extension has access" state:
         kIPHExtensionsMenuFeature
    
    For this, we moved the IPH trigger to UpdateControls(). This also fixes
    the bug where kIPHExtensionsMenuFeature was shown for the incorrect
    extensions toolbar button state, since UpdateAllIcons() is triggered
    many times.
    
    Screencast: https://drive.google.com/file/d/1zgGqZsGwrJduc1Oordtj6WoyuRxSh6yx/view?usp=sharing
    
    Bug: 1357159
    Change-Id: I2a4f1fdf5ff6ea0f291fda66206ece66e3b13564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4662115
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168355}

--- chrome/browser/ui/views/extensions/extensions_toolbar_container.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_container.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/extensions/extension_action_view_controller.h"
 #include "chrome/browser/ui/extensions/settings_api_bubble_helpers.h"
 #include "chrome/browser/ui/layout_constants.h"
@@ -34,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_action_hover_card_controller.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_actions_bar_bubble_views.h"
 #include "components/feature_engagement/public/event_constants.h"
-#include "components/feature_engagement/public/feature_constants.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/common/extension_features.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
@@ -216,18 +214,6 @@ ExtensionsToolbarContainer::~ExtensionsToolbarContainer() {
 }
 
 void ExtensionsToolbarContainer::UpdateAllIcons() {
-  // Display Extensions menu IPH. Toolbar view needs to be initialized, thus we
-  // show IPH once the extensions container is updating its icons.
-  // TODO(crbug.com/1357159): UpdateAllIcons() gets called multiple time when
-  // transitioning from one button state to the other. This causes to display
-  // the IPH when it's not needed. Have a better check of when we show this.
-  if (browser_->window() && GetVisible() &&
-      GetExtensionsButton()->state() ==
-          ExtensionsToolbarButton::State::kAnyExtensionHasAccess) {
-    browser_->window()->MaybeShowFeaturePromo(
-        feature_engagement::kIPHExtensionsMenuFeature);
-  }
-
   UpdateControlsVisibility();
 
   for (const auto& action : actions_)
@@ -1023,7 +1009,7 @@ void ExtensionsToolbarContainer::UpdateControlsVisibility() {
               web_contents->GetPrimaryMainFrame()->GetLastCommittedOrigin());
 
   extensions_controls_->UpdateControls(is_restricted_url, actions_,
-                                       site_setting, web_contents);
+                                       site_setting, web_contents, browser_);
 }
 
 void ExtensionsToolbarContainer::UpdateToolbarActionHoverCard(
