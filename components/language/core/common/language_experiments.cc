580fd2398a5cd78a54adeab1e61e79a7e254c0b2
Anthony Cui <cuianthony@google.com>
2023-07-10 17:34:54 +0000

    Clean up SRP Language Prompt feature code
    SRP Language Prompt has fully launched and the related feature code can be cleaned up. This involves three different feature flags
    - kAppLanguagePrompt: launched and can be cleaned up
    - kAppLanguagePromptULP: launched and can be cleaned up
    - kForceAppLanguagePrompt: was implemented for testing only and can be fully removed
    
    Bug: 1459423
    Change-Id: I32695d5da2b3dac77d23f394f94ceffc20440b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4658120
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168186}

--- components/language/core/common/language_experiments.cc
+++ components/language/core/common/language_experiments.cc
@@ -12,15 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace language {
 // Features:
-BASE_FEATURE(kAppLanguagePrompt,
-             "AppLanguagePrompt",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kAppLanguagePromptULP,
-             "AppLanguagePromptULP",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kForceAppLanguagePrompt,
-             "ForceAppLanguagePrompt",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kDetailedLanguageSettings,
              "DetailedLanguageSettings",
              base::FEATURE_ENABLED_BY_DEFAULT);
