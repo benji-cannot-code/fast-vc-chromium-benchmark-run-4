09f41c053071d27dc7f331610bf104d2f1b7fcfd
Chidera Olibie <colibie@google.com>
2023-07-10 12:45:36 +0000

    [Cronet] fix error when req is cancelled.
    Cancelling a request that has not started should not
    throw an error. This cl makes cancel() do nothing when
    the request is already in a terminal state or not_started.
    THis is in parity with the native engine's behaviour.
    
    Bug: b/290067767
    Change-Id: I7aeee52a38be78cad48717927021144c97369666
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672411
    Auto-Submit: Chidera Olibie <colibie@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168031}

--- components/cronet/android/fake/java/org/chromium/net/test/FakeUrlRequest.java
+++ components/cronet/android/fake/java/org/chromium/net/test/FakeUrlRequest.java
@@ -452,6 +452,10 @@ final class FakeUrlRequest extends UrlRequestBase {
     @Override
     public void cancel() {
         synchronized (mLock) {
+            if (mState == State.NOT_STARTED || isDone()) {
+                return;
+            }
+
             final UrlResponseInfo info = mUrlResponseInfo;
             final RefCountDelegate inflightDoneCallbackCount = setTerminalState(State.CANCELLED);
             if (inflightDoneCallbackCount != null) {
