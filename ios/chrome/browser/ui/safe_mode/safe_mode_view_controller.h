c2e89405c375c90bcdae814532b67a6df268255e
ginnnnnnny <ginnyhuang@chromium.org>
2023-07-10 10:05:07 +0000

    [iOS] Unfancify UI: Remove ios/c/b/ui/fancy_ui/ and deprecate content
    By the time of this CL there is only one class in ios/c/b/fancy_ui
    which is the PrimaryActionButton. This is only used by safe mode and
    one of its dependencies are deprecating, so removing it in this CL,
    and replacing its existence in safe mode view with PrimaryActionButton
    in ios/chrome/common/ui/util/button_util.h. The button text is also
    updated to follow the UI of our primary CTA.
    
    Before: https://screenshot.googleplex.com/BhAYWFnj5BmW75v
    After: https://screenshot.googleplex.com/8zdCRdemdVGJsXg
    
    Bug: N/A
    Change-Id: If5d862d9f74cbf92e62dbb42aae26d1f22a008c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651976
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167991}

--- ios/chrome/browser/ui/safe_mode/safe_mode_view_controller.h
+++ ios/chrome/browser/ui/safe_mode/safe_mode_view_controller.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-@class PrimaryActionButton;
-
 // A protocol required by delegates of the SafeModeViewController.
 @protocol SafeModeViewControllerDelegate
 @required
