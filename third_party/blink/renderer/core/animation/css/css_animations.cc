504b3ea0f8eb8a739238f5a5555967473c8e64ba
Kevin Ellis <kevers@google.com>
2023-07-10 12:56:04 +0000

    SDA: Fix ComputeTimeline on a pseudo-element.
    Bug: 1457107
    Change-Id: I22481266fb79714a27eba62982e8509e101a10de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651354
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168033}

--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -1509,7 +1509,7 @@ void CSSAnimations::CalculateTimelineUpdate(
 
 void CSSAnimations::CalculateAnimationUpdate(
     CSSAnimationUpdate& update,
-    const Element& animating_element,
+    Element& animating_element,
     Element& element,
     const ComputedStyleBuilder& style_builder,
     const ComputedStyle* parent_style,
@@ -1670,7 +1670,7 @@ void CSSAnimations::CalculateAnimationUpdate(
 
         AnimationTimeline* timeline = existing_animation->Timeline();
         if (!is_animation_style_change && !animation->GetIgnoreCSSTimeline()) {
-          timeline = ComputeTimeline(&element, style_timeline, update,
+          timeline = ComputeTimeline(&animating_element, style_timeline, update,
                                      existing_animation->Timeline());
         }
 
@@ -1706,8 +1706,9 @@ void CSSAnimations::CalculateAnimationUpdate(
         }
       } else {
         DCHECK(!is_animation_style_change);
-        AnimationTimeline* timeline = ComputeTimeline(
-            &element, style_timeline, update, /* existing_timeline */ nullptr);
+        AnimationTimeline* timeline =
+            ComputeTimeline(&animating_element, style_timeline, update,
+                            /* existing_timeline */ nullptr);
 
         CSSAnimationProxy animation_proxy(timeline, /* animation */ nullptr,
                                           is_paused, range_start, range_end,
