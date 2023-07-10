4dbe922ef604debf146f9f7f3fe1fb8daf11e54b
Christoph Schlosser <cschlosser@chromium.org>
2023-07-10 17:08:27 +0000

    redaction_tool: Add UMA metric measuring time to redact input
    With the growing list of redactions we want to make sure that we aren't
    running them too long in the background negatively impacting the users
    experience after a report.
    
    Bug: b:281836883
    Change-Id: I57b0ddc9c16ebb7e0d51eb34d6c6c72cef4be2f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659509
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Auto-Submit: Christoph Schlosser <cschlosser@chromium.org>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168164}

--- components/feedback/redaction_tool/inprocess_metrics_recorder.cc
+++ components/feedback/redaction_tool/inprocess_metrics_recorder.cc
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 
+namespace {
+constexpr char kTimeSpentRedactingHistogram[] =
+    "Feedback.RedactionTool.TimeSpentRedacting";
+}
+
 namespace redaction {
 
 std::unique_ptr<RedactionToolMetricsRecorder>
@@ -21,6 +26,16 @@ void InprocessMetricsRecorder::RecordPIIRedactedHistogram(PIIType pii_type) {
 void InprocessMetricsRecorder::RecordCreditCardRedactionHistogram(
     CreditCardDetection step) {
   UMA_HISTOGRAM_ENUMERATION(kCreditCardRedactionHistogram, step);
+}
+
+void InprocessMetricsRecorder::RecordTimeSpentRedactingHistogram(
+    base::TimeDelta time_spent) {
+  UMA_HISTOGRAM_MEDIUM_TIMES(kTimeSpentRedactingHistogram, time_spent);
+}
+
+base::StringPiece
+RedactionToolMetricsRecorder::GetTimeSpentRedactingHistogramNameForTesting() {
+  return kTimeSpentRedactingHistogram;
 }
 
 }  // namespace redaction
--- components/feedback/redaction_tool/redaction_tool_metrics_recorder.h
+++ components/feedback/redaction_tool/redaction_tool_metrics_recorder.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/strings/string_piece.h"
+#include "base/time/time.h"
 #include "components/feedback/redaction_tool/pii_types.h"
 
 namespace redaction {
@@ -48,6 +50,14 @@ class RedactionToolMetricsRecorder {
   // Records the `step` that was reached when validating a series of numbers
   // against credit card checks.
   virtual void RecordCreditCardRedactionHistogram(CreditCardDetection step) = 0;
+
+  // Records the `time_spent` in milliseconds for redacting an input text.
+  virtual void RecordTimeSpentRedactingHistogram(
+      base::TimeDelta time_spent) = 0;
+
+  // Returns the platform specific metric name used to measure how long it took
+  // to redact the input.
+  static base::StringPiece GetTimeSpentRedactingHistogramNameForTesting();
 };
 
 }  // namespace redaction
