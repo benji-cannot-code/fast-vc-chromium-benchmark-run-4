4f71a4fc83402b840734384f748c7365d04c87ee
Andrew Williams <awillia@chromium.org>
2023-07-10 16:07:36 +0000

    Update UNIT_TEST presubmit check to match more cases
    Updates the UNIT_TEST presubmit check to do more robust checking
    for the UNIT_TEST #define used in source files. Specifically,
    the check will now look for cases such as `#if defined(UNIT_TEST)`.
    
    Also, remove UNIT_TEST ifdef blocks from one .cc file that was
    using them.
    
    Bug: None
    Change-Id: I541d3d6de139f59a32285962afd9035ae99d2706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672065
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168134}

--- chrome/browser/predictors/preconnect_manager.cc
+++ chrome/browser/predictors/preconnect_manager.cc
@@ -195,11 +195,6 @@ void PreconnectManager::PreconnectUrl(
 
   auto* network_context = GetNetworkContext();
 
-#if defined(UNIT_TEST)
-  if (!network_context)
-    return;
-#endif
-
   network_context->PreconnectSockets(num_sockets, url, allow_credentials,
                                      network_anonymization_key);
 }
@@ -213,18 +208,6 @@ std::unique_ptr<ResolveHostClientImpl> PreconnectManager::PreresolveUrl(
 
   auto* network_context = GetNetworkContext();
 
-#if defined(UNIT_TEST)
-  if (!network_context) {
-    // Cannot invoke the callback right away because it would cause a
-    // use-after-free after returning from this method:
-    // The return value of this method is assigned to a member variable of a
-    // PreresolveJob that is destroyed when the callback executes.
-    content::GetUIThreadTaskRunner()->PostTask(
-        FROM_HERE, base::BindOnce(std::move(callback), false));
-    return nullptr;
-  }
-#endif
-
   return std::make_unique<ResolveHostClientImpl>(
       url, network_anonymization_key, std::move(callback), network_context);
 }
@@ -237,13 +220,6 @@ std::unique_ptr<ProxyLookupClientImpl> PreconnectManager::LookupProxyForUrl(
   DCHECK(url.SchemeIsHTTPOrHTTPS());
 
   auto* network_context = GetNetworkContext();
-
-#if defined(UNIT_TEST)
-  if (!network_context) {
-    std::move(callback).Run(false);
-    return nullptr;
-  }
-#endif
 
   return std::make_unique<ProxyLookupClientImpl>(
       url, network_anonymization_key, std::move(callback), network_context);
@@ -377,12 +353,6 @@ void PreconnectManager::AllPreresolvesForUrlFinished(PreresolveInfo* info) {
 network::mojom::NetworkContext* PreconnectManager::GetNetworkContext() const {
   if (network_context_)
     return network_context_;
-
-#if defined(UNIT_TEST)
-  // We're testing and |network_context_| wasn't set. Return nullptr to avoid
-  // hitting the network.
-  return nullptr;
-#endif
 
   auto* network_context =
       browser_context_->GetDefaultStoragePartition()->GetNetworkContext();
