5d8decd623a2de1f02e82347c79bea56eddf75d0
Muhamed Parić <muhamedp@google.com>
2023-07-10 15:48:37 +0000

    Enable the `kEnterpriseConnectorsEnabledOnMGS` Finch flag
    Launch bug: http://launch/4255200
    
    The plan is to have the feature available in Chrome 116 for a TT program.
    
    Bug: b/266534635
    Change-Id: I5a192126456a19b74048a44d39634bda1b0961e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575546
    Commit-Queue: Muhamed Parić <muhamedp@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Muhamed Parić <muhamedp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168124}

--- chrome/browser/enterprise/connectors/connectors_service.cc
+++ chrome/browser/enterprise/connectors/connectors_service.cc
@@ -160,7 +160,7 @@ std::unique_ptr<ClientMetadata> GetBasicClientMetadata() {
 
 BASE_FEATURE(kEnterpriseConnectorsEnabledOnMGS,
              "EnterpriseConnectorsEnabledOnMGS",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // --------------------------------
 // ConnectorsService implementation
