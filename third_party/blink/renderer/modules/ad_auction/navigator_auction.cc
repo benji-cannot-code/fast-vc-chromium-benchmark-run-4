ed6a2ac091366f7fb0c5212ba7eabef790b0fbca
Russ Hamilton <behamilton@google.com>
2023-07-10 20:11:27 +0000

    Fix renderer crash when decision URL not present
    Bug: 1442274
    Change-Id: Id148d06e954fa5a6943a9668d6cdf2981489ab29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670581
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168283}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
@@ -1095,6 +1095,9 @@ bool CopyDecisionLogicUrlFromIdlToMojo(const ExecutionContext& context,
                                        ExceptionState& exception_state,
                                        const AuctionAdConfig& input,
                                        mojom::blink::AuctionAdConfig& output) {
+  if (!input.hasDecisionLogicURL()) {
+    return true;
+  }
   KURL decision_logic_url = context.CompleteURL(input.decisionLogicURL());
   if (!decision_logic_url.IsValid()) {
     exception_state.ThrowTypeError(ErrorInvalidAuctionConfig(
