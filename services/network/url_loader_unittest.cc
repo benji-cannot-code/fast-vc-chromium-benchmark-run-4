27861ef30d03fc71a3fb32c97a39f57896e83184
Jonathan Hao <phao@chromium.org>
2023-07-10 23:21:40 +0000

    Revert "[Local Network Access] Rename net log event from private to local"
    This reverts commit 0e5a66bc88f07d6900b5e3d346d5eb9a5b8a76ae.
    
    Reason for revert: The spec has been renamed back to Private Network Access https://github.com/WICG/private-network-access/pull/106
    
    Original change's description:
    > [Local Network Access] Rename net log event from private to local
    >
    > In https://github.com/WICG/local-network-access/issues/91, we decided to
    > rename Private Network Access to Local Network Access. The spec has
    > already been renamed in
    > https://github.com/WICG/local-network-access/pull/97. The latest spec:
    > https://wicg.github.io/local-network-access/
    >
    > This CL renames PrivateNetwork to LocalNetwork in net log events.
    >
    > There are so many places to rename, so during the process, there will
    > inevitably be inconsistencies. Hopefully, we shall resolve all of them
    > soon.
    >
    > Bug: 1418287
    > Change-Id: Idc3dcaee7d761b64d0d92ed393899334445e0541
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4336324
    > Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    > Commit-Queue: Jonathan Hao <phao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1118082}
    
    Bug: 1418287
    Change-Id: Ie4a6824ae6c276960997d4548c36f48fd8d3def6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675841
    Auto-Submit: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168390}

--- services/network/url_loader_unittest.cc
+++ services/network/url_loader_unittest.cc
@@ -2037,7 +2037,7 @@ TEST_F(URLLoaderTest, AddsNetLogEntryForPrivateNetworkAccessCheckSuccess) {
   std::ignore = LoadRequest(request);
 
   std::vector<net::NetLogEntry> entries = net_log_observer.GetEntriesWithType(
-      net::NetLogEventType::LOCAL_NETWORK_ACCESS_CHECK);
+      net::NetLogEventType::PRIVATE_NETWORK_ACCESS_CHECK);
 
   ASSERT_THAT(entries, SizeIs(1));
 
@@ -2067,7 +2067,7 @@ TEST_F(URLLoaderTest, AddsNetLogEntryForPrivateNetworkAccessCheckFailure) {
   std::ignore = LoadRequest(request);
 
   std::vector<net::NetLogEntry> entries = net_log_observer.GetEntriesWithType(
-      net::NetLogEventType::LOCAL_NETWORK_ACCESS_CHECK);
+      net::NetLogEventType::PRIVATE_NETWORK_ACCESS_CHECK);
 
   ASSERT_THAT(entries, SizeIs(1));
 
@@ -2098,7 +2098,7 @@ TEST_F(URLLoaderTest, AddsNetLogEntryForPrivateNetworkAccessCheckSameOrigin) {
   EXPECT_EQ(net::OK, LoadRequest(request));
 
   std::vector<net::NetLogEntry> entries = net_log_observer.GetEntriesWithType(
-      net::NetLogEventType::LOCAL_NETWORK_ACCESS_CHECK);
+      net::NetLogEventType::PRIVATE_NETWORK_ACCESS_CHECK);
 
   ASSERT_THAT(entries, SizeIs(1));
 
