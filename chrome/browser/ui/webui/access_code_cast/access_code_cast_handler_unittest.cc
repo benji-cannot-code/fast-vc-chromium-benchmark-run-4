caf7a1c7d80d6e27e9a5634aeb0b461ca6935211
George Benz <gbj@google.com>
2023-07-10 17:44:56 +0000

    Adds UMA metric for new device histogram.
    Records the time it takes to connect to a new device. It is the combination of connecting to our server, validating the access code, constructing a cast device, opening a channel to that device, and then waiting for success.
    
    Bug: b/285939338
    Change-Id: If14b52e8039277325d39819403e2daa8b6689e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667968
    Commit-Queue: george benz <gbj@google.com>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168192}

--- chrome/browser/ui/webui/access_code_cast/access_code_cast_handler_unittest.cc
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_handler_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/test/bind.h"
 #include "base/test/gmock_callback_support.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/test/test_simple_task_runner.h"
@@ -76,6 +77,8 @@ class MockPage : public access_code_cast::mojom::Page {
 };
 
 const char kEmail[] = "mock_email@gmail.com";
+constexpr char histogram[] =
+    "AccessCodeCast.Session.NewDeviceRouteCreationDuration";
 
 }  // namespace
 
@@ -584,6 +587,20 @@ TEST_F(AccessCodeCastHandlerTest, ProfileSyncSuccess) {
       "foo_code",
       access_code_cast::mojom::CastDiscoveryMethod::INPUT_ACCESS_CODE,
       mock_callback_success.Get());
+}
+
+// Demonstrates that adding a sink and successfully casting to it will trigger a
+// histogram.
+TEST_F(AccessCodeCastHandlerTest, SuccessfulAddAndCastMetric) {
+  base::HistogramTester histogram_tester;
+  histogram_tester.ExpectTotalCount(histogram, 0);
+
+  set_expected_cast_result(mojom::RouteRequestResultCode::OK);
+  MockCastToSinkCallback mock_cast_sink_callback;
+  EXPECT_CALL(mock_cast_sink_callback, Run(RouteRequestResultCode::OK));
+  StartDesktopMirroring(MediaSource::ForUnchosenDesktop(),
+                        mock_cast_sink_callback);
+  histogram_tester.ExpectTotalCount(histogram, 1);
 }
 
 }  // namespace media_router
