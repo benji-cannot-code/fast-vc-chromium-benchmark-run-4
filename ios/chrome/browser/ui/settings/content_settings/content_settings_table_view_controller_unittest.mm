1460a8144487771bb762ae7cebd6573c5d065674
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-10 15:25:43 +0000

    [iOS] dealloc checks it was dismissed
    Change-Id: Ie263082d025cd3d6600c21fc46e4d5ea957db17e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672926
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168114}

--- ios/chrome/browser/ui/settings/content_settings/content_settings_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/content_settings/content_settings_table_view_controller_unittest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/content_settings/content_settings_table_view_controller.h"
 
+#import "base/mac/foundation_util.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/shared/ui/table_view/chrome_table_view_controller_test.h"
@@ -26,6 +27,12 @@ class ContentSettingsTableViewControllerTest
   ContentSettingsTableViewControllerTest() {
     browser_state_ = TestChromeBrowserState::Builder().Build();
     browser_ = std::make_unique<TestBrowser>(browser_state_.get());
+  }
+
+  void TearDown() override {
+    [base::mac::ObjCCastStrict<ContentSettingsTableViewController>(controller())
+        settingsWillBeDismissed];
+    ChromeTableViewControllerTest::TearDown();
   }
 
   ChromeTableViewController* InstantiateController() override {
