4bdb024ba93d96d19154bcaddbc0765b94b8e280
Hira Mahmood <hiramahmood@google.com>
2023-07-11 22:02:06 +0000

    [iOS] Create new trigger requirements for App Store Rating promo
    This change:
    * Creates anew trigger requirements for the App Store Rating promo
    behind a new flag.
    * Adds a unit test case for the new requirements to
    AppStoreRatingSceneAgentTest.
    
    Bug: None
    Change-Id: I7d8fb1d39fd5354095b72148279ce9c4b21ffe43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666427
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168976}

--- ios/chrome/browser/ui/app_store_rating/app_store_rating_scene_agent.mm
+++ ios/chrome/browser/ui/app_store_rating/app_store_rating_scene_agent.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/ui/app_store_rating/constants.h"
+#import "ios/chrome/browser/ui/app_store_rating/features.h"
 #import "ios/chrome/common/channel_info.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -63,6 +64,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (BOOL)isUserEngaged {
+  if (IsAppStoreRatingLoosenedTriggersEnabled()) {
+    return IsChromeLikelyDefaultBrowser() || self.CPEEnabled;
+  }
   return IsChromeLikelyDefaultBrowser() && self.daysInPastWeekRequirementMet &&
          self.totalDaysRequirementMet && self.CPEEnabled;
 }
--- ios/chrome/browser/ui/app_store_rating/features.h
+++ ios/chrome/browser/ui/app_store_rating/features.h
@@ -11,7 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Feature flag to enable the App Store Rating feature.
 BASE_DECLARE_FEATURE(kAppStoreRating);
 
+// Feature flag to enable the App Store Rating loosened triggers feature.
+BASE_DECLARE_FEATURE(kAppStoreRatingLoosenedTriggers);
+
 // Returns true if App Store Rating feature is enabled.
 bool IsAppStoreRatingEnabled();
+
+// Returns true if App Store Rating loosened triggers feature is enabled.
+bool IsAppStoreRatingLoosenedTriggersEnabled();
 
 #endif  // IOS_CHROME_BROWSER_UI_APP_STORE_RATING_FEATURES_H_
