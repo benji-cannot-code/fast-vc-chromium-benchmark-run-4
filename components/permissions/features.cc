3804d8ad32dbe64223a291012ecd6eb8d00e2e09
rubberyuzu <yuzus@chromium.org>
2023-07-11 05:11:28 +0000

    [bfcache] Unblock permission requests
    This CL makes permission requests not disable back/forward cache with a flag.
    The tests ensure that when the flag is on, the page can enter bfcache
    and the request is cancelled like a normal navigation.
    The flag is disabled by default and we will roll this out via finch.
    
    Bug: 1462829
    Change-Id: I6fdb04ef09330f6d516721e9dd9396ed7cf3a87f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546518
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168524}

--- components/permissions/features.cc
+++ components/permissions/features.cc
@@ -9,6 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace permissions {
 namespace features {
 
+// Enables or disables whether pages with pending permission requests will
+// go into back/forward cache.
+BASE_FEATURE(kBackForwardCacheUnblockPermissionRequest,
+             "BackForwardCacheUnblockPermissionRequest",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables or disables whether permission prompts are automatically blocked
 // after the user has explicitly dismissed them too many times.
 BASE_FEATURE(kBlockPromptsIfDismissedOften,
