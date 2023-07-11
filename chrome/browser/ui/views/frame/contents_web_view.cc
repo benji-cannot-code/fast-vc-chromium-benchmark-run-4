fe24b0ebd83fa6b6c6d68eca83895f61f1521be3
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-07-11 23:28:35 +0000

    [Rounded Windows] Add ability to round the background of ContentsWebView
    As part of MNext, we need to round all four corners of ash browser
    frame. In order to round the bottom two corners of browser frame, we
    need to round bottom two corners of web contents.
    
    This CL add the ability to round the background  of ContentsWebView and
    in followup CL use the api to round the background ContentsWebView.
    
    Bug: 1415486, b:287060338
    Change-Id: I78df92849bbd3ff81627242d9f97b305a07281b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671435
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169029}

--- chrome/browser/ui/views/frame/contents_web_view.cc
+++ chrome/browser/ui/views/frame/contents_web_view.cc
@@ -5,15 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
 
-#include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/views/status_bubble_views.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/base/theme_provider.h"
 #include "ui/color/color_provider.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_tree_owner.h"
@@ -50,6 +47,13 @@ void ContentsWebView::SetBackgroundVisible(bool background_visible) {
     UpdateBackgroundColor();
 }
 
+void ContentsWebView::SetBackgroundRadii(const gfx::RoundedCornersF& radii) {
+  background_radii_ = radii;
+  if (GetWidget()) {
+    UpdateBackgroundColor();
+  }
+}
+
 bool ContentsWebView::GetNeedsNotificationWhenVisibleBoundsChange() const {
   return true;
 }
@@ -73,7 +77,8 @@ void ContentsWebView::UpdateBackgroundColor() {
   SkColor color = GetColorProvider()->GetColor(
       is_letterboxing() ? kColorWebContentsBackgroundLetterboxing
                         : kColorWebContentsBackground);
-  SetBackground(background_visible_ ? views::CreateSolidBackground(color)
+  SetBackground(background_visible_ ? views::CreateRoundedRectBackground(
+                                          color, background_radii_)
                                     : nullptr);
 
   if (web_contents()) {
