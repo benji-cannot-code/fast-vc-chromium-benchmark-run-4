092f23d5711facb0bbe09353c4614ddfcb3edb5b
Kevin Haslett <khaslett@chromium.org>
2023-07-11 18:44:17 +0000

    [2/3] Send affine transforms via Wayland
    Change 2/3 to implement 2d transform delegation.
    
    Implement the lacros side of the augmented_sub_surface.set_transform()
    protocol.
    
    Based on River's CL: https://chromium-review.googlesource.com/c/chromium/src/+/4076413/7
    
    Bug: 1345254
    Change-Id: Ic89698222042241e595d3f119def88751bc95298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4558886
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168851}

--- ui/ozone/platform/wayland/host/surface_augmenter.h
+++ ui/ozone/platform/wayland/host/surface_augmenter.h
@@ -37,6 +37,7 @@ class SurfaceAugmenter : public wl::GlobalObjectRegistrar<SurfaceAugmenter> {
 
   bool SupportsSubpixelAccuratePosition() const;
   bool SupportsClipRect() const;
+  bool SupportsTransform() const;
 
   uint32_t GetSurfaceAugmentorVersion() const;
 
