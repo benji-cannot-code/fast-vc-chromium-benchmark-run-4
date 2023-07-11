277aa5f5ef1cc8f2164839d635cdc3c722353cfd
Ali Hijazi <ahijazi@chromium.org>
2023-07-11 18:33:09 +0000

    Rewrite T& field into const raw_ref<T> in mock_indexed_db_callbacks
    These changes were identified while enabling the raw_ref enforcement
    plugin.
    
    Bug: 1446146
    Change-Id: Iea5b54e490eeb06a85b0d0156d05dfbad6887d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678025
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168832}

--- content/browser/indexed_db/mock_indexed_db_callbacks.h
+++ content/browser/indexed_db/mock_indexed_db_callbacks.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback.h"
+#include "base/memory/raw_ref.h"
 #include "content/browser/indexed_db/indexed_db_callbacks.h"
 #include "content/browser/indexed_db/indexed_db_connection.h"
 #include "third_party/blink/public/common/indexeddb/indexeddb_key.h"
@@ -91,7 +92,7 @@ class ThunkCallbacks : public MockIndexedDBCallbacks {
   void OnSuccess(int64_t value) override;
 
  private:
-  IndexedDBCallbacks& wrapped_;
+  const raw_ref<IndexedDBCallbacks> wrapped_;
 };
 
 }  // namespace content
