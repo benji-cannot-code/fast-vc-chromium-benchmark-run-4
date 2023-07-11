6c6a9456b9c4717a2398e6827ed2b9042776534d
Camillia Smith Barnes <cammie@chromium.org>
2023-07-11 23:20:37 +0000

    Shared Storage: Hook SharedStorageRequestHelper up to URLLoader
    Here we create the `SharedStorageRequestHelper` in the
    `URLLoaderFactory` for shared storage eligible requests, and pass the
    helper into the `URLLoader`, where it will process shared storage
    related requests and responses.
    
    Integration tests will be added in the follow-up CL
    https://crrev.com/c/4519162.
    
    This is part of a project to add support to writing to shared storage
    from response headers. See
    https://github.com/WICG/shared-storage/pull/70 for more information
    and https://crrev.com/c/4444068 for a working prototype for `fetch()`.
    
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Bug: 1434529,1218540
    Change-Id: I853143446a9dd75ee54d912f7020755b86132d21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519145
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169024}

--- services/network/cors/cors_url_loader.cc
+++ services/network/cors/cors_url_loader.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/shared_dictionary/shared_dictionary_manager.h"
 #include "services/network/shared_dictionary/shared_dictionary_storage.h"
 #include "services/network/shared_dictionary/shared_dictionary_writer.h"
+#include "services/network/shared_storage/shared_storage_header_utils.h"
 #include "services/network/trust_tokens/trust_token_operation_metrics_recorder.h"
 #include "services/network/url_loader.h"
 #include "services/network/url_loader_factory.h"
@@ -416,6 +417,10 @@ void CorsURLLoader::FollowRedirect(
     request_.cors_exempt_headers.RemoveHeader(name);
   }
   request_.headers.MergeFrom(modified_headers);
+
+  if (base::Contains(removed_headers, kSharedStorageWritableHeader)) {
+    request_.shared_storage_writable = false;
+  }
 
   if (!allow_any_cors_exempt_header_ &&
       !CorsURLLoaderFactory::IsValidCorsExemptHeaders(
