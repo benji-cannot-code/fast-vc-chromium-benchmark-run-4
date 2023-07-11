68a757fe5480de18bd2a7da4dad211c18d5c97fc
Nicholas Hollingum <hollingum@google.com>
2023-07-11 01:10:51 +0000

    guest_os: Correctly identify vm_types in session_tracker
    This involves converting the vm_type from concierge's to apps'.
    
    Bug: None
    Change-Id: I465568cd5552e8949d5aa4233c66edc4c448ae4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672511
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168460}

--- chrome/browser/ash/guest_os/public/types.h
+++ chrome/browser/ash/guest_os/public/types.h
@@ -7,9 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_GUEST_OS_PUBLIC_TYPES_H_
 
 #include "chromeos/ash/components/dbus/vm_applications/apps.pb.h"
+
+namespace vm_tools::concierge {
+enum VmInfo_VmType : int;
+}
+
 namespace guest_os {
 
 using VmType = vm_tools::apps::VmType;
-}
+
+// Converts the concierge-specific type to the one we use in chrome (apps::).
+VmType ToVmType(vm_tools::concierge::VmInfo_VmType type);
+
+}  // namespace guest_os
 
 #endif  // CHROME_BROWSER_ASH_GUEST_OS_PUBLIC_TYPES_H_
