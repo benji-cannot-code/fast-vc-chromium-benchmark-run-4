287aac9e8631408cc33088296d19ec99e3abe4fb
Kevin Ellis <kevers@google.com>
2023-07-11 11:55:02 +0000

    Support SVG graphics or inline element as a view timeline source.
    Bug: 1453523
    Change-Id: I680386a77aacbc6136c383e24522b8902b456ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626941
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168648}

--- third_party/blink/renderer/core/animation/view_timeline.cc
+++ third_party/blink/renderer/core/animation/view_timeline.cc
@@ -24,8 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
+#include "third_party/blink/renderer/core/layout/layout_inline.h"
+#include "third_party/blink/renderer/core/layout/svg/svg_layout_support.h"
 #include "third_party/blink/renderer/core/page/scrolling/sticky_position_scrolling_constraints.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
+#include "third_party/blink/renderer/core/svg/svg_element.h"
+#include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/platform/geometry/calculation_value.h"
 
 namespace blink {
@@ -285,7 +289,7 @@ void ViewTimeline::CalculateOffsets(PaintLayerScrollableArea* scrollable_area,
   DCHECK(ComputeIsResolved(state->resolved_source));
   DCHECK(subject());
 
-  absl::optional<PhysicalSize> subject_size = SubjectSize();
+  absl::optional<gfx::Size> subject_size = SubjectSize();
   absl::optional<gfx::PointF> subject_position =
       SubjectPosition(state->resolved_source);
   DCHECK(subject_position);
@@ -299,10 +303,10 @@ void ViewTimeline::CalculateOffsets(PaintLayerScrollableArea* scrollable_area,
   double target_size;
   LayoutUnit viewport_size;
   if (physical_orientation == kHorizontalScroll) {
-    target_size = subject_size->width.ToDouble();
+    target_size = subject_size->width();
     viewport_size = scrollable_area->LayoutContentRect().Width();
   } else {
-    target_size = subject_size->height.ToDouble();
+    target_size = subject_size->height();
     viewport_size = scrollable_area->LayoutContentRect().Height();
   }
 
@@ -466,16 +470,35 @@ void ViewTimeline::ApplyStickyAdjustments(ScrollOffsets& scroll_offsets,
   }
 }
 
-absl::optional<PhysicalSize> ViewTimeline::SubjectSize() const {
+absl::optional<gfx::Size> ViewTimeline::SubjectSize() const {
   if (!subject()) {
     return absl::nullopt;
   }
-  LayoutBox* subject_layout_box = subject()->GetLayoutBox();
-  if (!subject_layout_box) {
+  LayoutObject* subject_layout_object = subject()->GetLayoutObject();
+  if (!subject_layout_object) {
     return absl::nullopt;
   }
 
-  return subject_layout_box->Size();
+  if (subject_layout_object->IsBox()) {
+    return To<LayoutBox>(subject_layout_object)
+        ->BorderBoxRect()
+        .PixelSnappedSize();
+  }
+
+  if (subject_layout_object->IsLayoutInline()) {
+    return PhysicalRect::EnclosingRect(
+               To<LayoutInline>(subject_layout_object)->LocalBoundingBoxRectF())
+        .PixelSnappedSize();
+  }
+
+  if (subject_layout_object->IsSVGChild()) {
+    gfx::RectF bounds =
+        SVGLayoutSupport::LocalVisualRect(*subject_layout_object);
+    return gfx::Size(std::round(bounds.width()), std::round(bounds.height()));
+  }
+
+  NOTREACHED();
+  return absl::nullopt;
 }
 
 absl::optional<gfx::PointF> ViewTimeline::SubjectPosition(
@@ -483,15 +506,15 @@ absl::optional<gfx::PointF> ViewTimeline::SubjectPosition(
   if (!subject() || !resolved_source) {
     return absl::nullopt;
   }
-  LayoutBox* subject_layout_box = subject()->GetLayoutBox();
+  LayoutObject* subject_layout_object = subject()->GetLayoutObject();
   LayoutBox* source_layout_box = resolved_source->GetLayoutBox();
-  if (!subject_layout_box || !source_layout_box) {
+  if (!subject_layout_object || !source_layout_box) {
     return absl::nullopt;
   }
   MapCoordinatesFlags flags =
       kIgnoreScrollOffset | kIgnoreStickyOffset | kIgnoreTransforms;
   gfx::PointF subject_pos =
-      gfx::PointF(subject_layout_box->LocalToAncestorPoint(
+      gfx::PointF(subject_layout_object->LocalToAncestorPoint(
           PhysicalOffset(), source_layout_box, flags));
 
   // We call LayoutObject::ClientLeft/Top directly and avoid
