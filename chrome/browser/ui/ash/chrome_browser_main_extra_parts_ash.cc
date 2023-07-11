5d26e156fedc19c932429c94387f8e554c2b45c0
Thomas Lukaszewicz <tluk@chromium.org>
2023-07-11 20:35:49 +0000

    [lacros] Fix the lacros-enabled crash key
    Currently the lacros-enabled crash key is being set in
    ChromeBrowserMainExtraPartsAsh::PostProfileInit() to try to ensure
    the primary user was set when IsLacrosEnabled() was queried.
    
    However on ash this is run as soon as ash starts for the default
    profile and before the user signs in (and thus before the primary
    user is set). Responsibility for setting the key has instead been
    moved to BrowserManager and is done after the session begins and
    the primary user has been set.
    
    Bug: 1454974, 1408962
    Change-Id: I4cd16830312061e95d8fef2204457f8bac029308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651380
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168934}

--- chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
+++ chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
@@ -78,7 +78,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/bluetooth_config/in_process_instance.h"
 #include "chromeos/components/quick_answers/public/cpp/controller/quick_answers_controller.h"
 #include "chromeos/components/quick_answers/quick_answers_client.h"
-#include "components/crash/core/common/crash_key.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/session_manager/core/session_manager_observer.h"
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
@@ -302,12 +301,6 @@ void ChromeBrowserMainExtraPartsAsh::PostProfileInit(Profile* profile,
   if (ash::features::IsMicMuteNotificationsEnabled()) {
     app_access_notifier_ = std::make_unique<AppAccessNotifier>();
   }
-
-  // Check if Lacros is enabled for crash reporting here to give the user
-  // manager a chance to be initialized first.
-  constexpr char kLacrosEnabledDataKey[] = "lacros-enabled";
-  static crash_reporter::CrashKeyString<4> key(kLacrosEnabledDataKey);
-  key.Set(crosapi::browser_util::IsLacrosEnabled() ? "yes" : "no");
 
   // Instantiate DisplaySettingsHandler after CrosSettings has been
   // initialized.
