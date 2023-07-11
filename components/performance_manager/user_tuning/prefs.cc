fde5956eebe0b453858e1812cb30d788c7447dc0
Daniel Cheng <dcheng@chromium.org>
2023-07-11 01:20:07 +0000

    Migrate performance manager pref string constants to inline constexpr.
    This is a set of changes to migrate various pref string constants to be
    defined using inline constexpr. The end goal here is to unblock
    migration of the syncable pref allowlist to a compile time FixedFlatMap
    rather than building the allowlist dynamically at run time.
    
    Bug: 1459785
    Change-Id: Icabeb6b6761a386fb399b1a4ebbe9e2eb7e1e4d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673454
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168465}

--- components/performance_manager/user_tuning/prefs.cc
+++ components/performance_manager/user_tuning/prefs.cc
@@ -12,29 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace performance_manager::user_tuning::prefs {
 
-const char kHighEfficiencyModeEnabled[] =
-    "performance_tuning.high_efficiency_mode.enabled";
-
-const char kHighEfficiencyModeState[] =
-    "performance_tuning.high_efficiency_mode.state";
-
-const char kHighEfficiencyModeTimeBeforeDiscardInMinutes[] =
-    "performance_tuning.high_efficiency_mode.time_before_discard_in_minutes";
-
-const int kDefaultHighEfficiencyModeTimeBeforeDiscardInMinutes = 120;
-
-const char kBatterySaverModeState[] =
-    "performance_tuning.battery_saver_mode.state";
-
-const char kLastBatteryUseTimestamp[] =
-    "performance_tuning.last_battery_use.timestamp";
-
-const char kTabDiscardingExceptions[] =
-    "performance_tuning.tab_discarding.exceptions";
-
-const char kManagedTabDiscardingExceptions[] =
-    "performance_tuning.tab_discarding.exceptions_managed";
-
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kHighEfficiencyModeEnabled, false);
   registry->RegisterIntegerPref(
