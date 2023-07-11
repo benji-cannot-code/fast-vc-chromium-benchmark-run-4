0de45593fd24fefbc2a39636d8ca2a87f84413fd
Mikel Astiz <mastiz@chromium.org>
2023-07-11 15:49:10 +0000

    [autofill] Consider wallet import disabled while signed out
    Outside tests, it shouldn't make a difference, because wallet import
    is only relevant while the user is signed in.
    
    The goal is to make SyncUserSettings::IsPaymentsIntegrationEnabled()
    behave more similarly to how SyncUserSettings::GetSelectedTypes()
    behaves for regular sync datatypes (UserSelectableType).
    
    Disable-Rts: True
    Change-Id: Ifb0afb2707cbdb54808cd1f818d0ea53bca80996
    Bug: 1459963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672970
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168742}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -1659,6 +1659,10 @@ bool PersonalDataManager::IsAutofillIBANEnabled() const {
 }
 
 bool PersonalDataManager::IsAutofillWalletImportEnabled() const {
+  if (is_syncing_for_test_) {
+    return true;
+  }
+
   if (!sync_service_) {
     // Without `sync_service_`, namely in off-the-record profiles, wallet import
     // is effectively disabled.
