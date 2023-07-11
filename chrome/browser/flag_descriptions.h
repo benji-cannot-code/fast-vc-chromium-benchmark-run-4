4ee75e2f5f746281ad8d31a696c2e0a568664852
Marlon Facey <mfacey@chromium.org>
2023-07-11 22:21:35 +0000

    [Quests 2.0] ] Add flag to toggle between images
    Add flag to toggle between text only arm for Quests 2.0.
    
    Bug: 1462492
    Change-Id: Ia3116bee2e7b0f6049e94e676a963c69bcf83dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4676043
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1168984}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2688,6 +2688,9 @@ extern const char kNtpHistoryClustersModuleSuggestionChipHeaderDescription[];
 extern const char kNtpHistoryClustersModuleUseModelRankingName[];
 extern const char kNtpHistoryClustersModuleUseModelRankingDescription[];
 
+extern const char kNtpHistoryClustersModuleTextOnlyName[];
+extern const char kNtpHistoryClustersModuleTextOnlyDescription[];
+
 extern const char kNtpChromeCartInHistoryClustersModuleName[];
 extern const char kNtpChromeCartInHistoryClustersModuleDescription[];
 
