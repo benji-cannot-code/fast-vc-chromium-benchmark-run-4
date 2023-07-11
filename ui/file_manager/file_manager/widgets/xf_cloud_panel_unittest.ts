f1403d14ddb0ec64b456e11ac8cec9ecb30dd8bf
Titouan Rigoudy <titouan@chromium.org>
2023-07-11 15:40:34 +0000

    Revert "file_manager: Simplify test code"
    This reverts commit 575e3af55edfca426e7cc33a93586242aade977d.
    
    Reason for revert: might have broken a file manager test.
    
    Bug: chromium:1463936
    
    Original change's description:
    > file_manager: Simplify test code
    >
    > BUG=b:290556321
    >
    > Change-Id: I467b82cd98e1b0ec050b4541d7d139d09e95b402
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671893
    > Reviewed-by: Ben Reich <benreich@chromium.org>
    > Commit-Queue: Ben Reich <benreich@chromium.org>
    > Auto-Submit: François Degros <fdegros@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1168550}
    
    BUG=b:290556321
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    
    Change-Id: Ie17258ec7916d6f5506ed21d0a10eb609331b467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4674349
    Owners-Override: Titouan Rigoudy <titouan@google.com>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Iman Saboori <isaboori@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Titouan Rigoudy <titouan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168732}

--- ui/file_manager/file_manager/widgets/xf_cloud_panel_unittest.ts
+++ ui/file_manager/file_manager/widgets/xf_cloud_panel_unittest.ts
@@ -20,10 +20,10 @@ export function setUp() {
  * Returns the <xf-cloud-panel> element.
  */
 function getCloudPanelElement(): XfCloudPanel {
-  const element = document.querySelector<XfCloudPanel>('xf-cloud-panel');
-  assertNotEquals(null, element);
+  const element = document.querySelector('xf-cloud-panel');
+  assertNotEquals(null, element, 'xf-cloud-panel is null');
   assertEquals('XF-CLOUD-PANEL', element!.tagName);
-  return element!;
+  return element! as XfCloudPanel;
 }
 
 /**
@@ -116,7 +116,7 @@ async function getElement<T extends HTMLElement>(
     element = root.querySelector(selector);
     return element !== null;
   });
-  assertNotEquals(null, element);
+  assertNotEquals(element, null);
   return element!;
 }
 
@@ -131,7 +131,7 @@ async function getProgressBar(): Promise<HTMLProgressElement> {
         element.shadowRoot!.querySelector<HTMLProgressElement>('progress');
     return progressElement !== null;
   });
-  return progressElement!;
+  return progressElement! as HTMLProgressElement;
 }
 
 /**
