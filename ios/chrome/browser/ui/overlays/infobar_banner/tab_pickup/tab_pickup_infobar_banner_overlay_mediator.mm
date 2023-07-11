16287cfb9850ceb3ff82b758779f2ecdf16af070
Ewann Pelle <ewannpv@chromium.org>
2023-07-11 10:01:55 +0000

    [iOS][TabPickup] Update displayed time
    This CL updates the lastSyncTimeStringFromTime: method.
    Now if the tab was synchronized within the last minute, it returns
    "1 min ago".
    
    Bug: 1457175
    Change-Id: Id529111d4c3f87374819b7656266d2c3228c5922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672974
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168612}

--- ios/chrome/browser/ui/overlays/infobar_banner/tab_pickup/tab_pickup_infobar_banner_overlay_mediator.mm
+++ ios/chrome/browser/ui/overlays/infobar_banner/tab_pickup/tab_pickup_infobar_banner_overlay_mediator.mm
@@ -111,11 +111,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSString*)lastSyncTimeStringFromTime:(base::Time)time {
   NSString* timeString;
   base::TimeDelta lastUsedDelta = base::Time::Now() - time;
+  base::TimeDelta oneMinuteDelta = base::Minutes(1);
 
-  if (lastUsedDelta.InMicroseconds() < base::Time::kMicrosecondsPerMinute) {
-    timeString = l10n_util::GetNSString(IDS_IOS_OPEN_TABS_RECENTLY_SYNCED);
-    // This will return something similar to "Seconds ago".
-    return [NSString stringWithFormat:@"%@", timeString];
+  // If the tab was synchronized within the last minute, set the time delta to 1
+  // minute.
+  if (lastUsedDelta < oneMinuteDelta) {
+    lastUsedDelta = oneMinuteDelta;
   }
 
   NSDate* date = [NSDate dateWithTimeIntervalSince1970:time.ToTimeT()];
