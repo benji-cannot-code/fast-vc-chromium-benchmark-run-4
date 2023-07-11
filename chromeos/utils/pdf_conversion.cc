6803dd3313036e050adb078d2dfaa89dd8b50908
Kevin Lubick <kjlubick@google.com>
2023-07-11 19:51:22 +0000

    Change scan_service unit tests to use JPEG instead of PNG
    A while ago, unit tests [1] were added which pretend to scan images in
    as PNGs. However, SaveAsPdf [2] appears to expect only JPEG images.
    This has worked because under the hood, scan service used Skia's
    codecs, which happened to include JPEG, PNG, and a bevy of other codecs.
    
    In [3] I am trying to remove that implicit dependency, but ran into
    failing tests because SaveAsPdf no longer "magically" accepted PNGs.
    
    As per discussion, the unit tests should be changed to reflect
    reality: the scan backend will be returning JPEGs if the target
    output is a PDF.
    
    [1] crrev.com/891515
    [2] https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:chrome/browser/ash/scanning/scan_service.cc;l=92;drc=b73134cfcce34a13f25202d077c0aa9dc03b662e
    [3] https://crrev.com/c/4647769
    
    Change-Id: Ib3921e53303f9132dbb46f99744d7a3e2e939d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673672
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168899}

--- chromeos/utils/pdf_conversion.cc
+++ chromeos/utils/pdf_conversion.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "printing/units.h"
+#include "third_party/skia/include/codec/SkJpegDecoder.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkImage.h"
@@ -29,14 +30,17 @@ constexpr int kRotationDegrees = 180;
 // `rotate` indicates whether the page should be rotated 180 degrees.
 // Returns whether the page was successfully created.
 bool AddPdfPage(sk_sp<SkDocument> pdf_doc,
-                const sk_sp<SkData>& image_data,
+                const sk_sp<SkData>& jpeg_image_data,
                 bool rotate,
                 absl::optional<int> dpi) {
-  const sk_sp<SkImage> image = SkImages::DeferredFromEncodedData(image_data);
+  const sk_sp<SkImage> image =
+      SkImages::DeferredFromEncodedData(jpeg_image_data);
   if (!image) {
     LOG(ERROR) << "Unable to generate image from encoded image data.";
     return false;
   }
+  CHECK(
+      SkJpegDecoder::IsJpeg(jpeg_image_data->data(), jpeg_image_data->size()));
 
   // Convert from JPG dimensions in pixels (DPI) to PDF dimensions in points
   // (1/72 in).
