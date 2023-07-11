803a16d5c1a63b30f245e1ee7a86a31b7a1cf469
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-11 08:18:09 +0000

    [iOS] remove trivial method
    R=jlebel
    
    Bug: None
    Change-Id: Ib242985ac2cf5072cdb7787303ae80d3a14c7166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675823
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168575}

--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.h
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.h
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This coordinator presents settings related to the Password Manager.
 @interface PasswordSettingsCoordinator : ChromeCoordinator
 
-- (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                                   browser:(Browser*)browser
-    NS_DESIGNATED_INITIALIZER;
-
 // Delegate.
 @property(nonatomic, weak) id<PasswordSettingsCoordinatorDelegate> delegate;
 
--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
@@ -128,11 +128,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation PasswordSettingsCoordinator
 
-- (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                                   browser:(Browser*)browser {
-  return [super initWithBaseViewController:viewController browser:browser];
-}
-
 - (void)dealloc {
   // TODO(crbug.com/1454777)
   DUMP_WILL_BE_CHECK(!_mediator);
