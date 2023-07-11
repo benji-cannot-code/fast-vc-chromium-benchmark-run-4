d02645b24ecd567f4a9bf8d75d9e52b35a35eec6
Jack Yammine <jyammine@google.com>
2023-07-11 09:20:20 +0000

    [waffle]Add a link that, when clicked, opens a dialog on top of waffle
    Screen recording: https://drive.google.com/file/d/1oILGDfWPY9G5aXsMUuMveStjHL83wmQu/view?usp=sharing
    
    Bug: b/280753754
    Change-Id: I0a31df9c266e006f29172be0abc48116aa22a729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4669623
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jens Mueller <muellerj@google.com>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168597}

--- chrome/browser/resources/waffle/app.ts
+++ chrome/browser/resources/waffle/app.ts
@@ -6,16 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_components/localized_link/localized_link.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
 import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
 import './strings.m.js';
 
+import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {afterNextRender, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './app.html.js';
 import {WaffleChoice} from './browser_proxy';
 import {WaffleBrowserProxy} from './browser_proxy.js';
+
+export interface WaffleAppElement {
+  $: {
+    infoDialog: CrDialogElement,
+  };
+}
 
 export class WaffleAppElement extends PolymerElement {
   static get is() {
@@ -54,6 +62,10 @@ export class WaffleAppElement extends PolymerElement {
       WaffleBrowserProxy.getInstance().handler.displayDialog(
           document.body.offsetHeight);
     });
+  }
+
+  private onLinkClicked_() {
+    this.$.infoDialog.showModal();
   }
 }
 
