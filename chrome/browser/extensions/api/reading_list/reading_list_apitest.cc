9e6a98eeed435b7165eed0afa855a5877f1bc9d9
Emiliano <emilianohdz@google.com>
2023-07-11 17:49:25 +0000

    [Extension] Introduce chrome.readingList extension API
    This CL creates the schema and adds a new directory with the implementation of the chrome.readingList.addEntry function to insert a new readingListEntry in the readingList.
    
    Bug: 1421058
    Change-Id: I330bd83645ad391619fc55931f8e05c9a6d924bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638666
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emiliano Hurtado <emilianohdz@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168816}

---
+++ chrome/browser/extensions/api/reading_list/reading_list_apitest.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "content/public/test/browser_test.h"
+
+namespace extensions {
+
+namespace {
+
+using ReadingListApiTest = ExtensionApiTest;
+
+IN_PROC_BROWSER_TEST_F(ReadingListApiTest, TestReadingListWorks) {
+  ASSERT_TRUE(RunExtensionTest("reading_list")) << message_;
+}
+
+}  // namespace
+
+}  // namespace extensions
---
+++ chrome/test/data/extensions/api_test/reading_list/background.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+
+  async function testAddEntryFunction() {
+    const entry = {
+      url: 'https://www.example.com',
+      title: 'example of title',
+      hasBeenRead: false
+    };
+    await chrome.readingList.addEntry(entry);
+    await chrome.test.assertPromiseRejects(
+        chrome.readingList.addEntry(entry), 'Error: Duplicate URL');
+    chrome.test.succeed();
+  },
+
+  async function testInvalidURL() {
+    const entry = {
+      url: 'Invalid URL',
+      title: 'example of title',
+      hasBeenRead: false
+    };
+    await chrome.test.assertPromiseRejects(
+        chrome.readingList.addEntry(entry), 'Error: URL is not valid');
+    chrome.test.succeed();
+  },
+
+  async function TestNotSupportedURL() {
+    const entry = {
+      url: 'chrome://example',
+      title: 'example of title',
+      hasBeenRead: false
+    };
+    await chrome.test.assertPromiseRejects(
+        chrome.readingList.addEntry(entry), 'Error: URL is not supported');
+    chrome.test.succeed();
+  },
+
+]);
