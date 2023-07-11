6afe2776cc21ff7538d85e0551e7bb1b13ea6769
Slobodan Pejic <slobodan@chromium.org>
2023-07-11 23:02:18 +0000

    Add an empty save card bottom sheet behind a flag.
    Part of migrating the Save Card infobar to a bottom sheet.
    * Add a flag, payments-autofill-android-bottom-sheet.
    * Trigger the placeholder bottom sheet instead of the save card infobar.
    
    BUG=1454271
    
    Change-Id: I5a74fd9e1e95ce43ae7c7bed97b510d40daf1d1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4601838
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Olivia Saul <jsaul@google.com>
    Auto-Submit: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169012}

--- chrome/browser/ui/autofill/chrome_autofill_client_unittest.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client_unittest.cc
@@ -27,8 +27,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/android/autofill/autofill_save_card_bottom_sheet_bridge.h"
+#endif
+
 namespace autofill {
 namespace {
+
+using ::testing::InSequence;
+
+#if BUILDFLAG(IS_ANDROID)
+class MockAutofillSaveCardBottomSheetBridge
+    : public AutofillSaveCardBottomSheetBridge {
+ public:
+  MockAutofillSaveCardBottomSheetBridge()
+      : AutofillSaveCardBottomSheetBridge(
+            base::android::ScopedJavaGlobalRef<jobject>(nullptr)) {}
+
+  MOCK_METHOD(bool, RequestShowContent, (), (override));
+};
+#endif
 
 // Exposes the protected constructor.
 class TestChromeAutofillClient : public ChromeAutofillClient {
@@ -41,7 +59,18 @@ class TestChromeAutofillClient : public ChromeAutofillClient {
     return &fast_checkout_client_;
   }
 
+  // Inject a new MockAutofillSaveCardBottomSheetBridge.
+  // Returns a pointer to the mock.
+  MockAutofillSaveCardBottomSheetBridge*
+  InjectMockAutofillSaveCardBottomSheetBridge() {
+    auto mock = std::make_unique<MockAutofillSaveCardBottomSheetBridge>();
+    auto* pointer = mock.get();
+    SetAutofillSaveCardBottomSheetBridgeForTesting(std::move(mock));
+    return pointer;
+  }
+
   MockFastCheckoutClient fast_checkout_client_;
+  base::test::ScopedFeatureList scoped_feature_list_;
 #endif
 };
 
@@ -61,7 +90,7 @@ class ChromeAutofillClientTest : public ChromeRenderViewHostTestHarness {
   }
 
  protected:
-  ChromeAutofillClient* client() {
+  TestChromeAutofillClient* client() {
     return test_autofill_client_injector_[web_contents()];
   }
 
@@ -103,6 +132,8 @@ class ChromeAutofillClientTest : public ChromeRenderViewHostTestHarness {
       test_autofill_driver_injector_;
   TestAutofillManagerInjector<TestBrowserAutofillManager>
       test_autofill_manager_injector_;
+
+  base::OnceCallback<void()> setup_flags_;
 };
 
 TEST_F(ChromeAutofillClientTest, GetFormInteractionsFlowId_BelowMaxFlowTime) {
@@ -158,6 +189,29 @@ TEST_F(ChromeAutofillClientTest, GetFormInteractionsFlowId_AdvancedTwice) {
   EXPECT_NE(first_interaction_flow_id,
             client()->GetCurrentFormInteractionsFlowId());
 }
+
+#if BUILDFLAG(IS_ANDROID)
+class ChromeAutofillClientTestWithPaymentsAndroidBottomSheetFeature
+    : public ChromeAutofillClientTest {
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kAutofillEnablePaymentsAndroidBottomSheet};
+};
+
+TEST_F(ChromeAutofillClientTestWithPaymentsAndroidBottomSheetFeature,
+       ConfirmSaveCreditCardToCloud_RequestsBottomSheet) {
+  TestChromeAutofillClient* autofill_client = client();
+  auto* bottom_sheet_bridge =
+      autofill_client->InjectMockAutofillSaveCardBottomSheetBridge();
+
+  EXPECT_CALL(*bottom_sheet_bridge, RequestShowContent());
+
+  autofill_client->ConfirmSaveCreditCardToCloud(
+      CreditCard(), LegalMessageLines(),
+      ChromeAutofillClient::SaveCreditCardOptions().with_show_prompt(true),
+      base::DoNothing());
+}
+#endif
 
 }  // namespace
 }  // namespace autofill
