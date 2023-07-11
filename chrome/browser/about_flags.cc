050e72da29947bd4998ee2b703a542bc5e986406
Stephen McGruer <smcgruer@chromium.org>
2023-07-11 13:35:16 +0000

    [Autofill] Clean-up payment autofill flags file
    Removed flags that had no 'real' code associated with them:
      - kAutofillAutoTriggerManualFallbackForCards
    
    Marked flags as launched (per go/finch101#post-launch-clean-up) where
    they are enabled-by-default and some have finch configs showing that
    they were launched. These flags should be removed and the related
    code cleaned up:
      - kAutofillEnforceDelaysInStrikeDatabase
      - kAutofillFillMerchantPromoCodeFields
      - kAutofillRemoveCardExpirationAndTypeTitles
      - kAutofillSaveCardInfobarEditSupport
      - kAutofillUseEloRegexForBinMatching
    
    Also identified the following flags which appear to be unused
    (disabled by default, no finch config turning them on), but not
    touched in this CL:
      - kAutofillAlwaysReturnCloudTokenizedCard
      - kAutofillShowUnmaskedCachedCardInManualFillingView
    
    
    Bug: 1007974
    Change-Id: I4fc9c08d152fc9035b94311505d34d0a9a4423bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670113
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168668}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8848,13 +8848,6 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableStickyManualFallbackForCards)},
 
-    {"autofill-auto-trigger-manual-fallback-for-cards",
-     flag_descriptions::kAutofillAutoTriggerManualFallbackForCardsName,
-     flag_descriptions::kAutofillAutoTriggerManualFallbackForCardsDescription,
-     kOsAndroid,
-     FEATURE_VALUE_TYPE(
-         autofill::features::kAutofillAutoTriggerManualFallbackForCards)},
-
     {"autofill-prevent-overriding-prefilled-values",
      flag_descriptions::kAutofillPreventOverridingPrefilledValuesName,
      flag_descriptions::kAutofillPreventOverridingPrefilledValuesDescription,
@@ -9123,12 +9116,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableDrDcDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(features::kEnableDrDcVulkan)},
 #endif  //   BUILDFLAG(IS_ANDROID)
-
-    {"autofill-fill-merchant-promo-code-fields",
-     flag_descriptions::kAutofillFillMerchantPromoCodeFieldsName,
-     flag_descriptions::kAutofillFillMerchantPromoCodeFieldsDescription, kOsAll,
-     FEATURE_VALUE_TYPE(
-         autofill::features::kAutofillFillMerchantPromoCodeFields)},
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     {"traffic-counters", flag_descriptions::kTrafficCountersEnabledName,
@@ -9523,13 +9510,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kSystemSoundsDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kSystemSounds)},
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
-    {"autofill-enforce-delays-in-strike-database",
-     flag_descriptions::kAutofillEnforceDelaysInStrikeDatabaseName,
-     flag_descriptions::kAutofillEnforceDelaysInStrikeDatabaseDescription,
-     kOsAll,
-     FEATURE_VALUE_TYPE(
-         autofill::features::kAutofillEnforceDelaysInStrikeDatabase)},
 
     {"autofill-enable-virtual-card-metadata",
      flag_descriptions::kAutofillEnableVirtualCardMetadataName,
@@ -10122,13 +10102,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kServiceWorkerStaticRouterName,
      flag_descriptions::kServiceWorkerStaticRouterDescription, kOsAll,
      FEATURE_VALUE_TYPE(features::kServiceWorkerStaticRouter)},
-
-    {"autofill-remove-card-expiration-and-type-titles",
-     flag_descriptions::kAutofillRemoveCardExpirationAndTypeTitlesName,
-     flag_descriptions::kAutofillRemoveCardExpirationAndTypeTitlesDescription,
-     kOsDesktop,
-     FEATURE_VALUE_TYPE(
-         autofill::features::kAutofillRemoveCardExpirationAndTypeTitles)},
 
     {"autofill-offer-to-save-card-with-same-last-four",
      flag_descriptions::kAutofillOfferToSaveCardWithSameLastFourName,
