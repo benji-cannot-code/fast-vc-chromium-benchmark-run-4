bbfd7b6b29755eeff3d8783424bad19298663e9d
John Palmer <jopalmer@chromium.org>
2023-07-11 06:05:12 +0000

    Create webui for Mako
    For now this webui does nothing, just implementing a skeleton to allow for hooking up mojo interfaces
    
    Has to live in /chrome rather than /ash even though it is a ChromeOS UI to allow for access to the bubble UI for webui
    
    Fixed: b:287139545
    Change-Id: I953eeb3c78386447fd2a37b4382e7a6a2dfd4c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660966
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168533}

--- chrome/browser/ui/webui/ash/chrome_untrusted_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/ash/chrome_untrusted_web_ui_configs_chromeos.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/system_web_apps/apps/projector_app/untrusted_projector_annotator_ui_config.h"
 #include "chrome/browser/ash/system_web_apps/apps/projector_app/untrusted_projector_ui_config.h"
 #include "chrome/browser/ash/system_web_apps/apps/terminal_ui.h"
+#include "chrome/browser/ui/webui/ash/mako/mako_ui.h"
 
 #if !defined(OFFICIAL_BUILD)
 #include "ash/webui/sample_system_web_app_ui/sample_system_web_app_untrusted_ui.h"
@@ -67,6 +68,7 @@ void RegisterAshChromeUntrustedWebUIConfigs() {
       std::make_unique<feedback::OsFeedbackUntrustedUIConfig>());
   map.AddUntrustedWebUIConfig(std::make_unique<FaceMLAppUntrustedUIConfig>());
   map.AddUntrustedWebUIConfig(MakeDemoModeAppUntrustedUIConfig());
+  map.AddUntrustedWebUIConfig(std::make_unique<MakoUntrustedUIConfig>());
 #if !defined(OFFICIAL_BUILD)
   map.AddUntrustedWebUIConfig(
       std::make_unique<SampleSystemWebAppUntrustedUIConfig>());
---
+++ chrome/browser/ui/webui/ash/mako/url_constants.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_MAKO_URL_CONSTANTS_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_MAKO_URL_CONSTANTS_H_
+
+namespace ash {
+
+extern const char kChromeUIMakoHost[];
+extern const char kChromeUIMakoURL[];
+
+}  // namespace ash
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_MAKO_URL_CONSTANTS_H_
