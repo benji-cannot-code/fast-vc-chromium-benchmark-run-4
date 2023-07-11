c1fa075c6dbbae9dfbbc24793eb6dd46489071d4
Joey Arhar <jarhar@chromium.org>
2023-07-11 00:05:31 +0000

    Move popover invoker CHECK
    I added this CHECK in http://crrev.com/1166594 but it will get hit when
    showPopover is called while the popover is open with an invoker. This
    patch moves the CHECK down past the IsPopoverReady check to avoid
    getting hit. If the popover is already open, then the IsPopoverReady
    check will return before the CHECK is run.
    
    The motivation behind the CHECK is still fine - it ensures that while
    the popover is closed, it can't have an invoker set.
    
    Fixed: 1463384
    Change-Id: Ie75a1026acefd9ce8018caf96b2bb314a18ac730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4676654
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168432}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1443,7 +1443,6 @@ void HTMLElement::ShowPopoverInternal(Element* invoker,
   CHECK(RuntimeEnabledFeatures::HTMLPopoverAttributeEnabled(
       GetDocument().GetExecutionContext()));
 
-  CHECK(!GetPopoverData() || !GetPopoverData()->invoker());
   if (!IsPopoverReady(PopoverTriggerAction::kShow, exception_state,
                       /*include_event_handler_text=*/false, /*document=*/nullptr)) {
     CHECK(exception_state)
@@ -1452,6 +1451,8 @@ void HTMLElement::ShowPopoverInternal(Element* invoker,
            "shown.";
     return;
   }
+
+  CHECK(!GetPopoverData() || !GetPopoverData()->invoker());
 
   // Fire events by default, unless we're recursively showing this popover.
   PopoverData::ScopedStartShowingOrHiding scoped_was_showing_or_hiding(*this);
