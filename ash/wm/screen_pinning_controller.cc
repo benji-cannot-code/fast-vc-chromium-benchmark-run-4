81799ea499438b271225436b0afabd2206bd1d62
Kazuki Takise <takise@google.com>
2023-07-11 01:13:25 +0000

    Clean up proper container in ScreenPinningController
    The parent container of a pinned window is set up for pinned mode
    in SetPinnedWindow() and cleaned up in ResetWindowPinningState().
    
    These functions need to target the same container but depending
    on the timing of ResetWindowPinningState() being called, the
    pinned window may have already moved to another container at the
    point ResetWindowPinningState() is called and could unexpectedly
    clean up the incorrect container.
    
    With this CL, the container is tracked in ScreenPinningController
    so we can ensure to clean up the proper container.
    
    Bug: b/287903215
    Test: WM CTS dosen't crash.
    Change-Id: I1aa76a399dad0b6713b045390603c0e2c5c5a236
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672171
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168462}

--- ash/wm/screen_pinning_controller.cc
+++ ash/wm/screen_pinning_controller.cc
@@ -190,7 +190,7 @@ void ScreenPinningController::SetPinnedWindow(aura::Window* pinned_window) {
       return;
     }
 
-    aura::Window* container = pinned_window->parent();
+    container_ = pinned_window->parent();
     aura::Window::Windows system_modal_containers =
         GetSystemModalWindowsExceptPinned(pinned_window);
 
@@ -199,8 +199,8 @@ void ScreenPinningController::SetPinnedWindow(aura::Window* pinned_window) {
     // To monitor destruction.
     pinned_window_->AddObserver(this);
     AlwaysOnTopController::SetDisallowReparent(pinned_window);
-    container->StackChildAtTop(pinned_window);
-    container->StackChildBelow(CreateWindowDimmer(container), pinned_window);
+    container_->StackChildAtTop(pinned_window);
+    container_->StackChildBelow(CreateWindowDimmer(container_), pinned_window);
 
     // Set the dim windows to the system containers, other than the one which
     // the root window of the pinned window holds.
@@ -208,8 +208,8 @@ void ScreenPinningController::SetPinnedWindow(aura::Window* pinned_window) {
       system_modal->StackChildAtBottom(CreateWindowDimmer(system_modal));
 
     // Set observers.
-    container->AddObserver(pinned_container_window_observer_.get());
-    AddObserverToChildren(container,
+    container_->AddObserver(pinned_container_window_observer_.get());
+    AddObserverToChildren(container_,
                           pinned_container_child_window_observer_.get());
     for (aura::Window* system_modal : system_modal_containers) {
       system_modal->AddObserver(system_modal_container_window_observer_.get());
@@ -275,7 +275,7 @@ aura::Window* ScreenPinningController::CreateWindowDimmer(
 }
 
 void ScreenPinningController::ResetWindowPinningState() {
-  aura::Window* container = pinned_window_->parent();
+  CHECK(container_);
   aura::Window::Windows system_modal_containers =
       GetSystemModalWindowsExceptPinned(pinned_window_);
 
@@ -286,13 +286,14 @@ void ScreenPinningController::ResetWindowPinningState() {
         system_modal, system_modal_container_child_window_observer_.get());
     system_modal->RemoveObserver(system_modal_container_window_observer_.get());
   }
-  RemoveObserverFromChildren(container,
+  RemoveObserverFromChildren(container_,
                              pinned_container_child_window_observer_.get());
-  container->RemoveObserver(pinned_container_window_observer_.get());
+  container_->RemoveObserver(pinned_container_window_observer_.get());
 
   window_dimmers_->clear();
   pinned_window_->RemoveObserver(this);
   pinned_window_ = nullptr;
+  container_ = nullptr;
 }
 
 void ScreenPinningController::OnDisplayConfigurationChanged() {
