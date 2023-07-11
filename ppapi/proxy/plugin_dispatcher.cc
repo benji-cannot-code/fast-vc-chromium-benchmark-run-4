300c0b7c35928f90279979ba0bfd8c69e7b42e1d
Gabriel Charette <gab@chromium.org>
2023-07-11 14:56:53 +0000

    [metrics] Address gab's long-expired histograms
    OBSOLETE_HISTOGRAMS=Settings.HashesDictionaryTrusted
    
    Fixed: 1460153,1460049,1460057,1460056
    Change-Id: Id9559f17a1d59819bd51f24ad58b755adae11f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660985
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168704}

--- ppapi/proxy/plugin_dispatcher.cc
+++ ppapi/proxy/plugin_dispatcher.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/compiler_specific.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/trace_event.h"
@@ -113,7 +112,6 @@ bool PluginDispatcher::Sender::Send(IPC::Message* msg) {
   if (msg->is_sync()) {
     // Synchronous messages might be re-entrant, so we need to drop the lock.
     ProxyAutoUnlock unlock;
-    SCOPED_UMA_HISTOGRAM_TIMER("Plugin.PpapiSyncIPCTime");
     return SendMessage(msg);
   }
   return SendMessage(msg);
