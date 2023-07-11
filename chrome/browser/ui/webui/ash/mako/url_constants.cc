a40864443d5b1084a2cc6d57a571f86b5b9c646e
John Palmer <jopalmer@chromium.org>
2023-07-11 07:38:50 +0000

    Fix up some minor things with the mako webui
    Bug: b:287139545
    Change-Id: I5f1ea462d6970c29961a1971839976ab714cc025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4677173
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168560}

--- chrome/browser/ui/webui/ash/mako/url_constants.cc
+++ chrome/browser/ui/webui/ash/mako/url_constants.cc
@@ -8,6 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 const char kChromeUIMakoHost[] = "mako";
-const char kChromeUIMakoURL[] = "chrome://mako";
+const char kChromeUIMakoURL[] = "chrome-untrusted://mako";
 
 }  // namespace ash
