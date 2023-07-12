4de31dc52dcfd88f2e5a8cf849926151683b2ab0
Jeffrey Young <cowmoo@google.com>
2023-07-12 00:51:28 +0000

    wallpaper: rm WallpaperControllerClient fn
    WallpaperControllerClient::SetDefaultWallpaper is unnecessary.
    Callers can use WallpaperController::SetDefaultWallpaper
    directly.
    
    Bug: b:290409765
    Test: ash_unittests --gtest_filter=*Wallpaper*
    Change-Id: Ie30b5bf90cd7cee36e81a6faa373b1246674cf3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675096
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169061}

--- chrome/browser/ash/arc/wallpaper/arc_wallpaper_service.cc
+++ chrome/browser/ash/arc/wallpaper/arc_wallpaper_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "ash/components/arc/session/arc_bridge_service.h"
+#include "ash/public/cpp/wallpaper/wallpaper_controller.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
@@ -153,7 +154,7 @@ void ArcWallpaperService::SetDefaultWallpaper() {
   decode_request_.reset();
   const user_manager::User* const primary_user =
       UserManager::Get()->GetPrimaryUser();
-  WallpaperControllerClientImpl::Get()->SetDefaultWallpaper(
+  ash::WallpaperController::Get()->SetDefaultWallpaper(
       primary_user->GetAccountId(),
       primary_user->is_active() /*show_wallpaper=*/, base::DoNothing());
 }
--- chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_wallpaper_provider_impl.cc
+++ chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_wallpaper_provider_impl.cc
@@ -540,8 +540,8 @@ void PersonalizationAppWallpaperProviderImpl::SelectDefaultImage(
   WallpaperControllerClientImpl* client = WallpaperControllerClientImpl::Get();
   DCHECK(client);
   client->RecordWallpaperSourceUMA(ash::WallpaperType::kDefault);
-  client->SetDefaultWallpaper(GetAccountId(profile_), /*show_wallpaper=*/true,
-                              std::move(callback));
+  WallpaperController::Get()->SetDefaultWallpaper(
+      GetAccountId(profile_), /*show_wallpaper=*/true, std::move(callback));
 }
 
 void PersonalizationAppWallpaperProviderImpl::SelectLocalImage(
