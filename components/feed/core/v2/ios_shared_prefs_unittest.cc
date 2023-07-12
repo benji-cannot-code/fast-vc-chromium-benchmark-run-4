79087adac748246561686e1d443ba8f017c80ea9
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-07-12 18:32:47 +0000

    [feed v2] Split tests for ios_shared to separate build target
    Split unit tests for //components/feed/core/v2:ios_shared to a new
    ios_shared_unit_tests target. Also move both targets down, as we
    prepare to wrap the rest of the BUILD file in `if (!is_ios)`.
    
    Change-Id: Id1069751b06833edf3555a7f5fff574f96f2021a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679680
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169468}

--- components/feed/core/v2/ios_shared_prefs_unittest.cc
+++ components/feed/core/v2/ios_shared_prefs_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "components/feed/core/common/pref_names.h"
-#include "components/feed/core/v2/types.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
