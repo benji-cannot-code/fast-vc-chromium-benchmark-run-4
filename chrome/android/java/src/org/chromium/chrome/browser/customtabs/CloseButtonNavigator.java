21f3e700732a3090668e0bb3380f1f6214ad0ec7
Peter Conn <peconn@chromium.org>
2023-07-12 15:38:37 +0000

    📋 Remove CustomTabs.CloseButton.ChildTab.ScopeAlgorithm.ClosesOneTab
    Bug: 1421899
    Change-Id: I39e46f1cee782e4aa9df3d2e55f7210e22878d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678753
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169357}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CloseButtonNavigator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CloseButtonNavigator.java
@@ -91,9 +91,6 @@ public class CloseButtonNavigator {
             // See if there's a close button navigation in our current Tab.
             NavigationController navigationController = getNavigationController();
             if (navigationController != null && navigateSingleTab(getNavigationController())) {
-                if (isFromChildTab) {
-                    recordChildTabScopeAlgorithmClosesOneTab(false);
-                }
                 return;
             }
 
@@ -106,9 +103,6 @@ public class CloseButtonNavigator {
             // we would not navigate at all.
             Tab nextTab = mTabProvider.getTab();
             if (nextTab != null && isLandingPage(nextTab.getUrl().getSpec())) {
-                if (isFromChildTab) {
-                    recordChildTabScopeAlgorithmClosesOneTab(numTabsClosed == 1);
-                }
                 return;
             }
         }
@@ -136,16 +130,6 @@ public class CloseButtonNavigator {
         }
 
         return false;
-    }
-
-    /**
-     * Records how often the "navigate to landing page" algorithm for child tabs for Custom Tabs and
-     * Trusted Web Activities produces the same behaviour as the webapp "close current tab"
-     * algorithm.
-     */
-    private void recordChildTabScopeAlgorithmClosesOneTab(boolean closesOneTab) {
-        RecordHistogram.recordBooleanHistogram(
-                "CustomTabs.CloseButton.ChildTab.ScopeAlgorithm.ClosesOneTab", closesOneTab);
     }
 
     private @Nullable NavigationController getNavigationController() {
