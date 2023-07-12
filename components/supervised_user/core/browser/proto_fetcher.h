d911795e6073c26cec4d2792fc6157b64c1a1815
Tomasz Jurkiewicz <tju@google.com>
2023-07-12 19:41:23 +0000

    Janitor the code before implementing next feature. No functional changes.
    * IWYU,
    * Use delegating constructor,
    * Add documentation,
    * Unify semantics of error handling (always return empty unique_ptr constructed from nullptr).
    * Store start time as property rather than binding it to the closure.
    
    
    Bug: b:290885798
    Change-Id: I932e5422f0517338ef97cb44e7440236af88f9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681340
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169510}

--- components/supervised_user/core/browser/proto_fetcher.h
+++ components/supervised_user/core/browser/proto_fetcher.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/strings/string_piece.h"
 #include "base/types/strong_alias.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/supervised_user/core/browser/fetcher_config.h"
