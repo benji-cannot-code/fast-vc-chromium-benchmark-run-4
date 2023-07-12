49c0585b6519bb8795d1d368866991288a9d3a61
Leonard Grey <lgrey@chromium.org>
2023-07-12 23:20:01 +0000

    Mac: ensure web content isn't obscured when find bar is active in immersive fullscreen
    In immersive fullscreen, web contents are laid out from the top of
    the window whether or not topchrome is showing. This means that when
    the find bar is active and there are find results at the very top of the
    page they may be obscured by the topchrome.
    
    This change adjusts the web content offset to sit below the topchrome when the
    find bar is active, and restores it when the find bar is hidden.
    
    Bug: 1350595
    Include-Ci-Only-Tests: true
    Change-Id: I534207910add2925da7c7b440423dc3791209491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670011
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169621}

--- chrome/browser/ui/views/frame/immersive_mode_controller_stub.cc
+++ chrome/browser/ui/views/frame/immersive_mode_controller_stub.cc
@@ -47,3 +47,7 @@ bool ImmersiveModeControllerStub::ShouldStayImmersiveAfterExitingFullscreen() {
 void ImmersiveModeControllerStub::OnWidgetActivationChanged(
     views::Widget* widget,
     bool active) {}
+
+int ImmersiveModeControllerStub::GetMinimumContentOffset() const {
+  return 0;
+}
