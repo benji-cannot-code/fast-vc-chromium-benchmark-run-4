7c452cc5524105fa5ee634bcb8067154e703e9c3
Bastian Kersting <bkersting@google.com>
2023-07-12 15:04:05 +0000

    [Telemetry] Create converter for common types
    This change moves conversion of the TelemetryExtensionSupportStatus
    and related types to a "common" folder so that in can be shared
    accross different namespaces.
    
    Bug: b/286235946
    Test: unit_tests --gtest_filter="*TelemetryExtensionConvertersTest*"
    Change-Id: I7365bafa9753953610fecee0a3e324455c78dc58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679084
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169343}

--- chrome/browser/ash/telemetry_extension/events/telemetry_event_service_ash.cc
+++ chrome/browser/ash/telemetry_extension/events/telemetry_event_service_ash.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
+#include "chrome/browser/ash/telemetry_extension/common/telemetry_extension_converters.h"
 #include "chrome/browser/ash/telemetry_extension/events/telemetry_event_forwarder.h"
 #include "chrome/browser/ash/telemetry_extension/events/telemetry_event_service_converters.h"
 #include "chromeos/ash/services/cros_healthd/public/cpp/service_connection.h"
@@ -80,7 +81,7 @@ void TelemetryEventServiceAsh::IsEventSupported(
               [](IsEventSupportedCallback callback,
                  cros_healthd::mojom::SupportStatusPtr ptr) {
                 std::move(callback).Run(
-                    converters::ConvertStructPtr(std::move(ptr)));
+                    converters::ConvertCommonPtr(std::move(ptr)));
               },
               std::move(callback)));
 }
--- chrome/browser/ash/telemetry_extension/events/telemetry_event_service_converters.h
+++ chrome/browser/ash/telemetry_extension/events/telemetry_event_service_converters.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/diagnostics/mojom/input.mojom.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_events.mojom.h"
-#include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_exception.mojom.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/nullable_primitives.mojom.h"
 #include "chromeos/crosapi/mojom/telemetry_event_service.mojom.h"
 #include "chromeos/crosapi/mojom/telemetry_extension_exception.mojom.h"
@@ -81,21 +80,6 @@ crosapi::mojom::UInt32ValuePtr UncheckedConvertPtr(
 crosapi::mojom::TelemetryEventInfoPtr UncheckedConvertPtr(
     cros_healthd::mojom::EventInfoPtr input);
 
-crosapi::mojom::TelemetryExtensionExceptionPtr UncheckedConvertPtr(
-    cros_healthd::mojom::ExceptionPtr input);
-
-crosapi::mojom::TelemetryExtensionSupportedPtr UncheckedConvertPtr(
-    cros_healthd::mojom::SupportedPtr input);
-
-crosapi::mojom::TelemetryExtensionUnsupportedReasonPtr UncheckedConvertPtr(
-    cros_healthd::mojom::UnsupportedReasonPtr input);
-
-crosapi::mojom::TelemetryExtensionUnsupportedPtr UncheckedConvertPtr(
-    cros_healthd::mojom::UnsupportedPtr input);
-
-crosapi::mojom::TelemetryExtensionSupportStatusPtr UncheckedConvertPtr(
-    cros_healthd::mojom::SupportStatusPtr input);
-
 }  // namespace unchecked
 
 crosapi::mojom::TelemetryAudioJackEventInfo::State Convert(
@@ -142,9 +126,6 @@ crosapi::mojom::TelemetryHdmiEventInfo::State Convert(
 
 crosapi::mojom::TelemetryInputTouchButton Convert(
     cros_healthd::mojom::InputTouchButton input);
-
-crosapi::mojom::TelemetryExtensionException::Reason Convert(
-    cros_healthd::mojom::Exception::Reason input);
 
 cros_healthd::mojom::EventCategoryEnum Convert(
     crosapi::mojom::TelemetryEventCategoryEnum input);
