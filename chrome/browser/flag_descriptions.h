ddc58741819b366dfce1fcdcf63ed3008fddd44f
Luca Accorsi <accorsi@google.com>
2023-07-12 08:44:47 +0000

    dlp: Add feature flag to guard the FileBackedBlobFactory interface
    Bug: b/273482511
    Change-Id: Ib578a6aba43a8ae34c43d3ef3b1710c33f9c2f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660777
    Reviewed-by: Daniel Brinkers <brinky@google.com>
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169203}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -223,6 +223,9 @@ extern const char kUseMessagesStagingUrlDescription[];
 extern const char kUseCustomMessagesDomainName[];
 extern const char kUseCustomMessagesDomainDescription[];
 
+extern const char kEnableFileBackedBlobFactoryName[];
+extern const char kEnableFileBackedBlobFactoryDescription[];
+
 extern const char kEnableAutoDisableAccessibilityName[];
 extern const char kEnableAutoDisableAccessibilityDescription[];
 
