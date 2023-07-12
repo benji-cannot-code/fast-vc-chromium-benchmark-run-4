571522a9829bc1528bb29272940d4b0701fd9f1a
Minoru Chikamune <chikamune@chromium.org>
2023-07-12 23:45:29 +0000

    Add DumpWithoutCrashing to ServiceWorkerEventQueue
    This CL adds DumpWithoutCrashing() to investigate crash.
    ScheduleIdleCallback seems to be called during the service worker
    warmed-up state. I want to know the call site by using
    DumpWithoutCrashing.
    
    Bug: 1462568
    Change-Id: Id90e754d16fe5bd28d5a924b43a03d12acf5dccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680985
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169642}

--- third_party/blink/renderer/modules/service_worker/service_worker_event_queue.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_event_queue.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/service_worker/service_worker_event_queue.h"
 
 #include "base/containers/contains.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/default_tick_clock.h"
@@ -68,6 +69,7 @@ ServiceWorkerEventQueue::~ServiceWorkerEventQueue() {
 
 void ServiceWorkerEventQueue::Start() {
   DCHECK(!timer_.IsRunning());
+  timer_started_ = true;
   if (!HasInflightEvent() && !HasScheduledIdleCallback()) {
     // If no event happens until Start(), the idle callback should be scheduled.
     OnNoInflightEvent();
@@ -301,8 +303,19 @@ void ServiceWorkerEventQueue::UpdateStatus() {
 }
 
 void ServiceWorkerEventQueue::ScheduleIdleCallback(base::TimeDelta delay) {
-  DCHECK(!HasInflightEvent());
-  DCHECK(!HasScheduledIdleCallback());
+  CHECK(!HasInflightEvent());
+  CHECK(!HasScheduledIdleCallback());
+
+  if (!timer_started_) {
+    // TODO(crbug.com/1462568): After investigating crash bug, the following
+    // DumpWithoutCrashing should be removed.
+    static bool has_dumped_without_crashing = false;
+    if (!has_dumped_without_crashing) {
+      has_dumped_without_crashing = true;
+      SCOPED_CRASH_KEY_NUMBER("SWEventQueue", "delay", delay.InMilliseconds());
+      base::debug::DumpWithoutCrashing();
+    }
+  }
 
   // WTF::Unretained() is safe because the task runner will be destroyed
   // before |this| is destroyed at ServiceWorkerGlobalScope::Dispose().
