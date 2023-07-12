b0b5a698d0fbed5afdd2956f9a12e43823c3158e
Mason Freed <masonf@chromium.org>
2023-07-12 22:50:12 +0000

    Add metadata support via the init parameter for Part
    This just connects the NodePartInit argument to the metadata
    already in Part, and plumbs metadata through all of the
    constructors.
    
    Bug: 1453291
    Change-Id: I4ccdf562ed875e41425708e1fe45fd59bc7f20b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679746
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169599}

--- third_party/blink/renderer/core/dom/part.cc
+++ third_party/blink/renderer/core/dom/part.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-Part::Part(PartRoot& root) : root_(root) {
+Part::Part(PartRoot& root, const Vector<String> metadata)
+    : root_(root), metadata_(metadata) {
   root.AddPart(*this);
 }
 
