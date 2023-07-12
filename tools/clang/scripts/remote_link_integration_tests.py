c778c3c1a3a29ba78a669c47bc5966128bfe01b8
Adrian Taylor <adetaylor@chromium.org>
2023-07-12 09:46:22 +0000

    [rust] Expand all archives in remote link.
    Prior to this change, the remote linking procedure would ask the linker
    to make a list of all the linkable object files using
     -Wl,-plugin-opt=thinlto-index-only=$out
    $out here is an .rsp file which is intended to be passed straight into
    the linker later.
    
    When presented with .a archives or equivalent, this would list invalid
    filenames within the rsp file, and thus caused linker errors.
    
    This is a known limitation of the remote LTO procedure:
    https://discourse.llvm.org/t/running-distributed-thinlto-without-thin-archives/52261.
    and the standard workaround seems to be to extract object files from
    archives prior to performing the indexing step. That's what this CL
    does.
    
    This wasn't previously important, because only one non-thin archive
    was included (a clang runtime .a), and as this was machine code not
    bitcode, none of its members underwent this treatment.
    However, the standard output format for a Rust "crate" (a Rust
    compilation unit) is an .rlib, which is a (non-thin) .a in disguise, and
    thus we have to cope with non-thin archives properly.
    
    This CL expands the .a or .rlib into individual object files before
    proceeding. The script already coped with "thin archives"; that is,
    lists of references to existing files on disk, so this change expands
    that existing logic to cope with all types of archives, actually
    extracting files if necessary. (Either way, such archives are
    wrapped in --start-lib, --end-lib switches which means the expanded
    list of .o files is equivalent to including the archive directly.)
    
    This requires us to switch from the previously python-coded .a
    parser to using the full power of llvm-ar, so tht we can cope with
    both thin and regular archives.
    
    Alternatives considered:
    (a) amend lld so that the output of thinlto-index-only no longer
        produces invalid filenames. For the reasons discussed in the
        LLVM thread above, this is deemed unlikely to work.
    (b) persuade rustc to generate thin archives instead of regular
        archives. This seems unlikely to work unless the component .o
        files are retained on disk somewhere after construction of the
        .rlib file - this could be a future optimisation
    (c) post-process the output of thinlto-index-only before using it
        as linker input (e.g. by quoting each line). This could work
        but is more complex, and it doesn't avoid the need to extract
        each file at some point.
    (d) Extract the files just as we're doing here, but instead
        using steps within the generated ninja file. This would have
        benefits in parallelizing the extraction and in avoiding
        unnecessary rebuilds for unchanged inputs, but did not work
        in practice because the process of generating the ninja file
        itself depends upon the nature of each .o file (bitcode or
        otherwise).
    (e) Expand the existing parser to cope with regular .a archives
        as well as thin archives, instead of calling out to llvm-ar.
        Although this would be slightly quicker, the complexity does
        not seem obviously merited, since linking time is dominated
        by other factors.
    
    As part of this change,
    * The --ar-path argument has become the first required=True
      argument to this script, so the overall argument parsing
      has been slightly simplified.
    * Suitable test code has been added. NB that on some platforms
      it may be easier to run the test code if you
      also have
      https://chromium-review.googlesource.com/c/chromium/src/+/4672673
    
    Bug: 1459492
    Change-Id: I7ea0c3c6ad14531ff6c7a3443c22931506ecde06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4665860
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Bob Haarman <inglorion@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169222}

--- tools/clang/scripts/remote_link_integration_tests.py
+++ tools/clang/scripts/remote_link_integration_tests.py
@@ -50,6 +50,16 @@ def _create_inputs(path):
     f.write('int bar() {\n  return 9;\n}\n')
 
 
+def _lto_args(generate_bitcode):
+  """
+  Returns list of arguments to clang to generate bitcode or not.
+  """
+  if generate_bitcode:
+    return ['-flto=thin']
+  else:
+    return []
+
+
 class RemoteLinkUnixAllowMain(remote_ld.RemoteLinkUnix):
   """
   Same as remote_ld.RemoteLinkUnix, but has "main" on the allow list.
@@ -77,6 +87,9 @@ class RemoteLinkIntegrationTest(unittest.TestCase):
   def lld_link(self):
     return os.path.join(LLVM_BIN_DIR, 'lld-link' + remote_link.exe_suffix())
 
+  def llvmar(self):
+    return os.path.join(LLVM_BIN_DIR, 'llvm-ar' + remote_link.exe_suffix())
+
   def test_distributed_lto_common_objs(self):
     with named_directory() as d, working_directory(d):
       _create_inputs(d)
@@ -91,15 +104,17 @@ class RemoteLinkIntegrationTest(unittest.TestCase):
       subprocess.check_call([
           self.clangcl(), '-c', '-Os', '-flto=thin', 'bar.cpp', '-Foobj/bar.obj'
       ])
-      subprocess.check_call(
-          ['llvm-ar', 'crsT', 'obj/foobar.lib', 'obj/bar.obj', 'obj/foo.obj'])
+      subprocess.check_call([
+          self.llvmar(), 'crsT', 'obj/foobar.lib', 'obj/bar.obj', 'obj/foo.obj'
+      ])
       with open('main.rsp', 'w') as f:
         f.write('obj/main.obj\n' 'obj/foobar.lib\n')
       with open('my_goma.sh', 'w') as f:
         f.write('#! /bin/sh\n\ngomacc "$@"\n')
       os.chmod('my_goma.sh', 0o755)
       rc = remote_link.RemoteLinkWindows().main([
-          'remote_link.py', '--wrapper', './my_goma.sh', '--',
+          'remote_link.py', '--wrapper', './my_goma.sh', '--ar-path',
+          self.llvmar(), '--',
           self.lld_link(), '-nodefaultlib', '-entry:main', '-out:main.exe',
           '@main.rsp'
       ])
@@ -146,12 +161,14 @@ class RemoteLinkIntegrationTest(unittest.TestCase):
           self.clangcl(), '-c', '-Os', '-flto=thin', '-m32', 'bar.cpp',
           '-Foobj/bar.obj'
       ])
-      subprocess.check_call(
-          ['llvm-ar', 'crsT', 'obj/foobar.lib', 'obj/bar.obj', 'obj/foo.obj'])
+      subprocess.check_call([
+          self.llvmar(), 'crsT', 'obj/foobar.lib', 'obj/bar.obj', 'obj/foo.obj'
+      ])
       with open('main.rsp', 'w') as f:
         f.write('obj/main.obj\n' 'obj/foobar.lib\n')
       rc = RemoteLinkWindowsAllowMain().main([
-          'remote_link.py', '--wrapper', 'gomacc', '--',
+          'remote_link.py', '--wrapper', 'gomacc', '--ar-path',
+          self.llvmar(), '--',
           self.lld_link(), '-nodefaultlib', '-entry:main', '-machine:X86',
           '-opt:lldlto=2', '-mllvm:-import-instr-limit=10', '-out:main.exe',
           '@main.rsp'
@@ -197,7 +214,8 @@ class RemoteLinkIntegrationTest(unittest.TestCase):
           self.clangcl(), '-c', '-O2', '-flto=thin', 'foo.cpp', '-Foobj/foo.obj'
       ])
       rc = remote_link.RemoteLinkWindows().main([
-          'remote_link.py', '--generate', '--allowlist', '--',
+          'remote_link.py', '--generate', '--allowlist', '--ar-path',
+          self.llvmar(), '--',
           self.lld_link(), '-nodefaultlib', '-entry:main', '-opt:lldlto=2',
           '-out:main.exe', 'obj/main.obj', 'obj/foo.obj'
       ])
@@ -225,6 +243,9 @@ class RemoteLdIntegrationTest(unittest.TestCase):
   def clangxx(self):
     return os.path.join(LLVM_BIN_DIR, 'clang++' + remote_link.exe_suffix())
 
+  def llvmar(self):
+    return os.path.join(LLVM_BIN_DIR, 'llvm-ar' + remote_link.exe_suffix())
+
   def test_nonlto(self):
     with named_directory() as d, working_directory(d):
       _create_inputs(d)
@@ -233,7 +254,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       subprocess.check_call(
           [self.clangxx(), '-c', '-Os', 'foo.cpp', '-o', 'foo.o'])
       rc = RemoteLinkUnixAllowMain().main([
-          'remote_ld.py', '--wrapper', 'gomacc', '--',
+          'remote_ld.py', '--wrapper', 'gomacc', '--ar-path',
+          self.llvmar(), '--',
           self.clangxx(), '-fuse-ld=lld', 'main.o', 'foo.o', '-o', 'main'
       ])
       # Should succeed.
@@ -256,7 +278,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       subprocess.check_call(
           [self.clangxx(), '-c', '-Os', '-flto=thin', 'foo.cpp', '-o', 'foo.o'])
       rc = remote_ld.RemoteLinkUnix().main([
-          'remote_ld.py', '--wrapper', 'gomacc', '--',
+          'remote_ld.py', '--wrapper', 'gomacc', '--ar-path',
+          self.llvmar(), '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', 'main.o', 'foo.o', '-o',
           'main'
       ])
@@ -280,7 +303,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       subprocess.check_call(
           [self.clangxx(), '-c', '-Os', '-flto=thin', 'foo.cpp', '-o', 'foo.o'])
       rc = RemoteLinkUnixAllowMain().main([
-          'remote_ld.py', '-j', '16', '--',
+          'remote_ld.py', '-j', '16', '--ar-path',
+          self.llvmar(), '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', 'main.o', 'foo.o', '-o',
           'main'
       ])
@@ -310,9 +334,10 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       subprocess.check_call(
           [self.clangxx(), '-c', '-Os', '-flto=thin', 'bar.cpp', '-o', 'bar.o'])
       subprocess.check_call(
-          ['llvm-ar', 'crsT', 'libfoobar.a', 'bar.o', 'foo.o'])
+          [self.llvmar(), 'crsT', 'libfoobar.a', 'bar.o', 'foo.o'])
       rc = RemoteLinkUnixAllowMain().main([
-          'remote_ld.py',
+          'remote_ld.py', '--ar-path',
+          self.llvmar(), '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', 'main.o', 'libfoobar.a',
           '-o', 'main'
       ])
@@ -332,42 +357,96 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       self.assertNotIn(b'foo', main_disasm)
 
   def test_distributed_lto_thin_archive_subdir(self):
+    self.run_archive_test(bitcode_archive=True,
+                          bitcode_main=True,
+                          thin_archive=True)
+
+  def test_distributed_machine_code_thin_archive_bitcode_main_subdir(self):
+    self.run_archive_test(bitcode_archive=False,
+                          bitcode_main=True,
+                          thin_archive=True)
+
+  def test_distributed_machine_code_thin_archive_subdir(self):
+    self.run_archive_test(bitcode_archive=False,
+                          bitcode_main=False,
+                          thin_archive=True)
+
+  def test_distributed_bitcode_thick_archive_subdir(self):
+    self.run_archive_test(bitcode_archive=True,
+                          bitcode_main=True,
+                          thin_archive=False)
+
+  def test_distributed_machine_code_thick_archive_subdir(self):
+    self.run_archive_test(bitcode_archive=False,
+                          bitcode_main=False,
+                          thin_archive=False)
+
+  def test_distributed_machine_code_thick_archive_bitcode_main_subdir(self):
+    self.run_archive_test(bitcode_archive=False,
+                          bitcode_main=True,
+                          thin_archive=False)
+
+  def run_archive_test(self, bitcode_archive, bitcode_main, thin_archive):
+    """
+    Runs a test to ensure correct remote linking handling of
+    an archive.
+    Arguments:
+    bitcode_archive: whether the archive should contain bitcode (true)
+    or machine code (false)
+    bitcode_main: whether the main object, outside the archive, should
+    contain bitcode (true) or machine code (false)
+    thin_archive: whether to create a thin archive instead of a regular archive.
+    """
     with named_directory() as d, working_directory(d):
       _create_inputs(d)
       os.makedirs('obj')
+      subprocess.check_call(
+          [self.clangxx(), '-c', '-Os', 'main.cpp', '-o', 'obj/main.o'] +
+          _lto_args(bitcode_main))
+      subprocess.check_call(
+          [self.clangxx(), '-c', '-Os', 'foo.cpp', '-o', 'obj/foo.o'] +
+          _lto_args(bitcode_archive))
+      subprocess.check_call(
+          [self.clangxx(), '-c', '-Os', 'bar.cpp', '-o', 'obj/bar.o'] +
+          _lto_args(bitcode_archive))
+      archive_creation_arg = 'crs'
+      if thin_archive:
+        archive_creation_arg = 'crsT'
       subprocess.check_call([
-          self.clangxx(), '-c', '-Os', '-flto=thin', 'main.cpp', '-o',
-          'obj/main.o'
-      ])
-      subprocess.check_call([
-          self.clangxx(), '-c', '-Os', '-flto=thin', 'foo.cpp', '-o',
+          self.llvmar(), archive_creation_arg, 'obj/libfoobar.a', 'obj/bar.o',
           'obj/foo.o'
       ])
-      subprocess.check_call([
-          self.clangxx(), '-c', '-Os', '-flto=thin', 'bar.cpp', '-o',
-          'obj/bar.o'
-      ])
-      subprocess.check_call(
-          ['llvm-ar', 'crsT', 'obj/libfoobar.a', 'obj/bar.o', 'obj/foo.o'])
       rc = RemoteLinkUnixAllowMain().main([
-          'remote_ld.py',
+          'remote_ld.py', '--ar-path',
+          self.llvmar(), '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', 'obj/main.o',
           'obj/libfoobar.a', '-o', 'main'
       ])
       # Should succeed.
       self.assertEqual(rc, 0)
-      # build.ninja file should have gomacc invocations in it.
-      with open(os.path.join(d, 'lto.main', 'build.ninja')) as f:
-        buildrules = f.read()
-        self.assertIn('gomacc ', buildrules)
-        self.assertIn('build lto.main/obj/main.o.stamp : codegen ', buildrules)
-        self.assertIn('build lto.main/obj/foo.o.stamp : codegen ', buildrules)
+      if bitcode_main or bitcode_archive:
+        # build.ninja file should have gomacc invocations in it.
+        with open(os.path.join(d, 'lto.main', 'build.ninja')) as f:
+          buildrules = f.read()
+          self.assertIn('gomacc ', buildrules)
+          if bitcode_main:
+            self.assertIn('build lto.main/obj/main.o.stamp : codegen ',
+                          buildrules)
+          if bitcode_archive:
+            if thin_archive:
+              self.assertIn('build lto.main/obj/foo.o.stamp : codegen ',
+                            buildrules)
+            else:
+              self.assertIn(
+                  'build lto.main/expanded_archives/obj/libfoobar.a/' +
+                  'foo.o.stamp : codegen ', buildrules)
       # Check that main does not call foo.
-      disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
-      main_idx = disasm.index(b' <main>:\n')
-      after_main_idx = disasm.index(b'\n\n', main_idx)
-      main_disasm = disasm[main_idx:after_main_idx]
-      self.assertNotIn(b'foo', main_disasm)
+      if bitcode_archive:
+        disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
+        main_idx = disasm.index(b' <main>:\n')
+        after_main_idx = disasm.index(b'\n\n', main_idx)
+        main_disasm = disasm[main_idx:after_main_idx]
+        self.assertNotIn(b'foo', main_disasm)
 
   def test_debug_params(self):
     with named_directory() as d, working_directory(d):
@@ -384,7 +463,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       with open('main.rsp', 'w') as f:
         f.write('obj/main.o\n' 'obj/foo.o\n')
       rc = RemoteLinkUnixAllowMain().main([
-          'remote_ld.py',
+          'remote_ld.py', '--ar-path',
+          self.llvmar(), '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', '-g', '-gsplit-dwarf',
           '-Wl,--lto-O2', '-o', 'main', '@main.rsp'
       ])
@@ -415,14 +495,15 @@ class RemoteLdIntegrationTest(unittest.TestCase):
           '-fwhole-program-vtables', 'bar.cpp', '-o', 'obj/bar.o'
       ])
       subprocess.check_call(
-          ['llvm-ar', 'crsT', 'obj/libfoobar.a', 'obj/bar.o', 'obj/foo.o'])
+          [self.llvmar(), 'crsT', 'obj/libfoobar.a', 'obj/bar.o', 'obj/foo.o'])
       with open('main.rsp', 'w') as f:
         f.write('-fsplit-lto-unit\n'
                 '-fwhole-program-vtables\n'
                 'obj/main.o\n'
                 'obj/libfoobar.a\n')
       rc = RemoteLinkUnixAllowMain().main([
-          'remote_ld.py',
+          'remote_ld.py', '--ar-path',
+          self.llvmar(), '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', '-m32', '-Wl,-mllvm',
           '-Wl,-generate-type-units', '-Wl,--lto-O2', '-o', 'main',
           '-Wl,--start-group', '@main.rsp', '-Wl,--end-group'
@@ -463,7 +544,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       subprocess.check_call(
           [self.clangxx(), '-c', '-Os', '-flto=thin', 'foo.cpp', '-o', 'foo.o'])
       rc = RemoteLinkUnixAllowMain().main([
-          'remote_ld.py', '--no-wrapper', '-j', '16', '--',
+          'remote_ld.py', '--ar-path',
+          self.llvmar(), '--no-wrapper', '-j', '16', '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', 'main.o', 'foo.o', '-o',
           'main'
       ])
@@ -488,7 +570,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
         f.write(b'\7fELF')
       with mock.patch('sys.stderr', new_callable=StringIO) as stderr:
         rc = RemoteLinkUnixAllowMain().main([
-            'remote_ld.py', '--generate', '--',
+            'remote_ld.py', '--ar-path',
+            self.llvmar(), '--generate', '--',
             self.clangxx(), 'main.o', '-o', 'main'
         ])
         self.assertEqual(rc, 5)
@@ -500,7 +583,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
         f.write(b'BC\xc0\xde')
       with mock.patch('sys.stderr', new_callable=StringIO) as stderr:
         rc = RemoteLinkUnixAllowMain().main([
-            'remote_ld.py', '--generate', '--',
+            'remote_ld.py', '--ar-path',
+            self.llvmar(), '--generate', '--',
             self.clangxx(), 'main.o', '-o', 'main'
         ])
         self.assertEqual(rc, 0)
@@ -523,7 +607,8 @@ class RemoteLdIntegrationTest(unittest.TestCase):
       subprocess.check_call(
           [self.clangxx(), '-c', '-Os', '-flto=thin', 'foo.cpp', '-o', 'foo.o'])
       rc = remote_ld.RemoteLinkUnix().main([
-          'remote_ld.py', '--generate', '--allowlist', '--',
+          'remote_ld.py', '--ar-path',
+          self.llvmar(), '--generate', '--allowlist', '--',
           self.clangxx(), '-fuse-ld=lld', '-flto=thin', 'main.o', 'foo.o', '-o',
           'main'
       ])
