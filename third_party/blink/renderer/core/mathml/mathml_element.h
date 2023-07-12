447a893abc330316742656991ad3b5553130d5f2
Frédéric Wang <fwang@igalia.com>
2023-07-12 20:56:41 +0000

    [mathml] Implement columnspan/rowspan attributes
    This CL implements the columnspan and rowspan attributes from MathML,
    which have same syntax and semantics as corresponding HTML
    attributes [1]. New WPT tests are added to check table layout with these
    attributes. However, parsing tests are written as internal ones until
    MathML as a proper IDL [2]. Intent to ship thread is at [3].
    
    To help with that implementation, a new MathMLTableCellElement class is
    introduced for the <mtd> element. This also simplifies other places
    where MathML table cells are handled specially.
    
    [1] https://w3c.github.io/mathml-core/#entry-in-table-or-matrix-mtd
    [2] https://github.com/w3c/mathml-core/issues/166
    [3] https://groups.google.com/a/chromium.org/g/blink-dev/c/SqIQf6nKxxk
    
    Bug: 1371806
    Change-Id: I389e60becf8956fc30ab4043b261982a2813f890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061476
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169551}

--- third_party/blink/renderer/core/mathml/mathml_element.h
+++ third_party/blink/renderer/core/mathml/mathml_element.h
@@ -63,8 +63,6 @@ class CORE_EXPORT MathMLElement : public Element {
 
   // https://w3c.github.io/mathml-core/#dfn-boolean
   absl::optional<bool> BooleanAttribute(const QualifiedName& name) const;
-
-  LayoutObject* CreateLayoutObject(const ComputedStyle&) override;
 };
 
 template <typename T>
