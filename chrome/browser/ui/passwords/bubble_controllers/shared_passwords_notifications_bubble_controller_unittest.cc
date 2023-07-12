c0455d882ce8b5b7e92ec6a7792688662b712c2d
Mohamed Amir Yosef <mamir@chromium.org>
2023-07-12 18:06:11 +0000

    [Passwords] Controller skeleton for the shared passwords notification
    This CL adds the controller skeleton for the shared password
    notification bubble.
    
    Follow-up CLs will add the View as well as connect pieces together.
    
    Mocks are in the linked bug.
    
    Bug: 1464209
    Change-Id: Ia6952d0610b602ae817e4499a1f0b394cd519746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678754
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169451}

---
+++ chrome/browser/ui/passwords/bubble_controllers/shared_passwords_notifications_bubble_controller_unittest.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/bubble_controllers/shared_passwords_notifications_bubble_controller.h"
+
+#include "chrome/browser/ui/passwords/passwords_model_delegate_mock.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class SharedPasswordsNotificationBubbleControllerTest : public ::testing::Test {
+ public:
+  SharedPasswordsNotificationBubbleControllerTest() {
+    mock_delegate_ =
+        std::make_unique<testing::NiceMock<PasswordsModelDelegateMock>>();
+    controller_ = std::make_unique<SharedPasswordsNotificationBubbleController>(
+        mock_delegate_->AsWeakPtr());
+  }
+  ~SharedPasswordsNotificationBubbleControllerTest() override = default;
+
+  PasswordsModelDelegateMock* delegate() { return mock_delegate_.get(); }
+  SharedPasswordsNotificationBubbleController* controller() {
+    return controller_.get();
+  }
+
+ private:
+  std::unique_ptr<PasswordsModelDelegateMock> mock_delegate_;
+  std::unique_ptr<SharedPasswordsNotificationBubbleController> controller_;
+};
+
+TEST_F(SharedPasswordsNotificationBubbleControllerTest, HasTitle) {
+  EXPECT_FALSE(controller()->GetTitle().empty());
+}
