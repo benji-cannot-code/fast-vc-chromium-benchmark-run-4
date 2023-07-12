3de2641e9e39a664ac3ab5993db2c547420c6f9f
Xiang Ji <jxiang@google.com>
2023-07-12 15:55:58 +0000

    Enable ProxyMode on Android
    Bug: 1310000
    Change-Id: If1930c81b9719abb2a00b577ba8ef9e2f3d1ba07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679499
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169365}

--- chrome/browser/policy/test/proxy_policies_browsertest.cc
+++ chrome/browser/policy/test/proxy_policies_browsertest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/chrome_test_utils.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_namespace.h"
 #include "components/policy/core/common/policy_service.h"
@@ -79,8 +79,8 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, SeparateProxyPoliciesMerging) {
   VerifyProxyPrefs(g_browser_process->local_state(), std::string(),
                    std::string(), absl::nullopt, std::string(),
                    ProxyPrefs::MODE_SYSTEM);
-  VerifyProxyPrefs(browser()->profile()->GetPrefs(), std::string(),
-                   std::string(), absl::nullopt, std::string(),
+  VerifyProxyPrefs(chrome_test_utils::GetProfile(this)->GetPrefs(),
+                   std::string(), std::string(), absl::nullopt, std::string(),
                    ProxyPrefs::MODE_SYSTEM);
 }
 
