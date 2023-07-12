1fe8ccc28d1faf32b894cfcfe99488fdd2be6473
dpapad <dpapad@chromium.org>
2023-07-12 21:52:59 +0000

    WebUI: Simplify the names and classes of c/b/r/side_panel tests.
    Now that side_panel_browsertest.cc is no longer auto-generated by
    js2gtest, a simpler pattern can be used that does not require each test
    case to use a different C++ class name.
    
    Specifically:
     - Add a dedicated *_browsertest.cc file for each side_panel/ subfolder.
     - Reduce the number of C++ test class names, and remove the
       meaningless '*.All' naming pattern.
    
    With this change running specific tests is much easier, for example
    see queries below:
     - Bookmarks       -> --gtest_filter=SidePanelBookmarksTest.*
     - Power Bookmarks -> --gtest_filter=SidePanelPowerBookmarksTest.*
     - Commerce        -> --gtest_filter=SidePanelShoppingInsightsTest.*
     - UserNotes       -> --gtest_filter=SidePanelUserNotesTest.*
     - ReadAnything    -> --gtest_filter=SidePanelReadingListTest.*
     - All SidePanels  -> --gtest_filter=SidePanel*Test.*
    
    Bug: 1457360,1396268
    Change-Id: I3324ba3246f779ddd74f0968c4c4fcc66ab7245c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679485
    Reviewed-by: John Lee <johntlee@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169583}

--- chrome/test/data/webui/side_panel/bookmarks/sp_bookmarks_focus_test.cc
+++ chrome/test/data/webui/side_panel/bookmarks/sp_bookmarks_focus_test.cc
@@ -7,9 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "content/public/test/browser_test.h"
 
-using SidePanelBookmarksListInteractiveUITest = WebUIMochaFocusTest;
-IN_PROC_BROWSER_TEST_F(SidePanelBookmarksListInteractiveUITest, All) {
+using SidePanelBookmarksFocusTest = WebUIMochaFocusTest;
+IN_PROC_BROWSER_TEST_F(SidePanelBookmarksFocusTest, List) {
   set_test_loader_host(chrome::kChromeUIBookmarksSidePanelHost);
-  RunTest("side_panel/bookmarks/bookmarks_list_interactive_ui_test.js",
-          "mocha.run()");
+  RunTest("side_panel/bookmarks/bookmarks_list_focus_test.js", "mocha.run()");
 }
