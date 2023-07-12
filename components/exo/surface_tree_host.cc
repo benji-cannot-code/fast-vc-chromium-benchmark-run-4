78aaac7770498e62c1831d39481925b7f8a27036
Eriko Kurimoto <elkurin@chromium.org>
2023-07-12 12:39:00 +0000

    exo: Allocate surface id if not when pixel host window bounds changed
    This CL explicitly allocates viz::LocalSurfaceId when the pixel bounds
    of host window is updated but id is not updated.
    
    We need to always allocate local surface id when pixel coordinates host
    window bounds has changed.
    - Scaled bounds is not changed: Allocated by crrev.com/c/4639542
    - Scaled size is changed: Allocated by host_window_->SetBounds
    - Scaled size is not changed, but origin is changed: NOT addressed
    
    This change is required to address the case where scaled bounds is
    changed but not the size since SetLocalSurfaceId is triggered by
    bound settings only when there is a size change[1].
    Note that pixel size may be changed in this case.
    
    This CL unblocks crrev.com/c/4639727 and clip rect delegation which
    introduces host window with non-zero origin.
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:ui/aura/window.cc;l=1418;drc=316850595842d7b40953b16f96e7615b1bfa57a7
    
    Bug: 1457446
    Change-Id: I5ba6daa6a120618de63599ef1e13adfc029b3e47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4677252
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169267}

--- components/exo/surface_tree_host.cc
+++ components/exo/surface_tree_host.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/shared_quad_state.h"
 #include "components/viz/common/quads/solid_color_draw_quad.h"
 #include "components/viz/common/resources/transferable_resource.h"
+#include "components/viz/common/surfaces/local_surface_id.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/accessibility/aura/aura_window_properties.h"
@@ -405,6 +406,7 @@ void SurfaceTreeHost::UpdateHostWindowBounds() {
 
   const gfx::Rect& bounds = root_surface_->surface_hierarchy_content_bounds();
   if (previous_content_bounds_ != bounds) {
+    const viz::LocalSurfaceId old_id = host_window_->GetLocalSurfaceId();
     previous_content_bounds_ = bounds;
     gfx::Size size = bounds.size();
     if (client_submits_surfaces_in_pixel_coordinates_) {
@@ -414,8 +416,11 @@ void SurfaceTreeHost::UpdateHostWindowBounds() {
     if (scaled_bounds != host_window_->bounds()) {
       // DP size has changed, set new bounds.
       host_window_->SetBounds({host_window_->bounds().origin(), size});
-    } else {
-      // DP size has not changed, but pixel size has - allocate new surface id.
+    }
+
+    if (host_window_->GetLocalSurfaceId() == old_id) {
+      // Explicitly allocate local surface id if it's not updated since pixel
+      // bounds has changed.
       host_window_->AllocateLocalSurfaceId();
     }
   }
