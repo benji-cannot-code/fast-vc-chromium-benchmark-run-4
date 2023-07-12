3857ffd17697713d1cb8b82c500944d595d95686
Takumi Fujimoto <takumif@chromium.org>
2023-07-12 23:43:58 +0000

    Delete unused Media Router histograms, extend ones being used
    Delete:
    Media.Notification.Cast.ArtworkPresent
    Media.Notification.Cast.MetadataPresent
    MediaRouter.Ui.FirstAction
    MediaRouter.Ui.IconStateAtInit
    
    Delay the expiry:
    MediaRouter.Cast.Discovery.KnownDevicesCount
    MediaRouter.Cast.Discovery.SinkSource
    
    Bug: 1449704, 1449705, 1463502, 1463627
    Change-Id: I08d170bf69721e5ce986a3ef22dcb76d96304487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4677032
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169639}

--- chrome/browser/ui/toolbar/media_router_action_controller.cc
+++ chrome/browser/ui/toolbar/media_router_action_controller.cc
@@ -168,10 +168,6 @@ MediaRouterActionController::MediaRouterActionController(
       prefs::kShowCastIconInToolbar,
       base::BindRepeating(&MediaRouterActionController::MaybeAddOrRemoveAction,
                           base::Unretained(this)));
-  if (!profile_->IsOffTheRecord()) {
-    media_router::MediaRouterMetrics::RecordIconStateAtInit(
-        MediaRouterActionController::GetAlwaysShowActionPref(profile_));
-  }
 }
 
 void MediaRouterActionController::MaybeAddOrRemoveAction() {
