a8a4e339277c777f8da8fae53f789b2f4eb682f0
Justin Cohen <justincohen@google.com>
2023-07-12 15:06:50 +0000

    ios: Deflake password manager export tests.
    Wait for activity sheet before dismissing, otherwise the wrong sheet may
    be dismissed.
    
    Change-Id: I0dc7ed94ace0e13fe86d9bd2048f838db9a3ff33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679588
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169348}

--- ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
@@ -1869,6 +1869,7 @@ void CopyPasswordDetailWithID(int detail_id) {
                                    IDS_IOS_EXPORT_PASSWORDS)]
       assertWithMatcher:exportButtonStatusMatcher];
 
+  [ChromeEarlGrey verifyActivitySheetVisible];
   [ChromeEarlGrey closeActivitySheet];
 
   // Wait until the activity view is dismissed.
