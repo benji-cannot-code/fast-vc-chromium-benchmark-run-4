4935d970606850eeb21749d2e67759589a93fc98
Juliet Levesque <julietlevesque@google.com>
2023-07-12 18:57:05 +0000

    [Nearby Presence] Save remote credentials to NP library
    Comples the CredentialManager first time registration flow by saving
    remote device credentials to the NP library over mojo pipe. See
    go/cros-nearby-presence-credential-web-integration.
    
    the registration logic into chrome://nearby-internals
    
    Test: Unit tests only, manual testing will follow in CL's to plumb
    Bug: b/276307539
    Change-Id: I88256db68c6837e79c2ddee384ea43b796b09e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4560344
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169484}

--- chromeos/ash/services/nearby/public/cpp/fake_nearby_presence.h
+++ chromeos/ash/services/nearby/public/cpp/fake_nearby_presence.h
@@ -79,6 +79,10 @@ class FakeNearbyPresence : public mojom::NearbyPresence,
     update_local_device_metadata_callback_ = std::move(callback);
   }
 
+  void SetUpdateRemoteCredentialsStatus(mojom::StatusCode status) {
+    update_remote_shared_credentials_status_ = status;
+  }
+
  private:
   mojo::SharedRemote<mojom::ScanObserver> scan_observer_remote_;
   mojom::MetadataPtr local_device_metadata_;
@@ -97,6 +101,8 @@ class FakeNearbyPresence : public mojom::NearbyPresence,
       generated_shared_credentials_response_;
   mojom::StatusCode generate_credentials_response_status_ =
       mojom::StatusCode::kFailure;
+  mojom::StatusCode update_remote_shared_credentials_status_ =
+      mojom::StatusCode::kOk;
   base::WeakPtrFactory<FakeNearbyPresence> weak_ptr_factory_{this};
 };
 
