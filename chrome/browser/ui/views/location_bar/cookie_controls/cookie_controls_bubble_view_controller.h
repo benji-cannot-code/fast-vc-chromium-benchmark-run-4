e39b78b9d5d6e5d182b38789900dbd48c460f88c
Theodore Olsauskas-Warren <sauski@google.com>
2023-07-13 08:42:39 +0000

    User Bypass: Create toggle row in bubble
    Bug: 1446230, b:280782656
    Change-Id: I260e1ee247d404fa330ae16cfadfe786b1dc9b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4677334
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169798}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_controller.h
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_controller.h
@@ -42,10 +42,14 @@ class CookieControlsBubbleViewController
       CookieControlsBreakageConfidenceLevel level) override;
 
  private:
-  void SetFeedbackButtonPressedCallback();
+  void SetButtonPressedCallbacks();
+  void OnToggleButtonPressed(bool new_value);
   void OnFeedbackButtonPressed();
 
   void OnFaviconFetched(const favicon_base::FaviconImageResult& result) const;
+
+  void ApplyThirdPartyCookiesAllowedState(base::Time expiration);
+  void ApplyThirdPartyCookiesBlockedState();
 
   [[nodiscard]] std::unique_ptr<views::View> InitReloadingView(
       content::WebContents* web_contents);
@@ -60,6 +64,7 @@ class CookieControlsBubbleViewController
   // Used for favicon loading tasks.
   base::CancelableTaskTracker cancelable_task_tracker_;
 
+  base::CallbackListSubscription toggle_button_callback_;
   base::CallbackListSubscription feedback_button_callback_;
   base::WeakPtr<content_settings::CookieControlsController> controller_;
   base::ScopedObservation<content_settings::CookieControlsController,
