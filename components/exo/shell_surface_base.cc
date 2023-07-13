f987de9dbfe1a667cd58a4e2232ef709a59f0024
Mitsuru Oshima <oshima@chromium.org>
2023-07-13 01:31:30 +0000

    [Cleanup] Remove unnecessary virtual from UpdateHostWindowBounds
    Bug: None
    Change-Id: I733de6e9c724541ab9c27f865998309faf9a986c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4677033
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169694}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -1981,7 +1981,7 @@ void ShellSurfaceBase::CommitWidget() {
   // type (e.g. caption height).
   UpdateFrameType();
   UpdateWidgetBounds();
-  SurfaceTreeHost::UpdateHostWindowBounds();
+  UpdateHostWindowBounds();
   gfx::Rect bounds = geometry_;
   if (!bounds.IsEmpty() && !widget_->GetNativeWindow()->GetProperty(
                                aura::client::kUseWindowBoundsForShadow)) {
