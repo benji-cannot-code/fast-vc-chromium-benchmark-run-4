0b03258632e793f21760d3ba1baca7ab089ffd17
Mark Schillaci <mschillaci@google.com>
2023-07-13 17:06:25 +0000

    [AX-Unserialize] Remove unserialize experiment related code
    This CL removes all code related to the AXTree Unserialize Optimization
    experiment. The data has shown this impl cause a performance regression,
    so this path is being abandoned. The experiment will not restart.
    
    AX-Relnotes: N/A
    Bug: 1429305, b/265493190
    Change-Id: Iba934f1803131df96c3ea7ddddd2ba793ba2cfd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680121
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170005}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -10355,12 +10355,6 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableEmailOtpForVcnYellowPath)},
 
-    {"enable-accessibility-unserialize-optimizations",
-     flag_descriptions::kAccessibilityUnserializeOptimizationsName,
-     flag_descriptions::kAccessibilityUnserializeOptimizationsDescription,
-     kOsAll,
-     FEATURE_VALUE_TYPE(features::kAccessibilityUnserializeOptimizations)},
-
     {"enable-compression-dictionary-transport",
      flag_descriptions::kCompressionDictionaryTransportName,
      flag_descriptions::kCompressionDictionaryTransportDescription, kOsAll,
