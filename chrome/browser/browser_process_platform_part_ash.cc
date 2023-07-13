b68e61364d5bba2553eaade5e94968e0dd233213
Andreea Costinas <acostinas@google.com>
2023-07-13 16:57:13 +0000

    Move code that monitors proxy out of NetworkSettingsServiceAsh
    The NetworkSettingsService monitors proxy changes in Ash and informs
    observers (Lacros browsers) of active proxy changes.
    
    Monitoring the proxy is non-trivial as updates can come from default
    network changes, extensions controlling the proxy in the primary profile
    or policy changes. This CL moves the monitoring logic outside the mojo
    service.
    
    Bug: 1411949, b/267135604, b/192453302
    Change-Id: I5ad00f63685a064f2530763fefcae8b2aa08b570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496399
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169992}

--- chrome/browser/browser_process_platform_part_ash.cc
+++ chrome/browser/browser_process_platform_part_ash.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/saml/in_session_password_change_manager.h"
 #include "chrome/browser/ash/login/session/chrome_session_manager.h"
 #include "chrome/browser/ash/login/users/chrome_user_manager_impl.h"
+#include "chrome/browser/ash/net/ash_proxy_monitor.h"
 #include "chrome/browser/ash/net/delay_network_call.h"
 #include "chrome/browser/ash/net/system_proxy_manager.h"
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
@@ -178,6 +179,16 @@ void BrowserProcessPlatformPart::InitializeSchedulerConfigurationManager() {
 
 void BrowserProcessPlatformPart::ShutdownSchedulerConfigurationManager() {
   scheduler_configuration_manager_.reset();
+}
+
+void BrowserProcessPlatformPart::InitializeAshProxyMonitor() {
+  DCHECK(!ash_proxy_monitor_);
+  ash_proxy_monitor_ = std::make_unique<ash::AshProxyMonitor>(
+      g_browser_process->local_state(), g_browser_process->profile_manager());
+}
+
+void BrowserProcessPlatformPart::ShutdownAshProxyMonitor() {
+  ash_proxy_monitor_.reset();
 }
 
 void BrowserProcessPlatformPart::InitializePrimaryProfileServices(
