e4e36c20d5c365f5de597140a212b1d68bd5ca34
Paola Lacouture <paolalac@google.com>
2023-07-13 17:42:13 +0000

    [Fast Pair] Implemented IsFastPairDevicesBluetoothSettingsEnabled()
    Adding implementation for the function that checks if the flag is
    enabled.
    
    TESTED: Manually verified that the flag appears in chrome://flags and
    ran unit tests.
    
    Change-Id: I265ee27988a698d4ecb536e83e9f7b00aad9a20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679514
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Commit-Queue: Paola Lacouture <paolalac@google.com>
    Reviewed-by: Alex Kingsborough <akingsb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170032}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2942,6 +2942,10 @@ bool IsFastPairDebugMetadataEnabled() {
   return base::FeatureList::IsEnabled(kFastPairDebugMetadata);
 }
 
+bool IsFastPairDevicesBluetoothSettingsEnabled() {
+  return base::FeatureList::IsEnabled(kFastPairDevicesBluetoothSettings);
+}
+
 bool IsFastPairHandshakeRefactorEnabled() {
   return base::FeatureList::IsEnabled(kFastPairHandshakeRefactor);
 }
