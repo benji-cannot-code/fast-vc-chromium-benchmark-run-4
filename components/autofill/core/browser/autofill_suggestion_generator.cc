2df438bb81fc694cbee039a1b92b669532e68079
Jihad Hanna <jihadghanna@google.com>
2023-07-13 16:29:18 +0000

    Discard non-fillable fields in AutofillSuggestionGenerator.
    During suggestion generation, Autofill uses the field types of the
    form's fields in order to deduplicate suggestions and generate labels.
    
    The problem is that the whole form's field types are considered, whereas
    there could be fields that won't be filled by the filling operation, for
    example if some field was already autofilled.
    
    This CL addresses this problem by making use of BAM::GetSkipStatuses.
    
    Fixed: 1430697
    Change-Id: I718f735adb10d331f440ab54b12115517f6bba31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4343529
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169977}

--- components/autofill/core/browser/autofill_suggestion_generator.cc
+++ components/autofill/core/browser/autofill_suggestion_generator.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_filler.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
+#include "components/autofill/core/browser/metrics/log_event.h"
 #include "components/autofill/core/browser/metrics/payments/card_metadata_metrics.h"
 #include "components/autofill/core/browser/payments/autofill_offer_manager.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
@@ -111,15 +112,14 @@ std::vector<Suggestion> AutofillSuggestionGenerator::GetSuggestionsForProfiles(
     const FormStructure& form,
     const FormFieldData& field,
     AutofillType field_type,
-    AutofillSuggestionTriggerSource trigger_source,
+    base::span<SkipStatus> skip_statuses,
     const std::string& app_locale) {
   std::vector<ServerFieldType> field_types;
   field_types.reserve(form.field_count());
-  for (const std::unique_ptr<AutofillField>& form_field : form) {
-    if (!form_field->ShouldSuppressSuggestionsAndFillingByDefault() ||
-        trigger_source == AutofillSuggestionTriggerSource::
-                              kManualFallbackForAutocompleteUnrecognized) {
-      field_types.push_back(form_field->Type().GetStorableType());
+  CHECK_EQ(skip_statuses.size(), form.field_count());
+  for (size_t i = 0; i < form.field_count(); ++i) {
+    if (skip_statuses[i] == SkipStatus::kNotSkipped) {
+      field_types.push_back(form.field(i)->Type().GetStorableType());
     }
   }
 
