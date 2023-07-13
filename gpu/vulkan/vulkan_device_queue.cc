3d641e5f5995e473c0a29641e58815a985fb5f06
Shahbaz Youssefi <syoussefi@chromium.org>
2023-07-13 19:31:37 +0000

    Vulkan: Add finch experiment for VMA block size
    Bug: 1457255
    Change-Id: I470b525baf76eda6c53815fb9276c85afc2bf513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637302
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170085}

--- gpu/vulkan/vulkan_device_queue.cc
+++ gpu/vulkan/vulkan_device_queue.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/bits.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/stringprintf.h"
@@ -26,7 +28,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/vulkan_util.h"
 #include "ui/gl/gl_angle_util_vulkan.h"
 
+namespace features {
+// 4MB was picked for the size here by looking at memory usage of Android
+// apps and runs of DM. It seems to be a good compromise of not wasting
+// unused allocated space and not making too many small allocations. The
+// AMD allocator will start making blocks at 1/8 the max size and builds
+// up block size as needed before capping at the max set here.
+//
+// To understand the amount of fragmentation caused by this choice of 4MB as
+// the block size, a few finch experiments are run to try different block
+// sizes.
+BASE_FEATURE(kVulkanVMALargeHeapBlockSizeExperiment,
+             "VulkanVMALargeHeapBlockSizeExperiment",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+constexpr base::FeatureParam<int> kVulkanVMALargeHeapBlockSize{
+    &kVulkanVMALargeHeapBlockSizeExperiment, "VulkanVMALargeHeapBlockSize",
+    4 * 1024 * 1024};
+}  // namespace features
+
 namespace gpu {
+namespace {
+VkDeviceSize GetPreferredVMALargeHeapBlockSize() {
+  const VkDeviceSize block_size =
+      ::features::kVulkanVMALargeHeapBlockSize.Get();
+  DCHECK(base::bits::IsPowerOfTwo(block_size));
+  return block_size;
+}
+}  // anonymous namespace
 
 VulkanDeviceQueue::VulkanDeviceQueue(VkInstance vk_instance)
     : vk_instance_(vk_instance) {}
@@ -311,8 +339,9 @@ bool VulkanDeviceQueue::Initialize(
       VK_MAX_MEMORY_HEAPS,
       heap_memory_limit ? heap_memory_limit : VK_WHOLE_SIZE);
   vma::CreateAllocator(vk_physical_device_, vk_device_, vk_instance_,
-                       enabled_extensions_, heap_size_limit.data(),
-                       is_thread_safe, &owned_vma_allocator_);
+                       enabled_extensions_, GetPreferredVMALargeHeapBlockSize(),
+                       heap_size_limit.data(), is_thread_safe,
+                       &owned_vma_allocator_);
   vma_allocator_ = owned_vma_allocator_;
 
   cleanup_helper_ = std::make_unique<VulkanFenceHelper>(this);
@@ -346,7 +375,9 @@ bool VulkanDeviceQueue::InitCommon(VkPhysicalDevice vk_physical_device,
 
   if (vma_allocator_ == VK_NULL_HANDLE) {
     vma::CreateAllocator(vk_physical_device_, vk_device_, vk_instance_,
-                         enabled_extensions_, /*heap_size_limit=*/nullptr,
+                         enabled_extensions_,
+                         GetPreferredVMALargeHeapBlockSize(),
+                         /*heap_size_limit=*/nullptr,
                          /*is_thread_safe =*/false, &owned_vma_allocator_);
     vma_allocator_ = owned_vma_allocator_;
   }
