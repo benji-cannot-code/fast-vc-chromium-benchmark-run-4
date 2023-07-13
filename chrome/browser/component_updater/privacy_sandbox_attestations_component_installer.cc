afb31468009d76ba8c5cf927515f8a892642f33d
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-07-13 17:17:15 +0000

    [Privacy Sandbox] Rename attestations file.
    The attestations file name is renamed to
    "privacy-sandbox-attestations.dat" to match the one produced from the
    upstream server.
    
    Bug: 1448875, 1464594
    Change-Id: I3d5d63f37084c003048b4286647fbbbffc3071f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679653
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170013}

--- chrome/browser/component_updater/privacy_sandbox_attestations_component_installer.cc
+++ chrome/browser/component_updater/privacy_sandbox_attestations_component_installer.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 constexpr base::FilePath::CharType kPrivacySandboxAttestationsFileName[] =
-    FILE_PATH_LITERAL("attestations");
+    FILE_PATH_LITERAL("privacy-sandbox-attestations.dat");
 
 constexpr base::FilePath::CharType
     kPrivacySandboxAttestationsRelativeInstallDir[] =
