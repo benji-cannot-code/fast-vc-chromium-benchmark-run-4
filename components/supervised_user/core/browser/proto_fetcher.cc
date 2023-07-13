9135400ba5e758b7d5f662f7a223840124011bc7
Tomasz Jurkiewicz <tju@google.com>
2023-07-13 13:34:06 +0000

    Remove CHECK on payload emptiness in GET requests, replace it with a warning.
    empty is proto2 implementation detail, and should not crash a browser.
    In GET request scenario, the payload_ is just ignored instead.
    
    ProtoFetcher: :payload_ is a serialized proto. Whether or not it is
    Bug: b:287470792
    Change-Id: I4e82e1746d5c638c64b97c0842a1221e3e0f108c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681643
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169889}

--- components/supervised_user/core/browser/proto_fetcher.cc
+++ components/supervised_user/core/browser/proto_fetcher.cc
@@ -150,7 +150,7 @@ class FetcherImpl final : public ProtoFetcher<Response> {
 
   FetcherImpl(IdentityManager& identity_manager,
               scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-              const std::string& payload,
+              StringPiece payload,
               const FetcherConfig& fetcher_config,
               Callback callback)
       : fetcher_(LaunchFetcher(identity_manager,
@@ -251,7 +251,6 @@ class FetcherImpl final : public ProtoFetcher<Response> {
   // Returns payload when it's eligible for the request type.
   absl::optional<std::string> GetRequestPayload() const {
     if (config_.method == FetcherConfig::Method::kGet) {
-      CHECK(payload_.empty()) << "Unexpected payload in GET request";
       return absl::nullopt;
     }
     return payload_;
