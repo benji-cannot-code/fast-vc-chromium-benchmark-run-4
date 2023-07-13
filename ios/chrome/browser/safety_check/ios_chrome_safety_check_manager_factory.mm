db57fb370c96b452db7963c20a685cc9c2825de5
Benjamin Williams <bwwilliams@google.com>
2023-07-13 19:00:07 +0000

    [iOS] Implement Safe Browsing Check in new Safety Check Manager.
    This change does the following:
    
    1. Implements the Safe Browsing Check in the new Safety Check Manager.
    
    2. Makes the new Safety Check Manager conform to base::SupportsWeakPtr.
    
    3. Unit tests the Safe Browsing Check logic.
    
    Change-Id: Idd93b566c522a5f41551a1a39d039de4f9e9b008
    Fixed: 1463258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673667
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170069}

--- ios/chrome/browser/safety_check/ios_chrome_safety_check_manager_factory.mm
+++ ios/chrome/browser/safety_check/ios_chrome_safety_check_manager_factory.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/keyed_service/core/keyed_service.h"
 #import "components/keyed_service/ios/browser_state_dependency_manager.h"
+#import "components/prefs/pref_service.h"
 #import "ios/chrome/browser/safety_check/ios_chrome_safety_check_manager.h"
 #import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
@@ -44,17 +45,14 @@ IOSChromeSafetyCheckManagerFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
   CHECK(IsSafetyCheckMagicStackEnabled());
 
-  // NOTE: In fast-follow CL, `ChromeBrowserState::FromBrowserState(context)`
-  // will be used to pass necessary services to `IOSChromeSafetyCheckManager`.
+  ChromeBrowserState* browser_state =
+      ChromeBrowserState::FromBrowserState(context);
 
-  return std::make_unique<IOSChromeSafetyCheckManager>();
+  return std::make_unique<IOSChromeSafetyCheckManager>(
+      browser_state->GetPrefs());
 }
 
 web::BrowserState* IOSChromeSafetyCheckManagerFactory::GetBrowserStateToUse(
     web::BrowserState* context) const {
   return GetBrowserStateRedirectedInIncognito(context);
-}
-
-bool IOSChromeSafetyCheckManagerFactory::ServiceIsNULLWhileTesting() const {
-  return true;
 }
