851208a8d530792e4fe065883940beaf2958a3b7
Chris Fredrickson <cfredric@chromium.org>
2023-07-13 14:27:16 +0000

    Stop immediately discarding scoped_refptrs from CookieSettingsFactory
    Calling `.get()` on the scoped_refptr immediately, without storing it
    in a local variable, allows the pointee to potentially be freed by
    ~scoped_refptr, so there's no guarantee that the pointer isn't dangling
    after the full expression containing `.get()` has been evaluated.
    
    The `.get()` call in PrivacySandboxSettingsFactory is especially
    dangerous (IIUC), since it discards the first scoped_refptr, then
    immediately constructs another scoped_refptr (implicitly). That
    pointer is never vended out by PrivacySandboxSettingsImpl, so its
    reference count will be 1 until ~PrivacySandboxSettingsImpl runs,
    at which point the underlying CookieSettings will be freed (even
    if other consumers still hold scoped_refptrs to it!).
    
    Change-Id: I24d1f800ef0f62f921c15e3acbd046b40f1eee3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4682856
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169914}

--- chrome/browser/signin/chrome_signin_client.cc
+++ chrome/browser/signin/chrome_signin_client.cc
@@ -126,9 +126,9 @@ void ChromeSigninClient::DoFinalInit() {
 
 // static
 bool ChromeSigninClient::ProfileAllowsSigninCookies(Profile* profile) {
-  content_settings::CookieSettings* cookie_settings =
-      CookieSettingsFactory::GetForProfile(profile).get();
-  return signin::SettingsAllowSigninCookies(cookie_settings);
+  scoped_refptr<content_settings::CookieSettings> cookie_settings =
+      CookieSettingsFactory::GetForProfile(profile);
+  return signin::SettingsAllowSigninCookies(cookie_settings.get());
 }
 
 PrefService* ChromeSigninClient::GetPrefs() { return profile_->GetPrefs(); }
@@ -152,9 +152,9 @@ bool ChromeSigninClient::AreSigninCookiesAllowed() {
 }
 
 bool ChromeSigninClient::AreSigninCookiesDeletedOnExit() {
-  content_settings::CookieSettings* cookie_settings =
-      CookieSettingsFactory::GetForProfile(profile_).get();
-  return signin::SettingsDeleteSigninCookiesOnExit(cookie_settings);
+  scoped_refptr<content_settings::CookieSettings> cookie_settings =
+      CookieSettingsFactory::GetForProfile(profile_);
+  return signin::SettingsDeleteSigninCookiesOnExit(cookie_settings.get());
 }
 
 void ChromeSigninClient::AddContentSettingsObserver(
