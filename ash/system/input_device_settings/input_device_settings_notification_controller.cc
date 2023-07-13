ebe4d8982cfb16027ab5364f308e5fed75f7146f
Michael Checo <michaelcheco@google.com>
2023-07-13 13:06:05 +0000

    [keyboard-alt]: Show each 6 pack key notification up to 3 times
    Bug: b/279503977
    Change-Id: I0acb884d8263a763dc9c860474e0d09ced929d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651524
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169876}

--- ash/system/input_device_settings/input_device_settings_notification_controller.cc
+++ ash/system/input_device_settings/input_device_settings_notification_controller.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/model/system_tray_model.h"
 #include "base/check_op.h"
+#include "base/containers/fixed_flat_map.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -38,6 +39,22 @@ namespace {
 
 using SimulateRightClickModifier = ui::mojom::SimulateRightClickModifier;
 using SixPackShortcutModifier = ui::mojom::SixPackShortcutModifier;
+
+constexpr auto kSixPackKeyToPrefName =
+    base::MakeFixedFlatMap<ui::KeyboardCode, const char*>({
+        {ui::KeyboardCode::VKEY_DELETE,
+         {prefs::kSixPackKeyDeleteNotificationsRemaining}},
+        {ui::KeyboardCode::VKEY_HOME,
+         {prefs::kSixPackKeyHomeNotificationsRemaining}},
+        {ui::KeyboardCode::VKEY_PRIOR,
+         {prefs::kSixPackKeyPageUpNotificationsRemaining}},
+        {ui::KeyboardCode::VKEY_END,
+         {prefs::kSixPackKeyEndNotificationsRemaining}},
+        {ui::KeyboardCode::VKEY_NEXT,
+         {prefs::kSixPackKeyPageDownNotificationsRemaining}},
+        {ui::KeyboardCode::VKEY_INSERT,
+         {prefs::kSixPackKeyInsertNotificationsRemaining}},
+    });
 
 const char kNotifierId[] = "input_device_settings_controller";
 const char kAltRightClickRewriteNotificationId[] =
@@ -111,9 +128,9 @@ bool IsActiveUserSession() {
 
 // If the user has reached the settings page through the notification, do
 // not show any more new notifications.
-void StopShowingRightClickNotification() {
+void StopShowingNotification(const char* pref_name) {
   Shell::Get()->session_controller()->GetActivePrefService()->SetInteger(
-      prefs::kRemapToRightClickNotificationsRemaining, 0);
+      pref_name, 0);
 }
 
 bool ShouldShowSixPackKeyNotification() {
@@ -188,9 +205,28 @@ InputDeviceSettingsNotificationController::
 
 void InputDeviceSettingsNotificationController::RegisterProfilePrefs(
     PrefRegistrySimple* pref_registry) {
-  // We'll show the remap to right click notification a total of three times.
+  // We'll show the remap to right click and Six Pack keys notifications a
+  // total of three times each.
   pref_registry->RegisterIntegerPref(
       prefs::kRemapToRightClickNotificationsRemaining, 3,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  pref_registry->RegisterIntegerPref(
+      prefs::kSixPackKeyDeleteNotificationsRemaining, 3,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  pref_registry->RegisterIntegerPref(
+      prefs::kSixPackKeyHomeNotificationsRemaining, 3,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  pref_registry->RegisterIntegerPref(
+      prefs::kSixPackKeyEndNotificationsRemaining, 3,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  pref_registry->RegisterIntegerPref(
+      prefs::kSixPackKeyPageUpNotificationsRemaining, 3,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  pref_registry->RegisterIntegerPref(
+      prefs::kSixPackKeyPageDownNotificationsRemaining, 3,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  pref_registry->RegisterIntegerPref(
+      prefs::kSixPackKeyInsertNotificationsRemaining, 3,
       user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
 }
 
@@ -225,7 +261,8 @@ void InputDeviceSettingsNotificationController::
                   ->system_tray_model()
                   ->client()
                   ->ShowTouchpadSettings();
-              StopShowingRightClickNotification();
+              StopShowingNotification(
+                  prefs::kRemapToRightClickNotificationsRemaining);
             }
           }));
   auto notification = CreateSystemNotificationPtr(
@@ -257,16 +294,30 @@ void InputDeviceSettingsNotificationController::
   }
   CHECK_NE(blocked_modifier, SixPackShortcutModifier::kNone);
   CHECK(ui::KeyboardCapability::IsSixPackKey(key_code));
+
+  auto* it = kSixPackKeyToPrefName.find(key_code);
+  CHECK(it != kSixPackKeyToPrefName.end());
+  const char* pref = it->second;
+  PrefService* prefs =
+      Shell::Get()->session_controller()->GetActivePrefService();
+  CHECK(prefs);
+  int num_notifications_remaining = prefs->GetInteger(pref);
+  if (num_notifications_remaining == 0) {
+    return;
+  }
+  prefs->SetInteger(pref, num_notifications_remaining - 1);
+
   auto on_click_handler =
       base::MakeRefCounted<message_center::HandleNotificationClickDelegate>(
           base::BindRepeating(
-              [](int device_id) {
+              [](int device_id, const char* pref_name) {
                 Shell::Get()
                     ->system_tray_model()
                     ->client()
                     ->ShowRemapKeysSubpage(device_id);
+                StopShowingNotification(pref_name);
               },
-              device_id));
+              device_id, pref));
   auto notification = CreateSystemNotificationPtr(
       message_center::NOTIFICATION_TYPE_SIMPLE,
       GetSixPackNotificationId(key_code, device_id),
