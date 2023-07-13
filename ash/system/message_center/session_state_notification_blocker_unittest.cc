7174b53675390f9088769f156ab3d6a60902cd1c
Melissa Zhang <melzhang@chromium.org>
2023-07-13 05:20:48 +0000

    Revert "Update notification state when blocker is added"
    This reverts commit 93a845736c3c61c61a511dadbcdb6c8042406e0c.
    
    Reason for revert: Breaking https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/34339/overview
    
    Original change's description:
    > Update notification state when blocker is added
    >
    > Treat the addition of a new `NotificationBlocker` as a state-change
    > event, as if `NotificationBlocker::Observer::OnBlockingStateChanged()`
    > were called. Create a new `NotificationBlocker::Init()` method that must
    > be called after a `NotificationBlocker`'s ctor to avoid calling a
    > virtual function during construction. Update existing usages to the new
    > initialization pattern.
    >
    > Fixed: b:286590651
    > Change-Id: I3ed5a1a818d0c086937bf676a47854113fdd5058
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4656056
    > Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    > Commit-Queue: Elliot Tuck <etuck@chromium.org>
    > Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    > Reviewed-by: David Black <dmblack@google.com>
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1169593}
    
    Change-Id: I9ba42946067e2c461f1eb7ab7d26cb8ee233a7b1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681152
    Owners-Override: Melissa Zhang <melzhang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169749}

--- ash/system/message_center/session_state_notification_blocker_unittest.cc
+++ ash/system/message_center/session_state_notification_blocker_unittest.cc
@@ -57,7 +57,6 @@ class SessionStateNotificationBlockerTest
     NoSessionAshTestBase::SetUp();
     blocker_ = std::make_unique<SessionStateNotificationBlocker>(
         message_center::MessageCenter::Get());
-    blocker_->Init();
     blocker_->AddObserver(this);
   }
 
