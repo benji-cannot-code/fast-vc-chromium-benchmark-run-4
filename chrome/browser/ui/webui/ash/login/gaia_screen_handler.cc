33790d2765c793fb58668522b0141a73604579cc
Steven Bennetts <stevenjb@chromium.org>
2023-07-13 16:34:32 +0000

    Ash login: Deprecate ERROR_REASON_PORTAL_DETECTED
    Once Upon A Time, ChromeOS portal detection was handled by Chrome,
    independent of NetworkState. This requred a separate observer and
    error state.
    
    That code has been deprecated and removed, so
    ERROR_REASON_PORTAL_DETECTED is no longer set and should be removed.
    
    BUG=b:243405151
    TEST==unit_tests, browser_tests
    
    Change-Id: Icbe3810eb43dd816ffed556e5216b0e1f92cac46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681264
    Quick-Run: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Auto-Submit: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169981}

--- chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/gaia_screen_handler.cc
@@ -1426,8 +1426,6 @@ void GaiaScreenHandler::UpdateStateInternal(NetworkError::ErrorReason reason,
   connecting_callback_.Cancel();
 
   const bool is_online = NetworkStateInformer::IsOnline(state, reason);
-  const bool is_behind_captive_portal =
-      NetworkStateInformer::IsBehindCaptivePortal(state, reason);
   const bool is_gaia_loading_timeout =
       (reason == NetworkError::ERROR_REASON_LOADING_TIMEOUT);
   const bool is_gaia_error =
@@ -1449,7 +1447,7 @@ void GaiaScreenHandler::UpdateStateInternal(NetworkError::ErrorReason reason,
     return;
   }
 
-  if (is_online || !is_behind_captive_portal) {
+  if (!NetworkStateInformer::IsBehindCaptivePortal(state)) {
     error_screen_->HideCaptivePortal();
   }
 
