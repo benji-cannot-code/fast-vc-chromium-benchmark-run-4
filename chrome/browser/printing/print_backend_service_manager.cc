50eda765057b270ed61983c720d9ff499c24abb9
Alan Screen <awscreen@chromium.org>
2023-07-13 22:44:37 +0000

    Use UI message loop type for Linux Print Backend service
    Printing a document on Linux makes calls through the
    PrintDialogLinuxInterface.  Calls made through this interface can
    appear to succeed but actually fail to run if there is not a running
    message pump to process the commands issued.  This causes a false
    positive for printing status, with no errors reported but also no print
    job actually printing.
    
    Update the invocation of Linux to use a UI message loop type.  Also
    change the thread runner used on Linux to work with the default runner.
    This ensures that the printing commands actually run.
    
    Fixed: 1464683
    Change-Id: I69d27739bef732bd52e12473d6ea7ff7deef039a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683911
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170201}

--- chrome/browser/printing/print_backend_service_manager.cc
+++ chrome/browser/printing/print_backend_service_manager.cc
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/buildflags/buildflags.h"
 #include "printing/printing_features.h"
 
+#if BUILDFLAG(IS_LINUX)
+#include "content/public/common/content_switches.h"
+#endif
+
 #if BUILDFLAG(IS_WIN)
 #include "base/win/win_util.h"
 #include "chrome/browser/printing/printer_xml_parser_impl.h"
@@ -848,6 +852,9 @@ PrintBackendServiceManager::GetServiceFromBundle(
         host.BindNewPipeAndPassReceiver(),
         content::ServiceProcessHost::Options()
             .WithDisplayName(IDS_UTILITY_PROCESS_PRINT_BACKEND_SERVICE_NAME)
+#if BUILDFLAG(IS_LINUX)
+            .WithExtraCommandLineSwitches({switches::kMessageLoopTypeUi})
+#endif
             .Pass());
     host->BindBackend(service.BindNewPipeAndPassReceiver());
 
