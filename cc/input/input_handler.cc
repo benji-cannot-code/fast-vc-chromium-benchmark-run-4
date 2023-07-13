8b6da673f09f1b85d2157b1584fd9378528bba6c
David Awogbemila <awogbemila@chromium.org>
2023-07-13 20:23:46 +0000

    Fix overscroll delta calculation
    InputHandler::ScrollUpdate was only counting overscroll deltas where
    content did not move at all. This omitted deltas in cases
    where content moved but not all of the input delta was consumed.
    
    This is the cause of the flakiness for some WPTs so this patch updates
    TestExpectations and removes an obsolete expected.txt file.
    
    Bug: 1430921,1313311
    Change-Id: I32248abcbd1217b78b13f69799e6ced7a9d35e31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683472
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170110}

--- cc/input/input_handler.cc
+++ cc/input/input_handler.cc
@@ -398,9 +398,6 @@ InputHandlerScrollResult InputHandler::ScrollUpdate(
     bool is_animated_scroll = ShouldAnimateScroll(*scroll_state);
     compositor_delegate_->DidScrollContent(scroll_node.element_id,
                                            is_animated_scroll);
-  } else {
-    overscroll_delta_for_main_thread_ +=
-        gfx::Vector2dF(scroll_state->delta_x(), scroll_state->delta_y());
   }
 
   SetNeedsCommit();
@@ -1866,6 +1863,7 @@ void InputHandler::ScrollLatchedScroller(ScrollState* scroll_state,
                                        scroll_state->is_direct_manipulation());
     }
   }
+  overscroll_delta_for_main_thread_ += delta - applied_delta;
 
   // If the layer wasn't able to move, try the next one in the hierarchy.
   bool scrolled = std::abs(applied_delta.x()) > kEpsilon;
