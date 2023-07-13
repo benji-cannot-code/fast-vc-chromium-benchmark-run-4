b0b8f56bfe140a43997bcef88d0485008be0e555
Luc Nguyen <lucnguyen@google.com>
2023-07-13 20:44:43 +0000

    Add traits to background task for periodic ongoing logs
    Explicitly add CONTINUE_ON_SHUTDOWN as a trait since there is no
    point in blocking shutdown since the reply task won't be run, and the
    work done on the background thread will have been done for nothing.
    
    Currently, the task uses default trait, which has a high priority
    (USER_BLOCKING). Explicitly add that as well.
    
    Bug: 1052796
    Change-Id: I37012113acc370e356cc1991b6f4abe000222bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684946
    Auto-Submit: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170123}

--- components/metrics/metrics_service.cc
+++ components/metrics/metrics_service.cc
@@ -143,6 +143,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -1060,6 +1061,15 @@ void MetricsService::CloseCurrentLog(
         log_histogram_writer.get();
     base::ThreadPool::PostTaskAndReplyWithResult(
         FROM_HERE,
+        // CONTINUE_ON_SHUTDOWN because the work done is only useful once the
+        // reply task is run (and there are no side effects). So, no need to
+        // block shutdown since the reply task won't be run anyway.
+        // NOTE: If attempting to change the USER_BLOCKING priority, do a study
+        // on the impact first since it might affect the number of logs being
+        // uploaded (which might have secondary effects, e.g. on metrics that
+        // rely on number of logs uploaded).
+        {base::TaskPriority::USER_BLOCKING,
+         base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
         base::BindOnce(&MetricsService::SnapshotUnloggedSamplesAndFinalizeLog,
                        log_histogram_writer_ptr, std::move(current_log),
                        /*truncate_events=*/true, std::move(close_time),
