21fde7b0f9e897d8058c79108e84ba9fd0b1f39b
Brando <brandosocarras@google.com>
2023-07-13 21:45:41 +0000

    [Nearby][Self Share] Add YourDevices to Visibility enum
    - Added to the Visibility enum
    - Added IsSelfShareEnabled()
    - Adjusted the files that operate on visibility to deal
      with Your Devices visibility if Self Share is enabled
    - Adjusted certificate mngr utests to test with and
      without Self Share enabled
    
    Tested: utests in this, no issues when deployed to DUT
    
    Fixed: b/279940095
    Change-Id: Ifb9c3582a8df81b7edde2225d71a0533447cbea7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4597992
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170157}

--- chrome/browser/nearby_sharing/common/nearby_share_features.cc
+++ chrome/browser/nearby_sharing/common/nearby_share_features.cc
@@ -40,4 +40,8 @@ BASE_FEATURE(kNearbySharingWifiLan,
              "NearbySharingWifiLan",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+bool IsSelfShareEnabled() {
+  return base::FeatureList::IsEnabled(kNearbySharingSelfShare);
+}
+
 }  // namespace features
