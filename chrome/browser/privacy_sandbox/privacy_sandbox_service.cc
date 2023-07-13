69f606b5518b2c01e54f9a5a14c645560fadeef2
Chris Fredrickson <cfredric@chromium.org>
2023-07-13 08:30:13 +0000

    Store a scoped_refptr instead of raw_ptr
    CookieSettingsFactory::GetForProfile returns a scoped_refptr. If
    `.get()` is called on that scoped_refptr immediately, then
    ~scoped_refptr is called as the pointer goes out of scope, which may
    result in the pointee being freed. Therefore, it is better to store
    the scoped_refptr directly, so that the pointee won't get accidentally
    freed while we hold a pointer to it.
    
    Change-Id: I278cdb3b805e7d1f57d63d3656f208c33a5a7dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678997
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169791}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service.cc
@@ -196,7 +196,7 @@ PrivacySandboxService::PrivacySandboxService() = default;
 
 PrivacySandboxService::PrivacySandboxService(
     privacy_sandbox::PrivacySandboxSettings* privacy_sandbox_settings,
-    content_settings::CookieSettings* cookie_settings,
+    scoped_refptr<content_settings::CookieSettings> cookie_settings,
     PrefService* pref_service,
     content::InterestGroupManager* interest_group_manager,
     profile_metrics::BrowserProfileType profile_type,
@@ -294,7 +294,7 @@ PrivacySandboxService::~PrivacySandboxService() = default;
 PrivacySandboxService::PromptType
 PrivacySandboxService::GetRequiredPromptType() {
   const auto third_party_cookies_blocked =
-      AreThirdPartyCookiesBlocked(cookie_settings_);
+      AreThirdPartyCookiesBlocked(cookie_settings_.get());
   if (base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4)) {
     return GetRequiredPromptTypeInternalM1(
         pref_service_, profile_type_, privacy_sandbox_settings_,
@@ -1518,7 +1518,7 @@ void PrivacySandboxService::MaybeInitializeFirstPartySetsPref() {
   // side of privacy, this init logic is run per-device (the pref recording that
   // init has been run is not synced). If any of the user's devices local state
   // would disable the pref, it is disabled across all devices.
-  if (AreThirdPartyCookiesBlocked(cookie_settings_)) {
+  if (AreThirdPartyCookiesBlocked(cookie_settings_.get())) {
     pref_service_->SetBoolean(prefs::kPrivacySandboxFirstPartySetsEnabled,
                               false);
   }
@@ -1539,7 +1539,7 @@ void PrivacySandboxService::MaybeInitializeAntiAbuseContentSetting() {
   // side of privacy, this init logic is run per-device (the pref recording that
   // init has been run is not synced). If any of the user's devices local state
   // would disable the setting, it is disabled across all devices.
-  if (AreThirdPartyCookiesBlocked(cookie_settings_)) {
+  if (AreThirdPartyCookiesBlocked(cookie_settings_.get())) {
     host_content_settings_map_->SetDefaultContentSetting(
         ContentSettingsType::ANTI_ABUSE, ContentSetting::CONTENT_SETTING_BLOCK);
   }
