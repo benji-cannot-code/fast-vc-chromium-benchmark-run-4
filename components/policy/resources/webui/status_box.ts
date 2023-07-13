b64aeabc35327eab20d7f521c9f2fd4312b20ae7
Aryan Kaushik <aryankaushik2023@gmail.com>
2023-07-13 04:42:13 +0000

    [Policy] Status Box enhancements
    The Status Box is created using fieldset, this CL transitions them to use Div instead.
    Fieldsets are rarely used and don't look good.
    They provide very few customisations and were primarily made to group form fields.
    
    The CL also beautifies them by adding background colour (same as search), rounded corners, consistent sizing, responsiveness and better - margins, padding, and font.
    
    Screenshots: https://drive.google.com/file/d/1yEfBn3Czb1CRAW_TPJz2_IptDEi-73iQ/view?usp=sharing
    
    Before screenshot: https://drive.google.com/file/d/1ACKL9JPy06i_LCLU4ad736aBZyXBPYAz/view?usp=sharing
    
    R=esalma@google.com
    
    Change-Id: If202b1734f9664cfcf6c746bc0d52d99044c7924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660200
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Aryan Kaushik <aryankaushik2023@gmail.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169742}

--- components/policy/resources/webui/status_box.ts
+++ components/policy/resources/webui/status_box.ts
@@ -77,8 +77,8 @@ export class StatusBoxElement extends CustomElement {
   initialize(scope: string, status: Status) {
     const notSpecifiedString = loadTimeData.getString('notSpecified');
 
-    // Set appropriate box legend based on status key
-    this.shadowRoot!.querySelector('.legend')!.textContent =
+    // Set appropriate box heading based on status key.
+    this.shadowRoot!.querySelector('.status-box-heading')!.textContent =
         loadTimeData.getString(status.policyDescriptionKey);
     if (status.flexOrgWarning) {
       this.setLabelInnerHtmlAndShow(
