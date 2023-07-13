711a6de72bc1297b9798fd082d9c2327c68d5b90
Leon Masopust <lmasopust@google.com>
2023-07-13 12:57:41 +0000

    DTC-BYOD: Prepare attestation key when DTC is enabled
    Try to create a DEVICE_TRUST_CONNECTOR_USER_CERTIFICATE when DTC is
    enabled. This is done using a
    `DeviceTrustConnectorService::PolicyObserver`. Since an error during the
    certificate creation will only lead to an increased latency during the
    first DTC flow, the service just adds an error to the log.
    
    Bug: b:284454044
    Change-Id: I390d097eea8aaff934eb5b15463c43b416fc4f4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637266
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Leon Masopust <lmasopust@google.com>
    Reviewed-by: Martin Rodriguez <rodmartin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169870}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_service_factory.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_service_factory.cc
@@ -46,7 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/enterprise/connectors/device_trust/attestation/ash/ash_attestation_service.h"
+#include "chrome/browser/enterprise/connectors/device_trust/ash/ash_attestation_policy_observer.h"
+#include "chrome/browser/enterprise/connectors/device_trust/attestation/ash/ash_attestation_service_impl.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 namespace {
@@ -143,8 +144,13 @@ KeyedService* DeviceTrustServiceFactory::BuildServiceInstanceFor(
   }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+  std::unique_ptr<AshAttestationServiceImpl> ash_attestation_service =
+      std::make_unique<AshAttestationServiceImpl>(profile);
+  dt_connector_service->AddObserver(
+      std::make_unique<AshAttestationPolicyObserver>(
+          ash_attestation_service->GetWeakPtr()));
   std::unique_ptr<AttestationService> attestation_service =
-      std::make_unique<AshAttestationService>(profile);
+      std::move(ash_attestation_service);
 #else
   DeviceTrustKeyManager* key_manager = nullptr;
   policy::CloudPolicyStore* browser_cloud_policy_store = nullptr;
