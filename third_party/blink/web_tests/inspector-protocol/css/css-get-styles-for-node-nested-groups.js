a271362780a1d111df3b878088f654d18f06569e
Ergün Erdoğmuş <ergunsh@chromium.org>
2023-07-13 22:53:41 +0000

    [Inspector] Skip adding source data for non-existent implicit nested groups
    See the bug for the reproduction and issue description.
    
    The crux of the issue lies in nested group rules[1] and how the parser notifies the observer when they encounter such rules.
    
    Take the CSS below as example:
    ```
    .foo {
        @supports(display: flex) {
            & {
               color: red;
            }
        }
    }
    ```
    
    While parsing the body of the `@supports` rule above, CSS parser notifies the observer that this might be the start of the implicit nested group with a zero length header range[2].
    
    However, if there are no properties for this implicit nested group, CSS parser doesn't register it so such a group doesn't exist for the rule list got from the parser[3].
    
    in this CL, to match the same behavior with the CSS parser while registering the source data, we check whether there are any properties parsed for this possible "implicit nested group".
    
    If not, we skip adding  the rule itself and only take its child rules' data.
    
    [1]: https://drafts.csswg.org/css-nesting-1/#nested-group-rules
    [2]: https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:third_party/blink/renderer/core/css/parser/css_parser_impl.cc;l=2122;drc=255b4e7036f1326f2219bd547d3d6dcf76064870
    [3]: https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:third_party/blink/renderer/core/css/parser/css_parser_impl.cc;l=2131;drc=255b4e7036f1326f2219bd547d3d6dcf76064870
    
    Fixed: 1445720
    Change-Id: Ifbd2e85873374929d0ceadf4fb5905eaf4ddd62b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681759
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170208}

---
+++ third_party/blink/web_tests/inspector-protocol/css/css-get-styles-for-node-nested-groups.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startHTML(`
+  <style>
+  #no-properties {
+    @supports (display: flex) {
+      & {
+        color: red;
+      }
+    }
+  }
+
+  #with-properties {
+    @supports (display: flex) {
+      color: blue;
+      & {
+        color: red;
+      }
+    }
+  }
+  </style>
+  <div id='no-properties'></div>
+  <div id='with-properties'></div>`,
+'The test verifies functionality of protocol method CSS.getMatchedStylesForNode for nested groups.');
+
+  await dp.DOM.enable();
+  await dp.CSS.enable();
+
+  const CSSHelper = await testRunner.loadScript('../resources/css-helper.js');
+  const cssHelper = new CSSHelper(testRunner, dp);
+
+  const documentNodeId = await cssHelper.requestDocumentNodeId();
+  // Test that when there are no properties inside a nested rule, it won't have an
+  // empty CSS rule.
+  testRunner.log("There shouldn't be an empty rule for #no-properties");
+  await cssHelper.loadAndDumpInlineAndMatchingRules(documentNodeId, '#no-properties');
+
+  testRunner.log("\nThere should be a rule for implicit nested group.");
+  await cssHelper.loadAndDumpInlineAndMatchingRules(documentNodeId, '#with-properties');
+
+  testRunner.completeTest();
+});
