41a8230ccbf58d2985962a913dee2ca96851b94c
Andre Le <leandre@chromium.org>
2023-07-13 18:29:56 +0000

    VC UI: Retrieve media apps in VideoConferenceTray
    With GuestOS, we need to know if there's a linux media apps exist to
    choose which view to render for the bubble view. Thus, we need to call
    GetMediaApps() right in VideoConferenceTray instead of in
    ReturnToAppPanel. Future CLs will use this media apps info in
    VideoConferenceTray to render the appropriate views.
    
    Bug: b:273820331
    Change-Id: I20be4bc45fb1efb12e6634435a4a61a9a2e87e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4669572
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170055}

--- ash/system/video_conference/bubble/bubble_view.h
+++ ash/system/video_conference/bubble/bubble_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/tray/tray_bubble_view.h"
 #include "base/memory/raw_ptr.h"
+#include "chromeos/crosapi/mojom/video_conference.mojom-forward.h"
 
 namespace views {
 class View;
@@ -19,15 +20,18 @@ class VideoConferenceTrayController;
 
 namespace video_conference {
 
+using MediaApps = std::vector<crosapi::mojom::VideoConferenceMediaAppInfoPtr>;
+
 // The bubble that contains controls for camera and microphone effects,
 // and for easy navigation to apps performing video/audio capture.
 class BubbleView : public TrayBubbleView {
  public:
   explicit BubbleView(const InitParams& init_params,
+                      const MediaApps& media_apps,
                       VideoConferenceTrayController* controller);
   BubbleView(const BubbleView&) = delete;
   BubbleView& operator=(const BubbleView&) = delete;
-  ~BubbleView() override = default;
+  ~BubbleView() override;
 
   // views::View:
   void AddedToWidget() override;
@@ -39,6 +43,8 @@ class BubbleView : public TrayBubbleView {
  private:
   // Unowned by `BubbleView`.
   raw_ptr<VideoConferenceTrayController, ExperimentalAsh> controller_;
+
+  const MediaApps& media_apps_;
 };
 
 }  // namespace video_conference
