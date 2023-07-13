d7ce50cb9fd05f81635e945ac8877c68fd614c80
Adem Derinel <derinel@google.com>
2023-07-13 06:34:11 +0000

    [Passwords] Remove InferConfirmationPasswordField flag
    Bug: 1164861, 1464469
    Change-Id: Ic8f562015552d6f1550c0cac1815979c2b5c62b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679082
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169769}

--- components/password_manager/core/browser/form_parsing/form_parser_unittest.cc
+++ components/password_manager/core/browser/form_parsing/form_parser_unittest.cc
@@ -11,13 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
-#include "build/build_config.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/form_field_data.h"
@@ -1260,8 +1258,6 @@ TEST_F(FormParserTest, ServerPredictionsForClearTextPasswordFields) {
 }
 
 TEST_F(FormParserTest, InferConfirmationPasswordField) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kInferConfirmationPasswordField);
   CheckTestData({
       {
           .description_for_logging = "Infer confirmation password during "
