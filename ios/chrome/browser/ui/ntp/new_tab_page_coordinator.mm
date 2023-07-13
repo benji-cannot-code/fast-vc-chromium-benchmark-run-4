f7819d4889f314fad66cd453e67c622672258f2d
Victor Vianna <victorvianna@google.com>
2023-07-13 19:16:16 +0000

    [ios] Change flag checked by new bottom-of-feed UI
    Check kFeedBottomSyncStringRemoval instead of kReplaceSyncPromosWithSignInPromos.
    
    Bug: 1447012, b/288291183
    Change-Id: I3441231d0a977a584f55fd1a3cb116f2eb4be6ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684207
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170077}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/time/time.h"
 #import "components/feed/core/v2/public/common_enums.h"
 #import "components/feed/core/v2/public/ios/pref_names.h"
+#import "components/feed/feed_feature_list.h"
 #import "components/policy/policy_constants.h"
 #import "components/pref_registry/pref_registry_syncable.h"
 #import "components/prefs/ios/pref_observer_bridge.h"
@@ -1109,7 +1110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   id<ApplicationCommands> handler = HandlerForProtocol(
       self.browser->GetCommandDispatcher(), ApplicationCommands);
   AuthenticationOperation operation =
-      base::FeatureList::IsEnabled(syncer::kReplaceSyncPromosWithSignInPromos)
+      base::FeatureList::IsEnabled(feed::kFeedBottomSyncStringRemoval)
           ? AuthenticationOperationSigninOnly
           : AuthenticationOperationSigninAndSync;
   ShowSigninCommand* command =
