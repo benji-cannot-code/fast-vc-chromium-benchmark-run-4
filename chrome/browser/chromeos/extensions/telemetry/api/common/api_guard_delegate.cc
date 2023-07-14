f769bdb8dc4bff0c7f8ba746a6ae119e0b684478
Ethan Cheng <yycheng@google.com>
2023-07-14 12:17:10 +0000

    [Telemetry] Enable IWA to use Extension API
    The SecurityLevel check was designed to enforce https and check that
    requests are from expected server by checking https certificate.
    This doesn't apply to IWA since IWA resource is signed local packages,
    the context is already verified while signing. We might be able to
    remove this special cases check here if SecurityLevel::SECURE also
    support IWA as well, which is discussing in b/290909386.
    
    Bug: b/290911225
    Test: None
    Change-Id: I3b939b5c38e47302f3783be53aa736b0724798f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680946
    Commit-Queue: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170456}

--- chrome/browser/chromeos/extensions/telemetry/api/common/api_guard_delegate.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/common/api_guard_delegate.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chromeos/extensions/chromeos_system_extension_info.h"
+#include "chrome/common/url_constants.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "components/security_state/content/content_utils.h"
 #include "components/security_state/core/security_state.h"
 #include "content/public/browser/web_contents.h"
@@ -145,6 +147,13 @@ class ApiGuardDelegateImplBase : public ApiGuardDelegate {
           // Ensure the PWA URL connection is secure (e.g. valid certificate).
           const auto visible_security_state =
               security_state::GetVisibleSecurityState(target_contents);
+          // TODO(b/290909386): Remove this line once we reach a conclusion on
+          // how we should perform security check on IWA.
+          if (chromeos::features::IsIWAForTelemetryExtensionAPIEnabled() &&
+              target_contents->GetLastCommittedURL().SchemeIs(
+                  chrome::kIsolatedAppScheme)) {
+            return true;
+          }
           return security_state::GetSecurityLevel(
                      *visible_security_state,
                      /*used_policy_installed_certificate=*/false) ==
