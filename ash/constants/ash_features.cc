d4b90da9ec0554923bbeadf9b0f61b2f332995ce
Andrew Rayskiy <greengrape@google.com>
2023-07-14 11:45:38 +0000

    [Shelf] Add kRemoveStalePolicyPinnedAppsFromShelf flag to ash
    This flag enables the shelf controller to unpin apps that were once
    introduced by PinnedLauncherApps policy but are no longer a part of it.
    
    Bug: b/289197373
    Change-Id: Ifc749bfa60328e5f18efb795b91c120f4aa41fdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4687050
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170444}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2089,6 +2089,12 @@ BASE_FEATURE(kRenderArcNotificationsByChrome,
              "RenderArcNotificationsByChrome",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Allows the OS to unpin apps that were pinned by PinnedLauncherApps policy
+// but are no longer a part of it from shelf under specific conditions.
+BASE_FEATURE(kRemoveStalePolicyPinnedAppsFromShelf,
+             "RemoveStalePolicyPinnedAppsFromShelf",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables ChromeOS scalable IPH.
 BASE_FEATURE(kScalableIph, "ScalableIph", base::FEATURE_DISABLED_BY_DEFAULT);
 
@@ -3604,6 +3610,10 @@ bool IsPerDeskZOrderEnabled() {
 
 bool IsRenderArcNotificationsByChromeEnabled() {
   return base::FeatureList::IsEnabled(kRenderArcNotificationsByChrome);
+}
+
+bool IsRemoveStalePolicyPinnedAppsFromShelfEnabled() {
+  return base::FeatureList::IsEnabled(kRemoveStalePolicyPinnedAppsFromShelf);
 }
 
 bool IsSameAppWindowCycleEnabled() {
