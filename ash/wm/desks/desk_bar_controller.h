9da0ca9b5f45eed3f7bcb2ccf07c3e643c41604f
Yongshun Liu <yongshun@chromium.org>
2023-07-14 06:40:40 +0000

    bento_button: Fix the bar widget bounds for display changes
    This makes sure that the bar widget shows up with the expected bounds
    for display changes, such as update in device scale, screen rotation,
    chromevox, and docked magnifier.
    
    Bug: b/290977542
    Test: DeskBarTest.{BarBoundsWithDeviceSacleFactorChange,BarBoundsWithScreenRotationChange,BarBoundsWithWorkAreaChangeChromevox,BarBoundsWithWorkAreaChangeDockedMagnifier}
    Change-Id: I390773344b80c38c418d8a2d36856721f4cc9d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679889
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170361}

--- ash/wm/desks/desk_bar_controller.h
+++ ash/wm/desks/desk_bar_controller.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desk_button/desk_button.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/overview/overview_observer.h"
+#include "ui/display/display_observer.h"
 #include "ui/events/event_handler.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/widget/widget.h"
@@ -36,7 +37,8 @@ class ASH_EXPORT DeskBarController : public DesksController::Observer,
                                      public OverviewObserver,
                                      public ShellObserver,
                                      public TabletModeObserver,
-                                     public wm::ActivationChangeObserver {
+                                     public wm::ActivationChangeObserver,
+                                     public display::DisplayObserver {
  public:
   DeskBarController();
 
@@ -66,6 +68,10 @@ class ASH_EXPORT DeskBarController : public DesksController::Observer,
   void OnWindowActivated(ActivationReason reason,
                          aura::Window* gained_active,
                          aura::Window* lost_active) override;
+
+  // display::DisplayObserver:
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t changed_metrics) override;
 
   // Returns desk bar view in `root`. If there is no such desk bar, nullptr is
   // returned.
@@ -159,6 +165,9 @@ class ASH_EXPORT DeskBarController : public DesksController::Observer,
   // desk button desk bar. Support for overview desk bar will be added later.
   std::vector<std::unique_ptr<views::Widget>> desk_bar_widgets_;
   std::vector<DeskBarViewBase*> desk_bar_views_;
+
+  // Observes display configuration changes.
+  display::ScopedDisplayObserver display_observer_{this};
 
   // Indicates that shell is destroying.
   bool is_shell_destroying = false;
