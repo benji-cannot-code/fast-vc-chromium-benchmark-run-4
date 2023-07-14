166280d20d1bc7b1979b894b2932ef0b58d90d6e
Jeremy Roman <jbroman@chromium.org>
2023-07-14 21:34:18 +0000

    Speculation Rules: don't react to trivial changes in anchor attributes.
    This resolves a secondary issue noticed in the linked bug.
    
    Bug: 1464696
    Change-Id: I034782bf1faf69730dd68a29bcd01dc83e04dc8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4687858
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170721}

--- third_party/blink/renderer/core/html/html_anchor_element.cc
+++ third_party/blink/renderer/core/html/html_anchor_element.cc
@@ -274,7 +274,7 @@ void HTMLAnchorElement::ParseAttribute(
       PseudoStateChanged(CSSSelector::kPseudoWebkitAnyLink);
       PseudoStateChanged(CSSSelector::kPseudoAnyLink);
     }
-    if (isConnected()) {
+    if (isConnected() && params.old_value != params.new_value) {
       if (auto* document_rules =
               DocumentSpeculationRules::FromIfExists(GetDocument())) {
         document_rules->HrefAttributeChanged(this, params.old_value,
@@ -289,21 +289,21 @@ void HTMLAnchorElement::ParseAttribute(
   } else if (params.name == html_names::kRelAttr) {
     SetRel(params.new_value);
     rel_list_->DidUpdateAttributeValue(params.old_value, params.new_value);
-    if (isConnected() && IsLink()) {
+    if (isConnected() && IsLink() && params.old_value != params.new_value) {
       if (auto* document_rules =
               DocumentSpeculationRules::FromIfExists(GetDocument())) {
         document_rules->RelAttributeChanged(this);
       }
     }
   } else if (params.name == html_names::kReferrerpolicyAttr) {
-    if (isConnected() && IsLink()) {
+    if (isConnected() && IsLink() && params.old_value != params.new_value) {
       if (auto* document_rules =
               DocumentSpeculationRules::FromIfExists(GetDocument())) {
         document_rules->ReferrerPolicyAttributeChanged(this);
       }
     }
   } else if (params.name == html_names::kTargetAttr) {
-    if (isConnected() && IsLink()) {
+    if (isConnected() && IsLink() && params.old_value != params.new_value) {
       if (auto* document_rules =
               DocumentSpeculationRules::FromIfExists(GetDocument())) {
         document_rules->TargetAttributeChanged(this);
