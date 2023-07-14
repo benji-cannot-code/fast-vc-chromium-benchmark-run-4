8b78c2e1ede050aacafbf1fcf87dce6c3362109f
Yuta Hijikata <ythjkt@google.com>
2023-07-14 16:34:18 +0000

    Remove copy migrator.
    This patch removes copy migrator code, which was disabled in
    crrev.com/c/4369253.
    
    Doc: go/deprecate-copy-migration.
    
    Test: tast run $DUT lacros.MigrateBasic, manually enabling Lacros.
    Bug: 1416750
    Change-Id: I5bc0da7be3f8fbbf0f8d5182177fcf9ad406e7df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678444
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170556}

--- chrome/browser/ash/crosapi/browser_util.h
+++ chrome/browser/ash/crosapi/browser_util.h
@@ -414,6 +414,8 @@ bool IsProfileMigrationAvailable();
 
 // Returns `MigrationMode::kMove` if LacrosOnly or `kLacrosMoveProfileMigration`
 // is enabled and `MigrationMode::kCopy` otherwise.
+// TODO(crbug.com/1416750): Remove this function once `CopyMigrator` is
+// completely removed.
 MigrationMode GetMigrationMode(const user_manager::User* user,
                                PolicyInitState policy_init_state);
 
