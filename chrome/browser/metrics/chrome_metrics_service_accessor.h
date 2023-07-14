ea4141885a54155b5fa8fc4fec20688d5fc3f8e0
dpapad <dpapad@chromium.org>
2023-07-14 17:30:04 +0000

    WebUI: Migrate chrome://metrics-internals tests to WebUIMochaBrowserTest.
    WebUIMochaBrowserTest is the new way of running WebUI tests without any
    dependency to the obsolete js2gtest and test_api.js mechanism.
    
    As part of the conversion, removed the standalone
    MetricsInternalsUIBrowserTest class, and merged it directly in the
    test that was previously inheriting from it (previously not possible
    due to the js2gtest intermediate layer).
    
    Bug: 1457360
    Change-Id: I54af2702f4f862f10dbfab12a2a95040dc87be5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4686190
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170583}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -169,7 +169,7 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class ClonedInstallClientIdResetBrowserTest;
   friend class metrics::ChromeOSPerUserMetricsBrowserTestBase;
   friend class SampledOutClientIdSavedBrowserTest;
-  friend class MetricsInternalsUIBrowserTest;
+  friend class MetricsInternalsUIBrowserTestWithLog;
   FRIEND_TEST_ALL_PREFIXES(ChromeMetricsServiceAccessorTest,
                            MetricsReportingEnabled);
   FRIEND_TEST_ALL_PREFIXES(ChromeMetricsServicesManagerClientTest,
