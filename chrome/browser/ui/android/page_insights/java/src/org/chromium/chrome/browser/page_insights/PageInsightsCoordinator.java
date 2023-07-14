d7c763953ac0be78a1106f1a445ac652dd1115e9
Jinsuk Kim <jinsukkim@chromium.org>
2023-07-14 14:40:47 +0000

    [gbb-cct] sWAA checker for page insights
    PageInsightsSwaaChecker uses WebHistoryService component
    to retrieve the sWAA status from the server. It is cached
    for future reference.
    
    The query is made in a periodic fashion to keep the value
    up to date.
    
    Low-Coverage-Reason: Main class PageInsightsSwaaChecker is fully covered. TRUC is mostly instantiating the relevant classes.
    Bug: 1448966
    Change-Id: I1318e2d52320f3d35e76dba6f8180e33f0f16a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574702
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170507}

--- chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsCoordinator.java
+++ chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsCoordinator.java
@@ -13,6 +13,8 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsSizer;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.flags.MutableFlagWithSafeDefault;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.bottomsheet.ExpandedSheetHelper;
@@ -23,6 +25,9 @@ import org.chromium.components.browser_ui.bottomsheet.ManagedBottomSheetControll
  * various components lazily.
  */
 public class PageInsightsCoordinator {
+    private static MutableFlagWithSafeDefault sPageInsightsHub =
+            new MutableFlagWithSafeDefault(ChromeFeatureList.CCT_PAGE_INSIGHTS_HUB, false);
+
     private final Context mContext;
 
     private final ObservableSupplier<Tab> mTabProvider;
@@ -34,6 +39,11 @@ public class PageInsightsCoordinator {
 
     private PageInsightsMediator mMediator;
     private PageInsightsSheetContent mSheetContent;
+
+    /** Returns true if page insight is enabled in the feature flag. */
+    public static boolean isFeatureEnabled() {
+        return sPageInsightsHub.isEnabled();
+    }
 
     /**
      * Constructor.
@@ -94,8 +104,7 @@ public class PageInsightsCoordinator {
 
     /** Destroy PageInsights component. */
     public void destroy() {
-        assert mMediator != null;
-        mMediator.destroy();
+        if (mMediator != null) mMediator.destroy();
     }
 
     @VisibleForTesting
