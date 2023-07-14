a9cc57a9da89a06b3bf31aaa729f50c52c84ec6c
Bartek Nowierski <bartekn@chromium.org>
2023-07-14 17:08:46 +0000

    Port (and use) MultipleUserProfilesState to WebView
    Bug: 1462131, 1367313
    Change-Id: I098e4df2fa2375019f78e3d4e5eee83c4d9530b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685530
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170573}

---
+++ android_webview/browser/metrics/system_state_util.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ANDROID_WEBVIEW_BROWSER_METRICS_SYSTEM_STATE_UTIL_H_
+#define ANDROID_WEBVIEW_BROWSER_METRICS_SYSTEM_STATE_UTIL_H_
+
+namespace android_webview {
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused. See MultipleUserProfilesState in
+// enums.xml.
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.android_webview
+enum class MultipleUserProfilesState {
+  kUnknown = 0,
+  kSingleProfile = 1,
+  kMultipleProfiles = 2,
+  kMaxValue = kMultipleProfiles,
+};
+
+// Returns whether there are multiple user profiles.
+MultipleUserProfilesState GetMultipleUserProfilesState();
+
+}  // namespace android_webview
+
+#endif  // ANDROID_WEBVIEW_BROWSER_METRICS_SYSTEM_STATE_UTIL_H_
---
+++ android_webview/java/src/org/chromium/android_webview/SystemStateUtil.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.os.UserManager;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+
+import java.util.List;
+
+/** Utility class to fetch information about system. */
+@JNINamespace("android_webview")
+public class SystemStateUtil {
+    /** Returns whether Android has multiple user profiles. */
+    @CalledByNative
+    public static @MultipleUserProfilesState int getMultipleUserProfilesState() {
+        UserManager userManager =
+                (UserManager) ContextUtils.getApplicationContext().getSystemService(
+                        Context.USER_SERVICE);
+        List<UserHandle> userHandles = userManager.getUserProfiles();
+        assert !userHandles.isEmpty();
+        return userHandles.size() > 1 ? MultipleUserProfilesState.MULTIPLE_PROFILES
+                                      : MultipleUserProfilesState.SINGLE_PROFILE;
+    }
+}
