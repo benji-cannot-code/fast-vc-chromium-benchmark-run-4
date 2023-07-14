ac2a18bb98166c1bf9d4c1fd3f4539a15f05f1ca
Ankush Singh <ankushkush@google.com>
2023-07-14 14:50:56 +0000

    [sync] Rename HasLocalChangesForTest() to HasLocalChanges()
    ModelTypeWorker::HasLocalChangesForTest() is used by
    ModelTypeRegistry::GetTypesWithUnsyncedData() which is not a test-only
    method. The implementation is okay for use outside of tests.
    
    Bug: 1447032
    Change-Id: I233372531711fbab07329b437db855726dcf912e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679535
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170517}

--- components/sync/engine/model_type_worker.cc
+++ components/sync/engine/model_type_worker.cc
@@ -791,7 +791,7 @@ std::unique_ptr<CommitContribution> ModelTypeWorker::GetContribution(
       CommitOnlyTypes().Has(type_));
 }
 
-bool ModelTypeWorker::HasLocalChangesForTest() const {
+bool ModelTypeWorker::HasLocalChanges() const {
   return has_local_changes_state_ != kNoNudgedLocalChanges;
 }
 
