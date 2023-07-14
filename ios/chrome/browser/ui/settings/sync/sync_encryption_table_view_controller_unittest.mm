cda32902a8cf9b1188c3f206d43075b16d165d87
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-14 21:27:18 +0000

    [iOS] dealloc checks settings is dismissed
    Change-Id: Id4ef29a2334d850062ba2204f7d9cf216ed5805a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670547
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170714}

--- ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller_unittest.mm
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 
 #import "base/functional/bind.h"
+#import "base/mac/foundation_util.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/chrome_table_view_controller_test.h"
+#import "ios/chrome/browser/ui/settings/content_settings/content_settings_table_view_controller.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/gtest/include/gtest/gtest.h"
@@ -35,6 +37,16 @@ class SyncEncryptionTableViewControllerTest
     browser_ = std::make_unique<TestBrowser>(browser_state_.get());
 
     CreateController();
+  }
+
+  void TearDown() override {
+    SyncEncryptionTableViewController* controller_ =
+        base::mac::ObjCCastStrict<SyncEncryptionTableViewController>(
+            controller());
+    if ([controller_ respondsToSelector:@selector(settingsWillBeDismissed)]) {
+      [controller_ performSelector:@selector(settingsWillBeDismissed)];
+    }
+    ChromeTableViewControllerTest::TearDown();
   }
 
   ChromeTableViewController* InstantiateController() override {
