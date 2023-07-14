b8168446ccc9da87fd10b1b10380931e041999bd
Michael Wilson <mjwilson@chromium.org>
2023-07-14 20:08:30 +0000

    Align MIDI permissions on change
    The two MIDI permissions have to interact with each other when either
    is set.  This CL keeps them synchronized.
    
    There is some special-casing to make sure that we don't get in an
    infinite loop of the permissions changing each other over and over.
    
    The method is loosely based on the pan-tilt-zoom permission handling:
    https://crsrc.org/c/components/permissions/contexts/camera_pan_tilt_zoom_permission_context.cc
    
    Bug: 1420307
    Change-Id: Ia1ea86de5f5e92c3477f224b89673dd3bf2c243e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679414
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170669}

--- components/permissions/contexts/midi_sysex_permission_context.h
+++ components/permissions/contexts/midi_sysex_permission_context.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/permissions/permission_context_base.h"
 
+class HostContentSettingsMap;
+
 namespace content {
 class BrowserContext;
 }  // namespace content
@@ -23,10 +25,18 @@ class MidiSysexPermissionContext : public PermissionContextBase {
   ~MidiSysexPermissionContext() override;
 
  private:
+  // content_settings::Observer
+  void OnContentSettingChanged(
+      const ContentSettingsPattern& primary_pattern,
+      const ContentSettingsPattern& secondary_pattern,
+      ContentSettingsTypeSet content_type_set) override;
+
   // PermissionContextBase:
   void UpdateTabContext(const PermissionRequestID& id,
                         const GURL& requesting_frame,
                         bool allowed) override;
+
+  raw_ptr<HostContentSettingsMap> host_content_settings_map_;
 };
 
 }  // namespace permissions
