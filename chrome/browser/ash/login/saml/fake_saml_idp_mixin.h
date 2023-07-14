42cf33da138a9aab4f930f835cd3d81f511735b8
Yoichi Osato <yoichio@chromium.org>
2023-07-14 01:30:32 +0000

    Revert "Authentication content control: SAML blocklist"
    This reverts commit a734afc153734ffcea929f262c7fc746b2dbe3ed.
    
    Reason for revert: Caused test failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/52301/test-results?sortby=&groupby=
    
    Original change's description:
    > Authentication content control: SAML blocklist
    >
    > Two new device policies (DeviceAuthenticationURLBlocklist,
    > DeviceAuthenticationURLAllowlist) allow admins to control content
    > in the login screen and lock screen via a URL blocklist and a
    > corresponding URL allowlist.  This helps admins close content
    > control escapes with undesired navigation during SAML flows on
    > managed devices.
    >
    > This change only defines the policies in Chromium in order to kick
    > off their propagation to the device management service, but this
    > change does not yet implement any behavior change based on the new
    > policies.  That will come in a separate later change.
    >
    > Bug: https://issuetracker.google.com/246531079
    > Change-Id: Ic0a1c781956ccce6907f87b3d153b69727052dda
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4075778
    > Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    > Reviewed-by: Andrey Davydov <andreydav@google.com>
    > Reviewed-by: Roman Sorokin <rsorokin@google.com>
    > Commit-Queue: Manuel Gómez <mgomezch@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1169975}
    
    Bug: https://issuetracker.google.com/246531079
    Change-Id: I9990de71a2912e92f698a88fc9eb7c11300fcf65
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685030
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Owners-Override: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170283}

--- chrome/browser/ash/login/saml/fake_saml_idp_mixin.h
+++ chrome/browser/ash/login/saml/fake_saml_idp_mixin.h
@@ -59,7 +59,6 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
   GURL GetHttpSamlPageUrl() const;
   GURL GetSamlWithDeviceAttestationUrl() const;
   GURL GetSamlWithDeviceTrustUrl() const;
-  GURL GetLinkedPageUrl() const;
 
  private:
   GURL GetSamlAuthPageUrl() const;
@@ -75,8 +74,7 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
     kLoginAuth,
     kLoginWithDeviceAttestation,
     kLoginCheckDeviceAnswer,
-    kLoginWithDeviceTrust,
-    kLinkedPage
+    kLoginWithDeviceTrust
   };
 
   // Returns the RequestType that corresponds to `url`, or RequestType::Unknown
@@ -101,17 +99,11 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
   BuildResponseForCheckDeviceAnswer(
       const net::test_server::HttpRequest& request,
       const GURL& request_url);
-  std::unique_ptr<net::test_server::HttpResponse> BuildResponseForLinkedPage(
-      const net::test_server::HttpRequest& request,
-      const GURL& request_url) const;
 
   std::unique_ptr<net::test_server::HttpResponse> BuildHTMLResponse(
       const std::string& html_template,
       const std::string& relay_state,
       const std::string& next_path) const;
-
-  std::unique_ptr<net::test_server::HttpResponse> BuildHTMLResponse(
-      const std::string& response_html) const;
 
   void SaveChallengeResponse(const std::string& response);
   void ClearChallengeResponse();
