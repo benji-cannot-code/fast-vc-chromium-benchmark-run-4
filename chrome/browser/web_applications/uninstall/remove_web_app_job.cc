91755b5e8eccc2836e1a68779e87280c2fdf94c5
Zelin Liu <zelin@chromium.org>
2023-07-14 22:23:32 +0000

    Reland "iwa: Migrate Storage Partitions GC from Extensions to Web Apps system"
    From web-app-internals logs, it seems the previous test failure was due
    to GarbageCollectStorageParttionsCommand being scheduled but not run
    when the assertion executed in the test.
    
    In this reland, instead of asserting the pref value change, which
    happens on command success, we are waiting on the pref value change
    using WaitForPrefValue(). This should allow command to execute
    successfully. Otherwise, the test will wait until timeout on failure.
    
    This is a reland of commit 3cba199e0ad9052733052a0cf884fc0ef9aae5b0
    
    Original change's description:
    > iwa: Migrate Storage Partitions GC from Extensions to Web Apps system
    >
    > Both Chrome Apps and Isolated Web Apps use isolated Storage Partitions.
    > This means any Storage Partition garbage collection system using
    > BrowserContext::GarbageCollectStoragePartitions() must be aware of valid
    > Storage Partition paths from both systems.
    >
    > Chrome Apps are being deprecated and IWAs need to work with web app
    > command system to avoid race conditions. We are migrating Storage
    > Partition garbage collection system from ExtensionsGarbageCollector to
    > web_app::GarbageCollectStoragePartitionCommand. The new GC system will
    > support Chrome Apps until they are fully removed.
    >
    > This CL does a few things to fully delete
    > ExtensionGarbageCollector::GarbageCollectIsolatedStorageIfNeeded and
    > replace it with GarbageCollectStoragePartitionCommand.
    >
    > For Extensions code:
    > - Replace ExtensionsPref::kStorageGarbageCollect with
    > pref::kShouldGarbageCollectStoragePartitions
    > - Remove
    > ExtensionGarbageCollector::GarbageCollectIsolatedStorageIfNeeded and
    > its associated InstallGate.
    >
    > For Web Apps code:
    > - Register pref::kShouldGarbageCollectStoragePartitions.
    > - Sets prefs::kShouldGarbageCollectStoragePartitions to true in
    > remove_web_app_job.cc
    > - Register and unregister extensions::InstallGate in
    > GarbageCollectStoragePartitionsCommand via ExtensionsManager.
    > - Invokes GarbageCollectStoragePartitionCommand in PostProfileInit if
    > pref is true.
    > - Implicitly call GarbageCollectStoragePartitionCommand from restart
    > using PRE test in
    > garbage_collect_storage_partitions_command_browsertest.cc
    >
    > Low-Coverage-Reason:<command called implicitly in browsertest restart>
    >
    > Change-Id: I5d418dc1b1f7bfc5890850d94d11a827b794b2ff
    > Bug: 1453520
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4663920
    > Reviewed-by: David Bertoni <dbertoni@chromium.org>
    > Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    > Commit-Queue: Zelin Liu <zelin@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1170246}
    
    Bug: 1453520
    Change-Id: If29cd58fc60cfc3771964c692b9e148fc6942aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685650
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170749}

--- chrome/browser/web_applications/uninstall/remove_web_app_job.cc
+++ chrome/browser/web_applications/uninstall/remove_web_app_job.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registry_update.h"
 #include "chrome/browser/web_applications/web_app_sync_bridge.h"
 #include "chrome/browser/web_applications/web_app_translation_manager.h"
+#include "chrome/common/pref_names.h"
+#include "components/prefs/pref_service.h"
 #include "components/webapps/browser/uninstall_result_code.h"
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
@@ -85,6 +87,10 @@ void RemoveWebAppJob::Start(AllAppsLock& lock, Callback callback) {
   if (!app) {
     CompleteAndSelfDestruct(webapps::UninstallResultCode::kNoAppToUninstall);
     return;
+  }
+
+  if (app->isolation_data().has_value()) {
+    has_isolated_storage_ = true;
   }
 
   if (is_initial_request_) {
@@ -260,6 +266,14 @@ void RemoveWebAppJob::MaybeFinishPrimaryRemoval() {
   primary_removal_result_ = errors_ ? webapps::UninstallResultCode::kError
                                     : webapps::UninstallResultCode::kSuccess;
   base::UmaHistogramBoolean("WebApp.Uninstall.Result", !errors_);
+
+  // For IWAs, set pref for garbage collection.
+  if (has_isolated_storage_ &&
+      primary_removal_result_ == webapps::UninstallResultCode::kSuccess) {
+    profile_->GetPrefs()->SetBoolean(
+        prefs::kShouldGarbageCollectStoragePartitions, true);
+  }
+
   lock_->install_manager().NotifyWebAppUninstalled(app_id_, uninstall_source_);
 
   ProcessSubAppsPendingRemovalOrComplete();
