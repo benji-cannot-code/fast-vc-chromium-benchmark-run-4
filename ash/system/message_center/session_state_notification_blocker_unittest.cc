0b56a66515723f0f07842db28e2959cae231fc58
Elliot Tuck <etuck@chromium.org>
2023-07-14 00:48:40 +0000

    Reland "Update notification state when blocker is added"
    This is a reland of commit 93a845736c3c61c61a511dadbcdb6c8042406e0c
    
    The issue with the original CL was that a newly-added death test would
    check for a particular log message regardless of the build type. Log
    messages for `CHECK()` statements are stripped for release builds, hence
    that test would fail for those builds. This CL fixes the issue by using
    `CHECK_WILL_STREAM()` to determine how strict the log message checking
    should be. View the diff between patchset 1 and the latest patchset to
    isolate this fix.
    
    Original change's description:
    > Update notification state when blocker is added
    >
    > Treat the addition of a new `NotificationBlocker` as a state-change
    > event, as if `NotificationBlocker::Observer::OnBlockingStateChanged()`
    > were called. Create a new `NotificationBlocker::Init()` method that must
    > be called after a `NotificationBlocker`'s ctor to avoid calling a
    > virtual function during construction. Update existing usages to the new
    > initialization pattern.
    >
    > Fixed: b:286590651
    > Change-Id: I3ed5a1a818d0c086937bf676a47854113fdd5058
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4656056
    > Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    > Commit-Queue: Elliot Tuck <etuck@chromium.org>
    > Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    > Reviewed-by: David Black <dmblack@google.com>
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1169593}
    
    Change-Id: Ie06feca468da99d1a79b7353761c378793e3ded1
    Fixed: b:286590651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681844
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    Commit-Queue: Elliot Tuck <etuck@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170266}

--- ash/system/message_center/session_state_notification_blocker_unittest.cc
+++ ash/system/message_center/session_state_notification_blocker_unittest.cc
@@ -57,6 +57,7 @@ class SessionStateNotificationBlockerTest
     NoSessionAshTestBase::SetUp();
     blocker_ = std::make_unique<SessionStateNotificationBlocker>(
         message_center::MessageCenter::Get());
+    blocker_->Init();
     blocker_->AddObserver(this);
   }
 
--- ui/message_center/notification_blocker.cc
+++ ui/message_center/notification_blocker.cc
@@ -11,14 +11,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace message_center {
 
 NotificationBlocker::NotificationBlocker(MessageCenter* message_center)
-    : message_center_(message_center) {
-  if (message_center_)
-    message_center_->AddNotificationBlocker(this);
-}
+    : message_center_(message_center) {}
 
 NotificationBlocker::~NotificationBlocker() {
   if (message_center_)
     message_center_->RemoveNotificationBlocker(this);
+}
+
+void NotificationBlocker::Init() {
+  CHECK(!is_initialized_)
+      << "Do not initialize a NotificationBlocker more than once.";
+  is_initialized_ = true;
+  if (message_center_) {
+    message_center_->AddNotificationBlocker(this);
+  }
 }
 
 void NotificationBlocker::AddObserver(NotificationBlocker::Observer* observer) {
