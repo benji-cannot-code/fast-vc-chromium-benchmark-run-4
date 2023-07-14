072f2836b05cd95020756655ff6a596e537d61b7
Kevin Lubick <kjlubick@google.com>
2023-07-14 15:17:52 +0000

    Fix missing #includes
    These were discovered after trying to land http://review.skia.org/721906
    
    Change-Id: I6fcace534fd115aced312ece5203eadc182c0829
    Bug: 242216, skia:13052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683573
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Herb Derby <herb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170531}

--- chrome/browser/vr/elements/scrollable_element.cc
+++ chrome/browser/vr/elements/scrollable_element.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/vr/elements/scrollable_element.h"
 
 #include <algorithm>
+#include <cfloat>
 
 #include "base/numerics/ranges.h"
 #include "chrome/browser/vr/input_event.h"
