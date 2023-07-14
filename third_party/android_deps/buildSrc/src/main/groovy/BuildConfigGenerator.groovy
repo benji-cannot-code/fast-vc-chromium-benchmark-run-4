80e07d5102592c31171aed70a6b1b7845b111067
Anne Redulla <aredulla@google.com>
2023-07-14 13:45:01 +0000

    [ssci] Update BuildConfigGenerator.makeReadme for Shipped field
    This CL updates BuildConfigGenerator's makeReadme method.
    NOT_SHIPPED is being deprecated and a new Shipped field
    has been added to the README.chromium format.
    
    See go/update-readme-chromium and go/lsc-shipped-field
    
    Bug: b:285450187
    Change-Id: Id42cf6dc73c56e0a8f1a1ba7197f53558599278c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4686010
    Auto-Submit: Anne Redulla <aredulla@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170485}

--- third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
@@ -195,7 +195,6 @@ class BuildConfigGenerator extends DefaultTask {
         String licenseString = String.join(', ', licenseStrings)
 
         boolean securityCritical = dependency.supportsAndroid && dependency.isShipped
-        String licenseFile = dependency.isShipped ? 'LICENSE' : 'NOT_SHIPPED'
         String cpePrefix = dependency.cpePrefix ? dependency.cpePrefix : 'unknown'
 
         return """\
@@ -204,9 +203,10 @@ class BuildConfigGenerator extends DefaultTask {
             URL: ${dependency.url}
             Version: ${dependency.version}
             License: ${licenseString}
-            License File: ${licenseFile}
+            License File: LICENSE
             CPEPrefix: ${cpePrefix}
             Security Critical: ${securityCritical ? 'yes' : 'no'}
+            Shipped: ${dependency.isShipped ? 'yes' : 'no'}
             ${dependency.licenseAndroidCompatible ? 'License Android Compatible: yes' : ''}
             Description:
             ${dependency.description}
