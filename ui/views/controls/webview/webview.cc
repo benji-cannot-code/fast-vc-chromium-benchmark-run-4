a59b0063693273b4cf22ce1e4c4ff38120bacc8e
Jeffrey Cohen <jeffreycohen@google.com>
2023-07-15 01:06:30 +0000

    [Code Health] clean up raw_ptr in webview
    Bug: 1462092
    Change-Id: Ib831579449f0d2f80efb6cac6e5393daa509f069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679221
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170822}

--- ui/views/controls/webview/webview.cc
+++ ui/views/controls/webview/webview.cc
@@ -73,6 +73,7 @@ WebView::WebView(content::BrowserContext* browser_context) {
 WebView::~WebView() {
   ui::AXPlatformNode::RemoveAXModeObserver(this);
   SetWebContents(nullptr);  // Make sure all necessary tear-down takes place.
+  browser_context_ = nullptr;
 }
 
 content::WebContents* WebView::GetWebContents(base::Location creator_location) {
