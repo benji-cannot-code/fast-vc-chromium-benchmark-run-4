b3c3219a105971c2aa6b29bb5667501a3866e014
Yuki Shiino <yukishiino@chromium.org>
2023-07-15 04:09:58 +0000

    bindings: Make sync iterators more like IDL interfaces
    SyncIterator.IRs were created right before processing IDL operation
    groups (overload groups). Extended attributes' propagation, etc.
    were not applied SyncIterator.IRs. However, upcoming AsyncIterator.IRs
    need extended attributes' propagation, etc. and in general, it's good
    to apply all compilation phases to IRs.
    
    In order to improve the above point, make SyncIterator.IR more like
    Interface.IR, e.g. inheriting IRMap.IR. Also apply all compilation
    phases to SyncIterator.IRs.
    
    Bug: 1087157
    Change-Id: I3136e74ac0f5b0b2697de84dbbfaca34a49d384a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4674470
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1170844}

--- third_party/blink/renderer/bindings/scripts/web_idl/database.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/database.py
@@ -35,6 +35,7 @@ class DatabaseBody(object):
         INTERFACE_MIXIN = 'interface mixin'
         NAMESPACE = 'namespace'
         OBSERVABLE_ARRAY = 'observable array'
+        SYNC_ITERATOR = 'sync iterator'
         TYPEDEF = 'typedef'
         UNION = 'union'
 
@@ -47,6 +48,7 @@ class DatabaseBody(object):
             INTERFACE_MIXIN,
             NAMESPACE,
             OBSERVABLE_ARRAY,
+            SYNC_ITERATOR,
             TYPEDEF,
             UNION,
         )
@@ -154,6 +156,11 @@ class Database(object):
     def observable_arrays(self):
         """Returns all observable arrays."""
         return self._view_by_kind(Database._Kind.OBSERVABLE_ARRAY)
+
+    @property
+    def sync_iterators(self):
+        """Returns all sync iterators."""
+        return self._view_by_kind(Database._Kind.SYNC_ITERATOR)
 
     @property
     def typedefs(self):
