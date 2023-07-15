65b155b51dd0d1e58aeb2de167cfee3aae9e06a2
Bruce Dawson <brucedawson@chromium.org>
2023-07-15 00:38:55 +0000

    Fix duplicate test name in sbox_integration_tests
    Duplicate test names can cause confusion and test failures. This fixes
    a duplicate test name in sbox_integration_tests. It had two tests named
    SandboxTypeTest.Utility.
    
    Bug: 1465008
    Change-Id: Ie0efd784e925c99b78eaa666f6635070086a81ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4688358
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170811}

--- sandbox/policy/win/mf_cdm_sandbox_type_unittest.cc
+++ sandbox/policy/win/mf_cdm_sandbox_type_unittest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-TEST(SandboxTypeTest, Utility) {
+TEST(MfCdmSandboxTypeTest, Utility) {
   // Setup to have '--type=utility' first (but no valid sandbox).
   base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
   command_line.AppendSwitchASCII(sandbox::policy::switches::kProcessType,
