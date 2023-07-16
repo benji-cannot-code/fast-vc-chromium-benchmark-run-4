2f20c52e21d39773237e20a2151b78236cb56b9e
Gang Wu <gangwu@chromium.org>
2023-07-16 22:33:20 +0000

    Delete unused methods in AuxiliarySearchProvider
    Bug: 1445112
    Change-Id: I5d32bd4ff287aa57eefdc5cd60783b20f41ba1f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683737
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170998}

--- chrome/android/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchProvider.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.auxiliary_search;
 
-import android.util.Pair;
-
 import org.chromium.chrome.browser.auxiliary_search.AuxiliarySearchGroupProto.AuxiliarySearchBookmarkGroup;
 import org.chromium.chrome.browser.auxiliary_search.AuxiliarySearchGroupProto.AuxiliarySearchEntry;
 import org.chromium.chrome.browser.auxiliary_search.AuxiliarySearchGroupProto.AuxiliarySearchTabGroup;
@@ -16,9 +14,6 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.state.CriticalPersistedTabData;
 import org.chromium.chrome.browser.tabmodel.TabList;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * This class provides information for the auxiliary search.
@@ -32,41 +27,6 @@ public class AuxiliarySearchProvider {
     public AuxiliarySearchProvider(Profile profile, TabModelSelector tabModelSelector) {
         mAuxiliarySearchBridge = new AuxiliarySearchBridge(profile);
         mTabModelSelector = tabModelSelector;
-    }
-
-    /**
-     * @return A list of titles and urls as pairs from tabs for the auxiliary search.
-     */
-    public List<Pair<String, String>> getTabsSearchableData() {
-        List<Pair<String, String>> tabsList = new ArrayList<>();
-
-        TabList tabList = mTabModelSelector.getModel(false).getComprehensiveModel();
-        int firstTabIndex = Math.max(tabList.getCount() - kNumTabsToSend, 0);
-        int end = tabList.getCount() - 1;
-        // Find the the bottom of tabs in the tab switcher view if the number of the tabs more than
-        // 'kNumTabsToSend'. In the multiwindow mode, the order of the 'tabList' is one window's
-        // tabs, and then another's.
-        for (int i = firstTabIndex; i <= end; i++) {
-            Tab tab = tabList.getTabAt(i);
-            tabsList.add(new Pair<>(tab.getTitle(), tab.getUrl().getSpec()));
-        }
-        return tabsList;
-    }
-
-    /**
-     * @return A list of titles and urls as pairs from bookmarks for the auxiliary search.
-     */
-    public List<Pair<String, String>> getBookmarksSearchableData() {
-        AuxiliarySearchBookmarkGroup group = mAuxiliarySearchBridge.getBookmarksSearchableData();
-
-        List<Pair<String, String>> bookmarksList = new ArrayList<>();
-        if (group != null) {
-            for (int i = 0; i < group.getBookmarkCount(); i++) {
-                AuxiliarySearchEntry bookmark = group.getBookmark(i);
-                bookmarksList.add(new Pair<>(bookmark.getTitle(), bookmark.getUrl()));
-            }
-        }
-        return bookmarksList;
     }
 
     /**
