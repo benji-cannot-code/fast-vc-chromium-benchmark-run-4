98e3a042311dee0ce936de913d153f802c6c237f
Paul Adedeji <pauladedeji@google.com>
2023-07-17 21:33:59 +0000

    [ntp] Fix borders in dark mode for categories.html and themes.html
    Fixes bugs where selected state border does not show for categories or themes in Panorama when Chrome is in dark mode.
    
    *demo in bug comments*
    
    Change-Id: I8ddee6c4a625601b35c3847435c8068394a0417d
    Bug: 1464679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684030
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171390}

--- chrome/browser/resources/side_panel/customize_chrome/check_mark_wrapper.ts
+++ chrome/browser/resources/side_panel/customize_chrome/check_mark_wrapper.ts
@@ -28,10 +28,16 @@ export class CheckMarkWrapperElement extends PolymerElement {
         type: Boolean,
         reflectToAttribute: true,
       },
+      checkmarkBorderHidden: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
     };
   }
 
   checked: boolean;
+  checkmarkBorderHidden: boolean;
 }
 
 declare global {
