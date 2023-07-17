50e9140385ecf755ef9cbee85029894bad0fd90e
Xiaocheng Hu <xiaochengh@chromium.org>
2023-07-17 19:55:33 +0000

    [anchor-position] Redo overscroll for anchor-positioned elements
    We currently undo overscroll for fixed positioned elements, which
    is not desired for anchor positioned elements (like a popover)
    because these elements can move when the viewport is scrolled, and
    if they don't overscroll, they appear detached from their anchors.
    
    So this patch redoes overscroll for these elements.
    
    Fixed: 1455460
    Change-Id: I2c5fde24971f89e8f448b3b298d52859191ec408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4687945
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171332}

--- third_party/blink/renderer/core/layout/anchor_position_scroll_data.h
+++ third_party/blink/renderer/core/layout/anchor_position_scroll_data.h
@@ -58,6 +58,9 @@ class AnchorPositionScrollData
 
     // Sum of the scroll origins of the above scroll containers.
     gfx::Vector2d accumulated_scroll_origin;
+
+    // Whether viewport is in `scroll_container_ids`.
+    bool scroll_containers_include_viewport = false;
   };
 
   Element* OwnerElement() const { return owner_; }
@@ -74,6 +77,12 @@ class AnchorPositionScrollData
   }
   gfx::Vector2dF AdditionalBoundsScrollOffset() const {
     return additional_bounds_scroll_offset_;
+  }
+
+  // Returns true if the snapshotted scroll offset is affected by viewport's
+  // scroll offset.
+  bool IsAffectedByViewportScrolling() const {
+    return is_affected_by_viewport_scrolling_;
   }
 
   // Utility function that returns accumulated_scroll_offset_ rounded as a
@@ -130,6 +139,8 @@ class AnchorPositionScrollData
 
   // The scroll offset applied to the additional fallback-bounds rect.
   gfx::Vector2dF additional_bounds_scroll_offset_;
+
+  bool is_affected_by_viewport_scrolling_ = false;
 };
 
 template <>
--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -5816,6 +5816,17 @@ bool LayoutBox::HasAnchorPositionScrollTranslation() const {
   return false;
 }
 
+bool LayoutBox::HasAnchorPositionScrollTranslationAffectedByViewportScrolling()
+    const {
+  if (Element* element = DynamicTo<Element>(GetNode())) {
+    if (AnchorPositionScrollData* data =
+            element->GetAnchorPositionScrollData()) {
+      return data->HasTranslation() && data->IsAffectedByViewportScrolling();
+    }
+  }
+  return false;
+}
+
 PhysicalOffset LayoutBox::AnchorPositionScrollTranslationOffset() const {
   if (Element* element = DynamicTo<Element>(GetNode())) {
     if (AnchorPositionScrollData* data =
