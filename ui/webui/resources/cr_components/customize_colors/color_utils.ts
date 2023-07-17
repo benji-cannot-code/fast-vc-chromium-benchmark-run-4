d335cd451d9a061aaf9748797cb2347dbdcb3a12
Riley Tatum <rtatum@google.com>
2023-07-17 23:54:56 +0000

    [GM3] Moving Panorama color picker to cr_component: Part 1
    * Put part of the html and ts of colors.ts and its dependencies from
      side panel customize_chrome into a new cr_component so that it can
      be reused in settings and new profile flow.
    * Tests will be moved over in a future CL. I'm trying to move over the
      code in sections so I don't create one giant CL. This is already very
      large.
    
    Bug: 1459420
    Change-Id: I3b88470253452730aa03063c2f8dcb5fb7d0d210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652259
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171469}

---
+++ ui/webui/resources/cr_components/customize_colors/color_utils.ts
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {SkColor} from 'chrome://resources/mojo/skia/public/mojom/skcolor.mojom-webui.js';
+
+export interface Color {
+  background: SkColor;
+  foreground: SkColor;
+  base?: SkColor;
+}
+
+export const LIGHT_DEFAULT_COLOR: Color = {
+  background: {value: 0xffffffff},
+  foreground: {value: 0xffdee1e6},
+  base: {value: 0},
+};
+
+export const LIGHT_BASELINE_GREY_COLOR: Color = {
+  background: {value: 0xff0b57d0},
+  foreground: {value: 0xffe3e3e3},
+  base: {value: 0xffc7c7c7},
+};
