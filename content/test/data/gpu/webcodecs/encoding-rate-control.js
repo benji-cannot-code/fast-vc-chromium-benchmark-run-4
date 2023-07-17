65ef8d019004a3a03bcc28909c4fb6514a3d5425
Sida Zhu <zhusida@bytedance.com>
2023-07-17 22:47:30 +0000

    Video: Use try catch wrap isConfigSupported
    This is a temporately workaround, add exceptions would be
    too much work since "*" can only be used at the end of the
    match glob.
    
    Bug: 1465534
    Change-Id: I197aaac93e7ea8f21104008bdef7322cf5cbd36f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4692042
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: 朱思达 <zhusida@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1171435}

--- content/test/data/gpu/webcodecs/encoding-rate-control.js
+++ content/test/data/gpu/webcodecs/encoding-rate-control.js
@@ -71,8 +71,13 @@ async function main(arg) {
     framerate: fps
   };
 
-  let support = await VideoEncoder.isConfigSupported(encoder_config);
-  if (!support.supported) {
+  let supported = false;
+  try {
+    supported =
+        (await VideoEncoder.isConfigSupported(encoder_config)).supported;
+  } catch (e) {
+  }
+  if (!supported) {
     TEST.skip('Unsupported codec: ' + arg.codec);
     return;
   }
