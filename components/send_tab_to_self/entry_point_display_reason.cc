b7867e173fea3f3d9b7542071bbb87bb4747f30d
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-07-17 16:25:57 +0000

    [stts] Fix infinite spinner upon sign-in with custom passphrase account
    See details in the linked bug.
    
    Fixed: 1465151
    Change-Id: I9b5b5a177972dd66dddd970d1f377a616b9d65bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4688100
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171216}

--- components/send_tab_to_self/entry_point_display_reason.cc
+++ components/send_tab_to_self/entry_point_display_reason.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/sync/service/sync_service.h"
+#include "components/sync/service/sync_user_settings.h"
 #include "url/gurl.h"
 
 namespace send_tab_to_self {
@@ -40,8 +41,8 @@ absl::optional<EntryPointDisplayReason> GetEntryPointDisplayReason(
   if (!url_to_share.SchemeIsHTTPOrHTTPS())
     return absl::nullopt;
 
-  if (!send_tab_to_self_sync_service) {
-    // Can happen in incognito or guest profile.
+  if (!send_tab_to_self_sync_service || !sync_service) {
+    // Can happen in incognito, guest profile, or tests.
     return absl::nullopt;
   }
 
@@ -52,8 +53,18 @@ absl::optional<EntryPointDisplayReason> GetEntryPointDisplayReason(
 
   SendTabToSelfModel* model =
       send_tab_to_self_sync_service->GetSendTabToSelfModel();
-  if (!model->IsReady())
+  if (!model->IsReady()) {
+    syncer::SyncUserSettings* settings = sync_service->GetUserSettings();
+    if (sync_service->IsEngineInitialized() &&
+        (settings->IsPassphraseRequiredForPreferredDataTypes() ||
+         settings->IsTrustedVaultKeyRequiredForPreferredDataTypes())) {
+      // There's an encryption error, the model won't become ready unless the
+      // user takes explicit action. But the error will be surfaced by dedicated
+      // non send-tab-to-self UI. So just treat this as the no device case.
+      return EntryPointDisplayReason::kInformNoTargetDevice;
+    }
     return absl::nullopt;
+  }
 
   if (!model->HasValidTargetDevice()) {
     return base::FeatureList::IsEnabled(kSendTabToSelfSigninPromo)
