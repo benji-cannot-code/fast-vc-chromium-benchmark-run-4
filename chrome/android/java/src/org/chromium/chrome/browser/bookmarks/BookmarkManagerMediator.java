2b8126ea6cd212713f25cd43eaeeb09bc8eb5e56
Sky Malice <skym@chromium.org>
2023-07-17 18:15:14 +0000

    Add @BookmarkUiMode annotation to BookmarkManagerMediator.
    Bug: 1464965
    Change-Id: Ie788fe3a9fc926c492976e5ca58f7a5c548b8f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4691103
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171284}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
@@ -587,7 +587,7 @@ class BookmarkManagerMediator
 
     @Override
     public void notifyStateChange(BookmarkUiObserver observer) {
-        int state = getCurrentUiMode();
+        final @BookmarkUiMode int state = getCurrentUiMode();
         observer.onUiModeChanged(state);
         switch (state) {
             case BookmarkUiMode.LOADING:
