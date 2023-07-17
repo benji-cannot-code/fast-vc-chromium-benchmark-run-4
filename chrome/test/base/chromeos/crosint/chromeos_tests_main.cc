608524443e2aa3d648855234d813b37da7367bd2
Sven Zheng <svenzheng@chromium.org>
2023-07-17 18:41:46 +0000

    ChromeOS integration test framework setup
    Design doc: go/crosint-design
    This is a new framework setup for integration testing on
    ChromeOS device/VM. The idea is that we build a framework
    on top of the browser test framework, take out all the fakes,
    stubs, and built-in on-device APIs so we can run gtest with
    a full browser on ChromeOS device.
    A few decisions we want to make in this change:
    1 Overall code structure.
    2 One test target for both ash and lacros
    3 crosint as the project name. And anywhere we want to use
      a short name, like 'crosint test'.
    4 All tests would be named as 'foo_integration_test.cc'.
    
    Bug: 1464750
    Test: Tested locally.
    Change-Id: I9a09613b7908c6e834256dddecbee8cbf48f9b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4676177
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171294}

---
+++ chrome/test/base/chromeos/crosint/chromeos_tests_main.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/command_line.h"
+#include "chrome/test/base/chromeos/crosint/chromeos_test_launcher.h"
+#include "chrome/test/base/chromeos/crosint/chromeos_test_suite.h"
+
+int main(int argc, char** argv) {
+  base::CommandLine::Init(argc, argv);
+
+  ChromeOSTestSuiteRunner runner;
+  ChromeOSTestLauncherDelegate delegate(&runner);
+  return LaunchChromeOSTests(&delegate, argc, argv);
+}
