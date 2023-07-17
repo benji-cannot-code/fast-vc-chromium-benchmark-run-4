f1c60d5f36d3be8f6e1703c27bfd5d00060f2464
Daniel Cheng <dcheng@chromium.org>
2023-07-17 19:13:06 +0000

    Make XML input callbacks in Blink more robust to unexpected situations.
    When parsing XML in the renderer process, there is never an instance
    where it's desirable to bypass using Blink's custom input callbacks. So
    rather than allowing this to fail open, CHECK() fail if the expected
    preconditions are violated instead.
    
    This also does a bit of miscellaneous cleanup to remove code for
    handling XML catalogs (which Blink never supported ayway) as well as add
    some compile-time checks that the expected disabled features are, in
    fact, disabled.
    
    Change-Id: I0a5733fba8df75384bb48035bf7caba08e125a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4691203
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171315}

---
+++ third_party/libxml/chromium/disabled_libxml_features.cc
@@ -0,0 +1,68 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <libxml/xmlversion.h>
+
+// Basic test that libxml features that are unused in Chromium remain disabled.
+
+#ifdef LIBXML_AUTOMATA_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_C14N_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_CATALOG_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_DEBUG_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_DOCB_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_EXPR_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_LEGACY_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_MODULES_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_REGEXP_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_SCHEMAS_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_SCHEMATRON_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_VALID_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_XINCLUDE_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_XPTR_ENABLED
+static_assert(false);
+#endif
+
+#ifdef LIBXML_ZLIB_ENABLED
+static_assert(false);
+#endif
