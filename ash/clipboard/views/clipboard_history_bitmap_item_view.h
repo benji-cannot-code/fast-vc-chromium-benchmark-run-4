5cfa4581e9529f963201b160a62fd5c8f540285f
Colin Kincaid <ckincaid@chromium.org>
2023-07-17 22:34:24 +0000

    multipaste: Clip contents when delete button is visible
    This CL causes a clipboard history item's contents to be clipped when
    the menu item's delete button is visible. The contents are clipped with
    a cutout so that they do not overlap with the button.
    
    Bitmap item: https://screenshot.googleplex.com/5VrUuNHsUmtZdRH
    Text item: https://screenshot.googleplex.com/B4pu6urVqaAR2Xa
    
    Note: In order to make it easier to see tell what coverage this CL adds,
    the test changes introduced here account for the clipboard history
    refresh being enabled or disabled but do not actually parameterize these
    tests. The tests are moved to a parameterized suite in a follow-up CL.
    
    Bug: b/267693870
    Change-Id: I3968edc0e1ad5f39a0fb084b37135d1f941860c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4688865
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171429}

--- ash/clipboard/views/clipboard_history_bitmap_item_view.h
+++ ash/clipboard/views/clipboard_history_bitmap_item_view.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_CLIPBOARD_VIEWS_CLIPBOARD_HISTORY_BITMAP_ITEM_VIEW_H_
 
 #include "ash/clipboard/views/clipboard_history_item_view.h"
-#include "base/memory/raw_ptr.h"
 #include "base/unguessable_token.h"
 #include "ui/base/clipboard/clipboard_data.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -32,7 +31,7 @@ class ClipboardHistoryBitmapItemView : public ClipboardHistoryItemView {
   class BitmapContentsView;
 
   // ClipboardHistoryItemView:
-  std::unique_ptr<views::View> CreateContentsView() override;
+  std::unique_ptr<ContentsView> CreateContentsView() override;
 
   // The format of the associated `ClipboardData`.
   const ui::ClipboardInternalFormat data_format_;
