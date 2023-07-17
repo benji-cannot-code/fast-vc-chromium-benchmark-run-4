2a6c58e26f72a0c18ba1539655217c806324e306
Artsiom Mitrokhin <amitrokhin@google.com>
2023-07-17 23:56:39 +0000

    Add classroom bubble if the corresponding user role is active
    Bug: b/283370562
    Change-Id: I1a088d50be327d52a3801367262f4c09727159cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4690445
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171472}

--- ash/system/unified/glanceable_tray_bubble_view.cc
+++ ash/system/unified/glanceable_tray_bubble_view.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/unified/glanceable_tray_bubble_view.h"
 
+#include <algorithm>
+
+#include "ash/glanceables/classroom/glanceables_classroom_client.h"
+#include "ash/glanceables/glanceables_v2_controller.h"
 #include "ash/public/cpp/session/user_info.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
@@ -17,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/unified/classroom_bubble_teacher_view.h"
 #include "ash/system/unified/tasks_bubble_view.h"
 #include "base/check.h"
+#include "base/functional/bind.h"
 #include "components/session_manager/session_manager_types.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
@@ -73,30 +78,9 @@ void GlanceableTrayBubbleView::UpdateBubble() {
           session_manager::SessionState::ACTIVE &&
       session_controller->GetUserSession(0)->user_info.has_gaia_account;
 
-  // TODO(b:277268122): set real contents for glanceables view.
   if (should_show_non_calendar_glanceables && !tasks_bubble_view_) {
     tasks_bubble_view_ = child_glanceable_container->AddChildView(
         std::make_unique<TasksBubbleView>(detailed_view_delegate_.get()));
-  }
-
-  // TODO(b:283370562): only add teacher/student classroom glanceables when
-  // the user is enrolled in courses.
-  if (should_show_non_calendar_glanceables && !classroom_bubble_teacher_view_) {
-    classroom_bubble_teacher_view_ = child_glanceable_container->AddChildView(
-        std::make_unique<ClassroomBubbleTeacherView>(
-            detailed_view_delegate_.get()));
-    // Add spacing between the classroom bubble and the previous bubble.
-    classroom_bubble_teacher_view_->SetProperty(views::kMarginsKey,
-                                                gfx::Insets::TLBR(8, 0, 0, 0));
-  }
-
-  if (should_show_non_calendar_glanceables && !classroom_bubble_student_view_) {
-    classroom_bubble_student_view_ = child_glanceable_container->AddChildView(
-        std::make_unique<ClassroomBubbleStudentView>(
-            detailed_view_delegate_.get()));
-    // Add spacing between the classroom bubble and the previous bubble.
-    classroom_bubble_student_view_->SetProperty(views::kMarginsKey,
-                                                gfx::Insets::TLBR(8, 0, 0, 0));
   }
 
   if (!calendar_view_) {
@@ -115,10 +99,46 @@ void GlanceableTrayBubbleView::UpdateBubble() {
   SetMaxHeight(max_height);
   ChangeAnchorAlignment(shelf_->alignment());
   ChangeAnchorRect(shelf_->GetSystemTrayAnchorRect());
+
+  auto* const classroom_client =
+      Shell::Get()->glanceables_v2_controller()->GetClassroomClient();
+  if (should_show_non_calendar_glanceables && classroom_client) {
+    if (!classroom_bubble_student_view_) {
+      classroom_client->IsStudentRoleActive(base::BindOnce(
+          &GlanceableTrayBubbleView::AddClassroomBubbleViewIfNeeded<
+              ClassroomBubbleStudentView>,
+          weak_ptr_factory_.GetWeakPtr(), classroom_bubble_student_view_));
+    }
+    if (!classroom_bubble_teacher_view_) {
+      classroom_client->IsTeacherRoleActive(base::BindOnce(
+          &GlanceableTrayBubbleView::AddClassroomBubbleViewIfNeeded<
+              ClassroomBubbleTeacherView>,
+          weak_ptr_factory_.GetWeakPtr(), classroom_bubble_teacher_view_));
+    }
+  }
 }
 
 bool GlanceableTrayBubbleView::CanActivate() const {
   return true;
+}
+
+template <typename T>
+void GlanceableTrayBubbleView::AddClassroomBubbleViewIfNeeded(
+    T* view,
+    bool is_role_active) {
+  if (!is_role_active) {
+    return;
+  }
+
+  // Add classroom bubble before `calendar_view_`.
+  auto* const scroll_contents = scroll_view_->contents();
+  const auto calendar_view_index =
+      std::find(scroll_contents->children().begin(),
+                scroll_contents->children().end(), calendar_view_) -
+      scroll_contents->children().begin();
+  view = scroll_contents->AddChildViewAt(
+      std::make_unique<T>(detailed_view_delegate_.get()), calendar_view_index);
+  view->SetProperty(views::kMarginsKey, gfx::Insets::TLBR(8, 0, 0, 0));
 }
 
 }  // namespace ash
