84e631f32dcc7c9506b4f93a6e0128f44fdb1fe0
Colin Kincaid <ckincaid@chromium.org>
2023-07-17 21:47:48 +0000

    multipaste: Create ClipboardHistoryUrlTitleFetcher abstraction
    This CL introduces the ClipboardHistoryUrlTitleFetcher, which queries
    the active user's browsing history whenever the user copies text. A
    subsequent CL will add logic so that if the text is determined to be the
    URL for a page the user has visited, that page title will be displayed
    underneath the URL in the clipboard history menu. This UI change will
    help users more easily identify the URLs in their clipboard history.
    
    Bug: b/267694762
    Change-Id: I46f5a9a50796a93c0956bf3444fbacf1d61ed762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4598400
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171400}

---
+++ chrome/browser/ui/ash/clipboard_history_url_title_fetcher_impl.h
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ASH_CLIPBOARD_HISTORY_URL_TITLE_FETCHER_IMPL_H_
+#define CHROME_BROWSER_UI_ASH_CLIPBOARD_HISTORY_URL_TITLE_FETCHER_IMPL_H_
+
+#include <string>
+
+#include "base/functional/callback_forward.h"
+#include "base/task/cancelable_task_tracker.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+class GURL;
+
+namespace history {
+struct QueryURLResult;
+}  // namespace history
+
+// Implements the singleton `ClipboardHistoryUrlTitleFetcher`.
+// TODO(http://b/267694762): Add the interface that this class implements.
+class ClipboardHistoryUrlTitleFetcherImpl {
+ public:
+  ClipboardHistoryUrlTitleFetcherImpl();
+  ClipboardHistoryUrlTitleFetcherImpl(ClipboardHistoryUrlTitleFetcherImpl&) =
+      delete;
+  ClipboardHistoryUrlTitleFetcherImpl& operator=(
+      ClipboardHistoryUrlTitleFetcherImpl&) = delete;
+  ~ClipboardHistoryUrlTitleFetcherImpl();
+
+  using OnHistoryQueryCompleteCallback =
+      base::OnceCallback<void(absl::optional<std::u16string>)>;
+  void QueryHistory(const GURL& url, OnHistoryQueryCompleteCallback callback);
+
+ private:
+  // Interprets the `result` from querying the history service and passes the
+  // extracted title, if any, to the client via `callback`.
+  void OnHistoryQueryComplete(OnHistoryQueryCompleteCallback callback,
+                              history::QueryURLResult result) const;
+
+  // Cancels URL title queries if `this` is destroyed.
+  base::CancelableTaskTracker task_tracker_;
+};
+
+#endif  // CHROME_BROWSER_UI_ASH_CLIPBOARD_HISTORY_URL_TITLE_FETCHER_IMPL_H_
