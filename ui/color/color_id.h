e0a3e3215cfaf1008ea93ddfe7325c70b502e0a1
David Yeung <dayeung@chromium.org>
2023-07-17 18:14:08 +0000

    [CR2023] Change icon colors to disabled when menu item is disabled.
    Icon colors need to be disabled if the menu item is disabled.
    
    Screenshots:
    https://screenshot.googleplex.com/4CKisDiipeM6rqA
    
    Bug: 1463070
    Change-Id: I32137f165bc3ad427e5b28e01a9695694dfee867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678526
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1171282}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -330,6 +330,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorMenuButtonBackgroundSelected) \
   E_CPONLY(kColorMenuDropmarker) \
   E_CPONLY(kColorMenuIcon) \
+  E_CPONLY(kColorMenuIconDisabled) \
   E_CPONLY(kColorMenuItemBackgroundAlertedInitial) \
   E_CPONLY(kColorMenuItemBackgroundAlertedTarget) \
   E_CPONLY(kColorMenuItemBackgroundHighlighted) \
