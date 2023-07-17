e7ee0b614a9143479d3d601695dfff30e5875104
Aliona DANGLA <alionadangla@google.com>
2023-07-17 13:57:41 +0000

    [iOS] Move metrics notification directly in the grid mediator
    This CL ensures that each grid manages its metrics log, so the tab grid
    mediator do not have to handle it anymore.
    
    Bug: 1457146
    Change-Id: Idc0ad2c1f6a0b8fb0c31c1dae1c5a02943860e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679811
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171160}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_mediator.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_mediator.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/recent_tabs/recent_tabs_mediator.h"
 
 #import "base/debug/dump_without_crashing.h"
+#import "base/metrics/user_metrics.h"
+#import "base/metrics/user_metrics_action.h"
 #import "base/notreached.h"
 #import "components/sessions/core/tab_restore_service.h"
 #import "components/sync/service/sync_service.h"
@@ -13,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync_sessions/open_tabs_ui_delegate.h"
 #import "components/sync_sessions/session_sync_service.h"
 #import "components/sync_sessions/synced_session.h"
+#import "ios/chrome/browser/default_browser/utils.h"
 #import "ios/chrome/browser/favicon/favicon_loader.h"
 #import "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
 #import "ios/chrome/browser/net/crurl.h"
@@ -283,6 +286,8 @@ bool UserActionIsRequiredToHaveTabSyncWork(syncer::SyncService* sync_service) {
 #pragma mark - TabGridPageMutator
 
 - (void)currentlySelectedGrid:(BOOL)selected {
+  base::RecordAction(base::UserMetricsAction("MobileTabGridSelectRemotePanel"));
+  LogLikelyInterestedDefaultBrowserUserActivity(DefaultPromoTypeAllTabs);
   // TODO(crbug.com/1457146): Implement.
 }
 
--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_mediator.mm
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_mediator.h"
 
+#import "base/metrics/user_metrics.h"
+#import "base/metrics/user_metrics_action.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -14,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - TabGridPageMutator
 
 - (void)currentlySelectedGrid:(BOOL)selected {
+  base::RecordAction(
+      base::UserMetricsAction("MobileTabGridSelectRegularPanel"));
   // TODO(crbug.com/1457146): Implement.
 }
 
