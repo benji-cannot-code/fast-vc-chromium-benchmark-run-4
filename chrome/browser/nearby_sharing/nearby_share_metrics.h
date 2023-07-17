f37a93d65d563bb0a6dae4c1cf867fec42c9e279
Jack Shira <jackshira@google.com>
2023-07-17 19:02:56 +0000

    [Nearby] Log duration metrics
    Adds UMA logging to `NearbyShareMetricsLogger`.
    
    TEST=unittested, manually verified via chrome://histograms
    
    Change-Id: Ie1e62c6e23cb5ba4686815265cf038e85b0b7992
    Bug: b/279464086
    Fixed: b/279464086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652967
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171305}

--- chrome/browser/nearby_sharing/nearby_share_metrics.h
+++ chrome/browser/nearby_sharing/nearby_share_metrics.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nearby_sharing/share_target.h"
 #include "chrome/browser/nearby_sharing/transfer_metadata.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chromeos/ash/services/nearby/public/mojom/nearby_connections_types.mojom-shared.h"
 #include "chromeos/ash/services/nearby/public/mojom/nearby_connections_types.mojom.h"
 #include "chromeos/ash/services/nearby/public/mojom/nearby_decoder_types.mojom.h"
 #include "chromeos/ash/services/nearby/public/mojom/nearby_share_target_types.mojom.h"
@@ -140,5 +141,45 @@ void RecordNearbyShareSetupNotificationFlowEvent(
 void RecordNearbyShareSetupNotificationTimeToAction(base::TimeDelta time);
 
 void RecordNearbyShareWifiConfigurationResultMetric(bool success);
+
+void RecordNearbyShareDiscoveredToConnectionEstablishedDuration(
+    base::TimeDelta delta);
+
+void RecordNearbyShareInitiatedToSentIntroductionFrameDuration(
+    base::TimeDelta delta);
+
+void RecordNearbyShareEndpointDecodedToReceivedIntroductionFrameDuration(
+    base::TimeDelta delta);
+
+void RecordNearbyShareConnectionEstablishedToBandwidthUpgradeDuration(
+    nearby::connections::mojom::Medium medium,
+    base::TimeDelta delta);
+
+void RecordNearbyShareHighVisibilityEndpointDecodedToBandwidthUpgradeDuration(
+    nearby::connections::mojom::Medium medium,
+    base::TimeDelta delta);
+
+void RecordNearbyShareNonHighVisibilityPairedKeyCompleteToBandwidthUpgradeDuration(
+    nearby::connections::mojom::Medium medium,
+    base::TimeDelta delta);
+
+void RecordNearbyShareBandwidthUpgradeToAllFilesReceivedDuration(
+    nearby::connections::mojom::Medium medium,
+    base::TimeDelta delta);
+
+void RecordNearbyShareAcceptedTransferToAllFilesReceivedDuration(
+    base::TimeDelta delta);
+
+void RecordNearbyShareReceivedIntroductionFrameToAllFilesReceivedDuration(
+    base::TimeDelta delta);
+
+void RecordNearbyShareBandwidthUpgradeToAllFilesSentDuration(
+    nearby::connections::mojom::Medium medium,
+    base::TimeDelta delta);
+
+void RecordNearbyShareStartSendFilesToAllFilesSentDuration(
+    base::TimeDelta delta);
+
+void RecordNearbyShareInitiatedToAllFilesSentDuration(base::TimeDelta delta);
 
 #endif  // CHROME_BROWSER_NEARBY_SHARING_NEARBY_SHARE_METRICS_H_
