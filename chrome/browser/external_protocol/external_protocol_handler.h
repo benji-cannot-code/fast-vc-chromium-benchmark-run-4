98ea079f6fa92166df4cb3f922c5eb1edf899830
Robert Sesek <rsesek@chromium.org>
2023-07-17 14:01:59 +0000

    Add the PromptForExternalNewsSchemes feature
    This enabled-by-default feature changes the external protocol handling
    to not treat news: and snews: schemes as default-allowed. If a handler
    is present, the user will be prompted to open the application, as with
    other arbitrary app schemes. With this feature enabled, only mailto:
    remains default-allowed.
    
    Bug: 1379358
    Change-Id: Ic136e19728c69599ccbba9172679388cbcb855c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678089
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171162}

--- chrome/browser/external_protocol/external_protocol_handler.h
+++ chrome/browser/external_protocol/external_protocol_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/features.h"
 #include "build/build_config.h"
 #include "chrome/browser/shell_integration.h"
 #include "content/public/browser/web_contents.h"
@@ -28,12 +29,31 @@ class GURL;
 class PrefRegistrySimple;
 class Profile;
 
+BASE_DECLARE_FEATURE(kPromptForExternalNewsSchemes);
+
 class ExternalProtocolHandler {
  public:
   enum BlockState {
     DONT_BLOCK,
     BLOCK,
     UNKNOWN,
+  };
+
+  // This is used to back a UMA histogram, so it should be treated as
+  // append-only.
+  // This metric is related to BlockState but is only used for metrics
+  // reporting, and it differentiates multiple possible states that map to
+  // BlockState.
+  enum class BlockStateMetric {
+    kDeniedDefault,
+    kAllowedDefaultMail,
+    kAllowedDefaultNews,
+    kNewsNotDefault,
+    kAllowedByEnterprisePolicy,
+    kAllowedByPreference,
+    kPrompt,
+    // Insert new metric values above this line and update kMaxValue.
+    kMaxValue = kPrompt,
   };
 
   // This is used to back a UMA histogram, so it should be treated as
@@ -74,6 +94,7 @@ class ExternalProtocolHandler {
   };
 
   // UMA histogram metric names.
+  static const char kBlockStateMetric[];
   static const char kHandleStateMetric[];
 
   ExternalProtocolHandler(const ExternalProtocolHandler&) = delete;
