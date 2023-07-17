096ed80f902da1609223cfaea834fd1be95bee47
Asami Doi <asamidoi@chromium.org>
2023-07-17 15:32:12 +0000

    [code health] Remove GetActiveIndex() from SyncedWindowDelegate
    This CL removes GetActiveIndex() from SyncedWindowDelegate because there is no caller of this method.
    
    Change-Id: Icf978cf45fb70242831c92831a6b4987e4fa81ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683257
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171196}

--- chrome/browser/ui/sync/browser_synced_window_delegate.cc
+++ chrome/browser/ui/sync/browser_synced_window_delegate.cc
@@ -52,10 +52,6 @@ int BrowserSyncedWindowDelegate::GetTabCount() const {
   return browser_->tab_strip_model()->count();
 }
 
-int BrowserSyncedWindowDelegate::GetActiveIndex() const {
-  return browser_->tab_strip_model()->active_index();
-}
-
 bool BrowserSyncedWindowDelegate::IsTypeNormal() const {
   return browser_->is_type_normal();
 }
