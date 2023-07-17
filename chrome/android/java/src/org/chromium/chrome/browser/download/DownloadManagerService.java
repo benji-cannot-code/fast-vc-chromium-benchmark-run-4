a03e531b1f1fdbef69abb148bb40a6a346316a20
Shubham Gupta <shubh.gupta@samsung.com>
2023-07-17 17:16:59 +0000

    Remove remaining of DownloadStatsUma
    BUG=1055424
    
    Change-Id: I3a3ebe56e2daa977085228bf1fb14b3335550c65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4686712
    Auto-Submit: Shubham Gupta <shubh.gupta@samsung.com>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171240}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -40,7 +40,6 @@ import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
 import org.chromium.chrome.browser.media.MediaViewerUtils;
-import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.OTRProfileID;
@@ -94,7 +93,6 @@ public class DownloadManagerService implements DownloadController.Observer,
     private static boolean sIsNetworkListenerDisabled;
     private static boolean sIsNetworkMetered;
 
-    private final SharedPreferencesManager mSharedPrefs;
     private final HashMap<String, DownloadProgress> mDownloadProgressMap =
             new HashMap<String, DownloadProgress>(4, 0.75f);
 
@@ -225,7 +223,6 @@ public class DownloadManagerService implements DownloadController.Observer,
     protected DownloadManagerService(
             DownloadNotifier downloadNotifier, Handler handler, long updateDelayInMillis) {
         Context applicationContext = ContextUtils.getApplicationContext();
-        mSharedPrefs = SharedPreferencesManager.getInstance();
         mDownloadNotifier = downloadNotifier;
         mUpdateDelayInMillis = updateDelayInMillis;
         mHandler = handler;
@@ -241,8 +238,6 @@ public class DownloadManagerService implements DownloadController.Observer,
     @VisibleForTesting
     protected void init() {
         DownloadController.setDownloadNotificationService(this);
-        // Clean up unused shared prefs. TODO(qinmin): remove this after M84.
-        mSharedPrefs.removeKey(ChromePreferenceKeys.DOWNLOAD_UMA_ENTRY);
     }
 
     /**
