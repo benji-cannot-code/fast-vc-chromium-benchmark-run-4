2681e9e86acd16651952cd6989bdb0f072bb2bc8
Orin Jaworski <orinj@google.com>
2023-07-17 23:52:01 +0000

    Reland^2 [omnibox] Instantiate presenter with webui omnibox popup view
    This is another reland, but with one additional change:
    Only run webui omnibox tests on Linux, Windows, and Mac.
    This is just extra precautionary guarding and avoids
    unnecessary burden on bots for mobile platforms, it is
    not related to the primary MenuItemViewTestInsert02 test failures.
    
    Note, this test causing reverts was disabled due to flakiness
    and its failures continued without this change.
    
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/menu_item_view_interactive_uitest.cc;drc=f53659988905da63263c9c04c1fdf996cf922c84;l=149
    
    There may be some threading and race issues with these menu tests:
    https://crrev.com/c/4683917?tab=checks
    
    See for more context:
    https://crrev.com/c/4683018
    
    Bug: 1458266, 1396174
    Change-Id: I335c4086c6298d9685d23f32322c6186d7daf2ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685646
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171468}

--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget_observer.h"
 
 class LocationBarView;
+class OmniboxController;
 class RealboxHandler;
 
 // An assistant class for OmniboxPopupViewWebUI, this manages a WebView and a
@@ -25,7 +26,8 @@ class OmniboxPopupPresenter : public views::WebView,
                               public views::WidgetObserver {
  public:
   METADATA_HEADER(OmniboxPopupPresenter);
-  explicit OmniboxPopupPresenter(LocationBarView* location_bar_view);
+  explicit OmniboxPopupPresenter(LocationBarView* location_bar_view,
+                                 OmniboxController* controller);
   OmniboxPopupPresenter(const OmniboxPopupPresenter&) = delete;
   OmniboxPopupPresenter& operator=(const OmniboxPopupPresenter&) = delete;
   ~OmniboxPopupPresenter() override;
@@ -34,8 +36,15 @@ class OmniboxPopupPresenter : public views::WebView,
   void Show();
   void Hide();
 
+  // Tells whether the popup widget exists.
+  bool IsShown() const;
+
+  // Tells whether the WebUI handler is loaded and ready to receive calls.
+  // This must return true before any calls to GetHandler are made.
+  bool IsHandlerReady();
+
   // Get the handler for communicating with the WebUI interface.
-  RealboxHandler* GetWebUIHandler();
+  RealboxHandler* GetHandler();
 
   // views::WidgetObserver:
   void OnWidgetDestroyed(views::Widget* widget) override;
