852ae60a00892a2c79d52e0881970e458bb925d3
Olesia Marukhno <olesiamarukhno@google.com>
2023-07-17 09:25:56 +0000

    [cookie controls] Add toggle to page info
    Add toggle to the new page info third-party cookies section. It is the
    opposite of the current toggle (old: Block 3PC, new: 3PC).
    
    Handle managed states: if the setting is enforced, hide labels and the toggle and show the enforced icon instead.
    
    Move helper methods for getting icons and tooltips based on the status
    and enforcement to CookieControlsUtil to reuse them between desktop UIs
    (page info and cookie controls bubble).
    
    Bug: 1446230, b:285314965, b:285315065
    Change-Id: Ied15f941e56389dfef1f85d78b5447d388f77cab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675804
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Auto-Submit: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171087}

---
+++ components/content_settings/browser/ui/cookie_controls_util.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CONTENT_SETTINGS_BROWSER_UI_COOKIE_CONTROLS_UTIL_H_
+#define COMPONENTS_CONTENT_SETTINGS_BROWSER_UI_COOKIE_CONTROLS_UTIL_H_
+
+#include "base/time/time.h"
+#include "components/content_settings/core/common/cookie_controls_enforcement.h"
+#include "ui/gfx/vector_icon_types.h"
+
+namespace content_settings {
+
+class CookieControlsUtil {
+ public:
+  static int GetDaysToExpiration(base::Time expiration);
+
+  static const gfx::VectorIcon& GetEnforcedIcon(
+      CookieControlsEnforcement enforcement);
+  static int GetEnforcedTooltipTextId(CookieControlsEnforcement enforcement);
+};
+
+}  // namespace content_settings
+
+#endif  // COMPONENTS_CONTENT_SETTINGS_BROWSER_UI_COOKIE_CONTROLS_UTIL_H_
