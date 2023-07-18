742bc6039adf7b8854a9ef6a1fa04c1609c668bb
Joel Hockey <joelhockey@chromium.org>
2023-07-18 03:16:31 +0000

    Set all GuestOs keyed services to redirect in incognito
    GuestOS is not available for Guest profiles, but should redirect in
    incognito to the regular profile.
    
    Bug: 1418376
    Bug: b/291522112
    Bug: b/288985423
    Bug: b/205659805
    Change-Id: Iad657458204cdb8e4275257a3d57eb4ebbbe979b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4692561
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171557}

--- chrome/browser/ash/borealis/borealis_service_factory.cc
+++ chrome/browser/ash/borealis/borealis_service_factory.cc
@@ -27,9 +27,7 @@ BorealisServiceFactory::BorealisServiceFactory()
           "BorealisService",
           ProfileSelections::Builder()
               .WithRegular(ProfileSelection::kRedirectedToOriginal)
-              // Required by BorealisApps for tests
-              // TODO(b/288985423) Change this to kNone
-              .WithGuest(ProfileSelection::kRedirectedToOriginal)
+              .WithGuest(ProfileSelection::kNone)
               .WithAshInternals(ProfileSelection::kNone)
               .WithSystem(ProfileSelection::kNone)
               .Build()) {}
