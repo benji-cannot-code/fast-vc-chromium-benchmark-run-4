3947fbb2504a664bf03de61ac56ba9bc65f22dd4
Daniel Rubery <drubery@chromium.org>
2023-07-18 16:18:45 +0000

    Allow download bubble to remain open for deep scanning status changes
    This CL allows the download bubble to remain open and on the security
    subpage when the user begins a deep scan. The user transitions directly
    from the "prompt for scanning" subpage to the "scan in progress"
    subpage.
    
    Bug: 1424527
    Change-Id: Id0db2a1e635ef538c2b73f14a53fb5703cfe82ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4691346
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171791}

--- chrome/browser/download/bubble/download_bubble_ui_controller.cc
+++ chrome/browser/download/bubble/download_bubble_ui_controller.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_stats.h"
 #include "components/feature_engagement/public/tracker.h"
 #include "components/offline_items_collection/core/offline_content_aggregator.h"
+#include "components/safe_browsing/core/common/features.h"
 #include "content/public/browser/download_manager.h"
 
 namespace {
@@ -247,6 +248,36 @@ void DownloadBubbleUIController::ProcessDownloadButtonPress(
     default:
       NOTREACHED() << "Unexpected button pressed on download bubble: "
                    << command;
+  }
+}
+
+bool DownloadBubbleUIController::ProcessDownloadButtonPressWithClose(
+    DownloadUIModel* model,
+    DownloadCommands::Command command,
+    bool is_main_view) {
+  ProcessDownloadButtonPress(model, command, is_main_view);
+  switch (command) {
+    case DownloadCommands::KEEP:
+    case DownloadCommands::DISCARD:
+    case DownloadCommands::REVIEW:
+    case DownloadCommands::RETRY:
+    case DownloadCommands::CANCEL:
+    case DownloadCommands::BYPASS_DEEP_SCANNING:
+    case DownloadCommands::RESUME:
+    case DownloadCommands::PAUSE:
+    case DownloadCommands::OPEN_WHEN_COMPLETE:
+    case DownloadCommands::SHOW_IN_FOLDER:
+    case DownloadCommands::ALWAYS_OPEN_TYPE:
+    case DownloadCommands::CANCEL_DEEP_SCAN:
+    case DownloadCommands::LEARN_MORE_SCANNING:
+      return true;
+    case DownloadCommands::DEEP_SCAN:
+      return !base::FeatureList::IsEnabled(
+          safe_browsing::kDeepScanningUpdatedUX);
+    default:
+      NOTREACHED() << "Unexpected button pressed on download bubble: "
+                   << command;
+      return true;
   }
 }
 
