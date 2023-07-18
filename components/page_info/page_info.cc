44737cff1c4dc0c7242f2783c213e213c815b065
Olesia Marukhno <olesiamarukhno@google.com>
2023-07-18 09:51:59 +0000

    [cookie controls] Record page info closed action
    Record page info closed action to have a clean session ended signal and
    help with user actions analysis.
    
    Bug: 1446230, b:285858367
    Change-Id: Iada9fbefd26b997a5d7173f1b8aaf9715fb663d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4687714
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171667}

--- components/page_info/page_info.cc
+++ components/page_info/page_info.cc
@@ -319,6 +319,8 @@ PageInfo::~PageInfo() {
                                                   safety_tip_info_.status),
         start_time_);
   }
+
+  base::RecordAction(base::UserMetricsAction("PageInfo.Closed"));
 }
 
 void PageInfo::OnStatusChanged(CookieControlsStatus status,
