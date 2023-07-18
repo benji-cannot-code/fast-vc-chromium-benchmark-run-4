566a797e5ea713e246dc2549a145a34404190437
Bartek Nowierski <bartekn@chromium.org>
2023-07-18 18:31:36 +0000

    Rename to AbiBitnessSupport -> CpuAbiBitnessSupport
    The old names haven't been consumed by anybody yet (human or software)
    
    It's more consistent with PrimaryCpuAbiBitness
    
    Bug: 1462131
    Change-Id: Id5b503f9ff79b5a7c0cc15a30463c5fa28a33f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4695693
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171883}

--- android_webview/browser/metrics/android_metrics_provider_unittest.cc
+++ android_webview/browser/metrics/android_metrics_provider_unittest.cc
@@ -36,7 +36,7 @@ class AndroidMetricsProviderTest : public testing::Test {
 TEST_F(AndroidMetricsProviderTest, ProvideCurrentSessionData) {
   metrics_provider_.ProvideCurrentSessionData(&uma_proto_);
   histogram_tester_.ExpectTotalCount("Android.VersionCode", 1);
-  histogram_tester_.ExpectTotalCount("Android.AbiBitnessSupport", 1);
+  histogram_tester_.ExpectTotalCount("Android.CpuAbiBitnessSupport", 1);
   histogram_tester_.ExpectTotalCount("Android.MultipleUserProfilesState", 1);
   histogram_tester_.ExpectTotalCount("Android.WebView.PrimaryCpuAbiBitness", 1);
 }
@@ -44,7 +44,7 @@ TEST_F(AndroidMetricsProviderTest, ProvideCurrentSessionData) {
 TEST_F(AndroidMetricsProviderTest, ProvidePreviousSessionData) {
   metrics_provider_.ProvidePreviousSessionData(&uma_proto_);
   histogram_tester_.ExpectTotalCount("Android.VersionCode", 0);
-  histogram_tester_.ExpectTotalCount("Android.AbiBitnessSupport", 1);
+  histogram_tester_.ExpectTotalCount("Android.CpuAbiBitnessSupport", 1);
   histogram_tester_.ExpectTotalCount("Android.MultipleUserProfilesState", 1);
   histogram_tester_.ExpectTotalCount("Android.WebView.PrimaryCpuAbiBitness", 0);
 }
@@ -55,7 +55,7 @@ TEST_F(AndroidMetricsProviderTest,
   pref_service_.SetInteger(prefs::kPrimaryCpuAbiBitnessPref, 1);
   metrics_provider_.ProvidePreviousSessionData(&uma_proto_);
   histogram_tester_.ExpectTotalCount("Android.VersionCode", 1);
-  histogram_tester_.ExpectTotalCount("Android.AbiBitnessSupport", 1);
+  histogram_tester_.ExpectTotalCount("Android.CpuAbiBitnessSupport", 1);
   histogram_tester_.ExpectTotalCount("Android.MultipleUserProfilesState", 1);
   histogram_tester_.ExpectTotalCount("Android.WebView.PrimaryCpuAbiBitness", 1);
 }
