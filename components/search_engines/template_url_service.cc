91d87c43d7a8cbe8f3d9f64653c00a6ef9713126
Fabio Tirelo <ftirelo@chromium.org>
2023-07-18 20:28:24 +0000

    [DSP policy] Log histogram for DSP set by policy
    The Chrome Enterprise team is currently looking into providing better
    tools for Enterprise search, and we'd like to track DSEs set by admins
    via policy.
    
    I also did two minor changes in histograms:
    - Replaced csharp@ with myself, since he's no longer working in Chrome
    - Extended the expiration date for the non-Enterprise policy, since it
      is still useful for us to track in 2024
    
    Bug: 1465004
    Change-Id: I0bcf2668965999e8f9a5ae63dc3ff9c4853e38af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4687541
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171947}

--- components/search_engines/template_url_service.cc
+++ components/search_engines/template_url_service.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
+#include "components/search_engines/search_engine_type.h"
 #include "components/search_engines/search_engines_pref_names.h"
 #include "components/search_engines/search_terms_data.h"
 #include "components/search_engines/template_url_prepopulate_data.h"
@@ -1062,10 +1063,15 @@ void TemplateURLService::OnWebDataServiceRequestDone(
   }
 
   if (default_search_provider_) {
-    base::UmaHistogramEnumeration(
-        "Search.DefaultSearchProviderType2",
-        default_search_provider_->GetEngineType(search_terms_data()),
-        SEARCH_ENGINE_MAX);
+    SearchEngineType engine_type =
+        default_search_provider_->GetEngineType(search_terms_data());
+    base::UmaHistogramEnumeration("Search.DefaultSearchProviderType2",
+                                  engine_type, SEARCH_ENGINE_MAX);
+    if (default_search_provider_->created_by_policy()) {
+      base::UmaHistogramEnumeration(
+          "Search.DefaultSearchProviderType2.SetByEnterprisePolicy",
+          engine_type, SEARCH_ENGINE_MAX);
+    }
   }
 }
 
