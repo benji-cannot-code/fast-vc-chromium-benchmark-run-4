ff0c13cb5b943789462821f80873c3d865af8385
Angus L. M. McLean IV <angusmclean@google.com>
2023-07-18 18:20:06 +0000

    Rename ash::StackingController to ash::WindowParentingController
    StackingController is the ash implementation of aura's
    WindowParentingClient, formerly named StackingClient. This updates the
    ash implementation to match the new name, which also more clearly
    communicates its purpose.
    
    This is being done in preparation for changes around hiding windows for
    Welcome Tour, as that may include added new related classes with similar
    names.
    
    Bug: b:277091006
    Change-Id: Ia65c23dec44d2cc3091faf99703872921dc4c32b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4691844
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171869}

--- ash/root_window_controller.h
+++ ash/root_window_controller.h
@@ -52,7 +52,6 @@ class RootWindowLayoutManager;
 class Shelf;
 class ShelfLayoutManager;
 class SplitViewController;
-class StackingController;
 class StatusAreaWidget;
 class SystemModalContainerLayoutManager;
 class SystemWallpaperController;
@@ -60,6 +59,7 @@ class TouchExplorationManager;
 class TouchHudDebug;
 class TouchHudProjection;
 class WallpaperWidgetController;
+class WindowParentingController;
 class WorkAreaInsets;
 
 namespace curtain {
@@ -306,7 +306,7 @@ class ASH_EXPORT RootWindowController {
   std::unique_ptr<AppMenuModelAdapter> root_window_menu_model_adapter_;
   std::unique_ptr<ui::SimpleMenuModel> sort_apps_submenu_;
 
-  std::unique_ptr<StackingController> stacking_controller_;
+  std::unique_ptr<WindowParentingController> window_parenting_controller_;
 
   std::unique_ptr<SplitViewController> split_view_controller_;
 
---
+++ ash/wm/window_parenting_controller.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2012 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_WINDOW_PARENTING_CONTROLLER_H_
+#define ASH_WM_WINDOW_PARENTING_CONTROLLER_H_
+
+#include "ui/aura/client/window_parenting_client.h"
+
+namespace ash {
+
+class WindowParentingController : public aura::client::WindowParentingClient {
+ public:
+  WindowParentingController();
+
+  WindowParentingController(const WindowParentingController&) = delete;
+  WindowParentingController& operator=(const WindowParentingController&) =
+      delete;
+
+  ~WindowParentingController() override;
+
+  // aura::client::WindowParentingClient:
+  aura::Window* GetDefaultParent(aura::Window* window,
+                                 const gfx::Rect& bounds) override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_WM_WINDOW_PARENTING_CONTROLLER_H_
