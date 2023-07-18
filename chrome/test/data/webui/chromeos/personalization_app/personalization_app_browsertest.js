aa7d52d6ede4b40c368ed905f092826be6e48b57
Erica Lee <ericamlee@google.com>
2023-07-18 21:33:47 +0000

    personalization: local UseKMeans pref
    Take into account the user's kmeans pref while showing the login screen.
    
    BUG=b:288149990
    TEST=ash_unittests *WallpaperController*
    TEST=ash_unittests *ColorPaletteController*
    TEST=ash_unittests *AshColorProvider*
    
    Change-Id: If52dc357d6dc48af0e7db3a9827affff8c3c06e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670123
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171986}

--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_browsertest.js
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_browsertest.js
@@ -668,7 +668,7 @@ TEST_F(
           toggle.click();
           await waitUntil(
               () => originalColor !== getComputedStyle(toggleDescription).color,
-              'toggle failed to update colors');
+              'toggle failed to update colors', 200, 5000);
         }
       }
 
@@ -700,7 +700,7 @@ TEST_F(
 
           await waitUntil(
               () => originalColor !== getComputedStyle(toggleDescription).color,
-              'failed to update colors');
+              'failed to update colors', 200, 5000);
         });
 
         test('shows color scheme options', async () => {
@@ -731,7 +731,7 @@ TEST_F(
                   () => originalColor !==
                       getComputedStyle(toggleDescription).color,
                   'failed to update colors', /* intervalMs= */ 200,
-                  /* timeoutMs= */ 3000);
+                  /* timeoutMs= */ 5000);
             }
 
             const newColor = getComputedStyle(toggleDescription).color;
@@ -781,7 +781,7 @@ TEST_F(
                   () => originalColor !==
                       getComputedStyle(lightButton).backgroundColor,
                   'failed to update colors', /* intervalMs= */ 200,
-                  /* timeoutMs= */ 3000);
+                  /* timeoutMs= */ 5000);
             }
 
             const newColor = getComputedStyle(lightButton).backgroundColor;
