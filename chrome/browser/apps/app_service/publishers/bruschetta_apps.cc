a975fa6e6f1419454d626f15b2f5c7da51452020
Emil Mikulic <easy@google.com>
2023-07-18 07:57:27 +0000

    bruschetta: Add a default icon.
    This is what shows up in the launcher if an app doesn't supply its own
    icon. Previously, it would look like the existing Crostini icon.
    
    Bug: b/247636749
    Test: manually checked Crostini and Bruschetta app groups on a DUT.
    Change-Id: I86f34802cdb0714650351ea216e3ff283d25d525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685031
    Reviewed-by: David Munro <davidmunro@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Emil Mikulic <easy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171622}

--- chrome/browser/apps/app_service/publishers/bruschetta_apps.cc
+++ chrome/browser/apps/app_service/publishers/bruschetta_apps.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
 #include "chrome/browser/ui/ash/shelf/shelf_spinner_controller.h"
 #include "chrome/browser/ui/ash/shelf/shelf_spinner_item_controller.h"
+#include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/cpp/intent.h"
@@ -145,6 +146,10 @@ apps::AppType BruschettaApps::AppType() const {
 
 guest_os::VmType BruschettaApps::VmType() const {
   return guest_os::VmType::BRUSCHETTA;
+}
+
+int BruschettaApps::DefaultIconResourceId() const {
+  return IDR_LOGO_BRUSCHETTA_DEFAULT;
 }
 
 void BruschettaApps::Launch(const std::string& app_id,
