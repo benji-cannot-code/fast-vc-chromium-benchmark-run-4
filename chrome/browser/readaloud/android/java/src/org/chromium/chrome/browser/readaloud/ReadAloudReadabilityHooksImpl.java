e8d15d9294ee85ef158b3397ac7b6b2322d0bb2a
Basia Zimirska <basiaz@google.com>
2023-07-18 16:58:28 +0000

    Reland "Prepare for internal hooks impl."
    This reverts commit bb9394468b175f2a5d28911dde6c5711ee521e2a.
    
    Reason for revert: I will land this and https://crrev.com/i/6160727 back to back which (IIUC) should prevent builder failures this time.
    
    Original change's description:
    > Revert "Prepare for internal hooks impl."
    >
    > This reverts commit 7e9c5bb69ad235d44228ab7bb1c1d15ce9186a2d.
    >
    > Reason for revert: Builders failures complaining about the missing implementation. Reverting, because I'll be OOo for the next 2 days and won;t be able to land the linked internal impl
    >
    > Original change's description:
    > > Prepare for internal hooks impl.
    > >
    > > These changes are needed by a WIP https://crrev.com/i/6160727
    > >
    > >
    > > Bug: b/289247373
    > > Change-Id: I58d80edded2c1e1a2982714446bf1c7d7d84bc4f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681071
    > > Reviewed-by: Ian Wells <iwells@chromium.org>
    > > Commit-Queue: Basia Zimirska <basiaz@google.com>
    > > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1170179}
    >
    > Bug: b/289247373
    > Change-Id: I60e3b735c5ec296ce07bdefed203ea1609cb38f8
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683940
    > Reviewed-by: Basia Zimirska <basiaz@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Ivana Žužić <izuzic@google.com>
    > Auto-Submit: Basia Zimirska <basiaz@google.com>
    > Reviewed-by: Ivana Žužić <izuzic@google.com>
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1170391}
    
    Bug: b/289247373
    Change-Id: I8cf331a64a953da3440dee6a88808b406d82ef93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694622
    Auto-Submit: Basia Zimirska <basiaz@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171809}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudReadabilityHooksImpl.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudReadabilityHooksImpl.java
@@ -5,18 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.readaloud;
 
+import android.content.Context;
+
+import androidx.annotation.Nullable;
 /** Empty implementation of ReadAloudReadabilityHooks. */
 public class ReadAloudReadabilityHooksImpl implements ReadAloudReadabilityHooks {
-    private static ReadAloudReadabilityHooksImpl sHooks;
-
-    private ReadAloudReadabilityHooksImpl(){};
-
-    public static ReadAloudReadabilityHooksImpl getInstance() {
-        if (sHooks == null) {
-            sHooks = new ReadAloudReadabilityHooksImpl();
-        }
-        return sHooks;
-    }
+    public ReadAloudReadabilityHooksImpl(Context context, @Nullable String apiKeyOverride){};
 
     @Override
     public boolean isEnabled() {
