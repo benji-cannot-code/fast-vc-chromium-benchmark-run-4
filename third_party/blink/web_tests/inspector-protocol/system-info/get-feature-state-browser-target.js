2fd9f2d225eedfadb35062d53c9a78524a077252
Domenic Denicola <domenic@chromium.org>
2023-07-18 03:05:01 +0000

    Replace Prerender2Holdback with PrerenderDSEHoldback + PreloadingConfig
    This removes Prerender2Holdback, and introduces PrerenderDSEHoldback.
    Holdback tests which previously relied on Prerender2Holdback now rely on
    either PreloadingConfig or PrerenderDSEHoldback. (Note that DevTools
    already is aware of PreloadingConfig, and it doesn't need to be aware of
    PrerenderDSEHoldback, so some holdback-related code and tests are just
    removed from the DevTools part of the codebase.)
    
    See the linked bug for the motivation.
    
    Bug: 1464173
    Change-Id: I56426e5bae9793c57dd28f98b1be2b7b32a89d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664017
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171551}

--- third_party/blink/web_tests/inspector-protocol/system-info/get-feature-state-browser-target.js
+++ third_party/blink/web_tests/inspector-protocol/system-info/get-feature-state-browser-target.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'Tests SystemInfo.getFeatureState() from browser target');
 
   const response = await testRunner.browserP().SystemInfo.getFeatureState(
-      {featureState: 'PrerenderHoldback'});
+      {featureState: 'PreloadingHoldback'});
   testRunner.log(response);
 
   testRunner.completeTest();
--- third_party/blink/web_tests/inspector-protocol/system-info/get-feature-state-frame-target.js
+++ third_party/blink/web_tests/inspector-protocol/system-info/get-feature-state-frame-target.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'Tests SystemInfo.getFeatureState() from frame target');
 
   const response =
-      await dp.SystemInfo.getFeatureState({featureState: 'PrerenderHoldback'});
+      await dp.SystemInfo.getFeatureState({featureState: 'PreloadingHoldback'});
   testRunner.log(response);
 
   testRunner.completeTest();
