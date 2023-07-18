39f047533f7731cc69486087e6b832484c96aac6
Lucy Qu <lqu@google.com>
2023-07-18 01:34:38 +0000

    Remove workaround for Steam chat windows stealing focus.
    Steam beta changes seem to have fixed this issue.
    
    BUG=b:242008072
    
    Change-Id: I836e42f9204dc1627d68c932899d29830330785d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4649689
    Reviewed-by: Daniel Ng <danielng@google.com>
    Commit-Queue: Lucy Qu <lqu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171520}

--- chrome/browser/ui/ash/shelf/app_service/exo_app_type_resolver.cc
+++ chrome/browser/ui/ash/shelf/app_service/exo_app_type_resolver.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/app_restore/app_restore_utils.h"
 #include "components/app_restore/window_properties.h"
 #include "components/exo/permission.h"
-#include "components/exo/shell_surface_util.h"
 #include "components/exo/window_properties.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/aura/client/aura_constants.h"
@@ -125,15 +124,6 @@ void ExoAppTypeResolver::PopulateProperties(
     // Auto-maximize causes compatibility issues, and we don't need it anyway.
     out_properties_container.SetProperty(chromeos::kAutoMaximizeXdgShellEnabled,
                                          false);
-
-    // In some instances we don't want new borealis windows to steal focus,
-    // instead they are created as minimized windows.
-    // TODO(b/210569001): this is intended to be a temporary solution.
-    if (borealis::BorealisWindowManager::ShouldNewWindowBeMinimized(
-            params.app_id.empty() ? params.startup_id : params.app_id)) {
-      out_properties_container.SetProperty(aura::client::kShowStateKey,
-                                           ui::SHOW_STATE_MINIMIZED);
-    }
     return;
   }
 }
