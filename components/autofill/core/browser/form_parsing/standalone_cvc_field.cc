82ad929ed026f10e2dd87741976fc8f9f0169a3e
Alexander Tekle <alexandertekle@google.com>
2023-07-18 19:56:38 +0000

    Add Standalone Cvc Field to BUILD
    Bug: 1281695
    Change-Id: Ib087b9f871d193dbdc88b013ba36a1023d139af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4696913
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171934}

--- components/autofill/core/browser/form_parsing/standalone_cvc_field.cc
+++ components/autofill/core/browser/form_parsing/standalone_cvc_field.cc
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_parsing/standalone_cvc_field.h"
 
 #include "components/autofill/core/browser/autofill_field.h"
-#include "components/autofill/core/browser/autofill_regex_constants.h"
-#include "components/autofill/core/browser/autofill_regexes.h"
 #include "components/autofill/core/browser/form_parsing/autofill_scanner.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
+#include "components/autofill/core/common/autofill_regex_constants.h"
+#include "components/autofill/core/common/autofill_regexes.h"
 
 namespace autofill {
 
@@ -31,7 +31,7 @@ std::unique_ptr<FormField> StandaloneCvcField::Parse(
     return nullptr;
   }
 
-  AutofillField* field;
+  raw_ptr<AutofillField> field;
   base::span<const MatchPatternRef> cvc_patterns = GetMatchPatterns(
       CREDIT_CARD_VERIFICATION_CODE, page_language, pattern_source);
 
