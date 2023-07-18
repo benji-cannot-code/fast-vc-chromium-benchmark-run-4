1fced564eeb0bbac2caf85d89ae83261378c117b
Viktor Semeniuk <vsemeniuk@google.com>
2023-07-18 08:24:36 +0000

    Clean up kPasswordsGroupingInfoRequested pref
    This pref was used to make one time request to affiliation service for
    grouping info.
    
    Bug: 1420597
    Change-Id: I5375b884bdd84bfe88d10a1c77c8206bbf7914e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684881
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171631}

--- components/password_manager/core/browser/affiliation/affiliation_service_impl.h
+++ components/password_manager/core/browser/affiliation/affiliation_service_impl.h
@@ -34,8 +34,6 @@ namespace url {
 class SchemeHostPort;
 }
 
-class PrefService;
-
 namespace password_manager {
 
 class AffiliationBackend;
@@ -52,8 +50,7 @@ class AffiliationServiceImpl : public AffiliationService,
 
   explicit AffiliationServiceImpl(
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-      scoped_refptr<base::SequencedTaskRunner> backend_task_runner,
-      PrefService* pref_service);
+      scoped_refptr<base::SequencedTaskRunner> backend_task_runner);
   ~AffiliationServiceImpl() override;
 
   AffiliationServiceImpl(const AffiliationServiceImpl& other) = delete;
@@ -131,8 +128,6 @@ class AffiliationServiceImpl : public AffiliationService,
   // shutdown on the backend thread, so it will outlive |this| along with all
   // its in-flight tasks.
   raw_ptr<AffiliationBackend, DanglingAcrossTasks> backend_;
-
-  raw_ptr<PrefService> pref_service_;
 
   scoped_refptr<base::SequencedTaskRunner> backend_task_runner_;
 
