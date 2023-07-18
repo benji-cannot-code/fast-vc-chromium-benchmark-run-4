e1579b3b6c7bdd0b8593bf5589435db4061d7059
Yulun Wu <yulunwu@chromium.org>
2023-07-18 21:17:44 +0000

    Revert "Reland "Authentication content control: SAML blocklist""
    This reverts commit 46328730cd3971ed977775a531cd209a8e860af4.
    
    Reason for revert: Caused test failure:
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome
    
    Original change's description:
    > Reland "Authentication content control: SAML blocklist"
    >
    > This is a reland of commit a734afc153734ffcea929f262c7fc746b2dbe3ed
    >
    > Original change's description:
    > > Authentication content control: SAML blocklist
    > >
    > > Two new device policies (DeviceAuthenticationURLBlocklist,
    > > DeviceAuthenticationURLAllowlist) allow admins to control content
    > > in the login screen and lock screen via a URL blocklist and a
    > > corresponding URL allowlist.  This helps admins close content
    > > control escapes with undesired navigation during SAML flows on
    > > managed devices.
    > >
    > > Bug: https://issuetracker.google.com/246531079
    > > Change-Id: Ic0a1c781956ccce6907f87b3d153b69727052dda
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4075778
    > > Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    > > Reviewed-by: Andrey Davydov <andreydav@google.com>
    > > Reviewed-by: Roman Sorokin <rsorokin@google.com>
    > > Commit-Queue: Manuel Gómez <mgomezch@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1169975}
    >
    > Bug: https://issuetracker.google.com/246531079
    > Change-Id: I15098ce9c275f0526f790cd69f10a45a7e9d16b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685988
    > Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    > Reviewed-by: Roman Sorokin <rsorokin@google.com>
    > Commit-Queue: Manuel Gómez <mgomezch@chromium.org>
    > Reviewed-by: Andrey Davydov <andreydav@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1171771}
    
    Bug: https://issuetracker.google.com/246531079
    Change-Id: Ifdb78f58cd6109c02136b3bd70fb8e7f3207c46e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698235
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Owners-Override: Yulun Wu <yulunwu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1171976}

--- chrome/browser/ui/webui/ash/login/gaia_screen_handler.h
+++ chrome/browser/ui/webui/ash/login/gaia_screen_handler.h
@@ -136,8 +136,7 @@ class GaiaScreenHandler
     FRAME_STATE_UNKNOWN = 0,
     FRAME_STATE_LOADING,
     FRAME_STATE_LOADED,
-    FRAME_STATE_ERROR,
-    FRAME_STATE_BLOCKED
+    FRAME_STATE_ERROR
   };
 
   GaiaScreenHandler(
@@ -197,8 +196,6 @@ class GaiaScreenHandler
   void set_offline_timeout_for_testing(base::TimeDelta offline_timeout) {
     offline_timeout_ = offline_timeout;
   }
-
-  bool IsNavigationBlockedForTesting() const;
 
  private:
   void LoadGaia(const login::GaiaContext& context);
