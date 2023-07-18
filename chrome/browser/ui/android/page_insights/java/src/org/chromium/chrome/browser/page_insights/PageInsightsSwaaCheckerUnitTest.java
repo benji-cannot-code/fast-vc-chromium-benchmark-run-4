845c014f929917a59bef95cc7502b7f065987192
Jinsuk Kim <jinsukkim@chromium.org>
2023-07-18 21:43:45 +0000

    [gbb-cct] Activator for page insights module
    PageInsightsActivator monitors the dynamic settings to instantiate
    page insights component at the first time when all the conditions are
    met, such as sign-in, history sync without a custom passphrase, and
    sWAA. It replaces SwaaChecker (which is now also one of the settings
    it manages) as an interface for UI coordinator.
    
    Low-Coverage-Reason: main class PageInsightsActivator is fully covered.
    Bug: 1448966
    Change-Id: If5ff112f21f5690eb885bc2c8d5e6433edd53da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694464
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171995}

--- chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsSwaaCheckerUnitTest.java
+++ chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsSwaaCheckerUnitTest.java
@@ -6,13 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.page_insights;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.After;
 import org.junit.Before;
@@ -20,7 +18,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.LooperMode;
 
@@ -29,8 +26,6 @@ import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
-import org.chromium.chrome.browser.signin.services.SigninManager;
 
 /**
  * Unit tests for {@link PageInsightsSwaaChecker}.
@@ -48,10 +43,7 @@ public class PageInsightsSwaaCheckerUnitTest {
     private Profile mProfile;
 
     @Mock
-    private IdentityServicesProvider mIdentityServicesProvider;
-
-    @Mock
-    private SigninManager mSigninManager;
+    private Runnable mActivateCallback;
 
     private SharedPreferencesManager mSharedPreferencesManager;
 
@@ -63,10 +55,8 @@ public class PageInsightsSwaaCheckerUnitTest {
         mJniMocker.mock(PageInsightsSwaaCheckerJni.TEST_HOOKS, mPageInsightsSwaaCheckerJni);
         mSharedPreferencesManager = SharedPreferencesManager.getInstance();
         mSharedPreferencesManager.disableKeyCheckerForTesting();
-        IdentityServicesProvider.setInstanceForTests(mIdentityServicesProvider);
-        when(mIdentityServicesProvider.getSigninManager(any())).thenReturn(mSigninManager);
 
-        mSwaaChecker = PageInsightsSwaaChecker.getForProfile(mProfile);
+        mSwaaChecker = new PageInsightsSwaaChecker(mProfile, mActivateCallback);
     }
 
     @After
@@ -79,20 +69,18 @@ public class PageInsightsSwaaCheckerUnitTest {
     public void testIsEnabled() throws Exception {
         assertFalse(mSwaaChecker.isSwaaEnabled().isPresent());
 
-        int token = mSwaaChecker.start();
         mSwaaChecker.onSwaaResponse(false);
         assertFalse(mSwaaChecker.isSwaaEnabled().get());
 
         mSwaaChecker.onSwaaResponse(true);
         assertTrue(mSwaaChecker.isSwaaEnabled().get());
-        mSwaaChecker.stop(token);
     }
 
     @Test
     public void testEmptyOnExpiry() throws Exception {
         long timeNow = 1000L;
         mSwaaChecker.setElapsedRealtimeSupplierForTesting(() -> timeNow);
-        int token = mSwaaChecker.start();
+        mSwaaChecker.start();
         mSwaaChecker.onSwaaResponse(true);
         assertTrue(mSwaaChecker.isSwaaEnabled().get());
 
@@ -105,14 +93,21 @@ public class PageInsightsSwaaCheckerUnitTest {
         long timeNow3 = timeNow + PageInsightsSwaaChecker.REFRESH_PERIOD_MS + 1;
         mSwaaChecker.setElapsedRealtimeSupplierForTesting(() -> timeNow3);
         assertFalse(mSwaaChecker.isSwaaEnabled().isPresent());
-        mSwaaChecker.stop(token);
+    }
+
+    @Test
+    public void testInvokeActivate() throws Exception {
+        mSwaaChecker.onSwaaResponse(false);
+        verify(mActivateCallback, never()).run();
+        mSwaaChecker.onSwaaResponse(true);
+        verify(mActivateCallback).run();
     }
 
     @Test
     public void testMultipleInstancesQueryAndUpdate() throws Exception {
         long timeNow = 1000L;
         mSwaaChecker.setElapsedRealtimeSupplierForTesting(() -> timeNow);
-        int token = mSwaaChecker.start();
+        mSwaaChecker.start();
 
         // Send a query when there is no cached data.
         verify(mPageInsightsSwaaCheckerJni).queryStatus(mSwaaChecker, mProfile);
@@ -122,37 +117,19 @@ public class PageInsightsSwaaCheckerUnitTest {
         assertFalse(mSwaaChecker.isSwaaEnabled().get());
         assertTrue(mSwaaChecker.isUpdateScheduled());
         clearInvocations(mPageInsightsSwaaCheckerJni);
-        mSwaaChecker.stop(token);
 
         // The 2nd CCT instance picks up the cached value without sending a new query
         // if the cache is valid. Verify the handler has an update scheduled.
-        var swaaChecker = PageInsightsSwaaChecker.getForProfile(mProfile);
         long timeNow2 = timeNow + PageInsightsSwaaChecker.REFRESH_PERIOD_MS / 2;
-        swaaChecker.setElapsedRealtimeSupplierForTesting(() -> timeNow2);
-        int token2 = swaaChecker.start();
+        mSwaaChecker.setElapsedRealtimeSupplierForTesting(() -> timeNow2);
+        mSwaaChecker.start();
         verify(mPageInsightsSwaaCheckerJni, never()).queryStatus(any(), any());
-        assertTrue(swaaChecker.isUpdateScheduled());
-        swaaChecker.stop(token2);
-    }
-
-    @Test
-    public void testProfileChange() throws Exception {
-        long timeNow = 1000L;
-        mSwaaChecker.setElapsedRealtimeSupplierForTesting(() -> timeNow);
-        mSwaaChecker.onSwaaResponse(true);
-        assertTrue(mSwaaChecker.isSwaaEnabled().get());
-
-        Profile profile2 = Mockito.mock(Profile.class);
-        var swaaChecker = PageInsightsSwaaChecker.getForProfile(profile2);
-
-        // A new profile creates a new static sWAA checker, and resets the sWAA status.
-        assertNotEquals(mSwaaChecker, swaaChecker);
-        assertFalse(swaaChecker.isSwaaEnabled().isPresent());
+        assertTrue(mSwaaChecker.isUpdateScheduled());
     }
 
     @Test
     public void testReactToSignIn() throws Exception {
-        int token = mSwaaChecker.start();
+        mSwaaChecker.start();
         verify(mPageInsightsSwaaCheckerJni).queryStatus(any(), any());
         mSwaaChecker.onSwaaResponse(false);
         assertFalse(mSwaaChecker.isSwaaEnabled().get());
@@ -165,6 +142,5 @@ public class PageInsightsSwaaCheckerUnitTest {
         // On signing in, a new query is made to update the cache immediately.
         mSwaaChecker.onSignedIn();
         verify(mPageInsightsSwaaCheckerJni).queryStatus(any(), any());
-        mSwaaChecker.stop(token);
     }
 }
