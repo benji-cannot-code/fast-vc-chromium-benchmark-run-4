681d63fc5ddbdd604ba699fb8a2ae484586674e0
Peng Huang <penghuang@chromium.org>
2023-07-18 18:18:09 +0000

    Fix compile error in iosurface_image_backing_factory_unittest.cc
    Bug: 1242712,1464603
    Change-Id: I760f9f379710d267177ac26ce4aca54e5d2940ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4696013
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171867}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing_factory_unittest.cc
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing_factory_unittest.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(USE_DAWN)
+#include <dawn/native/DawnNative.h>
 #include <dawn/webgpu_cpp.h>
 #endif  // BUILDFLAG(USE_DAWN)
 
