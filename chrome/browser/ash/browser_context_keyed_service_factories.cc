76b05679a4be6747f09e9259c1c0c821f8ef1f1a
Dmitry Grebenyuk <dgrebenyuk@google.com>
2023-07-18 02:26:09 +0000

    Make a service from image search
    This CL elevates local images search to a service, which is a
    wrapper around the AnnotationStorage. It allows us to keep a
    global lock for the SQLite database and serve async calls.
    
    Test: "passes unittests. Works on DUT."
    Bug: b:290706016
    Change-Id: Ib3515866a050a336016410c3f0934b77383dc3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4676854
    Commit-Queue: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171534}

--- chrome/browser/ash/browser_context_keyed_service_factories.cc
+++ chrome/browser/ash/browser_context_keyed_service_factories.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/android_sms/android_sms_service_factory.h"
 #include "chrome/browser/ash/app_list/app_sync_ui_state_factory.h"
 #include "chrome/browser/ash/app_list/arc/arc_vpn_provider_manager_factory.h"
+#include "chrome/browser/ash/app_list/search/local_image_search/local_image_search_service_factory.h"
 #include "chrome/browser/ash/app_mode/arc/arc_kiosk_app_service_factory.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_update_service.h"
 #include "chrome/browser/ash/app_restore/app_restore_arc_task_handler_factory.h"
@@ -134,6 +135,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   android_sms::AndroidSmsServiceFactory::GetInstance();
   ApkWebAppServiceFactory::GetInstance();
   app_list::ArcVpnProviderManagerFactory::GetInstance();
+  app_list::LocalImageSearchServiceFactory::GetInstance();
   app_restore::AppRestoreArcTaskHandlerFactory::GetInstance();
   apps::AppDiscoveryServiceFactory::GetInstance();
   apps::ArcAppsFactory::GetInstance();
