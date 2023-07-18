1b97183536fca2fd587dca6d5c0f405b3006bfee
Kevin Radtke <kradtke@chromium.org>
2023-07-18 17:51:44 +0000

    Stop nudge dismiss timer only when nudge is actually hidden
    The dismiss timer could be stopped without actually dismissing the
    nudge, which could cause nudges to stay on-screen forever.
    
    Change order of operations so the dismiss timer will be stopped only if
    the nudge is actually being hidden.
    
    Bug: b:287654951
    Change-Id: I43ed481097281a8c2a7fb94b820e2c2e3803f454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4688176
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171843}

--- ash/system/tray/system_nudge_unittest.cc
+++ ash/system/tray/system_nudge_unittest.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "ui/compositor/layer.h"
+#include "ui/compositor/layer_animator.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/gfx/vector_icon_types.h"
 
 namespace ash {
@@ -39,6 +42,9 @@ constexpr char kNudgeTimeToActionWithin1h[] =
     "Ash.NotifierFramework.Nudge.TimeToAction.Within1h";
 constexpr char kNudgeTimeToActionWithinSession[] =
     "Ash.NotifierFramework.Nudge.TimeToAction.WithinSession";
+
+constexpr base::TimeDelta kNudgeFadeAnimationTime = base::Milliseconds(250);
+constexpr base::TimeDelta kNudgeShowTime = base::Seconds(10);
 
 gfx::VectorIcon kEmptyIcon;
 
@@ -247,6 +253,34 @@ TEST_F(SystemNudgeTest, NudgePositionWithBottomLocked) {
   nudge_bounds.Outset(kNudgeMargin);
   EXPECT_EQ(nudge_bounds.x(), display_bounds.x());
   EXPECT_EQ(nudge_bounds.bottom(), display_bounds.bottom());
+}
+
+TEST_F(SystemNudgeTest, DismissTimer) {
+  ui::ScopedAnimationDurationScaleMode test_duration_mode(
+      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
+  auto nudge_controller = std::make_unique<TestSystemNudgeController>();
+
+  // Show a nudge.
+  EXPECT_FALSE(nudge_controller->GetSystemNudgeForTesting());
+  nudge_controller->ShowNudge();
+  auto* nudge = nudge_controller->GetSystemNudgeForTesting();
+  ASSERT_TRUE(nudge);
+
+  // Attempt hiding the nudge while it's animating in, the hide request should
+  // be ignored.
+  EXPECT_TRUE(nudge->widget()->GetLayer()->GetAnimator()->is_animating());
+  nudge_controller->HideNudge();
+  EXPECT_TRUE(nudge->widget());
+
+  // Fast forward the animation time, the nudge should have finished animating.
+  task_environment()->FastForwardBy(kNudgeFadeAnimationTime +
+                                    (kNudgeShowTime / 2));
+  EXPECT_FALSE(nudge->widget()->GetLayer()->GetAnimator()->is_animating());
+
+  // Fast forward nudge's default duration, the nudge should have been
+  // dismissed.
+  task_environment()->FastForwardBy(kNudgeShowTime + (kNudgeShowTime / 2));
+  EXPECT_FALSE(nudge_controller->GetSystemNudgeForTesting());
 }
 
 }  // namespace ash
