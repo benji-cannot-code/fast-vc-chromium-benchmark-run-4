c123756da5d1d37e25a3864bb402c8df7e99640e
Ahmed Elwasefi <a.m.elwasefi@gmail.com>
2023-07-18 22:05:06 +0000

    [a11y] Changed ax_object_cache_impl to use an enum instead of closures for the callbacks.
    This should improve overall performance and memory.
    
    Change-Id: Id840278f5525a09632462ab75e56880e25058304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678748
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172009}

--- third_party/blink/renderer/core/html/forms/html_form_control_element_test.cc
+++ third_party/blink/renderer/core/html/forms/html_form_control_element_test.cc
@@ -22,7 +22,7 @@ class MockFormValidationMessageClient
     : public GarbageCollected<MockFormValidationMessageClient>,
       public ValidationMessageClient {
  public:
-  void ShowValidationMessage(const Element& anchor,
+  void ShowValidationMessage(Element& anchor,
                              const String&,
                              TextDirection,
                              const String&,
--- third_party/blink/renderer/core/page/validation_message_client_impl.h
+++ third_party/blink/renderer/core/page/validation_message_client_impl.h
@@ -52,7 +52,7 @@ class CORE_EXPORT ValidationMessageClientImpl final
   explicit ValidationMessageClientImpl(Page&);
   ~ValidationMessageClientImpl() override;
 
-  void ShowValidationMessage(const Element& anchor,
+  void ShowValidationMessage(Element& anchor,
                              const String& message,
                              TextDirection message_dir,
                              const String& sub_message,
@@ -69,7 +69,7 @@ class CORE_EXPORT ValidationMessageClientImpl final
   LocalFrameView* CurrentView();
   void HideValidationMessageImmediately(const Element& anchor);
   void Reset(TimerBase*);
-  void ValidationMessageVisibilityChanged(const Element& anchor);
+  void ValidationMessageVisibilityChanged(Element& anchor);
 
   void HideValidationMessage(const Element& anchor) override;
   bool IsValidationMessageVisible(const Element& anchor) override;
@@ -85,7 +85,7 @@ class CORE_EXPORT ValidationMessageClientImpl final
   void WillOpenPopup() override;
 
   Member<Page> page_;
-  Member<const Element> current_anchor_;
+  Member<Element> current_anchor_;
   String message_;
   base::TimeTicks finish_time_;
   Member<DisallowNewWrapper<HeapTaskRunnerTimer<ValidationMessageClientImpl>>>
