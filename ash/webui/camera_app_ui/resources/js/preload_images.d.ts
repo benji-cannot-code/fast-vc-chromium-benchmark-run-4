c20c973e31715d8adabeb2efd27375f467dd7ddd
Pi-Hsun Shih <pihsun@chromium.org>
2023-07-18 09:03:55 +0000

    CCA: Generate Lit template directly in preload_images.js
    To enable trusted types and avoid having to set .innerHTML from
    "arbitrary" value, we generate Lit template directly in
    gen_preload_images_js.py using the `svg` tag from Lit.
    
    Bug: 1465284
    Test: tast run <DUT> camera.CCAUISmoke.*
    Change-Id: I08624b8534456ed9088a0fd68810915c609b3cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4689063
    Reviewed-by: Shik Chen <shik@chromium.org>
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171648}

--- ash/webui/camera_app_ui/resources/js/preload_images.d.ts
+++ ash/webui/camera_app_ui/resources/js/preload_images.d.ts
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TemplateResult} from 'chrome://resources/mwc/lit/index.js';
+
 export const preloadImagesList: string[];
 
 /**
- * Mapping of SVG file name to it's content.
+ * Mapping of SVG file name to it's content as lit template.
  */
-export const preloadedImages: Record<string, string>;
+export const preloadedImages: Map<string, TemplateResult>;
