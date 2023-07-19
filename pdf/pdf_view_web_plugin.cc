682088c4eaad6641a890c3a8162768813f790e15
Hui Yingst <nigi@chromium.org>
2023-07-19 20:33:59 +0000

    Send attachment information based on `kPdfPortfolio` feature flag
    Only sends attachment information to the front end when feature
    `kPdfPortfolio` is enabled. This helps put future UI development for
    PDF attachment behind the feature flag.
    
    Also parameterizes PdfViewWebPluginWithDocInfoTest so that it can
    check if attachment information is sent or not based on whether
    `kPdfPortfolio` is enabled.
    
    Bug: 177188
    Change-Id: I88be529371532f739319472915f91e32f015c185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698674
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Nigi <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172560}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -1107,7 +1107,9 @@ void PdfViewWebPlugin::DocumentLoadComplete() {
 
   RecordDocumentMetrics();
 
-  SendAttachments();
+  if (base::FeatureList::IsEnabled(chrome_pdf::features::kPdfPortfolio)) {
+    SendAttachments();
+  }
   SendBookmarks();
   SendMetadata();
 
