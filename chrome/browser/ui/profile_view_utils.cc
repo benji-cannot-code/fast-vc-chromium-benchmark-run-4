14f10a6103c98126a17bd828d44e15d2c5ed9cd3
David Yeung <dayeung@chromium.org>
2023-07-19 20:25:53 +0000

    [CR2023] Added metric for switching directly to another profile.
    Added a metric for switching directly to another profile. Use the same enum regardless of the profile.
    
    Bug: 1457554
    Change-Id: I8143e4bbe3aa4535c43227b91df1dd7260b88723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4697254
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172555}

--- chrome/browser/ui/profile_view_utils.cc
+++ chrome/browser/ui/profile_view_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/profile_view_utils.h"
 
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/layout_constants.h"
+#include "chrome/browser/ui/toolbar/app_menu_model.h"
 #include "chrome/common/url_constants.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/identity_manager/account_info.h"
@@ -120,4 +122,11 @@ std::vector<ProfileAttributesEntry*> GetAllOtherProfileEntriesForProfileSubMenu(
   }
 
   return result;
+}
+
+bool IsOtherProfileCommand(int command_id) {
+  return command_id >= AppMenuModel::kMinOtherProfileCommandId &&
+         ((command_id - IDC_FIRST_UNBOUNDED_MENU) %
+              AppMenuModel::kNumUnboundedMenuTypes ==
+          (AppMenuModel::kMinOtherProfileCommandId - IDC_FIRST_UNBOUNDED_MENU));
 }
