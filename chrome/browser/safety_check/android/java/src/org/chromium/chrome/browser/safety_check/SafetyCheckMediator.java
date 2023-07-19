68d930969aad1e7bb43159c93ec5042cd574d5cf
Ioana Pandele <ioanap@chromium.org>
2023-07-19 17:42:03 +0000

    [Passwords][Fixit] Password check starting should ensure the bridge is observed
    When safety check is started immediately, setInitialState is not
    called, so the mediator doesn't start observing password store events,
    but still waits for them to happen. This causes password check to
    hang when safety check is started from the omnibox.
    
    Bug: 1462455
    Change-Id: Ib641cfa05e40a151710a34f276dc297a30183ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694484
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1172462}

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
@@ -628,6 +628,7 @@ class SafetyCheckMediator
             return;
         }
 
+        mPasswordStoreBridge.addObserver(this, true);
         WeakReference<SafetyCheckMediator> weakRef = new WeakReference(this);
         PasswordManagerHelper.runPasswordCheckupInBackground(PasswordCheckReferrer.SAFETY_CHECK,
                 getSyncingAccount(),
