a388b7badb73d28f34c7244fab6ee480f9764ab9
Daniel Andersson <dandersson@chromium.org>
2023-07-19 23:41:30 +0000

    ash: Fix typography for desk buttons
    This CL fixes font inconsistencies between the desk bar buttons and desk
    names. The CL fixes both the Jelly and non-Jelly paths.
    
    BUG=b:286455492
    
    Change-Id: Icd64723953bb08be317ac46385be382342b9306e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4702339
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172659}

--- ash/wm/desks/desk_bar_view_base.cc
+++ ash/wm/desks/desk_bar_view_base.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
+#include "ash/style/typography.h"
 #include "ash/utility/haptics_util.h"
 #include "ash/wm/desks/desk_action_view.h"
 #include "ash/wm/desks/desk_mini_view_animations.h"
@@ -588,6 +589,9 @@ DeskBarViewBase::DeskBarViewBase(aura::Window* root, Type type)
                                   base::Unretained(this))));
       library_button_label_ =
           scroll_view_contents_->AddChildView(std::make_unique<views::Label>());
+      library_button_label_->SetFontList(
+          TypographyProvider::Get()->ResolveTypographyToken(
+              TypographyToken::kCrosAnnotation1));
       library_button_label_->SetPaintToLayer();
       library_button_label_->layer()->SetFillsBoundsOpaquely(false);
     } else {
--- ash/wm/desks/expanded_desks_bar_button.cc
+++ ash/wm/desks/expanded_desks_bar_button.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ash/style/color_util.h"
 #include "ash/style/style_util.h"
+#include "ash/style/typography.h"
 #include "ash/wm/desks/desk_bar_view_base.h"
 #include "ash/wm/desks/desk_button_base.h"
 #include "ash/wm/desks/desk_mini_view.h"
@@ -138,6 +139,8 @@ ExpandedDesksBarButton::ExpandedDesksBarButton(
   SetPaintToLayer();
   layer()->SetFillsBoundsOpaquely(false);
   label_->SetHorizontalAlignment(gfx::ALIGN_CENTER);
+  label_->SetFontList(TypographyProvider::Get()->ResolveTypographyToken(
+      TypographyToken::kCrosAnnotation1));
   SetButtonState(initially_enabled);
 
   views::InstallRoundRectHighlightPathGenerator(
