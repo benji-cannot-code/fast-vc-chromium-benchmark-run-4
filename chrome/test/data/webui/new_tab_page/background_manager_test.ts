7e101b6abcb4cf13354cc032f29b211ad701f23d
Paul Adedeji <pauladedeji@google.com>
2023-07-19 15:22:34 +0000

    [ntp] Prevent background image from being set twice on initial NTP load
    This is what happens when the NTP is first opened today:
    
    1. new_tab_page_ui.cc sets the background image [1].
    2. new_tab_page_handler.cc sets the theme data [2].
    3. app.ts sets the background image info [3] via background_manager.ts [4]
    
    The URL in step #1 does not include the scrim display parameter while the URL in step #3 does.
    This CL makes it so that the scrim display is set properly in both cases and
    background_manager.ts knows that the incoming URL is the same as the image that is
    already set [5].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc;drc=5788878ec2766e9917dc8f39e822cab9eb66dbd7;l=663
    [2]
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/new_tab_page/new_tab_page_handler.cc;drc=75e08319ecff189d180eeab8236a09a4d84f1920;l=467
    [3] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/resources/new_tab_page/app.ts;drc=d1e0e09e0e7eb412492819a346c508f3b13d8265;l=651
    [4] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/resources/new_tab_page/background_manager.ts;drc=a23cf0a9c340cee05d40d2e2affa352387e63d51;l=91
    [5]
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/resources/new_tab_page/background_manager.ts;drc=a23cf0a9c340cee05d40d2e2affa352387e63d51;l=116
    
    
    Change-Id: I0873aca40d14d7707c05c2c12c6df51198e148d2
    Bug: 1464597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683806
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172384}

--- chrome/test/data/webui/new_tab_page/background_manager_test.ts
+++ chrome/test/data/webui/new_tab_page/background_manager_test.ts
@@ -90,7 +90,6 @@ suite('NewTabPageBackgroundManagerTest', () => {
       repeatY: 'repeat',
       positionX: 'left',
       positionY: 'top',
-      scrimDisplay: 'none',
       imageSource: NtpBackgroundImageSource.kFirstPartyThemeWithoutDailyRefresh,
     });
 
@@ -100,7 +99,7 @@ suite('NewTabPageBackgroundManagerTest', () => {
         `url=${encodeURIComponent('https://example.com')}&` +
         `url2x=${encodeURIComponent('https://example2x.com')}&` +
         'size=cover&repeatX=no-repeat&repeatY=repeat&positionX=left&' +
-        'positionY=top&scrimDisplay=none';
+        'positionY=top';
     assertEquals(expected, backgroundImage.url);
   });
 
