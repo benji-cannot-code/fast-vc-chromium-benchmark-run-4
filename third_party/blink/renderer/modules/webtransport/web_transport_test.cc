9ee67939c53d40d8c32174fdd36a547b1cda8cb0
Kenichi Ishibashi <bashi@chromium.org>
2023-07-19 10:19:09 +0000

    [webtranport] Update streamErrorCode to 32-bit long
    The latest version of WebTransport protocol uses 32-bit unsigned
    integer for streamErrorCode [1, 2, 3].
    
    [1] https://github.com/ietf-wg-webtrans/draft-ietf-webtrans-http3/pull/115
    [2] https://github.com/w3c/webtransport/pull/509
    [3] https://github.com/w3c/webtransport/pull/527
    
    Bug: 1465282
    Change-Id: I3a3cf9936e09ea2a0b6bb01df18ffd1d9fb287bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4693162
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172268}

--- third_party/blink/renderer/modules/webtransport/web_transport_test.cc
+++ third_party/blink/renderer/modules/webtransport/web_transport_test.cc
@@ -1979,7 +1979,7 @@ TEST_F(WebTransportTest, ReceivedResetStream) {
   V8TestingScope scope;
   v8::Isolate* isolate = scope.GetIsolate();
   constexpr uint32_t kStreamId = 99;
-  constexpr uint8_t kCode = 24;
+  constexpr uint32_t kCode = 0xffffffff;
 
   auto* web_transport =
       CreateAndConnectSuccessfully(scope, "https://example.com");
@@ -2028,7 +2028,7 @@ TEST_F(WebTransportTest, ReceivedStopSending) {
   V8TestingScope scope;
   v8::Isolate* isolate = scope.GetIsolate();
   constexpr uint32_t kStreamId = 51;
-  constexpr uint8_t kCode = 255;
+  constexpr uint32_t kCode = 255;
 
   auto* web_transport =
       CreateAndConnectSuccessfully(scope, "https://example.com");
