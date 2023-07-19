fcadcc6185e462c25e8ebb24e7248b4c72e32e21
Wes Okuhara <wesokuhara@google.com>
2023-07-19 21:25:48 +0000

    CrOS Settings: Apply RouteOriginMixin to device page
    Uses RouteOriginMixin instead of os-settings-animated-pages to focus
    subpage trigger elements. Handled in preparation for upcoming
    category updates.
    
    Bug: b/291151247
    Test: browser_tests --gtest_filter="OSSettings*"
    Test: Manually test subpage trigger elements are focused
    Change-Id: I79a62f84bb8fde92b2401ca50f7c1580cce173f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4696293
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172590}

--- chrome/browser/resources/ash/settings/device_page/keyboard.ts
+++ chrome/browser/resources/ash/settings/device_page/keyboard.ts
@@ -17,15 +17,12 @@ import '/shared/settings/controls/settings_dropdown_menu.js';
 
 import {DropdownMenuOptionList} from '/shared/settings/controls/settings_dropdown_menu.js';
 import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
-import {focusWithoutInk} from 'chrome://resources/js/focus_without_ink.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
-import {afterNextRender, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {castExists} from '../assert_extras.js';
 import {DeepLinkingMixin} from '../deep_linking_mixin.js';
-import {FocusConfig} from '../focus_config.js';
 import {Setting} from '../mojom-webui/setting.mojom-webui.js';
-import {RouteObserverMixin} from '../route_observer_mixin.js';
+import {RouteOriginMixin} from '../route_origin_mixin.js';
 import {Route, Router, routes} from '../router.js';
 
 import {DevicePageBrowserProxy, DevicePageBrowserProxyImpl} from './device_page_browser_proxy.js';
@@ -47,11 +44,11 @@ enum ModifierKey {
 }
 
 const SettingsKeyboardElementBase =
-    DeepLinkingMixin(RouteObserverMixin(WebUiListenerMixin(PolymerElement)));
+    DeepLinkingMixin(RouteOriginMixin(WebUiListenerMixin(PolymerElement)));
 
 class SettingsKeyboardElement extends SettingsKeyboardElementBase {
   static get is() {
-    return 'settings-keyboard';
+    return 'settings-keyboard' as const;
   }
 
   static get template() {
@@ -64,11 +61,6 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
       prefs: {
         type: Object,
         notify: true,
-      },
-
-      focusConfig: {
-        type: Object,
-        observer: 'onFocusConfigChange_',
       },
 
       /** Whether to show Caps Lock options. */
@@ -152,7 +144,6 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
     };
   }
 
-  focusConfig: FocusConfig;
   private browserProxy_: DevicePageBrowserProxy;
   private hasAssistantKey_: boolean;
   private hasLauncherKey_: boolean;
@@ -167,6 +158,9 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
   constructor() {
     super();
 
+    /** RouteOriginMixin override */
+    this.route = routes.KEYBOARD;
+
     this.browserProxy_ = DevicePageBrowserProxyImpl.getInstance();
   }
 
@@ -177,14 +171,18 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
         'show-keys-changed', this.onShowKeysChange_.bind(this));
     this.browserProxy_.initializeKeyboard();
     this.setUpKeyMapTargets_();
+
+    this.addFocusConfig(routes.OS_LANGUAGES_INPUT, '#showLanguagesInput');
   }
 
-  override currentRouteChanged(route: Route) {
+  override currentRouteChanged(newRoute: Route, oldRoute?: Route) {
+    super.currentRouteChanged(newRoute, oldRoute);
+
     // Does not apply to this page.
-    if (route !== routes.KEYBOARD) {
+    if (newRoute !== this.route) {
       return;
     }
-    if (Router.getInstance().currentRoute === routes.KEYBOARD &&
+    if (Router.getInstance().currentRoute === this.route &&
         this.isDeviceSettingsSplitEnabled_) {
       // Call setCurrentRoute function to go to the per device keyboard subpage
       // when the feature flag is turned on. We don't use navigateTo function
@@ -238,17 +236,6 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
     ];
   }
 
-  private onFocusConfigChange_() {
-    this.focusConfig.set(routes.OS_LANGUAGES_INPUT.path, () => {
-      afterNextRender(this, () => {
-        const showLanguagesInputEl =
-            castExists(this.shadowRoot!.getElementById('showLanguagesInput'));
-        focusWithoutInk(showLanguagesInputEl);
-      });
-      return null;
-    });
-  }
-
   /**
    * Handler for updating which keys to show.
    */
@@ -283,7 +270,7 @@ class SettingsKeyboardElement extends SettingsKeyboardElementBase {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'settings-keyboard': SettingsKeyboardElement;
+    [SettingsKeyboardElement.is]: SettingsKeyboardElement;
   }
 }
 
