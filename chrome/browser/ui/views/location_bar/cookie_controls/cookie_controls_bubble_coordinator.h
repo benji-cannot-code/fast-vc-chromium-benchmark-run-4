75359a73f0a87089aef82d4c98433f0101c839cf
Theodore Olsauskas-Warren <sauski@google.com>
2023-07-19 14:12:55 +0000

    Cookie Controls: Refresh Page & show Refresh screen
    Bug: 1446230
    Change-Id: I4107e9eaef46d96e6f58f61907d938e2d462e9db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4693609
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172341}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_coordinator.h
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_coordinator.h
@@ -33,6 +33,8 @@ class CookieControlsBubbleCoordinator : public views::ViewObserver {
 
   CookieControlsBubbleViewImpl* GetBubble();
 
+  CookieControlsBubbleViewController* GetViewControllerForTesting();
+
  private:
   // views::ViewObserver
   void OnViewIsDeleting(views::View* observed_view) override;
