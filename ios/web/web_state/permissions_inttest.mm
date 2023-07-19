4f973fa0fad73795a6781bd49ceb42465edf931d
Justin Cohen <justincohen@google.com>
2023-07-19 03:29:59 +0000

    ios: Disable crashy permissions unittests on iOS17.
    Re-enable once crash is fixed in future iOS17 beta.
    
    Bug: 1462372
    Change-Id: Ie3fe4b8647f075c82c2eeee3b7996c243252fb0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699043
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172151}

--- ios/web/web_state/permissions_inttest.mm
+++ ios/web/web_state/permissions_inttest.mm
@@ -231,6 +231,10 @@ TEST_F(PermissionsInttest,
 // granted by WKUIDelegate in the first place.
 TEST_F(PermissionsInttest,
        TestsThatWebStateShouldNotAlterPermissionIfNotAccessible) {
+  if (@available(iOS 17.0, *)) {
+    // TODO(crbug.com/1462372): This crashes on iOS17, waiting for Apple fix.
+    GTEST_SKIP() << "This crashes on iOS17, waiting for Apple fix.";
+  }
   if (@available(iOS 15.0, *)) {
     delegate_.SetPermissionDecision(PermissionDecisionDeny);
     test::LoadUrl(web_state(), test_server_->GetURL("/camera_only.html"));
@@ -263,6 +267,10 @@ TEST_F(PermissionsInttest,
 
 // Tests that page reload resets permission states.
 TEST_F(PermissionsInttest, TestsThatPageReloadResetsPermissionState) {
+  if (@available(iOS 17.0, *)) {
+    // TODO(crbug.com/1462372): This crashes on iOS17, waiting for Apple fix.
+    GTEST_SKIP() << "This crashes on iOS17, waiting for Apple fix.";
+  }
   if (@available(iOS 15.0, *)) {
     // Initial load should allow permission.
     delegate_.SetPermissionDecision(PermissionDecisionGrant);
@@ -292,6 +300,10 @@ TEST_F(PermissionsInttest, TestsThatPageReloadResetsPermissionState) {
 // Tests that the web state does not preserve permission states between
 // navigations.
 TEST_F(PermissionsInttest, TestsThatWebStateDoesNotPreservePermissionState) {
+  if (@available(iOS 17.0, *)) {
+    // TODO(crbug.com/1462372): This crashes on iOS17, waiting for Apple fix.
+    GTEST_SKIP() << "This crashes on iOS17, waiting for Apple fix.";
+  }
   if (@available(iOS 15.0, *)) {
     // Initial load should allow permission.
     delegate_.SetPermissionDecision(PermissionDecisionGrant);
@@ -326,6 +338,10 @@ TEST_F(PermissionsInttest, TestsThatWebStateDoesNotPreservePermissionState) {
 // pages with existing accessible permission states.
 TEST_F(PermissionsInttest,
        TestsThatMovingBackwardOrForwardResetsPermissionState) {
+  if (@available(iOS 17.0, *)) {
+    // TODO(crbug.com/1462372): This crashes on iOS17, waiting for Apple fix.
+    GTEST_SKIP() << "This crashes on iOS17, waiting for Apple fix.";
+  }
   if (@available(iOS 15.0, *)) {
     // Initial load for both pages should allow permission.
     delegate_.SetPermissionDecision(PermissionDecisionGrant);
