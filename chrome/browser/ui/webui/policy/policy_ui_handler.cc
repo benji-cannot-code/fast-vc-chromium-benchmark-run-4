4273225bd204d46bc7e4dac4c174f5f41d3ff25f
Owen Min <zmin@chromium.org>
2023-07-19 14:56:47 +0000

    Count button usage on chrome://policy page
    Count reload policies, upload report and JSON export/copy buttons
    usage rate for every time the page is opened.
    
    Bug: b/290249598
    Change-Id: I9ccf07ee0663f148bb58336ff03dea24584fa00f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4688256
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172373}

--- chrome/browser/ui/webui/policy/policy_ui_handler.cc
+++ chrome/browser/ui/webui/policy/policy_ui_handler.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/browser/policy_conversions.h"
 #include "components/policy/core/browser/webui/json_generation.h"
 #include "components/policy/core/browser/webui/policy_webui_constants.h"
+#include "components/policy/core/browser/webui/statistics_collector.h"
 #include "components/policy/core/common/cloud/cloud_policy_manager.h"
 #include "components/policy/core/common/cloud/cloud_policy_refresh_scheduler.h"
 #include "components/policy/core/common/cloud/cloud_policy_util.h"
@@ -96,7 +97,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Key under which extension policies are grouped in JSON policy exports.
-const char kExtensionsKey[] = "extensions";
+constexpr char kExtensionsKey[] = "extensions";
 
 }  // namespace
 
@@ -106,6 +107,9 @@ PolicyUIHandler::~PolicyUIHandler() {
   if (export_policies_select_file_dialog_) {
     export_policies_select_file_dialog_->ListenerDestroyed();
   }
+  policy::RecordPolicyUIButtonUsage(reload_policies_count_,
+                                    export_to_json_count_, copy_to_json_count_,
+                                    upload_report_count_);
 }
 
 void PolicyUIHandler::AddCommonLocalizedStringsToSource(
@@ -237,6 +241,7 @@ void PolicyUIHandler::AddInfobarForActiveLocalTestPolicies() {
 }
 
 void PolicyUIHandler::HandleExportPoliciesJson(const base::Value::List& args) {
+  export_to_json_count_ += 1;
 #if BUILDFLAG(IS_ANDROID)
   // TODO(crbug.com/1228691): Unify download logic between all platforms to
   // use the WebUI download solution (and remove the Android check).
@@ -286,6 +291,7 @@ void PolicyUIHandler::HandleListenPoliciesUpdates(
 }
 
 void PolicyUIHandler::HandleReloadPolicies(const base::Value::List& args) {
+  reload_policies_count_ += 1;
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   // Allow user to manually fetch remote commands. Useful for testing or when
   // the invalidation service is not working properly.
@@ -313,6 +319,7 @@ void PolicyUIHandler::HandleReloadPolicies(const base::Value::List& args) {
 }
 
 void PolicyUIHandler::HandleCopyPoliciesJson(const base::Value::List& args) {
+  copy_to_json_count_ += 1;
   std::string policies_json = GetPoliciesAsJson();
   ui::ScopedClipboardWriter scw(ui::ClipboardBuffer::kCopyPaste);
   scw.WriteText(base::UTF8ToUTF16(policies_json));
@@ -353,6 +360,7 @@ void PolicyUIHandler::HandleGetPolicyLogs(const base::Value::List& args) {
 
 #if !BUILDFLAG(IS_CHROMEOS)
 void PolicyUIHandler::HandleUploadReport(const base::Value::List& args) {
+  upload_report_count_ += 1;
   DCHECK_EQ(1u, args.size());
   std::string callback_id = args[0].GetString();
   auto* report_scheduler = g_browser_process->browser_policy_connector()
---
+++ components/policy/core/browser/webui/statistics_collector.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_POLICY_CORE_BROWSER_WEBUI_STATISTICS_COLLECTOR_H_
+#define COMPONENTS_POLICY_CORE_BROWSER_WEBUI_STATISTICS_COLLECTOR_H_
+
+#include <stdint.h>
+
+#include "components/policy/policy_export.h"
+
+namespace policy {
+
+POLICY_EXPORT void RecordPolicyUIButtonUsage(uint32_t reload_policies_count,
+                                             uint32_t export_to_json_count,
+                                             uint32_t copy_to_json_count,
+                                             uint32_t upload_report_count);
+}  // namespace policy
+
+#endif  // COMPONENTS_POLICY_CORE_BROWSER_WEBUI_STATISTICS_COLLECTOR_H_
