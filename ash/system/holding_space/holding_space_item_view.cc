10308de0c20f045f54da4868205ab7444be8748f
David Black <dmblack@google.com>
2023-07-19 16:08:44 +0000

    Update background colors in holding space when Jelly is enabled.
    Background colors need to be updated when Jelly is enabled. When Jelly
    is disabled, there should be no change in behavior.
    
    Updated colors provided by UX.
    
    Jelly enabled (before): http://shortn/_ErB9WsT2qg
    Jelly enabled (after): http://shortn/_5SKRKYhTzT
    Jelly disabled (before): http://shortn/_ZHxuGf5x1H
    Jelly disabled (after): http://shortn/_CqngJHbnaw
    
    Bug: b:291828055
    Change-Id: If96eaf53a790663eccd7a9be92441e2abb24f2b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698186
    Reviewed-by: Angus McLean <angusmclean@google.com>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172406}

--- ash/system/holding_space/holding_space_item_view.cc
+++ ash/system/holding_space/holding_space_item_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/holding_space/holding_space_util.h"
 #include "ash/system/holding_space/holding_space_view_delegate.h"
 #include "base/functional/bind.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "ui/base/class_property.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -149,7 +150,10 @@ HoldingSpaceItemView::HoldingSpaceItemView(HoldingSpaceViewDelegate* delegate,
 
   // Background.
   SetBackground(views::CreateThemedRoundedRectBackground(
-      kColorAshControlBackgroundColorInactive, kHoldingSpaceCornerRadius));
+      chromeos::features::IsJellyEnabled()
+          ? static_cast<ui::ColorId>(cros_tokens::kCrosSysSystemOnBase)
+          : kColorAshControlBackgroundColorInactive,
+      kHoldingSpaceCornerRadius));
 
   // Layer.
   SetPaintToLayer();
