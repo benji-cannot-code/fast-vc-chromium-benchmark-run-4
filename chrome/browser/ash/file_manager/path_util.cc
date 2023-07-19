f72fa9953773ada71939eddeb85caa845552c5a6
Joel Hockey <joelhockey@chromium.org>
2023-07-19 03:12:34 +0000

    Fix GetGuestOsMountPointName() for termina VMs
    In recent http://crrev.com/c/4665122 we added checking for TERMINA VMs
    in GetGuestOsMountPointName() which should only apply to penguin and not
    other containers.
    
    Bug: b/265601951
    Change-Id: I063e92381c450d360eb27273e4d8b29cc40f1217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699442
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172146}

--- chrome/browser/ash/file_manager/path_util.cc
+++ chrome/browser/ash/file_manager/path_util.cc
@@ -483,7 +483,7 @@ std::string GetGuestOsMountPointName(Profile* profile,
   if (id.vm_type == guest_os::VmType::ARCVM) {
     return kAndroidFilesMountPointName;
   }
-  if (id.vm_type == guest_os::VmType::TERMINA) {
+  if (id == crostini::DefaultContainerId()) {
     return GetCrostiniMountPointName(profile);
   }
   return base::JoinString(
