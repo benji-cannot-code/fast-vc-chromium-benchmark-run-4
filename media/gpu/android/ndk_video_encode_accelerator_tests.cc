20b17fd9e62d2de397e8c44487bb79c9d24a4aea
Sida Zhu <zhusida@bytedance.com>
2023-07-19 22:14:43 +0000

    Video: Add Android HEVC HW encoding support for Webcodecs
    Only HEVC main profile is supported for now on Android 10.0+,
    also need to pass `--enable-features=PlatformHEVCEncoderSupport`
    to enable this feature.
    
    Bug: 1465760
    
    Change-Id: If3820eac57199e7fac7f9e4ff71bc603f33343b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4689056
    Commit-Queue: 朱思达 <zhusida@bytedance.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172620}

--- media/gpu/android/ndk_video_encode_accelerator_tests.cc
+++ media/gpu/android/ndk_video_encode_accelerator_tests.cc
@@ -12,8 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/ranges/algorithm.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "media/base/bitstream_buffer.h"
+#include "media/base/media_switches.h"
 #include "media/base/media_util.h"
 #include "media/base/test_helpers.h"
 #include "media/base/video_codecs.h"
@@ -42,6 +44,10 @@ class NdkVideoEncoderAcceleratorTest
   void SetUp() override {
     if (!NdkVideoEncodeAccelerator::IsSupported())
       GTEST_SKIP() << "Not supported Android version";
+
+#if BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
+    feature_list_.InitAndEnableFeature(kPlatformHEVCEncoderSupport);
+#endif
 
     auto args = GetParam();
     profile_ = args.profile;
@@ -205,6 +211,7 @@ class NdkVideoEncoderAcceleratorTest
   VideoPixelFormat pixel_format_;
 
   base::test::TaskEnvironment task_environment_;
+  base::test::ScopedFeatureList feature_list_;
   base::RunLoop loop_;
   std::unique_ptr<VideoEncodeAccelerator> accelerator_;
   size_t output_buffer_size_ = 0;
@@ -316,6 +323,10 @@ VideoParams kParams[] = {
     {VP8PROFILE_MIN, PIXEL_FORMAT_NV12},
     {H264PROFILE_BASELINE, PIXEL_FORMAT_I420},
     {H264PROFILE_BASELINE, PIXEL_FORMAT_NV12},
+#if BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
+    {HEVCPROFILE_MAIN, PIXEL_FORMAT_I420},
+    {HEVCPROFILE_MAIN, PIXEL_FORMAT_NV12},
+#endif
 };
 
 INSTANTIATE_TEST_SUITE_P(AllNdkEncoderTests,
