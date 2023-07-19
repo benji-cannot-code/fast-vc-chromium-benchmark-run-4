c278854f03873d9b79a5d36780c22269e30bb19c
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-19 10:19:26 +0000

    [iOS] SigninScreenMediator is a ChromeAccountManagerServiceObserver
    Admittedly, it’s not clear to me how
        _accountManagerServiceObserver =
            std::make_unique<ChromeAccountManagerServiceObserverBridge>(
                self, _accountManagerService);
    was typed. Given that self was not a
    `id<ChromeAccountManagerServiceObserver>`, and that is required by
    `ChromeAccountManagerServiceObserverBridge` constructor.
    
    Bug: None
    Change-Id: Ia54f03d71b43fd0d287cdab8247614bd0b01b57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4695094
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172269}

--- ios/chrome/browser/ui/first_run/signin/signin_screen_mediator.mm
+++ ios/chrome/browser/ui/first_run/signin/signin_screen_mediator.mm
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-@interface SigninScreenMediator () {
+@interface SigninScreenMediator () <ChromeAccountManagerServiceObserver> {
   std::unique_ptr<ChromeAccountManagerServiceObserverBridge>
       _accountManagerServiceObserver;
 }
