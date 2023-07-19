d9327c2be42ecb86071d531192ed996c8b76393e
Brian Sheedy <bsheedy@chromium.org>
2023-07-19 14:32:28 +0000

    Clean up WebGL version tag
    Cleans up the generation and most uses of the webgl-version-X tags in
    GPU code. We formally split the two suites a while back, so there is no
    need to keep the tag around.
    
    The tags are still present in the ignored tag list for now until we can
    be sure that any data with the tags present has aged out.
    
    Bug: 1140283
    Change-Id: Ie9212a38f38551b4749aab6149eabf1e9d9564a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4697607
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172351}

--- content/test/gpu/unexpected_passes/gpu_unittest_utils.py
+++ content/test/gpu/unexpected_passes/gpu_unittest_utils.py
@@ -29,6 +29,5 @@ class GenericGpuExpectations(gpu_expectations.GpuExpectations):
     return """\
 # tags: [ linux mac win ]
 # tags: [ amd intel nvidia ]
-# tags: [ webgl-version-1]
 # results: [ Failure RetryOnFailure Skip Pass ]
 """
