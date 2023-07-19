c9c7158185a891f05d3a5fcf1aaab696b59dfef9
Darren Shen <shend@chromium.org>
2023-07-19 01:02:03 +0000

    inputs: Migrate LacrosSupport flag to LacrosOnly.
    LacrosSupport is deprecated in favour of LacrosOnly. The internal shared
    library still expects LacrosSupport. For backwards compatibility, ensure
    the shared library can still query for the value of LacrosSupport (via
    LacrosOnly).
    
    BUG=1448575,b/290714161
    
    Change-Id: I8f076b6098dbae6cf77fb853cd4cbc736cfbb9df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671964
    Auto-Submit: Darren Shen <shend@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172097}

--- chromeos/ash/services/ime/ime_service.cc
+++ chromeos/ash/services/ime/ime_service.cc
@@ -143,7 +143,7 @@ bool ImeService::IsFeatureEnabled(const char* feature_name) {
       &features::kAssistMultiWord,
       &features::kAutocorrectParamsTuning,
       &features::kFirstPartyVietnameseInput,
-      &features::kLacrosSupport,
+      &features::kLacrosOnly,
       &features::kSystemJapanesePhysicalTyping,
       &features::kImeDownloaderUpdate,
       &features::kImeUsEnglishModelUpdate,
@@ -159,6 +159,15 @@ bool ImeService::IsFeatureEnabled(const char* feature_name) {
       return base::FeatureList::IsEnabled(*feature);
     }
   }
+
+  // For backwards-compatibility, check for the "LacrosSupport" flag, which was
+  // replaced by LacrosOnly.
+  // TODO(b/290714161): Remove this once the shared library no longer uses
+  // LacrosSupport.
+  if (strcmp(feature_name, "LacrosSupport") == 0) {
+    return base::FeatureList::IsEnabled(features::kLacrosOnly);
+  }
+
   return false;
 }
 
