90679ea5851f5b05525b804de2cac251a46a2cdf
Zijie He <zijiehe@google.com>
2023-07-19 20:02:25 +0000

    [Fuchsia] Move boot device logic to an individual module
    This is a trivial change to reduce the size of common so it can be
    easier to add more arguments without interfering with each other.
    
    Both fuchsia-smoke-astro and fuchsia-smoke-sherlock are tried with this
    change.
    
    Bug: 1465707, fuchsia:98288
    Change-Id: I36a2124f06ba781797762ccbbf9ec416655f5701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4702783
    Reviewed-by: Chong Gu <chonggu@google.com>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172541}

--- build/fuchsia/test/flash_device_unittests.py
+++ build/fuchsia/test/flash_device_unittests.py
@@ -10,7 +10,7 @@ import subprocess
 import unittest
 import unittest.mock as mock
 
-import common
+import boot_device
 import flash_device
 
 _TEST_IMAGE_DIR = 'test/image/dir'
@@ -124,7 +124,7 @@ class FlashDeviceTest(unittest.TestCase):
                 mock.patch('flash_device.boot_device') as mock_boot, \
                 mock.patch('flash_device.get_system_info') as mock_sys_info, \
                 mock.patch('flash_device.subprocess.run'):
-            mock_boot.side_effect = common.StateTransitionError(
+            mock_boot.side_effect = boot_device.StateTransitionError(
                 'Incorrect state')
             self._ffx_mock.return_value.stdout = \
                 '[{"title": "Build", "child": [{"value": "wrong.version"}, ' \
@@ -135,7 +135,8 @@ class FlashDeviceTest(unittest.TestCase):
                                 should_pave=False)
             # Regular boot is to check the versions.
             mock_boot.assert_called_once_with(mock.ANY,
-                                              common.BootMode.REGULAR, None)
+                                              boot_device.BootMode.REGULAR,
+                                              None)
             self.assertEqual(self._ffx_mock.call_count, 1)
 
             # get_system_info should not even be called due to early exit.
@@ -158,7 +159,8 @@ class FlashDeviceTest(unittest.TestCase):
                                 should_pave=False)
             # Regular boot is to check the versions.
             mock_boot.assert_called_once_with(mock.ANY,
-                                              common.BootMode.REGULAR, None)
+                                              boot_device.BootMode.REGULAR,
+                                              None)
             self.assertEqual(self._ffx_mock.call_count, 3)
 
     def test_incorrect_target_info(self) -> None:
@@ -184,7 +186,7 @@ class FlashDeviceTest(unittest.TestCase):
                                 'test_serial',
                                 should_pave=False)
             mock_boot.assert_called_once_with(mock.ANY,
-                                              common.BootMode.BOOTLOADER,
+                                              boot_device.BootMode.BOOTLOADER,
                                               'test_serial')
         self.assertEqual(self._ffx_mock.call_count, 2)
 
@@ -215,7 +217,8 @@ class FlashDeviceTest(unittest.TestCase):
                                 should_pave=True)
 
             mock_boot.assert_called_once_with('some-target-id',
-                                              common.BootMode.RECOVERY, None)
+                                              boot_device.BootMode.RECOVERY,
+                                              None)
             mock_pave.assert_called_once_with(_TEST_IMAGE_DIR,
                                               'some-target-id')
 
@@ -249,7 +252,7 @@ class FlashDeviceTest(unittest.TestCase):
                                 'test_serial',
                                 should_pave=False)
             mock_boot.assert_called_once_with(mock.ANY,
-                                              common.BootMode.BOOTLOADER,
+                                              boot_device.BootMode.BOOTLOADER,
                                               'test_serial')
         self.assertEqual(self._ffx_mock.call_count, 2)
 
