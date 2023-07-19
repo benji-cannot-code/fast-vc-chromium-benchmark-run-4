584f94e8285d60ab2d7fdec9312a9c90613185b6
Trent Apted <tapted@chromium.org>
2023-07-19 02:48:13 +0000

    Disable flaky LaunchNavigationBrowserRestartTest.CmdLineURLRestartTest on chrome bots
    Fails at launch_time_navigation_signal_browsertest.cc:176 with
    
    Histogram "Blink.UseCounter.Features" does not have the right number
      of samples (3) in the expected bucket (4572). It has (2).
    
    Bug: 1448030
    Cq-Include-Trybots: luci.chrome.try:linux-chrome
    Change-Id: Ia87c447cfc9ae6e971dc80bc36e36bf762be1319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699482
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Owners-Override: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172137}

--- chrome/browser/launch_time_navigation_signal/launch_time_navigation_signal_browsertest.cc
+++ chrome/browser/launch_time_navigation_signal/launch_time_navigation_signal_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -309,9 +310,15 @@ IN_PROC_BROWSER_TEST_P(LaunchNavigationBrowserRestartTest,
                             embedded_test_server()));
 }
 
+// Flaky on Chrome bots. See http://crbug.com/1448030.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_CmdLineURLRestartTest DISABLED_CmdLineURLRestartTest
+#else
+#define MAYBE_CmdLineURLRestartTest CmdLineURLRestartTest
+#endif
 // We attempt to restart the browser in this test body.
 IN_PROC_BROWSER_TEST_P(LaunchNavigationBrowserRestartTest,
-                       CmdLineURLRestartTest) {
+                       MAYBE_CmdLineURLRestartTest) {
   const ParamType& test_params = GetParam();
   const size_t expected_initial_tab_count = GetExpectedTabCountFromRestore();
   const bool expect_valid_system_entropy =
