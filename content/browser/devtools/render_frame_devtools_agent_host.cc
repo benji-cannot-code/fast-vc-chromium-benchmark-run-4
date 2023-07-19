a1796ac8b13d8ce6dbc89ea8955b728af0838a5d
Philip Pfaffe <pfaffe@chromium.org>
2023-07-19 11:01:04 +0000

    Consider tab-target as well when checking for a debugging session
    Fixed: 1452156
    Change-Id: I396a731d1c471f268b8bf8eabeacf342a1ffde8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694110
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172275}

--- content/browser/devtools/render_frame_devtools_agent_host.cc
+++ content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/devtools/protocol/system_info_handler.h"
 #include "content/browser/devtools/protocol/target_handler.h"
 #include "content/browser/devtools/protocol/tracing_handler.h"
+#include "content/browser/devtools/web_contents_devtools_agent_host.h"
 #include "content/browser/fenced_frame/fenced_frame.h"
 #include "content/browser/renderer_host/navigation_request.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
@@ -197,6 +198,13 @@ bool RenderFrameDevToolsAgentHost::WasEverAttachedToAnyFrame() {
 
 // static
 bool DevToolsAgentHost::IsDebuggerAttached(WebContents* web_contents) {
+  return RenderFrameDevToolsAgentHost::IsDebuggerAttached(web_contents) ||
+         WebContentsDevToolsAgentHost::IsDebuggerAttached(web_contents);
+}
+
+// static
+bool RenderFrameDevToolsAgentHost::IsDebuggerAttached(
+    WebContents* web_contents) {
   FrameTreeNode* node =
       static_cast<WebContentsImpl*>(web_contents)->GetPrimaryFrameTree().root();
   RenderFrameDevToolsAgentHost* host = node ? FindAgentHost(node) : nullptr;
