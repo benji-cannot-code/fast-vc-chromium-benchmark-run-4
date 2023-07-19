1c2f2fb1c8a2130e3d76a9510fa6244c4a92a3ba
Daniel Murphy <dmurph@chromium.org>
2023-07-19 20:21:19 +0000

    [dPWA] Fix ML reporting to not populate guardrails & sample cases
    R=dibyapal@chromium.org
    
    Bug: 1466168, b/290675903
    Change-Id: I172c9b0b4fa9186e4b04c3b3e035a381dcdcbc88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4702463
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172552}

--- components/webapps/browser/installable/ml_install_result_reporter.h
+++ components/webapps/browser/installable/ml_install_result_reporter.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_WEBAPPS_BROWSER_INSTALLABLE_ML_INSTALL_RESULT_REPORTER_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/types/pass_key.h"
 #include "components/segmentation_platform/public/trigger.h"
 #include "components/webapps/browser/installable/installable_metrics.h"
@@ -16,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace webapps {
 class AppBannerManager;
 enum class MlInstallUserResponse;
+
+extern const base::FeatureParam<double> kGuardrailResultReportProb;
+extern const base::FeatureParam<double> kModelDeclineUserDeclineReportProb;
 
 // This class is responsible for reporting the result of the Ml installation
 // classification prediction and updating any guardrail metrics if applicable.
@@ -31,13 +35,14 @@ enum class MlInstallUserResponse;
 // possibly occur & report a real result to classification.
 class MlInstallResultReporter {
  public:
-  // Public for tests.
+  // Public for tests. Reported to UMA, do not change values.
   enum class MlInstallResponse {
     kAccepted = 0,
     kIgnored = 1,
     kCancelled = 2,
     kBlockedGuardrails = 3,
-    kMaxValue = kBlockedGuardrails
+    kReporterDestroyed = 4,
+    kMaxValue = kReporterDestroyed
   };
 
   MlInstallResultReporter(
