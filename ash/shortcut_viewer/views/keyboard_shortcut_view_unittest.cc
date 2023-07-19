fe9e7a86bf18b17c988c57547ea5232c4b596a5b
Sean Kau <skau@chromium.org>
2023-07-19 19:50:20 +0000

    Disable Jelly for KeyboardShortcutViewTest
    The UI is being removed soon. It does not support Jelly.
    
    Change-Id: I7ca8e08fe7fa5e5ea64e215fa643dcb50b01cf61
    Bug: b:289459915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698173
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172533}

--- ash/shortcut_viewer/views/keyboard_shortcut_view_unittest.cc
+++ ash/shortcut_viewer/views/keyboard_shortcut_view_unittest.cc
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -52,6 +54,8 @@ class KeyboardShortcutViewTest : public ash::AshTestBase {
 
   // ash::AshTestBase:
   void SetUp() override {
+    // TODO(b/291803593): This is the old UI so it's not compatible with Jelly.
+    scoped_features_.InitAndDisableFeature(chromeos::features::kJelly);
     ash::AshTestBase::SetUp();
     // Simulate the complete listing of input devices, required by the viewer.
     ui::DeviceDataManagerTestApi().OnDeviceListsComplete();
@@ -97,6 +101,9 @@ class KeyboardShortcutViewTest : public ash::AshTestBase {
   KeyboardShortcutView* GetView() const {
     return KeyboardShortcutView::GetInstanceForTesting();
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_features_;
 };
 
 // Shows and closes the widget for KeyboardShortcutViewer.
