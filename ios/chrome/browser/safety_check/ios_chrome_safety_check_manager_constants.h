eccfb9cb8cf096cce3f6e9f3a876181132600b34
Benjamin Williams <bwwilliams@google.com>
2023-07-19 22:07:57 +0000

    [iOS] Implement Password Check in new Safety Check Manager.
    This change does the following:
    
    1. Implements the Password Check in the new Safety Check Manager.
    
    2. Makes the new Safety Check Manager conform to
    IOSChromePasswordCheckManager::Observer.
    
    3. Introduces new utils file, ios_chrome_safety_check_manager_utils.h.
    
    4. Unit tests the Password Check logic.
    
    Change-Id: I1520a96632558a58316d6fbf9cedeb57445e91b4
    Fixed: 1464741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670813
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172615}

--- ios/chrome/browser/safety_check/ios_chrome_safety_check_manager_constants.h
+++ ios/chrome/browser/safety_check/ios_chrome_safety_check_manager_constants.h
@@ -46,6 +46,8 @@ enum class PasswordSafetyCheckState {
   kDisabled,
   // When password check failed due to network issues, quota limit or others.
   kError,
+  // When password check failed due to user being signed out.
+  kSignedOut,
 };
 
 // Enum with all possible states of the Safe Browsing check.
