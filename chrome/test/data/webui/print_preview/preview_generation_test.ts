e02c1b574f1c3e75f782c2b76443caac3f18fd8a
Lei Zhang <thestig@chromium.org>
2023-07-19 04:09:00 +0000

    Print Preview: Store margins as integers again in TS
    https://crrev.com/1170725 changed margins to stored as floats in TS, but
    the C++ side is expecting integers. Revert back to storing integers by
    making the Margins class round the values in its ctor.
    
    Bug: 1465198
    Change-Id: I141c1daa322c018c33d41d50aaaeadc3525c6348
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4697424
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172157}

--- chrome/test/data/webui/print_preview/preview_generation_test.ts
+++ chrome/test/data/webui/print_preview/preview_generation_test.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {ColorMode, Destination, DestinationOrigin, DestinationState, Margins, MarginsType, NativeInitialSettings, NativeLayerImpl, PluginProxyImpl, PreviewTicket, PrintPreviewAppElement, PrintPreviewDestinationSettingsElement, Range, ScalingType} from 'chrome://print/print_preview.js';
+import {ColorMode, CustomMarginsOrientation, Destination, DestinationOrigin, DestinationState, Margins, MarginsType, NativeInitialSettings, NativeLayerImpl, PluginProxyImpl, PreviewTicket, PrintPreviewAppElement, PrintPreviewDestinationSettingsElement, Range, ScalingType} from 'chrome://print/print_preview.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 // <if expr="is_chromeos">
@@ -727,5 +727,11 @@ suite(preview_generation_test.suiteName, function() {
 
     assertEquals(612, page.$.documentInfo.pageSize.width);
     assertEquals(792, page.$.documentInfo.pageSize.height);
+
+    const o = CustomMarginsOrientation;
+    assertEquals(28, page.$.documentInfo.margins.get(o.TOP));
+    assertEquals(28, page.$.documentInfo.margins.get(o.RIGHT));
+    assertEquals(28, page.$.documentInfo.margins.get(o.BOTTOM));
+    assertEquals(28, page.$.documentInfo.margins.get(o.LEFT));
   });
 });
