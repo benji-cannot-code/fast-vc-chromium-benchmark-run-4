c3761ee27f6684004d0ed3085ec90a61e32bede8
Lingqi Chi <lingqi@chromium.org>
2023-07-19 03:05:19 +0000

    Reland "Prerender: Record activation IPC delay"
    This is a reland of commit 254d9b73fef39806bf919e4fc309e3dc63c24e0b
    Due to an existing bug(1415185), NavigatePrimaryPage cannot really wait for the prerendering page to finish loading, and cannot tell
    whether the activation signal has been sent to the renderer side or not.
    
    This reland makes the test code send another signal to the renderer
    side explicitly, to ensure all signals have been processed, as a
    workaround
    
    Original change's description:
    > Prerender: Record activation IPC delay
    >
    > It is possible that the activation IPC was queueing for quite a long
    > time before being processed.
    > This CL adds a metric to track the delay
    >
    > Bug: 1462834, 1342497
    > Change-Id: I196f5d22b9089d9d1b9ca9b2ff45b9367849013a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671290
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1170412}
    
    Bug: 1462834, 1342497
    Change-Id: Ic590fb7b82408c7ede9ccd08ef2f6c762b03a3bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4692484
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172142}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -858,6 +858,12 @@ _CONFIG = [
         ],
     },
     {
+        'paths': ['third_party/blink/renderer/core/exported/web_view_impl.cc'],
+        'allowed': [
+            'base::TaskAnnotator',
+        ],
+    },
+    {
         'paths': ['third_party/blink/renderer/core/clipboard'],
         'allowed': ['base::EscapeForHTML'],
     },
