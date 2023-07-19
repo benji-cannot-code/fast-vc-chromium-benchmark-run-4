eb8164f7e6409853a13b92b1b960532ae765e022
Alexis Hetu <sugoi@chromium.org>
2023-07-19 13:19:50 +0000

    [iOS] First EG test for Payments Bottom Sheet
    This CL adds a first EG test for Payments Bottom Sheet, which
    includes a new credit card form with various fields which can
    trigger the bottom sheet and a simple test which makes the
    bottom sheet appear and uses a button on the bottom sheet.
    
    Bug: 1450214
    Change-Id: Ia425c49bee5fe94456a2862c3ca3f4ec5b1e5ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666652
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172321}

--- components/autofill/core/browser/browser_autofill_manager.h
+++ components/autofill/core/browser/browser_autofill_manager.h
@@ -253,6 +253,9 @@ class BrowserAutofillManager : public AutofillManager,
   // metrics.
   void DidSuppressPopup(const FormData& form, const FormFieldData& field);
 
+  // Handle the parsed forms for the given web frame.
+  void HandleParsedForms(const std::vector<autofill::FormStructure*>& forms);
+
   // AutofillManager:
   base::WeakPtr<AutofillManager> GetWeakPtr() override;
   CreditCardAccessManager* GetCreditCardAccessManager() override;
