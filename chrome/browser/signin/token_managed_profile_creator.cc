78b3be158933416be3b6c0cbe59cb45310234238
Yann Dago <ydago@chromium.org>
2023-07-19 18:09:25 +0000

    Reland "Add a CookieMover in the DiceSignedInProfileCreator"
    This is a reland of commit 08389766d4227fa21fc4db26a1957c0beb6d2d0c
    
    Original change's description:
    > Add a CookieMover in the DiceSignedInProfileCreator
    >
    > This is the first CL in a series of CLs which allow support for profile
    > for third party federated accounts. This will allow cookies of the iDp
    > to be moved to a profile creayed via enterprise signin interception s
    > that the user doesn't have to signin another time nor that the user
    > stays signed in the previous account.
    >
    > Bug: 1444802
    > Change-Id: Ifc7e688a3862db93ef8791c7f0ded8c2c8a2d95a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4525116
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Yann Dago <ydago@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1171977}
    
    Bug: 1444802
    Change-Id: Iad2966cdc67c626fe3c550266cf81945874a773b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701763
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172480}

--- chrome/browser/signin/token_managed_profile_creator.cc
+++ chrome/browser/signin/token_managed_profile_creator.cc
@@ -18,17 +18,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/account_consistency_mode_manager_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/signin/signin_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 
 TokenManagedProfileCreator::TokenManagedProfileCreator(
+    Profile* source_profile,
     const std::string& id,
     const std::string& enrollment_token,
     const std::u16string& local_profile_name,
-    base::OnceCallback<void(Profile*)> callback)
-    : id_(id),
+    base::OnceCallback<void(base::WeakPtr<Profile>)> callback)
+    : source_profile_(source_profile),
+      id_(id),
       enrollment_token_(enrollment_token),
       expected_profile_path_(g_browser_process->profile_manager()
                                  ->GetNextExpectedProfileDirectoryPath()),
@@ -49,9 +52,10 @@ TokenManagedProfileCreator::TokenManagedProfileCreator(
 }
 
 TokenManagedProfileCreator::TokenManagedProfileCreator(
+    Profile* source_profile,
     const base::FilePath& target_profile_path,
-    base::OnceCallback<void(Profile*)> callback)
-    : callback_(std::move(callback)) {
+    base::OnceCallback<void(base::WeakPtr<Profile>)> callback)
+    : source_profile_(source_profile), callback_(std::move(callback)) {
   // Make sure the callback is not called synchronously.
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
@@ -91,7 +95,9 @@ void TokenManagedProfileCreator::OnNewProfileCreated(Profile* new_profile) {
 }
 
 void TokenManagedProfileCreator::OnNewProfileInitialized(Profile* new_profile) {
-  if (callback_) {
-    std::move(callback_).Run(new_profile);
-  }
+  // base::Unretained is fine because `cookies_mover_` is owned by this.
+  cookies_mover_ = std::make_unique<signin_util::CookiesMover>(
+      source_profile_->GetWeakPtr(), new_profile->GetWeakPtr(),
+      base::BindOnce(std::move(callback_), new_profile->GetWeakPtr()));
+  cookies_mover_->StartMovingCookies();
 }
