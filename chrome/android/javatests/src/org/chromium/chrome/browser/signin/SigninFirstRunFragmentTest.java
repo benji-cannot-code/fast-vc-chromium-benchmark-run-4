ff759f9706b37211096f3f07bf4a072397d91ffb
James Shen <zhiyuans@google.com>
2023-07-19 23:25:39 +0000

    Disable critical flaky tests on all Android builders.
    These 2 tests are disabled:
    -testContinueButtonWithAnAccountOtherThanTheSignedInAccount
    -testDismissButtonWhenUserIsSignedIn
    
    They are flaky on all Android builders: http://shortn/_Emc0L34GV6
    
    Bug: 1465867
    Change-Id: Id24213f3b79cc0847e84af9b29a50931b5af52d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4703012
    Reviewed-by: Neil Coronado <nemco@google.com>
    Owners-Override: Neil Coronado <nemco@google.com>
    Commit-Queue: James Shen <zhiyuans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172652}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentTest.java
@@ -629,6 +629,7 @@ public class SigninFirstRunFragmentTest {
     @Test
     @MediumTest
     @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO})
+    @DisabledTest(message = "https://crbug.com/1465867")
     public void testContinueButtonWithAnAccountOtherThanTheSignedInAccount() {
         final CoreAccountInfo targetPrimaryAccount =
                 mSigninTestRule.addAccount(TEST_EMAIL1, FULL_NAME1, GIVEN_NAME1, null);
@@ -682,6 +683,7 @@ public class SigninFirstRunFragmentTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1465867")
     public void testDismissButtonWhenUserIsSignedIn() {
         mSigninTestRule.addAccount(TEST_EMAIL1, FULL_NAME1, GIVEN_NAME1, null);
         final CoreAccountInfo primaryAccount = mSigninTestRule.addTestAccountThenSignin();
