2395ffa9a921d6ba29970603eeb7ed09338c323a
Ben Becker <benbecker@chromium.org>
2023-07-19 20:36:16 +0000

    DeskButton: Add desk switch metrics
    This CL separates out desk switches that occur due to interaction with
    the desk button desk bar, and adds unit tests to verify the metrics are
    correctly recorded. It also separates out a DesksCreationRemovalSource
    for the desk button desk bar and tests that those metrics are correctly
    recorded as well.
    
    Bug: b/285029311
    Change-Id: I72c7c64b48e83ee187995ad35e0de1a6b423b301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4695814
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172561}

--- ash/wm/desks/desk_bar_view_base.cc
+++ ash/wm/desks/desk_bar_view_base.cc
@@ -533,9 +533,12 @@ DeskBarViewBase::DeskBarViewBase(aura::Window* root, Type type)
             l10n_util::GetStringUTF16(IDS_ASH_DESKS_NEW_DESK_BUTTON),
             cros_tokens::kCrosSysOnPrimary, cros_tokens::kCrosSysPrimary,
             /*initially_enabled=*/DesksController::Get()->CanCreateDesks(),
-            base::BindRepeating(&DeskBarViewBase::OnNewDeskButtonPressed,
-                                base::Unretained(this),
-                                DesksCreationRemovalSource::kButton)));
+            base::BindRepeating(
+                &DeskBarViewBase::OnNewDeskButtonPressed,
+                base::Unretained(this),
+                type_ == Type::kDeskButton
+                    ? DesksCreationRemovalSource::kDeskButtonDeskBarButton
+                    : DesksCreationRemovalSource::kButton)));
     new_desk_button_label_ =
         scroll_view_contents_->AddChildView(std::make_unique<views::Label>());
     new_desk_button_label_->SetPaintToLayer();
@@ -546,9 +549,12 @@ DeskBarViewBase::DeskBarViewBase(aura::Window* root, Type type)
             this, &kDesksNewDeskButtonIcon,
             l10n_util::GetStringUTF16(IDS_ASH_DESKS_NEW_DESK_BUTTON),
             /*initially_enabled=*/DesksController::Get()->CanCreateDesks(),
-            base::BindRepeating(&DeskBarViewBase::OnNewDeskButtonPressed,
-                                base::Unretained(this),
-                                DesksCreationRemovalSource::kButton)));
+            base::BindRepeating(
+                &DeskBarViewBase::OnNewDeskButtonPressed,
+                base::Unretained(this),
+                type_ == Type::kDeskButton
+                    ? DesksCreationRemovalSource::kDeskButtonDeskBarButton
+                    : DesksCreationRemovalSource::kButton)));
 
     zero_state_default_desk_button_ = scroll_view_contents_->AddChildView(
         std::make_unique<ZeroStateDefaultDeskButton>(this));
@@ -556,9 +562,12 @@ DeskBarViewBase::DeskBarViewBase(aura::Window* root, Type type)
         std::make_unique<ZeroStateIconButton>(
             this, &kDesksNewDeskButtonIcon,
             l10n_util::GetStringUTF16(IDS_ASH_DESKS_NEW_DESK_BUTTON),
-            base::BindRepeating(&DeskBarViewBase::OnNewDeskButtonPressed,
-                                base::Unretained(this),
-                                DesksCreationRemovalSource::kButton)));
+            base::BindRepeating(
+                &DeskBarViewBase::OnNewDeskButtonPressed,
+                base::Unretained(this),
+                type_ == Type::kDeskButton
+                    ? DesksCreationRemovalSource::kDeskButtonDeskBarButton
+                    : DesksCreationRemovalSource::kButton)));
   }
 
   if (saved_desk_util::IsSavedDesksEnabled()) {
@@ -1234,7 +1243,10 @@ void DeskBarViewBase::OnActivateDeskTimer(const base::Uuid& uuid) {
   auto* controller = DesksController::Get();
 
   if (Desk* desk = controller->GetDeskByUuid(uuid)) {
-    controller->ActivateDesk(desk, DesksSwitchSource::kMiniViewButton);
+    controller->ActivateDesk(desk,
+                             type_ == Type::kDeskButton
+                                 ? DesksSwitchSource::kDeskButtonMiniViewButton
+                                 : DesksSwitchSource::kMiniViewButton);
   }
 }
 
