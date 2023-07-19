b76c21fdb806521e171bd842c6881f9a4b832a5f
Alexander Tekle <alexandertekle@google.com>
2023-07-19 20:31:02 +0000

    [VCN Card on File] Parsing and Field Rationalization for Standalone CVC Fields
    List of changes:
    1. Parses standalone CVC fields in form_field.cc. Refactored to its own
    method so that we only parse it if it has no other CC fields nor email
    fields. (Reducing false positives on card on file detection)
    
    2. Rationalize CVC fields through heuristic labels and HTML autocomplete
    labels.
    
    go/card-on-file-dom-1-pager
    
    Bug: 1281695
    Change-Id: I5cdb9b7793d152940a3e03cd6c24059d83177157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652958
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1172558}

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -1198,6 +1198,10 @@ void FormStructure::ParseFieldTypesWithPatterns(PatternSource pattern_source,
     FormField::ParseSingleFieldForms(fields_, current_page_language_,
                                      is_form_tag_, pattern_source,
                                      field_type_map, log_manager);
+
+    FormField::ParseStandaloneCVCFields(fields_, current_page_language_,
+                                        pattern_source, field_type_map,
+                                        log_manager);
   }
   if (field_type_map.empty())
     return;
