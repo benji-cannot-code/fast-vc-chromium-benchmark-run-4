5197552cd136ea941887eaf45616bdc96a828963
Rafał Godlewski <rgod@google.com>
2023-07-20 16:21:47 +0000

    [iOS] Add recipient rows to FamilyPickerViewController
    * Adds RecipientInfo NSObject for displaying info about recipient
    * Adds initial table view implementation for family picker (for now
    just rows with recipients with temporary default profile images)
    
    Mock: http://screen/5ENqSauYPyo7RcZ
    Screenshot: http://screen/AcqjMV4Ff24WZ7o
    
    Bug: 1463882
    Change-Id: Iafc0d209ef6839445c8cd7c0e23f2e80408bf985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701286
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173015}

--- ios/chrome/browser/ui/settings/password/password_sharing/family_picker_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_sharing/family_picker_view_controller.mm
@@ -5,9 +5,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/password/password_sharing/family_picker_view_controller.h"
 
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/browser/ui/authentication/authentication_constants.h"
+#import "ios/chrome/browser/ui/settings/cells/settings_image_detail_text_cell.h"
+#import "ios/chrome/browser/ui/settings/password/password_sharing/recipient_info.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+@interface FamilyPickerViewController () <UITableViewDataSource> {
+  // Height constraint for the bottom sheet.
+  NSLayoutConstraint* _heightConstraint;
+
+  // List of password sharing recipients that the user can pick from.
+  NSArray<RecipientInfo*>* _recipients;
+}
+
+@end
 
 @implementation FamilyPickerViewController
 
@@ -20,6 +35,52 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView*)tableView
+    numberOfRowsInSection:(NSInteger)section {
+  return _recipients.count;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView*)theTableView {
+  return 1;
+}
+
+- (UITableViewCell*)tableView:(UITableView*)tableView
+        cellForRowAtIndexPath:(NSIndexPath*)indexPath {
+  SettingsImageDetailTextCell* cell =
+      [tableView dequeueReusableCellWithIdentifier:@"cell"];
+  cell.selectionStyle = UITableViewCellSelectionStyleNone;
+
+  cell.textLabel.text = _recipients[indexPath.row].fullName;
+  cell.detailTextLabel.text = _recipients[indexPath.row].email;
+  // TODO(crbug.com/1463882): Replace with the actual image of the recipient.
+  cell.image = DefaultSymbolTemplateWithPointSize(
+      kPersonCropCircleSymbol, kAccountProfilePhotoDimension);
+
+  cell.userInteractionEnabled = YES;
+
+  return cell;
+}
+
+#pragma mark - Private
+
+- (UITableView*)createTableView {
+  UITableView* tableView = [super createTableView];
+
+  tableView.dataSource = self;
+  tableView.accessibilityIdentifier = @"FamilyPickerBottomSheetViewId";
+  [tableView registerClass:SettingsImageDetailTextCell.class
+      forCellReuseIdentifier:@"cell"];
+
+  _heightConstraint = [tableView.heightAnchor
+      constraintEqualToConstant:[self tableViewEstimatedRowHeight] *
+                                _recipients.count];
+  _heightConstraint.active = YES;
+
+  return tableView;
 }
 
 @end
