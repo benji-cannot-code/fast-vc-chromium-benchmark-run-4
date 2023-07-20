5b05d13d4babe759d936229c3f84acefaf5370e4
Federica Germinario <fedegermi@google.com>
2023-07-20 12:29:00 +0000

    [ios] Rename userdefaults-internal into userdefaults-internals
    Rename url chrome://userdefaults-internal into
    chrome://userdefaults-internals as suggested in the design doc:
    go/userdefaults-internal
    
    Bug: 1451365
    Change-Id: I6501abe8f0c07f5bcb8d4db5a32c1c54dc9872eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701377
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172907}

--- ios/chrome/browser/ui/webui/userdefaults_internals_ui.mm
+++ ios/chrome/browser/ui/webui/userdefaults_internals_ui.mm
@@ -36,7 +36,7 @@ class UserDefaultsInternalsSource : public web::URLDataSourceIOS {
 
   // web::URLDataSourceIOS
   std::string GetSource() const override {
-    return kChromeUIUserDefaultsInternalHost;
+    return kChromeUIUserDefaultsInternalsHost;
   }
 
   std::string GetMimeType(const std::string& path) const override {
