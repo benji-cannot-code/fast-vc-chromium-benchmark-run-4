105f46f6700365f508e40feb313a4812c32f6295
howardchung <howardchung@google.com>
2023-07-20 09:51:12 +0000

    Floss: Disable Floss if specified not available
    When FlossIsAvailable is overwritten to disable from commandline, we
    remove the floss flag from chrome://flags. This could have problems if
    finch experiment enables the flag on some unavailable devices, e.g.
    marasov. This CL fixes that by checking the FlossIsAvailable when
    determine if floss is enabled.
    
    BUG=b:285808593
    TEST=overwritten FlossIsAvailable and check Floss is not enabled.
    
    Change-Id: Ie6d8ac0dfa1bf084997f411f24e52f22b7c26f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684582
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Yun-Hao Chung <howardchung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172859}

--- chromeos/startup/browser_params_proxy.h
+++ chromeos/startup/browser_params_proxy.h
@@ -101,6 +101,8 @@ class COMPONENT_EXPORT(CHROMEOS_STARTUP) BrowserParamsProxy {
 
   bool UseFlossBluetooth() const;
 
+  bool IsFlossAvailable() const;
+
   bool IsCurrentUserDeviceOwner() const;
 
   bool IsCurrentUserEphemeral() const;
