53ebe146b949065474293f5c5de78030314b850b
James Forshaw <forshaw@chromium.org>
2023-07-20 17:08:08 +0000

    [Windows] Remove use of ResolveNtFunctionPtr in sandbox policy.
    This CL removes the use of ResolveNtFunctionPtr in the Windows sandbox
    policy code and replaces it with a direct call to NtQueryObject.
    
    Bug: 1282011
    Change-Id: I78ee47c90e979dcace3fc97d8401e58c40f670ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705944
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173042}

--- sandbox/policy/win/sandbox_win.cc
+++ sandbox/policy/win/sandbox_win.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/policy/win/sandbox_win.h"
 
 #include <stddef.h>
+#include <windows.h>
+#include <winternl.h>
 
 #include <map>
 #include <sstream>
@@ -60,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/win/src/sandbox.h"
 #include "sandbox/win/src/sandbox_nt_util.h"
 #include "sandbox/win/src/sandbox_policy_base.h"
-#include "sandbox/win/src/win_utils.h"
 
 namespace sandbox {
 namespace policy {
@@ -349,8 +350,6 @@ typedef BOOL(WINAPI* DuplicateHandleFunctionPtr)(HANDLE source_process_handle,
 
 DuplicateHandleFunctionPtr g_iat_orig_duplicate_handle;
 
-NtQueryObjectFunction g_QueryObject = NULL;
-
 static const char* kDuplicateHandleWarning =
     "You are attempting to duplicate a privileged handle into a sandboxed"
     " process.\n Please contact security@chromium.org for assistance.";
@@ -362,7 +361,8 @@ void CheckDuplicateHandle(HANDLE handle) {
       reinterpret_cast<OBJECT_TYPE_INFORMATION*>(buffer);
   ULONG size = sizeof(buffer) - sizeof(wchar_t);
   NTSTATUS error;
-  error = g_QueryObject(handle, ObjectTypeInformation, type_info, size, &size);
+  error =
+      ::NtQueryObject(handle, ObjectTypeInformation, type_info, size, &size);
   CHECK(NT_SUCCESS(error));
   type_info->Name.Buffer[type_info->Name.Length / sizeof(wchar_t)] = L'\0';
 
@@ -370,7 +370,7 @@ void CheckDuplicateHandle(HANDLE handle) {
   OBJECT_BASIC_INFORMATION basic_info;
   size = sizeof(basic_info);
   error =
-      g_QueryObject(handle, ObjectBasicInformation, &basic_info, size, &size);
+      ::NtQueryObject(handle, ObjectBasicInformation, &basic_info, size, &size);
   CHECK(NT_SUCCESS(error));
 
   CHECK(!(basic_info.GrantedAccess & WRITE_DAC)) << kDuplicateHandleWarning;
@@ -906,7 +906,6 @@ bool SandboxWin::InitBrokerServices(BrokerServices* broker_services) {
                               &module));
     DWORD result = ::GetModuleFileNameW(module, module_name, MAX_PATH);
     if (result && (result != MAX_PATH)) {
-      ResolveNTFunctionPtr("NtQueryObject", &g_QueryObject);
       result = GetIATPatchFunctionHandle().Patch(
           module_name, "kernel32.dll", "DuplicateHandle",
           reinterpret_cast<void*>(DuplicateHandlePatch));
