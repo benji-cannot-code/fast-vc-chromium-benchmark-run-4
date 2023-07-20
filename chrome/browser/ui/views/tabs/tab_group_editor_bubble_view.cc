09f68f6162d2ced7ebe090261a8006eb3c5d58af
Emiliano Hurtado <emilianohdz@google.com>
2023-07-20 20:16:19 +0000

    Revert "[ChromeRefresh2023] Update icons and labels of the Tab Group Edit Dialog"
    This reverts commit 2b5130c982c12106d3e2b54986ded331ea64177c.
    
    Reason for revert: It might have caused a regression.
    
    Original change's description:
    > [ChromeRefresh2023] Update icons and labels of the Tab Group Edit Dialog
    >
    > Bug: 1422112
    > Change-Id: Iadf2ff9be48ca2a5e60301a826b260b4c4d08c1b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679893
    > Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Commit-Queue: Emiliano Hurtado <emilianohdz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1172422}
    
    Bug: 1422112
    Change-Id: I94a219bc0a639f7e778fbdc3782e72b1ebf38950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4704207
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Owners-Override: Demetrios Papadopoulos <dpapad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173149}

--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/dialog_model_field.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/pointer/touch_ui_controller.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/events/types/event_type.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
@@ -101,10 +100,6 @@ std::unique_ptr<views::LabelButton> CreateMenuItem(
 
   auto button = CreateBubbleMenuItem(button_id, name, callback, icon);
   button->SetBorder(views::CreateEmptyBorder(control_insets));
-  if (features::IsChromeRefresh2023()) {
-    button->SetTitleTextStyle(views::style::STYLE_BODY_3_EMPHASIS,
-                              SK_ColorTRANSPARENT);
-  }
 
   return button;
 }
@@ -527,17 +522,13 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
     // utilizes a different view (view::Label) that does not have an option to
     // take in an image like the other line items do.
     save_group_icon = save_group_line_container->AddChildView(
-        std::make_unique<views::ImageView>(ui::ImageModel::FromVectorIcon(
-            features::IsChromeRefresh2023() ? kSaveGroupRefreshIcon
-                                            : kSaveGroupIcon)));
+        std::make_unique<views::ImageView>(
+            ui::ImageModel::FromVectorIcon(kSaveGroupIcon)));
 
     save_group_label =
         save_group_line_container->AddChildView(std::make_unique<views::Label>(
             l10n_util::GetStringUTF16(IDS_TAB_GROUP_HEADER_CXMENU_SAVE_GROUP)));
     save_group_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-    if (features::IsChromeRefresh2023()) {
-      save_group_label->SetTextStyle(views::style::STYLE_BODY_3_EMPHASIS);
-    }
 
     save_group_toggle_ = save_group_line_container->AddChildView(
         std::make_unique<views::ToggleButton>(
@@ -560,9 +551,7 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
       l10n_util::GetStringUTF16(IDS_TAB_GROUP_HEADER_CXMENU_NEW_TAB_IN_GROUP),
       base::BindRepeating(&TabGroupEditorBubbleView::NewTabInGroupPressed,
                           base::Unretained(this)),
-      ui::ImageModel::FromVectorIcon(features::IsChromeRefresh2023()
-                                         ? kNewTabInGroupRefreshIcon
-                                         : kNewTabInGroupIcon)));
+      ui::ImageModel::FromVectorIcon(kNewTabInGroupIcon)));
   menu_items_.push_back(new_tab_menu_item);
 
   menu_items_.push_back(AddChildView(CreateMenuItem(
@@ -570,17 +559,13 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
       l10n_util::GetStringUTF16(IDS_TAB_GROUP_HEADER_CXMENU_UNGROUP),
       base::BindRepeating(&TabGroupEditorBubbleView::UngroupPressed,
                           base::Unretained(this), header_view),
-      ui::ImageModel::FromVectorIcon(features::IsChromeRefresh2023()
-                                         ? kUngroupRefreshIcon
-                                         : kUngroupIcon))));
+      ui::ImageModel::FromVectorIcon(kUngroupIcon))));
 
   views::LabelButton* close_group_menu_item = AddChildView(CreateMenuItem(
       TAB_GROUP_HEADER_CXMENU_CLOSE_GROUP, GetTextForCloseButton(),
       base::BindRepeating(&TabGroupEditorBubbleView::CloseGroupPressed,
                           base::Unretained(this)),
-      ui::ImageModel::FromVectorIcon(features::IsChromeRefresh2023()
-                                         ? kCloseGroupRefreshIcon
-                                         : kCloseGroupIcon)));
+      ui::ImageModel::FromVectorIcon(kCloseGroupIcon)));
   close_group_menu_item->SetProperty(views::kElementIdentifierKey,
                                      kTabGroupEditorBubbleCloseGroupButtonId);
   menu_items_.push_back(close_group_menu_item);
@@ -592,9 +577,7 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
       base::BindRepeating(
           &TabGroupEditorBubbleView::MoveGroupToNewWindowPressed,
           base::Unretained(this)),
-      ui::ImageModel::FromVectorIcon(features::IsChromeRefresh2023()
-                                         ? kMoveGroupToNewWindowRefreshIcon
-                                         : kMoveGroupToNewWindowIcon)));
+      ui::ImageModel::FromVectorIcon(kMoveGroupToNewWindowIcon)));
   move_menu_item->SetEnabled(
       tab_strip_model->count() !=
       tab_strip_model->group_model()->GetTabGroup(group_)->tab_count());
