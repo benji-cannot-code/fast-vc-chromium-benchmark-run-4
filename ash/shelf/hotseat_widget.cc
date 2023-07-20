4504ea006082fd9480a23e0f74f31e87e088aa71
Xiaodan Zhu <zxdan@chromium.org>
2023-07-20 01:34:18 +0000

    jellyroll shadow: hotseat shadow
    This cl applies dynamic colors to hotseat shadow.
    
    The screenshots of dynamic colored shadows with test colors are
    shown below:
    https://screenshot.googleplex.com/8e72tJ7DSSkuDrj
    
    Bug: b:252359187
    Change-Id: I4ee628ee496a405cd5deb6f6e85ca7f49e177234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4700753
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172719}

--- ash/shelf/hotseat_widget.cc
+++ ash/shelf/hotseat_widget.cc
@@ -537,6 +537,9 @@ void HotseatWidget::DelegateView::Init(
   auto* shadow_layer = shadow_->GetLayer();
   parent_layer->Add(shadow_layer);
   parent_layer->StackAtBottom(shadow_layer);
+
+  // Make shadow observe the widget theme change.
+  shadow_->ObserveColorProviderSource(hotseat_widget);
 }
 
 void HotseatWidget::DelegateView::UpdateTranslucentBackground() {
