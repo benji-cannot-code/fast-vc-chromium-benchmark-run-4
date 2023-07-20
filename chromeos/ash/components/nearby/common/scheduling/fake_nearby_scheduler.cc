c8e50c858545dd21373eed558cd884988d83ab53
Juliet Levesque <julietlevesque@google.com>
2023-07-20 18:19:34 +0000

    [Nearby Presence] Download and save remote creds for daily sync
    Implements the daily sunc flow for downloading remote devices'
    shared credentials from the NP server, and saving them to the NP
    library. Exponential retry on failures. This completes the daily
    sync flow.
    
    when plumbed to chrome://nearby-internals debug page
    
    Test: unit tests only, although will manually test in a follow up CL
    Bug: b/4641058
    Change-Id: I5dd13301825f79621d3541e7f7103768c3aa6e20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4646130
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173093}

--- chromeos/ash/components/nearby/common/scheduling/fake_nearby_scheduler.cc
+++ chromeos/ash/components/nearby/common/scheduling/fake_nearby_scheduler.cc
@@ -20,6 +20,10 @@ void FakeNearbyScheduler::MakeImmediateRequest() {
 
 void FakeNearbyScheduler::HandleResult(bool success) {
   handled_results_.push_back(success);
+
+  if (handle_result_callback_) {
+    std::move(handle_result_callback_).Run();
+  }
 }
 
 void FakeNearbyScheduler::Reschedule() {
@@ -71,6 +75,10 @@ void FakeNearbyScheduler::SetIsWaitingForResult(bool is_waiting) {
 
 void FakeNearbyScheduler::SetNumConsecutiveFailures(size_t num_failures) {
   num_consecutive_failures_ = num_failures;
+}
+
+void FakeNearbyScheduler::SetHandleResultCallback(base::OnceClosure callback) {
+  handle_result_callback_ = std::move(callback);
 }
 
 }  // namespace ash::nearby
