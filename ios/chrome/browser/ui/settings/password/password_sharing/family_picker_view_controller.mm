1dc8650b1b021f6f4c4fb367c40ec3f7ad96b9d5
Rafał Godlewski <rgod@google.com>
2023-07-20 18:01:41 +0000

    [iOS] Add subtitle to family picker view
    Bug: 1463882
    Change-Id: I461b420f76f251fe499c542a36672ab1794262e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701216
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173085}

--- ios/chrome/browser/ui/settings/password/password_sharing/family_picker_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_sharing/family_picker_view_controller.mm
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/authentication_constants.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_image_detail_text_cell.h"
 #import "ios/chrome/browser/ui/settings/password/password_sharing/recipient_info.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -34,6 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
+  self.subtitleString =
+      l10n_util::GetNSString(IDS_IOS_PASSWORD_SHARING_FAMILY_PICKER_SUBTITLE);
+  self.subtitleTextStyle = UIFontTextStyleFootnote;
   [super viewDidLoad];
 }
 
