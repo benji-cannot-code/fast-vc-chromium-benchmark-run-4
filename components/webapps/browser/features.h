6e868054870208ad017acc51417e90aa2d8e68f0
Ella Ge <eirage@chromium.org>
2023-07-20 05:13:31 +0000

    Block mobile PWA install prompt if the prompt ignore recently
    Add the block if ignored recently behind a feature flag.
    Reduce the default "days_after_ignore" to 7 days - it's ok to just
    change the value because it's only used for mobile install prompt.
    
    Bug: 1466023
    Change-Id: Ic75f13258b01147eab1fef75c3c287dc03a253ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698425
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172779}

--- components/webapps/browser/features.h
+++ components/webapps/browser/features.h
@@ -17,7 +17,7 @@ namespace features {
 // Default number of days that dismissing or ignoring the banner will prevent it
 // being seen again for.
 constexpr unsigned int kMinimumBannerBlockedToBannerShown = 90;
-constexpr unsigned int kMinimumDaysBetweenBannerShows = 14;
+constexpr unsigned int kMinimumDaysBetweenBannerShows = 7;
 
 // Default site engagement required to trigger the banner.
 constexpr unsigned int kDefaultTotalEngagementToTrigger = 2;
@@ -41,6 +41,7 @@ extern const base::FeatureParam<base::TimeDelta>
     kInstallPromptGlobalGuardrails_IgnorePeriod;
 BASE_DECLARE_FEATURE(kWebApkInstallFailureNotification);
 BASE_DECLARE_FEATURE(kWebApkInstallFailureRetry);
+BASE_DECLARE_FEATURE(kBlockInstallPromptIfIgnoreRecently);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 BASE_DECLARE_FEATURE(kCreateShortcutIgnoresManifest);
