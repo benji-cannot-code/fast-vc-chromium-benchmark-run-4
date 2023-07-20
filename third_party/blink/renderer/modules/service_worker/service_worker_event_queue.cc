97ef3ef380feed4d5366cf81b037065c8ce24fd1
Minoru Chikamune <chikamune@chromium.org>
2023-07-20 07:21:30 +0000

    Remove the investigation code for https://crbug.com/1462568
    The following CLs added the investigation code for
    https://crbug.com/1462568. This CL reverts them since the M117 release
    is coming.
    
    - https://crrev.com/c/4672488
    - https://crrev.com/c/4672312
    - https://crrev.com/c/4680985
    - https://crrev.com/c/4691690
    
    Bug: 1462568
    Bug: 1466277
    Bug: 1466045
    Bug: 1466104
    Change-Id: I9e7d2731fea2cd701a28494616ecaab18822cdef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4703585
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172811}

--- third_party/blink/renderer/modules/service_worker/service_worker_event_queue.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_event_queue.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/service_worker/service_worker_event_queue.h"
 
 #include "base/containers/contains.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/default_tick_clock.h"
@@ -69,7 +68,6 @@ ServiceWorkerEventQueue::~ServiceWorkerEventQueue() {
 
 void ServiceWorkerEventQueue::Start() {
   DCHECK(!timer_.IsRunning());
-  timer_started_ = true;
   if (!HasInflightEvent() && !HasScheduledIdleCallback()) {
     // If no event happens until Start(), the idle callback should be scheduled.
     OnNoInflightEvent();
@@ -303,19 +301,8 @@ void ServiceWorkerEventQueue::UpdateStatus() {
 }
 
 void ServiceWorkerEventQueue::ScheduleIdleCallback(base::TimeDelta delay) {
-  CHECK(!HasInflightEvent());
-  CHECK(!HasScheduledIdleCallback());
-
-  if (!timer_started_) {
-    // TODO(crbug.com/1462568): After investigating crash bug, the following
-    // DumpWithoutCrashing should be removed.
-    static bool has_dumped_without_crashing = false;
-    if (!has_dumped_without_crashing) {
-      has_dumped_without_crashing = true;
-      SCOPED_CRASH_KEY_NUMBER("SWEventQueue", "delay", delay.InMilliseconds());
-      base::debug::DumpWithoutCrashing();
-    }
-  }
+  DCHECK(!HasInflightEvent());
+  DCHECK(!HasScheduledIdleCallback());
 
   // WTF::Unretained() is safe because the task runner will be destroyed
   // before |this| is destroyed at ServiceWorkerGlobalScope::Dispose().
