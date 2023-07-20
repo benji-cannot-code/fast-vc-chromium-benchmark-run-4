b3f3238836efeedf255cdceab40447b91f465daa
Matthias Kaehlcke <mka@chromium.org>
2023-07-20 16:08:52 +0000

    Add a function to feature service for checking if suspend to disk is enabled
    This adds IsSuspendToDiskEnabled() to ChromeFeaturesService.
    
    Bug: b:291068562,b:285946413
    
    Change-Id: I0ab4d71f4cbe479d718893ef82fa32f7b9e67e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4702183
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173008}

--- chrome/browser/ash/dbus/chrome_features_service_provider.cc
+++ chrome/browser/ash/dbus/chrome_features_service_provider.cc
@@ -166,6 +166,13 @@ void ChromeFeaturesServiceProvider::Start(
                           weak_ptr_factory_.GetWeakPtr()),
       base::BindRepeating(&ChromeFeaturesServiceProvider::OnExported,
                           weak_ptr_factory_.GetWeakPtr()));
+  exported_object->ExportMethod(
+      chromeos::kChromeFeaturesServiceInterface,
+      chromeos::kChromeFeaturesServiceIsSuspendToDiskEnabledMethod,
+      base::BindRepeating(&ChromeFeaturesServiceProvider::IsSuspendToDiskEnabled,
+                          weak_ptr_factory_.GetWeakPtr()),
+      base::BindRepeating(&ChromeFeaturesServiceProvider::OnExported,
+                          weak_ptr_factory_.GetWeakPtr()));
 }
 
 void ChromeFeaturesServiceProvider::OnExported(
@@ -430,6 +437,13 @@ void ChromeFeaturesServiceProvider::IsDnsProxyEnabled(
     dbus::ExportedObject::ResponseSender response_sender) {
   SendResponse(method_call, std::move(response_sender),
                !base::FeatureList::IsEnabled(features::kDisableDnsProxy));
+}
+
+void ChromeFeaturesServiceProvider::IsSuspendToDiskEnabled(
+    dbus::MethodCall* method_call,
+    dbus::ExportedObject::ResponseSender response_sender) {
+  SendResponse(method_call, std::move(response_sender),
+               base::FeatureList::IsEnabled(features::kSuspendToDisk));
 }
 
 }  // namespace ash
