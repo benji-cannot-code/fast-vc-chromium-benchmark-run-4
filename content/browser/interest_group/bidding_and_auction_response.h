a1d1f05c56fcb4592fb91d6b119cf741a831c7f5
Russ Hamilton <behamilton@google.com>
2023-07-20 13:12:28 +0000

    Fix bug in bidding and auction response parser
    We were incorrectly calculating the payload length for payloads with
    lengths greater than 255.
    
    Change-Id: I2ecb214f03937e7efff4e2d045a18c7c3ea1d99d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701904
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172920}

--- content/browser/interest_group/bidding_and_auction_response.h
+++ content/browser/interest_group/bidding_and_auction_response.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-absl::optional<base::span<const uint8_t>>
+absl::optional<base::span<const uint8_t>> CONTENT_EXPORT
 ExtractCompressedBiddingAndAuctionResponse(
     base::span<const uint8_t> decrypted_data);
 
