070230e0e54504d066c47ad3ec71db4961b2a82f
Lei Zhang <thestig@chromium.org>
2023-07-20 00:58:44 +0000

    CR2023: Start enabling Chrome Refresh 2023 for PDF Viewer
    This just applies the chrome-refresh-2023 attribute to the PDF Viewer,
    when the feature is enabled.
    
    Bug: 1466280
    Change-Id: I8d5892ad96ef06ddc89a4e56da5628613f7900fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4702191
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172701}

--- chrome/browser/pdf/pdf_extension_util.cc
+++ chrome/browser/pdf/pdf_extension_util.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/zoom/page_zoom_constants.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/base/webui/web_ui_util.h"
 
 #if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
@@ -50,6 +51,8 @@ void AddCommonStrings(base::Value::Dict* dict) {
   for (const auto& resource : kPdfResources)
     dict->Set(resource.name, l10n_util::GetStringUTF16(resource.id));
 
+  dict->Set("chromeRefresh2023Attribute",
+            features::IsChromeWebuiRefresh2023() ? "chrome-refresh-2023" : "");
   dict->Set("presetZoomFactors", zoom::GetPresetZoomFactorsAsJSON());
 }
 
