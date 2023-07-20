392cfaea8fbd09c676f98ff853cb95f91dd93568
Joel Fernandes <joelaf@google.com>
2023-07-20 19:25:25 +0000

    base/threading: Separate ChromeOS-specific Linux code from vanilla Linux
    Linux and ChromeOS code are intertwined making the code hard to read.
    This patch splits ChromeOS code into its own translation unit and get
    rid of a lot of IS_BUILD(CHROMEOS) macros. This will help maintenance
    in the future.
    
    Bug: b/262267726
    Test: base_unittests --gtest_filter="*ProcessTest.*ThreadBackgrounding*"
    Change-Id: I3fdb5229e4caf4b1fc2c3d3ad1dfc9b2fbc888d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615507
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173126}

--- base/threading/platform_thread.h
+++ base/threading/platform_thread.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/message_loop/message_pump_type.h"
+#include "base/process/process_handle.h"
 #include "base/threading/platform_thread_ref.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -310,6 +311,11 @@ class BASE_EXPORT PlatformThreadLinux : public PlatformThreadBase {
   static void SetThreadType(PlatformThreadId process_id,
                             PlatformThreadId thread_id,
                             ThreadType thread_type);
+
+  // For a given thread id and thread type, setup the cpuset and schedtune
+  // CGroups for the thread.
+  static void SetThreadCgroupsForThreadType(PlatformThreadId thread_id,
+                                            ThreadType thread_type);
 };
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 
@@ -319,6 +325,13 @@ class BASE_EXPORT PlatformThreadChromeOS : public PlatformThreadLinux {
   // Signals that the feature list has been initialized. Used for preventing
   // race conditions and crashes, see comments in PlatformThreadApple.
   static void InitFeaturesPostFieldTrial();
+
+  // Toggles a specific thread's type at runtime. This is the ChromeOS-specific
+  // version and includes Linux's functionality but does slightly more. See
+  // PlatformThreadLinux's SetThreadType() header comment for Linux details.
+  static void SetThreadType(PlatformThreadId process_id,
+                            PlatformThreadId thread_id,
+                            ThreadType thread_type);
 };
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
