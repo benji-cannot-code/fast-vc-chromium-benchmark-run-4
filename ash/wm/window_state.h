1401404b332abc69541344e496ba40ed3c389b26
minch <minch@google.com>
2023-07-20 20:26:59 +0000

    Rename setters inside WindowState
    Fixed: b/290657501
    Change-Id: I761c286b862f5bade6b3cb1130841821761adbf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671531
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173155}

--- ash/wm/window_state.h
+++ ash/wm/window_state.h
@@ -164,14 +164,12 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
     autohide_shelf_when_maximized_or_fullscreen_ = value;
   }
 
-  // TODO(minch): Rename the multiple setters below to be for example
-  // `set_pre_auto_manage_window_bounds`.
   // Gets/Sets the bounds of the window before it was moved by the auto window
   // management. As long as it was not auto-managed, it will return NULL.
   const absl::optional<gfx::Rect> pre_auto_manage_window_bounds() {
     return pre_auto_manage_window_bounds_;
   }
-  void SetPreAutoManageWindowBounds(const gfx::Rect& bounds) {
+  void set_pre_auto_manage_window_bounds(const gfx::Rect& bounds) {
     pre_auto_manage_window_bounds_ = absl::make_optional(bounds);
   }
 
@@ -179,7 +177,7 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   const absl::optional<gfx::Rect> pre_added_to_workspace_window_bounds() {
     return pre_added_to_workspace_window_bounds_;
   }
-  void SetPreAddedToWorkspaceWindowBounds(const gfx::Rect& bounds) {
+  void set_pre_added_to_workspace_window_bounds(const gfx::Rect& bounds) {
     pre_added_to_workspace_window_bounds_ = absl::make_optional(bounds);
   }
 
@@ -189,11 +187,11 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   persistent_window_info_of_display_removal() {
     return persistent_window_info_of_display_removal_;
   }
-  void SetPersistentWindowInfoOfDisplayRemoval(
+  void set_persistent_window_info_of_display_removal(
       const PersistentWindowInfo& info) {
     persistent_window_info_of_display_removal_ = absl::make_optional(info);
   }
-  void ResetPersistentWindowInfoOfDisplayRemoval() {
+  void reset_persistent_window_info_of_display_removal() {
     persistent_window_info_of_display_removal_.reset();
   }
 
@@ -203,7 +201,7 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   persistent_window_info_of_screen_rotation() {
     return persistent_window_info_of_screen_rotation_;
   }
-  void SetPersistentWindowInfoOfScreenRotation(
+  void set_persistent_window_info_of_screen_rotation(
       const PersistentWindowInfo& info) {
     persistent_window_info_of_screen_rotation_ = absl::make_optional(info);
   }
