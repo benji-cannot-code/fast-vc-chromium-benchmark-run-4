3850b44ffff0cf47f8e62344e98df3d12fbd28a7
Aida Zolic <aidazolic@chromium.org>
2023-07-20 17:23:15 +0000

    dlp:Add new i18n strings for Files block
    Adds translation strings for block (error) notifications and dialogs
    for files restriction.
    Adds util functions for computing the message based on action, number
    of restricted files, etc. that are shared between the notifications and
    the dialogs.
    
    R=chromeos-dlp@google.com
    
    Bug: b:279435843
    Change-Id: I8e0e5d61d6b0f4937c4effcb02f3e9cb941e269e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684182
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Luca Accorsi <accorsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173056}

--- chrome/browser/ash/policy/dlp/files_policy_notification_manager_browsertest.cc
+++ chrome/browser/ash/policy/dlp/files_policy_notification_manager_browsertest.cc
@@ -961,8 +961,9 @@ IN_PROC_BROWSER_TEST_P(IOTaskBrowserTest,
 
   auto notification = bridge_->GetDisplayedNotification(kNotificationId1);
   ASSERT_TRUE(notification.has_value());
-  const std::u16string title =
-      action == dlp::FileAction::kCopy ? u"Blocked copy" : u"Blocked move";
+  const std::u16string title = action == dlp::FileAction::kCopy
+                                   ? u"2 files blocked from copying"
+                                   : u"2 files blocked from moving";
   EXPECT_EQ(notification->title(), title);
 
   notification = bridge_->GetDisplayedNotification(kNotificationId2);
@@ -1022,8 +1023,9 @@ IN_PROC_BROWSER_TEST_P(IOTaskBrowserTest, MultiFileDismissRemovesIOInfo_Error) {
 
   auto notification = bridge_->GetDisplayedNotification(kNotificationId1);
   ASSERT_TRUE(notification.has_value());
-  const std::u16string title =
-      action == dlp::FileAction::kCopy ? u"Blocked copy" : u"Blocked move";
+  const std::u16string title = action == dlp::FileAction::kCopy
+                                   ? u"2 files blocked from copying"
+                                   : u"2 files blocked from moving";
   EXPECT_EQ(notification->title(), title);
 
   // Dismiss the notification.
@@ -1036,7 +1038,7 @@ IN_PROC_BROWSER_TEST_P(IOTaskBrowserTest, MultiFileDismissRemovesIOInfo_Error) {
 }
 
 // Tests that the IO task info for copy or move with single blocked file will
-// be removed upon the error notification is clicked.
+// be removed after the error notification is clicked.
 IN_PROC_BROWSER_TEST_P(IOTaskBrowserTest,
                        SingleFileNotificationRemovesIOInfo_Error) {
   auto [type, action] = GetParam();
@@ -1065,8 +1067,9 @@ IN_PROC_BROWSER_TEST_P(IOTaskBrowserTest,
 
   auto notification = bridge_->GetDisplayedNotification(kNotificationId1);
   ASSERT_TRUE(notification.has_value());
-  const std::u16string title =
-      action == dlp::FileAction::kCopy ? u"Blocked copy" : u"Blocked move";
+  const std::u16string title = action == dlp::FileAction::kCopy
+                                   ? u"File blocked from copying"
+                                   : u"File blocked from moving";
   EXPECT_EQ(notification->title(), title);
 
   // Click Learn more.
@@ -1124,8 +1127,9 @@ IN_PROC_BROWSER_TEST_P(IOTaskBrowserTest, SingleFileOkProceeds_Mix) {
   EXPECT_TRUE(fpnm_->HasIOTask(kTaskId1));
 
   // Error notification.
-  const std::u16string title2 =
-      action == dlp::FileAction::kCopy ? u"Blocked copy" : u"Blocked move";
+  const std::u16string title2 = action == dlp::FileAction::kCopy
+                                    ? u"File blocked from copying"
+                                    : u"File blocked from moving";
   notification = bridge_->GetDisplayedNotification(kNotificationId1);
   ASSERT_TRUE(notification.has_value());
   EXPECT_EQ(notification->title(), title2);
