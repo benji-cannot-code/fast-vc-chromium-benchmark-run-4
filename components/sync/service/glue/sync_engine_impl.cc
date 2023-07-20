907b8c08f74f59ca48ba0697564620442b7c5515
Rushan Suleymanov <rushans@google.com>
2023-07-20 09:25:55 +0000

    [Sync] Remove SyncUpdateInvalidatorState feature toggle
    The feature toggle was introduced and enabled by default in M109.
    
    Bug: 1404927
    Change-Id: Idaeec37d473f48c80961645a9141eb04d9196b2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4704224
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172848}

--- components/sync/service/glue/sync_engine_impl.cc
+++ components/sync/service/glue/sync_engine_impl.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/command_line.h"
-#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
 #include "base/functional/callback_helpers.h"
@@ -25,10 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/invalidation/public/invalidator_state.h"
 #include "components/invalidation/public/topic_invalidation_map.h"
 #include "components/sync/engine/data_type_activation_response.h"
-#include "components/sync/engine/engine_components_factory.h"
-#include "components/sync/engine/engine_components_factory_impl.h"
 #include "components/sync/engine/events/protocol_event.h"
-#include "components/sync/engine/net/http_bridge.h"
 #include "components/sync/engine/nigori/nigori.h"
 #include "components/sync/engine/polling_constants.h"
 #include "components/sync/engine/sync_engine_host.h"
@@ -37,16 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/service/active_devices_provider.h"
 #include "components/sync/service/glue/sync_engine_backend.h"
 #include "components/sync/service/glue/sync_transport_data_prefs.h"
-#include "components/sync/service/sync_prefs.h"
 
 namespace syncer {
 
 namespace {
-
-// Enables updating invalidator state for Sync standalone invalidations.
-BASE_FEATURE(kSyncUpdateInvalidatorState,
-             "SyncUpdateInvalidatorState",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Reads from prefs into a struct, to be posted across sequences.
 SyncEngineBackend::RestoredLocalTransportData
@@ -634,8 +624,7 @@ void SyncEngineImpl::ClearLocalTransportDataAndNotify() {
 
 void SyncEngineImpl::UpdateStandaloneInvalidationsState() {
   DCHECK(sync_invalidations_service_);
-  if (!sync_invalidations_service_->GetFCMRegistrationToken().has_value() &&
-      base::FeatureList::IsEnabled(kSyncUpdateInvalidatorState)) {
+  if (!sync_invalidations_service_->GetFCMRegistrationToken().has_value()) {
     OnInvalidatorStateChange(invalidation::TRANSIENT_INVALIDATION_ERROR);
     return;
   }
@@ -645,6 +634,9 @@ void SyncEngineImpl::UpdateStandaloneInvalidationsState() {
   // comparison between an optional and a string, so use has_value() directly.
   DCHECK(!sync_invalidations_service_->GetFCMRegistrationToken().has_value() ||
          sync_invalidations_service_->GetFCMRegistrationToken().value() != "");
+
+  // TODO(crbug.com/1442156): wait for FCM token to be committed before change
+  // the state to enabled.
   OnInvalidatorStateChange(invalidation::INVALIDATIONS_ENABLED);
 }
 
