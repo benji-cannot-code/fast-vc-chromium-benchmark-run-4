a0ec2e3427eddc8afe13f458953906c047f897f3
Evan Stade <estade@chromium.org>
2023-07-20 16:49:59 +0000

    IndexedDB: compress values before sending over IPC
    Renderer compresses IDB values using snappy to save on IPC overhead and
    to get the benefits of on-disk compression regardless of backing store.
    
    See DD linked in monorail issue.
    
    Bug: 1459300
    Change-Id: Idf036b7936314091bcf08cd30c92a671033893b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678529
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173030}

--- third_party/blink/renderer/modules/indexeddb/idb_value.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_value.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom-blink.h"
 #include "third_party/blink/public/platform/web_blob_info.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
+#include "third_party/blink/renderer/modules/indexeddb/idb_value_wrapping.h"
 #include "third_party/blink/renderer/platform/blob/blob_data.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "v8/include/v8.h"
@@ -34,6 +35,10 @@ IDBValue::~IDBValue() {
 }
 
 scoped_refptr<SerializedScriptValue> IDBValue::CreateSerializedValue() const {
+  scoped_refptr<SharedBuffer> decompressed;
+  if (IDBValueUnwrapper::Decompress(*data_, &decompressed)) {
+    const_cast<IDBValue*>(this)->SetData(decompressed);
+  }
   return SerializedScriptValue::Create(data_);
 }
 
--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -587,6 +587,7 @@ _CONFIG = [
             'v8::.+',
             'v8_inspector::.+',
             'inspector_protocol_encoding::.+',
+            'snappy::.+',
 
             # Inspector instrumentation and protocol
             'probe::.+',
