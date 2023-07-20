a2e1b114c57af41e09a0cbacbfb97490dd159743
Andrew Grieve <agrieve@chromium.org>
2023-07-20 18:34:44 +0000

    R8: Add a convenience wrapper for running disassembler
    Bug: None
    Change-Id: Icce32533200259de45fa589cdf401939e9db0e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4704785
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173101}

---
+++ third_party/r8/disassemble.sh
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+R8DIR=$(dirname $0)
+set -x
+exec $R8DIR/../jdk/current/bin/java -cp $R8DIR/lib/r8.jar \
+  com.android.tools.r8.Disassemble "${@:---help}"
