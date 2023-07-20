0b6936890bc946dd215ea81cc76b6485cc218b6e
Viktor Semeniuk <vsemeniuk@google.com>
2023-07-20 14:05:06 +0000

    Fix flakiness of NoMenuItemOnPasswordManagerPage test
    The test was flaky because on destruction some expectations were failing
    due to the page being closed to fast.
    
    Change-Id: Iaafa15dbf98a065d23a174d532559ad28764dcaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4693903
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172936}

--- chrome/browser/ui/toolbar/app_menu_model_interactive_uitest.cc
+++ chrome/browser/ui/toolbar/app_menu_model_interactive_uitest.cc
@@ -255,6 +255,8 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerMenuItemInteractiveTest,
   RunTestSequence(
       AddInstrumentedTab(kPrimaryTabPageElementId,
                          GURL("chrome://password-manager/passwords")),
+      WaitForWebContentsReady(kPrimaryTabPageElementId,
+                              GURL("chrome://password-manager/passwords")),
       PressButton(kAppMenuButtonElementId),
       EnsureNotPresent(AppMenuModel::kPasswordManagerMenuItem));
 }
