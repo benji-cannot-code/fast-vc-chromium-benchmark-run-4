fc88df180fab85e2b31f51f0d17fef077e45e16e
adamta <adamta@google.com>
2023-07-21 20:04:27 +0000

    [iOS] Remove webfeed flag from client
    Enables Follow on iOS by default. Removes flag from chrome://flags, but
    keeps the feature flag so that it can still be controlled through Finch.
    Adapts tests by adding mock browser states to the test web states, and
    removes Follow menu test that covered signed-in users with no Following
    feed.
    
    Change-Id: I8a3d7e84ae778edead6d5c87625ca43d869936bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706923
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173667}

--- ios/chrome/browser/ntp/features.mm
+++ ios/chrome/browser/ntp/features.mm
@@ -49,7 +49,7 @@ bool IsFeedOverrideForegroundDefaultsEnabled() {
 
 BASE_FEATURE(kEnableWebChannels,
              "EnableWebChannels",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableFeedBackgroundRefresh,
              "EnableFeedBackgroundRefresh",
