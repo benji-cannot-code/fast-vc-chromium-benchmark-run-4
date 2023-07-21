7f35bea0d978ce96050fac2ef87f952195c38441
Theodore Olsauskas-Warren <sauski@google.com>
2023-07-21 11:40:11 +0000

    Cookie controls: Move effective control state change into controller
    CL moves record keeping about whether the effective cookie setting was
    changed on the current page into the CookieControlsController.
    
    There is no behavior change difference with this CL, but this sets up
    the ability for the controller to notify when the page has been reloaded
    after a change has taken effect.
    
    Bug: 1446230
    Change-Id: I30c8efaa3c440f426863386593b973327f3d3574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4704944
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173427}

--- components/content_settings/browser/ui/cookie_controls_controller.cc
+++ components/content_settings/browser/ui/cookie_controls_controller.cc
@@ -224,6 +224,11 @@ bool CookieControlsController::FirstPartyCookiesBlocked() {
       net::CookieSettingOverrides());
 }
 
+bool CookieControlsController::HasCookieBlockingChangedForSite() {
+  auto current_status = GetStatus(GetWebContents());
+  return current_status.status != initial_page_cookie_controls_status_;
+}
+
 int CookieControlsController::GetAllowedCookieCount() const {
   auto* pscs = content_settings::PageSpecificContentSettings::GetForPage(
       GetWebContents()->GetPrimaryPage());
@@ -303,6 +308,11 @@ void CookieControlsController::PresentBlockedCookieCounter() {
 }
 
 void CookieControlsController::OnPageReloadDetected(int recent_reloads_count) {
+  auto status = GetStatus(GetWebContents());
+  initial_page_cookie_controls_status_ = status.status;
+  CHECK(initial_page_cookie_controls_status_ !=
+        CookieControlsStatus::kUninitialized);
+
   if (!base::FeatureList::IsEnabled(
           content_settings::features::kUserBypassUI)) {
     return;
@@ -314,7 +324,6 @@ void CookieControlsController::OnPageReloadDetected(int recent_reloads_count) {
     return;
   }
 
-  auto status = GetStatus(GetWebContents());
   int allowed_sites = GetAllowedSitesCount();
   int blocked_sites = GetBlockedSitesCount();
 
