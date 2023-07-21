6dcf77a76f129dbe2cb0e3f17bba48f9cb8fa98a
Nathan Muggli <nmuggli@google.com>
2023-07-21 21:54:03 +0000

    Update CDD MediaSize to have a min and max height
    To prepare for roll printing, the chrome.printing API needs to be able
    to inform the user what the min/max height for a print job can be.  This
    update will allow that information to be part of the MediaSize
    capability in the CDD message.
    
    Bug: b:271874152
    Test: ./out/components_unittests --gtest_filter=PrinterDescriptionTest*
    Change-Id: I1cba52452c1cd466777905f44fa1a03c54687ba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642118
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Nathan Muggli <nmuggli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173730}

--- components/cloud_devices/common/printer_description.h
+++ components/cloud_devices/common/printer_description.h
@@ -490,6 +490,11 @@ struct Media {
   std::string custom_display_name;
   std::string vendor_id;
   gfx::Rect printable_area_um;
+  // When `is_continuous_feed` is true, the min height will be stored in
+  // `size_um` and the max height is stored in `max_height_um`.  When
+  // `is_continuous_feed` is false, `max_height_um` is not used.  This only
+  // supports a variable height, not width, so the width is always fixed.
+  int max_height_um;
 };
 
 // Builds `Media` structs. The caller must call at least one method that sets
@@ -517,6 +522,7 @@ class MediaBuilder {
   MediaBuilder& WithSizeAndDefaultPrintableArea(const gfx::Size& size_um);
   MediaBuilder& WithSizeAndPrintableArea(const gfx::Size& size_um,
                                          const gfx::Rect& printable_area_um);
+  MediaBuilder& WithMaxHeight(int max_height_um);
 
   // This is equivalent to calling WithCustomName(), except it also tries to
   // match the name to a standard `MediaSize`.
@@ -544,6 +550,7 @@ class MediaBuilder {
 
   gfx::Size size_um_;
   gfx::Rect printable_area_um_;
+  int max_height_um_ = 0;
 };
 
 struct Interval {
