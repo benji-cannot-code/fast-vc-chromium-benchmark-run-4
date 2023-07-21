f6a0868039933e0010828b09e7883127dff83833
Solomon Kinard <solomonkinard@chromium.org>
2023-07-21 17:22:05 +0000

    file_url_loader: clang-tidy: concat-nested-namespace, use-equals-default
    * Nested namespaces can be concatenated
    * Use '= default' to define a trivial default constructor
    
    Change-Id: I938bc5280404e9c2226cac37b1c032e93148c2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707031
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173596}

--- content/public/browser/file_url_loader.h
+++ content/public/browser/file_url_loader.h
@@ -17,11 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/url_loader.mojom.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 
-namespace network {
-namespace mojom {
+namespace network::mojom {
 class URLLoaderFactory;
 }
-}  // namespace network
 
 namespace content {
 
@@ -30,7 +28,7 @@ class SharedCorsOriginAccessList;
 class CONTENT_EXPORT FileURLLoaderObserver
     : public mojo::FilteredDataSource::Filter {
  public:
-  FileURLLoaderObserver() {}
+  FileURLLoaderObserver() = default;
 
   FileURLLoaderObserver(const FileURLLoaderObserver&) = delete;
   FileURLLoaderObserver& operator=(const FileURLLoaderObserver&) = delete;
