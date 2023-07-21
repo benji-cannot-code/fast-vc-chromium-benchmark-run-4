c5e871d28dfda29f8b67502b171d7acd3699ab4a
Guido Urdaneta <guidou@chromium.org>
2023-07-21 12:52:01 +0000

    Integrate ChromeBrowsingDataModelDelegate with media device salts
    This CL:
    * Adds a new storage type for media device salts in CBDMD.
    * Adds media device salt support for the BrowsingDataModel::Delegate
      overrides in CBDMD: GetAllStorageKeys(), RemoveDataKey() and
      GetDataOwner()
    * Adds additional operations in MediaDeviceSaltService and
      MediaDeviceSaltDatabase to support the CBDMD functionality.
    
    Change-Id: I821e682a46ad38fb0323790d8c2439d5c31b74e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705267
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173451}

--- components/media_device_salt/media_device_salt_service.cc
+++ components/media_device_salt/media_device_salt_service.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "components/media_device_salt/media_device_id_salt.h"
 #include "components/media_device_salt/media_device_salt_database.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 
 namespace media_device_salt {
 
@@ -136,6 +137,38 @@ void MediaDeviceSaltService::FinalizeDeleteSalts(
     monitor->ProcessDevicesChanged(base::SystemMonitor::DEVTYPE_AUDIO);
   }
   std::move(done_closure).Run();
+}
+
+void MediaDeviceSaltService::DeleteSalt(const blink::StorageKey& storage_key,
+                                        base::OnceClosure done_closure) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (!base::FeatureList::IsEnabled(kMediaDeviceIdPartitioning)) {
+    std::move(done_closure).Run();
+    return;
+  }
+  db_.AsyncCall(&MediaDeviceSaltDatabase::DeleteEntry)
+      .WithArgs(storage_key)
+      .Then(base::BindOnce(&MediaDeviceSaltService::FinalizeDeleteSalts,
+                           weak_factory_.GetWeakPtr(),
+                           std::move(done_closure)));
+}
+
+void MediaDeviceSaltService::GetAllStorageKeys(
+    base::OnceCallback<void(std::vector<blink::StorageKey>)> callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (!base::FeatureList::IsEnabled(kMediaDeviceIdPartitioning)) {
+    std::move(callback).Run({});
+    return;
+  }
+  db_.AsyncCall(&MediaDeviceSaltDatabase::GetAllStorageKeys)
+      .Then(base::BindOnce(&MediaDeviceSaltService::FinalizeGetAllStorageKeys,
+                           weak_factory_.GetWeakPtr(), std::move(callback)));
+}
+
+void MediaDeviceSaltService::FinalizeGetAllStorageKeys(
+    base::OnceCallback<void(std::vector<blink::StorageKey>)> callback,
+    std::vector<blink::StorageKey> storage_keys) {
+  std::move(callback).Run(std::move(storage_keys));
 }
 
 std::string MediaDeviceSaltService::GetGlobalSalt() {
