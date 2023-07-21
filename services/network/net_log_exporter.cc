620379ce7dedc14c1ad270b3282705cc45581e69
Fergal Daly <fergal@chromium.org>
2023-07-21 02:23:06 +0000

    Revert "Add net-log max size command line flag"
    This reverts commit 41a946c134fe0401a47389f9fb2a07f1f7ed5684.
    
    This was not a clean revert due to https://crrev.com/c/4678102.
    
    I was going to just disable but this impacts fuchsia AND chromeos
    
    Reason for revert: Breaks on several Fuchsia bots
    
    E.g.
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-rel/8062/overview
    
    Original change's description:
    > Add net-log max size command line flag
    >
    > This CL adds support for a maximum file size flag when invoking
    > net-log export from the command line. The flag allows a user to specify the max size in MB for a net-log file.
    >
    > Bug: 1463983
    > Change-Id: I5fb4b493cc00db3e0d0181ce8ffe2cc5a63abd91
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678102
    > Commit-Queue: Steven Bingler <bingler@chromium.org>
    > Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1173118}
    
    Bug: 1463983
    Change-Id: I290d388f00c258b02d96f349c004a8fd52dca43e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707105
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Fergal Daly <fergal@google.com>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173289}

--- services/network/net_log_exporter.cc
+++ services/network/net_log_exporter.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
-#include "base/types/pass_key.h"
 #include "base/values.h"
 #include "net/log/file_net_log_observer.h"
 #include "net/log/net_log_util.h"
@@ -100,12 +99,6 @@ void NetLogExporter::Stop(base::Value::Dict polled_data,
                      std::move(callback)));
   file_net_observer_ = nullptr;
   state_ = STATE_IDLE;
-}
-
-// static
-base::FilePath NetLogExporter::CreateScratchDirForNetworkService(
-    base::PassKey<NetworkService>) {
-  return CreateScratchDir(base::RepeatingCallback<base::FilePath()>());
 }
 
 void NetLogExporter::SetCreateScratchDirHandlerForTesting(
