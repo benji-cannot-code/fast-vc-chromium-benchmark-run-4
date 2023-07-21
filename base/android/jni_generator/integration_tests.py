b3da4690bf18019dfcdc706948c03f4a38c54150
Andrew Grieve <agrieve@chromium.org>
2023-07-21 18:26:02 +0000

    Revert "jni_zero: Move ExtractNatives and CalledByNative parsing into parse.py"
    This reverts commit 363db7d995fd05b32395ded623da1167c6bb0827.
    
    Reason for revert: Broke webrtc roll:
    https://ci.chromium.org/ui/p/webrtc/builders/try/android_arm_rel/51932/overview
    
    Original change's description:
    > jni_zero: Move ExtractNatives and CalledByNative parsing into parse.py
    >
    > * Adds a dedicated .java file for javap integration tests
    > * Clean-ups around constructor handling - the CalledByNative
    >   for a constructor now has name=<init> and return_type=void.
    > * Rewrites javap parsing
    > * Use a custom exception type for parse errors so as not not mask
    >   Python syntax errors.
    >
    > Bug: 1406605
    > Change-Id: If099331246fe32ee5b6ce3bb4eb2041b1fc78cfa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4697557
    > Reviewed-by: Sam Maier <smaier@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1173219}
    
    Bug: 1406605
    Change-Id: I76d0883cb21e7193c91aa4737637b2da4fcc21a1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708010
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173628}

--- base/android/jni_generator/integration_tests.py
+++ base/android/jni_generator/integration_tests.py
@@ -290,7 +290,7 @@ class Tests(BaseTest):
     self._TestEndToEndRegistration(['SampleForTests.java'])
 
   def testFromClassFile(self):
-    self._TestEndToEndGeneration('JavapClass.class')
+    self._TestEndToEndGeneration('SampleNonProxy.class')
 
   def testUniqueAnnotations(self):
     self._TestEndToEndGeneration('SampleUniqueAnnotations.java', srcjar=True)
