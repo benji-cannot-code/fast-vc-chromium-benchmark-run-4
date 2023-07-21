366cd2accbe21f8d271644ed6686f00ade83f21c
Dirk Pranke <dpranke@google.com>
2023-07-21 19:41:09 +0000

    Revert "Visual debugger tracing implementation"
    This reverts commit 5c1dd56e888394b53bbaa407e8d0f39226ae6b74.
    
    Reason for revert: breaks viz_unittests on fuchsia-x64-cast-receiver-rel, see https://crbug.com/1466891
    
    Original change's description:
    > Visual debugger tracing implementation
    >
    > This change makes all visual debug positional logs now possibly trace
    > events when the visual debugger runtime is disabled.
    >
    > The default behavior remains the same but if the macro
    > "DBG_USE_VIZ_DEBUGGER_TRACE() 1" is defined at the time of call-site
    > invocation the draw call will result in a trace.
    >
    > This CL also merges the 'DrawRect' and 'DrawText' operations into one
    > single type everywhere.
    >
    > Bug: 1466354
    > Change-Id: I40ab07009b910ef8f0b68ed57a2b4fd7a824f9ac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672235
    > Reviewed-by: danakj <danakj@chromium.org>
    > Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    > Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1173320}
    
    Bug: 1466354
    Change-Id: Ie3fa1ea8d88d9da0a7ef6e996d23b5c567cc08ce
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706638
    Owners-Override: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173654}

--- components/viz/service/debugger/viz_debugger_unittests/viz_debugger_internal.h
+++ components/viz/service/debugger/viz_debugger_unittests/viz_debugger_internal.h
@@ -33,14 +33,17 @@ class VizDebuggerInternal : public VizDebugger {
   // Returns copies of corresponding buffers/vectors in the
   // VizDebugger instance.
   std::vector<DrawCall> GetDrawRectCalls();
+  std::vector<DrawTextCall> GetDrawTextCalls();
   std::vector<LogCall> GetLogs();
 
   // These functions return the tail index of each type of buffers.
   int GetRectCallsTailIdx();
+  int GetTextCallsTailIdx();
   int GetLogsTailIdx();
 
   // These functions get the size of each buffer.
   int GetRectCallsSize();
+  int GetTextCallsSize();
   int GetLogsSize();
 
   // This function returns a pointer to the Read-Write lock used
@@ -51,6 +54,7 @@ class VizDebuggerInternal : public VizDebugger {
 
   using VizDebugger::CallSubmitCommon;
   using VizDebugger::DrawCall;
+  using VizDebugger::DrawTextCall;
   using VizDebugger::FrameAsJson;
   using VizDebugger::LogCall;
   using VizDebugger::UpdateFilters;
