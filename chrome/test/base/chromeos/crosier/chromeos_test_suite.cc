94e5f35ecf1554d109232c3a0e8f1512ddcd42ef
Sven Zheng <svenzheng@chromium.org>
2023-07-21 23:54:21 +0000

    Create crosier mixin
    All crosier tests should use CrosierMixin. In the mixin,
    we add code that applies to all tests.
    In this change, it also integrats 2 ash arguments
    (--disable-mojo-broker, --user-data-dir) so we don't need to
    pass them manually.
    
    Next step: convert all other tests to use mixin.
    
    Test: out/betty/bin/run_chromeos_integration_tests --board=betty --stop-ui --use-vm  --gtest_filter=DemoIntegrationTest*
    Bug: 1464750
    Change-Id: I6c8c7c60923f4fe3a2b4b28996c1cd7e7f15f437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708492
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173766}

--- chrome/test/base/chromeos/crosier/chromeos_test_suite.cc
+++ chrome/test/base/chromeos/crosier/chromeos_test_suite.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/test/ui_controls_ash.h"
+#include "base/command_line.h"
+#include "content/public/common/content_switches.h"
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "base/check.h"
 #include "base/files/file_util.h"
@@ -24,6 +26,8 @@ void ChromeOSTestSuite::Initialize() {
   content::ContentTestSuiteBase::Initialize();
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   ash::test::EnableUIControlsAsh();
+  base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
+  cmdline->AppendSwitch(switches::kDisableMojoBroker);
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
   // The lacros binary receives certain paths from ash very early in startup.
   // Simulate that behavior here. See chrome_paths_lacros.cc for details. The
--- chrome/test/base/chromeos/crosier/demo_integration_test.cc
+++ chrome/test/base/chromeos/crosier/demo_integration_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_switches.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/test/base/chromeos/crosier/crosier_mixin.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "net/dns/mock_host_resolver.h"
@@ -62,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // 1. See http://go/crosint-run for instructions.
 // 2. You can optionally add --test-launcher-interactive to play with the
 //    browser after the test finishes.
-class DemoIntegrationTest : public InProcessBrowserTest {
+class DemoIntegrationTest : public MixinBasedInProcessBrowserTest {
  public:
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   void SetUpCommandLine(base::CommandLine* cmd_line) override {
@@ -100,6 +101,9 @@ class DemoIntegrationTest : public InProcessBrowserTest {
     InProcessBrowserTest::TearDownOnMainThread();
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+ private:
+  CrosierMixin crosier_mixin_{&mixin_host_};
 };
 
 IN_PROC_BROWSER_TEST_F(DemoIntegrationTest, NewTab) {
