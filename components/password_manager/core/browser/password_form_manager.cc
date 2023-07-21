b26830ba8e384a4217a100cb121f612af4119f49
Maria Kazinova <kazinova@google.com>
2023-07-21 15:34:17 +0000

    [Passwords, UFF] Stop using local predictions.
    Locally stored UFF predictions have little use and can trouble manual
    overriding. This CL stops adding and using them.
    The follow-up CLs will drop the already stored data and delete and
    adjust the classes involved to the ongoing work.
    
    Bug: 1466470
    Change-Id: I938977c735ec9591d366e5573d2d078fc6281702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705348
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173529}

--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/password_generation_util.h"
 #include "components/password_manager/core/browser/affiliation/affiliation_utils.h"
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
-#include "components/password_manager/core/browser/field_info_manager.h"
 #include "components/password_manager/core/browser/form_fetcher_impl.h"
 #include "components/password_manager/core/browser/password_change_success_tracker_impl.h"
 #include "components/password_manager/core/browser/password_feature_manager.h"
