482ede02060570b45468753e9c7d57ad87fe521d
Jiawei Chen <jiawei.chen@dolby.com>
2023-07-21 17:54:35 +0000

    Video: Enable HEVC bitstream converter for Media Foundation for clear
    By default, bitstream converter(to Annex-B) is not enabled for HEVC when
    use media foundation for clear, but it's reqiurenment of HEVC deocder in
    MediaFoundation.
    Refer to detail in this page:
    https://learn.microsoft.com/en-us/windows/win32/medfound/h-265---hevc-video-decoder
    
    This change only affect ffmpeg_demuxer used case.
    
    Bug: 1465217
    Change-Id: Ie8f44aea08cd2fe7d2e0d98e84f177e7abc375bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4688957
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: 朱思达 <zhusida@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1173613}

--- media/renderers/win/media_foundation_renderer_integration_test.cc
+++ media/renderers/win/media_foundation_renderer_integration_test.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/win/windows_version.h"
 #include "media/base/media_util.h"
+#include "media/base/supported_types.h"
 #include "media/test/pipeline_integration_test_base.h"
 #include "media/test/test_media_source.h"
 
@@ -18,10 +19,20 @@ namespace media {
 
 namespace {
 
+using VideoCodecMap = base::flat_map<VideoCodec, GUID>;
+
+const VideoCodecMap& GetVideoCodecsMap() {
+  static const base::NoDestructor<VideoCodecMap> AllVideoCodecsMap(
+      {{VideoCodec::kVP9, MFVideoFormat_VP90},
+       {VideoCodec::kHEVC, MFVideoFormat_HEVC}});
+  return *AllVideoCodecsMap;
+}
+
 // TODO(xhwang): Generalize this to support more codecs, or use CanPlay() or
 // IsTypeSupported() which can take mime types directly.
-bool CanDecodeVp9() {
-  MFT_REGISTER_TYPE_INFO input_type = {MFMediaType_Video, MFVideoFormat_VP90};
+bool CanDecodeVideoCodec(VideoCodec codec) {
+  auto codecs = GetVideoCodecsMap();
+  MFT_REGISTER_TYPE_INFO input_type = {MFMediaType_Video, codecs[codec]};
   IMFActivate** activates = nullptr;
   UINT32 count = 0;
 
@@ -39,7 +50,7 @@ bool CanDecodeVp9() {
   CoTaskMemFree(activates);
 
   if (count == 0) {
-    LOG(WARNING) << "No decoder for VP9";
+    LOG(WARNING) << "No decoder for " << media::GetCodecName(codec);
     return false;
   }
 
@@ -81,8 +92,9 @@ TEST_F(MediaFoundationRendererIntegrationTest, BasicPlayback) {
       base::win::Version::WIN10_20H2) {
     GTEST_SKIP() << "Skipping test for WIN10_20H2 and greater";
   }
-  if (!CanDecodeVp9())
+  if (!CanDecodeVideoCodec(VideoCodec::kVP9)) {
     return;
+  }
 
   ASSERT_EQ(PIPELINE_OK, Start("bear-vp9.webm"));
   Play();
@@ -95,8 +107,9 @@ TEST_F(MediaFoundationRendererIntegrationTest, BasicPlayback_MediaSource) {
       base::win::Version::WIN10_20H2) {
     GTEST_SKIP() << "Skipping test for WIN10_20H2 and greater";
   }
-  if (!CanDecodeVp9())
+  if (!CanDecodeVideoCodec(VideoCodec::kVP9)) {
     return;
+  }
 
   TestMediaSource source("bear-vp9.webm", 67504);
   EXPECT_EQ(PIPELINE_OK, StartPipelineWithMediaSource(&source));
@@ -105,6 +118,23 @@ TEST_F(MediaFoundationRendererIntegrationTest, BasicPlayback_MediaSource) {
   Play();
   ASSERT_TRUE(WaitUntilOnEnded());
   source.Shutdown();
+  Stop();
+}
+
+TEST_F(MediaFoundationRendererIntegrationTest,
+       HEVCPlayback_with_FFMpegDemuxer) {
+  if (!CanDecodeVideoCodec(VideoCodec::kHEVC)) {
+    return;
+  }
+
+  // FFMpegDemuxer will verify if video codec is supported internally, add HEVC
+  // profile here to let demuxer initialization successfully.
+  media::UpdateDefaultSupportedVideoProfiles(
+      {media::VideoCodecProfile::HEVCPROFILE_MAIN});
+
+  ASSERT_EQ(PIPELINE_OK, Start("bear-3840x2160-hevc.mp4", kUnreliableDuration));
+  Play();
+  ASSERT_TRUE(WaitUntilOnEnded());
   Stop();
 }
 
