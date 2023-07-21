464ebb9e5841d368393be5c21699b5283d73670d
Hiroki Nakagawa <nhiroki@chromium.org>
2023-07-21 08:39:39 +0000

    Prerender: Move PrerenderWebContentsDelegate from content/ to chrome/
    This CL moves PrerenderWebContentsDelegate from content/ to chrome/ so
    that subsequent CLs can implement chrome/ specific things there like
    TabHelpers support.
    
    Bug: 1350676
    Change-Id: I762fe775c5a0b628aff05607e76f26c477f2b106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680866
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173376}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -115,6 +115,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.h"
+#include "chrome/browser/preloading/prerender/prerender_web_contents_delegate.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/private_network_access/chrome_private_network_device_delegate.h"
 #include "chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.h"
@@ -7617,6 +7618,11 @@ std::unique_ptr<content::PrefetchServiceDelegate>
 ChromeContentBrowserClient::CreatePrefetchServiceDelegate(
     content::BrowserContext* browser_context) {
   return std::make_unique<ChromePrefetchServiceDelegate>(browser_context);
+}
+
+std::unique_ptr<content::PrerenderWebContentsDelegate>
+ChromeContentBrowserClient::CreatePrerenderWebContentsDelegate() {
+  return std::make_unique<PrerenderWebContentsDelegateImpl>();
 }
 
 void ChromeContentBrowserClient::OnWebContentsCreated(
---
+++ content/public/browser/prerender_web_contents_delegate.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_BROWSER_PRERENDER_WEB_CONTENTS_DELEGATE_H_
+#define CONTENT_PUBLIC_BROWSER_PRERENDER_WEB_CONTENTS_DELEGATE_H_
+
+#include "content/public/browser/web_contents_delegate.h"
+
+namespace content {
+
+// This is used as the delegate of WebContents created for a new tab where
+// prerendering runs. The delegate will be swapped with a proper one on
+// prerender page activation.
+//
+// This class is currently empty but necessary to bypass the "protected"
+// destructor of WebContentsDelegate. Without this indirect class, an owner of
+// WebContentsDelegate implementation in content/ cannot call the destructor.
+// See review comments on https://crrev.com/c/4680866/ for details.
+class PrerenderWebContentsDelegate : public WebContentsDelegate {};
+
+}  // namespace content
+
+#endif  // CONTENT_PUBLIC_BROWSER_PRERENDER_WEB_CONTENTS_DELEGATE_H_
