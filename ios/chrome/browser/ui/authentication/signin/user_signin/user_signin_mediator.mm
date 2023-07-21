d18724f431238e3f52b7d8fd0248d9c5692c67a4
Tanmoy Mollik <triploblastic@chromium.org>
2023-07-21 14:57:09 +0000

    [iOS] Add a TODO to deprecate ConsentLevel::kSync in authentication code
    Bug: 1462858
    Change-Id: Ib13b3662fc81f035a70531054ecb36116ab2689d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4693722
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173505}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
@@ -160,8 +160,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
       break;
     case IdentitySigninStateSignedInWithSyncDisabled:
-      DCHECK(!self.authenticationService->GetPrimaryIdentity(
-          signin::ConsentLevel::kSync));
       if ([self.authenticationService->GetPrimaryIdentity(
               signin::ConsentLevel::kSignin)
               isEqual:self.delegate.signinIdentityOnStart]) {
