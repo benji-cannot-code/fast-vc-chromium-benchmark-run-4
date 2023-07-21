e07d56d20cd5666a3516d12aa391723c84c4b0f8
Hirokazu Honda <hiroh@chromium.org>
2023-07-21 03:25:02 +0000

    mediarecorder: Add correct AV1 codecs string "av01"
    MediaRecorder uses the wrong AV1 codecs string, "av1." The correct
    one is "av01" [1] This CL adds the correct one. We don't remove
    the wrong codecs string in this CL for backward-compatibility.
    Instead we will remove it once we confirm nobody uses it in product through the UMA, Media.MediaRecorder.AV1CodecsStr.
    
    [1] https://aomediacodec.github.io/av1-isobmff/#codecsparam
    
    Bug: 1463953, 1465734
    Test: unit test
    
    Change-Id: Id41866b58156cf002df1f86d29846715d6011610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680672
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173312}

--- third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
+++ third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/system/sys_info.h"
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
@@ -143,7 +144,10 @@ VideoTrackRecorder::CodecProfile VideoStringToCodecProfile(
       return {codec_id, profile, level};
   }
 #endif
-  if (codecs_str.Find("av1") != kNotFound) {
+  // TODO(crbug.com/1465734): Remove the wrong AV1 codecs string, "av1", once
+  // we confirm nobody uses this in product.
+  if (codecs_str.Find("av01") != kNotFound ||
+      codecs_str.Find("av1") != kNotFound) {
     codec_id = VideoTrackRecorder::CodecId::kAv1;
   }
   return VideoTrackRecorder::CodecProfile(codec_id);
@@ -200,6 +204,9 @@ bool MediaRecorderHandler::CanSupportMimeType(const String& type,
     "h264",
     "avc1",
 #endif
+    "av01",
+    // TODO(crbug.com/1465734): Remove the wrong AV1 codecs string, "av1", once
+    // we confirm nobody uses this in product.
     "av1",
     "opus",
     "pcm"
@@ -217,7 +224,16 @@ bool MediaRecorderHandler::CanSupportMimeType(const String& type,
     String codec_string = String::FromUTF8(codec);
     if (std::none_of(relevant_codecs_begin, relevant_codecs_end,
                      [&codec_string](const char* name) {
-                       return EqualIgnoringASCIICase(codec_string, name);
+                       if (!EqualIgnoringASCIICase(codec_string, name)) {
+                         return false;
+                       }
+                       std::string_view name_str(name);
+                       if (name_str == "av01" || name_str == "av1") {
+                         base::UmaHistogramBoolean(
+                             "Media.MediaRecorder.HasCorrectAV1CodecString",
+                             name_str == "av01");
+                       }
+                       return true;
                      })) {
       return false;
     }
@@ -534,7 +550,7 @@ String MediaRecorderHandler::ActualMimeType() {
         break;
 #endif
       case VideoTrackRecorder::CodecId::kAv1:
-        mime_type.Append("av1");
+        mime_type.Append("av01");
         break;
       case VideoTrackRecorder::CodecId::kLast:
         DCHECK_NE(audio_codec_id_, AudioTrackRecorder::CodecId::kLast);
