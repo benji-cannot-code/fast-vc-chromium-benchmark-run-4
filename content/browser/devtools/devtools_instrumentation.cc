5daff4f601f63317a9c4be756995d1a17235c0c9
Shuran Huang <shuuran@chromium.org>
2023-07-21 15:18:50 +0000

    Reflect cookies with WARN_THIRD_PARTY_PHASEOUT reason to DevTools issues
    Bug: 1462693
    Change-Id: If7e37adaf936e0028d6f25ce9d391203e5b9538d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698374
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173521}

--- content/browser/devtools/devtools_instrumentation.cc
+++ content/browser/devtools/devtools_instrumentation.cc
@@ -1561,6 +1561,12 @@ std::unique_ptr<protocol::Array<protocol::String>> BuildWarningReasons(
         protocol::Audits::CookieWarningReasonEnum::WarnDomainNonASCII);
   }
 
+  if (status.HasWarningReason(
+          net::CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT)) {
+    warning_reasons->push_back(
+        protocol::Audits::CookieWarningReasonEnum::WarnThirdPartyPhaseout);
+  }
+
   return warning_reasons;
 }
 
