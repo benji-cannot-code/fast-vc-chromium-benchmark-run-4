e82c9f6b18c576df1596b6ebedc64605969a2e1c
dpapad <dpapad@chromium.org>
2023-07-21 19:14:28 +0000

    WebUI: Include test file name in WebUIMochaBrowserTest summary error.
    This makes it easier to differentiate between test failures, especially
    when filing bugs during gardening, or via scripts.
    
    Previously it would say:
    Tests are failing: web_ui_mocha_browser_test.cc(123): Failed\nMocha test failures detected.
    
    After it will say something as follows:
    Tests are failing: web_ui_mocha_browser_test.cc(123): Failed\nMocha test failures detected in file: cr_elements/cr_button_test.js, triggered by 'mocha.run()'
    
    Bug: 1457360, 1465135
    Change-Id: If5bb258455e06bb5f526bf49cd9996c2b461042d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706514
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173642}

--- chrome/test/base/web_ui_mocha_browser_test.cc
+++ chrome/test/base/web_ui_mocha_browser_test.cc
@@ -121,7 +121,8 @@ void WebUIMochaBrowserTest::RunTest(const std::string& file,
   }
 
   if (!success) {
-    FAIL() << "Mocha test failures detected.";
+    FAIL() << "Mocha test failures detected in file: " << file
+           << ", triggered by '" << trigger << "'";
   }
 }
 
