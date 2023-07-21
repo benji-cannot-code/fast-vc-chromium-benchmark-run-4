f4611e14d812b67c61d559b411fe831001ffef5c
Bruce Dawson <brucedawson@chromium.org>
2023-07-21 02:38:59 +0000

    Fix duplicate test names in unit_tests
    Duplicate test names can cause confusion and test failures. This fixes
    several duplicate test names in unit_tests. It had six tests that were
    duplicates, all from TelemetryEventServiceConvertersTest.*, copied from
    c/b/ash/telemetry_extension/events/telemetry_event_service_converters_unittest.cc.
    
    The duplicate test names appeared with crrev.com/c/4529786.
    
    Bug: 1465008
    Change-Id: I53e16d1752a6ec5e11a1b128b26fb5dae3c93007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706325
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173297}

--- chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters_unittest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters_unittest.cc
@@ -49,7 +49,7 @@ TEST(TelemetryExtensionEventsApiConvertersUnitTest,
       cx_events::AudioJackDeviceType::kMicrophone);
 }
 
-TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardConnectionType) {
+TEST(CrosTelemetryEventServiceConvertersTest, ConvertKeyboardConnectionType) {
   EXPECT_EQ(
       Convert(crosapi::TelemetryKeyboardConnectionType::kUnmappedEnumField),
       cx_events::KeyboardConnectionType::kNone);
@@ -67,7 +67,7 @@ TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardConnectionType) {
             cx_events::KeyboardConnectionType::kUnknown);
 }
 
-TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardPhysicalLayout) {
+TEST(CrosTelemetryEventServiceConvertersTest, ConvertKeyboardPhysicalLayout) {
   EXPECT_EQ(
       Convert(crosapi::TelemetryKeyboardPhysicalLayout::kUnmappedEnumField),
       cx_events::PhysicalKeyboardLayout::kNone);
@@ -79,7 +79,7 @@ TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardPhysicalLayout) {
             cx_events::PhysicalKeyboardLayout::kChromeOs);
 }
 
-TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardMechanicalLayout) {
+TEST(CrosTelemetryEventServiceConvertersTest, ConvertKeyboardMechanicalLayout) {
   EXPECT_EQ(
       Convert(crosapi::TelemetryKeyboardMechanicalLayout::kUnmappedEnumField),
       cx_events::MechanicalKeyboardLayout::kNone);
@@ -97,7 +97,8 @@ TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardMechanicalLayout) {
             cx_events::MechanicalKeyboardLayout::kJis);
 }
 
-TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardNumberPadPresence) {
+TEST(CrosTelemetryEventServiceConvertersTest,
+     ConvertKeyboardNumberPadPresence) {
   EXPECT_EQ(
       Convert(crosapi::TelemetryKeyboardNumberPadPresence::kUnmappedEnumField),
       cx_events::KeyboardNumberPadPresence::kNone);
@@ -112,7 +113,7 @@ TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardNumberPadPresence) {
             cx_events::KeyboardNumberPadPresence::kNotPresent);
 }
 
-TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardTopRowKey) {
+TEST(CrosTelemetryEventServiceConvertersTest, ConvertKeyboardTopRowKey) {
   EXPECT_EQ(Convert(crosapi::TelemetryKeyboardTopRowKey::kUnmappedEnumField),
             cx_events::KeyboardTopRowKey::kNone);
 
@@ -188,7 +189,7 @@ TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardTopRowKey) {
             cx_events::KeyboardTopRowKey::kDelete);
 }
 
-TEST(TelemetryEventServiceConvertersTest, ConvertKeyboardTopRightKey) {
+TEST(CrosTelemetryEventServiceConvertersTest, ConvertKeyboardTopRightKey) {
   EXPECT_EQ(Convert(crosapi::TelemetryKeyboardTopRightKey::kUnmappedEnumField),
             cx_events::KeyboardTopRightKey::kNone);
 
