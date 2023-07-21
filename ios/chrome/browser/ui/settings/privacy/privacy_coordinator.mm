13120fe50e986e1b9d45721def8c646116d16de3
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-21 19:49:52 +0000

    [iOS] lockdownModeCoordinator is stopped
    This is required in case Chrome is being stopped while the lockdown mode
    coordinator is opened
    
    Bug: 1454777
    Change-Id: I884918469c167cdc5f4b96a4187cd5c64d7c6d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705334
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173658}

--- ios/chrome/browser/ui/settings/privacy/privacy_coordinator.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_coordinator.mm
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)stop {
   [self.clearBrowsingDataCoordinator stop];
   self.clearBrowsingDataCoordinator = nil;
+  [self stopLockdownModeCoordinator];
 
   self.viewController = nil;
 }
@@ -159,6 +160,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)lockdownModeCoordinatorDidRemove:(LockdownModeCoordinator*)coordinator {
   DCHECK_EQ(self.lockdownModeCoordinator, coordinator);
+  [self stopLockdownModeCoordinator];
+}
+
+#pragma mark - Private
+
+- (void)stopLockdownModeCoordinator {
   [self.lockdownModeCoordinator stop];
   self.lockdownModeCoordinator.delegate = nil;
   self.lockdownModeCoordinator = nil;
