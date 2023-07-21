f1a4d2182b225797e3012d5d781da352e100d7a5
Sunggook Chue <sunggch@microsoft.com>
2023-07-21 22:02:11 +0000

    MP4 Muxer: Implement OnEncodedVideo for muxer.
    Muxer creates MP4 boxes with the information on OnEncodedVideo
    API call. It creates Movie and its children, fragment.
    
    1. It caculcate the duration with the first and last call of
    OnEncodedVideo, and uses timescale for duration TimeDelta.
    2. It calculate sample duration by using the current sample
    minus the prior sample.
    3. Every fragment ('moof') box will be created with key frame.
    (In Windows, 100 samples are in single fragment).
    4. It uses input code_descriptions for 'avcC' box.
    5. For test, it uses MP4StreamParser to parse the created data,
    which will validate it. It also uses BoxReader to validate
    respective box validation.
    
    Bug: 1072056
    Change-Id: Iaa4e45ba793b63bc32610c5decb746f30e694b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4674645
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1173736}

--- media/muxers/mp4_muxer_delegate.h
+++ media/muxers/mp4_muxer_delegate.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_MUXERS_MP4_MUXER_DELEGATE_H_
 #define MEDIA_MUXERS_MP4_MUXER_DELEGATE_H_
 
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "base/sequence_checker.h"
 #include "base/strings/string_piece.h"
 #include "base/thread_annotations.h"
@@ -14,13 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_encoder.h"
 #include "media/formats/mp4/box_definitions.h"
 #include "media/formats/mp4/writable_box_definitions.h"
+#include "media/muxers/mp4_muxer_context.h"
 #include "media/muxers/muxer.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace media {
 
 class AudioParameters;
-class Mp4MuxerNaluReader;
 
 // Mp4MuxerDelegate builds the MP4 boxes from the encoded stream.
 // The boxes fields will start to be populated from the first stream and
@@ -37,12 +41,14 @@ class MEDIA_EXPORT Mp4MuxerDelegate {
       const Muxer::VideoParameters& params,
       base::StringPiece encoded_data,
       absl::optional<VideoEncoder::CodecDescription> codec_description,
-      base::TimeTicks timestamp);
+      base::TimeTicks timestamp,
+      bool is_key_frame);
 
-  void AddAudioFrame(const AudioParameters& params,
-                     base::StringPiece encoded_data,
-                     const AudioEncoder::CodecDescription& codec_description,
-                     base::TimeTicks timestamp);
+  void AddAudioFrame(
+      const AudioParameters& params,
+      base::StringPiece encoded_data,
+      absl::optional<AudioEncoder::CodecDescription> codec_description,
+      base::TimeTicks timestamp);
   // Write to the big endian ISO-BMFF boxes and call `write_callback`.
   void Flush();
 
@@ -57,26 +63,28 @@ class MEDIA_EXPORT Mp4MuxerDelegate {
     mp4::writable_boxes::MediaData mdat;
   };
 
-  void PopulateMovieHeader();
-  void PopulateInitialVideoTrack(const Muxer::VideoParameters& params,
-                                 base::StringPiece encoded_data,
-                                 int index);
-  void PopulateVideoFragment(const Muxer::VideoParameters& params,
-                             base::StringPiece encoded_data,
-                             base::TimeTicks timestamp);
-  void AddSampleDataToTrunAndMdat(mp4::writable_boxes::TrackFragmentRun& trun,
-                                  Mp4MuxerDelegate::Fragment* fragment,
-                                  Mp4MuxerNaluReader& nalu_reader,
-                                  base::StringPiece encoded_data,
-                                  base::TimeTicks timestamp,
-                                  uint32_t timescale);
-  void AddSampleDuration(mp4::writable_boxes::TrackFragmentRun& trun,
-                         base::TimeTicks timestamp,
-                         uint32_t timescale);
+  void BuildMovieBox();
+  void BuildVideoTrackWithKeyframe(
+      const Muxer::VideoParameters& params,
+      base::StringPiece encoded_data,
+      VideoEncoder::CodecDescription codec_description);
+  void BuildVideoFragment(const Muxer::VideoParameters& params,
+                          base::StringPiece encoded_data,
+                          base::TimeTicks timestamp,
+                          bool is_key_frame);
+  void AddNewVideoFragment(Mp4MuxerDelegate::Fragment& fragment);
+  void AddDataToMdat(Mp4MuxerDelegate::Fragment& fragment,
+                     base::StringPiece encoded_data);
+  void AddLastVideoSampleTimestamp();
   int GetNextTrackIndex();
+  void EnsureInitialized();
+  void Reset();
+
+  std::unique_ptr<Mp4MuxerContext> context_;
+  Muxer::WriteDataCB write_callback_;
 
   // The MP4 has single movie box and multiple fragment boxes.
-  mp4::writable_boxes::Movie movie_box_;
+  std::unique_ptr<mp4::writable_boxes::Movie> moov_;
 
   // Only key video frame has `SPS` and `PPS` and it will be a
   // signal of new fragment. In Windows, key frame is every 100th frame.
@@ -84,10 +92,14 @@ class MEDIA_EXPORT Mp4MuxerDelegate {
 
   // video and audio index is a 0 based index that is an item of the container.
   // The track id would be plus one on this index value.
-  int video_track_index_ = -1;
+  absl::optional<int> video_track_index_;
   int next_track_index_ = 0;
-  std::vector<base::TimeTicks> video_captured_time_;
 
+  // Duration time delta for the video track.
+  base::TimeTicks start_video_time_;
+  base::TimeTicks last_video_time_;
+
+  double video_frame_rate_;
   Muxer::WriteDataCB write_data_callback_ GUARDED_BY_CONTEXT(sequence_checker_);
 
   SEQUENCE_CHECKER(sequence_checker_);
