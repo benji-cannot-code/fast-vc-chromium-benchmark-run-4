c95b303a7193e95287bd4dfedbcd04309e07d217
Jérôme Lebel <jlebel@chromium.org>
2023-07-21 09:45:11 +0000

    [iOS] Fix AuthenticationFlowPerformer when being interrupted
    The interrupt completion block needs to be always called when
    the performer is interrupted, even if there is no dialog to dismiss.
    
    Bug: 1455216
    Change-Id: Iabbb5ceecbdd8d4efccae71ddc91b79781cbb567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699081
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173403}

--- ios/chrome/browser/ui/authentication/authentication_flow_performer.mm
+++ ios/chrome/browser/ui/authentication/authentication_flow_performer.mm
@@ -122,6 +122,8 @@ NSString* const kAuthenticationSnackbarCategory =
         }
         break;
     }
+  } else if (completion) {
+    completion();
   }
   _delegate = nil;
   [self stopWatchdogTimer];
