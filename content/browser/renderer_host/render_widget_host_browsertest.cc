c410324fc0893e44d6daa7f665a7d54371891175
Ian Vollick <vollick@chromium.org>
2023-07-21 18:00:02 +0000

    [ios] Disable popup interceptor browsertests
    This aligns with Android (for which these tests are disabled).
    Test was introduced in crrev.com/c/4705070.
    
    Bug: 1441228
    Change-Id: If9b5e9781ea5e73af9c9223ed2dcb1559b97bbb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705070
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Auto-Submit: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173616}

--- content/browser/renderer_host/render_widget_host_browsertest.cc
+++ content/browser/renderer_host/render_widget_host_browsertest.cc
@@ -655,7 +655,7 @@ IN_PROC_BROWSER_TEST_F(RenderWidgetHostSitePerProcessTest,
 
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
 namespace {
 
@@ -827,7 +827,7 @@ IN_PROC_BROWSER_TEST_F(RenderWidgetHostSitePerProcessTest,
 #endif  // BUILDFLAG(IS_MAC)
 }
 
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
 // Tests that `window.screen` dimensions match the display, not the viewport,
 // while the frame is fullscreen. See crbug.com/1367416
