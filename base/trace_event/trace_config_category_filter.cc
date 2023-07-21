62fe17e66e163a910be00f6f0091f2e9d97e0559
Mikhail Khokhlov <khokhlov@google.com>
2023-07-21 11:22:11 +0000

    [Tracing w/SDK] Fix startup session adoption for Chrome datasources
    Tracing service fails to adopt a startup tracing session when its
    config doesn't match the service config exactly. This can prevent
    Chrome datasources from starting in child processes.
    
    This CL provides a custom config comparator for Chrome datasources
    that are registered via PerfettoTracedProcess::DataSourceProxy to correctly adopt sessions in this case.
    
    Bug: 1459658
    Change-Id: Ifd6580e17c30105a5e0f3a73d0cf8b52e3930362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672690
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1173422}

--- base/trace_event/trace_config_category_filter.cc
+++ base/trace_event/trace_config_category_filter.cc
@@ -17,8 +17,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base::trace_event {
 
 namespace {
+
 const char kIncludedCategoriesParam[] = "included_categories";
 const char kExcludedCategoriesParam[] = "excluded_categories";
+
+bool AreStringListsEquivalent(const std::vector<std::string>& list1,
+                              const std::vector<std::string>& list2) {
+  std::unordered_set<std::string> set1(list1.begin(), list1.end());
+  std::unordered_set<std::string> set2(list2.begin(), list2.end());
+  return set1 == set2;
+}
 }
 
 TraceConfigCategoryFilter::TraceConfigCategoryFilter() = default;
@@ -30,6 +38,16 @@ TraceConfigCategoryFilter::~TraceConfigCategoryFilter() = default;
 
 TraceConfigCategoryFilter& TraceConfigCategoryFilter::operator=(
     const TraceConfigCategoryFilter& rhs) = default;
+
+bool TraceConfigCategoryFilter::IsEquivalentTo(
+    const TraceConfigCategoryFilter& other) const {
+  return AreStringListsEquivalent(included_categories_,
+                                  other.included_categories_) &&
+         AreStringListsEquivalent(disabled_categories_,
+                                  other.disabled_categories_) &&
+         AreStringListsEquivalent(excluded_categories_,
+                                  other.excluded_categories_);
+}
 
 void TraceConfigCategoryFilter::InitializeFromString(
     const StringPiece& category_filter_string) {
