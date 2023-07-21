783bfa8ec9276d53ee3d05903106f43d6baf2ae4
Andrey Kosyakov <caseq@chromium.org>
2023-07-21 04:49:55 +0000

    Convert blink/renderer/core to use optional<>-compatible Maybe<> accessors
    Bug: 1465981
    Change-Id: Ie08e837a299cb15fb2e70b33148c4d3bf31121a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706507
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173326}

--- third_party/blink/renderer/core/inspector/inspector_performance_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_performance_agent.cc
@@ -83,7 +83,7 @@ void InspectorPerformanceAgent::InnerEnable() {
 
 protocol::Response InspectorPerformanceAgent::enable(
     Maybe<String> optional_time_domain) {
-  String time_domain = optional_time_domain.fromMaybe(TimeDomain::TimeTicks);
+  String time_domain = optional_time_domain.value_or(TimeDomain::TimeTicks);
   if (enabled_.Get()) {
     if (!HasTimeDomain(time_domain)) {
       return protocol::Response::ServerError(
