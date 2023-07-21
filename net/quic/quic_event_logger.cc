2be5d161b31adef0aa15986c82c22590d8584d76
Kenichi Ishibashi <bashi@chromium.org>
2023-07-21 07:27:45 +0000

    [netlog] Add ietf_error_code to QUIC RESET_STREAM and STOP_SENDING
    The field is used to bring application layer error code such as
    WEBTRANSPORT_APPLICATION_ERROR [1]. The field is useful when we debug
    application layer behavior.
    
    [1] https://datatracker.ietf.org/doc/html/draft-ietf-webtrans-http3/#section-8.5-4.2.1
    
    Bug: N/A
    Change-Id: I0b1e78db3d931d9b546d634e23dbdf27a0dec383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706904
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173354}

--- net/quic/quic_event_logger.cc
+++ net/quic/quic_event_logger.cc
@@ -169,6 +169,7 @@ base::Value::Dict NetLogQuicRstStreamFrameParams(
   base::Value::Dict dict;
   dict.Set("stream_id", static_cast<int>(frame->stream_id));
   dict.Set("quic_rst_stream_error", static_cast<int>(frame->error_code));
+  dict.Set("ietf_error_code", static_cast<int>(frame->ietf_error_code));
   dict.Set("offset", NetLogNumberValue(frame->byte_offset));
   return dict;
 }
@@ -322,6 +323,7 @@ base::Value::Dict NetLogQuicStopSendingFrameParams(
   base::Value::Dict dict;
   dict.Set("stream_id", static_cast<int>(frame.stream_id));
   dict.Set("quic_rst_stream_error", static_cast<int>(frame.error_code));
+  dict.Set("ietf_error_code", static_cast<int>(frame.ietf_error_code));
   return dict;
 }
 
