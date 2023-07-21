a38fe07d8788e42c48d211a6d23df36507cabdfd
Sven Zheng <svenzheng@chromium.org>
2023-07-21 19:33:56 +0000

    Rename crosint to crosier
    Project name change.
    
    Bug: 1464750
    Include-Ci-Only-Tests: true
    Change-Id: Ifaefbcc0256a7715138b712b5cb84d9e0e606127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707002
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173651}

--- chrome/test/base/chromeos/crosier/chromeos_test_launcher.cc
+++ chrome/test/base/chromeos/crosier/chromeos_test_launcher.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/test/base/chromeos/crosint/chromeos_test_launcher.h"
+#include "chrome/test/base/chromeos/crosier/chromeos_test_launcher.h"
 
 #include "base/test/allow_check_is_test_for_testing.h"
 #include "base/test/task_environment.h"
 #include "chrome/app/chrome_crash_reporter_client.h"
 #include "chrome/browser/chrome_content_browser_client.h"
-#include "chrome/test/base/chromeos/crosint/chromeos_test_suite.h"
+#include "chrome/test/base/chromeos/crosier/chromeos_test_suite.h"
 #include "chrome/utility/chrome_content_utility_client.h"
 #include "content/public/test/network_service_test_helper.h"
 #include "mojo/public/cpp/bindings/service_factory.h"
--- chrome/test/base/chromeos/crosier/chromeos_tests_main.cc
+++ chrome/test/base/chromeos/crosier/chromeos_tests_main.cc
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
-#include "chrome/test/base/chromeos/crosint/chromeos_test_launcher.h"
-#include "chrome/test/base/chromeos/crosint/chromeos_test_suite.h"
+#include "chrome/test/base/chromeos/crosier/chromeos_test_launcher.h"
+#include "chrome/test/base/chromeos/crosier/chromeos_test_suite.h"
 
 int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
