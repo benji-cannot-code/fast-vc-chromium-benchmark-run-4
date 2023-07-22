805793cb4aa749d6884d80653e9f1aae29ce66b1
Adam Rice <ricea@chromium.org>
2023-07-22 20:13:37 +0000

    Remove unused stack_container.h include in /media
    media/video/gpu_memory_buffer_video_frame_pool.cc was including
    stack_container.h despite not using it. Remove the #include
    statement.
    
    This CL was uploaded by git cl split.
    
    R=eugene@chromium.org
    
    Bug: 1465736
    Change-Id: I63d10822143a0cf28c8fb62657d3a5ffed384c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707116
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173886}

--- media/video/gpu_memory_buffer_video_frame_pool.cc
+++ media/video/gpu_memory_buffer_video_frame_pool.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bits.h"
 #include "base/command_line.h"
 #include "base/containers/circular_deque.h"
-#include "base/containers/stack_container.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
