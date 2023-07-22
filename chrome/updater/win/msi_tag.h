de78ddb57c99cc01723e7322ca46cf0762f00f77
Sorin Jianu <sorin@chromium.org>
2023-07-22 02:04:13 +0000

    updater: remove banner comment (coding style).
    Bug: 1465914
    Change-Id: Ieb312a230f41ee816ce1976507e184d7548dc9c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707279
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173798}

--- chrome/updater/win/msi_tag.h
+++ chrome/updater/win/msi_tag.h
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
-// +-----------------------------------------------------------------+
+//
 // Utilities for reading and writing tags to MSI files.
-// +-----------------------------------------------------------------+
+//
 //
 // The tag specification for MSI files is as follows:
 //   - The tag area begins with a magic signature 'Gact2.0Omaha'.
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // |    ..............................   |
 // |    Other parts of the MSI file      |
 // +-------------------------------------+
-// | Start of the certificate             |
+// | Start of the certificate            |
 // ~    ..............................   ~
 // ~    ..............................   ~
 // | Magic signature 'Gact2.0Omaha'      | Tag starts
