7bbb4215b0c0323aacf4c336dcc93ba024451abe
Adam Rice <ricea@chromium.org>
2023-07-22 13:07:53 +0000

    Fix header order in http_cache_transaction.cc
    In //net/http/http_cache_transaction.cc, the base/feature_list.h header
    was included in the wrong place and the
    base/task/single_thread_task_runner.h was included twice. Fix it.
    
    Change-Id: I411f681259a715809bb127ff2f5ac2e748220207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706749
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173849}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/http/http_cache_transaction.h"
 
-#include "base/feature_list.h"
-#include "base/task/single_thread_task_runner.h"
 #include "build/build_config.h"  // For IS_POSIX
 
 #if BUILDFLAG(IS_POSIX)
@@ -22,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/compiler_specific.h"
 #include "base/containers/fixed_flat_set.h"
+#include "base/feature_list.h"
 #include "base/format_macros.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
