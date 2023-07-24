ece9bf15cf838b9a8e4bcf3bb050973f6662972c
Yuta Hijikata <ythjkt@google.com>
2023-07-24 02:22:10 +0000

    Remove kLacrosMoveProfileMigrationFlag.
    The flag is no longer in use except in one unit test. Since there is
    only one type of profile migration which is move migration so the flag
    is no longer useful.
    
    Test: unit_tests
    Bug: 1416750
    Change-Id: I8d0be4a03d6448f6347fea0066ef5cd31f8ea11e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4686708
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174018}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3586,9 +3586,6 @@ extern const char kImeFstDecoderParamsUpdateDescription[];
 extern const char kImeTrayHideVoiceButtonName[];
 extern const char kImeTrayHideVoiceButtonDescription[];
 
-extern const char kLacrosMoveProfileMigrationName[];
-extern const char kLacrosMoveProfileMigrationDescription[];
-
 extern const char kLacrosProfileMigrationForceOffName[];
 extern const char kLacrosProfileMigrationForceOffDescription[];
 
