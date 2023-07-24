abda6b9de5ac781a6912153f4753b75045fa56ec
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-24 17:38:10 +0000

    [iOS] SafeBrowsingStandardProtectionCoordinator is stopped
    This is required in case Chrome is being stopped while the
    SafeBrowsingStandardProtectionCoordinator is opened.
    
    Bug: 1454777
    Change-Id: I8038bf955d6dc72fb07268e5c9a43ac76e064a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705221
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174290}

--- ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_coordinator.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_coordinator.mm
@@ -90,6 +90,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)stop {
   [self stopSafeBrowsingEnhancedProtectionCoordinator];
+  [self stopSafeBrowsingStandardProtectionCoordinator];
+  [super stop];
 }
 
 #pragma mark - SafeBrowsingViewControllerPresentationDelegate
@@ -175,6 +177,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)safeBrowsingStandardProtectionCoordinatorDidRemove:
     (SafeBrowsingStandardProtectionCoordinator*)coordinator {
   DCHECK_EQ(self.safeBrowsingStandardProtectionCoordinator, coordinator);
+  [self stopSafeBrowsingStandardProtectionCoordinator];
+}
+
+#pragma mark - Private
+
+- (void)stopSafeBrowsingStandardProtectionCoordinator {
   [self.safeBrowsingStandardProtectionCoordinator stop];
   self.safeBrowsingStandardProtectionCoordinator.delegate = nil;
   self.safeBrowsingStandardProtectionCoordinator = nil;
