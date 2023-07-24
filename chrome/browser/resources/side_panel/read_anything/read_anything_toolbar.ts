408a0a5e9218538248a7a792e5b4341c62949ee8
Kristi Saney <kristislee@google.com>
2023-07-24 17:23:43 +0000

    Add the basic dropdown for the existing reading mode settings.
    This is step 7 in go/read-mode-toolbar-refactor. https://screenshot.googleplex.com/69qDXZLypyriSm8
    
    mocks: https://www.figma.com/file/0Nf23Y0KMaZkoYBsuEWz5T/Maui-(reading-support)?type=design&node-id=4206-206760&mode=design&t=eqeyOxab6RmeKyjM-0
    Bug: 1465029
    Change-Id: I7a8f255a5ac356732b94551d7405c7c0f7f1616b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707067
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174276}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -6,11 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import '//resources/cr_elements/cr_icons.css.js';
 import '//resources/cr_elements/icons.html.js';
+import '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
+import './icons.html.js';
 
+import {AnchorAlignment, CrActionMenuElement, ShowAtPositionConfig} from '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import {WebUiListenerMixin} from '//resources/cr_elements/web_ui_listener_mixin.js';
+import {assert} from '//resources/js/assert_ts.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './read_anything_toolbar.html.js';
+
+export interface ReadAnythingToolbar {
+  $: {
+    menu: CrActionMenuElement,
+  };
+}
+
 
 const ReadAnythingToolbarBase = WebUiListenerMixin(PolymerElement);
 export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
@@ -24,6 +35,21 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
 
   static get properties() {
     return {};
+  }
+
+  private showAtPositionConfig_: ShowAtPositionConfig = {
+    top: 20,
+    left: 8,
+    anchorAlignmentY: AnchorAlignment.AFTER_END,
+  };
+
+
+  private onShowSettingsMenuClick_() {
+    const shadowRoot = this.shadowRoot;
+    assert(shadowRoot);
+    const button = shadowRoot.getElementById('settings');
+    assert(button);
+    this.$.menu.showAt(button, this.showAtPositionConfig_);
   }
 }
 
