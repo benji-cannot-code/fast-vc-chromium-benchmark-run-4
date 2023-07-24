0e5e0d2b75e1e9e93c7a92f6d50ea2307a6b9ce7
Kevin Radtke <kradtke@chromium.org>
2023-07-24 19:48:47 +0000

    Add Speak on mute opt-in nudge image
    Add a lottie image for the speak on mute opt-in nudge.
    
    screenshots
    dark mode: https://screenshot.googleplex.com/yqLQ3eva7QRzcUZ
    light mode: https://screenshot.googleplex.com/4QvCMCFfRL7wkCt
    
    Bug: b:286468502
    Change-Id: I2d11beb75fec650a9b5de77afdf2128992af82fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708945
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174382}

--- ash/system/video_conference/video_conference_tray_controller.cc
+++ ash/system/video_conference/video_conference_tray_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/constants/notifier_catalogs.h"
+#include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/system/anchored_nudge_data.h"
 #include "ash/public/cpp/system/anchored_nudge_manager.h"
@@ -235,6 +236,10 @@ void VideoConferenceTrayController::MaybeShowSpeakOnMuteOptInNudge(
       l10n_util::GetStringUTF16(
           IDS_ASH_VIDEO_CONFERENCE_NUDGE_SPEAK_ON_MUTE_OPT_IN_BODY),
       anchor_view);
+
+  nudge_data.image_model =
+      ui::ResourceBundle::GetSharedInstance().GetThemedLottieImageNamed(
+          IDR_SPEAK_ON_MUTE_OPT_IN_NUDGE_IMAGE);
 
   nudge_data.title_text = l10n_util::GetStringUTF16(
       IDS_ASH_VIDEO_CONFERENCE_NUDGE_SPEAK_ON_MUTE_OPT_IN_TITLE);
