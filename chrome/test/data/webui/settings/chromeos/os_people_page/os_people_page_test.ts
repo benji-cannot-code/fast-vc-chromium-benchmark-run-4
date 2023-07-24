b2558aa3beb1dab5e55f8f7177273c2366bf2a88
Wes Okuhara <wesokuhara@google.com>
2023-07-24 22:18:57 +0000

    CrOS Settings: Apply RouteOriginMixin to people page
    Uses RouteOriginMixin instead of os-settings-animated-pages to focus
    subpage trigger elements. Handled in preparation for upcoming
    category updates.
    
    Bug: b/291151247
    Test: browser_tests --gtest_filter="OSSettings*"
    Test: Manually test subpage trigger elements are focused
    Change-Id: Ic5867c46ddb8128bfc1d7cb0faaf8329db28e252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4704934
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174495}

--- chrome/test/data/webui/settings/chromeos/os_people_page/os_people_page_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_people_page/os_people_page_test.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://os-settings/os_settings.js';
 
 import {AccountManagerBrowserProxy, AccountManagerBrowserProxyImpl} from 'chrome://os-settings/lazy_load.js';
-import {createPageAvailabilityForTesting, OsSettingsPeoplePageElement, PageStatus, ProfileInfoBrowserProxy, ProfileInfoBrowserProxyImpl, Router, routes, settingMojom, SyncBrowserProxy, SyncBrowserProxyImpl} from 'chrome://os-settings/os_settings.js';
+import {createPageAvailabilityForTesting, OsSettingsPeoplePageElement, OsSettingsRoutes, PageStatus, ProfileInfoBrowserProxy, ProfileInfoBrowserProxyImpl, Router, routes, settingMojom, SyncBrowserProxy, SyncBrowserProxyImpl} from 'chrome://os-settings/os_settings.js';
 import {CrIconButtonElement} from 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import {CrRadioGroupElement} from 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
@@ -16,11 +16,17 @@ import {getDeepActiveElement} from 'chrome://resources/js/util_ts.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertEquals, assertFalse, assertStringContains, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
+import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
 import {TestSyncBrowserProxy} from '../test_os_sync_browser_proxy.js';
 import {TestProfileInfoBrowserProxy} from '../test_profile_info_browser_proxy.js';
 
 import {TestAccountManagerBrowserProxy} from './test_account_manager_browser_proxy.js';
+
+interface SubpageTriggerData {
+  triggerSelector: string;
+  routeName: keyof OsSettingsRoutes;
+}
 
 suite('<os-settings-people-page>', () => {
   let peoplePage: OsSettingsPeoplePageElement;
@@ -28,6 +34,13 @@ suite('<os-settings-people-page>', () => {
   let syncBrowserProxy: SyncBrowserProxy&TestSyncBrowserProxy;
   let accountManagerBrowserProxy: AccountManagerBrowserProxy&
       TestAccountManagerBrowserProxy;
+
+  function createPage(): void {
+    peoplePage = document.createElement('os-settings-people-page');
+    peoplePage.pageAvailability = createPageAvailabilityForTesting();
+    document.body.appendChild(peoplePage);
+    flush();
+  }
 
   setup(() => {
     browserProxy = new TestProfileInfoBrowserProxy();
@@ -39,6 +52,8 @@ suite('<os-settings-people-page>', () => {
     accountManagerBrowserProxy = new TestAccountManagerBrowserProxy();
     AccountManagerBrowserProxyImpl.setInstanceForTesting(
         accountManagerBrowserProxy);
+
+    Router.getInstance().navigateTo(routes.OS_PEOPLE);
   });
 
   teardown(() => {
@@ -50,9 +65,7 @@ suite('<os-settings-people-page>', () => {
     loadTimeData.overrideValues({
       isAccountManagerEnabled: false,
     });
-    peoplePage = document.createElement('os-settings-people-page');
-    peoplePage.pageAvailability = createPageAvailabilityForTesting();
-    document.body.appendChild(peoplePage);
+    createPage();
 
     await browserProxy.whenCalled('getProfileInfo');
     await syncBrowserProxy.whenCalled('getSyncStatus');
@@ -92,7 +105,7 @@ suite('<os-settings-people-page>', () => {
 
     // Sub-page trigger is hidden.
     const element = peoplePage.shadowRoot!.querySelector<CrIconButtonElement>(
-        '#account-manager-subpage-trigger');
+        '#accountManagerSubpageTrigger');
     assertTrue(!!element);
     assertTrue(element.hidden);
   });
@@ -102,10 +115,7 @@ suite('<os-settings-people-page>', () => {
       // Simulate parental controls.
       showParentalControls: true,
     });
-
-    peoplePage = document.createElement('os-settings-people-page');
-    document.body.appendChild(peoplePage);
-    flush();
+    createPage();
 
     // Setup button is shown and enabled.
     assert(peoplePage.shadowRoot!.querySelector(
@@ -117,10 +127,7 @@ suite('<os-settings-people-page>', () => {
       // Simulate parental controls.
       showParentalControls: true,
     });
-
-    peoplePage = document.createElement('os-settings-people-page');
-    document.body.appendChild(peoplePage);
-    flush();
+    createPage();
 
     const params = new URLSearchParams();
     params.append(
@@ -140,9 +147,7 @@ suite('<os-settings-people-page>', () => {
   });
 
   test('Deep link to encryption options on old sync page', async () => {
-    peoplePage = document.createElement('os-settings-people-page');
-    document.body.appendChild(peoplePage);
-    flush();
+    createPage();
 
     // Load the sync page.
     Router.getInstance().navigateTo(routes.SYNC);
@@ -244,9 +249,7 @@ suite('<os-settings-people-page>', () => {
       secondaryGoogleAccountSigninAllowed: true,
       osProfileName: fakeOsProfileName,
     });
-    peoplePage = document.createElement('os-settings-people-page');
-    peoplePage.pageAvailability = createPageAvailabilityForTesting();
-    document.body.appendChild(peoplePage);
+    createPage();
 
     await accountManagerBrowserProxy.whenCalled('getAccounts');
     await syncBrowserProxy.whenCalled('getSyncStatus');
@@ -281,12 +284,59 @@ suite('<os-settings-people-page>', () => {
     // Sub-page trigger is shown.
     const subpageTrigger =
         peoplePage.shadowRoot!.querySelector<CrIconButtonElement>(
-            '#account-manager-subpage-trigger');
+            '#accountManagerSubpageTrigger');
     assertTrue(!!subpageTrigger);
     assertFalse(subpageTrigger.hidden);
 
     // Sub-page trigger navigates to Google account manager.
     subpageTrigger.click();
     assertEquals(routes.ACCOUNT_MANAGER, Router.getInstance().currentRoute);
+  });
+
+  const subpageTriggerData: SubpageTriggerData[] = [
+    {
+      triggerSelector: '#syncSetupRow',
+      routeName: 'SYNC',
+    },
+    {
+      triggerSelector: '#accountManagerSubpageTrigger',
+      routeName: 'ACCOUNT_MANAGER',
+    },
+  ];
+  subpageTriggerData.forEach(({triggerSelector, routeName}) => {
+    test(
+        `Row for ${routeName} is focused when returning from subpage`,
+        async () => {
+          loadTimeData.overrideValues({
+            isAccountManagerEnabled: true,
+            // settings-account-manager-subpage requires this to have a value.
+            secondaryGoogleAccountSigninAllowed: true,
+            osProfileName: 'Currently signed in as Walter White',
+          });
+          createPage();
+
+          await accountManagerBrowserProxy.whenCalled('getAccounts');
+          await syncBrowserProxy.whenCalled('getSyncStatus');
+          flush();
+
+          const subpageTrigger =
+              peoplePage.shadowRoot!.querySelector<HTMLElement>(
+                  triggerSelector);
+          assertTrue(!!subpageTrigger);
+
+          // Sub-page trigger navigates to subpage for route
+          subpageTrigger.click();
+          assertEquals(routes[routeName], Router.getInstance().currentRoute);
+
+          // Navigate back
+          const popStateEventPromise = eventToPromise('popstate', window);
+          Router.getInstance().navigateToPreviousRoute();
+          await popStateEventPromise;
+          await waitAfterNextRender(peoplePage);
+
+          assertEquals(
+              subpageTrigger, peoplePage.shadowRoot!.activeElement,
+              `${triggerSelector} should be focused.`);
+        });
   });
 });
