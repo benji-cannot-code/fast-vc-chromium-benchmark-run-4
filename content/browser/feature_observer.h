ddc052c644d7521b7480e4b8b4e0348d95bfb932
Adam Rice <ricea@chromium.org>
2023-07-24 13:45:15 +0000

    Remove stack_container.h include in /content/browser
    feature_observer.h was including stack_container.h despite
    not using it. Remove the #include statement.
    
    This CL was uploaded by git cl split.
    
    R=boliu@chromium.org
    
    Bug: 1465736
    Change-Id: If9c442aa5dac390630d8a42d181d8d33abf1cef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4709546
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174177}

--- content/browser/feature_observer.h
+++ content/browser/feature_observer.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_FEATURE_OBSERVER_H_
 #define CONTENT_BROWSER_FEATURE_OBSERVER_H_
 
-#include "base/containers/stack_container.h"
 #include "base/memory/raw_ptr.h"
 #include "content/public/browser/global_routing_id.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
