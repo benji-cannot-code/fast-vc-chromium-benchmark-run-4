663251e2aa5022bc39483098d518a3a8a8b0795a
Solomon Kinard <solomonkinard@chromium.org>
2023-07-24 03:40:18 +0000

    AppIconLoader: Comment: Update to the correct class for overrides
    Bug: None
    Change-Id: I7de34bee4a3e39545258a112406fbcc98fed5171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4709834
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174035}

--- chrome/browser/apps/app_service/app_icon/app_icon_loader.h
+++ chrome/browser/apps/app_service/app_icon/app_icon_loader.h
@@ -217,7 +217,7 @@ class AppIconLoader : public base::RefCounted<AppIconLoader>,
 
   void MaybeLoadFallbackOrCompleteEmpty();
 
-  // ProfileManagerObserver overrides.
+  // ProfileObserver overrides.
   void OnProfileWillBeDestroyed(Profile* profile) override;
 
   // If non-null, points to the profile necessary to support the icon loading.
