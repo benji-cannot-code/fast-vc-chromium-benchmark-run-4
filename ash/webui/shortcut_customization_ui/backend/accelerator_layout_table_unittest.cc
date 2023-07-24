7dfa0c8a20b25fdc7e3b19f9936885b901c42e36
xiangdong kong <xiangdongkong@google.com>
2023-07-24 20:03:46 +0000

    shortcuts: Fix the AshAcceleratorsNotInAllowedListShouldHaveLayouts test
    We're passing too a lot of false positives. For example,
    AcceleratorAction::kLaunchApp7 does not have an associated layout but
    it's still being passed in the tests.
    
    The reason for this is because we have two sets of enums in
    kAcceleratorLayouts:
    - AcceleratorAction (ash)
    - NonConfigurableActions (non-ash)
    
    They have overlapped enum values. Since the kAcceleratorData contains
    only ash accelerators, we should filter the kAcceleratorLayouts by only
    mojom::AcceleratorSource::kAsh.
    
    Bug: b/216049298
    Test: ash_unittests --gtest_filter=AcceleratorLayoutMetadataTest*
    Change-Id: I05c11c62d6eda7191ac96e343abc3b62bc46e0ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708589
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174394}

--- ash/webui/shortcut_customization_ui/backend/accelerator_layout_table_unittest.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_layout_table_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/accelerator_actions.h"
 #include "ash/public/cpp/accelerators.h"
+#include "ash/public/mojom/accelerator_info.mojom-shared.h"
 #include "base/strings/strcat.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -33,8 +34,10 @@ class AcceleratorLayoutMetadataTest : public testing::Test {
 
   void SetUp() override {
     for (const auto& layout : ash::kAcceleratorLayouts) {
-      ash_accelerator_with_layouts_.insert(
-          static_cast<ash::AcceleratorAction>(layout.action_id));
+      if (layout.source == mojom::AcceleratorSource::kAsh) {
+        ash_accelerator_with_layouts_.insert(
+            static_cast<ash::AcceleratorAction>(layout.action_id));
+      }
     }
 
     testing::Test::SetUp();
@@ -42,8 +45,8 @@ class AcceleratorLayoutMetadataTest : public testing::Test {
 
  protected:
   bool ShouldNotHaveLayouts(ash::AcceleratorAction action) {
-    return kAcceleratorsWithoutLayout.find(action) !=
-           kAcceleratorsWithoutLayout.end();
+    return kAshAcceleratorsWithoutLayout.find(action) !=
+           kAshAcceleratorsWithoutLayout.end();
   }
 
   bool HasLayouts(ash::AcceleratorAction action) {
@@ -58,16 +61,16 @@ class AcceleratorLayoutMetadataTest : public testing::Test {
 }  // namespace
 
 // Test that all ash accelerators should have a layout or should be added to the
-// exception list kAcceleratorsWithoutLayout.
+// exception list kAshAcceleratorsWithoutLayout.
 TEST_F(AcceleratorLayoutMetadataTest,
-       DISABLED_AcceleratorsNotInAllowedListShouldHaveLayouts) {
+       AshAcceleratorsNotInAllowedListShouldHaveLayouts) {
   for (size_t i = 0; i < ash::kAcceleratorDataLength; ++i) {
     const ash::AcceleratorData& accel_data = ash::kAcceleratorData[i];
     if (ShouldNotHaveLayouts(accel_data.action)) {
       EXPECT_FALSE(HasLayouts(accel_data.action))
           << ToActionName(accel_data.action)
           << " has layouts. Please remove it from "
-             "kAcceleratorsWithoutLayout in "
+             "kAshAcceleratorsWithoutLayout in "
              "ash/webui/shortcut_customization_ui/backend/"
              "accelerator_layout_table.h.";
     } else {
@@ -76,7 +79,8 @@ TEST_F(AcceleratorLayoutMetadataTest,
           << " does not has layouts. Please add a layout to "
              "kAcceleratorLayouts and following the instruction in [1] or if "
              "it should not have layouts, state so "
-             "explicitly by adding it to kAcceleratorsWithoutLayout in [1].\n"
+             "explicitly by adding it to kAshAcceleratorsWithoutLayout in "
+             "[1].\n"
              "[1] ash/webui/shortcut_customization_ui/backend/"
              "accelerator_layout_table.h.";
     }
