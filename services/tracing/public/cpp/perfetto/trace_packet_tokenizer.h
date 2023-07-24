e07603fe52065a9bc1fbc459027f22aa5a6d328d
Adam Rice <ricea@chromium.org>
2023-07-24 11:03:13 +0000

    Use InlinedVector in /services/tracing
    Replace usages of base::StackVector with absl::InlinedVector. This is
    not a pure rename, as the semantics are slightly different:
    
    1. Methods like push_back() which were accessed with the -> operator in
       StackVector are instead accessed with . with InlinedVector.
    
    See also discussion thread in cxx@chromium.org:
    https://groups.google.com/a/chromium.org/g/cxx/c/jTfqVfU-Ka0/m/caaal90NCgAJ
    
    This CL was uploaded by git cl split.
    
    R=nuskos@chromium.org
    
    Bug: 1465736
    Change-Id: Ieb1607ef2c3460b8db032004994beed849f66a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708087
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174136}

--- services/tracing/public/cpp/perfetto/trace_packet_tokenizer.h
+++ services/tracing/public/cpp/perfetto/trace_packet_tokenizer.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/component_export.h"
-#include "base/containers/stack_container.h"
+#include "third_party/abseil-cpp/absl/container/inlined_vector.h"
 #include "third_party/perfetto/include/perfetto/protozero/proto_utils.h"
 
 namespace perfetto {
@@ -44,8 +44,8 @@ class COMPONENT_EXPORT(TRACING_CPP) TracePacketTokenizer {
 
   // Returns |true| if there is more data left to be consumed in the tokenizer.
   bool has_more() const {
-    return !next_packet_.header->empty() || next_packet_.parsed_size ||
-           !next_packet_.partial_data->empty();
+    return !next_packet_.header.empty() || next_packet_.parsed_size ||
+           !next_packet_.partial_data.empty();
   }
 
  private:
@@ -59,10 +59,10 @@ class COMPONENT_EXPORT(TRACING_CPP) TracePacketTokenizer {
 
     // Most trace packets are very small, so avoid heap allocations in the
     // common case where one packet straddles the boundary between chunks.
-    base::StackVector<uint8_t, 64> partial_data;
+    absl::InlinedVector<uint8_t, 64> partial_data;
 
     uint64_t parsed_size = 0;
-    base::StackVector<uint8_t, kMaxHeaderSize> header;
+    absl::InlinedVector<uint8_t, kMaxHeaderSize> header;
   };
 
   // Packet currently being parsed.
