5957d9d1ab886f59339b4cc1519f0eb862d60626
Adam Rice <ricea@chromium.org>
2023-07-24 09:56:02 +0000

    Use InlinedVector in /base
    Replace usages of base::StackVector with absl::InlinedVector. This is
    not a pure rename, as the semantics are slightly different:
    
    1. Methods like push_back() which were accessed with the -> operator in
       StackVector are instead accessed with . with InlinedVector.
    2. The .container() method is not needed with InlinedVector
    
    See also discussion thread in cxx@chromium.org:
    https://groups.google.com/a/chromium.org/g/cxx/c/jTfqVfU-Ka0/m/caaal90NCgAJ
    
    This CL was uploaded by git cl split.
    
    R=dcheng@chromium.org
    
    Bug: 1465736
    Change-Id: Ib6388a72cd77b2937dbcca13ac9281372078d223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705411
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174118}

--- base/message_loop/message_pump_epoll.h
+++ base/message_loop/message_pump_epoll.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/base_export.h"
-#include "base/containers/stack_container.h"
 #include "base/files/scoped_file.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ptr_exclusion.h"
@@ -23,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/watchable_io_message_pump_posix.h"
 #include "base/threading/thread_checker.h"
 #include "base/time/time.h"
+#include "third_party/abseil-cpp/absl/container/inlined_vector.h"
 
 namespace base {
 
@@ -96,7 +96,7 @@ class BASE_EXPORT MessagePumpEpoll : public MessagePump,
     // all real scenarios, since there's little practical value in having more
     // than two controllers (e.g. one reader and one writer) watch the same
     // descriptor on the same thread.
-    StackVector<scoped_refptr<Interest>, 2> interests;
+    absl::InlinedVector<scoped_refptr<Interest>, 2> interests;
 
     // Temporary pointer to an active epoll_event structure which refers to
     // this entry. This is set immediately upon returning from epoll_wait() and
