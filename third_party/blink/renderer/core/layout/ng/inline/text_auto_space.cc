240c407478488e6e2e02bc9de9d65dbe4b19520d
Koji Ishii <kojii@chromium.org>
2023-07-24 07:59:50 +0000

    [text-autospace] Optimzie for 8-bits string
    This patch makes `TextAutoSpace::ApplyIfNeeded` to early
    return if 8-bits string.
    
    Also adds a test to ensure 8-bits string has no changes.
    
    Bug: 1463890
    Change-Id: I622dfccd40a8e08814ccd640cb6e89ea4a3fa3d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4710107
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174080}

--- third_party/blink/renderer/core/layout/ng/inline/text_auto_space.cc
+++ third_party/blink/renderer/core/layout/ng/inline/text_auto_space.cc
@@ -32,6 +32,11 @@ inline bool IsIdeograph(UScriptCode script) {
 // static
 void TextAutoSpace::ApplyIfNeeded(NGInlineItemsData& data,
                                   Vector<wtf_size_t>* offsets_out) {
+  const String& text = data.text_content;
+  if (text.Is8Bit()) {
+    return;  // 8-bits never be `kIdeograph`. See `TextAutoSpaceTest`.
+  }
+
   HeapVector<NGInlineItem>& items = data.items;
   if (UNLIKELY(items.empty())) {
     return;
@@ -40,7 +45,6 @@ void TextAutoSpace::ApplyIfNeeded(NGInlineItemsData& data,
   // Compute `RunSegmenterRange` for the whole text content. It's pre-computed,
   // but packed in `NGInlineItemSegments` to save memory.
   NGInlineItemSegments::RunSegmenterRanges ranges;
-  const String& text = data.text_content;
   if (!data.segments) {
     const NGInlineItem& item0 = items.front();
     RunSegmenter::RunSegmenterRange range = item0.CreateRunSegmenterRange();
@@ -58,7 +62,6 @@ void TextAutoSpace::ApplyIfNeeded(NGInlineItemsData& data,
       return;
     }
   }
-  DCHECK(!text.Is8Bit());
   DCHECK_EQ(text.length(), ranges.back().end);
 
   Vector<wtf_size_t, 16> offsets;
@@ -146,9 +149,7 @@ void TextAutoSpace::ApplyIfNeeded(NGInlineItemsData& data,
 // static
 TextAutoSpace::CharType TextAutoSpace::GetTypeAndNext(const String& text,
                                                       wtf_size_t& offset) {
-  if (text.Is8Bit()) {
-    return GetType(text[offset++]);
-  }
+  CHECK(!text.Is8Bit());
   UChar ch;
   U16_NEXT(text.Characters16(), offset, text.length(), ch);
   return GetType(ch);
@@ -158,9 +159,7 @@ TextAutoSpace::CharType TextAutoSpace::GetTypeAndNext(const String& text,
 TextAutoSpace::CharType TextAutoSpace::GetPrevType(const String& text,
                                                    wtf_size_t offset) {
   DCHECK_GT(offset, 0u);
-  if (text.Is8Bit()) {
-    return GetType(text[offset - 1]);
-  }
+  CHECK(!text.Is8Bit());
   UChar last_ch;
   U16_PREV(text.Characters16(), 0, offset, last_ch);
   return GetType(last_ch);
