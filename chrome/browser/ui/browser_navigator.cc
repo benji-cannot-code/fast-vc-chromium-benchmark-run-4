4852e9b0c4fb909b527e1949a5c51ffbdd5d9994
Scott Violet <sky@chromium.org>
2023-07-24 17:14:01 +0000

    chrome: make GetIndexAndBrowserOfExistingTab() check delete scheduled
    This function shouldn't return browsers scheduled for deletion.
    
    This also removes an unnecessary include in browser_navigator.
    
    Bug: 1434387
    Change-Id: Ibaf3fc508c2ace1cc538c54f656fb38780fd4276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707012
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174270}

--- chrome/browser/ui/browser_navigator.cc
+++ chrome/browser/ui/browser_navigator.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "base/command_line.h"
 #include "base/memory/raw_ptr.h"
 #include "base/notreached.h"
 #include "base/strings/utf_string_conversions.h"
@@ -31,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/task_manager/web_contents_tags.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/location_bar/location_bar.h"
