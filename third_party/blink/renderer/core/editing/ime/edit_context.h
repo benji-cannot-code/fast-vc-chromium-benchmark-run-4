70493ee764c3eecdafd2d3f4d67b3ba902f67d45
Dan Clark <daniec@microsoft.com>
2023-07-24 20:02:36 +0000

    [EditContext] Remove inputMode, inputPanelPolicy, enterKeyHint attributes
    The following properties exist on the EditContext IDL but are not in
    the spec:
    - EditContext.inputMode
    - EditContext.inputPanelPolicy
    - EditContext.enterKeyHint
    
    All three of these now also exist as global attributes on HTMLElement,
    so instead of supporting them on EditContext we can instead have
    EditContext use the value on the currently focused element, like
    how things already work for normal editable elements.
    
    This way, the author can control the value of these by setting the
    attributes on the DOM element being edited rather than through
    the EditContext itself.
    
    Bug: 999184
    Change-Id: I50150bac20cf7448e28e36501f00a6de717e0680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698578
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174392}

--- third_party/blink/renderer/core/editing/ime/edit_context.h
+++ third_party/blink/renderer/core/editing/ime/edit_context.h
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_IME_EDIT_CONTEXT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_IME_EDIT_CONTEXT_H_
 
-#include "third_party/blink/public/platform/web_text_input_mode.h"
 #include "third_party/blink/public/platform/web_text_input_type.h"
 #include "third_party/blink/public/web/web_input_method_controller.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_edit_context_input_panel_policy.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/element_rare_data_field.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
@@ -20,16 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-using EditContextInputPanelPolicy = V8EditContextInputPanelPolicy::Enum;
-
 class DOMRect;
 class EditContext;
 class EditContextInit;
 class Element;
 class ExceptionState;
 class InputMethodController;
-class V8EditContextEnterKeyHint;
-class V8EditContextInputMode;
 
 // The goal of the EditContext is to expose the lower-level APIs provided by
 // modern operating systems to facilitate various input modalities to unlock
@@ -119,15 +113,6 @@ class CORE_EXPORT EditContext final : public EventTarget,
   // Returns the current cached character bounds.
   const HeapVector<Member<DOMRect>> characterBounds();
 
-  // Returns the InputMode of the EditContext.
-  V8EditContextInputMode inputMode() const;
-
-  // Returns the EnterKeyHint of the EditContext.
-  V8EditContextEnterKeyHint enterKeyHint() const;
-
-  // Returns the InputPanelPolicy of the EditContext.
-  V8EditContextInputPanelPolicy inputPanelPolicy() const;
-
   // Sets the text of the EditContext which is used to display suggestions.
   void setText(const String& text);
 
@@ -137,19 +122,6 @@ class CORE_EXPORT EditContext final : public EventTarget,
 
   // Sets the selectionEnd of the EditContext.
   void setSelectionEnd(uint32_t selection_end, ExceptionState& exception_state);
-
-  // Sets an input mode defined in EditContextInputMode.
-  // This relates to the inputMode attribute defined for input element:
-  // https://html.spec.whatwg.org/multipage/interaction.html#input-modalities:-the-inputmode-attribute.
-  void setInputMode(const V8EditContextInputMode& input_mode);
-
-  // Sets a specific action related to Enter key defined in
-  // https://html.spec.whatwg.org/multipage/interaction.html#input-modalities:-the-enterkeyhint-attribute.
-  void setEnterKeyHint(const V8EditContextEnterKeyHint& enter_key_hint);
-
-  // Sets a policy that determines whether the VK should be raised or dismissed.
-  // Auto raises the VK automatically, Manual suppresses it.
-  void setInputPanelPolicy(const V8EditContextInputPanelPolicy& input_policy);
 
   // Internal APIs (called from Blink).
 
@@ -176,7 +148,6 @@ class CORE_EXPORT EditContext final : public EventTarget,
       ConfirmCompositionBehavior selection_behavior) override;
   WebTextInputInfo TextInputInfo() override;
   int ComputeWebTextInputNextPreviousFlags() override { return 0; }
-  WebTextInputType TextInputType() override;
   int TextInputFlags() const;
   WebRange CompositionRange() const override;
   bool GetCompositionCharacterBounds(WebVector<gfx::Rect>& bounds) override;
@@ -213,7 +184,6 @@ class CORE_EXPORT EditContext final : public EventTarget,
   void DeleteWordBackward();
   void DeleteWordForward();
 
-  bool IsVirtualKeyboardPolicyManual() const override;
   bool IsEditContextActive() const override;
   // Returns whether show()/hide() API is called from virtualkeyboard or not.
   ui::mojom::VirtualKeyboardVisibilityRequest
@@ -240,12 +210,6 @@ class CORE_EXPORT EditContext final : public EventTarget,
   void DetachElement(Element* element_to_detach);
 
  private:
-  // Returns the enter key action attribute set in the EditContext.
-  ui::TextInputAction GetEditContextEnterKeyHint() const;
-
-  // Returns the inputMode of the EditContext from enterKeyHint property.
-  WebTextInputMode GetInputModeOfEditContext() const;
-
   InputMethodController& GetInputMethodController() const;
 
   void DeleteCurrentSelection();
@@ -302,10 +266,6 @@ class CORE_EXPORT EditContext final : public EventTarget,
   String text_;
   uint32_t selection_start_ = 0;
   uint32_t selection_end_ = 0;
-  WebTextInputMode input_mode_ = WebTextInputMode::kWebTextInputModeText;
-  ui::TextInputAction enter_key_hint_ = ui::TextInputAction::kEnter;
-  EditContextInputPanelPolicy input_panel_policy_ =
-      EditContextInputPanelPolicy::kManual;
   gfx::Rect control_bounds_;
   gfx::Rect selection_bounds_;
   WebVector<gfx::Rect> character_bounds_;
