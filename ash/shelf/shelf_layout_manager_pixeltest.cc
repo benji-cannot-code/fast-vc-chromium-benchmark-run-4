7f5559da866e85a9dbc90ed38fb26676291be27f
Sean Kau <skau@chromium.org>
2023-07-24 19:49:05 +0000

    Enable Jelly for ShelfLayout pixeltest
    Enable Jelly in test ahead of enabling flag by default so that the
    updated images can be reviewed in a managable group.
    
    Change-Id: I0c86953a8933c29822ab4d51baa2a4d19d3699c5
    Bug: b:292294480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708347
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174383}

--- ash/shelf/shelf_layout_manager_pixeltest.cc
+++ ash/shelf/shelf_layout_manager_pixeltest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/pixel/ash_pixel_test_init_params.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 
 namespace ash {
 
@@ -22,7 +23,9 @@ class ShelfLayoutManagerPixelRTLTest
   void SetUp() override {
     ShelfLayoutManagerTestBase::SetUp();
     PopulateAppShortcut(5);
-    scoped_feature_list_.InitAndDisableFeature(features::kContextualNudges);
+    scoped_feature_list_.InitWithFeatureStates(
+        {{features::kContextualNudges, false},
+         {chromeos::features::kJelly, true}});
   }
 
   absl::optional<pixel_test::InitParams> CreatePixelTestInitParams()
@@ -50,7 +53,7 @@ TEST_P(ShelfLayoutManagerPixelRTLTest, AutohideShelfVisibility) {
   shelf->SetAutoHideBehavior(ShelfAutoHideBehavior::kNever);
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "shelf_no_auto_hide",
-      /*revision_number=*/5, shelf->GetWindow(), shelf->hotseat_widget()));
+      /*revision_number=*/6, shelf->GetWindow(), shelf->hotseat_widget()));
 
   // When the auto-hide is set and a window is shown fullscreen, the shelf
   // should not be showing on the screen.
@@ -67,7 +70,7 @@ TEST_P(ShelfLayoutManagerPixelRTLTest, AutohideShelfVisibility) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "shelf_show_with_auto_hide",
-      /*revision_number=*/4, shelf->GetWindow(), shelf->hotseat_widget()));
+      /*revision_number=*/5, shelf->GetWindow(), shelf->hotseat_widget()));
 }
 
 }  // namespace ash
