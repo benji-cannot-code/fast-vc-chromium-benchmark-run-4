80ad91f787cb7a7a888ea7b866265b2bd3529d62
David Yeung <dayeung@chromium.org>
2023-07-24 20:59:15 +0000

    [CR2023] Fix page info typography.
    Fixed more label text styles.
    
    Screenshot:
    https://screenshot.googleplex.com/5VCCczMkETypQsU
    
    Bug: 1422123
    Change-Id: I6bdbe98dae601d8ac467a11021e2af1efd4226af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708554
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174427}

--- chrome/browser/ui/views/controls/rich_hover_button.cc
+++ chrome/browser/ui/views/controls/rich_hover_button.cc
@@ -116,7 +116,7 @@ RichHoverButton::RichHoverButton(
 
   if (features::IsChromeRefresh2023()) {
     title_->SetTextStyle(views::style::STYLE_BODY_3_MEDIUM);
-    secondary_label_->SetTextStyle(views::style::STYLE_BODY_5_MEDIUM);
+    secondary_label_->SetTextStyle(views::style::STYLE_BODY_5);
   }
 
   // State icon is optional and column is created only when it is set.
@@ -144,7 +144,7 @@ RichHoverButton::RichHoverButton(
         subtitle_text, views::style::CONTEXT_LABEL,
         views::style::STYLE_SECONDARY));
     if (features::IsChromeRefresh2023()) {
-      subtitle_->SetTextStyle(views::style::STYLE_BODY_5_MEDIUM);
+      subtitle_->SetTextStyle(views::style::STYLE_BODY_5);
     }
     subtitle_->SetMultiLine(true);
     subtitle_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
