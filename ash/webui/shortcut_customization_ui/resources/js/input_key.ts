8eac34d7f2e80cc5a172cce4e845060ef3e8cc05
Akihiro Ota <akihiroota@chromium.org>
2023-07-24 15:58:32 +0000

    Reland "Accessibility: Implement Dictation keyboard improvements"
    This is a reland of commit 68cbe69b809e3918c5ad275583ea285d6e8b6653
    
    The original change was reverted because of flaky failures:
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/34381/test-results?sortby=&groupby=
    
    This change relands the original change and ensures that the test
    passes using --gtest_repeat=70 --test-launcher-jobs=70.
    
    Original change's description:
    > Accessibility: Implement Dictation keyboard improvements
    >
    > This change implements improvements to Dictation's keyboard shortcuts
    > (Search + D combination and the Dictation key on supported keyboards).
    > If Dictation is enabled, those keys toggle Dictation normally (current
    > behavior). If Dictation is disabled, those keys will enable the
    > Dictation feature and show the network dialog if needed (new
    > behavior).
    >
    > To do so, this change:
    > 1. Adds a method called EnableOrToggleDictation(), which follows the
    > behavior described above.
    > 2. Adds automated tests at the AccessibilityController and
    > AccessibilityManager levels.
    > 3. Updates the accelerator action and associated methods to match the
    > new method name.
    > 4. Adds a feature flag so that the new behavior is protected behind
    > the flag.
    >
    > Please see the UI mocks [1] for more details on desired behavior. Note
    > that the mocks show updated dialog strings and two additional pieces
    > of UI - those changes are left out of this change and will be added
    > in a follow-up.
    >
    > [1] https://docs.google.com/presentation/d/1mRnYcRpyzbY-EjzptCP3KRB5I7v_rM9Kl2zqhhQVZUY/edit#slide=id.p
    >
    > Bug: b:259352600
    > Change-Id: I848bb75791448ad8fe2456ed76581f0c4b4dd400
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653221
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    > Reviewed-by: Katie Dektar <katie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1170579}
    
    Bug: b:259352600
    Change-Id: I4996b3d768fc7ca6cf4a216180addadfe6e03267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4689962
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174225}

--- ash/webui/shortcut_customization_ui/resources/js/input_key.ts
+++ ash/webui/shortcut_customization_ui/resources/js/input_key.ts
@@ -43,6 +43,7 @@ export const keyToIconNameMap: {[key: string]: string|undefined} = {
   'BrowserRefresh': 'refresh',
   'BrowserSearch': 'browser-search',
   'EmojiPicker': 'emoji-picker',
+  'EnableOrToggleDictation': 'dictation-toggle',
   'KeyboardBacklightToggle': 'keyboard-brightness-toggle',
   'KeyboardBrightnessUp': 'keyboard-brightness-up',
   'KeyboardBrightnessDown': 'keyboard-brightness-down',
@@ -62,7 +63,6 @@ export const keyToIconNameMap: {[key: string]: string|undefined} = {
   'PrintScreen': 'screenshot',
   'PrivacyScreenToggle': 'electronic-privacy-screen',
   'Settings': 'settings',
-  'ToggleDictation': 'dictation-toggle',
   'ZoomToggle': 'fullscreen',
 };
 
