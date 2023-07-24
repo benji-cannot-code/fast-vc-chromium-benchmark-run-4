98d4308f2a668003e6a77c0fe08450cc7ea3f770
Ivan Šandrk <isandrk@chromium.org>
2023-07-24 12:04:58 +0000

    [AWC] Require user activation for window.minimize|maximize APIs
    Require user activation for min|max APIs, reject calls made without it
    with NotAllowedError.
    
    Add a TODO for the restore API since that one is a bit more involved
    (requires capability delegation).
    
    I2P thread
    https://groups.google.com/a/chromium.org/g/blink-dev/c/oCxWg8q_OQY/m/LUTw0T5cCQAJ
    
    Bug: 1466855,b/288265319
    Change-Id: Icdc7cf36afda54b1bf827fa385677f7e3ad42603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705487
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174147}

--- third_party/blink/renderer/core/frame/local_dom_window.cc
+++ third_party/blink/renderer/core/frame/local_dom_window.cc
@@ -2503,6 +2503,13 @@ void LocalDOMWindow::maximize(ExceptionState& exception_state) {
     return;
   }
 
+  // Require user activation.
+  if (!LocalFrame::ConsumeTransientUserActivation(GetFrame())) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kNotAllowedError,
+                                      "API requires user activation.");
+    return;
+  }
+
 #if defined(USE_AURA)
   GetFrame()->GetLocalFrameHostRemote().Maximize();
 #else
@@ -2520,6 +2527,13 @@ void LocalDOMWindow::minimize(ExceptionState& exception_state) {
     exception_state.ThrowDOMException(
         DOMExceptionCode::kInvalidStateError,
         "API is only supported in primary top-level browsing contexts.");
+    return;
+  }
+
+  // Require user activation.
+  if (!LocalFrame::ConsumeTransientUserActivation(GetFrame())) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kNotAllowedError,
+                                      "API requires user activation.");
     return;
   }
 
@@ -2542,6 +2556,10 @@ void LocalDOMWindow::restore(ExceptionState& exception_state) {
         "API is only supported in primary top-level browsing contexts.");
     return;
   }
+
+  // TODO(crbug.com/1466853): Add transient user activation for window.restore.
+  // This one is a bit more involved compared to minimize/maximize since it
+  // requires capability delegation.
 
 #if defined(USE_AURA)
   GetFrame()->GetLocalFrameHostRemote().Restore();
