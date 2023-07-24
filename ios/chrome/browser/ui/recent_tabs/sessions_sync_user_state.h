f069c0caca75eac96669d9c52ecd2e673c43f4d1
Mikel Astiz <mastiz@chromium.org>
2023-07-24 15:32:39 +0000

    [iOS] Add TODOs for kSync references from recent tabs
    The underlying task is to actually redesign SessionsSyncUserState.
    
    Change-Id: I096d77ca9eeac6b6022623428182a9697b95b303
    Bug: 1466884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705721
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174217}

--- ios/chrome/browser/ui/recent_tabs/sessions_sync_user_state.h
+++ ios/chrome/browser/ui/recent_tabs/sessions_sync_user_state.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_RECENT_TABS_SESSIONS_SYNC_USER_STATE_H_
 
 // States listing the user's signed-in and sync status.
+// TODO(crbug.com/1466884): Update this enum to reflect the more modern product
+// state machine.
 enum class SessionsSyncUserState {
   USER_SIGNED_OUT,
   USER_SIGNED_IN_SYNC_OFF,
