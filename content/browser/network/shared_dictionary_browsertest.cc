5457a461377e038b909bd215d1c870e4410e24cd
Tsuyoshi Horo <horo@chromium.org>
2023-07-24 21:46:19 +0000

    Support dictionary load from subresources link headers
    Currently, the dictionary link HTTP header only works for document
    resources. This is because the `is_dictionary_load_allowed` flag is only
    set to true when PreloadHelper::LoadLinksFromHeader() is processing the
    link HTTP header of document resources. However, we should support
    dictionary loading from subresource link HTTP headers.
    
    To do this, this CL changes PreloadHelper::LoadLinksFromHeader() to also
    enable `is_dictionary_load_allowed` when processing the link HTTP header
    of subresources.
    
    Bug: 1413922
    Change-Id: I5d0ee5f0300c060a8fc540b01d8e86d46bcb808d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707120
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174470}

--- content/browser/network/shared_dictionary_browsertest.cc
+++ content/browser/network/shared_dictionary_browsertest.cc
@@ -163,6 +163,8 @@ enum class FeatureState { kDisabled, kBackendOnly, kFullyEnabled };
 enum class BrowserType { kNormal, kOffTheRecord };
 enum class FetchType {
   kLinkRelDictionary,
+  kLinkRelDictionaryDocumentHeader,
+  kLinkRelDictionarySubresourceHeader,
   kFetchApi,
   kFetchApiWithSameOriginMode,
   kFetchApiWithNoCorsMode,
@@ -179,6 +181,27 @@ std::string LinkRelDictionaryScript(const GURL& dictionary_url) {
                 link.rel = 'dictionary';
                 link.href = $1;
                 document.body.appendChild(link);
+              })();
+            )",
+                   dictionary_url);
+}
+
+std::string LinkRelDictionaryDocumentHeaderScript(const GURL& dictionary_url) {
+  return JsReplace(R"(
+              (()=>{
+                const iframe = document.createElement('iframe');
+                iframe.src = new URL('with_dict_header.html', $1);
+                document.body.appendChild(iframe);
+              })();
+            )",
+                   dictionary_url);
+}
+
+std::string LinkRelDictionarySubresourceHeaderScript(
+    const GURL& dictionary_url) {
+  return JsReplace(R"(
+              (()=>{
+                fetch(new URL('with_dict_header.html', $1));
               })();
             )",
                    dictionary_url);
@@ -498,6 +521,12 @@ class SharedDictionaryBrowserTestBase : public ContentBrowserTest {
     switch (fetch_type) {
       case FetchType::kLinkRelDictionary:
         script = LinkRelDictionaryScript(dictionary_url);
+        break;
+      case FetchType::kLinkRelDictionaryDocumentHeader:
+        script = LinkRelDictionaryDocumentHeaderScript(dictionary_url);
+        break;
+      case FetchType::kLinkRelDictionarySubresourceHeader:
+        script = LinkRelDictionarySubresourceHeaderScript(dictionary_url);
         break;
       case FetchType::kFetchApi:
         script = FetchDictionaryScript(dictionary_url);
@@ -1233,6 +1262,20 @@ IN_PROC_BROWSER_TEST_P(SharedDictionaryBrowserTest,
   // http://127.0.0.1:PORT/ is secure context, so the dictionary should be
   // written.
   RunWriteDictionaryTest(FetchType::kFetchApi,
+                         GetURL("/shared_dictionary/blank.html"),
+                         GetURL("/shared_dictionary/test.dict"));
+}
+
+IN_PROC_BROWSER_TEST_P(SharedDictionaryBrowserTest,
+                       LinkRelDictionaryDocumentHeader) {
+  RunWriteDictionaryTest(FetchType::kLinkRelDictionaryDocumentHeader,
+                         GetURL("/shared_dictionary/blank.html"),
+                         GetURL("/shared_dictionary/test.dict"));
+}
+
+IN_PROC_BROWSER_TEST_P(SharedDictionaryBrowserTest,
+                       LinkRelDictionarySubresourceHeader) {
+  RunWriteDictionaryTest(FetchType::kLinkRelDictionarySubresourceHeader,
                          GetURL("/shared_dictionary/blank.html"),
                          GetURL("/shared_dictionary/test.dict"));
 }
