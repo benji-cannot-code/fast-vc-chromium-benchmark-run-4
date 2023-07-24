56e37abe0c43ea71628769ed081b7ad630731e85
Adam Rice <ricea@chromium.org>
2023-07-24 16:29:10 +0000

    Remove stack_container.h include in /content/public/common
    common_param_traits.cc was including stack_container.h despite
    not using it. Remove the #include statement.
    
    This CL was uploaded by git cl split.
    
    R=joenotcharles@google.com
    
    Bug: 1465736
    Change-Id: Ic4017b8fa78939714ac54227c6f5ad413c5adc20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4709266
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174244}

--- content/public/common/common_param_traits.cc
+++ content/public/common/common_param_traits.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/containers/stack_container.h"
 #include "content/public/common/content_constants.h"
 #include "content/public/common/referrer.h"
 #include "net/http/http_util.h"
