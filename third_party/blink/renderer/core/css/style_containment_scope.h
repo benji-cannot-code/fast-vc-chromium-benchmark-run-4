36596a76b59774ff61ab8a32f8125d6237e738f9
Daniil Sakhapov <sakhapov@chromium.org>
2023-07-24 20:15:30 +0000

    Remove counters code from style containment code
    The code is incorrect and will be rewritten in future CLs.
    
    Change-Id: I748f8c3db73cbc71dbe50aa9ad9b642a5992e2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707853
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174402}

--- third_party/blink/renderer/core/css/style_containment_scope.h
+++ third_party/blink/renderer/core/css/style_containment_scope.h
@@ -7,18 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_STYLE_CONTAINMENT_SCOPE_H_
 
 #include "third_party/blink/renderer/core/dom/element.h"
-#include "third_party/blink/renderer/core/layout/counter_node.h"
 #include "third_party/blink/renderer/core/layout/layout_quote.h"
-#include "third_party/blink/renderer/core/style/counter_directives.h"
 
 namespace blink {
-
-using CountersVector = HeapVector<Member<CounterNode>>;
 
 // Represents the scope of the subtree that contains style.
 // Manages quotes and child scopes.
 // Managed by StyleContainmentScopeTree.
-class CORE_EXPORT StyleContainmentScope final
+class StyleContainmentScope final
     : public GarbageCollected<StyleContainmentScope> {
  public:
   explicit StyleContainmentScope(const Element* element)
@@ -31,16 +27,6 @@ class CORE_EXPORT StyleContainmentScope final
   void DetachQuote(LayoutQuote&);
   void UpdateQuotes() const;
 
-  void AttachCounter(CounterNode&);
-  void DetachCounter(CounterNode&);
-  void AttachLayoutCounter(LayoutCounter&);
-  void DetachLayoutCounter(LayoutCounter&);
-  void UpdateAllCounters(HashSet<AtomicString>&) const;
-
-  void CreateCounterNodesForLayoutObject(LayoutObject&);
-  void DeleteCounterNodesForLayoutObject(LayoutObject&,
-                                         const ComputedStyle& style);
-
   bool IsAncestorOf(const Element*, const Element* stay_within = nullptr);
 
   void AppendChild(StyleContainmentScope*);
@@ -50,9 +36,6 @@ class CORE_EXPORT StyleContainmentScope final
   StyleContainmentScope* Parent() { return parent_; }
   void SetParent(StyleContainmentScope* parent) { parent_ = parent; }
   const HeapVector<Member<LayoutQuote>>& Quotes() const { return quotes_; }
-  const HeapHashMap<AtomicString, Member<CountersVector>>& Counters() const {
-    return counters_;
-  }
   const HeapVector<Member<StyleContainmentScope>>& Children() const {
     return children_;
   }
@@ -63,14 +46,7 @@ class CORE_EXPORT StyleContainmentScope final
   // Get the quote which would be the last in preorder traversal before we hit
   // Element*.
   const LayoutQuote* FindQuotePrecedingElement(const Element&) const;
-  static const CounterNode* FindCounterPrecedingElement(const Element&,
-                                                        const CountersVector&);
   int ComputeInitialQuoteDepth() const;
-  int ComputeInitialCounterValue(const CountersVector&) const;
-  CounterNode* GetCounterWithIdentifier(const LayoutObject&,
-                                        const AtomicString&) const;
-  bool UpdateCounters(CountersVector& counters) const;
-  CounterNode* CreateCounter(LayoutObject&, const AtomicString&) const;
 
   // Element with style containment which is the root of the scope.
   Member<const Element> element_;
@@ -78,8 +54,6 @@ class CORE_EXPORT StyleContainmentScope final
   Member<StyleContainmentScope> parent_;
   // Vector of quotes.
   HeapVector<Member<LayoutQuote>> quotes_;
-  // Map of counters.
-  HeapHashMap<AtomicString, Member<CountersVector>> counters_;
   // Vector of children scope.
   HeapVector<Member<StyleContainmentScope>> children_;
 };
