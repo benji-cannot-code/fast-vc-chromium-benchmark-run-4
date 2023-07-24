ee0cc5e8fdbeb6a1973660bc06cc837ba6ad0d7c
Sean Kau <skau@chromium.org>
2023-07-24 19:50:12 +0000

    Actually enable Jelly for AssistantZeroState
    The previous CL added PreJelly to the name but didn't actually turn the
    feature on or off.
    
    Bug: b:292294480
    Change-Id: I459a69dde3f03feead8268b47e11770e444a7073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707014
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174385}

--- ash/app_list/views/app_list_view_pixeltest.cc
+++ ash/app_list/views/app_list_view_pixeltest.cc
@@ -433,8 +433,9 @@ class AppListViewAssistantZeroStateTest
   }
 
   void SetUp() override {
-    base::test::ScopedFeatureList scoped_feature_list(
-        assistant::features::kEnableAssistantLearnMore);
+    scoped_features_.InitWithFeatureStates(
+        {{assistant::features::kEnableAssistantLearnMore, true},
+         {chromeos::features::kJelly, JellyEnabled(GetParam())}});
 
     AssistantAshTestBase::SetUp();
     SetNumberOfSessionsWhereOnboardingShown(
@@ -445,6 +446,9 @@ class AppListViewAssistantZeroStateTest
         IsTabletMode(GetParam()));
     ShowAssistantUi();
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_features_;
 };
 
 INSTANTIATE_TEST_SUITE_P(RTL,
@@ -461,7 +465,7 @@ TEST_P(AppListViewAssistantZeroStateTest, Basic) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "app_list_view_assistant_zero_state",
-      /*revision_number=*/JellyEnabled(GetParam()) ? 0 : 1,
+      /*revision_number=*/JellyEnabled(GetParam()) ? 2 : 1,
       page_view()->GetViewByID(AssistantViewID::kZeroStateView)));
 }
 
