1d41b5f9bc9d5517b105784fae996da217b19d4e
Peter Kasting <pkasting@chromium.org>
2023-07-25 19:32:36 +0000

    Misc. cleanup.
    * DCHECK -> CHECK
    * Improved const correctness
    * Functional change: 60 Hz menu scrolling
    * Use ClampRound() instead of casting
    * Add MenuItemView::GetIconPreferredWidth() and GetContainingSubmenu()
      helpers for convenience
    * MenuItemView::GetDelegate() should correctly handle a null root
    * Simplify
    * Functional change: when `forced_visual_selection_` was set, _all_
      items were painted as selected, instead of the desired item. This was
      an accidental regression introduced by sky@ in 2021. Restored the
      intended behavior.
    * Init more members in declaration
    * Fix a DanglingUntriaged tag
    * Make SubmenuView::scroll_view_container_ a unique_ptr
    
    Bug: none
    Change-Id: I020f33d49f14252bff554131f088d59e75867b68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4713611
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1175013}

--- ui/views/controls/menu/menu_controller.cc
+++ ui/views/controls/menu/menu_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/case_conversion.h"
 #include "base/i18n/rtl.h"
 #include "base/memory/raw_ptr.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
@@ -448,9 +449,7 @@ struct MenuController::MenuPart {
 
 class MenuController::MenuScrollTask {
  public:
-  MenuScrollTask() {
-    pixels_per_second_ = MenuItemView::pref_menu_height() * 20;
-  }
+  MenuScrollTask() = default;
 
   MenuScrollTask(const MenuScrollTask&) = delete;
   MenuScrollTask& operator=(const MenuScrollTask&) = delete;
@@ -460,19 +459,20 @@ class MenuController::MenuScrollTask {
       StopScrolling();
       return;
     }
-    DCHECK(part.submenu);
-    SubmenuView* new_menu = part.submenu;
-    bool new_is_up = part.type == MenuPartType::kScrollUp;
-    if (new_menu == submenu_ && is_scrolling_up_ == new_is_up)
+
+    SubmenuView* const new_menu = part.submenu;
+    CHECK(new_menu);
+    const bool new_is_up = part.type == MenuPartType::kScrollUp;
+    if (std::exchange(submenu_, new_menu) == new_menu &&
+        std::exchange(is_scrolling_up_, new_is_up) == new_is_up) {
       return;
+    }
 
     start_scroll_time_ = base::Time::Now();
-    start_y_ = part.submenu->GetVisibleBounds().y();
-    submenu_ = new_menu;
-    is_scrolling_up_ = new_is_up;
-
+    pixels_per_second_ = MenuItemView::pref_menu_height() * 20;
+    start_y_ = submenu_->GetVisibleBounds().y();
     if (!scrolling_timer_.IsRunning()) {
-      scrolling_timer_.Start(FROM_HERE, base::Milliseconds(30), this,
+      scrolling_timer_.Start(FROM_HERE, base::Hertz(60), this,
                              &MenuScrollTask::Run);
     }
   }
@@ -485,15 +485,15 @@ class MenuController::MenuScrollTask {
   }
 
   // The menu being scrolled. Returns null if not scrolling.
-  SubmenuView* submenu() const { return submenu_; }
+  const SubmenuView* submenu() const { return submenu_; }
 
  private:
   void Run() {
-    DCHECK(submenu_);
+    CHECK(submenu_);
     gfx::Rect vis_rect = submenu_->GetVisibleBounds();
-    const int delta_y = static_cast<int>(
-        (base::Time::Now() - start_scroll_time_).InMilliseconds() *
-        pixels_per_second_ / 1000);
+    const int delta_y =
+        base::ClampRound((base::Time::Now() - start_scroll_time_).InSecondsF() *
+                         pixels_per_second_);
     vis_rect.set_y(is_scrolling_up_
                        ? std::max(0, start_y_ - delta_y)
                        : std::min(submenu_->height() - vis_rect.height(),
