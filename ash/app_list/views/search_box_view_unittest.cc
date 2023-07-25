cd6e8edccc24a6c4caef0de61471097b76cf7a0c
Jeffrey Young <cowmoo@google.com>
2023-07-25 22:56:25 +0000

    jelly: SearchBoxViewTest enable kJelly
    Update color assertions with kJelly feature on.
    
    Bug: b:289462348
    Test: ash_unittests --gtest_filter=*SearchBox*
    Change-Id: I9672fcefc8844a450b3ae5c9e107f5f531ea2bca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4717558
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175130}

--- ash/app_list/views/search_box_view_unittest.cc
+++ ash/app_list/views/search_box_view_unittest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/search_box/search_box_constants.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/style/ash_color_id.h"
 #include "ash/style/ash_color_mixer.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/test/ash_test_base.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/ime/composition_text.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -121,7 +123,9 @@ class KeyPressCounterView : public ContentsView {
 class SearchBoxViewTest : public views::test::WidgetTest,
                           public SearchBoxViewDelegate {
  public:
-  SearchBoxViewTest() = default;
+  SearchBoxViewTest() {
+    scoped_feature_list_.InitAndEnableFeature(chromeos::features::kJelly);
+  }
 
   SearchBoxViewTest(const SearchBoxViewTest&) = delete;
   SearchBoxViewTest& operator=(const SearchBoxViewTest&) = delete;
@@ -250,6 +254,7 @@ class SearchBoxViewTest : public views::test::WidgetTest,
   void OnSearchBoxKeyEvent(ui::KeyEvent* event) override {}
   bool CanSelectSearchResults() override { return true; }
 
+  base::test::ScopedFeatureList scoped_feature_list_;
   AshColorProvider ash_color_provider_;
   raw_ptr<AppListSearchView, ExperimentalAsh> search_view_ = nullptr;
   AppListTestViewDelegate view_delegate_;
@@ -265,7 +270,8 @@ class SearchBoxViewTest : public views::test::WidgetTest,
 TEST_F(SearchBoxViewTest, SearchBoxTextUsesAppListSearchBoxTextColor) {
   // With darklight mode enabled by default, search box text color should be the
   // same with and without productivity launcher enabled.
-  EXPECT_EQ(view()->search_box()->GetTextColor(), gfx::kGoogleGrey900);
+  EXPECT_EQ(view()->search_box()->GetTextColor(),
+            view()->GetColorProvider()->GetColor(kColorAshTextColorPrimary));
 }
 
 // Tests that the close button is invisible by default.
@@ -301,7 +307,8 @@ TEST_F(SearchBoxViewTest, SearchBoxInactiveSearchBoxGoogle) {
   SetSearchEngineIsGoogle(true);
   SetSearchBoxActive(false, ui::ET_UNKNOWN);
   const gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
-      kGoogleBlackIcon, view()->GetSearchBoxIconSize(), gfx::kGoogleGrey900);
+      kGoogleBlackIcon, view()->GetSearchBoxIconSize(),
+      view()->GetColorProvider()->GetColor(kColorAshButtonIconColor));
 
   const gfx::ImageSkia actual_icon = view()->search_icon()->GetImage();
 
@@ -315,7 +322,7 @@ TEST_F(SearchBoxViewTest, SearchBoxActiveSearchEngineGoogle) {
   SetSearchBoxActive(true, ui::ET_MOUSE_PRESSED);
   const gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
       vector_icons::kGoogleColorIcon, view()->GetSearchBoxIconSize(),
-      gfx::kGoogleGrey900);
+      view()->GetColorProvider()->GetColor(kColorAshButtonIconColor));
 
   const gfx::ImageSkia actual_icon = view()->search_icon()->GetImage();
 
@@ -329,7 +336,7 @@ TEST_F(SearchBoxViewTest, SearchBoxInactiveSearchEngineNotGoogle) {
   SetSearchBoxActive(false, ui::ET_UNKNOWN);
   const gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
       kSearchEngineNotGoogleIcon, view()->GetSearchBoxIconSize(),
-      gfx::kGoogleGrey900);
+      view()->GetColorProvider()->GetColor(kColorAshButtonIconColor));
 
   const gfx::ImageSkia actual_icon = view()->search_icon()->GetImage();
 
@@ -343,7 +350,7 @@ TEST_F(SearchBoxViewTest, SearchBoxActiveSearchEngineNotGoogle) {
   SetSearchBoxActive(true, ui::ET_UNKNOWN);
   const gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
       kSearchEngineNotGoogleIcon, view()->GetSearchBoxIconSize(),
-      gfx::kGoogleGrey900);
+      view()->GetColorProvider()->GetColor(kColorAshButtonIconColor));
 
   const gfx::ImageSkia actual_icon = view()->search_icon()->GetImage();
 
@@ -665,9 +672,17 @@ class SearchBoxViewAutocompleteTest : public SearchBoxViewTest,
                                       public testing::WithParamInterface<bool> {
  public:
   SearchBoxViewAutocompleteTest() {
-    scoped_features_.InitWithFeatureState(
-        features::kAutocompleteExtendedSuggestions,
-        IsExtendedAutocompleteEnabled());
+    scoped_feature_list_.Reset();
+    scoped_feature_list_.InitWithFeatureStates({
+        {
+            features::kAutocompleteExtendedSuggestions,
+            IsExtendedAutocompleteEnabled(),
+        },
+        {
+            chromeos::features::kJelly,
+            true,
+        },
+    });
   }
   SearchBoxViewAutocompleteTest(const SearchBoxViewAutocompleteTest&) = delete;
   SearchBoxViewAutocompleteTest& operator=(
@@ -693,9 +708,6 @@ class SearchBoxViewAutocompleteTest : public SearchBoxViewTest,
     base::RunLoop().RunUntilIdle();
     ProcessAutocomplete();
   }
-
- private:
-  base::test::ScopedFeatureList scoped_features_;
 };
 
 // Instantiate the values in the parameterized tests. The boolean
