50564ba2acd4c68e1c31c03adf142879e3f0bfca
seannli <seannli@google.com>
2023-07-25 09:40:08 +0000

    CCA: Skip calling getUserMedia on conflicting device until released
    If all the cameras failed to open, the watchdog will keep trying to open
    the cameras. In this case, the notification in the tool bar is
    annoying. This CL adds a private api IsDeviceInUse() which can only be
    called by CCA to let CCA get if a device is in use. If NotReadableError
    is thrown and the device is in use, it means that the device is used by
    Lacros. In this case, the watchdog skips to call getUserMedia on the
    failing device until it is not in use.
    
    BUG: b:289773559
    Test: Manually by open Meet, then open CCA, finally close Meet and check
    the console.
    
    Change-Id: Id45c4c115d1bf79e0509beced5fd041bc9f620c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4688637
    Commit-Queue: Sean Li <seannli@google.com>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174725}

--- media/capture/video/chromeos/camera_app_device_provider_impl.h
+++ media/capture/video/chromeos/camera_app_device_provider_impl.h
@@ -43,6 +43,8 @@ class CAPTURE_EXPORT CameraAppDeviceProviderImpl
       const std::string& device_id,
       bool enabled,
       SetVirtualDeviceEnabledCallback callback) override;
+  void IsDeviceInUse(const std::string& source_id,
+                     IsDeviceInUseCallback callback) override;
 
  private:
   void GetCameraAppDeviceWithDeviceId(
@@ -53,6 +55,8 @@ class CAPTURE_EXPORT CameraAppDeviceProviderImpl
       bool enable,
       SetVirtualDeviceEnabledCallback callback,
       const absl::optional<std::string>& device_id);
+  void IsDeviceInUseWithDeviceId(IsDeviceInUseCallback callback,
+                                 const absl::optional<std::string>& device_id);
 
   mojo::Remote<cros::mojom::CameraAppDeviceBridge> bridge_;
 
