3df1827c5f49f83710b1559baafa2e074f1fe2a3
Menghan YANG <myuu@google.com>
2023-07-25 13:22:22 +0000

    [iOS][FRE] Show generic sign-in subtitle if sync disabled
    The default sign-in subtitle string should be shown instead of the
    one showing sync benefit, if sync or any sync type is disabled by
    policy.
    
    This CL updates the display rules, and adds EGTest to ensure the
    subtitle is correct.
    
    Bug: 1450864
    Change-Id: I345ba48b86e82beee20e64b9cdd231d40b0954a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705228
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174802}

--- ios/chrome/browser/ui/first_run/signin/signin_screen_mediator.mm
+++ ios/chrome/browser/ui/first_run/signin/signin_screen_mediator.mm
@@ -245,6 +245,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       _consumer.signinStatus = SigninScreenConsumerSigninStatusDisabled;
       break;
   }
+  _consumer.syncEnabled = !IsSyncDisabledByPolicy(_syncService) &&
+                          !HasManagedSyncDataType(_syncService);
   self.consumer.isManaged = IsApplicationManagedByPlatform();
   if (!self.firstRun) {
     self.consumer.screenIntent = SigninScreenConsumerScreenIntentSigninOnly;
