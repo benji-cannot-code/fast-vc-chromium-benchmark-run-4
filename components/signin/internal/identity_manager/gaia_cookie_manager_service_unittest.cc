66425c818802a30d32646ee5b33a47298a9162bd
Tanmoy Mollik <triploblastic@chromium.org>
2023-07-25 08:28:03 +0000

    [FlakyTest] Fix GaiaCookieManagerServiceTest.ContinueAfterSuccess
    This cl fixes this test by setting up java environment to create
    account tracker service when this test is run on android device.
    
    Fixed: 1466586
    Change-Id: I2f9984b2e6c65dfd9a010c36697f2bc701602309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4709454
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174690}

--- components/signin/internal/identity_manager/gaia_cookie_manager_service_unittest.cc
+++ components/signin/internal/identity_manager/gaia_cookie_manager_service_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/internal/identity_manager/fake_profile_oauth2_token_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/base/test_signin_client.h"
+#include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "net/cookies/canonical_cookie.h"
@@ -155,7 +156,8 @@ class GaiaCookieManagerServiceTest : public testing::Test {
         account_id4_(CoreAccountId::FromGaiaId(kAccountId4)),
         no_error_(GoogleServiceAuthError::NONE),
         error_(GoogleServiceAuthError::SERVICE_ERROR),
-        canceled_(GoogleServiceAuthError::REQUEST_CANCELED) {
+        canceled_(GoogleServiceAuthError::REQUEST_CANCELED),
+        account_tracker_service_(CreateAccountTrackerService()) {
     AccountTrackerService::RegisterPrefs(pref_service_.registry());
     GaiaCookieManagerService::RegisterPrefs(pref_service_.registry());
     signin_client_ = std::make_unique<TestSigninClient>(&pref_service_);
@@ -269,6 +271,13 @@ class GaiaCookieManagerServiceTest : public testing::Test {
   const CoreAccountId account_id4_;
 
  private:
+  std::unique_ptr<AccountTrackerService> CreateAccountTrackerService() {
+#if BUILDFLAG(IS_ANDROID)
+    signin::SetUpMockAccountManagerFacade();
+#endif
+    return std::make_unique<AccountTrackerService>();
+  }
+
   base::test::TaskEnvironment task_environment_;
   GoogleServiceAuthError no_error_;
   GoogleServiceAuthError error_;
@@ -402,12 +411,7 @@ TEST_F(GaiaCookieManagerServiceTest, FailedUbertoken) {
   SimulateUbertokenFailure(&helper, error());
 }
 
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_ContinueAfterSuccess DISABLED_ContinueAfterSuccess
-#else
-#define MAYBE_ContinueAfterSuccess ContinueAfterSuccess
-#endif
-TEST_F(GaiaCookieManagerServiceTest, MAYBE_ContinueAfterSuccess) {
+TEST_F(GaiaCookieManagerServiceTest, ContinueAfterSuccess) {
   InstrumentedGaiaCookieManagerService helper(account_tracker_service(),
                                               token_service(), signin_client());
   MockObserver observer(&helper);
