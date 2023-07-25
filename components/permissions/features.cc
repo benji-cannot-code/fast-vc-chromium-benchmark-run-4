0b363990bb7c302ef25d5297ad19e97aed167d85
Kamila <hkamila@google.com>
2023-07-25 10:53:12 +0000

    Add Permission Element Feature
    Implementation general feature for the Permission Element project.
    Disabled by default.
    
    Bug: 1462930
    Change-Id: If3314b31fabe5159343e455e7b55a61cf15fffe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4712305
    Auto-Submit: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174750}

--- components/permissions/features.cc
+++ components/permissions/features.cc
@@ -48,6 +48,10 @@ BASE_FEATURE(kChipLocationBarIconOverride,
              "ChipLocationIconOverride",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kPermissionElement,
+             "PermissionElement",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kNotificationInteractionHistory,
              "NotificationInteractionHistory",
              base::FEATURE_ENABLED_BY_DEFAULT);
