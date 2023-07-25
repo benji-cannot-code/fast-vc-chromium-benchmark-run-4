0ea576263dbcf81612dbac16fd50d61c2cf15401
Brahim Chikhaoui <bchikhaoui@google.com>
2023-07-25 17:49:59 +0000

    [OOBE] UserCreationScreenSoftwareUpdateTest SignInForSelf fix browser_test
    fix the browser_test after adding the consumerUpdate screen
    
    Bug: b:292904894
    Change-Id: I1f707964fc0d70ede77c2d37a5beaecd1f74064a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4711735
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174930}

--- chrome/browser/ash/login/screens/user_creation_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/user_creation_screen_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/ash/login/ui/login_display_host.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
+#include "chrome/browser/ui/webui/ash/login/consumer_update_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/error_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/gaia_info_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/gaia_screen_handler.h"
@@ -244,21 +245,13 @@ class UserCreationScreenSoftwareUpdateTest : public UserCreationScreenTest {
   base::test::ScopedFeatureList feature_list_;
 };
 
-// TODO(crbug.com/1467313): Disabled due to flakiness.
-// Verify flow for setting up the device for self.
-IN_PROC_BROWSER_TEST_F(UserCreationScreenSoftwareUpdateTest,
-                       DISABLED_SignInForSelf) {
+IN_PROC_BROWSER_TEST_F(UserCreationScreenSoftwareUpdateTest, SignInForSelf) {
   SelectUserTypeOnUserCreationScreen(kSelfButton);
   WaitForScreenExit();
   EXPECT_FALSE(LoginDisplayHost::default_host()
                    ->GetWizardContextForTesting()
                    ->sign_in_as_child);
   EXPECT_EQ(screen_result_.value(), UserCreationScreen::Result::SIGNIN);
-  if (features::IsOobeGaiaInfoScreenEnabled()) {
-    OobeScreenWaiter(GaiaInfoScreenView::kScreenId).Wait();
-  } else {
-    OobeScreenWaiter(GaiaView::kScreenId).Wait();
-  }
 }
 
 // Verify that google account in the child setup step in user creation
