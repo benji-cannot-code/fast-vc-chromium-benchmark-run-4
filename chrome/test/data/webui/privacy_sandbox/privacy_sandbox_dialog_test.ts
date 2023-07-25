7904006a4d054444074a15651adb06f9828ab6f0
dpapad <dpapad@chromium.org>
2023-07-25 08:14:46 +0000

    WebUI: Migrate privacy sandbox dialog tests to WebUIMochaBrowserTest.
    WebUIMochaBrowserTest is the new way of running WebUI tests without any
    dependency to the obsolete js2gtest and test_api.js mechanism.
    
    Bug: 1457360
    Change-Id: I2dbeea01b73361ae3fd3dacab75f7936e7084555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4709741
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174687}

--- chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_test.ts
+++ chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_test.ts
@@ -96,7 +96,7 @@ function testClickButton(buttonSelector: string, element: HTMLElement|null) {
   actionButton.click();
 }
 
-suite('PrivacySandboxDialogConsent', function() {
+suite('Consent', function() {
   let page: PrivacySandboxDialogAppElement;
   let browserProxy: TestPrivacySandboxDialogBrowserProxy;
 
@@ -192,7 +192,7 @@ suite('PrivacySandboxDialogConsent', function() {
   });
 });
 
-suite('PrivacySandboxDialogNotice', function() {
+suite('Notice', function() {
   let page: PrivacySandboxDialogAppElement;
   let browserProxy: TestPrivacySandboxDialogBrowserProxy;
 
@@ -249,7 +249,7 @@ suite('PrivacySandboxDialogNotice', function() {
   });
 });
 
-suite('PrivacySandboxDialogCombined', function() {
+suite('Combined', function() {
   let page: PrivacySandboxCombinedDialogAppElement;
   let browserProxy: TestPrivacySandboxDialogBrowserProxy;
 
@@ -469,7 +469,7 @@ suite('PrivacySandboxDialogCombined', function() {
   });
 });
 
-suite('PrivacySandboxDialogNoticeEEA', function() {
+suite('NoticeEEA', function() {
   let page: PrivacySandboxCombinedDialogAppElement;
   let browserProxy: TestPrivacySandboxDialogBrowserProxy;
 
@@ -607,7 +607,7 @@ suite('PrivacySandboxDialogNoticeEEA', function() {
   });
 });
 
-suite('PrivacySandboxDialogNoticeROW', function() {
+suite('NoticeROW', function() {
   let page: PrivacySandboxNoticeDialogAppElement;
   let browserProxy: TestPrivacySandboxDialogBrowserProxy;
 
@@ -728,7 +728,7 @@ suite('PrivacySandboxDialogNoticeROW', function() {
   });
 });
 
-suite('PrivacySandboxDialogNoticeRestricted', function() {
+suite('NoticeRestricted', function() {
   let page: PrivacySandboxNoticeRestrictedDialogAppElement;
   let browserProxy: TestPrivacySandboxDialogBrowserProxy;
 
@@ -836,7 +836,7 @@ suite('PrivacySandboxDialogNoticeRestricted', function() {
   });
 });
 
-suite('PrivacySandboxDialogMixin', function() {
+suite('Mixin', function() {
   const TestElementBase = PrivacySandboxDialogMixin(PolymerElement);
 
   // Create a test element to have more control over size of the element and
