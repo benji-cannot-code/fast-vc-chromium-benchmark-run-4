1da28401377d5ca0e7d41b18369da65a759e0431
Salvador Guerrero <salg@google.com>
2023-07-25 22:17:45 +0000

    Disabled test in AwMetricsIntegrationTest
    This CL disables the following test:
    AwMetricsIntegrationTest#testPageLoadsEnableMultipleUploads
    
    Bug: 1467656
    Change-Id: I29c5d3a0f56e04dea091c09e4d0cb4f7432545c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718492
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Owners-Override: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175111}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwMetricsIntegrationTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwMetricsIntegrationTest.java
@@ -36,6 +36,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.metrics.AndroidMetricsLogUploader;
 import org.chromium.components.metrics.AndroidMetricsServiceClient;
@@ -479,6 +480,7 @@ public class AwMetricsIntegrationTest {
     @Test
     @MediumTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "https://crbug.com/1467656")
     public void testPageLoadsEnableMultipleUploads() throws Throwable {
         mPlatformServiceBridge.waitForNextMetricsLog();
 
