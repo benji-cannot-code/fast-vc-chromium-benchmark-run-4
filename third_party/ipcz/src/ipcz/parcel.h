e6c7921768c43bbcfc3c4d187b0d0975c7edac61
Ken Rockot <rockot@google.com>
2023-07-25 15:33:37 +0000

    ipcz: Fix stalling proxy bypass
    Due to a trivial logic inversion bug, if we need to allocate a new
    RouterLinkState at a certain stage of the proxy bypass flow, the bypass
    will stall out with the central link stuck in a locked state.
    
    A pipe stuck in this state can never communicate endpoint closure from
    one end to the other and can never eliminate proxies which accumulate
    along its route. The net result is probably bad performance, and
    definitely soft pipe leaks and other application-level stalling
    behavior (see linked fetch bug).
    
    Fixed: 1426471
    Change-Id: I6541f9d9cb2c735dc153393b3fd5fe2fd788a4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708353
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174868}

--- third_party/ipcz/src/ipcz/parcel.h
+++ third_party/ipcz/src/ipcz/parcel.h
@@ -160,7 +160,7 @@ class Parcel {
     std::atomic<uint32_t> size;
 
     // Reserved padding for 8-byte parcel data alignment.
-    uint32_t reserved;
+    std::atomic<uint32_t> reserved;
   };
 
   // Holds a Fragment and a reference to its backing memory. Used when a
