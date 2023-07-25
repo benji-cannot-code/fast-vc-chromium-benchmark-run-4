f58b9373e8d908f5337b48716cb006f3c04089c6
Scott Yoder <scottyoder@google.com>
2023-07-25 21:32:51 +0000

    [ios] Fix crash related to ContentSuggestionsMetricsRecorder
    References to this object should be either weak or strong in order to
    avoid a crash when using it after it is dealloc'd.
    
    Fixed: 1465028
    Change-Id: Ie3ef9150b6bb003e9bb9936c7f5e9bbc9732bbcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718725
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175084}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_favicon_mediator.h
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_favicon_mediator.h
@@ -48,7 +48,7 @@ class LargeIconCache;
 - (void)fetchFaviconForMostVisited:(ContentSuggestionsMostVisitedItem*)item;
 
 // Recorder for content suggestions metrics.
-@property(nonatomic, assign)
+@property(nonatomic, weak)
     ContentSuggestionsMetricsRecorder* contentSuggestionsMetricsRecorder;
 
 @end
