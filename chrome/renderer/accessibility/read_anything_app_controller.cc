06d7c2dc0a7c0df292f3da6cc8db3059ac00075b
Eitan Goldberger <eitang@google.com>
2023-07-25 20:14:26 +0000

    Change isWebUIToolbarEnabled function name to conform to C++ naming conventions
    Bug: 1465029
    Change-Id: Idf2aa813360cad45a4ecd168d871208306922c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718205
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175039}

--- chrome/renderer/accessibility/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything_app_controller.cc
@@ -591,7 +591,7 @@ gin::ObjectTemplateBuilder ReadAnythingAppController::GetObjectTemplateBuilder(
       .SetProperty("letterSpacing", &ReadAnythingAppController::LetterSpacing)
       .SetProperty("lineSpacing", &ReadAnythingAppController::LineSpacing)
       .SetProperty("isWebUIToolbarVisible",
-                   &ReadAnythingAppController::isWebUIToolbarEnabled)
+                   &ReadAnythingAppController::IsWebUIToolbarEnabled)
       .SetProperty("isSelectable", &ReadAnythingAppController::IsSelectable)
       .SetMethod("getChildren", &ReadAnythingAppController::GetChildren)
       .SetMethod("getTextDirection",
@@ -769,7 +769,7 @@ bool ReadAnythingAppController::IsSelectable() const {
   return model_.active_tree_selectable();
 }
 
-bool ReadAnythingAppController::isWebUIToolbarEnabled() const {
+bool ReadAnythingAppController::IsWebUIToolbarEnabled() const {
   return features::IsReadAnythingWebUIToolbarEnabled();
 }
 
