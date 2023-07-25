850f9ce469475e6ac37a46c13fafe3698c4b209b
David Pennington <dpenning@chromium.org>
2023-07-25 20:56:43 +0000

    [ChromeRefresh2023] Extend tab shape when tabs are small to fit favicon.
    In CR23, the tab separator and tab separator margins are increasing for
    visual appeal, but in the minimum tab width scenario, too much space was
    being taken from the tab contents in order to render separation. To fix
    this, the separation is reduced to 1 px when small tabs are needed. This
    occurs when the contents region shrinks to less than the size of a
    favicon.
    
    Bug: 1466501
    Change-Id: Ib00505479b6c11818793c3d58a44b970a3ae2212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705845
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1175065}

--- chrome/browser/ui/views/tabs/tab_style_views.cc
+++ chrome/browser/ui/views/tabs/tab_style_views.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/theme_provider.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/favicon_size.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 #include "ui/gfx/scoped_canvas.h"
@@ -1143,7 +1144,8 @@ SkPath ChromeRefresh2023TabStyleViews::GetPath(
   // and inactive tab fills are a detached squarcle tab.
   if ((path_type == TabStyle::PathType::kFill &&
        state != TabStyle::TabSelectionState::kActive) ||
-      (path_type == TabStyle::PathType::kHighlight)) {
+      path_type == TabStyle::PathType::kHighlight ||
+      path_type == TabStyle::PathType::kInteriorClip) {
     // TODO (crbug.com/1451400): This constant should be unified with
     // kCRtabstripRegionViewControlPadding in tab_strip_region_view.
     constexpr int kChromeRefreshDetachedTabBottomPadding = 6;
@@ -1157,17 +1159,32 @@ SkPath ChromeRefresh2023TabStyleViews::GetPath(
         GetTopCornerRadiusForWidth(tab()->width()) * scale;
     const float extension_corner_radius =
         tab_style()->GetBottomCornerRadius() * scale;
-    const float tab_height =
-        (tab_style()->GetHeight() - kChromeRefreshDetachedTabBottomPadding) *
-        scale;
+    float tab_height = tab_style()->GetHeight() * scale;
 
-    SkPath path;
+    // The tab displays favicon animations that can emerge from the toolbar. The
+    // interior clip needs to extend the entire height of the toolbar to support
+    // this. Detached tab shapes do not need to respect this.
+    if (path_type != TabStyle::PathType::kInteriorClip) {
+      tab_height -= kChromeRefreshDetachedTabBottomPadding * scale;
+    }
 
-    const int left = aligned_bounds.x() + extension_corner_radius;
+    int left = aligned_bounds.x() + extension_corner_radius;
     const int top = aligned_bounds.y();
-    const int right = aligned_bounds.right() - extension_corner_radius;
+    int right = aligned_bounds.right() - extension_corner_radius;
     const int bottom = top + tab_height;
 
+    // If the width is less than the favicon size, the separator margins are
+    // shrunk so that there is exactly 1 pixel of margins.
+    constexpr int kMinWidthSeparatorMarginPx = 1;
+    if ((right - left) < (gfx::kFaviconSize * scale)) {
+      // extend the interior clip to the separators by exactly 1 pixel.
+      left -= (tab_style()->GetSeparatorMargins().left() * scale) -
+              kMinWidthSeparatorMarginPx;
+      right += (tab_style()->GetSeparatorMargins().right() * scale) -
+               kMinWidthSeparatorMarginPx;
+    }
+
+    SkPath path;
     SkRRect rrect =
         SkRRect::MakeRectXY(SkRect::MakeLTRB(left, top, right, bottom),
                             content_corner_radius, content_corner_radius);
