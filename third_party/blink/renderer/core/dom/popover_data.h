db9858178f5d946225f744f1ec18f0facc7f91f1
Joey Arhar <jarhar@chromium.org>
2023-07-25 01:43:32 +0000

    Implement CloseWatcher integration for popovers
    This is being integrated in this HTML spec PR which adds CloseWatchers:
    https://github.com/whatwg/html/pull/9462
    
    CloseWatcher is not enabled right now, so this patch should not make any
    behavior changes to stable chrome.
    
    This patch makes it so that pressing the escape key uses the
    document/window's closewatcher stack to find a popover to light dismiss
    rather than HandlePopoverLightDismiss hiding whatever the
    TopmostPopoverOrHint is. Hopefully they agree with each other.
    
    The behavior change for popovers is that the android back button can
    close popovers, and this gives popovers the
    grouping-without-user-activation behavior that dialogs have (with the
    flag enabled). If you open 5 popovers with a single user activation,
    then any close signal (including Esc on desktop) will close all 5 of
    those popovers, not just the topmost one.
    
    Bug: 1171318
    Change-Id: I73740c3eb9c16b0883862ae259be4867b824a79c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4686066
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174590}

--- third_party/blink/renderer/core/dom/popover_data.h
+++ third_party/blink/renderer/core/dom/popover_data.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/element_rare_data_field.h"
+#include "third_party/blink/renderer/core/html/closewatcher/close_watcher.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_menu_element.h"
 #include "third_party/blink/renderer/core/html_element_type_helpers.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
@@ -118,11 +119,17 @@ class PopoverData final : public GarbageCollected<PopoverData>,
     owner_select_menu_element_ = element;
   }
 
+  CloseWatcher* closeWatcher() { return close_watcher_; }
+  void setCloseWatcher(CloseWatcher* close_watcher) {
+    close_watcher_ = close_watcher;
+  }
+
   void Trace(Visitor* visitor) const override {
     visitor->Trace(invoker_);
     visitor->Trace(previously_focused_element_);
     visitor->Trace(hover_show_tasks_);
     visitor->Trace(owner_select_menu_element_);
+    visitor->Trace(close_watcher_);
     ElementRareDataField::Trace(visitor);
   }
 
@@ -148,6 +155,8 @@ class PopoverData final : public GarbageCollected<PopoverData>,
   TaskHandle hover_hide_task_;
 
   WeakMember<HTMLSelectMenuElement> owner_select_menu_element_;
+
+  Member<CloseWatcher> close_watcher_;
 };
 
 }  // namespace blink
