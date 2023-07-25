ccb5d272c61dae88ffbf15407071510cf76ca211
Mingyu Lei <leimy@chromium.org>
2023-07-25 18:26:00 +0000

    Use SafeRef for BFCache restoration's RFH
    In crrev.com/c/4401995 and crrev.com/c/4634965 we ensure that the
    BFCache NavigationRequest will never outlive the speculative RFH.
    So the `rfh_restored_from_back_forward_cache_` should never be
    invalidated once set.
    
    This CL changes the type of `rfh_restored_from_back_forward_cache_`
    from WeakPtr to optional<SafeRef>, so the caller can claim that it's
    either null since the construction, or is currently referencing to a
    valid RFH.
    
    Bug: 1430653
    Change-Id: Ic9b4513248fbbda03108d49e2c5ebd55955f3f92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707476
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174960}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -718,9 +718,9 @@ class CONTENT_EXPORT NavigationRequest
   // default value if |timeout| is zero.
   static void SetCommitTimeoutForTesting(const base::TimeDelta& timeout);
 
-  RenderFrameHostImpl* rfh_restored_from_back_forward_cache() {
-    return rfh_restored_from_back_forward_cache_.get();
-  }
+  // Returns the `rfh_restored_from_back_forward_cache_` if the navigation is
+  // a BFCache restore, or nullptr otherwise.
+  RenderFrameHostImpl* GetRenderFrameHostRestoredFromBackForwardCache() const;
 
   // The NavigatorDelegate to notify/query for various navigation events. This
   // is always the WebContents.
@@ -1283,7 +1283,8 @@ class CONTENT_EXPORT NavigationRequest
       mojo::PendingAssociatedRemote<mojom::NavigationClient> navigation_client,
       scoped_refptr<PrefetchedSignedExchangeCache>
           prefetched_signed_exchange_cache,
-      base::WeakPtr<RenderFrameHostImpl> rfh_restored_from_back_forward_cache,
+      absl::optional<base::SafeRef<RenderFrameHostImpl>>
+          rfh_restored_from_back_forward_cache,
       int initiator_process_id,
       bool was_opener_suppressed,
       bool is_pdf,
@@ -2255,10 +2256,11 @@ class CONTENT_EXPORT NavigationRequest
   // modified during the redirect phase.
   std::vector<std::string> removed_request_headers_;
 
-  // A WeakPtr for the RenderFrameHost that is being restored from the
-  // back/forward cache. This can be null if this navigation is not restoring a
-  // page from the back/forward cache.
-  base::WeakPtr<RenderFrameHostImpl> rfh_restored_from_back_forward_cache_;
+  // The RenderFrameHost that is being restored from the back/forward cache.
+  // This can be null if this navigation is not restoring a page from the
+  // back/forward cache.
+  absl::optional<base::SafeRef<RenderFrameHostImpl>>
+      rfh_restored_from_back_forward_cache_;
 
   // Whether the navigation is for restoring a page from the back/forward cache
   // or not.
