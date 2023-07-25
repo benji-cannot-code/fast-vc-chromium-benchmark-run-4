a3eb1bd0e34001f27d2ffffd747e308275f054c5
Wayne Jackson Jr <wbjacksonjr@google.com>
2023-07-25 16:01:36 +0000

    [android] Catch Resources Exception for Selection Menu Item
    There is a minor crash occurring here on some devices running WebView.
    In this case we should catch the Resources.NotFoundException and return
    a null icon for the selection menu items.
    
    Bug: 1454900
    Change-Id: Iac7093d789c0ef47409e4c2f2b168fef86b493fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4711812
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174882}

--- content/public/android/java/src/org/chromium/content_public/browser/SelectionMenuItem.java
+++ content/public/android/java/src/org/chromium/content_public/browser/SelectionMenuItem.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content_public.browser;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
@@ -74,12 +75,16 @@ public final class SelectionMenuItem implements Comparable<SelectionMenuItem> {
     @Nullable
     public Drawable getIcon(Context context) {
         if (mIconAttr != 0) {
-            TypedArray a = context.obtainStyledAttributes(new int[] {mIconAttr});
-            int iconResId = a.getResourceId(0, 0);
-            Drawable icon =
-                    iconResId == 0 ? null : AppCompatResources.getDrawable(context, iconResId);
-            a.recycle();
-            return icon;
+            try {
+                TypedArray a = context.obtainStyledAttributes(new int[] {mIconAttr});
+                int iconResId = a.getResourceId(0, 0);
+                Drawable icon =
+                        iconResId == 0 ? null : AppCompatResources.getDrawable(context, iconResId);
+                a.recycle();
+                return icon;
+            } catch (Resources.NotFoundException e) {
+                return null;
+            }
         }
         return mIcon;
     }
