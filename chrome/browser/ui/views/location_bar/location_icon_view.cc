a60a0705799f5e19de8c8caa24ef9ea2e3db511f
Shibalik Mohapatra <shibalik@chromium.org>
2023-07-25 19:02:08 +0000

    [Refresh] Update the background color of Super G icon to be white.
    This change also calls GetText() for the dangerous chip which does additional checks like if the ominbox is editing or empty.
    
    Bug: 1462706
    Change-Id: I9da70bed3a7ea45f50538edcef0e4a6d19ac7ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4716761
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174999}

--- chrome/browser/ui/views/location_bar/location_icon_view.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_icon_view.h"
 
 #include "base/functional/bind.h"
+#include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_ui_util.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
@@ -33,8 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/color/color_id.h"
+#include "ui/color/color_provider_utils.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/vector_icon_types.h"
 #include "ui/views/animation/flood_fill_ink_drop_ripple.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/animation/ink_drop_highlight.h"
@@ -44,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/style/platform_style.h"
 #include "ui/views/view_class_properties.h"
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#include "components/vector_icons/vector_icons.h"  // nogncheck
+#endif
 
 using content::WebContents;
 using security_state::SecurityLevel;
@@ -84,8 +91,7 @@ bool LocationIconView::OnMouseDragged(const ui::MouseEvent& event) {
 }
 
 SkColor LocationIconView::GetForegroundColor() const {
-  const std::u16string& display_text =
-      delegate_->GetLocationBarModel()->GetSecureDisplayText();
+  const std::u16string& display_text = GetText();
   const bool is_text_dangerous =
       display_text == l10n_util::GetStringUTF16(IDS_DANGEROUS_VERBOSE_STATE);
 
@@ -265,6 +271,22 @@ void LocationIconView::UpdateIcon() {
   ui::ImageModel icon = delegate_->GetLocationIcon(
       base::BindOnce(&LocationIconView::OnIconFetched,
                      icon_fetch_weak_ptr_factory_.GetWeakPtr()));
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+
+  if (OmniboxFieldTrial::IsChromeRefreshIconsEnabled()) {
+    bool has_custom_theme =
+        this->GetWidget() && this->GetWidget()->GetCustomTheme();
+
+    if (has_custom_theme && !icon.IsEmpty() && icon.IsVectorIcon() &&
+        icon.GetVectorIcon().vector_icon()->name ==
+            vector_icons::kGoogleSuperGIcon.name) {
+      SetBackground(
+          views::CreateRoundedRectBackground(SK_ColorWHITE, height() / 2));
+    }
+  }
+#endif
+
   if (!icon.IsEmpty())
     SetImageModel(icon);
 }
@@ -272,8 +294,7 @@ void LocationIconView::UpdateIcon() {
 void LocationIconView::UpdateBackground() {
   if (OmniboxFieldTrial::IsChromeRefreshIconsEnabled()) {
     CHECK(GetColorProvider());
-    const std::u16string& display_text =
-        delegate_->GetLocationBarModel()->GetSecureDisplayText();
+    const std::u16string& display_text = GetText();
     const bool is_text_dangerous =
         display_text == l10n_util::GetStringUTF16(IDS_DANGEROUS_VERBOSE_STATE);
 
