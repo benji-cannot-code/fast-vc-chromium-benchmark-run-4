e43067b512d429beab901be7c8a4f952d751758e
Colin Blundell <blundell@chromium.org>
2023-07-25 12:41:30 +0000

    [//gpu] Move GraphiteImageProvider into its own file
    Precursor to adding significant functionality to this class for
    caching and hopefully adding unittests of it as well.
    
    Bug: 1457525
    Change-Id: I68e7ad2356026ff12e14093be866f70621e74a66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714887
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174789}

---
+++ gpu/command_buffer/service/graphite_image_provider.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/command_buffer/service/graphite_image_provider.h"
+#include "third_party/skia/include/gpu/graphite/Image.h"
+
+namespace gpu {
+
+sk_sp<SkImage> GraphiteImageProvider::findOrCreate(
+    skgpu::graphite::Recorder* recorder,
+    const SkImage* image,
+    SkImage::RequiredProperties requiredProps) {
+  return SkImages::TextureFromImage(recorder, image, requiredProps);
+}
+
+}  // namespace gpu
