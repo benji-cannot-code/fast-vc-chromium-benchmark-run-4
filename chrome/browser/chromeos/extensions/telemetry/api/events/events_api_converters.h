8a8c7febd139d1fe142ab5356b991b37f0164889
Ethan Cheng <yycheng@google.com>
2023-07-25 05:42:06 +0000

    [Telemetry] Rename `Hdmi` event to `ExternalDisplay` event
    Rename mojo and relevant code blocks from Hdmi to ExternalDisplay.
    
    BUG=b:291902295
    TEST=1.autoninja -C out/Default unit_tests && testing/xvfb.py out/Default/unit_tests --gtest_filter="*Telemetry*Event*" &&
         2.autoninja -C out/Default extensions_unittests && out/Default/extensions_unittests -ozone-platform=headless --gtest_filter="*TelemetryExtension*" &&
         3.autoninja -C out/Default browser_tests && out/Default/browser_tests -ozone-platform=headless --gtest_filter="*TelemetryExtensionEventsApi*"
    
    Change-Id: I930ebc25dbea0b8d3dff6867160667439578228e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4703807
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174644}

--- chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters.h
+++ chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters.h
@@ -35,8 +35,8 @@ api::os_events::LidEventInfo UncheckedConvertPtr(
 api::os_events::UsbEventInfo UncheckedConvertPtr(
     crosapi::mojom::TelemetryUsbEventInfoPtr ptr);
 
-api::os_events::HdmiEventInfo UncheckedConvertPtr(
-    crosapi::mojom::TelemetryHdmiEventInfoPtr ptr);
+api::os_events::ExternalDisplayEventInfo UncheckedConvertPtr(
+    crosapi::mojom::TelemetryExternalDisplayEventInfoPtr ptr);
 
 api::os_events::SdCardEventInfo UncheckedConvertPtr(
     crosapi::mojom::TelemetrySdCardEventInfoPtr ptr);
@@ -103,8 +103,8 @@ api::os_events::LidEvent Convert(
 api::os_events::UsbEvent Convert(
     crosapi::mojom::TelemetryUsbEventInfo::State state);
 
-api::os_events::HdmiEvent Convert(
-    crosapi::mojom::TelemetryHdmiEventInfo::State state);
+api::os_events::ExternalDisplayEvent Convert(
+    crosapi::mojom::TelemetryExternalDisplayEventInfo::State state);
 
 api::os_events::SdCardEvent Convert(
     crosapi::mojom::TelemetrySdCardEventInfo::State state);
