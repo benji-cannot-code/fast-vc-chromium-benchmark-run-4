c33b7f079fc27ca72dfd8a6d0711d1247db55308
Ken Buchanan <kenrb@chromium.org>
2023-07-25 00:32:28 +0000

    [WebAuthn] CBOR-encode enclave get assertion requests
    This change:
    - Incorporates the JSON-encoded getAssertion request into the command
      sent to the service.
    - CBOR-encodes the request and makes the command's structure match what
      is in the protocol proposal.
    
    Low-Coverage-Reason: Prototype code
    Bug: 1459620
    Change-Id: Iab4ead2026592a82d5fa20f7e1292344b799907a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705949
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174562}

--- device/fido/enclave/enclave_authenticator.cc
+++ device/fido/enclave/enclave_authenticator.cc
@@ -79,8 +79,7 @@ void EnclaveAuthenticator::GetAssertion(CtapGetAssertionRequest request,
                    });
   CHECK(found_passkey_it != available_passkeys_.end());
 
-  BuildGetAssertionRequestBody(*found_passkey_it,
-                               CtapGetAssertionRequestToJson(request),
+  BuildGetAssertionRequestBody(*found_passkey_it, std::move(options.json),
                                &pending_request_body_);
   pending_get_assertion_callback_ = std::move(callback);
 
@@ -140,7 +139,7 @@ void EnclaveAuthenticator::OnResponseReceived(
     }
     std::string plaintext_json(plaintext.begin(), plaintext.end());
     auto decode_result =
-        AuthenticatorGetAssertionRequestFromJson(plaintext_json);
+        AuthenticatorGetAssertionResponseFromJson(plaintext_json);
     if (!decode_result.first) {
       FIDO_LOG(ERROR) << "Failed to parse decrypted JSON: "
                       << decode_result.second;
