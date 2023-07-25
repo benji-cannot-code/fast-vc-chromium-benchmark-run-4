f75494798a84725a7dc834d054f55f78551e5c05
Adam Rice <ricea@chromium.org>
2023-07-25 09:27:54 +0000

    Use InlinedVector in /components/visitedlink
    Replace usages of base::StackVector with absl::InlinedVector. This is
    not a pure rename, as the semantics are slightly different:
    
    1. Methods like push_back() which were accessed with the -> operator in
       StackVector are instead accessed with . with InlinedVector.
    
    See also discussion thread in cxx@chromium.org:
    https://groups.google.com/a/chromium.org/g/cxx/c/jTfqVfU-Ka0/m/caaal90NCgAJ
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1465736
    Change-Id: I7e96fc9dc078468ee87373902d8bd461bb6e1d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707232
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174721}

--- components/visitedlink/browser/visitedlink_writer.cc
+++ components/visitedlink/browser/visitedlink_writer.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/containers/stack_container.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
 #include "base/functional/bind.h"
@@ -30,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "third_party/abseil-cpp/absl/container/inlined_vector.h"
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -555,12 +555,12 @@ bool VisitedLinkWriter::DeleteFingerprint(Fingerprint fingerprint,
   // instead we just remove them all and re-add them (minus our deleted one).
   // This will mean there's a small window of time where the affected links
   // won't be marked visited.
-  base::StackVector<Fingerprint, 32> shuffled_fingerprints;
+  absl::InlinedVector<Fingerprint, 32> shuffled_fingerprints;
   Hash stop_loop = IncrementHash(end_range);  // The end range is inclusive.
   for (Hash i = deleted_hash; i != stop_loop; i = IncrementHash(i)) {
     if (hash_table_[i] != fingerprint) {
       // Don't save the one we're deleting!
-      shuffled_fingerprints->push_back(hash_table_[i]);
+      shuffled_fingerprints.push_back(hash_table_[i]);
 
       // This will balance the increment of this value in AddFingerprint below
       // so there is no net change.
@@ -569,10 +569,11 @@ bool VisitedLinkWriter::DeleteFingerprint(Fingerprint fingerprint,
     hash_table_[i] = null_fingerprint_;
   }
 
-  if (!shuffled_fingerprints->empty()) {
+  if (!shuffled_fingerprints.empty()) {
     // Need to add the new items back.
-    for (size_t i = 0; i < shuffled_fingerprints->size(); i++)
+    for (size_t i = 0; i < shuffled_fingerprints.size(); i++) {
       AddFingerprint(shuffled_fingerprints[i], false);
+    }
   }
 
   // Write the affected range to disk [deleted_hash, end_range].
