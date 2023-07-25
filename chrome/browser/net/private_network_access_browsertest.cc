35e814be8c735a9834917220421e3334fcfb0c48
Titouan Rigoudy <titouan@chromium.org>
2023-07-25 16:02:20 +0000

    [Private Network Access] Use initiator policies for nested navigations.
    This change fixes a longstanding bug in the PNA implementation for
    iframes. PNA checks apply to the fetch client's policies, not the
    parent frame's policies.
    
    Also introduce a couple new feature flags to control the rollout of
    PNA for iframes: warning-only mode and full enforcement. These replace
    the previous feature flag, which only applied to non-secure contexts.
    
    The feature flag checking logic is moved to live inside
    `DerivePrivateNetworkRequestPolicy()`, as is the case for worker flags.
    
    A couple virtual test suites are introduced to exercise the code paths
    behind the two new flags. These show that there remain bugs when the
    feature is entirely enabled: preflight requests do not succeed. This
    seems to be due to a strange interaction of CORS logic with PNA, where
    navigation requests are made in no-cors mode with credentials. Further
    work is needed to fix this, either by fixing the tests or by fixing
    CORS logic.
    
    Bug: chromium:1170335
    Change-Id: Iea67b39c46ff48edac3f51e9ccc63d4bac47b7e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945088
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Jonathan Hao <phao@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174883}

--- chrome/browser/net/private_network_access_browsertest.cc
+++ chrome/browser/net/private_network_access_browsertest.cc
@@ -273,6 +273,7 @@ class PrivateNetworkAccessWithFeatureEnabledBrowserTest
                 features::kBlockInsecurePrivateNetworkRequestsFromPrivate,
                 features::kBlockInsecurePrivateNetworkRequestsDeprecationTrial,
                 features::kPrivateNetworkAccessSendPreflights,
+                features::kPrivateNetworkAccessForIframes,
                 features::kPrivateNetworkAccessForWorkers,
                 dom_distiller::kReaderMode,
             },
@@ -476,7 +477,7 @@ IN_PROC_BROWSER_TEST_F(
 // about:blank, no address space feature is recorded. It serves as a basis for
 // comparison with the following tests, which test behavior with iframes.
 IN_PROC_BROWSER_TEST_F(
-    PrivateNetworkAccessWithFeatureEnabledBrowserTest,
+    PrivateNetworkAccessWithFeatureDisabledBrowserTest,
     DoesNotRecordAddressSpaceFeatureForChildAboutBlankNavigation) {
   WebFeatureHistogramTester feature_histogram_tester;
   std::unique_ptr<net::EmbeddedTestServer> server = NewServer();
@@ -500,7 +501,7 @@ IN_PROC_BROWSER_TEST_F(
 // This test verifies that when a non-secure context served from the public
 // address space loads a child frame from the private network, the correct
 // WebFeature is use-counted.
-IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
+IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureDisabledBrowserTest,
                        RecordsAddressSpaceFeatureForChildNavigation) {
   WebFeatureHistogramTester feature_histogram_tester;
   std::unique_ptr<net::EmbeddedTestServer> server = NewServer();
@@ -536,7 +537,7 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
 // address space loads a grand-child frame from the private network, the correct
 // WebFeature is use-counted. If inheritance did not work correctly, the
 // intermediate about:blank frame might confuse the address space logic.
-IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
+IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureDisabledBrowserTest,
                        RecordsAddressSpaceFeatureForGrandchildNavigation) {
   WebFeatureHistogramTester feature_histogram_tester;
   std::unique_ptr<net::EmbeddedTestServer> server = NewServer();
@@ -578,7 +579,7 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
 // navigation results in a private network request. Specifically, in this test
 // the document being navigated is not the one initiating the navigation (the
 // latter being the "remote initiator" referenced by the test name).
-IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
+IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureDisabledBrowserTest,
                        RecordsAddressSpaceFeatureForRemoteInitiatorNavigation) {
   WebFeatureHistogramTester feature_histogram_tester;
   std::unique_ptr<net::EmbeddedTestServer> server = NewServer();
@@ -610,7 +611,7 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
 // around by the time the navigation finishes, then no address space feature is
 // recorded, and importantly: the browser does not crash.
 IN_PROC_BROWSER_TEST_F(
-    PrivateNetworkAccessWithFeatureEnabledBrowserTest,
+    PrivateNetworkAccessWithFeatureDisabledBrowserTest,
     DoesNotRecordAddressSpaceFeatureForClosedInitiatorNavigation) {
   WebFeatureHistogramTester feature_histogram_tester;
   std::unique_ptr<net::EmbeddedTestServer> server = NewServer();
@@ -637,7 +638,7 @@ IN_PROC_BROWSER_TEST_F(
 // navigated itself by the time the navigation finishes, then no address space
 // feature is recorded.
 IN_PROC_BROWSER_TEST_F(
-    PrivateNetworkAccessWithFeatureEnabledBrowserTest,
+    PrivateNetworkAccessWithFeatureDisabledBrowserTest,
     DoesNotRecordAddressSpaceFeatureForMissingInitiatorNavigation) {
   WebFeatureHistogramTester feature_histogram_tester;
   std::unique_ptr<net::EmbeddedTestServer> server = NewServer();
@@ -1460,8 +1461,8 @@ class PrivateNetworkAccessAutoReloadBrowserTest
       : PrivateNetworkAccessBrowserTestBase(
             {
                 features::kBlockInsecurePrivateNetworkRequests,
-                features::kBlockInsecurePrivateNetworkRequestsForNavigations,
                 features::kBlockInsecurePrivateNetworkRequestsDeprecationTrial,
+                features::kPrivateNetworkAccessForIframes,
             },
             {}) {}
 
