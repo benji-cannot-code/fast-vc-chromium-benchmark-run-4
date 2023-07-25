5c43181f06f710b28ec811cee10b765efda53faa
Bastian Kersting <bkersting@google.com>
2023-07-25 14:55:22 +0000

    [Lacros] Skip owner check for ChromeOS on Linux
    Bug: 1467401
    Change-Id: Ie2e31eb6f9b98df7efd53d4ceac8e93f83c78c82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714847
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174847}

--- chrome/browser/ash/crosapi/device_ownership_waiter_impl.cc
+++ chrome/browser/ash/crosapi/device_ownership_waiter_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
+#include "base/system/sys_info.h"
 #include "chrome/browser/profiles/profiles_state.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/user_manager.h"
@@ -20,7 +21,7 @@ void DeviceOwnershipWaiterImpl::WaitForOwnerhipFetched(
     bool launching_at_login_screen) {
   if (launching_at_login_screen ||
       user_manager::UserManager::Get()->IsLoggedInAsGuest() ||
-      profiles::IsDemoSession()) {
+      profiles::IsDemoSession() || !base::SysInfo::IsRunningOnChromeOS()) {
     std::move(callback).Run();
     return;
   }
