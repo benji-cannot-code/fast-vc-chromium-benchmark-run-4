fccd0caab492fcfb0985cd8dc163a7c805879898
Max Ihlenfeldt <max@igalia.com>
2023-07-25 14:46:43 +0000

    [WebEngine] Fix non-Android build
    This adds a `ConfirmSaveIBANLocally()` implementation that has been
    missing since it was introduced in crrev.com/c/4014479, and uses
    `autofill::BrowserDriverInitHook` instead of `AndroidDriverInitHook` on
    non-Android platforms.
    
    Bug: None
    Change-Id: I0eadb69b8ec31c5d1b2b02358bf01d01b14bf783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4715321
    Commit-Queue: Max Ihlenfeldt <max@igalia.com>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174840}

--- weblayer/browser/autofill_client_impl.cc
+++ weblayer/browser/autofill_client_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "build/build_config.h"
-#include "components/android_autofill/browser/android_autofill_manager.h"
 #include "components/autofill/core/browser/autofill_download_manager.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/ui/popup_item_ids.h"
@@ -20,6 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "weblayer/browser/translate_client_impl.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "components/android_autofill/browser/android_autofill_manager.h"
+#else
+#include "weblayer/browser/i18n_util.h"
+#endif
 
 namespace weblayer {
 
@@ -181,6 +186,13 @@ void AutofillClientImpl::ShowLocalCardMigrationResults(
     const std::u16string& tip_message,
     const std::vector<autofill::MigratableCreditCard>& migratable_credit_cards,
     MigrationDeleteCardCallback delete_local_card_callback) {
+  NOTREACHED();
+}
+
+void AutofillClientImpl::ConfirmSaveIBANLocally(
+    const autofill::IBAN& iban,
+    bool should_show_prompt,
+    LocalSaveIBANPromptCallback callback) {
   NOTREACHED();
 }
 
@@ -387,7 +399,15 @@ void AutofillClientImpl::LoadRiskData(
 AutofillClientImpl::AutofillClientImpl(content::WebContents* web_contents)
     : autofill::ContentAutofillClient(
           web_contents,
-          base::BindRepeating(&autofill::AndroidDriverInitHook, this)),
-      content::WebContentsObserver(web_contents) {}
+#if BUILDFLAG(IS_ANDROID)
+          base::BindRepeating(&autofill::AndroidDriverInitHook, this)
+#else
+          base::BindRepeating(&autofill::BrowserDriverInitHook,
+                              this,
+                              i18n::GetApplicationLocale())
+#endif
+              ),
+      content::WebContentsObserver(web_contents) {
+}
 
 }  // namespace weblayer
