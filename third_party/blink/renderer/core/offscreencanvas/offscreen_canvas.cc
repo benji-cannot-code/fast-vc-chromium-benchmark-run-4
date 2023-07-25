bd713c749e3e22f602cc3675ccbdbb2155663478
François Beaufort <beaufort.francois@gmail.com>
2023-07-25 19:27:24 +0000

    Remove WebGPU blink runtime feature
    This CL removes the WebGPU blink runtime feature enabled by default
    since Chrome 113.
    Note that access to the GPU through WebGPU is gated on the WebGPUService
    feature exposing the GPU process access to the renderer process.
    
    Change-Id: Ib827ffad6f54ac08ca0a11f1b9f40237b5cd4d8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714856
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175011}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -310,7 +310,7 @@ CanvasRenderingContext* OffscreenCanvas::GetCanvasRenderingContext(
     return nullptr;
 
   CanvasRenderingContext::CanvasRenderingAPI rendering_api =
-      CanvasRenderingContext::RenderingAPIFromId(id, execution_context);
+      CanvasRenderingContext::RenderingAPIFromId(id);
 
   // Unknown type.
   if (rendering_api == CanvasRenderingContext::CanvasRenderingAPI::kUnknown)
