b2f731134685605a759ed0493120440d9daa7848
Vladimir Levin <vmpstr@chromium.org>
2023-07-25 18:20:15 +0000

    VT: Avoid optimization clips for VT element and subtrees (except root)
    This patch adds a propagated bool to avoid any optimization clips
    since the content clipped as an optimization may be necessary in
    view transitions, since the position and size of the paint may change
    when view transition pseudos are changed.
    
    R=wangxianzhu@chromium.org, khushalsagar@chromium.org
    
    Bug: 1463282
    Change-Id: If19bf402b4fd722f21f534c3dd1d8b59b14ed13c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4702705
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174953}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
@@ -1220,7 +1220,6 @@ const EffectPaintPropertyNode* ViewTransitionStyleTracker::GetEffect(
     if (element_data->target_element != &element) {
       continue;
     }
-    DCHECK(element_data->effect_node);
     return element_data->effect_node.get();
   }
   NOTREACHED();
@@ -1229,7 +1228,6 @@ const EffectPaintPropertyNode* ViewTransitionStyleTracker::GetEffect(
 
 const EffectPaintPropertyNode* ViewTransitionStyleTracker::GetRootEffect()
     const {
-  DCHECK(root_effect_node_);
   return root_effect_node_.get();
 }
 
