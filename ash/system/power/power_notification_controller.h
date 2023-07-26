307e437fa7acadf7697abe47153a0a6e6088dca2
Michael Wojcicka <mwoj@google.com>
2023-07-26 00:36:48 +0000

    Add Experiment Arms for Battery Saver Mode.
    Change-Id: Ie5d586ad2f581a91fba5265779aa21fcc669e5c6
    Bug: b:283997338
    Test: ash_unittests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4674100
    Reviewed-by: Charles Dick <cwd@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175173}

--- ash/system/power/power_notification_controller.h
+++ ash/system/power/power_notification_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/constants/ash_features.h"
 #include "ash/system/power/power_status.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
@@ -27,12 +28,29 @@ class ASH_EXPORT PowerNotificationController : public PowerStatus::Observer {
   enum NotificationState {
     NOTIFICATION_NONE,
 
+    // Battery Saver Notification States.
+    NOTIFICATION_BSM_THRESHOLD_OPT_OUT,
+
+    NOTIFICATION_BSM_THRESHOLD_OPT_IN,
+
+    NOTIFICATION_BSM_LOW_POWER_OPT_IN,
+
     // Low battery charge.
     NOTIFICATION_LOW_POWER,
 
     // Critically low battery charge.
     NOTIFICATION_CRITICAL,
   };
+
+  // Time-based notification thresholds when on battery power.
+  static constexpr int kCriticalMinutes = 5;
+  static constexpr int kLowPowerMinutes = 15;
+  static constexpr int kNoWarningMinutes = 30;
+
+  // Percentage-based notification thresholds when using a low-power charger.
+  static constexpr int kCriticalPercentage = 5;
+  static constexpr int kLowPowerPercentage = 10;
+  static constexpr int kNoWarningPercentage = 15;
 
   explicit PowerNotificationController(
       message_center::MessageCenter* message_center);
@@ -62,20 +80,22 @@ class ASH_EXPORT PowerNotificationController : public PowerStatus::Observer {
   // Shows a notification when dual-role devices are connected.
   void MaybeShowDualRoleNotification();
 
+  // Determines whether a Battery Saver Notification should be shown. Returns
+  // true if a notification should be shown, or nullopt if none of the bsm
+  // branches were triggered.
+  absl::optional<bool> HandleBatterySaverNotifications();
+
+  // Resets the notifications for battery saver mode when we go above each
+  // set threshold.
+  void MaybeResetNotificationAvailability(
+      features::BatterySaverNotificationBehavior experiment,
+      const double battery_percent,
+      const int battery_remaining_minutes);
+
   // Sets |notification_state_|. Returns true if a notification should be shown.
   bool UpdateNotificationState();
   bool UpdateNotificationStateForRemainingTime();
   bool UpdateNotificationStateForRemainingPercentage();
-
-  // Time-based notification thresholds when on battery power.
-  static constexpr int kCriticalMinutes = 5;
-  static constexpr int kLowPowerMinutes = 15;
-  static constexpr int kNoWarningMinutes = 30;
-
-  // Percentage-based notification thresholds when using a low-power charger.
-  static constexpr int kCriticalPercentage = 5;
-  static constexpr int kLowPowerPercentage = 10;
-  static constexpr int kNoWarningPercentage = 15;
 
   static const char kUsbNotificationId[];
 
@@ -98,6 +118,16 @@ class ASH_EXPORT PowerNotificationController : public PowerStatus::Observer {
   // Has the user already dismissed a low-power notification? Should be set
   // back to false when all power sources are disconnected.
   bool usb_notification_dismissed_ = false;
+
+  // Based on the last OnPowerStatusChanged() callback, was battery saver mode
+  // active?
+  bool battery_saver_previously_active_ = false;
+
+  // Has the battery saver threshold been crossed?
+  bool threshold_crossed_ = false;
+
+  // Has the low power notification been crossed?
+  bool low_power_crossed_ = false;
 };
 
 }  // namespace ash
