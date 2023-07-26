3e90db3272c2228415f2d6421abc3dd97a3ed77a
Daniel d'Andrada <dandrader@google.com>
2023-07-26 14:12:44 +0000

    Clean up headers in smart_card_connection.*
    Bug: 1386175
    Change-Id: Ia629df8f7511c2b568616ec8e1f7427a8899376f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4720787
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175417}

--- third_party/blink/renderer/modules/smart_card/smart_card_connection.cc
+++ third_party/blink/renderer/modules/smart_card/smart_card_connection.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_smart_card_transaction_callback.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_array_buffer.h"
 #include "third_party/blink/renderer/modules/smart_card/smart_card_error.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_remote.h"
 
 namespace blink {
 namespace {
