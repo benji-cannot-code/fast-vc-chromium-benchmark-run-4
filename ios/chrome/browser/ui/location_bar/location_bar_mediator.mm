0ac94f0e32e1374e43be99dba5da19c65df8596f
Asami Doi <asamidoi@chromium.org>
2023-07-26 16:51:07 +0000

    [ios] Remove webStateList:didChangeActiveWebState:oldWebState:atIndex:reason: from LocationBarMediator
    This CL updates LocationBarMediator to handle the activation of WebState in didChangeWebStateList:change:status:.
    
    Bug: 1442546
    Change-Id: Ic573e04a62913f68f15aa8dcc583e23a6d603370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705346
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175505}

--- ios/chrome/browser/ui/location_bar/location_bar_mediator.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_mediator.mm
@@ -113,15 +113,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
-#pragma mark - WebStateListObserver
+#pragma mark - WebStateListObserving
 
-- (void)webStateList:(WebStateList*)webStateList
-    didChangeActiveWebState:(web::WebState*)newWebState
-                oldWebState:(web::WebState*)oldWebState
-                    atIndex:(int)atIndex
-                     reason:(ActiveWebStateChangeReason)reason {
+- (void)didChangeWebStateList:(WebStateList*)webStateList
+                       change:(const WebStateListChange&)change
+                       status:(const WebStateListStatus&)status {
   DCHECK_EQ(_webStateList, webStateList);
-  [self.consumer defocusOmnibox];
+  if (status.active_web_state_change()) {
+    [self.consumer defocusOmnibox];
+  }
 }
 
 @end
