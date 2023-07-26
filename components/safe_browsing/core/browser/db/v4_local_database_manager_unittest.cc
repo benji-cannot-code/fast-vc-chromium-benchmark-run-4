96e4de25ca0f3418608f324f885f3d07b26bb447
Francois Marier <francois@brave.com>
2023-07-26 05:13:25 +0000

    Enable download protection allowlist for all desktop builds
    While this allowlist is also applied server-side, when available
    locally, it will improve privacy by suppressing unnecessary remote
    lookups for known-safe sites.
    
    Bug: 1462994
    Change-Id: If7dba0834c471481256ffc4b701205e3636f5bee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671697
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Francois Marier <francois@brave.com>
    Cr-Commit-Position: refs/heads/main@{#1175244}

--- components/safe_browsing/core/browser/db/v4_local_database_manager_unittest.cc
+++ components/safe_browsing/core/browser/db/v4_local_database_manager_unittest.cc
@@ -1714,9 +1714,13 @@ TEST_F(V4LocalDatabaseManagerTest, SyncedLists) {
                                              GetUrlSuspiciousSiteId(),
                                              GetUrlHighConfidenceAllowlistId()};
 #else
-  std::vector<ListIdentifier> expected_lists{
-      GetUrlSocEngId(), GetUrlMalwareId(),       GetUrlUwsId(),
-      GetUrlMalBinId(), GetChromeExtMalwareId(), GetUrlBillingId()};
+  std::vector<ListIdentifier> expected_lists{GetUrlSocEngId(),
+                                             GetUrlMalwareId(),
+                                             GetUrlUwsId(),
+                                             GetUrlMalBinId(),
+                                             GetChromeExtMalwareId(),
+                                             GetUrlBillingId(),
+                                             GetUrlCsdDownloadAllowlistId()};
 #endif
 
   std::vector<ListIdentifier> synced_lists;
