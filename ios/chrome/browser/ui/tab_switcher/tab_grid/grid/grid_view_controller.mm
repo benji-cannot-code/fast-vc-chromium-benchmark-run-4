9a6c9172bf4f7ff120ac0dbd1801951665c9cf72
Louis Romero <lpromero@google.com>
2023-07-26 09:51:20 +0000

    Remove cell's zPosition as it was for thumbstrip
    Setting the zPosition was useful when moving from ThumbStrip to TabGrid
    (introduced in crrev.com/c/2445782). ThumbStrip is gone, so removing
    this.
    
    Bug: 1467716
    Bug: 1457148
    Change-Id: I12c4685e44ee3e7de3374b5314f8a338de703f1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719566
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175328}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -756,9 +756,6 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
     GridCell* gridCell = base::mac::ObjCCastStrict<GridCell>(cell);
     [self configureCell:gridCell withItem:item atIndex:itemIndex];
   }
-  // Set the z index of cells so that lower rows are moving behind the upper
-  // rows during transitions between grid and horizontal layouts.
-  cell.layer.zPosition = self.items.count - itemIndex;
 
   if (![self.pointerInteractionCells containsObject:cell]) {
     [cell addInteraction:[[UIPointerInteraction alloc] initWithDelegate:self]];
@@ -766,6 +763,7 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
     // the number of reusable cells in memory.
     [self.pointerInteractionCells addObject:cell];
   }
+
   return cell;
 }
 
@@ -1648,7 +1646,6 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
         }];
   }
 
-  [self updateVisibleCellZIndex];
   [self updateVisibleCellIdentifiers];
 }
 
@@ -2001,19 +1998,6 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
   }
 }
 
-// Updates the ZIndex of the visible cells to have the cells of the upper rows
-// be above the cell of the lower rows.
-- (void)updateVisibleCellZIndex {
-  for (NSIndexPath* indexPath in self.collectionView
-           .indexPathsForVisibleItems) {
-    // Set the z index of cells so that lower rows are moving behind the upper
-    // rows during transitions between grid and horizontal layouts.
-    UICollectionViewCell* cell =
-        [self.collectionView cellForItemAtIndexPath:indexPath];
-    cell.layer.zPosition = self.items.count - indexPath.item;
-  }
-}
-
 // Update visible cells identifier, following a reorg of cells.
 - (void)updateVisibleCellIdentifiers {
   for (NSIndexPath* indexPath in self.collectionView
@@ -2053,7 +2037,6 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
                intoSectionWithIdentifier:kOpenTabsSectionIdentifier];
     [self.diffableDataSource applySnapshot:snapshot animatingDifferences:NO];
 
-    [self updateVisibleCellZIndex];
     [self updateVisibleCellIdentifiers];
   } else {
     NSIndexSet* targetSections =
