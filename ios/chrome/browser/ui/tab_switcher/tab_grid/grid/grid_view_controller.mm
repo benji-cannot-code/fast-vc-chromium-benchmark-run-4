092ac3582f2318e912ab8a0d4ae45b8fff75b6c6
Louis Romero <lpromero@google.com>
2023-07-26 09:45:00 +0000

    Move accessibility identifier to the cell donfiguration method
    This CL moves where GridCell's accessibility identifier is set to fold
    it into the configuration method.
    
    Bug: 1467716
    Change-Id: I1b62e186ae05ca23a3274bd775818046ea14f8a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719526
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175326}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -94,6 +94,12 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
   return [NSIndexPath indexPathForItem:index inSection:0];
 }
 
+// Returns the accessibility identifier to set on a GridCell when positioned at
+// the given index.
+NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
+  return [NSString stringWithFormat:@"%@%ld", kGridCellIdentifierPrefix, index];
+}
+
 }  // namespace
 
 @interface BidirectionalCollectionViewTransitionLayout
@@ -747,10 +753,8 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
     cell =
         [collectionView dequeueReusableCellWithReuseIdentifier:kCellIdentifier
                                                   forIndexPath:indexPath];
-    cell.accessibilityIdentifier = [NSString
-        stringWithFormat:@"%@%ld", kGridCellIdentifierPrefix, itemIndex];
     GridCell* gridCell = base::mac::ObjCCastStrict<GridCell>(cell);
-    [self configureCell:gridCell withItem:item];
+    [self configureCell:gridCell withItem:item atIndex:itemIndex];
   }
   // Set the z index of cells so that lower rows are moving behind the upper
   // rows during transitions between grid and horizontal layouts.
@@ -961,7 +965,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
     // Stop animation of GridCells when removing them from the collection view.
     // This is important to prevent cells from animating indefinitely. This is
     // safe because the animation state of GridCells is set in
-    // `configureCell:withItem:` whenever a cell is used.
+    // `configureCell:withItem:atIndex:` whenever a cell is used.
     [base::mac::ObjCCastStrict<GridCell>(cell) hideActivityIndicator];
   }
 }
@@ -1409,8 +1413,9 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
   GridCell* cell = base::mac::ObjCCastStrict<GridCell>(
       [self.collectionView cellForItemAtIndexPath:CreateIndexPath(index)]);
   // `cell` may be nil if it is scrolled offscreen.
-  if (cell)
-    [self configureCell:cell withItem:item];
+  if (cell) {
+    [self configureCell:cell withItem:item atIndex:index];
+  }
 }
 
 - (void)moveItemWithID:(NSString*)itemID toIndex:(NSUInteger)toIndex {
@@ -1857,7 +1862,9 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 // snapshot asynchronously with information from `item`. Updates the `cell`'s
 // theme to this view controller's theme. This view controller becomes the
 // delegate for the cell.
-- (void)configureCell:(GridCell*)cell withItem:(TabSwitcherItem*)item {
+- (void)configureCell:(GridCell*)cell
+             withItem:(TabSwitcherItem*)item
+              atIndex:(NSUInteger)index {
   DCHECK(cell);
   DCHECK(item);
   cell.delegate = self;
@@ -1865,6 +1872,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
   cell.itemIdentifier = item.identifier;
   cell.title = item.title;
   cell.titleHidden = item.hidesTitle;
+  cell.accessibilityIdentifier = GridCellAccessibilityIdentifier(index);
   if (self.mode == TabGridModeSelection) {
     if ([self isItemWithIDSelectedForEditing:item.identifier]) {
       cell.state = GridCellStateEditingSelected;
@@ -2015,8 +2023,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
     if (![cell isKindOfClass:[GridCell class]])
       continue;
     NSUInteger itemIndex = base::checked_cast<NSUInteger>(indexPath.item);
-    cell.accessibilityIdentifier = [NSString
-        stringWithFormat:@"%@%ld", kGridCellIdentifierPrefix, itemIndex];
+    cell.accessibilityIdentifier = GridCellAccessibilityIdentifier(itemIndex);
   }
 }
 
