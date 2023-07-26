45bdd1398e806ed24da6715a360949379661f1c2
Ameur Hosni <ameurhosni@google.com>
2023-07-26 15:27:51 +0000

    [IOS][SPOTLIGHT] Fix topsites not indexed.
    Due to a recent change (4678904), topsites are not indexed anymore.
    This CL aims to fix that by adding a reindex to the resyncIndex method.
    
    Bug: None
    Change-Id: I7235a2db8646ebba0553f1f71727dd19931c90b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721326
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175450}

--- ios/chrome/app/spotlight/spotlight_manager.mm
+++ ios/chrome/app/spotlight/spotlight_manager.mm
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_bookmarkManager reindexBookmarksIfNeeded];
   [_actionsManager indexActionsWithIsGoogleDefaultSearchEngine:
                        [self isGoogleDefaultSearchEngine]];
+  [_topSitesManager reindexTopSites];
   [self.readingListManager clearAndReindexReadingList];
   [self.openTabsManager clearAndReindexOpenTabs];
 }
