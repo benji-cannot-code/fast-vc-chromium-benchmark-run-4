58b5a31f2d8b241d4c6c014a94f6f627ddb3681a
Chad Duffin <chadduffin@chromium.org>
2023-07-26 22:07:06 +0000

    cellular: Fix FeatureUsageMetrics for eSIM
    This change fixes the FeatureUsageMetrics for eSIM so that both the
    IsEligible() and IsEnabled() implementations use the same source of
    truth for determining their return. Previously the IsEnabled() method
    relied on device prefs that cached the eSIM state while IsEligible
    directly checked whether an EUICC was available; this breaks down in the
    situation where the EUICC might not be available, e.g. Hermes is down,
    but we still have cached eSIM profiles.
    
    Bug: b/291812699
    Test: Manually verified on a Linux build.
    Change-Id: I9032f47e3e1be654c26d0f0bf705ec26bfa447bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701104
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175718}

--- chromeos/ash/components/network/cellular_metrics_logger.cc
+++ chromeos/ash/components/network/cellular_metrics_logger.cc
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/constants/ash_features.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/time/tick_clock.h"
+#include "chromeos/ash/components/dbus/hermes/hermes_euicc_client.h"
 #include "chromeos/ash/components/dbus/hermes/hermes_manager_client.h"
+#include "chromeos/ash/components/dbus/hermes/hermes_profile_client.h"
 #include "chromeos/ash/components/feature_usage/feature_usage_metrics.h"
 #include "chromeos/ash/components/network/cellular_esim_profile.h"
 #include "chromeos/ash/components/network/cellular_esim_profile_handler.h"
@@ -370,10 +373,7 @@ CellularMetricsLogger::ShillErrorToConnectResult(
 class ESimFeatureUsageMetrics
     : public feature_usage::FeatureUsageMetrics::Delegate {
  public:
-  explicit ESimFeatureUsageMetrics(
-      CellularESimProfileHandler* cellular_esim_profile_handler)
-      : cellular_esim_profile_handler_(cellular_esim_profile_handler) {
-    DCHECK(cellular_esim_profile_handler);
+  ESimFeatureUsageMetrics() {
     feature_usage_metrics_ =
         std::make_unique<feature_usage::FeatureUsageMetrics>(
             kESimUMAFeatureName, this);
@@ -390,11 +390,29 @@ class ESimFeatureUsageMetrics
   // feature_usage::FeatureUsageMetrics::Delegate:
   bool IsEnabled() const final {
     // If there are installed ESim profiles.
-    for (const auto& profile :
-         cellular_esim_profile_handler_->GetESimProfiles()) {
-      if (profile.state() == CellularESimProfile::State::kActive ||
-          profile.state() == CellularESimProfile::State::kInactive) {
-        return true;
+    for (const dbus::ObjectPath& euicc_path :
+         HermesManagerClient::Get()->GetAvailableEuiccs()) {
+      HermesEuiccClient::Properties* euicc_properties =
+          HermesEuiccClient::Get()->GetProperties(euicc_path);
+      if (!euicc_properties) {
+        continue;
+      }
+      const std::vector<dbus::ObjectPath>& profiles =
+          ash::features::IsSmdsDbusMigrationEnabled()
+              ? euicc_properties->profiles().value()
+              : euicc_properties->installed_carrier_profiles().value();
+      for (const dbus::ObjectPath& profile_path : profiles) {
+        HermesProfileClient::Properties* profile_properties =
+            HermesProfileClient::Get()->GetProperties(profile_path);
+        if (!profile_properties) {
+          continue;
+        }
+        const hermes::profile::State profile_state =
+            profile_properties->state().value();
+        if (profile_state == hermes::profile::State::kActive ||
+            profile_state == hermes::profile::State::kInactive) {
+          return true;
+        }
       }
     }
     return false;
@@ -409,8 +427,6 @@ class ESimFeatureUsageMetrics
   void StopUsage() { feature_usage_metrics_->StopSuccessfulUsage(); }
 
  private:
-  raw_ptr<CellularESimProfileHandler, ExperimentalAsh>
-      cellular_esim_profile_handler_;
   std::unique_ptr<feature_usage::FeatureUsageMetrics> feature_usage_metrics_;
 };
 
@@ -483,10 +499,7 @@ void CellularMetricsLogger::Init(
     network_connection_handler_->AddObserver(this);
   }
 
-  if (cellular_esim_profile_handler_) {
-    esim_feature_usage_metrics_ = std::make_unique<ESimFeatureUsageMetrics>(
-        cellular_esim_profile_handler_);
-  }
+  esim_feature_usage_metrics_ = std::make_unique<ESimFeatureUsageMetrics>();
 
   if (LoginState::IsInitialized())
     LoginState::Get()->AddObserver(this);
