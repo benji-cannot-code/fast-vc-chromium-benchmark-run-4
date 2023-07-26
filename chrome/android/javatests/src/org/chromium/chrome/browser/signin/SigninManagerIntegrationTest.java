620f0083c5b360442fd99bdb2fba3518885b036b
Tanmoy Mollik <triploblastic@chromium.org>
2023-07-26 13:25:58 +0000

    [FlakyTest] Disable SigninManagerIntegrationTest.testUpdateAccountListTwoAccountsThenRemoveOne
    Temporarily disable the test as it's flaky while working on a fix.
    
    Bug: 1467211
    Change-Id: Ia5e19c5737931108a457470059f9143396ab7a7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4720867
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175391}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninManagerIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninManagerIntegrationTest.java
@@ -23,6 +23,7 @@ import org.mockito.quality.Strictness;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.SigninManager;
@@ -170,6 +171,7 @@ public class SigninManagerIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1467211")
     public void testUpdateAccountListTwoAccountsThenRemoveOne() {
         // Add accounts.
         mSigninTestRule.addAccountAndWaitForSeeding(TEST_ACCOUNT1);
