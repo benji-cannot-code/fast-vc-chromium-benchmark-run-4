9ca71fd970d38167d62e21b2ad931ec7049cb13f
Minoru Chikamune <chikamune@chromium.org>
2023-07-26 08:10:57 +0000

    Revert: [LCPP] Provide RecordLCPElementLocator() API
    This CL revert https://crrev.com/c/4596699 because we decided not to add
    lcp_element_locator field to PageRequestSummary class.
    
    This is a necessary step for the upcoming CL:
    (https://crrev.com/c/4672487).
    
    Bug: 1419756
    Change-Id: I2b9e62f6840320cc3a9bf7e9c52e5ed825ad8bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719636
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175286}

--- chrome/browser/predictors/resource_prefetch_predictor.cc
+++ chrome/browser/predictors/resource_prefetch_predictor.cc
@@ -330,7 +330,6 @@ void ResourcePrefetchPredictor::RecordPageRequestSummary(
   LearnOrigins(summary->main_frame_url.host(),
                summary->main_frame_url.DeprecatedGetOriginAsURL(),
                summary->origins);
-  LearnLcpp(summary->main_frame_url.host(), summary->lcp_element_locator);
 
   if (observer_)
     observer_->OnNavigationLearned(*summary);
