e8b2114d8e9d26a5b3c7f1293fb1e61dda19cf05
Colin Kincaid <ckincaid@chromium.org>
2023-07-26 22:56:36 +0000

    multipaste: Show titles for visited URLs in the clipboard history menu
    This CL adds a label below the contents of text clipboard history menu
    items that represent URLs that are present in the primary user's
    browsing history. This label shows the title of the URL's web page as
    stored in the user's browsing history. Currently, this functionality is
    only supported when the session has exactly one user profile.
    
    Screenshot: https://screenshot.googleplex.com/ApqitEu2ZNXLh3V
    
    Fixed: b/267694762
    Change-Id: Ia6f9ee6da873d1e4f4ca4d68a2ffc76318d36c5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4698083
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175744}

--- ash/clipboard/clipboard_history_util.h
+++ ash/clipboard/clipboard_history_util.h
@@ -62,6 +62,7 @@ enum MenuViewID {
   kContentsViewID = 1,
   kCtrlVLabelID,
   kDeleteButtonViewID,
+  kSecondaryDisplayTextLabelID,
 };
 
 // Modes for specifying a clipboard history pause's semantics.
