39d801325005f950b065622482ad24297b7275ed
Yifan Luo <lyf@chromium.org>
2023-07-26 09:25:10 +0000

    Revert "LayoutNG: Store LayoutOverflow in PhysicalFragmentRareData"
    This reverts commit c9331451261e21144839b361c0f040f072eac421.
    
    Reason for revert: Seems to be causing multiple failures:
    crbug.com/1467734
    
    Original change's description:
    > LayoutNG: Store LayoutOverflow in PhysicalFragmentRareData
    >
    > This CL changes the handling of LayoutOverflow.
    >  * Store LayoutOverflow in PhysicalFragmentRareData
    >  * Do not clone NGPhysicalBoxFragments on updating LayoutOverflow
    >
    > The `updated_layout_overflow` argument of CloneWithPostLayoutFragments()
    > is not used after this CL.
    >
    > This CL has no user-visible behavior changes, but the new code is
    > guarded by a runtime flag just in case.
    >
    > Bug: 1466737
    > Change-Id: Icdfd1f4697d16e70f271595692f1bd4c63fa59dd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699286
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1175189}
    
    Owners-Override
    
    Bug: 1466737, 1467734
    Change-Id: I42455f780b0ca58feedce62321f423a041f0c8b4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718909
    Auto-Submit: Yifan Luo <lyf@chromium.org>
    Owners-Override: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1175313}

--- third_party/blink/renderer/core/layout/ng/physical_fragment_rare_data.cc
+++ third_party/blink/renderer/core/layout/ng/physical_fragment_rare_data.cc
@@ -16,7 +16,6 @@ PhysicalFragmentRareData::PhysicalFragmentRareData(wtf_size_t num_fields) {
 }
 
 PhysicalFragmentRareData::PhysicalFragmentRareData(
-    const PhysicalRect* layout_overflow,
     const NGPhysicalBoxStrut* borders,
     const NGPhysicalBoxStrut* padding,
     absl::optional<PhysicalRect> inflow_bounds,
@@ -27,9 +26,6 @@ PhysicalFragmentRareData::PhysicalFragmentRareData(
   // Each field should be processed in order of FieldId to avoid vector
   // element insertions.
 
-  if (layout_overflow) {
-    SetField(FieldId::kLayoutOverflow).layout_overflow = *layout_overflow;
-  }
   if (borders) {
     SetField(FieldId::kBorders).borders = *borders;
   }
@@ -103,7 +99,6 @@ PhysicalFragmentRareData::PhysicalFragmentRareData(
   // Each field should be processed in order of FieldId to avoid vector
   // element insertions.
 
-  SET_IF_EXISTS(kLayoutOverflow, layout_overflow, other);
   SET_IF_EXISTS(kBorders, borders, other);
   SET_IF_EXISTS(kPadding, padding, other);
   SET_IF_EXISTS(kInflowBounds, inflow_bounds, other);
@@ -132,7 +127,6 @@ PhysicalFragmentRareData::~PhysicalFragmentRareData() = default;
 
 #define DISPATCH_BY_MEMBER_TYPE(FUNC)                                       \
   switch (type) {                                                           \
-    FUNC(kLayoutOverflow, layout_overflow);                                 \
     FUNC(kBorders, borders);                                                \
     FUNC(kPadding, padding);                                                \
     FUNC(kInflowBounds, inflow_bounds);                                     \
