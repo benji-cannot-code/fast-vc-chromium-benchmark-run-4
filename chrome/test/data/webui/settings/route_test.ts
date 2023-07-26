cf99a746cf32ecbdc64eede1d1e0a2ca7bec0e86
dpapad <dpapad@chromium.org>
2023-07-26 16:57:46 +0000

    Settings: Migrate tests to WebUIMochaBrowserTest, part 8
    In this part migrating Gtests triggering the Mocha tests in:
     - privacy_page_test.ts (only a subset)
     - route_test.ts
     - safety_check_permissions_test.ts
     - site_list_test.ts
    
    WebUIMochaBrowserTest is the new way of running WebUI tests without any
    dependency to the obsolete js2gtest and test_api.js mechanism.
    
    Bug: 145736
    Change-Id: I2860e4da7330a235189c866a99f12077148a9ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4717713
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175508}

--- chrome/test/data/webui/settings/route_test.ts
+++ chrome/test/data/webui/settings/route_test.ts
@@ -10,7 +10,7 @@ import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
 // clang-format on
 
-suite('route', function() {
+suite('Basic', function() {
   /**
    * Returns a new promise that resolves after a window 'popstate' event.
    */
@@ -364,7 +364,7 @@ suite('NonExistentRoute', function() {
   });
 });
 
-suite('SafetyHubReachableTests', function() {
+suite('SafetyHubReachable', function() {
   let routes: SettingsRoutes;
 
   setup(function() {
@@ -389,7 +389,7 @@ suite('SafetyHubReachableTests', function() {
   });
 });
 
-suite('SafetyHubNotReachableTests', function() {
+suite('SafetyHubNotReachable', function() {
   let routes: SettingsRoutes;
 
   setup(function() {
