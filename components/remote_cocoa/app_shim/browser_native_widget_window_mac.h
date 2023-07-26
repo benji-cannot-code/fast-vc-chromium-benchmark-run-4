b4fc7002912761cc9755ad4a143db600ca7f95c2
Jayson Adams <shrike@chromium.org>
2023-07-26 22:17:19 +0000

    [Mac Immersive Fullscreen] Prevent spaces switch on child window removal
    Removing a child window from a fullscreen window that isn't on the
    active space causes a switch to that space. This cl adds machinery to
    defer child window removals until a window becomes active, exits fullscreen, closes.
    
    Bug: 1454606
    
    Test: out/Default/interactive_ui_tests \
        --gtest_filter=ImmersiveModeControllerMacInteractiveTest.DeferChildWindowRemoval
    
    Include-Ci-Only-Tests: true
    
    Change-Id: I9bf6daa3f21e00ab80c4d9b657c6eefa1b554142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642775
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175725}

--- components/remote_cocoa/app_shim/browser_native_widget_window_mac.h
+++ components/remote_cocoa/app_shim/browser_native_widget_window_mac.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/remote_cocoa/app_shim/native_widget_mac_nswindow.h"
 
-@interface BrowserNativeWidgetWindow : NativeWidgetMacNSWindow
+#import "components/remote_cocoa/app_shim/views_scrollbar_bridge.h"
+
+REMOTE_COCOA_APP_SHIM_EXPORT @interface BrowserNativeWidgetWindow
+    : NativeWidgetMacNSWindow
 @end
 
 #endif  // COMPONENTS_REMOTE_COCOA_APP_SHIM_BROWSER_NATIVE_WIDGET_WINDOW_MAC_H_
--- components/remote_cocoa/app_shim/views_nswindow_delegate.mm
+++ components/remote_cocoa/app_shim/views_nswindow_delegate.mm
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   absl::optional<bool> _resizingHorizontally;
 }
 
+@synthesize willCloseLater;
+
 - (instancetype)initWithBridgedNativeWidget:
     (remote_cocoa::NativeWidgetNSWindowBridge*)parent {
   DCHECK(parent);
