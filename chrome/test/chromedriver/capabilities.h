e32b0c86502948fdc34b0376f2ce60109813570e
Vladimir Nechaev <nechaev@chromium.org>
2023-07-26 12:12:24 +0000

    Fix a rare crash during a session creation
    If the hosting system is under a heavy load it can happen that the
    browser startup timeout expires before the DevToolsHttpClient kicks in.
    Before this commit ChromeDriver skipped the part with the
    DevToolsHttpClient use but it tried to execute the following code that
    depended on the data received from the DevToolsHttpClient.
    With this commit the code is able to detect such a situation and stop
    its execution with a SessionNotCreated error.
    
    Bug: 1467496
    Change-Id: Iaf44de84c3602bf71d4d57b7e43c8aee8cb2f24a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719968
    Commit-Queue: Thiago Perrotta <tperrotta@chromium.org>
    Auto-Submit: Vladimir Nechaev <nechaev@chromium.org>
    Reviewed-by: Thiago Perrotta <tperrotta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175368}

--- chrome/test/chromedriver/capabilities.h
+++ chrome/test/chromedriver/capabilities.h
@@ -123,6 +123,8 @@ struct Capabilities {
   base::TimeDelta script_timeout = Session::kDefaultScriptTimeout;
   base::TimeDelta page_load_timeout = Session::kDefaultPageLoadTimeout;
   base::TimeDelta implicit_wait_timeout = Session::kDefaultImplicitWaitTimeout;
+  base::TimeDelta browser_startup_timeout =
+      Session::kDefaultBrowserStartupTimeout;
 
   bool strict_file_interactability;
 
