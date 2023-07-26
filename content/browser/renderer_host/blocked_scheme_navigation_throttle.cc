31c334d3ebc4cab11dc6ddd8b08f1ef1970b0874
Joel Hockey <joelhockey@chromium.org>
2023-07-26 04:06:54 +0000

    Block filesystem: navigation to external mounted files
    Bug: b/291526810
    Change-Id: I89eaaf8183b887ac266730c2168d10333f9f0e5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705137
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175231}

--- content/browser/renderer_host/blocked_scheme_navigation_throttle.cc
+++ content/browser/renderer_host/blocked_scheme_navigation_throttle.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/common/content_features.h"
+#include "storage/browser/file_system/external_mount_points.h"
+#include "storage/browser/file_system/file_system_url.h"
+#include "storage/common/file_system/file_system_util.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom.h"
 #include "url/url_constants.h"
 
@@ -23,6 +26,13 @@ namespace content {
 namespace {
 const char kConsoleError[] = "Not allowed to navigate top frame to %s URL: %s";
 const char kAnyFrameConsoleError[] = "Not allowed to navigate to %s URL: %s";
+
+bool IsExternalMountedFile(const GURL& url) {
+  storage::FileSystemURL file_system_url =
+      storage::ExternalMountPoints::GetSystemInstance()->CrackURL(
+          url, blink::StorageKey::CreateFirstParty(url::Origin::Create(url)));
+  return file_system_url.is_valid();
+}
 }
 
 BlockedSchemeNavigationThrottle::BlockedSchemeNavigationThrottle(
@@ -46,6 +56,7 @@ BlockedSchemeNavigationThrottle::WillStartRequest() {
 
   if (base::FeatureList::IsEnabled(
           blink::features::kFileSystemUrlNavigationForChromeAppsOnly) &&
+      !IsExternalMountedFile(request->GetURL()) &&
       (url::Origin::Create(request->GetURL()) ==
        request->GetInitiatorOrigin()) &&
       content::GetContentClient()->browser()->IsFileSystemURLNavigationAllowed(
@@ -122,6 +133,12 @@ BlockedSchemeNavigationThrottle::CreateThrottleForNavigation(
           blink::features::kFileSystemUrlNavigation) &&
       request->IsRendererInitiated() &&
       request->GetURL().SchemeIs(url::kFileSystemScheme)) {
+    return std::make_unique<BlockedSchemeNavigationThrottle>(request);
+  }
+  // Block any external mounted files.
+  if (!base::FeatureList::IsEnabled(
+          blink::features::kFileSystemUrlNavigation) &&
+      IsExternalMountedFile(request->GetURL())) {
     return std::make_unique<BlockedSchemeNavigationThrottle>(request);
   }
   return nullptr;
