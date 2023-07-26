650886e7f895de59d1b8d05977a8c7a17184d4c5
Koji Ishii <kojii@chromium.org>
2023-07-26 10:40:35 +0000

    [phrase-line-break] Add `LineBreakType::kPhrase`
    This patch adds the `kPhrase` mode to `LineBreakType`, and
    support it in `LazyLineBreakIterator`.
    
    When specified, it uses the AdaBoost ML engine in the ICU as
    the underlying engine by the `lw=phrase` keyword.
    
    This patch has no behavior changes as the core doesn't use
    this mode yet.
    
    Bug: 1443291
    Change-Id: I180f64c6183ea7295c4d5ff527a8d549d308252c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4577182
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175342}

--- third_party/blink/renderer/platform/text/text_break_iterator.cc
+++ third_party/blink/renderer/platform/text/text_break_iterator.cc
@@ -428,6 +428,7 @@ int LazyLineBreakIterator::NextBreakablePosition(int pos,
                                                  int len) const {
   switch (line_break_type) {
     case LineBreakType::kNormal:
+    case LineBreakType::kPhrase:
       return NextBreakablePosition<LineBreakType::kNormal>(pos, len);
     case LineBreakType::kBreakAll:
       return NextBreakablePosition<LineBreakType::kBreakAll>(pos, len);
@@ -494,6 +495,8 @@ std::ostream& operator<<(std::ostream& ostream, LineBreakType line_break_type) {
       return ostream << "BreakCharacter";
     case LineBreakType::kKeepAll:
       return ostream << "KeepAll";
+    case LineBreakType::kPhrase:
+      return ostream << "Phrase";
   }
   NOTREACHED();
   return ostream << "LineBreakType::" << static_cast<int>(line_break_type);
