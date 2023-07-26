b915320c85a2086735d2265c9432e218cd6a55a7
Louis Romero <lpromero@google.com>
2023-07-26 11:37:40 +0000

    Remove iOS 14 and below code paths related to key commands
    Now that iOS 14 is no longer supported, this CL removes the code paths
    related to it in keyboard commands support.
    
    Bug: none
    Change-Id: Ia55c72928b7fceffa38c45ac66ef32ef75d4d3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714351
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175365}

--- ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.mm
+++ ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.h"
 
-#import "ios/chrome/browser/shared/ui/util/rtl_geometry.h"
 #import "ios/chrome/browser/ui/keyboard/key_command_actions.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -22,14 +21,6 @@ UIKeyModifierFlags ShiftCommand = UIKeyModifierShift | UIKeyModifierCommand;
 UIKeyModifierFlags AltShiftCommand =
     UIKeyModifierAlternate | UIKeyModifierShift | UIKeyModifierCommand;
 UIKeyModifierFlags ControlShift = UIKeyModifierControl | UIKeyModifierShift;
-
-// Backport UIKeyInputDelete to iOS 14 and below.
-NSString* Delete(void) {
-  if (@available(iOS 15.0, *))
-    return UIKeyInputDelete;
-  else
-    return @"\b";
-}
 
 }  // namespace
 
@@ -119,9 +110,7 @@ NSString* Delete(void) {
                   modifierFlags:Control
                          action:@selector(keyCommand_showNextTab)
                 titleIDAsString:@"IDS_IOS_KEYBOARD_NEXT_TAB"];
-  if (@available(iOS 15.0, *)) {
-    keyCommand.wantsPriorityOverSystemBehavior = YES;
-  }
+  keyCommand.wantsPriorityOverSystemBehavior = YES;
   return keyCommand;
 }
 
@@ -131,26 +120,14 @@ NSString* Delete(void) {
                   modifierFlags:ControlShift
                          action:@selector(keyCommand_showPreviousTab)
                 titleIDAsString:@"IDS_IOS_KEYBOARD_PREVIOUS_TAB"];
-  if (@available(iOS 15.0, *)) {
-    keyCommand.wantsPriorityOverSystemBehavior = YES;
-  }
+  keyCommand.wantsPriorityOverSystemBehavior = YES;
   return keyCommand;
 }
 
 + (UIKeyCommand*)cr_showNextTab_2 {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is true
   // by default. It handles flipping the direction of braces.
-  NSString* braceNext;
-  if (@available(iOS 15.0, *)) {
-    braceNext = @"}";
-  } else {
-    if (UseRTLLayout()) {
-      braceNext = @"{";
-    } else {
-      braceNext = @"}";
-    }
-  }
-  return [self keyCommandWithInput:braceNext
+  return [self keyCommandWithInput:@"}"
                      modifierFlags:Command
                             action:@selector(keyCommand_showNextTab)];
 }
@@ -158,17 +135,7 @@ NSString* Delete(void) {
 + (UIKeyCommand*)cr_showPreviousTab_2 {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is true
   // by default. It handles flipping the direction of braces.
-  NSString* bracePrevious;
-  if (@available(iOS 15.0, *)) {
-    bracePrevious = @"{";
-  } else {
-    if (UseRTLLayout()) {
-      bracePrevious = @"}";
-    } else {
-      bracePrevious = @"{";
-    }
-  }
-  return [self keyCommandWithInput:bracePrevious
+  return [self keyCommandWithInput:@"{"
                      modifierFlags:Command
                             action:@selector(keyCommand_showPreviousTab)];
 }
@@ -176,17 +143,7 @@ NSString* Delete(void) {
 + (UIKeyCommand*)cr_showNextTab_3 {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is true
   // by default. It handles flipping the direction of arrows.
-  NSString* arrowNext;
-  if (@available(iOS 15.0, *)) {
-    arrowNext = UIKeyInputRightArrow;
-  } else {
-    if (UseRTLLayout()) {
-      arrowNext = UIKeyInputLeftArrow;
-    } else {
-      arrowNext = UIKeyInputRightArrow;
-    }
-  }
-  return [self keyCommandWithInput:arrowNext
+  return [self keyCommandWithInput:UIKeyInputRightArrow
                      modifierFlags:AltCommand
                             action:@selector(keyCommand_showNextTab)];
 }
@@ -194,17 +151,7 @@ NSString* Delete(void) {
 + (UIKeyCommand*)cr_showPreviousTab_3 {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is true
   // by default. It handles flipping the direction of arrows.
-  NSString* arrowPrevious;
-  if (@available(iOS 15.0, *)) {
-    arrowPrevious = UIKeyInputLeftArrow;
-  } else {
-    if (UseRTLLayout()) {
-      arrowPrevious = UIKeyInputRightArrow;
-    } else {
-      arrowPrevious = UIKeyInputLeftArrow;
-    }
-  }
-  return [self keyCommandWithInput:arrowPrevious
+  return [self keyCommandWithInput:UIKeyInputLeftArrow
                      modifierFlags:AltCommand
                             action:@selector(keyCommand_showPreviousTab)];
 }
@@ -233,17 +180,7 @@ NSString* Delete(void) {
 + (UIKeyCommand*)cr_back {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is true
   // by default. It handles flipping the direction of brackets.
-  NSString* bracketBack;
-  if (@available(iOS 15.0, *)) {
-    bracketBack = @"[";
-  } else {
-    if (UseRTLLayout()) {
-      bracketBack = @"]";
-    } else {
-      bracketBack = @"[";
-    }
-  }
-  return [self cr_commandWithInput:bracketBack
+  return [self cr_commandWithInput:@"["
                      modifierFlags:Command
                             action:@selector(keyCommand_back)
                    titleIDAsString:@"IDS_IOS_KEYBOARD_HISTORY_BACK"];
@@ -252,17 +189,7 @@ NSString* Delete(void) {
 + (UIKeyCommand*)cr_forward {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is true
   // by default. It handles flipping the direction of brackets.
-  NSString* bracketForward;
-  if (@available(iOS 15.0, *)) {
-    bracketForward = @"]";
-  } else {
-    if (UseRTLLayout()) {
-      bracketForward = @"[";
-    } else {
-      bracketForward = @"]";
-    }
-  }
-  return [self cr_commandWithInput:bracketForward
+  return [self cr_commandWithInput:@"]"
                      modifierFlags:Command
                             action:@selector(keyCommand_forward)
                    titleIDAsString:@"IDS_IOS_KEYBOARD_HISTORY_FORWARD"];
@@ -271,17 +198,7 @@ NSString* Delete(void) {
 + (UIKeyCommand*)cr_back_2 {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is
   // true by default. It handles flipping the direction of arrows.
-  NSString* arrowBack;
-  if (@available(iOS 15.0, *)) {
-    arrowBack = UIKeyInputLeftArrow;
-  } else {
-    if (UseRTLLayout()) {
-      arrowBack = UIKeyInputRightArrow;
-    } else {
-      arrowBack = UIKeyInputLeftArrow;
-    }
-  }
-  return [self keyCommandWithInput:arrowBack
+  return [self keyCommandWithInput:UIKeyInputLeftArrow
                      modifierFlags:Command
                             action:@selector(keyCommand_back)];
 }
@@ -289,17 +206,7 @@ NSString* Delete(void) {
 + (UIKeyCommand*)cr_forward_2 {
   // iOS 15+ supports -[UIKeyCommand allowsAutomaticMirroring], which is
   // true by default. It handles flipping the direction of arrows.
-  NSString* arrowForward;
-  if (@available(iOS 15.0, *)) {
-    arrowForward = UIKeyInputRightArrow;
-  } else {
-    if (UseRTLLayout()) {
-      arrowForward = UIKeyInputLeftArrow;
-    } else {
-      arrowForward = UIKeyInputRightArrow;
-    }
-  }
-  return [self keyCommandWithInput:arrowForward
+  return [self keyCommandWithInput:UIKeyInputRightArrow
                      modifierFlags:Command
                             action:@selector(keyCommand_forward)];
 }
@@ -448,7 +355,7 @@ NSString* Delete(void) {
 }
 
 + (UIKeyCommand*)cr_clearBrowsingData {
-  return [self cr_commandWithInput:Delete()
+  return [self cr_commandWithInput:UIKeyInputDelete
                      modifierFlags:ShiftCommand
                             action:@selector(keyCommand_clearBrowsingData)
                    titleIDAsString:@"IDS_IOS_KEYBOARD_CLEAR_BROWSING_DATA"];
@@ -466,9 +373,7 @@ NSString* Delete(void) {
       [self keyCommandWithInput:@"z"
                   modifierFlags:Command
                          action:@selector(keyCommand_undo)];
-  if (@available(iOS 15.0, *)) {
-    keyCommand.wantsPriorityOverSystemBehavior = YES;
-  }
+  keyCommand.wantsPriorityOverSystemBehavior = YES;
   return keyCommand;
 }
 
@@ -477,39 +382,46 @@ NSString* Delete(void) {
 - (NSString*)cr_symbolicDescription {
   NSMutableString* description = [NSMutableString string];
 
-  if (self.modifierFlags & UIKeyModifierNumericPad)
+  if (self.modifierFlags & UIKeyModifierNumericPad) {
     [description appendString:@"Num lock "];
-  if (self.modifierFlags & UIKeyModifierControl)
+  }
+  if (self.modifierFlags & UIKeyModifierControl) {
     [description appendString:@"⌃"];
-  if (self.modifierFlags & UIKeyModifierAlternate)
+  }
+  if (self.modifierFlags & UIKeyModifierAlternate) {
     [description appendString:@"⌥"];
-  if (self.modifierFlags & UIKeyModifierShift)
+  }
+  if (self.modifierFlags & UIKeyModifierShift) {
     [description appendString:@"⇧"];
-  if (self.modifierFlags & UIKeyModifierAlphaShift)
+  }
+  if (self.modifierFlags & UIKeyModifierAlphaShift) {
     [description appendString:@"⇪"];
-  if (self.modifierFlags & UIKeyModifierCommand)
+  }
+  if (self.modifierFlags & UIKeyModifierCommand) {
     [description appendString:@"⌘"];
+  }
 
-  if ([self.input isEqualToString:Delete()])
+  if ([self.input isEqualToString:UIKeyInputDelete]) {
     [description appendString:@"⌫"];
-  else if ([self.input isEqualToString:@"\r"])
+  } else if ([self.input isEqualToString:@"\r"]) {
     [description appendString:@"↵"];
-  else if ([self.input isEqualToString:@"\t"])
+  } else if ([self.input isEqualToString:@"\t"]) {
     [description appendString:@"⇥"];
-  else if ([self.input isEqualToString:UIKeyInputUpArrow])
+  } else if ([self.input isEqualToString:UIKeyInputUpArrow]) {
     [description appendString:@"↑"];
-  else if ([self.input isEqualToString:UIKeyInputDownArrow])
+  } else if ([self.input isEqualToString:UIKeyInputDownArrow]) {
     [description appendString:@"↓"];
-  else if ([self.input isEqualToString:UIKeyInputLeftArrow])
+  } else if ([self.input isEqualToString:UIKeyInputLeftArrow]) {
     [description appendString:@"←"];
-  else if ([self.input isEqualToString:UIKeyInputRightArrow])
+  } else if ([self.input isEqualToString:UIKeyInputRightArrow]) {
     [description appendString:@"→"];
-  else if ([self.input isEqualToString:UIKeyInputEscape])
+  } else if ([self.input isEqualToString:UIKeyInputEscape]) {
     [description appendString:@"⎋"];
-  else if ([self.input isEqualToString:@" "])
+  } else if ([self.input isEqualToString:@" "]) {
     [description appendString:@"␣"];
-  else
+  } else {
     [description appendString:[self.input uppercaseString]];
+  }
   return description;
 }
 
--- ios/chrome/browser/ui/keyboard/UIKeyCommand+ChromeTest.mm
+++ ios/chrome/browser/ui/keyboard/UIKeyCommand+ChromeTest.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <objc/runtime.h>
 
-#import "base/i18n/rtl.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
@@ -134,26 +133,6 @@ TEST_F(UIKeyCommandChromeTest, Factories) {
   Verify(UIKeyCommand.cr_closeAll, @"⇧⌘W", @"keyCommand_closeAll",
          IDS_IOS_KEYBOARD_CLOSE_ALL);
   Verify(UIKeyCommand.cr_undo, @"⌘Z", @"keyCommand_undo");
-
-  // Prior to iOS 15, RTL needs to be handled manually. Check it for key
-  // commands that need to adapt.
-  if (@available(iOS 15.0, *)) {
-    // Nothing to do on iOS 15+.
-  } else {
-    base::i18n::SetRTLForTesting(true);
-    Verify(UIKeyCommand.cr_showNextTab_2, @"⌘{", @"keyCommand_showNextTab");
-    Verify(UIKeyCommand.cr_showPreviousTab_2, @"⌘}",
-           @"keyCommand_showPreviousTab");
-    Verify(UIKeyCommand.cr_showNextTab_3, @"⌥⌘←", @"keyCommand_showNextTab");
-    Verify(UIKeyCommand.cr_showPreviousTab_3, @"⌥⌘→",
-           @"keyCommand_showPreviousTab");
-    Verify(UIKeyCommand.cr_back, @"⌘]", @"keyCommand_back",
-           IDS_IOS_KEYBOARD_HISTORY_BACK);
-    Verify(UIKeyCommand.cr_forward, @"⌘[", @"keyCommand_forward",
-           IDS_IOS_KEYBOARD_HISTORY_FORWARD);
-    Verify(UIKeyCommand.cr_back_2, @"⌘→", @"keyCommand_back");
-    Verify(UIKeyCommand.cr_forward_2, @"⌘←", @"keyCommand_forward");
-  }
 }
 
 // Checks that modifiers in the symbolic description are correct (correct symbol
