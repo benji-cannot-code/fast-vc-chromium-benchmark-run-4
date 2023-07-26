c28d429b791d7f3101acd631fc05970553474759
Lily Chen <chlily@chromium.org>
2023-07-26 19:10:02 +0000

    [ImprovedDownloadWarnings] Update strings for main view header/footer
    This updates the download bubble main view header/footer and tooltip
    strings when kImprovedDownloadBubbleWarnings is enabled.
    
    The string changes reflect the reconceptualization of the download list
    as a list of download *history*.
    
    Screenshots:
    https://drive.google.com/drive/folders/1p1HX_BJVdlWGqZSgXhzJ8NwlwAPRLXtH
    
    Bug: 1465966
    Change-Id: I964dea9815bbfec34fd43fd25431459836dd21ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721610
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175587}

--- chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
+++ chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/feature_engagement/public/feature_constants.h"
+#include "components/safe_browsing/core/common/features.h"
 #include "components/user_education/common/user_education_class_properties.h"
 #include "content/public/browser/browser_thread.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -514,7 +515,10 @@ void DownloadToolbarButtonView::CreateBubbleDialogDelegate() {
   auto bubble_delegate = std::make_unique<views::BubbleDialogDelegate>(
       this, views::BubbleBorder::TOP_RIGHT);
   bubble_delegate->SetTitle(
-      l10n_util::GetStringUTF16(IDS_DOWNLOAD_BUBBLE_HEADER_TEXT));
+      base::FeatureList::IsEnabled(
+          safe_browsing::kImprovedDownloadBubbleWarnings)
+          ? l10n_util::GetStringUTF16(IDS_DOWNLOAD_BUBBLE_HEADER_LABEL)
+          : l10n_util::GetStringUTF16(IDS_DOWNLOAD_BUBBLE_HEADER_TEXT));
   bubble_delegate->SetShowTitle(false);
   bubble_delegate->SetShowCloseButton(false);
   bubble_delegate->SetButtons(ui::DIALOG_BUTTON_NONE);
