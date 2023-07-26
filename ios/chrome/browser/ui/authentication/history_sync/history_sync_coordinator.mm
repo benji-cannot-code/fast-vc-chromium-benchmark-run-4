df07ca297afe7951ed2132785fee9302d831a78d
Menghan YANG <myuu@google.com>
2023-07-26 16:02:43 +0000

    [iOS][FRE] Hide History Sync Opt-In if sync or history types disabled
    The FRE History Sync Opt-In screen shouldn't be shown if sync for all
    data types or tabs/history sync is disabled by enterprise policies.
    This CL makes the screen skipped in those cases and adds EGTests for
    them.
    
    Bug: 1447014
    Change-Id: Ifc5f289d4e7414c46b2d97de7784353e26ca9a49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4711786
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175476}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_coordinator.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_coordinator.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/signin/identity_manager_factory.h"
 #import "ios/chrome/browser/sync/sync_service_factory.h"
+#import "ios/chrome/browser/ui/authentication/enterprise/enterprise_utils.h"
 #import "ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.h"
 #import "ios/chrome/browser/ui/authentication/history_sync/history_sync_view_controller.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_constants.h"
@@ -70,24 +71,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
-  _viewController = [[HistorySyncViewController alloc] init];
-  _viewController.delegate = self;
-  ChromeAccountManagerService* chromeAccountManagerService =
-      ChromeAccountManagerServiceFactory::GetForBrowserState(browserState);
-  signin::IdentityManager* identityManager =
-      IdentityManagerFactory::GetForBrowserState(browserState);
+  // Skip History Sync Opt-in if sync is disabled, or if history or tabs sync
+  // is disabled by policy.
   syncer::SyncService* syncService =
       SyncServiceFactory::GetForBrowserState(browserState);
-  _mediator = [[HistorySyncMediator alloc]
-      initWithAuthenticationService:authenticationService
-        chromeAccountManagerService:chromeAccountManagerService
-                    identityManager:identityManager
-                        syncService:syncService];
-  _mediator.consumer = _viewController;
-  _mediator.delegate = self;
-  if (_firstRun) {
-    _viewController.modalInPresentation = YES;
+  if (IsSyncDisabledByPolicy(syncService) ||
+      IsManagedSyncDataType(syncService, syncer::UserSelectableType::kTabs) ||
+      IsManagedSyncDataType(syncService,
+                            syncer::UserSelectableType::kHistory)) {
+    [_delegate closeHistorySyncCoordinator:self];
+    return;
   }
+
   syncer::SyncUserSettings* syncUserSettings = syncService->GetUserSettings();
   if (syncUserSettings->GetSelectedTypes().Has(
           syncer::UserSelectableType::kHistory)) {
@@ -98,6 +93,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_delegate closeHistorySyncCoordinator:self];
     return;
   }
+
+  _viewController = [[HistorySyncViewController alloc] init];
+  _viewController.delegate = self;
+  ChromeAccountManagerService* chromeAccountManagerService =
+      ChromeAccountManagerServiceFactory::GetForBrowserState(browserState);
+  signin::IdentityManager* identityManager =
+      IdentityManagerFactory::GetForBrowserState(browserState);
+  _mediator = [[HistorySyncMediator alloc]
+      initWithAuthenticationService:authenticationService
+        chromeAccountManagerService:chromeAccountManagerService
+                    identityManager:identityManager
+                        syncService:syncService];
+  _mediator.consumer = _viewController;
+  _mediator.delegate = self;
+  if (_firstRun) {
+    _viewController.modalInPresentation = YES;
+  }
+
   BOOL animated = self.baseNavigationController.topViewController != nil;
   [self.baseNavigationController setViewControllers:@[ _viewController ]
                                            animated:animated];
