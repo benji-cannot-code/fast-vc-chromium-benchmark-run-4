c23ecce5986564c07b3466184cc3aa9f5dc192c3
Luis Juan Sanchez Padilla <lusanpad@microsoft.com>
2023-07-26 20:16:44 +0000

    Do not show Eye Dropper if the owner frame is not active.
    Currently, while the Eye Dropper is dismissed when a window focus change happens, it can still be opened if the parent frame is not focused on the time of invocation. This gives opening to a scenario in which a different Chrome window is focused but no cursor is shown on the screen as the Eye Dropper grid is being rendered behind the focused window. Attackers could use this opportunity to trick the user into clicking different parts of the screen, such as having the user grant unwanted permissions. This change fixes this issue by ensuring that the frame view is focused when the Eye Dropper is shown.
    
    Bug: 1462723
    Change-Id: I5f7ca420f2cedd07ce03b70966b64ad200bd32df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4720045
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175644}

--- chrome/browser/ui/views/eye_dropper/eye_dropper_browsertest.cc
+++ chrome/browser/ui/views/eye_dropper/eye_dropper_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/test/test_browser_ui.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/eye_dropper.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "ui/display/display_switches.h"
@@ -40,6 +41,7 @@ class EyeDropperBrowserTest : public UiBrowserTest,
                                                  ->tab_strip_model()
                                                  ->GetActiveWebContents()
                                                  ->GetPrimaryMainFrame();
+    parent_frame->GetView()->Focus();
     eye_dropper_ = ShowEyeDropper(parent_frame, /*listener=*/nullptr);
 #endif
   }
--- chrome/browser/ui/views/eye_dropper/eye_dropper_view_aura.cc
+++ chrome/browser/ui/views/eye_dropper/eye_dropper_view_aura.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "ui/aura/client/cursor_client.h"
 #include "ui/aura/client/focus_change_observer.h"
 #include "ui/aura/client/focus_client.h"
@@ -149,7 +150,7 @@ float EyeDropperView::GetDiameter() const {
 std::unique_ptr<content::EyeDropper> ShowEyeDropper(
     content::RenderFrameHost* frame,
     content::EyeDropperListener* listener) {
-  return features::IsEyeDropperEnabled()
+  return (features::IsEyeDropperEnabled() && frame->GetView()->HasFocus())
              ? std::make_unique<EyeDropperView>(frame, listener)
              : nullptr;
 }
