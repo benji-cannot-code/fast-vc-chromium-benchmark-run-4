c48cec85dc35d3118706a4fd2b7811a4138baaa8
Chen-Yu Tsai <wenst@chromium.org>
2023-07-26 06:48:29 +0000

    media/gpu/v4l2: Include base/task/bind_post_task.h in V4L2StatefulVD
    V4L2StatefulVD uses base::BindPostTaskToCurrentDefault(), but doesn't
    include its header file directly. This causes build errors on Linux:
    
    ../../media/gpu/v4l2/v4l2_stateful_video_decoder.cc:657:17: error: no member named 'BindPostTaskToCurrentDefault' in namespace 'base'
      657 |           base::BindPostTaskToCurrentDefault(base::BindOnce(
          |           ~~~~~~^
    ../../media/gpu/v4l2/v4l2_stateful_video_decoder.cc:700:51: error: no member named 'BindPostTaskToCurrentDefault' in namespace 'base'
      700 |       wrapped_frame->AddDestructionObserver(base::BindPostTaskToCurrentDefault(
          |                                             ~~~~~~^
    
    Explicitly include its header file to fix this.
    
    Bug: b:258331312, b:291169645
    Change-Id: I5b24be83cfbe028429486704f20f078936e930e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4710350
    Commit-Queue: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175261}

--- media/gpu/v4l2/v4l2_stateful_video_decoder.cc
+++ media/gpu/v4l2/v4l2_stateful_video_decoder.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ptr_util.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "media/base/media_log.h"
