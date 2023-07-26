9d975d98f2f95dcecfef14ed11603ed9eed91075
Ana Salazar <anasalazar@chromium.org>
2023-07-26 23:30:30 +0000

    Add feature for glanceables trusted testers
    Bug: None
    Change-Id: I152672e7fc56d4204e831e1a4c0b9adfeff6c487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722947
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175764}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1193,6 +1193,11 @@ BASE_FEATURE(kGlanceablesV2,
              "GlanceablesV2",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables glanceables on time management surface for trusted testers.
+BASE_FEATURE(kGlanceablesV2TrustedTesters,
+             "GlanceablesV2TrustedTesters",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables the Gaia reauth endpoint.
 BASE_FEATURE(kGaiaReauthEndpoint,
              "GaiaReauthEndpoint",
@@ -3150,6 +3155,10 @@ bool AreGlanceablesEnabled() {
 
 bool AreGlanceablesV2Enabled() {
   return base::FeatureList::IsEnabled(kGlanceablesV2);
+}
+
+bool AreGlanceablesV2TrustedTestersEnabled() {
+  return base::FeatureList::IsEnabled(kGlanceablesV2TrustedTesters);
 }
 
 bool IsHibernateEnabled() {
