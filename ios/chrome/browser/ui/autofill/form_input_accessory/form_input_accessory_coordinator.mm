097073eb4c235c40924d4853a813c4002f5e2a7c
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-26 15:00:43 +0000

    [iOS] alertCoordinator is stopped
    According to ChromeCoordinator invariant, all coordinators should be
    stopped before being deallocated. This CL ensures that whatever the user chose, the coordinator is stopped; whether they tap a button or
    dismiss by tapping outside (which call the cancel button)
    
    Bug: 1466233
    Change-Id: I657ecc8695f34168c7ee0124e23e6470404059df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701190
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175438}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.mm
@@ -508,6 +508,11 @@ const CGFloat kIPHVerticalOffset = -5;
 
 #pragma mark - Private
 
+- (void)dismissAlertCoordinator {
+  [self.alertCoordinator stop];
+  self.alertCoordinator = nil;
+}
+
 - (ChromeBrowserState*)browserState {
   return self.browser ? self.browser->GetBrowserState() : nullptr;
 }
@@ -546,11 +551,14 @@ const CGFloat kIPHVerticalOffset = -5;
                          browser:self.browser
                            title:title
                          message:message];
+  [self.childCoordinators addObject:self.alertCoordinator];
 
   __weak __typeof__(self) weakSelf = self;
 
   [self.alertCoordinator addItemWithTitle:l10n_util::GetNSString(IDS_CANCEL)
-                                   action:nil
+                                   action:^{
+                                     [weakSelf dismissAlertCoordinator];
+                                   }
                                     style:UIAlertActionStyleCancel];
 
   NSString* actionTitle =
@@ -558,6 +566,7 @@ const CGFloat kIPHVerticalOffset = -5;
   [self.alertCoordinator addItemWithTitle:actionTitle
                                    action:^{
                                      [weakSelf showAllPasswords];
+                                     [weakSelf dismissAlertCoordinator];
                                    }
                                     style:UIAlertActionStyleDefault];
 
