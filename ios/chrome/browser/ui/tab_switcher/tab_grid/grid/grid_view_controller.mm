b58d2e2c34cf53546266b1e8b86f1f8b706e8758
Louis Romero <lpromero@google.com>
2023-07-26 12:42:14 +0000

    Add configuration helpers
    This CL adds methods to configure different headers. This is useful
    prior to using collection view registration.
    
    Bug: 1467716
    Change-Id: I954d2c8aa378e21747b874bb275a59170f6245b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4717692
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175377}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -662,15 +662,7 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
                              withReuseIdentifier:
                                  kInactiveTabsButtonHeaderIdentifier
                                     forIndexPath:indexPath];
-      header.parent = self;
-      __weak __typeof(self) weakSelf = self;
-      header.buttonAction = ^{
-        [weakSelf didTapInactiveTabsButton];
-      };
-      [header configureWithDaysThreshold:self.inactiveTabsDaysThreshold];
-      if (IsShowInactiveTabsCountEnabled()) {
-        [header configureWithCount:self.inactiveTabsCount];
-      }
+      [self configureInactiveTabsButtonHeader:header];
       return header;
     }
     case TabGridModeSelection:
@@ -685,9 +677,7 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
         case kOpenTabsSectionIndex: {
           headerView.title = l10n_util::GetNSString(
               IDS_IOS_TABS_SEARCH_OPEN_TABS_SECTION_HEADER_TITLE);
-          NSString* resultsCount = [NSString
-              stringWithFormat:@"%ld",
-                               base::checked_cast<NSInteger>(self.items.count)];
+          NSString* resultsCount = [@(self.items.count) stringValue];
           headerView.value =
               l10n_util::GetNSStringF(IDS_IOS_TABS_SEARCH_OPEN_TABS_COUNT,
                                       base::SysNSStringToUTF16(resultsCount));
@@ -710,13 +700,30 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
                              withReuseIdentifier:
                                  kInactiveTabsPreambleHeaderIdentifier
                                     forIndexPath:indexPath];
-      __weak __typeof(self) weakSelf = self;
-      header.settingsLinkAction = ^{
-        [weakSelf didTapInactiveTabsSettingsLink];
-      };
-      header.daysThreshold = self.inactiveTabsDaysThreshold;
+      [self configureInactiveTabsPreambleHeader:header];
       return header;
   }
+}
+
+- (void)configureInactiveTabsButtonHeader:(InactiveTabsButtonHeader*)header {
+  header.parent = self;
+  __weak __typeof(self) weakSelf = self;
+  header.buttonAction = ^{
+    [weakSelf didTapInactiveTabsButton];
+  };
+  [header configureWithDaysThreshold:self.inactiveTabsDaysThreshold];
+  if (IsShowInactiveTabsCountEnabled()) {
+    [header configureWithCount:self.inactiveTabsCount];
+  }
+}
+
+- (void)configureInactiveTabsPreambleHeader:
+    (InactiveTabsPreambleHeader*)header {
+  __weak __typeof(self) weakSelf = self;
+  header.settingsLinkAction = ^{
+    [weakSelf didTapInactiveTabsSettingsLink];
+  };
+  header.daysThreshold = self.inactiveTabsDaysThreshold;
 }
 
 - (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView
@@ -2059,8 +2066,7 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
                                         inSection:kOpenTabsSectionIndex]];
   if (!headerView)
     return;
-  NSString* resultsCount = [NSString
-      stringWithFormat:@"%ld", base::checked_cast<NSInteger>(self.items.count)];
+  NSString* resultsCount = [@(self.items.count) stringValue];
   headerView.value =
       l10n_util::GetNSStringF(IDS_IOS_TABS_SEARCH_OPEN_TABS_COUNT,
                               base::SysNSStringToUTF16(resultsCount));
@@ -2209,10 +2215,7 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
                               atIndexPath:indexPath]);
   // Note: At this point, `header` could be nil if not visible, or if the
   // supplementary view is not an InactiveTabsButtonHeader.
-  [header configureWithDaysThreshold:self.inactiveTabsDaysThreshold];
-  if (IsShowInactiveTabsCountEnabled()) {
-    [header configureWithCount:self.inactiveTabsCount];
-  }
+  [self configureInactiveTabsButtonHeader:header];
 }
 
 // Reconfigures the Inactive Tabs preamble header.
