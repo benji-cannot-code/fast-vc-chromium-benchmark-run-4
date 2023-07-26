d8ff043f83bc6bc73de72a3386d1affdf91614a3
Naman Narula <namankumarnarula@gmail.com>
2023-07-26 16:38:37 +0000

    Added copyright header Renamed variable in json_document_test.cc to lowercase
    This is a follow-up CL for https://chromium-review.googlesource.com/c/chromium/src/+/4659270
    
    Change-Id: I1741a999644d9dd0d0876d2ac40225fe47e20a58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4712547
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175496}

--- third_party/blink/renderer/core/html/json_document.h
+++ third_party/blink/renderer/core/html/json_document.h
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_JSON_DOCUMENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_JSON_DOCUMENT_H_
 
--- third_party/blink/renderer/core/html/json_document_test.cc
+++ third_party/blink/renderer/core/html/json_document_test.cc
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "third_party/blink/renderer/core/html/json_document.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
@@ -12,10 +16,10 @@ class JSONDocumentTest : public SimTest {
  public:
   void SetUp() override { SimTest::SetUp(); }
 
-  void LoadResource(const String& JSON) {
+  void LoadResource(const String& json) {
     SimRequest request("https://foobar.com", "application/json");
     LoadURL("https://foobar.com");
-    request.Complete(JSON);
+    request.Complete(json);
     Compositor().BeginFrame();
   }
   void ClickPrettyPrintCheckbox() {
