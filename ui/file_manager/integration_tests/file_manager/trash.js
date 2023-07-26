3bc31d05cb6aa4101d7e853484b2ac67e5b1ac7d
Cassy Chun-Crogan <cassycc@google.com>
2023-07-26 05:08:51 +0000

    [FilesTrash] Disable invoke-sharesheet action when in Trash root
    Files in Trash are not actually representative of the file on disk. They
    are actually the name in their metadata + the location on disk. The
    invoke-sharesheet (Share) action is not feasible and has been disabled
    whilst in the Trash root.
    
    Before: https://screenshot.googleplex.com/86PiwA5ucefnbvR.png
    After: https://screenshot.googleplex.com/9UqPHjFsfzn2L7g.png
    
    Bug: b/272643692
    Change-Id: I3f4991ab91e95fa483ef3ba58e5a2c1d28cc945f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719805
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175243}

--- ui/file_manager/integration_tests/file_manager/trash.js
+++ ui/file_manager/integration_tests/file_manager/trash.js
@@ -1059,6 +1059,7 @@ testcase.trashInfeasibleActionsForFileDisabledAndHiddenInTrashRoot =
     'copy',
     'zip-selection',
     'set-wallpaper',
+    'invoke-sharesheet',
   ];
   for (const action of infeasibleActions) {
     await remoteCall.waitForElement(
