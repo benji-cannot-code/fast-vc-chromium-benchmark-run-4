d717fcfc8724ddbc3ea6a804dea44da6848ec24d
Xi Han <hanxi@google.com>
2023-07-26 20:21:07 +0000

    Fix RE at top.ToolbarLayout.draw(ToolbarLayout.java)
    Clean up the HomeButton which seems the only ImageView in ToolbarLayout.
    
    Bug: 1461758
    Change-Id: I1838c000bb15839a6aff62c6ebfc70964ec65e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721769
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175646}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/HomeButton.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/HomeButton.java
@@ -12,7 +12,6 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.VisibleForTesting;
-import androidx.core.content.ContextCompat;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ResettersForTesting;
@@ -43,9 +42,6 @@ public class HomeButton extends ListMenuButton {
 
     public HomeButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        final int homeButtonIcon = R.drawable.btn_toolbar_home;
-        setImageDrawable(ContextCompat.getDrawable(context, homeButtonIcon));
     }
 
     /**
