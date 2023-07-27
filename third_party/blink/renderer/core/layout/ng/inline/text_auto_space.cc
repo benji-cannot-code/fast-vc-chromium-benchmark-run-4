d5bbecac8d04e58a4291d18abc1441bd16129ddb
Koji Ishii <kojii@chromium.org>
2023-07-27 06:10:41 +0000

    [text-autospace] Fix when `no-autospace`
    This patch fixes to reset `last_type` when there's an item
    with `no-autospace`.
    
    This patch has no behavior changes as changes are under a
    runtime flag.
    
    Bug: 1463890
    Change-Id: I1d10f536ec79746518f7716e84d06abca73021aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722150
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175878}

--- third_party/blink/renderer/core/layout/ng/inline/text_auto_space.cc
+++ third_party/blink/renderer/core/layout/ng/inline/text_auto_space.cc
@@ -103,6 +103,7 @@ void TextAutoSpace::ApplyIfNeeded(NGInlineItemsData& data,
     const ComputedStyle* style = item.Style();
     DCHECK(style);
     if (UNLIKELY(style->TextAutospace() != ETextAutospace::kNormal)) {
+      last_type.reset();
       continue;
     }
 
