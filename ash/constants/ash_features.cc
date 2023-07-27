0dea303d6663c4146ea90fcf4d034820554333b6
Masayuki Fujita <massan@google.com>
2023-07-27 06:49:59 +0000

    pip-pinch: Add feature flag to enable/disable pip pinch to resize
    Bug: b/284376227
    Test: Comfirmed flag appears in os://flags
    Change-Id: I3292ed910de0ca39821077b093bfb6bb251615a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719445
    Auto-Submit: Masayuki Fujita <massan@google.com>
    Reviewed-by: Kazuki Takise <takise@chromium.org>
    Commit-Queue: Masayuki Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175894}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1972,6 +1972,11 @@ BASE_FEATURE(kPipDoubleTapToResize,
              "PipDoubleTapToResize",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables or disables pinch to resize feature for Picture in Picture.
+BASE_FEATURE(kPipPinchToResize,
+             "PipPinchToResize",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Controls whether policy provided trust anchors are allowed at the lock
 // screen.
 BASE_FEATURE(kPolicyProvidedTrustAnchorsAllowedAtLockScreen,
@@ -3622,6 +3627,10 @@ bool IsPinAutosubmitFeatureEnabled() {
 
 bool IsPipDoubleTapToResizeEnabled() {
   return base::FeatureList::IsEnabled(kPipDoubleTapToResize);
+}
+
+bool IsPipPinchToResizeEnabled() {
+  return base::FeatureList::IsEnabled(kPipPinchToResize);
 }
 
 bool IsPrinterSettingsPrinterStatusEnabled() {
