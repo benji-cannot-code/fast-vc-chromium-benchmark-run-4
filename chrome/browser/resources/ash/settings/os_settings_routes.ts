92c6624509b71037b7517b8fa8bfe3073270565c
Wes Okuhara <wesokuhara@google.com>
2023-07-27 16:31:59 +0000

    CrOS Settings: Define System Preferences Section
    To represent the new System Preferences category under the Settings
    Revamp, `kSystemPreferences` is defined in the Section Mojo enum and the
    corresponding skeleton Section class is created. Additionally, the
    corresponding route and page availability entry are also defined.
    
    Bug: b/292678609
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: Ief71a5eee7cc5d70a7d20fd5752f104833a24502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4717479
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176113}

--- chrome/browser/resources/ash/settings/os_settings_routes.ts
+++ chrome/browser/resources/ash/settings/os_settings_routes.ts
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
-import {androidAppsVisible, isArcVmEnabled, isCrostiniSupported, isGuest, isKerberosEnabled, isPluginVmAvailable, isPowerwashAllowed} from './common/load_time_booleans.js';
+import {androidAppsVisible, isArcVmEnabled, isCrostiniSupported, isGuest, isKerberosEnabled, isPluginVmAvailable, isPowerwashAllowed, isRevampWayfindingEnabled} from './common/load_time_booleans.js';
 import * as routesMojom from './mojom-webui/routes.mojom-webui.js';
 
 /**
@@ -214,6 +214,7 @@ export interface OsSettingsRoutes extends MinimumRoutes {
   STYLUS: Route;
   SYNC: Route;
   SYNC_ADVANCED: Route;
+  SYSTEM_PREFERENCES: Route;
 }
 
 function createSection(
@@ -606,6 +607,12 @@ export function createRoutes(): OsSettingsRoutes {
   r.ABOUT_DETAILED_BUILD_INFO = createSubpage(
       r.ABOUT, routesMojom.DETAILED_BUILD_INFO_SUBPAGE_PATH,
       Subpage.kDetailedBuildInfo);
+
+  if (isRevampWayfindingEnabled()) {
+    r.SYSTEM_PREFERENCES = createSection(
+        r.BASIC, routesMojom.SYSTEM_PREFERENCES_SECTION_PATH,
+        Section.kSystemPreferences);
+  }
 
   return r as OsSettingsRoutes;
 }
