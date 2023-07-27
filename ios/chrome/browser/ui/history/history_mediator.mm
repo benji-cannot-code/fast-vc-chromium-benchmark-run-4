9af55509835f99407a7cb24482b0ebec57c99246
Gauthier Ambard <gambard@chromium.org>
2023-07-27 18:24:17 +0000

    [iOS] Add CHECKs for null services
    It seems that in some cases the faviconLoader in the HistoryMediator
    is nil. Adds some CHECK to try to capture the null before.
    
    Fixed: 1466952
    Change-Id: I0681baac52f55d9c19523d72341a1d48312c45dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4720794
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176185}

--- ios/chrome/browser/ui/history/history_mediator.mm
+++ ios/chrome/browser/ui/history/history_mediator.mm
@@ -27,9 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithBrowserState:(ChromeBrowserState*)browserState {
   self = [super init];
   if (self) {
+    CHECK(browserState);
     _browserState = browserState;
     _faviconLoader =
         IOSChromeFaviconLoaderFactory::GetForBrowserState(_browserState);
+    CHECK(_faviconLoader);
   }
   return self;
 }
