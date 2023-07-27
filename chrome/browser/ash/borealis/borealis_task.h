a6dd25e817a37095c7cb4e1796ad51e9db050522
Nicholas Hollingum <hollingum@google.com>
2023-07-27 04:33:02 +0000

    borealis: Fix the await task to complete synchronously
    This bug is caused by subscription_ still having default value when the
    SessionTracker synchronously invokes the callback. We fix it by removing
    the check on the subscription.
    
    Checking the subscription was just a trick to avoid double-calling
    complete, but that isn't really a problem since complete moves the
    callback.
    
    We *did* have a test for this case but, unlike its name suggests, the
    container isn't actually there before we add the callback. Fix this and
    add an assert.
    
    Bug: None
    Change-Id: Ia47c14e2ae6e3f22ae0399642f75ff8c8891a8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721068
    Reviewed-by: Lucy Qu <lqu@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175857}

--- chrome/browser/ash/borealis/borealis_task.h
+++ chrome/browser/ash/borealis/borealis_task.h
@@ -136,7 +136,7 @@ class AwaitBorealisStartup : public BorealisTask {
   void OnContainerStarted(BorealisContext* context, guest_os::GuestInfo info);
   void OnTimeout();
 
-  absl::optional<base::CallbackListSubscription> subscription_;
+  base::CallbackListSubscription subscription_;
   base::WeakPtrFactory<AwaitBorealisStartup> weak_factory_{this};
 };
 
