055c8818e4bd925434b2cf756e046cde75a4502d
jongmok kim <jongmok.kim@navercorp.com>
2023-07-27 03:30:44 +0000

    Cleanup header order
    Bug: None
    Change-Id: I59fc074a00c692d33f22129e0268577f04f64abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4689418
    Commit-Queue: jongmok kim <johny.kimc@gmail.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175843}

--- ui/base/models/image_model.cc
+++ ui/base/models/image_model.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <tuple>
 
-#include "base/functional/callback.h"
 #include "ui/base/models/image_model.h"
+
+#include "base/functional/callback.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/color/color_id.h"
 #include "ui/gfx/vector_icon_utils.h"
