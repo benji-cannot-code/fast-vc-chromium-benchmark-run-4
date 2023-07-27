569c3dd8f22f4a02e2acbebbc73e89af9f2a45ea
Matt Mueller <mattm@chromium.org>
2023-07-27 17:16:25 +0000

    use CertBuilder in CertVerifyProcInternalTest.RejectWeakKeys and X509CertificatePublicKeyInfoTest.GetPublicKeyInfo
    Removes the generate-weak-test-chains.sh certs, and the large_key.pem
    cert (from generate-test-certs.sh).
    
    Instead, add some pre-generated private keys that can be used with
    CertBuilder to generate appropriate certs on the fly without the cost of
    generating RSA keys at test runtime.
    
    Bug: 1266634
    Change-Id: If8417ca398ffc73a699e6fbd785a6461294c67d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722648
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176134}

---
+++ net/data/ssl/scripts/generate-test-keys.sh
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This scripts generates a set of keys of various sizes for use in tests that
+# need a key without having to pay the cost of generating one at runtime.
+
+set -e
+
+function genpkey {
+  alg="$1"
+  optname="$2"
+  optval="$3"
+  n="$4"
+  filename="../certificates/$alg-$optval-$n.key"
+  if ! grep -q -- '-----BEGIN.*PRIVATE KEY-----' "$filename" ; then
+    echo "generating $filename ..."
+    openssl genpkey -algorithm "$alg" -pkeyopt "$optname:$optval" \
+      -out "$filename"
+  else
+    echo "$filename already exists, skipping"
+  fi
+}
+
+for size in 768 1024 2048
+do
+  for i in 1 2 3
+  do
+    genpkey rsa rsa_keygen_bits "$size" "$i"
+  done
+done
+
+genpkey rsa rsa_keygen_bits 8200 1
+
+
+for curve in prime256v1
+do
+  for i in 1 2 3
+  do
+    genpkey ec ec_paramgen_curve "$curve" "$i"
+  done
+done
