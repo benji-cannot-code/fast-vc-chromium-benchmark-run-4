9010051d069aaea4a7f9b9951cc05056cbdc6ef3
Danny Wang <wangdanny@google.com>
2023-07-27 20:30:10 +0000

    customization: Convert button remapping mojom object to dict
    This CL converts the button remapping mojom object to dict and tests
    different cases for the button remapping object.
    
    Bug: b/241965717
    Test: ash_unittests ConvertButtonRemappingToDict
    Change-Id: Idc34bc6990d290819a24af89ffcc430299575e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654791
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176257}

--- ash/system/input_device_settings/input_device_settings_utils_unittest.cc
+++ ash/system/input_device_settings/input_device_settings_utils_unittest.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 
+#include "ash/public/mojom/input_device_settings.mojom.h"
+#include "ash/system/input_device_settings/input_device_settings_pref_names.h"
 #include "base/values.h"
 #include "components/account_id/account_id.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -23,6 +25,29 @@ namespace {
 constexpr char kTestPrefKey[] = "test_key";
 const AccountId account_id = AccountId::FromUserEmail("example@email.com");
 
+const mojom::ButtonRemapping button_remapping1(
+    /*name=*/"test1",
+    /*button=*/
+    mojom::Button::NewCustomizableButton(mojom::CustomizableButton::kBack),
+    /*remapping_action=*/
+    mojom::RemappingAction::NewAction(ash::AcceleratorAction::kBrightnessDown));
+const mojom::ButtonRemapping button_remapping2(
+    /*name=*/"test2",
+    /*button=*/
+    mojom::Button::NewCustomizableButton(mojom::CustomizableButton::kLeft),
+    /*remapping_action=*/
+    mojom::RemappingAction::NewKeyEvent(
+        mojom::KeyEvent::New(::ui::KeyboardCode::VKEY_0, 1, 2, 3)));
+const mojom::ButtonRemapping button_remapping3(
+    /*name=*/"test3",
+    /*button=*/mojom::Button::NewVkey(::ui::KeyboardCode::VKEY_1),
+    /*remapping_action=*/
+    mojom::RemappingAction::NewKeyEvent(
+        mojom::KeyEvent::New(::ui::KeyboardCode::VKEY_2, 4, 5, 6)));
+const mojom::ButtonRemapping button_remapping4(
+    /*name=*/"test4",
+    /*button=*/mojom::Button::NewVkey(::ui::KeyboardCode::VKEY_3),
+    /*remapping_action=*/nullptr);
 }  // namespace
 
 class DeviceKeyTest : public testing::TestWithParam<
@@ -61,6 +86,76 @@ TEST(GetLoginScreenSettingsDictTest, RetrieveSettingsDict) {
   const base::Value::Dict* valid_settings =
       GetLoginScreenSettingsDict(local_state.get(), account_id, kTestPrefKey);
   EXPECT_NE(nullptr, valid_settings);
+}
+
+TEST(ConvertButtonRemappingToDict, ConvertButtonRemappingToDict) {
+  const base::Value::Dict dict1 =
+      ConvertButtonRemappingToDict(button_remapping1);
+  EXPECT_EQ(button_remapping1.name,
+            *dict1.FindString(prefs::kButtonRemappingName));
+  EXPECT_EQ(
+      static_cast<int>(button_remapping1.button->get_customizable_button()),
+      *dict1.FindInt(prefs::kButtonRemappingCustomizableButton));
+  EXPECT_EQ(static_cast<int>(button_remapping1.remapping_action->get_action()),
+            *dict1.FindInt(prefs::kButtonRemappingAction));
+
+  const base::Value::Dict dict2 =
+      ConvertButtonRemappingToDict(button_remapping2);
+  EXPECT_EQ(button_remapping2.name,
+            *dict2.FindString(prefs::kButtonRemappingName));
+  EXPECT_EQ(
+      static_cast<int>(button_remapping2.button->get_customizable_button()),
+      *dict2.FindInt(prefs::kButtonRemappingCustomizableButton));
+  EXPECT_NE(nullptr, dict2.FindDict(prefs::kButtonRemappingKeyEvent));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping2.remapping_action->get_key_event()->dom_code),
+            *dict2.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingDomCode));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping2.remapping_action->get_key_event()->dom_key),
+            *dict2.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingDomKey));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping2.remapping_action->get_key_event()->modifiers),
+            *dict2.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingModifiers));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping2.remapping_action->get_key_event()->vkey),
+            *dict2.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingKeyboardCode));
+
+  const base::Value::Dict dict3 =
+      ConvertButtonRemappingToDict(button_remapping3);
+  EXPECT_EQ(button_remapping3.name,
+            *dict3.FindString(prefs::kButtonRemappingName));
+  EXPECT_EQ(static_cast<int>(button_remapping3.button->get_vkey()),
+            *dict3.FindInt(prefs::kButtonRemappingKeyboardCode));
+  EXPECT_NE(nullptr, dict3.FindDict(prefs::kButtonRemappingKeyEvent));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping3.remapping_action->get_key_event()->dom_code),
+            *dict3.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingDomCode));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping3.remapping_action->get_key_event()->dom_key),
+            *dict3.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingDomKey));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping3.remapping_action->get_key_event()->modifiers),
+            *dict3.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingModifiers));
+  EXPECT_EQ(static_cast<int>(
+                button_remapping3.remapping_action->get_key_event()->vkey),
+            *dict3.FindDict(prefs::kButtonRemappingKeyEvent)
+                 ->FindInt(prefs::kButtonRemappingKeyboardCode));
+
+  const base::Value::Dict dict4 =
+      ConvertButtonRemappingToDict(button_remapping4);
+  EXPECT_EQ(button_remapping4.name,
+            *dict4.FindString(prefs::kButtonRemappingName));
+  EXPECT_EQ(static_cast<int>(button_remapping4.button->get_vkey()),
+            *dict4.FindInt(prefs::kButtonRemappingKeyboardCode));
+  EXPECT_EQ(nullptr, dict4.FindDict(prefs::kButtonRemappingKeyEvent));
+  EXPECT_EQ(absl::nullopt, dict4.FindInt(prefs::kButtonRemappingAction));
 }
 
 }  // namespace ash
