3ff367a86422c572d96c3d0abb225b8d47fa2af7
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-07-27 17:56:52 +0000

    [PA] Remove nogncheck in dispatcher_unittest.cc
    `:partition_alloc_test_support` is already in `:base_unittests` deps.
    Nothing looks wrong to me. No need for this GN check opt-out.
    
    Bug: chromium:1467773
    Change-Id: I73254920c4208984ab337fc9f1dee848f91fd1c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4725144
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176165}

--- base/allocator/dispatcher/dispatcher_unittest.cc
+++ base/allocator/dispatcher/dispatcher_unittest.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #if BUILDFLAG(USE_PARTITION_ALLOC)
-#include "base/allocator/partition_allocator/partition_alloc_for_testing.h"  // nogncheck
+#include "base/allocator/partition_allocator/partition_alloc_for_testing.h"
 #endif
 
 #if BUILDFLAG(USE_ALLOCATOR_SHIM)
