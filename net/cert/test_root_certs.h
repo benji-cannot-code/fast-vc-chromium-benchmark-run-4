b4f6e77898edd295639d24de3d54c8a3742a2cbd
Matt Mueller <mattm@chromium.org>
2023-07-27 14:10:25 +0000

    Remove test_root_certs_win.cc
    The windows verifier is no longer used, so the windows-specific code
    for handling test root certs is no longer needed.
    
    Bug: 1380536
    Change-Id: I6699d758947e4eb5a8a65f5f36803b07a9d2d193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721902
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176029}

--- net/cert/test_root_certs.h
+++ net/cert/test_root_certs.h
@@ -12,11 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/pki/trust_store.h"
 #include "net/cert/pki/trust_store_in_memory.h"
 
-#if BUILDFLAG(IS_WIN)
-#include <windows.h>
-#include "base/win/wincrypt_shim.h"
-#include "crypto/scoped_capi_types.h"
-#elif BUILDFLAG(IS_IOS)
+#if BUILDFLAG(IS_IOS)
 #include <CoreFoundation/CFArray.h>
 #include <Security/SecTrust.h>
 #include "base/mac/scoped_cftyperef.h"
@@ -61,13 +57,6 @@ class NET_EXPORT TestRootCerts {
   // certificates stored in |temporary_roots_|. If IsEmpty() is true, this
   // does not modify |trust_ref|.
   OSStatus FixupSecTrustRef(SecTrustRef trust_ref) const;
-#elif BUILDFLAG(IS_WIN)
-  HCERTSTORE temporary_roots() const { return temporary_roots_; }
-
-  // Returns an HCERTCHAINENGINE suitable to be used for certificate
-  // validation routines, or NULL to indicate that the default system chain
-  // engine is appropriate.
-  crypto::ScopedHCERTCHAINENGINE GetChainEngine() const;
 #endif
 
   TrustStore* test_trust_store() { return &test_trust_store_; }
@@ -93,9 +82,7 @@ class NET_EXPORT TestRootCerts {
   bool AddImpl(X509Certificate* certificate);
   void ClearImpl();
 
-#if BUILDFLAG(IS_WIN)
-  HCERTSTORE temporary_roots_;
-#elif BUILDFLAG(IS_IOS)
+#if BUILDFLAG(IS_IOS)
   base::ScopedCFTypeRef<CFMutableArrayRef> temporary_roots_;
 #endif
 
