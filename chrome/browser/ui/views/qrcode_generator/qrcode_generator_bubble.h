9d6f5a387e1c23ced14bad987bf7ff2717616204
Elly Fong-Jones <ellyjones@chromium.org>
2023-07-27 17:19:55 +0000

    qrcode: use WeakPtr for WebContents
    This removes the last untriaged raw_ptr in this class.
    
    Despite extensive analysis, it is not clear to me that the pointed-to
    WebContents will always outlive the QRCodeGeneratorBubble. Even if that
    analysis *had* proven that the WebContents always outlives
    QRCodeGeneratorBubble, any future reader of this code would have to redo
    the analysis themselves or simply trust that my reasoning was correct
    and still valid, which seems like a bad deal. It's better to make the
    code obviously correct to a casual reader.
    
    Here's the analysis:
    The WebContents itself is owned by the TabStripModel and therefore
    indirectly by the Browser; its lifetime is partly under the control of
    the user and partly under the control of the website itself, due to the
    existence of window.close(). The WebContents is generally destroyed as
    part of tab closure.
    
    The QRCodeGeneratorBubble is owned (indirectly) by a views::Widget,
    which is in turn owned by a views::NativeWidget, which is owned by "the
    operating system", sort of. In particular, views::NativeWidget is
    usually destroyed as part of views::Widget::Close(), asynchronously.
    
    In theory, LocationBarBubbleDelegateView (a superclass of
    QRCodeGeneratorBubble) is responsible for closing that Widget, in
    response to WebContentsObserver::WebContentsDestroyed(). At the very
    least, that code appears to ensure that a close has *started* before the
    WebContents itself is destroyed. Since the close is asynchronous,
    though, it is not obvious that the close is *finished* before the
    WebContents is destroyed, which means the web_contents_ pointer may
    dangle temporarily during window teardown. To make matters worse, even
    after the window has started closing, queued UI events may still arrive,
    and I believe that QRCodeGeneratorBubble::DownloadButtonPressed()
    *could* still be reached while the Widget has not yet finished closing
    but the WebContents has been destroyed. How often this would happen in
    practice is an open question, but it is not certain (to me) that it's
    impossible.
    
    Fixed: 1467536
    Change-Id: I619498da899d5db5edd9dba5502919a3b77536fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722488
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176137}

--- chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble.h
+++ chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble.h
@@ -43,7 +43,7 @@ class QRCodeGeneratorBubble : public QRCodeGeneratorBubbleView,
  public:
   METADATA_HEADER(QRCodeGeneratorBubble);
   QRCodeGeneratorBubble(views::View* anchor_view,
-                        content::WebContents* web_contents,
+                        base::WeakPtr<content::WebContents> web_contents,
                         base::OnceClosure on_closing,
                         base::OnceClosure on_back_button_pressed,
                         const GURL& url);
@@ -160,8 +160,7 @@ class QRCodeGeneratorBubble : public QRCodeGeneratorBubbleView,
 
   base::OnceClosure on_closing_;
   base::OnceClosure on_back_button_pressed_;
-  raw_ptr<content::WebContents, AcrossTasksDanglingUntriaged>
-      web_contents_;  // weak.
+  base::WeakPtr<content::WebContents> web_contents_;
 };
 
 }  // namespace qrcode_generator
