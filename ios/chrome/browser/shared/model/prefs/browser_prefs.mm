00785b6f735229c1a0cfd6dfad3bc82bd93ba354
Christian Xu <christianxu@google.com>
2023-07-27 12:25:25 +0000

    [iOS][Toolbar] Add default omnibox position in toolbar mediator
    Set the default omnibox position in toolbar mediator. The omnibox is by
    default in the top toolbar, the kBottomOmniboxDefaultSetting feature
    changes the default omnibox position to:
    - Top (default)
    - Bottom
    - Bottom for Safari switcher
    
    Safari switcher is defined using the DeviceSwitcherResult, it will be
    defined in a future CL.
    
    Bug: 1467242
    Change-Id: I1d0bea248c7f2b5c0f73e436215ff6eba3696a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4712047
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175991}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -324,7 +324,7 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   [SigninCoordinator registerBrowserStatePrefs:registry];
   [SigninPromoViewMediator registerBrowserStatePrefs:registry];
 
-  registry->RegisterBooleanPref(prefs::kBottomOmnibox, true);
+  registry->RegisterBooleanPref(prefs::kBottomOmnibox, false);
   registry->RegisterBooleanPref(policy::policy_prefs::kPolicyTestPageEnabled,
                                 true);
   registry->RegisterBooleanPref(kDataSaverEnabled, false);
