9b8b160224dac4ab315cac7308816a2c5a118d25
Lily Chen <chlily@chromium.org>
2023-07-27 14:51:54 +0000

    Reland "[DownloadBubble] Fix notification behavior for Lacros tab fullscreen"
    This is a reland of commit cc3a8e9efbafefa405012d6d3d16c97da0a97a54
    
    The reland removes most of the test changes (those unrelated to the
    main change), and retains only the test for tab fullscreen which
    has behavior changes in this CL.
    
    The other tests will be rewritten at a later point.
    
    Original change's description:
    > [DownloadBubble] Fix notification behavior for Lacros tab fullscreen
    >
    > There are two types of fullscreen, browser fullscreen (where the whole
    > browser is fullscreen), and tab fullscreen (where an element on the web
    > page requests fullscreen). Before this patch, the behavior of browser
    > fullscreen downloads is that we show an exclusive access bubble with
    > wording instructing the user to exit fullscreen to view their downloads,
    > and then show the partial view upon exiting fullscreen. When not in
    > immersive fullscreen mode, tab fullscreen should work the same way.
    > However, this was not working properly in tab fullscreen on Lacros,
    > where the exclusive access bubble was not being shown.
    >
    > The issue was that we were passing an empty GURL to the exclusive access
    > context, which signifies browser (immersive on ChromeOS) fullscreen.
    > Since we always pass an empty GURL when notify_download is true, the
    > fix in this CL is to modify the logic in BrowserView so that the check
    > for a non-empty URL is skipped for notify_download == true. The logic
    > for immersive fullscreen is handled already by the download controller
    > anyway.
    >
    > Also adds interactive UI tests for both browser and tab fullscreen.
    >
    > Bug: 1361511
    > Change-Id: I9ac08ba46a60474db445d15e2394a8cc045ee531
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652317
    > Reviewed-by: Trent Apted <tapted@chromium.org>
    > Commit-Queue: Lily Chen <chlily@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1174447}
    
    Bug: 1361511
    Change-Id: Ie29e1ebc2ef4df90060ce83830f3d73a4d815605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722256
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176045}

--- chrome/browser/ui/views/exclusive_access_bubble_views.h
+++ chrome/browser/ui/views/exclusive_access_bubble_views.h
@@ -75,6 +75,8 @@ class ExclusiveAccessBubbleViews : public ExclusiveAccessBubble,
 
   gfx::SlideAnimation* animation_for_test() { return animation_.get(); }
 
+  bool IsVisibleForTesting() const { return IsVisible(); }
+
  private:
   // Starts or stops polling the mouse location based on |popup_| and
   // |bubble_type_|.
@@ -86,7 +88,7 @@ class ExclusiveAccessBubbleViews : public ExclusiveAccessBubble,
   void UpdateViewContent(ExclusiveAccessBubbleType bubble_type);
 
   // Returns whether the popup is visible.
-  bool IsVisible();
+  bool IsVisible() const;
 
   // Returns the root view containing |browser_view_|.
   views::View* GetBrowserRootView() const;
