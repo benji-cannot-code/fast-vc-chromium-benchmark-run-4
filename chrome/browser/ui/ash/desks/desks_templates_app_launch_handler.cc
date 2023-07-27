4e8502b5565cf3bf1e28df6567c35fe61ce49c92
zhumatthew <zhumatthew@google.com>
2023-07-27 23:55:56 +0000

    Ignore launching missing pWA
    When launching a floating workspace from another device onto a
    device that do not have PWAs in the template, it would launch them
    in a browser window, but does not actually fully load and the
    shelf icons are missing. When attempting to save the desk as a
    template, it would crash. We should ignore the missing PWA.
    
    BUG=b:293477863
    TEST=manually,DeskClients browser test
    
    Change-Id: I62a783842cae297f193eafe4baf7b8c364db0bea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4725245
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176339}

--- chrome/browser/ui/ash/desks/desks_templates_app_launch_handler.cc
+++ chrome/browser/ui/ash/desks/desks_templates_app_launch_handler.cc
@@ -30,10 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "components/app_constants/constants.h"
 #include "components/app_restore/app_restore_data.h"
+#include "components/app_restore/app_restore_utils.h"
 #include "components/app_restore/desk_template_read_handler.h"
 #include "components/app_restore/restore_data.h"
 #include "components/app_restore/window_info.h"
 #include "components/services/app_service/public/cpp/app_types.h"
+#include "components/services/app_service/public/cpp/types_util.h"
 #include "components/tab_groups/tab_group_info.h"
 #include "extensions/common/extension.h"
 
@@ -170,9 +172,9 @@ void DesksTemplatesAppLaunchHandler::LaunchBrowsers() {
   const auto& launch_list = restore_data()->app_id_to_launch_list();
   for (const auto& iter : launch_list) {
     const std::string& app_id = iter.first;
-    if (app_id != app_constants::kChromeAppId)
+    if (app_id != app_constants::kChromeAppId) {
       continue;
-
+    }
     for (const auto& window_iter : iter.second) {
       const std::unique_ptr<app_restore::AppRestoreData>& app_restore_data =
           window_iter.second;
@@ -185,6 +187,9 @@ void DesksTemplatesAppLaunchHandler::LaunchBrowsers() {
       const gfx::Rect current_bounds =
           app_restore_data->current_bounds.value_or(gfx::Rect());
       const std::string app_name = GetBrowserAppName(app_restore_data, app_id);
+      if (!app_name.empty() && !IsBrowserAppInstalled(app_name)) {
+        continue;
+      }
 
       Browser::CreateParams create_params =
           !app_name.empty()
@@ -264,16 +269,17 @@ void DesksTemplatesAppLaunchHandler::MaybeLaunchArcApps() {
       });
 
   // For each ARC app, check and see if there is an existing instance. We will
-  // move this instance over instead of launching a new one. Remove the app from
-  // the restore data if it was successfully moved so that the ARC launch
+  // move this instance over instead of launching a new one. Remove the app
+  // from the restore data if it was successfully moved so that the ARC launch
   // handler does not try to launch it later.
   for (const std::string& app_id : app_ids) {
     auto it = app_id_to_launch_list.find(app_id);
     DCHECK(it != app_id_to_launch_list.end());
     if (!ash::DesksController::Get()->OnSingleInstanceAppLaunchingFromSavedDesk(
             app_id, it->second)) {
-      for (auto& window : it->second)
+      for (auto& window : it->second) {
         NotifyMovedSingleInstanceApp(window.first);
+      }
       restore_data()->RemoveApp(app_id);
     }
   }
@@ -310,6 +316,10 @@ void DesksTemplatesAppLaunchHandler::MaybeLaunchLacrosBrowsers() {
       if (app_restore_data->urls.empty()) {
         continue;
       }
+      const std::string app_name = GetBrowserAppName(app_restore_data, app_id);
+      if (!app_name.empty() && !IsBrowserAppInstalled(app_name)) {
+        continue;
+      }
 
       // TODO(crbug.com/1442076): Remove after issue is root caused.
       windows_count++;
@@ -326,8 +336,8 @@ void DesksTemplatesAppLaunchHandler::MaybeLaunchLacrosBrowsers() {
           app_restore_data->active_tab_index.value_or(0),
           // Values of 0 will be ignored, other type constraints are
           // enforced on the browser side.
-          app_restore_data->first_non_pinned_tab_index.value_or(0),
-          GetBrowserAppName(app_restore_data, app_id), restore_window_id);
+          app_restore_data->first_non_pinned_tab_index.value_or(0), app_name,
+          restore_window_id);
     }
     // TODO(crbug.com/1442076): Remove after issue is root caused.
     LOG(ERROR) << windows_count
@@ -345,4 +355,21 @@ void DesksTemplatesAppLaunchHandler::RecordRestoredAppLaunch(
 void DesksTemplatesAppLaunchHandler::NotifyMovedSingleInstanceApp(
     int32_t window_id) {
   DesksClient::Get()->NotifyMovedSingleInstanceApp(window_id);
+}
+
+bool DesksTemplatesAppLaunchHandler::IsBrowserAppInstalled(
+    const std::string& app_name) {
+  apps::AppRegistryCache& cache =
+      apps::AppServiceProxyFactory::GetForProfile(profile())
+          ->AppRegistryCache();
+
+  std::string app_id = app_restore::GetAppIdFromAppName(app_name);
+  bool result = false;
+  cache.ForOneApp(app_id, [&result](const apps::AppUpdate& update) {
+    if (apps_util::IsInstalled(update.Readiness()) &&
+        update.AppType() != apps::AppType::kUnknown) {
+      result = true;
+    }
+  });
+  return result;
 }
