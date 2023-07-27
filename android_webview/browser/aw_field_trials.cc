6b84d1f4abc175f45b9586236ad798b39fdf2956
Boris Sazonov <bsazonov@chromium.org>
2023-07-27 14:55:35 +0000

    Revert "Enable passthrough command decoder by default for Android"
    This reverts commit 46f646e60d124dce28b3896a1ee00bd54d3dbd8d.
    
    Reason for revert: see https://crbug.com/1468263.
    
    Original change's description:
    > Enable passthrough command decoder by default for Android
    >
    > Bug: 1168587
    > Change-Id: I9a360bcab6d9eeae3b9dd73c4d3567216bed3a7b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705066
    > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1175715}
    
    Bug: 1168587, 1468263
    Change-Id: I41b6a378447ecd325844eaa61ef705c7e508a579
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4724987
    Owners-Override: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1176048}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "components/metrics/persistent_histograms.h"
 #include "third_party/blink/public/common/features.h"
-#include "ui/gl/gl_features.h"
 
 namespace {
 
@@ -65,10 +64,6 @@ void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
 
   // Disable user-agent client hints on WebView.
   aw_feature_overrides.DisableFeature(blink::features::kUserAgentClientHint);
-
-  // Disable the passthrough on WebView.
-  aw_feature_overrides.DisableFeature(
-      ::features::kDefaultPassthroughCommandDecoder);
 
   aw_feature_overrides.RegisterOverrides(feature_list);
 }
