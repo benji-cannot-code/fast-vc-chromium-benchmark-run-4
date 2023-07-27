31dd8aafecc35d927ea59231ca361db751af3c45
Longbo Wei <longbowei@google.com>
2023-07-27 20:59:27 +0000

    shortcuts: Ensure edit-dialog only opens on button click
    * on-click should be bound to <cr-icon-button> element rather than the
      "edit-icon-container". Otherwise, clicking the blank area around
      button will also open the edit-dialog.
    
    * Video
       - Before: http://go/scrcast/NTk5NDQwMDc2NDEzMzM3NnxlYzM0NjA1ZS1kZg
       - After:  http://go/scrcast/NTkxNDQ3Mjk1OTk2NzIzMnxmNWVlZDlkNC1jYw
    
    Bug: b/216049298
    Test: browser_tests
    Change-Id: Iabf65ada6a38d77eb72153d586882515deceadf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721023
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176279}

--- chrome/test/data/webui/chromeos/shortcut_customization/accelerator_subsection_test.ts
+++ chrome/test/data/webui/chromeos/shortcut_customization/accelerator_subsection_test.ts
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://shortcut-customization/js/accelerator_subsection.js';
 import 'chrome://webui-test/mojo_webui_test_support.js';
 
+import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
+import {CrIconButtonElement} from 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {AcceleratorLookupManager} from 'chrome://shortcut-customization/js/accelerator_lookup_manager.js';
@@ -158,9 +160,9 @@ suite('acceleratorSubsectionTest', function() {
     assertFalse(shortcutsAssignedElement.hidden);
 
     // Expect 'noShortcutsAssigned' has an edit button.
-    const editButtonElement = rowListElement[0]!.shadowRoot!.querySelector(
-                                  '.edit-icon-container') as HTMLDivElement;
-    assertTrue(!!editButtonElement);
+    const editButton = strictQuery(
+        '.edit-button', rowListElement[0]!.shadowRoot, CrIconButtonElement);
+    assertTrue(!!editButton);
 
     // Add event listend and verify clicking edit-button will open the dialog.
     let showDialogListenerCalled = false;
@@ -168,7 +170,7 @@ suite('acceleratorSubsectionTest', function() {
       showDialogListenerCalled = true;
     });
 
-    editButtonElement.click();
+    editButton.click();
     await flushTasks();
 
     // Expect the dialog is opened.
