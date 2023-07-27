16feb1b0f707dffc8dff23e81fb1fea9613d779a
Irem Uguz <iremuguz@google.com>
2023-07-27 14:26:17 +0000

    support-tool: Make chrome://support-tool strings translatable
    Replace the hardcoded string values with resource IDs for translation.
    Add screenshots of the string resource values.
    
    Remove background color from detected PII item checkboxes because it
    doesn't blend with the page background color.
    
    Bug: b:283941823
    Change-Id: Id9a1e2bb8a949942b185ce7b8574814512154139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666008
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176034}

--- chrome/browser/resources/support_tool/issue_details.ts
+++ chrome/browser/resources/support_tool/issue_details.ts
@@ -16,8 +16,6 @@ import {BrowserProxy, BrowserProxyImpl, IssueDetails} from './browser_proxy.js';
 import {getTemplate} from './issue_details.html.js';
 import {SupportToolPageMixin} from './support_tool_page_mixin.js';
 
-const DONT_INCLUDE_EMAIL: string = 'Do not include email address';
-
 const IssueDetailsElementBase = SupportToolPageMixin(PolymerElement);
 
 export class IssueDetailsElement extends IssueDetailsElementBase {
@@ -37,7 +35,7 @@ export class IssueDetailsElement extends IssueDetailsElementBase {
       },
       emails_: {
         type: Array,
-        value: () => [DONT_INCLUDE_EMAIL],
+        value: () => [],
       },
       issueDescription_: {
         type: String,
@@ -51,10 +49,11 @@ export class IssueDetailsElement extends IssueDetailsElementBase {
   }
 
   private caseId_: string;
-  private emails_: string[];
+  private emails_: string[] = [this.i18n('dontIncludeEmailAddress')];
   private issueDescription_: string;
   private selectedEmail_: string;
   private browserProxy_: BrowserProxy = BrowserProxyImpl.getInstance();
+
 
   override connectedCallback() {
     super.connectedCallback();
@@ -63,7 +62,7 @@ export class IssueDetailsElement extends IssueDetailsElementBase {
       this.emails_ = emails;
       // Add default email at the end of emails list for user to be able to
       // choose to not include email address.
-      this.emails_.push(DONT_INCLUDE_EMAIL);
+      this.emails_.push(this.i18n('dontIncludeEmailAddress'));
     });
   }
 
@@ -72,7 +71,8 @@ export class IssueDetailsElement extends IssueDetailsElementBase {
       caseId: this.caseId_,
       // Set emailAddress field to empty string if user selected to not include
       // email address.
-      emailAddress: (this.selectedEmail_ === DONT_INCLUDE_EMAIL) ?
+      emailAddress:
+          (this.selectedEmail_ === this.i18n('dontIncludeEmailAddress')) ?
           '' :
           this.selectedEmail_,
       issueDescription: this.issueDescription_,
--- chrome/browser/resources/support_tool/support_tool_page_mixin.ts
+++ chrome/browser/resources/support_tool/support_tool_page_mixin.ts
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * pages.
  */
 
+import {I18nMixin, I18nMixinInterface} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 type Constructor<T> = new (...args: any[]) => T;
@@ -15,7 +16,9 @@ type Constructor<T> = new (...args: any[]) => T;
 export const SupportToolPageMixin = dedupingMixin(
     <T extends Constructor<PolymerElement>>(superClass: T): T&
     Constructor<SupportToolPageMixinInterface> => {
-      class SupportToolPageMixin extends superClass implements
+      const superClassBase = I18nMixin(superClass);
+
+      class SupportToolPageMixin extends superClassBase implements
           SupportToolPageMixinInterface {
         $$<E extends Element = Element>(query: string) {
           return this.shadowRoot!.querySelector<E>(query);
@@ -31,7 +34,7 @@ export const SupportToolPageMixin = dedupingMixin(
       return SupportToolPageMixin;
     });
 
-export interface SupportToolPageMixinInterface {
+export interface SupportToolPageMixinInterface extends I18nMixinInterface {
   $$<E extends Element = Element>(query: string): E|null;
 
   ensureFocusOnPageHeader(): void;
