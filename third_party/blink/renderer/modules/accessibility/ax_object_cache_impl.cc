cde8641773b767376c95a82088a77f8378907186
Aaron Leventhal <aleventhal@google.com>
2023-07-28 16:42:24 +0000

    Small cleanup for tree update reasons related to role changes
    Bug: none
    Change-Id: I04490e739e32d6f7b9cda9b992fed1e4886d41c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727484
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176671}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -3193,9 +3193,7 @@ void AXObjectCacheImpl::FireTreeUpdatedEventImmediately(
     case TreeUpdateReason::kRoleChangeFromRoleOrType:
       HandleRoleChangeWithCleanLayout(node);
       break;
-    case TreeUpdateReason::kRoleMaybeChanged:
-      HandleRoleMaybeChangedWithCleanLayout(node);
-      break;
+    case TreeUpdateReason::kRoleMaybeChangedFromEventListener:
     case TreeUpdateReason::kRoleMaybeChangedFromHref:
       HandleRoleMaybeChangedWithCleanLayout(node);
       break;
@@ -3884,7 +3882,8 @@ void AXObjectCacheImpl::HandleEventSubscriptionChanged(
       // The role of an element depends on whether it has an event listener, so
       // check if the role changed, and if so re-create the object.
       if (obj->RoleValue() != obj->DetermineAccessibilityRole()) {
-        DeferTreeUpdate(TreeUpdateReason::kRoleMaybeChanged, &node);
+        DeferTreeUpdate(TreeUpdateReason::kRoleMaybeChangedFromEventListener,
+                        &node);
       }
     }
   }
