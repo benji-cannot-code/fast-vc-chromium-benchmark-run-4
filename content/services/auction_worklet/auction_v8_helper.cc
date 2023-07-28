5a1a434e89bc59711a7383b6c0dc2ac0a6cd0162
Ho Cheung <uioptt24@gmail.com>
2023-07-28 13:39:12 +0000

    [auction_worklet] Use base::Contains() instead of std::find()
    Use base::Contains() instead of std::find() where appropriate in
    auction_worklet.
    
    Bug: 561800
    Change-Id: I22e2424f999bd26fda7a30ec9b60f97eaf810c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4729226
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176589}

--- content/services/auction_worklet/auction_v8_helper.cc
+++ content/services/auction_worklet/auction_v8_helper.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
+#include "base/containers/contains.h"
 #include "base/functional/callback.h"
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
@@ -714,7 +715,7 @@ int AuctionV8Helper::AllocContextGroupId() {
     int candidate = ++last_context_group_id_;
     DCHECK_GT(candidate, 0);
 
-    if (resume_callbacks_.find(candidate) == resume_callbacks_.end()) {
+    if (!base::Contains(resume_callbacks_, candidate)) {
       resume_callbacks_.emplace(candidate, base::OnceClosure());
       return candidate;
     }
--- content/services/auction_worklet/debug_command_queue.cc
+++ content/services/auction_worklet/debug_command_queue.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/services/auction_worklet/debug_command_queue.h"
+
+#include "base/containers/contains.h"
 #include "base/task/sequenced_task_runner.h"
 
 namespace auction_worklet {
@@ -23,8 +25,7 @@ void DebugCommandQueue::PauseForDebuggerAndRunCommands(
   base::AutoLock auto_lock(lock_);
   CHECK(!v8_thread_paused_);
   DCHECK(!pause_abort_helper_);
-  if (aborted_context_group_ids_.find(context_group_id) !=
-      aborted_context_group_ids_.end()) {
+  if (base::Contains(aborted_context_group_ids_, context_group_id)) {
     // Pauses disallowed since worklet is in process of being destroyed
     return;
   }
