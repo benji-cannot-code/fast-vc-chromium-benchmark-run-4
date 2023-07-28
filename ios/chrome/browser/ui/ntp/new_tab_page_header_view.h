246323345ef399380f1a87133d4a058b8d89376f
adamta <adamta@google.com>
2023-07-28 17:48:23 +0000

    [iOS] Remove safe area inset dependencies from NTP
    Anchors NTP content within the safe area layout guide to prevent any
    dependencies on safe area insets when performing NTP operations. This
    allows us to move scroll position logic freely without breaking
    anything and greatly simplifies height calculations.
    
    Change-Id: I2e88065ee3486f4366fc5c82596c461eb4834b72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4730054
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1176714}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_view.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view.h
@@ -50,11 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Returns the progress of the search field position along
 // `ntp_header::kAnimationDistance` as the offset changes.
-- (CGFloat)searchFieldProgressForOffset:(CGFloat)offset
-                         safeAreaInsets:(UIEdgeInsets)safeAreaInsets;
-
-// The positive offset value to begin the fake omniobx expansion animation.
-- (CGFloat)offsetToBeginFakeOmniboxExpansionForSplitMode;
+- (CGFloat)searchFieldProgressForOffset:(CGFloat)offset;
 
 // Changes the constraints of searchField based on its initialFrame and the
 // scroll view's y `offset`. Also adjust the alpha values for `_searchBoxBorder`
--- ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.h
@@ -108,11 +108,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Returns the height of the entire header.
 - (CGFloat)headerHeight;
 
-// Update any dynamic constraints.
+// Update the constraints for the current header's content height.
 - (void)updateConstraints;
-
-// The positive offset value to begin the fake omniobx expansion animation.
-- (CGFloat)offsetToBeginFakeOmniboxExpansionForSplitMode;
 
 @end
 
