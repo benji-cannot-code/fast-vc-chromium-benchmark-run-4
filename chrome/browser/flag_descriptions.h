eda813eebd22e9f19885b122cb0d16b06f09843c
Kazuki Takise <takise@google.com>
2023-07-28 02:13:02 +0000

    Add Chrome flag for ARC XDG mode
    Make XDG mode controllable on chrome://flags for smoother launch
    process, debugging etc.
    
    Bug: b:293396324
    Test: ash_components_unittests
    Change-Id: I5e4348e68c47f85c556d5c3d663d86b82b2d5a48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721133
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176401}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3064,6 +3064,9 @@ extern const char kArcTrackpadScrollTouchscreenEmulationDesc[];
 extern const char kArcVmmSwapKBShortcutName[];
 extern const char kArcVmmSwapKBShortcutDesc[];
 
+extern const char kArcXdgModeName[];
+extern const char kArcXdgModeDesc[];
+
 extern const char kArcEnableAAudioMMAPName[];
 extern const char kArcEnableAAudioMMAPDescription[];
 
