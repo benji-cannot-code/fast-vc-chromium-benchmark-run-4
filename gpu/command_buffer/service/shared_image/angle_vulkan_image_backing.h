86b5ffe4506072d236b3f98d62b25bc5745e7a94
Colin Blundell <blundell@chromium.org>
2023-07-28 07:12:14 +0000

    [GPU] Move GLTexturePassthrough rep into AngleVulkanImageBacking
    AngleVulkanImageBacking is the only class that uses the
    GLTexturePassthrough image representation from
    gl_texture_common_representations.*. This CL moves it into
    angle_vulkan_image_backing.cc, streamlines it, and eliminates
    gl_texture_common_representations.*.
    
    Change-Id: Icfddf8523b989a5baeebb2c90d204c8e36ac4a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4724265
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176462}

--- gpu/command_buffer/service/shared_image/angle_vulkan_image_backing.h
+++ gpu/command_buffer/service/shared_image/angle_vulkan_image_backing.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/shared_context_state.h"
 #include "gpu/command_buffer/service/shared_image/gl_common_image_backing_factory.h"
-#include "gpu/command_buffer/service/shared_image/gl_texture_common_representations.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_backing.h"
 #include "gpu/command_buffer/service/shared_image/texture_holder_vk.h"
 #include "ui/gl/scoped_egl_image.h"
@@ -20,8 +19,7 @@ namespace gles2 {
 class TexturePassthrough;
 }
 
-class AngleVulkanImageBacking : public ClearTrackingSharedImageBacking,
-                                public GLTextureImageRepresentationClient {
+class AngleVulkanImageBacking : public ClearTrackingSharedImageBacking {
  public:
   AngleVulkanImageBacking(SharedContextState* context_state,
                           const Mailbox& mailbox,
@@ -49,12 +47,9 @@ class AngleVulkanImageBacking : public ClearTrackingSharedImageBacking,
       MemoryTypeTracker* tracker,
       scoped_refptr<SharedContextState> context_state) override;
 
-  // GLTextureImageRepresentationClient implementation.
-  bool GLTextureImageRepresentationBeginAccess(bool readonly) override;
-  void GLTextureImageRepresentationEndAccess(bool readonly) override;
-
  private:
   class SkiaAngleVulkanImageRepresentation;
+  class GLTexturePassthroughAngleVulkanImageRepresentation;
 
   struct TextureHolderGL {
     TextureHolderGL();
@@ -74,6 +69,8 @@ class AngleVulkanImageBacking : public ClearTrackingSharedImageBacking,
   void ReleaseTextureANGLE();
   void PrepareBackendTexture();
   void SyncImageLayoutFromBackendTexture();
+  bool BeginAccessGLTexturePassthrough(GLenum mode);
+  void EndAccessGLTexturePassthrough(GLenum mode);
   bool BeginAccessSkia(bool readonly);
   void EndAccessSkia();
   bool InitializePassthroughTexture();
