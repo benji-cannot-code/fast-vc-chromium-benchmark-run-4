4e98854958d92d3b4ceca8c51ad762331e9aec03
Norge Vizcay <vizcay@google.com>
2023-07-28 13:15:20 +0000

    [AutofillDataModel] Add legacy hierarchy default return.
    For countries that currently don't have a custom hierarchy model
    defined, we will continue to use our legacy structure.
    
    Note that there are still tests missing for custom hierarchies. These
    will be introduced once the process of building models from the edges
    is complete (i.e. BuildAutofillModel function).
    
    Bug: 1464568
    Change-Id: Iacc1e3bb54fd942f975c05735af1c20cf19d1357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708330
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176581}

--- components/autofill/core/browser/data_model/autofill_i18n_api.cc
+++ components/autofill/core/browser/data_model/autofill_i18n_api.cc
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/data_model/autofill_i18n_address_component.h"
 #include "components/autofill/core/browser/data_model/autofill_i18n_hierarchies.h"
+#include "components/autofill/core/browser/data_model/autofill_structured_address.h"
+#include "components/autofill/core/browser/data_model/autofill_structured_address_name.h"
+#include "components/autofill/core/browser/data_model/autofill_structured_address_utils.h"
 #include "components/autofill/core/browser/field_types.h"
 
 namespace autofill {
@@ -46,15 +49,28 @@ std::unique_ptr<I18nAddressComponent> BuildSubTree(
                                                 MergeMode::kDefault);
 }
 
+std::unique_ptr<AddressComponent> GetLegacyHierarchy(
+    AutofillModelType model_type) {
+  switch (model_type) {
+    case AutofillModelType::kAddressModel:
+      return std::make_unique<AddressNode>();
+    case AutofillModelType::kNameModel:
+      if (HonorificPrefixEnabled()) {
+        return std::make_unique<NameFullWithPrefix>();
+      }
+      return std::make_unique<NameFull>();
+  }
+  NOTREACHED_NORETURN();
+}
+
 }  // namespace
 
-std::unique_ptr<I18nAddressComponent> CreateAddressComponentModel(
+std::unique_ptr<AddressComponent> CreateAddressComponentModel(
     AutofillModelType model_type,
     std::string_view country_code) {
   auto* it = i18n_model_definition::kAutofillModelRules.find(country_code);
   if (it == i18n_model_definition::kAutofillModelRules.end()) {
-    // TODO(crbug.com/1464568): Return legacy hierarchies.
-    return nullptr;
+    return GetLegacyHierarchy(model_type);
   }
 
   // Convert the list of node properties into an adjacency lookup table.
--- components/autofill/core/browser/data_model/autofill_i18n_api_unittest.cc
+++ components/autofill/core/browser/data_model/autofill_i18n_api_unittest.cc
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
+#include "components/autofill/core/browser/data_model/autofill_structured_address.h"
+#include "components/autofill/core/browser/data_model/autofill_structured_address_name.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace autofill {
@@ -40,7 +44,7 @@ TEST(AutofillI18nApi, GetAddressComponentModel_ReturnsNonEmptyModel) {
          {AutofillModelType::kAddressModel, AutofillModelType::kNameModel}) {
       // Make sure that the process of building the model finishes and returns a
       // non empty hierarchy.
-      std::unique_ptr<I18nAddressComponent> model =
+      std::unique_ptr<AddressComponent> model =
           CreateAddressComponentModel(model_type, country_code);
 
       ASSERT_TRUE(model);
@@ -69,7 +73,7 @@ TEST(AutofillI18nApi, GetAddressComponentModel_ReturnedModelIsTree) {
        i18n_model_definition::kAutofillModelRules) {
     // Currently, the model for kAddressModel should comprise all the nodes in
     // the rules.
-    std::unique_ptr<I18nAddressComponent> root = CreateAddressComponentModel(
+    std::unique_ptr<AddressComponent> root = CreateAddressComponentModel(
         AutofillModelType::kAddressModel, country_code);
 
     ServerFieldTypeSet supported_types;
@@ -90,11 +94,39 @@ TEST(AutofillI18nApi, GetAddressComponentModel_ReturnedModelIsTree) {
 TEST(AutofillI18nApi, GetAddressComponentModel_CountryNodeHasValue) {
   for (const auto& [country_code, tree_def] :
        i18n_model_definition::kAutofillModelRules) {
-    std::unique_ptr<I18nAddressComponent> model = CreateAddressComponentModel(
+    std::unique_ptr<AddressComponent> model = CreateAddressComponentModel(
         AutofillModelType::kAddressModel, country_code);
     EXPECT_EQ(model->GetValueForType(ADDRESS_HOME_COUNTRY),
               base::UTF8ToUTF16(country_code));
   }
+}
+
+TEST(AutofillI18nApi, GetLegacy_FullName) {
+  // "Countries that have not been migrated to the new Autofill i18n model
+  // should use the legacy hierarchy."
+  ASSERT_FALSE(i18n_model_definition::kAutofillModelRules.contains("CA"));
+  EXPECT_TRUE(CreateAddressComponentModel(AutofillModelType::kNameModel, "CA")
+                  ->SameAs(NameFull()));
+}
+
+TEST(AutofillI18nApi, GetLegacy_FullNameWithPrefix) {
+  base::test::ScopedFeatureList structured_name_feature(
+      features::kAutofillEnableSupportForHonorificPrefixes);
+
+  // "Countries that have not been migrated to the new Autofill i18n model
+  // should use the legacy hierarchy."
+  ASSERT_FALSE(i18n_model_definition::kAutofillModelRules.contains("DE"));
+  EXPECT_TRUE(CreateAddressComponentModel(AutofillModelType::kNameModel, "DE")
+                  ->SameAs(NameFullWithPrefix()));
+}
+
+TEST(AutofillI18nApi, GetLegacy_AddressNode) {
+  // "Countries that have not been migrated to the new Autofill i18n model
+  // should use the legacy hierarchy."
+  ASSERT_FALSE(i18n_model_definition::kAutofillModelRules.contains("ES"));
+  EXPECT_TRUE(
+      CreateAddressComponentModel(AutofillModelType::kAddressModel, "ES")
+          ->SameAs(AddressNode()));
 }
 
 }  // namespace autofill
