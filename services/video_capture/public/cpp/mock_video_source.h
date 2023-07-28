17de681f5f0f0026c0bdedc367e4c439126bc4a6
Bryant Chandler <bryantchandler@chromium.org>
2023-07-28 20:08:32 +0000

    [media_effects] Add VideoEffectManager mojo
    This provides the basis for synchronizing effect configuration
    between BrowserUI, PrefService, and the video capture service.
    
    Bug: b:292149741
    Change-Id: I7bc531648dd9417b789fc94dc8ef7cb5bbac27ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694869
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176774}

--- services/video_capture/public/cpp/mock_video_source.h
+++ services/video_capture/public/cpp/mock_video_source.h
@@ -35,6 +35,10 @@ class MockVideoSource : public video_capture::mojom::VideoSource {
            mojo::PendingReceiver<
                video_capture::mojom::PushVideoStreamSubscription> subscription,
            CreatePushSubscriptionCallback& callback));
+
+  MOCK_METHOD1(
+      RegisterVideoEffectsManager,
+      void(mojo::PendingRemote<video_capture::mojom::VideoEffectsManager>));
 };
 
 }  // namespace video_capture
