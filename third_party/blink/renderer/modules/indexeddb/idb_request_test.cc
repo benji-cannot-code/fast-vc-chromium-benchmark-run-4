1b3276c5a336607bea90d2624c3d0acb4ea3be1e
Evan Stade <estade@chromium.org>
2023-07-28 19:40:02 +0000

    IndexedDB: Fix the new read/write UMA
    * Fix RequestDuration histogram declaration
    * Fix AsyncTraceState's move operation/ctor
    
    Bug: 1453648
    Change-Id: I43a62cbeaba44e4bb70bdba00f5e43720bdf5de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4724752
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176763}

--- third_party/blink/renderer/modules/indexeddb/idb_request_test.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_request_test.cc
@@ -471,6 +471,9 @@ class AsyncTraceStateForTesting : public IDBRequest::AsyncTraceState {
   absl::optional<IDBRequest::TypeForMetrics> type() const {
     return IDBRequest::AsyncTraceState::type();
   }
+  const base::TimeTicks& start_time() const {
+    return IDBRequest::AsyncTraceState::start_time();
+  }
   size_t id() const { return IDBRequest::AsyncTraceState::id(); }
 };
 
@@ -486,10 +489,12 @@ TEST(IDBRequestAsyncTraceStateTest, MoveConstructor) {
       IDBRequest::TypeForMetrics::kObjectStoreGetAllKeys;
   AsyncTraceStateForTesting source_state(type);
   size_t id = source_state.id();
+  base::TimeTicks start_time = source_state.start_time();
 
   AsyncTraceStateForTesting state(std::move(source_state));
   EXPECT_EQ(type, *state.type());
   EXPECT_EQ(id, state.id());
+  EXPECT_EQ(start_time, state.start_time());
   EXPECT_TRUE(source_state.IsEmpty());
 }
 
@@ -498,6 +503,7 @@ TEST(IDBRequestAsyncTraceStateTest, MoveAssignment) {
       IDBRequest::TypeForMetrics::kObjectStoreGetAllKeys;
   AsyncTraceStateForTesting source_state(type);
   size_t id = source_state.id();
+  base::TimeTicks start_time = source_state.start_time();
 
   AsyncTraceStateForTesting state;
   EXPECT_TRUE(state.IsEmpty());
@@ -505,6 +511,7 @@ TEST(IDBRequestAsyncTraceStateTest, MoveAssignment) {
   state = std::move(source_state);
   EXPECT_EQ(type, *state.type());
   EXPECT_EQ(id, state.id());
+  EXPECT_EQ(start_time, state.start_time());
   EXPECT_TRUE(source_state.IsEmpty());
 }
 
