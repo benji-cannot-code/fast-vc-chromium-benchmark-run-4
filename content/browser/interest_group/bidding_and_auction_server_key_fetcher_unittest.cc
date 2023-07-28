55dbc172ec906bf582a6b86db5011ee626090a6c
Dustin J. Mitchell <djmitche@chromium.org>
2023-07-28 18:19:35 +0000

    Add a missing run_loop.Run()
    This has "accidentally worked" until now because
    `url_loader_factory_.SimulateResponseForPendingRequest` calls
    `RunUntilIdle`. However, with the switch to a new Rust-based JSON
    parser, parsing happens in a thread pool and is not spotted by
    `RunUntilIdle`. Adding the missing `run_loop.Run`, as it appears in the
    other tests in this file, makes the test pass again.
    
    Bug: 1455145
    Change-Id: I2c615dcc79bd4d540f6d7201c255056941912ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727191
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1176723}

--- content/browser/interest_group/bidding_and_auction_server_key_fetcher_unittest.cc
+++ content/browser/interest_group/bidding_and_auction_server_key_fetcher_unittest.cc
@@ -198,6 +198,7 @@ TEST_F(BiddingAndAuctionServerKeyFetcherTest, RequestDuringSuccess) {
       "key": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\u003d",
       "id": "12345678-9abc-def0-1234-56789abcdef0"
       }]})"));
+  run_loop.Run();
   EXPECT_EQ(2, completed);
 }
 
