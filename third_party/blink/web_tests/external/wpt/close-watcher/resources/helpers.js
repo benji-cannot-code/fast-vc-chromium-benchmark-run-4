a51662bcbe9c6f17369b9bfd8fd800405f3852ce
Domenic Denicola <domenic@chromium.org>
2023-07-28 01:17:50 +0000

    CloseWatcher: add requestClose()
    Or rather, rename close() to requestClose(), and then add close().
    Follows https://github.com/whatwg/html/pull/9462/commits/63fb283b0bd6775f9aa8b063f3d397fedf6e0ef8,
    per https://github.com/WICG/close-watcher/issues/28.
    
    Bug: 1171318
    Change-Id: Ic1c6dff2440f38c04f7ffaaa19188403a46b9adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721666
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176384}

--- third_party/blink/web_tests/external/wpt/close-watcher/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/close-watcher/resources/helpers.js
@@ -2,10 +2,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(domenic): consider using these in all test files.
 
 window.createRecordingCloseWatcher = (t, events, name) => {
+  const prefix = name === undefined ? "" : name + " ";;
+
   const watcher = new CloseWatcher();
   t.add_cleanup(() => watcher.destroy());
-  watcher.oncancel = () => events.push(name + " cancel");
-  watcher.onclose = () => events.push(name + " close");
+  watcher.oncancel = () => events.push(prefix + "cancel");
+  watcher.onclose = () => events.push(prefix + "close");
 
   return watcher;
 };
