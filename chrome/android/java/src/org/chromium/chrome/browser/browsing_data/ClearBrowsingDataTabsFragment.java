e7c239d0eb17070e1b6e720d2dbcecf09b7f4cd1
Rohit Agarwal <roagarwal@chromium.org>
2023-07-28 09:31:02 +0000

    Quick Delete: Open Clear browsing data "Advanced" tab from dialog
    This CL adds the support in clear browsing data dialog to only show
    the Advanced section and allow quick delete dialog to integrate with
    the same.
    
    This CL also adds unit and render test for the same.
    
    Demo: https://drive.google.com/file/d/1AJDu6U-qdFG5nwWFeFNEzULD9kv8bpTB/view?usp=drive_link
    
    Bug: 1412087, b/290584439
    Change-Id: I2fc194fb0fdfb781561eb2dd414a842b9e0c4dbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722010
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176517}

--- chrome/android/java/src/org/chromium/chrome/browser/browsing_data/ClearBrowsingDataTabsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browsing_data/ClearBrowsingDataTabsFragment.java
@@ -134,6 +134,9 @@ public class ClearBrowsingDataTabsFragment
                 default:
                     throw new RuntimeException("invalid position: " + position);
             }
+            // We supply the fetcher in the next line.
+            fragment.setArguments(ClearBrowsingDataFragment.createFragmentArgs(
+                    /*isFetcherSuppliedFromOutside=*/true));
             fragment.setClearBrowsingDataFetcher(mFetcher);
             return fragment;
         }
--- chrome/browser/quick_delete/android/junit/src/org/chromium/chrome/browser/quick_delete/QuickDeleteDialogDelegateUnitTest.java
+++ chrome/browser/quick_delete/android/junit/src/org/chromium/chrome/browser/quick_delete/QuickDeleteDialogDelegateUnitTest.java
@@ -108,8 +108,8 @@ public class QuickDeleteDialogDelegateUnitTest {
     public void testClickingOnMoreOptions_LaunchesClearBrowsingData() {
         doNothing()
                 .when(mSettingsLauncherMock)
-                .launchSettingsActivity(
-                        eq(mActivity), eq(SettingsLauncher.SettingsFragment.CLEAR_BROWSING_DATA));
+                .launchSettingsActivity(eq(mActivity),
+                        eq(SettingsLauncher.SettingsFragment.CLEAR_BROWSING_DATA_ADVANCED_PAGE));
         new QuickDeleteDialogDelegate(mActivity, mQuickDeleteView, mModalDialogManager,
                 mOnDismissCallbackMock, mTabModelSelectorMock, mSettingsLauncherMock,
                 mTimePeriodChangeObserverMock)
@@ -120,8 +120,8 @@ public class QuickDeleteDialogDelegateUnitTest {
         moreOptionsView.performClick();
 
         verify(mSettingsLauncherMock)
-                .launchSettingsActivity(
-                        eq(mActivity), eq(SettingsLauncher.SettingsFragment.CLEAR_BROWSING_DATA));
+                .launchSettingsActivity(eq(mActivity),
+                        eq(SettingsLauncher.SettingsFragment.CLEAR_BROWSING_DATA_ADVANCED_PAGE));
     }
 
     @Test
