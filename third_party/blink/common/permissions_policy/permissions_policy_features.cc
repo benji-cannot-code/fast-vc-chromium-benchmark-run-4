59ca6a66893cd4b79411646785a3f1e5a412b77a
Fergal Daly <fergal@chromium.org>
2023-07-28 01:03:46 +0000

    Extract the non-templated code from permissions_policy_features.cc.tmpl.
    This code gets more complex in later unload-related CLs, so it's
    helpful for it to be editable directly.
    
    Bug: 1432116
    Change-Id: Idc91467404c9c77bc9af06fef688fc7c274281a0
    Cq-Include-Trybots: luci.chromium.try:win-presubmit
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675336
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176374}

---
+++ third_party/blink/common/permissions_policy/permissions_policy_features.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/public/common/permissions_policy/permissions_policy_features.h"
+
+#include "base/no_destructor.h"
+#include "third_party/blink/common/permissions_policy/permissions_policy_features_generated.h"
+
+// This file contains static code that is combined with templated code of
+// permissions_policy_features.cc.tmpl.
+
+namespace blink {
+
+const PermissionsPolicyFeatureList& GetPermissionsPolicyFeatureList() {
+  static const base::NoDestructor<PermissionsPolicyFeatureList> feature_list(
+      [] {
+        PermissionsPolicyFeatureList map =
+            GetBasePermissionsPolicyFeatureList();
+
+        UpdatePermissionsPolicyFeatureListFlagDefaults(map);
+        return map;
+      }());
+  return *feature_list;
+}
+
+void UpdatePermissionsPolicyFeatureListForTesting() {
+  const PermissionsPolicyFeatureList& feature_list =
+      GetPermissionsPolicyFeatureList();
+  PermissionsPolicyFeatureList& mutable_feature_list =
+      const_cast<PermissionsPolicyFeatureList&>(feature_list);
+  UpdatePermissionsPolicyFeatureListFlagDefaults(mutable_feature_list);
+}
+
+}  // namespace blink
