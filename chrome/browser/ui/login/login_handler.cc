2c1d4fc31f6796520ef2e68a9bda953e9bcde0f0
Kevin McNee <mcnee@chromium.org>
2023-07-28 19:50:10 +0000

    Don't de-duplicate login dialogs with cross-StoragePartition credentials
    We de-duplicate dialogs for HTTP auth when credentials have been
    provided for an identical challenge. We currently check that this
    sharing is not done across profiles, but this is insufficient. We
    now, more specifically, prevent sharing across StoragePartitions.
    
    We add tests for the webview tag which is a case of same-profile,
    different StoragePartition use.
    
    Bug: 1464783
    Change-Id: I8a1b4a438eed452a55e73660beca783b910675ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718485
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176765}

--- chrome/browser/ui/login/login_handler.cc
+++ chrome/browser/ui/login/login_handler.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "extensions/buildflags/buildflags.h"
@@ -233,11 +234,19 @@ void LoginHandler::Observe(int type,
   if (!auth_info().MatchesExceptPath(login_details->handler()->auth_info()))
     return;
 
-  // Ignore login notification events from other profiles.
-  NavigationController* controller =
-      content::Source<NavigationController>(source).ptr();
-  if (!controller ||
-      controller->GetBrowserContext() != web_contents_->GetBrowserContext()) {
+  // Ignore login notification events from other StoragePartitions.
+  // TODO(crbug.com/1261928): Getting the StoragePartition from the WebContents
+  // is fine for now, but we'll need to plumb frame information to LoginHandler
+  // as part of removing the multi-WebContents architecture.
+  content::StoragePartition* source_partition =
+      login_details->handler()->web_contents() ? login_details->handler()
+                                                     ->web_contents()
+                                                     ->GetPrimaryMainFrame()
+                                                     ->GetStoragePartition()
+                                               : nullptr;
+  content::StoragePartition* partition =
+      web_contents()->GetPrimaryMainFrame()->GetStoragePartition();
+  if (!source_partition || source_partition != partition) {
     return;
   }
 
