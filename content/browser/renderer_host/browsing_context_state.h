322cfccbfbcae03e6665d40538402bf6523cabbe
Sharon Yang <yangsharon@chromium.org>
2023-07-28 00:11:40 +0000

    Change SiteInstanceGroup observers to be checked
    SiteInstanceGroup has a list of observers, which were previously
    UncheckedObservers. There are no legacy observers so they can instead
    be CheckedObservers, which is the safer option.
    
    A unit test was added, though it passes both before and after this
    change.
    
    Test: SiteInstanceGroupTest.ObserverListReentrance, No Behaviour Change
    Bug: 1467593
    Change-Id: Idafe6e7d53f14dc6ae568ffd6c4772ab78ad6f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4717412
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176355}

--- content/browser/renderer_host/browsing_context_state.h
+++ content/browser/renderer_host/browsing_context_state.h
@@ -282,7 +282,7 @@ class CONTENT_EXPORT BrowsingContextState
  protected:
   friend class base::RefCounted<BrowsingContextState>;
 
-  virtual ~BrowsingContextState();
+  ~BrowsingContextState() override;
 
  private:
   RenderFrameProxyHost* GetRenderFrameProxyHostImpl(
