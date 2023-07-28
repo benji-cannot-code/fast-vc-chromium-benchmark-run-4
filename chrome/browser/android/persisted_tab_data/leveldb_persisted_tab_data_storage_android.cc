a6a65baa0a8457a03f97fe2818fd2a219583e0b2
David Maunder <davidjm@google.com>
2023-07-28 21:32:37 +0000

    Make LevelDBPersistedTabDataStorage profile keyed
    LevelDBPersistedTabDataStorageAndroid should be profile keyed, not
    use ProfileManager::GetLastUsedProfile. This CL makes it profile keyed
    and uses tab_android->GetProfile() for profile acquisition.
    PersistedTabDataConfigAndroid is adapted for the profile keyed storage.
    It holds references to the data_id and the LevelDBPersistedTabDataStorageAndroid. PersistedTabDataConfigAndroid
    returns a std::unique_ptr for ease of cleanup and avoid the risk of
    memory leaks.
    
    Bug: b/293387498
    Change-Id: Iea318f3d0f9cba09f0e16957afda5b8e5382d890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4723207
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176804}

--- chrome/browser/android/persisted_tab_data/leveldb_persisted_tab_data_storage_android.cc
+++ chrome/browser/android/persisted_tab_data/leveldb_persisted_tab_data_storage_android.cc
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+const char kLevelDBPersistedTabDataAndroid[] =
+    "level_db_persisted_tab_data_android";
+
 // Callback for inserting and deleting content.
 void OnUpdateCallback(bool success) {
   if (!success) {
@@ -34,24 +37,36 @@ void OnLoadCallback(
   std::move(restore_callback).Run(res);
 }
 
-const std::string GetKey(int tab_id, const std::string& data_id) {
-  return base::StringPrintf("%d-%s", tab_id, data_id.c_str());
+const std::string GetKey(int tab_id, const char* data_id) {
+  return base::StringPrintf("%d-%s", tab_id, data_id);
 }
 
 }  // namespace
 
-LevelDBPersistedTabDataStorageAndroid::LevelDBPersistedTabDataStorageAndroid()
-    : proto_db_(
-          SessionProtoDBFactory<
-              persisted_state_db::PersistedStateContentProto>::GetInstance()
-              ->GetForProfile(ProfileManager::GetLastUsedProfile())) {}
-
 LevelDBPersistedTabDataStorageAndroid::
     ~LevelDBPersistedTabDataStorageAndroid() = default;
 
+LevelDBPersistedTabDataStorageAndroid*
+LevelDBPersistedTabDataStorageAndroid::FromProfile(Profile* profile) {
+  if (!profile) {
+    return nullptr;
+  }
+
+  LevelDBPersistedTabDataStorageAndroid* level_db_persisted_tab_data_android =
+      static_cast<LevelDBPersistedTabDataStorageAndroid*>(
+          profile->GetUserData(kLevelDBPersistedTabDataAndroid));
+  if (!level_db_persisted_tab_data_android) {
+    level_db_persisted_tab_data_android =
+        new LevelDBPersistedTabDataStorageAndroid(profile);
+    profile->SetUserData(kLevelDBPersistedTabDataAndroid,
+                         base::WrapUnique(level_db_persisted_tab_data_android));
+  }
+  return level_db_persisted_tab_data_android;
+}
+
 void LevelDBPersistedTabDataStorageAndroid::Save(
     int tab_id,
-    const std::string& data_id,
+    const char* data_id,
     const std::vector<uint8_t>& data) {
   persisted_state_db::PersistedStateContentProto proto;
   std::string key = GetKey(tab_id, data_id);
@@ -62,7 +77,7 @@ void LevelDBPersistedTabDataStorageAndroid::Save(
 
 void LevelDBPersistedTabDataStorageAndroid::Restore(
     int tab_id,
-    const std::string& data_id,
+    const char* data_id,
     RestoreCallback restore_callback) {
   proto_db_->LoadContentWithPrefix(
       GetKey(tab_id, data_id),
@@ -70,7 +85,14 @@ void LevelDBPersistedTabDataStorageAndroid::Restore(
 }
 
 void LevelDBPersistedTabDataStorageAndroid::Remove(int tab_id,
-                                                   const std::string& data_id) {
+                                                   const char* data_id) {
   proto_db_->DeleteContentWithPrefix(GetKey(tab_id, data_id),
                                      base::BindOnce(&OnUpdateCallback));
 }
+
+LevelDBPersistedTabDataStorageAndroid::LevelDBPersistedTabDataStorageAndroid(
+    Profile* profile)
+    : proto_db_(
+          SessionProtoDBFactory<
+              persisted_state_db::PersistedStateContentProto>::GetInstance()
+              ->GetForProfile(profile)) {}
