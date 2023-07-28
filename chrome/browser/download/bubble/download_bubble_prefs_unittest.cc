5b57a4f68ecfde3960e4b9e378cbf8ed1eba6668
Daniel Rubery <drubery@chromium.org>
2023-07-28 20:24:12 +0000

    Use consumer deep scan UX when connector is report-only
    For enterprises with the download connector enabled in report-only mode,
    users can still start deep scans via the consumer prompt for scanning.
    In that case we should still give them the consumer UX, even though the
    connector is technically "enabled". This CL changes the string and
    buttons for this situation.
    
    Bug: 1424527
    Change-Id: I828a691d72952b5b3ad9b2c240f4c6cec6f6db45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728301
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176775}

--- chrome/browser/download/bubble/download_bubble_prefs_unittest.cc
+++ chrome/browser/download/bubble/download_bubble_prefs_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/enterprise/connectors/common.h"
+#include "chrome/browser/policy/dm_token_utils.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager_factory.h"
 #include "chrome/common/pref_names.h"
@@ -26,9 +27,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr char kDownloadConnectorEnabledPref[] = R"([
+constexpr char kDownloadConnectorEnabledNonBlockingPref[] = R"([
   {
     "service_provider": "google",
+    "enable": [
+      {"url_list": ["*"], "tags": ["malware"]}
+    ]
+  }
+])";
+
+constexpr char kDownloadConnectorEnabledBlockingPref[] = R"([
+  {
+    "service_provider": "google",
+    "block_until_verdict":1,
     "enable": [
       {"url_list": ["*"], "tags": ["malware"]}
     ]
@@ -50,6 +61,12 @@ class DownloadBubblePrefsTest : public testing::Test {
     ASSERT_TRUE(testing_profile_manager_.SetUp());
 
     profile_ = testing_profile_manager_.CreateTestingProfile("testing_profile");
+    policy::SetDMTokenForTesting(
+        policy::DMToken::CreateValidToken("fake-token"));
+    profile_->GetPrefs()->SetInteger(
+        ConnectorScopePref(
+            enterprise_connectors::AnalysisConnector::FILE_DOWNLOADED),
+        policy::POLICY_SCOPE_MACHINE);
   }
 
  protected:
@@ -99,13 +116,18 @@ TEST_F(DownloadBubblePrefsTest, DownloadBubbleEnabledManaged) {
   ExpectFeatureFlagEnabledStatus(/*expect_enabled=*/false);
 }
 
-TEST_F(DownloadBubblePrefsTest, IsDownloadConnectorEnabled) {
-  EXPECT_FALSE(IsDownloadConnectorEnabled(profile_));
+TEST_F(DownloadBubblePrefsTest, DoesDownloadConnectorBlock) {
+  EXPECT_FALSE(DoesDownloadConnectorBlock(profile_, GURL()));
   profile_->GetPrefs()->Set(
       enterprise_connectors::ConnectorPref(
           enterprise_connectors::FILE_DOWNLOADED),
-      *base::JSONReader::Read(kDownloadConnectorEnabledPref));
-  EXPECT_TRUE(IsDownloadConnectorEnabled(profile_));
+      *base::JSONReader::Read(kDownloadConnectorEnabledNonBlockingPref));
+  EXPECT_FALSE(DoesDownloadConnectorBlock(profile_, GURL()));
+  profile_->GetPrefs()->Set(
+      enterprise_connectors::ConnectorPref(
+          enterprise_connectors::FILE_DOWNLOADED),
+      *base::JSONReader::Read(kDownloadConnectorEnabledBlockingPref));
+  EXPECT_TRUE(DoesDownloadConnectorBlock(profile_, GURL()));
 }
 
 TEST_F(DownloadBubblePrefsTest, V2FeatureFlagEnabled) {
--- chrome/browser/download/download_ui_model.cc
+++ chrome/browser/download/download_ui_model.cc
@@ -1352,7 +1352,7 @@ DownloadUIModel::GetBubbleUIInfoForInProgressOrComplete(
                     IDS_DOWNLOAD_BUBBLE_SUBPAGE_SUMMARY_ASYNC_SCANNING))
                 .AddSubpageSecondaryIconAndText(
                     vector_icons::kDocumentScannerIcon,
-                    download::IsDownloadConnectorEnabled(profile())
+                    download::DoesDownloadConnectorBlock(profile(), GetURL())
                         ? l10n_util::GetStringUTF16(
                               IDS_DOWNLOAD_BUBBLE_SUBPAGE_SUMMARY_ASYNC_SCANNING_ENTERPRISE_SECONDARY)
                         : l10n_util::GetStringUTF16(
@@ -1366,7 +1366,7 @@ DownloadUIModel::GetBubbleUIInfoForInProgressOrComplete(
                         IDS_DOWNLOAD_BUBBLE_SUBPAGE_SUMMARY_ASYNC_SCANNING_DISCARD),
                     DownloadCommands::Command::DISCARD);
         ui_info.subpage_buttons[0].is_prominent = false;
-        if (!download::IsDownloadConnectorEnabled(profile())) {
+        if (!download::DoesDownloadConnectorBlock(profile(), GetURL())) {
           ui_info.AddSecondarySubpageButton(
               l10n_util::GetStringUTF16(
                   IDS_DOWNLOAD_BUBBLE_SUBPAGE_SUMMARY_ASYNC_SCANNING_CANCEL),
@@ -1377,7 +1377,7 @@ DownloadUIModel::GetBubbleUIInfoForInProgressOrComplete(
         ui_info = DownloadUIModel::BubbleUIInfo()
                       .AddProgressBar()
                       .SetProgressBarLooping();
-        if (!download::IsDownloadConnectorEnabled(profile())) {
+        if (!download::DoesDownloadConnectorBlock(profile(), GetURL())) {
           ui_info.AddPrimaryButton(
               DownloadCommands::Command::BYPASS_DEEP_SCANNING);
         }
@@ -1725,7 +1725,8 @@ DownloadUIModel::BubbleStatusTextBuilder::GetBubbleWarningStatusText() const {
 #else
       // Either "Checking with your organization's security policies..." or
       // "Scanning..."
-      return download::IsDownloadConnectorEnabled(model_->profile())
+      return download::DoesDownloadConnectorBlock(
+                 model_->profile(), model_->GetDownloadItem()->GetURL())
                  ? l10n_util::GetStringUTF16(
                        IDS_DOWNLOAD_BUBBLE_STATUS_ASYNC_SCANNING_ENTERPRISE)
                  : l10n_util::GetStringUTF16(
