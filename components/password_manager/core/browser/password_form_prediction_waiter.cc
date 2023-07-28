bcbe9cca83e9241b43052d5ca69ec813cea76074
Jan Keitel <jkeitel@google.com>
2023-07-28 12:05:01 +0000

    Add IsActive() method to PasswordFormPredictionWaiter.
    This allows eliminating a separate member variable in
    PasswordFormManager that essentially monitored the same state.
    
    Bug: 1465803
    Change-Id: Ic3bf71fb11faa9a50dca2b7e39ad078bb04092f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4720534
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1176555}

--- components/password_manager/core/browser/password_form_prediction_waiter.cc
+++ components/password_manager/core/browser/password_form_prediction_waiter.cc
@@ -29,6 +29,10 @@ void PasswordFormPredictionWaiter::Reset() {
   timer_.Stop();
 }
 
+bool PasswordFormPredictionWaiter::IsActive() const {
+  return timer_.IsRunning();
+}
+
 base::OnceClosure PasswordFormPredictionWaiter::CreateClosure() {
   outstanding_closures_++;
   return base::BindOnce(&PasswordFormPredictionWaiter::OnClosureComplete,
