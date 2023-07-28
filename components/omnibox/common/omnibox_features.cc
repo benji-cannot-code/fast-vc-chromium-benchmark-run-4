82fdceb47f1694e9c8d19aa852a999c72fc82a16
Stepan Khapugin <stkhapugin@chromium.org>
2023-07-28 08:51:30 +0000

    [iOS][Omnibox] Disable MV Tiles on SRP by default.
    With On Focus - no MV Tiles on SRP releasing to 100%, prepare to
    disable this flag by default and remove the corresponidng field trial.
    
    Bug: none
    Launch: 4200732
    Change-Id: If96bf235dd7166c1f53df63efdca5ad583557a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714030
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176493}

--- components/omnibox/common/omnibox_features.cc
+++ components/omnibox/common/omnibox_features.cc
@@ -24,13 +24,6 @@ constexpr auto enabled_by_default_android_only =
     base::FEATURE_DISABLED_BY_DEFAULT;
 #endif
 
-constexpr auto enabled_by_default_ios_only =
-#if BUILDFLAG(IS_IOS)
-    base::FEATURE_ENABLED_BY_DEFAULT;
-#else
-    base::FEATURE_DISABLED_BY_DEFAULT;
-#endif
-
 constexpr auto enabled_by_default_desktop_android =
 #if BUILDFLAG(IS_IOS)
     base::FEATURE_DISABLED_BY_DEFAULT;
@@ -367,7 +360,7 @@ BASE_FEATURE(kOmniboxMostVisitedTilesAddRecycledViewPool,
 // a search result page that does not do search term replacement.
 BASE_FEATURE(kOmniboxMostVisitedTilesOnSrp,
              "OmniboxMostVisitedTilesOnSrp",
-             enabled_by_default_ios_only);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // If enabled, adds a grey square background to search icons, and makes answer
 // icon square instead of round.
