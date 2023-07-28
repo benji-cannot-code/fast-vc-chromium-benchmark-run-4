3d41e29208f70899b1c532eaf1ff980198b633b5
Mohamed Amir Yosef <mamir@chromium.org>
2023-07-28 07:07:13 +0000

    [Passwords] Add Kombucha test for shared password notification bubble
    This adds a test that the shared password notification bubble is
    displayed when a shared password is present in dropdown menu.
    
    It also add a pixel test for that bubble.
    
    Bug: 1464209
    Change-Id: I4c07b259cb80cce0e3a967e85e101ce4333980bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714696
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176460}

--- chrome/browser/ui/views/passwords/shared_passwords_notification_view.cc
+++ chrome/browser/ui/views/passwords/shared_passwords_notification_view.cc
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/passwords_model_delegate.h"
 #include "chrome/browser/ui/passwords/ui_utils.h"
 #include "chrome/grit/generated_resources.h"
+#include "ui/base/interaction/element_identifier.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/gfx/text_constants.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/view_class_properties.h"
 
 SharedPasswordsNotificationView::SharedPasswordsNotificationView(
     content::WebContents* web_contents,
@@ -62,6 +64,8 @@ SharedPasswordsNotificationView::SharedPasswordsNotificationView(
         gfx::Font::Weight::BOLD);
     styled_label->AddStyleRange(sender_name_range, bold_style);
   }
+
+  SetProperty(views::kElementIdentifierKey, kTopView);
 }
 
 SharedPasswordsNotificationView::~SharedPasswordsNotificationView() = default;
@@ -80,3 +84,6 @@ ui::ImageModel SharedPasswordsNotificationView::GetWindowIcon() {
   return ui::ImageModel::FromVectorIcon(GooglePasswordManagerVectorIcon(),
                                         ui::kColorIcon);
 }
+
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(SharedPasswordsNotificationView,
+                                      kTopView);
