1552833fd2c88fcb07e3e86d0e6c78fdeb6770a3
Yongshun Liu <yongshun@chromium.org>
2023-07-29 00:01:58 +0000

    bento_button: Fix crash for desk switch button tooltip
    The crash occurs when the desk switch button tooltip appears during the
    desk switch animation. The root cause is that we update the switch
    buttons after the desk switch, which means the switch button may remain
    visible for longer than expected.
    
    To ensure that the switch buttons are valid throughout their lifetime,
    the solution is to update them before the desk switch.
    
    Bug: b/292257858
    Test: Manual
    Change-Id: I5a82e535a228f3f5a1390aeefb5d410dc7c660a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718311
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176874}

--- ash/wm/desks/desk_mini_view.cc
+++ ash/wm/desks/desk_mini_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desk_name_view.h"
 #include "ash/wm/desks/desk_preview_view.h"
 #include "ash/wm/desks/desk_textfield.h"
+#include "ash/wm/desks/desks_constants.h"
 #include "ash/wm/desks/desks_restore_util.h"
 #include "ash/wm/float/float_controller.h"
 #include "ash/wm/overview/overview_constants.h"
@@ -293,7 +294,8 @@ void DeskMiniView::UpdateDeskButtonVisibility() {
   if (!desk_->is_desk_being_removed() &&
       owner_bar_->type() == DeskBarViewBase::Type::kDeskButton) {
     const int desk_index = controller->GetDeskIndex(desk_);
-    desk_shortcut_view_->SetVisible(visible && desk_index <= 7);
+    desk_shortcut_view_->SetVisible(visible &&
+                                    desk_index < kDeskBarMaxDeskShortcut);
     desk_shortcut_label_->SetText(u"+ " +
                                   base::NumberToString16(desk_index + 1));
   }
