e17fdd445da88954a9c3350d50e6efc6d659f441
James Forshaw <forshaw@chromium.org>
2023-07-29 03:19:19 +0000

    [Windows] Simplify Chrome ELF's use of sandbox library.
    This CL simplifies Chrome ELF's use of the sandbox library. It adds a
    dependency on a new service_resolver target to not require linking
    to the full sandbox library. It also replaces some usage of NTDLL
    functions with their Win32 equivalent to remove the dependency on the
    sandbox library headers. These Win32 functions are just wrappers around
    the system calls and the change should be neutral.
    
    Bug: 1465514
    Change-Id: I4e3c710aeaf2f8ec17180c6ff63b247c9ccf1b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4716140
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176910}

--- chrome/chrome_elf/hook_util/hook_util.cc
+++ chrome/chrome_elf/hook_util/hook_util.cc
@@ -5,12 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "hook_util.h"
 
+#include <assert.h>
 #include <versionhelpers.h>  // windows.h must be before
 
 #include "base/win/pe_image.h"
-#include "chrome/chrome_elf/nt_registry/nt_registry.h"  // utils
-#include "sandbox/win/src/interception_internal.h"
-#include "sandbox/win/src/internal_types.h"
 
 namespace {
 
