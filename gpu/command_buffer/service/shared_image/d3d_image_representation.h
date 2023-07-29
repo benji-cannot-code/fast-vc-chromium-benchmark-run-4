00a3a33bf2a683a22357a7f751857d4cba974420
Sunny Sachanandani <sunnyps@chromium.org>
2023-07-29 01:46:11 +0000

    graphite: Multi-device synchronization implementation in D3DImageBacking
    Implement synchronization across multiple devices Dawn/ANGLE/D3D11/D3D12
    in D3DImageBacking. This CL makes the following changes:
    
    1) ExternalImageDXGI is cached in DXGISharedHandleState to ensure that
       we don't acquire the keyed mutex recursively if multiple shared
       images alias the same shared handle. Also, add some validation logic
       to BeginAccessDawn to disallow concurrent keyed mutex access.
    
    2) Extend the fence synchronization code in D3DImageBacking to support
       multiple devices. Also, factor out common functionality for getting
       the list of pending wait fences and state tracking into helpers that
       are used for both BeginAccessD3D11 and BeginAccessDawn.
    
    3) Support the case where we don't have a shared handle e.g. mappable
       textures created for shared memory compound backings used by software
       decoded video frames and canvas capture. For BeginAccessDawn, when
       Dawn is using using the same D3D11 device as the backing, avoid using
       a shared handle and instead pass the texture directly.
    
    Bug: 1464274, 1464548
    Change-Id: I673d81acb09ce4bc2fca4ca5c7725b8c7c6750f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721022
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176896}

--- gpu/command_buffer/service/shared_image/d3d_image_representation.h
+++ gpu/command_buffer/service/shared_image/d3d_image_representation.h
@@ -55,7 +55,8 @@ class DawnD3DImageRepresentation : public DawnImageRepresentation {
   DawnD3DImageRepresentation(SharedImageManager* manager,
                              SharedImageBacking* backing,
                              MemoryTypeTracker* tracker,
-                             const wgpu::Device& device);
+                             const wgpu::Device& device,
+                             wgpu::BackendType backend_type);
   ~DawnD3DImageRepresentation() override;
 
   wgpu::Texture BeginAccess(wgpu::TextureUsage usage) override;
@@ -63,6 +64,7 @@ class DawnD3DImageRepresentation : public DawnImageRepresentation {
 
  private:
   const wgpu::Device device_;
+  const wgpu::BackendType backend_type_;
   wgpu::Texture texture_;
 };
 #endif  // BUILDFLAG(USE_DAWN)
