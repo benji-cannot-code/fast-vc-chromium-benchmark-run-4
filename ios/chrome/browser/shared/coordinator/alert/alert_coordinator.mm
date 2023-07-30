7342de22ca091de95d68e5338f3551f4155953e3
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-30 00:41:06 +0000

    [iOS] check that no item is added to an alert when its visible.
    It does not seems to occur today. Still, if someone tries one day, it
    ensures they won’t lose time debugging why it failed.
    
    One Item was removed in `markAllReadingListItemsAfterConfirmation`
    because `initializeMarkConfirmationSheet` was already adding a Cancel.
    While they use `IDS_CANCEL` and `IDS_APP_CANCEL`, as both are described
    as "Used as ’cancel’ for button", there would be no change in
    behaviour.
    
    Bug: None
    Change-Id: Ie03fa30c57cbd8fcf5e32ea9467743a5aee74557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701263
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176994}

--- ios/chrome/browser/shared/coordinator/alert/alert_coordinator.mm
+++ ios/chrome/browser/shared/coordinator/alert/alert_coordinator.mm
@@ -68,9 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    style:(UIAlertActionStyle)style
                preferred:(BOOL)preferred
                  enabled:(BOOL)enabled {
-  if (self.visible) {
-    return;
-  }
+  CHECK(!self.visible);
 
   if (style == UIAlertActionStyleCancel) {
     CHECK(!self.cancelButtonAdded);
