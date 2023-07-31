c92e0bc568fc48edfc5976910ed06a7ba2cb7d42
Hyomin Kim <hyoputer.kim@samsung.com>
2023-07-31 00:26:33 +0000

    Remove unnecessary macros on Lacros tests
    Referring to [0] and [1], the related tests are only used on Lacros.
    So remove the "MAYBE" macro and set build flag for them.
    
    [0]: Ic26427b030e8c05f60567f0fc9d7edc392216c99
    
    [1]: I367026c4b61eba160698a662319d35c72f5f38b9
    
    Bug: 1353668
    Change-Id: If585a1679e0e9886e50fed7422e7c40005bcd423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719806
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hyomin Kim <hyoputer.kim@samsung.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177066}

--- ui/ozone/platform/wayland/host/wayland_input_method_context_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_input_method_context_unittest.cc
@@ -1246,16 +1246,8 @@ TEST_P(WaylandInputMethodContextTest, OnCommit) {
             gfx::Range(0));
 }
 
-// TODO(1353668): WaylandInputMethodContext::OnCursorPosition sets
-// |pending_keep_selection| only on lacros. That's the reason why this test
-// doesn't pass on Linux. We need to clarify that.
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-#define MAYBE(x) x
-#else
-#define MAYBE(x) DISABLED_##x
-#endif
-
-TEST_P(WaylandInputMethodContextTest, MAYBE(OnConfirmCompositionText)) {
+TEST_P(WaylandInputMethodContextTest, OnConfirmCompositionText) {
   constexpr char16_t text[] = u"ab😀cあdef";
   constexpr gfx::Range range(5, 6);  // あ is selected.
 
@@ -1299,7 +1291,7 @@ TEST_P(WaylandInputMethodContextTest, MAYBE(OnConfirmCompositionText)) {
 }
 
 TEST_P(WaylandInputMethodContextTest,
-       MAYBE(OnConfirmCompositionTextExtendedKeepSelectionNoComposition)) {
+       OnConfirmCompositionTextExtendedKeepSelectionNoComposition) {
   input_method_context_->SetSurroundingText(u"abcd", gfx::Range(0, 4),
                                             gfx::Range(0, 4), absl::nullopt,
                                             absl::nullopt);
@@ -1320,7 +1312,7 @@ TEST_P(WaylandInputMethodContextTest,
 }
 
 TEST_P(WaylandInputMethodContextTest,
-       MAYBE(OnConfirmCompositionTextExtendedKeepSelectionComposition)) {
+       OnConfirmCompositionTextExtendedKeepSelectionComposition) {
   input_method_context_->SetSurroundingText(
       u"abcd", gfx::Range(0, 4), gfx::Range(2), absl::nullopt, absl::nullopt);
   input_method_context_->OnPreeditString("xyz", {}, 1);
@@ -1341,7 +1333,7 @@ TEST_P(WaylandInputMethodContextTest,
 }
 
 TEST_P(WaylandInputMethodContextTest,
-       MAYBE(OnConfirmCompositionTextExtendedDontKeepSelectionNoComposition)) {
+       OnConfirmCompositionTextExtendedDontKeepSelectionNoComposition) {
   input_method_context_->SetSurroundingText(u"abcd", gfx::Range(0, 4),
                                             gfx::Range(0, 4), absl::nullopt,
                                             absl::nullopt);
@@ -1364,7 +1356,7 @@ TEST_P(WaylandInputMethodContextTest,
 }
 
 TEST_P(WaylandInputMethodContextTest,
-       MAYBE(OnConfirmCompositionTextExtendedDontKeepSelectionComposition)) {
+       OnConfirmCompositionTextExtendedDontKeepSelectionComposition) {
   input_method_context_->SetSurroundingText(
       u"abcd", gfx::Range(0, 4), gfx::Range(2), absl::nullopt, absl::nullopt);
   input_method_context_->OnPreeditString("xyz", {}, 1);
@@ -1386,8 +1378,7 @@ TEST_P(WaylandInputMethodContextTest,
             gfx::Range(0));
 }
 
-TEST_P(WaylandInputMethodContextTest,
-       MAYBE(OnConfirmCompositionTextForLongRange)) {
+TEST_P(WaylandInputMethodContextTest, OnConfirmCompositionTextForLongRange) {
   const std::u16string text(5000, u'あ');
   constexpr gfx::Range range(4000, 4500);
 
@@ -1450,6 +1441,7 @@ TEST_P(WaylandInputMethodContextTest,
   EXPECT_EQ(input_method_context_->predicted_state_for_testing().composition,
             gfx::Range(0));
 }
+#endif
 
 TEST_P(WaylandInputMethodContextTest, OnSetPreeditRegion_Success) {
   constexpr char16_t text[] = u"abcあdef";
