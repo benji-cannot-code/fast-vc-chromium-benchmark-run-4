4b428f8eb2f8ea6d13999c2f851cd3081869c6a1
Jack Yammine <jyammine@google.com>
2023-07-31 09:09:22 +0000

    [waffle]Handle having multiple tabs open on browser restore
    We should only show the dialog once in the case where the browser
    restores while having multiple tabs open.
    
    Screen recording: https://drive.google.com/file/d/1S1-s29a_vDRsCFZ24WkD7XPDSjp30Ec5/view?usp=sharing
    
    Bug: b/280753754
    Change-Id: I21211c6eae278cccbf54929c30a210910eff90c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714630
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Jens Mueller <muellerj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1177135}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -141,6 +141,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/tailored_security/tailored_security_service_factory.h"
 #include "chrome/browser/safe_browsing/url_lookup_service_factory.h"
 #include "chrome/browser/safe_browsing/verdict_cache_manager_factory.h"
+#include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
 #include "chrome/browser/search_engines/template_url_fetcher_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/search_provider_logos/logo_service_factory.h"
@@ -211,6 +212,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/buildflags.h"
 #include "components/services/screen_ai/buildflags/buildflags.h"
 #include "components/signin/public/base/signin_buildflags.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/site_engagement/content/site_engagement_service.h"
 #include "components/spellcheck/spellcheck_buildflags.h"
 #include "components/supervised_user/core/common/buildflags.h"
@@ -985,6 +987,13 @@ void ChromeBrowserMainExtraPartsProfiles::
   SCTReportingServiceFactory::GetInstance();
 #if BUILDFLAG(IS_ANDROID)
   search_resumption_module::StartSuggestServiceFactory::GetInstance();
+#endif
+#if BUILDFLAG(ENABLE_SEARCH_ENGINE_CHOICE)
+  if (base::FeatureList::IsEnabled(switches::kSearchEngineChoice)) {
+    SearchEngineChoiceServiceFactory::GetInstance();
+  }
+#endif
+#if BUILDFLAG(IS_ANDROID)
   SearchPermissionsService::Factory::GetInstance();
 #endif
   SearchPrefetchServiceFactory::GetInstance();
