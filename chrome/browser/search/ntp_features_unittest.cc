36c147895c7802133eb7e619efb6747bf3021393
Tibor Goldschwendt <tiborg@chromium.org>
2023-07-31 19:53:34 +0000

    Reland "[customize-chrome][cr2023] Establish dependencies of features in code"
    This is a reland of commit f0db7b86166ef32ecdfbcb2a72e4a0abe6189db8. The
    original CL got reverted due to issues with component layering. This CL
    defines the dependency in the //ui/base component to work around that.
    
    Original change's description:
    > [customize-chrome][cr2023] Establish dependencies of features in code
    >
    > Chrome Refresh requires the Customize Chrome side panel. This CL ensures
    > that Chrome Refresh is only enabled if Customize Chrome is enabled, too.
    >
    > Additionally, this CL introduces a new feature flag that disables Chrome
    > Refresh even if Customize Chrome is enabled. This allows us to set up a
    > holdback experiment for the Customize Chrome side panel where both
    > experiment arms have Chrome Refresh disabled.
    >
    > Things to note:
    > * Only some Omnibox features need to be gated by Customize Chrome. This
    >   CL adds a helper to gate those specific call sites. The helper lives
    >   in //components/omnibox/common to work around cyclic dependency
    >   issues. This CL modifies DEPS rules accordingly.
    > * This CL moves ntp_features into a new GN target so that users can
    >   depend on it in isolation. This, too, works around cyclic dependency
    >   issues.
    >
    > Bug: 1457991
    > Change-Id: I13ccb0df1cfa433aa5e10bc2bca5a7933913da6d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4690991
    > Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    > Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    > Reviewed-by: manuk hovanesian <manukh@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1175187}
    
    Bug: 1457991, 1467734
    Change-Id: Ib6a44b3016768dc82d6ae9e4b80c659803829106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722469
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1177431}

--- chrome/browser/search/ntp_features_unittest.cc
+++ chrome/browser/search/ntp_features_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/ui_base_features.h"
 
 namespace ntp_features {
 
@@ -50,6 +51,47 @@ TEST(NTPFeaturesTest, ModulesOrder) {
   scoped_feature_list_.InitWithFeaturesAndParameters(
       {{kNtpModulesOrder, {{kNtpModulesOrderParam, ""}}}}, {});
   EXPECT_TRUE(GetModulesOrder().empty());
+}
+
+TEST(NTPFeaturesTest, CustomizeChromeSupportsChromeRefresh2023) {
+  {
+    // Chrome Refresh 2023 should be off when Customize Chrome is on but
+    // Customize Chrome No Refresh is on, too.
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitWithFeatures(
+        {features::kCustomizeChromeSidePanel,
+         features::kCustomizeChromeSidePanelNoChromeRefresh2023},
+        {});
+    EXPECT_FALSE(features::CustomizeChromeSupportsChromeRefresh2023());
+  }
+
+  {
+    // Chrome Refresh 2023 should be on when Customize Chrome is on and
+    // Customize Chrome No Refresh is off.
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitWithFeatures(
+        {features::kCustomizeChromeSidePanel},
+        {features::kCustomizeChromeSidePanelNoChromeRefresh2023});
+    EXPECT_TRUE(features::CustomizeChromeSupportsChromeRefresh2023());
+  }
+
+  {
+    // Chrome Refresh 2023 should be off when Customize Chrome is off.
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitWithFeatures(
+        {}, {features::kCustomizeChromeSidePanel,
+             features::kCustomizeChromeSidePanelNoChromeRefresh2023});
+    EXPECT_FALSE(features::CustomizeChromeSupportsChromeRefresh2023());
+  }
+
+  {
+    // Chrome Refresh 2023 should be off when Customize Chrome is off.
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitWithFeatures(
+        {features::kCustomizeChromeSidePanelNoChromeRefresh2023},
+        {features::kCustomizeChromeSidePanel});
+    EXPECT_FALSE(features::CustomizeChromeSupportsChromeRefresh2023());
+  }
 }
 
 }  // namespace ntp_features
