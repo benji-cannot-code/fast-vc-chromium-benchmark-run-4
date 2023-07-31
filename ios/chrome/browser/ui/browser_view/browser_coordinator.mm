c8acd0692b705d6caf3bb15ccbb730e4f7bcb4a2
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-31 10:56:35 +0000

    [iOS] Disconnect the voice search controller
    This should be disconnected before being deallocated.
    
    The browser coordinator did not dismiss it.
    
    Bug: None
    Change-Id: I5f387d9582237ef8eb547ea5aa0be1762dd0d8af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4729484
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177168}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -1172,6 +1172,11 @@ enum class ToolbarKind {
         _promosManagerCoordinator;
     [_credentialProviderPromoCoordinator start];
   }
+
+  [_voiceSearchController dismissMicPermissionHelp];
+  [_voiceSearchController disconnect];
+  _voiceSearchController.dispatcher = nil;
+  _voiceSearchController = nil;
 }
 
 // Stops child coordinators.
