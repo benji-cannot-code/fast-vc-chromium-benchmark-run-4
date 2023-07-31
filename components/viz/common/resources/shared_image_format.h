d2a48d0ed639154424c4cc146807c9865100b883
Colin Blundell <blundell@chromium.org>
2023-07-31 16:38:29 +0000

    [viz] Move SIF::prefers_external_sampler_ into MultiplanarFormat
    This CL paves the way for passing SIF::prefers_external_sampler_ over
    Mojo by moving it into the MultiplanarFormat struct, which is mirrored
    exactly in Mojo. It's not straightforward to add a
    `prefers_external_sampler_` ivar to mojom::SharedImageFormat as the
    latter is a union over SinglePlanar/MultiplanarFormat rather than
    a struct wrapping that union. It would not be easy to change it to a
    struct wrapping the latter either as the UnionTraits code for
    mojom::SharedImageFormat relies on checking
    viz::SharedImageFormat::is_multi_plane() to calculate the tag of the
    Mojo union.
    
    Bug: 1457134
    Change-Id: I820a3e09436d9ad5176b52291f9748eb9de40db5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4733534
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177299}

--- components/viz/common/resources/shared_image_format.h
+++ components/viz/common/resources/shared_image_format.h
@@ -97,7 +97,9 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat {
   // external sampling is supported only on Ozone.
   bool PrefersExternalSampler() const {
 #if BUILDFLAG(IS_OZONE)
-    return prefers_external_sampler_;
+    return is_multi_plane()
+               ? format_.multiplanar_format.prefers_external_sampler
+               : false;
 #else
     return false;
 #endif
@@ -107,7 +109,7 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat {
   // Sets this format (which must be multiplanar) as needing external sampling.
   void SetPrefersExternalSampler() {
     CHECK(is_multi_plane());
-    prefers_external_sampler_ = true;
+    format_.multiplanar_format.prefers_external_sampler = true;
   }
 #endif
 
@@ -180,13 +182,21 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat {
       PlaneConfig plane_config;
       Subsampling subsampling;
       ChannelFormat channel_format;
+#if BUILDFLAG(IS_OZONE)
+      // NOTE: This field is intentionally not used as part of defining equality
+      // between two MultiplanarFormat instances as clients should not generally
+      // need to care. Clients who need to distinguish for a particular
+      // SharedImageFormat `format` should call
+      // format.PrefersExternalSampler().
+      bool prefers_external_sampler = false;
+#endif
 
       bool operator==(const MultiplanarFormat& o) const;
       bool operator!=(const MultiplanarFormat& o) const;
       bool operator<(const MultiplanarFormat& o) const;
     };
 
-    SharedImageFormatUnion() = default;
+    SharedImageFormatUnion() {}
     explicit constexpr SharedImageFormatUnion(
         mojom::SingleplanarFormat singleplanar_format)
         : singleplanar_format(singleplanar_format) {}
@@ -227,9 +237,6 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat {
   }
 
   PlaneType plane_type_ = PlaneType::kUnknown;
-#if BUILDFLAG(IS_OZONE)
-  bool prefers_external_sampler_ = false;
-#endif
   // `format_` can only be SingleplanarFormat (for single plane, eg. RGBA) or
   // MultiplanarFormat at any given time.
   SharedImageFormatUnion format_;
