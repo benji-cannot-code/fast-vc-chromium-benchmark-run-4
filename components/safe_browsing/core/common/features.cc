ea16bde90449ddebf86b2a2525716bead4bd3a69
Daniel Rubery <drubery@chromium.org>
2023-07-31 19:55:03 +0000

    Enable nested archive unpacking by default
    This feature is approved for launch, so enable it by default.
    
    Bug: 1373671
    Change-Id: Idf2bfe3a948ff9b260637ca9e89d46c800abe87c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4725765
    Reviewed-by: Adam Psarouthakis <psarouthakis@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177432}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -215,7 +215,7 @@ constexpr base::FeatureParam<bool> kMmapSafeBrowsingDatabaseAsync{
 
 BASE_FEATURE(kNestedArchives,
              "SafeBrowsingArchiveImprovements",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRedWarningSurvey,
              "RedWarningSurvey",
