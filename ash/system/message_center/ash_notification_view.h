2faf2cb278240d3a435df2b00c666df4ec1e5903
Andre Le <leandre@chromium.org>
2023-07-31 20:53:50 +0000

    QsRevamp: Set expand/collapse enabled state for group child view
    When calling SetExpandCollapseEnabled in a group parent notification
    view, child notification views should also be in sync with this state.
    
    Fixed: b:293486713
    Change-Id: I797f2de734697ddda4abacaa9b66866efa8bdb8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727492
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177471}

--- ash/system/message_center/ash_notification_view.h
+++ ash/system/message_center/ash_notification_view.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_MESSAGE_CENTER_ASH_NOTIFICATION_VIEW_H_
 #define ASH_SYSTEM_MESSAGE_CENTER_ASH_NOTIFICATION_VIEW_H_
 
+#include <vector>
+
 #include "ash/ash_export.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ptr_exclusion.h"
