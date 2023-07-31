56a598129f27b8673680cc8600cef6187f7bfc6d
Ioana Pandele <ioanap@chromium.org>
2023-07-31 08:51:57 +0000

    [UPMLocalPwd]Check that window!=null before attempting to use it
    Bug: 1466822
    Change-Id: Ia3fd80a8989c4331dda57d30054071b27a4446fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4729688
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177127}

--- chrome/browser/password_manager/android/local_passwords_migration_warning_util.cc
+++ chrome/browser/password_manager/android/local_passwords_migration_warning_util.cc
@@ -42,6 +42,9 @@ void ShowWarning(
     Profile* profile,
     password_manager::metrics_util::PasswordMigrationWarningTriggers
         trigger_source) {
+  if (!window) {
+    return;
+  }
   if (!ShouldShowWarning(profile)) {
     return;
   }
