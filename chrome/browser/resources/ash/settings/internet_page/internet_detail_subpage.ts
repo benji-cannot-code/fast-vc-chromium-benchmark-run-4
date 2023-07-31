b844fe3df7c4d8b1eeb2318a1927378e46f2f8c9
conniekxu <conniekxu@chromium.org>
2023-07-31 19:36:14 +0000

    CrOS Settings: Update Strings for Known Networks and Wi-Fi Details
    This CL updates strings for Settings Network catogary including the
    Known Networks screen and Wi-Fi Network Details screen when feature
    OsSettingsRevampWayfinding is enabled.
    
    After the change, the pages now look like:
    1. Known Networks: https://screenshot.googleplex.com/EEzmHfiopRjFZim
    2. Wi-Fi Details: https://screenshot.googleplex.com/C77TAN5t83NVujE
    
    Bug: b/293647344
    Change-Id: I0517a656f81caf1eacb0f39cff24ad5c6be1f21f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728916
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177422}

--- chrome/browser/resources/ash/settings/internet_page/internet_detail_subpage.ts
+++ chrome/browser/resources/ash/settings/internet_page/internet_detail_subpage.ts
@@ -55,6 +55,7 @@ import {ConnectionStateType, DeviceStateType, IPConfigType, NetworkType, OncSour
 import {afterNextRender, flush, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {assertExists, castExists} from '../assert_extras.js';
+import {isRevampWayfindingEnabled} from '../common/load_time_booleans.js';
 import {Constructor} from '../common/types.js';
 import {DeepLinkingMixin, DeepLinkingMixinInterface} from '../deep_linking_mixin.js';
 import {recordSettingChange} from '../metrics_recorder.js';
@@ -317,6 +318,13 @@ class SettingsInternetDetailPageElement extends
         notify: true,
       },
 
+      isRevampWayfindingEnabled_: {
+        type: Boolean,
+        value: () => {
+          return isRevampWayfindingEnabled();
+        },
+      },
+
       advancedExpanded_: Boolean,
 
       networkExpanded_: Boolean,
@@ -391,6 +399,7 @@ class SettingsInternetDetailPageElement extends
   private isApnRevampEnabled_: boolean;
   private isPasspointEnabled_: boolean;
   private isPasspointSettingsEnabled_: boolean;
+  private isRevampWayfindingEnabled_: boolean;
   private isSecondaryUser_: boolean;
   private isTrafficCountersEnabled_: boolean;
   private isWifiSyncEnabled_: boolean;
