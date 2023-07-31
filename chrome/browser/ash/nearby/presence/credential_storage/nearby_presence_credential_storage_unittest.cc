381b330da7dab623b8291ebd3d573d2d45a4532c
Mark Polner <polner@google.com>
2023-07-31 19:51:50 +0000

    [Nearby] Add Nearby Presence credential storage class skeletons.
    Added two skeleton classes for Nearby Presence credential storage
    implementation -- NearbyPresenceCredentialStorage and
    CredentialStorage. Added an empty NearbyPresenceCredentialStorage
    interface to facilitate communication between the two processes
    that the aforementioned classes are hosted in, which will have
    methods added to it as each method is implemented.
    
    CredentialStorage is a platform implementation of the Credential
    Storage API, used by Nearby Presence to store public and private
    credentials. NearbyPresenceCredentialStorage is the browser
    implementation of methods involved in storing credentials to a
    database. It implements the Mojo interface
    NearbyPresenceCredentialStorage, to allow CredentialStorage to
    call relevant methods to save and retrieve credentials.
    
    Test: Added stub unit tests
    Low-Coverage-Reason: Empty overrides stubs not tested.
    Bug: b:287334088
    Change-Id: I5c330eb899160cdaff5466d24e750a8e3a6313ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4627741
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Mark Polner <polner@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177429}

---
+++ chrome/browser/ash/nearby/presence/credential_storage/nearby_presence_credential_storage_unittest.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/nearby/presence/credential_storage/nearby_presence_credential_storage.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ash::nearby::presence {
+
+class NearbyPresenceCredentialStorageTest : public testing::Test {
+ public:
+  NearbyPresenceCredentialStorageTest() = default;
+
+  ~NearbyPresenceCredentialStorageTest() override = default;
+
+  // testing::Test:
+  void SetUp() override {
+    credential_storage_ = std::make_unique<NearbyPresenceCredentialStorage>();
+  }
+
+ protected:
+  std::unique_ptr<NearbyPresenceCredentialStorage> credential_storage_;
+};
+
+TEST_F(NearbyPresenceCredentialStorageTest, Initialize) {
+  EXPECT_TRUE(credential_storage_);
+}
+
+}  // namespace ash::nearby::presence
---
+++ chrome/services/sharing/nearby/platform/credential_storage.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/platform/credential_storage.h"
+
+namespace nearby::chrome {
+
+CredentialStorage::CredentialStorage(
+    const mojo::SharedRemote<
+        ash::nearby::presence::mojom::NearbyPresenceCredentialStorage>&
+        nearby_presence_credential_storage)
+    : nearby_presence_credential_storage_(nearby_presence_credential_storage) {}
+
+CredentialStorage::~CredentialStorage() = default;
+
+// TODO(b/287333989): Implement.
+void CredentialStorage::SaveCredentials(
+    absl::string_view manager_app_id,
+    absl::string_view account_name,
+    const std::vector<LocalCredential>& private_credentials,
+    const std::vector<SharedCredential>& public_credentials,
+    PublicCredentialType public_credential_type,
+    SaveCredentialsResultCallback callback) {}
+
+// TODO(b/287334012): Implement.
+void CredentialStorage::UpdateLocalCredential(
+    absl::string_view manager_app_id,
+    absl::string_view account_name,
+    nearby::internal::LocalCredential credential,
+    SaveCredentialsResultCallback callback) {}
+
+// TODO(b/287334225): Implement.
+void CredentialStorage::GetLocalCredentials(
+    const CredentialSelector& credential_selector,
+    GetLocalCredentialsResultCallback callback) {}
+
+// TODO(b/287334335): Implement.
+void CredentialStorage::GetPublicCredentials(
+    const CredentialSelector& credential_selector,
+    PublicCredentialType public_credential_type,
+    GetPublicCredentialsResultCallback callback) {}
+
+}  // namespace nearby::chrome
