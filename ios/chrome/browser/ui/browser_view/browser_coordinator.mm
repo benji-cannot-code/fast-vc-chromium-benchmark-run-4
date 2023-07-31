2ee8dfa6930cfc8aa17a9ce38873b9cf0eca4ad2
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-31 11:29:44 +0000

    Revert "[iOS] Disconnect the voice search controller"
    This reverts commit f5dc33d24bb77a593610a2cc0f6e69c115d89013.
    
    Reason for revert: The code was added in the wrong method.
    
    Original change's description:
    > [iOS] Disconnect the voice search controller
    >
    > This should be disconnected before being deallocated.
    >
    > The browser coordinator did not dismiss it.
    >
    > Bug: None
    > Change-Id: I5f387d9582237ef8eb547ea5aa0be1762dd0d8af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4729484
    > Reviewed-by: Federica Germinario <fedegermi@google.com>
    > Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1177168}
    
    Bug: None
    Change-Id: I760598c51282793e278d189218a2180517439b7e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4732992
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1177174}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -1179,11 +1179,6 @@ enum class ToolbarKind {
         _promosManagerCoordinator;
     [_credentialProviderPromoCoordinator start];
   }
-
-  [_voiceSearchController dismissMicPermissionHelp];
-  [_voiceSearchController disconnect];
-  _voiceSearchController.dispatcher = nil;
-  _voiceSearchController = nil;
 }
 
 // Stops child coordinators.
