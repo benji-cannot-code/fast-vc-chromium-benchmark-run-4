4c84eface0e283551d02f870cbf84fbb2cc783ec
Tomasz Jurkiewicz <tju@google.com>
2023-07-31 13:14:45 +0000

    Enable CacheOnlyCertainUrlClassifications experiment everywhere.
    There's a drop in unknown rate for all categories: https://uma.googleplex.com/p/chrome/variations?sid=ba7416cfb5399312592db5ba0c6adb91
    
    Bug: b:272209467
    Change-Id: I31c40c3c5981f1f1f06717698918163d8b0a0b2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4729744
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177205}

--- components/safe_search_api/url_checker.cc
+++ components/safe_search_api/url_checker.cc
@@ -20,7 +20,7 @@ namespace safe_search_api {
 
 BASE_FEATURE(kCacheOnlyCertainUrlClassifications,
              "CacheOnlyCertainUrlClassifications",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 namespace {
 bool ShouldCacheUrlClassification(bool is_uncertain) {
