6dd35ed535f472e0d84c6e1c4582352c80622461
Minoru Chikamune <chikamune@chromium.org>
2023-07-31 23:05:28 +0000

    Reland "[LCPP] Store LCP element locator in database via mojo"
    This CL is a relanding CL of https://crrev.com/c/4672487.
    
    The lifecycle of LCPCriticalPathPredictorHost (browser side) is the same
    as the lifecycle of Document (DocumentService managed the lifetime). On
    the other hand, the lifecycle of LCPCriticalPathPredictor (renderer
    side) is the same as the lifecycle of LocalFrame.
    
    Hence for the first navigation, LCPCriticalPathPredictor could
    successfully send lcp_element, but the subsequent navigation that use
    the same RenderFrameHost couldn't send lcp_element because host_ is
    already disconnected (but still bound) after the first document is
    deleted on the same RenderFrameHost.
    
    1. LCPCriticalPathPredictor --bound to--> LCPCriticalPathPredictorHost
    2. LCPCriticalPathPredictorHost will be deleted when Document is
       deleted.
    3. LCPCriticalPathPredictor will lose connection to
       LCPCriticalPathPredictorHost (but still bound).
    
    This CL fixes this bug by checking host_.is_connected(), and
    reconnecting host.
    
    ```
      if (!host_.is_bound() || !host_.is_connected()) {
        host_.reset();
    ```
    
    Original change's description:
    > Revert "[LCPP] Store LCP element locator in database via mojo"
    >
    > This reverts commit cf4361574a68b5e67aeac94945ad873c508bc04e.
    >
    > Reason for revert: Consistently failing on linux-bfcache-rel.
    >
    > Sample failure:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/49473
    >
    > Original change's description:
    > > [LCPP] Store LCP element locator in database via mojo
    > >
    > > This CL stores LCP element locator information in the
    > > database. Specifically, this CL does the following things:
    > >
    > > - Send LCP element locator candidates from renderer to browser via mojo
    > >   (LCPCriticalPathPredictor and LCPCriticalPathPredictorHost do this).
    > >
    > > - Send LCP element locator candidates to
    > >   LcpCriticalPathPredictorPageLoadMetricsObserver.
    > >
    > > - The latest LCP element locator will be kept in
    > >   LcpCriticalPathPredictorPageLoadMetricsObserver.
    > >
    > > - Save the latest LCP element locator in the database on complete.
    > >
    > > - Provides PredictLcpElementLocators() for prediction. (This is needed
    > >   for test.)
    > >
    > > To send the LCP element locator from LCPCriticalPathPredictorHost to
    > > LcpCriticalPathPredictorPageLoadMetricsObserver, this CL uses the
    > > PageUserData.
    > >
    > > https://docs.google.com/document/d/1waakt6bSvedWdaUQ2mC255NF4k8j7LybK2dQ7WptxiE
    > >
    > > Bug: 1419756
    > > Change-Id: Ie14ae5be298c15bed23f69d2fe49fc1ad03868a9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672487
    > > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > > Reviewed-by: Ken Okada <kenoss@chromium.org>
    > > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > > Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1176019}
    >
    > Bug: 1419756
    > Change-Id: Ic0377d21c151decaa26fe5a6cd09a9e1aa34aa1c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4725604
    > Owners-Override: Igor Ruvinov <igorruvinov@chromium.org>
    > Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1176078}
    
    Bug: 1419756
    Change-Id: Ife264cf2dd5420d89497ad4131d92fd7042ace47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728424
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177530}

--- chrome/browser/predictors/lcp_critical_path_predictor/lcp_critical_path_predictor_host.h
+++ chrome/browser/predictors/lcp_critical_path_predictor/lcp_critical_path_predictor_host.h
@@ -38,6 +38,9 @@ class LCPCriticalPathPredictorHost
           receiver);
 
   ~LCPCriticalPathPredictorHost() override;
+
+  // Implements blink::mojom::LCPCriticalPathPredictorHost.
+  void SetLcpElementLocator(const std::string& lcp_element_locator) override;
 };
 
 }  // namespace predictors
