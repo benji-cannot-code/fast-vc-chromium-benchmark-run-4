94851979c1508233b9ffb49f4d8d87fc56774cb8
Ivana Žužić <izuzic@google.com>
2023-08-01 13:55:55 +0000

    [UPMLocalPwd] Add an empty constructor to prevent Chrome from crashing
    PasswordMigrationWarningIntroFragment was lacking an empty constructor
    that is called by Android to recreate it if the Chrome Activity was
    destroyed because of a lack of memory.
    
    Bug: 1284896
    Change-Id: I3176673e6c1b07c095b83f671855fca280e823b4
    Low-Coverage-Reason: Pre-existing low-coverage.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738373
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1177785}

--- chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PasswordMigrationWarningIntroFragment.java
+++ chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PasswordMigrationWarningIntroFragment.java
@@ -30,6 +30,8 @@ public class PasswordMigrationWarningIntroFragment extends Fragment {
         mMoreOptionsCallback = moreOptionsCallback;
     }
 
+    public PasswordMigrationWarningIntroFragment() {}
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         TextViewWithLeading subtitleView = view.findViewById(R.id.migration_warning_sheet_subtitle);
