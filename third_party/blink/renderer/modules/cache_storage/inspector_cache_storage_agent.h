76cb99db1e5ca9a0cc4d940515a1a6f0514dcc47
Geoffrey Alexander <geoffalexander@google.com>
2023-08-01 18:07:55 +0000

    [Code Health] Migrate InspectorCacheStorageAgent to GC Mojo
    Migrate InspectorCacheStorageAgent use of mojo::Remote to
    HeapMojoRemote and use of HashMap to HeapHashMap.
    
    This CL adds a check for unbound, cached remotes in the default bucket
    and replaces the unbound remote.
    
    Bug: 1426370
    Change-Id: If39d475a29116712464b7e34e612bcab2db5de4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4716845
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Geoff Alexander <geoffalexander@google.com>
    Cr-Commit-Position: refs/heads/main@{#1177941}

--- third_party/blink/renderer/modules/cache_storage/inspector_cache_storage_agent.h
+++ third_party/blink/renderer/modules/cache_storage/inspector_cache_storage_agent.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/inspector_base_agent.h"
 #include "third_party/blink/renderer/core/inspector/protocol/cache_storage.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/heap/disallow_new_wrapper.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/wtf/gc_plugin.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -24,7 +26,9 @@ class InspectedFrames;
 class MODULES_EXPORT InspectorCacheStorageAgent final
     : public InspectorBaseAgent<protocol::CacheStorage::Metainfo> {
  public:
-  using CachesMap = HashMap<String, mojo::Remote<mojom::blink::CacheStorage>>;
+  using CachesMap = HeapHashMap<
+      String,
+      Member<DisallowNewWrapper<HeapMojoRemote<mojom::blink::CacheStorage>>>>;
 
   explicit InspectorCacheStorageAgent(InspectedFrames*);
 
@@ -69,8 +73,6 @@ class MODULES_EXPORT InspectorCacheStorageAgent final
       String& cache_name,
       base::OnceCallback<void(protocol::Response)> on_failure_callback);
   Member<InspectedFrames> frames_;
-
-  GC_PLUGIN_IGNORE("https://crbug.com/1381979")
   CachesMap caches_;
 };
 
