8c772cc69409e3d2cf36ad62cd8594fc5755def2
Joseph Pang <pjlee@google.com>
2023-08-01 23:48:51 +0000

    arc/gio: Remove button options menu from alpha
    Currently, the button options menu and the button options menu shows up
    in alpha after the user clicks on and drags the touch point for game
    controls alpha. This CL relegates that change to game controls beta only.
    
    Bug: b/293645413
    Test: Manual; disable input overlay beta, click on any touch point, then verify that there is no crash and nothing happens
    Change-Id: I13d3a932b8f0967021f37059297646b2efc4814d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740572
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Joseph Pang <pjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178144}

--- chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
+++ chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
@@ -642,6 +642,10 @@ void DisplayOverlayController::RemoveTouchInjectorObserver(
 }
 
 void DisplayOverlayController::AddButtonOptionsMenuWidget(Action* action) {
+  if (!IsBeta()) {
+    return;
+  }
+
   if (button_options_widget_) {
     auto* menu = static_cast<ButtonOptionsMenu*>(
         button_options_widget_->GetContentsView());
