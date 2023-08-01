07918972f4178bd7f7209b79185934814038b038
Gabriel Oliveira <gabolvr@google.com>
2023-08-01 10:06:34 +0000

    [fyfre] Set up layout for default browser promo subpage in FRE
    Preview:
    https://screencast.googleplex.com/cast/NTEzNDk3MjE2NDc2Nzc0NHxjOWQ1ZjA4Mi04Zg
    
    Bug: 1465822
    Change-Id: I089908ab3f430d3246799f5c617947fdaf935ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4732935
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1177704}

--- chrome/browser/resources/intro/default_browser/app.ts
+++ chrome/browser/resources/intro/default_browser/app.ts
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://intro/tangible_sync_style_shared.css.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
+
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './app.html.js';
@@ -14,6 +18,14 @@ export class DefaultBrowserAppElement extends PolymerElement {
 
   static get template() {
     return getTemplate();
+  }
+
+  private onConfirmDefaultBrowserClick_() {
+    // TODO(crbug.com/1465822): Implement button action.
+  }
+
+  private onSkipDefaultBrowserClick_() {
+    // TODO(crbug.com/1465822): Implement button action.
   }
 }
 
