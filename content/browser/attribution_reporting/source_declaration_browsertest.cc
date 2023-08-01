ecf92fe115cd157de0ff4125c929ad4a0c15c565
John Delaney <johnidel@chromium.org>
2023-08-01 18:09:25 +0000

    Enable PrivacySandboxAdsAPIsM1Override in field trial config
    Change-Id: I2dc9370dde921209baea1a44a0ca061d2b1e6d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706166
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177942}

--- content/browser/attribution_reporting/source_declaration_browsertest.cc
+++ content/browser/attribution_reporting/source_declaration_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_writer.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/platform_thread.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/attribution_reporting/test/source_observer.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -43,9 +45,9 @@ using ::testing::Pair;
 using ::testing::Pointee;
 using ::testing::UnorderedElementsAre;
 
-class AttributionSourceDisabledBrowserTest : public ContentBrowserTest {
+class AttributionSourceBrowserTest : public ContentBrowserTest {
  public:
-  AttributionSourceDisabledBrowserTest() = default;
+  AttributionSourceBrowserTest() = default;
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
@@ -74,6 +76,19 @@ class AttributionSourceDisabledBrowserTest : public ContentBrowserTest {
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
 };
 
+class AttributionSourceDisabledBrowserTest : public AttributionSourceBrowserTest {
+ public:
+  AttributionSourceDisabledBrowserTest() {
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/
+        {},
+        /*disabled_features=*/{features::kPrivacySandboxAdsAPIsM1Override});
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
 // Verifies that impressions are not logged when the Runtime feature isn't
 // enabled.
 IN_PROC_BROWSER_TEST_F(AttributionSourceDisabledBrowserTest,
@@ -100,7 +115,7 @@ IN_PROC_BROWSER_TEST_F(AttributionSourceDisabledBrowserTest,
 }
 
 class AttributionSourceDeclarationBrowserTest
-    : public AttributionSourceDisabledBrowserTest {
+    : public AttributionSourceBrowserTest {
  public:
   AttributionSourceDeclarationBrowserTest() = default;
 
