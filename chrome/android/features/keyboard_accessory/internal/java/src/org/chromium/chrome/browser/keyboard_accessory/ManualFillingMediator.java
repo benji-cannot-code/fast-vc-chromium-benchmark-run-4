ba51345e8cd707b6c9e4015b46534464d05d355e
Adem Derinel <derinel@google.com>
2023-08-01 11:14:33 +0000

    [Android][mfill] Remove Keyboard Accessory V1 states
    The feature is long launched.
    
    Bug: 1420520
    Change-Id: I2b9d331783b8b24d68c067b7842f7896d9cceba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4737991
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177723}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
@@ -10,12 +10,9 @@ import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProper
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.EXTENDING_KEYBOARD;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.FLOATING_BAR;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.FLOATING_SHEET;
-import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.FLOATING_SHEET_V2;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.HIDDEN;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.REPLACING_KEYBOARD;
-import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.REPLACING_KEYBOARD_V2;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.WAITING_TO_REPLACE;
-import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.KeyboardExtensionState.WAITING_TO_REPLACE_V2;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.PORTRAIT_ORIENTATION;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.SHOULD_EXTEND_KEYBOARD;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingProperties.SHOW_WHEN_VISIBLE;
@@ -211,7 +208,7 @@ class ManualFillingMediator
         if (!isInitialized()) return; // Activity uninitialized or cleaned up already.
         if (mKeyboardAccessory.empty()) return; // Exit early to not affect the layout.
         if (!hasSufficientSpace()) {
-            mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+            mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
             return;
         }
         if (hasPortraitOrientation() != mModel.get(PORTRAIT_ORIENTATION)) {
@@ -221,10 +218,10 @@ class ManualFillingMediator
         restrictAccessorySheetHeight();
         if (!isSoftKeyboardShowing(view)) {
             if (is(WAITING_TO_REPLACE)) {
-                mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(REPLACING_KEYBOARD));
+                mModel.set(KEYBOARD_EXTENSION_STATE, REPLACING_KEYBOARD);
             }
             if (is(EXTENDING_KEYBOARD)) {
-                mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+                mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
             }
             // Cancel animations if the keyboard suddenly closes so the bar doesn't linger.
             if (is(HIDDEN)) mKeyboardAccessory.skipClosingAnimationOnce();
@@ -234,8 +231,7 @@ class ManualFillingMediator
         }
         if (is(WAITING_TO_REPLACE)) return;
         mModel.set(KEYBOARD_EXTENSION_STATE,
-                mModel.get(SHOW_WHEN_VISIBLE) ? getCompatibleState(EXTENDING_KEYBOARD)
-                                              : getCompatibleState(HIDDEN));
+                mModel.get(SHOW_WHEN_VISIBLE) ? EXTENDING_KEYBOARD : HIDDEN);
     }
 
     private boolean hasPortraitOrientation() {
@@ -334,13 +330,13 @@ class ManualFillingMediator
         if (!isInitialized()) return;
         mModel.set(SHOW_WHEN_VISIBLE, true);
         mModel.set(SHOULD_EXTEND_KEYBOARD, shouldExtendKeyboard);
-        if (is(HIDDEN)) mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(FLOATING_BAR));
+        if (is(HIDDEN)) mModel.set(KEYBOARD_EXTENSION_STATE, FLOATING_BAR);
     }
 
     void hide() {
         mModel.set(SHOW_WHEN_VISIBLE, false);
         if (!isInitialized()) return;
-        mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+        mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
     }
 
     void showAccessorySheetTab(@AccessoryTabType int tabType) {
@@ -349,7 +345,7 @@ class ManualFillingMediator
         }
         mModel.set(SHOW_WHEN_VISIBLE, true);
         if (is(HIDDEN)) {
-            mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(REPLACING_KEYBOARD));
+            mModel.set(KEYBOARD_EXTENSION_STATE, REPLACING_KEYBOARD);
         }
         mKeyboardAccessory.setActiveTab(tabType);
     }
@@ -360,14 +356,14 @@ class ManualFillingMediator
         // When pause is called, the accessory needs to disappear fast since some UI forced it to
         // close (e.g. a scene changed or the screen was turned off).
         mKeyboardAccessory.skipClosingAnimationOnce();
-        mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+        mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
     }
 
     private void onOrientationChange() {
         if (!isInitialized()) return;
         if (ChromeFeatureList.isEnabled(AUTOFILL_KEYBOARD_ACCESSORY) || is(REPLACING_KEYBOARD)
                 || is(FLOATING_SHEET)) {
-            mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+            mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
             // Autofill suggestions are invalidated on rotation. Dismissing all filling UI forces
             // the user to interact with the field they want to edit. This refreshes Autofill.
             if (ChromeFeatureList.isEnabled(AUTOFILL_KEYBOARD_ACCESSORY)) {
@@ -420,7 +416,7 @@ class ManualFillingMediator
             return;
         } else if (property == SUPPRESSED_BY_BOTTOM_SHEET) {
             if (isInitialized() && mModel.get(SUPPRESSED_BY_BOTTOM_SHEET)) {
-                mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+                mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
             }
             return;
         } else if (property == SHOULD_EXTEND_KEYBOARD) {
@@ -456,35 +452,32 @@ class ManualFillingMediator
                 return true;
             case FLOATING_BAR:
                 if (mModel.get(SHOULD_EXTEND_KEYBOARD) && isSoftKeyboardShowing(getContentView())) {
-                    mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(EXTENDING_KEYBOARD));
+                    mModel.set(KEYBOARD_EXTENSION_STATE, EXTENDING_KEYBOARD);
                     return false;
                 }
                 if (!mModel.get(SHOULD_EXTEND_KEYBOARD)) return true;
                 // Intentional fallthrough.
             case EXTENDING_KEYBOARD:
                 if (!canExtendKeyboard() || mModel.get(SUPPRESSED_BY_BOTTOM_SHEET)) {
-                    mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+                    mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
                     return false;
                 }
                 return true;
             case FLOATING_SHEET:
-            case FLOATING_SHEET_V2:
                 if (isSoftKeyboardShowing(getContentView())) {
-                    mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(EXTENDING_KEYBOARD));
+                    mModel.set(KEYBOARD_EXTENSION_STATE, EXTENDING_KEYBOARD);
                     return false;
                 }
                 // Intentional fallthrough.
             case REPLACING_KEYBOARD:
-            case REPLACING_KEYBOARD_V2:
                 if (isSoftKeyboardShowing(getContentView())) {
-                    mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(WAITING_TO_REPLACE));
+                    mModel.set(KEYBOARD_EXTENSION_STATE, WAITING_TO_REPLACE);
                     return false; // Wait for the keyboard to disappear before replacing!
                 }
                 // Intentional fallthrough.
             case WAITING_TO_REPLACE:
-            case WAITING_TO_REPLACE_V2:
                 if (!hasSufficientSpace() || mModel.get(SUPPRESSED_BY_BOTTOM_SHEET)) {
-                    mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(HIDDEN));
+                    mModel.set(KEYBOARD_EXTENSION_STATE, HIDDEN);
                     return false;
                 }
                 return true;
@@ -531,7 +524,7 @@ class ManualFillingMediator
             // transitions into the EXTENDING state as soon as the keyboard appeared.
             ViewGroup contentView = getContentView();
             if (contentView != null) mSoftKeyboardDelegate.showSoftKeyboard(contentView);
-        } else if (extensionState == getCompatibleState(WAITING_TO_REPLACE)) {
+        } else if (extensionState == WAITING_TO_REPLACE) {
             // In order to give the keyboard time to disappear, hide the keyboard and enter the
             // REPLACING state.
             hideSoftKeyboard();
@@ -575,18 +568,18 @@ class ManualFillingMediator
         mAccessorySheet.setActiveTab(tabIndex);
         if (mPopup != null && mPopup.isShowing()) mPopup.dismiss();
         if (is(EXTENDING_KEYBOARD)) {
-            mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(REPLACING_KEYBOARD));
+            mModel.set(KEYBOARD_EXTENSION_STATE, REPLACING_KEYBOARD);
         } else if (is(FLOATING_BAR)) {
-            mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(FLOATING_SHEET));
+            mModel.set(KEYBOARD_EXTENSION_STATE, FLOATING_SHEET);
         }
     }
 
     @Override
     public void onCloseAccessorySheet() {
         if (is(REPLACING_KEYBOARD) || is(WAITING_TO_REPLACE)) {
-            mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(FLOATING_SHEET));
+            mModel.set(KEYBOARD_EXTENSION_STATE, FLOATING_SHEET);
         } else if (is(FLOATING_SHEET)) {
-            mModel.set(KEYBOARD_EXTENSION_STATE, getCompatibleState(FLOATING_BAR));
+            mModel.set(KEYBOARD_EXTENSION_STATE, FLOATING_BAR);
         }
     }
 
@@ -621,7 +614,7 @@ class ManualFillingMediator
     }
 
     private void changeBottomControlSpaceForState(int extensionState) {
-        if (extensionState == getCompatibleState(WAITING_TO_REPLACE)) return; // Don't change yet.
+        if (extensionState == WAITING_TO_REPLACE) return; // Don't change yet.
         int newControlsHeight = 0;
         int newControlsOffset = 0;
         if (requiresVisibleBar(extensionState)) {
@@ -836,7 +829,7 @@ class ManualFillingMediator
     }
 
     private boolean is(@KeyboardExtensionState int state) {
-        return mModel.get(KEYBOARD_EXTENSION_STATE) == getCompatibleState(state);
+        return mModel.get(KEYBOARD_EXTENSION_STATE) == state;
     }
 
     private static String getNameForState(@KeyboardExtensionState int state) {
@@ -853,12 +846,6 @@ class ManualFillingMediator
                 return "FLOATING_BAR";
             case FLOATING_SHEET:
                 return "FLOATING_SHEET";
-            case REPLACING_KEYBOARD_V2:
-                return "REPLACING_KEYBOARD_V2";
-            case WAITING_TO_REPLACE_V2:
-                return "WAITING_TO_REPLACE_V2";
-            case FLOATING_SHEET_V2:
-                return "FLOATING_SHEET_V2";
         }
         return null;
     }
@@ -868,32 +855,6 @@ class ManualFillingMediator
         if (accessibility != null) {
             accessibility.restoreFocus();
         }
-    }
-
-    private @KeyboardExtensionState int getCompatibleState(@KeyboardExtensionState int state) {
-        switch (state) {
-            case HIDDEN:
-                return HIDDEN;
-            case EXTENDING_KEYBOARD:
-                return EXTENDING_KEYBOARD;
-            case REPLACING_KEYBOARD:
-                return ChromeFeatureList.isEnabled(AUTOFILL_KEYBOARD_ACCESSORY)
-                        ? REPLACING_KEYBOARD_V2
-                        : REPLACING_KEYBOARD;
-            case WAITING_TO_REPLACE:
-                return ChromeFeatureList.isEnabled(AUTOFILL_KEYBOARD_ACCESSORY)
-                        ? WAITING_TO_REPLACE_V2
-                        : WAITING_TO_REPLACE;
-            case FLOATING_BAR:
-                return FLOATING_BAR;
-            case FLOATING_SHEET:
-                return ChromeFeatureList.isEnabled(AUTOFILL_KEYBOARD_ACCESSORY) ? FLOATING_SHEET_V2
-                                                                                : FLOATING_SHEET;
-            default:
-                assert false
-                    : "Non V2 states should not be queried to keep the state machine simple!";
-        }
-        return HIDDEN;
     }
 
     private int getHeaderHeight() {
