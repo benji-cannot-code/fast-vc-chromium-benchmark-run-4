1d8f7172eea999bb6321c4c53dbdfded9a39a75e
Wei-Sheng Su <weishengsu@google.com>
2023-08-01 18:36:59 +0000

    Add Enums for Cast CRL Fallback
    Add enums according to design doc:go/cast-crl-fallback
    
    Bug: 290788513
    Change-Id: I89e1d5ba4acfd58a58f74e3fd74ee63f599cc20b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678761
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Wei-Sheng Su <weishengsu@google.com>
    Commit-Queue: Ryan Chung <ryanchung@chromium.org>
    Reviewed-by: Ryan Chung <ryanchung@chromium.org>
    Auto-Submit: Wei-Sheng Su <weishengsu@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177962}

--- components/media_router/common/providers/cast/channel/cast_channel_metrics.h
+++ components/media_router/common/providers/cast/channel/cast_channel_metrics.h
@@ -27,7 +27,9 @@ enum class CastCertificateStatus {
   kRestrictionsFailed,
   kMissingCerts,
   kUnexpectedFailed,
-  kMaxValue = kUnexpectedFailed,
+  kInvalidFallbackCRL,
+  kCertificateRevokedByFallbackCRL,
+  kMaxValue = kCertificateRevokedByFallbackCRL,
 };
 
 // Must match with histogram enum CastNonce.
