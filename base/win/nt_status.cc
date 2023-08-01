619b728bdd597b7e1fb561432ba172377adac01c
James Forshaw <forshaw@chromium.org>
2023-08-01 16:35:23 +0000

    [Windows] Directly link to RtlGetLastNtStatus function.
    This CL directly links to the RtlGetLastNtStatus function from NTDLL
    instead of dynamically linking at runtime.
    
    Bug: 1282011
    Change-Id: I6b137a73e412f4bee56091668a5ce24d979b78c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4739172
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177878}

--- base/win/nt_status.cc
+++ base/win/nt_status.cc
@@ -6,14 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/nt_status.h"
 
 #include <windows.h>
-#include <winternl.h>
 
-#include "base/check.h"
-
-using GetLastNtStatusFn = NTSTATUS NTAPI (*)();
-
-constexpr const wchar_t kNtDllName[] = L"ntdll.dll";
-constexpr const char kLastStatusFnName[] = "RtlGetLastNtStatus";
+extern "C" NTSTATUS WINAPI RtlGetLastNtStatus();
 
 namespace base {
 namespace win {
@@ -23,10 +17,8 @@ NTSTATUS GetLastNtStatus() {
   // LastStatusValue from the returned _TEB structure, except that the public
   // _TEB struct definition does not actually specify the location of the
   // LastStatusValue field. We avoid depending on such a definition by
-  // internally using RtGetLastNtStatus() from ntdll.dll instead.
-  static auto* get_last_nt_status = reinterpret_cast<GetLastNtStatusFn>(
-      ::GetProcAddress(::GetModuleHandle(kNtDllName), kLastStatusFnName));
-  return get_last_nt_status();
+  // internally using RtlGetLastNtStatus() from ntdll.dll instead.
+  return ::RtlGetLastNtStatus();
 }
 
 }  // namespace win
