8bef5e5f1876febe718a772751bd8dabbd9552fb
conniekxu <conniekxu@chromium.org>
2023-08-01 22:31:59 +0000

    CrOS Settings: Update Strings for Device Top Level, Touchpad Screens
    This CL updates strings for Settings Device catogary including the Top
    Level screen and Touchpad screen when feature OsSettingsRevampWayfinding
    is enabled.
    
    After the change, the pages now look like:
    1. Top Level: https://screenshot.googleplex.com/72jQFfUrn7DaZME
    2. Touchpad: https://screenshot.googleplex.com/BHkJHWatS8HdhDa
    
    Bug: b/293896460
    Change-Id: I0f59a0e46551f6a6612efebef85c1a9af1b82b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4736587
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178108}

--- chrome/browser/resources/ash/settings/device_page/per_device_touchpad_subsection.ts
+++ chrome/browser/resources/ash/settings/device_page/per_device_touchpad_subsection.ts
@@ -26,6 +26,7 @@ import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {PolymerElementProperties} from 'chrome://resources/polymer/v3_0/polymer/interfaces.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {isRevampWayfindingEnabled} from '../common/load_time_booleans.js';
 import {DeepLinkingMixin} from '../deep_linking_mixin.js';
 import {Setting} from '../mojom-webui/setting.mojom-webui.js';
 import {RouteObserverMixin} from '../route_observer_mixin.js';
@@ -200,6 +201,13 @@ export class SettingsPerDeviceTouchpadSubsectionElement extends
         readOnly: true,
       },
 
+      isRevampWayfindingEnabled_: {
+        type: Boolean,
+        value: () => {
+          return isRevampWayfindingEnabled();
+        },
+      },
+
       reverseScrollValue: {
         type: Boolean,
         value: false,
@@ -294,6 +302,7 @@ export class SettingsPerDeviceTouchpadSubsectionElement extends
   private touchpadIndex: number;
   private isLastDevice: boolean;
   isAltClickAndSixPackCustomizationEnabled: boolean;
+  private isRevampWayfindingEnabled_: boolean;
 
   private updateSettingsToCurrentPrefs(): void {
     // `updateSettingsToCurrentPrefs` gets called when the `keyboard` object
@@ -397,6 +406,27 @@ export class SettingsPerDeviceTouchpadSubsectionElement extends
     });
 
     return tempEl.innerHTML;
+  }
+
+  private getTouchpadAccelerationDescription(): string {
+    if (this.isRevampWayfindingEnabled_) {
+      return this.i18n('touchpadAccelerationDescription');
+    }
+    return '';
+  }
+
+  private getTouchpadTapDraggingDescription(): string {
+    if (this.isRevampWayfindingEnabled_) {
+      return this.i18n('tapDraggingDescription');
+    }
+    return '';
+  }
+
+  private getTouchpadTapToClickDescription(): string {
+    if (this.isRevampWayfindingEnabled_) {
+      return this.i18n('touchpadTapToClickDescription');
+    }
+    return '';
   }
 }
 
--- chrome/browser/ui/webui/settings/ash/device_section.cc
+++ chrome/browser/ui/webui/settings/ash/device_section.cc
@@ -1188,9 +1188,16 @@ DeviceSection::~DeviceSection() {
 }
 
 void DeviceSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
-  static constexpr webui::LocalizedString kDeviceStrings[] = {
+  const bool kIsRevampEnabled =
+      ash::features::IsOsSettingsRevampWayfindingEnabled();
+
+  webui::LocalizedString kDeviceStrings[] = {
       {"devicePageTitle", IDS_SETTINGS_DEVICE_TITLE},
-      {"touchPadScrollLabel", IDS_OS_SETTINGS_TOUCHPAD_REVERSE_SCROLL_LABEL},
+      {"touchpadScrollLabel",
+       kIsRevampEnabled ? IDS_OS_SETTINGS_REVAMP_TOUCHPAD_REVERSE_SCROLL_LABEL
+                        : IDS_OS_SETTINGS_TOUCHPAD_REVERSE_SCROLL_LABEL},
+      {"touchpadScrollDescription",
+       IDS_OS_SETTINGS_REVAMP_TOUCHPAD_REVERSE_SCROLL_DESCRIPTION},
   };
   html_source->AddLocalizedStrings(kDeviceStrings);
 
@@ -1720,7 +1727,10 @@ void DeviceSection::UpdateStylusSearchTags() {
 
 void DeviceSection::AddDevicePointersStrings(
     content::WebUIDataSource* html_source) {
-  static constexpr webui::LocalizedString kPointersStrings[] = {
+  const bool kIsRevampEnabled =
+      ash::features::IsOsSettingsRevampWayfindingEnabled();
+
+  webui::LocalizedString kPointersStrings[] = {
       {"mouseTitle", IDS_SETTINGS_MOUSE_TITLE},
       {"builtInPointingStickName", IDS_SETTINGS_BUILT_IN_POINTING_STICK_NAME},
       {"pointingStickTitle", IDS_SETTINGS_POINTING_STICK_TITLE},
@@ -1728,7 +1738,10 @@ void DeviceSection::AddDevicePointersStrings(
       {"touchpadTitle", IDS_SETTINGS_TOUCHPAD_TITLE},
       {"mouseAndTouchpadTitle", IDS_SETTINGS_MOUSE_AND_TOUCHPAD_TITLE},
       {"touchpadTapToClickEnabledLabel",
-       IDS_SETTINGS_TOUCHPAD_TAP_TO_CLICK_ENABLED_LABEL},
+       kIsRevampEnabled ? IDS_OS_SETTINGS_REVAMP_TOUCHPAD_TAP_TO_CLICK_LABEL
+                        : IDS_SETTINGS_TOUCHPAD_TAP_TO_CLICK_ENABLED_LABEL},
+      {"touchpadTapToClickDescription",
+       IDS_OS_SETTINGS_REVAMP_TOUCHPAD_TAP_TO_CLICK_DESCRIPTION},
       {"touchpadSpeed", IDS_SETTINGS_TOUCHPAD_SPEED_LABEL},
       {"pointerSlow", IDS_SETTINGS_POINTER_SPEED_SLOW_LABEL},
       {"pointerFast", IDS_SETTINGS_POINTER_SPEED_FAST_LABEL},
@@ -1751,7 +1764,11 @@ void DeviceSection::AddDevicePointersStrings(
        IDS_SETTINGS_MOUSE_SCROLL_ACCELERATION_LABEL},
       {"pointingStickAccelerationLabel",
        IDS_SETTINGS_POINTING_STICK_ACCELERATION_LABEL},
-      {"touchpadAccelerationLabel", IDS_SETTINGS_TOUCHPAD_ACCELERATION_LABEL},
+      {"touchpadAccelerationLabel",
+       kIsRevampEnabled ? IDS_OS_SETTINGS_REVAMP_TOUCHPAD_ACCELERATION_LABEL
+                        : IDS_SETTINGS_TOUCHPAD_ACCELERATION_LABEL},
+      {"touchpadAccelerationDescription",
+       IDS_OS_SETTINGS_REVAMP_TOUCHPAD_ACCELERATION_DESCRIPTION},
       {"touchpadHapticClickSensitivityLabel",
        IDS_SETTINGS_TOUCHPAD_HAPTIC_CLICK_SENSITIVITY_LABEL},
       {"touchpadHapticFeedbackTitle",
@@ -1833,7 +1850,10 @@ void DeviceSection::AddCustomizeButtonsPageStrings(
 
 void DeviceSection::AddDeviceDisplayStrings(
     content::WebUIDataSource* html_source) const {
-  static constexpr webui::LocalizedString kDisplayStrings[] = {
+  const bool kIsRevampEnabled =
+      ash::features::IsOsSettingsRevampWayfindingEnabled();
+
+  webui::LocalizedString kDisplayStrings[] = {
       {"displayAmbientColorTitle", IDS_SETTINGS_DISPLAY_AMBIENT_COLOR_TITLE},
       {"displayAmbientColorSubtitle",
        IDS_SETTINGS_DISPLAY_AMBIENT_COLOR_SUBTITLE},
@@ -1895,7 +1915,8 @@ void DeviceSection::AddDeviceDisplayStrings(
       {"displayScreenTitle", IDS_SETTINGS_DISPLAY_SCREEN},
       {"displaySizeSliderMaxLabel", IDS_SETTINGS_DISPLAY_ZOOM_SLIDER_MAXIMUM},
       {"displaySizeSliderMinLabel", IDS_SETTINGS_DISPLAY_ZOOM_SLIDER_MINIMUM},
-      {"displayTitle", IDS_SETTINGS_DISPLAY_TITLE},
+      {"displayTitle", kIsRevampEnabled ? IDS_OS_SETTINGS_REVAMP_DISPLAY_TITLE
+                                        : IDS_SETTINGS_DISPLAY_TITLE},
       {"displayTouchCalibrationText",
        IDS_SETTINGS_DISPLAY_TOUCH_CALIBRATION_TEXT},
       {"displayTouchCalibrationTitle",
