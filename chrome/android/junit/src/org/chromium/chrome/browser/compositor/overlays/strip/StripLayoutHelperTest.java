a1e6ddf837dd03391ff8736fe77d677c6f11b400
Calder Kitagawa <ckitagawa@chromium.org>
2023-08-01 17:17:32 +0000

    [Tab Strip] Avoid closing tabs inside commitAllTabClosures.
    Credit to agdoug@amazon.com for the testcase and helping to track
    down where this is originating after I added some asserts last week.
    Also for suggesting the StripLayoutHelperTest fix.
    
    What looks to be happening is that during commitAllTabClosures the
    TabletTabStrip as part of its animation cycles pushes more updates
    to close tabs that weren't part of the last animation batch. This
    results in a concurrent modification to PendingTabClosureManager's
    mTabClosureEvents.
    
    I think post tasking the closures will avoid this issue, but I'm
    not an expert on Tablet Tab Strip so reviewers please let me
    know your thoughts.
    
    An alternative approach might be to just commit all tab
    closures in the queue, but this risks leaving the mRewoundList
    and mTabClosureEvents objects in a bad state so I'd rather we fix
    this at the source.
    
    Bug: 1465745
    Change-Id: I52ade81949ff89f726e3d4336c47619bd2ee20c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727816
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177911}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperTest.java
@@ -52,6 +52,8 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
+import org.robolectric.annotation.LooperMode;
+import org.robolectric.annotation.LooperMode.Mode;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -93,7 +95,7 @@ import java.util.List;
         ChromeFeatureList.TAB_GROUPS_FOR_TABLETS,
         ChromeFeatureList.TAB_DRAG_DROP_ANDROID})
 @Config(manifest = Config.NONE, qualifiers = "sw600dp", shadows = {ShadowAppCompatResources.class})
-
+@LooperMode(Mode.LEGACY)
 public class StripLayoutHelperTest {
     // clang-format on
     @Rule
@@ -1834,7 +1836,7 @@ public class StripLayoutHelperTest {
 
         // Assert: Tab is closed and animations are still running.
         int expectedTabCount = 14;
-        assertEquals("Unexpected tabs count.", expectedTabCount,
+        assertEquals("Unexpected tabs count", expectedTabCount,
                 mStripLayoutHelper.getStripLayoutTabsForTesting().length);
         assertTrue("MultiStepAnimations should still be running.",
                 mStripLayoutHelper.isMultiStepCloseAnimationsRunningForTesting());
