886b97d6caf25f039606a52e88e1c590fd870828
Vishwas Uppoor <vishwasuppoor@google.com>
2023-08-01 16:33:07 +0000

    [CVC Storage][Desktop] Update save animation string
    Update the save animation to inform "CVC saved" when only CVC is saved.
    
    Screen recording: https://screencast.googleplex.com/cast/NTIxMDc4NTUxODE5MDU5MnxmMGM3N2Q4Mi1lOQ
    Note: The recording is only a demo. CVC save flow is not ready yet.
    
    Change-Id: I3d1af9266c8dd8f9afa2f56749d2cdf35bdaf695
    Bug: 1462821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721639
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1177876}

--- chrome/browser/ui/autofill/payments/iban_bubble_controller_impl.h
+++ chrome/browser/ui/autofill/payments/iban_bubble_controller_impl.h
@@ -70,6 +70,7 @@ class IbanBubbleControllerImpl
   bool IsIconVisible() const override;
   AutofillBubbleBase* GetPaymentBubbleView() const override;
   PaymentBubbleType GetPaymentBubbleType() const override;
+  int GetSaveSuccessAnimationStringId() const override;
 
   // For testing.
   void SetEventObserverForTesting(ObserverForTest* observer) {
