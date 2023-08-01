8aed65ccbb4725506e9f64dabd21256464be5954
Muyao Xu <muyaoxu@google.com>
2023-08-01 22:01:17 +0000

    Revert "graphite: dcomp: refactor some methods into a new d3d_image_utils.{cc,h}"
    This reverts commit 814d5c705d4770dc78a3bc9595cfa0fcb800b4d3.
    
    Reason for revert: Possible culprit for test failures on fuchsia.
    crbug.com/1469428
    
    Original change's description:
    > graphite: dcomp: refactor some methods into a new d3d_image_utils.{cc,h}
    >
    > So those methods can be shared with dcomp and dxgi swapchain backing.
    >
    > Bug: 1454194
    > Change-Id: I3a78adf373dddd7acee64136115db48afce893a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728172
    > Auto-Submit: Peng Huang <penghuang@chromium.org>
    > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1177959}
    
    Bug: 1454194
    Change-Id: If761fe5ce96308983bad2ba34ef63a9c5782c9b2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740280
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Owners-Override: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178089}

--- gpu/command_buffer/service/shared_image/dcomp_surface_image_backing.cc
+++ gpu/command_buffer/service/shared_image/dcomp_surface_image_backing.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/shared_image_format_utils.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/service/shared_context_state.h"
-#include "gpu/command_buffer/service/shared_image/d3d_image_utils.h"
+#include "gpu/command_buffer/service/shared_image/d3d_image_backing_factory.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h"
 #include "third_party/angle/include/EGL/egl.h"
 #include "third_party/angle/include/EGL/eglext.h"
@@ -51,7 +51,8 @@ bool ClearDCompSurface(IDCompositionSurface* surface,
       gl::QueryD3D11DeviceObjectFromANGLE();
   // DX11 protects the DComp surface atlas so this clear only affects pixels in
   // the update rect.
-  if (!ClearD3D11TextureToColor(draw_texture, SkColors::kTransparent)) {
+  if (!D3DImageBackingFactory::ClearTextureToColor(
+          d3d11_device.Get(), draw_texture.Get(), SkColors::kTransparent)) {
     return false;
   }
 
