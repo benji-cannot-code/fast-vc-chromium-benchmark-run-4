fbaa576a7f9cb40b22b6f1274c95a5f891e6ee86
Dustin J. Mitchell <djmitche@chromium.org>
2023-08-01 20:48:48 +0000

    Fix util tests for new JSON parser error messages
    The new (Rust) JSON parser gives different error messages, so the
    assertions in the tests must change.
    
    Bug: 1455145
    Change-Id: Ie20255695c5573e84a8f827c8e44a0a9255e1c05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4734240
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178058}

--- extensions/common/extension_l10n_util_unittest.cc
+++ extensions/common/extension_l10n_util_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/path_service.h"
+#include "base/rust_buildflags.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "extensions/common/constants.h"
@@ -105,10 +106,17 @@ TEST(ExtensionL10nUtil, ValidateLocalesWithErroneousLocalizations) {
                          errors::kLocalesInvalidLocale,
                          base::UTF16ToUTF8(de_messages_file.LossyDisplayName()),
                          "Variable $VAR$ used but not defined.")));
+#if BUILDFLAG(BUILD_RUST_JSON_READER)
+  EXPECT_THAT(error, testing::HasSubstr(ErrorUtils::FormatErrorMessage(
+                         errors::kLocalesInvalidLocale,
+                         base::UTF16ToUTF8(es_messages_file.LossyDisplayName()),
+                         "expected value at line 1 column 24")));
+#else   // BUILDFLAG(BUILD_RUST_JSON_READER)
   EXPECT_THAT(error, testing::HasSubstr(ErrorUtils::FormatErrorMessage(
                          errors::kLocalesInvalidLocale,
                          base::UTF16ToUTF8(es_messages_file.LossyDisplayName()),
                          "Line: 1, column: 24, Unexpected token.")));
+#endif  // BUILDFLAG(BUILD_RUST_JSON_READER)
   EXPECT_THAT(error, testing::HasSubstr(ErrorUtils::FormatErrorMessage(
                          errors::kLocalesInvalidLocale,
                          base::UTF16ToUTF8(fr_messages_file.LossyDisplayName()),
@@ -267,11 +275,19 @@ TEST(ExtensionL10nUtil, LoadMessageCatalogsBadJSONFormat) {
   std::string error;
   EXPECT_FALSE(extension_l10n_util::LoadMessageCatalogs(
       src_path, "en_US", GzippedMessagesPermission::kDisallow, &error));
+#if BUILDFLAG(BUILD_RUST_JSON_READER)
+  EXPECT_NE(std::string::npos,
+            error.find(ErrorUtils::FormatErrorMessage(
+                errors::kLocalesInvalidLocale,
+                base::UTF16ToUTF8(messages_file.LossyDisplayName()),
+                "EOF while parsing a value at line 1 column 9")));
+#else   // BUILDFLAG(BUILD_RUST_JSON_READER)
   EXPECT_NE(std::string::npos,
             error.find(ErrorUtils::FormatErrorMessage(
                 errors::kLocalesInvalidLocale,
                 base::UTF16ToUTF8(messages_file.LossyDisplayName()),
                 "Line: 1, column: 10,")));
+#endif  // BUILDFLAG(BUILD_RUST_JSON_READER)
 }
 
 TEST(ExtensionL10nUtil, LoadMessageCatalogsDuplicateKeys) {
