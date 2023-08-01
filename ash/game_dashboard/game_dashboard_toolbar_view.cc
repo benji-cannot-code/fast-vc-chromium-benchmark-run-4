1b0e646ef35d8ba98eff1c5d356c84d62d06522a
Prameet Shah <phshah@google.com>
2023-08-01 06:28:01 +0000

    gd: Show Record Game button in the toolbar when the feature is enabled.
    Game Dashboard toolbar shows the record game button only if
    `kFeatureManagementGameDashboardRecordGame` is enabled.
    
    Bug: b:273641250
    Test: Ran ash unit tests
    Change-Id: I159fbf330eef5c1059fd6f641a6ff830f2bbcaaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4710088
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177631}

--- ash/game_dashboard/game_dashboard_toolbar_view.cc
+++ ash/game_dashboard/game_dashboard_toolbar_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/capture_mode/capture_mode_controller.h"
+#include "ash/constants/ash_features.h"
 #include "ash/game_dashboard/game_dashboard_context.h"
 #include "ash/game_dashboard/game_dashboard_utils.h"
 #include "ash/public/cpp/arc_game_controls_flag.h"
@@ -108,15 +109,17 @@ void GameDashboardToolbarView::AddShortcutTiles() {
 
   MayAddGameControlsTile();
 
-  // TODO(b/273641250): Filter out record game button based on device info.
-  record_game_button_ = AddChildView(CreateIconButton(
-      base::BindRepeating(&GameDashboardToolbarView::OnRecordButtonPressed,
-                          base::Unretained(this)),
-      &kGdRecordGameIcon,
-      base::to_underlying(ToolbarViewId::kScreenRecordButton),
-      l10n_util::GetStringUTF16(
-          IDS_ASH_GAME_DASHBOARD_RECORD_GAME_TILE_BUTTON_TITLE),
-      /*is_togglable=*/false));
+  if (base::FeatureList::IsEnabled(
+          features::kFeatureManagementGameDashboardRecordGame)) {
+    record_game_button_ = AddChildView(CreateIconButton(
+        base::BindRepeating(&GameDashboardToolbarView::OnRecordButtonPressed,
+                            base::Unretained(this)),
+        &kGdRecordGameIcon,
+        base::to_underlying(ToolbarViewId::kScreenRecordButton),
+        l10n_util::GetStringUTF16(
+            IDS_ASH_GAME_DASHBOARD_RECORD_GAME_TILE_BUTTON_TITLE),
+        /*is_togglable=*/false));
+  }
 
   AddChildView(CreateIconButton(
       base::BindRepeating(&GameDashboardToolbarView::OnScreenshotButtonPressed,
