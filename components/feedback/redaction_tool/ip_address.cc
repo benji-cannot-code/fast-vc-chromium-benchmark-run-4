04a9ffc1fc6053be07ca0f9eb52a881f2877949a
Adam Rice <ricea@chromium.org>
2023-08-01 20:32:26 +0000

    Use InlinedVector in /components/feedback
    Replace usages of base::StackVector with absl::InlinedVector. This is
    not a pure rename, as the semantics are slightly different:
    
    1. Methods like push_back() which were accessed with the -> operator in
       StackVector are instead accessed with . with InlinedVector.
    
    See also discussion thread in cxx@chromium.org:
    https://groups.google.com/a/chromium.org/g/cxx/c/jTfqVfU-Ka0/m/caaal90NCgAJ
    
    This CL was uploaded by git cl split.
    
    R=zentaro@chromium.org
    
    Bug: 1465736
    Change-Id: Ibc06064a7e6edd83d9e6694c770624f9d07718db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707685
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178045}

--- components/feedback/redaction_tool/ip_address.cc
+++ components/feedback/redaction_tool/ip_address.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <climits>
 
 #include "base/check_op.h"
-#include "base/containers/stack_container.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/strcat.h"
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/feedback/redaction_tool/url_canon_ip.h"
 #include "components/feedback/redaction_tool/url_canon_stdstring.h"
+#include "third_party/abseil-cpp/absl/container/inlined_vector.h"
 
 namespace redaction_internal {
 namespace {
@@ -250,21 +250,21 @@ IPAddress ConvertIPv4ToIPv4MappedIPv6(const IPAddress& address) {
   DCHECK(address.IsIPv4());
   // IPv4-mapped addresses are formed by:
   // <80 bits of zeros>  + <16 bits of ones> + <32-bit IPv4 address>.
-  base::StackVector<uint8_t, 16> bytes;
-  bytes->insert(bytes->end(), std::begin(kIPv4MappedPrefix),
-                std::end(kIPv4MappedPrefix));
-  bytes->insert(bytes->end(), address.bytes().begin(), address.bytes().end());
-  return IPAddress(bytes->data(), bytes->size());
+  absl::InlinedVector<uint8_t, 16> bytes;
+  bytes.insert(bytes.end(), std::begin(kIPv4MappedPrefix),
+               std::end(kIPv4MappedPrefix));
+  bytes.insert(bytes.end(), address.bytes().begin(), address.bytes().end());
+  return IPAddress(bytes.data(), bytes.size());
 }
 
 IPAddress ConvertIPv4MappedIPv6ToIPv4(const IPAddress& address) {
   DCHECK(address.IsIPv4MappedIPv6());
 
-  base::StackVector<uint8_t, 16> bytes;
-  bytes->insert(bytes->end(),
-                address.bytes().begin() + std::size(kIPv4MappedPrefix),
-                address.bytes().end());
-  return IPAddress(bytes->data(), bytes->size());
+  absl::InlinedVector<uint8_t, 16> bytes;
+  bytes.insert(bytes.end(),
+               address.bytes().begin() + std::size(kIPv4MappedPrefix),
+               address.bytes().end());
+  return IPAddress(bytes.data(), bytes.size());
 }
 
 bool IPAddressMatchesPrefix(const IPAddress& ip_address,
