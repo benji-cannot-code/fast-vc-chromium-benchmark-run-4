d351056a40bc7485293b702403ae877dcd3927c9
Yiren Wang <yrw@chromium.org>
2023-08-01 22:56:46 +0000

    CrOS GMC V2: Fix colors for time scrubber
    New screenshots for CrOS updated media view:
    - Light mode: https://screenshot.googleplex.com/9uS54udVmW7wTQ7
    - Dark mode: https://screenshot.googleplex.com/3QDbcJzSCXzFVU9
    
    Bug: 1467980
    Change-Id: Ia84651430e09cb97a421fd911c65162b258561f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728318
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178121}

---
+++ components/media_message_center/notification_theme.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/media_message_center/notification_theme.h"
+
+namespace media_message_center {
+
+MediaColorTheme::MediaColorTheme() = default;
+
+MediaColorTheme::MediaColorTheme(const MediaColorTheme& other) = default;
+
+MediaColorTheme& MediaColorTheme::operator=(const MediaColorTheme&) = default;
+
+MediaColorTheme::~MediaColorTheme() = default;
+
+}  // namespace media_message_center
