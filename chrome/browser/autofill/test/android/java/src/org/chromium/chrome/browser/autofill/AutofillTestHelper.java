4d7cd31b7056f6850271f388c44ab15e0cd78dce
Vishwas Uppoor <vishwasuppoor@google.com>
2023-08-01 15:36:41 +0000

    [Metadata][Android] Show Capital One card icon for virtual cards even if metadata is disabled
    The Capital One virtual card icon helps distinguish the FPAN from
    virtual card. This is not shown on the bottom sheet if the card art flag
    is not enabled. It should be shown irrespective of the experiment.
    
    Show the Capital One virtual card icon for virtual cards on the bottom
    sheet if the card contains the icon URL.
    
    Bug: 1468701
    Change-Id: I4511e9c686f1e361ec399a391a51b377308af29d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4731254
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1177846}

--- chrome/browser/autofill/test/android/java/src/org/chromium/chrome/browser/autofill/AutofillTestHelper.java
+++ chrome/browser/autofill/test/android/java/src/org/chromium/chrome/browser/autofill/AutofillTestHelper.java
@@ -14,6 +14,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
 import org.chromium.components.autofill.VirtualCardEnrollmentState;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.url.GURL;
 
 import java.util.Calendar;
 import java.util.List;
@@ -323,7 +324,7 @@ public class AutofillTestHelper {
                 /* basicCardIssuerNetwork =*/network, /* issuerIconDrawableId= */ iconId,
                 /* billingAddressId= */ "",
                 /* serverId= */ "", /* instrumentId= */ 0, /* cardLabel= */ "", /* nickname= */ "",
-                /* cardArtUrl= */ null,
+                /* cardArtUrl= */ new GURL(""),
                 /* virtualCardEnrollmentState= */ VirtualCardEnrollmentState.ENROLLED,
                 /* productDescription= */ "",
                 /* cardNameForAutofillDisplay= */ cardNameForAutofillDisplay,
