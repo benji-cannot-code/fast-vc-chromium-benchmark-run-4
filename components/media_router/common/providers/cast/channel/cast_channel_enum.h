6b3e2f44c86223d73d77365280f4f214d24fe096
Prakhar Shrivastav <p.shri@samsung.com>
2023-08-01 11:19:46 +0000

    Use base::to_underlying instead of static_cast to underlying types
    After C++23 we should use the base method instead of directly typecasting.
    
    Bug: None
    Change-Id: I297f1903ac415509d7da3af84da6f465d65633fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722154
    Commit-Queue: Prakhar Shrivastav <p.shri@samsung.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177725}

--- components/media_router/common/providers/cast/channel/cast_channel_enum.h
+++ components/media_router/common/providers/cast/channel/cast_channel_enum.h
@@ -9,14 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <string>
 
+#include "base/types/cxx23_to_underlying.h"
+
 namespace cast_channel {
 
 // Helper function to convert scoped enums to their underlying type, for use
 // with ostreams.
 template <typename Enumeration>
-auto AsInteger(Enumeration const value) ->
-    typename std::underlying_type<Enumeration>::type {
-  return static_cast<typename std::underlying_type<Enumeration>::type>(value);
+auto AsInteger(Enumeration const value) {
+  return base::to_underlying(value);
 }
 
 enum class ReadyState {
