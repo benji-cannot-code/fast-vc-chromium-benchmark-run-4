8f297e64ca53a6d39668f901830589527747f4fc
Mirko Bonadei <mbonadei@chromium.org>
2023-08-01 10:35:15 +0000

    Switch to the new header for rtc::HasIPv6Enabled
    Bug: b/292167110
    Change-Id: I0c016f3447f26d1104b82d08581eb08012ff6b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721470
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177715}

--- third_party/blink/renderer/platform/peerconnection/fake_connection_factory_test.cc
+++ third_party/blink/renderer/platform/peerconnection/fake_connection_factory_test.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#include "third_party/webrtc/rtc_base/net_helpers.h"
+#include "third_party/webrtc/rtc_base/net_test_helpers.h"
 #include "third_party/webrtc_overrides/p2p/base/ice_connection.h"
 
 namespace {
