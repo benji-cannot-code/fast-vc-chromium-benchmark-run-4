bea8bb8e74c9114a46616fda8921589395e4a156
Michael Tang <tangm@microsoft.com>
2023-08-01 21:29:04 +0000

    gpu: win: Add feature to enable DComp debug counters and redraw regions
    Introduces --enable-features=DCompDebugVisualization to enable debug
    features on the DComp device and root surface.
    
    Change-Id: I2223cb2a98977c9c4bd88a88c98758da027ed504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4730732
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1178073}

--- ui/gl/dc_layer_tree.cc
+++ ui/gl/dc_layer_tree.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/direct_composition_child_surface_win.h"
 #include "ui/gl/direct_composition_support.h"
 #include "ui/gl/gl_angle_util_win.h"
+#include "ui/gl/gl_switches.h"
 #include "ui/gl/swap_chain_presenter.h"
 
 namespace gl {
@@ -107,6 +108,27 @@ bool DCLayerTree::Initialize(
 
   hr = dcomp_device_->CreateVisual(&dcomp_root_visual_);
   CHECK_EQ(hr, S_OK);
+
+  if (base::FeatureList::IsEnabled(features::kDCompDebugVisualization)) {
+    Microsoft::WRL::ComPtr<IDCompositionDeviceDebug> debug_device;
+    hr = dcomp_device_.As(&debug_device);
+    CHECK_EQ(hr, S_OK);
+    CHECK(debug_device);
+    DLOG(WARNING) << "DComp debug counters enabled, visible in the top right.";
+    DLOG(WARNING) << "  - left: The composition engine FPS, averaged over the "
+                     "last 60 composition frames";
+    DLOG(WARNING) << "  - right: The overall CPU usage of the composition "
+                     "thread, in milliseconds";
+    hr = debug_device->EnableDebugCounters();
+    CHECK_EQ(hr, S_OK);
+
+    Microsoft::WRL::ComPtr<IDCompositionVisualDebug> debug_visual;
+    hr = dcomp_root_visual_.As(&debug_visual);
+    CHECK_EQ(hr, S_OK);
+    CHECK(debug_visual);
+    hr = debug_visual->EnableRedrawRegions();
+    CHECK_EQ(hr, S_OK);
+  }
 
   dcomp_target_->SetRoot(dcomp_root_visual_.Get());
   // A visual inherits the interpolation mode of the parent visual by default.
