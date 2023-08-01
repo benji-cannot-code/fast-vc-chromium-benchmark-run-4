70e107ed53b6257c6a74fb2801ad4a26db22a0fd
Leonard Grey <lgrey@chromium.org>
2023-08-01 18:21:22 +0000

    Mac immersive: Remove thin controller
    In https://crbug.com/1369643 we learned that on systems with "camera
    wells" (aka notches), if no titlebar accessory has a fullScreenMinHeight > 0, this glitches something and makes it so when one or more *does*
    change its fullScreenMinHeight > 0, the contents are not rendered.
    
    In product terms, this means that if we're in immersive fullscreen with
    the top chrome hidden, and enable "Always Show Toolbar in Full Screen",
    we'll see a black bar rather than the topchrome on affected systems.
    
    The fix for this was to add an extra titlebar accessory controller, the
    "thin controller", the purpose of which was to ensure that we always
    have at least one accessory with non-zero min height. This controller
    was painted black to blend in with the menu bar, which is black in both
    light and dark modes.
    
    Unfortunately, the thin controller is still visible when topchrome is
    revealed and in light mode it doesn't blend in with anything (see bug).
    I tried setting it to clear in 9790cd830413443fdad633f5de8624bfff0c97d5
    but this caused other problems.
    
    This change reverts the above and removes the thin controller entirely.
    Instead, the main topchrome titlebar accessory is set to a min height of
    .5. In my testing on a M1 MacBook Pro (camera well system) and iMac Pro
    2017, this causes no visual artifacts, fixes the original bug, and has
    no effect on revealed topchrome.
    
    Bug: 1468415
    Change-Id: Idb5b9ad163234f668d400f38a6fd84b4d7e63d5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735613
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177952}

--- ui/views/cocoa/immersive_mode_controller_unittest.mm
+++ ui/views/cocoa/immersive_mode_controller_unittest.mm
@@ -122,7 +122,7 @@ TEST_F(CocoaImmersiveModeControllerTest, ImmersiveModeController) {
   auto immersive_mode_controller =
       std::make_unique<ImmersiveModeController>(browser(), overlay());
   immersive_mode_controller->Enable();
-  EXPECT_EQ(browser().titlebarAccessoryViewControllers.count, 2u);
+  EXPECT_EQ(browser().titlebarAccessoryViewControllers.count, 1u);
 }
 
 // Test that reveal locks work as expected.
@@ -135,10 +135,11 @@ TEST_F(CocoaImmersiveModeControllerTest, RevealLock) {
   // Autohide top chrome.
   immersive_mode_controller->UpdateToolbarVisibility(
       mojom::ToolbarVisibilityStyle::kAutohide);
-  EXPECT_EQ(
+  // Hidden height is > 0 as a workaround for https://crbug.com/1369643
+  EXPECT_LT(
       browser()
           .titlebarAccessoryViewControllers.firstObject.fullScreenMinHeight,
-      0);
+      1);
 
   // Grab 3 reveal locks and make sure that top chrome is displayed.
   EXPECT_EQ(immersive_mode_controller->reveal_lock_count(), 0);
@@ -163,10 +164,10 @@ TEST_F(CocoaImmersiveModeControllerTest, RevealLock) {
 
   // Let go of the final reveal lock and make sure top chrome is hidden.
   immersive_mode_controller->RevealUnlock();
-  EXPECT_EQ(
+  EXPECT_LT(
       browser()
           .titlebarAccessoryViewControllers.firstObject.fullScreenMinHeight,
-      0);
+      1);
 }
 
 // Test ImmersiveModeController titlebar frame KVO.
@@ -290,10 +291,10 @@ TEST_F(CocoaImmersiveModeControllerTest, Tabbed) {
                                                       tab_overlay());
   immersive_mode_controller->Enable();
 
-  EXPECT_EQ(browser().titlebarAccessoryViewControllers.count, 3u);
+  EXPECT_EQ(browser().titlebarAccessoryViewControllers.count, 2u);
   immersive_mode_controller->UpdateToolbarVisibility(
       mojom::ToolbarVisibilityStyle::kNone);
-  EXPECT_EQ(browser().titlebarAccessoryViewControllers.count, 2u);
+  EXPECT_EQ(browser().titlebarAccessoryViewControllers.count, 1u);
 }
 
 // Test ImmersiveModeTabbedController reveal lock tests.
