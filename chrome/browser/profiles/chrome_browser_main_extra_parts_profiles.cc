7fd17a34905dfd4b082ab0aa2fcda61881cf173d
Matt Reichhoff <mreichhoff@chromium.org>
2023-08-01 19:06:43 +0000

    Implement KeyedService + factory for PlusAddresses
    A separate factory would be added for ios; this commit is intended for
    the //chrome portion only.
    
    Change-Id: Iaa8335b5a9191c1d3acf20dd35f84d55cd86d686
    Bug: 1467623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735231
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177983}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -116,6 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/permissions/prediction_service_factory.h"
 #include "chrome/browser/persisted_state_db/session_proto_db_factory.h"
 #include "chrome/browser/plugins/plugin_prefs_factory.h"
+#include "chrome/browser/plus_addresses/plus_address_service_factory.h"
 #include "chrome/browser/policy/cloud/user_cloud_policy_invalidator_factory.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
 #include "chrome/browser/predictors/loading_predictor_factory.h"
@@ -875,6 +876,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   PluginInfoHostImpl::EnsureFactoryBuilt();
   PluginPrefsFactory::GetInstance();
 #endif
+  PlusAddressServiceFactory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS)
   policy::DlpDownloadObserverFactory::GetInstance();
   policy::DlpRulesManagerFactory::GetInstance();
--- components/plus_addresses/plus_address_service.h
+++ components/plus_addresses/plus_address_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <unordered_set>
 #include "base/time/time.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace plus_addresses {
@@ -20,11 +21,11 @@ struct PlusProfile {
 
 // An experimental class for filling plus addresses (asdf+123@some-domain.com).
 // Not intended for widespread use.
-class PlusAddressService {
+class PlusAddressService : public KeyedService {
  public:
   // Default constructor/destructor only, for now.
   PlusAddressService();
-  ~PlusAddressService();
+  ~PlusAddressService() override;
 
   // Returns `true` when plus addresses are supported. Currently requires only
   // that the `kPlusAddressesEnabled` base::Feature is enabled.
