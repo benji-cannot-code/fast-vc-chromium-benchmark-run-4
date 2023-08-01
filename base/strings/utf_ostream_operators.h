416ec11d0f85fc9dbfa51f4750eb3dbeb468cac0
Wei4 Wang <wei4.wang@intel.com>
2023-08-01 07:53:57 +0000

    Add <string> head file in utf_ostream_operators.h
    When built on macOS, the following error may occur: no type named
    'u16string' in namespace 'std'. This patch resolve this issue
    by adding <string> head file in utf_ostream_operators.h.
    
    Bug: none
    Change-Id: Iec3aa88ffce40fff66444189537c14a0bf19776b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4737391
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177654}

--- base/strings/utf_ostream_operators.h
+++ base/strings/utf_ostream_operators.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_STRINGS_UTF_OSTREAM_OPERATORS_H_
 
 #include <iosfwd>
+#include <string>
 #include <string_view>
 
 #include "base/base_export.h"
