998af38eedd9eed46bbc37d897d6f016837eeb9f
Colin Blundell <blundell@chromium.org>
2023-08-02 06:50:06 +0000

    [//gpu] Eliminate GLTextureImageBackingHelper class
    This class just contains an inner class and a static function; by the
    style guide, that's not a reason to have a class :).
    
    Change-Id: Ia58dacf3e4f97609999d94e4c2a915da531da01f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738155
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178270}

--- gpu/command_buffer/service/shared_image/gl_texture_image_backing_helper.h
+++ gpu/command_buffer/service/shared_image/gl_texture_image_backing_helper.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
+// Common utilities for various backings.
+
 // Sets GL state for readback.
 class ScopedPackState {
  public:
@@ -69,34 +71,30 @@ class ScopedUnpackState {
   absl::optional<gl::ScopedPixelStore> unpack_lsb_first_;
 };
 
-// Common helper functions for various backings.
-class GPU_GLES2_EXPORT GLTextureImageBackingHelper {
+// At destruction time, restore `target`'s binding as of construction time. If
+// `new_binding` is non-zero, then bind `target` to it at construction time.
+// TODO(crbug.com/1367187): Fold into gl::ScopedRestoreTexture.
+class ScopedRestoreTexture {
  public:
-  // At destriction time, restore `target`'s binding as of construction time. If
-  // `new_binding` is non-zero, then bind `target` to it at construction time.
-  // TODO(crbug.com/1367187): Fold into gl::ScopedRestoreTexture.
-  class ScopedRestoreTexture {
-   public:
-    ScopedRestoreTexture(gl::GLApi* api, GLenum target, GLuint new_binding = 0);
+  ScopedRestoreTexture(gl::GLApi* api, GLenum target, GLuint new_binding = 0);
 
-    ScopedRestoreTexture(const ScopedRestoreTexture&) = delete;
-    ScopedRestoreTexture& operator=(const ScopedRestoreTexture&) = delete;
+  ScopedRestoreTexture(const ScopedRestoreTexture&) = delete;
+  ScopedRestoreTexture& operator=(const ScopedRestoreTexture&) = delete;
 
-    ~ScopedRestoreTexture();
+  ~ScopedRestoreTexture();
 
-   private:
-    raw_ptr<gl::GLApi> api_;
-    GLenum target_;
-    GLuint old_binding_ = 0;
-  };
-
-  // Creates a new GL texture and returns GL texture ID.
-  static GLuint MakeTextureAndSetParameters(
-      GLenum target,
-      bool framebuffer_attachment_angle,
-      scoped_refptr<gles2::TexturePassthrough>* passthrough_texture,
-      raw_ptr<gles2::Texture>* texture);
+ private:
+  raw_ptr<gl::GLApi> api_;
+  GLenum target_;
+  GLuint old_binding_ = 0;
 };
+
+// Creates a new GL texture and returns GL texture ID.
+GLuint MakeTextureAndSetParameters(
+    GLenum target,
+    bool framebuffer_attachment_angle,
+    scoped_refptr<gles2::TexturePassthrough>* passthrough_texture,
+    raw_ptr<gles2::Texture>* texture);
 
 }  // namespace gpu
 
