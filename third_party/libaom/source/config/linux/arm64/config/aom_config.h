713c77a4a027f9af69705068dbb2210478946c84
Wan-Teh Chang <wtc@google.com>
2023-08-02 19:29:25 +0000

    Roll src/third_party/libaom/source/libaom/ 84b015d90..b1fbd2abc (82 commits)
    https://aomedia.googlesource.com/aom.git/+log/84b015d90d57..b1fbd2abc97e
    
    $ git log 84b015d90..b1fbd2abc --date=short --no-merges --format='%ad %ae %s'
    2023-07-27 jonathan.wright Refactor I8MM convolve_2d_sr_horiz_12tap_neon implementation
    2023-07-27 jonathan.wright Refactor DotProd convolve_2d_sr_horiz_12tap_neon implementation
    2023-07-27 jonathan.wright Refactor Armv8.0 convolve_2d_sr_horiz_12tap_neon implementation
    2023-07-27 jonathan.wright Refactor convolve_2d_sr_vert_12tap_neon
    2023-07-27 jonathan.wright Refactor convolve_2d_sr_vert_8tap_neon
    2023-07-27 jonathan.wright Refactor convolve_2d_sr_vert_6tap_neon
    2023-07-27 jonathan.wright Remove w = 2 store paths in Neon convolve_2d_sr horiz pass
    2023-07-25 jzern test/*.cc: use bool literals
    2023-07-25 jzern test/*.cc: use nullptr
    2023-07-25 marpan rtc: RC fix for sudden high motion
    (...)
    2023-07-18 jzern av1_inv_txfm_neon: remove unneeded bd variable
    2023-07-18 jzern av1_inv_txfm_neon: simplify clip+add operation
    2023-07-18 jzern pickrst_neon: add missing cast / quiet warning w/cl.exe
    2023-07-18 jzern encodetxb_neon: work around VS arm64_neon.h macro bug
    2023-07-13 wtc Use 64-bit multiplication in does_level_match()
    2023-07-03 george.steed Specialize av1_warp_affine_neon horizontal filter for p_width==4
    2023-06-29 marpan rtc: Add simulcast mode to svc encoder.c
    2023-07-14 rachelbarker Merge loops in disflow
    2023-07-14 rachelbarker Disable debugging code in disflow SSE4.1
    2023-07-17 george.steed Enable Arm SVE run-time feature detection via getauxval
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    
    Change third_party/libaom/BUILD.gn to include sources for neon_dotprod
    and neon_i8mm. Change third_party/libaom/cmake_update.sh to disable
    neon_dotprod and neon_i8mm for the linux/arm64 config (used by Fuchsia
    only).
    
    Change-Id: Id6ccc1c9f2d32024894790d7b4926a669bcfccfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4739959
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178606}

--- third_party/libaom/source/config/linux/arm64/config/aom_config.h
+++ third_party/libaom/source/config/linux/arm64/config/aom_config.h
@@ -79,8 +79,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_FEXCEPT 1
 #define HAVE_MMX 0
 #define HAVE_NEON 1
-#define HAVE_NEON_DOTPROD 1
-#define HAVE_NEON_I8MM 1
+#define HAVE_NEON_DOTPROD 0
+#define HAVE_NEON_I8MM 0
 #define HAVE_PTHREAD_H 1
 #define HAVE_SSE 0
 #define HAVE_SSE2 0
@@ -88,6 +88,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_SSE4_1 0
 #define HAVE_SSE4_2 0
 #define HAVE_SSSE3 0
+#define HAVE_SVE 0
 #define HAVE_UNISTD_H 1
 #define HAVE_VSX 0
 #define HAVE_WXWIDGETS 0
