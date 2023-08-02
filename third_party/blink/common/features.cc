f05fbbc6acbb54541b1270d02238ea3f50247ba0
Minoru Chikamune <chikamune@chromium.org>
2023-08-02 10:39:56 +0000

    [LCPP] Limit maximum LCP element locator string length
    This CL adds maximum LCP element locator string length restriction that
    can be sent to the browser process.
    
    Bug: 1419756
    Change-Id: I5597a3362b22fc9078261472461c006776c50f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4739136
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178352}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1028,6 +1028,9 @@ BASE_FEATURE(kLCPCriticalPathPredictor,
              "LCPCriticalPathPredictor",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+const base::FeatureParam<int> kLCPCriticalPathPredictorMaxElementLocatorLength{
+    &kLCPCriticalPathPredictor, "lcpp_max_element_locator_length", 1024};
+
 // Enables reporting as LCP of the time the first frame of a video was painted.
 BASE_FEATURE(kLCPVideoFirstFrame,
              "LCPVideoFirstFrame",
