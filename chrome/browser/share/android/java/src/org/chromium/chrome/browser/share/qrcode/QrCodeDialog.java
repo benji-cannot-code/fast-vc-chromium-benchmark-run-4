2e83401e54573712ace4f5f8453781328ea469f6
Charles Hager <clhager@google.com>
2023-08-02 16:01:47 +0000

    Hide scan option for QR code share option
    Screenshot: https://screenshot.googleplex.com/4TpmekU7dAy6TRb.png
    
    Bug: b/265736095
    Change-Id: I2f668472fccceeaeac2e8d2b9a6a17c7973c45f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694021
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178451}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/QrCodeDialog.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/QrCodeDialog.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.share.qrcode;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
@@ -12,6 +13,7 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
@@ -33,7 +35,7 @@ public class QrCodeDialog extends DialogFragment {
     public static String URL_KEY = "url_key";
 
     private WindowAndroid mWindowAndroid;
-    private ArrayList<QrCodeDialogTab> mTabs;
+    protected ArrayList<QrCodeDialogTab> mTabs;
     private TabLayoutPageListener mTabLayoutPageListener;
 
     /**
@@ -53,7 +55,9 @@ public class QrCodeDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         initTabs();
-        return new FullscreenAlertDialog.Builder(getActivity()).setView(getDialogView()).create();
+        return new FullscreenAlertDialog.Builder(getActivity())
+                .setView(getDialogView(getActivity()))
+                .create();
     }
 
     @Override
@@ -99,19 +103,26 @@ public class QrCodeDialog extends DialogFragment {
         }
     }
 
-    private View getDialogView() {
-        View dialogView = getActivity().getLayoutInflater().inflate(R.layout.qrcode_dialog, null);
+    @VisibleForTesting
+    protected View getDialogView(Activity activity) {
+        View dialogView = activity.getLayoutInflater().inflate(R.layout.qrcode_dialog, null);
         ChromeImageButton closeButton = dialogView.findViewById(R.id.close_button);
         closeButton.setOnClickListener(v -> dismiss());
+        TabLayout tabLayout = dialogView.findViewById(R.id.tab_layout);
 
         // Setup page adapter and tab layout.
         ArrayList<View> pages = new ArrayList<View>();
-        for (QrCodeDialogTab tab : mTabs) {
-            pages.add(tab.getView());
+
+        for (int index = 0; index < mTabs.size(); index++) {
+            QrCodeDialogTab tab = mTabs.get(index);
+            if (tab.isEnabled()) {
+                pages.add(tab.getView());
+            } else {
+                tabLayout.removeTabAt(index);
+            }
         }
         QrCodePageAdapter pageAdapter = new QrCodePageAdapter(pages);
 
-        TabLayout tabLayout = dialogView.findViewById(R.id.tab_layout);
         ViewPager viewPager = dialogView.findViewById(R.id.qrcode_view_pager);
         viewPager.setAdapter(pageAdapter);
 
