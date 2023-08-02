bd9168a102f0ee0914323215b08ff3cfcd47abb8
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-08-02 18:35:25 +0000

    Ensure SidePanelCoordinator handles when SidePanel is deleted
    When browser view is being deconstructed, SidePanel is deleted first
    and SidePanelCoordinator follows it.
    
    This CL handles the cases where a client requests an action(Show,
    Toggle, etc) to SidePanelCoordinator after the SidePanel is deleted.
    
    Change-Id: I39599da7a3866174dd01e979ecc8bb82e85a9e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740314
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178571}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator.cc
@@ -421,6 +421,7 @@ void SidePanelCoordinator::Show(
   }
 
   if (GetContentContainerView() == nullptr) {
+    CHECK(browser_view_->unified_side_panel());
     InitializeSidePanel();
     opened_timestamp_ = base::TimeTicks::Now();
     SidePanelUtil::RecordSidePanelOpen(open_trigger);
@@ -473,8 +474,12 @@ void SidePanelCoordinator::Show(
 }
 
 views::View* SidePanelCoordinator::GetContentContainerView() const {
-  return browser_view_->unified_side_panel()->GetViewByID(
-      kSidePanelContentContainerViewId);
+  if (const SidePanel* side_panel = browser_view_->unified_side_panel()) {
+    return browser_view_->unified_side_panel()->GetViewByID(
+        kSidePanelContentContainerViewId);
+  }
+
+  return nullptr;
 }
 
 SidePanelEntry* SidePanelCoordinator::GetEntryForKey(
