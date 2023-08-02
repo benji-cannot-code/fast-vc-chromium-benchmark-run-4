85b7cc34064057b0d90b3644de3253f4a34c8cdc
Peter Kasting <pkasting@chromium.org>
2023-08-02 22:58:16 +0000

    Fix several issues with menu metrics and layout.
    * Updating the empty menu items/metrics did not invalidate layout. This
      could lead to flaky effects like empty menus showing up blank when
      opened a second time.
    * Menu items drew "new" badges immediately after the title, but reserved
      space for them in the minor text region. This could lead to overlap
      for items with both.
    * Menu dimensions did not reserve space for the minor icon, leading to
      potential overlap.
    * Menu dimensions did not reserve space in the minor text region for
      submenu arrows when !MenuConfig::reserve_dedicated_arrow_column,
      leading to potential overlap.
    
    This also attempts to reorder MenuItemView::CalculateDimensions() a bit
    for clarity.
    
    Bug: 1457972
    Change-Id: I2a7b982300700673a5d144181385f6e846a283a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742963
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178711}

--- ui/views/controls/menu/submenu_view.cc
+++ ui/views/controls/menu/submenu_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <numeric>
 #include <set>
+#include <tuple>
 #include <utility>
 
 #include "base/compiler_specific.h"
@@ -97,6 +98,11 @@ PrefixSelector* SubmenuView::GetPrefixSelector() {
 void SubmenuView::UpdateMenuPartSizes() {
   const MenuConfig& config = MenuConfig::instance();
 
+  const auto get_metrics = [&] {
+    return std::tie(icon_area_width_, label_start_, trailing_padding_);
+  };
+  const auto old_metrics = get_metrics();
+
   trailing_padding_ =
       config.item_horizontal_padding + config.item_horizontal_border_padding;
   const auto& menu_items = GetMenuItems();
@@ -153,6 +159,10 @@ void SubmenuView::UpdateMenuPartSizes() {
 
   if (config.icons_in_label) {
     icon_area_width_ = max_icon_width;
+  }
+
+  if (get_metrics() != old_metrics) {
+    InvalidateLayout();
   }
 }
 
