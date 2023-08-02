fcd2bd167657da9de404b7a9f63a3b015755b5e6
Dominik Röttsches <drott@chromium.org>
2023-08-02 12:44:12 +0000

    Revert "dlp: Pass correct policy in PauseParams"
    This reverts commit 032cf03b0e64019d64cdf5e32751bc2f37765936.
    
    Reason for revert: Suspected to fail on MSAN, MemorySanitizer: use-of-uninitialized-value in file_manager::io_task::ProgressStatus::HasPolicyError() [...] policy::FilesPolicyNotificationManager::ShowFilesPolicyNotification compare 
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/37557/overview
    
    Original change's description:
    > dlp: Pass correct policy in PauseParams
    >
    > Fixes a bug in `FPNM::PauseIOTask()` method where the `warning_reason`
    > parameter was ignored and tasks always paused with `DLP` policy reason.
    >
    > R=chromeos-dlp@google.com
    >
    > Fixed: b:294051125
    > Change-Id: I542b78f176fd202e12b5498a51671a5d59d8b0f4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738658
    > Reviewed-by: Daniel Brinkers <brinky@google.com>
    > Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    > Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1178326}
    
    Change-Id: I0219275ba24c8d67429fabb7edcd4826e0d47a57
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742360
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1178377}

--- chrome/browser/ash/policy/dlp/files_policy_notification_manager_unittest.cc
+++ chrome/browser/ash/policy/dlp/files_policy_notification_manager_unittest.cc
@@ -383,7 +383,7 @@ TEST_P(FilesPolicyNotificationManagerDlpAndConnectorsTest,
   EXPECT_TRUE(fpnm_->HasIOTask(task_id));
 
   file_manager::io_task::PauseParams pause_params;
-  pause_params.policy_params.emplace(GetPolicy(), /*warning_files_count=*/1,
+  pause_params.policy_params.emplace(Policy::kDlp, /*warning_files_count=*/1,
                                      src_file_path.BaseName().value());
 
   // Task is paused.
@@ -501,7 +501,8 @@ TEST_P(FilesPolicyNotificationManagerDlpAndConnectorsTest, WarningCancelled) {
 
   file_manager::io_task::PauseParams pause_params;
   pause_params.policy_params = file_manager::io_task::PolicyPauseParams(
-      GetPolicy(), /*warning_files_count=*/1, src_file_path.BaseName().value());
+      Policy::kDlp, /*warning_files_count=*/1,
+      src_file_path.BaseName().value());
 
   // Task is paused.
   EXPECT_CALL(
@@ -559,7 +560,8 @@ TEST_P(FilesPolicyNotificationManagerDlpAndConnectorsTest, WarningResumed) {
 
   file_manager::io_task::PauseParams pause_params;
   pause_params.policy_params = file_manager::io_task::PolicyPauseParams(
-      GetPolicy(), /*warning_files_count=*/1, src_file_path.BaseName().value());
+      Policy::kDlp, /*warning_files_count=*/1,
+      src_file_path.BaseName().value());
 
   // Task is paused.
   EXPECT_CALL(
@@ -640,7 +642,8 @@ TEST_P(FilesPolicyNotificationManagerDlpAndConnectorsTest,
 
   file_manager::io_task::PauseParams pause_params;
   pause_params.policy_params = file_manager::io_task::PolicyPauseParams(
-      GetPolicy(), /*warning_files_count=*/1, src_file_path.BaseName().value());
+      Policy::kDlp, /*warning_files_count=*/1,
+      src_file_path.BaseName().value());
 
   // Task is paused.
   EXPECT_CALL(
