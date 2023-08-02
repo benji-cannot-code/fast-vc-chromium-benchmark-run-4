893727b2adc976675d6f8b33d5c4326d00ddffee
Yue Zhang <yuezhanggg@chromium.org>
2023-08-02 23:17:31 +0000

    [CodeHealth] Fix a dangling pointer in UkmObserver
    On the one hand, UkmObserver holds a raw pointer reference to
    UKMRecorderImpl (ukm_recorder_); on the other hand, UkmDatabaseClient
    , the parent class of UkmObserver, is a static singleton. These two
    setups jointly lead to the fact that the raw pointer ukm_recorder_ is
    never properly released.
    
    This CL fixes this issue by explicitly setting UkmObserver to null in
    UkmDatabaseClient when its lifecycle should end, so that we can
    trigger the destruction process.
    
    Bug: 1462061
    Change-Id: Id803ef07de26f571a1a424407adf19571b508112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743080
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178722}

--- chrome/browser/segmentation_platform/ukm_database_client.cc
+++ chrome/browser/segmentation_platform/ukm_database_client.cc
@@ -70,6 +70,7 @@ void UkmDatabaseClient::PostMessageLoopRun() {
     // Some of the content browser implementations do not invoke
     // PreProfileInit().
     ukm_observer_->StopObserving();
+    ukm_observer_ = nullptr;
   }
 }
 
