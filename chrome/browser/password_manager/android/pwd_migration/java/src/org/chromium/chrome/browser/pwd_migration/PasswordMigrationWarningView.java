b194bc2742aa101f3461e7850c79cac76ef34443
Ivana Žužić <izuzic@google.com>
2023-08-02 12:37:19 +0000

    [UPMLocalPwd] Check that the Fragment is set for the right View
    This is a speculative fix for the crash that happens when the
    FragmentManager can't find the FragmentContainerView that should
    hold the PasswordMigrationWarningIntroFragment.
    The hypothesis is that the FragmentContainerView seems to be visible
    because it's inflated, but it was not yet added to the View hierarchy
    so the FragmentManager of the Activity can't yet find it at the moment
    when setFragment() is called.
    
    Bug: 1468464
    Change-Id: Ided4b41c3250060ee95b6fcdecd10335fc9268d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4741852
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178375}

--- chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PasswordMigrationWarningView.java
+++ chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PasswordMigrationWarningView.java
@@ -73,11 +73,8 @@ public class PasswordMigrationWarningView implements BottomSheetContent {
 
         @Override
         public void onSheetOpened(@StateChangeReason int reason) {
-            if (mBottomSheetController.getCurrentSheetContent()
-                    != PasswordMigrationWarningView.this) {
-                return;
-            }
-            if (mScreenType != ScreenType.NONE && getContentView().isShown()) {
+            if (mBottomSheetController.getCurrentSheetContent() == PasswordMigrationWarningView.this
+                    && mScreenType != ScreenType.NONE && getContentView().isShown()) {
                 setFragment();
             }
         }
@@ -119,7 +116,8 @@ public class PasswordMigrationWarningView implements BottomSheetContent {
 
     void setScreen(@ScreenType int screenType) {
         mScreenType = screenType;
-        if (getContentView().isShown()) {
+        if (mBottomSheetController.getCurrentSheetContent() == PasswordMigrationWarningView.this
+                && getContentView().isShown()) {
             setFragment();
         }
         // Makes sure the sheet is fully expanded.
