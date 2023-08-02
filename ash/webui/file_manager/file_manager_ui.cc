c0a4c2f2fc3473fbde9ef78410012a09f080413b
Dominik Röttsches <drott@chromium.org>
2023-08-02 13:07:20 +0000

    Revert "dlp: Show desktop notif. when all files apps close"
    This reverts commit 101c7b25fe135948e411418c4f706790465e0778.
    
    Reason for revert: Suspected to trigger/be involved in MSAN failures in https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/37557/overview.
    
    Original change's description:
    > dlp: Show desktop notif. when all files apps close
    >
    > Ensures that any opened DLP panel items are moved to a desktop (system)
    > notification, in case all Files app windows close:
    > * Changes `IOTask::IsPaused()` to include tasks paused by DLP warning
    >   * This then works because `IOTaskController::ProgressPausedTasks()`
    >   force-sends a status update and the desktop notification is shown.
    > * For error notifications, i.e. when the task is blocked by DLP, we
    > don't track the task anymore in the controller, but we do have all the
    > info in `FilesPolicyNotificationManager`
    >   * Adds a function in FPNM to loop over tracked IOTasks and show the
    >   notifications for those with errors.
    >   * Calls this function in the `FileManagerUI` dtor, when the last
    >   instance is being destroyed.
    >
    > The only case not covered is the timeout notification, as when this is
    > visible, the `FPNM` also doesn't track the task anymore, but it could
    > be fixed. However, it requires more changes in `FPNM` so will be done
    > in a separate CL if needed.
    >
    > R=lucmult@chromium.org,chromeos-dlp@google.com
    >
    > Bug: b:290903069
    > Change-Id: Ia66cbbaa12a8911235dd02eafd4b4bedf4ccc5b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4711968
    > Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    > Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Daniel Brinkers <brinky@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1178312}
    
    Bug: b:290903069
    Change-Id: Ia25f8d942b105b0d8fc117239ca43c25f01a6bd9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742499
    Reviewed-by: Daniel Brinkers <brinky@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178381}

--- ash/webui/file_manager/file_manager_ui.cc
+++ ash/webui/file_manager/file_manager_ui.cc
@@ -149,9 +149,6 @@ FileManagerUI::~FileManagerUI() {
   if (!instance_count_) {
     delegate_->ProgressPausedTasks();
     delegate_->ShouldPollDriveHostedPinStates(false);
-    // There might be some tasks blocked by policy that already completed, but
-    // still have a notification so notify FPNM to show them if necessary.
-    delegate_->ShowPolicyNotifications();
   }
 }
 
