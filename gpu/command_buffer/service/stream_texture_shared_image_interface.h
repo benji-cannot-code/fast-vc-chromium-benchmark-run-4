b9728bf8957af4174d7e14d7c4001756cfbb5ee8
Colin Blundell <blundell@chromium.org>
2023-08-02 20:34:52 +0000

    [GPU] s/SharedImageVideo/AndroidVideoImageBacking/g
    Various references to the stale name exist in comments in the codebase.
    
    Change-Id: Ifa7b1a467915826fe466ec374f96817a8c1a5a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742601
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178632}

--- gpu/command_buffer/service/stream_texture_shared_image_interface.h
+++ gpu/command_buffer/service/stream_texture_shared_image_interface.h
@@ -21,7 +21,7 @@ namespace gpu {
 class TextureOwner;
 class TextureBase;
 
-// This class lets SharedImageVideo draw video frames.
+// This class lets AndroidVideoImageBacking draw video frames.
 class GPU_GLES2_EXPORT StreamTextureSharedImageInterface
     : public base::RefCounted<StreamTextureSharedImageInterface> {
  public:
