a4440a335c768af692762ab47e3bd2277c4f65b2
Thomas Lukaszewicz <tluk@chromium.org>
2023-08-02 17:12:54 +0000

    [desks] Fix OsUrlHandler SWA failing to restore with desks
    Currently SWA's support AppLaunchInfo override_url. For some SWAs
    such as the OsUrlHandler this parameter is necessary in ensuring it
    is started / restored into the correct state.
    
    This CL updates desk code to ensure that the override_url is captured
    from launched SWAs (if present) when data is serialized to disk
    and is parsed successfully when this data is read as part of
    restoring a desk.
    
    Bug: 1466634, 1368285
    Change-Id: I20d3cbb0a9824fef1ede8f99c79dc6f4d5318c6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721596
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178506}

--- components/app_restore/app_restore_data.cc
+++ components/app_restore/app_restore_data.cc
@@ -575,6 +575,7 @@ std::unique_ptr<AppLaunchInfo> AppRestoreData::GetAppLaunchInfo(
   app_launch_info->app_type_browser = app_type_browser;
   app_launch_info->app_name = app_name;
   app_launch_info->tab_group_infos = tab_group_infos;
+  app_launch_info->override_url = override_url;
   return app_launch_info;
 }
 
--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -1611,6 +1611,7 @@ VISIT_PROTO_FIELDS(const sync_pb::WorkspaceDeskSpecifics::App& proto) {
   VISIT_ENUM(disposition);
   VISIT(app_name);
   VISIT(title);
+  VISIT(override_url);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::WorkspaceDeskSpecifics::AppOneOf& proto) {
