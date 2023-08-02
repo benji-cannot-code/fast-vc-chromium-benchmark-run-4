b989091fc1c6281bec74283e7c996e710cc15d33
Colin Kincaid <ckincaid@chromium.org>
2023-08-02 18:41:49 +0000

    multipaste: Update accessibility text for menu item delete button
    This CL updates the clipboard history menu item's delete button a11y
    text to include the item's text, so that someone using a screen reader
    knows immediately which item the button would delete.
    
    Bug: b/293941276
    Change-Id: I0c492fba766f4702bba6bbe6f7d9ffbb1dcbcc30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735835
    Auto-Submit: Colin Kincaid <ckincaid@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178580}

--- ash/clipboard/views/clipboard_history_delete_button.cc
+++ ash/clipboard/views/clipboard_history_delete_button.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/clipboard/views/clipboard_history_delete_button.h"
 
+#include <string>
+
 #include "ash/clipboard/clipboard_history_util.h"
 #include "ash/clipboard/views/clipboard_history_item_view.h"
 #include "ash/style/style_util.h"
@@ -19,7 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 ClipboardHistoryDeleteButton::ClipboardHistoryDeleteButton(
-    ClipboardHistoryItemView* listener)
+    ClipboardHistoryItemView* listener,
+    const std::u16string& item_text)
     : CloseButton(
           base::BindRepeating(
               [](ClipboardHistoryItemView* item_view, const ui::Event& event) {
@@ -32,8 +35,8 @@ ClipboardHistoryDeleteButton::ClipboardHistoryDeleteButton(
           cros_tokens::kCrosSysSecondary),
       listener_(listener) {
   SetID(clipboard_history_util::kDeleteButtonViewID);
-  SetAccessibleName(
-      l10n_util::GetStringUTF16(IDS_CLIPBOARD_HISTORY_DELETE_BUTTON));
+  SetAccessibleName(l10n_util::GetStringFUTF16(
+      IDS_CLIPBOARD_HISTORY_DELETE_ITEM_TEXT, item_text));
   SetTooltipText(l10n_util::GetStringUTF16(
       IDS_CLIPBOARD_HISTORY_DELETE_BUTTON_HOVER_TEXT));
   SetVisible(false);
--- chrome/browser/ui/ash/clipboard_history_browsertest.cc
+++ chrome/browser/ui/ash/clipboard_history_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <iterator>
 #include <list>
 #include <memory>
 #include <tuple>
@@ -182,6 +183,16 @@ const std::list<ash::ClipboardHistoryItem>& GetClipboardItems() {
   return GetClipboardHistoryController()->history()->GetItems();
 }
 
+// Returns the clipboard history item at the specified `index`, which is assumed
+// to exist in the clipboard history list.
+const ash::ClipboardHistoryItem& GetClipboardItemAt(size_t index) {
+  const auto& items = GetClipboardItems();
+  CHECK_LT(index, items.size());
+  auto items_iter = items.begin();
+  std::advance(items_iter, index);
+  return *items_iter;
+}
+
 gfx::Rect GetClipboardHistoryMenuBoundsInScreen() {
   return GetClipboardHistoryController()->GetMenuBoundsInScreenForTest();
 }
@@ -271,6 +282,10 @@ class ClipboardHistoryBrowserTest : public ash::LoginManagerTest {
         delete_button->GetTooltipText(delete_button->bounds().CenterPoint()),
         l10n_util::GetStringUTF16(
             IDS_CLIPBOARD_HISTORY_DELETE_BUTTON_HOVER_TEXT));
+    EXPECT_EQ(
+        delete_button->GetAccessibleName(),
+        l10n_util::GetStringFUTF16(IDS_CLIPBOARD_HISTORY_DELETE_ITEM_TEXT,
+                                   GetClipboardItemAt(index).display_text()));
     GetEventGenerator()->ClickLeftButton();
   }
 
