3ea4ae99257b8464b7da4686004d28858c065623
Xiaodan Zhu <zxdan@chromium.org>
2023-08-02 05:09:51 +0000

    fix view shadow offset
    Sometimes the ViewShadow may have an incorrect offset as shown in:
    https://screenshot.googleplex.com/4o83gD9j5PbewZd
    
    This is because when setting bounds to a view's layer, it will move
    shadow's layer to the view's origin. After that, if the view layer
    target bounds changed, ViewShadow will recalculate the shadow layer
    bounds. However, if the view layer target bounds are not changed,
    for example when the view is reparented, the wrong offset will not
    get fixed.
    
    This cl fixed the issue by changing the name of
    OnLayerTargetBoundsChanged to OnViewLayerBoundsSet and invoke it
    when the view layer bounds is set. Making the ViewShadow override
    this method to set shadow's content bounds with current layer bounds.
    
    To guarantee the shadow layer bounds change with content bounds
    accordingly, we add a attribute `last_layer_bounds_` to ui::Shadow to
    cache the shadow's layer bounds after setting the content bounds. When
    setting the same content bounds, if the shadow's layer bounds is
    modified, we will reset the layer bounds.
    
    Test: ShadowTest.ResetLayerBoundsBySettingSameContentBounds
    Test: ViewShadowTest.NoShiftWhenReparentViewLayer
    Bug: b:286568605
    Change-Id: Ibfeddddbd3d075d34d7966e5f9012da12a243c9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722487
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178247}

--- ui/views/view_unittest.cc
+++ ui/views/view_unittest.cc
@@ -4526,7 +4526,7 @@ class TestingLayerViewObserver : public ViewObserver {
 
  private:
   // ViewObserver:
-  void OnLayerTargetBoundsChanged(View* view) override {
+  void OnViewLayerBoundsSet(View* view) override {
     last_layer_bounds_ = view->layer()->bounds();
   }
 
