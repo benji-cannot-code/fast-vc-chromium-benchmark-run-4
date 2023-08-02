a2ac1aecc0ed74cdd8c3c2c2699c580c2672ef57
Muyao Xu <muyaoxu@google.com>
2023-08-02 17:45:54 +0000

    [Sheriff] Disable flaky PasswordManagerSyncTest on Mac
    Bug: 1469411
    Change-Id: If1cda21437e608de21793e3990543ffee3f5f101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738984
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Owners-Override: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178538}

--- chrome/browser/sync/test/integration/password_manager_sync_test.cc
+++ chrome/browser/sync/test/integration/password_manager_sync_test.cc
@@ -770,8 +770,16 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest,
                   MatchesLoginAndRealm("user", "pass", GetPSLOrigin())));
 }
 
+// TODO(crbug.com/1469411): Re-enable this test on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_DontOfferToSavePrimaryAccountCredential \
+  DISABLED_DontOfferToSavePrimaryAccountCredential
+#else
+#define MAYBE_DontOfferToSavePrimaryAccountCredential \
+  DontOfferToSavePrimaryAccountCredential
+#endif
 IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest,
-                       DontOfferToSavePrimaryAccountCredential) {
+                       MAYBE_DontOfferToSavePrimaryAccountCredential) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
 
   SetupSyncTransportWithPasswordAccountStorage();
@@ -791,8 +799,16 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest,
   EXPECT_FALSE(bubble_observer.IsSavePromptAvailable());
 }
 
+// TODO(crbug.com/1469411): Re-enable this test on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_OfferToSaveNonPrimaryAccountCredential \
+  DISABLED_OfferToSaveNonPrimaryAccountCredential
+#else
+#define MAYBE_OfferToSaveNonPrimaryAccountCredential \
+  OfferToSaveNonPrimaryAccountCredential
+#endif
 IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest,
-                       OfferToSaveNonPrimaryAccountCredential) {
+                       MAYBE_OfferToSaveNonPrimaryAccountCredential) {
   // Disable signin interception, because it suppresses the password bubble.
   // See PasswordManagerBrowserTestWithSigninInterception for tests with
   // interception enabled.
@@ -818,8 +834,16 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest,
   EXPECT_TRUE(bubble_observer.IsSavePromptAvailable());
 }
 
+// TODO(crbug.com/1469411): Re-enable this test on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_OfferToUpdatePrimaryAccountCredential \
+  DISABLED_OfferToUpdatePrimaryAccountCredential
+#else
+#define MAYBE_OfferToUpdatePrimaryAccountCredential \
+  OfferToUpdatePrimaryAccountCredential
+#endif
 IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest,
-                       OfferToUpdatePrimaryAccountCredential) {
+                       MAYBE_OfferToUpdatePrimaryAccountCredential) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
 
   // The password for the primary account is already saved.
