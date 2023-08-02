b81c4184aa4bb4ab09584e0d0a04cd226b694440
Qihui Zhao <qihuizhao@google.com>
2023-08-02 15:59:42 +0000

    Ignore offer-to-save locally and server credit card should both add strike database.
    Bug: 1430568
    Change-Id: Id970e2c4cf55b6795f565036cfbe0eec7917dcab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4726561
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178448}

--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views.cc
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views.cc
@@ -44,8 +44,6 @@ SaveCardBubbleViews::SaveCardBubbleViews(views::View* anchor_view,
   DCHECK(controller);
   SetButtonLabel(ui::DIALOG_BUTTON_OK, controller->GetAcceptButtonText());
   SetButtonLabel(ui::DIALOG_BUTTON_CANCEL, controller->GetDeclineButtonText());
-  SetCancelCallback(base::BindOnce(&SaveCardBubbleViews::OnDialogCancelled,
-                                   base::Unretained(this)));
   SetAcceptCallback(base::BindOnce(&SaveCardBubbleViews::OnDialogAccepted,
                                    base::Unretained(this)));
 
@@ -77,12 +75,6 @@ void SaveCardBubbleViews::OnDialogAccepted() {
   // TODO(https://crbug.com/1046793): Maybe delete this.
   if (controller_)
     controller_->OnSaveButton({});
-}
-
-void SaveCardBubbleViews::OnDialogCancelled() {
-  // TODO(https://crbug.com/1046793): Maybe delete this.
-  if (controller_)
-    controller_->OnCancelButton();
 }
 
 void SaveCardBubbleViews::AddedToWidget() {
--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views.h
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views.h
@@ -71,7 +71,6 @@ class SaveCardBubbleViews : public AutofillBubbleBase,
   void Init() override;
 
   void OnDialogAccepted();
-  void OnDialogCancelled();
 
   ~SaveCardBubbleViews() override;
 
