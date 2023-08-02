e9f496af4a9b48b2682f9f329294eb61c4968261
Joel Hockey <joelhockey@chromium.org>
2023-08-02 00:21:53 +0000

    Refactor FileType.getTypeFromName() into base file
    We will soon be changing ImageLoader to have a smaller
    imageLoaderPrivate API rather than fileManagerPrivate. ImageLoader
    imports FileType.getTypeFromName() from file_type.js which imports files
    which reference fileManagerPrivate.
    
    Putting this function into file_types_base.js with no deps on
    fileManagerPrivate means that the rollup process will not pull in any
    code that refers to fileManagerPrivate.
    
    Bug: b/293534917
    Change-Id: Ib3f216dd41c6bffe9b621d32795b10d96ae19bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4736831
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178160}

--- ui/file_manager/image_loader/image_request_task.js
+++ ui/file_manager/image_loader/image_request_task.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {FileType} from 'chrome://file-manager/common/js/file_type.js';
+import {getFileTypeForName} from 'chrome://file-manager/common/js/file_types_base.js';
 import {assert, assertInstanceof} from 'chrome://resources/ash/common/assert.js';
 
 import {ImageCache} from './cache.js';
@@ -399,7 +399,7 @@ ImageRequestTask.prototype.downloadThumbnail_ = function(onSuccess, onFailure) {
     return;
   }
 
-  const fileType = FileType.getTypeForName(this.request_.url);
+  const fileType = getFileTypeForName(this.request_.url);
 
   // Load RAW image source thumbnail.
   if (fileType.type === 'raw') {
