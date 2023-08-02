1e535303faffb7391e765640eb2d32fb6503776e
Jennifer Serrano <jennserrano@google.com>
2023-08-02 20:15:11 +0000

    [QuickStart] Add handshake metrics
    As part of adding FIDO Assertion metrics, handshake metrics are being added. In future a CL GAIA transfer metrics will also be added.
    
    See go/cros-quick-start-metrics.
    
    TEST=modified Connection and TargetDeviceConnectionBrokerImpl unittests.
    
    Bug: b/279614284
    Change-Id: I5bf4b16bd5d6764127e4e2f457f1e2759aba446a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684889
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Joe Antonetti <joeantonetti@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178626}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/target_device_connection_broker_impl_unittest.cc
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/target_device_connection_broker_impl_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/fake_connection.h"
@@ -378,6 +379,7 @@ class TargetDeviceConnectionBrokerImplTest : public testing::Test {
   FakeConnectionLifecycleListener connection_lifecycle_listener_;
   raw_ptr<FakeConnection::Factory, ExperimentalAsh> connection_factory_ =
       nullptr;
+  base::HistogramTester histogram_tester_;
 
   std::unique_ptr<FakeQuickStartDecoder> fake_quick_start_decoder_ =
       std::make_unique<FakeQuickStartDecoder>();
@@ -686,6 +688,7 @@ TEST_F(TargetDeviceConnectionBrokerImplTest, Handshake_Success) {
 
   connection()->HandleHandshakeResult(/*success=*/true);
   EXPECT_TRUE(connection_lifecycle_listener_.connection_authenticated_);
+  histogram_tester_.ExpectBucketCount("QuickStart.HandshakeStarted", true, 1);
 }
 
 TEST_F(TargetDeviceConnectionBrokerImplTest, Handshake_Failed) {
@@ -708,6 +711,7 @@ TEST_F(TargetDeviceConnectionBrokerImplTest, Handshake_Failed) {
 
   connection()->HandleHandshakeResult(/*success=*/false);
   EXPECT_FALSE(connection_lifecycle_listener_.connection_authenticated_);
+  histogram_tester_.ExpectBucketCount("QuickStart.HandshakeStarted", true, 1);
 }
 
 TEST_F(TargetDeviceConnectionBrokerImplTest,
@@ -728,6 +732,7 @@ TEST_F(TargetDeviceConnectionBrokerImplTest,
   ASSERT_TRUE(connection());
   EXPECT_TRUE(connection_lifecycle_listener_.connection_authenticated_);
   EXPECT_NE(connection_lifecycle_listener_.authenticated_connection_, nullptr);
+  histogram_tester_.ExpectBucketCount("QuickStart.HandshakeStarted", false, 1);
 }
 
 TEST_F(TargetDeviceConnectionBrokerImplTest,
--- chromeos/ash/components/quick_start/quick_start_metrics.cc
+++ chromeos/ash/components/quick_start/quick_start_metrics.cc
@@ -45,6 +45,13 @@ constexpr const char kMessageReceivedDesiredMessageTypeName[] =
     "QuickStart.MessageReceived.DesiredMessageType";
 constexpr const char kMessageSentMessageTypeName[] =
     "QuickStart.MessageSent.MessageType";
+constexpr const char kHandshakeResultSucceededName[] =
+    "QuickStart.HandshakeResult.Succeeded";
+constexpr const char kHandshakeResultDurationName[] =
+    "QuickStart.HandshakeResult.Duration";
+constexpr const char kHandshakeResultErrorCodeName[] =
+    "QuickStart.HandshakeResult.ErrorCode";
+constexpr const char kHandshakeStartedName[] = "QuickStart.HandshakeStarted";
 
 std::string MapMessageTypeToMetric(MessageType message_type) {
   switch (message_type) {
@@ -119,15 +126,22 @@ void RecordNearbyConnectionsAdvertisementEnded(
   // TODO(279614071): Add advertising metrics.
 }
 
-void RecordHandshakeStarted(int32_t session_id) {
-  // TODO(279614284): Add FIDO assertion metrics.
+void RecordHandshakeStarted(bool handshake_started) {
+  base::UmaHistogramBoolean(kHandshakeStartedName, handshake_started);
 }
 
-void RecordHandshakeResult(int32_t session_id,
-                           bool succeded,
-                           int duration,
+void RecordHandshakeResult(bool succeeded,
+                           base::TimeDelta duration,
                            absl::optional<HandshakeErrorCode> error_code) {
-  // TODO(279614284): Add FIDO assertion metrics.
+  if (succeeded) {
+    CHECK(!error_code.has_value());
+  } else {
+    CHECK(error_code.has_value());
+    base::UmaHistogramEnumeration(kHandshakeResultErrorCodeName,
+                                  error_code.value());
+  }
+  base::UmaHistogramBoolean(kHandshakeResultSucceededName, succeeded);
+  base::UmaHistogramTimes(kHandshakeResultDurationName, duration);
 }
 
 void RecordMessageSent(MessageType message_type) {
