efd1a057fb385424e4b2accb017674b17d96f52a
Lei Zhang <thestig@chromium.org>
2023-08-02 04:35:34 +0000

    Forward declare in Blink's wtf/allocator/partitions.h
    Since partitions.h is in 20% of all translation units, avoid expensive
    includes when possible. Then do IWYU to fix the build.
    
    Change-Id: I599a9f462abe360cf31824d9b8e762e7ce054c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735193
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178239}

--- third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.h"
 
+#include "base/notreached.h"
+
 namespace blink {
 
 GPUDeviceLostInfo::GPUDeviceLostInfo(const WGPUDeviceLostReason reason,
