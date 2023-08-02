3b9e56dd62411d5a94217953b4fab7092852c002
Rahul Yadav <rahul.yadav@samsung.com>
2023-08-02 12:00:05 +0000

    Use base::to_underlying in chrome/*
    After C++23 we should use base::to_underlying instead of static_cast
    to underlying types.
    
    Bug: None
    Change-Id: Ie91855108e104d5896e0211af29387f6e68fe1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4741215
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178368}

--- chrome/updater/util/util.h
+++ chrome/updater/util/util.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/types/cxx23_to_underlying.h"
 #include "build/build_config.h"
 #include "chrome/updater/tag.h"
 #include "chrome/updater/updater_scope.h"
@@ -47,7 +48,7 @@ template <typename T>
 std::ostream& operator<<(
     typename std::enable_if<std::is_enum<T>::value, std::ostream>::type& stream,
     const T& e) {
-  return stream << static_cast<typename std::underlying_type<T>::type>(e);
+  return stream << base::to_underlying(e);
 }
 
 namespace tagging {
