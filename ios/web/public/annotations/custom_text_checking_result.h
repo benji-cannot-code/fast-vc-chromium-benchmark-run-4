3c8ce4599b8502c0e2f6c9141817c79d455ca717
David Jean <djean@chromium.org>
2023-08-02 10:09:28 +0000

    [ios] Extend NSTextCheckingResult with TC custom types
    Bug: 1468905
    Change-Id: Ibf4c329915aa2f24cb6a5261743c712dbcdf0aa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4732977
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178346}

---
+++ ios/web/public/annotations/custom_text_checking_result.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_ANNOTATIONS_CUSTOM_TEXT_CHECKING_RESULT_H_
+#define IOS_WEB_PUBLIC_ANNOTATIONS_CUSTOM_TEXT_CHECKING_RESULT_H_
+
+#import <Foundation/Foundation.h>
+
+// Custom NSTextCheckingResult types.
+uint64_t const TCTextCheckingTypeParcelTracking = 1ULL << 32;
+uint64_t const TCTextCheckingTypeMeasurement = 1ULL << 33;
+
+// Custom NSTextCheckingResult class adding TextClassifier custom types.
+@interface CustomTextCheckingResult : NSTextCheckingResult
+
+@property(readonly, copy) NSMeasurement* measurement;
+@property(readonly) int carrier;
+@property(readonly, copy) NSString* carrierNumber;
+
+/* Methods for creating instances of the various types of results. */
++ (NSTextCheckingResult*)measurementCheckingResultWithRange:(NSRange)range
+                                                measurement:
+                                                    (NSMeasurement*)measurement;
++ (NSTextCheckingResult*)parcelCheckingResultWithRange:(NSRange)range
+                                               carrier:(int)carrier
+                                         carrierNumber:(NSString*)carrierNumber;
+@end
+
+#endif  // IOS_WEB_PUBLIC_ANNOTATIONS_CUSTOM_TEXT_CHECKING_RESULT_H_
