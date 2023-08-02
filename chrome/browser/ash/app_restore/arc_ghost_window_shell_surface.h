377f73426376af429b705d9c59cb5561d57df3e5
Kazuki Takise <takise@chromium.org>
2023-08-02 15:51:02 +0000

    Revert "arc: Implement scale factor request"
    This reverts commit 7c1787d488a73f708c5056b0aa32569f3f9106aa.
    
    Reason for revert: Broke arc.StandardizedMouseHover on some P devices
    
    Original change's description:
    > arc: Implement scale factor request
    >
    > Partly analogous to Lacros change: crrev.com/c/4034899.
    >
    > With this CL, ClientControlledShellSurface switches to
    > scale factor request model.
    >
    > - This CL removes `scale_`, `pending_scale_`, GetScale(), and
    >  CommitPendingScale() defined in ClientControlledShellSurface,
    >  which contributes to reduce duplication of similar logic.
    >
    > - As ARC R+ requests bounds in pixels, Exo converts them to DP, but
    >  as the scale factor requested for these bounds-related requests
    >  are effective in the next commit, ClientControlledShellSurface
    >  needs access to the pending scale in ShellSurfaceHost to do the
    >  proper conversion. (This assumes that ARC sends the next scale
    >  factor before bounds requests.)
    >
    > Bug: b/290565343
    > Bug: b/276985752
    > Test: Manually test multi-display with ARC P/R/T
    > Change-Id: If59c08ec635e0af96d8db9d2fc69bda03e6e6293
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680052
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Kazuki Takise <takise@chromium.org>
    > Auto-Submit: Kazuki Takise <takise@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1177071}
    
    Bug: b/290565343
    Bug: b/276985752
    Change-Id: Ibdae3d580f0ec0b5abe716120c76d1ab81886102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740423
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178433}

--- chrome/browser/ash/app_restore/arc_ghost_window_shell_surface.h
+++ chrome/browser/ash/app_restore/arc_ghost_window_shell_surface.h
@@ -53,6 +53,7 @@ class ArcGhostWindowShellSurface : public exo::ClientControlledShellSurface {
  private:
   ArcGhostWindowShellSurface(std::unique_ptr<exo::Surface> surface,
                              int container,
+                             double scale_factor,
                              const std::string& application_id);
 
   void InitContentOverlay(const std::string& app_id,
