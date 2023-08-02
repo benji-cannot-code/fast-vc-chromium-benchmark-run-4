a55c2b51572631905e6b15bdc2648ffa65c00238
Xiaoling Bao <xiaolingbao@chromium.org>
2023-08-02 23:37:29 +0000

    Make test app install take a version argument (preparing for app version rollback test).
    Bug: 1464354
    Change-Id: I23409aba0ea8886ce857d821a3a4233f87030f93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743624
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178732}

--- chrome/updater/test/integration_tests_impl.h
+++ chrome/updater/test/integration_tests_impl.h
@@ -201,7 +201,9 @@ void ExpectAppVersion(UpdaterScope scope,
                       const std::string& app_id,
                       const base::Version& version);
 
-void RegisterApp(UpdaterScope scope, const std::string& app_id);
+void RegisterApp(UpdaterScope scope,
+                 const std::string& app_id,
+                 const base::Version& version);
 
 [[nodiscard]] bool WaitForUpdaterExit(UpdaterScope scope);
 
@@ -292,7 +294,9 @@ void ExpectLastChecked(UpdaterScope scope);
 
 void ExpectLastStarted(UpdaterScope scope);
 
-void InstallApp(UpdaterScope scope, const std::string& app_id);
+void InstallApp(UpdaterScope scope,
+                const std::string& app_id,
+                const base::Version& version);
 
 void UninstallApp(UpdaterScope scope, const std::string& app_id);
 
