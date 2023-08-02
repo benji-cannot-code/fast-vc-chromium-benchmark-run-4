931618565e3e0cde75971acc360b0b45d234f264
Nicolás Peña <npm@chromium.org>
2023-08-02 20:03:58 +0000

    [FedCM] Tentatively fix flakiness in fedcm-iframe test
    The cancelHelper is not scoped accurately. Additionally, use a WeakPtr
    for the RFH in case a test accidentally invokes the function after the
    RFH is destroyed, to avoid UAF.
    
    Bug: 1404951
    Change-Id: I67c2259692b90a936cfeb3e268b839e1912879af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742921
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178618}

--- content/web_test/browser/web_test_fedcm_manager.cc
+++ content/web_test/browser/web_test_fedcm_manager.cc
@@ -5,22 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/web_test/browser/web_test_fedcm_manager.h"
 
+#include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/webid/federated_auth_request_impl.h"
 #include "content/browser/webid/federated_auth_request_page_data.h"
 #include "content/public/browser/identity_request_dialog_controller.h"
-#include "content/public/browser/page.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace content {
 
 WebTestFedCmManager::WebTestFedCmManager(RenderFrameHost* render_frame_host)
-    : render_frame_host_(render_frame_host) {}
+    : render_frame_host_(
+          static_cast<RenderFrameHostImpl*>(render_frame_host)->GetWeakPtr()) {}
 
 WebTestFedCmManager::~WebTestFedCmManager() = default;
 
 void WebTestFedCmManager::GetFedCmDialogTitle(
     blink::test::mojom::FederatedAuthRequestAutomation::
         GetFedCmDialogTitleCallback callback) {
+  if (!render_frame_host_) {
+    std::move(callback).Run(absl::nullopt);
+    return;
+  }
   FederatedAuthRequestPageData* page_data =
       PageUserData<FederatedAuthRequestPageData>::GetForPage(
           render_frame_host_->GetPage());
@@ -46,6 +51,10 @@ void WebTestFedCmManager::GetFedCmDialogTitle(
 void WebTestFedCmManager::SelectFedCmAccount(
     uint32_t account_index,
     SelectFedCmAccountCallback callback) {
+  if (!render_frame_host_) {
+    std::move(callback).Run(false);
+    return;
+  }
   FederatedAuthRequestPageData* page_data =
       PageUserData<FederatedAuthRequestPageData>::GetForPage(
           render_frame_host_->GetPage());
