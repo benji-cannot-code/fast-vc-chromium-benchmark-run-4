960d1f2c32cf5895bca9dc8e46dbcb8ea9620bca
Joe Mason <joenotcharles@google.com>
2023-08-02 17:41:30 +0000

    Allow discarded PageNode's to have type kUnknown
    Replaces CheckPageNodeTypeForBug1468730() with a production method,
    CheckPageState(), that asserts on a combination of page type and
    lifecycle state.
    
    R=anthonyvd
    
    Bug: 1468730
    Change-Id: I520b3277c1bdc5ecc6815d66b0cefce7e33e9750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743203
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178532}

--- chrome/browser/performance_manager/metrics/page_timeline_monitor.h
+++ chrome/browser/performance_manager/metrics/page_timeline_monitor.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PERFORMANCE_MANAGER_METRICS_PAGE_TIMELINE_MONITOR_H_
 #define CHROME_BROWSER_PERFORMANCE_MANAGER_METRICS_PAGE_TIMELINE_MONITOR_H_
 
+#include <map>
+#include <memory>
+
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -120,6 +123,10 @@ class PageTimelineMonitor : public PageNode::ObserverDefaultImpl,
   bool ShouldCollectSlice() const;
 
   void SetShouldCollectSliceCallbackForTesting(base::RepeatingCallback<bool()>);
+
+  // CHECK's that `page_node` and `info` are in the right state to be
+  // mapped to each other in `page_node_info_map_`.
+  void CheckPageState(const PageNode* page_node, const PageNodeInfo& info);
 
   // Monotonically increasing counters for tabs and slices.
   int slice_id_counter_;
