e4172cd2c57b3faf5c5896df0f96b6915f8597a4
Russ Hamilton <behamilton@google.com>
2023-08-02 01:40:27 +0000

    Add support for String valued chrome://flags
    String valued flags share storage with origin list flags to reduce
    complexity and avoid excessive changes to ChromeOS flag support.
    
    This feature is needed to support the Protected Audiences Bidding and
    Auction API's User Consented Debugging flow.
    
    Bug: 1467576
    Change-Id: Ic58f3d9192e83d90b67d30cc213aad25ef7c5db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705987
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178190}

--- components/flags_ui/resources/flags_browser_proxy.ts
+++ components/flags_ui/resources/flags_browser_proxy.ts
@@ -19,6 +19,7 @@ export interface Feature {
   is_default: boolean;
   supported_platforms: string[];
   origin_list_value?: string;
+  string_value?: string;
   options?: Array<{
     internal_name: string,
     description: string,
@@ -50,6 +51,7 @@ export interface FlagsBrowserProxy {
   enableExperimentalFeature(internalName: string, enable: boolean): void;
   selectExperimentalFeature(internalName: string, index: number): void;
   setOriginListFlag(internalName: string, value: string): void;
+  setStringFlag(internalName: string, value: string): void;
 }
 
 export class FlagsBrowserProxyImpl implements FlagsBrowserProxy {
@@ -84,6 +86,10 @@ export class FlagsBrowserProxyImpl implements FlagsBrowserProxy {
 
   setOriginListFlag(internalName: string, value: string) {
     chrome.send('setOriginListFlag', [internalName, value]);
+  }
+
+  setStringFlag(internalName: string, value: string) {
+    chrome.send('setStringFlag', [internalName, value]);
   }
 
   static getInstance(): FlagsBrowserProxy {
