41b2de0dd17d8ec16aa9150669dc0a23cca38d1c
Aiden Chiavatti <chiav@chromium.org>
2023-08-02 18:41:42 +0000

    Update annotations for autofill
    Add missing fields for the two network annotations in
    `autofill_download_manager.cc`. Replace deprecated AutoFillEnabled
    policy reference. Remove from safelist since these annotations are now
    complete.
    
    Bug: b:292111175
    Change-Id: I642d7ff7eeae55af9fd8a79ad6d7d4c9444d6ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735976
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Aiden Chiavatti <chiav@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178579}

--- components/autofill/core/browser/autofill_download_manager.cc
+++ components/autofill/core/browser/autofill_download_manager.cc
@@ -203,6 +203,15 @@ net::NetworkTrafficAnnotationTag GetNetworkTrafficAnnotation(
             "Hashed descriptions of the form and its fields. User data is not "
             "sent."
           destination: GOOGLE_OWNED_SERVICE
+          internal {
+            contacts {
+              owners: "//components/autofill/OWNERS"
+            }
+          }
+          user_data {
+            type: NONE
+          }
+          last_reviewed: "2023-07-31"
         }
         policy {
           cookies_allowed: NO
@@ -211,9 +220,15 @@ net::NetworkTrafficAnnotationTag GetNetworkTrafficAnnotation(
             "fill out web forms in a single click.' in Chromium's settings "
             "under 'Passwords and forms'. The feature is enabled by default."
           chrome_policy {
-            AutoFillEnabled {
+            AutofillCreditCardEnabled {
                 policy_options {mode: MANDATORY}
-                AutoFillEnabled: false
+                AutofillCreditCardEnabled: false
+            }
+          }
+          chrome_policy {
+            AutofillAddressEnabled {
+                policy_options {mode: MANDATORY}
+                AutofillAddressEnabled: false
             }
           }
         })");
@@ -237,6 +252,15 @@ net::NetworkTrafficAnnotationTag GetNetworkTrafficAnnotation(
           "data given to each field, if recognized from the user's "
           "profile(s). User data is not sent."
         destination: GOOGLE_OWNED_SERVICE
+        internal {
+          contacts {
+            owners: "//components/autofill/OWNERS"
+          }
+        }
+        user_data {
+          type: NONE
+        }
+        last_reviewed: "2023-07-31"
       }
       policy {
         cookies_allowed: NO
@@ -245,9 +269,15 @@ net::NetworkTrafficAnnotationTag GetNetworkTrafficAnnotation(
           "fill out web forms in a single click.' in Chromium's settings "
           "under 'Passwords and forms'. The feature is enabled by default."
         chrome_policy {
-          AutoFillEnabled {
+          AutofillCreditCardEnabled {
               policy_options {mode: MANDATORY}
-              AutoFillEnabled: false
+              AutofillCreditCardEnabled: false
+          }
+        }
+        chrome_policy {
+          AutofillAddressEnabled {
+              policy_options {mode: MANDATORY}
+              AutofillAddressEnabled: false
           }
         }
       })");
