40074e0bcf6580ad81b3021cd1031446ec328c80
Samar <samarchehade@google.com>
2023-08-02 07:28:43 +0000

    [Signin][Android] Add an accessible label to the account switcher.
    Fixed: 1449091
    Change-Id: Ibb1ce874a26ba6ba1833580782fefe353120db6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738657
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178275}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/ExistingAccountRowViewBinder.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/ExistingAccountRowViewBinder.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import org.chromium.chrome.browser.signin.services.DisplayableProfileData;
 import org.chromium.chrome.browser.ui.signin.R;
+import org.chromium.chrome.browser.ui.signin.SigninUtils;
 import org.chromium.chrome.browser.ui.signin.account_picker.AccountPickerProperties.ExistingAccountRowProperties;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -85,5 +86,7 @@ public class ExistingAccountRowViewBinder implements ViewBinder<PropertyModel, V
         accountImage.setImageDrawable(profileData.getImage());
         setAccountTextPrimary(profileData, view);
         setAccountTextSecondary(profileData, view);
+        view.setContentDescription(
+                SigninUtils.getChooseAccountLabel(view.getContext(), profileData));
     }
 }
