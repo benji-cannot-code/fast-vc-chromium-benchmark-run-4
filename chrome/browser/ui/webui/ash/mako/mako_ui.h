4f77deafbe95c8a239b1a1baf86022531661dbca
John Palmer <jopalmer@chromium.org>
2023-08-02 00:22:21 +0000

    Mako UI as a bubble
    Bug:b:291513201
    
    Change-Id: Ic1ca6ca578d1cfa513fb0cfff622f1aaac6eb684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4663790
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178161}

--- chrome/browser/ui/webui/ash/mako/mako_ui.h
+++ chrome/browser/ui/webui/ash/mako/mako_ui.h
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/input_method/mojom/editor.mojom.h"
 #include "content/public/browser/webui_config.h"
-#include "ui/webui/mojo_web_ui_controller.h"
+#include "ui/webui/untrusted_bubble_web_ui_controller.h"
+
+class Profile;
 
 namespace ash {
 
@@ -26,8 +28,9 @@ class MakoUntrustedUIConfig : public content::WebUIConfig {
 };
 
 // The WebUI for chrome://mako
-class MakoUntrustedUI : public ui::MojoWebUIController {
+class MakoUntrustedUI : public ui::UntrustedBubbleWebUIController {
  public:
+  static void Show(Profile* profile);
   explicit MakoUntrustedUI(content::WebUI* web_ui);
   ~MakoUntrustedUI() override;
 
--- chrome/browser/ui/webui/ash/mako/url_constants.cc
+++ chrome/browser/ui/webui/ash/mako/url_constants.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 const char kChromeUIMakoHost[] = "mako";
+const char kChromeUIOrcaURL[] = "chrome-untrusted://mako/orca.html";
 const char kChromeUIMakoURL[] = "chrome-untrusted://mako";
 
 }  // namespace ash
