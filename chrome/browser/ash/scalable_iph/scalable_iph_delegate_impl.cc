f28e80ec4e1316b3c37da5a0fa9f98cb0dd2b84c
Angela Xiao <angelaxiao@chromium.org>
2023-08-02 00:11:54 +0000

    Scalable Notifiers: update launching for settings CTAs
    Update the CTAs related to opening a settings subpage to launch using
    SettingsWindowManager to avoid opening via subpage urls. Using
    SettingsWindowManager will remove the need to directly specify/handle
    url specifics for the settings subpage paths.
    
    This fixes the printer and phone hub CTA crash issue on specific glinux
    builds. The crash was resulting from a settings subpage path check.
    
    Bug: b/293956407
    Change-Id: Id1bc0c09c3c139e04941310d853bbad1dc5d0842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740652
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178155}

--- chrome/browser/ash/scalable_iph/scalable_iph_delegate_impl.cc
+++ chrome/browser/ash/scalable_iph/scalable_iph_delegate_impl.cc
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
+#include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
+#include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/components/scalable_iph/buildflags.h"
@@ -92,9 +94,6 @@ const base::flat_map<ActionType, std::string>& GetActionTypeURLs() {
            {ActionType::kOpenGoogleDocs,
             "https://docs.google.com/document/?usp=installed_webapp/"},
            {ActionType::kOpenGooglePhotos, "https://photos.google.com/"},
-           {ActionType::kOpenSettingsPrinter,
-            "chrome://os-settings/cupsPrinters/"},
-           {ActionType::kOpenPhoneHub, "chrome://os-settings/multidevice/"},
            {ActionType::kOpenYouTube, "https://www.youtube.com/"}});
   return *action_type_urls;
 }
@@ -410,14 +409,13 @@ void ScalableIphDelegateImpl::PerformActionForScalableIph(
       break;
     }
     case ActionType::kOpenSettingsPrinter: {
-      OpenUrlForProfile(
-          profile_,
-          GURL(GetActionTypeURLs().at(ActionType::kOpenSettingsPrinter)));
+      chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
+          profile_, chromeos::settings::mojom::kPrintingDetailsSubpagePath);
       break;
     }
     case ActionType::kOpenPhoneHub: {
-      OpenUrlForProfile(
-          profile_, GURL(GetActionTypeURLs().at(ActionType::kOpenPhoneHub)));
+      chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
+          profile_, chromeos::settings::mojom::kMultiDeviceSectionPath);
       break;
     }
     case ActionType::kOpenYouTube: {
