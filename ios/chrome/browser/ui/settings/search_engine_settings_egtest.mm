c8cc2868ace064dba2319cf89743c5775b551a47
Vidhan Jain <vidhanj@google.com>
2023-08-02 13:04:20 +0000

    [iOS][Gardener] Disable flaky egtest testChangeSearchEngine
    Bug: 1469573
    Change-Id: Ic8c5f3857213a501c6dcb5e203bff1ea1e766ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738004
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Owners-Override: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178379}

--- ios/chrome/browser/ui/settings/search_engine_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/search_engine_settings_egtest.mm
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "base/ios/ios_util.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "ios/chrome/browser/ui/settings/settings_app_interface.h"
@@ -115,6 +116,11 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // Tests that when changing the default search engine, the URL used for the
 // search is updated.
 - (void)testChangeSearchEngine {
+  // TODO(crbug.com/1469573): Test flaky on iOS 17.
+  if (!base::ios::IsRunningOnIOS17OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Flaky on iOS 17.");
+  }
+
   self.testServer->RegisterRequestHandler(base::BindRepeating(&SearchResponse));
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
 
