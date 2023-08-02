5dd39b4ea4ce9e656170624e2c64bddfd3498e0e
Ziad Youssef <ziadyoussef@chromium.org>
2023-08-02 09:35:36 +0000

    Add PRESUBMIT for WebView histograms allowlist
    The added PRESUBMIT ensures that all histograms in the allowlist is
    valid. It should run for every change to either the allowlist or xml
    files which define histograms' names.
    
    This PRESUBMIT takes about 8 seconds to complete.
    
    Test: git cl presubmit --upload
    
    Change-Id: Icc62ec367c279681f3ff7d1d80bd3986d3c09126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727688
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178336}

---
+++ android_webview/java/res/raw/PRESUBMIT.py
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Presubmit tests for android_webview/java/res/raw"""
+
+import os
+import sys
+
+
+def CheckChangeOnUpload(input_api, output_api):
+    results = []
+    results.extend(_CheckHistogramsAllowlist(input_api, output_api))
+    return results
+
+
+def _CheckHistogramsAllowlist(input_api, output_api):
+    """Checks that histograms_allowlist.txt contains valid histograms."""
+    sys.path.append(input_api.PresubmitLocalPath())
+    from histograms_allowlist_check import HISTOGRAMS_ALLOWLIST_FILENAME
+    from histograms_allowlist_check import CheckWebViewHistogramsAllowlist
+
+    histograms_allowlist_filter = lambda f: f.LocalPath().endswith(
+        HISTOGRAMS_ALLOWLIST_FILENAME)
+    if not input_api.AffectedFiles(file_filter=histograms_allowlist_filter):
+        return []
+
+    # src_path should point to chromium/src
+    src_path = os.path.join(input_api.PresubmitLocalPath(), '..', '..', '..',
+                            '..')
+    return CheckWebViewHistogramsAllowlist(src_path, output_api)
