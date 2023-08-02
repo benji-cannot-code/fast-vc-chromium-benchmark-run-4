fba250558d8118ebb6ee2f06022f5ea12d529200
Camillo Bruni <cbruni@chromium.org>
2023-08-02 16:24:54 +0000

    [v8] Update deprecated v8::MicrotaskScope constructors
    The new version needs an explicit context or microtask queue.
    
    Change-Id: Ifd2b152395963d980e63e8d7b159ea9d8c5588b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714027
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178475}

--- chromecast/renderer/cast_demo_bindings.cc
+++ chromecast/renderer/cast_demo_bindings.cc
@@ -169,10 +169,10 @@ void CastDemoBindings::OnGetStoreId(v8::Global<v8::Promise::Resolver> resolver,
                                     v8::Global<v8::Context> original_context,
                                     const std::string& store_id) {
   v8::Isolate* isolate = blink::MainThreadIsolate();
-  v8::MicrotasksScope microtasks_scope(
-      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::HandleScope handle_scope(isolate);
   v8::Local<v8::Context> context = original_context.Get(isolate);
+  v8::MicrotasksScope microtasks_scope(
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Context::Scope context_scope(context);
 
   resolver.Get(isolate)
@@ -212,10 +212,10 @@ void CastDemoBindings::OnGetDefaultVolumeLevel(
     v8::Global<v8::Context> original_context,
     float level) {
   v8::Isolate* isolate = blink::MainThreadIsolate();
-  v8::MicrotasksScope microtasks_scope(
-      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::HandleScope handle_scope(isolate);
   v8::Local<v8::Context> context = original_context.Get(isolate);
+  v8::MicrotasksScope microtasks_scope(
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Context::Scope context_scope(context);
 
   resolver.Get(isolate)
@@ -253,10 +253,10 @@ void CastDemoBindings::OnGetAvailableWifiNetworks(
     v8::Global<v8::Context> original_context,
     base::Value network_list) {
   v8::Isolate* isolate = blink::MainThreadIsolate();
-  v8::MicrotasksScope microtasks_scope(
-      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::HandleScope handle_scope(isolate);
   v8::Local<v8::Context> context = original_context.Get(isolate);
+  v8::MicrotasksScope microtasks_scope(
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Context::Scope context_scope(context);
 
   std::unique_ptr<content::V8ValueConverter> v8_converter =
@@ -290,10 +290,10 @@ void CastDemoBindings::OnGetConnectionStatus(
     v8::Global<v8::Context> original_context,
     base::Value status) {
   v8::Isolate* isolate = blink::MainThreadIsolate();
-  v8::MicrotasksScope microtasks_scope(
-      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::HandleScope handle_scope(isolate);
   v8::Local<v8::Context> context = original_context.Get(isolate);
+  v8::MicrotasksScope microtasks_scope(
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Context::Scope context_scope(context);
 
   std::unique_ptr<content::V8ValueConverter> v8_converter =
--- content/renderer/pepper/ppb_var_deprecated_impl.cc
+++ content/renderer/pepper/ppb_var_deprecated_impl.cc
@@ -97,8 +97,7 @@ bool HasPropertyDeprecated(PP_Var var, PP_Var name, PP_Var* exception) {
                               exception);
   v8::Local<v8::Context> context = try_catch.GetContext();
   v8::MicrotasksScope microtasks_scope(
-      accessor.GetObject()->GetIsolate(), context->GetMicrotaskQueue(),
-      v8::MicrotasksScope::kDoNotRunMicrotasks);
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Local<v8::Value> v8_name = try_catch.ToV8(name);
   if (try_catch.HasException())
     return false;
@@ -121,8 +120,7 @@ bool HasMethodDeprecated(PP_Var var, PP_Var name, PP_Var* exception) {
                               exception);
   v8::Local<v8::Context> context = try_catch.GetContext();
   v8::MicrotasksScope microtasks_scope(
-      accessor.GetObject()->GetIsolate(), context->GetMicrotaskQueue(),
-      v8::MicrotasksScope::kDoNotRunMicrotasks);
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Local<v8::Value> v8_name = try_catch.ToV8(name);
   if (try_catch.HasException())
     return false;
@@ -150,8 +148,7 @@ PP_Var GetProperty(PP_Var var, PP_Var name, PP_Var* exception) {
                               exception);
   v8::Local<v8::Context> context = try_catch.GetContext();
   v8::MicrotasksScope microtasks_scope(
-      accessor.GetObject()->GetIsolate(), context->GetMicrotaskQueue(),
-      v8::MicrotasksScope::kDoNotRunMicrotasks);
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Local<v8::Value> v8_name = try_catch.ToV8(name);
   if (try_catch.HasException())
     return PP_MakeUndefined();
@@ -176,8 +173,7 @@ void EnumerateProperties(PP_Var var,
                               exception);
   v8::Local<v8::Context> context = try_catch.GetContext();
   v8::MicrotasksScope microtasks_scope(
-      accessor.GetObject()->GetIsolate(), context->GetMicrotaskQueue(),
-      v8::MicrotasksScope::kDoNotRunMicrotasks);
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
 
   *properties = nullptr;
   *property_count = 0;
@@ -212,8 +208,7 @@ void SetPropertyDeprecated(PP_Var var,
                               exception);
   v8::Local<v8::Context> context = try_catch.GetContext();
   v8::MicrotasksScope microtasks_scope(
-      accessor.GetObject()->GetIsolate(), context->GetMicrotaskQueue(),
-      v8::MicrotasksScope::kDoNotRunMicrotasks);
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Local<v8::Value> v8_name = try_catch.ToV8(name);
   v8::Local<v8::Value> v8_value = try_catch.ToV8(value);
 
@@ -236,8 +231,7 @@ void DeletePropertyDeprecated(PP_Var var, PP_Var name, PP_Var* exception) {
                               exception);
   v8::Local<v8::Context> context = try_catch.GetContext();
   v8::MicrotasksScope microtasks_scope(
-      accessor.GetObject()->GetIsolate(), context->GetMicrotaskQueue(),
-      v8::MicrotasksScope::kDoNotRunMicrotasks);
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Local<v8::Value> v8_name = try_catch.ToV8(name);
 
   if (try_catch.HasException())
@@ -272,8 +266,7 @@ PP_Var CallDeprecatedInternal(PP_Var var,
                               exception);
   v8::Local<v8::Context> context = try_catch.GetContext();
   v8::MicrotasksScope microtasks_scope(
-      accessor.GetObject()->GetIsolate(), context->GetMicrotaskQueue(),
-      v8::MicrotasksScope::kDoNotRunMicrotasks);
+      context, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Local<v8::Value> v8_method_name = try_catch.ToV8(scoped_name.get());
   if (try_catch.HasException())
     return PP_MakeUndefined();
