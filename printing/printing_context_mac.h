4517492730ef6440b8c800f41facdbafe0418c58
Avi Drissman <avi@chromium.org>
2023-08-03 19:14:17 +0000

    Remove ARC boilerplate in /printing
    ARC is now enabled by default, so there’s no need to enforce it
    against files being put into non-ARC targets.
    
    Bug: 1468376
    Change-Id: I0586503948d1b539855e5d9460d6a776a35dc71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750100
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179175}

--- printing/printing_context_mac.h
+++ printing/printing_context_mac.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/print_job_constants.h"
 #include "printing/printing_context.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 @class NSPrintInfo;
 
 namespace printing {
