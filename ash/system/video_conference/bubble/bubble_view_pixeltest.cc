009c4f3665dc3dd4546360d28df8be84fdaa5da0
Andre Le <leandre@chromium.org>
2023-08-03 00:44:52 +0000

    VC UI: Re-implement the privacy indicators in VC buttons
    Due to the bug below, we need to switch to use another approach to
    implement this green dot indicator. We will now use option 2 instead of
    option 3 in go/vc-button-indicator, and will investigate a fix for
    option 3 later.
    
    Also add new pixel test images for various states of these buttons.
    
    Screenshot: https://screenshot.googleplex.com/4wQFBkzu9jkd6dN
    
    Fixed: b:293902412
    Change-Id: Iaab0eae613611bbfbc4da0c31c61d8b57830a69a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4739636
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178757}

--- ash/system/video_conference/bubble/bubble_view_pixeltest.cc
+++ ash/system/video_conference/bubble/bubble_view_pixeltest.cc
@@ -280,7 +280,7 @@ TEST_F(BubbleViewPixelTest, ReturnToAppLinux) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "video_conference_tray_linux_bubble_one_app",
-      /*revision_number=*/1, video_conference_tray()->GetBubbleView()));
+      /*revision_number=*/2, video_conference_tray()->GetBubbleView()));
 
   controller()->AddMediaApp(CreateFakeMediaApp(
       /*is_capturing_camera=*/true, /*is_capturing_microphone=*/true,
@@ -295,7 +295,7 @@ TEST_F(BubbleViewPixelTest, ReturnToAppLinux) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "video_conference_tray_linux_bubble_two_app",
-      /*revision_number=*/1, video_conference_tray()->GetBubbleView()));
+      /*revision_number=*/2, video_conference_tray()->GetBubbleView()));
 }
 
 TEST_F(BubbleViewPixelTest, OneToggleEffects) {
