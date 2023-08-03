73584ee0d9715aa45620a4eae62c5e32de0ad271
Dibyajyoti Pal <dibyapal@google.com>
2023-08-03 18:35:24 +0000

    [dPWA/Link Handling] Move supported_links dialog to cr_components
    This CL moves the supported_links dialog to cr_components/ inside
    the app_management/ folder, and puts in place the plumbing
    required for it to be accessible on CrOS. This would make
    reusing this component simpler on Windows, Mac and Linux platforms.
    
    Bug: 1412856, b/269772981
    Change-Id: If7a2adf19a6e8ccd191068cc453c9f6d01ffa03e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743279
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179157}

--- chrome/browser/resources/ash/settings/os_apps_page/app_management_page/supported_links_item.ts
+++ chrome/browser/resources/ash/settings/os_apps_page/app_management_page/supported_links_item.ts
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_components/localized_link/localized_link.js';
+import 'chrome://resources/cr_components/app_management/supported_links_dialog.js';
 import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
 import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
-import './supported_links_dialog.js';
 import './supported_links_overlapping_apps_dialog.js';
 
 import {App} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
