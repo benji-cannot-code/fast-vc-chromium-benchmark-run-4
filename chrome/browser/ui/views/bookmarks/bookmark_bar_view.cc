e172baa6d1c62af68660c76370335a18ae5a78ea
dljames <dljames@google.com>
2023-08-03 23:13:43 +0000

    [ ChromeRefresh2023 ] Update bookmarks leading padding
    Changes the leading padding of the first bookmark in the bookmarks bar
    from 6px -> 5px to align with the back arrow button in the toolbar.
    
    Change-Id: Ic509cab982d0899a6e98fdc23240aec195f9669d
    Bug: 1450529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750223
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1179330}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
@@ -573,7 +573,7 @@ class BookmarkFolderButton : public BookmarkMenuButtonBase {
   BookmarkFolderButton& operator=(const BookmarkFolderButton&) = delete;
 
   std::u16string GetTooltipText(const gfx::Point& p) const override {
-    return GetAccessibleName();
+    return GetAccessibleText();
   }
 
   bool OnMousePressed(const ui::MouseEvent& event) override {
@@ -587,7 +587,7 @@ class BookmarkFolderButton : public BookmarkMenuButtonBase {
     return BookmarkMenuButtonBase::OnMousePressed(event);
   }
 
-  const std::u16string GetAccessibleText() {
+  const std::u16string GetAccessibleText() const {
     // If the folder is unnamed, set the name to a default string for unnamed
     // folders; otherwise set the name to the user-supplied folder name.
     return GetText().empty()
@@ -885,7 +885,7 @@ void BookmarkBarView::GetAnchorPositionForButton(
 
 int BookmarkBarView::GetLeadingMargin() const {
   static constexpr int kBookmarksBarLeadingMarginPreRefresh = 8;
-  static constexpr int kBookmarksBarLeadingMarginWithoutSavedTabGroups = 6;
+  static constexpr int kBookmarksBarLeadingMarginWithoutSavedTabGroups = 5;
   static constexpr int kBookmarksBarLeadingMarginWithSavedTabGroups = 12;
 
   if (!features::IsChromeRefresh2023()) {
