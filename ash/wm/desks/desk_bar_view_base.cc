f876c34a357ef5b8f7ba99ee3b8c906f532294f0
Yongshun Liu <yongshun@chromium.org>
2023-08-03 20:32:52 +0000

    bento_button: Fix bar bounds for RTL with side shelf
    This fixes the bar view bounds with side shelf when RTL is on.
    
    Bug: b/294074763
    Test: DeskButtonTest.BarBoundsWithRTL
    Change-Id: I0785fee18a04f6ff5083c963a88832df32b23ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743830
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179232}

--- ash/wm/desks/desk_bar_view_base.cc
+++ ash/wm/desks/desk_bar_view_base.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/work_area_insets.h"
 #include "base/check.h"
 #include "base/functional/bind.h"
+#include "base/i18n/rtl.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/uuid.h"
@@ -749,17 +750,20 @@ void DeskBarViewBase::Layout() {
   // TODO(b/293658108): Move dynamic width update out of `Layout`.
   gfx::Size preferred_size = CalculatePreferredSize();
   gfx::Rect new_bounds = GetAvailableBounds();
-  switch (Shelf::ForWindow(root_)->alignment()) {
+  ShelfAlignment shelf_alignment = Shelf::ForWindow(root_)->alignment();
+  switch (shelf_alignment) {
     case ShelfAlignment::kBottom:
       new_bounds.ClampToCenteredSize(preferred_size);
       break;
     case ShelfAlignment::kLeft:
-      new_bounds.set_size(preferred_size);
-      break;
     case ShelfAlignment::kRight:
-      new_bounds.set_origin({new_bounds.right() - preferred_size.width(),
-                             new_bounds.bottom() - preferred_size.height()});
-      new_bounds.set_size(preferred_size);
+      if ((shelf_alignment == ShelfAlignment::kRight) == base::i18n::IsRTL()) {
+        new_bounds.set_size(preferred_size);
+      } else {
+        new_bounds.set_origin({new_bounds.right() - preferred_size.width(),
+                               new_bounds.bottom() - preferred_size.height()});
+        new_bounds.set_size(preferred_size);
+      }
       break;
     case ShelfAlignment::kBottomLocked:
       return;
