4ec56ff43fb9a325858a6b1fd5d499cfcbe0f8dd
Claudio M <claudiomagni@google.com>
2023-08-03 06:20:18 +0000

    language_packs: Add error to Mojo response and update Impl
    We need to expose to clients more information on operation failures.
    This need is currently motivated by the high occurrence of
    "Need Reboot" errors.
    
    Bug: b:240873387
    Change-Id: I8933542d3139a6106adce30941911d8142811eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4741217
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178834}

--- chromeos/ash/components/language_packs/language_packs_impl.cc
+++ chromeos/ash/components/language_packs/language_packs_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::language_packs {
 
 using ::ash::language::mojom::BasePackInfo;
+using ::ash::language::mojom::ErrorCode;
 using ::ash::language::mojom::FeatureId;
 using ::ash::language::mojom::LanguagePackInfo;
 using ::ash::language::mojom::LanguagePacks;
@@ -45,7 +46,25 @@ PackState GetPackStateFromStatusCode(const PackResult::StatusCode status_code) {
       return PackState::INSTALLED;
     // Catch all remaining cases as error.
     default:
+      // TODO: b/294162606 - Deprecate this value and use UNKNOWN instead.
       return PackState::ERROR;
+  }
+}
+
+ErrorCode GetMojoErrorFromPackError(const PackResult::ErrorCode pack_error) {
+  // This conversion is exhaustive. We don't use a default: case so that we can
+  // catch missing values at compile time.
+  switch (pack_error) {
+    case PackResult::kErrorNone:
+      return ErrorCode::kNone;
+    case PackResult::kErrorOther:
+      return ErrorCode::kOther;
+    case PackResult::kErrorWrongId:
+      return ErrorCode::kWrongId;
+    case PackResult::kErrorNeedReboot:
+      return ErrorCode::kNeedReboot;
+    case PackResult::kErrorAllocation:
+      return ErrorCode::kAllocation;
   }
 }
 
@@ -55,6 +74,7 @@ void OnOperationComplete(LanguagePacksImpl::GetPackInfoCallback mojo_callback,
                          const PackResult& pack_result) {
   auto info = LanguagePackInfo::New();
   info->pack_state = GetPackStateFromStatusCode(pack_result.pack_state);
+  info->error = GetMojoErrorFromPackError(pack_result.operation_error);
   if (pack_result.pack_state == PackResult::INSTALLED) {
     info->path = pack_result.path;
   }
@@ -71,6 +91,7 @@ void OnInstallBasePackComplete(
     const PackResult& pack_result) {
   auto info = BasePackInfo::New();
   info->pack_state = GetPackStateFromStatusCode(pack_result.pack_state);
+  info->error = GetMojoErrorFromPackError(pack_result.operation_error);
   if (pack_result.pack_state == PackResult::INSTALLED) {
     info->path = pack_result.path;
   }
