9097f00004f49808932571b618a60d854e5af53d
Joel Hockey <joelhockey@chromium.org>
2023-08-03 23:31:33 +0000

    UpdateCaptionButtonColors(absl::nullopt) when ChromeRefresh2023 disabled
    Fixes Terminal app button colors when ChromeRefresh2023 is disabled.
    
    Bug: b/288494696
    Bug: b/294292992
    Change-Id: I6611cdbe60ab4e701355a6e2b35c70b5b5e5d810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4745480
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179339}

--- chrome/browser/ui/views/frame/browser_frame_header_chromeos.cc
+++ chrome/browser/ui/views/frame/browser_frame_header_chromeos.cc
@@ -177,6 +177,7 @@ SkColor BrowserFrameHeaderChromeOS::GetCurrentFrameColor() const {
 
 void BrowserFrameHeaderChromeOS::UpdateFrameColors() {
   SetPaintAsActive(target_widget()->ShouldPaintAsActive());
+  absl::optional<ui::ColorId> button_colors;
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   if (features::IsChromeRefresh2023()) {
@@ -192,16 +193,13 @@ void BrowserFrameHeaderChromeOS::UpdateFrameColors() {
     if (!app_browser_controller ||
         (app_browser_controller->system_app() &&
          app_browser_controller->system_app()->UseSystemThemeColor())) {
-      UpdateCaptionButtonColors(mode() == MODE_ACTIVE
-                                    ? ui::kColorSysPrimary
-                                    : ui::kColorFrameCaptionButtonUnfocused);
-    } else {
-      UpdateCaptionButtonColors(absl::nullopt);
+      button_colors = mode() == MODE_ACTIVE
+                          ? ui::kColorSysPrimary
+                          : ui::kColorFrameCaptionButtonUnfocused;
     }
   }
-#else
-  UpdateCaptionButtonColors(absl::nullopt);
 #endif
+  UpdateCaptionButtonColors(button_colors);
   view()->SchedulePaint();
 }
 
