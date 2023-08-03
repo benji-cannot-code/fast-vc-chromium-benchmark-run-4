9506983028506166da0114706dd17f41db5870e2
Zhe Li <zheliooo@google.com>
2023-08-03 23:59:11 +0000

    Disable test in PageInfoViewTest
    Bug: 1469675
    Change-Id: I05c8812c2cb43858257d40b4e18b3a2a1b50e1ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4749395
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Owners-Override: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179350}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrCardboardPermissionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrCardboardPermissionTest.java
@@ -12,7 +12,6 @@ import static org.chromium.chrome.test.util.ChromeRestriction.RESTRICTION_TYPE_V
 
 import androidx.test.filters.MediumTest;
 
-import org.chromium.base.test.util.DisabledTest;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,6 +23,7 @@ import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.vr.rules.XrActivityRestriction;
