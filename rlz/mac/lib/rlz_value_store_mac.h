dafe6ee0b842e4fa4600bb2e18804c769a7770e3
Avi Drissman <avi@chromium.org>
2023-08-03 20:42:31 +0000

    Remove ARC boilerplate in /rlz
    ARC is now enabled by default, so there’s no need to enforce it
    against files being put into non-ARC targets.
    
    Bug: 1468376
    Change-Id: Ia62a8fc956f5d32c28e5ce115bfb1e40281906d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742924
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179241}

--- rlz/mac/lib/rlz_value_store_mac.h
+++ rlz/mac/lib/rlz_value_store_mac.h
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "rlz/lib/rlz_value_store.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 namespace rlz_lib {
 
 // An implementation of RlzValueStore for mac. It stores information in a
--- rlz/mac/lib/rlz_value_store_mac.mm
+++ rlz/mac/lib/rlz_value_store_mac.mm
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "rlz/lib/recursive_cross_process_lock_posix.h"
 #include "rlz/lib/supplementary_branding.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 using base::mac::ObjCCast;
 
 namespace rlz_lib {
