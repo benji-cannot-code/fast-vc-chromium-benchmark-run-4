82cfcb110faa8d0ec002b42a0cddafb4f42664e0
Lei Zhang <thestig@chromium.org>
2023-08-03 21:10:16 +0000

    Replace over-inclusive header in viz's frame_sink_id.h
    This header currently includes base/trace_event/typed_macros.h, even
    though it does not use any macros from the header. This adds several GBs
    to the build cost. [1] Fix this by including the headers that are
    actually needed instead.
    
    [1] https://commondatastorage.googleapis.com/chromium-browser-clang/chrome_includes_2023-08-01_003523.html
    
    Change-Id: Iaa5886e3f57a51fa2b7677bdbaef8395aec0f565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750139
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179264}

--- components/viz/common/surfaces/frame_sink_id.h
+++ components/viz/common/surfaces/frame_sink_id.h
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/hash/hash.h"
 #include "base/strings/string_piece.h"
-#include "base/trace_event/typed_macros.h"
+#include "base/tracing/protos/chrome_track_event.pbzero.h"
 #include "components/viz/common/viz_common_export.h"
+#include "third_party/perfetto/include/perfetto/tracing/traced_proto.h"
 
 namespace viz {
 
