fc182bd9e6620c866b82ba5886bb4e2ecf113edf
Sylvain Defresne <sdefresne@chromium.org>
2023-08-03 13:26:31 +0000

    Remove ARC boilerplate in //ios
    ARC is now enabled by default, so there’s no need to enforce it
    against files being put into non-ARC targets.
    
        Those files were missed in https://crrev.com/c/4739534. It
        may be because the ARC guard where touching other lines,
        causing the script used to miss them.
    
    Bug: 1468376
    Change-Id: Ic6958db55cd4672fa187c9b417d80f6cf7a00f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4747121
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178991}

--- ios/chrome/browser/crash_report/main_thread_freeze_detector.mm
+++ ios/chrome/browser/crash_report/main_thread_freeze_detector.mm
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/tests_hook.h"
 #import "ios/chrome/browser/crash_report/crash_helper.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
 namespace {
 // The info contains a dictionary with info about the freeze report.
 // See description at `_lastSessionFreezeInfo`.
--- ios/net/protocol_handler_util.mm
+++ ios/net/protocol_handler_util.mm
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !BUILDFLAG(USE_PLATFORM_ICU_ALTERNATIVES)
 #include "base/i18n/encoding_detection.h"  // nogncheck
 #include "base/i18n/icu_string_conversions.h"  // nogncheck
-
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
 #endif  // !BUILDFLAG(USE_PLATFORM_ICU_ALTERNATIVES)
 
 namespace {
