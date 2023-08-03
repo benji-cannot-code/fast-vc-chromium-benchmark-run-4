a7b5176fc51ea148a72e7abbd565d70d570a5771
Courtney Wong <courtneywong@chromium.org>
2023-08-03 23:57:39 +0000

    [TS Migration]: Convert EDU Coexistence to module-based Polymer3 syntax
    Tested: Manually tested the EDU coexistence flow
    
    Bug: b:293655277
    Change-Id: I9405eb5e45cf0b98d86d3697dc1ee7eb504b773b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4739599
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179349}

--- chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_template.js
+++ chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_template.js
@@ -3,26 +3,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CrScrollableBehavior} from 'chrome://resources/ash/common/cr_scrollable_behavior.js';
-import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrScrollableBehavior, CrScrollableBehaviorInterface} from 'chrome://resources/ash/common/cr_scrollable_behavior.js';
+import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-Polymer({
-  is: 'edu-coexistence-template',
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ * @implements {CrScrollableBehaviorInterface}
+ */
+const EduCoexistenceTemplateBase =
+    mixinBehaviors([CrScrollableBehavior], PolymerElement);
 
-  _template: html`{__html_template__}`,
+/**
+ * @polymer
+ */
+class EduCoexistenceTemplate extends EduCoexistenceTemplateBase {
+  static get is() {
+    return 'edu-coexistence-template';
+  }
 
-  behaviors: [CrScrollableBehavior],
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
-  properties: {
-    /**
-     * Indicates whether the footer/button div should be shown.
-     * @private {boolean}
-     */
-    showButtonFooter_: {
-      type: Boolean,
-      value: false,
-    },
-  },
+  static get properties() {
+    return {
+      /**
+       * Indicates whether the footer/button div should be shown.
+       * @private
+       */
+      showButtonFooter_: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
 
   /**
    * Shows/hides the button footer.
@@ -30,6 +45,7 @@ Polymer({
    */
   showButtonFooter(show) {
     this.showButtonFooter_ = show;
-  },
+  }
+}
 
-});
+customElements.define(EduCoexistenceTemplate.is, EduCoexistenceTemplate);
