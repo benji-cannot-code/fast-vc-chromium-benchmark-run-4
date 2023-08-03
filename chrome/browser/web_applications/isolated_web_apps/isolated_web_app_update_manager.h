8233e9df37302fca9e02b91f927bf804e2904af5
Christian Flach <cmfcmf@chromium.org>
2023-08-03 09:52:22 +0000

    iwa: Add class to wait for the right moment to apply an update
    The new `IsolatedWebAppUpdateApplyWaiter`'s responsibility is to wait
    for the right moment to a apply a pending, prepared, and stored update.
    For now, all it does is to wait for all windows of the IWA to close.
    Later, it might do more sophisticated things, like showing a message to
    the user and waiting for the user to click "perform update now".
    
    The next CL will implement a new task type for actually applying the
    update.
    
    Low-Coverage-Reason: Some debug and logging code does not need tests.
                         IsolatedWebAppUpdateManager will receive more
                         coverage as part of the upcoming CLs in this chain.
    
    Bug: 1444407
    Change-Id: I071d409a3cf7b3c91fd87cbbcaad76b8f798165a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667270
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178922}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.h
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/containers/circular_deque.h"
+#include "base/containers/flat_map.h"
 #include "base/files/file_error_or.h"
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "base/values.h"
+#include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_apply_waiter.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_discovery_task.h"
 #include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_app_install_manager_observer.h"
@@ -86,6 +88,8 @@ class IsolatedWebAppUpdateManager : public WebAppInstallManagerObserver {
   void QueueUpdateDiscoveryTask(const IsolatedWebAppUrlInfo& url_info,
                                 const GURL& update_manifest_url);
 
+  void CreateUpdateApplyWaiter(const IsolatedWebAppUrlInfo& url_info);
+
   // Starts the next update discovery task if (a) no update discovery task is
   // currently running and (b) there is at least one update discovery task in
   // the queue.
@@ -93,6 +97,11 @@ class IsolatedWebAppUpdateManager : public WebAppInstallManagerObserver {
 
   void OnUpdateDiscoveryTaskCompleted(
       IsolatedWebAppUpdateDiscoveryTask::CompletionStatus status);
+
+  void OnUpdateApplyWaiterFinished(
+      IsolatedWebAppUrlInfo url_info,
+      std::unique_ptr<ScopedKeepAlive> keep_alive,
+      std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive);
 
   raw_ref<Profile> profile_;
   bool automatic_updates_enabled_;
@@ -109,6 +118,9 @@ class IsolatedWebAppUpdateManager : public WebAppInstallManagerObserver {
   base::circular_deque<std::unique_ptr<IsolatedWebAppUpdateDiscoveryTask>>
       update_discovery_tasks_;
   base::Value::List update_discovery_results_log_;
+
+  base::flat_map<AppId, std::unique_ptr<IsolatedWebAppUpdateApplyWaiter>>
+      update_apply_waiters_;
 
   base::ScopedObservation<WebAppInstallManager, WebAppInstallManagerObserver>
       install_manager_observation_{this};
