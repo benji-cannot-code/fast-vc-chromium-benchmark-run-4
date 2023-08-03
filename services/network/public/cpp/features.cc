cc99d6f95db930a481ee14fbca46257a66297b03
Ari Chivukula <arichiv@chromium.org>
2023-08-03 17:18:48 +0000

    [Clear-Site-Data] Enable clientHints option by default
    This can move forward after the launch process is approved.
    
    Bug: 1458394
    Change-Id: If1f72ef5ceffb94fbfc38d5d30f8896bd3a91f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701523
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179114}

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -373,6 +373,6 @@ BASE_FEATURE(kVisibilityAwareResourceScheduler,
 
 BASE_FEATURE(kClearSiteDataClientHintsSupport,
              "ClearSiteDataClientHintsSupport",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace network::features
