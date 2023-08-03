5c45751a3eccab698c18ece0ff9a25e307e09250
Thomas Nguyen <tungnh@chromium.org>
2023-08-03 08:02:32 +0000

    [PEPC] Add new mojo API to support requesting permission from PEPC
    Bug: 1462930
    Change-Id: Id90fe463253453999c8472ca69e290c26c203d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4711814
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178876}

--- content/browser/permissions/permission_service_impl.h
+++ content/browser/permissions/permission_service_impl.h
@@ -51,6 +51,9 @@ class PermissionServiceImpl : public blink::mojom::PermissionService {
   // blink::mojom::PermissionService.
   void HasPermission(blink::mojom::PermissionDescriptorPtr permission,
                      PermissionStatusCallback callback) override;
+  void RequestPageEmbeddedPermission(
+      blink::mojom::EmbeddedPermissionRequestDescriptorPtr descriptor,
+      RequestPageEmbeddedPermissionCallback callback) override;
   void RequestPermission(blink::mojom::PermissionDescriptorPtr permission,
                          bool user_gesture,
                          PermissionStatusCallback callback) override;
@@ -67,6 +70,12 @@ class PermissionServiceImpl : public blink::mojom::PermissionService {
   void NotifyEventListener(blink::mojom::PermissionDescriptorPtr permission,
                            const std::string& event_type,
                            bool is_added) override;
+
+  void RequestPermissionsInternal(
+      BrowserContext* browser_context,
+      const std::vector<blink::mojom::PermissionDescriptorPtr>& permissions,
+      bool user_gesture,
+      RequestPermissionsCallback callback);
 
   void OnRequestPermissionsResponse(
       int pending_request_id,
