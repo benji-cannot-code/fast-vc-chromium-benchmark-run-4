f3f736b2c528b2871ce7df8d59dbaee28b435094
Stefan Zager <szager@chromium.org>
2023-08-03 20:40:18 +0000

    Use threaded compositing by default in web_tests runner
    Bug: 1463799
    Change-Id: I4c930ce980cbd5f3960cf8e5a364a7ef363fb01b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4736695
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179238}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -3596,7 +3596,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnablePluginPlaceholderTesting,
     switches::kEnablePreciseMemoryInfo,
     switches::kEnableSkiaBenchmarking,
-    switches::kEnableThreadedCompositing,
     switches::kEnableTouchDragDrop,
     switches::kEnableUnsafeWebGPU,
     switches::kEnableViewport,
