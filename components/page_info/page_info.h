111261794cf8055902449f3dea4cd3d12f95ff73
Christian Dullweber <dullweber@chromium.org>
2023-08-03 15:20:35 +0000

    SAA: Also show permissions in pageinfo that were blocked on access
    Display StorageAccess permissions in pageinfo when they were denied
    e.g. due to a default BLOCK setting.
    
    Bug: 1433644
    Change-Id: I95215a26f4cb097f44e574601986826e91952d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4747043
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179040}

--- components/page_info/page_info.h
+++ components/page_info/page_info.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/buildflags.h"
 #include "components/security_state/core/security_state.h"
 #include "content/public/browser/web_contents.h"
+#include "net/base/schemeful_site.h"
 
 namespace content_settings {
 class PageSpecificContentSettings;
@@ -344,12 +345,9 @@ class PageInfo : private content_settings::CookieControlsObserver,
     is_subscribed_to_permission_change_for_testing = true;
   }
 
+  void PresentSitePermissionsForTesting() { PresentSitePermissions(); }
+
  private:
-  FRIEND_TEST_ALL_PREFIXES(PageInfoTest,
-                           NonFactoryDefaultAndRecentlyChangedPermissionsShown);
-  FRIEND_TEST_ALL_PREFIXES(PageInfoTest, StorageAccessGrantsAreFiltered);
-  FRIEND_TEST_ALL_PREFIXES(PageInfoTest, IncognitoPermissionsEmptyByDefault);
-  FRIEND_TEST_ALL_PREFIXES(PageInfoTest, IncognitoPermissionsDontShowAsk);
   FRIEND_TEST_ALL_PREFIXES(PageInfoTest,
                            ShowInfoBarWhenAllowingThirdPartyCookies);
   FRIEND_TEST_ALL_PREFIXES(PageInfoTest,
@@ -449,6 +447,9 @@ class PageInfo : private content_settings::CookieControlsObserver,
   int GetThirdPartySitesWithBlockedCookiesAccessCount(const GURL& site_url);
 
   bool IsIsolatedWebApp() const;
+
+  std::set<net::SchemefulSite> GetTwoSitePermissionRequesters(
+      ContentSettingsType type);
 
   // The page info UI displays information and controls for site-
   // specific data (local stored objects like cookies), site-specific
