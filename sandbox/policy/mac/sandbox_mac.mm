b2a1893582011f46533bb31d5410ff577cbecb1a
Avi Drissman <avi@chromium.org>
2023-08-03 20:34:00 +0000

    Remove ARC boilerplate in /sandbox
    ARC is now enabled by default, so there’s no need to enforce it
    against files being put into non-ARC targets.
    
    Bug: 1468376
    Change-Id: I7259f0e1596e49b222e10f9e9ec5a6e6adedf757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748524
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179234}

--- sandbox/policy/mac/sandbox_mac.mm
+++ sandbox/policy/mac/sandbox_mac.mm
@@ -38,10 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/policy/mac/utility.sb.h"
 #include "sandbox/policy/mojom/sandbox.mojom.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 namespace sandbox::policy {
 
 base::FilePath GetCanonicalPath(const base::FilePath& path) {
