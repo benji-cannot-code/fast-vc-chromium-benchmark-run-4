8e1d26f25dc43d2727bcf05d3e31a42fdf0079ab
Peter Kvitek <kvitekp@chromium.org>
2023-08-03 20:13:07 +0000

    [DevTools] Add 'generateTaggedPDF' option to DevTools Page.printToPDF
    DevTools protocol Page.printToPDF method has lots of useful
    parameters, however, there is no parameter to specify whether
    resulting PDF should include accessibility info (PDF tagging
    in PDF speak).
    
    Since PDF accessibility info may be quite large, it is useful to
    have the ability to control PDF tagging on per Page.printToPDF
    call basis.
    
    This CL adds 'generate_tagged_pdf' option to Page.printToPDF and
    plumbs it all the way down to the printing code via new optional
    'generate_tagged_pdf' member in PrintParams of print.mojom.
    
    Note: this change does not affect Chrome's print to PDF
    functionality -- it will still generate tagged PDFs according
    to PrintRenderFrameHelper::Delegate::ShouldGenerateTaggedPDF()
    return value. Only PDFs created by DevTools Page.printToPDF
    method are affected.
    
    Bug: 1469961
    Change-Id: I4af95c5809f2659d62c3c9de25d3f0e44908fc86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4744479
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179198}

--- components/printing/renderer/print_render_frame_helper.cc
+++ components/printing/renderer/print_render_frame_helper.cc
@@ -2363,6 +2363,9 @@ bool PrintRenderFrameHelper::PrintPagesNative(
   CHECK(metafile.Init());
   metafile.UtilizeTypefaceContext(&typeface_content_info);
 
+  bool generate_tagged_pdf = print_params.generate_tagged_pdf.value_or(
+      delegate_->ShouldGenerateTaggedPDF());
+
   // If tagged PDF exporting is enabled, we also need to capture an
   // accessibility tree and store it in the metafile. AXTreeSnapshotter
   // should stay alive through the end of this function, because text
@@ -2370,7 +2373,7 @@ bool PrintRenderFrameHelper::PrintPagesNative(
   // is enabled.
   std::unique_ptr<content::AXTreeSnapshotter> snapshotter;
   ui::AXTreeUpdate accessibility_tree;
-  if (delegate_->ShouldGenerateTaggedPDF()) {
+  if (generate_tagged_pdf) {
     snapshotter = render_frame()->CreateAXTreeSnapshotter(ui::AXMode::kPDF);
     snapshotter->Snapshot(
         /*max_node_count=*/0,
