25eb721c33799ae73cd7357720059d32a1911986
Lei Zhang <thestig@chromium.org>
2023-08-03 21:17:35 +0000

    Unify 2 "in-progress" bools in PrintRenderFrameHelper
    PrintRenderFrameHelper has two separate booleans, `in_scripted_print_`
    and `print_node_in_progress_`, for tracking whether 2 different types of
    print operations are in progress. Instead of tracking them separately,
    combine them into a single boolean. If one type of printing is already
    in progress, the other type should not start.
    
    Bug: 1459437
    Change-Id: I39e1a7e8192804d1d9744af673390b17c5144293
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728316
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179269}

--- components/printing/renderer/print_render_frame_helper.cc
+++ components/printing/renderer/print_render_frame_helper.cc
@@ -1334,10 +1334,11 @@ void PrintRenderFrameHelper::ScriptedPrint(bool user_initiated) {
   if (!web_frame->GetDocument().GetFrame())
     return;
 
-  if (in_scripted_print_)
+  if (print_in_progress_) {
     return;
+  }
 
-  in_scripted_print_ = true;
+  print_in_progress_ = true;
   auto weak_this = weak_ptr_factory_.GetWeakPtr();
   if (g_is_preview_enabled) {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
@@ -1363,7 +1364,7 @@ void PrintRenderFrameHelper::ScriptedPrint(bool user_initiated) {
   if (!weak_this)
     return;
 
-  in_scripted_print_ = false;
+  print_in_progress_ = false;
 }
 
 void PrintRenderFrameHelper::WillBeDestroyed() {
@@ -2129,14 +2130,14 @@ void PrintRenderFrameHelper::PrintNode(const blink::WebNode& node) {
     return;
   }
 
-  if (print_node_in_progress_) {
+  if (print_in_progress_) {
     // This can happen as a result of processing sync messages when printing
     // from ppapi plugins. It's a rare case, so its OK to just fail here.
     // See http://crbug.com/159165.
     return;
   }
 
-  print_node_in_progress_ = true;
+  print_in_progress_ = true;
 
   if (g_is_preview_enabled) {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
@@ -2173,7 +2174,7 @@ void PrintRenderFrameHelper::PrintNode(const blink::WebNode& node) {
       return;
   }
 
-  print_node_in_progress_ = false;
+  print_in_progress_ = false;
 }
 
 void PrintRenderFrameHelper::Print(blink::WebLocalFrame* frame,
