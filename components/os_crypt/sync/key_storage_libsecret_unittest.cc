0eb717a50f98bd486f78d6f4d9ccc2703f4b57bb
Christos Froussios <cfroussios@google.com>
2023-08-03 05:31:19 +0000

    [OSCrypt] Replace raw_ptr with ScopedGObject
    Bug: 1468856
    Change-Id: Ifa3e16c5e81e6cd761757ff6c70ba5d06e0ad44c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4736933
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@google.com>
    Auto-Submit: Christos Froussios <cfroussios@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178822}

--- components/os_crypt/sync/key_storage_libsecret_unittest.cc
+++ components/os_crypt/sync/key_storage_libsecret_unittest.cc
@@ -7,10 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 
 #include "base/lazy_instance.h"
-#include "base/memory/raw_ptr.h"
 #include "components/os_crypt/sync/key_storage_libsecret.h"
 #include "components/os_crypt/sync/libsecret_util_linux.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/glib/scoped_gobject.h"
 
 namespace {
 
@@ -37,33 +37,32 @@ class MockPasswordStore {
     ClearPassword();
     for (GObject* o : objects_returned_to_caller_) {
       ASSERT_EQ(o->ref_count, 1u);
-      g_object_unref(o);
     }
     objects_returned_to_caller_.clear();
   }
 
   void ClearPassword() {
     if (password_) {
-      ASSERT_EQ(password_->ref_count, 1u);
-      g_object_unref(password_.ExtractAsDangling());
-      password_ = nullptr;
+      ASSERT_EQ((*password_).ref_count, 1u);
+      password_ = ScopedGObject<GObject>();  // Reset
     }
   }
 
   void SetPassword(const std::string& password) {
     ASSERT_FALSE(password_);
-    password_ = static_cast<GObject*>(g_object_new(G_TYPE_OBJECT, nullptr));
-    mapping_[password_] = password;
+    password_ = TakeGObject(
+        static_cast<GObject*>(g_object_new(G_TYPE_OBJECT, nullptr)));
+    mapping_[password_.get()] = password;
   }
 
+  // The returned object has a ref count of 2. This way, after the client
+  // deletes the object, it isn't destroyed, and we can check that all these
+  // objects have ref count of 1 at the end of the test.
   GObject* MakeTempObject(const std::string& value) {
-    GObject* temp = static_cast<GObject*>(g_object_new(G_TYPE_OBJECT, nullptr));
-    // The returned object has a ref count of 2. This way, after the client
-    // deletes the object, it isn't destroyed, and we can check that all these
-    // objects have ref count of 1 at the end of the test.
-    g_object_ref(temp);
+    ScopedGObject temp = WrapGObject(
+        static_cast<GObject*>(g_object_new(G_TYPE_OBJECT, nullptr)));
     objects_returned_to_caller_.push_back(temp);
-    mapping_[temp] = value;
+    mapping_[temp.get()] = value;
     return temp;
   }
 
@@ -73,9 +72,11 @@ class MockPasswordStore {
 
   GObject* password() { return password_; }
 
+  // The keys in `mapping_` refer to objects in `objects_returned_to_caller_` or
+  // `password_`, which manage their lifetime.
   std::unordered_map<GObject*, std::string> mapping_;
-  std::vector<GObject*> objects_returned_to_caller_;
-  raw_ptr<GObject> password_ = nullptr;
+  std::vector<ScopedGObject<GObject>> objects_returned_to_caller_;
+  ScopedGObject<GObject> password_;
 };
 base::LazyInstance<MockPasswordStore>::Leaky g_password_store =
     LAZY_INSTANCE_INITIALIZER;
