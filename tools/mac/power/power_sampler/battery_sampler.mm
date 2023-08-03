7ae2309eb4144165f913465ab632c9d557edddfd
Avi Drissman <avi@chromium.org>
2023-08-03 13:21:57 +0000

    Remove ARC boilerplate in /tools
    ARC is now enabled by default, so there’s no need to enforce it
    against files being put into non-ARC targets.
    
    Bug: 1468376
    Change-Id: I06081204f7c8ef59e45563d958fa369e127863ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4744859
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178990}

--- tools/mac/power/power_sampler/battery_sampler.mm
+++ tools/mac/power/power_sampler/battery_sampler.mm
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/time/time.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 namespace power_sampler {
 namespace {
 
--- tools/mac/power/power_sampler/smc_sampler_unittest.mm
+++ tools/mac/power/power_sampler/smc_sampler_unittest.mm
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "tools/mac/power/power_sampler/battery_sampler.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 namespace power_sampler {
 
 namespace {
