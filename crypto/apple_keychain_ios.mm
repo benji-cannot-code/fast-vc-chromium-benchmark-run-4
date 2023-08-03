e499fa74c2e86a617b226409bfc1e8990a6b75c3
Avi Drissman <avi@chromium.org>
2023-08-03 20:26:10 +0000

    Remove ARC boilerplate in /crypto
    ARC is now enabled by default, so there’s no need to enforce it
    against files being put into non-ARC targets.
    
    Bug: 1468376
    Change-Id: I6258554471e8fdd226bfd0b96f1eba4eedb2a005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750101
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179221}

--- crypto/apple_keychain_ios.mm
+++ crypto/apple_keychain_ios.mm
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #include "base/mac/scoped_cftyperef.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 namespace {
 
 enum KeychainAction {
