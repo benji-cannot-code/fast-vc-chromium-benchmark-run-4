d7fd2b73c73261cbeb12d173e57ac4f728ebc59d
S. Ganesh <ganesh@chromium.org>
2023-08-03 02:30:53 +0000

    MSI custom action DLL implementation: set installer result on error
    This CL adds a DLL entrypoint `ShowInstallerResultUIString` that sets
    the last installer result from the registry so the MSI installer can
    display it in the case of custom errors.
    
    Bug: 1465914
    Low-Coverage-Reason: I will be adding additional tests in the next couple of CLs
    Change-Id: I3b95b93b68c01aa31924429aea5aa9d0b37c38c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743550
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178781}

--- chrome/updater/win/installer_api.h
+++ chrome/updater/win/installer_api.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/registry.h"
 #include "chrome/updater/enum_traits.h"
 #include "chrome/updater/installer.h"
+#include "chrome/updater/win/win_constants.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace updater {
@@ -67,25 +68,6 @@ enum class UpdaterScope;
 // `%COMPANY%_USAGE_STATS_ENABLED`: "1" (if the updater sends usage stats), or
 // "0" (if the updater does not send usage stats); %COMPANY% is the uppercase
 // short company name specified in branding.gni (e.g. "GOOGLE").
-
-// These values are defined by the Installer API.
-enum class InstallerResult {
-  // The installer succeeded, unconditionally.
-  kSuccess = 0,
-
-  // The installer returned a specific error using the Installer API mechanism.
-  kCustomError = 1,
-
-  // The MSI installer failed, with a system error.
-  kMsiError = 2,
-
-  // The installer failed with a a system error.
-  kSystemError = 3,
-
-  // The installer failed. The exit code of the installer process contains
-  // the error.
-  kExitCode = 4,
-};
 
 template <>
 struct EnumTraits<InstallerResult> {
