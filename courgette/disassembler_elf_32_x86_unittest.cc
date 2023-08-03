271fd9ce922d841510ac7075c211e28d883eec31
Peter Kasting <pkasting@chromium.org>
2023-08-03 17:11:07 +0000

    Ban [w]ctype.h: courgette/
    Bug: 1361094
    Change-Id: I2fcf44bee7c60b8035cd36ca8c07e250a1cbf9bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4749519
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179105}

--- courgette/disassembler_elf_32_x86_unittest.cc
+++ courgette/disassembler_elf_32_x86_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "courgette/disassembler_elf_32_x86.h"
 
-#include <ctype.h>
 #include <stddef.h>
 #include <stdint.h>
 
