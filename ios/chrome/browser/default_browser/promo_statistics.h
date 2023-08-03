7e453171c6890ec19f3aefb0bbf5a99bc9383278
Gayane Petrosyan <gayane@google.com>
2023-08-03 14:04:01 +0000

    [iOS][DefaultBrowser] Add autofill suggestions use count stats
    Add autofill suggestions use count stats for trigger criteria experiment.
    
    Bug: 1456438
    Change-Id: Iaf00d340ec3e2bf0c44ae39b10b5e63ed554e8f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4741981
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179005}

--- ios/chrome/browser/default_browser/promo_statistics.h
+++ ios/chrome/browser/default_browser/promo_statistics.h
@@ -36,9 +36,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Number of times user used the bookmarks or bookmark manager in the last
 // `kTriggerCriteriaExperimentStatExpiration` days.
 @property(nonatomic, assign) int bookmarkUseCount;
+// Number of times user used the autofill suggestions in the last
+// `kTriggerCriteriaExperimentStatExpiration` days.
+@property(nonatomic, assign) int autofillUseCount;
 
 // TODO(crbug.com/1456438): Implement rest of the metrics:
-// AutofillUseCount
 // SpecialTabsUseCount
 
 @end
