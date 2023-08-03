8bf492c3e92567b0906023d4dcc782a736229392
Daniel Rubery <drubery@chromium.org>
2023-08-03 21:39:39 +0000

    Add DownloadCheckResult for deep scan failure
    This CL adds a DownloadCheckResult corresponding to the
    DEEP_SCANNED_FAILED danger type. A followup CL will generate this enum
    value in the DeepScanningRequest.
    
    bug: 1424527
    Change-Id: Icda3a6281fc205f987d44aca09878177fcb3fa0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4744184
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179284}

--- chrome/browser/safe_browsing/download_protection/download_protection_service.cc
+++ chrome/browser/safe_browsing/download_protection/download_protection_service.cc
@@ -112,6 +112,7 @@ bool IsDownloadSecuritySensitive(safe_browsing::DownloadCheckResult result) {
     case Result::DANGEROUS_HOST:
     case Result::POTENTIALLY_UNWANTED:
     case Result::DANGEROUS_ACCOUNT_COMPROMISE:
+    case Result::DEEP_SCANNED_FAILED:
       return true;
     case Result::SAFE:
     case Result::ALLOWLISTED_BY_POLICY:
