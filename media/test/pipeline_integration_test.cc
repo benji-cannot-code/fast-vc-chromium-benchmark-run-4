d1ca0a554a1ad5a2a9353922b6d0549ca060ab87
Peter Kasting <pkasting@chromium.org>
2023-08-03 18:45:50 +0000

    Ban [w]ctype.h: media/
    Bug: 1361094
    Change-Id: I258d63c1787b7fd2550e9bc9b1adc232a31fa7b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748928
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179161}

--- media/test/pipeline_integration_test.cc
+++ media/test/pipeline_integration_test.cc
@@ -7,13 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/containers/cxx20_erase.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/task/single_thread_task_runner.h"
@@ -38,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/test/pipeline_integration_test_base.h"
 #include "media/test/test_media_source.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "third_party/abseil-cpp/absl/strings/ascii.h"
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_ANDROID)
@@ -520,16 +524,13 @@ class MSEChangeTypeTest
     template <class ParamType>
     std::string operator()(
         const testing::TestParamInfo<ParamType>& info) const {
-      std::stringstream ss;
-      ss << std::get<0>(info.param) << "_AND_" << std::get<1>(info.param);
-      std::string s = ss.str();
+      std::string s = base::StrCat({std::get<0>(info.param).filename, "_AND_",
+                                    std::get<1>(info.param).filename});
       // Strip out invalid param name characters.
-      std::stringstream ss2;
-      for (size_t i = 0; i < s.size(); ++i) {
-        if (isalnum(s[i]) || s[i] == '_')
-          ss2 << s[i];
-      }
-      return ss2.str();
+      base::EraseIf(s, [](char c) {
+        return !absl::ascii_isalnum(static_cast<unsigned char>(c)) && c != '_';
+      });
+      return s;
     }
   };
 
