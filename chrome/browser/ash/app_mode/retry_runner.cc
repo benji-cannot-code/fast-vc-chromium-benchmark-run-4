05fbb6aea8ca96b68791d99d8deb3b358054f24c
Edman Anjos <edmanjos@gmail.com>
2023-08-03 09:43:58 +0000

    kiosk: Support CancellableJobs in RunUpToNTimes
    RunUpToNTimes only accepts jobs that return void, and are therefore self
    owned.
    
    Extend support to jobs that return a std::unique_ptr<CancellableJob>.
    
    Bug: b:291275398
    Test: testing/xvfb.py tools/autotest.py -C out_/Default --run_all chrome/browser/ash/app_mode/retry_runner_unittest.cc
    
    Change-Id: I1b729a5119ec19059a8e15b371c50e48b0ff8238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4724387
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178917}

--- chrome/browser/ash/app_mode/retry_runner.cc
+++ chrome/browser/ash/app_mode/retry_runner.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/app_mode/retry_runner.h"
 
-#include "base/functional/callback_forward.h"
+#include "base/functional/callback.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 
