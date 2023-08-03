9c594433c39e2d740a896ab4f9d01524392c06b5
Shibalik Mohapatra <shibalik@chromium.org>
2023-08-03 19:01:24 +0000

    [Refresh] Add pressed state for new tab button and tab search button.
    Also sets the background for the control button as transparent if an image based theme exists.
    
    Bug: 1466599
    Change-Id: I7c0ac97d4c45e36749f35c392a619d53ee85a885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4744191
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179168}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -161,6 +161,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabButtonFocusRing) \
   E_CPONLY(kColorNewTabButtonInkDropFrameActive) \
   E_CPONLY(kColorNewTabButtonInkDropFrameInactive) \
+  E_CPONLY(kColorTabStripControlButtonInkDrop) \
+  E_CPONLY(kColorTabStripControlButtonInkDropRipple) \
   /* New tab button colors for ChromeRefresh.*/ \
   /* TODO (crbug.com/1399942) remove when theming works */ \
   E_CPONLY(kColorNewTabButtonCRForegroundFrameActive) \
--- chrome/browser/ui/color/material_tab_strip_color_mixer.cc
+++ chrome/browser/ui/color/material_tab_strip_color_mixer.cc
@@ -37,6 +37,10 @@ void AddMaterialTabStripColorMixer(ui::ColorProvider* provider,
       ui::kColorSysHeaderInactive};
   mixer[kColorTabBackgroundInactiveHoverFrameActive] = {
       ui::kColorSysStateHeaderHover};
+  mixer[kColorTabStripControlButtonInkDrop] = {ui::kColorSysStateHeaderHover};
+  mixer[kColorTabStripControlButtonInkDropRipple] = {
+      ui::kColorSysStateRippleNeutralOnSubtle};
+
   // TODO(tbergquist): Use kColorSysStateHeaderHoverInactive, once it exists.
   mixer[kColorTabBackgroundInactiveHoverFrameInactive] = {
       ui::kColorSysStateHeaderHover};
