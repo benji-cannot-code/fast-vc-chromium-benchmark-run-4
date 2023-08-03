c8e84ef4c5cd00e02bee8cc70ad6227eae38dc88
Gauthier Ambard <gambard@chromium.org>
2023-08-03 17:40:20 +0000

    [iOS] Fix Recent Tab crash after disconnect
    The Recent Tabs were sometimes asking for their imageDataSource (the
    mediator) after -disconnect was called on it. At that point the favicon
    fetcher was null, leading to a crash.
    
    Fixed: 1469608
    Change-Id: Ib55b3d5f7f66d7ce3f7023dd4403c5d567a94679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748365
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179132}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_coordinator.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_coordinator.mm
@@ -144,6 +144,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)stop {
   [self.recentTabsTableViewController dismissModals];
+  self.recentTabsTableViewController.imageDataSource = nil;
   self.recentTabsTableViewController.browser = nil;
   self.recentTabsTableViewController.delegate = nil;
   self.recentTabsTableViewController = nil;
