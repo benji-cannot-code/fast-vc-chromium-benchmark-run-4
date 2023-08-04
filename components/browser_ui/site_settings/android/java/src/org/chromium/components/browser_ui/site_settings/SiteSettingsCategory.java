5f7be1a93184b4bf09aa1e741b0ff4c3801946a0
Aashna Sheth <aashnas@google.com>
2023-08-04 19:38:09 +0000

    [PageZoom] Chrome Site Settings UI
    This CL adds another row to the Chrome Site Settings page. When
    clicked this row displays zoom levels on each website (that are not at
    the default zoom). Once the functionality of the buttons is
    implemented, users will be able to reset individual site zooms and
    also clear all of them at once.
    
    Note: Strings currently set to untranslatable.
    
    Screens Edited:
    (new row) https://hsv.googleplex.com/6112797957029888
    (new screen) https://hsv.googleplex.com/6138224096313344
    (new pop up) https://hsv.googleplex.com/6463438818639872
    (new empty screen) https://hsv.googleplex.com/4889057218789376
    
    AX-Relnotes: N/A
    Bug: 1459631, b/265491863
    
    Low-Coverage-Reason: Low coverage because re-using existing UI objects
    that are tested more generally (WebsitePreference,
    ChromeImageViewPreference, etc). Only UI changes in this CL. Any
    zoom-specific tests will be included in CL that adds functionality.
    
    Change-Id: Iccbc30f107f9bb5c6c9452410f6a994f17a1fce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685005
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179773}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
@@ -48,7 +48,7 @@ public class SiteSettingsCategory {
             Type.PROTECTED_MEDIA, Type.SENSORS, Type.SOUND, Type.USB, Type.VIRTUAL_REALITY,
             Type.USE_STORAGE, Type.AUTO_DARK_WEB_CONTENT, Type.REQUEST_DESKTOP_SITE,
             Type.FEDERATED_IDENTITY_API, Type.THIRD_PARTY_COOKIES, Type.SITE_DATA, Type.ANTI_ABUSE,
-            Type.NUM_ENTRIES})
+            Type.ZOOM, Type.NUM_ENTRIES})
     @Retention(RetentionPolicy.SOURCE)
     public @interface Type {
         // All updates here must also be reflected in {@link #preferenceKey(int)
@@ -82,10 +82,11 @@ public class SiteSettingsCategory {
         int THIRD_PARTY_COOKIES = 26;
         int SITE_DATA = 27;
         int ANTI_ABUSE = 28;
+        int ZOOM = 29;
         /**
          * Number of handled categories used for calculating array sizes.
          */
-        int NUM_ENTRIES = 29;
+        int NUM_ENTRIES = 30;
     }
 
     private final BrowserContextHandle mBrowserContextHandle;
@@ -216,6 +217,7 @@ public class SiteSettingsCategory {
                 return ContentSettingsType.VR;
             case Type.ALL_SITES:
             case Type.USE_STORAGE:
+            case Type.ZOOM:
                 return ContentSettingsType.DEFAULT; // Conversion unavailable.
         }
         assert false;
@@ -301,6 +303,8 @@ public class SiteSettingsCategory {
                 return "site_data";
             case Type.THIRD_PARTY_COOKIES:
                 return "third_party_cookies";
+            case Type.ZOOM:
+                return "zoom";
             default:
                 assert false;
                 return "";
