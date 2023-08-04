76d5f838eaae4460959531e7dc265862ce982458
Mariam Mostafa <mariammostafa@google.com>
2023-08-04 14:26:23 +0000

    Change file name from ml_model_tokenizer to autofill_model_vectorizer
    Since adding the Vectorize() method changed the functionality of the
    class. It is now a vectroizer and not a tokenizer.
    
    Bug: 1465926
    Change-Id: I919ae52f7ed6efa88a6a9f05001f270bc7f52568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750926
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Mariam Mostafa <mariammostafa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179581}

--- components/autofill/core/browser/ml_model/autofill_model_vectorizer_unittest.cc
+++ components/autofill/core/browser/ml_model/autofill_model_vectorizer_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/autofill/core/browser/ml_model/ml_model_tokenizer.h"
+#include "components/autofill/core/browser/ml_model/autofill_model_vectorizer.h"
 
 #include "base/base_paths.h"
 #include "base/files/file_path.h"
@@ -13,10 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-using TokenId = AutofillMLModelTokenizer::TokenId;
+using TokenId = AutofillModelVectorizer::TokenId;
 using testing::ElementsAre;
 
-class AutofillMLModelTokenizerTest : public testing::Test {
+class AutofillModelVectorizerTest : public testing::Test {
  public:
   void SetUp() override {
     base::FilePath source_root_dir;
@@ -33,42 +33,42 @@ class AutofillMLModelTokenizerTest : public testing::Test {
   base::FilePath dictionary_path_;
 };
 
-TEST_F(AutofillMLModelTokenizerTest, TokenizerIsInitialized) {
+TEST_F(AutofillModelVectorizerTest, VectorizerIsInitialized) {
   auto model_tokenizer =
-      AutofillMLModelTokenizer::CreateTokenizer(dictionary_path_);
+      AutofillModelVectorizer::CreateVectorizer(dictionary_path_);
   EXPECT_NE(model_tokenizer, nullptr);
 }
 
-// Initialize tokenizer with a path that does not exist.
-TEST_F(AutofillMLModelTokenizerTest, WrongDictionaryPath) {
+// Initialize vectorizer with a path that does not exist.
+TEST_F(AutofillModelVectorizerTest, WrongDictionaryPath) {
   auto missing_path = base::FilePath::FromASCII("missing");
-  EXPECT_EQ(AutofillMLModelTokenizer::CreateTokenizer(missing_path), nullptr);
+  EXPECT_EQ(AutofillModelVectorizer::CreateVectorizer(missing_path), nullptr);
 }
 
-TEST_F(AutofillMLModelTokenizerTest, TokensMappedCorrectly) {
+TEST_F(AutofillModelVectorizerTest, TokensMappedCorrectly) {
   auto model_tokenizer =
-      AutofillMLModelTokenizer::CreateTokenizer(dictionary_path_);
+      AutofillModelVectorizer::CreateVectorizer(dictionary_path_);
   EXPECT_EQ(model_tokenizer->GetDictionarySize(), 11u);
   EXPECT_EQ(model_tokenizer->TokenToId(u"first"), TokenId(5));
 }
 
 // Tests that words out of vocabulary return 1.
-TEST_F(AutofillMLModelTokenizerTest, WordOutOfVocab) {
+TEST_F(AutofillModelVectorizerTest, WordOutOfVocab) {
   auto model_tokenizer =
-      AutofillMLModelTokenizer::CreateTokenizer(dictionary_path_);
+      AutofillModelVectorizer::CreateVectorizer(dictionary_path_);
   EXPECT_EQ(model_tokenizer->TokenToId(u"address"), TokenId(1));
 }
 
 // Tests that empty strings return 0 for padding.
-TEST_F(AutofillMLModelTokenizerTest, EmptyToken) {
+TEST_F(AutofillModelVectorizerTest, EmptyToken) {
   auto model_tokenizer =
-      AutofillMLModelTokenizer::CreateTokenizer(dictionary_path_);
+      AutofillModelVectorizer::CreateVectorizer(dictionary_path_);
   EXPECT_EQ(model_tokenizer->TokenToId(u""), TokenId(0));
 }
 
-TEST_F(AutofillMLModelTokenizerTest, InputVectorizedCorrectly) {
+TEST_F(AutofillModelVectorizerTest, InputVectorizedCorrectly) {
   auto model_tokenizer =
-      AutofillMLModelTokenizer::CreateTokenizer(dictionary_path_);
+      AutofillModelVectorizer::CreateVectorizer(dictionary_path_);
   EXPECT_THAT(model_tokenizer->Vectorize(u"Phone 'number"),
               testing::ElementsAre(TokenId(8), TokenId(2), TokenId(0),
                                    TokenId(0), TokenId(0)));
@@ -76,9 +76,9 @@ TEST_F(AutofillMLModelTokenizerTest, InputVectorizedCorrectly) {
 
 // If a field label has more than one consecutive whitespace, they
 // should all be removed without any empty strings.
-TEST_F(AutofillMLModelTokenizerTest, InputHasMoreThanOneWhitespace) {
+TEST_F(AutofillModelVectorizerTest, InputHasMoreThanOneWhitespace) {
   auto model_tokenizer =
-      AutofillMLModelTokenizer::CreateTokenizer(dictionary_path_);
+      AutofillModelVectorizer::CreateVectorizer(dictionary_path_);
   EXPECT_THAT(model_tokenizer->Vectorize(u"Phone   &number  "),
               testing::ElementsAre(TokenId(8), TokenId(2), TokenId(0),
                                    TokenId(0), TokenId(0)));
@@ -87,10 +87,9 @@ TEST_F(AutofillMLModelTokenizerTest, InputHasMoreThanOneWhitespace) {
 // If a field label has more words than the kOutputSequenceLength,
 // only the first kOutputSequenceLength many words should be used and the
 // rest are ignored.
-TEST_F(AutofillMLModelTokenizerTest,
-       InputHasMoreWordsThanOutputSequenceLength) {
+TEST_F(AutofillModelVectorizerTest, InputHasMoreWordsThanOutputSequenceLength) {
   auto model_tokenizer =
-      AutofillMLModelTokenizer::CreateTokenizer(dictionary_path_);
+      AutofillModelVectorizer::CreateVectorizer(dictionary_path_);
   EXPECT_THAT(
       model_tokenizer->Vectorize(u"City Number Phone Address Card Last Zip "),
       testing::ElementsAre(TokenId(3), TokenId(2), TokenId(8), TokenId(1),
