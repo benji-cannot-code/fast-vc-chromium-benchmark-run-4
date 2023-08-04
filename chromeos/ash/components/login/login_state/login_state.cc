2aab848324e5514738191cf8c6aa1234221f1540
Emmanuel Arias Soto <eariassoto@google.com>
2023-08-04 07:44:24 +0000

    mgs: Rename login_state::IsPublicSessionUser
    * The ChromeOS "public session" profile was rebranded to "managed
    guest session" a long time ago.
    * Using the proper name will help better maintain and debug the managed
    guest session profile type.
    
    Bug: b:281635686
    Change-Id: I114b45827ab9657c890ce961d7a1c2b8a95e7007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4746820
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179468}

--- chromeos/ash/components/login/login_state/login_state.cc
+++ chromeos/ash/components/login/login_state/login_state.cc
@@ -124,7 +124,7 @@ bool LoginState::IsGuestSessionUser() const {
   return logged_in_user_type_ == LOGGED_IN_USER_GUEST;
 }
 
-bool LoginState::IsPublicSessionUser() const {
+bool LoginState::IsManagedGuestSessionUser() const {
   return logged_in_user_type_ == LOGGED_IN_USER_PUBLIC_ACCOUNT;
 }
 
@@ -139,7 +139,7 @@ bool LoginState::IsChildUser() const {
 bool LoginState::UserHasNetworkProfile() const {
   if (!IsUserLoggedIn())
     return false;
-  return !IsPublicSessionUser();
+  return !IsManagedGuestSessionUser();
 }
 
 bool LoginState::IsUserAuthenticated() const {
