23ed8c9d3130bdb98b4ce0a7efe6d4d7cdf6217b
Jérôme Lebel <jlebel@chromium.org>
2023-08-04 13:52:53 +0000

    [iOS][UNO] Update enterprise strings
    Adds the following strings:
    IDS_IOS_ENTERPRISE_SIGNED_OUT_MESSAGE_WITH_UNO
    IDS_IOS_ENTERPRISE_SYNC_DISABLED_MESSAGE_WITH_UNO
    IDS_IOS_ENTERPRISE_SYNC_DISABLED_TITLE_WITH_UNO
    
    Those strings are used in the enterprise dialog when sign-in or sync
    is disabled. Those strings are under kReplaceSyncPromosWithSignInPromos
    flag.
    
    Sign-in disabled:
    https://drive.google.com/file/d/13m5l-r201HNNc7QTsWgJexfRENIsW3VX/view?usp=drive_link&resourcekey=0-ZMq1r12rCNmHtdHWV283tQ
    Sync disabled:
    https://drive.google.com/file/d/1q0DqYuusq4E-CRjlsAv90ZZJsr3yYQUu/view?usp=drive_link&resourcekey=0-jBexUUh8J2mdGRxx_Ijeyg
    
    Bug: 1468536
    Change-Id: I9885af6b40bcb5082601c283528e84bbd9bc3416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4751783
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179568}

--- ios/chrome/browser/ui/authentication/enterprise/enterprise_prompt/enterprise_prompt_view_controller.mm
+++ ios/chrome/browser/ui/authentication/enterprise/enterprise_prompt/enterprise_prompt_view_controller.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/authentication/enterprise/enterprise_prompt/enterprise_prompt_view_controller.h"
 
+#import "base/feature_list.h"
+#import "components/sync/base/features.h"
 #import "ios/chrome/grit/ios_google_chrome_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
@@ -76,16 +78,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Updates the view with force sign out informations.
 - (void)setupForForceSignOut {
   self.titleString = l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT);
-  self.subtitleString =
-      l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_MESSAGE);
+  if (base::FeatureList::IsEnabled(
+          syncer::kReplaceSyncPromosWithSignInPromos)) {
+    self.subtitleString =
+        l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_MESSAGE_WITH_UNO);
+  } else {
+    self.subtitleString =
+        l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_MESSAGE);
+  }
 }
 
 // Updates the view with sync disabled informations.
 - (void)setupForSyncDisabled {
-  self.titleString =
-      l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SYNC_DISABLED_TITLE);
-  self.subtitleString =
-      l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SYNC_DISABLED_MESSAGE);
+  if (base::FeatureList::IsEnabled(
+          syncer::kReplaceSyncPromosWithSignInPromos)) {
+    self.titleString =
+        l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SYNC_DISABLED_TITLE_WITH_UNO);
+    self.subtitleString = l10n_util::GetNSString(
+        IDS_IOS_ENTERPRISE_SYNC_DISABLED_MESSAGE_WITH_UNO);
+  } else {
+    self.titleString =
+        l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SYNC_DISABLED_TITLE);
+    self.subtitleString =
+        l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SYNC_DISABLED_MESSAGE);
+  }
 }
 
 @end
