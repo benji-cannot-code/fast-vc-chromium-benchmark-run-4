38794e69379783451c7a3f919ab456e64ea694b5
Irem Uguz <iremuguz@google.com>
2023-08-04 09:41:59 +0000

    support-tool: Include DataCollector names in error messages
    Include the name of DataCollector when creating SupportToolError
    messages.
    
    Bug: b:232198953
    Change-Id: I32f17b1409a29594b23252357f68979ed96aa500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4732891
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179500}

--- chrome/browser/support_tool/system_log_source_data_collector_adaptor.cc
+++ chrome/browser/support_tool/system_log_source_data_collector_adaptor.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/strings/strcat.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -182,8 +183,9 @@ void SystemLogSourceDataCollectorAdaptor::OnFilesWritten(
     DataCollectorDoneCallback on_exported_callback,
     bool success) {
   if (!success) {
-    SupportToolError error = {SupportToolErrorCode::kDataCollectorError,
-                              "Failed on data export."};
+    SupportToolError error = {
+        SupportToolErrorCode::kDataCollectorError,
+        base::StrCat({GetName(), "failed on data export."})};
     std::move(on_exported_callback).Run(error);
     return;
   }
