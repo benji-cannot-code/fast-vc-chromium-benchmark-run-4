d27e9267db00c001e9e41773598107f6fa10a97b
Collin Baker <collinbaker@chromium.org>
2023-08-04 22:28:32 +0000

    Turn off deny(warnings) in gnrt
    This was added to mirror the behavior from the GN build. However, with
    no CI build coverage, it's minimally useful, and it's hard to turn off
    for local dev. New lints can be added in new rustc versions, so it can
    break e.g. tools/rust/gnrt_stdlib.py usage unexpectedly.
    
    If we decide we still want warnings-as-errors, we should do so by
    passing a rustc flag in the python wrapper scripts; this makes local
    development easier since it can easily be turned off.
    
    Bug: None
    Change-Id: I0258d6f8cbfe8b2cb09e2ceaeaa5581cbd4156ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4753838
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Auto-Submit: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179888}

--- tools/crates/gnrt/main.rs
+++ tools/crates/gnrt/main.rs
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![deny(warnings)]
 #![forbid(unsafe_op_in_unsafe_fn)]
 #![forbid(unsafe_code)]
 
