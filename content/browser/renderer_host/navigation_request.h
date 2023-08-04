a77aeac317310f32e2a12c245bca7934909a2bc0
Kouhei Ueno <kouhei@chromium.org>
2023-08-04 00:19:41 +0000

    [LCPP] Pageload.Clients.LCPP.PaintTiming.NavigationOrActivationToFirstContentfulPaint UMA
    This CL extends LcpCriticalPathPredictorPageLoadMetricsObserver so that
    it would emit the Pageload.Clients.LCPP.PaintTiming.NavigationOrActivationToFirstContentfulPaint UMA.
    
    For the PLMO to know if the LCPP hint is available for a particular `NavigationRequest`, this CL introduces `NavigationHandle::GetLCPPNavigationHint`.
    
    Bug: 1419756
    Change-Id: Ie8496dd283570b1de9f1d28aebd53494ff53bb7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4741336
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179359}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -375,6 +375,8 @@ class CONTENT_EXPORT NavigationRequest
   void SetLCPPNavigationHint(
       const blink::mojom::LCPCriticalPathPredictorNavigationTimeHint& hint)
       override;
+  const blink::mojom::LCPCriticalPathPredictorNavigationTimeHintPtr&
+  GetLCPPNavigationHint() override;
   const net::HttpResponseHeaders* GetResponseHeaders() override;
   net::HttpResponseInfo::ConnectionInfo GetConnectionInfo() override;
   const absl::optional<net::SSLInfo>& GetSSLInfo() override;
