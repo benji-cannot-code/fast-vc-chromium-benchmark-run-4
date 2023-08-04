425213576731ae06d0b0038343f33912d5594627
Aryan Kaushik <aryankaushik2023@gmail.com>
2023-08-04 22:28:04 +0000

    [PolicyLogs] Make Log Severity visible on PolicyLogs
    Policy Logs severity details were only visible in export.
    These details could be useful in debugging issues.
    The commit makes them visible on the PolicyLogs page for easy access.
    
    Screenshot: https://drive.google.com/file/d/1_YQMdB55YVptYeHfNhmAaopp8ZIGoKeY/view?usp=sharing
    
    R=esalma@google.com
    
    Change-Id: Ie20adc861fb1e361d2efde988688438b2ac38fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4747285
    Commit-Queue: Aryan Kaushik <aryankaushik2023@gmail.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179887}

--- components/policy/resources/webui/logs/types.ts
+++ components/policy/resources/webui/logs/types.ts
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 export interface Log {
   logSource: string;
+  logSeverity: string;
   message: string;
   location: string;
   timestamp: string;
