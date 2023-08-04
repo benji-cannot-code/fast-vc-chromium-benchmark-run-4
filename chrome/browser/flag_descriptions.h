0db7e17567fd946b670465ffc4adf1e84b64adc1
Russ Hamilton <behamilton@google.com>
2023-08-04 15:40:58 +0000

    Add command line flag to enable Protected Audiences debugging
    Bug: 1467576
    Change-Id: I97e2d23566c1e1c01521091957571bb1609fa0ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705988
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179592}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1627,6 +1627,9 @@ extern const char kPrivacySandboxSettings4Description[];
 extern const char kPrivateAggregationDeveloperModeName[];
 extern const char kPrivateAggregationDeveloperModeDescription[];
 
+extern const char kProtectedAudiencesConsentedDebugTokenName[];
+extern const char kProtectedAudiencesConsentedDebugTokenDescription[];
+
 extern const char kPullToRefreshName[];
 extern const char kPullToRefreshDescription[];
 extern const char kPullToRefreshEnabledTouchscreen[];
