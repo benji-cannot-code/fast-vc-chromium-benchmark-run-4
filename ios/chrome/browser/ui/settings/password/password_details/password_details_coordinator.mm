91d2fad1d17a62c584f6b35062165f2b748ed13f
Rafał Godlewski <rgod@google.com>
2023-08-04 18:58:00 +0000

    [iOS] Add main coordinator for password sharing
    Adds new coordinator responsible for handling password sharing flow
    (fetching sharing recipients, sending shared passwords, dispatching
    child coordinators, e.g. family picker).
    
    Adds empty password sharing view with a spinner being displayed during
    fetching the recipients.
    
    Screen: http://screen/BP9YXGSgVTT737z
    
    Bug: 1463882
    Change-Id: I830d520632a968f874aac180850d1bb262210c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748621
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179742}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details_mediator.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details_mediator_delegate.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller.h"
-#import "ios/chrome/browser/ui/settings/password/password_sharing/family_picker_coordinator.h"
+#import "ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_coordinator.h"
 #import "ios/chrome/browser/ui/settings/utils/password_utils.h"
 #import "ios/chrome/common/ui/reauthentication/reauthentication_module.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -72,8 +72,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The action sheet coordinator, if one is currently being shown.
 @property(nonatomic, strong) ActionSheetCoordinator* actionSheetCoordinator;
 
-// Coordinator for family picker for password sharing.
-@property(nonatomic, strong) FamilyPickerCoordinator* familyPickerCoordinator;
+// Coordinator for the password sharing flow.
+@property(nonatomic, strong)
+    PasswordSharingCoordinator* passwordSharingCoordinator;
 
 @end
 
@@ -368,13 +369,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)onShareButtonPressed {
-  // TODO(crbug.com/1463882): Implement displaying appropriate view based on the
-  // family query and amount of credential groups.
-  DCHECK(!self.familyPickerCoordinator);
-  self.familyPickerCoordinator = [[FamilyPickerCoordinator alloc]
+  [self.passwordSharingCoordinator stop];
+  self.passwordSharingCoordinator = [[PasswordSharingCoordinator alloc]
       initWithBaseViewController:self.viewController
                          browser:self.browser];
-  [self.familyPickerCoordinator start];
+  [self.passwordSharingCoordinator start];
 }
 
 #pragma mark - PasswordDetailsMediatorDelegate
