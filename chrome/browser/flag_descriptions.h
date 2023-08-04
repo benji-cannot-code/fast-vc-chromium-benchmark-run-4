6fe75df5c7f24e6ad0e731bb5fa81a5b1bd822c7
Curt Clemens <cclem@google.com>
2023-08-04 18:41:23 +0000

    Add a feature flag to disable Cross-Device on Reven
    Add a flag to chrome://flags that will be used to toggle the entire
    Cross-Device feature suite (PhoneHub, Nearby Share, Smart Lock, etc).
    The flag defaults to enabling the suite for all boards except Reven.
    
    See go/disable-cross-device-on-cros-flex for a discussion on why we
    want to disable Cross-Device on Reven.
    
    A followup CL will check this flag and disable each of the features.
    
    Bug: b/260915415
    Change-Id: I89777aad18a151e3fa182c98634c32e733b5da7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4744373
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179731}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2990,6 +2990,9 @@ extern const char kAdaptiveChargingDescription[];
 extern const char kAdaptiveChargingForTestingName[];
 extern const char kAdaptiveChargingForTestingDescription[];
 
+extern const char kAllowCrossDeviceFeatureSuiteName[];
+extern const char kAllowCrossDeviceFeatureSuiteDescription[];
+
 extern const char kAllowDevtoolsInSystemUIName[];
 extern const char kAllowDevtoolsInSystemUIDescription[];
 
