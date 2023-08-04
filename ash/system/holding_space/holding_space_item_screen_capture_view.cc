68e27f51248b098df37f8b356453a95f2a045fad
David Black <dmblack@google.com>
2023-08-04 05:33:45 +0000

    Apply accent colors to holding space screen capture unpin action.
    When holding space screen captures are pinned and jelly is enabled,
    the unpin action button should have accent colors applied.
    
    New behavior -------------------------------------------
    
    Before - Pinned, Jelly On: http://shortn/_Av7EhY8dDb
    After - Pinned, Jelly On: http://shortn/_LHu4DkEsEM
    
    Unchanged behavior -------------------------------------
    
    Before - Unpinned, Jelly On: http://shortn/_qeuLZIy6g5
    After - Unpinned, Jelly On: http://shortn/_UKsajSs6zY
    
    Before - Pinned, Jelly Off: http://shortn/_UmGQYFMyVr
    After - Pinned, Jelly Off: http://shortn/_z7fEwFB7FN
    
    Before - Unpinned, Jelly Off: http://shortn/_lw5cHAobdb
    After - Unpinned, Jelly Off: http://shortn/_exzPZE7MKf
    
    --------------------------------------------------------
    
    Bug: b:285827094
    Change-Id: Ib9cd03ee07c9796abddc3cc85432eafdad751fdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750569
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179439}

--- ash/system/holding_space/holding_space_item_screen_capture_view.cc
+++ ash/system/holding_space/holding_space_item_screen_capture_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/holding_space/holding_space_util.h"
 #include "ash/system/tray/tray_constants.h"
 #include "base/functional/bind.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
@@ -122,7 +123,10 @@ HoldingSpaceItemScreenCaptureView::HoldingSpaceItemScreenCaptureView(
                       views::MinimumFlexSizeRule::kScaleToZero,
                       views::MaximumFlexSizeRule::kUnbounded)))
               .AddChild(
-                  CreatePrimaryActionBuilder(kPrimaryActionSize)
+                  CreatePrimaryActionBuilder(
+                      /*apply_accent_colors=*/chromeos::features::
+                          IsJellyEnabled(),
+                      /*min_size=*/kPrimaryActionSize)
                       .SetBackground(holding_space_util::CreateCircleBackground(
                           kColorAshShieldAndBase80))))
       .AddChild(views::Builder<views::View>()
