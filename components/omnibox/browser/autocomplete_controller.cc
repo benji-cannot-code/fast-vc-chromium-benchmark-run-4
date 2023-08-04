8c785327aeed9bcc29a777ad6fce5a342cb91576
Gauthier Ambard <gambard@chromium.org>
2023-08-04 08:47:27 +0000

    Revert "Recognize Clipboard Search suggestions as Search types."
    This reverts commit 2bb09e52d1747c1efae84bdb7db692da57b10641.
    
    Reason for revert: Breaks iOS test. See crbug.com/1470185
    
    Original change's description:
    > Recognize Clipboard Search suggestions as Search types.
    >
    > This change ensures Clipboard suggestions are correctly categorized as
    > either URL (CLIPBOARD_URL) or Search (CLIPBOARD_TEXT,
    > CLIPBOARD_IMAGE).
    >
    > The Search clipboard types feature custom description: (Text you
    > copied / Image you copied), but for the sake of completeness, this
    > change ensures that the keyword field is also populated.
    >
    > The change ensures that Clipboard text and image suggestions do not
    > show up if the text/image searches are not supported by the template
    > url, or are blocked by policy.
    >
    > Bug: 1468402
    > Change-Id: Id55fe397c9d58d6c7ae1c3bd7b05ad08058b431e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4744064
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1179409}
    
    Bug: 1468402
    Change-Id: Ia382683b582224bd667c8c95d4b172712ae06f02
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748463
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Gauthier Ambard <gambard@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179483}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -129,9 +129,7 @@ bool AutocompleteMatchHasCustomDescription(const AutocompleteMatch& match) {
     return true;
   }
   return match.type == AutocompleteMatchType::SEARCH_SUGGEST_ENTITY ||
-         match.type == AutocompleteMatchType::SEARCH_SUGGEST_PROFILE ||
-         match.type == AutocompleteMatchType::CLIPBOARD_TEXT ||
-         match.type == AutocompleteMatchType::CLIPBOARD_IMAGE;
+         match.type == AutocompleteMatchType::SEARCH_SUGGEST_PROFILE;
 }
 
 // Returns which rich autocompletion type, if any, had (or would have had for
