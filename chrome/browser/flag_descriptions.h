6f78aafdc9abd4294e26bd86fcbe70557ba139a3
Phillip Vanderlaat <pvanderlaat@google.com>
2023-08-04 01:05:22 +0000

    Add overview scroll layout for clamshell chrome flag.
    Add a flag to enable the existing cros-labs flag,
    kOverviewScrollLayoutForClamshellName.
    
    Test: manual
    Bug: None
    Change-Id: I23ce36bc03f8fe3eb78ed7b33e3fb26aebdaa59f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743783
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Auto-Submit: Phillip Vanderlaat <pvanderlaat@google.com>
    Commit-Queue: Phillip Vanderlaat <pvanderlaat@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179376}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3853,6 +3853,9 @@ extern const char kSnoopingProtectionDescription[];
 extern const char kSpeakOnMuteOptInNudgePrefsResetName[];
 extern const char kSpeakOnMuteOptInNudgePrefsResetDescription[];
 
+extern const char kOverviewScrollLayoutForClamshellName[];
+extern const char kOverviewScrollLayoutForClamshellDescription[];
+
 extern const char kSpectreVariant2MitigationName[];
 extern const char kSpectreVariant2MitigationDescription[];
 
