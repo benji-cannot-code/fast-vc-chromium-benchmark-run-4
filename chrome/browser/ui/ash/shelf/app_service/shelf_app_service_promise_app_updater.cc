60f606d47885445f926114f9548a281495ec671b
Vivian Pao <vpao@google.com>
2023-08-04 05:40:33 +0000

    [Promise Icons] Move and rename shelf_promise_app_updater.h/cc files
    Move shelf_promise_app_updater.h/cc files into the shelf/app_service
    directory and rename them accordingly to match the other files in the
    directory. This will allow a more sensible grouping of App Service
    files.
    
    Bug: b/290558499
    Change-Id: I28497847aa4412d3ab8e92d144be4880467f0f29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4751139
    Commit-Queue: Vivian Pao <vpao@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179442}

--- chrome/browser/ui/ash/shelf/app_service/shelf_app_service_promise_app_updater.cc
+++ chrome/browser/ui/ash/shelf/app_service/shelf_app_service_promise_app_updater.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/ash/shelf/shelf_promise_app_updater.h"
+#include "chrome/browser/ui/ash/shelf/app_service/shelf_app_service_promise_app_updater.h"
 
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
--- chrome/browser/ui/ash/shelf/app_service/shelf_app_service_promise_app_updater.h
+++ chrome/browser/ui/ash/shelf/app_service/shelf_app_service_promise_app_updater.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_SHELF_SHELF_PROMISE_APP_UPDATER_H_
-#define CHROME_BROWSER_UI_ASH_SHELF_SHELF_PROMISE_APP_UPDATER_H_
+#ifndef CHROME_BROWSER_UI_ASH_SHELF_APP_SERVICE_SHELF_APP_SERVICE_PROMISE_APP_UPDATER_H_
+#define CHROME_BROWSER_UI_ASH_SHELF_APP_SERVICE_SHELF_APP_SERVICE_PROMISE_APP_UPDATER_H_
 
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
@@ -37,4 +37,4 @@ class ShelfPromiseAppUpdater : public ShelfAppUpdater,
       promise_app_registry_cache_observation_{this};
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_SHELF_SHELF_PROMISE_APP_UPDATER_H_
+#endif  // CHROME_BROWSER_UI_ASH_SHELF_APP_SERVICE_SHELF_APP_SERVICE_PROMISE_APP_UPDATER_H_
