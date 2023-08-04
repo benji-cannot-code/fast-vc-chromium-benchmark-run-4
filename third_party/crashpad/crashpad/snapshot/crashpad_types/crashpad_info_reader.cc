1bc8cdd522a2667fafcaa972af55bb5451a05a3c
Rahul Yadav <rahul.yadav@samsung.com>
2023-08-04 19:33:50 +0000

    Use base::to_underlying in crashpad.
    After C++23 we should use base::to_underlying instead of static_cast
    to underlying types.
    
    This patch also deletes the unused file.
    
    Bug: None
    Change-Id: I1963c2506bbe9c7751bea66a2df867f0c6e2116d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4741046
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179771}

--- third_party/crashpad/crashpad/snapshot/crashpad_types/crashpad_info_reader.cc
+++ third_party/crashpad/crashpad/snapshot/crashpad_types/crashpad_info_reader.cc
@@ -17,9 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <type_traits>
 
+#include "base/types/cxx23_to_underlying.h"
 #include "build/build_config.h"
 #include "client/crashpad_info.h"
-#include "util/misc/as_underlying_type.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "util/win/traits.h"
@@ -34,13 +34,13 @@ namespace crashpad {
 namespace {
 
 void UnsetIfNotValidTriState(TriState* value) {
-  switch (AsUnderlyingType(*value)) {
-    case AsUnderlyingType(TriState::kUnset):
-    case AsUnderlyingType(TriState::kEnabled):
-    case AsUnderlyingType(TriState::kDisabled):
+  switch (base::to_underlying(*value)) {
+    case base::to_underlying(TriState::kUnset):
+    case base::to_underlying(TriState::kEnabled):
+    case base::to_underlying(TriState::kDisabled):
       return;
   }
-  LOG(WARNING) << "Unsetting invalid TriState " << AsUnderlyingType(*value);
+  LOG(WARNING) << "Unsetting invalid TriState " << base::to_underlying(*value);
   *value = TriState::kUnset;
 }
 
--- third_party/crashpad/crashpad/util/linux/proc_task_reader.cc
+++ third_party/crashpad/crashpad/util/linux/proc_task_reader.cc
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/types/cxx23_to_underlying.h"
 #include "util/file/directory_reader.h"
-#include "util/misc/as_underlying_type.h"
 
 namespace crashpad {
 
@@ -50,8 +50,8 @@ bool ReadThreadIDs(pid_t pid, std::vector<pid_t>* tids) {
 
     local_tids.push_back(tid);
   }
-  DCHECK_EQ(AsUnderlyingType(result),
-            AsUnderlyingType(DirectoryReader::Result::kNoMoreFiles));
+  DCHECK_EQ(base::to_underlying(result),
+            base::to_underlying(DirectoryReader::Result::kNoMoreFiles));
   DCHECK(!local_tids.empty());
 
   tids->swap(local_tids);
