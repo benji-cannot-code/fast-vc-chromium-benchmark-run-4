8d6d9cb7c5bfc4a5d88052ca126a88744a53f0da
Mustafa Acer <meacer@chromium.org>
2023-08-04 21:57:44 +0000

    [ios] Disable HTTPS-Upgrades feature in typed navigation tests
    This is a speculative fix for flaky omnibox typed navigation tests.
    
    Bug: 1469971
    Change-Id: I69f614ba48a75194c0abe3a7927833fad8d7de45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754272
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179849}

--- ios/chrome/browser/https_upgrades/typed_navigation_upgrade_tab_helper_egtest.mm
+++ ios/chrome/browser/https_upgrades/typed_navigation_upgrade_tab_helper_egtest.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
+#import "ios/components/security_interstitials/https_only_mode/feature.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/testing/embedded_test_server_handlers.h"
 #import "ios/web/common/features.h"
@@ -53,6 +54,8 @@ std::string GetURLWithoutScheme(const GURL& url) {
   AppLaunchConfiguration config;
   config.relaunch_policy = NoForceRelaunchAndResetState;
   config.features_enabled.push_back(omnibox::kDefaultTypedNavigationsToHttps);
+  config.features_disabled.push_back(
+      security_interstitials::features::kHttpsUpgrades);
   return config;
 }
 
