ee16c23bd8a7242fcc1c4ba1369aaf715964079a
Viktor Semeniuk <vsemeniuk@google.com>
2023-08-04 15:59:49 +0000

    Show regular matches together with PSL matches inside account chooser
    This CL allows to see all matching passwords together inside account
    chooser dialog.
    
    Fixed: 1468913
    Change-Id: I93a783a6b331416f3be84600be97531c358c667b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728114
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179624}

--- components/password_manager/core/browser/credential_manager_impl_unittest.cc
+++ components/password_manager/core/browser/credential_manager_impl_unittest.cc
@@ -1556,9 +1556,8 @@ TEST_P(CredentialManagerImplTest, ZeroClickWithPSLAndNormalCredentials) {
   store_->AddLogin(subdomain_form_);
 
   std::vector<GURL> federations = {GURL("https://google.com/")};
-  ExpectZeroClickSignInSuccess(CredentialMediationRequirement::kSilent, true,
-                               federations,
-                               CredentialType::CREDENTIAL_TYPE_FEDERATED);
+  ExpectZeroClickSignInFailure(CredentialMediationRequirement::kSilent, true,
+                               federations);
 }
 
 TEST_P(CredentialManagerImplTest, ZeroClickAfterMigratingHttpCredential) {
