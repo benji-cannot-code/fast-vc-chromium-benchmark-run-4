7dff70970e1d6caebdb70cfbd32cbbadce34f12a
Paul Adedeji <pauladedeji@google.com>
2023-08-04 15:58:10 +0000

    Revert "Strengthen the deprecation warnings for Non standard CSS appearance values"
    This reverts commit 8c55781d1f83582638b73e0bc557066adcd8b6ab.
    
    Reason for revert: Suspected culprit for "chromium/ci/Linux Tests" and "chromium/ci/linux-bfcache-rel" builder failures (more info at https://bugs.chromium.org/p/chromium/issues/detail?id=1470290)
    
    Original change's description:
    > Strengthen the deprecation warnings for Non standard CSS appearance values
    >
    > We replace the existing console message warning to be marked as a
    > deprecation and add a deprecation counter.
    >
    > Intent to Deprecate:
    > https://groups.google.com/a/chromium.org/g/blink-dev/c/gfxScdL8O3g/m/AiC-QvjPCgAJ
    >
    > Change-Id: I80ceb734e16d5f1ed57d4c41932cc38159cddf0a
    > Bug: 924486, 1469700
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721696
    > Commit-Queue: Di Zhang <dizhangg@chromium.org>
    > Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1179317}
    
    Bug: 924486, 1469700
    Change-Id: I09567f37816f57a732fc0427474b4eb3d38858d4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752756
    Owners-Override: Paul Adedeji <pauladedeji@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Owners-Override: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179622}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_color.h"
 #include "third_party/blink/renderer/core/css_value_keywords.h"
 #include "third_party/blink/renderer/core/dom/document.h"
-#include "third_party/blink/renderer/core/frame/deprecation/deprecation.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/style_property_shorthand.h"
@@ -3508,8 +3507,8 @@ void CountKeywordOnlyPropertyUsage(CSSPropertyID property,
   }
   switch (property) {
     case CSSPropertyID::kAppearance:
-      // TODO(crbug.com/924486): Remove CSS value slider-vertical
-      // and the associated warnings.
+      // TODO(crbug.com/924486): Remove CSS value slider-horizontal,
+      // slider-vertical and the associated warnings.
       if (value_id == CSSValueID::kSliderVertical ||
           (!RuntimeEnabledFeatures::RemoveNonStandardAppearanceValueEnabled() &&
            (value_id == CSSValueID::kInnerSpinButton ||
@@ -3525,15 +3524,13 @@ void CountKeywordOnlyPropertyUsage(CSSPropertyID property,
             value_id == CSSValueID::kSearchfieldCancelButton))) {
         if (const auto* document = context.GetDocument()) {
           document->AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
-              mojom::blink::ConsoleMessageSource::kDeprecation,
+              mojom::blink::ConsoleMessageSource::kOther,
               mojom::blink::ConsoleMessageLevel::kWarning,
               String("The keyword '") + getValueName(value_id) +
                   "' specified to an 'appearance' property is not "
                   "standardized. It will be removed in the future."));
-          Deprecation::CountDeprecation(
-              document->GetExecutionContext(),
-              WebFeature::kCSSValueAppearanceNonStandard);
         }
+        context.Count(WebFeature::kCSSValueAppearanceNonStandard);
       }
       [[fallthrough]];
       // This function distinguishes 'appearance' and '-webkit-appearance'
