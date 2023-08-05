9818e334d1626b8cc9da1a436de31c94995ab7b9
Mei Liang <meiliang@chromium.org>
2023-08-05 00:20:27 +0000

    🌱Disabled PasswordMigrationWarningExportFlowTest#testExportIntent on N+O devices
    Bug: 1470333
    Change-Id: I3717daf20d071d03845c9bea5968367769dfa569
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754492
    Auto-Submit: Mei Liang <meiliang@chromium.org>
    Owners-Override: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179941}

--- chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/PasswordMigrationWarningExportFlowTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/PasswordMigrationWarningExportFlowTest.java
@@ -33,6 +33,7 @@ import android.app.Activity;
 import android.app.Instrumentation.ActivityResult;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build.VERSION_CODES;
 import android.widget.Button;
 
 import androidx.test.espresso.intent.Intents;
@@ -49,6 +50,7 @@ import org.mockito.MockitoAnnotations;
 import org.chromium.base.FileUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -132,6 +134,8 @@ public class PasswordMigrationWarningExportFlowTest {
      */
     @Test
     @MediumTest
+    @DisableIf.Build(message = "https://crbug.com/1470333",
+            sdk_is_greater_than = VERSION_CODES.M, sdk_is_less_than = VERSION_CODES.P)
     public void testExportIntent() throws Exception {
         ReauthenticationManager.setApiOverride(ReauthenticationManager.OverrideState.AVAILABLE);
         ReauthenticationManager.setScreenLockSetUpOverride(
