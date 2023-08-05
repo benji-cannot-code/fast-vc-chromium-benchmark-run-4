4b8195b363a4363fc9051e47002c0f71d72a85a4
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-05 00:12:41 +0000

    [iOS] ActionCoordinator is stopped after dismiss
    According to ChromeCoordinator invariant, all coordinators should be
    stopped before being deallocated. This CL ensures that whatever the user chose, the coordinator is stopped; whatever button they tap.
    
    Bug: 1466233
    Change-Id: Ie2650c7f4364bedff084f9d91fd0a7941a56e202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4700846
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179936}

--- ios/chrome/browser/ui/text_fragments/text_fragments_coordinator.mm
+++ ios/chrome/browser/ui/text_fragments/text_fragments_coordinator.mm
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state.h"
 #import "ios/web/public/web_state_observer_bridge.h"
 #import "ui/base/l10n/l10n_util.h"
+#import "ui/strings/grit/ui_strings.h"
 #import "url/gurl.h"
 
 @interface TextFragmentsCoordinator () <DependencyInstalling,
@@ -95,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                   base::RecordAction(
                       base::UserMetricsAction("TextFragments.Menu.Removed"));
                   [weakSelf.mediator removeTextFragmentsInWebState:webState];
+                  [weakSelf dismissActionSheet];
                 }
                  style:UIAlertActionStyleDestructive];
   [self.actionSheet
@@ -127,6 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                             sourceRect:rect];
 
                   [handler shareHighlight:command];
+                  [weakSelf dismissActionSheet];
                 }
                  style:UIAlertActionStyleDefault];
   [self.actionSheet
@@ -142,8 +145,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                                commandWithURLFromChrome:
                                                    GURL(shared_highlighting::
                                                             kLearnMoreUrl)]];
+                  [weakSelf dismissActionSheet];
                 }
                  style:UIAlertActionStyleDefault];
+  [self.actionSheet addItemWithTitle:l10n_util::GetNSString(IDS_APP_CANCEL)
+                              action:^{
+                                [weakSelf dismissActionSheet];
+                              }
+                               style:UIAlertActionStyleCancel];
   [self.actionSheet start];
 }
 
@@ -156,9 +165,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeCoordinator methods
 
 - (void)stop {
-  if ([self.actionSheet isVisible]) {
-    [self.actionSheet stop];
-  }
+  [self dismissActionSheet];
   // Reset this observer manually. We want this to go out of scope now, ensuring
   // it detaches before `browser` and its WebStateList get destroyed.
   _dependencyInstallerBridge.reset();
@@ -168,9 +175,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)webState:(web::WebState*)webState
     didStartNavigation:(web::NavigationContext*)navigationContext {
-  if ([self.actionSheet isVisible]) {
-    [self.actionSheet stop];
-  }
+  [self dismissActionSheet];
+}
+
+#pragma mark - Private
+
+- (void)dismissActionSheet {
+  [self.actionSheet stop];
+  self.actionSheet = nil;
 }
 
 @end
