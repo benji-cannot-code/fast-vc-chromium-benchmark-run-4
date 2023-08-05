de1f7bcde3fd87f226603a213f5b7ceadb98c1c5
Osama Fathy <osamafathy@google.com>
2023-08-05 18:02:35 +0000

    OOBE: Remove remaining `std::toupper()` usages in `ash/login`
    Bug: 1361094
    Change-Id: Ibc1cddf855e31dd7cb07863613477be51dae8e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754852
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179989}

--- chrome/browser/ash/login/oobe_metrics_helper.cc
+++ chrome/browser/ash/login/oobe_metrics_helper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/login/oobe_screen.h"
 #include "chrome/browser/ui/webui/ash/login/enrollment_screen_handler.h"
@@ -49,7 +50,7 @@ std::string GetUmaLegacyScreenName(const OobeScreenId& screen_id) {
       break;
     }
   }
-  uma_name[0] = std::toupper(uma_name[0]);
+  uma_name[0] = base::ToUpperASCII(uma_name[0]);
   return uma_name;
 }
 
@@ -85,7 +86,7 @@ void OobeMetricsHelper::OnScreenExited(OobeScreenId screen,
 
   // Use for this histogram real screen names.
   std::string screen_name = screen.name;
-  screen_name[0] = std::toupper(screen_name[0]);
+  screen_name[0] = base::ToUpperASCII(screen_name[0]);
   std::string histogram_name_with_reason =
       kUmaStepCompletionTimeByExitReasonName + screen_name + "." + exit_reason;
 
