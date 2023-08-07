9d09eaa22ebf37053bf3ff3914584d4a1acab1c3
Peter Kasting <pkasting@chromium.org>
2023-08-07 14:33:15 +0000

    Use new facilities for base::expected: components/password_manager/
    Bug: 1424513
    Change-Id: Ic4cbd4f5f271d2cb4bec7a0e1730a3cb82f9cc31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755209
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180284}

--- components/password_manager/services/csv_password/csv_password_parser_impl_unittest.cc
+++ components/password_manager/services/csv_password/csv_password_parser_impl_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/gmock_expected_support.h"
 #include "base/test/task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -115,19 +116,16 @@ TEST_F(CSVPasswordParserImplTest, ParseFileMissingFields) {
       R"(Display Name,   ,Login,Secret Question,Password,URL,Timestamp
         :)        Bob,ABCD!,blabla,https://example.org,132)";
 
-  ParseCSV(raw_csv, base::BindLambdaForTesting(
-                        [&](mojom::CSVPasswordSequencePtr sequence) {
-                          EXPECT_TRUE(sequence);
-                          EXPECT_EQ(1u, sequence->csv_passwords.size());
-                          EXPECT_EQ("blabla",
-                                    sequence->csv_passwords[0].GetUsername());
-                          EXPECT_EQ("132",
-                                    sequence->csv_passwords[0].GetPassword());
-                          ASSERT_FALSE(
-                              sequence->csv_passwords[0].GetURL().has_value());
-                          EXPECT_EQ(
-                              "", sequence->csv_passwords[0].GetURL().error());
-                        }));
+  ParseCSV(
+      raw_csv,
+      base::BindLambdaForTesting([&](mojom::CSVPasswordSequencePtr sequence) {
+        EXPECT_TRUE(sequence);
+        EXPECT_EQ(1u, sequence->csv_passwords.size());
+        EXPECT_EQ("blabla", sequence->csv_passwords[0].GetUsername());
+        EXPECT_EQ("132", sequence->csv_passwords[0].GetPassword());
+        EXPECT_THAT(sequence->csv_passwords[0].GetURL(),
+                    base::test::ErrorIs(""));
+      }));
 }
 
 }  // namespace password_manager
--- components/password_manager/services/csv_password/public/mojom/csv_password_parser_traits.h
+++ components/password_manager/services/csv_password/public/mojom/csv_password_parser_traits.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PASSWORD_MANAGER_SERVICES_CSV_PASSWORD_PUBLIC_MOJOM_CSV_PASSWORD_PARSER_TRAITS_H_
 
 #include "base/types/expected.h"
+#include "base/types/expected_macros.h"
 #include "components/password_manager/core/browser/import/csv_password.h"
 #include "components/password_manager/services/csv_password/public/mojom/csv_password_parser.mojom.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
@@ -35,9 +36,7 @@ struct StructTraits<password_manager::mojom::CSVPasswordDataView,
   }
   static absl::optional<std::string> invalid_url(
       const password_manager::CSVPassword& r) {
-    base::expected<GURL, std::string> expected_url = r.GetURL();
-    if (!expected_url.has_value())
-      return expected_url.error();
+    RETURN_IF_ERROR(r.GetURL());
     return absl::nullopt;
   }
   static const std::string& username(const password_manager::CSVPassword& r) {
