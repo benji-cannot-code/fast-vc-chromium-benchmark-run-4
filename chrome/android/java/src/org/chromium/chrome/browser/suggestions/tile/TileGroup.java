7a742726ef45ca3e0c170b04becde965926b748b
Ari Chivukula <arichiv@chromium.org>
2023-08-07 23:19:46 +0000

    Remove expired histograms NewTabPage.ContentSuggestions.CountOnNtpOpened*
    NewTabPage.ContentSuggestions.CountOnNtpOpenedIfVisible.Articles.Prefetched.Offline2
    expired in M90. Let's remove it.
    
    OBSOLETE_HISTOGRAMS=Deprecated in M90 and removed in crrev.com/c/4748439
    
    Bug: 1384900
    Change-Id: Ibdabada2a849ecbc11852f169eda1ca71b765135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748439
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180591}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/TileGroup.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/TileGroup.java
@@ -391,8 +391,7 @@ public class TileGroup implements MostVisitedSites.Observer {
 
         if (!dataChanged) return;
 
-        mOfflineModelObserver.updateAllSuggestionsOfflineAvailability(
-                /* reportPrefetchedSuggestionsCount = */ false);
+        mOfflineModelObserver.updateAllSuggestionsOfflineAvailability();
 
         if (countChanged) mObserver.onTileCountChanged();
 
