36b38560c910d5eb914c6fe33baf706bc272b13c
Claudio M <claudiomagni@google.com>
2023-08-07 01:48:52 +0000

    language_packs: Remove UMA metrics from Mojo calls
    We are already tracking all relevant metrics in LanguagePackManager.
    These histograms are now duplicated and we don't need them anymore.
    
    I will follow up with another CL in two milestones to remove the
    histograms from UMA.
    
    Bug: b:231288152
    Change-Id: I5bc1332c04888393321785e8d408369d21c22554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750805
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180099}

--- chromeos/ash/components/language_packs/language_packs_impl.cc
+++ chromeos/ash/components/language_packs/language_packs_impl.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/metrics/histogram_functions.h"
 #include "chromeos/ash/components/language_packs/language_packs_impl.h"
 
 #include "base/no_destructor.h"
@@ -79,9 +78,6 @@ void OnOperationComplete(LanguagePacksImpl::GetPackInfoCallback mojo_callback,
     info->path = pack_result.path;
   }
 
-  base::UmaHistogramEnumeration("ChromeOS.LanguagePacks.Mojo.PackStateResponse",
-                                info->pack_state);
-
   std::move(mojo_callback).Run(std::move(info));
 }
 
@@ -95,9 +91,6 @@ void OnInstallBasePackComplete(
   if (pack_result.pack_state == PackResult::StatusCode::kInstalled) {
     info->path = pack_result.path;
   }
-
-  base::UmaHistogramEnumeration(
-      "ChromeOS.LanguagePacks.Mojo.BasePackStateResponse", info->pack_state);
 
   std::move(mojo_callback).Run(std::move(info));
 }
@@ -120,8 +113,6 @@ void LanguagePacksImpl::BindReceiver(
 void LanguagePacksImpl::GetPackInfo(FeatureId feature_id,
                                     const std::string& language,
                                     GetPackInfoCallback mojo_callback) {
-  base::UmaHistogramEnumeration(
-      "ChromeOS.LanguagePacks.Mojo.GetPackInfo.Feature", feature_id);
 
   LanguagePackManager* lp = LanguagePackManager::GetInstance();
   const absl::optional<std::string> pack_id =
@@ -141,9 +132,6 @@ void LanguagePacksImpl::GetPackInfo(FeatureId feature_id,
 void LanguagePacksImpl::InstallPack(FeatureId feature_id,
                                     const std::string& language,
                                     InstallPackCallback mojo_callback) {
-  base::UmaHistogramEnumeration(
-      "ChromeOS.LanguagePacks.Mojo.InstallPack.Feature", feature_id);
-
   LanguagePackManager* lp = LanguagePackManager::GetInstance();
   const absl::optional<std::string> pack_id =
       ConvertMojoFeatureToPackId(feature_id);
@@ -161,9 +149,6 @@ void LanguagePacksImpl::InstallPack(FeatureId feature_id,
 
 void LanguagePacksImpl::InstallBasePack(FeatureId feature_id,
                                         InstallBasePackCallback mojo_callback) {
-  base::UmaHistogramEnumeration(
-      "ChromeOS.LanguagePacks.Mojo.InstallBasePack.Feature", feature_id);
-
   LanguagePackManager* lp = LanguagePackManager::GetInstance();
   const absl::optional<std::string> pack_id =
       ConvertMojoFeatureToPackId(feature_id);
