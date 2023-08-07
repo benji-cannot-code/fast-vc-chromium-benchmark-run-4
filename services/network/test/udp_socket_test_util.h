f212b3508bf0530cd281b28b2a62965a78d6cb5a
Andrew Rayskiy <greengrape@google.com>
2023-08-07 21:20:40 +0000

    Replace AsyncWaiter-s with TestFuture<> in services/network/
    Bug: 1456835
    Change-Id: Ibf980e447d0202210ecee72734f8c1c974cde1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750927
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180545}

--- services/network/test/udp_socket_test_util.h
+++ services/network/test/udp_socket_test_util.h
@@ -12,12 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/span.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "base/run_loop.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "net/base/ip_endpoint.h"
-#include "net/base/net_errors.h"
-#include "services/network/public/mojom/udp_socket.mojom-test-utils.h"
 #include "services/network/public/mojom/udp_socket.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -45,7 +43,7 @@ class UDPSocketTestHelper {
   int LeaveGroupSync(const net::IPAddress& group_address);
 
  private:
-  std::unique_ptr<mojom::UDPSocketAsyncWaiter> socket_;
+  const raw_ref<mojom::UDPSocket> socket_;
 };
 
 // An implementation of mojom::UDPSocketListener that records received results.
