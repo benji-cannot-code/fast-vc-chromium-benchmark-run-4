261b5e39d304a7fedead3b651cfa1f09aaeeeaff
Hidehiko Abe <hidehiko@chromium.org>
2023-08-07 04:54:34 +0000

    Ignore invalid range of selection text.
    Currently, in some edge case, it looks like selection text range is
    outside of surrounding text.
    Though, it is not yet clear what's the root cause.
    
    As a mitigation, this CL ignores the updates. If correct update
    is coming following the probrematic event, the state will be resolved.
    
    BUG=1457178
    TEST=Tryjob
    
    Change-Id: I3159025789c2e9ee2788b1c4fa30979fd14ad4e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750980
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180128}

--- ui/ozone/platform/wayland/host/wayland_input_method_context.cc
+++ ui/ozone/platform/wayland/host/wayland_input_method_context.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/containers/contains.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/i18n/char_iterator.h"
@@ -406,6 +407,18 @@ void WaylandInputMethodContext::SetSurroundingText(
     const gfx::Range& selection_range,
     const absl::optional<GrammarFragment>& fragment,
     const absl::optional<AutocorrectInfo>& autocorrect) {
+  if (!selection_range.IsBoundedBy(text_range)) {
+    // There seems some edge case that selection_range is outside of text_range.
+    // In the case we ignore it temporarily, wishing the next event will
+    // update the tracking correctly.
+    // See also crbug.com/1457178.
+    LOG(ERROR) << "selection_range is not bounded by text_range: "
+               << selection_range.ToString() << ", " << text_range.ToString();
+    // Make a crash report for further investigation in the future.
+    base::debug::DumpWithoutCrashing();
+    return;
+  }
+
   size_t utf16_offset = text_range.GetMin();
   surrounding_text_tracker_.Update(text, utf16_offset, selection_range);
 
