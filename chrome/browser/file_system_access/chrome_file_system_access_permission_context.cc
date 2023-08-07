8fd3189d068a490a09a58cc1a4167d195b0fdd4c
Austin Sullivan <asully@chromium.org>
2023-08-07 19:41:43 +0000

    FSA: Use std::string_view
    base::StringPiece is now deprecated in favor of std::string_view.
    See https://crbug.com/691162
    
    This CL has no functional changes. Just wanted to try out the
    new best practice
    
    Change-Id: I3f246c489dd58e00912667aaa6cd0b93e8ccf9bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754913
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180487}

--- chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
+++ chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "base/base_paths.h"
@@ -19,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/strcat.h"
-#include "base/strings/string_piece.h"
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
@@ -487,13 +487,13 @@ std::string GenerateLastPickedDirectoryKey(const std::string& id) {
                     : base::StrCat({kCustomLastPickedDirectoryKey, "-", id});
 }
 
-base::StringPiece PathAsPermissionKey(const base::FilePath& path) {
-  return base::StringPiece(
+std::string_view PathAsPermissionKey(const base::FilePath& path) {
+  return std::string_view(
       reinterpret_cast<const char*>(path.value().data()),
       path.value().size() * sizeof(base::FilePath::CharType));
 }
 
-base::StringPiece GetGrantKeyFromGrantType(GrantType type) {
+std::string_view GetGrantKeyFromGrantType(GrantType type) {
   return type == GrantType::kWrite ? kPermissionWritableKey
                                    : kPermissionReadableKey;
 }
@@ -893,7 +893,7 @@ class ChromeFileSystemAccessPermissionContext::PermissionGrantImpl
     std::move(callback).Run(outcome);
   }
 
-  base::StringPiece GetKey() const {
+  std::string_view GetKey() const {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     return PathAsPermissionKey(path_);
   }
