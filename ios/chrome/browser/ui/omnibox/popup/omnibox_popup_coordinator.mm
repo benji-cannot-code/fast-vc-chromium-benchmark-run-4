0aa2a9ae2fcdf572b030e3af0849b6b6b7ced61b
Christian Xu <christianxu@google.com>
2023-08-07 10:06:40 +0000

    [Omnibox] Add logging for steady state omnibox position
    Add logging for the preferred steady state (unfocused) omnibox position.
    On iOS users can choose between top and bottom omnibox. Log the omnibox
    position in omnibox event to measure the impact on omnibox usage.
    
    Bug: 1468484
    Change-Id: I938dd3484b1a5967442343934e38294f196265db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4753575
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180199}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_coordinator.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_coordinator.mm
@@ -167,6 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                  popupViewController:self.popupViewController
                    layoutGuideCenter:LayoutGuideCenterForBrowser(self.browser)
                            incognito:isIncognito];
+  self.mediator.prefService = self.browser->GetBrowserState()->GetPrefs();
 
   _popupView->SetMediator(self.mediator);
 
