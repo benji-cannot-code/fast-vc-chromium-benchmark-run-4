bdd7dd1eeeff444fb24eeb34114796fe805b028d
Orin Jaworski <orinj@google.com>
2023-08-07 18:15:46 +0000

    [omnibox][webui] Resize popup widget dynamically with web content
    This CL changes some CSS styles and some native Views presenter logic
    to get the omnibox popup resizing dynamically according to the size
    of the autocomplete result set. It's somewhat better than the fixed
    size with a lot of blank space, and it may be useful to be able to
    resize as done here, but some problems still remain. Of note:
    - Visual updates aren't synchronized between the browser UI/Views
      process and the webui renderer process. More detail added in a TODO.
    - The bottom isn't rounded properly, and some extra bits of html/body
      can sometimes be rendered over frame decorations.
    
    Some of these issues will require a fundamentally different approach
    to the webui popup rendering, which will be explored in future CLs,
    but this CL might be as good as we're likely to get with a hybrid
    rendering approach involving webui content plus views frame decoration.
    
    Bug: 1396174
    Change-Id: Ie3785be010a9ebc8733e70ed4d0849bb5a656dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742972
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180429}

--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_PRESENTER_H_
 
 #include "base/memory/weak_ptr.h"
+#include "content/public/browser/render_frame_host.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
@@ -43,12 +45,16 @@ class OmniboxPopupPresenter : public views::WebView,
   // Get the handler for communicating with the WebUI interface.
   RealboxHandler* GetHandler();
 
+  // views::WebView
+  void FrameSizeChanged(content::RenderFrameHost* render_frame_host,
+                        const gfx::Size& frame_size) override;
+
   // views::WidgetObserver:
   void OnWidgetDestroyed(views::Widget* widget) override;
 
   // Returns the target popup bounds in screen coordinates based on the bounds
-  // of |location_bar_view_|.
-  gfx::Rect GetTargetBounds() const;
+  // of `location_bar_view_` and given preferred size `start_height`.
+  gfx::Rect GetTargetBounds(int start_height) const;
 
  private:
   friend class OmniboxPopupViewWebUITest;
