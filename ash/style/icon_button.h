be83cc54a7f78e47c5fcb1a216d824c785c9908c
Xiaodan Zhu <zxdan@chromium.org>
2023-08-07 18:27:41 +0000

    cros_next: update toggled icon on state changes
    IconButton overlooked the toggled state change caused by button
    disabled behavior and enabled state change. When the button behavior
    is `kCanDisplayDisabledToggleValue` the button should show toggled
    icon when it is toggled regardless of the enabled state. Otherwise,
    when the button is disabled, it should show untoggled icon.
    
    This cl updates the button icon on enabled state change and button
    behavior change.
    
    Bug: b:294564287
    Change-Id: I06fd4e35920321649d770fc6ff79f5d1a047ccaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752584
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180440}

--- ash/style/icon_button.h
+++ ash/style/icon_button.h
@@ -99,11 +99,9 @@ class ASH_EXPORT IconButton : public views::ImageButton {
 
   bool toggled() const { return toggled_; }
 
-  void set_button_behavior(DisabledButtonBehavior button_behavior) {
-    button_behavior_ = button_behavior;
-  }
-
   void set_delegate(Delegate* delegate) { delegate_ = delegate; }
+
+  void SetButtonBehavior(DisabledButtonBehavior button_behavior);
 
   // Sets the vector icon of the button, it might change on different `toggled_`
   // states.
@@ -151,7 +149,9 @@ class ASH_EXPORT IconButton : public views::ImageButton {
 
  protected:
   void UpdateBackground();
-  void UpdateVectorIcon(bool icon_changed = false);
+  void UpdateVectorIcon(bool color_changes_only = false);
+
+  void OnEnabledStateChanged();
 
   // Gets the background color of the icon button.
   SkColor GetBackgroundColor() const;
