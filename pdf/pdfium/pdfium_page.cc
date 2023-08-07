e6c40cb662e5cbe39c02f986b97ddaaae8a960ab
Ramin Halavati <rhalavati@chromium.org>
2023-08-07 06:05:13 +0000

    Rotate images in rotated PDFs before sending them to OCR.
    If a PDF page is marked as rotated, the images in that page are rotated
    before being sent to OCR service.
    
    Bug: 1469236
    Change-Id: I82d8981df86f0d7224886765589ed10dec551338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735395
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180140}

--- pdf/pdfium/pdfium_page.cc
+++ pdf/pdfium/pdfium_page.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 #include "ui/gfx/range/range.h"
+#include "ui/gfx/skbitmap_operations.h"
 
 using printing::ConvertUnitFloat;
 using printing::kPixelsPerInch;
@@ -1323,8 +1324,27 @@ void PDFiumPage::CalculateImages() {
         kBGRA_8888_SkColorType, kOpaque_SkAlphaType);
     const size_t row_bytes = FPDFBitmap_GetStride(bitmap.get());
     SkPixmap pixels(info, FPDFBitmap_GetBuffer(bitmap.get()), row_bytes);
-    if (image.image_data.tryAllocPixels(info, row_bytes))
-      image.image_data.writePixels(pixels);
+    if (!image.image_data.tryAllocPixels(info, row_bytes)) {
+      continue;
+    }
+    image.image_data.writePixels(pixels);
+
+    SkBitmapOperations::RotationAmount rotation;
+    switch (FPDFPage_GetRotation(page)) {
+      case 0:
+        continue;
+      case 1:
+        rotation = SkBitmapOperations::RotationAmount::ROTATION_90_CW;
+        break;
+      case 2:
+        rotation = SkBitmapOperations::RotationAmount::ROTATION_180_CW;
+        break;
+      case 3:
+        rotation = SkBitmapOperations::RotationAmount::ROTATION_270_CW;
+        break;
+    }
+
+    image.image_data = SkBitmapOperations::Rotate(image.image_data, rotation);
   }
 }
 
