7ad114b7ed0f7acf59f0a547587474b7786f5555
Ameur Hosni <ameurhosni@google.com>
2023-08-07 13:53:33 +0000

    [IOS] Add string for top/bottom omnibox preference setting.
    Bug: None
    Change-Id: Ibfdb27d3df362d058ae3fbe894554d6d3c05a732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4732933
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180264}

--- ios/chrome/browser/ui/settings/address_bar_preference/address_bar_preference_view_controller.mm
+++ ios/chrome/browser/ui/settings/address_bar_preference/address_bar_preference_view_controller.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/address_bar_preference/address_bar_preference_view_controller.h"
 
 #import "ios/chrome/browser/ui/settings/address_bar_preference/cells/address_bar_options_item.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
 namespace {
 
@@ -26,7 +28,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  self.title = @"Address Bar";
+  self.title = l10n_util::GetNSString(IDS_IOS_ADDRESS_BAR_SETTING);
   [self loadModel];
 }
 
@@ -51,8 +53,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   TableViewLinkHeaderFooterItem* addressBarTip =
       [[TableViewLinkHeaderFooterItem alloc]
           initWithType:ItemTypeOptionsContent];
-  addressBarTip.text =
-      @"You can also touch & hold the address bar to change the position.";
+  addressBarTip.text = l10n_util::GetNSString(IDS_IOS_ADDRESS_BAR_TIP_NOTE);
   [self.tableViewModel setFooter:addressBarTip
         forSectionWithIdentifier:SectionIdentifierContent];
 }
