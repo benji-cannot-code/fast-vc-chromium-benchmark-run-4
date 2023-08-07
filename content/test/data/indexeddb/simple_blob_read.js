3b205b072a742e4dad1d232361bed726ebf52f7b
Evan Stade <estade@chromium.org>
2023-08-07 17:42:03 +0000

    IndexedDB: fix browser test Bug941965Test
    Fixed: 1469760
    Change-Id: If123c0db739030e2efe758523e262f4faae5fcaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748990
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180390}

--- content/test/data/indexeddb/simple_blob_read.js
+++ content/test/data/indexeddb/simple_blob_read.js
@@ -6,11 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const DB = 'db';
 const STORE = 'store';
 const DATA = 'blob value';
-run();
 
 function run() {
   Object.assign(indexedDB.open(DB), {
-    unexpectedErrorCallback,
+    onerror: unexpectedErrorCallback,
     onupgradeneeded(e) {
       debug("Created object store.");
       e.target.result.createObjectStore(STORE, {
@@ -27,7 +26,7 @@ function run() {
           id: 'foo',
           blob: new Blob([DATA]),
         });
-      op.onerror = onerror;
+      op.onerror = unexpectedErrorCallback;
       op.onsuccess = () => {
         debug("Wrote blob.");
         idb.close();
@@ -40,14 +39,14 @@ function run() {
 function verify(e) {
   debug("Reading blob.");
   Object.assign(indexedDB.open(DB), {
-    onerror,
+    onerror: unexpectedErrorCallback,
     onsuccess(e) {
       const idb = /** @type IDBDatabase */ e.target.result;
       const op = idb
         .transaction(STORE, 'readonly')
         .objectStore(STORE)
         .get('foo');
-      op.onerror = onerror;
+      op.onerror = unexpectedErrorCallback;
       op.onsuccess = async e => {
         debug("Got blob.");
         idb.close();
