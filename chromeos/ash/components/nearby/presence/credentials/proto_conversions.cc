4b1861b000d8943a2c6980faa01b3e6d1221b366
Juliet Levesque <julietlevesque@google.com>
2023-08-07 17:53:55 +0000

    [Nearby Presence] Add mojo::DeviceType::kUnknown
    Adds mojo::DeviceType::kUnknown to align with nearby::internal::
    DeviceTye::DEVICE_TYPE_UNKNOWN, and removes mojom::DeviceType::
    kUnspecified.
    
    Bug: b/276642472
    Change-Id: Ia85496c498afe87d805ea8439aed91975ab09a7f
    Fixed: b/290792190
    Tested: All unit tests still pass
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679652
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180405}

--- chromeos/ash/components/nearby/presence/credentials/proto_conversions.cc
+++ chromeos/ash/components/nearby/presence/credentials/proto_conversions.cc
@@ -31,7 +31,7 @@ mojom::PresenceDeviceType DeviceTypeToMojom(
     ::nearby::internal::DeviceType device_type) {
   switch (device_type) {
     case ::nearby::internal::DeviceType::DEVICE_TYPE_UNKNOWN:
-      return mojom::PresenceDeviceType::kUnspecified;
+      return mojom::PresenceDeviceType::kUnknown;
     case ::nearby::internal::DeviceType::DEVICE_TYPE_PHONE:
       return mojom::PresenceDeviceType::kPhone;
     case ::nearby::internal::DeviceType::DEVICE_TYPE_TABLET:
@@ -45,7 +45,7 @@ mojom::PresenceDeviceType DeviceTypeToMojom(
     case ::nearby::internal::DeviceType::DEVICE_TYPE_CHROMEOS:
       return mojom::PresenceDeviceType::kChromeos;
     default:
-      return mojom::PresenceDeviceType::kUnspecified;
+      return mojom::PresenceDeviceType::kUnknown;
   }
 }
 
