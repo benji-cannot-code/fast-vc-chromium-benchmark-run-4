07c8b30bc17fbfc975102a370c734890e3b05000
Nacho Coloma <icoloma@google.com>
2023-08-07 13:24:55 +0000

    support-tool: Add SigninDataCollector
    This Data Collector writes the signin internals information into
    a temporary signin.json file, detecting and optionally redacting Gaia IDs and other PII.
    
    Bug: b/236118965
    Change-Id: I90146e5a68ae10750b17d6a1997dc85d5b4a1081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4437909
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Irem Uguz <iremuguz@google.com>
    Commit-Queue: Nacho Coloma <icoloma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180248}

--- chrome/browser/support_tool/support_tool_util.cc
+++ chrome/browser/support_tool/support_tool_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/feedback/system_logs/log_sources/performance_log_source.h"
 #include "chrome/browser/support_tool/data_collection_module.pb.h"
 #include "chrome/browser/support_tool/policy_data_collector.h"
+#include "chrome/browser/support_tool/signin_data_collector.h"
 #include "chrome/browser/support_tool/support_tool_handler.h"
 #include "chrome/browser/support_tool/system_log_source_data_collector_adaptor.h"
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -54,7 +55,8 @@ namespace {
 constexpr support_tool::DataCollectorType kDataCollectors[] = {
     support_tool::CHROME_INTERNAL,       support_tool::CRASH_IDS,
     support_tool::MEMORY_DETAILS,        support_tool::POLICIES,
-    support_tool::CHROMEOS_DEVICE_EVENT, support_tool::PERFORMANCE};
+    support_tool::CHROMEOS_DEVICE_EVENT, support_tool::PERFORMANCE,
+    support_tool::SIGN_IN_STATE};
 
 // Data collector types can only work on Chrome OS Ash.
 constexpr support_tool::DataCollectorType kDataCollectorsChromeosAsh[] = {
@@ -144,6 +146,10 @@ std::unique_ptr<SupportToolHandler> GetSupportToolHandler(
                 "or battery saver mode is active and details about current "
                 "battery state.",
                 std::make_unique<system_logs::PerformanceLogSource>()));
+        break;
+      case support_tool::SIGN_IN_STATE:
+        handler->AddDataCollector(
+            std::make_unique<SigninDataCollector>(profile));
         break;
 #if BUILDFLAG(IS_CHROMEOS_ASH)
       case support_tool::CHROMEOS_UI_HIERARCHY:
@@ -295,11 +301,13 @@ GetAllAvailableDataCollectorsOnDevice() {
   for (const auto& type : kDataCollectorsChromeosAsh) {
     data_collectors.push_back(type);
   }
-  if (crosapi::browser_util::IsLacrosEnabled())
+  if (crosapi::browser_util::IsLacrosEnabled()) {
     data_collectors.push_back(support_tool::CHROMEOS_LACROS);
+  }
   if (crosapi::BrowserManager::Get()->IsRunning() &&
-      crosapi::BrowserManager::Get()->GetFeedbackDataSupported())
+      crosapi::BrowserManager::Get()->GetFeedbackDataSupported()) {
     data_collectors.push_back(support_tool::CHROMEOS_CROS_API);
+  }
 #if BUILDFLAG(IS_CHROMEOS_WITH_HW_DETAILS)
   for (const auto& type : kDataCollectorsChromeosHwDetails) {
     data_collectors.push_back(type);
