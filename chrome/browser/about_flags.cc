243037ecd5e6b89bb20eb2c3479588130849e4c6
Din Nezametdinov <shaikhitdin@google.com>
2023-08-07 17:40:11 +0000

    [Passwords] Create a feature flag for UFF with intermediate values
    Create flag for Username First Flow with intermediate values support.
    
    DD: go/uff-k-10-d
    
    Bug: 1470586
    Change-Id: I37b1051eb06326cb4f964cde0c06c78749f2feee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755904
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Din Nezametdinov <shaikhitdin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180387}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4550,6 +4550,12 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAll,
      FEATURE_VALUE_TYPE(
          password_manager::features::kUsernameFirstFlowFallbackCrowdsourcing)},
+    {"username-first-flow-with-intermediate-values",
+     flag_descriptions::kUsernameFirstFlowWithIntermediateValuesName,
+     flag_descriptions::kUsernameFirstFlowWithIntermediateValuesDescription,
+     kOsAll,
+     FEATURE_VALUE_TYPE(
+         password_manager::features::kUsernameFirstFlowWithIntermediateValues)},
     {"enable-show-autofill-signatures",
      flag_descriptions::kShowAutofillSignaturesName,
      flag_descriptions::kShowAutofillSignaturesDescription, kOsAll,
