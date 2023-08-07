d84bb01e26fef5c910659b5d233908ee4e8d3a2a
jhtin <jhtin@chromium.org>
2023-08-07 04:46:53 +0000

    vietnamese input engine: enable by default
    Bug: b/251679480
    Change-Id: I16bcd23066407f77fbd1d997b36edf0664a8a435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4756400
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180125}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1131,7 +1131,7 @@ BASE_FEATURE(kFirmwareUpdateJelly,
 // Enables first party Vietnamese input method.
 BASE_FEATURE(kFirstPartyVietnameseInput,
              "FirstPartyVietnameseInput",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables or disables Floating Workspace feature on ChromeOS
 BASE_FEATURE(kFloatingWorkspace,
