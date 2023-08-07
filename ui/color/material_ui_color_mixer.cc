4ca7ed494949bb78e848bab27837b64874a84179
Elaine Chien <elainechien@chromium.org>
2023-08-07 22:15:29 +0000

    [ChromeRefresh] Respect text highlight color on Mac
    kColorTextSelectionBackground definition is moved from
    AddNativeCoreColorMixer to AddNativeUiColorMixer so that it is added
    after the Chrome Refresh colors. No change in existing behavior.
    
    
    Bug: 1459389
    Change-Id: I208d2e7ba20433b5922237920b0b6a0e22b9cb0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740401
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1180566}

--- ui/color/material_ui_color_mixer.cc
+++ ui/color/material_ui_color_mixer.cc
@@ -86,6 +86,7 @@ void AddMaterialUiColorMixer(ColorProvider* provider,
       kColorSysOnSurfaceSubtle};
   mixer[kColorHistoryClustersSidePanelCardSecondaryForeground] = {
       kColorSysOnSurfaceSubtle};
+  mixer[kColorLabelSelectionBackground] = {kColorTextSelectionBackground};
   mixer[kColorLinkForegroundDefault] = {kColorSysPrimary};
   mixer[kColorListItemFolderIconBackground] = {kColorSysTonalContainer};
   mixer[kColorListItemFolderIconForeground] = {kColorSysOnTonalContainer};
@@ -144,11 +145,12 @@ void AddMaterialUiColorMixer(ColorProvider* provider,
   mixer[kColorTextfieldOutlineDisabled] = {SK_ColorTRANSPARENT};
   mixer[kColorTextfieldOutlineInvalid] = {
       kColorTextfieldForegroundPlaceholderInvalid};
+  mixer[kColorTextfieldSelectionBackground] = {kColorTextSelectionBackground};
+  mixer[kColorTextSelectionBackground] = {kColorSysTonalContainer};
   mixer[kColorThemeColorPickerCheckmarkBackground] = {kColorSysOnSurface};
   mixer[kColorThemeColorPickerCheckmarkForeground] = {
       kColorSysInverseOnSurface};
   mixer[kColorThemeColorPickerOptionBackground] = {kColorSysNeutralContainer};
-  mixer[kColorTextfieldSelectionBackground] = {kColorSysTonalContainer};
   mixer[kColorToastBackground] = {kColorSysInverseSurface};
   mixer[kColorToastButton] = {kColorSysInversePrimary};
   mixer[kColorToastForeground] = {kColorSysInverseOnSurface};
