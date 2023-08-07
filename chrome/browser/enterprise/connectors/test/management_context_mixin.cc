59acb77827fcdb51c1a4a16b9b3ffea837672917
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-08-07 19:02:23 +0000

    Create cloud_binary_upload_service_browsertest.cc
    This CL adds a browser test file for CloudBinaryUploadService.
    The initial tests added check that the FCM token and access token
    are populated as expected with regard to how the device is managed.
    
    Bug: b/293581152
    Change-Id: Ia9ee7cd965c2ab33e881dbaaf6fba36e6184df1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742946
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180459}

--- chrome/browser/enterprise/connectors/test/management_context_mixin.cc
+++ chrome/browser/enterprise/connectors/test/management_context_mixin.cc
@@ -23,11 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_connectors::test {
 
-ManagementContext::ManagementContext() = default;
-ManagementContext::ManagementContext(ManagementContext&&) = default;
-ManagementContext& ManagementContext::operator=(ManagementContext&&) = default;
-ManagementContext::~ManagementContext() = default;
-
 // static
 std::unique_ptr<ManagementContextMixin> ManagementContextMixin::Create(
     InProcessBrowserTestMixinHost* host,
