286f8a4875957238343c8d6e4b844074bcdc9cf4
Kenichi Ishibashi <bashi@chromium.org>
2023-08-07 11:48:26 +0000

    Remove unused includes from http_stream_factory_job.cc
    Bug: N/A
    Change-Id: I2b01272b19a21dbf4dd8a150c19bd690a6a041cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754721
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180223}

--- net/http/http_stream_factory_job.cc
+++ net/http/http_stream_factory_job.cc
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
-#include "base/metrics/histogram_macros.h"
-#include "base/metrics/sparse_histogram.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
