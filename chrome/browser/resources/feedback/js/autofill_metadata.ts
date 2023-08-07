13e98367e7f0e931dd0e2a75bed5727c96ac0036
xiangdong kong <xiangdongkong@google.com>
2023-08-07 20:07:55 +0000

    feedback: Apply Jelly colors to autofill metadata info page
    Screenshots:
    - Light mode: http://screen/4CVtxYbu92H9WWj.png.
    - Dark mode: http://screen/9fMfnxetu6NxfTw.png.
    
    Bug: b:291922051
    Test: manually open the feedback dialog on vm and inspect.
    Change-Id: Iefe97bf034eaab2c3af9e34f04dd61e793a1c1b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754335
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180502}

--- chrome/browser/resources/feedback/js/autofill_metadata.ts
+++ chrome/browser/resources/feedback/js/autofill_metadata.ts
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import {configureJellyColors} from './jelly_colors.js';
 import {createLogsMapTable} from './logs_map_page.js';
 
 const dialogArgs: string = chrome.getVariableValue('dialogArguments');
@@ -55,4 +56,5 @@ window.onload = function() {
   }
 
   createAutofillMetadataTable();
+  configureJellyColors();
 };
