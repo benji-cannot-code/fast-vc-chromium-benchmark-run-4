beb952c6799f2a379979bec07ced959c6108fc77
Navneet Rathi <navneetrathi@microsoft.com>
2023-08-07 07:21:38 +0000

    Update aria label properly in picture in picture.
    After launching PiP from media controls, the narrator reads
    "Exit Picture in Picture" as "Enter Picture in Picture". This
    commit updates aria label properly.
    
    Bug: 1465268
    Change-Id: Ie408a4a7b94389da80d8ce9b00a9b2d8b169ea6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4712371
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Navneet Rathi <navneetrathi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1180156}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_picture_in_picture_button_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_picture_in_picture_button_element.cc
@@ -45,9 +45,9 @@ void MediaControlPictureInPictureButtonElement::UpdateDisplayType() {
       PictureInPictureController::IsElementInPictureInPicture(
           &To<HTMLVideoElement>(MediaElement()));
   SetClass("on", isInPictureInPicture);
-  UpdateOverflowString();
 
   UpdateAriaString(isInPictureInPicture);
+  UpdateOverflowString();
 
   MediaControlInputElement::UpdateDisplayType();
 }
@@ -105,6 +105,7 @@ void MediaControlPictureInPictureButtonElement::UpdateAriaString(
                 IDS_AX_MEDIA_ENTER_PICTURE_IN_PICTURE_BUTTON);
 
   setAttribute(html_names::kAriaLabelAttr, WTF::AtomicString(aria_string));
+  UpdateAriaLabel(aria_string);
 }
 
 }  // namespace blink
