5630834e3fee87b46779ab85fbff9bcb12547a5d
Kevin McNee <mcnee@chromium.org>
2023-08-07 19:22:36 +0000

    Don't assume NaCl for ChromeURLDataManagerWebUITrustedTypesTest
    The test ifdefs whether to include about:nacl based on the platform, but
    this may not match the value of enable_nacl. In particular, for devs
    compiling without NaCl support, running this test would fail.
    
    We now include it based on the build flag.
    
    Bug: None
    Change-Id: I692feb14900f1f10acc6e0e1dc3b166228c60430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752625
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180480}

--- chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
+++ chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/enterprise/browser/controller/fake_browser_dm_token_storage.h"
 #include "components/history_clusters/core/features.h"
+#include "components/nacl/common/buildflags.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/user_notes/user_notes_features.h"
 #include "content/public/browser/navigation_details.h"
@@ -469,11 +470,10 @@ static constexpr const char* const kChromeUrls[] = {
 #endif
 #if !BUILDFLAG(IS_MAC)
     "chrome://sandbox",
-// NaCl isn't supported on ARM64 Windows.
-#if !BUILDFLAG(IS_WIN) || !defined(ARCH_CPU_ARM64)
+#endif  // !BUILDFLAG(IS_MAC)
+#if BUILDFLAG(ENABLE_NACL)
     "chrome://nacl",
 #endif
-#endif  // !BUILDFLAG(IS_MAC)
 #if !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_CHROMEOS_LACROS)
     // TODO(https://crbug.com/1219651): this test is flaky on mac.
     "chrome://bluetooth-internals",
