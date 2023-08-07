b2197bc6ad467ac91fea13d1e1532db90c965fac
Vignesh Shenvi <vshenvi@google.com>
2023-08-07 19:13:00 +0000

    Add new website metric reporting user policies and map to pref
    This change adds new website metric reporting policies that control
    website activity and usage telemetry reporting. This also maps these new
    policies with respective prefs so metric reporting components can easily
    look them up and subscribe to changes as needed.
    
    Bug: b:237811626
    DD: go/i&i_website_usage
    Change-Id: I1105cc8d34b8cedd28e6a4a76527b25a3c4128e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735840
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180469}

---
+++ chrome/browser/chromeos/reporting/metric_reporting_prefs.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/reporting/metric_reporting_prefs.h"
+
+#include "base/check.h"
+#include "chrome/browser/chromeos/reporting/metric_default_utils.h"
+#include "components/pref_registry/pref_registry_syncable.h"
+
+namespace reporting {
+
+void RegisterProfilePrefs(::user_prefs::PrefRegistrySyncable* registry) {
+  CHECK(registry);
+  registry->RegisterListPref(kReportWebsiteActivity);
+  registry->RegisterListPref(kReportWebsiteUsage);
+  registry->RegisterIntegerPref(
+      kReportWebsiteUsageCollectionRateMs,
+      ::reporting::metrics::kDefaultWebsiteUsageTelemetryCollectionRate
+          .InMilliseconds());
+}
+
+}  // namespace reporting
