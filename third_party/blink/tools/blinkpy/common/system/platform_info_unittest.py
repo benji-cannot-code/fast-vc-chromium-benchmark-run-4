a71be822dc858e2b6aeaa41b2e09cc2905ba511f
Jonathan Lee <jonathanjlee@google.com>
2023-08-07 21:02:43 +0000

    [blinkpy] Remove references to trusty
    No-op change to remove the `trusty` version identifier from web tests.
    The Linux port is now simply referred to as the versionless `linux`,
    since it seems unlikely we will ever test against more than one Linux
    version at a time, and this avoids naming churn for future upgrades.
    
    There's no need to rename the `test-linux-{trusty,precise}` ports, since
    those aren't implemented by the real `LinuxPort`.
    
    Fixed: 1468322
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_dbg_ng
    Cq-Include-Trybots: luci.chromium.try:dawn-linux-x64-deps-rel
    Change-Id: Ie41b67e29b4c36508e4e50b7b708a6224e9a58a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755286
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180534}

--- third_party/blink/tools/blinkpy/common/system/platform_info_unittest.py
+++ third_party/blink/tools/blinkpy/common/system/platform_info_unittest.py
@@ -51,14 +51,13 @@ def fake_sys(platform_str='darwin', windows_version_tuple=None):
 
 def fake_platform(mac_version_string='12.3.1',
                   release_string='bar',
-                  linux_version='trusty',
                   win_version_string=None):
     class FakePlatformModule(object):
         def mac_ver(self):
             return tuple([mac_version_string, tuple(['', '', '']), 'i386'])
 
         def linux_distribution(self):
-            return tuple([None, None, linux_version])
+            return 'unknown'
 
         def platform(self):
             return 'foo'
@@ -174,12 +173,6 @@ class TestPlatformInfo(unittest.TestCase):
                            fake_platform('13.0.0')).os_version, 'mac13')
         with self.assertRaises(AssertionError):
             self.make_info(fake_sys('darwin'), fake_platform('10.20.0'))
-
-        self.assertEqual(
-            self.make_info(fake_sys('linux2')).os_version, 'trusty')
-        info = self.make_info(
-            fake_sys('linux2'), fake_platform(linux_version='utopic'))
-        self.assertEqual(info.os_version, 'trusty')
 
         self.assertEqual(
             self.make_info(
--- third_party/blink/tools/blinkpy/web_tests/flake_suppressor/web_tests_queries_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/flake_suppressor/web_tests_queries_unittest.py
@@ -42,7 +42,7 @@ class WebTestQueriesUnittest(unittest.TestCase):
                 # Try results.
                 query_result = [
                     {
-                        'typ_tags': ['linux', 'trusty'],
+                        'typ_tags': ['linux'],
                         'test_name': 'foo/bar/linux',
                         'result_count': '25',
                     },
