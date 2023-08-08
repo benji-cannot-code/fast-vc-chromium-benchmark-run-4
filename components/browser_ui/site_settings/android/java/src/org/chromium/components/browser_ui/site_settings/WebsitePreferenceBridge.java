fe4fb51fff71b6f1b326d405a7fab3bbde71bfbf
Shu Yang <shuyng@google.com>
2023-08-08 05:12:55 +0000

    [RDSW] Add util method to check if ContentSettings is global setting
    This will be used to apply RDS window settings, whose priority is higher
    than global setting but lower than domain setting.
    
    Bug: 1470124
    Change-Id: I886f20ec89fcb42b09bea0ae853e77f6e015cc09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4749626
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180688}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePreferenceBridge.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePreferenceBridge.java
@@ -352,6 +352,15 @@ public class WebsitePreferenceBridge {
     }
 
     /**
+     * @return Whether the ContentSettings is global setting.
+     */
+    public static boolean isContentSettingGlobal(BrowserContextHandle browserContextHandle,
+            @ContentSettingsType int contentSettingType, GURL primaryUrl, GURL secondaryUrl) {
+        return WebsitePreferenceBridgeJni.get().isContentSettingGlobal(
+                browserContextHandle, contentSettingType, primaryUrl, secondaryUrl);
+    }
+
+    /**
      * Sets the content setting for the default scope of the url that is appropriate for the
      * given contentSettingType.
      * See HostContentSettingsMap::SetContentSettingDefaultScope() for more details.
@@ -451,6 +460,8 @@ public class WebsitePreferenceBridge {
                 @ContentSettingsType int contentSettingsType, List<ContentSettingException> list);
         @ContentSettingValues
         int getContentSetting(BrowserContextHandle browserContextHandle,
+                @ContentSettingsType int contentSettingType, GURL primaryUrl, GURL secondaryUrl);
+        boolean isContentSettingGlobal(BrowserContextHandle browserContextHandle,
                 @ContentSettingsType int contentSettingType, GURL primaryUrl, GURL secondaryUrl);
         void setContentSettingDefaultScope(BrowserContextHandle browserContextHandle,
                 @ContentSettingsType int contentSettingType, GURL primaryUrl, GURL secondaryUrl,
