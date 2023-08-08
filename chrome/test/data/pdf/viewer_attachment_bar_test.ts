234b01aaf2c97b599a0ad89acf3cc6e56ef28431
Hui Yingst <nigi@chromium.org>
2023-08-08 20:30:46 +0000

    Add browser tests for `viewer-attachment-bar`
    Bug: 177188
    Change-Id: I1bbadb47abfa731f73687496238053207df9b148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727567
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Nigi <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181135}

---
+++ chrome/test/data/pdf/viewer_attachment_bar_test.ts
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {ViewerAttachmentBarElement} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/elements/viewer-attachment-bar.js';
+
+function createAttachmentBar(): ViewerAttachmentBarElement {
+  document.body.innerHTML = '';
+  const attachmentBar = document.createElement('viewer-attachment-bar');
+  document.body.appendChild(attachmentBar);
+  return attachmentBar;
+}
+
+// Unit tests for the viewer-attachment-bar element.
+const tests = [
+  function testWithRegularAttachment() {
+    const attachmentBar = createAttachmentBar();
+    attachmentBar.attachments = [
+      {name: 'attachment1', size: 10, readable: true},
+      {name: 'attachment2', size: 1, readable: true},
+    ];
+
+    // No warning message is displayed.
+    const warning = attachmentBar.shadowRoot!.querySelector('#warning')!;
+    chrome.test.assertFalse(warning.getAttribute('hidden') === null);
+    chrome.test.succeed();
+  },
+
+  function testWithOversizeAttachment() {
+    const attachmentBar = createAttachmentBar();
+    attachmentBar.attachments = [
+      {name: 'attachment1', size: 10, readable: true},
+      {name: 'attachment2', size: -1, readable: true},
+    ];
+
+    // A warning message is displayed because `attachment2` is oversized.
+    const warning = attachmentBar.shadowRoot!.querySelector('#warning')!;
+    chrome.test.assertEq(null, warning.getAttribute('hidden'));
+    chrome.test.succeed();
+  },
+];
+
+chrome.test.runTests(tests);
