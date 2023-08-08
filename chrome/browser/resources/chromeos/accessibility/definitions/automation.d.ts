46a6310e4f2d4af7de3703c4f62c447849d9b289
Anastasia Helfinstein <anastasi@google.com>
2023-08-08 21:21:49 +0000

    [automation] Remove obsolete language annotation code
    This language annotation code is no longer being worked on. Remove the
    unused logic.
    
    AX-Relnotes: n/a.
    Bug: b/265481751
    Change-Id: I2842b3a118ffe3914ba8af4c37d8ff815e162068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707168
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181159}

--- chrome/browser/resources/chromeos/accessibility/definitions/automation.d.ts
+++ chrome/browser/resources/chromeos/accessibility/definitions/automation.d.ts
@@ -646,13 +646,6 @@ declare namespace chrome {
       id: number;
     }
 
-    export interface LanguageSpan {
-      endIndex: number;
-      language: string;
-      probability: number;
-      startIndex: number;
-    }
-
     export interface Marker {
       endOffset: number;
       flags: {[key in MarkerType]: boolean;};
@@ -895,7 +888,6 @@ declare namespace chrome {
       hitTest(x: number, y: number, eventToFire: EventType): void;
       hitTestWithReply(
           x: number, y: number, callback: PerformActionCallbackWithNode): void;
-      languageAnnotationForStringAttribute(attribute: string): LanguageSpan[];
       longClick(): void;
       makeVisible(): void;
       matches(params: FindParams): boolean;
