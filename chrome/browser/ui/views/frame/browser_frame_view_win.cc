ed681672de0de4a459a0c5270ff925bd078c3152
Darryl <dljames@chromium.org>
2023-08-08 22:18:32 +0000

    [ ChromeRefresh2023 ] Adjust caption button heights on Windows with no Tabstrip.
    Adds a new function to the Windows browser frame view that returns the browser frame height depending on the visibility of the Tabstrip.
    
    Change-Id: Ic0128c49dc0ec46b95b3b2db3e24700012469092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763264
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1181190}

--- chrome/browser/ui/views/frame/browser_frame_view_win.cc
+++ chrome/browser/ui/views/frame/browser_frame_view_win.cc
@@ -584,6 +584,14 @@ int BrowserFrameViewWin::TitlebarHeight(bool restored) const {
          FrameTopBorderThickness(false);
 }
 
+int BrowserFrameViewWin::GetFrameHeight() const {
+  if (browser_view()->GetTabStripVisible()) {
+    return browser_view()->tab_strip_region_view()->GetMinimumSize().height();
+  }
+  return IsMaximized() ? TitlebarMaximizedVisualHeight()
+                       : TitlebarHeight(false);
+}
+
 int BrowserFrameViewWin::WindowTopY() const {
   // The window top is SM_CYSIZEFRAME pixels when maximized (see the comment in
   // FrameTopBorderThickness()) and floor(system dsf) pixels when restored.
@@ -810,7 +818,7 @@ void BrowserFrameViewWin::LayoutCaptionButtons() {
       CaptionButtonsOnLeadingEdge()
           ? system_caption_buttons_width
           : width() - system_caption_buttons_width - preferred_size.width(),
-      WindowTopY(), preferred_size.width(), height);
+      WindowTopY(), preferred_size.width(), std::min(GetFrameHeight(), height));
 }
 
 void BrowserFrameViewWin::LayoutClientView() {
