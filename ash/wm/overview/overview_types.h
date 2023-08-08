d1acf6dcb7adb679ec57b1c9b615656daf4e3de7
Michele Fan <michelefan@chromium.org>
2023-08-08 19:32:03 +0000

    snap-group: Add `OverviewItemBase` and extract virtual functions
    This CL adds `OverviewItemBase` which will be implemented by
    `OverviewItem` and `OverviewGroupItem` in the tasks to be followed.
    The virtual functions extracted will be implemented by `OverviewItem`
    and `OverviewGroupItem` correspondingly.
    
    Fixed: b/294616329
    Test: Run existing
    Change-Id: I95e94df03a565485098fbabdb46b929c5574ea11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4756465
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181096}

--- ash/wm/overview/overview_types.h
+++ ash/wm/overview/overview_types.h
@@ -8,6 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+// Defines the overview item types.
+enum class OverviewItemType {
+  kSingleItem,
+  kGroupItem,
+};
+
 // Enumeration of the different overview mode animations.
 enum OverviewAnimationType {
   OVERVIEW_ANIMATION_NONE,
