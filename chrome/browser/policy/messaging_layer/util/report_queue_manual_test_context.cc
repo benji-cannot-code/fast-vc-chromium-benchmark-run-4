92ace125981f8e0cce8d8443a9f3e83be77909ed
Josh Hilke <jrhilke@google.com>
2023-08-08 23:14:02 +0000

    Add user heartbeat event.
    Manually enqueue a heartbeat event with EventType::kUser instead of
    EventType::kDevice when EncryptedReportingManualTestUserHeartbeatEvent feature is enabled. This is so that we use a user DM token instead
    of a device DM token when TAST testing reporting from unmanaged devices (which don't have device DM tokens).
    
    Change-Id: I92e9a686c08cd0f4e2ef36d9a65329d61dfd8fda
    Bug: b:295048007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761030
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181213}

--- chrome/browser/policy/messaging_layer/util/report_queue_manual_test_context.cc
+++ chrome/browser/policy/messaging_layer/util/report_queue_manual_test_context.cc
@@ -26,6 +26,7 @@ ReportQueueManualTestContext::ReportQueueManualTestContext(
     uint64_t number_of_messages_to_enqueue,
     Destination destination,
     Priority priority,
+    EventType event_type,
     CompletionCallback completion_cb,
     scoped_refptr<base::SequencedTaskRunner> sequenced_task_runner,
     BuildReportQueueCallback queue_builder)
@@ -35,6 +36,7 @@ ReportQueueManualTestContext::ReportQueueManualTestContext(
       number_of_messages_to_enqueue_(number_of_messages_to_enqueue),
       destination_(destination),
       priority_(priority),
+      event_type_(event_type),
       queue_builder_(std::move(queue_builder)),
       report_queue_(std::unique_ptr<ReportQueue, base::OnTaskRunnerDeleter>(
           nullptr,
@@ -66,7 +68,7 @@ void ReportQueueManualTestContext::OnStart() {
 
   auto config_result =
       ReportQueueConfiguration::Create(
-          {.event_type = EventType::kDevice, .destination = destination_})
+          {.event_type = event_type_, .destination = destination_})
           .Build();
   if (!config_result.ok()) {
     Complete(config_result.status());
