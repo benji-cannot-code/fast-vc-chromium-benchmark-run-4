eb65b7bd29f52ff0cc267fb7a049fef0af75cd8c
Asami Doi <asamidoi@chromium.org>
2023-08-08 15:27:07 +0000

    [ios] Clean up WebStateListStatus
    This CL removes `active_index` from WebStateListStatus because
    `WebStateList::active_index()` is equal to `active_index` now
    because of the change in https://chromium-review.googlesource.com/c/chromium/src/+/4712111.
    
    Observers can access the active index via
    `WebStateList::active_index()`.
    
    Bug: 1465845
    Change-Id: Ia902ff8176b929df95080a2226e4f0000833f3f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4757062
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180922}

--- ios/chrome/browser/shared/model/web_state_list/web_state_list.mm
+++ ios/chrome/browser/shared/model/web_state_list/web_state_list.mm
@@ -375,7 +375,6 @@ int WebStateList::InsertWebStateImpl(int index,
   const WebStateListChangeInsert insert_change(web_state_ptr);
   const WebStateListStatus status = {
       .index = index,
-      .active_index = activating ? index : active_index_,
       .pinned_state_change = false,
       .old_active_web_state = old_active_web_state,
       .new_active_web_state = GetActiveWebState()};
@@ -402,7 +401,6 @@ void WebStateList::MoveWebStateAtImpl(int from_index,
           web_state_wrappers_[to_index]->web_state());
       const WebStateListStatus status = {
           .index = to_index,
-          .active_index = active_index_,
           .pinned_state_change = true,
           // An active WebState doesn't change when a pinned state is updated.
           .old_active_web_state = GetActiveWebState(),
@@ -435,7 +433,6 @@ void WebStateList::MoveWebStateAtImpl(int from_index,
   const WebStateListChangeMove move_change(web_state, from_index);
   const WebStateListStatus status = {
       .index = to_index,
-      .active_index = active_index_,
       .pinned_state_change = pinned_state_change,
       // The move operation doesn't insert/delete a WebState and doesn't change
       // an active WebState.
@@ -469,7 +466,6 @@ std::unique_ptr<web::WebState> WebStateList::ReplaceWebStateAtImpl(
                                                  web_state_ptr);
   const WebStateListStatus status = {
       .index = index,
-      .active_index = active_index_,
       .pinned_state_change = false,
       .old_active_web_state = (index == active_index_)
                                   ? replaced_web_state.get()
@@ -508,7 +504,6 @@ std::unique_ptr<web::WebState> WebStateList::DetachWebStateAtImpl(
     // point and the new active WebState is not determined yet.
     const WebStateListStatus status = {
         .index = index,
-        .active_index = active_index_,
         .pinned_state_change = false,
         .old_active_web_state = use_old_active_web_state ? old_active_web_state
                                 : is_active_web_state_detached ? web_state
@@ -543,7 +538,6 @@ std::unique_ptr<web::WebState> WebStateList::DetachWebStateAtImpl(
 
   const WebStateListStatus status = {
       .index = index,
-      .active_index = active_index_,
       .pinned_state_change = false,
       .old_active_web_state = use_old_active_web_state ? old_active_web_state
                               : is_active_web_state_detached
@@ -639,7 +633,6 @@ void WebStateList::ActivateWebStateAtImpl(int index) {
   const WebStateListChangeStatusOnly status_only_change(old_active_web_state);
   const WebStateListStatus status = {
       .index = index,
-      .active_index = active_index_,
       .pinned_state_change = false,
       .old_active_web_state = old_active_web_state,
       .new_active_web_state = GetActiveWebState()};
--- ios/chrome/browser/ui/tabs/tab_strip_controller.mm
+++ ios/chrome/browser/ui/tabs/tab_strip_controller.mm
@@ -1279,7 +1279,8 @@ const CGFloat kSymbolSize = 18;
       [view setSelected:NO];
     }
 
-    NSUInteger index = [self indexForWebStateListIndex:status.active_index];
+    NSUInteger index =
+        [self indexForWebStateListIndex:webStateList->active_index()];
     TabView* activeView = [_tabArray objectAtIndex:index];
     [activeView setSelected:YES];
 
