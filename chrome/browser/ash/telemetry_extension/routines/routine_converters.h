912c7e1633112eb6a0ea3accd2feb40e7b1567e3
Bastian Kersting <bkersting@google.com>
2023-08-08 11:35:05 +0000

    [Telemetry] Add memory routine to diag v2 API
    This change adds support for the memory routine to the
    new routines interface.
    This routine was implemented on the ChromiumOS side in:
    - crrev/c/4041684 (interface added)
    - crrev/c/4055129 (empty impl)
    - crrev/c/4055133 (actual impl)
    
    Bug: b/291566272, b/286031590
    Test: unit_tests
    --gtest_filter="*TelemetryDiagnosticRoutineConvertersTest*"
    
    Change-Id: I97dea0c0abbcc6d7e2ab36227b044e4563a7416f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748885
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180822}

--- chrome/browser/ash/telemetry_extension/routines/routine_converters.h
+++ chrome/browser/ash/telemetry_extension/routines/routine_converters.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_TELEMETRY_EXTENSION_ROUTINES_ROUTINE_CONVERTERS_H_
 #define CHROME_BROWSER_ASH_TELEMETRY_EXTENSION_ROUTINES_ROUTINE_CONVERTERS_H_
 
+#include <type_traits>
 #include <utility>
 
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_routines.mojom.h"
@@ -20,6 +21,12 @@ namespace ash::converters {
 // Contains conversion functions that skip checking the `mojo::InlinedStructPtr`
 // for null.
 namespace unchecked {
+
+crosapi::mojom::TelemetryDiagnosticMemtesterResultPtr UncheckedConvertPtr(
+    cros_healthd::mojom::MemtesterResultPtr input);
+
+crosapi::mojom::TelemetryDiagnosticMemoryRoutineDetailPtr UncheckedConvertPtr(
+    cros_healthd::mojom::MemoryRoutineDetailPtr input);
 
 crosapi::mojom::TelemetryDiagnosticRoutineStateInitializedPtr
 UncheckedConvertPtr(cros_healthd::mojom::RoutineStateInitializedPtr input);
@@ -45,10 +52,27 @@ crosapi::mojom::TelemetryDiagnosticRoutineStatePtr UncheckedConvertPtr(
 cros_healthd::mojom::RoutineArgumentPtr UncheckedConvertPtr(
     crosapi::mojom::TelemetryDiagnosticRoutineArgumentPtr input);
 
+cros_healthd::mojom::MemoryRoutineArgumentPtr UncheckedConvertPtr(
+    crosapi::mojom::TelemetryDiagnosticMemoryRoutineArgumentPtr input);
+
 }  // namespace unchecked
+
+crosapi::mojom::TelemetryDiagnosticMemtesterTestItemEnum Convert(
+    cros_healthd::mojom::MemtesterTestItemEnum input);
 
 crosapi::mojom::TelemetryDiagnosticRoutineStateWaiting::Reason Convert(
     cros_healthd::mojom::RoutineStateWaiting::Reason input);
+
+template <class InputT,
+          class OutputT = decltype(Convert(std::declval<InputT>())),
+          class = std::enable_if_t<std::is_enum_v<InputT>, bool>>
+std::vector<OutputT> ConvertVector(std::vector<InputT> input) {
+  std::vector<OutputT> result;
+  for (auto elem : input) {
+    result.push_back(Convert(elem));
+  }
+  return result;
+}
 
 template <class InputT>
 auto ConvertRoutinePtr(InputT input) {
