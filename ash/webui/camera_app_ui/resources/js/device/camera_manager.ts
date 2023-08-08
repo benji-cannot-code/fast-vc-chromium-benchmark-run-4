0e72c59b11d796d1f7b869929fefdc4a552aae52
Dorah <dorahkim@google.com>
2023-08-08 06:03:45 +0000

    camera: add vid and pid metric in CCA
    In CCA, we collected the metrics if the user is using one of the top 20
    external cameras. If we could know which camera is mostly used in CCA,
    we can support those cameras for users.
    
    Added ModuleID to the documentation.
    METRICS_DOCUMENTATION_UPDATED=true
    
    Bug: b:193493869
    Test: Manually with DUT.
    Change-Id: Ie09e0209d0c8c683d2f9f3d5c749b6f8c6b83537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4452211
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180703}

--- ash/webui/camera_app_ui/resources/js/device/camera_manager.ts
+++ ash/webui/camera_app_ui/resources/js/device/camera_manager.ts
@@ -11,6 +11,7 @@ import {
 import * as error from '../error.js';
 import * as expert from '../expert.js';
 import {Point} from '../geometry.js';
+import * as metrics from '../metrics.js';
 import {isLocalDev} from '../models/load_time_data.js';
 import {ChromeHelper} from '../mojo/chrome_helper.js';
 import {ScreenState} from '../mojo/type.js';
@@ -334,6 +335,7 @@ export class CameraManager implements EventListener {
     }
     return promise.then((succeed) => {
       state.set(PerfEvent.CAMERA_SWITCHING, false, {hasError: !succeed});
+      metrics.sendOpenCameraEvent(this.getVidPid());
     });
   }
 
--- ash/webui/camera_app_ui/resources/js/main.ts
+++ ash/webui/camera_app_ui/resources/js/main.ts
@@ -380,6 +380,7 @@ export class App {
         {hasError: !cameraStartSuccessful});
 
     window.appWindow?.onAppLaunched();
+    metrics.sendOpenCameraEvent(this.cameraManager.getVidPid());
   }
 
   /**
