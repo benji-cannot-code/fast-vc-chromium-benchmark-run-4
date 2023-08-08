c535bd192893841b32f15f27883bd0a0c54da37f
Christoph Schwering <schwering@google.com>
2023-08-08 07:32:45 +0000

    [Autofill] Merge two parameters of LogInteractedWithForm() into one
    Bug: 1007974
    Change-Id: I1c38bc41f77ffccb457108461131b9b1412624c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755913
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1180721}

--- components/autofill/core/browser/metrics/autofill_metrics.h
+++ components/autofill/core/browser/metrics/autofill_metrics.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_state/core/security_state.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 
 class GURL;
 
@@ -741,8 +742,8 @@ class AutofillMetrics {
                              const AutofillField& field,
                              const base::TimeTicks& form_parsed_timestamp,
                              bool off_the_record);
-    void LogDidFillSuggestion(int record_type,
-                              bool is_for_credit_card,
+    void LogDidFillSuggestion(absl::variant<AutofillProfile::RecordType,
+                                            CreditCard::RecordType> record_type,
                               const FormStructure& form,
                               const AutofillField& field);
     void LogTextFieldDidChange(const FormStructure& form,
