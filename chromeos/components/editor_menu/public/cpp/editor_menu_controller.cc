3025d2263d57e66433dd47f405ab84cc2fc5e72e
Tao Wu <wutao@chromium.org>
2023-08-08 03:13:25 +0000

    editor_menu: Add controller interface
    This cl adds interfaces to open editor menu related views.
    
    Bug: b/294858936
    Test: Will add tests when more classes are added.
    Change-Id: I73887cc05064fe9277503ab02ab57f4b0df609fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4757759
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180668}

---
+++ chromeos/components/editor_menu/public/cpp/editor_menu_controller.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/editor_menu/public/cpp/editor_menu_controller.h"
+
+#include "base/check_op.h"
+
+namespace chromeos::editor_menu {
+
+namespace {
+
+EditorMenuController* g_instance = nullptr;
+
+}  // namespace
+
+EditorMenuController::EditorMenuController() {
+  DCHECK(!g_instance);
+  g_instance = this;
+}
+
+EditorMenuController::~EditorMenuController() {
+  DCHECK_EQ(this, g_instance);
+  g_instance = nullptr;
+}
+
+EditorMenuController* EditorMenuController::Get() {
+  return g_instance;
+}
+
+}  // namespace chromeos::editor_menu
