1c490ea675af28ed1baf186d6a0a975e734bdc81
David Padlipsky <dpad@google.com>
2023-08-08 21:45:19 +0000

    customization: Add support for MouseEvent button observing
    Adds support for observing mouse button events which does the following:
    - Blocks mouse button events from the given device
    - Notifies observers when the event is blocked and tells the observers
      what button was pressed.
    
    These mouse button events can come from either mice or graphics tablets.
    
    Bug: b/241965717
    Change-Id: I05a8a345727dec48d0387c0e9c36c622119c76de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680235
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181172}

--- ash/events/peripheral_customization_event_rewriter.h
+++ ash/events/peripheral_customization_event_rewriter.h
@@ -7,7 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_EVENTS_PERIPHERAL_CUSTOMIZATION_EVENT_REWRITER_H_
 
 #include "ash/ash_export.h"
+#include "ash/public/mojom/input_device_settings.mojom-forward.h"
+#include "base/containers/flat_set.h"
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "ui/events/event.h"
+#include "ui/events/event_dispatcher.h"
 #include "ui/events/event_rewriter.h"
 
 namespace ash {
@@ -18,6 +23,21 @@ namespace ash {
 class ASH_EXPORT PeripheralCustomizationEventRewriter
     : public ui::EventRewriter {
  public:
+  enum class DeviceType { kMouse, kGraphicsTablet };
+
+  class Observer : public base::CheckedObserver {
+   public:
+    // Called when a mouse that is currently being observed presses a button
+    // that is remappable on mice.
+    virtual void OnMouseButtonPressed(int device_id,
+                                      const mojom::Button& button) = 0;
+
+    // Called when a graphics tablet that is currently being observed presses a
+    // button that is remappable on graphics tablets.
+    virtual void OnGraphicsTabletButtonPressed(int device_id,
+                                               const mojom::Button& button) = 0;
+  };
+
   PeripheralCustomizationEventRewriter();
   PeripheralCustomizationEventRewriter(
       const PeripheralCustomizationEventRewriter&) = delete;
@@ -25,10 +45,38 @@ class ASH_EXPORT PeripheralCustomizationEventRewriter
       const PeripheralCustomizationEventRewriter&) = delete;
   ~PeripheralCustomizationEventRewriter() override;
 
+  // Starts observing and blocking mouse events for `device_id`. Notifies
+  // observers via `OnMouseButtonPressed` whenever an event
+  void StartObservingMouse(int device_id);
+
+  // Starts observing and blocking graphics tablet events for `device_id`.
+  // Notifies observers via `OnGraphicsTabletButtonPressed` whenever an event is
+  // received.
+  void StartObservingGraphicsTablet(int device_id);
+
+  // Stops observing for all devices of every type.
+  void StopObserving();
+
   // ui::EventRewriter:
   ui::EventDispatchDetails RewriteEvent(
       const ui::Event& event,
       const Continuation continuation) override;
+
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
+
+ private:
+  // Notifies observers if the given `mouse_event` is a remappable button for
+  // the given `device_type`. Returns true if the event should be discarded.
+  bool NotifyMouseEventObserving(const ui::MouseEvent& mouse_event,
+                                 DeviceType device_type);
+
+  ui::EventDispatchDetails RewriteMouseEvent(const ui::MouseEvent& mouse_event,
+                                             const Continuation continuation);
+
+  base::flat_set<int> mice_to_observe_;
+  base::flat_set<int> graphics_tablets_to_observe_;
+  base::ObserverList<Observer> observers_;
 };
 
 }  // namespace ash
