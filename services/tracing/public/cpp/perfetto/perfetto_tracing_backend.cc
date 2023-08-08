74ab90b53cf6d550218794eddee184981bf74051
Chinglin Yu <chinglinyu@chromium.org>
2023-08-08 14:23:21 +0000

    tracing: use default ShmemMode in creating the arbiter
    This CL is a manual roll of perfetto with a fix in Chrome. The perfetto
    upstream introduces the ShmemMode parameter in creating the
    SharedMemoryArbiter in aosp/2627899 and causes breakage in tracing. Fix
    by passing ShmemMode::kDefault (supporting cross-process Shmem) in
    creating the arbiter. Also include a fix of perfetto unittest for
    fuchsia (aosp/2696480).
    
    This CL manually rolls perfetto from 446adf5b7bb4 to 8e8917c609bd
    (36 revisions).
    https://android.googlesource.com/platform/external/perfetto.git/+log/446adf5b7bb4..8e8917c609bd
    
    git log --date=short --no-merges --format='%cd %ae %s' 446adf5b7bb4..8e8917c609bd
    2023-08-08 chinglinyu@google.com Fix HostImplTest crashes on fuchsia
    2023-08-07 hjd@google.com ui: Bump node max heap to 8gb
    2023-08-07 lalitm@google.com tp: fix error offset pointing to end of sql string
    2023-08-07 hjd@google.com ui: Add viz page
    2023-08-07 ddiproietto@google.com track_event_macros: Fix PERFETTO_INTERNAL_CATEGORY_ENABLED with MSVC
    2023-08-07 ddiproietto@google.com track_event_macros: Fix build with MSVC
    2023-08-07 keanmariotti@google.com Extend surfaceflinger layers trace with DPI info
    2023-08-07 chinglinyu@google.com Support shmem-less ABI for the producer port
    2023-08-06 hjd@google.com ui: Update to es2021
    2023-08-05 hjd@google.com ui: Move canned queries to commands
    2023-08-04 stevegolton@google.com Merge commands and hotkeys & display hotkey glyphs in command palette.
    2023-08-04 keanmariotti@google.com Add config for android.surfaceflinger.transactions
    2023-08-03 oysteine@google.com Support initializing SharedMemoryWindows from handles
    2023-08-03 haiping@google.com Make the long binder transaction threshold same as that of slow start reason check.
    2023-08-03 ddiproietto@google.com Fix protozero unittests on MSVC
    2023-08-03 ddiproietto@google.com Avoid using raw string literal inside macro
    2023-08-03 ddiproietto@google.com Fully qualify friend class in autogenerated table code
    2023-08-03 zezeozue@google.com [ui]: Add a debug track for a thread's critical path automatically
    2023-08-03 hjd@google.com ui: Support non-alphanumeric arg names in sql_table
    2023-08-03 ddiproietto@google.com Fix amalgamated build on windows
    2023-08-03 hjd@google.com ui: Move AndroidPerf commands into a plugin
    2023-08-03 hjd@google.com ui: Add openQuery to viewer API
    2023-08-02 hjd@google.com ui: Move validators from controller to base
    2023-08-02 stevegolton@google.com Fix inverted sidebar hide/show logic.
    2023-08-02 ddiproietto@google.com ipc: Remove wrong DeferredBase constructor.
    2023-08-02 zezeozue@google.com [ui]: Add some command palette functionality
    2023-08-02 marcinoc@google.com Discard incorrectly ending Choreographer#doFrame slices
    2023-08-02 stevegolton@google.com Add in-memory recent commands to the top of the ommnibox.
    2023-08-02 haiping@google.com Remove running duration check and update blocked by IO threshold.
    2023-08-01 lalitm@google.com sdk: reduce binary size by out-of-lining protozero equality
    2023-08-01 lalitm@google.com tp: workaround sqlite3 returning OOB error offsets
    2023-08-01 stevegolton@google.com Remove default popup padding.
    2023-08-01 hjd@google.com Add metric visualisations
    2023-08-01 ddiproietto@google.com shared_lib: New macro to initialize struct PerfettoProducerInitArgs
    2023-08-01 ddiproietto@google.com shared_lib: Implement OnDestroy callback for data source
    2023-07-17 primiano@google.com service: disallow ReadBuffers for CLONE_SNAPSHOT traces
    
    Bug: b:284258446
    Test: CQ dry run
    Change-Id: I34fca1255c111f1c1453a1751298936e400536e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752739
    Commit-Queue: Chinglin Yu <chinglinyu@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180894}

--- services/tracing/public/cpp/perfetto/perfetto_tracing_backend.cc
+++ services/tracing/public/cpp/perfetto/perfetto_tracing_backend.cc
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/tracing_service.h"
 #include "third_party/perfetto/include/perfetto/tracing/core/trace_config.h"
 
+using ShmemMode = perfetto::SharedMemoryArbiter::ShmemMode;
+
 namespace tracing {
 namespace {
 
@@ -265,8 +267,8 @@ class ProducerEndpoint : public perfetto::ProducerEndpoint,
       shared_memory_ =
           std::make_unique<ChromeBaseSharedMemory>(shmem_size_bytes_);
       shared_memory_arbiter_ = perfetto::SharedMemoryArbiter::CreateInstance(
-          shared_memory_.get(), shmem_page_size_bytes_, this,
-          producer_task_runner);
+          shared_memory_.get(), shmem_page_size_bytes_, ShmemMode::kDefault,
+          this, producer_task_runner);
     }
 
     mojo::PendingRemote<mojom::ProducerClient> client_remote;
@@ -654,7 +656,7 @@ PerfettoTracingBackend::ConnectProducer(const ConnectProducerArgs& args) {
   if (args.use_producer_provided_smb) {
     shm = std::make_unique<ChromeBaseSharedMemory>(shmem_size_hint);
     arbiter = perfetto::SharedMemoryArbiter::CreateUnboundInstance(
-        shm.get(), shmem_page_size_hint);
+        shm.get(), shmem_page_size_hint, ShmemMode::kDefault);
   }
 #endif  // BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
