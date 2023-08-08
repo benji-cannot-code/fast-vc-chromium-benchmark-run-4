0c44a2ecb47f2c2e21a96dd6e18b8345de74bb02
Paul Adedeji <pauladedeji@google.com>
2023-08-08 18:47:27 +0000

    [ntp][drive] Retrieve segmentation result before showing Drive module
    Retrieves prediction (classification result) from segmentation plaform before showing drive module (when the NtpDriveModuleSegmentation experiment is enabled).
    
    With this CL, the classification result will be ignored. Future CLs will make use of the result to conditionally show (or not show) the Drive module.
    
    Making a call to the segmentation platform before showing Drive files allows the platform to get better signals for when/how a user will use Drive. For example, if the API is called and returns 'show_drive' then the user does not interact with the module, future predictions can account for this.
    
    By getting this into M117, we can bolster the reliability of our classification results, before using them in future milestones, to show or not show the Drive module.
    
    
    Change-Id: If68f41096ca6998f2b0a16d10b0914962a9a6974
    Bug: 1470762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754911
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181065}

--- chrome/browser/new_tab_page/modules/drive/drive_service_factory.cc
+++ chrome/browser/new_tab_page/modules/drive/drive_service_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/new_tab_page/modules/drive/drive_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/segmentation_platform/segmentation_platform_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
@@ -34,6 +35,8 @@ DriveServiceFactory::DriveServiceFactory()
               .Build()) {
   DependsOn(CookieSettingsFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
+  DependsOn(
+      segmentation_platform::SegmentationPlatformServiceFactory::GetInstance());
 }
 
 DriveServiceFactory::~DriveServiceFactory() = default;
@@ -45,5 +48,7 @@ KeyedService* DriveServiceFactory::BuildServiceInstanceFor(
   auto* profile = Profile::FromBrowserContext(context);
   return new DriveService(
       url_loader_factory, IdentityManagerFactory::GetForProfile(profile),
+      segmentation_platform::SegmentationPlatformServiceFactory::GetForProfile(
+          profile),
       g_browser_process->GetApplicationLocale(), profile->GetPrefs());
 }
