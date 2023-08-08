432846e4269aaf2d26bd51ad066adfdd42bebd7f
Jérôme Lebel <jlebel@chromium.org>
2023-08-08 18:48:40 +0000

    [iOS][UNO] Fix ConsistencyPromoSigninMediator when receiving cookies
    When ConsistencyPromoSigninMediator is used for none WebSignin access
    point, The mediator should ignore cookie notifications.
    
    Fixed: 1471140
    Change-Id: Ieebd7404971fa656b217f8deffff2e2ea44f9159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761405
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181066}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_mediator_unittest.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_mediator_unittest.mm
@@ -87,7 +87,8 @@ class ConsistencyPromoSigninMediatorTest : public PlatformTest {
         browser_state_.get());
   }
 
-  TestConsistencyPromoSigninMediator* GetConsistencyPromoSigninMediator() {
+  TestConsistencyPromoSigninMediator* GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint access_point) {
     ChromeAccountManagerService* chromeAccountManagerService =
         ChromeAccountManagerServiceFactory::GetForBrowserState(
             browser_state_.get());
@@ -97,8 +98,7 @@ class ConsistencyPromoSigninMediatorTest : public PlatformTest {
                     authenticationService:GetAuthenticationService()
                           identityManager:GetIdentityManager()
                           userPrefService:&pref_service_
-                              accessPoint:signin_metrics::AccessPoint::
-                                              ACCESS_POINT_WEB_SIGNIN];
+                              accessPoint:access_point];
     mediator.delegate = mediator_delegate_mock_;
     return mediator;
   }
@@ -112,6 +112,11 @@ class ConsistencyPromoSigninMediatorTest : public PlatformTest {
     OCMExpect([mediator_delegate_mock_
         consistencyPromoSigninMediatorSignInDone:mediator
                                     withIdentity:identity]);
+    SimulateCookies(mediator, identity);
+  }
+
+  void SimulateCookies(ConsistencyPromoSigninMediator* mediator,
+                       id<SystemIdentity> identity) {
     id<IdentityManagerObserverBridgeDelegate>
         identityManagerObserverBridgeDelegate =
             (id<IdentityManagerObserverBridgeDelegate>)mediator;
@@ -200,8 +205,8 @@ class ConsistencyPromoSigninMediatorTest : public PlatformTest {
 // Tests start and cancel by user.
 TEST_F(ConsistencyPromoSigninMediatorTest, StartAndStopForCancel) {
   base::HistogramTester histogram_tester;
-  ConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+  ConsistencyPromoSigninMediator* mediator = GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   [mediator disconnectWithResult:SigninCoordinatorResultCanceledByUser];
 
   histogram_tester.ExpectTotalCount(
@@ -219,8 +224,8 @@ TEST_F(ConsistencyPromoSigninMediatorTest, StartAndStopForCancel) {
 // Tests start and interrupt.
 TEST_F(ConsistencyPromoSigninMediatorTest, StartAndStopForInterrupt) {
   base::HistogramTester histogram_tester;
-  ConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+  ConsistencyPromoSigninMediator* mediator = GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   [mediator disconnectWithResult:SigninCoordinatorResultInterrupted];
 
   histogram_tester.ExpectTotalCount(
@@ -240,8 +245,8 @@ TEST_F(ConsistencyPromoSigninMediatorTest,
        SigninCoordinatorResultSuccessWithDefaultIdentity) {
   base::HistogramTester histogram_tester;
   pref_service_.SetInteger(prefs::kSigninWebSignDismissalCount, 1);
-  ConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+  ConsistencyPromoSigninMediator* mediator = GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   OCMExpect([mediator_delegate_mock_
       consistencyPromoSigninMediatorSigninStarted:mediator]);
   SigninWithMediator(mediator, identity1_, /*signin_success=*/YES);
@@ -265,8 +270,8 @@ TEST_F(ConsistencyPromoSigninMediatorTest,
 TEST_F(ConsistencyPromoSigninMediatorTest,
        SigninCoordinatorResultSuccessWithSecondaryIdentity) {
   base::HistogramTester histogram_tester;
-  ConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+  ConsistencyPromoSigninMediator* mediator = GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   OCMExpect([mediator_delegate_mock_
       consistencyPromoSigninMediatorSigninStarted:mediator]);
   SigninWithMediator(mediator, identity2_, /*signin_success=*/YES);
@@ -294,8 +299,8 @@ TEST_F(ConsistencyPromoSigninMediatorTest,
                                      gaiaID:@"foo1ID3"
                                        name:@"Fake Foo 3"];
   GetSystemIdentityManager()->AddIdentity(identity3);
-  ConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+  ConsistencyPromoSigninMediator* mediator = GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   OCMExpect([mediator_delegate_mock_
       consistencyPromoSigninMediatorSigninStarted:mediator]);
   SigninWithMediator(mediator, identity3, /*signin_success=*/YES);
@@ -318,8 +323,8 @@ TEST_F(ConsistencyPromoSigninMediatorTest,
 // Tests start and sign-in with an error.
 TEST_F(ConsistencyPromoSigninMediatorTest, SigninCoordinatorWithError) {
   base::HistogramTester histogram_tester;
-  ConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+  ConsistencyPromoSigninMediator* mediator = GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   OCMExpect([mediator_delegate_mock_
       consistencyPromoSigninMediatorSigninStarted:mediator]);
   SigninWithMediator(mediator, identity1_, /*signin_success=*/YES);
@@ -347,7 +352,8 @@ TEST_F(ConsistencyPromoSigninMediatorTest, SigninCoordinatorWithError) {
 TEST_F(ConsistencyPromoSigninMediatorTest, SigninCoordinatorWithTimeoutError) {
   base::HistogramTester histogram_tester;
   TestConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+      GetConsistencyPromoSigninMediator(
+          signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   // Sets the timeout duration to 0, to trigger the timeout error without
   // waiting.
   mediator.signinTimeoutDurationSeconds = 0;
@@ -389,7 +395,8 @@ TEST_F(ConsistencyPromoSigninMediatorTest, SigninCoordinatorWithTimeoutError) {
 TEST_F(ConsistencyPromoSigninMediatorTest, SigninFailed) {
   base::HistogramTester histogram_tester;
   TestConsistencyPromoSigninMediator* mediator =
-      GetConsistencyPromoSigninMediator();
+      GetConsistencyPromoSigninMediator(
+          signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN);
   // Sets the timeout duration to 0, to trigger the timeout error without
   // waiting.
   mediator.signinTimeoutDurationSeconds = 0;
@@ -420,4 +427,33 @@ TEST_F(ConsistencyPromoSigninMediatorTest, SigninFailed) {
   histogram_tester.ExpectBucketCount(
       "Signin.AccountConsistencyPromoAction.DismissedButton",
       signin_metrics::AccessPoint::ACCESS_POINT_WEB_SIGNIN, 1);
+}
+
+// Tests start and sign-in with default identity from Settings access point, and
+// then update the cookies. Related to crrev.com/1471140.
+TEST_F(ConsistencyPromoSigninMediatorTest, SigninWithoutCookies) {
+  base::HistogramTester histogram_tester;
+  pref_service_.SetInteger(prefs::kSigninWebSignDismissalCount, 1);
+  ConsistencyPromoSigninMediator* mediator = GetConsistencyPromoSigninMediator(
+      signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS);
+  OCMExpect([mediator_delegate_mock_
+      consistencyPromoSigninMediatorSigninStarted:mediator]);
+  OCMExpect([mediator_delegate_mock_
+      consistencyPromoSigninMediatorSignInDone:mediator
+                                  withIdentity:identity1_]);
+  SigninWithMediator(mediator, identity1_, /*signin_success=*/YES);
+  SimulateCookies(mediator, identity1_);
+  EXPECT_EQ(1, pref_service_.GetInteger(prefs::kSigninWebSignDismissalCount));
+  [mediator disconnectWithResult:SigninCoordinatorResultSuccess];
+
+  histogram_tester.ExpectTotalCount(
+      "Signin.AccountConsistencyPromoAction.Shown", 1);
+  histogram_tester.ExpectBucketCount(
+      "Signin.AccountConsistencyPromoAction.Shown",
+      signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS, 1);
+  histogram_tester.ExpectTotalCount(
+      "Signin.AccountConsistencyPromoAction.SignedInWithDefaultAccount", 1);
+  histogram_tester.ExpectBucketCount(
+      "Signin.AccountConsistencyPromoAction.SignedInWithDefaultAccount",
+      signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS, 1);
 }
