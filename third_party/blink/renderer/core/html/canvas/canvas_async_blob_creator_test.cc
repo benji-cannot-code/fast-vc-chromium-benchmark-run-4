a9287e9db607ca05b043c514a4fcb50bbf3f2620
Anders Hartvoll Ruud <andruud@chromium.org>
2023-08-08 13:03:24 +0000

    Make CanvasAsyncBlobCreator::EncodeImage static
    To eliminate CrossThreadPersistent in CanvasAsyncBlobCreator,
    we need to remove our reliance on 'this' off-thread. This CL
    *reduces* our reliance on 'this'. It's not very useful on its own,
    but it's a step in the right direction, and it exists as a separate
    CL mostly to ease code review.
    
    Bug: 1370013
    Change-Id: I5fd6e3be1fa3c06df5d36a5301d56bc96c56d8fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750923
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180849}

--- third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator_test.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator_test.cc
@@ -55,7 +55,8 @@ class MockCanvasAsyncBlobCreator : public CanvasAsyncBlobCreator {
   MOCK_METHOD0(SignalTaskSwitchInCompleteTimeoutEventForTesting, void());
 
  protected:
-  void CreateBlobAndReturnResult() override {}
+  void CreateBlobAndReturnResult(Vector<unsigned char> encoded_image) override {
+  }
   void CreateNullAndReturnResult() override {}
   void SignalAlternativeCodePathFinishedForTesting() override;
   void PostDelayedTaskToCurrentThread(const base::Location&,
