064d74d45fd542b027e39955779a2f4824581822
Xiaodan Zhu <zxdan@chromium.org>
2023-08-08 19:16:28 +0000

    cros_next: refactor option buttons
    This cl makes several changes to OptionButtonBase and
    OptionButtonGroup to meet the requirement of jellyroll Combobox use.
    
    1. Make the image and label spacing adjustable.
    2. Make the pre-defined button width as the minimum width.
    
    Bug: b:285409039
    Change-Id: I56cdb3a725f9a9f69cd71aefee6dbabb4ff3385d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4757446
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181086}

--- ash/style/style_viewer/radio_button_group_instances_grid_view_factory.cc
+++ ash/style/style_viewer/radio_button_group_instances_grid_view_factory.cc
@@ -27,6 +27,7 @@ constexpr gfx::Insets kDefaultRadioButtonPadding =
     gfx::Insets::TLBR(8, 12, 8, 12);
 constexpr gfx::Insets kCustomizedRadioButtonDefaultPadding =
     gfx::Insets::TLBR(8, 24, 8, 12);
+constexpr int kCustomizedImageLabelSpacing = 16;
 
 }  // namespace
 
@@ -53,7 +54,7 @@ CreateRadioButtonGroupInstancesGridView() {
       std::make_unique<RadioButtonGroup>(
           kRadioButtonGroupWidth, kInsideBorderInsets, kBetweenChildSpacing,
           RadioButton::IconDirection::kLeading, RadioButton::IconType::kCircle,
-          kDefaultRadioButtonPadding);
+          kDefaultRadioButtonPadding, RadioButton::kImageLabelSpacingDP);
   radio_button_group_with_disabled_button->AddButton(
       RadioButton::PressedCallback(), u"Test Button1");
   radio_button_group_with_disabled_button->AddButton(
@@ -68,7 +69,7 @@ CreateRadioButtonGroupInstancesGridView() {
       std::make_unique<RadioButtonGroup>(
           kRadioButtonGroupWidth, kInsideBorderInsets, kBetweenChildSpacing,
           RadioButton::IconDirection::kFollowing, RadioButton::IconType::kCheck,
-          kCustomizedRadioButtonDefaultPadding);
+          kCustomizedRadioButtonDefaultPadding, kCustomizedImageLabelSpacing);
   customized_button_group->AddButton(RadioButton::PressedCallback(),
                                      u"Test Button1");
   customized_button_group->AddButton(RadioButton::PressedCallback(),
