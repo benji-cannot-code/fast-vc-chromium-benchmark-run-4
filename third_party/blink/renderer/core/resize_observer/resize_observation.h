1427449f93738af09755fd77bdec3ac021d750bd
Kent Tamura <tkent@chromium.org>
2023-08-08 08:06:35 +0000

    [RemoveLegacy] Change the type of ResizeObservation::observation_size_
    ... from DeprecatedLayoutSize to LogicalSize.
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: I09b9216b6a1dfa612cff7210ca1d49dccf53a60a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4758364
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180738}

--- third_party/blink/renderer/core/resize_observer/resize_observation.h
+++ third_party/blink/renderer/core/resize_observer/resize_observation.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_RESIZE_OBSERVER_RESIZE_OBSERVATION_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/layout/geometry/logical_size.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer_box_options.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer_entry.h"
-#include "third_party/blink/renderer/platform/geometry/layout_size.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
@@ -27,12 +27,12 @@ class CORE_EXPORT ResizeObservation final
 
   Element* Target() const { return target_; }
   size_t TargetDepth();
-  // True if observationSize differs from target's current size.
+  // True if observation_size_ differs from target's current size.
   bool ObservationSizeOutOfSync();
-  void SetObservationSize(const DeprecatedLayoutSize&);
+  void SetObservationSize(const LogicalSize&);
   ResizeObserverBoxOptions ObservedBox() const { return observed_box_; }
 
-  DeprecatedLayoutSize ComputeTargetSize() const;
+  LogicalSize ComputeTargetSize() const;
 
   void Trace(Visitor*) const;
 
@@ -40,7 +40,7 @@ class CORE_EXPORT ResizeObservation final
   WeakMember<Element> target_;
   Member<ResizeObserver> observer_;
   // Target size sent in last observation notification.
-  DeprecatedLayoutSize observation_size_;
+  LogicalSize observation_size_;
   ResizeObserverBoxOptions observed_box_;
 };
 
