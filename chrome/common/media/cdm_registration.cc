f97e62b0800d4f317a8d91a94b8a945a70065e64
John Rummell <jrummell@chromium.org>
2023-08-08 01:05:38 +0000

    Add Component Updater support for Widevine on ChromeOS Ash
    This enables ChromeOS Ash to use a component updated Widevine CDM, if
    one is available. Like Linux the CDM must be loaded at boot time, so a
    new CDM is simply registered and will be loaded next time the device
    restarts.
    
    The update is deployed as a disk image, so it needs to be mounted first
    in order to access the files. As well, at boot time the disk image must
    be mounted, so this depends on an init script running on ChromeOS to
    mount the image (if available) at boot time so that Chrome startup can
    access the new CDM.
    
    Tested on a ChromeOS device by manually changing the version of the CDM
    bundled, and then verifying that a newer version is downloaded and used
    next time the device is rebooted.
    
    Bug: b/223462652
    Test: see above
    Change-Id: I674e9c3760b627ffa142b410d0982b5503ee7ac4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4104287
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180631}

--- chrome/common/media/cdm_registration.cc
+++ chrome/common/media/cdm_registration.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "base/no_destructor.h"
+#include "chrome/common/media/component_widevine_cdm_hint_file_linux.h"
 #include "components/cdm/common/cdm_manifest.h"
 #include "media/cdm/supported_audio_codecs.h"
 // Needed for WIDEVINE_CDM_MIN_GLIBC_VERSION. This file is in
@@ -42,9 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gnu/libc-version.h>
 #include "base/version.h"
 #endif  // defined(WIDEVINE_CDM_MIN_GLIBC_VERSION)
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/common/media/component_widevine_cdm_hint_file_linux.h"
-#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #endif  // BUILDFLAG(ENABLE_WIDEVINE)
 
@@ -83,6 +81,7 @@ std::unique_ptr<content::CdmInfo> CreateCdmInfoFromWidevineDirectory(
       media::GetPlatformSpecificDirectory(cdm_base_path)
           .Append(base::GetNativeLibraryName(kWidevineCdmLibraryName));
   if (!base::PathExists(cdm_library_path)) {
+    DLOG(ERROR) << __func__ << " no directory: " << cdm_library_path;
     return nullptr;
   }
 
@@ -91,6 +90,7 @@ std::unique_ptr<content::CdmInfo> CreateCdmInfoFromWidevineDirectory(
   base::Version version;
   media::CdmCapability capability;
   if (!ParseCdmManifestFromPath(manifest_path, &version, &capability)) {
+    DLOG(ERROR) << __func__ << " no manifest: " << manifest_path;
     return nullptr;
   }
 
@@ -122,8 +122,8 @@ content::CdmInfo* GetBundledWidevine() {
       }());
   return s_cdm_info->get();
 }
-#endif  // BUILDFLAG(BUNDLE_WIDEVINE_CDM) && (BUILDFLAG(IS_LINUX) ||
-        // BUILDFLAG(IS_CHROMEOS))
+#endif  // BUILDFLAG(BUNDLE_WIDEVINE_CDM) &&
+        // (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS))
 
 #if BUILDFLAG(ENABLE_WIDEVINE_CDM_COMPONENT) && \
     (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS))
@@ -146,8 +146,8 @@ content::CdmInfo* GetComponentUpdatedWidevine() {
       }());
   return s_cdm_info->get();
 }
-#endif  // BUILDFLAG(ENABLE_WIDEVINE_CDM_COMPONENT) && (BUILDFLAG(IS_LINUX) ||
-        // BUILDFLAG(IS_CHROMEOS))
+#endif  // BUILDFLAG(ENABLE_WIDEVINE_CDM_COMPONENT) &&
+        // (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS))
 
 void AddSoftwareSecureWidevine(std::vector<content::CdmInfo>* cdms) {
   DVLOG(1) << __func__;
--- chrome/common/media/component_widevine_cdm_hint_file_linux.h
+++ chrome/common/media/component_widevine_cdm_hint_file_linux.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
-#if !(BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS))
-#error "This file only applies to desktop Linux."
+#if !(BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS))
+#error "This file only applies to desktop Linux and ChromeOS."
 #endif
 
 namespace base {
