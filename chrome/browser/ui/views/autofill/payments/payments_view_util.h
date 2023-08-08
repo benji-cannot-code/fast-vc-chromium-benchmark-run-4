b5822d47fbbbe42d84fe15b173aa93d57ba4191c
Junhui He <junhuihe@google.com>
2023-08-08 19:10:59 +0000

    [Desktop][UX Enhancement] Move Gpay logo to right for Autofill bubbles and dialogs
    Introduce an intermediary method to switch between TitleWithIconAndSeparatorView and TitleWithIconAfterLabelView to protect the change to move Gpay logo to right.
    1-pager: go/pay-autofill-chrome-desktop-move-gpay-logo-to-right
    
    Screenshots for all the changes:
    
    SaveCard bubble
    https://screenshot.googleplex.com/a7kEvkcJVjKKzeK.png
    
    Autofill error dialog
    https://screenshot.googleplex.com/3no7pZuWW2FZs3z.png
    
    Autofill progress dialog
    https://screenshot.googleplex.com/9JoEEX6WfsyUAz4.png
    mock: https://screenshot.googleplex.com/96SuhEEMafbNDsG
    
    Authentication selection dialog
    https://screenshot.googleplex.com/6RYwpaNQxMci5Je.png
    
    OTP input dialog
    https://screenshot.googleplex.com/7kwgyUWQeh3KHHh.png
    
    Offer notification dialog
    https://screenshot.googleplex.com/AvmLMYkZaLETPse.png
    
    VCN enrollment bubble
    https://screenshot.googleplex.com/yWxECyk7h3zRg89.png
    mock: https://screenshot.googleplex.com/5SbEq6yQywEc3fE.png
    
    VCN fallback bubble
    https://screenshot.googleplex.com/AwSuv89rKNE6BE2.png
    
    Change-Id: I7460fcd43742c526efad568973f84a548b4cea85
    Bug: 1459361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740225
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Junhui He <junhuihe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181077}

--- chrome/browser/ui/views/autofill/payments/payments_view_util.h
+++ chrome/browser/ui/views/autofill/payments/payments_view_util.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/image_model.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/layout/table_layout_view.h"
+#include "ui/views/view.h"
 
 class GURL;
 
@@ -71,6 +72,15 @@ class TitleWithIconAfterLabelView : public views::BoxLayoutView {
   // views::View:
   gfx::Size GetMinimumSize() const override;
 };
+
+// An intermediary method to switch between TitleWithIconAndSeparatorView and
+// TitleWithIconAfterLabelView protected by
+// kAutofillEnableMovingGPayLogoToTheRightOnDesktop.
+// TODO(crbug.com/1459361): Remove this method once
+// kAutofillEnableMovingGPayLogoToTheRightOnDesktop is fully launched.
+std::unique_ptr<views::View> CreateTitleView(
+    const std::u16string& window_title,
+    TitleWithIconAndSeparatorView::Icon icon_to_show);
 
 // Defines a view with legal message. This class handles the legal message
 // parsing and the links clicking events.
