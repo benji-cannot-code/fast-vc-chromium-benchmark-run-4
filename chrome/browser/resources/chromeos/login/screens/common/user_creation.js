b3b6855703933907aaaf087c1386f1ae265470bd
Brahim Chikhaoui <bchikhaoui@google.com>
2023-08-08 07:53:55 +0000

    [OOBE] Remove Device Enrollment shelf button from enroll triage step
    create new oobe state for enroll triage step to nuke the device
    enrollment shelf button.
    
    Bug: b:294952266
    Change-Id: I19f54db14656eba5885596fd3978185f8178c229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4757074
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Auto-Submit: Brahim Chikhaoui <bchikhaoui@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180730}

--- chrome/browser/resources/chromeos/login/screens/common/user_creation.js
+++ chrome/browser/resources/chromeos/login/screens/common/user_creation.js
@@ -228,8 +228,10 @@ class UserCreation extends UserCreationScreenElementBase {
   // clicking back will display user creation screen with child setup step
   // and we need to restore the oobe ui state.
   restoreOobeUIState() {
-    if (this.uiStep === UserCreationUIState.ENROLL_TRIAGE ||
-        this.uiStep === UserCreationUIState.CREATE) {
+    if (this.uiStep === UserCreationUIState.ENROLL_TRIAGE) {
+      Oobe.getInstance().setOobeUIState(OOBE_UI_STATE.ENROLL_TRIAGE);
+    }
+    if (this.uiStep === UserCreationUIState.CREATE) {
       Oobe.getInstance().setOobeUIState(OOBE_UI_STATE.USER_CREATION);
     }
     if (this.uiStep === UserCreationUIState.CHILD_SETUP) {
@@ -282,6 +284,7 @@ class UserCreation extends UserCreationScreenElementBase {
   }
 
   setTriageStep() {
+    Oobe.getInstance().setOobeUIState(OOBE_UI_STATE.ENROLL_TRIAGE);
     this.setUIStep(UserCreationUIState.ENROLL_TRIAGE);
   }
 
