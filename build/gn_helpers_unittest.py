594b10701828d9f6e6d0072f3fe6c66926be2927
Peter Wen <wnwen@chromium.org>
2023-08-08 20:33:49 +0000

    Android: Avoid redirection through depot_tools
    //third_party/depot_tools/ninja.py is unable to find the proper ninja
    binary in //third_party/ninja/ninja on bots. Fix this by directly
    referencing the correct binary.
    
    Bug: 1469636
    Change-Id: I9400b7c89bfb306bdfdbabe5bac4bdb72ea4ecda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761979
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181137}

--- build/gn_helpers_unittest.py
+++ build/gn_helpers_unittest.py
@@ -329,7 +329,7 @@ class UnitTest(unittest.TestCase):
 
       with mock.patch('shutil.which', lambda x: None):
         cmd = gn_helpers.CreateBuildCommand(temp_dir)
-        self.assertIn('depot_tools', cmd[0])
+        self.assertIn('third_party', cmd[0])
         self.assertIn(f'{os.sep}siso', cmd[0])
         self.assertEqual(['ninja', '-C', temp_dir], cmd[1:])
 
@@ -345,7 +345,7 @@ class UnitTest(unittest.TestCase):
 
       with mock.patch('shutil.which', lambda x: None):
         cmd = gn_helpers.CreateBuildCommand(temp_dir)
-        self.assertIn('depot_tools', cmd[0])
+        self.assertIn('third_party', cmd[0])
         self.assertIn(f'{os.sep}ninja', cmd[0])
         self.assertEqual(['-C', temp_dir], cmd[1:])
 
