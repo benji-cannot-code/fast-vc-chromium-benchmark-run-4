17a13d250403a01bb574c8ae6cdfec28b412ef35
Omer Katz <omerkatz@chromium.org>
2023-08-08 09:49:02 +0000

    [heap] Add plugin checks for off-heap collections
    Disallow off-heap collections (stl and WTF) of GCed objects, Members,
    and pointer and references to GCed objects and Members.
    
    Drive-by: Align stubs with actual definitions.
    
    Bug: chromium:1470687
    Change-Id: Ib3bdc1ffbcf48040073ee587b24eb5bfe3cdfc62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755611
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180777}

--- tools/clang/blink_gc_plugin/tests/heap/stubs.h
+++ tools/clang/blink_gc_plugin/tests/heap/stubs.h
@@ -14,33 +14,33 @@ namespace WTF {
 template<typename T> class RefCounted { };
 
 template<typename T> class RawPtr {
-public:
-    operator T*() const { return 0; }
-    T* operator->() const { return 0; }
+ public:
+  operator T*() const { return 0; }
+  T* operator->() const { return 0; }
 };
 
 template<typename T> class scoped_refptr {
-public:
-    ~scoped_refptr() { }
-    operator T*() const { return 0; }
-    T* operator->() const { return 0; }
+ public:
+  ~scoped_refptr() {}
+  operator T*() const { return 0; }
+  T* operator->() const { return 0; }
 };
 
 template<typename T> class WeakPtr {
-public:
-    ~WeakPtr() { }
-    operator T*() const { return 0; }
-    T* operator->() const { return 0; }
+ public:
+  ~WeakPtr() {}
+  operator T*() const { return 0; }
+  T* operator->() const { return 0; }
 };
 
-class DefaultAllocator {
-public:
-    static const bool isGarbageCollected = false;
+class PartitionAllocator {
+ public:
+  static const bool isGarbageCollected = false;
 };
 
 template <typename T,
           size_t inlineCapacity = 0,
-          typename Allocator = DefaultAllocator>
+          typename Allocator = PartitionAllocator>
 class Vector {
  public:
   using iterator = T*;
@@ -56,7 +56,7 @@ class Vector {
 
 template <typename T,
           size_t inlineCapacity = 0,
-          typename Allocator = DefaultAllocator>
+          typename Allocator = PartitionAllocator>
 class Deque {
  public:
   using iterator = T*;
@@ -68,9 +68,8 @@ class Deque {
 };
 
 template <typename ValueArg,
-          typename HashArg = void,
           typename TraitsArg = void,
-          typename Allocator = DefaultAllocator>
+          typename Allocator = PartitionAllocator>
 class HashSet {
  public:
   typedef ValueArg* iterator;
@@ -83,7 +82,7 @@ class HashSet {
 
 template <typename ValueArg,
           typename TraitsArg = void,
-          typename Allocator = DefaultAllocator>
+          typename Allocator = PartitionAllocator>
 class LinkedHashSet {
  public:
   typedef ValueArg* iterator;
@@ -95,9 +94,8 @@ class LinkedHashSet {
 };
 
 template <typename ValueArg,
-          typename HashArg = void,
           typename TraitsArg = void,
-          typename Allocator = DefaultAllocator>
+          typename Allocator = PartitionAllocator>
 class HashCountedSet {
  public:
   ~HashCountedSet() {}
@@ -105,10 +103,9 @@ class HashCountedSet {
 
 template <typename KeyArg,
           typename MappedArg,
-          typename HashArg = void,
           typename KeyTraitsArg = void,
           typename MappedTraitsArg = void,
-          typename Allocator = DefaultAllocator>
+          typename Allocator = PartitionAllocator>
 class HashMap {
  public:
   typedef MappedArg* iterator;
@@ -129,10 +126,10 @@ namespace std {
 namespace std {
 
 template<typename T> class unique_ptr {
-public:
-    ~unique_ptr() { }
-    operator T*() const { return 0; }
-    T* operator->() const { return 0; }
+ public:
+  ~unique_ptr() {}
+  operator T*() const { return 0; }
+  T* operator->() const { return 0; }
 };
 
 template <typename T, typename... Args>
@@ -142,10 +139,18 @@ unique_ptr<T> make_unique(Args&&... args) {
 
 template <typename Key>
 class set {};
+template <typename Key>
+class unordered_set {};
 template <typename Key, typename Value>
 class map {};
+template <typename Key, typename Value>
+class unordered_map {};
 template <typename Elem>
 class vector {};
+template <typename Elem, size_t N>
+class array {};
+template <typename T1, typename T2>
+class pair {};
 
 }  // namespace std
 
@@ -181,12 +186,14 @@ class Visitor {
 
 namespace internal {
 class WriteBarrierPolicyImpl;
+class NoWriteBarrierPolicyImpl;
 class CheckingPolicyImpl;
 class StorateTypeImpl;
 class LocationPolicyImpl;
 
 class StrongMemberTag;
 class WeakMemberTag;
+class UntracedMemberTag;
 
 template <typename StorageType>
 class MemberBase {};
@@ -273,6 +280,13 @@ using WeakMember = internal::BasicMember<T,
                                          internal::StorateTypeImpl>;
 
 template <typename T>
+using UntracedMember = internal::BasicMember<T,
+                                             internal::UntracedMemberTag,
+                                             internal::NoWriteBarrierPolicyImpl,
+                                             internal::CheckingPolicyImpl,
+                                             internal::StorateTypeImpl>;
+
+template <typename T>
 using Persistent = internal::BasicPersistent<T,
                                              internal::StrongPersistentPolicy,
                                              internal::LocationPolicyImpl,
@@ -319,6 +333,8 @@ template <typename T>
 using Member = cppgc::Member<T>;
 template <typename T>
 using WeakMember = cppgc::WeakMember<T>;
+template <typename T>
+using UntracedMember = cppgc::UntracedMember<T>;
 template <typename T>
 using Persistent = cppgc::Persistent<T>;
 template <typename T>
@@ -374,7 +390,7 @@ class HeapDeque : public GarbageCollected<HeapDeque<T, inlineCapacity>>,
 
 template <typename T>
 class HeapHashSet : public GarbageCollected<HeapHashSet<T>>,
-                    public HashSet<T, void, void, HeapAllocator> {};
+                    public HashSet<T, void, HeapAllocator> {};
 
 template <typename T>
 class HeapLinkedHashSet : public GarbageCollected<HeapLinkedHashSet<T>>,
@@ -382,12 +398,11 @@ class HeapLinkedHashSet : public GarbageCollected<HeapLinkedHashSet<T>>,
 
 template <typename T>
 class HeapHashCountedSet : public GarbageCollected<HeapHashCountedSet<T>>,
-                           public HashCountedSet<T, void, void, HeapAllocator> {
-};
+                           public HashCountedSet<T, void, HeapAllocator> {};
 
 template <typename K, typename V>
 class HeapHashMap : public GarbageCollected<HeapHashMap<K, V>>,
-                    public HashMap<K, V, void, void, void, HeapAllocator> {};
+                    public HashMap<K, V, void, void, HeapAllocator> {};
 
 template<typename T>
 struct TraceIfNeeded {
