7a28317ee078d8f488444b28833ef6809cb04378
Alan Screen <awscreen@chromium.org>
2023-08-08 16:37:20 +0000

    Track analyzing content for scripted print preview
    Printing entry points need to note when content analysis is happening,
    otherwise print jobs can invoke system calls that happen when they
    shouldn't.  That can lead to system dialogs appearing when they are not
    supposed to, and also to crashes.
    
    Include tracking of this state for scripted print preview so that sites
    that print using window.print() can succeed when content analysis is
    used.
    
    Fixed: 1464566
    Change-Id: I7bf8e7643720d289d0d6c861a7128001bbdd65fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4753434
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1180972}

--- chrome/browser/printing/print_view_manager.cc
+++ chrome/browser/printing/print_view_manager.cc
@@ -416,7 +416,9 @@ void PrintViewManager::ShowScriptedPrintPreview(bool source_is_modifiable) {
   DCHECK(print_preview_rfh_);
   if (GetCurrentTargetFrame() != print_preview_rfh_)
     return;
-
+#if BUILDFLAG(ENABLE_PRINT_CONTENT_ANALYSIS)
+  set_analyzing_content(/*analyzing=*/true);
+#endif
   RejectPrintPreviewRequestIfRestricted(
       print_preview_rfh_->GetGlobalId(),
       base::BindOnce(&PrintViewManager::OnScriptedPrintPreviewCallback,
@@ -428,6 +430,9 @@ void PrintViewManager::OnScriptedPrintPreviewCallback(
     bool source_is_modifiable,
     content::GlobalRenderFrameHostId rfh_id,
     bool should_proceed) {
+#if BUILDFLAG(ENABLE_PRINT_CONTENT_ANALYSIS)
+  set_analyzing_content(/*analyzing=*/false);
+#endif
   if (!should_proceed) {
     OnPrintPreviewRequestRejected(rfh_id);
     return;
