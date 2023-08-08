5439688c02ea46901f792057605d47581c1e7dfb
David Padlipsky <dpad@google.com>
2023-08-08 21:28:47 +0000

    settings: Add kInputDeviceSettingsSplit feature flag for bluetooth tests
    Adds kInputDeviceSettingsSplit flag for bluetooth device detail tests as
    they require the per-device settings routes which are only generated
    when the flag is enabled.
    
    Bug: b:1471238
    Change-Id: I72c5a30ea9620e1c9c61514e5d27cf85d5e9e446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762009
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181163}

--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -604,6 +604,7 @@ TEST_F('OSSettingsCrostiniExtraContainerPageTest', 'AllJsTests', () => {
  [
    'OsBluetoothPageOsBluetoothDeviceDetailSubpage',
    'os_bluetooth_page/os_bluetooth_device_detail_subpage_tests.js',
+   {enabled: ['ash::features::kInputDeviceSettingsSplit']}
  ],
  [
    'OsBluetoothPageOsBluetoothPairingDialog',
