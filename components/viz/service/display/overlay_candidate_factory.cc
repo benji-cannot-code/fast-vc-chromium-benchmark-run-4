1970d41f8a81d968d51933f5da8911b277507e78
Antonio Gomes <tonikitoo@igalia.com>
2023-08-08 16:35:06 +0000

    Harden the logic that sets viz::OverlayCandidate::has_mask_info to true
    Set viz::OverlayCandidate::has_mask_info to true only
    if it is a filter that is actually rounded corner and not the
    gradient, and fail if it is the gradient.
    
    BUG=NONE
    
    Change-Id: Ib58d7ddfad5abd42faeea9df98bbec9e7c9510b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738933
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180968}

--- components/viz/service/display/overlay_candidate_factory.cc
+++ components/viz/service/display/overlay_candidate_factory.cc
@@ -140,7 +140,9 @@ OverlayCandidate::CandidateStatus OverlayCandidateFactory::FromDrawQuad(
     return CandidateStatus::kFailBlending;
   }
 
-  if (!sqs->mask_filter_info.IsEmpty() && !context_.supports_mask_filter) {
+  if (!sqs->mask_filter_info.IsEmpty() &&
+      (!context_.supports_mask_filter ||
+       sqs->mask_filter_info.HasGradientMask())) {
     return CandidateStatus::kFailMaskFilterNotSupported;
   }
 
