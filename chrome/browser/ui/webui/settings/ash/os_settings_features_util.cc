6dd0f2ce5c1a4ce9311bec6e8a0ee729253f0fc0
Wes Okuhara <wesokuhara@google.com>
2023-08-08 19:11:32 +0000

    CrOS Settings: Add IsPowerwashAllowed() and backfill tests
    Extract IsPowerwashAllowed() method to utils file and backfill unit
    tests for it.
    
    Bug: b/293937513
    Test: unit_tests --gtest_filter="OsSettingsFeaturesUtilTest"
    Change-Id: Ieb374543659ad8eca19c0c89c9fc4508ede974ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4757900
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181078}

--- chrome/browser/ui/webui/settings/ash/os_settings_features_util.cc
+++ chrome/browser/ui/webui/settings/ash/os_settings_features_util.cc
@@ -8,16 +8,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/arc_features.h"
 #include "base/feature_list.h"
 #include "chrome/browser/ash/arc/arc_util.h"
+#include "chrome/browser/policy/management_utils.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+
 #include "components/user_manager/user_manager.h"
+
+#include "chrome/browser/ash/login/users/chrome_user_manager.h"
 
 namespace ash::settings {
 
 bool IsGuestModeActive() {
-  return user_manager::UserManager::Get()->IsLoggedInAsGuest() ||
-         user_manager::UserManager::Get()->IsLoggedInAsManagedGuestSession();
+  return ash::ChromeUserManager::Get()->IsLoggedInAsGuest() ||
+         ash::ChromeUserManager::Get()->IsLoggedInAsManagedGuestSession();
+}
+
+bool IsChildUser() {
+  return ash::ChromeUserManager::Get()->IsLoggedInAsChildUser();
+}
+
+bool IsDeviceEnterpriseManaged() {
+  return ash::ChromeUserManager::Get()->IsEnterpriseManaged();
+}
+
+bool IsPowerwashAllowed() {
+  return !IsDeviceEnterpriseManaged() && !IsGuestModeActive() && !IsChildUser();
 }
 
 bool ShouldShowParentalControlSettings(const Profile* profile) {
