b092ff6403816568f374185ec1596891e6b0e361
Brahim Chikhaoui <bchikhaoui@google.com>
2023-08-08 20:19:47 +0000

    Reland "Reland "[OOBE] Enable OobeSOftwareUpdate feature flag""
    This is a reland of commit 98fffd7e2326f4b3a2387b6550162b1065c5252d
    
    Original change's description:
    > Reland "[OOBE] Enable OobeSOftwareUpdate feature flag"
    >
    > This is a reland of commit 30fb83577427d557368ee651581f0f9157031bc6
    >
    > Original change's description:
    > > [OOBE] Enable OobeSOftwareUpdate feature flag
    > >
    > > enable the feature flag for consumer update screen by default.
    > >
    > > Bug: b:292904894
    > > Change-Id: I42cc6760583310807cd47612ea80ef3bc17edae3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727699
    > > Reviewed-by: Osama Fathy <osamafathy@google.com>
    > > Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1178236}
    >
    > Bug: b:292904894
    > Change-Id: I54741e7f37a8c5ecc93686658f8a6321743a4b18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738338
    > Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    > Reviewed-by: Osama Fathy <osamafathy@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1178590}
    
    Bug: b:292904894
    Change-Id: Ia7e96cd6405c5e220d686b534dd4450250ecfe77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754153
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181129}

--- chrome/browser/ash/login/test/oobe_screens_utils.cc
+++ chrome/browser/ash/login/test/oobe_screens_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/system/input_device_settings/input_device_settings_controller_impl.h"
 #include "chrome/browser/ash/login/oobe_screen.h"
+#include "chrome/browser/ash/login/screens/consumer_update_screen.h"
 #include "chrome/browser/ash/login/screens/sync_consent_screen.h"
 #include "chrome/browser/ash/login/screens/update_screen.h"
 #include "chrome/browser/ash/login/test/js_checker.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/wizard_controller.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/ui/webui/ash/login/consolidated_consent_screen_handler.h"
+#include "chrome/browser/ui/webui/ash/login/consumer_update_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/enrollment_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/fingerprint_setup_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/guest_tos_screen_handler.h"
@@ -99,6 +101,27 @@ void ExitUpdateScreenNoUpdate() {
   screen->GetVersionUpdaterForTesting()->UpdateStatusChangedForTesting(status);
 }
 
+void WaitForConsumerUpdateScreen() {
+  if (!LoginDisplayHost::default_host()->GetWizardContext()->is_branded_build) {
+    return;
+  }
+  WaitFor(ConsumerUpdateScreenView::kScreenId);
+  OobeJS().CreateVisibilityWaiter(true, {"consumer-update"})->Wait();
+}
+
+void ExitConsumerUpdateScreenNoUpdate() {
+  if (!LoginDisplayHost::default_host()->GetWizardContext()->is_branded_build) {
+    return;
+  }
+  update_engine::StatusResult status;
+  status.set_current_operation(update_engine::Operation::ERROR);
+
+  ConsumerUpdateScreen* screen =
+      WizardController::default_controller()->GetScreen<ConsumerUpdateScreen>();
+  screen->get_version_updater_for_testing()->UpdateStatusChangedForTesting(
+      status);
+}
+
 void WaitForFingerprintScreen() {
   LOG(INFO) << "Waiting for 'fingerprint-setup' screen.";
   OobeScreenWaiter(FingerprintSetupScreenView::kScreenId).Wait();
@@ -154,6 +177,10 @@ void WaitForEnrollmentScreen() {
 
 void WaitForUserCreationScreen() {
   WaitFor(UserCreationView::kScreenId);
+}
+
+void TapForPersonalUseCrRadioButton() {
+  OobeJS().TapOnPath({"user-creation", "selfButton"});
 }
 
 void TapUserCreationNext() {
--- chrome/browser/ash/login/test/oobe_screens_utils.h
+++ chrome/browser/ash/login/test/oobe_screens_utils.h
@@ -21,6 +21,8 @@ void WaitForNetworkSelectionScreen();
 void TapNetworkSelectionNext();
 void WaitForUpdateScreen();
 void ExitUpdateScreenNoUpdate();
+void WaitForConsumerUpdateScreen();
+void ExitConsumerUpdateScreenNoUpdate();
 void WaitForFingerprintScreen();
 void ExitFingerprintPinSetupScreen();
 void WaitForPinSetupScreen();
@@ -28,6 +30,7 @@ void ExitPinSetupScreen();
 void SkipToEnrollmentOnRecovery();
 void WaitForEnrollmentScreen();
 void WaitForUserCreationScreen();
+void TapForPersonalUseCrRadioButton();
 void TapUserCreationNext();
 // Wait for OobeUI to finish loading.
 void WaitForOobeJSReady();
