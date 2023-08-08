12fc572969e49a01ffa490d9c3492261e526e8a8
Anders Hartvoll Ruud <andruud@chromium.org>
2023-08-08 12:27:49 +0000

    [@scope] Store bucket information outside of RuleData
    RuleData currently has a memory optimization where we "borrow"
    the area ultimately used to store bloom filter hashes during
    construction of the RuleSet. A consequence of this optimization is that
    we delay computation of the bloom filter hashes until the end of
    RuleSet::Compact. This technique conflicts with our plan to support
    bloom filtering for @scope'd rules, because the relevant @scope for
    a given RuleData is only known around the time of RuleData construction.
    
    This CL restores the old behavior of computing bloom filter hashes
    during RuleData's constructor. However, since de-unionifying the
    bucket_number_/order_in_bucket_ members of RuleData would consume
    too much memory (measurements have shown 20k+ RuleData instances on
    modern sites), we store "BucketData" objects in a separate vector
    which is cleared at the end of RuleSet::Compact.
    
    The style perftest seems to be neutral for this CL.
    
    Bug: 1280240
    Change-Id: Ie4940922f57693f211e1595775ab58ba29424b4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755455
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180838}

--- third_party/blink/renderer/core/css/rule_set.cc
+++ third_party/blink/renderer/core/css/rule_set.cc
@@ -124,7 +124,9 @@ RuleData::RuleData(StyleRule* rule,
           false),  // Will be computed in ComputeEntirelyCoveredByBucketing().
       is_easy_(false),  // Ditto.
       is_starting_style_((add_rule_flags & kRuleIsStartingStyle) != 0),
-      descendant_selector_identifier_hashes_() {}
+      descendant_selector_identifier_hashes_() {
+  ComputeBloomFilterHashes();
+}
 
 void RuleData::ComputeEntirelyCoveredByBucketing() {
   is_easy_ = EasySelectorChecker::IsEasy(&Selector());
@@ -150,9 +152,6 @@ void RuleData::ResetEntirelyCoveredByBucketing() {
 }
 
 void RuleData::ComputeBloomFilterHashes() {
-#if DCHECK_IS_ON()
-  marker_ = 0;
-#endif
   SelectorFilter::CollectIdentifierHashes(
       Selector(), descendant_selector_identifier_hashes_,
       kMaximumIdentifierCount);
@@ -176,7 +175,6 @@ void RuleSet::AddToRuleSet(const AtomicString& key,
 void RuleSet::AddToRuleSet(HeapVector<RuleData>& rules,
                            const RuleData& rule_data) {
   rules.push_back(rule_data);
-  rules.back().ComputeBloomFilterHashes();
   rules.back().ComputeEntirelyCoveredByBucketing();
   need_compaction_ = true;
 }
@@ -893,8 +891,8 @@ void RuleMap::Add(const AtomicString& key, const RuleData& rule_data) {
   if (RuntimeEnabledFeatures::CSSEasySelectorsEnabled()) {
     rule_data_copy.ComputeEntirelyCoveredByBucketing();
   }
-  rule_data_copy.SetBucketInformation(rules.bucket_number,
-                                      /*order_in_bucket=*/rules.length++);
+  bucket_data_.push_back(BucketData{.bucket_number = rules.bucket_number,
+                                    .order_in_bucket = rules.length++});
   backing.push_back(std::move(rule_data_copy));
 }
 
@@ -903,6 +901,7 @@ void RuleMap::Compact() {
     return;
   }
   if (backing.empty()) {
+    DCHECK(bucket_data_.empty());
     // Nothing to do.
     compacted = true;
     return;
@@ -916,8 +915,8 @@ void RuleMap::Compact() {
   // First, we make an array that contains the number of elements in each
   // bucket, indexed by the bucket number.
   std::unique_ptr<unsigned[]> counts(new unsigned[num_buckets]());
-  for (const RuleData& rule_data : backing) {
-    ++counts[rule_data.GetBucketNumber()];
+  for (const BucketData& bucket_data : bucket_data_) {
+    ++counts[bucket_data.bucket_number];
   }
 
   // Do the prefix sum. After this, counts[i] is the desired start index
@@ -941,33 +940,35 @@ void RuleMap::Compact() {
   // because we put it there earlier), skip to the next array slot.
   // These will happen exactly n times each, giving us our O(n) runtime.
   for (wtf_size_t i = 0; i < backing.size();) {
-    const RuleData& rule_data = backing[i];
+    const BucketData& bucket_data = bucket_data_[i];
     wtf_size_t correct_pos =
-        counts[rule_data.GetBucketNumber()] + rule_data.GetOrderInBucket();
+        counts[bucket_data.bucket_number] + bucket_data.order_in_bucket;
     if (i == correct_pos) {
       ++i;
     } else {
       using std::swap;
       swap(backing[i], backing[correct_pos]);
+      swap(bucket_data_[i], bucket_data_[correct_pos]);
     }
   }
 
-  // Now that we don't need the grouping information anymore, we can compute
-  // the Bloom filter hashes that want to stay in the same memory area.
-  for (RuleData& rule_data : backing) {
-    rule_data.ComputeBloomFilterHashes();
-  }
+  // We're done with the bucket data, so we can release the memory.
+  // If we need the bucket data again, it will be reconstructed by
+  // RuleMap::Uncompact.
+  bucket_data_.clear();
 
   compacted = true;
 }
 
 void RuleMap::Uncompact() {
+  bucket_data_.resize(backing.size());
+
   num_buckets = 0;
   for (auto& [key, value] : buckets) {
     unsigned i = 0;
-    for (RuleData& rule_data : GetRulesFromExtent(value)) {
-      rule_data.SetBucketInformation(/*bucket_number=*/num_buckets,
-                                     /*order_in_bucket=*/i++);
+    for (BucketData& bucket_data : GetBucketDataFromExtent(value)) {
+      bucket_data =
+          BucketData{.bucket_number = num_buckets, .order_in_bucket = i++};
     }
     value.bucket_number = num_buckets++;
     value.length = i;
@@ -994,8 +995,8 @@ void RuleMap::Merge(const RuleMap& other, int offset) {
       for (const RuleData& rule_data : other.GetRulesFromExtent(extent)) {
         backing.push_back(rule_data);
         backing.back().AdjustPosition(offset);
-        backing.back().SetBucketInformation(rules.bucket_number,
-                                            /*order_in_bucket=*/rules.length++);
+        bucket_data_.push_back(BucketData{.bucket_number = rules.bucket_number,
+                                          .order_in_bucket = rules.length++});
       }
     }
   } else {
@@ -1020,12 +1021,15 @@ void RuleMap::Merge(const RuleMap& other, int offset) {
 
     // Now that we have the mapping, we can just copy over all the RuleData
     // and adjust the buckets/positions as we go.
-    for (const RuleData& rule_data : other.backing) {
-      const RuleMap::Extent& extent = extents[rule_data.GetBucketNumber()];
+    for (wtf_size_t i = 0; i < other.backing.size(); ++i) {
+      const BucketData& bucket_data = other.bucket_data_[i];
+      const RuleData& rule_data = other.backing[i];
+      const RuleMap::Extent& extent = extents[bucket_data.bucket_number];
       backing.push_back(rule_data);
       backing.back().AdjustPosition(offset);
-      backing.back().AdjustBucketPosition(extent.bucket_number,
-                                          /*offset=*/extent.length);
+      bucket_data_.push_back(BucketData{
+          .bucket_number = extent.bucket_number,
+          .order_in_bucket = bucket_data.order_in_bucket + extent.length});
     }
   }
 }
