f7a7de6dd787e85823da8178d9de6acc4a6076c0
Dibyajyoti Pal <dibyapal@google.com>
2023-08-08 14:19:51 +0000

    [dPWA/Link Capturing] Move supported_links items to cr_components
    This CL moves the supported_links item component and the overlapping
    apps dialog frontend component to cr_components/ so that it can be
    reused by both Desktop and CrOS platforms.
    
    The ReduxStore based behavior is removed, and the sites that show
    the supported links item instead read the apps from the ReduxStore
    and pass it to the components via Polymer 1 way binding.
    
    Verified manually that this works, need to fix the OSSettingsTest for
    this component.
    
    Bug: 1412856, b/294232431
    Change-Id: Ie7774f7eb674eeaf3a0841a24e31dffce227ca26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4749623
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180889}

--- chrome/test/data/webui/settings/chromeos/os_apps_page/app_management_page/supported_links_item_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_apps_page/app_management_page/supported_links_item_test.ts
@@ -7,7 +7,7 @@ import 'chrome://os-settings/lazy_load.js';
 
 import {AppManagementSupportedLinksItemElement, AppManagementSupportedLinksOverlappingAppsDialogElement} from 'chrome://os-settings/lazy_load.js';
 import {AppManagementStore, updateSelectedAppId} from 'chrome://os-settings/os_settings.js';
-import {AppType, WindowMode} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
+import {App, AppType, WindowMode} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {CrRadioButtonElement} from 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.js';
 import {CrRadioGroupElement} from 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.js';
 import {assertEquals, assertFalse, assertNull, assertTrue} from 'chrome://webui-test/chai_assert.js';
@@ -32,6 +32,11 @@ suite('<app-management-supported-links-item>', () => {
     supportedLinksItem.remove();
   });
 
+  function createSupportedLinksItemForApp(app: App): void {
+    supportedLinksItem.app = app;
+    supportedLinksItem.apps = AppManagementStore.getInstance().data.apps;
+  }
+
   test('PWA - preferred -> browser', async () => {
     const pwaOptions = {
       type: AppType.kWeb,
@@ -48,7 +53,7 @@ suite('<app-management-supported-links-item>', () => {
 
     assertTrue(!!AppManagementStore.getInstance().data.apps[app.id]);
 
-    supportedLinksItem.app = app;
+    createSupportedLinksItemForApp(app);
 
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
@@ -92,7 +97,7 @@ suite('<app-management-supported-links-item>', () => {
 
     assertTrue(!!AppManagementStore.getInstance().data.apps[app.id]);
 
-    supportedLinksItem.app = app;
+    createSupportedLinksItemForApp(app);
 
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
@@ -136,7 +141,7 @@ suite('<app-management-supported-links-item>', () => {
 
     assertTrue(!!AppManagementStore.getInstance().data.apps[app.id]);
 
-    supportedLinksItem.app = app;
+    createSupportedLinksItemForApp(app);
 
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
@@ -162,7 +167,7 @@ suite('<app-management-supported-links-item>', () => {
 
     assertTrue(!!AppManagementStore.getInstance().data.apps[app.id]);
 
-    supportedLinksItem.app = app;
+    createSupportedLinksItemForApp(app);
 
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
@@ -195,7 +200,7 @@ suite('<app-management-supported-links-item>', () => {
 
     assertTrue(!!AppManagementStore.getInstance().data.apps[app.id]);
 
-    supportedLinksItem.app = app;
+    createSupportedLinksItemForApp(app);
 
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
@@ -255,16 +260,16 @@ suite('<app-management-supported-links-item>', () => {
     };
 
     // Add PWA app, and make it the currently selected app.
-    const app = await fakeHandler.addApp('app1', pwaOptions);
+    const app1 = await fakeHandler.addApp('app1', pwaOptions);
     await fakeHandler.addApp('app2', pwaOptions);
     fakeHandler.overlappingAppIds = ['app2'];
 
-    AppManagementStore.getInstance().dispatch(updateSelectedAppId(app.id));
+    AppManagementStore.getInstance().dispatch(updateSelectedAppId(app1.id));
 
     await fakeHandler.flushPipesForTesting();
 
-    assertTrue(!!AppManagementStore.getInstance().data.apps[app.id]);
-    supportedLinksItem.app = app;
+    assertTrue(!!AppManagementStore.getInstance().data.apps[app1.id]);
+    createSupportedLinksItemForApp(app1);
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
     await flushTasks();
@@ -306,7 +311,7 @@ suite('<app-management-supported-links-item>', () => {
         supportedLinksItem.shadowRoot!.querySelector('#overlapDialog');
     assertNull(overlapDialog);
 
-    const selectedApp = AppManagementStore.getInstance().data.apps[app.id];
+    const selectedApp = AppManagementStore.getInstance().data.apps[app1.id];
     assertTrue(!!selectedApp);
     assertFalse(selectedApp.isPreferredApp);
     const radioGroup =
@@ -323,16 +328,16 @@ suite('<app-management-supported-links-item>', () => {
     };
 
     // Add PWA app, and make it the currently selected app.
-    const app = await fakeHandler.addApp('app1', pwaOptions);
+    const app1 = await fakeHandler.addApp('app1', pwaOptions);
     await fakeHandler.addApp('app2', pwaOptions);
     fakeHandler.overlappingAppIds = ['app2'];
 
-    AppManagementStore.getInstance().dispatch(updateSelectedAppId(app.id));
+    AppManagementStore.getInstance().dispatch(updateSelectedAppId(app1.id));
 
     await fakeHandler.flushPipesForTesting();
 
-    assertTrue(!!AppManagementStore.getInstance().data.apps[app.id]);
-    supportedLinksItem.app = app;
+    assertTrue(!!AppManagementStore.getInstance().data.apps[app1.id]);
+    createSupportedLinksItemForApp(app1);
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
     await flushTasks();
@@ -371,7 +376,7 @@ suite('<app-management-supported-links-item>', () => {
 
     assertNull(supportedLinksItem.shadowRoot!.querySelector('#overlapDialog'));
 
-    const selectedApp = AppManagementStore.getInstance().data.apps[app.id];
+    const selectedApp = AppManagementStore.getInstance().data.apps[app1.id];
     assertTrue(!!selectedApp);
     assertTrue(selectedApp.isPreferredApp);
     const radioGroup =
@@ -402,7 +407,7 @@ suite('<app-management-supported-links-item>', () => {
     await fakeHandler.flushPipesForTesting();
 
     assertTrue(!!AppManagementStore.getInstance().data.apps[app1.id]);
-    supportedLinksItem.app = app1;
+    createSupportedLinksItemForApp(app1);
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
     await flushTasks();
@@ -434,7 +439,7 @@ suite('<app-management-supported-links-item>', () => {
 
     assertTrue(!!AppManagementStore.getInstance().data.apps[app1.id]);
     assertTrue(!!AppManagementStore.getInstance().data.apps[app2.id]);
-    supportedLinksItem.app = app1;
+    createSupportedLinksItemForApp(app1);
     replaceBody(supportedLinksItem);
     await fakeHandler.flushPipesForTesting();
     await flushTasks();
--- ui/webui/resources/cr_components/app_management/constants.ts
+++ ui/webui/resources/cr_components/app_management/constants.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {App} from './app_management.mojom-webui.js';
+
 export {AppType, InstallReason, InstallSource, OptionalBool, RunOnOsLogin, RunOnOsLoginMode, WindowMode} from './app_management.mojom-webui.js';
 
 /**
@@ -79,3 +81,7 @@ export enum AppManagementUserAction {
   FILE_HANDLING_TURNED_OFF = 30,
   FILE_HANDLING_OVERFLOW_SHOWN = 31,
 }
+
+// A Record (tuple) of app IDs to app used mostly for the supported links
+// frontend components.
+export type AppMap = Record<string, App>;
