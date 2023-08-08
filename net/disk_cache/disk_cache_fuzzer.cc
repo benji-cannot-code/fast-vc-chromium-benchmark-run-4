cb5674a916821846350ab52aaf484363d52b2ade
Nico Weber <thakis@chromium.org>
2023-08-08 14:38:46 +0000

    Prepare for unreachable code warnings in fuzzer builds
    Bug: 1063180
    Change-Id: I844f6f9382057ba43e9f8f9e7a066191ad4a96da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755409
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180899}

--- net/disk_cache/disk_cache_fuzzer.cc
+++ net/disk_cache/disk_cache_fuzzer.cc
@@ -273,16 +273,13 @@ net::CacheType GetCacheTypeAndPrint(
     case disk_cache_fuzzer::FuzzCommands::APP_CACHE:
       MAYBE_PRINT << "Cache type = APP_CACHE." << std::endl;
       return net::CacheType::APP_CACHE;
-      break;
     case disk_cache_fuzzer::FuzzCommands::REMOVED_MEDIA_CACHE:
       // Media cache no longer in use; handle as HTTP_CACHE
       MAYBE_PRINT << "Cache type = REMOVED_MEDIA_CACHE." << std::endl;
       return net::CacheType::DISK_CACHE;
-      break;
     case disk_cache_fuzzer::FuzzCommands::SHADER_CACHE:
       MAYBE_PRINT << "Cache type = SHADER_CACHE." << std::endl;
       return net::CacheType::SHADER_CACHE;
-      break;
     case disk_cache_fuzzer::FuzzCommands::PNACL_CACHE:
       // Simple cache won't handle PNACL_CACHE.
       if (backend == disk_cache_fuzzer::FuzzCommands::SIMPLE) {
@@ -291,19 +288,15 @@ net::CacheType GetCacheTypeAndPrint(
       }
       MAYBE_PRINT << "Cache type = PNACL_CACHE." << std::endl;
       return net::CacheType::PNACL_CACHE;
-      break;
     case disk_cache_fuzzer::FuzzCommands::GENERATED_BYTE_CODE_CACHE:
       MAYBE_PRINT << "Cache type = GENERATED_BYTE_CODE_CACHE." << std::endl;
       return net::CacheType::GENERATED_BYTE_CODE_CACHE;
-      break;
     case disk_cache_fuzzer::FuzzCommands::GENERATED_NATIVE_CODE_CACHE:
       MAYBE_PRINT << "Cache type = GENERATED_NATIVE_CODE_CACHE." << std::endl;
       return net::CacheType::GENERATED_NATIVE_CODE_CACHE;
-      break;
     case disk_cache_fuzzer::FuzzCommands::DISK_CACHE:
       MAYBE_PRINT << "Cache type = DISK_CACHE." << std::endl;
       return net::CacheType::DISK_CACHE;
-      break;
   }
 }
 
@@ -1124,7 +1117,6 @@ void DiskCacheLPMFuzzer::RunCommands(
       }
       case disk_cache_fuzzer::FuzzCommand::FUZZ_COMMAND_ONEOF_NOT_SET: {
         continue;
-        break;
       }
     }
   }
