d122d0c5411aeb12f9f9c1dbf1c9e2bc0f1d21e2
YuhanYang <yyhyyh@google.com>
2023-08-08 23:43:10 +0000

    customization: Update dropdown to include key combination
    Use select element instead of SettingsDropdownMenu element to add
    key combination option and make it more customizable.
    
    Screenshot:
    https://screenshot.googleplex.com/5xjCEYGag5try6b
    https://screenshot.googleplex.com/5X43KxLvuHysUQt
    
    Mock:
    https://screenshot.googleplex.com/6K9XqyKQnVCSVhm
    
    Bug: b/241965717
    Test: Visual effect on DUT and VM, browser_tests.
    Change-Id: Iaac4865ba8685e1bca2217f3d7cdfffe9e079625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752683
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181226}

--- chrome/browser/ui/webui/settings/ash/device_section.cc
+++ chrome/browser/ui/webui/settings/ash/device_section.cc
@@ -1879,6 +1879,8 @@ void DeviceSection::AddCustomizeButtonsPageStrings(
   static constexpr webui::LocalizedString kCustomizeButtonsPageStrings[] = {
       {"customizeMouseButtonsTitle",
        IDS_SETTINGS_CUSTOMIZE_MOUSE_BUTTONS_TITLE},
+      {"keyCombinationOptionLabel", IDS_SETTINGS_KEY_COMBINATION_OPTION_LABEL},
+      {"noRemappingOptionLabel", IDS_SETTINGS_NO_REMAPPING_OPTION_LABEL},
   };
   html_source->AddLocalizedStrings(kCustomizeButtonsPageStrings);
 }
