cfc69474cd4193aeefb12fc9ffe37ebc8e3e78f6
Hubert Chao <hchao@chromium.org>
2023-08-08 16:49:37 +0000

    Turn on ChromeRootStoreUsed by default on Android Browser + CCT. Fix it so that (a) WebView doesn't turn it on, and (b) fix a test to continue working.
    Also remove the fieldtrial_testing_config entry.
    
    Change-Id: Idb4276bd10723db045a56f46a6a55799e7b8f8c3
    Bug: 1365571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4739896
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180983}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -10576,8 +10576,6 @@ static bool SystemSupportsHardFailRevocationChecking() {
 // several tests are effected because our testing EV certificate won't be
 // recognised as EV.
 static bool SystemUsesChromiumEVMetadata() {
-  if (UsingBuiltinCertVerifier())
-    return true;
 #if defined(PLATFORM_USES_CHROMIUM_EV_METADATA)
   return true;
 #else
