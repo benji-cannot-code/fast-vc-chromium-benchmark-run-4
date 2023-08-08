0e1f04cdb40c9ea77f18e5eaa885139c97890251
Junhui He <junhuihe@google.com>
2023-08-08 18:39:02 +0000

    Feature flag for enabling FPAN risk-based retrieval
    dd: go/pay-autofill-hagrid-integration-chrome-dd
    
    Change-Id: Ie85a77eea3cfda8c6140e5f42e65e8d66f105f62
    Bug: 1470933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754740
    Commit-Queue: Junhui He <junhuihe@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181059}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -253,6 +253,9 @@ extern const char kAutofillEnableCvcStorageAndFillingDescription[];
 extern const char kAutofillEnableFIDOProgressDialogName[];
 extern const char kAutofillEnableFIDOProgressDialogDescription[];
 
+extern const char kAutofillEnableFpanRiskBasedAuthenticationName[];
+extern const char kAutofillEnableFpanRiskBasedAuthenticationDescription[];
+
 extern const char kAutofillEnableIbanClientSideUrlFilteringName[];
 extern const char kAutofillEnableIbanClientSideUrlFilteringDescription[];
 
