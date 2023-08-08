4e83fd8943d70dab317d3bde7683931827e1703f
Vivian Pao <vpao@google.com>
2023-08-08 06:24:48 +0000

    [Promise Icons] Add context menu with pin option for promise shelf item
    Create a context menu for promise app shelf items. The menu should
    only have the option to toggle pinning the promise app in the Shelf (no
    launch capabilities).
    
    Bug: b/261907856
    Change-Id: I8d7d9d1b63e82f06a020b0aa68c9c5dad44bea54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4756026
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180709}

--- chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/shelf_item_delegate.h"
 #include "ash/public/cpp/shelf_model.h"
 #include "base/containers/contains.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/policy_util.h"
+#include "chrome/browser/apps/app_service/promise_apps/promise_app_registry_cache.h"
 #include "chrome/browser/ash/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_utils.h"
@@ -111,6 +113,13 @@ bool IsAppHiddenFromShelf(Profile* profile, const std::string& app_id) {
 bool IsAppPinEditable(apps::AppType app_type,
                       const std::string& app_id,
                       Profile* profile) {
+  if (ash::features::ArePromiseIconsEnabled() &&
+      apps::AppServiceProxyFactory::GetForProfile(profile)
+          ->PromiseAppRegistryCache()
+          ->GetPromiseAppForStringPackageId(app_id)) {
+    return true;
+  }
+
   if (IsAppHiddenFromShelf(profile, app_id)) {
     return false;
   }
