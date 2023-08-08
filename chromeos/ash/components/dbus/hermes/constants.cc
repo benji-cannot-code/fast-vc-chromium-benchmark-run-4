d59165a92c4fe387708a4bf865d24acca30de7de
Nikhil Nayunigari <nikhilcn@google.com>
2023-08-08 22:41:39 +0000

    [CrOS Cellular] Modifying the hermes network operation timeout
    Our operation timeout was set to 4 min and that is in line with
    platform's timeout for carrier call. However, theoretically, the call
    could take little over 4 mins end to end if the carrier call itself
    takes up 4 mins. We have received some reports of DBUS_NOREPLY that
    would confirm this.
    
    Bug: b/244447732
    Change-Id: Ia8a14f10a2935840fb5c3593e6e0bd785b8bb10f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763105
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181205}

--- chromeos/ash/components/dbus/hermes/constants.cc
+++ chromeos/ash/components/dbus/hermes/constants.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::hermes_constants {
 
-// 4min * 60s/min * 1000ms/s.
-int kHermesNetworkOperationTimeoutMs = 4 * 60 * 1000;
+// 6min * 60s/min * 1000ms/s.
+int kHermesNetworkOperationTimeoutMs = 6 * 60 * 1000;
 
 // 2min * 60s/min * 1000ms/s
 int kHermesOperationTimeoutMs = 2 * 60 * 1000;
