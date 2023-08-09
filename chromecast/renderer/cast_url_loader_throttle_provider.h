9b8b7f071c18278fd9c43c7336958b1c761b3010
Tsuyoshi Horo <horo@chromium.org>
2023-08-09 23:26:18 +0000

    Use SEQUENCE_CHECKER in CastURLLoaderThrottleProvider
    We are planning to conduct an experiment to move Blink's resource
    loading logic off the main thread. To run the loading logic in the
    background thread using SequencedTaskRunner, we need to replace
    THREAD_CHECKER in URLLoaderThrottleProvider with SEQUENCE_CHECKER.
    
    Bug: 1410609
    Change-Id: Id22f76e4ea7728ae521d0bde1bf44c433ddb7cc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764656
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181758}

--- chromecast/renderer/cast_url_loader_throttle_provider.h
+++ chromecast/renderer/cast_url_loader_throttle_provider.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/sequence_checker.h"
 #include "base/strings/string_piece.h"
-#include "base/threading/thread_checker.h"
 #include "third_party/blink/public/platform/url_loader_throttle_provider.h"
 
 namespace chromecast {
@@ -42,7 +42,7 @@ class CastURLLoaderThrottleProvider : public blink::URLLoaderThrottleProvider {
   blink::URLLoaderThrottleProviderType type_;
   CastActivityUrlFilterManager* const cast_activity_url_filter_manager_;
 
-  THREAD_CHECKER(thread_checker_);
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace chromecast
