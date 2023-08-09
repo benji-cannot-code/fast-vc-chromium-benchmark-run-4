48a23ae056f7785104dd83482d72f5a9f5da1b81
Emilia Paz <emiliapaz@chromium.org>
2023-08-09 04:11:08 +0000

    [Extensions Cleanup] Move CanShowActionsInToolbar to ToolbarModel
    CanShowActionsInToolbar was computed in the extensions container because
    that was the only view that needed that value. However,
    chrome://extensions also need to know if an extension can be shown
    in the toolbar. Therefore, we can move this to the ToolbarModel which
    arguably is a better place since we are computing a value for actions.
    
    Bug: N/A
    Change-Id: I0341b10dc154aff8b574eabb1475a9de8709ff25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740660
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181305}

--- chrome/browser/ui/extensions/extensions_container.h
+++ chrome/browser/ui/extensions/extensions_container.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback_forward.h"
-#include "chrome/browser/extensions/extension_context_menu_model.h"
 #include "chrome/browser/ui/extensions/extension_popup_types.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_hover_card_types.h"
 
@@ -36,9 +35,6 @@ class ExtensionsContainer {
   // Called when a context menu is closed so the container can perform any
   // necessary cleanup.
   virtual void OnContextMenuClosed() {}
-
-  // Whether the container supports showing extensions on the toolbar.
-  virtual bool CanShowActionsInToolbar() const = 0;
 
   // Returns true if the action pointed by `action_id` is visible on the
   // toolbar.
