34eb3d27edf49fb6f8d270b6710044f667d6c717
Jason Zhang <jiajunz@google.com>
2023-08-09 00:54:23 +0000

    [CrOS Network] Add auto connect metric for all network type.
    This CL defines and adds the automatic connection result metrics.
    
    Bug: b/261521181
    Change-Id: I7710e46a88fdcfae7ac8aa26f75f56ba277bf009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4730456
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181252}

--- chromeos/ash/components/network/metrics/connection_info_metrics_logger.cc
+++ chromeos/ash/components/network/metrics/connection_info_metrics_logger.cc
@@ -17,8 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 ConnectionInfoMetricsLogger::ConnectionInfo::ConnectionInfo(
-    const NetworkState* network)
-    : guid(network->guid()), shill_error(network->GetError()) {
+    const NetworkState* network,
+    bool is_user_initiated)
+    : guid(network->guid()),
+      shill_error(network->GetError()),
+      is_user_initiated(is_user_initiated) {
   if (network->IsConnectedState())
     status = Status::kConnected;
   else if (network->IsConnectingState())
@@ -34,6 +37,7 @@ ConnectionInfoMetricsLogger::ConnectionInfo::~ConnectionInfo() = default;
 bool ConnectionInfoMetricsLogger::ConnectionInfo::operator==(
     const ConnectionInfoMetricsLogger::ConnectionInfo& other) const {
   return status == other.status && guid == other.guid &&
+         is_user_initiated == other.is_user_initiated &&
          shill_error == other.shill_error;
 }
 
@@ -95,6 +99,24 @@ void ConnectionInfoMetricsLogger::NetworkConnectionStateChanged(
   UpdateConnectionInfo(network);
 }
 
+void ConnectionInfoMetricsLogger::ConnectToNetworkRequested(
+    const std::string& service_path) {
+  if (!network_state_handler_) {
+    return;
+  }
+
+  const NetworkState* network =
+      network_state_handler_->GetNetworkState(service_path);
+
+  if (!network) {
+    return;
+  }
+
+  guid_to_connection_info_.insert_or_assign(
+      network->guid(), ConnectionInfo(network,
+                                      /*is_user_initiated=*/true));
+}
+
 void ConnectionInfoMetricsLogger::OnShuttingDown() {
   network_state_handler_observer_.Reset();
 }
@@ -107,6 +129,10 @@ void ConnectionInfoMetricsLogger::ConnectSucceeded(
   if (!network)
     return;
 
+  // Update the connection request to no longer be "user initiated" so that we
+  // don't continue to emit subsequent connections as user initiated.
+  guid_to_connection_info_.insert_or_assign(
+      network->guid(), ConnectionInfo(network, /*is_user_initiated=*/false));
   NetworkMetricsHelper::LogUserInitiatedConnectionResult(network->guid());
 }
 
@@ -118,6 +144,10 @@ void ConnectionInfoMetricsLogger::ConnectFailed(const std::string& service_path,
   if (!network)
     return;
 
+  // Update the connection request to no longer be "user initiated" so that we
+  // don't continue to emit subsequent connections as user initiated.
+  guid_to_connection_info_.insert_or_assign(
+      network->guid(), ConnectionInfo(network, /*is_user_initiated=*/false));
   NetworkMetricsHelper::LogUserInitiatedConnectionResult(network->guid(),
                                                          error_name);
 }
@@ -126,15 +156,17 @@ void ConnectionInfoMetricsLogger::UpdateConnectionInfo(
     const NetworkState* network) {
   const absl::optional<ConnectionInfo> prev_info =
       GetCachedInfo(network->guid());
-  const ConnectionInfo& curr_info = ConnectionInfo(network);
+  // If a connect has been requested, maintain the connect request until the
+  // connect succeeded or failed.
+  const ConnectionInfo curr_info =
+      ConnectionInfo(network, prev_info && prev_info->is_user_initiated);
 
   // No updates if the ConnectionInfo did not change.
   if (prev_info == curr_info)
     return;
 
   // If the connection status has changed, attempt to log automatic connection
-  // and disconnection metrics. Otherwise, if a disconnect has been requested,
-  // maintain the request until the status changes.
+  // and disconnection metrics.
   if (!prev_info || prev_info->status != curr_info.status) {
     ConnectionAttemptFinished(prev_info, curr_info);
     AttemptLogConnectionStateResult(prev_info, curr_info);
@@ -148,7 +180,8 @@ void ConnectionInfoMetricsLogger::ConnectionAttemptFinished(
   DCHECK(!prev_info || prev_info && prev_info->guid == curr_info.guid);
 
   if (curr_info.status == ConnectionInfo::Status::kConnected) {
-    NetworkMetricsHelper::LogAllConnectionResult(curr_info.guid);
+    NetworkMetricsHelper::LogAllConnectionResult(curr_info.guid,
+                                                 !curr_info.is_user_initiated);
     NotifyConnectionResult(curr_info.guid, /*shill_error=*/absl::nullopt);
   }
 
@@ -159,8 +192,8 @@ void ConnectionInfoMetricsLogger::ConnectionAttemptFinished(
        prev_info->status == ConnectionInfo::Status::kDisconnecting) &&
       curr_info.status == ConnectionInfo::Status::kDisconnected &&
       NetworkState::ErrorIsValid(curr_info.shill_error)) {
-    NetworkMetricsHelper::LogAllConnectionResult(curr_info.guid,
-                                                 curr_info.shill_error);
+    NetworkMetricsHelper::LogAllConnectionResult(
+        curr_info.guid, !curr_info.is_user_initiated, curr_info.shill_error);
     NotifyConnectionResult(curr_info.guid, curr_info.shill_error);
   }
 }
