b96cf8dd237006e2f06a6b79152631ad695818b7
Alexander Alekseev <alemate@chromium.org>
2023-08-09 21:00:25 +0000

    Add ChromeOS trace events in /components/desks_storage
    Investigating ChromeOS login performance revealed that many potentially slow UI
    thread tasks are not instrumented. This Cl adds some events around slow tasks
    that we saw in our tests.
    
    Bug: b:293198304
    
    CC=achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=avynn@google.com
    
    Change-Id: I70c5818599b442e23c9d7bd4223c8c92ac34977b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763169
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181687}

--- components/desks_storage/core/desk_sync_bridge.cc
+++ components/desks_storage/core/desk_sync_bridge.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "base/uuid.h"
 #include "build/chromeos_buildflags.h"
 #include "chromeos/ui/base/window_state_type.h"
@@ -592,6 +593,7 @@ void DeskSyncBridge::OnReadAllData(
 void DeskSyncBridge::OnReadAllMetadata(
     const absl::optional<syncer::ModelError>& error,
     std::unique_ptr<syncer::MetadataBatch> metadata_batch) {
+  TRACE_EVENT0("ui", "DeskSyncBridge::OnReadAllMetadata");
   if (error) {
     change_processor()->ReportError(*error);
     return;
