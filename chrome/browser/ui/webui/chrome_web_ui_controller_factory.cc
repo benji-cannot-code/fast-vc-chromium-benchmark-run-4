87aea4174092ffcacdb1daa4ce7a27cf0207eb41
Jun Ishiguro <junis@google.com>
2023-08-09 03:10:49 +0000

    ChromeOS: Add chrome://chrome-urls to the acceptable URLs
    This CL makes `GetListOfAcceptableURLs()` include chrome://chrome-urls.
    
    This change resolves the following issues:
    - In Ash, middle-clicking on the "chrome://about" link in the os://about
      (or os://chrome-urls) page opens an Ash browser window in LacrosOnly.
    - Lacros does not accept "os://chrome-urls" in the address bar.
    
    Bug: 1457122, 1444283
    Change-Id: I3f4810c18505c507df2855ea891a3f2c697483f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659169
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181294}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -1112,6 +1112,7 @@ ChromeWebUIControllerFactory::GetListOfAcceptableURLs() {
     // avoid confusion, the two instances should provide a link to each other.
     GURL(chrome::kChromeUIAboutURL),
     GURL(chrome::kChromeUIAppServiceInternalsURL),
+    GURL(chrome::kChromeUIChromeURLsURL),
     GURL(chrome::kChromeUIComponentsUrl),
     GURL(chrome::kChromeUICreditsURL),
     GURL(chrome::kChromeUIDeviceLogUrl),
