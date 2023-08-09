defb496614765e8147b4b2475d4840d8c008bdfd
Alexander Alekseev <alemate@chromium.org>
2023-08-09 13:29:25 +0000

    Add ChromeOS trace events in /chrome/browser/ash/login/extensions
    Investigating ChromeOS login performance revealed that many potentially slow UI
    thread tasks are not instrumented. This Cl adds some events around slow tasks
    that we saw in our tests.
    
    Bug: b:293198304
    
    CC=achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=emaxx@chromium.org
    
    Change-Id: Ic971f6595dd9ae607d206c90ed5e0964ac1a50bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763531
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181439}

--- chrome/browser/ash/login/extensions/login_screen_extensions_lifetime_manager.cc
+++ chrome/browser/ash/login/extensions/login_screen_extensions_lifetime_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/trace_event/trace_event.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -89,6 +90,8 @@ void LoginScreenExtensionsLifetimeManager::OnProfileManagerDestroying() {
 }
 
 void LoginScreenExtensionsLifetimeManager::OnSessionStateChanged() {
+  TRACE_EVENT0("login",
+               "LoginScreenExtensionsLifetimeManager::OnSessionStateChanged");
   UpdateStateIfProfileReady();
 }
 
