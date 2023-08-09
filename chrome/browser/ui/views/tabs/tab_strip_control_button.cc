4f65331d3fbab2e580a37026dcaea0918a236bf0
Shibalik Mohapatra <shibalik@chromium.org>
2023-08-09 19:51:16 +0000

    [Refresh] Remove inkdrop configurations from UpdateInkdrop in tab strip control button.
    The resetting of mode and layer region in UpdateInkDrop seems to reset the state of the inkdrop. The UpdateInkDrop() should only update the color of the inkdrop.
    
    Bug: 1470520
    Change-Id: Ibb6fdab259bd3479314564f8763ffa6b1ab93d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766259
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181647}

--- chrome/browser/ui/views/tabs/tab_strip_control_button.cc
+++ chrome/browser/ui/views/tabs/tab_strip_control_button.cc
@@ -39,6 +39,10 @@ TabStripControlButton::TabStripControlButton(TabStrip* tab_strip,
   SetFocusRingCornerRadius(28);
   SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
 
+  views::InkDrop::Get(this)->SetMode(views::InkDropHost::InkDropMode::ON);
+  if (features::IsChromeRefresh2023()) {
+    views::InkDrop::Get(this)->SetLayerRegion(views::LayerRegion::kAbove);
+  }
   UpdateInkDrop();
   views::FocusRing::Get(this)->SetColorId(kColorNewTabButtonFocusRing);
 }
@@ -71,10 +75,8 @@ void TabStripControlButton::UpdateInkDrop() {
     return;
   }
 
-  views::InkDrop::Get(this)->SetMode(views::InkDropHost::InkDropMode::ON);
-
   if (features::IsChromeRefresh2023()) {
-    return ConfigureToolbarInkdropForRefresh2023(
+    return CreateToolbarInkdropCallbacks(
         this, kColorTabStripControlButtonInkDrop,
         kColorTabStripControlButtonInkDropRipple);
 
