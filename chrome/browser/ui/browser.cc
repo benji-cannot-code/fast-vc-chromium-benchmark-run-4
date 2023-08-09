53170dc2eea1fa2cab62cca757f384b0e8de6d3d
Jun Ishiguro <junis@google.com>
2023-08-09 02:26:58 +0000

    Lacros: Change OpenURLFromTab to open the URL in the proper browser
    This CL fixes URL handling issues that allow Ash browser to open when
    the user clicks a link in SWA while pressing the shift or control key.
    If Lacros is the only browser, that link should be opened in Lacros.
    Before this CL, such links opened an Ash browser.
    
    Bug: 1444283, 1450061
    Change-Id: I860cce343e38fe881d20e1cc38bc16153ddaf824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570024
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181284}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -256,6 +256,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
 #include "chrome/browser/ash/guest_os/guest_os_terminal.h"
+#include "chrome/browser/ash/url_handler.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "components/session_manager/core/session_manager.h"
 #endif
@@ -1592,6 +1593,13 @@ WebContents* Browser::OpenURLFromTab(WebContents* source,
     DCHECK(window);
     return window->OpenURLFromTab(source, params);
   }
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // Try to intercept the request and open the URL with Lacros.
+  if (ash::TryOpenUrl(params.url, params.disposition)) {
+    return nullptr;
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   NavigateParams nav_params(this, params.url, params.transition);
   nav_params.FillNavigateParamsFromOpenURLParams(params);
