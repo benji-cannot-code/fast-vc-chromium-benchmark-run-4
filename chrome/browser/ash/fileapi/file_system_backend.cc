f71e51ff778e2559312bd2f6b78c51f32ea95109
Joel Hockey <joelhockey@chromium.org>
2023-08-09 03:23:56 +0000

    Restrict ImageLoader to GetMetadata() and CreateFileStreamReader()
    Bug: b/293534917
    Change-Id: Id12f3c394948d4133868af6c65044671190edca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4744683
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181298}

--- chrome/browser/ash/fileapi/file_system_backend.cc
+++ chrome/browser/ash/fileapi/file_system_backend.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/ash/arc/fileapi/arc_documents_provider_util.h"
+#include "chrome/browser/ash/file_manager/app_id.h"
 #include "chrome/browser/ash/fileapi/file_access_permissions.h"
 #include "chrome/browser/ash/fileapi/file_system_backend.h"
 #include "chrome/browser/ash/fileapi/file_system_backend_delegate.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/cros_disks/cros_disks_client.h"
 #include "components/file_access/scoped_file_access_delegate.h"
 #include "components/user_manager/user.h"
+#include "extensions/common/extension.h"
 #include "storage/browser/file_system/async_file_util.h"
 #include "storage/browser/file_system/external_mount_points.h"
 #include "storage/browser/file_system/file_stream_reader.h"
@@ -52,6 +54,21 @@ AccountId GetAccountId(Profile* profile) {
   user_manager::User* user =
       profile ? ProfileHelper::Get()->GetUserByProfile(profile) : nullptr;
   return user ? user->GetAccountId() : AccountId();
+}
+
+// Returns true if the BackendFunction and OperationType correspond to the
+// GetMetadata() and CreateFileStreamReader() calls used when loading a
+// filesystem: URL via FileSystemUrlLoaderFactory.
+bool IsReadOperation(BackendFunction backend_function,
+                     storage::OperationType operation_type) {
+  if (backend_function == BackendFunction::kCreateFileSystemOperation &&
+      operation_type == storage::OperationType::kGetMetadata) {
+    return true;
+  }
+  if (backend_function == BackendFunction::kCreateFileStreamReader) {
+    return true;
+  }
+  return false;
 }
 
 }  // namespace
@@ -226,6 +243,8 @@ const storage::AccessObserverList* FileSystemBackend::GetAccessObservers(
 }
 
 bool FileSystemBackend::IsAccessAllowed(
+    BackendFunction backend_function,
+    storage::OperationType operation_type,
     const storage::FileSystemURL& url) const {
   if (!url.is_valid())
     return false;
@@ -244,6 +263,13 @@ bool FileSystemBackend::IsAccessAllowed(
 
   // The chrome://file-manager can access its filesystem origin.
   if (origin.GetURL() == file_manager::kChromeUIFileManagerURL) {
+    return true;
+  }
+
+  // ImageLoader extension has read-only access via FileSystemUrlLoaderFactory.
+  if (origin.GetURL() == extensions::Extension::GetBaseURLFromExtensionId(
+                             ::file_manager::kImageLoaderExtensionId) &&
+      IsReadOperation(backend_function, operation_type)) {
     return true;
   }
 
@@ -341,7 +367,8 @@ FileSystemBackend::CreateFileSystemOperation(
     base::File::Error* error_code) const {
   DCHECK(url.is_valid());
 
-  if (!IsAccessAllowed(url)) {
+  if (!IsAccessAllowed(BackendFunction::kCreateFileSystemOperation, type,
+                       url)) {
     *error_code = base::File::FILE_ERROR_SECURITY;
     return nullptr;
   }
@@ -414,8 +441,10 @@ FileSystemBackend::CreateFileStreamReader(
         file_access) const {
   DCHECK(url.is_valid());
 
-  if (!IsAccessAllowed(url))
+  if (!IsAccessAllowed(BackendFunction::kCreateFileStreamReader,
+                       storage::OperationType::kNone, url)) {
     return nullptr;
+  }
 
   switch (url.type()) {
     case storage::kFileSystemTypeProvided:
@@ -459,8 +488,10 @@ FileSystemBackend::CreateFileStreamWriter(
     storage::FileSystemContext* context) const {
   DCHECK(url.is_valid());
 
-  if (!IsAccessAllowed(url))
+  if (!IsAccessAllowed(BackendFunction::kCreateFileStreamWriter,
+                       storage::OperationType::kNone, url)) {
     return nullptr;
+  }
 
   switch (url.type()) {
     case storage::kFileSystemTypeProvided:
@@ -500,7 +531,8 @@ void FileSystemBackend::GetRedirectURLForContents(
     storage::URLCallback callback) const {
   DCHECK(url.is_valid());
 
-  if (!IsAccessAllowed(url)) {
+  if (!IsAccessAllowed(BackendFunction::kGetRedirectURLForContents,
+                       storage::OperationType::kNone, url)) {
     std::move(callback).Run(GURL());
     return;
   }
--- chrome/browser/ash/fileapi/file_system_backend_unittest.cc
+++ chrome/browser/ash/fileapi/file_system_backend_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "storage/browser/file_system/external_mount_points.h"
+#include "storage/browser/file_system/file_system_operation.h"
 #include "storage/browser/file_system/file_system_url.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
@@ -137,24 +138,36 @@ TEST(ChromeOSFileSystemBackendTest, AccessPermissions) {
 
   // Backend specific mount point access.
   EXPECT_FALSE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
       CreateFileSystemURL(extension, "removable/foo", mount_points.get())));
 
   backend.GrantFileAccessToOrigin(origin, base::FilePath(FPL("removable/foo")));
   EXPECT_TRUE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
       CreateFileSystemURL(extension, "removable/foo", mount_points.get())));
   EXPECT_FALSE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
       CreateFileSystemURL(extension, "removable/foo1", mount_points.get())));
 
   // System mount point access.
   EXPECT_FALSE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
       CreateFileSystemURL(extension, "system/foo", system_mount_points.get())));
 
   backend.GrantFileAccessToOrigin(origin, base::FilePath(FPL("system/foo")));
   EXPECT_TRUE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
       CreateFileSystemURL(extension, "system/foo", system_mount_points.get())));
-  EXPECT_FALSE(backend.IsAccessAllowed(
-      CreateFileSystemURL(extension, "system/foo1",
-                          system_mount_points.get())));
+  EXPECT_FALSE(
+      backend.IsAccessAllowed(ash::BackendFunction::kCreateFileSystemOperation,
+                              storage::OperationType::kCopy,
+                              CreateFileSystemURL(extension, "system/foo1",
+                                                  system_mount_points.get())));
 
   // The extension cannot access new mount points.
   // TODO(tbarzic): This should probably be changed.
@@ -162,11 +175,34 @@ TEST(ChromeOSFileSystemBackendTest, AccessPermissions) {
       "test", storage::kFileSystemTypeLocal, storage::FileSystemMountOption(),
       base::FilePath(FPL("/foo/test"))));
   EXPECT_FALSE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
       CreateFileSystemURL(extension, "test_/foo", mount_points.get())));
 
   backend.RevokeAccessForOrigin(origin);
   EXPECT_FALSE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
       CreateFileSystemURL(extension, "removable/foo", mount_points.get())));
+
+  // ImageLoader has access to all files GetMetadata(), GetFileStreamReader().
+  std::string image_loader("pmfjbimdmchhbnneeidfognadeopoehp");
+  EXPECT_TRUE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kGetMetadata,
+      CreateFileSystemURL(image_loader, "removable/foo", mount_points.get())));
+  EXPECT_TRUE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileStreamReader,
+      storage::OperationType::kNone,
+      CreateFileSystemURL(image_loader, "removable/foo", mount_points.get())));
+  EXPECT_FALSE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileSystemOperation,
+      storage::OperationType::kCopy,
+      CreateFileSystemURL(image_loader, "removable/foo", mount_points.get())));
+  EXPECT_FALSE(backend.IsAccessAllowed(
+      ash::BackendFunction::kCreateFileStreamWriter,
+      storage::OperationType::kNone,
+      CreateFileSystemURL(image_loader, "removable/foo", mount_points.get())));
 }
 
 TEST(ChromeOSFileSystemBackendTest, GetVirtualPathConflictWithSystemPoints) {
