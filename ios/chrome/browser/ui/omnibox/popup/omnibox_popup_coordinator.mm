20c898a318c278f88cb8c459410ef90ab4c56dcf
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-09 18:15:30 +0000

    [iOS] unset sharingCoordinator after stop
    Normally, coordinators could be released after being stopped
    
    Bug: 1454777
    Change-Id: I8f515087fccaf5d466d0b9a8cda07167b566cb58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706116
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181597}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_coordinator.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_coordinator.mm
@@ -178,6 +178,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)stop {
   [self.sharingCoordinator stop];
+  self.sharingCoordinator = nil;
   _popupView.reset();
 }
 
