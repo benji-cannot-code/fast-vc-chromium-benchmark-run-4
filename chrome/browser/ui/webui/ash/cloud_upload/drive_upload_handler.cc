71d517758de38338815a06d7efe3beada47534ca
Cassy Chun-Crogan <cassycc@google.com>
2023-08-09 00:46:02 +0000

    [Office upload] Add logging for the Drive upload generic error message
    Add logging when the upload to Drive fails and the error message is
    generic so the failure is more clear.
    
    Bug: None
    Change-Id: I1097821a4dd6b7abc27ec200b599a8e1cc8d0c72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4731153
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181245}

--- chrome/browser/ui/webui/ash/cloud_upload/drive_upload_handler.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/drive_upload_handler.cc
@@ -429,6 +429,7 @@ void DriveUploadHandler::ShowIOTaskError(
       } else {
         upload_result = OfficeFilesUploadResult::kMoveOperationError;
       }
+      LOG(ERROR) << "IO Task error";
       error_message = GetGenericErrorMessage();
   }
 
@@ -514,10 +515,12 @@ void DriveUploadHandler::OnError(const drivefs::mojom::DriveError& error) {
                       IDS_OFFICE_CLOUD_PROVIDER_GOOGLE_DRIVE_SHORT))));
       break;
     case drivefs::mojom::DriveError::Type::kPinningFailedDiskFull:
+      LOG(ERROR) << "Pinning failed, disk full";
       OnEndCopy(GURL(), OfficeFilesUploadResult::kPinningFailedDiskFull,
                 GetGenericErrorMessage());
       break;
     default:
+      LOG(ERROR) << "Cloud error";
       OnEndCopy(GURL(), OfficeFilesUploadResult::kCloudError,
                 GetGenericErrorMessage());
   }
@@ -526,6 +529,7 @@ void DriveUploadHandler::OnError(const drivefs::mojom::DriveError& error) {
 void DriveUploadHandler::OnDriveConnectionStatusChanged(
     drive::util::ConnectionStatusType status) {
   if (status != drive::util::DRIVE_CONNECTED) {
+    LOG(ERROR) << "Lost connection to Drive during upload";
     OnEndCopy(GURL(), OfficeFilesUploadResult::kNoConnection,
               GetGenericErrorMessage());
   }
