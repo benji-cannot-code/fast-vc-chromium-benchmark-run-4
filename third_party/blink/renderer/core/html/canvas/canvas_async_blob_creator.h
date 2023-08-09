a78d4d6c1782979ad2519b4b14696d8ca29ee4aa
Anders Hartvoll Ruud <andruud@chromium.org>
2023-08-09 09:29:52 +0000

    Send SkImage to CanvasAsyncBlobCreator::EncodeImageOnEncoderThread
    To encode the image on a worker thread, we currently access the
    pixel data via `this->src_data_`. This needs to be eliminated
    if we want to stop using CrossThreadPersistent. This CL instead
    sends a SkImage reference to the worker thread, which should be
    safe, since the underlying bitmap held by SkImage is immutable.
    We also send a std::unique_ptr<ImageDataBuffer> to the worker,
    but this is mostly as a way to transport `src_data_`.
    (SkPixmap does not have a CrossThreadCopier, so it can't be sent
    directly).
    
    As of this CL, there should not be any remaining accesses to `this`,
    except for the calls to WrapCrossThreadPersistent(this).
    
    Bug: 1370013
    Change-Id: If8c271d14d3103fc745e7b65c9bce39ce7e845f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4751820
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181376}

--- third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.h
+++ third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ExecutionContext;
+class ImageDataBuffer;
 
 class CORE_EXPORT CanvasAsyncBlobCreator
     : public GarbageCollected<CanvasAsyncBlobCreator> {
@@ -100,12 +101,16 @@ class CORE_EXPORT CanvasAsyncBlobCreator
   void Dispose();
 
   scoped_refptr<StaticBitmapImage> image_;
+  Member<ExecutionContext> context_;
+
+  // The following members are used for progressive/idle encoding,
+  // see comment above the implementation of ScheduleAsyncBlobCreation.
+  sk_sp<SkImage> skia_image_;
+  SkPixmap src_data_;  // Holds a raw pointer owned by `skia_ìmage`.
   std::unique_ptr<ImageEncoder> encoder_;
   Vector<unsigned char> encoded_image_;
   int num_rows_completed_;
-  Member<ExecutionContext> context_;
 
-  SkPixmap src_data_;
   ImageEncodingMimeType mime_type_;
   ToBlobFunctionType function_type_;
 
@@ -124,7 +129,7 @@ class CORE_EXPORT CanvasAsyncBlobCreator
   // Used for OffscreenCanvas only
   Member<ScriptPromiseResolver> script_promise_resolver_;
 
-  static bool EncodeImage(const SkPixmap& src_data,
+  static bool EncodeImage(std::unique_ptr<ImageDataBuffer>,
                           ImageEncodingMimeType,
                           const double& quality,
                           Vector<unsigned char>* encoded_image);
@@ -134,7 +139,11 @@ class CORE_EXPORT CanvasAsyncBlobCreator
   void ForceEncodeRows();  // Similar to IdleEncodeRows without deadline.
 
   // WEBP
-  void EncodeImageOnEncoderThread(double quality);
+  void EncodeImageOnEncoderThread(scoped_refptr<base::SingleThreadTaskRunner>,
+                                  sk_sp<SkImage>,
+                                  std::unique_ptr<ImageDataBuffer>,
+                                  ImageEncodingMimeType,
+                                  double quality);
 
   void IdleTaskStartTimeoutEvent(double quality);
   void IdleTaskCompleteTimeoutEvent();
