dd749e41f7b652871afd1c7baf6cccc3c578f22f
Erica Lee <ericamlee@google.com>
2023-08-09 20:08:57 +0000

    p13n: remove dynamic color delay
    Removed a delay meant to simulate latency.
    
    BUG=none
    TEST=ash_unittests *ColorPaletteController*
    TEST=browser_tests *PersonalizationApp*DynamicColor*
    
    Change-Id: If8d5fb53f69929600c236c7266092276e9d6fdb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4765476
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181658}

--- ash/style/color_palette_controller.cc
+++ ash/style/color_palette_controller.cc
@@ -200,8 +200,8 @@ class ColorPaletteControllerImpl : public ColorPaletteController,
     pref_service->SetInteger(prefs::kDynamicColorColorScheme,
                              static_cast<int>(scheme));
     NotifyObservers(GetColorPaletteSeed(account_id));
-    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
-        FROM_HERE, std::move(on_complete), base::Milliseconds(100));
+    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+        FROM_HERE, std::move(on_complete));
   }
 
   void SetStaticColor(SkColor seed_color,
@@ -219,8 +219,8 @@ class ColorPaletteControllerImpl : public ColorPaletteController,
                              static_cast<int>(ColorScheme::kStatic));
     pref_service->SetUint64(prefs::kDynamicColorSeedColor, seed_color);
     NotifyObservers(GetColorPaletteSeed(account_id));
-    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
-        FROM_HERE, std::move(on_complete), base::Milliseconds(100));
+    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+        FROM_HERE, std::move(on_complete));
   }
 
   absl::optional<ColorPaletteSeed> GetColorPaletteSeed(
