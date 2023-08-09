ec9a88b7bf24e74eb8e9096e005bbcf6bc2db841
Peter Kasting <pkasting@chromium.org>
2023-08-09 20:14:01 +0000

    Ban [w]ctype.h: testing/
    Bug: 1361094
    Change-Id: I773481be2bf5f46ecdc77a975b10bc66266beaf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761604
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181661}

--- testing/libfuzzer/proto/skia_image_filter_proto_converter.cc
+++ testing/libfuzzer/proto/skia_image_filter_proto_converter.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/libfuzzer/proto/skia_image_filter_proto_converter.h"
 
-#include <ctype.h>
 #include <stdlib.h>
 
 #include <algorithm>
