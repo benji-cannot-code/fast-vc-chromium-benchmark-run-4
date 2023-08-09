2cf4dfc2fcff4e3db706d5e2b871d4fe1aafce2d
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-09 19:45:39 +0000

    [iOS] TextZoomMediator dealloc does not disconnect
    Instead it check it is disconnected. It should be the case as long as its only owner is properly stopped
    
    Bug: None
    Change-Id: Id35e02fd89aec450dab27e03cd6c36f6bc66f024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4729978
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181643}

--- ios/chrome/browser/ui/text_zoom/text_zoom_mediator.mm
+++ ios/chrome/browser/ui/text_zoom/text_zoom_mediator.mm
@@ -56,7 +56,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)dealloc {
-  [self disconnect];
+  DCHECK(!_activeWebState);
+  DCHECK(!_webStateList);
 }
 
 - (void)disconnect {
