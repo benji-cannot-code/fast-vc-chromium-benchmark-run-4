ca40193f637b30e11f926efd17ab834bc978b08f
Michelle <michellegc@google.com>
2023-08-09 10:40:45 +0000

    Fix hit test update notifications for touch selection in child frames.
    In CL:4728431, I modified TouchSelectionControllerClientChildFrame to
    recompute selection bounds on hit test updates. However, I mistakenly
    put the hit test region update in an if clause for an unrelated screen
    rect movement check (which also occurs after an early return where the
    selection_controller_client_ probably also should have been updated).
    
    Fix this by moving the selection_controller_client_ update before the
    early return.
    
    Bug: b:286954885
    Change-Id: I8b40a6c34d8c3e203d27e2ce33d56ab3ad81b9f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764684
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181391}

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -706,6 +706,10 @@ const viz::LocalSurfaceId& RenderWidgetHostViewChildFrame::GetLocalSurfaceId()
 
 void RenderWidgetHostViewChildFrame::NotifyHitTestRegionUpdated(
     const viz::AggregatedHitTestRegion& region) {
+  if (selection_controller_client_) {
+    selection_controller_client_->OnHitTestRegionUpdated();
+  }
+
   absl::optional<gfx::RectF> screen_rect =
       region.transform.InverseMapRect(gfx::RectF(region.rect));
   if (!screen_rect) {
@@ -720,10 +724,6 @@ void RenderWidgetHostViewChildFrame::NotifyHitTestRegionUpdated(
        blink::mojom::kMaxChildFrameScreenRectMovement)) {
     last_stable_screen_rect_ = *screen_rect;
     screen_rect_stable_since_ = base::TimeTicks::Now();
-
-    if (selection_controller_client_) {
-      selection_controller_client_->OnHitTestRegionUpdated();
-    }
   }
 }
 
