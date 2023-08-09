00ba9de3b6b1a16e5ade7e5a898c648f093ec19f
Marijn Kruisselbrink <mek@chromium.org>
2023-08-09 18:27:56 +0000

    Add support for "hidden in background" mode to base::mac::LaunchApplication.
    This will be used to launch PWA app shims in the background when
    necessary to correctly attribute notifications to web apps.
    
    Bug: 938661
    Change-Id: I25e931dc914a7343cfed252b213e5178268bdd55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722428
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181605}

--- base/mac/test/launch_application_test_helper_main.mm
+++ base/mac/test/launch_application_test_helper_main.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation AppDelegate {
   NSArray* _command_line;
   NSURL* _fifo_url;
+  NSRunningApplication* _running_app;
 }
 
 - (instancetype)initWithCommandLine:(NSArray*)command_line {
@@ -30,14 +31,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             [bundle_url.lastPathComponent
                 stringByReplacingOccurrencesOfString:@".app"
                                           withString:@".fifo"]];
+    _running_app = NSRunningApplication.currentApplication;
+    [_running_app addObserver:self
+                   forKeyPath:@"activationPolicy"
+                      options:NSKeyValueObservingOptionNew
+                      context:nullptr];
   }
   return self;
+}
+
+- (void)dealloc {
+  [_running_app removeObserver:self
+                    forKeyPath:@"activationPolicy"
+                       context:nullptr];
+}
+
+- (void)observeValueForKeyPath:(NSString*)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary*)change
+                       context:(void*)context {
+  [self
+      addLaunchEvent:@"activationPolicyChanged"
+            withData:@{
+              @"activationPolicy" : change[@"new"],
+              @"processIdentifier" :
+                  @(NSRunningApplication.currentApplication.processIdentifier),
+            }];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification*)notification {
   [self
       addLaunchEvent:@"applicationDidFinishLaunching"
             withData:@{
+              @"activationPolicy" : @(NSApp.activationPolicy),
               @"commandLine" : _command_line,
               @"processIdentifier" :
                   @(NSRunningApplication.currentApplication.processIdentifier),
@@ -55,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self
       addLaunchEvent:@"openURLs"
             withData:@{
+              @"activationPolicy" : @(NSApp.activationPolicy),
               @"processIdentifier" :
                   @(NSRunningApplication.currentApplication.processIdentifier),
               @"urls" : url_specs,
