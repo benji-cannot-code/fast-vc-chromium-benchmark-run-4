4aea96c185a53cb6a2599043103e512d8f530326
Ben Franz <bfranz@google.com>
2023-08-09 10:45:15 +0000

    Add Unit-tests for WebKioskInstallerLacros
    Those unit tests test the Lacros-side only by faking out incoming mojom
    calls.
    
    Bug: b:284932363
    Test: WebKioskInstallerLacrosTest
    Change-Id: Ibe5d74f03cf99c3a94d10a1c6d5e614350f8edaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673212
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181392}

--- chrome/browser/lacros/app_mode/web_kiosk_installer_lacros.cc
+++ chrome/browser/lacros/app_mode/web_kiosk_installer_lacros.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/functional/callback_helpers.h"
 #include "chrome/browser/chromeos/app_mode/web_kiosk_app_installer.h"
 #include "chromeos/crosapi/mojom/web_kiosk_service.mojom.h"
 #include "chromeos/lacros/lacros_service.h"
@@ -39,7 +38,7 @@ WebKioskInstallerLacros::WebKioskInstallerLacros(Profile& profile)
       receiver_.BindNewPipeAndPassRemoteWithVersion());
 }
 
-WebKioskInstallerLacros::~WebKioskInstallerLacros() {}
+WebKioskInstallerLacros::~WebKioskInstallerLacros() = default;
 
 void WebKioskInstallerLacros::GetWebKioskInstallState(
     const GURL& url,
