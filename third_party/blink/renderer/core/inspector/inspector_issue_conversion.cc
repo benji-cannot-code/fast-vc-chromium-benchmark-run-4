0efe109fbec9d923f0e0fbf8eb68a4e19b1abe5e
Shuran Huang <shuuran@chromium.org>
2023-08-09 16:44:29 +0000

    Reflect EXCLUDE_THIRD_PARTY_PHASEOUT reason to DevTools issues
    Bug: 1469135
    Change-Id: I1c32c0444712af7d79d237b0c0243ea7ff1e8f22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748546
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181530}

--- third_party/blink/renderer/core/inspector/inspector_issue_conversion.cc
+++ third_party/blink/renderer/core/inspector/inspector_issue_conversion.cc
@@ -84,6 +84,9 @@ protocol::String BuildCookieExclusionReason(
       return protocol::Audits::CookieExclusionReasonEnum::ExcludeSameSiteStrict;
     case blink::mojom::blink::CookieExclusionReason::kExcludeDomainNonASCII:
       return protocol::Audits::CookieExclusionReasonEnum::ExcludeDomainNonASCII;
+    case blink::mojom::blink::CookieExclusionReason::kExcludeThirdPartyPhaseout:
+      return protocol::Audits::CookieExclusionReasonEnum::
+          ExcludeThirdPartyPhaseout;
   }
 }
 
---
+++ third_party/blink/web_tests/virtual/third-party-cookie-phaseout-enabled/README.md
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# virtual/third-party-cookie-phaseout-enabled
+
+This suite verifies tests when --test-third-party-cookie-phaseout switch is enabled.
