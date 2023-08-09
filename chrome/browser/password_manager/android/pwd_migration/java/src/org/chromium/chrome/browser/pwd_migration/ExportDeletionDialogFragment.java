4d0592d125fa78cb0be7f4d38522381052df5c5d
Ivana Žužić <izuzic@google.com>
2023-08-09 13:09:46 +0000

    [UPMLocalPwd] Change the progress bar title
    NonCancelableProgressBar used the old string: "Please wait...".
    Now it has an adjustable title.
    It's now titled "Deleting" for the case when it's shown during password
    deletion and "Exporting" during password export to downloads, following
    the UX guidance.
    Google Material recommends a spinner with no ellipses ("...").
    
    Bug: 1471127
    Change-Id: I7c45c1127bdca8e23308c2765e000cce305bbf3f
    Low-Coverage-Reason: Pre-existing low coverage.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4759966
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181430}

--- chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/ExportDeletionDialogFragment.java
+++ chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/ExportDeletionDialogFragment.java
@@ -103,7 +103,8 @@ public class ExportDeletionDialogFragment extends DialogFragment implements Pass
     }
 
     private void showProgressBar() {
-        NonCancelableProgressBar progressBarDialogFragment = new NonCancelableProgressBar();
+        NonCancelableProgressBar progressBarDialogFragment = new NonCancelableProgressBar(
+                R.string.exported_passwords_deletion_in_progress_title);
         mProgressBarManager.showWithDelay(
                 progressBarDialogFragment, mFragmentManager, PROGRESS_BAR_DELAY_MS);
     }
