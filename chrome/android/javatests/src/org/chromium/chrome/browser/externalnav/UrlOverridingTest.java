383013dfd0022f8f8202a5a94db21f0f78e5c2ef
Michael Thiessen <mthiesse@chromium.org>
2023-08-09 18:55:01 +0000

    Increase timeout for URL loading in UrlOverridingTest
    Looks like some tests have gotten flakier over time with a timeout here.
    I suspect this is just something like bots/Chrome getting slower and
    that we should just increase this timeout.
    
    I was unable to reproduce this locally after ~100 runs.
    
    Bug: 1469284
    Change-Id: Ied8848cb6ca6bafc077f06e664b644eb5f282947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764465
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181616}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -573,7 +573,7 @@ public class UrlOverridingTest {
             }
             if (expectedFinalUrl == null) return;
             Criteria.checkThat(latestTab.getUrl().getSpec(), Matchers.is(expectedFinalUrl));
-        });
+        }, 10000L, CriteriaHelper.DEFAULT_POLLING_INTERVAL);
 
         if (createsNewTab && shouldLaunchExternalIntent) {
             try {
