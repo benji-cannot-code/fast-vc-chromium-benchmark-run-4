2c3933aa1e967d541fbe5376e79e8f6fdb10d004
Glen Robertson <glenrob@chromium.org>
2023-08-09 01:14:21 +0000

    Change scope/shortcut TODOs to point to current bug
    These TODOs point to crbug.com/910016 which has been closed for a while
    as it is obsolete. Repointing them and changing the text to account for
    the current direction (where Create Shortcut ignores the manifest).
    
    Bug: 910016,1469482
    Change-Id: I59ab6b3fbbb964ac023c72d84b13a4b77801440f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740975
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181261}

--- chrome/browser/web_applications/web_app_registrar.cc
+++ chrome/browser/web_applications/web_app_registrar.cc
@@ -396,7 +396,7 @@ absl::optional<AppId> WebAppRegistrar::FindAppWithUrlInScope(
   bool best_app_is_shortcut = true;
 
   for (const AppId& app_id : GetAppIdsForAppSet(GetApps())) {
-    // TODO(crbug.com/910016): Treat shortcuts as PWAs.
+    // TODO(crbug.com/1469482): Consider treating shortcuts differently to PWAs.
     bool app_is_shortcut = IsShortcutApp(app_id);
     if (app_is_shortcut && !best_app_is_shortcut)
       continue;
@@ -461,7 +461,7 @@ absl::optional<AppId> WebAppRegistrar::FindInstalledAppWithUrlInScope(
   bool best_app_is_shortcut = true;
 
   for (const AppId& app_id : GetAppIds()) {
-    // TODO(crbug.com/910016): Treat shortcuts as PWAs.
+    // TODO(crbug.com/1469482): Consider treating shortcuts differently to PWAs.
     bool app_is_shortcut = IsShortcutApp(app_id);
     if (app_is_shortcut && !best_app_is_shortcut) {
       continue;
@@ -510,8 +510,8 @@ bool WebAppRegistrar::IsNonLocallyInstalledAppWithUrlInScope(
 }
 
 bool WebAppRegistrar::IsShortcutApp(const AppId& app_id) const {
-  // TODO (crbug/910016): Make app scope always return a value and record this
-  //  distinction in some other way.
+  // TODO(crbug.com/1469482): Record shortcut distinction explicitly instead of
+  // using scope.
   return !GetAppScopeInternal(app_id).has_value();
 }
 
@@ -1145,7 +1145,8 @@ absl::optional<GURL> WebAppRegistrar::GetAppScopeInternal(
   if (!web_app)
     return absl::nullopt;
 
-  // TODO(crbug.com/910016): Treat shortcuts as PWAs.
+  // TODO(crbug.com/1469482): Record shortcut distinction explicitly instead of
+  // using scope.
   // Shortcuts on the WebApp system have empty scopes, while the implementation
   // of IsShortcutApp just checks if the scope is |absl::nullopt|, so make sure
   // we return |absl::nullopt| rather than an empty scope.
