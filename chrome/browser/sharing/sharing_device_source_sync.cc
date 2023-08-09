feb13525275bd0e5b905900c5f5923f25a5efdb0
Alexander Alekseev <alemate@chromium.org>
2023-08-09 12:25:29 +0000

    Add ChromeOS trace events in /chrome/browser/sharing
    Investigating ChromeOS login performance revealed that many potentially slow UI
    thread tasks are not instrumented. This Cl adds some events around slow tasks
    that we saw in our tests.
    
    Bug: b:293198304
    
    CC=achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=mvanouwerkerk@chromium.org
    
    Change-Id: Ib8e94f4078ba595acc4bc6ff2e352bbb15323765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4765797
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181415}

--- chrome/browser/sharing/sharing_device_source_sync.cc
+++ chrome/browser/sharing/sharing_device_source_sync.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/stl_util.h"
 #include "base/task/thread_pool.h"
+#include "base/trace_event/trace_event.h"
 #include "chrome/browser/sharing/features.h"
 #include "chrome/browser/sharing/proto/sharing_message.pb.h"
 #include "chrome/browser/sharing/sharing_constants.h"
@@ -109,6 +110,7 @@ bool SharingDeviceSourceSync::IsReady() {
 }
 
 void SharingDeviceSourceSync::OnDeviceInfoChange() {
+  TRACE_EVENT0("sharing", "SharingDeviceSourceSync::OnDeviceInfoChange");
   if (device_info_tracker_->IsSyncing())
     device_info_tracker_->RemoveObserver(this);
   MaybeRunReadyCallbacks();
