fdae8a1034537db74474b422c398cd43f4d0f6b7
Chidera Olibie <colibie@google.com>
2023-08-09 13:18:15 +0000

    [Cronet] Deprecate Kitkat
    Removes some kitkat related codepaths
    
    Bug: 1042122, b/290765498
    Change-Id: I15e79c642313bbec9a6d380a4244dfabdcd3fa58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4746564
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181433}

--- components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleApplication.java
+++ components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleApplication.java
@@ -6,11 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.cronet_sample_apk;
 
 import android.app.Application;
-import android.content.Context;
-import android.os.Build;
 
 import androidx.annotation.OptIn;
-import androidx.multidex.MultiDex;
 
 import org.chromium.net.ConnectionMigrationOptions;
 import org.chromium.net.CronetEngine;
@@ -22,14 +19,6 @@ import org.chromium.net.QuicOptions;
  */
 public class CronetSampleApplication extends Application {
     private CronetEngine mCronetEngine;
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        // install multidex for Kitkat - crbug/1393424
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
-            MultiDex.install(this);
-        }
-    }
 
     @Override
     public void onCreate() {
