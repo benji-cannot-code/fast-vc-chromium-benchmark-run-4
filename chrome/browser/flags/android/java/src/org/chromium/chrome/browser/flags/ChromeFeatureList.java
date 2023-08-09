a93c79669a7c07d9098a0aee87bdd47a27dca87d
Awad Osman <awado@google.com>
2023-08-09 14:45:50 +0000

    [Android] Update standard protection dropdown on privacy guide
    Updates the standard protection summary text and removes the
    aux button which leads to an extra page discussing what standard
    protection offers. This change uses the visibiltiy attribute of xml to
    hide and show views depending on the status of our feature flag.
    
    Screenshots can be found here: https://crbug.com/1470419#c5
    
    Bug: 1470419
    Change-Id: I3ab75ce496d4b7c280190ca08ffffba4f7fa1964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752766
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181473}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -550,6 +550,8 @@ public abstract class ChromeFeatureList {
     public static final CachedFlag sExperimentsForAgsa = new CachedFlag(EXPERIMENTS_FOR_AGSA, true);
     public static final CachedFlag sFeedLoadingPlaceholder =
             new CachedFlag(FEED_LOADING_PLACEHOLDER, false);
+    public static final CachedFlag sFriendlierSafeBrowsingSettingsStandardProtection =
+            new CachedFlag(FRIENDLIER_SAFE_BROWSING_SETTINGS_STANDARD_PROTECTION, false);
     public static final CachedFlag sFoldableJankFix = new CachedFlag(FOLDABLE_JANK_FIX, true);
     public static final CachedFlag sGridTabSwitcherLandscapeAspectRatioPhones =
             new CachedFlag(GRID_TAB_SWITCHER_LANDSCAPE_ASPECT_RATIO_PHONES, false);
@@ -649,6 +651,7 @@ public abstract class ChromeFeatureList {
         sEarlyLibraryLoad,
         sEmptyStates,
         sFeedLoadingPlaceholder,
+        sFriendlierSafeBrowsingSettingsStandardProtection,
         sFoldableJankFix,
         sGridTabSwitcherLandscapeAspectRatioPhones,
         sIncognitoReauthenticationForAndroid,
