9a543bbb287cb7dbb5a2dc49baa3d72f2c28d84b
Xiaoling Bao <xiaolingbao@chromium.org>
2023-08-09 00:21:47 +0000

    Add test for CreatePolicyManagerVector
    Bug: 1470759
    Change-Id: I7237a01b2c127d8fe5b89927309c9dd609b8145c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763365
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181237}

--- chrome/updater/policy/service.cc
+++ chrome/updater/policy/service.cc
@@ -60,6 +60,8 @@ PolicyService::PolicyManagers SortManagers(
   return {managers_vector, managers_map};
 }
 
+}  // namespace
+
 PolicyService::PolicyManagerVector CreatePolicyManagerVector(
     bool should_take_policy_critical_section,
     scoped_refptr<ExternalConstants> external_constants,
@@ -99,8 +101,6 @@ PolicyService::PolicyManagerVector CreatePolicyManagerVector(
   managers.push_back(GetDefaultValuesPolicyManager());
   return managers;
 }
-
-}  // namespace
 
 PolicyService::PolicyManagers::PolicyManagers(
     PolicyManagerVector manager_vector,
