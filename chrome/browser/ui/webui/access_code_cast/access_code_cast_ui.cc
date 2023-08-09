d38d8d9ea3a8e3e82e072ceb46aa7aa4608567d2
Benjamin Zielinski <bzielinski@google.com>
2023-08-09 20:16:56 +0000

    [Cast2Class] Update dialog to use updated styles
    For Chrome refresh, the style for Chrome is updating, and it necessary
    to add an html attribute so that our WebUI dialog receives the updated
    style as well.
    
    Additionally, browser dialogs should have an updated corner radius. So,
    make sure to use the default for browser dialogs, and only manually set
    corner radius in the case that we are acting as a system dialog.
    
    As a note, the corner raduis for system dialogs should also be updated
    according to the spec, but for now leave our value as is to match
    SystemWebDialogDelegate.
    
    New style: https://screenshot.googleplex.com/FfzQ5CHU36MN5Pq.png
    
    Change-Id: I01e67b158faee4410cdf5fd5b734ea3be721dd03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766261
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181662}

--- chrome/browser/ui/webui/access_code_cast/access_code_cast_ui.cc
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_ui.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "ui/base/webui/web_ui_util.h"
 
 namespace media_router {
 
@@ -60,6 +61,8 @@ AccessCodeCastUI::AccessCodeCastUI(content::WebUI* web_ui)
   source->AddLocalizedStrings(kStrings);
   source->AddBoolean("qrScannerEnabled", false);
   source->AddString("learnMoreUrl", chrome::kAccessCodeCastLearnMoreURL);
+
+  webui::SetupChromeRefresh2023(source);
 
   Profile* const profile = Profile::FromWebUI(web_ui);
   source->AddInteger("rememberedDeviceDuration",
