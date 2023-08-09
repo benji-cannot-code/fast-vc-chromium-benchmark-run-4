f7eb33fbcf8a9b809e15e0e7a71ec5efe1504a69
Tsuyoshi Horo <horo@chromium.org>
2023-08-09 23:25:10 +0000

    Use SEQUENCE_CHECKER in URLLoaderThrottleProviderImpl
    We are planning to conduct an experiment to move Blink's resource
    loading logic off the main thread. To run the loading logic in the
    background thread using SequencedTaskRunner, we need to replace
    THREAD_CHECKER in URLLoaderThrottleProvider with SEQUENCE_CHECKER.
    
    Bug: 1410609
    Change-Id: I63ddf75def592cd8168f52ed05a0afb424a1afbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764188
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181756}

--- chrome/renderer/url_loader_throttle_provider_impl.h
+++ chrome/renderer/url_loader_throttle_provider_impl.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
-#include "base/threading/thread_checker.h"
+#include "base/sequence_checker.h"
 #include "components/safe_browsing/content/common/safe_browsing.mojom.h"
 #include "extensions/buildflags/buildflags.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -23,8 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class ChromeContentRendererClient;
 
-// Instances must be constructed on the render thread, and then used and
-// destructed on a single thread, which can be different from the render thread.
+// Instances must be constructed on the render main thread, and then used and
+// destructed on a single sequence, which can be different from the render main
+// thread.
 class URLLoaderThrottleProviderImpl : public blink::URLLoaderThrottleProvider {
  public:
   URLLoaderThrottleProviderImpl(
@@ -60,7 +61,7 @@ class URLLoaderThrottleProviderImpl : public blink::URLLoaderThrottleProvider {
       extension_throttle_manager_;
 #endif
 
-  THREAD_CHECKER(thread_checker_);
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 #endif  // CHROME_RENDERER_URL_LOADER_THROTTLE_PROVIDER_IMPL_H_
