b496bc523f6632940f7ab92f13bbe01a0226ceab
Federica Germinario <fedegermi@google.com>
2023-08-09 08:31:56 +0000

    [ios] Check that GlobalWebStateObserver is unregistered
    Check that the observer is unregistered before destruction.
    In particular, change GlobalWebStateObserver to inherit
    from base::CheckedObserver and check that IsInObserverList() returns
    false in the destructor.
    
    Bug: 1430392
    Change-Id: I27a0dab46db8c058decffd4d97a18d2fd08d8e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4746903
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181360}

--- ios/web/public/deprecated/global_web_state_observer.h
+++ ios/web/public/deprecated/global_web_state_observer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/observer_list_types.h"
 #include "ios/web/public/web_state_observer.h"
 
 namespace web {
@@ -24,10 +25,14 @@ class WebState;
 // DEPRECATED. Use TabHelper to observe WebState creation. Use WebStateObserver
 // to observe WebState changes.
 // TODO(crbug.com/782269): Remove this class.
-class GlobalWebStateObserver {
+class GlobalWebStateObserver : public base::CheckedObserver {
  public:
+  GlobalWebStateObserver();
+
   GlobalWebStateObserver(const GlobalWebStateObserver&) = delete;
   GlobalWebStateObserver& operator=(const GlobalWebStateObserver&) = delete;
+
+  ~GlobalWebStateObserver() override;
 
   // Called when `web_state` has started loading a page.
   // DEPRECATED. Use WebStateObserver's `DidStartLoading` instead.
@@ -56,10 +61,6 @@ class GlobalWebStateObserver {
   // DEPRECATED. Use WebStateObserver's `WebStateDestroyed` instead.
   // TODO(crbug.com/782269): Remove this method.
   virtual void WebStateDestroyed(WebState* web_state) {}
-
- protected:
-  GlobalWebStateObserver();
-  virtual ~GlobalWebStateObserver();
 };
 
 }  // namespace web
