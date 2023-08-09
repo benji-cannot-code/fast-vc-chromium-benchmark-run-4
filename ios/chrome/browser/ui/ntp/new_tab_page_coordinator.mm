7255c629ee18e8585ee69aa9d09ebc1b02c11f74
Scott Yoder <scottyoder@google.com>
2023-08-09 02:12:31 +0000

    [ios] Add a unit test for NTPCoordinator's selectFeedType
    Adds a unit tests to verify that the correct view controller is loaded
    when the following feed and the discover feed are selected.
    
    In order to add this test, TestingApplicationContext had to be changed
    to allow setting a VariationsService, so that the stored permanent
    country could be overridden.
    
    Bug: 1464692
    Change-Id: Ia613552e58ed128c19e2b4c6ab34a7732dd1d4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721591
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181281}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -650,21 +650,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Requests feeds here if the correct flags and prefs are enabled.
   if ([self shouldFeedBeVisible]) {
-    if ([self isFollowingFeedAvailable]) {
-      switch (self.selectedFeed) {
-        case FeedTypeDiscover:
-          self.feedViewController = [self.componentFactory
-                   discoverFeedForBrowser:self.browser
-              viewControllerConfiguration:[self
-                                              feedViewControllerConfiguration]];
-          break;
-        case FeedTypeFollowing:
-          self.feedViewController = [self.componentFactory
-                  followingFeedForBrowser:self.browser
-              viewControllerConfiguration:[self feedViewControllerConfiguration]
-                                 sortType:self.followingFeedSortType];
-          break;
-      }
+    if ([self isFollowingFeedAvailable] &&
+        self.selectedFeed == FeedTypeFollowing) {
+      self.feedViewController = [self.componentFactory
+              followingFeedForBrowser:self.browser
+          viewControllerConfiguration:[self feedViewControllerConfiguration]
+                             sortType:self.followingFeedSortType];
     } else {
       self.feedViewController = [self.componentFactory
                discoverFeedForBrowser:self.browser
--- ios/chrome/test/testing_application_context.mm
+++ ios/chrome/test/testing_application_context.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/time/default_clock.h"
 #import "base/time/default_tick_clock.h"
 #import "components/network_time/network_time_tracker.h"
+#import "components/variations/service/variations_service.h"
 #import "ios/chrome/browser/policy/browser_policy_connector_ios.h"
 #import "ios/chrome/browser/policy/configuration_policy_handler_list_factory.h"
 #import "ios/chrome/browser/promos_manager/features.h"
@@ -34,7 +35,8 @@ TestingApplicationContext::TestingApplicationContext()
       test_url_loader_factory_(
           std::make_unique<network::TestURLLoaderFactory>()),
       test_network_connection_tracker_(
-          network::TestNetworkConnectionTracker::CreateInstance()) {
+          network::TestNetworkConnectionTracker::CreateInstance()),
+      variations_service_(nullptr) {
   DCHECK(!GetApplicationContext());
   SetApplicationContext(this);
 }
@@ -75,6 +77,12 @@ void TestingApplicationContext::SetChromeBrowserStateManager(
     ios::ChromeBrowserStateManager* manager) {
   DCHECK(thread_checker_.CalledOnValidThread());
   chrome_browser_state_manager_ = manager;
+}
+
+void TestingApplicationContext::SetVariationsService(
+    variations::VariationsService* variations_service) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  variations_service_ = variations_service;
 }
 
 void TestingApplicationContext::OnAppEnterForeground() {
@@ -151,7 +159,7 @@ ukm::UkmRecorder* TestingApplicationContext::GetUkmRecorder() {
 variations::VariationsService*
 TestingApplicationContext::GetVariationsService() {
   DCHECK(thread_checker_.CalledOnValidThread());
-  return nullptr;
+  return variations_service_;
 }
 
 net::NetLog* TestingApplicationContext::GetNetLog() {
