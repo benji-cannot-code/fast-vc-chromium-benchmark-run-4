b85121aaccce58b1a147301662dc222a9225186f
Weizhong Xia <weizhong@google.com>
2023-08-09 17:09:44 +0000

    Skip flakiness check on Wpt importer
    Wpt importer is running continuously without any human intervention.
    Running flaky endorsor on imported CLs is not possible because:
    1. We want to continuously import WPTs to chromium.
    2. Two-way-sync garders do not have the capacity to fix flaky tests on
       time.
    
    Disable flakiness check on Imported CLs for now.
    
    Bug: None
    Change-Id: I3991956ace36701f98ce4c41908e8818d7ac79f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763413
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181546}

--- third_party/blink/tools/blinkpy/w3c/test_importer_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/test_importer_unittest.py
@@ -471,7 +471,8 @@ class TestImporterTest(LoggingTestCase):
         self.assertEqual(
             importer._commit_message('aaaa', '1111'), 'Import 1111\n\n'
             'Using wpt-import in Chromium aaaa.\n\n'
-            'No-Export: true')
+            'No-Export: true\n'
+            'Validate-Test-Flakiness: skip')
 
     def test_cl_description_with_empty_environ(self):
         host = self.mock_host()
@@ -488,6 +489,7 @@ class TestImporterTest(LoggingTestCase):
             '/chromium/src/+/main/docs/testing/web_platform_tests.md\n\n'
             'NOAUTOREVERT=true\n'
             'No-Export: true\n'
+            'Validate-Test-Flakiness: skip\n'
             'Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,'
             'linux-wpt-input-fyi-rel,linux-blink-rel')
         self.assertEqual(host.executive.calls,
