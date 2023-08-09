31e0fb541d47da0a1acdeadbf6922dc7ac118c38
Quentin Pubert <qpubert@google.com>
2023-08-09 15:21:01 +0000

    [iOS] Add Photos Library provider for Save to Photos
    - ios/chrome/browser/providers/BUILD.gn
    - ios/chrome/browser/providers/photos/BUILD.gn
    - ios/chrome/browser/providers/photos/chromium_photos.mm
    - ios/chrome/browser/providers/photos/DEPS
    The Chromium provider returns an UnavailablePhotosService which is
    always unavailable.
    
    - ios/chrome/test/providers/BUILD.gn
    - ios/chrome/test/providers/photos/BUILD.gn
    - ios/chrome/test/providers/photos/test_photos.mm
    The test provider returns a fake.
    
    - ios/public/provider/chrome/browser/BUILD.gn
    - ios/public/provider/chrome/browser/photos/BUILD.gn
    - ios/public/provider/chrome/browser/photos/photos_api.h
    The provider API contains on function
    ```
    std::unique_ptr<PhotosService> CreatePhotosService(
         PhotosServiceConfiguration* configuration)
    ```
    which returns the service implementation for a given `configuration`.
    
    Low-Coverage-Reason: Chromium providers cannot be tested in the same CL
    Bug: 1471386
    Change-Id: I65ee66f1f1533f64cbf020333e4a23d409177a9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4716339
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181490}

---
+++ ios/public/provider/chrome/browser/photos/photos_api.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PHOTOS_PHOTOS_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PHOTOS_PHOTOS_API_H_
+
+#import <memory>
+
+class PhotosService;
+@class PhotosServiceConfiguration;
+
+namespace ios {
+namespace provider {
+
+// Creates a new instance of PhotosService.
+std::unique_ptr<PhotosService> CreatePhotosService(
+    PhotosServiceConfiguration* configuration);
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PHOTOS_PHOTOS_API_H_
