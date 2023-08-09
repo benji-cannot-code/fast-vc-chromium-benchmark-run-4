873ddbe5abceaa24d54d82d20b319aa3f412ff4a
Philipp Hancke <phancke@microsoft.com>
2023-08-09 08:21:47 +0000

    Remove more interdependencies between legacy and WPT web_tests
    follow-up to
      https://chromium-review.googlesource.com/c/chromium/src/+/3672118
    which also resyncs RTCPeerConnection-helper.js from WPT upstream.
    
    BUG=chromium:1299834
    
    Change-Id: I72b52c619a0d1df1a97691f3a49bd2616d686ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4759646
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Philipp Hancke <phancke@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1181355}

--- third_party/blink/web_tests/fast/peerconnection/resources/RTCPeerConnection-helper.js
+++ third_party/blink/web_tests/fast/peerconnection/resources/RTCPeerConnection-helper.js
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict'
 
 /*
+ *  Taken from wpt/webrtc/RTCPeerConnection-helper.js, update occasionally.
+ *
  *  Helper Methods for testing the following methods in RTCPeerConnection:
  *    createOffer
  *    createAnswer
@@ -714,3 +716,10 @@ const expectNoMoreGatheringStateChanges = async (t, pc) => {
             'Should not get an icegatheringstatechange right now!');
       });
 };
+
+async function queueAWebrtcTask() {
+  const pc = new RTCPeerConnection();
+  pc.addTransceiver('audio');
+  await new Promise(r => pc.onnegotiationneeded = r);
+}
+
