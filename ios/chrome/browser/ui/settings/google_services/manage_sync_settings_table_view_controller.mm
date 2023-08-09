97495bbd47f31e1c0cdaf031847c009078c74623
Jérôme Lebel <jlebel@chromium.org>
2023-08-09 08:47:15 +0000

    [iOS][UNO] Fix footer height in the settings
    This patch updates the footer height to be dynamic for the sync data
    type section.
    
    Regular font size:
    https://drive.google.com/file/d/1V7TDJHX9toFJ16Z6TmF0_kKL7koHdWOC/view?usp=drive_link&resourcekey=0-2znyPlloU6NGKMIuIXYvAA
    Big font size:
    https://drive.google.com/file/d/1D5fLrc5BbZjGAsiLIfEg_95omZZjdRRY/view?usp=drive_link&resourcekey=0-LQz-gxCEiTO11B4_fW3MuA
    
    Fixed: 1470706
    Change-Id: I2cde3f2e079740d3162293c837e9bb5ea80ca1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761724
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181364}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_table_view_controller.mm
@@ -29,7 +29,6 @@ CGFloat kAdvancedSettingsSectionHeaderHeightPointSize = 26.;
 
 // Table view customized footer heights.
 CGFloat kAccountSectionFooterHeightPointSize = 16.;
-CGFloat kSyncDataTypeSectionFooterHeightPointSize = 50.;
 CGFloat kDefaultSectionFooterHeightPointSize = 10.;
 
 }  // namespace
@@ -205,7 +204,7 @@ CGFloat kDefaultSectionFooterHeightPointSize = 10.;
       case AccountSectionIdentifier:
         return kAccountSectionFooterHeightPointSize;
       case SyncDataTypeSectionIdentifier:
-        return kSyncDataTypeSectionFooterHeightPointSize;
+        return UITableViewAutomaticDimension;
       case AdvancedSettingsSectionIdentifier:
       case SyncErrorsSectionIdentifier:
       case SignOutSectionIdentifier:
