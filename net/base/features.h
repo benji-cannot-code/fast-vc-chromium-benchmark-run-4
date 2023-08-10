f80bc09bbab3956b1a8aa3028c75857cc505be77
Victor Vasiliev <vasilvv@chromium.org>
2023-08-10 05:33:26 +0000

    Add feature flags to enable BBRv1 and BBRv2
    We currently use CUBIC, which is poorly suited for low-latency
    applications that WebTransport targets. Those flags would let us
    experiment with BBR instead.
    
    Change-Id: Icf6aaec40b6d69008336e3a85673be984200913b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764285
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181883}

--- net/base/features.h
+++ net/base/features.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/crypto_buildflags.h"
 #include "net/base/net_export.h"
 #include "net/net_buildflags.h"
+#include "net/third_party/quiche/src/quiche/quic/core/quic_types.h"
 
 namespace net::features {
 
@@ -421,6 +422,11 @@ NET_EXPORT BASE_DECLARE_FEATURE(kEnableEarlyHintsOnHttp11);
 
 // Enables draft-07 version of WebTransport over HTTP/3.
 NET_EXPORT BASE_DECLARE_FEATURE(kEnableWebTransportDraft07);
+
+// Enables custom congestion control for WebTransport over HTTP/3.
+NET_EXPORT BASE_DECLARE_FEATURE(kWebTransportCongestionControl);
+NET_EXPORT extern const base::FeatureParam<quic::CongestionControlType>
+    kWebTransportCongestionControlAlgorithm;
 
 // Enables Zstandard Content-Encoding support.
 NET_EXPORT BASE_DECLARE_FEATURE(kZstdContentEncoding);
--- net/quic/dedicated_web_transport_http3_client.cc
+++ net/quic/dedicated_web_transport_http3_client.cc
@@ -327,6 +327,14 @@ void RecordNegotiatedWebTransportVersion(
       "Net.WebTransport.NegotiatedWebTransportVersion", negotiated);
 }
 
+void AdjustSendAlgorithm(quic::QuicConnection& connection) {
+  if (!base::FeatureList::IsEnabled(features::kWebTransportCongestionControl)) {
+    return;
+  }
+  connection.sent_packet_manager().SetSendAlgorithm(
+      features::kWebTransportCongestionControlAlgorithm.Get());
+}
+
 }  // namespace
 
 DedicatedWebTransportHttp3Client::DedicatedWebTransportHttp3Client(
@@ -597,6 +605,7 @@ void DedicatedWebTransportHttp3Client::CreateConnection() {
   event_logger_ = std::make_unique<QuicEventLogger>(session_.get(), net_log_);
   connection_->set_debug_visitor(event_logger_.get());
   connection_->set_creator_debug_delegate(event_logger_.get());
+  AdjustSendAlgorithm(*connection_);
 
   session_->Initialize();
   packet_reader_->StartReading();
