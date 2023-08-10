8048cf527ab56c452d1ad6478354bba651d2eae2
Solomon Kinard <solomonkinard@chromium.org>
2023-08-10 09:37:38 +0000

    Extensions: ExtensionId: Use alias instead of std::string
    Context:
    crrev.com/c/4763085/1..2/extensions/browser/api/api_resource.h#b22
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: chromium:1471741
    Change-Id: I59258b3c2714b686664fdfa23648922a04cb38d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767241
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181960}

--- chrome/browser/extensions/api/notifications/extension_notification_handler_unittest.cc
+++ chrome/browser/extensions/api/notifications/extension_notification_handler_unittest.cc
@@ -49,7 +49,7 @@ class TestExtensionNotificationHandler : public ExtensionNotificationHandler {
   }
 
  private:
-  std::string extension_id_;
+  ExtensionId extension_id_;
   std::string event_name_;
   size_t param_count_;
 };
