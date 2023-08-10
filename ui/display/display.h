60e60e93e3b5e6884093f262720aed87a1d0c1dd
Mitsuru Oshima <oshima@chromium.org>
2023-08-10 00:46:17 +0000

    Use RefCounted to share DisplayColorSpaces when copying Display
    This will reduce ~450 bytes in size. It needs to be thread safe because object can be deleted in different tests in some test.
    
    Bug: 1421577
    Test: no functional chang. all test must pass.
    Change-Id: Ie6f640138837a1ab04305caca5d13de420b138ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4765798
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181801}

--- ui/display/display.h
+++ ui/display/display.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/display/display_export.h"
@@ -286,6 +288,28 @@ class DISPLAY_EXPORT Display final {
  private:
   friend struct mojo::StructTraits<mojom::DisplayDataView, Display>;
 
+  // A ref counted object to avoid copying DisplayColorSpaces.
+  class DisplayColorSpacesRef
+      : public base::RefCountedThreadSafe<DisplayColorSpacesRef> {
+   public:
+    DisplayColorSpacesRef() = default;
+    explicit DisplayColorSpacesRef(const gfx::DisplayColorSpaces& color_spaces)
+        : color_spaces_(color_spaces) {}
+    DisplayColorSpacesRef(const DisplayColorSpacesRef& color_spaces) = delete;
+    const DisplayColorSpacesRef& operator=(const DisplayColorSpacesRef) =
+        delete;
+
+    const gfx::DisplayColorSpaces& color_spaces() const {
+      return color_spaces_;
+    }
+
+   private:
+    friend class base::RefCountedThreadSafe<DisplayColorSpacesRef>;
+
+    ~DisplayColorSpacesRef() = default;
+    const gfx::DisplayColorSpaces color_spaces_;
+  };
+
   int64_t id_ = kInvalidDisplayId;
   gfx::Rect bounds_;
   // If non-empty, then should be same as |bounds_|. Used to avoid rounding
@@ -299,7 +323,7 @@ class DISPLAY_EXPORT Display final {
   TouchSupport touch_support_ = TouchSupport::UNKNOWN;
   AccelerometerSupport accelerometer_support_ = AccelerometerSupport::UNKNOWN;
   gfx::Size maximum_cursor_size_;
-  gfx::DisplayColorSpaces color_spaces_;
+  scoped_refptr<const DisplayColorSpacesRef> color_spaces_;
   int color_depth_;
   int depth_per_component_;
   bool is_monochrome_ = false;
