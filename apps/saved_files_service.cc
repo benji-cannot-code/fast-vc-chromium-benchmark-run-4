fa71485b1cf33d442b840f80401e97a4eefbb7ab
Solomon Kinard <solomonkinard@chromium.org>
2023-08-10 00:50:20 +0000

    Extensions: ExtensionId: Use alias instead of std::string
    Context:
    crrev.com/c/4763085/1..2/extensions/browser/api/api_resource.h#b22
    
    This CL was uploaded by git cl split.
    
    R=dominickn@chromium.org
    
    Bug: chromium:1471741
    Change-Id: I458d7bebc7f83107f01f31d3ec88726c634908b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767416
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181802}

--- apps/saved_files_service.cc
+++ apps/saved_files_service.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/common/extension_id.h"
 #include "extensions/common/permissions/api_permission.h"
 #include "extensions/common/permissions/permission_set.h"
 #include "extensions/common/permissions/permissions_data.h"
@@ -169,7 +170,7 @@ class SavedFilesService::SavedFiles {
   void LoadSavedFileEntriesFromPreferences();
 
   raw_ptr<content::BrowserContext> context_;
-  const std::string extension_id_;
+  const extensions::ExtensionId extension_id_;
 
   // Contains all file entries that have been registered, keyed by ID.
   std::unordered_map<std::string, std::unique_ptr<SavedFileEntry>>
