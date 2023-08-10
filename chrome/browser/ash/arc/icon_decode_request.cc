c197fca5e8cc7bbacd6917a1031a3eec051a3244
Alexander Alekseev <alemate@chromium.org>
2023-08-10 00:05:04 +0000

    Add ChromeOS trace events in /chrome/browser/ash/arc
    Investigating ChromeOS login performance revealed that many
    potentially slow UI thread tasks are not instrumented.
    This Cl adds some events around slow tasks that we saw in
    our tests.
    
    Bug: b:293198304
    
    CC=​achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=khmel@chromium.org
    
    Change-Id: Idbe70631f3ae2c24c222961b1ef5d7832a20ced4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763375
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181775}

--- chrome/browser/ash/arc/icon_decode_request.cc
+++ chrome/browser/ash/arc/icon_decode_request.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/no_destructor.h"
+#include "base/trace_event/trace_event.h"
 #include "chrome/grit/component_extension_resources.h"
 #include "content/public/browser/browser_thread.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
@@ -91,6 +92,7 @@ IconDecodeRequest::~IconDecodeRequest() = default;
 
 void IconDecodeRequest::StartWithOptions(
     const std::vector<uint8_t>& image_data) {
+  TRACE_EVENT0("ui", "IconDecodeRequest::StartWithOptions");
   if (disable_safe_decoding_for_testing) {
     if (image_data.empty()) {
       OnDecodeImageFailed();
@@ -111,6 +113,7 @@ void IconDecodeRequest::StartWithOptions(
 }
 
 void IconDecodeRequest::OnImageDecoded(const SkBitmap& bitmap) {
+  TRACE_EVENT0("ui", "IconDecodeRequest::OnImageDecoded");
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   const gfx::ImageSkia icon(
       std::make_unique<IconSource>(bitmap, dimension_dip_),
@@ -120,6 +123,7 @@ void IconDecodeRequest::OnImageDecoded(const SkBitmap& bitmap) {
 }
 
 void IconDecodeRequest::OnDecodeImageFailed() {
+  TRACE_EVENT0("ui", "IconDecodeRequest::OnDecodeImageFailed");
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   DLOG(ERROR) << "Failed to decode an icon image.";
   OnImageDecoded(SkBitmap());
