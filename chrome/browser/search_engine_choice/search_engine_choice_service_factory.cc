70953b74a0d94aa12803dce26849c35d8881cc08
Nina Satragno <nsatragno@chromium.org>
2023-08-10 14:51:24 +0000

    Revert "[waffle]Enable build flag for desktop platforms"
    This reverts commit d5d54678a4bf1479d24aa0a1739befc8b454a28b.
    
    Reason for revert: crashing asan test
    crbug.com/1471982
    
    Original change's description:
    > [waffle]Enable build flag for desktop platforms
    >
    > Bug: b/280753754
    > Change-Id: I309bfdaa592c6126d815b5d0d450905dbad4fc76
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738465
    > Reviewed-by: Jens Mueller <muellerj@google.com>
    > Commit-Queue: Jack Yammine <jyammine@google.com>
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1181943}
    
    Bug: b/280753754
    Change-Id: I6c03a05ef26352acacbf27fcaa5a9bc967395bb8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770076
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182057}

--- chrome/browser/search_engine_choice/search_engine_choice_service_factory.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_service_factory.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
 
 #include "base/check_deref.h"
-#include "base/check_is_test.h"
-#include "build/branding_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_service.h"
@@ -18,16 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chromeos/components/kiosk/kiosk_utils.h"
 #endif
-
-namespace {
-// Stores whether this is a Google Chrome-branded build.
-bool g_is_chrome_build =
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-    true;
-#else
-    false;
-#endif
-}  // namespace
 
 SearchEngineChoiceServiceFactory::SearchEngineChoiceServiceFactory()
     : ProfileKeyedServiceFactory(
@@ -52,14 +40,6 @@ SearchEngineChoiceService* SearchEngineChoiceServiceFactory::GetForProfile(
       GetInstance()->GetServiceForBrowserContext(profile, true));
 }
 
-// static
-base::AutoReset<bool>
-SearchEngineChoiceServiceFactory::ScopedChromeBuildOverrideForTesting(
-    bool force_chrome_build) {
-  CHECK_IS_TEST();
-  return base::AutoReset<bool>(&g_is_chrome_build, force_chrome_build);
-}
-
 bool SearchEngineChoiceServiceFactory::IsProfileEligibleForChoiceScreen(
     const policy::PolicyService& policy_service,
     Profile& profile) const {
@@ -69,7 +49,7 @@ bool SearchEngineChoiceServiceFactory::IsProfileEligibleForChoiceScreen(
 
   bool is_regular_profile = profile.IsRegularProfile();
 #if BUILDFLAG(IS_CHROMEOS)
-  is_regular_profile &= !profiles::IsManagedGuestSession() &&
+  is_regular_profile &= !profiles::IsPublicSession() &&
                         !chromeos::IsKioskSession() &&
                         !profiles::IsChromeAppKioskSession();
 #endif
@@ -85,9 +65,6 @@ bool SearchEngineChoiceServiceFactory::IsProfileEligibleForChoiceScreen(
 
 KeyedService* SearchEngineChoiceServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
-  if (!g_is_chrome_build) {
-    return nullptr;
-  }
   auto& profile = CHECK_DEREF(Profile::FromBrowserContext(context));
 
   if (!IsProfileEligibleForChoiceScreen(
