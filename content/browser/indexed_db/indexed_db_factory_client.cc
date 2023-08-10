a237423fb1c299ebebffd81fbd827011954565bf
Evan Stade <estade@chromium.org>
2023-08-10 05:21:24 +0000

    IndexedDB: collapse CursorImpl into IndexedDBCursor.
    This simplifies ownership because there are fewer objects and
    IndexedDBCursor can be self-owned.
    
    Bug: 843764
    Change-Id: Ie5f0eb7556ce4936008ad3295fdc3e12fbcdb7b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735764
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181881}

--- content/browser/indexed_db/indexed_db_factory_client.cc
+++ content/browser/indexed_db/indexed_db_factory_client.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
-#include "content/browser/indexed_db/cursor_impl.h"
 #include "content/browser/indexed_db/database_impl.h"
 #include "content/browser/indexed_db/indexed_db_connection.h"
 #include "content/browser/indexed_db/indexed_db_context_impl.h"
