8dc03a0f4fefca729ad901a7f2c238e060d29620
Christoph Schwering <schwering@google.com>
2023-08-10 21:53:50 +0000

    [Autofill] Remove unneeded kAutofillSharedAutofill from tests
    Bug: 1007974
    Change-Id: I4da1fb6653425bd51a8e75ae673e90adb8e1d1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761984
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182302}

--- components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
@@ -7919,12 +7919,7 @@ class AutofillMetricsCrossFrameFormTest : public AutofillMetricsTest {
     std::u16string cvc;
   };
 
-  AutofillMetricsCrossFrameFormTest() {
-    scoped_feature_list_.InitWithFeaturesAndParameters(
-        {base::test::FeatureRefAndParams(features::kAutofillSharedAutofill,
-                                         {{"relax_shared_autofill", "true"}})},
-        {});
-  }
+  AutofillMetricsCrossFrameFormTest() = default;
   ~AutofillMetricsCrossFrameFormTest() override = default;
 
   void SetUp() override {
@@ -8028,7 +8023,6 @@ class AutofillMetricsCrossFrameFormTest : public AutofillMetricsTest {
     return *it;
   }
 
-  base::test::ScopedFeatureList scoped_feature_list_;
   FormData form_;
   CreditCardAndCvc credit_card_with_cvc_;
 };
