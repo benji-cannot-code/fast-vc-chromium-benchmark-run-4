ae01ef74d287d5113b28018d42d6cebce8d50c5d
Jason Thai <jasontt@chromium.org>
2023-08-10 19:12:50 +0000

    wallpaper: CHECK for wallpaper type in ShowUserWallpaper()
    This CHECK accounts for all the wallpaper types allowed to be saved in
    pref. This makes it clear how each wallpaper type is handled when the
    function `ShowUserWallpaper()` is called.
    
    BUG=b:294249094
    TEST=ash_unittests --gtest_filter="WallpaperControllerTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I02e24a38a70b83f6322de9f5b977c643e9e0f312
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742906
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182218}

--- ash/wallpaper/wallpaper_controller_impl.cc
+++ ash/wallpaper/wallpaper_controller_impl.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece_forward.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
+#include "base/types/cxx23_to_underlying.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/account_id/account_id.h"
 #include "components/prefs/pref_service.h"
@@ -1238,6 +1239,10 @@ void WallpaperControllerImpl::ShowUserWallpaper(
     SetWallpaperFromInfo(account_id, info);
     return;
   }
+
+  CHECK(info.type == WallpaperType::kCustomized ||
+        info.type == WallpaperType::kPolicy)
+      << " Got unhandled wallpaper type=" << base::to_underlying(info.type);
 
   std::string sub_dir = GetCustomWallpaperSubdirForCurrentResolution();
   base::FilePath wallpaper_path =
