ba6ef72dbbbb1afe7a52188987e3bca3cae90845
Jeffrey Young <cowmoo@google.com>
2023-08-10 19:30:48 +0000

    p13n: remove IronResizableBehavior from base class
    WallpaperCollections still relied on resize behavior to
    adjust layout while hidden. Manually fire iron-resize
    event in that case.
    
    Bug: b:294895874
    Test: navigate around Personalization App while resizing app
    Change-Id: I18765b28d88c86d873d8da9f0f53753021cadabb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4757322
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182230}

--- ash/webui/personalization_app/resources/js/wallpaper/wallpaper_collections_element.ts
+++ ash/webui/personalization_app/resources/js/wallpaper/wallpaper_collections_element.ts
@@ -19,6 +19,7 @@ import {assert} from 'chrome://resources/js/assert_ts.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {FilePath} from 'chrome://resources/mojo/mojo/public/mojom/base/file_path.mojom-webui.js';
 import {Url} from 'chrome://resources/mojo/url/mojom/url.mojom-webui.js';
+import {IronListElement} from 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
 import {afterNextRender} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {GooglePhotosEnablementState, WallpaperCollection, WallpaperImage} from '../../personalization_app.mojom-webui.js';
@@ -214,6 +215,12 @@ function getTemporaryBackdropCollectionId(index: number) {
 
 function isTimeOfDay({id}: WallpaperCollection|Tile): boolean {
   return id === loadTimeData.getString('timeOfDayWallpaperCollectionId');
+}
+
+export interface WallpaperCollections {
+  $: {
+    grid: IronListElement,
+  };
 }
 
 export class WallpaperCollections extends WithPersonalizationStore {
@@ -415,7 +422,7 @@ export class WallpaperCollections extends WithPersonalizationStore {
     if (!hidden) {
       document.title = this.i18n('wallpaperLabel');
     }
-    afterNextRender(this, () => this.notifyResize());
+    afterNextRender(this, () => this.$.grid.fire('iron-resize'));
   }
 
   /**
