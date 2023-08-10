2a6c02adfb068a27bcbc34f5a5d9cfea770bfb25
Jerry Liu <pzliu@google.com>
2023-08-10 22:17:41 +0000

    wallpaper: mv UpdateWallpaperForRootWindow to blur manager
    Move setting wallpaper blur logic for root window from Wallpaper
    Controller to |WallpaperBlurManager::UpdateBlurForRootWindow|.
    
    BUG=b:290378856
    TEST=ash_unittests --gtest_filter="*Wallpaper*"
    
    Change-Id: I9b6fe8d8400fbf338963f63ad8b157f9f75b7591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4749622
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182319}

--- ash/wallpaper/wallpaper_blur_manager.h
+++ ash/wallpaper/wallpaper_blur_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/wallpaper/wallpaper_types.h"
+#include "ash/root_window_controller.h"
 
 namespace ash {
 
@@ -44,9 +45,10 @@ class ASH_EXPORT WallpaperBlurManager {
     return is_wallpaper_blurred_for_lock_state_;
   }
 
-  void set_is_wallpaper_blurred_for_lock_state(bool blur) {
-    is_wallpaper_blurred_for_lock_state_ = blur;
-  }
+  bool UpdateBlurForRootWindow(aura::Window* root_window,
+                               bool lock_state_changed,
+                               bool new_root,
+                               WallpaperType wallpaper_type);
 
   // Make pixel testing more reliable by allowing wallpaper blur.
   void set_allow_blur_for_testing() { allow_blur_for_testing_ = true; }
