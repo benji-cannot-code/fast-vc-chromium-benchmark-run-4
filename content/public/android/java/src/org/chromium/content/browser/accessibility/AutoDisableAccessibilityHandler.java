a4117e0837c4f3bab108bcf4f311a0a59c2d63b8
Victor Vianna <victorvianna@google.com>
2023-08-10 14:23:09 +0000

    Revert "[Auto-disable Accessibility] Add histograms for feature usage"
    This reverts commit 156236164fc352a3b02b4e2bdaedeb36fee16dd5.
    
    Reason for revert: Causes CI failures crbug.com/1471902.
    
    Original change's description:
    > [Auto-disable Accessibility] Add histograms for feature usage
    >
    > This CL continues the work of the Auto-disable accessibility feature
    > in preparation of enabling the feature for A/B testing.
    >
    > With this CL we add histograms to track usage of the feature. We will
    > track how often we enable/disable the feature, and how long the given
    > instance was in the current state before changing (e.g. how long was
    > accessibility enabled before being auto-disabled, or, how long was
    > accessibility disabled before re-enabling). We also separate these
    > into initial and successive categories to separate the first call for
    > each instance from any later calls.
    >
    > AX-Relnotes: N/A
    > Bug: 1430202, b/265493191
    > Change-Id: Ibc42854105af4b486db39c6066bb468ca6f8f57f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738305
    > Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    > Reviewed-by: Evan Liu <evliu@google.com>
    > Commit-Queue: Mark Schillaci <mschillaci@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1181705}
    
    Bug: 1430202, 1471902, b/265493191
    Change-Id: Ia77fc250874f3816a7f691e907c3f1dbacbbb9dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4769364
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182034}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/AutoDisableAccessibilityHandler.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/AutoDisableAccessibilityHandler.java
@@ -65,8 +65,7 @@ public class AutoDisableAccessibilityHandler {
     /**
      * Helper method to notify Client and reset local state when the timer has expired.
      */
-    @VisibleForTesting()
-    public void notifyDisable() {
+    private void notifyDisable() {
         mClient.onDisabled();
         mHasPendingTimer = false;
     }
