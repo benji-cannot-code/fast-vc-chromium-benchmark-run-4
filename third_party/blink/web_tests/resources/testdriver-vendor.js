800d9164767628610e930b6d8969c96b6db03707
Christian Biesinger <cbiesinger@chromium.org>
2023-08-10 20:31:51 +0000

    [FedCM] Support a new dialog type for the IDP signin confirmation
    Also supports closing that dialog through dismissDialog
    
    Bug: 1451884
    Change-Id: If1d33837562cf5e28bf33d4a78a2091e506a6840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740672
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182269}

--- third_party/blink/web_tests/resources/testdriver-vendor.js
+++ third_party/blink/web_tests/resources/testdriver-vendor.js
@@ -527,12 +527,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   };
 
+  window.test_driver_internal.get_fedcm_dialog_type = async function() {
+    return internals.getFedCmDialogType();
+  }
+
   window.test_driver_internal.get_fedcm_dialog_title = async function() {
     return internals.getFedCmTitle();
   }
 
   window.test_driver_internal.select_fedcm_account = async function(account_index) {
     return internals.selectFedCmAccount(account_index);
+  }
+
+  window.test_driver_internal.cancel_fedcm_dialog = async function() {
+    return internals.dismissFedCmDialog();
   }
 
   // Enable automation so we don't wait for user input on unimplemented APIs
