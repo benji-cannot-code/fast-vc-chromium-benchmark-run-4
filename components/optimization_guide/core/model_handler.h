5e35ee7cba8974a9ac7154140b3d56ea32eeca41
Jun Zou <junzou@chromium.org>
2023-08-10 02:24:52 +0000

    [Omnibox][ML] Add sync TFLite ML model execution API.
    - Add sync batch model execution API.
    - Add a dedicated background thread for model file loading.
    - Update the existing async model execution to load model on the background thread if not loaded, and execute model on the model execution thread.
    - Enable sync url suggestion scoring in autocomplete controller.
    
    Bug: 1468407, b/294133650
    Change-Id: I61fa3711411cf9a9e59f88a01ff03ed27747dc81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4726969
    Commit-Queue: Jun Zou <junzou@google.com>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181841}

--- components/optimization_guide/core/model_handler.h
+++ components/optimization_guide/core/model_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/cancelable_task_tracker.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
+#include "base/timer/elapsed_timer.h"
 #include "components/optimization_guide/core/model_executor.h"
 #include "components/optimization_guide/core/model_util.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
@@ -154,6 +155,19 @@ class ModelHandler : public OptimizationTargetModelObserver {
 
     tracker->PostTask(model_executor_task_runner_.get(), FROM_HERE,
                       GetBatchExecutionTask(std::move(callback), batch_input));
+  }
+
+  // Runs synchronous batch model execution.
+  // Returns batch model outputs.
+  std::vector<absl::optional<OutputType>> BatchExecuteModelWithInputSync(
+      typename ModelExecutor<OutputType, InputType>::ConstRefInputVector
+          inputs) {
+    base::ElapsedTimer timer;
+    auto batch_model_outputs =
+        model_executor_->SendForBatchExecutionSync(inputs);
+    RecordTaskExecutionLatency(optimization_target_,
+                               /*execution_time=*/timer.Elapsed());
+    return batch_model_outputs;
   }
 
   void SetShouldUnloadModelOnComplete(bool should_auto_unload) {
