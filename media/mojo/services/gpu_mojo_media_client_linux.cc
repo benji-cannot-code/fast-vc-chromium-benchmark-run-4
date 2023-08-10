63e5cfee3827f72783e5409398a0011910f8cb4a
Jianhui Dai <jianhui.j.dai@intel.com>
2023-08-10 04:12:49 +0000

    media: Update commentary for legacy VDA
    Linux no longer uses the legacy VDA path (See CL:4585782). This CL
    updates the commentary to reflect this change.
    
    Bug: 1448064
    Change-Id: I294c4246b5232d2c470eebd0554fefb0b661eee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755084
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Jianhui J Dai <jianhui.j.dai@intel.com>
    Reviewed-by: Pilar Molina Lopez <pmolinalopez@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181870}

--- media/mojo/services/gpu_mojo_media_client_linux.cc
+++ media/mojo/services/gpu_mojo_media_client_linux.cc
@@ -9,18 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "media/base/audio_decoder.h"
 #include "media/base/audio_encoder.h"
+#include "media/base/media_log.h"
 #include "media/base/media_switches.h"
 #include "media/gpu/chromeos/mailbox_video_frame_converter.h"
 #include "media/gpu/chromeos/platform_video_frame_pool.h"
 #include "media/gpu/chromeos/video_decoder_pipeline.h"
-#include "media/gpu/ipc/service/vda_video_decoder.h"
 
 namespace media {
 
 namespace {
 
 VideoDecoderType GetPreferredLinuxDecoderImplementation() {
-  // VaapiVideoDecoder flag is required for both VDA and VaapiVideoDecoder.
+  // VaapiVideoDecoder flag is required for VaapiVideoDecoder.
   if (!base::FeatureList::IsEnabled(kVaapiVideoDecodeLinux)) {
     return VideoDecoderType::kUnknown;
   }
@@ -51,7 +51,7 @@ std::vector<Fourcc> GetPreferredRenderableFourccs(
 VideoDecoderType GetActualPlatformDecoderImplementation(
     const gpu::GpuPreferences& gpu_preferences,
     const gpu::GPUInfo& gpu_info) {
-  // On linux, VDA and Vaapi have GL restrictions.
+  // On linux, Vaapi has GL restrictions.
   switch (GetPreferredLinuxDecoderImplementation()) {
     case VideoDecoderType::kUnknown:
       return VideoDecoderType::kUnknown;
