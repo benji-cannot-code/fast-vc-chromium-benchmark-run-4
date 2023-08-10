da18cfa30b5a335d7d235e54a11a1e6d51afe0bb
Henrique Nakashima <hnakashima@chromium.org>
2023-08-10 20:00:06 +0000

    [Android] Cleanup ModalDialogButtonProtection flag
    The flag is launched, so clean up and remove the feature list too.
    
    Bug: 1445758
    Change-Id: Iee366458c3d9d14ecaa126a1e868375cd56bf3d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764144
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182247}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogViewTest.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogViewTest.java
@@ -51,7 +51,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.FakeTimeTestRule;
-import org.chromium.base.FeatureList;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
@@ -63,8 +62,6 @@ import org.chromium.ui.modaldialog.ModalDialogProperties;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.test.util.BlankUiTestActivity;
 import org.chromium.ui.test.util.DisableAnimationsTestRule;
-
-import java.util.Map;
 
 /**
  * Tests for {@link ModalDialogView}.
@@ -485,8 +482,6 @@ public class ModalDialogViewTest {
     @MediumTest
     @Feature({"ModalDialog"})
     public void testButtonTapProtection() {
-        FeatureList.setTestFeatures(
-                Map.of(ModalDialogFeatureList.MODALDIALOG_BUTTON_PROTECTION, true));
         final var callbackHelper = new CallbackHelper();
         var controller = new ModalDialogProperties.Controller() {
             @Override
