70ba0303cfdf9615ad9e7c1e64ea27a76b4cf03d
Alexis Hetu <sugoi@chromium.org>
2023-08-10 14:31:27 +0000

    [iOS] Rescale Payments Bottom Sheet icons when necessary
    This CL applies a conditional rescale of icons if they are smaller
    than expected, but doesn't scale them up past a scale of 1.0, in
    order to avoid showing blurry pixels. Applying a scale this way
    preserves the aspect ratio and is done without requiring a redraw.
    
    Bug: 1450214
    Change-Id: I5e79000609ec850d811d891f1dbf12d6097e198b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763217
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182043}

--- ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_mediator.mm
+++ ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_mediator.mm
@@ -62,7 +62,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                   /* with_prefix=*/false));
     self.accessibleCardName = [self accessibleCardName:creditCard];
     self.backendIdentifier = base::SysUTF8ToNSString(creditCard->guid());
-    self.icon = icon;
+
+    if (icon.size.width > 0.0 && icon.size.width < 40.0 && icon.scale > 1.0) {
+      // If the icon is smaller than desired, but is scaled, reduce the scale
+      // (to a minimum of 1.0) in order to attempt to achieve the desired size.
+      self.icon = [UIImage
+          imageWithCGImage:[icon CGImage]
+                     scale:MAX((icon.scale * icon.size.width / 40.0), 1.0)
+               orientation:(icon.imageOrientation)];
+    } else {
+      self.icon = icon;
+    }
   }
   return self;
 }
