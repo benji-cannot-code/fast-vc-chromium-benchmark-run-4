6397fd77b10197c9a4d18a5000a506670caf5013
Denis Kuznetsov <antrim@chromium.org>
2023-08-10 11:20:18 +0000

    ChromeOS: Allow passwordless user creation under a flag
    This CL prevents "Saml password confirmation" screen triggering
    when user goes through passwordless GAIA sign-in, and avoids
    setting up empty password in a cryptohome.
    
    Note that if no knowledge factor (e.g. PIN) is set up during the
    onboarding flow, user cleanup mechanism would be triggered upon
    next restart.
    
    Bug: b/292954982
    Change-Id: Ia3c26a2c56493160e82639a4b3a4f73c290d930f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4753254
    Reviewed-by: Martin Bidlingmaier <mbid@google.com>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181989}

--- chrome/browser/ui/webui/ash/login/oobe_ui.cc
+++ chrome/browser/ui/webui/ash/login/oobe_ui.cc
@@ -642,7 +642,8 @@ void OobeUI::BindInterface(
 void OobeUI::BindInterface(
     mojo::PendingReceiver<auth::mojom::AuthFactorConfig> receiver) {
   auth::BindToAuthFactorConfig(std::move(receiver),
-                               quick_unlock::QuickUnlockFactory::GetDelegate());
+                               quick_unlock::QuickUnlockFactory::GetDelegate(),
+                               g_browser_process->local_state());
 }
 
 void OobeUI::BindInterface(
@@ -651,7 +652,7 @@ void OobeUI::BindInterface(
   CHECK(pin_backend);
   auth::BindToPinFactorEditor(std::move(receiver),
                               quick_unlock::QuickUnlockFactory::GetDelegate(),
-                              *pin_backend);
+                              g_browser_process->local_state(), *pin_backend);
 }
 
 OobeUI::OobeUI(content::WebUI* web_ui, const GURL& url)
--- chromeos/ash/services/auth_factor_config/auth_factor_config.h
+++ chromeos/ash/services/auth_factor_config/auth_factor_config.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/login/auth/public/authentication_error.h"
 #include "chromeos/ash/services/auth_factor_config/chrome_browser_delegates.h"
 #include "chromeos/ash/services/auth_factor_config/public/mojom/auth_factor_config.mojom.h"
+#include "components/prefs/pref_service.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
 
@@ -26,7 +27,8 @@ class AuthFactorConfig : public mojom::AuthFactorConfig {
                                       mojom::AuthFactor::kMinValue,
                                       mojom::AuthFactor::kMaxValue>;
 
-  explicit AuthFactorConfig(QuickUnlockStorageDelegate*);
+  explicit AuthFactorConfig(QuickUnlockStorageDelegate*,
+                            PrefService* local_state);
   ~AuthFactorConfig() override;
 
   AuthFactorConfig(const AuthFactorConfig&) = delete;
@@ -88,6 +90,11 @@ class AuthFactorConfig : public mojom::AuthFactorConfig {
       absl::optional<AuthenticationError> error);
 
   raw_ptr<QuickUnlockStorageDelegate> quick_unlock_storage_;
+  // This instance is held by browser process (see in_process_instances)
+  // as well as local_state_, so they should be local state would become
+  // invalid quite late in the flow, by that time it should not be possible
+  // to interact with AuthFactorConfig.
+  raw_ptr<PrefService, DisableDanglingPtrDetection> local_state_;
   mojo::ReceiverSet<mojom::AuthFactorConfig> receivers_;
   mojo::RemoteSet<mojom::FactorObserver> observers_;
   AuthFactorEditor auth_factor_editor_;
