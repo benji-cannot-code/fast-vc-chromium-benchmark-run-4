12b173a195c4bc5284e5ef20326b927205d03a2e
Joey Arhar <jarhar@chromium.org>
2023-08-10 03:19:33 +0000

    Add closewatcher+popover+dialog WPT
    This was recommended here:
    https://chromium-review.googlesource.com/c/chromium/src/+/4686066/1..3//COMMIT_MSG#b23
    
    Change-Id: I6f6b920824b1d14e9a1cb35ec76a29854465e885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706545
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181860}

--- third_party/blink/web_tests/external/wpt/close-watcher/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/close-watcher/resources/helpers.js
@@ -1,12 +1,27 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-window.createRecordingCloseWatcher = (t, events, name, type) => {
+window.createRecordingCloseWatcher = (t, events, name, type, parent) => {
   let watcher = null;
   if (type === 'dialog') {
     watcher = document.createElement('dialog');
     watcher.textContent = 'hello world';
     t.add_cleanup(() => watcher.remove());
-    document.body.appendChild(watcher);
+    if (parent) {
+      parent.appendChild(watcher);
+    } else {
+      document.body.appendChild(watcher);
+    }
     watcher.showModal();
+  } else if (type === 'popover') {
+    watcher = document.createElement('div');
+    watcher.setAttribute('popover', 'auto');
+    watcher.textContent = 'hello world';
+    t.add_cleanup(() => watcher.remove());
+    if (parent) {
+      parent.appendChild(watcher);
+    } else {
+      document.body.appendChild(watcher);
+    }
+    watcher.showPopover();
   } else {
     watcher = new CloseWatcher();
     t.add_cleanup(() => watcher.destroy());
@@ -20,7 +35,7 @@ window.createRecordingCloseWatcher = (t, events, name, type) => {
 };
 
 window.createBlessedRecordingCloseWatcher = async (t, events, name, type, dialog) => {
-  return dialogResilientBless(dialog, () => createRecordingCloseWatcher(t, events, name, type));
+  return dialogResilientBless(dialog, () => createRecordingCloseWatcher(t, events, name, type, dialog));
 };
 
 window.sendEscKey = () => {
@@ -41,7 +56,7 @@ window.sendCloseRequest = window.sendEscKey;
 // This function is a version of test_driver.bless which works on dialog elements:
 // https://github.com/web-platform-tests/wpt/issues/41218
 window.dialogResilientBless = async (watcher, fn) => {
-  if (watcher instanceof HTMLDialogElement) {
+  if (watcher instanceof HTMLElement) {
     const button = document.createElement('button');
     watcher.appendChild(button);
     await test_driver.click(button);
