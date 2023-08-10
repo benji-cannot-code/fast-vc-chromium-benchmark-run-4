655ca840bb7be34722f22e84c2a79e43d43840b9
Hailey Wang <haileywang@google.com>
2023-08-10 01:10:29 +0000

    [Segmentation] Disable segmentation ukm
    Rolling back experiment.
    
    Bug: 1471754
    Change-Id: Id71832b8090c03616cdd6833d6732fc393a18ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766938
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181817}

--- components/segmentation_platform/public/features.cc
+++ components/segmentation_platform/public/features.cc
@@ -15,7 +15,7 @@ BASE_FEATURE(kSegmentationPlatformFeature,
 
 BASE_FEATURE(kSegmentationPlatformUkmEngine,
              "SegmentationPlatformUkmEngine",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSegmentationPlatformUserVisibleTaskRunner,
              "SegmentationPlatformUserVisibleTaskRunner",
