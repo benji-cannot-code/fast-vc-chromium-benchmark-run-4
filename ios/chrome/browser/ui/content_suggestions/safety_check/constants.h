1d11135690431cf1b86056b7ce1b2421211d4dcb
Benjamin Williams <bwwilliams@google.com>
2023-08-10 16:15:40 +0000

    [iOS] Create SafetyCheckItemView class.
    SafetyCheckItemView is used for displaying a particular Safety Check
    item in the Magic Stack module. SafetyCheckItemView is modeled after
    SetUpListItemView.
    
    This change:
    
    1. Creates SafetyCheckItemView.
    2. Updates SafetyCheckItemIcon styling.
    
    Change-Id: I5a9bf11034f0648781a5d9525227b658e9fa7523
    Fixed: 1471788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764107
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182108}

---
+++ ios/chrome/browser/ui/content_suggestions/safety_check/constants.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SAFETY_CHECK_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SAFETY_CHECK_CONSTANTS_H_
+
+#import <Foundation/Foundation.h>
+
+namespace safety_check {
+
+// Accessibility IDs used for various UI items.
+extern NSString* const kAllSafeItemID;
+extern NSString* const kRunningItemID;
+extern NSString* const kUpdateChromeItemID;
+extern NSString* const kPasswordItemID;
+extern NSString* const kSafeBrowsingItemID;
+extern NSString* const kDefaultItemID;
+
+}  // namespace safety_check
+
+#endif  // IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SAFETY_CHECK_CONSTANTS_H_
