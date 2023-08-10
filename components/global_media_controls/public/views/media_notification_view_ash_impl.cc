6bb1bf4dd85907c99d43c6f57d068cc30f1d7ed7
Yiren Wang <yrw@chromium.org>
2023-08-10 18:40:06 +0000

    CrOS GMC V2: Update labels for the start casting button
    Use labels to indicate whether user can use the button to show or hide
    the device list for casting
    
    Bug: 1470931
    Change-Id: Ie57de06871ad722932e12ee33c8856d0a9e5bb08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4768636
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182195}

--- components/global_media_controls/public/views/media_notification_view_ash_impl.cc
+++ components/global_media_controls/public/views/media_notification_view_ash_impl.cc
@@ -109,6 +109,10 @@ class MediaButton : public views::ImageButton {
         icon_size_);
   }
 
+  void UpdateText(int tooltip_text_id) {
+    SetTooltipText(l10n_util::GetStringUTF16(tooltip_text_id));
+  }
+
  private:
   const int icon_size_;
   const ui::ColorId foreground_color_id_;
@@ -272,7 +276,7 @@ MediaNotificationViewAshImpl::MediaNotificationViewAshImpl(
     start_casting_button_ = CreateMediaButton(
         controls_row, kNotMediaActionButtonId,
         media_message_center::kMediaCastStartIcon,
-        IDS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_ACTION_START_CASTING);
+        IDS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_ACTION_SHOW_DEVICE_LIST);
     start_casting_button_->SetCallback(base::BindRepeating(
         &MediaNotificationViewAshImpl::StartCastingButtonPressed,
         base::Unretained(this)));
@@ -542,9 +546,17 @@ void MediaNotificationViewAshImpl::UpdateCastingState() {
     // Use the ink drop color as the button background if user clicks the button
     // to show devices.
     views::InkDrop::Get(start_casting_button_)->GetInkDrop()->SnapToActivated();
+
+    // Indicate the user can hide the device list.
+    start_casting_button_->UpdateText(
+        IDS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_ACTION_HIDE_DEVICE_LIST);
   } else {
     // Hide the ink drop color if user clicks the button to hide devices.
     views::InkDrop::Get(start_casting_button_)->GetInkDrop()->SnapToHidden();
+
+    // Indicate the user can show the device list.
+    start_casting_button_->UpdateText(
+        IDS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_ACTION_SHOW_DEVICE_LIST);
   }
   device_selector_view_separator_->SetVisible(is_expanded);
 }
