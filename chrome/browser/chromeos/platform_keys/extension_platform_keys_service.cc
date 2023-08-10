1fd26ce45682d6a4d821711feb413938039149a0
Solomon Kinard <solomonkinard@chromium.org>
2023-08-10 17:39:50 +0000

    Extensions: ExtensionId: Use alias instead of std::string
    Context:
    crrev.com/c/4763085/1..2/extensions/browser/api/api_resource.h#b22
    
    This CL was uploaded by git cl split.
    
    Bug: chromium:1471741
    Change-Id: Ie8a07121929ac65e5f76cb7c2211559a455d71f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767221
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182160}

--- chrome/browser/chromeos/platform_keys/extension_platform_keys_service.cc
+++ chrome/browser/chromeos/platform_keys/extension_platform_keys_service.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/extension_id.h"
 #include "extensions/common/features/behavior_feature.h"
 #include "extensions/common/features/feature.h"
 #include "extensions/common/features/feature_provider.h"
@@ -194,7 +195,7 @@ class ExtensionPlatformKeysService::GenerateKeyTask : public Task {
   };
 
   GenerateKeyTask(platform_keys::TokenId token_id,
-                  std::string extension_id,
+                  extensions::ExtensionId extension_id,
                   GenerateKeyCallback callback,
                   ExtensionPlatformKeysService* service)
       : token_id_(token_id),
@@ -219,7 +220,7 @@ class ExtensionPlatformKeysService::GenerateKeyTask : public Task {
 
   platform_keys::TokenId token_id_;
   std::vector<uint8_t> public_key_spki_der_;
-  const std::string extension_id_;
+  const extensions::ExtensionId extension_id_;
   GenerateKeyCallback callback_;
   std::unique_ptr<platform_keys::ExtensionKeyPermissionsService>
       extension_key_permissions_service_;
@@ -571,7 +572,7 @@ class ExtensionPlatformKeysService::SignTask : public Task {
   const std::vector<uint8_t> public_key_spki_der_;
 
   KeystoreSigningScheme signing_scheme_;
-  const std::string extension_id_;
+  const extensions::ExtensionId extension_id_;
   SignCallback callback_;
   std::unique_ptr<platform_keys::ExtensionKeyPermissionsService>
       extension_key_permissions_service_;
@@ -875,7 +876,7 @@ class ExtensionPlatformKeysService::SelectTask : public Task {
   platform_keys::ClientCertificateRequest request_;
   std::unique_ptr<net::CertificateList> input_client_certificates_;
   const bool interactive_;
-  const std::string extension_id_;
+  const extensions::ExtensionId extension_id_;
   SelectCertificatesCallback callback_;
   const raw_ptr<content::WebContents> web_contents_;
   std::unique_ptr<platform_keys::ExtensionKeyPermissionsService>
