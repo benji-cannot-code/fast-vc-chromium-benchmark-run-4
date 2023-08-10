15cd0b80d8e38d6c59ad2f5be0ca5c79fb61260a
Alan Screen <awscreen@chromium.org>
2023-08-10 22:57:29 +0000

    Update OOPPD field trial config for desktop platforms
    Include all target platforms now, even the ones which are only enabled
    in Dev channel.
    
    Use a separate study for ChromeOS, to reflect existing experiment state
    for that platform which does not enable JobPrint feature.
    
    Use experiment names with current date, which is what will be used for
    an updated Finch experiment.
    
    Update PDFExtensionBasicPrintingTest to ensure that the command line is
    properly setup, which this test config illustrated was not happening.
    
    Bug: 809738
    Change-Id: I0dc858b4cfbf18d35f407c5bf25ad656e881b9e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764826
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182344}

--- chrome/browser/pdf/pdf_extension_printing_test.cc
+++ chrome/browser/pdf/pdf_extension_printing_test.cc
@@ -285,6 +285,7 @@ class PDFExtensionBasicPrintingTest : public PDFExtensionPrintingTest {
   // PDFExtensionPrintingTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitch(switches::kDisablePrintPreview);
+    PDFExtensionPrintingTest::SetUpCommandLine(command_line);
   }
 };
 
