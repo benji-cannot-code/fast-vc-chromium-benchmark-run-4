48cb22829e2a48ba722d255dfa3fd5e34efcbebd
Alexander Alekseev <alemate@chromium.org>
2023-08-10 00:04:27 +0000

    Add ChromeOS trace events in /chrome/browser/ash/android_sms
    Investigating ChromeOS login performance revealed that many
    potentially slow UI thread tasks are not instrumented.
    This Cl adds some events around slow tasks that we saw in
    our tests.
    
    Bug: b:293198304
    
    CC=​achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=khorimoto@chromium.org
    
    Change-Id: Ie541a45de9737bd295233914f7dc3831b1dd1b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764485
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181774}

--- chrome/browser/ash/android_sms/android_sms_service.cc
+++ chrome/browser/ash/android_sms/android_sms_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/default_clock.h"
 #include "base/timer/timer.h"
+#include "base/trace_event/trace_event.h"
 #include "chrome/browser/ash/android_sms/android_sms_app_setup_controller_impl.h"
 #include "chrome/browser/ash/android_sms/android_sms_urls.h"
 #include "chrome/browser/ash/android_sms/connection_manager.h"
@@ -56,6 +57,7 @@ void AndroidSmsService::Shutdown() {
 }
 
 void AndroidSmsService::OnSessionStateChanged() {
+  TRACE_EVENT0("login", "AndroidSmsService::OnSessionStateChanged");
   // ConnectionManager should not be created for blocked sessions.
   if (session_manager::SessionManager::Get()->IsUserSessionBlocked()) {
     return;
