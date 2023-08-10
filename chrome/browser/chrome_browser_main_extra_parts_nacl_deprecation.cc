268d7a91a4a7cb32e692c99b23e3a2feb59b5a49
Yuki Shiino <yukishiino@chromium.org>
2023-08-10 02:32:24 +0000

    Revert "Update disable NaCl logic to not use a flag on the browser process."
    This reverts commit 3a8d27bab4758e85185fcabd230443346539e2e2.
    
    Reason for revert: Suspicious about causing test failures of:
    ChromeBrowserMainExtraPartsNaclDeprecationTest.FieldTrialDisable
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/20246/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac12%20Tests/11848/overview
    
    Original change's description:
    > Update disable NaCl logic to not use a flag on the browser process.
    >
    > It is discouraged to add flags to the browser process, but flags are the
    > only way to propagate relevant state to renderers in a timely fashion.
    >
    > Change-Id: I53e2dca9ea3e8144b99d93bc28eae4994b152c2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742497
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1181739}
    
    Change-Id: Iedf1c24c7bffb4645647ddecad47bead27700512
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767601
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181844}

--- chrome/browser/chrome_browser_main_extra_parts_nacl_deprecation.cc
+++ chrome/browser/chrome_browser_main_extra_parts_nacl_deprecation.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/ppapi_utils.h"
 #include "chrome/common/pref_names.h"
-#include "components/nacl/common/buildflags.h"
 #include "components/prefs/pref_service.h"
 
 namespace {
@@ -38,9 +37,8 @@ BASE_FEATURE(kNaclAllow,
 );
 
 void ChromeBrowserMainExtraPartsNaclDeprecation::PostEarlyInitialization() {
-#if BUILDFLAG(ENABLE_NACL)
   if (!ShouldNaClBeAllowed()) {
-    DisallowNacl();
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        switches::kDisableNaCl);
   }
-#endif
 }
