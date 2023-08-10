57941611103c7c610edbb9aeeebc471fa3f6badc
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-10 14:32:53 +0000

    [Components] remote commands service uses scoped_observation
    This improve the safety that the queue properly stops to be observed.
    
    Bug: None
    Change-Id: I5c648d369ee7e1a335d7f9990f9e2769fe684f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767316
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182046}

--- components/policy/core/common/remote_commands/remote_commands_service.h
+++ components/policy/core/common/remote_commands/remote_commands_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
 #include "components/policy/core/common/cloud/policy_invalidation_scope.h"
 #include "components/policy/core/common/remote_commands/remote_command_job.h"
@@ -183,6 +184,9 @@ class POLICY_EXPORT RemoteCommandsService
 
   // Represents remote commands scope covered by service.
   const PolicyInvalidationScope scope_;
+
+  base::ScopedObservation<RemoteCommandsQueue, RemoteCommandsQueue::Observer>
+      remote_commands_queue_observation{this};
 
   base::WeakPtrFactory<RemoteCommandsService> weak_factory_{this};
 };
