bd8421d219d000e395bfc573ff12595bb2bae871
Sahir Vellani <savella@microsoft.com>
2023-08-10 02:50:06 +0000

    Add SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY to PenIdHandler
    Prevent priority inversion by
    using SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY().
    
    Change-Id: Ie08bc9920caec064f831cd5772e144891a55474b
    Bug: 1469434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740653
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181847}

--- ui/views/win/pen_id_handler.cc
+++ ui/views/win/pen_id_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/thread_pool.h"
+#include "base/threading/scoped_thread_priority.h"
 #include "base/win/com_init_util.h"
 #include "base/win/core_winrt_util.h"
 #include "base/win/hstring_reference.h"
@@ -38,6 +39,7 @@ class PenIdStatics {
     if (skip_initialization_) {
       return;
     }
+    SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY();
     base::win::AssertComInitialized();
     base::win::RoGetActivationFactory(
         base::win::HStringReference(
