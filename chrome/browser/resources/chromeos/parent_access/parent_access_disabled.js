21f7267eef3597ca83ab7873d03acf288fd27c87
Amber Haynes <amberhaynes@chromium.org>
2023-08-10 17:20:11 +0000

    Parent Access: Changed Dark Mode Background Color
    + Updated the PAW background color to cros-bg-color-elevation-3.
    + This necessitated the removal of the System Web Dialog default title
    bar, which has been replaced by a localized Parent Access title bar.
    + The title bar and close button were superimposed onto existing screens
    using a template.
    
    Updated Before Screen:
    Updated Authentication UI:
    Updated After Screen:
    Updated Error Screen:
    Updated Offline Screen:
    Original Authentication UI (for reference):
    
    Screenshots: 
    https: //screenshot.googleplex.com/7WJ3gSuch7XCoCP
    https: //screenshot.googleplex.com/7ppqjCcRiYzgV6P
    https: //screenshot.googleplex.com/8BUnss2KC5vUm7A
    https: //screenshot.googleplex.com/9McXbUdPyD35HjZ
    https: //screenshot.googleplex.com/AsvRJJgobT93CrV
    https: //screenshot.googleplex.com/3cMwXSDiqeKD35k
    Bug: b:283520196
    Change-Id: I4c5cb137aa2e881596caf5b5a0ca4e75cb90dd95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742706
    Commit-Queue: Amber Haynes <amberhaynes@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182152}

--- chrome/browser/resources/chromeos/parent_access/parent_access_disabled.js
+++ chrome/browser/resources/chromeos/parent_access/parent_access_disabled.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './parent_access_template.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
--- chrome/browser/resources/chromeos/parent_access/parent_access_ui.js
+++ chrome/browser/resources/chromeos/parent_access/parent_access_ui.js
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2021 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+import './parent_access_template.js';
 import 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
