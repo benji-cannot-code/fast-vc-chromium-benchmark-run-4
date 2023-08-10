b9da8dec33286c5707ac8e855d5bb2b2d5ffc7ac
Alexander Alekseev <alemate@chromium.org>
2023-08-10 00:05:34 +0000

    Add ChromeOS trace events in /chrome/browser/ash/login/saml
    Investigating ChromeOS login performance revealed that many
    potentially slow UI thread tasks are not instrumented.
    This Cl adds some events around slow tasks that we saw in
    our tests.
    
    Bug: b:293198304
    
    CC=​achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=rsorokin@google.com
    
    Change-Id: I8684550e0d020423b2ffaf844c252db43f98a042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763925
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181776}

--- chrome/browser/ash/login/saml/in_session_password_sync_manager.cc
+++ chrome/browser/ash/login/saml/in_session_password_sync_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/functional/callback_helpers.h"
 #include "base/time/default_clock.h"
+#include "base/trace_event/trace_event.h"
 #include "chrome/browser/ash/login/auth/chrome_safe_mode_delegate.h"
 #include "chrome/browser/ash/login/helper.h"
 #include "chrome/browser/ash/login/login_pref_names.h"
@@ -99,6 +100,7 @@ void InSessionPasswordSyncManager::SetClockForTesting(
 void InSessionPasswordSyncManager::Shutdown() {}
 
 void InSessionPasswordSyncManager::OnSessionStateChanged() {
+  TRACE_EVENT0("login", "InSessionPasswordSyncManager::OnSessionStateChanged");
   if (!session_manager::SessionManager::Get()->IsScreenLocked()) {
     // We are unlocking the session, no further action required.
     return;
