a82151a0b25d6b113a272341de990fa16b7ee99f
Jason Hu <hujasonx@google.com>
2023-08-10 19:52:43 +0000

    [CSC] Disable companion when Lens standalone is disabled
    Bug: 1472024
    Change-Id: Ib4c452b9a9232fdb5f602c35f630b782a8aa922d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4769607
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182241}

--- chrome/browser/ui/side_panel/companion/companion_utils.cc
+++ chrome/browser/ui/side_panel/companion/companion_utils.cc
@@ -15,12 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/lens/lens_features.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/web_contents.h"
 
 namespace companion {
 
 bool IsCompanionFeatureEnabled() {
+  if (!base::FeatureList::IsEnabled(lens::features::kLensStandalone)) {
+    return false;
+  }
   return base::FeatureList::IsEnabled(
              features::internal::kSidePanelCompanion) ||
          base::FeatureList::IsEnabled(
