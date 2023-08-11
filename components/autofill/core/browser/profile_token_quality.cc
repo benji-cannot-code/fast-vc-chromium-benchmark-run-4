b9316a4e37ec0527799cb28847dd1eadef4bcf2e
Florian Leimgruber <fleimgruber@google.com>
2023-08-11 14:45:08 +0000

    Add ProfileTokenQualityTestApi.
    This allows keeping the observation hashes private, but enables exposing
    them for testing (for AutofillTable + Sync related code).
    
    will only be used in the follow-up crrev.com/c/4720559 - but it's
    already added here to keep that CL smaller.
    
    Low-Coverage-Reason: This is test only code. GetHashesForStoredType()
    Bug: 1453650
    Change-Id: If78647484a17cc82d030f6e49bfb3111f8c0167d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4768207
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182569}

--- components/autofill/core/browser/profile_token_quality.cc
+++ components/autofill/core/browser/profile_token_quality.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check.h"
+#include "base/check_deref.h"
 #include "base/containers/circular_deque.h"
 #include "base/containers/contains.h"
 #include "base/containers/cxx20_erase.h"
@@ -195,9 +196,7 @@ ObservationType GetObservationTypeForEditedField(
 }  // namespace
 
 ProfileTokenQuality::ProfileTokenQuality(AutofillProfile* profile)
-    : profile_(profile) {
-  CHECK(profile);
-}
+    : profile_(CHECK_DEREF(profile)) {}
 
 ProfileTokenQuality::ProfileTokenQuality(const ProfileTokenQuality& other) =
     default;
@@ -275,12 +274,6 @@ void ProfileTokenQuality::SaveObservationsForFilledFormForAllSubmittedProfiles(
       // `AutofillTable` supports storing token quality.
     }
   }
-}
-
-void ProfileTokenQuality::AddObservationForTesting(
-    ServerFieldType field_type,
-    ObservationType observation_type) {
-  AddObservation(field_type, Observation{.type = observation_type});
 }
 
 std::vector<ObservationType>
--- components/autofill/core/browser/profile_token_quality_unittest.cc
+++ components/autofill/core/browser/profile_token_quality_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_structure.h"
+#include "components/autofill/core/browser/profile_token_quality_test_api.h"
 #include "components/autofill/core/browser/test_autofill_client.h"
 #include "components/autofill/core/browser/test_autofill_driver.h"
 #include "components/autofill/core/browser/test_browser_autofill_manager.h"
@@ -108,7 +109,7 @@ TEST_F(ProfileTokenQualityTest, GetObservationTypesForFieldType) {
 
   EXPECT_TRUE(quality.GetObservationTypesForFieldType(NAME_FIRST).empty());
 
-  quality.AddObservationForTesting(NAME_FIRST, ObservationType::kAccepted);
+  test_api(quality).AddObservation(NAME_FIRST, ObservationType::kAccepted);
   EXPECT_THAT(quality.GetObservationTypesForFieldType(NAME_FIRST),
               UnorderedElementsAre(ObservationType::kAccepted));
   EXPECT_TRUE(quality.GetObservationTypesForFieldType(NAME_LAST).empty());
@@ -116,7 +117,7 @@ TEST_F(ProfileTokenQualityTest, GetObservationTypesForFieldType) {
   // Test that if more than `kMaxObservationsPerToken` observations are added,
   // only the first `kMaxObservationsPerToken` are returned.
   for (size_t i = 0; i < ProfileTokenQuality::kMaxObservationsPerToken; i++) {
-    quality.AddObservationForTesting(NAME_FIRST,
+    test_api(quality).AddObservation(NAME_FIRST,
                                      ObservationType::kEditedToSimilarValue);
   }
   EXPECT_THAT(quality.GetObservationTypesForFieldType(NAME_FIRST),
