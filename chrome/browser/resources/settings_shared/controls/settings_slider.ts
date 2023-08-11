e7c47310b9d933b063b98d69fc198db0d9aa86ac
wenyu zhang <zhangwenyu@google.com>
2023-08-11 16:32:43 +0000

    os settings: Fix aria-disabled bug in settings_slider
    After updating the disabled status of the settings-slider element, the
    ChromeVox still announces the previous status. The issue is that the
    aria-disabled attribute is not being passed down to the cr-slider
    element.
    
    Before fix:
    http://screencast/cast/NjY5NDEwMDczMTQyODg2NHxlMGRiOTQ2MS1iNg
    After fix: http://screencast/cast/NjA5MDY4OTA5OTA3MTQ4OHxhODI4OGI3Ny1kNw
    
    In mouse settings, turning off the "Scroll acceleration" button will
    enable "Scrolling speed" slider, vise verse. In the screencast before
    the fix, in the beginning, "Scroll acceleration" button is on, the
    "Scrolling speed" slider is disabled. The ChromeVox announces correctly.
    However, after turning off the "Scroll acceleration" button, the
    "Scrolling speed" slider is enabled now, but the ChromeVox still
    announces it as disabled.
    
    In the screencast after the fix, the ChromeVox always announces the
    slider status correctly.
    
    Bug: b:295022565, b:292079653
    Test: Verified in DUT
    Change-Id: I23c4c4a454899139dea33a2c2b6829b0252091f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4769507
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182617}

--- chrome/browser/resources/settings_shared/controls/settings_slider.ts
+++ chrome/browser/resources/settings_shared/controls/settings_slider.ts
@@ -75,6 +75,9 @@ export class SettingsSliderElement extends SettingsSliderElementBase {
 
       disabled: Boolean,
 
+      // The value of ariaDisabled should only be "true" or "false".
+      ariaDisabled: String,
+
       showMarkers: Boolean,
 
       disableSlider_: {
@@ -111,6 +114,8 @@ export class SettingsSliderElement extends SettingsSliderElementBase {
   private disableSlider_: boolean;
   updateValueInstantly: boolean;
   private loaded_: boolean;
+
+  override ariaDisabled: string;
 
   override connectedCallback() {
     super.connectedCallback();
