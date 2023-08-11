62489e349eb8a5c10630036a938440769339f643
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-08-11 22:18:25 +0000

    [css-grid] Enable LayoutNewGridStaticPosition
    While test changes are being reviewed lets enable for M118.
    
    Bug: 1454831
    Change-Id: I21fd052fc0892155fd233bb850f3c867c578a86e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775367
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1182813}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm_test.cc
@@ -1392,6 +1392,10 @@ TEST_F(NGGridLayoutAlgorithmTest, ItemsSizeWithGap) {
 }
 
 TEST_F(NGGridLayoutAlgorithmTest, PositionedOutOfFlowItems) {
+  if (RuntimeEnabledFeatures::LayoutNewGridStaticPositionEnabled()) {
+    return;
+  }
+
   SetBodyInnerHTML(R"HTML(
     <style>
       #grid {
