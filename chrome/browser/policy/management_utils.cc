c9dda6acfecaa297416d65aecaa7acfd124a5cf7
Yann Dago <ydago@chromium.org>
2023-08-11 16:23:09 +0000

    Replace policy::IsDeviceCloudManaged uses by ManagementService calls
    Bug: 1471219
    Change-Id: I85116fb3c187cb5f82e4b41d854102757223f6be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762065
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182610}

--- chrome/browser/policy/management_utils.cc
+++ chrome/browser/policy/management_utils.cc
@@ -38,12 +38,4 @@ bool IsDeviceEnterpriseManaged() {
 #endif
 }
 
-bool IsDeviceCloudManaged() {
-#if BUILDFLAG(IS_CHROMEOS)
-  return IsDeviceEnterpriseManaged();
-#else
-  return policy::BrowserDMTokenStorage::Get()->RetrieveDMToken().is_valid();
-#endif
-}
-
 }  // namespace policy
--- chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
+++ chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/task/sequenced_task_runner.h"
+#include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/enterprise/connectors/connectors_service.h"
 #include "chrome/browser/enterprise/util/affiliation.h"
 #include "chrome/browser/policy/dm_token_utils.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/policy/core/common/cloud/dm_token.h"
+#include "components/policy/core/common/management/management_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/browser/realtime/policy_engine.h"
 #include "components/safe_browsing/core/browser/realtime/url_lookup_service_base.h"
@@ -65,7 +67,9 @@ bool ChromeEnterpriseRealTimeUrlLookupService::
 
   // Don't allow using the access token if the managed profile doesn't match the
   // managed device.
-  if (policy::IsDeviceCloudManaged() &&
+  if (policy::ManagementServiceFactory::GetForProfile(profile_)
+          ->HasManagementAuthority(
+              policy::EnterpriseManagementAuthority::CLOUD_DOMAIN) &&
       !chrome::enterprise_util::IsProfileAffiliated(profile_)) {
     return false;
   }
