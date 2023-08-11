85da5585656b1414d9fbc54574a0a5b35ecb46ce
Maria Kazinova <kazinova@google.com>
2023-08-11 16:44:35 +0000

    [Passwords, FPF] Add a class caching the potential username data.
    This CL adds FieldDataManager class that will keep track of potential
    username fields in forgot password flows, and a corresponding factory.
    
    Bug: 1468297
    Change-Id: Id1dba40377269293c35d788ecc73e2992fd421ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771014
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Din Nezametdinov <shaikhitdin@google.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182629}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -104,6 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/page_load_metrics_memory_tracker_factory.h"
 #include "chrome/browser/password_manager/account_password_store_factory.h"
 #include "chrome/browser/password_manager/bulk_leak_check_service_factory.h"
+#include "chrome/browser/password_manager/field_info_manager_factory.h"
 #include "chrome/browser/password_manager/password_manager_settings_service_factory.h"
 #include "chrome/browser/password_manager/password_reuse_manager_factory.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
@@ -715,6 +716,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if !BUILDFLAG(IS_ANDROID)
   feedback::FeedbackUploaderFactoryChrome::GetInstance();
 #endif
+  FieldInfoManagerFactory::GetInstance();
   FileSystemAccessPermissionContextFactory::GetInstance();
   FindBarStateFactory::GetInstance();
   first_party_sets::FirstPartySetsPolicyServiceFactory::GetInstance();
