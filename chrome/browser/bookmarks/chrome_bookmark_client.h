9ce993a69deb27fbb7b6218bd813d71bcc227921
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-11 16:23:59 +0000

    [Bookmarks] Chrome bookmark client uses scoped observation
    Previously, the offline_page_observer_ never stopped observing the
    bookmark. Using scoped observation ensure that, when the bookmark
    client is destructed, the observation halts.
    
    This CL works because, since crrev.com/c/4755448, the client is
    destructed before the model’s observer list. It use not to be the
    case, which caused the `RemoveObserver` call to fail as the list was
    destructed.
    
    
    
    Change-Id: I1d140149b27ec7685310fb9270dd3f34829fda80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755912
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182611}

--- chrome/browser/bookmarks/chrome_bookmark_client.h
+++ chrome/browser/bookmarks/chrome_bookmark_client.h
@@ -13,8 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/task/deferred_sequenced_task_runner.h"
+#include "components/bookmarks/browser/base_bookmark_model_observer.h"
 #include "components/bookmarks/browser/bookmark_client.h"
+#include "components/bookmarks/browser/bookmark_model.h"
 #include "components/offline_pages/buildflags/buildflags.h"
 
 class BookmarkUndoService;
@@ -97,6 +100,12 @@ class ChromeBookmarkClient : public bookmarks::BookmarkClient {
   // Owns the observer used by Offline Page listening to Bookmark Model events.
   std::unique_ptr<offline_pages::OfflinePageBookmarkObserver>
       offline_page_observer_;
+
+  // Observation of this by the bookmark model.
+  std::unique_ptr<base::ScopedObservation<bookmarks::BookmarkModel,
+                                          bookmarks::BaseBookmarkModelObserver>>
+      model_observation_{};
+
 #endif
 };
 
