d945cf20a81597106bddc2c2e5b246c5faaf8cc6
dljames <dljames@google.com>
2023-08-11 00:52:39 +0000

    [ SavedTabGroups ] Remove tabs from storage without notifying sync
    The sync bridge will only send the group and none of its tabs when a local deletion occurs. This will allow us to consistently update the Tab Strip when we receive a group deletion message since we no longer need to worry about the order of messages (Ex: Was this tab part of the group deletion? Or was it manually deleted).
    
    - Reduces the discard threshold for orphaned tabs from 90 days -> 30 days
    - Leaves orphaned tabs when a group deletion occurs.
    
    Bug: 1470922
    Change-Id: Id989434c6a020146c2a69a3fb59bf23bb5529dc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727283
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182390}

--- components/saved_tab_groups/saved_tab_group_sync_bridge.cc
+++ components/saved_tab_groups/saved_tab_group_sync_bridge.cc
@@ -34,8 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/protocol/saved_tab_group_specifics.pb.h"
 
 namespace {
-constexpr base::TimeDelta discard_orphaned_tabs_threshold =
-    base::Microseconds(base::Time::kMicrosecondsPerDay * 90);
+
+// Discard orphaned tabs after 30 days if the associated group cannot be found.
+constexpr base::TimeDelta kDiscardOrphanedTabsThreshold = base::Days(30);
 
 std::unique_ptr<syncer::EntityData> CreateEntityData(
     std::unique_ptr<sync_pb::SavedTabGroupSpecifics> specific) {
@@ -220,18 +221,19 @@ void SavedTabGroupSyncBridge::SavedTabGroupRemovedLocally(
   std::unique_ptr<syncer::ModelTypeStore::WriteBatch> write_batch =
       store_->CreateWriteBatch();
 
+  // Intentionally only remove the group (creating orphaned tabs in the
+  // process), so other devices with the group open in the Tabstrip can react to
+  // the deletion appropriately (i.e. We do not have to determine if a tab
+  // deletion was part of a group deletion).
   RemoveEntitySpecific(removed_group->saved_guid(), write_batch.get());
-  for (const SavedTabGroupTab& tab : removed_group->saved_tabs()) {
-    RemoveEntitySpecific(tab.saved_tab_guid(), write_batch.get());
-  }
 
   store_->CommitWriteBatch(
       std::move(write_batch),
       base::BindOnce(&SavedTabGroupSyncBridge::OnDatabaseSave,
                      weak_ptr_factory_.GetWeakPtr()));
 
-  // Update the ModelTypeStore (local storage) and sync with the new positions 
-  // of all the groups after a remove has occurred so the positions are 
+  // Update the ModelTypeStore (local storage) and sync with the new positions
+  // of all the groups after a remove has occurred so the positions are
   // preserved on browser restart. See crbug/1462443.
   SavedTabGroupReorderedLocally();
 }
@@ -415,8 +417,9 @@ void SavedTabGroupSyncBridge::ResolveTabsMissingGroups(
           base::Microseconds(tab_iterator->update_time_windows_epoch_micros()));
       base::Time now = base::Time::Now();
 
-      // Discard orphaned tabs that have not been updated for 90 days.
-      if (now - last_update_time >= discard_orphaned_tabs_threshold) {
+      // Discard the tabs that do not have an associated group and have not been
+      // updated within the discard threshold.
+      if (now - last_update_time >= kDiscardOrphanedTabsThreshold) {
         RemoveEntitySpecific(base::Uuid::ParseLowercase(tab_iterator->guid()),
                              write_batch);
         tab_iterator = tabs_missing_groups_.erase(tab_iterator);
